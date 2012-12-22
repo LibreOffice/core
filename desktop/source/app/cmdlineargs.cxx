LibreOffice/core

commit f4a4ba9ac1b58b4726825400e1edd1bf47d4080a

Clean up option processing
* Support --version on non-UNX, too.

* Consistently show the first unknown option and the help blob in the presence
  of any unknown options.

* There is no need to tunnel --help/--version past oosplash in the soffice
  script, as oosplash is prepared to treat them adequately (esp. not pass them
  over any pipe); this only added unnecessary variance to what spellings exactly
  are supported and how mixtures of --help, --version, and unknown options are
  handled.

Change-Id: I617f2e727e2f0eafd34a2de3b85d441c6783ec4f

desktop/source/app/cmdlineargs.cxx
@@ -287,26 +287,25 @@ void CommandLineArgs::ParseCommandLine_Impl( Supplier& supplier )
                     {
                         bConversionOutEvent = true;
                     }
-#if defined UNX
                     else
                     // because it's impossible to filter these options that
                     // are handled in the soffice shell script with the
                     // primitive tools that /bin/sh offers, ignore them here
-                    if (!oArg.equalsIgnoreAsciiCaseAsciiL(RTL_CONSTASCII_STRINGPARAM("backtrace")) &&
+                    if (
+#if defined UNX
+                        !oArg.equalsIgnoreAsciiCaseAsciiL(RTL_CONSTASCII_STRINGPARAM("backtrace")) &&
                         !oArg.equalsIgnoreAsciiCaseAsciiL(RTL_CONSTASCII_STRINGPARAM("strace")) &&
                         !oArg.equalsIgnoreAsciiCaseAsciiL(RTL_CONSTASCII_STRINGPARAM("valgrind")) &&
                     // for X Session Management, handled in
                     // vcl/unx/generic/app/sm.cxx:
                         !oArg.match("session=") &&
-                    //ignore additional legacy options that don't do anything anymore
-                        !oArg.equalsIgnoreAsciiCaseAsciiL(RTL_CONSTASCII_STRINGPARAM("nocrashreport")))
-                    {
-                        fprintf(stderr, "Unknown option %s\n",
-                            rtl::OUStringToOString(aArg, osl_getThreadTextEncoding()).getStr());
-                        fprintf(stderr, "Run 'soffice --help' to see a full list of available command line options.\n");
-                        m_unknown = true;
-                    }
 #endif
+                    //ignore additional legacy options that don't do anything anymore
+                        !oArg.equalsIgnoreAsciiCaseAsciiL(RTL_CONSTASCII_STRINGPARAM("nocrashreport")) &&
+                        m_unknown.isEmpty())
+                    {
+                        m_unknown = aArg;
+                    }
                 }
                 else
                 {
@@ -624,7 +623,6 @@ void CommandLineArgs::InitParamValues()
     m_helpbase = false;
     m_psn = false;
     m_version = false;
-    m_unknown = false;
     m_splashpipe = false;
     m_bEmpty = true;
     m_bDocumentArgs  = false;
@@ -762,7 +760,7 @@ bool CommandLineArgs::IsVersion() const
     return m_version;
 }
 
-bool CommandLineArgs::HasUnknown() const
+OUString CommandLineArgs::GetUnknown() const
 {
     return m_unknown;
 }

desktop/scripts/soffice.sh
@@ -158,21 +158,5 @@ if [ -n "$VALGRINDCHECK" -a -z "$VALGRIND" ] ; then
     exec &>valgrind.log
 fi
 
-# do not pass the request for command line help to oosplash
-for arg in $@ ; do
-    case "$arg" in
-        -h | --h | --he | --hel | --help)
-            "$sd_prog/soffice.bin" --help
-            exit 0
-            ;;
-        -V | --v | --ve | --ver | --vers | --versi | --versio | --version)
-            "$sd_prog/soffice.bin" --version
-            exit 0
-            ;;
-        *)
-            ;;
-    esac
-done
-
 # oosplash does the rest: forcing pages in, javaldx etc. are
 exec $VALGRINDCHECK $STRACECHECK "$sd_prog/oosplash" "$@"

desktop/source/app/app.cxx
@@ -634,7 +634,8 @@ void Desktop::Init()
             // 2nd office startup should terminate after sending cmdlineargs through pipe
             SetBootstrapStatus(BS_TERMINATE);
         }
-        else if ( rCmdLineArgs.IsHelp() )
+        else if ( !rCmdLineArgs.GetUnknown().isEmpty()
+                  || rCmdLineArgs.IsHelp() || rCmdLineArgs.IsVersion() )
         {
             // disable IPC thread in an instance that is just showing a help message
             OfficeIPCThread::DisableOfficeIPCThread();
@@ -1388,6 +1389,22 @@ int Desktop::Main()
         new DesktopContext( com::sun::star::uno::getCurrentContext() ) );
 
     CommandLineArgs& rCmdLineArgs = GetCommandLineArgs();
+    OUString aUnknown( rCmdLineArgs.GetUnknown() );
+    if ( !aUnknown.isEmpty() )
+    {
+        displayCmdlineHelp( aUnknown );
+        return EXIT_FAILURE;
+    }
+    if ( rCmdLineArgs.IsHelp() )
+    {
+        displayCmdlineHelp( OUString() );
+        return EXIT_SUCCESS;
+    }
+    if ( rCmdLineArgs.IsVersion() )
+    {
+        displayVersion();
+        return EXIT_SUCCESS;
+    }
 
     // setup configuration error handling
     ConfigurationErrorHandler aConfigErrHandler;
@@ -1430,14 +1447,6 @@ int Desktop::Main()
         RegisterServices();
 
         SetSplashScreenProgress(25);
-
-#ifndef UNX
-        if ( rCmdLineArgs.IsHelp() )
-        {
-            displayCmdlineHelp();
-            return EXIT_SUCCESS;
-        }
-#endif
 
         // check user installation directory for lockfile so we can be sure
         // there is no other instance using our data files from a remote host
@@ -2696,10 +2705,10 @@ void Desktop::HandleAppEvent( const ApplicationEvent& rAppEvent )
         }
         break;
     case ApplicationEvent::TYPE_HELP:
-#ifndef UNX
-        // in non unix version allow showing of cmdline help window
-        displayCmdlineHelp();
-#endif
+        displayCmdlineHelp(rAppEvent.GetData());
+        break;
+    case ApplicationEvent::TYPE_VERSION:
+        displayVersion();
         break;
     case ApplicationEvent::TYPE_OPEN:
         {

desktop/source/app/cmdlineargs.hxx
@@ -93,8 +93,9 @@ class CommandLineArgs: private boost::noncopyable
         bool                IsWeb() const;
         bool                IsVersion() const;
         bool                HasModuleParam() const;
-        bool                HasUnknown() const;
         bool                WantsToLoadDocument() const;
+
+        OUString            GetUnknown() const;
 
         // Access to string parameters
         bool                    HasSplashPipe() const;
@@ -153,8 +154,9 @@ class CommandLineArgs: private boost::noncopyable
         bool m_helpbase;
         bool m_psn;
         bool m_version;
-        bool m_unknown;
         bool m_splashpipe;
+
+        OUString m_unknown;
 
         bool m_bEmpty; // No Args at all
         bool m_bDocumentArgs; // A document creation/open/load arg is used

desktop/source/app/cmdlinehelp.cxx
@@ -136,7 +136,7 @@ namespace desktop
 
     rtl::OUString ReplaceStringHookProc(const rtl::OUString& rStr);
 
-    void displayCmdlineHelp()
+    void displayCmdlineHelp(OUString const & unknown)
     {
         // if you put variables in other chunks don't forget to call the replace routines
         // for those chunks...
@@ -147,6 +147,11 @@ namespace desktop
         String aHelpMessage_bottom(aCmdLineHelp_bottom, RTL_TEXTENCODING_ASCII_US);
         aHelpMessage_version = ReplaceStringHookProc(aHelpMessage_version);
         aHelpMessage_head.SearchAndReplaceAscii( "%CMDNAME", String( "soffice", RTL_TEXTENCODING_ASCII_US) );
+        if (!unknown.isEmpty())
+        {
+            aHelpMessage_head = "Unknown option: " + unknown + "\n\n"
+                + aHelpMessage_head;
+        }
 #ifdef UNX
         // on unix use console for output
         fprintf(stdout, "%s%s",

desktop/source/app/cmdlinehelp.hxx
@@ -5,7 +5,7 @@
 
 namespace desktop
 {
-    void displayCmdlineHelp( void );
+    void displayCmdlineHelp( OUString const & unknown );
     void displayVersion();
 #ifndef UNX
     class CmdlineHelpDialog : public ModalDialog

desktop/source/app/sofficemain.cxx
@@ -60,21 +60,24 @@ extern "C" int DESKTOP_DLLPUBLIC soffice_main()
     // handle --version and --help already here, otherwise they would be handled
     // after VCL initialization that might fail if $DISPLAY is not set
     const desktop::CommandLineArgs& rCmdLineArgs = aDesktop.GetCommandLineArgs();
+    OUString aUnknown( rCmdLineArgs.GetUnknown() );
+    if ( !aUnknown.isEmpty() )
+    {
+        desktop::Desktop::InitApplicationServiceManager();
+        desktop::displayCmdlineHelp( aUnknown );
+        return EXIT_FAILURE;
+    }
     if ( rCmdLineArgs.IsHelp() )
     {
         desktop::Desktop::InitApplicationServiceManager();
-        desktop::displayCmdlineHelp();
+        desktop::displayCmdlineHelp( OUString() );
         return EXIT_SUCCESS;
     }
-    else if ( rCmdLineArgs.IsVersion() )
+    if ( rCmdLineArgs.IsVersion() )
     {
         desktop::Desktop::InitApplicationServiceManager();
         desktop::displayVersion();
         return EXIT_SUCCESS;
-    }
-    else if ( rCmdLineArgs.HasUnknown() )
-    {
-        return EXIT_FAILURE;
     }
 #endif
     return SVMain();

commit dc09fc671bda9907080bbd3541ff59e9bbf80a9b

fdo#57203 Unknown option: -Embedding
Ignore this option on Windows.
This work-arounds a regression introduced by f4a4ba9ac
Windows appends -Embedding when LibreOffice is called by COM+
Before f4a4ba9ac this option was silently ignored.

Change-Id: I32cbfa276a6699eda99b87580cf9ef23366c40bc

desktop/source/app/cmdlineargs.cxx
@@ -502,6 +502,15 @@ bool CommandLineArgs::InterpretCommandLineParameter( const ::rtl::OUString& aArg
         return true;
     }
 #endif
+#ifdef WIN32
+    /* fdo#57203 ignore -Embedding on Windows
+       when LibreOffice is launched by COM+
+     */
+    else if ( oArg.equalsAsciiL( RTL_CONSTASCII_STRINGPARAM( "Embedding" )) )
+    {
+        return true;
+    }
+#endif
     else if ( oArg.matchIgnoreAsciiCaseAsciiL(RTL_CONSTASCII_STRINGPARAM("infilter=")))
     {
         m_infilter.push_back(oArg.copy(RTL_CONSTASCII_LENGTH("infilter=")));