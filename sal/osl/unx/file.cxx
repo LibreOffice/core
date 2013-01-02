52ad6005d3350127234667384266ae7a368fe067
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2013-01-02 11:54:08 +0100

    sal: use pread / pwrite on every *nix
    pread / pwrite don't look as a Linux and Solaris privilege.
    
    Change-Id: Ifb2e88445d4064c13a406007bfd523ae0caa38e5

--- sal/osl/unx/file.cxx
+++ sal/osl/unx/file.cxx
@@ -379,8 +379,6 @@ oslFileError FileHandle_Impl::readAt (
         return osl_File_E_None;
     }
 
-#if defined(LINUX) || defined(SOLARIS)
-
     ssize_t nBytes = ::pread (m_fd, pBuffer, nBytesRequested, nOffset);
     if ((-1 == nBytes) && (EOVERFLOW == errno))
     {
@@ -392,22 +390,6 @@ oslFileError FileHandle_Impl::readAt (
     }
     if (-1 == nBytes)
         return oslTranslateFileError (OSL_FET_ERROR, errno);
-
-#else /* !(LINUX || SOLARIS) */
-
-    if (nOffset != m_offset)
-    {
-        if (-1 == ::lseek (m_fd, nOffset, SEEK_SET))
-            return oslTranslateFileError (OSL_FET_ERROR, errno);
-        m_offset = nOffset;
-    }
-
-    ssize_t nBytes = ::read (m_fd, pBuffer, nBytesRequested);
-    if (-1 == nBytes)
-        return oslTranslateFileError (OSL_FET_ERROR, errno);
-    m_offset += nBytes;
-
-#endif /* !(LINUX || SOLARIS) */
 
     OSL_FILE_TRACE("FileHandle_Impl::readAt(%d, %lld, %ld)", m_fd, nOffset, nBytes);
     *pBytesRead = nBytes;
@@ -428,27 +410,9 @@ oslFileError FileHandle_Impl::writeAt (
     if (!(m_state & STATE_WRITEABLE))
         return osl_File_E_BADF;
 
-#if defined(LINUX) || defined(SOLARIS)
-
     ssize_t nBytes = ::pwrite (m_fd, pBuffer, nBytesToWrite, nOffset);
     if (-1 == nBytes)
         return oslTranslateFileError (OSL_FET_ERROR, errno);
-
-#else /* !(LINUX || SOLARIS) */
-
-    if (nOffset != m_offset)
-    {
-        if (-1 == ::lseek (m_fd, nOffset, SEEK_SET))
-            return oslTranslateFileError (OSL_FET_ERROR, errno);
-        m_offset = nOffset;
-    }
-
-    ssize_t nBytes = ::write (m_fd, pBuffer, nBytesToWrite);
-    if (-1 == nBytes)
-        return oslTranslateFileError (OSL_FET_ERROR, errno);
-    m_offset += nBytes;
-
-#endif /* !(LINUX || SOLARIS) */
 
     OSL_FILE_TRACE("FileHandle_Impl::writeAt(%d, %lld, %ld)", m_fd, nOffset, nBytes);
     m_size = std::max (m_size, sal::static_int_cast< sal_uInt64 >(nOffset + nBytes));
