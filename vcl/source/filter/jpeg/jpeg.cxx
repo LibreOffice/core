be98e8f1da869f4b757ad42de066f92d3f0eb56f
Jan Holesovsky <kendy@suse.cz>
2013-01-04 22:02:34 +0100

    Fix build on linux.
    Change-Id: I5753a822e9f18d1afa831ece6134e030f88ad47f
    Signed-off-by: Cor Nouws <oolst@nouenoff.nl>
    Signed-off-by: Olivier Hallot <olivier.hallot@documentfoundation.org>
    Signed-off-by: Adolfo Jayme Barrientos <fitoschido@ubuntu.com>
    Signed-off-by: Michael Meeks <michael.meeks@suse.com>

--- vcl/source/filter/jpeg/jpeg.cxx
+++ vcl/source/filter/jpeg/jpeg.cxx
@@ -24,8 +24,8 @@ extern "C"
 {
     #include "stdio.h"
     #include "jpeg.h"
-    #include <jpeg/jpeglib.h>
-    #include <jpeg/jerror.h>
+    #include <jpeglib.h>
+    #include <jerror.h>
 }
 
 #define _JPEGPRIVATE
