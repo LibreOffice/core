LibreOffice/core

commit 6da90944b5134d65bfbd7dcaf024956aa139efc1

ugly memory leak
Change-Id: I200713987eb1c19f7d795190e7acef02af569cc8

svx/source/tbxctrls/tbunosearchcontrollers.cxx
@@ -767,25 +767,25 @@ void SAL_CALL FindbarDispatcher::removeStatusListener( const css::uno::Reference
 css::uno::Reference< css::uno::XInterface > SAL_CALL FindTextToolbarController_createInstance(
     const css::uno::Reference< css::lang::XMultiServiceFactory >& rSMgr )
 {
-    return *new FindTextToolbarController( rSMgr );
+    return FindTextToolbarController( rSMgr );
 }
 
 css::uno::Reference< css::uno::XInterface > SAL_CALL DownSearchToolboxController_createInstance(
     const css::uno::Reference< css::lang::XMultiServiceFactory >& rSMgr )
 {
-    return *new UpDownSearchToolboxController( rSMgr, UpDownSearchToolboxController::DOWN );
+    return UpDownSearchToolboxController( rSMgr, UpDownSearchToolboxController::DOWN );
 }
 
 css::uno::Reference< css::uno::XInterface > SAL_CALL UpSearchToolboxController_createInstance(
     const css::uno::Reference< css::lang::XMultiServiceFactory >& rSMgr )
 {
-    return *new UpDownSearchToolboxController( rSMgr, UpDownSearchToolboxController::UP );
+    return UpDownSearchToolboxController( rSMgr, UpDownSearchToolboxController::UP );
 }
 
 css::uno::Reference< css::uno::XInterface > SAL_CALL FindbarDispatcher_createInstance(
     const css::uno::Reference< css::lang::XMultiServiceFactory >& rSMgr )
 {
-    return *new FindbarDispatcher( rSMgr );
+    return FindbarDispatcher( rSMgr );
 }
 
 //-----------------------------------------------------------------------------------------------------------

commit 5637b244cfe0c6b886188f71d3f974ef72abb9cd

UNO objects must be allocated on the heap
This was a regression introduced with 6da90944b5134d65bfbd7dcaf024956aa139efc1
"ugly memory leak."

Change-Id: Ie976d7ddcdac974aeeba14ffd64c4641b5d096a4

svx/source/tbxctrls/tbunosearchcontrollers.cxx
@@ -767,25 +767,30 @@ void SAL_CALL FindbarDispatcher::removeStatusListener( const css::uno::Reference
 css::uno::Reference< css::uno::XInterface > SAL_CALL FindTextToolbarController_createInstance(
     const css::uno::Reference< css::lang::XMultiServiceFactory >& rSMgr )
 {
-    return FindTextToolbarController( rSMgr );
+    return static_cast< cppu::OWeakObject * >(
+        new FindTextToolbarController( rSMgr ) );
 }
 
 css::uno::Reference< css::uno::XInterface > SAL_CALL DownSearchToolboxController_createInstance(
     const css::uno::Reference< css::lang::XMultiServiceFactory >& rSMgr )
 {
-    return UpDownSearchToolboxController( rSMgr, UpDownSearchToolboxController::DOWN );
+    return static_cast< cppu::OWeakObject * >(
+        new UpDownSearchToolboxController(
+            rSMgr, UpDownSearchToolboxController::DOWN ) );
 }
 
 css::uno::Reference< css::uno::XInterface > SAL_CALL UpSearchToolboxController_createInstance(
     const css::uno::Reference< css::lang::XMultiServiceFactory >& rSMgr )
 {
-    return UpDownSearchToolboxController( rSMgr, UpDownSearchToolboxController::UP );
+    return static_cast< cppu::OWeakObject * >(
+        new UpDownSearchToolboxController(
+            rSMgr, UpDownSearchToolboxController::UP ) );
 }
 
 css::uno::Reference< css::uno::XInterface > SAL_CALL FindbarDispatcher_createInstance(
     const css::uno::Reference< css::lang::XMultiServiceFactory >& rSMgr )
 {
-    return FindbarDispatcher( rSMgr );
+    return static_cast< cppu::OWeakObject * >( new FindbarDispatcher( rSMgr ) );
 }
 
 //-----------------------------------------------------------------------------------------------------------