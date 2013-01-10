4c80126ebc7f827f2b3dc4ba6ab8ccc6900136dc
Miklos Vajna <vmiklos@suse.cz>
2013-01-10 16:21:55 +0100

    fdo#57938 SwPaM::HasReadonlySel: commented text ranges are not read-only
    (cherry picked from commit fd0da52d371ca13d66be3b04d89f9d9cf73baddd)
    
    Change-Id: I8191ed6383c4c636be67b8d42654dbba0a5c8b6f

--- sw/source/core/crsr/pam.cxx
+++ sw/source/core/crsr/pam.cxx
@@ -685,6 +685,7 @@ sal_Bool SwPaM::HasReadonlySel( bool bFormView ) const
     sw::mark::IMark* pA = NULL;
     sw::mark::IMark* pB = NULL;
     bool bUnhandledMark = false;
+    bool bCommentrangeMark = false;
     if ( pDoc )
     {
         const IDocumentMarkAccess* pMarksAccess = pDoc->getIDocumentMarkAccess( );
@@ -693,7 +694,11 @@ sal_Bool SwPaM::HasReadonlySel( bool bFormView ) const
 
         sw::mark::IFieldmark* pFieldmark = pMarksAccess->getFieldmarkFor( *GetPoint() );
         if ( pFieldmark )
+        {
             bUnhandledMark = pFieldmark->GetFieldname( ) == ODF_UNHANDLED;
+            if (!bUnhandledMark)
+                bCommentrangeMark = pFieldmark->GetFieldname() == ODF_COMMENTRANGE;
+        }
     }
 
     if (!bRet)
@@ -701,7 +706,8 @@ sal_Bool SwPaM::HasReadonlySel( bool bFormView ) const
         // Unhandled fieldmarks case shouldn't be edited manually to avoid breaking anything
         if ( ( pA == pB ) && bUnhandledMark )
             bRet = sal_True;
-        else
+        // Allow editing of commented ranges.
+        else if (!((pA == pB) && bCommentrangeMark))
         {
             // Form protection case
             bool bAtStartA = pA != NULL && pA->GetMarkStart() == *GetPoint();
