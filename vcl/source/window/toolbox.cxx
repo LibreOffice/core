8713ae48fab49d066520dcf06746543e53783548
Jan Holesovsky <kendy@suse.cz>
2013-01-17 12:02:08 +0100

    Personas: Fix fdo#59230 - make Personas fully work in non-NWF case too.
    Change-Id: I19a475f198fcb24c4d66134eb67da719fc99099e

--- vcl/source/window/toolbox.cxx
+++ vcl/source/window/toolbox.cxx
@@ -622,7 +622,8 @@ void ToolBox::ImplDrawBackground( ToolBox* pThis, const Rectangle &rRect )
 
         if( !bNativeOk )
         {
-            if( !pThis->IsBackground() )
+            if( !pThis->IsBackground() ||
+                ( pThis->GetAlign() == WINDOWALIGN_TOP && !Application::GetSettings().GetStyleSettings().GetPersonaHeader().IsEmpty() ) )
             {
                 if( !pThis->IsInPaint() )
                     ImplDrawTransparentBackground( pThis, aPaintRegion );
@@ -1662,7 +1663,8 @@ void ToolBox::ImplInitSettings( sal_Bool bFont,
         }
         else
         {
-            if( IsNativeControlSupported( CTRL_TOOLBAR, PART_ENTIRE_CONTROL ) )
+            if( IsNativeControlSupported( CTRL_TOOLBAR, PART_ENTIRE_CONTROL ) ||
+                ( GetAlign() == WINDOWALIGN_TOP && !Application::GetSettings().GetStyleSettings().GetPersonaHeader().IsEmpty() ) )
             {
                 SetBackground();
                 SetPaintTransparent( sal_True );
