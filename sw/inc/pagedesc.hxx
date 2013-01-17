LibreOffice/core

commit fa0f42bafbf24e9141ddee728b160b5ab47077f2

SwPageDesc: add GetFirstFmt methods
Change-Id: Id0e1185950446d0c9b7159a406315540956baed1

sw/inc/pagedesc.hxx
@@ -127,8 +127,9 @@ namespace nsUseOnPage
     const UseOnPage PD_NONE           = 0x0000; // For internal use only.
     const UseOnPage PD_LEFT           = 0x0001;
     const UseOnPage PD_RIGHT          = 0x0002;
-    const UseOnPage PD_ALL            = 0x0003;
-    const UseOnPage PD_MIRROR         = 0x0007;
+    const UseOnPage PD_FIRST          = 0x0004;
+    const UseOnPage PD_ALL            = 0x0007;
+    const UseOnPage PD_MIRROR         = 0x000F;
     const UseOnPage PD_HEADERSHARE    = 0x0040;
     const UseOnPage PD_FOOTERSHARE    = 0x0080;
     const UseOnPage PD_NOHEADERSHARE  = 0x00BF; // For internal use only.
@@ -217,6 +218,8 @@ public:
     inline const SwFrmFmt *GetRightFmt() const;
     inline SwFrmFmt *GetLeftFmt();
     inline const SwFrmFmt *GetLeftFmt() const;
+    inline SwFrmFmt *GetFirstFmt();
+    inline const SwFrmFmt *GetFirstFmt() const;
 
     sal_uInt16 GetRegHeight() const { return nRegHeight; }
     sal_uInt16 GetRegAscent() const { return nRegAscent; }
@@ -325,6 +328,14 @@ inline SwFrmFmt *SwPageDesc::GetLeftFmt()
 inline const SwFrmFmt *SwPageDesc::GetLeftFmt() const
 {
     return nsUseOnPage::PD_LEFT & eUse ? &aLeft : 0;
+}
+inline SwFrmFmt *SwPageDesc::GetFirstFmt()
+{
+    return nsUseOnPage::PD_FIRST & eUse ? &aFirst : 0;
+}
+inline const SwFrmFmt *SwPageDesc::GetFirstFmt() const
+{
+    return nsUseOnPage::PD_FIRST & eUse ? &aFirst : 0;
 }
 
 class SwPageDescExt

commit 8d301533f0c7764c94f8ad72fced2ce9d9de7585

fdo#59428: SwPageDesc::GetUseOn(): mask PD_FIRSTSHARE.
There is at least one call in SwDoc::ChgPageDesc() checks if the result
equals PD_MIRROR.

(regression from fa0f42bafbf24e9141ddee728b160b5ab47077f2)

Change-Id: Id37ca46eec5885f7f5230337d8a8a594845f7842

sw/inc/pagedesc.hxx
@@ -292,6 +292,7 @@ inline UseOnPage SwPageDesc::GetUseOn() const
     UseOnPage eRet = eUse;
     eRet = (UseOnPage) (eRet & nsUseOnPage::PD_NOHEADERSHARE);
     eRet = (UseOnPage) (eRet & nsUseOnPage::PD_NOFOOTERSHARE);
+    eRet = (UseOnPage) (eRet & nsUseOnPage::PD_NOFIRSTSHARE);
     return eRet;
 }
 