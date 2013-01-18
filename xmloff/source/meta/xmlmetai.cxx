LibreOffice/core

commit 17ff7b41d15ab9928e2e2706faa26234a09802cd

re-base on ALv2 code. Includes:
    Patches contributed by: Armin Le Grand.
    #118558# Correcting OLE attributes of LO3.4 at load time by loading as OOo3.3, details see task.
    http://svn.apache.org/viewvc?view=revision&revision=1195906
    #118485# - Styles for OLEs are not saved.
    http://svn.apache.org/viewvc?view=revision&revision=1182166
    #118898# Adapted ImpGraphic::ImplGetBitmap to correctly convert metafiles
    http://svn.apache.org/viewvc?view=revision&revision=1293316
    #119337# Solves the wrong get/setPropertyValue calls in
    SvxShapeText (and thus in SvxOle2Shape)
    http://svn.apache.org/viewvc?view=revision&revision=1344156

    Patches contributed by Mathias Bauer (and others)
    gnumake4 work variously
    http://svn.apache.org/viewvc?view=revision&revision=1394707
    http://svn.apache.org/viewvc?view=revision&revision=1394326
    cws mba34issues01: #i117717#: remove wrong assertion
    http://svn.apache.org/viewvc?view=revision&revision=1172349

    Patch contributed by Herbert Duerr
    goodbye Registration and License dialogs, don't let the door hit you
    http://svn.apache.org/viewvc?view=revision&revision=1172613
    help gcc 4.6.0 on 32bit ubuntu 11.10"
    http://svn.apache.org/viewvc?view=revision&revision=1245357

    Do not add targets for junit tests when junit is disabled.
    Patch contributed by Andre Fischer
    http://svn.apache.org/viewvc?view=revision&revision=1241508

Revert "sb140: #i117082# avoid unncessary static class data members
    commit 21d97438e2944861e26e4984195f959a0cce1e41.
remove obsolete FreeBSD visibility special case.
retain consolidated BSD bridge code, remove OS/2 pieces.

xmloff/source/meta/xmlmetai.cxx
@@ -1,30 +1,21 @@
 /* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
-/*************************************************************************
+/*
+ * This file is part of the LibreOffice project.
  *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * Copyright 2000, 2010 Oracle and/or its affiliates.
+ * This file incorporates work covered by the following license notice:
  *
- * OpenOffice.org - a multi-platform office productivity suite
- *
- * This file is part of OpenOffice.org.
- *
- * OpenOffice.org is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License version 3
- * only, as published by the Free Software Foundation.
- *
- * OpenOffice.org is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License version 3 for more details
- * (a copy is included in the LICENSE file that accompanied this code).
- *
- * You should have received a copy of the GNU Lesser General Public License
- * version 3 along with OpenOffice.org.  If not, see
- * <http://www.openoffice.org/license.html>
- * for a copy of the LGPLv3 License.
- *
- ************************************************************************/
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
 
 
 #include <com/sun/star/lang/WrappedTargetRuntimeException.hpp>
@@ -277,20 +268,27 @@ void SvXMLMetaDocumentContext::setBuildId(::rtl::OUString const& i_rBuildId, con
 
     if ( sBuildId.isEmpty() )
     {
-        if ((i_rBuildId.compareToAscii(
-                RTL_CONSTASCII_STRINGPARAM("StarOffice 7") ) == 0) ||
-            (i_rBuildId.compareToAscii(
-                RTL_CONSTASCII_STRINGPARAM("StarSuite 7") ) == 0)  ||
-            (i_rBuildId.compareToAscii(
-                RTL_CONSTASCII_STRINGPARAM("OpenOffice.org 1") ) == 0))
+        if ( i_rBuildId == "StarOffice 7" || i_rBuildId == "StarSuite 7" ||
+             i_rBuildId == "OpenOffice.org 1" )
         {
             sBuildId = OUString("645$8687");
         }
-        if ((i_rBuildId.compareToAscii( RTL_CONSTASCII_STRINGPARAM("NeoOffice/2") ) == 0) )
+        else if ( i_rBuildId == "NeoOffice/2" )
         {
             sBuildId = OUString("680$9134"); // fake NeoOffice as OpenOffice.org 2.2 release
         }
     }
+// Is this really what we want / correct ?
+#ifdef FIXME_REMOVE_WHEN_RE_BASE_COMPLETE
+    else
+    {
+        if ( i_rBuildId == "LibreOffice/3" )
+        {
+            // #118558# fake LibreOffice3 as OpenOffice.org 3.3 release
+            sBuildId = OUString::createFromAscii( "330$9567" );
+        }
+    }
+#endif
 
     if ( !sBuildId.isEmpty() ) try
     {

commit 0669d78ea76ab9bfeff27ea02d785973f3720d6b

fdo#59517: fix detection of ancient OOo ODF generator strings:
SvXMLMetaDocumentContext::setBuildId: check only the prefix of the
generator string, not all of it.

(regression from 17ff7b41d15ab9928e2e2706faa26234a09802cd)

Change-Id: I0cdd958d67cd13fd2368cc6958893ce3528a9e94

xmloff/source/meta/xmlmetai.cxx
@@ -268,12 +268,13 @@ void SvXMLMetaDocumentContext::setBuildId(::rtl::OUString const& i_rBuildId, con
 
     if ( sBuildId.isEmpty() )
     {
-        if ( i_rBuildId == "StarOffice 7" || i_rBuildId == "StarSuite 7" ||
-             i_rBuildId == "OpenOffice.org 1" )
+        if (    i_rBuildId.startsWith("StarOffice 7")
+            ||  i_rBuildId.startsWith("StarSuite 7")
+            ||  i_rBuildId.startsWith("OpenOffice.org 1"))
         {
             sBuildId = OUString("645$8687");
         }
-        else if ( i_rBuildId == "NeoOffice/2" )
+        else if (i_rBuildId.startsWith("NeoOffice/2"))
         {
             sBuildId = OUString("680$9134"); // fake NeoOffice as OpenOffice.org 2.2 release
         }
@@ -282,7 +283,7 @@ void SvXMLMetaDocumentContext::setBuildId(::rtl::OUString const& i_rBuildId, con
 #ifdef FIXME_REMOVE_WHEN_RE_BASE_COMPLETE
     else
     {
-        if ( i_rBuildId == "LibreOffice/3" )
+        if (i_rBuildId.startsWith("LibreOffice/3"))
         {
             // #118558# fake LibreOffice3 as OpenOffice.org 3.3 release
             sBuildId = OUString::createFromAscii( "330$9567" );