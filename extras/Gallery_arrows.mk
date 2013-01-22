cfddb61cd35faa5371510939a34ef1abd8ffc51d
Armin Le Grand <alg@apache.org>
2013-01-22 12:48:54 +0000

    Resolves: #i121656# Replaced many gallery base graphics with SVG
    (cherry picked from commit bea9a9501814bdbc342a2adfa57019704260a532)
    
    Conflicts:
    	extras/source/gallery/gallery_system/sg10.sdg
    	extras/source/gallery/gallery_system/sg10.thm
    	extras/source/gallery/gallery_system/sg12.sdg
    	extras/source/gallery/gallery_system/sg12.sdv
    	extras/source/gallery/gallery_system/sg12.thm
    	extras/source/gallery/gallery_system/sg16.sdv
    	extras/source/gallery/gallery_system/sg17.sdg
    	extras/source/gallery/gallery_system/sg17.thm
    	extras/source/gallery/gallery_system/sg19.sdg
    	extras/source/gallery/gallery_system/sg19.sdv
    	extras/source/gallery/gallery_system/sg19.thm
    	extras/source/gallery/gallery_system/sg2.sdv
    
    Change-Id: I4c2e34c470b05ddae5923e782c31d15aa4961e0d

--- extras/Gallery_arrows.mk
+++ extras/Gallery_arrows.mk
@@ -10,74 +10,74 @@
 $(eval $(call gb_Gallery_Gallery,arrows,extras/source/gallery/arrows))
 
 $(eval $(call gb_Gallery_add_files,arrows,share/gallery/arrows,\
-	extras/source/gallery/arrows/A01-Arrow-Gray-Left.png \
-	extras/source/gallery/arrows/A02-Arrow-DarkBlue-Right.png \
-	extras/source/gallery/arrows/A03-Arrow-Gray-Left.png \
-	extras/source/gallery/arrows/A04-Arrow-DarkRed-Right.png \
-	extras/source/gallery/arrows/A05-Arrow-Blue-Left.png \
-	extras/source/gallery/arrows/A06-Arrow-Red-Right.png \
-	extras/source/gallery/arrows/A07-Arrow-LightBlue-Left.png \
-	extras/source/gallery/arrows/A08-Arrow-DarkRed-Right.png \
-	extras/source/gallery/arrows/A09-Arrow-Gray-Left.png \
-	extras/source/gallery/arrows/A10-Arrow-Purple-Right.png \
-	extras/source/gallery/arrows/A11-Arrow-Gray-Left.png \
-	extras/source/gallery/arrows/A12-Arrow-LightBlue-Right.png \
-	extras/source/gallery/arrows/A13-Arrow-Gray-Right.png \
-	extras/source/gallery/arrows/A14-CircleArrow-Green.png \
-	extras/source/gallery/arrows/A15-CircleArrow.png \
+	extras/source/gallery/arrows/A01-Arrow-Gray-Left.svg \
+	extras/source/gallery/arrows/A02-Arrow-DarkBlue-Right.svg \
+	extras/source/gallery/arrows/A03-Arrow-Gray-Left.svg \
+	extras/source/gallery/arrows/A04-Arrow-DarkRed-Right.svg \
+	extras/source/gallery/arrows/A05-Arrow-Blue-Left.svg \
+	extras/source/gallery/arrows/A06-Arrow-Red-Right.svg \
+	extras/source/gallery/arrows/A07-Arrow-LightBlue-Left.svg \
+	extras/source/gallery/arrows/A08-Arrow-DarkRed-Right.svg \
+	extras/source/gallery/arrows/A09-Arrow-Gray-Left.svg \
+	extras/source/gallery/arrows/A10-Arrow-Purple-Right.svg \
+	extras/source/gallery/arrows/A11-Arrow-Gray-Left.svg \
+	extras/source/gallery/arrows/A12-Arrow-LightBlue-Right.svg \
+	extras/source/gallery/arrows/A13-Arrow-Gray-Right.svg \
+	extras/source/gallery/arrows/A14-CircleArrow-Green.svg \
+	extras/source/gallery/arrows/A15-CircleArrow.svg \
 	extras/source/gallery/arrows/A16-CircleArrow.png \
 	extras/source/gallery/arrows/A17-CircleArrow.png \
 	extras/source/gallery/arrows/A18-CircleArrow.png \
 	extras/source/gallery/arrows/A19-CircleArrow.png \
-	extras/source/gallery/arrows/A20-CircleArrow-LightBlue.png \
-	extras/source/gallery/arrows/A21-CircleArrow-Green.png \
-	extras/source/gallery/arrows/A22-CircleArrow.png \
-	extras/source/gallery/arrows/A23-CurvedArrow-Gray-Left.png \
-	extras/source/gallery/arrows/A24-CurvedArrow-LightBlue-Right.png \
-	extras/source/gallery/arrows/A25-CurvedArrow-DarkBlue.png \
+	extras/source/gallery/arrows/A20-CircleArrow-LightBlue.svg \
+	extras/source/gallery/arrows/A21-CircleArrow-Green.svg \
+	extras/source/gallery/arrows/A22-CircleArrow.svg \
+	extras/source/gallery/arrows/A23-CurvedArrow-Gray-Left.svg \
+	extras/source/gallery/arrows/A24-CurvedArrow-LightBlue-Right.svg \
+	extras/source/gallery/arrows/A25-CurvedArrow-DarkBlue.svg \
 	extras/source/gallery/arrows/A26-CurvedArrow-Orange.png \
 	extras/source/gallery/arrows/A27-CurvedArrow-DarkRed.png \
 	extras/source/gallery/arrows/A28-CurvedArrow-DarkBlue.png \
 	extras/source/gallery/arrows/A29-CurvedArrow-Green.png \
-	extras/source/gallery/arrows/A30-CurvedArrow-Gray.png \
-	extras/source/gallery/arrows/A31-CurvedArrow-LightBlue.png \
-	extras/source/gallery/arrows/A32-CurvedArrow-Orange.png \
-	extras/source/gallery/arrows/A33-CurvedArrow-LightBlue-TwoDirections.png \
-	extras/source/gallery/arrows/A34-CurvedArrow-Green-TwoDirections.png \
-	extras/source/gallery/arrows/A35-CurvedArrow-Brown-Left.png \
-	extras/source/gallery/arrows/A36-CurvedArrow-LightBlue-Up.png \
-	extras/source/gallery/arrows/A37-CurvedArrow-Gray-TwoDirections.png \
-	extras/source/gallery/arrows/A38-CurvedArrow-Gray-TwoDirections.png \
-	extras/source/gallery/arrows/A39-CurvedArrow-Gray-Left.png \
-	extras/source/gallery/arrows/A40-CurvedArrow-Gray-Up.png \
-	extras/source/gallery/arrows/A41-CurvedArrow-Gray-Left.png \
-	extras/source/gallery/arrows/A42-TrendArrow-Red-GoUp.png \
-	extras/source/gallery/arrows/A43-TrendArrow-Green-GoDown.png \
+	extras/source/gallery/arrows/A30-CurvedArrow-Gray.svg \
+	extras/source/gallery/arrows/A31-CurvedArrow-LightBlue.svg \
+	extras/source/gallery/arrows/A32-CurvedArrow-Orange.svg \
+	extras/source/gallery/arrows/A33-CurvedArrow-LightBlue-TwoDirections.svg \
+	extras/source/gallery/arrows/A34-CurvedArrow-Green-TwoDirections.svg \
+	extras/source/gallery/arrows/A35-CurvedArrow-Brown-Left.svg \
+	extras/source/gallery/arrows/A36-CurvedArrow-LightBlue-Up.svg \
+	extras/source/gallery/arrows/A37-CurvedArrow-Gray-TwoDirections.svg \
+	extras/source/gallery/arrows/A38-CurvedArrow-Gray-TwoDirections.svg \
+	extras/source/gallery/arrows/A39-CurvedArrow-Gray-Left.svg \
+	extras/source/gallery/arrows/A40-CurvedArrow-Gray-Up.svg \
+	extras/source/gallery/arrows/A41-CurvedArrow-Gray-Left.svg \
+	extras/source/gallery/arrows/A42-TrendArrow-Red-GoUp.svg \
+	extras/source/gallery/arrows/A43-TrendArrow-Green-GoDown.svg \
 	extras/source/gallery/arrows/A44-TrendArrow-Orange-GoUp.png \
 	extras/source/gallery/arrows/A45-TrendArrow-Red-GoUp.png \
-	extras/source/gallery/arrows/A46-TrendArrow-Orange-GoUp.png \
+	extras/source/gallery/arrows/A46-TrendArrow-Orange-GoUp.svg \
 	extras/source/gallery/arrows/A47-TrendArrow-LightBlue.png \
-	extras/source/gallery/arrows/A48-TrendArrow-Orange-TwoDirections.png \
-	extras/source/gallery/arrows/A49-TrendArrow-Yellow-ThreeDirections.png \
-	extras/source/gallery/arrows/A50-TrendArrow-LightBlue-FourDirections.png \
-	extras/source/gallery/arrows/A51-TrendArrow-Blue-FourDirections.png \
-	extras/source/gallery/arrows/A52-TrendArrow-Blue-FourDirections.png \
-	extras/source/gallery/arrows/A53-TrendArrow-LightBlue-TwoDirections.png \
-	extras/source/gallery/arrows/A54-TrendArrow-Red-TwoDirections.png \
-	extras/source/gallery/arrows/A55-TrendArrow-TwoDirections.png \
-	extras/source/gallery/arrows/A56-TrendArrow-Blue-TwoDirections.png \
-	extras/source/gallery/arrows/A57-Arrow-Yellow-Left.png \
-	extras/source/gallery/arrows/A58-Arrow-Red-Right.png \
-	extras/source/gallery/arrows/A59-CurvedArrow-Gray-Left.png \
-	extras/source/gallery/arrows/A60-CurvedArrow-Purple-Right.png \
-	extras/source/gallery/arrows/A61-Arrow-StripedOrange-Left.png \
-	extras/source/gallery/arrows/A62-Arrow-StripedBlue-Right.png \
-	extras/source/gallery/arrows/A63-Arrow-LightBlue-Left.png \
-	extras/source/gallery/arrows/A64-Arrow-Green-Right.png \
-	extras/source/gallery/arrows/A65-Arrow-DarkBlue-Up.png \
-	extras/source/gallery/arrows/A66-Arrow-Green-Down.png \
-	extras/source/gallery/arrows/A67-Arrow-Yellow-Left.png \
-	extras/source/gallery/arrows/A68-Arrow-Gray-Right.png \
+	extras/source/gallery/arrows/A48-TrendArrow-Orange-TwoDirections.svg \
+	extras/source/gallery/arrows/A49-TrendArrow-Yellow-ThreeDirections.svg \
+	extras/source/gallery/arrows/A50-TrendArrow-LightBlue-FourDirections.svg \
+	extras/source/gallery/arrows/A51-TrendArrow-Blue-FourDirections.svg \
+	extras/source/gallery/arrows/A52-TrendArrow-Blue-FourDirections.svg \
+	extras/source/gallery/arrows/A53-TrendArrow-LightBlue-TwoDirections.svg \
+	extras/source/gallery/arrows/A54-TrendArrow-Red-TwoDirections.svg \
+	extras/source/gallery/arrows/A55-TrendArrow-TwoDirections.svg \
+	extras/source/gallery/arrows/A56-TrendArrow-Blue-TwoDirections.svg \
+	extras/source/gallery/arrows/A57-Arrow-Yellow-Left.svg \
+	extras/source/gallery/arrows/A58-Arrow-Red-Right.svg \
+	extras/source/gallery/arrows/A59-CurvedArrow-Gray-Left.svg \
+	extras/source/gallery/arrows/A60-CurvedArrow-Purple-Right.svg \
+	extras/source/gallery/arrows/A61-Arrow-StripedOrange-Left.svg \
+	extras/source/gallery/arrows/A62-Arrow-StripedBlue-Right.svg \
+	extras/source/gallery/arrows/A63-Arrow-LightBlue-Left.svg \
+	extras/source/gallery/arrows/A64-Arrow-Green-Right.svg \
+	extras/source/gallery/arrows/A65-Arrow-DarkBlue-Up.svg \
+	extras/source/gallery/arrows/A66-Arrow-Green-Down.svg \
+	extras/source/gallery/arrows/A67-Arrow-Yellow-Left.svg \
+	extras/source/gallery/arrows/A68-Arrow-Gray-Right.svg \
 ))
 
 # vim: set noet sw=4 ts=4:
