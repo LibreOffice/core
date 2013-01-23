LibreOffice/core

commit 24dd42f316e5cb16ba726c05ba486d53e033052d

sw-rtf-num-fix.diff:Fixes for RTF numbering export
n#569266

filter/source/msfilter/escherex.cxx
@@ -720,6 +720,29 @@ void EscherPropertyContainer::CreateTextProperties(
 
     if ( nTextId )
         AddOpt( ESCHER_Prop_lTxid, nTextId );
+
+    // n#404221: In case of rotation we need to write the txtflTextFlow
+    // attribute too.
+    if (bIsTextFrame) {
+        sal_uInt16 nAngle = EscherPropertyValueHelper::GetPropertyValue(
+        aAny,
+        rXPropSet,
+        String( RTL_CONSTASCII_USTRINGPARAM( "RotateAngle" ) ),
+        sal_True )
+        ? (sal_uInt16)( ( *((sal_Int32*)aAny.getValue() ) ) + 5 ) / 10 : 0;
+        if (nAngle==900) {
+        AddOpt( ESCHER_Prop_txflTextFlow, 1 );
+        bSuppressRotation=true;
+        }
+        if (nAngle==1800) {
+        AddOpt( ESCHER_Prop_txflTextFlow, 2 );
+        bSuppressRotation=true;
+        }
+        if (nAngle==2700) {
+        AddOpt( ESCHER_Prop_txflTextFlow, 3 );
+        bSuppressRotation=true;
+        }
+    }
 }
 
 sal_Bool EscherPropertyContainer::GetLineArrow( const sal_Bool bLineStart,

commit 26158a535d406f2f76052d576c2ba227a3ff1a52

fdo#58204: msfilter: do not add ESCHER_Prop_txflTextFlow
This breaks PPT export quite badly.
(regression from 24dd42f316e5cb16ba726c05ba486d53e033052d)

Change-Id: Id3dcfb6221d031cdabb7f543b68752b467d9e1a6

filter/source/msfilter/escherex.cxx
@@ -676,23 +676,6 @@ void EscherPropertyContainer::CreateTextProperties(
 
     if ( nTextId )
         AddOpt( ESCHER_Prop_lTxid, nTextId );
-
-    // n#404221: In case of rotation we need to write the txtflTextFlow
-    // attribute too.
-    if (bIsTextFrame)
-    {
-        sal_uInt16 nAngle = EscherPropertyValueHelper::GetPropertyValue(
-            aAny, rXPropSet, OUString( "RotateAngle" ), sal_True ) ?
-                (sal_uInt16)( ( *((sal_Int32*)aAny.getValue() ) ) + 5 ) / 10 : 0;
-        if (nAngle==900)
-        {
-            AddOpt( ESCHER_Prop_txflTextFlow, ESCHER_txflBtoT );
-        }
-        if (nAngle==2700)
-        {
-            AddOpt( ESCHER_Prop_txflTextFlow, ESCHER_txflTtoBA );
-        }
-    }
 }
 
 sal_Bool EscherPropertyContainer::GetLineArrow( const sal_Bool bLineStart,