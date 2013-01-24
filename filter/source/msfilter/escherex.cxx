LibreOffice/core

commit 24dd42f316e5cb16ba726c05ba486d53e033052d

sw-rtf-num-fix.diff:Fixes for RTF numbering export
n#569266

filter/source/msfilter/escherex.cxx
@@ -720,6 +720,29 @@ void EscherPropertyContainer::CreateTextProperties(
 
     if ( nTextId )
         AddOpt( ESCHER_Prop_lTxid, nTextId );
+
+    // n#404221: In case of rotation we need to write the txtflTextFlow
+    // attribute too.
+    if (bIsTextFrame) {
+        sal_uInt16 nAngle = EscherPropertyValueHelper::GetPropertyValue(
+        aAny,
+        rXPropSet,
+        String( RTL_CONSTASCII_USTRINGPARAM( "RotateAngle" ) ),
+        sal_True )
+        ? (sal_uInt16)( ( *((sal_Int32*)aAny.getValue() ) ) + 5 ) / 10 : 0;
+        if (nAngle==900) {
+        AddOpt( ESCHER_Prop_txflTextFlow, 1 );
+        bSuppressRotation=true;
+        }
+        if (nAngle==1800) {
+        AddOpt( ESCHER_Prop_txflTextFlow, 2 );
+        bSuppressRotation=true;
+        }
+        if (nAngle==2700) {
+        AddOpt( ESCHER_Prop_txflTextFlow, 3 );
+        bSuppressRotation=true;
+        }
+    }
 }
 
 sal_Bool EscherPropertyContainer::GetLineArrow( const sal_Bool bLineStart,

commit 2823789bec0c029d9714aff0ed65923e23177ef9

fdo#58204: PPT export: fix text rotation in shapes:
EscherPropertyContainer::CreateTextProperties: do not write
ESCHER_Prop_txflTextFlow for custom shapes.  This happens to export the
rotation properly for the WW8 bugdoc of n#404221, as well as the PPT at
fdo#58204 and both PPTs at fdo#34897, one of which (mangle.ppt) was not
handled properly on master currently.  The author disclaims having any
actual knowledge about rotation properties in MSO files.

(regression from 24dd42f316e5cb16ba726c05ba486d53e033052d)

Change-Id: Id3dcfb6221d031cdabb7f543b68752b467d9e1a6

filter/source/msfilter/escherex.cxx
@@ -679,7 +679,8 @@ void EscherPropertyContainer::CreateTextProperties(
 
     // n#404221: In case of rotation we need to write the txtflTextFlow
     // attribute too.
-    if (bIsTextFrame)
+    // fdo#58204: not custom shapes (TODO: other cases when it doesn't work?)
+    if (bIsTextFrame && !bIsCustomShape)
     {
         sal_uInt16 nAngle = EscherPropertyValueHelper::GetPropertyValue(
             aAny, rXPropSet, OUString( "RotateAngle" ), sal_True ) ?