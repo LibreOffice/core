a541135d0d843b62a54d0a637d3976a0df437d01
Tor Lillqvist <tml@iki.fi>
2013-01-25 14:59:02 +0200

    Avoid stray && in the system ICU case
    Change-Id: I65a803824fd79cd6ac5136861bba0b64998d0868

--- libcdr/ExternalProject_libcdr.mk
+++ libcdr/ExternalProject_libcdr.mk
@@ -61,7 +61,7 @@ else
 $(call gb_ExternalProject_get_state_target,libcdr,build) :
 	cd $(EXTERNAL_WORKDIR) \
 	&& export PKG_CONFIG="" \
-	&& $(if $(filter NO,$(SYSTEM_ICU)), export ICU_LIBS=" " ICU_CFLAGS="-I$(OUTDIR)/inc/external") \
+	$(if $(filter NO,$(SYSTEM_ICU)),&& export ICU_LIBS=" " ICU_CFLAGS="-I$(OUTDIR)/inc/external") \
 	&& ./configure \
 		--with-pic \
 		--enable-static \
