3afecfda4c84a65bd33e9a011a7e96b186d7849f
Miklos Vajna <vmiklos@suse.cz>
2013-02-01 11:20:52 +0100

    fdo#48440 fix import of RTF_PAGE between text frames
    Normally such a page break is surrounded by text, and text() already
    calls checkNeedPap(), but in case the whole paragraph contains a page
    break only, we need it here as well.
    
    (cherry picked from commit b70f74cb5f4b499f14831fd6ad49d9238735e6ef)
    
    Conflicts:
    	sw/qa/extras/rtfimport/rtfimport.cxx
    
    Change-Id: I3961272d77c743e0e6da2d5052e57cec5f5937e7

--- writerfilter/source/rtftok/rtfdocumentimpl.cxx
+++ writerfilter/source/rtftok/rtfdocumentimpl.cxx
@@ -1819,6 +1819,7 @@ int RTFDocumentImpl::dispatchSymbol(RTFKeyword nKeyword)
                 }
                 else
                 {
+                    checkNeedPap();
                     sal_uInt8 sBreak[] = { 0xc };
                     Mapper().text(sBreak, 1);
                     if (!m_bNeedPap)
