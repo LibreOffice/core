LibreOffice/core

commit a964cf666abb0c4df4e29ea8709532b7d45c104f

sw: proper fallback for first page in InsertNewPage()
For left/right share, the fallback is easy: the left page always falls
back to the right page, if there is a fallback. In case of first page,
the situation is more complicated, as there are multiple fallbacks
(left/right).

Since commit 9ff68a2, first page fallback was decided during registering
part of the SwPageDesc to SwPageFrm, which is bad, as without a document
export/import, a changed IsFirstShared() didn't have any effect: the
LeftFmt/RightFmt was in use, so FirstFmt had no SwClients -> nobody was
notified when that setting changed. This commit changes first pages to
always use FirstFmt, and (if a fallback is needed) sets FirstFmt to
point to LeftFmt/RightFmt as necessary.

Change-Id: I4654baf293d72070fcf7e9eea408a43a31079e60

sw/source/core/layout/frmtool.cxx
@@ -2742,10 +2742,28 @@ SwPageFrm * InsertNewPage( SwPageDesc &rDesc, SwFrm *pUpper,
     SwPageFrm *pRet;
     SwDoc *pDoc = ((SwLayoutFrm*)pUpper)->GetFmt()->GetDoc();
     SwFrmFmt *pFmt = 0;
-    // rDesc can't know if the first page will be 'left' or 'right', so if
-    // first is shared, let's ignore first here.
-    if (bFirst && !rDesc.IsFirstShared())
+    if (bFirst)
+    {
+        if (rDesc.IsFirstShared())
+        {
+            // We need to fallback to left or right page format, decide it now.
+            if (bOdd)
+            {
+                rDesc.GetFirst().SetFmtAttr( rDesc.GetMaster().GetHeader() );
+                rDesc.GetFirst().SetFmtAttr( rDesc.GetMaster().GetFooter() );
+            }
+            else
+            {
+                rDesc.GetFirst().SetFmtAttr( rDesc.GetLeft().GetHeader() );
+                rDesc.GetFirst().SetFmtAttr( rDesc.GetLeft().GetFooter() );
+            }
+        }
         pFmt = rDesc.GetFirstFmt();
+        if (!pFmt)
+        {
+            pFmt = bOdd ? rDesc.GetRightFmt() : rDesc.GetLeftFmt();
+        }
+    }
     else
         pFmt = bOdd ? rDesc.GetRightFmt() : rDesc.GetLeftFmt();
     //Wenn ich kein FrmFmt fuer die Seite gefunden habe, muss ich eben

commit 2fc87188bd4a73d25b64b32e0287cee5d6ee4a48

fdo#60250: InsertNewPage: copy margins for mirrored pages
When there is no explicit first page, set the proper left or right
margins, which are different for mirrored page styles.
(regression from a964cf666abb0c4df4e29ea8709532b7d45c104f)

Change-Id: I56ccc6f7987accefc27d627d2427ee37d83bd758

sw/source/core/layout/frmtool.cxx
@@ -2729,11 +2729,14 @@ SwPageFrm * InsertNewPage( SwPageDesc &rDesc, SwFrm *pUpper,
             {
                 rDesc.GetFirst().SetFmtAttr( rDesc.GetMaster().GetHeader() );
                 rDesc.GetFirst().SetFmtAttr( rDesc.GetMaster().GetFooter() );
+                // fdo#60250 copy margins for mirrored pages
+                rDesc.GetFirst().SetFmtAttr( rDesc.GetMaster().GetLRSpace() );
             }
             else
             {
                 rDesc.GetFirst().SetFmtAttr( rDesc.GetLeft().GetHeader() );
                 rDesc.GetFirst().SetFmtAttr( rDesc.GetLeft().GetFooter() );
+                rDesc.GetFirst().SetFmtAttr( rDesc.GetLeft().GetLRSpace() );
             }
         }
         pFmt = rDesc.GetFirstFmt();