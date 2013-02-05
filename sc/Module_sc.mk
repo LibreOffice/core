e77c476610ee3f7ca4bbb9728b495e41372ac33a
Muthu Subramanian <sumuthu@suse.com>
2013-02-05 18:21:31 +0530

    Disabling sc_chart_regression_test (like on Master)
    Change-Id: I18d8fb4b993771d3365bf3dbfbb781ef17ed20ab

--- sc/Module_sc.mk
+++ sc/Module_sc.mk
@@ -50,10 +50,12 @@ $(eval $(call gb_Module_add_slowcheck_targets,sc,\
     CppunitTest_sc_subsequent_filters_test \
 ))
 
+# Disabled to allow the check tinderbox execute the sd tests
+# CppunitTest_sc_chart_regression_test \
+
 $(eval $(call gb_Module_add_subsequentcheck_targets,sc,\
     JunitTest_sc_complex \
     JunitTest_sc_unoapi \
-    CppunitTest_sc_chart_regression_test \
     CppunitTest_sc_annotationshapeobj \
     CppunitTest_sc_cellrangeobj \
     $(if $(filter-out $(OS),IOS), \
