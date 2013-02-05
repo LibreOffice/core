LibreOffice/core

commit 528aba3a9cf91da5ce70c6d631d7b82e203f8086

SV_DECL_PTRARR_DEL->boost::ptr_vector

sfx2/source/control/dispatch.cxx
@@ -72,21 +72,17 @@
 #include <rtl/strbuf.hxx>
 
 #include <deque>
+#include <boost/ptr_container/ptr_vector.hpp>
 
 namespace css = ::com::sun::star;
 
-//==================================================================
 DBG_NAME(SfxDispatcherFlush)
 DBG_NAME(SfxDispatcherFillState)
 
-//==================================================================
-typedef SfxRequest* SfxRequestPtr;
-SV_IMPL_PTRARR( SfxItemPtrArray, SfxPoolItemPtr );
-SV_DECL_PTRARR_DEL( SfxRequestPtrArray, SfxRequestPtr, 4, 4 )
-SV_IMPL_PTRARR( SfxRequestPtrArray, SfxRequestPtr );
+typedef boost::ptr_vector<SfxRequest> SfxRequestPtrArray;
 
+SV_IMPL_PTRARR( SfxItemPtrArray, SfxPoolItemPtr );
 DECL_PTRSTACK(SfxShellStack_Impl, SfxShell*, 8, 4 );
-//==================================================================
 
 struct SfxToDo_Impl
 {
@@ -1264,7 +1260,7 @@ IMPL_LINK( SfxDispatcher, PostMsgHandler, SfxRequest*, pReq )
         else
         {
             if ( pImp->bLocked )
-                pImp->aReqArr.Insert( new SfxRequest(*pReq), pImp->aReqArr.Count() );
+                pImp->aReqArr.push_back(new SfxRequest(*pReq));
             else
                 pImp->xPoster->Post(new SfxRequest(*pReq));
         }
@@ -2176,10 +2172,9 @@ void SfxDispatcher::Lock( sal_Bool bLock )
     pImp->bLocked = bLock;
     if ( !bLock )
     {
-        sal_uInt16 nCount = pImp->aReqArr.Count();
-        for ( sal_uInt16 i=0; i<nCount; i++ )
-            pImp->xPoster->Post( pImp->aReqArr[i] );
-        pImp->aReqArr.Remove( 0, nCount );
+        for(size_t i = 0; i < pImp->aReqArr.size(); ++i)
+            pImp->xPoster->Post(&pImp->aReqArr[i]);
+        pImp->aReqArr.clear();
     }
 }
 

commit 1385896fe760fe61b19a682cb7dc7fdf31d31490

Resolves: fdo#60144 only delete *unposted* SfxRequests accumulated during lock
When the dispatched is locked, SfxRequests accumulate in aReqArr for later
dispatch when unlocked via Post

The pointers are typically deleted in Post, so only if we never get around to
posting them do we delete the unposted requests.

regression from 528aba3a9cf91da5ce70c6d631d7b82e203f8086

Change-Id: I4c214791d356ce0e5401a87b968b53e4866f6174

sfx2/source/control/dispatch.cxx
@@ -60,12 +60,12 @@
 #include <rtl/strbuf.hxx>
 
 #include <deque>
-#include <boost/ptr_container/ptr_vector.hpp>
+#include <vector>
 
 DBG_NAME(SfxDispatcherFlush)
 DBG_NAME(SfxDispatcherFillState)
 
-typedef boost::ptr_vector<SfxRequest> SfxRequestPtrArray;
+typedef std::vector<SfxRequest*> SfxRequestPtrArray;
 
 DECL_PTRSTACK(SfxShellStack_Impl, SfxShell*, 8, 4 );
 
@@ -110,7 +110,17 @@ struct SfxObjectBars_Impl
 
 struct SfxDispatcher_Impl
 {
-    SfxRequestPtrArray   aReqArr;
+    //When the dispatched is locked, SfxRequests accumulate in aReqArr for
+    //later dispatch when unlocked via Post
+    //
+    //The pointers are typically deleted in Post, only if we never get around
+    //to posting them do we delete the unposted requests.
+    SfxRequestPtrArray aReqArr;
+    ~SfxDispatcher_Impl()
+    {
+        for (SfxRequestPtrArray::iterator aI = aReqArr.begin(), aEnd = aReqArr.end(); aI != aEnd; ++aI)
+            delete *aI;
+    }
     const SfxSlotServer* pCachedServ1;  // last called message
     const SfxSlotServer* pCachedServ2;  // penultimate called Message
     SfxShellStack_Impl   aStack;        // active functionality
@@ -2149,7 +2159,7 @@ void SfxDispatcher::Lock( sal_Bool bLock )
     if ( !bLock )
     {
         for(size_t i = 0; i < pImp->aReqArr.size(); ++i)
-            pImp->xPoster->Post(&pImp->aReqArr[i]);
+            pImp->xPoster->Post(pImp->aReqArr[i]);
         pImp->aReqArr.clear();
     }
 }