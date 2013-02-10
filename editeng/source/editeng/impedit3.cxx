2d120ddf6ed46f18b93b546466f76af9babad737
navin patidar <patidar@kacst.edu.sa>
2013-02-10 09:40:50 +0300

    fix fdo#60533, Set TEXT_LAYOUT_BIDI_STRONG flag.
    Stop ImplLayoutArgs::ImplLayoutArgs() to perform
    ubidi algo on text by setting TEXT_LAYOUT_BIDI_STRONG flag.
    
    Change-Id: I7222517c1db6649eef0c0d9c11cbbbed1fc6bb04

--- editeng/source/editeng/impedit3.cxx
+++ editeng/source/editeng/impedit3.cxx
@@ -4274,18 +4274,13 @@ void ImpEditEngine::ImplInitDigitMode(OutputDevice* pOutDev, LanguageType eCurLa
 
 void ImpEditEngine::ImplInitLayoutMode( OutputDevice* pOutDev, sal_uInt16 nPara, sal_uInt16 nIndex )
 {
-    sal_Bool bCTL = sal_False;
     sal_Bool bR2L = sal_False;
     if ( nIndex == 0xFFFF )
     {
-        bCTL = HasScriptType( nPara, i18n::ScriptType::COMPLEX );
         bR2L = IsRightToLeft( nPara );
     }
     else
     {
-        ContentNode* pNode = GetEditDoc().GetObject( nPara );
-        short nScriptType = GetScriptType( EditPaM( pNode, nIndex+1 ) );
-        bCTL = nScriptType == i18n::ScriptType::COMPLEX;
         // this change was discussed in issue 37190
         bR2L = GetRightToLeft( nPara, nIndex + 1) % 2 ? sal_True : sal_False;
         // it also works for issue 55927
@@ -4294,21 +4289,18 @@ void ImpEditEngine::ImplInitLayoutMode( OutputDevice* pOutDev, sal_uInt16 nPara,
     sal_uLong nLayoutMode = pOutDev->GetLayoutMode();
 
     // We always use the left postion for DrawText()
-    nLayoutMode &= ~(TEXT_LAYOUT_BIDI_RTL);
+    // Let VCL do CTL checking
+    nLayoutMode &= ~(TEXT_LAYOUT_BIDI_RTL | TEXT_LAYOUT_COMPLEX_DISABLED);
 
-    if ( !bCTL && !bR2L)
+    if ( !bR2L )
     {
         // No CTL/Bidi checking neccessary
         nLayoutMode |= ( TEXT_LAYOUT_COMPLEX_DISABLED | TEXT_LAYOUT_BIDI_STRONG );
     }
     else
     {
-        // CTL/Bidi checking neccessary
-        // Don't use BIDI_STRONG, VCL must do some checks.
-        nLayoutMode &= ~( TEXT_LAYOUT_COMPLEX_DISABLED | TEXT_LAYOUT_BIDI_STRONG );
-
-        if ( bR2L )
-            nLayoutMode |= TEXT_LAYOUT_BIDI_RTL|TEXT_LAYOUT_TEXTORIGIN_LEFT;
+        //Use BIDI_STRONG, Stop VCL to perform ubidi algo on text.
+        nLayoutMode |= TEXT_LAYOUT_BIDI_RTL | TEXT_LAYOUT_TEXTORIGIN_LEFT | TEXT_LAYOUT_BIDI_STRONG;
     }
 
     pOutDev->SetLayoutMode( nLayoutMode );
