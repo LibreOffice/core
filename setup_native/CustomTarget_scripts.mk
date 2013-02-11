91a6fc6ee1405ab98a3e65d7d85e1e51cb514cfd
Peter Foley <pefoley2@verizon.net>
2013-02-10 22:02:02 -0500

    fix rpm build in setup_native
    Change-Id: Id7cba31ac24f6a45077f5a41039b7b79ad668b95

--- setup_native/CustomTarget_scripts.mk
+++ setup_native/CustomTarget_scripts.mk
@@ -16,16 +16,16 @@ ifneq ($(filter rpm,$(PKGFORMAT)),)
 $(eval $(call gb_CustomTarget_register_targets,setup_native/scripts,\
 	install \
 	uninstall \
-	fake-db-1.0-0.noarch.rpm \
+	noarch/fake-db-1.0-0.noarch.rpm \
 ))
 
-$(scripts_WORKDIR)/fake-db-1.0-0.noarch.rpm: $(SRCDIR)/setup_native/scripts/fake-db.spec
+$(scripts_WORKDIR)/noarch/fake-db-1.0-0.noarch.rpm: $(SRCDIR)/setup_native/scripts/fake-db.spec
 	mkdir -p $(scripts_WORKDIR)/fake-db-root
 	$(RPM) --define "_builddir $(scripts_WORKDIR)/fake-db-root" \
 		--define "_rpmdir $(scripts_WORKDIR)" -bb $<
 	chmod g+w $(scripts_WORKDIR)/fake-db-root
 
-$(scripts_WORKDIR)/install: $(SRCDIR)/setup_native/scripts/install_linux.sh $(scripts_WORKDIR)/fake-db-1.0-0.noarch.rpm
+$(scripts_WORKDIR)/install: $(SRCDIR)/setup_native/scripts/install_linux.sh $(scripts_WORKDIR)/noarch/fake-db-1.0-0.noarch.rpm
 	$(PERL) -w $(SRCDIR)/setup_native/scripts/install_create.pl $^ $@
 	chmod 775 $@
 
