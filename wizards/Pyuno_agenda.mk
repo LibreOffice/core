2b9394f0c009b385756a0c5d75e3c1cd412f7236
Xisco Fauli <anistenis@gmail.com>
2013-02-14 00:57:02 +0100

    pyagenda: forgot this file
    Change-Id: I7542a7c9ac3ab9d48251f0f5d39dbec17b5c475c

---
+++ wizards/Pyuno_agenda.mk
@@ -0,0 +1,45 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+# Version: MPL 1.1 / GPLv3+ / LGPLv3+
+#
+# The contents of this file are subject to the Mozilla Public License Version
+# 1.1 (the "License"); you may not use this file except in compliance with
+# the License or as specified alternatively below. You may obtain a copy of
+# the License at http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+#
+# Major Contributor(s):
+# Copyright (C) 2011 Red Hat, Inc., David Tardon <dtardon@redhat.com>
+#  (initial developer)
+#
+# All Rights Reserved.
+#
+# For minor contributions see the git repository.
+#
+# Alternatively, the contents of this file may be used under the terms of
+# either the GNU General Public License Version 3 or later (the "GPLv3+"), or
+# the GNU Lesser General Public License Version 3 or later (the "LGPLv3+"),
+# in which case the provisions of the GPLv3+ or the LGPLv3+ are applicable
+# instead of those above.
+
+$(eval $(call gb_Pyuno_Pyuno,agenda,$(SRCDIR)/wizards/com/sun/star/wizards/agenda))
+
+$(eval $(call gb_Pyuno_add_files,agenda,\
+    AgendaDocument.py \
+    AgendaWizardDialogConst.py \
+    AgendaWizardDialogImpl.py \
+    AgendaWizardDialog.py \
+    AgendaWizardDialogResources.py \
+    CallWizard.py \
+    CGAgenda.py \
+    CGTopic.py \
+    __init__.py \
+    TemplateConsts.py \
+    TopicsControl.py \
+))
+$(eval $(call gb_Pyuno_set_componentfile_full,agenda,wizards/com/sun/star/wizards/agenda/agenda,vnd.openoffice.pymodule:wizards.agenda,.CallWizard))
+
+# vim:set noet sw=4 ts=4:
