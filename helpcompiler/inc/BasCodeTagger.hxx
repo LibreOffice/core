496222d64d36b69f95b790a8cdd47074b2410add
Caolán McNamara <caolanm@redhat.com>
2013-02-13 12:00:22 +0000

    add headers to new files
    Change-Id: Iaa390e50ed3cfe2ba9770c446869158b758bc48d

--- helpcompiler/inc/BasCodeTagger.hxx
+++ helpcompiler/inc/BasCodeTagger.hxx
@@ -1,3 +1,12 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 #ifndef BASCODETAGGER_HXX
 #define BASCODETAGGER_HXX
 
@@ -54,3 +63,5 @@ class L10N_DLLPUBLIC LibXmlTreeWalker
 };
 
 #endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
