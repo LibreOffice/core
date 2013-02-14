8cefe14f428ab5521f10962ff68b02151d1207ad
Tor Lillqvist <tml@iki.fi>
2013-02-14 11:28:26 +0200

    i18nisolang1 is not a module;)
    Change-Id: I3e239258e9f5d20c57a46f8c70b0558dfb7813ff

--- Module_cross_tail_build.mk
+++ Module_cross_tail_build.mk
@@ -74,7 +74,6 @@ $(eval $(call gb_Module_add_moduledirs,cross_tail_build,\
 	$(call gb_Helper_optional,CLUCENE,clucene) \
 	$(call gb_Helper_optional_for_host,DESKTOP, \
 		helpcompiler \
-		i18nisolang1 \
 		l10ntools \
 		sot \
 		svtools \
