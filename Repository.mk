8e81a12b76d1ea206044d16347040432d48b1053
Bjoern Michaelsen <bjoern.michaelsen@canonical.com>
2013-02-15 14:11:28 +0100

    proposal: move towards killing UNOLIBS_OOO and merge libs to OOOLIBS
    - this is a partial example commit moving ogltrans and mtfrenderer to
      OOOLIBS
    - this renames the libs universally as gbuild is making this trivial and
      little risk of having libs linking against the old name
    - having libs named differently (lib*.uno.so and lib*lo.so) just adds
      needles complexity to the build system and creates insecurity for new
      contributors (and is not really documented either)
    - we should probably look at the other lib groups for cheap kills too
      (renaming in stable/frozen URE isnt worth it though)
    
    Change-Id: I2106dd2afb71861acf3de7d21a2283473ea2110c

--- Repository.mk
+++ Repository.mk
@@ -536,6 +536,10 @@ $(eval $(call gb_Helper_register_libraries,RTVERLIBS, \
     salhelper \
 ))
 
+$(eval $(call gb_Helper_register_libraries,OOOLIBS, \
+    mtfrenderer \
+    OGLTrans \
+))
 $(eval $(call gb_Helper_register_libraries,UNOLIBS_OOO, \
     PresenterScreen \
     basprov \
@@ -566,9 +570,7 @@ $(eval $(call gb_Helper_register_libraries,UNOLIBS_OOO, \
     migrationoo2 \
     migrationoo3 \
     msforms \
-    mtfrenderer \
     nullcanvas \
-    OGLTrans \
     passwordcontainer \
     pdfimport \
     pythonloader \
