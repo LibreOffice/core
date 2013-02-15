b307085bcbf5e17c1d72171c33a5cc1cffd70f38
Caolán McNamara <caolanm@redhat.com>
2013-02-15 12:38:42 +0000

    Resolves: fdo#61241 background style dialog too small
    (cherry picked from commit 1ccbb620946c7032c57054550b08ad5362fe1c3f)
    
    Conflicts:
    	vcl/source/control/tabctrl.cxx
    	vcl/source/window/tabpage.cxx
    
    Change-Id: Ie7396b63a64e89bb7aa7c0e284faab7ed380c4c1
    Reviewed-on: https://gerrit.libreoffice.org/3188
    Reviewed-by: Fridrich Strba <fridrich@documentfoundation.org>
    Tested-by: Fridrich Strba <fridrich@documentfoundation.org>

--- vcl/source/control/tabctrl.cxx
+++ vcl/source/control/tabctrl.cxx
@@ -29,6 +29,7 @@
 #include "vcl/tabpage.hxx"
 #include "vcl/tabctrl.hxx"
 #include "vcl/controllayout.hxx"
+#include "vcl/layout.hxx"
 #include "vcl/lstbox.hxx"
 
 #include "controldata.hxx"
@@ -2179,7 +2180,7 @@ Size TabControl::calculateRequisition() const
         if (!pPage)
             continue;
 
-        Size aPageSize(pPage->GetOptimalSize(WINDOWSIZE_PREFERRED));
+        Size aPageSize(VclContainer::getLayoutRequisition(*pPage));
 
         if (aPageSize.Width() > aOptimalPageSize.Width())
             aOptimalPageSize.Width() = aPageSize.Width();
