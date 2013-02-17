3d095eb4193eafcc4bf403c8e299ee4bc1a0295b
Norbert Thiebaud <nthiebaud@gmail.com>
2013-02-17 13:10:03 -0600

    coverity#983356 ressource leak
    Change-Id: Idcc2d548363ddab04dd8acaa1dfb854a19ca47ef

--- writerperfect/source/filter/OdgGenerator.cxx
+++ writerperfect/source/filter/OdgGenerator.cxx
@@ -1325,7 +1325,13 @@ void OdgGeneratorPrivate::_writeGraphicsStyle()
                 mGraphicsGradientStyles.push_back(new TagCloseElement("draw:opacity"));
             }
         }
-
+        else
+        {
+            /* if mxGradient.count() == 1 for some reason we would leak
+             * pDrawGradientElement
+             */
+            delete pDrawGradientElement;
+        }
         if(!bUseOpacityGradient)
             delete pDrawOpacityElement;
     }
