5559b7bf538aad28be615b587500fa9e93a4dc59
Stephan Bergmann <sbergman@redhat.com>
2013-02-17 11:49:34 +0100

    Missing include in liborcus when building against Boost 1.53.0
    Change-Id: Ie6c944d53d6e2fcdd161f674e32c9fbfd761d844

--- liborcus/UnpackedTarball_orcus.mk
+++ liborcus/UnpackedTarball_orcus.mk
@@ -18,6 +18,10 @@ orcus_patches :=
 orcus_patches += liborcus_0.1.0-configure.patch
 # disable boost "auto lib" in MSVC build
 orcus_patches += liborcus_0.1.0-boost_disable_auto_lib.patch
+# <https://gitorious.org/orcus/orcus/merge_requests/2#
+# f60d6eecee72349993a392a9a63ddf3383d3b8c8-
+# f60d6eecee72349993a392a9a63ddf3383d3b8c8@2>:
+orcus_patches += liborcus_0.1.0-boost-include.patch.1
 
 $(eval $(call gb_UnpackedTarball_fix_end_of_line,orcus,\
 	vsprojects/liborcus-static-nozip/liborcus-static-nozip.vcproj \
