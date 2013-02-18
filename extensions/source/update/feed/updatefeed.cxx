e6ec8e83bad7d04f9f7b503626b87af75c052deb
Petr Mladek <pmladek@suse.cz>
2013-02-18 11:03:12 +0100

    avoid space in the version of the update information provider
    LO-4.0.0.3 sends "4.0 .0.3" to the server.
    
    Change-Id: I2cda062778ac3632f89eb5c0d1cc85e57d8d1870
    Reviewed-on: https://gerrit.libreoffice.org/2228
    Reviewed-by: Jan Holesovsky <kendy@suse.cz>
    Tested-by: Jan Holesovsky <kendy@suse.cz>

--- extensions/source/update/feed/updatefeed.cxx
+++ extensions/source/update/feed/updatefeed.cxx
@@ -358,7 +358,6 @@ UpdateInformationProvider::UpdateInformationProvider(
         UNISTRING("org.openoffice.Setup/Product"),
         UNISTRING("ooSetupExtension")) >>= extension;
     if (!extension.isEmpty()) {
-        buf.append(sal_Unicode(' '));
         buf.append(extension);
     }
     rtl::OUString product(buf.makeStringAndClear());
@@ -376,6 +375,8 @@ UpdateInformationProvider::UpdateInformationProvider(
             i, RTL_CONSTASCII_LENGTH("<PRODUCT>"), product);
         i += product.getLength();
     }
+
+    SAL_INFO("extensions.update", "UpdateUserAgent: " << aUserAgent);
 
     m_aRequestHeaderList[0].Name = UNISTRING("Accept-Language");
     m_aRequestHeaderList[0].Value = getConfigurationItem( xConfigurationProvider, UNISTRING("org.openoffice.Setup/L10N"), UNISTRING("ooLocale") );
