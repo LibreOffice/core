aaf30ccdc3ce033958cf3e2b1cc847dd1d57e74a
Norbert Thiebaud <nthiebaud@gmail.com>
2013-02-18 04:20:14 -0600

    coverity#982590 Explicit null dereferenced
    Change-Id: Ie898603f64a4568a8502caf3db154ae5ce008af9

--- xmlsecurity/source/xmlsec/nss/nssinitializer.cxx
+++ xmlsecurity/source/xmlsec/nss/nssinitializer.cxx
@@ -294,10 +294,13 @@ bool nsscrypto_initialize( const css::uno::Reference< css::uno::XComponentContex
         if ( NSS_NoDB_Init(NULL) != SECSuccess )
         {
             xmlsec_trace("Initializing NSS without profile failed.");
-            char * error = NULL;
-            PR_GetErrorText(error);
-            if (error)
+            int errlen = PR_GetErrorTextLength();
+            if(errlen > 0)
+            {
+                char error[errlen + 1];
+                PR_GetErrorText(error);
                 xmlsec_trace("%s",error);
+            }
             return false ;
         }
     }
