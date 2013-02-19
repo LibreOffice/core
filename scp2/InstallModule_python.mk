83b4703137fdf49304e41202cc91a5574b2e53e7
Andras Timar <atimar@suse.com>
2013-02-19 15:21:23 +0100

    fdo#49202 install the "Python-UNO Bridge" component on Windows unconditionally
    Change-Id: I1d9d62b024c4442182d67d93915af0f793265ebd

--- scp2/InstallModule_python.mk
+++ scp2/InstallModule_python.mk
@@ -57,12 +57,11 @@ $(eval $(call gb_InstallModule_add_scpfiles,scp2/python,\
     scp2/source/python/file_python \
     scp2/source/python/file_python_librelogo \
     scp2/source/python/profileitem_python \
+    scp2/source/python/module_python \
 ))
 
 $(eval $(call gb_InstallModule_add_localized_scpfiles,scp2/python,\
-    scp2/source/python/module_python \
     scp2/source/python/module_python_librelogo \
-    scp2/source/python/module_python_mailmerge \
 ))
 
 # vim: set shiftwidth=4 tabstop=4 noexpandtab:
