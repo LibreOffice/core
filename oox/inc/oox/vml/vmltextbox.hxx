94108f529a11062eb2749b5141752c884d106464
Cédric Bosdonnat <cedric.bosdonnat@free.fr>
2013-02-21 17:34:16 +0100

    n#779642: Import vertical layout mode from VML elements. (cherry picked from commit 3d46b39491af97ba360fb92182501e6b399f4f56)

--- oox/inc/oox/vml/vmltextbox.hxx
+++ oox/inc/oox/vml/vmltextbox.hxx
@@ -95,6 +95,7 @@ public:
     /// Text distance from the border (inset attribute of v:textbox), valid only if set.
     bool borderDistanceSet;
     int borderDistanceLeft, borderDistanceTop, borderDistanceRight, borderDistanceBottom;
+    OUString maLayoutFlow;
 
 private:
     typedef ::std::vector< TextPortionModel > PortionVector;
