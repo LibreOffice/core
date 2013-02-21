613221bde1744265c336c2b5f2ade0bfb9b22d16
Peter Foley <pefoley2@verizon.net>
2013-02-21 00:56:13 -0600

    convert odk to gbuild and add to tail_build
    Change-Id: I8e8de7f2bb87cce7916c7c2df24c1b0ddaea55c0
    Reviewed-on: https://gerrit.libreoffice.org/2288
    Reviewed-by: Peter Foley <pefoley2@verizon.net>
    Tested-by: Peter Foley <pefoley2@verizon.net>

---
+++ odk/Zip_odkexamples.mk
@@ -0,0 +1,39 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_Zip_Zip,odkexamples,$(SRCDIR)/odk))
+
+$(eval $(call gb_Zip_add_commandoptions,odkexamples,\
+	-r \
+	-x "*makefile.mk" \
+	-x "*Container1*" \
+	-x "*Storage*" \
+	-x "*register_component*" \
+	-x "*examples.html" \
+	-x "*ConverterServlet*" \
+	-x "*NotesAccess*" \
+	-x "*delphi*" \
+))
+
+$(eval $(call gb_Zip_add_files,odkexamples,\
+	examples \
+))
+
+#FIXME: scp2 seems to require the zip to be in bin
+$(call gb_Zip_get_final_target,odkexamples) : $(OUTDIR)/bin/odkexamples.zip
+$(call gb_Zip_get_clean_target,odkexamples) : clean_odkexamples_zip
+
+$(OUTDIR)/bin/odkexamples.zip : $(call gb_Zip_get_target,odkexamples)
+	$(call gb_Deliver_deliver,$<,$@)
+
+.PHONY : clean_odkexamples_zip
+clean_odkexamples_zip:
+	rm -f $(OUTDIR)/bin/odkexamples.zip
+
+# vim: set noet sw=4 ts=4:
