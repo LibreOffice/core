bf80e39db6f8d60cbb920c77f4d61cd8f160a442
Norbert Thiebaud <nthiebaud@gmail.com>
2013-02-21 00:56:13 -0600

    convert odk to gbuild and add to tail_build
    Change-Id: I8e8de7f2bb87cce7916c7c2df24c1b0ddaea55c0

---
+++ odk/CustomTarget_classes.mk
@@ -0,0 +1,36 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_CustomTarget_CustomTarget,odk/odkcommon/classes))
+
+$(eval $(call gb_JavaClassSet_JavaClassSet,loader))
+
+$(eval $(call gb_JavaClassSet_add_sourcefiles,loader,\
+	odk/source/com/sun/star/lib/loader/Loader \
+	odk/source/com/sun/star/lib/loader/InstallationFinder \
+	odk/source/com/sun/star/lib/loader/WinRegKey \
+	odk/source/com/sun/star/lib/loader/WinRegKeyException \
+))
+
+odkcommon_ZIPLIST += classes/com/sun/star/lib/loader
+$(call gb_CustomTarget_get_target,odk/odkcommon/classes): $(odk_WORKDIR)/classes/java.done
+$(odk_WORKDIR)/classes/java.done: $(call gb_JavaClassSet_get_target,loader)
+	mkdir -p $(dir $@)
+	$(call gb_Output_announce,$(subst $(WORKDIR)/,,$@),build,CPY,1)
+	cp -a $(call gb_JavaClassSet_get_classdir,loader)/com $(dir $@)
+	touch $@
+
+odkcommon_ZIPLIST += classes/win/unowinreg.dll
+$(eval $(call gb_CustomTarget_register_target,odk/odkcommon/classes,win/unowinreg.dll))
+$(odk_WORKDIR)/classes/win/unowinreg.dll: $(call gb_CustomTarget_get_workdir,odk/unowinreg)/unowinreg.dll
+	mkdir -p $(dir $@)
+	$(call gb_Output_announce,$(subst $(WORKDIR)/,,$@),build,CPY,1)
+	cp $< $@
+
+# vim: set noet sw=4 ts=4:
---
+++ odk/Module_odk.mk
@@ -0,0 +1,41 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_Module_Module,odk))
+
+$(eval $(call gb_Module_add_targets,odk,\
+	CustomTarget_odkcommon \
+	$(if $(filter WNT,$(OS)),CustomTarget_cli) \
+	$(if $(DOXYGEN),CustomTarget_doxygen) \
+	CustomTarget_check \
+	CustomTarget_bin \
+	CustomTarget_lib \
+	CustomTarget_settings \
+	CustomTarget_autodoc \
+	CustomTarget_idl \
+	Executable_unoapploader \
+	Zip_odkexamples \
+))
+
+ifneq ($(SOLAR_JAVA),)
+$(eval $(call gb_Module_add_targets,odk,\
+	CustomTarget_unowinreg \
+	CustomTarget_classes \
+	CustomTarget_javadoc \
+	$(if $(filter WNT,$(OS)),Library_unowinreg) \
+	Zip_uno_loader_classes \
+))
+endif
+
+$(eval $(call gb_Module_add_targets,odk,\
+	Zip_odkcommon \
+))
+
+
+# vim: set noet sw=4 ts=4:
