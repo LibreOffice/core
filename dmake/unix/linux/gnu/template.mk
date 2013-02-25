a6ed986f12ec6bbbe1d1ba3c6de5ccd13f26f720
Bjoern Michaelsen <bjoern.michaelsen@canonical.com>
2013-02-25 11:56:21 +0100

    remove dmake, build.pl, deliver.pl and zipdep.pl
    This is writing history: LibreOffice builds in ONE non-recursive make process
    with full dependencies. We will now be able to really move forward without the
    old build system. A big 'Thank you!' goes out to everyone contributing to
    solenv/gbuild, especially:
    
    - David Tardon
    - Norbert Thiebaud
    - Tor Lillqvist
    - Michael Stahl
    - Matúš Kukan
    - Stephan Bergmann
    - Luboš Luňák
    - Caolán McNamara
    - Mathias Bauer
    - Jan Holesovsky
    - Peter Foley
    - Andras Timar
    - Hans-Joachim Lankenau
    
    and all the heroes migrating all the modules of LibreOffice to gbuild.
    
    By explicit request this commit has to be completed with this quote:
    
        I say we take off and nuke the entire site from orbit.
        It's the only way to be sure.
    
    Hold on a second.
    This installation has a substantial dollar value attached to it.
    
        They can bill me.
    
    Change-Id: I72fa17cfb24fae00ca78cfe0eb5782c1788d2dcc

--- dmake/unix/linux/gnu/template.mk
+++
@@ -1,7 +0,0 @@
-# ** Default build configuration for dmake.
-# ** DO NOT PLACE LOCAL DEFINITIONS INTO THIS FILE IT IS AUTO GENERATED
-# ** USE "startup/local.mk" for those.
-
-   OS            *:= unix
-   OSRELEASE     *:= linux
-   OSENVIRONMENT *:= gnu
