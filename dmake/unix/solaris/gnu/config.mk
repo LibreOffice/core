ebfe08f50f7f9f3be36aeaa5f6f2d634eab4bc87
Bjoern Michaelsen <bjoern.michaelsen@canonical.com>
2013-02-25 11:56:21 +0100

    remove dmake
    This is writing history: LibreOffice builds in ONE non-recursive make process
    with full dependencies. We will now be able to really move forward without the
    old build system. A big 'Thank you!' goes out to everyone contributing to
    solenv/gbuild, especially:
    
    - David Tardon
    - Norbert Thiebaud
    - Tor Lillqvist
    - Michael Stahl
    - Matúš Kukan
    - Stephan Bergmann
    - Luboš Luňák
    - Caolán McNamara
    - Mathias Bauer
    - Jan Holesovsky
    - Peter Foley
    - Andras Timar
    - Hans-Joachim Lankenau
    
    and all the heroes migrating all the modules of LibreOffice to gbuild.
    
    Conflicts:
    	Makefile.in
    
    Change-Id: I95400c6a61a6d4aed8f85c060ad3c795e8bcdd1c

--- dmake/unix/solaris/gnu/config.mk
+++
@@ -1,8 +0,0 @@
-# This is the Solaris gcc configuration file for DMAKE
-#	It modifies the value of CC to be gcc
-#
-
-CC = gcc
-
-# disable a gcc bug when compiling runargv.c
-runargv.o ?= CFLAGS += -g
