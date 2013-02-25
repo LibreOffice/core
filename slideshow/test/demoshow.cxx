288a8a2e55d5f9025abf863aa80b19adac1ab073
Ricardo Montania <ricardo@linuxafundo.com.br>
2013-02-25 15:36:57 -0300

    String::CreateFromAscii removal
    Change-Id: I86a33ef14b2bcdea1655db410f36d799689a1377

--- slideshow/test/demoshow.cxx
+++ slideshow/test/demoshow.cxx
@@ -491,7 +491,7 @@ sal_uInt16 DemoApp::Exception( sal_uInt16 nError )
     switch( nError & EXC_MAJORTYPE )
     {
         case EXC_RSCNOTLOADED:
-            Abort( String::CreateFromAscii( "Error: could not load language resources.\nPlease check your installation.\n" ) );
+            Abort( "Error: could not load language resources.\nPlease check your installation.\n" );
             break;
     }
     return 0;
