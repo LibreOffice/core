ca3a1ae7f4a3a504e23eedcb5c8103d2d122e605
David Tardon <dtardon@redhat.com>
2013-02-27 10:11:27 +0100

    add helpcontent2 to tail_build
    Change-Id: I14020dbe0a174ca58698a8605ff64f46e7502f19

--- Module_tail_build.mk
+++ Module_tail_build.mk
@@ -85,6 +85,7 @@ $(eval $(call gb_Module_add_moduledirs,tail_build,\
 	$(call gb_Helper_optional,FREETYPE,freetype) \
 	$(call gb_Helper_optional,GRAPHITE,graphite) \
 	$(call gb_Helper_optional,DESKTOP,helpcompiler) \
+	$(call gb_Helper_optional,HELP,helpcontent2) \
 	$(call gb_Helper_optional,HSQLDB,hsqldb) \
 	$(call gb_Helper_optional,HUNSPELL,hunspell) \
 	hwpfilter \
