LibreOffice/core

commit 50cb1667020494906afaacb68d4163d1eda527cf

fdo#49940 dmapper: handle m_bTitlePage when m_nBreakType is zero
We used to ignore m_bTitlePage in this case, resulting in wrong
'Default' page style for the first page, instead of 'First Page'.

Change-Id: I1899354fb39db4f0eb663fd5233395f2d4a5e72a

writerfilter/source/dmapper/PropertyMap.cxx
@@ -794,6 +794,19 @@ void SectionPropertyMap::PrepareHeaderFooterProperties( bool bFirstPage )
     operator[]( PropertyDefinition( PROP_BOTTOM_MARGIN, false )) = uno::makeAny( m_nBottomMargin + nFooterHeight );
 }
 
+uno::Reference<beans::XPropertySet> lcl_GetRangeProperties(bool bIsFirstSection, DomainMapper_Impl& rDM_Impl, uno::Reference<text::XTextRange> xStartingRange)
+{
+    uno::Reference< beans::XPropertySet > xRangeProperties;
+    if (bIsFirstSection)
+    {
+        uno::Reference<container::XEnumerationAccess> xEnumAccess(rDM_Impl.GetBodyText(), uno::UNO_QUERY_THROW);
+        uno::Reference<container::XEnumeration> xEnum = xEnumAccess->createEnumeration();
+        xRangeProperties = uno::Reference<beans::XPropertySet>(xEnum->nextElement(), uno::UNO_QUERY_THROW);
+    }
+    else
+        xRangeProperties = uno::Reference<beans::XPropertySet>(xStartingRange, uno::UNO_QUERY_THROW);
+    return xRangeProperties;
+}
 
 void SectionPropertyMap::CloseSectionGroup( DomainMapper_Impl& rDM_Impl )
 {
@@ -834,6 +847,8 @@ void SectionPropertyMap::CloseSectionGroup( DomainMapper_Impl& rDM_Impl )
                                     rDM_Impl.appendTextSectionAfter( m_xStartingRange );
         if( m_nColumnCount > 0 && xSection.is())
             ApplyColumnProperties( xSection );
+        uno::Reference<beans::XPropertySet> xRangeProperties(lcl_GetRangeProperties(m_bIsFirstSection, rDM_Impl, m_xStartingRange));
+        xRangeProperties->setPropertyValue(rPropNameSupplier.GetName(PROP_PAGE_DESC_NAME), uno::makeAny(m_bTitlePage ? m_sFirstPageStyleName : m_sFollowPageStyleName));
     }
     else
     {
@@ -963,15 +978,7 @@ void SectionPropertyMap::CloseSectionGroup( DomainMapper_Impl& rDM_Impl )
         {
             {
                 //now apply this break at the first paragraph of this section
-                uno::Reference< beans::XPropertySet > xRangeProperties;
-                if( m_bIsFirstSection )
-                {
-                    uno::Reference< container::XEnumerationAccess > xEnumAccess( rDM_Impl.GetBodyText( ), uno::UNO_QUERY_THROW );
-                    uno::Reference< container::XEnumeration >  xEnum = xEnumAccess->createEnumeration( );
-                    xRangeProperties = uno::Reference< beans::XPropertySet >( xEnum->nextElement( ), uno::UNO_QUERY_THROW );
-                }
-                else
-                    xRangeProperties = uno::Reference< beans::XPropertySet >( m_xStartingRange, uno::UNO_QUERY_THROW );
+                uno::Reference<beans::XPropertySet> xRangeProperties(lcl_GetRangeProperties(m_bIsFirstSection, rDM_Impl, m_xStartingRange));
             /* break type
             0 - No break 1 - New Colunn 2 - New page 3 - Even page 4 - odd page */
                 xRangeProperties->setPropertyValue(rPropNameSupplier.GetName( PROP_PAGE_DESC_NAME ),

commit 8281578b89c3df3fe3452a594f6b21483683638a

fdo#53985 DOCX import: don't try to set empty PageDescName
Regression from 50cb1667020494906afaacb68d4163d1eda527cf.

Change-Id: Ie04d5ad37555ed4cfeb34ccb8dc021675fbd25ea

writerfilter/source/dmapper/PropertyMap.cxx
@@ -840,7 +840,20 @@ void SectionPropertyMap::CloseSectionGroup( DomainMapper_Impl& rDM_Impl )
             ApplyColumnProperties( xSection );
         uno::Reference<beans::XPropertySet> xRangeProperties(lcl_GetRangeProperties(m_bIsFirstSection, rDM_Impl, m_xStartingRange));
         if (xRangeProperties.is())
-            xRangeProperties->setPropertyValue(rPropNameSupplier.GetName(PROP_PAGE_DESC_NAME), uno::makeAny(m_bTitlePage ? m_sFirstPageStyleName : m_sFollowPageStyleName));
+        {
+            OUString aName = m_bTitlePage ? m_sFirstPageStyleName : m_sFollowPageStyleName;
+            if (!aName.isEmpty())
+            {
+                try
+                {
+                    xRangeProperties->setPropertyValue(rPropNameSupplier.GetName(PROP_PAGE_DESC_NAME), uno::makeAny(aName));
+                }
+                catch( const uno::Exception& )
+                {
+                    SAL_WARN("writerfilter", "failed to set PageDescName!");
+                }
+            }
+        }
     }
     // If the section is of type "New column" (0x01), then simply insert a column break.
     // But only if there actually are columns on the page, otherwise a column break

sw/qa/extras/ooxmlimport/ooxmlimport.cxx
@@ -117,6 +117,7 @@ public:
     void testGroupshapeLine();
     void testN779642();
     void testTbLrHeight();
+    void testFdo53985();
 
     CPPUNIT_TEST_SUITE(Test);
 #if !defined(MACOSX) && !defined(WNT)
@@ -185,6 +186,7 @@ void Test::run()
         {"groupshape-line.docx", &Test::testGroupshapeLine},
         {"n779642.docx", &Test::testN779642},
         {"tblr-height.docx", &Test::testTbLrHeight},
+        {"fdo53985.docx", &Test::testFdo53985},
     };
     header();
     for (unsigned int i = 0; i < SAL_N_ELEMENTS(aMethods); ++i)
@@ -1194,6 +1196,15 @@ void Test::testTbLrHeight()
     uno::Reference<table::XTableRows> xTableRows(xTable->getRows(), uno::UNO_QUERY);
     // btLr text direction was imported as MIN, it should be FIX to avoid incorrectly large height in case of too much content.
     CPPUNIT_ASSERT_EQUAL(text::SizeType::FIX, getProperty<sal_Int16>(xTableRows->getByIndex(0), "SizeType"));
+}
+
+void Test::testFdo53985()
+{
+    // Unhandled excetion prevented import of the rest of the document.
+
+    uno::Reference<text::XTextTablesSupplier> xTablesSupplier(mxComponent, uno::UNO_QUERY);
+    uno::Reference<container::XIndexAccess> xTables(xTablesSupplier->getTextTables( ), uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(5), xTables->getCount()); // Only 4 tables were imported.
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(Test);