45532cef80be1c658484282ec41031d02b91a218
Miklos Vajna <vmiklos@suse.cz>
2013-02-27 10:44:20 +0100

    ww8export test: fail if control shape not found
    This check was added due to random failures (where it failed if the
    shape was completely missing, while the test wanted to check a property
    of the shape), but it shoult no longer be necessary since
    2ed931303b10386153a1337fa15b225aabd40cb4
    
    Change-Id: Ia5665b4f292cee09eab52b76e53c34a44d545896

--- sw/qa/extras/ww8export/ww8export.cxx
+++ sw/qa/extras/ww8export/ww8export.cxx
@@ -102,13 +102,9 @@ void Test::testFdo45724()
     uno::Reference<drawing::XDrawPageSupplier> xDrawPageSupplier(mxComponent, uno::UNO_QUERY);
     uno::Reference<container::XIndexAccess> xDraws(xDrawPageSupplier->getDrawPage(), uno::UNO_QUERY);
     uno::Reference<drawing::XControlShape> xControlShape(xDraws->getByIndex(0), uno::UNO_QUERY);
-    // This should not happen (the shape was there before the fix as well), but let's avoid a segfault in some random cases...
-    if (xControlShape.is())
-    {
-        uno::Reference<form::validation::XValidatableFormComponent> xComponent(xControlShape->getControl(), uno::UNO_QUERY);
-        CPPUNIT_ASSERT_EQUAL(COL_WHITE, getProperty<sal_uInt32>(xComponent, "BackgroundColor"));
-        CPPUNIT_ASSERT_EQUAL(OUString("xxx"), xComponent->getCurrentValue().get<OUString>());
-    }
+    uno::Reference<form::validation::XValidatableFormComponent> xComponent(xControlShape->getControl(), uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(COL_WHITE, getProperty<sal_uInt32>(xComponent, "BackgroundColor"));
+    CPPUNIT_ASSERT_EQUAL(OUString("xxx"), xComponent->getCurrentValue().get<OUString>());
 }
 
 void Test::testFdo46020()
