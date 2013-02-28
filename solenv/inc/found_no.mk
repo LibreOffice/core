270c204d336e17ee311d29ddff37392a823d34c2
Caolán McNamara <caolanm@redhat.com>
2013-02-28 13:16:54 +0000

    remove the unused dmake solenv .mk files
    note that the remaining langlist.mk and minor.mk are included by gbuild
    makefiles
    
    Change-Id: Ib3285cbba0e10ec033bb5968bad5ef9a7d507dd8

--- solenv/inc/found_no.mk
+++
@@ -1,5 +0,0 @@
-no_traget:
-    @echo -----------------------------------------------
-    @echo         Error: No makefile.mk found!
-    @echo -----------------------------------------------
-    force_dmake_to_error
