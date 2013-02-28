1db45205a92318fa3f7662c2e90b4ccb20510cb1
Michael Stahl <mstahl@redhat.com>
2013-02-28 17:40:00 +0100

    gbuild: WinResTarget: for dependencies use the actual target
    Hopefully fixes silly .d files that have "makedepend" as target.
    
    Change-Id: I7ebf3c56187acc94f4374353db255cdbffa6df02

--- solenv/gbuild/WinResTarget.mk
+++ solenv/gbuild/WinResTarget.mk
@@ -29,7 +29,7 @@ endef
 
 ifeq ($(gb_FULLDEPS),$(true))
 $(call gb_WinResTarget_get_dep_target,%) : $(gb_WinResTarget__command_target)
-	$(call gb_WinResTarget__command_dep,$@,$*,$<)
+	$(call gb_WinResTarget__command_dep,$@,$*,$(call gb_WinResTarget_get_target,%))
 endif
 
 
