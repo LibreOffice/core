LibreOffice/core

commit 76b3e61bb4ac66decd7800ef309f3241e77d538f

loplugin: unused variables
Change-Id: I07b82471f0f32e9e894d4eb392beedeafc6be0c7

vcl/source/gdi/image.cxx
@@ -347,7 +347,6 @@ ImageList::ImageList( const ResId& rResId ) :
         pResMgr->Increment( sizeof( RSHEADER_TYPE ) );
 
         sal_uLong                               nObjMask = pResMgr->ReadLong();
-        const String                        aPrefix( pResMgr->ReadString() );
         ::boost::scoped_ptr< Color >        spMaskColor;
 
         if( nObjMask & RSC_IMAGE_MASKCOLOR )

sc/source/core/data/dpgroup.cxx
@@ -845,8 +845,6 @@ void ScDPGroupTableData::FillGroupValues(vector<SCROW>& rItems, const vector<lon
                 SvNumberFormatter* pFormatter = pDoc->GetFormatTable();
                 sal_Int32 nPartValue = ScDPUtil::getDatePartValue(
                     pData->GetValue(), *pNumInfo, nDatePart, pFormatter);
-                rtl::OUString aName = ScDPUtil::getDateGroupName(
-                    nDatePart, nPartValue, pFormatter, pNumInfo->mfStart, pNumInfo->mfEnd);
 
                 ScDPItemData aItem(nDatePart, nPartValue);
                 rItems[i] = pCache->GetIdByItemData(nColumn, aItem);

sc/source/core/tool/dbdata.cxx
@@ -784,7 +784,6 @@ ScDBData* ScDBCollection::AnonDBs::getByRange(const ScRange& rRange)
 
 void ScDBCollection::AnonDBs::insert(ScDBData* p)
 {
-    rtl::OUString aName(RTL_CONSTASCII_USTRINGPARAM(STR_DB_GLOBAL_NONAME));
     SAL_WNODEPRECATED_DECLARATIONS_PUSH
     ::std::auto_ptr<ScDBData> pNew(p);
         SAL_WNODEPRECATED_DECLARATIONS_POP

sc/source/filter/oox/condformatbuffer.cxx
@@ -726,7 +726,7 @@ void CondFormatRule::finalizeImport()
 
     if( !aReplaceFormula.isEmpty() )
     {
-        OUString aAddress, aRanges, aText, aComp;
+        OUString aAddress;
         sal_Int32 nStrPos = aReplaceFormula.getLength();
         while( (nStrPos = aReplaceFormula.lastIndexOf( '#', nStrPos )) >= 0 )
         {

sc/source/ui/condformat/condformatdlgentry.cxx
@@ -449,7 +449,6 @@ void StyleSelect( ListBox& rLbStyle, ScDocument* pDoc, SvxFontPrevWindow& rWdPre
             pDisp->Lock(sal_True);
 
         // Find the new style and add it into the style list boxes
-        rtl::OUString aNewStyle;
         SfxStyleSheetIterator aStyleIter( pDoc->GetStyleSheetPool(), SFX_STYLE_FAMILY_PARA );
         bool bFound = false;
         for ( SfxStyleSheetBase* pStyle = aStyleIter.First(); pStyle && !bFound; pStyle = aStyleIter.Next() )

sc/source/ui/docshell/docsh.cxx
@@ -424,7 +424,6 @@ sal_Bool ScDocShell::LoadXML( SfxMedium* pLoadMedium, const ::com::sun::star::un
     //did not use cached formula results.
     uno::Reference<document::XDocumentPropertiesSupplier> xDPS(GetModel(), uno::UNO_QUERY_THROW);
     uno::Reference<document::XDocumentProperties> xDocProps = xDPS->getDocumentProperties();
-    rtl::OUString sGenerator = xDocProps->getGenerator();
 
     Reference<uno::XComponentContext> xContext = comphelper::getProcessComponentContext();
     ScRecalcOptions nRecalcMode =

sc/source/ui/navipi/content.cxx
@@ -370,7 +370,6 @@ void ScContentTree::KeyInput( const KeyEvent& rKEvt )
 
                     if( (nType != SC_CONTENT_ROOT) && (nChild == SC_CONTENT_NOCHILD) )
                     {
-                        String aText( GetEntryText( pEntry ) );
                         if ( IsExpanded( pEntry ) )
                             Collapse( pEntry );
                         else

sc/source/ui/unoobj/shapeuno.cxx
@@ -290,7 +290,6 @@ static ScRange lcl_GetAnchorCell( uno::Reference< drawing::XShape >& xShape, ScD
 {
     ScRange aReturn;
     rUnoPoint = xShape->getPosition();
-    rtl::OUString sType(xShape->getShapeType());
     sal_Bool bCaptionShape(lcl_GetCaptionPoint(xShape, rCaptionPoint));
     if (pDoc->IsNegativePage(nTab))
     {

commit a228c580c3107e6dae447fbae3bca544144a5c0b

Resolves: fdo#61459 icons loaded from .res broken
regression since 76b3e61bb4ac66decd7800ef309f3241e77d538f

Change-Id: Idfc7789b402aba0d6ab41875a9efc3b990173af0

vcl/source/gdi/image.cxx
@@ -347,6 +347,7 @@ ImageList::ImageList( const ResId& rResId ) :
         pResMgr->Increment( sizeof( RSHEADER_TYPE ) );
 
         sal_uLong                               nObjMask = pResMgr->ReadLong();
+        pResMgr->ReadString(); //skip string
         ::boost::scoped_ptr< Color >        spMaskColor;
 
         if( nObjMask & RSC_IMAGE_MASKCOLOR )