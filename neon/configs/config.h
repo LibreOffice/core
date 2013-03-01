331dd1e80616030f824a91a2358857485a798e29
Stephan Bergmann <sbergman@redhat.com>
2013-03-01 15:38:25 +0100

    Bring the hard-coded neon config.h in sync with neon 0.29.5 config.h.in
    ...keeping all the defines that had not yet been coverd as undefined for now.
    (HAVE_SOCKS_H curiously has disappeared from neon 0.29.5 config.h.in even though
    src/ne_socket.c still references it, but we had it undefined anyway.)
    
    In general, this hard-coded config.h is a bad idea, though, of course.
    
    Change-Id: I5337f1849b776b62676cee7794917742bcf35356

--- neon/configs/config.h
+++ neon/configs/config.h
@@ -1,3 +1,8 @@
+/* Contents kept in sync with config.h.in from neon 0.29.5 */
+
+/* Define if building universal (internal helper macro) */
+/* #undef AC_APPLE_UNIVERSAL_BUILD */
+
 /* Define to specific EGD socket path */
 /* #undef EGD_PATH */
 
@@ -13,7 +18,10 @@
 #endif
 
 /* Define to 1 if you have the `bind_textdomain_codeset' function. */
-/* #undef HAVE_BIND_TEXTDOMAIN_CODESET 1 */
+/* #undef HAVE_BIND_TEXTDOMAIN_CODESET */
+
+/* Define to 1 if you have the `CRYPTO_set_idptr_callback' function. */
+/* #undef HAVE_CRYPTO_SET_IDPTR_CALLBACK */
 
 /* Define to 1 if you have the declaration of `h_errno', and to 0 if you
    don't. */
@@ -44,17 +52,44 @@
 /* Define if you have expat */
 /* #undef HAVE_EXPAT */
 
+/* Define to 1 if you have the `fcntl' function. */
+/* #undef HAVE_FCNTL */
+
+/* Define to 1 if you have the <fcntl.h> header file. */
+/* #undef HAVE_FCNTL_H */
+
 /* Define to 1 if you have the `fstat64' function. */
 #define HAVE_FSTAT64
 
 /* Define to 1 if you have the `gai_strerror' function. */
 #define HAVE_GAI_STRERROR 1
 
+/* Define to 1 if you have the `gethostname' function. */
+/* #undef HAVE_GETHOSTNAME */
+
+/* Define to 1 if you have the `getnameinfo' function. */
+/* #undef HAVE_GETNAMEINFO */
+
+/* Define to 1 if you have the `getsockopt' function. */
+/* #undef HAVE_GETSOCKOPT */
+
 /* Define if GnuTLS support is enabled */
 /* #undef HAVE_GNUTLS */
 
+/* Define to 1 if you have the `gnutls_certificate_get_x509_cas' function. */
+/* #undef HAVE_GNUTLS_CERTIFICATE_GET_X509_CAS */
+
+/* Define to 1 if you have the `gnutls_certificate_verify_peers2' function. */
+/* #undef HAVE_GNUTLS_CERTIFICATE_VERIFY_PEERS2 */
+
 /* Define to 1 if you have the `gnutls_session_get_data2' function. */
 /* #undef HAVE_GNUTLS_SESSION_GET_DATA2 */
+
+/* Define to 1 if you have the `gnutls_sign_callback_set' function. */
+/* #undef HAVE_GNUTLS_SIGN_CALLBACK_SET */
+
+/* Define to 1 if you have the `gnutls_x509_dn_get_rdn_ava' function. */
+/* #undef HAVE_GNUTLS_X509_DN_GET_RDN_AVA */
 
 /* Define if GSSAPI support is enabled */
 /* #undef HAVE_GSSAPI */
@@ -74,10 +109,19 @@
 /* Define to 1 if you have the `hstrerror' function. */
 /* #undef HAVE_HSTRERROR */
 
+/* Define to 1 if you have the `iconv' function. */
+/* #undef HAVE_ICONV */
+
+/* Define to 1 if you have the <iconv.h> header file. */
+/* #undef HAVE_ICONV_H */
+
 /* Define to 1 if you have the `inet_ntop' function. */
 #ifdef UNX
 #define HAVE_INET_NTOP 1
 #endif
+
+/* Define to 1 if you have the `inet_pton' function. */
+/* #undef HAVE_INET_PTON */
 
 /* Define to 1 if you have the <inttypes.h> header file. */
 #ifdef UNX
@@ -89,6 +133,9 @@
 
 /* Define to 1 if you have the <libintl.h> header file. */
 /* #undef HAVE_LIBINTL_H */
+
+/* Define if libproxy is supported */
+/* #undef HAVE_LIBPROXY */
 
 /* Define if you have libxml */
 #define HAVE_LIBXML 1
@@ -126,6 +173,11 @@
 #define HAVE_NETINET_TCP_H 1
 #endif
 
+/* Define if NTLM is supported */
+#ifndef WIN32
+#define HAVE_NTLM 1
+#endif
+
 /* Define if OpenSSL support is enabled */
 #define HAVE_OPENSSL 1
 
@@ -134,6 +186,9 @@
 
 /* Define to 1 if you have the <openssl/ssl.h> header file. */
 #define HAVE_OPENSSL_SSL_H 1
+
+/* Define if pakchois library supported */
+/* #undef HAVE_PAKCHOIS */
 
 /* Define to 1 if you have the `pipe' function. */
 #define HAVE_PIPE 1
@@ -178,8 +233,11 @@
 #define snprintf _snprintf
 #endif
 
-/* Define to 1 if you have the <socks.h> header file. */
-/* #undef HAVE_SOCKS_H */
+/* Define to 1 if the system has the type `socklen_t'. */
+/* #undef HAVE_SOCKLEN_T */
+
+/* Define to 1 if you have the `SSL_SESSION_cmp' function. */
+/* #undef HAVE_SSL_SESSION_CMP */
 
 /* Define to 1 if you have the <stdarg.h> header file. */
 #define HAVE_STDARG_H 1
@@ -293,6 +351,10 @@
 /* Define to be location of localedir */
 /* #undef LOCALEDIR */
 
+/* Define to the sub-directory in which libtool stores uninstalled libraries.
+   */
+/* #undef LT_OBJDIR */
+
 /* Defined when neon is built as a library */
 #define NEON_IS_LIBRARY 1
 
@@ -301,6 +363,9 @@
 
 /* Define to enable debugging */
 /* #undef NE_DEBUGGING */
+
+/* Define to be printf format string for ne_off_t */
+/* #undef NE_FMT_NE_OFF_T */
 
 /* Define to be printf format string for off64_t */
 #define NE_FMT_OFF64_T "lld"
@@ -317,11 +382,14 @@
 /* Define to be printf format string for time_t */
 #define NE_FMT_TIME_T "ld"
 
+/* Define to be printf format string for XML_Size */
+/* #undef NE_FMT_XML_SIZE */
+
 /* Defined if DAV is supported */
 #define NE_HAVE_DAV 1
 
 /* Defined if I18N is supported */
-/* #undef NE_HAVE_I18N 1 */
+/* #undef NE_HAVE_I18N */
 
 /* Defined if IPV6 is supported */
 #define NE_HAVE_IPV6 1
@@ -329,8 +397,8 @@
 /* Defined if LFS is supported */
 #define NE_HAVE_LFS 1
 
-/* Defined if SOCKS is supported */
-/* #undef NE_HAVE_SOCKS */
+/* Defined if LIBPXY is supported */
+/* #undef NE_HAVE_LIBPXY */
 
 /* Defined if SSL is supported */
 #define NE_HAVE_SSL
@@ -373,29 +441,32 @@
 /* Define to the version of this package. */
 #define PACKAGE_VERSION "0.29.5"
 
-/* The size of a `int', as computed by sizeof. */
+/* The size of `int', as computed by sizeof. */
 #define SIZEOF_INT 4
 
-/* The size of a `long', as computed by sizeof. */
+/* The size of `long', as computed by sizeof. */
 #define SIZEOF_LONG 4
 
-/* The size of a `long long', as computed by sizeof. */
+/* The size of `long long', as computed by sizeof. */
 #define SIZEOF_LONG_LONG 8
 
-/* The size of a `off64_t', as computed by sizeof. */
+/* The size of `off64_t', as computed by sizeof. */
 #define SIZEOF_OFF64_T 8
 
-/* The size of a `off_t', as computed by sizeof. */
+/* The size of `off_t', as computed by sizeof. */
 #define SIZEOF_OFF_T 4
 
-/* The size of a `size_t', as computed by sizeof. */
+/* The size of `size_t', as computed by sizeof. */
 #define SIZEOF_SIZE_T 4
 
-/* The size of a `ssize_t', as computed by sizeof. */
+/* The size of `ssize_t', as computed by sizeof. */
 #define SIZEOF_SSIZE_T 4
 
-/* The size of a `time_t', as computed by sizeof. */
+/* The size of `time_t', as computed by sizeof. */
 #define SIZEOF_TIME_T 4
+
+/* The size of `XML_Size', as computed by sizeof. */
+/* #undef SIZEOF_XML_SIZE */
 
 /* Define to 1 if you have the ANSI C header files. */
 #ifdef SOLARIS
@@ -416,14 +487,46 @@
 /* Define if getaddrinfo() should be used */
 #define USE_GETADDRINFO 1
 
-/* Define to 1 if your processor stores words with the most significant byte
-   first (like Motorola and SPARC, unlike Intel and VAX). */
+/* Enable extensions on AIX 3, Interix.  */
+#ifndef _ALL_SOURCE
+# undef _ALL_SOURCE
+#endif
+/* Enable GNU extensions on systems that have them.  */
+#ifndef _GNU_SOURCE
+# undef _GNU_SOURCE
+#endif
+/* Enable threading extensions on Solaris.  */
+#ifndef _POSIX_PTHREAD_SEMANTICS
+# undef _POSIX_PTHREAD_SEMANTICS
+#endif
+/* Enable extensions on HP NonStop.  */
+#ifndef _TANDEM_SOURCE
+# undef _TANDEM_SOURCE
+#endif
+/* Enable general extensions on Solaris.  */
+#ifndef __EXTENSIONS__
+# undef __EXTENSIONS__
+#endif
+
+
+/* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
+   significant byte first (like Motorola and SPARC, unlike Intel). */
 #if defined (SOLARIS) && defined (SPARC)
 #define WORDS_BIGENDIAN 1
 #endif
 
 /* Always defined to enable GNU extensions */
 #define _GNU_SOURCE 1
+
+/* Define to 1 if on MINIX. */
+/* #undef _MINIX */
+
+/* Define to 2 if the system does not provide POSIX.1 features except with
+   this defined. */
+/* #undef _POSIX_1_SOURCE */
+
+/* Define to 1 if you need to in order for `stat' and other things to work. */
+/* #undef _POSIX_SOURCE */
 
 /* Define to empty if `const' does not conform to ANSI C. */
 /* #undef const */
@@ -439,19 +542,23 @@
 #endif
 #endif
 
-/* Define to `long' if <sys/types.h> does not define. */
+/* Define to `long int' if <sys/types.h> does not define. */
 /* #undef off_t */
 
 /* Define to `int' if <sys/types.h> does not define. */
 /* #undef pid_t */
 
-/* Define to `unsigned' if <sys/types.h> does not define. */
+/* Define to `unsigned int' if <sys/types.h> does not define. */
 /* #undef size_t */
+
+/* Define if socklen_t is not available */
+/* #undef socklen_t */
 
 /* MS IIS does not send Lock-Token response header after creation of a */
 /* new lock. This violates RFC, but... if we want to talk with this   */
 /* beast we need a workaround */
 #define IIS_LOCK_BUG_WORKAROUND 1
+
 
 /* Enable leak-tracking versions of ne_*alloc when NEON_MEMLEAK is enabled */
 #ifdef NEON_MEMLEAK
@@ -476,10 +583,6 @@ char *stpcpy(char *, const char *);
 #define NONLS
 #define NOCRYPT
 
-#endif
-
-#ifdef WIN32
 #define HAVE_SSPI 1
-#else
-#define HAVE_NTLM 1
+
 #endif
