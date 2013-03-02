LibreOffice/core

commit 2815396a1813cb3956c5aba066de49a7f34bc657

_SetGetExpFlds: this looks simpler with upper_bound
Change-Id: I37dd291aaa229493141fbb8b426488e8e4427185

sw/source/core/doc/docfld.cxx
@@ -1106,12 +1106,10 @@ void SwDoc::FldsToCalc( SwCalc& rCalc, const _SetGetExpFld& rToThisFld )
 
     if( !pUpdtFlds->GetSortLst()->empty() )
     {
-        _SetGetExpFlds::const_iterator itLast = pUpdtFlds->GetSortLst()->lower_bound( (_SetGetExpFld*)&rToThisFld );
-        if (pUpdtFlds->GetSortLst()->end() != itLast && **itLast == rToThisFld)
-        {
-            ++itLast;
-        }
-
+        _SetGetExpFlds::const_iterator const itLast = std::upper_bound(
+                pUpdtFlds->GetSortLst()->begin(),
+                pUpdtFlds->GetSortLst()->end(),
+                const_cast<_SetGetExpFld*>(&rToThisFld));
         for( _SetGetExpFlds::const_iterator it = pUpdtFlds->GetSortLst()->begin(); it != itLast; ++it )
             lcl_CalcFld( *this, rCalc, **it, pMgr );
     }
@@ -1154,14 +1152,10 @@ void SwDoc::FldsToExpand( SwHash**& ppHashTbl, sal_uInt16& rTblSize,
     ppHashTbl = new SwHash*[ rTblSize ];
     memset( ppHashTbl, 0, sizeof( _HashStr* ) * rTblSize );
 
-    _SetGetExpFlds::const_iterator itLast;
-    {
-        itLast = pUpdtFlds->GetSortLst()->lower_bound( (_SetGetExpFld*)&rToThisFld );
-        if (pUpdtFlds->GetSortLst()->end() != itLast && **itLast == rToThisFld)
-        {
-            ++itLast;
-        }
-    }
+    _SetGetExpFlds::const_iterator const itLast = std::upper_bound(
+        pUpdtFlds->GetSortLst()->begin(),
+        pUpdtFlds->GetSortLst()->end(),
+        const_cast<_SetGetExpFld*>(&rToThisFld));
 
     for( _SetGetExpFlds::const_iterator it = pUpdtFlds->GetSortLst()->begin(); it != itLast; ++it )
     {

commit 9ca5383abfb7f2f785e9f2e2d5b2565b9a60bbab

fdo#60886: _SetGetExpFlds: use the custom operator<
The calls to upper_bound in SwDoc::FldsToExpand and SwDoc::FldsToCalc
unfortunately compare the pointers and not the values pointed to,
which causes all sorts of trouble around non-updated fields.
(regression from 2815396a1813cb3956c5aba066de49a7f34bc657)

Change-Id: I48817247719671a16821fcb851b17b628c960a66

o3tl/inc/o3tl/sorted_vector.hxx
@@ -122,6 +122,11 @@ public:
         return std::lower_bound( base_t::begin(), base_t::end(), x, Compare() );
     }
 
+    const_iterator upper_bound( const Value& x ) const
+    {
+        return std::upper_bound( base_t::begin(), base_t::end(), x, Compare() );
+    }
+
     /* Searches the container for an element with a value of x
      * and returns an iterator to it if found, otherwise it returns an
      * iterator to sorted_vector::end (the element past the end of the container).

sw/source/core/doc/docfld.cxx
@@ -1097,9 +1097,8 @@ void SwDoc::FldsToCalc( SwCalc& rCalc, const _SetGetExpFld& rToThisFld )
 
     if( !mpUpdtFlds->GetSortLst()->empty() )
     {
-        _SetGetExpFlds::const_iterator const itLast = std::upper_bound(
-                mpUpdtFlds->GetSortLst()->begin(),
-                mpUpdtFlds->GetSortLst()->end(),
+        _SetGetExpFlds::const_iterator const itLast =
+            mpUpdtFlds->GetSortLst()->upper_bound(
                 const_cast<_SetGetExpFld*>(&rToThisFld));
         for( _SetGetExpFlds::const_iterator it = mpUpdtFlds->GetSortLst()->begin(); it != itLast; ++it )
             lcl_CalcFld( *this, rCalc, **it, pMgr );
@@ -1143,10 +1142,9 @@ void SwDoc::FldsToExpand( SwHash**& ppHashTbl, sal_uInt16& rTblSize,
     ppHashTbl = new SwHash*[ rTblSize ];
     memset( ppHashTbl, 0, sizeof( _HashStr* ) * rTblSize );
 
-    _SetGetExpFlds::const_iterator const itLast = std::upper_bound(
-        mpUpdtFlds->GetSortLst()->begin(),
-        mpUpdtFlds->GetSortLst()->end(),
-        const_cast<_SetGetExpFld*>(&rToThisFld));
+    _SetGetExpFlds::const_iterator const itLast =
+        mpUpdtFlds->GetSortLst()->upper_bound(
+            const_cast<_SetGetExpFld*>(&rToThisFld));
 
     for( _SetGetExpFlds::const_iterator it = mpUpdtFlds->GetSortLst()->begin(); it != itLast; ++it )
     {