2ee307d2300f5d5b3d56dec579b1c6fda97e60d4
David Tardon <dtardon@redhat.com>
2013-03-03 08:18:28 +0100

    do not copy files around needlessly
    Change-Id: I681500bb01e13155b355c8a45b41f73e8b9faf26

--- smoketest/Zip_smoketestdoc.mk
+++ smoketest/Zip_smoketestdoc.mk
@@ -27,34 +27,30 @@
 
 SMOKETESTDOC_DIR:=$(SRCDIR)/smoketest/data
 
-$(eval $(call gb_Zip_Zip,smoketestdoc,$(WORKDIR)/Zip/smoketestdoc))
+$(eval $(call gb_Zip_Zip,smoketestdoc,$(SRCDIR)/smoketest/data))
 
 $(eval $(call gb_Zip_add_commandoptions,smoketestdoc,-D))
 
 $(eval $(call gb_Zip_add_file,smoketestdoc,mimetype))
 
-$(eval $(call gb_Zip_add_file,smoketestdoc,content.xml,$(SMOKETESTDOC_DIR)/content.xml))
-$(eval $(call gb_Zip_add_file,smoketestdoc,meta.xml,$(SMOKETESTDOC_DIR)/meta.xml))
-$(eval $(call gb_Zip_add_file,smoketestdoc,settings.xml,$(SMOKETESTDOC_DIR)/settings.xml))
-$(eval $(call gb_Zip_add_file,smoketestdoc,styles.xml,$(SMOKETESTDOC_DIR)/styles.xml))
+$(eval $(call gb_Zip_add_file,smoketestdoc,content.xml))
+$(eval $(call gb_Zip_add_file,smoketestdoc,meta.xml))
+$(eval $(call gb_Zip_add_file,smoketestdoc,settings.xml))
+$(eval $(call gb_Zip_add_file,smoketestdoc,styles.xml))
 
-$(eval $(call gb_Zip_add_file,smoketestdoc,META-INF/manifest.xml,$(SMOKETESTDOC_DIR)/manifest.xml))
+$(eval $(call gb_Zip_add_file,smoketestdoc,META-INF/manifest.xml))
 
-$(eval $(call gb_Zip_add_file,smoketestdoc,Basic/script-lc.xml,$(SMOKETESTDOC_DIR)/script-lc.xml))
-$(eval $(call gb_Zip_add_file,smoketestdoc,Basic/Standard/script-lb.xml,$(SMOKETESTDOC_DIR)/script-lb.xml))
-$(eval $(call gb_Zip_add_file,smoketestdoc,Basic/Standard/Events.xml,$(SMOKETESTDOC_DIR)/Events.xml))
-$(eval $(call gb_Zip_add_file,smoketestdoc,Basic/Standard/Global.xml,$(SMOKETESTDOC_DIR)/Global.xml))
-$(eval $(call gb_Zip_add_file,smoketestdoc,Basic/Standard/Test_10er.xml,$(SMOKETESTDOC_DIR)/Test_10er.xml))
-$(eval $(call gb_Zip_add_file,smoketestdoc,Basic/Standard/Test_DB.xml,$(SMOKETESTDOC_DIR)/Test_DB.xml))
-$(eval $(call gb_Zip_add_file,smoketestdoc,Basic/Standard/Test_Ext.xml,$(SMOKETESTDOC_DIR)/Test_Ext.xml))
+$(eval $(call gb_Zip_add_file,smoketestdoc,Basic/script-lc.xml))
+$(eval $(call gb_Zip_add_file,smoketestdoc,Basic/Standard/script-lb.xml))
+$(eval $(call gb_Zip_add_file,smoketestdoc,Basic/Standard/Events.xml))
+$(eval $(call gb_Zip_add_file,smoketestdoc,Basic/Standard/Global.xml))
+$(eval $(call gb_Zip_add_file,smoketestdoc,Basic/Standard/Test_10er.xml))
+$(eval $(call gb_Zip_add_file,smoketestdoc,Basic/Standard/Test_DB.xml))
+$(eval $(call gb_Zip_add_file,smoketestdoc,Basic/Standard/Test_Ext.xml))
 
-$(eval $(call gb_Zip_add_file,smoketestdoc,Dialogs/dialog-lc.xml,$(SMOKETESTDOC_DIR)/dialog-lc.xml))
-$(eval $(call gb_Zip_add_file,smoketestdoc,Dialogs/Standard/dialog-lb.xml,$(SMOKETESTDOC_DIR)/dialog-lb.xml))
-$(eval $(call gb_Zip_add_file,smoketestdoc,Dialogs/Standard/OptionsDlg.xml,$(SMOKETESTDOC_DIR)/OptionsDlg.xml))
-
-$(WORKDIR)/Zip/smoketestdoc/mimetype :
-	mkdir -p $(dir $@)
-	printf application/vnd.sun.xml.writer > $@
+$(eval $(call gb_Zip_add_file,smoketestdoc,Dialogs/dialog-lc.xml))
+$(eval $(call gb_Zip_add_file,smoketestdoc,Dialogs/Standard/dialog-lb.xml))
+$(eval $(call gb_Zip_add_file,smoketestdoc,Dialogs/Standard/OptionsDlg.xml))
 
 $(call gb_Zip_get_final_target,smoketestdoc) : $(OUTDIR)/bin/smoketestdoc.sxw
 $(call gb_Zip_get_clean_target,smoketestdoc) : Clean_smoketestdoc_sxw
