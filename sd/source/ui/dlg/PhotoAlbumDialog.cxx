df515ba1e9fcb7ea53c920ce112ec1c06ab7a589
Gergő Mocsi <gmocsi91@gmail.com>
2013-03-07 11:57:04 +0100

    fdo#35546, in progress
    Created the dialog window (called photoalbum.ui), and started to integrate it.
    
    Change-Id: I14b337187dc5d373fcd28489098fda813b5fd5f6

---
+++ sd/source/ui/dlg/PhotoAlbumDialog.cxx
@@ -0,0 +1,25 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+* This file is part of the LibreOffice project.
+*
+* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+#include "sdattr.hxx"
+#include "sdresid.hxx"
+#include "cusshow.hxx"
+
+#include "PhotoAlbumDialog.hxx"
+#include "PhotoAlbumDialog.hrc"
+
+using namespace ::sd;
+using namespace ::com::sun::star::uno;
+
+SdPhotoAlbumDialog::SdPhotoAlbumDialog(Window* pWindow)
+: ModalDialog(pWindow, "PhotoAlbumCreatorDialog", "sd/uiconfig/simpress/photoalbum/photoalbum.ui")
+{
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
