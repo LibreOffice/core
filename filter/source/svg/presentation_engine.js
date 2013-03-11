97e33446de29e217cd12746f6d0586ae6eb52fc2
Marco Cecchetti <mrcekets@gmail.com>
2013-03-11 18:47:28 +0100

    Update JessyInk license statement after upstream change.
    Upstream JessyInk added optional MPL license to their .js
    files, mirror that change here:
    
     http://bazaar.launchpad.net/~hannes-hochreiner/jessyink/trunk/revision/122/scripts/jessyInk.js#scripts/jessyInk.js
    
    Change-Id: I2ea6c8f9d5b55e2c84db477784586891e932239c

--- filter/source/svg/presentation_engine.js
+++ filter/source/svg/presentation_engine.js
@@ -21,24 +21,26 @@
 
 /*****  ******************************************************************
  *
- *   Copyright 2008, 2009 Hannes Hochreiner
+ * Copyright 2008-2013 Hannes Hochreiner
  *
- *   The JavaScript code included between the start note '@jessyinkstart'
- *   and the end note '@jessyinkend' is free software: you can
- *   redistribute it and/or modify it under the terms of the GNU
- *   General Public License (GNU GPL) as published by the Free Software
- *   Foundation, either version 3 of the License, or (at your option)
- *   any later version.  The code is distributed WITHOUT ANY WARRANTY;
- *   without even the implied warranty of MERCHANTABILITY or FITNESS
- *   FOR A PARTICULAR PURPOSE.  See the GNU GPL for more details.
+ * The JavaScript code included between the start note '@jessyinkstart'
+ * and the end note '@jessyinkend' is subject to the terms of the Mozilla
+ * Public License, v. 2.0. If a copy of the MPL was not distributed with
+ * this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- *   As additional permission under GNU GPL version 3 section 7, you
- *   may distribute non-source (e.g., minimized or compacted) forms of
- *   that code without the copy of the GNU GPL normally required by
- *   section 4, provided you include this license notice and a URL
- *   through which recipients can access the Corresponding Source.
+ * Alternatively, you can redistribute and/or that part of this file
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *************************************************************************/
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+*/
 
 /*****
  *  You can find the complete source code of the JessyInk project at:
