954611cdea0ae4b0dab4f241e580c4f61792dc8b
Matúš Kukan <matus.kukan@gmail.com>
2013-03-11 20:50:39 +0100

    basic: make the makefile a bit nicer
    Change-Id: Ib606c0a9c84b35f4ab4b10dd7dc4dd82e85fcb71

--- basic/CppunitTest_basic_vba.mk
+++ basic/CppunitTest_basic_vba.mk
@@ -57,16 +57,11 @@ $(eval $(call gb_CppunitTest_use_api,basic_vba,\
 
 $(eval $(call gb_CppunitTest_use_ure,basic_vba))
 
-ifeq ($(OS),WNT)
 $(eval $(call gb_CppunitTest_use_components,basic_vba,\
-    configmgr/source/configmgr \
-    i18npool/util/i18npool \
-    extensions/source/ole/oleautobridge \
+	configmgr/source/configmgr \
+	i18npool/util/i18npool \
+	$(if $(filter $(OS),WNT), \
+		extensions/source/ole/oleautobridge) \
 ))
-else
-$(eval $(call gb_CppunitTest_use_components,basic_vba,\
-    configmgr/source/configmgr \
-    i18npool/util/i18npool \
-))
-endif
+
 $(eval $(call gb_CppunitTest_use_configuration,basic_vba))
