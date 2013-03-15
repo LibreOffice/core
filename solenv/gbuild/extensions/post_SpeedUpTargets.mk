c7f09e4fd4932f60c2e233d676537b6e786a7cfd
Bjoern Michaelsen <bjoern.michaelsen@canonical.com>
2013-03-15 11:20:41 +0100

    introduce gb_Module_SKIPTARGETS
    - this speeds up the old SKIP_TESTS targets build-nocheck and
      dev-install-nocheck
    - this allows e.g. tinderboxes, which know that they just completed a
      build, to only read the files for the tests (and exclude the build
      itself)
    - since the dep file can be quite large this might help performance in
      such scenarios quite a bit
    - also moving this to extensions as it is merely a performance
      optimization
    
    Conflicts:
    	Makefile.in
    	solenv/gbuild/Module.mk
    
    Change-Id: I1265dbacdd9f3731fe755a1b997c2fa5ac1f7421
    Reviewed-on: https://gerrit.libreoffice.org/2742
    Reviewed-by: Matúš Kukan <matus.kukan@gmail.com>
    Reviewed-by: Björn Michaelsen <bjoern.michaelsen@canonical.com>
    Tested-by: Björn Michaelsen <bjoern.michaelsen@canonical.com>

--- solenv/gbuild/extensions/post_SpeedUpTargets.mk
+++ solenv/gbuild/extensions/post_SpeedUpTargets.mk
@@ -30,36 +30,53 @@ gb_Module_add_targets_for_build :=
 endif
 
 ifeq ($(gb_Side),build)
-gb_Module_add_check_target :=
-gb_Module_add_slowcheck_target :=
-gb_Module_add_subsequentcheck_target :=
+gb_Module_SKIPTARGETS := check slowcheck subsequentcheck
 endif
 
 ifneq ($(strip $(MAKECMDGOALS)),)
 # speed up depending on the target
-gb_SpeedUpTargets_LEVEL_3 := debugrun help translations
-gb_SpeedUpTargets_LEVEL_2 := showmodules $(gb_SpeedUpTargets_LEVEL_3)
+gb_SpeedUpTargets_LEVEL_4 := debugrun help translations
+gb_SpeedUpTargets_LEVEL_3 := showmodules $(gb_SpeedUpTargets_LEVEL_4)
+gb_SpeedUpTargets_LEVEL_2 := build $(gb_SpeedUpTargets_LEVEL_3)
 gb_SpeedUpTargets_LEVEL_1 := clean showdeliverables $(gb_SpeedUpTargets_LEVEL_2)
 
 ifeq (T,$(if $(filter-out $(gb_SpeedUpTargets_LEVEL_1),$(MAKECMDGOALS)),,T))
 gb_FULLDEPS :=
 
 ifeq (T,$(if $(filter-out $(gb_SpeedUpTargets_LEVEL_2),$(MAKECMDGOALS)),,T))
-gb_Module_add_target :=
-gb_Module_add_check_target :=
-gb_Module_add_slowcheck_target :=
-gb_Module_add_subsequentcheck_target :=
-
-# Turns of check in bridges/Module_bridges.mk:
-gb_STRIPPED_BUILD := $(true)
+gb_Module_SKIPTARGETS += check slowcheck subsequentcheck
 
 ifeq (T,$(if $(filter-out $(gb_SpeedUpTargets_LEVEL_3),$(MAKECMDGOALS)),,T))
-gb_Module_add_moduledir :=
+gb_Module_SKIPTARGETS += build
+
+ifeq (T,$(if $(filter-out $(gb_SpeedUpTargets_LEVEL_4),$(MAKECMDGOALS)),,T))
+gb_Module_SKIPTARGETS += module
 
 endif
 endif
 endif
+endif
 
 endif
 
+
+ifneq (,$(filter build,$(gb_Module_SKIPTARGETS)))
+gb_Module_add_target =
+endif
+
+ifneq (,$(filter check,$(gb_Module_SKIPTARGETS)))
+gb_Module_add_check_target =
+endif
+
+ifneq (,$(filter slowcheck,$(gb_Module_SKIPTARGETS)))
+gb_Module_add_slowcheck_target =
+endif
+
+ifneq (,$(filter subsequentcheck,$(gb_Module_SKIPTARGETS)))
+gb_Module_add_subsequentcheck_target =
+endif
+
+ifneq (,$(filter module,$(gb_Module_SKIPTARGETS)))
+gb_Module_add_moduledir =
+endif
 # vim:set shiftwidth=4 softtabstop=4 noexpandtab:
