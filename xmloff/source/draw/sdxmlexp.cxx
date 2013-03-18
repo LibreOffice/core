LibreOffice/core

commit fd226710fef768543a3ad0fe5081f79dc875dcf3

-Werror,-Wunused-private-field (Clang towards 3.2)
Change-Id: I4b877ab97259f7e031e34daf7ce93e1b59417a08

xmloff/source/draw/sdxmlexp.cxx
@@ -456,7 +456,7 @@ void SAL_CALL SdXMLExport::setSourceDocument( const Reference< lang::XComponent 
         // construct PropertySetMapper
         UniReference < XMLPropertySetMapper > xMapper = new XMLShapePropertySetMapper( aFactoryRef);
 
-        mpPropertySetMapper = new XMLShapeExportPropertyMapper( xMapper, (XMLTextListAutoStylePool*)&GetTextParagraphExport()->GetListAutoStylePool(), *this );
+        mpPropertySetMapper = new XMLShapeExportPropertyMapper( xMapper, *this );
         // set lock to avoid deletion
         mpPropertySetMapper->acquire();
 

xmloff/source/draw/shapeexport.cxx
@@ -1025,9 +1025,7 @@ SvXMLExportPropertyMapper* XMLShapeExport::CreateShapePropMapper(
     UniReference< XMLPropertyHandlerFactory > xFactory = new XMLSdPropHdlFactory( rExport.GetModel(), rExport );
     UniReference < XMLPropertySetMapper > xMapper = new XMLShapePropertySetMapper( xFactory );
     SvXMLExportPropertyMapper* pResult =
-        new XMLShapeExportPropertyMapper( xMapper,
-                                          (XMLTextListAutoStylePool*)&rExport.GetTextParagraphExport()->GetListAutoStylePool(),
-                                          rExport );
+        new XMLShapeExportPropertyMapper( xMapper, rExport );
     // chain text attributes
     return pResult;
 }
@@ -1275,7 +1273,7 @@ const rtl::Reference< XMLTableExport >& XMLShapeExport::GetShapeTableExport()
     {
         rtl::Reference< XMLPropertyHandlerFactory > xFactory( new XMLSdPropHdlFactory( mrExport.GetModel(), mrExport ) );
         UniReference < XMLPropertySetMapper > xMapper( new XMLShapePropertySetMapper( xFactory.get() ) );
-        rtl::Reference< SvXMLExportPropertyMapper > xPropertySetMapper( new XMLShapeExportPropertyMapper( xMapper, (XMLTextListAutoStylePool*)&mrExport.GetTextParagraphExport()->GetListAutoStylePool(), mrExport ) );
+        rtl::Reference< SvXMLExportPropertyMapper > xPropertySetMapper( new XMLShapeExportPropertyMapper( xMapper, mrExport ) );
         mxShapeTableExport = new XMLTableExport( mrExport, xPropertySetMapper, xFactory );
     }
 

xmloff/inc/XMLNumberStylesImport.hxx
@@ -34,8 +34,6 @@ class SdXMLNumberFormatImportContext : public SvXMLNumFormatContext
 private:
     friend class SdXMLNumberFormatMemberImportContext;
 
-    SdXMLImport& mrImport;
-
     sal_Bool    mbTimeStyle;
     sal_Bool    mbAutomatic;
     sal_uInt8   mnElements[16];

xmloff/inc/txtfldi.hxx
@@ -1077,7 +1077,6 @@ class XMLMacroFieldImportContext : public XMLTextFieldImportContext
 
     ::rtl::OUString sMacro; // macro for old documents (pre 638i)
 
-    sal_Bool bMacroOK;
     sal_Bool bDescriptionOK;
 
 public:
@@ -1122,11 +1121,9 @@ class XMLReferenceFieldImportContext : public XMLTextFieldImportContext
     sal_uInt16 nElementToken;
     sal_Int16 nSource;
     sal_Int16 nType;
-    sal_Int16 nSequenceNumber;
 
     sal_Bool bNameOK;
     sal_Bool bTypeOK;
-    sal_Bool bSeqNumberOK;
 
 public:
 
@@ -1402,7 +1399,6 @@ class XMLScriptImportContext : public XMLTextFieldImportContext
 
     sal_Bool bContentOK;
     sal_Bool bScriptTypeOK;
-    sal_Bool bUrlContent;
 
 public:
 

xmloff/inc/txtvfldi.hxx
@@ -160,7 +160,6 @@ private:
     sal_Bool bHintOK;                   /// sHint was set
     sal_Bool bDisplayOK;                /// sDisplayFormula/-None were set
 
-    sal_Bool bSetName;                  /// set sName with ???-property
     sal_Bool bSetFormula;               /// set Formula property
     sal_Bool bSetFormulaDefault;        /// use content as default for formula
     sal_Bool bSetDescription;           /// set sDescription with Hint-property
@@ -182,7 +181,6 @@ public:
         sal_uInt16 nPrfx,               /// namespace prefix
         const ::rtl::OUString& rLocalName,  /// element name w/o prefix
         // config variables for PrepareField behavior:
-        sal_Bool bName,                 /// set sName with ???-property
         sal_Bool bFormula,              /// set Formula property
         sal_Bool bFormulaDefault,       /// use content as default for formula
         sal_Bool bDescription,          /// set sDescription with Hint-property
@@ -310,7 +308,6 @@ public:
         // for finding appropriate field master (see EndElement())
         VarType eVarType,               /// variable type
         // config variables:
-        sal_Bool bName,                 /// see XMLTextFieldImportContext
         sal_Bool bFormula,              /// see XMLTextFieldImportContext
         sal_Bool bFormulaDefault,       /// see XMLTextFieldImportContext
         sal_Bool bDescription,          /// see XMLTextFieldImportContext
@@ -542,7 +539,6 @@ class XMLTableFormulaImportContext : public XMLTextFieldImportContext
     XMLValueImportHelper aValueHelper;
 
     ::rtl::OUString sFormula;
-    sal_Bool bFormulaOK;
 
     sal_Bool bIsShowFormula;
 

xmloff/inc/xmloff/ProgressBarHelper.hxx
@@ -36,7 +36,6 @@ class XMLOFF_DLLPUBLIC ProgressBarHelper
             sal_Int32                                                                       nRange;
             sal_Int32                                                                       nReference;
             sal_Int32                                                                       nValue;
-            double                                                                          fOldPercent;
             sal_Bool                                                                        bStrict;
             // #96469#; if the value goes over the Range the progressbar starts again
             sal_Bool                                                                        bRepeat;

xmloff/source/chart/ColorPropertySet.cxx
@@ -56,13 +56,11 @@ protected:
     virtual sal_Bool SAL_CALL hasPropertyByName( const OUString& Name )  throw (RuntimeException);
 
 private:
-    bool m_bIsFillColor;
     OUString m_aColorPropName;
     Property m_aColorProp;
 };
 
 lcl_ColorPropertySetInfo::lcl_ColorPropertySetInfo( bool bFillColor ) :
-        m_bIsFillColor( bFillColor ),
         // note: length of FillColor and LineColor is 9
         m_aColorPropName( (bFillColor ? "FillColor" : "LineColor"), 9, RTL_TEXTENCODING_ASCII_US ),
         m_aColorProp( m_aColorPropName, -1,

xmloff/source/chart/SchXMLAxisContext.cxx
@@ -87,12 +87,10 @@ static SvXMLEnumMapEntry aXMLAxisTypeMap[] =
 class SchXMLCategoriesContext : public SvXMLImportContext
 {
 private:
-    SchXMLImportHelper& m_rImportHelper;
     OUString& mrAddress;
 
 public:
-    SchXMLCategoriesContext( SchXMLImportHelper& rImpHelper,
-                                   SvXMLImport& rImport,
+    SchXMLCategoriesContext( SvXMLImport& rImport,
                                    sal_uInt16 nPrefix,
                                    const OUString& rLocalName,
                                    OUString& rAddress );
@@ -107,7 +105,7 @@ public:
 class DateScaleContext : public SvXMLImportContext
 {
 public:
-    DateScaleContext( SchXMLImportHelper& rImpHelper, SvXMLImport& rImport,
+    DateScaleContext( SvXMLImport& rImport,
                         sal_uInt16 nPrefix, const OUString& rLocalName,
                         const Reference< beans::XPropertySet > xAxisProps );
 
@@ -115,7 +113,6 @@ public:
     virtual void StartElement( const Reference< ::com::sun::star::xml::sax::XAttributeList >& xAttrList );
 
 private:
-    SchXMLImportHelper& m_rImportHelper;
     Reference< beans::XPropertySet > m_xAxisProps;
 };
 
@@ -673,7 +670,7 @@ SvXMLImportContext* SchXMLAxisContext::CreateChildContext(
         break;
 
         case XML_TOK_AXIS_CATEGORIES:
-            pContext = new SchXMLCategoriesContext( m_rImportHelper, GetImport(),
+            pContext = new SchXMLCategoriesContext( GetImport(),
                                                           p_nPrefix, rLocalName,
                                                           m_rCategoriesAddress );
             m_aCurrentAxis.bHasCategories = true;
@@ -681,7 +678,7 @@ SvXMLImportContext* SchXMLAxisContext::CreateChildContext(
 
         case XML_TOK_AXIS_DATE_SCALE:
         case XML_TOK_AXIS_DATE_SCALE_EXT:
-            pContext = new DateScaleContext( m_rImportHelper, GetImport(),
+            pContext = new DateScaleContext( GetImport(),
                             p_nPrefix, rLocalName, m_xAxisProps );
             m_bDateScaleImported = true;
             break;
@@ -883,13 +880,11 @@ void SchXMLAxisContext::CorrectAxisPositions( const Reference< chart2::XChartDoc
 // ========================================
 
 SchXMLCategoriesContext::SchXMLCategoriesContext(
-    SchXMLImportHelper& rImpHelper,
     SvXMLImport& rImport,
     sal_uInt16 nPrefix,
     const OUString& rLocalName,
     OUString& rAddress ) :
         SvXMLImportContext( rImport, nPrefix, rLocalName ),
-        m_rImportHelper( rImpHelper ),
         mrAddress( rAddress )
 {
 }
@@ -920,13 +915,11 @@ void SchXMLCategoriesContext::StartElement( const Reference< xml::sax::XAttribut
 // ========================================
 
 DateScaleContext::DateScaleContext(
-    SchXMLImportHelper& rImpHelper,
     SvXMLImport& rImport,
     sal_uInt16 nPrefix,
     const OUString& rLocalName,
     const Reference< beans::XPropertySet > xAxisProps ) :
         SvXMLImportContext( rImport, nPrefix, rLocalName ),
-        m_rImportHelper( rImpHelper ),
         m_xAxisProps( xAxisProps )
 {
 }

commit 64595f591cdeebc389deee37adbc68e14f9a6286

fdo#61256 - the Get.*Export methods also create and register styles
unwind problem introduced by cleanup in:
fd226710fef768543a3ad0fe5081f79dc875dcf3

xmloff/source/draw/sdxmlexp.cxx
@@ -448,6 +448,8 @@ void SAL_CALL SdXMLExport::setSourceDocument( const Reference< lang::XComponent 
         // construct PropertySetMapper
         UniReference < XMLPropertySetMapper > xMapper = new XMLShapePropertySetMapper( aFactoryRef);
 
+        // get or create text paragraph export
+        GetTextParagraphExport();
         mpPropertySetMapper = new XMLShapeExportPropertyMapper( xMapper, *this );
         // set lock to avoid deletion
         mpPropertySetMapper->acquire();

xmloff/source/draw/shapeexport.cxx
@@ -1014,6 +1014,7 @@ SvXMLExportPropertyMapper* XMLShapeExport::CreateShapePropMapper(
 {
     UniReference< XMLPropertyHandlerFactory > xFactory = new XMLSdPropHdlFactory( rExport.GetModel(), rExport );
     UniReference < XMLPropertySetMapper > xMapper = new XMLShapePropertySetMapper( xFactory );
+    rExport.GetTextParagraphExport(); // get or create text paragraph export
     SvXMLExportPropertyMapper* pResult =
         new XMLShapeExportPropertyMapper( xMapper, rExport );
     // chain text attributes
@@ -1257,6 +1258,7 @@ const rtl::Reference< XMLTableExport >& XMLShapeExport::GetShapeTableExport()
     {
         rtl::Reference< XMLPropertyHandlerFactory > xFactory( new XMLSdPropHdlFactory( mrExport.GetModel(), mrExport ) );
         UniReference < XMLPropertySetMapper > xMapper( new XMLShapePropertySetMapper( xFactory.get() ) );
+        mrExport.GetTextParagraphExport(); // get or create text paragraph export
         rtl::Reference< SvXMLExportPropertyMapper > xPropertySetMapper( new XMLShapeExportPropertyMapper( xMapper, mrExport ) );
         mxShapeTableExport = new XMLTableExport( mrExport, xPropertySetMapper, xFactory );
     }