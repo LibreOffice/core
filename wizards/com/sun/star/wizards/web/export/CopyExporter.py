d357efdc302e4699d188a7b5dcb0c7975c0ad13a
Javier Fernandez <jfernandez@igalia.com>
2013-03-19 19:02:17 +0000

    Init: Added new Exporter classes.
    Change-Id: I45a0c3a4665581dcd0709d1ae72fbbbf404d7182

---
+++ wizards/com/sun/star/wizards/web/export/CopyExporter.py
@@ -0,0 +1,52 @@
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# This file incorporates work covered by the following license notice:
+#
+#   Licensed to the Apache Software Foundation (ASF) under one or more
+#   contributor license agreements. See the NOTICE file distributed
+#   with this work for additional information regarding copyright
+#   ownership. The ASF licenses this file to you under the Apache
+#   License, Version 2.0 (the "License"); you may not use this file
+#   except in compliance with the License. You may obtain a copy of
+#   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+#
+import traceback
+
+from .AbstractExporter import AbstractExporter
+from ...common.FileAccess import FileAccess
+
+class CopyExporter(AbstractExporter):
+
+    # @see com.sun.star.wizards.web.export.Exporter#export(java.lang.Object, java.io.File, com.sun.star.wizards.web.data.CGSettings, com.sun.star.lang.XMultiServiceFactory)
+    def export(self, source, target, xmsf, task):
+        try:
+            task.advance(True)
+
+            # this will open the document, and calculate the pages/slides number
+            # in it.
+            if (self.exporter.cp_PageType > 0):
+                self.closeDocument(self.openDocument(source, xmsf), xmsf)
+
+            task.advance(True)
+
+            print ("WARNING !!! CopyExporter (creating newtarget) - source.urlFilename: ", source.urlFilename)
+
+            newTarget = FileAccess.connectURLs(
+                    FileAccess.getParentDir(target), source.urlFilename)
+
+            print ("WARNING !!! CopyExporter (export) - source, target: ", source.cp_URL, newTarget)
+            b = self.getFileAccess(xmsf).copy(source.cp_URL, newTarget)
+
+            task.advance(True)
+
+            self.calcFileSize(source, newTarget, xmsf)
+
+            return b
+        except Exception:
+            traceback.print_exc()
+            return False
---
+++ wizards/com/sun/star/wizards/web/export/Exporter.py
@@ -0,0 +1,40 @@
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# This file incorporates work covered by the following license notice:
+#
+#   Licensed to the Apache Software Foundation (ASF) under one or more
+#   contributor license agreements. See the NOTICE file distributed
+#   with this work for additional information regarding copyright
+#   ownership. The ASF licenses this file to you under the Apache
+#   License, Version 2.0 (the "License"); you may not use this file
+#   except in compliance with the License. You may obtain a copy of
+#   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+#
+from ..abc import abstractmethod
+
+class Exporter:
+
+    # This method exports a document with a specified filter.<br/>
+    # The method is responsible for exporting the given source document
+    # to the given target directory.
+    # The exporter *must* also set the given CGDocument sizeBytes field to the
+    # size of the converted document, *if* the target document is
+    # of a binary format.
+    # always use source.urlFilename as destination filename.
+    # @param source is a CGDocument object.
+    # @param targetDirectory contains the URL of a directory to which the file should be exported to.
+    # @param xmsf this is a basic multiServiceFactory.
+    # @param task - a task monitoring object. This should advance
+    # (call task.advance(true) ) 3 times, while exporting.
+    @abstractmethod
+    def export(self, source, targetDirectory, xmsf, task):
+        pass
+
+    @abstractmethod
+    def init(self, exporter):
+        pass
