6ddd42768aab98332f80cc8713bd30becbcef2d5
Javier Fernandez <jfernandez@igalia.com>
2013-03-20 09:47:30 +0000

    Init: Relative paths and missing imports
    Change-Id: I2196567e0c9b90a9a56aa1db769f6efc9e56e74f

--- wizards/com/sun/star/wizards/web/data/CGGeneralInfo.py
+++ wizards/com/sun/star/wizards/web/data/CGGeneralInfo.py
@@ -15,7 +15,8 @@
 #   except in compliance with the License. You may obtain a copy of
 #   the License at http://www.apache.org/licenses/LICENSE-2.0 .
 #
-from common.ConfigGroup import ConfigGroup
+from ...common.ConfigGroup import ConfigGroup
+from ...common.XMLHelper import XMLHelper
 
 class CGGeneralInfo(ConfigGroup):
 
--- wizards/com/sun/star/wizards/web/data/CGImage.py
+++ wizards/com/sun/star/wizards/web/data/CGImage.py
@@ -15,7 +15,7 @@
 #   except in compliance with the License. You may obtain a copy of
 #   the License at http://www.apache.org/licenses/LICENSE-2.0 .
 #
-from common.ConfigGroup import ConfigGroup
+from ...common.ConfigGroup import ConfigGroup
 
 class CGImage(ConfigGroup):
     cp_Href = str()
