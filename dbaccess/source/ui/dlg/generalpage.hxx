5058fc52d11091a4b4d37b09a3d6a4ca11961cb9
Mathias Hasselmann <mathias@openismus.com>
2013-03-21 09:04:55 +0100

    Use UI builder for database wizard's start page
    Actually the resource based code maintaining two pretty different
    pages in one single class: The start page of the setup wizard and the
    start page for the change datasource type wizard. Both dialogs looked
    pretty different so to simplify the UI definition and to reduce number
    of null pointers and branches in the underlaying code I've split
    dbaui::OGeneralPage into two separate classes that share a few common
    pieces.
    
    Change-Id: I690e178992c528de112d91cecf00cc3c5156f1cf

--- dbaccess/source/ui/dlg/generalpage.hxx
+++ dbaccess/source/ui/dlg/generalpage.hxx
@@ -37,8 +37,94 @@ namespace dbaui
     //=========================================================================
     class OGeneralPage : public OGenericAdministrationPage
     {
-        OGeneralPage(Window* pParent, const SfxItemSet& _rItems, sal_Bool _bDBWizardMode = sal_False);
+    protected:
+        OGeneralPage( Window* pParent, const rtl::OUString& _rUIXMLDescription, const SfxItemSet& _rItems );
         ~OGeneralPage();
+
+    private:
+        FixedText*          m_pSpecialMessage;
+
+        OUString            m_eCurrentSelection;    /// currently selected type
+        ::dbaccess::DATASOURCE_TYPE
+                            m_eNotSupportedKnownType;   /// if a data source of an unsupported, but known type is encountered ....
+
+        enum SPECIAL_MESSAGE
+        {
+            smNone,
+            smUnsupportedType
+        };
+        SPECIAL_MESSAGE     m_eLastMessage;
+
+        Link                m_aTypeSelectHandler;   /// to be called if a new type is selected
+        sal_Bool            m_bDisplayingInvalid : 1;   // the currently displayed data source is deleted
+        bool                m_bInitTypeList : 1;
+        bool                approveDatasourceType( const OUString& _sURLPrefix, OUString& _inout_rDisplayName );
+        void                insertDatasourceTypeEntryData( const OUString& _sType, String sDisplayName );
+
+    protected:
+        ListBox*            m_pDatasourceType;
+
+        ::dbaccess::ODsnTypeCollection*
+                            m_pCollection;  /// the DSN type collection instance
+
+        ::std::vector< OUString>
+                            m_aURLPrefixes;
+
+    public:
+        /// set a handler which gets called every time the user selects a new type
+        void            SetTypeSelectHandler( const Link& _rHandler ) { m_aTypeSelectHandler = _rHandler; }
+
+        /// get the currently selected datasource type
+        OUString     GetSelectedType() const { return m_eCurrentSelection; }
+
+    protected:
+        // SfxTabPage overridables
+        virtual void Reset( const SfxItemSet& _rCoreAttrs );
+
+        virtual void implInitControls( const SfxItemSet& _rSet, sal_Bool _bSaveValue );
+        virtual OUString getDatasourceName( const SfxItemSet& _rSet );
+        virtual bool approveDatasourceType( ::dbaccess::DATASOURCE_TYPE eType, OUString& _inout_rDisplayName );
+
+        // <method>OGenericAdministrationPage::fillControls</method>
+        virtual void fillControls(::std::vector< ISaveValueWrapper* >& _rControlList);
+        // <method>OGenericAdministrationPage::fillWindows</method>
+        virtual void fillWindows(::std::vector< ISaveValueWrapper* >& _rControlList);
+
+        void onTypeSelected(const OUString& _sURLPrefix);
+        void initializeTypeList();
+
+        void implSetCurrentType( const OUString& _eType );
+
+        void switchMessage(const OUString& _sURLPrefix);
+
+        /// sets the title of the parent dialog
+        virtual void setParentTitle( const OUString& _sURLPrefix );
+
+        DECL_LINK(OnDatasourceTypeSelected, ListBox*);
+    };
+
+    //=========================================================================
+    //= OGeneralPageDialog
+    //=========================================================================
+    class OGeneralPageDialog : public OGeneralPage
+    {
+    public:
+        OGeneralPageDialog( Window* pParent, const SfxItemSet& _rItems );
+
+    protected:
+        virtual sal_Bool FillItemSet( SfxItemSet& _rCoreAttrs );
+
+        virtual void implInitControls( const SfxItemSet& _rSet, sal_Bool _bSaveValue );
+        virtual void setParentTitle( const OUString& _sURLPrefix );
+    };
+
+    //=========================================================================
+    //= OGeneralPageWizard
+    //=========================================================================
+    class OGeneralPageWizard : public OGeneralPage
+    {
+    public:
+        OGeneralPageWizard( Window* pParent, const SfxItemSet& _rItems );
 
     public:
         enum CreationMode
@@ -56,99 +142,49 @@ namespace dbaui
 
     private:
         // dialog controls
-        FixedText           m_aFTHeaderText;
-        FixedText           m_aFTHelpText;
-        FixedText           m_aFT_DatasourceTypeHeader;
-        RadioButton         m_aRB_CreateDatabase;
-        RadioButton         m_aRB_OpenDocument;
-        RadioButton         m_aRB_GetExistingDatabase;
-        FixedText           m_aFT_DocListLabel;
-        ::std::auto_ptr< OpenDocumentListBox >
-                            m_pLB_DocumentList;
-        OpenDocumentButton  m_aPB_OpenDocument;
-        FixedText           m_aTypePreLabel;
-        FixedText           m_aDatasourceTypeLabel;
-        ::std::auto_ptr< ListBox >
-                            m_pDatasourceType;
-        FixedText           m_aFTDataSourceAppendix;
-        FixedText           m_aTypePostLabel;
-        FixedText           m_aSpecialMessage;
-        sal_Bool            m_DBWizardMode;
-        String              m_sMySQLEntry;
-        CreationMode        m_eOriginalCreationMode;
-        DocumentDescriptor  m_aBrowsedDocument;
+        FixedText*              m_pFT_HeaderText;
+        FixedText*              m_pFT_HelpText;
+
+        RadioButton*            m_pRB_CreateDatabase;
+        RadioButton*            m_pRB_OpenExistingDatabase;
+        RadioButton*            m_pRB_ConnectDatabase;
+
+        FixedText*              m_pFT_DocListLabel;
+        OpenDocumentListBox*    m_pLB_DocumentList;
+        OpenDocumentButton*     m_pPB_OpenDatabase;
+
+        // state
+        DocumentDescriptor      m_aBrowsedDocument;
+        CreationMode            m_eOriginalCreationMode;
+
+        Link                    m_aCreationModeHandler;         /// to be called if a new type is selected
+        Link                    m_aDocumentSelectionHandler;    /// to be called when a document in the RecentDoc list is selected
+        Link                    m_aChooseDocumentHandler;       /// to be called when a recent document has been definitely chosen
 
         ::svt::ControlDependencyManager
-                            m_aControlDependencies;
-        ::std::vector< OUString> m_aURLPrefixes;
-
-
-        ::dbaccess::ODsnTypeCollection*
-                            m_pCollection;  /// the DSN type collection instance
-        OUString     m_eCurrentSelection;    /// currently selected type
-        ::dbaccess::DATASOURCE_TYPE     m_eNotSupportedKnownType;   /// if a data source of an unsupported, but known type is encountered ....
-
-        enum SPECIAL_MESSAGE
-        {
-            smNone,
-            smUnsupportedType
-        };
-        SPECIAL_MESSAGE     m_eLastMessage;
-
-        Link                m_aTypeSelectHandler;   /// to be called if a new type is selected
-        Link                m_aCreationModeHandler; /// to be called if a new type is selected
-        Link                m_aDocumentSelectionHandler;    /// to be called when a document in the RecentDoc list is selected
-        Link                m_aChooseDocumentHandler;       /// to be called when a recent document has been definitely chosen
-        sal_Bool            m_bDisplayingInvalid : 1;   // the currently displayed data source is deleted
-        bool                m_bInitTypeList : 1;
-        bool                approveDataSourceType( const OUString& _sURLPrefix, OUString& _inout_rDisplayName );
-        void                insertDatasourceTypeEntryData(const OUString& _sType, String sDisplayName);
+                                m_aControlDependencies;
 
     public:
-        static SfxTabPage*  Create(Window* pParent, const SfxItemSet& _rAttrSet, sal_Bool _bDBWizardMode = sal_False);
+        void                    SetCreationModeHandler( const Link& _rHandler ) { m_aCreationModeHandler = _rHandler; }
+        CreationMode            GetDatabaseCreationMode() const;
 
-        /// set a handler which gets called every time the user selects a new type
-        void            SetTypeSelectHandler(const Link& _rHandler) { m_aTypeSelectHandler = _rHandler; }
-        void            SetCreationModeHandler(const Link& _rHandler) { m_aCreationModeHandler = _rHandler; }
-        void            SetDocumentSelectionHandler( const Link& _rHandler) { m_aDocumentSelectionHandler = _rHandler; }
-        void            SetChooseDocumentHandler( const Link& _rHandler) { m_aChooseDocumentHandler = _rHandler; }
-        CreationMode    GetDatabaseCreationMode() const;
-
-        DocumentDescriptor  GetSelectedDocument() const;
-
-        /// get the currently selected datasource type
-        OUString     GetSelectedType() const { return m_eCurrentSelection; }
+        void                    SetDocumentSelectionHandler( const Link& _rHandler) { m_aDocumentSelectionHandler = _rHandler; }
+        void                    SetChooseDocumentHandler( const Link& _rHandler) { m_aChooseDocumentHandler = _rHandler; }
+        DocumentDescriptor      GetSelectedDocument() const;
 
     protected:
-        // SfxTabPage overridables
-        virtual sal_Bool FillItemSet(SfxItemSet& _rCoreAttrs);
-        virtual void Reset(const SfxItemSet& _rCoreAttrs);
-
-        virtual void implInitControls(const SfxItemSet& _rSet, sal_Bool _bSaveValue);
+        virtual sal_Bool FillItemSet( SfxItemSet& _rCoreAttrs );
 
         virtual void GetFocus();
 
-        // <method>OGenericAdministrationPage::fillControls</method>
-        virtual void fillControls(::std::vector< ISaveValueWrapper* >& _rControlList);
-        // <method>OGenericAdministrationPage::fillWindows</method>
-        virtual void fillWindows(::std::vector< ISaveValueWrapper* >& _rControlList);
+        virtual void implInitControls( const SfxItemSet& _rSet, sal_Bool _bSaveValue );
+        virtual OUString getDatasourceName( const SfxItemSet& _rSet );
+        virtual bool approveDatasourceType( ::dbaccess::DATASOURCE_TYPE eType, OUString& _inout_rDisplayName );
 
     protected:
-
-        void onTypeSelected(const OUString& _sURLPrefix);
-        void initializeTypeList();
-
-        void implSetCurrentType( const OUString& _eType );
-
-        void switchMessage(const OUString& _sURLPrefix);
-
-        /// sets the title of the parent dialog
-        void setParentTitle(const OUString& _sURLPrefix);
-
-        DECL_LINK(OnDatasourceTypeSelected, ListBox*);
-        DECL_LINK(OnSetupModeSelected, RadioButton*);
-        DECL_LINK(OnDocumentSelected, ListBox*);
-        DECL_LINK(OnOpenDocument, PushButton*);
+        DECL_LINK( OnSetupModeSelected, RadioButton* );
+        DECL_LINK( OnDocumentSelected, ListBox* );
+        DECL_LINK( OnOpenDocument, PushButton* );
     };
 
 //.........................................................................
