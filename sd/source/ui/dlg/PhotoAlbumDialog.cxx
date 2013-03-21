5a1e2be29d886e821e5727ba185c40be1d24fc12
Stephan Bergmann <sbergman@redhat.com>
2013-03-21 12:58:24 +0100

    Short-term fix around the SD_DLLIMPLEMTATION debacle
    (being defined for three different sd/Library_sd*.mk files), just like it is
    done in all the other sd/source/ui/dlg/*.cxx files already.
    
    Change-Id: I1b9e886c12acb1bfd73bea52f40260760a5f46ff

--- sd/source/ui/dlg/PhotoAlbumDialog.cxx
+++ sd/source/ui/dlg/PhotoAlbumDialog.cxx
@@ -6,6 +6,11 @@
 * License, v. 2.0. If a copy of the MPL was not distributed with this
 * file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
+
+#ifdef SD_DLLIMPLEMENTATION
+#undef SD_DLLIMPLEMENTATION
+#endif
+
 #include "PhotoAlbumDialog.hxx"
 
 #include <com/sun/star/graphic/GraphicProvider.hpp>
