LibreOffice/core

commit 4a507f732d82c188ad81b022cbe3037951e58ac3

fdo#57678 fix import of RTF_PARD between RTF_CELL and RTF_ROW

writerfilter/source/rtftok/rtfdocumentimpl.cxx
@@ -2078,11 +2078,15 @@ int RTFDocumentImpl::dispatchFlag(RTFKeyword nKeyword)
         case RTF_PARD:
             if (m_bHadPicture)
                 dispatchSymbol(RTF_PAR);
-            m_aStates.top().aParagraphSprms = m_aDefaultState.aParagraphSprms;
-            m_aStates.top().aParagraphAttributes = m_aDefaultState.aParagraphAttributes;
+            // \pard is allowed between \cell and \row, but in that case it should not reset the fact that we're inside a table.
+            if (m_aStates.top().nCells == 0)
+            {
+                m_aStates.top().aParagraphSprms = m_aDefaultState.aParagraphSprms;
+                m_aStates.top().aParagraphAttributes = m_aDefaultState.aParagraphAttributes;
+                if (m_aStates.top().nDestinationState != DESTINATION_SHAPETEXT)
+                    m_pCurrentBuffer = 0;
+            }
             m_aStates.top().resetFrame();
-            if (m_aStates.top().nDestinationState != DESTINATION_SHAPETEXT)
-                m_pCurrentBuffer = 0;
             break;
         case RTF_SECTD:
             m_aStates.top().aSectionSprms = m_aDefaultState.aSectionSprms;

sw/qa/extras/rtfimport/rtfimport.cxx
@@ -131,6 +131,7 @@ public:
     void testFdo49934();
     void testFdo57886();
     void testFdo58076();
+    void testFdo57678();
 
     CPPUNIT_TEST_SUITE(Test);
 #if !defined(MACOSX) && !defined(WNT)
@@ -212,6 +213,7 @@ void Test::run()
         {"fdo49934.rtf", &Test::testFdo49934},
         {"fdo57886.rtf", &Test::testFdo57886},
         {"fdo58076.rtf", &Test::testFdo58076},
+        {"fdo57678.rtf", &Test::testFdo57678},
     };
     header();
     for (unsigned int i = 0; i < SAL_N_ELEMENTS(aMethods); ++i)
@@ -978,6 +980,14 @@ void Test::testFdo58076()
     CPPUNIT_ASSERT_EQUAL(sal_Int32(1752), getProperty<sal_Int32>(xStyle, "RightMargin"));
     CPPUNIT_ASSERT_EQUAL(sal_Int32(635), getProperty<sal_Int32>(xStyle, "TopMargin"));
     CPPUNIT_ASSERT_EQUAL(sal_Int32(635), getProperty<sal_Int32>(xStyle, "BottomMargin"));
+}
+
+void Test::testFdo57678()
+{
+    // Paragraphs of the two tables were not converted to tables.
+    uno::Reference<text::XTextTablesSupplier> xTextTablesSupplier(mxComponent, uno::UNO_QUERY);
+    uno::Reference<container::XIndexAccess> xIndexAccess(xTextTablesSupplier->getTextTables(), uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(2), xIndexAccess->getCount());
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(Test);

sw/qa/extras/rtfimport/data/fdo57678.rtf
@@ -1,0 +1,34 @@
+{\rtf\ansi\deff0
+{\fonttbl{\f0 Times New Roman;}}
+{\colortbl;\red0\green0\blue0;}
+{\info
+}
+\paperw11907 \paperh16443 \deftab1298 \margl1701 \margr1701 \margt567 \margb1134 \pgnstart1\ftnnar \aftnnrlc \ftnstart1 \aftnstart1 
+\trowd\trowd\cellx1701\cellx3402
+
+\intbl\itap1
+\qc{Cell 1:1}
+\cell\pard
+
+\intbl\itap1
+\qc{Cell 1:2}
+\cell\pard
+
+\row
+\pard\itap0
+\par \trowd\trowd \cellx1701\cellx3402\cellx5103\cellx6804
+
+\intbl\itap1
+{cell 1:1}
+\cell\pard
+
+{{\~}}\cell\pard
+
+{{\~}}\cell\pard
+
+{{\~}}\cell\pard
+
+\row
+\pard\itap0
+
+}

commit 24d5261f5a122e22675210445056cdf67663237b

fdo#62288 fix RTF import of table paragraph margins
Regression from 4a507f732d82c188ad81b022cbe3037951e58ac3. The problem
was that in some cases \pard can't reset all paragraph properties. The
original commit just made this keyword a noop when it occurred between
\cell and \row, but this is too much. At least margins do need
resetting.

Change-Id: I5cbb1df72bf1211f85ef69ab64d5b46cbce5c742

writerfilter/source/rtftok/rtfdocumentimpl.cxx
@@ -2173,10 +2173,19 @@ int RTFDocumentImpl::dispatchFlag(RTFKeyword nKeyword)
             // \pard is allowed between \cell and \row, but in that case it should not reset the fact that we're inside a table.
             if (m_aStates.top().nCells == 0)
             {
+                // Reset everything.
                 m_aStates.top().aParagraphSprms = m_aDefaultState.aParagraphSprms;
                 m_aStates.top().aParagraphAttributes = m_aDefaultState.aParagraphAttributes;
                 if (m_aStates.top().nDestinationState != DESTINATION_SHAPETEXT)
                     m_pCurrentBuffer = 0;
+            }
+            else
+            {
+                // Reset only margins.
+                lcl_eraseNestedAttribute(m_aStates.top().aParagraphSprms, NS_ooxml::LN_CT_PPrBase_spacing, NS_ooxml::LN_CT_Spacing_before);
+                lcl_eraseNestedAttribute(m_aStates.top().aParagraphSprms, NS_ooxml::LN_CT_PPrBase_spacing, NS_ooxml::LN_CT_Spacing_after);
+                m_aStates.top().aParagraphSprms.erase(NS_sprm::LN_PDxaLeft);
+                m_aStates.top().aParagraphSprms.erase(NS_sprm::LN_PDxaRight);
             }
             m_aStates.top().resetFrame();
             break;

sw/qa/extras/rtfimport/data/fdo62288.rtf
@@ -1,0 +1,23 @@
+{\rtf1
+\paperw11907\paperh16840\margl567\margr567\margt567\margb567 
+\sb113\sa113
+{\b\fs22\cf1\kerning1\cgrid0 Objectives}
+{\fs24\kerning1\cgrid0 
+\par }
+\trowd \trgaph10\trleft-10 \clvertalt\cltxlrtb \cellx4808\clvertalt\cltxlrtb \cellx5375\clvertalt\cltxlrtb \cellx10194\clvertalt\cltxlrtb \cellx10762\pard \li567\nowidctlpar\intbl\adjustright 
+{\cf1\kerning1\cgrid0 One}
+{\fs24\kerning1\cgrid0 \cell }
+\pard \qc\nowidctlpar\intbl\adjustright 
+{\cf1\kerning1\cgrid0 [ ]}
+{\fs24\kerning1\cgrid0 \cell }
+\pard \li567\nowidctlpar\intbl\adjustright 
+{\cf1\kerning1\cgrid0 Two}
+{
+\fs24\kerning1\cgrid0 \cell }
+\pard \qc\nowidctlpar\intbl\adjustright 
+{\cf1\kerning1\cgrid0 [ ]}
+{\fs24\kerning1\cgrid0 \cell }
+\pard \widctlpar\intbl\adjustright 
+{\fs24\kerning1\cgrid0 \row }
+\pard\par
+}

sw/qa/extras/rtfimport/rtfimport.cxx
@@ -146,6 +146,7 @@ public:
     void testFdo59638();
     void testFdo60722();
     void testFdo61909();
+    void testFdo62288();
 
     CPPUNIT_TEST_SUITE(Test);
 #if !defined(MACOSX) && !defined(WNT)
@@ -264,6 +265,7 @@ void Test::run()
         {"fdo59638.rtf", &Test::testFdo59638},
         {"fdo60722.rtf", &Test::testFdo60722},
         {"fdo61909.rtf", &Test::testFdo61909},
+        {"fdo62288.rtf", &Test::testFdo62288},
     };
     header();
     for (unsigned int i = 0; i < SAL_N_ELEMENTS(aMethods); ++i)
@@ -1175,6 +1177,19 @@ void Test::testFdo61909()
     CPPUNIT_ASSERT_EQUAL(OUString("Courier New"), getProperty<OUString>(xTextRange, "CharFontName"));
     // Was 0x008000.
     CPPUNIT_ASSERT_EQUAL(COL_AUTO, getProperty<sal_uInt32>(xTextRange, "CharBackColor"));
+}
+
+void Test::testFdo62288()
+{
+    uno::Reference<text::XTextTablesSupplier> xTextTablesSupplier(mxComponent, uno::UNO_QUERY);
+    uno::Reference<container::XIndexAccess> xTables(xTextTablesSupplier->getTextTables(), uno::UNO_QUERY);
+    uno::Reference<text::XTextTable> xTable(xTables->getByIndex(0), uno::UNO_QUERY);
+    uno::Reference<text::XTextRange> xCell(xTable->getCellByName("B1"), uno::UNO_QUERY);
+    uno::Reference<container::XEnumerationAccess> xParaEnumAccess(xCell->getText(), uno::UNO_QUERY);
+    uno::Reference<container::XEnumeration> xParaEnum = xParaEnumAccess->createEnumeration();
+    uno::Reference<text::XTextRange> xPara(xParaEnum->nextElement(), uno::UNO_QUERY);
+    // Margins were inherited from the previous cell, even there was a \pard there.
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(0), getProperty<sal_Int32>(xPara, "ParaLeftMargin"));
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(Test);