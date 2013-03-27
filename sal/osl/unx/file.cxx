c1ea7a2fb43a8f51134fafba78b3acc68f10fcf4
Stephan Bergmann <sbergman@redhat.com>
2013-03-27 11:35:40 +0100

    At least Mac OS X SDK 10.7 doesn't experience TimeValue clash here
    (causes -Werror,-Wunused-macros, rather)
    
    Change-Id: I1dc05a5cf73aec7827df662342f5d9189215e64e

--- sal/osl/unx/file.cxx
+++ sal/osl/unx/file.cxx
@@ -43,9 +43,13 @@
 #define HAVE_O_EXLOCK
 
 // add MACOSX Time Value
+#if MACOSX_SDK_VERSION < 1070
 #define TimeValue CFTimeValue
+#endif
 #include <CoreFoundation/CoreFoundation.h>
+#if MACOSX_SDK_VERSION < 1070
 #undef TimeValue
+#endif
 
 #endif /* MACOSX */
 
