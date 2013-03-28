2c8ea7d9dff35e64701883b089e30715ed7b56b5
Rafael Dominguez <venccsralph@gmail.com>
2013-03-28 14:49:49 -0430

    Deselect items when searching for templates to reset toolbars.
    Change-Id: Ifacbc257abcc84284deb1723d30e74d45adca192

--- sfx2/source/doc/templatedlg.cxx
+++ sfx2/source/doc/templatedlg.cxx
@@ -682,6 +682,7 @@ IMPL_LINK_NOARG(SfxTemplateManagerDlg, SearchUpdateHdl)
         // if the search view is hidden, hide the folder view and display search one
         if (!mpSearchView->IsVisible())
         {
+            mpCurView->deselectItems();
             mpSearchView->Show();
             mpCurView->Hide();
         }
