96699848ec8e72ec4893777d9f2d657872862ffc
Chr. Rossmanith <ChrRossmanith@gmx.de>
2013-03-30 18:59:57 +0100

    Use OUString and sal_Int32 in GetTextWidth() (2)
    Change-Id: I85ea6cc60add141954c8b75f78a8024c872d7174
    Reviewed-on: https://gerrit.libreoffice.org/3158
    Reviewed-by: Michael Stahl <mstahl@redhat.com>
    Tested-by: Michael Stahl <mstahl@redhat.com>

--- vcl/inc/textlayout.hxx
+++ vcl/inc/textlayout.hxx
@@ -40,7 +40,7 @@ namespace vcl
     class SAL_NO_VTABLE ITextLayout
     {
     public:
-        virtual long        GetTextWidth( const XubString& _rText, xub_StrLen _nStartIndex, xub_StrLen _nLength ) const = 0;
+        virtual long        GetTextWidth( const OUString& _rText, sal_Int32 _nStartIndex, sal_Int32 _nLength ) const = 0;
         virtual void        DrawText( const Point& _rStartPoint, const OUString& _rText, sal_Int32 _nStartIndex, sal_Int32 _nLength,
                                 MetricVector* _pVector, OUString* _pDisplayText ) = 0;
         virtual bool        GetCaretPositions( const OUString& _rText, sal_Int32* _pCaretXArray, sal_Int32 _nStartIndex, sal_Int32 _nLength ) const = 0;
@@ -68,9 +68,9 @@ namespace vcl
 
         // ITextLayout overridables
         virtual long        GetTextWidth(
-                                const XubString& _rText,
-                                xub_StrLen _nStartIndex,
-                                xub_StrLen _nLength
+                                const OUString& _rText,
+                                sal_Int32 _nStartIndex,
+                                sal_Int32 _nLength
                             ) const;
         virtual void        DrawText(
                                 const Point& _rStartPoint,
