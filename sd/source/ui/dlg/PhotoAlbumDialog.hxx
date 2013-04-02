4d5bc9168be03c6254e13e16b64218cafcda8c31
Gergő Mocsi <gmocsi91@gmail.com>
2013-04-02 12:57:59 +0200

    Create Photo Album implementation
    Added a checkbox to enable/diable Keep Aspect ratio, plus 2 images/slide option is implemented.
    UI tweaks solved: buttons get enabled/disabled properly. Dialog window can be opened again (it works for me).
    
    Change-Id: I68d2a3e00f8c55b909fe4d98aa3958188653390e

--- sd/source/ui/dlg/PhotoAlbumDialog.hxx
+++ sd/source/ui/dlg/PhotoAlbumDialog.hxx
@@ -23,9 +23,18 @@
 #include <vcl/graphicfilter.hxx>
 #include <svx/svdotext.hxx>
 
+#include <com/sun/star/drawing/XDrawPage.hpp>
+#include <com/sun/star/drawing/XDrawPages.hpp>
+#include <com/sun/star/graphic/GraphicProvider.hpp>
+#include <com/sun/star/graphic/XGraphicProvider.hpp>
+
 class SdrTextObj;
 class SdDrawDocument;
 class SdPage;
+
+using namespace ::com::sun::star;
+using namespace ::com::sun::star::uno;
+using namespace ::com::sun::star::presentation;
 
 namespace sd
 {
@@ -36,6 +45,7 @@ public:
     SdPhotoAlbumDialog(Window* pWindow, SdDrawDocument* pActDoc);
     ~SdPhotoAlbumDialog();
 
+    virtual short Execute();
 private:
     CancelButton*   pCancelBtn;
     PushButton*     pCreateBtn;
@@ -49,7 +59,9 @@ private:
     ListBox*        pImagesLst;
     FixedImage*     pImg;
 
-    ListBox*   pInsTypeCombo;
+    ListBox*    pInsTypeCombo;
+
+    CheckBox*   pASRCheck;
 
     SdDrawDocument* pDoc;
     GraphicFilter* mpGraphicFilter;
@@ -66,6 +78,18 @@ private:
     DECL_LINK(SelectHdl, void*);
 
     void setFirstSlide(SdPage* pFirstSlide);
+
+    Reference< drawing::XDrawPage > appendNewSlide(AutoLayout aLayout,
+        Reference< drawing::XDrawPages > xDrawPages);
+
+    awt::Size createASRSize(const awt::Size& aPicSize, const awt::Size& aMaxSize);
+
+    Reference< drawing::XShape > createXShapeFromUrl(const OUString& sUrl,
+        Reference< lang::XMultiServiceFactory > xShapeFactory,
+        Reference< graphic::XGraphicProvider> xProvider);
+
+    Reference< graphic::XGraphic> createXGraphicFromUrl(const OUString& sUrl,
+        Reference< graphic::XGraphicProvider> xProvider);
 };
 
 }
