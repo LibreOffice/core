LibreOffice/core

commit bb920862c02047471f0ac5e78358e8d8fd9b93e7

fix fdo#40496: don't reset RES_PARATR_ADJUST & RES_FRAMEDIR attributes.
Change-Id: I69298632d56decbce3eb7058b982d845cb3e7240
Reviewed-on: https://gerrit.libreoffice.org/3146
Reviewed-by: Bosdonnat Cedric <cedric.bosdonnat@free.fr>
Tested-by: Bosdonnat Cedric <cedric.bosdonnat@free.fr>

sw/source/ui/shells/textsh1.cxx
@@ -499,7 +499,9 @@ void SwTextShell::Execute(SfxRequest &rReq)
                     aAttribs.insert( aAttribs.end(), nE++ );
                 pUShorts += 2;
             }
-
+            // we don't want to change writing direction and text alignment.
+            aAttribs.erase( RES_FRAMEDIR );
+            aAttribs.erase( RES_PARATR_ADJUST );
             rWrtSh.ResetAttr( aAttribs );
             rReq.Done();
             break;

commit 6fa38b6a3df6d9ec05d502326ea0532e27f65e0e

fix fdo#40496: don't reset  RES_FRAMEDIR attribute.
fix regression introduced by bb920862c02047471f0ac5e78358e8d8fd9b93e7 commit

discussed here :
https://bugs.freedesktop.org/show_bug.cgi?id=40496#c13

choosing correct default alignment for RTL text need some discussion.

Change-Id: I2aec9391d076af6d5fbf00ba5bdad030ac0286ed

sw/source/ui/shells/textsh1.cxx
@@ -499,9 +499,8 @@ void SwTextShell::Execute(SfxRequest &rReq)
                     aAttribs.insert( aAttribs.end(), nE++ );
                 pUShorts += 2;
             }
-            // we don't want to change writing direction and text alignment.
+            // we don't want to change writing direction.
             aAttribs.erase( RES_FRAMEDIR );
-            aAttribs.erase( RES_PARATR_ADJUST );
             rWrtSh.ResetAttr( aAttribs );
             rReq.Done();
             break;