2f6d5d5f9b31f3b8a1b5cecf7136e118948ab84d
David Tardon <dtardon@redhat.com>
2013-04-06 15:34:43 +0200

    deliver all uiconfig files by UIConfig
    Change-Id: Ie78b7ce399ba34485146ca7622c59d31f8105d02

--- sd/UIConfig_sdraw.mk
+++ sd/UIConfig_sdraw.mk
@@ -9,6 +9,62 @@
 
 $(eval $(call gb_UIConfig_UIConfig,modules/sdraw))
 
+$(eval $(call gb_UIConfig_add_menubarfiles,modules/sdraw,\
+	sd/uiconfig/sdraw/menubar/menubar \
+))
+
+$(eval $(call gb_UIConfig_add_statusbarfiles,modules/sdraw,\
+	sd/uiconfig/sdraw/statusbar/statusbar \
+))
+
+$(eval $(call gb_UIConfig_add_toolbarfiles,modules/sdraw,\
+	sd/uiconfig/sdraw/toolbar/3dobjectsbar \
+	sd/uiconfig/sdraw/toolbar/alignmentbar \
+	sd/uiconfig/sdraw/toolbar/arrowsbar \
+	sd/uiconfig/sdraw/toolbar/arrowshapes \
+	sd/uiconfig/sdraw/toolbar/basicshapes \
+	sd/uiconfig/sdraw/toolbar/bezierobjectbar \
+	sd/uiconfig/sdraw/toolbar/calloutshapes \
+	sd/uiconfig/sdraw/toolbar/choosemodebar \
+	sd/uiconfig/sdraw/toolbar/colorbar \
+	sd/uiconfig/sdraw/toolbar/commentsbar \
+	sd/uiconfig/sdraw/toolbar/connectorsbar \
+	sd/uiconfig/sdraw/toolbar/drawingobjectbar \
+	sd/uiconfig/sdraw/toolbar/ellipsesbar \
+	sd/uiconfig/sdraw/toolbar/extrusionobjectbar \
+	sd/uiconfig/sdraw/toolbar/findbar \
+	sd/uiconfig/sdraw/toolbar/flowchartshapes \
+	sd/uiconfig/sdraw/toolbar/fontworkobjectbar \
+	sd/uiconfig/sdraw/toolbar/fontworkshapetype \
+	sd/uiconfig/sdraw/toolbar/formcontrols \
+	sd/uiconfig/sdraw/toolbar/formdesign \
+	sd/uiconfig/sdraw/toolbar/formsfilterbar \
+	sd/uiconfig/sdraw/toolbar/formsnavigationbar \
+	sd/uiconfig/sdraw/toolbar/formtextobjectbar \
+	sd/uiconfig/sdraw/toolbar/fullscreenbar \
+	sd/uiconfig/sdraw/toolbar/gluepointsobjectbar \
+	sd/uiconfig/sdraw/toolbar/graffilterbar \
+	sd/uiconfig/sdraw/toolbar/graphicobjectbar \
+	sd/uiconfig/sdraw/toolbar/insertbar \
+	sd/uiconfig/sdraw/toolbar/linesbar \
+	sd/uiconfig/sdraw/toolbar/masterviewtoolbar \
+	sd/uiconfig/sdraw/toolbar/mediaobjectbar \
+	sd/uiconfig/sdraw/toolbar/moreformcontrols \
+	sd/uiconfig/sdraw/toolbar/optimizetablebar \
+	sd/uiconfig/sdraw/toolbar/optionsbar \
+	sd/uiconfig/sdraw/toolbar/positionbar \
+	sd/uiconfig/sdraw/toolbar/rectanglesbar \
+	sd/uiconfig/sdraw/toolbar/standardbar \
+	sd/uiconfig/sdraw/toolbar/starshapes \
+	sd/uiconfig/sdraw/toolbar/symbolshapes \
+	sd/uiconfig/sdraw/toolbar/tableobjectbar \
+	sd/uiconfig/sdraw/toolbar/textbar \
+	sd/uiconfig/sdraw/toolbar/textobjectbar \
+	sd/uiconfig/sdraw/toolbar/toolbar \
+	sd/uiconfig/sdraw/toolbar/viewerbar \
+	sd/uiconfig/sdraw/toolbar/zoombar \
+))
+
 $(eval $(call gb_UIConfig_add_uifiles,modules/sdraw,\
 	sd/uiconfig/sdraw/ui/printeroptions \
 ))
