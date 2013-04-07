9d54f0e1dec4d84f914f17c8c2445dfc53190d8a
David Tardon <dtardon@redhat.com>
2013-04-07 09:45:19 +0200

    add gbuild class PackageSet
    This is intended for bundling filelists of multiple Packages together,
    to simplify the scp2 configuration.
    
    Change-Id: I6f700f691b37375a09a5400166aac80a8bb2a2b4
    Reviewed-on: https://gerrit.libreoffice.org/3230
    Reviewed-by: David Tardon <dtardon@redhat.com>
    Tested-by: David Tardon <dtardon@redhat.com>

--- solenv/gbuild/TargetLocations.mk
+++ solenv/gbuild/TargetLocations.mk
@@ -166,6 +166,7 @@ gb_Pagein_get_target = $(WORKDIR)/Pagein/$(1)
 gb_Package_get_preparation_target = $(WORKDIR)/Package/prepared/$(1)
 gb_Package_get_target = $(WORKDIR)/Package/$(1).filelist
 gb_Package_get_target_for_build = $(WORKDIR_FOR_BUILD)/Package/$(1).filelist
+gb_PackageSet_get_target = $(WORKDIR)/PackageSet/$(1).filelist
 gb_Postprocess_get_target = $(WORKDIR)/Postprocess/$(1)
 gb_PrecompiledHeader_get_dep_target = $(WORKDIR)/Dep/PrecompiledHeader/$(gb_PrecompiledHeader_DEBUGDIR)/$(1).hxx.gch.d
 gb_PrecompiledHeader_get_target = $(WORKDIR)/PrecompiledHeader/$(gb_PrecompiledHeader_DEBUGDIR)/$(1).hxx.gch
@@ -288,6 +289,7 @@ $(eval $(call gb_Helper_make_clean_targets,\
 	NoexPrecompiledHeader \
 	PackagePart \
 	Package \
+	PackageSet \
 	Pagein \
 	PrecompiledHeader \
 	Pyuno \
