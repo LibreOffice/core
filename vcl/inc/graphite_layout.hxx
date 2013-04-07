dc06b7681cb7b60d31a16b72bcee93a6c602097f
navin patidar <patidar@kacst.edu.sa>
2013-04-07 11:47:01 +0300

    fix fdo#60534 : use DefaultCharMapper::mapChar() to map RTL string unicodes.
    Use DefaultCharMapper::mapChar() to map RTL string unicodes
    to their mirror compatible  unicodes.
    
    Change-Id: I5bd2fd18bf96c49bbdf2be521a9cf28c311c7a09

--- vcl/inc/graphite_layout.hxx
+++ vcl/inc/graphite_layout.hxx
@@ -97,6 +97,7 @@ public:
 private:
     const gr_face *         mpFace; // not owned by layout
     gr_font *               mpFont; // not owned by layout
+    sal_Unicode *           mpStr;
     int                     mnSegCharOffset; // relative to ImplLayoutArgs::mpStr
     long                    mnWidth;
     std::vector<int>        mvChar2BaseGlyph;
