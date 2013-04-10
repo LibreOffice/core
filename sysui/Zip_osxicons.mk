ba6989df7dffb4fd2c62dbb9f5b1c9b000b4abdd
David Tardon <dtardon@redhat.com>
2013-04-10 13:53:12 +0200

    just use explicit list of files
    IMHO it does not make much sense to dynamically generate a list that is
    practically unchanging.
    
    Change-Id: If5898956d11b9f410cf36d1ebe5535194365a954

--- sysui/Zip_osxicons.mk
+++ sysui/Zip_osxicons.mk
@@ -7,12 +7,32 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 
-osxicons_LIST := $(shell $(PERL) -w $(SRCDIR)/sysui/desktop/macosx/list_icons.pl < $(SRCDIR)/sysui/desktop/macosx/Info.plist)
-
 $(eval $(call gb_Zip_Zip,osxicons,$(SRCDIR)/sysui/desktop))
 
 $(eval $(call gb_Zip_add_files,osxicons,\
-	$(addprefix icons/,$(osxicons_LIST)) \
+    icons/text.icns \
+    icons/oasis-text.icns \
+    icons/spreadsheet.icns \
+    icons/oasis-spreadsheet.icns \
+    icons/presentation.icns \
+    icons/oasis-presentation.icns \
+    icons/drawing.icns \
+    icons/oasis-drawing.icns \
+    icons/master-document.icns \
+    icons/oasis-master-document.icns \
+    icons/formula.icns \
+    icons/oasis-formula.icns \
+    icons/text-template.icns \
+    icons/oasis-text-template.icns \
+    icons/spreadsheet-template.icns \
+    icons/oasis-spreadsheet-template.icns \
+    icons/presentation-template.icns \
+    icons/oasis-presentation-template.icns \
+    icons/drawing-template.icns \
+    icons/oasis-drawing-template.icns \
+    icons/oasis-database.icns \
+    icons/extension.icns \
+    icons/main.icns \
 	macosx/Info.plist \
 ))
 
