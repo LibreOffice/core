327adac44129264f7bc0dab161afbb88ec3003d8
Faisal M. Al-Otaibi <fmalotaibi@kacst.edu.sa>
2013-04-10 12:50:30 +0300

    more fix to fdo#62470
    fix some problem appear when delete the right border.
    
    Change-Id: I2bd1d69142bb0a920bdb58c833f8cbbb461a8350
    Reviewed-on: https://gerrit.libreoffice.org/3305
    Reviewed-by: Ahmad Harthi <aalharthi@kacst.edu.sa>
    Tested-by: Ahmad Harthi <aalharthi@kacst.edu.sa>

--- sw/source/core/docnode/ndtbl1.cxx
+++ sw/source/core/docnode/ndtbl1.cxx
@@ -696,33 +696,37 @@ void SwDoc::SetTabBorders( const SwCursor& rCursor, const SfxItemSet& rSet )
                 {
                         if( bLeftOver && bRightOver)
                         {
-                            if ( bLeftOver ? bLeftValid : bVertValid )
-                                {
-                                    aBox.SetLine( bLeftOver ? pLeft : 0,
-                                                  BOX_LINE_RIGHT );
-                                    bVertValid ? nType |= 0x0020 : nType |= 0x0010;
-                                }
-                                if ( bRightOver ? bRightValid : bVertValid )
-                                {
-                                    aBox.SetLine( bRightOver ? pRight : pVert,
-                                                  BOX_LINE_LEFT );
-                                    bVertValid ? nType |= 0x0008 : nType |= 0x0004;
-                                }
+                            if ( bLeftValid )
+                            {
+                                aBox.SetLine( pLeft, BOX_LINE_RIGHT );
+                                nType |= 0x0010;
+                            }
+                            if ( bRightValid )
+                            {
+                                aBox.SetLine( pRight, BOX_LINE_LEFT );
+                                nType |= 0x0004;
+                            }
                         }
                         else
                         {
-                                if ( bRightOver ? bLeftValid : bVertValid )
+                            if ( bLeftValid )
+                            {
+                                aBox.SetLine( bRightOver ? pLeft : 0, BOX_LINE_RIGHT );
+                                bVertValid ? nType |= 0x0020 : nType |= 0x0010;
+                            }
+                            if ( bLeftOver )
+                            {
+                                if ( bRightValid )
                                 {
-                                    aBox.SetLine( bRightOver ? pLeft : 0,
-                                                  BOX_LINE_RIGHT );
-                                    bVertValid ? nType |= 0x0020 : nType |= 0x0010;
+                                    aBox.SetLine( pRight, BOX_LINE_LEFT );
+                                    nType |= 0x0004;
                                 }
-                                if ( bLeftOver ? bRightValid : bVertValid )
-                                {
-                                    aBox.SetLine( bLeftOver ? pRight : pVert,
-                                                  BOX_LINE_LEFT );
-                                    bVertValid ? nType |= 0x0008 : nType |= 0x0004;
-                                }
+                            }
+                            else if ( bVertValid )
+                            {
+                                aBox.SetLine( pVert, BOX_LINE_LEFT );
+                                nType |= 0x0008;
+                            }
                         }
                 }
                 else
