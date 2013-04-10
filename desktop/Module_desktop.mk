a33e45182cb8ca95c8772cb2185de59522480501
Matúš Kukan <matus.kukan@gmail.com>
2013-04-10 13:09:50 +0200

    remove duplicated else branch
    Change-Id: I2d4676e3b44551d1730fe6e7b2898a2a38e4649c

--- desktop/Module_desktop.mk
+++ desktop/Module_desktop.mk
@@ -121,13 +121,6 @@ else ifeq ($(OS),ANDROID)
 
 else ifeq ($(OS),IOS)
 
-else ifeq ($(GUIBASE),headless)
-
-$(eval $(call gb_Module_add_targets,desktop,\
-    Executable_oosplash \
-    Library_spl_unx \
-))
-
 else
 
 $(eval $(call gb_Module_add_targets,desktop,\
