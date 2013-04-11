1da3c602feab944b3d5ef2835993274046c2ba08
Julien Nabet <serval2412@yahoo.fr>
2013-04-11 22:20:41 +0200

    fdo#63197, Convert Zip_autotextuser
    Change-Id: I6a7764637f6e5754e68ee6e4ab417224f542592d

--- extras/Package_autotextuser.mk
+++ extras/Package_autotextuser.mk
@@ -7,9 +7,11 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 
-$(eval $(call gb_Zip_Zip,autotextuser,$(SRCDIR)/extras/source/autotext))
+$(eval $(call gb_Package_Package,extras_autotextuser,$(SRCDIR)/extras/source/autotext))
 
-$(eval $(call gb_Zip_add_files,autotextuser,\
+$(eval $(call gb_Package_set_outdir,extras_autotextuser,$(INSTDIR)))
+
+$(eval $(call gb_Package_add_files,extras_autotextuser,presets/autotext,\
 	mytexts.bau \
 ))
 
