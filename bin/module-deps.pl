cd809a1e4397af66f2e418532c368a7ec7a8c1b1
Peter Foley <pefoley2@verizon.net>
2013-04-14 10:24:46 -0400

    fix weird Circular Dep messages
    Change-Id: Ib8e442740a421d3ed86202813e97aa0239fb2299

--- bin/module-deps.pl
+++ bin/module-deps.pl
@@ -33,7 +33,7 @@ sub read_deps()
     if (defined $from_file) {
         open ($p, $from_file) || die "can't read deps from cache file: $!";
     } else {
-        open ($p, "ENABLE_PRINT_DEPS=1 $gnumake -q -f $makefile_build all|") || die "can't launch make: $!";
+        open ($p, "ENABLE_PRINT_DEPS=1 $gnumake -qrf $makefile_build|") || die "can't launch make: $!";
     }
     $|=1;
     print STDERR "reading deps ";
