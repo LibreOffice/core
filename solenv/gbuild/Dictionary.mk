fb3bec7b3dd9b00600d5b0ef9e8ee0052f7fa248
Michael Stahl <mstahl@redhat.com>
2013-04-17 13:37:26 +0200

    gbuild: fix gb_Dictionary_add_localized_xcu_file
    ... clearly David should have reverted my commit since it didn't
    actually work :-/
    
    Change-Id: Ib34e540e247f1082bc9d155cd011eb31cd33048e

--- solenv/gbuild/Dictionary.mk
+++ solenv/gbuild/Dictionary.mk
@@ -291,7 +291,7 @@ endef
 # gb_Dictionary_add_localized_xcu_file dictionary destdir file
 define gb_Dictionary_add_localized_xcu_file
 ifeq ($(gb_WITH_LANG),)
-$(call gb_Dictionary__add_file,$(1),$(1),$(2),$(strip $(3)))
+$(call gb_Dictionary__add_file,$(1),$(1),$(2)/$(notdir $(3)),$(strip $(3)))
 else
 $(call gb_Dictionary__add_file,$(1),$(1)_generated,$(2),$(subst $(WORKDIR)/,,$(call gb_XcuFile_for_extension,$(strip $(3)))))
 $(call gb_PackageSet_add_package,$(call gb_Dictionary_get_packagesetname,$(1)),$(call gb_Dictionary_get_packagename,$(1)_generated))
