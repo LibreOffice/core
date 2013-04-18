b4f4cdc231fb8f35954cd896d95c087933ab96b0
Caolán McNamara <caolanm@redhat.com>
2013-04-18 13:13:43 +0100

    Resolves: rhbz#963276 rhbz#761009 lp#766153 lp#892904 HandleFontOptions crash
    The mpServerFont member of a ImplServerFontEntry must not be deleted while the
    ImplServerFontEntry still exists
    
    see also 39cbce553da1834f78b77f48b2f1be9578d6cc05 for another reason a crash in
    the same place can happen.  Its impossible from traces in crashes before
    39cbce553da1834f78b77f48b2f1be9578d6cc05 was fixed to distinguish those crashes
    from this crash.
    
    This crash is a regression due to 7a416820ab5e03f8b988656e0f6a592cb1e81d07
    where we went from modifying pServerFont in X11SalGraphics::setFont directly to
    modifying it/a-different-one indirectly via ImplServerFontEntry
    
    The various font caches and font thing lifecycles of LibreOffice are somewhat
    confusing.
    
    This crash had eluded me for years, to reproduce:
    insert->special chars->select a font with loads of glyphs, i.e. "AR PL UKai CN"
    click on the first row of glyphs and hold down page-down until you hit the
    bottom, then page-up until you hit the top. Pre patch it won't survive the
    whole down+up (and valgrind will moan quite a bit)
    
    (cherry picked from commit 3d6424a752902bba513102834966868f609d91e6)
    
    Change-Id: Ifde0cb375f487c556b04a640d77765a7dc2f0913
    Reviewed-on: https://gerrit.libreoffice.org/3922
    Reviewed-by: Fridrich Strba <fridrich@documentfoundation.org>
    Tested-by: Fridrich Strba <fridrich@documentfoundation.org>

--- vcl/inc/generic/glyphcache.hxx
+++ vcl/inc/generic/glyphcache.hxx
@@ -224,6 +224,7 @@ public:
 private:
     friend class GlyphCache;
     friend class ServerFontLayout;
+    friend class ImplServerFontEntry;
     friend class X11SalGraphics;
 
     void                        AddRef() const      { ++mnRefCount; }
@@ -301,7 +302,7 @@ private:
 public:
                    ImplServerFontEntry( FontSelectPattern& );
     virtual        ~ImplServerFontEntry();
-    void           SetServerFont( ServerFont* p) { mpServerFont = p; }
+    void           SetServerFont(ServerFont* p);
     void           HandleFontOptions();
 };
 
