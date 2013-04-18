f1efc98b45fcf462a74b31790d4075f29cc0f8af
Tor Lillqvist <tml@iki.fi>
2013-04-19 00:30:10 +0300

    WaE: format specifies type 'int' but the argument has type 'sal_uInt32'
    Change-Id: I33f603df67db2caaeebec5fc6f5f6ee2341a0b8f

--- sd/source/filter/eppt/pptx-epptooxml.cxx
+++ sd/source/filter/eppt/pptx-epptooxml.cxx
@@ -1314,7 +1314,7 @@ bool PowerPointExport::WriteComments( sal_uInt32 nPageNum )
                 sal_Int32 nId = GetAuthorIdAndLastIndex ( xAnnotation->getAuthor(), nLastIndex );
                 char cDateTime[32];
 
-                snprintf(cDateTime, 31, "%02d-%02d-%02dT%02d:%02d:%02d.%09d", aDateTime.Year, aDateTime.Month, aDateTime.Day, aDateTime.Hours, aDateTime.Minutes, aDateTime.Seconds, aDateTime.NanoSeconds);
+                snprintf(cDateTime, 31, "%02d-%02d-%02dT%02d:%02d:%02d.%09" SAL_PRIuUINT32, aDateTime.Year, aDateTime.Month, aDateTime.Day, aDateTime.Hours, aDateTime.Minutes, aDateTime.Seconds, aDateTime.NanoSeconds);
 
                 pFS->startElementNS( XML_p, XML_cm,
                                      XML_authorId, I32S( nId ),
