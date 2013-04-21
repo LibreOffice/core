9c7a0f5ef9298208e380c612cc0366c8b54b1775
David Tardon <dtardon@redhat.com>
2013-04-21 17:38:36 +0200

    replace CustomTarget_odk/odkcommon/settings by Package
    Change-Id: Iee97a510be822836b2115f50d0b1c9e7e14b5e1a

--- odk/CustomTarget_settings.mk
+++ odk/CustomTarget_settings.mk
@@ -9,25 +9,9 @@
 
 $(eval $(call gb_CustomTarget_CustomTarget,odk/odkcommon/settings))
 
-odk_SETTINGSLIST := settings.mk \
-	std.mk \
-	stdtarget.mk \
-	$(if $(filter WNT,$(OS)),component.uno.def)
-
-define odk_settings
-odkcommon_ZIPLIST += settings/$(1)
-$(call gb_CustomTarget_get_target,odk/odkcommon/settings): $(odk_WORKDIR)/settings/$(1)
-$(odk_WORKDIR)/settings/$(1): $(SRCDIR)/odk/settings/$(1)
-	mkdir -p $$(dir $$@)
-	$$(call gb_Output_announce,$$(subst $$(WORKDIR)/,,$$@),build,CPY,1)
-	cp $$< $$@
-endef
-
-$(foreach setting,$(odk_SETTINGSLIST),$(eval $(call odk_settings,$(setting))))
-
-odkcommon_ZIPLIST += settings/dk.mk
 $(eval $(call gb_CustomTarget_register_target,odk/odkcommon/settings,dk.mk))
-$(odk_WORKDIR)/settings/dk.mk: $(SRCDIR)/odk/pack/copying/dk.mk
+
+$(call gb_CustomTarget_get_workdir,odk/odkcommon/settings)/dk.mk : $(SRCDIR)/odk/pack/copying/dk.mk
 	$(call gb_Output_announce,$(subst $(WORKDIR)/,,$@),build,SED,1)
 	tr -d "\015" < $< | sed -e 's/@@RELEASE@@/$(PRODUCTVERSION)/' \
 		-e 's/@@BUILDID@@/$(LIBO_VERSION_MAJOR).$(LIBO_VERSION_MINOR).$(LIBO_VERSION_MICRO).$(LIBO_VERSION_PATCH)/' \
