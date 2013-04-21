fab1e58ab9e3d941c421f3722fbbbaffa1716bcb
David Tardon <dtardon@redhat.com>
2013-04-21 17:38:36 +0200

    replace CustomTarget_odk/odkcommon/settings by Package
    Change-Id: Iee97a510be822836b2115f50d0b1c9e7e14b5e1a

--- odk/CustomTarget_check.mk
+++ odk/CustomTarget_check.mk
@@ -38,6 +38,8 @@ $(call gb_CustomTarget_get_workdir,odk/check)/checkbin : \
 		$(call gb_Package_get_target,odk_html) \
 		$(call gb_Package_get_target,odk_lib) \
 		$(if $(filter MACOSX,$(OS)),$(call gb_Package_get_target,odk_macosx)) \
+		$(call gb_Package_get_target,odk_settings) \
+		$(call gb_Package_get_target,odk_settings_generated) \
 		$(if $(SOLAR_JAVA),$(call gb_Package_get_target,odk_unowinreg)) \
 		$(call gb_CustomTarget_get_target,odk/odkcommon)
 	$(call gb_Output_announce,$(subst $(WORKDIR)/,,$@),$(true),CHK,1)
