90666f50f5f6542d436e8187d432024b58397812
Michael Stahl <mstahl@redhat.com>
2013-04-21 14:34:50 +0200

    replace python-core zip built in pyuno with direct use of Package
    - python3: deliver files to INSTDIR, with same layout as instset
      and do not deliver .lib files
    - pyuno: remove obsolete python.bin targets
    - pyuno: remove usage of CustomTarget_zip for WNT and non-Mac UNX
      platforms (sadly it is apparently still needed for "system" python on
      MinGW)
    - scp2: use the python3 filelist
      There is still a problem here because the installer does not currently
      allow to preserve the executable bit on files in a filelist
    - RepositoryExternal: run python executable from INSTDIR
      and link against libraries in UnpackedTarball dir
    
    Change-Id: I931ca0a8be6ff40051b1ca50da1f0770e6057832

--- solenv/gbuild/platform/solaris.mk
+++ solenv/gbuild/platform/solaris.mk
@@ -434,7 +434,7 @@ gb_UIMenubarTarget_UIMenubarTarget_platform :=
 gb_Pyuno_PROGRAMDIRNAME := program
 
 # Python
-gb_PYTHON_PRECOMMAND := $(gb_Helper_set_ld_path) PYTHONHOME=$(OUTDIR)/lib/python PYTHONPATH=$(OUTDIR)/lib/python:$(OUTDIR)/lib/python/lib-dynload
+gb_Python_PRECOMMAND := $(gb_Helper_set_ld_path) PYTHONHOME="$(INSTDIR)/program/python-core-$(PYTHON_VERSION)" PYTHONPATH="$(INSTDIR)/program/python-core-$(PYTHON_VERSION)/lib:$(INSTDIR)/program/python-core-$(PYTHON_VERSION)/lib/lib-dynload"
 gb_Python_INSTALLED_EXECUTABLE := /bin/sh $(DEVINSTALLDIR)/opt/program/python
 # this is passed to gdb as executable when running tests
 gb_Python_INSTALLED_EXECUTABLE_GDB := $(DEVINSTALLDIR)/opt/program/python.bin
