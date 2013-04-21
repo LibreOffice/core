65bb16373057c590bb9a097a48a6d15b79d669ee
David Tardon <dtardon@redhat.com>
2013-04-21 17:38:36 +0200

    replace CustomTarget_odk/odkcommon/settings by Package
    Change-Id: Iee97a510be822836b2115f50d0b1c9e7e14b5e1a

---
+++ odk/Package_settings_generated.mk
@@ -0,0 +1,18 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_Package_Package,odk_settings_generated,$(call gb_CustomTarget_get_workdir,odk/odkcommon/settings)))
+
+$(eval $(call gb_Package_set_outdir,odk_settings_generated,$(INSTDIR)))
+
+$(eval $(call gb_Package_add_files,odk_settings_generated,$(gb_Package_SDKDIRNAME)/settings,\
+	dk.mk \
+))
+
+# vim: set noet sw=4 ts=4:
