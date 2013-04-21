1ae59b52e302491ffcd97bde25a6dd68bf109123
Michael Stahl <mstahl@redhat.com>
2013-04-21 18:06:17 +0200

    gbuild: fix spurious delivering of import libs (MSVC)
    The DLL aux-target is always newer than the work-dir import lib,
    which causes spurious re-delivery of the solver import lib.
    Touch the DLL file to the same timestamp as the import lib
    to prevent this.
    
    Change-Id: I8394201c7a672a50db3af51b9442f8992cdef1d1

--- solenv/gbuild/platform/com_MSC_class.mk
+++ solenv/gbuild/platform/com_MSC_class.mk
@@ -213,8 +213,8 @@ $(call gb_Helper_abbreviate_dirs,\
 		$(if $(filter-out StaticLibrary,$(TARGETTYPE)),user32.lib) \
 		$(if $(DLLTARGET),-out:$(DLLTARGET) -implib:$(1),-out:$(1)); RC=$$?; rm $${RESPONSEFILE} \
 	$(if $(DLLTARGET),; if [ ! -f $(DLLTARGET) ]; then rm -f $(1) && false; fi) \
-	$(if $(filter Library,$(TARGETTYPE)),; if [ -f $(DLLTARGET).manifest ]; then mt.exe $(MTFLAGS) -nologo -manifest $(DLLTARGET).manifest -outputresource:$(DLLTARGET)\;2; fi) \
-	$(if $(filter Executable,$(TARGETTYPE)),; if [ -f $(1).manifest ]; then mt.exe $(MTFLAGS) -nologo -manifest $(1).manifest -outputresource:$(1)\;1; fi) \
+	$(if $(filter Library,$(TARGETTYPE)),&& if [ -f $(DLLTARGET).manifest ]; then mt.exe $(MTFLAGS) -nologo -manifest $(DLLTARGET).manifest -outputresource:$(DLLTARGET)\;2 && touch -r $(1) $(DLLTARGET).manifest $(DLLTARGET); fi) \
+	$(if $(filter Executable,$(TARGETTYPE)),&& if [ -f $(1).manifest ]; then mt.exe $(MTFLAGS) -nologo -manifest $(1).manifest -outputresource:$(1)\;1 && touch -r $(1) $(1).manifest; fi) \
 	; exit $$RC)
 endef
 
