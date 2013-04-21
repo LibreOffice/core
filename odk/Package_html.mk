4f4344b51599d7fd7febeefa4fa46c38c36af57b
David Tardon <dtardon@redhat.com>
2013-04-21 16:36:06 +0200

    chip more files off the odkcommon.zip block
    Change-Id: I3d5d841d094f397130e37799a2f26e4d85f7c136
    Reviewed-on: https://gerrit.libreoffice.org/3533
    Reviewed-by: Fridrich Strba <fridrich@documentfoundation.org>
    Tested-by: Fridrich Strba <fridrich@documentfoundation.org>

---
+++ odk/Package_html.mk
@@ -0,0 +1,23 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_Package_Package,odk_html,$(call gb_CustomTarget_get_workdir,odk/html)))
+
+$(eval $(call gb_Package_set_outdir,odk_html,$(INSTDIR)))
+
+$(eval $(call gb_Package_add_files_with_dir,odk_html,$(gb_Package_SDKDIRNAME),\
+	docs/install.html \
+	docs/notsupported.html \
+	docs/tools.html \
+	examples/DevelopersGuide/examples.html \
+	examples/examples.html \
+	index.html \
+))
+
+# vim: set noet sw=4 ts=4:
