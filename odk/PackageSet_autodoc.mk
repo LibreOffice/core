970c0e0035d9b8e6374f1676c57e83d6417caa31
David Tardon <dtardon@redhat.com>
2013-04-23 09:03:24 +0200

    install gen. IDL docs using filelist
    Change-Id: I474f6e7a45d09683eb0dd7172114407c9dca84d7

---
+++ odk/PackageSet_autodoc.mk
@@ -0,0 +1,18 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_PackageSet_PackageSet,odk_autodoc))
+
+$(eval $(call gb_PackageSet_add_packages,odk_autodoc,\
+	odk_autodoc \
+	offapi_doc \
+	udkapi_doc \
+))
+
+# vim: set noet sw=4 ts=4:
