954d695827431a5badd1a993d8f364c669ca9c9c
Michael Stahl <mstahl@redhat.com>
2013-04-23 22:27:57 +0200

    fdo#60280: sw: restore anti-aliasing for all OLE objects
    The OLE case in SwNoTxtFrm::PaintPicture() was mis-merged to modify the
    anti-aliasing always but reset it only for Charts.
    
    (regression from 44cfc7cb6533d827fd2d6e586d92c61d7d7f7a70)
    
    Change-Id: I0d740e621c3fd71f308d980581edb3c16308c06e

--- sw/source/core/doc/notxtfrm.cxx
+++ sw/source/core/doc/notxtfrm.cxx
@@ -1065,8 +1065,7 @@ void SwNoTxtFrm::PaintPicture( OutputDevice* pOut, const SwRect &rGrfArea ) cons
         }
 
         // see #i99665#
-        if ( pOLENd->IsChart() &&
-                pShell->Imp()->GetDrawView()->IsAntiAliasing() )
+        if (pShell->Imp()->GetDrawView()->IsAntiAliasing())
         {
             pOut->SetAntialiasing( nFormerAntialiasingAtOutput );
         }
