162ef53d441c4eb56a51f48b29b54c3915ba9da5
David Tardon <dtardon@redhat.com>
2013-04-25 09:42:40 +0200

    fix dep on doxygen CustomTarget
    Change-Id: Id53ab5de54ff8e86a39685b370b1114666755cfe

--- odk/CustomTarget_check.mk
+++ odk/CustomTarget_check.mk
@@ -29,7 +29,7 @@ odk_PLATFORM := $(if $(filter WNT,$(OS)),\
 $(call gb_CustomTarget_get_workdir,odk/check)/checkbin : \
 		$(SRCDIR)/odk/util/check.pl \
 		$(call gb_CustomTarget_get_target,odk/odkcommon/classes) \
-		$(if $(DOXYGEN),$(call gb_CustomTarget_get_target,odk/odkcommon/docs/cpp/ref)) \
+		$(if $(DOXYGEN),$(call gb_CustomTarget_get_target,odk/odkcommon/docs/cpp)) \
 		$(call gb_Package_get_target,odk_bin) \
 		$(if $(filter WNT,$(OS)),$(call gb_Package_get_target,odk_cli)) \
 		$(call gb_Package_get_target,odk_config) \
