dfb6a625d355a92bef3b85fb1dfdce1da64fde0a
Noel Grandin <noel@peralex.com>
2013-04-26 08:38:50 +0200

    Java cleanup
    -fix constructor declaration
    -remove dead copyright notice
    
    Change-Id: I021ef11dd5000a75dba3359fe6d4f49740c3b237

--- qadevOOo/qa/complex/junitskeleton/justatest.java
+++ qadevOOo/qa/complex/junitskeleton/justatest.java
@@ -16,37 +16,27 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-/**
- * @copyright: Sun Microsystems Inc. 2010
- */
-
 package complex.junitskeleton;
 
 public class justatest {
 
-    public void justatest()
-        {
-            System.out.println("justatest CTor.");
-        }
+    public justatest() {
+        System.out.println("justatest CTor.");
+    }
 
-    public void testfkt()
-        {
-            System.out.println("Test called.");
-        }
+    public void testfkt() {
+        System.out.println("Test called.");
+    }
 
-      /**
-    * Sleeps for 0.5 sec. to allow StarOffice to react on <code>
-    * reset</code> call.
-    */
-    public static void shortWait()
-    {
-        try
-        {
-            Thread.sleep(500) ;
-        }
-        catch (InterruptedException e)
-        {
-            System.out.println("While waiting :" + e) ;
+    /**
+     * Sleeps for 0.5 sec. to allow StarOffice to react on <code>
+     * reset</code> call.
+     */
+    public static void shortWait() {
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            System.out.println("While waiting :" + e);
         }
     }
 
