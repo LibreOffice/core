ecc07f080fba809dd3f87c88a2b265c1c63bf2dd
David Tardon <dtardon@redhat.com>
2013-04-27 15:11:42 +0200

    copy rc files to instdir
    Change-Id: I5a4433595ac19930a40132c5749a8c0150c90aae

---
+++ instsetoo_native/Package_config.mk
@@ -0,0 +1,22 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_Package_Package,instsetoo_native_config,$(SRCDIR)/instsetoo_native/config))
+
+$(eval $(call gb_Package_set_outdir,instsetoo_native_config,$(INSTDIR)))
+
+$(eval $(call gb_Package_add_files,instsetoo_native_config,program,\
+	bootstraprc \
+	fundamentalrc \
+	unorc \
+))
+
+$(eval $(call gb_Package_add_file,instsetoo_native_config,ure/lib/unorc,unorc.ure))
+
+# vim: set noet sw=4 ts=4:
