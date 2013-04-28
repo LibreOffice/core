a6126455324861a16fade96318365653f55852ea
Markus Mohrhard <markus.mohrhard@googlemail.com>
2013-04-29 00:12:07 +0200

    change order to allow validation of chart part
    My simple test file is finally valid but is still now shown in Excel.
    There must be another bug in our exporter.
    
    Change-Id: Ib55e5b32edc3a556e9081b3008df539275dc289b
    (cherry picked from commit e81d0c400c02a87d7fa492dbc5ac9f7921167920)

--- oox/source/export/chartexport.cxx
+++ oox/source/export/chartexport.cxx
@@ -2187,6 +2187,9 @@ void ChartExport::_exportAxis(
             XML_val, sTickLblPos,
             FSEND );
 
+    // shape properties
+    exportShapeProps( xAxisProp );
+
     pFS->singleElement( FSNS( XML_c, XML_crossAx ),
             XML_val, I32S( aAxisIdPair.nCrossAx ),
             FSEND );
@@ -2281,8 +2284,6 @@ void ChartExport::_exportAxis(
             FSEND );
     }
 
-    // shape properties
-    exportShapeProps( xAxisProp );
     // TODO: text properties
 
     pFS->endElement( FSNS( XML_c, nAxisType ) );
