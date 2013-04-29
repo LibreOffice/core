0bb97f5656ef5b01b6e1b97e25e33622a44d4d34
Thorsten Behrens <tbehrens@suse.com>
2013-04-30 00:05:18 +0200

    Fix import of images from pdf.
    Seems poppler changed outputdev virt func signatures with version
    12, so with system poppler, we never got any image import functions
    called. Added override annotation to prevent this from happening in
    the future.
    
    Change-Id: I565de80ce218e831e276d3a1d88ded3baee876e7

--- sdext/source/pdfimport/xpdfwrapper/pdfioutdev_gpl.cxx
+++ sdext/source/pdfimport/xpdfwrapper/pdfioutdev_gpl.cxx
@@ -836,6 +836,9 @@ void PDFOutDev::endTextObject(GfxState*)
 
 void PDFOutDev::drawImageMask(GfxState* pState, Object*, Stream* str,
                               int width, int height, GBool invert,
+#if POPPLER_CHECK_VERSION(0, 12, 0)
+                              GBool /*interpolate*/,
+#endif
                               GBool /*inlineImg*/ )
 {
     OutputBuffer aBuf; initBuf(aBuf);
@@ -862,6 +865,9 @@ void PDFOutDev::drawImageMask(GfxState* pState, Object*, Stream* str,
 
 void PDFOutDev::drawImage(GfxState*, Object*, Stream* str,
                           int width, int height, GfxImageColorMap* colorMap,
+#if POPPLER_CHECK_VERSION(0, 12, 0)
+                          GBool /*interpolate*/,
+#endif
                           int* maskColors, GBool /*inlineImg*/ )
 {
     OutputBuffer aBuf; initBuf(aBuf);
@@ -908,9 +914,16 @@ void PDFOutDev::drawImage(GfxState*, Object*, Stream* str,
 void PDFOutDev::drawMaskedImage(GfxState*, Object*, Stream* str,
                                 int width, int height,
                                 GfxImageColorMap* colorMap,
+#if POPPLER_CHECK_VERSION(0, 12, 0)
+                                GBool /*interpolate*/,
+#endif
                                 Stream* maskStr,
                                 int maskWidth, int maskHeight,
-                                GBool maskInvert)
+                                GBool maskInvert
+#if POPPLER_CHECK_VERSION(0, 12, 0)
+                                , GBool /*maskInterpolate*/
+#endif
+                               )
 {
     OutputBuffer aBuf;     initBuf(aBuf);
     printf( "drawImage %d %d 0", width, height );
@@ -921,9 +934,16 @@ void PDFOutDev::drawMaskedImage(GfxState*, Object*, Stream* str,
 void PDFOutDev::drawSoftMaskedImage(GfxState*, Object*, Stream* str,
                                     int width, int height,
                                     GfxImageColorMap* colorMap,
+#if POPPLER_CHECK_VERSION(0, 12, 0)
+                                    GBool /*interpolate*/,
+#endif
                                     Stream* maskStr,
                                     int maskWidth, int maskHeight,
-                                    GfxImageColorMap* maskColorMap )
+                                    GfxImageColorMap* maskColorMap
+#if POPPLER_CHECK_VERSION(0, 12, 0)
+                                    , GBool /*maskInterpolate*/
+#endif
+                                   )
 {
     OutputBuffer aBuf;     initBuf(aBuf);
     printf( "drawImage %d %d 0", width, height );
