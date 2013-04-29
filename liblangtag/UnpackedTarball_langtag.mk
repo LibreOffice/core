574b2bc5d15a6e4e317599a290f0605fe703e91f
Eike Rathke <erack@redhat.com>
2013-04-29 15:08:50 +0200

    new upstream release liblangtag-0.5.0
    Change-Id: I4b71bbd3ff457d7b4fb2a409e8d34d8e4904acb3

--- liblangtag/UnpackedTarball_langtag.mk
+++ liblangtag/UnpackedTarball_langtag.mk
@@ -12,52 +12,15 @@ $(eval $(call gb_UnpackedTarball_UnpackedTarball,langtag))
 $(eval $(call gb_UnpackedTarball_set_tarball,langtag,$(LIBLANGTAG_TARBALL)))
 
 $(eval $(call gb_UnpackedTarball_add_patches,langtag,\
-	liblangtag/liblangtag-0.4.0-xmlDocCopyNode.patch \
+	liblangtag/liblangtag-0.5.0-msvc-warning.patch \
+	liblangtag/liblangtag-0.5.0-reg2xml-encoding-problem.patch \
+	liblangtag/liblangtag-0.5.0-vsnprintf.patch \
 ))
-    # liblangtag-0.4.0-xmlDocCopyNode.patch addressed upstream as
-    # <https://github.com/tagoh/liblangtag/pull/8> "Use xmlDocCopyNode to merge
-    #  nodes into different docs"
-
-ifeq ($(filter-out MACOSX AIX,$(OS)),)
-$(eval $(call gb_UnpackedTarball_add_patches,langtag,\
-	liblangtag/liblangtag-0.4.0-mac.patch \
-	liblangtag/liblangtag-0.4.0-configure-atomic-cflag-pollution.patch \
-))
-else
-$(eval $(call gb_UnpackedTarball_add_patches,langtag,\
-	liblangtag/liblangtag-0.4.0-msvc-warning.patch \
-	liblangtag/liblangtag-0.4.0-configure-atomic-cflag-pollution.patch \
-	liblangtag/liblangtag-0.4.0-windows.patch \
-	liblangtag/liblangtag-0.4.0-reg2xml-encoding-problem.patch \
-	liblangtag/liblangtag-0.4.0-windows2.patch \
-	liblangtag/liblangtag-0.4.0-msvcprojects.patch \
-	liblangtag/liblangtag-0.4.0-ppc.patch \
-	liblangtag/liblangtag-0.4.0-vsnprintf.patch \
-))
-endif
-
-ifeq ($(CROSS_COMPILING),YES)
-$(eval $(call gb_UnpackedTarball_add_patches,langtag,\
-	liblangtag/liblangtag-0.4.0-cross.patch \
-))
-endif
 
 ifeq ($(OS),WNT)
 ifeq ($(COM),GCC)
 $(eval $(call gb_UnpackedTarball_add_patches,langtag,\
-	liblangtag/liblangtag-0.4.0-mingw.patch \
-))
-else
-
-# liblangtag-0.4.0-ar-lib.patch and ar-lib are addressed upstream as
-# <https://bitbucket.org/tagoh/liblangtag/issue/1>
-
-$(eval $(call gb_UnpackedTarball_add_patches,langtag,\
-	liblangtag/liblangtag-0.4.0-cross.patch \
-	liblangtag/liblangtag-0.4.0-ar-lib.patch \
-))
-$(eval $(call gb_UnpackedTarball_add_files,langtag,,\
-	liblangtag/ar-lib \
+	liblangtag/liblangtag-0.5.0-mingw.patch \
 ))
 endif
 endif
