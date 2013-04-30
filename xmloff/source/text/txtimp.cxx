cd09b5379e9370c66fe9d819dc3cd01dd30e3a91
Thorsten Behrens <tbehrens@suse.com>
2013-04-30 11:20:01 +0200

    Fix fdo#64062 - can't assume para styles to be there.
    Change-Id: I5da53ff44fdf00ec6c0fdf1a722f806971b6683a

--- xmloff/source/text/txtimp.cxx
+++ xmloff/source/text/txtimp.cxx
@@ -1801,7 +1801,7 @@ OUString XMLTextImportHelper::SetStyleAndAttrs(
                  style, if it has no direct list style property and (if exists) the
                  automatic paragraph style has also no direct list style set.
             */
-            if (m_pImpl->m_xParaStyles->hasByName(sStyleName))
+            if (m_pImpl->m_xParaStyles.is() && m_pImpl->m_xParaStyles->hasByName(sStyleName))
             {
                 bool bOutlineStyleCandidate( false );
 
