3e52762c5a9b4d1976b968634c72b172927a51d8
Michael Meeks <michael.meeks@suse.com>
2013-05-02 12:42:08 +0100

    Move to MPLv2 license headers, with ESC decision and author's permission.
    Change-Id: I9f1102d4f0e189a048d8fccfe9cae246c0136311

--- connectivity/source/drivers/postgresql/pq_connection.hxx
+++ connectivity/source/drivers/postgresql/pq_connection.hxx
@@ -31,7 +31,6 @@
  *    This Source Code Form is subject to the terms of the Mozilla Public
  *    License, v. 2.0. If a copy of the MPL was not distributed with this
  *    file, You can obtain one at http://mozilla.org/MPL/2.0/.
- *    Version: MPL 1.1 / GPLv3+ / LGPLv2.1+
  *
  ************************************************************************/
 
