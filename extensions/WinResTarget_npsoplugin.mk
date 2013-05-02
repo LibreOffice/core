4c8ca04dbe9fd213a3d16a2b5b828cd9a8843f99
Andras Timar <atimar@suse.com>
2013-05-02 08:59:57 -0700

    remove defines from npsoplugin resource, that are defined elsewhere
    Change-Id: I745944202a25a348f36e79d28f03740a28b70eee

--- extensions/WinResTarget_npsoplugin.mk
+++ extensions/WinResTarget_npsoplugin.mk
@@ -30,6 +30,11 @@
 
 $(eval $(call gb_WinResTarget_WinResTarget,npsoplugin/npsoplugin))
 
+$(eval $(call gb_WinResTarget_add_defs,npsoplugin/npsoplugin,\
+    -DVERVARIANT=$(BUILD) \
+    -DRES_APP_VENDOR="$(OOO_VENDOR)" \
+))
+
 $(eval $(call gb_WinResTarget_set_rcfile,npsoplugin/npsoplugin,extensions/source/nsplugin/source/nsplugin_oo))
 
 # vim:set shiftwidth=4 softtabstop=4 expandtab:
