6f8e3163300be0671ce476b2098e3b2bdfabb539
Luke Deller <luke@deller.id.au>
2013-05-03 00:32:22 +1000

    Fix style renaming confusion in ww8 filter (solves fdo#63603)
    - Mapping LO default style to Word "Normal" style for docx output used a
      hard coded style name, so it broke when LO's default style was renamed
      in 4.0 (plus it would never have worked for non-English languages)
    - This renaming did not cater for nameclashes, leading to fdo#63603
    - Similar renaming when importing doc styles did cater for nameclashes,
      but had a minor bug in that
    
    Change-Id: Id23f3021c6507b474c16e93abf43ba748606ebc7

--- sw/source/filter/ww8/writerwordglue.cxx
+++ sw/source/filter/ww8/writerwordglue.cxx
@@ -300,11 +300,13 @@ namespace myImplHelpers
                 aName.InsertAscii("WW-" , 0);
 
             sal_Int32 nI = 1;
+            String aBaseName = aName;
             while (
                     0 != (pColl = maHelper.GetStyle(aName)) &&
                     (nI < SAL_MAX_INT32)
                   )
             {
+                aName = aBaseName;
                 aName += OUString::number(nI++);
             }
         }
