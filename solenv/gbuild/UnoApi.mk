84f31cd5bb5eb940eb05c94673c8b5ad889536ba
Michael Stahl <mstahl@redhat.com>
2013-05-03 23:43:45 +0200

    gbuild: UnoApi: revert documentation Package support
    This was a good idea in its time and age (2 weeks ago) but has been
    obsoleted by recent developments: sadly with the zoo of files
    generated by doxygen this kind of tracking makes no sense.
    
    Change-Id: I243de5cf6129ea3532067a39006d8e4259f15a95

--- solenv/gbuild/UnoApi.mk
+++ solenv/gbuild/UnoApi.mk
@@ -7,9 +7,6 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 
-gb_UnoApi_DOCDIR := $(gb_Package_SDKDIRNAME)/docs/common/ref
-# NOTE: this is the output dir used in odk/CustomTarget_autodoc.mk
-gb_UnoApi_SRCDOCDIR := $(call gb_CustomTarget_get_workdir,odk/docs/common/ref)
 gb_UnoApi_ENABLE_INSTALL := $(and $(filter host,$(gb_Side)),$(filter ODK,$(BUILD_TYPE)))
 
 .PHONY : $(call gb_UnoApi_get_clean_target,%)
@@ -23,8 +20,6 @@ $(call gb_UnoApiHeadersTarget_UnoApiHeadersTarget,$(1))
 ifneq ($(gb_UnoApi_ENABLE_INSTALL),)
 $(call gb_Package_Package_internal,$(1)_idl,$(SRCDIR))
 $(call gb_Package_set_outdir,$(1)_idl,$(INSTDIR))
-$(call gb_Package_Package_internal,$(1)_doc,$(gb_UnoApi_SRCDOCDIR))
-$(call gb_Package_set_outdir,$(1)_doc,$(INSTDIR))
 endif
 
 $(call gb_UnoApiTarget_set_root,$(1),UCR)
@@ -77,51 +72,6 @@ $(call gb_UnoApi__add_headerfile_impl,$(1),$(2),$(subst $() $(),/,$(wordlist 2,$
 
 endef
 
-ifneq ($(gb_UnoApi_ENABLE_INSTALL),)
-
-# gb_UnoApi__add_docfile_impl api htmlfile
-define gb_UnoApi__add_docfile_impl
-$(if $(filter-out com ooo org,$(firstword $(subst /, ,$(2)))),\
-	$(call gb_Output_error,gb_UnoApi__add_docfile_impl: unknown namespace: $(firstword $(subst /, ,$(2)))) \
-)
-$(call gb_Package_add_file,$(1)_doc,$(gb_UnoApi_DOCDIR)/$(2),$(2))
-
-endef
-
-# gb_UnoApi__add_docfile_direct api htmlfile
-define gb_UnoApi__add_docfile_direct
-$(call gb_UnoApi__add_docfile_impl,$(1),$(patsubst $(1)/%,%,$(2)))
-
-endef
-
-# gb_UnoApi__add_docfile_for_idl api idlfile has-xref?
-define gb_UnoApi__add_docfile_for_idl
-$(if $(filter-out modules %-modules,$(basename $(notdir $(2)))),\
-	$(call gb_UnoApi__add_docfile_direct,$(1),$(2).html) \
-	$(if $(3),$(call gb_UnoApi__add_docfile_direct,$(1),$(2)-xref.html)) \
-)
-
-endef
-
-# gb_UnoApi__add_docfile api idlfile has-xref?
-define gb_UnoApi__add_docfile
-$(call gb_UnoApi__add_docfile_for_idl,$(1),$(2),$(3))
-
-endef
-
-# gb_UnoApi__add_docfiles api dir idlfiles has-xref?
-define gb_UnoApi__add_docfiles
-$(foreach idlfile,$(3),$(call gb_UnoApi__add_docfile_for_idl,$(1),$(2)/$(idlfile),$(3)))
-
-endef
-
-else # !gb_UnoApi_ENABLE_INSTALL
-
-gb_UnoApi__add_docfile :=
-gb_UnoApi__add_docfiles :=
-
-endif
-
 define gb_UnoApi__add_idlfile_noheader
 $(call gb_UnoApi__add_idlfile,$(1),$(2).idl)
 
@@ -143,14 +93,12 @@ endef
 define gb_UnoApi_add_idlfile
 $(call gb_UnoApiTarget_add_idlfile,$(1),$(2),$(3))
 $(call gb_UnoApi__add_idlfile_full,$(1),$(2)/$(3))
-$(call gb_UnoApi__add_docfile,$(1),$(2)/$(3),$(true))
 
 endef
 
 define gb_UnoApi_add_idlfiles
 $(call gb_UnoApiTarget_add_idlfiles,$(1),$(2),$(3))
 $(foreach idl,$(3),$(call gb_UnoApi__add_idlfile_full,$(1),$(2)/$(idl)))
-$(call gb_UnoApi__add_docfiles,$(1),$(2),$(3),$(true))
 
 endef
 
@@ -158,14 +106,12 @@ endef
 define gb_UnoApi_add_idlfile_nohdl
 $(call gb_UnoApiTarget_add_idlfile,$(1),$(2),$(3))
 $(call gb_UnoApi__add_idlfile_nohdl,$(1),$(2)/$(3))
-$(call gb_UnoApi__add_docfile,$(1),$(2)/$(3))
 
 endef
 
 define gb_UnoApi_add_idlfiles_nohdl
 $(call gb_UnoApiTarget_add_idlfiles,$(1),$(2),$(3))
 $(foreach idl,$(3),$(call gb_UnoApi__add_idlfile_nohdl,$(1),$(2)/$(idl)))
-$(call gb_UnoApi__add_docfiles,$(1),$(2),$(3))
 
 endef
 
@@ -173,14 +119,12 @@ endef
 define gb_UnoApi_add_idlfile_noheader
 $(call gb_UnoApiTarget_add_idlfile,$(1),$(2),$(3))
 $(call gb_UnoApi__add_idlfile_noheader,$(1),$(2)/$(3))
-$(call gb_UnoApi__add_docfile,$(1),$(2)/$(3))
 
 endef
 
 define gb_UnoApi_add_idlfiles_noheader
 $(call gb_UnoApiTarget_add_idlfiles,$(1),$(2),$(3))
 $(foreach idl,$(3),$(call gb_UnoApi__add_idlfile_noheader,$(1),$(2)/$(idl)))
-$(call gb_UnoApi__add_docfiles,$(1),$(2),$(3))
 
 endef
 
