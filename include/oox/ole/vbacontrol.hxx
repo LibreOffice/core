fdc14139b235b3695a67e073a20e65392ebf1f2f
Noel Power <noel.power@suse.com>
2013-05-03 15:14:26 +0100

    fix userform data aware control import
    Change-Id: I86f5cc0c60bba48eaaf40a3471cc0fb22e5e4b9c

--- include/oox/ole/vbacontrol.hxx
+++ include/oox/ole/vbacontrol.hxx
@@ -73,7 +73,8 @@ public:
                             const ControlConverter& rConv,
                             ApiControlType eCtrlType,
                             sal_Int32 nCtrlIndex ) const;
-
+    inline ::rtl::OUString getControlSource() { return  maControlSource; }
+    inline ::rtl::OUString getRowSource() { return  maRowSource; }
 protected:
     OUString     maName;             ///< Name of the control.
     OUString     maTag;              ///< User defined tag.
