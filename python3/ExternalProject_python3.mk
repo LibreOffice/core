ef9c9729d241c82749fe2bc13503c54f2701aa9c
David Tardon <dtardon@redhat.com>
2013-05-04 14:50:51 +0200

    the MacOS X cleanup is logically a part of build
    ... so move it to python/ExternalProject_python3.mk, where it belongs.
    
    Change-Id: Ib99a6a40182341257f79dd289eac51806be46fcf

--- python3/ExternalProject_python3.mk
+++ python3/ExternalProject_python3.mk
@@ -18,6 +18,11 @@ $(eval $(call gb_ExternalProject_use_externals,python3,\
 
 $(eval $(call gb_ExternalProject_register_targets,python3,\
 	build \
+	$(if $(filter MACOSX,$(OS)),\
+		fixscripts \
+		fixinstallnames \
+		executables \
+	) \
 ))
 
 ifeq ($(OS)$(COM),WNTMSC)
@@ -100,6 +105,52 @@ $(call gb_ExternalProject_get_state_target,python3,build) :
 			$(if $(filter MACOSX,$(OS)),DESTDIR=$(EXTERNAL_WORKDIR)/python-inst install) \
 		&& ln -s build/lib.* LO_lib \
 	)
+
+endif
+
+ifeq ($(OS),MACOSX)
+
+python3_fw_prefix=$(call gb_UnpackedTarball_get_dir,python3)/python-inst/@__________________________________________________OOO/LibreOfficePython.framework
+
+# rule to allow relocating the whole framework, removing reference to buildinstallation directory
+$(call gb_ExternalProject_get_state_target,python3,fixscripts) : $(call gb_ExternalProject_get_state_target,python3,build)
+	$(call gb_Output_announce,python3 - remove reference to installroot from scripts,build,CUS,5)
+	$(COMMAND_ECHO)for file in \
+			$(python3_fw_prefix)/Versions/$(PYTHON_VERSION_MAJOR).$(PYTHON_VERSION_MINOR)/bin/2to3 \
+			$(python3_fw_prefix)/Versions/$(PYTHON_VERSION_MAJOR).$(PYTHON_VERSION_MINOR)/bin/2to3-$(PYTHON_VERSION_MAJOR).$(PYTHON_VERSION_MINOR) \
+			$(python3_fw_prefix)/Versions/$(PYTHON_VERSION_MAJOR).$(PYTHON_VERSION_MINOR)/bin/idle$(PYTHON_VERSION_MAJOR).$(PYTHON_VERSION_MINOR) \
+			$(python3_fw_prefix)/Versions/$(PYTHON_VERSION_MAJOR).$(PYTHON_VERSION_MINOR)/bin/pydoc$(PYTHON_VERSION_MAJOR).$(PYTHON_VERSION_MINOR) \
+			$(python3_fw_prefix)/Versions/$(PYTHON_VERSION_MAJOR).$(PYTHON_VERSION_MINOR)/bin/python$(PYTHON_VERSION_MAJOR).$(PYTHON_VERSION_MINOR)-config \
+			$(python3_fw_prefix)/Versions/$(PYTHON_VERSION_MAJOR).$(PYTHON_VERSION_MINOR)/bin/python$(PYTHON_VERSION_MAJOR).$(PYTHON_VERSION_MINOR)m-config \
+			$(python3_fw_prefix)/Versions/$(PYTHON_VERSION_MAJOR).$(PYTHON_VERSION_MINOR)/bin/pyvenv-$(PYTHON_VERSION_MAJOR).$(PYTHON_VERSION_MINOR) ; do \
+	{ rm "$$file" && awk '\
+		BEGIN {print "#!/bin/bash\n\
+origpath=$$(pwd)\n\
+bindir=$$(cd $$(dirname \"$$0\") ; pwd)\n\
+cd \"$$origpath\"\n\
+\"$$bindir/../Resources/Python.app/Contents/MacOS/LibreOfficePython\" - $$@ <<EOF"} \
+		FNR==1{next} \
+		      {print} \
+		END   {print "EOF"}' > "$$file" ; } < "$$file" ; chmod +x "$$file" ; done
+	touch $@
+
+$(call gb_ExternalProject_get_state_target,python3,fixinstallnames) : $(call gb_ExternalProject_get_state_target,python3,build)
+	install_name_tool -change \
+		$(python3_fw_prefix)/Versions/$(PYTHON_VERSION_MAJOR).$(PYTHON_VERSION_MINOR)/LibreOfficePython \
+		@executable_path/../../../../LibreOfficePython \
+		$(python3_fw_prefix)/Versions/$(PYTHON_VERSION_MAJOR).$(PYTHON_VERSION_MINOR)/Resources/Python.app/Contents/MacOS/LibreOfficePython
+	touch $@
+
+# also delete binaries that are symlinked in scp2
+$(call gb_ExternalProject_get_state_target,python3,executables) : $(call gb_ExternalProject_get_state_target,python3,build)
+	cd $(python3_fw_prefix)/Versions/$(PYTHON_VERSION_MAJOR).$(PYTHON_VERSION_MINOR)/bin ; \
+	for file in python$(PYTHON_VERSION_MAJOR).$(PYTHON_VERSION_MINOR) \
+	            python$(PYTHON_VERSION_MAJOR).$(PYTHON_VERSION_MINOR)m \
+	            pythonw$(PYTHON_VERSION_MAJOR).$(PYTHON_VERSION_MINOR) ; do \
+	install_name_tool -change \
+		$(python3_fw_prefix)/Versions/$(PYTHON_VERSION_MAJOR).$(PYTHON_VERSION_MINOR)/LibreOfficePython \
+		@executable_path/../LibreOfficePython $$file ; done
+	touch $@
 
 endif
 
