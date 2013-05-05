919f853278c3187e30484c8e542beb92b3165e2b
Andras Timar <atimar@suse.com>
2013-05-05 12:05:43 -0700

    moz: build fix
    Change-Id: I1a8685ec5fff2e78aa9eb97ba9b331a1a2a99e74

--- moz/Module_moz.mk
+++ moz/Module_moz.mk
@@ -14,9 +14,9 @@ ifeq ($(WITH_MOZAB4WIN),YES)
 $(eval $(call gb_Module_add_targets,moz,\
 	ExternalPackage_moz_lib \
 	ExternalPackage_runtime \
-	Package_runtime \
 	UnpackedTarball_moz_inc \
 	UnpackedTarball_moz_lib \
+	UnpackedTarball_mozruntime \
 ))
 
 endif
