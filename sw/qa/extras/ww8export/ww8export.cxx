LibreOffice/core

commit 1348288905ffa75d3634eee4392ba4660d28cdb9

CWS-TOOLING: integrate CWS hb32bugs01 2009-08-03 22:06:54 +0200 hbrinkm  r274599 : resolved warning on unxlngi6 2009-08-03 17:52:32 +0200 hbrinkm  r274592 : CWS-TOOLING: rebase CWS hb32bugs01 to trunk@274203 (milestone: DEV300:m53) 2009-08-03 13:52:40 +0200 hbrinkm  r274578 : #i102260# applied patches 2009-08-03 11:49:22 +0200 hbrinkm  r274570 : #i96371# applied patch 2009-07-24 17:44:05 +0200 hbrinkm  r274316 : #i99261# adjust to change in behaviour of DeleteAndJoin 2009-07-24 17:40:30 +0200 hbrinkm  r274315 : resolved warnings on wntmsci12 2009-07-17 13:33:22 +0200 hbrinkm  r274091 : #i74153# robustness check in SwRTFParser::NextToken 2009-07-06 16:34:52 +0200 hbrinkm  r273753 : #97488# applied patch 2009-07-06 16:04:05 +0200 hbrinkm  r273750 : #i96875# some robustness changes 2009-06-30 17:06:15 +0200 hbrinkm  r273523 : #i99786# applied patch 2009-06-30 14:17:49 +0200 hbrinkm  r273505 : #i100918# applied patch 2009-06-18 13:18:08 +0200 hbrinkm  r273111 : #i100952# TabCellEnd: Do not remove empty text node if it has SwFrmFmts anchored 2009-06-18 13:14:23 +0200 hbrinkm  r273110 : #i100952# SwFltAnchorClient to watch for changes of SwFrmFmt in SwFltAnchor 2009-06-16 13:20:50 +0200 hbrinkm  r273018 : #102490# applied patch 2009-06-16 12:51:01 +0200 hbrinkm  r273017 : #i102473# applied patch 2009-06-16 10:45:04 +0200 hbrinkm  r273009 : #100353# applied patch 2009-06-15 17:39:47 +0200 hbrinkm  r273004 : CWS-TOOLING: rebase CWS hb32bugs01 to trunk@272827 (milestone: DEV300:m50) 2009-06-10 14:32:45 +0200 hbrinkm  r272812 : CWS-TOOLING: rebase CWS hb32bugs01 to trunk@272291 (milestone: DEV300:m49)

sw/source/filter/ww8/wrtw8sty.cxx
@@ -1117,7 +1117,8 @@ void MSWordSections::SetFooterFlag( BYTE& rHeadFootFlags, const SwFmt& rFmt,
 }
 
 void WW8_WrPlcSepx::OutHeaderFooter( WW8Export& rWrt, bool bHeader,
-        const SwFmt& rFmt, ULONG& rCpPos, BYTE nHFFlags, BYTE nFlag )
+                     const SwFmt& rFmt, ULONG& rCpPos, BYTE nHFFlags,
+                     BYTE nFlag,  BYTE nBreakCode)
 {
     if ( nFlag & nHFFlags )
     {
@@ -1129,7 +1130,7 @@ void WW8_WrPlcSepx::OutHeaderFooter( WW8Export& rWrt, bool bHeader,
     else if ( rWrt.bWrtWW8 )
     {
         pTxtPos->Append( rCpPos );
-        if ( bHeader? rWrt.bHasHdr: rWrt.bHasFtr )
+        if (rWrt.bHasHdr && nBreakCode!=0)
         {
             rWrt.WriteStringAsPara( aEmptyStr ); // Empty paragraph for empty header/footer
             rWrt.WriteStringAsPara( aEmptyStr ); // a CR that WW8 needs for end of the stream
@@ -1702,7 +1703,6 @@ bool WW8_WrPlcSepx::WriteKFTxt( WW8Export& rWrt )
         pA->nSepxFcPos = 0xffffffff;                // Default: none
 
         WW8_SepInfo& rSepInfo = aSects[i];
-
         rWrt.SectionProperties( rSepInfo, pA );
     }
     rWrt.SetHdFtIndex( nOldIndex ); //0

sw/source/core/swg/SwXMLTextBlocks1.cxx
@@ -40,6 +40,7 @@
 #include <com/sun/star/xml/sax/InputSource.hpp>
 #include <com/sun/star/io/XActiveDataSource.hpp>
 #include <com/sun/star/xml/sax/XParser.hpp>
+#include <com/sun/star/document/XStorageBasedDocument.hpp>
 #include <doc.hxx>
 #ifndef _DOCSH_HXX
 #include <docsh.hxx>
@@ -81,6 +82,21 @@ ULONG SwXMLTextBlocks::GetDoc( USHORT nIdx )
             ReadXML->SetBlockMode( sal_True );
             aReader.Read( *ReadXML );
             ReadXML->SetBlockMode( sal_False );
+            // Ole objects fails to display when inserted into document
+            // because the ObjectReplacement folder ( and contents are missing )
+            rtl::OUString sObjReplacements( RTL_CONSTASCII_USTRINGPARAM( "ObjectReplacements" ) );
+            if ( xRoot->hasByName( sObjReplacements ) )
+            {
+                uno::Reference< document::XStorageBasedDocument > xDocStor( pDoc->GetDocShell()->GetModel(), uno::UNO_QUERY_THROW );
+                uno::Reference< embed::XStorage > xStr( xDocStor->getDocumentStorage() );
+                if ( xStr.is() )
+                {
+                    xRoot->copyElementTo( sObjReplacements, xStr, sObjReplacements );
+                    uno::Reference< embed::XTransactedObject > xTrans( xStr, uno::UNO_QUERY );
+                    if ( xTrans.is() )
+                        xTrans->commit();
+                }
+            }
         }
         catch( uno::Exception& )
         {

sw/source/filter/inc/fltshell.hxx
@@ -144,18 +144,36 @@ public:
     void DeleteAndDestroy(Entries::size_type nCnt);
 };
 
+class SwFltAnchorClient;
+
 class SW_DLLPUBLIC SwFltAnchor : public SfxPoolItem
 {
     SwFrmFmt* pFrmFmt;
+    SwFltAnchorClient * pClient;
+
 public:
     SwFltAnchor(SwFrmFmt* pFlyFmt);
     SwFltAnchor(const SwFltAnchor&);
+    virtual ~SwFltAnchor();
+
     // "pure virtual Methoden" vom SfxPoolItem
     virtual int operator==(const SfxPoolItem&) const;
     virtual SfxPoolItem* Clone(SfxItemPool* = 0) const;
-    const SwFrmFmt* GetFrmFmt() const       { return pFrmFmt; }
-          SwFrmFmt* GetFrmFmt()             { return pFrmFmt; }
+    void SetFrmFmt(SwFrmFmt * _pFrmFmt);
+    const SwFrmFmt* GetFrmFmt() const;
+          SwFrmFmt* GetFrmFmt();
 };
+
+class SwFltAnchorClient : public SwClient
+{
+    SwFltAnchor * m_pFltAnchor;
+
+public:
+    SwFltAnchorClient(SwFltAnchor * pFltAnchor);
+
+    virtual void Modify (SfxPoolItem *pOld, SfxPoolItem *pNew);
+};
+
 
 class SW_DLLPUBLIC SwFltRedline : public SfxPoolItem
 {

sw/source/filter/rtf/rtfatr.cxx
@@ -230,6 +230,8 @@ void OutRTF_SfxItemSet( SwRTFWriter& rWrt, const SfxItemSet& rSet,
                 ( *pItem != rPool.GetDefaultItem( nWhich )
                     || ( rSet.GetParent() &&
                         *pItem != rSet.GetParent()->Get( nWhich ) )
+                    || ( rWrt.GetAttrSet() &&
+                        *pItem != rWrt.GetAttrSet()->Get( nWhich ) )
               ) )
                 ;
             else
@@ -1895,7 +1897,12 @@ Writer& OutRTF_SwTblNode(Writer& rWrt, const SwTableNode & rNode)
         const SwWriteTableCells& rCells = pRow->GetCells();
 
         BOOL bFixRowHeight = false;
-        for( nColCnt = 0, nBox = 0; nBox < rCells.Count(); ++nColCnt )
+
+        USHORT nBoxes = rCells.Count();
+        if (nColCnt < nBoxes)
+            nBoxes = nColCnt;
+
+        for( nColCnt = 0, nBox = 0; nBox < rCells.Count() && nColCnt < nBoxes; ++nColCnt )
         {
             SwWriteTableCell* pCell = rCells[ nBox ];
             const bool bProcessCoveredCell = bNewTableModel && 0 == pCell->GetRowSpan();
@@ -1994,7 +2001,7 @@ Writer& OutRTF_SwTblNode(Writer& rWrt, const SwTableNode & rNode)
         for( nBox = 0; nBox < nColCnt; ++nBox )
         {
             SwWriteTableCell* pCell = pBoxArr[ nBox ];
-            if( nBox && pBoxArr[ nBox-1 ] == pBoxArr[ nBox ] )
+            if( (nBox && pBoxArr[ nBox-1 ] == pBoxArr[ nBox ]) || (pCell == NULL) )
                 continue;
 
             const SwFrmFmt& rFmt = *pCell->GetBox()->GetFrmFmt();
@@ -2045,15 +2052,17 @@ Writer& OutRTF_SwTblNode(Writer& rWrt, const SwTableNode & rNode)
 
         // Inhalt der Boxen ausgeben
         rWrt.Strm() << SwRTFWriter::sNewLine << OOO_STRING_SVTOOLS_RTF_PARD << OOO_STRING_SVTOOLS_RTF_INTBL;
-        for( nBox = 0; nBox < nColCnt; ++nBox )
+        for( nBox = 0; nBox < nBoxes; ++nBox )
         {
-            if( nBox && pBoxArr[ nBox-1 ] == pBoxArr[ nBox ] )
+            SwWriteTableCell * pCell = pBoxArr[nBox];
+
+            if( (nBox && pBoxArr[ nBox-1 ] == pBoxArr[ nBox ]) || pCell == NULL)
                 continue;
 
-            if( pBoxArr[ nBox ]->GetRowSpan() == pRowSpans[ nBox ] )
+            if( pCell->GetRowSpan() == pRowSpans[ nBox ] )
             {
                 // new Box
-                const SwStartNode* pSttNd = pBoxArr[ nBox ]->GetBox()->GetSttNd();
+                const SwStartNode* pSttNd = pCell->GetBox()->GetSttNd();
                 RTFSaveData aSaveData( rRTFWrt,
                         pSttNd->GetIndex()+1, pSttNd->EndOfSectionIndex() );
                 rRTFWrt.bOutTable = TRUE;

sw/source/filter/rtf/rtffly.cxx
@@ -263,20 +263,26 @@ void SwRTFParser::SetFlysInDoc()
                 pSttNd->GetIndex() + 1 == pEndNd->GetIndex()
                 && pSttNd->GetTxt().Len()>0 /* #i38227# leave drop caps with no content as fly frames */ )
             {
+                ULONG nPos = pSttNd->GetIndex();
+                SwDoc * pDoc1 = pSttNd->GetDoc();
+
                 BOOL bJoined;
                 {
-                    SwPaM aTmp( *pEndNd, 0, *pSttNd, pSttNd->GetTxt().Len() );
-                    bJoined = pDoc->DeleteAndJoin( aTmp );
+                    SwPaM aTmp( *pSttNd, pSttNd->GetTxt().Len(), *pEndNd, 0 );
+                    bJoined = pDoc1->DeleteAndJoin( aTmp );
                 }
-                if( bJoined )
+
+                SwTxtNode * pNd = (pDoc1->GetNodes()[nPos])->GetTxtNode();
+
+                if( bJoined && pNd != NULL)
                 {
                     SwFmtDrop aDropCap;
                     aDropCap.GetLines() = (BYTE)pFlySave->nDropLines;
                     aDropCap.GetChars() = 1;
 
                     SwIndex aIdx( pEndNd );
-                    pEndNd->RstAttr( aIdx, 1, RES_CHRATR_FONTSIZE );
-                    pEndNd->SetAttr( aDropCap );
+                    pNd->RstAttr( aIdx, 1, RES_CHRATR_FONTSIZE );
+                    pNd->SetAttr( aDropCap );
                 }
                 delete pFlySave;
                 continue;

sw/source/filter/rtf/swparrtf.cxx
@@ -1948,7 +1948,9 @@ void SwRTFParser::NextToken( int nToken )
         break;
 
     case RTF_REVDTTM:
-        pRedlineInsert->aStamp = sw::ms::DTTM2DateTime(nTokenValue);
+        if (pRedlineInsert != NULL)
+            pRedlineInsert->aStamp = sw::ms::DTTM2DateTime(nTokenValue);
+
         break;
 
     case RTF_REVDTTMDEL:

sw/source/filter/ww1/fltshell.cxx
@@ -33,6 +33,7 @@
 
 #include <ctype.h>
 #include <hintids.hxx>
+#include <hints.hxx>
 
 #ifndef _GRAPH_HXX //autogen
 #include <vcl/graph.hxx>
@@ -175,7 +176,6 @@ BOOL SwFltStackEntry::MakeRegion(SwDoc* pDoc, SwPaM& rRegion, BOOL bCheck )
     else
         return TRUE;
 }
-
 
 SwFltControlStack::SwFltControlStack(SwDoc* pDo, ULONG nFieldFl)
     : nFieldFlags(nFieldFl), pDoc(pDo), bIsEndStack(false)
@@ -427,16 +427,20 @@ void SwFltControlStack::SetAttrInDoc(const SwPosition& rTmpPos, SwFltStackEntry*
     {
     case RES_FLTR_ANCHOR:
         {
-            MakePoint(pEntry, pDoc, aRegion);
             SwFrmFmt* pFmt = ((SwFltAnchor*)pEntry->pAttr)->GetFrmFmt();
-            SwFmtAnchor aAnchor(pFmt->GetAnchor());
-            aAnchor.SetAnchor(aRegion.GetPoint());
-            pFmt->SetFmtAttr(aAnchor);
-                    // Damit die Frames bei Einfuegen in existierendes Doc
-                    //  erzeugt werden (erst nach Setzen des Ankers!):
-            if(pDoc->GetRootFrm()
-                && FLY_AT_CNTNT == pFmt->GetAnchor().GetAnchorId()){
-                pFmt->MakeFrms();
+            if (pFmt != NULL)
+            {
+                MakePoint(pEntry, pDoc, aRegion);
+                SwFmtAnchor aAnchor(pFmt->GetAnchor());
+                aAnchor.SetAnchor(aRegion.GetPoint());
+                pFmt->SetFmtAttr(aAnchor);
+                // Damit die Frames bei Einfuegen in existierendes Doc
+                //  erzeugt werden (erst nach Setzen des Ankers!):
+                if(pDoc->GetRootFrm()
+                   && FLY_AT_CNTNT == pFmt->GetAnchor().GetAnchorId())
+                {
+                    pFmt->MakeFrms();
+                }
             }
         }
         break;
@@ -728,11 +732,35 @@ void SwFltControlStack::Delete(const SwPaM &rPam)
 SwFltAnchor::SwFltAnchor(SwFrmFmt* pFmt) :
     SfxPoolItem(RES_FLTR_ANCHOR), pFrmFmt(pFmt)
 {
+    pClient = new SwFltAnchorClient(this);
+    pFrmFmt->Add(pClient);
 }
 
 SwFltAnchor::SwFltAnchor(const SwFltAnchor& rCpy) :
     SfxPoolItem(RES_FLTR_ANCHOR), pFrmFmt(rCpy.pFrmFmt)
 {
+    pClient = new SwFltAnchorClient(this);
+    pFrmFmt->Add(pClient);
+}
+
+SwFltAnchor::~SwFltAnchor()
+{
+    delete pClient;
+}
+
+void SwFltAnchor::SetFrmFmt(SwFrmFmt * _pFrmFmt)
+{
+    pFrmFmt = _pFrmFmt;
+}
+
+const SwFrmFmt * SwFltAnchor::GetFrmFmt() const
+{
+    return pFrmFmt;
+}
+
+SwFrmFmt * SwFltAnchor::GetFrmFmt()
+{
+    return pFrmFmt;
 }
 
 int SwFltAnchor::operator==(const SfxPoolItem& rItem) const
@@ -743,6 +771,29 @@ int SwFltAnchor::operator==(const SfxPoolItem& rItem) const
 SfxPoolItem* __EXPORT SwFltAnchor::Clone(SfxItemPool*) const
 {
     return new SwFltAnchor(*this);
+}
+
+// SwFltAnchorClient
+
+SwFltAnchorClient::SwFltAnchorClient(SwFltAnchor * pFltAnchor)
+: m_pFltAnchor(pFltAnchor)
+{
+}
+
+void  SwFltAnchorClient::Modify(SfxPoolItem *, SfxPoolItem * pNew)
+{
+    if (pNew->Which() == RES_FMT_CHG)
+    {
+        SwFmtChg * pFmtChg = dynamic_cast<SwFmtChg *> (pNew);
+
+        if (pFmtChg != NULL)
+        {
+            SwFrmFmt * pFrmFmt = dynamic_cast<SwFrmFmt *> (pFmtChg->pChangedFmt);
+
+            if (pFrmFmt != NULL)
+                m_pFltAnchor->SetFrmFmt(pFrmFmt);
+        }
+    }
 }
 
 //------ hier stehen die Methoden von SwFltRedline -----------

sw/source/filter/ww8/wrtw8num.cxx
@@ -343,9 +343,23 @@ void MSWordExportBase::AbstractNumberingDefinitions()
             {
                 switch ( rFmt.GetLabelFollowedBy() )
                 {
-                    case SvxNumberFormat::LISTTAB: nFollow = 0; break;
-                    case SvxNumberFormat::SPACE:   nFollow = 1; break;
-                    case SvxNumberFormat::NOTHING: nFollow = 2; break;
+                    case SvxNumberFormat::LISTTAB:
+                    {
+            // 0 (tab) unless there would be no content before the tab, in which case 2 (nothing)
+            nFollow = (SVX_NUM_NUMBER_NONE != rFmt.GetNumberingType()) ? 0 : 2;
+                    }
+                    break;
+                    case SvxNumberFormat::SPACE:
+                    {
+            // 1 (space) unless there would be no content before the space in which case 2 (nothing)
+            nFollow = (SVX_NUM_NUMBER_NONE != rFmt.GetNumberingType()) ? 1 : 2;
+                    }
+                    break;
+                    case SvxNumberFormat::NOTHING:
+                    {
+                        nFollow = 2;
+                    }
+                    break;
                     default:
                     {
                         nFollow = 0;

commit 4144cd5a851466778004af9de98dbcfb019067bb

fdo#42144 WW8 export: avoid writing pointless empty footers
Regression from 1348288905ffa75d3634eee4392ba4660d28cdb9 (mismerge, I
guess), when header was enabled, actually empty footer was exported,
even in case footer was disabled.

Change-Id: Ied55dc2a9f0faef87689c07935a47db384d37e1a

sw/source/filter/ww8/wrtw8sty.cxx
@@ -1183,7 +1183,7 @@ void WW8_WrPlcSepx::OutHeaderFooter( WW8Export& rWrt, bool bHeader,
     else if ( rWrt.bWrtWW8 )
     {
         pTxtPos->Append( rCpPos );
-        if (rWrt.bHasHdr && nBreakCode!=0)
+        if ((bHeader? rWrt.bHasHdr : rWrt.bHasFtr) && nBreakCode!=0)
         {
             rWrt.WriteStringAsPara( aEmptyStr ); // Empty paragraph for empty header/footer
             rWrt.WriteStringAsPara( aEmptyStr ); // a CR that WW8 needs for end of the stream

sw/qa/extras/ww8export/ww8export.cxx
@@ -25,6 +25,7 @@ public:
     void testZoom();
     void test56513();
     void testNewPageStylesTable();
+    void testFdo42144();
 
     CPPUNIT_TEST_SUITE(Test);
 #if !defined(MACOSX) && !defined(WNT)
@@ -46,6 +47,7 @@ void Test::run()
         {"zoom.doc", &Test::testZoom},
         {"fdo56513.doc", &Test::test56513},
         {"new-page-styles.doc", &Test::testNewPageStylesTable},
+        {"fdo42144.odt", &Test::testFdo42144},
     };
     header();
     for (unsigned int i = 0; i < SAL_N_ELEMENTS(aMethods); ++i)
@@ -138,6 +140,13 @@ void Test::testNewPageStylesTable()
     CPPUNIT_ASSERT_EQUAL(OUString("Sigma Space Performance Goals and Results (Page 1)*"),  parseDump("/root/page[1]/header/txt/text()"));
     CPPUNIT_ASSERT_EQUAL(OUString("Sigma Space Performance Assessment (Page 2)****"),   parseDump("/root/page[2]/header/txt/text()"));
     CPPUNIT_ASSERT_EQUAL(OUString("Sigma Space Performance Goals: Next Year (Page 3)*******"),  parseDump("/root/page[3]/header/txt/text()"));
+}
+
+void Test::testFdo42144()
+{
+    // Footer wasn't disabled -- instead empty footer was exported.
+    uno::Reference<beans::XPropertySet> xStyle(getStyles("PageStyles")->getByName(DEFAULT_STYLE), uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(false, bool(getProperty<sal_Bool>(xStyle, "FooterIsOn")));
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(Test);