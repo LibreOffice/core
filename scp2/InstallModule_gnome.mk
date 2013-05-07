30538b9b4d958d07134ad20fe7e4dfd4458ea6bd
Marcos Paulo de Souza <marcos.souza.org@gmail.com>
2013-05-07 12:56:04 -0300

    fdo#60924: autoinstall gnome libraries
    Change-Id: I0fd872d15a0c938d32aac185c3c00c5fb14a091a

--- scp2/InstallModule_gnome.mk
+++ scp2/InstallModule_gnome.mk
@@ -11,21 +11,6 @@ $(eval $(call gb_InstallModule_InstallModule,scp2/gnome))
 
 $(eval $(call gb_InstallModule_use_auto_install_libs,scp2/gnome,gnome))
 
-$(eval $(call gb_InstallModule_define_if_set,scp2/gnome,\
-	ENABLE_EVOAB2 \
-	ENABLE_GCONF \
-	ENABLE_GIO \
-	ENABLE_GNOMEVFS \
-	ENABLE_GTK \
-	ENABLE_GTK3 \
-	ENABLE_LOCKDOWN \
-	ENABLE_SYSTRAY_GTK \
-))
-
-$(eval $(call gb_InstallModule_add_scpfiles,scp2/gnome,\
-    scp2/source/gnome/file_gnome \
-))
-
 $(eval $(call gb_InstallModule_add_localized_scpfiles,scp2/gnome,\
     scp2/source/gnome/module_gnome \
 ))
