LibreOffice/core

commit 45a420b2f5ea1765ca3da3f31a4b357e292e0423

gbuild: do not use ' quotes when echoing dependencies
In AllLangResTarget the gb_Helper_abbreviate_dirs replaces the paths in
the echo command with $W, which the shell then does not expand due to '
quotes.  Mixing gb_Helper_abbreviate_dirs and ' is unsafe.

Change-Id: Ia99d185917a55486d5fd67f82d5b323a05e3f5bd

solenv/gbuild/PrecompiledHeaders.mk
@@ -43,12 +43,12 @@ endif
 $(call gb_PrecompiledHeader_get_dep_target,%) :
 	$(call gb_Helper_abbreviate_dirs,\
 		mkdir -p $(dir $@) && \
-		echo '$(call gb_PrecompiledHeader_get_target,$*) : $$(gb_Helper_PHONY)' > $@)
+		echo "$(call gb_PrecompiledHeader_get_target,$*) : $$(gb_Helper_PHONY)" > $@)
 
 $(call gb_NoexPrecompiledHeader_get_dep_target,%) :
 	$(call gb_Helper_abbreviate_dirs,\
 		mkdir -p $(dir $@) && \
-		echo '$(call gb_NoexPrecompiledHeader_get_target,$*) : $$(gb_Helper_PHONY)' > $@)
+		echo "$(call gb_NoexPrecompiledHeader_get_target,$*) : $$(gb_Helper_PHONY)" > $@)
 
 $(call gb_PrecompiledHeader_get_target,%) :
 	$(call gb_PrecompiledHeader__command,$@,$*,$<,$(PCH_DEFS),$(PCH_CXXFLAGS) $(gb_PrecompiledHeader_EXCEPTIONFLAGS),$(INCLUDE))

solenv/gbuild/AllLangResTarget.mk
@@ -112,7 +112,7 @@ ifeq ($(gb_FULLDEPS),$(true))
 $(call gb_SrsPartTarget_get_dep_target,%) : $(SRCDIR)/% $(gb_Helper_MISCDUMMY)
 	$(call gb_Helper_abbreviate_dirs,\
 		mkdir -p $(dir $@) && \
-		echo '$(call gb_SrsPartTarget_get_target,$*) : $(gb_Helper_PHONY)' > $@)
+		echo "$(call gb_SrsPartTarget_get_target,$*) : $(gb_Helper_PHONY)" > $@)
 endif
 
 

solenv/gbuild/LinkTarget.mk
@@ -114,7 +114,7 @@ endef
 ifeq ($(gb_FULLDEPS),$(true))
 define gb_Object__command_dep
 mkdir -p $(dir $(1)) && \
-	echo '$(2) : $$(gb_Helper_PHONY)' > $(1)
+	echo "$(2) : $$(gb_Helper_PHONY)" > $(1)
 
 endef
 else

commit 0f0b250a481b674824b4258bbe44bc1521b0d680

gbuild: PrecompiledHeaders: fix badly quoted gb_Helper_PHONY
(regression from 45a420b2f5ea1765ca3da3f31a4b357e292e0423)

Change-Id: Iec3e5f1d9b26fc093377a8e0cb61f7943c610611

solenv/gbuild/PrecompiledHeaders.mk
@@ -43,12 +43,12 @@ endif
 $(call gb_PrecompiledHeader_get_dep_target,%) :
 	$(call gb_Helper_abbreviate_dirs,\
 		mkdir -p $(dir $@) && \
-		echo "$(call gb_PrecompiledHeader_get_target,$*) : $$(gb_Helper_PHONY)" > $@)
+		echo "$(call gb_PrecompiledHeader_get_target,$*) : $(gb_Helper_PHONY)" > $@)
 
 $(call gb_NoexPrecompiledHeader_get_dep_target,%) :
 	$(call gb_Helper_abbreviate_dirs,\
 		mkdir -p $(dir $@) && \
-		echo "$(call gb_NoexPrecompiledHeader_get_target,$*) : $$(gb_Helper_PHONY)" > $@)
+		echo "$(call gb_NoexPrecompiledHeader_get_target,$*) : $(gb_Helper_PHONY)" > $@)
 
 $(call gb_PrecompiledHeader_get_target,%) :
 	$(call gb_PrecompiledHeader__command,$@,$*,$<,$(PCH_DEFS),$(PCH_CXXFLAGS) $(gb_PrecompiledHeader_EXCEPTIONFLAGS),$(INCLUDE))