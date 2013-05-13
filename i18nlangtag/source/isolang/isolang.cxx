ec97c59ad376e895c1015e04f638804ce71077a6
Andras Timar <atimar@suse.com>
2013-05-13 13:31:07 +0200

    added Udmurt [udm-RU] to language list
    Change-Id: Ief2413fda7ecbf4cc73bc0d2bf483f9a6c16cca8

--- i18nlangtag/source/isolang/isolang.cxx
+++ i18nlangtag/source/isolang/isolang.cxx
@@ -512,6 +512,7 @@ static MsLangId::IsoLangEntry const aImplIsoLangEntries[] =
     { LANGUAGE_USER_YOMBE_CONGO,           "yom", "CG" },
     { LANGUAGE_USER_SIDAMA,                "sid", "ET" },
     { LANGUAGE_USER_NKO,                   "nqo", "GN" },
+    { LANGUAGE_USER_UDMURT,                "udm", "RU" },
     { LANGUAGE_MULTIPLE,                   "mul", ""   },   // multiple languages, many languages are used
     { LANGUAGE_UNDETERMINED,               "und", ""   },   // undetermined language, language cannot be identified
     { LANGUAGE_NONE,                       "zxx", ""   },   // added to ISO 639-2 on 2006-01-11: Used to declare the absence of linguistic information
--- include/i18nlangtag/lang.h
+++ include/i18nlangtag/lang.h
@@ -555,6 +555,7 @@ typedef unsigned short LanguageType;
 #define LANGUAGE_USER_YOMBE_CONGO           0x8284  /* makeLangID( 0x20, getPrimaryLanguage( LANGUAGE_USER_YOMBE)) */
 #define LANGUAGE_USER_SIDAMA                0x0685
 #define LANGUAGE_USER_NKO                   0x0686
+#define LANGUAGE_USER_UDMURT                0x0687
 
 #define LANGUAGE_USER_PRIV_JOKER            0xFFEB  /* privateuse "*" (sic! bad! nasty!), primary 0x3eb, sub 0x3f */
 #define LANGUAGE_USER_PRIV_COMMENT          0xFFEC  /* privateuse "x-comment", primary 0x3ec, sub 0x3f */
