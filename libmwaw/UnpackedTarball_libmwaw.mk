edab1a30fcb159347ee6034fefcfd0773aa5f335
Fridrich Štrba <fridrich.strba@bluewin.ch>
2013-05-14 16:56:23 +0200

    trying to unbreak windows build
    Change-Id: I3db28992dc3d3bbe63d8679d5a43bab11a66bdcd

--- libmwaw/UnpackedTarball_libmwaw.mk
+++ libmwaw/UnpackedTarball_libmwaw.mk
@@ -11,4 +11,8 @@ $(eval $(call gb_UnpackedTarball_UnpackedTarball,libmwaw))
 
 $(eval $(call gb_UnpackedTarball_set_tarball,libmwaw,$(MWAW_TARBALL)))
 
+$(eval $(call gb_UnpackedTarball_add_patches,libmwaw,\
+       libmwaw/libmwaw-0.1.9.patch.1 \
+))
+
 # vim: set noet sw=4 ts=4:
