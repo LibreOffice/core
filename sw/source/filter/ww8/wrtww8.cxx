LibreOffice/core

commit cbba191cf0fff148935f24d111cb124bd5a5328b

hb33issues01: #b6951450# export table spacing

sw/source/filter/ww8/wrtww8.cxx
@@ -58,10 +58,12 @@
 #include <filter/msfilter/msoleexp.hxx>
 #include <filter/msfilter/msocximex.hxx>
 #include <editeng/lrspitem.hxx>
+#include <editeng/ulspitem.hxx>
 #include <editeng/boxitem.hxx>
 #include <editeng/brshitem.hxx>
 #include <swtypes.hxx>
 #include <swrect.hxx>
+#include <swtblfmt.hxx>
 #include <txatbase.hxx>
 #include <fmtcntnt.hxx>
 #include <fmtpdsc.hxx>
@@ -1910,6 +1912,7 @@ void WW8AttributeOutput::TableInfoRow( ww8::WW8TableNodeInfoInner::Pointer_t pTa
             TableBidi( pTableTextNodeInfoInner );
             TableVerticalCell( pTableTextNodeInfoInner );
             TableOrientation( pTableTextNodeInfoInner );
+            TableSpacing( pTableTextNodeInfoInner );
         }
     }
 }
@@ -2087,6 +2090,41 @@ void WW8AttributeOutput::TableOrientation( ww8::WW8TableNodeInfoInner::Pointer_t
                 break;
             default:
                 break;
+        }
+    }
+}
+
+void WW8AttributeOutput::TableSpacing(ww8::WW8TableNodeInfoInner::Pointer_t pTableTextNodeInfoInner)
+{
+    const SwTable * pTable = pTableTextNodeInfoInner->getTable();
+    const SwTableFmt * pTableFmt = dynamic_cast<const SwTableFmt *>(pTable->GetRegisteredIn());
+
+    if (pTableFmt != NULL)
+    {
+        const SvxULSpaceItem & rUL = pTableFmt->GetULSpace();
+
+        if (rUL.GetUpper() > 0)
+        {
+            sal_uInt8 nPadding = 2;
+            sal_uInt8 nPcVert = 0;
+            sal_uInt8 nPcHorz = 0;
+
+            sal_uInt8 nTPc = (nPadding << 4) | (nPcVert << 2) | nPcHorz;
+
+            m_rWW8Export.InsUInt16(NS_sprm::LN_TPc);
+            m_rWW8Export.pO->Insert( nTPc, m_rWW8Export.pO->Count() );
+
+            m_rWW8Export.InsUInt16(NS_sprm::LN_TDyaAbs);
+            m_rWW8Export.InsUInt16(rUL.GetUpper());
+
+            m_rWW8Export.InsUInt16(NS_sprm::LN_TDyaFromText);
+            m_rWW8Export.InsUInt16(rUL.GetUpper());
+        }
+
+        if (rUL.GetLower() > 0)
+        {
+            m_rWW8Export.InsUInt16(NS_sprm::LN_TDyaFromTextBottom);
+            m_rWW8Export.InsUInt16(rUL.GetLower());
         }
     }
 }

sw/source/filter/ww8/attributeoutputbase.hxx
@@ -232,6 +232,8 @@ public:
 
     virtual void TableOrientation( ww8::WW8TableNodeInfoInner::Pointer_t pTableTextNodeInfoInner ) = 0;
 
+    virtual void TableSpacing( ww8::WW8TableNodeInfoInner::Pointer_t pTableTextNodeInfoInner ) = 0;
+
     virtual void TableRowEnd( sal_uInt32 nDepth ) = 0;
 
     /// Start of the styles table.

sw/source/filter/ww8/docxattributeoutput.cxx
@@ -1460,6 +1460,13 @@ void DocxAttributeOutput::TableOrientation( ww8::WW8TableNodeInfoInner::Pointer_
 #endif
 }
 
+void DocxAttributeOutput::TableSpacing( ww8::WW8TableNodeInfoInner::Pointer_t /*pTableTextNodeInfoInner*/ )
+{
+#if OSL_DEBUG_LEVEL > 0
+    fprintf( stderr, "TODO: DocxAttributeOutput::TableSpacing( ww8::WW8TableNodeInfoInner::Pointer_t pTableTextNodeInfoInner )\n" );
+#endif
+}
+
 void DocxAttributeOutput::TableRowEnd( sal_uInt32 /*nDepth*/ )
 {
 #if OSL_DEBUG_LEVEL > 0

sw/source/filter/ww8/ww8attributeoutput.hxx
@@ -124,6 +124,8 @@ public:
 
     virtual void TableOrientation( ww8::WW8TableNodeInfoInner::Pointer_t pTableTextNodeInfoInner );
 
+    virtual void TableSpacing( ww8::WW8TableNodeInfoInner::Pointer_t pTableTextNodeInfoInner );
+
     virtual void TableRowEnd( sal_uInt32 nDepth = 1 );
 
     /// Start of the styles table.

commit 335c264d508a9c73845b94483475435a0cebfe64

fdo#36876 WW8 filter: fix export of table margins
Actually no testcase, as the export bug does not affect our import
filter. The problem was that we wrote SPRM's, which are properties of
floating tables, so once the user edited the resulting document in Word
and imported it back to Writer, frames around tables got created.  Fix
this by only writing floating table SPRM's for tables which are already
in a fly frame.

Regression from cbba191cf0fff148935f24d111cb124bd5a5328b.

Change-Id: Ifeae93100ca80dcde34fc79f69a91064d44ea8f4

sw/source/filter/ww8/wrtww8.cxx
@@ -2158,7 +2158,9 @@ void WW8AttributeOutput::TableSpacing(ww8::WW8TableNodeInfoInner::Pointer_t pTab
     const SwTable * pTable = pTableTextNodeInfoInner->getTable();
     const SwTableFmt * pTableFmt = pTable->GetTableFmt();
 
-    if (pTableFmt != NULL)
+    // Writing these SPRM's will make the table a floating one, so only write
+    // them in case the table is already inside a frame.
+    if (pTableFmt != NULL /*&& pTable->GetTableNode()->GetFlyFmt()*/)
     {
         const SvxULSpaceItem & rUL = pTableFmt->GetULSpace();
 