2fcf944788eb39550a4e3079cf80568c1b35960c
Jan Holesovsky <kendy@suse.cz>
2013-05-15 16:10:40 +0200

    bnc#819614: Test for the runaway lines fix.
    Change-Id: I60c072720392822eb5866310f3607271d72815b7

--- sd/qa/unit/import-tests.cxx
+++ sd/qa/unit/import-tests.cxx
@@ -54,7 +54,8 @@ void SdFiltersTest::testDocumentLayout()
         { "odp/shapes-test.odp", "xml/shapes-test_page" },
         { "pptx/fdo47434-all.pptx", "pptx/xml/fdo47434_page" },
         { "n758621.ppt", "xml/n758621_" },
-        { "fdo64586.ppt", "xml/fdo64586_" }
+        { "fdo64586.ppt", "xml/fdo64586_" },
+        { "n819614.pptx", "xml/n819614_" },
     };
 
     for ( int i = 0; i < static_cast< int >( SAL_N_ELEMENTS( aFilesToCompare ) ); ++i )
