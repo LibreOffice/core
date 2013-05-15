3baa1c8c9baf6bb35f9d2ff5e002a5403d2d4af4
Tor Lillqvist <tlillqvist@suse.com>
2013-05-15 12:07:16 +0300

    I almost sense a pattern, except that AR is not needed here for some reason
    Change-Id: I260304559eec7b92add33a758623bc039c2e2d3a

--- libodfgen/ExternalProject_libodfgen.mk
+++ libodfgen/ExternalProject_libodfgen.mk
@@ -33,6 +33,7 @@ $(call gb_ExternalProject_get_state_target,libodfgen,build) :
 			--disable-weffc \
 			CXXFLAGS="$(if $(filter NO,$(SYSTEM_BOOST)),-I$(call gb_UnpackedTarball_get_dir,boost),$(BOOST_CPPFLAGS))" \
 			$(if $(filter YES,$(CROSS_COMPILING)),--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM)) \
+			$(if $(filter MSC,$(COM)),SED=sed.exe) \
 		&& (cd $(EXTERNAL_WORKDIR)/src && $(MAKE)) \
 	)
 
