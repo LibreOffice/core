ab808b1e43bae74c4613f2ce25b280d2b900e64a
Eike Rathke <erack@redhat.com>
2013-05-17 17:23:26 +0200

    obtain days in month from non-normalized date, fdo#63805 related
    Change-Id: I05cefb96d7d6290ff7e1a89fa88b21da7a6c82a2
    (cherry picked from commit 59d6bde4045e928b5d68fb23e3340fa0e48f3e33)

--- vcl/source/control/field2.cxx
+++ vcl/source/control/field2.cxx
@@ -1387,7 +1387,7 @@ static void ImplDateIncrementMonth( Date& rDate, sal_Bool bUp )
         }
     }
 
-    sal_uInt16 nDaysInMonth = rDate.GetDaysInMonth();
+    sal_uInt16 nDaysInMonth = Date::GetDaysInMonth( rDate.GetMonth(), rDate.GetYear());
     if ( rDate.GetDay() > nDaysInMonth )
         rDate.SetDay( nDaysInMonth );
 }
