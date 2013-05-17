c04dc880044ca84b80bb13ac900de84ceae2a561
Michael Stahl <mstahl@redhat.com>
2013-05-17 14:02:25 +0200

    fdo#57079: SvxSuperContourDlg: unregister configuration listener
    ... in destructor.  Otherwise it will access freed memory when
    eventually called.  This crashes since OOo 3.2 but no idea why it would
    appear to not crash in earlier versions.
    
    Change-Id: Ie3ab7ddf19fb1086f0b7d824555ab6b063e2b9ad
    (cherry picked from commit fc63700181ab0f42eab5f98328074fe1682344f8)
    Reviewed-on: https://gerrit.libreoffice.org/3941
    Reviewed-by: Fridrich Strba <fridrich@documentfoundation.org>
    Tested-by: Fridrich Strba <fridrich@documentfoundation.org>

--- svx/source/dialog/_contdlg.cxx
+++ svx/source/dialog/_contdlg.cxx
@@ -283,6 +283,8 @@ SvxSuperContourDlg::SvxSuperContourDlg( SfxBindings *_pBindings, SfxChildWindow 
 
 SvxSuperContourDlg::~SvxSuperContourDlg()
 {
+    SvtMiscOptions aMiscOptions;
+    aMiscOptions.RemoveListenerLink( LINK(this, SvxSuperContourDlg, MiscHdl) );
 }
 
 // Resize methods
