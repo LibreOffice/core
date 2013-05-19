5f8ed7de1c32f88f45b5cf80a1f52fd57aefa5f0
Norbert Thiebaud <nthiebaud@gmail.com>
2013-05-19 11:47:39 -0500

    Multi thread-safe version of osl_getGlobalTime
    Change-Id: Ibb9d23780600437f607d866ff3d396b96879245d

--- sal/osl/w32/salinit.cxx
+++ sal/osl/w32/salinit.cxx
@@ -31,6 +31,9 @@
 extern "C" {
 #endif
 
+//From time.c
+void sal_initGlobalTimer();
+
 // _set_invalid_parameter_handler appears unavailable with MinGW:
 #if defined _MSC_VER
 namespace {
@@ -52,6 +55,7 @@ extern "C" void invalidParameterHandler(
 
 void sal_detail_initialize(int argc, char ** argv)
 {
+    sal_initGlobalTimer();
     // SetProcessDEPPolicy(PROCESS_DEP_ENABLE);
     // SetDllDirectoryW(L"");
     // SetSearchPathMode(
