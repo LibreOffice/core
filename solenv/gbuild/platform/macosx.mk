65da24773fffea4e4bc81adfa265378064186488
Stephan Bergmann <sbergman@redhat.com>
2013-05-22 16:16:03 +0200

    Be more picky about Mac OS X linker warnings
    ...useful for cases like 913ba61d008c6629272ba3df8d22dbff0e2dc4f9 "Make liborcus
    -fvisibility setting consistent with LO one."
    
    Feel free to revert if it causes trouble, though.  (I was only able to test it
    with a 10.7 build, not a 10.6 baseline one.)
    
    Change-Id: Ic8e57cd917ef186f4dec8d876fc8da6d6db57b3d

--- solenv/gbuild/platform/macosx.mk
+++ solenv/gbuild/platform/macosx.mk
@@ -91,6 +91,7 @@ gb_OBJCXXFLAGS := -x objective-c++ -fobjc-exceptions
 gb_OBJCFLAGS := -x objective-c -fobjc-exceptions
 
 gb_LinkTarget_LDFLAGS := \
+	-Wl,-fatal_warnings \
 	$(SOLARLIB) \
 #man ld says: obsolete	-Wl,-multiply_defined,suppress \
 
