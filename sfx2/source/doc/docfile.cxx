LibreOffice/core

commit 966d20e35d5a2be2fce6c204af5c156c3ead7063

CMIS ucp: write documents back to CMIS server
This change needs a git repo build of libcmis.

Change-Id: I I740d18dbf3c36d9387b867e750fcbe8e114e5362

sfx2/source/doc/docfile.cxx
@@ -33,6 +33,7 @@
 #include <com/sun/star/task/XInteractionHandler.hpp>
 #include <com/sun/star/uno/Reference.h>
 #include <com/sun/star/ucb/XContent.hpp>
+#include <com/sun/star/container/XChild.hpp>
 #include <com/sun/star/document/XDocumentRevisionListPersistence.hpp>
 #include <com/sun/star/document/LockedDocumentRequest.hpp>
 #include <com/sun/star/document/OwnLockOnDocumentRequest.hpp>
@@ -1918,6 +1919,7 @@ void SfxMedium::Transfer_Impl()
                                                       Reference< ::com::sun::star::ucb::XProgressHandler >() );
 
         rtl::OUString aDestURL( aDest.GetMainURL( INetURLObject::NO_DECODE ) );
+
         if ( ::utl::LocalFileHelper::IsLocalFile( aDestURL ) || !aDest.removeSegment() )
         {
             TransactedTransferForFS_Impl( aSource, aDest, xComEnv );
@@ -1939,8 +1941,30 @@ void SfxMedium::Transfer_Impl()
             ::ucbhelper::Content aSourceContent;
             ::ucbhelper::Content aTransferContent;
 
-            String aFileName = GetLongName();
-            if ( !aFileName.Len() )
+            // Get the parent URL from the XChild if possible: why would the URL necessarily have
+            // a hierarchical path? It's not the case for CMIS.
+            ::ucbhelper::Content aDestContent;
+            ::ucbhelper::Content::create( aDestURL, xComEnv, aDestContent );
+            Reference< ::com::sun::star::container::XChild> xChild( aDestContent.get(), uno::UNO_QUERY );
+            rtl::OUString sParentUrl;
+            if ( xChild.is( ) )
+            {
+                Reference< ::com::sun::star::ucb::XContent > xParent( xChild->getParent( ), uno::UNO_QUERY );
+                if ( xParent.is( ) )
+                {
+                    sParentUrl = xParent->getIdentifier( )->getContentIdentifier();
+                }
+            }
+
+            if ( !sParentUrl.isEmpty() )
+                aDest = INetURLObject( sParentUrl );
+
+            // LongName wasn't defined anywhere, only used here... get the Title instead
+            // as it's less probably empty
+            rtl::OUString aFileName;
+            Any aAny = aDestContent.getPropertyValue( ::rtl::OUString(RTL_CONSTASCII_USTRINGPARAM("Title" )) );
+            aAny >>= aFileName;
+            if ( aFileName.isEmpty() )
                 aFileName = GetURLObject().getName( INetURLObject::LAST_SEGMENT, true, INetURLObject::DECODE_WITH_CHARSET );
 
             try

ucb/source/ucp/cmis/cmis_content.hxx
@@ -29,6 +29,8 @@
 #ifndef CMIS_CONTENT_HXX
 #define CMIS_CONTENT_HXX
 
+#include "cmis_url.hxx"
+
 #include <com/sun/star/io/XInputStream.hpp>
 #include <com/sun/star/io/XOutputStream.hpp>
 #include <com/sun/star/ucb/ContentCreationException.hpp>
@@ -68,9 +70,18 @@ class Content : public ::ucbhelper::ContentImplHelper, public com::sun::star::uc
 private:
     ContentProvider*       m_pProvider;
     libcmis::Session*      m_pSession;
-    libcmis::CmisObjectPtr m_pObject;
+    libcmis::ObjectPtr     m_pObject;
+    rtl::OUString          m_sObjectId;
+    rtl::OUString          m_sURL;
+    rtl::OUString          m_sBindingUrl;
+
+    // Members to be set for non-persistent content
+    bool                   m_bTransient;
+    libcmis::ObjectTypePtr m_pObjectType;
+    std::map< std::string, libcmis::PropertyPtr > m_pObjectProps;
 
     bool isFolder(const com::sun::star::uno::Reference< com::sun::star::ucb::XCommandEnvironment >& xEnv);
+    void setCmisProperty( std::string sName, std::string sValue );
 
     com::sun::star::uno::Any getBadArgExcept();
 
@@ -111,6 +122,9 @@ private:
         const com::sun::star::uno::Reference< com::sun::star::ucb::XCommandEnvironment >& xEnv );
 
     sal_Bool exchangeIdentity(const com::sun::star::uno::Reference< com::sun::star::ucb::XContentIdentifier >&  xNewId);
+
+    void resetAuthProvider( const com::sun::star::uno::Reference< com::sun::star::ucb::XCommandEnvironment >& xEnv );
+    libcmis::ObjectPtr getObject( );
 
 public:
     Content( const com::sun::star::uno::Reference<

ucb/source/ucp/cmis/cmis_provider.cxx
@@ -45,12 +45,6 @@ ContentProvider::queryContent(
     throw( com::sun::star::ucb::IllegalIdentifierException,
            uno::RuntimeException )
 {
-#if OSL_DEBUG_LEVEL > 1
-    fprintf(stderr, "QueryContent: '%s'\n",
-       rtl::OUStringToOString
-       (Identifier->getContentIdentifier(), RTL_TEXTENCODING_UTF8).getStr());
-#endif
-
     osl::MutexGuard aGuard( m_aMutex );
 
     // Check, if a content with given id already exists...
@@ -60,7 +54,8 @@ ContentProvider::queryContent(
 
     try
     {
-        xContent = new ::cmis::Content(m_xSMgr, this, Identifier);
+        xContent = new ::cmis::Content( m_xSMgr, this, Identifier );
+        registerNewContent( xContent );
     }
     catch ( com::sun::star::ucb::ContentCreationException const & )
     {
@@ -73,14 +68,36 @@ ContentProvider::queryContent(
     return xContent;
 }
 
+libcmis::Session* ContentProvider::getSession( const rtl::OUString& sBindingUrl )
+{
+    libcmis::Session* pSession = NULL;
+    std::map< rtl::OUString, libcmis::Session* >::iterator it = m_aSessionCache.find( sBindingUrl );
+    if ( it != m_aSessionCache.end( ) )
+    {
+        pSession = it->second;
+    }
+    return pSession;
+}
+
+void ContentProvider::registerSession( const rtl::OUString& sBindingUrl, libcmis::Session* pSession )
+{
+    m_aSessionCache.insert( std::pair< rtl::OUString, libcmis::Session* >( sBindingUrl, pSession ) );
+}
+
 ContentProvider::ContentProvider(
     const uno::Reference< lang::XMultiServiceFactory >& rSMgr )
 : ::ucbhelper::ContentProviderImplHelper( rSMgr )
 {
+#if OSL_DEBUG_LEVEL > 1
+    fprintf(stderr, "ContentProvider::ContentProvider( )\n" );
+#endif
 }
 
 ContentProvider::~ContentProvider()
 {
+#if OSL_DEBUG_LEVEL > 1
+    fprintf(stderr, "ContentProvider::~ContentProvider( )\n" );
+#endif
 }
 
 XINTERFACE_IMPL_3( ContentProvider,

ucb/source/ucp/cmis/cmis_provider.hxx
@@ -31,12 +31,16 @@
 
 #include <com/sun/star/beans/Property.hpp>
 #include <ucbhelper/providerhelper.hxx>
+#include <libcmis/session.hxx>
 
 namespace cmis
 {
 
 class ContentProvider : public ::ucbhelper::ContentProviderImplHelper
 {
+private:
+    std::map< rtl::OUString, libcmis::Session* > m_aSessionCache;
+
 public:
     ContentProvider( const ::com::sun::star::uno::Reference<
                         ::com::sun::star::lang::XMultiServiceFactory >& rSMgr );
@@ -58,6 +62,9 @@ public:
                     ::com::sun::star::ucb::XContentIdentifier >& Identifier )
         throw( ::com::sun::star::ucb::IllegalIdentifierException,
                ::com::sun::star::uno::RuntimeException );
+
+    libcmis::Session* getSession( const rtl::OUString& sBindingUrl );
+    void registerSession( const rtl::OUString& sBindingUrl, libcmis::Session* pSession );
 };
 
 }

ucb/source/ucp/cmis/cmis_url.cxx
@@ -27,7 +27,6 @@
  */
 
 #include <libcmis/session-factory.hxx>
-#include <tools/urlobj.hxx>
 
 #include "cmis_url.hxx"
 
@@ -38,17 +37,17 @@ using namespace std;
 
 namespace cmis
 {
-    URL::URL( rtl::OUString const & urlStr )
+    URL::URL( rtl::OUString const & urlStr ) :
+        m_aUrl( urlStr )
     {
-        INetURLObject url( urlStr );
 
-        string bindingUrl( "http://" );
-        bindingUrl += OUSTR_TO_STDSTR( url.GetHostPort( ) );
-        bindingUrl += OUSTR_TO_STDSTR( url.GetURLPath( ) );
+        rtl::OUString bindingUrl( "http://" );
+        bindingUrl += m_aUrl.GetHostPort( );
+        bindingUrl += m_aUrl.GetURLPath( );
         m_sBindingUrl = bindingUrl;
 
         // Split the query into bits and locate the repo-id key
-        rtl::OUString query = url.GetParam( );
+        rtl::OUString query = m_aUrl.GetParam( );
         while ( !query.isEmpty() )
         {
             sal_Int32 nPos = query.indexOfAsciiL( "&", 1 );
@@ -65,8 +64,8 @@ namespace cmis
             }
 
             sal_Int32 nEqPos = segment.indexOfAsciiL( "=", 1 );
-            string key = OUSTR_TO_STDSTR( segment.copy( 0, nEqPos ) );
-            string value = OUSTR_TO_STDSTR( segment.copy( nEqPos +1 ) );
+            rtl::OUString key = segment.copy( 0, nEqPos );
+            rtl::OUString value = segment.copy( nEqPos +1 );
 
             if ( key == "repo-id" )
                 m_sRepositoryId = value;
@@ -79,15 +78,39 @@ namespace cmis
     map< int, string > URL::getSessionParams( )
     {
         map< int, string > params;
-        params[ATOMPUB_URL] = m_sBindingUrl;
-        params[REPOSITORY_ID] = m_sRepositoryId;
+        params[ATOMPUB_URL] = OUSTR_TO_STDSTR( m_sBindingUrl );
+        params[REPOSITORY_ID] = OUSTR_TO_STDSTR( m_sRepositoryId );
+        params[USERNAME] = OUSTR_TO_STDSTR( m_aUrl.GetUser() );
+        params[PASSWORD] = OUSTR_TO_STDSTR( m_aUrl.GetPass() );
 
         return params;
     }
 
-    string URL::getObjectId( )
+    rtl::OUString URL::getObjectId( )
     {
         return m_aQuery["id"];
+    }
+
+    rtl::OUString URL::getBindingUrl( )
+    {
+        return m_sBindingUrl;
+    }
+
+    void URL::setObjectId( rtl::OUString sId )
+    {
+        m_aQuery["id"] = sId;
+        updateUrlQuery( );
+    }
+
+    rtl::OUString URL::asString( )
+    {
+        return m_aUrl.GetMainURL( INetURLObject::NO_DECODE );
+    }
+
+    void URL::updateUrlQuery( )
+    {
+        rtl::OUString sParam =  "repo-id=" + m_sRepositoryId + "&id=" + m_aQuery["id"];
+        m_aUrl.SetParam( sParam );
     }
 }
 

ucb/source/ucp/cmis/cmis_url.hxx
@@ -32,22 +32,32 @@
 #include <string>
 
 #include <rtl/ustring.hxx>
+#include <tools/urlobj.hxx>
 
 namespace cmis
 {
     class URL
     {
         private:
-            std::string m_sBindingUrl;
-            std::string m_sRepositoryId;
+            INetURLObject m_aUrl;
+            rtl::OUString m_sBindingUrl;
+            rtl::OUString m_sRepositoryId;
 
-            std::map< std::string, std::string > m_aQuery;
+            std::map< rtl::OUString, rtl::OUString > m_aQuery;
 
         public:
             URL( rtl::OUString const & urlStr );
 
             std::map< int, std::string > getSessionParams( );
-            std::string getObjectId( );
+            rtl::OUString getObjectId( );
+            rtl::OUString getBindingUrl( );
+            void setObjectId( rtl::OUString sId );
+
+            rtl::OUString asString( );
+
+        private:
+
+            void updateUrlQuery( );
     };
 }
 

ucbhelper/Library_ucbhelper.mk
@@ -70,6 +70,8 @@ $(eval $(call gb_Library_add_exception_objects,ucbhelper,\
 	ucbhelper/source/provider/interactionrequest \
 	ucbhelper/source/provider/providerhelper \
 	ucbhelper/source/provider/fd_inputstream \
+	ucbhelper/source/provider/std_inputstream \
+	ucbhelper/source/provider/std_outputstream \
 	ucbhelper/source/client/proxydecider \
 	ucbhelper/source/client/content \
 	ucbhelper/source/client/contentbroker \

commit 3f5c45b70864af95a6362acf4684fb57eb85e348

rhbz#961460: Don't needlessly pass URLs through INetURLObject
The WebDAV UCP uses https/davs URLs that may contain a userinfo (cf. RFC 3986)
part, and INetURLObject does not support that (in accordance with RFCs 2818 and
2616) and thus creates an empty INET_PROT_NOT_VALID INetURLObject for such a
URL, leading to failure when trying to save a document to such a URL.
(Regression introduced with 966d20e35d5a2be2fce6c204af5c156c3ead7063 "CMIS ucp:
write documents back to CMIS server.")

Change-Id: Ifd396852b211cab1d29575da7fccb32306479f93

sfx2/source/doc/docfile.cxx
@@ -1949,6 +1949,7 @@ void SfxMedium::Transfer_Impl()
 
             ::ucbhelper::Content aDestContent;
             ::ucbhelper::Content::create( aDestURL, xComEnv, comphelper::getProcessComponentContext(), aDestContent );
+            // For checkin, we need the object URL, not the parent folder:
             if ( !IsInCheckIn( ) )
             {
                 // Get the parent URL from the XChild if possible: why would the URL necessarily have
@@ -1964,13 +1965,11 @@ void SfxMedium::Transfer_Impl()
                     }
                 }
 
-                if ( !sParentUrl.isEmpty() )
-                    aDest = INetURLObject( sParentUrl );
-            }
-            else
-            {
-                // For checkin, we need the object URL, not the parent folder
-                aDest = INetURLObject( aDestURL );
+                if ( sParentUrl.isEmpty() )
+                    aDestURL = aDest.GetMainURL( INetURLObject::NO_DECODE );
+                        // adjust to above aDest.removeSegment()
+                else
+                    aDestURL = sParentUrl;
             }
 
             // LongName wasn't defined anywhere, only used here... get the Title instead
@@ -1983,7 +1982,7 @@ void SfxMedium::Transfer_Impl()
 
             try
             {
-                aTransferContent = ::ucbhelper::Content( aDest.GetMainURL( INetURLObject::NO_DECODE ), xComEnv, comphelper::getProcessComponentContext() );
+                aTransferContent = ::ucbhelper::Content( aDestURL, xComEnv, comphelper::getProcessComponentContext() );
             }
             catch (const ::com::sun::star::ucb::ContentCreationException& ex)
             {