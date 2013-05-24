87a22d6a2371d8b58f96a6a9cf23c1063ce383d7
David Tardon <dtardon@redhat.com>
2013-05-24 14:36:44 +0200

    add missing modelines
    Change-Id: I84028e254ed1bea71321da11be2027577ce4fa85

--- vcl/StaticLibrary_headless.mk
+++ vcl/StaticLibrary_headless.mk
@@ -1,3 +1,4 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
 #
 # This file is part of the LibreOffice project.
 #
@@ -9,26 +10,28 @@
 $(eval $(call gb_StaticLibrary_StaticLibrary,headless))
 
 $(eval $(call gb_StaticLibrary_use_externals,headless,\
-    boost_headers \
-    freetype_headers \
+	boost_headers \
+	freetype_headers \
 ))
 
 $(eval $(call gb_StaticLibrary_use_api,headless,\
-    offapi \
-    udkapi \
+	offapi \
+	udkapi \
 ))
 
 $(eval $(call gb_StaticLibrary_add_exception_objects,headless,\
-    vcl/headless/svpbmp \
-    vcl/headless/svpdummies \
-    vcl/headless/svpelement \
-    vcl/headless/svpframe \
-    vcl/headless/svpprn \
-    vcl/headless/svptext \
-    vcl/headless/svpvd \
+	vcl/headless/svpbmp \
+	vcl/headless/svpdummies \
+	vcl/headless/svpelement \
+	vcl/headless/svpframe \
+	vcl/headless/svpprn \
+	vcl/headless/svptext \
+	vcl/headless/svpvd \
 ))
 
 $(eval $(call gb_StaticLibrary_set_include,headless,\
-    $$(INCLUDE) \
-    -I$(SRCDIR)/vcl/inc \
+	$$(INCLUDE) \
+	-I$(SRCDIR)/vcl/inc \
 ))
+
+# vim: set noet sw=4 ts=4:
