5e0733e904d25b45a08fd410c0f94b196322cf94
Abdulaziz A Alayed <aalayed@kacst.edu.sa>
2013-05-24 13:11:31 +0300

    fdo#63254 UI:Horizontal scroll bar isn't displayed when RTL tab
    opened with LTR UI.
    
    Change-Id: Iff6899539fe0b1788df7b09f91f850b91bcdb811
    Reviewed-on: https://gerrit.libreoffice.org/4024
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>
    Tested-by: Stephan Bergmann <sbergman@redhat.com>
    (cherry picked from commit 898a3e84807bccbb876e3f5583728f6d24e05e9a)

--- vcl/unx/gtk/gdi/salnativewidgets-gtk.cxx
+++ vcl/unx/gtk/gdi/salnativewidgets-gtk.cxx
@@ -1048,8 +1048,9 @@ sal_Bool GtkSalGraphics::getNativeControlRegion(  ControlType nType,
             rNativeContentRegion.Right() = rNativeContentRegion.Left() + 1;
         if (!rNativeContentRegion.GetHeight())
             rNativeContentRegion.Bottom() = rNativeContentRegion.Top() + 1;
-
-        returnVal = true;
+        //fdo#63254 horizontal scrool bar isn't displayed in RTL tab
+        // with LTR UI in calc
+        returnVal =Application::GetSettings().GetLayoutRTL();
 
         //See fdo#44582, Horizontal scrollbar in navigator window is broken
         if ((nPart==PART_BUTTON_LEFT || nPart==PART_BUTTON_RIGHT) && Application::GetSettings().GetLayoutRTL())
