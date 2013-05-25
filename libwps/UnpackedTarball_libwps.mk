09c3c1fc45aa6e8bf8c704995fd90c06af21ea2b
Fridrich Štrba <fridrich.strba@bluewin.ch>
2013-05-25 15:36:46 +0200

    Uploading libwps 0.2.9
    Change-Id: I3307208226ffa8bc06e7b860d10e92c9a6e04758

--- libwps/UnpackedTarball_libwps.mk
+++ libwps/UnpackedTarball_libwps.mk
@@ -11,11 +11,4 @@ $(eval $(call gb_UnpackedTarball_UnpackedTarball,libwps))
 
 $(eval $(call gb_UnpackedTarball_set_tarball,libwps,$(WPS_TARBALL)))
 
-# HACK to avoid the need to deliver the headers to $(OUTDIR). This
-# should be removed when libwps is updated.
-$(eval $(call gb_UnpackedTarball_set_post_action,libwps,\
-	mkdir -p inc/libwps && \
-	cp -f src/lib/libwps.h src/lib/WPSDocument.h inc/libwps \
-))
-
 # vim: set noet sw=4 ts=4:
