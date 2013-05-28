a599e5242751057537c3de6eb58ceff2a173580e
Herbert Dürr <hdu@apache.org>
2013-05-28 09:52:00 +0000

    #i122378# avoid non-iterator bound std::transform() in namedvaluecollection.hxx
    in some build environments the use of std::transform() with plain pointers as
    input iterators results in many warnings about how unsafe such a construct is.
    The warnings could be suppressed e.g. on MSVC with the SCL_SECURE_NO_WARNINGS
    define. Open coding the construct makes it cleaner and more debugable though.

--- comphelper/inc/comphelper/namedvaluecollection.hxx
+++ comphelper/inc/comphelper/namedvaluecollection.hxx
@@ -353,12 +353,12 @@ namespace comphelper
             ::com::sun::star::uno::Sequence< VALUE_TYPE > aValues;
             *this >>= aValues;
             ::com::sun::star::uno::Sequence< ::com::sun::star::uno::Any > aWrappedValues( aValues.getLength() );
-            ::std::transform(
-                aValues.getConstArray(),
-                aValues.getConstArray() + aValues.getLength(),
-                aWrappedValues.getArray(),
-                ::com::sun::star::uno::makeAny< VALUE_TYPE >
-            );
+
+            ::com::sun::star::uno::Any* pO = aWrappedValues.getArray();
+            const VALUE_TYPE* pV = aValues.getConstArray();
+            const sal_Int32 nLen = aValues.getLength();
+            for( sal_Int32 i = 0; i < nLen; ++i )
+                *(pO++) = ::com::sun::star::uno::makeAny<VALUE_TYPE>( *(pV++) );
             return aWrappedValues;
         }
     };
