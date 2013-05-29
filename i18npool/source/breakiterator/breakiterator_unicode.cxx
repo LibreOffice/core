878b09a1a936c2d5bf4461e0123fafd377d948e2
Elie Roux <elie.roux@telecom-bretagne.eu>
2013-05-29 09:58:17 +0200

    fdo#64926 use icu's breakiterator for Tibetan and Dzongkha
    Change-Id: I78010e216eb1582fa5074666f36c95f5b0eceee5
    Reviewed-on: https://gerrit.libreoffice.org/4082
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>

--- i18npool/source/breakiterator/breakiterator_unicode.cxx
+++ i18npool/source/breakiterator/breakiterator_unicode.cxx
@@ -120,7 +120,8 @@ void SAL_CALL BreakIterator_Unicode::loadICUBreakIterator(const com::sun::star::
                 rbi = new OOoRuleBasedBreakIterator(udata_open("OpenOffice", "brk",
                     OUStringToOString(breakRules[breakType], RTL_TEXTENCODING_ASCII_US).getStr(), &status), status);
             }
-            else if (rLocale.Language != "th" && rLocale.Language != "km") //use icu's breakiterator for Thai and Khmer
+            //use icu's breakiterator for Thai, Khmer, Tibetan and Dzongkha
+            else if (rLocale.Language != "th" && rLocale.Language != "km" && rLocale.Language != "bo" && rLocale.Language != "dz")
             {
                 status = U_ZERO_ERROR;
                 OStringBuffer aUDName(64);
