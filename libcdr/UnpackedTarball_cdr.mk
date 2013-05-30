c3356d5f3437742f7bd0c162c2e7be605ed53798
Michael Stahl <mstahl@redhat.com>
2013-05-30 17:42:27 +0200

    libcdr: fix build with MSVC2008
    Change-Id: I70731e13ab4ef0d6eadd803b8f6a24f565be5346

--- libcdr/UnpackedTarball_cdr.mk
+++ libcdr/UnpackedTarball_cdr.mk
@@ -11,4 +11,8 @@ $(eval $(call gb_UnpackedTarball_UnpackedTarball,cdr))
 
 $(eval $(call gb_UnpackedTarball_set_tarball,cdr,$(CDR_TARBALL)))
 
+$(eval $(call gb_UnpackedTarball_add_patches,cdr,\
+	libcdr/msvc2008.patch.1 \
+))
+
 # vim: set noet sw=4 ts=4:
