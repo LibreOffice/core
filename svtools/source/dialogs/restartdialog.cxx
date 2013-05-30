c40202a032c9f10161f46142c6960427522466fa
Caolán McNamara <caolanm@redhat.com>
2013-05-30 20:00:46 +0100

    prompt to restart office when required for experimental features
    Change-Id: I43051ec6b0efeaebcf4a28940f70913423b8ea9f
    (cherry picked from commit 7ce2a89f4f8fa84232896766276cb3433ada239c)

--- svtools/source/dialogs/restartdialog.cxx
+++ svtools/source/dialogs/restartdialog.cxx
@@ -40,6 +40,9 @@ public:
         case svtools::RESTART_REASON_BIBLIOGRAPHY_INSTALL:
             get(reason_, "reason_bibliography_install");
             break;
+        case svtools::RESTART_REASON_MODIFIED_SIDEBAR:
+            get(reason_, "reason_sidebar");
+            break;
         default:
             assert(false); // this cannot happen
         }
