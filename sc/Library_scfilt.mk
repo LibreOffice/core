5dc41015ed10d4b4bd0eaa59e8cd6c0b1b96cc04
Stephan Bergmann <sbergman@redhat.com>
2013-05-30 14:31:25 +0200

    Clean up for consistency
    Change-Id: Ib5de2c3d22b36c906a52f882a85a44e4330edb41

--- sc/Library_scfilt.mk
+++ sc/Library_scfilt.mk
@@ -218,9 +218,9 @@ $(eval $(call gb_Library_add_exception_objects,scfilt,\
 	sc/source/filter/oox/worksheetfragment \
 	sc/source/filter/oox/worksheethelper \
 	sc/source/filter/oox/worksheetsettings \
+	sc/source/filter/orcus/interface \
 	sc/source/filter/orcus/orcusfiltersimpl \
-        sc/source/filter/orcus/interface \
-        sc/source/filter/orcus/xmlcontext \
+	sc/source/filter/orcus/xmlcontext \
 ))
 
 ifeq ($(SYSTEM_ZLIB),YES)
