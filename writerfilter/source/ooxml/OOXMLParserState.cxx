fa671cecd4bb2f38a0a9171cfdbb56efddda1cfa
Caolán McNamara <caolanm@redhat.com>
2013-06-06 11:41:03 +0100

    add some checks against NULL derefs
    Change-Id: I85fbf279991ee361ea081d92d9acc84d36677af1

--- writerfilter/source/ooxml/OOXMLParserState.cxx
+++ writerfilter/source/ooxml/OOXMLParserState.cxx
@@ -125,6 +125,9 @@ OOXMLDocument * OOXMLParserState::getDocument() const
 
 void OOXMLParserState::setXNoteId(const sal_Int32 nId)
 {
+    SAL_WARN_IF(!mpDocument, "writerfilter", "no document to set note id on");
+    if (!mpDocument)
+        return;
     mpDocument->setXNoteId(nId);
 }
 
