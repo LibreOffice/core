0cd10346f94430128beee0b4084b27837ccda6fa
Stephan Bergmann <sbergman@redhat.com>
2013-06-07 11:55:00 +0200

    fdo#65168: Fix regressions introduced with de-extension-ing report builder
    ...in 90a326c7028d5af132c62edfaef77c53627e4c0e "fdo#61950 move report builder
    from bundled extensions to plain code":
    
    * For one, ReportWizard.startReportWizard
    (wizards/com/sun/star/wizards/report/ReportWizard.java) would still try to load
    reportbuilderwizard.jar from the unpacked extension (and, failing that, fell
    back to the old default engine, so this didn't cause any failure per se).
    Instead, reference reportbuilderwizard.jar (which was also missing from scp2)
    from report.jar's manifest Class-Path (which will always be OK, even if
    reportbuilderwizard.jar is not installed) and check for the relevant class via
    Class::forName directly.
    
    * For another, the en-US/wizard/report/default.otr template had gone missing.
    The way to find it inside the unpacked extension in the past was to include a
    Paths.xcu update that extends the Template path.  Instead, install that
    template directly into share/template now, and drop the (unused already)
    Paths.xcu update.
    
    Change-Id: I09eca2b69aa55d5b15fb5ecfec6881f8a6f6e5e5
    (cherry picked from commit e57eeb6d5f547a4a49df796641141c733f6c5204)

---
+++ reportbuilder/Package_reportbuilder-templates.mk
@@ -0,0 +1,23 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+# There is currently only a single, en-US, template that needs to be included in
+# any installation set regardless of language settings, so keep this as a
+# Package instead of an AllLangPackage (whose content would need to go into a
+# language pack in scp2) for now.
+
+$(eval $(call gb_Package_Package,reportbuilder-templates,$(SRCDIR)/reportbuilder/template))
+
+$(eval $(call gb_Package_set_outdir,reportbuilder-templates,$(INSTDIR)))
+
+$(eval $(call gb_Package_add_files_with_dir,reportbuilder-templates,share/template, \
+    en-US/wizard/report/default.otr \
+))
+
+# vim: set noet sw=4 ts=4:
