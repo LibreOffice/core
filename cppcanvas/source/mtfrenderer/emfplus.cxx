203068747d1db768e9e3934d3851d685c3db9739
Fridrich Štrba <fridrich.strba@bluewin.ch>
2013-06-07 15:33:13 +0200

    Transform the clipping polygon before using it
    (cherry picked from commit abdbb847fa135dd758ef3ef99db4c07a2671ca47)
    
    Change-Id: I71a00f7d4b80ef41b637889e51a414fac6883ba1
    Signed-off-by: Thorsten Behrens <tbehrens@suse.com>

--- cppcanvas/source/mtfrenderer/emfplus.cxx
+++ cppcanvas/source/mtfrenderer/emfplus.cxx
@@ -1763,6 +1763,7 @@ namespace cppcanvas
                                                                                                                                                         mappedPoint.getX() + mappedSize.getX(),
                                                                                                                                                         mappedPoint.getY() + mappedSize.getY() ) ) ) );
 
+                        polyPolygon.transform(rState.mapModeTransform);
                         updateClipping (polyPolygon, rFactoryParms, combineMode == 1);
 
                         break;
