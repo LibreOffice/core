cd3b1847a5549e6348aa5b9b0ed1f59f679c6cf0
Fridrich Štrba <fridrich.strba@bluewin.ch>
2013-06-10 13:15:46 +0200

    Deprecated conversion to char* from string litteral
    Change-Id: I64fc36b38be59c909aa96a9faf876232caaac182

--- sd/source/ui/remotecontrol/BluetoothServer.cxx
+++ sd/source/ui/remotecontrol/BluetoothServer.cxx
@@ -829,8 +829,8 @@ void SAL_CALL BluetoothServer::run()
     WSAQUERYSET aRecord;
     memset( &aRecord, 0, sizeof(aRecord));
     aRecord.dwSize = sizeof(aRecord);
-    aRecord.lpszServiceInstanceName = "LibreOffice Impress Remote Control";
-    aRecord.lpszComment = "Remote control of presentations over bluetooth.";
+    aRecord.lpszServiceInstanceName = (char *)"LibreOffice Impress Remote Control";
+    aRecord.lpszComment = (char *)"Remote control of presentations over bluetooth.";
     aRecord.lpServiceClassId = (LPGUID) &SerialPortServiceClass_UUID;
     aRecord.dwNameSpace = NS_BTH;
     aRecord.dwNumberOfCsAddrs = 1;
