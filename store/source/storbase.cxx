008147e8056f31bcc71da0158e2593e088ffec6d
Fridrich Štrba <fridrich.strba@bluewin.ch>
2013-06-10 12:56:17 +0200

    mingw64: use the sal formatting strings in store
    Change-Id: Id6b8f4ed6df14bd80a7937001ec1cfb98acd7f52

--- store/source/storbase.cxx
+++ store/source/storbase.cxx
@@ -127,7 +127,7 @@ PageData::Allocator_Impl::initialize (sal_uInt16 nPageSize)
 {
     char name[RTL_CACHE_NAME_LENGTH + 1];
     sal_Size size = sal::static_int_cast< sal_Size >(nPageSize);
-    (void) snprintf (name, sizeof(name), "store_page_alloc_%lu", size);
+    (void) snprintf (name, sizeof(name), "store_page_alloc_%" SAL_PRIuUINTPTR, size);
 
     m_page_cache = rtl_cache_create (name, size, 0, 0, 0, 0, 0, 0, 0);
     if (!m_page_cache)
