5a0781a213c944900c5b84d3733c0b71934f817f
Caolán McNamara <caolanm@redhat.com>
2013-06-14 17:26:18 +0100

    min/max foo for msvc
    Change-Id: Icdb19eb9f8e3925f2da2d2ffe514f0da2bd14471

--- vcl/win/source/gdi/salbmp.cxx
+++ vcl/win/source/gdi/salbmp.cxx
@@ -32,6 +32,15 @@
 #include <map>
 
 #if defined _MSC_VER
+#ifndef min
+#define min(a,b)    (((a) < (b)) ? (a) : (b))
+#endif
+#ifndef max
+#define max(a,b)    (((a) > (b)) ? (a) : (b))
+#endif
+#endif
+
+#if defined _MSC_VER
 #pragma warning(push, 1)
 #endif
 
