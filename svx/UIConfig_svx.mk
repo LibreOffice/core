89a841bc6423f43964bd074e5f4f77ec6c78fe8a
Prashant Pandey <prashant3.yishu@gmail.com>
2013-06-18 18:53:19 +0530

    ParaPropertyPanel: Sidebar conversion to .ui
    Change-Id: Ib51c1499c51d9d7ca87f90622add431a8755dac6

--- svx/UIConfig_svx.mk
+++ svx/UIConfig_svx.mk
@@ -21,6 +21,7 @@ $(eval $(call gb_UIConfig_add_uifiles,svx,\
 	svx/uiconfig/ui/redlinecontrol \
 	svx/uiconfig/ui/redlinefilterpage \
 	svx/uiconfig/ui/redlineviewpage \
+	svx/uiconfig/ui/sidebarparagraph \
 	svx/uiconfig/ui/sidebartextpanel \
 ))
 
