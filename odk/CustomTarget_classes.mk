5b53053af54bb8fce2a91c0d59929c02dbb1e912
Stephan Bergmann <sbergman@redhat.com>
2013-06-19 17:18:00 +0200

    Remove unused, broken rule
    That rule was broken as it did not quote $@ which could be a pathname containing
    "$", but it is unused anyway since 41efa1535827b3dfef66ed4ce6c20e85081fe060 "use
    GeneratedPackage to deliver uno loader classes."
    
    Change-Id: I4e5a15be8e4acca3d5be0c1b228f6c324d12331c

--- odk/CustomTarget_classes.mk
+++ odk/CustomTarget_classes.mk
@@ -22,9 +22,6 @@ $(eval $(call gb_JavaClassSet_add_sourcefiles,loader,\
 	odk/source/com/sun/star/lib/loader/WinRegKeyException \
 ))
 
-$(call gb_CustomTarget_get_workdir,odk/classes)/%.class : $(call gb_CustomTarget_get_workdir,odk/classes)/java.done
-	touch $@
-
 $(call gb_CustomTarget_get_workdir,odk/classes)/java.done: $(call gb_JavaClassSet_get_target,loader)
 	$(call gb_Output_announce,$(subst $(WORKDIR)/,,$@),build,CPY,1)
 	cp -a $(call gb_JavaClassSet_get_classdir,loader)/com $(dir $@) && \
