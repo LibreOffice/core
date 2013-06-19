LibreOffice/core

commit 64e85a4cc8259a7d80c781e6698a8f30e7e0151d

Java cleanup, remove unused imports
Change-Id: I697017f196a7f7e5684011ae23941e54051c45dd

odk/examples/java/ToDo/ToDo.java
@@ -40,8 +40,6 @@ import com.sun.star.lang.XServiceInfo;
 import com.sun.star.lib.uno.helper.WeakBase;
 import com.sun.star.uno.UnoRuntime;
 import com.sun.star.uno.XComponentContext;
-import org.openoffice.*;
-
 // addintional interfaces used by the implementation
 import com.sun.star.sheet.XSpreadsheetDocument;
 import com.sun.star.sheet.XSpreadsheet;

bridges/test/testclient.java
@@ -19,7 +19,6 @@
 package test;
 
 import com.sun.star.uno.UnoRuntime;
-import com.sun.star.uno.IBridge;
 import com.sun.star.connection.XConnector;
 import com.sun.star.connection.XConnection;
 import com.sun.star.bridge.XInstanceProvider;

dbaccess/qa/complex/dbaccess/PropertyBag.java
@@ -28,7 +28,6 @@ import com.sun.star.beans.XPropertyAccess;
 import com.sun.star.beans.XPropertySet;
 import com.sun.star.beans.XPropertyContainer;
 import com.sun.star.uno.UnoRuntime;
-import com.sun.star.uno.XInterface;
 import com.sun.star.lang.XMultiServiceFactory;
 
 // ---------- junit imports -----------------

nlpsolver/ThirdParty/EvolutionarySolver/src/net/adaptivebox/global/GlobalCompare.java
@@ -12,8 +12,6 @@
 
 package net.adaptivebox.global;
 
-import java.util.*;
-
 public class GlobalCompare {
 
 /* compare the data1 and data2, if data1=data2, return 0

odk/examples/DevelopersGuide/Drawing/StyleDemo.java
@@ -44,8 +44,6 @@ import com.sun.star.beans.PropertyValue;
 import com.sun.star.beans.XPropertySet;
 import com.sun.star.beans.XPropertySetInfo;
 
-import com.sun.star.container.XNameAccess;
-
 import com.sun.star.drawing.XShape;
 import com.sun.star.drawing.XShapes;
 import com.sun.star.drawing.XDrawPage;

qadevOOo/tests/java/mod/_sc/XMLMetaExporter.java
@@ -27,7 +27,6 @@ import lib.TestParameters;
 import util.SOfficeFactory;
 import util.XMLTools;
 
-import com.sun.star.beans.XPropertySet;
 import com.sun.star.document.XDocumentPropertiesSupplier;
 import com.sun.star.document.XDocumentProperties;
 import com.sun.star.document.XExporter;

qadevOOo/tests/java/mod/_sc/XMLMetaImporter.java
@@ -26,7 +26,6 @@ import lib.TestEnvironment;
 import lib.TestParameters;
 import util.SOfficeFactory;
 
-import com.sun.star.beans.XPropertySet;
 import com.sun.star.document.XDocumentPropertiesSupplier;
 import com.sun.star.document.XDocumentProperties;
 import com.sun.star.document.XImporter;

qadevOOo/tests/java/mod/_sw/XMLMetaExporter.java
@@ -27,7 +27,6 @@ import lib.TestParameters;
 import util.SOfficeFactory;
 import util.XMLTools;
 
-import com.sun.star.beans.XPropertySet;
 import com.sun.star.document.XDocumentPropertiesSupplier;
 import com.sun.star.document.XDocumentProperties;
 import com.sun.star.document.XExporter;

commit 38d2ea2b25f323337b91ab1507bc0b955876a216

Missing import
Regression introduced with 64e85a4cc8259a7d80c781e6698a8f30e7e0151d "Java
cleanup, remove unused imports."

Change-Id: I843cf651e20d2dd410f481222aca5206e0166a30

odk/examples/java/ToDo/ToDo.java
@@ -62,7 +62,7 @@ import com.sun.star.text.XTextField;
 import com.sun.star.text.XTextRange;
 import com.sun.star.uno.UnoRuntime;
 import com.sun.star.uno.XComponentContext;
-// addintional interfaces used by the implementation
+import org.openoffice.XToDo;
 
 /** This class capsulates the class, that implements the minimal component, a
  * factory for creating the service (<CODE>__getServiceFactory</CODE>) and a