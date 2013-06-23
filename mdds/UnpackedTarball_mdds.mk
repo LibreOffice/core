4347e3b15f10784b482544bd6324d3fcd4f0146c
Kohei Yoshida <kohei.yoshida@gmail.com>
2013-06-22 21:32:07 -0400

    Adjusted the patch against mdds 0.9.0.
    Change-Id: Ia68f49996d4d50142c04d35ef2babb321afc4b3f

--- mdds/UnpackedTarball_mdds.mk
+++ mdds/UnpackedTarball_mdds.mk
@@ -15,7 +15,7 @@ $(eval $(call gb_UnpackedTarball_set_patchlevel,mdds,3))
 
 $(eval $(call gb_UnpackedTarball_add_patches,mdds,\
 	mdds/mdds_0.6.0.patch \
-	mdds/0001-Workaround-for-gcc-bug.patch \
+	mdds/0001-Workaround-for-an-old-gcc-bug.patch \
 ))
 
 # vim: set noet sw=4 ts=4:
