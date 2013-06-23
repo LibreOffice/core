LibreOffice/core

commit e717d1dcce7f8906311c5ccdbb2326b61a702630

Resolves: #i121237# Rework/Cleanup of Region code...
due to missing complete support for B2DPolygon class

(cherry picked from commit cab10eeb7878edf224a004fd7640bd4adf8d3c51)

Conflicts:
	cppcanvas/source/mtfrenderer/implrenderer.cxx
	svx/source/sdr/overlay/overlaymanagerbuffered.cxx
	svx/source/svdraw/svdpntv.cxx
	vcl/aqua/source/gdi/salgdi.cxx
	vcl/inc/region.h
	vcl/inc/unx/gtk/gtkgdi.hxx
	vcl/inc/vcl/regband.hxx
	vcl/inc/vcl/region.hxx
	vcl/os2/source/gdi/salgdi.cxx
	vcl/source/gdi/bmpacc3.cxx
	vcl/source/gdi/outdev2.cxx
	vcl/source/gdi/outmap.cxx
	vcl/source/gdi/regband.cxx
	vcl/source/gdi/region.cxx
	vcl/source/window/window.cxx
	vcl/unx/generic/gdi/pspgraphics.cxx
	vcl/unx/headless/svpgdi.cxx
	vcl/unx/headless/svppspgraphics.cxx
	vcl/win/source/gdi/salgdi.cxx

Change-Id: Iee9a66ff431c3cecb7603e445147b67715de0f7d

Remove unused variable to prevent compiler warning

(cherry picked from commit 0ac65ccf079e3e22ac23cbe7ae546504c863c31f)

Change-Id: Icbcaa9d576a7e560d96debc7360bdbe9090b3fd3

Wrong comparison with bool corrected

(cherry picked from commit 612cefdcf6176b6bb847ce899d89af40ef313a90)

Change-Id: I1cf5de6734b588f78d8e870ba7b7860634b461ce

vcl/unx/gtk/gdi/salnativewidgets-gtk.cxx
@@ -49,7 +49,7 @@ sal_Bool GtkSalGraphics::bNeedPixmapPaint = sal_False;
 GtkSalGraphics::GtkSalGraphics( GtkSalFrame *pFrame, GtkWidget *pWindow )
     : X11SalGraphics(),
       m_pWindow( pWindow ),
-      m_aClipRegion( REGION_NULL )
+      m_aClipRegion(true)
 {
     Init( pFrame, GDK_WINDOW_XID( widget_get_window( pWindow ) ),
           SalX11Screen( gdk_x11_screen_get_screen_number(
@@ -828,16 +828,29 @@ sal_Bool GtkSalGraphics::drawNativeControl(    ControlType nType,
     }
     else
     {
-        RegionHandle aHdl = aClipRegion.BeginEnumRects();
-        Rectangle aPaintRect;
-        while( aClipRegion.GetNextEnumRect( aHdl, aPaintRect ) )
+        RectangleVector aRectangles;
+        aClipRegion.GetRegionRectangles(aRectangles);
+
+        for(RectangleVector::const_iterator aRectIter(aRectangles.begin()); aRectIter != aRectangles.end(); aRectIter++)
         {
-            aPaintRect = aCtrlRect.GetIntersection( aPaintRect );
-            if( aPaintRect.IsEmpty() )
+            if(aRectIter->IsEmpty())
+            {
                 continue;
-            aClip.push_back( aPaintRect );
+            }
+
+            aClip.push_back(*aRectIter);
         }
-        aClipRegion.EndEnumRects( aHdl );
+
+        //RegionHandle aHdl = aClipRegion.BeginEnumRects();
+        //Rectangle aPaintRect;
+        //while( aClipRegion.GetEnumRects( aHdl, aPaintRect ) )
+        //{
+        //    aPaintRect = aCtrlRect.GetIntersection( aPaintRect );
+        //    if( aPaintRect.IsEmpty() )
+        //        continue;
+        //    aClip.push_back( aPaintRect );
+        //}
+        //aClipRegion.EndEnumRects( aHdl );
     }
 
     if ( (nType==CTRL_PUSHBUTTON) && (nPart==PART_ENTIRE_CONTROL) )

canvas/source/vcl/canvashelper.cxx
@@ -1231,7 +1231,7 @@ namespace vclcanvas
         // accumulate non-empty clips into one region
         // ==========================================
 
-        Region aClipRegion( REGION_NULL );
+        Region aClipRegion(true);
 
         if( viewState.Clip.is() )
         {

cppcanvas/source/mtfrenderer/implrenderer.cxx
@@ -1368,7 +1368,7 @@ namespace cppcanvas
                         }
                         else
                         {
-                            if( !pClipAction->GetRegion().HasPolyPolygon() )
+                            if( !pClipAction->GetRegion().HasPolyPolygonOrB2DPolyPolygon() )
                             {
                                 VERBOSE_TRACE( "ImplRenderer::createActions(): non-polygonal clip "
                                                "region encountered, falling back to bounding box!" );
@@ -1390,9 +1390,11 @@ namespace cppcanvas
                                 // with old one, just set it)
 
                                 // #121806# explicitly kept integer
+                                basegfx::B2DPolyPolygon aPolyPolygon(pClipAction->GetRegion().GetAsB2DPolyPolygon());
+
+                                aPolyPolygon.transform(rVDev.GetViewTransformation());
                                 updateClipping(
-                                    rVDev.LogicToPixel(
-                                        pClipAction->GetRegion().GetPolyPolygon() ).getB2DPolyPolygon(),
+                                    aPolyPolygon,
                                     rFactoryParms,
                                     false );
                             }
@@ -1422,7 +1424,7 @@ namespace cppcanvas
                     {
                         MetaISectRegionClipRegionAction* pClipAction = static_cast<MetaISectRegionClipRegionAction*>(pCurrAct);
 
-                        if( !pClipAction->GetRegion().HasPolyPolygon() )
+                        if( !pClipAction->GetRegion().HasPolyPolygonOrB2DPolyPolygon() )
                         {
                             VERBOSE_TRACE( "ImplRenderer::createActions(): non-polygonal clip "
                                            "region encountered, falling back to bounding box!" );
@@ -1442,9 +1444,11 @@ namespace cppcanvas
                             // intersect current clip with given clip polygon
 
                             // #121806# explicitly kept integer
+                            basegfx::B2DPolyPolygon aPolyPolygon(pClipAction->GetRegion().GetAsB2DPolyPolygon());
+
+                            aPolyPolygon.transform(rVDev.GetViewTransformation());
                             updateClipping(
-                                rVDev.LogicToPixel(
-                                    pClipAction->GetRegion().GetPolyPolygon() ).getB2DPolyPolygon(),
+                                aPolyPolygon,
                                 rFactoryParms,
                                 true );
                         }

drawinglayer/source/primitive2d/metafileprimitive2d.cxx
@@ -380,26 +380,8 @@ namespace
         if(!rRegion.IsEmpty())
         {
             Region aRegion(rRegion);
-            aRetval = aRegion.GetB2DPolyPolygon();
 
-            if(!aRetval.count())
-            {
-                RegionHandle aRegionHandle(aRegion.BeginEnumRects());
-                Rectangle aRegionRectangle;
-
-                while(aRegion.GetEnumRects(aRegionHandle, aRegionRectangle))
-                {
-                    if(!aRegionRectangle.IsEmpty())
-                    {
-                        const basegfx::B2DRange aRegionRange(
-                            aRegionRectangle.Left(), aRegionRectangle.Top(),
-                            aRegionRectangle.Right(), aRegionRectangle.Bottom());
-                        aRetval.append(basegfx::tools::createPolygonFromRect(aRegionRange));
-                    }
-                }
-
-                aRegion.EndEnumRects(aRegionHandle);
-            }
+            aRetval = aRegion.GetAsB2DPolyPolygon();
         }
 
         return aRetval;

filter/source/graphicfilter/eps/eps.cxx
@@ -1649,15 +1649,16 @@ void PSWriter::ImplSetClipRegion( Region& rClipRegion )
 {
     if ( !rClipRegion.IsEmpty() )
     {
-        Rectangle       aRect;
-        RegionHandle    hRegionHandle = rClipRegion.BeginEnumRects();
+        RectangleVector aRectangles;
+        rClipRegion.GetRegionRectangles(aRectangles);
 
-        while ( rClipRegion.GetNextEnumRect( hRegionHandle, aRect ) )
+        for(RectangleVector::const_iterator aRectIter(aRectangles.begin()); aRectIter != aRectangles.end(); aRectIter++)
         {
-            double nX1 = aRect.Left();
-            double nY1 = aRect.Top();
-            double nX2 = aRect.Right();
-            double nY2 = aRect.Bottom();
+            double nX1(aRectIter->Left());
+            double nY1(aRectIter->Top());
+            double nX2(aRectIter->Right());
+            double nY2(aRectIter->Bottom());
+
             ImplWriteDouble( nX1 );
             ImplWriteDouble( nY1 );
             ImplWriteByte( 'm' );
@@ -1673,8 +1674,34 @@ void PSWriter::ImplSetClipRegion( Region& rClipRegion )
             ImplWriteDouble( nX1 );
             ImplWriteDouble( nY1 );
             ImplWriteByte( 'l', PS_SPACE | PS_WRAP );
-        };
-        rClipRegion.EndEnumRects( hRegionHandle );
+        }
+
+        //Rectangle     aRect;
+        //RegionHandle  hRegionHandle = rClipRegion.BeginEnumRects();
+        //
+        //while ( rClipRegion.GetEnumRects( hRegionHandle, aRect ) )
+        //{
+        //  double nX1 = aRect.Left();
+        //  double nY1 = aRect.Top();
+        //  double nX2 = aRect.Right();
+        //  double nY2 = aRect.Bottom();
+        //  ImplWriteDouble( nX1 );
+        //  ImplWriteDouble( nY1 );
+        //  ImplWriteByte( 'm' );
+        //  ImplWriteDouble( nX2 );
+        //  ImplWriteDouble( nY1 );
+        //  ImplWriteByte( 'l' );
+        //  ImplWriteDouble( nX2 );
+        //  ImplWriteDouble( nY2 );
+        //  ImplWriteByte( 'l' );
+        //  ImplWriteDouble( nX1 );
+        //  ImplWriteDouble( nY2 );
+        //  ImplWriteByte( 'l' );
+        //  ImplWriteDouble( nX1 );
+        //  ImplWriteDouble( nY1 );
+        //  ImplWriteByte( 'l', PS_SPACE | PS_WRAP );
+        //};
+        //rClipRegion.EndEnumRects( hRegionHandle );
         ImplWriteLine( "eoclip newpath" );
     }
 }
@@ -1723,12 +1750,18 @@ void PSWriter::ImplBmp( Bitmap* pBitmap, Bitmap* pMaskBitmap, const Point & rPoi
                 aRect = Rectangle( Point( 0, nHeightOrg - nHeightLeft ), Size( (long)nWidth, (long)nHeight ) );
                 aRegion = Region( pMaskBitmap->CreateRegion( COL_BLACK, aRect ) );
 
-                if ( ( mnLevel == 1 ) && ( aRegion.GetRectCount() * 5 > 1000 ) )
+                if( mnLevel == 1 )
                 {
-                    nHeight >>= 1;
-                    if ( nHeight < 2 )
-                        return;
-                    continue;
+                    RectangleVector aRectangleVector;
+                    aRegion.GetRegionRectangles(aRectangleVector);
+
+                    if ( aRectangleVector.size() * 5 > 1000 )
+                    {
+                        nHeight >>= 1;
+                        if ( nHeight < 2 )
+                            return;
+                        continue;
+                    }
                 }
                 break;
             }
@@ -1743,26 +1776,50 @@ void PSWriter::ImplBmp( Bitmap* pBitmap, Bitmap* pMaskBitmap, const Point & rPoi
             ImplWriteLine( "gs\npum" );
             ImplTranslate( aSourcePos.X(), aSourcePos.Y() );
             ImplScale( nXWidth / nWidth,  nYHeight / nHeight );
-            RegionHandle    hRegionHandle = aRegion.BeginEnumRects();
 
-            while ( aRegion.GetNextEnumRect( hRegionHandle, aRect ) )
+            RectangleVector aRectangles;
+            aRegion.GetRegionRectangles(aRectangles);
+            const long nMoveVertical(nHeightLeft - nHeightOrg);
+
+            for(RectangleVector::iterator aRectIter(aRectangles.begin()); aRectIter != aRectangles.end(); aRectIter++)
             {
-                aRect.Move( 0, - ( nHeightOrg - nHeightLeft ) );
-                ImplWriteLong( aRect.Left() );
-                ImplWriteLong( aRect.Top() );
+                aRectIter->Move(0, nMoveVertical);
+
+                ImplWriteLong( aRectIter->Left() );
+                ImplWriteLong( aRectIter->Top() );
                 ImplWriteByte( 'm' );
-                ImplWriteLong( aRect.Right() + 1 );
-                ImplWriteLong( aRect.Top() );
+                ImplWriteLong( aRectIter->Right() + 1 );
+                ImplWriteLong( aRectIter->Top() );
                 ImplWriteByte( 'l' );
-                ImplWriteLong( aRect.Right() + 1 );
-                ImplWriteLong( aRect.Bottom() + 1 );
+                ImplWriteLong( aRectIter->Right() + 1 );
+                ImplWriteLong( aRectIter->Bottom() + 1 );
                 ImplWriteByte( 'l' );
-                ImplWriteLong( aRect.Left() );
-                ImplWriteLong( aRect.Bottom() + 1 );
+                ImplWriteLong( aRectIter->Left() );
+                ImplWriteLong( aRectIter->Bottom() + 1 );
                 ImplWriteByte( 'l' );
                 ImplWriteByte( 'p', PS_SPACE | PS_WRAP );
-            };
-            aRegion.EndEnumRects( hRegionHandle );
+            }
+
+            //RegionHandle  hRegionHandle = aRegion.BeginEnumRects();
+            //
+            //while ( aRegion.GetEnumRects( hRegionHandle, aRect ) )
+            //{
+            //  aRect.Move( 0, - ( nHeightOrg - nHeightLeft ) );
+            //  ImplWriteLong( aRect.Left() );
+            //  ImplWriteLong( aRect.Top() );
+            //  ImplWriteByte( 'm' );
+            //  ImplWriteLong( aRect.Right() + 1 );
+            //  ImplWriteLong( aRect.Top() );
+            //  ImplWriteByte( 'l' );
+            //  ImplWriteLong( aRect.Right() + 1 );
+            //  ImplWriteLong( aRect.Bottom() + 1 );
+            //  ImplWriteByte( 'l' );
+            //  ImplWriteLong( aRect.Left() );
+            //  ImplWriteLong( aRect.Bottom() + 1 );
+            //  ImplWriteByte( 'l' );
+            //  ImplWriteByte( 'p', PS_SPACE | PS_WRAP );
+            //};
+            //aRegion.EndEnumRects( hRegionHandle );
             ImplWriteLine( "eoclip newpath" );
             ImplWriteLine( "pom" );
         }

include/vcl/bmpacc.hxx
@@ -195,6 +195,12 @@ public:
     void                        FillRect( const Rectangle& rRect );
     void                        DrawRect( const Rectangle& rRect );
 
+    void                        FillPolygon( const Polygon& rPoly );
+    void                        DrawPolygon( const Polygon& rPoly );
+
+    void                        FillPolyPolygon( const PolyPolygon& rPoly );
+    void                        DrawPolyPolygon( const PolyPolygon& rPolyPoly );
+
 private:
 
     BitmapColor*                mpLineColor;

include/vcl/outdev.hxx
@@ -965,6 +965,7 @@ public:
     Rectangle           LogicToPixel( const Rectangle& rLogicRect ) const;
     Polygon             LogicToPixel( const Polygon& rLogicPoly ) const;
     PolyPolygon         LogicToPixel( const PolyPolygon& rLogicPolyPoly ) const;
+    basegfx::B2DPolyPolygon LogicToPixel( const basegfx::B2DPolyPolygon& rLogicPolyPoly ) const;
     Region              LogicToPixel( const Region& rLogicRegion )const;
     Point               LogicToPixel( const Point& rLogicPt,
                                       const MapMode& rMapMode ) const;
@@ -974,12 +975,19 @@ public:
                                       const MapMode& rMapMode ) const;
     Polygon             LogicToPixel( const Polygon& rLogicPoly,
                                       const MapMode& rMapMode ) const;
+    basegfx::B2DPolygon LogicToPixel( const basegfx::B2DPolygon& rLogicPoly,
+                                          const MapMode& rMapMode ) const;
+    PolyPolygon         LogicToPixel( const PolyPolygon& rLogicPolyPoly,
+                                      const MapMode& rMapMode ) const;
     basegfx::B2DPolyPolygon LogicToPixel( const basegfx::B2DPolyPolygon& rLogicPolyPoly,
                                           const MapMode& rMapMode ) const;
+    Region              LogicToPixel( const Region& rLogicRegion,
+                                      const MapMode& rMapMode ) const;
     Point               PixelToLogic( const Point& rDevicePt ) const;
     Size                PixelToLogic( const Size& rDeviceSize ) const;
     Rectangle           PixelToLogic( const Rectangle& rDeviceRect ) const;
     Polygon             PixelToLogic( const Polygon& rDevicePoly ) const;
+    basegfx::B2DPolygon LogicToPixel( const basegfx::B2DPolygon& rLogicPoly ) const;
     PolyPolygon         PixelToLogic( const PolyPolygon& rDevicePolyPoly ) const;
     basegfx::B2DPolyPolygon PixelToLogic( const basegfx::B2DPolyPolygon& rDevicePolyPoly ) const;
     Region              PixelToLogic( const Region& rDeviceRegion ) const;
@@ -993,8 +1001,12 @@ public:
                                       const MapMode& rMapMode ) const;
     basegfx::B2DPolygon PixelToLogic( const basegfx::B2DPolygon& rDevicePoly,
                                       const MapMode& rMapMode ) const;
+    PolyPolygon         PixelToLogic( const PolyPolygon& rDevicePolyPoly,
+                                      const MapMode& rMapMode ) const;
     basegfx::B2DPolyPolygon PixelToLogic( const basegfx::B2DPolyPolygon& rDevicePolyPoly,
                                           const MapMode& rMapMode ) const;
+    Region              PixelToLogic( const Region& rDeviceRegion,
+                                      const MapMode& rMapMode ) const;
     Point               LogicToLogic( const Point&      rPtSource,
                                       const MapMode*    pMapModeSource,
                                       const MapMode*    pMapModeDest ) const;

include/vcl/regband.hxx
@@ -49,7 +49,7 @@ struct ImplRegionBandSep
     ImplRegionBandSep*          mpNextSep;
     long                        mnXLeft;
     long                        mnXRight;
-    sal_Bool                        mbRemoved;
+    bool                        mbRemoved;
 };
 
 enum LineType { LINE_ASCENDING, LINE_DESCENDING, LINE_HORIZONTAL };
@@ -60,7 +60,7 @@ struct ImplRegionBandPoint
     ImplRegionBandPoint*        mpNextBandPoint;
     long                        mnX;
     long                        mnLineId;
-    sal_Bool                        mbEndPoint;
+    bool                        mbEndPoint;
     LineType                    meLineType;
 };
 
@@ -77,7 +77,9 @@ public:
     ImplRegionBandPoint*        mpFirstBandPoint;   // root of the list with lines
     long                        mnYTop;             // actual boundary of the band
     long                        mnYBottom;
-    sal_Bool                        mbTouched;
+
+    // bitfield
+    bool                        mbTouched : 1;
 
                                 // create y-band with boundaries
                                 ImplRegionBand( long nYTop, long nYBottom );
@@ -102,14 +104,14 @@ public:
     long                        GetXRightBoundary() const;
 
                                 // combine overlapping bands
-    sal_Bool                        OptimizeBand();
+    bool                        OptimizeBand();
 
                                 // generate separations from lines and process
                                 // union with existing separations
     void                        ProcessPoints();
                                 // insert point in the list for later processing
-    sal_Bool                        InsertPoint( long nX, long nLineID,
-                                             sal_Bool bEndPoint, LineType eLineType );
+    bool                        InsertPoint( long nX, long nLineID,
+                                             bool bEndPoint, LineType eLineType );
 
     void                        Union( long nXLeft, long nXRight );
     void                        Intersect( long nXLeft, long nXRight );
@@ -119,11 +121,14 @@ public:
     void                        MoveX( long nHorzMove );
     void                        ScaleX( double fHorzScale );
 
-    sal_Bool                        IsInside( long nX );
+    bool                        IsInside( long nX );
+    bool                        IsInside( long nLeft, long nRight );
+    bool                        IsOver( long nLeft, long nRight );
 
-    sal_Bool                        IsEmpty() const { return ((!mpFirstSep) && (!mpFirstBandPoint)); }
 
-    sal_Bool                        operator==( const ImplRegionBand& rRegionBand ) const;
+    bool                        IsEmpty() const { return ((!mpFirstSep) && (!mpFirstBandPoint)); }
+
+    bool                        operator==( const ImplRegionBand& rRegionBand ) const;
 
     /** Split the called band at the given vertical coordinate.  After the
         split the called band will cover the upper part not including nY.

commit 0e7237e76215f06f5aee8354b2a06078bf34f5dc

fix missing content of header tabs with gtk vclplug
regression since e717d1dcce7f8906311c5ccdbb2326b61a702630

Change-Id: I72e6e2f3ef093f272765036ebfc60b3f56a8fc34

vcl/unx/gtk/gdi/salnativewidgets-gtk.cxx
@@ -833,24 +833,11 @@ sal_Bool GtkSalGraphics::drawNativeControl(    ControlType nType,
 
         for(RectangleVector::const_iterator aRectIter(aRectangles.begin()); aRectIter != aRectangles.end(); ++aRectIter)
         {
-            if(aRectIter->IsEmpty())
-            {
+            Rectangle aPaintRect = aCtrlRect.GetIntersection(*aRectIter);
+            if( aPaintRect.IsEmpty() )
                 continue;
-            }
-
-            aClip.push_back(*aRectIter);
+            aClip.push_back( aPaintRect );
         }
-
-        //RegionHandle aHdl = aClipRegion.BeginEnumRects();
-        //Rectangle aPaintRect;
-        //while( aClipRegion.GetEnumRects( aHdl, aPaintRect ) )
-        //{
-        //    aPaintRect = aCtrlRect.GetIntersection( aPaintRect );
-        //    if( aPaintRect.IsEmpty() )
-        //        continue;
-        //    aClip.push_back( aPaintRect );
-        //}
-        //aClipRegion.EndEnumRects( aHdl );
     }
 
     if ( (nType==CTRL_PUSHBUTTON) && (nPart==PART_ENTIRE_CONTROL) )