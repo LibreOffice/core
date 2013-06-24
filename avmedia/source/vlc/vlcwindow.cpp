b6090460d9b596b8814d9a93e2cfa49b5b9a7dfd
Minh Ngo <nlminhtl@gmail.com>
2013-06-25 00:06:34 +0300

    Fixing source for building
    Change-Id: I0470684c8e2f043e891a58e11f498706c9a7a7f6

--- avmedia/source/vlc/vlcwindow.cpp
+++ avmedia/source/vlc/vlcwindow.cpp
@@ -1,6 +1,6 @@
 #include "vlcwindow.hxx"
 
-using namespace ::com::sun::star;
+using namespace ;
 
 namespace avmedia {
 namespace vlc {
@@ -36,6 +36,86 @@ void SAL_CALL VLCWindow::setPointerType( ::sal_Int32 SystemPointerType )
 ::uno::Sequence< ::rtl::OUString > SAL_CALL VLCWindow::getSupportedServiceNames()
 {
     return ::uno::Sequence< ::rtl::OUString >();
+}
+
+void SAL_CALL VLCWindow::dispose() throw (::uno::RuntimeException)
+{
+}
+
+void SAL_CALL VLCWindow::addEventListener( const ::uno::Reference< ::lang::XEventListener >& xListener ) throw (::uno::RuntimeException)
+{
+}
+
+void SAL_CALL VLCWindow::removeEventListener( const ::uno::Reference< :::lang::XEventListener >& aListener ) throw (::uno::RuntimeException)
+{
+}
+
+void SAL_CALL VLCWindow::setPosSize( sal_Int32 X, sal_Int32 Y, sal_Int32 Width, sal_Int32 Height, sal_Int16 Flags ) throw (::uno::RuntimeException)
+{
+}
+
+::awt::Rectangle SAL_CALL VLCWindow::getPosSize() throw (::uno::RuntimeException)
+{
+}
+
+void SAL_CALL VLCWindow::setVisible( sal_Bool Visible ) throw (::uno::RuntimeException)
+{
+}
+
+void SAL_CALL VLCWindow::setEnable( sal_Bool Enable ) throw (::uno::RuntimeException)
+{
+}
+
+void SAL_CALL VLCWindow::setFocus() throw (::uno::RuntimeException)
+{
+}
+
+void SAL_CALL VLCWindow::addWindowListener( const ::uno::Reference< ::awt::XWindowListener >& xListener ) throw (::uno::RuntimeException)
+{
+}
+
+void SAL_CALL VLCWindow::removeWindowListener( const ::uno::Reference< ::awt::XWindowListener >& xListener ) throw (::uno::RuntimeException)
+{
+}
+
+void SAL_CALL VLCWindow::addFocusListener( const ::uno::Reference< ::awt::XFocusListener >& xListener ) throw (::uno::RuntimeException)
+{
+}
+
+void SAL_CALL VLCWindow::removeFocusListener( const ::uno::Reference< ::awt::XFocusListener >& xListener ) throw (::uno::RuntimeException)
+{
+}
+
+void SAL_CALL VLCWindow::addKeyListener( const ::uno::Reference< ::awt::XKeyListener >& xListener ) throw (::uno::RuntimeException)
+{
+}
+
+void SAL_CALL VLCWindow::removeKeyListener( const ::uno::Reference< ::awt::XKeyListener >& xListener ) throw (::uno::RuntimeException)
+{
+}
+
+void SAL_CALL VLCWindow::addMouseListener( const ::uno::Reference< ::awt::XMouseListener >& xListener ) throw (::uno::RuntimeException)
+{
+}
+
+void SAL_CALL VLCWindow::removeMouseListener( const ::uno::Reference< ::awt::XMouseListener >& xListener ) throw (::uno::RuntimeException)
+{
+}
+
+void SAL_CALL VLCWindow::addMouseMotionListener( const ::uno::Reference< ::awt::XMouseMotionListener >& xListener ) throw (::uno::RuntimeException)
+{
+}
+
+void SAL_CALL VLCWindow::removeMouseMotionListener( const ::uno::Reference< ::awt::XMouseMotionListener >& xListener ) throw (::uno::RuntimeException)
+{
+}
+
+void SAL_CALL VLCWindow::addPaintListener( const ::uno::Reference< ::awt::XPaintListener >& xListener ) throw (::uno::RuntimeException)
+{
+}
+
+void SAL_CALL VLCWindow::removePaintListener( const ::uno::Reference< ::awt::XPaintListener >& xListener ) throw (::uno::RuntimeException)
+{
 }
 
 }
--- avmedia/source/vlc/vlcwindow.hxx
+++ avmedia/source/vlc/vlcwindow.hxx
@@ -36,6 +36,28 @@ public:
     ::rtl::OUString SAL_CALL getImplementationName();
     ::sal_Bool SAL_CALL supportsService( const ::rtl::OUString& ServiceName );
     ::com::sun::star::uno::Sequence< ::rtl::OUString > SAL_CALL getSupportedServiceNames();
+
+    void SAL_CALL dispose() throw (::com::sun::star::uno::RuntimeException);
+    void SAL_CALL addEventListener( const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XEventListener >& xListener ) throw (::com::sun::star::uno::RuntimeException);
+    void SAL_CALL removeEventListener( const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XEventListener >& aListener ) throw (::com::sun::star::uno::RuntimeException);
+
+    void SAL_CALL setPosSize( sal_Int32 X, sal_Int32 Y, sal_Int32 Width, sal_Int32 Height, sal_Int16 Flags ) throw (::com::sun::star::uno::RuntimeException);
+    ::com::sun::star::awt::Rectangle SAL_CALL getPosSize() throw (::com::sun::star::uno::RuntimeException);
+    void SAL_CALL setVisible( sal_Bool Visible ) throw (::com::sun::star::uno::RuntimeException);
+    void SAL_CALL setEnable( sal_Bool Enable ) throw (::com::sun::star::uno::RuntimeException);
+    void SAL_CALL setFocus() throw (::com::sun::star::uno::RuntimeException);
+    void SAL_CALL addWindowListener( const ::com::sun::star::uno::Reference< ::com::sun::star::awt::XWindowListener >& xListener ) throw (::com::sun::star::uno::RuntimeException);
+    void SAL_CALL removeWindowListener( const ::com::sun::star::uno::Reference< ::com::sun::star::awt::XWindowListener >& xListener ) throw (::com::sun::star::uno::RuntimeException);
+    void SAL_CALL addFocusListener( const ::com::sun::star::uno::Reference< ::com::sun::star::awt::XFocusListener >& xListener ) throw (::com::sun::star::uno::RuntimeException);
+    void SAL_CALL removeFocusListener( const ::com::sun::star::uno::Reference< ::com::sun::star::awt::XFocusListener >& xListener ) throw (::com::sun::star::uno::RuntimeException);
+    void SAL_CALL addKeyListener( const ::com::sun::star::uno::Reference< ::com::sun::star::awt::XKeyListener >& xListener ) throw (::com::sun::star::uno::RuntimeException);
+    void SAL_CALL removeKeyListener( const ::com::sun::star::uno::Reference< ::com::sun::star::awt::XKeyListener >& xListener ) throw (::com::sun::star::uno::RuntimeException);
+    void SAL_CALL addMouseListener( const ::com::sun::star::uno::Reference< ::com::sun::star::awt::XMouseListener >& xListener ) throw (::com::sun::star::uno::RuntimeException);
+    void SAL_CALL removeMouseListener( const ::com::sun::star::uno::Reference< ::com::sun::star::awt::XMouseListener >& xListener ) throw (::com::sun::star::uno::RuntimeException);
+    void SAL_CALL addMouseMotionListener( const ::com::sun::star::uno::Reference< ::com::sun::star::awt::XMouseMotionListener >& xListener ) throw (::com::sun::star::uno::RuntimeException);
+    void SAL_CALL removeMouseMotionListener( const ::com::sun::star::uno::Reference< ::com::sun::star::awt::XMouseMotionListener >& xListener ) throw (::com::sun::star::uno::RuntimeException);
+    void SAL_CALL addPaintListener( const ::com::sun::star::uno::Reference< ::com::sun::star::awt::XPaintListener >& xListener ) throw (::com::sun::star::uno::RuntimeException);
+    void SAL_CALL removePaintListener( const ::com::sun::star::uno::Reference< ::com::sun::star::awt::XPaintListener >& xListener ) throw (::com::sun::star::uno::RuntimeException);
 };
 
 }
