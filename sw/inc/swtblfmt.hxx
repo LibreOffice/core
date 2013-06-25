75c1eb2bb4d42129da9f37f10d74f81a5b6887f3
Alex Ivan <alexnivan@yahoo.com>
2013-06-25 18:44:36 +0300

    Add structure for format classes
    SwTableFmt now stores data about the lines/columns.
    These, in turn, store information about the boxes.
    
    Not yet integrated with the current implementation.
    
    Change-Id: Ibc936f68c20905fca1cd636977eb47850eb0faa9

--- sw/inc/swtblfmt.hxx
+++ sw/inc/swtblfmt.hxx
@@ -22,6 +22,8 @@
 #include <frmfmt.hxx>
 
 class SwDoc;
+class SwTableLineFormat;
+class SwTableBoxFormat;
 
 class SW_DLLPUBLIC SwTableFormat : public SwFrameFormat
 {
@@ -31,16 +33,66 @@ protected:
     SwTableFormat( SwAttrPool& rPool, const sal_Char* pFormatNm,
                     SwFrameFormat *pDrvdFrm )
         : SwFrameFormat( rPool, pFormatNm, pDrvdFrm, RES_FRMFMT, aTableSetRange )
-    {}
+    {
+        pFstLineFormat = NULL;
+        pLstLineFormat = NULL;
+        pOddLineFormat = NULL;
+        pEvnLineFormat = NULL;
+
+        pFstColFormat = NULL;
+        pLstColFormat = NULL;
+        pOddColFormat = NULL;
+        pEvnColFormat = NULL;
+    }
     SwTableFormat( SwAttrPool& rPool, const OUString &rFormatNm,
                     SwFrameFormat *pDrvdFrm )
         : SwFrameFormat( rPool, rFormatNm, pDrvdFrm, RES_FRMFMT, aTableSetRange )
-    {}
+    {
+        pFstLineFormat = NULL;
+        pLstLineFormat = NULL;
+        pOddLineFormat = NULL;
+        pEvnLineFormat = NULL;
+
+        pFstColFormat = NULL;
+        pLstColFormat = NULL;
+        pOddColFormat = NULL;
+        pEvnColFormat = NULL;
+    }
+
+    SwTableLineFormat* pFstLineFormat;
+    SwTableLineFormat* pLstLineFormat;
+    SwTableLineFormat* pOddLineFormat;
+    SwTableLineFormat* pEvnLineFormat;
+
+    SwTableLineFormat* pFstColFormat;
+    SwTableLineFormat* pLstColFormat;
+    SwTableLineFormat* pOddColFormat;
+    SwTableLineFormat* pEvnColFormat;
 
 public:
     TYPEINFO_OVERRIDE();     // Already in base class Content.
 
     DECL_FIXEDMEMPOOL_NEWDEL(SwTableFormat)
+
+    void SetFirstLineFormat( SwTableLineFormat* pNew ) { pFstLineFormat = pNew; }
+    void SetLastLineFormat( SwTableLineFormat* pNew ) { pLstLineFormat = pNew; }
+    void SetOddLineFormat( SwTableLineFormat* pNew ) { pOddLineFormat = pNew; }
+    void SetEvenLineFormat( SwTableLineFormat* pNew ) { pEvnLineFormat = pNew; }
+
+    void SetFirstColFormat( SwTableLineFormat* pNew ) { pFstColFormat = pNew; }
+    void SetLastColFormat( SwTableLineFormat* pNew ) { pLstColFormat = pNew; }
+    void SetOddColFormat( SwTableLineFormat* pNew ) { pOddColFormat = pNew; }
+    void SetEvenColFormat( SwTableLineFormat* pNew ) { pEvnColFormat = pNew; }
+
+    SwTableLineFormat* GetFirstLineFormat() { return pFstLineFormat; }
+    SwTableLineFormat* GetLastLineFormat() { return pLstLineFormat; }
+    SwTableLineFormat* GetOddLineFormat() { return pOddLineFormat; }
+    SwTableLineFormat* GetEvenLineFormat() { return pEvnLineFormat; }
+
+    SwTableLineFormat* GetFirstColFormat() { return pFstColFormat; }
+    SwTableLineFormat* GetLastColFormat() { return pLstColFormat; }
+    SwTableLineFormat* GetOddColFormat() { return pOddColFormat; }
+    SwTableLineFormat* GetEvenColFormat() { return pEvnColFormat; }
 
     virtual bool supportsFullDrawingLayerFillAttributeSet() const SAL_OVERRIDE;
 };
@@ -53,16 +105,42 @@ protected:
     SwTableLineFormat( SwAttrPool& rPool, const sal_Char* pFormatNm,
                     SwFrameFormat *pDrvdFrm )
         : SwFrameFormat( rPool, pFormatNm, pDrvdFrm, RES_FRMFMT, aTableLineSetRange )
-    {}
+    {
+        pFstBoxFormat = NULL;
+        pLstBoxFormat = NULL;
+        pOddBoxFormat = NULL;
+        pEvnBoxFormat = NULL;
+    }
     SwTableLineFormat( SwAttrPool& rPool, const OUString &rFormatNm,
                     SwFrameFormat *pDrvdFrm )
         : SwFrameFormat( rPool, rFormatNm, pDrvdFrm, RES_FRMFMT, aTableLineSetRange )
-    {}
+    {
+        pFstBoxFormat = NULL;
+        pLstBoxFormat = NULL;
+        pOddBoxFormat = NULL;
+        pEvnBoxFormat = NULL;
+    }
+
+    // Odd and Even Box formats will be used to handle alternating columns
+    SwTableBoxFormat* pFstBoxFormat;
+    SwTableBoxFormat* pLstBoxFormat;
+    SwTableBoxFormat* pOddBoxFormat;
+    SwTableBoxFormat* pEvnBoxFormat;
 
 public:
     TYPEINFO_OVERRIDE();     // Already in base class Content.
 
     DECL_FIXEDMEMPOOL_NEWDEL(SwTableLineFormat)
+
+    void SetFirstBoxFormat( SwTableBoxFormat* pNew ) { pFstBoxFormat = pNew; }
+    void SetLastBoxFormat( SwTableBoxFormat* pNew ) { pLstBoxFormat = pNew; }
+    void SetOddBoxFormat( SwTableBoxFormat* pNew ) { pOddBoxFormat = pNew; }
+    void SetEvenBoxFormat( SwTableBoxFormat* pNew ) { pEvnBoxFormat = pNew; }
+
+    SwTableBoxFormat* GetFirstBoxFormat() { return pFstBoxFormat; }
+    SwTableBoxFormat* GetLastBoxFormat() { return pLstBoxFormat; }
+    SwTableBoxFormat* GetOddBoxFormat() { return pOddBoxFormat; }
+    SwTableBoxFormat* GetEvenBoxFormat() { return pEvnBoxFormat; }
 
     virtual bool supportsFullDrawingLayerFillAttributeSet() const SAL_OVERRIDE;
 };
