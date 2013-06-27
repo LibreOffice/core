f5d9b2f9759edaa2085479735792465959f0fe0e
Nagy Akos <akoscomp@gmail.com>
2013-06-27 12:11:59 +0300

    Add Romanian auto correct words.
    Change-Id: Iaa95e20aa069db6945f8ae22c5cea2de84047e61
    Reviewed-on: https://gerrit.libreoffice.org/4570
    Reviewed-by: Fridrich Strba <fridrich@documentfoundation.org>
    Tested-by: Fridrich Strba <fridrich@documentfoundation.org>

--- extras/CustomTarget_autocorr.mk
+++ extras/CustomTarget_autocorr.mk
@@ -38,6 +38,7 @@ extras_AUTOCORR_LANGS := \
 	pl:pl-PL \
 	pt:pt-PT \
 	pt-BR:pt-BR \
+	ro:ro-RO \
 	ru:ru-RU \
 	sh-ME:sh-ME \
 	sh-RS:sh-RS \
@@ -174,6 +175,11 @@ extras_AUTOCORR_XMLFILES := \
 	pt/acor/META-INF/manifest.xml \
 	pt/acor/SentenceExceptList.xml \
 	pt/acor/WordExceptList.xml \
+	ro/acor/BlockList.xml \
+	ro/acor/DocumentList.xml \
+	ro/acor/META-INF/manifest.xml \
+	ro/acor/SentenceExceptList.xml \
+	ro/acor/WordExceptList.xml \
 	ru/acor/BlockList.xml \
 	ru/acor/DocumentList.xml \
 	ru/acor/META-INF/manifest.xml \
@@ -257,6 +263,7 @@ extras_AUTOCORR_MIMETYPEFILES := \
 	pl/acor/mimetype \
 	pt-BR/acor/mimetype \
 	pt/acor/mimetype \
+	ro/acor/mimetype \
 	ru/acor/mimetype \
 	sh-ME/acor/mimetype \
 	sh-RS/acor/mimetype \
