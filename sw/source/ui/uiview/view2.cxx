6d866fec7117b5264136371bd8c04d5b882548e9
Caolán McNamara <caolanm@redhat.com>
2013-07-01 08:55:36 +0100

    fix indent
    Change-Id: I079e88700f3353b906f0d8fc85083695cfe569bb

--- sw/source/ui/uiview/view2.cxx
+++ sw/source/ui/uiview/view2.cxx
@@ -2293,16 +2293,16 @@ void SwView::GenerateFormLetter(sal_Bool bUseCurrentDocument)
             else
             {
                 //take an existing data source or create a new one?
-                    SwAbstractDialogFactory* pFact = SwAbstractDialogFactory::Create();
-                    OSL_ENSURE(pFact, "Dialogdiet fail!");
-                    AbstractMailMergeFieldConnectionsDlg* pConnectionsDlg = pFact->CreateMailMergeFieldConnectionsDlg(
-                                                        DLG_MERGE_FIELD_CONNECTIONS,
-                                                        &GetViewFrame()->GetWindow());
-                    OSL_ENSURE(pConnectionsDlg, "Dialogdiet fail!");
-                    if(RET_OK == pConnectionsDlg->Execute())
-                        bCallAddressPilot = !pConnectionsDlg->IsUseExistingConnections();
-                    else
-                        return;
+                SwAbstractDialogFactory* pFact = SwAbstractDialogFactory::Create();
+                OSL_ENSURE(pFact, "Dialogdiet fail!");
+                AbstractMailMergeFieldConnectionsDlg* pConnectionsDlg = pFact->CreateMailMergeFieldConnectionsDlg(
+                                                    DLG_MERGE_FIELD_CONNECTIONS,
+                                                    &GetViewFrame()->GetWindow());
+                OSL_ENSURE(pConnectionsDlg, "Dialogdiet fail!");
+                if(RET_OK == pConnectionsDlg->Execute())
+                    bCallAddressPilot = !pConnectionsDlg->IsUseExistingConnections();
+                else
+                    return;
 
             }
             if(bCallAddressPilot)
