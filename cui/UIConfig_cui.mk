a164468315fbdc3f2d14838803e1cc04214e07af
Olivier Hallot <olivier.hallot@edx.srv.br>
2013-06-30 23:48:19 -0300

    Convert tab stop page to widget UI
    Conflicts:
    	cui/UIConfig_cui.mk
    
    Change-Id: Idd4401966933da1e4de3e3f31f74e2908dd7d705

--- cui/UIConfig_cui.mk
+++ cui/UIConfig_cui.mk
@@ -62,6 +62,7 @@ $(eval $(call gb_UIConfig_add_uifiles,cui,\
 	cui/uiconfig/ui/pageformatpage \
 	cui/uiconfig/ui/paragalignpage \
 	cui/uiconfig/ui/paraindentspacing \
+	cui/uiconfig/ui/paratabspage \
 	cui/uiconfig/ui/pastespecial \
 	cui/uiconfig/ui/personalization_tab \
 	cui/uiconfig/ui/pickbulletpage \
