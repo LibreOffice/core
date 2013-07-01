LibreOffice/core

commit 0c3500115c4fd86284a027fc32be704afcf77061

Related rhbz#915743: Do not call into DAVResourceAccess with mutex locked
...from webdav Content::getResourceType, as otherwise Content::abort would be
blocked waiting for the mutex (in code that would call abort, which will be
required to fix rhbz#915743 "thread deadlock/slow join in insert->hyperlink in
impress").  This required to get the odd reference to enum return type of
getResourceType straight.

Also, propagate information about !shouldAccessNetworkAfterException from
getResourceType out to getPropertyValues, to avoid further calls that would
again block/fail.

Change-Id: I8b9d43a61eb4078acb90079c4eb7aa98a59a8983

ucb/source/ucp/webdav-neon/webdavcontent.cxx
@@ -782,8 +782,8 @@ void SAL_CALL Content::addProperty( const rtl::OUString& Name,
             {
                 try
                 {
-                    const ResourceType & rType = getResourceType( xEnv );
-                    switch ( rType )
+                    ResourceType eType = getResourceType( xEnv );
+                    switch ( eType )
                     {
                     case UNKNOWN:
                     case DAV:
@@ -877,8 +877,8 @@ void SAL_CALL Content::removeProperty( const rtl::OUString& Name )
             {
                 try
                 {
-                    const ResourceType & rType = getResourceType( xEnv );
-                    switch ( rType )
+                    ResourceType eType = getResourceType( xEnv );
+                    switch ( eType )
                     {
                         case UNKNOWN:
                         case DAV:
@@ -1175,11 +1175,11 @@ uno::Reference< sdbc::XRow > Content::getPropertyValues(
         /////////////////////////////////////////////////////////////////////
 
         // First, identify whether resource is DAV or not
-        const ResourceType & rType = getResourceType( xEnv, xResAccess );
-
         bool bNetworkAccessAllowed = true;
+        ResourceType eType = getResourceType(
+            xEnv, xResAccess, &bNetworkAccessAllowed );
 
-        if ( DAV == rType )
+        if ( eType == DAV )
         {
             // cache lookup... getResourceType may fill the props cache via
             // PROPFIND!
@@ -1269,8 +1269,8 @@ uno::Reference< sdbc::XRow > Content::getPropertyValues(
                     }
                     catch ( DAVException const & e )
                     {
-                        bNetworkAccessAllowed
-                            = shouldAccessNetworkAfterException( e );
+                        bNetworkAccessAllowed = bNetworkAccessAllowed
+                            && shouldAccessNetworkAfterException( e );
 
                         if ( !bNetworkAccessAllowed )
                         {
@@ -1341,7 +1341,7 @@ uno::Reference< sdbc::XRow > Content::getPropertyValues(
         NeonUri aUri( xResAccess->getURL() );
         aUnescapedTitle = aUri.GetPathBaseNameUnescaped();
 
-        if ( rType == UNKNOWN )
+        if ( eType == UNKNOWN )
         {
             xProps.reset( new ContentProperties( aUnescapedTitle ) );
         }
@@ -1349,7 +1349,7 @@ uno::Reference< sdbc::XRow > Content::getPropertyValues(
         // For DAV resources we only know the Title, for non-DAV
         // resources we additionally know that it is a document.
 
-        if ( rType == DAV )
+        if ( eType == DAV )
         {
             //xProps.reset(
             //    new ContentProperties( aUnescapedTitle ) );
@@ -3070,83 +3070,93 @@ Content::getBaseURI( const std::auto_ptr< DAVResourceAccess > & rResAccess )
 }
 
 //=========================================================================
-const Content::ResourceType & Content::getResourceType(
+Content::ResourceType Content::getResourceType(
                     const uno::Reference< ucb::XCommandEnvironment >& xEnv,
-                    const std::auto_ptr< DAVResourceAccess > & rResAccess )
+                    const std::auto_ptr< DAVResourceAccess > & rResAccess,
+                    bool * networkAccessAllowed)
     throw ( uno::Exception )
 {
-    if ( m_eResourceType == UNKNOWN )
     {
-        osl::Guard< osl::Mutex > aGuard( m_aMutex );
-
-        ResourceType eResourceType;
-        eResourceType = m_eResourceType;
-
-        const rtl::OUString & rURL = rResAccess->getURL();
-        const rtl::OUString aScheme(
-            rURL.copy( 0, rURL.indexOf( ':' ) ).toAsciiLowerCase() );
-
-        if ( aScheme == FTP_URL_SCHEME )
-        {
-            eResourceType = FTP;
+        osl::MutexGuard g(m_aMutex);
+        if (m_eResourceType != UNKNOWN) {
+            return m_eResourceType;
         }
-        else
+    }
+
+    ResourceType eResourceType;
+
+    const rtl::OUString & rURL = rResAccess->getURL();
+    const rtl::OUString aScheme(
+        rURL.copy( 0, rURL.indexOf( ':' ) ).toAsciiLowerCase() );
+
+    if ( aScheme == FTP_URL_SCHEME )
+    {
+        eResourceType = FTP;
+    }
+    else
+    {
+        try
         {
-            try
+            // Try to fetch some frequently used property value, e.g. those
+            // used when loading documents... along with identifying whether
+            // this is a DAV resource.
+            std::vector< DAVResource > resources;
+            std::vector< rtl::OUString > aPropNames;
+            uno::Sequence< beans::Property > aProperties( 5 );
+            aProperties[ 0 ].Name = rtl::OUString("IsFolder");
+            aProperties[ 1 ].Name = rtl::OUString("IsDocument");
+            aProperties[ 2 ].Name = rtl::OUString("IsReadOnly");
+            aProperties[ 3 ].Name = rtl::OUString("MediaType");
+            aProperties[ 4 ].Name = DAVProperties::SUPPORTEDLOCK;
+
+            ContentProperties::UCBNamesToDAVNames( aProperties, aPropNames );
+
+            rResAccess->PROPFIND( DAVZERO, aPropNames, resources, xEnv );
+
+            if ( resources.size() == 1 )
             {
-                // Try to fetch some frequently used property value, e.g. those
-                // used when loading documents... along with identifying whether
-                // this is a DAV resource.
-                std::vector< DAVResource > resources;
-                std::vector< rtl::OUString > aPropNames;
-                uno::Sequence< beans::Property > aProperties( 5 );
-                aProperties[ 0 ].Name
-                    = rtl::OUString("IsFolder");
-                aProperties[ 1 ].Name
-                    = rtl::OUString("IsDocument");
-                aProperties[ 2 ].Name
-                    = rtl::OUString("IsReadOnly");
-                aProperties[ 3 ].Name
-                    = rtl::OUString("MediaType");
-                aProperties[ 4 ].Name
-                    = DAVProperties::SUPPORTEDLOCK;
-
-                ContentProperties::UCBNamesToDAVNames(
-                    aProperties, aPropNames );
-
-                rResAccess->PROPFIND(
-                    DAVZERO, aPropNames, resources, xEnv );
-
-                if ( resources.size() == 1 )
-                {
-                    m_xCachedProps.reset(
-                        new CachableContentProperties( resources[ 0 ] ) );
-                    m_xCachedProps->containsAllNames(
-                        aProperties, m_aFailedPropNames );
-                }
-
-                eResourceType = DAV;
+                osl::MutexGuard g(m_aMutex);
+                m_xCachedProps.reset(
+                    new CachableContentProperties( resources[ 0 ] ) );
+                m_xCachedProps->containsAllNames(
+                    aProperties, m_aFailedPropNames );
             }
-            catch ( DAVException const & e )
-            {
-                rResAccess->resetUri();
 
-                if ( e.getStatus() == SC_METHOD_NOT_ALLOWED )
-                {
-                    // Status SC_METHOD_NOT_ALLOWED is a safe indicator that the
-                    // resource is NON_DAV
-                    eResourceType = NON_DAV;
-                }
+            eResourceType = DAV;
+        }
+        catch ( DAVException const & e )
+        {
+            rResAccess->resetUri();
+
+            if ( e.getStatus() == SC_METHOD_NOT_ALLOWED )
+            {
+                // Status SC_METHOD_NOT_ALLOWED is a safe indicator that the
+                // resource is NON_DAV
+                eResourceType = NON_DAV;
+            }
+            else if (networkAccessAllowed != 0)
+            {
+                *networkAccessAllowed = *networkAccessAllowed
+                    && shouldAccessNetworkAfterException(e);
             }
         }
+    }
+
+    osl::MutexGuard g(m_aMutex);
+    if (m_eResourceType == UNKNOWN) {
         m_eResourceType = eResourceType;
+    } else {
+        SAL_WARN_IF(
+            eResourceType != m_eResourceType, "ucb.ucp.webdav",
+            "different resource types for <" << rURL << ">: "
+            << +eResourceType << " vs. " << +m_eResourceType);
     }
     return m_eResourceType;
 }
 SAL_WNODEPRECATED_DECLARATIONS_POP
 
 //=========================================================================
-const Content::ResourceType & Content::getResourceType(
+Content::ResourceType Content::getResourceType(
                     const uno::Reference< ucb::XCommandEnvironment >& xEnv )
     throw ( uno::Exception )
 {

ucb/source/ucp/webdav-neon/webdavcontent.hxx
@@ -133,16 +133,17 @@ private:
     getBaseURI( const std::auto_ptr< DAVResourceAccess > & rResAccess );
     SAL_WNODEPRECATED_DECLARATIONS_POP
 
-    const ResourceType &
+    ResourceType
     getResourceType( const ::com::sun::star::uno::Reference<
                          ::com::sun::star::ucb::XCommandEnvironment >& xEnv )
         throw ( ::com::sun::star::uno::Exception );
 
     SAL_WNODEPRECATED_DECLARATIONS_PUSH
-    const ResourceType &
+    ResourceType
     getResourceType( const ::com::sun::star::uno::Reference<
                           ::com::sun::star::ucb::XCommandEnvironment >& xEnv,
-                     const std::auto_ptr< DAVResourceAccess > & rResAccess )
+                     const std::auto_ptr< DAVResourceAccess > & rResAccess,
+                     bool * networkAccessAllowed = 0)
         throw ( ::com::sun::star::uno::Exception );
     SAL_WNODEPRECATED_DECLARATIONS_POP
 

commit 49a454225e35699d7351faaba3d296e1858c6107

fdo#66324 fix locking in webdav_ucp::Content::getResourceType()
Crash in there while accessing an evidently deleted DAVResourceAccess
instance with about 4 other threads also in various webdav_ucp::Content
methods.

The problem is apparently that the "outer" getResourceType() passes the
m_xResAccess to the "inner" getResourceType(), which accesses it
without locking the mutex, while another thread resets m_xResAccess
and thereby deletes the current instance.

(regression from 0c3500115c4fd86284a027fc32be704afcf77061)

Change-Id: I1c67021c536e303d766c7ff93fb71e40f991f3af

ucb/source/ucp/webdav-neon/webdavcontent.cxx
@@ -3164,7 +3164,19 @@ Content::ResourceType Content::getResourceType(
                     const uno::Reference< ucb::XCommandEnvironment >& xEnv )
     throw ( uno::Exception )
 {
-    return getResourceType( xEnv, m_xResAccess );
+    SAL_WNODEPRECATED_DECLARATIONS_PUSH
+    std::auto_ptr< DAVResourceAccess > xResAccess;
+    SAL_WNODEPRECATED_DECLARATIONS_POP
+    {
+        osl::MutexGuard aGuard( m_aMutex );
+        xResAccess.reset( new DAVResourceAccess( *m_xResAccess.get() ) );
+    }
+    Content::ResourceType const ret = getResourceType( xEnv, xResAccess );
+    {
+        osl::Guard< osl::Mutex > aGuard( m_aMutex );
+        m_xResAccess.reset( new DAVResourceAccess( *xResAccess.get() ) );
+    }
+    return ret;
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */