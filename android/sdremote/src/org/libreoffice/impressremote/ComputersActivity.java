668c57f61d31987b12dc4c41cf4bced022fcbf0d
Artur Dryomov <artur.dryomov@gmail.com>
2013-07-02 19:04:09 +0300

    Add missed copyrights.
    Change-Id: I3b750e853fc74a19329236e186ee05fd31ee77cc

--- android/sdremote/src/org/libreoffice/impressremote/ComputersActivity.java
+++ android/sdremote/src/org/libreoffice/impressremote/ComputersActivity.java
@@ -1,3 +1,11 @@
+/* -*- Mode: Java; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package org.libreoffice.impressremote;
 
 import android.os.Bundle;
@@ -42,3 +50,5 @@ public class ComputersActivity extends SherlockFragmentActivity {
         return buildActionBarTab(aFragment, R.string.title_wifi);
     }
 }
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
--- android/sdremote/src/org/libreoffice/impressremote/communication/MessagesListener.java
+++ android/sdremote/src/org/libreoffice/impressremote/communication/MessagesListener.java
@@ -1,3 +1,11 @@
+/* -*- Mode: Java; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package org.libreoffice.impressremote.communication;
 
 public interface MessagesListener {
@@ -15,3 +23,5 @@ public interface MessagesListener {
 
     public void onSlideNotes(int aSlideIndex, String aNotes);
 }
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
