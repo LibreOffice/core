b8904b25d622bcf5c2b1435a993e507d1c7f7555
Kohei Yoshida <kohei.yoshida@gmail.com>
2013-07-02 09:24:09 -0400

    Try not to iterate beyond the end of the formula block.
    Or else a crash would ensue...
    
    Change-Id: Ib08163ba91e4bac023ae778c704a0c052fa48ebb

--- sc/source/core/data/column2.cxx
+++ sc/source/core/data/column2.cxx
@@ -2258,8 +2258,9 @@ const double* ScColumn::FetchDoubleArray( sc::FormulaGroupContext& rCxt, SCROW n
                 return &rArray[0];
             }
 
+            // Requested length goes beyond a single block.  Fill the array
+            // with the content of this formula block first.
             itEnd = sc::formula_block::end(*aPos.first->data);
-            std::advance(itEnd, nLenRequested);
             for (; it != itEnd; ++it)
             {
                 ScFormulaCell& rCell = **it;
