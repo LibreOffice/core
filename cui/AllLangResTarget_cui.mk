9e689ce13a43c536c17043e351dbdf5687fd32bc
Caolán McNamara <caolanm@redhat.com>
2013-07-04 11:21:44 +0100

    drop unused .src now
    Change-Id: I6ec69375dc1023f6f899bc3bb46bccc47e508634

--- cui/AllLangResTarget_cui.mk
+++ cui/AllLangResTarget_cui.mk
@@ -57,7 +57,6 @@ $(eval $(call gb_SrsTarget_add_files,cui/res,\
     cui/source/options/optdict.src \
     cui/source/options/optfltr.src \
     cui/source/options/optgdlg.src \
-    cui/source/options/opthtml.src \
     cui/source/options/optinet2.src \
     cui/source/options/optjava.src \
     cui/source/options/optlingu.src \
