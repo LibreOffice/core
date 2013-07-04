3bf5b6e4c4f032b7e030961cdebed9530231be83
Norbert Thiebaud <nthiebaud@gmail.com>
2013-07-04 04:42:05 -0500

    fdo#66100 : fix icons associations on MacOSX
    Change-Id: Idbce5516f1cc59b8d1cc39ee0a0db2aad90e8074
    Reviewed-on: https://gerrit.libreoffice.org/4714
    Reviewed-by: Fridrich Strba <fridrich@documentfoundation.org>
    Tested-by: Fridrich Strba <fridrich@documentfoundation.org>

--- sysui/Package_osxicons.mk
+++ sysui/Package_osxicons.mk
@@ -7,35 +7,34 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 
-$(eval $(call gb_Package_Package,sysui_osxicons,$(SRCDIR)/sysui/desktop))
+$(eval $(call gb_Package_Package,sysui_osxicons,$(SRCDIR)/sysui/desktop/icons))
 
 $(eval $(call gb_Package_set_outdir,sysui_osxicons,$(INSTDIR)))
 
 $(eval $(call gb_Package_add_files_with_dir,sysui_osxicons,Resources,\
-    icons/text.icns \
-    icons/oasis-text.icns \
-    icons/spreadsheet.icns \
-    icons/oasis-spreadsheet.icns \
-    icons/presentation.icns \
-    icons/oasis-presentation.icns \
-    icons/drawing.icns \
-    icons/oasis-drawing.icns \
-    icons/master-document.icns \
-    icons/oasis-master-document.icns \
-    icons/formula.icns \
-    icons/oasis-formula.icns \
-    icons/text-template.icns \
-    icons/oasis-text-template.icns \
-    icons/spreadsheet-template.icns \
-    icons/oasis-spreadsheet-template.icns \
-    icons/presentation-template.icns \
-    icons/oasis-presentation-template.icns \
-    icons/drawing-template.icns \
-    icons/oasis-drawing-template.icns \
-    icons/oasis-database.icns \
-    icons/extension.icns \
-    icons/main.icns \
-	macosx/Info.plist \
+    text.icns \
+    oasis-text.icns \
+    spreadsheet.icns \
+    oasis-spreadsheet.icns \
+    presentation.icns \
+    oasis-presentation.icns \
+    drawing.icns \
+    oasis-drawing.icns \
+    master-document.icns \
+    oasis-master-document.icns \
+    formula.icns \
+    oasis-formula.icns \
+    text-template.icns \
+    oasis-text-template.icns \
+    spreadsheet-template.icns \
+    oasis-spreadsheet-template.icns \
+    presentation-template.icns \
+    oasis-presentation-template.icns \
+    drawing-template.icns \
+    oasis-drawing-template.icns \
+    oasis-database.icns \
+    extension.icns \
+    main.icns \
 ))
 
 # vim: set ts=4 sw=4 et:
