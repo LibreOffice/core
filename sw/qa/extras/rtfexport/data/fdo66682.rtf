LibreOffice/core

commit 07d01742c69f1c0335bc7e1b57abd8341ce255e7

i#119219 fix non-unicode RTF export
There were two problems here:

- non-unicode charset being set to Shift JIS for all UTF-8 text
- non-unicode text using UTF-8

Change-Id: I533d3e950a4ebd611b5d5db8a95e724e729a641f

sw/source/filter/ww8/rtfattributeoutput.cxx
@@ -412,10 +412,10 @@ void RtfAttributeOutput::EndRunProperties( const SwRedlineData* /*pRedlineData*/
     m_aRun->append(m_aStyles.makeStringAndClear());
 }
 
-void RtfAttributeOutput::RunText( const String& rText, rtl_TextEncoding eCharSet )
+void RtfAttributeOutput::RunText( const String& rText, rtl_TextEncoding /*eCharSet*/ )
 {
     SAL_INFO("sw.rtf", OSL_THIS_FUNC);
-    RawText( rText, 0, eCharSet );
+    RawText( rText, 0, m_rExport.eCurrentEncoding );
 }
 
 OStringBuffer& RtfAttributeOutput::RunText()
@@ -1911,7 +1911,7 @@ void RtfAttributeOutput::CharFont( const SvxFontItem& rFont)
     m_aStylesEnd.append(OOO_STRING_SVTOOLS_RTF_LOCH);
     m_aStylesEnd.append(OOO_STRING_SVTOOLS_RTF_F);
     m_aStylesEnd.append((sal_Int32)m_rExport.maFontHelper.GetId(rFont));
-    m_rExport.eCurrentEncoding = rtl_getTextEncodingFromWindowsCharset(sw::ms::rtl_TextEncodingToWinCharset(rFont.GetCharSet()));
+    m_rExport.eCurrentEncoding = rtl_getTextEncodingFromWindowsCharset(rtl_getBestWindowsCharsetFromTextEncoding(rFont.GetCharSet()));
 }
 
 void RtfAttributeOutput::CharFontSize( const SvxFontHeightItem& rFontSize)

sw/source/filter/ww8/rtfexport.cxx
@@ -882,27 +882,13 @@ OString RtfExport::OutChar(sal_Unicode c, int *pUCMode, rtl_TextEncoding eDestEn
             if (c >= ' ' && c <= '~')
                 aBuf.append((sal_Char)c);
             else {
-                //If we can't convert to the dest encoding, or if
-                //its an uncommon multibyte sequence which most
-                //readers won't be able to handle correctly, then
-                //If we can't convert to the dest encoding, then
-                //export as unicode
                 OUString sBuf(&c, 1);
                 OString sConverted;
-                sal_uInt32 nFlags =
-                    RTL_UNICODETOTEXT_FLAGS_UNDEFINED_ERROR |
-                    RTL_UNICODETOTEXT_FLAGS_INVALID_ERROR;
-                bool bWriteAsUnicode = !(sBuf.convertToString(&sConverted,
-                            eDestEnc, nFlags))
-                    || (RTL_TEXTENCODING_UTF8==eDestEnc); // #i43933# do not export UTF-8 chars in RTF;
-                if (bWriteAsUnicode)
-                    sBuf.convertToString(&sConverted,
-                            eDestEnc, OUSTRING_TO_OSTRING_CVTFLAGS);
+                sBuf.convertToString(&sConverted, eDestEnc, OUSTRING_TO_OSTRING_CVTFLAGS);
                 const sal_Int32 nLen = sConverted.getLength();
 
-                if (bWriteAsUnicode && pUCMode)
+                if (pUCMode)
                 {
-                    // then write as unicode - character
                     if (*pUCMode != nLen)
                     {
                         aBuf.append("\\uc");

sw/source/filter/ww8/wrtw8sty.cxx
@@ -735,7 +735,7 @@ void wwFont::WriteRtf( const RtfAttributeOutput* rAttrOutput ) const
 {
     rAttrOutput->FontFamilyType( meFamily, *this );
     rAttrOutput->FontPitchType( mePitch );
-    rAttrOutput->FontCharset( sw::ms::rtl_TextEncodingToWinCharset( meChrSet ) );
+    rAttrOutput->FontCharset( rtl_getBestWindowsCharsetFromTextEncoding( meChrSet ) );
     rAttrOutput->StartFont( msFamilyNm );
     if ( mbAlt )
         rAttrOutput->FontAlternateName( msAltNm );

commit 8422d1f5042de08e8b090a907889b41e133bda40

fdo#66682 RTF export: fix level text of SVX_NUM_ARABIC
Even if level text is not ASCII, we should just write the placeholder
values (0x00, 0x01, etc.) in a hex encoded form, without Unicode.
Regression from 07d01742c69f1c0335bc7e1b57abd8341ce255e7.

Also, fix the import side, so the previous bad output now looks bad in
Writer as well.

Change-Id: I2f93892006c07eec243ec7dfcb23372d243a2db4

sw/source/filter/ww8/rtfattributeoutput.cxx
@@ -1397,7 +1397,7 @@ void RtfAttributeOutput::NumberingLevel( sal_uInt8 nLevel,
     else
     {
         m_rExport.Strm() << "\\'" << msfilter::rtfutil::OutHex( rNumberingString.Len(), 2 ).getStr();
-        m_rExport.Strm() << msfilter::rtfutil::OutString( rNumberingString, m_rExport.eDefaultEncoding ).getStr();
+        m_rExport.Strm() << msfilter::rtfutil::OutString( rNumberingString, m_rExport.eDefaultEncoding, /*bUnicode =*/ false ).getStr();
     }
 
     m_rExport.Strm() << ";}";

writerfilter/source/rtftok/rtfdocumentimpl.cxx
@@ -1713,6 +1713,8 @@ int RTFDocumentImpl::dispatchSymbol(RTFKeyword nKeyword)
 {
     if (nKeyword != RTF_HEXCHAR)
         checkUnicode(/*bUnicode =*/ true, /*bHex =*/ true);
+    else
+        checkUnicode(/*bUnicode =*/ true, /*bHex =*/ false);
     setNeedSect();
     RTFSkipDestination aSkip(*this);
 
@@ -3111,7 +3113,8 @@ int RTFDocumentImpl::dispatchValue(RTFKeyword nKeyword, int nParam)
             if ((SAL_MIN_INT16 <= nParam) && (nParam <= SAL_MAX_UINT16))
             {
                 m_aUnicodeBuffer.append(static_cast<sal_Unicode>(nParam));
-                m_aStates.top().nCharsToSkip = m_aStates.top().nUc;
+                if (m_aStates.top().nDestinationState != DESTINATION_LEVELTEXT)
+                    m_aStates.top().nCharsToSkip = m_aStates.top().nUc;
             }
             break;
         case RTF_LEVELFOLLOW:

sw/qa/extras/rtfexport/data/fdo66682.rtf
@@ -1,0 +1,25 @@
+{\rtf1\ansi\deff4\adeflang1025
+{\*\listtable
+{\list\listtemplateid1
+{\listlevel\levelnfc0\leveljc0\levelstartat1\levelfollow0
+{\leveltext \'03 \'00 ;}
+{\levelnumbers\'02;}
+\fi-360\li720}
+\listid1}
+}
+{\listoverridetable
+{\listoverride\listid1\listoverridecount0\ls1}
+}
+\pard\plain \s0\sb0\sa113\nowidctlpar
+\ilvl0\ls1 \li720\ri0\lin720\rin0\fi-360
+{\rtlch \ltrch\loch
+This should be 1}
+\par \pard\plain \s0\sb0\sa113\nowidctlpar
+\ilvl0\ls1 \li720\ri0\lin720\rin0\fi-360
+{\rtlch \ltrch\loch
+and 2}
+\par \pard\plain \s0\sb0\sa113\nowidctlpar
+\ilvl0\ls1 \li720\ri0\lin720\rin0\fi-360\sb0\sa113
+{\rtlch \ltrch\loch
+and 3}
+\par }

sw/qa/extras/rtfexport/rtfexport.cxx
@@ -66,6 +66,7 @@ public:
     void testTextframeGradient();
     void testRecordChanges();
     void testTextframeTable();
+    void testFdo66682();
 
     CPPUNIT_TEST_SUITE(Test);
 #if !defined(MACOSX) && !defined(WNT)
@@ -121,6 +122,7 @@ void Test::run()
         {"textframe-gradient.rtf", &Test::testTextframeGradient},
         {"record-changes.rtf", &Test::testRecordChanges},
         {"textframe-table.rtf", &Test::testTextframeTable},
+        {"fdo66682.rtf", &Test::testFdo66682},
     };
     // Don't test the first import of these, for some reason those tests fail
     const char* aBlacklist[] = {
@@ -605,6 +607,25 @@ void Test::testTextframeTable()
     CPPUNIT_ASSERT_EQUAL(OUString("A"), uno::Reference<text::XTextRange>(xTable->getCellByName("A1"), uno::UNO_QUERY)->getString());
     CPPUNIT_ASSERT_EQUAL(OUString("B"), uno::Reference<text::XTextRange>(xTable->getCellByName("B1"), uno::UNO_QUERY)->getString());
     CPPUNIT_ASSERT_EQUAL(OUString("Last para."), getParagraphOfText(3, xText)->getString());
+}
+
+void Test::testFdo66682()
+{
+    uno::Reference<beans::XPropertySet> xPropertySet(getStyles("NumberingStyles")->getByName("WWNum1"), uno::UNO_QUERY);
+    uno::Reference<container::XIndexAccess> xLevels(xPropertySet->getPropertyValue("NumberingRules"), uno::UNO_QUERY);
+    uno::Sequence<beans::PropertyValue> aProps;
+    xLevels->getByIndex(0) >>= aProps; // 1st level
+
+    OUString aSuffix;
+    for (int i = 0; i < aProps.getLength(); ++i)
+    {
+        const beans::PropertyValue& rProp = aProps[i];
+
+        if (rProp.Name == "Suffix")
+            aSuffix = rProp.Value.get<OUString>();
+    }
+    // Suffix was '\0' instead of ' '.
+    CPPUNIT_ASSERT_EQUAL(OUString(" "), aSuffix);
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(Test);