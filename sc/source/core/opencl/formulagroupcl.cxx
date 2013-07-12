f81290249dc5d4f782c2bebe1f9501559642efe0
Michael Meeks <michael.meeks@suse.com>
2013-07-12 12:45:42 +0100

    add SC_SOFTWARE tweak to force sw group interpret.

--- sc/source/core/opencl/formulagroupcl.cxx
+++ sc/source/core/opencl/formulagroupcl.cxx
@@ -426,6 +426,11 @@ bool FormulaGroupInterpreterGroundwater::interpret(ScDocument& rDoc, const ScAdd
 namespace opencl {
     sc::FormulaGroupInterpreter *createFormulaGroupInterpreter()
     {
+        if (getenv("SC_SOFTWARE"))
+        {
+            fprintf(stderr, "Create S/W interp\n");
+            return new sc::FormulaGroupInterpreterSoftware();
+        }
         if (getenv("SC_GROUNDWATER"))
             return new sc::FormulaGroupInterpreterGroundwater();
         else
