6089121ebd075bb6422d33d6bff69dff2e88efb2
Michael Stahl <mstahl@redhat.com>
2013-07-12 23:13:10 +0200

    fdo#66811: vcl: fix broken OUString with length STRING_LEN
    ImplSalGetUniString was wrongly converted and constructs OUString with
    invalid length in WinSalGraphics::CreateFontSubset; this is then
    implicitly converted to an empty UniString so the font names are
    missing in the PDF files generated on Windows.
    
    (regression from 9e310cc32923ceb4b18d97ce68d54a339b935f01)
    
    Change-Id: I1603e62cf18f353f3d7de322b9111a173dc6b225
    (cherry picked from commit 82f742f65d35896c69be38fa3b1c78a22226f71c)
    Reviewed-on: https://gerrit.libreoffice.org/4875
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>

--- vcl/inc/win/saldata.hxx
+++ vcl/inc/win/saldata.hxx
@@ -196,7 +196,7 @@ void ImplSalPostDispatchMsg( MSG* pMsg, LRESULT nDispatchResult );
 void ImplSalLogFontToFontW( HDC hDC, const LOGFONTW& rLogFont, Font& rFont );
 
 rtl_TextEncoding ImplSalGetSystemEncoding();
-OUString ImplSalGetUniString( const sal_Char* pStr, xub_StrLen nLen = STRING_LEN );
+OUString ImplSalGetUniString(const sal_Char* pStr, sal_Int32 nLen = -1);
 int ImplSalWICompareAscii( const wchar_t* pStr1, const char* pStr2 );
 
 #define SAL_FRAME_WNDEXTRA          sizeof( DWORD )
