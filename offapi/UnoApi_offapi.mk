4079afed49aaa5b1d84443a21f63007754ba31ae
Noel Power <noel.power@suse.com>
2013-07-12 14:22:30 +0100

    fix fdo#66586 Revert "fdo#46808, create IDL fortree::DefaultTreeDataModel"
    This reverts commit fcd01fba69db6de6cfc983fae65b6ba6764de0d6.
    
    Service DefaultTreeDataModel actually doens't exist ( and we can't find
    when/where it used to :-( ) The treecontrolpeer.cxx change to
    use the new service also had the undesired effect of throwing an exception
    when the UnoTreeControl model is inserted ( previously this failed silently )
    The net effect is the dialog control is malformed and not initialised
    correctly
    (cherry picked from commit df9f26c22a59ae2309201d2709ea5f8a0ecbf247)
    
    also partial revert of f50553593613cbefb7f197fed66498bb6556a39a on
    DefaultTreeDataModel.idl so the revert above would apply
    
    Change-Id: Id76cc12a5360f6435bd39ca86020655feba20c9f
    Reviewed-on: https://gerrit.libreoffice.org/4865
    Reviewed-by: David Tardon <dtardon@redhat.com>
    Tested-by: David Tardon <dtardon@redhat.com>

--- offapi/UnoApi_offapi.mk
+++ offapi/UnoApi_offapi.mk
@@ -70,7 +70,6 @@ $(eval $(call gb_UnoApi_add_idlfiles_nohdl,offapi,offapi/com/sun/star/awt/tab,\
 	UnoControlTabPageModel \
 ))
 $(eval $(call gb_UnoApi_add_idlfiles_nohdl,offapi,offapi/com/sun/star/awt/tree,\
-	DefaultTreeDataModel \
 	MutableTreeDataModel \
 	MutableTreeNode \
 	TreeControl \
