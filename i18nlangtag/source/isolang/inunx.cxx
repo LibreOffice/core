7c52d0d0a48611e571c4d7f85a71fbe81e711655
Eike Rathke <erack@redhat.com>
2013-07-13 12:12:42 +0200

    grml.. Mac
    Change-Id: Ia49d45a896a3ea5d180d98c3579fb93c6d4b9911

--- i18nlangtag/source/isolang/inunx.cxx
+++ i18nlangtag/source/isolang/inunx.cxx
@@ -99,7 +99,7 @@ static void getPlatformSystemLanguageImpl( LanguageType& rSystemLanguage,
 
             if ( osl_getProcessLocale(&procLocale) == osl_Process_E_None )
             {
-                nLang = LanguageTag::convertToLanguageType( *procLocale );
+                nLang = LanguageTag( *procLocale ).getLanguageType();
                 OSL_DOUBLE_CHECKED_LOCKING_MEMORY_BARRIER();
                 rSystemLanguage = nLang;
 #ifdef DEBUG
