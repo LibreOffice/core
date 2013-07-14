a3e5c43ff690da09390634dd7c54c9ff193a95cc
Cao Cuong Ngo <cao.cuong.ngo@gmail.com>
2013-07-14 23:59:51 +0200

    CMIS: connect LO to Google Drive
    Change-Id: I2c1b1d3e2bfd50ceb8647e62d37512ee3a0b9dae

---
+++ ucb/source/ucp/cmis/cmis_oauth2_providers.hxx
@@ -0,0 +1,32 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ */
+
+#ifndef CMIS_OAUTH2_PROVIDER2_HXX
+#define CMIS_OAUTH2_PROVIDERS_HXX
+
+
+namespace cmis
+{
+
+#define GDRIVE_BASE_URL "https://www.googleapis.com/drive/v2"
+#define GDRIVE_CLIENT_ID "457862564325.apps.googleusercontent.com"
+#define GDRIVE_CLIENT_SECRET "GYWrDtzyZQZ0_g5YoBCC6F0I"
+#define GDRIVE_AUTH_URL "https://accounts.google.com/o/oauth2/auth"
+#define GDRIVE_TOKEN_URL "https://accounts.google.com/o/oauth2/token"
+#define GDRIVE_REDIRECT_URI "urn:ietf:wg:oauth:2.0:oob"
+#define GDRIVE_SCOPE "https://www.googleapis.com/auth/drive"
+
+}
+
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
