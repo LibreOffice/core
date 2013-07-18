LibreOffice/core

commit 0143805a565418d2a114c16b7eeba3b784176d9e

Resolves: #i122332# Don't change sidebar context when switching...
to other application window

(cherry picked from commit 604502e2a4b53f35aedbf0205598a9b691c0d532)

Conflicts:
	sd/source/ui/view/drviewsa.cxx
	sfx2/inc/sfx2/shell.hxx
	sfx2/source/control/shell.cxx
	svx/inc/svx/sidebar/SelectionAnalyzer.hxx
	svx/inc/svx/sidebar/SelectionChangeHandler.hxx

Change-Id: Id3c427e02714ef0d6686a78094e2f7f3b390a693

sd/source/ui/view/drviews1.cxx
@@ -114,9 +114,9 @@ void DrawViewShell::UIDeactivated( SfxInPlaceClient* pCli )
 }
 
 
-void DrawViewShell::Deactivate(sal_Bool bIsMDIActivate)
+void DrawViewShell::Deactivate(sal_Bool /*bIsMDIActivate*/)
 {
-    ViewShell::Deactivate(bIsMDIActivate);
+    // Do not forward to ViewShell::Deactivate() to prevent a context change.
 }
 
 namespace
@@ -459,13 +459,7 @@ void DrawViewShell::ChangeEditMode(EditMode eEMode, bool bIsLayerModeActive)
         Invalidate( SID_NOTES_MASTERPAGE );
         Invalidate( SID_HANDOUT_MASTERPAGE );
 
-        if (meEditMode == EM_PAGE)
-            SetContextName(sfx2::sidebar::EnumContext::GetContextName(sfx2::sidebar::EnumContext::Context_DrawPage));
-        else if (mePageKind == PK_HANDOUT)
-
-            SetContextName(sfx2::sidebar::EnumContext::GetContextName(sfx2::sidebar::EnumContext::Context_HandoutPage));
-        else
-            SetContextName(sfx2::sidebar::EnumContext::GetContextName(sfx2::sidebar::EnumContext::Context_MasterPage));
+        SetContextName(GetSidebarContextName());
     }
 }
 

sd/source/ui/view/outlnvsh.cxx
@@ -363,6 +363,8 @@ void OutlineViewShell::Activate( sal_Bool bIsMDIActivate )
     }
 
     ViewShell::Activate( bIsMDIActivate );
+    SfxShell::BroadcastContextForActivation(true);
+
     pOlView->SetLinks();
     pOlView->ConnectToApplication();
 

sd/source/ui/view/viewshel.cxx
@@ -304,7 +304,7 @@ void ViewShell::Exit (void)
  */
 void ViewShell::Activate(sal_Bool bIsMDIActivate)
 {
-    SfxShell::Activate(bIsMDIActivate);
+    // Do not forward to SfxShell::Activate()
 
     /* According to MI, nobody is allowed to call GrabFocus, who does not
        exactly know from which window the focus is grabbed. Since Activate()

include/sfx2/shell.hxx
@@ -238,7 +238,20 @@ public:
     virtual SfxItemSet*         CreateItemSet( sal_uInt16 nId );
     virtual void                ApplyItemSet( sal_uInt16 nId, const SfxItemSet& rSet );
 
-    void                        SetContextName (const ::rtl::OUString& rsContextName);
+    /** Set the name of the sidebar context that is broadcast on calls
+        to Activation().
+    */
+    void SetContextName (const ::rtl::OUString& rsContextName);
+
+    /** Broadcast a sidebar context change.
+        This method is typically called from Activate() or
+        Deactivate().
+        @param bIsActivated
+            When <TRUE/> then broadcast the context name that was
+            defined with an earlier call to SetContextName().
+            When <FALSE/> then broadcast the 'default' context.
+    */
+    void BroadcastContextForActivation (const bool bIsActivated);
 
 #ifndef _SFXSH_HXX
     SAL_DLLPRIVATE bool CanExecuteSlot_Impl( const SfxSlot &rSlot );

include/svx/sidebar/SelectionAnalyzer.hxx
@@ -37,19 +37,25 @@ class SVX_DLLPUBLIC SelectionAnalyzer
 public :
     static sfx2::sidebar::EnumContext::Context GetContextForSelection_SC (
         const SdrMarkList& rMarkList);
+
+    enum ViewType
+    {
+        VT_Standard,
+        VT_Master,
+        VT_Handout,
+        VT_Notes,
+        VT_Outline
+    };
     static sfx2::sidebar::EnumContext::Context GetContextForSelection_SD (
         const SdrMarkList& rMarkList,
-        const bool bIsMasterPage,
-        const bool bIsHandoutPage,
-        const bool bIsNotesPage);
+        const ViewType eViewType);
 
 private:
     static sfx2::sidebar::EnumContext::Context GetContextForObjectId_SC (
         const sal_uInt16 nObjectId);
     static sfx2::sidebar::EnumContext::Context GetContextForObjectId_SD (
         const sal_uInt16 nObjectId,
-        const bool bIsHandoutPage,
-        const bool bIsNotesPage);
+        const ViewType eViewType);
     static sal_uInt32 GetInventorTypeFromMark (
         const SdrMarkList& rMarkList);
     static sal_uInt16 GetObjectTypeFromMark (

include/svx/sidebar/SelectionChangeHandler.hxx
@@ -53,7 +53,7 @@ class SVX_DLLPUBLIC SelectionChangeHandler
 {
 public:
     SelectionChangeHandler (
-        const boost::function<sfx2::sidebar::EnumContext::Context(void)>& rSelectionChangeCallback,
+        const boost::function<rtl::OUString(void)>& rSelectionChangeCallback,
         const cssu::Reference<css::frame::XController>& rxController,
         const sfx2::sidebar::EnumContext::Context eDefaultContext);
     virtual ~SelectionChangeHandler (void);
@@ -71,7 +71,7 @@ public:
     void Disconnect (void);
 
 private:
-    const boost::function<sfx2::sidebar::EnumContext::Context(void)> maSelectionChangeCallback;
+    const boost::function<rtl::OUString(void)> maSelectionChangeCallback;
     cssu::Reference<css::frame::XController> mxController;
     const sfx2::sidebar::EnumContext::Context meDefaultContext;
     bool mbIsConnected;

sc/source/ui/drawfunc/drawsh2.cxx
@@ -35,6 +35,7 @@
 #include <svx/fontworkbar.hxx>
 #include <svx/sidebar/SelectionChangeHandler.hxx>
 #include <svx/sidebar/SelectionAnalyzer.hxx>
+#include <svx/sidebar/ContextChangeEventMultiplexer.hxx>
 
 #include "drawsh.hxx"
 #include "drawview.hxx"
@@ -61,7 +62,7 @@ ScDrawShell::ScDrawShell( ScViewData* pData ) :
     SfxShell(pData->GetViewShell()),
     pViewData( pData ),
     mpSelectionChangeHandler(new svx::sidebar::SelectionChangeHandler(
-            ::boost::bind(&ScDrawShell::GetContextForSelection, this),
+            ::boost::bind(&ScDrawShell::GetSidebarContextName, this),
             GetFrame()->GetFrame().GetController(),
             sfx2::sidebar::EnumContext::Context_Cell))
 {
@@ -396,10 +397,27 @@ void ScDrawShell::GetDrawAttrStateForIFBX( SfxItemSet& rSet )
     }
 }
 
-sfx2::sidebar::EnumContext::Context ScDrawShell::GetContextForSelection (void)
+
+
+
+void ScDrawShell::Activate (const sal_Bool bMDI)
 {
-    return ::svx::sidebar::SelectionAnalyzer::GetContextForSelection_SC(
-        GetDrawView()->GetMarkedObjectList());
+    (void)bMDI;
+
+    ContextChangeEventMultiplexer::NotifyContextChange(
+        GetFrame()->GetFrame().GetController(),
+        ::sfx2::sidebar::EnumContext::GetContextEnum(
+            GetSidebarContextName()));
+}
+
+
+
+
+::rtl::OUString ScDrawShell::GetSidebarContextName (void)
+{
+    return sfx2::sidebar::EnumContext::GetContextName(
+        ::svx::sidebar::SelectionAnalyzer::GetContextForSelection_SC(
+            GetDrawView()->GetMarkedObjectList()));
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sc/source/ui/inc/drawsh.hxx
@@ -23,7 +23,6 @@
 #include <sfx2/shell.hxx>
 #include "shellids.hxx"
 #include <sfx2/module.hxx>
-#include <sfx2/sidebar/EnumContext.hxx>
 #include <svx/svdmark.hxx>
 #include <tools/link.hxx>
 #include <rtl/ref.hxx>
@@ -45,6 +44,7 @@ class ScDrawShell : public SfxShell
     void SetHlinkForObject( SdrObject* pObj, const OUString& rHlnk );
 
 protected:
+    virtual void    Activate(sal_Bool bMDI);
     ScViewData* GetViewData()   { return pViewData; }
 
 public:
@@ -83,7 +83,7 @@ public:
     sal_Bool    AreAllObjectsOnLayer(sal_uInt16 nLayerNo,const SdrMarkList& rMark);
 
     void GetDrawAttrStateForIFBX( SfxItemSet& rSet );
-    ::sfx2::sidebar::EnumContext::Context GetContextForSelection (void);
+    ::rtl::OUString GetSidebarContextName (void);
 };
 
 

commit 4a578dc9c7b9c5697f624807790565c99ccb919d

Resolves: fdo#66924 switching to master view is broken
It seems 0143805a565418d2a114c16b7eeba3b784176d9e wanted to not call
SfxShell::Activate from ViewShell::Activate and removed that call and added an
explicit SfxShell::BroadcastContextForActivation(true) (which is the body of
SfxShell::Activate) to OutlineViewShell::Activate for the case where it was
apparently wanted.

Then DrawViewShell::Deactivate had the call to ViewShell::Deactivate removed
persumably to avoid calling SfxShell::Deactivate via ViewShell::Deactivate

But with ViewShell::Deactivate omitted switching to master view is broken, it
does quite a lot more than call just SfxShell::Deactivate

Restore the call of ViewShell::Deactivate from DrawViewShell::Deactivate and
remove the call of SfxShell::Deactivate from ViewShell::Deactivate and put its
equivalent of SfxShell::BroadcastContextForActivation(false) into
OutlineViewShell::Deactivate to balance OutlineViewShell::Activate

regression since 0143805a565418d2a114c16b7eeba3b784176d9e

Change-Id: I5175378af6a6527a8be4d5011f13029d028b4304

sd/source/ui/view/drviews1.cxx
@@ -114,9 +114,9 @@ void DrawViewShell::UIDeactivated( SfxInPlaceClient* pCli )
 }
 
 
-void DrawViewShell::Deactivate(sal_Bool /*bIsMDIActivate*/)
+void DrawViewShell::Deactivate(sal_Bool bIsMDIActivate)
 {
-    // Do not forward to ViewShell::Deactivate() to prevent a context change.
+    ViewShell::Deactivate(bIsMDIActivate);
 }
 
 namespace

sd/source/ui/view/outlnvsh.cxx
@@ -383,6 +383,7 @@ void OutlineViewShell::Deactivate( sal_Bool bIsMDIActivate )
     // Links must be kept also on deactivated viewshell, to allow drag'n'drop
     // to function properly
     ViewShell::Deactivate( bIsMDIActivate );
+    SfxShell::BroadcastContextForActivation(false);
 }
 
 /**

sd/source/ui/view/viewshel.cxx
@@ -403,8 +403,7 @@ void ViewShell::Deactivate(sal_Bool bIsMDIActivate)
         mpHorizontalRuler->SetActive(sal_False);
     if (mpVerticalRuler.get() != NULL)
         mpVerticalRuler->SetActive(sal_False);
-
-    SfxShell::Deactivate(bIsMDIActivate);
+    // Do not forward to SfxShell::Deactivate()
 }
 
 