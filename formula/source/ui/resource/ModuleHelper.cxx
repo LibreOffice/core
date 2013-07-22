b32a9e9ccb685b663941a1f4aa945a70ef784446
Julien Nabet <serval2412@yahoo.fr>
2013-07-22 23:16:06 +0200

    fdo#46037: no more comphelper/configurationhelper.hxx in formula module
    Change-Id: I6ed4af811d96cb0ddbb2093d6627d4d39836a033

--- formula/source/ui/resource/ModuleHelper.cxx
+++ formula/source/ui/resource/ModuleHelper.cxx
@@ -17,7 +17,6 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 #include "ModuleHelper.hxx"
-#include <comphelper/configurationhelper.hxx>
 #include <comphelper/processfactory.hxx>
 #include <osl/thread.h>
 #include <com/sun/star/util/XMacroExpander.hpp>
