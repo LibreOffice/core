LibreOffice/core

commit 20430c74f391ceffd8a063c0b6b8a8fd615abac0

fdo#46037: no more comphelper/configurationhelper.hxx in uui
Change-Id: Ie7f42eb02f38d5cac2eff7b434ca369ea28b4ed1
Reviewed-on: https://gerrit.libreoffice.org/5065
Reviewed-by: Fridrich Strba <fridrich@documentfoundation.org>
Tested-by: Fridrich Strba <fridrich@documentfoundation.org>

uui/source/newerverwarn.cxx
@@ -31,11 +31,11 @@
 #include <com/sun/star/container/XNameReplace.hpp>
 
 #include <comphelper/processfactory.hxx>
-#include <comphelper/configurationhelper.hxx>
 #include <rtl/bootstrap.hxx>
 #include <tools/diagnose_ex.h>
 #include <vcl/msgbox.hxx>
 #include <osl/process.h>
+#include <officecfg/Office/Addons.hxx>
 
 using namespace com::sun::star;
 
@@ -105,15 +105,10 @@ IMPL_LINK_NOARG(NewerVersionWarningDialog, UpdateHdl)
 
             // TODO: do we need to respect the bUpdateCheckEnabled flag? Finally, its meaning is "are automatic
             // updates enabled", but this here is not an automatic update, but one triggered explicitly by the user.
+            css::uno::Reference< css::container::XNameAccess > xOfficeHelp = officecfg::Office::Addons::AddonUI::OfficeHelp::get(xContext);
 
-            uno::Any aVal = ::comphelper::ConfigurationHelper::readDirectKey(
-                                    xContext,
-                                    "org.openoffice.Office.Addons/",
-                                    "AddonUI/OfficeHelp/UpdateCheckJob",
-                                    "URL",
-                                    ::comphelper::ConfigurationHelper::E_READONLY );
             util::URL aURL;
-            if ( aVal >>= aURL.Complete )
+            if ( xOfficeHelp->getByName("UpdateCheckJob") >>= aURL.Complete )
             {
                 uno::Reference< util::XURLTransformer > xTransformer( util::URLTransformer::create(xContext) );
                 xTransformer->parseStrict( aURL );

uui/Library_uui.mk
@@ -13,6 +13,10 @@ $(eval $(call gb_Library_set_componentfile,uui,uui/util/uui))
 
 $(eval $(call gb_Library_use_external,uui,boost_headers))
 
+$(eval $(call gb_Library_use_custom_headers,uui,\
+    officecfg/registry \
+))
+
 $(eval $(call gb_Library_use_sdk_api,uui))
 
 $(eval $(call gb_Library_use_libraries,uui,\

commit 0d874ee2e452ea67c03a27bf1a7f26d0ffc617dc

Fix configuration access path for "URL" group member
...broken with 20430c74f391ceffd8a063c0b6b8a8fd615abac0 "fdo#46037: no more
comphelper/configurationhelper.hxx in uui" (but note that
NewerVersionWarningDialog is dead code anyway since
718c0fe73251be083d032f0905880058128d4054 "Ported
sfx2-remove-check-update-on-fileload.diff from ooo-build."

Change-Id: I7095044b73888aad12704d76a39551158e6c6b33

uui/source/newerverwarn.cxx
@@ -28,6 +28,7 @@
 #include <com/sun/star/setup/UpdateCheckConfig.hpp>
 #include <com/sun/star/util/URLTransformer.hpp>
 #include <com/sun/star/util/XURLTransformer.hpp>
+#include <com/sun/star/container/XHierarchicalNameAccess.hpp>
 #include <com/sun/star/container/XNameReplace.hpp>
 
 #include <comphelper/processfactory.hxx>
@@ -105,10 +106,10 @@ IMPL_LINK_NOARG(NewerVersionWarningDialog, UpdateHdl)
 
             // TODO: do we need to respect the bUpdateCheckEnabled flag? Finally, its meaning is "are automatic
             // updates enabled", but this here is not an automatic update, but one triggered explicitly by the user.
-            css::uno::Reference< css::container::XNameAccess > xOfficeHelp = officecfg::Office::Addons::AddonUI::OfficeHelp::get(xContext);
+            css::uno::Reference< css::container::XHierarchicalNameAccess > xOfficeHelp(officecfg::Office::Addons::AddonUI::OfficeHelp::get(xContext), css::uno::UNO_QUERY_THROW);
 
             util::URL aURL;
-            if ( xOfficeHelp->getByName("UpdateCheckJob") >>= aURL.Complete )
+            if ( xOfficeHelp->getByHierarchicalName("['UpdateCheckJob']/URL") >>= aURL.Complete )
             {
                 uno::Reference< util::XURLTransformer > xTransformer( util::URLTransformer::create(xContext) );
                 xTransformer->parseStrict( aURL );