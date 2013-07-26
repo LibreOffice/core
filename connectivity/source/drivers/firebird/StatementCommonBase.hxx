1d8eab5478168dbd2b0182db9f7b52b6d2602931
Andrzej J.R. Hunt <andrzej@ahunt.org>
2013-07-26 17:49:13 +0200

    Update licence headers / rebase on APL'd skeleton driver (firebird-sdbc)
    The firebird driver was created on the basis of the skeleton driver
    (odk/examples/DevelopersGuide/Database/DriverSkeleton/). This is under
    BSD licence within the LO database but was relicenced as ALV2 within AOO.
    (see http://svn.apache.org/viewvc?view=revision&revision=1239200 )
    
    MPL added as all my changes to the driver are released as MPL.
    
    was under the BSD Licence in the
    
    Change-Id: Iea22043281fbc16f6813ce97dce449b10914a91d

--- connectivity/source/drivers/firebird/StatementCommonBase.hxx
+++ connectivity/source/drivers/firebird/StatementCommonBase.hxx
@@ -1,37 +1,21 @@
 /* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
-/*************************************************************************
+/*
+ * This file is part of the LibreOffice project.
  *
- *  The Contents of this file are made available subject to the terms of
- *  the BSD license.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- *  Copyright 2000, 2010 Oracle and/or its affiliates.
- *  All rights reserved.
+ * This file incorporates work covered by the following license notice:
  *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *  1. Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *  2. Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *  3. Neither the name of Sun Microsystems, Inc. nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
- *  OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- *  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
- *  TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
- *  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- *************************************************************************/
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
 
 #ifndef CONNECTIVITY_FIREBIRD_STATEMENT_BASE_HXX
 #define CONNECTIVITY_FIREBIRD_STATEMENT_BASE_HXX
