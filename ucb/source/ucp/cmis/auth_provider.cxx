830c24751483abfcf61b3d31e4c270fc45dac982
Michael Meeks <michael.meeks@suse.com>
2013-07-26 12:01:16 +0100

    targetted clean of redundant header piece from 62badf3828
    Change-Id: Ic1240114d667fb7797afae4847427cc889f3cb48
    (cherry picked from commit 03993b47c522dcc71f7cd73ee21df3de9650080d)

--- ucb/source/ucp/cmis/auth_provider.cxx
+++ ucb/source/ucp/cmis/auth_provider.cxx
@@ -5,9 +5,6 @@
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- *
- * This file incorporates work covered by the following license notice:
- *
  */
 
 #define OUSTR_TO_STDSTR(s) string( OUStringToOString( s, RTL_TEXTENCODING_UTF8 ).getStr() )
