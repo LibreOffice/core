15066dc6be358287ec69e5ca63a77106558944b2
Lionel Elie Mamane <lionel@mamane.lu>
2013-07-30 05:21:33 +0200

    fixup SAL_WARN log area
    Change-Id: Ie0848f8dc7d0bce171a2081dd91f48519414eebe

--- dbaccess/source/ui/dlg/generalpage.cxx
+++ dbaccess/source/ui/dlg/generalpage.cxx
@@ -332,7 +332,7 @@ namespace dbaui
         sal_uInt16 nSelected = _pBox->GetSelectEntryPos();
         if (nSelected >= m_aURLPrefixes.size() )
         {
-            SAL_WARN("dbaui.OGeneralPage", "Got out-of-range value '" << nSelected <<  "' from the DatasourceType selection ListBox's GetSelectEntryPos(): no corresponding URL prefix");
+            SAL_WARN("dbaccess.ui.OGeneralPage", "Got out-of-range value '" << nSelected <<  "' from the DatasourceType selection ListBox's GetSelectEntryPos(): no corresponding URL prefix");
             return 0L;
         }
         const OUString sURLPrefix = m_aURLPrefixes[ nSelected ];
