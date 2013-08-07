38e24914ef87dc3247f736eb67bb17463388707b
Olivier Hallot <olivier.hallot@edx.srv.br>
2013-08-06 22:44:34 -0300

    Convert line end change warning to widget UI
    Change-Id: I1aca130736d2c45a251a3551976e549f107cad0e
    Reviewed-on: https://gerrit.libreoffice.org/5298
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>

--- cui/UIConfig_cui.mk
+++ cui/UIConfig_cui.mk
@@ -88,6 +88,7 @@ $(eval $(call gb_UIConfig_add_uifiles,cui,\
 	cui/uiconfig/ui/picknumberingpage \
 	cui/uiconfig/ui/pickoutlinepage \
 	cui/uiconfig/ui/positionpage \
+	cui/uiconfig/ui/querychangelineenddialog \
 	cui/uiconfig/ui/querydeletechartcolordialog \
 	cui/uiconfig/ui/querydeletedictionarydialog \
 	cui/uiconfig/ui/querydeletelineenddialog \
