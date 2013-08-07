563ea4528ecde5bd7d4aa4627200929c6f3b29cb
Olivier Hallot <olivier.hallot@edx.srv.br>
2013-08-06 22:17:07 -0300

    Convert delete arrow warning to widget UI
    Change-Id: I9848233357b5cc3b07e66e1ef689db1ae7f28075

--- cui/source/tabpages/tplneend.cxx
+++ cui/source/tabpages/tplneend.cxx
@@ -495,7 +495,9 @@ IMPL_LINK_NOARG(SvxLineEndDefTabPage, ClickDeleteHdl_Impl)
 
     if( nPos != LISTBOX_ENTRY_NOTFOUND )
     {
-        QueryBox aQueryBox( GetParentDialog(), WinBits( WB_YES_NO | WB_DEF_NO ), OUString( CUI_RES( RID_SVXSTR_ASK_DEL_LINEEND ) ) );
+        MessageDialog aQueryBox( GetParentDialog()
+                                ,"AskDelLineEndDialog"
+                                ,"cui/ui/querydeletelineenddialog.ui");
 
         if ( aQueryBox.Execute() == RET_YES )
         {
