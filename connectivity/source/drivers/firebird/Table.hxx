73341eddbf3e42a121d80cada4d1252af80f5396
Andrzej J.R. Hunt <andrzej@ahunt.org>
2013-08-08 22:00:36 +0100

    Add sdbcx.Table (firebird-sdbc)
    Change-Id: Iae9a4d1292d0dcd2da17c5a5552a3986b306a550

---
+++ connectivity/source/drivers/firebird/Table.hxx
@@ -0,0 +1,29 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef CONNECTIVITY_FIREBIRD_TABLE_HXX
+#define CONNECTIVITY_FIREBIRD_TABLE_HXX
+
+#include <connectivity/TTableHelper.hxx>
+
+namespace connectivity
+{
+    namespace firebird
+    {
+
+        class Table: public OTableHelper
+        {
+        };
+
+    } // namespace firebird
+} // namespace connectivity
+
+#endif // CONNECTIVITY_FIREBIRD_TABLE_HXX
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
