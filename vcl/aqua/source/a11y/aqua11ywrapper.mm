487d9ffe0480470fb7c03fe71e466e8bcc700c33
Boris Dušek <me@dusek.me>
2013-08-11 16:34:07 +0200

    Add XAccessibleTextMarkup to AquaA11yWrapper
    Change-Id: Ic15aeb46b384531c4d64d6ec8a103f3e44068cf1
    Reviewed-on: https://gerrit.libreoffice.org/5350
    Reviewed-by: Tor Lillqvist <tml@iki.fi>
    Tested-by: Tor Lillqvist <tml@iki.fi>

--- vcl/aqua/source/a11y/aqua11ywrapper.mm
+++ vcl/aqua/source/a11y/aqua11ywrapper.mm
@@ -107,6 +107,8 @@ static std::ostream &operator<<(std::ostream &s, NSPoint point) {
         mpReferenceWrapper -> rAccessibleTextAttributes = Reference < XAccessibleTextAttributes > ( rxAccessibleContext, UNO_QUERY );
         // XAccessibleMultiLineText
         mpReferenceWrapper -> rAccessibleMultiLineText = Reference < XAccessibleMultiLineText > ( rxAccessibleContext, UNO_QUERY );
+        // XAccessibleTextMarkup
+        mpReferenceWrapper -> rAccessibleTextMarkup = Reference < XAccessibleTextMarkup > ( rxAccessibleContext, UNO_QUERY );
         // XAccessibleEventBroadcaster
         #if 0
         /* #i102033# NSAccessibility does not seemt to know an equivalent for transient children.
@@ -1111,6 +1113,10 @@ Reference < XAccessibleContext > hitTestRunner ( com::sun::star::awt::Point poin
 
 -(XAccessibleMultiLineText *)accessibleMultiLineText {
     return mpReferenceWrapper -> rAccessibleMultiLineText.get();
+}
+
+-(XAccessibleTextMarkup *)accessibleTextMarkup {
+    return mpReferenceWrapper -> rAccessibleTextMarkup.get();
 }
 
 -(NSView *)viewElementForParent {
