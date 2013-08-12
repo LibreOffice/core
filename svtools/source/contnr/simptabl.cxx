ae035a32737a77be75c9d017f7f5a90a05048e06
Caolán McNamara <caolanm@redhat.com>
2013-08-12 11:00:50 +0100

    Resolves: fdo#67944 set dialogcontrol bit to tab into simpletable
    Change-Id: I1ea653d5546961096a9368b4d1d9a1dd39a80745

--- svtools/source/contnr/simptabl.cxx
+++ svtools/source/contnr/simptabl.cxx
@@ -43,7 +43,7 @@ SvSimpleTableContainer::SvSimpleTableContainer(Window* pParent, WinBits nBits)
 extern "C" SAL_DLLPUBLIC_EXPORT Window* SAL_CALL makeSvSimpleTableContainer(Window *pParent,
     VclBuilder::stringmap &)
 {
-    return new SvSimpleTableContainer(pParent, WB_TABSTOP);
+    return new SvSimpleTableContainer(pParent, WB_TABSTOP | WB_DIALOGCONTROL);
 }
 
 void SvSimpleTableContainer::SetTable(SvSimpleTable* pTable)
