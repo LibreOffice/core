46d122d4cc405c4070eb4945abd20cdf3a5fac33
Noel Power <noel.power@suse.com>
2013-08-14 12:21:55 +0100

    Revert "Always disable anti-aliasing for drawing cell borders." fdo#60805
    Unfortunately this patch creates a very weird ( and took me forever to find
    ) bug ( fdo#60805 ). Basically certain double line borders seems to
    dissappear at certain zoom levels. I guess maybe its possible to tweak
    the create2DDecomposition to process the line as hairline at those problematic
    zoom levels ( but I suspect that actually would look worse ) Best maybe
    would be to tweak the rendering/painting ( but isn't that processor specific
    maybe ? ) as is maybe this problem. I admit this is outside my comfort zone :/
    
    This reverts commit a551cad4e35b6b664167d65dfc25e5a0f6990687.
    
    Change-Id: Idb25493ffb038cb1f8ae9b364a29052abafd940d

--- sc/source/ui/view/output.cxx
+++ sc/source/ui/view/output.cxx
@@ -24,7 +24,6 @@
 #include <editeng/brushitem.hxx>
 #include <editeng/editdata.hxx>
 #include <svtools/colorcfg.hxx>
-#include "svtools/optionsdrawinglayer.hxx"
 #include <svx/rotmodit.hxx>
 #include <editeng/shaditem.hxx>
 #include <editeng/svxfont.hxx>
@@ -1273,7 +1272,10 @@ void ScOutputData::DrawClear()
     }
 }
 
-namespace {
+
+//
+//  Linien
+//
 
 long lclGetSnappedX( OutputDevice& rDev, long nPosX, bool bSnapPixel )
 {
@@ -1290,32 +1292,8 @@ size_t lclGetArrayColFromCellInfoX( sal_uInt16 nCellInfoX, sal_uInt16 nCellInfoF
     return static_cast< size_t >( bRTL ? (nCellInfoLastX + 2 - nCellInfoX) : (nCellInfoX - nCellInfoFirstX) );
 }
 
-/**
- * Temporarily turn off antialiasing.
- */
-class AntiAliasingSwitch
-{
-    SvtOptionsDrawinglayer maDrawOpt;
-    bool mbOldSetting;
-public:
-    AntiAliasingSwitch(bool bOn) : mbOldSetting(maDrawOpt.IsAntiAliasing())
-    {
-        maDrawOpt.SetAntiAliasing(bOn);
-    }
-
-    ~AntiAliasingSwitch()
-    {
-        maDrawOpt.SetAntiAliasing(mbOldSetting);
-    }
-};
-
-}
-
 void ScOutputData::DrawFrame()
 {
-    // No anti-aliasing for drawing cell borders.
-    AntiAliasingSwitch aAASwitch(false);
-
     sal_uLong nOldDrawMode = mpDev->GetDrawMode();
 
     Color aSingleColor;
