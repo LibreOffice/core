376feb6a6fcd32887b50fba790a3230baf3dae98
Stephan Bergmann <sbergman@redhat.com>
2013-08-14 15:54:46 +0200

    left shift of 1 by 31 places cannot be represented in type 'int'
    Clang -fsanitize=undefined
    
    Change-Id: I2b3f1bd28e098d5eb57892d2c7438c18c5c3b357

--- soltools/cpp/cpp.h
+++ soltools/cpp/cpp.h
@@ -127,8 +127,8 @@ typedef struct wraplist
 }   Wraplist;
 
 #define new(t)  (t *)domalloc(sizeof(t))
-#define quicklook(a,b)  (namebit[(a)&077] & (1<<((b)&037)))
-#define quickset(a,b)   namebit[(a)&077] |= (1<<((b)&037))
+#define quicklook(a,b)  (namebit[(a)&077] & (1U<<((b)&037)))
+#define quickset(a,b)   namebit[(a)&077] |= (1U<<((b)&037))
 extern unsigned long namebit[077 + 1];
 
 enum errtype
