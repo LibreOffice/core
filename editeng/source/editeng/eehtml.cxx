79adda35a9963eeac00172f64ca8013f4b3923d1
Michael Stahl <mstahl@redhat.com>
2013-08-15 12:34:17 +0200

    EditHTMLParser::StartPara: shut up clang tinderbox
    Change-Id: I8d65f43b95944583b50874adb90db38eee7623de

--- editeng/source/editeng/eehtml.cxx
+++ editeng/source/editeng/eehtml.cxx
@@ -717,11 +717,12 @@ void EditHTMLParser::StartPara( bool bReal )
             {
                 case HTML_O_ALIGN:
                 {
-                    if ( aOption.GetString().equalsIgnoreAsciiCaseAscii( OOO_STRING_SVTOOLS_HTML_AL_right ) )
+                    OUString const& rTmp(aOption.GetString());
+                    if (rTmp.equalsIgnoreAsciiCase(OOO_STRING_SVTOOLS_HTML_AL_right))
                         eAdjust = SVX_ADJUST_RIGHT;
-                    else if ( aOption.GetString().equalsIgnoreAsciiCaseAscii( OOO_STRING_SVTOOLS_HTML_AL_middle ) )
+                    else if (rTmp.equalsIgnoreAsciiCase(OOO_STRING_SVTOOLS_HTML_AL_middle))
                         eAdjust = SVX_ADJUST_CENTER;
-                    else if ( aOption.GetString().equalsIgnoreAsciiCaseAscii( OOO_STRING_SVTOOLS_HTML_AL_center ) )
+                    else if (rTmp.equalsIgnoreAsciiCase(OOO_STRING_SVTOOLS_HTML_AL_center))
                         eAdjust = SVX_ADJUST_CENTER;
                     else
                         eAdjust = SVX_ADJUST_LEFT;
