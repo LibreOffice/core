ddba5a4360e88ac4f6f848ac74ab1f350b9b7d5f
Tor Lillqvist <tlillqvist@suse.com>
2013-08-16 23:42:22 +0300

    SAL_INFO is not available here
    Change-Id: Ic40cb29d9ade32690903bfa0f3993782711cc53f

--- sd/source/ui/remotecontrol/WINNetworkService.cxx
+++ sd/source/ui/remotecontrol/WINNetworkService.cxx
@@ -7,13 +7,10 @@ void sd::WINNetworkService::setup()
 {
     DNSServiceErrorType err = DNSServiceRegister(&client, 0, 0, NULL, kREG_TYPE, "local", NULL, 1599, 1, "", NULL, this );
 
-    if ( err == 0 ) {
-        SAL_INFO("sd", "Windows bonjour service setup");
-    } // Fail silently otherwise
+    // Fail silently
 }
 
 void sd::WINNetworkService::clear()
 {
     DNSServiceRefDeallocate(client);
-    SAL_INFO("sd", "Windows mDNSResponder removed");
 }
