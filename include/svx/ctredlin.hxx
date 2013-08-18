LibreOffice/core

commit 8655095a4910ca795dfd182e6796d9375727a694

try and beat this weird SvxAcceptChgCtr into something sanish
a) convert to .ui

b) The accept/reject changes dialogs now have a "close" button and the
accept/reject etc buttons are relocated into the main dialog button area rather
than as direct children of the view page. They now get disabled on switching to
the non-view page.

c) Merges the sc redline changes dialog, the sw redline changes dialog and the
sw autocorrect changes dialog

Change-Id: If0ba376c3f2d36709aeeed995fa9901741434c40

svx/source/dialog/ctredlin.cxx
@@ -17,6 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
+#include <vcl/dialog.hxx>
 #include <vcl/layout.hxx>
 #include <vcl/svapp.hxx>
 #include <tools/shl.hxx>
@@ -486,17 +487,18 @@ void SvxRedlinTable::InitEntry(SvTreeListEntry* pEntry, const OUString& rStr,
 //  class SvxTPView
 //----------------------------------------------------------------------------
 
-SvxTPView::SvxTPView( Window * pParent)
+SvxTPView::SvxTPView(Window * pParent)
     : TabPage(pParent, "RedlineViewPage", "svx/ui/redlineviewpage.ui")
 {
-    get(m_pAccept, "accept");
-    get(m_pReject, "reject");
-    get(m_pAcceptAll, "acceptall");
-    get(m_pRejectAll, "rejectall");
-    get(m_pUndo, "undo");
+    Dialog *pTopLevel = GetParentDialog();
+    pTopLevel->get(m_pAccept, "accept");
+    pTopLevel->get(m_pReject, "reject");
+    pTopLevel->get(m_pAcceptAll, "acceptall");
+    pTopLevel->get(m_pRejectAll, "rejectall");
+    pTopLevel->get(m_pUndo, "undo");
 
     SvxSimpleTableContainer* pTable = get<SvxSimpleTableContainer>("changes");
-    Size aControlSize(221, 130);
+    Size aControlSize(221, 65);
     aControlSize = LogicToPixel(aControlSize, MAP_APPFONT);
     pTable->set_width_request(aControlSize.Width());
     pTable->set_height_request(aControlSize.Height());
@@ -511,6 +513,26 @@ SvxTPView::SvxTPView( Window * pParent)
     m_pUndo->SetClickHdl(aLink);
 
     m_pViewData->SetTabs(nStaticTabs);
+}
+
+void SvxTPView::ActivatePage()
+{
+    m_pAccept->Enable();
+    m_pReject->Enable();
+    m_pAcceptAll->Enable();
+    m_pRejectAll->Enable();
+    m_pUndo->Enable();
+    TabPage::ActivatePage();
+}
+
+void SvxTPView::DeactivatePage()
+{
+    TabPage::DeactivatePage();
+    m_pAccept->Disable();
+    m_pReject->Disable();
+    m_pAcceptAll->Disable();
+    m_pRejectAll->Disable();
+    m_pUndo->Disable();
 }
 
 SvxTPView::~SvxTPView()
@@ -1153,40 +1175,28 @@ IMPL_LINK( SvxTPFilter, RefHandle, PushButton*, pRef )
     return 0;
 }
 
-static Size gDiffSize;
 //============================================================================
 //  class SvxAcceptChgCtr
 //----------------------------------------------------------------------------
 
-SvxAcceptChgCtr::SvxAcceptChgCtr( Window* pParent, const ResId& rResId )
-        :   Control(pParent,rResId ),
-            aTCAccept(this,WB_TABSTOP |WB_DIALOGCONTROL)
+SvxAcceptChgCtr::SvxAcceptChgCtr(Window* pParent)
+    : TabControl(pParent, WB_TABSTOP | WB_DIALOGCONTROL)
 {
-    pTPFilter=new SvxTPFilter(&aTCAccept);
-    pTPView=new SvxTPView(&aTCAccept);
+    m_pUIBuilder = new VclBuilder(this, getUIRootDir(), "svx/ui/redlinecontrol.ui", "RedlineControl");
 
-    aMinSize = VclContainer::getLayoutRequisition(*pTPView);
+    pTPFilter = new SvxTPFilter(this);
+    pTPView = new SvxTPView(this);
 
-    aTCAccept.InsertPage( TP_VIEW,   SVX_RESSTR(RID_SVXSTR_VIEW));
-    aTCAccept.InsertPage( TP_FILTER, SVX_RESSTR(RID_SVXSTR_FILTER));
-    aTCAccept.SetTabPage( TP_VIEW,   pTPView);
-    aTCAccept.SetTabPage( TP_FILTER, pTPFilter);
-    aTCAccept.SetHelpId(HID_REDLINING_TABCONTROL);
-
-    aTCAccept.SetTabPageSizePixel(aMinSize);
-    Size aSize=aTCAccept.GetSizePixel();
-
-    gDiffSize.Height()=aSize.Height()-aMinSize.Height();
-    gDiffSize.Width()=aSize.Width()-aMinSize.Width();
-
+    m_nViewPageId = GetPageId("view");
+    m_nFilterPageId = GetPageId("filter");
+    SetTabPage(m_nViewPageId, pTPView);
+    SetTabPage(m_nFilterPageId, pTPFilter);
 
     pTPFilter->SetRedlinTable(GetViewTable());
-    WinBits nWinStyle=GetStyle()|WB_DIALOGCONTROL;
-    SetStyle(nWinStyle);
 
-    aTCAccept.Show();
     ShowViewPage();
-    Resize();
+
+    Show();
 }
 
 SvxAcceptChgCtr::~SvxAcceptChgCtr()
@@ -1195,50 +1205,14 @@ SvxAcceptChgCtr::~SvxAcceptChgCtr()
     delete pTPFilter;
 }
 
-void SvxAcceptChgCtr::Resize()
-{
-    aMinSize = VclContainer::getLayoutRequisition(*pTPView);
-    Size aSize=GetOutputSizePixel();
-    sal_Bool bFlag=sal_False;
-
-    if(aMinSize.Height()>aSize.Height())
-    {
-        aSize.Height()=aMinSize.Height();
-        bFlag=sal_True;
-    }
-    if(aMinSize.Width()>aSize.Width())
-    {
-        aSize.Width()=aMinSize.Width();
-        bFlag=sal_True;
-    }
-
-    if(bFlag)
-    {
-        SetOutputSizePixel(aSize);
-        aMinSizeLink.Call(this);
-    }
-
-    aSize.Height()-=2;
-    aSize.Width()-=2;
-    aTCAccept.SetSizePixel(aSize);
-}
-
-Size SvxAcceptChgCtr::GetMinSizePixel() const
-{
-    Size aSize = VclContainer::getLayoutRequisition(*pTPView);
-    aSize.Height()+=gDiffSize.Height();
-    aSize.Width()+=gDiffSize.Width();
-    return aSize;
-}
-
 void SvxAcceptChgCtr::ShowFilterPage()
 {
-    aTCAccept.SetCurPageId(TP_FILTER);
+    SetCurPageId(m_nFilterPageId);
 }
 
 void SvxAcceptChgCtr::ShowViewPage()
 {
-    aTCAccept.SetCurPageId(TP_VIEW);
+    SetCurPageId(m_nViewPageId);
 }
 
 SvxTPFilter* SvxAcceptChgCtr::GetFilterPage()
@@ -1253,15 +1227,7 @@ SvxTPView* SvxAcceptChgCtr::GetViewPage()
 
 SvxRedlinTable* SvxAcceptChgCtr::GetViewTable()
 {
-    if(pTPView!=NULL)
-    {
-        return pTPView->GetTableControl();
-    }
-    else
-    {
-        return NULL;
-    }
+    return pTPView ? pTPView->GetTableControl() : NULL;
 }
-
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

svx/inc/svx/ctredlin.hxx
@@ -351,32 +351,31 @@ public:
 
     void            SetUndoClickHdl( const Link& rLink ) { UndoClickLk = rLink; }
     const Link&     GetUndoAllClickHdl() const { return UndoClickLk; }
+
+    virtual void    ActivatePage();
+    virtual void    DeactivatePage();
 };
 
 //==================================================================
 //  Redlining - Control (Accept- Changes)
 //==================================================================
 
-class SVX_DLLPUBLIC SvxAcceptChgCtr : public Control
+class SVX_DLLPUBLIC SvxAcceptChgCtr
+    : public TabControl
+    , public VclBuilderContainer
 {
 private:
 
-    Link            aMinSizeLink;
-    TabControl      aTCAccept;
     SvxTPFilter*    pTPFilter;
     SvxTPView*      pTPView;
-    Size            aMinSize;
 
-protected:
-
-    virtual void    Resize();
+    sal_uInt16      m_nViewPageId;
+    sal_uInt16      m_nFilterPageId;
 
 public:
-                    SvxAcceptChgCtr( Window* pParent, const ResId& rResId );
+                    SvxAcceptChgCtr(Window* pParent);
 
                     ~SvxAcceptChgCtr();
-
-    Size            GetMinSizePixel() const;
 
     void            ShowFilterPage();
     void            ShowViewPage();
@@ -384,9 +383,6 @@ public:
     SvxTPFilter*    GetFilterPage();
     SvxTPView*      GetViewPage();
     SvxRedlinTable* GetViewTable();
-
-    void            SetMinSizeHdl( const Link& rLink ) { aMinSizeLink= rLink; }
-    const Link&     GetMinSizeHdl() const { return aMinSizeLink; }
 };
 
 

sc/inc/globstr.hrc
@@ -636,6 +636,23 @@
 
 #define STR_NO_INSERT_DELETE_OVER_PIVOT_TABLE 509
 
-#define STR_COUNT                   510
+#define STR_CHG_INSERT_COLS         510
+#define STR_CHG_INSERT_ROWS         511
+#define STR_CHG_INSERT_TABS         512
+#define STR_CHG_DELETE_COLS         513
+#define STR_CHG_DELETE_ROWS         514
+#define STR_CHG_DELETE_TABS         515
+#define STR_CHG_MOVE                516
+#define STR_CHG_CONTENT             517
+#define STR_CHG_REJECT              518
+#define STR_CHG_ACCEPTED            519
+#define STR_CHG_REJECTED            520
+#define STR_CHG_NO_ENTRY            521
+#define STR_CHG_CONTENT_WITH_CHILD  522
+#define STR_CHG_CHILD_CONTENT       523
+#define STR_CHG_CHILD_ORGCONTENT    524
+#define STR_CHG_EMPTY               525
+
+#define STR_COUNT                   526
 
 #endif

sc/source/ui/inc/acredlin.hrc
@@ -22,37 +22,6 @@
 #define BTN_CANCEL      2
 #define BTN_HELP        5
 
-#define CTR_REDLINING   10
-#define TP_FILTER       11
-#define TP_VIEW         12
-
-#define FT_ASSIGN       14
-#define ED_ASSIGN       15
-#define RB_ASSIGN       16
-
-#define STR_INSERT_COLS  20
-#define STR_INSERT_ROWS 21
-#define STR_INSERT_TABS 22
-#define STR_DELETE_COLS 23
-#define STR_DELETE_ROWS 24
-#define STR_DELETE_TABS 25
-#define STR_MOVE        26
-#define STR_CONTENT     27
-#define STR_REJECT      28
-#define STR_ACCEPTED    29
-#define STR_REJECTED    30
-#define STR_NO_ENTRY    31
-#define STR_CONTENT_WITH_CHILD  32
-#define STR_CHILD_CONTENT       33
-#define STR_CHILD_ORGCONTENT    34
-#define STR_EMPTY               35
-
-// Bitmaps
-#define BMP_STR_CLOSE       41
-#define BMP_STR_OPEN        42
-#define BMP_STR_END         43
-#define BMP_STR_ERROR       44
-
 #define SC_CHANGES_COMMENT  49
 #define SC_SUB_SORT         50
 #define SC_SORT_ACTION      51

sc/source/ui/inc/acredlin.hxx
@@ -99,7 +99,7 @@ private:
 
     Timer                   aSelectionTimer;
     Timer                   aReOpenTimer;
-    SvxAcceptChgCtr         aAcceptChgCtr;
+    SvxAcceptChgCtr*        m_pAcceptChgCtr;
     ScViewData*             pViewData;
     ScDocument*             pDoc;
     ScRangeName             aLocalRangeName;
@@ -107,7 +107,6 @@ private:
     SvxTPFilter*            pTPFilter;
     SvxTPView*              pTPView;
     SvxRedlinTable*         pTheView; // #i48648 now SvHeaderTabListBox
-    Size                    MinSize;
     ScRangeList             aRangeList;
     ScChangeViewSettings    aChangeViewSet;
     rtl::OUString           aStrInsertCols;
@@ -143,7 +142,6 @@ private:
     DECL_LINK( FilterHandle, SvxTPFilter* );
     DECL_LINK( RefHandle, void* );
     DECL_LINK( FilterModified, void* );
-    DECL_LINK( MinSizeHandle, SvxAcceptChgCtr*);
     DECL_LINK( RejectHandle, SvxTPView*);
     DECL_LINK( AcceptHandle, SvxTPView*);
     DECL_LINK( RejectAllHandle, void*);

sc/source/ui/inc/highred.hrc
@@ -26,8 +26,6 @@
 #define CB_HIGHLIGHT_REJECT 7
 
 #define FL_FILTER           10
-#define TP_FILTER           11
-#define TP_VIEW             12
 #define CB_HIGHLIGHT        13
 
 #define FT_ASSIGN           14
@@ -43,9 +41,3 @@
 #define STR_MOVE        26
 #define STR_CONTENT     27
 #define STR_REJECT      28
-
-// Bitmaps
-#define BMP_STR_CLOSE       31
-#define BMP_STR_OPEN        32
-#define BMP_STR_END         33
-#define BMP_STR_ERROR       34

sc/source/ui/inc/optdlg.hrc
@@ -21,7 +21,6 @@
 #define TP_GENERAL          1
 #define TP_SAVE             2
 #define TP_PATH             3
-#define TP_VIEW             5
 #define TP_CALC             6
 #define TP_USERLISTS        7
 #define TP_PRINT            8

sc/source/ui/miscdlgs/acredlin.cxx
@@ -93,30 +93,29 @@ ScRedlinData::~ScRedlinData()
 //============================================================================
 //  class ScAcceptChgDlg
 //----------------------------------------------------------------------------
-ScAcceptChgDlg::ScAcceptChgDlg( SfxBindings* pB, SfxChildWindow* pCW, Window* pParent,
-                      ScViewData*       ptrViewData)
-
-    :   SfxModelessDialog( pB, pCW, pParent, ScResId(RID_SCDLG_CHANGES) ),
-        aAcceptChgCtr           ( this, ScResId( CTR_REDLINING ) ),
+ScAcceptChgDlg::ScAcceptChgDlg(SfxBindings* pB, SfxChildWindow* pCW, Window* pParent,
+    ScViewData* ptrViewData)
+    : SfxModelessDialog(pB, pCW, pParent,
+        "AcceptRejectChangesDialog", "svx/ui/acceptrejectchangesdialog.ui"),
         pViewData       ( ptrViewData ),
         pDoc            ( ptrViewData->GetDocument() ),
         aLocalRangeName ( *(pDoc->GetRangeName()) ),
-        aStrInsertCols       (SC_RESSTR(STR_INSERT_COLS)),
-        aStrInsertRows       (SC_RESSTR(STR_INSERT_ROWS)),
-        aStrInsertTabs       (SC_RESSTR(STR_INSERT_TABS)),
-        aStrDeleteCols       (SC_RESSTR(STR_DELETE_COLS)),
-        aStrDeleteRows       (SC_RESSTR(STR_DELETE_ROWS)),
-        aStrDeleteTabs       (SC_RESSTR(STR_DELETE_TABS)),
-        aStrMove             (SC_RESSTR(STR_MOVE)),
-        aStrContent          (SC_RESSTR(STR_CONTENT)),
-        aStrReject           (SC_RESSTR(STR_REJECT)),
-        aStrAllAccepted      (SC_RESSTR(STR_ACCEPTED)),
-        aStrAllRejected      (SC_RESSTR(STR_REJECTED)),
-        aStrNoEntry          (SC_RESSTR(STR_NO_ENTRY)),
-        aStrContentWithChild (SC_RESSTR(STR_CONTENT_WITH_CHILD)),
-        aStrChildContent     (SC_RESSTR(STR_CHILD_CONTENT)),
-        aStrChildOrgContent  (SC_RESSTR(STR_CHILD_ORGCONTENT)),
-        aStrEmpty            (SC_RESSTR(STR_EMPTY)),
+        aStrInsertCols       (SC_RESSTR(STR_CHG_INSERT_COLS)),
+        aStrInsertRows       (SC_RESSTR(STR_CHG_INSERT_ROWS)),
+        aStrInsertTabs       (SC_RESSTR(STR_CHG_INSERT_TABS)),
+        aStrDeleteCols       (SC_RESSTR(STR_CHG_DELETE_COLS)),
+        aStrDeleteRows       (SC_RESSTR(STR_CHG_DELETE_ROWS)),
+        aStrDeleteTabs       (SC_RESSTR(STR_CHG_DELETE_TABS)),
+        aStrMove             (SC_RESSTR(STR_CHG_MOVE)),
+        aStrContent          (SC_RESSTR(STR_CHG_CONTENT)),
+        aStrReject           (SC_RESSTR(STR_CHG_REJECT)),
+        aStrAllAccepted      (SC_RESSTR(STR_CHG_ACCEPTED)),
+        aStrAllRejected      (SC_RESSTR(STR_CHG_REJECTED)),
+        aStrNoEntry          (SC_RESSTR(STR_CHG_NO_ENTRY)),
+        aStrContentWithChild (SC_RESSTR(STR_CHG_CONTENT_WITH_CHILD)),
+        aStrChildContent     (SC_RESSTR(STR_CHG_CHILD_CONTENT)),
+        aStrChildOrgContent  (SC_RESSTR(STR_CHG_CHILD_ORGCONTENT)),
+        aStrEmpty            (SC_RESSTR(STR_CHG_EMPTY)),
         aUnknown(RTL_CONSTASCII_USTRINGPARAM("Unknown")),
         bAcceptEnableFlag(true),
         bRejectEnableFlag(true),
@@ -126,21 +125,14 @@ ScAcceptChgDlg::ScAcceptChgDlg( SfxBindings* pB, SfxChildWindow* pCW, Window* pP
         bHasFilterEntry(false),
         bUseColor(false)
 {
-    FreeResource();
+    m_pAcceptChgCtr = new SvxAcceptChgCtr(get_content_area());
     nAcceptCount=0;
     nRejectCount=0;
     aReOpenTimer.SetTimeout(50);
     aReOpenTimer.SetTimeoutHdl(LINK( this, ScAcceptChgDlg, ReOpenTimerHdl ));
 
-    //  dialog is now only hidden, not deleted, on switching views,
-    //  so there's no need to restore settings when reopening
-    MinSize=aAcceptChgCtr.GetMinSizePixel();
-    MinSize.Height()+=2;
-    MinSize.Width()+=2;
-    SetMinOutputSizePixel(MinSize);
-
-    pTPFilter=aAcceptChgCtr.GetFilterPage();
-    pTPView=aAcceptChgCtr.GetViewPage();
+    pTPFilter=m_pAcceptChgCtr->GetFilterPage();
+    pTPView=m_pAcceptChgCtr->GetViewPage();
     pTheView=pTPView->GetTableControl();
     aSelectionTimer.SetTimeout(100);
     aSelectionTimer.SetTimeoutHdl(LINK( this, ScAcceptChgDlg, UpdateSelectionHdl ));
@@ -166,8 +158,6 @@ ScAcceptChgDlg::ScAcceptChgDlg( SfxBindings* pB, SfxChildWindow* pCW, Window* pP
 
     Init();
 
-    aAcceptChgCtr.SetMinSizeHdl( LINK( this, ScAcceptChgDlg, MinSizeHandle ));
-
     UpdateView();
     SvTreeListEntry* pEntry=pTheView->First();
     if(pEntry!=NULL)
@@ -175,6 +165,7 @@ ScAcceptChgDlg::ScAcceptChgDlg( SfxBindings* pB, SfxChildWindow* pCW, Window* pP
         pTheView->Select(pEntry);
     }
 }
+
 ScAcceptChgDlg::~ScAcceptChgDlg()
 {
     ClearView();
@@ -185,6 +176,8 @@ ScAcceptChgDlg::~ScAcceptChgDlg()
         Link aLink;
         pChanges->SetModifiedLink(aLink);
     }
+
+    delete m_pAcceptChgCtr;
 }
 
 void ScAcceptChgDlg::ReInit(ScViewData* ptrViewData)
@@ -277,12 +270,8 @@ void ScAcceptChgDlg::Init()
         pTPFilter->SetRange(aRefStr);
     }
 
-    Point aPoint(1,1);
-    aAcceptChgCtr.SetPosPixel(aPoint);
     InitFilter();
 }
-
-
 
 void ScAcceptChgDlg::ClearView()
 {
@@ -896,15 +885,7 @@ void ScAcceptChgDlg::Resize()
 {
     SfxModelessDialog::Resize();
     Size aOutSize=GetOutputSizePixel();
-    aAcceptChgCtr.SetSizePixel(aOutSize);
-}
-
-IMPL_LINK( ScAcceptChgDlg, MinSizeHandle, SvxAcceptChgCtr*, pCtr )
-{
-    if(pCtr==&aAcceptChgCtr)
-        if(!IsRollUp())
-            SetOutputSizePixel(MinSize);
-    return 0;
+    m_pAcceptChgCtr->SetSizePixel(aOutSize);
 }
 
 IMPL_LINK_NOARG(ScAcceptChgDlg, RefHandle)
@@ -1686,7 +1667,7 @@ IMPL_LINK( ScAcceptChgDlg, ChgTrackModHdl, ScChangeTrack*, pChgTrack)
 IMPL_LINK_NOARG(ScAcceptChgDlg, ReOpenTimerHdl)
 {
     ScSimpleRefDlgWrapper::SetAutoReOpen(true);
-    aAcceptChgCtr.ShowFilterPage();
+    m_pAcceptChgCtr->ShowFilterPage();
     RefHandle(NULL);
 
     return 0;

commit 30ba8fa46aafb0868ada3b0092cd33524c145989

don't unconditionally enable buttons on switching to accept/reject page
we want to disable all of them when switch to the options page and *restore*
the original enable/disable state when switching back i.e. don't enable them if
they were disabled due to change tracking password protection

regression since 8655095a4910ca795dfd182e6796d9375727a694

Change-Id: I2dd6c83fd9613adf42571684a08bff81b86c9ee1

include/svx/ctredlin.hxx
@@ -305,6 +305,12 @@ private:
     PushButton*     m_pRejectAll;
     PushButton*     m_pUndo;
 
+    bool bEnableAccept;
+    bool bEnableAcceptAll;
+    bool bEnableReject;
+    bool bEnableRejectAll;
+    bool bEnableUndo;
+
     DECL_LINK( PbClickHdl, PushButton* );
 
 public:

svx/source/dialog/ctredlin.cxx
@@ -394,6 +394,11 @@ void SvxRedlinTable::InitEntry(SvTreeListEntry* pEntry, const OUString& rStr,
 
 SvxTPView::SvxTPView(Window * pParent)
     : TabPage(pParent, "RedlineViewPage", "svx/ui/redlineviewpage.ui")
+    , bEnableAccept(true)
+    , bEnableAcceptAll(true)
+    , bEnableReject(true)
+    , bEnableRejectAll(true)
+    , bEnableUndo(true)
 {
     Dialog *pTopLevel = GetParentDialog();
     pTopLevel->get(m_pAccept, "accept");
@@ -422,11 +427,11 @@ SvxTPView::SvxTPView(Window * pParent)
 
 void SvxTPView::ActivatePage()
 {
-    m_pAccept->Enable();
-    m_pReject->Enable();
-    m_pAcceptAll->Enable();
-    m_pRejectAll->Enable();
-    m_pUndo->Enable();
+    m_pAccept->Enable(bEnableAccept);
+    m_pReject->Enable(bEnableReject);
+    m_pAcceptAll->Enable(bEnableAcceptAll);
+    m_pRejectAll->Enable(bEnableRejectAll);
+    m_pUndo->Enable(bEnableUndo);
     TabPage::ActivatePage();
 }
 
@@ -477,21 +482,25 @@ void SvxTPView::InsertCalcHeader()
 
 void SvxTPView::EnableAccept(sal_Bool nFlag)
 {
+    bEnableAccept = nFlag;
     m_pAccept->Enable(nFlag);
 }
 
 void SvxTPView::EnableAcceptAll(sal_Bool nFlag)
 {
+    bEnableAcceptAll = nFlag;
     m_pAcceptAll->Enable(nFlag);
 }
 
 void SvxTPView::EnableReject(sal_Bool nFlag)
 {
+    bEnableReject = nFlag;
     m_pReject->Enable(nFlag);
 }
 
 void SvxTPView::EnableRejectAll(sal_Bool nFlag)
 {
+    bEnableRejectAll = nFlag;
     m_pRejectAll->Enable(nFlag);
 }
 
@@ -502,6 +511,7 @@ void SvxTPView::ShowUndo(sal_Bool nFlag)
 
 void SvxTPView::EnableUndo(sal_Bool nFlag)
 {
+    bEnableUndo = nFlag;
     m_pUndo->Enable(nFlag);
 }
 