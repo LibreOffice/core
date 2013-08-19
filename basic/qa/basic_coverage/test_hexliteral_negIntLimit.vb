c59b0db39bc34c9708d7a8689775e16d64190148
Noel Power <noel.power@suse.com>
2013-08-19 16:58:07 +0100

    fix for fdo#62323 bad conversion of Hex strings for certain values
    Basic hex literals are basic Integer ( e.g. 4 byte ) types with
    -2,147,483,648 through 2,147,483,647 range. Interally the scanner
    was using a long to form/scan the literal, this led to bad behaviour
    on 64bit linux ( where normally long -> 8 bytes )
    
    (cherry picked from commit 4c9a08e78b6e2c5d19628281bd4141c268299bea)
    
    and squash of
    fix for fdo#62323 fix duplicated and wrong tests
    
    Change-Id: I73dc238f7de59367a0a9d00e5421ea6675b4f556
    (cherry picked from commit fbf8ae82411d56189f844f9f00ccc6cf6b0827bf)
    Reviewed-on: https://gerrit.libreoffice.org/5527
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>

---
+++ basic/qa/basic_coverage/test_hexliteral_negIntLimit.vb
@@ -0,0 +1,18 @@
+'
+' This file is part of the LibreOffice project.
+'
+' This Source Code Form is subject to the terms of the Mozilla Public
+' License, v. 2.0. If a copy of the MPL was not distributed with this
+' file, You can obtain one at http://mozilla.org/MPL/2.0/.
+'
+
+
+Function doUnitTest as Integer
+  Dim lngDecimal as Long
+  lngDecimal = Clng(&HFFFFFFFF)
+  If lngDecimal = -1 Then
+     doUnitTest = 1
+  Else
+     doUnitTest = 0
+  End If
+End Function
