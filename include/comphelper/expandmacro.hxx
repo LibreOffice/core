7b0a4344394675a6a8c7fd7754f6a935437ca08e
Vishv Brahmbhatt <vishvbrahmbhatt19@gmail.com>
2013-08-28 01:52:17 +0530

    Parsing 'presobj' properties from 'objectlist.xml': gsoc-impresslayout
    Change-Id: If0381a12155673e85103ddb5d51c34ae53fe2ecb

---
+++ include/comphelper/expandmacro.hxx
@@ -0,0 +1,23 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef COMPHELPER_EXPANDMACRO_HXX_INCLUDED
+#define COMPHELPER_EXPANDMACRO_HXX_INCLUDED
+#include "rtl/ustring.hxx"
+#include "comphelper/comphelperdllapi.h"
+#include <boost/shared_ptr.hpp>
+#include <com/sun/star/uno/Reference.hxx>
+#include <comphelper/processfactory.hxx>
+
+namespace comphelper
+{
+    COMPHELPER_DLLPUBLIC rtl::OUString getExpandedFilePath(const rtl::OUString&);
+    COMPHELPER_DLLPUBLIC rtl::OUString getExpandedFilePath(const rtl::OUString&,::com::sun::star::uno::Reference<css::uno::XComponentContext>);
+}
+#endif
