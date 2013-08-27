eed831380d3fdadc7defdbe66e7a8275507ca748
Lionel Elie Mamane <lionel@mamane.lu>
2013-08-27 18:03:21 +0200

    odbc insert: adjust buffer size *after* it is written to
    the buffer (aBookmark) is written to by SQLBulkOperations or
    SQLSetPos, not by SQLBindCol, which merely sets up the instruction
    that the next call should write there. So its finak size is known
    only *after* the call that makes the write, obviously.
    
    Change-Id: I4faaaeffc060433645188f0eda3d9958b087cf6c
    Reviewed-on: https://gerrit.libreoffice.org/5651
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>

--- connectivity/source/drivers/odbcbase/OResultSet.cxx
+++ connectivity/source/drivers/odbcbase/OResultSet.cxx
@@ -871,7 +871,6 @@ void SAL_CALL OResultSet::insertRow(  ) throw(SQLException, RuntimeException)
                                 &nRealLen
                                 );
 
-    aBookmark.realloc(nRealLen);
     sal_Bool bPositionByBookmark = ( NULL != getOdbcFunction( ODBC3SQLBulkOperations ) );
     if ( bPositionByBookmark )
     {
@@ -885,6 +884,7 @@ void SAL_CALL OResultSet::insertRow(  ) throw(SQLException, RuntimeException)
         nRet = N3SQLSetPos( m_aStatementHandle, 1, SQL_ADD, SQL_LOCK_NO_CHANGE );
         fillNeededData( nRet );
     }
+    aBookmark.realloc(nRealLen);
     try
     {
         OTools::ThrowException(m_pStatement->getOwnConnection(),nRet,m_aStatementHandle,SQL_HANDLE_STMT,*this);
