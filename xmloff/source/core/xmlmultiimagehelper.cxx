d5f3bb7e94cdfbaf978064486eb658637ab60642
Michael Stahl <mstahl@redhat.com>
2013-09-03 00:32:08 +0200

    fdo#68839: ODF import: fix name of multi-image frame
    When multiple image child elements are inside a frame, each one is
    imported and gets unique name via SwDoc::SetFlyName().  But the
    retained one is not necessarily the first one, which is the only one
    that may have the original name.
    
    Also the solveMultipleImages needs to return a smart pointer, as nothing
    else keeps the image contexts alive.
    
    (regression from 44cfc7cb6533d827fd2d6e586d92c61d7d7f7a70)
    
    (cherry picked from commit b69d152cfa1da868ba960345d72ba78f9f8e1b35)
    
    Conflicts:
    	xmloff/source/core/xmlmultiimagehelper.cxx
    	xmloff/source/draw/ximpshap.cxx
    	xmloff/source/text/XMLTextFrameContext.cxx
    
    Reviewed-on: https://gerrit.libreoffice.org/5781
    Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
    Tested-by: Miklos Vajna <vmiklos@collabora.co.uk>
    
    Conflicts:
    	xmloff/source/core/xmlmultiimagehelper.cxx
    	xmloff/source/draw/ximpshap.cxx
    
    Change-Id: I28a8a752f3eed176cc2ebb4c9af11a0dd4d18ea6

--- xmloff/source/core/xmlmultiimagehelper.cxx
+++ xmloff/source/core/xmlmultiimagehelper.cxx
@@ -89,9 +89,9 @@ MultiImageImportHelper::~MultiImageImportHelper()
     }
 }
 
-const SvXMLImportContext* MultiImageImportHelper::solveMultipleImages()
+SvXMLImportContextRef MultiImageImportHelper::solveMultipleImages()
 {
-    const SvXMLImportContext* pContext(0);
+    SvXMLImportContextRef pContext;
     if(maImplContextVector.size() > 1)
     {
         // multiple child contexts were imported, decide which is the most valuable one
