d8d26ba1565d597e333dc474b13d8e63fe31976d
Tor Lillqvist <tml@collabora.com>
2013-09-03 18:23:42 +0300

    More headers need un-symlinking for MSVC
    Change-Id: Ic5f0bfab4a729b0abca332454817aa899a5adc6f

--- openssl/UnpackedTarball_openssl.mk
+++ openssl/UnpackedTarball_openssl.mk
@@ -13,19 +13,30 @@ $(eval $(call gb_UnpackedTarball_set_tarball,openssl,$(OPENSSL_TARBALL),,openssl
 
 # hack to fix symlinks with MSVC
 $(eval $(call gb_UnpackedTarball_fix_end_of_line,openssl,\
+	include/openssl/aes.h \
 	include/openssl/asn1.h \
+	include/openssl/asn1_mac.h \
+	include/openssl/asn1t.h \
 	include/openssl/bio.h \
+	include/openssl/blowfish.h \
 	include/openssl/bn.h \
 	include/openssl/buffer.h \
+	include/openssl/camellia.h \
+	include/openssl/cast.h \
+	include/openssl/cmac.h \
+	include/openssl/cms.h \
 	include/openssl/comp.h \
 	include/openssl/conf.h \
+	include/openssl/conf_api.h \
 	include/openssl/crypto.h \
 	include/openssl/des.h \
 	include/openssl/des_old.h \
 	include/openssl/dh.h \
 	include/openssl/dsa.h \
+	include/openssl/dso.h \
 	include/openssl/dtls1.h \
 	include/openssl/e_os2.h \
+	include/openssl/ebcdic.h \
 	include/openssl/ec.h \
 	include/openssl/ecdh.h \
 	include/openssl/ecdsa.h \
@@ -33,12 +44,17 @@ $(eval $(call gb_UnpackedTarball_fix_end_of_line,openssl,\
 	include/openssl/err.h \
 	include/openssl/evp.h \
 	include/openssl/hmac.h \
+	include/openssl/idea.h \
+	include/openssl/krb5_asn.h \
 	include/openssl/kssl.h \
 	include/openssl/lhash.h \
 	include/openssl/md4.h \
 	include/openssl/md5.h \
+	include/openssl/mdc2.h \
+	include/openssl/modes.h \
 	include/openssl/obj_mac.h \
 	include/openssl/objects.h \
+	include/openssl/ocsp.h \
 	include/openssl/opensslconf.h \
 	include/openssl/opensslv.h \
 	include/openssl/ossl_typ.h \
@@ -48,10 +64,15 @@ $(eval $(call gb_UnpackedTarball_fix_end_of_line,openssl,\
 	include/openssl/pkcs7.h \
 	include/openssl/pqueue.h \
 	include/openssl/rand.h \
+	include/openssl/rc2.h \
+	include/openssl/rc4.h \
 	include/openssl/ripemd.h \
 	include/openssl/rsa.h \
 	include/openssl/safestack.h \
+	include/openssl/seed.h \
 	include/openssl/sha.h \
+	include/openssl/srp.h \
+	include/openssl/srtp.h \
 	include/openssl/ssl.h \
 	include/openssl/ssl2.h \
 	include/openssl/ssl23.h \
@@ -59,8 +80,11 @@ $(eval $(call gb_UnpackedTarball_fix_end_of_line,openssl,\
 	include/openssl/stack.h \
 	include/openssl/symhacks.h \
 	include/openssl/tls1.h \
+	include/openssl/ts.h \
+	include/openssl/txt_db.h \
 	include/openssl/ui.h \
 	include/openssl/ui_compat.h \
+	include/openssl/whrlpool.h \
 	include/openssl/x509.h \
 	include/openssl/x509_vfy.h \
 	include/openssl/x509v3.h \
