LibreOffice/core

commit 710f41b7aec8e7d35a0da8be332aa289f98942af

Clean String and sal_Bool in tools
Change-Id: I6a92196f33d7a5278c7dcc426112e9c56d582655
Reviewed-on: https://gerrit.libreoffice.org/4627
Reviewed-by: Norbert Thiebaud <nthiebaud@gmail.com>
Tested-by: Norbert Thiebaud <nthiebaud@gmail.com>

editeng/source/editeng/impedit4.cxx
@@ -123,11 +123,11 @@ EditPaM ImpEditEngine::ReadText( SvStream& rInput, EditSelection aSel )
         aSel = ImpDeleteSelection( aSel );
     EditPaM aPaM = aSel.Max();
 
-    XubString aTmpStr;
-    sal_Bool bDone = rInput.ReadByteStringLine( aTmpStr, rInput.GetStreamCharSet() );
+    OUString aTmpStr;
+    bool bDone = rInput.ReadByteStringLine( aTmpStr, rInput.GetStreamCharSet() );
     while ( bDone )
     {
-        aTmpStr.Erase( MAXCHARSINPARA );
+        aTmpStr = aTmpStr.copy(0, MAXCHARSINPARA );
         aPaM = ImpInsertText( EditSelection( aPaM, aPaM ), aTmpStr );
         aPaM = ImpInsertParaBreak( aPaM );
         bDone = rInput.ReadByteStringLine( aTmpStr, rInput.GetStreamCharSet() );

commit 03737426620f570e33b93f92a4bd4e4b6d2deba3

fix string conversion
regression from 710f41b7aec8e7d35a0da8be332aa289f98942af

Change-Id: I7c5e41294e072eceb036a24397fbb899a568587c

editeng/source/editeng/impedit4.cxx
@@ -127,7 +127,10 @@ EditPaM ImpEditEngine::ReadText( SvStream& rInput, EditSelection aSel )
     bool bDone = rInput.ReadByteStringLine( aTmpStr, rInput.GetStreamCharSet() );
     while ( bDone )
     {
-        aTmpStr = aTmpStr.copy(0, MAXCHARSINPARA );
+        if (aTmpStr.getLength() > MAXCHARSINPARA)
+        {
+            aTmpStr = aTmpStr.copy(0, MAXCHARSINPARA);
+        }
         aPaM = ImpInsertText( EditSelection( aPaM, aPaM ), aTmpStr );
         aPaM = ImpInsertParaBreak( aPaM );
         bDone = rInput.ReadByteStringLine( aTmpStr, rInput.GetStreamCharSet() );