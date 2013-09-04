e0b0c1f3d6b8f305ce35940f5b6a382972bbae20
Bjoern Michaelsen <bjoern.michaelsen@canonical.com>
2013-09-04 16:33:43 +0200

    fdo#68210: Make tiff import work again with merged-libs
    - there was a symbol mismatch for dynamic loading
    - while this fixes fdo#68210, similar fixes are likely needed for the other
      dynamically loaded functionality
    - this also includes Change-Id: I6041ca10f58335187928995368db673c1ce22e7b
    
    Change-Id: I7e4d6baf256dbe7b145dcbc118b4cac2075e07f5

--- filter/source/graphicfilter/itiff/itiff.cxx
+++ filter/source/graphicfilter/itiff/itiff.cxx
@@ -1358,7 +1358,10 @@ sal_Bool TIFFReader::ReadTIFF(SvStream & rTIFF, Graphic & rGraphic )
 
 //================== GraphicImport - the exported function ================
 
-#if defined(DISABLE_DYNLOADING) || defined(LIBO_MERGELIBS)
+// this needs to be kept in sync with
+// ImpFilterLibCacheEntry::GetImportFunction() from
+// vcl/source/filter/graphicfilter.cxx
+#if defined(DISABLE_DYNLOADING)
 #define GraphicImport itiGraphicImport
 #endif
 
