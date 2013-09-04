LibreOffice/core

commit a8af548c3be5a42cec7177c60c6d421d2dbc35b4

always call setDeferredProperties for dialog get/set title
so we always get the correct up to date title even if we have
been holding off on setting some properties to ensure that
virtual method aren't called during construction

e.g. insert/edit index marks is always "Insert Index Mark"

(cherry picked from commit 31fc0b74425253589b2ac0dcecb9e8a2714954b4)

Change-Id: I57f9cc8fbdb9d2a304c346e03893fa55d6a61bd5
Reviewed-on: https://gerrit.libreoffice.org/5762
Reviewed-by: Fridrich Strba <fridrich@documentfoundation.org>
Tested-by: Fridrich Strba <fridrich@documentfoundation.org>

include/sfx2/passwd.hxx
@@ -73,7 +73,7 @@ private:
     void            SetPasswdText();
 
 public:
-    SfxPasswordDialog(Window* pParent, const String* pGroupText = NULL);
+    SfxPasswordDialog(Window* pParent, const OUString* pGroupText = NULL);
 
     String GetUser() const
     {

cui/source/dialogs/scriptdlg.cxx
@@ -508,7 +508,6 @@ SvxScriptOrgDialog::SvxScriptOrgDialog( Window* pParent, OUString language )
     get(m_pDelButton, "delete");
     // must be a neater way to deal with the strings than as above
     // append the language to the dialog title
-    setDeferredProperties();
     String winTitle( GetText() );
     winTitle.SearchAndReplace( OUString( "%MACROLANG" ), m_sLanguage );
     SetText( winTitle );

filter/source/xsltdialog/xmlfiltertestdialog.cxx
@@ -162,7 +162,6 @@ XMLFilterTestDialog::XMLFilterTestDialog(Window* pParent,
     m_pPBRecentFile->SetClickHdl(LINK( this, XMLFilterTestDialog, ClickHdl_Impl ) );
     m_pPBClose->SetClickHdl(LINK( this, XMLFilterTestDialog, ClickHdl_Impl ) );
 
-    setDeferredProperties();
     m_sDialogTitle = GetText();
 
     try

include/vcl/dialog.hxx
@@ -114,6 +114,9 @@ public:
     virtual short   Execute();
     sal_Bool            IsInExecute() const { return mbInExecute; }
 
+    virtual void      SetText( const OUString& rStr );
+    virtual OUString  GetText() const;
+
     ////////////////////////////////////////
     // Dialog::Execute replacement API
 public:

padmin/source/prtsetup.cxx
@@ -91,8 +91,6 @@ RTSDialog::RTSDialog( const PrinterInfo& rJobData, const String& rPrinter, bool 
     get(m_pCancelButton, "cancel");
     get(m_pTabControl, "notebook");
 
-    setDeferredProperties();
-
     OUString aTitle(GetText());
     SetText(aTitle.replaceAll("%s", m_aJobData.m_aPrinterName));
 

vcl/source/window/builder.cxx
@@ -1339,9 +1339,10 @@ void VclBuilder::setDeferredProperties()
 {
     if (!m_bToplevelHasDeferredProperties)
         return;
-    set_properties(m_pParent, m_aDeferredProperties);
-    m_aDeferredProperties.clear();
+    stringmap aDeferredProperties;
+    aDeferredProperties.swap(m_aDeferredProperties);
     m_bToplevelHasDeferredProperties = false;
+    set_properties(m_pParent, aDeferredProperties);
 }
 
 void VclBuilder::set_properties(Window *pWindow, const stringmap &rProps)

vcl/source/window/dialog.cxx
@@ -1270,6 +1270,18 @@ bool Dialog::set_property(const OString &rKey, const OString &rValue)
     return true;
 }
 
+void Dialog::SetText(const OUString& rStr)
+{
+    setDeferredProperties();
+    SystemWindow::SetText(rStr);
+}
+
+OUString Dialog::GetText() const
+{
+    const_cast<Dialog*>(this)->setDeferredProperties();
+    return SystemWindow::GetText();
+}
+
 VclBuilderContainer::VclBuilderContainer()
     : m_pUIBuilder(NULL)
 {

commit d83630190890969d6a668205d058c69983875a8c

revert stray OUString->String conversion
introduced accidentally by a8af548c3be5a42cec7177c60c6d421d2dbc35b4

Change-Id: Ib99e5373a14e95f331ef9f00b9b392633f02d700

include/sfx2/passwd.hxx
@@ -73,7 +73,7 @@ private:
     void            SetPasswdText();
 
 public:
-    SfxPasswordDialog(Window* pParent, const OUString* pGroupText = NULL);
+    SfxPasswordDialog(Window* pParent, const String* pGroupText = NULL);
 
     String GetUser() const
     {