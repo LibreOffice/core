fa1b2554d57ea72e4ff45d0d7377aa3671fb2c30
Thomas Arnhold <thomas@arnhold.org>
2013-09-05 18:47:32 +0200

    use replaceFirst
    Change-Id: Ia20eec6e868579ed31e478514b7f2722a0972094
    Reviewed-on: https://gerrit.libreoffice.org/5828
    Reviewed-by: Thomas Arnhold <thomas@arnhold.org>
    Tested-by: Thomas Arnhold <thomas@arnhold.org>

--- dbaccess/source/ui/uno/copytablewizard.cxx
+++ dbaccess/source/ui/uno/copytablewizard.cxx
@@ -1302,8 +1302,8 @@ void CopyTableWizard::impl_copyRows_throw( const Reference< XResultSet >& _rxSou
                     {
                         OUString aMessage( String( ModuleRes( STR_CTW_UNSUPPORTED_COLUMN_TYPE ) ) );
 
-                        aMessage = aMessage.replaceAt( aMessage.indexOfAsciiL( "$type$", 6 ), 6, OUString::number( aSourceColTypes[ nSourceColumn ] ) );
-                        aMessage = aMessage.replaceAt( aMessage.indexOfAsciiL( "$pos$", 5 ), 5, OUString::number( nSourceColumn ) );
+                        aMessage = aMessage.replaceFirst( "$type$", OUString::number( aSourceColTypes[ nSourceColumn ] ) );
+                        aMessage = aMessage.replaceFirst( "$pos$", OUString::number( nSourceColumn ) );
 
                         ::dbtools::throwSQLException(
                             aMessage,
