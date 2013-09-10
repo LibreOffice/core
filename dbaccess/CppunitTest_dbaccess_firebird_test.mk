e4b07b2917154d68019e37b4eadf81a8b73d666b
Andrzej J.R. Hunt <andrzej@ahunt.org>
2013-09-10 11:27:15 +0100

    Add missing component to firebird unit test.
    (Windows throws an Error on this.)
    
    Change-Id: Ica3aeac294d3a32a4faa6837309a0fb5d8d15b92

--- dbaccess/CppunitTest_dbaccess_firebird_test.mk
+++ dbaccess/CppunitTest_dbaccess_firebird_test.mk
@@ -48,6 +48,7 @@ $(eval $(call gb_CppunitTest_use_components,dbaccess_firebird_test,\
     filter/source/config/cache/filterconfig1 \
     framework/util/fwk \
     i18npool/util/i18npool \
+    linguistic/source/lng \
     package/source/xstor/xstor \
     package/util/package2 \
     sax/source/expatwrap/expwrap \
