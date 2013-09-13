d67144b34ddbe8d5b00ed24c588cf571b070235d
Joren De Cuyper <joren.libreoffice@telenet.be>
2013-09-13 16:35:08 +0200

    fdo#67461 add multiple selection support to hide/show multiple styles
    With commit 439ac45925039aa7a537feedab3e731e8a60a8e5 I only add support
    to delete multiple styles.
    Hereby I add the possibility to hide and show multiple styles too.
    
    Change-Id: I46a8400c281c87b2881367f89ee92db2ba77bfb2

--- sfx2/source/dialog/templdlg.cxx
+++ sfx2/source/dialog/templdlg.cxx
@@ -2136,7 +2136,6 @@ void SfxCommonTemplateDialog_Impl::DeleteHdl(void *)
     if ( IsInitialized() && HasSelectedStyle() )
     {
         bool bUsedStyle = 0;     // one of the selected styles are used in the document?
-        OUString aRet;
 
         std::vector<SvTreeListEntry*> aList;
         SvTreeListEntry* pEntry = aFmtLb.FirstSelected();
@@ -2148,13 +2147,10 @@ void SfxCommonTemplateDialog_Impl::DeleteHdl(void *)
         while (pEntry)
         {
             aList.push_back( pEntry );
-            // check the style is used or not
-            if (pTreeBox)
-                aRet = pTreeBox->GetEntryText( pEntry );
-            else
-                aRet = aFmtLb.GetEntryText( pEntry );
 
-            const OUString aTemplName( aRet );
+            // check the style is used or not
+
+            OUString aTemplName = aFmtLb.GetEntryText( pEntry );
 
             SfxStyleSheetBase* pStyle = pStyleSheetPool->Find( aTemplName, pItem->GetFamily(), SFXSTYLEBIT_ALL );
 
@@ -2189,12 +2185,8 @@ void SfxCommonTemplateDialog_Impl::DeleteHdl(void *)
 
             for (; it != itEnd; ++it)
             {
-                if (pTreeBox)
-                    aRet = pTreeBox->GetEntryText( *it );
-                else
-                    aRet = aFmtLb.GetEntryText( *it );
+                OUString aTemplName = aFmtLb.GetEntryText( *it );
 
-                const OUString aTemplName( aRet );
                 PrepareDeleteAction();
                 bDontUpdate = sal_True; // To prevent the Treelistbox to shut down while deleting
                 Execute_Impl( SID_STYLE_DELETE, aTemplName,
@@ -2216,26 +2208,35 @@ void SfxCommonTemplateDialog_Impl::HideHdl(void *)
 {
     if ( IsInitialized() && HasSelectedStyle() )
     {
-        const OUString aTemplName( GetSelectedEntry() );
-        SfxStyleSheetBase* pStyle = GetSelectedStyle();
-        if ( pStyle )
+        SvTreeListEntry* pEntry = aFmtLb.FirstSelected();
+
+        while (pEntry)
         {
+            OUString aTemplName = aFmtLb.GetEntryText( pEntry );
+
             Execute_Impl( SID_STYLE_HIDE, aTemplName,
                           OUString(), (sal_uInt16)GetFamilyItem_Impl()->GetFamily() );
+
+            pEntry = aFmtLb.NextSelected( pEntry );
         }
     }
 }
 
 void SfxCommonTemplateDialog_Impl::ShowHdl(void *)
 {
+
     if ( IsInitialized() && HasSelectedStyle() )
     {
-        const OUString aTemplName( GetSelectedEntry() );
-        SfxStyleSheetBase* pStyle = GetSelectedStyle();
-        if ( pStyle )
+        SvTreeListEntry* pEntry = aFmtLb.FirstSelected();
+
+        while (pEntry)
         {
+            OUString aTemplName = aFmtLb.GetEntryText( pEntry );
+
             Execute_Impl( SID_STYLE_SHOW, aTemplName,
                           OUString(), (sal_uInt16)GetFamilyItem_Impl()->GetFamily() );
+
+            pEntry = aFmtLb.NextSelected( pEntry );
         }
     }
 }
