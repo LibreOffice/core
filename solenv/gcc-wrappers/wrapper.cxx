5f0145818cba17558708ef731e2bcc6a0b92a9f7
Michael Meeks <michael.meeks@collabora.com>
2013-09-18 17:05:21 +0100

    gcc-wrapper: warn on invalid path names.
    Change-Id: Ib057d519485da3a454bad579675ae718c42155eb

--- solenv/gcc-wrappers/wrapper.cxx
+++ solenv/gcc-wrappers/wrapper.cxx
@@ -87,7 +87,11 @@ string processccargs(vector<string> rawargs) {
 
     for(vector<string>::iterator i = rawargs.begin(); i != rawargs.end(); ++i) {
         args.append(" ");
-        if(*i == "-o") {
+        if(i->find("/") == 0) {
+            cerr << "Error: absolute unix path passed that looks like an option: '" << *i << "'";
+            args.append(*i);
+        }
+        else if(*i == "-o") {
             // TODO: handle more than just exe output
             ++i;
             size_t dot=(*i).find_last_of(".");
