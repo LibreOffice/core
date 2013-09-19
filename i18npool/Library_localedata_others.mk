f1ccfc64fca471f140a4b1830e370d8e4fe8ad4f
Eike Rathke <erack@redhat.com>
2013-09-20 01:43:42 +0200

    missing .mk for renamed 'mn-MN' to 'mn-Cyrl-MN'
    Change-Id: I77e457b27b083c7149e3696f978b1e85a6fe1e21

--- i18npool/Library_localedata_others.mk
+++ i18npool/Library_localedata_others.mk
@@ -74,7 +74,7 @@ $(eval $(call gb_Library_add_generated_exception_objects,localedata_others,\
 	CustomTarget/i18npool/localedata/localedata_mdw_CG \
 	CustomTarget/i18npool/localedata/localedata_mkw_CG \
 	CustomTarget/i18npool/localedata/localedata_ml_IN \
-	CustomTarget/i18npool/localedata/localedata_mn_MN \
+	CustomTarget/i18npool/localedata/localedata_mn_Cyrl_MN \
 	CustomTarget/i18npool/localedata/localedata_mr_IN \
 	CustomTarget/i18npool/localedata/localedata_ms_MY \
 	CustomTarget/i18npool/localedata/localedata_my_MM \
