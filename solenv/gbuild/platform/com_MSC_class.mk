2cfef67cd564e0e17868e1e2d7d1208b222f8aae
Michael Stahl <mstahl@redhat.com>
2013-09-21 01:47:44 +0200

    gbuild: MSVC: ignore LIBS for static libraries
    Not sure if it causes actual problems but all other platforms ignore
    LIBS on static libraries already when linking.
    
    Change-Id: I8a5ff4b2243b625827f82b0f64f007cea7c9d7c5

--- solenv/gbuild/platform/com_MSC_class.mk
+++ solenv/gbuild/platform/com_MSC_class.mk
@@ -196,8 +196,7 @@ $(call gb_Helper_abbreviate_dirs,\
 		@$${RESPONSEFILE} \
 		$(foreach lib,$(LINKED_LIBS),$(call gb_Library_get_ilibfilename,$(lib))) \
 		$(foreach lib,$(LINKED_STATIC_LIBS),$(call gb_StaticLibrary_get_filename,$(lib))) \
-		$(T_LIBS) \
-		$(if $(filter-out StaticLibrary,$(TARGETTYPE)),user32.lib) \
+		$(if $(filter-out StaticLibrary,$(TARGETTYPE)),$(T_LIBS) user32.lib) \
 		$(if $(filter-out StaticLibrary,$(TARGETTYPE)),-manifestfile:$(WORKDIR)/LinkTarget/$(2).manifest) \
 		-pdb:$(WORKDIR)/LinkTarget/$(2).pdb \
 		$(if $(ILIBTARGET),-out:$(1) -implib:$(ILIBTARGET),-out:$(1)); RC=$$?; rm $${RESPONSEFILE} \
