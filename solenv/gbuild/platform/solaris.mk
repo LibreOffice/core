LibreOffice/core

commit f861ae6acdbde8acc9560366d344852391d17b8d

gbuild: change linking order on ELF platforms
If there are static libraries in $(LIBS) then things get really annoying
if they depend on other static libs that are StaticLibraries, e.g. orcus
and boost_system.  So reorder this to include $(LIBS) in the --start-group
--end-group and hope that doesn't break anything.

Hopefully $(LIBS) does not need -Wl$(COMMA)--no-as-needed, that should
only be required for $(LINKEDLIBS).

Change-Id: Ie15239a0fb7b5295f85941150e2c60912be73bfb

solenv/gbuild/platform/solaris.mk
@@ -182,9 +182,12 @@ $(call gb_Helper_abbreviate_dirs,\
 		$(foreach object,$(GENCOBJECTS),$(call gb_GenCObject_get_target,$(object))) \
 		$(foreach object,$(GENCXXOBJECTS),$(call gb_GenCxxObject_get_target,$(object))) \
 		$(foreach extraobjectlist,$(EXTRAOBJECTLISTS),`cat $(extraobjectlist)`) \
-		-Wl$(COMMA)--start-group $(foreach lib,$(LINKED_STATIC_LIBS),$(call gb_StaticLibrary_get_target,$(lib))) -Wl$(COMMA)--end-group \
-		-Wl$(COMMA)--no-as-needed \
+		-Wl$(COMMA)--start-group \
 		$(LIBS) \
+		$(foreach lib,$(LINKED_STATIC_LIBS),\
+			$(call gb_StaticLibrary_get_target,$(lib))) \
+		-Wl$(COMMA)--end-group \
+		-Wl$(COMMA)--no-as-needed \
 		$(patsubst lib%.a,-l%,$(patsubst lib%.so,-l%,$(patsubst %.$(gb_Library_UDK_MAJORVER),%,$(foreach lib,$(LINKED_LIBS),$(call gb_Library_get_filename,$(lib)))))) \
 		-o $(1) \
 	$(if $(SOVERSIONSCRIPT),&& ln -sf ../../ure-link/lib/$(notdir $(1)) $(ILIBTARGET)))

solenv/gbuild/platform/unxgcc.mk
@@ -184,9 +184,12 @@ $(call gb_Helper_abbreviate_dirs,\
 		$(foreach object,$(GENCOBJECTS),$(call gb_GenCObject_get_target,$(object))) \
 		$(foreach object,$(GENCXXOBJECTS),$(call gb_GenCxxObject_get_target,$(object))) \
 		$(foreach extraobjectlist,$(EXTRAOBJECTLISTS),`cat $(extraobjectlist)`) \
-		-Wl$(COMMA)--start-group $(foreach lib,$(LINKED_STATIC_LIBS),$(call gb_StaticLibrary_get_target,$(lib))) -Wl$(COMMA)--end-group \
-		-Wl$(COMMA)--no-as-needed \
+		-Wl$(COMMA)--start-group \
 		$(LIBS) \
+		$(foreach lib,$(LINKED_STATIC_LIBS),\
+			$(call gb_StaticLibrary_get_target,$(lib))) \
+		-Wl$(COMMA)--end-group \
+		-Wl$(COMMA)--no-as-needed \
 		$(patsubst lib%.a,-l%,$(patsubst lib%.so,-l%,$(patsubst %.$(gb_Library_UDK_MAJORVER),%,$(foreach lib,$(LINKED_LIBS),$(call gb_Library_get_filename,$(lib)))))) \
 		-o $(1) \
 	$(if $(SOVERSIONSCRIPT),&& ln -sf ../../ure-link/lib/$(notdir $(1)) $(ILIBTARGET)))

commit 2292b3cd8d9a06b2f073b945b228dd7894955b09

gbuild: try to fix linking with default --as-needed
As reported by Jbfaure, linking npsoplugin library fails on Ubuntu
because of unresolved libdl symbols from a StaticLibrary.
It turns out that --start-group/--end-group only affects static
libraries, so reorder the link command again to put the StaticLibrary
first.
(regression from f861ae6acdbde8acc9560366d344852391d17b8d)

Change-Id: Icd31faace8bf7764fee97cd93c3ecd0caad85947

solenv/gbuild/platform/solaris.mk
@@ -179,9 +179,9 @@ $(call gb_Helper_abbreviate_dirs,\
 		$(foreach object,$(GENCXXOBJECTS),$(call gb_GenCxxObject_get_target,$(object))) \
 		$(foreach extraobjectlist,$(EXTRAOBJECTLISTS),`cat $(extraobjectlist)`) \
 		-Wl$(COMMA)--start-group \
-		$(T_LIBS) \
 		$(foreach lib,$(LINKED_STATIC_LIBS),\
 			$(call gb_StaticLibrary_get_target,$(lib))) \
+		$(T_LIBS) \
 		-Wl$(COMMA)--end-group \
 		-Wl$(COMMA)--no-as-needed \
 		$(patsubst lib%.a,-l%,$(patsubst lib%.so,-l%,$(patsubst %.$(gb_Library_UDK_MAJORVER),%,$(foreach lib,$(LINKED_LIBS),$(call gb_Library_get_filename,$(lib)))))) \

solenv/gbuild/platform/unxgcc.mk
@@ -181,9 +181,9 @@ $(call gb_Helper_abbreviate_dirs,\
 		$(foreach object,$(GENCXXOBJECTS),$(call gb_GenCxxObject_get_target,$(object))) \
 		$(foreach extraobjectlist,$(EXTRAOBJECTLISTS),`cat $(extraobjectlist)`) \
 		-Wl$(COMMA)--start-group \
-		$(T_LIBS) \
 		$(foreach lib,$(LINKED_STATIC_LIBS),\
 			$(call gb_StaticLibrary_get_target,$(lib))) \
+		$(T_LIBS) \
 		-Wl$(COMMA)--end-group \
 		-Wl$(COMMA)--no-as-needed \
 		$(patsubst lib%.a,-l%,$(patsubst lib%.so,-l%,$(patsubst %.$(gb_Library_UDK_MAJORVER),%,$(foreach lib,$(LINKED_LIBS),$(call gb_Library_get_filename,$(lib)))))) \
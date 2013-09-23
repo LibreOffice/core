2292b3cd8d9a06b2f073b945b228dd7894955b09
Michael Stahl <mstahl@redhat.com>
2013-09-23 12:09:51 +0200

    gbuild: try to fix linking with default --as-needed
    As reported by Jbfaure, linking npsoplugin library fails on Ubuntu
    because of unresolved libdl symbols from a StaticLibrary.
    It turns out that --start-group/--end-group only affects static
    libraries, so reorder the link command again to put the StaticLibrary
    first.
    (regression from f861ae6acdbde8acc9560366d344852391d17b8d)
    
    Change-Id: Icd31faace8bf7764fee97cd93c3ecd0caad85947

--- solenv/gbuild/platform/unxgcc.mk
+++ solenv/gbuild/platform/unxgcc.mk
@@ -181,9 +181,9 @@ $(call gb_Helper_abbreviate_dirs,\
 		$(foreach object,$(GENCXXOBJECTS),$(call gb_GenCxxObject_get_target,$(object))) \
 		$(foreach extraobjectlist,$(EXTRAOBJECTLISTS),`cat $(extraobjectlist)`) \
 		-Wl$(COMMA)--start-group \
-		$(T_LIBS) \
 		$(foreach lib,$(LINKED_STATIC_LIBS),\
 			$(call gb_StaticLibrary_get_target,$(lib))) \
+		$(T_LIBS) \
 		-Wl$(COMMA)--end-group \
 		-Wl$(COMMA)--no-as-needed \
 		$(patsubst lib%.a,-l%,$(patsubst lib%.so,-l%,$(patsubst %.$(gb_Library_UDK_MAJORVER),%,$(foreach lib,$(LINKED_LIBS),$(call gb_Library_get_filename,$(lib)))))) \
