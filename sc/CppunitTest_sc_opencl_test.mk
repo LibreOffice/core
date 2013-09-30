ea661f9192527bf1b2f6b3f93ce2c384767b55da
I-Jui (Ray) Sung <ray@multicorewareinc.com>
2013-09-30 15:32:34 -0500

    Initial testcase for dynamic kernel compiler
    Test the jitting compiler with a simple vector formula like C1=A1+B1*2
    This stresses:
    1) vector operations add, mul and,
    2) simple constant and vector operands.

---
+++ sc/CppunitTest_sc_opencl_test.mk
@@ -0,0 +1,116 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_CppunitTest_CppunitTest,sc_opencl_test))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,sc_opencl_test, \
+    sc/qa/unit/opencl-test \
+))
+
+$(eval $(call gb_CppunitTest_use_externals,sc_opencl_test, \
+	boost_headers \
+	mdds_headers \
+	orcus \
+	orcus-parser \
+))
+
+$(eval $(call gb_CppunitTest_use_libraries,sc_opencl_test, \
+    avmedia \
+    basegfx \
+    comphelper \
+    cppu \
+    cppuhelper \
+    drawinglayer \
+    editeng \
+    fileacc \
+    for \
+    forui \
+    i18nlangtag \
+    msfilter \
+    oox \
+    sal \
+    salhelper \
+    sax \
+    sc \
+    scqahelper \
+    sfx \
+    sot \
+    svl \
+    svt \
+    svx \
+    svxcore \
+	test \
+    tl \
+    tk \
+    ucbhelper \
+	unotest \
+    utl \
+    vcl \
+    xo \
+	$(gb_UWINAPI) \
+))
+
+$(eval $(call gb_CppunitTest_set_include,sc_opencl_test,\
+    -I$(SRCDIR)/sc/source/ui/inc \
+    -I$(SRCDIR)/sc/source/core/inc \
+    -I$(SRCDIR)/sc/inc \
+    $$(INCLUDE) \
+))
+
+$(eval $(call gb_CppunitTest_use_api,sc_opencl_test,\
+    offapi \
+    udkapi \
+))
+
+$(eval $(call gb_CppunitTest_use_ure,sc_opencl_test))
+
+$(eval $(call gb_CppunitTest_use_components,sc_opencl_test,\
+    basic/util/sb \
+    chart2/source/controller/chartcontroller \
+    chart2/source/chartcore \
+    comphelper/util/comphelp \
+    configmgr/source/configmgr \
+    dbaccess/util/dba \
+    embeddedobj/util/embobj \
+    eventattacher/source/evtatt \
+    fileaccess/source/fileacc \
+    filter/source/config/cache/filterconfig1 \
+    forms/util/frm \
+    framework/util/fwk \
+    i18npool/util/i18npool \
+    linguistic/source/lng \
+    oox/util/oox \
+    package/source/xstor/xstor \
+    package/util/package2 \
+    sax/source/expatwrap/expwrap \
+    sax/source/fastparser/fastsax \
+    sc/util/sc \
+    sc/util/scfilt \
+    scaddins/source/analysis/analysis \
+    scaddins/source/datefunc/date \
+    sfx2/util/sfx \
+    sot/util/sot \
+    svl/util/svl \
+    svx/util/svx \
+    svtools/util/svt \
+    toolkit/util/tk \
+    ucb/source/core/ucb1 \
+    ucb/source/ucp/file/ucpfile1 \
+    ucb/source/ucp/tdoc/ucptdoc1 \
+    unotools/util/utl \
+    unoxml/source/rdf/unordf \
+    unoxml/source/service/unoxml \
+    xmloff/util/xo \
+))
+
+$(eval $(call gb_CppunitTest_use_configuration,sc_opencl_test))
+
+$(eval $(call gb_CppunitTest_use_filter_configuration,sc_opencl_test))
+
+# vim: set noet sw=4 ts=4:
---
+++ sc/qa/unit/opencl-test.cxx
@@ -0,0 +1,163 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is added by mcw.
+ */
+
+#include <sal/config.h>
+#include <unotest/filters-test.hxx>
+#include <test/bootstrapfixture.hxx>
+#include <rtl/strbuf.hxx>
+#include <osl/file.hxx>
+
+#include "scdll.hxx"
+#include <sfx2/app.hxx>
+#include <sfx2/docfilt.hxx>
+#include <sfx2/docfile.hxx>
+#include <sfx2/sfxmodelfactory.hxx>
+#include <svl/stritem.hxx>
+
+#define TEST_BUG_FILES 0
+
+#include "helper/qahelper.hxx"
+
+#include "calcconfig.hxx"
+#include "interpre.hxx"
+
+#include "docsh.hxx"
+#include "postit.hxx"
+#include "patattr.hxx"
+#include "scitems.hxx"
+#include "document.hxx"
+#include "cellform.hxx"
+#include "drwlayer.hxx"
+#include "userdat.hxx"
+#include "formulacell.hxx"
+
+#include <svx/svdpage.hxx>
+
+using namespace ::com::sun::star;
+using namespace ::com::sun::star::uno;
+
+/* Implementation of Filters test */
+
+class ScOpenclTest
+    : public test::FiltersTest
+    , public ScBootstrapFixture
+{
+public:
+    ScOpenclTest();
+    void enableOpenCL(ScDocShell* pShell);
+    virtual void setUp();
+    virtual void tearDown();
+
+    virtual bool load( const OUString &rFilter, const OUString &rURL,
+            const OUString &rUserData, unsigned int nFilterFlags,
+            unsigned int nClipboardID, unsigned int nFilterVersion);
+    void testSharedFormulaXLS();
+    CPPUNIT_TEST_SUITE(ScOpenclTest);
+    CPPUNIT_TEST(testSharedFormulaXLS);
+    CPPUNIT_TEST_SUITE_END();
+
+private:
+    uno::Reference<uno::XInterface> m_xCalcComponent;
+};
+
+bool ScOpenclTest::load(const OUString &rFilter, const OUString &rURL,
+    const OUString &rUserData, unsigned int nFilterFlags,
+        unsigned int nClipboardID, unsigned int nFilterVersion)
+{
+    ScDocShellRef xDocShRef = ScBootstrapFixture::load(rURL, rFilter, rUserData,
+        OUString(), nFilterFlags, nClipboardID, nFilterVersion );
+    bool bLoaded = xDocShRef.Is();
+    //reference counting of ScDocShellRef is very confused.
+    if (bLoaded)
+        xDocShRef->DoClose();
+    return bLoaded;
+}
+
+void ScOpenclTest::enableOpenCL(ScDocShell* pShell)
+{
+    ScModule* pScMod = SC_MOD();
+    ScFormulaOptions rOpt = pScMod->GetFormulaOptions();
+    ScCalcConfig maSavedConfig = rOpt.GetCalcConfig();
+    maSavedConfig.mbOpenCLEnabled = true;
+    rOpt.SetCalcConfig(maSavedConfig);
+    pShell->SetFormulaOptions(rOpt);
+}
+
+#if 0
+void ScOpenclTest::testSharedFormulaXLS()
+{
+    ScDocShellRef xDocSh = loadDoc("ground-water-daily.", XLS);
+
+    enableOpenCL(xDocSh);
+
+    ScDocument* pDoc = xDocSh->GetDocument();
+    CPPUNIT_ASSERT(pDoc);
+    xDocSh->DoHardRecalc(true);
+
+    ScDocShellRef xDocShRes = loadDoc("ground-water-daily-result.", XLS);
+    ScDocument* pDocRes = xDocShRes->GetDocument();
+    CPPUNIT_ASSERT(pDocRes);
+    // Check the results of formula cells in the shared formula range.
+    for (SCROW i = 5; i <= 77; ++i)
+    {
+        double fLibre = pDoc->GetValue(ScAddress(11,i,1));
+        double fExcel = pDocRes->GetValue(ScAddress(11,i,1));
+        CPPUNIT_ASSERT_EQUAL(fExcel, fLibre);
+    }
+    xDocSh->DoClose();
+    xDocShRes->DoClose();
+}
+#else
+void ScOpenclTest::testSharedFormulaXLS()
+{
+    ScDocShellRef xDocSh = loadDoc("sum_ex.", XLS);
+    enableOpenCL(xDocSh);
+    ScDocument *pDoc = xDocSh->GetDocument();
+    CPPUNIT_ASSERT(pDoc);
+    xDocSh->DoHardRecalc(true);
+    ScDocShellRef xDocShRes = loadDoc("sum_ex.", XLS);
+    ScDocument *pDocRes = xDocShRes->GetDocument();
+    CPPUNIT_ASSERT(pDocRes);
+    // Check the results of formula cells in the shared formula range.
+    for (SCROW i = 0; i < 5; ++i)
+    {
+        double fLibre = pDoc->GetValue(ScAddress(2, i, 0));
+        double fExcel = pDocRes->GetValue(ScAddress(2, i, 0));
+        CPPUNIT_ASSERT_EQUAL(fExcel, fLibre);
+    }
+    xDocSh->DoClose();
+    xDocShRes->DoClose();
+}
+
+
+#endif
+
+ScOpenclTest::ScOpenclTest()
+      : ScBootstrapFixture( "/sc/qa/unit/data" )
+{
+}
+
+void ScOpenclTest::setUp()
+{
+    test::BootstrapFixture::setUp();
+
+    // This is a bit of a fudge, we do this to ensure that ScGlobals::ensure,
+    // which is a private symbol to us, gets called
+    m_xCalcComponent =
+        getMultiServiceFactory()->createInstance("com.sun.star.comp.Calc.SpreadsheetDocument");
+    CPPUNIT_ASSERT_MESSAGE("no calc component!", m_xCalcComponent.is());
+}
+
+void ScOpenclTest::tearDown()
+{
+    uno::Reference< lang::XComponent >( m_xCalcComponent, UNO_QUERY_THROW )->dispose();
+    test::BootstrapFixture::tearDown();
+}
+
+CPPUNIT_TEST_SUITE_REGISTRATION(ScOpenclTest);
+
+CPPUNIT_PLUGIN_IMPLEMENT();
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
