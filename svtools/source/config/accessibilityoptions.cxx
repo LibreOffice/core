fed39ad41f0cdd06f9d890fb5f57bf12ebd74479
Stephan Bergmann <sbergman@redhat.com>
2013-09-30 18:23:50 +0200

    -Werror,-Wunused-const-variable
    Change-Id: Iddccf82bc39557ebf03486df18630d1d860aa039

--- svtools/source/config/accessibilityoptions.cxx
+++ svtools/source/config/accessibilityoptions.cxx
@@ -54,8 +54,6 @@ namespace
     const char s_sIsAutomaticFontColor[] = "IsAutomaticFontColor";
     const char s_sIsSystemFont[] = "IsSystemFont";
     const char s_sIsSelectionInReadonly[] = "IsSelectionInReadonly";
-    const char s_sColorValueSetMaximumRowCount[] = "ColorValueSetMaximumRowCount";
-    const char s_sColorValueSetEntryEdgeLength[] = "ColorValueSetEntryEdgeLength";
     const char s_sColorValueSetColumnCount[] = "ColorValueSetColumnCount";
     const char s_sEdgeBlending[] = "EdgeBlending";
     const char s_sListBoxMaximumLineCount[] = "ListBoxMaximumLineCount";
