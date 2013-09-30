d74ca89ae860025aea98a36cee19fc128a7846e4
Tor Lillqvist <tml@collabora.com>
2013-09-30 19:26:03 +0300

    Include <algorithm> for std::sort()
    Hopefully helps a Gentoo tinderbox.
    
    Change-Id: I2e83b867113ba04a708c9fbb46c728368c4328c0

--- xmloff/source/style/impastpl.cxx
+++ xmloff/source/style/impastpl.cxx
@@ -17,6 +17,8 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
+#include <algorithm>
+
 #include <rtl/ustrbuf.hxx>
 #include <tools/debug.hxx>
 #include <xmloff/PageMasterStyleMap.hxx>
