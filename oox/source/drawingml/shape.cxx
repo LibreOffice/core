3243987ae5bc5b78809f554222fc3ee6b36f1a16
Muthu Subramanian <sumuthu@collabora.com>
2013-10-07 19:19:10 +0530

    n#819614: Fix for lines running off screens.

--- oox/source/drawingml/shape.cxx
+++ oox/source/drawingml/shape.cxx
@@ -580,7 +580,7 @@ Reference< XShape > Shape::createAndInsert(
             }
 
             SAL_INFO("oox", "==cscode== shape name: '" << msName << "'");
-            mpCustomShapePropertiesPtr->pushToPropSet( rFilterBase, xSet, mxShape );
+            mpCustomShapePropertiesPtr->pushToPropSet( rFilterBase, xSet, mxShape, maSize );
         }
         else if( getTextBody() )
             getTextBody()->getTextProperties().pushVertSimulation();
