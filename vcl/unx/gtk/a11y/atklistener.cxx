d73acee01885fae5465fa9c7c1d88be683d60e98
Julien Nabet <serval2412@yahoo.fr>
2013-10-09 07:32:59 +0200

    silent deprecated: atk_focus_tracker_notify
    Change-Id: Ibff239592b517f8ba2ccd0649184e36f95cf1c2d

--- vcl/unx/gtk/a11y/atklistener.cxx
+++ vcl/unx/gtk/a11y/atklistener.cxx
@@ -78,7 +78,10 @@ extern "C" {
         // This is an equivalent to a state change to DEFUNC(T).
         atk_object_notify_state_change( atk_obj, ATK_STATE_DEFUNCT, TRUE );
         if( atk_get_focus_object() == atk_obj )
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
             atk_focus_tracker_notify( NULL );
+#pragma GCC diagnostic pop
         g_object_unref( G_OBJECT( atk_obj ) );
         return FALSE;
     }
