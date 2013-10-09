LibreOffice/core

commit 55a15541a17e5c9b49039b0db31473ab81c2456b

convert svx/source/engine3d/*.cxx from String to OUString
Change-Id: I0ef33d146a54c35ff9ad6ed8e1105de55d2fb78a

svx/source/engine3d/float3d.cxx
@@ -2183,7 +2183,7 @@ void Svx3DWin::GetAttr( SfxItemSet& rAttrs )
     if( aLbMatColor.GetSelectEntryCount() )
     {
         aColor = aLbMatColor.GetSelectEntryColor();
-        rAttrs.Put( XFillColorItem( String(), aColor) );
+        rAttrs.Put( XFillColorItem( "", aColor) );
     }
     else
     {
@@ -3053,13 +3053,13 @@ sal_Bool Svx3DWin::LBSelectColor( ColorLB* pLb, const Color& rColor )
     pLb->SelectEntry( rColor );
     if( pLb->GetSelectEntryCount() == 0 )
     {
-        String aStr(SVX_RESSTR(RID_SVXFLOAT3D_FIX_R));
+        OUString aStr(SVX_RESSTR(RID_SVXFLOAT3D_FIX_R));
 
         aStr += OUString::number((sal_Int32)rColor.GetRed());
-        aStr += sal_Unicode(' ');
+        aStr += " '";
         aStr += SVX_RESSTR(RID_SVXFLOAT3D_FIX_G);
         aStr += OUString::number((sal_Int32)rColor.GetGreen());
-        aStr += sal_Unicode(' ');
+        aStr += " ";
         aStr += SVX_RESSTR(RID_SVXFLOAT3D_FIX_B);
         aStr += OUString::number((sal_Int32)rColor.GetBlue());
 

commit 0ba03a38f022ca8d3705ae1370bb1d739e1e973f

stray ' in string
regression since 55a15541a17e5c9b49039b0db31473ab81c2456b

Change-Id: I9c5853061eef5befba7644ea0ca05b7751addc6c

svx/source/engine3d/float3d.cxx
@@ -3056,7 +3056,7 @@ sal_Bool Svx3DWin::LBSelectColor( ColorLB* pLb, const Color& rColor )
         OUString aStr(SVX_RESSTR(RID_SVXFLOAT3D_FIX_R));
 
         aStr += OUString::number((sal_Int32)rColor.GetRed());
-        aStr += " '";
+        aStr += " ";
         aStr += SVX_RESSTR(RID_SVXFLOAT3D_FIX_G);
         aStr += OUString::number((sal_Int32)rColor.GetGreen());
         aStr += " ";