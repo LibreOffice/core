46ddcbbe2a4b2e97770f2c962c6450a48f539cd9
Tor Lillqvist <tml@collabora.com>
2013-10-10 09:38:58 +0300

    Normalize ENABLE_PDFIMPORT: TRUE or empty
    Change-Id: I962b37757dbf3569df73e70ae2d7c666d7cdf43e

--- sdext/source/pdfimport/xpdftest/makefile.mk
+++ sdext/source/pdfimport/xpdftest/makefile.mk
@@ -26,7 +26,7 @@ ENABLE_EXCEPTIONS=TRUE
 
 .INCLUDE: settings.mk
 
-.IF "$(ENABLE_PDFIMPORT)" == "FALSE"
+.IF "$(ENABLE_PDFIMPORT)" != "TRUE"
 @all:
     @echo "PDF Import extension disabled."
 .ENDIF
