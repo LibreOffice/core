365230c12f8c6ff7ebf83af6a64093b0ab79ba0e
Andras Timar <andras.timar@collabora.com>
2013-10-16 09:29:56 +0200

    fdo#69548 do not remove last space from hyperlink target
    Change-Id: Idd6e28a8838245440e350f0593ea544e057483e6

--- writerfilter/source/dmapper/DomainMapper_Impl.cxx
+++ writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -2138,14 +2138,10 @@ void FieldContext::AppendCommand(const OUString& rPart)
 
         if (bInString)
         {
-            if (bInStringNext)
+            sPart += OUString(' ');
+            sPart += sToken;
+            if (!bInStringNext)
             {
-                sPart += OUString(' ');
-                sPart += sToken;
-            }
-            else
-            {
-                sPart += sToken;
                 aResult.push_back(sPart);
             }
         }
