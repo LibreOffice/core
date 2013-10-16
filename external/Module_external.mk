a54be4fae66bad8234de209b5137bff437ddc4ce
Khaled Hosny <khaledhosny@eglug.org>
2013-10-16 19:59:27 +0200

    fdo#70393: move harfbuzz to a subdir of external
    Change-Id: I3eaa6d95aaa1753822e20d21f90f39cadb939332

--- external/Module_external.mk
+++ external/Module_external.mk
@@ -31,7 +31,9 @@ $(eval $(call gb_Module_add_targets,external,\
 endif
 endif
 
-$(call gb_Helper_optional,LIBPNG,\
-    $(eval $(call gb_Module_add_moduledir,external,libpng)))
+$(eval $(call gb_Module_add_moduledirs,external,\
+	$(call gb_Helper_optional,HARFBUZZ,harfbuzz) \
+	$(call gb_Helper_optional,LIBPNG,libpng) \
+))
 
 # vim: set noet sw=4 ts=4:
