39f73d93874114e2bacfd73da137180aad95a3f6
Khaled Hosny <khaledhosny@eglug.org>
2013-10-17 19:22:46 +0200

    fdo#70393: move ct2n to a subdir of external
    Change-Id: If93ae0fa968539f72c9dbf29b4496355b6d66e05
    Reviewed-on: https://gerrit.libreoffice.org/6297
    Reviewed-by: David Tardon <dtardon@redhat.com>
    Tested-by: David Tardon <dtardon@redhat.com>

--- external/Module_external.mk
+++ external/Module_external.mk
@@ -25,6 +25,7 @@ $(eval $(call gb_Module_add_moduledirs,external,\
 	$(call gb_Helper_optional,CAIRO,cairo) \
 	$(call gb_Helper_optional,CLUCENE,clucene) \
 	$(call gb_Helper_optional,CPPUNIT,cppunit) \
+	$(call gb_Helper_optional,CT2N,ct2n) \
 	$(call gb_Helper_optional,HARFBUZZ,harfbuzz) \
 	$(call gb_Helper_optional,LIBPNG,libpng) \
 ))
--- external/ct2n/Module_ct2n.mk
+++ external/ct2n/Module_ct2n.mk
@@ -9,13 +9,9 @@
 
 $(eval $(call gb_Module_Module,ConvertTextToNumber))
 
-ifneq (,$(filter CT2N,$(BUILD_TYPE)))
-
 $(eval $(call gb_Module_add_targets,ConvertTextToNumber,\
 	ExternalPackage_ct2n \
 	UnpackedTarball_ct2n \
 ))
-
-endif
 
 # vim:set noet sw=4 ts=4:
