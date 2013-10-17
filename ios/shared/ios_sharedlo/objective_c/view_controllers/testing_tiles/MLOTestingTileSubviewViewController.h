76a9ae801bcc252947da991e9bf6dc3ebfde5ebf
Ptyl Dragon <ptyl@cloudon.com>
2013-10-17 17:01:35 +0200

    added for infrastructure for ios tile testing app
    Change-Id: Ie8ac64769c01c5e967a56844a69747ef4d4f20af

---
+++ ios/shared/ios_sharedlo/objective_c/view_controllers/testing_tiles/MLOTestingTileSubviewViewController.h
@@ -0,0 +1,15 @@
+//
+//  MLOTestingTileSubviewViewController.h
+//  ios_sharedlo
+//
+//  Created by ptyl on 10/17/13.
+//  Copyright (c) 2013 LibreOffice.org. All rights reserved.
+//
+
+#import "MLOViewController.h"
+
+@class MLOAppRoleTileTester;
+@interface MLOTestingTileSubviewViewController : MLOViewController
+@property MLOAppRoleTileTester * tester;
+
+@end
---
+++ ios/shared/ios_sharedlo/objective_c/view_controllers/testing_tiles/MLOTestingTileSubviewViewController.m
@@ -0,0 +1,14 @@
+//
+//  MLOTestingTileSubviewViewController.m
+//  ios_sharedlo
+//
+//  Created by ptyl on 10/17/13.
+//  Copyright (c) 2013 LibreOffice.org. All rights reserved.
+//
+
+#import "MLOTestingTileSubviewViewController.h"
+
+@implementation MLOTestingTileSubviewViewController
+
+
+@end
