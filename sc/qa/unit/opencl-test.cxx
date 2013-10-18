3836da8de207d8d346c8dbdb959f9f25248d93a2
haochen <haochen@multicorewareinc.com>
2013-10-18 20:40:28 +0800

    Testcases for MIRR
    Signed-off-by: I-Jui (Ray) Sung <ray@multicorewareinc.com>

--- sc/qa/unit/opencl-test.cxx
+++ sc/qa/unit/opencl-test.cxx
@@ -379,6 +379,13 @@ void ScOpenclTest::testFinacialFormula()
         double fExcel = pDocRes->GetValue(ScAddress(6,i,15));
         CPPUNIT_ASSERT_DOUBLES_EQUAL(fExcel, fLibre, fabs(0.0001*fExcel));
     }
+    //[AMLOEXT-37]
+    for (SCROW i = 1; i <= 10; ++i)
+    {
+        double fLibre = pDoc->GetValue(ScAddress(6,i,16));
+        double fExcel = pDocRes->GetValue(ScAddress(6,i,16));
+        CPPUNIT_ASSERT_DOUBLES_EQUAL(fExcel, fLibre, fabs(0.0001*fExcel));
+    }
     xDocSh->DoClose();
     xDocShRes->DoClose();
 }
