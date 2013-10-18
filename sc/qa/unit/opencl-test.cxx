7c426dc025ee026cd8459f8beee8a1f0e080d064
haochen <haochen@multicorewareinc.com>
2013-10-18 16:52:53 +0800

    Testcase for RECEIVED
    Signed-off-by: I-Jui (Ray) Sung <ray@multicorewareinc.com>

--- sc/qa/unit/opencl-test.cxx
+++ sc/qa/unit/opencl-test.cxx
@@ -309,6 +309,13 @@ void ScOpenclTest::testFinacialFormula()
         double fExcel = pDocRes->GetValue(ScAddress(3,i,5));
         CPPUNIT_ASSERT_DOUBLES_EQUAL(fExcel, fLibre, fabs(0.0001*fExcel));
     }
+	//[AMLOEXT-27]
+	for (SCROW i = 0; i < 10; ++i)
+    {
+        double fLibre = pDoc->GetValue(ScAddress(5,i,6));
+        double fExcel = pDocRes->GetValue(ScAddress(5,i,6));
+        CPPUNIT_ASSERT_DOUBLES_EQUAL(fExcel, fLibre, fabs(0.0001*fExcel));
+    }
     xDocSh->DoClose();
     xDocShRes->DoClose();
 }
