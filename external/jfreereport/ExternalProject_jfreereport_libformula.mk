aa927f479108a519f97cd4378c876fc0b8d1a909
Khaled Hosny <khaledhosny@eglug.org>
2013-10-18 18:51:26 +0200

    fdo#70393: move jfreereport to a subdir of external
    Change-Id: I92d5458b48cc7e1c4794407bf71c54f0f048f2d8

---
+++ external/jfreereport/ExternalProject_jfreereport_libformula.mk
@@ -0,0 +1,36 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+# for VERSION
+include $(SRCDIR)/external/jfreereport/version.mk
+
+$(eval $(call gb_ExternalProject_ExternalProject,jfreereport_libformula))
+
+$(eval $(call gb_ExternalProject_register_targets,jfreereport_libformula,\
+	build \
+))
+
+$(call gb_ExternalProject_get_state_target,jfreereport_libformula,build) :
+	$(call gb_ExternalProject_run,build,\
+		$(ICECREAM_RUN) "$(ANT)" \
+			-q \
+			-f build.xml \
+			-Dbuild.label="build-$(LIBO_VERSION_MAJOR).$(LIBO_VERSION_MINOR).$(LIBO_VERSION_MICRO).$(LIBO_VERSION_PATCH)" \
+			$(if $(filter yes,$(JAVACISGCJ))\
+				,-Dbuild.compiler=gcj \
+				,-Dant.build.javac.source=$(JAVA_SOURCE_VER) \
+				 -Dant.build.javac.target=$(JAVA_TARGET_VER) \
+			) \
+			-Dantcontrib.available="true" \
+			-Dbuild.id="10682" \
+			-Dproject.revision="$(LIBFORMULA_VERSION)" \
+			$(if $(debug),-Dbuild.debug="on") jar \
+	)
+
+# vim: set noet sw=4 ts=4:
