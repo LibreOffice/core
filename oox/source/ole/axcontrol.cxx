LibreOffice/core

commit ed338bc212725f422f0def21aafc82f05e350646

OUString cleanup in oox
Change-Id: Id3c121acddd515621325c93735bb3dd88592749e
Signed-off-by: José Guilherme Vanz <guilherme.sft@gmail.com>
Reviewed-on: https://gerrit.libreoffice.org/1485
Reviewed-by: Muthu Subramanian K <muthusuba@gmail.com>
Tested-by: Muthu Subramanian K <muthusuba@gmail.com>

oox/source/ole/axcontrol.cxx
@@ -558,11 +558,11 @@ void ControlConverter::convertToAxState( PropertySet& rPropSet,
     // control is awt or not )
     rPropSet.getProperty( nState, PROP_State );
 
-    rValue = rtl::OUString(); // empty e.g. 'don't know'
+    rValue = OUString(); // empty e.g. 'don't know'
     if ( nState == API_STATE_UNCHECKED )
-        rValue = rtl::OUString( '0' );
+        rValue = OUString('0');
     else if ( nState == API_STATE_CHECKED )
-        rValue = rtl::OUString( '1' );
+        rValue = OUString('1');
 
     // tristate
     if( bSupportsTriState && rPropSet.getProperty( bTmp, PROP_TriState ) )
@@ -2414,16 +2414,16 @@ HtmlSelectModel::HtmlSelectModel()
 bool
 HtmlSelectModel::importBinaryModel( BinaryInputStream& rInStrm )
 {
-    static String sMultiple( RTL_CONSTASCII_USTRINGPARAM("<SELECT MULTIPLE") );
-    static String sSelected( RTL_CONSTASCII_USTRINGPARAM("OPTION SELECTED") );
+    static OUString sMultiple( "<SELECT MULTIPLE" );
+    static OUString sSelected( "OPTION SELECTED" );
 
     OUString sStringContents = rInStrm.readUnicodeArray( rInStrm.size() );
 
-    String data = sStringContents;
+    OUString data = sStringContents;
 
     // replace crlf with lf
-    data.SearchAndReplaceAll( String( RTL_CONSTASCII_USTRINGPARAM( "\x0D\x0A" ) ), String( RTL_CONSTASCII_USTRINGPARAM( "\x0A" ) ) );
-    std::vector< rtl::OUString > listValues;
+    data = data.replaceAll( "\x0D\x0A" , "\x0A" );
+    std::vector< OUString > listValues;
     std::vector< sal_Int16 > selectedIndices;
 
     // Ultra hacky parser for the info
@@ -2431,10 +2431,10 @@ HtmlSelectModel::importBinaryModel( BinaryInputStream& rInStrm )
 
     for ( sal_Int32 nToken = 0; nToken < nTokenCount; ++nToken )
     {
-        String sLine( data.GetToken( nToken, '\n' ) );
+        OUString sLine( data.getToken( nToken, '\n' ) );
         if ( !nToken ) // first line will tell us if multiselect is enabled
         {
-            if ( sLine.CompareTo( sMultiple, sMultiple.Len() ) == COMPARE_EQUAL )
+            if ( sLine == sMultiple )
                 mnMultiSelect = true;
         }
         // skip first and last lines, no data there
@@ -2442,17 +2442,17 @@ HtmlSelectModel::importBinaryModel( BinaryInputStream& rInStrm )
         {
             if ( comphelper::string::getTokenCount(sLine, '>') )
             {
-                String displayValue  = sLine.GetToken( 1, '>' );
-                if ( displayValue.Len() )
+                OUString displayValue  = sLine.getToken( 1, '>' );
+                if ( displayValue.getLength() )
                 {
                     // Really we should be using a proper html parser
                     // escaping some common bits to be escaped
-                    displayValue.SearchAndReplace( String( RTL_CONSTASCII_USTRINGPARAM( "&lt;" ) ), String( RTL_CONSTASCII_USTRINGPARAM("<") ) );
-                    displayValue.SearchAndReplace( String( RTL_CONSTASCII_USTRINGPARAM( "&gt;" ) ), String( RTL_CONSTASCII_USTRINGPARAM(">") ) );
-                    displayValue.SearchAndReplace( String( RTL_CONSTASCII_USTRINGPARAM( "&quot;" ) ), String( RTL_CONSTASCII_USTRINGPARAM("\"") ) );
-                    displayValue.SearchAndReplace( String( RTL_CONSTASCII_USTRINGPARAM( "&amp;" ) ), String( RTL_CONSTASCII_USTRINGPARAM("&") ) );
+                    displayValue = displayValue.replaceAll( "&lt;", "<" );
+                    displayValue = displayValue.replaceAll( "&gt;", ">" );
+                    displayValue = displayValue.replaceAll( "&quot;", "\"" );
+                    displayValue = displayValue.replaceAll( "&amp;", "&" );
                     listValues.push_back( displayValue );
-                    if( sLine.Search( sSelected ) != STRING_NOTFOUND )
+                    if( sLine.indexOf( sSelected ) != STRING_NOTFOUND )
                         selectedIndices.push_back( static_cast< sal_Int16 >( listValues.size() ) - 1 );
                 }
             }
@@ -2462,7 +2462,7 @@ HtmlSelectModel::importBinaryModel( BinaryInputStream& rInStrm )
     {
         msListData.realloc( listValues.size() );
         sal_Int32 index = 0;
-        for( std::vector< rtl::OUString >::iterator it = listValues.begin(); it != listValues.end(); ++it, ++index )
+        for( std::vector< OUString >::iterator it = listValues.begin(); it != listValues.end(); ++it, ++index )
              msListData[ index ] = *it;
     }
     if ( !selectedIndices.empty() )
@@ -2497,7 +2497,7 @@ HtmlTextBoxModel::importBinaryModel( BinaryInputStream& rInStrm )
     // in msocximex ( where this is ported from, it appears *nothing* is read
     // from the control stream ), surely there is some useful info there ?
     OSL_TRACE("HtmlTextBoxModel::importBinaryModel - string contents of stream :");
-    OSL_TRACE("%s", rtl::OUStringToOString( sStringContents, RTL_TEXTENCODING_UTF8 ).getStr() );
+    OSL_TRACE("%s", OUStringToOString( sStringContents, RTL_TEXTENCODING_UTF8 ).getStr() );
 #else
     (void) rInStrm;
 #endif

oox/source/ole/axbinaryreader.cxx
@@ -26,10 +26,6 @@ namespace ole {
 
 // ============================================================================
 
-using ::rtl::OUString;
-
-// ============================================================================
-
 namespace {
 
 const sal_uInt32 AX_STRING_SIZEMASK         = 0x7FFFFFFF;
@@ -218,7 +214,7 @@ void AxBinaryPropertyReader::readStringProperty( OUString& orValue )
     }
 }
 
-void AxBinaryPropertyReader::readGuidProperty( ::rtl::OUString& orGuid )
+void AxBinaryPropertyReader::readGuidProperty( OUString& orGuid )
 {
     if( startNextProperty() )
         maLargeProps.push_back( ComplexPropVector::value_type( new GuidProperty( orGuid ) ) );

oox/source/ole/axbinarywriter.cxx
@@ -34,10 +34,6 @@ namespace ole {
 
 // ============================================================================
 
-using ::rtl::OUString;
-
-// ============================================================================
-
 namespace {
 
 const sal_uInt32 AX_STRING_SIZEMASK         = 0x7FFFFFFF;

oox/source/ole/axcontrolfragment.cxx
@@ -38,7 +38,6 @@ using ::oox::core::ContextHandler2;
 using ::oox::core::ContextHandlerRef;
 using ::oox::core::FragmentHandler2;
 using ::oox::core::XmlFilterBase;
-using ::rtl::OUString;
 
 // ============================================================================
 

oox/source/ole/axfontdata.cxx
@@ -25,8 +25,6 @@
 namespace oox {
 namespace ole {
 
-using ::rtl::OUString;
-
 // ============================================================================
 
 AxFontData::AxFontData() :

oox/source/ole/olehelper.cxx
@@ -46,9 +46,6 @@ namespace ole {
 
 // ============================================================================
 
-using ::rtl::OUString;
-using ::rtl::OUStringBuffer;
-
 using ::com::sun::star::form::XFormComponent;
 using ::com::sun::star::form::XForm;
 using ::com::sun::star::awt::XControlModel;
@@ -238,7 +235,7 @@ StdFontInfo::StdFontInfo() :
 {
 }
 
-StdFontInfo::StdFontInfo( const ::rtl::OUString& rName, sal_uInt32 nHeight,
+StdFontInfo::StdFontInfo( const OUString& rName, sal_uInt32 nHeight,
         sal_uInt16 nWeight, sal_uInt16 nCharSet, sal_uInt8 nFlags ) :
     maName( rName ),
     mnHeight( nHeight ),
@@ -361,22 +358,22 @@ class OleFormCtrlExportHelper
     Reference< XModel > mxDocModel;
     Reference< XControlModel > mxControlModel;
 
-    ::rtl::OUString maName;
-    ::rtl::OUString maTypeName;
-    ::rtl::OUString maFullName;
-    ::rtl::OUString maGUID;
+    OUString maName;
+    OUString maTypeName;
+    OUString maFullName;
+    OUString maGUID;
 public:
     OleFormCtrlExportHelper( const Reference< XComponentContext >& rxCtx, const Reference< XModel >& xDocModel, const Reference< XControlModel >& xModel );
     virtual ~OleFormCtrlExportHelper() { }
-    virtual ::rtl::OUString getGUID()
+    virtual OUString getGUID()
     {
-        rtl::OUString sResult;
+        OUString sResult;
         if ( maGUID.getLength() > 2 )
             sResult = maGUID.copy(1, maGUID.getLength() - 2 );
         return sResult;
     }
-    ::rtl::OUString getFullName() { return maFullName; }
-    ::rtl::OUString getTypeName() { return maTypeName; }
+    OUString getFullName() { return maFullName; }
+    OUString getTypeName() { return maTypeName; }
     bool isValid() { return mpModel != NULL; }
     void exportName( const Reference< XOutputStream >& rxOut );
     void exportCompObj( const Reference< XOutputStream >& rxOut );
@@ -475,7 +472,7 @@ MSConvertOCXControls::~MSConvertOCXControls()
 }
 
 bool
-MSConvertOCXControls::importControlFromStream( ::oox::BinaryInputStream& rInStrm, Reference< XFormComponent >& rxFormComp, const ::rtl::OUString& rGuidString )
+MSConvertOCXControls::importControlFromStream( ::oox::BinaryInputStream& rInStrm, Reference< XFormComponent >& rxFormComp, const OUString& rGuidString )
 {
     ::oox::ole::EmbeddedControl aControl( CREATE_OUSTRING( "Unknown" ) );
     if( ::oox::ole::ControlModelBase* pModel = aControl.createModelFromGuid( rGuidString  ) )
@@ -504,7 +501,7 @@ MSConvertOCXControls::ReadOCXCtlsStream( SotStorageStreamRef& rSrc1, Reference< 
     return sal_False;
 }
 
-bool MSConvertOCXControls::importControlFromStream( ::oox::BinaryInputStream& rInStrm, Reference< XFormComponent >& rxFormComp, const rtl::OUString& rStrmClassId,
+bool MSConvertOCXControls::importControlFromStream( ::oox::BinaryInputStream& rInStrm, Reference< XFormComponent >& rxFormComp, const OUString& rStrmClassId,
                                    sal_Int32 nStreamSize)
 {
     if ( !rInStrm.isEof() )
@@ -575,7 +572,7 @@ sal_Bool MSConvertOCXControls::ReadOCXStorage( SotStorageRef& xOleStg,
 
 sal_Bool MSConvertOCXControls::WriteOCXStream( const Reference< XModel >& rxModel, SotStorageRef &xOleStg,
     const Reference< XControlModel > &rxControlModel,
-    const com::sun::star::awt::Size& rSize, rtl::OUString &rName)
+    const com::sun::star::awt::Size& rSize, OUString &rName)
 {
     SvGlobalName aName;
 
@@ -584,10 +581,10 @@ sal_Bool MSConvertOCXControls::WriteOCXStream( const Reference< XModel >& rxMode
     if ( !exportHelper.isValid() )
         return sal_False;
 
-    rtl::OUString sId = exportHelper.getGUID();
+    OUString sId = exportHelper.getGUID();
     aName.MakeId(sId);
 
-    rtl::OUString sFullName = exportHelper.getFullName();
+    OUString sFullName = exportHelper.getFullName();
     rName = exportHelper.getTypeName();
     xOleStg->SetClass( aName,0x5C,sFullName);
     {
@@ -632,14 +629,14 @@ const Reference< XIndexContainer >&
             Reference< XNameContainer >  xNameCont =
                 xFormsSupplier->getForms();
 
-            rtl::OUString sStdName = CREATE_OUSTRING( "WW-Standard" );
-            rtl::OUString sName( sStdName );
+            OUString sStdName = CREATE_OUSTRING( "WW-Standard" );
+            OUString sName( sStdName );
             sal_uInt16 n = 0;
 
             while( xNameCont->hasByName( sName ) )
             {
                 sName = sStdName;
-                sName += rtl::OUString::valueOf(static_cast<sal_Int32>(++n));
+                sName += OUString::valueOf(static_cast<sal_Int32>(++n));
             }
 
             const Reference< XMultiServiceFactory > &rServiceFactory

oox/source/ole/oleobjecthelper.cxx
@@ -41,8 +41,6 @@ using namespace ::com::sun::star::io;
 using namespace ::com::sun::star::lang;
 using namespace ::com::sun::star::uno;
 
-using ::rtl::OUString;
-
 // ============================================================================
 
 OleObjectInfo::OleObjectInfo() :

oox/source/ole/olestorage.cxx
@@ -47,8 +47,6 @@ using namespace ::com::sun::star::io;
 using namespace ::com::sun::star::lang;
 using namespace ::com::sun::star::uno;
 
-using ::rtl::OUString;
-
 // ============================================================================
 
 namespace {

commit 3f16ec73e07b7c187200656cfa969d71b4bea7b3

wrong comparison
regression since ed338bc212725f422f0def21aafc82f05e350646

Change-Id: Ia5dfe7a551abe244c313d3bdb2b90196fc0c6ba2

oox/source/ole/axcontrol.cxx
@@ -2609,7 +2609,7 @@ HtmlSelectModel::importBinaryModel( BinaryInputStream& rInStrm )
                     displayValue = displayValue.replaceAll( "&quot;", "\"" );
                     displayValue = displayValue.replaceAll( "&amp;", "&" );
                     listValues.push_back( displayValue );
-                    if( sLine.indexOf( sSelected ) != STRING_NOTFOUND )
+                    if( sLine.indexOf( sSelected ) != -1 )
                         selectedIndices.push_back( static_cast< sal_Int16 >( listValues.size() ) - 1 );
                 }
             }