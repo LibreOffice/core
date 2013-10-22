bcc31e249fde6536ab96ffcf2a8e542275efa69b
Stephan Bergmann <sbergman@redhat.com>
2013-10-22 23:47:53 +0200

    Simplify SVLIBRARY
    Change-Id: Ib1c3cdca7652f5621ba79b1e3590a9241021c389

--- include/tools/solar.h
+++ include/tools/solar.h
@@ -120,20 +120,12 @@ inline void     DoubleToSVBT64( double n, SVBT64 p ) { p[0] = ((sal_uInt8*)&n)[7
 
 #define xub_StrLen      sal_uInt16
 
-#define STRING_CONCAT3( s1, s2, s3 ) \
-    s1 s2 s3
-
-// dll file extensions
-
 #if defined WNT
-#define SVLIBRARY( Base ) \
-    STRING_CONCAT3( Base, "lo", ".dll" )
+#define SVLIBRARY( Base ) Base "lo.dll"
 #elif defined MACOSX
-#define SVLIBRARY( Base ) \
-    STRING_CONCAT3( "lib", Base, "lo.dylib" )
+#define SVLIBRARY( Base ) "lib" Base "lo.dylib"
 #elif defined UNX
-#define SVLIBRARY( Base ) \
-    STRING_CONCAT3( "lib", Base, "lo.so" )
+#define SVLIBRARY( Base ) "lib" Base "lo.so"
 #else
   #error unknown platform
 #endif
