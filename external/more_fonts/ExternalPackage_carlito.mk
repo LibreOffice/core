bde5e683286096b9255254b28a862e519d57f547
Caolán McNamara <caolanm@redhat.com>
2013-10-23 14:57:32 +0100

    bundle Carlito and Caladea
    Change-Id: Ibb68ad33764bcbab88e68c35805a00287177a5c8

---
+++ external/more_fonts/ExternalPackage_carlito.mk
@@ -0,0 +1,21 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_ExternalPackage_ExternalPackage,fonts_carlito,font_carlito))
+
+$(eval $(call gb_ExternalPackage_set_outdir,fonts_carlito,$(INSTROOT)))
+
+$(eval $(call gb_ExternalPackage_add_unpacked_files,fonts_carlito,$(LIBO_SHARE_FOLDER)/fonts/truetype,\
+	Carlito-BoldItalic.ttf \
+	Carlito-Regular.ttf \
+	Carlito-Italic.ttf \
+	Carlito-Bold.ttf \
+))
+
+# vim: set noet sw=4 ts=4:
