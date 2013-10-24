7de8a3f6877690c41a52c21e58d7c31528f5455d
haochen <haochen@multicorewareinc.com>
2013-10-24 17:04:53 +0800

    Implement the fix for COUPDAYS in GPU Calc
    Change-Id: Id96a70fe9092d052a30c8af13d2e1f95a236f7fd
    
    Signed-off-by: Wei Wei <weiwei@multicorewareinc.com>

--- sc/source/core/opencl/formulagroupcl.cxx
+++ sc/source/core/opencl/formulagroupcl.cxx
@@ -912,6 +912,12 @@ DynamicKernelSoPArguments<Op>::DynamicKernelSoPArguments(const std::string &s,
                     mvSubArguments.push_back(SoPHelper<OpDollarfr>(ts,
                         ft->Children[i]));
                 }
+                else if ( !(pChild->GetExternal().compareTo(OUString(
+                     "com.sun.star.sheet.addin.Analysis.getCoupdays"))))
+                {
+                    mvSubArguments.push_back(SoPHelper<OpCoupdays>(ts,
+                        ft->Children[i]));
+                }
                 break;
             default:
                 assert(0 && "Unsupported");
