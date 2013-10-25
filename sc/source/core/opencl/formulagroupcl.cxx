f15ab5095a37bb2bd7f6e1ac4d5bdfb46ab29c6f
haochen <haochen@multicorewareinc.com>
2013-10-25 10:53:21 +0800

    Implement fix for PEARSON in GPU Calc
    Change-Id: I16aeba7c889677119995939a35be6ccad2517725
    
    Signed-off-by: Wei Wei <weiwei@multicorewareinc.com>

--- sc/source/core/opencl/formulagroupcl.cxx
+++ sc/source/core/opencl/formulagroupcl.cxx
@@ -811,6 +811,10 @@ DynamicKernelSoPArguments<Op>::DynamicKernelSoPArguments(const std::string &s,
                 mvSubArguments.push_back(SoPHelper<OpNegbinomdist>(ts,
                     ft->Children[i]));
                 break;
+            case ocPearson:
+                mvSubArguments.push_back(SoPHelper<OpPearson>(ts,
+                    ft->Children[i]));
+                break;
             case ocExternal:
                 if ( !(pChild->GetExternal().compareTo(OUString(
                     "com.sun.star.sheet.addin.Analysis.getEffect"))))
