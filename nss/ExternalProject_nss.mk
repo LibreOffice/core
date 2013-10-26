1aae6f6087214f143886dd4c18d226fb1657b5c3
Michael Stahl <mstahl@redhat.com>
2013-10-26 21:35:32 +0200

    nss: fix build breakage of postgresql
    Somehow postgresql needs to link both openssl and NSS (yay) ... and it
    turns out that NSS builds a "libssl.a" on MacOSX and Linux, and of
    course OpenSSL does too... so avoid that by removing *.a in NSS tree,
    which are not needed anyway (some of these are named the same as the
    corresponding .so/.dylib to...).
    
    Change-Id: I5c9718e655d350c2e7d0ae05596f8e3130e9bfe1

--- nss/ExternalProject_nss.mk
+++ nss/ExternalProject_nss.mk
@@ -51,6 +51,7 @@ $(call gb_ExternalProject_get_state_target,nss,build): $(call gb_ExternalProject
 			IMPORT_LIB_SUFFIX=dll.a \
 			NSPR_CONFIGURE_OPTS="--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM) --enable-shared --disable-static" \
 			NSINSTALL="$(call gb_ExternalExecutable_get_command,python) $(SRCDIR)/nss/nsinstall.py" \
+		&& rm -f $(call gb_UnpackedTarball_get_dir,nss)/mozilla/dist/out/lib/*.a \
 	,mozilla/security/nss)
 
 endif
@@ -65,6 +66,7 @@ $(call gb_ExternalProject_get_state_target,nss,build): $(call gb_ExternalProject
 		NSINSTALL="$(call gb_ExternalExecutable_get_command,python) $(SRCDIR)/nss/nsinstall.py") \
 		NSDISTMODE=copy \
 		$(MAKE) -j1 AR=$(AR) RANLIB=$(RANLIB) NMEDIT=$(NM)edit nss_build_all \
+		&& rm -f $(call gb_UnpackedTarball_get_dir,nss)/mozilla/dist/out/lib/*.a \
 		$(if $(filter MACOSX,$(OS)),&& $(PERL) \
 			$(SOLARENV)/bin/macosx-change-install-names.pl shl OOO \
 			$(gb_Package_SOURCEDIR_nss)/mozilla/dist/out/lib/libfreebl3.dylib \
