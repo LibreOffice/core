033830b3b020d63a1b77459ab30497aeecc856bb
Michael Stahl <mstahl@redhat.com>
2013-10-28 14:11:00 +0100

    comphelper: use boost_headers in unit test
    Change-Id: I3858dd3d176757743a39d6a8d9c5328dd5ff34df

--- comphelper/CppunitTest_comphelper_syntaxhighlight_test.mk
+++ comphelper/CppunitTest_comphelper_syntaxhighlight_test.mk
@@ -13,6 +13,10 @@ $(eval $(call gb_CppunitTest_add_exception_objects,comphelper_syntaxhighlight_te
     comphelper/qa/unit/syntaxhighlighttest \
 ))
 
+$(eval $(call gb_CppunitTest_use_externals,comphelper_syntaxhighlight_test,\
+	boost_headers \
+))
+
 $(eval $(call gb_CppunitTest_use_api,comphelper_syntaxhighlight_test, \
 	udkapi \
 	offapi \
