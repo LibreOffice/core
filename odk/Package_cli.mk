d1a06bc2b48de471fc2d7516f13c017b9da009b7
Michael Stahl <mstahl@redhat.com>
2013-10-28 11:31:45 +0100

    odk: adapt to cli_ure/unoil changes
    Change-Id: I9fec64c247de793ae30434c615752f48d1bc2205

--- odk/Package_cli.mk
+++ odk/Package_cli.mk
@@ -7,16 +7,16 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 
-$(eval $(call gb_Package_Package,odk_cli,$(OUTDIR)/bin))
+$(eval $(call gb_Package_Package,odk_cli,$(INSTROOT)))
 
 $(eval $(call gb_Package_set_outdir,odk_cli,$(INSTDIR)))
 
 $(eval $(call gb_Package_add_files,odk_cli,$(SDKDIRNAME)/cli,\
-	assembly/cli_cppuhelper.dll \
-	cli_basetypes.dll \
-	cli_oootypes.dll \
-	cli_ure.dll \
-	cli_uretypes.dll \
+	$(LIBO_URE_LIB_FOLDER)/cli_cppuhelper.dll \
+	$(LIBO_URE_LIB_FOLDER)/cli_basetypes.dll \
+	$(LIBO_URE_LIB_FOLDER)/cli_ure.dll \
+	$(LIBO_URE_LIB_FOLDER)/cli_uretypes.dll \
+	$(LIBO_LIB_FOLDER)/cli_oootypes.dll \
 ))
 
 # vim: set noet sw=4 ts=4:
