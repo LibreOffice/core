89ded41db1f9884eec75097206a64df1b1c7f72a
Yuri Dario <ydario@apache.org>
2013-10-28 11:07:01 +0000

    i118923 - OS/2 port: make use of system ExceptQ exception handler.

--- sal/osl/os2/salinit.cxx
+++ sal/osl/os2/salinit.cxx
@@ -19,99 +19,29 @@
  *
  *************************************************************/
 
+#include <stdio.h>
 
-
-#define INCL_DOS
-#include <os2.h>
-
-#include "precompiled_sal.hxx"
-#include "sal/config.h"
-
-#include "osl/process.h"
-#include "sal/main.h"
 #include "sal/types.h"
 
-// for exception logging
-#include <stdio.h>
-#include <string.h>
-#include <setjmp.h>
-
+#ifdef __cplusplus
 extern "C" {
-
-/*----------------------------------------------------------------------------*/
-
-static CHAR        szOOoExe[CCHMAXPATH];
-
-static FILE* APIENTRY _oslExceptOpenLogFile(VOID)
-{
-   FILE        *file;
-   DATETIME    DT;
-   PPIB        pib;
-   PSZ         slash;
-
-   // get executable fullpath
-   DosGetInfoBlocks(NULL, &pib);
-   DosQueryModuleName(pib->pib_hmte, sizeof(szOOoExe), szOOoExe);
-   // truncate to exe name
-   slash = (PSZ)strrchr( szOOoExe, '.');
-   *slash = '\0';
-   // make log path
-   strcat( szOOoExe, ".log");
-
-   file = fopen( szOOoExe, "a");
-   if (!file) { // e.g. readonly drive
-      // try again, usually C exist and is writable
-      file = fopen( "c:\\OOo.log", "a");
-   }
-   if (file) {
-        DosGetDateTime(&DT);
-        fprintf(file, "\nTrap message -- Date: %04d-%02d-%02d, Time: %02d:%02d:%02d\n",
-            DT.year, DT.month, DT.day,
-            DT.hours, DT.minutes, DT.seconds);
-        fprintf(file, "-------------------------------------------------------\n"
-                      "\nAn internal error occurred (Built " __DATE__ "-" __TIME__ ").\n");
-
-   }
-
-   // ok, return handle
-   return (file);
-}
-
-/*----------------------------------------------------------------------------*/
-
-#if 0 // until exceptions restored OSL_DEBUG_LEVEL == 0
-static EXCEPTSTRUCT     g_excptstruct = {0};
 #endif
 
+/*----------------------------------------------------------------------------*/
+
+// replaced by macros in sal/main.h
 void SAL_CALL sal_detail_initialize(int argc, char ** argv)
 {
-    APIRET rc = -1;
-
-#if 0 // until exceptions restored OSL_DEBUG_LEVEL == 0
-    excRegisterHooks(_oslExceptOpenLogFile, NULL, NULL, FALSE);
-
-    g_excptstruct.RegRec2.pfnHandler = (PFN)excHandlerLoud;
-    g_excptstruct.arc = DosSetExceptionHandler(
-               (PEXCEPTIONREGISTRATIONRECORD)&(g_excptstruct.RegRec2));
-
-    if (g_excptstruct.arc)
-        if (G_pfnExcHookError)
-            G_pfnExcHookError(__FILE__, __LINE__, __FUNCTION__, g_excptstruct.arc);
-        else
-            DosBeep(1000, 1000);
-    g_excptstruct.ulExcpt = setjmp(g_excptstruct.RegRec2.jmpThread);
-#endif
-
-    osl_setCommandArgs(argc, argv);
+    printf("Dead code\n");
+    exit(1);
 }
 
 void SAL_CALL sal_detail_deinitialize()
 {
-    APIRET rc = -1;
+    printf("Dead code\n");
+    exit(1);
+}
 
-#if 0 // until exceptions restored OSL_DEBUG_LEVEL == 0
-    rc = DosUnsetExceptionHandler((PEXCEPTIONREGISTRATIONRECORD)&(g_excptstruct.RegRec2));
+#ifdef __cplusplus
+}   // extern "C"
 #endif
-}
-
-}
