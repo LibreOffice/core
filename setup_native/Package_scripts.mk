bdd97ec6ee5ad96fd2452ce99b3ebd28e540629a
Michael Stahl <mstahl@redhat.com>
2013-10-30 15:58:42 +0100

    setup_native: remove obsolete scripts
    - update.sh is broken by design (i#116865)
    - javaloader.sh belongs to the obsolete Java GUI installer
    - admin.pl is not used by the build and is possibly useful
    
    Change-Id: I5e4c43b09a1726075e6d5ddd85cae6109788e51e

--- setup_native/Package_scripts.mk
+++ setup_native/Package_scripts.mk
@@ -11,12 +11,9 @@ $(eval $(call gb_Package_Package,setup_native/scripts,$(SRCDIR)/setup_native/scr
 
 $(eval $(call gb_Package_set_outdir,setup_native/scripts,$(OUTDIR)))
 
-$(eval $(call gb_Package_add_file,setup_native/scripts,bin/admin.pl,admin.pl))
 $(eval $(call gb_Package_add_file,setup_native/scripts,bin/mac_install.script,mac_install.script))
 $(eval $(call gb_Package_add_file,setup_native/scripts,bin/osx_install_languagepack.applescript,osx_install_languagepack.applescript))
 $(eval $(call gb_Package_add_file,setup_native/scripts,bin/langpackscript.sh,langpackscript.sh))
-$(eval $(call gb_Package_add_file,setup_native/scripts,bin/javaloader.sh,javaloader.sh))
-$(eval $(call gb_Package_add_file,setup_native/scripts,bin/update,update.sh))
 $(eval $(call gb_Package_add_file,setup_native/scripts,bin/downloadscript.sh,downloadscript.sh))
 
 ifeq ($(ENABLE_ONLINE_UPDATE),TRUE)
