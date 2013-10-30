88d87cefb61b03909b26ea3ad89e2dc7b5538a0f
Eike Rathke <erack@redhat.com>
2013-10-30 15:49:21 +0100

    added missing HIDs instead of using copy&paste IDs of other functions
    Change-Id: I4d3cb89501f287fa2b278881352a766715ec7c6e

--- sc/inc/helpids.h
+++ sc/inc/helpids.h
@@ -678,5 +678,9 @@
 #define HID_FUNC_BITRSHIFT                                      "SC_HID_FUNC_BITRSHIFT"
 #define HID_FUNC_FILTERXML                                      "SC_HID_FUNC_FILTERXML"
 #define HID_FUNC_WEBSERVICE                                     "SC_HID_FUNC_WEBSERVICE"
+#define HID_FUNC_COVARIANCE_P                                   "SC_HID_FUNC_COVARIANCE_P"
+#define HID_FUNC_COVARIANCE_S                                   "SC_HID_FUNC_COVARIANCE_S"
+#define HID_FUNC_ST_DEV_P_MS                                    "SC_HID_FUNC_ST_DEV_P_MS"
+#define HID_FUNC_ST_DEV_S                                       "SC_HID_FUNC_ST_DEV_S"
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
