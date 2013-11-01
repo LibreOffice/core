251e5534b183a1329ddb5464f12eccc151a65bcc
Tor Lillqvist <tml@collabora.com>
2013-11-01 14:50:40 +0200

    Saving the graphics state around touch_lo_draw_tile() seems unnecessary
    Change-Id: Ic1a6cf64fb05810eccc91d0095c9ced54d3d9fe1

--- ios/shared/ios_sharedlo/objective_c/view_controllers/testing_tiles/MLOTestingTile.m
+++ ios/shared/ios_sharedlo/objective_c/view_controllers/testing_tiles/MLOTestingTile.m
@@ -45,14 +45,11 @@
 {
     CGContextRef context = UIGraphicsGetCurrentContext();
 
-    CGContextSaveGState(context);
     touch_lo_draw_tile(context,
                        self.tester.params.contextWidth,
                        self.tester.params.contextHeight,
                        MLODpxPointByDpxes(self.tester.params.tilePosX,self.tester.params.tilePosY),
                        MLODpxSizeByDpxes(self.tester.params.tileWidth,self.tester.params.tileHeight));
-
-    CGContextRestoreGState(context);
 
     MLODpxSize size = touch_lo_get_content_size();
     NSLog(@"touch_lo_get_content_size: width=%f, height=%f",size.width, size.height);
