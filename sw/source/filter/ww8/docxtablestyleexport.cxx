8d2fe858c15dd6b68e80ee70b7208a38495aae37
Nikhil Walvekar <nikhil.walvekar@synerzip.com>
2013-11-01 23:25:41 +0530

    Resolved: Preservation of para before and after auto spacing.
    During import a flag and fixed value is stored in InteropGrabBag and
    during export we check for fixed value and value available in para spacing
    object. Write autospacing only if para spacing values are not modified during
    edit.wq
    
    Change-Id: If34c5b61c3180b01e68dfe83862784c3d6f33981

--- sw/source/filter/ww8/docxtablestyleexport.cxx
+++ sw/source/filter/ww8/docxtablestyleexport.cxx
@@ -277,12 +277,14 @@ void DocxTableStyleExport::Impl::tableStylePSpacing(uno::Sequence<beans::Propert
             pAttributeList->add(FSNS(XML_w, XML_lineRule), OUStringToOString(rSpacing[i].Value.get<OUString>(), RTL_TEXTENCODING_UTF8).getStr());
         else if (rSpacing[i].Name == "beforeLines")
             pAttributeList->add(FSNS(XML_w, XML_beforeLines), OUStringToOString(rSpacing[i].Value.get<OUString>(), RTL_TEXTENCODING_UTF8).getStr());
-        else if (rSpacing[i].Name == "beforeAutospacing")
-            pAttributeList->add(FSNS(XML_w, XML_beforeAutospacing), OUStringToOString(rSpacing[i].Value.get<OUString>(), RTL_TEXTENCODING_UTF8).getStr());
+        else if (rSpacing[i].Name == "ParaTopMarginBeforeAutoSpacing")
+            // Auto spacing will be available in grab bag only if it was set to true
+            pAttributeList->add(FSNS(XML_w, XML_beforeAutospacing), "1");
         else if (rSpacing[i].Name == "afterLines")
             pAttributeList->add(FSNS(XML_w, XML_afterLines), OUStringToOString(rSpacing[i].Value.get<OUString>(), RTL_TEXTENCODING_UTF8).getStr());
-        else if (rSpacing[i].Name == "afterAutospacing")
-            pAttributeList->add(FSNS(XML_w, XML_afterAutospacing), OUStringToOString(rSpacing[i].Value.get<OUString>(), RTL_TEXTENCODING_UTF8).getStr());
+        else if (rSpacing[i].Name == "ParaBottomMarginAfterAutoSpacing")
+            // Auto spacing will be available in grab bag only if it was set to true
+            pAttributeList->add(FSNS(XML_w, XML_afterAutospacing), "1");
     }
     sax_fastparser::XFastAttributeListRef xAttributeList(pAttributeList);
     m_pSerializer->singleElementNS(XML_w, XML_spacing, xAttributeList);
