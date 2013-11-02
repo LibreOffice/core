705e9fd454e17e7e2a7b8d53bc7f260a436d53b4
David Ostrovsky <david@ostrovsky.org>
2013-11-02 11:07:01 +0100

    Add custom target to process IA2 COM idl files
    Change-Id: Id20cba53fc21eaa396c3a3d3ed8fa1eb9fdb4978

---
+++ winaccessibility/CustomTarget_ia2_idl.mk
@@ -0,0 +1,56 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_CustomTarget_CustomTarget,winaccessibility/ia2/idl))
+
+wina11y_COMIDLDIR := $(call gb_CustomTarget_get_workdir,winaccessibility/ia2/idl)
+wina11y_SOURCE := $(SRCDIR)/winaccessibility/source/UAccCOMIDL
+
+# We cannot depend on *.tlb because they only produced by IDL compiler
+# if idl contains 'library' statement.
+$(call gb_CustomTarget_get_target,winaccessibility/ia2/idl) : \
+	$(wina11y_COMIDLDIR)/Accessible2.h \
+	$(wina11y_COMIDLDIR)/AccessibleAction.h \
+	$(wina11y_COMIDLDIR)/AccessibleApplication.h \
+	$(wina11y_COMIDLDIR)/AccessibleComponent.h \
+	$(wina11y_COMIDLDIR)/AccessibleEditableText.h \
+	$(wina11y_COMIDLDIR)/AccessibleEventId.h \
+	$(wina11y_COMIDLDIR)/AccessibleHyperlink.h \
+	$(wina11y_COMIDLDIR)/AccessibleHypertext.h \
+	$(wina11y_COMIDLDIR)/AccessibleImage.h \
+	$(wina11y_COMIDLDIR)/AccessibleKeyBinding.h \
+	$(wina11y_COMIDLDIR)/AccessibleKeyStroke.h \
+	$(wina11y_COMIDLDIR)/AccessibleRelation.h \
+	$(wina11y_COMIDLDIR)/AccessibleRole.h \
+	$(wina11y_COMIDLDIR)/AccessibleStates.h \
+	$(wina11y_COMIDLDIR)/AccessibleTable.h \
+	$(wina11y_COMIDLDIR)/AccessibleText.h \
+	$(wina11y_COMIDLDIR)/AccessibleValue.h \
+	$(wina11y_COMIDLDIR)/Charset.h \
+	$(wina11y_COMIDLDIR)/IA2CommonTypes.h \
+	$(wina11y_COMIDLDIR)/IA2TypeLibrary.h \
+	$(wina11y_COMIDLDIR)/UAccCOM.h \
+	$(wina11y_COMIDLDIR)/defines.h
+
+$(wina11y_COMIDLDIR)/%.h : $(wina11y_SOURCE)/%.idl \
+		| $(wina11y_COMIDLDIR)/.dir
+	$(call gb_Output_announce,$(subst $(WORKDIR)/,,$@),$(true),IDL,1)
+	$(call gb_Helper_abbreviate_dirs, \
+	midl.exe \
+		-tlb $(wina11y_COMIDLDIR)/$*.tlb \
+		-h $(wina11y_COMIDLDIR)/$*.h \
+		-iid $(wina11y_COMIDLDIR)/$*_i.c \
+		-dlldata $(wina11y_COMIDLDIR)/dlldata.c \
+		-proxy $(wina11y_COMIDLDIR)/$*_p.c \
+		-Oicf \
+		$(SOLARINC) \
+		-I $(wina11y_SOURCE) \
+		$<)
+
+# vim:set shiftwidth=4 tabstop=4 noexpandtab:
