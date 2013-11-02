5730ff36a17fcb7fc4613d3887e7e6b5017db275
Miklos Vajna <vmiklos@collabora.co.uk>
2013-11-02 21:04:59 +0100

    writerfilter: remove unused WW8List class in doctok
    Change-Id: Ie295fda55747995f4e4a4fd877e1bc3f8c194521

--- writerfilter/Library_writerfilter.mk
+++ writerfilter/Library_writerfilter.mk
@@ -127,7 +127,6 @@ $(eval $(call gb_Library_add_exception_objects,writerfilter,\
     writerfilter/source/doctok/WW8CpAndFc \
     writerfilter/source/doctok/WW8FKPImpl \
     writerfilter/source/doctok/WW8FontTable \
-    writerfilter/source/doctok/WW8ListTable \
     writerfilter/source/doctok/WW8Picture \
     writerfilter/source/doctok/WW8PropertySetImpl \
     writerfilter/source/doctok/WW8ResourceModelImpl \
