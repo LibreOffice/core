a30856befb527a2c1d9cecfcef76fc6b761fb6b3
David Ostrovsky <david@ostrovsky.org>
2013-11-02 11:07:01 +0100

    Add custom target to process IA2 COM idl files
    Change-Id: Id20cba53fc21eaa396c3a3d3ed8fa1eb9fdb4978

---
+++ winaccessibility/Module_winaccessibility.mk
@@ -0,0 +1,22 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_Module_Module,winaccessibility))
+
+ifeq ($(OS),WNT)
+ifeq ($(COM),MSC)
+ifneq ($(ENABLE_IA2),)
+$(eval $(call gb_Module_add_targets,winaccessibility,\
+	CustomTarget_ia2_idl \
+))
+endif # ENABLE_IA2
+endif # COM=MSC
+endif # WNT
+
+# vim:set shiftwidth=4 softtabstop=4 noexpandtab:
