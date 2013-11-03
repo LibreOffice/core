fde2065b0f3266fc7ee9e1b85733e3ddcf1c9e03
Khaled Hosny <khaledhosny@eglug.org>
2013-11-03 20:19:22 +0200

    fdo#70393: move openssl to a subdir of external
    Change-Id: I10bf92b18ee5ad56f1b4fbee3e4008b35b822be4
    Reviewed-on: https://gerrit.libreoffice.org/6547
    Reviewed-by: David Tardon <dtardon@redhat.com>
    Tested-by: David Tardon <dtardon@redhat.com>

--- RepositoryModule_host.mk
+++ RepositoryModule_host.mk
@@ -83,7 +83,6 @@ $(eval $(call gb_Module_add_moduledirs,libreoffice,\
 	officecfg \
 	oovbaapi \
 	oox \
-	$(call gb_Helper_optional,OPENSSL,openssl) \
 	package \
 	padmin \
 	$(call gb_Helper_optional,POPPLER,poppler) \
