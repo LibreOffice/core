3f5a48bf8aae758f5eb2cae5fafae68e2adaec6c
David Tardon <dtardon@redhat.com>
2013-11-03 12:30:36 +0100

    fix windows build
    Change-Id: I6ee6738fe869fbcd15ec045523c250fd1811e343

--- external/libmwaw/UnpackedTarball_libmwaw.mk
+++ external/libmwaw/UnpackedTarball_libmwaw.mk
@@ -11,4 +11,10 @@ $(eval $(call gb_UnpackedTarball_UnpackedTarball,libmwaw))
 
 $(eval $(call gb_UnpackedTarball_set_tarball,libmwaw,$(MWAW_TARBALL)))
 
+$(eval $(call gb_UnpackedTarball_set_patchlevel,libmwaw,1))
+
+$(eval $(call gb_UnpackedTarball_add_patches,libmwaw,\
+	external/libmwaw/0001-std-isfinite-is-C-11.patch \
+))
+
 # vim: set noet sw=4 ts=4:
