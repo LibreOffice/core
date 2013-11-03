fa560217dbd4c15abb3f7623d99cf7017dcb3f2a
Khaled Hosny <khaledhosny@eglug.org>
2013-11-03 20:33:32 +0200

    fdo#70393: move redland to a subdir of external
    Change-Id: Ia857f943b8f2915c857ec4c9fa363760699e5206
    Reviewed-on: https://gerrit.libreoffice.org/6551
    Reviewed-by: David Tardon <dtardon@redhat.com>
    Tested-by: David Tardon <dtardon@redhat.com>

---
+++ external/redland/UnpackedTarball_redland.mk
@@ -0,0 +1,28 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_UnpackedTarball_UnpackedTarball,redland))
+
+$(eval $(call gb_UnpackedTarball_set_tarball,redland,$(REDLAND_TARBALL),,redland))
+
+# configure generated files for MSVC
+$(eval $(call gb_UnpackedTarball_add_file,redland,src/librdf.h,external/redland/redland/librdf.h))
+
+$(eval $(call gb_UnpackedTarball_add_patches,redland,\
+	external/redland/redland/redland-query-rasqal-avoid-assert.patch.1 \
+	external/redland/redland/redland-pkgconfig.patch.1 \
+	external/redland/redland/redland-freebsd.patch.1 \
+	external/redland/redland/redland-msvc.patch.1 \
+	$(if $(filter-out WNT,$(OS)),external/redland/redland/redland-bundled-soname.patch.1) \
+	$(if $(filter ANDROID,$(OS)),external/redland/redland/redland-android.patch.1) \
+	$(if $(filter WNTGCC,$(OS)$(COM)),external/redland/redland/redland-mingw.patch.1) \
+	$(if $(filter YES,$(CROSS_COMPILING)),external/redland/redland/redland-xcompile.patch.1) \
+))
+
+# vim: set noet sw=4 ts=4:
