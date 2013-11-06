b08f6ca2e1f3d9b393435a137dbd247f3cf20b6d
Andras Timar <andras.timar@collabora.com>
2013-11-06 07:58:38 +0100

    INSTDIR <> INSTROOT on OSX, and we need INSTROOT
    Change-Id: I629c9791756c7c2bf95772c9b44386bd14911bd3

--- solenv/bin/modules/installer/environment.pm
+++ solenv/bin/modules/installer/environment.pm
@@ -55,7 +55,7 @@ sub create_pathvariables
     my $platformname  = $environment->{'OUTPATH'};
     $variables{'platformname'} = $platformname;
 
-    my $instdir = $ENV{'INSTDIR'};
+    my $instdir = $ENV{'INSTROOT'};
     $variables{'instdir'} = $instdir;
 
     my $installscriptdir = $environment->{'WORKDIR'} . $installer::globals::separator . "InstallScriptTarget";
