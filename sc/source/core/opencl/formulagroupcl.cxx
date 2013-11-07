76af0bf451db99c0549f360f0fb3b6bf5d5ef07a
zhenyu yuan <zhenyuyuan@multicorewareinc.com>
2013-11-07 15:43:16 +0800

    GPU Calc: implemented COTH
    AMLOEXT-91 FIX
    
    Change-Id: I27b885734f926dc3ca8a8375efcfe281e17b2adb
    Signed-off-by: haochen <haochen@multicorewareinc.com>
    Signed-off-by: I-Jui (Ray) Sung <ray@multicorewareinc.com>

--- sc/source/core/opencl/formulagroupcl.cxx
+++ sc/source/core/opencl/formulagroupcl.cxx
@@ -1204,6 +1204,10 @@ DynamicKernelSoPArguments::DynamicKernelSoPArguments(
                 mvSubArguments.push_back(SoPHelper(ts,
                          ft->Children[i], new OpCot));
                 break;
+            case ocCotHyp:
+                mvSubArguments.push_back(SoPHelper(ts,
+                        ft->Children[i], new OpCoth));
+                break;
             case ocExternal:
                 if ( !(pChild->GetExternal().compareTo(OUString(
                     "com.sun.star.sheet.addin.Analysis.getEffect"))))
--- sc/source/core/opencl/op_math.hxx
+++ sc/source/core/opencl/op_math.hxx
@@ -175,6 +175,14 @@ public:
 
     virtual std::string BinFuncName(void) const { return "Cot"; }
 };
+class OpCoth: public Normal
+{
+public:
+    virtual void GenSlidingWindowFunction(std::stringstream &ss,
+            const std::string sSymName, SubArguments &vSubArguments);
+
+    virtual std::string BinFuncName(void) const { return "Coth"; }
+};
 }}
 
 #endif
