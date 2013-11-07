8a8c128a9ee711f701c7caa750f491324e64fe54
yangzhang <yangzhang@multicorewareinc.com>
2013-11-07 12:29:20 +0800

    GPU Calc: implemented ASINH
    AMLOEXT-143 FIX
    
    Change-Id: Ib6d408a18881252271718715de4fdbb9e6068c06
    Signed-off-by: haochen <haochen@multicorewareinc.com>
    Signed-off-by: I-Jui (Ray) Sung <ray@multicorewareinc.com>

--- sc/source/core/opencl/formulagroupcl.cxx
+++ sc/source/core/opencl/formulagroupcl.cxx
@@ -1164,6 +1164,10 @@ DynamicKernelSoPArguments::DynamicKernelSoPArguments(
                 mvSubArguments.push_back(SoPHelper(ts,
                         ft->Children[i], new OpArcSin));
                 break;
+            case ocArcSinHyp:
+                mvSubArguments.push_back(SoPHelper(ts,
+                    ft->Children[i], new OpArcSinHyp));
+                break;
             case ocExternal:
                 if ( !(pChild->GetExternal().compareTo(OUString(
                     "com.sun.star.sheet.addin.Analysis.getEffect"))))
--- sc/source/core/opencl/op_math.hxx
+++ sc/source/core/opencl/op_math.hxx
@@ -125,6 +125,13 @@ public:
     virtual std::string GetBottom(void) { return "0.0"; }
     virtual std::string BinFuncName(void) const { return "ScASin"; }
 };
+class OpArcSinHyp:public Normal{
+public:
+    virtual void GenSlidingWindowFunction(std::stringstream &ss,
+        const std::string sSymName, SubArguments &vSubArguments);
+    virtual std::string GetBottom(void) { return "0.0"; }
+    virtual std::string BinFuncName(void) const { return "ScASinH"; }
+};
 }}
 
 #endif
