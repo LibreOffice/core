LibreOffice/core

commit e13a3d566ecc34a46e037aca1b2d70e58501e422

fdo#38838 Some removal/replacement of the String/UniString with OUString
Also used the new OUString::number(...) methods.

Change-Id: I3174c43d56d1ae359901bb8a13fe0096f2c74808
Reviewed-on: https://gerrit.libreoffice.org/1766
Reviewed-by: Luboš Luňák <l.lunak@suse.cz>
Tested-by: Luboš Luňák <l.lunak@suse.cz>

basctl/source/basicide/baside2.cxx
@@ -149,25 +149,27 @@ void lcl_PrintHeader( Printer* pPrinter, sal_uInt16 nPages, sal_uInt16 nCurPage,
     pPrinter->SetLineColor( aOldLineColor );
 }
 
-void lcl_ConvertTabsToSpaces( String& rLine )
+void lcl_ConvertTabsToSpaces( OUString& rLine )
 {
-    if ( rLine.Len() )
+    if ( !rLine.isEmpty() )
     {
-        sal_uInt16 nPos = 0;
-        sal_uInt16 nMax = rLine.Len();
+        OUStringBuffer aResult( rLine );
+        sal_Int32 nPos = 0;
+        sal_Int32 nMax = aResult.getLength();
         while ( nPos < nMax )
         {
-            if ( rLine.GetChar( nPos ) == '\t' )
+            if ( aResult[nPos] == '\t' )
             {
                 // not 4 Blanks, but at 4 TabPos:
-                rtl::OUStringBuffer aBlanker;
+                OUStringBuffer aBlanker;
                 string::padToLength(aBlanker, ( 4 - ( nPos % 4 ) ), ' ');
-                rLine.Erase( nPos, 1 );
-                rLine.Insert( aBlanker.makeStringAndClear(), nPos );
-                nMax = rLine.Len();
+                aResult.remove( nPos, 1 );
+                aResult.insert( nPos, aBlanker.makeStringAndClear() );
+                nMax = aResult.getLength();
             }
             ++nPos;
         }
+        rLine = aResult.makeStringAndClear();
     }
 }
 
@@ -756,7 +758,6 @@ long ModulWindow::BasicBreakHdl( StarBASIC* pBasic )
 void ModulWindow::BasicAddWatch()
 {
     DBG_CHKTHIS( ModulWindow, 0 );
-    String aWatchStr;
     AssertValidEditEngine();
     bool bAdd = true;
     if ( !GetEditView()->HasSelection() )
@@ -903,7 +904,7 @@ sal_Int32 ModulWindow::FormatAndPrint( Printer* pPrinter, sal_Int32 nPrintPage )
     pPrinter->SetFont( aFont );
     pPrinter->SetMapMode( MAP_100TH_MM );
 
-    String aTitle( CreateQualifiedName() );
+    OUString aTitle( CreateQualifiedName() );
 
     sal_uInt16 nLineHeight = (sal_uInt16) pPrinter->GetTextHeight(); // etwas mehr.
     sal_uInt16 nParaSpace = 10;
@@ -924,12 +925,12 @@ sal_Int32 ModulWindow::FormatAndPrint( Printer* pPrinter, sal_Int32 nPrintPage )
     Point aPos( Print::nLeftMargin, Print::nTopMargin );
     for ( sal_uLong nPara = 0; nPara < nParas; nPara++ )
     {
-        String aLine( GetEditEngine()->GetText( nPara ) );
+        OUString aLine( GetEditEngine()->GetText( nPara ) );
         lcl_ConvertTabsToSpaces( aLine );
-        sal_uInt16 nLines = aLine.Len()/nCharspLine+1;
+        sal_uInt16 nLines = aLine.getLength()/nCharspLine+1;
         for ( sal_uInt16 nLine = 0; nLine < nLines; nLine++ )
         {
-            String aTmpLine( aLine, nLine*nCharspLine, nCharspLine );
+            OUString aTmpLine = aLine.copy(nLine*nCharspLine, nCharspLine );
             aPos.Y() += nLineHeight;
             if ( aPos.Y() > ( aPaperSz.Height() + Print::nTopMargin ) )
             {
@@ -1154,13 +1155,13 @@ void ModulWindow::GetState( SfxItemSet &rSet )
                 if ( pView )
                 {
                     TextSelection aSel = pView->GetSelection();
-                    String aPos( IDEResId( RID_STR_LINE ) );
-                    aPos += ' ';
-                    aPos += String::CreateFromInt32( aSel.GetEnd().GetPara()+1 );
-                    aPos += String( ", " );
-                    aPos += String( IDEResId( RID_STR_COLUMN ) );
-                    aPos += ' ';
-                    aPos += String::CreateFromInt32( aSel.GetEnd().GetIndex()+1 );
+                    OUString aPos = OUString( IDEResId( RID_STR_LINE ) ) +
+                                   " " +
+                                   OUString::number(aSel.GetEnd().GetPara()+1) +
+                                   ", " +
+                                   OUString( IDEResId( RID_STR_COLUMN ) ) +
+                                   " " +
+                                   OUString::number(aSel.GetEnd().GetIndex()+1);
                     SfxStringItem aItem( SID_BASICIDE_STAT_POS, aPos );
                     rSet.Put( aItem );
                 }

basctl/source/basicide/baside2b.cxx
@@ -106,7 +106,7 @@ OUString getTextEngineText (ExtTextEngine& rEngine)
 
 void setTextEngineText (ExtTextEngine& rEngine, OUString const& aStr)
 {
-    rEngine.SetText(String());
+    rEngine.SetText(OUString());
     OString aUTF8Str = OUStringToOString( aStr, RTL_TEXTENCODING_UTF8 );
     SvMemoryStream aMemStream( (void*)aUTF8Str.getStr(), aUTF8Str.getLength(),
         STREAM_READ | STREAM_SEEK_TO_BEGIN );
@@ -1549,20 +1549,19 @@ void StackWindow::UpdateCalls()
         SbxError eOld = SbxBase::GetError();
         aTreeListBox.SetSelectionMode( SINGLE_SELECTION );
 
-        sal_uInt16 nScope = 0;
+        sal_Int32 nScope = 0;
         SbMethod* pMethod = StarBASIC::GetActiveMethod( nScope );
         while ( pMethod )
         {
-            String aEntry( String::CreateFromInt32(nScope ));
-            if ( aEntry.Len() < 2 )
-                aEntry.Insert( ' ', 0 );
-            aEntry += OUString( ": " );
-            aEntry += pMethod->GetName();
+            OUString aEntry( OUString::number(nScope ));
+            if ( aEntry.getLength() < 2 )
+                aEntry = " " + aEntry;
+            aEntry += ": "  + pMethod->GetName();
             SbxArray* pParams = pMethod->GetParameters();
             SbxInfo* pInfo = pMethod->GetInfo();
             if ( pParams )
             {
-                aEntry += '(';
+                aEntry += "(";
                 // 0 is the sub's name...
                 for ( sal_uInt16 nParam = 1; nParam < pParams->Count(); nParam++ )
                 {
@@ -1580,11 +1579,11 @@ void StackWindow::UpdateCalls()
                             aEntry += pParam->aName;
                         }
                     }
-                    aEntry += '=';
+                    aEntry += "=";
                     SbxDataType eType = pVar->GetType();
                     if( eType & SbxARRAY )
                     {
-                        aEntry += OUString( "..." );
+                        aEntry += "..." ;
                     }
                     else if( eType != SbxOBJECT )
                     {
@@ -1592,10 +1591,10 @@ void StackWindow::UpdateCalls()
                     }
                     if ( nParam < ( pParams->Count() - 1 ) )
                     {
-                        aEntry += OUString( ", " );
+                        aEntry += ", ";
                     }
                 }
-                aEntry += ')';
+                aEntry += ")";
             }
             aTreeListBox.InsertEntry( aEntry, 0, false, LIST_APPEND );
             nScope++;
@@ -1814,23 +1813,21 @@ void WatchTreeListBox::RequestingChildren( SvTreeListEntry * pParent )
 
             // Copy data and create name
 
-            String aIndexStr = OUString( "(" );
+            OUString aIndexStr = "(";
             pChildItem->mpArrayParentItem = pItem;
             pChildItem->nDimLevel = nThisLevel;
             pChildItem->nDimCount = pItem->nDimCount;
             pChildItem->vIndices.resize(pChildItem->nDimCount);
-            sal_uInt16 j;
+            sal_Int32 j;
             for( j = 0 ; j < nParentLevel ; j++ )
             {
                 short n = pChildItem->vIndices[j] = pItem->vIndices[j];
-                aIndexStr += String::CreateFromInt32( n );
-                aIndexStr += OUString( "," );
+                aIndexStr += OUString::number( n ) + ",";
             }
             pChildItem->vIndices[nParentLevel] = sal::static_int_cast<short>( i );
-            aIndexStr += String::CreateFromInt32( i );
-            aIndexStr += OUString( ")" );
+            aIndexStr += OUString::number( i ) + ")";
 
-            String aDisplayName;
+            OUString aDisplayName;
             WatchItem* pArrayRootItem = pChildItem->GetRootItem();
             if( pArrayRootItem && pArrayRootItem->mpArrayParentItem )
                 aDisplayName = pItem->maDisplayName;
@@ -1981,7 +1978,7 @@ void implCollapseModifiedObjectEntry( SvTreeListEntry* pParent, WatchTreeListBox
 
 String implCreateTypeStringForDimArray( WatchItem* pItem, SbxDataType eType )
 {
-    String aRetStr = getBasicTypeName( eType );
+    OUString aRetStr = getBasicTypeName( eType );
 
     SbxDimArray* pArray = pItem->mpArray;
     if( !pArray )
@@ -1992,18 +1989,16 @@ String implCreateTypeStringForDimArray( WatchItem* pItem, SbxDataType eType )
         int nDims = pItem->nDimCount;
         if( nDimLevel < nDims )
         {
-            aRetStr += '(';
+            aRetStr += "(";
             for( int i = nDimLevel ; i < nDims ; i++ )
             {
                 short nMin, nMax;
                 pArray->GetDim( sal::static_int_cast<short>( i+1 ), nMin, nMax );
-                aRetStr += String::CreateFromInt32( nMin );
-                aRetStr += OUString( " to " );
-                aRetStr += String::CreateFromInt32( nMax );
+                aRetStr += OUString::number(nMin) + " to "  + OUString::number(nMax);
                 if( i < nDims - 1 )
-                    aRetStr += OUString( ", " );
+                    aRetStr += ", ";
             }
-            aRetStr += ')';
+            aRetStr += ")";
         }
     }
     return aRetStr;

basctl/source/basicide/brkdlg.cxx
@@ -82,8 +82,7 @@ BreakPointDialog::BreakPointDialog( Window* pParent, BreakPointList& rBrkPntList
     for ( size_t i = 0, n = m_aModifiedBreakPointList.size(); i < n; ++i )
     {
         BreakPoint* pBrk = m_aModifiedBreakPointList.at( i );
-        OUString aEntryStr( "# " );
-        aEntryStr += String::CreateFromInt32( pBrk->nLine );
+        OUString aEntryStr( "# " + OUString::number(pBrk->nLine) );
         aComboBox.InsertEntry( aEntryStr, COMBOBOX_APPEND );
     }
     aComboBox.SetUpdateMode(true);
@@ -111,8 +110,7 @@ BreakPointDialog::BreakPointDialog( Window* pParent, BreakPointList& rBrkPntList
 
 void BreakPointDialog::SetCurrentBreakPoint( BreakPoint* pBrk )
 {
-    String aStr( "# " );
-    aStr += String::CreateFromInt32( pBrk->nLine );
+    OUString aStr( "# " + OUString::number(pBrk->nLine) );
     aComboBox.SetText( aStr );
     UpdateFields( pBrk );
 }
@@ -191,7 +189,7 @@ IMPL_LINK( BreakPointDialog, ButtonHdl, Button *, pButton )
     else if ( pButton == &aNewButton )
     {
         // keep checkbox in mind!
-        String aText( aComboBox.GetText() );
+        OUString aText( aComboBox.GetText() );
         size_t nLine;
         bool bValid = lcl_ParseText( aText, nLine );
         if ( bValid )
@@ -200,8 +198,7 @@ IMPL_LINK( BreakPointDialog, ButtonHdl, Button *, pButton )
             pBrk->bEnabled = aCheckBox.IsChecked();
             pBrk->nStopAfter = (size_t) aNumericField.GetValue();
             m_aModifiedBreakPointList.InsertSorted( pBrk );
-            OUString aEntryStr( "# " );
-            aEntryStr += String::CreateFromInt32( pBrk->nLine );
+            OUString aEntryStr( "# " + OUString::number(pBrk->nLine) );
             aComboBox.InsertEntry( aEntryStr, COMBOBOX_APPEND );
             if (SfxDispatcher* pDispatcher = GetDispatcher())
                 pDispatcher->Execute( SID_BASICIDE_BRKPNTSCHANGED );

basctl/source/basicide/moduldl2.cxx
@@ -1566,15 +1566,14 @@ void createLibImpl( Window* pWin, const ScriptDocument& rDocument,
         return;
 
     // create library name
-    String aLibName;
-    String aLibStdName( String( "Library" ) );
+    OUString aLibName;
+    OUString aLibStdName( "Library" );
     //String aLibStdName( IDEResId( RID_STR_STDLIBNAME ) );
     bool bValid = false;
-    sal_uInt16 i = 1;
+    sal_Int32 i = 1;
     while ( !bValid )
     {
-        aLibName = aLibStdName;
-        aLibName += String::CreateFromInt32( i );
+        aLibName = aLibStdName + OUString::number( i );
         if ( !rDocument.hasLibrary( E_SCRIPTS, aLibName ) && !rDocument.hasLibrary( E_DIALOGS, aLibName ) )
             bValid = true;
         i++;
@@ -1588,7 +1587,7 @@ void createLibImpl( Window* pWin, const ScriptDocument& rDocument,
         if (aNewDlg.GetObjectName().Len())
             aLibName = aNewDlg.GetObjectName();
 
-        if ( aLibName.Len() > 30 )
+        if ( aLibName.getLength() > 30 )
         {
             ErrorBox( pWin, WB_OK | WB_DEF_OK, String( IDEResId( RID_STR_LIBNAMETOLONG ) ) ).Execute();
         }

connectivity/source/drivers/dbase/DTable.cxx
@@ -350,13 +350,13 @@ OSL_TRACE("column type: %c",aDBFColumn.db_typ);
         {
             case 'C':
                 eType = DataType::VARCHAR;
-                aTypeName = ::rtl::OUString("VARCHAR");
+                aTypeName = "VARCHAR";
                 break;
             case 'F':
-                aTypeName = ::rtl::OUString("DECIMAL");
+                aTypeName = "DECIMAL";
             case 'N':
                 if ( aDBFColumn.db_typ == 'N' )
-                    aTypeName = ::rtl::OUString("NUMERIC");
+                    aTypeName = "NUMERIC";
                 eType = DataType::DECIMAL;
 
                 // for numeric fields two characters more are written, than the precision of the column description predescribes,
@@ -366,40 +366,40 @@ OSL_TRACE("column type: %c",aDBFColumn.db_typ);
                 break;
             case 'L':
                 eType = DataType::BIT;
-                aTypeName = ::rtl::OUString("BOOLEAN");
+                aTypeName = "BOOLEAN";
                 break;
             case 'Y':
                 bIsCurrency = sal_True;
                 eType = DataType::DOUBLE;
-                aTypeName = ::rtl::OUString("DOUBLE");
+                aTypeName = "DOUBLE";
                 break;
             case 'D':
                 eType = DataType::DATE;
-                aTypeName = ::rtl::OUString("DATE");
+                aTypeName = "DATE";
                 break;
             case 'T':
                 eType = DataType::TIMESTAMP;
-                aTypeName = ::rtl::OUString("TIMESTAMP");
+                aTypeName = "TIMESTAMP";
                 break;
             case 'I':
                 eType = DataType::INTEGER;
-                aTypeName = ::rtl::OUString("INTEGER");
+                aTypeName = "INTEGER";
                 break;
             case 'M':
                 if ( bFoxPro && ( aDBFColumn.db_frei2[0] & 0x04 ) == 0x04 )
                 {
                     eType = DataType::LONGVARBINARY;
-                    aTypeName = ::rtl::OUString("LONGVARBINARY");
+                    aTypeName = "LONGVARBINARY";
                 }
                 else
                 {
-                    aTypeName = ::rtl::OUString("LONGVARCHAR");
+                    aTypeName = "LONGVARCHAR";
                     eType = DataType::LONGVARCHAR;
                 }
                 nPrecision = 2147483647;
                 break;
             case 'P':
-                aTypeName = ::rtl::OUString("LONGVARBINARY");
+                aTypeName = "LONGVARBINARY";
                 eType = DataType::LONGVARBINARY;
                 nPrecision = 2147483647;
                 break;
@@ -407,12 +407,12 @@ OSL_TRACE("column type: %c",aDBFColumn.db_typ);
             case 'B':
                 if ( m_aHeader.db_typ == VisualFoxPro || m_aHeader.db_typ == VisualFoxProAuto )
                 {
-                    aTypeName = ::rtl::OUString("DOUBLE");
+                    aTypeName = "DOUBLE";
                     eType = DataType::DOUBLE;
                 }
                 else
                 {
-                    aTypeName = ::rtl::OUString("LONGVARBINARY");
+                    aTypeName = "LONGVARBINARY";
                     eType = DataType::LONGVARBINARY;
                     nPrecision = 2147483647;
                 }
@@ -513,9 +513,9 @@ void ODbaseTable::construct()
         // nyi: Ugly for Unix and Mac!
 
             if ( m_aHeader.db_typ == FoxProMemo || VisualFoxPro == m_aHeader.db_typ || VisualFoxProAuto == m_aHeader.db_typ ) // foxpro uses another extension
-                aURL.SetExtension(rtl::OUString("fpt"));
+                aURL.SetExtension("fpt");
             else
-                aURL.SetExtension(rtl::OUString("dbt"));
+                aURL.SetExtension("dbt");
 
             // If the memo file isn't found, the data will be displayed anyhow.
             // However, updates can't be done
@@ -679,7 +679,7 @@ void ODbaseTable::refreshIndexes()
         INetURLObject aURL;
         aURL.SetURL(getEntry(m_pConnection,m_Name));
 
-        aURL.setExtension(rtl::OUString("inf"));
+        aURL.setExtension("inf");
         Config aInfFile(aURL.getFSysPath(INetURLObject::FSYS_DETECT));
         aInfFile.SetGroup(dBASE_III_GROUP);
         sal_uInt16 nKeyCnt = aInfFile.GetKeyCount();
@@ -1042,7 +1042,7 @@ sal_Bool ODbaseTable::CreateImpl()
     {
         ::rtl::OUString aIdent = m_pConnection->getContent()->getIdentifier()->getContentIdentifier();
         if ( aIdent.lastIndexOf('/') != (aIdent.getLength()-1) )
-            aIdent += ::rtl::OUString("/");
+            aIdent += "/";
         aIdent += m_Name;
         aName = aIdent.getStr();
     }
@@ -1081,7 +1081,7 @@ sal_Bool ODbaseTable::CreateImpl()
         try
         {
             Content aContent(aURL.GetMainURL(INetURLObject::NO_DECODE),Reference<XCommandEnvironment>(), comphelper::getProcessComponentContext());
-            aContent.executeCommand( rtl::OUString("delete"),bool2any( sal_True ) );
+            aContent.executeCommand( "delete",bool2any( sal_True ) );
         }
         catch(const Exception&) // an exception is thrown when no file exists
         {
@@ -1092,7 +1092,7 @@ sal_Bool ODbaseTable::CreateImpl()
     if (bMemoFile)
     {
         String aExt = aURL.getExtension();
-        aURL.setExtension(rtl::OUString("dbt"));                      // extension for memo file
+        aURL.setExtension("dbt");                      // extension for memo file
         Content aMemo1Content(aURL.GetMainURL(INetURLObject::NO_DECODE),Reference<XCommandEnvironment>(), comphelper::getProcessComponentContext());
 
         sal_Bool bMemoAlreadyExists = sal_False;
@@ -1109,7 +1109,7 @@ sal_Bool ODbaseTable::CreateImpl()
             try
             {
                 Content aMemoContent(aURL.GetMainURL(INetURLObject::NO_DECODE),Reference<XCommandEnvironment>(), comphelper::getProcessComponentContext());
-                aMemoContent.executeCommand( rtl::OUString("delete"),bool2any( sal_True ) );
+                aMemoContent.executeCommand( "delete",bool2any( sal_True ) );
             }
             catch(const Exception&)
             {
@@ -1125,7 +1125,7 @@ sal_Bool ODbaseTable::CreateImpl()
         {
             aURL.setExtension(aExt);      // kill dbf file
             Content aMemoContent(aURL.GetMainURL(INetURLObject::NO_DECODE),Reference<XCommandEnvironment>(), comphelper::getProcessComponentContext());
-            aMemoContent.executeCommand( rtl::OUString("delete"),bool2any( sal_True ) );
+            aMemoContent.executeCommand( "delete",bool2any( sal_True ) );
             return sal_False;
         }
         m_aHeader.db_typ = dBaseIIIMemo;
@@ -1437,7 +1437,7 @@ sal_Bool ODbaseTable::Drop_Static(const ::rtl::OUString& _sUrl,sal_Bool _bHasMem
     {
         if (_bHasMemoFields)
         {  // delete the memo fields
-            aURL.setExtension(rtl::OUString("dbt"));
+            aURL.setExtension("dbt");
             bDropped = ::utl::UCBContentHelper::Kill(aURL.GetMainURL(INetURLObject::NO_DECODE));
         }
 
@@ -1457,13 +1457,13 @@ sal_Bool ODbaseTable::Drop_Static(const ::rtl::OUString& _sUrl,sal_Bool _bHasMem
                 {
                 }
             }
-            aURL.setExtension(rtl::OUString("inf"));
+            aURL.setExtension("inf");
 
             // as the inf file does not necessarily exist, we aren't allowed to use UCBContentHelper::Kill
             try
             {
                 ::ucbhelper::Content aDeleteContent( aURL.GetMainURL( INetURLObject::NO_DECODE ), Reference< XCommandEnvironment >(), comphelper::getProcessComponentContext() );
-                aDeleteContent.executeCommand( ::rtl::OUString("delete"), makeAny( sal_Bool( sal_True ) ) );
+                aDeleteContent.executeCommand( "delete", makeAny( sal_Bool( sal_True ) ) );
             }
             catch(const Exception&)
             {
@@ -1929,8 +1929,8 @@ sal_Bool ODbaseTable::UpdateBuffer(OValueRefVector& rRow, OValueRefRow pOrgRow, 
                         xCol->getPropertyValue(OMetaConnection::getPropMap().getNameByIndex(PROPERTY_ID_NAME)) >>= aColName;
                         ::std::list< ::std::pair<const sal_Char* , ::rtl::OUString > > aStringToSubstitutes;
                         aStringToSubstitutes.push_back(::std::pair<const sal_Char* , ::rtl::OUString >("$columnname$", aColName));
-                        aStringToSubstitutes.push_back(::std::pair<const sal_Char* , ::rtl::OUString >("$precision$", String::CreateFromInt32(nLen)));
-                        aStringToSubstitutes.push_back(::std::pair<const sal_Char* , ::rtl::OUString >("$scale$", String::CreateFromInt32(nScale)));
+                        aStringToSubstitutes.push_back(::std::pair<const sal_Char* , OUString >("$precision$", OUString::number(nLen)));
+                        aStringToSubstitutes.push_back(::std::pair<const sal_Char* , OUString >("$scale$", OUString::number(nScale)));
                         aStringToSubstitutes.push_back(::std::pair<const sal_Char* , ::rtl::OUString >("$value$", ::rtl::OStringToOUString(aDefaultValue,RTL_TEXTENCODING_UTF8)));
 
                         const ::rtl::OUString sError( getConnection()->getResources().getResourceStringWithSubstitution(
@@ -2162,7 +2162,7 @@ void SAL_CALL ODbaseTable::alterColumnByIndex( sal_Int32 index, const Reference<
     checkDisposed(OTableDescriptor_BASE::rBHelper.bDisposed);
 
     if(index < 0 || index >= m_pColumns->getCount())
-        throw IndexOutOfBoundsException(::rtl::OUString::valueOf(index),*this);
+        throw IndexOutOfBoundsException(OUString::number(index),*this);
 
     Reference<XDataDescriptorFactory> xOldColumn;
     m_pColumns->getByIndex(index) >>= xOldColumn;
@@ -2175,7 +2175,7 @@ void ODbaseTable::alterColumn(sal_Int32 index,
 {
     RTL_LOGFILE_CONTEXT_AUTHOR( aLogger, "dbase", "Ocke.Janssen@sun.com", "ODbaseTable::alterColumn" );
     if(index < 0 || index >= m_pColumns->getCount())
-        throw IndexOutOfBoundsException(::rtl::OUString::valueOf(index),*this);
+        throw IndexOutOfBoundsException(OUString::number(index),*this);
 
     ODbaseTable* pNewTable = NULL;
     try
@@ -2327,11 +2327,11 @@ namespace
             Content aContent(aURL.GetMainURL(INetURLObject::NO_DECODE),Reference<XCommandEnvironment>(), comphelper::getProcessComponentContext());
 
             Sequence< PropertyValue > aProps( 1 );
-            aProps[0].Name      = ::rtl::OUString("Title");
+            aProps[0].Name      = "Title";
             aProps[0].Handle    = -1; // n/a
             aProps[0].Value     = makeAny( ::rtl::OUString(sNewName) );
             Sequence< Any > aValues;
-            aContent.executeCommand( rtl::OUString("setPropertyValues"),makeAny(aProps) ) >>= aValues;
+            aContent.executeCommand( "setPropertyValues",makeAny(aProps) ) >>= aValues;
             if(aValues.getLength() && aValues[0].hasValue())
                 throw Exception();
         }
@@ -2469,7 +2469,7 @@ void ODbaseTable::dropColumn(sal_Int32 _nPos)
         xHold = pNewTable = NULL;
         const ::rtl::OUString sError( getConnection()->getResources().getResourceStringWithSubstitution(
                 STR_COLUMN_NOT_DROP,
-                "$position$", ::rtl::OUString::valueOf(_nPos)
+                "$position$", OUString::number(_nPos)
              ) );
         ::dbtools::throwGenericSQLException( sError, *this );
     }
@@ -2492,7 +2492,7 @@ String ODbaseTable::createTempFile()
     RTL_LOGFILE_CONTEXT_AUTHOR( aLogger, "dbase", "Ocke.Janssen@sun.com", "ODbaseTable::createTempFile" );
     ::rtl::OUString aIdent = m_pConnection->getContent()->getIdentifier()->getContentIdentifier();
     if ( aIdent.lastIndexOf('/') != (aIdent.getLength()-1) )
-        aIdent += ::rtl::OUString("/");
+        aIdent += "/";
     String sTempName(aIdent);
     String sExt;
     sExt.AssignAscii(".");

connectivity/source/drivers/flat/ETable.cxx
@@ -115,9 +115,10 @@ void OFlatTable::fillColumns(const ::com::sun::star::lang::Locale& _aLocale)
     // read description
     const sal_Unicode cDecimalDelimiter  = pConnection->getDecimalDelimiter();
     const sal_Unicode cThousandDelimiter = pConnection->getThousandDelimiter();
-    String aColumnName;
+    OUString aColumnName;
     ::comphelper::UStringMixEqual aCase(bCase);
-    ::std::vector<String> aColumnNames,m_aTypeNames;
+    ::std::vector<OUString> aColumnNames;
+    ::std::vector<String> m_aTypeNames;
     m_aTypeNames.resize(nFieldCount);
     const sal_Int32 nMaxRowsToScan = pConnection->getMaxRowsToScan();
     sal_Int32 nRowCount = 0;
@@ -133,17 +134,15 @@ void OFlatTable::fillColumns(const ::com::sun::star::lang::Locale& _aLocale)
                 if ( bHasHeaderLine )
                 {
                     aColumnName = aHeaderLine.GetTokenSpecial(nStartPosHeaderLine,m_cFieldDelimiter,m_cStringDelimiter);
-                    if ( !aColumnName.Len() )
+                    if ( !aColumnName.getLength() )
                     {
-                        aColumnName = 'C';
-                        aColumnName += String::CreateFromInt32(i+1);
+                        aColumnName = "C" + OUString::number(i+1);
                     }
                 }
                 else
                 {
                     // no column name so ...
-                    aColumnName = 'C';
-                    aColumnName += String::CreateFromInt32(i+1);
+                    aColumnName = "C" + OUString::number(i+1);
                 }
                 aColumnNames.push_back(aColumnName);
             }
@@ -156,12 +155,12 @@ void OFlatTable::fillColumns(const ::com::sun::star::lang::Locale& _aLocale)
     for (xub_StrLen i = 0; i < nFieldCount; i++)
     {
         // check if the columname already exists
-        String aAlias(aColumnNames[i]);
+        OUString aAlias(aColumnNames[i]);
         OSQLColumns::Vector::const_iterator aFind = connectivity::find(m_aColumns->get().begin(),m_aColumns->get().end(),aAlias,aCase);
         sal_Int32 nExprCnt = 0;
         while(aFind != m_aColumns->get().end())
         {
-            (aAlias = aColumnNames[i]) += String::CreateFromInt32(++nExprCnt);
+            aAlias = aColumnNames[i] + OUString::number(++nExprCnt);
             aFind = connectivity::find(m_aColumns->get().begin(),m_aColumns->get().end(),aAlias,aCase);
         }
 
@@ -705,7 +704,7 @@ sal_Bool OFlatTable::fetchRow(OValueRefRow& _rRow,const OSQLColumns & _rCols,sal
 
                     // #99178# OJ
                     if ( DataType::DECIMAL == nType || DataType::NUMERIC == nType )
-                        *(_rRow->get())[i] = ::rtl::OUString::valueOf(nVal);
+                        *(_rRow->get())[i] = OUString::number(nVal);
                     else
                         *(_rRow->get())[i] = nVal;
                 } break;

commit 560b27613679d2a3b3ddb89c2afe481bcf2996e3

fdo#71306: fix printing from basic ide
regression from e13a3d566ecc34a46e037aca1b2d70e58501e422

Change-Id: I0d734e112038746aa48df669d4b82b644a95c220

basctl/source/basicide/baside2.cxx
@@ -910,7 +910,9 @@ sal_Int32 ModulWindow::FormatAndPrint( Printer* pPrinter, sal_Int32 nPrintPage )
         sal_uInt16 nLines = aLine.getLength()/nCharspLine+1;
         for ( sal_uInt16 nLine = 0; nLine < nLines; nLine++ )
         {
-            OUString aTmpLine = aLine.copy(nLine*nCharspLine, nCharspLine );
+            sal_Int32 nBeginIndex = nLine*nCharspLine;
+            sal_Int32 nCopyCount = std::min<sal_Int32>(nCharspLine, aLine.getLength()-nBeginIndex);
+            OUString aTmpLine = aLine.copy(nBeginIndex, nCopyCount);
             aPos.Y() += nLineHeight;
             if ( aPos.Y() > ( aPaperSz.Height() + Print::nTopMargin ) )
             {