6c006e1c661ba011aa2bb2c9fe753cab85b5b366
mingli <mingli@multicorewareinc.com>
2013-11-09 11:08:44 +0800

    GPU Calc: implemented FINV
    AMLOEXT-159 FIX
    
    Change-Id: Ib15803d87b2f108bc1a9f77eeab908b41e3eb402
    Signed-off-by: haochen <haochen@multicorewareinc.com>
    Signed-off-by: I-Jui (Ray) Sung <ray@multicorewareinc.com>

--- sc/source/core/opencl/formulagroupcl.cxx
+++ sc/source/core/opencl/formulagroupcl.cxx
@@ -1241,6 +1241,10 @@ DynamicKernelSoPArguments::DynamicKernelSoPArguments(
                 mvSubArguments.push_back(SoPHelper(ts,
                          ft->Children[i], new OpFloor));
                 break;
+            case ocFInv:
+                mvSubArguments.push_back(SoPHelper(ts,
+                         ft->Children[i], new OpFInv));
+                break;
             case ocExternal:
                 if ( !(pChild->GetExternal().compareTo(OUString(
                     "com.sun.star.sheet.addin.Analysis.getEffect"))))
