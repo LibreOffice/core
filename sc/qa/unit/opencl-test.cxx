2a3225899e8d98410d5f47c59aa4cd6fe911b9f6
tianyao <tianyao@multicorewareinc.com>
2013-11-09 12:04:30 +0800

    GPU Calc: unit test cases for CSCH
    Need open macro NO_FALLBACK_TO_SWINTERP in formulagroupcl.cxx for test
    
    AMLOEXT-106 BUG
    
    Change-Id: Ic20019c3f8f823bc9107c42d2e5f27eff6418a3a
    Signed-off-by: haochen <haochen@multicorewareinc.com>
    Signed-off-by: I-Jui (Ray) Sung <ray@multicorewareinc.com>

--- sc/qa/unit/opencl-test.cxx
+++ sc/qa/unit/opencl-test.cxx
@@ -194,6 +194,7 @@ public:
     void testStatisticalFormulaFTest();
     void testStatisticalFormulaB();
     void testStatisticalFormulaBetaDist();
+    void testMathFormulaCscH();
     CPPUNIT_TEST_SUITE(ScOpenclTest);
     CPPUNIT_TEST(testSharedFormulaXLS);
     CPPUNIT_TEST(testFinacialFormula);
@@ -319,6 +320,7 @@ public:
     CPPUNIT_TEST(testStatisticalFormulaFTest);
     CPPUNIT_TEST(testStatisticalFormulaB);
     CPPUNIT_TEST(testStatisticalFormulaBetaDist);
+    CPPUNIT_TEST(testMathFormulaCscH);
     CPPUNIT_TEST_SUITE_END();
 
 private:
@@ -2040,6 +2042,28 @@ void ScOpenclTest::testStatisticalFormulaPhi()
     CPPUNIT_ASSERT(pDocRes);
     // Check the results of formula cells in the shared formula range.
     for (SCROW i = 1; i <= 19; ++i)
+    {
+        double fLibre = pDoc->GetValue(ScAddress(1,i,0));
+        double fExcel = pDocRes->GetValue(ScAddress(1,i,0));
+        CPPUNIT_ASSERT_DOUBLES_EQUAL(fExcel, fLibre, fabs(0.0001*fExcel));
+    }
+    xDocSh->DoClose();
+    xDocShRes->DoClose();
+}
+//[AMLOEXT-106]
+void ScOpenclTest::testMathFormulaCscH()
+{
+    if (!detectOpenCLDevice())
+        return;
+    ScDocShellRef xDocSh = loadDoc("opencl/math/csch.", ODS);
+    ScDocument* pDoc = xDocSh->GetDocument();
+    CPPUNIT_ASSERT(pDoc);
+    enableOpenCL();
+    pDoc->CalcAll();
+    ScDocShellRef xDocShRes = loadDoc("opencl/math/csch.", ODS);
+    ScDocument* pDocRes = xDocShRes->GetDocument();
+    CPPUNIT_ASSERT(pDocRes);
+    for (SCROW i = 0; i <= 15; ++i)
     {
         double fLibre = pDoc->GetValue(ScAddress(1,i,0));
         double fExcel = pDocRes->GetValue(ScAddress(1,i,0));
