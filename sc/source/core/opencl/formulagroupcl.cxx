7ccd1f328b95a42f2b6d9ea43c6b8bf4aaa25233
tianyao <tianyao@multicorewareinc.com>
2013-11-09 12:25:38 +0800

    GPU Calc: implemented EXP
    AMLOEXT-165 FIX
    
    Change-Id: I73693ecab752d48f620bc0cd53e29d9a8fe16c93
    Signed-off-by: haochen <haochen@multicorewareinc.com>
    Signed-off-by: I-Jui (Ray) Sung <ray@multicorewareinc.com>

--- sc/source/core/opencl/formulagroupcl.cxx
+++ sc/source/core/opencl/formulagroupcl.cxx
@@ -1261,6 +1261,9 @@ DynamicKernelSoPArguments::DynamicKernelSoPArguments(
                 mvSubArguments.push_back(SoPHelper(ts,
                           ft->Children[i], new OpCscH));
                 break;
+            case ocExp:
+                mvSubArguments.push_back(SoPHelper(ts, ft->Children[i], new OpExp));
+                break;
             case ocExternal:
                 if ( !(pChild->GetExternal().compareTo(OUString(
                     "com.sun.star.sheet.addin.Analysis.getEffect"))))
