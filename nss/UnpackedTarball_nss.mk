063b7eb7da37cb67dfb29bb4c3768beac0f52913
Michael Stahl <mstahl@redhat.com>
2013-11-13 18:18:27 +0100

    nss: move Darwin stuff from nss.patch to nss_macosx.patch
    (cherry picked from commit 226e89180c7959795394f7f0b7d4202c31e59187)
    
    Change-Id: I5fc7cdbcdf079edb101f6a8c3851c4cf4f92e258

--- nss/UnpackedTarball_nss.mk
+++ nss/UnpackedTarball_nss.mk
@@ -15,7 +15,7 @@ $(eval $(call gb_UnpackedTarball_add_patches,nss,\
 	nss/nss.patch \
 	nss/nss.aix.patch \
 	nss/nss-3.13.5-zlib-werror.patch \
-	$(if $(filter MACOSX,$(OS)),nss/nss_macosx.patch) \
+	nss/nss_macosx.patch \
 	$(if $(filter WNTMSC,$(OS)$(COM)),nss/nss.windows.patch) \
 	$(if $(filter WNTGCC,$(OS)$(COM)),nss/nspr-4.9-build.patch.3 \
 	nss/nss-3.13.3-build.patch.3 \
