cd44cc1d2249062fb68b063a0efdf3542124dcaa
Michael Meeks <michael.meeks@collabora.com>
2013-11-14 21:00:21 +0000

    Move to MPLv2 license headers, add modelines.
    Change-Id: I895bab038eda82b80e1a223ad877a9674fe561ee

--- winaccessibility/source/UAccCOM/AccComponentBase.cxx
+++ winaccessibility/source/UAccCOM/AccComponentBase.cxx
@@ -1,23 +1,21 @@
-/**************************************************************
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
  *
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * This file incorporates work covered by the following license notice:
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- *
- *************************************************************/
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
 
 #include "stdafx.h"
 #include "AccComponentBase.h"
@@ -211,3 +209,5 @@ STDMETHODIMP CAccComponentBase::put_XInterface(long pXInterface)
 
     LEAVE_PROTECTED_BLOCK
 }
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
