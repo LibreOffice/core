1263b20f56d896706c982ad1b91d2936abf5d617
David Ostrovsky <david@ostrovsky.org>
2013-11-16 22:29:12 +0100

    Disable _WIN32_WINNT definition
    Change-Id: Ibfa5839700da5ec272c95199b09cd4265d82525d

--- winaccessibility/source/UAccCOM/stdafx.h
+++ winaccessibility/source/UAccCOM/stdafx.h
@@ -28,10 +28,10 @@
 #pragma once
 #endif // _MSC_VER > 1000
 
-#define STRICT
-#ifndef _WIN32_WINNT
-#define _WIN32_WINNT 0x0400
-#endif
+//#define STRICT
+//#ifndef _WIN32_WINNT
+//#define _WIN32_WINNT 0x0400
+//#endif
 //#define _ATL_APARTMENT_THREADED
 
 #include <atlbase.h>
