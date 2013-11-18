460d6ce526b79f4d98600d60a63f894e197b66fe
Michael Stahl <mstahl@redhat.com>
2013-11-19 00:06:50 +0100

    curl: use WNT native Schannel SSL/TLS implementation
    This should give better OS integration for things like adding CAs.
    
    Change-Id: I53fbfad402618e98d4116a0fecf0bf59e905e292

--- external/curl/UnpackedTarball_curl.mk
+++ external/curl/UnpackedTarball_curl.mk
@@ -20,6 +20,7 @@ $(eval $(call gb_UnpackedTarball_fix_end_of_line,curl,\
 $(eval $(call gb_UnpackedTarball_add_patches,curl,\
 	external/curl/curl-freebsd.patch.1 \
 	external/curl/curl-msvc.patch.1 \
+	external/curl/curl-msvc-schannel.patch.1 \
 	external/curl/curl-7.26.0_mingw.patch \
 	external/curl/curl-7.26.0_win-proxy.patch \
 ))
