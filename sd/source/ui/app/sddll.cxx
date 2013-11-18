297e316cac3118b0052aa5a9cdc2008c9aad5549
Marcos Paulo de Souza <marcos.souza.org@gmail.com>
2013-11-18 19:04:21 -0200

    Merge sddll files in sd
    Change-Id: I65de77467c929be30d8f38a4d86fa672b397aff6
    Reviewed-on: https://gerrit.libreoffice.org/6714
    Tested-by: LibreOffice gerrit bot <gerrit@libreoffice.org>
    Reviewed-by: Thorsten Behrens <thb@documentfoundation.org>
    Tested-by: Thorsten Behrens <thb@documentfoundation.org>

--- sd/source/ui/app/sddll.cxx
+++ sd/source/ui/app/sddll.cxx
@@ -17,8 +17,9 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
+#include <avmedia/mediaplayer.hxx>
+#include <avmedia/mediatoolbox.hxx>
 #include <editeng/eeitem.hxx>
-
 #include <editeng/editeng.hxx>
 #include <svx/svdobj.hxx>
 #include <unotools/moduleoptions.hxx>
@@ -28,30 +29,252 @@
 #include <vcl/svapp.hxx>
 
 #include "sddll.hxx"
+#include "app.hrc"
+#include "AnimationChildWindow.hxx"
+#include "BezierObjectBar.hxx"
+#include "diactrl.hxx"
 #include "DrawDocShell.hxx"
+#include "FactoryIds.hxx"
+#include "gluectrl.hxx"
 #include "GraphicDocShell.hxx"
+#include "GraphicObjectBar.hxx"
+#include "GraphicViewShell.hxx"
+#include "GraphicViewShellBase.hxx"
+#include "ImpressViewShellBase.hxx"
+#include "PresentationViewShell.hxx"
+#include "PresentationViewShellBase.hxx"
+#include "LayerDialogChildWindow.hxx"
+#include "MediaObjectBar.hxx"
+#include "NavigatorChildWindow.hxx"
+#include "OutlineViewShell.hxx"
+#include "OutlineViewShellBase.hxx"
+#include "PaneChildWindows.hxx"
 #include "sdresid.hxx"
 #include "sdobjfac.hxx"
 #include "cfgids.hxx"
+#include "SpellDialogChildWindow.hxx"
+#include "SlideSorterViewShell.hxx"
+#include "SlideSorterViewShellBase.hxx"
 #include "strmname.h"
 #include "SdShapeTypes.hxx"
+#include "tbx_ww.hxx"
+#include "TextObjectBar.hxx"
+#include "tmplctrl.hxx"
 
+#include <svx/svxids.hrc>
+#include <svx/bmpmask.hxx>
+#include <svx/clipboardctl.hxx>
+#include <svx/extrusioncolorcontrol.hxx>
+#include <svx/f3dchild.hxx>
+#include <svx/fillctrl.hxx>
+#include <svx/fntctl.hxx>
+#include <svx/fntszctl.hxx>
+#include <svx/fontwork.hxx>
+#include <svx/fontworkgallery.hxx>
+#include <svx/formatpaintbrushctrl.hxx>
+#include <svx/galbrws.hxx>
+#include <svx/grafctrl.hxx>
+#include <svx/hyperdlg.hxx>
+#include <svx/imapdlg.hxx>
+#include <svx/layctrl.hxx>
+#include <svx/lboxctrl.hxx>
+#include <svx/linectrl.hxx>
+#include <svx/modctrl.hxx>
+#include <svx/pszctrl.hxx>
+#include <svx/srchdlg.hxx>
+#include <svx/subtoolboxcontrol.hxx>
+#include <svx/SvxColorChildWindow.hxx>
 #include <svx/SvxShapeTypes.hxx>
+#include <svx/tbcontrl.hxx>
+#include <svx/tbxcustomshapes.hxx>
+#include <svx/verttexttbxctrl.hxx>
+#include <svx/xmlsecctrl.hxx>
+#include <svx/zoomctrl.hxx>
+#include <svx/zoomsliderctrl.hxx>
 #include <sfx2/docfilt.hxx>
 #include <sfx2/docfile.hxx>
 #include <sfx2/fcontnr.hxx>
+#include <sfx2/sidebar/SidebarChildWindow.hxx>
 #include <vcl/FilterConfigItem.hxx>
 #include <comphelper/processfactory.hxx>
 
 using namespace ::rtl;
 using namespace ::com::sun::star;
 
+namespace sd { namespace ui { namespace table {
+    extern void RegisterInterfaces( SfxModule* pMod );
+} } }
 
-/*************************************************************************
-|*
-|* Init
-|*
-\************************************************************************/
+// Register all Factorys
+void SdDLL::RegisterFactorys()
+{
+    if (SvtModuleOptions().IsImpress())
+    {
+        ::sd::ImpressViewShellBase::RegisterFactory (
+            ::sd::IMPRESS_FACTORY_ID);
+        ::sd::SlideSorterViewShellBase::RegisterFactory (
+            ::sd::SLIDE_SORTER_FACTORY_ID);
+        ::sd::OutlineViewShellBase::RegisterFactory (
+            ::sd::OUTLINE_FACTORY_ID);
+        ::sd::PresentationViewShellBase::RegisterFactory (
+            ::sd::PRESENTATION_FACTORY_ID);
+    }
+    if (SvtModuleOptions().IsDraw())
+    {
+        ::sd::GraphicViewShellBase::RegisterFactory (::sd::DRAW_FACTORY_ID);
+    }
+}
+
+// Register all Interfaces
+
+void SdDLL::RegisterInterfaces()
+{
+    // Module
+    SfxModule* pMod = SD_MOD();
+    SdModule::RegisterInterface(pMod);
+
+    // View shell base.
+    ::sd::ViewShellBase::RegisterInterface(pMod);
+
+    // DocShells
+    ::sd::DrawDocShell::RegisterInterface(pMod);
+    ::sd::GraphicDocShell::RegisterInterface(pMod);
+
+    // Impress ViewShells
+    ::sd::DrawViewShell::RegisterInterface(pMod);
+    ::sd::OutlineViewShell::RegisterInterface(pMod);
+    ::sd::PresentationViewShell::RegisterInterface(pMod);
+
+    // Draw ViewShell
+    ::sd::GraphicViewShell::RegisterInterface(pMod);
+
+    // Impress ObjectShells
+    ::sd::BezierObjectBar::RegisterInterface(pMod);
+    ::sd::TextObjectBar::RegisterInterface(pMod);
+    ::sd::GraphicObjectBar::RegisterInterface(pMod);
+
+    // Media ObjectShell
+    ::sd::MediaObjectBar::RegisterInterface(pMod);
+
+    // Table ObjectShell
+    ::sd::ui::table::RegisterInterfaces(pMod);
+
+    // View shells for the side panes.
+    ::sd::slidesorter::SlideSorterViewShell::RegisterInterface (pMod);
+}
+
+// Register all Controllers
+
+void SdDLL::RegisterControllers()
+{
+    SfxModule* pMod = SD_MOD();
+
+    // ToolBoxControls registrieren
+    SdTbxControl::RegisterControl( SID_OBJECT_ALIGN, pMod );
+    SdTbxControl::RegisterControl( SID_ZOOM_TOOLBOX, pMod );
+    SdTbxControl::RegisterControl( SID_OBJECT_CHOOSE_MODE, pMod );
+    SdTbxControl::RegisterControl( SID_POSITION, pMod );
+    SdTbxControl::RegisterControl( SID_DRAWTBX_TEXT, pMod );
+    SdTbxControl::RegisterControl( SID_DRAWTBX_RECTANGLES, pMod );
+    SdTbxControl::RegisterControl( SID_DRAWTBX_ELLIPSES, pMod );
+    SdTbxControl::RegisterControl( SID_DRAWTBX_LINES, pMod );
+    SdTbxControl::RegisterControl( SID_DRAWTBX_ARROWS, pMod );
+    SdTbxControl::RegisterControl( SID_DRAWTBX_3D_OBJECTS, pMod );
+    SdTbxControl::RegisterControl( SID_DRAWTBX_CONNECTORS, pMod );
+    SdTbxControl::RegisterControl( SID_DRAWTBX_INSERT, pMod );
+
+    SvxTbxCtlCustomShapes::RegisterControl( SID_DRAWTBX_CS_BASIC, pMod );
+    SvxTbxCtlCustomShapes::RegisterControl( SID_DRAWTBX_CS_SYMBOL, pMod );
+    SvxTbxCtlCustomShapes::RegisterControl( SID_DRAWTBX_CS_ARROW, pMod );
+    SvxTbxCtlCustomShapes::RegisterControl( SID_DRAWTBX_CS_FLOWCHART, pMod );
+    SvxTbxCtlCustomShapes::RegisterControl( SID_DRAWTBX_CS_CALLOUT, pMod );
+    SvxTbxCtlCustomShapes::RegisterControl( SID_DRAWTBX_CS_STAR, pMod );
+
+    SdTbxCtlDiaPages::RegisterControl( SID_PAGES_PER_ROW, pMod );
+    SdTbxCtlGlueEscDir::RegisterControl( SID_GLUE_ESCDIR, pMod );
+
+    ::sd::AnimationChildWindow::RegisterChildWindow(0, pMod);
+    ::sd::NavigatorChildWindow::RegisterChildWindowContext( (sal_uInt16) ::sd::DrawViewShell::GetInterfaceId(), pMod );
+    ::sd::NavigatorChildWindow::RegisterChildWindowContext( (sal_uInt16) ::sd::GraphicViewShell::GetInterfaceId(), pMod );
+    ::sd::LayerDialogChildWindow::RegisterChildWindow(0, pMod);
+
+    Svx3DChildWindow::RegisterChildWindow(0, pMod);
+    SvxFontWorkChildWindow::RegisterChildWindow(0, pMod);
+    SvxColorChildWindow::RegisterChildWindow(0, pMod, SFX_CHILDWIN_TASK);
+    SvxSearchDialogWrapper::RegisterChildWindow(0, pMod);
+    SvxBmpMaskChildWindow::RegisterChildWindow(0, pMod);
+    GalleryChildWindow::RegisterChildWindow(0, pMod);
+    SvxIMapDlgChildWindow::RegisterChildWindow(0, pMod);
+    SvxHlinkDlgWrapper::RegisterChildWindow(0, pMod);
+    ::sd::SpellDialogChildWindow::RegisterChildWindow(0, pMod);
+    ::avmedia::MediaPlayer::RegisterChildWindow(0, pMod);
+    ::sd::LeftPaneImpressChildWindow::RegisterChildWindow(0, pMod);
+    ::sd::LeftPaneDrawChildWindow::RegisterChildWindow(0, pMod);
+    ::sfx2::sidebar::SidebarChildWindow::RegisterChildWindow(0, pMod);
+
+    SvxFillToolBoxControl::RegisterControl(0, pMod);
+    SvxLineStyleToolBoxControl::RegisterControl(0, pMod);
+    SvxLineWidthToolBoxControl::RegisterControl(0, pMod);
+    SvxLineColorToolBoxControl::RegisterControl(0, pMod);
+
+    SvxLineEndToolBoxControl::RegisterControl( SID_ATTR_LINEEND_STYLE, pMod );
+
+    SvxStyleToolBoxControl::RegisterControl(0, pMod);
+    SvxFontNameToolBoxControl::RegisterControl(0, pMod);
+    SvxFontColorToolBoxControl::RegisterControl(0, pMod);
+
+    SvxGrafFilterToolBoxControl::RegisterControl( SID_GRFFILTER, pMod );
+    SvxGrafModeToolBoxControl::RegisterControl( SID_ATTR_GRAF_MODE, pMod );
+    SvxGrafRedToolBoxControl::RegisterControl( SID_ATTR_GRAF_RED, pMod );
+    SvxGrafGreenToolBoxControl::RegisterControl( SID_ATTR_GRAF_GREEN, pMod );
+    SvxGrafBlueToolBoxControl::RegisterControl( SID_ATTR_GRAF_BLUE, pMod );
+    SvxGrafLuminanceToolBoxControl::RegisterControl( SID_ATTR_GRAF_LUMINANCE, pMod );
+    SvxGrafContrastToolBoxControl::RegisterControl( SID_ATTR_GRAF_CONTRAST, pMod );
+    SvxGrafGammaToolBoxControl::RegisterControl( SID_ATTR_GRAF_GAMMA, pMod );
+    SvxGrafTransparenceToolBoxControl::RegisterControl( SID_ATTR_GRAF_TRANSPARENCE, pMod );
+    SvxVertTextTbxCtrl::RegisterControl(SID_TEXTDIRECTION_TOP_TO_BOTTOM, pMod);
+    SvxVertTextTbxCtrl::RegisterControl(SID_TEXTDIRECTION_LEFT_TO_RIGHT, pMod);
+    SvxVertTextTbxCtrl::RegisterControl(SID_DRAW_CAPTION_VERTICAL, pMod);
+    SvxVertTextTbxCtrl::RegisterControl(SID_DRAW_FONTWORK_VERTICAL, pMod);
+    SvxVertTextTbxCtrl::RegisterControl(SID_DRAW_TEXT_VERTICAL, pMod);
+    SvxVertTextTbxCtrl::RegisterControl(SID_TEXT_FITTOSIZE_VERTICAL, pMod);
+    SvxCTLTextTbxCtrl::RegisterControl(SID_ATTR_PARA_LEFT_TO_RIGHT, pMod);
+    SvxCTLTextTbxCtrl::RegisterControl(SID_ATTR_PARA_RIGHT_TO_LEFT, pMod);
+
+    // register StatusBarControls
+    SvxZoomStatusBarControl::RegisterControl( SID_ATTR_ZOOM, pMod );
+    SvxPosSizeStatusBarControl::RegisterControl( SID_ATTR_SIZE, pMod );
+    SvxModifyControl::RegisterControl( SID_DOC_MODIFIED, pMod );
+    SvxZoomSliderControl::RegisterControl( SID_ATTR_ZOOMSLIDER, pMod );
+
+    // MenuControls for PopupMenu
+    SvxFontMenuControl::RegisterControl( SID_ATTR_CHAR_FONT, pMod );
+    SvxFontSizeMenuControl::RegisterControl( SID_ATTR_CHAR_FONTHEIGHT, pMod );
+
+    SfxMenuControl::RegisterControl( SID_SET_SNAPITEM, pMod );
+    SfxMenuControl::RegisterControl( SID_DELETE_SNAPITEM, pMod );
+    SfxMenuControl::RegisterControl( SID_BEZIER_CLOSE, pMod );
+
+    // #UndoRedo#
+    SvxUndoRedoControl::RegisterControl( SID_UNDO , pMod );
+    SvxUndoRedoControl::RegisterControl( SID_REDO , pMod );
+
+    svx::FormatPaintBrushToolBoxControl::RegisterControl(SID_FORMATPAINTBRUSH, pMod );
+
+    SvxClipBoardControl::RegisterControl( SID_PASTE, pMod );
+
+    svx::ExtrusionColorControl::RegisterControl( SID_EXTRUSION_3D_COLOR, pMod );
+    svx::FontWorkShapeTypeControl::RegisterControl( SID_FONTWORK_SHAPE_TYPE, pMod );
+
+    ::avmedia::MediaToolBoxControl::RegisterControl( SID_AVMEDIA_TOOLBOX, pMod );
+    XmlSecStatusBarControl::RegisterControl( SID_SIGNATURE, pMod );
+    SdTemplateControl::RegisterControl( SID_STATUS_LAYOUT, pMod );
+    SvxTableToolBoxControl::RegisterControl(SID_INSERT_TABLE, pMod );
+    SvxFrameLineStyleToolBoxControl::RegisterControl(SID_FRAME_LINESTYLE, pMod );
+    SvxFrameLineColorToolBoxControl::RegisterControl(SID_FRAME_LINECOLOR, pMod );
+    SvxFrameToolBoxControl::RegisterControl(SID_ATTR_BORDER, pMod );
+    SvxSubToolBoxControl::RegisterControl(SID_OPTIMIZE_TABLE, pMod);
+}
 
 void SdDLL::Init()
 {
