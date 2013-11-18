f1390100e005cece4fdc93a7c760c79929153a4d
Herbert Dürr <hdu@apache.org>
2013-11-18 08:32:52 +0000

    #i107914# adjust the g_msaasvc.h license header to the ALv2
    as intended by IBM's contribution of the Symphony source code
    and the individual ICLAs of the developers

--- vcl/inc/win/g_msaasvc.h
+++ vcl/inc/win/g_msaasvc.h
@@ -1,23 +1,26 @@
-/************************************************************************
+/**************************************************************
  *
- * Licensed Materials - Property of IBM.
- * (C) Copyright IBM Corporation 2003, 2009.  All Rights Reserved.
- * U.S. Government Users Restricted Rights:
- * Use, duplication or disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- ************************************************************************/
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ *************************************************************/
 
-/*************************************************************************
- *
- *
- * (C) Copyright IBM Corp. 2003, 2005
- * The source code for this program is not published or otherwise
- * divested of its IBM trade secrets, irrespective of what has
- * been deposited with the U.S. Copyright Office.
- * All rights reserved
- *
-*************************************************************************/
 #ifndef _SV_G_MSASSVR_H_
 #define _SV_G_MSASSVR_H_
 extern com::sun::star::accessibility::XMSAAService* g_acc_manager1;
 #endif
+
