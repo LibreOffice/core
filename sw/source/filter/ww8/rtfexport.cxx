698c2829e0692f989cb9d4e6e55454d2d732f4ac
Miklos Vajna <vmiklos@collabora.co.uk>
2013-11-18 11:32:57 +0100

    cp#1000018 RTF export: avoid additional paragraph at footnote end
    (cherry picked from commit 8299924b750f74f799dc683134788a285b38bd72)
    
    Conflicts:
    	sw/source/filter/ww8/rtfattributeoutput.cxx
    
    Change-Id: I430a7d705208f197050a7d521c9c20b267c33f26

--- sw/source/filter/ww8/rtfexport.cxx
+++ sw/source/filter/ww8/rtfexport.cxx
@@ -755,8 +755,10 @@ void RtfExport::OutputLinkedOLE( const OUString& )
 
 void RtfExport::OutputTextNode( const SwTxtNode& rNode )
 {
+    m_nCurrentNodeIndex = rNode.GetIndex();
     if ( !m_bOutOutlineOnly || rNode.IsOutline( ) )
         MSWordExportBase::OutputTextNode( rNode );
+    m_nCurrentNodeIndex = 0;
 }
 
 void RtfExport::AppendSection( const SwPageDesc* pPageDesc, const SwSectionFmt* pFmt, sal_uLong nLnNum )
@@ -779,7 +781,8 @@ RtfExport::RtfExport( RtfExportFilter *pFilter, SwDoc *pDocument, SwPaM *pCurren
               rtl_getTextEncodingFromWindowsCharset(
                   sw::ms::rtl_TextEncodingToWinCharset(DEF_ENCODING))),
       eCurrentEncoding(eDefaultEncoding),
-      bRTFFlySyntax(false)
+      bRTFFlySyntax(false),
+      m_nCurrentNodeIndex(0)
 {
     mbExportModeRTF = true;
     // the attribute output for the document
