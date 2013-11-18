43d477b1685f6c1ad48885c7a73411030c78513d
Michael Stahl <mstahl@redhat.com>
2013-11-18 23:40:05 +0100

    curl: actually use Makefile.vc10 instead of Makefile.vc9
    ... they are currently identical but it seems more appropriate.
    
    Change-Id: I5ecd7209bc29f32a2f04299d35364a10fe381a35

--- external/curl/UnpackedTarball_curl.mk
+++ external/curl/UnpackedTarball_curl.mk
@@ -14,7 +14,7 @@ $(eval $(call gb_UnpackedTarball_set_tarball,curl,$(CURL_TARBALL),,curl))
 $(eval $(call gb_UnpackedTarball_set_patchlevel,curl,1))
 
 $(eval $(call gb_UnpackedTarball_fix_end_of_line,curl,\
-	lib/Makefile.vc9 \
+	lib/Makefile.vc10 \
 ))
 
 $(eval $(call gb_UnpackedTarball_add_patches,curl,\
