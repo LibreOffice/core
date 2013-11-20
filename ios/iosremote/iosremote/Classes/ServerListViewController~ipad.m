ec11f022cc1bb2857b20ff96af1f37ca49b73b00
Siqi LIU <me@siqi.fr>
2013-11-21 00:54:19 +0100

    move ServerListViewController to appropriate place
    Change-Id: I0f66efe7c306ef8110361495eee0a3f1dbde1013

---
+++ ios/iosremote/iosremote/Classes/ServerListViewController~ipad.m
@@ -0,0 +1,46 @@
+// -*- Mode: ObjC; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+//
+// This file is part of the LibreOffice project.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#import "ServerListViewController~ipad.h"
+
+@interface ServerListViewController_ipad ()
+
+@end
+
+@implementation ServerListViewController_ipad
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)viewDidUnload {
+    [super viewDidUnload];
+}
+
+#pragma mark - Actions
+- (IBAction)cancelModalView:(id)sender {
+    [self.presentingViewController dismissModalViewControllerAnimated:YES];
+}
+@end
