c037ec6acc66efdeee738f057acee7c52ab979f3
Oliver-Rainer Wittmann <orw@apache.org>
2013-11-20 13:26:34 +0000

    123578: adjust table layout algorithm to avoid layout loops
    --> no shrink of master table, when follow table current is layouting its lower table row

--- sw/source/core/inc/tabfrm.hxx
+++ sw/source/core/inc/tabfrm.hxx
@@ -33,8 +33,6 @@ class SwAttrSetChg;
 
 class SwTabFrm: public SwLayoutFrm, public SwFlowFrm
 {
-    // OD 14.03.2003 #i11760# - adjustment, because of method signature change
-    //darf mit den Flags spielen.
     friend void CalcCntnt( SwLayoutFrm *pLay, bool bNoColl, bool bNoCalcFollow );
 
     //Fuert Spezialbehandlung fuer _Get[Next|Prev]Leaf() durch.
@@ -44,22 +42,22 @@ class SwTabFrm: public SwLayoutFrm, public SwFlowFrm
     SwTable* pTable;
 
     sal_Bool bComplete          :1; //Eintrage als Repaint ohne das CompletePaint
-                                //der Basisklasse gesetzt werden muss. Damit
-                                //sollen unertraegliche Tabellen-Repaints
-                                //vermieden werden.
+                                    //der Basisklasse gesetzt werden muss. Damit
+                                    //sollen unertraegliche Tabellen-Repaints
+                                    //vermieden werden.
     sal_Bool bCalcLowers        :1; //Im MakeAll auf jedenfall auch fuer Stabilitaet
-                                //des Inhaltes sorgen.
+    //des Inhaltes sorgen.
     sal_Bool bLowersFormatted   :1;//Kommunikation zwischen MakeAll und Layact
     sal_Bool bLockBackMove      :1; //BackMove-Test hat der Master erledigt.
     sal_Bool bResizeHTMLTable   :1; //Resize des HTMLTableLayout rufen im MakeAll
-                                //Zur Optimierung, damit dies nicht im
-                                //CntntFrm::Grow gerufen werden muss, denn dann
-                                //wird es ggf. fuer jede Zelle gerufen #47483#
+    //Zur Optimierung, damit dies nicht im
+    //CntntFrm::Grow gerufen werden muss, denn dann
+    //wird es ggf. fuer jede Zelle gerufen #47483#
     sal_Bool bONECalcLowers     :1; //Primaer fuer die StarONE-SS. Beim MakeAll werden
-                                //die Cntnts auf jedenfall per Calc() formatiert.
-                                //es finden keine zusaetzlichen Invalidierungen
-                                //statt und dieser Weg kann auch kaum garantien
-                                //geben.
+    //die Cntnts auf jedenfall per Calc() formatiert.
+    //es finden keine zusaetzlichen Invalidierungen
+    //statt und dieser Weg kann auch kaum garantien
+    //geben.
 
     sal_Bool bHasFollowFlowLine :1; // Means that the first line in the follow
                                 // is indented to contain content from a broken
@@ -76,7 +74,6 @@ class SwTabFrm: public SwLayoutFrm, public SwFlowFrm
                                         // is necessary, in order to let the text
                                         // flow into the FollowFlowLine
     sal_Bool bRemoveFollowFlowLinePending :1;
-    // --> OD 2004-10-04 #i26945#
     sal_Bool bConsiderObjsForMinCellHeight :1; // Usually, the floating screen objects
                                            // are considered on the calculation
                                            // for the minimal cell height.
@@ -85,12 +82,10 @@ class SwTabFrm: public SwLayoutFrm, public SwFlowFrm
                                            // screen object for the preparation
                                            // of the re-calculation of the
                                            // last table row.
-    // <--
-    // --> OD 2004-10-15 #i26945#
     sal_Bool bObjsDoesFit :1; // For splitting table rows algorithm, this boolean
                           // indicates, if the floating screen objects fits
-    // <--
-    sal_Bool bDummy4 :1;
+
+    bool mbInRecalcLowerRow : 1;
 
     //Split() spaltet den Frm an der angegebenen Stelle, es wird ein
     //Follow erzeugt und aufgebaut und direkt hinter this gepastet.
@@ -98,8 +93,11 @@ class SwTabFrm: public SwLayoutFrm, public SwFlowFrm
     bool Split( const SwTwips nCutPos, bool bTryToSplit, bool bTableRowKeep );
     bool Join();
 
-    void _UpdateAttr( const SfxPoolItem*, const SfxPoolItem*, sal_uInt8 &,
-                      SwAttrSetChg *pa = 0, SwAttrSetChg *pb = 0 );
+    void _UpdateAttr(
+        const SfxPoolItem*,
+        const SfxPoolItem*, sal_uInt8 &,
+        SwAttrSetChg *pa = 0,
+        SwAttrSetChg *pb = 0 );
 
     virtual sal_Bool ShouldBwdMoved( SwLayoutFrm *pNewUpper, sal_Bool bHead, sal_Bool &rReformat );
 
@@ -168,7 +166,15 @@ public:
     sal_Bool IsRemoveFollowFlowLinePending() const { return bRemoveFollowFlowLinePending; }
     void SetRemoveFollowFlowLinePending( sal_Bool bNew ) { bRemoveFollowFlowLinePending = bNew; }
 
-    // --> OD 2004-10-04 #i26945#
+    bool IsInRecalcLowerRow() const
+    {
+        return mbInRecalcLowerRow;
+    }
+    void SetInRecalcLowerRow( bool bNew )
+    {
+        mbInRecalcLowerRow = bNew;
+    }
+
     sal_Bool IsConsiderObjsForMinCellHeight() const
     {
         return bConsiderObjsForMinCellHeight;
@@ -177,8 +183,7 @@ public:
     {
         bConsiderObjsForMinCellHeight = _bNewConsiderObjsForMinCellHeight;
     }
-    // <--
-    // --> OD 2004-10-04 #i26945#
+
     sal_Bool DoesObjsFit() const
     {
         return bObjsDoesFit;
@@ -187,15 +192,16 @@ public:
     {
         bObjsDoesFit = _bNewObjsDoesFit;
     }
-    // <--
 
     bool RemoveFollowFlowLine();
     //
     // End: New stuff for breaking table rows
     //
 
-    sal_Bool CalcFlyOffsets( SwTwips& rUpper, long& rLeftOffset,
-                         long& rRightOffset ) const;
+    sal_Bool CalcFlyOffsets(
+        SwTwips& rUpper,
+        long& rLeftOffset,
+        long& rRightOffset ) const;
 
     SwTwips CalcHeightOfFirstContentLine() const;
 
@@ -204,12 +210,9 @@ public:
 
     bool IsLayoutSplitAllowed() const;
 
-    // --> collapsing borders FME 2005-05-27 #i29550#
     bool IsCollapsingBorders() const;
 
-    // used for collapsing border lines:
     sal_uInt16 GetBottomLineSize() const;
-    // <-- collapsing
 
     DECL_FIXEDMEMPOOL_NEWDEL(SwTabFrm)
 };
