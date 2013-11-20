eca6b8eb8e0d15046810d8d5e0bebe99b79dba48
Xisco Fauli <anistenis@gmail.com>
2013-11-20 23:53:35 +0100

    pyweb: remove unused code
    Change-Id: I6141e314e04222544f8fe356d8cdfd3be1984840

--- wizards/com/sun/star/wizards/web/data/CGGeneralInfo.py
+++ wizards/com/sun/star/wizards/web/data/CGGeneralInfo.py
@@ -37,25 +37,3 @@ class CGGeneralInfo(ConfigGroup):
             [self.cp_Title, self.cp_Author, self.cp_Description,
                 str(self.cp_CreationDate), str(self.cp_UpdateDate),
                 self.cp_Email, self.cp_Copyright])
-
-    def getCreationDate(self):
-        if self.cp_CreationDate == 0:
-            self.cp_CreationDate = currentDate()
-
-        return self.cp_CreationDate
-
-    def getUpdateDate(self):
-        if self.cp_UpdateDate == 0:
-            self.cp_UpdateDate = currentDate()
-
-        return self.cp_UpdateDate
-
-    def setCreationDate(self, i):
-        self.cp_CreationDate = i.intValue()
-
-    def setUpdateDate(self, i):
-        self.cp_UpdateDate = i.intValue()
-
-    def currentDate(self):
-        dt = JavaTools.getDateTime(System.currentTimeMillis())
-        return dt.Day + dt.Month * 100 + dt.Year * 10000
