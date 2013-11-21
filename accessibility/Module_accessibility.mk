af0909f1b88a47db1ea0ea53cdac59daee42db0b
Michael Stahl <mstahl@redhat.com>
2013-11-21 14:53:57 +0100

    --enable-ia2 no longer disables Java access bridge
    Just install both; 448727e598ca0c35ee9cd6f2c25a847c8147943b already made
    IA2 an experimental feature with fallback to Java access bridge if
    experimental mode is disabled.
    
    Change-Id: I4ca64adbc586bd87b4d28087709f5c2e02bf7990
    (cherry picked from commit 31b91f6f8a7cacb062005d6ca2a7b5b2645a9799)

--- accessibility/Module_accessibility.mk
+++ accessibility/Module_accessibility.mk
@@ -17,7 +17,6 @@ $(eval $(call gb_Module_add_l10n_targets,accessibility,\
     AllLangResTarget_acc \
 ))
 
-ifneq ($(ENABLE_IA2),TRUE)
 ifneq ($(ENABLE_JAVA),)
 ifeq ($(OS),WNT)
 $(eval $(call gb_Module_add_targets,accessibility,\
@@ -26,7 +25,6 @@ $(eval $(call gb_Module_add_targets,accessibility,\
     Jar_uno_accessbridge \
     Library_java_uno_accessbridge \
 ))
-endif
 endif
 endif
 
