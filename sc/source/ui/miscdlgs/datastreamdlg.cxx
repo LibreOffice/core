6ce9618ef9ebe58bc6f3ced34c465bbeb5df5b23
Matúš Kukan <matus.kukan@collabora.com>
2013-11-25 12:01:57 +0100

    Allow to edit data stream from Edit-> Links... dialog.
    Change-Id: Ibb456da55f04c39ddb7b58eb71ca488cf209edb5

--- sc/source/ui/miscdlgs/datastreamdlg.cxx
+++ sc/source/ui/miscdlgs/datastreamdlg.cxx
@@ -23,6 +23,7 @@ DataStreamDlg::DataStreamDlg(ScDocShell *pDocShell, Window* pParent)
     get(m_pRBScriptData, "scriptdata");
     get(m_pRBValuesInLine, "valuesinline");
     get(m_pRBAddressValue, "addressvalue");
+    get(m_pRBDataDown, "datadown");
     get(m_pRBRangeDown, "rangedown");
     get(m_pRBNoMove, "nomove");
     get(m_pRBMaxLimit, "maxlimit");
@@ -63,6 +64,7 @@ void DataStreamDlg::UpdateEnable()
     bool bOk = !m_pCbUrl->GetURL().isEmpty();
     if (m_pRBAddressValue->IsChecked())
     {
+        m_pRBNoMove->Check();
         m_pVclFrameLimit->Hide();
         m_pVclFrameMove->Hide();
         m_pVclFrameRange->Hide();
@@ -70,15 +72,35 @@ void DataStreamDlg::UpdateEnable()
     }
     else
     {
-        m_pVclFrameLimit->Show(true);
+        m_pVclFrameLimit->Show();
         m_pVclFrameMove->Show();
         m_pVclFrameRange->Show();
         bOk = bOk && !m_pEdRange->GetText().isEmpty();
     }
     m_pBtnOk->Enable(bOk);
+    setOptimalLayoutSize();
 }
 
-void DataStreamDlg::StartStream()
+void DataStreamDlg::Init(const OUString& rURL, const OUString& rRange, const sal_Int32 nLimit,
+        const OUString& rMove, const sal_uInt32 nSettings)
+{
+    m_pEdLimit->SetText(OUString::number(nLimit));
+    m_pCbUrl->SetText(rURL);
+    if (nSettings & DataStream::SCRIPT_STREAM)
+        m_pRBScriptData->Check();
+    if (!(nSettings & DataStream::VALUES_IN_LINE))
+        m_pRBAddressValue->Check();
+    m_pEdRange->SetText(rRange);
+    if (rMove == "NO_MOVE")
+        m_pRBNoMove->Check();
+    else if (rMove == "RANGE_DOWN")
+        m_pRBRangeDown->Check();
+    else if (rMove == "MOVE_DOWN")
+        m_pRBDataDown->Check();
+    UpdateEnable();
+}
+
+void DataStreamDlg::StartStream(DataStream *pStream)
 {
     sal_Int32 nLimit = 0;
     if (m_pRBMaxLimit->IsChecked())
@@ -89,7 +111,15 @@ void DataStreamDlg::StartStream()
        nSettings |= DataStream::SCRIPT_STREAM;
     if (m_pRBValuesInLine->IsChecked())
        nSettings |= DataStream::VALUES_IN_LINE;
-    DataStream *pStream = DataStream::Set( mpDocShell,
+    if (pStream)
+    {
+        pStream->Decode(rURL, m_pEdRange->GetText(), nLimit,
+                m_pRBNoMove->IsChecked() ? OUString("NO_MOVE") : m_pRBRangeDown->IsChecked()
+                    ? OUString("RANGE_DOWN") : OUString("MOVE_DOWN"),
+                nSettings);
+        return;
+    }
+    pStream = DataStream::Set( mpDocShell,
             rURL,
             m_pEdRange->GetText(),
             nLimit,
