b9c38039f44861801ce3221f6383517a5c342f6e
Michael Meeks <michael.meeks@collabora.com>
2013-11-26 17:25:37 +0000

    fastparser: special case xmlns more sensibly.

--- sax/source/fastparser/fastparser.cxx
+++ sax/source/fastparser/fastparser.cxx
@@ -949,6 +949,10 @@ void FastSaxParser::callbackStartElement( const XML_Char* pwName, const XML_Char
         {
             assert(awAttributes[i+1]);
 
+            if( awAttributes[i][0] != 'x' ||
+                strncmp( awAttributes[i], "xmlns") != 0 )
+                continue;
+
             splitName( awAttributes[i], pPrefix, nPrefixLen, pName, nNameLen );
             if( nPrefixLen )
             {
