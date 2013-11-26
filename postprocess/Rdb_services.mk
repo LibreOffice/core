4ad3127b630ad36b0c052bdbae9d3825d77d80f3
David Ostrovsky <david@ostrovsky.org>
2013-11-26 15:05:51 +0100

    fdo#39956 Delete JAB
    JAB has been discontinued in favor of the native IA2 bridge.
    
    Change-Id: I88461f73c620507d3e97b077ea2abdd2229322e1

--- postprocess/Rdb_services.mk
+++ postprocess/Rdb_services.mk
@@ -137,12 +137,7 @@ $(eval $(call gb_Rdb_add_components,services,\
 		shell/source/win32/simplemail/smplmail \
 		shell/source/win32/syssh \
 		vcl/vcl.windows \
-		$(if $(ENABLE_IA2), \
-			winaccessibility/source/service/winaccessibility, \
-			$(if $(ENABLE_JAVA), \
-			    accessibility/bridge/org/openoffice/accessibility/java_uno_accessbridge \
-			) \
-		) \
+		winaccessibility/source/service/winaccessibility \
 	) \
 	$(if $(ENABLE_HEADLESS), \
 		vcl/vcl.headless \
