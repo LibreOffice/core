b1a660f9298f00cb97b51d77e896abece4767562
Pavel Janík <paveljanik@apache.org>
2013-11-27 11:10:10 +0000

    WaE: reorder initializations properly to prevent compiler warnings.

--- sw/source/core/layout/wsfrm.cxx
+++ sw/source/core/layout/wsfrm.cxx
@@ -88,6 +88,10 @@ using namespace ::com::sun::star;
 
 SwFrm::SwFrm( SwModify *pMod, SwFrm* pSib ) :
     SwClient( pMod ),
+    //IAccessibility2 Implementation 2009-----
+    //Solution:Add a member to identify if the acc table should dispose
+    bIfAccTableShouldDisposing( sal_False ),
+    //-----IAccessibility2 Implementation 2009
     // --> OD 2006-05-10 #i65250#
     mnFrmId( SwFrm::mnLastFrmId++ ),
     // <--
@@ -96,10 +100,6 @@ SwFrm::SwFrm( SwModify *pMod, SwFrm* pSib ) :
     pNext( 0 ),
     pPrev( 0 ),
     pDrawObjs( 0 )
-    //IAccessibility2 Implementation 2009-----
-    //Solution:Add a member to identify if the acc table should dispose
-    , bIfAccTableShouldDisposing( sal_False )
-    //-----IAccessibility2 Implementation 2009
     , bInfBody( sal_False )
     , bInfTab ( sal_False )
     , bInfFly ( sal_False )
