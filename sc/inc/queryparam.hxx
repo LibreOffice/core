LibreOffice/core

commit ebdd9c300718bce454ef56a31d5d8fb699fc1822

Revert "Revert "Removing the mixed comparison flag, which is no longer needed.""
This reverts commit 7d7036c235a92268f23cc04eab8abcb08cddeb3f.

sc/inc/queryparam.hxx
@@ -45,7 +45,6 @@ struct ScQueryParamBase
     bool            bCaseSens;
     bool            bRegExp;
     bool            bDuplicate;
-    bool            bMixedComparison;   // whether numbers are smaller than strings
 
     virtual ~ScQueryParamBase();
 

sc/source/core/data/dociter.cxx
@@ -865,10 +865,6 @@ bool ScDBQueryDataIterator::DataAccessMatrix::isValidQuery(SCROW nRow, const ScM
             }
             while (false);
         }
-        else if (mpParam->bMixedComparison)
-        {
-            // Not used at the moment.
-        }
 
         if (aResults.empty())
             // First query entry.

sc/source/core/data/table3.cxx
@@ -1442,23 +1442,6 @@ bool ScTable::ValidQuery(SCROW nRow, const ScQueryParam& rParam,
                 }
             }
         }
-        else if (rParam.bMixedComparison)
-        {
-            if (rItem.meType == ScQueryEntry::ByString &&
-                    (rEntry.eOp == SC_LESS || rEntry.eOp == SC_LESS_EQUAL) &&
-                    (pCell ? pCell->HasValueData() :
-                     HasValueData( static_cast<SCCOL>(rEntry.nField), nRow)))
-            {
-                bOk = true;
-            }
-            else if (rItem.meType != ScQueryEntry::ByString &&
-                    (rEntry.eOp == SC_GREATER || rEntry.eOp == SC_GREATER_EQUAL) &&
-                    (pCell ? pCell->HasStringData() :
-                     HasStringData( static_cast<SCCOL>(rEntry.nField), nRow)))
-            {
-                bOk = true;
-            }
-        }
 
         if (nPos == -1)
         {

sc/source/core/tool/queryparam.cxx
@@ -48,7 +48,7 @@ ScQueryParamBase::ScQueryParamBase()
 
 ScQueryParamBase::ScQueryParamBase(const ScQueryParamBase& r) :
     bHasHeader(r.bHasHeader), bByRow(r.bByRow), bInplace(r.bInplace), bCaseSens(r.bCaseSens),
-    bRegExp(r.bRegExp), bDuplicate(r.bDuplicate), bMixedComparison(r.bMixedComparison),
+    bRegExp(r.bRegExp), bDuplicate(r.bDuplicate),
     maEntries(r.maEntries)
 {
 }
@@ -226,7 +226,7 @@ void ScQueryParam::Clear()
     nCol1=nCol2 = 0;
     nRow1=nRow2 = 0;
     nTab = SCTAB_MAX;
-    bHasHeader = bCaseSens = bRegExp = bMixedComparison = false;
+    bHasHeader = bCaseSens = bRegExp = false;
     bInplace = bByRow = bDuplicate = sal_True;
 
     boost::ptr_vector<ScQueryEntry>::iterator itr = maEntries.begin(), itrEnd = maEntries.end();
@@ -260,7 +260,6 @@ ScQueryParam& ScQueryParam::operator=( const ScQueryParam& r )
     bInplace    = r.bInplace;
     bCaseSens   = r.bCaseSens;
     bRegExp     = r.bRegExp;
-    bMixedComparison = r.bMixedComparison;
     bDuplicate  = r.bDuplicate;
     bByRow      = r.bByRow;
     bDestPers   = r.bDestPers;
@@ -297,7 +296,6 @@ bool ScQueryParam::operator==( const ScQueryParam& rOther ) const
         && (bInplace    == rOther.bInplace)
         && (bCaseSens   == rOther.bCaseSens)
         && (bRegExp     == rOther.bRegExp)
-        && (bMixedComparison == rOther.bMixedComparison)
         && (bDuplicate  == rOther.bDuplicate)
         && (bDestPers   == rOther.bDestPers)
         && (nDestTab    == rOther.nDestTab)

sc/source/core/tool/doubleref.cxx
@@ -276,7 +276,6 @@ void ScDBRangeBase::fillQueryOptions(ScQueryParamBase* pParam)
     pParam->bCaseSens = false;
     pParam->bRegExp = false;
     pParam->bDuplicate = true;
-    pParam->bMixedComparison = false;
 }
 
 ScDocument* ScDBRangeBase::getDoc() const

sc/source/core/tool/interpr1.cxx
@@ -4473,7 +4473,6 @@ void ScInterpreter::ScMatch()
             rParam.nRow1       = nRow1;
             rParam.nCol2       = nCol2;
             rParam.nTab        = nTab1;
-            rParam.bMixedComparison = true;
 
             ScQueryEntry& rEntry = rParam.GetEntry(0);
             ScQueryEntry::Item& rItem = rEntry.GetQueryItem();
@@ -5805,7 +5804,6 @@ void ScInterpreter::ScLookup()
     aParam.nCol2            = bVertical ? nCol1 : nCol2;
     aParam.nRow2            = bVertical ? nRow2 : nRow1;
     aParam.bByRow           = bVertical;
-    aParam.bMixedComparison = true;
 
     rEntry.bDoQuery = true;
     rEntry.eOp = SC_LESS_EQUAL;
@@ -6026,7 +6024,6 @@ void ScInterpreter::CalculateLookup(bool HLookup)
                 rParam.nRow2       = nRow2;
                 rParam.nTab        = nTab1;
             }
-            rParam.bMixedComparison = true;
 
             ScQueryEntry& rEntry = rParam.GetEntry(0);
             rEntry.bDoQuery = true;

commit f0701470858f57a855ba57c0c2283e52953db327

resolved fdo#71589 reimplemented horizontal range lookup
Regression introduced with ebdd9c300718bce454ef56a31d5d8fb699fc1822
(first eaea417bfdf8d06df2b7f2e42c904c32ce77e871) that removed the
bMixedComparison member from ScQueryParam under the false assumption
that is was only used to emulate a legacy Excel behavior. In fact it was
also needed to do the at least horizontal range lookup in sorted mixed
data, though didn't evaluate exactly the same conditions as Excel and
defined in ODFF.

Reimplemented a similar behavior for the new code structures but this
time also checking for the additional condtion that a query ByString
does not return the last numeric result and vice versa, which previously
was missing.

Change-Id: I46061777879ba5301bfcaca2d50cf87a994f93f2

sc/inc/queryparam.hxx
@@ -42,6 +42,7 @@ struct ScQueryParamBase
     bool            bCaseSens;
     bool            bRegExp;
     bool            bDuplicate;
+    bool            mbRangeLookup;      ///< for spreadsheet functions like MATCH, LOOKUP, HLOOKUP, VLOOKUP
 
     virtual ~ScQueryParamBase();
 

sc/source/core/data/dociter.cxx
@@ -1245,6 +1245,17 @@ bool ScQueryCellIterator::FindEqualOrSortedLastInRange( SCCOL& nFoundCol,
         SCROW& nFoundRow, bool bSearchForEqualAfterMismatch,
         bool bIgnoreMismatchOnLeadingStringsP )
 {
+    // Set and automatically reset mpParam->mbRangeLookup when returning. We
+    // could use comphelper::FlagRestorationGuard, but really, that one is
+    // overengineered for this simple purpose here.
+    struct BoolResetter
+    {
+        bool& mr;
+        bool  mb;
+        BoolResetter( bool& r, bool b ) : mr(r), mb(r) { r = b; }
+        ~BoolResetter() { mr = mb; }
+    } aRangeLookupResetter( mpParam->mbRangeLookup, true);
+
     nFoundCol = MAXCOL+1;
     nFoundRow = MAXROW+1;
     SetStopOnMismatch( true ); // assume sorted keys
@@ -1253,7 +1264,22 @@ bool ScQueryCellIterator::FindEqualOrSortedLastInRange( SCCOL& nFoundCol,
     bool bRegExp = mpParam->bRegExp && mpParam->GetEntry(0).GetQueryItem().meType == ScQueryEntry::ByString;
     bool bBinary = !bRegExp && mpParam->bByRow && (mpParam->GetEntry(0).eOp ==
             SC_LESS_EQUAL || mpParam->GetEntry(0).eOp == SC_GREATER_EQUAL);
-    if (bBinary ? (BinarySearch() ? GetThis() : 0) : GetFirst())
+    bool bFound = false;
+    if (bBinary)
+    {
+        if (BinarySearch())
+        {
+            // BinarySearch() already positions correctly and only needs real
+            // query comparisons afterwards, skip the verification check below.
+            mpParam->mbRangeLookup = false;
+            bFound = GetThis();
+        }
+    }
+    else
+    {
+        bFound = GetFirst();
+    }
+    if (bFound)
     {
         // First equal entry or last smaller than (greater than) entry.
         PositionType aPosSave;
@@ -1272,9 +1298,43 @@ bool ScQueryCellIterator::FindEqualOrSortedLastInRange( SCCOL& nFoundCol,
         {
             // Step back to last in range and adjust position markers for
             // GetNumberFormat() or similar.
+            SCCOL nColDiff = nCol - nFoundCol;
             nCol = nFoundCol;
             nRow = nFoundRow;
             maCurPos = aPosSave;
+            if (mpParam->mbRangeLookup)
+            {
+                // Verify that the found entry does not only fulfill the range
+                // lookup but also the real query, i.e. not numeric was found
+                // if query is ByString and vice versa.
+                mpParam->mbRangeLookup = false;
+                // Step back the last field advance if GetNext() did one.
+                if (bAdvanceQuery && nColDiff)
+                {
+                    SCSIZE nEntries = mpParam->GetEntryCount();
+                    for (SCSIZE j=0; j < nEntries; ++j)
+                    {
+                        ScQueryEntry& rEntry = mpParam->GetEntry( j );
+                        if (rEntry.bDoQuery)
+                        {
+                            if (rEntry.nField - nColDiff >= 0)
+                                rEntry.nField -= nColDiff;
+                            else
+                            {
+                                assert(!"FindEqualOrSortedLastInRange: rEntry.nField -= nColDiff < 0");
+                            }
+                        }
+                        else
+                            break;  // for
+                    }
+                }
+                // Check it.
+                if (!GetThis())
+                {
+                    nFoundCol = MAXCOL+1;
+                    nFoundRow = MAXROW+1;
+                }
+            }
         }
     }
     if ( IsEqualConditionFulfilled() )

sc/source/core/data/table3.cxx
@@ -1513,6 +1513,41 @@ public:
 
         return std::pair<bool,bool>(bOk, bTestEqual);
     }
+
+    // To be called only if both isQueryByValue() and isQueryByString()
+    // returned false and range lookup is wanted! In range lookup comparison
+    // numbers are less than strings. Nothing else is compared.
+    std::pair<bool,bool> compareByRangeLookup(
+        const ScRefCellValue& rCell, SCCOL nCol, SCROW nRow,
+        const ScQueryEntry& rEntry, const ScQueryEntry::Item& rItem)
+    {
+        bool bTestEqual = false;
+
+        if (rItem.meType == ScQueryEntry::ByString && rEntry.eOp != SC_LESS && rEntry.eOp != SC_LESS_EQUAL)
+            return std::pair<bool,bool>(false, bTestEqual);
+
+        if (rItem.meType != ScQueryEntry::ByString && rEntry.eOp != SC_GREATER && rEntry.eOp != SC_GREATER_EQUAL)
+            return std::pair<bool,bool>(false, bTestEqual);
+
+        if (!rCell.isEmpty())
+        {
+            if (rItem.meType == ScQueryEntry::ByString)
+            {
+                if (rCell.meType == CELLTYPE_FORMULA && rCell.mpFormula->GetErrCode())
+                    // Error values are compared as string.
+                    return std::pair<bool,bool>(false, bTestEqual);
+
+                return std::pair<bool,bool>(rCell.hasNumeric(), bTestEqual);
+            }
+
+            return std::pair<bool,bool>(!rCell.hasNumeric(), bTestEqual);
+        }
+
+        if (rItem.meType == ScQueryEntry::ByString)
+            return std::pair<bool,bool>(mrTab.HasValueData(nCol, nRow), bTestEqual);
+
+        return std::pair<bool,bool>(!mrTab.HasValueData(nCol, nRow), bTestEqual);
+    }
 };
 
 }
@@ -1575,6 +1610,13 @@ bool ScTable::ValidQuery(
                 {
                     std::pair<bool,bool> aThisRes =
                         aEval.compareByString(aCell, nRow, rEntry, *itr);
+                    aRes.first |= aThisRes.first;
+                    aRes.second |= aThisRes.second;
+                }
+                else if (rParam.mbRangeLookup)
+                {
+                    std::pair<bool,bool> aThisRes =
+                        aEval.compareByRangeLookup(aCell, nCol, nRow, rEntry, *itr);
                     aRes.first |= aThisRes.first;
                     aRes.second |= aThisRes.second;
                 }

sc/source/core/tool/queryparam.cxx
@@ -64,7 +64,8 @@ ScQueryParamBase::ScQueryParamBase() :
     bInplace(true),
     bCaseSens(false),
     bRegExp(false),
-    bDuplicate(false)
+    bDuplicate(false),
+    mbRangeLookup(false)
 {
     for (size_t i = 0; i < MAXQUERY; ++i)
         maEntries.push_back(new ScQueryEntry);
@@ -72,7 +73,7 @@ ScQueryParamBase::ScQueryParamBase() :
 
 ScQueryParamBase::ScQueryParamBase(const ScQueryParamBase& r) :
     bHasHeader(r.bHasHeader), bByRow(r.bByRow), bInplace(r.bInplace), bCaseSens(r.bCaseSens),
-    bRegExp(r.bRegExp), bDuplicate(r.bDuplicate),
+    bRegExp(r.bRegExp), bDuplicate(r.bDuplicate), mbRangeLookup(r.mbRangeLookup),
     maEntries(r.maEntries)
 {
 }