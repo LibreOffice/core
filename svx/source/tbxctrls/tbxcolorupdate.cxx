LibreOffice/core

commit f59c47fc1e79050d3342924e6173b0f4b6ddaf02

[Sidebar]: Fix the vague behaviour of color fill toolbox in Draw
Currently, in 'Draw' when we make any object, say a rectangle and fill
color inside it, then in the sidebar 'Fill and Controls', the color of
the rectangle made is shown over the entire button. This is vague and
the color is to be shown such that it is only filled inside the color-
button appropriately.

Change-Id: I31502a2931364d83569ff04f9ee8429bfe2225ad
Reviewed-on: https://gerrit.libreoffice.org/5930
Reviewed-by: Caolán McNamara <caolanm@redhat.com>
Tested-by: Caolán McNamara <caolanm@redhat.com>

svx/source/tbxctrls/tbxcolorupdate.cxx
@@ -36,7 +36,7 @@ namespace svx
     /* Note:
        The initial color shown on the button is set in /core/svx/source/tbxctrls/tbxcolorupdate.cxx
        (ToolboxButtonColorUpdater::ToolboxButtonColorUpdater()) .
-       The initial color used by the button is set in /core/svx/source/tbxcntrls/tbcontrl.cxx
+       The initial color used by the button is set in /core/svx/source/tbxctrls/tbcontrl.cxx
        (SvxColorExtToolBoxControl::SvxColorExtToolBoxControl())
        and in case of writer for text(background)color also in /core/sw/source/ui/docvw/edtwin.cxx
        (SwEditWin::aTextBackColor and SwEditWin::aTextBackColor)
@@ -148,7 +148,16 @@ namespace svx
                     maUpdRect.Bottom() = maBmpSize.Height() - 3;
                 }
                 else
-                    maUpdRect = Rectangle( Point( 1, maBmpSize.Height() - 7 ), Size( maBmpSize.Width() - 2 ,6 ) );
+                {
+                    maUpdRect = Rectangle( Point( 0, 0 ), Size( maBmpSize.Width(), maBmpSize.Height() ) );
+
+                    // Now, fit the selected color inside the toolbox color-rectangle such that
+                    // the distinct boundaries of the rectangle of toolbox are also clearly visible.
+                    maUpdRect.Left() += 21;
+                    maUpdRect.Top() += 1;
+                    maUpdRect.Bottom() -= 1;
+                    maUpdRect.Right() -= 4;
+                }
 
                 pBmpAcc->DrawRect( maUpdRect );
 

commit 4dc8d604b66d8b60317c3881f29db98da5d3d1ba

Fix color preview for large icons
Fix for a regression introduced by
commit f59c47fc1e79050d3342924e6173b0f4b6ddaf02.
There is no color preview when large icon are used
on toolbars. Also a partial fix for fdo#71748.

Change-Id: I6cbf36c92b4914ff3ab3c90ac04ac90e3143b748

svx/source/tbxctrls/tbxcolorupdate.cxx
@@ -137,14 +137,14 @@ namespace svx
 
                 if( maBmpSize.Width() <= 16 )
                     maUpdRect = Rectangle( Point( 0,12 ), Size( maBmpSize.Width(), 4 ) );
-                else if(76 == maBmpSize.Width() && 12 == maBmpSize.Height())
+                else if( 12 == maBmpSize.Height() ) // Area & Line Sidebar panels
                 {
                     maUpdRect.Left() = 22;
                     maUpdRect.Top() = 2;
                     maUpdRect.Right() = 73;
                     maUpdRect.Bottom() = 9;
                 }
-                else if(maBmpSize.Width() >= (2 * maBmpSize.Height() - 2) && maBmpSize.Height() >= 16)
+                else if( 16 == maBmpSize.Height() ) // Paragraph & Cell Appearance Sidebar panels
                 {
                     maUpdRect.Left() = maBmpSize.Height() + 2;
                     maUpdRect.Top() = 2;
@@ -152,16 +152,7 @@ namespace svx
                     maUpdRect.Bottom() = maBmpSize.Height() - 3;
                 }
                 else
-                {
-                    maUpdRect = Rectangle( Point( 0, 0 ), Size( maBmpSize.Width(), maBmpSize.Height() ) );
-
-                    // Now, fit the selected color inside the toolbox color-rectangle such that
-                    // the distinct boundaries of the rectangle of toolbox are also clearly visible.
-                    maUpdRect.Left() += 21;
-                    maUpdRect.Top() += 1;
-                    maUpdRect.Bottom() -= 1;
-                    maUpdRect.Right() -= 4;
-                }
+                    maUpdRect = Rectangle( Point( 1, maBmpSize.Height() - 7 ), Size( maBmpSize.Width() - 2 ,6 ) );
 
                 pBmpAcc->DrawRect( maUpdRect );
 