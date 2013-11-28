f46fbca76017154f7feb7a4341f3030b2f126693
Caolán McNamara <caolanm@redhat.com>
2013-11-28 09:53:47 +0000

    fdo#72733 Fix crash on abi2010-1.doc example
    (cherry picked from commit 3b65cd24408bb78676dfa42cf01669970125068a)
    Signed-off-by: David Tardon <dtardon@redhat.com>
    
    Change-Id: Ib3e43e14f9223bc53f20f1241d9b8c0eaa5470ad

--- external/libebook/UnpackedTarball_libebook.mk
+++ external/libebook/UnpackedTarball_libebook.mk
@@ -11,4 +11,10 @@ $(eval $(call gb_UnpackedTarball_UnpackedTarball,libebook))
 
 $(eval $(call gb_UnpackedTarball_set_tarball,libebook,$(EBOOK_TARBALL)))
 
+$(eval $(call gb_UnpackedTarball_set_patchlevel,libebook,1))
+
+$(eval $(call gb_UnpackedTarball_add_patches,libebook,\
+    external/libebook/0001-Fix-crash-on-abi2010-1.doc-example.patch \
+))
+
 # vim: set noet sw=4 ts=4:
