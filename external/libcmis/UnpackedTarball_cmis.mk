69e8bf0e2435ee4ee38122752327638c8e49942e
Cédric Bosdonnat <cedric.bosdonnat@free.fr>
2013-12-04 11:09:45 +0100

    fdo#62531: checkout failed due to bad import of properties in libcmis
    Change-Id: Ic9477039d7a1d9649487be634052608efe0d07b7
    (cherry picked from commit e8df1838ec2d4aa52522334e94e77fae00223490)

--- external/libcmis/UnpackedTarball_cmis.mk
+++ external/libcmis/UnpackedTarball_cmis.mk
@@ -15,7 +15,8 @@ $(eval $(call gb_UnpackedTarball_set_patchlevel,cmis,0))
 
 $(eval $(call gb_UnpackedTarball_add_patches,cmis, \
 						external/libcmis/libcmis-0.4.1.patch \
-						external/libcmis/libcmis-0.4.1-empty-path.patch))
+						external/libcmis/libcmis-0.4.1-empty-path.patch \
+						external/libcmis/libcmis-0.4.1-properties-fix.patch))
 
 ifeq ($(OS)$(COM),WNTMSC)
 $(eval $(call gb_UnpackedTarball_add_patches,cmis,external/libcmis/boost-win.patch))
