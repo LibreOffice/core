d209348d849aa6e4c01ed0928186e50c466e8c67
Andrzej J.R. Hunt <andrzej.hunt@collabora.com>
2013-12-05 11:36:53 +0000

    EMF+: force canvas if EMF+ comments are used.
    Without this the drawinglayer renderer is used, whereas
    only the canvas renderer supports EMF+.
    
    Change-Id: Id8a10e400e08c1913e7d6864a51f7b73fc9be437
    (cherry picked from commit be7d87bea2611eb316b9b379aebc031179d4f794)

--- vcl/source/gdi/gdimtf.cxx
+++ vcl/source/gdi/gdimtf.cxx
@@ -2896,6 +2896,13 @@ SvStream& operator>>( SvStream& rIStm, GDIMetaFile& rGDIMetaFile )
             {
                 pAction = MetaAction::ReadMetaAction( rIStm, &aReadData );
 
+                if (pAction->GetType() == META_COMMENT_ACTION)
+                {
+                    MetaCommentAction* pCommentAct = static_cast<MetaCommentAction*>(pAction);
+                    if ( pCommentAct->GetComment() == "EMF_PLUS" )
+                        rGDIMetaFile.UseCanvas( sal_True );
+                }
+
                 if( pAction )
                     rGDIMetaFile.AddAction( pAction );
             }
