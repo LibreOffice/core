557aee6efee258d632ee4f4eed31365591ab3319
Eike Rathke <erack@redhat.com>
2013-12-09 21:04:03 +0100

    resolved fdo#72537 write <loext:fill-character>
    ... instead of <number:fill-character> but read both.
    
    (cherry picked from commit 9f87de91cee05656808a98ab1bd65ad9509ef7df)
    
    Also needed for this is:
    
    xmloff: add a generic LibreOffice ODF extension namespace
    
    XML_NAMESPACE_LO_EXT can be used for elements and attributes that are
    not yet specified by OpenDocument.
    
    (Change-Id: Id29392533d46f6592d964ce79c05ffefa4d69ebc)
    Reviewed-on: https://gerrit.libreoffice.org/5419
    Tested-by: Thorsten Behrens <tbehrens@suse.com>
    Reviewed-by: Thorsten Behrens <tbehrens@suse.com>
    (cherry picked from commit de9b067bd13ec6632b49c9683255bd3a1d7ae13e)
    
    Change-Id: Ia620fad575782f6174a9ee5fbbd8b396b21948e3
    Reviewed-on: https://gerrit.libreoffice.org/7013
    Tested-by: Michael Stahl <mstahl@redhat.com>
    Reviewed-by: Michael Stahl <mstahl@redhat.com>

--- xmloff/source/style/xmlnumfi.cxx
+++ xmloff/source/style/xmlnumfi.cxx
@@ -509,7 +509,8 @@ const SvXMLTokenMap& SvXMLNumImpData::GetStyleElemTokenMap()
         {
             //  elements in a style
             { XML_NAMESPACE_NUMBER, XML_TEXT,               XML_TOK_STYLE_TEXT              },
-            { XML_NAMESPACE_NUMBER, XML_FILL_CHARACTER,           XML_TOK_STYLE_FILL_CHARACTER   },
+            { XML_NAMESPACE_LO_EXT, XML_FILL_CHARACTER,     XML_TOK_STYLE_FILL_CHARACTER    },
+            { XML_NAMESPACE_NUMBER, XML_FILL_CHARACTER,     XML_TOK_STYLE_FILL_CHARACTER    },
             { XML_NAMESPACE_NUMBER, XML_NUMBER,             XML_TOK_STYLE_NUMBER            },
             { XML_NAMESPACE_NUMBER, XML_SCIENTIFIC_NUMBER,  XML_TOK_STYLE_SCIENTIFIC_NUMBER },
             { XML_NAMESPACE_NUMBER, XML_FRACTION,           XML_TOK_STYLE_FRACTION          },
