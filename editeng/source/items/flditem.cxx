LibreOffice/core

commit ee5fc5d25fe102c30daf7d181b8181d40b85a4f3

Removing EXTENDED_DATE. There is really no such thing.
This can be safely substituted by the normal DATE.

Change-Id: I I6b4ccfa342a8d7b638b013cdce64a0b7ff477ec0

editeng/source/items/flditem.cxx
@@ -63,7 +63,6 @@ SvxFieldData* SvxFieldData::Create(const uno::Reference<text::XTextContent>& xTe
         case text::textfield::Type::TIME:
         case text::textfield::Type::EXTENDED_TIME:
         case text::textfield::Type::DATE:
-        case text::textfield::Type::EXTENDED_DATE:
         {
             sal_Bool bIsDate = false;
             xPropSet->getPropertyValue(UNO_TC_PROP_IS_DATE) >>= bIsDate;

editeng/source/uno/unofield.cxx
@@ -146,7 +146,6 @@ const SfxItemPropertySet* ImplGetFieldItemPropertySet( sal_Int32 mnId )
 
     switch( mnId )
     {
-    case text::textfield::Type::EXTENDED_DATE:
     case text::textfield::Type::EXTENDED_TIME:
     case text::textfield::Type::DATE:
         return &aExDateTimeFieldPropertySet_Impl;
@@ -266,7 +265,6 @@ SvxUnoTextField::SvxUnoTextField( sal_Int32 nServiceId ) throw()
 
     switch( nServiceId )
     {
-    case text::textfield::Type::EXTENDED_DATE:
     case text::textfield::Type::DATE:
         mpImpl->mbBoolean2 = sal_True;
         mpImpl->mnInt32 = SVXDATEFORMAT_STDSMALL;
@@ -329,7 +327,6 @@ SvxUnoTextField::SvxUnoTextField( uno::Reference< text::XTextRange > xAnchor, co
             switch( mnServiceId )
             {
             case text::textfield::Type::DATE:
-            case text::textfield::Type::EXTENDED_DATE:
                 {
                     mpImpl->mbBoolean2 = sal_True;
                     // #i35416# for variable date field, don't use invalid "0000-00-00" date,
@@ -403,7 +400,6 @@ SvxFieldData* SvxUnoTextField::CreateFieldData() const throw()
     case text::textfield::Type::TIME:
     case text::textfield::Type::EXTENDED_TIME:
     case text::textfield::Type::DATE:
-    case text::textfield::Type::EXTENDED_DATE:
     {
         if( mpImpl->mbBoolean2 ) // IsDate?
         {
@@ -630,8 +626,6 @@ OUString SAL_CALL SvxUnoTextField::getPresentation( sal_Bool bShowCommand )
                 return OUString("Footer");
             case text::textfield::Type::PRESENTATION_DATE_TIME:
                 return OUString("DateTime");
-            case text::textfield::Type::EXTENDED_DATE:
-                return OUString("ExtDate");
             default:
                 return OUString("Unknown");
         }
@@ -878,10 +872,6 @@ uno::Sequence< OUString > SAL_CALL SvxUnoTextField::getSupportedServiceNames()
         case text::textfield::Type::PRESENTATION_DATE_TIME:
             pServices[2] = "com.sun.star.presentation.TextField.DateTime";
             pServices[3] = "com.sun.star.presentation.textfield.DateTime";
-        break;
-        case text::textfield::Type::EXTENDED_DATE:
-            pServices[2] = "com.sun.star.text.TextField.DateTime";
-            pServices[3] = "com.sun.star.text.textfield.DateTime";
         break;
         default:
             aSeq.realloc(0);

editeng/source/xml/xmltxtexp.cxx
@@ -245,7 +245,7 @@ uno::Reference< uno::XInterface > SAL_CALL SvxSimpleUnoModel::createInstance( co
             RTL_CONSTASCII_STRINGPARAM("com.sun.star.text.TextField.DateTime")))
        )
     {
-        return (::cppu::OWeakObject * )new SvxUnoTextField( text::textfield::Type::EXTENDED_DATE );
+        return (::cppu::OWeakObject * )new SvxUnoTextField( text::textfield::Type::DATE );
     }
 
     return SvxUnoTextCreateTextField( aServiceSpecifier );

offapi/com/sun/star/text/textfield/Type.idl
@@ -60,8 +60,6 @@ constants Type
     const long PRESENTATION_HEADER    = 11;
     const long PRESENTATION_FOOTER    = 12;
     const long PRESENTATION_DATE_TIME = 13;
-
-    const long EXTENDED_DATE = 14;
 };
 
 }; }; }; }; };

sc/source/ui/unoobj/fielduno.cxx
@@ -1166,7 +1166,6 @@ ScEditFieldObj::ScEditFieldObj(
             pPropSet = lcl_GetURLPropertySet();
         break;
         case text::textfield::Type::DATE:
-        case text::textfield::Type::EXTENDED_DATE:
         case text::textfield::Type::TIME:
         case text::textfield::Type::EXTENDED_TIME:
             pPropSet = getDateTimePropertySet();
@@ -1175,7 +1174,7 @@ ScEditFieldObj::ScEditFieldObj(
             pPropSet = lcl_GetHeaderFieldPropertySet();
     }
 
-    if (meType == text::textfield::Type::DATE || meType == text::textfield::Type::EXTENDED_DATE)
+    if (meType == text::textfield::Type::DATE)
         mbIsDate = true;
 }
 
@@ -1343,7 +1342,6 @@ void SAL_CALL ScEditFieldObj::setPropertyValue(
             setPropertyValueFile(aPropertyName, aValue);
         break;
         case text::textfield::Type::DATE:
-        case text::textfield::Type::EXTENDED_DATE:
         case text::textfield::Type::TIME:
         case text::textfield::Type::EXTENDED_TIME:
             setPropertyValueDateTime(aPropertyName, aValue);
@@ -1397,7 +1395,6 @@ uno::Any SAL_CALL ScEditFieldObj::getPropertyValue( const rtl::OUString& aProper
             return getPropertyValueFile(aPropertyName);
         case text::textfield::Type::DATE:
         case text::textfield::Type::TIME:
-        case text::textfield::Type::EXTENDED_DATE:
         case text::textfield::Type::EXTENDED_TIME:
             return getPropertyValueDateTime(aPropertyName);
         case text::textfield::Type::FILE:

sd/source/ui/unoidl/unomodel.cxx
@@ -907,7 +907,7 @@ uno::Reference< uno::XInterface > SAL_CALL SdXImpressDocument::createInstance( c
     if( ( 0 == aServiceSpecifier.reverseCompareToAsciiL( RTL_CONSTASCII_STRINGPARAM("com.sun.star.text.TextField.DateTime") ) ) ||
         ( 0 == aServiceSpecifier.reverseCompareToAsciiL( RTL_CONSTASCII_STRINGPARAM("com.sun.star.text.textfield.DateTime") ) ) )
     {
-        return (::cppu::OWeakObject * )new SvxUnoTextField( text::textfield::Type::EXTENDED_DATE );
+        return (::cppu::OWeakObject * )new SvxUnoTextField( text::textfield::Type::DATE );
     }
 
     if( (0 == aServiceSpecifier.reverseCompareToAsciiL( RTL_CONSTASCII_STRINGPARAM("com.sun.star.presentation.TextField.Header"))) ||

svx/source/unodraw/unomod.cxx
@@ -413,7 +413,7 @@ uno::Reference< uno::XInterface > SAL_CALL SvxUnoDrawingModel::createInstance( c
 
     if( 0 == aServiceSpecifier.reverseCompareToAsciiL( RTL_CONSTASCII_STRINGPARAM("com.sun.star.text.TextField.DateTime") ) )
     {
-        return (::cppu::OWeakObject * )new SvxUnoTextField(text::textfield::Type::EXTENDED_DATE);
+        return (::cppu::OWeakObject * )new SvxUnoTextField(text::textfield::Type::DATE);
     }
 
     uno::Reference< uno::XInterface > xRet;

commit 1235e694c21f6e3e000e24a176123c386ea91df1

fdo#70278: editeng: fix Time fields in Impress
Apparenty an uninitialized SvxTimeField is created instead of the
SvxExtTimeField that is needed to preserve the value and format.

(regression from ee5fc5d25fe102c30daf7d181b8181d40b85a4f3)

Change-Id: I36b16af0c143e8b5451a1925806756492dc2334e

editeng/source/items/flditem.cxx
@@ -78,7 +78,7 @@ SvxFieldData* SvxFieldData::Create(const uno::Reference<text::XTextContent>& xTe
                         return pData;
                     }
 
-                    if (nFieldType != text::textfield::Type::TIME && nFieldType != text::textfield::Type::DATE)
+                    if (nFieldType != text::textfield::Type::TIME)
                     {
                         util::DateTime aDateTime = xPropSet->getPropertyValue(UNO_TC_PROP_DATE_TIME).get<util::DateTime>();
                         Time aTime(aDateTime.Hours, aDateTime.Minutes, aDateTime.Seconds, aDateTime.NanoSeconds);