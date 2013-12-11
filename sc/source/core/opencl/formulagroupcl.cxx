68226cbd578a316c8190867daf76f9b6f5d435aa
dechuang <dechuang@multicorewareinc.com>
2013-12-11 16:51:05 +0800

    GPU Calc: implemented for DVARP
    AMLOEXT-343 FIX
    
    Change-Id: I8a56eeb016992b113d8b161df6256a4630249dfe
    Signed-off-by: haochen <haochen@multicorewareinc.com>
    Signed-off-by: Wei Wei <weiwei@multicorewareinc.com>
    Signed-off-by: I-Jui (Ray) Sung <ray@multicorewareinc.com>

--- sc/source/core/opencl/formulagroupcl.cxx
+++ sc/source/core/opencl/formulagroupcl.cxx
@@ -2404,6 +2404,10 @@ DynamicKernelSoPArguments::DynamicKernelSoPArguments(
                 mvSubArguments.push_back(SoPHelper(ts,
                          ft->Children[i], new OpDvar));
                  break;
+            case ocDBVarP:
+                mvSubArguments.push_back(SoPHelper(ts,
+                         ft->Children[i], new OpDvarp));
+                 break;
             case ocExternal:
                 if ( !(pChild->GetExternal().compareTo(OUString(
                     "com.sun.star.sheet.addin.Analysis.getEffect"))))
