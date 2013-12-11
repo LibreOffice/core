0f0eca2cc3da0c784a07fb56c54af4bae87746fe
dechuang <dechuang@multicorewareinc.com>
2013-12-11 16:51:05 +0800

    GPU Calc: implemented for DVARP
    AMLOEXT-343 FIX
    
    Change-Id: I8a56eeb016992b113d8b161df6256a4630249dfe
    Signed-off-by: haochen <haochen@multicorewareinc.com>
    Signed-off-by: Wei Wei <weiwei@multicorewareinc.com>
    Signed-off-by: I-Jui (Ray) Sung <ray@multicorewareinc.com>
    Signed-off-by: Wei Wei <weiwei@multicorewareinc.com>

--- sc/source/core/opencl/op_database.hxx
+++ sc/source/core/opencl/op_database.hxx
@@ -79,6 +79,13 @@ public:
     virtual std::string BinFuncName(void) const { return "Dvar"; }
 };
 
+class OpDvarp: public CheckVariables
+{
+public:
+    virtual void GenSlidingWindowFunction(std::stringstream &ss,
+             const std::string sSymName, SubArguments &vSubArguments);
+    virtual std::string BinFuncName(void) const { return "Dvarp"; }
+};
 
 
 }}
