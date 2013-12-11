d937dd2622a0e0be44d6037a595d679a45d6cbe6
dechuang <dechuang@multicorewareinc.com>
2013-12-11 15:49:50 +0800

    GPU Calc: implemented for DSTDEVP
    AMLOEXT-340 FIX
    
    Change-Id: I180595e08858b91fc84c58abfaa1a7b2b05b199b
    Signed-off-by: haochen <haochen@multicorewareinc.com>
    Signed-off-by: Wei Wei <weiwei@multicorewareinc.com>
    Signed-off-by: I-Jui (Ray) Sung <ray@multicorewareinc.com>

--- sc/source/core/opencl/formulagroupcl.cxx
+++ sc/source/core/opencl/formulagroupcl.cxx
@@ -2392,6 +2392,10 @@ DynamicKernelSoPArguments::DynamicKernelSoPArguments(
                 mvSubArguments.push_back(SoPHelper(ts,
                          ft->Children[i], new OpDstdev));
                  break;
+            case ocDBStdDevP:
+                mvSubArguments.push_back(SoPHelper(ts,
+                         ft->Children[i], new OpDstdevp));
+                 break;
             case ocExternal:
                 if ( !(pChild->GetExternal().compareTo(OUString(
                     "com.sun.star.sheet.addin.Analysis.getEffect"))))
