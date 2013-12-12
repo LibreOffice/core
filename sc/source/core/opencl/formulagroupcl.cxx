867e7fc2274f942fcc83dc1e4a6846ef23a444b4
dechuang <dechuang@multicorewareinc.com>
2013-12-12 10:30:17 +0800

    GPU Calc: implemented for DCOUNTA
    AMLOEXT-334 FIX
    
    Change-Id: I9bc1f54f064cf5382e76d5826f6aa048208ebf86
    Signed-off-by: haochen <haochen@multicorewareinc.com>
    Signed-off-by: Wei Wei <weiwei@multicorewareinc.com>
    Signed-off-by: I-Jui (Ray) Sung <ray@multicorewareinc.com>

--- sc/source/core/opencl/formulagroupcl.cxx
+++ sc/source/core/opencl/formulagroupcl.cxx
@@ -2416,6 +2416,10 @@ DynamicKernelSoPArguments::DynamicKernelSoPArguments(
                 mvSubArguments.push_back(SoPHelper(ts,
                          ft->Children[i], new OpDcount));
                  break;
+            case ocDBCount2:
+                mvSubArguments.push_back(SoPHelper(ts,
+                         ft->Children[i], new OpDcount2));
+                 break;
             case ocExternal:
                 if ( !(pChild->GetExternal().compareTo(OUString(
                     "com.sun.star.sheet.addin.Analysis.getEffect"))))
--- sc/source/core/opencl/op_database.hxx
+++ sc/source/core/opencl/op_database.hxx
@@ -95,6 +95,15 @@ public:
     virtual std::string BinFuncName(void) const { return "Dcount"; }
 };
 
+class OpDcount2: public CheckVariables
+{
+public:
+    virtual void GenSlidingWindowFunction(std::stringstream &ss,
+             const std::string sSymName, SubArguments &vSubArguments);
+    virtual std::string BinFuncName(void) const { return "Dcount2"; }
+};
+
+
 }}
 
 #endif
