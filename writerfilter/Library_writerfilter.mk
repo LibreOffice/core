641dac98cddc63304e374a43c299d62b9dff52c9
Miklos Vajna <vmiklos@collabora.co.uk>
2013-12-13 22:05:57 +0100

    writerfilter: remove unused WW8CpAndFc
    Change-Id: I3e56a41c6fe5fab3e16d8e09a5404d83f5477738

--- writerfilter/Library_writerfilter.mk
+++ writerfilter/Library_writerfilter.mk
@@ -122,7 +122,6 @@ $(eval $(call gb_Library_add_exception_objects,writerfilter,\
     writerfilter/source/dmapper/TblStylePrHandler \
     writerfilter/source/dmapper/ThemeTable \
     writerfilter/source/dmapper/WrapPolygonHandler \
-    writerfilter/source/doctok/WW8CpAndFc \
     writerfilter/source/filter/ImportFilter \
     writerfilter/source/filter/RtfFilter \
     writerfilter/source/filter/WriterFilter \
