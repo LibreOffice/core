8bdde72ce452222468278f7c7f8532dca2335284
yiming ju <yiming@multicorewareinc.com>
2013-12-13 14:49:32 +0800

    GPU Calc: unit test cases for ISODD
    Turn  NO_FALLBACK_TO_SWINTERP on in  formulagroupcl.cxx for test
    
    AMLOEXT-361 BUG
    
    Change-Id: Ia4a0603cf913c4e211b1b4e2eef255c4a4c8ffaa
    Signed-off-by: haochen <haochen@multicorewareinc.com>
    Signed-off-by: Wei Wei <weiwei@multicorewareinc.com>
    Signed-off-by: I-Jui (Ray) Sung <ray@multicorewareinc.com>

--- sc/qa/unit/opencl-test.cxx
+++ sc/qa/unit/opencl-test.cxx
@@ -267,6 +267,7 @@ public:
     void testMathFormulaRadians();
     void testMathFormulaCountIf();
     void testMathFormulaIsEven();
+    void testMathFormulaIsOdd();
     CPPUNIT_TEST_SUITE(ScOpenclTest);
     CPPUNIT_TEST(testSharedFormulaXLS);
     CPPUNIT_TEST(testFinacialFormula);
@@ -465,6 +466,7 @@ public:
     CPPUNIT_TEST(testMathFormulaRadians);
     CPPUNIT_TEST(testMathFormulaCountIf);
     CPPUNIT_TEST(testMathFormulaIsEven);
+    CPPUNIT_TEST(testMathFormulaIsOdd);
     CPPUNIT_TEST_SUITE_END();
 
 private:
@@ -5458,7 +5460,29 @@ void ScOpenclTest::testMathFormulaCountIf()
     xDocSh->DoClose();
     xDocShRes->DoClose();
 }
-
+//[ AMLOEXT-361]
+void ScOpenclTest::testMathFormulaIsOdd()
+{
+    if (!detectOpenCLDevice())
+        return;
+    ScDocShellRef xDocSh = loadDoc("opencl/math/isodd.", XLS);
+    ScDocument* pDoc = xDocSh->GetDocument();
+    CPPUNIT_ASSERT(pDoc);
+    enableOpenCL();
+    pDoc->CalcAll();
+    ScDocShellRef xDocShRes = loadDoc("opencl/math/isodd.", XLS);
+    ScDocument* pDocRes = xDocShRes->GetDocument();
+    CPPUNIT_ASSERT(pDocRes);
+    // Check the results of formula cells in the shared formula range.
+    for (SCROW i = 1; i <= 9; ++i)
+    {
+        double fLibre = pDoc->GetValue(ScAddress(1,i,0));
+        double fExcel = pDocRes->GetValue(ScAddress(1,i,0));
+        CPPUNIT_ASSERT_DOUBLES_EQUAL(fExcel, fLibre, fabs(0.0001*fExcel));
+    }
+    xDocSh->DoClose();
+    xDocShRes->DoClose();
+}
 ScOpenclTest::ScOpenclTest()
       : ScBootstrapFixture( "/sc/qa/unit/data" )
 {
