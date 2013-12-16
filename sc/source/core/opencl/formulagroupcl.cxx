07566812ebf152dfc760091ab391bc89c993c4d7
shiming zhang <shiming@multicorewareinc.com>
2013-12-16 11:43:55 +0800

    GPU Calc: implemented STDEVA
    AMLOEXT-220 FIX
    
    Change-Id: I93bf659a31835c8e4ecaf63f473ad2ffc22a7566
    Signed-off-by: haochen <haochen@multicorewareinc.com>
    Signed-off-by: Wei Wei <weiwei@multicorewareinc.com>
    Signed-off-by: I-Jui (Ray) Sung <ray@multicorewareinc.com>

--- sc/source/core/opencl/formulagroupcl.cxx
+++ sc/source/core/opencl/formulagroupcl.cxx
@@ -2569,6 +2569,10 @@ DynamicKernelSoPArguments::DynamicKernelSoPArguments(
                 mvSubArguments.push_back(SoPHelper(ts,
                          ft->Children[i], new OpVarPA));
                  break;
+            case ocStDevA:
+                mvSubArguments.push_back(SoPHelper(ts,
+                         ft->Children[i], new OpStDevA));
+                 break;
             case ocExternal:
                 if ( !(pChild->GetExternal().compareTo(OUString(
                     "com.sun.star.sheet.addin.Analysis.getEffect"))))
--- sc/source/core/opencl/op_statistical.hxx
+++ sc/source/core/opencl/op_statistical.hxx
@@ -514,6 +514,15 @@ public:
     virtual bool takeString() const { return true; }
     virtual bool takeNumeric() const { return true; }
 };
+class OpStDevA: public Normal
+{
+public:
+    virtual void GenSlidingWindowFunction(std::stringstream &ss,
+            const std::string sSymName, SubArguments &vSubArguments);
+    virtual std::string BinFuncName(void) const { return "OpStDevA"; }
+    virtual bool takeString() const { return true; }
+    virtual bool takeNumeric() const { return true; }
+};
 }}
 
 #endif
