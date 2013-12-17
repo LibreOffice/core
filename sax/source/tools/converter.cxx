71448690d7c5904df45bf98243c5bb05a99245e5
Stephan Bergmann <sbergman@redhat.com>
2013-12-17 17:20:42 +0100

    readUnsignedNumberMaxDigits can read more than maxDigits chars
    ...so that is what the std::min was good for that the previous commit erroneously removed.
    
    Change-Id: I0cb08ab79f85ce4b919232845994c9b8bae35646

--- sax/source/tools/converter.cxx
+++ sax/source/tools/converter.cxx
@@ -1123,8 +1123,8 @@ bool Converter::convertDuration(util::Duration& rDuration,
                         if (-1 != nTemp)
                         {
                             nNanoSeconds = nTemp;
-                            sal_Int32 nDigits = nPos - nStart;
-                            assert(nDigits >= 0 && nDigits <= 9);
+                            sal_Int32 nDigits = std::min<sal_Int32>(nPos - nStart, 9);
+                            assert(nDigits >= 0);
                             for (; nDigits < 9; ++nDigits)
                             {
                                 nNanoSeconds *= 10;
