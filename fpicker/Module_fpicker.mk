fc985c30048d410ab68a55af64f56df85547a6bf
Caolán McNamara <caolanm@redhat.com>
2013-12-17 09:16:59 +0000

    should be a l10n target
    Change-Id: If6f7540ba59dc518f258b3b322299432f9b34a7d

--- fpicker/Module_fpicker.mk
+++ fpicker/Module_fpicker.mk
@@ -12,11 +12,11 @@ $(eval $(call gb_Module_Module,fpicker))
 
 $(eval $(call gb_Module_add_targets,fpicker,\
 	Library_fps_office \
-	UIConfig_fps \
 ))
 
 $(eval $(call gb_Module_add_l10n_targets,fpicker,\
 	AllLangResTarget_fps_office \
+	UIConfig_fps \
 ))
 
 ifeq ($(OS),MACOSX)
