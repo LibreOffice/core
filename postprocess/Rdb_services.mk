2806f4960217a7a67c5b19d2b3a9ebe1f11e889e
Isamu Mogi <saturday6c@gmail.com>
2013-12-18 20:52:45 +0900

    --disable-atl disables winaccessibility
    Change-Id: I548ea294e6c632b8ae95e6745a51d9b12c7e5297

--- postprocess/Rdb_services.mk
+++ postprocess/Rdb_services.mk
@@ -135,7 +135,6 @@ $(eval $(call gb_Rdb_add_components,services,\
 		shell/source/win32/simplemail/smplmail \
 		shell/source/win32/syssh \
 		vcl/vcl.windows \
-		winaccessibility/source/service/winaccessibility \
 	) \
 	$(if $(ENABLE_HEADLESS), \
 		vcl/vcl.headless \
@@ -148,6 +147,7 @@ $(eval $(call gb_Rdb_add_components,services,\
 			embeddedobj/source/msole/emboleobj.windows \
 			embedserv/util/emser \
 			extensions/source/ole/oleautobridge \
+			winaccessibility/source/service/winaccessibility \
 		) \
 	) \
 	$(if $(DISABLE_NEON),, \
