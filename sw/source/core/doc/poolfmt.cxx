9d4154eead9ce5e8e9726fbb0491598149fdfb7e
Michael Stahl <mstahl@redhat.com>
2013-12-20 17:58:12 +0100

    fdo#69065: sw: fix mirrored page style with first-page
    If a mirrored page style is used with first-page on both a right page
    and a left page the current design cannot work because the margins in
    the SwPageDesc::aFirst cannot be right for both cases.
    
    So split that up so we get a first-master and first-left format and
    copy the headers/footers and margins as appropriate... which is really
    adding epicycles to a flawed design; probably this would be better with
    just a single SwFrmFmt that has different items for master/left/first
    headers.
    
    Change-Id: I0889a4ab5959b5a71172203bb64d185960a07d73
    (cherry picked from commit e936ecc92a7e362f57ce72a955697840920636b8)

--- sw/source/core/doc/poolfmt.cxx
+++ sw/source/core/doc/poolfmt.cxx
@@ -1632,9 +1632,12 @@ SwPageDesc* SwDoc::GetPageDescFromPool( sal_uInt16 nId, bool bRegardLanguage )
     if( aSet.Count() )
     {
         if( bSetLeft )
+        {
             pNewPgDsc->GetLeft().SetFmtAttr( aSet );
+            pNewPgDsc->GetFirstLeft().SetFmtAttr( aSet );
+        }
         pNewPgDsc->GetMaster().SetFmtAttr( aSet );
-        pNewPgDsc->GetFirst().SetFmtAttr( aSet );
+        pNewPgDsc->GetFirstMaster().SetFmtAttr( aSet );
     }
     return pNewPgDsc;
 }
