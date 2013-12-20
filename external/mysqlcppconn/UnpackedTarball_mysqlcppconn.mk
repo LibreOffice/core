12be6dcb692ba33207a69412055555ab1a38fb31
Julien Nabet <serval2412@yahoo.fr>
2013-12-20 22:17:50 +0100

    User grants debug Mysql part: retrieve IS_GRANTABLE + update column privs
    (See http://dev.mysql.com/doc/refman/5.6/en/grant-table-structure.html
    Table 6.7. Set-Type Privilege Column Values Section)
    
    Change-Id: I6173ba0df84f722a872afa59d7d7dc9eefe3e368

--- external/mysqlcppconn/UnpackedTarball_mysqlcppconn.mk
+++ external/mysqlcppconn/UnpackedTarball_mysqlcppconn.mk
@@ -18,6 +18,7 @@ $(eval $(call gb_UnpackedTarball_add_file,mysqlcppconn,driver/nativeapi/binding_
 $(eval $(call gb_UnpackedTarball_add_patches,mysqlcppconn,\
 	external/mysqlcppconn/patches/mysql-connector-c++-1.1.0.patch \
 	external/mysqlcppconn/patches/default_to_protocol_tcp.patch \
+	external/mysqlcppconn/patches/retrieve_grants.patch.1 \
 ))
 
 # vim: set noet sw=4 ts=4:
