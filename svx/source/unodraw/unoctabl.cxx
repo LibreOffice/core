02eb7511f99d2e2322c57ea86695c3db87a037fd
Matúš Kukan <matus.kukan@collabora.com>
2013-12-30 10:04:05 +0100

    svx: Use constructor feature for sidebar::PanelFactory.
    Change-Id: Iafc01d943c9862a8e8775b79b81c885ab9565dbc

--- svx/source/unodraw/unoctabl.cxx
+++ svx/source/unodraw/unoctabl.cxx
@@ -230,8 +230,6 @@ uno::Reference< uno::XInterface > SAL_CALL create_EnhancedCustomShapeEngine( con
 #include "osl/diagnose.h"
 #include "cppuhelper/factory.hxx"
 #include <svx/sdr/primitive2d/primitiveFactory2d.hxx>
-#include "sidebar/PanelFactory.hxx"
-
 
 extern "C"
 {
@@ -365,14 +363,6 @@ SAL_DLLPUBLIC_EXPORT void * SAL_CALL svx_component_getFactory (
                 ::svx::SvXMLGraphicExportHelper_getImplementationName(),
                 ::svx::SvXMLGraphicExportHelper_createInstance,
                 ::svx::SvXMLGraphicExportHelper_getSupportedServiceNames() );
-        }
-        else if (::svx::sidebar::PanelFactory::getImplementationName().equalsAscii(pImplName))
-        {
-            xFactory = ::cppu::createSingleFactory(
-                reinterpret_cast<lang::XMultiServiceFactory*>(pServiceManager),
-                ::svx::sidebar::PanelFactory::getImplementationName(),
-                ::svx::sidebar::PanelFactory::createInstance,
-                ::svx::sidebar::PanelFactory::getSupportedServiceNames());
         }
 
         if( xFactory.is())
