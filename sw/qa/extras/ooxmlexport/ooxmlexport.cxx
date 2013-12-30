fe612075ba02c3335b3934b1a25ebb629fe7fa25
Vinaya Mandke <vinaya.mandke@synerzip.com>
2013-12-30 16:22:30 +0530

    fdo#69649 Fix for pre-rendered Table Of Contents
    Added Import and Export Uts for the same
    
    Change-Id: I966b3d268dd407aa05823e29fe1da4d097091265

--- sw/qa/extras/ooxmlexport/ooxmlexport.cxx
+++ sw/qa/extras/ooxmlexport/ooxmlexport.cxx
@@ -43,6 +43,7 @@
 #include <unotools/tempfile.hxx>
 #include <unotools/ucbstreamhelper.hxx>
 #include <rtl/strbuf.hxx>
+#include <com/sun/star/text/XDocumentIndex.hpp>
 
 #include <libxml/xpathInternals.h>
 #include <libxml/parserInternals.h>
@@ -2173,6 +2174,18 @@ DECLARE_OOXMLEXPORT_TEST(testRelorientation, "relorientation.docx")
     // This resulted in lang::IndexOutOfBoundsException, as nested groupshapes weren't handled.
     uno::Reference<drawing::XShapeDescriptor> xShapeDescriptor(xGroup->getByIndex(0), uno::UNO_QUERY);
     CPPUNIT_ASSERT_EQUAL(OUString("com.sun.star.drawing.GroupShape"), xShapeDescriptor->getShapeType());
+}
+
+DECLARE_OOXMLEXPORT_TEST(testFdo69649, "fdo69649.docx")
+{
+    // The DOCX containing the Table of Contents was not exported with correct page nos
+    xmlDocPtr pXmlDoc = parseExport("word/document.xml");
+    if (!pXmlDoc)
+        return;
+    xmlNodeSetPtr pXmlNodes = getXPathNode(pXmlDoc,"/w:document/w:body/w:p[21]/w:hyperlink/w:r[2]/w:t");
+    xmlNodePtr pXmlNode = pXmlNodes->nodeTab[0];
+    OUString contents = OUString::createFromAscii((const char*)((pXmlNode->children[0]).content));
+    CPPUNIT_ASSERT(contents.match("15"));
 }
 
 #endif
