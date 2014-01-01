2056f93f2d9f62741045e2aee0ed4d31001febef
Manal Alhassoun <malhassoun@kacst.edu.sa>
2014-01-01 08:34:41 +0300

    convert update file list queryboxes to .ui
    Change-Id: I9e9ad3674c6b45181b4fc3d4e252431b0dcab06d
    Reviewed-on: https://gerrit.libreoffice.org/7243
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>

--- cui/UIConfig_cui.mk
+++ cui/UIConfig_cui.mk
@@ -125,6 +125,7 @@ $(eval $(call gb_UIConfig_add_uifiles,cui,\
 	cui/uiconfig/ui/querynoloadedfiledialog \
 	cui/uiconfig/ui/querynosavefiledialog \
 	cui/uiconfig/ui/querysavelistdialog \
+	cui/uiconfig/ui/queryupdategalleryfilelistdialog \
 	cui/uiconfig/ui/recordnumberdialog \
 	cui/uiconfig/ui/rotationtabpage \
 	cui/uiconfig/ui/scriptorganizer \
