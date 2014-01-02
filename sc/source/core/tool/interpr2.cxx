cc1b6535e01db62688a7bee50852d33e4ee50761
Eike Rathke <erack@redhat.com>
2014-01-02 22:14:07 +0100

    added Latvian "LVL" to EUROCONVERT(), fdo#73239
    Change-Id: I2041a4a8fa2387a217eac96d4424f8ee511a2e00

--- sc/source/core/tool/interpr2.cxx
+++ sc/source/core/tool/interpr2.cxx
@@ -2562,7 +2562,8 @@ static bool lclConvertMoney( const OUString& aSearchUnit, double& rfRate, int& r
         { "MTL", 0.429300, 2 },
         { "CYP", 0.585274, 2 },
         { "SKK", 30.1260,  2 },
-        { "EEK", 15.6466,  2 }
+        { "EEK", 15.6466,  2 },
+        { "LVL", 0.702804, 2 }
     };
 
     const size_t nConversionCount = sizeof( aConvertTable ) / sizeof( aConvertTable[0] );
