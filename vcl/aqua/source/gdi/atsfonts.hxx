874cc95d768256310b6009b9d392bed4ea7558bb
Herbert Dürr <hdu@apache.org>
2014-01-03 16:03:40 +0000

    #i123951# add atsfonts.hxx header file
    Found by: Pavel Janik <Pavel.Janik.cz>
    Patch by: Herbert Duerr <hdu@apache.org>

---
+++ vcl/aqua/source/gdi/atsfonts.hxx
@@ -0,0 +1,57 @@
+/**************************************************************
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ *************************************************************/
+
+#ifndef _ATSFONTS_HXX
+#define _ATSFONTS_HXX
+
+class ImplMacFontData;
+class ImplDevFontList;
+
+#include <premac.h>
+#include <ApplicationServices/ApplicationServices.h>
+#include <postmac.h>
+
+#include <map>
+
+/* This class has the responsibility of assembling a list
+   of atsui compatible fonts available on the system and
+   enabling access to that list.
+ */
+class SystemFontList
+{
+public:
+    SystemFontList();
+    ~SystemFontList();
+
+    void AnnounceFonts( ImplDevFontList& ) const;
+    ImplMacFontData* GetFontDataFromId( ATSUFontID ) const;
+
+    ATSUFontFallbacks maFontFallbacks;
+
+private:
+    typedef std::hash_map<ATSUFontID,ImplMacFontData*> MacFontContainer;
+    MacFontContainer maFontContainer;
+
+    void InitGlyphFallbacks();
+};
+
+#endif // _ATSFONTS_HXX
+
