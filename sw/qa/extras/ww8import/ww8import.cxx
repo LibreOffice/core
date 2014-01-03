d6fc6624e1319257945f49388d635c36685108d5
Zolnai Tamás <tamas.zolnai@collabora.com>
2014-01-03 11:38:05 +0100

    cp#2013101510000026: wrong highlight of commented text range imported from doc
    When makeFieldBookmark() method is called with
    ODF_COMMENTRANGE it will ignore the added field name and
    generate an own one. We have to set the name of the
    SwPostItField to this generated name so these two names will match.
    
    Note: lTagBkmk is only an identifier but not the exported name
    of the fieldmark so we don't need to stick to use it as
    a name.
    
    (cherry picked from commit 12e287220ebaf3a8f5eaf7bf526dce2c47f45a20)
    
    Conflicts:
    	sw/source/filter/ww8/ww8par.cxx
    
    Change-Id: I499abdcce1be0563c308bcf9f5c0a959a07f858b

--- sw/qa/extras/ww8import/ww8import.cxx
+++ sw/qa/extras/ww8import/ww8import.cxx
@@ -246,7 +246,6 @@ void Test::testFdo59530()
     uno::Reference<container::XEnumerationAccess> xFieldsAccess(xTextFieldsSupplier->getTextFields());
     uno::Reference<container::XEnumeration> xFields(xFieldsAccess->createEnumeration());
     xPropertySet.set(xFields->nextElement(), uno::UNO_QUERY);
-    CPPUNIT_ASSERT_EQUAL(OUString("346376201"), getProperty<OUString>(xPropertySet, "Name"));
     CPPUNIT_ASSERT_EQUAL(OUString("M"), getProperty<OUString>(xPropertySet, "Initials"));
 }
 
