08940a0d21db8be76342e426c378b9f5c66ff624
Matúš Kukan <matus.kukan@collabora.com>
2014-01-09 16:28:38 +0100

    fwk: Use constructor feature for JobDispatch.
    Change-Id: I49a09c4dd0550c318e9f263e9d1a8d0a5e0e15b5

--- framework/source/jobs/jobdispatch.cxx
+++ framework/source/jobs/jobdispatch.cxx
@@ -17,24 +17,118 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#include <jobs/jobdispatch.hxx>
+#include <jobs/configaccess.hxx>
 #include <jobs/joburl.hxx>
 #include <jobs/job.hxx>
+#include <macros/xinterface.hxx>
+#include <macros/xtypeprovider.hxx>
+#include <macros/xserviceinfo.hxx>
 #include <threadhelp/readguard.hxx>
-#include <threadhelp/writeguard.hxx>
 #include <threadhelp/resetableguard.hxx>
+#include <threadhelp/threadhelpbase.hxx>
+#include <threadhelp/writeguard.hxx>
 #include <classes/converter.hxx>
 #include <general.h>
 #include <services.h>
 
-#include <com/sun/star/beans/XPropertySet.hpp>
+#include <com/sun/star/frame/DispatchResultEvent.hpp>
 #include <com/sun/star/frame/DispatchResultState.hpp>
 #include <com/sun/star/frame/ModuleManager.hpp>
+#include <com/sun/star/frame/XNotifyingDispatch.hpp>
+#include <com/sun/star/frame/XDispatch.hpp>
+#include <com/sun/star/frame/XStatusListener.hpp>
+#include <com/sun/star/frame/XDispatchResultListener.hpp>
+#include <com/sun/star/frame/XDispatchProvider.hpp>
+#include <com/sun/star/lang/XInitialization.hpp>
 
+#include <cppuhelper/implbase4.hxx>
+#include <rtl/ref.hxx>
 #include <rtl/ustrbuf.hxx>
+#include <unotools/configpaths.hxx>
 #include <vcl/svapp.hxx>
 
-namespace framework{
+using namespace framework;
+
+namespace {
+
+//_______________________________________
+/**
+    @short  implements a dispatch object for jobs
+    @descr  Such dispatch object will be used by the generic dispatch mechanism if
+            an URL "vnd.sun.star.job:alias=<name>" occurs.
+            Then an instance of this class will be created and used.
+            This new instance will be called within his method
+            dispatch() or dispatchWithNotification() for executing the
+            real job. We do it, control the life cycle of this internal
+            wrapped job and inform any interested listener if it finish.
+ */
+class JobDispatch : private ThreadHelpBase
+                  , public  ::cppu::WeakImplHelper4<
+                            css::lang::XServiceInfo
+                          , css::lang::XInitialization
+                          , css::frame::XDispatchProvider
+                          , css::frame::XNotifyingDispatch >      // => XDispatch
+{
+    private:
+
+        /** reference to the uno service manager */
+        css::uno::Reference< css::uno::XComponentContext > m_xContext;
+
+        /** reference to the frame, inside which this dispatch is used */
+        css::uno::Reference< css::frame::XFrame > m_xFrame;
+
+        /** name of module (writer, impress etc.) the frame is for */
+        OUString m_sModuleIdentifier;
+
+    //___________________________________
+    // native interface methods
+
+    public:
+
+                  JobDispatch( const css::uno::Reference< css::uno::XComponentContext >& xContext );
+         virtual ~JobDispatch(                                                                     );
+
+        void impl_dispatchEvent  ( const OUString&                                            sEvent    ,
+                                   const css::uno::Sequence< css::beans::PropertyValue >&            lArgs     ,
+                                   const css::uno::Reference< css::frame::XDispatchResultListener >& xListener );
+        void impl_dispatchService( const OUString&                                            sService  ,
+                                   const css::uno::Sequence< css::beans::PropertyValue >&            lArgs     ,
+                                   const css::uno::Reference< css::frame::XDispatchResultListener >& xListener );
+        void impl_dispatchAlias  ( const OUString&                                            sAlias    ,
+                                   const css::uno::Sequence< css::beans::PropertyValue >&            lArgs     ,
+                                   const css::uno::Reference< css::frame::XDispatchResultListener >& xListener );
+
+    //___________________________________
+    // uno interface methods
+
+    public:
+
+        // XInterface, XTypeProvider, XServiceInfo
+        DECLARE_XSERVICEINFO
+
+        // Xinitialization
+        virtual void SAL_CALL initialize( const css::uno::Sequence< css::uno::Any >& lArguments ) throw(css::uno::Exception       ,
+                                                                                                        css::uno::RuntimeException);
+
+        // XDispatchProvider
+        virtual css::uno::Reference< css::frame::XDispatch > SAL_CALL                       queryDispatch  ( const css::util::URL&                                       aURL             ,
+                                                                                                             const OUString&                                      sTargetFrameName ,
+                                                                                                                   sal_Int32                                             nSearchFlags     ) throw(css::uno::RuntimeException);
+        virtual css::uno::Sequence< css::uno::Reference< css::frame::XDispatch > > SAL_CALL queryDispatches( const css::uno::Sequence< css::frame::DispatchDescriptor >& lDescriptor      ) throw(css::uno::RuntimeException);
+
+        // XNotifyingDispatch
+        virtual void SAL_CALL dispatchWithNotification( const css::util::URL&                                             aURL      ,
+                                                        const css::uno::Sequence< css::beans::PropertyValue >&            lArgs     ,
+                                                        const css::uno::Reference< css::frame::XDispatchResultListener >& xListener ) throw(css::uno::RuntimeException);
+
+        // XDispatch
+        virtual void SAL_CALL dispatch            ( const css::util::URL&                                     aURL      ,
+                                                    const css::uno::Sequence< css::beans::PropertyValue >&    lArgs     ) throw(css::uno::RuntimeException);
+        virtual void SAL_CALL addStatusListener   ( const css::uno::Reference< css::frame::XStatusListener >& xListener ,
+                                                    const css::util::URL&                                     aURL      ) throw(css::uno::RuntimeException);
+        virtual void SAL_CALL removeStatusListener( const css::uno::Reference< css::frame::XStatusListener >& xListener ,
+                                                    const css::util::URL&                                     aURL      ) throw(css::uno::RuntimeException);
+};
 
 DEFINE_XSERVICEINFO_MULTISERVICE_2( JobDispatch                   ,
                                     ::cppu::OWeakObject           ,
@@ -440,6 +534,21 @@ void SAL_CALL JobDispatch::removeStatusListener( /*IN*/ const css::uno::Referenc
 {
 }
 
-} // namespace framework
+}
+
+extern "C" SAL_DLLPUBLIC_EXPORT css::uno::XInterface * SAL_CALL
+com_sun_star_comp_framework_jobs_JobDispatch_get_implementation(
+        css::uno::XComponentContext * context,
+        uno_Sequence * arguments)
+{
+    assert(arguments != 0);
+    rtl::Reference<JobDispatch> x(new JobDispatch(context));
+    css::uno::Sequence<css::uno::Any> aArgs(
+            reinterpret_cast<css::uno::Any *>(arguments->elements),
+            arguments->nElements);
+    x->initialize(aArgs);
+    x->acquire();
+    return static_cast<cppu::OWeakObject *>(x.get());
+}
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
