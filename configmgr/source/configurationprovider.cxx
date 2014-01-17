5f209df8858e56e639394687784d34d3944acad5
Eike Rathke <erack@redhat.com>
2014-01-17 19:21:56 +0100

    fdo#73549 do not attempt to resolve an empty locale
    ... to match installed UI locales
    
    Change-Id: If0ad0fde392bc5ea3cf454f4858330f5e93a6666
    (cherry picked from commit 8e826c7ff7c597e9f585377b2117f4dc24239dcc)
    
    fdo#73549 do not resolve empty locale here if not determined yet
    
    Change-Id: Iad03d6209a2b4024b27c6195195e5aa557c295fe
    (cherry picked from commit db6f8f9f8969b592ed90c841960fdd186e1cbc5a)
    
    do not resolve empty locale here when set, fdo#73549 related
    
    Change-Id: Id9c8c3926f64826fd2295e0f75f007ce435f5915
    (cherry picked from commit 26fc9be1bf2d6aaeb52a571ea416f4527a52e146)
    Reviewed-on: https://gerrit.libreoffice.org/7530
    Reviewed-by: Andras Timar <andras.timar@collabora.com>
    Tested-by: Andras Timar <andras.timar@collabora.com>
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>

--- configmgr/source/configurationprovider.cxx
+++ configmgr/source/configurationprovider.cxx
@@ -324,7 +324,7 @@ void Service::setLocale(css::lang::Locale const & eLocale)
     throw (css::uno::RuntimeException)
 {
     osl::MutexGuard guard(*lock_);
-    locale_ = LanguageTag::convertToBcp47( eLocale);
+    locale_ = LanguageTag::convertToBcp47( eLocale, false);
 }
 
 css::lang::Locale Service::getLocale() throw (css::uno::RuntimeException) {
