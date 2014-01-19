70068f428d570a4ee00c682f1b8e12de395a8be4
Andras Timar <andras.timar@collabora.com>
2014-01-19 19:13:57 +0100

    fdo#73764 partially revert 3998d1abb291
    Change-Id: Ie9b6d6a318fad256f3f0999fe9c0810a4323f10d

--- vcl/source/filter/wmf/winmtf.cxx
+++ vcl/source/filter/wmf/winmtf.cxx
@@ -316,7 +316,7 @@ Point WinMtfOutput::ImplScale( const Point& rPt) // Hack to set varying defaults
 {
         if (mbIsMapDevSet && mbIsMapWinSet)
         {
-            return Point((rPt.X())*mnWinExtX/mnDevWidth-mrclFrame.Left(),(rPt.Y())*mnWinExtY/mnDevHeight-mrclFrame.Top());
+            return rPt; //fdo#73764
         }
         else
         {
