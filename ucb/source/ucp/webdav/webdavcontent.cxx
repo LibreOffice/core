LibreOffice/core

commit a3f32769fc4bb23c64168b412dd10ec769a3854d

Fix bogus mass-conversion equalsAsciiL -> startsWith
3af99e4d59d89c343965a928681a30f36b1007d2 "convert equalsAsciiL calls to
startsWith calls" should rather have converted to oprator ==.

Change-Id: Id4a8836c5d6d570e54661c40be7214632e202b21

ucb/source/ucp/webdav/webdavcontent.cxx
@@ -97,7 +97,7 @@ static void lcl_sendPartialGETRequest( bool &bError,
     for ( std::vector< rtl::OUString >::const_iterator it = aHeaderNames.begin();
             it != aHeaderNames.end(); ++it )
     {
-        if ( it->startsWith( "Content-Length" ) )
+        if ( *it == "Content-Length" )
         {
             bIsRequestSize = true;
             break;
@@ -129,11 +129,11 @@ static void lcl_sendPartialGETRequest( bool &bError,
             for ( std::vector< DAVPropertyValue >::const_iterator it = aResponseProps.begin();
                     it != aResponseProps.end(); ++it )
             {
-                if ( it->Name.startsWith( "Accept-Ranges" ) )
+                if ( it->Name == "Accept-Ranges" )
                     it->Value >>= aAcceptRanges;
-                else if ( it->Name.startsWith( "Content-Range" ) )
+                else if ( it->Name == "Content-Range" )
                     it->Value >>= aContentRange;
-                else if ( it->Name.startsWith( "Content-Length" ) )
+                else if ( it->Name == "Content-Length" )
                     it->Value >>= aContentLength;
             }
 
@@ -148,7 +148,7 @@ static void lcl_sendPartialGETRequest( bool &bError,
             // MAY ignore ranges specified using other units.
             if ( nSize == 1 &&
                     aContentRange.getLength() &&
-                    aAcceptRanges.startsWith( "bytes" ) )
+                    aAcceptRanges == "bytes" )
             {
                 // Parse the Content-Range to get the size
                 // vid. http://tools.ietf.org/html/rfc2616#section-14.16
@@ -158,12 +158,12 @@ static void lcl_sendPartialGETRequest( bool &bError,
                 {
                     rtl::OUString aSize = aContentRange.copy( nSlash + 1 );
                     // "*" means that the instance-length is unknown at the time when the response was generated
-                    if ( !aSize.startsWith( "*" ))
+                    if ( aSize != "*" )
                     {
                         for ( std::vector< DAVPropertyValue >::iterator it = aResponseProps.begin();
                                 it != aResponseProps.end(); ++it )
                         {
-                            if ( it->Name.startsWith( "Content-Length" ) )
+                            if ( it->Name != "Content-Length" )
                             {
                                 it->Value <<= aSize;
                                 break;
@@ -734,7 +734,7 @@ uno::Any SAL_CALL Content::execute(
 
         aRet = uno::makeAny( createNewContent( aArg ) );
     }
-    else if ( aCommand.Name.startsWith( "addProperty" ))
+    else if ( aCommand.Name == "addProperty" )
     {
         ucb::PropertyCommandArgument aPropArg;
         if ( !( aCommand.Argument >>= aPropArg ))
@@ -766,7 +766,7 @@ uno::Any SAL_CALL Content::execute(
             ucbhelper::cancelCommandExecution( uno::makeAny( e ), Environment );
         }
     }
-    else if ( aCommand.Name.startsWith( "removeProperty" ))
+    else if ( aCommand.Name == "removeProperty" )
     {
         rtl::OUString sPropName;
         if ( !( aCommand.Argument >>= sPropName ) )
@@ -1870,7 +1870,7 @@ uno::Sequence< uno::Any > Content::setPropertyValues(
                                 OUString( "Property is read-only!" ),
                                 static_cast< cppu::OWeakObject * >( this ) );
             }
-            if ( rName.startsWith( "CreatableContentsInfo" ) )
+            if ( rName == "CreatableContentsInfo" )
             {
                 // Read-only property!
                 aRet[ n ] <<= lang::IllegalAccessException(

connectivity/source/commontools/dbtools2.cxx
@@ -874,7 +874,7 @@ bool isEmbeddedInDatabase( const Reference< XInterface >& _rxComponent, Referenc
                     const PropertyValue* pContextEnd  = pContextIter + aDocumentContext.getLength();
                     for(;pContextIter != pContextEnd;++pContextIter)
                     {
-                        if (   pContextIter->Name.startsWith( "ActiveConnection" )
+                        if (   pContextIter->Name == "ActiveConnection"
                             && ( pContextIter->Value >>= _rxActualConnection )
                            )
                         {

cppu/source/uno/lbmap.cxx
@@ -390,7 +390,7 @@ static Mapping loadExternalMapping(
         OUString aName;
         uno_ext_getMappingFunc fpGetMapFunc = 0;
 
-        if (EnvDcp::getTypeName(rFrom.getTypeName()).startsWith( UNO_LB_UNO ))
+        if (EnvDcp::getTypeName(rFrom.getTypeName()) == UNO_LB_UNO)
         {
             aName = getBridgeName( rTo, rFrom, rAddPurpose );
             fpGetMapFunc = selectMapFunc( aName );

cui/source/customize/cfg.cxx
@@ -411,8 +411,8 @@ SvxConfigPage::CanConfig( const OUString& aModuleId )
 
 OUString GetModuleName( const OUString& aModuleId )
 {
-    if ( aModuleId.startsWith( "com.sun.star.text.TextDocument" ) ||
-         aModuleId.startsWith( "com.sun.star.text.GlobalDocument" ) )
+    if ( aModuleId == "com.sun.star.text.TextDocument" ||
+         aModuleId == "com.sun.star.text.GlobalDocument" )
         return OUString("Writer");
     else if ( aModuleId == "com.sun.star.text.WebDocument" )
         return OUString("Writer/Web");

cui/source/options/treeopt.cxx
@@ -1613,9 +1613,9 @@ void OfaTreeOptionsDialog::Initialize( const Reference< XFrame >& _xFrame )
     {
         // text document
         ResStringArray& rTextArray = aDlgResource.GetTextArray();
-        if (   aFactory.startsWith( "com.sun.star.text.TextDocument" )
-            || aFactory.startsWith( "com.sun.star.text.WebDocument" )
-            || aFactory.startsWith( "com.sun.star.text.GlobalDocument" ) )
+        if (   aFactory == "com.sun.star.text.TextDocument"
+            || aFactory == "com.sun.star.text.WebDocument"
+            || aFactory == "com.sun.star.text.GlobalDocument" )
         {
             SfxModule* pSwMod = (*(SfxModule**) GetAppData(SHL_WRITER));
             if ( !lcl_isOptionHidden( SID_SW_EDITOPTIONS, aOptionsDlgOpt ) )

dtrans/source/cnttype/mcnttype.cxx
@@ -170,7 +170,7 @@ void SAL_CALL CMimeContentType::acceptSym( const OUString& pSymTlb )
 
 void SAL_CALL CMimeContentType::skipSpaces( void )
 {
-    while (m_nxtSym.startsWith(SPACE))
+    while (m_nxtSym == SPACE)
         getSym( );
 }
 
@@ -353,8 +353,8 @@ OUString SAL_CALL CMimeContentType::quotedPValue( )
     while ( !m_nxtSym.isEmpty( ) )
     {
         if ( bAfterQuoteSign && (
-            (m_nxtSym.startsWith(SPACE)) ||
-            (m_nxtSym.startsWith(SEMICOLON)))
+            (m_nxtSym == SPACE) ||
+            (m_nxtSym == SEMICOLON))
            )
         {
             break;

editeng/source/editeng/edtspell.cxx
@@ -652,7 +652,7 @@ sal_Bool EdtAutoCorrDoc::SetAttr(sal_Int32 nStt, sal_Int32 nEnd,
 {
     SfxItemPool* pPool = &mpEditEngine->GetEditDoc().GetItemPool();
     while ( pPool->GetSecondaryPool() &&
-            !pPool->GetName().startsWith("EditEngineItemPool") )
+            pPool->GetName() != "EditEngineItemPool" )
     {
         pPool = pPool->GetSecondaryPool();
 

editeng/source/editeng/impedit4.cxx
@@ -158,13 +158,13 @@ EditPaM ImpEditEngine::ReadRTF( SvStream& rInput, EditSelection aSel )
     // The SvRTF parser expects the Which-mapping passed on in the pool, not
     // dependent on a secondary.
     SfxItemPool* pPool = &aEditDoc.GetItemPool();
-    while (pPool->GetSecondaryPool() && !pPool->GetName().startsWith("EditEngineItemPool"))
-    {
+    while (pPool->GetSecondaryPool() && pPool->GetName() != "EditEngineItemPool")
+   {
         pPool = pPool->GetSecondaryPool();
 
     }
 
-    DBG_ASSERT(pPool && pPool->GetName().startsWith("EditEngineItemPool"),
+    DBG_ASSERT(pPool && pPool->GetName() == "EditEngineItemPool",
         "ReadRTF: no EditEnginePool!");
 
     EditRTFParserRef xPrsr = new EditRTFParser(rInput, aSel, *pPool, pEditEngine);

commit 3780738154b8c3b3f9d85c64cccf621d97574886

ucb: fix inverted condition in webdav lcl_sendPartialGETRequest
(regression from a3f32769fc4bb23c64168b412dd10ec769a3854d)

Change-Id: Ic72b0e874ebf19e25e43af1f8750c63c04ed8a8e

ucb/source/ucp/webdav/webdavcontent.cxx
@@ -163,7 +163,7 @@ static void lcl_sendPartialGETRequest( bool &bError,
                         for ( std::vector< DAVPropertyValue >::iterator it = aResponseProps.begin();
                                 it != aResponseProps.end(); ++it )
                         {
-                            if ( it->Name != "Content-Length" )
+                            if (it->Name == "Content-Length")
                             {
                                 it->Value <<= aSize;
                                 break;