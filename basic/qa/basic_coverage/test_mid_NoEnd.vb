4a96c719dee503b38218812828bc87ef27a15f96
Julien Nabet <serval2412@yahoo.fr>
2014-01-21 23:40:11 +0100

    Add some unit-tests for Mid vb function (includes fdo#73771)
    Change-Id: Ib5ee18207897cbe16a818096769cdb055dc9dad7

---
+++ basic/qa/basic_coverage/test_mid_NoEnd.vb
@@ -0,0 +1,15 @@
+'
+' This file is part of the LibreOffice project.
+'
+' This Source Code Form is subject to the terms of the Mozilla Public
+' License, v. 2.0. If a copy of the MPL was not distributed with this
+' file, You can obtain one at http://mozilla.org/MPL/2.0/.
+'
+
+Function doUnitTest as Integer
+    If (Mid("abc", 2) = "bc") Then
+        doUnitTest = 1
+    Else
+        doUnitTest = 0
+    End If
+End Function
