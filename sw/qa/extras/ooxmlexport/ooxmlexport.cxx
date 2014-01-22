9c0ab18a3c8c7bf5b1eb1b7f61bf507f3f93f30f
Rohit Deshmukh <rohit.deshmukh@synerzip.com>
2014-01-22 18:03:21 +0530

    Fix for Footer is missing if fisrt page different header/footer is set
    Change-Id: I2a2f2abc0dcf5542b7b950f9a232d7155a055fdd

--- sw/qa/extras/ooxmlexport/ooxmlexport.cxx
+++ sw/qa/extras/ooxmlexport/ooxmlexport.cxx
@@ -2554,6 +2554,13 @@ DECLARE_OOXMLEXPORT_TEST(testTOCFlag_u,"testTOCFlag_u.docx")
     OUString contents = OUString::createFromAscii((const char*)((pXmlNode->children[0]).content));
     CPPUNIT_ASSERT(contents.match(" TOC \\z \\o \"1-9\" \\u \\h"));
 }
+DECLARE_OOXMLEXPORT_TEST(testTestTitlePage, "testTitlePage.docx")
+{
+    xmlDocPtr pXmlDoc = parseExport("word/document.xml");
+    if (!pXmlDoc)
+        return;
+    assertXPathChildren(pXmlDoc, "/w:document/w:body/w:sectPr/w:titlePg", 0);
+}
 
 #endif
 
