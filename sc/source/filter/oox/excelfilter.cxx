c0ea29a5ff6517bf20c3eefe84a07bd53e942f14
Caolán McNamara <caolanm@redhat.com>
2014-01-24 17:09:01 +0000

    coverity#1158385 Uncaught exception
    Change-Id: I204efd1c17d07f4fb24be9783d78173a57ef4ea9

--- sc/source/filter/oox/excelfilter.cxx
+++ sc/source/filter/oox/excelfilter.cxx
@@ -127,6 +127,10 @@ bool ExcelFilter::importDocument() throw()
         {
             SAL_WARN("sc", "exception when importing document properties " << e.Message);
         }
+        catch( ... )
+        {
+            SAL_WARN("sc", "exception when importing document properties");
+        }
         return true;
     }
     return false;
