5a2252d3231039820546e2a72109dfc1f36b0529
Norbert Thiebaud <nthiebaud@gmail.com>
2014-01-26 02:42:06 -0600

    coverity#1157770 : Logically dead code
    Change-Id: I92ccd06eb690205f1a47a86297077436e65e17b2

--- sw/source/core/access/accpara.cxx
+++ sw/source/core/access/accpara.cxx
@@ -1562,21 +1562,22 @@ OUString SwAccessibleParagraph::GetFieldTypeNameAtIndex(sal_Int32 nIndex)
         const SwpHints* pSwpHints = GetTxtNode()->GetpSwpHints();
         if (pSwpHints)
         {
-            const sal_uInt16  nSize = pSwpHints ? pSwpHints->Count() : 0;
+            const sal_uInt16  nSize = pSwpHints->Count();
             for( sal_uInt16 i = 0; i < nSize; ++i )
             {
                 const SwTxtAttr* pHt = (*pSwpHints)[i];
-                if ( ( pHt->Which() == RES_TXTATR_FIELD
-                       || pHt->Which() == RES_TXTATR_ANNOTATION
-                       || pHt->Which() == RES_TXTATR_INPUTFIELD )
-                     && (nFldIndex-- == 0))
+                if ( ( pHt->Which() == RES_TXTATR_FIELD ||
+                       pHt->Which() == RES_TXTATR_ANNOTATION ||
+                       pHt->Which() == RES_TXTATR_INPUTFIELD ) &&
+                     ( nFldIndex-- == 0))
                 {
                     pTxtFld = (SwTxtFld *)pHt;
                     break;
                 }
-                else if (pHt->Which() == RES_TXTATR_REFMARK
-                         && (nFldIndex-- == 0))
+                else if (pHt->Which() == RES_TXTATR_REFMARK && (nFldIndex-- == 0))
+                {
                     strTypeName = "set reference";
+                }
             }
         }
     }
