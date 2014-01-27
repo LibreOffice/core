271011ff347790ae2b045cfd8035512cedeefb1b
Miklos Vajna <vmiklos@collabora.co.uk>
2014-01-27 14:54:04 +0100

    writerfilter: recurse into NS_ooxml::LN_sizeRelH_sizeRelH
    Change-Id: I2fdb9ff8d5bcec194502a43207953103aa0b0dee

--- writerfilter/source/dmapper/GraphicImport.cxx
+++ writerfilter/source/dmapper/GraphicImport.cxx
@@ -822,6 +822,7 @@ void GraphicImport::lcl_sprm(Sprm & rSprm)
         case NS_ooxml::LN_c_chart:
         case NS_ooxml::LN_wps_wsp:
         case NS_ooxml::LN_wpg_wgp:
+        case NS_ooxml::LN_sizeRelH_sizeRelH:
         {
             writerfilter::Reference<Properties>::Pointer_t pProperties = rSprm.getProps();
             if( pProperties.get())
