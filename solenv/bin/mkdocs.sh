LibreOffice/core

commit 247cd33817457789645827a52d79a1876b9760a3

fdo#74013 make makedocs.sh look in correct project header directories
At the moment, project headers can be either in $PROJECT/inc or
include/$PROJECT. I believe it is possible for them to be in both
locations. Unfortunately, we check for both areas, and doxygen spits
out an error.

Fix is to check if the header directory exists, only use it if it does.

Change-Id: I2e59d8ce0028c20b7d239dcc6e07792edf7078d1
Reviewed-on: https://gerrit.libreoffice.org/7631
Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
Tested-by: Miklos Vajna <vmiklos@collabora.co.uk>

solenv/bin/mkdocs.sh
@@ -157,7 +157,17 @@ DOXYGEN_REF_TAGFILES=""
 for PROJECT in $INPUT_PROJECTS;
 do
   # avoid processing of full project subdirs, only add source and inc
-  DOXYGEN_INPUT=`printf "%s" "$PROJECT/source $PROJECT/inc include/$PROJECT "`
+
+  # project header files can be in $PROJECT/inc and/pr include/$PROJECT
+  if [ -d "$PROJECT/inc" ]; then
+      PROJECT_INCLUDE="$PROJECT/inc"
+  fi
+
+  if [ -d "include/$PROJECT" ]; then
+      PROJECT_INCLUDE="$PROJECT_INCLUDE $PROJECT_INCLUDE"
+  fi
+
+  DOXYGEN_INPUT=`printf "%s" "$PROJECT/source $PROJECT_INCLUDE"`
 
   DOXYGEN_OUTPUT="$BASE_OUTPUT/$PROJECT"
   DOXYGEN_OUR_TAGFILE="$DOXYGEN_OUTPUT/$PROJECT.tags"

commit 78d498f3257ef3447566a28b95c9c5df7d4a0059

Fix include path error in mkdocs.sh introduced in 247cd3381745
Unfortunately in commit 247cd33817457789645827a52d79a1876b9760a3 I
mistakenly included the directories wrongly. This fixes this.

Change-Id: I91193321d1264985eab4865ca05adeab96401b2f

solenv/bin/mkdocs.sh
@@ -164,7 +164,7 @@ do
   fi
 
   if [ -d "include/$PROJECT" ]; then
-      PROJECT_INCLUDE="$PROJECT_INCLUDE $PROJECT_INCLUDE"
+      PROJECT_INCLUDE="$PROJECT_INCLUDE include/$PROJECT"
   fi
 
   DOXYGEN_INPUT=`printf "%s" "$PROJECT/source $PROJECT_INCLUDE"`