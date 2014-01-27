44cb71f90ed0e9315e4fa18594e03bfcc812f667
Andras Timar <andras.timar@collabora.com>
2014-01-27 22:27:24 +0100

    fdo#74119 fix unlocalized redline menu
    Conflicts:
    	sw/AllLangResTarget_sw.mk
    
    Change-Id: Ia4836a69ddd8bebe8cc21cea67be96b029388cb1

--- sw/AllLangResTarget_sw.mk
+++ sw/AllLangResTarget_sw.mk
@@ -58,6 +58,7 @@ $(eval $(call gb_SrsTarget_set_include,sw/res,\
     -I$(SRCDIR)/sw/source/ui/web \
     -I$(SRCDIR)/sw/source/ui/wrtsh \
     -I$(call gb_SrsTemplateTarget_get_include_dir,) \
+    -I$(call gb_SrsTemplateTarget_get_include_dir,sw) \
 ))
 
 $(eval $(call gb_SrsTarget_add_files,sw/res,\
@@ -121,6 +122,10 @@ $(eval $(call gb_SrsTarget_add_files,sw/res,\
     sw/source/ui/utlui/utlui.src \
     sw/source/ui/web/web.src \
     sw/source/ui/wrtsh/wrtsh.src \
+))
+
+$(eval $(call gb_SrsTarget_add_templates,sw/res,\
+    sw/source/ui/inc/redline_tmpl.hrc \
 ))
 
 # vim: set noet sw=4 ts=4:
