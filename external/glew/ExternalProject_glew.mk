993eff9f18ee22d2d70d23bc65e36bba3d4755e0
Markus Mohrhard <markus.mohrhard@googlemail.com>
2014-01-29 00:14:19 +0100

    we only need the glew libs
    Change-Id: Ibb67b7cbf9c41188f026826cdacacff26b29c576

--- external/glew/ExternalProject_glew.mk
+++ external/glew/ExternalProject_glew.mk
@@ -24,7 +24,7 @@ else
 
 $(call gb_ExternalProject_get_state_target,glew,build) :
 	$(call gb_ExternalProject_run,glew,\
-		$(if $(filter TRUE,$(ENABLE_DEBUG)),STRIP=) $(MAKE) $(if $(filter TRUE,$(ENABLE_DEBUG)),debug) \
+		$(if $(filter TRUE,$(ENABLE_DEBUG)),STRIP=) $(MAKE) glew.lib $(if $(filter TRUE,$(ENABLE_DEBUG)),debug) \
 	)
 
 endif
