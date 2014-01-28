65230ffbe1ec27575dd04751ea1bae684dd6ef7e
Michael Stahl <mstahl@redhat.com>
2014-01-28 20:45:20 +0100

    fix bad cast in SvxStyleBox_Impl::StateChanged()
    This causes various JunitTest crashes on Windows, regression from
    887bc4dd3e62fe6dd19dc9d1c3ba273a5b21b5ec.
    
    Change-Id: Iab24e0d39375ac1e5ac63d32470dca1e54d518c5
    Reviewed-on: https://gerrit.libreoffice.org/9621
    Reviewed-by: Tor Lillqvist <tml@collabora.com>
    Tested-by: Tor Lillqvist <tml@collabora.com>

--- svx/source/tbxctrls/tbcontrl.cxx
+++ svx/source/tbxctrls/tbcontrl.cxx
@@ -598,7 +598,10 @@ void SvxStyleBox_Impl::UserDraw( const UserDrawEvent& rUDEvt )
                 Size aPixelSize( pDevice->LogicToPixel( aFontSize, pShell->GetMapUnit() ) );
 
                 // setup the font properties
-                Font aFont( pFontItem->GetFamilyName(), pFontItem->GetStyleName(), aPixelSize );
+                SvxFont aFont;
+                aFont.SetName(pFontItem->GetFamilyName());
+                aFont.SetStyleName(pFontItem->GetStyleName());
+                aFont.SetSize(aPixelSize);
 
                 const SfxPoolItem *pItem = aItemSet.GetItem( SID_ATTR_CHAR_WEIGHT );
                 if ( pItem )
@@ -634,7 +637,7 @@ void SvxStyleBox_Impl::UserDraw( const UserDrawEvent& rUDEvt )
 
                 pItem = aItemSet.GetItem( SID_ATTR_CHAR_CASEMAP );
                 if ( pItem )
-                    ((SvxFont &)aFont).SetCaseMap( static_cast< const SvxCaseMapItem* >( pItem )->GetCaseMap() );
+                    aFont.SetCaseMap(static_cast<const SvxCaseMapItem*>(pItem)->GetCaseMap());
 
                 pItem = aItemSet.GetItem( SID_ATTR_CHAR_EMPHASISMARK );
                 if ( pItem )
