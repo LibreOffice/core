LibreOffice/core

commit 7347482cc360ad3dc9ed77047441c2e30476fed3

hidpi: Make many places Hi-DPI aware.
This also introduces a getter for the mnDPIScaleFactor variable.

Change-Id: I02ba6858fb1842f911d62976f4c54afc3bfa337f

svx/source/tbxctrls/tbunocontroller.cxx
@@ -85,7 +85,7 @@ SvxFontSizeBox_Impl::SvxFontSizeBox_Impl(
     FontSizeBox( _pParent, WinBits( WB_DROPDOWN ) ),
 
     m_pCtrl             ( &_rCtrl ),
-    m_aLogicalSize      ( 30,100 ),
+    m_aLogicalSize      ( 20,100 ),
     m_bRelease          ( true ),
     m_xDispatchProvider ( _rDispatchProvider ),
     m_xFrame            ( _xFrame )

cui/source/tabpages/align.cxx
@@ -322,6 +322,18 @@ void AlignmentTabPage::InitVsRefEgde()
 
     ResId aResId( IL_LOCK_BMPS, CUI_MGR() );
     ImageList aImageList( aResId );
+
+    if( GetDPIScaleFactor() > 1 )
+    {
+        for (short i = 0; i < aImageList.GetImageCount(); i++)
+        {
+            OUString rImageName = aImageList.GetImageName(i);
+            BitmapEx b = aImageList.GetImage(rImageName).GetBitmapEx();
+            b.Scale(GetDPIScaleFactor(), GetDPIScaleFactor(), BMP_SCALE_FAST);
+            aImageList.ReplaceImage(rImageName, Image(b));
+        }
+    }
+
     Size aItemSize( aImageList.GetImage( IID_BOTTOMLOCK ).GetSizePixel() );
 
     m_pVsRefEdge->Clear();

cui/source/tabpages/border.cxx
@@ -87,9 +87,6 @@ SvxBorderTabPage::SvxBorderTabPage(Window* pParent, const SfxItemSet& rCoreAttrs
 
 ,
 
-
-
-
         aShadowImgLst( CUI_RES(IL_SDW_BITMAPS)),
         aBorderImgLst( CUI_RES(IL_PRE_BITMAPS)),
         nMinValue(0),
@@ -130,6 +127,25 @@ SvxBorderTabPage::SvxBorderTabPage(Window* pParent, const SfxItemSet& rCoreAttrs
     get(m_pPropertiesFrame, "properties");
     get(m_pMergeWithNextCB, "mergewithnext");
     get(m_pMergeAdjacentBordersCB, "mergeadjacent");
+
+    if ( GetDPIScaleFactor() > 1 )
+    {
+        for (short i = 0; i < aBorderImgLst.GetImageCount(); i++)
+        {
+            OUString rImageName = aBorderImgLst.GetImageName(i);
+            BitmapEx b = aBorderImgLst.GetImage(rImageName).GetBitmapEx();
+            b.Scale(GetDPIScaleFactor(), GetDPIScaleFactor(), BMP_SCALE_FAST);
+            aBorderImgLst.ReplaceImage(rImageName, Image(b));
+        }
+
+        for (short i = 0; i < aShadowImgLst.GetImageCount(); i++)
+        {
+            OUString rImageName = aShadowImgLst.GetImageName(i);
+            BitmapEx b = aShadowImgLst.GetImage(rImageName).GetBitmapEx();
+            b.Scale(GetDPIScaleFactor(), GetDPIScaleFactor(), BMP_SCALE_FAST);
+            aShadowImgLst.ReplaceImage(rImageName, Image(b));
+        }
+    }
 
     // this page needs ExchangeSupport
     SetExchangeSupport();

include/vcl/outdev.hxx
@@ -559,6 +559,8 @@ public:
     */
     bool                supportsOperation( OutDevSupportType ) const;
 
+    sal_Int32           GetDPIScaleFactor() const { return mnDPIScaleFactor; }
+
     vcl::PDFWriterImpl* GetPDFWriter() const { return mpPDFWriter; }
 
     void                SetExtOutDevData( vcl::ExtOutDevData* pExtOutDevData ) { mpExtOutDevData = pExtOutDevData; }

sd/source/ui/sidebar/LayoutMenu.cxx
@@ -650,6 +650,9 @@ void LayoutMenu::Fill (void)
         {
             BitmapEx aBmp(SdResId(pInfo->mnBmpResId));
 
+            if (GetDPIScaleFactor() > 1)
+                aBmp.Scale(GetDPIScaleFactor(), GetDPIScaleFactor(), BMP_SCALE_FAST);
+
             if (bRightToLeft && (WritingMode_TB_RL != pInfo->meWritingMode))
                 aBmp.Mirror (BMP_MIRROR_HORZ);
 

sd/uiconfig/sdraw/statusbar/statusbar.xml
@@ -19,7 +19,7 @@
 -->
 <statusbar:statusbar xmlns:statusbar="http://openoffice.org/2001/statusbar" xmlns:xlink="http://www.w3.org/1999/xlink">
  <statusbar:statusbaritem xlink:href=".uno:Context" statusbar:align="left" statusbar:autosize="true" statusbar:width="184"/>
- <statusbar:statusbaritem xlink:href=".uno:Size" statusbar:align="center" statusbar:ownerdraw="true" statusbar:width="208"/>
+ <statusbar:statusbaritem xlink:href=".uno:Size" statusbar:align="center" statusbar:ownerdraw="true" statusbar:width="400"/>
  <statusbar:statusbaritem xlink:href=".uno:ModifiedStatus" statusbar:align="center" statusbar:ownerdraw="true" statusbar:width="9"/>
  <statusbar:statusbaritem xlink:href=".uno:Signature" statusbar:align="center" statusbar:ownerdraw="true" statusbar:width="16"/>
  <statusbar:statusbaritem xlink:href=".uno:PageStatus" statusbar:align="left" statusbar:width="124"/>

sd/uiconfig/simpress/statusbar/statusbar.xml
@@ -19,7 +19,7 @@
 -->
 <statusbar:statusbar xmlns:statusbar="http://openoffice.org/2001/statusbar" xmlns:xlink="http://www.w3.org/1999/xlink">
  <statusbar:statusbaritem xlink:href=".uno:Context" statusbar:align="left" statusbar:autosize="true" statusbar:width="184"/>
- <statusbar:statusbaritem xlink:href=".uno:Size" statusbar:align="center" statusbar:ownerdraw="true" statusbar:width="208"/>
+ <statusbar:statusbaritem xlink:href=".uno:Size" statusbar:align="center" statusbar:ownerdraw="true" statusbar:width="400"/>
  <statusbar:statusbaritem xlink:href=".uno:ModifiedStatus" statusbar:align="center" statusbar:ownerdraw="true" statusbar:width="9"/>
  <statusbar:statusbaritem xlink:href=".uno:Signature" statusbar:align="center" statusbar:ownerdraw="true" statusbar:width="16"/>
  <statusbar:statusbaritem xlink:href=".uno:PageStatus" statusbar:align="left" statusbar:width="124"/>

svx/source/dialog/dialcontrol.cxx
@@ -449,8 +449,13 @@ void DialControl::Init( const Size& rWinSize, const Font& rWinFont )
 
 void DialControl::Init( const Size& rWinSize )
 {
+    //hidpi TODO: GetDefaultFont() picks a font size too small, so fix it here.
+    Font aDefaultSize = GetFont();
+
     Font aFont( OutputDevice::GetDefaultFont(
         DEFAULTFONT_UI_SANS, Application::GetSettings().GetUILanguageTag().getLanguageType(), DEFAULTFONT_FLAGS_ONLYONE ) );
+
+    aFont.SetHeight(aDefaultSize.GetHeight());
     Init( rWinSize, aFont );
 }
 

commit 8138c07b984ba1654483b4dc3488820ca4deaaca

Resolves: fdo#73051 font size menubar widget too narrow
regression since 7347482cc360ad3dc9ed77047441c2e30476fed3

Change-Id: I44e1709f5f391c4ce8b5eaddf02da6fd9652d030

svx/source/tbxctrls/tbunocontroller.cxx
@@ -124,7 +124,7 @@ SvxFontSizeBox_Impl::SvxFontSizeBox_Impl(
     FontSizeBox( _pParent, WinBits( WB_DROPDOWN ) ),
 
     m_pCtrl             ( &_rCtrl ),
-    m_aLogicalSize      ( 20,100 ),
+    m_aLogicalSize      ( 30,100 ),
     m_bRelease          ( true ),
     m_xDispatchProvider ( _rDispatchProvider ),
     m_xFrame            ( _xFrame )