5423bd314ada27285cb524f15ae24836aa0af7a5
Vinaya Mandke <vinaya.mandke@synerzip.com>
2014-01-29 12:40:01 +0530

    fdo#74115 Fix for DOCX GradientFill for Chart Wall
    Fixed import and export for chart wall Gradient Fill in DOCX
    Added UT for the same.
    
    Change-Id: Ie6caa2b238aeb70f7225145da8c5c78003e73002

--- chart2/qa/extras/chart2export.cxx
+++ chart2/qa/extras/chart2export.cxx
@@ -42,6 +42,7 @@ public:
     void testAreaChartLoad();
     void testUpDownBars();
     void testDoughnutChart();
+    void testFdo74115WallGradientFill();
 
     CPPUNIT_TEST_SUITE(Chart2ExportTest);
     CPPUNIT_TEST(test);
@@ -56,6 +57,7 @@ public:
     CPPUNIT_TEST(testAreaChartLoad);
     CPPUNIT_TEST(testUpDownBars);
     CPPUNIT_TEST(testDoughnutChart);
+    CPPUNIT_TEST(testFdo74115WallGradientFill);
     CPPUNIT_TEST_SUITE_END();
 
 protected:
@@ -160,6 +162,7 @@ xmlNodeSetPtr Chart2ExportTest::getXPathNode(xmlDocPtr pXmlDoc, const OString& r
     xmlXPathRegisterNs(pXmlXpathCtx, BAD_CAST("w"), BAD_CAST("http://schemas.openxmlformats.org/wordprocessingml/2006/main"));
     xmlXPathRegisterNs(pXmlXpathCtx, BAD_CAST("v"), BAD_CAST("urn:schemas-microsoft-com:vml"));
     xmlXPathRegisterNs(pXmlXpathCtx, BAD_CAST("c"), BAD_CAST("http://schemas.openxmlformats.org/drawingml/2006/chart"));
+    xmlXPathRegisterNs(pXmlXpathCtx, BAD_CAST("a"), BAD_CAST("http://schemas.openxmlformats.org/drawingml/2006/main"));
     xmlXPathObjectPtr pXmlXpathObj = xmlXPathEvalExpression(BAD_CAST(rXPath.getStr()), pXmlXpathCtx);
     return pXmlXpathObj->nodesetval;
 }
@@ -531,6 +534,14 @@ void Chart2ExportTest::testDoughnutChart()
     CPPUNIT_ASSERT(pXmlDoc);
 
     assertXPath(pXmlDoc, "/c:chartSpace/c:chart/c:plotArea/c:doughnutChart", "1");
+}
+
+void Chart2ExportTest::testFdo74115WallGradientFill()
+{
+    load("/chart2/qa/extras/data/docx/", "fdo74115_WallGradientFill.docx");
+    xmlDocPtr pXmlDoc = parseExport("word/charts/chart", "Office Open XML Text");
+    CPPUNIT_ASSERT(pXmlDoc);
+    assertXPath(pXmlDoc, "/c:chartSpace/c:chart/c:plotArea/c:spPr/a:gradFill");
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(Chart2ExportTest);
