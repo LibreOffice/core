9740a2af05e1042d737c62b42a7028889e2ceaf3
Norbert Thiebaud <nthiebaud@gmail.com>
2014-02-01 14:42:02 -0600

    coverity#707731 : Uninitialized scalar field
    Change-Id: I048ad8477df070dd77a36620ea239fcd8bd9da60

--- dbaccess/source/filter/xml/xmlfilter.cxx
+++ dbaccess/source/filter/xml/xmlfilter.cxx
@@ -346,8 +346,9 @@ sal_Int32 ReadThroughComponent(
 // - ODBFilter -
 
 ODBFilter::ODBFilter( const uno::Reference< XComponentContext >& _rxContext )
-    :SvXMLImport(_rxContext, getImplementationName_Static())
-    ,m_bNewFormat(false)
+    : SvXMLImport(_rxContext, getImplementationName_Static())
+    , m_nPreviewMode(0)
+    , m_bNewFormat(false)
 {
 
     GetMM100UnitConverter().SetCoreMeasureUnit(util::MeasureUnit::MM_10TH);
