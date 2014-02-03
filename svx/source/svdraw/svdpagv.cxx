LibreOffice/core

commit 8af09bf33291df2fb2bfbbd6e42f9bf074fcc4fc

n#777699: Clip the objects to the pagewe are painting
Change-Id: I78ac7de5346a2adea9a2c6cf3cbb0321e05d79dc

svx/source/svdraw/svdpagv.cxx
@@ -346,7 +346,7 @@ void SdrPageView::setPreparedPageWindow(SdrPageWindow* pKnownTarget)
     mpPreparedPageWindow = pKnownTarget;
 }
 
-void SdrPageView::DrawLayer(SdrLayerID nID, OutputDevice* pGivenTarget, sdr::contact::ViewObjectContactRedirector* pRedirector) const
+void SdrPageView::DrawLayer(SdrLayerID nID, OutputDevice* pGivenTarget, sdr::contact::ViewObjectContactRedirector* pRedirector, const Rectangle& rRect) const
 {
     if(GetPage())
     {
@@ -374,7 +374,10 @@ void SdrPageView::DrawLayer(SdrLayerID nID, OutputDevice* pGivenTarget, sdr::con
                     // Copy existing paint region to use the same as prepared in BeginDrawLayer
                     SdrPaintWindow& rExistingPaintWindow = pPreparedTarget->GetPaintWindow();
                     const Region& rExistingRegion = rExistingPaintWindow.GetRedrawRegion();
-                    aTemporaryPaintWindow.SetRedrawRegion(rExistingRegion);
+                    if ( rRect.IsEmpty() )
+                        aTemporaryPaintWindow.SetRedrawRegion(rExistingRegion);
+                    else
+                        aTemporaryPaintWindow.SetRedrawRegion(Region(rRect));
 
                     // patch the ExistingPageWindow
                     pPreparedTarget->patchPaintWindow(aTemporaryPaintWindow);

svx/inc/svx/svdpagv.hxx
@@ -181,7 +181,8 @@ public:
     // write access to mpPreparedPageWindow
     void setPreparedPageWindow(SdrPageWindow* pKnownTarget);
 
-    void DrawLayer(SdrLayerID nID, OutputDevice* pGivenTarget = 0, sdr::contact::ViewObjectContactRedirector* pRedirector = 0L) const;
+    void DrawLayer(SdrLayerID nID, OutputDevice* pGivenTarget = 0, sdr::contact::ViewObjectContactRedirector* pRedirector = 0L,
+                   const Rectangle& rRect =  Rectangle()) const;
     void DrawPageViewGrid(OutputDevice& rOut, const Rectangle& rRect, Color aColor = Color( COL_BLACK ) );
 
     Rectangle GetPageRect() const;

sw/source/core/layout/paintfrm.cxx
@@ -3122,7 +3122,7 @@ SwRootFrm::Paint(SwRect const& rRect, SwPrintData const*const pPrintData) const
                     const IDocumentDrawModelAccess* pIDDMA = pSh->getIDocumentDrawModelAccess();
                     pSh->Imp()->PaintLayer( pIDDMA->GetHellId(),
                                             pPrintData,
-                                            aPaintRect,
+                                            pPage->Frm(),
                                             &aPageBackgrdColor,
                                             (pPage->IsRightToLeft() ? true : false),
                                             &aSwRedirector );
@@ -3166,7 +3166,7 @@ SwRootFrm::Paint(SwRect const& rRect, SwPrintData const*const pPrintData) const
                     // OD 09.12.2002 #103045# - add 4th parameter for horizontal text direction.
                     pSh->Imp()->PaintLayer( pSh->GetDoc()->GetHeavenId(),
                                             pPrintData,
-                                            aPaintRect,
+                                            pPage->Frm(),
                                             &aPageBackgrdColor,
                                             (pPage->IsRightToLeft() ? true : false),
                                             &aSwRedirector );

sw/source/core/view/vdraw.cxx
@@ -97,7 +97,7 @@ void SwViewImp::UnlockPaint()
 
 void SwViewImp::PaintLayer( const SdrLayerID _nLayerID,
                             SwPrintData const*const pPrintData,
-                            const SwRect& ,
+                            const SwRect& aPaintRect,
                             const Color* _pPageBackgrdColor,
                             const bool _bIsPageRightToLeft,
                             sdr::contact::ViewObjectContactRedirector* pRedirector ) const
@@ -150,7 +150,7 @@ void SwViewImp::PaintLayer( const SdrLayerID _nLayerID,
             SdrView &rSdrView = const_cast< SdrView & >(GetPageView()->GetView());
             rSdrView.setHideDraw( !pPrintData->IsPrintDraw() );
         }
-        GetPageView()->DrawLayer( _nLayerID, pOutDev, pRedirector );
+        GetPageView()->DrawLayer( _nLayerID, pOutDev, pRedirector, aPaintRect.SVRect() );
         pOutDev->Pop();
 
         // reset background color of the outliner & default horiz. text dir.

commit 66f8b54931f193bdc844c6bca388d9bde98779cd

fdo#74435: SdrPageView::DrawLayer(): hack to avoid spuriously visible images
In the TestBook2.odt of fdo#73300 it happens that at some particular
points when scrolling down, the
ViewObjectContactOfPageHierarchy::getPrimitive2DSequenceHierarchy()
will determine that > 200 images are visible; most likely this is due to
an empty ObjectContactOfPageView::maViewInformation2D::getViewport().

Loading this many images may need more RAM than is available on 32bit
platforms.

That appears to happen only (but not always) if somehow (not sure
how exactly) the rectangle passed to SdrPageView::DrawLayer() and the
existing GetRedrawRegion() do not overlap at all.

(regression from commit 8af09bf33291df2fb2bfbbd6e42f9bf074fcc4fc)

Change-Id: Iafc595241df05204bccec1f2336014fc471b6602

svx/source/svdraw/svdpagv.cxx
@@ -350,7 +350,16 @@ void SdrPageView::DrawLayer(SdrLayerID nID, OutputDevice* pGivenTarget, sdr::con
                     // Copy existing paint region to use the same as prepared in BeginDrawLayer
                     SdrPaintWindow& rExistingPaintWindow = pPreparedTarget->GetPaintWindow();
                     const Region& rExistingRegion = rExistingPaintWindow.GetRedrawRegion();
-                    if ( rRect.IsEmpty() )
+                    bool bUseRect(false);
+                    if (!rRect.IsEmpty())
+                    {
+                        Region r(rExistingRegion);
+                        r.Intersect(rRect);
+                        // fdo#74435: FIXME: visibility check broken if empty
+                        if (!r.IsEmpty())
+                            bUseRect = true;
+                    }
+                    if (!bUseRect)
                         aTemporaryPaintWindow.SetRedrawRegion(rExistingRegion);
                     else
                         aTemporaryPaintWindow.SetRedrawRegion(Region(rRect));