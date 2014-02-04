LibreOffice/core

commit 70c61a549184cf8611f7890ac31b0ba4c460cdec

resolved fdo#70275 write correct office:string-value if text format applied
Change-Id: I52e5135139fdf88abe380b6e5ae60e2afb582289
Reviewed-on: https://gerrit.libreoffice.org/7555
Reviewed-by: David Tardon <dtardon@redhat.com>
Tested-by: David Tardon <dtardon@redhat.com>

sc/source/filter/xml/xmlexprt.cxx
@@ -2882,13 +2882,15 @@ void ScXMLExport::WriteCell(ScMyCell& aCell, sal_Int32 nEqualCellCount)
             break;
         case table::CellContentType_TEXT :
             {
-                GetCellText(aCell, aCellPos);
                 OUString sFormula(lcl_GetRawString(pDoc, aCellPos));
+                if (aCell.maBaseCell.isEmpty())
+                    aCell.maBaseCell.assign( *pDoc, aCellPos);
+                OUString sCellString = aCell.maBaseCell.getString(pDoc);
                 GetNumberFormatAttributesExportHelper()->SetNumberFormatAttributes(
-                        sFormula, aCell.sStringValue, true, true);
+                        sCellString, sFormula, true, true);
                 if( getDefaultVersion() > SvtSaveOptions::ODFVER_012 )
                     GetNumberFormatAttributesExportHelper()->SetNumberFormatAttributes(
-                            sFormula, aCell.sStringValue, false, true, XML_NAMESPACE_CALC_EXT);
+                            sCellString, sFormula, false, true, XML_NAMESPACE_CALC_EXT);
             }
             break;
         case table::CellContentType_FORMULA :

commit b3f4b0cf65409807e230680f4fa5ba81c423831e

resolved fdo#74493 do not trust aCell.maBaseCell
Regression introduced with 70c61a549184cf8611f7890ac31b0ba4c460cdec on
4-1 and 68a753b6a36560eb83ba6ebe4fe9897a8759d265 on 4-1-5

Apparently aCell.maBaseCell does not always follow the current cell
position, so do not trust it. Always explicitly obtain the correct cell.

Change-Id: Ib88de44a326be75c13d37538b8ae56d265b60ee5

sc/source/filter/xml/xmlexprt.cxx
@@ -2883,8 +2883,7 @@ void ScXMLExport::WriteCell(ScMyCell& aCell, sal_Int32 nEqualCellCount)
         case table::CellContentType_TEXT :
             {
                 OUString sFormula(lcl_GetRawString(pDoc, aCellPos));
-                if (aCell.maBaseCell.isEmpty())
-                    aCell.maBaseCell.assign( *pDoc, aCellPos);
+                aCell.maBaseCell.assign( *pDoc, aCellPos);
                 OUString sCellString = aCell.maBaseCell.getString(pDoc);
                 GetNumberFormatAttributesExportHelper()->SetNumberFormatAttributes(
                         sCellString, sFormula, true, true);