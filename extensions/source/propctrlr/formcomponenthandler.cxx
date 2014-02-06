eb07ab05c3b8774ab2b2059befef0eadf61c97d6
Caolán McNamara <caolanm@redhat.com>
2014-02-06 13:25:44 +0000

    coverity#707771 coverity gold, Uninitialized m_refCount
    Change-Id: I98398a3cd7a86a5f31c711f7f1cff0d467a1d2e5

--- extensions/source/propctrlr/formcomponenthandler.cxx
+++ extensions/source/propctrlr/formcomponenthandler.cxx
@@ -2986,7 +2986,8 @@ namespace pcr
         {
         protected:
             SQLCommandPropertyUI( const Reference< XPropertySet >& _rxObject )
-                :m_xObject( _rxObject )
+                : m_xObject(_rxObject)
+                , m_refCount(0)
             {
                 if ( !m_xObject.is() )
                     throw NullPointerException();
