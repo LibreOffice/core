f0c24877ed7cc3046bc69deb46b0cf7e2b919bcc
Julien Nabet <serval2412@yahoo.fr>
2014-02-06 21:23:57 +0100

    Typo: chek -> check
    Change-Id: I979956276a9d2d76604576d8b92e8f48a9f46c74

--- qadevOOo/tests/java/ifc/sdb/_XSingleSelectQueryComposer.java
+++ qadevOOo/tests/java/ifc/sdb/_XSingleSelectQueryComposer.java
@@ -151,7 +151,7 @@ public class _XSingleSelectQueryComposer extends MultiMethodTest {
 
     /**
     * At first the object relation <code>xProp</code> was set as parameter.
-    * Relation <code>xQueryAna</code> was used to chek if realtion
+    * Relation <code>xQueryAna</code> was used to check if realtion
     * <code>colName</code> was found.
     * Second an empty <code>XPropertySet</code> was used as parameter. A
     * <code>com.sun.star.sdbc.SQLException</code> must be thrown.
@@ -195,7 +195,7 @@ public class _XSingleSelectQueryComposer extends MultiMethodTest {
 
     /**
     * At first the object relation <code>xProp</code> was used as parameter.
-    * Relation <code>xQueryAna</code> was used to chek if realtion
+    * Relation <code>xQueryAna</code> was used to check if realtion
     * <code>colName</code> was found.
     * Second an empty <code>XPropertySet</code> was used as parameter. An
     * <code>com.sun.star.sdbc.SQLException</code> must be thrown.
@@ -299,7 +299,7 @@ public class _XSingleSelectQueryComposer extends MultiMethodTest {
 
     /**
     * First object relation <code>xProp</code> was used as parameter. Relation
-    * <code>xQueryAna</code> was used to chek if realtion <code>colName</code>
+    * <code>xQueryAna</code> was used to check if realtion <code>colName</code>
     * was found.
     * Second an empty <code>XPropertySet</code> was given as parameter. An
     * <code>com.sun.star.sdbc.SQLException</code> must be thrown.
@@ -331,7 +331,7 @@ public class _XSingleSelectQueryComposer extends MultiMethodTest {
 
     /**
     * First object relation <code>xProp</code> was set as parameter. Relation
-    * <code>xQueryAna</code> was used to chek if realtion <code>colName</code>
+    * <code>xQueryAna</code> was used to check if realtion <code>colName</code>
     * was found.
     * Second an empty <code>XPropertySet</code> was given as parameter. An
     * <code>com.sun.star.sdbc.SQLException</code> must be thrown.
