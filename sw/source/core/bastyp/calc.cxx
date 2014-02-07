LibreOffice/core

commit fa469b2e00d83459faebe4c1fcb3ea1aac5fb20d

convert sw/source/ui/app/*.cxx from String to OUString
Change-Id: I08cb425ae4be64116c3f88dad73d330a953af77a

sw/source/ui/app/appenv.cxx
@@ -73,41 +73,41 @@
 #define ENV_INSERT      RET_USER
 
 // Function used for labels and envelopes in applab.cxx and appenv.cxx
-String InsertLabEnvText( SwWrtShell& rSh, SwFldMgr& rFldMgr, const String& rText )
+OUString InsertLabEnvText( SwWrtShell& rSh, SwFldMgr& rFldMgr, const OUString& rText )
 {
-    String sRet;
-    String aText(comphelper::string::remove(rText, '\r'));
+    OUString sRet;
+    OUString aText(comphelper::string::remove(rText, '\r'));
 
     sal_Int32 nTokenPos = 0;
     while( -1 != nTokenPos )
     {
-        String aLine = aText.GetToken( 0, '\n', nTokenPos );
-        while ( aLine.Len() )
+        OUString aLine = aText.getToken( 0, '\n', nTokenPos );
+        while ( !aLine.isEmpty() )
         {
-            String sTmpText;
+            OUString sTmpText;
             bool bField = false;
 
-            sal_uInt16 nPos = aLine.Search( '<' );
-            if ( nPos )
+            sal_Int32 nPos = aLine.indexOf( '<' );
+            if ( nPos != -1)
             {
-                sTmpText = aLine.Copy( 0, nPos );
-                aLine.Erase( 0, nPos );
+                sTmpText = aLine.copy( 0, nPos );
+                aLine = aLine.copy( nPos );
             }
             else
             {
-                nPos = aLine.Search( '>' );
-                if ( nPos == STRING_NOTFOUND )
+                nPos = aLine.indexOf( '>' );
+                if ( nPos == -1 )
                 {
                     sTmpText = aLine;
-                    aLine.Erase();
+                    aLine = "";
                 }
                 else
                 {
-                    sTmpText = aLine.Copy( 0, nPos + 1);
-                    aLine.Erase( 0, nPos + 1);
+                    sTmpText = aLine.copy( 0, nPos + 1);
+                    aLine = aLine.copy( nPos + 1);
 
                     // Database fields must contain at least 3 points!
-                    String sDBName( sTmpText.Copy( 1, sTmpText.Len() - 2));
+                    OUString sDBName( sTmpText.copy( 1, sTmpText.getLength() - 2));
                     sal_uInt16 nCnt = comphelper::string::getTokenCount(sDBName, '.');
                     if (nCnt >= 3)
                     {
@@ -160,7 +160,7 @@ void SwModule::InsertEnv( SfxRequest& rReq )
     pNewView->AttrChangedNotify( &pNewView->GetWrtShell() ); // so that SelectShell is being called
     pSh = pNewView->GetWrtShellPtr();
 
-    String aTmp( SW_RES(STR_ENV_TITLE) );
+    OUString aTmp( SW_RES(STR_ENV_TITLE) );
     aTmp += OUString::number( ++nTitleNo );
     xDocSh->SetTitle( aTmp );
 

sw/source/ui/app/apphdl.cxx
@@ -181,10 +181,10 @@ void SwModule::StateOther(SfxItemSet &rSet)
     }
 }
 
-static SwView* lcl_LoadDoc(SwView* pView, const String& rURL)
+static SwView* lcl_LoadDoc(SwView* pView, const OUString& rURL)
 {
     SwView* pNewView = 0;
-    if(rURL.Len())
+    if(!rURL.isEmpty())
     {
         SfxStringItem aURL(SID_FILE_NAME, rURL);
         SfxStringItem aTargetFrameName( SID_TARGETNAME, OUString("_blank") );
@@ -347,11 +347,11 @@ void SwMailMergeWizardExecutor::ExecuteMailMergeWizard( const SfxItemSet * pArgs
                 m_pView->GetWrtShell().GetAllUsedDB( aDBNameList, &aAllDBNames );
                 if(!aDBNameList.empty())
                 {
-                    String sDBName(aDBNameList[0]);
+                    OUString sDBName(aDBNameList[0]);
                     SwDBData aDBData;
-                    aDBData.sDataSource = sDBName.GetToken(0, DB_DELIM);
-                    aDBData.sCommand = sDBName.GetToken(1, DB_DELIM);
-                    aDBData.nCommandType = sDBName.GetToken(2, DB_DELIM ).ToInt32();
+                    aDBData.sDataSource = sDBName.getToken(0, DB_DELIM);
+                    aDBData.sCommand = sDBName.getToken(1, DB_DELIM);
+                    aDBData.nCommandType = sDBName.getToken(2, DB_DELIM ).toInt32();
                     //set the currently used database for the wizard
                     m_pMMConfig->SetCurrentDBData( aDBData );
                 }

sw/source/ui/app/applab.cxx
@@ -76,7 +76,7 @@
 using namespace ::com::sun::star;
 
 // is in appenv.cxx
-extern String InsertLabEnvText( SwWrtShell& , SwFldMgr& , const String& );
+extern OUString InsertLabEnvText( SwWrtShell& , SwFldMgr& , const OUString& );
 
 const char MASTER_LABEL[] = "MasterLabel";
 
@@ -140,8 +140,8 @@ static const SwFrmFmt *lcl_InsertLabText( SwWrtShell& rSh, const SwLabItem& rIte
     rSh.SetTxtFmtColl( rSh.GetTxtCollFromPool( RES_POOLCOLL_STANDARD ) );
 
     // If applicable "next dataset"
-    String sDBName;
-    if( (!rItem.bSynchron || !(nCol|nRow)) && (sDBName = InsertLabEnvText( rSh, rFldMgr, rItem.aWriting )).Len() && !bLast )
+    OUString sDBName;
+    if( (!rItem.bSynchron || !(nCol|nRow)) && !(sDBName = InsertLabEnvText( rSh, rFldMgr, rItem.aWriting )).isEmpty() && !bLast )
     {
         sDBName = comphelper::string::setToken(sDBName, 3, DB_DELIM, "True");
         SwInsertFld_Data aData(TYP_DBNEXTSETFLD, 0, sDBName, aEmptyStr, 0, &rSh );
@@ -198,10 +198,10 @@ void SwModule::InsertLab(SfxRequest& rReq, sal_Bool bLabel)
         pNewView->AttrChangedNotify( &pNewView->GetWrtShell() );// So that SelectShell is being called.
 
         // Set document title
-        String aTmp;
+        OUString aTmp;
         if(bLabel)
         {
-            aTmp = String(SW_RES( STR_LAB_TITLE));
+            aTmp = SW_RES( STR_LAB_TITLE);
             aTmp += OUString::number(++nLabelTitleNo );
         }
         else

sw/source/ui/app/docsh.cxx
@@ -144,7 +144,7 @@ Reader* SwDocShell::StartConvertFrom(SfxMedium& rMedium, SwReader** ppRdr,
         }
         return 0;
     }
-    String aFileName( rMedium.GetName() );
+    OUString aFileName( rMedium.GetName() );
     SwRead pRead = SwReaderWriter::GetReader( pFlt->GetUserData() );
     if( !pRead )
         return 0;
@@ -162,7 +162,7 @@ Reader* SwDocShell::StartConvertFrom(SfxMedium& rMedium, SwReader** ppRdr,
         return 0;
 
     // Check password
-    String aPasswd;
+    OUString aPasswd;
     if ((*ppRdr)->NeedsPasswd( *pRead ))
     {
         if(!bAPICall)
@@ -655,7 +655,7 @@ sal_Bool SwDocShell::ConvertTo( SfxMedium& rMedium )
         ( pWrtShell || !::lcl_GetSourceView( this ) ))
     {
         SwAsciiOptions aOpt;
-        String sItemOpt;
+        OUString sItemOpt;
         const SfxItemSet* pSet;
         const SfxPoolItem* pItem;
         if( 0 != ( pSet = rMedium.GetItemSet() ) )
@@ -664,7 +664,7 @@ sal_Bool SwDocShell::ConvertTo( SfxMedium& rMedium )
                                                     sal_True, &pItem ) )
                 sItemOpt = ((const SfxStringItem*)pItem)->GetValue();
         }
-        if(sItemOpt.Len())
+        if(!sItemOpt.isEmpty())
             aOpt.ReadUserData( sItemOpt );
 
         xWriter->SetAsciiOptions( aOpt );

sw/source/ui/app/docsh2.cxx
@@ -118,7 +118,8 @@ using namespace ::com::sun::star::lang;
 using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star;
 using namespace ::sfx2;
-extern bool FindPhyStyle( SwDoc& , const String& , SfxStyleFamily );
+
+extern bool FindPhyStyle( SwDoc& , const OUString& , SfxStyleFamily );
 
 // create DocInfo (virtual)
 SfxDocumentInfoDialog* SwDocShell::CreateDocumentInfoDialog(
@@ -450,7 +451,7 @@ void SwDocShell::Execute(SfxRequest& rReq)
             break;
         case SID_TEMPLATE_LOAD:
             {
-                String aFileName;
+                OUString aFileName;
                 static sal_Bool bText = sal_True;
                 static sal_Bool bFrame = sal_False;
                 static sal_Bool bPage =  sal_False;
@@ -480,7 +481,7 @@ void SwDocShell::Execute(SfxRequest& rReq)
                     }
                 }
 
-                if ( !aFileName.Len() )
+                if ( aFileName.isEmpty() )
                 {
                     SvtPathOptions aPathOpt;
                     SfxNewFileDialog* pNewFileDlg =
@@ -507,7 +508,7 @@ void SwDocShell::Execute(SfxRequest& rReq)
                                 ( pFlt->GetUserData() == "CXML" ||
                                   pFlt->GetUserData() == "CXMLV" ) )
                             {
-                                const String sWild = pFlt->GetWildcard().getGlob();
+                                const OUString sWild = pFlt->GetWildcard().getGlob();
                                 xFltMgr->appendFilter( pFlt->GetUIName(), sWild );
                             }
                             pFlt = aIter.Next();
@@ -520,7 +521,7 @@ void SwDocShell::Execute(SfxRequest& rReq)
                         // make sure the default file format is also available
                         if(bWeb)
                         {
-                            const String sWild = pOwnFlt->GetWildcard().getGlob();
+                            const OUString sWild = pOwnFlt->GetWildcard().getGlob();
                             xFltMgr->appendFilter( pOwnFlt->GetUIName(), sWild );
                         }
 
@@ -551,7 +552,7 @@ void SwDocShell::Execute(SfxRequest& rReq)
                     delete pNewFileDlg;
                 }
 
-                if( aFileName.Len() )
+                if( !aFileName.isEmpty() )
                 {
                     SwgReaderOption aOpt;
                     aOpt.SetTxtFmts(    bText = (0 != (nFlags&SFX_LOAD_TEXT_STYLES) ));
@@ -610,7 +611,7 @@ void SwDocShell::Execute(SfxRequest& rReq)
                         {
                             break;
                         }
-                        String sPath = aDlgHelper.GetPath();
+                        OUString sPath = aDlgHelper.GetPath();
                         SfxStringItem aName(SID_FILE_NAME, sPath);
                         SfxStringItem aFilter(SID_FILTER_NAME, pHtmlFlt->GetName());
                         const SfxBoolItem* pBool = (const SfxBoolItem*)
@@ -697,7 +698,7 @@ void SwDocShell::Execute(SfxRequest& rReq)
                 {
                     WriterRef xWrt;
                     // mba: looks as if relative URLs don't make sense here
-                    ::GetRTFWriter( aEmptyStr, String(), xWrt );
+                    ::GetRTFWriter( aEmptyStr, OUString(), xWrt );
                     SvMemoryStream *pStrm = new SvMemoryStream();
                     pStrm->SetBufferSize( 16348 );
                     SwWriter aWrt( *pStrm, *pSmryDoc );
@@ -739,7 +740,7 @@ void SwDocShell::Execute(SfxRequest& rReq)
                     SwView      *pCurrView = (SwView*) pFrame->GetViewShell();
 
                     // Set document's title
-                    String aTmp( SW_RES(STR_ABSTRACT_TITLE) );
+                    OUString aTmp( SW_RES(STR_ABSTRACT_TITLE) );
                     aTmp += GetTitle();
                     xDocSh->SetTitle( aTmp );
                     pCurrView->GetWrtShell().SetNewDoc();
@@ -851,7 +852,7 @@ void SwDocShell::Execute(SfxRequest& rReq)
                 bool bCreateByOutlineLevel = false;
                 sal_Int32  nTemplateOutlineLevel = 0;
 
-                String aFileName, aTemplateName;
+                OUString aFileName, aTemplateName;
                 if( pArgs && SFX_ITEM_SET == pArgs->GetItemState( nWhich, sal_False, &pItem ) )
                 {
                     aFileName = ((const SfxStringItem*)pItem)->GetValue();
@@ -860,7 +861,7 @@ void SwDocShell::Execute(SfxRequest& rReq)
                         aTemplateName = pTemplItem->GetValue();
                 }
                 bool bError = false;
-                if ( !aFileName.Len() )
+                if ( aFileName.isEmpty() )
                 {
                     FileDialogHelper aDlgHelper( TemplateDescription::FILESAVE_AUTOEXTENSION_TEMPLATE, 0 );
 
@@ -925,7 +926,7 @@ void SwDocShell::Execute(SfxRequest& rReq)
                     if( pFlt )
                     {
                         uno::Reference<XFilterManager> xFltMgr(xFP, UNO_QUERY);
-                        const String sWild = pFlt->GetWildcard().getGlob();
+                        const OUString sWild = pFlt->GetWildcard().getGlob();
                         xFltMgr->appendFilter( pFlt->GetUIName(), sWild );
                         try
                         {
@@ -988,7 +989,7 @@ void SwDocShell::Execute(SfxRequest& rReq)
                             xCtrlAcc->setValue( ExtendedFilePickerElementIds::LISTBOX_TEMPLATE,
                                 ListboxControlActions::SET_SELECT_ITEM, aSelectPos );
                             xCtrlAcc->setLabel( ExtendedFilePickerElementIds::LISTBOX_TEMPLATE,
-                                                    String(SW_RES( STR_FDLG_TEMPLATE_NAME )));
+                                                    OUString(SW_RES( STR_FDLG_TEMPLATE_NAME )));
                         }
                         catch (const Exception&)
                         {
@@ -1019,17 +1020,17 @@ void SwDocShell::Execute(SfxRequest& rReq)
                                 bCreateByOutlineLevel = true;
                             }
 
-                            if ( aFileName.Len() )
+                            if ( !aFileName.isEmpty() )
                             {
                                 rReq.AppendItem( SfxStringItem( nWhich, aFileName ) );
-                                if( aTemplateName.Len() )
+                                if( !aTemplateName.isEmpty() )
                                     rReq.AppendItem( SfxStringItem( SID_TEMPLATE_NAME, aTemplateName ) );
                             }
                         }
                     }
                 }
 
-                if( aFileName.Len() )
+                if( !aFileName.isEmpty() )
                 {
                     if( PrepareClose( sal_False ) )
                     {
@@ -1044,7 +1045,7 @@ void SwDocShell::Execute(SfxRequest& rReq)
                         else
                         {
                             const SwTxtFmtColl* pSplitColl = 0;
-                            if ( aTemplateName.Len() )
+                            if ( !aTemplateName.isEmpty() )
                                 pSplitColl = pDoc->FindTxtFmtCollByName(aTemplateName);
                             bDone = bCreateHtml
                                 ? pDoc->GenerateHTMLDoc( aFileName, pSplitColl )
@@ -1268,7 +1269,7 @@ class SwReloadFromHtmlReader : public SwReader
 {
     public:
         SwReloadFromHtmlReader( SfxMedium& _rTmpMedium,
-                                const String& _rFilename,
+                                const OUString& _rFilename,
                                 SwDoc* _pDoc )
             : SwReader( _rTmpMedium, _rFilename, _pDoc )
         {
@@ -1306,7 +1307,7 @@ void SwDocShell::ReloadFromHtml( const OUString& rStreamName, SwSrcView* pSrcVie
                 {
                     // Notify the IDE
                     SfxUsrAnyItem aShellItem( SID_BASICIDE_ARG_DOCUMENT_MODEL, makeAny( GetModel() ) );
-                    String aLibName( pBasic->GetName() );
+                    OUString aLibName( pBasic->GetName() );
                     SfxStringItem aLibNameItem( SID_BASICIDE_ARG_LIBNAME, aLibName );
                     pSrcView->GetViewFrame()->GetDispatcher()->Execute(
                                             SID_BASICIDE_LIBREMOVED,
@@ -1340,7 +1341,7 @@ void SwDocShell::ReloadFromHtml( const OUString& rStreamName, SwSrcView* pSrcVie
     pDoc->set(IDocumentSettingAccess::BROWSE_MODE, bWasBrowseMode);
     pSrcView->SetPool(&GetPool());
 
-    const String& rMedname = GetMedium()->GetName();
+    const OUString& rMedname = GetMedium()->GetName();
 
     // The HTML template still has to be set
     SetHTMLTemplate( *GetDoc() );   //Styles from HTML.vor
@@ -1391,7 +1392,7 @@ sal_uLong SwDocShell::LoadStylesFromFile( const OUString& rURL,
     INetURLObject aURLObj( rURL );
 
     // Set filter:
-    String sFactory(OUString::createFromAscii(SwDocShell::Factory().GetShortName()));
+    OUString sFactory(OUString::createFromAscii(SwDocShell::Factory().GetShortName()));
     SfxFilterMatcher aMatcher( sFactory );
 
     // search for filter in WebDocShell, too
@@ -1400,7 +1401,7 @@ sal_uLong SwDocShell::LoadStylesFromFile( const OUString& rURL,
     aMatcher.DetectFilter( aMed, &pFlt, sal_False, sal_False );
     if(!pFlt)
     {
-        String sWebFactory(OUString::createFromAscii(SwWebDocShell::Factory().GetShortName()));
+        OUString sWebFactory(OUString::createFromAscii(SwWebDocShell::Factory().GetShortName()));
         SfxFilterMatcher aWebMatcher( sWebFactory );
         aWebMatcher.DetectFilter( aMed, &pFlt, sal_False, sal_False );
     }

sw/source/ui/app/docshini.cxx
@@ -134,7 +134,7 @@ sal_Bool SwDocShell::InitNew( const uno::Reference < embed::XStorage >& xStor )
         SwStdFontConfig* pStdFont = SW_MOD()->GetStdFontConfig();
         SfxPrinter* pPrt = pDoc->getPrinter( false );
 
-        String sEntry;
+        OUString sEntry;
         sal_uInt16 aFontWhich[] =
         {   RES_CHRATR_FONT,
             RES_CHRATR_CJK_FONT,
@@ -574,8 +574,7 @@ sal_Bool  SwDocShell::LoadFrom( SfxMedium& rMedium )
 
     do {        // middle check loop
         sal_uInt32 nErr = ERR_SWG_READ_ERROR;
-        String aStreamName;
-        aStreamName = OUString("styles.xml");
+        OUString aStreamName = "styles.xml";
         uno::Reference < container::XNameAccess > xAccess( rMedium.GetStorage(), uno::UNO_QUERY );
         if ( xAccess->hasByName( aStreamName ) && rMedium.GetStorage()->isStreamElement( aStreamName ) )
         {

sw/source/ui/app/docst.cxx
@@ -107,7 +107,7 @@ void  SwDocShell::StateStyleSheet(SfxItemSet& rSet, SwWrtShell* pSh)
     while (nWhich)
     {
         // determine current template to every family
-        String aName;
+        OUString aName;
         switch (nWhich)
         {
             case SID_STYLE_APPLY:
@@ -267,7 +267,7 @@ void SwDocShell::ExecStyleSheet( SfxRequest& rReq )
         {
             const sal_uInt16 nFamily = ((const SfxUInt16Item*)pItem)->GetValue();
 
-            String sName;
+            OUString sName;
             sal_uInt16 nMask = 0;
             if( SFX_ITEM_SET == pArgs->GetItemState( SID_STYLE_NEW,
                 sal_False, &pItem ))
@@ -275,7 +275,7 @@ void SwDocShell::ExecStyleSheet( SfxRequest& rReq )
             if( SFX_ITEM_SET == pArgs->GetItemState( SID_STYLE_MASK,
                 sal_False, &pItem ))
                 nMask = ((const SfxUInt16Item*)pItem)->GetValue();
-            String sParent;
+            OUString sParent;
             if( SFX_ITEM_SET == pArgs->GetItemState( SID_STYLE_REFERENCE,
                 sal_False, &pItem ))
                 sParent = ((const SfxStringItem*)pItem)->GetValue();
@@ -327,7 +327,7 @@ void SwDocShell::ExecStyleSheet( SfxRequest& rReq )
         case SID_STYLE_UPDATE_BY_EXAMPLE:
         case SID_STYLE_NEW_BY_EXAMPLE:
         {
-            String aParam;
+            OUString aParam;
             sal_uInt16 nFamily = SFX_STYLE_FAMILY_PARA;
             sal_uInt16 nMask = 0;
 
@@ -378,20 +378,20 @@ void SwDocShell::ExecStyleSheet( SfxRequest& rReq )
 
                 if( SFX_ITEM_SET == pArgs->GetItemState(SID_STYLE_FAMILYNAME, sal_False, &pItem ))
                 {
-                    String aFamily = ((const SfxStringItem*)pItem)->GetValue();
-                    if(aFamily.CompareToAscii("CharacterStyles") == COMPARE_EQUAL)
+                    OUString aFamily = ((const SfxStringItem*)pItem)->GetValue();
+                    if(aFamily.equalsAscii("CharacterStyles"))
                         nFamily = SFX_STYLE_FAMILY_CHAR;
                     else
-                    if(aFamily.CompareToAscii("ParagraphStyles") == COMPARE_EQUAL)
+                    if(aFamily.equalsAscii("ParagraphStyles"))
                         nFamily = SFX_STYLE_FAMILY_PARA;
                     else
-                    if(aFamily.CompareToAscii("PageStyles") == COMPARE_EQUAL)
+                    if(aFamily.equalsAscii("PageStyles"))
                         nFamily = SFX_STYLE_FAMILY_PAGE;
                     else
-                    if(aFamily.CompareToAscii("FrameStyles") == COMPARE_EQUAL)
+                    if(aFamily.equalsAscii("FrameStyles"))
                         nFamily = SFX_STYLE_FAMILY_FRAME;
                     else
-                    if(aFamily.CompareToAscii("NumberingStyles") == COMPARE_EQUAL)
+                    if(aFamily.equalsAscii("NumberingStyles"))
                         nFamily = SFX_STYLE_FAMILY_PSEUDO;
                 }
 
@@ -437,7 +437,7 @@ void SwDocShell::ExecStyleSheet( SfxRequest& rReq )
                     rReq.AppendItem(SfxStringItem(nSlot, aParam));
                 }
             }
-            if (aParam.Len() || nSlot == SID_STYLE_WATERCAN )
+            if (!aParam.isEmpty() || nSlot == SID_STYLE_WATERCAN )
             {
                 switch(nSlot)
                 {
@@ -893,7 +893,7 @@ sal_uInt16 SwDocShell::ApplyStyles(const OUString &rName, sal_uInt16 nFamily,
             // reset indent attribute on applying list style
             // continue list of list style
             const SwNumRule* pNumRule = pStyle->GetNumRule();
-            const String sListIdForStyle =pNumRule->GetDefaultListId();
+            const OUString sListIdForStyle =pNumRule->GetDefaultListId();
             pSh->SetCurNumRule( *pNumRule, false, sListIdForStyle, true );
             break;
         }
@@ -1171,7 +1171,7 @@ sal_uInt16 SwDocShell::MakeByExample( const OUString &rName, sal_uInt16 nFamily,
             pCurrWrtShell->StartAllAction();
 
             SwNumRule aRule( *pCurrWrtShell->GetCurNumRule() );
-            String sOrigRule( aRule.GetName() );
+            OUString sOrigRule( aRule.GetName() );
             // #i91400#
             aRule.SetName( pStyle->GetNumRule()->GetName(),
                            *(pCurrWrtShell->GetDoc()) );

sw/source/ui/app/docstyle.cxx
@@ -321,7 +321,7 @@ static sal_uInt16 lcl_FindName(const SwPoolFmtList& rLst, SfxStyleFamily eFam,
     return USHRT_MAX;
 }
 
-bool FindPhyStyle( SwDoc& rDoc, const String& rName, SfxStyleFamily eFam )
+bool FindPhyStyle( SwDoc& rDoc, const OUString& rName, SfxStyleFamily eFam )
 {
     switch( eFam )
     {
@@ -2659,7 +2659,7 @@ SfxStyleSheetBase*  SwStyleSheetIterator::First()
                     };
 
                 sal_uInt16* pPoolIds = aPoolIds;
-                String s;
+                OUString s;
                 while( *pPoolIds )
                 {
                     if( !bIsSearchUsed || rDoc.IsPoolTxtCollUsed( *pPoolIds ) )

commit 6659b6080284577658a4b784551c893f963386d7

fdo#74474: sw: fix Label wizard document creation (2 in 1)
Infinite loop in InsertLabEnvText() due to wrong handling of starting '<'.

(regression from fa469b2e00d83459faebe4c1fcb3ea1aac5fb20d)

(cherry picked from commit 7e61bfe813347949307cdf2876ead3cc42e4cd7b)

The function ReplacePoint() was changed to return a value but not all
call sites were adapted.
(regression from 263153842741d7ce21cc0bf1c5296a55a1138024)

(cherry picked from commit 9dbe5a576330b7fadd8838249d07aafe548a4ee2)

Change-Id: I33efd9ce61611e025dfc76047187826aa0f8dd84

sw/source/core/bastyp/calc.cxx
@@ -512,8 +512,7 @@ SwCalcExp* SwCalc::VarLook( const OUString& rStr, sal_uInt16 ins )
     }
 
     // At this point the "real" case variable has to be used
-    OUString sTmpName( rStr );
-    ::ReplacePoint( sTmpName );
+    OUString const sTmpName( ::ReplacePoint(rStr) );
 
     if( !ins )
     {

sw/source/ui/app/appenv.cxx
@@ -88,10 +88,11 @@ OUString InsertLabEnvText( SwWrtShell& rSh, SwFldMgr& rFldMgr, const OUString& r
             bool bField = false;
 
             sal_Int32 nPos = aLine.indexOf( '<' );
-            if ( nPos != -1)
+            if (0 != nPos)
             {
-                sTmpText = aLine.copy( 0, nPos );
-                aLine = aLine.copy( nPos );
+                sal_Int32 const nCopy((nPos != -1) ? nPos : aLine.getLength());
+                sTmpText = aLine.copy(0, nCopy);
+                aLine = aLine.copy(nCopy);
             }
             else
             {
@@ -111,7 +112,7 @@ OUString InsertLabEnvText( SwWrtShell& rSh, SwFldMgr& rFldMgr, const OUString& r
                     sal_uInt16 nCnt = comphelper::string::getTokenCount(sDBName, '.');
                     if (nCnt >= 3)
                     {
-                        ::ReplacePoint(sDBName, true);
+                        sDBName = ::ReplacePoint(sDBName, true);
                         SwInsertFld_Data aData(TYP_DBFLD, 0, sDBName, aEmptyOUStr, 0, &rSh );
                         rFldMgr.InsertFld( aData );
                         sRet = sDBName;