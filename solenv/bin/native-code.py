5306890b2c960235183caad0bf89e5f31f2f29a0
Matúš Kukan <matus.kukan@collabora.com>
2014-02-10 12:15:07 +0100

    native code generator: Do not use uui component.
    Since fe537786a42abea57551395f73328db48bebd086 and
    4ff56fd1fa6f0ad2501274086a03d51a0e9cf03f it should not be needed.
    
    Change-Id: I497c7920e94c322cee5db1b10cd61f30dfbba634

--- solenv/bin/native-code.py
+++ solenv/bin/native-code.py
@@ -41,7 +41,6 @@ core_factory_list = [
     ("libunordflo.a", "unordf_component_getFactory"),
     ("libunoxmllo.a", "unoxml_component_getFactory"),
     ("libutllo.a", "utl_component_getFactory"),
-    ("libuuilo.a", "uui_component_getFactory"),
     ("libxmlsecurity.a", "xmlsecurity_component_getFactory"),
     ("libxolo.a", "xo_component_getFactory"),
     ("libxstor.a", "xstor_component_getFactory"),
