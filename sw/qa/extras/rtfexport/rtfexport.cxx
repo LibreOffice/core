6b5ef795ed4a56195bf09633b03e88474f0b133c
Miklos Vajna <vmiklos@collabora.co.uk>
2014-02-10 09:39:16 +0100

    fdo#74709 RTF import: implement RTF_CLPAD*
    Change-Id: I482735e3fc4091be983b2c7484e086f5d0dd283a

--- sw/qa/extras/rtfexport/rtfexport.cxx
+++ sw/qa/extras/rtfexport/rtfexport.cxx
@@ -595,6 +595,13 @@ DECLARE_RTFEXPORT_TEST(testFdo68787, "fdo68787.rtf")
     CPPUNIT_ASSERT_EQUAL(sal_Int32(25), getProperty<sal_Int32>(xPageStyle, "FootnoteLineRelativeWidth"));
 }
 
+DECLARE_RTFEXPORT_TEST(testFdo74709, "fdo74709.rtf")
+{
+    uno::Reference<table::XCell> xCell = getCell(getParagraphOrTable(1), "B1");
+    // This was 0, as top/bottom/left/right padding wasn't imported.
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(TWIP_TO_MM100(360)), getProperty<sal_Int32>(xCell, "RightBorderDistance"));
+}
+
 #endif
 
 CPPUNIT_PLUGIN_IMPLEMENT();
