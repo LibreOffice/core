787903f0ab54b2933b7c416358fbefba5b179506
Chris Sherlock <chris.sherlock79@gmail.com>
2014-02-10 19:35:44 +1100

    Add default parameter to OutputDevice::GetClipRegion
    Currently, GetClipRegion converts to logical units. I have added a
    default parameter to allow for returning device pixel units - this
    defaults to return logical units.
    
    Change-Id: I6bf8da96a41a616993879343cdc0a82baa010e16

--- include/vcl/outdev.hxx
+++ include/vcl/outdev.hxx
@@ -593,7 +593,7 @@ public:
 
     void                        SetClipRegion();
     void                        SetClipRegion( const Region& rRegion );
-    Region                      GetClipRegion() const;
+    Region                      GetClipRegion( bool bLogicalRegion=true ) const;
     bool                        IsClipRegion() const { return mbClipRegion; }
     Region                      GetActiveClipRegion() const;
 
