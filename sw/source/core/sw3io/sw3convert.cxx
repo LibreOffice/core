3df1b8927a485192c55daf087449c00f505f6e5d
Takeshi Abe <tabe@fixedpoint.jp>
2014-02-10 15:43:42 +0900

    Drop unused headers
    Change-Id: I71b036ff0683307e7a2676e09c90d5a95667d599

--- sw/source/core/sw3io/sw3convert.cxx
+++ sw/source/core/sw3io/sw3convert.cxx
@@ -17,9 +17,6 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#include <stdlib.h>
-
-#include <stdio.h>
 #include "hintids.hxx"
 #include <tools/resid.hxx>
 #include <svl/macitem.hxx>
