182ac74065d8724bf74003cb72928b2cfa7388e9
Kohei Yoshida <kohei.yoshida@collabora.com>
2014-02-11 01:08:15 -0500

    fdo#74014: Broadcast here....
    Change-Id: Ib3c1e6b18af2c96c8c38237ab65f868ffec5f139

--- sc/source/ui/undo/undoblk3.cxx
+++ sc/source/ui/undo/undoblk3.cxx
@@ -142,6 +142,7 @@ void ScUndoDeleteContents::DoChange( const sal_Bool bUndo )
         aCopyRange.aEnd.SetTab(nTabCount-1);
 
         pUndoDoc->CopyToDocument( aCopyRange, nUndoFlags, bMulti, pDoc, &aMarkData );
+        BroadcastChanges(aCopyRange);
 
         DoSdrUndoAction( pDrawUndo, pDoc );
 
