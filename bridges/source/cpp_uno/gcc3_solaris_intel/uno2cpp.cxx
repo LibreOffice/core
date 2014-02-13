95eeed58af27c63977a43c4d688b1467ef3cf047
Herbert Dürr <hdu@apache.org>
2014-02-13 13:21:50 +0000

    #i124086# preserve 16byte stack alignment in gcc3-solaris-intel's uno2cpp bridge
    Patch-by: Apostolos Syropoulos <asyropoulos123@hotmail.com>
    Review-by: Herbert Dürr <hdu@apache.org>

--- bridges/source/cpp_uno/gcc3_solaris_intel/uno2cpp.cxx
+++ bridges/source/cpp_uno/gcc3_solaris_intel/uno2cpp.cxx
@@ -45,7 +45,15 @@ namespace
 {
 
 //==================================================================================================
-static void callVirtualMethod(
+void callVirtualMethod(
+    void * pAdjustedThisPtr,
+    sal_Int32 nVtableIndex,
+    void * pRegisterReturn,
+    typelib_TypeClass eReturnType,
+    sal_Int32 * pStackLongs,
+    sal_Int32 nStackLongs ) __attribute__((noinline));
+
+void callVirtualMethod(
     void * pAdjustedThisPtr,
     sal_Int32 nVtableIndex,
     void * pRegisterReturn,
@@ -67,6 +75,13 @@ static void callVirtualMethod(
     void * stackptr;
     asm volatile (
         "mov   %%esp, %6\n\t"
+       // preserve potential 128bit stack alignment
+        "and   $0xfffffff0, %%esp\n\t"
+        "mov   %0, %%eax\n\t"
+        "lea   -4(,%%eax,4), %%eax\n\t"
+        "and   $0xf, %%eax\n\t"
+        "sub   $0xc, %%eax\n\t"
+        "add   %%eax, %%esp\n\t"
         // copy values
         "mov   %0, %%eax\n\t"
         "mov   %%eax, %%edx\n\t"
