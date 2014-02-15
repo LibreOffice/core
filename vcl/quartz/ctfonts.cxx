466fd27f04c459833ffebc370380e56c0fed0b79
Tomofumi Yagi <yagit@mknada.sakura.ne.jp>
2014-02-15 11:48:20 +0900

    fdo#72042: Fix for "Font names are not localized (Mac OS X)"
    This patch will bring the behavior of 4.0 to master(4.3).
    This patch has 1 changes on implementation of vcl/quartz/ctfonts.cxx.
    
    We will use CTFontDescriptorCopyLocalizedAttribute() API,If possible.
    So DevFontFromCTFontDescriptor() can return the localized fontname.
    
    Change-Id: I7eb5bda4fa69540fb32de573190a30bcb2c01cd2

--- vcl/quartz/ctfonts.cxx
+++ vcl/quartz/ctfonts.cxx
@@ -305,8 +305,17 @@ ImplDevFontAttributes DevFontFromCTFontDescriptor( CTFontDescriptorRef pFD, bool
     rDFA.mbSubsettable = true;
 
     // get font name
-    CFStringRef pFamilyName = (CFStringRef)CTFontDescriptorCopyAttribute( pFD, kCTFontFamilyNameAttribute );
+    const OUString aUILang = Application::GetSettings().GetUILanguageTag().getLanguage();
+    CFStringRef pUILang = CFStringCreateWithCharacters( kCFAllocatorDefault, aUILang.getStr(), aUILang.getLength() );
+    CFStringRef pLang = NULL;
+    CFStringRef pFamilyName = (CFStringRef)CTFontDescriptorCopyLocalizedAttribute( pFD, kCTFontFamilyNameAttribute, &pLang );
+    if ( CFStringCompare( pUILang, pLang, 0 ) != kCFCompareEqualTo )
+    {
+        CFRelease( pFamilyName );
+        pFamilyName = (CFStringRef)CTFontDescriptorCopyAttribute( pFD, kCTFontFamilyNameAttribute );
+    }
     rDFA.SetFamilyName( GetOUString( pFamilyName ) );
+
     // get font style
     CFStringRef pStyleName = (CFStringRef)CTFontDescriptorCopyAttribute( pFD, kCTFontStyleNameAttribute );
     rDFA.SetStyleName( GetOUString( pStyleName ) );
