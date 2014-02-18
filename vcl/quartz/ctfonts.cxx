LibreOffice/core

commit cda903f83831341f44833b94e7d38e598f5a7198

fdo#72042: Fix for "Font names are not localized (Mac OS X)"
This patch will bring the behavior of 4.0 to master(4.3).
This patch has 1 changes on implementation of vcl/quartz/ctfonts.cxx.

We will use CTFontDescriptorCopyLocalizedAttribute() API,If possible.
So DevFontFromCTFontDescriptor() can return the localized fontname.

Change-Id: I7eb5bda4fa69540fb32de573190a30bcb2c01cd2
Reviewed-on: https://gerrit.libreoffice.org/8071
Reviewed-by: Norbert Thiebaud <nthiebaud@gmail.com>
Tested-by: Norbert Thiebaud <nthiebaud@gmail.com>

vcl/quartz/ctfonts.cxx
@@ -305,8 +305,17 @@ ImplDevFontAttributes DevFontFromCTFontDescriptor( CTFontDescriptorRef pFD, bool
     rDFA.mbSubsettable = true;
 
     // get font name
-    CFStringRef pFamilyName = (CFStringRef)CTFontDescriptorCopyAttribute( pFD, kCTFontFamilyNameAttribute );
+    const OUString aUILang = Application::GetSettings().GetUILanguageTag().getLanguage();
+    CFStringRef pUILang = CFStringCreateWithCharacters( kCFAllocatorDefault, aUILang.getStr(), aUILang.getLength() );
+    CFStringRef pLang = NULL;
+    CFStringRef pFamilyName = (CFStringRef)CTFontDescriptorCopyLocalizedAttribute( pFD, kCTFontFamilyNameAttribute, &pLang );
+    if ( CFStringCompare( pUILang, pLang, 0 ) != kCFCompareEqualTo )
+    {
+        CFRelease( pFamilyName );
+        pFamilyName = (CFStringRef)CTFontDescriptorCopyAttribute( pFD, kCTFontFamilyNameAttribute );
+    }
     rDFA.SetFamilyName( GetOUString( pFamilyName ) );
+
     // get font style
     CFStringRef pStyleName = (CFStringRef)CTFontDescriptorCopyAttribute( pFD, kCTFontStyleNameAttribute );
     rDFA.SetStyleName( GetOUString( pStyleName ) );

commit 4531a442454d81e140d720326f323cb5ad6ebfb5

Fix iOS fallout from cda903f83831341f44833b94e7d38e598f5a7198
Change-Id: I48e418ff7d5a8bbf64bc6072b4ad2ef30cb24625

vcl/quartz/ctfonts.cxx
@@ -305,6 +305,7 @@ ImplDevFontAttributes DevFontFromCTFontDescriptor( CTFontDescriptorRef pFD, bool
     rDFA.mbSubsettable = true;
 
     // get font name
+#ifdef MACOSX
     const OUString aUILang = Application::GetSettings().GetUILanguageTag().getLanguage();
     CFStringRef pUILang = CFStringCreateWithCharacters( kCFAllocatorDefault, aUILang.getStr(), aUILang.getLength() );
     CFStringRef pLang = NULL;
@@ -314,6 +315,13 @@ ImplDevFontAttributes DevFontFromCTFontDescriptor( CTFontDescriptorRef pFD, bool
         CFRelease( pFamilyName );
         pFamilyName = (CFStringRef)CTFontDescriptorCopyAttribute( pFD, kCTFontFamilyNameAttribute );
     }
+#else
+    // No "Application" on iOS. And it is unclear whether this code
+    // snippet will actually ever get invoked on iOS anyway. So just
+    // use the old code that uses a non-localized font name.
+    CFStringRef pFamilyName = (CFStringRef)CTFontDescriptorCopyAttribute( pFD, kCTFontFamilyNameAttribute );
+#endif
+
     rDFA.SetFamilyName( GetOUString( pFamilyName ) );
 
     // get font style