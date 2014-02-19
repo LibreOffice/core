5bb7c0ab476ca108aecb5bf43ec9adc6d173f13b
Caolán McNamara <caolanm@redhat.com>
2014-02-19 10:21:10 +0000

    coverity#738892 Uninitialized scalar field
    Change-Id: I4eecd759cf22f91a98830fc51b394ac959d6dc8a

--- sw/source/filter/ww8/rtfsdrexport.cxx
+++ sw/source/filter/ww8/rtfsdrexport.cxx
@@ -39,6 +39,7 @@ RtfSdrExport::RtfSdrExport( RtfExport &rExport )
       m_rAttrOutput( (RtfAttributeOutput&)m_rExport.AttrOutput() ),
       m_pSdrObject( NULL ),
       m_nShapeType( ESCHER_ShpInst_Nil ),
+      m_nShapeFlags ( 0 ) ,
       m_pShapeStyle( new OStringBuffer( 200 ) ),
       m_pShapeTypeWritten( new bool[ ESCHER_ShpInst_COUNT ] )
 {
