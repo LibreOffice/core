3ce65cb358dfd070224d59a34b55a5068fdbf527
Matúš Kukan <matus.kukan@collabora.com>
2014-02-22 17:26:02 +0100

    Do not try to use gb_STDLIBS explicitly. It's used by default anyway.
    Change-Id: I5761d616e0b7255c2fbe17bb709cae0bdcb95910

--- vcl/CppunitTest_vcl_app_test.mk
+++ vcl/CppunitTest_vcl_app_test.mk
@@ -10,25 +10,18 @@
 $(eval $(call gb_CppunitTest_CppunitTest,vcl_app_test))
 
 $(eval $(call gb_CppunitTest_add_exception_objects,vcl_app_test, \
-    vcl/qa/cppunit/app/test_IconThemeInfo \
-    vcl/qa/cppunit/app/test_IconThemeScanner \
-    vcl/qa/cppunit/app/test_IconThemeSelector \
+	vcl/qa/cppunit/app/test_IconThemeInfo \
+	vcl/qa/cppunit/app/test_IconThemeScanner \
+	vcl/qa/cppunit/app/test_IconThemeSelector \
 ))
 
-# add a list of all needed libraries here
 $(eval $(call gb_CppunitTest_use_libraries,vcl_app_test, \
-sal \
-vcl \
-$(gb_STDLIBS) \
+	sal \
+	vcl \
 ))
 
 $(eval $(call gb_CppunitTest_use_externals,vcl_app_test, \
 	boost_headers \
 ))
-
-$(eval $(call gb_CppunitTest_set_include,vcl_app_test, \
-$$(INCLUDE) \
--I$(OUTDIR)/inc \
- ))
 
 # vim: set noet sw=4 ts=4:
