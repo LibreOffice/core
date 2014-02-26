5c1236fe2dd4a7a0aeca40487788af095429e64b
Jan-Marek Glogowski <glogow@fbihome.de>
2014-02-26 18:19:11 +0000

    fdo#74416: sleep in yield for native file picker
    As it seems to be the only way to poll the clipboard, reintroduce
    
     m_pApplication->clipboard()->setProperty(
        "useEventLoopWhenWaiting", true );
    
    To prevent crashes, disable event processing in the Qt thread while
    the dialog is open.
    
    Instead this applies the same workaround as the Windows backend to
    sleep a ms, which keeps the FP dialogs more usable, but feels like
    a horrible workaround.
    
    This is still slower then running processEvent in Yield but still
    much better then the current situation.
    
    (cherry picked from commit 380f3b4b6cbbe8e82b58ddf55e95c5005307b51f)
    
    Change-Id: I10c422f1c0d7448d4a7ad28e57a32ed2cb42f48f
    Reviewed-on: https://gerrit.libreoffice.org/8435
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>

--- vcl/unx/kde4/KDE4FilePicker.hxx
+++ vcl/unx/kde4/KDE4FilePicker.hxx
@@ -40,6 +40,7 @@
 class KFileDialog;
 class QWidget;
 class QLayout;
+class KDEXLib;
 
 class ResMgr;
 
@@ -82,8 +83,10 @@ protected:
 
     bool allowRemoteUrls;
 
+    KDEXLib* _mXLib;
+
 public:
-    KDE4FilePicker( const ::com::sun::star::uno::Reference< ::com::sun::star::uno::XComponentContext >& );
+    KDE4FilePicker( const ::com::sun::star::uno::Reference< ::com::sun::star::uno::XComponentContext >&, KDEXLib* );
     virtual ~KDE4FilePicker();
 
     // XFilePickerNotifier
