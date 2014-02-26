LibreOffice/core

commit 70c35265f517ef372cb739d4cc64499abf57a838

gbuild: remove libraries from OUTDIR and WORKDIR
Refactor everything to find and link libraries directly in INSTDIR.

- add gb_LinkTarget_get_linksearchpath_for_layer, and use it to set up
  -L paths for T_LDFLAGS in such a way that only allowed libraries
  can be linked against; i.e. it's not possible to link URE
  linktargets against OOO or not-installed libraries
- gb_Library_get_target is now same as the gb_LinkTarget_get_target
  (TODO: this needs cleanup)
- since a pattern rule won't work for linking libraries in INSTDIR,
  add a separate per-file rule for every INSTDIR lib
- pattern rule can't find link target in the clean target any more
  so add a LINKTARGET variable
- disable gb_Library_add_auxtarget, no auxtargets need to be copied
- tweak the call to gb_Library_Library_platform to pass in a path
  in sdk/lib for the versioned URE libs
- fix the Library clean target
- add LAYER parameter to gb_LinkTarget_LinkTarget
- adjust platform link commands
- MSVC link command now uses explicit -manifestfile and -pdb
  parameters to keep misc. files out of INSTDIR
- remove gb_Helper_OUTDIR_FOR_BUILDLIBDIR
- adjust Extension, CppunitTest,  JunitTest, PythonTest, Gallery,
  various CustomTargets to search INSTDIR
- remove SDK library symlinks and import libs from odk/Package_lib
- on Mac OS X, put .dylib symlinks into sdk/lib even though those
  are not packaged and would be created by the SDK configury;
  we need these to be somewhere for linking anyway
- add a (unfortunately cyclic) dependency on Package ure_install to sal

Change-Id: I70d88742f8c8232ad7b9521416275c67b64fe6cf

solenv/gbuild/Helper.mk
@@ -215,17 +215,6 @@ $(1) : $(2) | $(dir $(1)).dir
 $(call gb_Deliver_add_deliverable,$(1),$(2),$(1))
 endef
 
-# 4th parameter overrides 3rd for the dependency, necessary for Library
-# which needs to copy DLL but that must not be a real target...
-define gb_Library__install
-$(1) :| $(2)
-$(2) : $(4) | $(dir $(2)).dir
-	cp $(3) $$@
-
-$(call gb_Deliver_add_deliverable,$(2),$(3),$(2))
-endef
-
-
 # gb_Helper_OUTDIRLIBDIR is set by the platform to the path the dynamic linker need to use
 # for libraries from the OUTDIR
 

solenv/gbuild/platform/com_MSC_class.mk
@@ -138,12 +138,19 @@ gb_LinkTarget_INCLUDE :=\
 
 gb_LinkTarget_get_pdbfile = \
  $(WORKDIR)/LinkTarget/pdb/$(call gb_LinkTarget__get_workdir_linktargetname,$(1)).pdb
+# substitute .pyd here because pyuno has to follow python's crazy conventions
 gb_LinkTarget_get_pdbfile2 = \
  $(WORKDIR)/LinkTarget/$(patsubst %.dll,%.pdb,$(patsubst %.pyd,%.dll,$(call gb_LinkTarget__get_workdir_linktargetname,$(1))))
 gb_LinkTarget_get_ilkfile = \
  $(WORKDIR)/LinkTarget/$(patsubst %.dll,%.ilk,$(patsubst %.pyd,%.dll,$(call gb_LinkTarget__get_workdir_linktargetname,$(1))))
 gb_LinkTarget_get_manifestfile = \
  $(WORKDIR)/LinkTarget/$(call gb_LinkTarget__get_workdir_linktargetname,$(1)).manifest
+
+gb_LinkTarget_get_linksearchpath_for_layer = \
+	-LIBPATH:$(INSTDIR)/$(gb_Package_SDKDIRNAME)/lib \
+	$(if $(filter OXT,$(1)),\
+		-LIBPATH:$(WORKDIR)/LinkTarget/ExtensionLibrary, \
+		-LIBPATH:$(WORKDIR)/LinkTarget/Library)
 
 # avoid fatal error LNK1170 for Library_merged
 define gb_LinkTarget_MergedResponseFile
@@ -190,18 +197,22 @@ $(call gb_Helper_abbreviate_dirs,\
 		$(foreach lib,$(LINKED_STATIC_LIBS),$(call gb_StaticLibrary_get_filename,$(lib))) \
 		$(LIBS) \
 		$(if $(filter-out StaticLibrary,$(TARGETTYPE)),user32.lib) \
+		$(if $(filter-out StaticLibrary,$(TARGETTYPE)),-manifestfile:$(WORKDIR)/LinkTarget/$(2).manifest) \
+		-pdb:$(WORKDIR)/LinkTarget/$(2).pdb \
 		$(if $(ILIBTARGET),-out:$(1) -implib:$(ILIBTARGET),-out:$(1)); RC=$$?; rm $${RESPONSEFILE} \
-	$(if $(ILIBTARGET),; if [ ! -f $(ILIBTARGET) ]; then rm -f $(1) && false; fi) \
-	$(if $(filter Library,$(TARGETTYPE)),&& if [ -f $(1).manifest ]; then mt.exe $(MTFLAGS) -nologo -manifest $(1).manifest -outputresource:$(1)\;2 && touch -r $(1) $(1).manifest $(ILIBTARGET); fi) \
-	$(if $(filter Executable,$(TARGETTYPE)),&& if [ -f $(1).manifest ]; then mt.exe $(MTFLAGS) -nologo -manifest $(1).manifest -outputresource:$(1)\;1 && touch -r $(1) $(1).manifest; fi) \
+	$(if $(filter Library,$(TARGETTYPE)),; if [ ! -f $(ILIBTARGET) ]; then rm -f $(1); exit 42; fi) \
+	$(if $(filter Library,$(TARGETTYPE)),&& if [ -f $(WORKDIR)/LinkTarget/$(2).manifest ]; then mt.exe $(MTFLAGS) -nologo -manifest $(WORKDIR)/LinkTarget/$(2).manifest -outputresource:$(1)\;2 && touch -r $(1) $(WORKDIR)/LinkTarget/$(2).manifest $(ILIBTARGET); fi) \
+	$(if $(filter Executable,$(TARGETTYPE)),&& if [ -f $(WORKDIR)/LinkTarget/$(2).manifest ]; then mt.exe $(MTFLAGS) -nologo -manifest $(WORKDIR)/LinkTarget/$(2).manifest -outputresource:$(1)\;1 && touch -r $(1) $(WORKDIR)/LinkTarget/$(2).manifest; fi) \
 	$(if $(filter YES,$(TARGETGUI)),&& if [ -f $(SRCDIR)/solenv/inc/DeclareDPIAware.manifest ]; then mt.exe $(MTFLAGS) -nologo -manifest $(SRCDIR)/solenv/inc/DeclareDPIAware.manifest -updateresource:$(1)\;1 ; fi) \
 	$(if $(filter Library,$(TARGETTYPE)),&& \
-		echo $(notdir $(1)) > $(1).exports.tmp && \
+		echo $(notdir $(1)) > $(WORKDIR)/LinkTarget/$(2).exports.tmp && \
 		$(if $(filter YES,$(LIBRARY_X64)),$(LINK_X64_BINARY),$(gb_LINK)) \
-			-dump -exports $(ILIBTARGET) >> $(1).exports.tmp && \
-		if cmp -s $(1).exports.tmp $(1).exports; \
-			then rm $(1).exports.tmp; \
-			else mv $(1).exports.tmp $(1).exports; touch -r $(1) $(1).exports; \
+			-dump -exports $(ILIBTARGET) \
+			>> $(WORKDIR)/LinkTarget/$(2).exports.tmp && \
+		if cmp -s $(WORKDIR)/LinkTarget/$(2).exports.tmp $(WORKDIR)/LinkTarget/$(2).exports; \
+			then rm $(WORKDIR)/LinkTarget/$(2).exports.tmp; \
+			else mv $(WORKDIR)/LinkTarget/$(2).exports.tmp $(WORKDIR)/LinkTarget/$(2).exports && \
+				touch -r $(1) $(WORKDIR)/LinkTarget/$(2).exports; \
 		fi) \
 	; exit $$RC)
 endef
@@ -287,22 +298,9 @@ $(call gb_LinkTarget_add_auxtargets,$(2),\
 	$(patsubst %.lib,%.exp,$(3)) \
 	$(call gb_LinkTarget_get_manifestfile,$(2)) \
 	$(call gb_LinkTarget_get_pdbfile,$(2)) \
-)
-
-$(call gb_Library_add_auxtarget,$(1),$(OUTDIR)/lib/$(notdir $(3)))
-
-# substitute .pyd here because pyuno has to follow python's crazy conventions
-ifneq ($(ENABLE_CRASHDUMP),)
-$(call gb_Library_add_auxtargets,$(1),\
-	$(OUTDIR)/bin/$(patsubst %.dll,%.pdb,$(patsubst %.pyd,%.dll,$(call gb_Library_get_filename,$(1)))) \
-	$(OUTDIR)/bin/$(patsubst %.dll,%.ilk,$(patsubst %.pyd,%.dll,$(call gb_Library_get_filename,$(1)))) \
-)
-else
-$(call gb_LinkTarget_add_auxtargets,$(2),\
 	$(call gb_LinkTarget_get_pdbfile2,$(2)) \
 	$(call gb_LinkTarget_get_ilkfile,$(2)) \
 )
-endif
 
 $(call gb_Library_add_default_nativeres,$(1),$(1)/default)
 
@@ -390,7 +388,7 @@ endef
 gb_CppunitTest_DEFS := -D_DLL
 # cppunittester.exe is in the cppunit subdirectory of ${OUTDIR}/bin,
 # thus it won't find its DLLs unless ${OUTDIR}/bin is added to PATH.
-gb_CppunitTest_CPPTESTPRECOMMAND := $(gb_Helper_set_ld_path)
+gb_CppunitTest_CPPTESTPRECOMMAND := $(gb_Helper_set_ld_path):"$(shell cygpath -w $(gb_Library_DLLDIR))"
 
 gb_CppunitTest_LIBDIR := $(gb_Helper_OUTDIRLIBDIR)
 gb_CppunitTest_get_filename = test_$(1).dll
@@ -542,7 +540,7 @@ gb_InstallScript_EXT := .inf
 # CliAssemblyTarget class
 
 gb_CliAssemblyTarget_POLICYEXT := $(gb_Library_DLLEXT)
-gb_CliAssemblyTarget_get_dll = $(OUTDIR)/bin/$(1)$(gb_CliAssemblyTarget_POLICYEXT)
+gb_CliAssemblyTarget_get_dll = $(call gb_Library__get_dir_for_layer,URELIB)/$(1)$(gb_CliAssemblyTarget_POLICYEXT)
 
 # Extension class
 

solenv/gbuild/platform/macosx.mk
@@ -153,7 +153,7 @@ $(call gb_Helper_abbreviate_dirs,\
 		$(LIBS) \
 		$(foreach lib,$(LINKED_STATIC_LIBS),$(call gb_StaticLibrary_get_target,$(lib))) \
 		-o $(1) && \
-	$(if $(SOVERSIONSCRIPT),ln -sf $(notdir $(1)) $(ILIBTARGET),:) && \
+	$(if $(SOVERSIONSCRIPT),ln -sf $(1) $(ILIBTARGET),:) && \
 	$(if $(filter Executable,$(TARGETTYPE)), \
 		$(PERL) $(SOLARENV)/bin/macosx-change-install-names.pl app $(LAYER) $(1) &&) \
 	$(if $(filter Library Bundle CppunitTest,$(TARGETTYPE)),\
@@ -162,12 +162,14 @@ $(call gb_Helper_abbreviate_dirs,\
 		$(if $(filter Executable,$(TARGETTYPE)), \
 			(codesign --identifier=$(MACOSX_BUNDLE_IDENTIFIER).$(notdir $(1)) --sign $(MACOSX_CODESIGNING_IDENTITY) --force $(1) || true) &&)) \
 	$(if $(filter Library,$(TARGETTYPE)),\
-		otool -l $(1) | grep -A 5 LC_ID_DYLIB > $(1).exports.tmp && \
+		otool -l $(1) | grep -A 5 LC_ID_DYLIB \
+			> $(WORKDIR)/LinkTarget/$(2).exports.tmp && \
 		$(NM) -g -P $(1) | cut -d' ' -f1-2 | grep -v U$$ \
-			>> $(1).exports.tmp && \
-		if cmp -s $(1).exports.tmp $(1).exports; \
-			then rm $(1).exports.tmp; \
-			else mv $(1).exports.tmp $(1).exports; touch -r $(1) $(1).exports; \
+			>> $(WORKDIR)/LinkTarget/$(2).exports.tmp && \
+		if cmp -s $(WORKDIR)/LinkTarget/$(2).exports.tmp $(WORKDIR)/LinkTarget/$(2).exports; \
+			then rm $(WORKDIR)/LinkTarget/$(2).exports.tmp; \
+			else mv $(WORKDIR)/LinkTarget/$(2).exports.tmp $(WORKDIR)/LinkTarget/$(2).exports && \
+				touch -r $(1) $(WORKDIR)/LinkTarget/$(2).exports; \
 		fi &&) \
 	:)
 endef
@@ -281,7 +283,7 @@ endef
 
 # CppunitTest class
 
-gb_CppunitTest_CPPTESTPRECOMMAND := $(gb_Helper_set_ld_path)
+gb_CppunitTest_CPPTESTPRECOMMAND := $(gb_Helper_set_ld_path):"$(gb_Library_DLLDIR)"
 gb_CppunitTest_LIBDIR := $(gb_Helper_OUTDIRLIBDIR)
 gb_CppunitTest_get_filename = libtest_$(1).dylib
 gb_CppunitTest_get_ilibfilename = $(gb_CppunitTest_get_filename)

solenv/gbuild/platform/mingw.mk
@@ -111,7 +111,7 @@ $(call gb_Helper_abbreviate_dirs,\
 		$(if $(LINKED_STATIC_LIBS),-Wl$(COMMA)--start-group $(foreach lib,$(LINKED_STATIC_LIBS),$(call gb_StaticLibrary_get_target,$(lib))) -Wl$(COMMA)--end-group) \
 		$(patsubst lib%.a,-l%,$(patsubst lib%.dll.a,-l%,$(foreach lib,$(LINKED_LIBS),$(call gb_Library_get_ilibfilename,$(lib))))) \
 		$(LIBS) \
-		-Wl$(COMMA)-Map$(COMMA)$(basename $(1)).map \
+		-Wl$(COMMA)-Map$(COMMA)$(WORKDIR)/LinkTarget/$(2).map \
 		-o $(1)))
 endef
 
@@ -131,7 +131,7 @@ $(call gb_Helper_abbreviate_dirs,\
 		$(if $(LINKED_STATIC_LIBS),-Wl$(COMMA)--start-group $(foreach lib,$(LINKED_STATIC_LIBS),$(call gb_StaticLibrary_get_target,$(lib))) -Wl$(COMMA)--end-group) \
 		$(patsubst lib%.a,-l%,$(patsubst lib%.dll.a,-l%,$(foreach lib,$(LINKED_LIBS),$(call gb_Library_get_ilibfilename,$(lib))))) \
 		$(LIBS) \
-		-Wl$(COMMA)-Map$(COMMA)$(dir $(1))$(notdir $(basename $(1)).map) \
+		-Wl$(COMMA)-Map$(COMMA)$(WORKDIR)/LinkTarget/$(2).map \
 		-Wl$(COMMA)--out-implib$(COMMA)$(ILIBTARGET) \
 		-o $(1) \
 		$(if $(findstring s,$(MAKEFLAGS)),> /dev/null))
@@ -161,6 +161,8 @@ define gb_LinkTarget_use_system_win32_libs
 $(call gb_LinkTarget_add_libs,$(1),$(foreach lib,$(2),-l$(patsubst oldnames,moldname,$(lib))))
 endef
 
+gb_LinkTarget_get_mapfile = \
+$(WORKDIR)/LinkTarget/$(patsubst %.dll,%.map,$(call gb_LinkTarget__get_workdir_linktargetname,$(1)))
 
 # Library class
 
@@ -228,9 +230,9 @@ $(call gb_LinkTarget_set_ilibtarget,$(2),$(3))
 
 $(call gb_Library_get_target,$(1)) :| $(OUTDIR)/bin/.dir
 
-$(call gb_Library_add_auxtargets,$(1), \
-	$(OUTDIR)/bin/$(notdir $(3)) \
-	$(OUTDIR)/bin/$(patsubst %.dll,%.map,$(call gb_Library_get_filename,$(1))) \
+$(call gb_LinkTarget_add_auxtargets,$(2), \
+	$(3) \
+	$(call gb_LinkTarget_get_mapfile,$(2)) \
 )
 
 $(call gb_Library_add_default_nativeres,$(1),$(1)/default)
@@ -289,7 +291,7 @@ $(call gb_LinkTarget_set_ilibtarget,$(2),$(3))
 
 $(call gb_LinkTarget_add_auxtargets,$(2),\
 	$(3) \
-	$(patsubst %.dll,%.map,$(call gb_LinkTarget_get_target,$(2))) \
+	$(call gb_LinkTarget_get_mapfile,$(2)) \
 )
 
 endef

solenv/gbuild/platform/solaris.mk
@@ -154,7 +154,7 @@ gb_LinkTarget__RPATHS := \
 	OOO:\dORIGIN:\dORIGIN/../ure-link/lib \
 	SDKBIN:\dORIGIN/../../ure-link/lib \
 	OXT: \
-	NONE:\dORIGIN/../lib:\dORIGIN \
+	NONE:\dORIGIN/../Library \
 
 gb_LinkTarget_CFLAGS := $(gb_CFLAGS) $(gb_CFLAGS_WERROR)
 gb_LinkTarget_CXXFLAGS := $(gb_CXXFLAGS) $(gb_CXXFLAGS_WERROR)
@@ -187,15 +187,16 @@ $(call gb_Helper_abbreviate_dirs,\
 		$(LIBS) \
 		$(patsubst lib%.a,-l%,$(patsubst lib%.so,-l%,$(patsubst %.$(gb_Library_UDK_MAJORVER),%,$(foreach lib,$(LINKED_LIBS),$(call gb_Library_get_filename,$(lib)))))) \
 		-o $(1) \
-	$(if $(SOVERSIONSCRIPT),&& ln -sf $(notdir $(1)) $(ILIBTARGET)) \
-	$(if $(filter Library,$(TARGETTYPE)),&& \
-		readelf -d $(1) | grep SONAME > $(1).exports.tmp ; \
+	$(if $(SOVERSIONSCRIPT),&& ln -sf ../../ure-link/lib/$(notdir $(1)) $(ILIBTARGET)))
+	$(if $(filter Library,$(TARGETTYPE)), $(call gb_Helper_abbreviate_dirs,\
+		readelf -d $(1) | grep SONAME > $(WORKDIR)/LinkTarget/$(2).exports.tmp; \
 		$(NM) --dynamic --extern-only --defined-only --format=posix $(1) \
 			| cut -d' ' -f1-2 \
-			>> $(1).exports.tmp && \
-		if cmp -s $(1).exports.tmp $(1).exports; \
-			then rm $(1).exports.tmp; \
-			else mv $(1).exports.tmp $(1).exports; touch -r $(1) $(1).exports; \
+			>> $(WORKDIR)/LinkTarget/$(2).exports.tmp && \
+		if cmp -s $(WORKDIR)/LinkTarget/$(2).exports.tmp $(WORKDIR)/LinkTarget/$(2).exports; \
+			then rm $(WORKDIR)/LinkTarget/$(2).exports.tmp; \
+			else mv $(WORKDIR)/LinkTarget/$(2).exports.tmp $(WORKDIR)/LinkTarget/$(2).exports && \
+				touch -r $(1) $(WORKDIR)/LinkTarget/$(2).exports; \
 		fi))
 endef
 
@@ -311,7 +312,7 @@ gb_CppunitTest_get_filename = libtest_$(1).so
 gb_CppunitTest_get_ilibfilename = $(gb_CppunitTest_get_filename)
 
 define gb_CppunitTest_CppunitTest_platform
-$(call gb_LinkTarget_get_target,$(2)) : RPATH :=
+$(call gb_LinkTarget_get_target,$(2)) : RPATH := $(call gb_Library__get_rpath,$(call gb_LinkTarget__get_rpath_for_layer,NONE))
 
 endef
 

solenv/gbuild/platform/unxgcc.mk
@@ -155,7 +155,7 @@ gb_LinkTarget__RPATHS := \
 	OOO:\dORIGIN:\dORIGIN/../ure-link/lib \
 	SDKBIN:\dORIGIN/../../ure-link/lib \
 	OXT: \
-	NONE:\dORIGIN/../lib:\dORIGIN \
+	NONE:\dORIGIN/../Library \
 
 gb_LinkTarget_CFLAGS := $(gb_CFLAGS)
 gb_LinkTarget_CXXFLAGS := $(gb_CXXFLAGS)
@@ -189,15 +189,16 @@ $(call gb_Helper_abbreviate_dirs,\
 		$(LIBS) \
 		$(patsubst lib%.a,-l%,$(patsubst lib%.so,-l%,$(patsubst %.$(gb_Library_UDK_MAJORVER),%,$(foreach lib,$(LINKED_LIBS),$(call gb_Library_get_filename,$(lib)))))) \
 		-o $(1) \
-	$(if $(SOVERSIONSCRIPT),&& ln -sf $(notdir $(1)) $(ILIBTARGET)) \
-	$(if $(filter Library,$(TARGETTYPE)),&& \
-		readelf -d $(1) | grep SONAME > $(1).exports.tmp ; \
+	$(if $(SOVERSIONSCRIPT),&& ln -sf ../../ure-link/lib/$(notdir $(1)) $(ILIBTARGET)))
+	$(if $(filter Library,$(TARGETTYPE)), $(call gb_Helper_abbreviate_dirs,\
+		readelf -d $(1) | grep SONAME > $(WORKDIR)/LinkTarget/$(2).exports.tmp; \
 		$(NM) --dynamic --extern-only --defined-only --format=posix $(1) \
 			| cut -d' ' -f1-2 \
-			>> $(1).exports.tmp && \
-		if cmp -s $(1).exports.tmp $(1).exports; \
-			then rm $(1).exports.tmp; \
-			else mv $(1).exports.tmp $(1).exports; touch -r $(1) $(1).exports; \
+			>> $(WORKDIR)/LinkTarget/$(2).exports.tmp && \
+		if cmp -s $(WORKDIR)/LinkTarget/$(2).exports.tmp $(WORKDIR)/LinkTarget/$(2).exports; \
+			then rm $(WORKDIR)/LinkTarget/$(2).exports.tmp; \
+			else mv $(WORKDIR)/LinkTarget/$(2).exports.tmp $(WORKDIR)/LinkTarget/$(2).exports && \
+				touch -r $(1) $(WORKDIR)/LinkTarget/$(2).exports; \
 		fi))
 endef
 
@@ -307,7 +308,7 @@ gb_CppunitTest_get_filename = libtest_$(1).so
 gb_CppunitTest_get_ilibfilename = $(gb_CppunitTest_get_filename)
 
 define gb_CppunitTest_CppunitTest_platform
-$(call gb_LinkTarget_get_target,$(2)) : RPATH :=
+$(call gb_LinkTarget_get_target,$(2)) : RPATH := $(call gb_Library__get_rpath,$(call gb_LinkTarget__get_rpath_for_layer,NONE))
 
 endef
 

extensions/Package_OOoSpotlightImporter.mk
@@ -7,7 +7,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 
-$(eval $(call gb_Package_Package,extensions_OOoSpotlightImporter,$(OUTDIR)/lib))
+$(eval $(call gb_Package_Package,extensions_OOoSpotlightImporter,$(gb_Library_DLLDIR)))
 
 $(eval $(call gb_Package_set_outdir,extensions_OOoSpotlightImporter,$(gb_INSTROOT)))
 

i18npool/CustomTarget_localedata.mk
@@ -22,7 +22,7 @@ $(call gb_CustomTarget_get_workdir,i18npool/localedata)/localedata_$(1).cxx : \
 	$$(call gb_Output_announce,$$(subst $(WORKDIR)/,,$$@),$(true),SAX,1)
 	$$(call gb_Helper_abbreviate_dirs, \
 		$$(call gb_Helper_execute,saxparser) $(1) $$< $$@.tmp \
-			-env:LO_LIB_DIR=$(call gb_Helper_make_url,$(gb_Helper_OUTDIR_FOR_BUILDLIBDIR)) \
+			-env:LO_LIB_DIR=$(call gb_Helper_make_url,$(gb_INSTROOT)/$(gb_PROGRAMDIRNAME)) \
 			-env:URE_MORE_SERVICES=$(call gb_Helper_make_url,$(call gb_Rdb_get_target_for_build,saxparser)) \
 			$(if $(findstring s,$(MAKEFLAGS)),> /dev/null 2>&1) && \
 		sed 's/\(^.*get[^;]*$$$$\)/SAL_DLLPUBLIC_EXPORT \1/' $$@.tmp > $$@ && \

commit 482ab0d09620f92eb3618125930dc7c4c8335b5e

fdo#75526: sdk: install URE library symlinks / import libs again
This is a bit involved because since the LinkTarget now creates the
instdir/sdk/lib/* files itself a Package cannot be used; so convert the
URE libraries to AutoInstall and add special handling for them to
gb_Helper_register_libraries_for_install to create the necessary links
in the "sdk" install-module.

(regression from 70c35265f517ef372cb739d4cc64499abf57a838)

Change-Id: Ia5467f3303d59f7f5f4a88adc22ceffb82a21ff1

Repository.mk
@@ -611,7 +611,7 @@ $(eval $(call gb_Helper_register_libraries,PLAINLIBS_OOO, \
 ))
 endif
 
-$(eval $(call gb_Helper_register_libraries,RTVERLIBS, \
+$(eval $(call gb_Helper_register_libraries_for_install,RTVERLIBS,ure, \
 	cppuhelper \
 	purpenvhelper \
 	salhelper \
@@ -625,7 +625,7 @@ $(eval $(call gb_Helper_register_libraries,OOOLIBS, \
 	pythonloader \
 ))
 
-$(eval $(call gb_Helper_register_libraries,UNOVERLIBS, \
+$(eval $(call gb_Helper_register_libraries_for_install,UNOVERLIBS,ure, \
 	cppu \
 	sal \
 ))

scp2/inc/macros.inc
@@ -172,6 +172,28 @@ End
         ComponentCondition = cond; \
     End
 
+#ifdef MACOSX
+// links are craeted at configure time by odk/config/setsdkenv_unix.sh.in
+#define SDK_LIBRARY_LINK(id,name,target)
+#elif defined(WNT)
+#define SDK_LIBRARY_LINK(id,name,target) \
+    File id \
+        TXT_FILE_BODY; \
+        Styles = (PACKED); \
+        Dir = gid_Dir_Lib_Sdk; \
+        Name = name \
+    End
+#else
+#define SDK_LIBRARY_LINK(id,name,target) \
+    Unixlink id \
+     BIN_FILE_BODY; \
+     Styles = (); \
+     Dir = gid_Dir_Lib_Sdk; \
+     Name = name; \
+     Target = target; \
+    End
+#endif
+
 #define STD_LIB_FILE(id,name) \
     File id \
         Name   = LIBNAME(name); \

scp2/source/ooo/ure.scp
@@ -94,13 +94,6 @@ End
 
 // Public Dynamic Libraries:
 
-File gid_File_Dl_Cppu
-    LIB_FILE_BODY;
-    Dir = SCP2_URE_DL_DIR;
-    Name = SCP2_URE_DL_UNO_VER("cppu", "3");
-    Styles = (PACKED);
-End
-
 #ifdef AIX
 Unixlink gid_Unixlink_File_Dl_Cppu
     BIN_FILE_BODY;
@@ -110,13 +103,6 @@ Unixlink gid_Unixlink_File_Dl_Cppu
     Target = SCP2_URE_DL_UNO_VER("cppu", "3");
 End
 #endif
-
-File gid_File_Dl_Cppuhelper
-    LIB_FILE_BODY;
-    Dir = SCP2_URE_DL_DIR;
-    Name = SCP2_URE_DL_UNO_COMID_VER("cppuhelper", "3");
-    Styles = (PACKED);
-End
 
 #ifdef AIX
 Unixlink gid_Unixlink_File_Dl_Cppuhelper
@@ -128,13 +114,6 @@ Unixlink gid_Unixlink_File_Dl_Cppuhelper
 End
 #endif
 
-File gid_File_Dl_PurpEnvHelper
-    LIB_FILE_BODY;
-    Dir = SCP2_URE_DL_DIR;
-    Name = SCP2_URE_DL_UNO_COMID_VER("purpenvhelper", "3");
-    Styles = (PACKED);
-End
-
 #ifdef AIX
 Unixlink gid_Unixlink_File_Dl_PurpEnvHelper
     BIN_FILE_BODY;
@@ -145,13 +124,6 @@ Unixlink gid_Unixlink_File_Dl_PurpEnvHelper
 End
 #endif
 
-File gid_File_Dl_Sal
-    LIB_FILE_BODY;
-    Dir = SCP2_URE_DL_DIR;
-    Name = SCP2_URE_DL_UNO_VER("sal", "3");
-    Styles = (PACKED);
-End
-
 #ifdef AIX
 Unixlink gid_Unixlink_File_Dl_Sal
     BIN_FILE_BODY;
@@ -161,13 +133,6 @@ Unixlink gid_Unixlink_File_Dl_Sal
     Target = SCP2_URE_DL_UNO_VER("sal", "3");
 End
 #endif
-
-File gid_File_Dl_Salhelper
-    LIB_FILE_BODY;
-    Dir = SCP2_URE_DL_DIR;
-    Name = SCP2_URE_DL_UNO_COMID_VER("salhelper", "3");
-    Styles = (PACKED);
-End
 
 #ifdef AIX
 Unixlink gid_Unixlink_File_Dl_Salhelper

scp2/source/sdkoo/sdkoo.scp
@@ -59,6 +59,11 @@ Directory gid_Dir_Sdkoo_Bin
     DosName = "bin";
 End
 
+Directory gid_Dir_Lib_Sdk
+    ParentID = gid_Dir_Sdkoo_Sdk;
+    DosName = "lib";
+End
+
 Module gid_Module_Root_SDK
     Name = "sdkoo";
     Description = "sdkoo";

solenv/gbuild/AutoInstall.mk
@@ -25,11 +25,14 @@ $(call gb_AutoInstall_get_target,%) : $(GBUILDDIR)/AutoInstall.mk \
 	echo "/* autogenerated installs for group $* */" > $@
 	$(call gb_AutoInstall__make_define,\
 		$(foreach lib,$(gb_Library_MODULE_$*),auto_$*_lib_$(lib)) \
+		$(foreach lib,$(gb_SdkLinkLibrary_MODULE_$*),auto_$*_link_$(lib)) \
 		$(foreach exe,$(gb_Executable_MODULE_$*),auto_$*_exe_$(exe)) \
 		$(foreach jar,$(gb_Jar_MODULE_$*),auto_$*_jar_$(jar)) \
 		$(foreach pkg,$(gb_Package_MODULE_$*),auto_$*_pkg_$(pkg)))
 	$(foreach lib,$(gb_Library_MODULE_$*),\
 		echo '$(SCP2LIBTEMPLATE)(auto_$*_lib_$(lib),$(call gb_Library_get_runtime_filename,$(lib))$(if $(SCP2COMPONENTCONDITION),$(COMMA)$(SCP2COMPONENTCONDITION)))' >> $@;)
+	$(foreach lib,$(gb_SdkLinkLibrary_MODULE_$*),\
+		echo 'SDK_LIBRARY_LINK(auto_$*_link_$(lib),$(notdir $(call gb_Library_get_sdk_link_lib,$(lib))),../../ure-link/lib/$(call gb_Library_get_runtime_filename,$(lib)))' >> $@;)
 	$(foreach exe,$(gb_Executable_MODULE_$*),\
 		echo "$(SCP2EXETEMPLATE)(auto_$*_exe_$(subst -,_,$(exe)),$(call gb_Executable_get_filename,$(exe)))" >> $@;)
 	$(foreach jar,$(gb_Jar_MODULE_$*),\

solenv/gbuild/Helper.mk
@@ -157,11 +157,15 @@ endef
 
 # the first argument is the group, which sets rpaths etc.
 # the second argument is the install module, which describes in which distro package/msi a lib should show up
+# UGLY: for versioned libraries "sdk" module is hard-coded for now
 define gb_Helper_register_libraries_for_install
 $(if $(3),,$(call gb_Output_error,gb_Helper_register_libraries_for_install: no libraries - need 3 parameters))
 $(call gb_Helper_register_libraries,$(1),$(3))
 
 gb_Library_MODULE_$(2) += $(filter-out $(gb_MERGEDLIBS) $(gb_URELIBS),$(3))
+
+$(if $(filter UNOVERLIBS RTVERLIBS,$(1)),\
+	gb_SdkLinkLibrary_MODULE_sdk += $(3))
 
 endef
 

solenv/gbuild/extensions/post_PackageInfo.mk
@@ -27,6 +27,7 @@ gb_PackageInfo_InstallModules := \
 define gb_PackageInfo_emit_binaries_command
 @touch $(foreach suf,executables libraries files,$(gb_PackageInfo_get_target)/$(1).$(suf))
 @$(foreach executable,$(gb_Executable_MODULE_$(1)),echo "$(patsubst $(INSTDIR)/%,%,$(call gb_Executable_get_target,$(executable)))" >> $(gb_PackageInfo_get_target)/$(1).executables &&) true
+@$(foreach library,$(gb_SdkLinkLibrary_MODULE_$(1)),echo "sdk/lib/$(call gb_Library_get_linktarget,$(library))" >> $(gb_PackageInfo_get_target)/$(1).sdklinklibraries &&) true
 @$(foreach library,$(gb_Library_MODULE_$(1)),echo "$(patsubst $(INSTDIR)/%,%,$(call gb_Library_get_target,$(library)))" >> $(gb_PackageInfo_get_target)/$(1).libraries &&) true
 @$(foreach jar,$(gb_Jar_MODULE_$(1)),echo "$(patsubst $(INSTDIR)/%,%,$(call gb_Jar_get_target,$(jar)))" >> $(gb_PackageInfo_get_target)/$(1).jars &&) true
 @$(foreach pkg,$(gb_Package_MODULE_$(1)),echo "$(call gb_Package_get_target,$(pkg))" >> $(gb_PackageInfo_get_target)/$(1).packages &&) true
@@ -99,6 +100,10 @@ install-package-%: $(gb_PackageInfo_get_target)/packageinfo_all
 	for executable in `cat $(gb_PackageInfo_get_target)/$*.executables`; \
 	do \
 		install -D $(INSTDIR)/$${executable} $(INSTALLDIR)/$${executable} ;\
+	done
+	for library in `cat $(gb_PackageInfo_get_target)/$*.sdklinklibraries`; \
+	do \
+		install -D -m644 $(INSTDIR)/$${library} $(INSTALLDIR)/$${library}; \
 	done
 	for library in `cat $(gb_PackageInfo_get_target)/$*.libraries`; \
 	do \

solenv/gbuild/platform/com_MSC_class.mk
@@ -320,6 +320,7 @@ define gb_Library_get_ilibfilename
 $(patsubst $(1):%,%,$(filter $(1):%,$(gb_Library_ILIBFILENAMES)))
 endef
 
+gb_Library_get_sdk_link_lib = $(gb_Library_get_ilib_target)
 
 # StaticLibrary class
 

solenv/gbuild/platform/macosx.mk
@@ -239,6 +239,8 @@ endef
 
 gb_Library__set_soversion_script_platform = $(gb_Library__set_soversion_script)
 
+gb_Library_get_sdk_link_lib = $(gb_Library_get_versionlink_target)
+
 # bundle is a special kind of library that exists only on Darwin/OSX
 # set the TARGETTYPE to Bundle, and clear install_name(RPATH)
 define gb_Library_Bundle

solenv/gbuild/platform/mingw.mk
@@ -264,6 +264,8 @@ define gb_Library_get_ilibfilename
 $(patsubst $(1):%,%,$(filter $(1):%,$(gb_Library_ILIBFILENAMES)))
 endef
 
+gb_Library_get_sdk_link_lib = $(gb_Library_get_ilib_target)
+
 # Executable class
 
 gb_Executable_EXT := .exe

solenv/gbuild/platform/solaris.mk
@@ -264,6 +264,8 @@ endef
 
 gb_Library__set_soversion_script_platform = $(gb_Library__set_soversion_script)
 
+gb_Library_get_sdk_link_lib = $(gb_Library_get_versionlink_target)
+
 # Executable class
 
 gb_Executable_EXT :=

solenv/gbuild/platform/unxgcc.mk
@@ -262,6 +262,7 @@ endef
 
 gb_Library__set_soversion_script_platform = $(gb_Library__set_soversion_script)
 
+gb_Library_get_sdk_link_lib = $(gb_Library_get_versionlink_target)
 
 # Executable class
 