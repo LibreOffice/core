d985f419e59f01a6e513d732d16b55e772dc6d5a
Matúš Kukan <matus.kukan@collabora.com>
2014-02-27 15:55:40 +0100

    Add CoinMP external as an alternative to lpsolve.
    Change-Id: I9bf5c339a34655c3b842cef3af76814a14c483bc

---
+++ external/coinmp/ExternalPackage_coinmp.mk
@@ -0,0 +1,22 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_ExternalPackage_ExternalPackage,coinmp,coinmp))
+
+$(eval $(call gb_ExternalPackage_use_external_project,coinmp,coinmp))
+
+ifeq ($(OS),WNT)
+$(eval $(call gb_ExternalPackage_add_file,coinmp,$(LIBO_LIB_FOLDER)/CoinMP.dll,CoinMP/MSVisualStudio/v9/release/CoinMP.dll))
+else ifeq ($(OS),MACOSX)
+$(eval $(call gb_ExternalPackage_add_file,coinmp,$(LIBO_LIB_FOLDER)/libCoinMP.dylib,CoinMP/src/.libs/libCoinMP.1.dylib))
+else
+$(eval $(call gb_ExternalPackage_add_file,coinmp,$(LIBO_LIB_FOLDER)/libCoinMP.so,CoinMP/src/.libs/libCoinMP.so.1.7.6))
+endif
+
+# vim: set noet sw=4 ts=4:
