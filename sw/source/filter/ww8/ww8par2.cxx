e88ee8aa307ecec1cc22cda56b97dcfd8b838ce8
Miklos Vajna <vmiklos@collabora.co.uk>
2014-03-04 14:47:13 +0100

    bnc#821208 DOC import: don't overwrite WW8Num* character styles
    In general we're overwriting styles on import in case we're not pasting.
    But these WW8Num* character styles are in general not from the document,
    they are created because Writer needs a character style for each
    numbering level to handle what's in the DOC file.
    
    So, in case there is a character style with the same name as our
    "character style for numbering" styles, prefer the later ones, as that's
    intended most likely.
    
    Change-Id: I675f867722360aca765bb96b0b43ea47deab9847

--- sw/source/filter/ww8/ww8par2.cxx
+++ sw/source/filter/ww8/ww8par2.cxx
@@ -3736,6 +3736,11 @@ bool WW8RStyle::PrepareStyle(SwWW8StyInf &rSI, ww::sti eSti, sal_uInt16 nThisSty
     }
 
     bool bImport = !bStyExist || pIo->mbNewDoc; // import content ?
+
+    // Do not override character styles the list import code created earlier.
+    if (bImport && bStyExist && rSI.GetOrgWWName().startsWith("WW8Num"))
+        bImport = false;
+
     bool bOldNoImp = pIo->bNoAttrImport;
     rSI.bImportSkipped = !bImport;
 
