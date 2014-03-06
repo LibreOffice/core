LibreOffice/core

commit 6536826f2f4c747582d60ed40b0418c6a67a9829

fdo#74792 [DOCX] Grab-bag rels and images for SmartArt
Added support to grab-bag rels and associated Images for
data[i].xml, and drawing[i].xml.
Added UT for the same

Conflicts:
	sw/qa/extras/ooxmlexport/ooxmlexport.cxx
Reviewed on:
	https://gerrit.libreoffice.org/8362

Change-Id: I545825f67214f14037ab72b77764a07d575b8b5b

include/oox/drawingml/shape.hxx
@@ -178,7 +178,8 @@ public:
     const com::sun::star::uno::Sequence<com::sun::star::beans::PropertyValue> &
                         getDiagramDoms() { return maDiagramDoms; }
     void                setDiagramDoms(const com::sun::star::uno::Sequence<com::sun::star::beans::PropertyValue>& rDiagramDoms) { maDiagramDoms = rDiagramDoms; }
-
+    com::sun::star::uno::Sequence< com::sun::star::uno::Sequence< com::sun::star::uno::Any > >resolveRelationshipsOfType(
+                                                                          core::XmlFilterBase& rFilter, OUString sFragment, OUString sType );
 protected:
 
     ::com::sun::star::uno::Reference< ::com::sun::star::drawing::XShape >

oox/source/drawingml/diagram/diagram.cxx
@@ -338,6 +338,9 @@ uno::Sequence<beans::PropertyValue> Diagram::getDomsAsPropertyValues() const
 {
     sal_Int32 length = maMainDomMap.size();
 
+    if ( 0 < maDataRelsMap.getLength() )
+        ++length;
+
     uno::Sequence<beans::PropertyValue> aValue(length);
     beans::PropertyValue* pValue = aValue.getArray();
     for (DiagramDomMap::const_iterator i = maMainDomMap.begin();
@@ -346,6 +349,13 @@ uno::Sequence<beans::PropertyValue> Diagram::getDomsAsPropertyValues() const
     {
         pValue[0].Name = i->first;
         pValue[0].Value = uno::makeAny(i->second);
+        ++pValue;
+    }
+
+    if ( 0 < maDataRelsMap.getLength() )
+    {
+        pValue[0].Name = OUString("OOXDiagramDataRels");
+        pValue[0].Value = uno::makeAny ( maDataRelsMap );
         ++pValue;
     }
 
@@ -410,6 +420,11 @@ void loadDiagram( ShapePtr& pShape,
                        "OOXData",
                        pDiagram,
                        xRefDataModel);
+
+        pDiagram->getDataRelsMap() = pShape->resolveRelationshipsOfType( rFilter, xRefDataModel->getFragmentPath(),
+                                                           "http://schemas.openxmlformats.org/officeDocument/2006/relationships/image" );
+
+
         // Pass the info to pShape
         for( ::std::vector<OUString>::const_iterator aIt = pData->getExtDrawings().begin(), aEnd = pData->getExtDrawings().end();
                 aIt != aEnd; ++aIt )

oox/source/drawingml/shape.cxx
@@ -1200,6 +1200,46 @@ void Shape::putPropertiesToGrabBag( const Sequence< PropertyValue >& aProperties
     }
 }
 
+uno::Sequence< uno::Sequence< uno::Any > >  Shape::resolveRelationshipsOfType(core::XmlFilterBase& rFilter, OUString sFragment, OUString sType )
+{
+    uno::Sequence< uno::Sequence< uno::Any > > xRelListTemp;
+    sal_Int32 counter = 0;
+
+    core::RelationsRef xRels = rFilter.importRelations( sFragment );
+    if ( xRels )
+    {
+        core::RelationsRef xImageRels = xRels->getRelationsFromType( sType );
+        if ( xImageRels )
+        {
+            xRelListTemp.realloc( xImageRels->size() );
+            for( ::std::map< OUString, core::Relation >::const_iterator aIt = xImageRels->begin(), aEnd = xImageRels->end(); aIt != aEnd; ++aIt )
+            {
+                uno::Sequence< uno::Any > diagramRelTuple (3);
+                // [0] => RID, [1] => InputStream [2] => extension
+                OUString sRelId = aIt->second.maId;
+
+                diagramRelTuple[0] = uno::makeAny ( sRelId );
+                OUString sTarget = xImageRels->getFragmentPathFromRelId( sRelId );
+
+                uno::Reference< io::XInputStream > xImageInputStrm( rFilter.openInputStream( sTarget ), uno::UNO_SET_THROW );
+                StreamDataSequence dataSeq;
+                if ( rFilter.importBinaryData( dataSeq, sTarget ) )
+                {
+                    diagramRelTuple[1] = uno::makeAny( dataSeq );
+                }
+
+                diagramRelTuple[2] = uno::makeAny( sTarget.copy( sTarget.lastIndexOf(".") ) );
+
+                xRelListTemp[counter] = diagramRelTuple;
+                ++counter;
+            }
+            xRelListTemp.realloc(counter);
+
+        }
+    }
+    return xRelListTemp;
+}
+
 } }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

oox/source/shape/ShapeContextHandler.cxx
@@ -426,11 +426,20 @@ ShapeContextHandler::getShape() throw (uno::RuntimeException, std::exception)
                     mxFilterBase->importFragment(new ShapeDrawingFragmentHandler(*mxFilterBase, aFragmentPath, pShapePtr));
 
                     uno::Sequence<beans::PropertyValue> aValue(mpShape->getDiagramDoms());
+                    uno::Sequence < uno::Any > diagramDrawing(2);
+                    // drawingValue[0] => dom, drawingValue[1] => Sequence of associated relationships
+
                     sal_Int32 length = aValue.getLength();
                     aValue.realloc(length+1);
+
+                    diagramDrawing[0] = uno::makeAny( mxFilterBase->importFragment( aFragmentPath ) );
+                    diagramDrawing[1] = uno::makeAny( pShapePtr->resolveRelationshipsOfType( *mxFilterBase, aFragmentPath,
+                            "http://schemas.openxmlformats.org/officeDocument/2006/relationships/image" )  );
+
                     beans::PropertyValue* pValue = aValue.getArray();
                     pValue[length].Name = "OOXDrawing";
-                    pValue[length].Value = uno::makeAny( mxFilterBase->importFragment( aFragmentPath ) );
+                    pValue[length].Value = uno::makeAny( diagramDrawing );
+
                     pShapePtr->setDiagramDoms( aValue );
 
                     pShapePtr->addShape( *mxFilterBase, mpThemePtr.get(), xShapes, aMatrix, pShapePtr->getFillProperties() );

oox/source/drawingml/diagram/diagram.hxx
@@ -157,6 +157,7 @@ typedef boost::shared_ptr< LayoutNode > LayoutNodePtr;
 
 
 typedef std::map< OUString, uno::Reference<xml::dom::XDocument> > DiagramDomMap;
+typedef uno::Sequence< uno::Sequence< uno::Any > > DiagramRelsMap;
 
 
 
@@ -296,7 +297,7 @@ public:
     DiagramColorMap& getColors() { return maColors; }
     const DiagramColorMap& getColors() const { return maColors; }
     DiagramDomMap & getDomMap() { return maMainDomMap; }
-
+    DiagramRelsMap & getDataRelsMap() { return maDataRelsMap; }
     void addTo( const ShapePtr & pShape );
 
     uno::Sequence<beans::PropertyValue> getDomsAsPropertyValues() const;
@@ -308,6 +309,7 @@ private:
     DiagramColorMap                maColors;
     std::map< OUString, ShapePtr > maShapeMap;
     DiagramDomMap                  maMainDomMap;
+    DiagramRelsMap                 maDataRelsMap;
 };
 
 

sw/qa/extras/ooxmlexport/ooxmlexport.cxx
@@ -1443,8 +1443,10 @@ DECLARE_OOXMLEXPORT_TEST(testSmartart, "smartart.docx")
       else if (aGrabBag[i].Name == "OOXDrawing")
       {
         bDrawing = sal_True;
+        uno::Sequence< uno::Any > diagramDrawing;
         uno::Reference<xml::dom::XDocument> aDrawingDom;
-        CPPUNIT_ASSERT(aGrabBag[i].Value >>= aDrawingDom); // PropertyValue of proper type
+        CPPUNIT_ASSERT(aGrabBag[i].Value >>= diagramDrawing);
+        CPPUNIT_ASSERT(diagramDrawing[0] >>= aDrawingDom); // PropertyValue of proper type
         CPPUNIT_ASSERT(aDrawingDom.get()); // Reference not empty
       }
     }
@@ -2938,6 +2940,26 @@ DECLARE_OOXMLEXPORT_TEST(testCompatSettingsForW14, "TextEffects_StylisticSets_Cn
     assertXPath(pXmlDoc, "/w:settings/w:compat/w:compatSetting[5]", "name", "differentiateMultirowTableHeaders");
     assertXPath(pXmlDoc, "/w:settings/w:compat/w:compatSetting[5]", "uri", "http://schemas.microsoft.com/office/word");
     assertXPath(pXmlDoc, "/w:settings/w:compat/w:compatSetting[5]", "val", "1");
+}
+
+DECLARE_OOXMLEXPORT_TEST(testFdo74792, "fdo74792.docx")
+{
+    /*
+     * fdo#74792 : The images associated with smart-art data[i].xml
+     * were not preserved on exporting to DOCX format
+     * Added support to grabbag the rels, with associated images.
+     */
+    xmlDocPtr pXmlDoc = parseExport("word/diagrams/_rels/data1.xml.rels");
+    if(!pXmlDoc)
+        return;
+    assertXPath(pXmlDoc,"/rels:Relationships/rels:Relationship", 4);
+    uno::Reference<packages::zip::XZipFileAccess2> xNameAccess = packages::zip::ZipFileAccess::createWithURL(
+                         comphelper::getComponentContext(m_xSFactory), m_aTempFile.GetURL());
+
+    //check that images are also saved
+    OUString sImageFile( "word/media/OOXDiagramDataRels0.jpeg" );
+    uno::Reference<io::XInputStream> xInputStream(xNameAccess->getByName( sImageFile ), uno::UNO_QUERY);
+    CPPUNIT_ASSERT( xInputStream.is() );
 }
 
 #endif

sw/source/filter/ww8/docxsdrexport.cxx
@@ -25,6 +25,7 @@
 #include <oox/drawingml/drawingmltypes.hxx>
 #include <oox/export/utils.hxx>
 #include <oox/export/vmlexport.hxx>
+#include <oox/token/properties.hxx>
 
 #include <frmatr.hxx>
 #include <frmfmt.hxx>
@@ -41,6 +42,7 @@
 #include <docxexport.hxx>
 #include <docxexportfilter.hxx>
 #include <writerhelper.hxx>
+#include <comphelper/seqstream.hxx>
 
 using namespace com::sun::star;
 using namespace oox;
@@ -683,6 +685,78 @@ void DocxSdrExport::writeDMLEffectLst(const SwFrmFmt& rFrmFmt)
 
 }
 
+void DocxSdrExport::writeDiagramRels(uno::Reference<xml::dom::XDocument> xDom,
+                                     uno::Sequence< uno::Sequence< uno::Any > > xRelSeq,
+                                     uno::Reference< io::XOutputStream > xOutStream, OUString sGrabBagProperyName)
+{
+    // add image relationships of OOXData, OOXDiagram
+    OUString sType("http://schemas.openxmlformats.org/officeDocument/2006/relationships/image");
+    uno::Reference< xml::sax::XSAXSerializable > xSerializer(xDom, uno::UNO_QUERY);
+    uno::Reference< xml::sax::XWriter > xWriter = xml::sax::Writer::create(comphelper::getProcessComponentContext());
+    xWriter->setOutputStream(xOutStream);
+
+    // retrieve the relationships from Sequence
+    for (sal_Int32 j = 0; j < xRelSeq.getLength(); j++)
+    {
+        // diagramDataRelTuple[0] => RID,
+        // diagramDataRelTuple[1] => xInputStream
+        // diagramDataRelTuple[2] => extension
+        uno::Sequence< uno::Any > diagramDataRelTuple = xRelSeq[j];
+
+        OUString sRelId, sExtension;
+        diagramDataRelTuple[0] >>= sRelId;
+        diagramDataRelTuple[2] >>= sExtension;
+        OUString sContentType;
+        if (sExtension == ".jpeg")
+            sContentType = "image/jpeg";
+        else if (sExtension == ".WMF")
+            sContentType = "image/x-wmf";
+        sRelId = sRelId.copy(3);
+
+        StreamDataSequence dataSeq;
+        diagramDataRelTuple[1] >>= dataSeq;
+        uno::Reference<io::XInputStream> dataImagebin(new ::comphelper::SequenceInputStream(dataSeq));
+
+        OUString sFragment("../media/");
+        sFragment += sGrabBagProperyName + OUString::number(j) + sExtension;
+
+        PropertySet aProps(xOutStream);
+        aProps.setAnyProperty(PROP_RelId, uno::makeAny(sal_Int32(sRelId.toInt32())));
+
+        m_pImpl->m_rExport.GetFilter().addRelation(xOutStream, sType, sFragment);
+
+        sFragment = sFragment.replaceFirst("..","word");
+        uno::Reference< io::XOutputStream > xBinOutStream = m_pImpl->m_rExport.GetFilter().openFragmentStream(sFragment, sContentType);
+
+        try
+        {
+            sal_Int32 nBufferSize = 512;
+            uno::Sequence< sal_Int8 > aDataBuffer(nBufferSize);
+            sal_Int32 nRead;
+            do
+            {
+                nRead = dataImagebin->readBytes(aDataBuffer, nBufferSize);
+                if (nRead)
+                {
+                    if (nRead < nBufferSize)
+                    {
+                        nBufferSize = nRead;
+                        aDataBuffer.realloc(nRead);
+                    }
+                    xBinOutStream->writeBytes(aDataBuffer);
+                }
+            }
+            while (nRead);
+            xBinOutStream->flush();
+        }
+        catch (const uno::Exception& rException)
+        {
+            SAL_WARN("sw.ww8", "DocxSdrExport::writeDiagramRels Failed to copy grabbaged Image: " << rException.Message);
+        }
+        dataImagebin->closeInput();
+    }
+}
+
 void DocxSdrExport::writeDiagram(const SdrObject* sdrObject, const SwFrmFmt& rFrmFmt,  int nAnchorId)
 {
     sax_fastparser::FSHelperPtr pFS = m_pImpl->m_pSerializer;
@@ -694,6 +768,8 @@ void DocxSdrExport::writeDiagram(const SdrObject* sdrObject, const SwFrmFmt& rFr
     uno::Reference<xml::dom::XDocument> styleDom;
     uno::Reference<xml::dom::XDocument> colorDom;
     uno::Reference<xml::dom::XDocument> drawingDom;
+    uno::Sequence< uno::Sequence< uno::Any > > xDataRelSeq;
+    uno::Sequence< uno::Any > diagramDrawing;
 
     // retrieve the doms from the GrabBag
     OUString pName = UNO_NAME_MISC_OBJ_INTEROPGRABBAG;
@@ -711,9 +787,12 @@ void DocxSdrExport::writeDiagram(const SdrObject* sdrObject, const SwFrmFmt& rFr
         else if (propName == "OOXColor")
             propList[nProp].Value >>= colorDom;
         else if (propName == "OOXDrawing")
-            propList[nProp].Value >>= drawingDom;
+            propList[nProp].Value >>= diagramDrawing;
+        else if (propName == "OOXDiagramDataRels")
+            propList[nProp].Value >>= xDataRelSeq;
     }
 
+    diagramDrawing[0] >>= drawingDom;
     // check that we have the 4 mandatory XDocuments
     // if not, there was an error importing and we won't output anything
     if (!dataDom.is() || !layoutDom.is() || !styleDom.is() || !colorDom.is())
@@ -750,6 +829,7 @@ void DocxSdrExport::writeDiagram(const SdrObject* sdrObject, const SwFrmFmt& rFr
     OString dataRelId = OUStringToOString(m_pImpl->m_rExport.GetFilter().addRelation(pFS->getOutputStream(),
                                           "http://schemas.openxmlformats.org/officeDocument/2006/relationships/diagramData",
                                           dataFileName, false), RTL_TEXTENCODING_UTF8);
+
 
     // add layout relation
     OUString layoutFileName = "diagrams/layout" + OUString::number(diagramCount) + ".xml";
@@ -814,10 +894,14 @@ void DocxSdrExport::writeDiagram(const SdrObject* sdrObject, const SwFrmFmt& rFr
 
     // write data file
     serializer.set(dataDom, uno::UNO_QUERY);
-    writer->setOutputStream(m_pImpl->m_rExport.GetFilter().openFragmentStream("word/" + dataFileName,
-                            "application/vnd.openxmlformats-officedocument.drawingml.diagramData+xml"));
+    uno::Reference< io::XOutputStream > xDataOutputStream = m_pImpl->m_rExport.GetFilter().openFragmentStream(
+                "word/" + dataFileName, "application/vnd.openxmlformats-officedocument.drawingml.diagramData+xml");
+    writer->setOutputStream(xDataOutputStream);
     serializer->serialize(uno::Reference< xml::sax::XDocumentHandler >(writer, uno::UNO_QUERY_THROW),
                           uno::Sequence< beans::StringPair >());
+
+    // write the associated Images and rels for data file
+    writeDiagramRels(dataDom, xDataRelSeq, xDataOutputStream, OUString("OOXDiagramDataRels"));
 
     // write layout file
     serializer.set(layoutDom, uno::UNO_QUERY);
@@ -841,13 +925,20 @@ void DocxSdrExport::writeDiagram(const SdrObject* sdrObject, const SwFrmFmt& rFr
                           uno::Sequence< beans::StringPair >());
 
     // write drawing file
+
     if (drawingDom.is())
     {
         serializer.set(drawingDom, uno::UNO_QUERY);
-        writer->setOutputStream(m_pImpl->m_rExport.GetFilter().openFragmentStream("word/" + drawingFileName,
-                                "application/vnd.openxmlformats-officedocument.drawingml.diagramDrawing+xml"));
+        uno::Reference< io::XOutputStream > xDrawingOutputStream = m_pImpl->m_rExport.GetFilter().openFragmentStream("word/" + drawingFileName,
+                "application/vnd.openxmlformats-officedocument.drawingml.diagramDrawing+xml");
+        writer->setOutputStream(xDrawingOutputStream);
         serializer->serialize(uno::Reference< xml::sax::XDocumentHandler >(writer, uno::UNO_QUERY_THROW),
                               uno::Sequence< beans::StringPair >());
+
+        // write the associated Images and rels for drawing file
+        uno::Sequence< uno::Sequence< uno::Any > > xDrawingRelSeq;
+        diagramDrawing[1] >>= xDrawingRelSeq;
+        writeDiagramRels(drawingDom, xDrawingRelSeq, xDrawingOutputStream, OUString("OOXDiagramDrawingRels"));
     }
 }
 

commit 291c3ae79c45bc470f8a903a2541b10e9784fac5

fix OOXML strict import
fix the problem introduced by 6536826f2f4c747582d60ed40b0418c6a67a9829

Change-Id: I6c499f8490d92badda0f086d48c2347fa112bb4e

include/oox/drawingml/shape.hxx
@@ -178,7 +178,7 @@ public:
     const com::sun::star::uno::Sequence<com::sun::star::beans::PropertyValue> &
                         getDiagramDoms() { return maDiagramDoms; }
     void                setDiagramDoms(const com::sun::star::uno::Sequence<com::sun::star::beans::PropertyValue>& rDiagramDoms) { maDiagramDoms = rDiagramDoms; }
-    com::sun::star::uno::Sequence< com::sun::star::uno::Sequence< com::sun::star::uno::Any > >resolveRelationshipsOfType(
+    com::sun::star::uno::Sequence< com::sun::star::uno::Sequence< com::sun::star::uno::Any > >resolveRelationshipsOfTypeFromOfficeDoc(
                                                                           core::XmlFilterBase& rFilter, OUString sFragment, OUString sType );
 protected:
 

oox/source/drawingml/diagram/diagram.cxx
@@ -421,8 +421,8 @@ void loadDiagram( ShapePtr& pShape,
                        pDiagram,
                        xRefDataModel);
 
-        pDiagram->getDataRelsMap() = pShape->resolveRelationshipsOfType( rFilter, xRefDataModel->getFragmentPath(),
-                                                           "http://schemas.openxmlformats.org/officeDocument/2006/relationships/image" );
+        pDiagram->getDataRelsMap() = pShape->resolveRelationshipsOfTypeFromOfficeDoc( rFilter,
+                xRefDataModel->getFragmentPath(), "image" );
 
 
         // Pass the info to pShape

oox/source/drawingml/shape.cxx
@@ -1200,7 +1200,7 @@ void Shape::putPropertiesToGrabBag( const Sequence< PropertyValue >& aProperties
     }
 }
 
-uno::Sequence< uno::Sequence< uno::Any > >  Shape::resolveRelationshipsOfType(core::XmlFilterBase& rFilter, OUString sFragment, OUString sType )
+uno::Sequence< uno::Sequence< uno::Any > >  Shape::resolveRelationshipsOfTypeFromOfficeDoc(core::XmlFilterBase& rFilter, OUString sFragment, OUString sType )
 {
     uno::Sequence< uno::Sequence< uno::Any > > xRelListTemp;
     sal_Int32 counter = 0;
@@ -1208,7 +1208,7 @@ uno::Sequence< uno::Sequence< uno::Any > >  Shape::resolveRelationshipsOfType(co
     core::RelationsRef xRels = rFilter.importRelations( sFragment );
     if ( xRels )
     {
-        core::RelationsRef xImageRels = xRels->getRelationsFromType( sType );
+        core::RelationsRef xImageRels = xRels->getRelationsFromTypeFromOfficeDoc( sType );
         if ( xImageRels )
         {
             xRelListTemp.realloc( xImageRels->size() );

oox/source/shape/ShapeContextHandler.cxx
@@ -433,8 +433,8 @@ ShapeContextHandler::getShape() throw (uno::RuntimeException, std::exception)
                     aValue.realloc(length+1);
 
                     diagramDrawing[0] = uno::makeAny( mxFilterBase->importFragment( aFragmentPath ) );
-                    diagramDrawing[1] = uno::makeAny( pShapePtr->resolveRelationshipsOfType( *mxFilterBase, aFragmentPath,
-                            "http://schemas.openxmlformats.org/officeDocument/2006/relationships/image" )  );
+                    diagramDrawing[1] = uno::makeAny( pShapePtr->resolveRelationshipsOfTypeFromOfficeDoc(
+                                *mxFilterBase, aFragmentPath, "image" )  );
 
                     beans::PropertyValue* pValue = aValue.getArray();
                     pValue[length].Name = "OOXDrawing";