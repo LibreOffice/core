7d7f91aead9af7e0d3d262663dea4b3c96c3072b
Caolán McNamara <caolanm@redhat.com>
2014-03-06 11:32:46 +0000

    coverity#1190366 Uncaught exception
    Change-Id: I18e732d8818ea9b6c0837b0df2bd58cb13066ec6

--- filter/qa/cppunit/xslt-test.cxx
+++ filter/qa/cppunit/xslt-test.cxx
@@ -61,7 +61,7 @@ struct Listener : public ::cppu::WeakImplHelper1<io::XStreamListener>
     virtual void SAL_CALL started() throw() SAL_OVERRIDE { m_bDone = false; }
     virtual void SAL_CALL closed() throw() SAL_OVERRIDE { m_bDone = true; }
     virtual void SAL_CALL terminated() throw() SAL_OVERRIDE { m_bDone = true; }
-    virtual void SAL_CALL error(const uno::Any& e) throw() SAL_OVERRIDE
+    virtual void SAL_CALL error(const uno::Any& e) throw(uno::RuntimeException, std::exception) SAL_OVERRIDE
     {
         m_bDone = true; // set on error too, otherwise main thread waits forever
         SAL_WARN("filter.xslt", "exception " << e);
