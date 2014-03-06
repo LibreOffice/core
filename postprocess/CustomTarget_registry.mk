71ed01e2edb27d09104a412425d5b99247efa4d4
Stephan Bergmann <sbergman@redhat.com>
2014-03-06 20:17:48 +0100

    Revert "install:module <value>s must be mutually exclusive"
    This reverts commit 768ea2924680fc4beb75a782cb0faf26695fee53, which was
    completely misguided---configmgr allows oor:external values to let a "lower"
    value shine through when the backend says it cannot produce a value, even when
    that "lower" value comes from the same layer (as is e.g. already used in
    officecfg/registry/data/org/openoffice/VCL.xcu).
    
    Conflicts:
    	postprocess/CustomTarget_registry.mk
    
    Change-Id: Ic8f0c9c931e2c73c9bfb8fbe5a57f60ad6aebde1

--- postprocess/CustomTarget_registry.mk
+++ postprocess/CustomTarget_registry.mk
@@ -331,10 +331,6 @@ postprocess_FILES_main += \
 		# Inet-wnt.xcu must come after Inet.xcu
 postprocess_DRIVERS += ado
 endif
-ifneq (unx,$(GUIBASE))
-postprocess_FILES_main += \
-	$(postprocess_MOD)/org/openoffice/Office/Paths-notunixdesktop.xcu
-endif
 ifneq ($(WITH_WEBDAV),)
 postprocess_FILES_main += $(postprocess_MOD)/org/openoffice/ucb/Configuration-neon.xcu
 endif
