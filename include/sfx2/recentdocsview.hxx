LibreOffice/core

commit 0314034d8b23d5b69399cfcdf6b2f2add73f9f47

fdo#71628 Startcenter: remove thumbnail selection when focus is moved away
mnHighItemId is never assaigned with a valid value and there is
no sense to store one selected element's index anyway,
because there can be more.
The bMove parameter of ImplGetItem is never used and doesn' work
so remove it.

Change-Id: I91d6bc3fd13a2b44e6961fea66d02702a8f31c2b

sfx2/source/control/thumbnailview.cxx
@@ -149,7 +149,6 @@ void ThumbnailView::ImplInit()
     mnLines             = 0;
     mnFineness          = 5;
     mnFirstLine         = 0;
-    mnHighItemId        = 0;
     mnCols              = 0;
     mnSpacing           = 0;
     mbScroll            = false;
@@ -450,7 +449,7 @@ void ThumbnailView::CalculateItemPositions ()
     delete pDelScrBar;
 }
 
-size_t ThumbnailView::ImplGetItem( const Point& rPos, bool bMove ) const
+size_t ThumbnailView::ImplGetItem( const Point& rPos ) const
 {
     if ( !mbHasVisibleItems )
     {
@@ -463,13 +462,6 @@ size_t ThumbnailView::ImplGetItem( const Point& rPos, bool bMove ) const
         {
             if (mFilteredItemList[i]->isVisible() && mFilteredItemList[i]->getDrawArea().IsInside(rPos))
                 return i;
-        }
-
-        // return the previously selected item if spacing is set and
-        // the mouse hasn't left the window yet
-        if ( bMove && mnSpacing && mnHighItemId )
-        {
-            return GetItemPos( mnHighItemId );
         }
     }
 
@@ -926,22 +918,7 @@ void ThumbnailView::GetFocus()
 
 void ThumbnailView::LoseFocus()
 {
-    if (mnHighItemId)
-    {
-        size_t nPos = GetItemPos(mnHighItemId);
-
-        if (nPos != THUMBNAILVIEW_ITEM_NOTFOUND)
-        {
-            ThumbnailViewItem *pOld = mItemList[nPos];
-
-            pOld->setHighlight(false);
-
-            if (!pOld->isSelected())
-                DrawItem(pOld);
-        }
-
-        mnHighItemId = 0;
-    }
+    deselectItems();
 
     Control::LoseFocus();
 
@@ -1049,12 +1026,6 @@ void ThumbnailView::RemoveItem( sal_uInt16 nItemId )
         mpStartSelRange = mFilteredItemList.end();
     }
 
-    // reset variables
-    if ( mnHighItemId == nItemId )
-    {
-        mnHighItemId    = 0;
-    }
-
     CalculateItemPositions();
 
     if ( IsReallyVisible() && IsUpdateMode() )
@@ -1067,7 +1038,6 @@ void ThumbnailView::Clear()
 
     // reset variables
     mnFirstLine     = 0;
-    mnHighItemId    = 0;
 
     CalculateItemPositions();
 
@@ -1081,7 +1051,6 @@ void ThumbnailView::updateItems (const std::vector<ThumbnailViewItem*> &items)
 
     // reset variables
     mnFirstLine     = 0;
-    mnHighItemId    = 0;
 
     mItemList = items;
 

include/sfx2/thumbnailview.hxx
@@ -300,7 +300,7 @@ protected:
     SFX2_DLLPRIVATE void         ImplInitScrollBar();
     SFX2_DLLPRIVATE void         ImplDeleteItems();
     SFX2_DLLPRIVATE void         ImplDraw();
-    SFX2_DLLPRIVATE size_t       ImplGetItem( const Point& rPoint, bool bMove = false ) const;
+    SFX2_DLLPRIVATE size_t       ImplGetItem( const Point& rPoint ) const;
     SFX2_DLLPRIVATE ThumbnailViewItem*    ImplGetItem( size_t nPos );
     SFX2_DLLPRIVATE sal_uInt16          ImplGetVisibleItemCount() const;
     SFX2_DLLPRIVATE ThumbnailViewItem*    ImplGetVisibleItem( sal_uInt16 nVisiblePos );
@@ -328,7 +328,6 @@ protected:
 
     int mnFineness;
 
-    sal_uInt16 mnHighItemId;
     sal_uInt16 mnCols;
     sal_uInt16 mnFirstLine;
     sal_uInt16 mnSpacing;

commit 0dbf2d654203d79ce049bf72e31ef4fa75e333ba

fdo#74787, rhbz#1072553: Fix deselection problems of template view
deselectItems() were added to make recent docs
view work, but it is unneccessary to
affect template view too.

(regression from 0314034d8b23d5b69399cfcdf6b2f2add73f9f47)

Conflicts:
	sfx2/source/control/recentdocsview.cxx

Change-Id: I0c805774321939991e308e9c8c669604e81dbad5

include/sfx2/recentdocsview.hxx
@@ -66,6 +66,8 @@ protected:
     void OpenItem( const ThumbnailViewItem *pItem );
     virtual void Paint( const Rectangle& rRect );
 
+    virtual void LoseFocus();
+
     bool isAcceptedFile(const OUString &rURL) const;
 
     long    mnItemMaxSize;

sfx2/source/control/recentdocsview.cxx
@@ -302,6 +302,13 @@ void RecentDocsView::Paint( const Rectangle &aRect )
         ThumbnailView::Paint(aRect);
 }
 
+void RecentDocsView::LoseFocus()
+{
+    deselectItems();
+
+    ThumbnailView::LoseFocus();
+}
+
 void RecentDocsView::SetThumbnailSize(long thumbnailSize)
 {
     mnItemMaxSize = thumbnailSize;

sfx2/source/control/thumbnailview.cxx
@@ -918,8 +918,6 @@ void ThumbnailView::GetFocus()
 
 void ThumbnailView::LoseFocus()
 {
-    deselectItems();
-
     Control::LoseFocus();
 
     // Tell the accessible object that we lost the focus.