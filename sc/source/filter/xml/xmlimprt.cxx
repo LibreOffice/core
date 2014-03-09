78d7bc222c01614a783450bb06036b8770d82a7a
Kohei Yoshida <kohei.yoshida@collabora.com>
2014-03-09 18:40:13 -0400

    fdo#75960: Process pivot tables after named ranges are all set.
    Especially for those pivot tables that reference named ranges as their
    data sources, it's critical that we process them after named ranges are
    set. Else things would start to fail.
    
    Change-Id: I4bf8aa1a844aae3953f2dfbeba0e4d2542a7e53f
    (cherry picked from commit 057d269c5d1faf45c4c935b2f8120c45e646de65)
    (cherry picked from commit 98bd1911420f46380aa5373a92de6a748ced080d)

--- sc/source/filter/xml/xmlimprt.cxx
+++ sc/source/filter/xml/xmlimprt.cxx
@@ -67,6 +67,7 @@
 #include "editutil.hxx"
 #include "editattributemap.hxx"
 #include "documentimport.hxx"
+#include "pivotsource.hxx"
 
 #include <comphelper/extract.hxx>
 
@@ -1961,6 +1962,14 @@ sc::ImportPostProcessData* ScXMLImport::GetPostProcessData()
     return mpPostProcessData;
 }
 
+sc::PivotTableSources& ScXMLImport::GetPivotTableSources()
+{
+    if (!mpPivotSources)
+        mpPivotSources.reset(new sc::PivotTableSources);
+
+    return *mpPivotSources;
+}
+
 SvXMLImportContext *ScXMLImport::CreateContext( sal_uInt16 nPrefix,
                                                const OUString& rLocalName,
                                                const uno::Reference<xml::sax::XAttributeList>& xAttrList )
@@ -3243,6 +3252,9 @@ throw( ::com::sun::star::xml::sax::SAXException, ::com::sun::star::uno::RuntimeE
             SetLabelRanges();
             SetNamedRanges();
             SetSheetNamedRanges();
+            if (mpPivotSources)
+                // Process pivot table sources after the named ranges have been set.
+                mpPivotSources->process();
         }
         GetProgressBarHelper()->End();  // make room for subsequent SfxProgressBars
         if (pDoc)
