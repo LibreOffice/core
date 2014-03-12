012de8a0056bd75299ef97d03a95905c272cc26c
Muthu Subramanian <sumuthu@collabora.com>
2014-03-12 16:44:10 +0530

    n#821567: Import PPTX background images with table-name.
    Ported from 2ec4d410de5bd98527336a9dc49abb76656373df
    
    Change-Id: I50693f7fea8de4f1d3668378aa2334539dd80c42
    Reviewed-on: https://gerrit.libreoffice.org/8546
    Reviewed-by: Andras Timar <andras.timar@collabora.com>
    Tested-by: Andras Timar <andras.timar@collabora.com>

--- oox/source/drawingml/shapepropertymap.cxx
+++ oox/source/drawingml/shapepropertymap.cxx
@@ -47,7 +47,8 @@ static const sal_Int32 spnDefaultShapeIds[ SHAPEPROP_END ] =
     PROP_FillBitmapURL, PROP_FillBitmapMode, PROP_FillBitmapSizeX, PROP_FillBitmapSizeY,
     PROP_FillBitmapPositionOffsetX, PROP_FillBitmapPositionOffsetY, PROP_FillBitmapRectanglePoint,
     PROP_FillHatch,
-    PROP_ShadowXDistance
+    PROP_ShadowXDistance,
+    PROP_FillBitmapName
 };
 
 } // namespace
@@ -107,6 +108,9 @@ bool ShapePropertyMap::setAnyProperty( ShapePropertyId ePropId, const Any& rValu
 
         case SHAPEPROP_FillBitmapUrl:
             return setFillBitmapUrl( nPropId, rValue );
+
+        case SHAPEPROP_FillBitmapNameFromUrl:
+            return setFillBitmapNameFromUrl( nPropId, rValue );
 
         default:;   // suppress compiler warnings
     }
@@ -192,6 +196,16 @@ bool ShapePropertyMap::setFillBitmapUrl( sal_Int32 nPropId, const Any& rValue )
         return !aBitmapUrlName.isEmpty() && setProperty( nPropId, aBitmapUrlName );
     }
 
+    return false;
+}
+
+bool ShapePropertyMap::setFillBitmapNameFromUrl( sal_Int32 /*nPropId*/, const Any& rValue )
+{
+    if( rValue.has< OUString >() )
+    {
+        OUString aBitmapUrlName = mrModelObjHelper.insertFillBitmapUrl( rValue.get< OUString >() );
+        return !aBitmapUrlName.isEmpty() && setProperty( PROP_FillBitmapName, aBitmapUrlName );
+    }
     return false;
 }
 
