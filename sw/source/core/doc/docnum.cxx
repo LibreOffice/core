5483a31bfb3d3d0a44e198da196432cc430c67f5
Tor Lillqvist <tml@collabora.com>
2014-03-14 08:33:20 +0200

    WaE: expression result unused
    Change-Id: Ic0bb1e059817ff0f0c056720894b025222c6c5b4

--- sw/source/core/doc/docnum.cxx
+++ sw/source/core/doc/docnum.cxx
@@ -385,7 +385,7 @@ bool SwDoc::OutlineUpDown( const SwPaM& rPam, short nOffset )
                 pColl = aCollArr[ aMoveArr[nLevel] ];
 
                 if (pColl != NULL)
-                    (SwTxtFmtColl*)pTxtNd->ChgFmtColl( pColl );
+                    pTxtNd->ChgFmtColl( pColl );
             }
 
         }
