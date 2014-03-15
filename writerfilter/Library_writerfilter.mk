9ab8c3b3b74070f5e094f98dc8fa2963b1aad208
Miklos Vajna <vmiklos@collabora.co.uk>
2014-03-15 20:59:47 +0100

    unused WW8BinaryObjHandler
    Change-Id: Ief8c158a25393cc4e6259ebf9557e76b70554a63

--- writerfilter/Library_writerfilter.mk
+++ writerfilter/Library_writerfilter.mk
@@ -147,7 +147,6 @@ $(eval $(call gb_Library_add_exception_objects,writerfilter,\
     writerfilter/source/resourcemodel/TagLogger \
     writerfilter/source/resourcemodel/XPathLogger \
     writerfilter/source/resourcemodel/qnametostrcore \
-    writerfilter/source/resourcemodel/resourcemodel \
     writerfilter/source/resourcemodel/util \
 ))
 
