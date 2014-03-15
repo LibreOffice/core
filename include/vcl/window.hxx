d659c8bbc2204b2735b45692b624df265d268f1d
Chris Sherlock <chris.sherlock79@gmail.com>
2014-03-16 02:22:20 +1100

    fdo#74702 Moved CopyArea() Window specific function
    There is Window specific code in OutputDevice::CopyArea(...).
    I have moved this to a protected function CopyAreaFinal(...).
    
    Change-Id: I9098ec960527a2aca6154ac6e791b947a8e4f78e

--- include/vcl/window.hxx
+++ include/vcl/window.hxx
@@ -577,6 +577,8 @@ protected:
     virtual sal_uInt16 getDefaultAccessibleRole() const;
     virtual OUString getDefaultAccessibleName() const;
 
+    virtual void               CopyAreaFinal( SalTwoRect& aPosAry, sal_uInt32 nFlags);
+
 public:
     bool HasMirroredGraphics() const;
 
