509441038ab95dd3a60efd1b6c302bf22bfbc631
Keith Curtis <keithcu@gmail.com>
2014-03-17 20:16:57 -0400

    Hopefully fix Windows HiDPI toolbar layout bug
    On Windows HiDPI, toolbar buttons are cut off. This may fix the problem.
    
    Here is a screenshot:
    http://i.imgur.com/NADAvYi.png
    
    I can't prove this fixes anything on Windows because I can't see this
    on Linux and don't really understand the surrounding code. On the other
    hand, it is easy to prove this is reasonable code.
    
    Change-Id: I69c19ad46844bead942ce63883d163cb9d0690c9
    Reviewed-on: https://gerrit.libreoffice.org/8637
    Tested-by: LibreOffice gerrit bot <gerrit@libreoffice.org>
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>

--- vcl/source/window/toolbox.cxx
+++ vcl/source/window/toolbox.cxx
@@ -1736,8 +1736,8 @@ bool ToolBox::ImplCalcItem()
     long            nDropDownArrowWidth = TB_DROPDOWNARROWWIDTH;
 
     // set defaults if image or text is needed but empty
-    nDefWidth       = GetDefaultImageSize().Width();
-    nDefHeight      = GetDefaultImageSize().Height();
+    nDefWidth       = GetDefaultImageSize().Width() * GetDPIScaleFactor();
+    nDefHeight      = GetDefaultImageSize().Height() * GetDPIScaleFactor();
 
     mnWinHeight = 0;
     // determine minimum size necessary in NWF
