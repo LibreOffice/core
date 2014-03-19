LibreOffice/core

commit 75934fc178ed90b56d77eb1efebed5bdac8427b6

VML import: handle shape with text inside groupshape as TextShape
This matches the behavior of the WW8 import and gives us the required
text wrapping when the shape text doesn't fit in a single line.

Change-Id: I32a13516503620344d313593834be29a3dc9f726

oox/source/vml/vmlshapecontext.cxx
@@ -459,6 +459,8 @@ ContextHandlerRef ShapeContext::onCreateContext( sal_Int32 nElement, const Attri
                 dynamic_cast<SimpleShape&>( mrShape ).setService(
                         "com.sun.star.text.TextFrame");
             }
+            else
+                dynamic_cast<SimpleShape&>(mrShape).setService("com.sun.star.drawing.TextShape");
             return new TextBoxContext( *this, mrShapeModel.createTextBox(mrShape.getTypeModel()), rAttribs,
                 mrShape.getDrawing().getFilter().getGraphicHelper());
         case VMLX_TOKEN( ClientData ):

sw/qa/extras/ooxmlimport/ooxmlimport.cxx
@@ -975,6 +975,10 @@ DECLARE_OOXMLIMPORT_TEST(testGroupshapeChildRotation, "groupshape-child-rotation
     xShape.set(xGroupShape->getByIndex(4), uno::UNO_QUERY);
     // This was 887, i.e. border distances were included in the height.
     CPPUNIT_ASSERT_EQUAL(sal_Int32(686), xShape->getSize().Height);
+
+    uno::Reference<drawing::XShapeDescriptor> xShapeDescriptor(xGroupShape->getByIndex(5), uno::UNO_QUERY);
+    // This was com.sun.star.drawing.RectangleShape, all shape text in a single line.
+    CPPUNIT_ASSERT_EQUAL(OUString("com.sun.star.drawing.TextShape"), xShapeDescriptor->getShapeType());
 }
 
 DECLARE_OOXMLIMPORT_TEST(testN793262, "n793262.docx")

commit 81f9fe3a14f0fc99afbfa7ce3a26a9c7855d0919

fdo#74401 VML groupshape import: only handle v:rect as TextShape
Regression from commit 75934fc178ed90b56d77eb1efebed5bdac8427b6 (VML
import: handle shape with text inside groupshape as TextShape,
2014-01-17), the problem was that TextShape is also a kind of rectangle,
so that shouldn't be used for all shape with text in groupshapes, just
for v:rect elements.

This fix gives the required text wrapping for the referenced bugdoc
without turning the rest of the shapes into rectangles.

Change-Id: I6e343c65a869e335b641728adbc7984ee85377b5

oox/source/vml/vmlshapecontext.cxx
@@ -459,7 +459,8 @@ ContextHandlerRef ShapeContext::onCreateContext( sal_Int32 nElement, const Attri
                 dynamic_cast<SimpleShape&>( mrShape ).setService(
                         "com.sun.star.text.TextFrame");
             }
-            else
+            else if (getCurrentElement() == VML_TOKEN(rect))
+                // Transform only rectangles into a TextShape inside a groupshape.
                 dynamic_cast<SimpleShape&>(mrShape).setService("com.sun.star.drawing.TextShape");
             return new TextBoxContext( *this, mrShapeModel.createTextBox(mrShape.getTypeModel()), rAttribs,
                 mrShape.getDrawing().getFilter().getGraphicHelper());

sw/qa/extras/ooxmlimport/ooxmlimport.cxx
@@ -2004,6 +2004,15 @@ DECLARE_OOXMLIMPORT_TEST(testFdo38414, "fdo38414.docx" )
     OUString height4 = parseDump("/root/page/body/tab/row[1]/cell[4]/infos/bounds", "height" );
     CPPUNIT_ASSERT_EQUAL( height3, height4 );
 }
+
+DECLARE_OOXMLIMPORT_TEST(testFdo74401, "fdo74401.docx")
+{
+    uno::Reference<drawing::XShapes> xGroupShape(getShape(1), uno::UNO_QUERY);
+    uno::Reference<drawing::XShapeDescriptor> xShape(xGroupShape->getByIndex(1), uno::UNO_QUERY);
+    // The triangle (second child) was a TextShape before, so it was shown as a rectangle.
+    CPPUNIT_ASSERT_EQUAL(OUString("com.sun.star.drawing.CustomShape"), xShape->getShapeType());
+}
+
 #endif
 
 CPPUNIT_PLUGIN_IMPLEMENT();