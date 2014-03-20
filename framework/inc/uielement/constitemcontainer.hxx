be1df5d2f1d3b46303acc5d5c447dcfbaa9e466c
Stephan Bergmann <sbergman@redhat.com>
2014-03-20 11:55:03 +0100

    Remove unused includes
    Change-Id: Ia3b3ce4e9e3504e3b9e4a623879893755dcd1ba9

--- framework/inc/uielement/constitemcontainer.hxx
+++ framework/inc/uielement/constitemcontainer.hxx
@@ -20,7 +20,6 @@
 #ifndef INCLUDED_FRAMEWORK_INC_UIELEMENT_CONSTITEMCONTAINER_HXX
 #define INCLUDED_FRAMEWORK_INC_UIELEMENT_CONSTITEMCONTAINER_HXX
 
-#include <threadhelp/threadhelpbase.hxx>
 #include <macros/generic.hxx>
 #include <macros/xinterface.hxx>
 #include <macros/xtypeprovider.hxx>
--- framework/inc/uielement/itemcontainer.hxx
+++ framework/inc/uielement/itemcontainer.hxx
@@ -20,7 +20,6 @@
 #ifndef INCLUDED_FRAMEWORK_INC_UIELEMENT_ITEMCONTAINER_HXX
 #define INCLUDED_FRAMEWORK_INC_UIELEMENT_ITEMCONTAINER_HXX
 
-#include <threadhelp/threadhelpbase.hxx>
 #include <macros/generic.hxx>
 #include <macros/xinterface.hxx>
 #include <macros/xtypeprovider.hxx>
