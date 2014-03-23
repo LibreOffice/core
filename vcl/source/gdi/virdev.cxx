ba7dabb0e7bdfda38ea5793cefa32ce62c86f858
Chris Sherlock <chris.sherlock79@gmail.com>
2014-03-23 18:51:13 +1100

    fdo#74702 Moved EnableRTL() logic to specific classes where appropriate
    OutputDevice::EnableRTL() is a bit of a mess. It uses a runtime
    variable to see if it is using a VirtualDevice, and it uses a
    dynamic_cast to see if the object is a Window or a Control!
    
    I have made it virtual and moved the knowledge of class specific
    functionality from OutputDevice to VirtualDevice, Window and Control
    as needed. OutputDevice::EnableRTL() functionality is then called.
    
    Also: small formatting change to outdev.hxx, also included a note
    that WindowImpl is a pimpl in window.hxx.
    
    Change-Id: I44b66601c4457fb2e0bbc1014fb7acf8f6942f80

--- vcl/source/gdi/virdev.cxx
+++ vcl/source/gdi/virdev.cxx
@@ -200,8 +200,6 @@ void VirtualDevice::ImplInitVirDev( const OutputDevice* pOutDev,
     pSVData->maGDIData.mpFirstVirDev = this;
 }
 
-
-
 VirtualDevice::VirtualDevice( sal_uInt16 nBitCount )
 :   mpVirDev( NULL ),
     meRefDevMode( REFDEV_NONE )
@@ -213,8 +211,6 @@ VirtualDevice::VirtualDevice( sal_uInt16 nBitCount )
     ImplInitVirDev( Application::GetDefaultDevice(), 1, 1, nBitCount );
 }
 
-
-
 VirtualDevice::VirtualDevice( const OutputDevice& rCompDev, sal_uInt16 nBitCount )
     : mpVirDev( NULL ),
     meRefDevMode( REFDEV_NONE )
@@ -225,8 +221,6 @@ VirtualDevice::VirtualDevice( const OutputDevice& rCompDev, sal_uInt16 nBitCount
 
     ImplInitVirDev( &rCompDev, 1, 1, nBitCount );
 }
-
-
 
 VirtualDevice::VirtualDevice( const OutputDevice& rCompDev, sal_uInt16 nBitCount, sal_uInt16 nAlphaBitCount )
     : mpVirDev( NULL ),
@@ -243,8 +237,6 @@ VirtualDevice::VirtualDevice( const OutputDevice& rCompDev, sal_uInt16 nBitCount
     mnAlphaDepth = sal::static_int_cast<sal_Int8>(nAlphaBitCount);
 }
 
-
-
 VirtualDevice::VirtualDevice( const SystemGraphicsData *pData, sal_uInt16 nBitCount )
 :   mpVirDev( NULL ),
     meRefDevMode( REFDEV_NONE )
@@ -253,8 +245,6 @@ VirtualDevice::VirtualDevice( const SystemGraphicsData *pData, sal_uInt16 nBitCo
 
     ImplInitVirDev( Application::GetDefaultDevice(), 1, 1, nBitCount, pData );
 }
-
-
 
 VirtualDevice::~VirtualDevice()
 {
@@ -278,8 +268,6 @@ VirtualDevice::~VirtualDevice()
     else
         pSVData->maGDIData.mpLastVirDev = mpPrev;
 }
-
-
 
 bool VirtualDevice::InnerImplSetOutputSizePixel( const Size& rNewSize, bool bErase, const basebmp::RawMemorySharedArray &pBuffer )
 {
@@ -381,8 +369,6 @@ bool VirtualDevice::InnerImplSetOutputSizePixel( const Size& rNewSize, bool bEra
     return bRet;
 }
 
-
-
 // #i32109#: Fill opaque areas correctly (without relying on
 // fill/linecolor state)
 void VirtualDevice::ImplFillOpaqueRectangle( const Rectangle& rRect )
@@ -396,8 +382,6 @@ void VirtualDevice::ImplFillOpaqueRectangle( const Rectangle& rRect )
     DrawRect( rRect );
     Pop();
 }
-
-
 
 bool VirtualDevice::ImplSetOutputSizePixel( const Size& rNewSize, bool bErase, const basebmp::RawMemorySharedArray &pBuffer )
 {
@@ -434,6 +418,18 @@ bool VirtualDevice::ImplSetOutputSizePixel( const Size& rNewSize, bool bErase, c
     return false;
 }
 
+void VirtualDevice::EnableRTL( bool bEnable )
+{
+    // virdevs default to not mirroring, they will only be set to mirroring
+    // under rare circumstances in the UI, eg the valueset control
+    // because each virdev has its own SalGraphics we can safely switch the SalGraphics here
+    // ...hopefully
+    if( ImplGetGraphics() )
+        mpGraphics->SetLayout( bEnable ? SAL_LAYOUT_BIDI_RTL : 0 );
+
+    OutputDevice::EnableRTL(bEnable);
+}
+
 bool VirtualDevice::SetOutputSizePixel( const Size& rNewSize, bool bErase )
 {
     return ImplSetOutputSizePixel( rNewSize, bErase, basebmp::RawMemorySharedArray() );
@@ -450,8 +446,6 @@ bool VirtualDevice::SetOutputSizePixelScaleOffsetAndBuffer( const Size& rNewSize
     }
     return ImplSetOutputSizePixel( rNewSize, true, pBuffer);
 }
-
-
 
 void VirtualDevice::SetReferenceDevice( RefDevMode i_eRefDevMode )
 {
@@ -533,13 +527,9 @@ void VirtualDevice::ImplSetReferenceDevice( RefDevMode i_eRefDevMode, sal_Int32 
     mpFontCache = new ImplFontCache();
 }
 
-
-
 void VirtualDevice::Compat_ZeroExtleadBug()
 {
     meRefDevMode = (sal_uInt8)meRefDevMode | REFDEV_FORCE_ZERO_EXTLEAD;
 }
-
-
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
