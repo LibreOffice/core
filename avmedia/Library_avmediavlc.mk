c4cf1a1891122eb1136484dd16145620b7678e96
Zolnai Tamás <tamas.zolnai@collabora.com>
2014-03-25 12:28:58 +0100

    avmediavlc: Remove duplicated external usage
    Change-Id: I2f41c13465740e5079e6af413f4c8217d6dcd36b

--- avmedia/Library_avmediavlc.mk
+++ avmedia/Library_avmediavlc.mk
@@ -16,8 +16,6 @@ $(eval $(call gb_Library_set_include,avmediavlc,\
     -I$(SRCDIR)/avmedia/source/inc \
 ))
 
-$(eval $(call gb_Library_use_external,avmediavlc,boost_headers))
-
 $(eval $(call gb_Library_use_sdk_api,avmediavlc))
 
 $(eval $(call gb_Library_use_externals,avmediavlc,\
