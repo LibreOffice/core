88b1d35131b0ea44ac6ba6f7edeab82d1c29a39f
Armin Le Grand <alg@apache.org>
2014-03-26 15:38:38 +0000

    i120201 added forcing TableLayouter to take action in table undo/redo (CellUndo)

--- svx/source/table/tableundo.cxx
+++ svx/source/table/tableundo.cxx
@@ -136,8 +136,13 @@ void CellUndo::setDataToCell( const Data& rData )
     mxCell->mnRowSpan = rData.mnRowSpan;
     mxCell->mnColSpan = rData.mnColSpan;
 
-    if( mxObjRef.is() )
+    if(mxObjRef.is())
+    {
+        // #120201# ActionChanged is not enough, we need to trigger TableLayouter::UpdateBorderLayout()
+        // and this is done best using ReformatText() for table objects
         mxObjRef->ActionChanged();
+        mxObjRef->NbcReformatText();
+    }
 }
 
 void CellUndo::getDataFromCell( Data& rData )
