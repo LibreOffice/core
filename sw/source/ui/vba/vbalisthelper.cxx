460262591311487cf45f9d92b1f7106b90c66e96
Caolán McNamara <caolanm@redhat.com>
2014-03-27 11:02:56 +0000

    coverity#735550 Dead default in switch
    Change-Id: I730b12aa971757e70d7df7b9cd4ec9c67fb4468e

--- sw/source/ui/vba/vbalisthelper.cxx
+++ sw/source/ui/vba/vbalisthelper.cxx
@@ -371,10 +371,6 @@ void SwVbaListHelper::CreateOutlineNumberForType1() throw( css::uno::RuntimeExce
                 sSuffix = OUString( '.' );
                 break;
             }
-            default:
-            {
-                throw uno::RuntimeException();
-            }
         }
         setOrAppendPropertyValue( aPropertyValues, OUString(UNO_NAME_NUMBERING_TYPE ), uno::makeAny( nNumberingType ) );
         setOrAppendPropertyValue( aPropertyValues, OUString(UNO_NAME_PREFIX ), uno::makeAny( sPrefix ) );
