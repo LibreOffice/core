ba59a951e595bd44754c4e60603b27334bbf9897
Stephan Bergmann <sbergman@redhat.com>
2014-03-27 20:54:26 +0100

    SAL_OVERRIDE, Mac OS X fpicker redux
    Change-Id: I02c234f36ed642b63a4a15ac4f865d8926fe081f

--- fpicker/source/aqua/SalAquaFilePicker.hxx
+++ fpicker/source/aqua/SalAquaFilePicker.hxx
@@ -65,92 +65,92 @@ public:
 
 
     virtual void SAL_CALL addFilePickerListener( const ::com::sun::star::uno::Reference< ::com::sun::star::ui::dialogs::XFilePickerListener >& xListener )
-        throw( ::com::sun::star::uno::RuntimeException );
+        throw( ::com::sun::star::uno::RuntimeException ) SAL_OVERRIDE;
     virtual void SAL_CALL removeFilePickerListener( const ::com::sun::star::uno::Reference< ::com::sun::star::ui::dialogs::XFilePickerListener >& xListener )
-        throw( ::com::sun::star::uno::RuntimeException );
+        throw( ::com::sun::star::uno::RuntimeException ) SAL_OVERRIDE;
 
 
     // XExecutableDialog functions
 
 
     virtual void SAL_CALL setTitle( const OUString& aTitle )
-        throw( ::com::sun::star::uno::RuntimeException );
+        throw( ::com::sun::star::uno::RuntimeException ) SAL_OVERRIDE;
 
     virtual sal_Int16 SAL_CALL execute(  )
-        throw( ::com::sun::star::uno::RuntimeException );
+        throw( ::com::sun::star::uno::RuntimeException ) SAL_OVERRIDE;
 
 
     // XFilePicker functions
 
 
     virtual void SAL_CALL setMultiSelectionMode( sal_Bool bMode )
-        throw( ::com::sun::star::uno::RuntimeException );
+        throw( ::com::sun::star::uno::RuntimeException ) SAL_OVERRIDE;
 
     virtual void SAL_CALL setDefaultName( const OUString& aName )
-        throw( ::com::sun::star::uno::RuntimeException );
+        throw( ::com::sun::star::uno::RuntimeException ) SAL_OVERRIDE;
 
     virtual void SAL_CALL setDisplayDirectory( const OUString& aDirectory )
         throw( com::sun::star::lang::IllegalArgumentException,
-               ::com::sun::star::uno::RuntimeException );
+               ::com::sun::star::uno::RuntimeException ) SAL_OVERRIDE;
 
     virtual OUString SAL_CALL getDisplayDirectory(  )
-        throw( ::com::sun::star::uno::RuntimeException );
+        throw( ::com::sun::star::uno::RuntimeException ) SAL_OVERRIDE;
 
     virtual ::com::sun::star::uno::Sequence< OUString > SAL_CALL getFiles(  )
-        throw( ::com::sun::star::uno::RuntimeException );
+        throw( ::com::sun::star::uno::RuntimeException ) SAL_OVERRIDE;
 
 
     // XFilterManager functions
 
 
     virtual void SAL_CALL appendFilter( const OUString& aTitle, const OUString& aFilter )
-        throw( ::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::uno::RuntimeException );
+        throw( ::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::uno::RuntimeException ) SAL_OVERRIDE;
 
     virtual void SAL_CALL setCurrentFilter( const OUString& aTitle )
-        throw( ::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::uno::RuntimeException );
+        throw( ::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::uno::RuntimeException ) SAL_OVERRIDE;
 
     virtual OUString SAL_CALL getCurrentFilter(  )
-        throw( ::com::sun::star::uno::RuntimeException );
+        throw( ::com::sun::star::uno::RuntimeException ) SAL_OVERRIDE;
 
 
     // XFilterGroupManager functions
 
 
     virtual void SAL_CALL appendFilterGroup( const OUString& sGroupTitle, const ::com::sun::star::uno::Sequence< ::com::sun::star::beans::StringPair >& aFilters )
-        throw (::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::uno::RuntimeException);
+        throw (::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::uno::RuntimeException) SAL_OVERRIDE;
 
 
     // XFilePickerControlAccess functions
 
 
     virtual void SAL_CALL setValue( sal_Int16 nControlId, sal_Int16 nControlAction, const ::com::sun::star::uno::Any& aValue )
-        throw (::com::sun::star::uno::RuntimeException);
+        throw (::com::sun::star::uno::RuntimeException) SAL_OVERRIDE;
 
     virtual ::com::sun::star::uno::Any SAL_CALL getValue( sal_Int16 aControlId, sal_Int16 aControlAction )
-        throw (::com::sun::star::uno::RuntimeException);
+        throw (::com::sun::star::uno::RuntimeException) SAL_OVERRIDE;
 
     virtual void SAL_CALL enableControl( sal_Int16 nControlId, sal_Bool bEnable )
-        throw(::com::sun::star::uno::RuntimeException );
+        throw(::com::sun::star::uno::RuntimeException ) SAL_OVERRIDE;
 
     virtual void SAL_CALL setLabel( sal_Int16 nControlId, const OUString& aLabel )
-        throw (::com::sun::star::uno::RuntimeException);
+        throw (::com::sun::star::uno::RuntimeException) SAL_OVERRIDE;
 
     virtual OUString SAL_CALL getLabel( sal_Int16 nControlId )
-        throw (::com::sun::star::uno::RuntimeException);
+        throw (::com::sun::star::uno::RuntimeException) SAL_OVERRIDE;
 
 
     // XInitialization
 
 
     virtual void SAL_CALL initialize( const ::com::sun::star::uno::Sequence< ::com::sun::star::uno::Any >& aArguments )
-        throw(::com::sun::star::uno::Exception, ::com::sun::star::uno::RuntimeException);
+        throw(::com::sun::star::uno::Exception, ::com::sun::star::uno::RuntimeException) SAL_OVERRIDE;
 
 
     // XCancellable
 
 
     virtual void SAL_CALL cancel( )
-        throw( ::com::sun::star::uno::RuntimeException );
+        throw( ::com::sun::star::uno::RuntimeException ) SAL_OVERRIDE;
 
 
     // XEventListener
@@ -165,13 +165,13 @@ public:
 
 
     virtual OUString SAL_CALL getImplementationName(  )
-        throw(::com::sun::star::uno::RuntimeException);
+        throw(::com::sun::star::uno::RuntimeException) SAL_OVERRIDE;
 
     virtual sal_Bool SAL_CALL supportsService( const OUString& ServiceName )
-        throw(::com::sun::star::uno::RuntimeException);
+        throw(::com::sun::star::uno::RuntimeException) SAL_OVERRIDE;
 
     virtual ::com::sun::star::uno::Sequence< OUString > SAL_CALL getSupportedServiceNames(  )
-        throw(::com::sun::star::uno::RuntimeException);
+        throw(::com::sun::star::uno::RuntimeException) SAL_OVERRIDE;
 
 
     // FilePicker Event functions
