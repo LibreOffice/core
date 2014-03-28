bfbe520e5f02558be0aadf534d300decf3169de0
Muthu Subramanian <sumuthu@collabora.com>
2014-03-28 17:24:45 +0530

    n#870234: Import gradfill for text colors.
    Uses the first color from the gradfill list.
    (Which is better than plain black!)
    
    (cherry picked from commit cfc76de83e3c0a56abd30a8f3bd7c69d3500d223)
    Signed-off-by: Andras Timar <andras.timar@collabora.com>
    
    Conflicts:
    	oox/source/drawingml/textcharacterproperties.cxx
    	oox/source/drawingml/textcharacterpropertiescontext.cxx
    
    Change-Id: I4c1c0c4b031f3681c95b75b3c0683eb4de95bffb

--- oox/source/drawingml/textcharacterproperties.cxx
+++ oox/source/drawingml/textcharacterproperties.cxx
@@ -60,6 +60,7 @@ void TextCharacterProperties::assignUsed( const TextCharacterProperties& rSource
     moItalic.assignIfUsed( rSourceProps.moItalic );
     moUnderlineLineFollowText.assignIfUsed( rSourceProps.moUnderlineLineFollowText );
     moUnderlineFillFollowText.assignIfUsed( rSourceProps.moUnderlineFillFollowText );
+    maGradientProps.assignUsed( rSourceProps.maGradientProps );
 }
 
 void TextCharacterProperties::pushToPropMap( PropertyMap& rPropMap, const XmlFilterBase& rFilter, bool bUseOptional ) const
@@ -93,6 +94,8 @@ void TextCharacterProperties::pushToPropMap( PropertyMap& rPropMap, const XmlFil
 
     if( maCharColor.isUsed() )
         rPropMap[ PROP_CharColor ] <<= maCharColor.getColor( rFilter.getGraphicHelper() );
+    if( maGradientProps.maGradientStops.size() > 0 )
+        rPropMap[ PROP_CharColor ] <<= maGradientProps.maGradientStops.begin()->second.getColor( rFilter.getGraphicHelper() );
 
     if( moLang.has() && !moLang.get().isEmpty() )
     {
