b9446b7d8dc75cf0df57b172b2561f5bcbb5d1c9
Douglas Mencken <dougmencken@gmail.com>
2014-03-29 07:53:40 -0400

    sal: use posix_memalign only if it's available
    for example, Darwin/OSX <10.6 doesn't have posix_memalign:
        checking for posix_memalign in -lc... no
    
    Change-Id: I4a02b40c36d353c2b7a78d0bacb3b14e1f2d94da

--- sal/osl/unx/memory.c
+++ sal/osl/unx/memory.c
@@ -16,25 +16,36 @@
 
 void* osl_aligned_alloc( sal_Size align, sal_Size size )
 {
-#ifdef __ANDROID__
-    return memalign(align, size);
-#else
     if (size == 0)
     {
         return NULL;
     }
     else
     {
+#ifdef HAVE_POSIX_MEMALIGN
         void* ptr;
         int err = posix_memalign(&ptr, align, size);
         return err ? NULL : ptr;
-    }
+#else
+        void* ptr = malloc(size + (align - 1) + sizeof(void*));
+        if (!ptr) return NULL;
+
+        char* aptr = ((char*)ptr) + sizeof(void*);
+        aptr += (align - ((size_t)aptr & (align - 1)) & (align - 1));
+
+        ((void**)aptr)[-1] = ptr;
+        return aptr;
 #endif
+    }
 }
 
 void osl_aligned_free( void* p )
 {
+#ifdef HAVE_POSIX_MEMALIGN
     free(p);
+#else
+    free(((void**)p)[-1]);
+#endif
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
