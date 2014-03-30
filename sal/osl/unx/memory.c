321a047bea18d7d15466d6db5f5f36520c2ca0db
Douglas Mencken <dougmencken@gmail.com>
2014-03-30 02:32:16 -0400

    sal: add universal aligned memory allocation with malloc
    Note that posix_memalign is not available everywhere
    (as are its replacements like memalign). For example,
    Darwin/OSX <10.6 has neither posix_memalign or memalign.
    
    Change-Id: I4a02b40c36d353c2b7a78d0bacb3b14e1f2d94da

--- sal/osl/unx/memory.c
+++ sal/osl/unx/memory.c
@@ -10,31 +10,55 @@
 #include <internal/oslmemory.h>
 
 #include <stdlib.h>
+
+#define USE_POSIX_MEMALIGN
+
 #ifdef __ANDROID__
 #include <malloc.h>
+#undef USE_POSIX_MEMALIGN
+#define USE_MEMALIGN
+#endif
+
+#ifdef __APPLE__
+#if MAC_OS_X_VERSION_MAX_ALLOWED < 1060
+#undef USE_POSIX_MEMALIGN
+#endif
 #endif
 
 void* osl_aligned_alloc( sal_Size align, sal_Size size )
 {
-#ifdef __ANDROID__
-    return memalign(align, size);
-#else
     if (size == 0)
     {
         return NULL;
     }
     else
     {
+#if defined(USE_POSIX_MEMALIGN)
         void* ptr;
         int err = posix_memalign(&ptr, align, size);
         return err ? NULL : ptr;
-    }
+#elif defined(USE_MEMALIGN)
+        return memalign(align, size);
+#else
+        void* ptr = malloc(size + (align - 1) + sizeof(void*));
+        if (!ptr) return NULL;
+
+        char* aptr = ((char*)ptr) + sizeof(void*);
+        aptr += (align - ((size_t)aptr & (align - 1))) & (align - 1);
+
+        ((void**)aptr)[-1] = ptr;
+        return aptr;
 #endif
+    }
 }
 
 void osl_aligned_free( void* p )
 {
+#if defined(USE_POSIX_MEMALIGN) || defined(USE_MEMALIGN)
     free(p);
+#else
+    free(((void**)p)[-1]);
+#endif
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
