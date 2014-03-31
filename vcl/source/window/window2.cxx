902ed18886e321e80a95250fee423f6e871ceb28
Caolán McNamara <caolanm@redhat.com>
2014-03-31 09:20:02 +0100

    use toBool
    Change-Id: I334570bac6f0ab8fbd606610f0dcd812041c72b1

--- vcl/source/window/window2.cxx
+++ vcl/source/window/window2.cxx
@@ -1865,7 +1865,7 @@ bool Window::set_font_attribute(const OString &rKey, const OString &rValue)
             aFont.SetItalic(ITALIC_NORMAL);
         SetControlFont(aFont);
     }
-    else if (rKey == "underline" && rValue == "True")
+    else if (rKey == "underline" && toBool(rValue) == true)
     {
         Font aFont(GetControlFont());
         aFont.SetUnderline(UNDERLINE_SINGLE);
