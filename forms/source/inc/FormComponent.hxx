6478730e126f0bf90790eb89c99a5eebced1409d
Stephan Bergmann <sbergman@redhat.com>
2014-04-01 19:42:02 +0200

    Forgotten TODO
    Change-Id: I4fb1c155c533445856639498e73c728933bb3dd7

--- forms/source/inc/FormComponent.hxx
+++ forms/source/inc/FormComponent.hxx
@@ -529,7 +529,7 @@ public:
 
 // simple destructor
 #define DECLARE_DEFAULT_DTOR( classname )   \
-    ~classname()override/*TODO*/            \
+    virtual ~classname()
 
 // constructor for cloning a class
 #define DECLARE_DEFAULT_CLONE_CTOR( classname )  \
