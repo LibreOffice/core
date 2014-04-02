414ff5650a810be807fed61ded1b9d6a23aa720b
Zolnai Tamás <tamas.zolnai@collabora.com>
2014-04-02 11:41:59 +0200

    Avoid Wsahdow warnings related to boost json parser
    Problematic variable: separator -> separator_
    
    Change-Id: Ia4e1b1543f61b03e029e1bc4151ec58334e8df57

--- external/boost/UnpackedTarball_boost.mk
+++ external/boost/UnpackedTarball_boost.mk
@@ -49,6 +49,7 @@ boost_patches += boost.wuninitialized.patch
 boost_patches += boost.wdeprecated-auto_ptr.patch.0
 #https://svn.boost.org/trac/boost/ticket/9397
 boost_patches += boost.9397.warnings.patch.0
+boost_patches += boost.jsonparser.warnings.patch.0
 
 $(eval $(call gb_UnpackedTarball_UnpackedTarball,boost))
 
