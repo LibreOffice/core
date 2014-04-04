d4e4bcb2df3f12f7ee19c084a5deab6691f290f2
Caolán McNamara <caolanm@redhat.com>
2014-04-04 20:56:23 +0100

    coverity#706511 Uncaught exception
    Change-Id: I2822c35e60a93b86bdcb56f1e4782aa46e1ce8e0

--- embeddedobj/source/msole/olemisc.cxx
+++ embeddedobj/source/msole/olemisc.cxx
@@ -383,7 +383,7 @@ uno::Reference< util::XCloseable > SAL_CALL OleEmbeddedObject::getComponent()
     if ( m_nObjectState == -1 ) // || m_nObjectState == embed::EmbedStates::LOADED )
     {
         // the object is still not running
-        throw embed::WrongStateException( OUString( "The object is not loaded!\n" ),
+        throw uno::RuntimeException( OUString( "The object is not loaded!\n" ),
                                         uno::Reference< uno::XInterface >( static_cast< ::cppu::OWeakObject* >(this) ) );
     }
 
