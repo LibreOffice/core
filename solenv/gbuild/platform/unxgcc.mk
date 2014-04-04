07d66957e0d919f7a6e9c1f95ef78a6305146241
Matúš Kukan <matus.kukan@collabora.com>
2014-04-04 11:31:35 +0200

    gbuild: fix linking problems on ELF platforms
    This was done in master with f861ae6acdbde8acc9560366d344852391d17b8d
    
    Change-Id: Ia3c282ce49b2a2e5209304ce60c806f738e25bba

--- solenv/gbuild/platform/unxgcc.mk
+++ solenv/gbuild/platform/unxgcc.mk
@@ -192,9 +192,11 @@ $(call gb_Helper_abbreviate_dirs,\
 		$(foreach object,$(GENCOBJECTS),$(call gb_GenCObject_get_target,$(object))) \
 		$(foreach object,$(GENCXXOBJECTS),$(call gb_GenCxxObject_get_target,$(object))) \
 		$(foreach extraobjectlist,$(EXTRAOBJECTLISTS),`cat $(extraobjectlist)`) \
-		-Wl$(COMMA)--start-group $(foreach lib,$(LINKED_STATIC_LIBS),$(call gb_StaticLibrary_get_target,$(lib))) -Wl$(COMMA)--end-group \
-		-Wl$(COMMA)--no-as-needed \
+		-Wl$(COMMA)--start-group \
+		$(foreach lib,$(LINKED_STATIC_LIBS),$(call gb_StaticLibrary_get_target,$(lib))) \
 		$(LIBS) \
+		-Wl$(COMMA)--end-group \
+		-Wl$(COMMA)--no-as-needed \
 		$(patsubst lib%.a,-l%,$(patsubst lib%.so,-l%,$(foreach lib,$(LINKED_LIBS),$(call gb_Library_get_filename,$(lib))))) \
 		-o $(if $(SOVERSION),$(1).$(SOVERSION),$(1)))
 	$(if $(SOVERSION),ln -sf $(notdir $(1)).$(SOVERSION) $(1))
