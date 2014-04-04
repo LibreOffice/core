3f5069524e7bb70968352316538f19ea9159b531
Fridrich Štrba <fridrich.strba@bluewin.ch>
2014-04-04 15:45:36 +0200

    Uploading libcdr-0.0.15: Support for CorelDraw x7
    (cherry picked from commit 32f301f5a64d8ff7815cf88858fc8a87cb9ad76a)
    
    Conflicts:
    	download.lst
    
    Change-Id: I1cc6c6ab39ec27dbf08c2019c7dd7f8f29537520

--- external/libcdr/UnpackedTarball_libcdr.mk
+++ external/libcdr/UnpackedTarball_libcdr.mk
@@ -11,8 +11,4 @@ $(eval $(call gb_UnpackedTarball_UnpackedTarball,libcdr))
 
 $(eval $(call gb_UnpackedTarball_set_tarball,libcdr,$(CDR_TARBALL)))
 
-$(eval $(call gb_UnpackedTarball_add_patches,libcdr,\
-	external/libcdr/libcdr-wpx_seek_end.patch.1 \
-))
-
 # vim: set noet sw=4 ts=4:
