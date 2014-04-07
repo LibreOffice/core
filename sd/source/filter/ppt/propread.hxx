dcfa6ba11f23c45fcbe82d15aff3741bbb930569
Caolán McNamara <caolanm@redhat.com>
2014-04-07 13:18:41 +0100

    coverity#708128 Uninitialized scalar field
    Change-Id: Ic22b6640ba6b87769bdf00bc630d463de1ba04ad

--- sd/source/filter/ppt/propread.hxx
+++ sd/source/filter/ppt/propread.hxx
@@ -113,15 +113,18 @@ struct PropEntry
 
 class PropItem : public SvMemoryStream
 {
-        sal_uInt16      mnTextEnc;
+    sal_uInt16      mnTextEnc;
 
-    public :
-                        PropItem(){};
-        void            Clear();
+public:
+    PropItem()
+        : mnTextEnc(RTL_TEXTENCODING_DONTKNOW)
+    {
+    }
+    void            Clear();
 
-        void            SetTextEncoding( sal_uInt16 nTextEnc ){ mnTextEnc = nTextEnc; };
-        sal_Bool        Read( OUString& rString, sal_uInt32 nType = VT_EMPTY, sal_Bool bDwordAlign = sal_True );
-        PropItem&       operator=( PropItem& rPropItem );
+    void            SetTextEncoding( sal_uInt16 nTextEnc ){ mnTextEnc = nTextEnc; };
+    sal_Bool        Read( OUString& rString, sal_uInt32 nType = VT_EMPTY, sal_Bool bDwordAlign = sal_True );
+    PropItem&       operator=( PropItem& rPropItem );
 
     using SvStream::Read;
 };
