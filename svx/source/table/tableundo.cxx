409ebeb20d1ee4552ac21bd739ae4e25eaeb68d9
Caolán McNamara <caolanm@redhat.com>
2014-04-07 21:22:18 +0100

    coverity#708388 Uninitialized scalar field
    Change-Id: Idaa2ec4863beb6be790022c3c66bc4a72e9bf626

--- svx/source/table/tableundo.cxx
+++ svx/source/table/tableundo.cxx
@@ -511,6 +511,7 @@ void TableRowUndo::getData( Data& rData )
 TableStyleUndo::TableStyleUndo( const SdrTableObj& rTableObj )
     : SdrUndoAction( *rTableObj.GetModel() )
     , mxObjRef( const_cast< sdr::table::SdrTableObj*>( &rTableObj ) )
+    , mbHasRedoData(false)
 {
     getData( maUndoData );
 }
