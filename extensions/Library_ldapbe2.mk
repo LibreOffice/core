d2555ebb240fea2780f152e5ea39d145aab508fe
Stephan Bergmann <sbergman@redhat.com>
2014-04-14 09:57:50 +0200

    better?
    Change-Id: Idacacf531d237c2c7482db85c56b58954fd04ac5

--- extensions/Library_ldapbe2.mk
+++ extensions/Library_ldapbe2.mk
@@ -35,8 +35,10 @@ $(eval $(call gb_Library_use_libraries,ldapbe2,\
 ))
 
 ifeq ($(OS),WNT)
-$(eval $(call gb_Library_use_system_win32_libs,ldapbe2,\
+$(eval $(call gb_Library_use_externals,ldapbe2,\
 	boost_headers \
+))
+$(eval $(call gb_Library_use_system_win32_libs,ldapbe2,\
 	wldap32 \
 ))
 else # 0S!=WNT
