bd92bfdf8eca1217877192cb80a7eccca2659a6c
Tor Lillqvist <tml@collabora.com>
2014-04-14 19:40:36 +0300

    Need to skip these now on iOS
    Change-Id: I4bf25a30152d3cbbcc2fae05e40accdde1548298

--- i18npool/Module_i18npool.mk
+++ i18npool/Module_i18npool.mk
@@ -16,8 +16,9 @@ $(eval $(call gb_Module_add_targets,i18npool,\
 	CustomTarget_localedata \
 	CustomTarget_textconversion \
 	Library_collator_data \
-	Library_dict_ja \
-	Library_dict_zh \
+	$(if $(filter-ouut IOS,$(OS)), \
+		Library_dict_ja \
+		Library_dict_zh) \
 	Library_i18npool \
 	Library_i18nsearch \
 	Library_index_data \
