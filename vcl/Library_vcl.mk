647c1529be2a1ebe9a35ffb9b076c6f36349abf1
Chris Sherlock <chris.sherlock79@gmail.com>
2014-04-14 18:07:00 +1000

    Move hatch functions to own source file.
    Move outdev4.cxx to hatch.cxx as this now only contains hatch
    functions.
    
    Change-Id: Ib2dd18eab63f98dde79aba315fe140ce68d3c9dc

--- vcl/Library_vcl.mk
+++ vcl/Library_vcl.mk
@@ -236,7 +236,7 @@ $(eval $(call gb_Library_add_exception_objects,vcl,\
     vcl/source/gdi/oldprintadaptor \
     vcl/source/gdi/outdev/outdev2 \
     vcl/source/gdi/outdev/outdev3 \
-    vcl/source/gdi/outdev/outdev4 \
+    vcl/source/gdi/outdev/hatch \
     vcl/source/gdi/outdev/gradient \
     vcl/source/gdi/outdev/outdev5 \
     vcl/source/gdi/outdev/outdev6 \
