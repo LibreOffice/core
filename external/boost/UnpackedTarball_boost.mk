efadf1f90b1038661f54c6c18d5a8d17415d47a7
Michael Stahl <mstahl@redhat.com>
2014-04-14 12:08:32 +0200

    boost: annotate upstream status of patches
    Change-Id: Ie841bbc23ddd99fc023c3066b107172f9cfb7a24

--- external/boost/UnpackedTarball_boost.mk
+++ external/boost/UnpackedTarball_boost.mk
@@ -21,36 +21,73 @@ boost_patches += boost.7551.unusedvars.patch
 #https://svn.boost.org/trac/boost/ticket/6142
 boost_patches += boost.6142.warnings.patch.1
 
+# fixed upstream
 boost_patches += boost.bind.Wunused-local-typedefs.warnings.patch
+# fixed upstream
 boost_patches += boost.concept_check.C4100.warnings.patch
+# fixed upstream
 boost_patches += boost.concept_check.Wunused-local-typedefs.warnings.patch
+# https://svn.boost.org/trac/boost/ticket/9881
 boost_patches += boost.crc.Wshadow.warnings.patch
+# https://svn.boost.org/trac/boost/ticket/9882
 boost_patches += boost.date_time.Wshadow.warnings.patch
+# https://svn.boost.org/trac/boost/ticket/9883
 boost_patches += boost.date_time.Wunused-local-typedefs.warnings.patch
+# fixed upstream
 boost_patches += boost.math.Wdeprecated-register.patch.0
+# https://svn.boost.org/trac/boost/ticket/9884
 boost_patches += boost.math.Wunused-local-typedefs.warnings.patch
+# https://svn.boost.org/trac/boost/ticket/9885
 boost_patches += boost.multi_array.C4510.warnings.patch
+# https://svn.boost.org/trac/boost/ticket/9886
 boost_patches += boost.multi_array.Wshadow.warnings.patch
+# https://svn.boost.org/trac/boost/ticket/9887
 boost_patches += boost.multi_array.Wundef.warnings.patch
+# https://svn.boost.org/trac/boost/ticket/9888
 boost_patches += boost.multi_array.Wunused-local-typedefs.warnings.patch
+# fixed upstream
 boost_patches += boost.multi_index.Wshadow.warnings.patch.1
+# https://svn.boost.org/trac/boost/ticket/9889
 boost_patches += boost.predef.Wundef.warnings.patch
+# https://svn.boost.org/trac/boost/ticket/9891
 boost_patches += boost.preprocessor.Wundef.warnings.patch
+# https://svn.boost.org/trac/boost/ticket/9892
 boost_patches += boost.property_tree.Wshadow.warnings.patch.1
+# https://svn.boost.org/trac/boost/ticket/9893
 boost_patches += boost.property_tree.Wtautological-constant-out-of-range-compare.warnings.patch.0
+# https://svn.boost.org/trac/boost/ticket/9894
 boost_patches += boost.ptr_container.Wshadow.warnings.patch
+# https://svn.boost.org/trac/boost/ticket/9895
 boost_patches += boost.ptr_container.Wignored-qualifiers.warnings.patch
+# https://svn.boost.org/trac/boost/ticket/9896
 boost_patches += boost.ptr_container.Wextra.warnings.patch
+# https://svn.boost.org/trac/boost/ticket/9897
 boost_patches += boost.ptr_container.Wunused-parameter.warnings.patch
+# https://svn.boost.org/trac/boost/ticket/9898
 boost_patches += boost.random.Wshadow.warnings.patch
+# https://svn.boost.org/trac/boost/ticket/9899
 boost_patches += boost.random.Wuninitialized.patch
+# fixed upstream
 boost_patches += boost.random.Wunused-local-typedefs.warnings.patch
+# https://svn.boost.org/trac/boost/ticket/9900
 boost_patches += boost.spirit.Wshadow.warnings.patch
+# https://svn.boost.org/trac/boost/ticket/9901
 boost_patches += boost.spirit.Wunused-local-typedefs.warnings.patch
+# fixed upstream
 boost_patches += boost.unordered.Wshadow.warnings.patch
+# https://svn.boost.org/trac/boost/ticket/9902
 boost_patches += boost.tuple.Wunused-local-typedefs.warnings.patch
+# https://svn.boost.org/trac/boost/ticket/9903
 boost_patches += boost.utility.Wundef.warnings.patch
+# fixed upstream
 boost_patches += boost.uuid.Wshadow.warnings.patch
+
+# https://svn.boost.org/trac/boost/ticket/9890
+boost_patches += boost.endian.patch
+# fixed upstream
+boost_patches += boost.loplugin.patch
+# fixed upstream
+boost_patches += boost.transform_width.patch
 
 # Help static analysis tools (see SAL_UNUSED_PARAMETER in sal/types.h):
 ifeq (GCC,$(COM))
@@ -61,10 +98,7 @@ endif
 boost_patches += boost_1_44_0-clang-warnings.patch
 
 boost_patches += boost.auto_link.patch
-boost_patches += boost.endian.patch
-boost_patches += boost.loplugin.patch
 boost_patches += boost.std.move.patch
-boost_patches += boost.transform_width.patch
 boost_patches += boost.wunused.patch
 boost_patches += boost.wdeprecated-auto_ptr.patch.0
 
