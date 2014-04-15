a2212cefc1a4a380f2cb72fe620f046b947e1dab
Tor Lillqvist <tml@collabora.com>
2014-04-16 00:54:45 +0300

    WaE: Unreferenced function definition [loplugin:unreffun]
    Change-Id: Ia6946179a5567fdf1a980113d2efe77d4e87af50

--- vcl/source/outdev/bezier.cxx
+++ vcl/source/outdev/bezier.cxx
@@ -17,6 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
+#include <vcl/outdev.hxx>
 #include <tools/poly.hxx>
 
 Polygon ImplSubdivideBezier( const Polygon& rPoly )
