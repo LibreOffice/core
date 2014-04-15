c10898d7b11862e9f0804e2dbd83b9014502c36c
Ravindra Vidhate <ravindra.vidhate@synerzip.com>
2014-04-15 15:24:40 +0530

    fdo#77476:FILE_SAVE : 'TIF' is changed to 'TIFF' in RT
    1. Open the attached file in LO which has images [.TIF] format
    2. Save the document as DOCX
    3. Open the saved document in MS-Office or extract the content of the saved docx file.
     In [Content_Types].xml , .tif extension is changed to .tiff.
    
    Change-Id: I73baea9ad6cd991b04d47e23865b77432127a510

--- sw/qa/extras/ooxmlexport/ooxmlexport.cxx
+++ sw/qa/extras/ooxmlexport/ooxmlexport.cxx
@@ -3119,6 +3119,17 @@ DECLARE_OOXMLEXPORT_TEST(testFDO76597, "fdo76597.docx")
     assertXPath(pXmlDoc, "/w:document/w:body/w:p[4]/w:pPr/w:spacing", "before", "96");
     assertXPath(pXmlDoc, "/w:document/w:body/w:p[4]/w:pPr/w:spacing", "after", "120");
 }
+
+DECLARE_OOXMLEXPORT_TEST(testContentTypeTIF, "fdo77476.docx")
+{
+    xmlDocPtr pXmlDoc = parseExport("[Content_Types].xml");
+
+    if (!pXmlDoc)
+       return;
+
+    assertXPath(pXmlDoc, "/ContentType:Types/ContentType:Override[@ContentType='image/tif']", "PartName", "/word/media/image9.tif");
+}
+
 #endif
 
 CPPUNIT_PLUGIN_IMPLEMENT();
