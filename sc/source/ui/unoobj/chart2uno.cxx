0ce6d9bcea7c37a7cb2c42e8393e2e20780bd171
Markus Mohrhard <markus.mohrhard@collabora.co.uk>
2014-04-17 02:02:15 +0200

    limit the range shrinking in charts to really large ranges, fdo#70609,
    Also improves the situation for fdo#55697.
    
    It is just the fist step to a better algorithm.
    
    Change-Id: I15de1ca6604e585fcf5690f0d903c757d641e765

--- sc/source/ui/unoobj/chart2uno.cxx
+++ sc/source/ui/unoobj/chart2uno.cxx
@@ -1368,6 +1368,8 @@ bool lcl_addUpperLeftCornerIfMissing(vector<ScTokenRef>& rRefTokens,
     return true;
 }
 
+#define SHRINK_RANGE_THRESHOLD 10000
+
 class ShrinkRefTokenToDataRange : std::unary_function<ScTokenRef, void>
 {
     ScDocument* mpDoc;
@@ -1386,6 +1388,9 @@ public:
         ScComplexRefData& rData = rRef->GetDoubleRef();
         ScSingleRefData& s = rData.Ref1;
         ScSingleRefData& e = rData.Ref2;
+
+        if(abs((e.Col()-s.Col())*(e.Row()-s.Row())) < SHRINK_RANGE_THRESHOLD)
+            return;
 
         SCCOL nMinCol = MAXCOL, nMaxCol = 0;
         SCROW nMinRow = MAXROW, nMaxRow = 0;
