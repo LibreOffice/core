9b9f1a5f0642a8ccffb67bd53dc18025f99950c7
Zolnai Tamás <tamas.zolnai@collabora.com>
2014-04-18 13:24:00 +0200

    Fix android build: fix condition
    Change-Id: I5e20375791f038b38ef079712204a1afed07c39c

--- external/freetype/Module_freetype.mk
+++ external/freetype/Module_freetype.mk
@@ -9,7 +9,7 @@
 
 $(eval $(call gb_Module_Module,freetype))
 
-ifneq (,$(or $(filter-out ANDROID,$(OS)),$(ENABLE_GLTF)))
+ifneq (,$(or $(findstring ANDROID,$(OS)),$(ENABLE_GLTF)))
 
 $(eval $(call gb_Module_add_targets,freetype,\
 	ExternalProject_freetype \
