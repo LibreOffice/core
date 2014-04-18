9cf12d7544d501e6794cfbb6eabf508603d59966
Zolnai Tamás <tamas.zolnai@collabora.com>
2014-04-18 15:12:09 +0200

    Patching libgltf: character buffer is used as cstring
    Change-Id: If0213fc5406ec9cbdc72be36ffc693fcbbf8ffcc

--- external/libgltf/UnpackedTarball_libgltf.mk
+++ external/libgltf/UnpackedTarball_libgltf.mk
@@ -24,6 +24,7 @@ $(eval $(call gb_UnpackedTarball_add_patches,libgltf,\
 	external/libgltf/patches/include_typo_texture.patch \
 	external/libgltf/patches/adress_of_temporary.patch \
 	external/libgltf/patches/avoid_c++11.patch \
+	external/libgltf/patches/charbuffer_used_as_cstring.patch \
 ))
 
 # vim: set noet sw=4 ts=4:
