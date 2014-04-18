f1ef894e14d62bba3c85a079d5fef60b2dae336c
Andras Timar <andras.timar@collabora.com>
2014-04-18 02:16:38 -0700

    [cp] Collabora Office EULA
    Change-Id: Ia380db465f5094216cb36526f6cc178043376356

--- readlicense_oo/Package_files.mk
+++ readlicense_oo/Package_files.mk
@@ -11,9 +11,13 @@ $(eval $(call gb_Package_Package,readlicense_oo_files,$(SRCDIR)/readlicense_oo/l
 
 ifneq ($(OS),MACOSX)
 $(eval $(call gb_Package_add_file,readlicense_oo_files,NOTICE,NOTICE))
+$(eval $(call gb_Package_add_file,readlicense_oo_files,EULA.odt,EULA.odt))
+$(eval $(call gb_Package_add_file,readlicense_oo_files,EULA_en-US.rtf,EULA_en-US.rtf))
 $(eval $(call gb_Package_add_file,readlicense_oo_files,CREDITS.fodt,CREDITS.fodt))
 else
 $(eval $(call gb_Package_add_file,readlicense_oo_files,Resources/NOTICE,NOTICE))
+$(eval $(call gb_Package_add_file,readlicense_oo_files,Resources/EULA.odt,EULA.odt))
+$(eval $(call gb_Package_add_file,readlicense_oo_files,Resources/EULA_en-US.rtf,EULA_en-US.rtf))
 $(eval $(call gb_Package_add_file,readlicense_oo_files,Resources/CREDITS.fodt,CREDITS.fodt))
 endif
 
