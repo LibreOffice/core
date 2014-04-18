1a1daff34e23e6b6c25975675a1f1a5a4db6ff08
Jan Holesovsky <kendy@collabora.com>
2014-04-18 15:32:53 +0200

    sfx2: Kill SFX_FEATURED_OBJECTBAR_REGISTRATION macro.
    Change-Id: I7a364b74a9223a6256f004ee2367aaaff80e7f2e

--- include/sfx2/shell.hxx
+++ include/sfx2/shell.hxx
@@ -352,9 +352,6 @@ inline void SfxShell::SetPool
 #define SFX_VISIBILITY_SERVER           0x8000
 #define SFX_VISIBILITY_NOCONTEXT        0xFFFF  // Always visable
 
-#define SFX_FEATURED_OBJECTBAR_REGISTRATION(nPos,rResId,nFeature) \
-        GetStaticInterface()->RegisterObjectBar( nPos, rResId, nFeature )
-
 #define SFX_CHILDWINDOW_REGISTRATION(nId) \
         GetStaticInterface()->RegisterChildWindow( nId, false )
 
