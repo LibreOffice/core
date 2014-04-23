b80dc0ed7be65c448c7a286454c4304120b4c20e
Muthu Subramanian <sumuthu@collabora.com>
2014-04-23 22:41:11 +0530

    fdo#64047: n#863021: Add set-all language menu.
    Problems:
    * Doesn't reset the spell error markers
    * Modifies only at object level
    * Currently has only setting for 'all text'
    * Maybe provide a current slide only option?
    
    Change-Id: Idcf8555bab9a5618362dfd2aa8773dd736cbdbb0

--- sd/source/ui/docshell/docshel3.cxx
+++ sd/source/ui/docshell/docshel3.cxx
@@ -18,6 +18,7 @@
  */
 
 
+#include "Window.hxx"
 #include "DrawDocShell.hxx"
 
 #include "app.hrc"
@@ -29,14 +30,21 @@
 #include <svx/svxerr.hxx>
 #include <svx/dialmgr.hxx>
 #include <svl/srchitem.hxx>
+#include <svl/languageoptions.hxx>
+#include <svtools/langtab.hxx>
 #include <svx/srchdlg.hxx>
 #ifdef _OUTLINER_HXX
 #include <editeng/outliner.hxx>
 #endif
 #include <sfx2/request.hxx>
+#include <sfx2/sfxdlg.hxx>
+#include <vcl/abstdlg.hxx>
+#include <vcl/window.hxx>
 #include <svl/style.hxx>
 #include <svx/drawitem.hxx>
 #include <editeng/unolingu.hxx>
+#include <editeng/langitem.hxx>
+#include <editeng/eeitem.hxx>
 #include <com/sun/star/i18n/TextConversionOption.hpp>
 
 
@@ -59,6 +67,68 @@ using namespace ::com::sun::star::beans;
 using namespace ::com::sun::star::uno;
 
 namespace sd {
+
+static void lcl_setLanguageForObj( SdrObject *pObj, LanguageType nLang, bool bLanguageNone = false )
+{
+    const sal_uInt16 aLangWhichId_EE[3] =
+    {
+        EE_CHAR_LANGUAGE,
+        EE_CHAR_LANGUAGE_CJK,
+        EE_CHAR_LANGUAGE_CTL
+    };
+
+    if( bLanguageNone )
+        nLang = LANGUAGE_NONE;
+
+    if( nLang != LANGUAGE_DONTKNOW )
+    {
+        if( nLang == LANGUAGE_NONE )
+        {
+            for(sal_Int32 n = 0; n < 3; n++ )
+                pObj->SetMergedItem( SvxLanguageItem( nLang, aLangWhichId_EE[n] ) );
+        }
+        else
+        {
+            sal_uInt16 nLangWhichId = 0;
+            sal_uInt16 nScriptType = SvtLanguageOptions::GetScriptTypeOfLanguage( nLang );
+            switch (nScriptType)
+            {
+                case SCRIPTTYPE_LATIN :    nLangWhichId = EE_CHAR_LANGUAGE; break;
+                case SCRIPTTYPE_ASIAN :    nLangWhichId = EE_CHAR_LANGUAGE_CJK; break;
+                case SCRIPTTYPE_COMPLEX :  nLangWhichId = EE_CHAR_LANGUAGE_CTL; break;
+                default:
+                    OSL_FAIL("unexpected case" );
+                    return;
+            }
+            pObj->SetMergedItem( SvxLanguageItem( nLang, nLangWhichId ) );
+        }
+    }
+    else    // Reset to default
+    {
+        for( sal_Int32 n = 0; n < 3; n++ )
+            pObj->ClearMergedItem( aLangWhichId_EE[n] );
+    }
+}
+
+
+static void lcl_setLanguage( const SdDrawDocument *pDoc, const OUString &rLanguage, bool bLanguageNone = false )
+{
+    LanguageType nLang = SvtLanguageTable().GetType( rLanguage );
+
+    // Do it for SdDrawDocument->SetLanguage as well?
+
+    sal_uInt16 nPageCount = pDoc->GetPageCount();   // Pick All Pages
+    for( sal_uInt16 nPage = 0; nPage < nPageCount; nPage++ )
+    {
+        const SdrPage *pPage = pDoc->GetPage( nPage );
+        sal_uIntPtr nObjCount = pPage->GetObjCount();
+        for( sal_uInt16 nObj = 0; nObj < nObjCount; nObj++ )
+        {
+            SdrObject *pObj = pPage->GetObj( nObj );
+            lcl_setLanguageForObj( pObj, nLang, bLanguageNone );
+        }
+    }
+}
 
 /*************************************************************************
 |*
@@ -206,6 +276,50 @@ void DrawDocShell::Execute( SfxRequest& rReq )
             {
                 FunctionReference aFunc( FuHangulHanjaConversion::Create( mpViewShell, mpViewShell->GetActiveWindow(), mpViewShell->GetView(), mpDoc, rReq ) );
                 static_cast< FuHangulHanjaConversion* >( aFunc.get() )->StartChineseConversion();
+            }
+        }
+        break;
+        case SID_LANGUAGE_STATUS:
+        {
+            OUString aNewLangTxt;
+            SFX_REQUEST_ARG( rReq, pItem, SfxStringItem, SID_LANGUAGE_STATUS , false );
+            if (pItem)
+                aNewLangTxt = pItem->GetValue();
+            if (aNewLangTxt == "*" )
+            {
+                // open the dialog "Tools/Options/Language Settings - Language"
+                SfxAbstractDialogFactory* pFact = SfxAbstractDialogFactory::Create();
+                if (pFact && mpViewShell)
+                {
+                    VclAbstractDialog* pDlg = pFact->CreateVclDialog( mpViewShell->GetActiveWindow(), SID_LANGUAGE_OPTIONS );
+                    pDlg->Execute();
+                    delete pDlg;
+                }
+            }
+            else
+            {
+                // setting the new language...
+                if (!aNewLangTxt.isEmpty())
+                {
+                    const OUString aDocumentLangPrefix("Default_");
+                    const OUString aStrNone("LANGUAGE_NONE");
+                    const OUString aStrResetLangs("RESET_LANGUAGES");
+                    sal_Int32 nPos = -1;
+                    if (-1 != (nPos = aNewLangTxt.indexOf( aDocumentLangPrefix , 0 )))
+                    {
+                        aNewLangTxt = aNewLangTxt.replaceAt( nPos, aDocumentLangPrefix.getLength(), "" );
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    if (aNewLangTxt == aStrNone)
+                        lcl_setLanguage( mpViewShell->GetDoc(), OUString() );
+                    else if (aNewLangTxt == aStrResetLangs)
+                        lcl_setLanguage( mpViewShell->GetDoc(), OUString(), true );
+                    else
+                        lcl_setLanguage( mpViewShell->GetDoc(), aNewLangTxt );
+                }
             }
         }
         break;
