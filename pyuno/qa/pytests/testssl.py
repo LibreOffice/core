ca4a39eff66af3a27e1c02baff09aaab48f266e1
Caolán McNamara <caolanm@redhat.com>
2014-04-24 14:31:24 +0100

    as suspected
    Change-Id: Idad0ba862da7317b5824f1bfb400d05cef105b7f

--- pyuno/qa/pytests/testssl.py
+++ pyuno/qa/pytests/testssl.py
@@ -3,7 +3,7 @@ import unittest
 #I want to ensure that import ssl works on all platforms
 class SSLTest(unittest.TestCase):
     def test_ssl_import(self):
-        import _ssl
+        import ssl
 
 if __name__ == '__main__':
     unittest.main()
