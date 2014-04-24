c1e8cd6f825f9981a6473afd883ec667d5579746
Miklos Vajna <vmiklos@collabora.co.uk>
2014-04-24 21:49:54 +0200

    chart2: use constructor feature for ElementSelectorToolbarController
    Change-Id: Iee99f1f86af4ed494dbbe33b31baeb707b7ed544

--- chart2/source/controller/main/ElementSelector.cxx
+++ chart2/source/controller/main/ElementSelector.cxx
@@ -318,4 +318,11 @@ uno::Reference< awt::XWindow > SAL_CALL ElementSelectorToolbarController::create
 
 } // chart2
 
+extern "C" SAL_DLLPUBLIC_EXPORT css::uno::XInterface * SAL_CALL
+com_sun_star_comp_chart_ElementSelectorToolbarController_get_implementation(css::uno::XComponentContext *context,
+                                                                            css::uno::Sequence<css::uno::Any> const &)
+{
+    return cppu::acquire(new chart::ElementSelectorToolbarController(context));
+}
+
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
--- chart2/source/controller/main/_serviceregistration_controller.cxx
+++ chart2/source/controller/main/_serviceregistration_controller.cxx
@@ -70,14 +70,6 @@ static const struct ::cppu::ImplementationEntry g_entries_chart2_controller[] =
         , 0
     }
     ,{
-          ::chart::ElementSelectorToolbarController::create
-        , ::chart::ElementSelectorToolbarController::getImplementationName_Static
-        , ::chart::ElementSelectorToolbarController::getSupportedServiceNames_Static
-        , ::cppu::createSingleComponentFactory
-        , 0
-        , 0
-    }
-    ,{
           ::chart::ShapeToolbarController::create
         , ::chart::ShapeToolbarController::getImplementationName_Static
         , ::chart::ShapeToolbarController::getSupportedServiceNames_Static
