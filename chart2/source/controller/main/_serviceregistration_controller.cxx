98fa76296ef77391cab213951fbb65960a894f12
Miklos Vajna <vmiklos@collabora.co.uk>
2014-04-25 10:23:18 +0200

    chart2: use constructor feature for ChartTypeDialog
    Change-Id: I5efc89d8d0139216c1ae92aa692704f9f58915ca

--- chart2/source/controller/main/_serviceregistration_controller.cxx
+++ chart2/source/controller/main/_serviceregistration_controller.cxx
@@ -38,14 +38,6 @@ static const struct ::cppu::ImplementationEntry g_entries_chart2_controller[] =
         , 0
     }
     ,{
-          ::chart::ChartTypeUnoDlg::Create
-        , ::chart::ChartTypeUnoDlg::getImplementationName_Static
-        , ::chart::ChartTypeUnoDlg::getSupportedServiceNames_Static
-        , ::cppu::createSingleComponentFactory
-        , 0
-        , 0
-    }
-    ,{
           ::chart::ShapeToolbarController::create
         , ::chart::ShapeToolbarController::getImplementationName_Static
         , ::chart::ShapeToolbarController::getSupportedServiceNames_Static
