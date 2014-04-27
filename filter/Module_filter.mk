f82f7bf3dd5053049259f6933d1335f6c9e314dd
Maxim Monastirsky <momonasmon@gmail.com>
2014-04-27 15:54:58 +0300

    Introduce centralized detection for storage-based files
    This will allow us to get rid of the duplicated code,
    and simplify the type detectors.
    
    Also this commit fixes fdo#46310 and fdo#74978.
    
    Change-Id: I08e958ab10d05e1036cdc2bed5ce14bf73b6245c
    Reviewed-on: https://gerrit.libreoffice.org/9221
    Reviewed-by: Maxim Monastirsky <momonasmon@gmail.com>
    Tested-by: Maxim Monastirsky <momonasmon@gmail.com>

--- filter/Module_filter.mk
+++ filter/Module_filter.mk
@@ -50,6 +50,7 @@ $(eval $(call gb_Module_add_targets,filter,\
 	Library_odfflatxml \
 	Library_pdffilter \
 	Library_placeware \
+	Library_storagefd \
 	Library_svgfilter \
 	Library_graphicfilter \
 	Library_t602filter \
---
+++ filter/source/storagefilterdetect/filterdetect.hxx
@@ -0,0 +1,72 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#ifndef INCLUDED_FILTER_SOURCE_STORAGEFILTERDETECT_FILTERDETECT_HXX
+#define INCLUDED_FILTER_SOURCE_STORAGEFILTERDETECT_FILTERDETECT_HXX
+
+#include <com/sun/star/document/XExtendedFilterDetection.hpp>
+#include <com/sun/star/lang/XInitialization.hpp>
+#include <com/sun/star/lang/XServiceInfo.hpp>
+#include <com/sun/star/uno/XComponentContext.hpp>
+
+#include <cppuhelper/implbase3.hxx>
+
+class StorageFilterDetect : public cppu::WeakImplHelper3<
+    com::sun::star::document::XExtendedFilterDetection,
+    com::sun::star::lang::XInitialization,
+    com::sun::star::lang::XServiceInfo>
+{
+    com::sun::star::uno::Reference<com::sun::star::uno::XComponentContext> mxCxt;
+
+public:
+
+    StorageFilterDetect (const com::sun::star::uno::Reference<com::sun::star::uno::XComponentContext>& xCxt);
+    virtual ~StorageFilterDetect();
+
+    // XExtendedFilterDetection
+    virtual OUString SAL_CALL detect(com::sun::star::uno::Sequence<com::sun::star::beans::PropertyValue>& rDescriptor)
+            throw( com::sun::star::uno::RuntimeException, std::exception ) SAL_OVERRIDE;
+
+    // XInitialization
+    virtual void SAL_CALL initialize(const ::com::sun::star::uno::Sequence<com::sun::star::uno::Any>& aArguments)
+        throw (com::sun::star::uno::Exception, com::sun::star::uno::RuntimeException, std::exception) SAL_OVERRIDE;
+
+    // XServiceInfo
+    virtual OUString SAL_CALL getImplementationName()
+        throw (com::sun::star::uno::RuntimeException, std::exception) SAL_OVERRIDE;
+
+    virtual sal_Bool SAL_CALL supportsService(const OUString& ServiceName)
+        throw (com::sun::star::uno::RuntimeException, std::exception) SAL_OVERRIDE;
+
+    virtual com::sun::star::uno::Sequence<OUString> SAL_CALL getSupportedServiceNames()
+        throw (com::sun::star::uno::RuntimeException, std::exception) SAL_OVERRIDE;
+};
+
+OUString StorageFilterDetect_getImplementationName();
+
+sal_Bool StorageFilterDetect_supportsService(const OUString& ServiceName);
+
+com::sun::star::uno::Sequence<OUString> StorageFilterDetect_getSupportedServiceNames();
+
+com::sun::star::uno::Reference<com::sun::star::uno::XInterface>
+StorageFilterDetect_createInstance(const com::sun::star::uno::Reference<com::sun::star::uno::XComponentContext>& rCxt);
+
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
