LibreOffice/core

commit 465730622e2eb77828c1cf4845f8e8a40b92f1b2

svx: speed up ObjectContactOfPageView in case there is nothing to display
Reviewed on:
	https://gerrit.libreoffice.org/8675

Change-Id: I8d797acb3ca9e2341c5db08b2fc409a3cd97635e

svx/source/sdr/contact/objectcontactofpageview.cxx
@@ -233,9 +233,30 @@ namespace sdr
                 uno::Sequence<beans::PropertyValue>());
             updateViewInformation2D(aNewViewInformation2D);
 
-            // get whole Primitive2DSequence; this will already make use of updated ViewInformation2D
-            // and may use the MapMode from the Target OutDev in the DisplayInfo
-            drawinglayer::primitive2d::Primitive2DSequence xPrimitiveSequence(rDrawPageVOContact.getPrimitive2DSequenceHierarchy(rDisplayInfo));
+            drawinglayer::primitive2d::Primitive2DSequence xPrimitiveSequence;
+            // Only get the expensive hierarchy if we can be sure that the
+            // returned sequence won't be empty anyway.
+            bool bGetHierarchy = rRedrawArea.IsEmpty();
+            if (!bGetHierarchy)
+            {
+                // Not empty? Then not doing a full redraw, check if
+                // getPrimitive2DSequenceHierarchy() is still needed.
+                sal_Int32 nObjCount = GetSdrPage()->GetObjCount();
+                for (sal_Int32 i = 0; i < nObjCount; ++i)
+                {
+                    SdrObject* pObject = GetSdrPage()->GetObj(i);
+                    if (rRedrawArea.IsOver(pObject->GetCurrentBoundRect()))
+                    {
+                        bGetHierarchy = true;
+                        break;
+                    }
+                }
+            }
+
+            if (bGetHierarchy)
+                // get whole Primitive2DSequence; this will already make use of updated ViewInformation2D
+                // and may use the MapMode from the Target OutDev in the DisplayInfo
+                xPrimitiveSequence = rDrawPageVOContact.getPrimitive2DSequenceHierarchy(rDisplayInfo);
 
             // if there is something to show, use a primitive processor to render it. There
             // is a choice between VCL and Canvas processors currently. The decision is made in

commit 89e14d6c318580354081b3b3ad39188f25481ae7

Fix repainting problems in Draw.
Regression from 465730622e2eb77828c1cf4845f8e8a40b92f1b2; unfortunately the
hierarchy can contain more than just the sdr shapes, in which case it fails to
recognize that there is still something to paint.

We need to improve the way getPrimitive2DSequenceHierarchy() works, and come
up with a way how to cut early the primitives that are not interesting for the
area that we are redrawing.

Change-Id: Ieb2e40a576e9a394fe28f248e582c132ab5b6578

svx/source/sdr/contact/objectcontactofpageview.cxx
@@ -17,6 +17,8 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
+#include <config_features.h>
+
 #include <svx/sdr/contact/objectcontactofpageview.hxx>
 #include <svx/sdr/contact/viewobjectcontactofunocontrol.hxx>
 #include <svx/svdpagv.hxx>
@@ -231,6 +233,18 @@ namespace sdr
             updateViewInformation2D(aNewViewInformation2D);
 
             drawinglayer::primitive2d::Primitive2DSequence xPrimitiveSequence;
+#if HAVE_FEATURE_DESKTOP
+            // get whole Primitive2DSequence; this will already make use of updated ViewInformation2D
+            // and may use the MapMode from the Target OutDev in the DisplayInfo
+            xPrimitiveSequence = rDrawPageVOContact.getPrimitive2DSequenceHierarchy(rDisplayInfo);
+#else
+            // HACK: this only works when we are drawing sdr shapes via
+            // drawinglayer; but it can happen that the hierarchy contains
+            // more than just the shapes, and then it fails.
+            //
+            // This is good enough for the tiled rendering for the moment, but
+            // we need to come up with with the real solution shortly.
+
             // Only get the expensive hierarchy if we can be sure that the
             // returned sequence won't be empty anyway.
             bool bGetHierarchy = rRedrawArea.IsEmpty();
@@ -254,6 +268,7 @@ namespace sdr
                 // get whole Primitive2DSequence; this will already make use of updated ViewInformation2D
                 // and may use the MapMode from the Target OutDev in the DisplayInfo
                 xPrimitiveSequence = rDrawPageVOContact.getPrimitive2DSequenceHierarchy(rDisplayInfo);
+#endif
 
             // if there is something to show, use a primitive processor to render it. There
             // is a choice between VCL and Canvas processors currently. The decision is made in