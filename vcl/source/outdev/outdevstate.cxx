54a5880ba04b55460bd7854b17d5edb94a78cf91
Chris Sherlock <chris.sherlock79@gmail.com>
2014-04-28 20:22:27 +1000

    VCL move SetAntialiasing from outdev.cxx to outdevstate.cxx
    Change-Id: I0d0336f4f3a7add0f11f7b8d19aec517c3d8c9af

--- vcl/source/outdev/outdevstate.cxx
+++ vcl/source/outdev/outdevstate.cxx
@@ -262,6 +262,25 @@ void OutputDevice::EnableOutput( bool bEnable )
         mpAlphaVDev->EnableOutput( bEnable );
 }
 
+
+void OutputDevice::SetAntialiasing( sal_uInt16 nMode )
+{
+    if ( mnAntialiasing != nMode )
+    {
+        mnAntialiasing = nMode;
+        mbInitFont = true;
+
+        if(mpGraphics)
+        {
+            mpGraphics->setAntiAliasB2DDraw(mnAntialiasing & ANTIALIASING_ENABLE_B2DDRAW);
+        }
+    }
+
+    if( mpAlphaVDev )
+        mpAlphaVDev->SetAntialiasing( nMode );
+}
+
+
 void OutputDevice::SetFillColor()
 {
 
