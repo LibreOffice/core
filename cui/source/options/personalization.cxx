41610f99cfbf910a83957859f2d3e68097cab2d9
Rachit Gupta <rachitgupta1792@gmail.com>
2014-04-29 19:48:48 +0530

    Created basic architecture for PersonasDocHandler.
    Change-Id: Ic1454344756c48090ebe821799d10dd6ace0264c

--- cui/source/options/personalization.cxx
+++ cui/source/options/personalization.cxx
@@ -31,8 +31,16 @@
 #include <com/sun/star/ui/dialogs/TemplateDescription.hpp>
 #include <com/sun/star/ui/dialogs/XFilePickerControlAccess.hpp>
 #include <com/sun/star/ui/dialogs/XFilterManager.hpp>
+#include <com/sun/star/xml/sax/XParser.hpp>
+#include <com/sun/star/xml/sax/Parser.hpp>
+#include "ucbhelper/content.hxx"
+
+#include "personasdochandler.hxx"
 
 using namespace com::sun::star;
+using namespace ::com::sun::star::uno;
+using namespace ::com::sun::star::ucb;
+
 
 /** Dialog that will allow the user to choose a Persona to use.
 
@@ -78,9 +86,18 @@ OUString SelectPersonaDialog::GetPersonaURL() const
 
 IMPL_LINK( SelectPersonaDialog, VisitPersonas, PushButton*, /*pButton*/ )
 {
-    uno::Reference< com::sun::star::system::XSystemShellExecute > xSystemShell( com::sun::star::system::SystemShellExecute::create( ::comphelper::getProcessComponentContext() ) );
+    // uno::Reference< com::sun::star::system::XSystemShellExecute > xSystemShell( com::sun::star::system::SystemShellExecute::create( ::comphelper::getProcessComponentContext() ) );
 
-    xSystemShell->execute( "https://addons.mozilla.org/firefox/themes/", OUString(), com::sun::star::system::SystemShellExecuteFlags::URIS_ONLY );
+    // xSystemShell->execute( "https://addons.mozilla.org/firefox/themes/", OUString(), com::sun::star::system::SystemShellExecuteFlags::URIS_ONLY );
+    Reference<XComponentContext> xContext( ::comphelper::getProcessComponentContext() );
+    Reference< xml::sax::XParser > xParser = xml::sax::Parser::create(xContext);
+    PersonasDocHandler* pHandler = new PersonasDocHandler();
+    Reference< xml::sax::XDocumentHandler > xDocHandler = pHandler;
+    xParser->setDocumentHandler( xDocHandler );
+
+    OUString sParcelDescURL = "parcel-descriptor.xml";
+
+    ::ucbhelper::Content ucb_content;
 
     return 0;
 }
---
+++ cui/source/options/personasdochandler.hxx
@@ -0,0 +1,73 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#include <cppuhelper/implbase1.hxx>
+
+#include <com/sun/star/xml/sax/XAttributeList.hpp>
+#include <com/sun/star/xml/sax/SAXException.hpp>
+#include <com/sun/star/xml/sax/XDocumentHandler.hpp>
+#include <com/sun/star/xml/sax/XExtendedDocumentHandler.hpp>
+#include <com/sun/star/xml/sax/XParser.hpp>
+
+#include <com/sun/star/lang/NoSupportException.hpp>
+#include <com/sun/star/lang/XMultiComponentFactory.hpp>
+
+typedef ::cppu::WeakImplHelper1< css::xml::sax::XDocumentHandler > t_DocHandlerImpl;
+
+class PersonasDocHandler : public t_DocHandlerImpl
+{
+private:
+    bool m_bIsParsed;
+    OUString m_sLang;
+    sal_Int32 skipIndex;
+public:
+    PersonasDocHandler():m_bIsParsed( false ), skipIndex( 0 ){}
+    OUString getParcelLanguage() { return m_sLang; }
+    bool isParsed() { return m_bIsParsed; }
+    // XDocumentHandler
+    virtual void SAL_CALL startDocument()
+        throw ( css::xml::sax::SAXException, css::uno::RuntimeException, std::exception ) SAL_OVERRIDE;
+
+    virtual void SAL_CALL endDocument()
+        throw ( css::xml::sax::SAXException, css::uno::RuntimeException, std::exception ) SAL_OVERRIDE;
+
+    virtual void SAL_CALL startElement( const OUString& aName,
+        const css::uno::Reference< css::xml::sax::XAttributeList > & xAttribs )
+        throw ( css::xml::sax::SAXException,
+            css::uno::RuntimeException, std::exception ) SAL_OVERRIDE;
+
+    virtual void SAL_CALL endElement( const OUString & aName )
+        throw ( css::xml::sax::SAXException, css::uno::RuntimeException, std::exception ) SAL_OVERRIDE;
+
+    virtual void SAL_CALL characters( const OUString & aChars )
+        throw ( css::xml::sax::SAXException, css::uno::RuntimeException, std::exception ) SAL_OVERRIDE;
+
+    virtual void SAL_CALL ignorableWhitespace( const OUString & aWhitespaces )
+        throw ( css::xml::sax::SAXException, css::uno::RuntimeException, std::exception ) SAL_OVERRIDE;
+
+    virtual void SAL_CALL processingInstruction(
+        const OUString & aTarget, const OUString & aData )
+        throw ( css::xml::sax::SAXException, css::uno::RuntimeException, std::exception ) SAL_OVERRIDE;
+
+    virtual void SAL_CALL setDocumentLocator(
+        const css::uno::Reference< css::xml::sax::XLocator >& xLocator )
+        throw ( css::xml::sax::SAXException, css::uno::RuntimeException, std::exception ) SAL_OVERRIDE;
+};
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
