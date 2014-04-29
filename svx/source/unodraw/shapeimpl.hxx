4d82d12fd5f42d72844a403ba486e2a696ef00f3
Caolán McNamara <caolanm@redhat.com>
2014-04-29 20:56:49 +0100

    coverity#1210130 Uncaught exception
    Change-Id: I6f4523a70caac6f3d43a11df78b58575b7c7a720

--- svx/source/unodraw/shapeimpl.hxx
+++ svx/source/unodraw/shapeimpl.hxx
@@ -109,7 +109,7 @@ class SvxTableShape : public SvxShape
 protected:
     // overide these for special property handling in subcasses. Return true if property is handled
     virtual bool setPropertyValueImpl( const OUString& rName, const SfxItemPropertySimpleEntry* pProperty, const ::com::sun::star::uno::Any& rValue ) throw(::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::beans::PropertyVetoException, ::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::lang::WrappedTargetException, ::com::sun::star::uno::RuntimeException) SAL_OVERRIDE;
-    virtual bool getPropertyValueImpl( const OUString& rName, const SfxItemPropertySimpleEntry* pProperty, ::com::sun::star::uno::Any& rValue ) throw(::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::lang::WrappedTargetException, ::com::sun::star::uno::RuntimeException) SAL_OVERRIDE;
+    virtual bool getPropertyValueImpl( const OUString& rName, const SfxItemPropertySimpleEntry* pProperty, ::com::sun::star::uno::Any& rValue ) throw(::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::lang::WrappedTargetException, ::com::sun::star::uno::RuntimeException, std::exception) SAL_OVERRIDE;
 
     virtual void lock() SAL_OVERRIDE;
     virtual void unlock() SAL_OVERRIDE;
--- svx/source/unodraw/tableshape.cxx
+++ svx/source/unodraw/tableshape.cxx
@@ -107,7 +107,7 @@ bool SvxTableShape::getPropertyValueImpl(
     const OUString& rName,
     const SfxItemPropertySimpleEntry* pProperty,
     ::com::sun::star::uno::Any& rValue )
-        throw(::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::lang::WrappedTargetException, ::com::sun::star::uno::RuntimeException)
+        throw(::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::lang::WrappedTargetException, ::com::sun::star::uno::RuntimeException, std::exception)
 {
     switch( pProperty->nWID )
     {
