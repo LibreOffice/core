66c2360491614597ba1e8b05dff33e6b142e382d
Markus Mohrhard <markus.mohrhard@googlemail.com>
2014-04-30 01:20:36 +0200

    set graphic properties for error bars during import, fdo#78041
    Change-Id: I43138745b074dcacdafa090d97eeb9aa2ccecf78

--- xmloff/source/chart/SchXMLPlotAreaContext.cxx
+++ xmloff/source/chart/SchXMLPlotAreaContext.cxx
@@ -1025,6 +1025,19 @@ SchXMLStatisticsObjectContext::~SchXMLStatisticsObjectContext()
 
 namespace {
 
+void SetErrorBarStyleProperties( const OUString& rStyleName, uno::Reference< beans::XPropertySet > xBarProp,
+                                        SchXMLImportHelper& rImportHelper )
+{
+    const SvXMLStylesContext* pStylesCtxt = rImportHelper.GetAutoStylesContext();
+    const SvXMLStyleContext* pStyle = pStylesCtxt->FindStyleChildContext(rImportHelper.GetChartFamilyID(),
+            rStyleName);
+
+    XMLPropStyleContext * pSeriesStyleContext =
+        const_cast< XMLPropStyleContext * >( dynamic_cast< const XMLPropStyleContext * >( pStyle ));
+
+    pSeriesStyleContext->FillPropertySet( xBarProp );
+}
+
 void SetErrorBarPropertiesFromStyleName( const OUString& aStyleName, uno::Reference< beans::XPropertySet> xBarProp,
                                             SchXMLImportHelper& rImportHelper, OUString& aPosRange, OUString& aNegRange)
 {
@@ -1198,6 +1211,8 @@ void SchXMLStatisticsObjectContext::StartElement( const uno::Reference< xml::sax
 
 
                     // first import defaults from parent style
+                    SetErrorBarStyleProperties( maSeriesStyleName, xBarProp, mrImportHelper );
+                    SetErrorBarStyleProperties( sAutoStyleName, xBarProp, mrImportHelper );
                     SetErrorBarPropertiesFromStyleName( maSeriesStyleName, xBarProp, mrImportHelper, aPosRange, aNegRange );
                     SetErrorBarPropertiesFromStyleName( sAutoStyleName, xBarProp, mrImportHelper, aPosRange, aNegRange );
 
