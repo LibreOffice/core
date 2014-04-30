e72131b84a464464d6b64ecbe5816e2bad0d4094
Eike Rathke <erack@redhat.com>
2014-04-30 17:51:05 +0100

    resolve crashes with ICU 53.1 in locales with collator data, fdo#77071
    ICU 53.1 changed API behavior:
    https://ssl.icu-project.org/apiref/icu4c/classicu_1_1RuleBasedCollator.html#a2f4c7eeaf020ad68e3bd9722dd272357
    isn't correct anymore:
    
    length: size of the image. If negative, the API will try to figure out the
            length of the image
    
            NO, IT WILL NOT! It bails out with an error instead.
            Introduced a function to obtain the length for each collator data.
    
    base:  collator, usually root. The base is required to be present through the
           lifetime of the collator. Currently it cannot be NULL.
    
           NOT "usually root"! There's a check now that bails out if
           if(base->tailoring != CollationRoot::getRoot(errorCode))
           So using an instance created with icu::Locale::getRoot()
    
    (cherry picked from commit a3c627fe38236e82bc6008075d862b3cbfbd9ce3)
    
    Conflicts:
    	i18npool/source/collator/collator_unicode.cxx
    
    Backported.
    
    Change-Id: Ia9c4e27d5ef4f8083bbe57e4e2f4b3ff63bb42ed

--- i18npool/source/collator/gencoll_rule.cxx
+++ i18npool/source/collator/gencoll_rule.cxx
@@ -65,8 +65,10 @@ void data_write(char* file, char* name, sal_uInt8 *data, sal_Int32 len)
 
     fprintf(fp, "#ifndef DISABLE_DYNLOADING\n");
     fprintf(fp, "SAL_DLLPUBLIC_EXPORT const sal_uInt8* get_%s() { return %s; }\n", name, name);
+    fprintf(fp, "SAL_DLLPUBLIC_EXPORT size_t get_%s_length() { return sizeof(%s); }\n", name, name);
     fprintf(fp, "#else\n");
     fprintf(fp, "SAL_DLLPUBLIC_EXPORT const sal_uInt8* get_collator_data_%s() { return %s; }\n", name, name);
+    fprintf(fp, "SAL_DLLPUBLIC_EXPORT size_t get_collator_data_%s_length() { return sizeof(%s); }\n", name, name);
     fprintf(fp, "#endif\n");
     fprintf(fp, "\n");
     fprintf (fp, "}\n");
