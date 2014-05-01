8d3295bdac2182d58e6ef3a1eeb3313a1ca19c0d
Miklos Vajna <vmiklos@collabora.co.uk>
2014-05-01 20:28:08 +0200

    writerfilter: fix indentation in rtf{fly,listener}
    Change-Id: I6cbb7ee535cb3c6aab2968150feff9e80fd7e010

--- writerfilter/source/rtftok/rtffly.hxx
+++ writerfilter/source/rtftok/rtffly.hxx
@@ -10,151 +10,153 @@
 #ifndef INCLUDED_WRITERFILTER_SOURCE_RTFTOK_RTFFLY_HXX
 #define INCLUDED_WRITERFILTER_SOURCE_RTFTOK_RTFFLY_HXX
 
-namespace writerfilter {
-    namespace rtftok {
-        /// Stores the vertical orientation properties of an RTF fly frame.
-        class RTFVertOrient
+namespace writerfilter
+{
+namespace rtftok
+{
+/// Stores the vertical orientation properties of an RTF fly frame.
+class RTFVertOrient
+{
+public:
+    RTFVertOrient(sal_uInt16 nValue)
+        : nVal(nValue)
+    {
+    }
+
+    sal_uInt16 GetOrient() const
+    {
+        return OSL_LONIBBLE(OSL_LOBYTE(nVal));
+    }
+
+    sal_uInt16 GetRelation() const
+    {
+        return OSL_HINIBBLE(OSL_LOBYTE(nVal));
+    }
+
+    sal_Int32 GetAlign() const
+    {
+        sal_Int32 nAlign = 0;
+        switch (GetOrient())
         {
-            public:
-                RTFVertOrient(sal_uInt16 nValue)
-                    : nVal(nValue)
-                {
-                }
+        case text::VertOrientation::CENTER:
+            nAlign = NS_ooxml::LN_Value_wordprocessingml_ST_YAlign_center;
+            break;
+        case text::VertOrientation::TOP:
+            nAlign = NS_ooxml::LN_Value_wordprocessingml_ST_YAlign_top;
+            break;
+        case text::VertOrientation::BOTTOM:
+            nAlign = NS_ooxml::LN_Value_wordprocessingml_ST_YAlign_bottom;
+            break;
+        }
 
-                sal_uInt16 GetOrient() const
-                {
-                    return OSL_LONIBBLE(OSL_LOBYTE(nVal));
-                }
+        return nAlign;
+    }
 
-                sal_uInt16 GetRelation() const
-                {
-                    return OSL_HINIBBLE(OSL_LOBYTE(nVal));
-                }
-
-                sal_Int32 GetAlign() const
-                {
-                    sal_Int32 nAlign = 0;
-                    switch( GetOrient( ) )
-                    {
-                        case text::VertOrientation::CENTER:
-                            nAlign = NS_ooxml::LN_Value_wordprocessingml_ST_YAlign_center;
-                            break;
-                        case text::VertOrientation::TOP:
-                            nAlign = NS_ooxml::LN_Value_wordprocessingml_ST_YAlign_top;
-                            break;
-                        case text::VertOrientation::BOTTOM:
-                            nAlign = NS_ooxml::LN_Value_wordprocessingml_ST_YAlign_bottom;
-                            break;
-                    }
-
-                    return nAlign;
-                }
-
-                sal_Int32 GetAnchor() const
-                {
-                    sal_Int32 nAnchor = 0;
-                    switch( GetRelation( ) )
-                    {
-                        case text::RelOrientation::FRAME:
-                            nAnchor = NS_ooxml::LN_Value_wordprocessingml_ST_VAnchor_text;
-                            break;
-                        case text::RelOrientation::PAGE_FRAME:
-                            nAnchor = NS_ooxml::LN_Value_wordprocessingml_ST_VAnchor_page;
-                            break;
-                        case text::RelOrientation::PAGE_PRINT_AREA:
-                            nAnchor = NS_ooxml::LN_Value_wordprocessingml_ST_VAnchor_margin;
-                            break;
-                    }
-
-                    return nAnchor;
-                }
-
-                sal_uInt16 GetValue() const
-                {
-                    return nVal;
-                }
-
-            private:
-                sal_uInt16 nVal;
-        };
-
-        /// Stores the horizontal orientation properties of an RTF fly frame.
-        class RTFHoriOrient
+    sal_Int32 GetAnchor() const
+    {
+        sal_Int32 nAnchor = 0;
+        switch (GetRelation())
         {
-            public:
-                RTFHoriOrient(sal_uInt16 nValue)
-                    : nVal(nValue)
-                {
-                }
+        case text::RelOrientation::FRAME:
+            nAnchor = NS_ooxml::LN_Value_wordprocessingml_ST_VAnchor_text;
+            break;
+        case text::RelOrientation::PAGE_FRAME:
+            nAnchor = NS_ooxml::LN_Value_wordprocessingml_ST_VAnchor_page;
+            break;
+        case text::RelOrientation::PAGE_PRINT_AREA:
+            nAnchor = NS_ooxml::LN_Value_wordprocessingml_ST_VAnchor_margin;
+            break;
+        }
 
-                sal_uInt16 GetOrient() const
-                {
-                    return OSL_LONIBBLE(OSL_LOBYTE(nVal));
-                }
+        return nAnchor;
+    }
 
-                sal_uInt16 GetRelation() const
-                {
-                    return OSL_LONIBBLE(OSL_HIBYTE(nVal));
-                }
+    sal_uInt16 GetValue() const
+    {
+        return nVal;
+    }
 
-                sal_uInt16 GetRelAnchor() const
-                {
-                    return OSL_HINIBBLE(OSL_LOBYTE(nVal));
-                }
+private:
+    sal_uInt16 nVal;
+};
 
-                sal_Int32 GetAlign() const
-                {
-                    sal_Int32 nAlign = 0;
-                    switch( GetOrient( ) )
-                    {
-                        case text::HoriOrientation::CENTER:
-                            nAlign = NS_ooxml::LN_Value_wordprocessingml_ST_XAlign_center;
-                            break;
-                        case text::HoriOrientation::RIGHT:
-                            nAlign = NS_ooxml::LN_Value_wordprocessingml_ST_XAlign_right;
-                            break;
-                        case text::HoriOrientation::LEFT:
-                            nAlign = NS_ooxml::LN_Value_wordprocessingml_ST_XAlign_left;
-                            break;
-                        case text::HoriOrientation::INSIDE:
-                            nAlign = NS_ooxml::LN_Value_wordprocessingml_ST_XAlign_inside;
-                            break;
-                        case text::HoriOrientation::OUTSIDE:
-                            nAlign = NS_ooxml::LN_Value_wordprocessingml_ST_XAlign_outside;
-                            break;
-                    }
+/// Stores the horizontal orientation properties of an RTF fly frame.
+class RTFHoriOrient
+{
+public:
+    RTFHoriOrient(sal_uInt16 nValue)
+        : nVal(nValue)
+    {
+    }
 
-                    return nAlign;
-                }
+    sal_uInt16 GetOrient() const
+    {
+        return OSL_LONIBBLE(OSL_LOBYTE(nVal));
+    }
 
-                sal_Int32 GetAnchor() const
-                {
-                    sal_Int32 nAnchor = 0;
-                    switch( GetRelation( ) )
-                    {
-                        case text::RelOrientation::FRAME:
-                            nAnchor = NS_ooxml::LN_Value_wordprocessingml_ST_HAnchor_text;
-                            break;
-                        case text::RelOrientation::PAGE_FRAME:
-                            nAnchor = NS_ooxml::LN_Value_wordprocessingml_ST_HAnchor_page;
-                            break;
-                        case text::RelOrientation::PAGE_PRINT_AREA:
-                            nAnchor = NS_ooxml::LN_Value_wordprocessingml_ST_HAnchor_margin;
-                            break;
-                    }
+    sal_uInt16 GetRelation() const
+    {
+        return OSL_LONIBBLE(OSL_HIBYTE(nVal));
+    }
 
-                    return nAnchor;
-                }
+    sal_uInt16 GetRelAnchor() const
+    {
+        return OSL_HINIBBLE(OSL_LOBYTE(nVal));
+    }
 
-                sal_uInt16 GetValue() const
-                {
-                    return nVal;
-                }
+    sal_Int32 GetAlign() const
+    {
+        sal_Int32 nAlign = 0;
+        switch (GetOrient())
+        {
+        case text::HoriOrientation::CENTER:
+            nAlign = NS_ooxml::LN_Value_wordprocessingml_ST_XAlign_center;
+            break;
+        case text::HoriOrientation::RIGHT:
+            nAlign = NS_ooxml::LN_Value_wordprocessingml_ST_XAlign_right;
+            break;
+        case text::HoriOrientation::LEFT:
+            nAlign = NS_ooxml::LN_Value_wordprocessingml_ST_XAlign_left;
+            break;
+        case text::HoriOrientation::INSIDE:
+            nAlign = NS_ooxml::LN_Value_wordprocessingml_ST_XAlign_inside;
+            break;
+        case text::HoriOrientation::OUTSIDE:
+            nAlign = NS_ooxml::LN_Value_wordprocessingml_ST_XAlign_outside;
+            break;
+        }
 
-            private:
-                sal_uInt16 nVal;
-        };
-    } // namespace rtftok
+        return nAlign;
+    }
+
+    sal_Int32 GetAnchor() const
+    {
+        sal_Int32 nAnchor = 0;
+        switch (GetRelation())
+        {
+        case text::RelOrientation::FRAME:
+            nAnchor = NS_ooxml::LN_Value_wordprocessingml_ST_HAnchor_text;
+            break;
+        case text::RelOrientation::PAGE_FRAME:
+            nAnchor = NS_ooxml::LN_Value_wordprocessingml_ST_HAnchor_page;
+            break;
+        case text::RelOrientation::PAGE_PRINT_AREA:
+            nAnchor = NS_ooxml::LN_Value_wordprocessingml_ST_HAnchor_margin;
+            break;
+        }
+
+        return nAnchor;
+    }
+
+    sal_uInt16 GetValue() const
+    {
+        return nVal;
+    }
+
+private:
+    sal_uInt16 nVal;
+};
+} // namespace rtftok
 } // namespace writerfilter
 
 #endif // INCLUDED_WRITERFILTER_SOURCE_RTFTOK_RTFFLY_HXX
--- writerfilter/source/rtftok/rtflistener.hxx
+++ writerfilter/source/rtftok/rtflistener.hxx
@@ -12,59 +12,61 @@
 
 #include <rtfcontrolwords.hxx>
 
-namespace writerfilter {
-    namespace rtftok {
-        enum RTFInternalState
-        {
-            INTERNAL_NORMAL,
-            INTERNAL_BIN,
-            INTERNAL_HEX
-        };
+namespace writerfilter
+{
+namespace rtftok
+{
+enum RTFInternalState
+{
+    INTERNAL_NORMAL,
+    INTERNAL_BIN,
+    INTERNAL_HEX
+};
 
-        enum RTFErrors
-        {
-            ERROR_OK,
-            ERROR_GROUP_UNDER,
-            ERROR_GROUP_OVER,
-            ERROR_EOF,
-            ERROR_HEX_INVALID,
-            ERROR_CHAR_OVER
-        };
+enum RTFErrors
+{
+    ERROR_OK,
+    ERROR_GROUP_UNDER,
+    ERROR_GROUP_OVER,
+    ERROR_EOF,
+    ERROR_HEX_INVALID,
+    ERROR_CHAR_OVER
+};
 
-        /**
-         * RTFTokenizer needs a class implementing this this interface. While
-         * RTFTokenizer separates control words (and their arguments) from
-         * text, the class implementing this interface is expected to map the
-         * raw RTF tokens to dmapper tokens.
-         */
-        class RTFListener
-        {
-            public:
-                virtual ~RTFListener() { }
-                // Dispatching of control words and characters.
-                virtual int dispatchDestination(RTFKeyword nKeyword) = 0;
-                virtual int dispatchFlag(RTFKeyword nKeyword) = 0;
-                virtual int dispatchSymbol(RTFKeyword nKeyword) = 0;
-                virtual int dispatchToggle(RTFKeyword nKeyword, bool bParam, int nParam) = 0;
-                virtual int dispatchValue(RTFKeyword nKeyword, int nParam) = 0;
-                virtual int resolveChars(char ch) = 0;
+/**
+ * RTFTokenizer needs a class implementing this this interface. While
+ * RTFTokenizer separates control words (and their arguments) from
+ * text, the class implementing this interface is expected to map the
+ * raw RTF tokens to dmapper tokens.
+ */
+class RTFListener
+{
+public:
+    virtual ~RTFListener() { }
+    // Dispatching of control words and characters.
+    virtual int dispatchDestination(RTFKeyword nKeyword) = 0;
+    virtual int dispatchFlag(RTFKeyword nKeyword) = 0;
+    virtual int dispatchSymbol(RTFKeyword nKeyword) = 0;
+    virtual int dispatchToggle(RTFKeyword nKeyword, bool bParam, int nParam) = 0;
+    virtual int dispatchValue(RTFKeyword nKeyword, int nParam) = 0;
+    virtual int resolveChars(char ch) = 0;
 
-                // State handling.
-                virtual int pushState() = 0;
-                virtual int popState() = 0;
+    // State handling.
+    virtual int pushState() = 0;
+    virtual int popState() = 0;
 
-                virtual RTFDestinationState getDestinationState() = 0;
-                virtual void setDestinationState(RTFDestinationState nDestinationState) = 0;
-                virtual RTFInternalState getInternalState() = 0;
-                virtual void setInternalState(RTFInternalState nInternalState) = 0;
-                virtual bool getSkipUnknown() = 0;
-                virtual void setSkipUnknown(bool bSkipUnknown) = 0;
+    virtual RTFDestinationState getDestinationState() = 0;
+    virtual void setDestinationState(RTFDestinationState nDestinationState) = 0;
+    virtual RTFInternalState getInternalState() = 0;
+    virtual void setInternalState(RTFInternalState nInternalState) = 0;
+    virtual bool getSkipUnknown() = 0;
+    virtual void setSkipUnknown(bool bSkipUnknown) = 0;
 
-                // Substream handling.
-                virtual void finishSubstream() = 0;
-                virtual bool isSubstream() const = 0;
-        };
-    } // namespace rtftok
+    // Substream handling.
+    virtual void finishSubstream() = 0;
+    virtual bool isSubstream() const = 0;
+};
+} // namespace rtftok
 } // namespace writerfilter
 
 #endif // INCLUDED_WRITERFILTER_SOURCE_RTFTOK_RTFLISTENER_HXX
