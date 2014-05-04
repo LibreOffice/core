a34a62b32de788f57a3e46f634c271f32277fd16
Stephan Bergmann <sbergman@redhat.com>
2014-05-04 12:52:54 +0200

    loplugin:saloverride
    Change-Id: I5c3b3c6212524430068e3fd8d9e9d259557bbea2

--- sc/source/ui/undo/undobase.cxx
+++ sc/source/ui/undo/undobase.cxx
@@ -161,13 +161,13 @@ class SpanBroadcaster : public sc::ColumnSpanSet::ColumnAction
 public:
     SpanBroadcaster( ScDocument& rDoc ) : mrDoc(rDoc), mnCurTab(-1), mnCurCol(-1) {}
 
-    virtual void startColumn( ScColumn* pCol )
+    virtual void startColumn( ScColumn* pCol ) SAL_OVERRIDE
     {
         mnCurTab = pCol->GetTab();
         mnCurCol = pCol->GetCol();
     }
 
-    virtual void execute( SCROW nRow1, SCROW nRow2, bool bVal )
+    virtual void execute( SCROW nRow1, SCROW nRow2, bool bVal ) SAL_OVERRIDE
     {
         if (!bVal)
             return;
