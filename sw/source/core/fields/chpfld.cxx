LibreOffice/core

commit 6319803762d051c4aa645692cde7245a1bbcf7ee

Resolves: #i120759# Bookmark value changed when opening the doc file
* sw/source/core/fields/chpfld.cxx
MS Word Binary compatibility

Patch by: Fan Zheng,<zheng.easyfan@gmail.com>
Found by: dongjun zong,<zongdj001@gmail.com>
Review by: Jian Hong Cheng,<chengjh@apache.org>

(cherry picked from commit 6b6ccbcac76eb416b4ece896714d20280761598b)

Change-Id: I42461863f7e880f7b9387ca08ce66fb3dad938a0

sw/source/core/fields/chpfld.cxx
@@ -112,6 +112,35 @@ void SwChapterField::ChangeExpansion(const SwFrm* pFrm,
 
 void SwChapterField::ChangeExpansion(const SwTxtNode &rTxtNd, sal_Bool bSrchNum)
 {
+    //i120759,this function is for both the reference chapter field and normal chapter field
+    //bSrchNum can distinguish the two types,to the latter type,the outline num rule is must...
+    sNumber = aEmptyStr;
+    sTitle = aEmptyStr;
+    sPost = aEmptyStr;
+    sPre = aEmptyStr;
+    //The reference chapter field of normal num rule will be handled in this code segment
+    if (bSrchNum && !rTxtNd.IsOutline())
+    {
+        SwNumRule* pRule(rTxtNd.GetNumRule());
+        if (rTxtNd.IsCountedInList() && pRule)
+        {
+            sNumber = rTxtNd.GetNumString(false);
+            const SwNumFmt& rNFmt = pRule->Get(static_cast<unsigned short>(rTxtNd.GetActualListLevel()));
+            sPost = rNFmt.GetSuffix();
+            sPre = rNFmt.GetPrefix();
+        }
+        else
+        {
+            sNumber = String("??", RTL_TEXTENCODING_ASCII_US);
+        }
+        sTitle = rTxtNd.GetExpandTxt();
+
+        for( xub_StrLen i = 0; i < sTitle.Len(); ++i )
+            if( ' ' > sTitle.GetChar( i ) )
+                sTitle.Erase( i--, 1 );
+    }else
+    {
+    //End
     SwDoc* pDoc = (SwDoc*)rTxtNd.GetDoc();
     const SwTxtNode *pTxtNd = rTxtNd.FindOutlineNodeOfLevel( nLevel );
     if( pTxtNd )
@@ -162,13 +191,9 @@ void SwChapterField::ChangeExpansion(const SwTxtNode &rTxtNd, sal_Bool bSrchNum)
                 sPost = rNFmt.GetSuffix();
                 sPre = rNFmt.GetPrefix();
             }
-            else
-                sPost = aEmptyStr, sPre = aEmptyStr;
         }
         else
         {
-            sPost = aEmptyStr;
-            sPre = aEmptyStr;
             sNumber = String("??", RTL_TEXTENCODING_ASCII_US);
         }
 
@@ -178,12 +203,6 @@ void SwChapterField::ChangeExpansion(const SwTxtNode &rTxtNd, sal_Bool bSrchNum)
             if( ' ' > sTitle.GetChar( i ) )
                 sTitle.Erase( i--, 1 );
     }
-    else
-    {
-        sNumber = aEmptyStr;
-        sTitle = aEmptyStr;
-        sPost = aEmptyStr;
-        sPre = aEmptyStr;
     }
 }
 

commit cc0a7699bb79efad488094e33464d4793758ee3f

fdo#75519: fix chapter cross references
It would be possible to modify the condition in
SwChapterField::ChangeExpansion() to check for rTxtNd.GetNumRule() as
well and make the bugdoc work again, but do we really want to use
"chapter reference" to refer to plain numbering?  Perhaps it would be
better to add a "numbering reference" field type; without investigating
what Word actually does here and what the actual problem is that was
supposedly fixed here (which is not possible given there's no reproducer
document) it's best for now to just revert it.

(regression from commit 6319803762d051c4aa645692cde7245a1bbcf7ee)

Change-Id: I7eb9ea46e42729c53c350ff450d49a1eb84ee8eb

sw/source/core/fields/chpfld.cxx
@@ -122,41 +122,11 @@ void SwChapterField::ChangeExpansion(const SwFrm* pFrm,
 
 void SwChapterField::ChangeExpansion(const SwTxtNode &rTxtNd, bool bSrchNum)
 {
-    //i120759,this function is for both the reference chapter field and normal chapter field
-    //bSrchNum can distinguish the two types,to the latter type,the outline num rule is must...
     sNumber = OUString();
     sTitle = OUString();
     sPost = OUString();
     sPre = OUString();
-    //The reference chapter field of normal num rule will be handled in this code segment
-    if (bSrchNum && !rTxtNd.IsOutline())
-    {
-        SwNumRule* pRule(rTxtNd.GetNumRule());
-        if (rTxtNd.IsCountedInList() && pRule)
-        {
-            sNumber = rTxtNd.GetNumString(false);
 
-            int nListLevel = rTxtNd.GetActualListLevel();
-
-            if (nListLevel < 0)
-                nListLevel = 0;
-
-            if (nListLevel >= MAXLEVEL)
-                nListLevel = MAXLEVEL - 1;
-
-            const SwNumFmt& rNFmt = pRule->Get(static_cast<unsigned short>(nListLevel));
-            sPost = rNFmt.GetSuffix();
-            sPre = rNFmt.GetPrefix();
-        }
-        else
-        {
-            sNumber = "??";
-        }
-        sTitle = removeControlChars(rTxtNd.GetExpandTxt());
-    }
-    else
-    {
-    //End
     SwDoc* pDoc = (SwDoc*)rTxtNd.GetDoc();
     const SwTxtNode *pTxtNd = rTxtNd.FindOutlineNodeOfLevel( nLevel );
     if( pTxtNd )
@@ -221,7 +191,6 @@ void SwChapterField::ChangeExpansion(const SwTxtNode &rTxtNd, bool bSrchNum)
 
         sTitle = removeControlChars(pTxtNd->GetExpandTxt(0, -1, false, false, false, false));
 
-    }
     }
 }
 