9e4f23698af8079edc114d7006ef6a7fc229c1c9
Andrzej Hunt <andrzej.hunt@collabora.com>
2014-05-07 20:14:59 +0100

    TiledRendering: outline of basic gtk tiled rendering app using liblibreoffice.
    Change-Id: I46760c0a1329b9823fd999c470b57fef66d28914

---
+++ desktop/Executable_gtktiledviewer.mk
@@ -0,0 +1,44 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_Executable_Executable,gtktiledviewer))
+
+$(eval $(call gb_Executable_set_include,gtktiledviewer,\
+    $$(INCLUDE) \
+    -I$(SRCDIR)/desktop/inc \
+))
+
+$(eval $(call gb_Executable_use_externals,gtktiledviewer,\
+    gtk \
+))
+
+$(eval $(call gb_Executable_use_static_libraries,gtktiledviewer,\
+    libreofficekit \
+))
+
+$(eval $(call gb_Executable_add_libs,gtktiledviewer,\
+    -lX11 \
+    -lXext \
+    -lSM \
+    -lICE \
+))
+
+ifeq ($(OS),LINUX)
+$(eval $(call gb_Executable_add_libs,gtktiledviewer,\
+    -lm \
+    -ldl \
+    -lpthread \
+))
+endif
+
+$(eval $(call gb_Executable_add_exception_objects,gtktiledviewer,\
+    desktop/qa/gtktiledviewer/gtktiledviewer \
+))
+
+# vim: set noet sw=4 ts=4:
