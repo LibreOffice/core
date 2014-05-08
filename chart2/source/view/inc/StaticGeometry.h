2ca8dbba58a44d4b65cbe78508d7ad0ef76cffa0
Markus Mohrhard <markus.mohrhard@googlemail.com>
2014-05-08 05:22:45 +0200

    add include guards
    Change-Id: I42ba4a97de241637b36704958fcf7b32d308a8a1

--- chart2/source/view/inc/StaticGeometry.h
+++ chart2/source/view/inc/StaticGeometry.h
@@ -1,3 +1,15 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef INCLUDED_CHART2_STATIC_GEOMETRY_H
+#define INCLUDED_CHART2_STATIC_GEOMETRY_H
+
 #define TOP_SURFACE 0
 #define MIDDLE_SURFACE 1
 #define BOTTOM_SURFACE 2
@@ -127,4 +139,6 @@ static GLfloat squareVertices[] = {
     -1.0f,  1.0f, -1.0
 };
 
+#endif
 
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
