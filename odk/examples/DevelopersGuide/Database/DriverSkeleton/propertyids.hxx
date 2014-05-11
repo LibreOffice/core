5e2918bc5a1279231fbcfc359432af3f37eb8d3c
Julien Nabet <serval2412@yahoo.fr>
2014-05-11 23:12:55 +0200

    Kill DECL_PROP1IMPL and related (odk part)
    Change-Id: Id4cd66b6082ec53facc985af48fc92de58a4c066

--- odk/examples/DevelopersGuide/Database/DriverSkeleton/propertyids.hxx
+++ odk/examples/DevelopersGuide/Database/DriverSkeleton/propertyids.hxx
@@ -83,20 +83,6 @@ namespace skeleton
 }
 
 
-
-#define DECL_PROP1IMPL(varname, type) \
-pProperties[nPos++] = ::com::sun::star::beans::Property(OPropertyMap::getPropMap().getNameByIndex(PROPERTY_ID_##varname), PROPERTY_ID_##varname, ::cppu::UnoType< type >::get(),
-
-#define DECL_PROP0(varname, type)   \
-    DECL_PROP1IMPL(varname, type) 0)
-
-#define DECL_BOOL_PROP1IMPL(varname) \
-        pProperties[nPos++] = ::com::sun::star::beans::Property(OPropertyMap::getPropMap().getNameByIndex(PROPERTY_ID_##varname), PROPERTY_ID_##varname, ::getBooleanCppuType(),
-
-#define DECL_BOOL_PROP0(varname)    \
-    DECL_BOOL_PROP1IMPL(varname) 0)
-
-
 #define PROPERTY_ID_QUERYTIMEOUT                    1
 #define PROPERTY_ID_MAXFIELDSIZE                    2
 #define PROPERTY_ID_MAXROWS                         3
