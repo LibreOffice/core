30d6bf6f25c1531e10454739b791ee572b59b251
Markus Mohrhard <markus.mohrhard@collabora.co.uk>
2014-05-12 08:51:51 +0200

    volatile does not make any sense here
    Change-Id: I895f6d04bbd5bd1829ec72b2051e640cbc153b26

--- chart2/inc/ChartView.hxx
+++ chart2/inc/ChartView.hxx
@@ -252,10 +252,10 @@ private: //member
     ::cppu::OMultiTypeInterfaceContainerHelper
                         m_aListenerContainer;
 
-    bool volatile       m_bViewDirty; //states whether the view needs to be rebuild
-    bool volatile       m_bInViewUpdate;
-    bool volatile       m_bViewUpdatePending;
-    bool volatile       m_bRefreshAddIn;
+    bool m_bViewDirty; //states whether the view needs to be rebuild
+    bool m_bInViewUpdate;
+    bool m_bViewUpdatePending;
+    bool m_bRefreshAddIn;
 
     //better performance for big data
     ::com::sun::star::awt::Size m_aPageResolution;
