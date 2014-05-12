005fae2bddf4e43cb361bbdb9fc2cfb961693ffd
Thomas Arnhold <thomas@arnhold.org>
2014-05-12 16:13:30 +0200

    upgrade to python-3.3.5
    - remove now obselete patches, which were applied upstream.
    - Hack to get MacOS to build
    
    Change-Id: Id68e78e411efc92a46ea9e180f09c390fe5acb4a
    Reviewed-on: https://gerrit.libreoffice.org/9311
    Tested-by: LibreOffice gerrit bot <gerrit@libreoffice.org>
    Reviewed-by: David Tardon <dtardon@redhat.com>
    Tested-by: David Tardon <dtardon@redhat.com>

--- external/python3/UnpackedTarball_python3.mk
+++ external/python3/UnpackedTarball_python3.mk
@@ -24,13 +24,12 @@ $(eval $(call gb_UnpackedTarball_add_patches,python3,\
 	external/python3/python-3.3.0-msvc-disable.patch.1 \
 	external/python3/python-3.3.0-msvc-x64.patch.1 \
 	external/python3/python-3.3.0-ssl.patch.1 \
-	external/python3/python-3.3.0-implicit-int.patch.1 \
 	external/python3/python-3.3.0-gcc-4.8.patch.1 \
 	external/python3/python-3.3.0-pythreadstate.patch.1 \
 	external/python3/python-3.3.0-clang.patch.1 \
-	external/python3/python-3.3.3-quoted-printable.patch.1 \
 	external/python3/python-3.3.3-py17797.patch.1 \
 	external/python3/python-3.3.3-msvc2012-winxp.patch.1 \
+	external/python3/python-3.3.5-pyexpat-symbols.patch.1 \
 ))
 
 ifneq ($(filter DRAGONFLY FREEBSD LINUX NETBSD OPENBSD SOLARIS,$(OS)),)
