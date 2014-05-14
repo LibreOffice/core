b28c0afdae884dff3a2f06adce80e3520f6ebf68
Miklos Vajna <vmiklos@collabora.co.uk>
2014-05-14 14:52:19 +0200

    DomainMapper_Impl::AddDummyParaForTableInSection: shapes can't have sections
    Trying to add (and later remove) a dummy paragraph is just not needed at
    the moment, but it'll cause a real problem for textboxes.
    
    Change-Id: I9f90b515171ce4b498de49c2381b7324002e3c20

--- writerfilter/source/dmapper/DomainMapper_Impl.cxx
+++ writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -301,6 +301,9 @@ void DomainMapper_Impl::RemoveDummyParaForTableInSection()
 }
 void DomainMapper_Impl::AddDummyParaForTableInSection()
 {
+    // Shapes can't have sections.
+    if (IsInShape())
+        return;
 
     if (!m_aTextAppendStack.empty())
     {
