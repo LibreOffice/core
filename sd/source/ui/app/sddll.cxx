205801782d51861d9cee6fa342367af92bb1d845
Maxim Monastirsky <momonasmon@gmail.com>
2014-05-16 11:45:17 +0300

    sd: Use correct SID instead of 0
    Change-Id: I181519578b2984463ad20f4b028917bcb49ba4d0

--- sd/source/ui/app/sddll.cxx
+++ sd/source/ui/app/sddll.cxx
@@ -217,7 +217,7 @@ void SdDLL::RegisterControllers()
 
     SvxStyleToolBoxControl::RegisterControl(0, pMod);
     SvxFontNameToolBoxControl::RegisterControl(0, pMod);
-    SvxColorToolBoxControl::RegisterControl(0, pMod);
+    SvxColorToolBoxControl::RegisterControl( SID_ATTR_CHAR_COLOR, pMod );
 
     SvxGrafFilterToolBoxControl::RegisterControl( SID_GRFFILTER, pMod );
     SvxGrafModeToolBoxControl::RegisterControl( SID_ATTR_GRAF_MODE, pMod );
