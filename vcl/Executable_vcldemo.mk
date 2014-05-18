2701cc1aed7264582cfe07278a74b9de7460a3a6
Chris Sherlock <chris.sherlock79@gmail.com>
2014-05-18 19:10:23 +1000

    vcl: missing demo files
    Change-Id: If269ee28c80210186699a2f2de85db8c914b28f1

---
+++ vcl/Executable_vcldemo.mk
@@ -0,0 +1,39 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_Executable_Executable,vcldemo))
+
+$(eval $(call gb_Executable_use_api,vcldemo,\
+    offapi \
+    udkapi \
+))
+
+$(eval $(call gb_Executable_use_external,vcldemo,boost_headers))
+
+$(eval $(call gb_Executable_set_include,vcldemo,\
+    $$(INCLUDE) \
+    -I$(SRCDIR)/vcl/inc \
+    -I$(SRCDIR)/solenv/inc \
+))
+
+$(eval $(call gb_Executable_use_libraries,vcldemo,\
+    tl \
+    sal \
+    vcl \
+    cppu \
+    cppuhelper \
+    comphelper \
+))
+
+$(eval $(call gb_Executable_add_exception_objects,vcldemo,\
+    vcl/workben/vcldemo \
+))
+
+# vim: set noet sw=4 ts=4:
