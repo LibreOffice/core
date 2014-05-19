36f58d4dd1ee5ebe220a5e1e7d1b28aeb15b8977
Miklos Vajna <vmiklos@collabora.co.uk>
2014-05-19 19:37:44 +0200

    sw doc model xml dump: handle SwDoc::mpFrmFmtTbl
    Change-Id: Ie15ff20524e7065323e681945bb5eaf199a2bf3a

--- sw/inc/docary.hxx
+++ sw/inc/docary.hxx
@@ -81,7 +81,7 @@ public:
     virtual SwFmt* GetFmt(size_t idx) const SAL_OVERRIDE { return (SwFmt*)operator[](idx); }
     sal_uInt16 GetPos(const SwFrmFmt* pFmt) const;
     bool Contains(const SwFrmFmt* pFmt) const;
-    void dumpAsXml(xmlTextWriterPtr w);
+    void dumpAsXml(xmlTextWriterPtr w, const char* pName);
     /// free's any remaining child objects
     virtual ~SwFrmFmts();
 };
