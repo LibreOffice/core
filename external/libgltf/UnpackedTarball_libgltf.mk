582958890558d0d04465466727a86268ef426a13
Jan Holesovsky <kendy@collabora.com>
2014-05-20 22:00:19 +0200

    libgltf: Updated version.
    Change-Id: Ia394ff00421f495757cc2ce55e7b91bfebe715eb

--- external/libgltf/UnpackedTarball_libgltf.mk
+++ external/libgltf/UnpackedTarball_libgltf.mk
@@ -18,7 +18,7 @@ $(eval $(call gb_UnpackedTarball_set_patchlevel,libgltf,1))
 $(eval $(call gb_UnpackedTarball_add_patches,libgltf,\
 	external/libgltf/patches/include_path_glew.patch \
 	external/libgltf/patches/include_path_freetype.patch \
-	external/libgltf/patches/disable_fps.patch \
+	external/libgltf/patches/disable_fps_and_timer.patch \
 ))
 
 # vim: set noet sw=4 ts=4:
