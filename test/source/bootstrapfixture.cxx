3b952de5d7e467aa2109fd990a9dff22c919915a
Tor Lillqvist <tml@collabora.com>
2014-05-22 11:12:44 +0300

    Avoid including <config_features.h> in <vcl/svapp.hxx>
    Change-Id: I711621c89e8f75d4450867e303ccdc8017bcd9d7

--- test/source/bootstrapfixture.cxx
+++ test/source/bootstrapfixture.cxx
@@ -6,6 +6,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
+
+#include <config_features.h>
+
 #include <test/bootstrapfixture.hxx>
 #include <tools/errinf.hxx>
 #include <rtl/strbuf.hxx>
