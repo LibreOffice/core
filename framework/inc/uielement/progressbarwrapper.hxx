3166336e535c15578bc91a00b0a96d72d676c8a4
Caolán McNamara <caolanm@redhat.com>
2014-05-22 20:49:43 +0100

    coverity#1215386 Uncaught exception
    Change-Id: I2eb374a9209cf4bbf9e2b533ec2522bc1efe6468

--- framework/inc/uielement/progressbarwrapper.hxx
+++ framework/inc/uielement/progressbarwrapper.hxx
@@ -47,7 +47,7 @@ class ProgressBarWrapper : public UIElementWrapperBase
         ::com::sun::star::uno::Reference< ::com::sun::star::awt::XWindow > getStatusBar() const;
 
         // wrapped methods of ::com::sun::star::task::XStatusIndicator
-        void start( const OUString& Text, ::sal_Int32 Range ) throw (::com::sun::star::uno::RuntimeException);
+        void start( const OUString& Text, ::sal_Int32 Range ) throw (css::uno::RuntimeException, std::exception);
         void end() throw (::com::sun::star::uno::RuntimeException);
         void setText( const OUString& Text ) throw (::com::sun::star::uno::RuntimeException);
         void setValue( ::sal_Int32 Value ) throw (::com::sun::star::uno::RuntimeException);
