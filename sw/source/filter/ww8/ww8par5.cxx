d4a19c4098f4556f05666ed92a85c6138b4f769a
Luboš Luňák <l.lunak@collabora.com>
2014-05-23 11:35:10 +0200

    MSWord uses \xb for linebreaks in DB fields (bnc#878854)
    Change-Id: I9b1e4f8dc44d3f6f41e395f6e5f3c1fdeff52441

--- sw/source/filter/ww8/ww8par5.cxx
+++ sw/source/filter/ww8/ww8par5.cxx
@@ -2413,6 +2413,8 @@ eF_ResT SwWW8ImplReader::Read_F_DBField( WW8FieldDesc* pF, OUString& rStr )
     pSBase->WW8ReadString( *pStrm, aResult, pPlcxMan->GetCpOfs()+
                            pF->nSRes, pF->nLRes, eTextCharSet );
 
+    aResult = aResult.replace( '\xb', '\n' );
+
     aFld.InitContent(aResult);
 
     rDoc.InsertPoolItem(*pPaM, SwFmtFld( aFld ), 0);
