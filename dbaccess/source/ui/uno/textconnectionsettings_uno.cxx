509c5b149d85dd54e6c0d807d538b90741a461fc
Caolán McNamara <caolanm@redhat.com>
2014-05-24 12:00:56 +0100

    coverity#1000846 Uncaught exception
    Change-Id: I4e92ad97e7b3567c028d63ac0936fa9cdef12e24

--- dbaccess/source/ui/uno/textconnectionsettings_uno.cxx
+++ dbaccess/source/ui/uno/textconnectionsettings_uno.cxx
@@ -95,7 +95,7 @@ namespace dbaui
             { ODatabaseAdministrationDialog::removePropertyChangeListener(p1, p2); }
         virtual void SAL_CALL addVetoableChangeListener(const OUString& p1, const css::uno::Reference<css::beans::XVetoableChangeListener>& p2) throw (css::uno::RuntimeException, std::exception) SAL_OVERRIDE
             { ODatabaseAdministrationDialog::addVetoableChangeListener(p1, p2); }
-        virtual void SAL_CALL removeVetoableChangeListener(const OUString& p1, const css::uno::Reference<css::beans::XVetoableChangeListener>& p2) throw (css::uno::RuntimeException, std::exception) SAL_OVERRIDE
+        virtual void SAL_CALL removeVetoableChangeListener(const OUString& p1, const css::uno::Reference<css::beans::XVetoableChangeListener>& p2) throw (css::beans::UnknownPropertyException, css::uno::RuntimeException, std::exception) SAL_OVERRIDE
             { ODatabaseAdministrationDialog::removeVetoableChangeListener(p1, p2); }
         virtual void SAL_CALL setTitle(const OUString& p1) throw (css::uno::RuntimeException, std::exception) SAL_OVERRIDE
             { ODatabaseAdministrationDialog::setTitle(p1); }
