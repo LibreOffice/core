d2a80640c96394228b098aa54a7f117573c85caa
David Tardon <dtardon@redhat.com>
2014-05-24 08:39:54 +0200

    bundle librevenge
    Change-Id: Ic36c1670866545db2cf2f29867de7e5b0ad2d57d
    (cherry picked from commit b421ce79e39867917ba53be7bf69c8a4e7f8a8ac)

---
+++ external/librevenge/ExternalProject_librevenge.mk
@@ -0,0 +1,44 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_ExternalProject_ExternalProject,librevenge))
+
+$(eval $(call gb_ExternalProject_use_autoconf,librevenge,build))
+
+$(eval $(call gb_ExternalProject_register_targets,librevenge,\
+	build \
+))
+
+# TODO: test for zlib is unconditional in configure by mistake. Remove on the next release.
+$(eval $(call gb_ExternalProject_use_externals,librevenge,\
+	boost_headers \
+	zlib \
+))
+
+$(call gb_ExternalProject_get_state_target,librevenge,build) :
+	$(call gb_ExternalProject_run,build,\
+		export PKG_CONFIG="" \
+		&& ./configure \
+			--with-pic \
+			--enable-shared \
+			--disable-static \
+			--disable-debug \
+			--disable-werror \
+			--disable-weffc \
+			--disable-streams \
+			--disable-generators \
+			--without-docs \
+			$(if $(VERBOSE)$(verbose),--enable-silent-rules) \
+			CXXFLAGS="$(if $(SYSTEM_BOOST),$(BOOST_CPPFLAGS),-I$(call gb_UnpackedTarball_get_dir,boost) -I$(BUILDDIR)/config_$(gb_Side))" \
+			$(if $(CROSS_COMPILING),--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM)) \
+			$(if $(filter MACOSX,$(OS)),--prefix=/@.__________________________________________________OOO) \
+		&& $(MAKE) \
+	)
+
+# vim: set noet sw=4 ts=4:
