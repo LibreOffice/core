f50aa62ea93b00228daa63d9830ee9712ab7839e
David Tardon <dtardon@redhat.com>
2014-05-24 08:39:54 +0200

    bundle librevenge
    Change-Id: Ic36c1670866545db2cf2f29867de7e5b0ad2d57d

---
+++ external/librevenge/Library_revenge.mk
@@ -0,0 +1,48 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_Library_Library,revenge))
+
+$(eval $(call gb_Library_use_unpacked,revenge,librevenge))
+
+$(eval $(call gb_Library_use_externals,revenge,\
+    boost_headers \
+))
+
+$(eval $(call gb_Library_set_warnings_not_errors,revenge))
+
+$(eval $(call gb_Library_set_include,revenge,\
+    -I$(call gb_UnpackedTarball_get_dir,librevenge)/inc \
+    $$(INCLUDE) \
+))
+
+# -DLIBREVENGE_STREAM_BUILD is present so that linker does not complain
+#  about unresolved external symbol RVNGStream::~RVNGStream
+$(eval $(call gb_Library_add_defs,revenge,\
+	-DBOOST_ALL_NO_LIB \
+	-DDLL_EXPORT \
+	-DLIBREVENGE_BUILD \
+	-DLIBREVENGE_STREAM_BUILD \
+	-DNDEBUG \
+))
+
+$(eval $(call gb_Library_set_generated_cxx_suffix,revenge,cpp))
+
+$(eval $(call gb_Library_add_generated_exception_objects,revenge,\
+    UnpackedTarball/librevenge/src/lib/RVNGBinaryData \
+    UnpackedTarball/librevenge/src/lib/RVNGMemoryStream \
+    UnpackedTarball/librevenge/src/lib/RVNGProperty \
+    UnpackedTarball/librevenge/src/lib/RVNGPropertyList \
+    UnpackedTarball/librevenge/src/lib/RVNGPropertyListVector \
+    UnpackedTarball/librevenge/src/lib/RVNGString \
+    UnpackedTarball/librevenge/src/lib/RVNGStringVector \
+    UnpackedTarball/librevenge/src/lib/RVNGSVGDrawingGenerator \
+))
+
+# vim: set noet sw=4 ts=4:
---
+++ external/librevenge/Module_librevenge.mk
@@ -0,0 +1,31 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_Module_Module,librevenge))
+
+$(eval $(call gb_Module_add_targets,librevenge,\
+	UnpackedTarball_librevenge \
+))
+
+ifeq ($(COM),MSC)
+
+$(eval $(call gb_Module_add_targets,librevenge,\
+	Library_revenge \
+))
+
+else
+
+$(eval $(call gb_Module_add_targets,librevenge,\
+	ExternalPackage_librevenge \
+	ExternalProject_librevenge \
+))
+
+endif
+
+# vim: set noet sw=4 ts=4:
