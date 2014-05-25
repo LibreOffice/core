a6bd14511519966c1760fcc41fd1498970f098da
Zolnai Tamás <tamas.zolnai@collabora.com>
2014-05-25 15:36:21 +0200

    avmedia: call resize before player window is created
    Since b5529c8c457f442bfc43305becc0c02d6a09ee80
    MediaChildWindow is depend on the media URL so
    it is constructed later and so one resizing is
    missed before creating the player window which
    causes to display nothing in the media player
    (Tools -> Media Player)
    
    Change-Id: I548e6bb73193fccd1735e8343e9c06428ee686bc

--- avmedia/source/viewer/mediawindow_impl.cxx
+++ avmedia/source/viewer/mediawindow_impl.cxx
@@ -514,6 +514,7 @@ void MediaWindowImpl::onURLChanged()
 
     if( mxPlayer.is() )
     {
+        Resize();
         uno::Sequence< uno::Any >              aArgs( 3 );
         uno::Reference< media::XPlayerWindow > xPlayerWindow;
         const Point                            aPoint;
