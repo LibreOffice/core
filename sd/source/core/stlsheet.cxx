7a892c82c11d87ca4b3810e8a6f9767d3d132c8d
Caolán McNamara <caolanm@redhat.com>
2014-05-26 10:48:05 +0100

    coverity#983778 Uncaught exception
    Change-Id: I27ebb40f21a15c7e1696217a53d8c07e1d1161ad

--- sd/source/core/stlsheet.cxx
+++ sd/source/core/stlsheet.cxx
@@ -1359,7 +1359,7 @@ Any SAL_CALL SdStyleSheet::getPropertyDefault( const OUString& aPropertyName ) t
 
 
 /** this is used because our property map is not sorted yet */
-const SfxItemPropertySimpleEntry* SdStyleSheet::getPropertyMapEntry( const OUString& rPropertyName ) const throw()
+const SfxItemPropertySimpleEntry* SdStyleSheet::getPropertyMapEntry( const OUString& rPropertyName ) const throw (css::uno::RuntimeException)
 {
     return GetStylePropertySet().getPropertyMapEntry(rPropertyName);
 }
