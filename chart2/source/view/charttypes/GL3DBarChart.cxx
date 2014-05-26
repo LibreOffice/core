b8dc914cdc0eab0735e4fec7613221fef2fc0fdb
Markus Mohrhard <markus.mohrhard@collabora.co.uk>
2014-05-26 03:08:08 +0200

    adapt bar size to demo
    Change-Id: Id746ce00f9e91397a3edf0b39bf7dbb08ea59885

--- chart2/source/view/charttypes/GL3DBarChart.cxx
+++ chart2/source/view/charttypes/GL3DBarChart.cxx
@@ -97,7 +97,7 @@ void GL3DBarChart::create3DShapes(const boost::ptr_vector<VDataSeries>& rDataSer
     // guarantee they are positioned correctly.  In fact, they are guaranteed
     // to be positioned incorrectly.
 
-    const float nBarSizeX = 10.0f;
+    const float nBarSizeX = 5.0f;
     const float nBarSizeY = 30.0f;
     const float nBarDistanceX = 5.0f;
     const float nBarDistanceY = 5.0;
