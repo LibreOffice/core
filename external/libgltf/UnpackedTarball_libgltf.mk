37dd6ced1e1763b2da2d08f5dd0df1bf7a6458a9
Zolnai Tamás <tamas.zolnai@collabora.com>
2014-05-26 23:40:51 +0200

    patching libgltf: stop a huge memory leak
    It is ambiguos where we should free file buffers because they
    are allocated by the caller code, but since gltfHandle is
    deallocated inside libgltf and libgltf has a gltf_renderer_release
    method for deallocation it seems better to free them by libgltf.
    
    
    Conflicts:
    	external/libgltf/UnpackedTarball_libgltf.mk
    
    Change-Id: If3d9abed0514128c13cde8c1967ea80c4d27bf59

--- external/libgltf/UnpackedTarball_libgltf.mk
+++ external/libgltf/UnpackedTarball_libgltf.mk
@@ -19,6 +19,7 @@ $(eval $(call gb_UnpackedTarball_add_patches,libgltf,\
 	external/libgltf/patches/include_path_glew.patch \
 	external/libgltf/patches/include_path_freetype.patch \
 	external/libgltf/patches/openmp-disable.patch \
+	external/libgltf/patches/free_file_buffers.patch \
 ))
 
 # vim: set noet sw=4 ts=4:
