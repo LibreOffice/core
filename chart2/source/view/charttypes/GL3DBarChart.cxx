522cbe4eb6fab7e752775f98b363b1d6e4c116ee
Markus Mohrhard <markus.mohrhard@collabora.co.uk>
2014-05-27 16:32:25 +0200

    prevent that early return prevents further user interaction
    Change-Id: I24f814a402d2b8af3ddf888719b78d3158c2a38f

--- chart2/source/view/charttypes/GL3DBarChart.cxx
+++ chart2/source/view/charttypes/GL3DBarChart.cxx
@@ -293,7 +293,6 @@ void GL3DBarChart::clickedAt(const Point& /*rPos*/, sal_uInt16 nButtons)
     if(nButtons != MOUSE_LEFT)
         return;
 
-    mbBlockUserInput = true;
     sal_uInt32 nId = 5;
     /*
     {
@@ -308,6 +307,8 @@ void GL3DBarChart::clickedAt(const Point& /*rPos*/, sal_uInt16 nButtons)
 
     if(itr == maBarMap.end())
         return;
+
+    mbBlockUserInput = true;
 
     const BarInformation& rBarInfo = itr->second;
     mnStepsTotal = 100;
