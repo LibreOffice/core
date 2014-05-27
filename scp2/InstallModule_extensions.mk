35a81048ca2c11d0a1092d8178e070127fd5711b
Michael Stahl <mstahl@redhat.com>
2014-05-27 23:30:11 +0200

    scp2: looks like those BSH_JAR RHINO_JAR macros are unused
    Change-Id: I6621529921120170bc1572dac727e9dc3b9438d5

--- scp2/InstallModule_extensions.mk
+++ scp2/InstallModule_extensions.mk
@@ -12,15 +12,6 @@ $(eval $(call gb_InstallModule_InstallModule,scp2/extensions))
 $(eval $(call gb_InstallModule_use_auto_install_libs,scp2/extensions,extensions_bsh))
 $(eval $(call gb_InstallModule_use_auto_install_libs,scp2/extensions,extensions_rhino))
 
-$(eval $(call gb_InstallModule_add_defs,scp2/extensions,\
-	$(if $(SYSTEM_BSH),\
-		-DBSH_JAR=\""$(call gb_Helper_make_url,$(BSH_JAR))"\" \
-	) \
-	$(if $(SYSTEM_RHINO),\
-		-DRHINO_JAR=\""$(call gb_Helper_make_url,$(RHINO_JAR))"\" \
-	) \
-))
-
 $(eval $(call gb_InstallModule_add_templates,scp2/extensions,\
     scp2/source/templates/module_langpack_extensions_templates \
 ))
