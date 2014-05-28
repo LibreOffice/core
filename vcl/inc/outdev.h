LibreOffice/core

commit a6b00d16eb27a5e7e31c721671001a909ecef960

Related bnc#822625: Cache FontEntry with the original FontSelectPattern.
Otherwise we never hit cache directly, only after expensive call to
ImplFindByFont.

Change-Id: If15b368feeba94c8fff8ee7cbe049fc4a2069768

vcl/inc/outdev.h
@@ -149,7 +149,7 @@ public:
 
     ImplFontEntry*      GetFontEntry( PhysicalFontCollection*,
                              const Font&, const Size& rPixelSize, float fExactHeight);
-    ImplFontEntry*      GetFontEntry( PhysicalFontCollection*, FontSelectPattern& );
+    ImplFontEntry*      GetFontEntry( PhysicalFontCollection*, const FontSelectPattern& );
     ImplFontEntry*      GetGlyphFallbackFont( PhysicalFontCollection*, FontSelectPattern&,
                             int nFallbackLevel, OUString& rMissingCodes );
     void                Release( ImplFontEntry* );

vcl/source/outdev/font.cxx
@@ -1227,22 +1227,23 @@ ImplFontEntry* ImplFontCache::GetFontEntry( PhysicalFontCollection* pFontList,
 }
 
 ImplFontEntry* ImplFontCache::GetFontEntry( PhysicalFontCollection* pFontList,
-    FontSelectPattern& aFontSelData )
+    const FontSelectPattern& rFontSelData )
 {
     // check if a directly matching logical font instance is already cached,
     // the most recently used font usually has a hit rate of >50%
     ImplFontEntry *pEntry = NULL;
     PhysicalFontFamily* pFontFamily = NULL;
     IFSD_Equal aIFSD_Equal;
-    if( mpFirstEntry && aIFSD_Equal( aFontSelData, mpFirstEntry->maFontSelData ) )
+    if( mpFirstEntry && aIFSD_Equal( rFontSelData, mpFirstEntry->maFontSelData ) )
         pEntry = mpFirstEntry;
     else
     {
-        FontInstanceList::iterator it = maFontInstanceList.find( aFontSelData );
+        FontInstanceList::iterator it = maFontInstanceList.find( rFontSelData );
         if( it != maFontInstanceList.end() )
             pEntry = (*it).second;
     }
 
+    FontSelectPattern aFontSelData(rFontSelData);
     if( !pEntry ) // no direct cache hit
     {
         // find the best matching logical font family and update font selector accordingly
@@ -1315,8 +1316,9 @@ ImplFontEntry* ImplFontCache::GetFontEntry( PhysicalFontCollection* pFontList,
         }
 #endif
 
-        // add the new entry to the cache
-        maFontInstanceList[ aFontSelData ] = pEntry;
+        // Add the new entry to the cache with the original FontSelectPattern,
+        // so that we can find it next time as a direct cache hit.
+        maFontInstanceList[ rFontSelData ] = pEntry;
     }
 
     mpFirstEntry = pEntry;

commit 16a62079018aea0e72636bdb00576487b4e830b9

Oops, this reference can't be const.
It's used in ImplFontCache::GetGlyphFallbackFont.

This is fix for commit a6b00d16eb27a5e7e31c721671001a909ecef960,
which caused huge performance regression for document in fdo#59882,
fortunately immediately detected by loperf.

Change-Id: I475742b5249f106d34c4f6c43b1e39e9bb7b897a

vcl/inc/outdev.h
@@ -149,7 +149,7 @@ public:
 
     ImplFontEntry*      GetFontEntry( PhysicalFontCollection*,
                              const Font&, const Size& rPixelSize, float fExactHeight);
-    ImplFontEntry*      GetFontEntry( PhysicalFontCollection*, const FontSelectPattern& );
+    ImplFontEntry*      GetFontEntry( PhysicalFontCollection*, FontSelectPattern& );
     ImplFontEntry*      GetGlyphFallbackFont( PhysicalFontCollection*, FontSelectPattern&,
                             int nFallbackLevel, OUString& rMissingCodes );
     void                Release( ImplFontEntry* );

vcl/source/outdev/font.cxx
@@ -1227,23 +1227,23 @@ ImplFontEntry* ImplFontCache::GetFontEntry( PhysicalFontCollection* pFontList,
 }
 
 ImplFontEntry* ImplFontCache::GetFontEntry( PhysicalFontCollection* pFontList,
-    const FontSelectPattern& rFontSelData )
+    FontSelectPattern& aFontSelData )
 {
+    const FontSelectPattern aFontSelDataOrig(aFontSelData);
     // check if a directly matching logical font instance is already cached,
     // the most recently used font usually has a hit rate of >50%
     ImplFontEntry *pEntry = NULL;
     PhysicalFontFamily* pFontFamily = NULL;
     IFSD_Equal aIFSD_Equal;
-    if( mpFirstEntry && aIFSD_Equal( rFontSelData, mpFirstEntry->maFontSelData ) )
+    if( mpFirstEntry && aIFSD_Equal( aFontSelData, mpFirstEntry->maFontSelData ) )
         pEntry = mpFirstEntry;
     else
     {
-        FontInstanceList::iterator it = maFontInstanceList.find( rFontSelData );
+        FontInstanceList::iterator it = maFontInstanceList.find( aFontSelData );
         if( it != maFontInstanceList.end() )
             pEntry = (*it).second;
     }
 
-    FontSelectPattern aFontSelData(rFontSelData);
     if( !pEntry ) // no direct cache hit
     {
         // find the best matching logical font family and update font selector accordingly
@@ -1318,7 +1318,7 @@ ImplFontEntry* ImplFontCache::GetFontEntry( PhysicalFontCollection* pFontList,
 
         // Add the new entry to the cache with the original FontSelectPattern,
         // so that we can find it next time as a direct cache hit.
-        maFontInstanceList[ rFontSelData ] = pEntry;
+        maFontInstanceList[ aFontSelDataOrig ] = pEntry;
     }
 
     mpFirstEntry = pEntry;