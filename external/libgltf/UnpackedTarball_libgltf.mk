32f60d78b15b5f2c677e8687720665f6814fef62
Zolnai Tamás <tamas.zolnai@collabora.com>
2014-05-28 13:07:38 +0200

    libgltf: solve this memory leak problem on a better way 2
    Handle those cases when file loading failes.
    Release *.json file on a different way since it is allocated
    by libgltf (inside the glTFHandle).
    
    (cherry picked from commit fafc1e29c1f060c1a44361a0445300f9786ad6f4)
    
    Change-Id: Idf6c6971a8ac1b342d89dc4f61a62624183e01d0

--- external/libgltf/UnpackedTarball_libgltf.mk
+++ external/libgltf/UnpackedTarball_libgltf.mk
@@ -19,7 +19,6 @@ $(eval $(call gb_UnpackedTarball_add_patches,libgltf,\
 	external/libgltf/patches/include_path_glew.patch \
 	external/libgltf/patches/include_path_freetype.patch \
 	external/libgltf/patches/openmp-disable.patch \
-	external/libgltf/patches/free_file_buffers.patch \
 	external/libgltf/patches/rgba_textures.patch \
 ))
 
