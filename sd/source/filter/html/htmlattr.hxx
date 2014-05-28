e5a8ecd8f74b466ad87eff9ca194c5fc900a3fb7
Palenik Mihály <palenik.mihaly@gmail.com>
2014-05-28 19:26:56 +0200

    Convert DLG_PUBLISHING to .ui
    Change-Id: I0e15b6f7610c7b262a037d6a9ea482112a5bb8b4

--- sd/source/filter/html/htmlattr.hxx
+++ sd/source/filter/html/htmlattr.hxx
@@ -31,7 +31,7 @@ protected:
     Color   m_aVLinkColor, m_aALinkColor;
 
 public:
-    SdHtmlAttrPreview( Window* pParent, const ResId& rResId );
+    SdHtmlAttrPreview( Window* pParent, WinBits nStyle );
     virtual ~SdHtmlAttrPreview();
 
     virtual void    Paint( const Rectangle& rRect ) SAL_OVERRIDE;
