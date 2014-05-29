93f09843c0922341c52fbe78eb756c2eb2469608
David Tardon <dtardon@redhat.com>
2014-05-29 13:33:04 +0200

    re-enable libe-book integration test
    Change-Id: I217183d55607f16c3b996b27b131953962afee96
    (cherry picked from commit 5dfb6e74c68073eb1742c53393c683d5841de255)

--- writerperfect/qa/unit/WpftWriterFilterTest.cxx
+++ writerperfect/qa/unit/WpftWriterFilterTest.cxx
@@ -32,7 +32,7 @@ WpftWriterFilterTest::WpftWriterFilterTest()
 void WpftWriterFilterTest::test()
 {
     doTest("com.sun.star.comp.Writer.AbiWordImportFilter", "/writerperfect/qa/unit/data/writer/libabw/");
-    // doTest("org.libreoffice.comp.Writer.EBookImportFilter", "/writerperfect/qa/unit/data/writer/libe-book/");
+    doTest("org.libreoffice.comp.Writer.EBookImportFilter", "/writerperfect/qa/unit/data/writer/libe-book/");
     doTest("com.sun.star.comp.Writer.MSWorksImportFilter", "/writerperfect/qa/unit/data/writer/libwps/");
     // doTest("com.sun.star.comp.Writer.MWAWImportFilter", "/writerperfect/qa/unit/data/writer/libmwaw/");
     doTest("com.sun.star.comp.Writer.WordPerfectImportFilter", "/writerperfect/qa/unit/data/writer/libwpd/");
