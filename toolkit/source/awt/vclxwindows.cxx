2f59d362f9d6111270fcf58957496359ff352502
Lionel Elie Mamane <lionel@mamane.lu>
2014-05-29 08:09:57 +0200

    fdo#75364 flat style and 3d style were swapped
    Change-Id: I929f8f63e8017727595be30f4e56f3aabeec2b1e
    Reviewed-on: https://gerrit.libreoffice.org/9552
    Tested-by: David Tardon <dtardon@redhat.com>
    Reviewed-by: David Tardon <dtardon@redhat.com>

--- toolkit/source/awt/vclxwindows.cxx
+++ toolkit/source/awt/vclxwindows.cxx
@@ -178,11 +178,11 @@ namespace toolkit
         switch ( nStyle )
         {
         case FLAT:
-            aStyleSettings.SetOptions( aStyleSettings.GetOptions() & ~STYLE_OPTION_MONO );
+            aStyleSettings.SetOptions( aStyleSettings.GetOptions() | STYLE_OPTION_MONO );
             break;
         case LOOK3D:
         default:
-            aStyleSettings.SetOptions( aStyleSettings.GetOptions() | STYLE_OPTION_MONO );
+            aStyleSettings.SetOptions( aStyleSettings.GetOptions() & ~STYLE_OPTION_MONO );
         }
         aSettings.SetStyleSettings( aStyleSettings );
         _pWindow->SetSettings( aSettings );
