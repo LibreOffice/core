ee0714cb95c14b889074d4baa584eba8e285c8e3
Jens Carl <j.carl43@gmx.de>
2014-05-30 20:30:35 +0000

    Fixed lower case typo in header guard definition
    Change-Id: Ib4f09892850bfcb72ddec0b0a924746495c5551f
    Reviewed-on: https://gerrit.libreoffice.org/9579
    Reviewed-by: Thomas Arnhold <thomas@arnhold.org>
    Tested-by: Thomas Arnhold <thomas@arnhold.org>

--- forms/source/component/FormsCollection.hxx
+++ forms/source/component/FormsCollection.hxx
@@ -17,8 +17,8 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_FORMS_SOURCE_COMPONENT_fORMSCOLLECTION_HXX
-#define INCLUDED_FORMS_SOURCE_COMPONENT_fORMSCOLLECTION_HXX
+#ifndef INCLUDED_FORMS_SOURCE_COMPONENT_FORMSCOLLECTION_HXX
+#define INCLUDED_FORMS_SOURCE_COMPONENT_FORMSCOLLECTION_HXX
 
 #include "InterfaceContainer.hxx"
 #include <cppuhelper/component.hxx>
@@ -187,6 +187,6 @@ public:
 }   // namespace frm
 
 
-#endif // INCLUDED_FORMS_SOURCE_COMPONENT_fORMSCOLLECTION_HXX
+#endif // INCLUDED_FORMS_SOURCE_COMPONENT_FORMSCOLLECTION_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
