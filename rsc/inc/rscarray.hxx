7610a80ad1a47f2401b4d6aca9d4f77e5be7c630
Jens Carl <j.carl43@gmx.de>
2014-05-30 20:32:57 +0000

    fdo#68849: Add header guards to all include files
    Added header guards to files in directories l10ntools/, lotuswordpro/, and rsc/
    
    Change-Id: I9c034d4bb5c92d78378bda4658d43a8b603d5281
    Reviewed-on: https://gerrit.libreoffice.org/9581
    Reviewed-by: Thomas Arnhold <thomas@arnhold.org>
    Tested-by: Thomas Arnhold <thomas@arnhold.org>

--- rsc/inc/rscarray.hxx
+++ rsc/inc/rscarray.hxx
@@ -16,6 +16,7 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
+
 #ifndef INCLUDED_RSC_INC_RSCARRAY_HXX
 #define INCLUDED_RSC_INC_RSCARRAY_HXX
 
@@ -119,6 +120,6 @@ public:
     virtual RSCCLASS_TYPE   GetClassType() const SAL_OVERRIDE;
 };
 
-#endif //_RSCARRAY
+#endif // INCLUDED_RSC_INC_RSCARRAY_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
