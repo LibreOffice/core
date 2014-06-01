90f3ebd4d3aa009a5d7fa77d213c1042a346f1c8
Tobias Lippert <drtl@fastmail.fm>
2014-06-01 14:13:28 +0200

    Extract TOX text generation to its own source file
    The separate source file will make it easier to unit test the functionality
    
    Change-Id: Ib0369cc4875043829f55d30a549997560a38cc35
    Also: move some constants to tox.hxx to have them available in the new source file

---
+++ sw/inc/ToxTextGenerator.hxx
@@ -0,0 +1,46 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef SW_TOXTEXTGENERATOR_HXX_
+#define SW_TOXTEXTGENERATOR_HXX_
+
+#include "sal/types.h"
+#include <vector>
+
+class SwDoc;
+class SwForm;
+class SwPageDesc;
+class SwTOXSortTabBase;
+
+namespace sw {
+
+class SW_DLLPUBLIC ToxTextGenerator
+{
+public:
+    ToxTextGenerator(const SwForm& toxForm)
+    :mToxForm(toxForm)
+    {;}
+    /** Generate the text for an entry of a table of X (X is, e.g., content).
+     *
+     * This method will process the entries in @p entries, starting at @p indexOfEntryToProcess and
+     * process @p numberOfEntriesToProcess entries.
+     */
+    void
+    GenerateText(SwDoc *doc, const std::vector<SwTOXSortTabBase*>& entries,
+                      sal_uInt16 indexOfEntryToProcess, sal_uInt16 numberOfEntriesToProcess,
+                      sal_uInt32 _nTOXSectNdIdx, const SwPageDesc* _pDefaultPageDesc);
+
+
+private:
+    const SwForm& mToxForm;
+};
+
+}
+
+#endif /* SW_TOXTEXTGENERATOR_HXX_ */
