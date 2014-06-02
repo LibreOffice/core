0b8fe9834b398db7550f30718b3cb63860c6165d
matteocam <matteo.campanelli@gmail.com>
2014-06-02 10:43:34 -0400

    Minor changes.
    Change-Id: I3b8c51690eb14601435653e9a2255a5c6ed4fd7e

--- editeng/source/editeng/editattr.cxx
+++ editeng/source/editeng/editattr.cxx
@@ -134,6 +134,8 @@ void EditCharAttribUnderline::SetFont( SvxFont& rFont, OutputDevice* pOutDev )
 
     /* FIXME(matteocam) */
     rFont.SetFillColor(aColor); // XXX: alone it works but it set it white (vcl causing troubes?)
+    //if (pOutDev)
+    //    pOutDev->SetTextFillColor(aColor); // this doesn't work either
     // end FIXME
 
     if ( pOutDev )
