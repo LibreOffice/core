e39081a08941b39c98decc8a5709cbdb170b935b
Michael Stahl <mstahl@redhat.com>
2014-06-02 23:30:03 +0200

    openssl: stop (ab)using fix_end_of_line to break symlinks
    Win32 make has problems because the command line gets too long.
    
    Change-Id: I157b7b2b61353b158b1a3f412331e54aafec206c

--- external/openssl/UnpackedTarball_openssl.mk
+++ external/openssl/UnpackedTarball_openssl.mk
@@ -11,60 +11,15 @@ $(eval $(call gb_UnpackedTarball_UnpackedTarball,openssl))
 
 $(eval $(call gb_UnpackedTarball_set_tarball,openssl,$(OPENSSL_TARBALL)))
 
-# hack to fix symlinks with MSVC
-$(eval $(call gb_UnpackedTarball_fix_end_of_line,openssl,\
-	include/openssl/asn1.h \
-	include/openssl/bio.h \
-	include/openssl/bn.h \
-	include/openssl/buffer.h \
-	include/openssl/comp.h \
-	include/openssl/conf.h \
-	include/openssl/crypto.h \
-	include/openssl/des.h \
-	include/openssl/des_old.h \
-	include/openssl/dh.h \
-	include/openssl/dsa.h \
-	include/openssl/dtls1.h \
-	include/openssl/e_os2.h \
-	include/openssl/ec.h \
-	include/openssl/ecdh.h \
-	include/openssl/ecdsa.h \
-	include/openssl/engine.h \
-	include/openssl/err.h \
-	include/openssl/evp.h \
-	include/openssl/hmac.h \
-	include/openssl/kssl.h \
-	include/openssl/lhash.h \
-	include/openssl/md4.h \
-	include/openssl/md5.h \
-	include/openssl/obj_mac.h \
-	include/openssl/objects.h \
-	include/openssl/opensslconf.h \
-	include/openssl/opensslv.h \
-	include/openssl/ossl_typ.h \
-	include/openssl/pem.h \
-	include/openssl/pem2.h \
-	include/openssl/pkcs12.h \
-	include/openssl/pkcs7.h \
-	include/openssl/pqueue.h \
-	include/openssl/rand.h \
-	include/openssl/ripemd.h \
-	include/openssl/rsa.h \
-	include/openssl/safestack.h \
-	include/openssl/sha.h \
-	include/openssl/ssl.h \
-	include/openssl/ssl2.h \
-	include/openssl/ssl23.h \
-	include/openssl/ssl3.h \
-	include/openssl/stack.h \
-	include/openssl/symhacks.h \
-	include/openssl/tls1.h \
-	include/openssl/ui.h \
-	include/openssl/ui_compat.h \
-	include/openssl/x509.h \
-	include/openssl/x509_vfy.h \
-	include/openssl/x509v3.h \
-))
+# break symlinks after extracting tarball
+# note: escape \; because LO patched make 3.82 cuts off the command otherwise
+ifeq ($(OS_FOR_BUILD),WNT)
+$(eval $(call gb_UnpackedTarball_set_pre_action,openssl,\
+	cd include/openssl && \
+	for header in `find . -type l` \; do \
+		cp --remove-destination `readlink $$$$header` $$$$header \; \
+	done && cd -))
+endif
 
 $(eval $(call gb_UnpackedTarball_add_patches,openssl,\
 	external/openssl/CVE-2013-6449.patch \
