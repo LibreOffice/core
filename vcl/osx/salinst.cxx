39ffccd26151d255168b072a2d439df48d172011
Tor Lillqvist <tml@collabora.com>
2014-06-03 09:09:20 +0300

    WaE: case value not in enumerated type 'NSEventSubtype'
    Change-Id: Iaf5de07b7f0da7294681b9cdc152575a756a73c7

--- vcl/osx/salinst.cxx
+++ vcl/osx/salinst.cxx
@@ -445,7 +445,8 @@ bool AquaSalInstance::isNSAppThread() const
 
 void AquaSalInstance::handleAppDefinedEvent( NSEvent* pEvent )
 {
-    switch( [pEvent subtype] )
+    int nSubtype = [pEvent subtype];
+    switch( nSubtype )
     {
     case AppStartTimerEvent:
         AquaSalTimer::handleStartTimerEvent( pEvent );
