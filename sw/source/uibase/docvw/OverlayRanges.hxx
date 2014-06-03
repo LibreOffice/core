972aec23bd509424fbb594e880c737fdc07453d6
Thomas Arnhold <thomas@arnhold.org>
2014-06-03 10:01:18 +0200

    sw: fix include guards
    after the latest file move
    
    Change-Id: I105eb04ad98d1bf71eddec4528657c248fcc4348

--- sw/source/uibase/docvw/OverlayRanges.hxx
+++ sw/source/uibase/docvw/OverlayRanges.hxx
@@ -17,8 +17,8 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_SW_SOURCE_CORE_UIBASE_DOCVW_OVERLAYRANGES_HXX
-#define INCLUDED_SW_SOURCE_CORE_UIBASE_DOCVW_OVERLAYRANGES_HXX
+#ifndef INCLUDED_SW_SOURCE_UIBASE_DOCVW_OVERLAYRANGES_HXX
+#define INCLUDED_SW_SOURCE_UIBASE_DOCVW_OVERLAYRANGES_HXX
 
 #include <svx/sdr/overlay/overlayobject.hxx>
 #include <basegfx/range/b2drange.hxx>
@@ -73,6 +73,6 @@ namespace sw
     } // end of namespace overlay
 } // end of namespace sw
 
-#endif // INCLUDED_SW_SOURCE_CORE_UIBASE_DOCVW_OVERLAYRANGES_HXX
+#endif // INCLUDED_SW_SOURCE_UIBASE_DOCVW_OVERLAYRANGES_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
