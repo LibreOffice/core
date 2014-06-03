5b886c730ed2b85624e32d516fc74bfe21666c06
Tor Lillqvist <tml@collabora.com>
2014-06-03 18:25:05 +0300

    Add (debugging) function to get the symbolic name of a MetaAction
    Change-Id: I3c1e5c23e7be636cc93c97da0c2db00ebb1050e3

--- include/vcl/metaact.hxx
+++ include/vcl/metaact.hxx
@@ -88,7 +88,10 @@ public:
     void                Delete() { if ( 0 == --mnRefCount ) delete this; }
 
 public:
+    OUString            GetTypeName() { return TypeName( mnType ); }
+
     static MetaAction*  ReadMetaAction( SvStream& rIStm, ImplMetaReadData* pData );
+    static OUString     TypeName( sal_uInt16 nType );
 };
 
 class VCL_DLLPUBLIC MetaPixelAction : public MetaAction
