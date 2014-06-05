60cd618ec87643ca79b3904415bdb89eb3523692
sk94 <eszkadev@gmail.com>
2014-06-06 00:27:57 +0200

    RID_SCDLG_COND_FORMAT_MANAGER conversion to .ui
    Change-Id: If3e0ac5afd73ed31f8c21ab68d0c3c765cb447f4

--- sc/Library_scui.mk
+++ sc/Library_scui.mk
@@ -55,7 +55,7 @@ $(eval $(call gb_Library_add_exception_objects,scui,\
     sc/source/ui/attrdlg/scuiexp \
     sc/source/ui/attrdlg/tabpages \
     sc/source/ui/cctrl/editfield \
-    sc/source/ui/condformat/condformatmgr \
+	sc/source/ui/condformat/condformatmgr \
     sc/source/ui/dbgui/dapidata \
     sc/source/ui/dbgui/dapitype \
     sc/source/ui/dbgui/dpgroupdlg \
