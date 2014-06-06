ae915ba34f6b721b81747e7b90a46e1133d5238e
matteocam <matteo.campanelli@gmail.com>
2014-06-06 15:14:52 -0400

    More debugging; fixed Font::IsTransparent
    Change-Id: Idcb54408dd3cf6561b78eb28fdb409d2e31df541
    (cherry picked from commit d84dba8b01c1f240f22b774c951ebf206d5a3ac9)

--- include/vcl/outdev.hxx
+++ include/vcl/outdev.hxx
@@ -1040,7 +1040,7 @@ public:
     void                        SetTextFillColor();
     void                        SetTextFillColor( const Color& rColor );
     Color                       GetTextFillColor() const;
-    bool                        IsTextFillColor() const {  return return !maFont.IsTransparent(); }
+    bool                        IsTextFillColor() const { return !maFont.IsTransparent(); }
 
     void                        SetTextLineColor();
     void                        SetTextLineColor( const Color& rColor );
