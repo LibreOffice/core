e197043cf1c792fb5311d6597a18ee84b289985b
Caolán McNamara <caolanm@redhat.com>
2014-06-06 10:06:00 +0100

    c++03 vs c++11 enum namespacing
    Change-Id: I91be0e7d2b013221b1d47a5cd1e4e75eb16c2b67
    (cherry picked from commit e990e4d145f16c0c7e4e9e12c4882ddde63a3d5a)

--- sd/qa/unit/import-tests.cxx
+++ sd/qa/unit/import-tests.cxx
@@ -458,12 +458,12 @@ void SdFiltersTest::testBnc870233_1()
             const SvxWeightItem *pWeight = dynamic_cast<const SvxWeightItem *>((*it).pAttr);
             if( pWeight )
             {
-                CPPUNIT_ASSERT_EQUAL( FontWeight::WEIGHT_BOLD, pWeight->GetWeight());
+                CPPUNIT_ASSERT_EQUAL( WEIGHT_BOLD, pWeight->GetWeight());
             }
             const SvxPostureItem *pPosture = dynamic_cast<const SvxPostureItem *>((*it).pAttr);
             if( pPosture )
             {
-                CPPUNIT_ASSERT_EQUAL( FontItalic::ITALIC_NONE, pPosture->GetPosture());
+                CPPUNIT_ASSERT_EQUAL( ITALIC_NONE, pPosture->GetPosture());
             }
         }
     }
@@ -485,12 +485,12 @@ void SdFiltersTest::testBnc870233_1()
             const SvxWeightItem *pWeight = dynamic_cast<const SvxWeightItem *>((*it).pAttr);
             if( pWeight )
             {
-                CPPUNIT_ASSERT_EQUAL( FontWeight::WEIGHT_NORMAL, pWeight->GetWeight());
+                CPPUNIT_ASSERT_EQUAL( WEIGHT_NORMAL, pWeight->GetWeight());
             }
             const SvxPostureItem *pPosture = dynamic_cast<const SvxPostureItem *>((*it).pAttr);
             if( pPosture )
             {
-                CPPUNIT_ASSERT_EQUAL( FontItalic::ITALIC_NORMAL, pPosture->GetPosture());
+                CPPUNIT_ASSERT_EQUAL( ITALIC_NORMAL, pPosture->GetPosture());
             }
         }
     }
