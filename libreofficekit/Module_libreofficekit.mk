7d210d6b32dc28bb2725983ffb66d8f153299c01
Andrzej Hunt <andrzej.hunt@collabora.com>
2014-06-09 11:33:25 +0100

    Move liblibreoffice into LibreOfficeKit.
    Conflicts:
    	include/LibreOfficeKit/LibreOfficeKit.hxx
    
    Change-Id: Ib2754a77be470faaa6f9b27644a1d8dd082ed2b8

---
+++ libreofficekit/Module_libreofficekit.mk
@@ -0,0 +1,18 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_Module_Module,libreofficekit))
+
+ifeq ($(OS),LINUX)
+$(eval $(call gb_Module_add_targets,libreofficekit,\
+    Library_libreofficekit \
+))
+endif
+
+# vim: set ts=4 sw=4 et:
