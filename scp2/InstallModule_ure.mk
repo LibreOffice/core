04c18d98557a3a85660877b42dff6e10324e3699
David Tardon <dtardon@redhat.com>
2014-06-09 08:56:24 +0200

    remove a bunch of unneeded defines
    Change-Id: I02e99812b7a6afba04afee1b5c810bf5b1640c51

--- scp2/InstallModule_ure.mk
+++ scp2/InstallModule_ure.mk
@@ -14,7 +14,6 @@ $(eval $(call gb_InstallModule_use_auto_install_libs,scp2/ure,ure))
 $(eval $(call gb_InstallModule_define_if_set,scp2/ure,\
 	ENABLE_MACOSX_MACLIKE_APP_STRUCTURE \
 	ENABLE_MACOSX_SANDBOX \
-	SYSTEM_LIBXML \
 ))
 
 $(eval $(call gb_InstallModule_define_value_if_set,scp2/ure,\
