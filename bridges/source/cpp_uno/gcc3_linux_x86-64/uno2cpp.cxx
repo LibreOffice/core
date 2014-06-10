7511a8eeca171575e878528a1fa434197f1c1f22
Baptiste Daroussin <bapt@FreeBSD.org>
2014-06-10 18:35:36 +0200

    Fix build on FreeBSD with libc++
    Change-Id: Ib51b05eb3f7c5d1288bee0f9f738ab1f37251c3e

--- bridges/source/cpp_uno/gcc3_linux_x86-64/uno2cpp.cxx
+++ bridges/source/cpp_uno/gcc3_linux_x86-64/uno2cpp.cxx
@@ -285,10 +285,17 @@ static void cpp_call(
      catch (...)
      {
          // fill uno exception
+#ifdef _LIBCPP_VERSION
+         CPPU_CURRENT_NAMESPACE::fillUnoException(
+             reinterpret_cast< __cxxabiv1::__cxa_eh_globals * >(
+                 __cxxabiv1::__cxa_get_globals())->caughtExceptions,
+             *ppUnoExc, pThis->getBridge()->getCpp2Uno());
+#else
          fillUnoException(
              reinterpret_cast< CPPU_CURRENT_NAMESPACE::__cxa_eh_globals * >(
                  __cxxabiv1::__cxa_get_globals())->caughtExceptions,
              *ppUnoExc, pThis->getBridge()->getCpp2Uno());
+#endif
 
         // temporary params
         for ( ; nTempIndices--; )
