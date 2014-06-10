921c9e1a269cc2b34cfd230f73f3c2350204fde8
Jan Holesovsky <kendy@collabora.com>
2014-06-10 17:13:38 +0200

    pct import: Reset clip region before closing the metafile.
    Otherwise parts of the image are missing.
    
    Change-Id: If9018c32807359862a9be825b0ebdaacce2d3490

--- filter/source/graphicfilter/ipict/ipict.cxx
+++ filter/source/graphicfilter/ipict/ipict.cxx
@@ -1860,6 +1860,7 @@ void PictReader::ReadPict( SvStream & rStreamPict, GDIMetaFile & rGDIMetaFile )
         pPict->Seek(nPos);
     }
 
+    pVirDev->SetClipRegion();
     rGDIMetaFile.Stop();
     delete pVirDev;
 
