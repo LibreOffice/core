a2cdc375708434c159bb704b2c490480579405a3
Caolán McNamara <caolanm@redhat.com>
2014-06-10 13:42:42 +0100

    coverity#1222232 Division or modulo by zero
    Change-Id: I4a3921df8e6510a37702ae8bd4856ba43c98213a

--- vcl/source/bitmap/bitmapscalesuper.cxx
+++ vcl/source/bitmap/bitmapscalesuper.cxx
@@ -502,9 +502,14 @@ void scalePalleteGeneral2(BitmapReadAccess* pAcc, BitmapWriteAccess* pWAcc,
                 nTotalWeightY += nWeightY;
             }
 
-            BitmapColor aColRes( ( sal_uInt8 ) (( nSumR / nTotalWeightY ) ),
-                    ( sal_uInt8 ) (( nSumG / nTotalWeightY) ),
-                    ( sal_uInt8 ) (( nSumB / nTotalWeightY) ));
+            if (nTotalWeightY)
+            {
+                nSumR /= nTotalWeightY;
+                nSumG /= nTotalWeightY;
+                nSumB /= nTotalWeightY;
+            }
+
+            BitmapColor aColRes((sal_uInt8)nSumR, (sal_uInt8)nSumG, (sal_uInt8)nSumB);
             pWAcc->SetPixel( nYDst, nXDst++, aColRes );
         }
     }
