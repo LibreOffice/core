853f0fc557c703c23c59645c4cff2264340c87d0
Caolán McNamara <caolanm@redhat.com>
2014-06-10 13:49:29 +0100

    coverity#1222227 Division or modulo by zero
    Change-Id: I96b74d164a582cac13c419267879619e6986d71f

--- vcl/source/bitmap/bitmapscalesuper.cxx
+++ vcl/source/bitmap/bitmapscalesuper.cxx
@@ -762,9 +762,15 @@ void scale24bitRGB2(BitmapReadAccess* pAcc, BitmapWriteAccess* pWAcc,
                 else if ( nLineRange == i )
                     nWeightY = pMapFY[ nBottom ];
 
-                nSumB += nWeightY * ( nSumRowB / nTotalWeightX );
-                nSumG += nWeightY * ( nSumRowG / nTotalWeightX );
-                nSumR += nWeightY * ( nSumRowR / nTotalWeightX );
+                if (nTotalWeightX)
+                {
+                    nSumRowB /= nTotalWeightX;
+                    nSumRowG /= nTotalWeightX;
+                    nSumRowR /= nTotalWeightX;
+                }
+                nSumB += nWeightY * nSumRowB;
+                nSumG += nWeightY * nSumRowG;
+                nSumR += nWeightY * nSumRowR;
                 nTotalWeightY += nWeightY;
             }
 
