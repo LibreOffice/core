3e9238767319b5ebc131e69f338e5052a1739253
Oliver-Rainer Wittmann <orw@apache.org>
2014-06-10 14:49:06 +0000

    Related: #i125072# map given style name to corresponding UI name...
    before searching for it.
    
    (cherry picked from commit 4a76d7f700da112a553ec9d9811731e645581a7f)
    
    Conflicts:
    	sw/source/core/unocore/unoframe.cxx
    
    Change-Id: I294bd7f21ec0acee0f7b8e7be067e04b5a785e43
    (cherry picked from commit 3a72f4ba07458ee9c4c726486304f31900d89044)
    Reviewed-on: https://gerrit.libreoffice.org/9736
    Reviewed-by: Michael Stahl <mstahl@redhat.com>
    Tested-by: Michael Stahl <mstahl@redhat.com>

--- sw/source/core/unocore/unoframe.cxx
+++ sw/source/core/unocore/unoframe.cxx
@@ -693,6 +693,7 @@ bool SwGraphicProperties_Impl::AnyToItemSet(
     {
         OUString sStyle;
         *pStyleName >>= sStyle;
+        SwStyleNameMapper::FillUIName(sStyle, sStyle, nsSwGetPoolIdFromName::GET_POOLID_FRMFMT, true);
         pStyle = (SwDocStyleSheet*)pDoc->GetDocShell()->GetStyleSheetPool()->Find(sStyle,
                                                     SFX_STYLE_FAMILY_FRAME);
     }
