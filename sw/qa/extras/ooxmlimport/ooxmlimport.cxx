LibreOffice/core

commit abe1e852a232e3bdab7b9177d099fe3929f5a4aa

handle properly page breaks even if a page contains only a frame (fdo#55381)
The test document has 4 pages which only contain frames, first 3 pages
(sections) have <p> block with <framePr> and another <p> with just <sectPr>,
and the second paragraph would be removed, thus there would be no nodes on which
to actually set the page style and they would be set on nodes for the previous
page, overwriting its page style. The last page does not have its own <p>
at all, so it needs to be faked.

The changes in finishParagraph() are because of e.g. the comments-nested.odt
sw test, which there causes exception because of unknown property ParaStyleName
(comments do not use those it seems) and the code is skipped over (which
is probably a bug on its own, but it's unrelated to this change otherwise).

Change-Id: I9d37f992407a9b649c710d2a031ec4447fb11771

writerfilter/source/dmapper/DomainMapper.cxx
@@ -2458,6 +2458,18 @@ void DomainMapper::lcl_endSectionGroup()
     {
         m_pImpl->CheckUnregisteredFrameConversion();
         m_pImpl->ExecuteFrameConversion();
+        if(m_pImpl->GetIsFirstParagraphInSection())
+        {
+            // This section has no paragraph at all (e.g. they are all actually in a frame).
+            // If this section has a page break, there would be nothing to apply to the page
+            // style, so force a dummy paragraph.
+            lcl_startParagraphGroup();
+            lcl_startCharacterGroup();
+            sal_uInt8 sBreak[] = { 0xd };
+            lcl_text(sBreak, 1);
+            lcl_endCharacterGroup();
+            lcl_endParagraphGroup();
+        }
         PropertyMapPtr pContext = m_pImpl->GetTopContextOfType(CONTEXT_SECTION);
         SectionPropertyMap* pSectionContext = dynamic_cast< SectionPropertyMap* >( pContext.get() );
         OSL_ENSURE(pSectionContext, "SectionContext unavailable!");

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -1105,12 +1105,6 @@ void DomainMapper_Impl::finishParagraph( PropertyMapPtr pPropertyMap )
                 uno::Reference< text::XTextRange > xParaEnd( xCur, uno::UNO_QUERY );
                 CheckParaMarkerRedline( xParaEnd );
 
-                m_bIsFirstParaInSection = false;
-                m_bIsLastParaInSection = false;
-                m_bParaChanged = false;
-
-                // Reset the frame properties for the next paragraph
-                pParaContext->ResetFrameProperties();
             }
             if( !bKeepLastParagraphProperties )
                 rAppendContext.pLastParagraphProperties = pToBeSavedProperties;
@@ -1119,10 +1113,21 @@ void DomainMapper_Impl::finishParagraph( PropertyMapPtr pPropertyMap )
         {
             OSL_FAIL( "IllegalArgumentException in DomainMapper_Impl::finishParagraph" );
         }
-        catch(const uno::Exception&)
+        catch(const uno::Exception& e)
         {
+            SAL_WARN( "writerfilter", "finishParagraph() exception: " << e.Message );
         }
     }
+
+    m_bParaChanged = false;
+    if(!pParaContext->IsFrameMode())
+    { // If the paragraph is in a frame, it's not a paragraph of the section itself.
+        m_bIsFirstParaInSection = false;
+        m_bIsLastParaInSection = false;
+    }
+
+    // Reset the frame properties for the next paragraph
+    pParaContext->ResetFrameProperties();
 
 #ifdef DEBUG_DOMAINMAPPER
     dmapper_logger->endElement();

sw/qa/extras/ooxmlimport/ooxmlimport.cxx
@@ -2056,6 +2056,15 @@ DECLARE_OOXMLIMPORT_TEST(testFdo75722dml, "fdo75722-dml.docx")
     CPPUNIT_ASSERT_EQUAL(sal_Int64(3128), nRot);
 }
 
+DECLARE_OOXMLIMPORT_TEST(testFdo55381, "fdo55381.docx")
+{
+    uno::Reference<frame::XModel> xModel(mxComponent, uno::UNO_QUERY);
+    uno::Reference<text::XTextViewCursorSupplier> xTextViewCursorSupplier(xModel->getCurrentController(), uno::UNO_QUERY);
+    uno::Reference<text::XPageCursor> xCursor(xTextViewCursorSupplier->getViewCursor(), uno::UNO_QUERY);
+    xCursor->jumpToLastPage();
+    CPPUNIT_ASSERT_EQUAL(sal_Int16(4), xCursor->getPage());
+}
+
 #endif
 
 CPPUNIT_PLUGIN_IMPLEMENT();

commit 4d120b6ab181f530d3fedc963b1c6ec777f2608a

DOCX import: fix additional empty paragraphs around footnote text
Regression from commit abe1e852a232e3bdab7b9177d099fe3929f5a4aa (handle
properly page breaks even if a page contains only a frame (fdo#55381),
2014-04-21), DomainMapper::lcl_endSectionGroup() inserted an empty
paragraph before and after the real footnote text. Disable addition of
the dummy paragraph for footnotes.

Change-Id: I6e1954c9bd5520ac284d66244ce1c9004e970305

writerfilter/source/dmapper/DomainMapper.cxx
@@ -2502,7 +2502,9 @@ void DomainMapper::lcl_endSectionGroup()
     {
         m_pImpl->CheckUnregisteredFrameConversion();
         m_pImpl->ExecuteFrameConversion();
-        if(m_pImpl->GetIsFirstParagraphInSection())
+        // First paragraph in a footnote doesn't count: that would create
+        // additional paragraphs before and after the real footnote content.
+        if(m_pImpl->GetIsFirstParagraphInSection() && !m_pImpl->IsInFootOrEndnote())
         {
             // This section has no paragraph at all (e.g. they are all actually in a frame).
             // If this section has a page break, there would be nothing to apply to the page

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -172,6 +172,7 @@ DomainMapper_Impl::DomainMapper_Impl(
         m_bInAnyTableImport( false ),
         m_bInHeaderFooterImport( false ),
         m_bDiscardHeaderFooter( false ),
+        m_bInFootOrEndnote(false),
         m_bLineNumberingSet( false ),
         m_bIsInFootnoteProperties( false ),
         m_bIsCustomFtnMark( false ),
@@ -1515,6 +1516,7 @@ void DomainMapper_Impl::PopPageHeaderFooter()
 
 void DomainMapper_Impl::PushFootOrEndnote( bool bIsFootnote )
 {
+    m_bInFootOrEndnote = true;
     try
     {
         // Redlines outside the footnote should not affect footnote content
@@ -1685,6 +1687,7 @@ void DomainMapper_Impl::PopFootOrEndnote()
         return;
     }
     m_aRedlines.pop();
+    m_bInFootOrEndnote = false;
 }
 
 

writerfilter/source/dmapper/DomainMapper_Impl.hxx
@@ -359,6 +359,7 @@ private:
     bool                            m_bInAnyTableImport; //in import of fonts, styles, lists or lfos
     bool                            m_bInHeaderFooterImport;
     bool                            m_bDiscardHeaderFooter;
+    bool                            m_bInFootOrEndnote;
 
     bool                            m_bLineNumberingSet;
     bool                            m_bIsInFootnoteProperties;
@@ -571,6 +572,7 @@ public:
 
     void PushFootOrEndnote( bool bIsFootnote );
     void PopFootOrEndnote();
+    bool IsInFootOrEndnote() const { return m_bInFootOrEndnote; }
 
     void PushAnnotation();
     void PopAnnotation();

sw/qa/extras/ooxmlimport/ooxmlimport.cxx
@@ -2210,6 +2210,16 @@ DECLARE_OOXMLIMPORT_TEST(testFdo78939, "fdo78939.docx")
     CPPUNIT_ASSERT_EQUAL(OUString(), getProperty<OUString>(getParagraph(1), "NumberingStyleName"));
 }
 
+DECLARE_OOXMLIMPORT_TEST(testFootnote, "footnote.docx")
+{
+    uno::Reference<text::XFootnotesSupplier> xFootnotesSupplier(mxComponent, uno::UNO_QUERY);
+    uno::Reference<container::XIndexAccess> xFootnotes(xFootnotesSupplier->getFootnotes(), uno::UNO_QUERY);
+    uno::Reference<text::XTextRange> xFootnote(xFootnotes->getByIndex(0), uno::UNO_QUERY);
+    OUString aFootnote = xFootnote->getString();
+    // Ensure there are no additional newlines after "bar".
+    CPPUNIT_ASSERT(aFootnote.endsWith("bar"));
+}
+
 #endif
 
 CPPUNIT_PLUGIN_IMPLEMENT();