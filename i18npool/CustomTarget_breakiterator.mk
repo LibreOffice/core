1f3a94ff778658a36733ea3a1c0bbf47a60c5162
Baptiste Daroussin <bapt@FreeBSD.org>
2014-06-11 11:23:22 +0200

    Do not hard code sed but use $(SED)
    The sed binary is not always the gnu sed, the SED macro exists to give a
    chance to use another binary name like gsed
    
    Change-Id: Ie21d830fc49f38a29aecf685b0f2c55c4404786a

--- i18npool/CustomTarget_breakiterator.mk
+++ i18npool/CustomTarget_breakiterator.mk
@@ -98,7 +98,7 @@ $(i18npool_BIDIR)/%.brk : $(i18npool_BIDIR)/%.txt $(call gb_ExternalExecutable_g
 #   sed substitution...
 $(i18npool_BIDIR)/%.txt : \
 	$(SRCDIR)/i18npool/source/breakiterator/data/%.txt | $(i18npool_BIDIR)/.dir
-	sed -e ': dummy' \
+	$(SED) -e ': dummy' \
 		$(if $(ICU_RECLASSIFIED_CLOSE_PARENTHESIS),-e "s#\[:LineBreak =  Close_Punctuation:\]#\[& \[:LineBreak = Close_Parenthesis:\]\]#") \
 		$(if $(ICU_RECLASSIFIED_CONDITIONAL_JAPANESE_STARTER),,\
 			-e '/\[:LineBreak =  Conditional_Japanese_Starter:\]/d' \
