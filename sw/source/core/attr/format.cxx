4af5712214680083d86a1ecbd3f85d48341c4c1c
Herbert Dürr <hdu@apache.org>
2014-06-12 08:55:46 +0000

    Resolves: #i125071# check SwFmt::GetItemState()'s ppItem parameter...
    for NULL before assigning something into it. The ppItem default value is NULL
    so it is allowed. The method should be prepared to handle that.
    
    (cherry picked from commit 7d096ff2e25e422f55c821ecce99cecac6ee4b95)
    
    Conflicts:
    	sw/source/core/attr/format.cxx
    
    Change-Id: I5b4bc84cf10f6f2955af21ae80a43e985f0555fb
    (cherry picked from commit 6de99d2d046a4fefbd9c264b8b19e3283099ed81)

--- sw/source/core/attr/format.cxx
+++ sw/source/core/attr/format.cxx
@@ -422,14 +422,16 @@ SfxItemState SwFmt::GetItemState( sal_uInt16 nWhich, bool bSrchInParent, const S
             static SvxBrushItem aSvxBrushItem(RES_BACKGROUND);
 
             aSvxBrushItem = sw::getSvxBrushItemFromSourceSet(aSet, bSrchInParent);
-            *ppItem = &aSvxBrushItem;
+            if( ppItem )
+                *ppItem = &aSvxBrushItem;
 
             return SFX_ITEM_SET;
         }
 
         // if not, reset pointer and return SFX_ITEM_DEFAULT to signal that
         // the item is not set
-        *ppItem = 0;
+        if( ppItem )
+            *ppItem = NULL;
 
         return SFX_ITEM_DEFAULT;
     }
