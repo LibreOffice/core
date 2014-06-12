625263c8379217465fa041504c88f9a7e006c73a
Stephan Bergmann <sbergman@redhat.com>
2014-06-12 16:20:01 +0200

    Avoid "must return a value" warnings
    Change-Id: I2b8ac2204ec4f47dcce033da18e31b8bb5285552

--- sw/source/core/crsr/crossrefbookmark.cxx
+++ sw/source/core/crsr/crossrefbookmark.cxx
@@ -17,6 +17,10 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
+#include <sal/config.h>
+
+#include <cstdlib>
+
 #include <IDocumentMarkAccess.hxx>
 #include <crossrefbookmark.hxx>
 #include <ndtxt.hxx>
@@ -59,7 +63,7 @@ namespace sw { namespace mark
         OSL_PRECOND(false,
             "<sw::mark::CrossRefBookmark::GetOtherMarkPos(..)>"
             " - this should never be called!");
-        abort();
+        for (;;) { std::abort(); } // avoid "must return a value" warnings
     }
 
     CrossRefHeadingBookmark::CrossRefHeadingBookmark(const SwPaM& rPaM,
