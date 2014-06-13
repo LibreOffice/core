de0d60327e2cd274f55e73417d3281e4a81b2271
Yogesh Bharate <yogesh.bharate@synerzip.com>
2014-06-13 16:15:16 +0530

    fdo#79968: & fdo#79969: Fix for embedded powerpoint & excel sheet in docx.
    Problem Description:
    fdo#79968:
    1. Document contains embedded powerpoint in docx.
    2. After RT, embedded powerpoint object is not preserved.
    3. In this case, .sldx is get converted to .bin due to this powerpoint slides
    is getting converted to picture.
    
    fdo#79969:
    1. Document contains embedded excel work sheet.
    2. After RT, embedded excel sheet is not preserved.
    3. In this case, .xlsm is get converted to .xls.
    4. Similar case happened with binary excel work sheet.
    
    Implementation:
    Added sMediaType, sRelationType & sFileExtension for embedded excel sheet,
    binary excel sheet & powerpoint slide.
    
    Signed-off-by: Michael Stahl <mstahl@redhat.com>
    
    Conflicts:
    	sw/qa/extras/ooxmlexport/ooxmlexport.cxx
    Reviewed on:
    	https://gerrit.libreoffice.org/9765
    
    (cherry picked from commit 9f8785074512d6442fd97d67860fd625ac4d6bb1)
    
    Conflicts:
    	sw/qa/extras/ooxmlexport/ooxmlexport.cxx
    
    Change-Id: I8f94e3f9293f2ac98b61f919d60ca556695b26c2

--- sw/qa/extras/ooxmlexport/ooxmlexport.cxx
+++ sw/qa/extras/ooxmlexport/ooxmlexport.cxx
@@ -1657,6 +1657,49 @@ DECLARE_OOXMLEXPORT_TEST(testFdo69644, "fdo69644.docx")
     assertXPath(pXmlDoc, "/w:document/w:body/w:tbl/w:tblGrid/w:gridCol", 5);
 }
 
+DECLARE_OOXMLEXPORT_TEST(testfdo79968_sldx, "fdo79968.docx")
+{
+    // This UT for DOCX embedded with powerpoint slide
+    xmlDocPtr pXmlDoc = parseExport("[Content_Types].xml");
+
+    if (!pXmlDoc)
+       return;
+
+    assertXPath(pXmlDoc,
+                "/ContentType:Types/ContentType:Override[@ContentType='application/vnd.openxmlformats-officedocument.presentationml.slide']",
+                "PartName",
+                "/word/embeddings/oleObject1.sldx");
+}
+
+DECLARE_OOXMLEXPORT_TEST(testfdo79969_xlsb, "fdo79969_xlsb.docx")
+{
+    // This UT for DOCX embedded with binary excel work sheet.
+    xmlDocPtr pXmlDoc = parseExport("[Content_Types].xml");
+
+    if (!pXmlDoc)
+       return;
+
+    assertXPath(pXmlDoc,
+                "/ContentType:Types/ContentType:Override[@ContentType='application/vnd.ms-excel.sheet.binary.macroEnabled.12']",
+                "PartName",
+                "/word/embeddings/oleObject1.xlsb");
+}
+
+DECLARE_OOXMLEXPORT_TEST(testfdo79969_xlsm, "fdo79969_xlsm.docx")
+{
+    // This UT for DOCX embedded with excel work sheet.
+    xmlDocPtr pXmlDoc = parseExport("[Content_Types].xml");
+
+    if (!pXmlDoc)
+       return;
+
+    assertXPath(pXmlDoc,
+                "/ContentType:Types/ContentType:Override[@ContentType='application/vnd.ms-excel.sheet.macroEnabled.12']",
+                "PartName",
+                "/word/embeddings/oleObject1.xlsm");
+}
+
+
 DECLARE_OOXMLEXPORT_TEST(testCp1000015, "cp1000015.odt")
 {
     // Redline and hyperlink end got exported in an incorrect order.
