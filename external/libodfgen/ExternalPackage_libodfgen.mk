efec628c96470eba450bf43e0d5507a49c329c12
Douglas Mencken <dougmencken@gmail.com>
2014-06-14 08:11:52 -0400

    external/librevenge,libmwaw,libodfgen,libwps: fix self-linked symlinks build problem
    ...LibreOfficeDev.app/Contents/MacOS/librevenge-0.0.0.dylib: Too many levels of symbolic links
    (librevenge-0.0.0.dylib --> librevenge-0.0.0.dylib: broken symbolic link to librevenge-0.0.0.dylib)
    
    The reason for this is that symlink librevenge-0.0.dylib
    (UnpackedTarball/librevenge/src/lib/.libs/librevenge-0.0.dylib -> librevenge-0.0.0.dylib)
    is copied via cp --no-dereference, thus becoming linked to self.
    
    Change-Id: I4b918c35c594800fb2d7f84ee0ee9f2ff2a5fe14
    Reviewed-on: https://gerrit.libreoffice.org/9783
    Tested-by: David Tardon <dtardon@redhat.com>
    Reviewed-by: David Tardon <dtardon@redhat.com>
    (cherry picked from commit 9f339a89453808b917177a3ee675a76385758902)

--- external/libodfgen/ExternalPackage_libodfgen.mk
+++ external/libodfgen/ExternalPackage_libodfgen.mk
@@ -12,7 +12,7 @@ $(eval $(call gb_ExternalPackage_ExternalPackage,libodfgen,libodfgen))
 $(eval $(call gb_ExternalPackage_use_external_project,libodfgen,libodfgen))
 
 ifeq ($(OS),MACOSX)
-$(eval $(call gb_ExternalPackage_add_file,libodfgen,$(LIBO_LIB_FOLDER)/libodfgen-0.1.1.dylib,src/.libs/libodfgen-0.1.dylib))
+$(eval $(call gb_ExternalPackage_add_file,libodfgen,$(LIBO_LIB_FOLDER)/libodfgen-0.1.1.dylib,src/.libs/libodfgen-0.1.1.dylib))
 else ifeq ($(OS),WNT)
 $(eval $(call gb_ExternalPackage_add_file,libodfgen,$(LIBO_LIB_FOLDER)/libodfgen-0.1.dll,src/.libs/libodfgen-0.1.dll))
 else ifeq ($(filter IOS ANDROID,$(OS)),)
