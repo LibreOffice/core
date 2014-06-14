33a40a317ade7dd3fc1b2d3dd6662820e104e67c
weigao <weigao@multicorewareinc.com>
2014-06-14 08:00:02 -0700

    add shaders for 3.0 glsl version
    Change-Id: I52dd49132a9b625e8be51b4d5feb7e567ffa0a6e

---
+++ chart2/opengl/shape3DVertexShaderV300.glsl
@@ -0,0 +1,29 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+attribute vec3 vertexPositionModelspace;
+attribute vec3 vertexNormalModelspace;
+
+varying vec3 positionWorldspace;
+varying vec3 normalCameraspace;
+
+uniform mat4 P;
+uniform mat4 M;
+uniform mat4 V;
+uniform mat3 normalMatrix;
+
+void main()
+{
+    gl_Position =  P * V * M * vec4(vertexPositionModelspace,1);
+
+    positionWorldspace = (M * vec4(vertexPositionModelspace,1)).xyz;
+
+    normalCameraspace = normalize(mat3(V) * normalMatrix * vertexNormalModelspace);
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
