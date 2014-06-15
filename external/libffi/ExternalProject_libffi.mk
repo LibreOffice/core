39c94b2e14f52a3d0ce39559716804150de3712e
Jan Holesovsky <kendy@collabora.com>
2014-06-15 18:49:43 +0200

    libffi: Add it as an external library.
    Change-Id: I823d81a8dd88ddb0631808b7bdc125154a27b482

---
+++ external/libffi/ExternalProject_libffi.mk
@@ -0,0 +1,32 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_ExternalProject_ExternalProject,libffi))
+
+$(eval $(call gb_ExternalProject_use_autoconf,libffi,build))
+
+$(eval $(call gb_ExternalProject_register_targets,libffi,\
+	build \
+))
+
+$(call gb_ExternalProject_get_state_target,libffi,build) :
+	$(call gb_ExternalProject_run,build,\
+		$(if $(ENABLE_DEBUG),CFLAGS="$$CFLAGS -g") \
+		./configure \
+			--with-pic \
+			--enable-static \
+			--disable-shared \
+			--without-docs \
+			$(if $(ENABLE_DEBUG),--enable-debug,--disable-debug) \
+			$(if $(VERBOSE)$(verbose),--disable-silent-rules,--enable-silent-rules) \
+			$(if $(CROSS_COMPILING),--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM)) \
+		&& $(MAKE) \
+	)
+
+# vim: set noet sw=4 ts=4:
---
+++ external/libffi/UnpackedTarball_libffi.mk
@@ -0,0 +1,20 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_UnpackedTarball_UnpackedTarball,libffi))
+
+$(eval $(call gb_UnpackedTarball_set_tarball,libffi,$(LIBFFI_TARBALL)))
+
+$(eval $(call gb_UnpackedTarball_set_patchlevel,libffi,1))
+
+$(eval $(call gb_UnpackedTarball_add_patches,libffi,\
+	external/libffi/libffi-ifndef.patch \
+))
+
+# vim: set noet sw=4 ts=4:
