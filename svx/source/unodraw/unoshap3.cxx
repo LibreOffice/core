1bacd2dc1835feeb27ce4d322153d351dd9e299a
Caolán McNamara <caolanm@redhat.com>
2014-06-17 10:25:15 +0100

    coverity#1209942 Uncaught exception
    Change-Id: Ice7a4802605fbe59dfb24a27408106756a3494ba

--- svx/source/unodraw/unoshap3.cxx
+++ svx/source/unodraw/unoshap3.cxx
@@ -421,7 +421,12 @@ bool Svx3DSceneObject::setPropertyValueImpl( const OUString& rName, const SfxIte
 
 
 
-bool Svx3DSceneObject::getPropertyValueImpl( const OUString& rName, const SfxItemPropertySimpleEntry* pProperty, ::com::sun::star::uno::Any& rValue ) throw(::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::lang::WrappedTargetException, ::com::sun::star::uno::RuntimeException)
+bool Svx3DSceneObject::getPropertyValueImpl(const OUString& rName, const SfxItemPropertySimpleEntry* pProperty,
+    css::uno::Any& rValue)
+        throw (css::beans::UnknownPropertyException,
+               css::lang::WrappedTargetException,
+               css::uno::RuntimeException,
+               std::exception)
 {
     switch( pProperty->nWID )
     {
