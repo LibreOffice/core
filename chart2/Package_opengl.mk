016467d8a54bc5ed4990236dc990824b3efefa9c
weigao <weigao@multicorewareinc.com>
2014-06-18 08:01:46 -0700

    add batch render texture shaders
    Change-Id: I922d24bc63ed7c1f16318c25da0b6b3401d24b6f

--- chart2/Package_opengl.mk
+++ chart2/Package_opengl.mk
@@ -32,6 +32,8 @@ $(eval $(call gb_Package_add_files,chart2_opengl_shader,$(LIBO_BIN_FOLDER)/openg
 	shape3DVertexShaderBatch.glsl \
 	shape3DFragmentShaderV300.glsl \
 	shape3DVertexShaderV300.glsl \
+	textFragmentShaderBatch.glsl \
+	textVertexShaderBatch.glsl \
 ))
 
 # vim: set noet sw=4 ts=4:
