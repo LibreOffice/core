bec4a103511c74edc85a6fdcc8d5355eb2d8a854
Markus Mohrhard <markus.mohrhard@collabora.co.uk>
2014-06-21 03:31:33 +0200

    small whitespace changes
    Change-Id: I10aa08268bf148eb792440f13bcf044b350d6937

--- chart2/opengl/textFragmentShaderBatch.glsl
+++ chart2/opengl/textFragmentShaderBatch.glsl
@@ -6,15 +6,18 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
+
 #version 330 core
 #extension GL_EXT_texture_array : enable
+
 uniform sampler2DArray texArray;
 in vec3 vTexCoord;
 
 out vec4 actualColor;
 void main()
 {
-	vec3 color = texture2DArray(texArray, vTexCoord.xyz).rgb;
+    vec3 color = texture2DArray(texArray, vTexCoord.xyz).rgb;
     actualColor = vec4(color, 1.0 - color.r);
 }
+
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
