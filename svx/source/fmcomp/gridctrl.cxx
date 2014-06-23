6fa383b20da7f39b5ac8bb433917beb9fa9e6f99
Stephan Bergmann <sbergman@redhat.com>
2014-06-23 23:20:52 +0200

    Missing #include
    Change-Id: Ib78efbb40f95884e8446bf247265004fe66a966b

--- svx/source/fmcomp/gridctrl.cxx
+++ svx/source/fmcomp/gridctrl.cxx
@@ -40,6 +40,7 @@
 #include <com/sun/star/beans/XPropertySet.hpp>
 #include <com/sun/star/beans/PropertyChangeEvent.hpp>
 #include <comphelper/processfactory.hxx>
+#include <cppuhelper/exc_hlp.hxx>
 #include <tools/resid.hxx>
 #include <tools/diagnose_ex.h>
 #include <vcl/menu.hxx>
