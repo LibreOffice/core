a7b86c0e647bef6da677fa71aa921976bd165138
Mihai Varga <mihai.mv13@gmail.com>
2014-06-23 11:44:40 +0300

    Updated CMIS patches and the makefile for the OneDrive binding
    To fix build, also squashed in:
    
    Author: Miklos Vajna <vmiklos@collabora.co.uk>
    Date:   Sun Dec 14 15:18:58 2014 +0100
    
        upload libcmis 0.5.0
    
    Author: Mihai Varga <mihai.mv13@gmail.com>
    Date:   Mon Aug 11 13:23:53 2014 +0300
    
        SharePoint binding settings, part 1
    
        Conflicts:
            officecfg/registry/data/org/openoffice/Office/Common.xcu
    
    Change-Id: I45eb81a0139c42864f93ad89c4a31185f3ac5bd6

--- external/libcmis/StaticLibrary_cmis.mk
+++ external/libcmis/StaticLibrary_cmis.mk
@@ -46,17 +46,34 @@ $(eval $(call gb_StaticLibrary_add_generated_exception_objects,cmislib,\
 	UnpackedTarball/cmis/src/libcmis/gdrive-repository \
 	UnpackedTarball/cmis/src/libcmis/gdrive-session \
 	UnpackedTarball/cmis/src/libcmis/gdrive-utils \
+	UnpackedTarball/cmis/src/libcmis/http-session \
 	UnpackedTarball/cmis/src/libcmis/json-utils \
 	UnpackedTarball/cmis/src/libcmis/oauth2-data \
 	UnpackedTarball/cmis/src/libcmis/oauth2-handler \
 	UnpackedTarball/cmis/src/libcmis/oauth2-providers \
 	UnpackedTarball/cmis/src/libcmis/object \
 	UnpackedTarball/cmis/src/libcmis/object-type \
-	UnpackedTarball/cmis/src/libcmis/property-type \
+	UnpackedTarball/cmis/src/libcmis/onedrive-document \
+	UnpackedTarball/cmis/src/libcmis/onedrive-folder \
+	UnpackedTarball/cmis/src/libcmis/onedrive-object \
+	UnpackedTarball/cmis/src/libcmis/onedrive-object-type \
+	UnpackedTarball/cmis/src/libcmis/onedrive-property \
+	UnpackedTarball/cmis/src/libcmis/onedrive-repository \
+	UnpackedTarball/cmis/src/libcmis/onedrive-session \
+	UnpackedTarball/cmis/src/libcmis/onedrive-utils \
 	UnpackedTarball/cmis/src/libcmis/property \
+	UnpackedTarball/cmis/src/libcmis/property-type \
 	UnpackedTarball/cmis/src/libcmis/rendition \
 	UnpackedTarball/cmis/src/libcmis/repository \
 	UnpackedTarball/cmis/src/libcmis/session-factory \
+	UnpackedTarball/cmis/src/libcmis/sharepoint-document \
+	UnpackedTarball/cmis/src/libcmis/sharepoint-folder \
+	UnpackedTarball/cmis/src/libcmis/sharepoint-object \
+	UnpackedTarball/cmis/src/libcmis/sharepoint-object-type \
+	UnpackedTarball/cmis/src/libcmis/sharepoint-property \
+	UnpackedTarball/cmis/src/libcmis/sharepoint-repository \
+	UnpackedTarball/cmis/src/libcmis/sharepoint-session \
+	UnpackedTarball/cmis/src/libcmis/sharepoint-utils \
 	UnpackedTarball/cmis/src/libcmis/ws-document \
 	UnpackedTarball/cmis/src/libcmis/ws-folder \
 	UnpackedTarball/cmis/src/libcmis/ws-navigationservice \
