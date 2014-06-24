LibreOffice/core

commit 6a043e9c0acff20e1618ca8ec15c21d5d0fd0d37

Use the new type-checking Reference constructor to reduce code noise
Also create a Clang compiler plugin to detect such cases.

Change-Id: I61ad1a1d6b1c017eeb51f226d2dde0e9bb7f1752
Reviewed-on: https://gerrit.libreoffice.org/4001
Tested-by: LibreOffice gerrit bot <gerrit@libreoffice.org>
Reviewed-by: Noel Grandin <noelgrandin@gmail.com>
Tested-by: Noel Grandin <noelgrandin@gmail.com>

svx/source/form/formcontroller.cxx
@@ -276,8 +276,7 @@ ColumnInfoCache::ColumnInfoCache( const Reference< XColumnsSupplier >& _rxColSup
     {
         m_aColumns.clear();
 
-        Reference< XColumnsSupplier > xSupplyCols( _rxColSupplier, UNO_SET_THROW );
-        Reference< XIndexAccess > xColumns( xSupplyCols->getColumns(), UNO_QUERY_THROW );
+        Reference< XIndexAccess > xColumns( _rxColSupplier->getColumns(), UNO_QUERY_THROW );
         sal_Int32 nColumnCount = xColumns->getCount();
         m_aColumns.reserve( nColumnCount );
 
@@ -354,8 +353,6 @@ void ColumnInfoCache::initializeControls( const Sequence< Reference< XControl > 
 
             lcl_resetColumnControlInfo( *col );
 
-            Reference< XInterface > xNormColumn( col->xColumn, UNO_QUERY_THROW );
-
             const Reference< XControl >* pControl( _rControls.getConstArray() );
             const Reference< XControl >* pControlEnd( pControl + _rControls.getLength() );
             for ( ; pControl != pControlEnd; ++pControl )
@@ -377,7 +374,7 @@ void ColumnInfoCache::initializeControls( const Sequence< Reference< XControl > 
                     {
                         Reference< XPropertySet > xGridColumnModel( xGridColAccess->getByIndex( gridCol ), UNO_QUERY_THROW );
 
-                        if  (   !lcl_isBoundTo( xGridColumnModel, xNormColumn )
+                        if  (   !lcl_isBoundTo( xGridColumnModel, col->xColumn )
                             ||  !lcl_isInputRequired( xGridColumnModel )
                             )
                             continue;   // with next grid column
@@ -397,7 +394,7 @@ void ColumnInfoCache::initializeControls( const Sequence< Reference< XControl > 
                 }
 
                 if  (   !xModelPSI->hasPropertyByName( FM_PROP_BOUNDFIELD )
-                    ||  !lcl_isBoundTo( xModel, xNormColumn )
+                    ||  !lcl_isBoundTo( xModel, col->xColumn )
                     ||  !lcl_isInputRequired( xModel )
                     )
                     continue;   // with next control
@@ -2019,8 +2016,7 @@ void FormController::addToEventAttacher(const Reference< XControl > & xControl)
             m_xModelAsIndex->getByIndex(--nPos) >>= xTemp;
             if ((XFormComponent*)xComp.get() == (XFormComponent*)xTemp.get())
             {
-                Reference< XInterface >  xIfc(xControl, UNO_QUERY);
-                m_xModelAsManager->attach( nPos, xIfc, makeAny(xControl) );
+                m_xModelAsManager->attach( nPos, xControl, makeAny(xControl) );
                 break;
             }
         }
@@ -2047,8 +2043,7 @@ void FormController::removeFromEventAttacher(const Reference< XControl > & xCont
             m_xModelAsIndex->getByIndex(--nPos) >>= xTemp;
             if ((XFormComponent*)xComp.get() == (XFormComponent*)xTemp.get())
             {
-                Reference< XInterface >  xIfc(xControl, UNO_QUERY);
-                m_xModelAsManager->detach( nPos, xIfc );
+                m_xModelAsManager->detach( nPos, xControl );
                 break;
             }
         }
@@ -3055,8 +3050,7 @@ void SAL_CALL FormController::addChildController( const Reference< XFormControll
         m_xModelAsIndex->getByIndex(--nPos) >>= xTemp;
         if ( xFormOfChild == xTemp )
         {
-            Reference< XInterface >  xIfc( _ChildController, UNO_QUERY );
-            m_xModelAsManager->attach( nPos, xIfc, makeAny( _ChildController) );
+            m_xModelAsManager->attach( nPos, _ChildController, makeAny( _ChildController) );
             break;
         }
     }

commit f8115ce72dad45b82b044d9c8f5c253d0514574d

fdo#75339: Substituting XInterface* eq. for object eq. requires queryInterface
Regression introduced with 6a043e9c0acff20e1618ca8ec15c21d5d0fd0d37 "Use the new
type-checking Reference constructor to reduce code noise" which should have left
(at least some) "querying upcasts" to XInterface alone.  Closer inspection of
that commit will probably turn up further regressions that need to be reverted.

Change-Id: Ib81afc12d9df205b5b0fe6cec6ce85d012e9629f

svx/source/form/formcontroller.cxx
@@ -354,6 +354,8 @@ void ColumnInfoCache::initializeControls( const Sequence< Reference< XControl > 
 
             lcl_resetColumnControlInfo( *col );
 
+            Reference< XInterface > xNormColumn( col->xColumn, UNO_QUERY_THROW );
+
             const Reference< XControl >* pControl( _rControls.getConstArray() );
             const Reference< XControl >* pControlEnd( pControl + _rControls.getLength() );
             for ( ; pControl != pControlEnd; ++pControl )
@@ -375,7 +377,7 @@ void ColumnInfoCache::initializeControls( const Sequence< Reference< XControl > 
                     {
                         Reference< XPropertySet > xGridColumnModel( xGridColAccess->getByIndex( gridCol ), UNO_QUERY_THROW );
 
-                        if  (   !lcl_isBoundTo( xGridColumnModel, col->xColumn )
+                        if  (   !lcl_isBoundTo( xGridColumnModel, xNormColumn )
                             ||  !lcl_isInputRequired( xGridColumnModel )
                             )
                             continue;   // with next grid column
@@ -395,7 +397,7 @@ void ColumnInfoCache::initializeControls( const Sequence< Reference< XControl > 
                 }
 
                 if  (   !xModelPSI->hasPropertyByName( FM_PROP_BOUNDFIELD )
-                    ||  !lcl_isBoundTo( xModel, col->xColumn )
+                    ||  !lcl_isBoundTo( xModel, xNormColumn )
                     ||  !lcl_isInputRequired( xModel )
                     )
                     continue;   // with next control