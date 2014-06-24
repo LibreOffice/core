1144b528d1d3ad6e7291d309967ca850e5fb1254
Armin Le Grand <alg@apache.org>
2014-06-24 12:48:21 +0000

    Resolves: #i124816# use correct layer for new object when converting
    (cherry picked from commit c0f03c75c71dde47e210ca23eefe3c879b89d97c)
    
    Change-Id: Iaf6f9e040553482f5b7d9522ea640dcea0fdd8cc

--- sd/source/ui/view/drviews2.cxx
+++ sd/source/ui/view/drviews2.cxx
@@ -810,6 +810,9 @@ void DrawViewShell::FuTemporary(SfxRequest& rReq)
                     mpDrawView->DeleteMarkedObj();
                 }
 
+                // #i124816# copy layer from lowest object which gets replaced
+                pGraphicObj->SetLayer(pReplacementCandidate->GetLayer());
+
                 // now replace lowest object with new one
                 mpDrawView->ReplaceObjectAtView(pReplacementCandidate, *pPageView, pGraphicObj);
 
