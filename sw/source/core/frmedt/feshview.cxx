01ee29125610a0fe4d6dcb080da09e3961f1236a
Julien Nabet <serval2412@yahoo.fr>
2014-06-25 21:58:47 +0200

    Resolves fdo#54155: Crash when playing with a Group inside a Group
    Cherry-picked from 7bd94d73ec56622685d0e23f8b5133ba110524be
    
    Change-Id: I89c80fca1cdc8bc88172eaf8057141d5e42a06dc
    Reviewed-on: https://gerrit.libreoffice.org/9910
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>

--- sw/source/core/frmedt/feshview.cxx
+++ sw/source/core/frmedt/feshview.cxx
@@ -1198,7 +1198,7 @@ sal_Bool SwFEShell::ShouldObjectBeSelected(const Point& rPt)
 
             // Don't select header / footer objects in body edition and vice-versa
             SwContact* pContact = static_cast<SwContact*>(pObj->GetUserCall());
-            if ( !pContact->ObjAnchoredAtPage() )
+            if (pContact && !pContact->ObjAnchoredAtPage() )
             {
                 const SwPosition& rPos = pContact->GetCntntAnchor();
                 bool bInHdrFtr = GetDoc()->IsInHeaderFooter( rPos.nNode );
