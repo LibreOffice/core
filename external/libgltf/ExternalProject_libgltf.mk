a353868ffaee382e3bb073aa0bda1784818e4c34
Zolnai Tamás <tamas.zolnai@collabora.com>
2014-06-25 15:19:41 +0200

    Update libgltf: new build system and various fixes
    Building:
    - The new tarball has reasonable build system so
    build libgltf as external package instead of compiling
    source files directly.
    - Freetype dependancy is removed
    
    Improvements comes with the new libgltf
    - Can rotate the models too (orbit mode)
    - Two camera handling mode: walkthrough and orbit
    (press M to change).
    - gltf_animation_set_time() works
    - FPS can be displayed without freetype (press F)
    
    Additional notes:
    - There were some bugs/regressions which are fixed
    during the integration (see patches).
    - License files are uddated now.
    - libgltf building is enabled only on those platforms
    on which gltf support actually works (windows and linux)
    
    Reviewed-on: https://gerrit.libreoffice.org/9895
    Reviewed-by: Zolnai Tamás <zolnaitamas2000@gmail.com>
    Tested-by: Zolnai Tamás <zolnaitamas2000@gmail.com>
    (cherry picked from commit 6403f2b59b4c83e0899012e5ca0ddd705784350c)
    
    Conflicts:
    	avmedia/source/opengl/oglplayer.cxx
    
    Change-Id: Ia6c9c4da53a9b4fedba0d73aa5791489f8ad424b
    
    libgltf: variable names need module prefix
    
    Change-Id: Id144a483fe5576cdd546ac5da87890dc61f964b3
    (cherry picked from commit d83e66da94d9c789190c4729c8f0c8464e99ba88)
    
    libgltf: add patch to find the headers in WORKDIR
    
    Change-Id: I8570511be7c1d6de94ee82e18bad5b00b36d99bb
    (cherry picked from commit dfe787ef5de1b38e61989507b26ee4b92f25b9d5)
    
    libgltf: try to fix the MSVC2010 build
    
    Change-Id: I8dde439d19be45d5039904b6e917b17ca0ee4758
    (cherry picked from commit 96ff10449464ae9d869fe21dccf193a6eef670c1)
    
    glTF rendering: moving in walktrough mode is to fast
    
    Change-Id: I2b7869c6c14b69732db864a03a2d9b6638424cf7
    (cherry picked from commit f2b5ece0caca18f21f7c8e42a84d451bb399ec35)
    
    libgltf: use --with-pick
    
    Change-Id: Ide01491460c9c81ed044be762c7dd07e8a5ca575
    (cherry picked from commit bb9078e3dee48afc4c2bd8006a892924f8b38b39)
    
    libgltf: replace configure patch to avoid using autoreconf
    
    Change-Id: Idcab012af9c27c4fed31c07ce230816d1c61d605
    (cherry picked from commit c5bb272883e042e38c58847f6eb2a2f94c6ef919)
    
    libgltf: try again with this configure patch
    
    Touch only the configure but not the configure.ac
    
    Change-Id: Iee93036f2378f29530ffae9be8b4324e8425d2ec
    (cherry picked from commit 211e07c0e02ebfe9cea4b5d97b614a23f8b2012c)
    
    libgltf: remove dependencies of config.h.in
    
    It seems something is wrong with the files in the
    libgltf tarball (maybe wrong timestamps?), because
    config.h.in is always regenerated by make (autoheader).
    It causes build problems so just remove the deps for this time.
    
    Change-Id: I404c29673987d7c8f75ec8fe4978173593c75a2f
    (cherry picked from commit 3bebda9cd3269d684a3b430e25f17951deaf3d3d)
    
    libgltf: fix typo in license
    
    Change-Id: I0e5f3afb19c2457c5ccca922e75e617312801ab9
    (cherry picked from commit 1d5e875a6c61ddc9fbf0ed09878b7dfb75f89fe3)
    
    libgltf: compiler error fixes: missing include and missing namespace
    
    Thanks to Rene Engelhard for the report and the fixes.
    
    Change-Id: Ia14980bdbcd9e714b1e9cd41f04af9371aa22371
    (cherry picked from commit 481a17b6a6fb332935c3e41c077cc5f60b751021)

---
+++ external/libgltf/ExternalProject_libgltf.mk
@@ -0,0 +1,71 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_ExternalProject_ExternalProject,libgltf))
+
+$(eval $(call gb_ExternalProject_use_autoconf,libgltf,build))
+
+$(eval $(call gb_ExternalProject_register_targets,libgltf,\
+	build \
+))
+
+$(eval $(call gb_ExternalProject_use_externals,libgltf,\
+	boost_headers \
+	glew \
+	glm_headers \
+))
+
+
+ifeq ($(COM),MSC)
+
+libgltf_AdditionalIncludes :=
+
+ifeq ($(SYSTEM_BOOST),)
+libgltf_AdditionalIncludes += "$(call gb_UnpackedTarball_get_dir,boost)"
+libgltf_AdditionalIncludes += "$(SRCDIR)/config_host"
+endif
+
+ifeq ($(SYSTEM_GLEW),)
+libgltf_AdditionalIncludes += "$(call gb_UnpackedTarball_get_dir,glew)/include"
+endif
+
+ifeq ($(SYSTEM_GLM),)
+libgltf_AdditionalIncludes += "$(call gb_UnpackedTarball_get_dir,glm)"
+endif
+
+$(call gb_ExternalProject_get_state_target,libgltf,build) :
+	$(call gb_ExternalProject_run,build,\
+		msbuild.exe libgltf.vcxproj \
+			/p:Configuration=$(if $(MSVC_USE_DEBUG_RUNTIME),Debug,Release) \
+			$(if $(filter 110,$(VCVER)),/p:PlatformToolset=$(if $(filter 80,$(WINDOWS_SDK_VERSION)),v110,v110_xp) \
+				/p:VisualStudioVersion=11.0) \
+			$(if $(filter 100,$(VCVER)),/p:PlatformToolset=v100 \
+				/p:VisualStudioVersion=10.0) \
+			'/p:AdditionalIncludeDirectories=$(subst $(gb_SPACE),;,$(subst /,\,$(strip $(libgltf_AdditionalIncludes))))' \
+			/p:AdditionalLibraryDirectories=$(if $(SYSTEM_GLEW),,"$(subst /,\,$(call gb_UnpackedTarball_get_dir,glew))\lib\$(if $(MSVC_USE_DEBUG_RUNTIME),Debug,Release)\Win32") \
+	,build/win32)
+
+else
+
+$(call gb_ExternalProject_get_state_target,libgltf,build) :
+	$(call gb_ExternalProject_run,build,\
+		export PKG_CONFIG="" \
+		&& ./configure \
+			--with-pic \
+			--disable-debug \
+			--disable-werror \
+			BOOST_CFLAGS="$(if $(SYSTEM_BOOST),$(BOOST_CPPFLAGS),-I$(call gb_UnpackedTarball_get_dir,boost)) -I$(BUILDDIR)/config_$(gb_Side)" \
+			GLEW_CFLAGS="$(if $(SYSTEM_GLEW),$(GLEW_CFLAGS),-I$(call gb_UnpackedTarball_get_dir,glew)/include)" \
+			GLM_CFLAGS="$(if $(SYSTEM_GLM),$(GLM_CFLAGS),-I$(call gb_UnpackedTarball_get_dir,glm))" \
+		&& $(MAKE) \
+	)
+
+endif
+
+# vim: set noet sw=4 ts=4:
--- external/libgltf/UnpackedTarball_libgltf.mk
+++ external/libgltf/UnpackedTarball_libgltf.mk
@@ -16,12 +16,19 @@ $(eval $(call gb_UnpackedTarball_set_patchflags,libgltf,--binary))
 $(eval $(call gb_UnpackedTarball_set_patchlevel,libgltf,1))
 
 $(eval $(call gb_UnpackedTarball_add_patches,libgltf,\
-	external/libgltf/patches/include_path_glew.patch \
-	external/libgltf/patches/include_path_freetype.patch \
-	external/libgltf/patches/openmp-disable.patch \
-	external/libgltf/patches/rgba_textures.patch \
-	external/libgltf/patches/deallocation_fix.patch \
+	external/libgltf/patches/libgltf_configure.patch \
+	external/libgltf/patches/remove_config_h_in_dependencies.patch \
 	external/libgltf/patches/init_scene_set_handle.patch \
+	external/libgltf/patches/extend_get_bitmap_for_rgba_ones.patch \
+	external/libgltf/patches/wrong_duration_caused_by_uninitialized_member.patch \
+	external/libgltf/patches/wrong_media_time_caused_by_missing_fmod.patch \
+	external/libgltf/patches/wrong_animation_state_when_set_time_to_null.patch \
+	external/libgltf/patches/no_effect_of_set_looping.patch \
+	external/libgltf/patches/wrong_media_time_at_first_run_caused_by_uninitilized_member.patch \
+	external/libgltf/patches/no_animation_in_orbit_mode.patch \
+	external/libgltf/patches/walkthrough_mode_affects_orbit_mode_unexpectedly.patch \
+	external/libgltf/patches/move_fps_closer_to_the_corner.patch \
+	external/libgltf/patches/compiler_error_fixes.patch \
 ))
 
 # vim: set noet sw=4 ts=4:
