e6ee0d2fff70885fc683d8963e46b4184f0effc6
Kohei Yoshida <kohei.yoshida@collabora.com>
2014-06-27 11:31:06 -0400

    Flush modified data to the model when pressing OK in the data table dialog.
    This dialog used to have no OK / Cancel buttons, and the data would get
    flushed when the dialog got dismissed.  This behavior got lost during the
    dialog conversion.
    
    Also, SaveModified() does check whether or not the data table is modified.
    No need to call IsModified() before calling SaveModified().
    
    Change-Id: I5cea88cb52127cc5c7c1be67a5a592d87df92d0c
    (cherry picked from commit 483a4b546c850e82a2d1d304238d4830ac6b441f)

--- chart2/source/controller/dialogs/DataBrowser.cxx
+++ chart2/source/controller/dialogs/DataBrowser.cxx
@@ -1202,8 +1202,7 @@ bool DataBrowser::SaveModified()
 
 bool DataBrowser::EndEditing()
 {
-    if( IsModified())
-        SaveModified();
+    SaveModified();
 
     // apply changes made to series headers
     ::std::for_each( m_aSeriesHeaders.begin(), m_aSeriesHeaders.end(), impl::applyChangesFunctor());
