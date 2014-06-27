d6ef33877fc855ff00aa8bb8f4ea18693a1bedc4
David Tardon <dtardon@redhat.com>
2014-06-27 11:36:04 +0200

    fix build of libpagemaker on windows
    Change-Id: Iac9920b9886fda514e6db35dc89facefeebe4d68

--- external/libpagemaker/UnpackedTarball_libpagemaker.mk
+++ external/libpagemaker/UnpackedTarball_libpagemaker.mk
@@ -11,4 +11,12 @@ $(eval $(call gb_UnpackedTarball_UnpackedTarball,libpagemaker))
 
 $(eval $(call gb_UnpackedTarball_set_tarball,libpagemaker,$(PAGEMAKER_TARBALL)))
 
+$(eval $(call gb_UnpackedTarball_set_patchlevel,libpagemaker,1))
+
+$(eval $(call gb_UnpackedTarball_add_patches,libpagemaker,\
+	external/libpagemaker/0001-define-M_PI-if-not-available.patch \
+	external/libpagemaker/0002-add-missing-typedef-on-windows.patch \
+	external/libpagemaker/0003-round-is-not-available-on-windows.patch \
+))
+
 # vim: set noet sw=4 ts=4:
