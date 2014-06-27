77af6cd18c2df43b75cd2c973a5267647de2f6e5
Caolán McNamara <caolanm@redhat.com>
2014-06-27 09:59:03 +0100

    update hyphen 2.8.7
    Change-Id: Icbe95429903dd3605acfd659c46f001d838b52c3

--- external/hyphen/UnpackedTarball_hyphen.mk
+++ external/hyphen/UnpackedTarball_hyphen.mk
@@ -12,10 +12,6 @@ $(eval $(call gb_UnpackedTarball_UnpackedTarball,hyphen))
 $(eval $(call gb_UnpackedTarball_set_tarball,hyphen,$(HYPHEN_TARBALL)))
 
 $(eval $(call gb_UnpackedTarball_add_patches,hyphen,\
-	external/hyphen/hyphen-lenwaswrong.patch \
-	external/hyphen/hyphen-fdo43931.patch \
-	external/hyphen/hyphen-android.patch \
-	external/hyphen/hyphen-rhmin.patch \
 	external/hyphen/hyphen-build.patch \
 	external/hyphen/hyphen-fdo48017-wfopen.patch \
 ))
