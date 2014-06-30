LibreOffice/core

commit 12dfca2ea61116665a7abefd43a9a4caa2b8920b

Resolves: #i119307# added deletion of sw's BmpWindow control...
...due to the fact that graphics may be transparent

Conflicts:
	sw/source/ui/frmdlg/frmpage.cxx

Change-Id: I049cc5a6abf1a19ef181991bb1015e0708dc1cc2

sw/source/ui/frmdlg/frmpage.cxx
@@ -2525,8 +2525,8 @@ BmpWindow::BmpWindow( Window* pPar, sal_uInt16 nId,
     bGraphic(sal_False),
     bLeftAlign(false)
 {
-    SetBackground();
-    SetPaintTransparent(sal_True);
+    // #i119307# use background, the graphic might have transparency
+    SetBackground(Wallpaper(Color(COL_WHITE)));
 }
 
 void BmpWindow::Paint( const Rectangle& )
@@ -2572,6 +2572,9 @@ void BmpWindow::Paint( const Rectangle& )
         aPntPos.X()--;
         aPntSz.Width()  *= -1;
     }
+
+    // #i119307# clear window background, the graphic might have transparency
+    Erase();
 
     if ( bGraphic )
         aGraphic.Draw( this, aPntPos, aPntSz );

commit 1caad856de1c1c5b0a8c31d2226416a2db9632cd

Resolves: fdo#80681 widget is not transparent
regression from 12dfca2ea61116665a7abefd43a9a4caa2b8920b
Author: Armin Le Grand <alg@apache.org>
Date:   Mon May 14 15:16:00 2012 +0000
    Resolves: #i119307# added deletion of sw's BmpWindow control...
    ...due to the fact that graphics may be transparent

Change-Id: I7eb2ee8ed53f0ef907a4573a735c71de43b6e8d3

sw/source/ui/frmdlg/frmpage.cxx
@@ -2606,8 +2606,12 @@ BmpWindow::BmpWindow(Window* pPar, WinBits nStyle)
     , bGraphic(false)
     , bLeftAlign(false)
 {
-    // #i119307# use background, the graphic might have transparency
-    SetBackground(Wallpaper(Color(COL_WHITE)));
+    SetBackground();
+    SetPaintTransparent(sal_True);
+    // #i119307# the graphic might have transparency, set up white as the color
+    // to use when drawing a rectangle under the image
+    SetLineColor(COL_WHITE);
+    SetFillColor(COL_WHITE);
 }
 
 Size BmpWindow::GetOptimalSize() const
@@ -2665,7 +2669,7 @@ void BmpWindow::Paint( const Rectangle& )
     }
 
     // #i119307# clear window background, the graphic might have transparency
-    Erase();
+    DrawRect(Rectangle(aPntPos, aPntSz));
 
     if ( bGraphic )
         aGraphic.Draw( this, aPntPos, aPntSz );