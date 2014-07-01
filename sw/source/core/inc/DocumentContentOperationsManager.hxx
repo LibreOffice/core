e87fe7ed0874b6001e1ad893f2be658470d4d23a
Jan-Marek Glogowski <glogow@fbihome.de>
2014-07-01 15:45:47 +0200

    Fix single node CopyRange
    Previously there was a restriction for the range to contain at
    least two nodes.
    
    Change-Id: Iadf21d3928303c2ab8df0d93dabd969ed1ff7e09

--- sw/source/core/inc/DocumentContentOperationsManager.hxx
+++ sw/source/core/inc/DocumentContentOperationsManager.hxx
@@ -106,11 +106,13 @@ public:
                             const SwPaM* pCopiedPaM = NULL,
                             bool bMakeNewFrms = true,
                             bool bDelRedlines = true,
-                            bool bCopyFlyAtFly = false ) const;
+                            bool bCopyFlyAtFly = false,
+                            const bool bMergedFirstNode = false ) const;
     void CopyFlyInFlyImpl(  const SwNodeRange& rRg,
                             const sal_Int32 nEndContentIndex,
                             const SwNodeIndex& rStartIdx,
-                            const bool bCopyFlyAtFly = false ) const;
+                            const bool bCopyFlyAtFly = false,
+                            const bool bMergedFirstNode = false ) const;
 
     /// Parameters for _Rst and lcl_SetTxtFmtColl
     //originallyfrom docfmt.cxx
