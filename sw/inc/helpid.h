775a86af2989ae0ed6873917ae2c2b727e161ff9
Caolán McNamara <caolanm@redhat.com>
2014-07-02 12:14:10 +0100

    drop unused helpids
    Change-Id: I6bab5d141e0891611ab0a9e1f4f1029bc53b845a

--- sw/inc/helpid.h
+++ sw/inc/helpid.h
@@ -365,8 +365,6 @@
 #define HID_MODULE_TOOLBOX                                      "SW_HID_MODULE_TOOLBOX"
 
 #define HID_MM_SELECTDBTABLEDDIALOG                             "SW_HID_MM_SELECTDBTABLEDDIALOG"
-#define HID_MM_DBTABLEPREVIEWDIALOG                             "SW_HID_MM_DBTABLEPREVIEWDIALOG"
-#define HID_MM_MAILBODY                                         "SW_HID_MM_MAILBODY"
 #define HID_MM_SENDMAILS                                        "SW_HID_MM_SENDMAILS"
 #define HID_MM_SENDMAILS_STATUSLB                               "SW_HID_MM_SENDMAILS_STATUSLB"
 #define HID_MAILMERGECHILD                                      "SW_HID_MAILMERGECHILD"
