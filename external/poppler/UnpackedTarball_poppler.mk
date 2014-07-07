9c95aabbbe01026cf9995d2b08aea49b1184528e
Tor Lillqvist <tml@collabora.com>
2014-07-07 22:15:26 +0300

    VS 2013 has fmin() and fmax()
    Change-Id: Iecb8e289d94c479447b80f1d5ebc21eb2210301b

--- external/poppler/UnpackedTarball_poppler.mk
+++ external/poppler/UnpackedTarball_poppler.mk
@@ -16,6 +16,7 @@ $(eval $(call gb_UnpackedTarball_add_patches,poppler,\
 	external/poppler/poppler-snprintf.patch.1 \
 	external/poppler/poppler-mac-fake.patch.1 \
 	external/poppler/poppler-nochecknew.patch.1 \
+	external/poppler/poppler-vs2013.patch.1 \
 ))
 
 # vim: set noet sw=4 ts=4:
