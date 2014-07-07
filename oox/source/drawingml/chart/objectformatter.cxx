1adced18837cb6c5aedca5cbaa28df53f618b369
Kohei Yoshida <kohei.yoshida@collabora.com>
2014-07-07 13:26:23 -0400

    Adjust for the splitting of number format properties in chart.
    Since 1d38cb365543924f9c50014e6b2227e77de1d0c9, "number format" and
    "link number format to source" properties are 2 separate properties.  Adjust
    OOXML import code for that split.
    
    Also, always set axis' number format via NumberFormat property even when it's
    a percent format.  The axis object doesn't keep a non-percent and percent
    number formats separately.
    
    Change-Id: I8667b6f1a78d88cc37d059518919ad1b37f154e1
    (cherry picked from commit af5a6615dfdbe5c2cacdcacb00fc6f418b925c06)

--- oox/source/drawingml/chart/objectformatter.cxx
+++ oox/source/drawingml/chart/objectformatter.cxx
@@ -1133,11 +1133,7 @@ void ObjectFormatter::convertNumberFormat( PropertySet& rPropSet, const NumberFo
     if( mxData->mxNumFmts.is() )
     {
         sal_Int32 nPropId = bPercentFormat ? PROP_PercentageNumberFormat : PROP_NumberFormat;
-        if( rNumberFormat.mbSourceLinked || rNumberFormat.maFormatCode.isEmpty() )
-        {
-            rPropSet.setAnyProperty( nPropId, Any() );
-        }
-        else try
+        try
         {
             sal_Int32 nIndex = rNumberFormat.maFormatCode.equalsIgnoreAsciiCase("general") ?
                 mxData->mxNumTypes->getStandardIndex( mxData->maFromLocale ) :
@@ -1150,6 +1146,8 @@ void ObjectFormatter::convertNumberFormat( PropertySet& rPropSet, const NumberFo
             OSL_FAIL( OStringBuffer( "ObjectFormatter::convertNumberFormat - cannot create number format '" ).
                 append( OUStringToOString( rNumberFormat.maFormatCode, osl_getThreadTextEncoding() ) ).append( '\'' ).getStr() );
         }
+
+        rPropSet.setProperty(PROP_LinkNumberFormatToSource, makeAny(rNumberFormat.mbSourceLinked));
     }
 }
 
