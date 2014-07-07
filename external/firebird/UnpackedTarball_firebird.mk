8159e3bd1a68c17e39cac10ecbb1ef880081c176
Tor Lillqvist <tml@collabora.com>
2014-07-07 23:14:31 +0300

    Adapt for VS 2013
    Change-Id: Icd7a3d0cbcb994986ea53a5fe91d3996b26cbe0c

--- external/firebird/UnpackedTarball_firebird.mk
+++ external/firebird/UnpackedTarball_firebird.mk
@@ -22,6 +22,7 @@ $(eval $(call gb_UnpackedTarball_add_patches,firebird,\
 ifeq ($(OS)-$(COM),WNT-MSC)
 $(eval $(call gb_UnpackedTarball_add_patches,firebird,\
 	external/firebird/firebird-cygwin-msvc.patch.1 \
+	external/firebird/firebird-vs2013.patch.1 \
 ))
 endif
 
