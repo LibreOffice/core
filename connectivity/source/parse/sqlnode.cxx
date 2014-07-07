4e549156ef63b08965ed0271b6506479483bef4c
Tor Lillqvist <tml@collabora.com>
2014-07-07 13:20:54 +0300

    WaE: enumeration values not handled in switch
    The previous commit to this file removed the default: case, so add it back
    (even if doing nothing now).
    
    Change-Id: Idc02818a30216dda0d64fddf21188de5766672e2

--- connectivity/source/parse/sqlnode.cxx
+++ connectivity/source/parse/sqlnode.cxx
@@ -1993,6 +1993,8 @@ void OSQLParseNode::negateSearchCondition(OSQLParseNode*& pSearchCondition, bool
             case SQL_NODE_NOTEQUAL:
                 pNewComparison = new OSQLParseNode(OUString("="),SQL_NODE_EQUAL,SQL_EQUAL);
                 break;
+            default:
+                break;
             }
             assert(pNewComparison && "OSQLParseNode::negateSearchCondition: unexpected node type!");
             pSearchCondition->replace(pComparison, pNewComparison);
