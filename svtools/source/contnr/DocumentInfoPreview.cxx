6a393f7843bc32b14d007ccdb41922f42afe0f3d
Markus Mohrhard <markus.mohrhard@collabora.co.uk>
2014-07-12 12:34:56 +0200

    pass by reference
    Change-Id: I44e8f40935a19f8023bb23f53c5fb458eb28a3dc

--- svtools/source/contnr/DocumentInfoPreview.cxx
+++ svtools/source/contnr/DocumentInfoPreview.cxx
@@ -54,7 +54,7 @@ ODocumentInfoPreview::ODocumentInfoPreview(Window * pParent, WinBits nBits):
 
 ODocumentInfoPreview::~ODocumentInfoPreview() {}
 
-extern "C" SAL_DLLPUBLIC_EXPORT Window* SAL_CALL makeODocumentInfoPreview(Window *pParent, VclBuilder::stringmap)
+extern "C" SAL_DLLPUBLIC_EXPORT Window* SAL_CALL makeODocumentInfoPreview(Window *pParent, VclBuilder::stringmap&)
 {
     return new ODocumentInfoPreview(pParent, WB_BORDER | WB_READONLY);
 }
