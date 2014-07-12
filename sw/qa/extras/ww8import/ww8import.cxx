LibreOffice/core

commit aa2faee4b3954b57a613963b501ecf611b3a14ca

CharBrd 9.4: DOC filters
-WW8 filter is the only MS filter which ignores padding
of character border.

Change-Id: I17973d8d2ae624d68356729334c701864fe0a10d

sw/source/filter/ww8/ww8par6.cxx
@@ -1290,14 +1290,14 @@ void GetLineIndex(SvxBoxItem &rBox, short nLineThickness, short nSpace, sal_uInt
 }
 
 void Set1Border(bool bVer67, SvxBoxItem &rBox, const WW8_BRC& rBor,
-    sal_uInt16 nOOIndex, sal_uInt16 nWWIndex, short *pSize=0)
+    sal_uInt16 nOOIndex, sal_uInt16 nWWIndex, short *pSize, const bool bIgnoreSpace)
 {
     sal_uInt8 nCol;
     short nSpace, nIdx;
     short nLineThickness = rBor.DetermineBorderProperties(bVer67,&nSpace,&nCol,
         &nIdx);
 
-    GetLineIndex(rBox, nLineThickness, nSpace, nCol, nIdx, nOOIndex, nWWIndex, pSize );
+    GetLineIndex(rBox, nLineThickness, bIgnoreSpace ? 0 : nSpace, nCol, nIdx, nOOIndex, nWWIndex, pSize );
 
 }
 
@@ -1365,7 +1365,7 @@ bool SwWW8ImplReader::SetBorder(SvxBoxItem& rBox, const WW8_BRC* pbrc,
         const WW8_BRC& rB = pbrc[ aIdArr[ i ] ];
         if( !rB.IsEmpty(bVer67))
         {
-            Set1Border(bVer67, rBox, rB, aIdArr[i+1], aIdArr[i], pSizeArray);
+            Set1Border(bVer67, rBox, rB, aIdArr[i+1], aIdArr[i], pSizeArray, false);
             bChange = true;
         }
         else if ( nSetBorders & (1 << aIdArr[i]) )
@@ -1389,18 +1389,18 @@ bool SwWW8ImplReader::SetBorder(SvxBoxItem& rBox, const WW8_BRC* pbrc,
 
 
 bool SwWW8ImplReader::SetShadow(SvxShadowItem& rShadow, const short *pSizeArray,
-    const WW8_BRC *pbrc) const
+    const WW8_BRC& aRightBrc) const
 {
     bool bRet = (
-                ( bVer67 ? (pbrc[WW8_RIGHT].aBits1[ 0 ] & 0x20 )
-                         : (pbrc[WW8_RIGHT].aBits2[ 1 ] & 0x20 ) )
+                ( bVer67 ? (aRightBrc.aBits1[ 0 ] & 0x20 )
+                         : (aRightBrc.aBits2[ 1 ] & 0x20 ) )
                 && (pSizeArray && pSizeArray[WW8_RIGHT])
                 );
     if (bRet)
     {
         rShadow.SetColor(Color(COL_BLACK));
     //i120718
-        short nVal = pbrc[WW8_RIGHT].DetermineBorderProperties(bVer67);
+        short nVal = aRightBrc.DetermineBorderProperties(bVer67);
     //End
         if (nVal < 0x10)
             nVal = 0x10;
@@ -1445,7 +1445,7 @@ bool SwWW8ImplReader::SetFlyBordersShadow(SfxItemSet& rFlySet,
 
         // fShadow
         SvxShadowItem aShadow( RES_SHADOW );
-        if( SetShadow( aShadow, pSizeArray, pbrc ))
+        if( SetShadow( aShadow, pSizeArray, pbrc[WW8_RIGHT] ))
         {
             bShadowed = true;
             rFlySet.Put( aShadow );
@@ -4648,7 +4648,7 @@ sal_uInt32 SwWW8ImplReader::ExtractColour(const sal_uInt8* &rpData, bool bVer67)
     return aShade.aColor.GetColor();
 }
 
-void SwWW8ImplReader::Read_Border(sal_uInt16 , const sal_uInt8* , short nLen)
+void SwWW8ImplReader::Read_Border(sal_uInt16 , const sal_uInt8*, short nLen)
 {
     if( nLen < 0 )
     {
@@ -4715,11 +4715,49 @@ void SwWW8ImplReader::Read_Border(sal_uInt16 , const sal_uInt8* , short nLen)
                 NewAttr( aBox );
 
                 SvxShadowItem aS(RES_SHADOW);
-                if( SetShadow( aS, &aSizeArray[0], aBrcs ) )
+                if( SetShadow( aS, &aSizeArray[0], aBrcs[WW8_RIGHT] ) )
                     NewAttr( aS );
             }
         }
     }
+}
+
+void SwWW8ImplReader::Read_CharBorder(sal_uInt16 /*nId*/, const sal_uInt8* pData, short nLen )
+{
+    //Ignore this old border type
+    if (!bVer67 && pPlcxMan && pPlcxMan->GetChpPLCF()->HasSprm(0xCA72))
+        return;
+
+    if( nLen < 0 )
+    {
+        pCtrlStck->SetAttr( *pPaM->GetPoint(), RES_CHRATR_BOX );
+        pCtrlStck->SetAttr( *pPaM->GetPoint(), RES_CHRATR_SHADOW );
+    }
+    else
+    {
+        const SvxBoxItem* pBox
+            = (const SvxBoxItem*)GetFmtAttr( RES_CHRATR_BOX );
+        if( pBox )
+        {
+            SvxBoxItem aBoxItem(RES_CHRATR_BOX);
+            aBoxItem = *pBox;
+            WW8_BRC aBrc;
+            _SetWW8_BRC(bVer67, aBrc, pData);
+
+            Set1Border(bVer67, aBoxItem, aBrc, BOX_LINE_TOP, 0, 0, true);
+            Set1Border(bVer67, aBoxItem, aBrc, BOX_LINE_BOTTOM, 0, 0, true);
+            Set1Border(bVer67, aBoxItem, aBrc, BOX_LINE_LEFT, 0, 0, true);
+            Set1Border(bVer67, aBoxItem, aBrc, BOX_LINE_RIGHT, 0, 0, true);
+            NewAttr( aBoxItem );
+
+            short aSizeArray[WW8_RIGHT+1]={0}; aSizeArray[WW8_RIGHT] = 1;
+            SvxShadowItem aShadowItem(RES_CHRATR_SHADOW);
+            if( SetShadow( aShadowItem, &aSizeArray[0], aBrc ) )
+                NewAttr( aShadowItem );
+        }
+    }
+
+
 }
 
 void SwWW8ImplReader::Read_Hyphenation( sal_uInt16, const sal_uInt8* pData, short nLen )
@@ -5802,7 +5840,7 @@ const wwSprmDispatcher *GetWW8SprmDispatcher()
                                                      //sttbRMark;short;
         {0x6864, 0},                                 //"sprmCDttmRMarkDel"
                                                      //chp.dttmRMarkDel;DTTM;long;
-        {0x6865, 0},                                 //"sprmCBrc" chp.brc;BRC;long;
+        {0x6865, &SwWW8ImplReader::Read_CharBorder}, //"sprmCBrc" chp.brc;BRC;long;
         {0x4866, &SwWW8ImplReader::Read_CharShadow}, //"sprmCShd" chp.shd;SHD;short;
         {0x4867, 0},                                 //"sprmCIdslRMarkDel"
                                                      //chp.idslRMReasonDel;an index

sw/qa/extras/ww8export/ww8export.cxx
@@ -11,6 +11,7 @@
 #include <com/sun/star/drawing/XControlShape.hpp>
 #include <com/sun/star/beans/XPropertySet.hpp>
 #include <com/sun/star/view/XViewSettingsSupplier.hpp>
+#include <com/sun/star/table/ShadowFormat.hpp>
 
 #include <swmodeltestbase.hxx>
 
@@ -25,6 +26,7 @@ public:
     void test56513();
     void testNewPageStylesTable();
     void testFdo42144();
+    void testCharacterBorder();
 
     CPPUNIT_TEST_SUITE(Test);
 #if !defined(MACOSX) && !defined(WNT)
@@ -47,6 +49,7 @@ void Test::run()
         {"fdo56513.doc", &Test::test56513},
         {"new-page-styles.doc", &Test::testNewPageStylesTable},
         {"fdo42144.odt", &Test::testFdo42144},
+        {"charborder.odt", &Test::testCharacterBorder},
     };
     header();
     for (unsigned int i = 0; i < SAL_N_ELEMENTS(aMethods); ++i)
@@ -142,6 +145,42 @@ void Test::testFdo42144()
     // Footer wasn't disabled -- instead empty footer was exported.
     uno::Reference<beans::XPropertySet> xStyle(getStyles("PageStyles")->getByName(DEFAULT_STYLE), uno::UNO_QUERY);
     CPPUNIT_ASSERT_EQUAL(false, bool(getProperty<sal_Bool>(xStyle, "FooterIsOn")));
+}
+
+void Test::testCharacterBorder()
+{
+    uno::Reference<beans::XPropertySet> xRun(getRun(getParagraph(1),1), uno::UNO_QUERY);
+    // WW8 has just one border attribute sprmCBrc for text border so all side has
+    // the same border
+    // Border
+    {
+        const table::BorderLine2 aTopBorder = getProperty<table::BorderLine2>(xRun,"CharTopBorder");
+        CPPUNIT_ASSERT_EQUAL_BORDER(table::BorderLine2(16711680,0,318,0,0,318), aTopBorder);
+        CPPUNIT_ASSERT_EQUAL_BORDER(aTopBorder, getProperty<table::BorderLine2>(xRun,"CharLeftBorder"));
+        CPPUNIT_ASSERT_EQUAL_BORDER(aTopBorder, getProperty<table::BorderLine2>(xRun,"CharBottomBorder"));
+        CPPUNIT_ASSERT_EQUAL_BORDER(aTopBorder, getProperty<table::BorderLine2>(xRun,"CharRightBorder"));
+    }
+
+    // Padding (dptSpace) it is constant 0
+    {
+        CPPUNIT_ASSERT_EQUAL(sal_Int32(0), getProperty<sal_Int32>(xRun,"CharTopBorderDistance"));
+        CPPUNIT_ASSERT_EQUAL(sal_Int32(0), getProperty<sal_Int32>(xRun,"CharLeftBorderDistance"));
+        CPPUNIT_ASSERT_EQUAL(sal_Int32(0), getProperty<sal_Int32>(xRun,"CharBottomBorderDistance"));
+        CPPUNIT_ASSERT_EQUAL(sal_Int32(0), getProperty<sal_Int32>(xRun,"CharRightBorderDistance"));
+    }
+
+    // Shadow (fShadow)
+    /* WW8 use just one bool value for shadow so the next conversions
+       are made during an export-import round
+       color: any -> black
+       location: any -> bottom-right
+       width: any -> border width */
+    {
+        const table::ShadowFormat aShadow = getProperty<table::ShadowFormat>(xRun, "CharShadowFormat");
+        CPPUNIT_ASSERT_EQUAL(COL_BLACK, sal_uInt32(aShadow.Color));
+        CPPUNIT_ASSERT_EQUAL(table::ShadowLocation_BOTTOM_RIGHT, aShadow.Location);
+        CPPUNIT_ASSERT_EQUAL(sal_Int16(318), aShadow.ShadowWidth);
+    }
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(Test);

sw/source/filter/ww8/wrtww8.hxx
@@ -950,8 +950,6 @@ private:
     static void BuildAnlvBase( WW8_ANLV& rAnlv, sal_uInt8*& rpCh, sal_uInt16& rCharLen,
                    const SwNumRule& rRul, const SwNumFmt& rFmt, sal_uInt8 nSwLevel );
 
-    void Out_BorderLine(ww::bytes& rO, const ::editeng::SvxBorderLine* pLine,
-        sal_uInt16 nDist, sal_uInt16 nSprmNo, bool bShadow);
 
     /// Output the numbering table.
     virtual void WriteNumbering();
@@ -1049,6 +1047,9 @@ public:
                                      const SwNode& rNd,
                                      const SwFmtPageDesc* pNewPgDescFmt = 0,
                                      const SwPageDesc* pNewPgDesc = 0 );
+
+    void Out_BorderLine(ww::bytes& rO, const ::editeng::SvxBorderLine* pLine,
+        sal_uInt16 nDist, sal_uInt16 nSprmNo, bool bShadow);
 
     void Out_SwFmtBox(const SvxBoxItem& rBox, bool bShadow);
     void Out_SwFmtTableBox( ww::bytes& rO, const SvxBoxItem * rBox );

sw/source/filter/ww8/ww8atr.cxx
@@ -1204,6 +1204,11 @@ void WW8AttributeOutput::CharHidden( const SvxCharHiddenItem& rHidden )
     OutputWW8Attribute( 7, rHidden.GetValue() );
 }
 
+void WW8AttributeOutput::CharBorder( const SvxBorderLine* pAllBorder, const sal_uInt16 /*nDist*/, const bool bShadow )
+{
+    m_rWW8Export.Out_BorderLine( *m_rWW8Export.pO, pAllBorder, 0, NS_sprm::LN_CBrc, bShadow );
+}
+
 void WW8AttributeOutput::CharUnderline( const SvxUnderlineItem& rUnderline )
 {
     if ( m_rWW8Export.bWrtWW8 )
@@ -5347,9 +5352,10 @@ void AttributeOutputBase::FormatCharBorder( const SvxBoxItem& rBox )
     if( pBorderLine )
     {
         const SfxPoolItem* pItem = GetExport().HasItem( RES_CHRATR_SHADOW );
+        const SvxShadowItem* pShadowItem = static_cast<const SvxShadowItem*>(pItem);
         const bool bShadow =
-            pItem &&
-            static_cast<const SvxShadowItem*>(pItem)->GetLocation() != SVX_SHADOW_NONE;
+            pShadowItem && pShadowItem->GetLocation() != SVX_SHADOW_NONE &&
+            pShadowItem->GetWidth() > 0;
 
         CharBorder( pBorderLine, nDist, bShadow );
     }

sw/source/filter/ww8/ww8attributeoutput.hxx
@@ -294,7 +294,7 @@ protected:
     virtual void CharHidden( const SvxCharHiddenItem& );
 
     /// Sfx item RES_CHRATR_BOX
-    virtual void CharBorder( const SvxBorderLine* /*pAllBorder*/, const sal_uInt16 /*nDist*/, const bool /*bShadow*/ ){};
+    virtual void CharBorder( const SvxBorderLine* pAllBorder, const sal_uInt16 nDist, const bool bShadow );
 
     /// Sfx item RES_TXTATR_INETFMT
     virtual void TextINetFormat( const SwFmtINetFmt& );

sw/source/filter/ww8/ww8par.hxx
@@ -1369,7 +1369,7 @@ private:
     void GetBorderDistance(const WW8_BRC* pbrc, Rectangle& rInnerDist) const;
     sal_uInt16 GetParagraphAutoSpace(bool fDontUseHTMLAutoSpacing);
     bool SetShadow(SvxShadowItem& rShadow, const short *pSizeArray,
-        const WW8_BRC *pbrc) const;
+    const WW8_BRC& aRightBrc) const;
     //returns true is a shadow was set
     bool SetFlyBordersShadow(SfxItemSet& rFlySet, const WW8_BRC *pbrc,
         short *SizeArray=0) const;
@@ -1673,6 +1673,7 @@ public:     // eigentlich private, geht aber leider nur public
     void Read_BoolItem(         sal_uInt16 nId, const sal_uInt8*, short nLen );
 
     void Read_Border(           sal_uInt16 nId, const sal_uInt8* pData, short nLen );
+    void Read_CharBorder(sal_uInt16 nId, const sal_uInt8* pData, short nLen );
     void Read_Tab(              sal_uInt16 nId, const sal_uInt8* pData, short nLen );
     void Read_Symbol(sal_uInt16, const sal_uInt8* pData, short nLen);
     void Read_FldVanish(        sal_uInt16 nId, const sal_uInt8* pData, short nLen );

commit 10841ba3cd9ff0a6f782189cfd39e3c87757bfdc

fdo#80333: .doc has large black rectangles between paragraphs
These black rectangles are character shadows, because checking
whether there is a border at all was missing. In Word there
is no shadow without a border.

Regression from aa2faee4b3954b57a613963b501ecf611b3a14ca

(cherry picked from commit c2e708d61746a259888c0e5fb1d3e9a43e3ca696)

Change-Id: Ib3cb4e904fdd33f215c81d7c02762a196f484b1b

sw/source/filter/ww8/ww8par6.cxx
@@ -4778,16 +4778,20 @@ void SwWW8ImplReader::Read_CharBorder(sal_uInt16 /*nId*/, const sal_uInt8* pData
             WW8_BRC aBrc;
             _SetWW8_BRC(bVer67, aBrc, pData);
 
-            Set1Border(bVer67, aBoxItem, aBrc, BOX_LINE_TOP, 0, 0, true);
-            Set1Border(bVer67, aBoxItem, aBrc, BOX_LINE_BOTTOM, 0, 0, true);
-            Set1Border(bVer67, aBoxItem, aBrc, BOX_LINE_LEFT, 0, 0, true);
-            Set1Border(bVer67, aBoxItem, aBrc, BOX_LINE_RIGHT, 0, 0, true);
-            NewAttr( aBoxItem );
+            // WW8_BRC is empty -> no border, no shadow
+            if( !aBrc.IsEmpty(bVer67) )
+            {
+                Set1Border(bVer67, aBoxItem, aBrc, BOX_LINE_TOP, 0, 0, true);
+                Set1Border(bVer67, aBoxItem, aBrc, BOX_LINE_BOTTOM, 0, 0, true);
+                Set1Border(bVer67, aBoxItem, aBrc, BOX_LINE_LEFT, 0, 0, true);
+                Set1Border(bVer67, aBoxItem, aBrc, BOX_LINE_RIGHT, 0, 0, true);
+                NewAttr( aBoxItem );
 
-            short aSizeArray[WW8_RIGHT+1]={0}; aSizeArray[WW8_RIGHT] = 1;
-            SvxShadowItem aShadowItem(RES_CHRATR_SHADOW);
-            if( SetShadow( aShadowItem, &aSizeArray[0], aBrc ) )
-                NewAttr( aShadowItem );
+                short aSizeArray[WW8_RIGHT+1]={0}; aSizeArray[WW8_RIGHT] = 1;
+                SvxShadowItem aShadowItem(RES_CHRATR_SHADOW);
+                if( SetShadow( aShadowItem, &aSizeArray[0], aBrc ) )
+                    NewAttr( aShadowItem );
+            }
         }
     }
 

sw/qa/extras/ww8import/ww8import.cxx
@@ -15,6 +15,7 @@
 #include <com/sun/star/text/XDependentTextField.hpp>
 #include <com/sun/star/text/XTextFramesSupplier.hpp>
 #include <com/sun/star/text/XTextTablesSupplier.hpp>
+#include <com/sun/star/table/ShadowFormat.hpp>
 
 #include <vcl/svapp.hxx>
 
@@ -323,6 +324,14 @@ DECLARE_WW8IMPORT_TEST(testFdo77844, "fdo77844.doc")
     headerIsOn = getProperty<bool>(pageStyle, "HeaderIsOn");
     CPPUNIT_ASSERT(!headerIsOn);
 #endif
+}
+
+DECLARE_WW8IMPORT_TEST(testFdp80333, "fdo80333.doc")
+{
+    // Despite there is no character border, border shadow was imported
+    uno::Reference<beans::XPropertySet> xRun(getRun(getParagraph(1),1), uno::UNO_QUERY);
+    const table::ShadowFormat aShadow = getProperty<table::ShadowFormat>(xRun, "CharShadowFormat");
+    CPPUNIT_ASSERT_EQUAL(table::ShadowLocation_NONE, aShadow.Location);
 }
 
 #endif