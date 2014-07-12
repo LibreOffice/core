717f8a87a7f009b821fd512c26cb35b2cac14993
Markus Mohrhard <markus.mohrhard@googlemail.com>
2014-07-12 12:59:47 +0200

    enable debug informations for gltf in debug builds
    Change-Id: I81fc7ebc77a67d2d64dd74e1dec0ce7ffbb1b19b

--- external/libgltf/ExternalProject_libgltf.mk
+++ external/libgltf/ExternalProject_libgltf.mk
@@ -59,7 +59,7 @@ $(call gb_ExternalProject_get_state_target,libgltf,build) :
 		export PKG_CONFIG="" \
 		&& ./configure \
 			--with-pic \
-			--disable-debug \
+			$(if $(filter TRUE,$(ENABLE_DEBUG)),--enable-debug,--disable-debug) \
 			--disable-werror \
 			BOOST_CFLAGS="$(if $(SYSTEM_BOOST),$(BOOST_CPPFLAGS),-I$(call gb_UnpackedTarball_get_dir,boost)) -I$(BUILDDIR)/config_$(gb_Side)" \
 			GLEW_CFLAGS="$(if $(SYSTEM_GLEW),$(GLEW_CFLAGS),-I$(call gb_UnpackedTarball_get_dir,glew)/include)" \
