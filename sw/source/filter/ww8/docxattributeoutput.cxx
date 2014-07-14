8d9dca2e6cdec98165acec18ac9ab78573072036
Caolán McNamara <caolanm@redhat.com>
2014-07-14 11:36:32 +0100

    Resolves: fdo#52226 swap in graphics on .docx and .rtf export
    (cherry picked from commit 9dd5caac62083f7162d83319284df68ee83e3777)
    
    Conflicts:
    	sw/source/filter/ww8/docxattributeoutput.cxx
    	sw/source/filter/ww8/rtfattributeoutput.cxx
    	sw/source/filter/ww8/wrtww8gr.cxx
    
    Change-Id: Ie818b382c0b17760c720ff2f2c73a3697989f97e

--- sw/source/filter/ww8/docxattributeoutput.cxx
+++ sw/source/filter/ww8/docxattributeoutput.cxx
@@ -2757,12 +2757,28 @@ void DocxAttributeOutput::FlyFrameGraphic( const SwGrfNode* pGrfNode, const Size
         // inline, we also have to write the image itself
         const Graphic* pGraphic = 0;
         if (pGrfNode)
-            pGraphic = &const_cast< Graphic& >( pGrfNode->GetGrf() );
+            pGraphic = &pGrfNode->GetGrf();
         else
             pGraphic = pOLENode->GetGraphic();
 
         m_rDrawingML.SetFS( m_pSerializer ); // to be sure that we write to the right stream
+
+        bool bSwapped = pGraphic->IsSwapOut();
+        if (bSwapped)
+        {
+            if (pGrfNode)
+            {
+                // always swapin via the Node
+                const_cast<SwGrfNode*>(pGrfNode)->SwapIn();
+            }
+            else
+                const_cast<Graphic*>(pGraphic)->SwapIn();
+        }
+
         OUString aImageId = m_rDrawingML.WriteImage( *pGraphic );
+
+        if (bSwapped)
+            const_cast<Graphic*>(pGraphic)->SwapOut();
 
         aRelId = OUStringToOString( aImageId, RTL_TEXTENCODING_UTF8 );
 
