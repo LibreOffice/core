cec7ea21217143e754e544382461a74375475a11
Palenik Mihály <palenik.mihaly@gmail.com>
2014-07-15 13:08:40 +0200

    Convert RID_OFADLG_OPTIONS_TREE to .ui
    Conflicts:
    	cui/source/inc/treeopt.hxx
    	cui/source/options/treeopt.cxx
    
    Change-Id: I971c7760e7252321aeca8a4f403515562a9057bb

--- cui/UIConfig_cui.mk
+++ cui/UIConfig_cui.mk
@@ -122,6 +122,7 @@ $(eval $(call gb_UIConfig_add_uifiles,cui,\
 	cui/uiconfig/ui/optfontspage \
 	cui/uiconfig/ui/optgeneralpage \
 	cui/uiconfig/ui/opthtmlpage \
+	cui/uiconfig/ui/optionsdialog \
 	cui/uiconfig/ui/optjsearchpage \
 	cui/uiconfig/ui/optlanguagespage \
 	cui/uiconfig/ui/optlingupage \
