5748a5abbebe5fd9c39f19b9c7235609d6667a0b
Miklos Vajna <vmiklos@collabora.co.uk>
2014-07-17 17:35:22 +0200

    Add CppunitTest_sw_ooxmlfieldexport.mk
    Change-Id: Ib3c26cf90a26db31a3d145beeecc877ef57ab249

---
+++ sw/CppunitTest_sw_ooxmlfieldexport.mk
@@ -0,0 +1,53 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#*************************************************************************
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+#*************************************************************************
+
+include $(SRCDIR)/sw/ooxmlexport_setup.mk
+
+$(eval $(call gb_CppunitTest_CppunitTest,sw_ooxmlfieldexport))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,sw_ooxmlfieldexport, \
+    sw/qa/extras/ooxmlexport/ooxmlfieldexport \
+))
+
+$(eval $(call gb_CppunitTest_use_libraries,sw_ooxmlfieldexport, \
+	$(sw_ooxmlexport_libraries) \
+))
+
+$(eval $(call gb_CppunitTest_use_externals,sw_ooxmlfieldexport,\
+	boost_headers \
+    libxml2 \
+))
+
+$(eval $(call gb_CppunitTest_set_include,sw_ooxmlfieldexport,\
+    -I$(SRCDIR)/sw/inc \
+    -I$(SRCDIR)/sw/source/core/inc \
+	-I$(SRCDIR)/sw/qa/extras/inc \
+    $$(INCLUDE) \
+))
+
+$(eval $(call gb_CppunitTest_use_api,sw_ooxmlfieldexport,\
+    offapi \
+    udkapi \
+))
+
+$(eval $(call gb_CppunitTest_use_ure,sw_ooxmlfieldexport))
+$(eval $(call gb_CppunitTest_use_vcl,sw_ooxmlfieldexport))
+
+$(eval $(call gb_CppunitTest_use_components,sw_ooxmlfieldexport,\
+	$(sw_ooxmlexport_components) \
+	filter/source/storagefilterdetect/storagefd \
+))
+
+$(eval $(call gb_CppunitTest_use_configuration,sw_ooxmlfieldexport))
+
+$(eval $(call gb_CppunitTest_use_unittest_configuration,sw_ooxmlfieldexport))
+
+# vim: set noet sw=4 ts=4:
