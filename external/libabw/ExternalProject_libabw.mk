5f7fd6fef5466cf1533d8cb7996dcfb23fdd8cfe
David Tardon <dtardon@redhat.com>
2014-07-17 17:27:05 +0200

    fdo#63756 build libxml2 with ICU support
    Change-Id: I0523e49e640812be435ba4c97b1881ca253eb2ab

--- external/libabw/ExternalProject_libabw.mk
+++ external/libabw/ExternalProject_libabw.mk
@@ -36,7 +36,6 @@ $(call gb_ExternalProject_get_state_target,libabw,build) :
 			$(if $(VERBOSE)$(verbose),--disable-silent-rules,--enable-silent-rules) \
 			CXXFLAGS="$(if $(SYSTEM_BOOST),$(BOOST_CPPFLAGS),\
 				-I$(call gb_UnpackedTarball_get_dir,boost) -I$(BUILDDIR)/config_$(gb_Side)) \
-			$(if $(SYSTEM_LIBXML),,-I$(call gb_UnpackedTarball_get_dir,xml2)/include)" \
 			$(if $(CROSS_COMPILING),--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM)) \
 		&& $(MAKE) \
 	)
