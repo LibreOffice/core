LibreOffice/core

commit ba9b63d8101197d3fd8612193b1ca188271dfc1a

fdo#76633: writerfilter: RTF import: do not leak the XShape of image
RTFSdrImport::resolve() is called for \picprop and creates an XShape
that is stored in RTFSdrImport::m_xShape and also
DomainMapper_Impl::m_aPendingShapes;
later RTFDocumentImpl::resolvePict() completely ignores that XShape
and creates a new one, which is also inserted in the document;
the first XShape is effectively leaked.

Try to avoid that by re-using the exising m_xShape in resolvePict().
Not sure if there are any problems with doing this, it's all a bit
confusing.

Change-Id: I98456242acb0766f547eb8f7d877f51d53323f3a

writerfilter/source/rtftok/rtfdocumentimpl.cxx
@@ -771,16 +771,29 @@ int RTFDocumentImpl::resolvePict(bool bInline)
 
     // Wrap it in an XShape.
     uno::Reference<drawing::XShape> xShape;
-    if (m_xModelFactory.is())
-        xShape.set(m_xModelFactory->createInstance("com.sun.star.drawing.GraphicObjectShape"), uno::UNO_QUERY);
-    uno::Reference<beans::XPropertySet> xPropertySet(xShape, uno::UNO_QUERY);
-    uno::Reference<drawing::XDrawPageSupplier> xDrawSupplier(m_xDstDoc, uno::UNO_QUERY);
-    if (xDrawSupplier.is())
+    xShape = m_pSdrImport->getCurrentShape();//Mapper().PopPendingShape();
+    if (xShape.is())
     {
-        uno::Reference< drawing::XShapes > xShapes(xDrawSupplier->getDrawPage(), uno::UNO_QUERY);
-        if (xShapes.is())
-            xShapes->add(xShape);
+        uno::Reference<lang::XServiceInfo> xSI(xShape, uno::UNO_QUERY_THROW);
+        assert(xSI->supportsService("com.sun.star.drawing.GraphicObjectShape"));
     }
+    else
+    {
+        if (m_xModelFactory.is())
+            xShape.set(m_xModelFactory->createInstance(
+                "com.sun.star.drawing.GraphicObjectShape"), uno::UNO_QUERY);
+        uno::Reference<drawing::XDrawPageSupplier> const xDrawSupplier(
+                m_xDstDoc, uno::UNO_QUERY);
+        if (xDrawSupplier.is())
+        {
+            uno::Reference<drawing::XShapes> xShapes(
+                    xDrawSupplier->getDrawPage(), uno::UNO_QUERY);
+            if (xShapes.is())
+                xShapes->add(xShape);
+        }
+    }
+
+    uno::Reference<beans::XPropertySet> xPropertySet(xShape, uno::UNO_QUERY);
 
     // check if the picture is in an OLE object and if the \objdata element is used
     // (see RTF_OBJECT in RTFDocumentImpl::dispatchDestination)

writerfilter/source/rtftok/rtfsdrimport.cxx
@@ -226,7 +226,10 @@ void RTFSdrImport::resolve(RTFShape& rShape, bool bClose,
     uno::Reference<drawing::XShape> xShape;
     uno::Reference<beans::XPropertySet> xPropertySet;
     // Create this early, as custom shapes may have properties before the type arrives.
-    createShape("com.sun.star.drawing.CustomShape", xShape, xPropertySet);
+    if (PICT == shapeOrPict)
+        createShape("com.sun.star.drawing.GraphicObjectShape", xShape, xPropertySet);
+    else
+        createShape("com.sun.star.drawing.CustomShape", xShape, xPropertySet);
     uno::Any aAny;
     beans::PropertyValue aPropertyValue;
     awt::Rectangle aViewBox;

writerfilter/source/rtftok/rtfsdrimport.hxx
@@ -43,6 +43,8 @@ public:
     void pushParent(css::uno::Reference<css::drawing::XShapes> xParent);
     /// Pop the current group shape from the parent stack.
     void popParent();
+    css::uno::Reference<css::drawing::XShape> const& getCurrentShape()
+        { return m_xShape; }
 private:
     void createShape(const OUString& aService, css::uno::Reference<css::drawing::XShape>& xShape, css::uno::Reference<css::beans::XPropertySet>& xPropertySet);
     void applyProperty(css::uno::Reference<css::drawing::XShape> xShape, const OUString& aKey, const OUString& aValue);

sw/qa/extras/rtfimport/rtfimport.cxx
@@ -577,14 +577,11 @@ DECLARE_RTFIMPORT_TEST(testFdo76633, "fdo76633.rtf")
     // check that there is only a graphic object, not an additional rectangle
     uno::Reference<lang::XServiceInfo> xShape(getShape(1), uno::UNO_QUERY);
     CPPUNIT_ASSERT(xShape.is());
-#if 0
-    // disabled - fails currently
     CPPUNIT_ASSERT(xShape->supportsService("com.sun.star.text.TextGraphicObject"));
     try {
         uno::Reference<drawing::XShape> xShape2(getShape(2), uno::UNO_QUERY);
         CPPUNIT_FAIL("exception expected");
     } catch (lang::IndexOutOfBoundsException const&) { /* expected */ }
-#endif
 }
 
 DECLARE_RTFIMPORT_TEST(testFdo48033, "fdo48033.rtf")

commit 2b9e782497cb962d9ca74a851a1389b0e29df49c

(related: fdo#79319) writerfilter: RTF import: fix crash on "fake" pict
The document has a \pict with {\sp{\sn shapeType}{\sv 1}}, i.e. it's
actually a rectangle shape; Word seems to ignore the picture data in
this case, so try to do the same.  Also consolidate the shape creation
in a new function RTFSdrImport::initShape().

(regression from ba9b63d8101197d3fd8612193b1ca188271dfc1a)

Change-Id: Iec94852ddc4c1ca3d8284119e6f1818a8dbb4149

writerfilter/source/rtftok/rtfdocumentimpl.cxx
@@ -4904,9 +4904,6 @@ int RTFDocumentImpl::popState()
     case DESTINATION_BOOKMARKEND:
         Mapper().props(lcl_getBookmarkProperties(m_aBookmarks[m_aStates.top().aDestinationText.makeStringAndClear()]));
         break;
-    case DESTINATION_PICT:
-        resolvePict(true, m_pSdrImport->getCurrentShape());
-        break;
     case DESTINATION_FORMFIELDNAME:
     {
         RTFValue::Pointer_t pValue(new RTFValue(m_aStates.top().aDestinationText.makeStringAndClear()));
@@ -5192,6 +5189,13 @@ int RTFDocumentImpl::popState()
                 Mapper().startShape(xShape);
             }
             Mapper().endShape();
+        }
+        break;
+    case DESTINATION_PICT:
+        // fdo#79319 ignore picture data if it's really a shape
+        if (!m_pSdrImport->isFakePict())
+        {
+            resolvePict(true, m_pSdrImport->getCurrentShape());
         }
         break;
     case DESTINATION_SHAPE:

writerfilter/source/rtftok/rtfsdrimport.cxx
@@ -50,8 +50,9 @@ namespace rtftok
 
 RTFSdrImport::RTFSdrImport(RTFDocumentImpl& rDocument,
                            uno::Reference<lang::XComponent> const& xDstDoc)
-    : m_rImport(rDocument),
-      m_bTextFrame(false)
+    : m_rImport(rDocument)
+    , m_bTextFrame(false)
+    , m_bFakePict(false)
 {
     uno::Reference<drawing::XDrawPageSupplier> xDrawings(xDstDoc, uno::UNO_QUERY);
     if (xDrawings.is())
@@ -216,20 +217,92 @@ void RTFSdrImport::applyProperty(uno::Reference<drawing::XShape> xShape, const O
     }
 }
 
+int RTFSdrImport::initShape(
+    uno::Reference<drawing::XShape> & o_xShape,
+    uno::Reference<beans::XPropertySet> & o_xPropSet,
+    bool & o_rIsCustomShape,
+    RTFShape const& rShape, bool const bClose, ShapeOrPict const shapeOrPict)
+{
+    assert(!o_xShape.is());
+    assert(!o_xPropSet.is());
+    o_rIsCustomShape = false;
+    m_bFakePict = false;
+
+    // first, find the shape type
+    int nType = -1;
+    std::vector< std::pair<OUString, OUString> >::const_iterator const iter(
+        std::find_if(rShape.aProperties.begin(),
+                     rShape.aProperties.end(),
+                     boost::bind(&OUString::equals,
+                         boost::bind(&std::pair<OUString, OUString>::first, _1),
+                         OUString("shapeType"))));
+
+    if (iter == rShape.aProperties.end())
+    {
+        if (SHAPE == shapeOrPict)
+        {
+            // The spec doesn't state what is the default for shapeType,
+            // Word seems to implement it as a rectangle.
+            nType = ESCHER_ShpInst_Rectangle;
+        }
+        else
+        {   // pict is picture by default but can be a rectangle too fdo#79319
+            nType = ESCHER_ShpInst_PictureFrame;
+        }
+    }
+    else
+    {
+        nType = iter->second.toInt32();
+        if (PICT == shapeOrPict && ESCHER_ShpInst_PictureFrame != nType)
+        {
+            m_bFakePict = true;
+        }
+    }
+
+    switch (nType)
+    {
+    case ESCHER_ShpInst_PictureFrame:
+        createShape("com.sun.star.drawing.GraphicObjectShape", o_xShape, o_xPropSet);
+        break;
+    case ESCHER_ShpInst_Line:
+        createShape("com.sun.star.drawing.LineShape", o_xShape, o_xPropSet);
+        break;
+    case ESCHER_ShpInst_Rectangle:
+    case ESCHER_ShpInst_TextBox:
+        // If we're inside a groupshape, can't use text frames.
+        if (!bClose && m_aParents.size() == 1)
+        {
+            createShape("com.sun.star.text.TextFrame", o_xShape, o_xPropSet);
+            m_bTextFrame = true;
+            std::vector<beans::PropertyValue> aDefaults = getTextFrameDefaults(true);
+            for (size_t j = 0; j < aDefaults.size(); ++j)
+                o_xPropSet->setPropertyValue(aDefaults[j].Name, aDefaults[j].Value);
+            break;
+        }
+        // fall-through intended
+    default:
+        createShape("com.sun.star.drawing.CustomShape", o_xShape, o_xPropSet);
+        o_rIsCustomShape = true;
+        break;
+    }
+
+    // Defaults
+    if (o_xPropSet.is() && !m_bTextFrame)
+    {
+        o_xPropSet->setPropertyValue("FillColor", uno::makeAny(sal_uInt32(
+                        0xffffff))); // White in Word, kind of blue in Writer.
+    }
+
+    return nType;
+}
+
 void RTFSdrImport::resolve(RTFShape& rShape, bool bClose, ShapeOrPict const shapeOrPict)
 {
-    int nType = -1;
     bool bPib = false;
-    bool bCustom = false;
     m_bTextFrame = false;
 
     uno::Reference<drawing::XShape> xShape;
     uno::Reference<beans::XPropertySet> xPropertySet;
-    // Create this early, as custom shapes may have properties before the type arrives.
-    if (PICT == shapeOrPict)
-        createShape("com.sun.star.drawing.GraphicObjectShape", xShape, xPropertySet);
-    else
-        createShape("com.sun.star.drawing.CustomShape", xShape, xPropertySet);
     uno::Any aAny;
     beans::PropertyValue aPropertyValue;
     awt::Rectangle aViewBox;
@@ -253,53 +326,16 @@ void RTFSdrImport::resolve(RTFShape& rShape, bool bClose, ShapeOrPict const shap
     sal_Int16 nRelativeWidthRelation = text::RelOrientation::PAGE_FRAME;
     sal_Int16 nRelativeHeightRelation = text::RelOrientation::PAGE_FRAME;
 
-    if (SHAPE == shapeOrPict)
-    {
-        // The spec doesn't state what is the default for shapeType, Word seems to implement it as a rectangle.
-        if (std::find_if(rShape.aProperties.begin(),
-                         rShape.aProperties.end(),
-                         boost::bind(&OUString::equals, boost::bind(&std::pair<OUString, OUString>::first, _1), OUString("shapeType")))
-                == rShape.aProperties.end())
-            rShape.aProperties.insert(rShape.aProperties.begin(), std::pair<OUString, OUString>("shapeType", OUString::number(ESCHER_ShpInst_Rectangle)));
-    }
+    bool bCustom(false);
+    int const nType =
+        initShape(xShape, xPropertySet, bCustom, rShape, bClose, shapeOrPict);
 
     for (std::vector< std::pair<OUString, OUString> >::iterator i = rShape.aProperties.begin();
             i != rShape.aProperties.end(); ++i)
     {
         if (i->first == "shapeType")
         {
-            nType = i->second.toInt32();
-            switch (nType)
-            {
-            case ESCHER_ShpInst_PictureFrame:
-                createShape("com.sun.star.drawing.GraphicObjectShape", xShape, xPropertySet);
-                break;
-            case ESCHER_ShpInst_Line:
-                createShape("com.sun.star.drawing.LineShape", xShape, xPropertySet);
-                break;
-            case ESCHER_ShpInst_Rectangle:
-            case ESCHER_ShpInst_TextBox:
-                // If we're inside a groupshape, can't use text frames.
-                if (!bClose && m_aParents.size() == 1)
-                {
-                    createShape("com.sun.star.text.TextFrame", xShape, xPropertySet);
-                    m_bTextFrame = true;
-                    std::vector<beans::PropertyValue> aDefaults = getTextFrameDefaults(true);
-                    for (size_t j = 0; j < aDefaults.size(); ++j)
-                        xPropertySet->setPropertyValue(aDefaults[j].Name, aDefaults[j].Value);
-                }
-                else
-                    bCustom = true;
-                break;
-            default:
-                bCustom = true;
-                break;
-            }
-
-            // Defaults
-            aAny <<= (sal_uInt32)0xffffff; // White in Word, kind of blue in Writer.
-            if (xPropertySet.is() && !m_bTextFrame)
-                xPropertySet->setPropertyValue("FillColor", aAny);
+            continue; // ignore: already handled by initShape
         }
         else if (i->first == "wzName")
         {

writerfilter/source/rtftok/rtfsdrimport.hxx
@@ -47,15 +47,23 @@ public:
     {
         return m_xShape;
     }
+    bool isFakePict() { return m_bFakePict; }
 private:
     void createShape(const OUString& aService, css::uno::Reference<css::drawing::XShape>& xShape, css::uno::Reference<css::beans::XPropertySet>& xPropertySet);
     void applyProperty(css::uno::Reference<css::drawing::XShape> xShape, const OUString& aKey, const OUString& aValue);
+    int initShape(
+        css::uno::Reference<css::drawing::XShape> & o_xShape,
+        css::uno::Reference<css::beans::XPropertySet> & o_xPropSet,
+        bool & o_rIsCustomShape,
+        RTFShape const& rShape, bool bClose, ShapeOrPict const shapeOrPict);
 
     RTFDocumentImpl& m_rImport;
     std::stack< css::uno::Reference<css::drawing::XShapes> > m_aParents;
     css::uno::Reference<css::drawing::XShape> m_xShape;
     /// If m_xShape is imported as a Writer text frame (instead of a drawinglayer rectangle).
     bool m_bTextFrame;
+    /// if inside \pict, but actually it's a shape (not a picture)
+    bool m_bFakePict;
 };
 } // namespace rtftok
 } // namespace writerfilter