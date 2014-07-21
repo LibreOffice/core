d28e19069c065ba74f31834142f7eca88a25c9e2
Andrzej Hunt <andrzej.hunt@collabora.com>
2014-07-21 19:36:11 +0200

    Use gb_CppunitTest_use_confpreinit for LOK tiled rendering.
    Now that we use the event loop for LOK we need to make sure that
    it is able to run successfully first time, as unit tests start
    with a completely clean config.
    
    Change-Id: If9ddcb75fe8286381d71ecdc76ac7eb3697960d0

--- libreofficekit/CppunitTest_libreofficekit_tiledrendering.mk
+++ libreofficekit/CppunitTest_libreofficekit_tiledrendering.mk
@@ -29,7 +29,6 @@ $(eval $(call gb_CppunitTest_use_libraries,libreofficekit_tiledrendering, \
 	$(gb_UWINAPI) \
 ))
 
-
 # 	unotest \
 
 ifeq ($(OS),LINUX)
@@ -53,5 +52,7 @@ $(eval $(call gb_CppunitTest_use_ure,libreofficekit_tiledrendering))
 $(eval $(call gb_CppunitTest_use_rdb,libreofficekit_tiledrendering,services))
 
 $(eval $(call gb_CppunitTest_use_configuration,libreofficekit_tiledrendering))
+
+$(eval $(call gb_CppunitTest_use_confpreinit,libreofficekit_tiledrendering))
 
 # vim: set noet sw=4 ts=4:
