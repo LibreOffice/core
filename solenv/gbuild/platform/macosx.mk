c952224dcd73fa5dbc5738e49bb71880de5b020b
Douglas Mencken <dougmencken@gmail.com>
2014-07-22 09:18:44 -0400

    OSX: change CXXFLAGS_CXX11 to be used not only with clang
    Otherwise, zillions of warnings are displayed like:
      "defaulted and deleted functions only available with -std=c++11 or -std=gnu++11"
      "override controls (override/final) only available with -std=c++11 or -std=gnu++11"
    
    Change-Id: Icedaf55c4f301d44587141ad937abbfce8d10457

--- solenv/gbuild/platform/macosx.mk
+++ solenv/gbuild/platform/macosx.mk
@@ -56,7 +56,8 @@ gb_CXXFLAGS := \
 	-Wno-non-virtual-dtor \
 	-fno-strict-aliasing \
 	-fsigned-char \
-	$(if $(filter TRUE,$(COM_GCC_IS_CLANG)),$(CXXFLAGS_CXX11),-malign-natural) \
+	$(if $(filter TRUE,$(HAVE_CXX11)),$(CXXFLAGS_CXX11)) \
+	$(if $(filter-out TRUE,$(COM_GCC_IS_CLANG)),-malign-natural)
 
 	#-Wshadow \ break in compiler headers already
 	#-fsigned-char \ might be removed?
