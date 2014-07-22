LibreOffice/core

commit 5089bdb234a93bcb62b597c18b8ab66048246b13

Related: fdo#38838 remove UniString::EqualsAscii
Change-Id: I55c85c1c44452c7fb6ac40591aea7177d054affe

sw/source/filter/html/htmlcss1.cxx
@@ -1692,7 +1692,7 @@ _HTMLAttr **SwHTMLParser::GetAttrTabEntry( sal_uInt16 nWhich )
 
 void SwHTMLParser::NewStyle()
 {
-    String sType;
+    OUString sType;
 
     const HTMLOptions& rOptions2 = GetOptions();
     for (size_t i = rOptions2.size(); i; )
@@ -1702,8 +1702,8 @@ void SwHTMLParser::NewStyle()
             sType = rOption.GetString();
     }
 
-    bIgnoreRawData = sType.Len() &&
-                     !sType.GetToken(0,';').EqualsAscii(sCSS_mimetype);
+    bIgnoreRawData = !sType.getLength() &&
+                     !sType.getToken(0,';').equalsAscii(sCSS_mimetype);
 }
 
 void SwHTMLParser::EndStyle()
@@ -1778,8 +1778,7 @@ void SwHTMLParser::InsertLink()
     }
     else
     {
-        OUString sRel;
-        String sHRef, sType;
+        OUString sRel, sHRef, sType;
 
         const HTMLOptions& rOptions2 = GetOptions();
         for (size_t i = rOptions2.size(); i; )
@@ -1799,9 +1798,9 @@ void SwHTMLParser::InsertLink()
             }
         }
 
-        if( sHRef.Len() && sRel.equalsIgnoreAsciiCase( "STYLESHEET" ) &&
-            ( !sType.Len() ||
-              sType.GetToken(0,';').EqualsAscii(sCSS_mimetype) ) )
+        if( !sHRef.isEmpty() && sRel.equalsIgnoreAsciiCase( "STYLESHEET" ) &&
+            ( sType.isEmpty() ||
+              sType.getToken(0,';').equalsAscii(sCSS_mimetype) ) )
         {
             if( GetMedium() )
             {

comphelper/source/misc/string.cxx
@@ -417,6 +417,38 @@ sal_Int32 indexOfAny(OUString const& rIn,
     return -1;
 }
 
+OUString setToken(const OUString& rIn, sal_Int32 nToken, sal_Unicode cTok,
+    const OUString& rNewToken)
+{
+    const sal_Unicode* pStr = rIn.getStr();
+    sal_Int32 nLen = rIn.getLength();
+    sal_Int32 nTok = 0;
+    sal_Int32 nFirstChar = 0;
+    sal_Int32 i = 0;
+
+    // Determine token position and length
+    while ( i < nLen )
+    {
+        // Increase token count if match
+        if (*pStr == cTok)
+        {
+            ++nTok;
+
+            if (nTok == nToken)
+                nFirstChar = i+1;
+            else if (nTok > nToken)
+                break;
+        }
+
+        ++pStr,
+        ++i;
+    }
+
+    if (nTok >= nToken)
+        return rIn.replaceAt(nFirstChar, i-nFirstChar, rNewToken);
+    return rIn;
+}
+
 } }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

include/comphelper/string.hxx
@@ -285,6 +285,17 @@ COMPHELPER_DLLPUBLIC inline OUStringBuffer& padToLength(
     return detail::padToLength(rBuffer, nLength, cFill);
 }
 
+/** Replace a token in a string
+    @param rIn       OUString in which the token is to be replaced
+    @param nToken    which nToken to replace
+    @param cTok      token delimiter
+    @param rNewToken replacement token
+
+    @return original string with token nToken replaced by rNewToken
+ */
+COMPHELPER_DLLPUBLIC OUString setToken(const OUString& rIn, sal_Int32 nToken, sal_Unicode cTok,
+    const OUString& rNewToken);
+
 /** Find any of a list of code units in the string.
     @param rIn      OUString to search
     @param pChars   0-terminated array of sal_Unicode code units to search for

include/tools/string.hxx
@@ -230,11 +230,8 @@ public:
     StringCompare       CompareToAscii( const sal_Char* pAsciiStr,
                                         xub_StrLen nLen = STRING_LEN ) const;
     sal_Bool            Equals( const UniString& rStr ) const;
-    sal_Bool            EqualsAscii( const sal_Char* pAsciiStr ) const;
     sal_Bool            Equals( const UniString& rStr,
                                 xub_StrLen nIndex, xub_StrLen nLen ) const;
-    sal_Bool            EqualsAscii( const sal_Char* pAsciiStr,
-                                     xub_StrLen nIndex, xub_StrLen nLen ) const;
 
     xub_StrLen          Match( const UniString& rStr ) const;
 

sw/inc/iodetect.hxx
@@ -46,20 +46,19 @@
 
 struct SwIoDetect
 {
-    const sal_Char* pName;
-    sal_uInt16 nLen;
+    const OUString sName;
 
-    inline SwIoDetect( const sal_Char *pN, sal_uInt16 nL )
-        : pName( pN ), nLen( nL )
-    {}
-
-    inline int IsFilter( const String& rNm ) const
+    SwIoDetect(const OUString &rN)
+        : sName(rN)
     {
-        return pName && rNm.EqualsAscii( pName, 0, nLen );
     }
 
-    const sal_Char* IsReader( const sal_Char* pHeader, sal_uLong nLen_,
-            const String &rFileName, const String& rUserData ) const;
+    bool IsFilter( const OUString& rNm ) const
+    {
+        return rNm.startsWith(sName);
+    }
+
+    OUString IsReader(const sal_Char* pHeader, sal_uLong nLen_) const;
 };
 
 enum ReaderWriterEnum {

sw/inc/shellres.hxx
@@ -57,7 +57,7 @@ struct SW_DLLPUBLIC ShellResource : public Resource
 
     //Names of TOXs.
     String          aTOXIndexName;
-    String          aTOXUserName;
+    OUString        aTOXUserName;
     String          aTOXContentName;
     String          aTOXIllustrationsName;
     String          aTOXObjectsName;

sw/source/core/unocore/unofield.cxx
@@ -2590,44 +2590,40 @@ SwXTextFieldMasters::~SwXTextFieldMasters()
     Falls wir grosszuegig werden wollen, dann koennte man com.sun.star.text
     auch optional weglassen
   -----------------------------------------------------------------------*/
-static sal_uInt16 lcl_GetIdByName( String& rName, String& rTypeName )
+static sal_uInt16 lcl_GetIdByName( OUString& rName, OUString& rTypeName )
 {
-    if( rName.EqualsAscii( COM_TEXT_FLDMASTER, 0, RTL_CONSTASCII_LENGTH(COM_TEXT_FLDMASTER ))
-        ||  rName.EqualsAscii( COM_TEXT_FLDMASTER_CC, 0, RTL_CONSTASCII_LENGTH(COM_TEXT_FLDMASTER_CC )))
-        rName.Erase(0, 30);
+    if (rName.startsWith(COM_TEXT_FLDMASTER) || rName.startsWith(COM_TEXT_FLDMASTER_CC))
+        rName = rName.copy(30);
 
     sal_uInt16 nResId = USHRT_MAX;
     sal_Int32 nFound = 0;
-    rTypeName = rName.GetToken( 0, '.', nFound );
-    if(rTypeName.EqualsAscii("User"))
+    rTypeName = rName.getToken( 0, '.', nFound );
+    if (rTypeName == "User")
         nResId = RES_USERFLD;
-    else if(rTypeName.EqualsAscii("DDE"))
+    else if (rTypeName == "DDE")
         nResId = RES_DDEFLD;
-    else if(rTypeName.EqualsAscii("SetExpression"))
+    else if (rTypeName == "SetExpression")
     {
         nResId = RES_SETEXPFLD;
 
-        String sFldTypName( rName.GetToken( 1, '.' ));
+        String sFldTypName( rName.getToken( 1, '.' ));
         String sUIName( SwStyleNameMapper::GetSpecialExtraUIName( sFldTypName ) );
 
         if( sUIName != sFldTypName )
-            rName.SetToken( 1, '.', sUIName );
+            rName = comphelper::string::setToken(rName, 1, '.', sUIName);
     }
-    else if(rTypeName.EqualsAscii("DataBase"))
+    else if (rTypeName == "DataBase")
     {
-        rName.Erase( 0, RTL_CONSTASCII_LENGTH( "DataBase." ));
+        rName = rName.copy(RTL_CONSTASCII_LENGTH("DataBase."));
         sal_uInt16 nDotCount = comphelper::string::getTokenCount(rName, '.');
         if( 2 <= nDotCount )
         {
             // #i51815#
-            //rName.SearchAndReplace('.', DB_DELIM);
-            //rName.SetChar( rName.SearchBackward( '.' ), DB_DELIM );
-
             rName = OUString("DataBase.") + rName;
             nResId = RES_DBFLD;
         }
     }
-    else if( rTypeName.EqualsAscii("Bibliography"))
+    else if (rTypeName == "Bibliography")
         nResId = RES_AUTHORITY;
     return nResId;
 }
@@ -2639,12 +2635,12 @@ uno::Any SwXTextFieldMasters::getByName(const OUString& rName)
     if(!GetDoc())
         throw uno::RuntimeException();
 
-    String sName(rName), sTypeName;
+    OUString sName(rName), sTypeName;
     sal_uInt16 nResId = lcl_GetIdByName( sName, sTypeName );
     if( USHRT_MAX == nResId )
         throw container::NoSuchElementException();
 
-    sName.Erase(0, sTypeName.Len()+1);
+    sName = sName.copy(sTypeName.getLength()+1);
     SwFieldType* pType = GetDoc()->GetFldType(nResId, sName, sal_True);
     if(!pType)
         throw container::NoSuchElementException();
@@ -2726,12 +2722,12 @@ sal_Bool SwXTextFieldMasters::hasByName(const OUString& rName) throw( uno::Runti
     if(!GetDoc())
         throw uno::RuntimeException();
 
-    String sName(rName), sTypeName;
+    OUString sName(rName), sTypeName;
     sal_uInt16 nResId = lcl_GetIdByName( sName, sTypeName );
     sal_Bool bRet = sal_False;
     if( USHRT_MAX != nResId )
     {
-        sName.Erase(0, sTypeName.Len()+1);
+        sName = sName.copy(sTypeName.getLength()+1);
         bRet = USHRT_MAX != nResId && 0 != GetDoc()->GetFldType(nResId, sName, sal_True);
     }
     return bRet;

sw/source/core/unocore/unoidx.cxx
@@ -160,11 +160,11 @@ static void
 lcl_ConvertTOUNameToUserName(OUString& rTmp)
 {
     ShellResource* pShellRes = ViewShell::GetShellRes();
-    if(rTmp.equalsAscii(cUserDefined))
+    if (rTmp.equalsAscii(cUserDefined))
     {
         rTmp = pShellRes->aTOXUserName;
     }
-    else if(!pShellRes->aTOXUserName.EqualsAscii(cUserDefined) &&
+    else if (!pShellRes->aTOXUserName.equalsAscii(cUserDefined) &&
         USER_AND_SUFFIXLEN == rTmp.getLength())
     {
         //make sure that in non-English versions the " (user)" suffix is removed

commit 7dff37c65e08985b80ac79e13f5571819a8e739d

fdo#81276: sw: HTML import: fix inverted test in CSS import
(regression from 5089bdb234a93bcb62b597c18b8ab66048246b13)

Change-Id: I5b45c4c47ba66385b2b528b5a3039bda37b060d6

sw/source/filter/html/htmlcss1.cxx
@@ -1688,7 +1688,7 @@ void SwHTMLParser::NewStyle()
             sType = rOption.GetString();
     }
 
-    bIgnoreRawData = !sType.getLength() &&
+    bIgnoreRawData = sType.getLength() &&
                      !sType.getToken(0,';').equalsAscii(sCSS_mimetype);
 }
 

sw/qa/extras/htmlexport/data/fdo81276.html
@@ -1,0 +1,21 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html lang="ru-ru" >
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <style>
+
+    @page { size: 29.7cm 21cm;
+            margin-left: 0.5cm;
+            margin-right: 0.5cm;
+            margin-top: 2cm;
+            margin-bottom: 0.5cm
+    }
+
+    </style>
+</head>
+
+<body>
+
+</body>
+</html>

sw/qa/extras/htmlexport/htmlexport.cxx
@@ -72,6 +72,19 @@ private:
 
 #define DECLARE_HTMLEXPORT_ROUNDTRIP_TEST(TestName, filename) DECLARE_SW_ROUNDTRIP_TEST(TestName, filename, HtmlExportTest)
 
+DECLARE_HTMLEXPORT_ROUNDTRIP_TEST(testFdo81276, "fdo81276.html")
+{
+    uno::Reference<container::XNameAccess> xPageStyles(getStyles("PageStyles"));
+    uno::Reference<beans::XPropertySet> xStyle(xPageStyles->getByName("HTML"), uno::UNO_QUERY);
+    // some rounding going on here?
+    CPPUNIT_ASSERT(abs(sal_Int32(29700) - getProperty<sal_Int32>(xStyle, "Width")) < 10);
+    CPPUNIT_ASSERT(abs(sal_Int32(21006) - getProperty<sal_Int32>(xStyle, "Height")) < 10);
+    CPPUNIT_ASSERT(abs(sal_Int32(500) - getProperty<sal_Int32>(xStyle, "LeftMargin")) < 10);
+    CPPUNIT_ASSERT(abs(sal_Int32(500) - getProperty<sal_Int32>(xStyle, "RightMargin")) < 10);
+    CPPUNIT_ASSERT(abs(sal_Int32(2000) - getProperty<sal_Int32>(xStyle, "TopMargin")) < 10);
+    CPPUNIT_ASSERT(abs(sal_Int32(500) - getProperty<sal_Int32>(xStyle, "BottomMargin")) < 10);
+}
+
 DECLARE_HTMLEXPORT_ROUNDTRIP_TEST(testFdo62336, "fdo62336.docx")
 {
     // The problem was essentially a crash during table export as docx/rtf/html