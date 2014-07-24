LibreOffice/core

commit 6ecfbf153a7c11c63d208630ac6a27064058099d

drop unused resources and toggle label to activate replacement widget
Change-Id: I4a608c3b8163b9e171cd73dc7921d16e7dbfa3a1

sfx2/source/dialog/mgetempl.cxx
@@ -35,7 +35,6 @@
 
 #include <sfx2/sfx.hrc>
 #include "dialog.hrc"
-#include "mgetempl.hrc"
 
 #include <svl/style.hrc>
 
@@ -58,10 +57,13 @@ SfxManageStyleSheetPage::SfxManageStyleSheetPage(Window* pParent, const SfxItemS
     get(m_pAutoCB, "autoupdate");
     get(m_pFollowFt, "nextstyleft");
     get(m_pFollowLb, "nextstyle");
+    m_pFollowLb->SetStyle(m_pFollowLb->GetStyle() | WB_SORT);
     get(m_pBaseFt, "linkedwithft");
     get(m_pBaseLb, "linkedwith");
+    m_pBaseLb->SetStyle(m_pBaseLb->GetStyle() | WB_SORT);
     get(m_pFilterFt, "categoryft");
     get(m_pFilterLb, "category");
+    m_pFilterLb->SetStyle(m_pFilterLb->GetStyle() | WB_SORT);
     get(m_pDescFt, "desc");
 
     // this Page needs ExchangeSupport
@@ -109,8 +111,10 @@ SfxManageStyleSheetPage::SfxManageStyleSheetPage(Window* pParent, const SfxItemS
     if (!pStyle->IsUserDefined())
     {
         m_pNameRo->SetText(m_pNameRw->GetText());
-        m_pNameRo->Show();
         m_pNameRw->Hide();
+        m_pNameRo->Show();
+        FixedText *pLabel = get<FixedText>("nameft");
+        pLabel->set_mnemonic_widget(m_pNameRo);
     }
 
     if ( pStyle->HasFollowSupport() && pPool )

sfx2/AllLangResTarget_sfx2.mk
@@ -56,7 +56,6 @@ $(eval $(call gb_SrsTarget_add_files,sfx/res,\
     sfx2/source/dialog/filedlghelper.src \
     sfx2/source/dialog/inputdlg.src \
     sfx2/source/dialog/mailwindow.src \
-    sfx2/source/dialog/mgetempl.src \
     sfx2/source/dialog/newstyle.src \
     sfx2/source/dialog/recfloat.src \
     sfx2/source/dialog/srchdlg.src \

sfx2/inc/sfx2/sfx.hrc
@@ -131,7 +131,7 @@
 #define RID_APPTITLE                        (RID_SFX_START+4)
 
 #define DLG_DOCINFO_EDT                     (RID_SFX_START+6)
-#define TP_MANAGE_STYLES                    (RID_SFX_START+7)
+
 #define DLG_STYLE_DESIGNER                  (RID_SFX_START+8)
 
 #define RID_SFX_INCORRECT_PASSWORD          (RID_SFX_START+216)

sfx2/source/dialog/mgetempl.hrc
@@ -1,33 +1,0 @@
-/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
-/*
- * This file is part of the LibreOffice project.
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- *
- * This file incorporates work covered by the following license notice:
- *
- *   Licensed to the Apache Software Foundation (ASF) under one or more
- *   contributor license agreements. See the NOTICE file distributed
- *   with this work for additional information regarding copyright
- *   ownership. The ASF licenses this file to you under the Apache
- *   License, Version 2.0 (the "License"); you may not use this file
- *   except in compliance with the License. You may obtain a copy of
- *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
- */
-#define ED_NAME 2
-#define MLE_NAME 13
-#define LB_NEXT 4
-#define FT_BASE 5
-#define LB_BASE 6
-#define LB_REGION 8
-#define FT_DESC 9
-#define GB_DESC 10
-#define FT_REGION 7
-#define FT_NEXT 3
-#define FT_NAME 1
-#define ED_DESC 11
-#define CB_AUTO 12
-
-/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sfx2/source/dialog/mgetempl.src
@@ -1,121 +1,0 @@
-/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
-/*
- * This file is part of the LibreOffice project.
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- *
- * This file incorporates work covered by the following license notice:
- *
- *   Licensed to the Apache Software Foundation (ASF) under one or more
- *   contributor license agreements. See the NOTICE file distributed
- *   with this work for additional information regarding copyright
- *   ownership. The ASF licenses this file to you under the Apache
- *   License, Version 2.0 (the "License"); you may not use this file
- *   except in compliance with the License. You may obtain a copy of
- *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
- */
-
-#include <sfx2/sfx.hrc>
-#include <sfx2/tabpage.hrc>
-#include "helpid.hrc"
-#include "mgetempl.hrc"
-
- // TP_MANAGE_STYLES ------------------------------------------------------
-TabPage TP_MANAGE_STYLES
-{
-    HelpId = HID_MANAGE_STYLES ;
-    Hide = TRUE ;
-    Size = MAP_APPFONT ( TP_WIDTH , TP_HEIGHT ) ;
-    FixedText FT_NAME
-    {
-        Pos = MAP_APPFONT ( 6 , 8 ) ;
-        Size = MAP_APPFONT ( 61 , 10 ) ;
-        Text [ en-US ] = "~Name" ;
-    };
-    Edit ED_NAME
-    {
-        HelpID = "sfx2:Edit:TP_MANAGE_STYLES:ED_NAME";
-        Border = TRUE ;
-        Pos = MAP_APPFONT ( 70 , 6  ) ;
-        Size = MAP_APPFONT ( 184 , 12 ) ;
-    };
-    MultiLineEdit MLE_NAME
-    {
-        Pos = MAP_APPFONT ( 70 , 8  ) ;
-        Size = MAP_APPFONT ( 184 , 12 ) ;
-        Border = FALSE;
-        ReadOnly = TRUE;
-        Hide = TRUE;
-    };
-    CheckBox CB_AUTO
-    {
-        HelpID = "sfx2:CheckBox:TP_MANAGE_STYLES:CB_AUTO";
-        Pos = MAP_APPFONT ( 164 , 8 ) ;
-        Size = MAP_APPFONT ( 90 , 10 ) ;
-        Hide = TRUE ;
-        Text [ en-US ] = "~AutoUpdate" ;
-    };
-    FixedText FT_NEXT
-    {
-        Pos = MAP_APPFONT ( 6 , 26 ) ;
-        Size = MAP_APPFONT ( 61 , 10 ) ;
-        Text [ en-US ] = "Ne~xt Style" ;
-    };
-    ListBox LB_NEXT
-    {
-        HelpID = "sfx2:ListBox:TP_MANAGE_STYLES:LB_NEXT";
-        Border = TRUE ;
-        Pos = MAP_APPFONT ( 70 , 24 ) ;
-        Size = MAP_APPFONT ( 184 , 65 ) ;
-        DropDown = TRUE ;
-        Sort = TRUE ;
-    };
-    FixedText FT_BASE
-    {
-        Pos = MAP_APPFONT ( 6 , 44 ) ;
-        Size = MAP_APPFONT ( 61 , 10 ) ;
-        Text [ en-US ] = "Linked with" ;
-    };
-    ListBox LB_BASE
-    {
-        HelpID = "sfx2:ListBox:TP_MANAGE_STYLES:LB_BASE";
-        Border = TRUE ;
-        Pos = MAP_APPFONT ( 70 , 42 ) ;
-        Size = MAP_APPFONT ( 184 , 65 ) ;
-        DropDown = TRUE ;
-        Sort = TRUE ;
-    };
-    FixedText FT_REGION
-    {
-        Pos = MAP_APPFONT ( 6 , 62 ) ;
-        Size = MAP_APPFONT ( 61 , 10 ) ;
-        Text [ en-US ] = "~Category" ;
-    };
-    ListBox LB_REGION
-    {
-        HelpID = "sfx2:ListBox:TP_MANAGE_STYLES:LB_REGION";
-        Border = TRUE ;
-        Pos = MAP_APPFONT ( 70 , 60 ) ;
-        Size = MAP_APPFONT ( 184 , 64 ) ;
-        DropDown = TRUE ;
-    };
-    FixedText FT_DESC
-    {
-        HelpID = "sfx2:MultiLineEdit:TP_MANAGE_STYLES:ED_DESC";
-        Pos = MAP_APPFONT ( 12 , 88  ) ;
-        Size = MAP_APPFONT ( 236 , 85 ) ;
-        WordBreak = TRUE ;
-        Hide = TRUE ;
-    };
-    FixedLine GB_DESC
-    {
-        Pos = MAP_APPFONT ( 6 , 77  ) ;
-        Size = MAP_APPFONT ( 248 , 8 ) ;
-        Text [ en-US ] = "Contains" ;
-    };
-};
- // ********************************************************************** EOF
-
-/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sfx2/source/inc/helpid.hrc
@@ -25,7 +25,6 @@
 
 #define HID_DOCINFO_EDT                                       "SFX2_HID_DOCINFO_EDT"
 #define HID_PRINTMONITOR                                      "SFX2_HID_PRINTMONITOR"
-#define HID_MANAGE_STYLES                                     "SFX2_HID_MANAGE_STYLES"
 #define HID_CONFIG_EVENT                                      "SFX2_HID_CONFIG_EVENT"
 #define HID_BROWSERCONTROL                                    "SFX2_HID_BROWSERCONTROL"
 #define HID_PRINT_OPTIONS                                     "SFX2_HID_PRINT_OPTIONS"

sfx2/uiconfig/ui/managestylepage.ui
@@ -31,12 +31,13 @@
                 <property name="column_spacing">12</property>
                 <property name="row_homogeneous">True</property>
                 <child>
-                  <object class="GtkLabel" id="label3">
+                  <object class="GtkLabel" id="nameft">
                     <property name="visible">True</property>
                     <property name="can_focus">False</property>
                     <property name="xalign">0</property>
                     <property name="label" translatable="yes">_Name</property>
                     <property name="use_underline">True</property>
+                    <property name="mnemonic_widget">namerw</property>
                   </object>
                   <packing>
                     <property name="left_attach">0</property>

commit a7dd6d0274271f2f9cb8e01ed4e558db2bfb1f24

Related: fdo#81641 create new styles with an initial name
instead of creating with a blank name and changing it later

this way undo of a style creation is initialized with its name and so create a
style followed by deleting a style and undoing all those steps doesn't crash

(cherry picked from commit e904562af54545684b32d2042ded6bdb9459edca)

Conflicts:
	include/sfx2/styledlg.hxx
	sw/source/uibase/app/docst.cxx

Change-Id: Ida7a34410d5d7b05edcb86950aabb3c672b82c8f

style category listbox depends on being unsorted

regression from

commit 6ecfbf153a7c11c63d208630ac6a27064058099d
Author: Caolán McNamara <caolanm@redhat.com>
Date:   Wed Mar 13 15:10:25 2013 +0000

    drop unused resources and toggle label to activate replacement widget

Change-Id: I65ca8ec58ed984120f3c2e7ba68507cbf8acf089
(cherry picked from commit 11fab6b705638359b2ca0e3e85590aa7cc70a03a)

Related: fdo#81641 exclude 'all styles' category from organizer page

it doesn't make sense to be able to assign a style to
'all styles'

Change-Id: I632c62f293b553e436ceb8da0baf2bb39941fd39
(cherry picked from commit 2223ff6cb99df097a357674801835c7a260b551d)

Resolves: fdo#81641 the new style shouldn't be a conditional style

regression from

commit 715d79bcc59d68c77dc84a1038998dd873df92e9
Author: Cédric Bosdonnat <cedric.bosdonnat@free.fr>
Date:   Fri Nov 30 11:51:46 2012 +0100

    Styles & Formatting: added SFXSTYLEBIT_ALL_VISIBLE and show hidden styles

commit 2fd9c2bf9af0c50dae3af3dbe5e22965ccdb4ae7
Author: Cédric Bosdonnat <cedric.bosdonnat@free.fr>
Date:   Sun Nov 25 15:24:42 2012 +0100

    Allow to hide styles in the "Styles & Formating" dialog

    In its current state, the feature only hides the styles. Things to sort
    out are:
     + Add some automatic filter to show hidden styles
     + Make the visibility persist in the file format

(cherry picked from commit 1af0e46102350114dd5e854b7692c640dae2727f)

Conflicts:
	sw/source/uibase/app/docst.cxx

Change-Id: I7244d424765db0d5ed3aeaf380a8e1af56aa7eaf

include/sfx2/styledlg.hxx
@@ -25,6 +25,7 @@
 #include <sfx2/tabdlg.hxx>
 
 class SfxStyleSheetBase;
+class SfxStyleSheetBasePool;
 
 class SFX2_DLLPUBLIC SfxStyleDialog: public SfxTabDialog
 {
@@ -45,6 +46,8 @@ public:
     const SfxStyleSheetBase&    GetStyleSheet() const { return *pStyle; }
 
     virtual short               Ok();
+
+    static OUString GenerateUnusedName(SfxStyleSheetBasePool &rPool);
 };
 
 #endif

sfx2/source/dialog/mgetempl.cxx
@@ -67,7 +67,9 @@ SfxManageStyleSheetPage::SfxManageStyleSheetPage(Window* pParent, const SfxItemS
     m_pBaseLb->setMaxWidthChars(nMaxWidth);
     get(m_pFilterFt, "categoryft");
     get(m_pFilterLb, "category");
-    m_pFilterLb->SetStyle(m_pFilterLb->GetStyle() | WB_SORT);
+    //note that the code depends on categories not being lexically
+    //sorted, so if its changed to sorted, the code needs to
+    //be adapted to be position unaware
     m_pFilterLb->setMaxWidthChars(nMaxWidth);
     get(m_pDescFt, "desc");
 
@@ -94,16 +96,7 @@ SfxManageStyleSheetPage::SfxManageStyleSheetPage(Window* pParent, const SfxItemS
     if ( pStyle->GetName().isEmpty() && pPool )
     {
         // NullString as Name -> generate Name
-        OUString aNoName( SfxResId(STR_NONAME).toString() );
-        sal_uInt16 nNo = 1;
-        OUString aNo( aNoName );
-        aNoName += OUString::number( nNo );
-        while ( pPool->Find( aNoName ) )
-        {
-            ++nNo;
-            aNoName = aNo;
-            aNoName += OUString::number( nNo );
-        }
+        OUString aNoName(SfxStyleDialog::GenerateUnusedName(*pPool));
         pStyle->SetName( aNoName );
         aName = aNoName;
         aFollow = pStyle->GetFollow();
@@ -193,6 +186,7 @@ SfxManageStyleSheetPage::SfxManageStyleSheetPage(Window* pParent, const SfxItemS
 
             if ( pTupel->nFlags != SFXSTYLEBIT_AUTO     &&
                  pTupel->nFlags != SFXSTYLEBIT_USED     &&
+                 pTupel->nFlags != SFXSTYLEBIT_ALL_VISIBLE &&
                  pTupel->nFlags != SFXSTYLEBIT_ALL )
             {
                 m_pFilterLb->InsertEntry( pTupel->aName, nIdx );

sfx2/source/dialog/styledlg.cxx
@@ -147,5 +147,19 @@ IMPL_LINK( SfxStyleDialog, CancelHdl, Button *, pButton )
     return 0;
 }
 
+OUString SfxStyleDialog::GenerateUnusedName(SfxStyleSheetBasePool &rPool)
+{
+    OUString aNoName(SfxResId(STR_NONAME).toString());
+    sal_uInt16 nNo = 1;
+    OUString aNo(aNoName);
+    aNoName += OUString::number(nNo);
+    while (rPool.Find(aNoName))
+    {
+        ++nNo;
+        aNoName = aNo;
+        aNoName += OUString::number(nNo);
+    }
+    return aNoName;
+}
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sw/source/ui/app/docst.cxx
@@ -25,6 +25,7 @@
 
 #include <hintids.hxx>
 #include <sfx2/app.hxx>
+#include <sfx2/styledlg.hxx>
 #include <svl/whiter.hxx>
 #include <sfx2/templdlg.hxx>
 #include <sfx2/tplpitem.hxx>
@@ -284,6 +285,9 @@ void SwDocShell::ExecStyleSheet( SfxRequest& rReq )
             if( SFX_ITEM_SET == pArgs->GetItemState( SID_STYLE_REFERENCE,
                 sal_False, &pItem ))
                 sParent = ((const SfxStringItem*)pItem)->GetValue();
+
+            if (sName.isEmpty() && mxBasePool.get())
+                sName = SfxStyleDialog::GenerateUnusedName(*mxBasePool);
 
             nRet = Edit( sName, sParent, nFamily, nMask, sal_True, OString(), 0, rReq.IsAPI() );
         }
@@ -615,7 +619,7 @@ sal_uInt16 SwDocShell::Edit(
 
     if( bNew )
     {
-        if( SFXSTYLEBIT_ALL != nMask && SFXSTYLEBIT_USED != nMask )
+        if (SFXSTYLEBIT_ALL != nMask && SFXSTYLEBIT_ALL_VISIBLE != nMask && SFXSTYLEBIT_USED != nMask)
             nMask |= SFXSTYLEBIT_USERDEF;
         else
             nMask = SFXSTYLEBIT_USERDEF;