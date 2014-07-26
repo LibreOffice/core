aaa18a45c6f5e4f2b5fb1ee2deee510ea3353053
Kohei Yoshida <kohei.yoshida@collabora.com>
2014-07-26 14:44:38 -0400

    bnc#885825: Handle ODF import and export of data label border properties.
    (cherry picked from commit 6c2c974dd3f0b21a23bc3bc5560487d28bbe0fad)
    
    Conflicts:
    	include/xmloff/xmltoken.hxx
    	xmloff/source/chart/PropertyMap.hxx
    	xmloff/source/core/xmltoken.cxx
    
    Change-Id: Ic8c7bc873768008537cd52a3fd4b11031b403139

--- xmloff/source/chart/PropertyMaps.cxx
+++ xmloff/source/chart/PropertyMaps.cxx
@@ -34,6 +34,7 @@
 #include "XMLTextOrientationHdl.hxx"
 #include "XMLSymbolTypePropertyHdl.hxx"
 #include "XMLAxisPositionPropertyHdl.hxx"
+#include "../draw/propimp0.hxx"
 
 #include <xmloff/EnumPropertyHdl.hxx>
 #include <xmloff/XMLConstantsPropertyHandler.hxx>
@@ -66,6 +67,18 @@
 
 using namespace com::sun::star;
 using namespace ::xmloff::token;
+
+namespace {
+
+SvXMLEnumMapEntry const aLineStyleMap[] =
+{
+    { XML_NONE,     drawing::LineStyle_NONE },
+    { XML_SOLID,    drawing::LineStyle_SOLID },
+    { XML_DASH,     drawing::LineStyle_DASH },
+    { XML_TOKEN_INVALID, 0 }
+};
+
+}
 
 // the following class implementations are in this file:
 //
@@ -155,6 +168,14 @@ const XMLPropertyHandler* XMLChartPropHdlFactory::GetPropertyHandler( sal_Int32 
                 pHdl = new XMLEnumPropertyHdl( aXMLChartMissingValueTreatmentEnumMap,
                                                ::getCppuType((const sal_Int32*)0) );
                 break;
+            case XML_SCH_TYPE_LABEL_BORDER_STYLE:
+                pHdl = new XMLEnumPropertyHdl(aLineStyleMap, cppu::UnoType<drawing::LineStyle>::get());
+            break;
+            case XML_SCH_TYPE_LABEL_BORDER_OPACITY:
+                pHdl = new XMLOpacityPropertyHdl(NULL);
+            break;
+            default:
+                ;
         }
         if( pHdl )
             PutHdlCache( nType, pHdl );
