eeeef4e83c3cc643f32c094df36cbd311b1c294a
Tor Lillqvist <tml@collabora.com>
2014-07-29 17:22:26 +0300

    Include <algorithm> for std::min
    Change-Id: If2bf31494f1354ff3856ad8b32f6799042e23b18

--- comphelper/source/misc/docpasswordhelper.cxx
+++ comphelper/source/misc/docpasswordhelper.cxx
@@ -17,6 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
+#include <algorithm>
 
 #include <comphelper/docpasswordhelper.hxx>
 #include <com/sun/star/beans/PropertyValue.hpp>
