15b190d8b8da08de57a9e6f8e5e06f4dc8bcafc4
Michael Stahl <mstahl@redhat.com>
2014-07-29 22:21:53 +0200

    fdo#81384: writerfilter: turn the horrible btLr cell text dir hack
    ... by 5 degrees to starboard, which lets the row in the bugdoc appear,
    altough the height is a bit insufficient still.  Why that works, is a
    nautic mystery to me, i'd say this whole btLr emulation nonsense needs
    to be keel-hauled, but after this patch i'll need some rum, arrrr...
    
    (regression from commit 0208ead70a9412ccd554fcef3e9308f8ca17037b
     and commit 970160f78ef6cc7abacfa252daa8451e1f0117bb)
    
    Change-Id: Ie0c6ec88b6d6635379b9127f6460647f14776aad
    (cherry picked from commit 5893a7536a0bbce57c4a2f89680dcb4cff3d06d7)
    Reviewed-on: https://gerrit.libreoffice.org/10631
    Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
    Tested-by: Miklos Vajna <vmiklos@collabora.co.uk>

--- writerfilter/source/dmapper/TablePropertiesHandler.cxx
+++ writerfilter/source/dmapper/TablePropertiesHandler.cxx
@@ -97,11 +97,14 @@ namespace dmapper {
 
                     DomainMapperTableManager* pManager = dynamic_cast<DomainMapperTableManager*>(m_pTableManager);
                     // In case any of the cells has the btLr cell direction, then an explicit minimal size will just hide the whole row, don't do that.
-                    if (pMeasureHandler->GetRowHeightSizeType() != text::SizeType::MIN || !pManager || !pManager->HasBtlrCell())
+                    const int MINLAY = 23; // sw/inc/swtypes.hxx, minimal possible size of frames.
+                    if (!pManager || !pManager->HasBtlrCell() || pMeasureHandler->getMeasureValue() > ConversionHelper::convertTwipToMM100(MINLAY))
                     {
                         // In case a cell already wanted fixed size, we should not overwrite it here.
                         if (!pManager || !pManager->IsRowSizeTypeInserted())
                             pPropMap->Insert( PROP_SIZE_TYPE, uno::makeAny( pMeasureHandler->GetRowHeightSizeType() ), false);
+                        else
+                            pPropMap->Insert( PROP_SIZE_TYPE, uno::makeAny(text::SizeType::FIX), false);
 
                         pPropMap->Insert( PROP_HEIGHT, uno::makeAny(pMeasureHandler->getMeasureValue() ));
                     }
