0aebffb4cc12cb5ae3f19aa19dc3a9a741b106f8
Palenik Mihály <palenik.mihaly@gmail.com>
2014-07-29 16:34:55 +0200

    Convert 3 tabpages to .ui
    I converted PAGE_DBWIZARD_MYSQL_JDBC, PAGE_DBWIZARD_ORACLE and
    PAGE_DBWIZARD_JDBC to .ui.
    
    Change-Id: I920183c4abf303918ac0406b5810e1b8b377a04c

--- dbaccess/UIConfig_dbaccess.mk
+++ dbaccess/UIConfig_dbaccess.mk
@@ -29,6 +29,7 @@ $(eval $(call gb_UIConfig_add_uifiles,dbaccess, \
     dbaccess/uiconfig/ui/generalpagewizard \
     dbaccess/uiconfig/ui/generatedvaluespage \
     dbaccess/uiconfig/ui/indexdesigndialog \
+    dbaccess/uiconfig/ui/jdbcconnectionpage \
     dbaccess/uiconfig/ui/joindialog \
     dbaccess/uiconfig/ui/migratepage \
     dbaccess/uiconfig/ui/password \
@@ -38,6 +39,7 @@ $(eval $(call gb_UIConfig_add_uifiles,dbaccess, \
     dbaccess/uiconfig/ui/relationdialog \
     dbaccess/uiconfig/ui/rowheightdialog \
     dbaccess/uiconfig/ui/savedialog \
+    dbaccess/uiconfig/ui/specialjdbcconnectionpage \
     dbaccess/uiconfig/ui/specialsettingspage \
     dbaccess/uiconfig/ui/sortdialog \
     dbaccess/uiconfig/ui/sqlexception \
