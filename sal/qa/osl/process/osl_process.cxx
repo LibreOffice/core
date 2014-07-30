e3521500027f3f07e8f8d24559bb9c37420935c4
Douglas Mencken <dougmencken@gmail.com>
2014-07-30 06:10:06 -0400

    sal_osl_process: fix "Parent and child environment not equal" for OSX <10.6
    It fails with "assertion failed":
    - Expression: compare_environments() [osl_process.cxx:370]
    - Expression: compare_merged_environments(different_child_env_vars) [osl_process.cxx:428]
    
    Just disable it (yet) for OSX <10.6 as it's already done for Windows.
    
    Change-Id: I88e838481a25ee7e977512e33e23afa2b9d394ff

--- sal/qa/osl/process/osl_process.cxx
+++ sal/qa/osl/process/osl_process.cxx
@@ -465,8 +465,8 @@ public:
     }
 
     CPPUNIT_TEST_SUITE(Test_osl_executeProcess);
-    //TODO: Repair these under windows.
-#ifndef WNT
+    //TODO: Repair these (at least under Windows)
+#if !defined(WNT) && !(defined(MACOSX) && (MACOSX_SDK_VERSION < 1060))
     CPPUNIT_TEST(osl_execProc_parent_equals_child_environment);
     CPPUNIT_TEST(osl_execProc_merged_child_environment);
 #endif
