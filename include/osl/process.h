41fbe85870d0f83c0f7eb59f6f517500b0323239
Andrzej Hunt <andrzej.hunt@collabora.com>
2014-07-31 15:34:04 +0200

    Mark osl_areCommandArgsSet as internal only.
    Change-Id: Iac97290738088bd1ce7469ffee773994e7653efc

--- include/osl/process.h
+++ include/osl/process.h
@@ -367,6 +367,9 @@ SAL_DLLPUBLIC oslProcessError SAL_CALL osl_getCommandArg(
         sal_uInt32 nArg, rtl_uString **strCommandArg);
 
 /** Determine whether or not the command args have already been set.
+
+    Deprecated: This function is only for internal use
+
     @return The command args are already set, and may not be set again.
     @since LibreOffice 4.3
 */
