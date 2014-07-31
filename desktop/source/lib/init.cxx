8c1bb1cd0e6e08b5aedb67c1279dcd5f57050238
Andrzej Hunt <andrzej.hunt@collabora.com>
2014-07-31 11:20:00 +0200

    LOK documentLoad needs SolarMutex protection too.
    We probably need to do this for most of LOK now that
    we use soffice_main.
    
    Change-Id: I77be7865511c083679cf2f68b0e5df106506fd70

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -282,6 +282,8 @@ static LibreOfficeKitDocument* lo_documentLoad(LibreOfficeKit* pThis, const char
 
     OUString aURL = getAbsoluteURL(pURL);
 
+    SolarMutexGuard aGuard;
+
     uno::Reference<frame::XDesktop2> xComponentLoader = frame::Desktop::create(xContext);
 
     pLib->maLastExceptionMsg = "";
