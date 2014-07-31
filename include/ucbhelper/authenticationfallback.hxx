d82976726b8f9f26f1c9bfbe199f57b147e71c65
Mihai Varga <mihai.mv13@gmail.com>
2014-07-31 11:33:02 +0300

    Authentication fallback request for the OneDrive connection
    To fix build, also squashed in:
    
    Author: Mihai Varga <mihai.mv13@gmail.com>
    Date:   Fri Aug 1 12:50:23 2014 +0300
    
        OneDrive auth fallback dialog interaction continuation
    
    Change-Id: Id37f502217da3fde55ae93a6e1c01409509d657c
    (cherry picked from commit 4998e6a361252529cabcaac37b0f03bbed173544)

---
+++ include/ucbhelper/authenticationfallback.hxx
@@ -0,0 +1,52 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef INCLUDED_UCBHELPER_AUTHENTICATIONFALLBACK_HXX
+#define INCLUDED_UCBHELPER_AUTHENTICATIONFALLBACK_HXX
+
+#include <rtl/ref.hxx>
+#include <ucbhelper/interactionrequest.hxx>
+#include <ucbhelper/ucbhelperdllapi.h>
+#include <com/sun/star/ucb/AuthenticationFallbackRequest.hpp>
+
+
+namespace ucbhelper {
+
+/**
+  * This class implements a simple authentication interaction request used
+  * when programmatically authentication cannot succeed.
+  *
+  * Read-only values : instructions, url
+  * Read-write values: code
+  */
+class UCBHELPER_DLLPUBLIC AuthenticationFallbackRequest : public ucbhelper::InteractionRequest
+{
+private:
+    rtl::Reference< ucbhelper::InteractionAuthFallback > m_xAuthFallback;
+
+public:
+    /**
+      * Constructor.
+      *
+      * @param rInstructions instructions to be followed by the user
+      * @param rURL contains a URL for which authentication is requested.
+      */
+    AuthenticationFallbackRequest( const OUString & rInstructions,
+                                 const OUString & rURL );
+
+    const rtl::Reference< ucbhelper::InteractionAuthFallback >&
+        getAuthFallbackInter( ) const { return m_xAuthFallback; }
+
+};
+
+} // namespace ucbhelper
+
+#endif /* ! INCLUDED_UCBHELPER_AUTHENTICATIONFALLBACK_HXX*/
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
--- offapi/UnoApi_offapi.mk
+++ offapi/UnoApi_offapi.mk
@@ -3938,6 +3938,7 @@ $(eval $(call gb_UnoApi_add_idlfiles,offapi,com/sun/star/ucb,\
 	XFetchProvider \
 	XFetchProviderForContentAccess \
 	XFileIdentifierConverter \
+	XInteractionAuthFallback \
 	XInteractionHandlerSupplier \
 	XInteractionReplaceExistingData \
 	XInteractionSupplyAuthentication \
