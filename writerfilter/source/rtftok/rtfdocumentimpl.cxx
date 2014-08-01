LibreOffice/core

commit 94765a1b3f543b3b60f8c1de05048d12f2576be4

fdo#48440 fix import of RTF_PAGE between text frames
Normally such a page break is surrounded by text, and text() already
calls checkNeedPap(), but in case the whole paragraph contains a page
break only, we need it here as well.

(cherry picked from commit b70f74cb5f4b499f14831fd6ad49d9238735e6ef)

Conflicts:
	sw/qa/extras/rtfimport/rtfimport.cxx

Change-Id: I3961272d77c743e0e6da2d5052e57cec5f5937e7
Reviewed-on: https://gerrit.libreoffice.org/1965
Reviewed-by: Michael Stahl <mstahl@redhat.com>
Tested-by: Michael Stahl <mstahl@redhat.com>

writerfilter/source/rtftok/rtfdocumentimpl.cxx
@@ -1829,6 +1829,7 @@ int RTFDocumentImpl::dispatchSymbol(RTFKeyword nKeyword)
                 }
                 else
                 {
+                    checkNeedPap();
                     sal_uInt8 sBreak[] = { 0xc };
                     Mapper().text(sBreak, 1);
                     if (!m_bNeedPap)

commit 008d1187a9f3b36d6cbf31742632d237afecf1bd

fdo#81893: writerfilter: RTF import: \qc before \page was lost
The paragraph properties need to be applied to the paragraph following
the \page too, which did not happen in this case.

Also the minimized test doc shows that checkNeedPap() must be preceded
by checkFirstRun(), since there is no run preceding the \page.

(regression from 94765a1b3f543b3b60f8c1de05048d12f2576be4)

Change-Id: Id286c4fbdeee688fa4f6b24063b634bac637eab6

writerfilter/source/rtftok/rtfdocumentimpl.cxx
@@ -2291,11 +2291,15 @@ int RTFDocumentImpl::dispatchSymbol(RTFKeyword nKeyword)
         }
         else
         {
+            checkFirstRun();
             checkNeedPap();
             sal_uInt8 sBreak[] = { 0xc };
             Mapper().text(sBreak, 1);
             if (!m_bNeedPap)
+            {
                 parBreak();
+                m_bNeedPap = true;
+            }
             m_bNeedCr = true;
         }
     }

sw/qa/extras/rtfimport/rtfimport.cxx
@@ -343,6 +343,10 @@ DECLARE_RTFIMPORT_TEST(testFdo81892, "fdo81892.rtf")
     uno::Reference<container::XIndexAccess> xTables(xTextTablesSupplier->getTextTables(), uno::UNO_QUERY);
     uno::Reference<text::XTextTable> xTable(xTables->getByIndex(0), uno::UNO_QUERY);
     CPPUNIT_ASSERT_EQUAL(text::HoriOrientation::CENTER, getProperty<sal_Int16>(xTable, "HoriOrient"));
+
+    // fdo#81893: paragraph with \page was not centered
+    uno::Reference<text::XTextRange> xPara(getParagraph(2, "Performance"));
+    CPPUNIT_ASSERT_EQUAL(style::ParagraphAdjust_CENTER, static_cast<style::ParagraphAdjust>(getProperty<sal_Int16>(xPara, "ParaAdjust")));
 }
 
 DECLARE_RTFIMPORT_TEST(testFdo45394, "fdo45394.rtf")