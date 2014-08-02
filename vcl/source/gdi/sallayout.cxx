LibreOffice/core

commit 2b8528a2745bec7909bfe2265d6110a9964eef47

Use DeviceCorrdinate for DXArray in FillDXArray and ImplLayoutArgs
Change-Id: Ifba7104a13cf737200c81d1a1bd5ece9a29da60e

vcl/source/gdi/sallayout.cxx
@@ -836,7 +836,7 @@ void GenericSalLayout::AppendGlyph( const GlyphItem& rGlyphItem )
     m_GlyphItems.push_back(rGlyphItem);
 }
 
-bool GenericSalLayout::GetCharWidths( sal_Int32* pCharWidths ) const
+bool GenericSalLayout::GetCharWidths( DeviceCoordinate* pCharWidths ) const
 {
     // initialize character extents buffer
     int nCharCount = mnEndCharPos - mnMinCharPos;
@@ -910,7 +910,7 @@ bool GenericSalLayout::GetCharWidths( sal_Int32* pCharWidths ) const
     return true;
 }
 
-long GenericSalLayout::FillDXArray( sal_Int32* pCharWidths ) const
+DeviceCoordinate GenericSalLayout::FillDXArray( DeviceCoordinate* pCharWidths ) const
 {
     if( pCharWidths )
         if( !GetCharWidths( pCharWidths ) )
@@ -1281,7 +1281,7 @@ void GenericSalLayout::GetCaretPositions( int nMaxIndex, sal_Int32* pCaretXArray
 sal_Int32 GenericSalLayout::GetTextBreak( long nMaxWidth, long nCharExtra, int nFactor ) const
 {
     int nCharCapacity = mnEndCharPos - mnMinCharPos;
-    sal_Int32* pCharWidths = (sal_Int32*)alloca( nCharCapacity * sizeof(sal_Int32) );
+    DeviceCoordinate* pCharWidths = (DeviceCoordinate*)alloca( nCharCapacity * sizeof(DeviceCoordinate) );
     if( !GetCharWidths( pCharWidths ) )
         return -1;
 
@@ -1528,7 +1528,7 @@ void MultiSalLayout::AdjustLayout( ImplLayoutArgs& rArgs )
     {
         // for stretched text in a MultiSalLayout the target width needs to be
         // distributed by individually adjusting its virtual character widths
-        long nTargetWidth = aMultiArgs.mnLayoutWidth;
+        DeviceCoordinate nTargetWidth = aMultiArgs.mnLayoutWidth;
         nTargetWidth *= mnUnitsPerPixel; // convert target width to base font units
         aMultiArgs.mnLayoutWidth = 0;
 
@@ -1537,12 +1537,12 @@ void MultiSalLayout::AdjustLayout( ImplLayoutArgs& rArgs )
             mpLayouts[n]->SalLayout::AdjustLayout( aMultiArgs );
         // then we can measure the unmodified metrics
         int nCharCount = rArgs.mnEndCharPos - rArgs.mnMinCharPos;
-        sal_Int32* pJustificationArray = (sal_Int32*)alloca( nCharCount * sizeof(sal_Int32) );
+        DeviceCoordinate* pJustificationArray = (DeviceCoordinate*)alloca( nCharCount * sizeof(sal_Int32) );
         FillDXArray( pJustificationArray );
         // #i17359# multilayout is not simplified yet, so calculating the
         // unjustified width needs handholding; also count the number of
         // stretchable virtual char widths
-        long nOrigWidth = 0;
+        DeviceCoordinate nOrigWidth = 0;
         int nStretchable = 0;
         for( int i = 0; i < nCharCount; ++i )
         {
@@ -1555,14 +1555,14 @@ void MultiSalLayout::AdjustLayout( ImplLayoutArgs& rArgs )
         // now we are able to distribute the extra width over the virtual char widths
         if( nOrigWidth && (nTargetWidth != nOrigWidth) )
         {
-            int nDiffWidth = nTargetWidth - nOrigWidth;
-            int nWidthSum = 0;
+            DeviceCoordinate nDiffWidth = nTargetWidth - nOrigWidth;
+            DeviceCoordinate nWidthSum = 0;
             for( int i = 0; i < nCharCount; ++i )
             {
-                int nJustWidth = pJustificationArray[i];
+                DeviceCoordinate nJustWidth = pJustificationArray[i];
                 if( (nJustWidth > 0) && (nStretchable > 0) )
                 {
-                    int nDeltaWidth = nDiffWidth / nStretchable;
+                    DeviceCoordinate nDeltaWidth = nDiffWidth / nStretchable;
                     nJustWidth += nDeltaWidth;
                     nDiffWidth -= nDeltaWidth;
                     --nStretchable;
@@ -1579,7 +1579,7 @@ void MultiSalLayout::AdjustLayout( ImplLayoutArgs& rArgs )
             {
                 for( int i = 0; i < nCharCount; ++i )
                 {
-                    sal_Int32 nVal = pJustificationArray[ i ];
+                    DeviceCoordinate nVal = pJustificationArray[ i ];
                     nVal += (mnUnitsPerPixel + 1) / 2;
                     pJustificationArray[ i ] = nVal / mnUnitsPerPixel;
                 }
@@ -1915,7 +1915,7 @@ sal_Int32 MultiSalLayout::GetTextBreak( long nMaxWidth, long nCharExtra, int nFa
         return mpLayouts[0]->GetTextBreak( nMaxWidth, nCharExtra, nFactor );
 
     int nCharCount = mnEndCharPos - mnMinCharPos;
-    sal_Int32* pCharWidths = (sal_Int32*)alloca( 2*nCharCount * sizeof(sal_Int32) );
+    DeviceCoordinate* pCharWidths = (DeviceCoordinate*)alloca( 2*nCharCount * sizeof(DeviceCoordinate) );
     mpLayouts[0]->FillDXArray( pCharWidths );
 
     for( int n = 1; n < mnLevel; ++n )
@@ -1926,13 +1926,13 @@ sal_Int32 MultiSalLayout::GetTextBreak( long nMaxWidth, long nCharExtra, int nFa
         fUnitMul /= rLayout.GetUnitsPerPixel();
         for( int i = 0; i < nCharCount; ++i )
         {
-            long w = pCharWidths[ i + nCharCount ];
-            w = static_cast<long>(w*fUnitMul + 0.5);
+            DeviceCoordinate w = pCharWidths[ i + nCharCount ];
+            w = (DeviceCoordinate)(w * fUnitMul + 0.5);
             pCharWidths[ i ] += w;
         }
     }
 
-    long nWidth = 0;
+    DeviceCoordinate nWidth = 0;
     for( int i = 0; i < nCharCount; ++i )
     {
         nWidth += pCharWidths[ i ] * nFactor;
@@ -1944,30 +1944,30 @@ sal_Int32 MultiSalLayout::GetTextBreak( long nMaxWidth, long nCharExtra, int nFa
     return -1;
 }
 
-long MultiSalLayout::FillDXArray( sal_Int32* pCharWidths ) const
+DeviceCoordinate MultiSalLayout::FillDXArray( DeviceCoordinate* pCharWidths ) const
 {
-    long nMaxWidth = 0;
+    DeviceCoordinate nMaxWidth = 0;
 
     // prepare merging of fallback levels
-    sal_Int32* pTempWidths = NULL;
+    DeviceCoordinate* pTempWidths = NULL;
     const int nCharCount = mnEndCharPos - mnMinCharPos;
     if( pCharWidths )
     {
         for( int i = 0; i < nCharCount; ++i )
             pCharWidths[i] = 0;
-        pTempWidths = (sal_Int32*)alloca( nCharCount * sizeof(sal_Int32) );
+        pTempWidths = (DeviceCoordinate*)alloca( nCharCount * sizeof(DeviceCoordinate) );
     }
 
     for( int n = mnLevel; --n >= 0; )
     {
         // query every fallback level
-        long nTextWidth = mpLayouts[n]->FillDXArray( pTempWidths );
+        DeviceCoordinate nTextWidth = mpLayouts[n]->FillDXArray( pTempWidths );
         if( !nTextWidth )
             continue;
         // merge results from current level
         double fUnitMul = mnUnitsPerPixel;
         fUnitMul /= mpLayouts[n]->GetUnitsPerPixel();
-        nTextWidth = static_cast<long>(nTextWidth * fUnitMul + 0.5);
+        nTextWidth = (DeviceCoordinate)(nTextWidth * fUnitMul + 0.5);
         if( nMaxWidth < nTextWidth )
             nMaxWidth = nTextWidth;
         if( !pCharWidths )
@@ -1979,10 +1979,10 @@ long MultiSalLayout::FillDXArray( sal_Int32* pCharWidths ) const
             // one char cannot be resolved from different fallbacks
             if( pCharWidths[i] != 0 )
                 continue;
-            long nCharWidth = pTempWidths[i];
+            DeviceCoordinate nCharWidth = pTempWidths[i];
             if( !nCharWidth )
                 continue;
-            nCharWidth = static_cast<long>(nCharWidth * fUnitMul + 0.5);
+            nCharWidth = (DeviceCoordinate)(nCharWidth * fUnitMul + 0.5);
             pCharWidths[i] = nCharWidth;
         }
     }

include/vcl/devicecoordinate.hxx
@@ -1,0 +1,30 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef INCLUDED_VCL_DEVICE_COORDINATE_HXX
+#define INCLUDED_VCL_DEVICE_COORDINATE_HXX
+
+#include <config_vcl.h>
+
+#if VCL_FLOAT_DEVICE_PIXEL
+#include <basegfx/point/b2dpoint.hxx>
+typedef double DeviceCoordinate;
+typedef ::basegfx::B2DPoint DevicePoint;
+
+#else /* !VCL_FLOAT_DEVICE_PIXEL */
+
+#include <basegfx/point/b2ipoint.hxx>
+typedef sal_Int32 DeviceCoordinate;
+typedef ::basegfx::B2IPoint DevicePoint;
+
+#endif /* ! Carpet Cushion */
+
+#endif /* NDef INCLUDED_VCL_DEVICE_COORDINATE_HXX */
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

include/vcl/outdev.hxx
@@ -20,14 +20,13 @@
 #ifndef INCLUDED_VCL_OUTDEV_HXX
 #define INCLUDED_VCL_OUTDEV_HXX
 
-#include <config_vcl.h>
-
 #include <tools/gen.hxx>
 #include <tools/solar.h>
 #include <tools/rc.hxx>
 #include <tools/color.hxx>
 #include <tools/poly.hxx>
 
+#include <vcl/devicecoordinate.hxx>
 #include <vcl/dllapi.h>
 #include <vcl/font.hxx>
 #include <vcl/region.hxx>
@@ -1250,7 +1249,7 @@ public:
                                             const Point& rLogicPos = Point(0,0), long nLogicWidth=0,
                                             const sal_Int32* pLogicDXArray=NULL, int flags=0 ) const;
     SAL_DLLPRIVATE ImplLayoutArgs ImplPrepareLayoutArgs( OUString&, const sal_Int32 nIndex, const sal_Int32 nLen,
-                                                         long nPixelWidth, const sal_Int32* pPixelDXArray, int flags = 0 ) const;
+                                                         DeviceCoordinate nPixelWidth, const DeviceCoordinate* pPixelDXArray, int flags = 0 ) const;
     SAL_DLLPRIVATE SalLayout*   ImplGlyphFallbackLayout( SalLayout*, ImplLayoutArgs& ) const;
     // tells whether this output device is RTL in an LTR UI or LTR in a RTL UI
     SAL_DLLPRIVATE SalLayout*   getFallbackFont(ImplFontEntry &rFallbackFont,

vcl/inc/graphite_layout.hxx
@@ -124,7 +124,7 @@ public:
 
     // methods using string indexing
     virtual sal_Int32 GetTextBreak(long nMaxWidth, long nCharExtra=0, int nFactor=1) const SAL_OVERRIDE;
-    virtual long  FillDXArray( sal_Int32* pDXArray ) const SAL_OVERRIDE;
+    virtual DeviceCoordinate FillDXArray( DeviceCoordinate* pDXArray ) const SAL_OVERRIDE;
     virtual void  ApplyDXArray(ImplLayoutArgs &rArgs, std::vector<int> & rDeltaWidth);
 
     virtual void  GetCaretPositions( int nArraySize, sal_Int32* pCaretXArray ) const SAL_OVERRIDE;

vcl/inc/graphite_serverfont.hxx
@@ -68,11 +68,22 @@ public:
             maImpl.DrawOffset() = maDrawOffset;
             maImpl.AdjustLayout(rArgs);
         };
-        virtual long    GetTextWidth() const SAL_OVERRIDE                           { return maImpl.GetTextWidth(); }
-        virtual long    FillDXArray( sal_Int32* dxa ) const SAL_OVERRIDE                 { return maImpl.FillDXArray(dxa); }
+        virtual long GetTextWidth() const SAL_OVERRIDE
+        {
+            return maImpl.GetTextWidth();
+        }
+        virtual DeviceCoordinate FillDXArray( DeviceCoordinate* dxa ) const SAL_OVERRIDE
+        {
+            return maImpl.FillDXArray(dxa);
+        }
         virtual sal_Int32 GetTextBreak(long mw, long ce, int f) const SAL_OVERRIDE
-            { return maImpl.GetTextBreak(mw, ce, f); }
-        virtual void    GetCaretPositions( int as, sal_Int32* cxa ) const SAL_OVERRIDE   { maImpl.GetCaretPositions(as, cxa); }
+        {
+            return maImpl.GetTextBreak(mw, ce, f);
+        }
+        virtual void    GetCaretPositions( int as, sal_Int32* cxa ) const SAL_OVERRIDE
+        {
+            maImpl.GetCaretPositions(as, cxa);
+        }
 
         // used by display layers
         virtual int     GetNextGlyphs( int l, sal_GlyphId* gia, Point& p, int& s,

vcl/inc/sallayout.hxx
@@ -29,6 +29,7 @@
 #include <tools/gen.hxx>
 #include <vcl/dllapi.h>
 #include <vcl/vclenum.hxx> // for typedef sal_UCS4
+#include <vcl/devicecoordinate.hxx>
 
 #ifndef _TOOLS_LANG_HXX
 typedef unsigned short LanguageType;
@@ -76,8 +77,8 @@ public:
     const sal_Unicode*  mpStr;
 
     // positioning related inputs
-    const sal_Int32*    mpDXArray;          // in pixel units
-    long                mnLayoutWidth;      // in pixel units
+    const DeviceCoordinate* mpDXArray;     // in pixel units
+    DeviceCoordinate   mnLayoutWidth;      // in pixel units
     int                 mnOrientation;      // in 0-3600 system
 
     // data for bidi and glyph+script fallback
@@ -89,8 +90,8 @@ public:
                                 int nMinCharPos, int nEndCharPos, int nFlags,
                                 const LanguageTag& rLanguageTag );
 
-    void        SetLayoutWidth( long nWidth )       { mnLayoutWidth = nWidth; }
-    void        SetDXArray( const sal_Int32* pDXArray )  { mpDXArray = pDXArray; }
+    void        SetLayoutWidth( DeviceCoordinate nWidth )       { mnLayoutWidth = nWidth; }
+    void        SetDXArray( const DeviceCoordinate* pDXArray )  { mpDXArray = pDXArray; }
     void        SetOrientation( int nOrientation )  { mnOrientation = nOrientation; }
 
     void        ResetPos()
@@ -171,7 +172,7 @@ public:
 
     // methods using string indexing
     virtual sal_Int32 GetTextBreak(long nMaxWidth, long nCharExtra=0, int nFactor=1) const = 0;
-    virtual long    FillDXArray( sal_Int32* pDXArray ) const = 0;
+    virtual DeviceCoordinate FillDXArray( DeviceCoordinate* pDXArray ) const = 0;
     virtual long    GetTextWidth() const { return FillDXArray( NULL ); }
     virtual void    GetCaretPositions( int nArraySize, sal_Int32* pCaretXArray ) const = 0;
     virtual bool    IsKashidaPosValid ( int /*nCharPos*/ ) const { return true; } // i60594
@@ -229,7 +230,7 @@ class VCL_PLUGIN_PUBLIC MultiSalLayout : public SalLayout
 public:
     virtual void    DrawText( SalGraphics& ) const SAL_OVERRIDE;
     virtual sal_Int32 GetTextBreak(long nMaxWidth, long nCharExtra, int nFactor) const SAL_OVERRIDE;
-    virtual long    FillDXArray( sal_Int32* pDXArray ) const SAL_OVERRIDE;
+    virtual DeviceCoordinate FillDXArray( DeviceCoordinate* pDXArray ) const SAL_OVERRIDE;
     virtual void    GetCaretPositions( int nArraySize, sal_Int32* pCaretXArray ) const SAL_OVERRIDE;
     virtual int     GetNextGlyphs( int nLen, sal_GlyphId* pGlyphIdxAry, Point& rPos,
                                    int&, sal_Int32* pGlyphAdvAry, int* pCharPosAry,
@@ -329,7 +330,7 @@ public:
 
     // used by upper layers
     virtual long    GetTextWidth() const SAL_OVERRIDE;
-    virtual long    FillDXArray( sal_Int32* pDXArray ) const SAL_OVERRIDE;
+    virtual DeviceCoordinate FillDXArray( DeviceCoordinate* pDXArray ) const SAL_OVERRIDE;
     virtual sal_Int32 GetTextBreak(long nMaxWidth, long nCharExtra, int nFactor) const SAL_OVERRIDE;
     virtual void    GetCaretPositions( int nArraySize, sal_Int32* pCaretXArray ) const SAL_OVERRIDE;
 
@@ -347,7 +348,7 @@ protected:
     virtual void    DropGlyph( int nStart ) SAL_OVERRIDE;
     virtual void    Simplify( bool bIsBase ) SAL_OVERRIDE;
 
-    bool            GetCharWidths( sal_Int32* pCharWidths ) const;
+    bool            GetCharWidths( DeviceCoordinate* pCharWidths ) const;
 
     GlyphVector     m_GlyphItems;
 

vcl/quartz/ctlayout.cxx
@@ -43,7 +43,7 @@ public:
                                    const PhysicalFontFace** pFallbackFonts ) const SAL_OVERRIDE;
 
     virtual long    GetTextWidth() const SAL_OVERRIDE;
-    virtual long    FillDXArray( sal_Int32* pDXArray ) const SAL_OVERRIDE;
+    virtual DeviceCoordinate FillDXArray( DeviceCoordinate* pDXArray ) const SAL_OVERRIDE;
     virtual sal_Int32 GetTextBreak(long nMaxWidth, long nCharExtra, int nFactor) const SAL_OVERRIDE;
     virtual void    GetCaretPositions( int nArraySize, sal_Int32* pCaretXArray ) const SAL_OVERRIDE;
     virtual bool    GetBoundRect( SalGraphics&, Rectangle& ) const SAL_OVERRIDE;
@@ -587,9 +587,9 @@ long CTLayout::GetTextWidth() const
     return lrint(GetWidth());
 }
 
-long CTLayout::FillDXArray( sal_Int32* pDXArray ) const
+DeviceCoordinate CTLayout::FillDXArray( DeviceCoordinate* pDXArray ) const
 {
-    long nPixWidth = GetTextWidth();
+    DeviceCoordinate nPixWidth = GetWidth();
     // short circuit requests which don't need full details
     if( !pDXArray )
         return nPixWidth;

vcl/source/glyphs/graphite_layout.cxx
@@ -732,7 +732,7 @@ sal_Int32 GraphiteLayout::GetTextBreak(long maxmnWidth, long char_extra, int fac
     return nBreak;
 }
 
-long GraphiteLayout::FillDXArray( sal_Int32* pDXArray ) const
+DeviceCoordinate GraphiteLayout::FillDXArray( DeviceCoordinate* pDXArray ) const
 {
     if (mnEndCharPos == mnMinCharPos)
         // Then we must be zero width!
@@ -766,7 +766,7 @@ long GraphiteLayout::FillDXArray( sal_Int32* pDXArray ) const
         //fprintf(grLog(),"FillDX %ld,%d\n", mnWidth, std::accumulate(pDXArray, pDXArray + mvCharDxs.size(), 0));
     }
 #ifdef GRLAYOUT_DEBUG
-    fprintf(grLog(),"FillDXArray %d-%d=%ld\n", mnMinCharPos, mnEndCharPos, mnWidth);
+    fprintf(grLog(),"FillDXArray %d-%d=%g\n", mnMinCharPos, mnEndCharPos, (double)mnWidth);
 #endif
     return mnWidth;
 }

commit 6defffea57ba803a47fd10d1496e0c5c477009c8

vcl: evidently a little more type safety is required here
MultiSalLayout::AdjustLayout() crashes with mangled stack.

(regression from 2b8528a2745bec7909bfe2265d6110a9964eef47)

Change-Id: I20b7980dd89dd1b72063c2187ac3a4735babc4c4

vcl/source/gdi/sallayout.cxx
@@ -1523,6 +1523,7 @@ void MultiSalLayout::AdjustLayout( ImplLayoutArgs& rArgs )
 {
     SalLayout::AdjustLayout( rArgs );
     ImplLayoutArgs aMultiArgs = rArgs;
+    boost::scoped_array<DeviceCoordinate> pJustificationArray;
 
     if( !rArgs.mpDXArray && rArgs.mnLayoutWidth )
     {
@@ -1537,8 +1538,8 @@ void MultiSalLayout::AdjustLayout( ImplLayoutArgs& rArgs )
             mpLayouts[n]->SalLayout::AdjustLayout( aMultiArgs );
         // then we can measure the unmodified metrics
         int nCharCount = rArgs.mnEndCharPos - rArgs.mnMinCharPos;
-        DeviceCoordinate* pJustificationArray = (DeviceCoordinate*)alloca( nCharCount * sizeof(sal_Int32) );
-        FillDXArray( pJustificationArray );
+        pJustificationArray.reset(new DeviceCoordinate[nCharCount]);
+        FillDXArray( pJustificationArray.get() );
         // #i17359# multilayout is not simplified yet, so calculating the
         // unjustified width needs handholding; also count the number of
         // stretchable virtual char widths
@@ -1585,8 +1586,8 @@ void MultiSalLayout::AdjustLayout( ImplLayoutArgs& rArgs )
                 }
             }
 
-            // change the mpDXArray temporarilly (just for the justification)
-            aMultiArgs.mpDXArray = pJustificationArray;
+            // change the mpDXArray temporarily (just for the justification)
+            aMultiArgs.mpDXArray = pJustificationArray.get();
         }
     }
 