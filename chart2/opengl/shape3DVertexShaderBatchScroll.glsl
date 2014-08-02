d15dcc08b83aac0d30826bbba2e4136e4501eda7
weigao <weigao@multicorewareinc.com>
2014-08-02 17:36:43 +0800

    add bars scroll shaders
    Change-Id: I006062986534950485ffa9e280caad3e37c2f35f

--- chart2/opengl/shape3DVertexShaderBatchScroll.glsl
+++ chart2/opengl/shape3DVertexShaderBatchScroll.glsl
@@ -9,22 +9,31 @@
 #version 330 core
 in vec3 vertexPositionModelspace;
 in vec3 vertexNormalModelspace;
+in mat4 M;
+in mat3 normalMatrix;
+in vec4 barColor;
 
 out vec3 positionWorldspace;
 out vec3 normalCameraspace;
+out vec4 fragBarColor;
 
 uniform mat4 P;
-uniform mat4 M;
 uniform mat4 V;
-uniform mat3 normalMatrix;
+uniform mat4 transMatrix;
+uniform float minCoordX;
+uniform float maxCoordX;
 
 void main()
 {
-    positionWorldspace = (M * vec4(vertexPositionModelspace,1)).xyz;
+	positionWorldspace = (transMatrix * M * vec4(vertexPositionModelspace,1)).xyz;
 
-    gl_Position =  P * V * vec4(positionWorldspace,1);
+	positionWorldspace.x = clamp(positionWorldspace.x, minCoordX, maxCoordX);
+
+	gl_Position =  P * V * vec4(positionWorldspace,1);
 
     normalCameraspace = normalize(mat3(V) * normalMatrix * vertexNormalModelspace);
+
+	fragBarColor = barColor;
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
