6cf40d263e33dd6934005af8319ae6fc8a386592
David Tardon <dtardon@redhat.com>
2014-08-04 09:27:24 +0200

    fdo#82035 fix loader paths
    Change-Id: Ibecd7a89491b487bec54e8a86edbb1b133cdb8f0

--- external/libwps/ExternalProject_libwps.mk
+++ external/libwps/ExternalProject_libwps.mk
@@ -39,6 +39,10 @@ $(call gb_ExternalProject_get_state_target,libwps,build) :
 			$(if $(CROSS_COMPILING),--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM)) \
 			$(if $(filter MACOSX,$(OS)),--prefix=/@.__________________________________________________OOO) \
 		&& $(MAKE) \
+		$(if $(filter MACOSX,$(OS)),\
+			&& $(PERL) $(SRCDIR)/solenv/bin/macosx-change-install-names.pl shl OOO \
+				$(EXTERNAL_WORKDIR)/src/lib/.libs/libwps-0.3.3.dylib \
+		) \
 	)
 
 # vim: set noet sw=4 ts=4:
