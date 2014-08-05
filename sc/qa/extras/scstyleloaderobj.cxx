b80d4e75333c6dc1e9e3742ac6d2e2d8fc98c722
Laurent Godard <lgodard.libre@laposte.net>
2014-08-05 17:02:49 +0200

    XStyleLoader2 loadStylesFromDocument - unit tests
    Change-Id: Id143689f573e6f8585db9bc48a252e311ff0e721

---
+++ sc/qa/extras/scstyleloaderobj.cxx
@@ -0,0 +1,128 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <test/calc_unoapi_test.hxx>
+#include <test/sheet/xstyleloader.hxx>
+#include <com/sun/star/sheet/XSpreadsheetDocument.hpp>
+#include <com/sun/star/beans/XPropertySet.hpp>
+
+
+using namespace css;
+using namespace css::uno;
+
+namespace sc_apitest {
+
+#define NUMBER_OF_TESTS 2
+
+class ScStyleLoaderObj : public CalcUnoApiTest, apitest::XStyleLoader
+{
+public:
+    ScStyleLoaderObj();
+
+    virtual void setUp();
+    virtual void tearDown();
+
+    virtual uno::Reference< uno::XInterface > init();
+    virtual uno::Reference< lang::XComponent  > getTargetComponent();
+    virtual uno::Reference< lang::XComponent > getSourceComponent();
+    virtual OUString getTestURL();
+
+    CPPUNIT_TEST_SUITE(ScStyleLoaderObj);
+    CPPUNIT_TEST(testLoadStylesFromURL);
+    CPPUNIT_TEST(testLoadStylesFromDocument);
+    CPPUNIT_TEST_SUITE_END();
+
+private:
+
+    static sal_Int32 nTest;
+    static uno::Reference< lang::XComponent > mxSourceComponent;
+//    static uno::Reference< sheet::XSpreadsheetDocument > mxTargetDocument;
+    static uno::Reference< lang::XComponent > mxTargetComponent;
+
+
+};
+
+sal_Int32 ScStyleLoaderObj::nTest = 0;
+uno::Reference< lang::XComponent > ScStyleLoaderObj::mxSourceComponent;
+//uno::Reference< sheet::XSpreadsheetDocument > ScStyleLoaderObj::mxTargetDocument;
+uno::Reference< lang::XComponent > ScStyleLoaderObj::mxTargetComponent;
+
+
+
+ScStyleLoaderObj::ScStyleLoaderObj()
+       : CalcUnoApiTest("sc/qa/extras/testdocuments")
+{
+}
+
+
+uno::Reference< uno::XInterface > ScStyleLoaderObj::init()
+{
+  return getTargetComponent();
+}
+
+
+uno::Reference< lang::XComponent > ScStyleLoaderObj::getTargetComponent(){
+  // target is always an empty document
+
+    if (mxTargetComponent.is())
+        closeDocument(mxTargetComponent);
+
+    mxTargetComponent = loadFromDesktop("private:factory/scalc");
+
+    return mxTargetComponent;
+}
+
+uno::Reference< lang::XComponent > ScStyleLoaderObj::getSourceComponent(){
+
+    if (mxSourceComponent.is())
+        closeDocument(mxSourceComponent);
+
+      // get the test file url
+      OUString aFileURL = getTestURL();
+
+      if(!mxSourceComponent.is())
+          mxSourceComponent = loadFromDesktop(aFileURL);
+      CPPUNIT_ASSERT_MESSAGE("Component not loaded",mxSourceComponent.is());
+
+    return mxSourceComponent;
+}
+
+OUString ScStyleLoaderObj::getTestURL(){
+
+      OUString aFileURL;
+      createFileURL(OUString("ScStyleLoaderObj.ods"), aFileURL);
+
+      return aFileURL;
+}
+
+void ScStyleLoaderObj::setUp()
+{
+    nTest++;
+    CalcUnoApiTest::setUp();
+}
+
+void ScStyleLoaderObj::tearDown()
+{
+    if (nTest == NUMBER_OF_TESTS)
+    {
+      if (mxSourceComponent.is())
+          closeDocument(mxSourceComponent);
+      if (mxTargetComponent.is())
+          closeDocument(mxTargetComponent);
+    }
+    CalcUnoApiTest::tearDown();
+}
+
+CPPUNIT_TEST_SUITE_REGISTRATION(ScStyleLoaderObj);
+
+}
+
+CPPUNIT_PLUGIN_IMPLEMENT();
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
--- test/Library_subsequenttest.mk
+++ test/Library_subsequenttest.mk
@@ -62,6 +62,7 @@ $(eval $(call gb_Library_add_exception_objects,subsequenttest,\
     test/source/sheet/xsheetannotation \
     test/source/sheet/xsheetannotations \
     test/source/sheet/xsheetoutline \
+    test/source/sheet/xstyleloader \
     test/source/text/xtext \
     test/source/text/xtextfield \
     test/source/text/xtextcontent \
