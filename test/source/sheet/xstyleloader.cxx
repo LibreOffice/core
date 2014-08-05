1984002ea458dfd223e49321740892c5b92055c2
Laurent Godard <lgodard.libre@laposte.net>
2014-08-05 17:02:49 +0200

    XStyleLoader2 loadStylesFromDocument - unit tests
    Change-Id: Id143689f573e6f8585db9bc48a252e311ff0e721

---
+++ test/source/sheet/xstyleloader.cxx
@@ -0,0 +1,100 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <test/sheet/xstyleloader.hxx>
+
+#include <com/sun/star/beans/XPropertySet.hpp>
+#include <com/sun/star/sheet/XSpreadsheetDocument.hpp>
+
+#include <com/sun/star/style/XStyleFamiliesSupplier.hpp>
+#include <com/sun/star/container/XNameContainer.hpp>
+
+#include <com/sun/star/style/XStyleLoader.hpp>
+#include <com/sun/star/style/XStyleLoader2.hpp>
+
+#include <com/sun/star/style/XStyle.hpp>
+
+
+#include <rtl/ustring.hxx>
+#include "cppunit/extensions/HelperMacros.h"
+
+using namespace css;
+using namespace css::uno;
+
+namespace apitest {
+
+
+void XStyleLoader::testLoadStylesFromURL()
+{
+
+  uno::Reference< sheet::XSpreadsheetDocument > xTargetDoc(init(), UNO_QUERY_THROW);
+
+  OUString aFileURL = getTestURL();
+
+  uno::Reference< style::XStyleFamiliesSupplier > xFamilySupplier (xTargetDoc, UNO_QUERY_THROW);
+  uno::Reference< style::XStyleLoader > xTargetStyleLoader (xFamilySupplier->getStyleFamilies(), UNO_QUERY_THROW);
+
+  uno::Sequence< beans::PropertyValue > aOptions = xTargetStyleLoader->getStyleLoaderOptions();
+
+  xTargetStyleLoader->loadStylesFromURL(aFileURL, aOptions);
+
+  // check if targetDocument has myStyle
+  uno::Reference< container::XNameAccess > xFamilies(xFamilySupplier->getStyleFamilies(), UNO_QUERY_THROW);
+  uno::Reference< container::XNameContainer > xCellStyles(xFamilies->getByName("CellStyles"), UNO_QUERY_THROW);
+
+  CPPUNIT_ASSERT_MESSAGE("Style not imported", xCellStyles->hasByName("myStyle"));
+
+  // test the backgroundcolor is correctly imported
+
+  uno::Reference< style::XStyle > xMyStyle (xCellStyles->getByName("myStyle"), UNO_QUERY_THROW);
+  uno::Reference< beans::XPropertySet > xPropSet (xMyStyle, UNO_QUERY_THROW);
+
+  OUString aCellStyleName("CellBackColor");
+  uno::Any aBackColor = xPropSet->getPropertyValue(aCellStyleName);
+  uno::Any expectedBackColor(16724787);
+
+  CPPUNIT_ASSERT_EQUAL_MESSAGE("Wrong CellBackColor" , expectedBackColor, aBackColor);
+
+}
+
+void XStyleLoader::testLoadStylesFromDocument()
+{
+
+  uno::Reference< sheet::XSpreadsheetDocument > xTargetDoc(init(), UNO_QUERY_THROW);
+
+  uno::Reference< lang::XComponent > xSourceDoc (getSourceComponent(), UNO_QUERY_THROW);
+
+  uno::Reference< style::XStyleFamiliesSupplier > xFamilySupplier (xTargetDoc, UNO_QUERY_THROW);
+  uno::Reference< style::XStyleLoader2 > xTargetStyleLoader (xFamilySupplier->getStyleFamilies(), UNO_QUERY_THROW);
+
+  uno::Sequence< beans::PropertyValue > aOptions = xTargetStyleLoader->getStyleLoaderOptions();
+
+  xTargetStyleLoader->loadStylesFromDocument(xSourceDoc, aOptions);
+
+  // check if targetDocument has myStyle
+  uno::Reference< container::XNameAccess > xFamilies(xFamilySupplier->getStyleFamilies(), UNO_QUERY_THROW);
+  uno::Reference< container::XNameContainer > xCellStyles(xFamilies->getByName("CellStyles"), UNO_QUERY_THROW);
+
+  CPPUNIT_ASSERT_MESSAGE("Style not imported", xCellStyles->hasByName("myStyle"));
+
+  // test the backgroundcolor is correctly imported
+  uno::Reference< style::XStyle > xMyStyle (xCellStyles->getByName("myStyle"), UNO_QUERY_THROW);
+  uno::Reference< beans::XPropertySet > xPropSet (xMyStyle, UNO_QUERY_THROW);
+
+  OUString aCellStyleName("CellBackColor");
+  uno::Any aBackColor = xPropSet->getPropertyValue(aCellStyleName);
+  uno::Any expectedBackColor(16724787);
+
+  CPPUNIT_ASSERT_EQUAL_MESSAGE("Wrong CellBackColor" , expectedBackColor, aBackColor);
+
+}
+
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
