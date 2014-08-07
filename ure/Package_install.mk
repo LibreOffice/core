f5f1f8332584a1bbc1d1a8a688fb3d0bce24a5dd
Tor Lillqvist <tml@collabora.com>
2014-08-08 00:31:29 +0300

    Fixes for the --enable-canonical-installation-tree-structure case
    Do install the uno and startup.sh commands, smoketest needs them
    
    Change-Id: Ia530564e4b664881d730355a80e2af4476987e10
    (cherry picked from commit b6869717a389d65bd50e8096d033a689d9298e84)

--- ure/Package_install.mk
+++ ure/Package_install.mk
@@ -10,10 +10,8 @@
 $(eval $(call gb_Package_Package,ure_install,$(SRCDIR)/ure/source))
 
 ifneq (WNT,$(OS))
-ifeq ($(ENABLE_MACOSX_MACLIKE_APP_STRUCTURE),)
 $(eval $(call gb_Package_add_file,ure_install,$(LIBO_URE_BIN_FOLDER)/startup.sh,startup.sh))
 $(eval $(call gb_Package_add_symbolic_link,ure_install,$(LIBO_URE_BIN_FOLDER)/uno,startup.sh))
-endif
 endif
 
 ifeq (MACOSX,$(OS))
