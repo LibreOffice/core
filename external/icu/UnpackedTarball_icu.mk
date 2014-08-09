d79d2f0ef7a0e300a3ee4be128c625121d0c4b26
Caolán McNamara <caolanm@redhat.com>
2014-08-09 21:14:03 +0100

    Resolves: icu#11054 crashes in bracketAddOpening
    Change-Id: I5dd63fa41c1568e8bf2d120cc0de5d2c44dd789c

--- external/icu/UnpackedTarball_icu.mk
+++ external/icu/UnpackedTarball_icu.mk
@@ -22,6 +22,7 @@ $(eval $(call gb_UnpackedTarball_add_patches,icu,\
 	external/icu/icu4c-buffer-overflow.patch \
 	external/icu/icu4c-$(if $(filter ANDROID,$(OS)),android,rpath).patch \
 	external/icu/icu-ubsan.patch.0 \
+	external/icu/icu4c-icu11054.patch.1 \
 ))
 
 # vim: set noet sw=4 ts=4:
