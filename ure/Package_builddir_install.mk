8e9487cf97d944b202d4406561b88068aaf8e386
Tor Lillqvist <tml@collabora.com>
2014-08-10 19:41:24 +0300

    Ugly hack to fix build when srcdir!=builddir
    The problem is that the ure/source/unorc file is now expanded by the configure
    script, and thus exists only in builddir. But a further complication is that
    the uno.ini file is in srcdir. This is one way to handle it. Seems to work for
    me, let's see what the srcdir!=builddir tinderbox slaves say.
    
    Change-Id: I6fb456cf849ce5077e2c5bd25dc9149096aab241

--- ure/Package_builddir_install.mk
+++ ure/Package_builddir_install.mk
@@ -7,12 +7,10 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 
-$(eval $(call gb_Module_Module,ure))
+$(eval $(call gb_Package_Package,ure_builddir_install,$(BUILDDIR)/ure/source))
 
-$(eval $(call gb_Module_add_targets,ure,\
-	Package_install \
-	Package_builddir_install \
-	Rdb_ure \
+$(eval $(call gb_Package_add_files,ure_builddir_install,$(LIBO_URE_ETC_FOLDER),\
+	$(if $(filter unorc,$(call gb_Helper_get_rcfile,uno)),$(call gb_Helper_get_rcfile,uno)) \
 ))
 
 # vim:set noet sw=4 ts=4:
