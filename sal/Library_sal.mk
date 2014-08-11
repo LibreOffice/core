c35f923e1902de3beddfbcac3d4787e8e6f6d824
Tor Lillqvist <tml@collabora.com>
2014-08-11 08:10:38 +0300

    "fixes" needed because ure/source/unorc is now expanded at configure time
    Argh, this is getting even uglier.
    
    We cheerfully ignore for now the theoretical possibility that the URE
    unorc used by build-time tools (i.e. the configure-expanded
    ure/source/unorc) could be different for HOST and BUILD (in case they
    use different --enable-canonical-installation-tree-structure), and use
    the HOST one for the BUILD tools.
    
    The right thing would probably be to construct the URE unorc in the
    relevant Makefile, like we do for fundamentalrc? Or then to just
    re-design the whole mess of rc files into some simpler (good luck).
    
    Change-Id: I654309503d0e696778910acadcbf2f6b90ffa02a

--- sal/Library_sal.mk
+++ sal/Library_sal.mk
@@ -36,6 +36,7 @@ $(eval $(call gb_Library_add_defs,sal,\
 # need the "ure-link" symlink to exist in INSTDIR so it's possible to link sal
 # FIXME: this creates cyclic dependency between ure and sal modules
 $(eval $(call gb_Library_use_package,sal,ure_install))
+$(eval $(call gb_Library_use_package,sal,ure_builddir_install))
 
 $(eval $(call gb_Library_use_libraries,sal,\
 	$(if $(filter $(OS),ANDROID), \
