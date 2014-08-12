f0e72d5afbb34335813255f66e406d9f0274101e
matteocam <matteo.campanelli@gmail.com>
2014-08-12 15:52:29 +0200

    restoring unsetting of EE_CNTRL_AUTOPAGESIZE
    Change-Id: Ic23f7d8e1646dac7ed3e0ec8d41e7ed2cb56f2aa

--- svx/source/svdraw/svdotxed.cxx
+++ svx/source/svdraw/svdotxed.cxx
@@ -63,9 +63,9 @@ bool SdrTextObj::BegTextEdit(SdrOutliner& rOutl)
     }
 
     // FIXME(matteocam)
-    /*sal_uIntPtr nStat1=rOutl.GetControlWord();
+    sal_uIntPtr nStat1=rOutl.GetControlWord();
     nStat1 &=~EE_CNTRL_AUTOPAGESIZE;
-    rOutl.SetControlWord(nStat1);*/
+    rOutl.SetControlWord(nStat1);
 
     OutlinerParaObject* pOutlinerParaObject = GetOutlinerParaObject();
     if(pOutlinerParaObject!=NULL)
