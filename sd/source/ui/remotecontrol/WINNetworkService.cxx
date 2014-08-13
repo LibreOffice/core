d7e5ad0bb2ef455254261f2f30a13de6a948f1ed
Michael Stahl <mstahl@redhat.com>
2014-08-13 23:38:13 +0200

    warning C4189 local variable initialized but not referenced
    Change-Id: I6047e0fc10bb6de0009da40855a97df89904cb0c

--- sd/source/ui/remotecontrol/WINNetworkService.cxx
+++ sd/source/ui/remotecontrol/WINNetworkService.cxx
@@ -7,6 +7,9 @@ void sd::WINNetworkService::setup()
 {
     DNSServiceErrorType err = DNSServiceRegister(&client, 0, 0, NULL, kREG_TYPE, "local", NULL, 1599, 1, "", NULL, this );
 
+    if (kDNSServiceErr_NoError != err)
+        SAL_WARN("sdremote.wifi", "DNSServiceRegister failed: " << err);
+
     // Fail silently
 }
 
