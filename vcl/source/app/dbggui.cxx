0e3a462c641a3a7a3b0d991463f9b05683c7ec5d
Caolán McNamara <caolanm@redhat.com>
2014-08-18 11:20:44 +0100

    missing InfoBox
    Change-Id: I67e6e60e5a03696ec27b11afaa62ea443c5924ce

--- vcl/source/app/dbggui.cxx
+++ vcl/source/app/dbggui.cxx
@@ -349,10 +349,9 @@ IMPL_LINK( DbgDialog, ClickHdl, Button*, pButton )
         }
         if( (aData.nTestFlags & ~IMMEDIATE_FLAGS) != (pData->nTestFlags & ~IMMEDIATE_FLAGS) )
         {
-            InfoBox aBox( this, OUString(
+            MessageDialog aBox(this, OUString(
                 "Some of the changed settings will only be active after "
-                "restarting the process"
-                ) );
+                "restarting the process"), VCL_MESSAGE_INFO);
             aBox.Execute();
         }
         EndDialog( RET_OK );
