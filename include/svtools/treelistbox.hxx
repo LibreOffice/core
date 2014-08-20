LibreOffice/core

commit ac7acb0ab1329913b0cec79790adcde0263960be

Merged SvTreeListBox and SvLBox.
There is no reason to keep SvLBox separate.

Change-Id: I0b34aa8f8ca539d4f4f0f9cea9fa9b8faec241e8

sw/source/ui/inc/conttree.hxx
@@ -28,7 +28,8 @@
 #ifndef _CONTTREE_HXX
 #define _CONTTREE_HXX
 
-#include <svtools/svtreebx.hxx>
+#include "svtools/treelistbox.hxx"
+#include "svtools/svlbitm.hxx"
 #include "swcont.hxx"
 
 #include <map>
@@ -101,7 +102,7 @@ class SwContentTree : public SvTreeListBox
 
     void                FindActiveTypeAndRemoveUserData();
 
-    using SvLBox::ExecuteDrop;
+    using SvTreeListBox::ExecuteDrop;
     using SvTreeListBox::EditEntry;
     using SvListView::Expand;
     using SvListView::Collapse;
@@ -219,7 +220,7 @@ public:
     {
     }
 
-    virtual void Paint( const Point& rPos, SvLBox& rDev, sal_uInt16 nFlags,
+    virtual void Paint( const Point& rPos, SvTreeListBox& rDev, sal_uInt16 nFlags,
         SvLBoxEntry* pEntry);
 };
 
@@ -254,8 +255,8 @@ private:
 
     DECL_LINK(  DialogClosedHdl, sfx2::FileDialogHelper* );
 
-    using SvLBox::DoubleClickHdl;
-    using SvLBox::ExecuteDrop;
+    using SvTreeListBox::DoubleClickHdl;
+    using SvTreeListBox::ExecuteDrop;
     using Window::Update;
 
 protected:

commit d94adafa6bbfb61864e38940fed2d42f6cf34050

fdo#58187 Expand and Collapse should be virtual
regression from ac7acb0ab1329913b0cec79790adcde0263960be

(cherry picked from commit 6eaad9e41ae95a3d973e72439ef580349e3e8e4b)

Conflicts:

	include/svtools/treelistbox.hxx
	sw/source/core/uibase/inc/conttree.hxx

Change-Id: I773871f08f43a8abadfc5ef9b641db722deb7d20

include/svtools/treelistbox.hxx
@@ -712,8 +712,8 @@ public:
     void            SetDragDropMode( DragDropMode );
     void            SetSelectionMode( SelectionMode );
 
-    sal_Bool Expand( SvTreeListEntry* pParent );
-    sal_Bool Collapse( SvTreeListEntry* pParent );
+    virtual sal_Bool Expand( SvTreeListEntry* pParent );
+    virtual sal_Bool Collapse( SvTreeListEntry* pParent );
     virtual sal_Bool Select( SvTreeListEntry* pEntry, sal_Bool bSelect=sal_True );
     sal_uLong SelectChildren( SvTreeListEntry* pParent, sal_Bool bSelect );
     virtual void SelectAll( sal_Bool bSelect, sal_Bool bPaint = sal_True );

sw/source/ui/inc/conttree.hxx
@@ -170,9 +170,9 @@ public:
     sal_uInt8           GetOutlineLevel()const {return nOutlineLevel;}
     void            SetOutlineLevel(sal_uInt8 nSet);
 
-    sal_Bool            Expand( SvTreeListEntry* pParent );
+    virtual sal_Bool            Expand( SvTreeListEntry* pParent );
 
-    sal_Bool            Collapse( SvTreeListEntry* pParent );
+    virtual sal_Bool            Collapse( SvTreeListEntry* pParent );
 
     void            ExecCommand(sal_uInt16 nCmd, sal_Bool bModifier);
 