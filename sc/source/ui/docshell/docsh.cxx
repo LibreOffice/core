a9969503d7a95bf266099466ca08f732f1251c03
Eike Rathke <erack@redhat.com>
2014-08-21 23:14:52 +0200

    fdo#82183 do not reset globals while loading a document
    Destroying the function list while an instance of the Formula Wizard is
    still open is a bad idea. Workaround not doing this when loading a
    document due to a DDE function or external reference being entered in
    the wizard.
    
    (cherry picked from commit c0aba5007b6e468336b41138f099914c32f4b0cf)
    
    init formula options once, fdo#82183
    
    (cherry picked from commit 10b6bda51c9da5429ca562c70ce75ee03e5f4e56)
    
    Change-Id: I6fa00fb4f442bf7c9410679e446ff460289e4b16
    Reviewed-on: https://gerrit.libreoffice.org/11063
    Reviewed-by: Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>
    Tested-by: Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>

--- sc/source/ui/docshell/docsh.cxx
+++ sc/source/ui/docshell/docsh.cxx
@@ -486,6 +486,8 @@ bool ScDocShell::LoadXML( SfxMedium* pLoadMedium, const ::com::sun::star::uno::R
                 officecfg::Office::Calc::Formula::Load::ODFRecalcMode::set(sal_Int32(0), batch);
                 ScFormulaOptions aOpt = SC_MOD()->GetFormulaOptions();
                 aOpt.SetODFRecalcOptions(bHardRecalc ? RECALC_ALWAYS : RECALC_NEVER);
+                /* XXX  is this really supposed to set the ScModule options?
+                 *      Not the ScDocShell options? */
                 SC_MOD()->SetFormulaOptions(aOpt);
 
                 batch->commit();
