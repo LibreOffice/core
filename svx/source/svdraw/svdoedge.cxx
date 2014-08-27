f37f93f69b28328d252b8a10df709d1f70e75bc9
Caolán McNamara <caolanm@redhat.com>
2014-08-27 16:40:58 +0100

    Resolves: fdo#83062 infinite recursion
    regression from
    
    commit 7c03fc2fe77f9b1f910f4ab395923e52648c32b5
    Date:   Mon Sep 2 14:30:09 2013 +0000
    Related: #i123048# Corrected connector layout after reload
    
    verified that #i123048# continues to work
    
    (cherry picked from commit 8d8e8942b2ee2a2e85c65d71628057316c9f5897)
    
    Conflicts:
    	svx/source/svdraw/svdoedge.cxx
    
    Change-Id: If5fce4b7a88d4d0972f52705334e4b9b48425cca
    Reviewed-on: https://gerrit.libreoffice.org/11150
    Reviewed-by: Eike Rathke <erack@redhat.com>
    Tested-by: Eike Rathke <erack@redhat.com>

--- svx/source/svdraw/svdoedge.cxx
+++ svx/source/svdraw/svdoedge.cxx
@@ -570,6 +570,10 @@ void SdrEdgeObj::ImpRecalcEdgeTrack()
     }
     else
     {
+        // To not run in a depth loop, use a coloring algorithm on
+        // SdrEdgeObj BoundRect calculations
+        mbBoundRectCalculationRunning = sal_True;
+
         if(mbSuppressed)
         {
             // #i123048# If layouting was ever suppressed, it needs to be done once
@@ -580,10 +584,6 @@ void SdrEdgeObj::ImpRecalcEdgeTrack()
             ImpSetAttrToEdgeInfo();
             mbSuppressed = false;
         }
-
-        // To not run in a depth loop, use a coloring algorythm on
-        // SdrEdgeObj BoundRect calculations
-        mbBoundRectCalculationRunning = sal_True;
 
         Rectangle aBoundRect0; if (pUserCall!=NULL) aBoundRect0=GetCurrentBoundRect();
         SetRectsDirty();
