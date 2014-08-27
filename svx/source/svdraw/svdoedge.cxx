LibreOffice/core

commit 7c03fc2fe77f9b1f910f4ab395923e52648c32b5

Related: #i123048# Corrected connector layout after reload
(cherry picked from commit c7d96eed93415894c5ca4522bd32ca06acd88233)

Conflicts:
	svx/inc/svx/svdoedge.hxx
	svx/source/svdraw/svdoedge.cxx

Change-Id: Id388fa7396751a2e8b5908373ca57eceee1a1af1

svx/source/svdraw/svdoedge.cxx
@@ -174,7 +174,8 @@ SdrEdgeObj::SdrEdgeObj()
     bEdgeTrackUserDefined(sal_False),
     // Default is to allow default connects
     mbSuppressDefaultConnect(sal_False),
-    mbBoundRectCalculationRunning(sal_False)
+    mbBoundRectCalculationRunning(sal_False),
+    mbSuppressed(false)
 {
     bClosedObj=sal_False;
     bIsEdge=sal_True;
@@ -546,12 +547,20 @@ void SdrEdgeObj::ImpUndirtyEdgeTrack()
 
 void SdrEdgeObj::ImpRecalcEdgeTrack()
 {
-    // #i120437# if bEdgeTrackUserDefined, do not recalculate. Also not when model locked
-    if(bEdgeTrackUserDefined || !GetModel() || GetModel()->isLocked())
+    // #i120437# if bEdgeTrackUserDefined, do not recalculate
+    if(bEdgeTrackUserDefined)
     {
         return;
     }
 
+    // #i120437# also not when model locked during import, but remember
+    if(!GetModel() || GetModel()->isLocked())
+    {
+        mbSuppressed = true;
+        return;
+    }
+
+    // #i110649#
     if(IsBoundRectCalculationRunning())
     {
         // This object is involved into another ImpRecalcEdgeTrack() call
@@ -559,20 +568,20 @@ void SdrEdgeObj::ImpRecalcEdgeTrack()
         // Also, do not change bEdgeTrackDirty so that it gets recalculated
         // later at the first non-looping call.
     }
-    // #i43068#
-    else if(GetModel() && GetModel()->isLocked())
-    {
-        // avoid re-layout during imports/API call sequences
-        // #i45294# but calculate EdgeTrack and secure properties there
-        mbBoundRectCalculationRunning = sal_True;
-        *pEdgeTrack=ImpCalcEdgeTrack(*pEdgeTrack,aCon1,aCon2,&aEdgeInfo);
-        ImpSetAttrToEdgeInfo();
-        bEdgeTrackDirty=sal_False;
-        mbBoundRectCalculationRunning = sal_False;
-    }
     else
     {
-        // To not run in a depth loop, use a coloring algorithm on
+        if(mbSuppressed)
+        {
+            // #i123048# If layouting was ever suppressed, it needs to be done once
+            // and the attr need to be set at EdgeInfo, else these attr *will be lost*
+            // in the following call to ImpSetEdgeInfoToAttr() sice they were never
+            // set before (!)
+            *pEdgeTrack=ImpCalcEdgeTrack(*pEdgeTrack,aCon1,aCon2,&aEdgeInfo);
+            ImpSetAttrToEdgeInfo();
+            mbSuppressed = false;
+        }
+
+        // To not run in a depth loop, use a coloring algorythm on
         // SdrEdgeObj BoundRect calculations
         mbBoundRectCalculationRunning = sal_True;
 

include/svx/svdoedge.hxx
@@ -182,6 +182,10 @@ protected:
     // is running, the flag is set, else it is always sal_False.
     unsigned                    mbBoundRectCalculationRunning : 1;
 
+    // #i123048# need to remember if layouting was suppressed before to get
+    // to a correct state for first real layouting
+    unsigned                    mbSuppressed : 1;
+
 public:
     // #109007#
     // Interface to default connect suppression

commit 8d8e8942b2ee2a2e85c65d71628057316c9f5897

Resolves: fdo#83062 infinite recursion
regression from

commit 7c03fc2fe77f9b1f910f4ab395923e52648c32b5
Date:   Mon Sep 2 14:30:09 2013 +0000
Related: #i123048# Corrected connector layout after reload

verified that #i123048# continues to work

Change-Id: If5fce4b7a88d4d0972f52705334e4b9b48425cca

svx/source/svdraw/svdoedge.cxx
@@ -570,6 +570,10 @@ void SdrEdgeObj::ImpRecalcEdgeTrack()
     }
     else
     {
+        // To not run in a depth loop, use a coloring algorithm on
+        // SdrEdgeObj BoundRect calculations
+        mbBoundRectCalculationRunning = true;
+
         if(mbSuppressed)
         {
             // #i123048# If layouting was ever suppressed, it needs to be done once
@@ -580,10 +584,6 @@ void SdrEdgeObj::ImpRecalcEdgeTrack()
             ImpSetAttrToEdgeInfo();
             mbSuppressed = false;
         }
-
-        // To not run in a depth loop, use a coloring algorithm on
-        // SdrEdgeObj BoundRect calculations
-        mbBoundRectCalculationRunning = true;
 
         Rectangle aBoundRect0; if (pUserCall!=NULL) aBoundRect0=GetCurrentBoundRect();
         SetRectsDirty();