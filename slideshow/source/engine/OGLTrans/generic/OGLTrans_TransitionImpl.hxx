f605b8f7536a8fccf74cff6a69829813922c8ecf
Markus Mohrhard <markus.mohrhard@googlemail.com>
2014-08-27 16:51:07 +0200

    no need to make these variables public
    Change-Id: I9b57d6811252c75186e03ed6928bd8829fa0307c

--- slideshow/source/engine/OGLTrans/generic/OGLTrans_TransitionImpl.hxx
+++ slideshow/source/engine/OGLTrans/generic/OGLTrans_TransitionImpl.hxx
@@ -347,6 +347,7 @@ class Operation : private boost::noncopyable
 public:
     virtual ~Operation(){}
 
+protected:
     /** Should this operation be interpolated . If TRUE, the transform will smoothly move from making no difference from t = 0.0 to nT0 to being completely transformed from t = nT1 to 1. If FALSE, the transform will be inneffectual from t = 0 to nT0, and completely transformed from t = nT0 to 1.
     */
     bool bInterpolate;
@@ -374,7 +375,8 @@ public:
     virtual void interpolate(double t,double SlideWidthScale,double SlideHeightScale) const = 0;
 
 protected:
-    Operation():bInterpolate(false), nT0(0.0), nT1(0.0){}
+    Operation():
+        bInterpolate(false), nT0(0.0), nT1(0.0){}
 };
 
 /** this class is a generic CounterClockWise(CCW) rotation with an axis angle
