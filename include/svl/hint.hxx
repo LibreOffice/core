40c45e0fac44c2a43ef556ea86d08a69d8958bce
Noel Grandin <noel@peralex.com>
2014-08-27 08:31:36 +0200

    remove unused bIsOwner field in SfxHint
    Change-Id: I676039b07519fbed3b92c6ada0fa8217ee2cbe27

--- include/svl/hint.hxx
+++ include/svl/hint.hxx
@@ -36,21 +36,18 @@ public:
         class Visibility Name: public SfxHint \
         { \
             Type* pObj; \
-            bool  bIsOwner; \
         \
         public: \
             TYPEINFO_OVERRIDE(); \
-            explicit Name( Type* Object, bool bOwnedByHint = false ); \
+            explicit Name( Type* Object ); \
             virtual ~Name(); \
         \
             Type* GetObject() const { return pObj; } \
-            bool  IsOwner() const { return bIsOwner; } \
         }
 
 #define IMPL_PTRHINT(Name, Type) \
         TYPEINIT1(Name, SfxHint);   \
-        Name::Name( Type* pObject, bool bOwnedByHint ) \
-            { pObj = pObject; bIsOwner = bOwnedByHint; } \
+        Name::Name( Type* pObject ) { pObj = pObject; } \
         Name::~Name() {}
 
 
