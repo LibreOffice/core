7175b781e2e9a20a140b1122a33ddcf5a4f9a0d4
Tomaž Vajngerl <tomaz.vajngerl@collabora.com>
2014-08-28 13:32:38 +0200

    bnc#822347: if number type is not set, skip numbering
    If numbering is detected then (level is > 0) and the number type
    is not set, the defult bullet symbol is written. This is not
    correct as the default should be SVX_NUM_NUMBER_NONE which should
    skip numbering or set it to none. With this change the numbering
    is skipped (as in MSO).
    
    (cherry picked from commit 14fa2698f2f651343675761e75be01b84c4c5ff1)
    
    Conflicts:
    	oox/source/export/drawingml.cxx
    
    Change-Id: I8d08a6325509c7bd6f96f64c8d29e5f3045458ca

--- oox/source/export/drawingml.cxx
+++ oox/source/export/drawingml.cxx
@@ -1063,7 +1063,7 @@ void DrawingML::WriteParagraphNumbering( Reference< XPropertySet > rXPropSet, sa
 
             if ( nPropertyCount ) {
 
-                sal_Int16 nNumberingType = -1;
+                sal_Int16 nNumberingType = SVX_NUM_NUMBER_NONE;
                 bool bSDot = false;
                 bool bPBehind = false;
                 bool bPBoth = false;
@@ -1124,6 +1124,9 @@ void DrawingML::WriteParagraphNumbering( Reference< XPropertySet > rXPropSet, sa
                         }
                     }
                 }
+
+                if (nNumberingType == SVX_NUM_NUMBER_NONE)
+                    return;
 
                 const char* pAutoNumType = GetAutoNumType( nNumberingType, bSDot, bPBehind, bPBoth );
 
