2f60a1bdabac11e985dbcc6265dd8b97ef1ca6fd
Caolán McNamara <caolanm@redhat.com>
2014-09-03 15:11:32 +0100

    forget about the Left/Top/Right/Bottom frame distance
    I think these are actually already factored in, at
    least the undo popup doesn't want it and a quick look
    around gives 0 for other dialogs.
    
    Change-Id: Ib5abf91992b0e6075dbdce209798596f21342818

--- vcl/source/window/syswin.cxx
+++ vcl/source/window/syswin.cxx
@@ -1031,14 +1031,10 @@ void SystemWindow::setPosSizeOnContainee(Size aSize, VclContainer &rBox)
 {
     sal_Int32 nBorderWidth = get_border_width();
 
-    aSize.Width() -= mpWindowImpl->mnLeftBorder + mpWindowImpl->mnRightBorder
-        + 2 * nBorderWidth;
-    aSize.Height() -= mpWindowImpl->mnTopBorder + mpWindowImpl->mnBottomBorder
-        + 2 * nBorderWidth;
+    aSize.Width() -= 2 * nBorderWidth;
+    aSize.Height() -= 2 * nBorderWidth;
 
-    Point aPos(mpWindowImpl->mnLeftBorder + nBorderWidth,
-        mpWindowImpl->mnTopBorder + nBorderWidth);
-
+    Point aPos(nBorderWidth, nBorderWidth);
     VclContainer::setLayoutAllocation(rBox, aPos, aSize);
 }
 
