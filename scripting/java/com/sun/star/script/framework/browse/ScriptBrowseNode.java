bfde912e865c041be840177655e4d8257a75b109
rbuj <robert.buj@gmail.com>
2014-09-06 21:15:33 +0200

    scripting: avoid a possible NullPointerException
    Change-Id: I90589bc042f4b88b700e9f3c167c460c3cff043c

--- scripting/java/com/sun/star/script/framework/browse/ScriptBrowseNode.java
+++ scripting/java/com/sun/star/script/framework/browse/ScriptBrowseNode.java
@@ -65,44 +65,42 @@ public class ScriptBrowseNode extends PropertySet
         this.name = name;
         this.parent = parent;
         ScriptMetaData data = null;
-        XSimpleFileAccess xSFA = null;
         XComponentContext xCtx = provider.getScriptingContext().getComponentContext();
         XMultiComponentFactory xFac = xCtx.getServiceManager();
         try
         {
             data = (ScriptMetaData)parent.getByName( name );
-            xSFA = UnoRuntime.queryInterface( XSimpleFileAccess.class,
+            XSimpleFileAccess xSFA = UnoRuntime.queryInterface( XSimpleFileAccess.class,
                 xFac.createInstanceWithContext(
                     "com.sun.star.ucb.SimpleFileAccess",
                     xCtx ) );
-        }
 
+            if (provider.hasScriptEditor())
+            {
+                this.editable = true;
+                try
+                {
+                    if (!parent.isUnoPkg()
+                            && !xSFA.isReadOnly(parent.getPathToParcel()))
+                    {
+                        this.deletable = true;
+                        this.renamable = true;
+                    }
+                }
+                // TODO propagate errors
+                catch (Exception e)
+                {
+                    LogUtils.DEBUG("Caught exception in creation of ScriptBrowseNode");
+                    LogUtils.DEBUG(LogUtils.getTrace(e));
+                }
+
+            }
+
+        }
         // TODO fix exception types to be caught here, should we rethrow?
         catch (  Exception e )
         {
             LogUtils.DEBUG("** caught exception getting script data for " + name + " ->" + e.toString() );
-        }
-
-        if (provider.hasScriptEditor())
-        {
-
-            this.editable  = true;
-            try
-            {
-                if ( !parent.isUnoPkg() &&
-                    !xSFA.isReadOnly( parent.getPathToParcel() ) )
-                {
-                    this.deletable = true;
-                    this.renamable = true;
-                }
-            }
-            // TODO propagate errors
-            catch ( Exception e )
-            {
-                LogUtils.DEBUG("Caught exception in creation of ScriptBrowseNode");
-                LogUtils.DEBUG( LogUtils.getTrace(e));
-            }
-
         }
 
         registerProperty("Deletable", new Type(boolean.class),
