da3b039d41298b2c85ccf632577eb0e5699c76b8
rbuj <robert.buj@gmail.com>
2014-09-06 16:04:59 +0200

    reportbuilder: fix javadoc error: reference not found
    Change-Id: Ic548a23a9d1e1acc90386da76416c76990f3a5b3
    Reviewed-on: https://gerrit.libreoffice.org/11309
    Reviewed-by: Noel Grandin <noelgrandin@gmail.com>
    Tested-by: Noel Grandin <noelgrandin@gmail.com>

--- reportbuilder/java/org/libreoffice/report/DataSourceException.java
+++ reportbuilder/java/org/libreoffice/report/DataSourceException.java
@@ -70,7 +70,7 @@ public class DataSourceException extends Exception
      * <tt>(cause==null ? null : cause.toString())</tt> (which typically contains
      * the class and detail message of <tt>cause</tt>). This constructor is useful
      * for exceptions that are little more than wrappers for other throwables (for
-     * example, {@link PrivilegedActionException}).
+     * example, {@link java.security.PrivilegedActionException}).
      *
      * @param cause the cause (which is saved for later retrieval by the {@link
      *              #getCause()} method).  (A <tt>null</tt> value is permitted,
