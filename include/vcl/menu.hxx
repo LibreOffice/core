LibreOffice/core

commit fcc49e118c9da28309dce3389d111a546cba19cd

vcl menu: Make CloseStartedFrom() virtual to eliminate IsMenuBar() call.
Change-Id: Ifd89b7a9264075b166dd621fcb2c55225e25ea87

include/vcl/menu.hxx
@@ -187,7 +187,7 @@ protected:
     SAL_DLLPRIVATE void                ImplRemoveDel( ImplMenuDelData &rDel );
 
     /// Close the 'pStartedFrom' menu window.
-    SAL_DLLPRIVATE void CloseStartedFrom();
+    virtual void CloseStartedFrom() = 0;
 
     /// Deactivate the MenuBarWindow.
     SAL_DLLPRIVATE sal_uLong DeactivateMenuBar(sal_uLong nFocusId);
@@ -420,6 +420,9 @@ public:
 
     virtual bool        IsMenuBar() const SAL_OVERRIDE { return true; }
 
+    /// Close the 'pStartedFrom' menu window.
+    virtual void CloseStartedFrom() SAL_OVERRIDE;
+
     void                ShowCloseButton( bool bShow = true );
     bool                HasCloseButton() const { return mbCloseBtnVisible; }
     bool            HasFloatButton() const { return mbFloatBtnVisible; }
@@ -503,6 +506,9 @@ public:
     virtual             ~PopupMenu();
 
     virtual bool        IsMenuBar() const SAL_OVERRIDE { return false; }
+
+    /// Close the 'pStartedFrom' menu window.
+    virtual void CloseStartedFrom() SAL_OVERRIDE;
 
     void                SetText( const OUString& rTitle )  { aTitleText = rTitle; }
     const OUString&     GetText() const                     { return aTitleText; }

vcl/source/window/menu.cxx
@@ -2263,30 +2263,6 @@ bool Menu::HasValidEntries( bool bCheckPopups )
     return bValidEntries;
 }
 
-void Menu::CloseStartedFrom()
-{
-    // if the window was closed by TH, there is another menu
-    // which has this window as pActivePopup
-    if (!pStartedFrom)
-        return;
-
-    // pWin from parent could be 0, if the list is
-    // cleaned from the start, now clean up the endpopup-events
-    if (pStartedFrom->IsMenuBar())
-    {
-        MenuBarWindow* p = (MenuBarWindow*) pStartedFrom->ImplGetWindow();
-        if (p)
-            p->PopupClosed(this);
-    }
-    else
-    {
-        MenuFloatingWindow* p = (MenuFloatingWindow*) pStartedFrom->ImplGetWindow();
-        PopupMenu *pMenu = dynamic_cast<PopupMenu*>(this);
-        if (p && pMenu)
-            p->KillActivePopup(pMenu);
-    }
-}
-
 sal_uLong Menu::DeactivateMenuBar(sal_uLong nFocusId)
 {
     if (!IsMenuBar())
@@ -2489,6 +2465,18 @@ MenuBar::MenuBar( const MenuBar& rMenu )
 MenuBar::~MenuBar()
 {
     ImplDestroy( this, true );
+}
+
+void MenuBar::CloseStartedFrom()
+{
+    // if the window was closed by TH, there is another menu
+    // which has this window as pActivePopup
+    if (!pStartedFrom)
+        return;
+
+    MenuBarWindow* p = (MenuBarWindow*) pStartedFrom->ImplGetWindow();
+    if (p)
+        p->PopupClosed(this);
 }
 
 void MenuBar::ShowCloseButton(bool bShow)
@@ -2715,6 +2703,19 @@ PopupMenu::~PopupMenu()
 {
     if( pRefAutoSubMenu && *pRefAutoSubMenu == this )
         *pRefAutoSubMenu = NULL;    // #111060# avoid second delete in ~MenuItemData
+}
+
+void PopupMenu::CloseStartedFrom()
+{
+    // if the window was closed by TH, there is another menu
+    // which has this window as pActivePopup
+    if (!pStartedFrom)
+        return;
+
+    MenuFloatingWindow* p = (MenuFloatingWindow*) pStartedFrom->ImplGetWindow();
+    PopupMenu *pMenu = dynamic_cast<PopupMenu*>(this);
+    if (p && pMenu)
+        p->KillActivePopup(pMenu);
 }
 
 bool PopupMenu::IsInExecute()

commit 2636ab883adbcb1f6593787934c6d6d3fff67c21

vcl menu: Rename CloseStartedFrom() to ClosePopup() + adapt code.
Fixes a regression from fcc49e118c9da28309dce3389d111a546cba19cd (clicking
outside of an opened menubar's menu did not close the menubar).

Change-Id: Ib6efddd6c250b173469772c7b2ae642b5ef609aa

include/vcl/menu.hxx
@@ -186,7 +186,7 @@ protected:
     SAL_DLLPRIVATE void                ImplRemoveDel( ImplMenuDelData &rDel );
 
     /// Close the 'pStartedFrom' menu window.
-    virtual void CloseStartedFrom() = 0;
+    virtual void ClosePopup(Menu* pMenu) = 0;
 
     /// Deactivate the MenuBarWindow.
     virtual sal_uLong DeactivateMenuBar(sal_uLong nFocusId);
@@ -420,7 +420,7 @@ public:
     virtual bool        IsMenuBar() const SAL_OVERRIDE { return true; }
 
     /// Close the 'pStartedFrom' menu window.
-    virtual void CloseStartedFrom() SAL_OVERRIDE;
+    virtual void ClosePopup(Menu* pMenu) SAL_OVERRIDE;
 
     /// Deactivate the MenuBarWindow.
     virtual sal_uLong DeactivateMenuBar(sal_uLong nFocusId) SAL_OVERRIDE;
@@ -513,7 +513,7 @@ public:
     virtual bool        IsMenuBar() const SAL_OVERRIDE { return false; }
 
     /// Close the 'pStartedFrom' menu window.
-    virtual void CloseStartedFrom() SAL_OVERRIDE;
+    virtual void ClosePopup(Menu* pMenu) SAL_OVERRIDE;
 
     void                SetText( const OUString& rTitle )  { aTitleText = rTitle; }
     const OUString&     GetText() const                     { return aTitleText; }

vcl/source/window/menu.cxx
@@ -2420,24 +2420,19 @@ MenuBar::~MenuBar()
     ImplDestroy( this, true );
 }
 
-void MenuBar::CloseStartedFrom()
+void MenuBar::ClosePopup(Menu *pMenu)
 {
-    // if the window was closed by TH, there is another menu
-    // which has this window as pActivePopup
-    if (!pStartedFrom)
-        return;
-
-    MenuBarWindow* p = (MenuBarWindow*) pStartedFrom->ImplGetWindow();
+    MenuBarWindow* p = dynamic_cast<MenuBarWindow*>(ImplGetWindow());
     if (p)
-        p->PopupClosed(this);
+        p->PopupClosed(pMenu);
 }
 
 sal_uLong MenuBar::DeactivateMenuBar(sal_uLong nFocusId)
 {
-    nFocusId = ((MenuBarWindow*)(dynamic_cast<MenuBar*>(this))->ImplGetWindow())->GetFocusId();
+    nFocusId = dynamic_cast<MenuBarWindow*>(ImplGetWindow())->GetFocusId();
     if (nFocusId)
     {
-        ((MenuBarWindow*)(dynamic_cast<MenuBar*>(this))->ImplGetWindow())->SetFocusId(0);
+        dynamic_cast<MenuBarWindow*>(ImplGetWindow())->SetFocusId(0);
         ImplGetSVData()->maWinData.mbNoDeactivate = false;
     }
 
@@ -2701,17 +2696,12 @@ PopupMenu::~PopupMenu()
         *pRefAutoSubMenu = NULL;    // #111060# avoid second delete in ~MenuItemData
 }
 
-void PopupMenu::CloseStartedFrom()
+void PopupMenu::ClosePopup(Menu* pMenu)
 {
-    // if the window was closed by TH, there is another menu
-    // which has this window as pActivePopup
-    if (!pStartedFrom)
-        return;
-
-    MenuFloatingWindow* p = (MenuFloatingWindow*) pStartedFrom->ImplGetWindow();
-    PopupMenu *pMenu = dynamic_cast<PopupMenu*>(this);
+    MenuFloatingWindow* p = dynamic_cast<MenuFloatingWindow*>(ImplGetWindow());
+    PopupMenu *pPopup = dynamic_cast<PopupMenu*>(pMenu);
     if (p && pMenu)
-        p->KillActivePopup(pMenu);
+        p->KillActivePopup(pPopup);
 }
 
 bool PopupMenu::IsInExecute()

vcl/source/window/menufloatingwindow.cxx
@@ -252,8 +252,8 @@ IMPL_LINK_NOARG(MenuFloatingWindow, PopupEnd)
     }
     else
     {
-        if (pMenu)
-            pMenu->CloseStartedFrom();
+        if (pMenu && pMenu->pStartedFrom)
+            pMenu->pStartedFrom->ClosePopup(pMenu);
     }
 
     if ( pM )