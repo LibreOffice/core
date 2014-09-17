d2a1de9910c801e317016d34bacb910e6f9f248d
Katarina Behrens <bubli@bubli.org>
2014-09-17 21:40:34 +0200

    WIP fdo#63483: support for modify passwd in Impress and Draw
    iow, allow opening Impress and Draw ODF documents read-only
    
    Change-Id: I379b9f92b9bfb20e99998177d9a5fae80069d98f

--- sd/source/ui/unoidl/UnoDocumentSettings.cxx
+++ sd/source/ui/unoidl/UnoDocumentSettings.cxx
@@ -139,7 +139,7 @@ enum SdDocumentSettingsPropertyHandles
     HANDLE_PRINTERNAME, HANDLE_PRINTERJOB, HANDLE_PARAGRAPHSUMMATION, HANDLE_CHARCOMPRESS, HANDLE_ASIANPUNCT, HANDLE_UPDATEFROMTEMPLATE,
     HANDLE_PRINTER_INDEPENDENT_LAYOUT
     // #i33095#
-    ,HANDLE_LOAD_READONLY, HANDLE_SAVE_VERSION
+    ,HANDLE_LOAD_READONLY, HANDLE_MODIFY_PASSWD, HANDLE_SAVE_VERSION
     ,HANDLE_SLIDESPERHANDOUT, HANDLE_HANDOUTHORIZONTAL, HANDLE_EMBED_FONTS
 };
 
@@ -200,6 +200,7 @@ enum SdDocumentSettingsPropertyHandles
             { OUString("PrinterIndependentLayout"),HANDLE_PRINTER_INDEPENDENT_LAYOUT,::cppu::UnoType<sal_Int16>::get(), 0,  0 },
             // --> #i33095#
             { OUString("LoadReadonly"),          HANDLE_LOAD_READONLY,       ::getBooleanCppuType(),                0,  0 },
+            { OUString("ModifyPasswordInfo"),    HANDLE_MODIFY_PASSWD,       ::getCppuType((uno::Sequence < beans::PropertyValue > *)0),  0,  0 },
             { OUString("SaveVersionOnClose"),    HANDLE_SAVE_VERSION,        ::getBooleanCppuType(),                0,  0 },
             { OUString("EmbedFonts"),            HANDLE_EMBED_FONTS,         ::getBooleanCppuType(),                0,  0 },
             { OUString(), 0, css::uno::Type(), 0, 0 }
@@ -388,6 +389,10 @@ uno::Sequence<beans::PropertyValue>
     return aRet;
 }
 
+// Most of the code reading/writing UNO document settings is the same in
+// sd, sc and sw and it is mostly copy-pasted back and forth.
+// TODO: Move _setPropertyValues and _getPropertyValues to some shared
+// place, at least for the settings that are common to sd, sc and sw
 void
 DocumentSettings::_setPropertyValues(const PropertyMapEntry** ppEntries,
         const Any* pValues)
@@ -898,6 +903,22 @@ throw (UnknownPropertyException, PropertyVetoException,
             }
             break;
 
+            case HANDLE_MODIFY_PASSWD:
+            {
+                uno::Sequence< beans::PropertyValue > aInfo;
+                if ( !( *pValues >>= aInfo ) )
+                    throw lang::IllegalArgumentException(
+                        OUString( "Value of type Sequence<PropertyValue> expected!" ),
+                        uno::Reference< uno::XInterface >(),
+                        2 );
+
+                if ( !pDocSh->SetModifyPasswordInfo( aInfo ) )
+                    throw beans::PropertyVetoException(
+                        "The hash is not allowed to be changed now!" );
+
+            }
+            break;
+
             case HANDLE_SAVE_VERSION:
             {
                 bool bNewValue = false;
@@ -1146,6 +1167,12 @@ throw (UnknownPropertyException, WrappedTargetException, RuntimeException)
             case HANDLE_LOAD_READONLY:
             {
                 *pValue <<= pDocSh->IsLoadReadonly();
+            }
+            break;
+
+            case HANDLE_MODIFY_PASSWD:
+            {
+                *pValue <<= pDocSh->GetModifyPasswordInfo();
             }
             break;
 
