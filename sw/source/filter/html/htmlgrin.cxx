LibreOffice/core

commit 3914a711060341345f15b83656457f90095f32d6

bnc#580834: HTML import: Embed images if source URL is a local file.
Use also INET_PROT_FILE to determine whether to import images as links.

Change-Id: I072be21086505f6300463e4e3cfb276cbe506803

sw/source/filter/html/htmlgrin.cxx
@@ -696,23 +696,18 @@ IMAGE_SETEVENT:
 
     Graphic aEmptyGrf;
     INetURLObject aGraphicURL( sGrfNm );
-    if( aGraphicURL.GetProtocol() == INET_PROT_DATA )
+    if (aGraphicURL.GetProtocol() == INET_PROT_DATA ||
+        aGraphicURL.GetProtocol() == INET_PROT_FILE)
     {
-        SvMemoryStream* aStream = aGraphicURL.getData();
-        if( aStream )
-        {
-            GraphicFilter::GetGraphicFilter().ImportGraphic( aEmptyGrf, OUString(), *aStream );
-            free( aStream );
-        }
-        else
-        {
-            aEmptyGrf.SetDefaultType();
-        }
+        if (GRFILTER_OK == GraphicFilter::GetGraphicFilter().ImportGraphic(aEmptyGrf, aGraphicURL))
+            sGrfNm = "";
     }
-    else
+    if (!sGrfNm.isEmpty())
     {
         aEmptyGrf.SetDefaultType();
     }
+
+    // passing empty sGrfNm here, means we don't want the graphic to be linked
     SwFrmFmt *pFlyFmt = pDoc->getIDocumentContentOperations().Insert( *pPam, sGrfNm, aEmptyOUStr, &aEmptyGrf,
                                       &aFrmSet, NULL, NULL );
     SwGrfNode *pGrfNd = pDoc->GetNodes()[ pFlyFmt->GetCntnt().GetCntntIdx()

commit 72703173066a2db5c977d422ace59d60b998bbfc

HTML import: fix importing of inlined images (related: fdo#50763)
Regression from 3914a711060341345f15b83656457f90095f32d6

Change-Id: I0523d2b07dc33c74caa711cf7aae274c935f8eed

sw/source/filter/html/htmlgrin.cxx
@@ -694,21 +694,30 @@ IMAGE_SETEVENT:
     aFrmSize.SetHeightPercent( nPrcHeight );
     aFrmSet.Put( aFrmSize );
 
-    Graphic aEmptyGrf;
+    Graphic aGraphic;
     INetURLObject aGraphicURL( sGrfNm );
-    if (aGraphicURL.GetProtocol() == INET_PROT_DATA ||
-        aGraphicURL.GetProtocol() == INET_PROT_FILE)
+    if( aGraphicURL.GetProtocol() == INET_PROT_DATA )
     {
-        if (GRFILTER_OK == GraphicFilter::GetGraphicFilter().ImportGraphic(aEmptyGrf, aGraphicURL))
+        SvMemoryStream* aStream = aGraphicURL.getData();
+        if( aStream )
+        {
+            if (GRFILTER_OK == GraphicFilter::GetGraphicFilter().ImportGraphic(aGraphic, "", *aStream))
+                sGrfNm = "";
+            free( aStream );
+        }
+    }
+    else if (aGraphicURL.GetProtocol() == INET_PROT_FILE)
+    {
+        if (GRFILTER_OK == GraphicFilter::GetGraphicFilter().ImportGraphic(aGraphic, aGraphicURL))
             sGrfNm = "";
     }
     if (!sGrfNm.isEmpty())
     {
-        aEmptyGrf.SetDefaultType();
+        aGraphic.SetDefaultType();
     }
 
     // passing empty sGrfNm here, means we don't want the graphic to be linked
-    SwFrmFmt *pFlyFmt = pDoc->getIDocumentContentOperations().Insert( *pPam, sGrfNm, aEmptyOUStr, &aEmptyGrf,
+    SwFrmFmt *pFlyFmt = pDoc->getIDocumentContentOperations().Insert( *pPam, sGrfNm, aEmptyOUStr, &aGraphic,
                                       &aFrmSet, NULL, NULL );
     SwGrfNode *pGrfNd = pDoc->GetNodes()[ pFlyFmt->GetCntnt().GetCntntIdx()
                                   ->GetIndex()+1 ]->GetGrfNode();