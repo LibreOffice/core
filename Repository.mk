ae73d9b3ea54c106599e1c39c2590583bf9b1e66
Tor Lillqvist <tml@collabora.com>
2014-09-20 01:01:09 +0300

    Skip some executables in the OS X sandboxed (Mac App Store) case
    I doubt end-users will miss gengal.bin, regview, or regmerge.
    
    Change-Id: I353610c0d039f25fa415f35902fe2b9890cd423f
    (cherry picked from commit beb30bbd867f9a28878e0d004458c3507f6c0956)

--- Repository.mk
+++ Repository.mk
@@ -82,7 +82,9 @@ $(eval $(call gb_Helper_register_executables_for_install,SDK,sdk, \
 ))
 
 $(eval $(call gb_Helper_register_executables_for_install,OOO,ooo, \
-	gengal \
+	$(if $(ENABLE_MACOSX_SANDBOX),, \
+		gengal \
+	)\
 ))
 
 $(eval $(call gb_Helper_register_executables,OOO, \
@@ -128,8 +130,10 @@ $(eval $(call gb_Helper_register_executables,OOO, \
 
 $(eval $(call gb_Helper_register_executables_for_install,UREBIN,ure,\
 	$(if $(and $(ENABLE_JAVA),$(filter-out MACOSX WNT,$(OS)),$(filter DESKTOP,$(BUILD_TYPE))),javaldx) \
-	regmerge \
-	regview \
+	$(if $(ENABLE_MACOSX_SANDBOX),, \
+		regmerge \
+		regview \
+	) \
 	$(if $(filter DESKTOP,$(BUILD_TYPE)),uno) \
 ))
 
