061d601a9e000fa72053ed559c8c5b0203c5db09
David Ostrovsky <david@ostrovsky.org>
2014-09-20 16:57:13 +0200

    Add unit test for oox::TokenMap
    Change-Id: Iced083868a84fd9fbb97ad91dfc0acbf5b69b766

---
+++ oox/qa/token/tokenmap-test.cxx
@@ -0,0 +1,68 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <cppunit/TestAssert.h>
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/plugin/TestPlugIn.h>
+
+#include "oox/token/tokenmap.hxx"
+#include "oox/token/tokens.hxx"
+
+using namespace std;
+using namespace com::sun::star::uno;
+
+namespace oox {
+
+class TokenmapTest: public CppUnit::TestFixture
+{
+public:
+    void test_roundTrip();
+
+    virtual void setUp() SAL_OVERRIDE;
+    virtual void tearDown() SAL_OVERRIDE;
+
+    CPPUNIT_TEST_SUITE(TokenmapTest);
+
+    CPPUNIT_TEST(test_roundTrip);
+    CPPUNIT_TEST_SUITE_END();
+
+private:
+    TokenMap* tokenMap;
+};
+
+void TokenmapTest::setUp()
+{
+    tokenMap = new TokenMap();
+}
+
+void TokenmapTest::tearDown()
+{
+    delete tokenMap;
+}
+
+void TokenmapTest::test_roundTrip()
+{
+    for ( sal_Int32 nToken = 0; nToken < XML_TOKEN_COUNT; ++nToken )
+    {
+        // check that the getIdentifier <-> getToken roundtrip works
+        Sequence< sal_Int8 > rUtf8Name = tokenMap->getUtf8TokenName(nToken);
+        OString aUtf8Name(reinterpret_cast< const char * >(
+                          rUtf8Name.getConstArray() ),
+                          rUtf8Name.getLength());
+        sal_Int32 ret = tokenMap->getTokenFromUTF8( aUtf8Name.getStr(), aUtf8Name.getLength() );
+        CPPUNIT_ASSERT_EQUAL(ret, nToken);
+    }
+}
+
+CPPUNIT_TEST_SUITE_REGISTRATION(TokenmapTest);
+
+}
+
+CPPUNIT_PLUGIN_IMPLEMENT();
