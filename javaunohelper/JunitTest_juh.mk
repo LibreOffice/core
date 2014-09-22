0bcf593ea5d12e66a9c53f2b357588e634fcf3f0
Robert Antoni Buj i Gelonch <robert.buj@gmail.com>
2014-09-22 16:31:56 +0200

    javaunohelper: migrate ComponentBase_Test to JUnit
    Use -XX:MaxGCPauseMillis=50 in conjunction with Thread.sleep(51) to wait 51ms after a GC call, instead of waiting 10s.
    http://docs.oracle.com/javase/1.5.0/docs/guide/vm/gc-ergonomics.html
    
    $ make JunitTest_juh_ComponentBase
    
    Change-Id: Id2cdada0e493fd450aab5dbec164502e0173857f

---
+++ javaunohelper/JunitTest_juh.mk
@@ -0,0 +1,45 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_JunitTest_JunitTest,juh))
+
+$(eval $(call gb_JunitTest_set_defs,juh,\
+    $$(DEFS) \
+    -XX:MaxGCPauseMillis=5 \
+))
+
+$(eval $(call gb_JunitTest_use_jars,juh,\
+    ridl \
+    jurt \
+))
+
+$(eval $(call gb_JunitTest_use_jar_classset,juh,ridljar))
+
+$(eval $(call gb_JunitTest_add_sourcefiles,juh,\
+    javaunohelper/com/sun/star/comp/helper/Bootstrap \
+    javaunohelper/com/sun/star/comp/helper/BootstrapException \
+    javaunohelper/com/sun/star/comp/helper/ComponentContext \
+    javaunohelper/com/sun/star/comp/helper/ComponentContextEntry \
+    javaunohelper/com/sun/star/lib/uno/helper/ComponentBase \
+    javaunohelper/com/sun/star/lib/uno/helper/InterfaceContainer \
+    javaunohelper/com/sun/star/lib/uno/helper/MultiTypeInterfaceContainer \
+    javaunohelper/com/sun/star/lib/uno/helper/WeakAdapter \
+    javaunohelper/com/sun/star/lib/uno/helper/WeakBase \
+    javaunohelper/test/com/sun/star/comp/helper/ComponentContext_Test \
+    javaunohelper/test/com/sun/star/lib/uno/helper/AWeakBase \
+    javaunohelper/test/com/sun/star/lib/uno/helper/ComponentBase_Test \
+    javaunohelper/test/com/sun/star/lib/uno/helper/ProxyProvider \
+))
+
+$(eval $(call gb_JunitTest_add_classes,juh,\
+    com.sun.star.comp.helper.ComponentContext_Test \
+    com.sun.star.lib.uno.helper.ComponentBase_Test \
+))
+
+# vim:set noet sw=4 ts=4:
