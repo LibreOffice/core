dbbd89efdeaa44421d1f2ad774c342009d881d9b
Tobias Madl <tobias.madl.dev@gmail.com>
2014-09-23 11:43:14 +0000

    fdo#39625 Make existing CppUnit tests work
    As in the ticket description, this test is now working, but
    some parts of it are still commented out, because they are waiting
    for a fix of another bug (Bug 84237). Once it is fixed, it should
    be possible to just uncomment them, and then they should work.
    
    Change-Id: I73d1cc7391080e357c08cb3442616e2901070c1f
    Signed-off-by: Stephan Bergmann <sbergman@redhat.com> (with minor amendments)

---
+++ unoxml/CppunitTest_unoxml_domtest.mk
@@ -0,0 +1,47 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_CppunitTest_CppunitTest,unoxml_domtest))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,unoxml_domtest, \
+    unoxml/test/domtest \
+))
+
+$(eval $(call gb_CppunitTest_use_api,unoxml_domtest,\
+    offapi \
+    udkapi \
+))
+
+$(eval $(call gb_CppunitTest_use_components,unoxml_domtest,\
+    configmgr/source/configmgr \
+    framework/util/fwk \
+    i18npool/util/i18npool \
+    sfx2/util/sfx \
+    ucb/source/core/ucb1 \
+    ucb/source/ucp/file/ucpfile1 \
+    unoxml/source/service/unoxml \
+))
+
+$(eval $(call gb_CppunitTest_use_externals,unoxml_domtest, \
+    boost_headers \
+))
+
+$(eval $(call gb_CppunitTest_use_libraries,unoxml_domtest, \
+    comphelper \
+    cppu \
+    cppuhelper \
+    sal \
+    test \
+))
+
+$(eval $(call gb_CppunitTest_use_configuration,unoxml_domtest))
+$(eval $(call gb_CppunitTest_use_ure,unoxml_domtest))
+$(eval $(call gb_CppunitTest_use_vcl,unoxml_domtest))
+
+# vim: set noet sw=4 ts=4:
