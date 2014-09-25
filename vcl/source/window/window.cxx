17b0cc20a8559fcd2adb625c816448cf685ab074
Andrzej Hunt <andrzej@ahunt.org>
2014-09-25 09:25:47 +0100

    Don't use ImplDoTiledRendering for Android.
    With svp this causes images not to be rendered for tiled rendering,
    however this could potentially cause issues with SmartArt (however
    SmartArt support for Tiled Rendering is possibly problematic in any case).
    
    Change-Id: Iccbdce7b6d5353966892fbde3d3d10eb49164df9

--- vcl/source/window/window.cxx
+++ vcl/source/window/window.cxx
@@ -3940,7 +3940,7 @@ Any Window::GetSystemDataAny() const
 
 bool ImplDoTiledRendering()
 {
-#if !HAVE_FEATURE_DESKTOP
+#if !HAVE_FEATURE_DESKTOP && !ANDROID
     // We do tiled rendering only for iOS at the moment, actually, but
     // let's see what happens if we assume it for Android, too.
     return true;
