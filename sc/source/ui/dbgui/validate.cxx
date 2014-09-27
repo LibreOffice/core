f8ad613b1ed8ff6889027247be008a39198e17f8
Laurent Balland-Poirier <laurent.balland-poirier@laposte.net>
2014-09-27 07:59:04 +0200

    fdo#53920 Switch back tab at RefButton closure time
    Bug occurs because visible tab is not the initial tab. So, instead of
    switching back to initial tab at dialog closure time (removed from
    cellsh2.cxx), this commit proposes that switching back to initial tab
    is operate when expanding the RefButton shrink dialog.
    
    Reviewed-on: https://gerrit.libreoffice.org/11660
    Reviewed-by: Kohei Yoshida <libreoffice@kohei.us>
    Tested-by: Kohei Yoshida <libreoffice@kohei.us>
    (cherry picked from commit 31432c4469e7e0d05516143533d6b5e0b411dda3)
    
    Change-Id: I3cf7ed14054fa69881d4ecbc55c48dfbfc4356aa

--- sc/source/ui/dbgui/validate.cxx
+++ sc/source/ui/dbgui/validate.cxx
@@ -145,11 +145,21 @@ void            ScTPValidationValue::RefInputDonePostHdl()
     }
 
     if ( ScValidationDlg *pValidationDlg = GetValidationDlg() )
+    {
         pValidationDlg->get_refinput_shrink_parent()->Hide();
+        ScViewData* pViewData = pValidationDlg->GetTabViewShell()->GetViewData();
+        SCTAB nCurTab = pViewData->GetTabNo();
+        SCTAB nRefTab = pViewData->GetRefTabNo();
+        // If RefInput switched to a different sheet from the data sheet,
+        // switch back: fdo#53920
+        if ( nCurTab != nRefTab )
+        {
+             pViewData->GetViewShell()->SetTabNo( nRefTab );
+        }
+    }
 
     if( m_pRefEdit && !m_pRefEdit->HasFocus() )
         m_pRefEdit->GrabFocus();
-
 }
 
 sal_Bool ScValidationDlg::Close()
