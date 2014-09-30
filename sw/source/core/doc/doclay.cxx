Echo360/LibreOffice-core

Directly convert frame name tails to Int32
This is a little optimization for large mail merge jobs with many
frames. The current alorithm doesn't only try to produce unique but
also reasonable names for the frames.

Per se the algorithm is horrible ineffective for large numbers of
frames, but this is impossible to fix without changing the
underlying vector of frames to a sorted one to find and check just
the correctly prefixed frames.

This patch directly converts the frame names tail instead of
creating substring of the number tail, which saves billions of
malloc and free calls seen with my sample document.

Change-Id: Iefdee4053480f40f106c49867bc5a64ec207ba1b

sw/source/core/doc/doclay.cxx
@@ -1926,7 +1926,7 @@ static OUString lcl_GetUniqueFlyName( const SwDoc* pDoc, sal_uInt16 nDefStrId )
             pFlyFmt->GetName().startsWith( aName ) )
         {
             // Only get and set the Flag
-            nNum = static_cast< sal_uInt16 >( pFlyFmt->GetName().copy( nNmLen ).toInt32() );
+            nNum = static_cast< sal_uInt16 >( rtl_ustr_toInt32( pFlyFmt->GetName().getStr() + nNmLen, 10 ) );
             if( nNum-- && nNum < rFmts.size() )
                 pSetFlags[ nNum / 8 ] |= (0x01 << ( nNum & 0x07 ));
         }

LibreOffice/core

(cherry picked from commit 5f6db9260ec42a040a4ce21e93af2f1e18746fe6)
Conflicts:
	sw/source/core/doc/doclay.cxx

sw/source/core/doc/doclay.cxx
@@ -1926,7 +1926,7 @@ static String lcl_GetUniqueFlyName( const SwDoc* pDoc, sal_uInt16 nDefStrId )
             pFlyFmt->GetName().Match( aName ) == nNmLen )
         {
             // Only get and set the Flag
-            nNum = static_cast< sal_uInt16 >( pFlyFmt->GetName().Copy( nNmLen ).ToInt32() );
+            nNum = static_cast< sal_uInt16 >( rtl_ustr_toInt32( pFlyFmt->GetName().GetBuffer() + nNmLen, 10 ) );
             if( nNum-- && nNum < rFmts.size() )
                 pSetFlags[ nNum / 8 ] |= (0x01 << ( nNum & 0x07 ));
         }