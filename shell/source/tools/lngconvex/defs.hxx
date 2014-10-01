5b401a06a616238cfbd961e2afb47a21c2835220
Stephan Bergmann <sbergman@redhat.com>
2014-10-01 15:14:36 +0200

    shell: std::auto_ptr -> std::unique_ptr
    Change-Id: I6fc7ef7438e1b999b3b65e4e661b9e787a66cf47

--- shell/source/tools/lngconvex/defs.hxx
+++ shell/source/tools/lngconvex/defs.hxx
@@ -25,10 +25,10 @@
 #include <memory>
 
 typedef std::vector<std::string> StringList_t;
-typedef std::auto_ptr<StringList_t> StringListPtr_t;
+typedef std::unique_ptr<StringList_t> StringListPtr_t;
 
 typedef std::vector<int> IntegerList_t;
-typedef std::auto_ptr<IntegerList_t> IntegerListPtr_t;
+typedef std::unique_ptr<IntegerList_t> IntegerListPtr_t;
 
 #endif
 
