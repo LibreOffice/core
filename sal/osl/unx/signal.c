9086a4bbd38b9a5320525392affe495e7772796a
Michael Stahl <mstahl@redhat.com>
2014-10-02 18:42:07 +0200

    remove checks of __GCC_MINOR__ that are always true in GCC 4.6 or later
    Change-Id: I4412a0c1e4dee94cd717f24b8df5e257d895f721

--- sal/osl/unx/signal.c
+++ sal/osl/unx/signal.c
@@ -893,14 +893,14 @@ static void DUMPCURRENTALLOCS(void)
 {
     VALGRIND_PRINTF( "=== start memcheck dump of active allocations ===\n" );
 
-#if (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 6))
+#ifdef __GNUC__
 #   pragma GCC diagnostic push
 #   pragma GCC diagnostic ignored "-Wunused-but-set-variable"
 #endif
 
     VALGRIND_DO_LEAK_CHECK;
 
-#if (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 6))
+#ifdef __GNUC__
 #   pragma GCC diagnostic pop
 #endif
 
