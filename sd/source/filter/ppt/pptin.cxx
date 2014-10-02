164bd64255d71d96e35e908f6425acf8372795fb
Norbert Thiebaud <nthiebaud@gmail.com>
2014-10-02 04:44:33 -0500

    coverity#1242757 Result is not floating-point
    Change-Id: Iae8ac2ad9d1713512619c5c33eeee9a21b5835f1

--- sd/source/filter/ppt/pptin.cxx
+++ sd/source/filter/ppt/pptin.cxx
@@ -2445,8 +2445,8 @@ SdrObject* ImplSdPPTImport::ApplyTextObj( PPTTextObj* pTextObj, SdrTextObj* pObj
                                             if (std::abs(aLogicRect.Right()  - aOutlineRect.Right())  > MAX_USER_MOVE ||
                                                 std::abs(aLogicRect.Top()    - aOutlineRect.Top())    > MAX_USER_MOVE ||
                                                 std::abs(aLogicRect.Bottom() - aOutlineRect.Bottom()) > MAX_USER_MOVE ||
-                                                    aLogicSize.Width()  / aOutlineSize.Width()   < 0.48          ||
-                                                    aLogicSize.Width()  / aOutlineSize.Width()   > 0.5)
+                                                (double)aLogicSize.Width()  / aOutlineSize.Width()   < 0.48          ||
+                                                (double)aLogicSize.Width()  / aOutlineSize.Width()   > 0.5)
                                             {
                                                 pPresObj->SetUserCall( NULL );
                                             }
