ed059853b0fbf8a59b6ac8bc283a53ff3b593c02
Chris Sherlock <chris.sherlock79@gmail.com>
2014-10-04 21:55:58 +1000

    vcl: FontCharMap to use intrusive_ptr ImplFontCharMap
    ImplFontCharMap was using it's own reference counting mechanism,
    however we can use intrusive_ptr more effectively.
    
    Added a unit test around FontCharMap.
    
    Change-Id: Ifab6ce002fd1df8feb7e017dea3012ff9ea7f18a

--- vcl/Module_vcl.mk
+++ vcl/Module_vcl.mk
@@ -94,6 +94,7 @@ $(eval $(call gb_Module_add_targets,vcl,\
 endif
 
 $(eval $(call gb_Module_add_check_targets,vcl,\
+	CppunitTest_vcl_fontcharmap \
 	CppunitTest_vcl_complextext \
 	CppunitTest_vcl_filters_test \
 	CppunitTest_vcl_app_test \
--- vcl/inc/win/salgdi.h
+++ vcl/inc/win/salgdi.h
@@ -94,7 +94,7 @@ public:
     const gr_face*          GraphiteFace() const;
 #endif
 
-    const ImplFontCharMap*  GetImplFontCharMap() const;
+    const ImplFontCharMapPtr GetImplFontCharMap() const;
     bool GetFontCapabilities(vcl::FontCapabilities &rFontCapabilities) const;
     const Ucs2SIntMap* GetEncodingVector() const { return mpEncodingVector; }
     void SetEncodingVector( const Ucs2SIntMap* pNewVec ) const
@@ -116,7 +116,7 @@ private:
 #endif
     mutable bool                    mbHasArabicSupport;
     mutable bool                    mbFontCapabilitiesRead;
-    mutable ImplFontCharMap*        mpUnicodeMap;
+    mutable ImplFontCharMapPtr      mpUnicodeMap;
     mutable const Ucs2SIntMap*      mpEncodingVector;
     mutable vcl::FontCapabilities   maFontCapabilities;
 
@@ -310,7 +310,7 @@ public:
     // get the current font's metrics
     virtual void            GetFontMetric( ImplFontMetricData*, int nFallbackLevel );
     // get the repertoire of the current font
-    virtual const ImplFontCharMap* GetImplFontCharMap() const;
+    virtual const ImplFontCharMapPtr GetImplFontCharMap() const;
     // get the layout capabilities of the current font
     virtual bool GetFontCapabilities(vcl::FontCapabilities &rGetFontCapabilities) const;
     // graphics must fill supplied font list
