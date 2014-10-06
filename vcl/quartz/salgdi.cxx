5952194fa851b41a275d9a6e1dcd253f75cbe793
Chris Sherlock <chris.sherlock79@gmail.com>
2014-10-06 18:16:16 +1100

    vcl: Make ImplFontCharMap a pImpl and move functions to FontCharMap
    To do this, I've made FontCharMap a friend class for ImplFontCharMap,
    and have moved the functions directly into FontCharMap. In this patch,
    I am attempting to stop the direct use of ImplFontCharMap by anything
    other than FontCharMap - everything should now use FontCharMap.
    
    Change-Id: I509b990a8cbd911c5cc1572c7d24fc5348ca06d9

--- vcl/quartz/salgdi.cxx
+++ vcl/quartz/salgdi.cxx
@@ -37,6 +37,9 @@
 
 #include "vcl/sysdata.hxx"
 #include "vcl/svapp.hxx"
+#include "vcl/metric.hxx"
+
+#include "impfont.hxx"
 
 #include "quartz/salgdi.h"
 #include "quartz/utils.h"
@@ -53,7 +56,6 @@
 #include "ctfonts.hxx"
 
 #include "fontsubset.hxx"
-#include "impfont.hxx"
 #include "sallayout.hxx"
 #include "sft.hxx"
 
@@ -93,37 +95,37 @@ sal_IntPtr CoreTextFontData::GetFontId() const
 
 static unsigned GetUShort( const unsigned char* p ){return((p[0]<<8)+p[1]);}
 
-const ImplFontCharMapPtr CoreTextFontData::GetImplFontCharMap() const
+const FontCharMap* CoreTextFontData::GetFontCharMap() const
 {
     // return the cached charmap
     if( mpCharMap )
         return mpCharMap;
 
     // set the default charmap
-    mpCharMap = ImplFontCharMap::GetDefaultMap();
+    mpCharMap = new FontCharMap();
 
     // get the CMAP byte size
     // allocate a buffer for the CMAP raw data
     const int nBufSize = GetFontTable( "cmap", NULL );
-    DBG_ASSERT( (nBufSize > 0), "CoreTextFontData::GetImplFontCharMap : GetFontTable1 failed!\n");
+    DBG_ASSERT( (nBufSize > 0), "CoreTextFontData::GetFontCharMap : GetFontTable1 failed!\n");
     if( nBufSize <= 0 )
         return mpCharMap;
 
     // get the CMAP raw data
     ByteVector aBuffer( nBufSize );
     const int nRawLength = GetFontTable( "cmap", &aBuffer[0] );
-    DBG_ASSERT( (nRawLength > 0), "CoreTextFontData::GetImplFontCharMap : GetFontTable2 failed!\n");
+    DBG_ASSERT( (nRawLength > 0), "CoreTextFontData::GetFontCharMap : GetFontTable2 failed!\n");
     if( nRawLength <= 0 )
         return mpCharMap;
-    DBG_ASSERT( (nBufSize==nRawLength), "CoreTextFontData::GetImplFontCharMap : ByteCount mismatch!\n");
+    DBG_ASSERT( (nBufSize==nRawLength), "CoreTextFontData::GetFontCharMap : ByteCount mismatch!\n");
 
     // parse the CMAP
     CmapResult aCmapResult;
     if( ParseCMAP( &aBuffer[0], nRawLength, aCmapResult ) )
     {
-        ImplFontCharMapPtr pCharMap(new ImplFontCharMap( aCmapResult ) );
+        delete mpCharMap;
         // create the matching charmap
-        mpCharMap = pCharMap;
+        mpCharMap = new FontCharMap(aCmapResult);
     }
 
     return mpCharMap;
@@ -485,12 +487,12 @@ SalLayout* AquaSalGraphics::GetTextLayout( ImplLayoutArgs& /*rArgs*/, int /*nFal
     return pSalLayout;
 }
 
-const ImplFontCharMapPtr AquaSalGraphics::GetImplFontCharMap() const
+const FontCharMap* AquaSalGraphics::GetFontCharMap() const
 {
     if( !mpFontData )
-        return ImplFontCharMap::GetDefaultMap();
+        return new FontCharMap();
 
-    return mpFontData->GetImplFontCharMap();
+    return mpFontData->GetFontCharMap();
 }
 
 bool AquaSalGraphics::GetFontCapabilities(vcl::FontCapabilities &rFontCapabilities) const
@@ -722,7 +724,7 @@ void AquaSalGraphics::GetGlyphWidths( const PhysicalFontFace* pFontData, bool bV
                 free( (void*)pGlyphMetrics );
             }
 
-            ImplFontCharMapPtr pMap = mpFontData->GetImplFontCharMap();
+            const FontCharMap *pMap = mpFontData->GetFontCharMap();
             DBG_ASSERT( pMap && pMap->GetCharCount(), "no charmap" );
 
             // get unicode<->glyph encoding
@@ -739,7 +741,7 @@ void AquaSalGraphics::GetGlyphWidths( const PhysicalFontFace* pFontData, bool bV
                     rUnicodeEnc[ nUcsChar ] = nGlyph;
             }
 
-            pMap = 0;
+            delete pMap;
         }
 
         ::CloseTTFont( pSftFont );
