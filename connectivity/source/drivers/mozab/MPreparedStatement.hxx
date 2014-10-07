a98f12b0ad7a5ef30219bfdf4a3ab1507bcd0bec
Michael Stahl <mstahl@redhat.com>
2014-10-07 22:38:20 +0200

    connectivity: try to fix build after f641fd15
    Change-Id: I6a8534044eb0c245ef8aa80c52cd1a37ba8fa5a5

--- connectivity/source/drivers/mozab/MPreparedStatement.hxx
+++ connectivity/source/drivers/mozab/MPreparedStatement.hxx
@@ -27,14 +27,14 @@
 #include <com/sun/star/sdbc/XResultSetMetaDataSupplier.hpp>
 #include <com/sun/star/io/XInputStream.hpp>
 #include <com/sun/star/sdbc/XMultipleResults.hpp>
-#include <cppuhelper/compbase5.hxx>
+#include <cppuhelper/compbase4.hxx>
 
 namespace connectivity
 {
     namespace mozab
     {
 
-        typedef ::cppu::ImplHelper5<    ::com::sun::star::sdbc::XPreparedStatement,
+        typedef ::cppu::ImplHelper4<    ::com::sun::star::sdbc::XPreparedStatement,
                                         ::com::sun::star::sdbc::XParameters,
                                         ::com::sun::star::sdbc::XResultSetMetaDataSupplier,
                                         ::com::sun::star::lang::XServiceInfo> OPreparedStatement_BASE;
