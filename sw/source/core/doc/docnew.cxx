9b52ce7d88ae613927f33816e4f994d21dc4ded8
Luboš Luňák <l.lunak@collabora.com>
2014-10-07 12:18:02 +0200

    use -> with pointers
    Change-Id: I0df4775931e8e0b289a63afd501efaaf079302fe

--- sw/source/core/doc/docnew.cxx
+++ sw/source/core/doc/docnew.cxx
@@ -1235,11 +1235,11 @@ void SwDoc::Append( const SwDoc& rSource, sal_uInt16 nStartPageNumber,
     SwPaM aInsertPam( aTargetIdx );
 
 #ifdef DBG_UTIL
-    SAL_INFO( "sw.docappend", "Pam-Nd: " << aCpyPam.GetNode( true ).GetIndex() - aCpyPam.GetNode( false ).GetIndex() + 1
-                              << " (0x" << std::hex << (int) aCpyPam.GetNode( false ).GetNodeType() << std::dec
-                              << " " << aCpyPam.GetNode( false ).GetIndex()
-                              << " - 0x" << std::hex << (int) aCpyPam.GetNode( true ).GetNodeType() << std::dec
-                              << " " << aCpyPam.GetNode( true ).GetIndex() << ")" );
+    SAL_INFO( "sw.docappend", "Pam-Nd: " << aCpyPam.GetNode( true )->GetIndex() - aCpyPam.GetNode( false )->GetIndex() + 1
+                              << " (0x" << std::hex << (int) aCpyPam.GetNode( false )->GetNodeType() << std::dec
+                              << " " << aCpyPam.GetNode( false )->GetIndex()
+                              << " - 0x" << std::hex << (int) aCpyPam.GetNode( true )->GetNodeType() << std::dec
+                              << " " << aCpyPam.GetNode( true )->GetIndex() << ")" );
 #endif
 
     this->GetIDocumentUndoRedo().StartUndo( UNDO_INSGLOSSARY, NULL );
