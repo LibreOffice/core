53c836701e22b1babd3702b502facd76236df7ff
Juergen Funk <juergen.funk_ml@cib.de>
2014-10-09 15:44:29 +0200

    Fix fdo#84795 Menu, DropDown-List not disappears with right mouse click
    Set for menu and float-list-box the PopupModeFlags to
    FLOATWIN_POPUPMODE_PATHMOUSECANCELCLICK + FLOATWIN_POPUPMODE_ALLMOUSEBUTTONCLOSE
    
    Improving the comments
    
    Change-Id: I313e4ec38637fa3d46567e08bdb9560ce8475b6a
    Reviewed-on: https://gerrit.libreoffice.org/11881
    Reviewed-by: Samuel Mehrbrodt <s.mehrbrodt@gmail.com>
    Tested-by: Samuel Mehrbrodt <s.mehrbrodt@gmail.com>

--- vcl/source/window/menu.cxx
+++ vcl/source/window/menu.cxx
@@ -2816,8 +2816,9 @@ sal_uInt16 PopupMenu::ImplExecute( vcl::Window* pW, const Rectangle& rRect, sal_
     WinBits nStyle = WB_BORDER;
     if (bRealExecute)
         nPopupModeFlags |= FLOATWIN_POPUPMODE_NEWLEVEL;
-    if (!pStartedFrom || !pStartedFrom->IsMenuBar())
-        nPopupModeFlags |= FLOATWIN_POPUPMODE_PATHMOUSECANCELCLICK | FLOATWIN_POPUPMODE_ALLMOUSEBUTTONCLOSE;
+
+    // mouse-button right: close the sub-menu (float-win) and don't stop the handling fdo#84795
+    nPopupModeFlags |= FLOATWIN_POPUPMODE_PATHMOUSECANCELCLICK | FLOATWIN_POPUPMODE_ALLMOUSEBUTTONCLOSE;
 
     nPopupModeFlags |= FLOATWIN_POPUPMODE_NOKEYCLOSE;
 
