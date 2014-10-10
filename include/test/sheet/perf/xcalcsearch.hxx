1a50f046b33eb301d272a8ee39303aa6950903a9
Laurent Godard <lgodard.libre@laposte.net>
2014-10-10 12:21:55 +0200

    unit test performance for big calc file
    - scBigFile.ods contains
      - 1000 sheets
      - a sheet with 60000 values (20000 x 3 different values)
      - 20000 named ranges
      - ~20000 formulas pointing on named ranges
    
    - test searching one value
    
    Change-Id: I9c4e2e0dc6e2df92bd7cfb7b47160c7437cb9a24

---
+++ include/test/sheet/perf/xcalcsearch.hxx
@@ -0,0 +1,37 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef INCLUDED_TEST_SHEET_XCALCSEARCH_HXX
+#define INCLUDED_TEST_SHEET_XCALCSEARCH_HXX
+
+#include <com/sun/star/util/XSearchDescriptor.hpp>
+#include <com/sun/star/sheet/XSpreadsheetDocument.hpp>
+#include <com/sun/star/sheet/XSpreadsheet.hpp>
+#include <com/sun/star/text/XTextRange.hpp>
+
+#include <test/testdllapi.hxx>
+
+namespace apitest {
+
+class OOO_DLLPUBLIC_TEST XCalcSearch
+{
+public:
+    virtual css::uno::Reference< css::uno::XInterface > init() = 0;
+
+    // XSearchable
+    void testSheetFindAll();
+
+protected:
+    ~XCalcSearch() {}
+
+};
+
+}
+
+#endif // INCLUDED_TEST_SHEET_XCALCSEARCH_HXX
