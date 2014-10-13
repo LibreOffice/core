410c5bd58c36d9064a79520d5bc31cb32ead1326
Kohei Yoshida <kohei.yoshida@collabora.com>
2014-10-13 14:56:11 -0400

    Make VCartesianAxis::get2DAxisMainLine() truly const method.
    No internal state gets modified by this method now.
    
    Change-Id: I10475ad3483cb3a92e41208e1a55a6b925c10223

--- chart2/source/view/axes/VAxisProperties.hxx
+++ chart2/source/view/axes/VAxisProperties.hxx
@@ -84,6 +84,16 @@ struct AxisLabelProperties SAL_FINAL
     bool getIsStaggered() const;
 };
 
+struct AxisLabelAlignment
+{
+    double mfLabelDirection; /// which direction the labels are to be drawn.
+    double mfInnerTickDirection; /// which direction the inner tickmarks are to be drawn.
+
+    LabelAlignment meAlignment;
+
+    AxisLabelAlignment();
+};
+
 struct AxisProperties SAL_FINAL
 {
     css::uno::Reference<css::chart2::XAxis> m_xAxisModel;
@@ -102,11 +112,8 @@ struct AxisProperties SAL_FINAL
     bool        m_bCrossingAxisHasReverseDirection;
     bool        m_bCrossingAxisIsCategoryAxes;
 
-    //this direction is used to indicate in which direction the labels are to be drawn
-    double          m_fLabelDirectionSign;
-    //this direction is used to indicate in which direction inner tickmarks are to be drawn
-    double          m_fInnerDirectionSign;
-    LabelAlignment  m_aLabelAlignment;
+    AxisLabelAlignment maLabelAlignment;
+
     bool            m_bDisplayLabels;
 
     sal_Int32       m_nNumberFormatKey;
