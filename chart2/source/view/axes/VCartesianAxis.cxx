859cb431f1208142529af8f7166347d84e4f4e75
Kohei Yoshida <kohei.yoshida@collabora.com>
2014-10-17 17:01:09 -0400

    Adjust text properties after adding a cached text object...
    But it still doesn't work, and I have a feeling that this strategy will
    not work without a huge amount of effort.  I'll probably abandon it and
    try something else.
    
    Change-Id: Ic09b6e65abdd5595810b29c6ba7858cb5e7b31b5

--- chart2/source/view/axes/VCartesianAxis.cxx
+++ chart2/source/view/axes/VCartesianAxis.cxx
@@ -103,6 +103,39 @@ Reference< drawing::XShape > createSingleLabel(
     return xShape2DText;
 }
 
+void adjustSingleLabel(
+    const Reference<drawing::XShape>& xShape, const awt::Point& rAnchorPos,
+    const AxisLabelProperties& rAxisLabelProps, const AxisProperties& rAxisProps,
+    const tNameSequence& rPropNames, const tAnySequence& rPropValues )
+{
+    if (!xShape.is())
+        return;
+
+    uno::Reference<beans::XPropertySet> xProp(xShape, uno::UNO_QUERY);
+    if (xProp.is())
+    {
+        //set properties
+        PropertyMapper::setMultiProperties(rPropNames, rPropValues, xProp);
+
+        //set position matrix
+        //the matrix needs to be set at the end behind autogrow and such position influencing properties
+        double fRotationAnglePi = rAxisLabelProps.fRotationAngleDegree * (F_PI / -180.0);
+        uno::Any aATransformation = AbstractShapeFactory::makeTransformation(rAnchorPos, fRotationAnglePi);
+        try
+        {
+            xProp->setPropertyValue("Transformation", aATransformation);
+        }
+        catch( const uno::Exception& e )
+        {
+            ASSERT_EXCEPTION( e );
+        }
+    }
+
+    LabelPositionHelper::correctPositionForRotation(
+        xShape, rAxisProps.maLabelAlignment.meAlignment,
+        rAxisLabelProps.fRotationAngleDegree, rAxisProps.m_bComplexCategories);
+}
+
 bool lcl_doesShapeOverlapWithTickmark( const Reference< drawing::XShape >& xShape
                        , double fRotationAngleDegree
                        , const basegfx::B2DVector& rTickScreenPosition
@@ -763,8 +796,12 @@ bool VCartesianAxis::createTextShapes(
             uno::Reference<drawing::XShape> xCached = rTickIter.getTextShape();
             if (xCached.is())
             {
-                xTarget->add(xCached);
                 pTickInfo->xTextShape = xCached;
+                xTarget->add(xCached);
+                adjustSingleLabel(
+                    pTickInfo->xTextShape, aAnchorScreenPosition2D,
+                    rAxisLabelProperties, m_aAxisProperties,
+                    aPropNames, aPropValues);
             }
             else
             {
