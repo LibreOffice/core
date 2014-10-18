b2503f187341f9a37737f34c7fc6a02d03c2e84e
Zolnai Tamás <tamas.zolnai@collabora.com>
2014-10-18 15:06:57 +0200

    Remove manual SwapOut() call in ODF export
    We have a good auto swapout mechanism which will
    prevent excessive memory use.
    
    Change-Id: I362f51c724ac31704561abe8b961910f5d490f04

--- sw/source/filter/xml/xmltexte.cxx
+++ sw/source/filter/xml/xmltexte.cxx
@@ -207,10 +207,6 @@ void SwXMLTextParagraphExport::setTextEmbeddedGraphicURL(
         {
             pGrfNd->ApplyNewEmbeddedStreamName("vnd.sun.star.Package:" + rURL);
         }
-
-        // #i15411# save-as will swap all graphics in; we need to swap
-        // them out again, to prevent excessive memory use
-        pGrfNd->SwapOut();
     }
 }
 
