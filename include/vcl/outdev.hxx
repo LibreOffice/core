7b69cc9a66348fcb6985ec4dc6df5375acbf8100
Chris Sherlock <chris.sherlock79@gmail.com>
2014-10-19 18:09:42 +1100

    vcl: move mirror from SalGraphics to OutputDevice - part 5: polylines
    Migrate mirroring in DrawPolyLine out of SalGraphics and into
    OutputDevice.
    
    Change-Id: I05606cc3a173532bb5afd3c4f0c5c3daa152c2f0

--- include/vcl/outdev.hxx
+++ include/vcl/outdev.hxx
@@ -742,6 +742,15 @@ public:
                                     css::drawing::LineCap eLineCap = css::drawing::LineCap_BUTT);
 private:
 
+    bool                        drawPolyLine(
+                                    const basegfx::B2DPolygon& rPolygon,
+                                    double fTransparency,
+                                    const basegfx::B2DVector& rLineWidth,
+                                    basegfx::B2DLineJoin eLineJoin,
+                                    com::sun::star::drawing::LineCap eLineCap );
+
+    void                        drawPolyLine( sal_uInt32 nPoints, const SalPoint* pPtAry );
+
     // #i101491#
     // Helper which holds the old line geometry creation and is extended to use AA when
     // switched on. Advantage is that line geometry is only temporarily used for paint
