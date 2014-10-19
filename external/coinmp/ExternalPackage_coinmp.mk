b62d69a653d6562a9982d3cb2395a685e0f2d070
Robert Antoni Buj i Gelonch <robert.buj@gmail.com>
2014-10-19 22:09:24 +0200

    coinmp: change dynamic shared library install names
    Change-Id: I7b28e799bdd68d115175bb890584bdbc1027fb6f

--- external/coinmp/ExternalPackage_coinmp.mk
+++ external/coinmp/ExternalPackage_coinmp.mk
@@ -7,6 +7,9 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 
+# for VERSION
+include $(SRCDIR)/external/coinmp/version.mk
+
 $(eval $(call gb_ExternalPackage_ExternalPackage,coinmp,coinmp))
 
 $(eval $(call gb_ExternalPackage_use_external_project,coinmp,coinmp))
@@ -16,23 +19,23 @@ $(eval $(call gb_ExternalPackage_add_file,coinmp,$(LIBO_LIB_FOLDER)/CoinMP.dll,C
 else ifneq ($(DISABLE_DYNLOADING),)
 # Just use the static archives from workdir. See bin/lo-all-static-libs
 else ifeq ($(OS),MACOSX)
-$(eval $(call gb_ExternalPackage_add_file,coinmp,$(LIBO_LIB_FOLDER)/libCbc.3.dylib,Cbc/src/.libs/libCbc.3.8.8.dylib))
-$(eval $(call gb_ExternalPackage_add_file,coinmp,$(LIBO_LIB_FOLDER)/libCbcSolver.3.dylib,Cbc/src/.libs/libCbcSolver.3.8.8.dylib))
-$(eval $(call gb_ExternalPackage_add_file,coinmp,$(LIBO_LIB_FOLDER)/libCgl.1.dylib,Cgl/src/.libs/libCgl.1.8.5.dylib))
-$(eval $(call gb_ExternalPackage_add_file,coinmp,$(LIBO_LIB_FOLDER)/libClp.1.dylib,Clp/src/.libs/libClp.1.12.6.dylib))
-$(eval $(call gb_ExternalPackage_add_file,coinmp,$(LIBO_LIB_FOLDER)/libOsiClp.1.dylib,Clp/src/OsiClp/.libs/libOsiClp.1.12.6.dylib))
-$(eval $(call gb_ExternalPackage_add_file,coinmp,$(LIBO_LIB_FOLDER)/libCoinMP.1.dylib,CoinMP/src/.libs/libCoinMP.1.7.6.dylib))
-$(eval $(call gb_ExternalPackage_add_file,coinmp,$(LIBO_LIB_FOLDER)/libCoinUtils.3.dylib,CoinUtils/src/.libs/libCoinUtils.3.9.11.dylib))
-$(eval $(call gb_ExternalPackage_add_file,coinmp,$(LIBO_LIB_FOLDER)/libOsi.1.dylib,Osi/src/Osi/.libs/libOsi.1.11.5.dylib))
+$(eval $(call gb_ExternalPackage_add_file,coinmp,$(LIBO_LIB_FOLDER)/libCbc.$(CBC_MAJOR_VERSION).dylib,Cbc/src/.libs/libCbc.$(CBC_VERSION).dylib))
+$(eval $(call gb_ExternalPackage_add_file,coinmp,$(LIBO_LIB_FOLDER)/libCbcSolver.$(CBCSOLVER_MAJOR_VERSION).dylib,Cbc/src/.libs/libCbcSolver.$(CBCSOLVER_VERSION).dylib))
+$(eval $(call gb_ExternalPackage_add_file,coinmp,$(LIBO_LIB_FOLDER)/libCgl.$(CGL_MAJOR_VERSION).dylib,Cgl/src/.libs/libCgl.$(CGL_VERSION).dylib))
+$(eval $(call gb_ExternalPackage_add_file,coinmp,$(LIBO_LIB_FOLDER)/libClp.$(CLP_MAJOR_VERSION).dylib,Clp/src/.libs/libClp.$(CLP_VERSION).dylib))
+$(eval $(call gb_ExternalPackage_add_file,coinmp,$(LIBO_LIB_FOLDER)/libOsiClp.$(OSICLP_MAJOR_VERSION).dylib,Clp/src/OsiClp/.libs/libOsiClp.$(OSICLP_VERSION).dylib))
+$(eval $(call gb_ExternalPackage_add_file,coinmp,$(LIBO_LIB_FOLDER)/libCoinMP.$(COINMP_MAJOR_VERSION).dylib,CoinMP/src/.libs/libCoinMP.$(COINMP_VERSION).dylib))
+$(eval $(call gb_ExternalPackage_add_file,coinmp,$(LIBO_LIB_FOLDER)/libCoinUtils.$(COINUTILS_MAJOR_VERSION).dylib,CoinUtils/src/.libs/libCoinUtils.$(COINUTILS_VERSION).dylib))
+$(eval $(call gb_ExternalPackage_add_file,coinmp,$(LIBO_LIB_FOLDER)/libOsi.$(OSI_MAJOR_VERSION).dylib,Osi/src/Osi/.libs/libOsi.$(OSI_VERSION).dylib))
 else
-$(eval $(call gb_ExternalPackage_add_file,coinmp,$(LIBO_LIB_FOLDER)/libCbc.so.3,Cbc/src/.libs/libCbc.so.3.8.8))
-$(eval $(call gb_ExternalPackage_add_file,coinmp,$(LIBO_LIB_FOLDER)/libCbcSolver.so.3,Cbc/src/.libs/libCbcSolver.so.3.8.8))
-$(eval $(call gb_ExternalPackage_add_file,coinmp,$(LIBO_LIB_FOLDER)/libCgl.so.1,Cgl/src/.libs/libCgl.so.1.8.5))
-$(eval $(call gb_ExternalPackage_add_file,coinmp,$(LIBO_LIB_FOLDER)/libClp.so.1,Clp/src/.libs/libClp.so.1.12.6))
-$(eval $(call gb_ExternalPackage_add_file,coinmp,$(LIBO_LIB_FOLDER)/libOsiClp.so.1,Clp/src/OsiClp/.libs/libOsiClp.so.1.12.6))
-$(eval $(call gb_ExternalPackage_add_file,coinmp,$(LIBO_LIB_FOLDER)/libCoinMP.so.1,CoinMP/src/.libs/libCoinMP.so.1.7.6))
-$(eval $(call gb_ExternalPackage_add_file,coinmp,$(LIBO_LIB_FOLDER)/libCoinUtils.so.3,CoinUtils/src/.libs/libCoinUtils.so.3.9.11))
-$(eval $(call gb_ExternalPackage_add_file,coinmp,$(LIBO_LIB_FOLDER)/libOsi.so.1,Osi/src/Osi/.libs/libOsi.so.1.11.5))
+$(eval $(call gb_ExternalPackage_add_file,coinmp,$(LIBO_LIB_FOLDER)/libCbc.so.$(CBC_MAJOR_VERSION),Cbc/src/.libs/libCbc.so.$(CBC_VERSION)))
+$(eval $(call gb_ExternalPackage_add_file,coinmp,$(LIBO_LIB_FOLDER)/libCbcSolver.so.$(CBCSOLVER_MAJOR_VERSION),Cbc/src/.libs/libCbcSolver.so.$(CBCSOLVER_VERSION)))
+$(eval $(call gb_ExternalPackage_add_file,coinmp,$(LIBO_LIB_FOLDER)/libCgl.so.$(CGL_MAJOR_VERSION),Cgl/src/.libs/libCgl.so.$(CGL_VERSION)))
+$(eval $(call gb_ExternalPackage_add_file,coinmp,$(LIBO_LIB_FOLDER)/libClp.so.$(CLP_MAJOR_VERSION),Clp/src/.libs/libClp.so.$(CLP_VERSION)))
+$(eval $(call gb_ExternalPackage_add_file,coinmp,$(LIBO_LIB_FOLDER)/libOsiClp.so.$(OSICLP_MAJOR_VERSION),Clp/src/OsiClp/.libs/libOsiClp.so.$(OSICLP_VERSION)))
+$(eval $(call gb_ExternalPackage_add_file,coinmp,$(LIBO_LIB_FOLDER)/libCoinMP.so.$(COINMP_MAJOR_VERSION),CoinMP/src/.libs/libCoinMP.so.$(COINMP_VERSION)))
+$(eval $(call gb_ExternalPackage_add_file,coinmp,$(LIBO_LIB_FOLDER)/libCoinUtils.so.$(COINUTILS_MAJOR_VERSION),CoinUtils/src/.libs/libCoinUtils.so.$(COINUTILS_VERSION)))
+$(eval $(call gb_ExternalPackage_add_file,coinmp,$(LIBO_LIB_FOLDER)/libOsi.so.$(OSI_MAJOR_VERSION),Osi/src/Osi/.libs/libOsi.so.$(OSI_VERSION)))
 endif
 
 # vim: set noet sw=4 ts=4:
