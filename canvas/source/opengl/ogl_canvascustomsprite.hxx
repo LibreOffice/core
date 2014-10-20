5943fdb3dfa10268699cd55ea79aee5d1ca156fb
Michael Jaumann <meta_dev@yahoo.com>
2014-10-20 12:14:42 +0000

    reformating, alpha blending now in fragment shader
    Change-Id: I9f1d707e2434712efb9e3e346ecf50aa9c539d5b

--- canvas/source/opengl/ogl_canvascustomsprite.hxx
+++ canvas/source/opengl/ogl_canvascustomsprite.hxx
@@ -92,20 +92,26 @@ namespace oglcanvas
         ::basegfx::B2DPoint                               maPosition;
         double                                            mfAlpha;
         double                                            mfPriority;
+
         GLuint                                            m_vertexBuffer;
         GLuint                                            m_uvBuffer;
-        GLuint                                            m_texManProgID;
-        GLuint                                            m_texProgID;
+
+
         GLuint                                            m_simpleProgID;
-        GLuint                                            m_manTexUnf;
-        GLuint                                            m_manPosAttrb;
         GLuint                                            m_simpleUvAttrb;
         GLuint                                            m_simplePosAttrb;
-        GLuint                                            m_texPosAttrb;
-        GLuint                                            m_manCordUnf;
-        GLuint                                            m_texColorUnf;
         GLuint                                            m_simpleTexUnf;
+        GLuint                                            m_simpleColorUnf;
 
+        GLuint                                            m_manTexUnf;
+        GLuint                                            m_manPosAttrb;
+        GLuint                                            m_manCordUnf;
+        GLuint                                            m_manColorUnf;
+
+        GLuint                                            m_texPosAttrb;
+        GLuint                                            m_texColorUnf;
+        GLuint                                            m_texManProgID;
+        GLuint                                            m_texProgID;
     };
 }
 
