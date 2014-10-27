8430b03c79084c0777759a28e682614f73810743
Robert Antoni Buj i Gelonch <robert.buj@gmail.com>
2014-10-27 19:52:51 +0100

    Apache Commons Codec 1.9 requires Java 1.6
    Change-Id: Ie7834a908c8b19cfa46d71ff6fd712e10bbe8fbb

--- external/apache-commons/ExternalProject_apache_commons_codec.mk
+++ external/apache-commons/ExternalProject_apache_commons_codec.mk
@@ -22,8 +22,10 @@ $(call gb_ExternalProject_get_state_target,apache_commons_codec,build) :
 		-Dbuild.label="build-$(LIBO_VERSION_MAJOR).$(LIBO_VERSION_MINOR).$(LIBO_VERSION_MICRO).$(LIBO_VERSION_PATCH)" \
 		$(if $(filter yes,$(JAVACISGCJ))\
 			,-Dbuild.compiler=gcj \
-			,-Dant.build.javac.source=$(JAVA_SOURCE_VER) \
-				-Dant.build.javac.target=$(JAVA_TARGET_VER) \
+			,$(if $(filter YES,$(HAVE_JAVA6)) \
+				-Dcompile.source=1.6 -Dcompile.target=1.6 -Dcompile.debug=false, \
+				-Dant.build.javac.source=$(JAVA_SOURCE_VER) -Dant.build.javac.target=$(JAVA_TARGET_VER) \
+			) \
 		) \
 		$(if $(debug),-Dbuild.debug="on") \
 		jar \
