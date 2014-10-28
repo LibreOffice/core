e9566f27c8b8bab822888c285abec06b58432358
Caolán McNamara <caolanm@redhat.com>
2014-10-28 15:24:23 +0000

    fix a big pile of the crash tester .doc import failures
    wrt the blasted SwIndexReg dtor assert
    
    Change-Id: I4b3eeab76d5d76f7b769b458baeadbc0a9bd4238

--- sw/source/filter/ww8/ww8par.cxx
+++ sw/source/filter/ww8/ww8par.cxx
@@ -5404,11 +5404,13 @@ sal_uLong SwWW8ImplReader::CoreLoad(WW8Glossary *pGloss, const SwPosition &rPos)
     if (mbNewDoc)
       rDoc.getIDocumentRedlineAccess().SetRedlineMode((RedlineMode_t)( eMode ));
 
-    UpdatePageDescs(rDoc, nPageDescOffset);
-
     SAL_WARN_IF(mpTableEndPaM, "sw.ww8", "document ended without table ending");
     mpTableEndPaM.reset();  //ensure this is deleted before pPaM
     delete pPaM, pPaM = 0;
+    lastAnchorPos.reset();  //ensure this is deleted before UpdatePageDescs
+
+    UpdatePageDescs(rDoc, nPageDescOffset);
+
     return nErrRet;
 }
 
