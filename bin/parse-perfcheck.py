926488ba433b6906b650ff959eb9a15f841ef6c2
Laurent Godard <lgodard.libre@laposte.net>
2014-10-30 16:29:45 +0100

    Parse perfcheck results
    add a columned output
    
    Change-Id: Ib7ba87e9cb55b03ac8665db7a8dc302d2e8611a0

--- bin/parse-perfcheck.py
+++ bin/parse-perfcheck.py
@@ -15,12 +15,13 @@ parseTotal = "totals: "
 separator = os.path.sep
 
 lastCommitId = ""
+lastCommitDate = ""
 needsCsvHeader = True
 
 def processDirectory(rootDir):
 
   if needsCsvHeader:
-    intermediateResult = "lastCommit\ttest name\tfiledatetime\tdump comment\tcount\n"
+    intermediateResult = "lastCommit\ttest name\tdatetime\tdump comment\tcount\n"
   else:
    intermediateResult = ""
 
@@ -60,66 +61,202 @@ def parseFile(dirname, filename):
 
   message = message.replace("\n","")
 
-  fileDate = time.strftime('%m/%d/%Y %H:%M:%S', time.gmtime(os.path.getmtime(path)))
-
-  result = lastCommitId + "\t" + testName + "\t" + fileDate + "\t" + message + "\t" + total
+  result = lastCommitId + "\t" + testName + "\t" + lastCommitDate + "\t" + message + "\t" + total
 
   return result
 
-def getLastCommitId():
+def getLastCommitInfo():
 
-  stream = os.popen("git log")
+  stream = os.popen("git log --date=iso")
   line = stream.readline()
-  return line.replace("commit ","").replace("\n","")
+  commitId = line.replace("commit ","").replace("\n","")
+  line = stream.readline()
+  line = stream.readline()
+  commitDate = line.replace("Date: ","").replace("\n","").strip()
+
+  return commitId, commitDate
 
 def displayUsage():
 
   print
   print "Parses the callgrind results of make percheck"
   print
-  print "Usage: bin/parse_perfcheck.py [targetFileName = perfcheckResult.csv] [sourceDirectory = ./workdir/CppunitTest]"
-  print "default assumes running from core root directory"
+  print "Usage: bin/parse_perfcheck.py [targetFileName = perfcheckResult.csv] [sourceDirectory = ./workdir/CppunitTest] ['--alert-previous'||'--alert-global'] [alertValue = 10]"
   print
+  print "default assumes running from core root directory"
+  print "per test columned output written at targetFileName +'.col'"
+  print
+  print "--alert-previous : compare to the previous entry of the test"
+  print "--alert-global : compare to the first entry entry of the test"
+  print "alertValue : value in %"
+  print
+
+def analyzeArgs(args):
+
+    isValid = True
+
+    alertType = ""
+    alertValue = 10
+
+    if len(args) < 3:
+      if len(args) == 2:
+          if args[1] == "--help":
+            isValid = False
+          else:
+            targetFileName = args[1]
+            sourceDirectory = "./workdir/CppunitTest"
+      elif len(args) == 1:
+          targetFileName = "perfcheckResult.csv"
+          sourceDirectory = "./workdir/CppunitTest"
+      else:
+          isValid = False
+    else:
+
+      targetFileName = args[1]
+      sourceDirectory = args[2]
+
+      if len(args) > 3:
+        alertType = args[3]
+        if alertType not in ['--alert-previous','--alert-global']:
+          isValid = False
+
+      if len(args) > 4:
+          alertValue = args[4]
+
+    return isValid, targetFileName, sourceDirectory, alertType, alertValue
 
 if __name__ == '__main__':
 
   #check args
+  isOk, targetFileName, sourceDirectory, alertType, alertValue = analyzeArgs(sys.argv)
 
-  if len(sys.argv) < 3:
-      if len(sys.argv) == 2:
-          if sys.argv[1] == "--help":
-            displayUsage()
-            sys.exit(1)
-          else:
-            targetFileName = sys.argv[1]
-            sourceDirectory = "./workdir/CppunitTest"
-      elif len(sys.argv) == 1:
-          targetFileName = "perfcheckResult.csv"
-          sourceDirectory = "./workdir/CppunitTest"
-      else:
-          displayUsage()
-          sys.exit(1)
-  else:
-      targetFileName = sys.argv[1]
-      sourceDirectory = sys.argv[2]
+  if not isOk:
+    displayUsage()
+    sys.exit(1)
 
   # check if sourceDirectorty exists
   if not os.path.isdir(sourceDirectory):
-    print "sourceDirectorty %s not found - Aborting" % (sourceDirectory)
+    print "sourceDirectory %s not found - Aborting" % (sourceDirectory)
     sys.exit(1)
 
   # last commit Id
-  lastCommitId = getLastCommitId()
+  lastCommitId, lastCommitDate = getLastCommitInfo()
 
-  # needs header in csv file ?
+  # needs header in csv file ? yes if new
   needsCsvHeader = not os.path.isfile(targetFileName)
 
-  # call walker
+  # walker through directory
   globalResult = processDirectory(sourceDirectory)
 
-  print globalResult
+  print '\n' + globalResult
 
-  # write result
+  # append raw result
   fileResult = open(targetFileName,'a')
   fileResult.write(globalResult)
   fileResult.close()
+
+  print "\nCSV file written at " + targetFileName + '\n'
+
+# format result by column
+
+  # read the complete CSV file
+  fileResult = open(targetFileName,'r')
+  lines = fileResult.readlines()
+  fileResult.close
+
+  lines = lines[1:]
+
+  colsResult = {}
+  allTests = []
+
+  for line in lines:
+
+    spliter = line.replace('\n','').split('\t')
+    curId = spliter[0]
+    curDate = spliter[2]
+    curTestName = spliter[1]
+    curTestComment = spliter[3]
+    curValue = spliter[4]
+
+    if not curTestComment in allTests:
+      allTests.append(curTestComment)
+
+    if not colsResult.has_key(curId):
+      colsResult[curId] = {}
+      colsResult[curId]['date'] = curDate
+      colsResult[curId]['values'] = {}
+
+    colsResult[curId]['values'][curTestComment] = curValue
+
+# build columned output
+
+  # header
+  mLine = "commit\tdate\t"
+  for t in allTests:
+    mLine += t + "\t"
+  mLine += '\n'
+
+  # commits
+  alertTest = {}
+
+  for k in colsResult.keys():
+
+    mLine += k + "\t" + colsResult[k]['date'] + "\t"
+
+    for t in allTests:
+
+      if colsResult[k]['values'].has_key(t):
+        mValue= colsResult[k]['values'][t]
+
+        if not t in alertTest:
+          alertTest[t] = {}
+        alertTest[t][colsResult[k]['date']] = mValue
+
+      else:
+        mValue = ""
+
+      mLine += mValue + "\t"
+
+    mLine += "\n"
+
+  # write columned result
+  fileResult = open(targetFileName + '.col','w')
+  fileResult.write(mLine)
+  fileResult.close()
+
+  print "Columned file written at " + targetFileName + '.col\n'
+
+# check for Alerts
+
+  if alertType == "":
+    sys.exit(1)
+
+  alertResult = ""
+
+  for t in alertTest:
+
+    testDict = alertTest[t]
+
+    # sort
+    keylist = testDict.keys()
+    keylist.sort()
+    maxVal = float(testDict[keylist[-1]])
+    minVal = 0
+
+    if alertType == "--alert-previous":
+      if len(keylist) > 1:
+        minVal = float(testDict[keylist[-2]])
+
+    else:
+      minVal = float(testDict[keylist[0]])
+
+    if minVal != 0:
+      delta = 100 * ((maxVal-minVal)/minVal)
+    else:
+      delta = 0
+
+    if delta > float(alertValue):
+      alertResult +=  t + "\t" + "{:.2f}".format(delta) + " %\n"
+
+  if alertResult != "":
+    print "!!!!!!!! ALERT !!!!!!!\n\n" + alertResult
