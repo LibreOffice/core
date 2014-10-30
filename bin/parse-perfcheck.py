1aa9186eb98631f6c7eb800bba50d91f89fc3763
Laurent Godard <lgodard.libre@laposte.net>
2014-10-30 16:29:45 +0100

    Parse perfcheck results
    add a columned output
    
    Change-Id: Ib7ba87e9cb55b03ac8665db7a8dc302d2e8611a0

--- bin/parse-perfcheck.py
+++ bin/parse-perfcheck.py
@@ -15,12 +15,13 @@ parseTotal = "totals: "
 separator = os.path.sep
 
 lastCommitId = ""
+lastCommitDate = ""
 needsCsvHeader = True
 
 def processDirectory(rootDir):
 
   if needsCsvHeader:
-    intermediateResult = "lastCommit\ttest name\tfiledatetime\tdump comment\tcount\n"
+    intermediateResult = "lastCommit\ttest name\tdatetime\tdump comment\tcount\n"
   else:
    intermediateResult = ""
 
@@ -60,17 +61,20 @@ def parseFile(dirname, filename):
 
   message = message.replace("\n","")
 
-  fileDate = time.strftime('%m/%d/%Y %H:%M:%S', time.gmtime(os.path.getmtime(path)))
-
-  result = lastCommitId + "\t" + testName + "\t" + fileDate + "\t" + message + "\t" + total
+  result = lastCommitId + "\t" + testName + "\t" + lastCommitDate + "\t" + message + "\t" + total
 
   return result
 
-def getLastCommitId():
+def getLastCommitInfo():
 
-  stream = os.popen("git log")
+  stream = os.popen("git log --date=iso")
   line = stream.readline()
-  return line.replace("commit ","").replace("\n","")
+  commitId = line.replace("commit ","").replace("\n","")
+  line = stream.readline()
+  line = stream.readline()
+  commitDate = line.replace("Date: ","").replace("\n","").strip()
+
+  return commitId, commitDate
 
 def displayUsage():
 
@@ -80,6 +84,9 @@ def displayUsage():
   print "Usage: bin/parse_perfcheck.py [targetFileName = perfcheckResult.csv] [sourceDirectory = ./workdir/CppunitTest]"
   print "default assumes running from core root directory"
   print
+  print "per test columned output written at targetFileName +'.col'"
+  print
+
 
 if __name__ == '__main__':
 
@@ -109,17 +116,81 @@ if __name__ == '__main__':
     sys.exit(1)
 
   # last commit Id
-  lastCommitId = getLastCommitId()
+  lastCommitId, lastCommitDate = getLastCommitInfo()
 
-  # needs header in csv file ?
+  # needs header in csv file ? yes if new
   needsCsvHeader = not os.path.isfile(targetFileName)
 
-  # call walker
+  # walker through directory
   globalResult = processDirectory(sourceDirectory)
 
-  print globalResult
+  print '\n' + globalResult
 
-  # write result
+  # append raw result
   fileResult = open(targetFileName,'a')
   fileResult.write(globalResult)
   fileResult.close()
+
+  print "\nCSV file written at " + targetFileName + '\n'
+
+# format result by column
+
+  # read the complete CSV file
+  fileResult = open(targetFileName,'r')
+  lines = fileResult.readlines()
+  fileResult.close
+
+  lines = lines[1:]
+
+  colsResult = {}
+  allTests = []
+
+  for line in lines:
+
+    spliter = line.replace('\n','').split('\t')
+    curId = spliter[0]
+    curDate = spliter[2]
+    curTestName = spliter[1]
+    curTestComment = spliter[3]
+    curValue = spliter[4]
+
+    if not curTestComment in allTests:
+      allTests.append(curTestComment)
+
+    if not colsResult.has_key(curId):
+      colsResult[curId] = {}
+      colsResult[curId]['date'] = curDate
+      colsResult[curId]['values'] = {}
+
+    colsResult[curId]['values'][curTestComment] = curValue
+
+# build columned output
+
+  # header
+  mLine = "commit\tdate\t"
+  for t in allTests:
+    mLine += t + "\t"
+  mLine += '\n'
+
+  # commits
+  for k in colsResult.keys():
+
+    mLine += k + "\t" + colsResult[k]['date'] + "\t"
+
+    for t in allTests:
+
+      if colsResult[k]['values'].has_key(t):
+        mValue= colsResult[k]['values'][t]
+      else:
+        mValue = ""
+
+      mLine += mValue + "\t"
+
+    mLine += "\n"
+
+  # write columned result
+  fileResult = open(targetFileName + '.col','w')
+  fileResult.write(mLine)
+  fileResult.close()
+
+  print "Columned file written at " + targetFileName + '.col\n'
