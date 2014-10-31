11bf07e809d6b7be7b420c03caddcca32a36041b
Michael Stahl <mstahl@redhat.com>
2014-10-31 19:34:12 +0100

    scp2: forgot to remove file_quickstart from makefile
    Change-Id: Ieb34db019c03e7f20f06d3a690b3717b41645e90

--- scp2/InstallModule_quickstart.mk
+++ scp2/InstallModule_quickstart.mk
@@ -14,7 +14,6 @@ $(eval $(call gb_installmodule_use_auto_install_libs,scp2/quickstart,\
 ))
 
 $(eval $(call gb_InstallModule_add_scpfiles,scp2/quickstart,\
-    scp2/source/quickstart/file_quickstart \
     scp2/source/quickstart/registryitem_quickstart \
 ))
 
