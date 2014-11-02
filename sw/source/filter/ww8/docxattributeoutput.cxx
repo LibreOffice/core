d72237741ed1d8f976032ff2ee5d2a8702d3380e
Miklos Vajna <vmiklos@collabora.co.uk>
2014-11-02 18:22:16 +0100

    abi#9930 DocxAttributeOutput::AddToAttrList: avoid duplicated attributes
    Change-Id: Ia799fc8b1b0e9bc6852b21194b71e1f3e683955e

--- sw/source/filter/ww8/docxattributeoutput.cxx
+++ sw/source/filter/ww8/docxattributeoutput.cxx
@@ -8422,7 +8422,7 @@ void DocxAttributeOutput::AddToAttrList( ::sax_fastparser::FastAttributeList* &p
     {
         sal_Int32 nName = va_arg( args, sal_Int32 );
         const char* pValue = va_arg( args, const char* );
-        if( pValue )
+        if( pValue && !pAttrList->hasAttribute(nName) )
             pAttrList->add( nName, pValue );
     }
     va_end( args );
