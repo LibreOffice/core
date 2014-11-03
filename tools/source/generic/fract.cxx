7f3326f2aa37f16dbf12844895d269dec5ad8c45
Caolán McNamara <caolanm@redhat.com>
2014-11-03 08:53:46 +0000

    checker type doesn't work, try event type
    Change-Id: I8b92c583bf1d6167b713648f417ba5569b3593ba

--- tools/source/generic/fract.cxx
+++ tools/source/generic/fract.cxx
@@ -63,7 +63,7 @@ Fraction::Fraction( double dVal )
 
 bool Fraction::HasOverflowValue()
 {
-    //coverity[constant_expression_result]
+    //coverity[result_independent_of_operands]
     return value.numerator() < std::numeric_limits<long>::min() ||
         value.numerator() > std::numeric_limits<long>::max() ||
         value.denominator() < std::numeric_limits<long>::min() ||
