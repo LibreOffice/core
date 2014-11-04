9ef8ab5379aff4d748c56afe21e4b321e857f7a7
Miklos Vajna <vmiklos@collabora.co.uk>
2014-11-04 16:48:29 +0100

    CppunitTest_sw_rtfexport: fix windows build
    Change-Id: I79d4e46abe289b3d9c1f0a2eec2a29045891cae3

--- sw/qa/extras/rtfexport/rtfexport.cxx
+++ sw/qa/extras/rtfexport/rtfexport.cxx
@@ -659,13 +659,13 @@ DECLARE_RTFEXPORT_TEST(testFdo79599, "fdo79599.rtf")
     CPPUNIT_ASSERT_EQUAL(sal_uInt32(0x800080), getProperty<sal_uInt32>(xRun, "CharBackColor"));
 }
 
-#endif
-
 DECLARE_RTFEXPORT_TEST(testFdo82858, "fdo82858.docx")
 {
     // This was table::BorderLineStyle::SOLID, exporter failed to write explicit no line when line color was written.
     CPPUNIT_ASSERT_EQUAL(table::BorderLineStyle::NONE, getProperty<table::BorderLine2>(getShape(1), "TopBorder").LineStyle);
 }
+
+#endif
 
 CPPUNIT_PLUGIN_IMPLEMENT();
 
