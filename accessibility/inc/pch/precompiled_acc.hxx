0ddc9f710b5ebfaa42835b717c7ae534ed82d8fd
Andrea Gelmini <andrea.gelmini@gelma.net>
2014-11-06 00:43:24 +0100

    Removed vcl/fixed.hxx
    Change-Id: I93dca1e26c7f4d1136d69395ed081dcc6868a2d5

--- accessibility/inc/pch/precompiled_acc.hxx
+++ accessibility/inc/pch/precompiled_acc.hxx
@@ -19,7 +19,6 @@
 #include <svtools/svlbitm.hxx>
 #include <svtools/tabbar.hxx>
 #include <svtools/treelistbox.hxx>
-#include <vcl/fixed.hxx>
 #include <vcl/help.hxx>
 #include <vcl/lstbox.hxx>
 #include <vcl/menu.hxx>
