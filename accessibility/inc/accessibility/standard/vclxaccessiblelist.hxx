ee77a243cf25e94569b8a0d8a2f14a13909e0cc3
Andrea Gelmini <andrea.gelmini@gelma.net>
2014-11-05 21:58:53 +0100

    Removed accessibility/standard/vclxaccessiblelistitem.hxx
    Change-Id: I162848ac7dac5b901b517b0c95a2ad72e0ff805f

--- accessibility/inc/accessibility/standard/vclxaccessiblelist.hxx
+++ accessibility/inc/accessibility/standard/vclxaccessiblelist.hxx
@@ -20,7 +20,6 @@
 #ifndef INCLUDED_ACCESSIBILITY_INC_ACCESSIBILITY_STANDARD_VCLXACCESSIBLELIST_HXX
 #define INCLUDED_ACCESSIBILITY_INC_ACCESSIBILITY_STANDARD_VCLXACCESSIBLELIST_HXX
 
-#include "accessibility/standard/vclxaccessiblelistitem.hxx"
 #include <accessibility/standard/vclxaccessibleedit.hxx>
 #include <com/sun/star/accessibility/AccessibleRole.hpp>
 #include <com/sun/star/accessibility/XAccessibleAction.hpp>
