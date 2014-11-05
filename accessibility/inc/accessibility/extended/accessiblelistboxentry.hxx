2e93ea06a4aa1375872d200ce148430bdfeabb7c
Andrea Gelmini <andrea.gelmini@gelma.net>
2014-11-05 19:13:35 +0100

    Removed com/sun/star/accessibility/XAccessibleStateSet.hpp
    Change-Id: Ic7723be0f6199adb8b65b9efb6a747a273804bb0

--- accessibility/inc/accessibility/extended/accessiblelistboxentry.hxx
+++ accessibility/inc/accessibility/extended/accessiblelistboxentry.hxx
@@ -20,7 +20,6 @@
 #ifndef INCLUDED_ACCESSIBILITY_INC_ACCESSIBILITY_EXTENDED_ACCESSIBLELISTBOXENTRY_HXX
 #define INCLUDED_ACCESSIBILITY_INC_ACCESSIBILITY_EXTENDED_ACCESSIBLELISTBOXENTRY_HXX
 
-#include <com/sun/star/accessibility/XAccessibleStateSet.hpp>
 #include <com/sun/star/accessibility/XAccessibleEventBroadcaster.hpp>
 #include <com/sun/star/accessibility/XAccessibleAction.hpp>
 #include <com/sun/star/accessibility/XAccessibleSelection.hpp>
