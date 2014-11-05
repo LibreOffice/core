99e9277ef679e74527fca6c1861b411880c245e1
Andrea Gelmini <andrea.gelmini@gelma.net>
2014-11-05 17:09:19 +0100

    Removed tools/gen.hxx
    Change-Id: I09669f68af7399a2085e77823650d7a972d8e455

--- accessibility/inc/accessibility/extended/AccessibleBrowseBoxBase.hxx
+++ accessibility/inc/accessibility/extended/AccessibleBrowseBoxBase.hxx
@@ -23,7 +23,6 @@
 
 
 #include <svtools/AccessibleBrowseBoxObjType.hxx>
-#include <tools/gen.hxx>
 #include <vcl/svapp.hxx>
 #include <cppuhelper/compbase5.hxx>
 #include <comphelper/broadcasthelper.hxx>
