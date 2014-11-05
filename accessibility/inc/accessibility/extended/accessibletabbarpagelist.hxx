7a13854d2de4d518d95e23d8a0d22bb93fc99bed
Andrea Gelmini <andrea.gelmini@gelma.net>
2014-11-05 19:51:00 +0100

    Removed com/sun/star/accessibility/XAccessible.hpp
    Change-Id: Icd8f9a37ec1528e1dd27d0075d291cc4801b2700

--- accessibility/inc/accessibility/extended/accessibletabbarpagelist.hxx
+++ accessibility/inc/accessibility/extended/accessibletabbarpagelist.hxx
@@ -20,7 +20,6 @@
 #ifndef INCLUDED_ACCESSIBILITY_INC_ACCESSIBILITY_EXTENDED_ACCESSIBLETABBARPAGELIST_HXX
 #define INCLUDED_ACCESSIBILITY_INC_ACCESSIBILITY_EXTENDED_ACCESSIBLETABBARPAGELIST_HXX
 
-#include <com/sun/star/accessibility/XAccessible.hpp>
 #include <com/sun/star/accessibility/XAccessibleSelection.hpp>
 #include <com/sun/star/lang/XServiceInfo.hpp>
 #include <cppuhelper/implbase3.hxx>
