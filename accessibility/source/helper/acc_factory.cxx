e30a2aae739cd075f5f2dddbf0a128c00d1300ec
Andrea Gelmini <andrea.gelmini@gelma.net>
2014-11-06 04:42:34 +0100

    Removed config_features.h
    Change-Id: If4caaa781aa5ca9a704e95c30b6cfeefaea15962

--- accessibility/source/helper/acc_factory.cxx
+++ accessibility/source/helper/acc_factory.cxx
@@ -17,7 +17,6 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#include <config_features.h>
 
 #include <toolkit/awt/vclxwindows.hxx>
 #include <toolkit/helper/accessiblefactory.hxx>
