1f4258c6a96105d3ef30a5ed651a0f3a5058cc8a
Andrea Gelmini <andrea.gelmini@gelma.net>
2014-11-06 02:51:54 +0100

    Removed com/sun/star/awt/Size.hpp
    Change-Id: Iaa25588abb2cfdd8e20279eca908624ce8869f5d

--- accessibility/source/extended/accessiblelistbox.cxx
+++ accessibility/source/extended/accessiblelistbox.cxx
@@ -19,7 +19,6 @@
 
 #include <accessibility/extended/accessiblelistbox.hxx>
 #include <accessibility/extended/accessiblelistboxentry.hxx>
-#include <com/sun/star/awt/Size.hpp>
 #include <com/sun/star/accessibility/AccessibleEventId.hpp>
 #include <com/sun/star/accessibility/AccessibleRole.hpp>
 #include <com/sun/star/accessibility/AccessibleStateType.hpp>
