ee916a2c84b1852e7e891fe8c3a31fc1b632b010
Andrea Gelmini <andrea.gelmini@gelma.net>
2014-11-06 03:34:39 +0100

    Removed accessibility/extended/accessibletabbarbase.hxx
    Change-Id: Icb256628c45205ad35a5ff5fd37cff6d14a57908

--- accessibility/source/extended/accessibletabbarbase.cxx
+++ accessibility/source/extended/accessibletabbarbase.cxx
@@ -17,7 +17,6 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#include "accessibility/extended/accessibletabbarbase.hxx"
 #ifndef ACCESSIBILITY_EXT_ACCESSIBLETABBARPAGELIST
 #include "accessibility/extended/accessibletabbarpagelist.hxx"
 #endif
