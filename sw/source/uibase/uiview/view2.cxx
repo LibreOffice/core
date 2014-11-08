549da700ad9cbea68cff933ee4c83b95407f47fb
Samuel Mehrbrodt <s.mehrbrodt@gmail.com>
2014-11-08 23:02:11 +0100

    fdo#86081 Don't do anything when clicked on placeholder in statusbar
    No idea why this code got executed here, it has no action in sd and sc.
    
    Change-Id: I1d2b4b049f6ae6d117e7bf3464ae993c6951f1dc

--- sw/source/uibase/uiview/view2.cxx
+++ sw/source/uibase/uiview/view2.cxx
@@ -1478,26 +1478,6 @@ void SwView::StateStatusLine(SfxItemSet &rSet)
                     rSet.DisableItem( SID_ATTR_ZOOMSLIDER );
             }
             break;
-            case SID_ATTR_POSITION:
-            case SID_ATTR_SIZE:
-            {
-                if( !rShell.IsFrmSelected() && !rShell.IsObjSelected() )
-                    SwBaseShell::_SetFrmMode( FLY_DRAG_END );
-                else
-                {
-                    FlyMode eFrameMode = SwBaseShell::GetFrmMode();
-                    if ( eFrameMode == FLY_DRAG_START || eFrameMode == FLY_DRAG )
-                    {
-                        if ( nWhich == SID_ATTR_POSITION )
-                            rSet.Put( SfxPointItem( SID_ATTR_POSITION,
-                                                    rShell.GetAnchorObjDiff()));
-                        else
-                            rSet.Put( SvxSizeItem( SID_ATTR_SIZE,
-                                                   rShell.GetObjSize()));
-                    }
-                }
-            }
-            break;
             case SID_TABLE_CELL:
 
             if( rShell.IsFrmSelected() || rShell.IsObjSelected() )
@@ -1786,39 +1766,6 @@ void SwView::ExecuteStatusLine(SfxRequest &rReq)
                 bUp = true;
                 rReq.Done();
             }
-        }
-        break;
-
-        case SID_ATTR_SIZE:
-        {
-            sal_uInt16 nId = FN_INSERT_FIELD;
-            if( rSh.IsCrsrInTbl() )
-                nId = FN_FORMAT_TABLE_DLG;
-            else if( rSh.GetCurTOX() )
-                nId = FN_INSERT_MULTI_TOX;
-            else if( rSh.GetCurrSection() )
-                nId = FN_EDIT_REGION;
-            else
-            {
-                const SwNumRule* pNumRule = rSh.GetNumRuleAtCurrCrsrPos();
-                if( pNumRule )  // cursor in numbering
-                {
-                    if( pNumRule->IsAutoRule() )
-                        nId = FN_NUMBER_BULLETS;
-                    else
-                    {
-                        // start dialog of the painter
-                        nId = 0;
-                    }
-                }
-                else if( rSh.IsFrmSelected() )
-                    nId = FN_FORMAT_FRAME_DLG;
-                else if( rSh.IsObjSelected() )
-                    nId = SID_ATTR_TRANSFORM;
-            }
-            if( nId )
-                GetViewFrame()->GetDispatcher()->Execute(nId,
-                    SfxCallMode::SYNCHRON | SfxCallMode::RECORD );
         }
         break;
 
