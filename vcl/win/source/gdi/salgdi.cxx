556ddb68dca3515597517bb25a7463dcc68fdc2e
Markus Mohrhard <markus.mohrhard@googlemail.com>
2014-11-10 04:06:25 +0100

    only use OpenGL for window devices for now
    That fixes the build problems that I have seen.
    
    Change-Id: Ida89aa153d73ce4e07e3f0e0499df567e4df5009

--- vcl/win/source/gdi/salgdi.cxx
+++ vcl/win/source/gdi/salgdi.cxx
@@ -590,7 +590,7 @@ WinSalGraphics::WinSalGraphics(WinSalGraphics::Type eType, bool bScreen, HWND hW
     mnPenWidth(GSL_PEN_WIDTH)
 {
     static bool bOpenGLPossible = OpenGLHelper::supportsVCLOpenGL();
-    bool bUseOpenGL = bOpenGLPossible && !mbPrinter ? officecfg::Office::Common::VCL::UseOpenGL::get() : false;
+    bool bUseOpenGL = bOpenGLPossible && mbWindow ? officecfg::Office::Common::VCL::UseOpenGL::get() : false;
     if (bUseOpenGL)
         mpImpl.reset(new WinOpenGLSalGraphicsImpl(*this));
     else
