730a3ac2daa9ec5774fc7b9482ed1aa94afab67f
Markus Mohrhard <markus.mohrhard@collabora.co.uk>
2014-11-10 07:09:59 +0100

    forgot to add the files
    Change-Id: I44a7e9bec213514708e4c971479660574508ad1b

---
+++ vcl/unx/generic/gdi/openglx11cairotextrender.hxx
@@ -0,0 +1,26 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef INCLUDED_VCL_UNX_GENERIC_GDI_OPENGLX11CAIROTEXTRENDER_HXX
+#define INCLUDED_VCL_UNX_GENERIC_GDI_OPENGLX11CAIROTEXTRENDER_HXX value
+
+#include "x11cairotextrender.hxx"
+
+class OpenGLX11CairoTextRender : public X11CairoTextRender
+{
+public:
+    OpenGLX11CairoTextRender(bool bPrinter, X11SalGraphics& rParent);
+
+    virtual cairo_surface_t* getCairoSurface() SAL_OVERRIDE;
+    virtual void drawSurface(cairo_t* cr) SAL_OVERRIDE;
+};
+
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
