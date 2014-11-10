def0fe570bfeb94c283e4e4be58ffc10f45a197f
Daniel Sikeler <d.sikeler94@gmail.com>
2014-11-10 12:23:12 +0000

    fdo#80403: Writer specific AutoCorr use FastParser
    Contextclasses implement XFastContextHandler
    Importclasses implement XFastDocumentHandler
    Tokens for BlockList.xml are defined in BlockListTokens.txt
    Tokens for content.xml are defined in TextBlockTokens.txt
    A perfect hash table is generated with gperf and used by the tokenhandlers
    
    Conflicts:
    	sw/source/core/swg/SwXMLTextBlocks1.cxx
    
    Change-Id: Iff8ad5f1a4df0b808ee6fdf13f75a821596e9ee9

--- sw/Library_sw.mk
+++ sw/Library_sw.mk
@@ -36,6 +36,7 @@ $(eval $(call gb_Library_set_include,sw,\
 
 $(eval $(call gb_Library_use_custom_headers,sw,\
 	officecfg/registry \
+	sw/generated \
 ))
 
 $(eval $(call gb_Library_use_sdk_api,sw))
