6d69185611a88308c3f0b2aa94f6a8549dd2b8fd
Caolán McNamara <caolanm@redhat.com>
2014-11-13 21:03:03 +0000

    coverity#1251584 Unchecked dynamic_cast
    Change-Id: I2022c25f821473240a5ff3998249e933ea7fb74d

--- vcl/unx/generic/gdi/salgdi.cxx
+++ vcl/unx/generic/gdi/salgdi.cxx
@@ -145,7 +145,7 @@ void X11SalGraphics::SetDrawable( Drawable aDrawable, SalX11Screen nXScreen )
 
     if( hDrawable_ )
     {
-        dynamic_cast<X11GraphicsImpl*>(mpImpl.get())->Init();
+        dynamic_cast<X11GraphicsImpl&>(*mpImpl.get()).Init();
         // TODO: moggi: FIXME nTextPixel_     = GetPixel( nTextColor_ );
     }
 }
