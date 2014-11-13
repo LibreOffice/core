b3a6a93abb26e2c1ba987ed7dcd42923f2ef63ba
Stephan Bergmann <sbergman@redhat.com>
2014-11-13 16:44:42 +0100

    external/boost: WaE
    Change-Id: Iffc69577f12020c60bf5db1f740ec3fec43c3d8e

--- external/boost/UnpackedTarball_boost.mk
+++ external/boost/UnpackedTarball_boost.mk
@@ -105,6 +105,7 @@ boost_patches += boost.wunused.patch
 boost_patches += boost.wdeprecated-auto_ptr.patch.0
 boost_patches += boost.signal2.Wshadow.warnings.patch
 boost_patches += boost.boost_static_assert_unused_attribute.patch.0
+boost_patches += w4702.patch.0
 
 $(eval $(call gb_UnpackedTarball_UnpackedTarball,boost))
 
