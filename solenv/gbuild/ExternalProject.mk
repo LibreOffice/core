af85db1aa3468ccf17de72a661d10631df0736ee
Jan Holesovsky <kendy@collabora.com>
2014-11-16 00:11:38 +0100

    windows: msbuild gets completely confused when the 'Platform' envvar is set.
    Change-Id: Iea04859c6afa203bd6b527b99c680ff4176cf9e1

--- solenv/gbuild/ExternalProject.mk
+++ solenv/gbuild/ExternalProject.mk
@@ -212,6 +212,7 @@ define gb_ExternalProject_run
 $(if $(findstring YES,$(UNPACKED_IS_BIN_TARBALL)),\
 	touch $@,
 $(call gb_Helper_print_on_error,cd $(EXTERNAL_WORKDIR)/$(3) && \
+	unset Platform && \
 	$(if $(WRAPPERS),export $(WRAPPERS) &&) \
 	$(if $(NMAKE),INCLUDE="$(gb_ExternalProject_INCLUDE)" LIB="$(ILIB)" MAKEFLAGS=) \
 	$(2) && touch $@,$(EXTERNAL_WORKDIR)/$(if $(3),$(3)/,)$(if $(4),$(4),$(1).log))
