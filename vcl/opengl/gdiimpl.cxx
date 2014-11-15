b7d74366a9a15557a1c3dd98f019034ade137b4e
Michael Meeks <michael.meeks@collabora.com>
2014-11-15 17:33:36 +0100

    vcldemo: disable broken gradient stencil use.
    Change-Id: Ifce85b1a7fa8e8b6fc57802a5fb559d2db50ba2a

--- vcl/opengl/gdiimpl.cxx
+++ vcl/opengl/gdiimpl.cxx
@@ -1373,6 +1373,8 @@ bool OpenGLSalGraphicsImpl::drawGradient(const tools::PolyPolygon& rPolyPoly,
 
     PreDraw();
 
+#define FIXME_BROKEN_STENCIL_FOR_GRADIENTS 0
+#if FIXME_BROKEN_STENCIL_FOR_GRADIENTS
     ImplSetClipBit( vcl::Region( rPolyPoly ), 0x02 );
     if( mbUseStencil )
     {
@@ -1384,6 +1386,7 @@ bool OpenGLSalGraphicsImpl::drawGradient(const tools::PolyPolygon& rPolyPoly,
         glEnable( GL_STENCIL_TEST );
         glStencilFunc( GL_EQUAL, 2, 0xFF );
     }
+#endif
 
     // if border >= 100%, draw solid rectangle with start color
     if( rGradient.GetBorder() >= 100.0 )
@@ -1408,8 +1411,10 @@ bool OpenGLSalGraphicsImpl::drawGradient(const tools::PolyPolygon& rPolyPoly,
         DrawRadialGradient( rGradient, aBoundRect );
     }
 
+#if FIXME_BROKEN_STENCIL_FOR_GRADIENTS
     if( !mbUseStencil )
         glDisable( GL_STENCIL_TEST );
+#endif
     PostDraw();
 
     CHECK_GL_ERROR();
