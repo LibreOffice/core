952b968042e51ff3245a81cb6b970863a92fab5f
Julien Nabet <serval2412@yahoo.fr>
2014-11-15 17:48:20 +0100

    cppcheck: Same expression on both sides of '||'
    Change-Id: I89c758366c2140f0033c1006f6a03bbc17ea8096
    Reviewed-on: https://gerrit.libreoffice.org/12459
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>

--- svx/source/table/svdotable.cxx
+++ svx/source/table/svdotable.cxx
@@ -693,7 +693,7 @@ void SdrTableObjImpl::LayoutTable( Rectangle& rArea, bool bFitWidth, bool bFitHe
         // Optimization: SdrTableObj::SetChanged() can call this very often, repeatedly
         // with the same settings, noticeably increasing load time. Skip if already done.
         bool bInteractiveMightGrowBecauseTextChanged =
-            mpTableObj->IsReallyEdited() && (mpTableObj->IsAutoGrowHeight() || mpTableObj->IsAutoGrowHeight());
+            mpTableObj->IsReallyEdited() && (mpTableObj->IsAutoGrowHeight() || mpTableObj->IsAutoGrowWidth());
         WritingMode writingMode = mpTableObj->GetWritingMode();
         if( bInteractiveMightGrowBecauseTextChanged
             || lastLayoutTable != this || lastLayoutInputRectangle != rArea
