3181a7a10f79fd065af88e32c402874878715684
Markus Mohrhard <markus.mohrhard@googlemail.com>
2014-11-16 17:07:15 +0100

    Invert Native Widget rendering in Y.
    Change-Id: I65a0bef7e817c82285a0e053846798f02e8d7fc8

--- vcl/opengl/x11/gdiimpl.cxx
+++ vcl/opengl/x11/gdiimpl.cxx
@@ -142,9 +142,9 @@ bool X11OpenGLSalGraphicsImpl::RenderPixmapToScreen( X11Pixmap* pPixmap, int nX,
     SAL_INFO( "vcl.opengl", "RenderPixmapToScreen (" << nX << " " << nY << ")" );
 
     aPosAry.mnSrcX = 0;
-    aPosAry.mnSrcY = nY;
+    aPosAry.mnSrcY = 0;
     aPosAry.mnDestX = nX;
-    aPosAry.mnDestY = 0;
+    aPosAry.mnDestY = nY;
     aPosAry.mnSrcWidth = aPosAry.mnDestWidth = pPixmap->GetWidth();
     aPosAry.mnSrcHeight = aPosAry.mnDestHeight = pPixmap->GetHeight();
 
