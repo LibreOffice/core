a7d798e35e084e4c65798b016a88b215a745530d
Stephan Bergmann <sbergman@redhat.com>
2014-11-17 16:33:21 +0100

    Work around warning C4702: unreachable code
    ...introduced with 82d08580e368afbc9d73da3613845a36a89b0a8c " switch saxparser
    from expat to libxml2"
    
    Change-Id: I4e214fff0f3650b44f299f112b27c607c7936c03

--- sw/qa/extras/ooxmlexport/ooxmlexport4.cxx
+++ sw/qa/extras/ooxmlexport/ooxmlexport4.cxx
@@ -582,7 +582,7 @@ DECLARE_OOXMLEXPORT_TEST(testTableCurruption, "tableCurrupt.docx")
 
 DECLARE_OOXMLEXPORT_TEST(testDateControl, "date-control.docx")
 {
-return;
+if (true) return;
     // check XML
     xmlDocPtr pXmlDoc = parseExport("word/document.xml");
     if (!pXmlDoc)
