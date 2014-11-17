080654c9a973c4e25650e02b53024b3e2c186e13
Mark Hung <marklh9@gmail.com>
2014-11-17 13:13:10 -0800

    Fix outline numbering for ooxml import filter.
    Change-Id: Ifd8a562bcee4f57cc99ed3215e6d8d6dd95216b0

--- sw/qa/extras/ww8import/ww8import.cxx
+++ sw/qa/extras/ww8import/ww8import.cxx
@@ -515,6 +515,29 @@ DECLARE_WW8IMPORT_TEST(testLayoutHanging, "fdo68967.doc")
     // This must not hang in layout
 }
 
+DECLARE_WW8IMPORT_TEST(testChtOutlineNumberingWw8, "chtoutline.doc")
+{
+    const sal_Unicode aExpectedPrefix[2] = { 0x7b2c, 0x0020 };
+    const sal_Unicode aExpectedSuffix[2] = { 0x0020, 0x7ae0 };
+    uno::Reference< text::XChapterNumberingSupplier > xChapterNumberingSupplier(mxComponent, uno::UNO_QUERY);
+    uno::Reference< container::XIndexAccess> xLevels(xChapterNumberingSupplier->getChapterNumberingRules());
+    uno::Sequence<beans::PropertyValue> aProps;
+    xLevels->getByIndex(0) >>= aProps; // 1st level
+
+    OUString aSuffix,aPrefix;
+    for (int i = 0; i < aProps.getLength(); ++i)
+    {
+        const beans::PropertyValue& rProp = aProps[i];
+
+        if (rProp.Name == "Suffix")
+            aSuffix = rProp.Value.get<OUString>();
+        if (rProp.Name == "Prefix")
+            aPrefix = rProp.Value.get<OUString>();
+    }
+    CPPUNIT_ASSERT_EQUAL(OUString(aExpectedPrefix,SAL_N_ELEMENTS(aExpectedPrefix)), aPrefix);
+    CPPUNIT_ASSERT_EQUAL(OUString(aExpectedSuffix,SAL_N_ELEMENTS(aExpectedSuffix)), aSuffix);
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
