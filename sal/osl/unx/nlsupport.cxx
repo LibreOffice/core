437cf27b6db107b2b74836ac2d897a10ebc67c0b
Stephan Bergmann <sbergman@redhat.com>
2014-11-19 17:15:19 +0100

    Blind fix for Android
    Change-Id: I091fa9f9fd4c2df1818aadcba7995b174b07f098

--- sal/osl/unx/nlsupport.cxx
+++ sal/osl/unx/nlsupport.cxx
@@ -901,7 +901,7 @@ void _imp_getProcessLocale( rtl_Locale ** ppLocale )
     /* No locale environment variables on Android, so why even bother
      * with getenv().
      */
-   char * locale = "en-US.UTF-8";
+   char const * locale = "en-US.UTF-8";
 #else
     /* simulate behavior off setlocale */
     char * locale = getenv( "LC_ALL" );
