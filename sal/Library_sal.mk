7b10a36f8dc2c27d881f5c47b404b2b22900ae5a
Stephan Bergmann <sbergman@redhat.com>
2014-11-19 12:24:40 +0100

    system.c -> system.cxx
    Change-Id: I4629a01a67dd57d5915403a89da90e98b5184503

--- sal/Library_sal.mk
+++ sal/Library_sal.mk
@@ -170,12 +170,12 @@ $(eval $(call gb_Library_add_exception_objects,sal,\
 	sal/osl/unx/readwrite_helper \
 	sal/osl/unx/security \
 	sal/osl/unx/socket \
+	sal/osl/unx/system \
 	sal/osl/unx/tempfile \
 	sal/osl/unx/thread \
         $(if $(filter DESKTOP,$(BUILD_TYPE)), sal/osl/unx/salinit) \
 ))
 $(eval $(call gb_Library_add_cobjects,sal,\
-	sal/osl/unx/system \
 	sal/osl/unx/time \
 ))
 $(eval $(call gb_Library_add_cobject,sal,sal/osl/unx/signal, \
