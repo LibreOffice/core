3b1fc03cef647f919d6312ab98582bf70294b00f
Michael Jaumann <meta_dev@yahoo.com>
2014-11-19 13:17:21 +0000

    added licence statement, vim modeline
    Change-Id: Ibc9dd608b9b49df17a13513f2c0b2ed2076b4b6c

--- canvas/source/opengl/ogl_renderHelper.cxx
+++ canvas/source/opengl/ogl_renderHelper.cxx
@@ -1,3 +1,12 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 #include <GL/glew.h>
 #include "ogl_renderHelper.hxx"
 #include <vcl/opengl/OpenGLHelper.hxx>
@@ -169,3 +178,5 @@ namespace oglcanvas
         glUseProgram(0);
     }
 }
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
