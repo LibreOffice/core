c55ac9ff1ac8da794dd534348feab7b0465e5a50
Michael Stahl <mstahl@redhat.com>
2014-11-21 17:38:06 +0100

    bridges: forgot to remove some include diagnose.h
    Change-Id: Ia4fb18a36769a3eecc20f92a6f5719b841964765
    (cherry picked from commit 42ff131336b79ffb5d6a498227bc4dbcfb7bcbee)

--- bridges/source/cpp_uno/gcc3_ios_arm/cpp2uno-arm.cxx
+++ bridges/source/cpp_uno/gcc3_ios_arm/cpp2uno-arm.cxx
@@ -34,8 +34,6 @@
 // file as close to ../gcc3_linux_arm/cpp2uno.cxx as possible, to make
 // future unification easier.
 
-#include <osl/diagnose.h>
-
 #include <com/sun/star/uno/RuntimeException.hpp>
 #include <uno/data.h>
 #include <typelib/typedescription.hxx>
