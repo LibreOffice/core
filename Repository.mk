5e9220895aa00fd4d758226527ae99013b40ea5e
David Ostrovsky <david@ostrovsky.org>
2014-11-21 15:03:13 +0100

    scp2: move ct2n to AutoInstall
    Change-Id: Ib4501298c78e591f24c2ad73cbc8aec0c4fc0347

--- Repository.mk
+++ Repository.mk
@@ -771,6 +771,12 @@ $(eval $(call gb_Helper_register_jars_for_install,OOO,extensions_rhino, \
 ))
 endif
 
+ifeq (CT2N,$(filter CT2N,$(BUILD_TYPE)))
+$(eval $(call gb_Helper_register_packages_for_install,extensions_ct2n,\
+	ct2n \
+))
+endif
+
 $(eval $(call gb_Helper_register_jars,OXT, \
 	EvolutionarySolver \
 	active_java \
