e7b68d9c8b8532d4990f46e934a07460219b6d34
Jonathan Riddell <jr@jriddell.org>
2014-11-22 23:49:52 +0100

    more breeze icons
    Change-Id: I26b1aa5594e8aeec754ddd42f8eb4a98c393a71c

--- icon-themes/breeze/update-icons.py
+++ icon-themes/breeze/update-icons.py
@@ -5,6 +5,7 @@
 
 import os
 import subprocess
+import re
 
 if os.path.isdir("plasma-next-icons"):
     os.chdir("plasma-next-icons")
@@ -17,8 +18,19 @@ mapping = {}
 
 mappingFile = open('mapping', 'r')
 for line in mappingFile:
+    if line.startswith("#"):
+        continue
+    if len(line.rsplit(" ")) != 2:
+        continue
     freeDesktopFile = "plasma-next-icons/" + line.rsplit(" ")[0]
     libreOfficeFile = line.rsplit(" ")[1].rstrip()
-    size = line.rsplit(" ")[2].rstrip()
+    size = "16"
+    if "lc_" in libreOfficeFile:
+        size = "24"
     print(freeDesktopFile + " → " + libreOfficeFile + " @ " + size+"x"+size)
-    subprocess.check_call(["ksvgtopng", size, size, freeDesktopFile, libreOfficeFile])
+    subprocess.check_call(["ksvgtopng5", size, size, freeDesktopFile, libreOfficeFile], env=os.environ)
+    if "lc_" in libreOfficeFile:
+        smallLibreOfficeFile = re.sub("lc_", "sc_", libreOfficeFile)
+        size = "16"
+        print(freeDesktopFile + " → " + smallLibreOfficeFile + " @ " + size+"x"+size)
+        subprocess.check_call(["ksvgtopng5", size, size, freeDesktopFile, smallLibreOfficeFile], env=os.environ)
