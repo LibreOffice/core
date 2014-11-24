LibreOffice/core

commit 7d9bb549d498d6beed2c4050c402d09643febdfa

Related: #i124638# Second step of DrawingLayer FillAttributes...
for Writer objects, now added support for Paragraph and PageStyle (including
Header and Footer) for direct attributes and style attributes

(cherry picked from commit cc25c58f7052827bfebdc9fbeec668c8fa29ed1b)

Conflicts:
	cui/source/factory/dlgfact.cxx
	cui/source/factory/dlgfact.hxx
	cui/source/inc/bbdlg.hxx
	cui/source/inc/cuires.hrc
	cui/source/tabpages/bbdlg.cxx
	cui/source/tabpages/bbdlg.src
	cui/source/tabpages/page.cxx
	cui/source/tabpages/tparea.cxx
	include/svx/pagectrl.hxx
	include/svx/svxdlg.hxx
	include/svx/svxids.hrc
	include/xmloff/PageMasterStyleMap.hxx
	include/xmloff/prstylei.hxx
	include/xmloff/txtprmap.hxx
	svx/Package_inc.mk
	svx/inc/svx/hdft.hxx
	svx/source/dialog/hdft.cxx
	svx/source/dialog/pagectrl.cxx
	svx/source/tbxctrls/tbxcolorupdate.cxx
	svx/source/unodraw/unobrushitemhelper.cxx
	sw/Library_sw.mk
	sw/inc/fillattributes.hxx
	sw/inc/format.hxx
	sw/inc/frmatr.hxx
	sw/inc/frmfmt.hxx
	sw/inc/hintids.hxx
	sw/inc/hints.hxx
	sw/inc/ndtxt.hxx
	sw/inc/node.hxx
	sw/inc/swunohelper.hxx
	sw/inc/unobrushitemhelper.hxx
	sw/inc/unoprnms.hxx
	sw/source/core/attr/format.cxx
	sw/source/core/attr/hints.cxx
	sw/source/core/doc/docdesc.cxx
	sw/source/core/doc/docdraw.cxx
	sw/source/core/doc/docfmt.cxx
	sw/source/core/doc/docnew.cxx
	sw/source/core/doc/docredln.cxx
	sw/source/core/doc/poolfmt.cxx
	sw/source/core/doc/visiturl.cxx
	sw/source/core/docnode/node.cxx
	sw/source/core/inc/frame.hxx
	sw/source/core/inc/frmtool.hxx
	sw/source/core/inc/rolbck.hxx
	sw/source/core/layout/atrfrm.cxx
	sw/source/core/layout/fillattributes.cxx
	sw/source/core/layout/findfrm.cxx
	sw/source/core/layout/paintfrm.cxx
	sw/source/core/txtnode/ndtxt.cxx
	sw/source/core/txtnode/thints.cxx
	sw/source/core/txtnode/txtedt.cxx
	sw/source/core/undo/rolbck.cxx
	sw/source/core/unocore/swunohelper.cxx
	sw/source/core/unocore/unoframe.cxx
	sw/source/core/unocore/unomap.cxx
	sw/source/core/unocore/unoparagraph.cxx

sw/source/ui/chrdlg/pardlg.cxx
@@ -65,6 +65,8 @@ SwParaDlg::SwParaDlg(Window *pParent,
     , m_nParaDrpCps(0)
     , m_nParaBckGrnd(0)
     , m_nParaBorder(0)
+    , m_nAreaId(0)
+    , m_nTransparenceId(0)
 {
     nHtmlMode = ::GetHtmlMode(rVw.GetDocShell());
     bool bHtmlMode = (nHtmlMode & HTMLMODE_ON) == HTMLMODE_ON;
@@ -139,13 +141,20 @@ SwParaDlg::SwParaDlg(Window *pParent,
 
         if(!bHtmlMode || (nHtmlMode & (HTMLMODE_SOME_STYLES|HTMLMODE_FULL_STYLES)))
         {
-            OSL_ENSURE(pFact->GetTabPageCreatorFunc( RID_SVXPAGE_BACKGROUND ), "GetTabPageCreatorFunc fail!");
-            OSL_ENSURE(pFact->GetTabPageRangesFunc( RID_SVXPAGE_BACKGROUND ), "GetTabPageRangesFunc fail!");
-            m_nParaBckGrnd = AddTabPage("labelTP_BACKGROUND", pFact->GetTabPageCreatorFunc( RID_SVXPAGE_BACKGROUND ), pFact->GetTabPageRangesFunc( RID_SVXPAGE_BACKGROUND ) );
+            //UUUU remove?
+            //OSL_ENSURE(pFact->GetTabPageCreatorFunc( RID_SVXPAGE_BACKGROUND ), "GetTabPageCreatorFunc fail!");
+            //OSL_ENSURE(pFact->GetTabPageRangesFunc( RID_SVXPAGE_BACKGROUND ), "GetTabPageRangesFunc fail!");
+            //m_nParaBckGrnd = AddTabPage("labelTP_BACKGROUND", pFact->GetTabPageCreatorFunc( RID_SVXPAGE_BACKGROUND ), pFact->GetTabPageRangesFunc( RID_SVXPAGE_BACKGROUND ) );
+            //
+            //UUUU add Area and Transparence TabPages
+            m_nAreaId = AddTabPage("area", pFact->GetTabPageCreatorFunc( RID_SVXPAGE_AREA ), pFact->GetTabPageRangesFunc( RID_SVXPAGE_AREA ));
+            m_nTransparenceId = AddTabPage("transparence", pFact->GetTabPageCreatorFunc( RID_SVXPAGE_TRANSPARENCE ), pFact->GetTabPageRangesFunc( RID_SVXPAGE_TRANSPARENCE ) );
         }
         else
         {
-            RemoveTabPage("labelTP_BACKGROUND");
+            //UUUU RemoveTabPage("labelTP_BACKGROUND");
+            RemoveTabPage("area");
+            RemoveTabPage("transparence");
         }
 
         OSL_ENSURE(pFact->GetTabPageCreatorFunc( RID_SVXPAGE_BORDER ), "GetTabPageCreatorFunc fail!");
@@ -214,8 +223,15 @@ void SwParaDlg::PageCreated(sal_uInt16 nId, SfxTabPage& rPage)
       if(!( nHtmlMode & HTMLMODE_ON ) ||
         nHtmlMode & HTMLMODE_SOME_STYLES)
         {
-            aSet.Put (SfxUInt32Item(SID_FLAG_TYPE, SVX_SHOW_SELECTOR));
-            rPage.PageCreated(aSet);
+            // Seitenumbruch nur, wenn der Cursor im Body-Bereich und nicht in
+            // einer Tabelle steht
+            const sal_uInt16 eType = rSh.GetFrmType(0,sal_True);
+            if(!(FRMTYPE_BODY & eType) ||
+                rSh.GetSelectionType() & nsSelectionType::SEL_TBL)
+            {
+                aSet.Put(SfxBoolItem(SID_DISABLE_SVXEXTPARAGRAPHTABPAGE_PAGEBREAK,sal_True));
+                rPage.PageCreated(aSet);
+            }
         }
     }
     else if( m_nParaNumPara == nId)
@@ -240,7 +256,29 @@ void SwParaDlg::PageCreated(sal_uInt16 nId, SfxTabPage& rPage)
         for(std::set<OUString>::const_iterator it = aNames.begin(); it != aNames.end(); ++it)
             rBox.InsertEntry(*it);
     }
+    //UUUU inits for Area and Transparency TabPages
+    // The selection attribute lists (XPropertyList derivates, e.g. XColorList for
+    // the color table) need to be added as items (e.g. SvxColorTableItem) to make
+    // these pages find the needed attributes for fill style suggestions.
+    // These are added in SwDocStyleSheet::GetItemSet() for the SFX_STYLE_FAMILY_PARA on
+    // demand, but could also be directly added from the DrawModel.
+    else if (m_nAreaId == nId)
+    {
+        SfxItemSet aNew(*aSet.GetPool(),
+            SID_COLOR_TABLE, SID_BITMAP_LIST,
+            SID_OFFER_IMPORT, SID_OFFER_IMPORT, 0, 0);
 
+        aNew.Put(*GetInputSetImpl());
+
+        // add flag for direct graphic content selection
+        aNew.Put(SfxBoolItem(SID_OFFER_IMPORT, true));
+
+        rPage.PageCreated(aNew);
+    }
+    else if (m_nTransparenceId == nId)
+    {
+        rPage.PageCreated(*GetInputSetImpl());
+    }
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

commit 5c31a1b5474f5284cf694d04de5734921891915c

fdo#86633: sw: remove bad tab pages from SwParaDlg for drawing objects
"Area" and "Transparence" only supported by Writer paragraphs.

(regression from 7d9bb549d498d6beed2c4050c402d09643febdfa)

Change-Id: I732e8d40fdeafaebf219fc7c5ddd53b850d8eca6

sfx2/source/dialog/tabdlg.cxx
@@ -551,7 +551,8 @@ void SfxTabDialog::SetApplyHandler(const Link& _rHdl)
 
 void SfxTabDialog::Start_Impl()
 {
-    DBG_ASSERT( pImpl->aData.size() == m_pTabCtrl->GetPageCount(), "not all pages registered" );
+    assert(pImpl->aData.size() == m_pTabCtrl->GetPageCount()
+            && "not all pages registered");
     sal_uInt16 nActPage = m_pTabCtrl->GetPageId( 0 );
 
     // load old settings, when exists

sw/source/ui/chrdlg/pardlg.cxx
@@ -129,6 +129,8 @@ SwParaDlg::SwParaDlg(vcl::Window *pParent,
         RemoveTabPage("labelTP_DROPCAPS");
         RemoveTabPage("labelTP_BACKGROUND");
         RemoveTabPage("labelTP_BORDER");
+        RemoveTabPage("area");
+        RemoveTabPage("transparence");
     }
     else
     {