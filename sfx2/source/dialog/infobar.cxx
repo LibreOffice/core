993deeae29c12d14dbece467b207af3e65ef3ca8
Caolán McNamara <caolanm@redhat.com>
2014-11-26 11:32:42 +0000

    fix 'Childwindow slot missing' warning
    and rename SID_INFOBARCONTAINER to SID_INFOBAR
    
    Change-Id: Ibe8a2925f62b43bd96d1092c98e500c8d433ac80

--- sfx2/source/dialog/infobar.cxx
+++ sfx2/source/dialog/infobar.cxx
@@ -315,7 +315,7 @@ void SfxInfoBarContainerWindow::Resize( )
     }
 }
 
-SFX_IMPL_POS_CHILDWINDOW_WITHID( SfxInfoBarContainerChild, SID_INFOBARCONTAINER, SFX_OBJECTBAR_OBJECT );
+SFX_IMPL_POS_CHILDWINDOW_WITHID( SfxInfoBarContainerChild, SID_INFOBAR, SFX_OBJECTBAR_OBJECT );
 
 SfxInfoBarContainerChild::SfxInfoBarContainerChild( vcl::Window* _pParent, sal_uInt16 nId, SfxBindings* pBindings, SfxChildWinInfo* ) :
     SfxChildWindow( _pParent, nId ),
