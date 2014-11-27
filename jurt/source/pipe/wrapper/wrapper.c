c4cea51aec06825f583e04d0c2755f35154ae042
Juergen Funk <juergen.funk_ml@cib.de>
2014-11-27 11:35:57 +0100

    fdo#86745 - Possible exception/segfault in jurt jpipe.dll under Windows ...
    - Remove the LoadLibrary from DLLMain (from windows not recommended)
       see http://msdn.microsoft.com/en-us/library/windows/desktop/ms682583(v=vs.85).aspx
       in section Remarks
    - Improve the comment why we need two dll's (jpipe.dll and jpipx.dll)
    - Integrate CriticalSection
    
    Change-Id: I6d5f655a4942437f6dc722236f6c371063e2c407

--- jurt/source/pipe/wrapper/wrapper.c
+++ jurt/source/pipe/wrapper/wrapper.c
@@ -26,27 +26,54 @@
 #include "jni.h"
 #include "sal/types.h"
 
-static HMODULE module;
 
-static FARPROC getFunction(char const * name) {
+static HMODULE   module   = NULL;
+static HINSTANCE hInstDLL = NULL;
+static CRITICAL_SECTION CriticalSection = {0, 0, 0, 0, INVALID_HANDLE_VALUE};
+
+void InitWrapper(void) {
+    #define MAXPATH 512
+    wchar_t path[MAXPATH];
+    DWORD size;
+
+    size = GetModuleFileNameW(hInstDLL, path, MAXPATH);
+    if (size == 0) {
+        abort();
+    }
+    path[size - 5] = L'x'; /* ...\jpipe.dll -> ...\jpipx.dll */
+    module = LoadLibraryExW(path, NULL, LOAD_WITH_ALTERED_SEARCH_PATH);
+    if (module == NULL) {
+        abort();
+    }
+}
+
+static FARPROC getFunction(char const * name)
+{
+    static BOOL first = TRUE;
+
+    if((module == NULL) && (CriticalSection.LockSemaphore == INVALID_HANDLE_VALUE))
+    {
+        if(CriticalSection.LockSemaphore == INVALID_HANDLE_VALUE)
+            InitializeCriticalSection(&CriticalSection);
+        EnterCriticalSection(&CriticalSection);
+
+        if(first)
+            InitWrapper();
+
+        first = FALSE;
+
+        LeaveCriticalSection(&CriticalSection);
+    }
+
     return GetProcAddress(module, name);
 }
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
     (void) lpvReserved;
-    if (fdwReason == DLL_PROCESS_ATTACH) {
-        wchar_t path[32767];
-        DWORD size;
-        size = GetModuleFileNameW(hinstDLL, path, 32767);
-        if (size == 0) {
-            return FALSE;
-        }
-        path[size - 5] = L'x'; /* ...\jpipe.dll -> ...\jpipx.dll */
-        module = LoadLibraryExW(path, NULL, LOAD_WITH_ALTERED_SEARCH_PATH);
-        if (module == NULL) {
-            return FALSE;
-        }
-    }
+
+    if (fdwReason == DLL_PROCESS_ATTACH)
+        hInstDLL = hinstDLL;
+
     return TRUE;
 }
 
