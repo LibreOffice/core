LibreOffice/core

commit 0c7f35277575fee8594737f5b2842dfac2c6d04f

remove SvStream::operator>> methods
in favour of ReadXXX methods.

Change-Id: Ic2c0a7b6b92ff4c236ae99b39d77f3d935b301e3
Reviewed-on: https://gerrit.libreoffice.org/7915
Tested-by: Michael Stahl <mstahl@redhat.com>
Reviewed-by: Michael Stahl <mstahl@redhat.com>

sc/source/filter/inc/xlescher.hxx
@@ -304,16 +304,15 @@ struct XclObjAnchor : public XclRange
 };
 
 
-template< typename StreamType >
-StreamType& operator>>( StreamType& rStrm, XclObjAnchor& rAnchor )
+inline SvStream& operator>>( SvStream& rStrm, XclObjAnchor& rAnchor )
 {
     sal_uInt16 tmpFirstRow, tmpTY, tmpLastRow, tmpBY;
 
     rStrm
-        >> rAnchor.maFirst.mnCol >> rAnchor.mnLX
-        >> tmpFirstRow >> tmpTY
-        >> rAnchor.maLast.mnCol  >> rAnchor.mnRX
-        >> tmpLastRow  >> tmpBY;
+       .ReadUInt16( rAnchor.maFirst.mnCol ).ReadUInt16( rAnchor.mnLX )
+       .ReadUInt16( tmpFirstRow ).ReadUInt16( tmpTY )
+       .ReadUInt16( rAnchor.maLast.mnCol ).ReadUInt16( rAnchor.mnRX )
+       .ReadUInt16( tmpLastRow ).ReadUInt16( tmpBY );
 
     rAnchor.maFirst.mnRow = static_cast<sal_uInt32> (tmpFirstRow);
     rAnchor.mnTY = static_cast<sal_uInt32> (tmpTY);

cppcanvas/source/inc/implrenderer.hxx
@@ -164,8 +164,8 @@ static float GetSwapFloat( SvStream& rSt )
                     rXForm.eDx = GetSwapFloat( rIn );
                     rXForm.eDy = GetSwapFloat( rIn );
 #else
-                    rIn >> rXForm.eM11 >> rXForm.eM12 >> rXForm.eM21 >> rXForm.eM22
-                        >> rXForm.eDx >> rXForm.eDy;
+                    rIn.ReadFloat( rXForm.eM11 ).ReadFloat( rXForm.eM12 ).ReadFloat( rXForm.eM21 ).ReadFloat( rXForm.eM22 )
+                       .ReadFloat( rXForm.eDx ).ReadFloat( rXForm.eDy );
 #endif
                 }
                 return rIn;

idl/inc/bastype.hxx
@@ -65,7 +65,7 @@ public:
     friend SvStream& WriteSvint(SvStream & rStm, const Svint & r )
                 { SvUINT32::Write( rStm, (sal_uInt32)r.nVal ); rStm.WriteUInt8( r.bSet ); return rStm; }
     friend SvStream& operator >> (SvStream & rStm, Svint & r )
-                { r.nVal = (int)SvUINT32::Read( rStm ); rStm >> r.bSet ; return rStm; }
+                { r.nVal = (int)SvUINT32::Read( rStm ); rStm.ReadUChar( r.bSet ); return rStm; }
 };
 
 

include/filter/msfilter/mstoolbar.hxx
@@ -310,7 +310,7 @@ public:
     sal_Int16 top;
     sal_Int16 right;
     sal_Int16 bottom;
-    bool Read( SvStream &rS ) { rS >> left >> top >> right >> bottom; return true; }
+    bool Read( SvStream &rS ) { rS.ReadInt16( left ).ReadInt16( top ).ReadInt16( right ).ReadInt16( bottom ); return true; }
     void Print( FILE* fo );
 };
 

include/tools/stream.hxx
@@ -300,30 +300,6 @@ public:
                         { eLineDelimiter = eLineEnd; }
     LineEnd         GetLineDelimiter() const { return eLineDelimiter; }
 
-    SvStream&       operator>>( sal_uInt16& rUInt16 )
-                        { return ReadUInt16(rUInt16); }
-    SvStream&       operator>>( sal_uInt32& rUInt32 )
-                        { return ReadUInt32(rUInt32); }
-    SvStream&       operator>>( sal_uInt64& rUInt64 )
-                        { return ReadUInt64(rUInt64); }
-    SvStream&       operator>>( sal_Int16& rInt16 )
-                        { return ReadInt16(rInt16); }
-    SvStream&       operator>>( sal_Int32& rInt32 )
-                        { return ReadInt32(rInt32); }
-    SvStream&       operator>>( sal_Int64& rInt64 ) SAL_DELETED_FUNCTION;
-    SvStream&       operator>>( signed char& rChar )
-                        { return ReadSChar(rChar); }
-    SvStream&       operator>>( char& rChar )
-                        { return ReadChar(rChar); }
-    SvStream&       operator>>( unsigned char& rChar )
-                        { return ReadUChar(rChar); }
-    SvStream&       operator>>( float& rFloat )
-                        { return ReadFloat(rFloat); }
-    SvStream&       operator>>( double& rDouble )
-                        { return ReadDouble(rDouble); }
-    SvStream&       operator>>( SvStream& rStream )
-                        { return ReadStream(rStream); }
-
     SvStream&       ReadUInt16( sal_uInt16& rUInt16 );
     SvStream&       ReadUInt32( sal_uInt32& rUInt32 );
     SvStream&       ReadUInt64( sal_uInt64& rUInt64 );

sc/inc/bigrange.hxx
@@ -140,7 +140,7 @@ inline SvStream& WriteScBigAddress( SvStream& rStream, const ScBigAddress& rAdr 
 
 inline SvStream& ReadScBigAddress( SvStream& rStream, ScBigAddress& rAdr )
 {
-    rStream >> rAdr.nCol >> rAdr.nRow >> rAdr.nTab;
+    rStream.ReadInt32( rAdr.nCol ).ReadInt32( rAdr.nRow ).ReadInt32( rAdr.nTab );
     return rStream;
 }
 

sc/source/filter/inc/formel.hxx
@@ -146,37 +146,37 @@ inline void LotusConverterBase::Ignore( const long nSeekRel )
 
 inline void LotusConverterBase::Read( sal_Char& nByte )
 {
-    aIn >> nByte;
+    aIn.ReadChar( nByte );
     nBytesLeft--;
 }
 
 inline void LotusConverterBase::Read( sal_uInt8& nByte )
 {
-    aIn >> nByte;
+    aIn.ReadUChar( nByte );
     nBytesLeft--;
 }
 
 inline void LotusConverterBase::Read( sal_uInt16& nUINT16 )
 {
-    aIn >> nUINT16;
+    aIn.ReadUInt16( nUINT16 );
     nBytesLeft -= 2;
 }
 
 inline void LotusConverterBase::Read( sal_Int16& nINT16 )
 {
-    aIn >> nINT16;
+    aIn.ReadInt16( nINT16 );
     nBytesLeft -= 2;
 }
 
 inline void LotusConverterBase::Read( double& fDouble )
 {
-    aIn >> fDouble;
+    aIn.ReadDouble( fDouble );
     nBytesLeft -= 8;
 }
 
 inline void LotusConverterBase::Read( sal_uInt32& nUINT32 )
 {
-    aIn >> nUINT32;
+    aIn.ReadUInt32( nUINT32 );
     nBytesLeft -= 4;
 }
 

sc/source/filter/inc/lotimpop.hxx
@@ -90,12 +90,12 @@ public:
 inline void ImportLotus::Read( ScAddress& rAddr )
 {
     sal_uInt16 nRow;
-    *pIn >> nRow;
+    pIn->ReadUInt16( nRow );
     rAddr.SetRow( static_cast<SCROW>(nRow) );
     sal_uInt8 nByte;
-    *pIn >> nByte;
+    pIn->ReadUChar( nByte );
     rAddr.SetTab( static_cast<SCTAB>(nByte) );
-    *pIn >> nByte;
+    pIn->ReadUChar( nByte );
     rAddr.SetCol( static_cast<SCCOL>(nByte) );
 }
 
@@ -109,31 +109,31 @@ inline void ImportLotus::Read( ScRange& rRange )
 
 inline void ImportLotus::Read( sal_Char& r )
 {
-    *pIn >> r;
+    pIn->ReadChar( r );
 }
 
 
 inline void ImportLotus::Read( sal_uInt8& r )
 {
-    *pIn >> r;
+    pIn->ReadUChar( r );
 }
 
 
 inline void ImportLotus::Read( sal_uInt16& r )
 {
-    *pIn >> r;
+    pIn->ReadUInt16( r );
 }
 
 
 inline void ImportLotus::Read( sal_Int16& r )
 {
-    *pIn >> r;
+    pIn->ReadInt16( r );
 }
 
 
 inline void ImportLotus::Read( sal_uInt32& r )
 {
-    *pIn >> r;
+    pIn->ReadUInt32( r );
 }
 
 
@@ -145,7 +145,7 @@ inline void ImportLotus::Read( double& r )
 
 inline void ImportLotus::Read( LotAttrWK3& r )
 {
-    *pIn >> r.nFont >> r.nFontCol >> r.nBack >> r.nLineStyle;
+    pIn->ReadUChar( r.nFont ).ReadUChar( r.nFontCol ).ReadUChar( r.nBack ).ReadUChar( r.nLineStyle );
 }
 
 

commit 2850bb46b02466622d6615db76a40e2382d06cfc

fdo#82312 - Excel chart has a wrong size
bug introduced in commit 0c7f35277575fee8594737f5b2842dfac2c6d04f
"remove SvStream::operator>> methods"

Change-Id: I72a1c05b3a4a3f3a250055775eb2d35b3114406d

sc/source/filter/inc/xlescher.hxx
@@ -321,6 +321,26 @@ inline SvStream& operator>>( SvStream& rStrm, XclObjAnchor& rAnchor )
     return rStrm;
 }
 
+// creates read method for XclImpStream
+template< typename StreamType >
+StreamType& operator>>( StreamType& rStrm, XclObjAnchor& rAnchor )
+{
+    sal_uInt16 tmpFirstRow, tmpTY, tmpLastRow, tmpBY;
+
+    rStrm
+        >> rAnchor.maFirst.mnCol >> rAnchor.mnLX
+        >> tmpFirstRow >> tmpTY
+        >> rAnchor.maLast.mnCol  >> rAnchor.mnRX
+        >> tmpLastRow  >> tmpBY;
+
+    rAnchor.maFirst.mnRow = static_cast<sal_uInt32> (tmpFirstRow);
+    rAnchor.mnTY = static_cast<sal_uInt32> (tmpTY);
+    rAnchor.maLast.mnRow = static_cast<sal_uInt32> (tmpLastRow);
+    rAnchor.mnBY = static_cast<sal_uInt32> (tmpBY);
+
+    return rStrm;
+}
+
 template< typename StreamType >
 StreamType& operator<<( StreamType& rStrm, const XclObjAnchor& rAnchor )
 {