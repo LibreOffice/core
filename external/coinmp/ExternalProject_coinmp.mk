b98d3d34144eb1754b50e58736a8b636ef7813c6
David Ostrovsky <david@ostrovsky.org>
2014-11-30 18:26:01 +0100

    coinmp: Don't hard code Win32 platform on Windows
    Change-Id: I39eafa22b12e62c766a182c2ebc2b115084f4cef

--- external/coinmp/ExternalProject_coinmp.mk
+++ external/coinmp/ExternalProject_coinmp.mk
@@ -18,7 +18,7 @@ $(call gb_ExternalProject_get_state_target,coinmp,build) :
 	$(call gb_ExternalProject_run,build,\
 		MSBuild.exe CoinMP.sln /t:Build \
 			/p:Configuration=$(if $(MSVC_USE_DEBUG_RUNTIME),Debug,Release) \
-			/p:Platform=Win32 \
+			/p:Platform=$(if $(filter INTEL,$(CPUNAME)),Win32,x64) \
 			$(if $(filter 110,$(VCVER)),/p:PlatformToolset=$(if $(filter 80,$(WINDOWS_SDK_VERSION)),v110,v110_xp) /p:VisualStudioVersion=11.0) \
 			$(if $(filter 120,$(VCVER)),/p:PlatformToolset=v120 /p:VisualStudioVersion=12.0 /ToolsVersion:12.0) \
 	,CoinMP/MSVisualStudio/v9)
