219c42d1236e3ec6c6e01fb1a933343297a3e725
Kohei Yoshida <kohei.yoshida@collabora.com>
2014-12-02 16:35:06 -0500

    Let's not forget to move the comment too.
    Change-Id: I86f2107860cf7e014277abdc14307c444142fea6
    (cherry picked from commit 93b809d4c4151a02404308a20ecb20f1d6378e0a)

--- sc/source/core/data/document.cxx
+++ sc/source/core/data/document.cxx
@@ -2846,12 +2846,14 @@ void ScDocument::CopyFromClip( const ScRange& rDestRange, const ScMarkData& rMar
     // Listener aufbauen nachdem alles inserted wurde
     StartListeningFromClip( nAllCol1, nAllRow1, nAllCol2, nAllRow2, rMark, nInsFlag );
 
-    // Set all formula cells dirty, and collect non-empty non-formula cell
-    // positions so that we can broadcast on them below.
 
     {
         ScBulkBroadcast aBulkBroadcast( GetBASM());
+
+        // Set all formula cells dirty, and collect non-empty non-formula cell
+        // positions so that we can broadcast on them below.
         SetDirtyFromClip(nAllCol1, nAllRow1, nAllCol2, nAllRow2, rMark, nInsFlag, aBroadcastSpans);
+
         BroadcastAction aAction(*this);
         aBroadcastSpans.executeColumnAction(*this, aAction);
     }
