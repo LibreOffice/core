8091457a237bad0fd95a8b4d2c93650ce7847169
Markus Mohrhard <markus.mohrhard@collabora.co.uk>
2014-12-02 21:51:50 +0100

    Revert "use boost::shared_ptr instead of manual ref counting"
    This reverts commit 8eeb02dcc1a4bc99b083b1a591b4a70003a1604f.
    
    Conflicts:
    	include/vcl/opengl/OpenGLContext.hxx
    	vcl/inc/openglgdiimpl.hxx
    	vcl/opengl/gdiimpl.cxx
    
    Change-Id: I85cc7a46876ffba5ab861f6dd83b07da466b212b

--- vcl/source/opengl/OpenGLContext.cxx
+++ vcl/source/opengl/OpenGLContext.cxx
@@ -53,6 +53,7 @@ OpenGLContext::OpenGLContext():
     mpWindow(NULL),
     m_pChildWindow(NULL),
     mbInitialized(false),
+    mnRefCount(1),
     mbRequestLegacyContext(false),
     mbUseDoubleBufferedRendering(true),
     mbRequestVirtualDevice(false),
@@ -119,6 +120,17 @@ OpenGLContext::~OpenGLContext()
             glXDestroyPixmap(m_aGLWin.dpy, m_aGLWin.glPix);
     }
 #endif
+}
+
+void OpenGLContext::AddRef()
+{
+    mnRefCount++;
+}
+
+void OpenGLContext::DeRef()
+{
+    if( --mnRefCount == 0 )
+        delete this;
 }
 
 void OpenGLContext::requestLegacyContext()
