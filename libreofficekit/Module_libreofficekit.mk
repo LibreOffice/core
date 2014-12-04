e5cd9ee78b36a0f23e2b85b01ceb8899fe11c0fc
Tomaž Vajngerl <tomaz.vajngerl@collabora.com>
2014-12-04 13:11:41 +0100

    Revert "Implement lokconf_init."
    This reverts commit 5acadbfb0c79fe51f8025b065b46b0ccd25c804d.
    
    Conflicts:
    	libreofficekit/qa/lokconf_init/lokconf_init.cxx
    
    Change-Id: I88221f660bcf15cbc29e90bfc3016b367570f9af

--- libreofficekit/Module_libreofficekit.mk
+++ libreofficekit/Module_libreofficekit.mk
@@ -11,10 +11,6 @@ $(eval $(call gb_Module_Module,libreofficekit))
 
 ifeq ($(OS),LINUX)
 
-$(eval $(call gb_Module_add_targets,libreofficekit,\
-    Executable_lokconf_init \
-))
-
 # $(eval $(call gb_Module_add_check_targets,libreofficekit,\
 #     CppunitTest_libreofficekit_tiledrendering \
 # ))
