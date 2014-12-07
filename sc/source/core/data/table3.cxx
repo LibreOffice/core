bd004cdb1e407480b76b2bc1909ef45763cf20ba
Eike Rathke <erack@redhat.com>
2014-12-07 17:32:09 +0100

    add ReorderParam.mbHasHeaders to include in Undo range mark
    Undo Sort did not select/mark the original range if it included header
    row/column.
    
    (cherry picked from commit ec1b1af8af7ead149f96c09e16402e66e0973372)
    
    Backported with merge conflicts.
    
    umm.. row is row and col is col
    
    (cherry picked from commit 413a0655adec3e9364bc94afff638a5a9f88560c)
    
    26c62fa1fc97aa0dd4eda1102422e5f012a126eb
    
    Change-Id: Ie4c800e7a44fd910c871731673093400c6400e4a

--- sc/source/core/data/table3.cxx
+++ sc/source/core/data/table3.cxx
@@ -1416,6 +1416,7 @@ void ScTable::Sort(
         pUndo->mbPattern = rSortParam.bIncludePattern;
         pUndo->mbHiddenFiltered = bKeepQuery;
         pUndo->mbUpdateRefs = bUpdateRefs;
+        pUndo->mbHasHeaders = rSortParam.bHasHeader;
     }
 
     if (rSortParam.bByRow)
