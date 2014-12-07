764836cb00e8e6dfd2ab48e080a166ec90359e01
David Tardon <dtardon@redhat.com>
2014-12-07 21:48:55 +0100

    Revert "No need to have OpenCL optional at configure-time"
    As long as this requires bundling clcc, not allowing to disable it is
    unacceptable.
    
    This reverts commit 3b0b76462aa54f60bc9823d9c6ef64e5e35c1208.
    
    Conflicts:
    	configure.ac
    
    Change-Id: I12b3d5e3325bbd03cd98e1fb5139705d694493c2

--- sc/CppunitTest_sc_opencl_test.mk
+++ sc/CppunitTest_sc_opencl_test.mk
@@ -15,7 +15,7 @@ $(eval $(call gb_CppunitTest_add_exception_objects,sc_opencl_test, \
 
 $(eval $(call gb_CppunitTest_use_externals,sc_opencl_test, \
 	boost_headers \
-	clew \
+	$(call gb_Helper_optional,OPENCL,clew) \
 	mdds_headers \
 	orcus \
 	orcus-parser \
