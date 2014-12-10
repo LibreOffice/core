7a12360e1f2c0a5bc0927131a750740d35cfd1ab
Michael Stahl <mstahl@redhat.com>
2014-12-10 14:56:27 +0100

    sw: don't assert when SwXBookmark creates duplicate CrossRef*Bookmark
    The bugdoc of fdo#87110 has a couple such duplicates.  Check for it and
    let SwXBookmark throw an IllegalArgumentException.
    
    Change-Id: I460dc3a8d9c554c194a32a73526e13422ddd1c52

--- sw/source/core/doc/docbm.cxx
+++ sw/source/core/doc/docbm.cxx
@@ -374,11 +374,13 @@ namespace sw { namespace mark
             "MarkManager::makeMark(..)"
             " - more than USHRT_MAX marks are not supported correctly");
         // There should only be one CrossRefBookmark per Textnode per Type
-        assert((
-            (eType != CROSSREF_NUMITEM_BOOKMARK && eType != CROSSREF_HEADING_BOOKMARK)
-            || (lcl_FindMarkAtPos(m_vBookmarks, *rPaM.GetPoint(), eType) == m_vBookmarks.end())) &&
-            "MarkManager::makeMark(..)"
-            " - creating duplicate CrossRefBookmark");
+        if ((eType == CROSSREF_NUMITEM_BOOKMARK || eType == CROSSREF_HEADING_BOOKMARK)
+            && (lcl_FindMarkAtPos(m_vBookmarks, *rPaM.GetPoint(), eType) != m_vBookmarks.end()))
+        {   // this can happen via UNO API
+            SAL_WARN("sw.core", "MarkManager::makeMark(..)"
+                " - refusing to create duplicate CrossRefBookmark");
+            return 0;
+        }
 
         // create mark
         pMark_t pMark;
