27a6f49febe32145835ad5ebac92ff68820e45cf
Winfried Donkers <winfrieddonkers@libreoffice.org>
2014-12-15 09:45:26 +0100

    fdo#76870 Add support for Excel2013 function ENCODEURL
    Change-Id: I369bcd58055364757b6a098fc3aa0c0065c79b67

--- sc/inc/helpids.h
+++ sc/inc/helpids.h
@@ -563,6 +563,7 @@
 #define HID_FUNC_BITRSHIFT                                      "SC_HID_FUNC_BITRSHIFT"
 #define HID_FUNC_FILTERXML                                      "SC_HID_FUNC_FILTERXML"
 #define HID_FUNC_WEBSERVICE                                     "SC_HID_FUNC_WEBSERVICE"
+#define HID_FUNC_ENCODEURL                                      "SC_HID_FUNC_ENCODEURL"
 #define HID_FUNC_COLOR                                          "SC_HID_FUNC_COLOR"
 #define HID_FUNC_COVARIANCE_P                                   "SC_HID_FUNC_COVARIANCE_P"
 #define HID_FUNC_COVARIANCE_S                                   "SC_HID_FUNC_COVARIANCE_S"
