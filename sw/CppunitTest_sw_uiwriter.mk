4423637d4e9c949b22109f7db029c9afaccc7d7e
Miklos Vajna <vmiklos@collabora.co.uk>
2014-12-18 18:40:12 +0100

    CppunitTest_sw_uiwriter: use gb_CppunitTest_use_rdb
    I can't easily see what component is missing for SVM saving, and we do
    the same in CppunitTest_sw_htmlimport already, so why bother.
    
    Change-Id: Iba917e211fc721f8e275b3ce04a0e02c0759485b

--- sw/CppunitTest_sw_uiwriter.mk
+++ sw/CppunitTest_sw_uiwriter.mk
@@ -55,40 +55,7 @@ $(eval $(call gb_CppunitTest_use_api,sw_uiwriter,\
 $(eval $(call gb_CppunitTest_use_ure,sw_uiwriter))
 $(eval $(call gb_CppunitTest_use_vcl,sw_uiwriter))
 
-$(eval $(call gb_CppunitTest_use_components,sw_uiwriter,\
-    basic/util/sb \
-    comphelper/util/comphelp \
-    configmgr/source/configmgr \
-    embeddedobj/util/embobj \
-    filter/source/config/cache/filterconfig1 \
-    filter/source/storagefilterdetect/storagefd \
-    framework/util/fwk \
-    i18npool/util/i18npool \
-    linguistic/source/lng \
-    package/util/package2 \
-    package/source/xstor/xstor \
-    sw/util/msword \
-    sw/util/sw \
-    sw/util/swd \
-    sax/source/expatwrap/expwrap \
-    sfx2/util/sfx \
-    svl/source/fsstor/fsstorage \
-    svtools/util/svt \
-    toolkit/util/tk \
-    ucb/source/core/ucb1 \
-    ucb/source/ucp/file/ucpfile1 \
-    unotools/util/utl \
-    unoxml/source/service/unoxml \
-    uui/util/uui \
-    $(if $(filter-out MACOSX WNT,$(OS)), \
-        $(if $(ENABLE_HEADLESS),, \
-            vcl/vcl.unx \
-        ) \
-    ) \
-    $(if $(filter DESKTOP,$(BUILD_TYPE)),xmlhelp/util/ucpchelp1) \
-	writerfilter/util/writerfilter \
-    xmloff/util/xo \
-))
+$(eval $(call gb_CppunitTest_use_rdb,sw_uiwriter,services))
 
 $(eval $(call gb_CppunitTest_use_configuration,sw_uiwriter))
 
