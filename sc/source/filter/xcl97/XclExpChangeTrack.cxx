995a06e212c1a5ca6b5f29ad01d445a1c0d460f1
Markus Mohrhard <markus.mohrhard@collabora.co.uk>
2014-12-22 05:00:24 +0100

    fix OOXML validation errors
    Change-Id: Ieac082a3b312602eff5ec101529536d747fa3836
    Reviewed-on: https://gerrit.libreoffice.org/13586
    Reviewed-by: Eike Rathke <erack@redhat.com>
    Tested-by: Eike Rathke <erack@redhat.com>

--- sc/source/filter/xcl97/XclExpChangeTrack.cxx
+++ sc/source/filter/xcl97/XclExpChangeTrack.cxx
@@ -1079,6 +1079,12 @@ void XclExpChTrCellContent::SaveXml( XclExpXmlStream& rRevisionLogStrm )
     if( pOldData )
     {
         lcl_WriteCell( rRevisionLogStrm, XML_oc, aPosition, pOldData );
+        if (!pNewData)
+        {
+            pStream->singleElement(XML_nc,
+                    XML_r,  XclXmlUtils::ToOString( aPosition ).getStr(),
+                    FSEND);
+        }
     }
     if( pNewData )
     {
