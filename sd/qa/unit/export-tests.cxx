416596954529dd7897ab81951c47ba5984b11a34
Zolnai Tamás <tamas.zolnai@collabora.com>
2014-12-23 10:45:19 +0100

    bnc#822341: PPTX export of embedded text documents
    -Make embedded text documents exported by analogy with spreadsheet
    embedded documents.
    -Convert MS ole objects to LO sepcific objecst to allow
    exporting those ole objects too.
    
    Plus use gb_CppunitTest_use_rdb instead of wasting time on
    finding out which missing components cause test failing.
    Especcially when we have platform dependent components
    (e.g. related to embedded object)
    
    (cherry picked from commit 0c3d5fb0ad35ff7fc18917fc86fa58d9312fe3ae)
    
    Conflicts:
    	sd/qa/unit/export-tests.cxx
    
    
    ERROR: Attribute 'spid' must appear on element 'p:oleObj'.
    
    As pointed out by CppunitTest_sd_export_tests.
    
    (cherry picked from commit 124c33dd1d6bf2040f0153de8c9ecaebe759c3bd)
    
    ERROR: Invalid content was found starting with element 'p:pic'.
    
    Again, as pointed out by CppunitTest_sd_export_tests. Sadly it seems that the
    spec and PowerPoint is not in sync here, so just do what makes both happy. This
    fixes the --with-export-validation build.
    
    (cherry picked from commit cf15306ccf49da290b391517e2c5dd22a4f1be45)
    
    Change-Id: Ic76d659b51274777a1669c215344a1169ebcba81
    8ecbc3ef4ea9dc84ebf88506967d02de04ded852
    c76d659b51274777a1669c215344a1169ebcba81
    Reviewed-on: https://gerrit.libreoffice.org/13600
    Reviewed-by: Andras Timar <andras.timar@collabora.com>
    Tested-by: Andras Timar <andras.timar@collabora.com>

--- sd/qa/unit/export-tests.cxx
+++ sd/qa/unit/export-tests.cxx
@@ -80,6 +80,9 @@ public:
     void testFdo79731();
     void testSwappedOutImageExport();
     void testLinkedGraphicRT();
+#if !defined WNT
+    void testBnc822341();
+#endif
 
     CPPUNIT_TEST_SUITE(SdExportTest);
     CPPUNIT_TEST(testN821567);
@@ -98,6 +101,9 @@ public:
     CPPUNIT_TEST(testFdo79731);
     CPPUNIT_TEST(testSwappedOutImageExport);
     CPPUNIT_TEST(testLinkedGraphicRT);
+#if !defined WNT
+    CPPUNIT_TEST(testBnc822341);
+#endif
     CPPUNIT_TEST_SUITE_END();
 };
 
@@ -637,6 +643,45 @@ void SdExportTest::testLinkedGraphicRT()
         xDocShRef->DoClose();
     }
 }
+
+#if !defined WNT
+
+void SdExportTest::testBnc822341()
+{
+    // Check import / export of embedded text document
+    ::sd::DrawDocShellRef xDocShRef = loadURL(getURLFromSrc("sd/qa/unit/data/odp/bnc822341.odp"), ODP);
+    xDocShRef = saveAndReload( xDocShRef, PPTX );
+
+    // Export an LO specific ole object (imported from an ODP document)
+    {
+        SdDrawDocument *pDoc = xDocShRef->GetDoc();
+        CPPUNIT_ASSERT_MESSAGE( "no document", pDoc != NULL );
+        const SdrPage *pPage = pDoc->GetPage(1);
+        CPPUNIT_ASSERT_MESSAGE( "no page", pPage != NULL );
+
+        const SdrObject* pObj = dynamic_cast<SdrObject*>( pPage->GetObj(0) );
+        CPPUNIT_ASSERT_MESSAGE( "no object", pObj != NULL);
+        CPPUNIT_ASSERT_EQUAL( static_cast<sal_uInt16>(OBJ_OLE2), pObj->GetObjIdentifier() );
+    }
+
+    xDocShRef = saveAndReload( xDocShRef, PPTX );
+
+    // Export an MS specific ole object (imported from a PPTX document)
+    {
+        SdDrawDocument *pDoc = xDocShRef->GetDoc();
+        CPPUNIT_ASSERT_MESSAGE( "no document", pDoc != NULL );
+        const SdrPage *pPage = pDoc->GetPage(1);
+        CPPUNIT_ASSERT_MESSAGE( "no page", pPage != NULL );
+
+        const SdrObject* pObj = dynamic_cast<SdrObject*>( pPage->GetObj(0) );
+        CPPUNIT_ASSERT_MESSAGE( "no object", pObj != NULL);
+        CPPUNIT_ASSERT_EQUAL( static_cast<sal_uInt16>(OBJ_OLE2), pObj->GetObjIdentifier() );
+    }
+
+    xDocShRef->DoClose();
+}
+
+#endif
 
 CPPUNIT_TEST_SUITE_REGISTRATION(SdExportTest);
 
