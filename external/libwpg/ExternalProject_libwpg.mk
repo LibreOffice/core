30fc6e27f75de855f64e2eb789e6df2dfc1d7996
David Tardon <dtardon@redhat.com>
2014-12-23 18:37:54 +0100

    build bundled libwpd and libwpg as shared libs
    Change-Id: I7a00dfaa3252e2a0ab145ef8a4355f9bd14570da

--- external/libwpg/ExternalProject_libwpg.mk
+++ external/libwpg/ExternalProject_libwpg.mk
@@ -25,8 +25,8 @@ $(call gb_ExternalProject_get_state_target,libwpg,build) :
 		export PKG_CONFIG="" \
 		&& MAKE=$(MAKE) ./configure \
 			--with-pic \
-			--enable-static \
-			--disable-shared \
+			--enable-shared \
+			--disable-static \
 			--without-docs \
 			--disable-tools \
 			--disable-debug \
