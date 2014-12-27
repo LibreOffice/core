54c4ae08fc07f325c3bacfd50973dc8225f407b6
Markus Mohrhard <markus.mohrhard@collabora.co.uk>
2014-12-27 06:42:16 +0100

    NO_DECODE breaks non-ascii urls, fdo#85617
    Change-Id: Ic76346d20dc7fdbb5d77ba08d87f0d6c6e7b3c88

--- sc/source/core/data/global2.cxx
+++ sc/source/core/data/global2.cxx
@@ -327,7 +327,7 @@ OUString ScGlobal::GetAbsDocName( const OUString& rFileName,
         if ( pMedium )
         {
             bool bWasAbs = true;
-            aAbsName = pMedium->GetURLObject().smartRel2Abs( rFileName, bWasAbs ).GetMainURL(INetURLObject::NO_DECODE);
+            aAbsName = pMedium->GetURLObject().smartRel2Abs( rFileName, bWasAbs ).GetMainURL(INetURLObject::DECODE_TO_IURI);
         }
         else
         {   // This can't happen, but ...
