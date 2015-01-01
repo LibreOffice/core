9fbc8c06a3cb0df6d1208b8916e0e9f31ca8d025
David Tardon <dtardon@redhat.com>
2015-01-01 10:31:24 +0100

    upload libodfgen 0.1.3
    Change-Id: Ia28bac4b12f23f3e85e42e323b35d8715a6d5d02

--- external/libodfgen/UnpackedTarball_libodfgen.mk
+++ external/libodfgen/UnpackedTarball_libodfgen.mk
@@ -13,8 +13,4 @@ $(eval $(call gb_UnpackedTarball_set_tarball,libodfgen,$(ODFGEN_TARBALL)))
 
 $(eval $(call gb_UnpackedTarball_set_patchlevel,libodfgen,0))
 
-$(eval $(call gb_UnpackedTarball_add_patches,libodfgen, \
-    external/libodfgen/metadata.patch \
-))
-
 # vim: set noet sw=4 ts=4:
