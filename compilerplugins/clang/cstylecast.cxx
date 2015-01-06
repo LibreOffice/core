1887d4f5e98960183ebac61d4ac931915e6f8d8d
Stephan Bergmann <sbergman@redhat.com>
2015-01-06 21:43:56 +0100

    Minor beautification
    Change-Id: I1300d1d696878c1b646decc2841f13d489d3cadc

--- compilerplugins/clang/cstylecast.cxx
+++ compilerplugins/clang/cstylecast.cxx
@@ -98,11 +98,11 @@ bool CStyleCast::VisitCStyleCastExpr(const CStyleCastExpr * expr) {
     }
     if ( compat::isInMainFile(compiler.getSourceManager(), spellingLocation) ) {
         if (filename.startswith(SRCDIR "/sal/") // sal has tons of weird stuff going on that I don't understand enough to fix
-           || filename.startswith(SRCDIR "/bridges")) { // I'm not messing with this code - far too dangerous
+           || filename.startswith(SRCDIR "/bridges/")) { // I'm not messing with this code - far too dangerous
             return true;
         }
     } else {
-        if (filename.startswith(SRCDIR "/include/tools/solar.h")
+        if (filename == SRCDIR "/include/tools/solar.h"
            || filename.startswith(SRCDIR "/include/cppuhelper/")
            || ((StringRef(
                     compiler.getSourceManager().getFileEntryForID(
