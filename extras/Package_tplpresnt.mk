eb6079fcb257cbfbff54a63a0a27ed9d1fbacba9
Jan Holesovsky <kendy@collabora.com>
2015-01-07 22:19:09 +0100

    Include the new templates in the installation.
    All the templates are result of the Templates contest for 4.4, and were
    contributed under CC0.
    
    The templates were contributed by the following people:
    
    * Alexander Wilms <f.alexander.wilms@gmail.com>
    * Edmund Laugasson <edmund.laugasson@gmail.com>
    * Jun NOGATA <nogajun@gmail.com>
    * Michael Kovarik <fractalsandflowers@gmail.com>
    * Péter Szathmáry <mi84aa@gmail.com>
    * Zirk <showeet@ymail.com>
    
    Thank you (and all the other participants) for your contributions!
    
    Change-Id: I9fa578fafa5e350a186a77b3e023474b289e0fb8

---
+++ extras/Package_tplpresnt.mk
@@ -0,0 +1,19 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_Package_Package,extras_tplpresnt,$(SRCDIR)/extras/source/templates/presnt))
+
+$(eval $(call gb_Package_add_files,extras_tplpresnt,$(LIBO_SHARE_FOLDER)/template/common/presnt,\
+	Alizarin.otp \
+	Focus.otp \
+	Impress.otp \
+	Midnightblue.otp \
+))
+
+# vim: set noet sw=4 ts=4:
