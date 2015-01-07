091073ccff4a559fe0959267b8c57613068bf582
Lionel Elie Mamane <lionel@mamane.lu>
2015-01-07 16:43:38 +0100

    fix compilation
    Change-Id: If55862447acc364203079ad98a0bffd0da2dff79

--- sc/source/ui/view/viewfun5.cxx
+++ sc/source/ui/view/viewfun5.cxx
@@ -294,8 +294,8 @@ bool ScViewFunc::PasteDataFormat( sal_uLong nFormatId,
         }
         else
         {
-            ScAddress aCellPos( nPosX, nPosY, GetViewData().GetTabNo() );
-            ScImportExport aObj( GetViewData().GetDocument(), aCellPos );
+            ScAddress aCellPos( nPosX, nPosY, GetViewData()->GetTabNo() );
+            ScImportExport aObj( GetViewData()->GetDocument(), aCellPos );
             aObj.SetOverwriting( true );
 
             OUString aStr;
