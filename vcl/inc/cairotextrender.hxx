49a6d0151d85ddc64857c66c97c577ebd438a392
Chris Sherlock <chris.sherlock79@gmail.com>
2015-01-09 18:35:38 +1100

    vcl: cleanup cairotextrender.hxx
    Change-Id: Ia8471c9745b86c4137297fd9cd0a3fea712a14fa

--- vcl/inc/cairotextrender.hxx
+++ vcl/inc/cairotextrender.hxx
@@ -55,74 +55,80 @@ public:
                 mbVerticalMetrics == rOther.mbVerticalMetrics;
         }
     };
+
 private:
-    static int mnRefCount;
-    typedef std::deque< std::pair<void *, CacheId> > LRUFonts;
+    static int      mnRefCount;
+    typedef         std::deque< std::pair<void *, CacheId> > LRUFonts;
     static LRUFonts maLRUFonts;
 public:
-    CairoFontsCache();
-    static void  CacheFont(void *pFont, const CacheId &rId);
-    static void* FindCachedFont(const CacheId &rId);
-    ~CairoFontsCache();
+                                CairoFontsCache();
+                                ~CairoFontsCache();
+
+    static void                 CacheFont(void *pFont, const CacheId &rId);
+    static void*                FindCachedFont(const CacheId &rId);
 };
 
 class CairoTextRender : public TextRenderImpl
 {
-    bool mbPrinter;
-    ServerFont* mpServerFont[ MAX_FALLBACK ];
+    bool            mbPrinter;
+    ServerFont*     mpServerFont[ MAX_FALLBACK ];
 
-    SalColor nTextColor_;
+    SalColor        nTextColor_;
     CairoFontsCache m_aCairoFontsCache;
 
+    bool            bDisableGraphite_;
+
 protected:
-    virtual GlyphCache& getPlatformGlyphCache() = 0;
-    virtual cairo_surface_t* getCairoSurface() = 0;
-    virtual void getSurfaceOffset(double& nDX, double& nDY) = 0;
-    virtual void drawSurface(cairo_t* cr) = 0;
+    virtual GlyphCache&         getPlatformGlyphCache() = 0;
+    virtual cairo_surface_t*    getCairoSurface() = 0;
+    virtual void                getSurfaceOffset(double& nDX, double& nDY) = 0;
+    virtual void                drawSurface(cairo_t* cr) = 0;
 
-bool setFont( const FontSelectPattern *pEntry, int nFallbackLevel );
+    bool                        setFont( const FontSelectPattern *pEntry, int nFallbackLevel );
 
-    virtual void clipRegion(cairo_t* cr) = 0;
+    virtual void                clipRegion(cairo_t* cr) = 0;
 
 public:
-    CairoTextRender(bool bPrinter);
+                                CairoTextRender(bool bPrinter);
 
 
-    virtual void            SetTextColor( SalColor nSalColor ) SAL_OVERRIDE;
+    virtual void                SetTextColor( SalColor nSalColor ) SAL_OVERRIDE;
     virtual sal_uInt16          SetFont( FontSelectPattern*, int nFallbackLevel ) SAL_OVERRIDE;
-    virtual void            GetFontMetric( ImplFontMetricData*, int nFallbackLevel ) SAL_OVERRIDE;
+    virtual void                GetFontMetric( ImplFontMetricData*, int nFallbackLevel ) SAL_OVERRIDE;
     virtual const FontCharMapPtr GetFontCharMap() const SAL_OVERRIDE;
-    virtual bool GetFontCapabilities(vcl::FontCapabilities &rFontCapabilities) const SAL_OVERRIDE;
-    virtual void            GetDevFontList( PhysicalFontCollection* ) SAL_OVERRIDE;
-    virtual void ClearDevFontCache() SAL_OVERRIDE;
-    virtual bool            AddTempDevFont( PhysicalFontCollection*, const OUString& rFileURL, const OUString& rFontName ) SAL_OVERRIDE;
-    virtual bool            CreateFontSubset( const OUString& rToFile,
-                                              const PhysicalFontFace*,
-                                              sal_GlyphId* pGlyphIDs,
-                                              sal_uInt8* pEncoding,
-                                              sal_Int32* pWidths,
-                                              int nGlyphs,
-                                              FontSubsetInfo& rInfo
-                                              ) SAL_OVERRIDE;
-    virtual const Ucs2SIntMap* GetFontEncodingVector( const PhysicalFontFace*, const Ucs2OStrMap** ppNonEncoded ) SAL_OVERRIDE;
-    virtual const void* GetEmbedFontData( const PhysicalFontFace*,
-                                          const sal_Ucs* pUnicodes,
-                                          sal_Int32* pWidths,
-                                          FontSubsetInfo& rInfo,
-                                          long* pDataLen ) SAL_OVERRIDE;
-    virtual void            FreeEmbedFontData( const void* pData, long nDataLen ) SAL_OVERRIDE;
-    virtual void            GetGlyphWidths( const PhysicalFontFace*,
-                                            bool bVertical,
-                                            Int32Vector& rWidths,
-                                            Ucs2UIntMap& rUnicodeEnc ) SAL_OVERRIDE;
-    virtual bool            GetGlyphBoundRect( sal_GlyphId nIndex, Rectangle& ) SAL_OVERRIDE;
-    virtual bool            GetGlyphOutline( sal_GlyphId nIndex, ::basegfx::B2DPolyPolygon& ) SAL_OVERRIDE;
-    virtual SalLayout*      GetTextLayout( ImplLayoutArgs&, int nFallbackLevel ) SAL_OVERRIDE;
-    virtual void            DrawServerFontLayout( const ServerFontLayout& ) SAL_OVERRIDE;
-    virtual SystemFontData  GetSysFontData( int nFallbackLevel ) const SAL_OVERRIDE;
+    virtual bool                GetFontCapabilities(vcl::FontCapabilities &rFontCapabilities) const SAL_OVERRIDE;
+    virtual void                GetDevFontList( PhysicalFontCollection* ) SAL_OVERRIDE;
+    virtual void                ClearDevFontCache() SAL_OVERRIDE;
+    virtual bool                AddTempDevFont( PhysicalFontCollection*, const OUString& rFileURL, const OUString& rFontName ) SAL_OVERRIDE;
+    virtual bool                CreateFontSubset(
+                                    const OUString& rToFile,
+                                    const PhysicalFontFace*,
+                                    sal_GlyphId* pGlyphIDs,
+                                    sal_uInt8* pEncoding,
+                                    sal_Int32* pWidths,
+                                    int nGlyphs,
+                                    FontSubsetInfo& rInfo) SAL_OVERRIDE;
 
-private:
-    bool            bDisableGraphite_;
+    virtual const Ucs2SIntMap*  GetFontEncodingVector( const PhysicalFontFace*, const Ucs2OStrMap** ppNonEncoded ) SAL_OVERRIDE;
+    virtual const void*         GetEmbedFontData(
+                                    const PhysicalFontFace*,
+                                    const sal_Ucs* pUnicodes,
+                                    sal_Int32* pWidths,
+                                    FontSubsetInfo& rInfo,
+                                    long* pDataLen ) SAL_OVERRIDE;
+
+    virtual void                FreeEmbedFontData( const void* pData, long nDataLen ) SAL_OVERRIDE;
+    virtual void                GetGlyphWidths(
+                                    const PhysicalFontFace*,
+                                    bool bVertical,
+                                    Int32Vector& rWidths,
+                                    Ucs2UIntMap& rUnicodeEnc ) SAL_OVERRIDE;
+
+    virtual bool                GetGlyphBoundRect( sal_GlyphId nIndex, Rectangle& ) SAL_OVERRIDE;
+    virtual bool                GetGlyphOutline( sal_GlyphId nIndex, ::basegfx::B2DPolyPolygon& ) SAL_OVERRIDE;
+    virtual SalLayout*          GetTextLayout( ImplLayoutArgs&, int nFallbackLevel ) SAL_OVERRIDE;
+    virtual void                DrawServerFontLayout( const ServerFontLayout& ) SAL_OVERRIDE;
+    virtual SystemFontData      GetSysFontData( int nFallbackLevel ) const SAL_OVERRIDE;
 };
 
 #endif
