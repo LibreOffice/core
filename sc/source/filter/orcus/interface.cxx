aedc3550458cd9dc51e40820ac271edd6ccc5078
Markus Mohrhard <markus.mohrhard@collabora.co.uk>
2015-01-11 22:04:42 +0100

    apply fill properties to item set
    Change-Id: I89d8d84d199a991d75b9c613fd72d66c18642b12

--- sc/source/filter/orcus/interface.cxx
+++ sc/source/filter/orcus/interface.cxx
@@ -659,6 +659,9 @@ void ScOrcusStyles::applyXfToItemSet(SfxItemSet& rSet, const xf& rXf)
         return;
     }
 
+    const fill& rFill = maFills[nFillId];
+    rFill.applyToItemSet(rSet);
+
     size_t nBorderId = rXf.mnBorderId;
     if (nBorderId >= maBorders.size())
     {
