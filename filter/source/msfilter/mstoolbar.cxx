LibreOffice/core

commit e0cce521f1ad0cc384d30ce2f1077ea229fffe62

Resolves: #i121504# Support for alpha channel in clipboard for all systems
(cherry picked from commit ef3931ff410117e1237b3bef7bc090e8b83b9519)

Conflicts:
	automation/source/server/statemnt.cxx
	basic/source/runtime/methods.cxx
	canvas/source/vcl/devicehelper.cxx
	canvas/source/vcl/spritedevicehelper.cxx
	drawinglayer/source/processor2d/vclhelperbufferdevice.cxx
	drawinglayer/source/processor2d/vclmetafileprocessor2d.cxx
	drawinglayer/source/tools/converters.cxx
	dtrans/source/win32/dtobj/FmtFilter.cxx
	editeng/source/items/bulitem.cxx
	extensions/source/scanner/sanedlg.cxx
	external/gcc3_specific/makefile.mk
	filter/source/graphicfilter/eos2met/eos2met.cxx
	filter/source/graphicfilter/ios2met/ios2met.cxx
	filter/source/msfilter/msdffimp.cxx
	fpicker/source/office/iodlg.cxx
	framework/source/fwe/classes/addonsoptions.cxx
	framework/source/fwe/helper/actiontriggerhelper.cxx
	sc/source/filter/excel/xiescher.cxx
	sc/source/ui/docshell/docsh.cxx
	sc/source/ui/inc/viewfunc.hxx
	sd/source/ui/app/sdxfer.cxx
	sd/source/ui/unoidl/unopage.cxx
	sd/source/ui/view/sdview3.cxx
	sfx2/source/appl/fileobj.cxx
	sfx2/source/appl/linkmgr2.cxx
	sfx2/source/dialog/filedlghelper.cxx
	sfx2/source/dialog/intro.cxx
	sfx2/source/doc/docinf.cxx
	sot/inc/sot/formats.hxx
	sot/source/base/formats.cxx
	svtools/bmpmaker/bmpcore.cxx
	svtools/bmpmaker/bmpsum.cxx
	svtools/inc/svtools/transfer.hxx
	svtools/source/filter/filter.cxx
	svtools/source/filter/wmf/emfwr.cxx
	svtools/source/filter/wmf/enhwmf.cxx
	svtools/source/filter/wmf/winwmf.cxx
	svtools/source/filter/wmf/wmfwr.cxx
	svtools/source/graphic/graphic.cxx
	svtools/source/graphic/provider.cxx
	svtools/source/misc/transfer.cxx
	svx/inc/svx/xoutbmp.hxx
	svx/source/sdr/overlay/overlaymanagerbuffered.cxx
	svx/source/xoutdev/_xoutbmp.cxx
	sw/source/core/view/viewsh.cxx
	sw/source/filter/ww1/w1filter.cxx
	sw/source/filter/ww8/ww8par.hxx
	sw/source/ui/dochdl/swdtflvr.cxx
	toolkit/source/awt/vclxbitmap.cxx
	toolkit/source/helper/vclunohelper.cxx

filter/source/msfilter/mstoolbar.cxx
@@ -15,6 +15,7 @@
 #include <com/sun/star/ui/ItemStyle.hpp>
 #include <com/sun/star/frame/XLayoutManager.hpp>
 #include <fstream>
+#include <vcl/dibtools.hxx>
 #include <vcl/graph.hxx>
 #include <vcl/bitmapex.hxx>
 #include <vcl/image.hxx>
@@ -712,7 +713,7 @@ bool TBCBitMap::Read( SvStream& rS)
     nOffSet = rS.Tell();
     rS >> cbDIB;
     // cbDIB = sizeOf(biHeader) + sizeOf(colors) + sizeOf(bitmapData) + 10
-    return mBitMap.Read( rS, sal_False, sal_True );
+    return ReadDIB(mBitMap, rS, false);
 }
 
 void TBCBitMap::Print( FILE* fp )

include/vcl/dibtools.hxx
@@ -1,0 +1,70 @@
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+#ifndef _SV_DIBTOOLS_HXX
+#define _SV_DIBTOOLS_HXX
+
+#include <vcl/dllapi.h>
+#include <vcl/mapmod.hxx>
+#include <tools/rc.hxx>
+#include <vcl/region.hxx>
+
+//////////////////////////////////////////////////////////////////////////////
+// predefines
+
+class SvStream;
+class BitmapEx;
+class Bitmap;
+
+//////////////////////////////////////////////////////////////////////////////
+
+bool VCL_DLLPUBLIC ReadDIB( // ReadDIB(rBitmap, rIStm, true);
+    Bitmap& rTarget,
+    SvStream& rIStm,
+    bool bFileHeader);
+
+bool VCL_DLLPUBLIC ReadDIBBitmapEx(
+    BitmapEx& rTarget,
+    SvStream& rIStm);
+
+bool VCL_DLLPUBLIC ReadDIBV5(
+    Bitmap& rTarget,
+    Bitmap& rTargetAlpha,
+    SvStream& rIStm);
+
+//////////////////////////////////////////////////////////////////////////////
+
+bool VCL_DLLPUBLIC WriteDIB( // WriteDIB(rBitmap, rOStm, false, true);
+    const Bitmap& rSource,
+    SvStream& rOStm,
+    bool bCompressed,
+    bool bFileHeader);
+
+bool VCL_DLLPUBLIC WriteDIBBitmapEx(
+    const BitmapEx& rSource,
+    SvStream& rOStm);
+
+bool VCL_DLLPUBLIC WriteDIBV5(
+    const Bitmap& rSource,
+    const Bitmap& rSourceAlpha,
+    SvStream& rOStm);
+
+//////////////////////////////////////////////////////////////////////////////
+
+#endif // _SV_DIBTOOLS_HXX
+
+//////////////////////////////////////////////////////////////////////////////

commit 470fcca594ba4f6bf473d4d44b415c2ba151b0d9

Resolves: fdo#87015 image missing from doc
regression from

commit e0cce521f1ad0cc384d30ce2f1077ea229fffe62
Author:     Armin Le Grand <alg@apache.org>
AuthorDate: Thu Jan 10 16:28:40 2013 +0000
Commit:     Caolán McNamara <caolanm@redhat.com>
CommitDate: Thu Jun 13 14:50:46 2013 +0100
    Resolves: #i121504# Support for alpha channel in clipboard for all systems
    (cherry picked from commit ef3931ff410117e1237b3bef7bc090e8b83b9519)

which blindly just bulldozed out the bMSOFormat branch

Change-Id: Iec354f1fb585f0803b9df472bc9ec9e103aa5847

filter/source/msfilter/mstoolbar.cxx
@@ -724,7 +724,7 @@ bool TBCBitMap::Read( SvStream& rS)
     nOffSet = rS.Tell();
     rS.ReadInt32( cbDIB );
     // cbDIB = sizeOf(biHeader) + sizeOf(colors) + sizeOf(bitmapData) + 10
-    return ReadDIB(mBitMap, rS, false);
+    return ReadDIB(mBitMap, rS, false, true);
 }
 
 #if OSL_DEBUG_LEVEL > 1

include/vcl/dibtools.hxx
@@ -36,7 +36,8 @@ class Bitmap;
 bool VCL_DLLPUBLIC ReadDIB( // ReadDIB(rBitmap, rIStm, true);
     Bitmap& rTarget,
     SvStream& rIStm,
-    bool bFileHeader);
+    bool bFileHeader,
+    bool bMSOFormat=false);
 
 bool VCL_DLLPUBLIC ReadDIBBitmapEx(
     BitmapEx& rTarget,

vcl/source/gdi/dibtools.cxx
@@ -166,7 +166,7 @@ namespace
     }
 }
 
-bool ImplReadDIBInfoHeader(SvStream& rIStm, DIBV5Header& rHeader, bool& bTopDown)
+bool ImplReadDIBInfoHeader(SvStream& rIStm, DIBV5Header& rHeader, bool& bTopDown, bool bMSOFormat)
 {
     // BITMAPINFOHEADER or BITMAPCOREHEADER or BITMAPV5HEADER
     const sal_Size aStartPos(rIStm.Tell());
@@ -181,6 +181,29 @@ bool ImplReadDIBInfoHeader(SvStream& rIStm, DIBV5Header& rHeader, bool& bTopDown
         rIStm.ReadInt16( nTmp16 ); rHeader.nHeight = nTmp16;
         rIStm.ReadUInt16( rHeader.nPlanes );
         rIStm.ReadUInt16( rHeader.nBitCount );
+    }
+    else if ( bMSOFormat && rHeader.nSize == DIBINFOHEADERSIZE )
+    {
+        sal_Int16 nTmp16(0);
+        rIStm.ReadInt16(nTmp16);
+        rHeader.nWidth = nTmp16;
+        rIStm.ReadInt16(nTmp16);
+        rHeader.nHeight = nTmp16;
+        sal_uInt8 nTmp8(0);
+        rIStm.ReadUChar(nTmp8);
+        rHeader.nPlanes = nTmp8;
+        rIStm.ReadUChar(nTmp8);
+        rHeader.nBitCount = nTmp8;
+        rIStm.ReadInt16(nTmp16);
+        rHeader.nSizeImage = nTmp16;
+        rIStm.ReadInt16(nTmp16);
+        rHeader.nCompression = nTmp16;
+        if ( !rHeader.nSizeImage ) // uncompressed?
+            rHeader.nSizeImage = ((rHeader.nWidth * rHeader.nBitCount + 31) & ~31) / 8 * rHeader.nHeight;
+        rIStm.ReadInt32( rHeader.nXPelsPerMeter );
+        rIStm.ReadInt32( rHeader.nYPelsPerMeter );
+        rIStm.ReadUInt32( rHeader.nColsUsed );
+        rIStm.ReadUInt32( rHeader.nColsImportant );
     }
     else
     {
@@ -679,14 +702,14 @@ bool ImplReadDIBBits(SvStream& rIStm, DIBV5Header& rHeader, BitmapWriteAccess& r
     return( rIStm.GetError() == 0UL );
 }
 
-bool ImplReadDIBBody( SvStream& rIStm, Bitmap& rBmp, Bitmap* pBmpAlpha, sal_uLong nOffset )
+bool ImplReadDIBBody( SvStream& rIStm, Bitmap& rBmp, Bitmap* pBmpAlpha, sal_uLong nOffset, bool bMSOFormat = false )
 {
     DIBV5Header aHeader;
     const sal_uLong nStmPos = rIStm.Tell();
     bool bRet(false);
     bool bTopDown(false);
 
-    if(ImplReadDIBInfoHeader(rIStm, aHeader, bTopDown) && aHeader.nWidth && aHeader.nHeight && aHeader.nBitCount)
+    if(ImplReadDIBInfoHeader(rIStm, aHeader, bTopDown, bMSOFormat) && aHeader.nWidth && aHeader.nHeight && aHeader.nBitCount)
     {
         // In case ImplReadDIB() didn't call ImplReadDIBFileHeader() before
         // this method, nOffset is 0, that's OK.
@@ -1417,7 +1440,8 @@ bool ImplReadDIB(
     Bitmap& rTarget, Bitmap*
     pTargetAlpha,
     SvStream& rIStm,
-    bool bFileHeader)
+    bool bFileHeader,
+    bool bMSOFormat=false)
 {
     const SvStreamEndian nOldFormat(rIStm.GetEndian());
     const sal_uLong nOldPos(rIStm.Tell());
@@ -1435,7 +1459,7 @@ bool ImplReadDIB(
     }
     else
     {
-        bRet = ImplReadDIBBody(rIStm, rTarget, 0, nOffset);
+        bRet = ImplReadDIBBody(rIStm, rTarget, 0, nOffset, bMSOFormat);
     }
 
     if(!bRet)
@@ -1523,9 +1547,10 @@ bool ImplWriteDIB(
 bool ReadDIB(
     Bitmap& rTarget,
     SvStream& rIStm,
-    bool bFileHeader)
+    bool bFileHeader,
+    bool bMSOFormat)
 {
-    return ImplReadDIB(rTarget, 0, rIStm, bFileHeader);
+    return ImplReadDIB(rTarget, 0, rIStm, bFileHeader, bMSOFormat);
 }
 
 bool ReadDIBBitmapEx(