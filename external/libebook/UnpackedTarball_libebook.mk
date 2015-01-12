34fb25cd5bbf056e127f457a7a02d01efd65a8eb
David Tardon <dtardon@redhat.com>
2015-01-12 17:12:31 +0100

    fix alpha computation, followup
    Change-Id: I7701368e152ce20adbc05802c7c6518183919d1f

--- external/libebook/UnpackedTarball_libebook.mk
+++ external/libebook/UnpackedTarball_libebook.mk
@@ -15,6 +15,7 @@ $(eval $(call gb_UnpackedTarball_set_patchlevel,libebook,0))
 
 $(eval $(call gb_UnpackedTarball_add_patches,libebook, \
     external/libebook/ubsan.patch \
+    external/libebook/0001-lrf-compute-color-interpolation-coeff.-correctly.patch.1 \
 ))
 
 # vim: set noet sw=4 ts=4:
