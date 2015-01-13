LibreOffice/core

commit 8583da1e934a49791ef8d86668f3d5c3c5dae1d7

java: remove unused fields
found by UCDetector

Change-Id: I4a7c56a9918054c23469de5680658e7b501f0165

scripting/java/com/sun/star/script/framework/browse/ParcelBrowseNode.java
@@ -57,10 +57,10 @@ public class ParcelBrowseNode extends PropertySet
     private Collection<XBrowseNode> browsenodes;
     private ParcelContainer container;
     private Parcel parcel;
-    private boolean deletable = true;
-    private boolean editable  = false;
-    private boolean creatable = false;
-    private boolean renamable = true;
+
+
+
+
 
     public ParcelBrowseNode( ScriptProvider provider, ParcelContainer container, String parcelName ) {
         this.provider = provider;
@@ -86,10 +86,6 @@ public class ParcelBrowseNode extends PropertySet
             (short)0, "creatable");
         registerProperty("Renamable", new Type(boolean.class),
             (short)0, "renamable");
-        if (provider.hasScriptEditor() == true)
-        {
-            this.creatable = true;
-        }
 
         String parcelDirUrl = parcel.getPathToParcel();
         XComponentContext xCtx = provider.getScriptingContext().getComponentContext();
@@ -100,14 +96,6 @@ public class ParcelBrowseNode extends PropertySet
                 xFac.createInstanceWithContext(
                     "com.sun.star.ucb.SimpleFileAccess",
                     xCtx ) );
-            if ( xSFA != null && ( xSFA.isReadOnly( parcelDirUrl ) ||
-                container.isUnoPkg() ) )
-            {
-                deletable = false;
-                editable  = false;
-                creatable = false;
-                renamable = false;
-            }
         }
         catch ( com.sun.star.uno.Exception e )
         {

scripting/java/com/sun/star/script/framework/browse/PkgProviderBrowseNode.java
@@ -33,10 +33,6 @@ public class PkgProviderBrowseNode extends ProviderBrowseNode
         LogUtils.DEBUG("*** PkgProviderBrowseNode ctor container name = " + container.getName());
         LogUtils.DEBUG("*** PkgProviderBrowseNode ctor container path = " + container.getParcelContainerDir());
         LogUtils.DEBUG("*** PkgProviderBrowseNode ctor, container has num parcels = " + container.getElementNames().length);
-        deletable = false;
-        editable  = false;
-        creatable = false;
-
     }
 
     @Override

scripting/java/com/sun/star/script/framework/browse/ProviderBrowseNode.java
@@ -50,12 +50,12 @@ public class ProviderBrowseNode extends PropertySet
     private Collection<XBrowseNode> browsenodes;
     private String name;
     protected ParcelContainer container;
-    protected Parcel parcel;
+
     private XComponentContext m_xCtx;
 
-    public boolean deletable = true;
-    public boolean creatable = true;
-    public boolean editable = false;
+
+
+
 
     public ProviderBrowseNode( ScriptProvider provider, ParcelContainer container, XComponentContext xCtx ) {
         LogUtils.DEBUG("*** ProviderBrowseNode ctor");
@@ -78,11 +78,6 @@ public class ProviderBrowseNode extends PropertySet
                 xFac.createInstanceWithContext(
                     "com.sun.star.ucb.SimpleFileAccess",
                     xCtx ) );
-            if (  container.isUnoPkg() || xSFA.isReadOnly( container.getParcelContainerDir() ) )
-            {
-                deletable = false;
-                creatable = false;
-            }
         }
         // TODO propage errors
         catch( com.sun.star.uno.Exception e )

scripting/java/com/sun/star/script/framework/browse/ScriptBrowseNode.java
@@ -52,8 +52,8 @@ public class ScriptBrowseNode extends PropertySet
 
     private Parcel parent;
     private String name;
-    private String uri;
-    private String description;
+
+
     private boolean editable  = false;
     private boolean deletable = false;
     private boolean renamable = false;
@@ -82,9 +82,6 @@ public class ScriptBrowseNode extends PropertySet
         {
             LogUtils.DEBUG("** caught exception getting script data for " + name + " ->" + e.toString() );
         }
-
-        uri = data.getShortFormScriptURL();
-        description = data.getDescription();
 
         if (provider.hasScriptEditor() == true)
         {
@@ -155,7 +152,6 @@ public class ScriptBrowseNode extends PropertySet
         {
             LogUtils.DEBUG("** caught exception getting script data for " + name + " ->" + e.toString() );
         }
-        uri = data.getShortFormScriptURL();
     }
     // implementation of XInvocation interface
     public XIntrospectionAccess getIntrospection() {
@@ -271,7 +267,6 @@ public class ScriptBrowseNode extends PropertySet
                 LogUtils.DEBUG("Now remove old script");
                 parent.removeByName( name );
 
-                uri = data.getShortFormScriptURL();
                 name = languageName;
                 result = new Any(new Type(XBrowseNode.class), this);
             }

scripting/java/com/sun/star/script/framework/provider/ScriptContext.java
@@ -42,9 +42,9 @@ public class ScriptContext extends PropertySet implements XScriptContext
 
     private XModel m_xModel = null;
     private XScriptInvocationContext m_xInvocationContext = null;
-    public String m_sDocURI = null;
+
     private XDesktop m_xDeskTop = null;
-    public Integer m_iStorageID = null;
+
     private XComponentContext m_xComponentContext = null;
 
     private ScriptContext( XComponentContext xmComponentContext,

commit 3ab2d3a2c5b802ab88171770d19871e081c3252b

fdo#88256 fix crash when running javascript macro
caused by my commit 8583da1e934a49791ef8d86668f3d5c3c5dae1d7
"java: remove unused fields"

Change-Id: Ibcd6462e8229a0a6cb98ebfd16ce5d2ea45ca931

configure.ac
@@ -4527,10 +4527,6 @@ dnl Check for syslog header
 dnl ===================================================================
 AC_CHECK_HEADER(syslog.h, AC_DEFINE(HAVE_SYSLOG_H))
 
-# placeholder for future crash reporter feature
-ENABLE_CRASHDUMP=""
-AC_SUBST(ENABLE_CRASHDUMP)
-
 dnl Set the ENABLE_WERROR variable. (Activate --enable-werror)
 dnl ===================================================================
 AC_MSG_CHECKING([whether to turn warnings to errors])

scripting/java/com/sun/star/script/framework/browse/ParcelBrowseNode.java
@@ -55,6 +55,11 @@ public class ParcelBrowseNode extends PropertySet implements
     private Collection<XBrowseNode> browsenodes;
     private final ParcelContainer container;
     private Parcel parcel;
+    // these four are properties, they are accessed via reflection
+    public boolean deletable = true;
+    public boolean editable  = false;
+    public boolean creatable = false;
+    public boolean renamable = true;
 
     public ParcelBrowseNode(ScriptProvider provider, ParcelContainer container,
                             String parcelName) {
@@ -75,15 +80,28 @@ public class ParcelBrowseNode extends PropertySet implements
         registerProperty("Editable", new Type(boolean.class), (short)0, "editable");
         registerProperty("Creatable", new Type(boolean.class), (short)0, "creatable");
         registerProperty("Renamable", new Type(boolean.class), (short)0, "renamable");
+        if (provider.hasScriptEditor())
+        {
+            this.creatable = true;
+        }
 
+        String parcelDirUrl = parcel.getPathToParcel();
         XComponentContext xCtx = provider.getScriptingContext().getComponentContext();
         XMultiComponentFactory xFac = xCtx.getServiceManager();
 
         try {
-            UnoRuntime.queryInterface(XSimpleFileAccess.class,
+            XSimpleFileAccess xSFA = UnoRuntime.queryInterface(XSimpleFileAccess.class,
                                       xFac.createInstanceWithContext(
                                           "com.sun.star.ucb.SimpleFileAccess",
                                           xCtx));
+            if ( xSFA != null && ( xSFA.isReadOnly( parcelDirUrl ) ||
+                 container.isUnoPkg() ) )
+            {
+                deletable = false;
+                editable  = false;
+                creatable = false;
+                renamable = false;
+            }
         } catch (com.sun.star.uno.Exception e) {
             // TODO propagate potential errors
             LogUtils.DEBUG("Caught exception creating ParcelBrowseNode " + e);

scripting/java/com/sun/star/script/framework/browse/PkgProviderBrowseNode.java
@@ -37,6 +37,9 @@ public class PkgProviderBrowseNode extends ProviderBrowseNode {
                        container.getParcelContainerDir());
         LogUtils.DEBUG("*** PkgProviderBrowseNode ctor, container has num parcels = " +
                        container.getElementNames().length);
+        deletable = false;
+        editable  = false;
+        creatable = false;
     }
 
     @Override public String getName() {

scripting/java/com/sun/star/script/framework/browse/ProviderBrowseNode.java
@@ -52,6 +52,10 @@ public class ProviderBrowseNode extends PropertySet implements
     private final String name;
     protected ParcelContainer container;
     private final XComponentContext m_xCtx;
+    // these are properties, they are accessed by reflection
+    public boolean deletable = true;
+    public boolean creatable = true;
+    public boolean editable = false;
 
     public ProviderBrowseNode(ScriptProvider provider, ParcelContainer container,
                               XComponentContext xCtx) {
@@ -68,10 +72,15 @@ public class ProviderBrowseNode extends PropertySet implements
         XMultiComponentFactory xFac = m_xCtx.getServiceManager();
 
         try {
-            UnoRuntime.queryInterface(XSimpleFileAccess.class,
+            XSimpleFileAccess xSFA = UnoRuntime.queryInterface(XSimpleFileAccess.class,
                                          xFac.createInstanceWithContext(
                                              "com.sun.star.ucb.SimpleFileAccess",
                                              xCtx));
+            if (  container.isUnoPkg() || xSFA.isReadOnly( container.getParcelContainerDir() ) )
+            {
+                deletable = false;
+                creatable = false;
+            }
         }
         // TODO propage errors
         catch (com.sun.star.uno.Exception e) {

scripting/java/com/sun/star/script/framework/browse/ScriptBrowseNode.java
@@ -58,25 +58,32 @@ public class ScriptBrowseNode extends PropertySet implements
     private Parcel parent;
     private String name;
 
-    private boolean editable;
-    private boolean deletable = false;
-    private boolean renamable = false;
+    // these are properties, accessed by reflection
+    public String uri;
+    public String description;
+    public boolean editable;
+    public boolean deletable = false;
+    public boolean renamable = false;
 
     public ScriptBrowseNode(ScriptProvider provider, Parcel parent, String name) {
 
         this.provider = provider;
         this.name = name;
         this.parent = parent;
+        ScriptMetaData data = null;
         XComponentContext xCtx = provider.getScriptingContext().getComponentContext();
         XMultiComponentFactory xFac = xCtx.getServiceManager();
 
         try {
+            data = (ScriptMetaData)parent.getByName( name );
             XSimpleFileAccess xSFA = UnoRuntime.queryInterface(
                                          XSimpleFileAccess.class,
                                          xFac.createInstanceWithContext(
                                              "com.sun.star.ucb.SimpleFileAccess",
                                              xCtx));
 
+            uri = data.getShortFormScriptURL();
+            description = data.getDescription();
             if (provider.hasScriptEditor()) {
                 this.editable = true;
 
@@ -147,7 +154,9 @@ public class ScriptBrowseNode extends PropertySet implements
             LogUtils.DEBUG("** caught exception getting script data for " + name +
                            " ->" + e.toString());
         }
+        uri = data.getShortFormScriptURL();
     }
+
     // implementation of XInvocation interface
     public XIntrospectionAccess getIntrospection() {
         return null;
@@ -250,6 +259,7 @@ public class ScriptBrowseNode extends PropertySet implements
                 LogUtils.DEBUG("Now remove old script");
                 parent.removeByName(name);
 
+                uri = data.getShortFormScriptURL();
                 name = languageName;
                 result = new Any(new Type(XBrowseNode.class), this);
             } catch (NoSuchElementException nse) {

scripting/java/com/sun/star/script/framework/provider/ScriptContext.java
@@ -44,12 +44,15 @@ public class ScriptContext extends PropertySet implements XScriptContext {
 
     private final static String DOC_URI = "SCRIPTING_DOC_URI";
 
-    private final XModel m_xModel;
+    public final XModel m_xModel;
     private final XScriptInvocationContext m_xInvocationContext;
 
     private final XDesktop m_xDeskTop;
 
     private final XComponentContext m_xComponentContext;
+
+    // property, accessed via reflection
+    public String m_sDocURI = null;
 
     private ScriptContext(XComponentContext xmComponentContext, XDesktop xDesktop,
                           XModel xModel, XScriptInvocationContext xInvocContext) {