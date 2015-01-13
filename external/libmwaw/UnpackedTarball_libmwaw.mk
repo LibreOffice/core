fcfa399067a7606e0ffc426265428b25c8e9260d
Stephan Bergmann <sbergman@redhat.com>
2015-01-13 10:54:36 +0100

    external/libmwaw: Work around -fsanitize=shift
    Change-Id: Ia44ef02bc19653e2a20b2da52f5b5b377950353d

--- external/libmwaw/UnpackedTarball_libmwaw.mk
+++ external/libmwaw/UnpackedTarball_libmwaw.mk
@@ -16,6 +16,7 @@ $(eval $(call gb_UnpackedTarball_set_patchlevel,libmwaw,1))
 $(eval $(call gb_UnpackedTarball_add_patches,libmwaw,\
 	external/libmwaw/0001-ClarisWorks-AppleWorks-spreadsheet-do-not-remove-fir.patch \
 	external/libmwaw/0002-ClarisWors-AppleWorks-spreadsheet-Oops-correct-of-a-.patch \
+	external/libmwaw/ubsan.patch.0 \
 ))
 
 # vim: set noet sw=4 ts=4:
