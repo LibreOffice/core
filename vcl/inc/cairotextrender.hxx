2c6bc5acde1c6d013c67629815a89efb7a0b0b42
Chris Sherlock <chris.sherlock@collabora.com>
2015-01-19 16:04:24 +1100

    vcl: Reduce OpenGLX11CairoTextRender surface area to size of text bounds
    With this patch I get the text boundary and use it to reduce the size
    of the surface to this area. This is needed because OpenGLX11CairoTextRender
    creates a surface image and uses this for OpenGL rendering, and by reducing
    this we reduce memory usage and also reduce cycles taken in creating the
    image pixmap and the corresponding OpenGL texture processing.
    
    Change-Id: I88a11253f6c2dc28439547d9431e223fc1f68161

--- vcl/inc/cairotextrender.hxx
+++ vcl/inc/cairotextrender.hxx
@@ -80,7 +80,7 @@ class CairoTextRender : public TextRenderImpl
 
 protected:
     virtual GlyphCache&         getPlatformGlyphCache() = 0;
-    virtual cairo_surface_t*    getCairoSurface() = 0;
+    virtual cairo_surface_t*    getCairoSurface( const ServerFontLayout &rLayout ) = 0;
     virtual void                getSurfaceOffset(double& nDX, double& nDY) = 0;
     virtual void                drawSurface(cairo_t* cr) = 0;
 
