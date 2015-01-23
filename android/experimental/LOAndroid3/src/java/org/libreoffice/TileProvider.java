5fcd13c49f1a55ae9e6eb23d43bc326910ae0aa0
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2015-01-23 23:48:14 +0900

    android: add some comments to TileProvider interface
    Change-Id: I1c6f7bccda2392235242f5c26af1437f1365f728

--- android/experimental/LOAndroid3/src/java/org/libreoffice/TileProvider.java
+++ android/experimental/LOAndroid3/src/java/org/libreoffice/TileProvider.java
@@ -9,8 +9,14 @@ import org.mozilla.gecko.gfx.CairoImage;
 import org.mozilla.gecko.gfx.IntSize;
 
 public interface TileProvider {
+    /**
+     * Returns the page width in pixels.
+     */
     int getPageWidth();
 
+    /**
+     * Returns the page height in pixels.
+     */
     int getPageHeight();
 
     boolean isReady();
@@ -22,18 +28,42 @@ public interface TileProvider {
      */
     void rerenderTile(CairoImage image, float x, float y, IntSize tileSize, float zoom);
 
+    /**
+     * Change the document part to the one specified by the partIndex input parameter.
+     * @param partIndex - part index to change to
+     */
     void changePart(int partIndex);
 
+    /**
+     * Get the current document part number.
+     * @return
+     */
     int getCurrentPartNumber();
 
     Bitmap thumbnail(int size);
 
+    /**
+     * Closes the document.
+     */
     void close();
 
+    /**
+     * Returns true if the current open document is a text document.
+     */
     boolean isTextDocument();
 
+    /**
+     * Register a callback that is invoked when a tile invalidation is
+     * required.
+     *
+     * @param tileInvalidationCallback - the tile invalidation callback
+     */
     void registerInvalidationCallback(TileProvider.TileInvalidationCallback tileInvalidationCallback);
 
+    /**
+     * Trigger a key press.
+     * @param keyEvent - contains the
+     */
     void keyPress(KeyEvent keyEvent);
 
     /**
