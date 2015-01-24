3a5fa612b4afb72b5f91877a5c52e25c7604ae1a
Caolán McNamara <caolanm@redhat.com>
2015-01-24 20:37:36 +0000

    Resolves: tdf#81124 crash on setting autofilter on column with no content
    Change-Id: Id53e589789144d892427a8a4ec1af1926aa97b52

--- sc/source/ui/cctrl/checklistmenu.cxx
+++ sc/source/ui/cctrl/checklistmenu.cxx
@@ -1065,6 +1065,8 @@ void ScCheckListMenuWindow::selectCurrentMemberOnly(bool bSet)
 {
     setAllMemberState(!bSet);
     SvTreeListEntry* pEntry = maChecks.GetCurEntry();
+    if (!pEntry)
+        return;
     maChecks.CheckEntry(pEntry, bSet );
 }
 
@@ -1456,7 +1458,6 @@ void ScCheckListBox::KeyInput( const KeyEvent& rKEvt )
     if ( rKey.GetCode() == KEY_RETURN || rKey.GetCode() == KEY_SPACE )
     {
         SvTreeListEntry* pEntry = GetCurEntry();
-
         if ( pEntry )
         {
             bool bCheck = ( GetCheckButtonState( pEntry ) == SV_BUTTON_CHECKED );
