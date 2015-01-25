00c42b64ab57a54db2475084cdfd70e64405bd8c
Caolán McNamara <caolanm@redhat.com>
2015-01-25 14:20:35 +0000

    coverity#1266499 Uninitialized scalar field
    Change-Id: I788f0b34abd80feab247c3f2bba8ac5d49fc5c08

--- vcl/workben/vcldemo.cxx
+++ vcl/workben/vcldemo.cxx
@@ -1209,6 +1209,7 @@ class DemoWin : public WorkWindow
             , mrWin(rWin)
         {
             maDelay.Seconds = nDelaySecs;
+            maDelay.Nanosec = 0;
             launch();
         }
         virtual ~RenderThread()
