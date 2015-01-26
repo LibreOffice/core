875dc064aa3533dcc41416166ba957c02f81f0bf
Stephan Bergmann <sbergman@redhat.com>
2015-01-26 15:04:19 +0100

    external/boost: Visible function type RTTI for Clang -fsanitize=function
    Change-Id: I6a1ab01bbe5cd128b132c3f6dde4b520ddb4c373

--- external/boost/UnpackedTarball_boost.mk
+++ external/boost/UnpackedTarball_boost.mk
@@ -108,6 +108,7 @@ boost_patches += boost.boost_static_assert_unused_attribute.patch.0
 boost_patches += w4702.patch.0
 
 boost_patches += ubsan.patch.0
+boost_patches += rtti.patch.0
 
 $(eval $(call gb_UnpackedTarball_UnpackedTarball,boost))
 
