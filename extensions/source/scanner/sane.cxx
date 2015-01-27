LibreOffice/core

commit bbf3aa7f92c194c9cc4546c95706e6b28e0c070c

coverity#705989 silence Insecure temporary file
and

coverity#705983 Insecure temporary file

Change-Id: I2a501f8114ddf8968381e1880263abaf9d1bdc6f

extensions/source/scanner/sane.cxx
@@ -21,6 +21,7 @@
 #include <math.h>
 #include <osl/file.h>
 #include <tools/stream.hxx>
+#include <unotools/tempfile.hxx>
 #include <sane.hxx>
 #include <dlfcn.h>
 #include <stdio.h>
@@ -694,7 +695,9 @@ bool Sane::Start( BitmapTransporter& rBitmap )
                 if( nStatus != SANE_STATUS_GOOD )
                     bSynchronousRead = true;
             }
-            FILE* pFrame = tmpfile();
+            utl::TempFile aFrame;
+            aFrame.EnableKillingFile();
+            FILE* pFrame = fopen(OUStringToOString(aFrame.GetFileName(), osl_getThreadTextEncoding()).getStr(), "wb");
             if( ! pFrame )
             {
                 bSuccess = false;

vcl/generic/print/glyphset.cxx
@@ -35,6 +35,8 @@
 #include "rtl/ustring.hxx"
 #include "rtl/strbuf.hxx"
 
+#include <unotools/tempfile.hxx>
+
 #include <set>
 #include <map>
 #include <algorithm>
@@ -58,9 +60,6 @@ GlyphSet::~GlyphSet ()
 {
     /* FIXME delete the glyphlist ??? */
 }
-
-
-
 
 bool
 GlyphSet::GetCharID (
@@ -742,7 +741,10 @@ GlyphSet::PSUploadFont (osl::File& rOutFile, PrinterGfx &rGfx, bool bAllowType42
     sal_Int32 nSuccess = OpenTTFontFile(aTTFileName.getStr(), nFace, &pTTFont);
     if (nSuccess != SF_OK)
         return false;
-    FILE* pTmpFile = tmpfile();
+
+    utl::TempFile aTmpFile;
+    aTmpFile.EnableKillingFile();
+    FILE* pTmpFile = fopen(OUStringToOString(aTmpFile.GetFileName(), osl_getThreadTextEncoding()).getStr(), "wb");
     if (pTmpFile == NULL)
         return false;
 

extensions/Library_scn.mk
@@ -24,6 +24,7 @@ $(eval $(call gb_Library_use_libraries,scn,\
 	svt \
 	vcl \
 	tl \
+	utl \
 	comphelper \
 	cppuhelper \
 	cppu \

commit 42a5bce41c6315f9f5534fd7a8cc35a5dfd1fbf3

Resolves: tdf#88819 printing with ps hangs
regression from

commit bbf3aa7f92c194c9cc4546c95706e6b28e0c070c
Author: Caolán McNamara <caolanm@redhat.com>
Date:   Fri Nov 14 11:06:44 2014 +0000
    coverity#705989 silence Insecure temporary file
    and
    coverity#705983 Insecure temporary file

Change-Id: If580196322e68098adc38a9fdb775827843a9c30

extensions/source/scanner/sane.cxx
@@ -697,7 +697,7 @@ bool Sane::Start( BitmapTransporter& rBitmap )
             }
             utl::TempFile aFrame;
             aFrame.EnableKillingFile();
-            FILE* pFrame = fopen(OUStringToOString(aFrame.GetFileName(), osl_getThreadTextEncoding()).getStr(), "wb");
+            FILE* pFrame = fopen(OUStringToOString(aFrame.GetFileName(), osl_getThreadTextEncoding()).getStr(), "w+b");
             if( ! pFrame )
             {
                 bSuccess = false;

vcl/generic/print/glyphset.cxx
@@ -744,7 +744,7 @@ GlyphSet::PSUploadFont (osl::File& rOutFile, PrinterGfx &rGfx, bool bAllowType42
 
     utl::TempFile aTmpFile;
     aTmpFile.EnableKillingFile();
-    FILE* pTmpFile = fopen(OUStringToOString(aTmpFile.GetFileName(), osl_getThreadTextEncoding()).getStr(), "wb");
+    FILE* pTmpFile = fopen(OUStringToOString(aTmpFile.GetFileName(), osl_getThreadTextEncoding()).getStr(), "w+b");
     if (pTmpFile == NULL)
         return false;
 