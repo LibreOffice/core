199cec3af070a59fd8e4bc550e9819e3949c2d08
Caolán McNamara <caolanm@redhat.com>
2015-01-27 09:28:14 +0000

    remove spurious xDocStorage temp
    Change-Id: Ia47394bcd4d39fca5096413ca7e0fbffbc3d7ac6

--- xmlscript/source/xmldlg_imexp/xmldlg_impmodels.cxx
+++ xmlscript/source/xmldlg_imexp/xmldlg_impmodels.cxx
@@ -911,8 +911,6 @@ void ImageControlElement::endElement()
     ctx.importDefaults( _nBasePosX, _nBasePosY, _xAttributes );
     ctx.importBooleanProperty( "ScaleImage", "scale-image", _xAttributes );
     ctx.importImageScaleModeProperty( "ScaleMode" , "scale-mode" , _xAttributes );
-    Reference< document::XStorageBasedDocument > xDocStorage( _pImport->getDocOwner(), UNO_QUERY );
-
     ctx.importImageURLProperty( "ImageURL" , "src" , _xAttributes );
     ctx.importBooleanProperty( "Tabstop", "tabstop", _xAttributes );
     ctx.importEvents( _events );
