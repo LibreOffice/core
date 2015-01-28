93f807bc15dc6580541f79701ff1561a4a6b9848
Eike Rathke <erack@redhat.com>
2015-01-28 13:47:39 +0100

    Resolves: tdf#88792 do not hold a ScFormulaCell** in group area listener
    ... as the mdds storage segment may change when a formula cell is
    inserted at a position such that two segments are merged into a new one.
    
    (cherry picked from commit 47230a036fe35b9a7a7c0609232849fcbb51efcc)
    
    add some SAL_INFO to group area listener and formula cell ctor/dtor
    
    guess we'll need them more often..
    
    (cherry picked from commit f8d6640afa3c8528d2a66741edc49a48c67902d3)
    
    let have FormulaGroupAreaListener a virtual dtor
    
    (cherry picked from commit 0fafb53d1c860388461f30e613a37c128c90ad79)
    
    0ef149fc5edceee765419764bf0efa571ba9d977
    1f5e246d68971124ef21e9c2d0db80499928998b
    
    Change-Id: I449a89005418aff7ea12099ea931b786959dbc3b
    Reviewed-on: https://gerrit.libreoffice.org/14413
    Reviewed-by: Michael Meeks <michael.meeks@collabora.com>
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>

--- sc/source/core/tool/grouparealistener.cxx
+++ sc/source/core/tool/grouparealistener.cxx
@@ -15,6 +15,8 @@
 #include <column.hxx>
 #include <listenerquery.hxx>
 #include <listenerqueryids.hxx>
+#include <document.hxx>
+#include <table.hxx>
 
 namespace sc {
 
@@ -69,15 +71,31 @@ public:
 
 }
 
-FormulaGroupAreaListener::FormulaGroupAreaListener(
-    const ScRange& rRange, ScFormulaCell** ppTopCell, SCROW nGroupLen, bool bStartFixed, bool bEndFixed ) :
+FormulaGroupAreaListener::FormulaGroupAreaListener( const ScRange& rRange, const ScDocument& rDocument,
+        const ScAddress& rTopCellPos, SCROW nGroupLen, bool bStartFixed, bool bEndFixed ) :
     maRange(rRange),
-    mppTopCell(ppTopCell),
+    mpColumn(NULL),
+    mnTopCellRow(rTopCellPos.Row()),
     mnGroupLen(nGroupLen),
     mbStartFixed(bStartFixed),
     mbEndFixed(bEndFixed)
 {
-    assert(mppTopCell); // This can't be NULL.
+    const ScTable* pTab = rDocument.FetchTable( rTopCellPos.Tab());
+    assert(pTab);
+    mpColumn = pTab->FetchColumn( rTopCellPos.Col());
+    assert(mpColumn);
+    SAL_INFO( "sc.core.grouparealistener",
+            "FormulaGroupAreaListener ctor this " << this <<
+            " range " << maRange.Format(SCA_VALID) <<
+            " mnTopCellRow " << mnTopCellRow << " length " << mnGroupLen);
+}
+
+FormulaGroupAreaListener::~FormulaGroupAreaListener()
+{
+    SAL_INFO( "sc.core.grouparealistener",
+            "FormulaGroupAreaListener dtor this " << this <<
+            " range " << maRange.Format(SCA_VALID) <<
+            " mnTopCellRow " << mnTopCellRow << " length " << mnGroupLen);
 }
 
 ScRange FormulaGroupAreaListener::getListeningRange() const
@@ -116,7 +134,7 @@ void FormulaGroupAreaListener::Query( QueryBase& rQuery ) const
     {
         case SC_LISTENER_QUERY_FORMULA_GROUP_RANGE:
         {
-            ScFormulaCell* pTop = *mppTopCell;
+            const ScFormulaCell* pTop = getTopCell();
             ScRange aRange(pTop->aPos);
             aRange.aEnd.IncRow(mnGroupLen-1);
             QueryRange& rQR = static_cast<QueryRange&>(rQuery);
@@ -164,8 +182,17 @@ void FormulaGroupAreaListener::collectFormulaCells(
 void FormulaGroupAreaListener::collectFormulaCells(
     SCROW nRow1, SCROW nRow2, std::vector<ScFormulaCell*>& rCells ) const
 {
-    ScFormulaCell** pp = mppTopCell;
-    ScFormulaCell** ppEnd = pp + mnGroupLen;
+    SAL_INFO( "sc.core.grouparealistener",
+            "FormulaGroupAreaListener::collectFormulaCells() this " << this <<
+            " range " << maRange.Format(SCA_VALID) <<
+            " mnTopCellRow " << mnTopCellRow << " length " << mnGroupLen);
+
+    ScFormulaCell* const * pp = mpColumn->GetFormulaCellBlockAddress( mnTopCellRow);
+    assert(pp);
+    if (!pp)
+        return;
+
+    ScFormulaCell* const * ppEnd = pp + mnGroupLen;
 
     if (mbStartFixed)
     {
@@ -247,8 +274,15 @@ void FormulaGroupAreaListener::collectFormulaCells(
 
 ScAddress FormulaGroupAreaListener::getTopCellPos() const
 {
-    const ScFormulaCell& rFC = **mppTopCell;
-    return rFC.aPos;
+    const ScFormulaCell* p = getTopCell();
+    return p ? p->aPos : ScAddress();
+}
+
+const ScFormulaCell* FormulaGroupAreaListener::getTopCell() const
+{
+    const ScFormulaCell* const * pp = mpColumn->GetFormulaCellBlockAddress( mnTopCellRow);
+    assert(pp);
+    return pp ? *pp : NULL;
 }
 
 const ScRange& FormulaGroupAreaListener::getRange() const
