ff424e420116b2123b62f7869af25d7caec65288
Caolán McNamara <caolanm@redhat.com>
2015-01-30 15:34:30 +0000

    During DocumentRedlineManager::SetRedlineMode the array becomes unsorted
    so GetPos cannot be used safely, so pass down the known index
    of the redline and propogate it everywhere the redline goes
    
    This reverts
    
    commit 36e158ce7a0effb130936ba4598a193102faa6a1
    Author: Caolán McNamara <caolanm@redhat.com>
    Date:   Mon Jan 19 12:09:17 2015 +0000
    
        if we change the keys we have to resort based on the new keys
    
    which tried to keep the table sorted, but thats no use because
    DocumentRedlineManager::SetRedlineMode loops over by index
    so sorting the table during the process busts that.
    
    Taking a copy of the entries and looping over that shows another
    gadzillion problems.
    
    So try this approach instead.
    
    I imagine it should be possible to calculate the correct
    current index of pRedl in DocumentRedlineManager::AppendRedline
    but for now assume that we are sorted correctly at that
    point and can use GetPos
    
    Change-Id: If092dce185e3b36fd256db390132358cba155847
    (cherry picked from commit a5a20187c3a5e5956492f932c49501f9547e4915)
    Reviewed-on: https://gerrit.libreoffice.org/14260
    Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
    Tested-by: Miklos Vajna <vmiklos@collabora.co.uk>

--- sw/inc/docary.hxx
+++ sw/inc/docary.hxx
@@ -209,6 +209,7 @@ public:
     using _SwRedlineTbl::size;
     using _SwRedlineTbl::operator[];
     using _SwRedlineTbl::empty;
+    using _SwRedlineTbl::Resort;
 };
 
 /// Table that holds 'extra' redlines, such as 'table row insert\delete', 'paragraph moves' etc...
