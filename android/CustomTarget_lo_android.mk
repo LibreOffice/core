bfceafa67ed1cc3e4e03f8f214a2716f57b2d1e7
Tor Lillqvist <tml@collabora.com>
2015-02-01 13:43:55 +0100

    Bin obsolete comments

--- android/CustomTarget_lo_android.mk
+++ android/CustomTarget_lo_android.mk
@@ -13,18 +13,11 @@ loandroid3_DIR := $(call gb_CustomTarget_get_workdir,android/experimental/LOAndr
 $(call gb_CustomTarget_get_target,android/loandroid3) : \
 	$(loandroid3_DIR)/done
 
-# We want that to be built completely first,
-# so that we can serialize Ant access to Bootstrap, which is used both
-# by DocumentLoader. We don't want one Ant to be cleaning
-# out Bootstrap while another is building stuff that depends on it.
-# Yeah, this sucks
-
 $(loandroid3_DIR)/done : $(call gb_Postprocess_get_target,AllModulesButInstsetNative)
 	$(call gb_Output_announce,$(subst $(WORKDIR)/,,$@),$(true),MAK,2)
 	cd $(SRCDIR)/android/experimental/LOAndroid3 && $(MAKE) all
 # Copy to $(BUILDDIR)/instsetoo_native as that is where the tinderbox build script
-# still looks for the .apk, and we want fresh daily builds to be uploaded. Even if
-# the apps as such are mostly useless.
+# still looks for the .apk, and we want fresh daily builds to be uploaded.
 # Us "foo" instead of the old INPATH
 	mkdir -p $(BUILDDIR)/instsetoo_native/foo/bin; \
 	cp $(SRCDIR)/android/experimental/LOAndroid3/bin/*-debug.apk $(BUILDDIR)/instsetoo_native/foo/bin
