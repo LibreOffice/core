74a8f7225eca591f922f20cdf354c112e803f246
Jacobo Aragunde Pérez <jaragunde@igalia.com>
2015-02-02 19:49:37 +0000

    Android: add ownCloud library to the build.
    Library code from https://github.com/jaragunde/owncloud-android-library
    
    This patch downloads, builds and installs the library from a tarball
    uploaded to http://dev-www.libreoffice.org/src/.
    
    Reviewed-on: https://gerrit.libreoffice.org/16190
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Jacobo Aragunde Pérez <jaragunde@igalia.com>
    (cherry picked from commit e6fe508d6835590c6245e62e2c014808453e7d92)
    
    Change-Id: I28afaea4dabe2dab869b53b1881f4d5a6522943f

--- external/Module_external.mk
+++ external/Module_external.mk
@@ -80,6 +80,7 @@ $(eval $(call gb_Module_add_moduledirs,external,\
 	$(call gb_Helper_optional,OPENLDAP,openldap) \
 	$(call gb_Helper_optional,OPENSSL,openssl) \
 	$(call gb_Helper_optional,ORCUS,liborcus) \
+	$(call gb_Helper_optional,OWNCLOUD_ANDROID_LIB,owncloud-android-lib) \
 	$(call gb_Helper_optional,PAGEMAKER,libpagemaker) \
 	$(call gb_Helper_optional,POPPLER,poppler) \
 	$(call gb_Helper_optional,POSTGRESQL,postgresql) \
