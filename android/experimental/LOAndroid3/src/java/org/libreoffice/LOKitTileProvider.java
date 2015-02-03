1eb411480643cbba748b268e5c760552253b2f8a
Jan Holesovsky <kendy@collabora.com>
2015-02-03 11:44:54 +0100

    android: Always log the SAL_WARN's.
    Change-Id: Iab470097cc09835efd5ab3c13a0defb267c985a0

--- android/experimental/LOAndroid3/src/java/org/libreoffice/LOKitTileProvider.java
+++ android/experimental/LOAndroid3/src/java/org/libreoffice/LOKitTileProvider.java
@@ -46,7 +46,7 @@ public class LOKitTileProvider implements TileProvider, Document.MessageCallback
         mTileWidth = pixelToTwip(TILE_SIZE, mDPI);
         mTileHeight = pixelToTwip(TILE_SIZE, mDPI);
 
-        LibreOfficeKit.putenv("SAL_LOG=-WARN+INFO.lok");
+        LibreOfficeKit.putenv("SAL_LOG=+WARN+INFO.lok");
         LibreOfficeKit.init(LibreOfficeMainActivity.mAppContext);
 
         mOffice = new Office(LibreOfficeKit.getLibreOfficeKitHandle());
@@ -66,7 +66,7 @@ public class LOKitTileProvider implements TileProvider, Document.MessageCallback
             mOffice = new Office(handle);
             Log.i(LOGTAG, "====> new Office created");
             mDocument = mOffice.documentLoad(input);
-            LibreOfficeKit.putenv("SAL_LOG=-WARN+INFO.lok");
+            LibreOfficeKit.putenv("SAL_LOG=+WARN+INFO.lok");
         }
 
         Log.i(LOGTAG, "====> mDocument = " + mDocument);
