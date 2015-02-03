LibreOffice/core

commit 8a7a9992906ba59d575ca9e4441b3e84fea5fae0

String to OUString
Change-Id: I8ff61895eac1dc9714de5ded6e80c06e4ce5357b

sw/source/ui/fldui/changedb.cxx
@@ -205,20 +205,22 @@ void SwChangeDBDlg::UpdateFlds()
     }
 
     pSh->StartAllAction();
-    String sTableName, sColumnName;
+    OUString sTableName;
+    OUString sColumnName;
     sal_Bool bIsTable = sal_False;
-    String sTemp(m_pAvailDBTLB->GetDBName(sTableName, sColumnName, &bIsTable));
-    sTemp += DB_DELIM;
-    sTemp += sTableName;
-    sTemp += DB_DELIM;
-    sTemp += bIsTable ? '0' : '1';
+    const OUString sTemp = m_pAvailDBTLB->GetDBName(sTableName, sColumnName, &bIsTable)
+        + OUString(DB_DELIM)
+        + sTableName
+        + OUString(DB_DELIM)
+        + OUString(static_cast<sal_Unicode>(bIsTable ? '0' : '1'));
     pSh->ChangeDBFields( aDBNames, sTemp);
     pSh->EndAllAction();
 }
 
 IMPL_LINK_NOARG(SwChangeDBDlg, ButtonHdl)
 {
-    String sTableName, sColumnName;
+    OUString sTableName;
+    OUString sColumnName;
     SwDBData aData;
     sal_Bool bIsTable = sal_False;
     aData.sDataSource = m_pAvailDBTLB->GetDBName(sTableName, sColumnName, &bIsTable);

sw/source/ui/dbui/dbtree.cxx
@@ -245,8 +245,9 @@ void SwDBTreeList::ShowColumns(sal_Bool bShowCol)
     if (bShowCol != bShowColumns)
     {
         bShowColumns = bShowCol;
-        String sTableName, sColumnName;
-        String  sDBName(GetDBName(sTableName, sColumnName));
+        OUString sTableName;
+        OUString sColumnName;
+        const OUString sDBName(GetDBName(sTableName, sColumnName));
 
         SetUpdateMode(sal_False);
 
@@ -264,7 +265,7 @@ void SwDBTreeList::ShowColumns(sal_Bool bShowCol)
             pEntry = Next(pEntry);
         }
 
-        if (sDBName.Len())
+        if (!sDBName.isEmpty())
         {
             Select(sDBName, sTableName, sColumnName);   // force RequestingChildren
         }
@@ -408,9 +409,9 @@ IMPL_LINK( SwDBTreeList, DBCompare, SvSortData*, pData )
     return DefaultCompare(pData);   // otherwise call base class
 }
 
-String  SwDBTreeList::GetDBName(String& rTableName, String& rColumnName, sal_Bool* pbIsTable)
+OUString SwDBTreeList::GetDBName(OUString& rTableName, OUString& rColumnName, sal_Bool* pbIsTable)
 {
-    String sDBName;
+    OUString sDBName;
     SvTreeListEntry* pEntry = FirstSelected();
 
     if (pEntry && GetParent(pEntry))
@@ -477,13 +478,14 @@ void SwDBTreeList::Select(const String& rDBName, const String& rTableName, const
 
 void SwDBTreeList::StartDrag( sal_Int8 /*nAction*/, const Point& /*rPosPixel*/ )
 {
-    String sTableName, sColumnName;
-    String  sDBName( GetDBName( sTableName, sColumnName ));
-    if( sDBName.Len() )
+    OUString sTableName;
+    OUString sColumnName;
+    OUString sDBName( GetDBName( sTableName, sColumnName ));
+    if (!sDBName.isEmpty())
     {
         TransferDataContainer* pContainer = new TransferDataContainer;
         ::com::sun::star::uno::Reference< ::com::sun::star::datatransfer::XTransferable > xRef( pContainer );
-        if( sColumnName.Len() )
+        if( !sColumnName.isEmpty() )
         {
             // drag database field
             svx::OColumnTransferable aColTransfer(
@@ -496,12 +498,10 @@ void SwDBTreeList::StartDrag( sal_Int8 /*nAction*/, const Point& /*rPosPixel*/ )
             aColTransfer.addDataToContainer( pContainer );
         }
 
-        sDBName += '.';
-        sDBName += sTableName;
-        if( sColumnName.Len() )
+        sDBName += "." + sTableName;
+        if (!sColumnName.isEmpty())
         {
-            sDBName += '.';
-            sDBName += sColumnName;
+            sDBName += "." + sColumnName;
         }
 
         pContainer->CopyString( FORMAT_STRING, sDBName );

sw/source/ui/fldui/flddb.cxx
@@ -129,7 +129,7 @@ void SwFldDBPage::Reset(const SfxItemSet&)
         if (nOldPos != LISTBOX_ENTRY_NOTFOUND)
             m_pTypeLB->SelectEntryPos(nOldPos);
 
-        if (sOldDBName.Len())
+        if (!sOldDBName.isEmpty())
         {
             m_pDatabaseTLB->Select(sOldDBName, sOldTableName, sOldColumnName);
         }
@@ -182,7 +182,8 @@ void SwFldDBPage::Reset(const SfxItemSet&)
 
 sal_Bool SwFldDBPage::FillItemSet(SfxItemSet& )
 {
-    String sTableName, sColumnName;
+    OUString sTableName;
+    OUString sColumnName;
     SwDBData aData;
     sal_Bool bIsTable;
     aData.sDataSource = m_pDatabaseTLB->GetDBName(sTableName, sColumnName, &bIsTable);
@@ -198,23 +199,20 @@ sal_Bool SwFldDBPage::FillItemSet(SfxItemSet& )
     if(!aData.sDataSource.isEmpty())       // without database no new field command
     {
         sal_uInt16 nTypeId = (sal_uInt16)(sal_uLong)m_pTypeLB->GetEntryData(GetTypeSel());
-        String aVal(m_pValueED->GetText());
-        String aName(m_pConditionED->GetText());
         sal_uLong nFormat = 0;
         sal_uInt16 nSubType = 0;
 
-        String sDBName = aData.sDataSource;
-        sDBName += DB_DELIM;
-        sDBName += (String)aData.sCommand;
-        sDBName += DB_DELIM;
-        sDBName += OUString::number(aData.nCommandType);
-        sDBName += DB_DELIM;
-        if(sColumnName.Len())
+        OUString sDBName = aData.sDataSource
+            + OUString(DB_DELIM)
+            + aData.sCommand
+            + OUString(DB_DELIM)
+            + OUString::number(aData.nCommandType)
+            + OUString(DB_DELIM);
+        if (!sColumnName.isEmpty())
         {
-            sDBName += sColumnName;
-            sDBName += DB_DELIM;
+            sDBName += sColumnName + OUString(DB_DELIM);
         }
-        aName.Insert(sDBName, 0);
+        OUString aName = sDBName + m_pConditionED->GetText();
 
         switch (nTypeId)
         {
@@ -231,9 +229,10 @@ sal_Bool SwFldDBPage::FillItemSet(SfxItemSet& )
             break;
         }
 
-
-        String sTempDBName, sTempTableName, sTempColumnName;
-        sTempDBName = m_pDatabaseTLB->GetDBName(sTempTableName, sTempColumnName);
+        const OUString aVal(m_pValueED->GetText());
+        OUString sTempTableName;
+        OUString sTempColumnName;
+        OUString sTempDBName = m_pDatabaseTLB->GetDBName(sTempTableName, sTempColumnName);
         sal_Bool bDBListBoxChanged = sOldDBName != sTempDBName ||
             sOldTableName != sTempTableName || sOldColumnName != sTempColumnName;
         if (!IsFldEdit() ||
@@ -440,10 +439,10 @@ IMPL_LINK( SwFldDBPage, TreeSelectHdl, SvTreeListBox *, pBox )
 
             if (pEntry != 0)
             {
-                String sTableName;
-                String sColumnName;
+                OUString sTableName;
+                OUString sColumnName;
                 sal_Bool bIsTable;
-                String sDBName = m_pDatabaseTLB->GetDBName(sTableName, sColumnName, &bIsTable);
+                OUString sDBName = m_pDatabaseTLB->GetDBName(sTableName, sColumnName, &bIsTable);
                 bNumFormat = GetFldMgr().IsDBNumeric(sDBName,
                             sTableName,
                             bIsTable,

sw/source/ui/fldui/flddb.hxx
@@ -47,9 +47,9 @@ class SwFldDBPage : public SwFldPage
     ListBox*            m_pFormatLB;
     VclContainer*       m_pFormat;
 
-    String              sOldDBName;
-    String              sOldTableName;
-    String              sOldColumnName;
+    OUString            sOldDBName;
+    OUString            sOldTableName;
+    OUString            sOldColumnName;
     sal_uLong               nOldFormat;
     sal_uInt16              nOldSubType;
     Link                aOldNumSelectHdl;

sw/source/ui/inc/dbtree.hxx
@@ -56,7 +56,7 @@ public:
     virtual ~SwDBTreeList();
     virtual Size GetOptimalSize() const;
 
-    String  GetDBName( String& rTableName, String& rColumnName, sal_Bool* pbIsTable = 0);
+    OUString GetDBName( OUString& rTableName, OUString& rColumnName, sal_Bool* pbIsTable = 0);
 
     void    Select( const String& rDBName, const String& rTableName,
                     const String& rColumnName );

commit fee7119bd6456db13d72e6bd3b60cdbbe56504cb

tdf#77241: sw: fix creation of field data source string
SwChangeDBDlg::UpdateFlds() depends on undefined behaviour in
constructing sTemp, because the call to GetDBName initializes local
variables that are already used in other sub-expressions of the
statement.

(regression from 8a7a9992906ba59d575ca9e4441b3e84fea5fae0)

Change-Id: Ibf8c0091da672e133d7a35bc61c059eaf65a3bc2

sw/source/ui/fldui/changedb.cxx
@@ -192,7 +192,8 @@ void SwChangeDBDlg::UpdateFlds()
     OUString sTableName;
     OUString sColumnName;
     sal_Bool bIsTable = sal_False;
-    const OUString sTemp = m_pAvailDBTLB->GetDBName(sTableName, sColumnName, &bIsTable)
+    const OUString DBName(m_pAvailDBTLB->GetDBName(sTableName, sColumnName, &bIsTable));
+    const OUString sTemp = DBName
         + OUString(DB_DELIM)
         + sTableName
         + OUString(DB_DELIM)