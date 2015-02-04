3c50f98dc266b42ffe9cdc8843d21e4a47a16bcb
Tor Lillqvist <tml@collabora.com>
2015-02-04 16:19:38 +0200

    Check number of cells referenced by group instead of group size
    Put using it in #if 0 for now, though.
    
    This reverts commit 1fd902d4b851c534cf7473dd13983fc2c46500ed
    
    Change-Id: I7bb3efdc594bf7f1a61b037bf3488dae4ca119c9
    Reviewed-by: Michael Meeks <michael.meeks@collabora.com>
    Tested-by: Michael Meeks <michael.meeks@collabora.com>

--- sc/source/core/data/formulacell.cxx
+++ sc/source/core/data/formulacell.cxx
@@ -3750,7 +3750,7 @@ bool ScFormulaCell::InterpretFormulaGroup()
     if (mxGroup->meCalcState == sc::GroupCalcDisabled)
         return false;
 
-    if (GetSharedLength() < ScInterpreter::GetGlobalConfig().mnOpenCLMinimumFormulaGroupSize)
+    if (GetWeight() < ScInterpreter::GetGlobalConfig().mnOpenCLMinimumFormulaGroupSize)
     {
         mxGroup->meCalcState = sc::GroupCalcDisabled;
         return false;
@@ -4126,6 +4126,17 @@ SCROW ScFormulaCell::GetSharedTopRow() const
 SCROW ScFormulaCell::GetSharedLength() const
 {
     return mxGroup ? mxGroup->mnLength : 0;
+}
+
+sal_Int32 ScFormulaCell::GetWeight() const
+{
+#if 0
+    if (!mxGroup)
+        return pCode->GetWeight();
+    return GetSharedLength() * GetSharedCode()->GetWeight();
+#else
+    return GetSharedLength();
+#endif
 }
 
 ScTokenArray* ScFormulaCell::GetSharedCode()
