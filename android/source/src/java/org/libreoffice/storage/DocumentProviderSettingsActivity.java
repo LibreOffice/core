c5f92a9f698e1f43171eeb195b522f83b39dd66f
Jacobo Aragunde Pérez <jaragunde@igalia.com>
2015-02-09 12:37:46 +0100

    Android: settings activity for document providers
    Creates an activity to manage the settings of any document provider
    that needs them, and populated it with the settings required by the
    ownCloud implementation.
    
    The settings screen is not yet plugged to the document browser UI but
    can be launched with this command:
    
      adb shell am start -a android.intent.action.MAIN \
      -n org.libreoffice/.storage.DocumentProviderSettingsActivity
    
    Change-Id: I83cff641fa61078f2bddbb98262af989c06985a9
    Reviewed-on: https://gerrit.libreoffice.org/16193
    Reviewed-by: Jacobo Aragunde Pérez <jaragunde@igalia.com>
    Tested-by: Jacobo Aragunde Pérez <jaragunde@igalia.com>
    (cherry picked from commit d5c9d1dec60085f97eb07402eefcc3fd7a989877)

---
+++ android/source/src/java/org/libreoffice/storage/DocumentProviderSettingsActivity.java
@@ -0,0 +1,37 @@
+/* -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package org.libreoffice.storage;
+
+import org.libreoffice.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+public class DocumentProviderSettingsActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Display the fragment as the main content.
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment()).commit();
+    }
+
+    public static class SettingsFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.documentprovider_preferences);
+        }
+    }
+}
