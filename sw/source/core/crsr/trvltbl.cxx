9665911b79b75a82d1c287826087c717b8158976
Michael Stahl <mstahl@redhat.com>
2015-02-10 16:30:57 +0100

    coverity#1267654: sw: logically dead code in SwCrsrShell::GoNextCell()
    IsTableMode() should imply cursor is in table.
    
    Change-Id: Ibb94911430f947038abba2ebe013fc82fae7dd54

--- sw/source/core/crsr/trvltbl.cxx
+++ sw/source/core/crsr/trvltbl.cxx
@@ -65,8 +65,6 @@ bool SwCrsrShell::GoNextCell( bool bAppendLine )
                 if ( !pTblNd )
                     pTblNd = IsCrsrInTbl();
                 assert (pTblNd);
-                if (!pTblNd)
-                    return false;
                 pTableBox = & pTableBox->FindEndOfRowSpan( pTblNd->GetTable(),
                                                            (sal_uInt16)(pTableBox->getRowSpan() + pCrsr->GetCrsrRowSpanOffset() ) );
                 pTableBoxStartNode = pTableBox->GetSttNd();
