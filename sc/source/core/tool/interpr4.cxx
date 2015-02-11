3f7558360c4805076a1705c204c9e8dba12b7358
Winfried Donkers <winfrieddonkers@libreoffice.org>
2015-02-11 09:32:09 +0100

    tdf#88576 fix handling of empty arguments in IF(), IFERROR() and IFNA()
    Reviewed-on: https://gerrit.libreoffice.org/14415
    Reviewed-by: Eike Rathke <erack@redhat.com>
    Tested-by: Eike Rathke <erack@redhat.com>
    (cherry picked from commit 0b75eda1090f92adc678ceff2565da2dc7d9328c)
    
    tdf#88576 check that two parameters are given for IFERROR() and IFNA()
    
    Otherwise the functions failed only for the error case resulting in
     #NULL! error.
    
    (cherry picked from commit 6583f4e30015164af4972921b5bb7880dfb65f65)
    
    eb987637698ab418fc0a60cd873e23878c9f497b
    
    Change-Id: I8acca26cf7398768a9e25f97f3a9e61754ab2179
    Reviewed-on: https://gerrit.libreoffice.org/14421
    Tested-by: Eike Rathke <erack@redhat.com>
    Reviewed-by: Eike Rathke <erack@redhat.com>

--- sc/source/core/tool/interpr4.cxx
+++ sc/source/core/tool/interpr4.cxx
@@ -3025,7 +3025,10 @@ void ScInterpreter::ScExternal()
 
 void ScInterpreter::ScMissing()
 {
-    PushTempToken( new FormulaMissingToken );
+    if ( aCode.IsEndOfPath() )
+        PushTempToken( new ScEmptyCellToken( false, false ) );
+    else
+        PushTempToken( new FormulaMissingToken );
 }
 
 #if HAVE_FEATURE_SCRIPTING
