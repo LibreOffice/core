5d388443b9809e73202ce551bcffa10d442094ca
Jan Holesovsky <kendy@collabora.com>
2015-02-12 20:04:52 +0100

    Blind attempt to fix the Windows build.
    Change-Id: Iac7b066e3af311f71bbcff7ac418fd3b5e94dff2

--- chart2/source/tools/CachedDataSequence.cxx
+++ chart2/source/tools/CachedDataSequence.cxx
@@ -398,7 +398,7 @@ extern "C" SAL_DLLPUBLIC_EXPORT css::uno::XInterface * SAL_CALL
 com_sun_star_comp_chart_CachedDataSequence_get_implementation(css::uno::XComponentContext *context,
         css::uno::Sequence<css::uno::Any> const &)
 {
-    return cppu::acquire(new chart::CachedDataSequence(context));
+    return cppu::acquire(new ::chart::CachedDataSequence(context));
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
