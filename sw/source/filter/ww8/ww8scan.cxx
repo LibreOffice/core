b31fbe0dc48be70b3efbc7a7a3aecbffd1c73273
Caolán McNamara <caolanm@redhat.com>
2015-02-17 15:52:39 +0000

    asan: failure to load tdf#55620 attachment under asan
    still loops in layout though
    
    how am I *still* finding bugs in this thing
    
    Change-Id: I048595956516b9acfc60e90c64180b904d6cc1d7
    (cherry picked from commit d6b28f0bfe5df0248b18e306c3144282f052c7f0)
    Reviewed-on: https://gerrit.libreoffice.org/14524
    Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
    Tested-by: Miklos Vajna <vmiklos@collabora.co.uk>

--- sw/source/filter/ww8/ww8scan.cxx
+++ sw/source/filter/ww8/ww8scan.cxx
@@ -5206,6 +5206,7 @@ void WW8PLCFxDesc::Restore( const WW8PLCFxSave1& rSave )
             pPLCFx->SetDirty(false);
             aD.ReduceByOffset();
             pMemPos = aD.pMemPos + rSave.nPLCFxMemOfs;
+            nSprmsLen = nOrigSprmsLen - rSave.nPLCFxMemOfs;
         }
     }
 }
