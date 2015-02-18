420fc7d1ada2c86f27e54c515c21abb88b4cd91f
Stephan Bergmann <sbergman@redhat.com>
2015-02-18 09:59:08 +0100

    Missing include
    Change-Id: I067a83ef97e55afc27673c59b0bdae974d7f3232

--- xmloff/source/xforms/TokenContext.cxx
+++ xmloff/source/xforms/TokenContext.cxx
@@ -17,6 +17,10 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
+#include <sal/config.h>
+
+#include <functional>
+
 #include "TokenContext.hxx"
 #include <xmloff/xmltkmap.hxx>
 #include <xmloff/xmlimp.hxx>
