4801b29677462f7206fa281f06646e5cd8d81bfa
Gabriele Bulfon <gabriele.bulfon@sonicle.com>
2015-02-19 14:36:39 +0100

    Adapt gcc3_solaris_intel bridge to GCC 4.7
    ...similarly to 0fdbb5b0eabbaa571f3747fda12a56c938cba474 "Make
    cpp_uno/gcc3_linux_x86-64 bridge work with GCC 4.7"
    
    Change-Id: Idcafcb07678d02446172d7fde30631a342f6437e
    (cherry picked from commit 834afd885bd74ff80b59898d3f63ba940a58c1d8)
    Signed-off-by: Michael Stahl <mstahl@redhat.com>

--- bridges/source/cpp_uno/gcc3_solaris_intel/uno2cpp.cxx
+++ bridges/source/cpp_uno/gcc3_solaris_intel/uno2cpp.cxx
@@ -32,94 +32,12 @@
 
 #include "share.hxx"
 
+#include "callvirtualmethod.hxx"
+
 using namespace ::com::sun::star::uno;
 
 namespace
 {
-
-static void  __attribute__((noinline)) callVirtualMethod(
-    void * pAdjustedThisPtr,
-    sal_Int32 nVtableIndex,
-    void * pRegisterReturn,
-    typelib_TypeClass eReturnType,
-    sal_Int32 * pStackLongs,
-    sal_Int32 nStackLongs )
-{
-    // parameter list is mixed list of * and values
-    // reference parameters are pointers
-
-    assert(pStackLongs && pAdjustedThisPtr);
-    static_assert((sizeof(void *) == 4) && (sizeof(sal_Int32) == 4), "### unexpected size of int!");
-    assert(nStackLongs && pStackLongs && "### no stack in callVirtualMethod !");
-
-    // never called
-    if (! pAdjustedThisPtr) CPPU_CURRENT_NAMESPACE::dummy_can_throw_anything("xxx"); // address something
-
-    long edx, eax; // for register returns
-    void * stackptr;
-    asm volatile (
-        "mov   %%esp, %2\n\t"
-       // preserve potential 128bit stack alignment
-        "and   $0xfffffff0, %%esp\n\t"
-        "mov   %3, %%eax\n\t"
-        "lea   -4(,%%eax,4), %%eax\n\t"
-        "and   $0xf, %%eax\n\t"
-        "sub   $0xc, %%eax\n\t"
-        "add   %%eax, %%esp\n\t"
-        // copy values
-        "mov   %3, %%eax\n\t"
-        "mov   %%eax, %%edx\n\t"
-        "dec   %%edx\n\t"
-        "shl   $2, %%edx\n\t"
-        "add   %4, %%edx\n"
-        "Lcopy:\n\t"
-        "pushl 0(%%edx)\n\t"
-        "sub   $4, %%edx\n\t"
-        "dec   %%eax\n\t"
-        "jne   Lcopy\n\t"
-        // do the actual call
-        "mov   %5, %%edx\n\t"
-        "mov   0(%%edx), %%edx\n\t"
-        "mov   %6, %%eax\n\t"
-        "shl   $2, %%eax\n\t"
-        "add   %%eax, %%edx\n\t"
-        "mov   0(%%edx), %%edx\n\t"
-        "call  *%%edx\n\t"
-        // save return registers
-         "mov   %%eax, %0\n\t"
-         "mov   %%edx, %1\n\t"
-        // cleanup stack
-        "mov   %2, %%esp\n\t"
-        : "=m"(eax), "=m"(edx), "=m"(stackptr)
-        : "m"(nStackLongs), "m"(pStackLongs), "m"(pAdjustedThisPtr), "m"(nVtableIndex)
-        : "eax", "ecx", "edx" );
-    switch( eReturnType )
-    {
-        case typelib_TypeClass_HYPER:
-        case typelib_TypeClass_UNSIGNED_HYPER:
-            ((long*)pRegisterReturn)[1] = edx;
-        case typelib_TypeClass_LONG:
-        case typelib_TypeClass_UNSIGNED_LONG:
-        case typelib_TypeClass_CHAR:
-        case typelib_TypeClass_ENUM:
-            ((long*)pRegisterReturn)[0] = eax;
-            break;
-        case typelib_TypeClass_SHORT:
-        case typelib_TypeClass_UNSIGNED_SHORT:
-            *(unsigned short*)pRegisterReturn = eax;
-            break;
-        case typelib_TypeClass_BOOLEAN:
-        case typelib_TypeClass_BYTE:
-            *(unsigned char*)pRegisterReturn = eax;
-            break;
-        case typelib_TypeClass_FLOAT:
-            asm ( "fstps %0" : : "m"(*(char *)pRegisterReturn) );
-            break;
-        case typelib_TypeClass_DOUBLE:
-            asm ( "fstpl %0\n\t" : : "m"(*(char *)pRegisterReturn) );
-            break;
-    }
-}
 
 static void cpp_call(
     bridges::cpp_uno::shared::UnoInterfaceProxy * pThis,
@@ -234,7 +152,7 @@ static void cpp_call(
     try
     {
         assert( !( (pCppStack - pCppStackStart ) & 3) && "UNALIGNED STACK !!! (Please DO panic)");
-        callVirtualMethod(
+        CPPU_CURRENT_NAMESPACE::callVirtualMethod(
             pAdjustedThisPtr, aVtableSlot.index,
             pCppReturn, pReturnTypeDescr->eTypeClass,
             (sal_Int32 *)pCppStackStart, (pCppStack - pCppStackStart) / sizeof(sal_Int32) );
