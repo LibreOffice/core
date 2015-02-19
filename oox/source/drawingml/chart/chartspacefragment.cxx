3b44fc8c4b4a4469ae1e4d511d2c0df15c026c4e
Markus Mohrhard <markus.mohrhard@collabora.co.uk>
2015-02-19 01:32:54 +0100

    document one more place where MSO 2007 export wrong values
    (cherry picked from commit 5fb41ae3920cd635fa42057e1623aa21bcb53167)
    
    Conflicts:
    	oox/source/drawingml/chart/chartspacefragment.cxx
    
    Change-Id: Ic35f1188e66cf89f203b34f354a2c4b11f1f65a2

--- oox/source/drawingml/chart/chartspacefragment.cxx
+++ oox/source/drawingml/chart/chartspacefragment.cxx
@@ -81,8 +81,7 @@ ContextHandlerRef ChartSpaceFragment::onCreateContext( sal_Int32 nElement, const
             switch( nElement )
             {
                 case C_TOKEN( autoTitleDeleted ):
-                    // default is 'false', not 'true' as specified
-                    mrModel.mbAutoTitleDel = rAttribs.getBool( XML_val, false );
+                    mrModel.mbAutoTitleDel = rAttribs.getBool( XML_val, true ); // TODO: OOXML_spec
                     return 0;
                 case C_TOKEN( backWall ):
                     return new WallFloorContext( *this, mrModel.mxBackWall.create() );
