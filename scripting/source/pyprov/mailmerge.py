2d37268d623a59a4797c33bcb6b9bb15f24a7fd8
Julien Nabet <serval2412@yahoo.fr>
2015-02-21 16:15:27 +0100

    Resolves tdf#63388: Mailmerge with SSL
    Thanks to suggestions from:
    - Andrea Tessadri for smtplib.SMTP_SSL when using SSL
    - Florian Schmitt for self.server.use_ssl = True (SMTPS instead of starttls)
    
    Change-Id: I9eeb6e9c94780c754b2f7da29d4c67d65d92fde3

--- scripting/source/pyprov/mailmerge.py
+++ scripting/source/pyprov/mailmerge.py
@@ -97,7 +97,10 @@ class PyMailSMTPService(unohelper.Base, XSmtpService):
 			tout = _GLOBAL_DEFAULT_TIMEOUT
 		if dbg:
 			print("Timeout: " + str(tout), file=dbgout)
-		self.server = smtplib.SMTP(server, port,timeout=tout)
+		if connectiontype.upper() == 'SSL':
+			self.server = smtplib.SMTP_SSL(server, port,timeout=tout)
+		else:
+			self.server = smtplib.SMTP(server, port,timeout=tout)
 
 		#stderr not available for us under windows, but
 		#set_debuglevel outputs there, and so throw
@@ -111,7 +114,11 @@ class PyMailSMTPService(unohelper.Base, XSmtpService):
 			print("ConnectionType: " + connectiontype, file=dbgout)
 		if connectiontype.upper() == 'SSL':
 			self.server.ehlo()
-			self.server.starttls()
+			try:
+				self.server.starttls()
+			except:
+				# older use, see http://en.wikipedia.org/wiki/SMTPS
+				self.server.use_ssl = True
 			self.server.ehlo()
 
 		user = xAuthenticator.getUserName()
