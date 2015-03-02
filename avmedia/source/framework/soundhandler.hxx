61e213a27afbaf7094c31f1d6ee457d0819bedc5
Chris Sherlock <chris.sherlock@collabora.com>
2015-03-02 17:09:28 +1100

    avmedia: use constructor syntax for avmedia.component
    Change-Id: Ia4572e10fb97a5e88f08341d8a1c8af87e68904c

--- avmedia/source/framework/soundhandler.hxx
+++ avmedia/source/framework/soundhandler.hxx
@@ -91,9 +91,6 @@ class SoundHandler  :   // interfaces
     /* Helper for XServiceInfo */
        static css::uno::Sequence< OUString >                          SAL_CALL impl_getStaticSupportedServiceNames(                                                                               );
        static OUString                                                SAL_CALL impl_getStaticImplementationName   (                                                                               );
-    /* Helper for registry */
-       static css::uno::Reference< css::uno::XInterface >             SAL_CALL impl_createInstance                ( const css::uno::Reference< css::lang::XMultiServiceFactory >& xServiceManager ) throw( css::uno::Exception );
-       static css::uno::Reference< css::lang::XSingleServiceFactory > SAL_CALL impl_createFactory                 ( const css::uno::Reference< css::lang::XMultiServiceFactory >& xServiceManager );
     /* Helper for initialization of service by using own reference! */
        void                                                   SAL_CALL impl_initService                   (                                                                               );
 
