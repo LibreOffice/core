69bc259abe1c9c8576179c7d764c03b5fbf239f2
Henry Castro <hcvcastro@gmail.com>
2015-03-04 17:26:05 -0400

    Resolves tdf#78221 Cannot drag twice the same cell when editing formula
    Change-Id: I69402778e68a2955bdda1ba2c9d31d9b10fb60cc
    Reviewed-on: https://gerrit.libreoffice.org/14748
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Andras Timar <andras.timar@collabora.com>
    (cherry picked from commit 3381cb4a421bf390445b7dac9ea42f9ccaf3d875)

--- sc/source/ui/app/rfindlst.cxx
+++ sc/source/ui/app/rfindlst.cxx
@@ -18,6 +18,7 @@
  */
 
 #include "rfindlst.hxx"
+#include <tools/debug.hxx>
 
 // STATIC DATA -----------------------------------------------------------
 
@@ -29,28 +30,67 @@ static const ColorData aColNames[SC_RANGECOLORS] =
 
 ScRangeFindList::ScRangeFindList(const OUString& rName) :
     aDocName( rName ),
-    bHidden( false )
+    bHidden( false ),
+    nIndexColor( 0 )
 {
 }
 
 ColorData ScRangeFindList::Insert( const ScRangeFindData &rNew )
 {
-    for(std::vector<ScRangeFindData>::iterator it=maEntries.begin(); it!=maEntries.end(); ++it)
+    std::vector<ScRangeFindData>::iterator it=maEntries.begin();
+    for( ; it!=maEntries.end(); ++it)
     {
         if(it->aRef == rNew.aRef)
-        {
-            return it->nColorData;
-        }
+            break;
     }
     ScRangeFindData insertData(rNew);
-    insertData.nColorData = aColNames[maEntries.size() % SC_RANGECOLORS];
+    insertData.nColorData = ( it != maEntries.end() ? it->nColorData :
+                              ScRangeFindList::GetColorName( maEntries.size() ) );
     maEntries.push_back(insertData);
+    nIndexColor = maEntries.size() - 1;
     return insertData.nColorData;
 }
 
 ColorData ScRangeFindList::GetColorName( const size_t nIndex )
 {
     return aColNames[nIndex % SC_RANGECOLORS];
+}
+
+ColorData ScRangeFindList::FindColor( const ScRange& rRef, const size_t nIndex )
+{
+    sal_Int32 nOldCntr = 0;
+    sal_Int32 nNewCntr = 0;
+    ColorData nOldColor = 0;
+    ColorData nNewColor = 0;
+
+    DBG_ASSERT( (nIndex < maEntries.size()), "nIndex out of range!" );
+
+    nOldColor = maEntries[nIndex].nColorData;
+    nNewColor = ScRangeFindList::GetColorName( nIndex );
+
+    std::vector<ScRangeFindData>::iterator it=maEntries.begin();
+    for( ;it!=maEntries.end(); ++it)
+    {
+        if(it->aRef == rRef)
+            break;
+
+        if (it->nColorData == nOldColor )
+            nOldCntr++;
+
+        if (it->nColorData == nNewColor )
+            nNewCntr++;
+    }
+
+    if ( it != maEntries.end() )
+        return it->nColorData;
+
+    if ( nOldCntr == 1 )
+        return nOldColor;
+
+    if ( nNewCntr > 0 )
+        return ScRangeFindList::GetColorName( ++nIndexColor );
+
+    return nNewColor;
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
