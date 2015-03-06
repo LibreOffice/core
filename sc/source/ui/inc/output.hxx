a812c8754572a0edb45777ec291b7453b45d6e16
Jan Holesovsky <kendy@collabora.com>
2015-03-06 22:00:15 +0100

    sc tiled rendering: Make the drawings and charts work.
    Change-Id: Ibd7e9e398fe24ec2b3553c8488b46b65de316da6

--- sc/source/ui/inc/output.hxx
+++ sc/source/ui/inc/output.hxx
@@ -60,6 +60,7 @@ class ScFieldEditEngine;
 class ScOutputData
 {
 friend class ScDrawStringsVars;
+friend class ScGridWindow;
 private:
     struct OutputAreaParam
     {
@@ -147,7 +148,6 @@ private:
     RowInfo* pRowInfo;          // Info block
     SCSIZE nArrCount;           // occupied lines in info block
     ScDocument* mpDoc;          // Document
-public:
     SCTAB nTab;                 // sheet
     long nScrX;                 // Output Startpos. (Pixel)
     long nScrY;
@@ -158,7 +158,6 @@ public:
     SCROW nY1;                  //  ( incl. hidden )
     SCCOL nX2;
     SCROW nY2;
-private:
     SCCOL nVisX1;               // Start-/End coordinates
     SCROW nVisY1;               //  ( visible range )
     SCCOL nVisX2;
