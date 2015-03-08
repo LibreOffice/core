83965d7f4ff1fb8e7bce6d5e0be8f610a07756b6
Julien Nabet <serval2412@yahoo.fr>
2015-03-08 13:39:42 +0100

    cppcheck: cstyleCast
    Change-Id: Ia51cea076e5368956bcd87dd201d15eafbf6ff24

--- starmath/source/edit.cxx
+++ starmath/source/edit.cxx
@@ -731,7 +731,7 @@ void SmEditWindow::LoseFocus()
 bool SmEditWindow::IsAllSelected() const
 {
     bool bRes = false;
-    EditEngine *pEditEngine = ((SmEditWindow *) this)->GetEditEngine();
+    EditEngine *pEditEngine = (const_cast<SmEditWindow *>(this))->GetEditEngine();
     OSL_ENSURE( pEditView, "NULL pointer" );
     OSL_ENSURE( pEditEngine, "NULL pointer" );
     if (pEditEngine  &&  pEditView)
@@ -945,7 +945,7 @@ void SmEditWindow::SetSelection(const ESelection &rSel)
 
 bool SmEditWindow::IsEmpty() const
 {
-    EditEngine *pEditEngine = ((SmEditWindow *) this)->GetEditEngine();
+    EditEngine *pEditEngine = (const_cast<SmEditWindow *>(this))->GetEditEngine();
     bool bEmpty = ( pEditEngine && pEditEngine->GetTextLen() == 0 );
     return bEmpty;
 }
