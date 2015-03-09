c9bae5c5f0b9219391e604a1d61126ea3d2c943c
Markus Mohrhard <markus.mohrhard@collabora.co.uk>
2015-03-10 00:34:07 +0100

    detect MSO 2007 OOXML documents
    (cherry picked from commit 15174177091367332b57cd79575e2f7dd27388b2)
    
    Conflicts:
    	oox/source/core/xmlfilterbase.cxx
    
    Conflicts:
    	oox/source/core/xmlfilterbase.cxx
    
    Change-Id: I4052c6f1e5dde71ce4cede1ec9a313f461861d71

--- include/oox/core/xmlfilterbase.hxx
+++ include/oox/core/xmlfilterbase.hxx
@@ -233,6 +233,8 @@ public:
 
     FastParser* createParser() const;
 
+    bool isMSO2007Document() const;
+
 protected:
     virtual ::com::sun::star::uno::Reference< ::com::sun::star::io::XInputStream >
         implGetInputStream( utl::MediaDescriptor& rMediaDesc ) const SAL_OVERRIDE;
@@ -252,6 +254,7 @@ private:
     ::std::auto_ptr< XmlFilterBaseImpl > mxImpl;
     sal_Int32 mnRelId;
     sal_Int32 mnMaxDocId;
+    bool mbMSO2007;
 };
 
 typedef ::rtl::Reference< XmlFilterBase > XmlFilterRef;
