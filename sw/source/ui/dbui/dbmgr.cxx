743966cdf34a85c01f42c303a341ef9a9ce7288d
Christoph Lutz <christoph.lutz_ml@cib.de>
2015-03-09 15:37:45 +0000

    Don't store temporary mail merge file if mail merge job was cancelled
    If a mail merge "to single file" is canceled, the temporary file was still saved.
    This needed a lot of time for large mail merge results (>20s) which is not needed,
    wanted and inteded. We want mail merge to return asap.
    
    Reviewed-on: https://gerrit.libreoffice.org/14811
    Reviewed-by: Michael Stahl <mstahl@redhat.com>
    Tested-by: Michael Stahl <mstahl@redhat.com>
    (cherry picked from commit 86a547e33bab2a89513385c5af535a6099526616)
    
    Conflicts:
    
    	sw/source/uibase/dbui/dbmgr.cxx
    
    Change-Id: I74e9048c7eb483766e7e677dd21167b7d844a518

--- sw/source/ui/dbui/dbmgr.cxx
+++ sw/source/ui/dbui/dbmgr.cxx
@@ -1445,27 +1445,30 @@ sal_Bool SwNewDBMgr::MergeMailFiles(SwWrtShell* pSourceShell,
             {
                 if( rMergeDescriptor.nMergeType != DBMGR_MERGE_PRINTER )
                 {
-                    assert( aTempFile.get());
-                    INetURLObject aTempFileURL( rMergeDescriptor.bSubjectIsFilename ? sSubject : aTempFile->GetURL());
-                    SfxMedium* pDstMed = new SfxMedium(
-                        aTempFileURL.GetMainURL( INetURLObject::NO_DECODE ),
-                        STREAM_STD_READWRITE );
-                    pDstMed->SetFilter( pStoreToFilter );
-                    if(pDstMed->GetItemSet())
+                    if( !bCancel )
                     {
-                        if(pStoreToFilterOptions )
-                            pDstMed->GetItemSet()->Put(SfxStringItem(SID_FILE_FILTEROPTIONS, *pStoreToFilterOptions));
-                        if(rMergeDescriptor.aSaveToFilterData.getLength())
-                            pDstMed->GetItemSet()->Put(SfxUsrAnyItem(SID_FILTER_DATA, makeAny(rMergeDescriptor.aSaveToFilterData)));
-                    }
+                        assert( aTempFile.get());
+                        INetURLObject aTempFileURL( rMergeDescriptor.bSubjectIsFilename ? sSubject : aTempFile->GetURL());
+                        SfxMedium* pDstMed = new SfxMedium(
+                            aTempFileURL.GetMainURL( INetURLObject::NO_DECODE ),
+                            STREAM_STD_READWRITE );
+                        pDstMed->SetFilter( pStoreToFilter );
+                        if(pDstMed->GetItemSet())
+                        {
+                            if(pStoreToFilterOptions )
+                                pDstMed->GetItemSet()->Put(SfxStringItem(SID_FILE_FILTEROPTIONS, *pStoreToFilterOptions));
+                            if(rMergeDescriptor.aSaveToFilterData.getLength())
+                                pDstMed->GetItemSet()->Put(SfxUsrAnyItem(SID_FILTER_DATA, makeAny(rMergeDescriptor.aSaveToFilterData)));
+                        }
 
-                    xTargetDocShell->DoSaveAs(*pDstMed);
-                    xTargetDocShell->DoSaveCompleted(pDstMed);
-                    if( xTargetDocShell->GetError() )
-                    {
-                        // error message ??
-                        ErrorHandler::HandleError( xTargetDocShell->GetError() );
-                        bNoError = sal_False;
+                        xTargetDocShell->DoSaveAs(*pDstMed);
+                        xTargetDocShell->DoSaveCompleted(pDstMed);
+                        if( xTargetDocShell->GetError() )
+                        {
+                            // error message ??
+                            ErrorHandler::HandleError( xTargetDocShell->GetError() );
+                            bNoError = sal_False;
+                        }
                     }
                 }
                 else if( pTargetView ) // must be available!
