43a8207c9daff7cf5afe2436249163adc14404c4
Eike Rathke <erack@redhat.com>
2015-03-09 20:01:19 +0100

    CID#1287400: Control flow issues (MISSING_BREAK)
    Change-Id: I74da9d277d21e01ffb316e6885470a1a3cbe0cf2

--- sc/source/core/tool/compiler.cxx
+++ sc/source/core/tool/compiler.cxx
@@ -3847,6 +3847,7 @@ ScTokenArray* ScCompiler::CompileString( const OUString& rFormula )
                 if (bUseFunctionStack && nFunction)
                     --nFunction;
             }
+            break;
             case ocTableRefOpen:
             {
                 // Don't count following item separator as parameter separator.
