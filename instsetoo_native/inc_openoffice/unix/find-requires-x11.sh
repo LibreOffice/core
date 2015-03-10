5d970faf2e5444c35e9c614eb55d621b02b0289e
Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>
2015-03-10 16:54:36 +0100

    libGLU actually is not required anymore for master
    Change-Id: I543a561fc7fa1213c8e319f227b761f6328a6eaa

--- instsetoo_native/inc_openoffice/unix/find-requires-x11.sh
+++ instsetoo_native/inc_openoffice/unix/find-requires-x11.sh
@@ -23,6 +23,5 @@ if [[ "${OS}" == "AIX" ]]; then
   echo "libfreetype.a(libfreetype.so.6${mark64})"
 else
   echo "libfreetype.so.6${mark64}"
-  echo "libGLU.so.1${mark64}"
   echo "libXinerama.so.1${mark64}"
 fi
