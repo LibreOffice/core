LibreOffice/core

commit de0866b507e259f1726d1015293aad8fcadd763a

sw-invert-border-spacing.diff:
n#391591

sw/source/filter/ww8/ww8par6.cxx
@@ -4798,10 +4798,19 @@ void SwWW8ImplReader::Read_Border(USHORT , const BYTE* , short nLen)
 
                 maTracer.Log(sw::log::eBorderDistOutside);
 
-                aBox.SetDistance( (USHORT)aInnerDist.Left(), BOX_LINE_LEFT );
-                aBox.SetDistance( (USHORT)aInnerDist.Top(), BOX_LINE_TOP );
-                aBox.SetDistance( (USHORT)aInnerDist.Right(), BOX_LINE_RIGHT );
-                aBox.SetDistance( (USHORT)aInnerDist.Bottom(), BOX_LINE_BOTTOM );
+        if ((nBorder & WW8_LEFT)==WW8_LEFT) {
+            aBox.SetDistance( (USHORT)aInnerDist.Left(), BOX_LINE_LEFT );
+        }
+        if ((nBorder & WW8_TOP)==WW8_TOP) {
+            aBox.SetDistance( (USHORT)aInnerDist.Top(), BOX_LINE_TOP );
+        }
+        if ((nBorder & WW8_RIGHT)==WW8_RIGHT) {
+            aBox.SetDistance( (USHORT)aInnerDist.Right(), BOX_LINE_RIGHT );
+        }
+
+        if ((nBorder & WW8_BOT)==WW8_BOT) {
+            aBox.SetDistance( (USHORT)aInnerDist.Bottom(), BOX_LINE_BOTTOM );
+        }
 
                 NewAttr( aBox );
 

sw/inc/IDocumentSettingAccess.hxx
@@ -81,6 +81,7 @@ namespace com { namespace sun { namespace star { namespace i18n { struct Forbidd
          // --> OD 2008-06-05 #i89181#
          TAB_AT_LEFT_INDENT_FOR_PARA_IN_LIST,
          // <--
+     INVERT_BORDER_SPACING,
          // COMPATIBILITY FLAGS END
 
          BROWSE_MODE,

sw/inc/doc.hxx
@@ -603,6 +603,7 @@ private:
     bool mbOldPrinterMetrics                        : 1;   // FME 2007-05-14 #147385#
     bool mbTabRelativeToIndent                      : 1;   // #i24363# tab stops relative to indent
     bool mbProtectForm                              : 1;
+    bool mbInvertBorderSpacing                      : 1;
     bool mbTabAtLeftIndentForParagraphsInList;             // OD 2008-06-05 #i89181# - see above
 
     // #i78591#

sw/source/core/doc/doc.cxx
@@ -197,6 +197,7 @@ bool SwDoc::get(/*[in]*/ DocumentSettingId id) const
         // --> OD 2008-06-05 #i89181#
         case TAB_AT_LEFT_INDENT_FOR_PARA_IN_LIST: return mbTabAtLeftIndentForParagraphsInList;
         // <--
+    case INVERT_BORDER_SPACING: return mbInvertBorderSpacing;
          // COMPATIBILITY FLAGS END
 
         case BROWSE_MODE: return mbBrowseMode;
@@ -320,6 +321,9 @@ void SwDoc::set(/*[in]*/ DocumentSettingId id, /*[in]*/ bool value)
             mbTabAtLeftIndentForParagraphsInList = value;
         break;
         // <--
+    case INVERT_BORDER_SPACING:
+        mbInvertBorderSpacing = value;
+    break;
          // COMPATIBILITY FLAGS END
 
         case BROWSE_MODE:

sw/source/core/doc/docnew.cxx
@@ -383,6 +383,7 @@ SwDoc::SwDoc() :
     // --> OD 2008-06-05 #i89181#
     mbTabAtLeftIndentForParagraphsInList    = false;        // hidden
     // <--
+    mbInvertBorderSpacing                   = false;        // hidden
 
     //
     // COMPATIBILITY FLAGS END

sw/source/core/layout/frmtool.cxx
@@ -2002,8 +2002,9 @@ void SwBorderAttrs::_CalcBottom()
 
 long SwBorderAttrs::CalcRight( const SwFrm* pCaller ) const
 {
-    long nRight;
+    long nRight=0;
 
+    if (!pCaller->IsTxtFrm() || !((SwTxtFrm*)pCaller)->GetTxtNode()->GetDoc()->get(IDocumentSettingAccess::INVERT_BORDER_SPACING)) {
     // OD 23.01.2003 #106895# - for cell frame in R2L text direction the left
     // and right border are painted on the right respectively left.
     if ( pCaller->IsCellFrm() && pCaller->IsRightToLeft() )
@@ -2011,6 +2012,7 @@ long SwBorderAttrs::CalcRight( const SwFrm* pCaller ) const
     else
         nRight = CalcRightLine();
 
+    }
     // for paragraphs, "left" is "before text" and "right" is "after text"
     if ( pCaller->IsTxtFrm() && pCaller->IsRightToLeft() )
         nRight += rLR.GetLeft();
@@ -2030,20 +2032,23 @@ long SwBorderAttrs::CalcRight( const SwFrm* pCaller ) const
 
 long SwBorderAttrs::CalcLeft( const SwFrm *pCaller ) const
 {
-    long nLeft;
+    long nLeft=0;
 
+    if (!pCaller->IsTxtFrm() || !((SwTxtFrm*)pCaller)->GetTxtNode()->GetDoc()->get(IDocumentSettingAccess::INVERT_BORDER_SPACING)) {
     // OD 23.01.2003 #106895# - for cell frame in R2L text direction the left
     // and right border are painted on the right respectively left.
     if ( pCaller->IsCellFrm() && pCaller->IsRightToLeft() )
         nLeft = CalcRightLine();
     else
         nLeft = CalcLeftLine();
+    }
 
     // for paragraphs, "left" is "before text" and "right" is "after text"
     if ( pCaller->IsTxtFrm() && pCaller->IsRightToLeft() )
         nLeft += rLR.GetRight();
     else
         nLeft += rLR.GetLeft();
+
 
     // --> OD 2008-01-21 #newlistlevelattrs#
     // correction: do not retrieve left margin for numbering in R2L-layout

sw/source/core/layout/paintfrm.cxx
@@ -4662,7 +4662,7 @@ void SwFrm::PaintBorder( const SwRect& rRect, const SwPageFrm *pPage,
                          const SwBorderAttrs &rAttrs ) const
 {
     //fuer (Row,Body,Ftn,Root,Column,NoTxt) gibt's hier nix zu tun
-    if ( (GetType() & 0x90C5) || (Prt().SSize() == Frm().SSize()) )
+    if ( (GetType() & 0x90C5) )
         return;
 
     if ( (GetType() & 0x2000) &&    //Cell

sw/source/filter/rtf/swparrtf.cxx
@@ -296,7 +296,7 @@ void SwRTFParser::Continue( int nToken )
             // --> FME 2006-02-10 #131283#
             pDoc->set(IDocumentSettingAccess::TABLE_ROW_KEEP, true);
             pDoc->set(IDocumentSettingAccess::IGNORE_TABS_AND_BLANKS_FOR_LINE_CALCULATION, true);
-
+        pDoc->set(IDocumentSettingAccess::INVERT_BORDER_SPACING, true);
             //
             // COMPATIBILITY FLAGS END
             //

commit 932b283dee0bda1eb2e45ca6a72bbe0e75a8be98

sw: PVS-Studio V616 'WW8_TOP' named constant with the value of 0
... is used in the bitwise operation

(regression from de0866b507e259f1726d1015293aad8fcadd763a)

Change-Id: Id2377543df7bce31aadf8ad536ef96bcf818963f

sw/source/filter/ww8/ww8par6.cxx
@@ -4736,16 +4736,16 @@ void SwWW8ImplReader::Read_Border(sal_uInt16 , const sal_uInt8*, short nLen)
                 Rectangle aInnerDist;
                 GetBorderDistance( aBrcs, aInnerDist );
 
-                if ((nBorder & WW8_LEFT)==WW8_LEFT)
+                if (nBorder & (1 << WW8_LEFT))
                     aBox.SetDistance( (sal_uInt16)aInnerDist.Left(), BOX_LINE_LEFT );
 
-                if ((nBorder & WW8_TOP)==WW8_TOP)
+                if (nBorder & (1 << WW8_TOP))
                     aBox.SetDistance( (sal_uInt16)aInnerDist.Top(), BOX_LINE_TOP );
 
-                if ((nBorder & WW8_RIGHT)==WW8_RIGHT)
+                if (nBorder & (1 << WW8_RIGHT))
                     aBox.SetDistance( (sal_uInt16)aInnerDist.Right(), BOX_LINE_RIGHT );
 
-                if ((nBorder & WW8_BOT)==WW8_BOT)
+                if (nBorder & (1 << WW8_BOT))
                     aBox.SetDistance( (sal_uInt16)aInnerDist.Bottom(), BOX_LINE_BOTTOM );
 
                 NewAttr( aBox );