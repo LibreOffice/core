29070e77b768cd45b377b822526f9d2728959676
Pranav Kant <pranav913@gmail.com>
2015-03-14 17:34:21 +0530

    tdf#88230: add a clear() method to SolarMutexReleaser
    This is helpful, especially, when one has to release the solar mutex
    before the automatic destruction of the created object.
    
    Change-Id: I76cf0578982051bb1912a948b55f5818f6385710

--- include/vcl/svapp.hxx
+++ include/vcl/svapp.hxx
@@ -1674,13 +1674,29 @@ class SolarMutexTryAndBuyGuard
 class SolarMutexReleaser
 {
     sal_uLong mnReleased;
+    bool m_bCleared;
 
 public:
-    SolarMutexReleaser(): mnReleased(Application::ReleaseSolarMutex()) {}
+    SolarMutexReleaser()
+        : m_bCleared(false)
+        , mnReleased( Application::ReleaseSolarMutex() ) { }
 
     ~SolarMutexReleaser()
     {
-        Application::AcquireSolarMutex( mnReleased );
+        if( !m_bCleared )
+        {
+            Application::AcquireSolarMutex( mnReleased );
+        }
+    }
+
+    /** Releases mutex. **/
+    void SAL_CALL clear()
+    {
+        if( !m_bCleared )
+            {
+                Application::AcquireSolarMutex( mnReleased );
+                m_bCleared = true;
+            }
     }
 };
 
