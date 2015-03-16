05d81a728e748fd60fd27eeaf8629b547d4d66da
Miklos Vajna <vmiklos@collabora.co.uk>
2015-03-16 09:50:18 +0100

    android: add missing license headers and modelines
    Change-Id: I66b2fc7ee846d6865b11cf1cbb168a6e5fab2eea

--- android/experimental/LOAndroid3/src/java/org/libreoffice/TileIdentifier.java
+++ android/experimental/LOAndroid3/src/java/org/libreoffice/TileIdentifier.java
@@ -1,3 +1,11 @@
+/* -*- Mode: Java; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package org.libreoffice;
 
 import android.graphics.Rect;
@@ -68,3 +76,5 @@ public class TileIdentifier {
         return String.format("TileIdentifier (%d, %d) z=%f s=(%d, %d)", x, y, zoom, size.width, size.height);
     }
 }
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
--- android/experimental/LOAndroid3/src/java/org/mozilla/gecko/OnSlideSwipeListener.java
+++ android/experimental/LOAndroid3/src/java/org/mozilla/gecko/OnSlideSwipeListener.java
@@ -1,7 +1,11 @@
-/* -*- Mode: Java; c-basic-offset: 4; tab-width: 20; indent-tabs-mode: nil; -*-
+/* -*- Mode: Java; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 
 package org.mozilla.gecko;
 
