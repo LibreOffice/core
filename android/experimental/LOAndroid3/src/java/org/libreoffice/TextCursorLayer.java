43a503cfc9e7ac627b4202985c173403a57f80a5
Miklos Vajna <vmiklos@collabora.co.uk>
2015-03-16 09:50:18 +0100

    android: add missing license headers and modelines
    Change-Id: I66b2fc7ee846d6865b11cf1cbb168a6e5fab2eea

--- android/experimental/LOAndroid3/src/java/org/libreoffice/TextCursorLayer.java
+++ android/experimental/LOAndroid3/src/java/org/libreoffice/TextCursorLayer.java
@@ -1,3 +1,11 @@
+/* -*- Mode: Java; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package org.libreoffice;
 
 import android.app.Activity;
@@ -132,3 +140,5 @@ public class TextCursorLayer extends Layer {
         });
     }
 }
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
--- android/experimental/LOAndroid3/src/java/org/libreoffice/ToolbarController.java
+++ android/experimental/LOAndroid3/src/java/org/libreoffice/ToolbarController.java
@@ -1,3 +1,11 @@
+/* -*- Mode: Java; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package org.libreoffice;
 
 import android.support.v7.app.ActionBar;
@@ -36,3 +44,5 @@ public class ToolbarController {
         });
     }
 }
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
