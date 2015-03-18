f59345721b1731c0fd5ed052f2dee99042bd7f4c
Caolán McNamara <caolanm@redhat.com>
2015-03-18 13:38:11 +0000

    move the cairo resize hack into vcl
    Change-Id: Ifd1817a28f4fb7ddcc85447fd3388fd0efd86476

--- vcl/inc/salgdi.hxx
+++ vcl/inc/salgdi.hxx
@@ -440,6 +440,7 @@ public:
 
     /// Check whether cairo will work
     virtual bool                SupportsCairo() const = 0;
+    virtual bool                CanResizeCairoSurface() const = 0;
 
     virtual SystemFontData      GetSysFontData( int nFallbacklevel ) const = 0;
 
