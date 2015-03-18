4b8120b058c03a4385ed211161e793071c117d7d
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2015-03-18 18:12:02 +0900

    android: Hit test for handles, show resize rect
    Change-Id: Ibf9f98968fd9007be6dc5481d1652923ecb98b89

--- android/experimental/LOAndroid3/src/java/org/libreoffice/DrawElementHandle.java
+++ android/experimental/LOAndroid3/src/java/org/libreoffice/DrawElementHandle.java
@@ -12,28 +12,57 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PointF;
+import android.graphics.RectF;
 
+import org.mozilla.gecko.gfx.RectUtils;
+
+/**
+ * This class is responsible to draw the selection handles, track the handle
+ * position and perform a hit test to determine if the selection handle was
+ * touched.
+ */
 public class DrawElementHandle {
     public PointF mPosition = new PointF();
     private float mRadius = 20.0f;
     private Paint mGraphicHandleFillPaint = new Paint();
     private Paint mGraphicSelectionPaint = new Paint();
+    private Paint mGraphicHandleSelectedFillPaint = new Paint();
+    private RectF mHitRect = new RectF();
 
     public DrawElementHandle(Paint graphicSelectionPaint) {
         mGraphicSelectionPaint = graphicSelectionPaint;
 
         mGraphicHandleFillPaint.setStyle(Paint.Style.FILL);
         mGraphicHandleFillPaint.setColor(Color.WHITE);
+
+        mGraphicHandleSelectedFillPaint.setStyle(Paint.Style.FILL);
+        mGraphicHandleSelectedFillPaint.setColor(Color.BLACK);
     }
 
     public void draw(Canvas canvas) {
-        canvas.drawCircle(mPosition.x, mPosition.y, mRadius, mGraphicHandleFillPaint);
-        canvas.drawCircle(mPosition.x, mPosition.y, mRadius, mGraphicSelectionPaint);
+        drawFilledCircle(canvas, mPosition.x, mPosition.y, mRadius, mGraphicSelectionPaint, mGraphicHandleFillPaint);
+    }
+
+    public void drawSelected(Canvas canvas) {
+        drawFilledCircle(canvas, mPosition.x, mPosition.y, mRadius, mGraphicSelectionPaint, mGraphicHandleSelectedFillPaint);
+    }
+
+    private void drawFilledCircle(Canvas canvas, float x, float y, float radius, Paint strokePaint, Paint fillPaint) {
+        canvas.drawCircle(x, y, radius, fillPaint);
+        canvas.drawCircle(x, y, radius, strokePaint);
     }
 
     public void reposition(float x, float y) {
         mPosition.x = x;
         mPosition.y = y;
+        mHitRect.left = mPosition.x - mRadius * 2;
+        mHitRect.right = mPosition.x + mRadius * 2;
+        mHitRect.top = mPosition.y - mRadius * 2;
+        mHitRect.bottom = mPosition.y + mRadius * 2;
+    }
+
+    public boolean contains(float x, float y) {
+        return mHitRect.contains(x, y);
     }
 }
 
