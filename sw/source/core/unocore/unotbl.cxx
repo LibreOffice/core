bc0557001212e4a94e8123c46f643defcb85afa0
Bjoern Michaelsen <bjoern.michaelsen@canonical.com>
2015-03-28 16:57:12 +0100

    flatten and simplify
    Change-Id: I198793d0ab9a91a7f6a3aebc5fafbb6d7de7b98f

--- sw/source/core/unocore/unotbl.cxx
+++ sw/source/core/unocore/unotbl.cxx
@@ -1559,18 +1559,15 @@ sal_Bool SwXTextTableCursor::gotoCellByName(const OUString& sCellName, sal_Bool 
     return rTblCrsr.GotoTblBox(sCellName);
 }
 
-sal_Bool SwXTextTableCursor::goLeft(sal_Int16 Count, sal_Bool Expand) throw( uno::RuntimeException, std::exception )
+sal_Bool SwXTextTableCursor::goLeft(sal_Int16 Count, sal_Bool bExpand) throw( uno::RuntimeException, std::exception )
 {
     SolarMutexGuard aGuard;
-    bool bRet = false;
     SwUnoCrsr* pUnoCrsr = GetCrsr();
-    if(pUnoCrsr)
-    {
-        SwUnoTableCrsr& rTblCrsr = dynamic_cast<SwUnoTableCrsr&>(*pUnoCrsr);
-        lcl_CrsrSelect(rTblCrsr, Expand);
-        bRet = rTblCrsr.Left(Count, CRSR_SKIP_CHARS, false, false);
-    }
-    return bRet;
+    if(!pUnoCrsr)
+        return false;
+    SwUnoTableCrsr& rTblCrsr = dynamic_cast<SwUnoTableCrsr&>(*pUnoCrsr);
+    lcl_CrsrSelect(rTblCrsr, bExpand);
+    return rTblCrsr.Left(Count, CRSR_SKIP_CHARS, false, false);
 }
 
 sal_Bool SwXTextTableCursor::goRight(sal_Int16 Count, sal_Bool Expand) throw( uno::RuntimeException, std::exception )
