fec207b77aadd6fa8f8411c5918c99af95dea786
Stephan Bergmann <sbergman@redhat.com>
2015-03-29 20:30:53 +0200

    Fix test on non-Linux
    Change-Id: I0852c732d580d85ebad629ca26c28ede1576b957

--- vcl/qa/cppunit/BitmapTest.cxx
+++ vcl/qa/cppunit/BitmapTest.cxx
@@ -57,9 +57,14 @@ void BitmapTest::testConvert()
     CPPUNIT_ASSERT_EQUAL(sal_uInt16(24), aBitmap.GetBitCount());
     {
         Bitmap::ScopedReadAccess pReadAccess(aBitmap);
+#if defined LINUX
         // 24 bit Bitmap on SVP backend uses 32bit BGRX format
         CPPUNIT_ASSERT_EQUAL(static_cast<sal_uInt16>(32), pReadAccess->GetBitCount());
         CPPUNIT_ASSERT_EQUAL(sal_uLong(40), pReadAccess->GetScanlineSize());
+#else
+        CPPUNIT_ASSERT_EQUAL(static_cast<sal_uInt16>(24), pReadAccess->GetBitCount());
+        CPPUNIT_ASSERT_EQUAL(sal_uLong(30), pReadAccess->GetScanlineSize());
+#endif
         CPPUNIT_ASSERT(!pReadAccess->HasPalette());
         Color aColor = pReadAccess->GetPixel(0, 0);
         CPPUNIT_ASSERT_EQUAL(sal_Int32(204), sal_Int32(aColor.GetRed()));
