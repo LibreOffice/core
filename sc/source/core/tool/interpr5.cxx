e27d7c38ba1de0f4bbb1197bd84c063a0fdee9d1
Eike Rathke <erack@redhat.com>
2015-03-31 17:21:44 +0200

    Revert "tdf#89387 Add functor for ScAmpersand"
    This reverts commit 9a7959cd63be7b2f36da8af25e7673a525c4d66c.
    
    It is not an equivalent replacement for the existing functionality, i.e.
    it lacks the number to string conversion done through SvNumberFormatter
    and instead of concatenating a numeric element produces an error value
    element.

--- sc/source/core/tool/interpr5.cxx
+++ sc/source/core/tool/interpr5.cxx
@@ -1367,9 +1367,37 @@ void ScInterpreter::ScAmpersand()
                     for (SCSIZE j = 0; j < nR; ++j)
                         pResMat->PutError( nGlobalError, i, j);
             }
+            else if (bFlag)
+            {
+                for (SCSIZE i = 0; i < nC; ++i)
+                    for (SCSIZE j = 0; j < nR; ++j)
+                    {
+                        sal_uInt16 nErr = pMat->GetErrorIfNotString( i, j);
+                        if (nErr)
+                            pResMat->PutError( nErr, i, j);
+                        else
+                        {
+                            OUString aTmp = sStr;
+                            aTmp += pMat->GetString(*pFormatter, i, j).getString();
+                            pResMat->PutString(mrStrPool.intern(aTmp), i, j);
+                        }
+                    }
+            }
             else
             {
-                pMat->AmpersandOp(bFlag, sStr, *pResMat, mrStrPool);
+                for (SCSIZE i = 0; i < nC; ++i)
+                    for (SCSIZE j = 0; j < nR; ++j)
+                    {
+                        sal_uInt16 nErr = pMat->GetErrorIfNotString( i, j);
+                        if (nErr)
+                            pResMat->PutError( nErr, i, j);
+                        else
+                        {
+                            OUString aTmp = pMat->GetString(*pFormatter, i, j).getString();
+                            aTmp += sStr;
+                            pResMat->PutString(mrStrPool.intern(aTmp), i, j);
+                        }
+                    }
             }
             PushMatrix(pResMat);
         }
