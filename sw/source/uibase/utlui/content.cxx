LibreOffice/core

commit 329742e6c9da7cd7848d92a6846e3d1249d8d9b4

fdo#85886 don't redraw the Navigator content tree if nothing changed
This fixes the flickering of the scrollbar on re-draw once a second.
Perhaps it helps for the performance issue too.

Change-Id: I2ec8f0a8a241b128113bfa3d47fb09ba472b4a7e

sw/source/uibase/utlui/content.cxx
@@ -1756,6 +1756,7 @@ void SwContentTree::Display( bool bActive )
         ScrollOutputArea( (short)nDelta );
     }
 
+    m_bActiveDocModified = false;
 }
 
 // In the Clear the content types have to be deleted, also.
@@ -2208,6 +2209,8 @@ void SwContentTree::SetActiveShell(SwWrtShell* pSh)
     bool bClear = pActiveShell != pSh;
     if(bIsActive && bClear)
     {
+        if (pActiveShell)
+            EndListening(*pActiveShell->GetView().GetDocShell());
         pActiveShell = pSh;
         FindActiveTypeAndRemoveUserData();
         Clear();
@@ -2216,6 +2219,7 @@ void SwContentTree::SetActiveShell(SwWrtShell* pSh)
     {
         if(!lcl_FindShell(pActiveShell))
         {
+            EndListening(*pActiveShell->GetView().GetDocShell());
             pActiveShell = pSh;
             bIsActive = true;
             bIsConstant = false;
@@ -2226,6 +2230,7 @@ void SwContentTree::SetActiveShell(SwWrtShell* pSh)
     // the screen filled new.
     if(bIsActive && bClear)
     {
+        StartListening(*pActiveShell->GetView().GetDocShell());
         FindActiveTypeAndRemoveUserData();
         for(sal_uInt16 i=0; i < CONTENT_TYPE_MAX; i++)
         {
@@ -2239,15 +2244,33 @@ void SwContentTree::SetActiveShell(SwWrtShell* pSh)
 
 void SwContentTree::SetConstantShell(SwWrtShell* pSh)
 {
+    if (pActiveShell)
+        EndListening(*pActiveShell->GetView().GetDocShell());
     pActiveShell = pSh;
     bIsActive       = false;
     bIsConstant     = true;
+    StartListening(*pActiveShell->GetView().GetDocShell());
     FindActiveTypeAndRemoveUserData();
     for(sal_uInt16 i=0; i < CONTENT_TYPE_MAX; i++)
     {
         DELETEZ(aActiveContentArr[i]);
     }
     Display(true);
+}
+
+
+
+void SwContentTree::Notify(SfxBroadcaster & rBC, SfxHint const& rHint)
+{
+    SfxSimpleHint const*const pHint(dynamic_cast<SfxSimpleHint const*>(&rHint));
+    if (pHint && SFX_HINT_DOCCHANGED == pHint->GetId())
+    {
+        m_bActiveDocModified = true;
+    }
+    else
+    {
+        SfxListener::Notify(rBC, rHint);
+    }
 }
 
 // Execute commands of the Navigator
@@ -2436,18 +2459,25 @@ IMPL_LINK_NOARG(SwContentTree, TimerUpdate)
         }
 
         if(bIsActive && pActShell != GetWrtShell())
+        {
             SetActiveShell(pActShell);
+        }
         else if( (bIsActive || (bIsConstant && pActShell == GetWrtShell())) &&
                     HasContentChanged())
         {
-            FindActiveTypeAndRemoveUserData();
-            Display(true);
+            if (!bIsActive || m_bActiveDocModified)
+            {   // don't burn cpu and redraw and flicker if not modified
+                FindActiveTypeAndRemoveUserData();
+                Display(true);
+            }
         }
     }
     else if(!pView && bIsActive && !bIsIdleClear)
     {
         if(pActiveShell)
+        {
             SetActiveShell(0);
+        }
         Clear();
         bIsIdleClear = true;
     }

sw/source/uibase/inc/conttree.hxx
@@ -19,6 +19,7 @@
 #ifndef INCLUDED_SW_SOURCE_UIBASE_INC_CONTTREE_HXX
 #define INCLUDED_SW_SOURCE_UIBASE_INC_CONTTREE_HXX
 
+#include <svl/lstner.hxx>
 #include <svtools/treelistbox.hxx>
 #include <svtools/svlbitm.hxx>
 #include "swcont.hxx"
@@ -43,7 +44,9 @@ class SdrObject;
 #define EDIT_MODE_DELETE        4
 #define EDIT_MODE_RENAME        5
 
-class SwContentTree : public SvTreeListBox
+class SwContentTree
+    : public SvTreeListBox
+    , public SfxListener
 {
     ImageList           aEntryImages;
     OUString            sSpace;
@@ -85,6 +88,7 @@ class SwContentTree : public SvTreeListBox
     bool                bIsOutlineMoveable  :1;
     bool                bViewHasChanged     :1;
     bool                bIsImageListInitialized : 1;
+    bool                m_bActiveDocModified :1;
 
     static bool         bIsInDrag;
 
@@ -196,6 +200,10 @@ public:
 
     virtual bool    Select( SvTreeListEntry* pEntry, bool bSelect=true ) SAL_OVERRIDE;
     virtual sal_Int32  GetEntryRealChildrenNum( SvTreeListEntry* pEntry ) const;
+
+    using Control::Notify; // FIXME why do we have 2 of these
+    virtual void Notify(SfxBroadcaster& rBC, SfxHint const& rHint) SAL_OVERRIDE;
+
 };
 
 // TreeListBox for global documents

commit fcdc9a92748ca5a48946c0505931e0749e5b4bdd

tdf#88360: sw: fix crash on closing help in Navigator window
SwContentTree::pActiveShell is not cleared because of a check that the
shell exists, which is only done in the special case bIsConstant.

* at most one of bIsActive or bIsConstant is true
* the only place that calls SetActiveShell() if bIsConstant is true
  already checks this condition

So remove the unnecessary check from SetActiveShell()

(regression from 329742e6c9da7cd7848d92a6846e3d1249d8d9b4)

(cherry picked from commit 7ddd6c008142a6b88878ffc96f5e339a0fddd7d9)

Change-Id: Id3b3b98bd38303b5afc529cc8be669872bd354b7

sw/source/uibase/utlui/content.cxx
@@ -2220,15 +2220,12 @@ void SwContentTree::SetActiveShell(SwWrtShell* pSh)
     }
     else if(bIsConstant)
     {
-        if(!lcl_FindShell(pActiveShell))
-        {
-            if (pActiveShell)
-                EndListening(*pActiveShell->GetView().GetDocShell());
-            pActiveShell = pSh;
-            bIsActive = true;
-            bIsConstant = false;
-            bClear = true;
-        }
+        if (pActiveShell)
+            EndListening(*pActiveShell->GetView().GetDocShell());
+        pActiveShell = pSh;
+        bIsActive = true;
+        bIsConstant = false;
+        bClear = true;
     }
     // Only if it is the active view, the array will be deleted and
     // the screen filled new.