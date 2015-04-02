LibreOffice/core

commit f9b62506b22c3eb885ffd5a4ec8025c33df7b2d4

fdo#75110 SwEditShell::DeleteSel fix start of selection when bSelAll == true
Change-Id: I3e8ce585b29619061b18ab1ab8f0cf6c7d421074

sw/source/core/edit/eddel.cxx
@@ -89,9 +89,14 @@ void SwEditShell::DeleteSel( SwPaM& rPam, sal_Bool* pUndo )
     }
     else
     {
+        SwPaM aPam(rPam);
+        if (bSelectAll)
+            // Selection starts at the first para of the first cell, but we
+            // want to delete the table node before the first cell as well.
+            aPam.Start()->nNode = aPam.Start()->nNode.GetNode().FindTableNode()->GetIndex();
         // delete everything
-        GetDoc()->DeleteAndJoin( rPam );
-        SaveTblBoxCntnt( rPam.GetPoint() );
+        GetDoc()->DeleteAndJoin( aPam );
+        SaveTblBoxCntnt( aPam.GetPoint() );
     }
 
     // Selection is not needed anymore

sw/qa/extras/uiwriter/uiwriter.cxx
@@ -28,6 +28,7 @@ public:
     void testFdo70807();
     void testImportRTF();
     void testExportRTF();
+    void testFdo75110();
 
     CPPUNIT_TEST_SUITE(SwUiWriterTest);
     CPPUNIT_TEST(testReplaceForward);
@@ -36,6 +37,7 @@ public:
     CPPUNIT_TEST(testFdo70807);
     CPPUNIT_TEST(testImportRTF);
     CPPUNIT_TEST(testExportRTF);
+    CPPUNIT_TEST(testFdo75110);
     CPPUNIT_TEST_SUITE_END();
 
 private:
@@ -85,6 +87,19 @@ void SwUiWriterTest::testReplaceForward()
     rUndoManager.Undo();
 
     CPPUNIT_ASSERT_EQUAL(ORIGINAL_REPLACE_CONTENT, pTxtNode->GetTxt());
+}
+
+void SwUiWriterTest::testFdo75110()
+{
+    SwDoc* pDoc = createDoc("fdo75110.odt");
+    SwWrtShell* pWrtShell = pDoc->GetDocShell()->GetWrtShell();
+
+    pWrtShell->SelAll();
+    // The problem was that SwEditShell::DeleteSel() what this Delete() invokes took the wrong selection...
+    pWrtShell->Delete();
+    sw::UndoManager& rUndoManager = pDoc->GetUndoManager();
+    // ... so this Undo() call resulted in a crash.
+    rUndoManager.Undo();
 }
 
 void SwUiWriterTest::testReplaceBackward()

commit 41e4998cdeb54a6fc316f349de61296be820fe47

tdf#87400: sw: fix ~SwIndexReg assertion in AutoCorrect
SwAutoFormat::DoUnderline() calls DeleteSel() with a SwPaM that is not
corrected, except if it's passed along directly to SwDoc methods.

(regression from f9b62506b22c3eb885ffd5a4ec8025c33df7b2d4)

Change-Id: I741e0391e7f8e09a64bcfe99ca4d650f1016aaa7

sw/source/core/edit/eddel.cxx
@@ -90,14 +90,20 @@ void SwEditShell::DeleteSel( SwPaM& rPam, bool* pUndo )
     }
     else
     {
-        SwPaM aPam(rPam);
+        std::unique_ptr<SwPaM> pNewPam;
+        SwPaM * pPam = &rPam;
         if (bSelectAll)
+        {
+            assert(dynamic_cast<SwShellCrsr*>(&rPam)); // must be corrected pam
+            pNewPam.reset(new SwPaM(rPam));
             // Selection starts at the first para of the first cell, but we
             // want to delete the table node before the first cell as well.
-            aPam.Start()->nNode = aPam.Start()->nNode.GetNode().FindTableNode()->GetIndex();
+            pNewPam->Start()->nNode = pNewPam->Start()->nNode.GetNode().FindTableNode()->GetIndex();
+            pPam = pNewPam.get();
+        }
         // delete everything
-        GetDoc()->getIDocumentContentOperations().DeleteAndJoin( aPam );
-        SaveTblBoxCntnt( aPam.GetPoint() );
+        GetDoc()->getIDocumentContentOperations().DeleteAndJoin(*pPam);
+        SaveTblBoxCntnt( pPam->GetPoint() );
     }
 
     // Selection is not needed anymore