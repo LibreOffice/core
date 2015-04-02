LibreOffice/core

commit 3d12549335229aca1a6a57575292111274709992

fdo#70345 Reuse XStream for Windows platform create a new one for others.
Change-Id: I911c24b82b55445c6f95e14be29759e2fe3d9dfd
Reviewed-on: https://gerrit.libreoffice.org/6580
Reviewed-by: Michael Stahl <mstahl@redhat.com>
Tested-by: Michael Stahl <mstahl@redhat.com>

sfx2/source/doc/docfile.cxx
@@ -604,13 +604,26 @@ SvStream* SfxMedium::GetOutStream()
 
         if ( pImp->pTempFile )
         {
-            // don't try to re-use XOutStream from xStream if that exists;
+            // On windows we try to re-use XOutStream from xStream if that exists;
+            // because opening new SvFileStream in this situation may fail with ERROR_SHARING_VIOLATION
+            #ifdef WNT
+            if (pImp->xStream.is())
+            {
+                assert(pImp->xStream->getOutputStream().is()); // need that...
+                pImp->m_pOutStream = utl::UcbStreamHelper::CreateStream(
+                        pImp->xStream, false);
+            }
+            else
+            {
+                pImp->m_pOutStream = new SvFileStream(
+                        pImp->m_aName, STREAM_STD_READWRITE);
+            }
+            // On Unix don't try to re-use XOutStream from xStream if that exists;
             // it causes fdo#59022 (fails opening files via SMB on Linux)
-            // OTOH opening new SvFileStream in this situation may fail on
-            // Windows with ERROR_SHARING_VIOLATION... but that is apparently
-            // not a problem in practice currently
+            #else
             pImp->m_pOutStream = new SvFileStream(
                         pImp->m_aName, STREAM_STD_READWRITE);
+            #endif
             CloseStorage();
         }
     }

commit 0d825b587f0d97f057a0e85b7b6fc0f7094e0542

tdf#72337: sfx2: make the horrible hack runtime-optional
SfxMedium::GetOutStream() does something different on WNT to solve
sharing issues on one kind of file server but that causes issues with
some other kind of file server that worked before.

Make this runtime-changeable with (undocumented) env variable
SFX_MEDIUM_REUSE_STREAM.

(regression from 3d12549335229aca1a6a57575292111274709992)

Change-Id: Id7a1fc8169dbf09c67a109b36ffa312ef33231c5

sfx2/source/doc/docfile.cxx
@@ -564,8 +564,9 @@ SvStream* SfxMedium::GetOutStream()
         {
             // On windows we try to re-use XOutStream from xStream if that exists;
             // because opening new SvFileStream in this situation may fail with ERROR_SHARING_VIOLATION
-            #ifdef WNT
-            if (pImp->xStream.is())
+            // TODO: this is a horrible hack that should probably be removed,
+            // somebody needs to investigate this more thoroughly...
+            if (getenv("SFX_MEDIUM_REUSE_STREAM") && pImp->xStream.is())
             {
                 assert(pImp->xStream->getOutputStream().is()); // need that...
                 pImp->m_pOutStream = utl::UcbStreamHelper::CreateStream(
@@ -573,15 +574,11 @@ SvStream* SfxMedium::GetOutStream()
             }
             else
             {
-                pImp->m_pOutStream = new SvFileStream(
-                        pImp->m_aName, STREAM_STD_READWRITE);
-            }
             // On Unix don't try to re-use XOutStream from xStream if that exists;
             // it causes fdo#59022 (fails opening files via SMB on Linux)
-            #else
-            pImp->m_pOutStream = new SvFileStream(
-                        pImp->m_aName, STREAM_STD_READWRITE);
-            #endif
+                pImp->m_pOutStream = new SvFileStream(
+                            pImp->m_aName, STREAM_STD_READWRITE);
+            }
             CloseStorage();
         }
     }