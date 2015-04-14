LibreOffice/core

commit ef490df2119523e1761f99b5fdbb387c2c71608d

svx: ExternalToolEdit: check that temp file is actually created
(cherry picked from commit c8ae30a720474db8efed3db31348f8ae52c149c3)

Conflicts:
	svx/source/core/extedit.cxx

Change-Id: I03e49493c549561b4dc806f1e191a73d06733cff
Reviewed-on: https://gerrit.libreoffice.org/13996
Reviewed-by: Caolán McNamara <caolanm@redhat.com>
Tested-by: Caolán McNamara <caolanm@redhat.com>

svx/source/core/extedit.cxx
@@ -106,11 +106,23 @@ void ExternalToolEdit::Edit(GraphicObject const*const pGraphicObject)
     OUString aTempFileName;
 
     oslFileHandle pHandle;
-    osl::FileBase::createTempFile(0, &pHandle, &aTempFileBase);
+    osl::FileBase::RC rc =
+        osl::FileBase::createTempFile(0, &pHandle, &aTempFileBase);
+    if (osl::FileBase::E_None != rc)
+    {
+        SAL_WARN("svx", "ExternalToolEdit::Edit: cannot create temp file");
+        return;
+    }
 
     // Move it to a file name with image extension properly set
     aTempFileName = aTempFileBase + OUString('.') + OUString(fExtension);
-    osl::File::move(aTempFileBase, aTempFileName);
+    // FIXME: this is pretty stupid, need a better osl temp file API
+    rc = osl::File::move(aTempFileBase, aTempFileName);
+    if (osl::FileBase::E_None != rc)
+    {
+        SAL_WARN("svx", "ExternalToolEdit::Edit: cannot move temp file");
+        return;
+    }
 
     //Write Graphic to the Temp File
     GraphicFilter& rGraphicFilter = GraphicFilter::GetGraphicFilter();

commit 582cdc273ec0e91d27be8a7893994acadf4d7d18

tdf#89750: svx: fix ExternalToolEdit on Windows
While adding check that the temp file move succeeds, overlooked that the
file handle is actually leaked, which means the move will fail on WNT
because the file is already open.

(regression from ef490df2119523e1761f99b5fdbb387c2c71608d)

Change-Id: I779ccde79871930fef3bad7e17b633fb62e36a46

svx/source/core/extedit.cxx
@@ -113,9 +113,8 @@ void ExternalToolEdit::Edit(GraphicObject const*const pGraphicObject)
     OUString aTempFileBase;
     OUString aTempFileName;
 
-    oslFileHandle pHandle;
     osl::FileBase::RC rc =
-        osl::FileBase::createTempFile(0, &pHandle, &aTempFileBase);
+        osl::FileBase::createTempFile(nullptr, nullptr, &aTempFileBase);
     if (osl::FileBase::E_None != rc)
     {
         SAL_WARN("svx", "ExternalToolEdit::Edit: cannot create temp file");