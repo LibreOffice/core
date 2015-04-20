LibreOffice/core

commit 1f77a5e8d5bf8a7f1b7bd7206996d2b2efc59462

n#203704 fix RTF export table output for subtables
Older ODT files containing <table:table table:is-sub-table="true">
confused the RTF exporter. Add a few sanity checks to make sure the
output is still valid.

sw/source/filter/ww8/rtfattributeoutput.cxx
@@ -251,13 +251,19 @@ void RtfAttributeOutput::StartParagraph( ww8::WW8TableNodeInfo::Pointer_t pTextN
         {
             ww8::WW8TableNodeInfoInner::Pointer_t pDeepInner( pTextNodeInfo->getInnerForDepth( m_nTableDepth ) );
             OSL_ENSURE( pDeepInner, "TableNodeInfoInner not found");
-            if ( pDeepInner && pDeepInner->getCell() == 0 )
+            // Make sure we always start a row between ending one and starting a cell.
+            // In case of subtables, we may not get the first cell.
+            if ((pDeepInner && pDeepInner->getCell() == 0) || m_bTableRowEnded)
+            {
+                m_bTableRowEnded = false;
                 StartTableRow( pDeepInner );
+            }
 
             StartTableCell( pDeepInner );
         }
 
-        if ( nRow == 0 && nCell == 0 )
+        // Again, if depth was incremented, start a new table even if we skipped the first cell.
+        if ((nRow == 0 && nCell == 0) || (m_nTableDepth == 0 && pTextNodeInfo->getDepth()))
         {
             // Do we have to start the table?
             // [If we are at the rigth depth already, it means that we
@@ -610,7 +616,10 @@ void RtfAttributeOutput::TableDefinition( ww8::WW8TableNodeInfoInner::Pointer_t 
                         rLR.GetLeft() - rLR.GetRight();
     }
     SwTwips nTblSz = pFmt->GetFrmSize().GetWidth();
-    for( sal_uInt16 i = 0; i < pRow->GetCells().Count(); i++ )
+    // Not using m_nTableDepth, which is not yet incremented here.
+    sal_uInt32 nCurrentDepth = pTableTextNodeInfoInner->getDepth();
+    m_aCells[nCurrentDepth] = pRow->GetCells().Count();
+    for( sal_uInt16 i = 0; i < m_aCells[nCurrentDepth]; i++ )
     {
         SwWriteTableCell *pCell = pRow->GetCells( )[ i ];
         const SwFrmFmt *pCellFmt = pCell->GetBox()->GetFrmFmt();
@@ -906,6 +915,9 @@ void RtfAttributeOutput::StartTableRow( ww8::WW8TableNodeInfoInner::Pointer_t pT
     // We'll write the table definition for nested tables later
     if ( nCurrentDepth > 1 )
         return;
+    // Empty the previous row closing buffer before starting the new one,
+    // necessary for subtables.
+    m_rExport.Strm() << m_aAfterRuns.makeStringAndClear().getStr();
     m_rExport.Strm() << m_aRowDefs.makeStringAndClear().getStr();
     }
 }
@@ -944,11 +956,16 @@ void RtfAttributeOutput::EndTableCell( )
     m_bTableCellOpen = false;
     m_bTblAfterCell = true;
     m_bWroteCellInfo = false;
+    m_aCells[m_nTableDepth]--;
 }
 
 void RtfAttributeOutput::EndTableRow( )
 {
     SAL_INFO("sw.rtf", OSL_THIS_FUNC << ", (depth is " << m_nTableDepth << ")");
+
+    // Trying to end the row without writing the required number of cells? Fill with empty ones.
+    for( sal_uInt16 i = 0; i < m_aCells[m_nTableDepth]; i++ )
+        m_aAfterRuns.append(OOO_STRING_SVTOOLS_RTF_CELL);
 
     if ( m_nTableDepth > 1 )
     {
@@ -971,6 +988,7 @@ void RtfAttributeOutput::EndTableRow( )
         }
         m_aAfterRuns.append(OOO_STRING_SVTOOLS_RTF_ROW).append(OOO_STRING_SVTOOLS_RTF_PARD);
     }
+    m_bTableRowEnded = true;
 }
 
 void RtfAttributeOutput::EndTable()
@@ -2997,7 +3015,9 @@ RtfAttributeOutput::RtfAttributeOutput( RtfExport &rExport )
     m_bBufferSectionHeaders( false ),
     m_bLastTable( true ),
     m_bWroteCellInfo( false ),
-    m_bHadFieldResult( false )
+    m_bHadFieldResult( false ),
+    m_bTableRowEnded( false ),
+    m_aCells()
 {
     SAL_INFO("sw.rtf", OSL_THIS_FUNC);
 }

sw/source/filter/ww8/rtfattributeoutput.hxx
@@ -533,6 +533,12 @@ private:
      * If we had a field result in the URL.
      */
     bool m_bHadFieldResult;
+
+    /// If we ended a table row without starting a new one.
+    bool m_bTableRowEnded;
+
+    /// Number of cells from the table definition, by depth.
+    std::map<sal_uInt32,sal_uInt32> m_aCells;
 public:
     RtfAttributeOutput( RtfExport &rExport );
 

commit 5e2708a5a8cad3c2f4578742264610abab677395

tdf#80708 RTF export: fix unexpected table row break
Regression from commit 1f77a5e8d5bf8a7f1b7bd7206996d2b2efc59462
(n#203704 fix RTF export table output for subtables, 2012-02-08), the
problem is that RtfAttributeOutput::StartTableRow() has two callers
depending on if it's the first or a later row, but the m_bTableRowEnded
flag storing if that method is called is only set in one case.

Fix the problem by moving the update of the flag to the method itself.

Change-Id: Ieae1fb53f9528fc74d037d2fd783b3979b17cab8

sw/source/filter/ww8/rtfattributeoutput.cxx
@@ -235,7 +235,6 @@ void RtfAttributeOutput::StartParagraph(ww8::WW8TableNodeInfo::Pointer_t pTextNo
             // In case of subtables, we may not get the first cell.
             if (pDeepInner && (pDeepInner->getCell() == 0 || m_bTableRowEnded))
             {
-                m_bTableRowEnded = false;
                 StartTableRow(pDeepInner);
             }
 
@@ -873,6 +872,7 @@ void RtfAttributeOutput::StartTableRow(ww8::WW8TableNodeInfoInner::Pointer_t pTa
 {
     sal_uInt32 nCurrentDepth = pTableTextNodeInfoInner->getDepth();
     SAL_INFO("sw.rtf", OSL_THIS_FUNC << ", (depth is " << nCurrentDepth << ")");
+    m_bTableRowEnded = false;
 
     TableDefinition(pTableTextNodeInfoInner);
 

sw/qa/extras/rtfexport/data/tdf80708.rtf
@@ -1,0 +1,15 @@
+{\rtf1
+\pard\plain
+{first table}
+\par \trowd\trql\trleft-108\ltrrow\trrh-374\trkeep\trpaddft3\trpaddt0\trpaddfl3\trpaddl0\trpaddfb3\trpaddb0\trpaddfr3\trpaddr0\clbrdrl\brdrhair\brdrcf1\cellx296\clbrdrl\brdrhair\brdrcf1\clbrdrr\brdrhair\brdrcf1\cellx702\pard\plain
+{A1}
+\cell\pard\plain \s0\ql
+{A2}
+\cell\row\pard\pard\plain \s42\ql
+{\rtlch \ltrch\loch\lang2067 second table}
+\par \trowd\trql\trleft-108\ltrrow\trrh-561\trkeep\trpaddft3\trpaddt0\trpaddfl3\trpaddl0\trpaddfb3\trpaddb0\trpaddfr3\trpaddr0\clbrdrl\brdrhair\brdrcf1\cellx296\clbrdrl\brdrhair\brdrcf1\clbrdrr\brdrhair\brdrcf1\cellx702\pard\plain \s0\ql
+{A1}
+\cell\pard\plain \s0\ql
+{A2}
+\cell\row\pard\pard\plain\par
+}

sw/qa/extras/rtfexport/rtfexport.cxx
@@ -20,6 +20,7 @@
 #include <com/sun/star/table/ShadowFormat.hpp>
 #include <com/sun/star/text/FontEmphasis.hpp>
 #include <com/sun/star/text/RelOrientation.hpp>
+#include <com/sun/star/text/TableColumnSeparator.hpp>
 #include <com/sun/star/text/TextContentAnchorType.hpp>
 #include <com/sun/star/text/XFootnotesSupplier.hpp>
 #include <com/sun/star/text/XPageCursor.hpp>
@@ -876,6 +877,16 @@ DECLARE_RTFEXPORT_TEST(testHyphpar, "hyphpar.rtf")
 {
     // Hyphenation was enabled for all 3 paragraphs, but it should be disabled for the 2nd one.
     CPPUNIT_ASSERT_EQUAL(false, getProperty<bool>(getParagraph(2), "ParaIsHyphenation"));
+}
+
+DECLARE_RTFEXPORT_TEST(testTdf80708, "tdf80708.rtf")
+{
+    uno::Reference<text::XTextTablesSupplier> xTextTablesSupplier(mxComponent, uno::UNO_QUERY);
+    uno::Reference<container::XIndexAccess> xTables(xTextTablesSupplier->getTextTables(), uno::UNO_QUERY);
+    uno::Reference<text::XTextTable> xTable(xTables->getByIndex(1), uno::UNO_QUERY);
+    uno::Reference<table::XTableRows> xTableRows(xTable->getRows(), uno::UNO_QUERY);
+    // This was 2, i.e. the second table had 3 cols, now 2 as expected.
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(1), getProperty< uno::Sequence<text::TableColumnSeparator> >(xTableRows->getByIndex(0), "TableColumnSeparators").getLength());
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();