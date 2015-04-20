LibreOffice/core

commit 5aa360cae0383f270c12708e7e94179a7fde6711

Resolves: fdo#80468 and fdo#81223 image/frame backgrounds wrong
writer has code to fill in missing new properties from the old properties
if they exist. But xmloff is stripping them out before they get there.

Don't strip them out, and add in a missing check for one of the
archaic bg colors and add a regression test for fdo#81223

Change-Id: I9a541a9bee0a01c90f2c33383f1144ecd8b0bfff

sw/source/core/unocore/unoframe.cxx
@@ -355,11 +355,14 @@ bool BaseFrameProperties_Impl::FillBaseProperties(SfxItemSet& rToSet, const SfxI
             aXFillColorItem.PutValue(*pXFillColorItem);
             rToSet.Put(aXFillColorItem);
         }
-        else if (aXFillStyleItem.GetValue() == drawing::FillStyle_SOLID && pCol)
+        else if (aXFillStyleItem.GetValue() == drawing::FillStyle_SOLID && (pCol || pRGBCol))
         {
             // Fill style is set to solid, but no fill color is given.
             // On the other hand, we have a BackColor, so use that.
-            aBrush.PutValue(*pCol, MID_BACK_COLOR);
+            if (pCol)
+                aBrush.PutValue(*pCol, MID_BACK_COLOR);
+            else
+                aBrush.PutValue(*pRGBCol, MID_BACK_COLOR_R_G_B);
             setSvxBrushItemAsFillAttributesToTargetSet(aBrush, rToSet);
         }
 

include/xmloff/XMLShapeStyleContext.hxx
@@ -37,9 +37,6 @@ protected:
     OUString     m_sListStyleName;
     bool         m_bIsNumRuleAlreadyConverted;
 
-    //UUUU
-    bool         m_bIsFillStyleAlreadyConverted : 1;
-
 protected:
     virtual void SetAttribute( sal_uInt16 nPrefixKey,
                                const OUString& rLocalName,

sw/qa/extras/odfimport/odfimport.cxx
@@ -478,6 +478,16 @@ DECLARE_ODFIMPORT_TEST(testAnnotationFormatting, "annotation-formatting.odt")
     CPPUNIT_ASSERT_EQUAL(awt::FontWeight::BOLD, getProperty<float>(getRun(xParagraph, 2), "CharWeight"));
 }
 
+DECLARE_ODFIMPORT_TEST(fdo81223, "fdo81223.odt")
+{
+    uno::Reference<text::XTextFramesSupplier> xTextFramesSupplier(mxComponent, uno::UNO_QUERY);
+    uno::Reference<container::XIndexAccess> xIndexAccess(xTextFramesSupplier->getTextFrames(), uno::UNO_QUERY);
+    uno::Reference<beans::XPropertySet> xFrame(xIndexAccess->getByIndex(0), uno::UNO_QUERY);
+    sal_Int32 nValue(0);
+    xFrame->getPropertyValue("BackColor") >>= nValue;
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(0xfeffffff), nValue);
+}
+
 #endif
 
 CPPUNIT_PLUGIN_IMPLEMENT();

xmloff/source/draw/XMLShapeStyleContext.cxx
@@ -53,8 +53,7 @@ XMLShapeStyleContext::XMLShapeStyleContext(
     SvXMLStylesContext& rStyles,
     sal_uInt16 nFamily)
 :   XMLPropStyleContext(rImport, nPrfx, rLName, xAttrList, rStyles, nFamily ),
-    m_bIsNumRuleAlreadyConverted( false ),
-    m_bIsFillStyleAlreadyConverted(false) //UUUU
+    m_bIsNumRuleAlreadyConverted( false )
 {
 }
 
@@ -184,18 +183,6 @@ void XMLShapeStyleContext::FillPropertySet( const Reference< beans::XPropertySet
                 property->mnIndex = -1;
             }
         }
-    }
-
-    //UUUU need to filter out old fill definitions when the new ones are used. The new
-    // ones are used when a FillStyle is defined
-    if(!m_bIsFillStyleAlreadyConverted && GetProperties().size())
-    {
-        if(doNewDrawingLayerFillStyleDefinitionsExist(OUString("FillStyle")))
-        {
-            deactivateOldFillStyleDefinitions(getStandardSet());
-        }
-
-        m_bIsFillStyleAlreadyConverted = true;
     }
 
     struct _ContextID_Index_Pair aContextIDs[] =

commit 09f0919f9a1686f6c1492777307e77f841294192

tdf#90640: sw: ODF import: ignore style:background-transparency for
... bitmap backgrounds of frames.  OOo/LO versions <= 4.3 wrongly write
fo:background-color="transparent" style:background-transparency="100%"
for bitmap backgrounds, because of a stupid API implementation with
SvxBrushItem.  They overwrite that with draw:opacity="100%" on the child
element style:background-image, which we don't read if we see draw:fill
attributes, so ignore the background-transparency in that case.

(regression from 5aa360cae0383f270c12708e7e94179a7fde6711)

Change-Id: I4ca59d8932a8e5d034c35543a54528198ad7d51c

sw/source/core/unocore/unoframe.cxx
@@ -459,9 +459,15 @@ bool BaseFrameProperties_Impl::FillBaseProperties(SfxItemSet& rToSet, const SfxI
         {
             // No fill transparency is given.  On the other hand, we have a
             // BackColorTransparency, so use that.
+            // tdf#90640 tdf#90130: this is necessary for LO 4.4.0 - 4.4.2
+            // that forgot to write draw:opacity into documents
+            // but: the value was *always* wrong for bitmaps! => ignore it
             sal_Int8 nGraphicTransparency(0);
             *pColTrans >>= nGraphicTransparency;
-            rToSet.Put(XFillTransparenceItem(nGraphicTransparency));
+            if (aXFillStyleItem.GetValue() != drawing::FillStyle_BITMAP)
+            {
+                rToSet.Put(XFillTransparenceItem(nGraphicTransparency));
+            }
             if (aXFillStyleItem.GetValue() == drawing::FillStyle_SOLID)
             {
                 aBrush.PutValue(*pColTrans, MID_BACK_COLOR_TRANSPARENCY);