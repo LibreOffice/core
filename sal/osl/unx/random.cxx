5ede1d017cb6e602db699a1b2364f9708de441c8
Norbert Thiebaud <nthiebaud@gmail.com>
2015-04-21 20:51:04 -0500

    add an internal api to osl to collect 'native' random numbers
    use /dev/urandom on unixes
    and
    rand_s() on windows
    
    Change-Id: Iccd6e01706f16d82f23cda0f0a410953c32fbc02
    Reviewed-on: https://gerrit.libreoffice.org/15473
    Reviewed-by: Michael Stahl <mstahl@redhat.com>
    Tested-by: Michael Stahl <mstahl@redhat.com>

---
+++ sal/osl/unx/random.cxx
@@ -0,0 +1,48 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "system.hxx"
+#include "internal/oslrandom.h"
+
+#include <assert.h>
+
+int osl_get_system_random_data(char* buffer, size_t desired_len)
+{
+    ssize_t nb_read;
+    int fd;
+
+    assert(buffer);
+    fd = open("/dev/urandom", O_RDONLY);
+    if(fd != -1)
+    {
+        while(desired_len)
+        {
+            if ((nb_read = read(fd, buffer, desired_len)) == -1)
+            {
+                if (errno != EINTR)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                buffer  += nb_read;
+                desired_len -= nb_read;
+            }
+        }
+        close(fd);
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
