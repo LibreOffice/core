d36da8c19378f4772c896ca4df94117077e823d6
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2015-04-28 08:44:30 +0900

    remove "y"
    Change-Id: I792a15ad04cf8a54c2609ec3cc91ed46caab66be

--- include/vcl/window.hxx
+++ include/vcl/window.hxx
@@ -1,4 +1,4 @@
-y/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 /*
  * This file is part of the LibreOffice project.
  *
