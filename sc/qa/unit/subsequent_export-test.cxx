a7d843b6112250d1a41c2a56d8e083ccf6538bca
yogesh.bharate001 <yogesh.bharate@synerzip.com>
2015-04-27 19:28:54 +0530

    tdf#90890: hyperlink inside textbox is not exported after roundtrip.
    Problem Description:
    - Text box hyperlink is not exported after roundtrip.
    
    XML Difference: in drawing1.xml
    Original file:
    <xdr:cNvPr id="2" name="TextBox 1">
        <a:hlinkClick xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" r:id="rId1" tooltip="text123"/>
    </xdr:cNvPr>
    
    After RT:
    <xdr:cNvPr id="0" name="CustomShape 1"/>
    <a:hlinkClick> is missing.
    
    Change-Id: I01082de5d7d2e1a9323e84f54c97d5444cab13cd
    Solution: Added support for hyperlink in text box.
    Reviewed-on: https://gerrit.libreoffice.org/15555
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: David Tardon <dtardon@redhat.com>
    Tested-by: David Tardon <dtardon@redhat.com>

--- sc/qa/unit/subsequent_export-test.cxx
+++ sc/qa/unit/subsequent_export-test.cxx
@@ -137,6 +137,7 @@ public:
 
     void testSupBookVirtualPath();
     void testSheetLocalRangeNameXLS();
+    void testSheetTextBoxHyperlink();
 
     CPPUNIT_TEST_SUITE(ScExportTest);
     CPPUNIT_TEST(test);
@@ -184,6 +185,7 @@ public:
     CPPUNIT_TEST(testLinkedGraphicRT);
     CPPUNIT_TEST(testImageWithSpecialID);
     CPPUNIT_TEST(testSheetLocalRangeNameXLS);
+    CPPUNIT_TEST(testSheetTextBoxHyperlink);
 
     CPPUNIT_TEST_SUITE_END();
 
@@ -209,7 +211,8 @@ void ScExportTest::registerNamespaces(xmlXPathContextPtr& pXmlXPathCtx)
         { BAD_CAST("office"), BAD_CAST("urn:oasis:names:tc:opendocument:xmlns:office:1.0") },
         { BAD_CAST("table"), BAD_CAST("urn:oasis:names:tc:opendocument:xmlns:table:1.0") },
         { BAD_CAST("text"), BAD_CAST("urn:oasis:names:tc:opendocument:xmlns:text:1.0") },
-        { BAD_CAST("xlink"), BAD_CAST("http://www.w3c.org/1999/xlink") }
+        { BAD_CAST("xlink"), BAD_CAST("http://www.w3c.org/1999/xlink") },
+        { BAD_CAST("xdr"), BAD_CAST("http://schemas.openxmlformats.org/drawingml/2006/spreadsheetDrawing") }
     };
     for(size_t i = 0; i < SAL_N_ELEMENTS(aNamespaces); ++i)
     {
@@ -2515,6 +2518,22 @@ void ScExportTest::testSheetLocalRangeNameXLS()
     CPPUNIT_ASSERT_EQUAL(OUString("=local_name1"), aFormula);
 
     xDocSh2->DoClose();
+}
+
+void ScExportTest::testSheetTextBoxHyperlink()
+{
+    ScDocShellRef xShell = loadDoc("textbox-hyperlink.", XLSX);
+    CPPUNIT_ASSERT(xShell.Is());
+
+    ScDocShellRef xDocSh = saveAndReload(&(*xShell), XLSX);
+    CPPUNIT_ASSERT(xDocSh.Is());
+
+    xmlDocPtr pDoc = XPathHelper::parseExport(&(*xDocSh), m_xSFactory, "xl/drawings/drawing1.xml", XLSX);
+    CPPUNIT_ASSERT(pDoc);
+
+    assertXPath(pDoc, "/xdr:wsDr[1]/xdr:twoCellAnchor[1]/xdr:sp[1]/xdr:nvSpPr[1]/xdr:cNvPr[1]/a:hlinkClick[1]", 1);
+
+    xDocSh->DoClose();
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(ScExportTest);
