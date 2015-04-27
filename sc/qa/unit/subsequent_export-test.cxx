adf69166bf655b3d6deb88e66f632f6d72456f2d
yogesh.bharate001 <yogesh.bharate@synerzip.com>
2015-04-27 15:08:16 +0530

    tdf#90812: rPr is not exported after roundtrip.
    Problem Description:
    XML Difference: In sharedStrings.xml
    Original file :
    <r>
      <rPr>
          <sz val="11"/>
          <color rgb="FFFF0000"/>
          <rFont val="Calibri"/>
          <family val="2"/>
          <scheme val="minor"/>
      </rPr>
       <t>Red</t>
    </r>
    
    Roundtrip file:
    <r>
      <t>Red</t>
    </r>
    
    rPr is missing in roundtrip file.
    
    Change-Id: I79efd0f8f1a735ef7e4ebd3fda220b3e339ea91c

--- sc/qa/unit/subsequent_export-test.cxx
+++ sc/qa/unit/subsequent_export-test.cxx
@@ -140,6 +140,7 @@ public:
     void testSheetTextBoxHyperlink();
     void testFontSize();
     void testSheetCharacterKerningSpace();
+    void testSheetRunParagraphProperty();
 
     CPPUNIT_TEST_SUITE(ScExportTest);
     CPPUNIT_TEST(test);
@@ -190,6 +191,7 @@ public:
     CPPUNIT_TEST(testSheetTextBoxHyperlink);
     CPPUNIT_TEST(testFontSize);
     CPPUNIT_TEST(testSheetCharacterKerningSpace);
+    CPPUNIT_TEST(testSheetRunParagraphProperty);
 
     CPPUNIT_TEST_SUITE_END();
 
@@ -216,7 +218,8 @@ void ScExportTest::registerNamespaces(xmlXPathContextPtr& pXmlXPathCtx)
         { BAD_CAST("table"), BAD_CAST("urn:oasis:names:tc:opendocument:xmlns:table:1.0") },
         { BAD_CAST("text"), BAD_CAST("urn:oasis:names:tc:opendocument:xmlns:text:1.0") },
         { BAD_CAST("xlink"), BAD_CAST("http://www.w3c.org/1999/xlink") },
-        { BAD_CAST("xdr"), BAD_CAST("http://schemas.openxmlformats.org/drawingml/2006/spreadsheetDrawing") }
+        { BAD_CAST("xdr"), BAD_CAST("http://schemas.openxmlformats.org/drawingml/2006/spreadsheetDrawing") },
+        { BAD_CAST("x"), BAD_CAST("http://schemas.openxmlformats.org/spreadsheetml/2006/main") }
     };
     for(size_t i = 0; i < SAL_N_ELEMENTS(aNamespaces); ++i)
     {
@@ -2573,6 +2576,21 @@ void ScExportTest::testSheetCharacterKerningSpace()
     xDocSh->DoClose();
 }
 
+void ScExportTest::testSheetRunParagraphProperty()
+{
+    ScDocShellRef xShell = loadDoc("TextColor.", XLSX);
+    CPPUNIT_ASSERT(xShell.Is());
+
+    ScDocShellRef xDocSh = saveAndReload(&(*xShell), XLSX);
+    CPPUNIT_ASSERT(xDocSh.Is());
+
+    xmlDocPtr pDoc = XPathHelper::parseExport(&(*xDocSh), m_xSFactory, "xl/sharedStrings.xml", XLSX);
+    CPPUNIT_ASSERT(pDoc);
+
+    assertXPath(pDoc, "/x:sst/x:si/x:r[1]/x:rPr[1]", 1);
+
+    xDocSh->DoClose();
+}
 
 CPPUNIT_TEST_SUITE_REGISTRATION(ScExportTest);
 
