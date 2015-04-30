2874d41ac0bf26eb2814b392facc58215b2dc9b9
Laurent Godard <lgodard.libre@laposte.net>
2015-04-30 16:56:14 +0200

    new uno sidebar api
    the entry point is SfxBaseController::getSidebar()
    
    the Decks and panels are now per SidebarController and not global anymore
    
    ' macro example
    Sub testSidebarApi
    
    	controller =  thisComponent.currentcontroller
    	frameIni =  thisComponent.currentcontroller.frame
    	sidebar =  thisComponent.currentcontroller.getSidebar()
    
    	sidebar.setVisible(true)
    
    	frame = sidebar.getFrame()
    
    	decks = sidebar.getdecks()
    
    	c = decks.count
    
    	h = decks.hasElements()
    
    	e = decks.getElementNames()
    
    	a =  decks.hasByName("MyDeck")
    
    	deck = decks.getByName("MyDeck")
    	deck.activate(true)
    
    	t = deck.getTitle()
    	deck.setTitle("new deck title")
    
            deck.moveFirst()
            deck.moveLast()
            deck.moveUp()
            deck.moveDown()
    
            index = deck.getOrderIndex()
    
    	panels = deck.getPanels()
    	ep = panels.getElementnames()
    
    	ap = panels.hasByName("aPanelName")
    
    	panel = panels.getByName("aPanelName")
    	panel.setTitle("new panel title")
    
    	panel.collapse()
    
    	panel.expand(true) ' expand and collapse other
    
            index = panel.getOrderIndex()
    
            panel.moveLast()
            panel.moveFirst()
            panel.moveDown()
            panel.moveUp()
    
    End Sub
    
    Change-Id: I7a4b953f2277dea483bb296ba6ead35de7205ace

---
+++ include/sfx2/sidebar/UnoDeck.hxx
@@ -0,0 +1,67 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#ifndef INCLUDED_SFX2_SIDEBAR_UNODECK_HXX
+#define INCLUDED_SFX2_SIDEBAR_UNODECK_HXX
+
+
+#include <com/sun/star/ui/XDeck.hpp>
+#include <com/sun/star/frame/XFrame.hpp>
+#include <com/sun/star/ui/XPanels.hpp>
+
+#include <cppuhelper/compbase1.hxx>
+#include <cppuhelper/weakref.hxx>
+
+#include <sfx2/sidebar/SidebarController.hxx>
+#include <sfx2/sidebar/ResourceManager.hxx>
+
+/** get the decks
+*/
+class SfxUnoDeck : public ::cppu::WeakImplHelper1< css::ui::XDeck >
+{
+
+public:
+
+    SfxUnoDeck(const css::uno::Reference<css::frame::XFrame>& , const OUString&);
+    virtual ~SfxUnoDeck() {};
+
+    virtual OUString SAL_CALL getId() SAL_OVERRIDE;
+
+    virtual OUString SAL_CALL  getTitle() SAL_OVERRIDE;
+    virtual void SAL_CALL setTitle( const OUString& newTitle ) SAL_OVERRIDE;
+
+    virtual sal_Bool SAL_CALL isActive() SAL_OVERRIDE;
+    virtual void SAL_CALL activate( const sal_Bool bActivate ) SAL_OVERRIDE;
+
+    virtual css::uno::Reference<css::ui::XPanels> SAL_CALL getPanels() SAL_OVERRIDE;
+
+    virtual sal_Int32 SAL_CALL getOrderIndex() SAL_OVERRIDE;
+    virtual void SAL_CALL setOrderIndex( const sal_Int32 newOrderIndex ) SAL_OVERRIDE;
+    virtual void SAL_CALL moveFirst() SAL_OVERRIDE;
+    virtual void SAL_CALL moveLast() SAL_OVERRIDE;
+    virtual void SAL_CALL moveUp() SAL_OVERRIDE;
+    virtual void SAL_CALL moveDown() SAL_OVERRIDE;
+
+private:
+
+    const css::uno::Reference<css::frame::XFrame> xFrame;
+    sfx2::sidebar::SidebarController* getSidebarController();
+
+    const OUString mDeckId;
+    const OUString mTitle;
+
+    sal_Int32 GetMaxOrderIndex(sfx2::sidebar::ResourceManager::DeckContextDescriptorContainer aDecks);
+    sal_Int32 GetMinOrderIndex(sfx2::sidebar::ResourceManager::DeckContextDescriptorContainer aDecks);
+
+};
+
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
