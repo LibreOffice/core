7082fcb4791f95dabf7fc45ed8c4eb299c43646d
Stephan Bergmann <sbergman@redhat.com>
2015-04-30 08:53:12 +0200

    -Werror,-Winconsistent-missing-override
    Change-Id: I68f674dcdb16a507e962945ed90caf796daa48d3

--- chart2/source/controller/chartapiwrapper/DiagramWrapper.cxx
+++ chart2/source/controller/chartapiwrapper/DiagramWrapper.cxx
@@ -1983,7 +1983,7 @@ public:
     virtual void setPropertyValue( const ::com::sun::star::uno::Any& rOuterValue, const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >& xInnerPropertySet ) const
                         throw (::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::beans::PropertyVetoException, ::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::lang::WrappedTargetException, ::com::sun::star::uno::RuntimeException) SAL_OVERRIDE;
     virtual Any getPropertyValue(const Reference<beans::XPropertySet>& xInnerPropertySet) const
-                        throw (beans::UnknownPropertyException, lang::WrappedTargetException, uno::RuntimeException);
+                        throw (beans::UnknownPropertyException, lang::WrappedTargetException, uno::RuntimeException) SAL_OVERRIDE;
 
 private: //member
     ::boost::shared_ptr< Chart2ModelContact >   m_spChart2ModelContact;
