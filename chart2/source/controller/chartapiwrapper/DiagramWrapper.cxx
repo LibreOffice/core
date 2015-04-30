1be9365d7f7c33e23d4599931616fdf56bb26fbf
Markus Mohrhard <markus.mohrhard@googlemail.com>
2015-04-30 05:21:18 +0200

    support querying about hidden cells in chart data sequences
    Change-Id: I8886c022d709acfd0f65453e36f29c1c953b4b1c

--- chart2/source/controller/chartapiwrapper/DiagramWrapper.cxx
+++ chart2/source/controller/chartapiwrapper/DiagramWrapper.cxx
@@ -1982,6 +1982,8 @@ public:
 
     virtual void setPropertyValue( const ::com::sun::star::uno::Any& rOuterValue, const ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet >& xInnerPropertySet ) const
                         throw (::com::sun::star::beans::UnknownPropertyException, ::com::sun::star::beans::PropertyVetoException, ::com::sun::star::lang::IllegalArgumentException, ::com::sun::star::lang::WrappedTargetException, ::com::sun::star::uno::RuntimeException) SAL_OVERRIDE;
+    virtual Any getPropertyValue(const Reference<beans::XPropertySet>& xInnerPropertySet) const
+                        throw (beans::UnknownPropertyException, lang::WrappedTargetException, uno::RuntimeException);
 
 private: //member
     ::boost::shared_ptr< Chart2ModelContact >   m_spChart2ModelContact;
@@ -2002,9 +2004,18 @@ void WrappedIncludeHiddenCellsProperty::setPropertyValue( const Any& rOuterValue
 {
     bool bNewValue = false;
     if( ! (rOuterValue >>= bNewValue) )
-        throw lang::IllegalArgumentException( "Property Dim3D requires boolean value", 0, 0 );
+        throw lang::IllegalArgumentException( "Property IncludeHiddenCells requires boolean value", 0, 0 );
 
     ChartModelHelper::setIncludeHiddenCells( bNewValue, *m_spChart2ModelContact->getModel() );
+}
+
+Any WrappedIncludeHiddenCellsProperty::getPropertyValue( const Reference< beans::XPropertySet >& /*xInnerPropertySet*/ ) const
+                        throw (beans::UnknownPropertyException, lang::WrappedTargetException, uno::RuntimeException)
+{
+    bool bValue = ChartModelHelper::isIncludeHiddenCells( m_spChart2ModelContact->getChartModel() );
+    uno::Any aAny;
+    aAny <<= bValue;
+    return aAny;
 }
 
 // ____ XDiagramProvider ____
