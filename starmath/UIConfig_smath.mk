7c32876c9b5c7d76ff7bf2bbaf9a2347a280d4fa
Julien Nabet <serval2412@yahoo.fr>
2015-05-02 19:03:21 +0200

    tdf#68871: Remove the old Elements panel from Math
    Change-Id: Ie148d57dc14ed3f38566c24ee027f23e25987810

--- starmath/UIConfig_smath.mk
+++ starmath/UIConfig_smath.mk
@@ -27,7 +27,6 @@ $(eval $(call gb_UIConfig_add_uifiles,modules/smath,\
 	starmath/uiconfig/smath/ui/alignmentdialog \
 	starmath/uiconfig/smath/ui/catalogdialog \
 	starmath/uiconfig/smath/ui/dockingelements \
-	starmath/uiconfig/smath/ui/floatingelements \
 	starmath/uiconfig/smath/ui/fontdialog \
 	starmath/uiconfig/smath/ui/fontsizedialog \
 	starmath/uiconfig/smath/ui/fonttypedialog \
