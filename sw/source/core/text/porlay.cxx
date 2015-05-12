LibreOffice/core

commit 2db379e22a7854dc16cc0066af70f16d5662d7e8

cppcheck: cstyleCast
Change-Id: Id3ffa264ba7990b53d264745eaf0726f1468577b

sw/source/core/text/porlay.cxx
@@ -139,7 +139,7 @@ SwLinePortion *SwLineLayout::Insert( SwLinePortion *pIns )
     {
         if( GetLen() )
         {
-            pPortion = new SwTxtPortion( *(SwLinePortion*)this );
+            pPortion = new SwTxtPortion(*this);
             if( IsBlinking() && pBlink )
             {
                 SetBlinking( false );
@@ -161,7 +161,7 @@ SwLinePortion *SwLineLayout::Append( SwLinePortion *pIns )
     // First attribute change: copy mass and length from *pIns into the first
     // text portion
     if( !pPortion )
-        pPortion = new SwTxtPortion( *(SwLinePortion*)this );
+        pPortion = new SwTxtPortion(*this);
     // Call with scope or we'll end up with recursion!
     return pPortion->SwLinePortion::Append( pIns );
 }
@@ -184,7 +184,7 @@ SwMarginPortion *SwLineLayout::CalcLeftMargin()
     SwMarginPortion *pLeft = (GetPortion() && GetPortion()->IsMarginPortion()) ?
         static_cast<SwMarginPortion *>(GetPortion()) : 0;
     if( !GetPortion() )
-         SetPortion( new SwTxtPortion( *(SwLinePortion*)this ) );
+         SetPortion(new SwTxtPortion(*this));
     if( !pLeft )
     {
         pLeft = new SwMarginPortion( 0 );

sw/source/core/text/porglue.cxx
@@ -169,7 +169,7 @@ void SwMarginPortion::AdjustRight( const SwLineLayout *pCurr )
     {
 
         // 1) We search for the left Glue
-        SwLinePortion *pPos = (SwLinePortion*)this;
+        SwLinePortion *pPos = this;
         SwGluePortion *pLeft = 0;
         while( pPos )
         {
@@ -254,7 +254,7 @@ void SwMarginPortion::AdjustRight( const SwLineLayout *pCurr )
             }
         }
         // If no left Glue remaines we set the break condition.
-        pRight = pLeft ? pLeft : (SwGluePortion*)this;
+        pRight = pLeft ? pLeft : this;
     }
 }
 

sw/source/core/text/txtfld.cxx
@@ -72,7 +72,7 @@ SwExpandPortion *SwTxtFormatter::NewFldPortion( SwTxtFormatInfo &rInf,
                                                 const SwTxtAttr *pHint ) const
 {
     SwExpandPortion *pRet = 0;
-    SwFrm *pFrame = (SwFrm*)pFrm;
+    SwFrm *pFrame = pFrm;
     SwField *pFld = const_cast<SwField*>(pHint->GetFmtFld().GetField());
     const bool bName = rInf.GetOpt().IsFldName();
 
@@ -478,7 +478,7 @@ SwNumberPortion *SwTxtFormatter::NewNumberPortion( SwTxtFormatInfo &rInf ) const
 
         if( SVX_NUM_BITMAP == rNumFmt.GetNumberingType() )
         {
-            pRet = new SwGrfNumPortion( (SwFrm*)GetTxtFrm(),
+            pRet = new SwGrfNumPortion( const_cast<SwTxtFrm*>(GetTxtFrm()),
                                         pTxtNd->GetLabelFollowedBy(),
                                         rNumFmt.GetBrush(),
                                         rNumFmt.GetGraphicOrientation(),

sw/source/core/text/txtftn.cxx
@@ -812,7 +812,7 @@ SwFtnPortion *SwTxtFormatter::NewFtnPortion( SwTxtFormatInfo &rInf,
         sal_uInt16 nOldReal = pCurr->GetRealHeight();
         sal_uInt16 nOldAscent = pCurr->GetAscent();
         sal_uInt16 nOldHeight = pCurr->Height();
-        ((SwTxtFormatter*)this)->CalcRealHeight();
+        CalcRealHeight();
         nReal = pCurr->GetRealHeight();
         if( nReal < nOldReal )
             nReal = nOldReal;

sw/source/filter/ww8/rtfexport.cxx
@@ -209,7 +209,7 @@ void RtfExport::BuildNumbering()
         SwNumRule* pRule;
         --n;
         if (n == rListTbl.size())
-            pRule = (SwNumRule*)m_pDoc->GetOutlineNumRule();
+            pRule = m_pDoc->GetOutlineNumRule();
         else
         {
             pRule = rListTbl[ n ];

sw/source/filter/ww8/wrtw8num.cxx
@@ -95,7 +95,7 @@ sal_uInt16 MSWordExportBase::GetId( const SwNumRule& rNumRule )
         if ( !bOutlineRuleAdded )
         {
             // still need to paste the OutlineRule
-            SwNumRule* pR = (SwNumRule*)m_pDoc->GetOutlineNumRule();
+            SwNumRule* pR = m_pDoc->GetOutlineNumRule();
             m_pUsedNumTbl->push_back( pR );
         }
     }

sw/source/filter/ww8/ww8par.cxx
@@ -4046,7 +4046,7 @@ bool SwWW8ImplReader::ReadText(long nStartCp, long nTextLen, ManTypes nType)
             if(m_pAktItemSet && !pFmt)
             {
                 OUString sPrefix(OUStringBuffer("WW8Dropcap").append(m_nDropCap++).makeStringAndClear());
-                pNewSwCharFmt = m_rDoc.MakeCharFmt(sPrefix, (SwCharFmt*)m_rDoc.GetDfltCharFmt());
+                pNewSwCharFmt = m_rDoc.MakeCharFmt(sPrefix, m_rDoc.GetDfltCharFmt());
                  m_pAktItemSet->ClearItem(RES_CHRATR_ESCAPEMENT);
                 pNewSwCharFmt->SetFmtAttr( *m_pAktItemSet );
             }

sw/source/filter/ww8/ww8par3.cxx
@@ -1053,7 +1053,7 @@ void WW8ListManager::AdjustLVL( sal_uInt8 nLevel, SwNumRule& rNumRule,
                                   + "z" + OUString::number( nLevel ) );
 
             // const Wegcasten
-            pFmt = rDoc.MakeCharFmt(aName, (SwCharFmt*)rDoc.GetDfltCharFmt());
+            pFmt = rDoc.MakeCharFmt(aName, rDoc.GetDfltCharFmt());
             bNewCharFmtCreated = true;
             // Attribute reinsetzen
             pFmt->SetFmtAttr( *pThisLevelItemSet );
@@ -1080,7 +1080,7 @@ void WW8ListManager::AdjustLVL( sal_uInt8 nLevel, SwNumRule& rNumRule,
             const OUString aName( (!sPrefix.isEmpty() ? sPrefix : rNumRule.GetName())
                                   + "z" + OUString::number( nLevel ) );
 
-            pFmt = rDoc.MakeCharFmt(aName, (SwCharFmt*)rDoc.GetDfltCharFmt());
+            pFmt = rDoc.MakeCharFmt(aName, rDoc.GetDfltCharFmt());
             bNewCharFmtCreated = true;
             rCharFmt[ nLevel ] = pFmt;
             aNumFmt.SetCharFmt( pFmt );

commit 8875c2e06ffd055f5e4637a71761457b1131a1c3

sw: fix crash in sw_unoapi due to calling wrong SwTxtPortion ctor
Crash in sw.SwXTextRange because the SwTxtPortion copy ctor is
invoked, which copies the portion type POR_LAY, but SwTxtPortion
must have POR_TXT.

(hilarious regression from 2db379e22a7854dc16cc0066af70f16d5662d7e8)

Change-Id: I781191a60dafeba2257edf01699fafae78b45783

sw/source/core/text/porlay.cxx
@@ -139,7 +139,7 @@ SwLinePortion *SwLineLayout::Insert( SwLinePortion *pIns )
     {
         if( GetLen() )
         {
-            pPortion = new SwTxtPortion(*this);
+            pPortion = new SwTxtPortion(*static_cast<SwLinePortion*>(this));
             if( IsBlinking() && pBlink )
             {
                 SetBlinking( false );
@@ -161,7 +161,7 @@ SwLinePortion *SwLineLayout::Append( SwLinePortion *pIns )
     // First attribute change: copy mass and length from *pIns into the first
     // text portion
     if( !pPortion )
-        pPortion = new SwTxtPortion(*this);
+        pPortion = new SwTxtPortion(*static_cast<SwLinePortion*>(this));
     // Call with scope or we'll end up with recursion!
     return pPortion->SwLinePortion::Append( pIns );
 }
@@ -184,7 +184,7 @@ SwMarginPortion *SwLineLayout::CalcLeftMargin()
     SwMarginPortion *pLeft = (GetPortion() && GetPortion()->IsMarginPortion()) ?
         static_cast<SwMarginPortion *>(GetPortion()) : 0;
     if( !GetPortion() )
-         SetPortion(new SwTxtPortion(*this));
+         SetPortion(new SwTxtPortion(*static_cast<SwLinePortion*>(this)));
     if( !pLeft )
     {
         pLeft = new SwMarginPortion( 0 );