LibreOffice/core

commit ad0c5e6c663642c1e4b212e4e6a38ebfe8c3e0a7

convert META_*_ACTION constants to scoped enum
Change-Id: I8ecfbfecd765a35fafcbcc5452b0d04a89be2459

include/vcl/metaactiontypes.hxx
@@ -8,11 +8,11 @@
  *
  * This file incorporates work covered by the following license notice:
  *
- *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   Licensed to the Apache Software Foundation = ASF, under one or more
  *   contributor license agreements. See the NOTICE file distributed
  *   with this work for additional information regarding copyright
  *   ownership. The ASF licenses this file to you under the Apache
- *   License, Version 2.0 (the "License"); you may not use this file
+ *   License, Version 2.0 = the "License",; you may not use this file
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
@@ -20,61 +20,65 @@
 #ifndef INCLUDED_VCL_METAACTIONTYPES_HXX
 #define INCLUDED_VCL_METAACTIONTYPES_HXX
 
-#define META_NULL_ACTION                    (0)
-#define META_PIXEL_ACTION                   (100)
-#define META_POINT_ACTION                   (101)
-#define META_LINE_ACTION                    (102)
-#define META_RECT_ACTION                    (103)
-#define META_ROUNDRECT_ACTION               (104)
-#define META_ELLIPSE_ACTION                 (105)
-#define META_ARC_ACTION                     (106)
-#define META_PIE_ACTION                     (107)
-#define META_CHORD_ACTION                   (108)
-#define META_POLYLINE_ACTION                (109)
-#define META_POLYGON_ACTION                 (110)
-#define META_POLYPOLYGON_ACTION             (111)
-#define META_TEXT_ACTION                    (112)
-#define META_TEXTARRAY_ACTION               (113)
-#define META_STRETCHTEXT_ACTION             (114)
-#define META_TEXTRECT_ACTION                (115)
-#define META_BMP_ACTION                     (116)
-#define META_BMPSCALE_ACTION                (117)
-#define META_BMPSCALEPART_ACTION            (118)
-#define META_BMPEX_ACTION                   (119)
-#define META_BMPEXSCALE_ACTION              (120)
-#define META_BMPEXSCALEPART_ACTION          (121)
-#define META_MASK_ACTION                    (122)
-#define META_MASKSCALE_ACTION               (123)
-#define META_MASKSCALEPART_ACTION           (124)
-#define META_GRADIENT_ACTION                (125)
-#define META_HATCH_ACTION                   (126)
-#define META_WALLPAPER_ACTION               (127)
-#define META_CLIPREGION_ACTION              (128)
-#define META_ISECTRECTCLIPREGION_ACTION     (129)
-#define META_ISECTREGIONCLIPREGION_ACTION   (130)
-#define META_MOVECLIPREGION_ACTION          (131)
-#define META_LINECOLOR_ACTION               (132)
-#define META_FILLCOLOR_ACTION               (133)
-#define META_TEXTCOLOR_ACTION               (134)
-#define META_TEXTFILLCOLOR_ACTION           (135)
-#define META_TEXTALIGN_ACTION               (136)
-#define META_MAPMODE_ACTION                 (137)
-#define META_FONT_ACTION                    (138)
-#define META_PUSH_ACTION                    (139)
-#define META_POP_ACTION                     (140)
-#define META_RASTEROP_ACTION                (141)
-#define META_TRANSPARENT_ACTION             (142)
-#define META_EPS_ACTION                     (143)
-#define META_REFPOINT_ACTION                (144)
-#define META_TEXTLINECOLOR_ACTION           (145)
-#define META_TEXTLINE_ACTION                (146)
-#define META_FLOATTRANSPARENT_ACTION        (147)
-#define META_GRADIENTEX_ACTION              (148)
-#define META_LAYOUTMODE_ACTION              (149)
-#define META_TEXTLANGUAGE_ACTION            (150)
-#define META_OVERLINECOLOR_ACTION           (151)
+enum class MetaActionType
+{
+    NONE                    = 0,
+    PIXEL                   = 100,
+    POINT                   = 101,
+    LINE                    = 102,
+    RECT                    = 103,
+    ROUNDRECT               = 104,
+    ELLIPSE                 = 105,
+    ARC                     = 106,
+    PIE                     = 107,
+    CHORD                   = 108,
+    POLYLINE                = 109,
+    POLYGON                 = 110,
+    POLYPOLYGON             = 111,
+    TEXT                    = 112,
+    TEXTARRAY               = 113,
+    STRETCHTEXT             = 114,
+    TEXTRECT                = 115,
+    BMP                     = 116,
+    BMPSCALE                = 117,
+    BMPSCALEPART            = 118,
+    BMPEX                   = 119,
+    BMPEXSCALE              = 120,
+    BMPEXSCALEPART          = 121,
+    MASK                    = 122,
+    MASKSCALE               = 123,
+    MASKSCALEPART           = 124,
+    GRADIENT                = 125,
+    HATCH                   = 126,
+    WALLPAPER               = 127,
+    CLIPREGION              = 128,
+    ISECTRECTCLIPREGION     = 129,
+    ISECTREGIONCLIPREGION   = 130,
+    MOVECLIPREGION          = 131,
+    LINECOLOR               = 132,
+    FILLCOLOR               = 133,
+    TEXTCOLOR               = 134,
+    TEXTFILLCOLOR           = 135,
+    TEXTALIGN               = 136,
+    MAPMODE                 = 137,
+    FONT                    = 138,
+    PUSH                    = 139,
+    POP                     = 140,
+    RASTEROP                = 141,
+    TRANSPARENT             = 142,
+    EPS                     = 143,
+    REFPOINT                = 144,
+    TEXTLINECOLOR           = 145,
+    TEXTLINE                = 146,
+    FLOATTRANSPARENT        = 147,
+    GRADIENTEX              = 148,
+    LAYOUTMODE              = 149,
+    TEXTLANGUAGE            = 150,
+    OVERLINECOLOR           = 151,
 
-#define META_COMMENT_ACTION                 (512)
+    COMMENT                 = 512,
+    LAST                    = COMMENT
+};
 
 #endif // INCLUDED_VCL_METAACTIONTYPES_HXX
 

cppcanvas/source/inc/implrenderer.hxx
@@ -255,8 +255,8 @@ static float GetSwapFloat( SvStream& rSt )
                               sal_Int32&   io_rCurrActionIndex );
 
             static bool isActionContained( GDIMetaFile& rMtf,
-                                    const char*  pCommentString,
-                                    sal_uInt16       nType );
+                                    const char*     pCommentString,
+                                    MetaActionType  nType );
 
             void createGradientAction( const ::tools::PolyPolygon&    rPoly,
                                        const ::Gradient&              rGradient,

cppcanvas/source/mtfrenderer/transparencygroupaction.cxx
@@ -264,60 +264,60 @@ namespace cppcanvas
                         {
                             switch( pCurrAct->GetType() )
                             {
-                                case META_PUSH_ACTION:
-                                case META_POP_ACTION:
-                                case META_CLIPREGION_ACTION:
-                                case META_ISECTRECTCLIPREGION_ACTION:
-                                case META_ISECTREGIONCLIPREGION_ACTION:
-                                case META_MOVECLIPREGION_ACTION:
-                                case META_LINECOLOR_ACTION:
-                                case META_FILLCOLOR_ACTION:
-                                case META_TEXTCOLOR_ACTION:
-                                case META_TEXTFILLCOLOR_ACTION:
-                                case META_TEXTLINECOLOR_ACTION:
-                                case META_TEXTALIGN_ACTION:
-                                case META_FONT_ACTION:
-                                case META_RASTEROP_ACTION:
-                                case META_REFPOINT_ACTION:
-                                case META_LAYOUTMODE_ACTION:
+                                case MetaActionType::PUSH:
+                                case MetaActionType::POP:
+                                case MetaActionType::CLIPREGION:
+                                case MetaActionType::ISECTRECTCLIPREGION:
+                                case MetaActionType::ISECTREGIONCLIPREGION:
+                                case MetaActionType::MOVECLIPREGION:
+                                case MetaActionType::LINECOLOR:
+                                case MetaActionType::FILLCOLOR:
+                                case MetaActionType::TEXTCOLOR:
+                                case MetaActionType::TEXTFILLCOLOR:
+                                case MetaActionType::TEXTLINECOLOR:
+                                case MetaActionType::TEXTALIGN:
+                                case MetaActionType::FONT:
+                                case MetaActionType::RASTEROP:
+                                case MetaActionType::REFPOINT:
+                                case MetaActionType::LAYOUTMODE:
                                     // state-changing action - copy as-is
                                     aMtf.AddAction( pCurrAct->Clone() );
                                     break;
 
-                                case META_GRADIENT_ACTION:
-                                case META_HATCH_ACTION:
-                                case META_EPS_ACTION:
-                                case META_COMMENT_ACTION:
-                                case META_POINT_ACTION:
-                                case META_PIXEL_ACTION:
-                                case META_LINE_ACTION:
-                                case META_RECT_ACTION:
-                                case META_ROUNDRECT_ACTION:
-                                case META_ELLIPSE_ACTION:
-                                case META_ARC_ACTION:
-                                case META_PIE_ACTION:
-                                case META_CHORD_ACTION:
-                                case META_POLYLINE_ACTION:
-                                case META_POLYGON_ACTION:
-                                case META_POLYPOLYGON_ACTION:
-                                case META_BMP_ACTION:
-                                case META_BMPSCALE_ACTION:
-                                case META_BMPSCALEPART_ACTION:
-                                case META_BMPEX_ACTION:
-                                case META_BMPEXSCALE_ACTION:
-                                case META_BMPEXSCALEPART_ACTION:
-                                case META_MASK_ACTION:
-                                case META_MASKSCALE_ACTION:
-                                case META_MASKSCALEPART_ACTION:
-                                case META_GRADIENTEX_ACTION:
-                                case META_WALLPAPER_ACTION:
-                                case META_TRANSPARENT_ACTION:
-                                case META_FLOATTRANSPARENT_ACTION:
-                                case META_TEXT_ACTION:
-                                case META_TEXTARRAY_ACTION:
-                                case META_TEXTLINE_ACTION:
-                                case META_TEXTRECT_ACTION:
-                                case META_STRETCHTEXT_ACTION:
+                                case MetaActionType::GRADIENT:
+                                case MetaActionType::HATCH:
+                                case MetaActionType::EPS:
+                                case MetaActionType::COMMENT:
+                                case MetaActionType::POINT:
+                                case MetaActionType::PIXEL:
+                                case MetaActionType::LINE:
+                                case MetaActionType::RECT:
+                                case MetaActionType::ROUNDRECT:
+                                case MetaActionType::ELLIPSE:
+                                case MetaActionType::ARC:
+                                case MetaActionType::PIE:
+                                case MetaActionType::CHORD:
+                                case MetaActionType::POLYLINE:
+                                case MetaActionType::POLYGON:
+                                case MetaActionType::POLYPOLYGON:
+                                case MetaActionType::BMP:
+                                case MetaActionType::BMPSCALE:
+                                case MetaActionType::BMPSCALEPART:
+                                case MetaActionType::BMPEX:
+                                case MetaActionType::BMPEXSCALE:
+                                case MetaActionType::BMPEXSCALEPART:
+                                case MetaActionType::MASK:
+                                case MetaActionType::MASKSCALE:
+                                case MetaActionType::MASKSCALEPART:
+                                case MetaActionType::GRADIENTEX:
+                                case MetaActionType::WALLPAPER:
+                                case MetaActionType::TRANSPARENT:
+                                case MetaActionType::FLOATTRANSPARENT:
+                                case MetaActionType::TEXT:
+                                case MetaActionType::TEXTARRAY:
+                                case MetaActionType::TEXTLINE:
+                                case MetaActionType::TEXTRECT:
+                                case MetaActionType::STRETCHTEXT:
                                     // output-generating action - only
                                     // copy, if we're within the
                                     // requested subset

drawinglayer/source/processor2d/vclmetafileprocessor2d.cxx
@@ -1180,7 +1180,7 @@ namespace drawinglayer
                         SvtGraphicStroke* pSvtGraphicStroke = 0;
 
                         // #i121267# Not needed, does not give better quality compared with
-                        // the META_POLYPOLYGON_ACTION written by RenderPolygonHairlinePrimitive2D
+                        // the MetaActionType::POLYPOLYGON written by RenderPolygonHairlinePrimitive2D
                         // below
                         bool bSupportSvtGraphicStroke(false);
 
@@ -1686,7 +1686,7 @@ namespace drawinglayer
                     SvtGraphicFill* pSvtGraphicFill = 0;
 
                     // #i121267# Not needed, does not give better quality compared with
-                    // the META_POLYPOLYGON_ACTION written by the DrawPolyPolygon command
+                    // the MetaActionType::POLYPOLYGON written by the DrawPolyPolygon command
                     // below
                     bool bSupportSvtGraphicFill(false);
 
@@ -1864,7 +1864,7 @@ namespace drawinglayer
                                 SvtGraphicFill* pSvtGraphicFill = 0;
 
                                 // #i121267# Not needed, does not give better quality compared with
-                                // the META_POLYPOLYGON_ACTION written by the DrawPolyPolygon command
+                                // the MetaActionType::POLYPOLYGON written by the DrawPolyPolygon command
                                 // below
                                 bool bSupportSvtGraphicFill(false);
 

filter/qa/cppunit/filters-pict-test.cxx
@@ -86,7 +86,7 @@ void PictFilterTest::testDontClipTooMuch()
 
     MetafileXmlDump dumper;
     dumper.filterAllActionTypes();
-    dumper.filterActionType(META_CLIPREGION_ACTION, false);
+    dumper.filterActionType(MetaActionType::CLIPREGION, false);
     xmlDocPtr pDoc = dumper.dumpAndParse(aGDIMetaFile);
 
     CPPUNIT_ASSERT (pDoc);

filter/source/flash/swfexporter.cxx
@@ -711,18 +711,19 @@ bool FlashExporter::getMetaFile( Reference< XComponent >&xComponent, GDIMetaFile
         Rectangle clipRect;
         for( size_t i = 0, nCount = rMtf.GetActionSize(); i < nCount; i++ )
         {
-            const MetaAction*   pAction = rMtf.GetAction( i );
-            const sal_uInt16    nType = pAction->GetType();
+            const MetaAction*    pAction = rMtf.GetAction( i );
+            const MetaActionType nType = pAction->GetType();
 
             switch( nType )
             {
-                case( META_ISECTRECTCLIPREGION_ACTION ):
+                case( MetaActionType::ISECTRECTCLIPREGION ):
                 {
                     const MetaISectRectClipRegionAction* pA = static_cast<const MetaISectRectClipRegionAction*>(pAction);
                     clipRect = pA->GetRect();
                     i = nCount;
                     break;
                 }
+                default: break;
             }
         }
         MetaBmpExScaleAction *pmetaAct = new MetaBmpExScaleAction(Point(clipRect.Left(), clipRect.Top()), Size(clipRect.GetWidth(), clipRect.GetHeight()), rBitmapEx);

commit 4cedf7c2a1a7661d95ecca572ca24382e54dbe7d

restore accidentally altered license header
ad0c5e6c663642c1e4b212e4e6a38ebfe8c3e0a7 accidentally made some
purely cosmetic change to the license section.
This restore it to is original canonical content.

include/vcl/metaactiontypes.hxx
@@ -8,11 +8,11 @@
  *
  * This file incorporates work covered by the following license notice:
  *
- *   Licensed to the Apache Software Foundation = ASF, under one or more
+ *   Licensed to the Apache Software Foundation (ASF), under one or more
  *   contributor license agreements. See the NOTICE file distributed
  *   with this work for additional information regarding copyright
  *   ownership. The ASF licenses this file to you under the Apache
- *   License, Version 2.0 = the "License",; you may not use this file
+ *   License, Version 2.0 (the "License"); you may not use this file
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */