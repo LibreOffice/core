fd5a67308d4238230437477af98783a97b7b6381
David Tardon <dtardon@redhat.com>
2015-05-16 15:13:12 +0200

    upload libmwaw 0.3.5
    Change-Id: Ife74524f82190a06775656271965c869da8702a9

--- external/libmwaw/UnpackedTarball_libmwaw.mk
+++ external/libmwaw/UnpackedTarball_libmwaw.mk
@@ -14,8 +14,6 @@ $(eval $(call gb_UnpackedTarball_set_tarball,libmwaw,$(MWAW_TARBALL)))
 $(eval $(call gb_UnpackedTarball_set_patchlevel,libmwaw,1))
 
 $(eval $(call gb_UnpackedTarball_add_patches,libmwaw,\
-	external/libmwaw/0001-ClarisWorks-AppleWorks-spreadsheet-do-not-remove-fir.patch \
-	external/libmwaw/0002-ClarisWors-AppleWorks-spreadsheet-Oops-correct-of-a-.patch \
 	external/libmwaw/ubsan.patch.0 \
 ))
 
