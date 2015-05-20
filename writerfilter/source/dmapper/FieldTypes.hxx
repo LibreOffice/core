4a61fba7f1d1c457b8b5049644d793bd209d4f75
Michael Stahl <mstahl@redhat.com>
2015-05-20 17:12:28 +0200

    writerfilter: remove mojibake from comments
    Change-Id: I8662c3d34c0025379d4e7757b233befa7cae8235

--- writerfilter/source/dmapper/FieldTypes.hxx
+++ writerfilter/source/dmapper/FieldTypes.hxx
@@ -259,7 +259,7 @@ enum FieldId
     \f Builds a table of contents using TC entries instead of outline levels
     \h Hyperlinks the entries and page numbers within the table of contents
     \l Defines the TC entries field level used to build a table of contents
-    \n Builds a table of contents or a range of entries, sucah as �1-9�, in a table of contents without page numbers
+    \n Builds a table of contents or a range of entries, such as 1-9, in a table of contents without page numbers
     \o  Builds a table of contents by using outline levels instead of TC entries
     \p Defines the separator between the table entry and its page number
     \s  Builds a table of contents by using a sequence type
@@ -271,7 +271,7 @@ enum FieldId
      */
     ,FIELD_TOC
     /*
-     TOC entry: �text�
+     TOC entry: text
         \f TC entry in doc with multiple tables
         \l Outline Level
         \n Suppress page numbers
