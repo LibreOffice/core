8dbeb6368cdcb1605e6750771f96b5865e0c5e93
Miklos Vajna <vmiklos@collabora.co.uk>
2015-05-21 13:17:18 +0100

    gtktiledviewer: handle LOK_CALLBACK_SEARCH_NOT_FOUND
    Change-Id: Ic545bbd14d11a3b310f4d72ee02e51a7cb6f4f22
    (cherry picked from commit cb0b7ff34dc193c37e89f4a968e1cb7502338230)

--- libreofficekit/source/gtk/lokdocview.cxx
+++ libreofficekit/source/gtk/lokdocview.cxx
@@ -192,6 +192,8 @@ struct LOKDocView_Impl
     void globalCallbackWorkerImpl(int nType, const char* pPayload);
     /// Command state (various buttons like bold are toggled or not) is changed.
     void commandChanged(const std::string& rPayload);
+    /// Search did not find any matches.
+    void searchNotFound(const std::string& rPayload);
 };
 
 namespace {
@@ -1006,6 +1008,9 @@ gboolean LOKDocView_Impl::callbackImpl(CallbackData* pCallback)
     }
     break;
     case LOK_CALLBACK_SEARCH_NOT_FOUND:
+    {
+        searchNotFound(pCallback->m_aPayload);
+    }
     break;
     case LOK_CALLBACK_PAGE_COUNT_CHANGED:
     {
@@ -1060,6 +1065,7 @@ enum
 {
     EDIT_CHANGED,
     COMMAND_CHANGED,
+    SEARCH_NOT_FOUND,
     LAST_SIGNAL
 };
 
@@ -1068,6 +1074,11 @@ static guint docview_signals[LAST_SIGNAL] = { 0 };
 void LOKDocView_Impl::commandChanged(const std::string& rString)
 {
     g_signal_emit(m_pDocView, docview_signals[COMMAND_CHANGED], 0, rString.c_str());
+}
+
+void LOKDocView_Impl::searchNotFound(const std::string& rString)
+{
+    g_signal_emit(m_pDocView, docview_signals[SEARCH_NOT_FOUND], 0, rString.c_str());
 }
 
 static void lok_docview_class_init( gpointer ptr )
@@ -1090,6 +1101,16 @@ static void lok_docview_class_init( gpointer ptr )
                      G_TYPE_FROM_CLASS(gobject_class),
                      G_SIGNAL_RUN_FIRST,
                      G_STRUCT_OFFSET(LOKDocViewClass, command_changed),
+                     NULL, NULL,
+                     g_cclosure_marshal_VOID__STRING,
+                     G_TYPE_NONE, 1,
+                     G_TYPE_STRING);
+    pClass->search_not_found = 0;
+    docview_signals[SEARCH_NOT_FOUND] =
+        g_signal_new("search-not-found",
+                     G_TYPE_FROM_CLASS(gobject_class),
+                     G_SIGNAL_RUN_FIRST,
+                     G_STRUCT_OFFSET(LOKDocViewClass, search_not_found),
                      NULL, NULL,
                      g_cclosure_marshal_VOID__STRING,
                      G_TYPE_NONE, 1,
