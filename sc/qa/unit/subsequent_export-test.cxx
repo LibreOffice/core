d7e60c1242a5ae820753e6161371315336404fee
PriyankaGaikwad <priyanka.gaikwad@synerzip.com>
2015-05-27 12:22:10 +0530

    tdf#58122 FILESAVE: XLSX - Cell RTL direction state not saved
    Current behavior:
    The direction of the sheet is set to default from left to right
    
    Expected behavior:
    The action of changing the direction from left to right to become from right to left
    
    Solution:
    Added export for text writting direction.
    
    Now <alignment readingorder="2"> export properly
    
    Change-Id: I0a49ba0e458cbd6a7feae8e205ae583b7da30d0b

--- sc/qa/unit/subsequent_export-test.cxx
+++ sc/qa/unit/subsequent_export-test.cxx
@@ -154,6 +154,7 @@ public:
     void testMoveCellAnchoredShapes();
     void testMatrixMultiplication();
     void testPreserveTextWhitespaceXLSX();
+    void testTextDirection();
 
     CPPUNIT_TEST_SUITE(ScExportTest);
     CPPUNIT_TEST(test);
@@ -215,6 +216,7 @@ public:
     CPPUNIT_TEST(testHyperlinkXLSX);
     CPPUNIT_TEST(testMoveCellAnchoredShapes);
     CPPUNIT_TEST(testMatrixMultiplication);
+    CPPUNIT_TEST(testTextDirection);
 
 
     CPPUNIT_TEST_SUITE_END();
@@ -2899,6 +2901,18 @@ void ScExportTest::testMatrixMultiplication()
     CPPUNIT_ASSERT_EQUAL(OUString("array"), CellFormulaType);
 
     xDocSh->DoClose();
+}
+
+void ScExportTest::testTextDirection()
+{
+    ScDocShellRef xDocSh = loadDoc("writingMode.", XLSX);
+    CPPUNIT_ASSERT(xDocSh.Is());
+
+    xmlDocPtr pDoc = XPathHelper::parseExport(&(*xDocSh), m_xSFactory, "xl/styles.xml", XLSX);
+    CPPUNIT_ASSERT(pDoc);
+
+    assertXPath(pDoc, "/x:styleSheet/x:cellXfs/x:xf[2]/x:alignment", "readingOrder", "1");//LTR
+    assertXPath(pDoc, "/x:styleSheet/x:cellXfs/x:xf[3]/x:alignment", "readingOrder", "2");//RTL
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(ScExportTest);
