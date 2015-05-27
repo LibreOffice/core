LibreOffice/core

commit 0ed7c8b55a026d7d98f9c994ad5e8626dd9504b2

do not use manual iteration
Change-Id: I71da7d2810309266b6e37fd531232ca33928f5b9

sw/source/core/crsr/crsrsh.cxx
@@ -907,8 +907,12 @@ void SwCrsrShell::ClearMark()
     // is there any GetMark?
     if( m_pTblCrsr )
     {
-        while( m_pCurCrsr->GetNext() != m_pCurCrsr )
-            delete m_pCurCrsr->GetNext();
+        std::vector<SwViewShell*> vShells;
+        for(SwViewShell& rShell : GetRingContainer())
+            if(&rShell != this)
+                vShells.push_back(&rShell);
+        for(SwViewShell* pShell : vShells)
+            delete pShell;
         m_pTblCrsr->DeleteMark();
 
         m_pCurCrsr->DeleteMark();
@@ -2678,7 +2682,7 @@ SwCrsrShell::SwCrsrShell( SwDoc& rDoc, vcl::Window *pInitWin,
 SwCrsrShell::~SwCrsrShell()
 {
     // if it is not the last view then at least the field should be updated
-    if( GetNext() != this )
+    if( !unique() )
         CheckTblBoxCntnt( m_pCurCrsr->GetPoint() );
     else
         ClearTblBoxCntnt();

commit adce9bf3ad0f12490fc3c8ae429045579642886c

tdf#91145: Set Cell Background Color in Table Broken in Writer 5.0
- iterate over PaMs, not over Shells
- regression from 0ed7c8b55a026d7d98f9c994ad5e8626dd9504b2

Change-Id: I33ea486d416a7e64fd97938ce09492209c102c88

sw/source/core/crsr/crsrsh.cxx
@@ -908,12 +908,12 @@ void SwCrsrShell::ClearMark()
     // is there any GetMark?
     if( m_pTableCrsr )
     {
-        std::vector<SwViewShell*> vShells;
-        for(SwViewShell& rShell : GetRingContainer())
-            if(&rShell != this)
-                vShells.push_back(&rShell);
-        for(SwViewShell* pShell : vShells)
-            delete pShell;
+        std::vector<SwPaM*> vCrsrs;
+        for(auto& rCrsr : m_pCurCrsr->GetRingContainer())
+            if(&rCrsr != m_pCurCrsr)
+                vCrsrs.push_back(&rCrsr);
+        for(auto pCrsr : vCrsrs)
+            delete pCrsr;
         m_pTableCrsr->DeleteMark();
 
         m_pCurCrsr->DeleteMark();