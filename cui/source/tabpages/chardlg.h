f2bd8b5e0907d804feb7082708bc8be7b6075a26
Noel Grandin <noel@peralex.com>
2015-05-28 13:54:50 +0200

    remove unused CHRDLG_ defines
    Change-Id: I160bb11908ad2c7e41e23a2f2837fad9fafa4ff2

--- cui/source/tabpages/chardlg.h
+++ cui/source/tabpages/chardlg.h
@@ -21,34 +21,6 @@
 
 // define ----------------------------------------------------------------
 
-#define CHRDLG_UNDERLINE_NONE               0
-#define CHRDLG_UNDERLINE_SINGLE             1
-#define CHRDLG_UNDERLINE_DOUBLE             2
-#define CHRDLG_UNDERLINE_DOTTED             3
-#define CHRDLG_UNDERLINE_DONTKNOW           4
-#define CHRDLG_UNDERLINE_DASH               5
-#define CHRDLG_UNDERLINE_LONGDASH           6
-#define CHRDLG_UNDERLINE_DASHDOT            7
-#define CHRDLG_UNDERLINE_DASHDOTDOT         8
-#define CHRDLG_UNDERLINE_SMALLWAVE          9
-#define CHRDLG_UNDERLINE_WAVE               10
-#define CHRDLG_UNDERLINE_DOUBLEWAVE         11
-#define CHRDLG_UNDERLINE_BOLD               12
-#define CHRDLG_UNDERLINE_BOLDDOTTED         13
-#define CHRDLG_UNDERLINE_BOLDDASH           14
-#define CHRDLG_UNDERLINE_BOLDLONGDASH       15
-#define CHRDLG_UNDERLINE_BOLDDASHDOT        16
-#define CHRDLG_UNDERLINE_BOLDDASHDOTDOT     17
-#define CHRDLG_UNDERLINE_BOLDWAVE           18
-
-#define CHRDLG_STRIKEOUT_NONE               0
-#define CHRDLG_STRIKEOUT_SINGLE             1
-#define CHRDLG_STRIKEOUT_DOUBLE             2
-#define CHRDLG_STRIKEOUT_DONTKNOW           3
-#define CHRDLG_STRIKEOUT_BOLD               4
-#define CHRDLG_STRIKEOUT_SLASH              5
-#define CHRDLG_STRIKEOUT_X                  6
-
 #define CHRDLG_ENCLOSE_NONE                 0
 #define CHRDLG_ENCLOSE_ROUND                1
 #define CHRDLG_ENCLOSE_SQUARE               2
