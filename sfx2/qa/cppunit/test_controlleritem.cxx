5ccc46b0fe2df55d4f4794978bd62dbd02fc7fec
Michael Meeks <michael.meeks@collabora.com>
2015-06-02 16:05:33 +0100

    Skeletal SfxControllerItem / bindings unit test.
    Change-Id: I0fdda736243c3e4a74f2faedf6fa5ea49453fe8c
    Reviewed-on: https://gerrit.libreoffice.org/16040
    Reviewed-by: Michael Meeks <michael.meeks@collabora.com>
    Tested-by: Michael Meeks <michael.meeks@collabora.com>

---
+++ sfx2/qa/cppunit/test_controlleritem.cxx
@@ -0,0 +1,62 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <sal/types.h>
+#include <cppunit/TestAssert.h>
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/plugin/TestPlugIn.h>
+
+#include <sfx2/ctrlitem.hxx>
+#include <sfx2/bindings.hxx>
+
+namespace {
+
+class ControllerItemTest
+    : public ::CppUnit::TestFixture
+{
+public:
+    virtual void setUp() SAL_OVERRIDE {}
+    virtual void tearDown() SAL_OVERRIDE {}
+
+    void test();
+
+    CPPUNIT_TEST_SUITE(ControllerItemTest);
+    CPPUNIT_TEST(test);
+    CPPUNIT_TEST_SUITE_END();
+
+private:
+};
+
+static bool bDeleted = false;
+
+class FooController : public SfxControllerItem {
+public:
+    FooController() : SfxControllerItem() {}
+    ~FooController() { bDeleted = true; }
+};
+
+void ControllerItemTest::test()
+{
+    FooController *pController(new FooController());
+
+    // TESTME: binding, un-binding, re-binding, IsBound, SetId etc.
+
+    pController->dispose();
+    delete pController;
+    CPPUNIT_ASSERT( bDeleted );
+}
+
+CPPUNIT_TEST_SUITE_REGISTRATION(ControllerItemTest);
+
+}
+
+CPPUNIT_PLUGIN_IMPLEMENT();
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
