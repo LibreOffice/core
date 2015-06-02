432622050084e7f738da414feb8834e1191a3dbe
David Tardon <dtardon@redhat.com>
2015-06-02 17:49:48 +0200

    upload libetonyek 0.1.3
    (cherry picked from commit ee3d40b30816a8fc6d4e8f984659c8dfac19ec3b)
    
    Change-Id: I451f02ee93a798c9c1712082d59a7ff96b0d4a4e
    Reviewed-on: https://gerrit.libreoffice.org/16508
    Reviewed-by: Michael Stahl <mstahl@redhat.com>
    Tested-by: Michael Stahl <mstahl@redhat.com>

--- external/libetonyek/UnpackedTarball_libetonyek.mk
+++ external/libetonyek/UnpackedTarball_libetonyek.mk
@@ -15,12 +15,11 @@ $(eval $(call gb_UnpackedTarball_set_patchlevel,libetonyek,0))
 
 $(eval $(call gb_UnpackedTarball_add_patches,libetonyek,\
 	external/libetonyek/assert.patch \
-	external/libetonyek/0001-missing-include.patch.1 \
 	external/libetonyek/win_build.patch.1 \
 	external/libetonyek/ubsan.patch \
-	external/libetonyek/0001-add-missing-breaks.patch.1 \
-	external/libetonyek/0002-remove-surplus-breaks.patch.1 \
 	external/libetonyek/rpath.patch \
+	external/libetonyek/0001-add-missing-include.patch.1 \
+	external/libetonyek/0001-try-to-fix-build-on-Windows.patch.1 \
 ))
 
 ifeq ($(COM_GCC_IS_CLANG),TRUE)
