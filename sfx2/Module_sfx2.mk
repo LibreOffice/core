463b7a9eab455e75d8f81f139c341f3041c1c228
Michael Meeks <michael.meeks@collabora.com>
2015-06-02 16:05:33 +0100

    Skeletal SfxControllerItem / bindings unit test.
    Change-Id: I0fdda736243c3e4a74f2faedf6fa5ea49453fe8c

--- sfx2/Module_sfx2.mk
+++ sfx2/Module_sfx2.mk
@@ -30,6 +30,7 @@ $(eval $(call gb_Module_add_l10n_targets,sfx2,\
 
 $(eval $(call gb_Module_add_check_targets,sfx2,\
     CppunitTest_sfx2_metadatable \
+    CppunitTest_sfx2_controlleritem \
 ))
 
 ifneq ($(ENABLE_JAVA),)
