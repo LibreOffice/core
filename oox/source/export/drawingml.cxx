21d0ae24a8403cab29dbab3d281131c679330b63
Stephan Bergmann <sbergman@redhat.com>
2015-06-02 11:27:27 +0200

    loplugin:cstylecast: deal with those that are (technically) const_cast
    Change-Id: I4373b1698ed81cd84dc5f319fde003f9b8c301fb

--- oox/source/export/drawingml.cxx
+++ oox/source/export/drawingml.cxx
@@ -1479,7 +1479,7 @@ void DrawingML::GetUUID( OStringBuffer& rBuffer )
 {
     Sequence< sal_uInt8 > aSeq( 16 );
     static const char cDigits[17] = "0123456789ABCDEF";
-    rtl_createUuid( (sal_uInt8*)aSeq.getArray(), 0, true );
+    rtl_createUuid( aSeq.getArray(), 0, true );
     int i;
 
     rBuffer.append( '{' );
