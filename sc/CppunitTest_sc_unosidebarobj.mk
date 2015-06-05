62844ee1016852d4de19f8dfb8fc5c207f93debf
Markus Mohrhard <markus.mohrhard@googlemail.com>
2015-06-05 14:11:21 +0200

    add missing component
    Change-Id: Ic7391f07c72349821fa834f7ce175341964b63ab

--- sc/CppunitTest_sc_unosidebarobj.mk
+++ sc/CppunitTest_sc_unosidebarobj.mk
@@ -92,6 +92,7 @@ $(eval $(call gb_CppunitTest_use_components,sc_unosidebarobj,\
     sfx2/util/sfx \
     sot/util/sot \
     svl/source/fsstor/fsstorage \
+    svx/util/svx \
     toolkit/util/tk \
     ucb/source/core/ucb1 \
     ucb/source/ucp/file/ucpfile1 \
