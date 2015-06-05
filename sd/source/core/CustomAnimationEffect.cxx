bc8adace59ab5bebb93610e0ec3b16ef2a8f5151
Julien Nabet <serval2412@yahoo.fr>
2015-06-06 00:00:56 +0200

    Typo: readd -> read
    Change-Id: I62b44c9dfe3e4db963230a3dc35ddb3359453911

--- sd/source/core/CustomAnimationEffect.cxx
+++ sd/source/core/CustomAnimationEffect.cxx
@@ -2675,7 +2675,7 @@ void EffectSequenceHelper::setAnimateForm( CustomAnimationTextGroupPtr pTextGrou
         }
         else
         {
-            // readd the rest to the group again
+            // read the rest to the group again
             while( aIter != aEnd )
             {
                 CustomAnimationEffectPtr pEffect( (*aIter++) );
