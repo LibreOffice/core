84bf617f32d9f1de573e6c760911ec5740db0843
Stephan Bergmann <sbergman@redhat.com>
2015-06-08 16:23:09 +0200

    loplugin:cstylecast: deal with remaining pointer casts
    Change-Id: I2c65b3bd52bc3d8c3dfb8d05c9dcc5d2a8484549

--- avmedia/source/macavf/framegrabber.mm
+++ avmedia/source/macavf/framegrabber.mm
@@ -96,7 +96,7 @@ uno::Reference< graphic::XGraphic > SAL_CALL FrameGrabber::grabFrame( double fMe
     CGImageDestinationFinalize( pCGImgDest );
     CFRelease( pCGImgDest );
     const long nBitmapLen = CFDataGetLength( pCFData );
-    void* pBitmapBytes = (void*)CFDataGetBytePtr( pCFData );
+    UInt8 * pBitmapBytes = const_cast<UInt8 *>(CFDataGetBytePtr( pCFData ));
 
     // convert the image into the return-value type which is a graphic::XGraphic
     SvMemoryStream aMemStm( pBitmapBytes, nBitmapLen, StreamMode::READ | StreamMode::WRITE );
