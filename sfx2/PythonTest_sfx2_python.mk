dc88e395600b733fe302f904dd66f4b4612b2db2
Laurent Godard <lgodard.libre@laposte.net>
2015-06-08 10:24:42 +0200

    Uno api sidebar unit test tdf#91806
    - python test subsequentcheck
    - correct deck setTitle APi (UI update)
    
    Change-Id: Id04cb78c6162ac84fb3bfd8577f84763109d993e

---
+++ sfx2/PythonTest_sfx2_python.mk
@@ -0,0 +1,20 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_PythonTest_PythonTest,sfx2_python))
+
+$(eval $(call gb_PythonTest_set_defs,sfx2_python,\
+    TDOC="$(SRCDIR)/sfx2/qa/python/testdocuments" \
+))
+
+$(eval $(call gb_PythonTest_add_modules,sfx2_python,$(SRCDIR)/sfx2/qa/python,\
+	check_sidebar \
+))
+
+# vim: set noet sw=4 ts=4:
