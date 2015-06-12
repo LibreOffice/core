4764958607d8360091edf5a548509e7627df7aa8
Stephan Bergmann <sbergman@redhat.com>
2015-06-12 15:52:51 +0200

    For Clang -fsanitize=vptr use -fvisibility-ms-compat, not -fvisibility=hidden
    ...same as f0aa1a78fb209310e8baef53c02f365fca518d11
    
    Change-Id: I35c1e6928dd4b29ef0ab448a181a5af7fb0640ab

--- external/libfreehand/UnpackedTarball_libfreehand.mk
+++ external/libfreehand/UnpackedTarball_libfreehand.mk
@@ -11,4 +11,14 @@ $(eval $(call gb_UnpackedTarball_UnpackedTarball,libfreehand))
 
 $(eval $(call gb_UnpackedTarball_set_tarball,libfreehand,$(FREEHAND_TARBALL)))
 
+$(eval $(call gb_UnpackedTarball_set_patchlevel,libfreehand,0))
+
+ifeq ($(COM_GCC_IS_CLANG),TRUE)
+ifneq ($(filter -fsanitize=%,$(CC)),)
+$(eval $(call gb_UnpackedTarball_add_patches,libfreehand, \
+    external/libfreehand/ubsan-visibility.patch \
+))
+endif
+endif
+
 # vim: set noet sw=4 ts=4:
