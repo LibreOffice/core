876c9f428a7af80368ee867f398c25e80fc568c7
Miklos Vajna <vmiklos@collabora.co.uk>
2015-06-15 09:11:20 +0200

    sysui: drop dead assignments
    Change-Id: I95748d5acc523a571c230e23dae3fd0d8ce089fa

--- sysui/productlist.mk
+++ sysui/productlist.mk
@@ -7,14 +7,12 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 
-PRODUCTLIST := libreoffice libreofficedev oxygenoffice
+PRODUCTLIST := libreoffice libreofficedev
 PKGVERSION := $(LIBO_VERSION_MAJOR).$(LIBO_VERSION_MINOR).$(LIBO_VERSION_MICRO)
 PKGVERSIONSHORT := $(LIBO_VERSION_MAJOR).$(LIBO_VERSION_MINOR)
 PRODUCTNAME.libreoffice := LibreOffice
 PRODUCTNAME.libreofficeodev := LibreOfficeDev
-PRODUCTNAME.oxygenoffice := OxygenOffice
 UNIXFILENAME.libreoffice := libreoffice$(PKGVERSIONSHORT)
 UNIXFILENAME.libreofficedev := libreofficedev$(PKGVERSIONSHORT)
-UNIXFILENAME.oxygenoffice := oxygenoffice$(PKGVERSIONSHORT)
 
 # vim: set noet sw=4 ts=4:
