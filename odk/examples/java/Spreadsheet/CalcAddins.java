LibreOffice/core

commit 70f56bc22fe952c75ec714e05e1bb5296491a36a

java: reduce scope, make member classes private
found by UCDetector

Change-Id: Ief32d078090102b14b60b35fc36542f8d4fb252b

odk/examples/java/Spreadsheet/CalcAddins.java
@@ -60,7 +60,7 @@ public class CalcAddins {
  * of the service description. It implements the needed interfaces.
  * @implements XCalcAddins, XAddIn, XServiceName, XServiceInfo, XTypeProvider
  */
-    static public class _CalcAddins extends WeakBase implements
+    static private class _CalcAddins extends WeakBase implements
                                             XCalcAddins,
                                             XAddIn,
                                             XServiceName,

commit 3b07120a72a5fd28836b226becbc100e24e84edf

Fix CalcAddins example
was broken with 70f56bc22fe952c75ec714e05e1bb5296491a36a "java: reduce scope,
make member classes private"

Change-Id: I84a3fafbfdddf6bb04ab7787d344bf50ffd22994

odk/examples/java/Spreadsheet/CalcAddins.java
@@ -60,7 +60,7 @@ public class CalcAddins {
  * of the service description. It implements the needed interfaces.
  * @implements XCalcAddins, XAddIn, XServiceName, XServiceInfo, XTypeProvider
  */
-    private static class _CalcAddins extends WeakBase implements
+    public static class _CalcAddins extends WeakBase implements
                                             XCalcAddins,
                                             XAddIn,
                                             XServiceName,