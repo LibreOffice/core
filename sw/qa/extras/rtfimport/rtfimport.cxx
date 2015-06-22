2b78f54d06769d42fd73b6bc3240b14097255c34
Miklos Vajna <vmiklos@collabora.co.uk>
2015-06-22 09:05:30 +0200

    tdf#92061 RTF import: fix missing body text after footnote
    A missing seek in the \footnote handler could result in a situation that
    the missed text contained a "{" but not its matching "}", which resulted
    in the parser terminating earlier than the end of the document.
    
    (cherry picked from commit 7b08304b55cf2284a3c583426c60baef618ba206)
    
    Conflicts:
    	sw/qa/extras/rtfimport/rtfimport.cxx
    
    Reviewed-on: https://gerrit.libreoffice.org/16501
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>
    (cherry picked from commit 326ca9e0710d17274eb0a6cdc0d95d4f933784f0)
    
    Conflicts:
    	writerfilter/source/rtftok/rtfdocumentimpl.cxx
    
    Change-Id: I6df476b2d6397dfa918111b33854dc2f95fbe81d

--- sw/qa/extras/rtfimport/rtfimport.cxx
+++ sw/qa/extras/rtfimport/rtfimport.cxx
@@ -2125,6 +2125,12 @@ DECLARE_RTFIMPORT_TEST(testFdo75614, "tdf75614.rtf")
     CPPUNIT_ASSERT_EQUAL(OUString("after."), getRun(getParagraph(1), 3)->getString());
 }
 
+DECLARE_RTFIMPORT_TEST(testTdf92061, "tdf92061.rtf")
+{
+    // This was "C", i.e. part of the footnote ended up in the body text.
+    CPPUNIT_ASSERT_EQUAL(OUString("body-after"), getRun(getParagraph(1), 3)->getString());
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
