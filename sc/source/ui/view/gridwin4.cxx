62feefc607d4c714d3dea85394a92ebb056c1c6b
Krisztian Pinter <pin.terminator@gmail.com>
2015-06-23 00:56:35 +0200

    calc mapmode: Refactor DrawExtraShadow to use logic units
    Change-Id: I902e83f648b1d464f7702d14bd5cb0ff1b21c06a

--- sc/source/ui/view/gridwin4.cxx
+++ sc/source/ui/view/gridwin4.cxx
@@ -735,9 +735,10 @@ void ScGridWindow::DrawContent(OutputDevice &rDevice, const ScTableInfo& rTableI
         }
     }
 
+    aOutputData.DrawShadow(*pContentDev);
+
     pContentDev->SetMapMode(MAP_PIXEL);
 
-    aOutputData.DrawShadow(*pContentDev);
     aOutputData.DrawFrame(*pContentDev);
 
     // Show Note Mark
