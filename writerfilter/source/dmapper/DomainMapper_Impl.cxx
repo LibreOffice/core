53e86198f2a40f40f291fb6a9cf5cc67e45df94c
Mark Hung <marklh9@gmail.com>
2015-06-24 01:40:13 +0800

    tdf#91594 misinterprets letters from Symbol font in docx files
    Fix the issue caused by wrong assumption about symbol chracter
    and symbol font attributes order in writerfilter. Also allow
    symbols to be displayed if user's language is not Western.
    
    Reviewed-on: https://gerrit.libreoffice.org/16543
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Michael Stahl <mstahl@redhat.com>
    
    Conflicts:
    	writerfilter/source/dmapper/DomainMapper.cxx
    
    Change-Id: I602d9fbfa79c33c90f655dbf5ee22738b6391ae6
    Reviewed-on: https://gerrit.libreoffice.org/17456
    Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
    Tested-by: Miklos Vajna <vmiklos@collabora.co.uk>

--- writerfilter/source/dmapper/DomainMapper_Impl.cxx
+++ writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -175,7 +175,6 @@ DomainMapper_Impl::DomainMapper_Impl(
         m_bTOCPageRef(false),
         m_bStartGenericField(false),
         m_bTextInserted(false),
-        m_nSymboldata(-1),
         m_pLastSectionContext( ),
         m_pLastCharacterContext(),
         m_nCurrentTabStopIndex( 0 ),
@@ -401,11 +400,6 @@ void DomainMapper_Impl::RemoveLastParagraph( )
     catch( const uno::Exception& )
     {
     }
-}
-
-void DomainMapper_Impl::SetSymbolData( sal_Int32 nSymbolData )
-{
-    m_nSymboldata = nSymbolData;
 }
 
 
