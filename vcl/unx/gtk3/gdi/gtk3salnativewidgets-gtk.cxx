588d6e326157ae314528048f7f8e695fdafee39b
Stephan Bergmann <sbergman@redhat.com>
2015-06-24 15:08:56 +0200

    Another gross inclusion hack that can apparently go now
    Change-Id: I261596e5d20360ecfaf57f9ddc4419ca305e3194
    (cherry picked from commit 5cfc58783ebb896cc0d3b9be3f9da53cbaf89161)

--- vcl/unx/gtk3/gdi/gtk3salnativewidgets-gtk.cxx
+++ vcl/unx/gtk3/gdi/gtk3salnativewidgets-gtk.cxx
@@ -7,9 +7,9 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-// Gross inclusion hacks for now ...
+#include <sal/config.h>
 
-#include "../../../headless/svpgdi.cxx"
+#include <basegfx/range/b2ibox.hxx>
 #include <unx/gtk/gtkframe.hxx>
 #include <unx/gtk/gtkdata.hxx>
 #include <unx/gtk/gtkinst.hxx>
