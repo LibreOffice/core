053e92e8e6841a5636e533c8c37f483ee5d679b2
Szymon Kłos <eszkadev@gmail.com>
2015-06-24 16:07:08 +0200

    RemoteFilesDialog: showing of the filename depends on the dialog's type
    Change-Id: I753beb28d07a5cc74634d5fa2cd9a78c8711628f

--- svtools/source/dialogs/RemoteFilesDialog.cxx
+++ svtools/source/dialogs/RemoteFilesDialog.cxx
@@ -638,8 +638,16 @@ IMPL_LINK_NOARG ( RemoteFilesDialog, SelectHdl )
     SvTreeListEntry* pEntry = m_pFileView->FirstSelected();
     SvtContentEntry* pData = static_cast< SvtContentEntry* >( pEntry->GetUserData() );
 
-    INetURLObject aURL( pData->maURL );
-    m_pName_ed->SetText( INetURLObject::decode( aURL.GetLastName(), INetURLObject::DECODE_WITH_CHARSET ) );
+    if( ( pData->mbIsFolder && ( m_eType == REMOTEDLG_TYPE_PATHDLG ) )
+       || ( !pData->mbIsFolder && ( m_eType == REMOTEDLG_TYPE_FILEDLG ) ) )
+    {
+        INetURLObject aURL( pData->maURL );
+        m_pName_ed->SetText( INetURLObject::decode( aURL.GetLastName(), INetURLObject::DECODE_WITH_CHARSET ) );
+    }
+    else
+    {
+        m_pName_ed->SetText( "" );
+    }
 
     return 1;
 }
