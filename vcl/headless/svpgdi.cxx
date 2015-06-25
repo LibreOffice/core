e3acf2b71c6772d3c0c03fff87656c6a4815f1da
Caolán McNamara <caolanm@redhat.com>
2015-06-25 16:50:00 +0100

    try harder to hide this from android tinderbox
    (cherry picked from commit f1761903cdafd12d4b33d4e8733971a3d9ad8a10)
    
    Change-Id: I586fea9f1570b68ed789da8b10a3b46216c42969

--- vcl/headless/svpgdi.cxx
+++ vcl/headless/svpgdi.cxx
@@ -185,10 +185,9 @@ namespace
 bool SvpSalGraphics::drawAlphaRect(long nX, long nY, long nWidth, long nHeight, sal_uInt8 nTransparency)
 {
     bool bRet = false;
-#if !ENABLE_CAIRO_CANVAS
     (void)nX; (void)nY; (void)nWidth; (void)nHeight; (void)nTransparency;
-    return false;
-#elif defined(CAIRO_VERSION) && defined(CAIRO_VERSION_ENCODE) && CAIRO_VERSION >= CAIRO_VERSION_ENCODE(1, 6, 0)
+#if ENABLE_CAIRO_CANVAS
+#if CAIRO_VERSION >= CAIRO_VERSION_ENCODE(1, 6, 0)
     if (m_bUseLineColor || !m_bUseFillColor)
     {
         SAL_WARN("vcl.gdi", "unsupported SvpSalGraphics::drawAlphaRect case");
@@ -230,8 +229,7 @@ bool SvpSalGraphics::drawAlphaRect(long nX, long nY, long nWidth, long nHeight, 
                                                 extents.y + extents.height));
     }
     bRet = true;
-#else
-    (void)nX; (void)nY; (void)nWidth; (void)nHeight; (void)nTransparency;
+#endif
 #endif
     return bRet;
 }
