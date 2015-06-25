LibreOffice/core

commit 7b355da6853af6678c4ba22710d157cf8a6d43eb

Enhancement tdf#87972 :  Cannot repeat items labels on a pivot table
Change-Id: I44b2521ea548b51a1b3e9b42cfa64c5f50d7798a
Reviewed-on: https://gerrit.libreoffice.org/14504
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Kohei Yoshida <libreoffice@kohei.us>
Tested-by: Kohei Yoshida <libreoffice@kohei.us>

sc/source/core/data/dptabsrc.cxx
@@ -2015,7 +2015,8 @@ ScDPLevel::ScDPLevel( ScDPSource* pSrc, long nD, long nH, long nL ) :
     nSortMeasure( 0 ),
     nAutoMeasure( 0 ),
     bShowEmpty( false ),
-    bEnableLayout( false )
+    bEnableLayout( false ),
+    bRepeatItemLabels( false )
 {
     //TODO: hold pSource
     //  aSubTotals is empty
@@ -2201,6 +2202,7 @@ uno::Reference<beans::XPropertySetInfo> SAL_CALL ScDPLevel::getPropertySetInfo()
         { OUString(SC_UNO_DP_AUTOSHOW), 0,  cppu::UnoType<sheet::DataPilotFieldAutoShowInfo>::get(),     0, 0 },
         { OUString(SC_UNO_DP_LAYOUT),   0,  cppu::UnoType<sheet::DataPilotFieldLayoutInfo>::get(),       0, 0 },
         { OUString(SC_UNO_DP_SHOWEMPTY), 0, getBooleanCppuType(),                                   0, 0 },
+        { OUString(SC_UNO_DP_REPEATITEMLABELS), 0, getBooleanCppuType(),                                   0, 0 },
         { OUString(SC_UNO_DP_SORTING),  0,  cppu::UnoType<sheet::DataPilotFieldSortInfo>::get(),         0, 0 },
         { OUString(SC_UNO_DP_SUBTOTAL), 0,  getCppuType((uno::Sequence<sheet::GeneralFunction>*)0), 0, 0 },
         { OUString(), 0, css::uno::Type(), 0, 0 }
@@ -2217,6 +2219,8 @@ void SAL_CALL ScDPLevel::setPropertyValue( const OUString& aPropertyName, const 
 {
     if ( aPropertyName == SC_UNO_DP_SHOWEMPTY )
         bShowEmpty = lcl_GetBoolFromAny(aValue);
+    else if ( aPropertyName == SC_UNO_DP_REPEATITEMLABELS )
+        bRepeatItemLabels = lcl_GetBoolFromAny(aValue);
     else if ( aPropertyName == SC_UNO_DP_SUBTOTAL )
         aValue >>= aSubTotals;
     else if ( aPropertyName == SC_UNO_DP_SORTING )
@@ -2238,6 +2242,8 @@ uno::Any SAL_CALL ScDPLevel::getPropertyValue( const OUString& aPropertyName )
     uno::Any aRet;
     if ( aPropertyName == SC_UNO_DP_SHOWEMPTY )
         lcl_SetBoolInAny(aRet, bShowEmpty);
+    if ( aPropertyName == SC_UNO_DP_REPEATITEMLABELS )
+        lcl_SetBoolInAny(aRet, bRepeatItemLabels);
     else if ( aPropertyName == SC_UNO_DP_SUBTOTAL )
     {
         uno::Sequence<sheet::GeneralFunction> aSeq = getSubTotals();        //TODO: avoid extra copy?

include/xmloff/xmltoken.hxx
@@ -2637,6 +2637,7 @@ namespace xmloff { namespace token {
         XML_DEFAULT_OUTLINE_LEVEL,
         XML_SHOW_DETAILS,
         XML_SHOW_EMPTY,
+        XML_REPEAT_ITEM_LABELS,
         XML_ITERATIVE,
         XML_uX,
         XML_NP_DLG,

sc/inc/dapiuno.hxx
@@ -586,6 +586,8 @@ public:
     void setSortInfo(const com::sun::star::sheet::DataPilotFieldSortInfo* pInfo);
     bool getShowEmpty() const;
     void setShowEmpty(bool bShow);
+    bool getRepeatItemLabels() const;
+    void setRepeatItemLabels(bool bShow);
 
     bool hasGroupInfo();
     com::sun::star::sheet::DataPilotFieldGroupInfo getGroupInfo();

sc/inc/dpsave.hxx
@@ -106,6 +106,7 @@ private:
     sal_uInt16 nFunction; // enum GeneralFunction, for data dimensions
     long nUsedHierarchy;
     sal_uInt16 nShowEmptyMode; //! at level
+    bool bRepeatItemLabels; //! at level
     bool bSubTotalDefault; //! at level
     long nSubTotalCount;
     sal_uInt16* pSubTotalFuncs; // enum GeneralFunction
@@ -161,6 +162,10 @@ public:
     void SetShowEmpty(bool bSet);
     bool GetShowEmpty() const
         { return bool(nShowEmptyMode); }
+
+    void SetRepeatItemLabels(bool bSet);
+    bool GetRepeatItemLabels() const
+        { return bRepeatItemLabels; }
 
     void SetFunction(sal_uInt16 nNew); // enum GeneralFunction
     sal_uInt16 GetFunction() const

sc/inc/dptabsrc.hxx
@@ -564,6 +564,7 @@ private:
     long                        nAutoMeasure;       // measure (index of data dimension) for AutoShow
     bool                        bShowEmpty:1;
     bool                        bEnableLayout:1;      // enabled only for row fields, not for the innermost one
+    bool                        bRepeatItemLabels:1;
 
 public:
                             ScDPLevel( ScDPSource* pSrc, long nD, long nH, long nL );
@@ -635,6 +636,7 @@ public:
 
     com::sun::star::uno::Sequence<com::sun::star::sheet::GeneralFunction> getSubTotals() const;
     bool getShowEmpty() const { return bShowEmpty;}
+    bool getRepeatItemLabels() const { return bRepeatItemLabels; }
 
     const ::com::sun::star::sheet::DataPilotFieldSortInfo& GetSortInfo() const      { return aSortInfo; }
     const ::com::sun::star::sheet::DataPilotFieldAutoShowInfo& GetAutoShow() const  { return aAutoShowInfo; }

sc/inc/pivot.hxx
@@ -75,6 +75,7 @@ struct ScDPLabelData
     bool       mbShowAll:1;    ///< true = Show all (also empty) results.
     bool       mbIsValue:1;    ///< true = Sum or count in data field.
     bool       mbDataLayout:1;
+    bool       mbRepeatItemLabels:1;
 
     struct Member
     {

sc/inc/unonames.hxx
@@ -261,6 +261,7 @@
 #define SC_UNONAME_ISGROUP          "IsGroupField"
 #define SC_UNONAME_GROUPINFO        "GroupInfo"
 #define SC_UNONAME_SHOWEMPTY        "ShowEmpty"
+#define SC_UNONAME_REPEATITEMLABELS "RepeatItemLabels"
 
 //  data pilot item
 #define SC_UNONAME_SHOWDETAIL       "ShowDetail"
@@ -577,6 +578,7 @@
 #define SC_UNO_DP_FILTER               "Filter"
 #define SC_UNO_DP_SUBTOTAL             "SubTotals"
 #define SC_UNO_DP_SHOWEMPTY            "ShowEmpty"
+#define SC_UNO_DP_REPEATITEMLABELS     "RepeatItemLabels"
 #define SC_UNO_DP_ISVISIBLE            "IsVisible"
 #define SC_UNO_DP_SHOWDETAILS          "ShowDetails"
 #define SC_UNO_DP_IGNOREEMPTY          "IgnoreEmptyRows"

sc/qa/unit/ucalc.hxx
@@ -290,6 +290,11 @@ public:
      */
     void testPivotTableDocFunc();
 
+    /**
+     * Test pivot table per-field repeat item labels functionality
+     */
+    void testPivotTableRepeatItemLabels();
+
     void testCellCopy();
     void testSheetCopy();
     void testSheetMove();
@@ -530,6 +535,7 @@ public:
     CPPUNIT_TEST(testPivotTableNumStability);
     CPPUNIT_TEST(testPivotTableFieldReference);
     CPPUNIT_TEST(testPivotTableDocFunc);
+    CPPUNIT_TEST(testPivotTableRepeatItemLabels);
     CPPUNIT_TEST(testCellCopy);
     CPPUNIT_TEST(testSheetCopy);
     CPPUNIT_TEST(testSheetMove);

commit b6a30a997eed9fdca6498a5daeceeef58867d236

Related tdf#92262: PIVOTTABLE: crash when clicking on the autofilter arrow
Missing "else" in front of the "if"
Regression from http://cgit.freedesktop.org/libreoffice/core/commit/?id=7b355da6853af6678c4ba22710d157cf8a6d43eb

Change-Id: I3c32e8f09acbea7094c4268384ec4b1c4e028fc5

sc/source/core/data/dptabsrc.cxx
@@ -2235,7 +2235,7 @@ uno::Any SAL_CALL ScDPLevel::getPropertyValue( const OUString& aPropertyName )
     uno::Any aRet;
     if ( aPropertyName == SC_UNO_DP_SHOWEMPTY )
         lcl_SetBoolInAny(aRet, bShowEmpty);
-    if ( aPropertyName == SC_UNO_DP_REPEATITEMLABELS )
+    else if ( aPropertyName == SC_UNO_DP_REPEATITEMLABELS )
         lcl_SetBoolInAny(aRet, bRepeatItemLabels);
     else if ( aPropertyName == SC_UNO_DP_SUBTOTAL )
     {