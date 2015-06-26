6a8b38e698ba26f5a9dc8c1bd6b305ff9b3366fb
Noel Grandin <noel@peralex.com>
2015-06-26 10:33:45 +0200

    remove custom RTTI from XFillExchangeData
    nothing is using it
    
    Change-Id: Ie626fcb7f35d98294ec41620c679d46a34a77d90

--- svx/source/xoutdev/xexch.cxx
+++ svx/source/xoutdev/xexch.cxx
@@ -29,8 +29,6 @@
 #include <boost/scoped_ptr.hpp>
 
 
-TYPEINIT0_AUTOFACTORY( XFillExchangeData );
-
 /// default CTOR, for Assign()
 XFillExchangeData::XFillExchangeData() :
     pXFillAttrSetItem( NULL ),
