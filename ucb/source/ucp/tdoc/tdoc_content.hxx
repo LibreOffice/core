8db808c5003cd10a87b99cbdb193171ce8d0869c
Caolán McNamara <caolanm@redhat.com>
2015-06-27 20:14:15 +0100

    coverity#1308481 Uncaught exception
    Change-Id: Ieb1796d1da5b887f7d0d5748007cc581c3d287d7

--- ucb/source/ucp/tdoc/tdoc_content.hxx
+++ ucb/source/ucp/tdoc/tdoc_content.hxx
@@ -175,7 +175,7 @@ private:
                     ::com::sun::star::beans::PropertyValue >& rValues,
             const ::com::sun::star::uno::Reference<
                     ::com::sun::star::ucb::XCommandEnvironment > & xEnv )
-        throw( ::com::sun::star::uno::Exception );
+        throw( ::com::sun::star::uno::Exception, std::exception );
 
     com::sun::star::uno::Any
     open( const ::com::sun::star::ucb::OpenCommandArgument2& rArg,
