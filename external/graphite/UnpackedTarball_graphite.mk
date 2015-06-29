37b67904b2c71ed0997d4f4e5b929398fb14d48e
Stephan Bergmann <sbergman@redhat.com>
2015-06-29 12:26:16 +0200

    external/graphite: work around -fsanitize=nonnull-attribute/vptr
    Change-Id: Ic0766555dc7a04bbb8c7ad8f9cba3d74d84c59db

--- external/graphite/UnpackedTarball_graphite.mk
+++ external/graphite/UnpackedTarball_graphite.mk
@@ -11,10 +11,13 @@ $(eval $(call gb_UnpackedTarball_UnpackedTarball,graphite))
 
 $(eval $(call gb_UnpackedTarball_set_tarball,graphite,$(GRAPHITE_TARBALL)))
 
+$(eval $(call gb_UnpackedTarball_set_patchlevel,graphite,0))
+
 # http://projects.palaso.org/issues/1115
 $(eval $(call gb_UnpackedTarball_add_patches,graphite,\
 	external/graphite/graphite2.issue1115.patch.1 \
     external/graphite/graphite2.win64.patch.1 \
+    external/graphite/ubsan.patch \
 ))
 
 # vim: set noet sw=4 ts=4:
