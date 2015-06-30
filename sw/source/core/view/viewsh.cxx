01d25476b9ecf074a924affec8feed427c912488
Stephan Bergmann <sbergman@redhat.com>
2015-06-30 13:36:28 +0200

    loplugin:vclwidgets
    Change-Id: Icebaba828316b0f304ad407dc88d7a45f1f62cba
    (cherry picked from commit 8c92b5ae3d6e96f64754da603a36f42b7feaac5a)

--- sw/source/core/view/viewsh.cxx
+++ sw/source/core/view/viewsh.cxx
@@ -1674,7 +1674,7 @@ namespace
 class RenderContextGuard
 {
     VclPtr<vcl::RenderContext>& m_pRef;
-    vcl::RenderContext* m_pOriginalValue;
+    VclPtr<vcl::RenderContext> m_pOriginalValue;
     SwViewShell* m_pShell;
 
 public:
