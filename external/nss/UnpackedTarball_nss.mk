060258175195eebb0ff5017d7e7334ce37bac6e8
Michael Stahl <mstahl@redhat.com>
2015-07-01 17:15:39 +0200

    nss: upgrade to release 3.19.2
    - drop nss-linux-x86-patch.0:
      this was actually patching OS/2-specific code so is evidently obsolete
    - drop nspr-build-config.patch:
      there does not appear to be any justification why this huge patch exists
      at all, so the only reasonable thing to do is to drop it
    - nss_macosx.patch:
      drop several hunks relevant only to unuspported MacOSX/PPC platform
    
    Change-Id: Id55fcb8eaa637c10a42203f1897480df8a6ec0fa
    (cherry picked from commit 6e7991dfd8c54a833f4a9795a0d57f4690e92e6b)
    Reviewed-on: https://gerrit.libreoffice.org/16727
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>

--- external/nss/UnpackedTarball_nss.mk
+++ external/nss/UnpackedTarball_nss.mk
@@ -15,11 +15,7 @@ $(eval $(call gb_UnpackedTarball_add_patches,nss,\
 	external/nss/nss.patch \
 	external/nss/nss.aix.patch \
 	external/nss/nss-3.13.5-zlib-werror.patch \
-	external/nss/nss_macosx.patch \
 	$(if $(filter WNTMSC,$(OS)$(COM)),external/nss/nss.windows.patch) \
-	$(if $(filter WNTGCC,$(OS)$(COM)),external/nss/nspr-4.9-build.patch.3 \
-		external/nss/nss-3.13.3-build.patch.3 \
-		external/nss/nss.mingw.patch.3) \
 ))
 
 # vim: set noet sw=4 ts=4:
