02eaa2d8565910fdc6a6a3e56767cd620681728d
Justin Luth <justin_luth@sil.org>
2015-07-01 07:20:53 +0300

    tdf#91641 adjust cursor when deleting preceding characters
    IMDeleteSurrounding is used by input methods to take multiple
    keystrokes and convert them into a special character.  Then the
    composing keystrokes are removed.
    Before this fix, the cursor placement was not adjusted,
    so the special character was inserted in the wrong position
    and if 3+ keystrokes were involved, the wrong characters were deleted.
    
    Also fixes "editLine should have focus on accessibleText init."
    The first time an accessibleEdit is created, it didnt recognize any
    focused text when editing in the "Input Line".
    
    Change-Id: Ib2608c46bba1c84bb49aa29619a004c125a3bf98

--- vcl/unx/gtk/window/gtksalframe.cxx
+++ vcl/unx/gtk/window/gtksalframe.cxx
@@ -4462,7 +4462,7 @@ gboolean GtkSalFrame::IMHandler::signalIMRetrieveSurrounding( GtkIMContext* pCon
     uno::Reference<accessibility::XAccessibleEditableText> xText = lcl_GetxText(pFocusWin);
     if (xText.is())
     {
-        sal_uInt32 nPosition = xText->getCaretPosition();
+        sal_Int32 nPosition = xText->getCaretPosition();
         OUString sAllText = xText->getText();
         OString sUTF = OUStringToOString(sAllText, RTL_TEXTENCODING_UTF8);
         OUString sCursorText(sAllText.copy(0, nPosition));
@@ -4484,7 +4484,7 @@ gboolean GtkSalFrame::IMHandler::signalIMDeleteSurrounding( GtkIMContext*, gint 
     uno::Reference<accessibility::XAccessibleEditableText> xText = lcl_GetxText(pFocusWin);
     if (xText.is())
     {
-        sal_uInt32 nPosition = xText->getCaretPosition();
+        sal_Int32 nPosition = xText->getCaretPosition();
         // #i111768# range checking
         sal_Int32 nDeletePos = nPosition + offset;
         sal_Int32 nDeleteEnd = nDeletePos + nchars;
@@ -4496,6 +4496,14 @@ gboolean GtkSalFrame::IMHandler::signalIMDeleteSurrounding( GtkIMContext*, gint 
             nDeleteEnd = xText->getCharacterCount();
 
         xText->deleteText(nDeletePos, nDeleteEnd);
+        //tdf91641 adjust cursor if deleted chars shift it forward (normal case)
+        if (nDeletePos < nPosition)
+        {
+            if (nDeleteEnd <= nPosition)
+                xText->setCaretPosition( nPosition-(nDeleteEnd-nDeletePos) );
+            else
+                xText->setCaretPosition( nDeletePos );
+        }
         return true;
     }
 
