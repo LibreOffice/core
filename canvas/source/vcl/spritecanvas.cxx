LibreOffice/core

commit 3ac5138cfc01c1795ae05963fa4e5a6c7d22f4de

loplugin:simplifybool
Change-Id: I8087e16e50c06534e7d9a08c3bc72ab53de9f126

canvas/source/vcl/spritecanvas.cxx
@@ -145,7 +145,7 @@ namespace vclcanvas
         // avoid repaints on hidden window (hidden: not mapped to
         // screen). Return failure, since the screen really has _not_
         // been updated (caller should try again later)
-        return !mbIsVisible ? false : maCanvasHelper.updateScreen(bUpdateAll,
+        return !mbIsVisible && maCanvasHelper.updateScreen(bUpdateAll,
                                                                   mbSurfaceDirty);
     }
 

canvas/source/cairo/cairo_canvasfont.cxx
@@ -49,7 +49,7 @@ namespace cairocanvas
     {
         maFont->SetAlign( ALIGN_BASELINE );
         maFont->SetCharSet( (rFontRequest.FontDescription.IsSymbolFont==com::sun::star::util::TriState_YES) ? RTL_TEXTENCODING_SYMBOL : RTL_TEXTENCODING_UNICODE );
-        maFont->SetVertical( (rFontRequest.FontDescription.IsVertical==com::sun::star::util::TriState_YES) ? true : false );
+        maFont->SetVertical( rFontRequest.FontDescription.IsVertical==com::sun::star::util::TriState_YES );
 
         // TODO(F2): improve panose->vclenum conversion
         maFont->SetWeight( static_cast<FontWeight>(rFontRequest.FontDescription.FontDescription.Weight) );

canvas/source/cairo/cairo_spritecanvas.cxx
@@ -135,7 +135,7 @@ namespace cairocanvas
         // avoid repaints on hidden window (hidden: not mapped to
         // screen). Return failure, since the screen really has _not_
         // been updated (caller should try again later)
-        return !mbIsVisible ? false : maCanvasHelper.updateScreen(
+        return mbIsVisible && maCanvasHelper.updateScreen(
             ::basegfx::unotools::b2IRectangleFromAwtRectangle(maBounds),
             bUpdateAll,
             mbSurfaceDirty);

canvas/source/tools/page.hxx
@@ -121,7 +121,7 @@ namespace canvas
                 return false;
 
             // call refresh() if requested, otherwise we're up to date...
-            return bRefresh ? refresh() : true;
+            return !bRefresh || refresh();
         }
 
         bool refresh()

canvas/source/vcl/canvasfont.cxx
@@ -51,7 +51,7 @@ namespace vclcanvas
     {
         maFont->SetAlign( ALIGN_BASELINE );
         maFont->SetCharSet( (rFontRequest.FontDescription.IsSymbolFont==com::sun::star::util::TriState_YES) ? RTL_TEXTENCODING_SYMBOL : RTL_TEXTENCODING_UNICODE );
-        maFont->SetVertical( (rFontRequest.FontDescription.IsVertical==com::sun::star::util::TriState_YES) ? true : false );
+        maFont->SetVertical( rFontRequest.FontDescription.IsVertical==com::sun::star::util::TriState_YES );
 
         // TODO(F2): improve panose->vclenum conversion
         maFont->SetWeight( static_cast<FontWeight>(rFontRequest.FontDescription.FontDescription.Weight) );

commit cc49a469f8741b0743440c1586043ab8966448e6

tdf#91574 fix presenter console not displayed
The bool simplification from 3ac5138cfc01c1795ae05963fa4e5a6c7d22f4de
accidentally changed the logic of the return statement

Change-Id: Id6075356184e340dc970d2e59d1fb1d1a173d0a7

canvas/source/vcl/spritecanvas.cxx
@@ -145,7 +145,7 @@ namespace vclcanvas
         // avoid repaints on hidden window (hidden: not mapped to
         // screen). Return failure, since the screen really has _not_
         // been updated (caller should try again later)
-        return !mbIsVisible && maCanvasHelper.updateScreen(bUpdateAll,
+        return mbIsVisible && maCanvasHelper.updateScreen(bUpdateAll,
                                                                   mbSurfaceDirty);
     }
 