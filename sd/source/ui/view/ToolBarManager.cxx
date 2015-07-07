LibreOffice/core

commit 1191ed8584f9fd63c39514dd2e3e71682913d4bc

tdf#87822 Impress: hide common task toolbar in master view
When Master Slide or Master Notes are active,
hides the Common task toolbar as it it not usefull.

Change-Id: Ice07661bf203c029d733614e1eceb5ef3d0661f9
Reviewed-on: https://gerrit.libreoffice.org/16060
Reviewed-by: Yousuf Philips <philipz85@hotmail.com>
Tested-by: Yousuf Philips <philipz85@hotmail.com>

sd/source/ui/view/ToolBarManager.cxx
@@ -1001,9 +1001,6 @@ void ToolBarRules::MainViewShellChanged (ViewShell::ShellType nShellType)
                 ToolBarManager::msOptionsToolBar);
             mpToolBarManager->AddToolBar(
                 ToolBarManager::TBG_PERMANENT,
-                ToolBarManager::msCommonTaskToolBar);
-            mpToolBarManager->AddToolBar(
-                ToolBarManager::TBG_PERMANENT,
                 ToolBarManager::msViewerToolBar);
             break;
 
@@ -1065,10 +1062,16 @@ void ToolBarRules::MainViewShellChanged (const ViewShell& rMainViewShell)
             const DrawViewShell* pDrawViewShell
                 = dynamic_cast<const DrawViewShell*>(&rMainViewShell);
             if (pDrawViewShell != NULL)
+            {
                 if (pDrawViewShell->GetEditMode() == EM_MASTERPAGE)
                     mpToolBarManager->AddToolBar(
                         ToolBarManager::TBG_MASTER_MODE,
                         ToolBarManager::msMasterViewToolBar);
+                else
+                    mpToolBarManager->AddToolBar(
+                        ToolBarManager::TBG_COMMON_TASK,
+                        ToolBarManager::msCommonTaskToolBar);
+            }
             break;
         }
 

sd/source/ui/view/drviews1.cxx
@@ -363,14 +363,18 @@ void DrawViewShell::ChangeEditMode(EditMode eEMode, bool bIsLayerModeActive)
         // view.
         bool bShowMasterViewToolbar (meEditMode == EM_MASTERPAGE
              && GetShellType() != ViewShell::ST_HANDOUT);
+        bool bShowPresentationToolbar (meEditMode != EM_MASTERPAGE
+             && GetShellType() != ViewShell::ST_HANDOUT);
 
         // If the master view toolbar is not shown we hide it before
         // switching the edit mode.
         if (::sd::ViewShell::mpImpl->mbIsInitialized
-            && IsMainViewShell()
-            && ! bShowMasterViewToolbar)
+            && IsMainViewShell())
         {
-            GetViewShellBase().GetToolBarManager()->ResetToolBars(ToolBarManager::TBG_MASTER_MODE);
+            if ( !bShowMasterViewToolbar )
+                GetViewShellBase().GetToolBarManager()->ResetToolBars(ToolBarManager::TBG_MASTER_MODE);
+            if ( !bShowPresentationToolbar )
+                GetViewShellBase().GetToolBarManager()->ResetToolBars(ToolBarManager::TBG_COMMON_TASK);
         }
 
         if (meEditMode == EM_PAGE)
@@ -441,12 +445,16 @@ void DrawViewShell::ChangeEditMode(EditMode eEMode, bool bIsLayerModeActive)
         // If the master view toolbar is to be shown we turn it on after the
         // edit mode has been changed.
         if (::sd::ViewShell::mpImpl->mbIsInitialized
-            && IsMainViewShell()
-            && bShowMasterViewToolbar)
+            && IsMainViewShell())
         {
-            GetViewShellBase().GetToolBarManager()->SetToolBar(
-                ToolBarManager::TBG_MASTER_MODE,
-                ToolBarManager::msMasterViewToolBar);
+            if (bShowMasterViewToolbar)
+                GetViewShellBase().GetToolBarManager()->SetToolBar(
+                    ToolBarManager::TBG_MASTER_MODE,
+                    ToolBarManager::msMasterViewToolBar);
+            if (bShowPresentationToolbar)
+                GetViewShellBase().GetToolBarManager()->SetToolBar(
+                    ToolBarManager::TBG_COMMON_TASK,
+                    ToolBarManager::msCommonTaskToolBar);
         }
 
         if ( ! mbIsLayerModeActive)

sd/source/ui/inc/ToolBarManager.hxx
@@ -126,6 +126,7 @@ public:
 
         TBG_PERMANENT = TBG__FIRST,
         TBG_FUNCTION,
+        TBG_COMMON_TASK,
         TBG_MASTER_MODE,
 
         TBG__LAST = TBG_MASTER_MODE

commit 5d19f90eb247275420a6774403cb77940ac6131b

tdf#92017 Don't attempt to show commontaskbar in Draw
Regression from 1191ed8584f9fd63c39514dd2e3e71682913d4bc

Change-Id: Ib631c7ac7d0c8688907555d0fb2e4343ce37d660

sd/source/ui/view/ToolBarManager.cxx
@@ -1067,7 +1067,7 @@ void ToolBarRules::MainViewShellChanged (const ViewShell& rMainViewShell)
                     mpToolBarManager->AddToolBar(
                         ToolBarManager::TBG_MASTER_MODE,
                         ToolBarManager::msMasterViewToolBar);
-                else
+                else if ( rMainViewShell.GetShellType() != ::sd::ViewShell::ST_DRAW )
                     mpToolBarManager->AddToolBar(
                         ToolBarManager::TBG_COMMON_TASK,
                         ToolBarManager::msCommonTaskToolBar);

sd/source/ui/view/drviews1.cxx
@@ -365,7 +365,8 @@ void DrawViewShell::ChangeEditMode(EditMode eEMode, bool bIsLayerModeActive)
         bool bShowMasterViewToolbar (meEditMode == EM_MASTERPAGE
              && GetShellType() != ViewShell::ST_HANDOUT);
         bool bShowPresentationToolbar (meEditMode != EM_MASTERPAGE
-             && GetShellType() != ViewShell::ST_HANDOUT);
+             && GetShellType() != ViewShell::ST_HANDOUT
+             && GetShellType() != ViewShell::ST_DRAW);
 
         // If the master view toolbar is not shown we hide it before
         // switching the edit mode.