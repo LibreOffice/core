LibreOffice/core

commit 42b521f800a08b11432cf906ce1bdf9fcbfffdeb

tdf#88056: Implement ODF import/export of page title field
to/from loext namespace, since new tag's needed for the new field
and it isn't approved by ODF-TC yet

Change-Id: I70ceb4e16e1199663520dbdf3beae31423bc81d0
Reviewed-on: https://gerrit.libreoffice.org/15485
Tested-by: Michael Stahl <mstahl@redhat.com>
Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>
Tested-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>

sd/source/ui/app/sdmod2.cxx
@@ -268,6 +268,12 @@ IMPL_LINK(SdModule, CalcFieldValueHdl, EditFieldInfo*, pInfo)
             {
                 aRepresentation = pPage->GetName();
             }
+            else
+            {
+                aRepresentation = ( ( pDoc->GetDocumentType() == DOCUMENT_TYPE_IMPRESS )
+                                    ? SdResId(STR_FIELD_PLACEHOLDER_SLIDENAME).toString()
+                                    : SdResId(STR_FIELD_PLACEHOLDER_PAGENAME).toString() );
+            }
 
             pInfo->SetRepresentation( aRepresentation );
         }

editeng/source/items/flditem.cxx
@@ -117,7 +117,7 @@ SvxFieldData* SvxFieldData::Create(const uno::Reference<text::XTextContent>& xTe
                 return new SvxPageField();
             case text::textfield::Type::PAGES:
                 return new SvxPagesField();
-            case text::textfield::Type::PAGE_TITLE:
+            case text::textfield::Type::PAGE_NAME:
                 return new SvxPageTitleField();
             case text::textfield::Type::DOCINFO_TITLE:
                 return new SvxFileField();

editeng/source/uno/unofield.cxx
@@ -504,6 +504,9 @@ SvxFieldData* SvxUnoTextField::CreateFieldData() const throw()
     case text::textfield::Type::PRESENTATION_DATE_TIME:
         pData = new SvxDateTimeField();
         break;
+    case text::textfield::Type::PAGE_NAME:
+        pData = new SvxPageTitleField();
+        break;
     };
 
     return pData;
@@ -606,6 +609,8 @@ OUString SAL_CALL SvxUnoTextField::getPresentation( sal_Bool bShowCommand )
                 return OUString("Footer");
             case text::textfield::Type::PRESENTATION_DATE_TIME:
                 return OUString("DateTime");
+            case text::textfield::Type::PAGE_NAME:
+                return OUString("PageName");
             default:
                 return OUString("Unknown");
         }
@@ -852,6 +857,10 @@ uno::Sequence< OUString > SAL_CALL SvxUnoTextField::getSupportedServiceNames()
         case text::textfield::Type::PRESENTATION_DATE_TIME:
             pServices[2] = "com.sun.star.presentation.TextField.DateTime";
             pServices[3] = "com.sun.star.presentation.textfield.DateTime";
+        break;
+        case text::textfield::Type::PAGE_NAME:
+            pServices[2] = "com.sun.star.text.TextField.PageName";
+            pServices[3] = "com.sun.star.text.textfield.PageName";
         break;
         default:
             aSeq.realloc(0);

include/editeng/flditem.hxx
@@ -194,7 +194,7 @@ public:
 class EDITENG_DLLPUBLIC SvxPageTitleField : public SvxFieldData
 {
 public:
-    SV_DECL_PERSIST1( SvxPageTitleField, SvxFieldData, com::sun::star::text::textfield::Type::PAGE_TITLE )
+    SV_DECL_PERSIST1( SvxPageTitleField, SvxFieldData, com::sun::star::text::textfield::Type::PAGE_NAME )
     SvxPageTitleField();
 
     virtual SvxFieldData*   Clone() const SAL_OVERRIDE;

include/xmloff/txtimp.hxx
@@ -210,6 +210,7 @@ enum XMLTextPElemTokens
     XML_TOK_TEXT_SEQUENCE_REF,
     XML_TOK_TEXT_NOTE_REF,
     XML_TOK_TEXT_SHEET_NAME,
+    XML_TOK_TEXT_PAGE_NAME,
     XML_TOK_TEXT_BIBLIOGRAPHY_MARK,
     XML_TOK_TEXT_ANNOTATION,
     XML_TOK_TEXT_ANNOTATION_END,

include/xmloff/xmltoken.hxx
@@ -1368,6 +1368,7 @@ namespace xmloff { namespace token {
         XML_PAGE_HEIGHT,
         XML_PAGE_MASTER,
         XML_PAGE_MASTER_NAME,
+        XML_PAGE_NAME,
         XML_PAGE_NUMBER,
         XML_PAGE_START_MARGIN,
         XML_PAGE_STYLE_NAME,

offapi/com/sun/star/text/textfield/Type.idl
@@ -42,7 +42,7 @@ constants Type
     const long PRESENTATION_HEADER    = 11;
     const long PRESENTATION_FOOTER    = 12;
     const long PRESENTATION_DATE_TIME = 13;
-    const long PAGE_TITLE    = 14;
+    const long PAGE_NAME    = 14;
 };
 
 }; }; }; }; };

sd/source/ui/app/strings.src
@@ -1066,6 +1066,16 @@ String STR_FIELD_PLACEHOLDER_COUNT
     Text [ en-US ] = "<count>" ;
 };
 
+String STR_FIELD_PLACEHOLDER_SLIDENAME
+{
+    Text [ en-US ] = "<slide-name>" ;
+};
+
+String STR_FIELD_PLACEHOLDER_PAGENAME
+{
+    Text [ en-US ] = "<page-name>" ;
+};
+
 String STR_PLACEHOLDER_DESCRIPTION_NOTES
 {
     Text [ en-US ] = "Notes Area";

commit 737e392537d909f7915fb071230cc59ee1ff51e6

fix crash on loading presentation with page name field
regression from

commit 42b521f800a08b11432cf906ce1bdf9fcbfffdeb
Date:   Wed Apr 22 23:31:43 2015 +0200

    tdf#88056: Implement ODF import/export of page title field

Change-Id: I577a7c52836bbb13cc93910b4458aee5482ffc86

sd/source/ui/app/sdmod2.cxx
@@ -270,7 +270,8 @@ IMPL_LINK(SdModule, CalcFieldValueHdl, EditFieldInfo*, pInfo)
             }
             else
             {
-                aRepresentation = ( ( pDoc->GetDocumentType() == DOCUMENT_TYPE_IMPRESS )
+                DocumentType eDocType = pDoc ? pDoc->GetDocumentType() : DOCUMENT_TYPE_IMPRESS;
+                aRepresentation = ( ( eDocType == DOCUMENT_TYPE_IMPRESS )
                                     ? SdResId(STR_FIELD_PLACEHOLDER_SLIDENAME).toString()
                                     : SdResId(STR_FIELD_PLACEHOLDER_PAGENAME).toString() );
             }