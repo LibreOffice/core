d6366b2dfe8506cd03740b67c3c1936cc1ae56b2
Ashod Nakashian <ashodnakashian@yahoo.com>
2015-07-11 12:53:08 -0400

    tdf#92612 Right-clicking after applying "paint buckets" leads to multiple Undo
    In paint bucket mode the user can undo by right-clicking.
    Undoing, however, is only possible after the first paint op.
    After undoing is enabled, the user can undo indefinetely by
    righ-clicking. This is not consistent and can be confusing.
    
    This patch tracks the changes done after entering the paint
    bucket mode, and allows the user to undo them using right-clicking
    until they reach the first operation before entering this context.
    
    Because the user can undo/redo using other means, the patch takes
    it into account and will reset its tracking to accomodate this.
    
    Ultimately, the user is able to make any changes in paint bucket
    mode and undo them all by right-clicking, but no further than that.
    
    Reviewed-on: https://gerrit.libreoffice.org/16951
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>
    (cherry picked from commit d0489d0827fc6cef04d0f3602023d82ceda82480)
    
    Change-Id: I93380fb60db83aee37a1d324218ba6ab1700a2e5

--- sw/source/uibase/inc/view.hxx
+++ sw/source/uibase/inc/view.hxx
@@ -130,13 +130,13 @@ struct SwApplyTemplate
     int eType;
     sal_uInt16 nColor;
     SwFormatClipboard* m_pFormatClipboard;
-    bool bUndo;
+    size_t nUndo; //< The initial undo stack depth.
 
     SwApplyTemplate() :
         eType(0),
         nColor(0),
         m_pFormatClipboard(0),
-        bUndo(false)
+        nUndo(0)
     {
         aColl.pTxtColl = 0;
     }
