69632c9c3291a7bb9d465495943aa1dbe12c5af2
Caolán McNamara <caolanm@redhat.com>
2015-07-12 09:25:27 +0100

    Revert "Update firebird to version 2.5.4"
    This reverts commit e63673f8991492aaf814665f58435bc6884ca06d.

--- external/firebird/UnpackedTarball_firebird.mk
+++ external/firebird/UnpackedTarball_firebird.mk
@@ -12,9 +12,11 @@ $(eval $(call gb_UnpackedTarball_UnpackedTarball,firebird))
 $(eval $(call gb_UnpackedTarball_set_tarball,firebird,$(FIREBIRD_TARBALL)))
 
 $(eval $(call gb_UnpackedTarball_add_patches,firebird,\
+	external/firebird/firebird-icu.patch.1 \
 	external/firebird/firebird-rpath.patch.0 \
 	external/firebird/firebird-c++11.patch.1 \
 	external/firebird/firebird-c++11replfn.patch.0 \
+	external/firebird/firebird-no-open-temp-on-shutdown.patch.1 \
         external/firebird/firebird.disable-ib-util-not-found.patch.1 \
 ))
 
@@ -28,7 +30,6 @@ endif
 ifeq ($(OS),MACOSX)
 $(eval $(call gb_UnpackedTarball_add_patches,firebird,\
 	external/firebird/firebird-macosx.patch.1 \
-	external/firebird/firebird-configure-x86-64-macosx.patch.1 \
 ))
 endif
 # vim: set noet sw=4 ts=4:
