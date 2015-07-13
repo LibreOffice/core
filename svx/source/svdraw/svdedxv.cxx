af0508a70bf83cf9517609b8bf94eb72ade78e1c
matteocam <matteo.campanelli@gmail.com>
2015-07-13 16:49:18 -0400

    Change cursor
    Change-Id: I893008684b640e90e07b3e770780a395c12267ca

--- svx/source/svdraw/svdedxv.cxx
+++ svx/source/svdraw/svdedxv.cxx
@@ -561,7 +561,7 @@ void SdrObjEditView::ImpMoveCursorAfterChainingEvent()
                 SdrBeginTextEdit(pNextLink);
                 // OutlinerView has changed, so we update the pointer
                 pOLV = GetTextEditOutlinerView();
-                //pOLV->SetSelection(pTextChain->GetPostChainingSel(pTextObj)); // XXX
+                pOLV->SetSelection(pTextChain->GetPostChainingSel(pTextObj)); // XXX
 
                 break;
             case CursorChainingEvent::TO_PREV_LINK:
