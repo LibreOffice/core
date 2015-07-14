8a1e063aa0000ff5244171f5b9164980a027b734
Michael Stahl <mstahl@redhat.com>
2015-07-14 12:22:18 +0200

    oops, forgot to add new header file
    Change-Id: I83ce7cb1b06ad69250aee48e9a8c1167ae7ef239

---
+++ include/framework/ContextChangeEventMultiplexerTunnel.hxx
@@ -0,0 +1,24 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <framework/fwedllapi.h>
+#include <com/sun/star/ui/XContextChangeEventListener.hpp>
+#include <functional>
+
+namespace framework {
+
+// this is pretty horrible, don't use it!
+FWE_DLLPUBLIC css::uno::Reference<css::ui::XContextChangeEventListener>
+GetFirstListenerWith(
+    css::uno::Reference<css::uno::XInterface> const& xEventFocus,
+    std::function<bool (css::uno::Reference<css::ui::XContextChangeEventListener> const&)> const& rPredicate);
+
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
