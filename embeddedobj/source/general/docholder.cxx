730606527399685991384f8df102ae66ebee69c7
Markus Mohrhard <markus.mohrhard@googlemail.com>
2015-07-14 15:07:29 +0200

    for now show chart sidebar
    This is a hack that needs to be cleaned up later.
    
    Change-Id: I2772fe6ff8a4792d746a29653635d3eca4088e68

--- embeddedobj/source/general/docholder.cxx
+++ embeddedobj/source/general/docholder.cxx
@@ -714,8 +714,9 @@ bool DocumentHolder::ShowUI( const uno::Reference< ::com::sun::star::frame::XLay
                     xContainerLM->setDockingAreaAcceptor( uno::Reference < ui::XDockingAreaAcceptor >() );
 
                     // prevent further changes at this LM
-                    xContainerLM->setVisible( sal_False );
-                       xContainerLM->lock();
+                    // TODO: moggi: why is this necessary?
+                    // xContainerLM->setVisible( sal_False );
+                    // xContainerLM->lock();
                     bUnlock = true;
 
                     // by unlocking the LM each layout change will now resize the containers window; pending layouts will be processed now
