5f321b5011c619e88e19018ff05ed3ca2cdd5851
David Ostrovsky <david@ostrovsky.org>
2015-07-15 08:24:22 +0200

    Bump boost to 1.59
    1.59 upcoming release is needed for MSVC 14.0 (aka VS 2015) support.
    
    There is a known breakage on MSVC with boost::none instance in
    optional library.  Use not documented define as explained in this
    issue upstream [1]: BOOST_OPTIONAL_USE_OLD_DEFINITION_OF_NONE.
    
    [1] https://svn.boost.org/trac/boost/ticket/11203
    
    Change-Id: I25b1797da93f7959d15a70105d67100e0e2c4b3b
    Reviewed-on: https://gerrit.libreoffice.org/17056
    Reviewed-by: Michael Stahl <mstahl@redhat.com>
    Tested-by: Jenkins <ci@libreoffice.org>

--- external/boost/UnpackedTarball_boost.mk
+++ external/boost/UnpackedTarball_boost.mk
@@ -10,51 +10,25 @@
 boost_patches :=
 #https://svn.boost.org/trac/boost/ticket/3780
 boost_patches += boost.3780.aliasing.patch
-#https://svn.boost.org/trac/boost/ticket/9397
-boost_patches += boost.9397.warnings.patch.0
 #http://gcc.gnu.org/bugzilla/show_bug.cgi?id=47679
 boost_patches += boost.gcc47679.patch
 #https://svn.boost.org/trac/boost/ticket/6369
 boost_patches += boost.6369.warnings.patch
-#https://svn.boost.org/trac/boost/ticket/7551
-boost_patches += boost.7551.unusedvars.patch
 #https://svn.boost.org/trac/boost/ticket/6142
 boost_patches += boost.6142.warnings.patch.1
 
-# fixed upstream
-boost_patches += boost.bind.Wunused-local-typedefs.warnings.patch
-# fixed upstream
-boost_patches += boost.concept_check.C4100.warnings.patch
-# fixed upstream
-boost_patches += boost.concept_check.Wunused-local-typedefs.warnings.patch
 # https://svn.boost.org/trac/boost/ticket/9881
 boost_patches += boost.crc.Wshadow.warnings.patch
 # https://svn.boost.org/trac/boost/ticket/9882
 boost_patches += boost.date_time.Wshadow.warnings.patch
-# https://svn.boost.org/trac/boost/ticket/9883
-boost_patches += boost.date_time.Wunused-local-typedefs.warnings.patch
-# fixed upstream
-boost_patches += boost.math.Wdeprecated-register.patch.0
-# fixed upstream
-boost_patches += boost.math.Wunused-local-typedefs.warnings.patch
 # https://svn.boost.org/trac/boost/ticket/9885
 boost_patches += boost.multi_array.C4510.warnings.patch
-# https://svn.boost.org/trac/boost/ticket/9886
-boost_patches += boost.multi_array.Wshadow.warnings.patch.2
-# fixed upstream
-boost_patches += boost.multi_array.Wundef.warnings.patch
 # https://svn.boost.org/trac/boost/ticket/9888
 boost_patches += boost.multi_array.Wunused-local-typedefs.warnings.patch
-# fixed upstream
-boost_patches += boost.multi_index.Wshadow.warnings.patch.1
-# https://svn.boost.org/trac/boost/ticket/9889
-boost_patches += boost.predef.Wundef.warnings.patch
 # https://svn.boost.org/trac/boost/ticket/9891
 boost_patches += boost.preprocessor.Wundef.warnings.patch
 # https://svn.boost.org/trac/boost/ticket/9892
 boost_patches += boost.property_tree.Wshadow.warnings.patch.1
-# https://svn.boost.org/trac/boost/ticket/9893
-boost_patches += boost.property_tree.Wtype-limits.warnings.patch.1
 # https://svn.boost.org/trac/boost/ticket/9894
 boost_patches += boost.ptr_container.Wshadow.warnings.patch
 # https://svn.boost.org/trac/boost/ticket/9895
@@ -63,8 +37,6 @@ boost_patches += boost.ptr_container.Wignored-qualifiers.warnings.patch
 boost_patches += boost.ptr_container.Wextra.warnings.patch
 # https://svn.boost.org/trac/boost/ticket/9897
 boost_patches += boost.ptr_container.Wunused-parameter.warnings.patch
-# https://svn.boost.org/trac/boost/ticket/9898
-boost_patches += boost.random.Wshadow.warnings.patch
 # fixed upstream
 boost_patches += boost.random.Wunused-local-typedefs.warnings.patch
 # https://svn.boost.org/trac/boost/ticket/9900
@@ -74,21 +46,8 @@ boost_patches += boost.spirit.Wunused-local-typedefs.warnings.patch
 # to-do: submit upstream
 boost_patches += boost.spirit.Wunused-parameter.warnings.patch
 boost_patches += boost.date_time.Wshadow.warnings.patch.1
-# fixed upstream
-boost_patches += boost.unordered.Wshadow.warnings.patch
-# fixed upstream
-boost_patches += boost.tuple.Wunused-local-typedefs.warnings.patch
 # https://svn.boost.org/trac/boost/ticket/9903
 boost_patches += boost.utility.Wundef.warnings.patch
-# fixed upstream
-boost_patches += boost.uuid.Wshadow.warnings.patch
-
-# https://svn.boost.org/trac/boost/ticket/9890
-boost_patches += boost.endian.patch
-# fixed upstream
-boost_patches += boost.loplugin.patch
-# fixed upstream
-boost_patches += boost.transform_width.patch
 
 # Help static analysis tools (see SAL_UNUSED_PARAMETER in sal/types.h):
 ifeq (GCC,$(COM))
@@ -105,10 +64,35 @@ boost_patches += boost.wunused.patch
 boost_patches += boost.wdeprecated-auto_ptr.patch.0
 boost_patches += boost.signal2.Wshadow.warnings.patch
 boost_patches += boost.boost_static_assert_unused_attribute.patch.0
-boost_patches += w4702.patch.0
 
 boost_patches += ubsan.patch.0
 boost_patches += rtti.patch.0
+
+# https://svn.boost.org/trac/boost/ticket/11505
+boost_patches += boost_1_59_0.mpl.config.wundef.patch
+# https://svn.boost.org/trac/boost/ticket/11503
+boost_patches += boost_1_59_0.type_index.wshadow.patch
+# https://svn.boost.org/trac/boost/ticket/11502
+boost_patches += boost_1_59_0.property_tree.wtype-limits.patch
+# https://svn.boost.org/trac/boost/ticket/11507
+boost_patches += boost_1_59_0.property_tree.wshadow.patch
+# https://svn.boost.org/trac/boost/ticket/11508
+boost_patches += boost_1_59_0.iostreams.wshadow.patch
+# https://svn.boost.org/trac/boost/ticket/11509
+boost_patches += boost_1_59_0.iostreams.wunused.patch
+# https://svn.boost.org/trac/boost/ticket/11506
+boost_patches += boost_1_59_0.rational.wshadow.patch
+# https://svn.boost.org/trac/boost/ticket/11510
+boost_patches += boost_1_59_0.unique_ptr.wshadow.patch
+# fixed upstream
+# https://svn.boost.org/trac/boost/ticket/11500
+boost_patches += boost_1_59_0.move.Bool-type-collision.4f9c2b62fbdcf5995ecf50a2ecf2494048a6696d.patch
+# https://svn.boost.org/trac/boost/ticket/11511
+boost_patches += boost_1_59_0.multi_array.wshadow.patch
+# https://svn.boost.org/trac/boost/ticket/11512
+boost_patches += boost_1_59_0.signal2.wshadow.patch
+# https://svn.boost.org/trac/boost/ticket/11501
+boost_patches += boost_1_59_0.property_tree.wreturn-type.patch
 
 $(eval $(call gb_UnpackedTarball_UnpackedTarball,boost))
 
