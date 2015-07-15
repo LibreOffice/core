2a889aa978e4c3360335bf994918362c76e091ff
Ashod Nakashian <ashodnakashian@yahoo.com>
2015-07-15 10:42:39 -0400

    tdf#39080 Hide vertical whitespace when HideWhitespace option is set.
    Change-Id: I1d0a974a0b4b7053f59c23aa883bef61fd1df177

--- sw/source/core/layout/paintfrm.cxx
+++ sw/source/core/layout/paintfrm.cxx
@@ -113,9 +113,10 @@ struct SwPaintProperties;
 
 //other subsidiary lines enabled?
 #define IS_SUBS (!gProp.pSGlobalShell->GetViewOptions()->IsPagePreview() && \
-        !gProp.pSGlobalShell->GetViewOptions()->IsReadonly() && \
-        !gProp.pSGlobalShell->GetViewOptions()->IsFormView() &&\
-         SwViewOption::IsDocBoundaries())
+                 !gProp.pSGlobalShell->GetViewOptions()->IsReadonly() && \
+                 !gProp.pSGlobalShell->GetViewOptions()->IsFormView() &&\
+                 !gProp.pSGlobalShell->GetViewOptions()->IsHideWhitespaceMode() &&\
+                 SwViewOption::IsDocBoundaries())
 //subsidiary lines for sections
 #define IS_SUBS_SECTION (!gProp.pSGlobalShell->GetViewOptions()->IsPagePreview() && \
                          !gProp.pSGlobalShell->GetViewOptions()->IsReadonly()&&\
