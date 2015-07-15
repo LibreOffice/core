66d77903b7699df6b3e93b63528b7054b02f4f8e
Rishabh Kumar <kris.kr296@gmail.com>
2015-07-16 05:17:52 +0530

    tdf#87813: Moving effects list into the animation tab
    Adding the animation effect -
    1.Press the + button
    
    Modifying the animation effect
    1. Select the animation from the list of added animations
    2. Select the animation style from the Listbox
    
    Change-Id: I14f9242b9f04279622d879ae8c3e162ded6e3e3d

---
+++ sd/source/ui/animations/CategoryListBox.hxx
@@ -0,0 +1,31 @@
+#ifndef INCLUDED_SD_SOURCE_UI_ANIMATIONS_CATEGORYLISTBOX_HXX
+#define INCLUDED_SD_SOURCE_UI_ANIMATIONS_CATEGORYLISTBOX_HXX
+
+#include <vcl/lstbox.hxx>
+#include <vcl/builderfactory.hxx>
+
+namespace sd {
+
+class CategoryListBox : public ListBox
+{
+public:
+    CategoryListBox( vcl::Window* pParent );
+    virtual ~CategoryListBox();
+
+    virtual void        MouseButtonUp( const MouseEvent& rMEvt ) SAL_OVERRIDE;
+
+    sal_Int32           InsertCategory( const OUString& rStr, sal_Int32  nPos = LISTBOX_APPEND );
+
+    void            SetDoubleClickLink( const Link<>& rDoubleClickHdl ) { maDoubleClickHdl = rDoubleClickHdl; }
+
+    DECL_LINK(implDoubleClickHdl, void *);
+
+private:
+    virtual void    UserDraw( const UserDrawEvent& rUDEvt ) SAL_OVERRIDE;
+
+    Link<>          maDoubleClickHdl;
+};
+
+}
+
+#endif
