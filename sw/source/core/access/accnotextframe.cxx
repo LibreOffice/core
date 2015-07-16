LibreOffice/core

commit 7de992bcc66c973bb6b247184cac38f01cd1104a

fix a11y crash seen on close of tdf#92573
its not the reported crash, which has "gone away" which might
be a duplicate of tdf#90502

the switch only handled RES_TITLE_CHANGED and RES_DESCRIPTION_CHANGED so if its
anything else, e.g. OBJ_DYING, then don't attempt GetNoTextNode

Change-Id: I642beb66613481cbc7ee18647f0204a67d670a84

sw/source/core/access/accnotextframe.cxx
@@ -102,6 +102,9 @@ void SwAccessibleNoTextFrame::Modify( const SfxPoolItem* pOld, const SfxPoolItem
             return; // probably was deleted - avoid doing anything
     }
 
+    if (nWhich != RES_TITLE_CHANGED || nWhich != RES_DESCRIPTION_CHANGED)
+        return;
+
     const SwNoTextNode *pNd = GetNoTextNode();
     OSL_ENSURE( pNd == aDepend.GetRegisteredIn(), "invalid frame" );
     switch( nWhich )

commit 34c0b8869784cb44a516c97213ef24566a4ed111

coverity#1311944 Logically dead code
for the record its a SwFormatChg we get here at the crash time

regression from

commit 7de992bcc66c973bb6b247184cac38f01cd1104a
Author: Caolán McNamara <caolanm@redhat.com>
Date:   Mon Jul 13 12:38:18 2015 +0100

    fix a11y crash seen on close of tdf#92573

    its not the reported crash, which has "gone away" which might
    be a duplicate of tdf#90502

    the switch only handled RES_TITLE_CHANGED and RES_DESCRIPTION_CHANGED so if its
    anything else, e.g. OBJ_DYING, then don't attempt GetNoTextNode

Change-Id: I290d6e633811423c3acc198aba7a60943518ec4a

sw/source/core/access/accnotextframe.cxx
@@ -102,7 +102,7 @@ void SwAccessibleNoTextFrame::Modify( const SfxPoolItem* pOld, const SfxPoolItem
             return; // probably was deleted - avoid doing anything
     }
 
-    if (nWhich != RES_TITLE_CHANGED || nWhich != RES_DESCRIPTION_CHANGED)
+    if (nWhich != RES_TITLE_CHANGED && nWhich != RES_DESCRIPTION_CHANGED)
         return;
 
     const SwNoTextNode *pNd = GetNoTextNode();