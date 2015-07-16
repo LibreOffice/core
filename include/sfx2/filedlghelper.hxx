1f366b13f766e588e58ee6ebe67149fb8028f3ee
Julien Nabet <serval2412@yahoo.fr>
2015-07-16 15:59:16 +0200

    Typo: deprected->deprecated
    Change-Id: I9e2e349f66a9d5bd7c45430ee9761330c792061c

--- include/sfx2/filedlghelper.hxx
+++ include/sfx2/filedlghelper.hxx
@@ -140,7 +140,7 @@ public:
     void                    SetTitle( const OUString&  rNewTitle );
     OUString                GetPath() const;
 
-    /** @deprected: Don't use this method to retrieve the selected files
+    /** @deprecated: Don't use this method to retrieve the selected files
         There are file picker which can provide multiple selected file which belong
         to different folders. As this method always provides the root folder for all selected
         files this cannot work.
