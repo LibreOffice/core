fcdddbd30a8b5cf6a5cc4d2ff28b7d4a20f8ec6b
Caolán McNamara <caolanm@redhat.com>
2015-07-19 21:32:05 +0100

    check np bounds again
    Change-Id: I0fb61954b2eaf0c015d7bdefe9f03bd459b31501

--- filter/source/graphicfilter/itiff/itiff.cxx
+++ filter/source/graphicfilter/itiff/itiff.cxx
@@ -640,6 +640,8 @@ bool TIFFReader::ReadMap()
                     pTIFF->Seek(pStripOffsets[nStrip]);
                     aLZWDecom.StartDecompression(*pTIFF);
                 }
+                if (np >= SAL_N_ELEMENTS(pMap))
+                    return false;
                 if ( ( aLZWDecom.Decompress( pMap[ np ], nBytesPerRow ) != nBytesPerRow ) || pTIFF->GetError() )
                     return false;
             }
