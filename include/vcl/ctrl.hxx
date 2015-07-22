ef22ec2d8c6dabe30b5d46a8a57f13f383b1aa27
Katarina Behrens <Katarina.Behrens@cib.de>
2015-07-22 11:27:24 +0200

    tdf#92600: Restore ability to apply font settings to controls
    SmShowSymbol handles font/fg/bg settings by itself, so make
    ApplySettings noop there.
    
    This partially reverts commit b010e4074e5d5ee3a3905f1351f04efafe937c2a
    
    Reviewed-on: https://gerrit.libreoffice.org/17288
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>
    (cherry picked from commit 94ceda8b2fea37587424b664e17fa9ee8b01e158)
    
    Change-Id: Ib5074684ef1277d9b9b4646bd73dce4cbff3943e

--- include/vcl/ctrl.hxx
+++ include/vcl/ctrl.hxx
@@ -41,8 +41,6 @@ protected:
 
 private:
     bool                    mbHasControlFocus;
-    bool                    mbFont;
-    bool                    mbForeground;
     Link<>                  maGetFocusHdl;
     Link<>                  maLoseFocusHdl;
 
