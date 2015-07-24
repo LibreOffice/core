c29e194b4c37e0735c3bae8b35902c7401e7964a
Noel Grandin <noel@peralex.com>
2015-07-24 10:33:47 +0200

    no need to redeclare these constants with slightly different names
    Change-Id: Ic9a1f2f0cef25baaf8b97db4989dc536a5da89f7

--- include/linguistic/lngprops.hxx
+++ include/linguistic/lngprops.hxx
@@ -26,30 +26,6 @@
 // (may not include results added by looking up user dictionaries)
 #define UPN_MAX_NUMBER_OF_SUGGESTIONS       "MaxNumberOfSuggestions"
 
-// WIDs for property names
-//!! Don't change values! They are used as the property handles in
-//!! the service description
-#define WID_IS_GERMAN_PRE_REFORM            UPH_IS_GERMAN_PRE_REFORM        /*! deprecated !*/
-#define WID_IS_USE_DICTIONARY_LIST          UPH_IS_USE_DICTIONARY_LIST
-#define WID_IS_IGNORE_CONTROL_CHARACTERS    UPH_IS_IGNORE_CONTROL_CHARACTERS
-#define WID_IS_SPELL_UPPER_CASE             UPH_IS_SPELL_UPPER_CASE
-#define WID_IS_SPELL_WITH_DIGITS            UPH_IS_SPELL_WITH_DIGITS
-#define WID_IS_SPELL_CAPITALIZATION         UPH_IS_SPELL_CAPITALIZATION
-#define WID_HYPH_MIN_LEADING                UPH_HYPH_MIN_LEADING
-#define WID_HYPH_MIN_TRAILING               UPH_HYPH_MIN_TRAILING
-#define WID_HYPH_MIN_WORD_LENGTH            UPH_HYPH_MIN_WORD_LENGTH
-#define WID_DEFAULT_LOCALE                  UPH_DEFAULT_LOCALE
-#define WID_IS_SPELL_AUTO                   UPH_IS_SPELL_AUTO
-#define WID_IS_SPELL_HIDE                   UPH_IS_SPELL_HIDE               /*! deprecated !*/
-#define WID_IS_SPELL_IN_ALL_LANGUAGES       UPH_IS_SPELL_IN_ALL_LANGUAGES   /*! deprecated !*/
-#define WID_IS_SPELL_SPECIAL                UPH_IS_SPELL_SPECIAL
-#define WID_IS_HYPH_AUTO                    UPH_IS_HYPH_AUTO
-#define WID_IS_HYPH_SPECIAL                 UPH_IS_HYPH_SPECIAL
-#define WID_IS_WRAP_REVERSE                 UPH_IS_WRAP_REVERSE
-#define WID_DEFAULT_LANGUAGE                UPH_DEFAULT_LANGUAGE
-#define WID_DEFAULT_LOCALE_CJK              UPH_DEFAULT_LOCALE_CJK
-#define WID_DEFAULT_LOCALE_CTL              UPH_DEFAULT_LOCALE_CTL
-
 #endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
