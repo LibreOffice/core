5af4e8e37e187949724ac4f57573341429bdebf8
Michael Stahl <mstahl@redhat.com>
2015-07-24 14:40:53 +0200

    tdf#90046: RTF import: ensure that a run is started before \footnote
    Avoids crashing with empty context stacks.
    
    (cherry picked from commit a61fd02c819433a1206b3b3e61017ba2d0d3d467)
    Reviewed-on: https://gerrit.libreoffice.org/17333
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>
    (cherry picked from commit 7e086276cd27bac93040bfd7ff5f5dbdfc0b9f86)
    
    Conflicts:
    	writerfilter/source/rtftok/rtfdocumentimpl.cxx
    
    Change-Id: I0ee8b457fdbb19b55f5c15876b7253680cde6e23

--- writerfilter/source/rtftok/rtfdocumentimpl.cxx
+++ writerfilter/source/rtftok/rtfdocumentimpl.cxx
@@ -1657,6 +1657,7 @@ int RTFDocumentImpl::dispatchDestination(RTFKeyword nKeyword)
         }
         break;
     case RTF_FOOTNOTE:
+        checkFirstRun();
         if (!m_pSuperstream)
         {
             Id nId = NS_ooxml::LN_footnote;
