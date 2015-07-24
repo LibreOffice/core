75eb5a5cae3276fff4eee17af24916bdf5e7381b
David Ostrovsky <david@ostrovsky.org>
2015-07-24 21:08:47 +0200

    Fix cppunit on MSVC 14.0
    Change-Id: Iebfbd843cd3fbe46f3e090654a8493adb5309800
    Reviewed-on: https://gerrit.libreoffice.org/17341
    Reviewed-by: Michael Stahl <mstahl@redhat.com>
    Tested-by: Michael Stahl <mstahl@redhat.com>

--- external/cppunit/ExternalProject_cppunit.mk
+++ external/cppunit/ExternalProject_cppunit.mk
@@ -20,7 +20,8 @@ $(call gb_ExternalProject_get_state_target,cppunit,build) :
 		/p:Platform=$(if $(filter INTEL,$(CPUNAME)),Win32,x64) \
 			$(if $(filter 110,$(VCVER)),/p:PlatformToolset=$(if $(filter 80,$(WINDOWS_SDK_VERSION)),v110,v110_xp) \
 				/p:VisualStudioVersion=11.0) \
-			$(if $(filter 120,$(VCVER)),/p:PlatformToolset=v120 /p:VisualStudioVersion=12.0 /ToolsVersion:12.0)" \
+			$(if $(filter 120,$(VCVER)),/p:PlatformToolset=v120 /p:VisualStudioVersion=12.0 /ToolsVersion:12.0) \
+			$(if $(filter 140,$(VCVER)),/p:PlatformToolset=v140 /p:VisualStudioVersion=14.0 /ToolsVersion:14.0)" \
 		&& msbuild.exe cppunit_dll.vcxproj /p:Configuration=$${PROFILEFLAGS}  \
 		&& cd ../DllPlugInTester \
 		&& msbuild.exe DllPlugInTester.vcxproj /p:Configuration=$${PROFILEFLAGS} \
