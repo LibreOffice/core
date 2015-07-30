3a704fc0e15430e98eb8ddc3ed2c781055911a7a
Noel Grandin <noel@peralex.com>
2015-07-30 09:50:59 +0200

    Fix Android build
    after commit a45827b2308febc7369db27fb489a6d1389534e1
    "loplugin:unusedmethods"
    
    Change-Id: I8981a6bee237d4e2e5b9fe837a56e073e59b2697

--- include/vcl/svapp.hxx
+++ include/vcl/svapp.hxx
@@ -834,6 +834,30 @@ public:
     */
     static ImplSVEvent *        PostMouseEvent( sal_uLong nEvent, vcl::Window *pWin, MouseEvent* pMouseEvent );
 
+#if !HAVE_FEATURE_DESKTOP
+    /** Send zoom event
+
+     Experimental work in progress. Available only for iOS and Android, and unclear whether actually
+     is needed now with tiled rendering.
+
+     @param     nEvent          Event ID for zoom event
+     @param     pWin            Pointer to window to which the event is sent
+     @param     pZoomEvent      Zoom event to send
+    */
+    static ImplSVEvent *        PostZoomEvent( sal_uLong nEvent, vcl::Window *pWin, ZoomEvent* pZoomEvent );
+
+    /* Send scroll event
+
+     Experimental work in progress. Available only for iOS and Android, and unclear whether actually
+     is needed now with tiled rendering.
+
+     @param      nEvent          Event ID for scroll event
+     @param      pWin            Pointer to window to which the event is sent
+     @param      pScrollEvent    Scroll event to send
+    */
+    static ImplSVEvent *         PostScrollEvent( sal_uLong nEvent, vcl::Window *pWin, ScrollEvent* pScrollEvent );
+#endif
+
     /** Remove mouse and keypress events from a window... any also zoom and scroll events
      if the platform supports it.
 
