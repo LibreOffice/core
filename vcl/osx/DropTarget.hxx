b3364c13ced09d903d7390febb36f35faf3baa86
mmeof2 <manikmalhotra92@gmail.com>
2015-08-01 19:26:40 +0400

    tdf#44621: Fixed drag and drop in OSX 10.7+
    Change-Id: I8f9e1e9205dd9611f4bc1ef47c8cde2e284b403f
    Reviewed-on: https://gerrit.libreoffice.org/17461
    Reviewed-by: Norbert Thiebaud <nthiebaud@gmail.com>
    Tested-by: Norbert Thiebaud <nthiebaud@gmail.com>

--- vcl/osx/DropTarget.hxx
+++ vcl/osx/DropTarget.hxx
@@ -125,7 +125,7 @@ public:
   NSDragOperation draggingUpdated(id sender);
   void draggingExited(id sender);
   BOOL prepareForDragOperation(id sender);
-  BOOL performDragOperation(id sender);
+  BOOL performDragOperation();
   void concludeDragOperation(id sender);
 
   /* If multiple actions are supported by the drag source and
