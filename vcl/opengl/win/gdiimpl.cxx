b63b54e7453cdda92914dfe3d1ea2d9f0d1f0d29
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2015-08-03 15:06:55 +0900

    opengl: cache native widget textures also for Windows
    Change-Id: I476f0ffaef383f3227c0c12b50fcdebf393190f6
    Reviewed-on: https://gerrit.libreoffice.org/17487
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Tor Lillqvist <tml@collabora.com>
    Tested-by: Tor Lillqvist <tml@collabora.com>
    (cherry picked from commit dea885f80a80c6a5839ee5dbf8521487186a9522)
    Reviewed-on: https://gerrit.libreoffice.org/17561
    Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>

--- vcl/opengl/win/gdiimpl.cxx
+++ vcl/opengl/win/gdiimpl.cxx
@@ -7,6 +7,9 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+#include <o3tl/lru_map.hxx>
+
+
 #include "opengl/win/gdiimpl.hxx"
 
 #include <win/wincomp.hxx>
@@ -41,6 +44,88 @@ bool WinOpenGLSalGraphicsImpl::UseContext( OpenGLContext* pContext )
     if( IsOffscreen() )
         return true;
     return ( pContext->getOpenGLWindow().hWnd == mrParent.mhWnd );
+}
+
+namespace
+{
+
+typedef std::pair<ControlCacheKey, std::unique_ptr<TextureCombo>> ControlCachePair;
+typedef o3tl::lru_map<ControlCacheKey, std::unique_ptr<TextureCombo>, ControlCacheHashFunction> ControlCacheType;
+
+ControlCacheType gTextureCache(200);
+
+}
+
+bool WinOpenGLSalGraphicsImpl::TryRenderCachedNativeControl(ControlCacheKey& rControlCacheKey, int nX, int nY)
+{
+    static bool gbCacheEnabled = !getenv("SAL_WITHOUT_WIDGET_CACHE");
+
+    if (!gbCacheEnabled)
+        return false;
+
+    ControlCacheType::const_iterator iterator = gTextureCache.find(rControlCacheKey);
+
+    if (iterator == gTextureCache.end())
+        return false;
+
+    const std::unique_ptr<TextureCombo>& pCombo = iterator->second;
+
+    PreDraw();
+
+    OpenGLTexture& rTexture = *pCombo->mpTexture;
+
+    SalTwoRect aPosAry(0,  0,  rTexture.GetWidth(), rTexture.GetHeight(),
+                       nX, nY, rTexture.GetWidth(), rTexture.GetHeight());
+
+    if (pCombo->mpMask)
+        DrawTextureDiff(rTexture, *pCombo->mpMask, aPosAry, true);
+    else
+        DrawTexture(rTexture, aPosAry, true);
+
+    PostDraw();
+
+    return true;
+}
+
+bool WinOpenGLSalGraphicsImpl::RenderCompatibleDC(OpenGLCompatibleDC& rWhite, OpenGLCompatibleDC& rBlack,
+                                                  int nX, int nY, TextureCombo& rCombo)
+{
+    PreDraw();
+
+    rCombo.mpTexture.reset(rWhite.getTexture());
+    rCombo.mpMask.reset(rBlack.getTexture());
+
+
+    if (rCombo.mpTexture && rCombo.mpMask)
+    {
+        OpenGLTexture& rTexture = *rCombo.mpTexture;
+
+        SalTwoRect aPosAry(0,   0, rTexture.GetWidth(), rTexture.GetHeight(),
+                           nX, nY, rTexture.GetWidth(), rTexture.GetHeight());
+
+        DrawTextureDiff(*rCombo.mpTexture, *rCombo.mpMask, aPosAry);
+    }
+
+    PostDraw();
+    return true;
+}
+
+bool WinOpenGLSalGraphicsImpl::RenderAndCacheNativeControl(OpenGLCompatibleDC& rWhite, OpenGLCompatibleDC& rBlack,
+                                                           int nX, int nY , ControlCacheKey& aControlCacheKey)
+{
+    std::unique_ptr<TextureCombo> pCombo(new TextureCombo);
+
+    bool bResult = RenderCompatibleDC(rWhite, rBlack, nX, nY, *pCombo);
+    if (!bResult)
+        return false;
+
+    if (aControlCacheKey.mnType == CTRL_CHECKBOX)
+        return true;
+
+    ControlCachePair pair(aControlCacheKey, std::move(pCombo));
+    gTextureCache.insert(std::move(pair));
+
+    return bResult;
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
