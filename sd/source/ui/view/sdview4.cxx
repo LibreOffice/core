ea3b960398a07f2eea8c453c06fad78113a1c7da
Noel Grandin <noel@peralex.com>
2015-08-05 10:39:50 +0200

    remove unused SfxFilterFlags values
    from both the code and the XCU files
    
    Change-Id: I47f1f33c3ad4a37e346e3b8020dd9d3e284ccb77

--- sd/source/ui/view/sdview4.cxx
+++ sd/source/ui/view/sdview4.cxx
@@ -449,7 +449,7 @@ IMPL_LINK_NOARG_TYPED(View, DropInsertFileHdl, Idle *, void)
             {
                 const SfxFilter*        pFoundFilter = NULL;
                 SfxMedium               aSfxMedium( aCurrentDropFile, StreamMode::READ | StreamMode::SHARE_DENYNONE );
-                ErrCode                 nErr = SfxGetpApp()->GetFilterMatcher().GuessFilter(  aSfxMedium, &pFoundFilter, SfxFilterFlags::IMPORT, SFX_FILTER_NOTINSTALLED | SfxFilterFlags::EXECUTABLE );
+                ErrCode                 nErr = SfxGetpApp()->GetFilterMatcher().GuessFilter(  aSfxMedium, &pFoundFilter, SfxFilterFlags::IMPORT, SFX_FILTER_NOTINSTALLED );
 
                 if( pFoundFilter && !nErr )
                 {
