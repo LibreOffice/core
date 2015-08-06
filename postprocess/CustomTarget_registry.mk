7ad86194d0361c2c9cf306d17065e49c095e3642
Michael Stahl <mstahl@redhat.com>
2015-08-06 22:48:14 +0200

    postprocess: d'oh generated files are in $(BUILDDIR)
    Change-Id: Id9dee9cfe065b30c2ae9bade1098d16e2c659c23
    (cherry picked from commit d657a976f5ac73da5277612b4bdf6a25641e2797)

--- postprocess/CustomTarget_registry.mk
+++ postprocess/CustomTarget_registry.mk
@@ -568,7 +568,7 @@ postprocess_main_SED := \
 	-e 's,$${STARTCENTER_TEMPLREP_URL},http://templates.libreoffice.org/,g' \
 
 $(call gb_XcdTarget_get_target,main.xcd) \
-		: $(SRCDIR)/config_host.mk.stamp \
+		: $(BUILDDIR)/config_host.mk.stamp \
         | $(call gb_ExternalExecutable_get_dependencies,xsltproc)
 	$(call gb_Output_announce,main,$(true),XCD,3)
 	$(call gb_Helper_abbreviate_dirs, \
