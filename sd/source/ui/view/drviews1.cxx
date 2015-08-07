68ea840ccab58f7eae617ab94018755f33c3c8d2
Mihai Varga <mihai.varga@collabora.com>
2015-08-07 09:03:26 +0300

    Impress: set current page Id before showing the new page
    For tiled rendering this has the effect of not invalidating the
    previous slide when switching slides. After switching slides, an
    invalidation occurs caused by showing the new slide, and calling
    'GetPart' before 'SwitchPage' finishes returned the old part number.
    
    Change-Id: I1cafd0e51cd39be3a80d0559ae3051238b8df744

--- sd/source/ui/view/drviews1.cxx
+++ sd/source/ui/view/drviews1.cxx
@@ -969,6 +969,7 @@ bool DrawViewShell::SwitchPage(sal_uInt16 nSelectedPage)
             }
 
             mpDrawView->HideSdrPage();
+            maTabControl->SetCurPageId(nSelectedPage+1);
             mpDrawView->ShowSdrPage(mpActualPage);
             GetViewShellBase().GetDrawController().FireSwitchCurrentPage(mpActualPage);
 
@@ -994,7 +995,6 @@ bool DrawViewShell::SwitchPage(sal_uInt16 nSelectedPage)
                 }
             }
 
-            maTabControl->SetCurPageId(nSelectedPage+1);
             OUString aPageName = mpActualPage->GetName();
 
             if (maTabControl->GetPageText(nSelectedPage+1) != aPageName)
@@ -1030,6 +1030,7 @@ bool DrawViewShell::SwitchPage(sal_uInt16 nSelectedPage)
             }
 
             mpDrawView->HideSdrPage();
+            maTabControl->SetCurPageId(nSelectedPage+1);
 
             SdPage* pMaster = GetDoc()->GetMasterSdPage(nSelectedPage, mePageKind);
 
@@ -1067,8 +1068,6 @@ bool DrawViewShell::SwitchPage(sal_uInt16 nSelectedPage)
             sal_Int32 nPos = aLayoutName.indexOf(SD_LT_SEPARATOR);
             if (nPos != -1)
                 aLayoutName = aLayoutName.copy(0, nPos);
-
-            maTabControl->SetCurPageId(nSelectedPage+1);
 
             if (maTabControl->GetPageText(nSelectedPage+1) != aLayoutName)
             {
