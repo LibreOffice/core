LibreOffice/core

commit 4b66829390b286010b37b37ec1537a320d8cea8f

convert BOX_LINE and BOXINFO_LINE to enum class
since their usage is intertwined.

Also introduce new o3tl utilities enumrange and enumarray to make
working with scoped enums a little simpler.

Change-Id: I2e1cc65dd7c638e59f17d96dfae504747cad6533

sd/source/ui/table/TableDesignPane.cxx
@@ -43,6 +43,7 @@
 #include <editeng/colritem.hxx>
 #include <editeng/eeitem.hxx>
 #include <svx/sdr/table/tabledesign.hxx>
+#include <o3tl/enumrange.hxx>
 
 #include "TableDesignPane.hxx"
 #include "createtabledesignpanel.hxx"
@@ -694,7 +695,7 @@ const Bitmap CreateDesignPreview( const Reference< XIndexAccess >& xTableStyle, 
                     sal_Int32* pDiff = &border_diffs[0];
 
                     // draw top border
-                    for( sal_uInt16 nLine = 0; nLine < 4; ++nLine )
+                    for( SvxBoxItemLine nLine : o3tl::enumrange<SvxBoxItemLine>() )
                     {
                         const ::editeng::SvxBorderLine* pBorderLine = xCellInfo->maBorder.GetLine(nLine);
                         if( !pBorderLine || ((pBorderLine->GetOutWidth() == 0) && (pBorderLine->GetInWidth()==0)) )
@@ -708,8 +709,7 @@ const Bitmap CreateDesignPreview( const Reference< XIndexAccess >& xTableStyle, 
                             boost::shared_ptr< CellInfo > xBorderInfo( aMatrix[nBorderCol][nBorderRow] );
                             if( xBorderInfo.get() )
                             {
-                                const sal_uInt16 nOtherLine = nLine ^ 1;
-                                const ::editeng::SvxBorderLine* pBorderLine2 = xBorderInfo->maBorder.GetLine(nOtherLine^1);
+                                const ::editeng::SvxBorderLine* pBorderLine2 = xBorderInfo->maBorder.GetLine(static_cast<SvxBoxItemLine>(static_cast<int>(nLine)^1^1));
                                 if( pBorderLine2 && pBorderLine2->HasPriority(*pBorderLine) )
                                     continue; // other border line wins
                             }
@@ -718,10 +718,10 @@ const Bitmap CreateDesignPreview( const Reference< XIndexAccess >& xTableStyle, 
                         pAccess->SetLineColor( pBorderLine->GetColor() );
                         switch( nLine )
                         {
-                        case 0: pAccess->DrawLine( aPntTL, aPntTR ); break;
-                        case 1: pAccess->DrawLine( aPntBL, aPntBR ); break;
-                        case 2: pAccess->DrawLine( aPntTL, aPntBL ); break;
-                        case 3: pAccess->DrawLine( aPntTR, aPntBR ); break;
+                        case SvxBoxItemLine::TOP: pAccess->DrawLine( aPntTL, aPntTR ); break;
+                        case SvxBoxItemLine::BOTTOM: pAccess->DrawLine( aPntBL, aPntBR ); break;
+                        case SvxBoxItemLine::LEFT: pAccess->DrawLine( aPntTL, aPntBL ); break;
+                        case SvxBoxItemLine::RIGHT: pAccess->DrawLine( aPntTR, aPntBR ); break;
                         }
                     }
                 }

sw/source/filter/ww8/wrtww8gr.cxx
@@ -65,6 +65,7 @@
 
 #include "docsh.hxx"
 #include <cstdio>
+#include <o3tl/enumrange.hxx>
 
 #if OSL_DEBUG_LEVEL > 1
 #include <stdio.h>
@@ -574,16 +575,14 @@ void SwWW8WrGrf::WritePICFHeader(SvStream& rStrm, const sw::Frame &rFly,
                     (pSI->GetWidth() != 0);
             }
 
-            sal_uInt8 aLnArr[4] = { BOX_LINE_TOP, BOX_LINE_LEFT,
-                                BOX_LINE_BOTTOM, BOX_LINE_RIGHT };
-            for( sal_uInt8 i = 0; i < 4; ++i )
+            for( SvxBoxItemLine i : o3tl::enumrange<SvxBoxItemLine>() )
             {
-                const ::editeng::SvxBorderLine* pLn = pBox->GetLine( aLnArr[ i ] );
+                const ::editeng::SvxBorderLine* pLn = pBox->GetLine( i );
                 WW8_BRC aBrc;
                 if (pLn)
                 {
                     WW8_BRCVer9 aBrc90 = rWrt.TranslateBorderLine( *pLn,
-                        pBox->GetDistance( aLnArr[ i ] ), bShadow );
+                        pBox->GetDistance( i ), bShadow );
                     sal_uInt8 ico = msfilter::util::TransColToIco(msfilter::util::BGRToRGB(
                         aBrc90.cv()));
                     aBrc = WW8_BRC(aBrc90.dptLineWidth(), aBrc90.brcType(), ico,
@@ -594,15 +593,15 @@ void SwWW8WrGrf::WritePICFHeader(SvStream& rStrm, const sw::Frame &rFly,
                 // border will really be in word and adjust accordingly
                 short nSpacing;
                 short nThick = aBrc.DetermineBorderProperties(&nSpacing);
-                switch (aLnArr[ i ])
+                switch (i)
                 {
-                    case BOX_LINE_TOP:
-                    case BOX_LINE_BOTTOM:
+                    case SvxBoxItemLine::TOP:
+                    case SvxBoxItemLine::BOTTOM:
                         nHeight -= bShadow ? nThick*2 : nThick;
                         nHeight = nHeight - nSpacing;
                         break;
-                    case BOX_LINE_LEFT:
-                    case BOX_LINE_RIGHT:
+                    case SvxBoxItemLine::LEFT:
+                    case SvxBoxItemLine::RIGHT:
                     default:
                         nWidth -= bShadow ? nThick*2 : nThick;
                         nWidth = nWidth - nSpacing;
@@ -764,23 +763,21 @@ void SwWW8WrGrf::WritePICBulletFHeader(SvStream& rStrm, const Graphic &rGrf,
 
     sal_uInt8* pArr = aArr + 0x2E;  //Do borders first
 
-    sal_uInt8 aLnArr[4] = { BOX_LINE_TOP, BOX_LINE_LEFT,
-    BOX_LINE_BOTTOM, BOX_LINE_RIGHT };
-    for( sal_uInt8 i = 0; i < 4; ++i )
+    for( SvxBoxItemLine i : o3tl::enumrange<SvxBoxItemLine>() )
     {
         WW8_BRC aBrc;
 
         short nSpacing;
         short nThick = aBrc.DetermineBorderProperties(&nSpacing);
-        switch (aLnArr[ i ])
+        switch (i)
         {
-            case BOX_LINE_TOP:
-            case BOX_LINE_BOTTOM:
+            case SvxBoxItemLine::TOP:
+            case SvxBoxItemLine::BOTTOM:
             nHeight -= nThick;
             nHeight = nHeight - nSpacing;
             break;
-            case BOX_LINE_LEFT:
-            case BOX_LINE_RIGHT:
+            case SvxBoxItemLine::LEFT:
+            case SvxBoxItemLine::RIGHT:
             default:
             nWidth -= nThick;
             nWidth = nWidth - nSpacing;

sw/source/filter/ww8/ww8atr.cxx
@@ -135,6 +135,7 @@
 #include <vcl/outdev.hxx>
 #include <i18nlangtag/languagetag.hxx>
 #include <unotools/fltrcfg.hxx>
+#include <o3tl/enumrange.hxx>
 
 using ::editeng::SvxBorderLine;
 using namespace ::com::sun::star;
@@ -3901,8 +3902,8 @@ void WW8AttributeOutput::FormatLRSpace( const SvxLRSpaceItem& rLR )
         const SfxPoolItem* pItem = m_rWW8Export.HasItem( RES_BOX );
         if ( pItem )
         {
-            nRDist = static_cast<const SvxBoxItem*>(pItem)->CalcLineSpace( BOX_LINE_LEFT );
-            nLDist = static_cast<const SvxBoxItem*>(pItem)->CalcLineSpace( BOX_LINE_RIGHT );
+            nRDist = static_cast<const SvxBoxItem*>(pItem)->CalcLineSpace( SvxBoxItemLine::LEFT );
+            nLDist = static_cast<const SvxBoxItem*>(pItem)->CalcLineSpace( SvxBoxItemLine::RIGHT );
         }
         else
             nLDist = nRDist = 0;
@@ -4377,9 +4378,9 @@ void WW8Export::Out_SwFmtBox(const SvxBoxItem& rBox, bool bShadow)
     if ( bOutPageDescs && !bWrtWW8 )
         return; // no page ouline in WW6
 
-    static const sal_uInt16 aBorders[] =
+    static const SvxBoxItemLine aBorders[] =
     {
-        BOX_LINE_TOP, BOX_LINE_LEFT, BOX_LINE_BOTTOM, BOX_LINE_RIGHT
+        SvxBoxItemLine::TOP, SvxBoxItemLine::LEFT, SvxBoxItemLine::BOTTOM, SvxBoxItemLine::RIGHT
     };
     static const sal_uInt16 aPBrc[] =
     {
@@ -4404,7 +4405,7 @@ void WW8Export::Out_SwFmtBox(const SvxBoxItem& rBox, bool bShadow)
         38, 39, 40, 41
     };
 
-    const sal_uInt16* pBrd = aBorders;
+    const SvxBoxItemLine* pBrd = aBorders;
     for( sal_uInt16 i = 0; i < 4; ++i, ++pBrd )
     {
         const SvxBorderLine* pLn = rBox.GetLine( *pBrd );
@@ -4437,18 +4438,13 @@ void WW8Export::Out_SwFmtBox(const SvxBoxItem& rBox, bool bShadow)
 void WW8Export::Out_SwFmtTableBox( ww::bytes& rO, const SvxBoxItem * pBox )
 {
     // moeglich und vielleicht besser waere 0xffff
-    static const sal_uInt16 aBorders[] =
-    {
-        BOX_LINE_TOP, BOX_LINE_LEFT, BOX_LINE_BOTTOM, BOX_LINE_RIGHT
-    };
     static const SvxBorderLine aBorderLine;
 
-    const sal_uInt16* pBrd = aBorders;
-    for( int i = 0; i < 4; ++i, ++pBrd )
+    for( SvxBoxItemLine i : o3tl::enumrange<SvxBoxItemLine>() )
     {
         const SvxBorderLine* pLn;
         if (pBox != NULL)
-            pLn = pBox->GetLine( *pBrd );
+            pLn = pBox->GetLine( i );
         else
             pLn = & aBorderLine;
 
@@ -4459,9 +4455,9 @@ void WW8Export::Out_SwFmtTableBox( ww::bytes& rO, const SvxBoxItem * pBox )
 void WW8Export::Out_CellRangeBorders( const SvxBoxItem * pBox, sal_uInt8 nStart,
        sal_uInt8 nLimit )
 {
-    static const sal_uInt16 aBorders[] =
+    static const SvxBoxItemLine aBorders[] =
     {
-        BOX_LINE_TOP, BOX_LINE_LEFT, BOX_LINE_BOTTOM, BOX_LINE_RIGHT
+        SvxBoxItemLine::TOP, SvxBoxItemLine::LEFT, SvxBoxItemLine::BOTTOM, SvxBoxItemLine::RIGHT
     };
 
     for( int i = 0; i < 4; ++i )
@@ -5512,22 +5508,22 @@ void AttributeOutputBase::FormatCharBorder( const SvxBoxItem& rBox )
     if( rBox.GetTop() )
     {
        pBorderLine = rBox.GetTop();
-       nDist = rBox.GetDistance( BOX_LINE_TOP );
+       nDist = rBox.GetDistance( SvxBoxItemLine::TOP );
     }
     else if( rBox.GetLeft() )
     {
        pBorderLine = rBox.GetLeft();
-       nDist = rBox.GetDistance( BOX_LINE_LEFT );
+       nDist = rBox.GetDistance( SvxBoxItemLine::LEFT );
     }
     else if( rBox.GetBottom() )
     {
        pBorderLine = rBox.GetBottom();
-       nDist = rBox.GetDistance( BOX_LINE_BOTTOM );
+       nDist = rBox.GetDistance( SvxBoxItemLine::BOTTOM );
     }
     else if( rBox.GetRight() )
     {
        pBorderLine = rBox.GetRight();
-       nDist = rBox.GetDistance( BOX_LINE_RIGHT );
+       nDist = rBox.GetDistance( SvxBoxItemLine::RIGHT );
     }
 
     if( pBorderLine )

commit 78446df26f45e2a71187873726388c9d2f6ba6e5

tdf#92885 - Incorrect cell border drawing in DOC file after saving in 5.0
caused by commit 4b66829390b286010b37b37ec1537a320d8cea8f
"convert BOX_LINE and BOXINFO_LINE to enum class"

Change-Id: I23eb359a40035dd40bdb60703de567b1c328e793

sd/source/ui/table/TableDesignPane.cxx
@@ -710,7 +710,7 @@ const Bitmap CreateDesignPreview( const Reference< XIndexAccess >& xTableStyle, 
                             boost::shared_ptr< CellInfo > xBorderInfo( aMatrix[nBorderCol][nBorderRow] );
                             if( xBorderInfo.get() )
                             {
-                                const ::editeng::SvxBorderLine* pBorderLine2 = xBorderInfo->maBorder.GetLine(static_cast<SvxBoxItemLine>(static_cast<int>(nLine)^1^1));
+                                const ::editeng::SvxBorderLine* pBorderLine2 = xBorderInfo->maBorder.GetLine(static_cast<SvxBoxItemLine>(static_cast<int>(nLine)^1));
                                 if( pBorderLine2 && pBorderLine2->HasPriority(*pBorderLine) )
                                     continue; // other border line wins
                             }

sw/source/filter/ww8/wrtww8gr.cxx
@@ -544,14 +544,16 @@ void SwWW8WrGrf::WritePICFHeader(SvStream& rStrm, const sw::Frame &rFly,
                     (pSI->GetWidth() != 0);
             }
 
-            for( SvxBoxItemLine i : o3tl::enumrange<SvxBoxItemLine>() )
+            static const SvxBoxItemLine aLnArr[4] = { SvxBoxItemLine::TOP, SvxBoxItemLine::LEFT,
+                                SvxBoxItemLine::BOTTOM, SvxBoxItemLine::RIGHT };
+            for( sal_uInt8 i = 0; i < 4; ++i )
             {
-                const ::editeng::SvxBorderLine* pLn = pBox->GetLine( i );
+                const ::editeng::SvxBorderLine* pLn = pBox->GetLine( aLnArr[i] );
                 WW8_BRC aBrc;
                 if (pLn)
                 {
                     WW8_BRCVer9 aBrc90 = WW8Export::TranslateBorderLine( *pLn,
-                        pBox->GetDistance( i ), bShadow );
+                        pBox->GetDistance( aLnArr[i] ), bShadow );
                     sal_uInt8 ico = msfilter::util::TransColToIco(msfilter::util::BGRToRGB(
                         aBrc90.cv()));
                     aBrc = WW8_BRC(aBrc90.dptLineWidth(), aBrc90.brcType(), ico,
@@ -562,7 +564,7 @@ void SwWW8WrGrf::WritePICFHeader(SvStream& rStrm, const sw::Frame &rFly,
                 // border will really be in word and adjust accordingly
                 short nSpacing;
                 short nThick = aBrc.DetermineBorderProperties(&nSpacing);
-                switch (i)
+                switch (aLnArr[i])
                 {
                     case SvxBoxItemLine::TOP:
                     case SvxBoxItemLine::BOTTOM:
@@ -696,13 +698,15 @@ void SwWW8WrGrf::WritePICBulletFHeader(SvStream& rStrm, const Graphic &rGrf,
 
     sal_uInt8* pArr = aArr + 0x2E;  //Do borders first
 
-    for( SvxBoxItemLine i : o3tl::enumrange<SvxBoxItemLine>() )
+    static const SvxBoxItemLine aLnArr[4] = { SvxBoxItemLine::TOP, SvxBoxItemLine::LEFT,
+        SvxBoxItemLine::BOTTOM, SvxBoxItemLine::RIGHT };
+    for( sal_uInt8 i = 0; i < 4; ++i )
     {
         WW8_BRC aBrc;
 
         short nSpacing;
         short nThick = aBrc.DetermineBorderProperties(&nSpacing);
-        switch (i)
+        switch (aLnArr[i])
         {
             case SvxBoxItemLine::TOP:
             case SvxBoxItemLine::BOTTOM:

sw/source/filter/ww8/ww8atr.cxx
@@ -4064,13 +4064,17 @@ void WW8Export::Out_SwFormatBox(const SvxBoxItem& rBox, bool bShadow)
 void WW8Export::Out_SwFormatTableBox( ww::bytes& rO, const SvxBoxItem * pBox )
 {
     // moeglich und vielleicht besser waere 0xffff
+    static const SvxBoxItemLine aBorders[] =
+    {
+        SvxBoxItemLine::TOP, SvxBoxItemLine::LEFT, SvxBoxItemLine::BOTTOM, SvxBoxItemLine::RIGHT
+    };
     static const SvxBorderLine aBorderLine;
 
-    for( SvxBoxItemLine i : o3tl::enumrange<SvxBoxItemLine>() )
+    for( int i = 0; i < 4; ++i )
     {
         const SvxBorderLine* pLn;
         if (pBox != NULL)
-            pLn = pBox->GetLine( i );
+            pLn = pBox->GetLine( aBorders[i] );
         else
             pLn = & aBorderLine;
 