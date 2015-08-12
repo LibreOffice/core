bc5ce73f401eea5a7a7cc2133d53215249ae03a3
Mihai Varga <mihai.varga@collabora.com>
2015-08-12 10:32:07 +0300

    tdf#93154: Save button often does not save
    The problem is that in tiled rendering LO doesn't always consider the
    document to be modified. Some operations such as Bold, Italic, etc do
    not mark the document as being modified, but we need to be able to save
    the changes. This solved the issue by always allowing to save.
    
    Change-Id: Iaf8120abadc768a07b24f42c287c50080b4e4d89

--- sw/source/uibase/uno/unotxdoc.cxx
+++ sw/source/uibase/uno/unotxdoc.cxx
@@ -136,6 +136,7 @@
 #include <swdtflvr.hxx>
 
 #include <svtools/langtab.hxx>
+#include <svtools/miscopt.hxx>
 #include <map>
 #include <set>
 #include <vector>
@@ -3224,6 +3225,9 @@ void SwXTextDocument::initializeForTiledRendering()
     // directly in twips.
     SwEditWin& rEditWin = pDocShell->GetView()->GetEditWin();
     rEditWin.EnableMapMode(false);
+    // tdf#93154: in tiled rendering LO doesn't always detect changes
+    SvtMiscOptions aMiscOpt;
+    aMiscOpt.SetSaveAlwaysAllowed(true);
 }
 
 void SwXTextDocument::registerCallback(LibreOfficeKitCallback pCallback, void* pData)
