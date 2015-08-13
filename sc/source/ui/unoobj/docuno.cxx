332c98489b8c5c7ebf5fd21e607b259bd8796d44
Henry Castro <hcastro@collabora.com>
2015-08-13 16:08:46 -0400

    sc: fix LOKit invalidate setPart
    The bindings invalidate correct tiles.
    
    Change-Id: Id28a7a78157030243ef1ad798daba9583746f0d3

--- sc/source/ui/unoobj/docuno.cxx
+++ sc/source/ui/unoobj/docuno.cxx
@@ -496,7 +496,22 @@ void ScModelObj::paintTile( VirtualDevice& rDevice,
 void ScModelObj::setPart( int nPart )
 {
     ScViewData* pViewData = ScDocShell::GetViewData();
-    pViewData->SetTabNo( nPart );
+    SfxUInt16Item aItem( SID_CURRENTTAB, nPart + 1 );
+    SfxDispatcher& rDisp = pViewData->GetDispatcher();
+
+    rDisp.Execute( SID_CURRENTTAB, SfxCallMode::SLOT | SfxCallMode::RECORD, &aItem, nullptr );
+
+    SfxBindings& rBind = pViewData->GetBindings();
+    rBind.Invalidate( FID_FILL_TAB );
+    rBind.Invalidate( FID_TAB_DESELECTALL );
+    rBind.Invalidate( FID_INS_TABLE );
+    rBind.Invalidate( FID_TAB_APPEND );
+    rBind.Invalidate( FID_TAB_MOVE );
+    rBind.Invalidate( FID_TAB_RENAME );
+    rBind.Invalidate( FID_DELETE_TABLE );
+    rBind.Invalidate( FID_TABLE_SHOW );
+    rBind.Invalidate( FID_TABLE_HIDE );
+    rBind.Invalidate( FID_TAB_SET_TAB_BG_COLOR );
 }
 
 int ScModelObj::getParts()
