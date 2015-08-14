57669c741c7b02e54b769184f3aa41ba67fab278
Rosemary <rosemaryseb8@gmail.com>
2015-08-14 10:12:37 +0530

    MODULETYPE record
    Change-Id: Ie015931985a66f7fb2a47bc94d8215f64a667438

--- oox/source/ole/vbaexport.cxx
+++ oox/source/ole/vbaexport.cxx
@@ -273,6 +273,13 @@ void writeMODULECOOKIE(SvStream& rStrm)
     rStrm.WriteUInt16(0xFFFF); // HelpContext
 }
 
+// section 2.3.4.2.3.2.8
+void writeMODULETYPE(SvStream& rStrm)
+{
+    rStrm.WriteUInt16(0x0021); // id for procedural module
+    rStrm.WriteUInt32(0x00000000); // reserved
+}
+
 // section 2.3.4.2.3.2
 void writePROJECTMODULE(SvStream& rStrm)
 {
@@ -283,6 +290,7 @@ void writePROJECTMODULE(SvStream& rStrm)
     writeMODULEOFFSET(rStrm);
     writeMODULEHELPCONTEXT(rStrm);
     writeMODULECOOKIE(rStrm);
+    writeMODULETYPE(rStrm);
 }
 
 // section 2.3.4.2.3
