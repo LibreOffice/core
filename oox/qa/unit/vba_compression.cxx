6917cd98ca6b6fd2d495d0257c7fe50611982d34
Markus Mohrhard <markus.mohrhard@googlemail.com>
2015-08-15 02:24:34 +0200

    add one more test for vba compression
    Change-Id: Iba6b4e7b5d26ac7943f72e2a5b3d5d2dc4fc95fd

--- oox/qa/unit/vba_compression.cxx
+++ oox/qa/unit/vba_compression.cxx
@@ -19,7 +19,11 @@ class TestVbaCompression : public test::BootstrapFixtureBase
 {
 public:
 
+    // just a sequence of bytes that should not be compressed at all
     void testSimple1();
+
+    // a sequence containing one subsequence that can be compressed
+    void testSimple2();
 
     // avoid the BootstrapFixtureBase::setUp and tearDown
     virtual void setUp() SAL_OVERRIDE;
@@ -27,32 +31,67 @@ public:
 
     CPPUNIT_TEST_SUITE(TestVbaCompression);
     CPPUNIT_TEST(testSimple1);
+    CPPUNIT_TEST(testSimple2);
     CPPUNIT_TEST_SUITE_END();
 
 private:
 };
+
+namespace {
+
+void ReadFiles(const OUString& rTestFile, const OUString& rReference,
+        SvMemoryStream& rOutputMemoryStream, SvMemoryStream& rReferenceMemoryStream,
+        const OUString& rDebugPath)
+{
+    SvFileStream aInputStream(rTestFile, StreamMode::READ);
+    SvMemoryStream aInputMemoryStream(4096, 4096);
+    aInputMemoryStream.WriteStream(aInputStream);
+
+    VBACompression aCompression(rOutputMemoryStream, aInputMemoryStream);
+    aCompression.write();
+
+    SvFileStream aReferenceStream(rReference, StreamMode::READ);
+    rReferenceMemoryStream.WriteStream(aReferenceStream);
+
+    rOutputMemoryStream.Seek(0);
+    SvFileStream aDebugStream(rDebugPath, StreamMode::WRITE);
+    aDebugStream.WriteStream(rOutputMemoryStream);
+}
+
+}
 
 void TestVbaCompression::testSimple1()
 {
     OUString aTestFile = getPathFromSrc("/oox/qa/unit/data/vba/simple1.bin");
     OUString aReference = getPathFromSrc("/oox/qa/unit/data/vba/reference/simple1.bin");
 
-    SvFileStream aInputStream(aTestFile, StreamMode::READ);
-    SvMemoryStream aInputMemoryStream(4096, 4096);
-    aInputMemoryStream.WriteStream(aInputStream);
+    SvMemoryStream aOutputMemoryStream(4096, 4096);
+    SvMemoryStream aReferenceMemoryStream(4096, 4096);
+    ReadFiles(aTestFile, aReference, aOutputMemoryStream,
+            aReferenceMemoryStream, "/tmp/vba_debug.bin");
+
+    // CPPUNIT_ASSERT_EQUAL(aReferenceMemoryStream.GetSize(), aOutputMemoryStream.GetSize());
+
+    const sal_uInt8* pReferenceData = (const sal_uInt8*) aReferenceMemoryStream.GetData();
+    const sal_uInt8* pData = (const sal_uInt8*)aOutputMemoryStream.GetData();
+
+    size_t nSize = std::min(aReferenceMemoryStream.GetSize(),
+            aOutputMemoryStream.GetSize());
+    for (size_t i = 0; i < nSize; ++i)
+    {
+        CPPUNIT_ASSERT_EQUAL((int)pReferenceData[i], (int)pData[i]);
+    }
+}
+
+void TestVbaCompression::testSimple2()
+{
+    OUString aTestFile = getPathFromSrc("/oox/qa/unit/data/vba/simple2.bin");
+    OUString aReference = getPathFromSrc("/oox/qa/unit/data/vba/reference/simple2.bin");
 
     SvMemoryStream aOutputMemoryStream(4096, 4096);
-    VBACompression aCompression(aOutputMemoryStream, aInputMemoryStream);
-    aCompression.write();
-
-    SvFileStream aReferenceStream(aReference, StreamMode::READ);
     SvMemoryStream aReferenceMemoryStream(4096, 4096);
-    aReferenceMemoryStream.WriteStream(aReferenceStream);
-
-    aOutputMemoryStream.Seek(0);
-    SvFileStream aDebugStream("/tmp/vba_debug.bin", StreamMode::WRITE);
-    aDebugStream.WriteStream(aOutputMemoryStream);
-
+    ReadFiles(aTestFile, aReference, aOutputMemoryStream, aReferenceMemoryStream, "/tmp/vba_debug2.bin");
+    //
     // CPPUNIT_ASSERT_EQUAL(aReferenceMemoryStream.GetSize(), aOutputMemoryStream.GetSize());
 
     const sal_uInt8* pReferenceData = (const sal_uInt8*) aReferenceMemoryStream.GetData();
