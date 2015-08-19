4b37e6bebb89827edc27fb8f616bb080e78a4acc
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2015-08-19 11:51:15 +0900

    Fixed (fixed size) texture atlas for "caching" OpenGL texures
    Plus two trivial follow-up build fixes.
    
    Change-Id: I7fe90c0a0033b4d9a341a4f0b8356d7f7133e93e
    (cherry picked from commit b26ba85d04e81f48622dfdbb71d9d80b54dacc40)
    (cherry picked from commit 2792868df58808902a6bc7f58fd50970a0a756c0)
    (cherry picked from commit 1e8e3a8c4624e9a4f16a7fb7f24c5d7125d7f106)
    Reviewed-on: https://gerrit.libreoffice.org/18011
    Reviewed-by: Tor Lillqvist <tml@collabora.com>
    Tested-by: Tor Lillqvist <tml@collabora.com>

--- vcl/inc/opengl/x11/gdiimpl.hxx
+++ vcl/inc/opengl/x11/gdiimpl.hxx
@@ -16,7 +16,7 @@
 #include "unx/x11/x11gdiimpl.h"
 #include "openglgdiimpl.hxx"
 
-class TextureCombo;
+struct TextureCombo;
 
 class VCL_PLUGIN_PUBLIC X11OpenGLSalGraphicsImpl : public OpenGLSalGraphicsImpl, public X11GraphicsImpl
 {
