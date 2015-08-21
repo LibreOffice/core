41d4e2d9178eeacd1d8bcabe6ae85d1243c4edee
Rishabh Kumar <kris.kr296@gmail.com>
2015-08-21 18:24:28 +0530

    tdf#83830: Writer page sidebar deck
    Change-Id: I6eb16a5a721fad28aa7822b0ca8d7c06a0c08541

---
+++ sw/source/uibase/sidebar/PageFormatPanel.cxx
@@ -0,0 +1,102 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+#include <sal/config.h>
+#include <swtypes.hxx>
+#include <svl/intitem.hxx>
+#include <editeng/sizeitem.hxx>
+#include <editeng/paperinf.hxx>
+#include <svx/svxids.hrc>
+#include <svx/dlgutil.hxx>
+#include <svx/rulritem.hxx>
+#include "PageFormatPanel.hxx"
+#include <sfx2/sidebar/ControlFactory.hxx>
+#include <sfx2/dispatch.hxx>
+#include <sfx2/bindings.hxx>
+#include <sfx2/viewsh.hxx>
+#include <sfx2/objsh.hxx>
+#include "cmdid.h"
+#include <com/sun/star/frame/XController.hpp>
+#include <com/sun/star/frame/XModel.hpp>
+#include <com/sun/star/document/XUndoManagerSupplier.hpp>
+
+namespace sw { namespace sidebar{
+
+VclPtr<vcl::Window> PageFormatPanel::Create(
+    vcl::Window* pParent,
+    const ::com::sun::star::uno::Reference< ::com::sun::star::frame::XFrame >& rxFrame,
+    SfxBindings* pBindings)
+{
+    if( pParent == NULL )
+        throw ::com::sun::star::lang::IllegalArgumentException("no parent window given to PageFormatPanel::Create", NULL, 0);
+    if( !rxFrame.is() )
+        throw ::com::sun::star::lang::IllegalArgumentException("no XFrame given to PageFormatPanel::Create", NULL, 0);
+    if( pBindings == NULL )
+        throw ::com::sun::star::lang::IllegalArgumentException("no SfxBindings given to PageFormatPanel::Create", NULL, 0);
+
+    return VclPtr<PageFormatPanel>::Create(pParent, rxFrame, pBindings);
+}
+
+PageFormatPanel::PageFormatPanel(
+    vcl::Window* pParent,
+    const ::com::sun::star::uno::Reference< ::com::sun::star::frame::XFrame >& rxFrame,
+    SfxBindings* pBindings
+    ) :
+    PanelLayout(pParent, "PageFormatPanel", "modules/swriter/ui/pageformatpanel.ui", rxFrame),
+    mpBindings( pBindings )
+{
+    get(mpPaperSizeLB, "papersize");
+    get(mpPaperWidth, "paperwidth");
+    get(mpPaperHeight, "paperheight");
+    get(mpPaperOrientation, "paperorientation");
+
+    Initialize();
+}
+
+PageFormatPanel::~PageFormatPanel()
+{
+    disposeOnce();
+}
+
+void PageFormatPanel::dispose()
+{
+    mpPaperSizeLB.disposeAndClear();
+    mpPaperWidth.disposeAndClear();
+    mpPaperHeight.disposeAndClear();
+    mpPaperOrientation.disposeAndClear();
+
+    PanelLayout::dispose();
+}
+
+void PageFormatPanel::Initialize()
+{
+
+}
+
+void PageFormatPanel::NotifyItemUpdate(
+    const sal_uInt16 /*nSid*/,
+    const SfxItemState /*eState*/,
+    const SfxPoolItem* /*pState*/,
+    const bool /*bIsEnabled*/)
+{
+
+}
+
+} }
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
