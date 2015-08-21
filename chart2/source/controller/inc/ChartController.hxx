ae0cd13fa3b4468997aa66ae569f724e8fbbd372
Caolán McNamara <caolanm@redhat.com>
2015-08-21 12:05:22 +0100

    remove last WeakImplHelper12 use
    Change-Id: I804132abe01ef3a54e6824d415e19c7edc8039cb

--- chart2/source/controller/inc/ChartController.hxx
+++ chart2/source/controller/inc/ChartController.hxx
@@ -27,7 +27,7 @@
 #include <vcl/timer.hxx>
 #include <vcl/event.hxx>
 
-#include <cppuhelper/implbase12.hxx>
+#include <cppuhelper/implbase.hxx>
 
 #include <com/sun/star/accessibility/XAccessible.hpp>
 #include <com/sun/star/document/XUndoManager.hpp>
@@ -129,7 +129,7 @@ class DrawViewWrapper;
 class ReferenceSizeProvider;
 class ViewElementListProvider;
 
-class ChartController   : public ::cppu::WeakImplHelper12 <
+class ChartController   : public ::cppu::WeakImplHelper <
          ::com::sun::star::frame::XController   //comprehends XComponent (required interface)
         ,::com::sun::star::frame::XDispatchProvider     //(required interface)
         ,::com::sun::star::view::XSelectionSupplier     //(optional interface)
