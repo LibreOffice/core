LibreOffice/core

commit 087f7fe50b03307e2d9202365886e8c37bd7e6e3

tdf#91125 - nail a clutch of calc modeless dialog lifecycle issues.
Good idea to hold a reference after creation.

Change-Id: I450c415259c4011c8b3fb8be715e55643765d9db

sc/source/ui/inc/tabvwsh.hxx
@@ -356,9 +356,9 @@ public:
     void            DeactivateOle();
 
     SC_DLLPUBLIC static ScTabViewShell* GetActiveViewShell();
-    SfxModelessDialog*  CreateRefDialog( SfxBindings* pB, SfxChildWindow* pCW,
-                                        SfxChildWinInfo* pInfo,
-                                        vcl::Window* pParent, sal_uInt16 nSlotId );
+    VclPtr<SfxModelessDialog> CreateRefDialog( SfxBindings* pB, SfxChildWindow* pCW,
+                                               SfxChildWinInfo* pInfo,
+                                               vcl::Window* pParent, sal_uInt16 nSlotId );
 
     void            UpdateOleZoom();
     inline SbxObject* GetScSbxObject() const

sc/source/ui/view/reffact.cxx
@@ -90,7 +90,7 @@ namespace
             pViewShell = PTR_CAST( ScTabViewShell, SfxViewShell::Current() ); \
         OSL_ENSURE( pViewShell, "missing view shell :-(" );         \
         pWindow = pViewShell ?                                      \
-            pViewShell->CreateRefDialog( p, this, pInfo, pParentP, sid ) : NULL;    \
+            pViewShell->CreateRefDialog( p, this, pInfo, pParentP, sid ) : nullptr;    \
         if (pViewShell && !pWindow)                                             \
             pViewShell->GetViewFrame()->SetChildWindow( nId, false );           \
     }

sc/source/ui/view/tabvwshc.cxx
@@ -113,9 +113,10 @@ void ScTabViewShell::SwitchBetweenRefDialogs(SfxModelessDialog* pDialog)
    }
 }
 
-SfxModelessDialog* ScTabViewShell::CreateRefDialog(
-                        SfxBindings* pB, SfxChildWindow* pCW, SfxChildWinInfo* pInfo,
-                        vcl::Window* pParent, sal_uInt16 nSlotId )
+VclPtr<SfxModelessDialog> ScTabViewShell::CreateRefDialog(
+                                SfxBindings* pB, SfxChildWindow* pCW,
+                                SfxChildWinInfo* pInfo,
+                                vcl::Window* pParent, sal_uInt16 nSlotId )
 {
     //  Dialog nur aufmachen, wenn ueber ScModule::SetRefDialog gerufen, damit
     //  z.B. nach einem Absturz offene Ref-Dialoge nicht wiederkommen (#42341#).
@@ -132,7 +133,7 @@ SfxModelessDialog* ScTabViewShell::CreateRefDialog(
         return NULL;
     }
 
-    SfxModelessDialog* pResult = 0;
+    VclPtr<SfxModelessDialog> pResult;
 
     if(pCW)
         pCW->SetHideNotDelete(true);
@@ -156,7 +157,7 @@ SfxModelessDialog* ScTabViewShell::CreateRefDialog(
                                      ScAddress( GetViewData().GetCurX(),
                                                 GetViewData().GetCurY(),
                                                 GetViewData().GetTabNo() ), &maRangeMap);
-                static_cast<ScNameDlg*>(pResult)->SetEntry( maName, maScope);
+                static_cast<ScNameDlg*>(pResult.get())->SetEntry( maName, maScope);
                 mbInSwitch = false;
             }
         }

commit 994b9c4ae7f34012bbfa171cc91a1dddf3c4f88c

tdf#93437: Calc RefDialogs were never actually destructed
as ScTabViewShell::CreateRefDialog was still holding VclPtr
reference to them and never released it -- regression from
commit 087f7fe50b03307e2d9202365886e8c37bd7e6e3 which is hereby
reverted.

Now it is *DlgWrapper holding the reference (via
SfxChildWindow::pWindow) and dialogs get properly destructed
when SfxChildWindow dies in SfxViewFrame::SetChildWindow(0, false)

Re tdf#91125, it turns out that the follow-up commit
06ecf7d2aebe64226142ee27c2512351960f8bb1 is actually enough to
prevent the crash.

Change-Id: I895825da59aabc7c0de530ecb528b9bf5acde562

sc/source/ui/inc/tabvwsh.hxx
@@ -341,9 +341,9 @@ public:
     void            DeactivateOle();
 
     SC_DLLPUBLIC static ScTabViewShell* GetActiveViewShell();
-    VclPtr<SfxModelessDialog> CreateRefDialog( SfxBindings* pB, SfxChildWindow* pCW,
-                                               SfxChildWinInfo* pInfo,
-                                               vcl::Window* pParent, sal_uInt16 nSlotId );
+    SfxModelessDialog*  CreateRefDialog( SfxBindings* pB, SfxChildWindow* pCW,
+                                        SfxChildWinInfo* pInfo,
+                                        vcl::Window* pParent, sal_uInt16 nSlotId );
 
     void            UpdateOleZoom();
 

sc/source/ui/view/reffact.cxx
@@ -91,7 +91,7 @@ namespace
             pViewShell = PTR_CAST( ScTabViewShell, SfxViewShell::Current() ); \
         OSL_ENSURE( pViewShell, "missing view shell :-(" );         \
         pWindow = pViewShell ?                                      \
-            pViewShell->CreateRefDialog( p, this, pInfo, pParentP, sid ) : nullptr;    \
+            pViewShell->CreateRefDialog( p, this, pInfo, pParentP, sid ) : NULL;    \
         if (pViewShell && !pWindow)                                             \
             pViewShell->GetViewFrame()->SetChildWindow( nId, false );           \
     }

sc/source/ui/view/tabvwshc.cxx
@@ -113,10 +113,9 @@ void ScTabViewShell::SwitchBetweenRefDialogs(SfxModelessDialog* pDialog)
    }
 }
 
-VclPtr<SfxModelessDialog> ScTabViewShell::CreateRefDialog(
-                                SfxBindings* pB, SfxChildWindow* pCW,
-                                SfxChildWinInfo* pInfo,
-                                vcl::Window* pParent, sal_uInt16 nSlotId )
+SfxModelessDialog* ScTabViewShell::CreateRefDialog(
+                        SfxBindings* pB, SfxChildWindow* pCW, SfxChildWinInfo* pInfo,
+                        vcl::Window* pParent, sal_uInt16 nSlotId )
 {
     //  Dialog nur aufmachen, wenn ueber ScModule::SetRefDialog gerufen, damit
     //  z.B. nach einem Absturz offene Ref-Dialoge nicht wiederkommen (#42341#).
@@ -133,7 +132,7 @@ VclPtr<SfxModelessDialog> ScTabViewShell::CreateRefDialog(
         return NULL;
     }
 
-    VclPtr<SfxModelessDialog> pResult;
+    SfxModelessDialog* pResult = 0;
 
     if(pCW)
         pCW->SetHideNotDelete(true);
@@ -157,7 +156,7 @@ VclPtr<SfxModelessDialog> ScTabViewShell::CreateRefDialog(
                                      ScAddress( GetViewData().GetCurX(),
                                                 GetViewData().GetCurY(),
                                                 GetViewData().GetTabNo() ), &maRangeMap);
-                static_cast<ScNameDlg*>(pResult.get())->SetEntry( maName, maScope);
+                static_cast<ScNameDlg*>(pResult)->SetEntry( maName, maScope);
                 mbInSwitch = false;
             }
         }
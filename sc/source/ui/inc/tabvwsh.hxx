994b9c4ae7f34012bbfa171cc91a1dddf3c4f88c
Katarina Behrens <Katarina.Behrens@cib.de>
2015-08-21 11:53:57 +0200

    tdf#93437: Calc RefDialogs were never actually destructed
    as ScTabViewShell::CreateRefDialog was still holding VclPtr
    reference to them and never released it -- regression from
    commit 087f7fe50b03307e2d9202365886e8c37bd7e6e3 which is hereby
    reverted.
    
    Now it is *DlgWrapper holding the reference (via
    SfxChildWindow::pWindow) and dialogs get properly destructed
    when SfxChildWindow dies in SfxViewFrame::SetChildWindow(0, false)
    
    Re tdf#91125, it turns out that the follow-up commit
    06ecf7d2aebe64226142ee27c2512351960f8bb1 is actually enough to
    prevent the crash.
    
    Change-Id: I895825da59aabc7c0de530ecb528b9bf5acde562

--- sc/source/ui/inc/tabvwsh.hxx
+++ sc/source/ui/inc/tabvwsh.hxx
@@ -341,9 +341,9 @@ public:
     void            DeactivateOle();
 
     SC_DLLPUBLIC static ScTabViewShell* GetActiveViewShell();
-    VclPtr<SfxModelessDialog> CreateRefDialog( SfxBindings* pB, SfxChildWindow* pCW,
-                                               SfxChildWinInfo* pInfo,
-                                               vcl::Window* pParent, sal_uInt16 nSlotId );
+    SfxModelessDialog*  CreateRefDialog( SfxBindings* pB, SfxChildWindow* pCW,
+                                        SfxChildWinInfo* pInfo,
+                                        vcl::Window* pParent, sal_uInt16 nSlotId );
 
     void            UpdateOleZoom();
 
