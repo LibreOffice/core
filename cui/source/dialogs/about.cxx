be881075568d7686ba2cbcd85d4d9c085e0517ef
László Németh <laszlo.nemeth@collabora.com>
2015-08-26 12:25:14 +0200

    tdf#93620: show OpenGL status in Help->About
    Build ID will show enabled OpenGL with an extra string "-GL"
    helping the fix of rendering issues.
    
    Change-Id: Id7bf2db2edb165542bf7a2a253c698c494278a03

--- cui/source/dialogs/about.cxx
+++ cui/source/dialogs/about.cxx
@@ -48,6 +48,7 @@
 #include <rtl/ustrbuf.hxx>
 #include <vcl/bitmap.hxx>
 #include <officecfg/Office/Common.hxx>
+#include <vcl/opengl/OpenGLHelper.hxx>
 
 using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star::beans;
@@ -292,6 +293,11 @@ OUString AboutDialog::GetVersionString()
             m_sBuildStr += " $BUILDID";
         }
         sVersion += m_sBuildStr.replaceAll("$BUILDID", sBuildId);
+    }
+
+    if (OpenGLHelper::isVCLOpenGLEnabled())
+    {
+        sVersion += "-GL";
     }
 
     if (EXTRA_BUILDID[0] != '\0')
