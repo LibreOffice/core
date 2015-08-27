3a29fd1332434904d5421127bfd08ec135445302
Caolán McNamara <caolanm@redhat.com>
2015-08-27 14:06:04 +0100

    check stream state after read attempt
    Change-Id: Ie3836f2e95acab963634181a07565343501f00f8
    (cherry picked from commit 9a695e071020639926f8b038aba64eb016a1801a)
    Reviewed-on: https://gerrit.libreoffice.org/18079
    Reviewed-by: Michael Meeks <michael.meeks@collabora.com>
    Tested-by: Michael Meeks <michael.meeks@collabora.com>

--- filter/source/msfilter/svdfppt.cxx
+++ filter/source/msfilter/svdfppt.cxx
@@ -5245,9 +5245,13 @@ void PPTStyleTextPropReader::Init( SvStream& rIn, SdrPowerPointImport& rMan, con
 
             PPTCharPropSet aCharPropSet( nCurrentPara );
             if ( bTextPropAtom )
+            {
                 ReadCharProps( rIn, aCharPropSet, aString, nCharCount, nCharAnzRead,
                                bTextPropAtom, nExtParaPos, aStyleTextProp9, nExtParaFlags,
                                nBuBlip, nHasAnm, nAnmScheme );
+                if (!rIn.good())
+                    break;
+            }
             else
                 nCharCount = nStringLen;
 
@@ -5314,7 +5318,7 @@ void PPTStyleTextPropReader::Init( SvStream& rIn, SdrPowerPointImport& rMan, con
                     break;
                 }
             }
-         }
+        }
         if ( !aCharPropList.empty() && ( aCharPropList.back()->mnParagraph != nCurrentPara ) )
         {
             PPTCharPropSet* pCharPropSet = new PPTCharPropSet( *aCharPropList.back(), nCurrentPara );
