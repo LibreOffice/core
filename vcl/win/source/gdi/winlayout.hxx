4176d495dbca1da3316d9e3ac370ac5f2f046b1c
Michael Stahl <mstahl@redhat.com>
2015-08-28 18:37:37 +0200

    vcl: fix resource management issue in WinSalLayout
    Since commit 65a66d41fd0e13d0aad9df935091b731b4af650a the
    sd_exports_test crashes on Windows in UniscribeLayout because
    it uses a ImplFontEntry that has been removed from the font cache.
    
    Tweak the refcount in WinLayout so it will be valid.
    
    (cherry picked from commit 34700400247e378e074ce4164ab2809edb092201)
    
    Change-Id: Ic4bf984ea9fd70de9fa95ca964ae12d95d47d5bf
    Reviewed-on: https://gerrit.libreoffice.org/18120
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>

--- vcl/win/source/gdi/winlayout.hxx
+++ vcl/win/source/gdi/winlayout.hxx
@@ -41,6 +41,7 @@ class WinLayout : public SalLayout
 {
 public:
                         WinLayout(HDC, const ImplWinFontData&, ImplWinFontEntry&, bool bUseOpenGL);
+    virtual             ~WinLayout();
     virtual void        InitFont() const;
     void                SetFontScale( float f ) { mfFontScale = f; }
     HFONT               DisableFontScaling( void) const;
