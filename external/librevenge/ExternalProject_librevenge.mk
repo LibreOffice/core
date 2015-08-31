ad7868d987fc8b5e2c0c4c13ca368cc3962f4ec8
David Ostrovsky <david@ostrovsky.org>
2015-08-31 19:53:49 +0200

    core: fix build with system boost 1.59
    9a6cdce37e601b1406c71fef16ad9b315045c9da was trying to fix the problem
    with exposing deprecated vars and functions in system's error_code.hpp
    include file by patching bundled boost version.  This approach would
    only make sense, when upstream version is going to be fixed ASAP. Apply
    another approach, and follow the same pattern as applied in external
    libraries, by defining
    
      -DBOOST_ERROR_CODE_HEADER_ONLY \
      -DBOOST_SYSTEM_NO_DEPRECATED
    
    instead of patching bundled boost version.  This way, the code would
    work with unpatched system boost 1.59 final as well.
    
    Reviewed-on: https://gerrit.libreoffice.org/18201
    Reviewed-by: David Ostrovsky <david@ostrovsky.org>
    Tested-by: David Ostrovsky <david@ostrovsky.org>
    (cherry picked from commit e00a3a684e22f45f36b4002ce726cb2975cb36ae)
    
    Change-Id: I8684ca458ea4a5b7d7c3c3acfe7c14a6d19bc665
    Reviewed-on: https://gerrit.libreoffice.org/18374
    Reviewed-by: David Tardon <dtardon@redhat.com>
    Tested-by: David Tardon <dtardon@redhat.com>

--- external/librevenge/ExternalProject_librevenge.mk
+++ external/librevenge/ExternalProject_librevenge.mk
@@ -34,7 +34,8 @@ $(call gb_ExternalProject_get_state_target,librevenge,build) :
 			--disable-generators \
 			--without-docs \
 			$(if $(VERBOSE)$(verbose),--disable-silent-rules,--enable-silent-rules) \
-			CXXFLAGS="$(if $(SYSTEM_BOOST),$(BOOST_CPPFLAGS),-I$(call gb_UnpackedTarball_get_dir,boost))" \
+			CXXFLAGS="$(if $(SYSTEM_BOOST),$(BOOST_CPPFLAGS),-I$(call gb_UnpackedTarball_get_dir,boost)) \
+				-DBOOST_ERROR_CODE_HEADER_ONLY -DBOOST_SYSTEM_NO_DEPRECATED" \
 			$(if $(CROSS_COMPILING),--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM)) \
 			$(if $(filter MACOSX,$(OS)),--prefix=/@.__________________________________________________OOO) \
 		&& $(MAKE) \
