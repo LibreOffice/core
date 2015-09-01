d9305ae68bf3707242a96fb1b697c36d6eaf91c4
Juergen Funk <juergen.funk_ml@cib.de>
2015-09-01 11:03:46 +0200

    tdf#93052 - When save, generate unused list-styles in context.xml
    First Patch do not solved the problem
    The evaluation of the 0 is here
    http://opengrok.libreoffice.org/xref/core/xmloff/source/text/txtparae.cxx#615
    and this methode "xNumRule->getCount()" get back the 0.
    
    Change-Id: I2a8f53199b0819f36a7d3e1887c75089143b29a9
    Reviewed-on: https://gerrit.libreoffice.org/18228
    Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>
    Tested-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>

--- editeng/source/editeng/eerdll.cxx
+++ editeng/source/editeng/eerdll.cxx
@@ -101,7 +101,7 @@ SfxPoolItem** GlobalEditData::GetDefItems()
         ppDefItems = new SfxPoolItem*[EDITITEMCOUNT];
 
         // Paragraph attributes:
-        SvxNumRule aDefaultNumRule( SvxNumRuleFlags::NONE, SVX_MAX_NUM, false );
+        SvxNumRule aDefaultNumRule( SvxNumRuleFlags::NONE, 0, false );
 
         ppDefItems[0]  = new SvxFrameDirectionItem( FRMDIR_HORI_LEFT_TOP, EE_PARA_WRITINGDIR );
         ppDefItems[1]  = new SvXMLAttrContainerItem( EE_PARA_XMLATTRIBS );
