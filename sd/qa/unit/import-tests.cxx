ce4a43f6be7ad71acc9ab513cc5db20d5cf5f36a
Andras Timar <andras.timar@collabora.com>
2015-09-01 10:46:42 +0200

    tdf#93830 txXfrm offset workaround works in rounded rectangle case as well
    Reviewed-on: https://gerrit.libreoffice.org/18226
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Andras Timar <andras.timar@collabora.com>
    (cherry picked from commit cc88a88caad591270760d947848ad0a032d32bdb)
    
    Conflicts:
    	sd/qa/unit/import-tests.cxx
    
    Change-Id: I5d8df5c0d55e8dfd7c978b16b2b220c127f3dac9

--- sd/qa/unit/import-tests.cxx
+++ sd/qa/unit/import-tests.cxx
@@ -101,6 +101,7 @@ public:
     void testPDFImportSkipImages();
 #endif
     void testBnc910045();
+    void testTdf93830();
 
     CPPUNIT_TEST_SUITE(SdImportTest);
 
@@ -138,6 +139,7 @@ public:
     CPPUNIT_TEST(testPDFImportSkipImages);
 #endif
     CPPUNIT_TEST(testBnc910045);
+    CPPUNIT_TEST(testTdf93830);
 
     CPPUNIT_TEST_SUITE_END();
 };
@@ -1145,6 +1147,29 @@ void SdImportTest::testBnc910045()
     xCell.set(xTable->getCellByPosition(0, 0), uno::UNO_QUERY_THROW);
     xCell->getPropertyValue("FillColor") >>= nColor;
     CPPUNIT_ASSERT_EQUAL(sal_Int32(5210557), nColor);
+}
+
+void SdImportTest::testTdf93830()
+{
+    // Text shape offset was ignored
+    sd::DrawDocShellRef xDocShRef = loadURL(getURLFromSrc("/sd/qa/unit/data/pptx/tdf93830.pptx"), PPTX);
+
+    uno::Reference< drawing::XDrawPagesSupplier > xDoc(
+        xDocShRef->GetDoc()->getUnoModel(), uno::UNO_QUERY_THROW );
+
+    uno::Reference< drawing::XDrawPage > xPage(
+        xDoc->getDrawPages()->getByIndex(0), uno::UNO_QUERY_THROW );
+
+    // Get the first text box from group shape
+    uno::Reference< container::XIndexAccess > xShape( xPage->getByIndex(0), uno::UNO_QUERY );
+    uno::Reference< beans::XPropertySet > xPropSet( xShape->getByIndex(2), uno::UNO_QUERY );
+    CPPUNIT_ASSERT_MESSAGE( "no textbox shape", xPropSet.is() );
+
+    sal_Int32 nTextLeftDistance = 0;
+    xPropSet->getPropertyValue( "TextLeftDistance" ) >>= nTextLeftDistance;
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(4152), nTextLeftDistance);
+
+    xDocShRef->DoClose();
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(SdImportTest);
