03354b2220d6e04bb81dcb6c52470d7d255029c3
David Ostrovsky <david@ostrovsky.org>
2015-09-13 11:36:09 +0200

    libfreehand: Replace FHTransform forward declaration with struct
    Change-Id: I67093550d89b323914ae42014d55e89a38adec8c

--- external/libfreehand/UnpackedTarball_libfreehand.mk
+++ external/libfreehand/UnpackedTarball_libfreehand.mk
@@ -13,6 +13,10 @@ $(eval $(call gb_UnpackedTarball_set_tarball,libfreehand,$(FREEHAND_TARBALL)))
 
 $(eval $(call gb_UnpackedTarball_set_patchlevel,libfreehand,0))
 
+$(eval $(call gb_UnpackedTarball_add_patches,libfreehand,\
+	external/libfreehand/replace_transform_class_with_struct.patch \
+))
+
 ifeq ($(COM_GCC_IS_CLANG),TRUE)
 ifneq ($(filter -fsanitize=%,$(CC)),)
 $(eval $(call gb_UnpackedTarball_add_patches,libfreehand, \
