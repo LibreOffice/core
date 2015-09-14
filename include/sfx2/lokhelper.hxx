2fc69762a43f97df4f790a641b23f2d556c069b3
Miklos Vajna <vmiklos@collabora.co.uk>
2015-09-14 14:36:38 +0200

    sfx2: add SfxLokHelper
    This is meant to be a class that is visible outside sfx2 (so e.g.
    desktop can use it later), but has access to various sfx2 internals.
    
    Change-Id: I83204963492b11c1c4a621e86528a64fba27acf3

---
+++ include/sfx2/lokhelper.hxx
@@ -0,0 +1,21 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <sfx2/dllapi.h>
+
+class SfxViewShell;
+
+class SFX2_DLLPUBLIC SfxLokHelper
+{
+public:
+    /// Create a new view shell for pViewShell's object shell.
+    static int createView(SfxViewShell* pViewShell);
+};
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
