477bd979170e754322d7656c5bf195bdc4777bd9
Miklos Vajna <vmiklos@collabora.co.uk>
2015-09-14 14:36:38 +0200

    sfx2: add SfxLokHelper
    This is meant to be a class that is visible outside sfx2 (so e.g.
    desktop can use it later), but has access to various sfx2 internals.
    
    Change-Id: I83204963492b11c1c4a621e86528a64fba27acf3
    (cherry picked from commit 58b5c13b00cd4d881e1d6313316cc621198a4b04)

---
+++ sfx2/source/view/lokhelper.cxx
@@ -0,0 +1,29 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <sfx2/lokhelper.hxx>
+#include <sfx2/viewsh.hxx>
+#include <sfx2/request.hxx>
+#include <sfx2/sfxsids.hrc>
+#include <sfx2/viewfrm.hxx>
+
+#include <shellimpl.hxx>
+
+int SfxLokHelper::createView(SfxViewShell* pViewShell)
+{
+    SfxViewFrame* pViewFrame = pViewShell->GetViewFrame();
+    SfxRequest aRequest(pViewFrame, SID_NEWWINDOW);
+    pViewFrame->ExecView_Impl(aRequest);
+
+    // The SfxViewShell ctor always puts the view shell to the end of the vector.
+    SfxViewShellArr_Impl& rViewArr = SfxGetpApp()->GetViewShells_Impl();
+    return rViewArr.size() - 1;
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
