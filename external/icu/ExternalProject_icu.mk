e7b15e556ca462f85678753f43e17769ffded244
David Tardon <dtardon@redhat.com>
2015-09-17 20:36:40 +0200

    pass user-set CXXFLAGS
    Change-Id: I1b4f0f3e7f5a7092f904fc8de59bb704073ed7db
    (cherry picked from commit 8e8ad3f697c4caf0e48edf0953e706625fba47bb)

--- external/icu/ExternalProject_icu.mk
+++ external/icu/ExternalProject_icu.mk
@@ -57,7 +57,7 @@ icu_CFLAGS:=" \
 	$(if $(filter GCC,$(COM)),-fno-strict-aliasing) \
 	$(if $(filter $(true),$(gb_SYMBOL)),-g) \
 	$(if $(filter ANDROID,$(OS)),-fvisibility=hidden -fno-omit-frame-pointer)"
-icu_CXXFLAGS:="$(CXXFLAGS_CXX11) \
+icu_CXXFLAGS:="$(CXXFLAGS) $(CXXFLAGS_CXX11) \
 	$(if $(filter IOS,$(OS)),-DUCONFIG_NO_FILE_IO) \
 	$(if $(debug),$(gb_DEBUG_CFLAGS),$(gb_COMPILEROPTFLAGS)) \
 	$(if $(ENABLE_LTO),$(gb_LTOFLAGS)) \
