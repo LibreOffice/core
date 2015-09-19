a7b9d29dd6b166c134666f8a235afbc1f2bd0288
Tor Lillqvist <tml@collabora.com>
2015-09-19 21:06:14 +0300

    We don't want libxmlsec on iOS, I think
    It would require NSS which I just decided to skip. If/when we patch it
    to support native iOS (and OS X?) crypto APIs, re-consider. Or
    something.
    
    Now the TiledLibreOffice iOS demo app builds again.
    
    Change-Id: I2816a49bbdbde5a8f9d3b9fcdfff420bdca05ff8

--- external/Module_external.mk
+++ external/Module_external.mk
@@ -19,7 +19,7 @@ endif
 
 $(eval $(call gb_Module_add_moduledirs,external,\
 	accessories \
-	libxmlsec \
+	$(if $(filter-out IOS,$(OS)),libxmlsec) \
 	$(call gb_Helper_optional,ABW,libabw) \
 	$(call gb_Helper_optional,APACHE_COMMONS,apache-commons) \
 	$(call gb_Helper_optional,APR,apr) \
