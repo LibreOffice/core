15fba2512f41ca8a27839c3600f3e996cb4a4719
Cor Nouws <oolst@nouenoff.nl>
2015-09-23 00:40:06 +0200

    tdf#86397  Bookmark: no consecutive name numbering applied when copied
    this partially changes behaviour introduced with commit bb00a0097900ae054401f7758a915047cfde4065
    but without the performace problems from the old code
    thanks to kendy for helping
    
    (cherry picked from commit 2fcf8923d2c520a5a16b1b3a45877adaadd7eab4)
    
    Change-Id: I5dab81b58262f67db1c70223c612636a8b8c90ee
    Reviewed-on: https://gerrit.libreoffice.org/18816
    Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
    Tested-by: Miklos Vajna <vmiklos@collabora.co.uk>
    (cherry picked from commit a9670e0735b77ecc40aa8af4106af7d32ec548a0)

--- sw/source/core/inc/MarkManager.hxx
+++ sw/source/core/inc/MarkManager.hxx
@@ -27,6 +27,7 @@
 
 namespace sw {
     namespace mark {
+    typedef std::unordered_map<OUString, sal_Int32, OUStringHash> MarkBasenameMapUniqueOffset_t;
 
     class MarkManager
         : private ::boost::noncopyable
@@ -109,6 +110,7 @@ namespace sw {
             container_t m_vFieldmarks;
 
             std::unordered_set<OUString, OUStringHash> m_aMarkNamesSet;
+            mutable MarkBasenameMapUniqueOffset_t m_aMarkBasenameMapUniqueOffset;
 
             // container for annotation marks
             container_t m_vAnnotationMarks;
