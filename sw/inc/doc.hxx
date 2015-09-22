ad0e0f8c6889fe45679779aeb14ace1ceff7cbb5
Noel Grandin <noelgrandin@gmail.com>
2015-09-22 21:40:58 +0200

    remove unused Link<> parameter
    Change-Id: I9d8916b69e2e986ee1e30091cb046e7812a68ed1

--- sw/inc/doc.hxx
+++ sw/inc/doc.hxx
@@ -1403,8 +1403,7 @@ public:
 
     // Call into intransparent Basic / JavaScript.
     sal_uInt16 CallEvent( sal_uInt16 nEvent, const SwCallMouseEvent& rCallEvent,
-                        bool bChkPtr = false, SbxArray* pArgs = 0,
-                        const Link<>* pCallBack = 0 );
+                        bool bChkPtr = false, SbxArray* pArgs = 0 );
 
     /** Adjust left margin via object bar (similar to adjustment of numerations).
      One can either change the margin "by" adding or subtracting a given
--- sw/source/uibase/wrtsh/wrtsh3.cxx
+++ sw/source/uibase/wrtsh/wrtsh3.cxx
@@ -159,10 +159,9 @@ void SwWrtShell::ExecMacro( const SvxMacro& rMacro, OUString* pRet, SbxArray* pA
 }
 
 sal_uInt16 SwWrtShell::CallEvent( sal_uInt16 nEvent, const SwCallMouseEvent& rCallEvent,
-                                bool bChkPtr, SbxArray* pArgs,
-                                const Link<>* pCallBack )
+                                bool bChkPtr, SbxArray* pArgs)
 {
-    return GetDoc()->CallEvent( nEvent, rCallEvent, bChkPtr, pArgs, pCallBack );
+    return GetDoc()->CallEvent( nEvent, rCallEvent, bChkPtr, pArgs );
 }
 
     // If a util::URL-Button is selected, return its util::URL
