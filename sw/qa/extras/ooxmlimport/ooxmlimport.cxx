2ca166ebbe2adbd8f129403c7f8e5646c70ab67d
Miklos Vajna <vmiklos@collabora.co.uk>
2015-09-22 08:47:03 +0200

    tdf#92454 DOCX import: allow overriding para prop from num style in para style
    Word has a feature like this: a paragraph style can refer to a numbering
    style, and both can specify paragraph margins. If that's the case, then
    the ones from the paragraph style has priority.
    
    In Writer, the numbering style has priority, so the only chance for
    correct import result is to set the margin directly on the paragraph in
    this case.
    
    (cherry picked from commit f4badd9a485f32f787d78431ed673e2932973887)
    
    Conflicts:
    	sw/qa/extras/ooxmlimport/ooxmlimport.cxx
    
    Change-Id: Iff3b03bcc56e0db3a48452c293acf41c91b8f159
    Reviewed-on: https://gerrit.libreoffice.org/19100
    Reviewed-by: Andras Timar <andras.timar@collabora.com>
    Tested-by: Andras Timar <andras.timar@collabora.com>

--- sw/qa/extras/ooxmlimport/ooxmlimport.cxx
+++ sw/qa/extras/ooxmlimport/ooxmlimport.cxx
@@ -2647,6 +2647,16 @@ DECLARE_OOXMLIMPORT_TEST(testTdf93919, "tdf93919.docx")
     CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(1270), getProperty<sal_Int32>(getParagraph(1), "ParaLeftMargin"));
 }
 
+DECLARE_OOXMLIMPORT_TEST(testTdf92454, "tdf92454.docx")
+{
+    // The first paragraph had a large indentation / left margin as inheritance
+    // in Word and Writer works differently, and no direct value was set to be
+    // explicit.
+    uno::Reference<beans::XPropertyState> xParagraph(getParagraph(1), uno::UNO_QUERY);
+    // This was beans::PropertyState_DEFAULT_VALUE.
+    CPPUNIT_ASSERT_EQUAL(beans::PropertyState_DIRECT_VALUE, xParagraph->getPropertyState("ParaFirstLineIndent"));
+}
+
 #endif
 
 CPPUNIT_PLUGIN_IMPLEMENT();
