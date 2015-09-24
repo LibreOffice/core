9304c17788856be82a39da6bd59fca48afe7c000
Caolán McNamara <caolanm@redhat.com>
2015-09-24 10:48:53 +0100

    sd/source/filter boost::scoped_ptr->std::unique_ptr
    Change-Id: I335ff685def480933d846657df81f28242059c18

--- sd/source/filter/ppt/pptinanimations.cxx
+++ sd/source/filter/ppt/pptinanimations.cxx
@@ -68,7 +68,6 @@
 
 #include <algorithm>
 #include <memory>
-#include <boost/scoped_ptr.hpp>
 
 using ::std::map;
 using ::com::sun::star::beans::NamedValue;
@@ -223,7 +222,7 @@ int AnimationImporter::import( const Reference< XDrawPage >& xPage, const DffRec
         {
             Reference< XAnimationNode > xParent;
 
-            boost::scoped_ptr<Atom> pAtom(Atom::import( rProgTagContentHd, mrStCtrl ));
+            std::unique_ptr<Atom> pAtom(Atom::import( rProgTagContentHd, mrStCtrl ));
             if( pAtom )
             {
                 nNodes = importAnimationContainer( pAtom.get(), xParent );
