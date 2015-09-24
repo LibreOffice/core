ec63c2eda701a905dd1252166d76d9c3a73db18c
Takeshi Abe <tabe@fixedpoint.jp>
2015-09-24 13:12:36 +0900

    starmath: tdf#93240 replace boost::ptr_vector
    with std::vector<std::unique_ptr>
    
    Change-Id: I72f96b08273c73cbd11c7796c34a45b262325209

--- starmath/inc/parse.hxx
+++ starmath/inc/parse.hxx
@@ -20,8 +20,9 @@
 #define INCLUDED_STARMATH_INC_PARSE_HXX
 
 #include <vcl/svapp.hxx>
+#include <memory>
 #include <set>
-#include <boost/ptr_container/ptr_vector.hpp>
+#include <vector>
 
 #include "types.hxx"
 #include "token.hxx"
@@ -33,7 +34,7 @@ class SmParser
     OUString        m_aBufferString;
     SmToken         m_aCurToken;
     SmNodeStack     m_aNodeStack;
-    boost::ptr_vector< SmErrorDesc > m_aErrDescList;
+    std::vector<std::unique_ptr<SmErrorDesc>> m_aErrDescList;
     int             m_nCurError;
     LanguageType    m_nLang;
     sal_Int32       m_nBufferIndex,
