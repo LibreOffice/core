3f70d2d2a2fd1a243430db9bf2e6313ccbea417d
Miklos Vajna <vmiklos@collabora.co.uk>
2015-09-25 01:06:31 +0200

    desktop: make LibLibreOffice_Impl visible to testcases
    (cherry picked from commit 14a9a7d52074104afa24278a7a350f05d6df572a)
    
    Conflicts:
    	desktop/source/lib/init.cxx
    
    Change-Id: I345a6cab57cc594614e348700bb3c1c9a0673989

--- desktop/inc/lib/init.hxx
+++ desktop/inc/lib/init.hxx
@@ -11,6 +11,7 @@
 #include <com/sun/star/lang/XComponent.hpp>
 #include <boost/shared_ptr.hpp>
 #include "../../source/inc/desktopdllapi.h"
+#include <osl/thread.h>
 
 using namespace css;
 using namespace boost;
@@ -23,5 +24,16 @@ namespace desktop {
 
         explicit LibLODocument_Impl(const uno::Reference <css::lang::XComponent> &xComponent);
         ~LibLODocument_Impl();
+    };
+
+    struct DESKTOP_DLLPUBLIC LibLibreOffice_Impl : public _LibreOfficeKit
+    {
+        OUString maLastExceptionMsg;
+        boost::shared_ptr< LibreOfficeKitClass > m_pOfficeClass;
+        oslThread maThread;
+        LibreOfficeKitCallback mpCallback;
+        void *mpCallbackData;
+
+        LibLibreOffice_Impl();
     };
 }
