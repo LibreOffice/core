5214e130af7f8c5476977f5a150a7cdeb1f100ba
Eike Rathke <erack@redhat.com>
2015-09-25 13:42:04 +0200

    fix IterateMatrix for ifPRODUCT and ifSUMSQ
    ... if more than one argument is passed to PRODUCT() or SUMSQ() and
    matrix/array arguments are involved the functions returned wrong
    results.
    
    For example, wrong behavior
    =PRODUCT({2,3},{4,5})   gave 6
    =SUMSQ({2,3},{4,5})     gave 34
    
    Correct is
    =PRODUCT({2,3},{4,5})   result 120
    =SUMSQ({2,3},{4,5})     result 54
    
    Change-Id: Iec7b136a5cc678416f28b1abfe19cd0901ef22b6
    (cherry picked from commit c1780ad265ce5f6abb3b91a4f70bf2b3d7a47149)

--- sc/source/core/tool/interpr6.cxx
+++ sc/source/core/tool/interpr6.cxx
@@ -387,7 +387,7 @@ void IterateMatrix(
         case ifPRODUCT:
         {
             ScMatrix::IterateResult aRes = pMat->Product(bTextAsZero);
-            fRes = aRes.mfFirst;
+            fRes *= aRes.mfFirst;
             fRes *= aRes.mfRest;
             rCount += aRes.mnCount;
         }
@@ -395,6 +395,7 @@ void IterateMatrix(
         case ifSUMSQ:
         {
             ScMatrix::IterateResult aRes = pMat->SumSquare(bTextAsZero);
+            fRes += aRes.mfFirst;
             fRes += aRes.mfRest;
             rCount += aRes.mnCount;
         }
