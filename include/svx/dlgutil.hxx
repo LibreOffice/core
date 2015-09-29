f7f3a899e4d3d70715528bed0ce9689ba2fc894b
Stephan Bergmann <sbergman@redhat.com>
2015-09-29 14:19:47 +0200

    Avoid unhelpful -Wunused-variable
    ...at least from "g++ (GCC) 5.1.1 20150618 (Red Hat 5.1.1-4)" with
    --disable-debug, when a namespace-scope const variable with a "complex"
    initializer declared in an include file remains unused.
    
    Avoid that warning via SAL_CONSTEXPR, which in turn requires large parts of
    o3tl::is_typed_flags to be SAL_CONSTEXPR, which in turn requires a new
    HAVE_CXX14_CONSTEXPR to allow assert in constexpr functions, which in turn
    requires using -std=c++14 instead of -std=c++11 where available, which in turn
    (a) requires to /not/ use -std=c++14 if it would run into a bug between Clang
    and libstdc++ discussed at <https://llvm.org/bugs/show_bug.cgi?id=24115>
    "llvm-nm fails to build with gcc 5.1's libstdc++" (and which hits us in
    sfx2/source/control/thumbnailview.cxx), and (b) requires a new
    HAVE_CXX14_SIZED_DEALLOCATION to work around GCC 5.1 -Werror=sized-deallocation
    (where Clang >= 3.7 only supports C++14 sized deallocation when explictly
    enabled via -fsized-deallocation, btw).
    
    This effectively reverts ff6462e6307e6924dc6c8178043ae9032f4b4152 "avoid unused
    variable warning:" again.
    
    Change-Id: I424e3561452a3e6d8c8a9604d6c737cab49840c4

--- include/svx/dlgutil.hxx
+++ include/svx/dlgutil.hxx
@@ -33,8 +33,8 @@ SVX_DLLPUBLIC FieldUnit GetModuleFieldUnit( const SfxItemSet& );
 SVX_DLLPUBLIC FieldUnit GetModuleFieldUnit();
 SVX_DLLPUBLIC bool GetApplyCharUnit( const SfxItemSet& );
 
-#define OUTPUT_DRAWMODE_COLOR    DrawModeFlags::Default
-#define OUTPUT_DRAWMODE_CONTRAST (DrawModeFlags::SettingsLine | DrawModeFlags::SettingsFill | DrawModeFlags::SettingsText | DrawModeFlags::SettingsGradient)
+SAL_CONSTEXPR DrawModeFlags const OUTPUT_DRAWMODE_COLOR = DrawModeFlags::Default;
+SAL_CONSTEXPR DrawModeFlags const OUTPUT_DRAWMODE_CONTRAST = DrawModeFlags::SettingsLine | DrawModeFlags::SettingsFill | DrawModeFlags::SettingsText | DrawModeFlags::SettingsGradient;
 
 #endif
 
