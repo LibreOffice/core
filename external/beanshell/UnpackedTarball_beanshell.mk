841a58da93e10f543876eefca470d8fd4bd96ded
Douglas Mencken <dougmencken@gmail.com>
2015-09-30 12:16:08 -0400

    beanshell: improve compatibility for script engine
    javax.script.Invocable may contain either
    • two `invoke' methods
    or
    • `invokeMethod' and `invokeFunction'
    so let's support any variant
    
    Change-Id: I47196af79cc2de75725ded29992ab1f6f4cd623e
    Reviewed-on: https://gerrit.libreoffice.org/23163
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>

--- external/beanshell/UnpackedTarball_beanshell.mk
+++ external/beanshell/UnpackedTarball_beanshell.mk
@@ -11,8 +11,13 @@ $(eval $(call gb_UnpackedTarball_UnpackedTarball,beanshell))
 
 $(eval $(call gb_UnpackedTarball_set_tarball,beanshell,$(BSH_TARBALL),,beanshell))
 
+$(eval $(call gb_UnpackedTarball_fix_end_of_line,beanshell,\
+	engine/src/TestBshScriptEngine.java \
+))
+
 $(eval $(call gb_UnpackedTarball_add_patches,beanshell,\
 	external/beanshell/bsh-2.0b1-src.patch \
+	external/beanshell/beanshell-invoke.patch \
 ))
 
 # vim: set noet sw=4 ts=4:
