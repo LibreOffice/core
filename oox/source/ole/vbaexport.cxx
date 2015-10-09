bb0c5f767a1e124c1ca24f783676f91749b73e80
Rosemary <rosemaryseb8@gmail.com>
2015-10-09 13:10:36 +0200

    Convert hex string to upper case
    Change-Id: I5ccdfc62a8fd3521b6c4ee0029e70b41aaf4542e

--- oox/source/ole/vbaexport.cxx
+++ oox/source/ole/vbaexport.cxx
@@ -830,7 +830,7 @@ OUString createHexStringFromDigit(sal_uInt8 nDigit)
     OUString aString = OUString::number( nDigit, 16 );
     if(aString.getLength() == 1)
         aString = aString + OUString::number(0);
-    return aString;
+    return aString.toAsciiUpperCase();
 }
 
 OUString createGuidStringFromInt(sal_uInt8 nGuid[16])
