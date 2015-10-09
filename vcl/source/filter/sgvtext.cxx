948c2d7b9fb30c5da0b0c10685828f401cb5bd03
Stephan Bergmann <sbergman@redhat.com>
2015-10-09 12:24:03 +0200

    clang-analyzer-deadcode.DeadStores
    Change-Id: Ic76cf540896ba217f7df6045be31197b64298bf3

--- vcl/source/filter/sgvtext.cxx
+++ vcl/source/filter/sgvtext.cxx
@@ -724,10 +724,10 @@ void FormatLine(UCHAR* TBuf, sal_uInt16& Index, ObjTextType& Atr0, ObjTextType& 
 
     if (Border) { // separate and crush
         (*WErec0)=(*WErec); WEnChar0=WEnChar;
-        AbsEnd=false; c0=0;
+        c0=0;
         (*R)=(*WErec); nChars=WEnChar;
         (*TRrec)=(*R); TRnChar=nChars;
-        Border0=false; Border=false;
+        Border0=false;
         do {                // first check how many syllables fit
             UCHAR ct=ProcessChar(*vOut.get(),TBuf,*TRrec,Atr0,TRnChar,DoTrenn,Line,cLine);
             c=ProcessChar(*vOut.get(),TBuf,*R,Atr0,nChars,NoTrenn,Line,cLine);
