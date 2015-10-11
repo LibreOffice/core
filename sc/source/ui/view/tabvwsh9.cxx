d0f3d516828cc783450afffb893e78479b5b6e76
Albert Thuswaldner <albert.thuswaldner@gmail.com>
2015-10-11 20:15:35 +0200

    tdf#39468 translated german comments in tabvwsh9.cxx
    Change-Id: Iaab7081df2f15e7228f7f99184a048abd26decc7

--- sc/source/ui/view/tabvwsh9.cxx
+++ sc/source/ui/view/tabvwsh9.cxx
@@ -157,7 +157,7 @@ void ScTabViewShell::GetImageMapState( SfxItemSet& rSet )
         {
             case SID_IMAP:
                 {
-                    //  Disabled wird nicht mehr...
+                    // disabled is no more...
 
                     bool bThere = false;
                     SfxViewFrame* pThisFrame = GetViewFrame();
