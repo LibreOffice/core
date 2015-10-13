02188ace1201b45b3c17fa58a62da794d809b270
Eike Rathke <erack@redhat.com>
2015-10-13 14:44:09 +0200

    do not use white text on white background in control
    ... setting a control's background color has no effect (until input
    field text is changed?)
    
    Could be observed in the pivot table dialog's source range edit when an
    invalid range was entered.
    
    Change-Id: Iafb2a9016ac6bd4c020273911d62dd7423ee0458
    (cherry picked from commit 01a93d348b84a72b0d6829c2c730263f29a305bc)
    Reviewed-on: https://gerrit.libreoffice.org/19354
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>

--- formula/source/ui/dlg/funcutl.cxx
+++ formula/source/ui/dlg/funcutl.cxx
@@ -519,8 +519,14 @@ void RefEdit::SetRefValid(bool bValid)
     }
     else
     {
+#if 0
+        // Setting background color has no effect here so we'd end up with
+        // white on white!
         SetControlForeground(COL_WHITE);
         SetControlBackground(0xff6563);
+#else
+        SetControlForeground( ::Color( RGB_COLORDATA( 0xf0, 0, 0)));
+#endif
     }
 }
 
