3eefa774e41d2f813044b67a363895e8328a76e5
Miklos Vajna <vmiklos@collabora.co.uk>
2015-10-14 12:32:12 +0200

    sd tiled rendering: search rectangle is part-specific
    Change-Id: I88865db539779bc401550d7b4a3729b89d7e18b1
    (cherry picked from commit dd1fc2242a64a0b9ae8031a5edc7ecfcde4ec3df)

--- sd/source/ui/view/Outliner.cxx
+++ sd/source/ui/view/Outliner.cxx
@@ -83,6 +83,12 @@ class SfxStyleSheetPool;
 
 namespace sd {
 
+SearchSelection::SearchSelection(int nPage, const OString& rRectangles)
+    : m_nPage(nPage),
+    m_aRectangles(rRectangles)
+{
+}
+
 class Outliner::Implementation
 {
 public:
@@ -621,7 +627,7 @@ bool Outliner::SearchAndReplaceAll()
 
         // Search/replace until the end of the document is reached.
         bool bFoundMatch;
-        std::vector<OString> aSelections;
+        std::vector<SearchSelection> aSelections;
         do
         {
             bFoundMatch = ! SearchAndReplaceOnce(&aSelections);
@@ -634,10 +640,10 @@ bool Outliner::SearchAndReplaceAll()
             aTree.put("searchString", mpSearchItem->GetSearchString().toUtf8().getStr());
 
             boost::property_tree::ptree aChildren;
-            for (const OString& rSelection : aSelections)
+            for (const SearchSelection& rSelection : aSelections)
             {
                 boost::property_tree::ptree aChild;
-                aChild.put("", rSelection.getStr());
+                aChild.put("", rSelection.m_aRectangles.getStr());
                 aChildren.push_back(std::make_pair("", aChild));
             }
             aTree.add_child("searchResultSelection", aChildren);
@@ -655,7 +661,7 @@ bool Outliner::SearchAndReplaceAll()
     return true;
 }
 
-bool Outliner::SearchAndReplaceOnce(std::vector<OString>* pSelections)
+bool Outliner::SearchAndReplaceOnce(std::vector<SearchSelection>* pSelections)
 {
     DetectChange ();
 
@@ -775,7 +781,7 @@ bool Outliner::SearchAndReplaceOnce(std::vector<OString>* pSelections)
         }
         else
         {
-            pSelections->push_back(sRectangles);
+            pSelections->push_back(SearchSelection(maCurrentPosition.mnPageIndex, sRectangles));
         }
     }
 
