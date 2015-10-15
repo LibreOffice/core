026fd18e4b288bbaeb16030922a1c192da4f9fa5
Oliver Specht <oliver.specht@cib.de>
2015-10-15 18:55:55 +0200

    tdf#95091: enable delete and backspace in editable elements within protections
    when editing is enabled also delete and backspace should be possible
    this applies e.g. to input fields within protected sections
    
    Change-Id: Iee5ef133971a3f2967e6c92e685a16afa003cfe8

--- sw/source/uibase/docvw/edtwin.cxx
+++ sw/source/uibase/docvw/edtwin.cxx
@@ -1831,7 +1831,7 @@ KEYINPUT_CHECKTABLE_INSDEL:
                     break;
 
                 case KEY_DELETE:
-                    if ( !rSh.HasReadonlySel() )
+                    if ( !rSh.HasReadonlySel() || rSh.CrsrInsideInputField())
                     {
                         if (rSh.IsInFrontOfLabel() && rSh.NumOrNoNum(false))
                             eKeyState = KS_NumOrNoNum;
@@ -1918,7 +1918,7 @@ KEYINPUT_CHECKTABLE_INSDEL:
                 break;
                 case KEY_BACKSPACE:
                 case KEY_BACKSPACE | KEY_SHIFT:
-                    if ( !rSh.HasReadonlySel() )
+                    if ( !rSh.HasReadonlySel() || rSh.CrsrInsideInputField())
                     {
                         bool bDone = false;
                         // try to add comment for code snip:
