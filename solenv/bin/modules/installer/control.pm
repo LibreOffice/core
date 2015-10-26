7bcd8ed0faaefb8fe3783d6e31dea5c727eddc2a
Michael Stahl <mstahl@redhat.com>
2015-10-26 16:41:41 +0100

    solenv: try to fix installer erroneous error detection of python3.5
    ... which (on Mac) has files named "error.cpython-35.opt-1.pyc",
    "error.cpython-35.opt-2.pyc", and "error.cpython-35.pyc".
    
    Change-Id: I24cea3c7d2aacbda5e39224a975e1c6b34a57244
    Reviewed-on: https://gerrit.libreoffice.org/19608
    Reviewed-by: Norbert Thiebaud <nthiebaud@gmail.com>
    Tested-by: Norbert Thiebaud <nthiebaud@gmail.com>

--- solenv/bin/modules/installer/control.pm
+++ solenv/bin/modules/installer/control.pm
@@ -324,7 +324,7 @@ sub check_logfile
         $compareline =~ s/Error\.idl//g;    # removing all occurrences of "Error.idl"
         $compareline =~ s/Error\.html//g;   # removing all occurrences of "Error.html"
         $compareline =~ s/error\.py//g;     # removing all occurrences of "error.py"
-        $compareline =~ s/error\.cpython-33\.py[oc]//g;  # removing all occurrences of "error-cpython"
+        $compareline =~ s/error\.cpython\-3.(\.opt\-.|)\.py[co]//g;  # removing all occurrences of "error-cpython"
         $compareline =~ s/libgpg-error-0.dll//g;
         $compareline =~ s/Error-xref\.html//g;
 
