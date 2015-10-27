1ee968e09083d86ac0bf3d1bbebd7bc4c1f759a8
Maxim Monastirsky <momonasmon@gmail.com>
2015-10-27 14:20:03 +0200

    tdf#64027 sw: Make the text background color button work
    Change-Id: I305163b691a8fa51ef5da0c940c59c3a3440a2dc
    (cherry picked from commit 96d03636a5f932151c7842ae34631258891fe807)

--- sw/source/uibase/shells/drwtxtex.cxx
+++ sw/source/uibase/shells/drwtxtex.cxx
@@ -169,6 +169,7 @@ void SwDrawTextShell::Execute( SfxRequest &rReq )
         break;
 
         case SID_ATTR_CHAR_COLOR: nEEWhich = EE_CHAR_COLOR; break;
+        case SID_ATTR_CHAR_BACK_COLOR: nEEWhich = EE_CHAR_BKGCOLOR; break;
 
         case SID_ATTR_CHAR_UNDERLINE:
         {
@@ -913,6 +914,7 @@ void SwDrawTextShell::GetDrawTextCtrlState(SfxItemSet& rSet)
             }
             break;
             case SID_ATTR_CHAR_COLOR: nEEWhich = EE_CHAR_COLOR; break;
+            case SID_ATTR_CHAR_BACK_COLOR: nEEWhich = EE_CHAR_BKGCOLOR; break;
             case SID_ATTR_CHAR_UNDERLINE: nEEWhich = EE_CHAR_UNDERLINE;break;
             case SID_ATTR_CHAR_OVERLINE: nEEWhich = EE_CHAR_OVERLINE;break;
             case SID_ATTR_CHAR_CONTOUR: nEEWhich = EE_CHAR_OUTLINE; break;
