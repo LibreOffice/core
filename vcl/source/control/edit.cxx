66980d5618b66d7229d08977f32130d4f58af00d
Tomaž Vajngerl <tomaz.vajngerl@collabora.com>
2015-10-28 20:27:48 +0100

    tdf#94138 also apply settings when printing (Draw call)
    (cherry picked from commit 93a007b1b02c13025b08bcefe19dd54e24e2b80f)
    
    Change-Id: I72056f3e167923f00c37648a24190b533eb9b471

--- vcl/source/control/edit.cxx
+++ vcl/source/control/edit.cxx
@@ -1804,6 +1804,8 @@ void Edit::Resize()
 
 void Edit::Draw( OutputDevice* pDev, const Point& rPos, const Size& rSize, sal_uLong nFlags )
 {
+    ApplySettings(*pDev);
+
     Point aPos = pDev->LogicToPixel( rPos );
     Size aSize = pDev->LogicToPixel( rSize );
     vcl::Font aFont = GetDrawPixelFont( pDev );
