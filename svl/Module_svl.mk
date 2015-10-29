c86c51a142301316f78db65ce8697da8d34fd9eb
Stephan Bergmann <sbergman@redhat.com>
2015-10-29 10:06:32 +0100

    sort
    Change-Id: I074eb6c6ecd651aa63257f7f8424efbd87af899c

--- svl/Module_svl.mk
+++ svl/Module_svl.mk
@@ -30,12 +30,12 @@ $(eval $(call gb_Module_add_l10n_targets,svl,\
 ))
 
 $(eval $(call gb_Module_add_check_targets,svl,\
-	CppunitTest_svl_lngmisc \
-	CppunitTest_svl_qa_cppunit \
 	CppunitTest_svl_inetcontenttype \
-	CppunitTest_svl_items \
 	CppunitTest_svl_itempool \
+	CppunitTest_svl_items \
+	CppunitTest_svl_lngmisc \
 	CppunitTest_svl_notify \
+	CppunitTest_svl_qa_cppunit \
 ))
 
 #FIXME: fails on MSVC
