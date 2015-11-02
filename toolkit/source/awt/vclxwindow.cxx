LibreOffice/core

commit 76e75d2dd6dafe55fd1740693529640652ed6455

tdf#93243 replace boost::bind with c++11 lambdas in toolkit/
Change-Id: I33ba22fc5b3fa4d9b2b79f33b9abb7a2a0bbf9a2
Reviewed-on: https://gerrit.libreoffice.org/19716
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noelgrandin@gmail.com>

toolkit/source/awt/vclxwindow.cxx
@@ -58,9 +58,6 @@
 #include "stylesettings.hxx"
 #include <tools/urlobj.hxx>
 
-#include <boost/bind.hpp>
-#include <boost/noncopyable.hpp>
-
 #include "helper/accessibilityclient.hxx"
 #include "helper/unopropertyarrayhelper.hxx"
 
@@ -86,7 +83,7 @@ namespace WritingMode2 = ::com::sun::star::text::WritingMode2;
 
 //= VCLXWindowImpl
 
-class VCLXWindowImpl: private boost::noncopyable
+class VCLXWindowImpl
 {
 private:
     typedef ::std::vector< VCLXWindow::Callback >                       CallbackArray;
@@ -146,6 +143,9 @@ public:
         live longer then the object just being constructed.
     */
     VCLXWindowImpl( VCLXWindow& _rAntiImpl, bool _bWithDefaultProps );
+
+    VCLXWindowImpl( const VCLXWindowImpl& ) = delete;
+    const VCLXWindowImpl& operator=(const VCLXWindowImpl&) = delete;
 
     /** synchronously mbEnableVisible
     */
@@ -679,11 +679,9 @@ void VCLXWindow::ProcessWindowEvent( const VclWindowEvent& rVclWindowEvent )
                 awt::MouseEvent aEvent( VCLUnoHelper::createMouseEvent( aMEvt, *this ) );
                 aEvent.PopupTrigger = sal_True;
 
-                Callback aCallback = ::boost::bind(
-                    &MouseListenerMultiplexer::mousePressed,
-                    &mpImpl->getMouseListeners(),
-                    aEvent
-                );
+                Callback aCallback = [ this, &aEvent ]()
+                                     { this->mpImpl->getMouseListeners().mousePressed( aEvent ); };
+
                 ImplExecuteAsyncWithoutSolarLock( aCallback );
             }
         }
@@ -695,11 +693,10 @@ void VCLXWindow::ProcessWindowEvent( const VclWindowEvent& rVclWindowEvent )
             {
                 awt::MouseEvent aEvent( VCLUnoHelper::createMouseEvent( *pMouseEvt, *this ) );
 
-                Callback aCallback = ::boost::bind(
-                    pMouseEvt->IsEnterWindow() ? &MouseListenerMultiplexer::mouseEntered : &MouseListenerMultiplexer::mouseExited,
-                    &mpImpl->getMouseListeners(),
-                    aEvent
-                );
+                Callback aCallback = [ this, &pMouseEvt, &aEvent ]()
+                                     { MouseListenerMultiplexer& maMouseListeners = this->mpImpl->getMouseListeners();
+                                       pMouseEvt->IsEnterWindow() ? maMouseListeners.mouseEntered( aEvent ) : maMouseListeners.mouseExited( aEvent ); };
+
                 ImplExecuteAsyncWithoutSolarLock( aCallback );
             }
 
@@ -719,11 +716,8 @@ void VCLXWindow::ProcessWindowEvent( const VclWindowEvent& rVclWindowEvent )
             if ( mpImpl->getMouseListeners().getLength() )
             {
                 awt::MouseEvent aEvent( VCLUnoHelper::createMouseEvent( *static_cast<MouseEvent*>(rVclWindowEvent.GetData()), *this ) );
-                Callback aCallback = ::boost::bind(
-                    &MouseListenerMultiplexer::mousePressed,
-                    &mpImpl->getMouseListeners(),
-                    aEvent
-                );
+                Callback aCallback = [ this, &aEvent ]()
+                                     { this->mpImpl->getMouseListeners().mousePressed( aEvent ); };
                 ImplExecuteAsyncWithoutSolarLock( aCallback );
             }
         }
@@ -733,11 +727,9 @@ void VCLXWindow::ProcessWindowEvent( const VclWindowEvent& rVclWindowEvent )
             if ( mpImpl->getMouseListeners().getLength() )
             {
                 awt::MouseEvent aEvent( VCLUnoHelper::createMouseEvent( *static_cast<MouseEvent*>(rVclWindowEvent.GetData()), *this ) );
-                Callback aCallback = ::boost::bind(
-                    &MouseListenerMultiplexer::mouseReleased,
-                    &mpImpl->getMouseListeners(),
-                    aEvent
-                );
+
+                Callback aCallback = [ this, &aEvent ]()
+                                     { this->mpImpl->getMouseListeners().mouseReleased( aEvent ); };
                 ImplExecuteAsyncWithoutSolarLock( aCallback );
             }
         }

toolkit/source/controls/controlmodelcontainerbase.cxx
@@ -53,8 +53,6 @@
 #include "grid/gridcontrol.hxx"
 #include <toolkit/controls/tabpagecontainer.hxx>
 
-#include <boost/bind.hpp>
-
 #include <map>
 #include <algorithm>
 #include <functional>
@@ -287,7 +285,8 @@ void SAL_CALL ControlModelContainerBase::dispose(  ) throw(RuntimeException, std
     ::std::transform(
         maModels.begin(), maModels.end(),               // source range
         aChildModels.begin(),                           // target location
-        ::boost::bind( &UnoControlModelHolder::first, _1 ) // operation to apply -> select the XControlModel part
+        []( const UnoControlModelHolder& aUnoControlModelHolder )
+        { return aUnoControlModelHolder.first; }        // operation to apply -> select the XControlModel part
     );
 
     // now dispose
@@ -541,7 +540,8 @@ Sequence< OUString > ControlModelContainerBase::getElementNames() throw(RuntimeE
     ::std::transform(
         maModels.begin(), maModels.end(),               // source range
         aNames.getArray(),                              // target range
-        ::boost::bind( &UnoControlModelHolder::second, _1 ) // operator to apply: select the second element (the name)
+        []( const UnoControlModelHolder& aUnoControlModelHolder )
+        { return aUnoControlModelHolder.second; }        // operator to apply: select the second element (the name)
     );
 
     return aNames;
@@ -751,7 +751,8 @@ Sequence< Reference< XControlModel > > SAL_CALL ControlModelContainerBase::getCo
     ::std::transform(
             aSortedModels.begin(), aSortedModels.end(),
             ::std::copy( aUnindexedModels.begin(), aUnindexedModels.end(), aReturn.getArray() ),
-            ::boost::bind( &MapIndexToModel::value_type::second, _1 )
+            [] ( const MapIndexToModel::value_type& entryIndexToModel )
+            { return entryIndexToModel.second; }
         );
 
     return aReturn;

toolkit/source/awt/vclxwindows.cxx
@@ -54,8 +54,6 @@
 #include <vcl/settings.hxx>
 #include <tools/diagnose_ex.h>
 
-#include <boost/bind.hpp>
-
 #include <vcl/group.hxx>
 
 #include "helper/accessibilityclient.hxx"
@@ -587,11 +585,9 @@ void VCLXButton::ProcessWindowEvent( const VclWindowEvent& rVclWindowEvent )
                 aEvent.Source = static_cast<cppu::OWeakObject*>(this);
                 aEvent.ActionCommand = maActionCommand;
 
-                Callback aCallback = ::boost::bind(
-                    &ActionListenerMultiplexer::actionPerformed,
-                    &maActionListeners,
-                    aEvent
-                );
+                Callback aCallback = [ this, &aEvent ]()
+                                     { this->maActionListeners.actionPerformed( aEvent ); };
+
                 ImplExecuteAsyncWithoutSolarLock( aCallback );
             }
         }

toolkit/source/controls/grid/defaultgriddatamodel.cxx
@@ -32,7 +32,6 @@
 #include <algorithm>
 #include <functional>
 #include <vector>
-#include <boost/bind.hpp>
 
 using namespace ::com::sun::star;
 using namespace ::com::sun::star::uno;
@@ -235,7 +234,8 @@ private:
         RowData& rRowData = impl_getRowDataAccess_throw( i_rowIndex, m_nColumnCount );
 
         ::std::transform( rRowData.begin(), rRowData.end(), resultData.getArray(),
-                          boost::bind(&CellData::first,_1));
+                          [] ( const CellData& rCellData )
+                          { return rCellData.first; });
         return resultData;
     }
 

commit 021a5e7123d7e338a21c7bac964e4318894a53e0

toolkit: pMouseEvt must not be captured
Its lifetime is bounded by the currently processed event.

(regression from 76e75d2dd6dafe55fd1740693529640652ed6455)

Change-Id: I4896b161295eeab3bbf08e59c5a82afac4338f08

toolkit/source/awt/vclxwindow.cxx
@@ -692,10 +692,12 @@ void VCLXWindow::ProcessWindowEvent( const VclWindowEvent& rVclWindowEvent )
             if ( mpImpl->getMouseListeners().getLength() && ( pMouseEvt->IsEnterWindow() || pMouseEvt->IsLeaveWindow() ) )
             {
                 awt::MouseEvent aEvent( VCLUnoHelper::createMouseEvent( *pMouseEvt, *this ) );
-
-                Callback aCallback = [ this, pMouseEvt, aEvent ]()
-                                     { MouseListenerMultiplexer& maMouseListeners = this->mpImpl->getMouseListeners();
-                                       pMouseEvt->IsEnterWindow() ? maMouseListeners.mouseEntered( aEvent ) : maMouseListeners.mouseExited( aEvent ); };
+                bool const isEnter(pMouseEvt->IsEnterWindow());
+                Callback aCallback = [ this, isEnter, aEvent ]()
+                     { MouseListenerMultiplexer& rMouseListeners = this->mpImpl->getMouseListeners();
+                       (isEnter)
+                           ? rMouseListeners.mouseEntered(aEvent)
+                           : rMouseListeners.mouseExited(aEvent); };
 
                 ImplExecuteAsyncWithoutSolarLock( aCallback );
             }

toolkit/source/controls/controlmodelcontainerbase.cxx
@@ -285,8 +285,8 @@ void SAL_CALL ControlModelContainerBase::dispose(  ) throw(RuntimeException, std
     ::std::transform(
         maModels.begin(), maModels.end(),               // source range
         aChildModels.begin(),                           // target location
-        []( const UnoControlModelHolder& aUnoControlModelHolder )
-        { return aUnoControlModelHolder.first; }        // operation to apply -> select the XControlModel part
+        []( const UnoControlModelHolder& rUnoControlModelHolder )
+        { return rUnoControlModelHolder.first; }        // operation to apply -> select the XControlModel part
     );
 
     // now dispose
@@ -540,8 +540,8 @@ Sequence< OUString > ControlModelContainerBase::getElementNames() throw(RuntimeE
     ::std::transform(
         maModels.begin(), maModels.end(),               // source range
         aNames.getArray(),                              // target range
-        []( const UnoControlModelHolder& aUnoControlModelHolder )
-        { return aUnoControlModelHolder.second; }        // operator to apply: select the second element (the name)
+        []( const UnoControlModelHolder& rUnoControlModelHolder )
+        { return rUnoControlModelHolder.second; }        // operator to apply: select the second element (the name)
     );
 
     return aNames;