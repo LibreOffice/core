30d04756881ec60f80173387a836ae74874f7b54
Miklos Vajna <vmiklos@collabora.co.uk>
2015-11-03 14:34:58 +0100

    ScGlobal::UpdatePPT: make conversion more precise
    Old situation was (96 DPI, 100% zoom): 0.06669 factor, 1088 pixels,
    result is 16311 twips (1087.4 pixels).
    
    New situation is: 0.066669 factor, 1088 pixels, result is 16319 twips
    (1087.93 pixels).
    
    Change-Id: I0ff11520fd719aefd2b351a6d4ef949d66b66282
    (cherry picked from commit cce7d78baa91ab348e85407ada8387c9c89176cb)

--- sc/source/core/data/global.cxx
+++ sc/source/core/data/global.cxx
@@ -544,9 +544,9 @@ void ScGlobal::UpdatePPT( OutputDevice* pDev )
 
         if ( !pDev )
             pDev = Application::GetDefaultDevice();
-        Point aPix1000 = pDev->LogicToPixel( Point(10000,10000), MAP_TWIP );
-        nScreenPPTX = aPix1000.X() / 10000.0;
-        nScreenPPTY = aPix1000.Y() / 10000.0;
+        Point aPix1000 = pDev->LogicToPixel( Point(100000,100000), MAP_TWIP );
+        nScreenPPTX = aPix1000.X() / 100000.0;
+        nScreenPPTY = aPix1000.Y() / 100000.0;
         nPPTZoom = nCurrentZoom;
     }
 }
