ee9816c32c320cc043c884a51070b48aaebfc1e4
Daniel Robertson <danlrobertson89@gmail.com>
2015-11-03 09:47:36 -0500

    rtl::Reference Add move construction/assignment
    Add move constructor and appropriately overloaded assignment operator to
    rtl::Reference, and add basic unit tests for the reference counting of
    rtl::Reference.
    
    Change-Id: Ia7ff5d786bdf3b17709cec06608c91e22379746c

---
+++ sal/CppunitTest_sal_rtl_ref.mk
@@ -0,0 +1,27 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_CppunitTest_CppunitTest,sal_rtl_ref))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,sal_rtl_ref,\
+    sal/qa/rtl/ref/rtl_ref \
+))
+
+$(eval $(call gb_CppunitTest_use_libraries,sal_rtl_ref,\
+    sal \
+	$(gb_UWINAPI) \
+))
+
+$(eval $(call gb_CppunitTest_set_include,sal_rtl_ref,\
+		    -I$(SRCDIR)/sal/qa/inc \
+			    $$(INCLUDE) \
+				))
+
+# vim: set noet sw=4 ts=4:
