c6822f927a73e5b2ee72208b139b6808116fe1e0
Stephan Bergmann <sbergman@redhat.com>
2015-11-03 14:48:35 +0100

    Use newly introduced gb_DEBUGINFO_FLAGS instead of hardcoded -g
    Change-Id: Iaaa80d997fa7babb9212787653c149b72d842a6c

--- external/icu/ExternalProject_icu.mk
+++ external/icu/ExternalProject_icu.mk
@@ -55,14 +55,14 @@ icu_CFLAGS:=" \
 	$(if $(debug),$(gb_DEBUG_CFLAGS),$(gb_COMPILEROPTFLAGS)) \
 	$(if $(ENABLE_LTO),$(gb_LTOFLAGS)) \
 	$(if $(filter GCC,$(COM)),-fno-strict-aliasing) \
-	$(if $(filter $(true),$(gb_SYMBOL)),-g) \
+	$(if $(filter $(true),$(gb_SYMBOL)),$(gb_DEBUGINFO_FLAGS)) \
 	$(if $(filter ANDROID,$(OS)),-fvisibility=hidden -fno-omit-frame-pointer)"
 icu_CXXFLAGS:="$(CXXFLAGS) $(CXXFLAGS_CXX11) \
 	$(if $(filter IOS,$(OS)),-DUCONFIG_NO_FILE_IO) \
 	$(if $(debug),$(gb_DEBUG_CFLAGS),$(gb_COMPILEROPTFLAGS)) \
 	$(if $(ENABLE_LTO),$(gb_LTOFLAGS)) \
 	$(if $(filter GCC,$(COM)),-fno-strict-aliasing) \
-	$(if $(filter $(true),$(gb_SYMBOL)),-g) \
+	$(if $(filter $(true),$(gb_SYMBOL)),$(gb_DEBUGINFO_FLAGS)) \
 	$(if $(filter ANDROID,$(OS)),-fvisibility=hidden -fno-omit-frame-pointer)"
 icu_LDFLAGS:=" \
 	$(if $(ENABLE_LTO),$(gb_LTOFLAGS)) \
