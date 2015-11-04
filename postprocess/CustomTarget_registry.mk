4731dbce7f0c9a089baecf960ede38f3ce824849
Andras Timar <andras.timar@collabora.com>
2015-11-04 15:15:55 +0100

    STARTCENTER_INFO_URL = https://collaboraoffice.com/
    (cherry picked from commit 666337ce629737a73791e6fa4f0a7ac98541dc1f)
    (cherry picked from commit a156cd0b9d712485a2624edcda1584361c8913c0)
    
    Change-Id: Id84259ff8bf27475d63c0ab31c4312bc78cb55d8

--- postprocess/CustomTarget_registry.mk
+++ postprocess/CustomTarget_registry.mk
@@ -559,7 +559,7 @@ postprocess_main_SED := \
 	-e 's,$${PRODUCTVERSION},$(PRODUCTVERSION),g' \
 	-e 's,$${PRODUCTEXTENSION},.$(LIBO_VERSION_MICRO).$(LIBO_VERSION_PATCH)$(LIBO_VERSION_SUFFIX),g' \
 	-e 's,$${STARTCENTER_ADDFEATURE_URL},http://extensions.libreoffice.org/,g' \
-	-e 's,$${STARTCENTER_INFO_URL},https://www.libreoffice.org/,g' \
+	-e 's,$${STARTCENTER_INFO_URL},https://collaboraoffice.com/,g' \
 	-e 's,$${STARTCENTER_TEMPLREP_URL},http://templates.libreoffice.org/,g' \
 	-e 's,$${SYSTEM_LIBEXTTEXTCAT_DATA},$(SYSTEM_LIBEXTTEXTCAT_DATA),g' \
 	-e 's,$${SYSTEM_LIBNUMBERTEXT_DATA},$(SYSTEM_LIBNUMBERTEXT_DATA),g' \
