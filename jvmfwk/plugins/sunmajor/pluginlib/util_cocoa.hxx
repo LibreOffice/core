55d64bd47238ff1e390a09cc3466fd76c408aa50
Stephan Bergmann <sbergman@redhat.com>
2015-11-04 14:50:52 +0100

    Avoid using reserved identifiers, add missing includes
    Change-Id: I23b548de1f07a4ec5acfeef7c553030a1f098311

--- jvmfwk/plugins/sunmajor/pluginlib/util_cocoa.hxx
+++ jvmfwk/plugins/sunmajor/pluginlib/util_cocoa.hxx
@@ -1,7 +1,11 @@
 /* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 
-#ifndef __UTIL_COCOA_H__
-#define __UTIL_COCOA_H__
+#ifndef INCLUDED_JVMFWK_PLUGINS_SUNMAJOR_PLUGINLIB_UTIL_COCOA_HXX
+#define INCLUDED_JVMFWK_PLUGINS_SUNMAJOR_PLUGINLIB_UTIL_COCOA_HXX
+
+#include <sal/config.h>
+
+#include <rtl/ustring.hxx>
 
 bool JvmfwkUtil_isLoadableJVM( OUString const & aURL );
 
