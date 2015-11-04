fa6504066e59c779d201e6ea3176952984bb48b5
Stephan Bergmann <sbergman@redhat.com>
2015-11-04 14:49:11 +0100

    Add mode lines
    Change-Id: I4026040cfa2cd34c3a33a0a3dd4651caeb11c76f

--- jvmfwk/plugins/sunmajor/pluginlib/util_cocoa.hxx
+++ jvmfwk/plugins/sunmajor/pluginlib/util_cocoa.hxx
@@ -1,6 +1,10 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
 #ifndef __UTIL_COCOA_H__
 #define __UTIL_COCOA_H__
 
 bool JvmfwkUtil_isLoadableJVM( OUString const & aURL );
 
 #endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
--- jvmfwk/plugins/sunmajor/pluginlib/util_cocoa.mm
+++ jvmfwk/plugins/sunmajor/pluginlib/util_cocoa.mm
@@ -1,3 +1,5 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
 #include <rtl/ustring.hxx>
 
 #include <premac.h>
@@ -127,3 +129,5 @@ bool JvmfwkUtil_isLoadableJVM( OUString const & aURL )
 
     return bRet;
 }
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
