fb0a51d4f6d35f626a92abc72bc79a5783928e1a
Michael Stahl <mstahl@redhat.com>
2015-11-05 13:16:50 +0100

    compilerplugins: enhance "badstatics" plugin to find members
    Finds static variables of types that have bad non-static members.
    
    Change-Id: I81ee940580c0f043feb543840ea4d3dd27734202

--- compilerplugins/clang/badstatics.cxx
+++ compilerplugins/clang/badstatics.cxx
@@ -25,23 +25,80 @@ public:
         }
     }
 
+    /*static*/ std::pair<bool, FieldDecl const*> isBadStaticType(
+            QualType const& rType, FieldDecl const*const pCurrentFieldDecl)
+    {
+        QualType const pCanonical(rType.getUnqualifiedType().getCanonicalType());
+        RecordType const*const pRecordType(pCanonical->getAs<RecordType>());
+        if (!pRecordType) {
+            return std::make_pair(false, nullptr);
+        }
+        auto const type(pCanonical.getAsString());
+        if (   type == "class Image"
+            || type == "class Bitmap"
+            || type == "class BitmapEx"
+           )
+        {
+            return std::make_pair(true, pCurrentFieldDecl);
+        }
+        RecordDecl const*const pDefinition(pRecordType->getDecl()->getDefinition());
+#if (__clang_major__ == 3 && __clang_minor__ > 5) || __clang_major__ > 3
+        // TODO not sure if it works with clang 3.6, trunk is known to work
+        assert(pDefinition);
+#else
+        if (!pDefinition) {
+            // no definition if its a template, e.g. "class std::weak_ptr<class SvtUserOptions::Impl>"
+            report(DiagnosticsEngine::Remark,
+                    "old clang version unable to find definition of this class",
+                    pRecordType->getDecl()->getLocation())
+                << pRecordType->getDecl()->getSourceRange();
+            return std::make_pair(false, nullptr);
+        }
+#endif
+        CXXRecordDecl const*const pDecl(dyn_cast<CXXRecordDecl>(pDefinition));
+        assert(pDecl);
+        for (auto it = pDecl->field_begin(); it != pDecl->field_end(); ++it) {
+            auto const ret(isBadStaticType((*it)->getType(), *it));
+            if (ret.first) {
+                return ret;
+            }
+        }
+        for (auto it = pDecl->bases_begin(); it != pDecl->bases_end(); ++it) {
+            auto const ret(isBadStaticType((*it).getType(), pCurrentFieldDecl));
+            if (ret.first) {
+                return ret;
+            }
+        }
+        for (auto it = pDecl->vbases_begin(); it != pDecl->vbases_end(); ++it) {
+            auto const ret(isBadStaticType((*it).getType(), pCurrentFieldDecl));
+            if (ret.first) {
+                return ret;
+            }
+        }
+        return std::make_pair(false, nullptr);
+    }
+
     bool VisitVarDecl(VarDecl const*const pVarDecl)
     {
         if (ignoreLocation(pVarDecl)) {
             return true;
         }
 
-        if (pVarDecl->hasGlobalStorage()) {
-            auto const type(pVarDecl->getType().getUnqualifiedType().getCanonicalType().getAsString());
-            if (   type == "class Image"
-                || type == "class Bitmap"
-                || type == "class BitmapEx"
-               )
-            {
+        if (pVarDecl->hasGlobalStorage()
+            && pVarDecl->isThisDeclarationADefinition())
+        {
+            auto const ret(isBadStaticType(pVarDecl->getType(), nullptr));
+            if (ret.first) {
                 report(DiagnosticsEngine::Warning,
                         "bad static variable causes crash on shutdown",
                         pVarDecl->getLocation())
                     << pVarDecl->getSourceRange();
+                if (ret.second != nullptr) {
+                    report(DiagnosticsEngine::Remark,
+                            "... due to this member",
+                            ret.second->getLocation())
+                        << ret.second->getSourceRange();
+                }
             }
         }
 
