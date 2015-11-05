488dec370ac30c2712853c7d625209f9e5c125cb
Michael Stahl <mstahl@redhat.com>
2015-11-05 13:16:50 +0100

    compilerplugins: enhance "badstatics" plugin to find members
    Finds static variables of types that have bad non-static members.
    
    Change-Id: I81ee940580c0f043feb543840ea4d3dd27734202

--- compilerplugins/clang/badstatics.cxx
+++ compilerplugins/clang/badstatics.cxx
@@ -9,6 +9,8 @@
 
 #include "plugin.hxx"
 
+// FIXME debug
+#include <iostream>
 namespace {
 
 class BadStatics
@@ -25,6 +27,62 @@ public:
         }
     }
 
+    static std::pair<bool, FieldDecl const*> isBadStaticType(
+            QualType const& rType, FieldDecl const*const pCurrentFieldDecl)
+    {
+        QualType const pCanonical(rType.getUnqualifiedType().getCanonicalType());
+        RecordType const*const pRecordType(pCanonical->getAs<RecordType>());
+        if (!pRecordType) {
+            return std::make_pair(false, nullptr);
+        }
+        auto const type(pCanonical.getAsString());
+        std::cerr << "XXX type: " << type << std::endl;
+        if (   type == "class Image"
+            || type == "class Bitmap"
+            || type == "class BitmapEx"
+           )
+        {
+            return std::make_pair(true, pCurrentFieldDecl);
+        }
+        // FIXME no definition if its a "class std::weak_ptr<class SvtUserOptions::Impl>"
+//        RecordDecl const*const pRDecl(pRecordType->getDecl()->getDefinition());
+//fail        assert(pRDecl);
+        RecordDecl const*const pCDecl(dyn_cast<RecordDecl>(pRecordType->getDecl()->getCanonicalDecl()));
+//        assert(pCDecl);
+        CXXRecordDecl const*const pDecl1(dyn_cast<CXXRecordDecl>(pCDecl));
+        ClassTemplateDecl const* pTemplateDecl(pDecl1->getDescribedClassTemplate());
+        CXXRecordDecl const*pDecl;
+        if (pTemplateDecl)
+            pDecl = pTemplateDecl->getTemplatedDecl()->getDefinition();
+        else
+            pDecl = pDecl1;
+
+        std::cerr << "XXX " << __LINE__ << std::endl;
+        assert(pDecl);
+        for (auto it = pDecl->field_begin(); it != pDecl->field_end(); ++it) {
+            std::cerr << "XXX " << __LINE__ << std::endl;
+            auto const ret(isBadStaticType((*it)->getType(), *it));
+            if (ret.first) {
+                return ret;
+            }
+        }
+        for (auto it = pDecl->bases_begin(); it != pDecl->bases_end(); ++it) {
+            std::cerr << "XXX " << __LINE__ << std::endl;
+            auto const ret(isBadStaticType((*it).getType(), pCurrentFieldDecl));
+            if (ret.first) {
+                return ret;
+            }
+        }
+        for (auto it = pDecl->vbases_begin(); it != pDecl->vbases_end(); ++it) {
+            std::cerr << "XXX " << __LINE__ << std::endl;
+            auto const ret(isBadStaticType((*it).getType(), pCurrentFieldDecl));
+            if (ret.first) {
+                return ret;
+            }
+        }
+        return std::make_pair(false, nullptr);
+    }
+
     bool VisitVarDecl(VarDecl const*const pVarDecl)
     {
         if (ignoreLocation(pVarDecl)) {
@@ -32,16 +90,18 @@ public:
         }
 
         if (pVarDecl->hasGlobalStorage()) {
-            auto const type(pVarDecl->getType().getUnqualifiedType().getCanonicalType().getAsString());
-            if (   type == "class Image"
-                || type == "class Bitmap"
-                || type == "class BitmapEx"
-               )
-            {
+            auto const ret(isBadStaticType(pVarDecl->getType(), nullptr));
+            if (ret.first) {
                 report(DiagnosticsEngine::Warning,
                         "bad static variable causes crash on shutdown",
                         pVarDecl->getLocation())
                     << pVarDecl->getSourceRange();
+                if (ret.second != nullptr) {
+                    report(DiagnosticsEngine::Warning,
+                            "... due to this member",
+                            ret.second->getLocation())
+                        << ret.second->getSourceRange();
+                }
             }
         }
 
