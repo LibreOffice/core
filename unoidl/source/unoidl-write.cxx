df35df21ce4868f551dac1f01a91db80db75137f
Caolán McNamara <caolanm@redhat.com>
2015-11-06 09:46:39 +0000

    coverity#1338259 Uncaught exception
    Change-Id: I36e2194c304adae5b5c3377519eccc5ef78fd9ea

--- unoidl/source/unoidl-write.cxx
+++ unoidl/source/unoidl-write.cxx
@@ -1104,7 +1104,13 @@ SAL_IMPLEMENT_MAIN() {
             << "Bad input <" << e1.getUri() << ">: " << e1.getDetail()
             << std::endl;
         std::exit(EXIT_FAILURE);
+    } catch (std::exception & e1) {
+        std::cerr
+            << "Failure: " << e1.what()
+            << std::endl;
+        std::exit(EXIT_FAILURE);
     }
+
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
