c261fa908ee586d306f9b2f27af80ddf2f0e77af
jan iversen <jani@documentfoundation.org>
2015-11-06 09:15:51 +0100

    cid#1326599, cid#1326598: added throw (status generation)
    Status.failed( ... ); is prepended with a throw
    added import
    
    Change-Id: I73bc971e054c8726e45db0d5a37613c871691eab

--- qadevOOo/tests/java/ifc/drawing/_XDrawPageSummarizer.java
+++ qadevOOo/tests/java/ifc/drawing/_XDrawPageSummarizer.java
@@ -19,6 +19,7 @@
 package ifc.drawing;
 
 import lib.MultiMethodTest;
+import lib.StatusException;
 import lib.Status;
 
 import com.sun.star.drawing.XDrawPageSummarizer;
@@ -50,7 +51,7 @@ public class _XDrawPageSummarizer extends MultiMethodTest {
         XDrawPages oDrawPages = (XDrawPages) tEnv.getObjRelation("DrawPages");
         if ( oDrawPages == null ) {
             // SKIPPED.FAILED - the ObjectRelation is not available
-            Status.failed( "ObjectRelation('DrawPages') XDrawPages n.a." );
+            throw new StatusException(Status.failed( "ObjectRelation('DrawPages') XDrawPages n.a." ));
             return;
         }
         log.println("testing summarize() ... ");
