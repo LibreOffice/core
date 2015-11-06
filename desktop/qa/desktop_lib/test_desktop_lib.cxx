23155d78adebf5394a55f2325448d5ad9a3eed0d
Andrzej Hunt <andrzej@ahunt.org>
2015-11-06 18:09:34 +0100

    sc lok: Add initial test for .uno:CellCursor
    This should be extended with checking that we receive "EMPTY"
    when there is no cursor shown - that would require e.g. simulating
    keyboard input to hide the cell cursor.
    
    Change-Id: Ia7be5ec3e158f21967b4c307ac10abb2b5e2a56a

--- desktop/qa/desktop_lib/test_desktop_lib.cxx
+++ desktop/qa/desktop_lib/test_desktop_lib.cxx
@@ -68,6 +68,7 @@ public:
     void testSaveAsCalc();
     void testPasteWriter();
     void testRowColumnHeaders();
+    void testCellCursor();
     void testCommandResult();
 
     CPPUNIT_TEST_SUITE(DesktopLOKTest);
@@ -82,6 +83,7 @@ public:
     CPPUNIT_TEST(testSaveAsCalc);
     CPPUNIT_TEST(testPasteWriter);
     CPPUNIT_TEST(testRowColumnHeaders);
+    CPPUNIT_TEST(testCellCursor);
     CPPUNIT_TEST(testCommandResult);
     CPPUNIT_TEST_SUITE_END();
 
@@ -433,6 +435,24 @@ void DesktopLOKTest::testRowColumnHeaders()
         }
         nPrevious = nSize;
     }
+}
+
+void DesktopLOKTest::testCellCursor()
+{
+    LibLODocument_Impl* pDocument = loadDoc("search.ods");
+
+    boost::property_tree::ptree aTree;
+
+    char* pJSON = pDocument->m_pDocumentClass->getCommandValues(pDocument, ".uno:CellCursor?tileWidth=1&tileHeight=1&outputWidth=1&outputHeight=1");
+
+    std::stringstream aStream(pJSON);
+    free(pJSON);
+    CPPUNIT_ASSERT(!aStream.str().empty());
+
+    boost::property_tree::read_json(aStream, aTree);
+
+    OString aRectangle(aTree.get<std::string>("commandValues").c_str());
+    CPPUNIT_ASSERT_EQUAL(aRectangle, OString("0, 0, 1278, 254"));
 }
 
 void DesktopLOKTest::testCommandResult()
