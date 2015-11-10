60679f4740602cbd487ec36ad8be59429acc47b3
Stephan Bergmann <sbergman@redhat.com>
2015-11-10 14:08:26 +0100

    loplugin:nullptr
    Change-Id: I7e266a0e29d86247d01df38da00899a8a1700719

--- sc/qa/unit/bugfix-test.cxx
+++ sc/qa/unit/bugfix-test.cxx
@@ -192,11 +192,11 @@ void ScFiltersTest::testTdf91979()
     CPPUNIT_ASSERT_MESSAGE("Failed to access document shell", pFoundShell);
 
     ScDocShellRef xDocSh = dynamic_cast<ScDocShell*>(pFoundShell);
-    CPPUNIT_ASSERT(xDocSh != NULL);
+    CPPUNIT_ASSERT(xDocSh != nullptr);
 
     // Get the document controller
     ScTabViewShell* pViewShell = xDocSh->GetBestViewShell(false);
-    CPPUNIT_ASSERT(pViewShell != NULL);
+    CPPUNIT_ASSERT(pViewShell != nullptr);
     auto& aViewData = pViewShell->GetViewData();
     auto* pDoc = aViewData.GetDocument();
 
