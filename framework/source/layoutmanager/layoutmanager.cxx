53f66fa6793d96f410acc7a256beb3284d65f296
Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
2015-11-11 08:49:53 +0100

    Fix NotebookBar not showing up when directly opening a document
    Change-Id: I6bb154102d5e58e7c7e1f1b0d68629555a6d1697

--- framework/source/layoutmanager/layoutmanager.cxx
+++ framework/source/layoutmanager/layoutmanager.cxx
@@ -784,11 +784,6 @@ void LayoutManager::implts_updateUIElementsVisibleState( bool bSetVisible )
             if ( bSetVisible )
             {
                 pSysWindow->SetMenuBar(pMenuBar);
-                if (getenv("LO_USE_NOTEBOOKBAR"))
-                {
-                    pSysWindow->CreateNotebookBar("vcl/ui/notebookbar.ui", m_xFrame);
-                    pSysWindow->SetMenuBarMode(MenuBarMode::Hide);
-                }
             }
             else
                 pSysWindow->SetMenuBar( nullptr );
@@ -1466,7 +1461,7 @@ throw (RuntimeException, std::exception)
         {
             // #i38743# don't create a menubar if frame isn't top
             if ( !bInPlaceMenu && !m_xMenuBar.is() && implts_isFrameOrWindowTop( xFrame ))
-                {
+            {
                 m_xMenuBar = implts_createElement( aName );
                 if ( m_xMenuBar.is() )
                 {
@@ -1507,6 +1502,11 @@ throw (RuntimeException, std::exception)
                                     implts_updateMenuBarClose();
                                 }
                             }
+                        }
+                        if (getenv("LO_USE_NOTEBOOKBAR"))
+                        {
+                            pSysWindow->CreateNotebookBar("vcl/ui/notebookbar.ui", m_xFrame);
+                            pSysWindow->SetMenuBarMode(MenuBarMode::Hide);
                         }
                     }
                 }
