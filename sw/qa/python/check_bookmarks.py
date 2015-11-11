8ec28522c29eada0d61116a5bf70a7c84403d887
kadertarlan <kadertarlan1@gmail.com>
2015-11-11 03:01:22 +0200

    Fixed the written test that ''check_bookmarks.py
    Change-Id: Idd47543fe16a8cb019c618e9c09c58d95fbaa6d2

--- sw/qa/python/check_bookmarks.py
+++ sw/qa/python/check_bookmarks.py
@@ -1,3 +1,21 @@
+'''
+  This file is part of the LibreOffice project.
+ 
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ 
+  This file incorporates work covered by the following license notice:
+ 
+    Licensed to the Apache Software Foundation (ASF) under one or more
+    contributor license agreements. See the NOTICE file distributed
+    with this work for additional information regarding copyright
+    ownership. The ASF licenses this file to you under the Apache
+    License, Version 2.0 (the "License"); you may not use this file
+    except in compliance with the License. You may obtain a copy of
+    the License at http://www.apache.org/licenses/LICENSE-2.0 .
+'''
+
 import unittest
 import unohelper
 import os
@@ -5,22 +23,20 @@ from org.libreoffice.unotest import UnoInProcess
 import hashlib
 from com.sun.star.text.ControlCharacter import PARAGRAPH_BREAK
 from random import randint
-import random 
-import sys 
+import random
+import sys
 from com.sun.star.beans import PropertyValue
 from com.sun.star.container import XNamed
 from com.sun.star.container import XNameAccess
 from com.sun.star.frame import XStorable
 from com.sun.star.lang import XMultiServiceFactory
 from com.sun.star.text import XBookmarksSupplier
-from com.sun.star.text  import XSimpleText
+from com.sun.star.text import XSimpleText
 from com.sun.star.text import XText
 from com.sun.star.text import XTextContent
 from com.sun.star.text import XTextCursor
-from com.sun.star.text import XTextDocument 
-from com.sun.star.text import XTextRange 
-#from com.sun.star.uno import UnoRuntime 
-#from  org.openoffice.test import OfficeConnection
+from com.sun.star.text import XTextDocument
+from com.sun.star.text import XTextRange
 
 class BookmarkHashes(unittest.TestCase):
 
@@ -68,10 +84,9 @@ class BookmarkHashes(unittest.TestCase):
     def getBookmarksHash(xDoc):
         buffer = StringBuffer("")
         xBookmarksSupplier = UnoRuntime.queryInterface(type(XBookmarksSupplier).__name__, xDoc)
-        
         xBookmarks = xBookmarksSupplier.getBookmarks()
  
-        for sBookmarkname in xBooksmars.getElementNames():
+        for sBookmarkname in xBookmarks.getElementNames():
             xBookmark = xBookmarks.getByName(sBookmarkname)
             xBookmarkAsContent = UnoRuntime.queryInterface(type(XTextContent).__name__, xBookmark)
             buffer.append(sBookmarkname)
@@ -83,7 +98,7 @@ class BookmarkHashes(unittest.TestCase):
         s.update(buffer) 
         return s.digest()
 
-class CheckBookmarks():
+class CheckBookmarks(unittest.TestCase):
     m_xMsf = None
     m_xDoc = None
     m_xOdfReloadedDoc = None
