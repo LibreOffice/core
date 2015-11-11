0f4829ac7d614c0d9be2ffc5648c6cf88cb9d53e
Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
2015-11-11 12:16:30 +0100

    Vcl: Set PushButtons pressed if the uno slot is active
    Change-Id: I89708d13da2b3882aa0b7578b5acf48b6408010b

--- vcl/source/control/button.cxx
+++ vcl/source/control/button.cxx
@@ -619,6 +619,11 @@ bool Button::set_property(const OString &rKey, const OString &rValue)
     return true;
 }
 
+void Button::SetStateUno(const css::frame::FeatureStateEvent& rEvent)
+{
+    Enable(rEvent.IsEnabled);
+}
+
 IMPL_STATIC_LINK_TYPED( Button, dispatchCommandHandler, Button*, pButton, void )
 {
     if (pButton == nullptr)
@@ -1601,6 +1606,13 @@ void PushButton::SetState( TriState eState )
         CompatStateChanged( StateChangedType::State );
         Toggle();
     }
+}
+
+void PushButton::SetStateUno(const css::frame::FeatureStateEvent& rEvent)
+{
+    Button::SetStateUno(rEvent);
+    if (rEvent.State.has<bool>())
+        SetPressed(rEvent.State.get<bool>());
 }
 
 void PushButton::SetPressed( bool bPressed )
