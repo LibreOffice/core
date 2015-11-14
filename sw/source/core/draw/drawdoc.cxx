dacc616f7020d045a1cfdb3806436e06046dae10
Miklos Vajna <vmiklos@collabora.co.uk>
2015-11-14 11:06:08 +0100

    sw lok comments: fix callback of newly created outliner views
    SwPostItMgr::registerLibreOfficeKitCallback() already took care of
    informing existing outliners, this commit gives a callback to newly
    registered ones as well.
    
    Change-Id: I660dcb54231a9d404bf80b4284003d119dae6a5c

--- sw/source/core/draw/drawdoc.cxx
+++ sw/source/core/draw/drawdoc.cxx
@@ -129,6 +129,12 @@ uno::Reference<embed::XStorage> SwDrawModel::GetDocumentStorage() const
     return m_pDoc->GetDocStorage();
 }
 
+void SwDrawModel::getLibreOfficeKitCallback(LibreOfficeKitCallback& rCallback, void*& rLibreOfficeKitData)
+{
+    rCallback = mpLibreOfficeKitCallback;
+    rLibreOfficeKitData = mpLibreOfficeKitData;
+}
+
 uno::Reference< uno::XInterface > SwDrawModel::createUnoModel()
 {
     uno::Reference< uno::XInterface > xModel;
