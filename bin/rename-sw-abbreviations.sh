3190f5a71dd3e63260edebc345aead4746a269a3
Jan Holesovsky <kendy@collabora.com>
2015-11-16 12:42:04 +0100

    rename-sw-abbreviations.sh: Add Crsr -> Cursor, Frm -> Frame and Stk -> Stack.
    The build works fine with these now, and produces no (new) warning.
    
    Change-Id: I3e5aad7baaec172ee4d2b50ea66876bdb7e9cac5

--- bin/rename-sw-abbreviations.sh
+++ bin/rename-sw-abbreviations.sh
@@ -12,7 +12,7 @@
 
 # sw only:
 
-for I in "FrmFmt/FrameFormat" "Fmt/Format" "Cntnt/Content" "Txt/Text" "Tbl/Table" "GotoFld/GotoFormatField" "Fld/Field" "Ftn/Footnote" "Updt/Update" "Fml/Formula" "Hnt/Hint"
+for I in "FrmFmt/FrameFormat" "Fmt/Format" "Cntnt/Content" "Txt/Text" "Tbl/Table" "GotoFld/GotoFormatField" "Fld/Field" "Ftn/Footnote" "Updt/Update" "Fml/Formula" "Hnt/Hint" "CurCrsr/CurrentCursor" "VisCrsr/VisibleCursor" "Crsr/Cursor" "CntFrm/ContentFrame" "Frm/Frame" "Stk/Stack"
 do
     S="${I%/*}"
     # change all except the filenames (in the .mk and in #include)
