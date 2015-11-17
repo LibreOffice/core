4b6481f0e5ff5d059a6b50ff8812ceeaab512ee9
Markus Mohrhard <markus.mohrhard@googlemail.com>
2015-11-17 07:09:20 +0100

    package minidump_upload tool
    We are going to use this tool for uploading the minidumps for now.
    
    Change-Id: I2c59d420e3884aee46411df913c2ec067e0c636b
    Reviewed-on: https://gerrit.libreoffice.org/22549
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Markus Mohrhard <markus.mohrhard@googlemail.com>

--- RepositoryExternal.mk
+++ RepositoryExternal.mk
@@ -3426,6 +3426,11 @@ endif
 
 $(call gb_LinkTarget_use_external_project,$(1),breakpad)
 
+$(eval $(call gb_Helper_register_packages_for_install,ooo,\
+	breakpad \
+))
+
+
 endef
 
 # else # SYSTEM_LIBBREAKPAD
