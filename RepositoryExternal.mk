738d0d21ab46f49524eb043c9cb7ae7dd9f9619e
Markus Mohrhard <markus.mohrhard@googlemail.com>
2015-11-17 07:09:20 +0100

    package minidump_upload tool
    We are going to use this tool for uploading the minidumps for now.
    
    Change-Id: I2c59d420e3884aee46411df913c2ec067e0c636b

--- RepositoryExternal.mk
+++ RepositoryExternal.mk
@@ -3368,6 +3368,11 @@ endif
 
 $(call gb_LinkTarget_use_external_project,$(1),breakpad)
 
+$(eval $(call gb_Helper_register_packages_for_install,ooo,\
+	breakpad \
+))
+
+
 endef
 
 # else # SYSTEM_LIBBREAKPAD
