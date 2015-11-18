4d518e6bf6a3bc99316c7cf9966131d21cfd54b3
Armin Le Grand <Armin.Le.Grand@cib.de>
2015-11-18 15:37:26 +0100

    Resolves: tdf#93994 flush API objects at load time
    When importing SdrObjCustomShape for each an Outliner and a
    VirtualDevice as RefDevice are created and held. On 32bit systems this
    breaks the import with many such objects (in the bugdoc around 4300). On
    64bit it works, but more memory as necessary is used. To avoid this,
    flush the UNO API implementations at these objects after one page is
    imported.  Do not do this for each single object, that will break the
    connector import which does some processing later.
    
    Change-Id: I7296edd40f01c1a40258808d78bf39e6d4fe29bf

--- xmloff/source/draw/ximpbody.cxx
+++ xmloff/source/draw/ximpbody.cxx
@@ -37,6 +37,7 @@
 #include "PropertySetMerger.hxx"
 #include "animationimport.hxx"
 #include <tools/debug.hxx>
+#include <osl/diagnose.hxx>
 
 using namespace ::com::sun::star;
 
@@ -276,6 +277,29 @@ void SdXMLDrawPageContext::EndElement()
         uno::Reference< beans::XPropertySet > xPageProps( GetLocalShapesContext(), uno::UNO_QUERY );
         if(xNodeSupplier.is())
             xmloff::AnimationNodeContext::postProcessRootNode( GetSdImport(), xNodeSupplier->getAnimationNode(), xPageProps );
+    }
+
+    // tdf#93994 call a custom slot to be able to reset the UNO API
+    // implementations held on the SdrObjects of type
+    // SdrObjCustomShape - those tend to linger until the entire file
+    // is loaded. For large files with a lot of these 32bit systems
+    // may crash due to being out of ressources after ca. 4200
+    // Outliners and VirtualDevices used there as RefDevice
+    try
+    {
+        uno::Reference< beans::XPropertySet > xPropSet(GetLocalShapesContext(), uno::UNO_QUERY);
+
+        if(xPropSet.is())
+        {
+            const OUString sFlushCustomShapeUnoApiObjects("FlushCustomShapeUnoApiObjects");
+            uno::Any aAny;
+            aAny <<= sal_True;
+            xPropSet->setPropertyValue(sFlushCustomShapeUnoApiObjects, aAny);
+        }
+    }
+    catch(const uno::Exception&)
+    {
+        OSL_FAIL("could not flush after load");
     }
 }
 
