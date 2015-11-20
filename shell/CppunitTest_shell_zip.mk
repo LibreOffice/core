e5c55a6f99052d71505ca97d7a1ef3822960cbba
Michael Stahl <mstahl@redhat.com>
2015-11-20 10:59:34 +0100

    shell: rename CppunitTest to the same name as the makefile
    Change-Id: I3d1ef0573a460a3101cb8c9afd30162397d8012c

--- shell/CppunitTest_shell_zip.mk
+++ shell/CppunitTest_shell_zip.mk
@@ -7,31 +7,31 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 
-$(eval $(call gb_CppunitTest_CppunitTest,zip))
+$(eval $(call gb_CppunitTest_CppunitTest,shell_zip))
 
-$(eval $(call gb_CppunitTest_add_exception_objects,zip, \
+$(eval $(call gb_CppunitTest_add_exception_objects,shell_zip, \
     shell/qa/zip/testzipimpl \
     shell/qa/zip/ziptest \
 ))
 
 
-$(eval $(call gb_CppunitTest_set_include,zip,\
+$(eval $(call gb_CppunitTest_set_include,shell_zip,\
 	-I$(SRCDIR)/shell/inc \
 	$$(INCLUDE) \
 ))
 
-$(eval $(call gb_CppunitTest_use_externals,zip,\
+$(eval $(call gb_CppunitTest_use_externals,shell_zip,\
 	expat \
 	zlib \
 ))
 
-$(eval $(call gb_CppunitTest_use_system_win32_libs,zip, \
+$(eval $(call gb_CppunitTest_use_system_win32_libs,shell_zip, \
 	ole32 \
 	kernel32 \
 	msvcprt \
 ))
 
-$(eval $(call gb_CppunitTest_use_static_libraries,zip,\
+$(eval $(call gb_CppunitTest_use_static_libraries,shell_zip,\
 	shell_xmlparser \
 	shlxthandler_common \
 ))
