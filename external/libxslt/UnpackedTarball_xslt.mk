59793a00deef09654aa6f181b6fc3920da5c5b35
Michael Stahl <mstahl@redhat.com>
2015-11-20 13:53:43 +0100

    libxslt: add patch for CVE-2015-7995
    (cherry picked from commit 45d1abeff9d4041b3e174f0eddd10bfd6ab8a360)
    
    Change-Id: I733cd21f6d8c7ea1e01f594d1483ad9c2043c188

--- external/libxslt/UnpackedTarball_xslt.mk
+++ external/libxslt/UnpackedTarball_xslt.mk
@@ -21,6 +21,7 @@ $(eval $(call gb_UnpackedTarball_add_patches,xslt,\
 	$(if $(filter ANDROID,$(OS)),external/libxslt/libxslt-android.patch) \
 	external/libxslt/libxslt-config-guess.patch.0 \
 	external/libxslt/rpath.patch.0 \
+	external/libxslt/0001-Fix-for-type-confusion-in-preprocessing-attributes.patch.1 \
 ))
 
 # vim: set noet sw=4 ts=4:
