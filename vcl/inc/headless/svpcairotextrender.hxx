54336499a8bc5fbee2891f0b50f3183d8b83d165
Jan-Marek Glogowski <glogow@fbihome.de>
2015-11-23 20:26:24 +0100

    Restore deleted svpcairotextrender.hxx
    Probably 0c75202ad20d3dc1f0a2a68375253d547f054405 just wanted to
    move the file to vcl/inc/headless.
    
    Change-Id: I8a11a24c272fa9cacb1505050e2c568aeb6fd4b2

---
+++ vcl/inc/headless/svpcairotextrender.hxx
@@ -0,0 +1,34 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef INCLUDED_VCL_UNX_GTK3_GDI_GTK3CAIROTEXTRENDER_HXX
+#define INCLUDED_VCL_UNX_GTK3_GDI_GTK3CAIROTEXTRENDER_HXX
+
+#include "cairotextrender.hxx"
+
+class SvpSalGraphics;
+
+class SvpCairoTextRender : public CairoTextRender
+{
+protected:
+    SvpSalGraphics& mrParent;
+
+public:
+    explicit SvpCairoTextRender(SvpSalGraphics& rParent);
+
+    virtual GlyphCache& getPlatformGlyphCache() override;
+    virtual cairo_t* getCairoContext() override;
+    virtual void getSurfaceOffset(double& nDX, double& nDY) override;
+    virtual void clipRegion(cairo_t* cr) override;
+    virtual void drawSurface(cairo_t* cr) override;
+};
+
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
