457ecf7eb9c66a8a2c918705b2ed73447b3fd269
Miklos Vajna <vmiklos@collabora.co.uk>
2015-11-24 11:58:09 +0100

    sw lok comments: fix missing invalidations from the scrollbar
    If a comment had a scrollbar, and the user clicked on the down arrow of
    it, then the button remained in the "pushed" state, as the scrollbar
    invalidations were not routed to the LOK client.
    
    With this, the button gets back to its non-pushed state after the mouse
    button is released.
    
    (cherry picked from commit 55040ea13426e337418143dcfe226dd2a395f041)
    
    Change-Id: Ie4ba5d0ec07229b0cfc08532e8e91ae25f7a4c9e

---
+++ sw/source/uibase/docvw/SidebarScrollBar.hxx
@@ -0,0 +1,43 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef INCLUDED_SW_SOURCE_UIBASE_DOCVW_SIDEBARSCROLLBAR_HXX
+#define INCLUDED_SW_SOURCE_UIBASE_DOCVW_SIDEBARSCROLLBAR_HXX
+
+#include <vcl/scrbar.hxx>
+
+class SwView;
+
+namespace sw
+{
+namespace sidebarwindows
+{
+
+class SwSidebarWin;
+
+/// Similar to the VCL scrollbar, but instrumented with Writer-specific details for LOK.
+class SidebarScrollBar : public ScrollBar
+{
+    SwSidebarWin& m_rSidebarWin;
+    SwView& m_rView;
+
+protected:
+    /// @see OutputDevice::LogicInvalidate().
+    void LogicInvalidate(const Rectangle* pRectangle) override;
+public:
+    SidebarScrollBar(SwSidebarWin& rSidebarWin, WinBits nStyle, SwView& rView);
+    virtual ~SidebarScrollBar();
+};
+
+}
+}
+
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
