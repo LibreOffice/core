2e6a5731fcc5ee4de468bf320165f54da0b7c36f
Michael Meeks <michael.meeks@collabora.com>
2015-11-24 15:32:16 +0000

    vcl: reduce maximum timeout to some sensible range.
    This fixes a bug with glib, where prepare of a source with such a
    large (max uint64) causes precision & wrap-around problems.
    Add assert to avoid recurrence.
    Also add a helpful user-visible source name in debug mode.
    
    Change-Id: I9e1eb41af9cc49f8bff88e07f04d4bc4d1877376

--- include/vcl/scheduler.hxx
+++ include/vcl/scheduler.hxx
@@ -61,7 +61,7 @@ protected:
 
     // These should be constexpr static, when supported.
     static const sal_uInt64 ImmediateTimeoutMs = 1;
-    static const sal_uInt64 MaximumTimeoutMs = SAL_MAX_UINT64;
+    static const sal_uInt64 MaximumTimeoutMs = 1000 * 60; // 1 minute
 
     static void ImplStartTimer(sal_uInt64 nMS, bool bForce = false);
 
