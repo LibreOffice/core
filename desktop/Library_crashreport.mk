563ca8372bf7a0e7ecdc3a99871bfede65add906
Markus Mohrhard <markus.mohrhard@googlemail.com>
2015-11-26 00:15:27 +0100

    use the new ini file based minidump uploader
    Change-Id: Iaf85fb53e6eff34e268b6948d62ca84b7f8e6dd2

---
+++ desktop/Library_crashreport.mk
@@ -0,0 +1,44 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_Library_Library,crashreport))
+
+$(eval $(call gb_Library_set_include,crashreport,\
+    $$(INCLUDE) \
+    -I$(SRCDIR)/desktop/inc \
+))
+
+$(eval $(call gb_Library_use_external,crashreport,breakpad))
+
+$(eval $(call gb_Library_add_defs,crashreport,\
+    -DCRASHREPORT_DLLIMPLEMENTATION \
+))
+
+$(eval $(call gb_Library_add_libs,crashreport,\
+    $(if $(filter LINUX %BSD SOLARIS, $(OS)), \
+        $(DLOPEN_LIBS) \
+        -lpthread \
+    ) \
+))
+
+$(eval $(call gb_Library_use_libraries,crashreport,\
+    comphelper \
+    cppu \
+    cppuhelper \
+    sal \
+    salhelper \
+	$(gb_UWINAPI) \
+))
+
+$(eval $(call gb_Library_add_exception_objects,crashreport,\
+    desktop/source/app/crashreport \
+))
+
+
+# vim: set ts=4 sw=4 et:
