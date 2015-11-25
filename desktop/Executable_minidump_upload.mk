a5f86d53c1b1764d119c17bba159a4f5ad406bee
Markus Mohrhard <markus.mohrhard@googlemail.com>
2015-11-25 23:48:20 +0100

    write an own minidump uploader
    This one is platform independent and uses a key-value file.
    
    Change-Id: I28a4483763666a5a8520874bf8e984eaaf24e9c0

---
+++ desktop/Executable_minidump_upload.mk
@@ -0,0 +1,22 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_Executable_Executable,minidump_upload))
+
+$(eval $(call gb_Executable_use_libraries,minidump_upload,\
+    sal \
+))
+
+$(eval $(call gb_Executable_use_external,minidump_upload,curl))
+
+$(eval $(call gb_Executable_add_exception_objects,minidump_upload,\
+    desktop/source/minidump/minidump_upload \
+))
+
+# vim: set noet sw=4 ts=4:
