976bf3cf4942c1a2399244bb01c9708aa8f71a00
Caolán McNamara <caolanm@redhat.com>
2015-11-25 14:56:51 +0000

    coverity#1340226 Missing break in switch
    Change-Id: Ibd1c15ce65501d4dc6d0cea8c8e5f4e683d1b7a4
    (cherry picked from commit c5bfac3f26dab2308b5a9797876927d410ac7876)

--- sd/source/ui/view/ViewShellBase.cxx
+++ sd/source/ui/view/ViewShellBase.cxx
@@ -1255,6 +1255,7 @@ void ViewShellBase::Implementation::GetSlotState (SfxItemSet& rSet)
                         pFrameView = mrBase.GetMainViewShell()->GetFrameView();
                         bState = pFrameView->GetViewShEditMode() == EM_MASTERPAGE
                             && pFrameView->GetPageKind() == PK_STANDARD;
+                        break;
 
                     case SID_SLIDE_SORTER_MULTI_PANE_GUI:
                     case SID_SLIDE_SORTER_MODE:
