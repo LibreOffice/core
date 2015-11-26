e1628be85186ac695e8b343a99687c58ba579ca5
Michael Stahl <mstahl@redhat.com>
2015-11-26 14:58:20 +0100

    vcl: fix build
    Change-Id: I51d992e9befc78f6e429480e29bfb78e9be87ac2

--- vcl/source/window/toolbox2.cxx
+++ vcl/source/window/toolbox2.cxx
@@ -357,7 +357,7 @@ void ToolBox::ImplInvalidate( bool bNewCalc, bool bFullPaint )
         {
             Invalidate( Rectangle( mnLeftBorder, mnTopBorder,
                                    mnDX-mnRightBorder-1, mnDY-mnBottomBorder-1 ) );
-            maIdle.Stop();
+            mpIdle->Stop();
         }
     }
     else
@@ -368,7 +368,7 @@ void ToolBox::ImplInvalidate( bool bNewCalc, bool bFullPaint )
 
             // do we need to redraw?
             if ( IsReallyVisible() && IsUpdateMode() )
-                maIdle.Start();
+                mpIdle->Start();
         }
     }
 
