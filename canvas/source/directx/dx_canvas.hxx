1145de8660691eb295a72fe8216092afdcde6d60
Stephan Bergmann <sbergman@redhat.com>
2015-11-26 09:07:47 +0100

    -Werror,-Winconsistent-missing-override
    Change-Id: I41d1e0d5e6263255426efb011c7b4bb5ae6775b5

--- canvas/source/directx/dx_canvas.hxx
+++ canvas/source/directx/dx_canvas.hxx
@@ -92,7 +92,7 @@ namespace dxcanvas
         DECLARE_UNO3_XCOMPONENT_AGG_DEFAULTS( Canvas, GraphicDeviceBase1_Base, ::cppu::WeakComponentImplHelperBase )
 
         // XServiceName
-        virtual OUString SAL_CALL getServiceName(  ) throw (css::uno::RuntimeException);
+        virtual OUString SAL_CALL getServiceName(  ) throw (css::uno::RuntimeException) override;
 
     private:
         css::uno::Sequence< css::uno::Any >                maArguments;
@@ -149,10 +149,10 @@ namespace dxcanvas
         DECLARE_UNO3_XCOMPONENT_AGG_DEFAULTS( BitmapCanvas, GraphicDeviceBase2_Base, ::cppu::WeakComponentImplHelperBase )
 
         // XServiceName
-        virtual OUString SAL_CALL getServiceName(  ) throw (css::uno::RuntimeException);
+        virtual OUString SAL_CALL getServiceName(  ) throw (css::uno::RuntimeException) override;
 
         // BitmapProvider
-        virtual IBitmapSharedPtr getBitmap() const;
+        virtual IBitmapSharedPtr getBitmap() const override;
 
      private:
         css::uno::Sequence< css::uno::Any >                maArguments;
