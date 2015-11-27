d764100d620ab8331a8d6bb80984ace09ec65b5c
Michael Stahl <mstahl@redhat.com>
2015-11-27 11:40:16 +0100

    try to fix gengal dependencies
    The component files are not necessary as gb_Executable__register_gengal
    already adds the service.rdb; the gengal.rc is insufficient as that file
    references fundamental.rc which is missing, so depend on the Package
    instead.  Also, INSTDIR is wrong for cross-compile.
    
    Change-Id: I30711a51e04539832657a242d2573c626b1cf8ec

--- RepositoryExternal.mk
+++ RepositoryExternal.mk
@@ -3755,6 +3755,7 @@ $(call gb_Executable_add_runtime_dependencies,gengal,\
 	$(call gb_Package_get_target_for_build,postprocess_images) \
 	$(call gb_Package_get_target_for_build,postprocess_registry) \
 	$(call gb_Package_get_target_for_build,instsetoo_native_setup_ure) \
+	$(call gb_Package_get_target_for_build,instsetoo_native_setup) \
 	$(call gb_Rdb_get_target_for_build,ure/services) \
 	$(INSTROOT)/$(LIBO_URE_MISC_FOLDER)/services.rdb \
 	$(call gb_UnoApi_get_target,offapi) \
