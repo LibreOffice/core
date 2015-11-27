fa906e531bd1b6d5e3b9ecef7016f6d67c987fa7
Stephan Bergmann <sbergman@redhat.com>
2015-11-27 09:31:07 +0100

    -Werror,-Wunused-function
    Change-Id: Icb5476f117ec44f5a4dc3026595305293b99fdd8

--- include/LibreOfficeKit/LibreOfficeKitInit.h
+++ include/LibreOfficeKit/LibreOfficeKitInit.h
@@ -253,7 +253,7 @@ static LibreOfficeKit *lok_init_2( const char *install_path,  const char *user_p
 }
 
 static
-#ifdef __GNUC__
+#if defined __GNUC__ || defined __clang__
 __attribute__((used))
 #endif
 LibreOfficeKit *lok_init( const char *install_path )
