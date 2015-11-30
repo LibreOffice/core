0309043fb3521923821edb4fa9a7524eb7062e92
Noel Grandin <noel@peralex.com>
2015-11-30 10:08:22 +0200

    tdf#96135 - Crash : When Field Format Window Close
    regression from commit db17d3c17c40d6b0e92392cf3c6e343d1d17b771
    "new loplugin: memoryvar"
    
    Change-Id: Id37226646a561579a375110bf684140d1fa8cb92
    (cherry picked from commit 2ba8476f650400c5285041e956d70d2c1c0bcf76)

--- dbaccess/source/ui/misc/UITools.cxx
+++ dbaccess/source/ui/misc/UITools.cxx
@@ -891,6 +891,7 @@ bool callColumnFormatDialog(vcl::Window* _pParent,
         }
     }
 
+    pFormatDescriptor.reset();
     SfxItemPool::Free(pPool);
     for (sal_uInt16 i=0; i<sizeof(pDefaults)/sizeof(pDefaults[0]); ++i)
         delete pDefaults[i];
