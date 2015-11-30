e24d08836403fef63aec70af054ef0d42c924345
Michael Stahl <mstahl@redhat.com>
2015-11-30 16:08:01 +0100

    mDNSResponder: upgrade to release 576.30.4
    That's 197 more than the current release, surely a big improvement.
    
    Change-Id: I48c81f9ae472a623ed9f59b94bb0bc921ad97c5c
    Reviewed-on: https://gerrit.libreoffice.org/20303
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Michael Stahl <mstahl@redhat.com>

--- external/mdnsresponder/UnpackedTarball_mDNSResponder.mk
+++ external/mdnsresponder/UnpackedTarball_mDNSResponder.mk
@@ -13,6 +13,7 @@ $(eval $(call gb_UnpackedTarball_set_tarball,mDNSResponder,$(MDNSRESPONDER_TARBA
 
 $(eval $(call gb_UnpackedTarball_add_patches,mDNSResponder,\
 	external/mdnsresponder/mDNSResponder_Win32_SOCKET.patch.1 \
+	external/mdnsresponder/mDNSResponder_Win32_buildfix.patch.1 \
 ))
 
 # vim: set noet sw=4 ts=4:
