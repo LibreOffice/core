756e103214c3c74c2af7e0da836b568259f81226
David Ostrovsky <david@ostrovsky.org>
2015-12-01 23:59:11 +0100

    Add build toolchain to upload LibreOffice API to Maven Central
    Set up the toolchain to create sources and javadocs artifacts in
    addition to JARs created during the build. Use Buck build tool for
    that: [1]. This is a fork of Google's build tool Blaze, created by
    Xooglers at Facebook. This build tool (like Blaze itself) uses
    Python to write build files.
    
    Add needed tools and build files to install LibreOffice API artifacts
    to local Maven repository or deploy them to Maven Central.
    
    To build all needed artifacts LibreOffice must be built regularly
    with GNU make first. To build the rest of the API (sources and
    javadocs):
    
      $> buck build api
    
    To replace version number with upcoming release version:
    
      $> solenv/bin/version.py 5.1.0
    
    To install the API to local Maven repository:
    
      $> buck build api_install
    
    To deploy the API to Maven Central:
    
      $> buck build api_deploy
    
    Detailed documentation is added to document the prerequisites and
    the workflow to upload LibreOffice API to Maven Central.
    
    * [1] https://buckbuild.com
    
    Change-Id: Ibdd552a01110836703bc069abe829b9921491cac
    Reviewed-on: https://gerrit.libreoffice.org/20343
    Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>
    Tested-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>
    (cherry picked from commit 1fd41f43eb73c373cb94d32d82c5fb7a7e243367)

--- solenv/maven/mvn.py
+++ solenv/maven/mvn.py
@@ -7,19 +7,25 @@
 
 from __future__ import print_function
 from optparse import OptionParser
-from os import path
+from os import path, environ
+from subprocess import check_output
 from sys import stderr
-try:
-  from subprocess import check_output
-except ImportError:
-  from subprocess import Popen, PIPE
-  def check_output(*cmd):
-    return Popen(*cmd, stdout=PIPE).communicate()[0]
+
+M = {
+  'juh': 'javaunohelper',
+  'jurt': 'jurt',
+  'officebean': 'bean',
+  'ridl': 'ridljar',
+  'unoil': 'unoil',
+  'unoloader': 'ridljar',
+}
+
 opts = OptionParser()
 opts.add_option('--repository', help='maven repository id')
 opts.add_option('--url', help='maven repository url')
+opts.add_option('-o')
 opts.add_option('-a', help='action (valid actions are: install,deploy)')
-opts.add_option('-v', help='libreoffice version')
+opts.add_option('-v', help='gerrit version')
 opts.add_option('-s', action='append', help='triplet of artifactId:type:path')
 
 args, ctx = opts.parse_args()
@@ -27,34 +33,45 @@ if not args.v:
   print('version is empty', file=stderr)
   exit(1)
 
-common = [
-  '-DgroupId=org.libreoffice',
-  '-Dversion=%s' % args.v,
-]
-
-self = path.dirname(path.abspath(__file__))
-mvn = ['mvn', '--file', path.join(self, 'fake_pom.xml')]
+root = path.abspath(__file__)
+while not path.exists(path.join(root, '.buckconfig')):
+  root = path.dirname(root)
 
 if 'install' == args.a:
-  cmd = mvn + ['install:install-file'] + common
+  cmd = [
+    'mvn',
+    'install:install-file',
+    '-Dversion=%s' % args.v,
+  ]
 elif 'deploy' == args.a:
-  cmd = mvn + [
-    'deploy:deploy-file',
+  cmd = [
+    'mvn',
+    'gpg:sign-and-deploy-file',
     '-DrepositoryId=%s' % args.repository,
     '-Durl=%s' % args.url,
-  ] + common
+  ]
 else:
   print("unknown action -a %s" % args.a, file=stderr)
   exit(1)
 
 for spec in args.s:
   artifact, packaging_type, src = spec.split(':')
+  exe = cmd + [
+    '-DpomFile=%s' % path.join(root, '%s/pom.%s.xml' % (M[artifact], artifact)),
+    '-Dpackaging=%s' % packaging_type,
+    '-Dfile=%s' % src,
+  ]
   try:
-    check_output(cmd + [
-      '-DartifactId=%s' % artifact,
-      '-Dpackaging=%s' % packaging_type,
-      '-Dfile=%s' % src,
-    ])
+    if environ.get('VERBOSE'):
+      print(' '.join(exe), file=stderr)
+    check_output(exe)
   except Exception as e:
     print('%s command failed: %s' % (args.a, e), file=stderr)
     exit(1)
+
+with open(args.o, 'w') as fd:
+  if args.repository:
+    print('Repository: %s' % args.repository, file=fd)
+  if args.url:
+    print('URL: %s' % args.url, file=fd)
+  print('Version: %s' % args.v, file=fd)
