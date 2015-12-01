LibreOffice/core

commit 536c259cb9644971d0eb0cdfb7d823c5383a5481

tdf#67770 store sidebar elements states
deck & panel states at user registry level
when disposing a sidebar

if a shared deck (with "any" context application) is modified,
it apply to all applications (eg. Gallery between calc and writer); TODO ?

clean ResourceManager Get/Set const/non-const

add panels when deck is created & refactoring

* macro example

Sub Main

controller = thisComponent.currentcontroller
sidebar = thisComponent.currentcontroller.getSidebar()

sidebar.setVisible(true)

decks = sidebar.getdecks()
deck = decks.getByName("PropertyDeck")

deck.activate(true)
deck.setTitle("new deck title")
deck.moveLast()

panels = deck.getPanels()
panel = panels.getByName("TextPropertyPanel")

panel.setTitle("new panel title")
panel.expand(true) ' expand and collapse other
panel.moveLast()

End Sub

Change-Id: I2552000af92a366ebb51a479962a094b72e400b6
Reviewed-on: https://gerrit.libreoffice.org/17992
Reviewed-by: Katarina Behrens <Katarina.Behrens@cib.de>
Tested-by: Katarina Behrens <Katarina.Behrens@cib.de>

sfx2/source/sidebar/SidebarController.cxx
@@ -198,6 +198,34 @@ void SAL_CALL SidebarController::disposing()
     maFocusManager.Clear();
     mpTabBar.disposeAndClear();
 
+    // save decks settings
+    // Impress shutdown : context (frame) is disposed before sidebar disposing
+    // calc writer : context (frame) is disposed after sidebar disposing
+    // so need to test if GetCurrentContext is still valid regarding msApplication
+
+    if (GetCurrentContext().msApplication != "none")
+        mpResourceManager->SaveDecksSettings(GetCurrentContext());
+
+    // clear decks
+    ResourceManager::DeckContextDescriptorContainer aDecks;
+
+    mpResourceManager->GetMatchingDecks (
+            aDecks,
+            GetCurrentContext(),
+            IsDocumentReadOnly(),
+            mxFrame->getController());
+
+    for (ResourceManager::DeckContextDescriptorContainer::const_iterator
+            iDeck(aDecks.begin()), iEnd(aDecks.end());
+            iDeck!=iEnd; ++iDeck)
+    {
+        const DeckDescriptor* deckDesc = mpResourceManager->GetDeckDescriptor(iDeck->msId);
+
+        VclPtr<Deck> aDeck = deckDesc->mpDeck;
+        if (aDeck)
+            aDeck.disposeAndClear();
+    }
+
     uno::Reference<css::frame::XController> xController = mxFrame->getController();
     if (!xController.is())
         xController = mxCurrentController;
@@ -233,9 +261,11 @@ void SAL_CALL SidebarController::notifyContextChangeEvent (const css::ui::Contex
     // Update to the requested new context asynchronously to avoid
     // subtle errors caused by SFX2 which in rare cases can not
     // properly handle a synchronous update.
+
     maRequestedContext = Context(
         rEvent.ApplicationName,
         rEvent.ContextName);
+
     if (maRequestedContext != maCurrentContext)
     {
         mxCurrentController.set(rEvent.Source, css::uno::UNO_QUERY);
@@ -272,6 +302,7 @@ void SAL_CALL SidebarController::statusChanged (const css::frame::FeatureStateEv
         // Force the current deck to update its panel list.
         if ( ! mbIsDocumentReadOnly)
             msCurrentDeckId = gsDefaultDeckId;
+
         mnRequestedForceFlags |= SwitchFlag_ForceSwitch;
         maAsynchronousDeckSwitch.CancelRequest();
         maContextChangeUpdate.RequestCall();
@@ -401,10 +432,17 @@ void SidebarController::ProcessNewWidth (const sal_Int32 nNewWidth)
 
 void SidebarController::UpdateConfigurations()
 {
+
     if (maCurrentContext != maRequestedContext
         || mnRequestedForceFlags!=SwitchFlag_NoForce)
     {
+
+        if (maCurrentContext.msApplication != "none")
+            mpResourceManager->SaveDecksSettings(maCurrentContext);
+
         maCurrentContext = maRequestedContext;
+
+        mpResourceManager->InitDeckContext(GetCurrentContext());
 
         // Find the set of decks that could be displayed for the new context.
         ResourceManager::DeckContextDescriptorContainer aDecks;
@@ -507,22 +545,93 @@ void SidebarController::SwitchToDeck (
 }
 
 
-void SidebarController::CreateDeck(const ::rtl::OUString& rDeckId)
+void SidebarController::CreateDeck(const ::rtl::OUString& rDeckId, bool bForceCreate)
 {
-    const DeckDescriptor* pDeckDescriptor = mpResourceManager->GetDeckDescriptor(rDeckId);
+    DeckDescriptor* pDeckDescriptor = mpResourceManager->GetDeckDescriptor(rDeckId);
 
-    if (pDeckDescriptor->mpDeck.get()==nullptr)
+    if (pDeckDescriptor)
     {
-        VclPtr<Deck> aDeck = VclPtr<Deck>::Create(
-                *pDeckDescriptor,
-                mpParentWindow,
-                [this]() { return this->RequestCloseDeck(); });
+        VclPtr<Deck> aDeck = pDeckDescriptor->mpDeck;
+        if (aDeck.get()==nullptr || bForceCreate)
+        {
+            if (aDeck.get()!=nullptr)
+                aDeck.disposeAndClear();
 
-        mpResourceManager->SetDeckToDescriptor(rDeckId, aDeck);
+            aDeck = VclPtr<Deck>::Create(
+                            *pDeckDescriptor,
+                            mpParentWindow,
+                            [this]() { return this->RequestCloseDeck(); });
+        }
+        pDeckDescriptor->mpDeck = aDeck;
+        CreatePanels(rDeckId);
     }
-
 }
 
+void SidebarController::CreatePanels(const ::rtl::OUString& rDeckId)
+{
+     DeckDescriptor* pDeckDescriptor = mpResourceManager->GetDeckDescriptor(rDeckId);
+
+    // init panels bounded to that deck, do not wait them being displayed as may be accessed through API
+
+            VclPtr<Deck> pDeck = pDeckDescriptor->mpDeck;
+
+            ResourceManager::PanelContextDescriptorContainer aPanelContextDescriptors;
+
+            css::uno::Reference<css::frame::XController> xController = mxCurrentController.is() ? mxCurrentController : mxFrame->getController();
+
+            mpResourceManager->GetMatchingPanels(
+                                                aPanelContextDescriptors,
+                                                maCurrentContext,
+                                                rDeckId,
+                                                xController);
+
+            // Update the panel list.
+            const sal_Int32 nNewPanelCount (aPanelContextDescriptors.size());
+            SharedPanelContainer aNewPanels;
+
+            aNewPanels.resize(nNewPanelCount);
+            sal_Int32 nWriteIndex (0);
+
+            for (sal_Int32 nReadIndex=0; nReadIndex<nNewPanelCount; ++nReadIndex)
+            {
+                const ResourceManager::PanelContextDescriptor& rPanelContexDescriptor (
+                    aPanelContextDescriptors[nReadIndex]);
+
+                // Determine if the panel can be displayed.
+                const bool bIsPanelVisible (!mbIsDocumentReadOnly || rPanelContexDescriptor.mbShowForReadOnlyDocuments);
+                if ( ! bIsPanelVisible)
+                    continue;
+
+                VclPtr<Panel>  aPanel = CreatePanel(
+                                            rPanelContexDescriptor.msId,
+                                            pDeck->GetPanelParentWindow(),
+                                            rPanelContexDescriptor.mbIsInitiallyVisible,
+                                            maCurrentContext,
+                                            pDeck);
+                if (aPanel.get()!=nullptr )
+                {
+                    aNewPanels[nWriteIndex] = aPanel;
+
+                    // Depending on the context we have to change the command
+                    // for the "more options" dialog.
+                    PanelTitleBar* pTitleBar = aNewPanels[nWriteIndex]->GetTitleBar();
+                    if (pTitleBar != NULL)
+                    {
+                        pTitleBar->SetMoreOptionsCommand(
+                            rPanelContexDescriptor.msMenuCommand,
+                            mxFrame, xController);
+                    }
+                    ++nWriteIndex;
+                 }
+
+            }
+
+            // mpCurrentPanels - may miss stuff (?)
+            aNewPanels.resize(nWriteIndex);
+            pDeck->ResetPanels(aNewPanels);
+
+            pDeckDescriptor->mpDeck = pDeck;
+}
 
 void SidebarController::SwitchToDeck (
     const DeckDescriptor& rDeckDescriptor,
@@ -578,7 +687,11 @@ void SidebarController::SwitchToDeck (
 
     // Provide a configuration and Deck object.
 
-    CreateDeck(rDeckDescriptor.msId);
+    CreateDeck(rDeckDescriptor.msId, bForceNewDeck);
+
+    if (bForceNewPanels && !bForceNewDeck) // already forced if bForceNewDeck
+        CreatePanels(rDeckDescriptor.msId);
+
     mpCurrentDeck.reset(rDeckDescriptor.mpDeck);
 
     if ( ! mpCurrentDeck)
@@ -590,79 +703,6 @@ void SidebarController::SwitchToDeck (
     if (pDebugTitleBar != NULL)
         pDebugTitleBar->SetTitle(rDeckDescriptor.msTitle + " (" + maCurrentContext.msContext + ")");
 #endif
-
-
-    // Update the panel list.
-    const sal_Int32 nNewPanelCount (aPanelContextDescriptors.size());
-    SharedPanelContainer aNewPanels;
-    const SharedPanelContainer& rCurrentPanels (mpCurrentDeck->GetPanels());
-
-    aNewPanels.resize(nNewPanelCount);
-    sal_Int32 nWriteIndex (0);
-    bool bHasPanelSetChanged (false);
-    for (sal_Int32 nReadIndex=0; nReadIndex<nNewPanelCount; ++nReadIndex)
-    {
-        const ResourceManager::PanelContextDescriptor& rPanelContexDescriptor (
-            aPanelContextDescriptors[nReadIndex]);
-
-        // Determine if the panel can be displayed.
-        const bool bIsPanelVisible (!mbIsDocumentReadOnly || rPanelContexDescriptor.mbShowForReadOnlyDocuments);
-        if ( ! bIsPanelVisible)
-            continue;
-
-        // Find the corresponding panel among the currently active
-        // panels.
-        SharedPanelContainer::const_iterator iPanel = rCurrentPanels.end();
-
-        if (!bForceNewPanels)
-        {
-            iPanel = rCurrentPanels.end();
-            for (auto a = rCurrentPanels.begin(); a != rCurrentPanels.end(); ++a)
-            {
-                if ((*a)->HasIdPredicate(rPanelContexDescriptor.msId))
-                {
-                    iPanel = a;
-                    break;
-                }
-            }
-        }
-        if (iPanel != rCurrentPanels.end())
-        {
-            // Panel already exists in current deck.  Reuse it.
-            aNewPanels[nWriteIndex] = *iPanel;
-            aNewPanels[nWriteIndex]->SetExpanded(rPanelContexDescriptor.mbIsInitiallyVisible);
-        }
-        else
-        {
-            // Panel does not yet exist or creation of new panels is forced.
-            // Create it.
-            aNewPanels[nWriteIndex] = CreatePanel(
-                rPanelContexDescriptor.msId,
-                mpCurrentDeck->GetPanelParentWindow(),
-                rPanelContexDescriptor.mbIsInitiallyVisible,
-                rContext);
-            bHasPanelSetChanged = true;
-        }
-
-        if (aNewPanels[nWriteIndex] != nullptr)
-        {
-            // Depending on the context we have to change the command
-            // for the "more options" dialog.
-            PanelTitleBar* pTitleBar = aNewPanels[nWriteIndex]->GetTitleBar();
-            if (pTitleBar != nullptr)
-            {
-                pTitleBar->SetMoreOptionsCommand(
-                    rPanelContexDescriptor.msMenuCommand,
-                    mxFrame, xController);
-            }
-
-            ++nWriteIndex;
-        }
-
-    }
-    // mpCurrentPanels - may miss stuff (?)
-    aNewPanels.resize(nWriteIndex);
-
 
     SfxSplitWindow* pSplitWindow = GetSplitWindow();
     sal_Int32 nTabBarDefaultWidth = TabBar::GetDefaultWidth() * mpTabBar->GetDPIScaleFactor();
@@ -685,13 +725,11 @@ void SidebarController::SwitchToDeck (
         mpParentWindow->GetSizePixel().Width() - nTabBarDefaultWidth,
         mpParentWindow->GetSizePixel().Height());
 
-    mpCurrentDeck->ResetPanels(aNewPanels);
     mpCurrentDeck->Show();
 
     mpParentWindow->SetText(rDeckDescriptor.msTitle);
 
-    if (bHasPanelSetChanged)
-        NotifyResize();
+    NotifyResize();
 
     // Tell the focus manager about the new panels and tab bar
     // buttons.
@@ -716,7 +754,8 @@ VclPtr<Panel> SidebarController::CreatePanel (
     const OUString& rsPanelId,
     vcl::Window* pParentWindow,
     const bool bIsInitiallyExpanded,
-    const Context& rContext)
+    const Context& rContext,
+    VclPtr<Deck> pDeck)
 {
     const PanelDescriptor* pPanelDescriptor = mpResourceManager->GetPanelDescriptor(rsPanelId);
 
@@ -728,7 +767,7 @@ VclPtr<Panel> SidebarController::CreatePanel (
         *pPanelDescriptor,
         pParentWindow,
         bIsInitiallyExpanded,
-        [this]() { return mpCurrentDeck.get()->RequestLayout(); },
+        [pDeck]() { return pDeck.get()->RequestLayout(); },
         [this]() { return this->GetCurrentContext(); },
         mxFrame);
 
@@ -915,7 +954,7 @@ std::shared_ptr<PopupMenu> SidebarController::CreatePopupMenu (
         else
         {
             pCustomizationMenu->InsertItem(nSubMenuIndex, iItem->msDisplayName, MenuItemBits::CHECKABLE);
-            pCustomizationMenu->CheckItem(nSubMenuIndex, iItem->mbIsActive);
+            pCustomizationMenu->CheckItem(nSubMenuIndex, iItem->mbIsEnabled && iItem->mbIsActive);
         }
     }
 
@@ -982,7 +1021,19 @@ IMPL_LINK_TYPED(SidebarController, OnMenuItemSelected, Menu*, pMenu, bool)
                 }
                 else if (nIndex >=MID_FIRST_HIDE)
                     if (pMenu->GetItemBits(nIndex) == MenuItemBits::CHECKABLE)
+                    {
                         mpTabBar->ToggleHideFlag(nIndex-MID_FIRST_HIDE);
+
+                        // Find the set of decks that could be displayed for the new context.
+                        ResourceManager::DeckContextDescriptorContainer aDecks;
+                        mpResourceManager->GetMatchingDecks (
+                                                    aDecks,
+                                                    GetCurrentContext(),
+                                                    IsDocumentReadOnly(),
+                                                    mxFrame->getController());
+                        // Notify the tab bar about the updated set of decks.
+                        mpTabBar->SetDecks(aDecks);
+                    }
             }
             catch (RuntimeException&)
             {

include/sfx2/sidebar/ContextList.hxx
@@ -55,6 +55,8 @@ public:
         const bool bIsInitiallyVisible,
         const ::rtl::OUString& rsMenuCommand);
 
+    const ::std::vector<Entry>& GetEntries() const {return maEntries;};
+
 private:
     ::std::vector<Entry> maEntries;
 

include/sfx2/sidebar/Deck.hxx
@@ -94,6 +94,7 @@ private:
 
     DECL_LINK_TYPED(HandleVerticalScrollBarChange, ScrollBar*, void);
     bool ProcessWheelEvent(CommandEvent* pCommandEvent);
+
 };
 
 } } // end of namespace sfx2::sidebar

include/sfx2/sidebar/DeckDescriptor.hxx
@@ -42,6 +42,8 @@ public:
     sal_Int32 mnOrderIndex;
     bool mbExperimental;
 
+    OUString msNodeName; // some impress deck nodes names are different from their Id
+
     VclPtr<Deck> mpDeck;
 
     DeckDescriptor();

include/sfx2/sidebar/PanelDescriptor.hxx
@@ -41,6 +41,8 @@ public:
     bool mbWantsCanvas;
     bool mbExperimental;
 
+    OUString msNodeName; // some impress panel nodes names are different from their Id
+
     PanelDescriptor();
     PanelDescriptor (const PanelDescriptor& rPanelDescriptor);
     ~PanelDescriptor();

include/sfx2/sidebar/ResourceManager.hxx
@@ -43,29 +43,17 @@ public:
      ResourceManager();
     ~ResourceManager();
 
-    static ResourceManager& Instance();
-
     const DeckDescriptor* GetDeckDescriptor(const OUString& rsDeckId) const;
+    DeckDescriptor* GetDeckDescriptor(const OUString& rsDeckId);
+
     const PanelDescriptor* GetPanelDescriptor(const OUString& rsPanelId) const;
-
-    /** Excluded or include a deck from being displayed in the tab
-        bar.
-        Note that this value is not persistent.
-        The flag can not be set directly at a DeckDescriptor object
-        because the ResourceManager gives access to them only
-        read-only.
-    */
-    void SetIsDeckEnabled(const OUString& rsDeckId, const bool bIsEnabled);
-
-    void SetDeckTitle(const OUString& rsDeckId, const OUString& sTitle);
-
-    void SetDeckToDescriptor(const OUString& rsDeckId, VclPtr<Deck> aDeck);
-
-    void SetDeckOrderIndex(const OUString& rsDeckId, const sal_Int32 orderIndex);
-
-    void SetPanelOrderIndex(const OUString& rsPanelId, const sal_Int32 orderIndex);
+    PanelDescriptor* GetPanelDescriptor(const OUString& rsPanelId);
 
     void UpdateModel(css::uno::Reference<css::frame::XModel> xModel);
+
+    void InitDeckContext(const Context& rContex);
+    void SaveDecksSettings(const Context& rContext);
+    void SaveDeckSettings(const DeckDescriptor* pDeckDesc);
 
     void disposeDecks();
 
@@ -100,7 +88,6 @@ public:
                                             const css::uno::Reference<css::frame::XController>& rxController);
 
     /** Remember the expansions state per panel and context.
-        This is not persistent past application end.
     */
     void StorePanelExpansionState(const OUString& rsPanelId,
                                   const bool bExpansionState,
@@ -123,13 +110,19 @@ private:
     static void ReadContextList(const utl::OConfigurationNode& rNode,
                          ContextList& rContextList,
                          const OUString& rsDefaultMenuCommand);
+
+    css::uno::Sequence<OUString> BuildContextList (ContextList rContextList, bool isDeckEnabled);
+
     void ReadLegacyAddons(const css::uno::Reference<css::frame::XController>& rxController);
     static utl::OConfigurationTreeRoot GetLegacyAddonRootNode(const OUString& rsModuleName);
     static void GetToolPanelNodeNames(std::vector<OUString>& rMatchingNames,
                                const utl::OConfigurationTreeRoot& aRoot);
-    static bool IsDeckEnabled(const OUString& rsDeckId,
+    bool IsDeckEnabled(const OUString& rsDeckId,
                        const Context& rContext,
                        const css::uno::Reference<css::frame::XController>& rxController);
+
+    const DeckDescriptor* ImplGetDeckDescriptor(const OUString& rsDeckId) const;
+    const PanelDescriptor* ImplGetPanelDescriptor(const OUString& rsPanelId) const;
 };
 
 } } // end of namespace sfx2::sidebar

include/sfx2/sidebar/SidebarController.hxx
@@ -151,7 +151,8 @@ public:
     void SwitchToDeck ( const ::rtl::OUString& rsDeckId);
     void SwitchToDefaultDeck();
 
-    void CreateDeck(const ::rtl::OUString& rDeckId);
+    void CreateDeck(const ::rtl::OUString& rDeckId, bool bForceCreate = false);
+    void CreatePanels(const ::rtl::OUString& rDeckId);
 
     ResourceManager::DeckContextDescriptorContainer GetMatchingDecks();
     ResourceManager::PanelContextDescriptorContainer GetMatchingPanels( const ::rtl::OUString& rDeckId);
@@ -223,7 +224,8 @@ private:
         const ::rtl::OUString& rsPanelId,
         vcl::Window* pParentWindow,
         const bool bIsInitiallyExpanded,
-        const Context& rContext);
+        const Context& rContext,
+        VclPtr<Deck> pDeck);
 
     void SwitchToDeck (
         const DeckDescriptor& rDeckDescriptor,

commit 02f2c2f8a160133731d707a8359f5cd576bff434

tdf#96162: sfx2: avoid re-creating existing sidebar Panels
Writer's naviagtor crashes when you click on a frame to select it,
because the SwContent* that is passed to SwContentTree::GotoContent()
is deleted because SidebarController::CreatePanels() re-creates
every single panel and then ResetPanels() disposes the existing ones,
which deletes the naviagtor's SwContent instances.

(regression from 536c259cb9644971d0eb0cdfb7d823c5383a5481)

Change-Id: I8f875df816a2d5948026a1d3f30b86bb2418acda

sfx2/source/sidebar/SidebarController.cxx
@@ -573,35 +573,43 @@ void SidebarController::CreatePanels(const ::rtl::OUString& rDeckId)
 
     // init panels bounded to that deck, do not wait them being displayed as may be accessed through API
 
-            VclPtr<Deck> pDeck = pDeckDescriptor->mpDeck;
+    VclPtr<Deck> pDeck = pDeckDescriptor->mpDeck;
 
-            ResourceManager::PanelContextDescriptorContainer aPanelContextDescriptors;
+    ResourceManager::PanelContextDescriptorContainer aPanelContextDescriptors;
 
-            css::uno::Reference<css::frame::XController> xController = mxCurrentController.is() ? mxCurrentController : mxFrame->getController();
+    css::uno::Reference<css::frame::XController> xController = mxCurrentController.is() ? mxCurrentController : mxFrame->getController();
 
-            mpResourceManager->GetMatchingPanels(
-                                                aPanelContextDescriptors,
-                                                maCurrentContext,
-                                                rDeckId,
-                                                xController);
+    mpResourceManager->GetMatchingPanels(
+                                        aPanelContextDescriptors,
+                                        maCurrentContext,
+                                        rDeckId,
+                                        xController);
 
-            // Update the panel list.
-            const sal_Int32 nNewPanelCount (aPanelContextDescriptors.size());
-            SharedPanelContainer aNewPanels;
+    // Update the panel list.
+    const sal_Int32 nNewPanelCount (aPanelContextDescriptors.size());
+    SharedPanelContainer aNewPanels;
 
-            aNewPanels.resize(nNewPanelCount);
-            sal_Int32 nWriteIndex (0);
+    aNewPanels.resize(nNewPanelCount);
+    sal_Int32 nWriteIndex (0);
 
-            for (sal_Int32 nReadIndex=0; nReadIndex<nNewPanelCount; ++nReadIndex)
-            {
-                const ResourceManager::PanelContextDescriptor& rPanelContexDescriptor (
-                    aPanelContextDescriptors[nReadIndex]);
+    for (sal_Int32 nReadIndex=0; nReadIndex<nNewPanelCount; ++nReadIndex)
+    {
+        const ResourceManager::PanelContextDescriptor& rPanelContexDescriptor (
+            aPanelContextDescriptors[nReadIndex]);
 
-                // Determine if the panel can be displayed.
-                const bool bIsPanelVisible (!mbIsDocumentReadOnly || rPanelContexDescriptor.mbShowForReadOnlyDocuments);
-                if ( ! bIsPanelVisible)
-                    continue;
+        // Determine if the panel can be displayed.
+        const bool bIsPanelVisible (!mbIsDocumentReadOnly || rPanelContexDescriptor.mbShowForReadOnlyDocuments);
+        if ( ! bIsPanelVisible)
+            continue;
 
+        Panel *const pPanel(pDeck->GetPanel(rPanelContexDescriptor.msId));
+        if (pPanel != nullptr)
+        {
+            aNewPanels[nWriteIndex] = pPanel;
+            ++nWriteIndex;
+        }
+        else
+        {
                 VclPtr<Panel>  aPanel = CreatePanel(
                                             rPanelContexDescriptor.msId,
                                             pDeck->GetPanelParentWindow(),
@@ -625,12 +633,11 @@ void SidebarController::CreatePanels(const ::rtl::OUString& rDeckId)
                  }
 
             }
+        }
 
-            // mpCurrentPanels - may miss stuff (?)
-            aNewPanels.resize(nWriteIndex);
-            pDeck->ResetPanels(aNewPanels);
-
-            pDeckDescriptor->mpDeck = pDeck;
+    // mpCurrentPanels - may miss stuff (?)
+    aNewPanels.resize(nWriteIndex);
+    pDeck->ResetPanels(aNewPanels);
 }
 
 void SidebarController::SwitchToDeck (