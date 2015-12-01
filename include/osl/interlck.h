52fc58c724b4a5a31044bc65cd7dc1aa1ae03da0
Noel Grandin <noel@peralex.com>
2015-12-01 07:58:12 +0200

    use compiler intrinsic for osl_atomic_increment on Windows
    Change-Id: I66b2ba5d8e419f6af5eb3f85f8c12effcdaf4d5e

--- include/osl/interlck.h
+++ include/osl/interlck.h
@@ -58,6 +58,8 @@ SAL_DLLPUBLIC oslInterlockedCount SAL_CALL osl_decrementInterlockedCount(oslInte
 */
 #if HAVE_GCC_BUILTIN_ATOMIC
 #    define osl_atomic_increment(p)  __sync_add_and_fetch((p), 1)
+#elsif defined( _WIN32 )
+#    define osl_atomic_increment(p)  _InterlockedIncrement(p)
 #else
 #    define osl_atomic_increment(p) osl_incrementInterlockedCount((p))
 #endif
@@ -75,6 +77,8 @@ SAL_DLLPUBLIC oslInterlockedCount SAL_CALL osl_decrementInterlockedCount(oslInte
 */
 #if HAVE_GCC_BUILTIN_ATOMIC
 #    define osl_atomic_decrement(p) __sync_sub_and_fetch((p), 1)
+#elsif defined( _WIN32 )
+#    define osl_atomic_decrement(p)  _InterlockedDecrement(p)
 #else
 #    define osl_atomic_decrement(p) osl_decrementInterlockedCount((p))
 #endif
