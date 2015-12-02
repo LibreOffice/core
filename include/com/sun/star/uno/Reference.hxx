ad8834495570e05ce01d60e9067e6cff32ac993c
Noel Grandin <noel@peralex.com>
2015-12-02 12:58:50 +0200

    add move assignment and constructor to uno::Reference
    "perf stat" says:
    Before  14,009,233,975,201      cycles
    After   13,660,876,595,941      cycles
    i.e. shaved roughly 2% of the cycles
    
    Change-Id: If604a125a8a5040281abd699678d0c791d5bbb51

--- include/com/sun/star/uno/Reference.hxx
+++ include/com/sun/star/uno/Reference.hxx
@@ -124,6 +124,15 @@ inline Reference< interface_type >::Reference( const Reference< interface_type >
         _pInterface->acquire();
 }
 
+#if defined LIBO_INTERNAL_ONLY
+template< class interface_type >
+inline Reference< interface_type >::Reference( Reference< interface_type > && rRef ) noexcept
+{
+    _pInterface = rRef._pInterface;
+    rRef._pInterface = nullptr;
+}
+#endif
+
 template< class interface_type > template< class derived_type >
 inline Reference< interface_type >::Reference(
     const Reference< derived_type > & rRef,
@@ -341,6 +350,15 @@ inline Reference< interface_type > & Reference< interface_type >::operator = (
     return *this;
 }
 
+#if defined LIBO_INTERNAL_ONLY
+template< class interface_type >
+inline Reference< interface_type > & Reference< interface_type >::operator = (
+     Reference< interface_type > && rRef ) noexcept
+{
+    std::swap(_pInterface, rRef._pInterface);
+    return *this;
+}
+#endif
 
 template< class interface_type >
 inline Reference< interface_type > Reference< interface_type >::query(
