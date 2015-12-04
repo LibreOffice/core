6c0d137403f09a61b7c125dd94818b92d55f9b05
Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
2015-12-04 17:05:11 +0100

    Move icon rotation/flip handling from ToolbarManager to vcl's Toolbar
    This enables rotatad/flipped icons in the Sidebar and other non-toolbar places (tdf#85767)
    
    Change-Id: I034394ae6f9b8d91ef5a376a43eda587098f064e

--- vcl/source/window/toolbox.cxx
+++ vcl/source/window/toolbox.cxx
@@ -20,7 +20,9 @@
 #include <tools/debug.hxx>
 #include <tools/rc.h>
 #include <tools/poly.hxx>
+#include <svl/imageitm.hxx>
 
+#include <vcl/vclstatuslistener.hxx>
 #include <vcl/event.hxx>
 #include <vcl/decoview.hxx>
 #include <vcl/accel.hxx>
@@ -1398,6 +1400,7 @@ void ToolBox::ImplInit( vcl::Window* pParent, WinBits nStyle )
     mnLastFocusItemId = 0;
     mnKeyModifier     = 0;
     mnActivateCount   = 0;
+    mpStatusListener  = new VclStatusListener<ToolBox>(this, ".uno:ImageOrientation");
 
     mpIdle = new Idle("toolbox update");
     mpIdle->SetPriority( SchedulerPriority::RESIZE );
@@ -1658,6 +1661,10 @@ void ToolBox::dispose()
             pSVData->maCtrlData.mpTBDragMgr = nullptr;
         }
     }
+
+    if (mpStatusListener.is())
+        mpStatusListener->dispose();
+
     mpFloatWin.clear();
 
     delete mpIdle;
@@ -4540,6 +4547,21 @@ void ToolBox::DataChanged( const DataChangedEvent& rDCEvt )
     }
 
     maDataChangedHandler.Call( &rDCEvt );
+}
+
+void ToolBox::statusChanged( const css::frame::FeatureStateEvent& Event )
+{
+    // Update image mirroring/rotation
+    if ( Event.FeatureURL.Complete == ".uno:ImageOrientation" )
+    {
+        SfxImageItem aItem( 1, 0 );
+        aItem.PutValue( Event.State, 0 );
+
+        mbImagesMirrored = aItem.IsMirrored();
+        mnImagesRotationAngle = aItem.GetRotation();
+
+        UpdateImageOrientation();
+    }
 }
 
 bool ToolBox::PrepareToggleFloatingMode()
