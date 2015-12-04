7b3639011817fba9a8a2f25fc3400d89ae860903
Caolán McNamara <caolanm@redhat.com>
2015-12-04 14:11:35 +0000

    silence vcl release mouse warning
    Change-Id: I12eb894d88c7b96f92e3d080ac3cf24ff2e025ec
    (cherry picked from commit cfc119c4322e36283ffc4bebfb0757e8e0039a5a)

--- sw/source/uibase/docvw/edtwin.cxx
+++ sw/source/uibase/docvw/edtwin.cxx
@@ -4419,7 +4419,8 @@ void SwEditWin::MouseButtonUp(const MouseEvent& rMEvt)
             m_rView.GetDrawFuncPtr()->BreakCreate();   // abort drawing
 
         g_bNoInterrupt = false;
-        ReleaseMouse();
+        if (IsMouseCaptured())
+            ReleaseMouse();
         return;
     }
     bool bPopMode = false;
