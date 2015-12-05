9d1e3f5532e57f988deb7c7e08fc1dd29f2dfadb
pasqual milvaques <pasqual.milvaques@gmail.com>
2015-12-05 17:17:57 +0100

    tdf#90319: make image flipping work in writer for png
    Flipping of images was not working if it was only horizontal or
    vertical, if both options are selected flipping is treated as a 180º
    rotation and that make it work. modifications are done so the
    flipping state is correctly passed down to the BlendBitmap function and
    it transforms correctly the bitmap
    
    Change-Id: I014828eb8c821377c979fc9992190bda81ab70e2

--- vcl/source/outdev/bitmap.cxx
+++ vcl/source/outdev/bitmap.cxx
@@ -633,8 +633,6 @@ void OutputDevice::DrawDeviceAlphaBitmap( const Bitmap& rBmp, const AlphaMask& r
     const bool bHMirr = aOutSz.Width() < 0;
     const bool bVMirr = aOutSz.Height() < 0;
 
-    ClipToPaintRegion(aDstRect);
-
     if (bHMirr)
     {
         aOutSz.Width() = -aOutSz.Width();
@@ -693,7 +691,7 @@ void OutputDevice::DrawDeviceAlphaBitmap( const Bitmap& rBmp, const AlphaMask& r
         Rectangle aBmpRect(Point(), rBmp.GetSizePixel());
         if (!aBmpRect.Intersection(Rectangle(rSrcPtPixel, rSrcSizePixel)).IsEmpty())
         {
-            DrawDeviceAlphaBitmapSlowPath(rBmp, rAlpha, aDstRect, aBmpRect, aOutSz, aOutPt);
+            DrawDeviceAlphaBitmapSlowPath(rBmp, rAlpha, aDstRect, aBmpRect, aOutSz, aOutPt,bHMirr,bVMirr);
         }
     }
 }
@@ -923,14 +921,11 @@ private:
 
 } // end anonymous namespace
 
-void OutputDevice::DrawDeviceAlphaBitmapSlowPath(const Bitmap& rBitmap, const AlphaMask& rAlpha, Rectangle aDstRect, Rectangle aBmpRect, Size& aOutSize, Point& aOutPoint)
+void OutputDevice::DrawDeviceAlphaBitmapSlowPath(const Bitmap& rBitmap, const AlphaMask& rAlpha, Rectangle aDstRect, Rectangle aBmpRect, Size& aOutSize, Point& aOutPoint, const bool bHMirr,const bool bVMirr)
 {
     assert(!is_double_buffered_window());
 
     VirtualDevice* pOldVDev = mpAlphaVDev;
-
-    const bool  bHMirr = aOutSize.Width() < 0;
-    const bool  bVMirr = aOutSize.Height() < 0;
 
     // The scaling in this code path produces really ugly results - it
     // does the most trivial scaling with no smoothing.
@@ -1487,6 +1482,7 @@ Bitmap OutputDevice::BlendBitmap(
     BitmapColor aDstCol;
     Bitmap      res;
     int         nX, nY;
+    int         destnX, destnY;
 
     if( GetBitCount() <= 8 )
     {
@@ -1552,11 +1548,21 @@ Bitmap OutputDevice::BlendBitmap(
                             Scanline    pPScan = pP->GetScanline( nMapY );
                             Scanline    pAScan = pA->GetScanline( nMapY );
 
+                            destnY=nY;
+                            if (bVMirr)
+                            {
+                                destnY=nDstHeight-nY-1;
+                            }
                             for( nX = 0; nX < nDstWidth; nX++ )
                             {
                                 const long nMapX = pMapX[ nX ];
                                 aDstCol = pB->GetPixel( nY, nX );
-                                pB->SetPixel( nY, nX, aDstCol.Merge( pP->GetPaletteColor( pPScan[ nMapX ] ),
+                                destnX=nX;
+                                if (bHMirr)
+                                {
+                                    destnX=nDstWidth-nX-1;
+                                }
+                                pB->SetPixel( destnY, destnX, aDstCol.Merge( pP->GetPaletteColor( pPScan[ nMapX ] ),
                                                                      pAScan[ nMapX ] ) );
                             }
                         }
@@ -1570,12 +1576,23 @@ Bitmap OutputDevice::BlendBitmap(
                         const long  nMapY = pMapY[ nY ];
                         Scanline    pAScan = pA->GetScanline( nMapY );
 
+                        destnY=nY;
+                        if (bVMirr)
+                        {
+                            destnY=nDstHeight-nY-1;
+                        }
                         for( nX = 0; nX < nDstWidth; nX++ )
                         {
                             const long nMapX = pMapX[ nX ];
                             aDstCol = pB->GetPixel( nY, nX );
-                            pB->SetPixel( nY, nX, aDstCol.Merge( pP->GetColor( nMapY, nMapX ),
-                                                                 pAScan[ nMapX ] ) );
+
+                            destnX=nX;
+                            if (bHMirr)
+                            {
+                                destnX=nDstWidth-nX-1;
+                            }
+                            pB->SetPixel( destnY, destnX, aDstCol.Merge( pP->GetColor( nMapY, nMapX ),
+                                                                pAScan[ nMapX ] ) );
                         }
                     }
                 }
