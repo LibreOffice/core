32cd9f3c3bf1f55523c38416ad1adc814249db67
David Tardon <dtardon@redhat.com>
2015-12-07 23:09:01 +0100

    merge libmwaw type/filter defs (Calc)
    Change-Id: Ic0f01d2f71cad69bea0944c62fb0832bc6caa252
    (cherry picked from commit 33ff1ba8583f9461ddf5b773bee6c1974c21afbe)

--- writerperfect/source/calc/MWAWCalcImportFilter.cxx
+++ writerperfect/source/calc/MWAWCalcImportFilter.cxx
@@ -54,89 +54,43 @@ bool MWAWCalcImportFilter::doDetectFormat(librevenge::RVNGInputStream &rInput, O
 
     if (confidence == MWAWDocument::MWAW_C_EXCELLENT)
     {
-        if (docKind == MWAWDocument::MWAW_K_SPREADSHEET || docKind == MWAWDocument::MWAW_K_DATABASE)
+        switch (docKind)
+        {
+        case MWAWDocument::MWAW_K_DATABASE:
         {
             switch (docType)
             {
-            case MWAWDocument::MWAW_T_BEAGLEWORKS:
-                rTypeName = "calc_Beagle_Works";
+            case MWAWDocument::MWAW_T_CLARISWORKS:
+                rTypeName = "calc_ClarisWorks";
                 break;
+            case MWAWDocument::MWAW_T_MICROSOFTWORKS:
+                rTypeName = "calc_Mac_Works";
+                break;
+            default:
+                rTypeName = "MWAW_Database";
+                break;
+            }
+        }
+        case MWAWDocument::MWAW_K_SPREADSHEET:
+        {
+            switch (docType)
+            {
             case MWAWDocument::MWAW_T_CLARISRESOLVE:
                 rTypeName = "calc_Claris_Resolve";
                 break;
             case MWAWDocument::MWAW_T_CLARISWORKS:
                 rTypeName = "calc_ClarisWorks";
                 break;
-            case MWAWDocument::MWAW_T_GREATWORKS:
-                rTypeName = "calc_Great_Works";
-                break;
             case MWAWDocument::MWAW_T_MICROSOFTWORKS:
                 rTypeName = "calc_Mac_Works";
                 break;
-            case MWAWDocument::MWAW_T_WINGZ:
-                rTypeName = "calc_Mac_Wingz";
-                break;
-
-            case MWAWDocument::MWAW_T_ACTA:
-            case MWAWDocument::MWAW_T_ADOBEILLUSTRATOR:
-            case MWAWDocument::MWAW_T_DBASE:
-            case MWAWDocument::MWAW_T_DOCMAKER:
-            case MWAWDocument::MWAW_T_EDOC:
-            case MWAWDocument::MWAW_T_FAMILYTREEMAKER:
-            case MWAWDocument::MWAW_T_FILEMAKER:
-            case MWAWDocument::MWAW_T_FOXBASE:
-            case MWAWDocument::MWAW_T_FRAMEMAKER:
-            case MWAWDocument::MWAW_T_FULLIMPACT:
-            case MWAWDocument::MWAW_T_FULLPAINT:
-            case MWAWDocument::MWAW_T_FULLWRITE:
-            case MWAWDocument::MWAW_T_INFOGENIE:
-            case MWAWDocument::MWAW_T_KALEIDAGRAPH:
-            case MWAWDocument::MWAW_T_HANMACWORDJ:
-            case MWAWDocument::MWAW_T_HANMACWORDK:
-            case MWAWDocument::MWAW_T_LIGHTWAYTEXT:
-            case MWAWDocument::MWAW_T_MACDOC:
-            case MWAWDocument::MWAW_T_MACDRAFT:
-            case MWAWDocument::MWAW_T_MACDRAW:
-            case MWAWDocument::MWAW_T_MACDRAWPRO:
-            case MWAWDocument::MWAW_T_MACPAINT:
-            case MWAWDocument::MWAW_T_MACWRITE:
-            case MWAWDocument::MWAW_T_MACWRITEPRO:
-            case MWAWDocument::MWAW_T_MARINERWRITE:
-            case MWAWDocument::MWAW_T_MINDWRITE:
-            case MWAWDocument::MWAW_T_MICROSOFTFILE:
-            case MWAWDocument::MWAW_T_MICROSOFTMULTIPLAN:
-            case MWAWDocument::MWAW_T_MICROSOFTWORD:
-            case MWAWDocument::MWAW_T_MORE:
-            case MWAWDocument::MWAW_T_NISUSWRITER:
-            case MWAWDocument::MWAW_T_OVERVUE:
-            case MWAWDocument::MWAW_T_PAGEMAKER:
-            case MWAWDocument::MWAW_T_PIXELPAINT:
-            case MWAWDocument::MWAW_T_RAGTIME:
-            case MWAWDocument::MWAW_T_READYSETGO:
-            case MWAWDocument::MWAW_T_SUPERPAINT:
-            case MWAWDocument::MWAW_T_SYMPOSIUM:
-            case MWAWDocument::MWAW_T_TEACHTEXT:
-            case MWAWDocument::MWAW_T_TEXEDIT:
-            case MWAWDocument::MWAW_T_TRAPEZE:
-            case MWAWDocument::MWAW_T_WRITENOW:
-            case MWAWDocument::MWAW_T_WRITERPLUS:
-            case MWAWDocument::MWAW_T_XPRESS:
-            case MWAWDocument::MWAW_T_ZWRITE:
-            case MWAWDocument::MWAW_T_4DIMENSION:
-
-            case MWAWDocument::MWAW_T_RESERVED1:
-            case MWAWDocument::MWAW_T_RESERVED2:
-            case MWAWDocument::MWAW_T_RESERVED3:
-            case MWAWDocument::MWAW_T_RESERVED4:
-            case MWAWDocument::MWAW_T_RESERVED5:
-            case MWAWDocument::MWAW_T_RESERVED6:
-            case MWAWDocument::MWAW_T_RESERVED7:
-            case MWAWDocument::MWAW_T_RESERVED8:
-            case MWAWDocument::MWAW_T_RESERVED9:
-            case MWAWDocument::MWAW_T_UNKNOWN:
             default:
+                rTypeName = "MWAW_Spreadsheet";
                 break;
             }
+        }
+        default:
+            break;
         }
     }
 
