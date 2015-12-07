75e13bee869dd34e9d824762c0d7c81708b1f4f4
kripton <yeliztaneroglu@gmail.com>
2015-12-07 20:57:20 +0200

    tdf#74608 xmloff:Constructor feature for XMLMetaExportComponent
    Change-Id: I7106433ddb2adfcf9e307e5e420bf823c33584e3

--- xmloff/source/core/facreg.cxx
+++ xmloff/source/core/facreg.cxx
@@ -125,9 +125,6 @@ XMLOFF_DLLPUBLIC void * SAL_CALL xo_component_getFactory( const sal_Char * pImpl
         else SINGLEFACTORY( SchXMLExport_Styles )
         else SINGLEFACTORY( SchXMLExport_Content )
 
-        // meta import/export
-        else SINGLEFACTORY( XMLMetaExportComponent )
-
         // meta import/export OOo
         else SINGLEFACTORY( XMLMetaExportOOO )
 
