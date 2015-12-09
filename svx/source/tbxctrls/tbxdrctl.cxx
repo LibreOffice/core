c8790652178db666a6886475cc64d1da5405a761
Gulsah Kose <gulsah.1004@gmail.com>
2015-12-09 23:22:25 +0200

    tdf#89540 Completed show draw functions button for impress and draw.
    This patch is not completed. Created for getting help to developers.
    v2: failed commit.
    v3: Solved slot defination and dimmed button problem.
    v4: Tried to find slot execution method.
    v5: Added a control for draw toolbar name in impress and draw
    v6: Toolbar name control is changed for checking request only impress and draw.
    v7: Overrided initialize method of base class.
    v8: Control provided with module name.
    v9: Cleaned unnecessary included headers
    v10: Removed m_sModuleName defination and reused base class' m_sModuleName variable.
    Added white space before header files. Moved control into the initialize method.
    Control is changed as only controls impress or draw.
    
    Change-Id: I781783a72954af1f16212b981356db472ed48739
    Signed-off-by: Gulsah Kose <gulsah.1004@gmail.com>

--- svx/source/tbxctrls/tbxdrctl.cxx
+++ svx/source/tbxctrls/tbxdrctl.cxx
@@ -29,7 +29,6 @@
 
 #include <svx/dialmgr.hxx>
 #include <svx/dialogs.hrc>
-
 #include "svx/tbxctl.hxx"
 #include "svx/tbxcolor.hxx"
 #include <com/sun/star/frame/XLayoutManager.hpp>
@@ -43,14 +42,26 @@ using namespace ::com::sun::star::frame;
 
 SvxTbxCtlDraw::SvxTbxCtlDraw( sal_uInt16 nSlotId, sal_uInt16 nId, ToolBox& rTbx ) :
 
-    SfxToolBoxControl( nSlotId, nId, rTbx ),
-
-    m_sToolboxName( "private:resource/toolbar/drawbar" )
+    SfxToolBoxControl( nSlotId, nId, rTbx )
 
 {
     rTbx.SetItemBits( nId, ToolBoxItemBits::CHECKABLE | rTbx.GetItemBits( nId ) );
     rTbx.Invalidate();
 }
+
+void SAL_CALL SvxTbxCtlDraw::initialize( const css::uno::Sequence< css::uno::Any >& aArguments ) throw ( css::uno::Exception, css::uno::RuntimeException, std::exception)
+    {
+        svt::ToolboxController::initialize(aArguments);
+    /*
+     * Toolbar name is defined as "private:resource/toolbar/drawbar" in writer and calc,
+     * "private:resource/toolbar/toolbar" in draw and impress. Control is added for this
+     * difference.
+     */
+    if (svt::ToolboxController::m_sModuleName=="com.sun.star.presentation.PresentationDocument" || svt::ToolboxController::m_sModuleName=="com.sun.star.drawing.DrawingDocument")
+        m_sToolboxName="private:resource/toolbar/toolbar";
+    else
+        m_sToolboxName="private:resource/toolbar/drawbar";
+    }
 
 
 
