fe3a6d331c3c1f9a699380a28b2029ac033434f2
Caolán McNamara <caolanm@redhat.com>
2015-12-09 12:23:11 +0000

    protect against missing SuperTable
    Change-Id: I788ec28424bb638f727901b7d25e48a284ab67d0
    (cherry picked from commit 356109d4ead137ea22b358b20ed22a4bd09d35f6)

--- lotuswordpro/source/filter/lwpfribtable.cxx
+++ lotuswordpro/source/filter/lwpfribtable.cxx
@@ -98,6 +98,8 @@ void LwpFribTable::XFConvert(XFContentContainer* pCont)
 {
     XFContentContainer* pXFContentContainer = pCont;
     LwpSuperTableLayout* pSuper = GetSuperTable();
+    if (!pSuper)
+        return;
     sal_uInt8 nType = pSuper->GetRelativeType();
     LwpVirtualLayout* pContainer = pSuper->GetContainerLayout();
     if (!pContainer)
