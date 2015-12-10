82aa16b5d6ea5e01055361de05acdd3488179388
Caolán McNamara <caolanm@redhat.com>
2015-12-10 10:19:12 +0000

    protect against missing Ruby Marker
    Change-Id: I04809443b5dc50904654936e608360b57c2e85c3
    (cherry picked from commit 215516a52fd01170bb4a79828b0f0948884b727a)
    (cherry picked from commit 6c296afad6b04328a596a92326aa50b1b480aba0)

--- lotuswordpro/source/filter/lwpframelayout.cxx
+++ lotuswordpro/source/filter/lwpframelayout.cxx
@@ -1240,6 +1240,8 @@ void LwpRubyLayout::ConvertContentText()
 void LwpRubyLayout::RegisterStyle()
 {
     LwpRubyMarker* pMarker = GetMarker();
+    if (!pMarker)
+        throw std::runtime_error("missing Ruby Marker");
 
     XFRubyStyle* pRubyStyle = new XFRubyStyle;
 
