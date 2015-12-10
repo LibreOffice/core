ec53d798f68fcbab7abfd9d578f9cd4721a21f0b
Caolán McNamara <caolanm@redhat.com>
2015-12-10 10:00:58 +0000

    guard against infinite recursion on parsing objects
    Change-Id: Ia6502afde54dbf379bc9951c80c6594f1f436ef5
    (cherry picked from commit fbd329800340a496def839181858964b1e3709a5)

--- lotuswordpro/source/filter/lwpframelayout.cxx
+++ lotuswordpro/source/filter/lwpframelayout.cxx
@@ -1148,7 +1148,7 @@ void LwpDropcapLayout::Parse(IXFStream* pOutputStream)
     if(pPara.is())
     {
         pPara->SetFoundry(m_pFoundry);
-        pPara->Parse(pOutputStream);
+        pPara->DoParse(pOutputStream);
     }
 }
 
