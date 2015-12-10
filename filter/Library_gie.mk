03bbe1c0328de26c949c7e57d079fe6827e8b293
Bryan Quigley <gquigs@gmail.com>
2015-12-10 10:14:10 -0500

    tdf#92925 Remove MET (OS/2) export
    MET Files were used with OS/2 and are not used anymore.
    I did test import which this patch did not remove.  However,
    with MET files you can not export them as an image using "Save as
    image" (this patch didn't cause or fix that).  You can still use
    the export of the whole document to convert the MET file.
    
    Change-Id: Ie818ef963db14166be354379d4b2991a3b72b645

--- filter/Library_gie.mk
+++ filter/Library_gie.mk
@@ -40,7 +40,6 @@ $(eval $(call gb_Library_use_libraries,gie,\
 $(eval $(call gb_Library_add_exception_objects,gie,\
 	filter/source/graphicfilter/egif/egif \
 	filter/source/graphicfilter/etiff/etiff \
-	filter/source/graphicfilter/eos2met/eos2met \
 	filter/source/graphicfilter/eps/eps \
 	filter/source/graphicfilter/egif/giflzwc \
 	filter/source/graphicfilter/itiff/ccidecom \
