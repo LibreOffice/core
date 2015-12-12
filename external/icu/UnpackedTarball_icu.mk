62eb7dcb55e4fb6aeb53d9ad78d31dfcb2ed594f
Martin Hosken <martin_hosken@sil.org>
2015-12-12 11:36:53 +0700

    Refactor Khmer line breaking as per bug tdf#59448
    Change-Id: I36a97e0d6dffd536ab53255d53b9e7babbd0bc84

--- external/icu/UnpackedTarball_icu.mk
+++ external/icu/UnpackedTarball_icu.mk
@@ -27,6 +27,7 @@ $(eval $(call gb_UnpackedTarball_add_patches,icu,\
 	external/icu/clang-cl.patch.0 \
 	$(if $(filter-out ANDROID,$(OS)),external/icu/icu4c-icudata-stdlibs.diff) \
 	$(if $(filter EMSCRIPTEN,$(OS)),external/icu/icu4c-emscripten.patch.1) \
+	external/icu/khmerbreakengine.patch \
 ))
 
 # vim: set noet sw=4 ts=4:
