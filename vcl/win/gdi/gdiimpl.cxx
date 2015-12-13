LibreOffice/core

commit 1321ae8ceba1801cd75322d6cf51ab53bac86b2a

check in dbg if we use correct SalBitmap instance that we expect
Change-Id: If1f176291304d2ef401bfb65c73ebf6635f0e756
Reviewed-on: https://gerrit.libreoffice.org/20668
Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>
Tested-by: Tomaž Vajngerl <quikee@gmail.com>

vcl/win/gdi/gdiimpl.cxx
@@ -707,6 +707,8 @@ void WinSalGraphicsImpl::drawBitmap(const SalTwoRect& rPosAry, const SalBitmap& 
     }
 
     // fall back old stuff
+    assert(dynamic_cast<const WinSalBitmap*>(&rSalBitmap));
+
     ImplDrawBitmap(mrParent.getHDC(), rPosAry, static_cast<const WinSalBitmap&>(rSalBitmap),
         mrParent.isPrinter(),
         mbXORMode ? SRCINVERT : SRCCOPY );
@@ -724,6 +726,9 @@ void WinSalGraphicsImpl::drawBitmap( const SalTwoRect& rPosAry,
     {
         return;
     }
+
+    assert(dynamic_cast<const WinSalBitmap*>(&rSSalBitmap));
+    assert(dynamic_cast<const WinSalBitmap*>(&rSTransparentBitmap));
 
     const WinSalBitmap& rSalBitmap = static_cast<const WinSalBitmap&>(rSSalBitmap);
     const WinSalBitmap& rTransparentBitmap = static_cast<const WinSalBitmap&>(rSTransparentBitmap);
@@ -826,6 +831,8 @@ void WinSalGraphicsImpl::drawMask( const SalTwoRect& rPosAry,
                             SalColor nMaskColor )
 {
     DBG_ASSERT( !mrParent.isPrinter(), "No transparency print possible!" );
+
+    assert(dynamic_cast<const WinSalBitmap*>(&rSSalBitmap));
 
     const WinSalBitmap& rSalBitmap = static_cast<const WinSalBitmap&>(rSSalBitmap);
 
@@ -2214,6 +2221,8 @@ bool WinSalGraphicsImpl::tryDrawBitmapGdiPlus(const SalTwoRect& rTR, const SalBi
 {
     if(rTR.mnSrcWidth && rTR.mnSrcHeight && rTR.mnDestWidth && rTR.mnDestHeight)
     {
+        assert(dynamic_cast<const WinSalBitmap*>(&rSrcBitmap));
+
         const WinSalBitmap& rSalBitmap = static_cast< const WinSalBitmap& >(rSrcBitmap);
         GdiPlusBmpPtr aARGB(rSalBitmap.ImplGetGdiPlusBitmap());
 
@@ -2263,6 +2272,9 @@ bool WinSalGraphicsImpl::drawAlphaBitmap(
 {
     if(rTR.mnSrcWidth && rTR.mnSrcHeight && rTR.mnDestWidth && rTR.mnDestHeight)
     {
+        assert(dynamic_cast<const WinSalBitmap*>(&rSrcBitmap));
+        assert(dynamic_cast<const WinSalBitmap*>(&rAlphaBmp));
+
         const WinSalBitmap& rSalBitmap = static_cast< const WinSalBitmap& >(rSrcBitmap);
         const WinSalBitmap& rSalAlpha = static_cast< const WinSalBitmap& >(rAlphaBmp);
         GdiPlusBmpPtr aARGB(rSalBitmap.ImplGetGdiPlusBitmap(&rSalAlpha));
@@ -2297,6 +2309,9 @@ bool WinSalGraphicsImpl::drawTransformedBitmap(
     const SalBitmap& rSourceBitmap,
     const SalBitmap* pAlphaBitmap)
 {
+    assert(dynamic_cast<const WinSalBitmap*>(&rSourceBitmap));
+    assert(dynamic_cast<const WinSalBitmap*>(&pAlphaBitmap));
+
     const WinSalBitmap& rSalBitmap = static_cast< const WinSalBitmap& >(rSourceBitmap);
     const WinSalBitmap* pSalAlpha = static_cast< const WinSalBitmap* >(pAlphaBitmap);
     GdiPlusBmpPtr aARGB(rSalBitmap.ImplGetGdiPlusBitmap(pSalAlpha));

vcl/opengl/gdiimpl.cxx
@@ -1665,6 +1665,9 @@ void OpenGLSalGraphicsImpl::drawBitmap(
             const SalBitmap& rSalBitmap,
             const SalBitmap& rMaskBitmap )
 {
+    assert(dynamic_cast<const OpenGLSalBitmap*>(&rSalBitmap));
+    assert(dynamic_cast<const OpenGLSalBitmap*>(&rMaskBitmap));
+
     const OpenGLSalBitmap& rBitmap = static_cast<const OpenGLSalBitmap&>(rSalBitmap);
     const OpenGLSalBitmap& rMask = static_cast<const OpenGLSalBitmap&>(rMaskBitmap);
     OpenGLTexture& rTexture( rBitmap.GetTexture() );
@@ -1681,6 +1684,7 @@ void OpenGLSalGraphicsImpl::drawMask(
             const SalBitmap& rSalBitmap,
             SalColor nMaskColor )
 {
+    assert(dynamic_cast<const OpenGLSalBitmap*>(&rSalBitmap));
     const OpenGLSalBitmap& rBitmap = static_cast<const OpenGLSalBitmap&>(rSalBitmap);
     OpenGLTexture& rTexture( rBitmap.GetTexture() );
 
@@ -1782,6 +1786,8 @@ bool OpenGLSalGraphicsImpl::blendBitmap(
             const SalTwoRect& rPosAry,
             const SalBitmap& rSalBitmap )
 {
+    assert(dynamic_cast<const OpenGLSalBitmap*>(&rSalBitmap));
+
     const OpenGLSalBitmap& rBitmap = static_cast<const OpenGLSalBitmap&>(rSalBitmap);
     OpenGLTexture& rTexture( rBitmap.GetTexture() );
 
@@ -1804,6 +1810,10 @@ bool OpenGLSalGraphicsImpl::blendAlphaBitmap(
             const SalBitmap& rSalMaskBitmap,
             const SalBitmap& rSalAlphaBitmap )
 {
+    assert(dynamic_cast<const OpenGLSalBitmap*>(&rSalSrcBitmap));
+    assert(dynamic_cast<const OpenGLSalBitmap*>(&rSalMaskBitmap));
+    assert(dynamic_cast<const OpenGLSalBitmap*>(&rSalAlphaBitmap));
+
     const OpenGLSalBitmap& rSrcBitmap = static_cast<const OpenGLSalBitmap&>(rSalSrcBitmap);
     const OpenGLSalBitmap& rMaskBitmap = static_cast<const OpenGLSalBitmap&>(rSalMaskBitmap);
     const OpenGLSalBitmap& rAlphaBitmap = static_cast<const OpenGLSalBitmap&>(rSalAlphaBitmap);
@@ -1835,6 +1845,9 @@ bool OpenGLSalGraphicsImpl::drawAlphaBitmap(
             const SalBitmap& rSalBitmap,
             const SalBitmap& rAlphaBitmap )
 {
+    assert(dynamic_cast<const OpenGLSalBitmap*>(&rSalBitmap));
+    assert(dynamic_cast<const OpenGLSalBitmap*>(&rAlphaBitmap));
+
     const OpenGLSalBitmap& rBitmap = static_cast<const OpenGLSalBitmap&>(rSalBitmap);
     const OpenGLSalBitmap& rAlpha = static_cast<const OpenGLSalBitmap&>(rAlphaBitmap);
     OpenGLTexture& rTexture( rBitmap.GetTexture() );
@@ -1855,6 +1868,9 @@ bool OpenGLSalGraphicsImpl::drawTransformedBitmap(
             const SalBitmap& rSrcBitmap,
             const SalBitmap* pAlphaBitmap)
 {
+    assert(dynamic_cast<const OpenGLSalBitmap*>(&rSrcBitmap));
+    assert(dynamic_cast<const OpenGLSalBitmap*>(pAlphaBitmap));
+
     const OpenGLSalBitmap& rBitmap = static_cast<const OpenGLSalBitmap&>(rSrcBitmap);
     const OpenGLSalBitmap* pMaskBitmap = static_cast<const OpenGLSalBitmap*>(pAlphaBitmap);
     OpenGLTexture& rTexture( rBitmap.GetTexture() );

commit 628229a24c2bbf8c1544a54d1a9fc09d5809e371

Blind fix for TB Win64Dbg
Regression from http://cgit.freedesktop.org/libreoffice/core/commit/?id=1321ae8ceba1801cd75322d6cf51ab53bac86b2a

Change-Id: Iba9e9560e34d9107787d117a7cfff82349b7f933

vcl/win/gdi/gdiimpl.cxx
@@ -2310,7 +2310,7 @@ bool WinSalGraphicsImpl::drawTransformedBitmap(
     const SalBitmap* pAlphaBitmap)
 {
     assert(dynamic_cast<const WinSalBitmap*>(&rSourceBitmap));
-    assert(dynamic_cast<const WinSalBitmap*>(&pAlphaBitmap));
+    assert(dynamic_cast<const WinSalBitmap*>(pAlphaBitmap));
 
     const WinSalBitmap& rSalBitmap = static_cast< const WinSalBitmap& >(rSourceBitmap);
     const WinSalBitmap* pSalAlpha = static_cast< const WinSalBitmap* >(pAlphaBitmap);