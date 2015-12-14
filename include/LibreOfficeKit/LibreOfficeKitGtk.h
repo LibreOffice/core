1af32e83d32f3d718293c424820b6d2abf14b447
Pranav Kant <pranavk@libreoffice.org>
2015-12-14 22:57:06 +0530

    lokdocview: New API: destroy_document()
    Change-Id: I7d2064605d5cec0fe26b73c591b2f1c695a24e1f

--- include/LibreOfficeKit/LibreOfficeKitGtk.h
+++ include/LibreOfficeKit/LibreOfficeKitGtk.h
@@ -273,6 +273,14 @@ gboolean                        lok_doc_view_paste                 (LOKDocView* 
                                                                     gsize nSize);
 
 /**
+ * lok_doc_view_destroy_document:
+ * @pDocView: The #LOKDocView instance
+ *
+ * Destroys all the tiles of the document
+ */
+void                            lok_doc_view_destroy_document       (LOKDocView* pDocView);
+
+/**
  * lok_doc_view_pixel_to_twip:
  * @pDocView: The #LOKDocView instance
  * @fInput: The value in pixels to convert to twips
