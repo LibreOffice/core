3bf4f65c4a5f86127eb626735422c60ff4f0e8c4
Maxim Monastirsky <momonasmon@gmail.com>
2015-12-16 14:09:48 +0200

    Clean unused defines after src->xml conversion
    ... in 1c035f1b0eb6f5e5fbb6c54bf2eea9a6c983eb8f
    
    Change-Id: I71271824b68ed21ac5db95b01fee61337fde189b

--- sc/AllLangResTarget_sc.mk
+++ sc/AllLangResTarget_sc.mk
@@ -47,8 +47,6 @@ $(eval $(call gb_SrsTarget_add_files,sc/res,\
     sc/source/ui/navipi/navipi.src \
     sc/source/ui/styleui/scstyles.src \
     sc/source/ui/dbgui/pvfundlg.src \
-    sc/source/ui/drawfunc/drformsh.src \
-    sc/source/ui/drawfunc/objdraw.src \
     sc/source/ui/miscdlgs/retypepassdlg.src \
     sc/source/ui/miscdlgs/conflictsdlg.src \
     sc/source/ui/miscdlgs/acredlin.src \
