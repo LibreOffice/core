392b682c9523311d70b0dd68c6b8eb28426d5602
aqcoder <flw.aquarius@gmail.com>
2015-12-17 21:22:34 +0800

    tdf#96412 Fix export text table cell UserDefinedAttributes to xml.
    It also export to different Fmt when only have the difference of
    UserDefinedAttributes for the text table cell.
    
    Change-Id: I022b627926870b883eea363567a34b61c34ab353

--- sw/qa/extras/inc/swmodeltestbase.hxx
+++ sw/qa/extras/inc/swmodeltestbase.hxx
@@ -11,6 +11,7 @@
 #define INCLUDED_SW_QA_EXTRAS_INC_SWMODELTESTBASE_HXX
 
 #include <com/sun/star/container/XContentEnumerationAccess.hpp>
+#include <com/sun/star/container/XNameAccess.hpp>
 #include <com/sun/star/frame/Desktop.hpp>
 #include <com/sun/star/packages/zip/ZipFileAccess.hpp>
 #include <com/sun/star/style/XStyleFamiliesSupplier.hpp>
@@ -27,6 +28,7 @@
 #include <com/sun/star/sdb/CommandType.hpp>
 #include <com/sun/star/sdb/DatabaseContext.hpp>
 #include <com/sun/star/sdb/XDocumentDataSource.hpp>
+#include <com/sun/star/xml/AttributeData.hpp>
 
 #include <test/bootstrapfixture.hxx>
 #include <test/xmltesttools.hxx>
@@ -429,6 +431,18 @@ protected:
     {
         uno::Reference<beans::XPropertySet> properties(obj, uno::UNO_QUERY_THROW);
         return properties->getPropertySetInfo()->hasPropertyByName(name);
+    }
+
+    xml::AttributeData getUserDefineAttribute(const uno::Any& obj, const OUString& name, const OUString& rValue = OUString()) const
+    {
+        uno::Reference<container::XNameContainer> attrsCnt(getProperty<uno::Any>(obj, "UserDefinedAttributes"), uno::UNO_QUERY_THROW);
+
+        xml::AttributeData aValue;
+        attrsCnt->getByName(name) >>= aValue;
+        if (!rValue.isEmpty())
+            CPPUNIT_ASSERT_EQUAL_MESSAGE("attribtes of cell does not contain expected value", rValue, aValue.Value);
+
+        return aValue;
     }
 
     /// Get number of paragraphs of the document.
