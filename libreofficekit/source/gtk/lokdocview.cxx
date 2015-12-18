4200a678fb54f0fa5d2f0c26c655252f9267a527
Pranav Kant <pranavk@libreoffice.org>
2015-12-18 22:42:53 +0530

    tdf#96421: Return if no window is realized
    Change-Id: I13de7e6eae4e73932e8441ba2e2ad3e4ff888f41
    Reviewed-on: https://gerrit.libreoffice.org/20799
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: David Tardon <dtardon@redhat.com>

--- libreofficekit/source/gtk/lokdocview.cxx
+++ libreofficekit/source/gtk/lokdocview.cxx
@@ -344,6 +344,8 @@ doSearch(LOKDocView* pDocView, const char* pText, bool bBackwards, bool highligh
     boost::property_tree::ptree aTree;
     GtkWidget* drawingWidget = GTK_WIDGET(pDocView);
     GdkWindow* drawingWindow = gtk_widget_get_window(drawingWidget);
+    if (!drawingWindow)
+        return;
     std::shared_ptr<cairo_region_t> cairoVisRegion( gdk_window_get_visible_region(drawingWindow),
                                                     cairo_region_destroy);
     cairo_rectangle_int_t cairoVisRect;
