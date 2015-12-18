c37340c8df8655c8789d0272b39988e1d7aa44de
Lionel Elie Mamane <lionel@mamane.lu>
2015-12-18 16:54:00 +0100

    tdf#96572 firebird correctly transmit request for NULL
    even if column is not nullable
    
    Conflicts:
    	connectivity/source/drivers/firebird/Util.cxx
    
    Change-Id: I72ceda68e983125aef26c8f0aacc06320bd16b77

--- connectivity/source/drivers/firebird/Util.cxx
+++ connectivity/source/drivers/firebird/Util.cxx
@@ -254,11 +254,8 @@ void firebird::mallocSQLVAR(XSQLDA* pSqlda)
             assert(false);
             break;
         }
-        if (pVar->sqltype & 1)
-        {
-            /* allocate variable to hold NULL status */
-            pVar->sqlind = static_cast<short *>(malloc(sizeof(short)));
-        }
+        /* allocate variable to hold NULL status */
+        pVar->sqlind = static_cast<short *>(malloc(sizeof(short)));
     }
 }
 
@@ -302,13 +299,10 @@ void firebird::freeSQLVAR(XSQLDA* pSqlda)
             break;
         }
 
-        if (pVar->sqltype & 1)
+        if(pVar->sqlind)
         {
-            if(pVar->sqlind)
-            {
-                free(pVar->sqlind);
-                pVar->sqlind = NULL;
-            }
+            free(pVar->sqlind);
+            pVar->sqlind = NULL;
         }
     }
 }
