e268810581a5a4d14d4264430b8894092998cbfb
burcinakalin <brcnakalin@gmail.com>
2015-12-23 19:51:09 +0200

    tdf#95416 Get rid of #include "../foo/bar.hxx" style includes
    Change-Id: Icbff7c5f596d4ec1918e0d7924972a0651dde811
    Signed-off-by: burcinakalin <brcnakalin@gmail.com>

--- dtrans/source/inc/WinClip.hxx
+++ dtrans/source/inc/WinClip.hxx
@@ -17,8 +17,8 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_DTRANS_SOURCE_WIN32_MISC_WINCLIP_HXX
-#define INCLUDED_DTRANS_SOURCE_WIN32_MISC_WINCLIP_HXX
+#ifndef INCLUDED_DTRANS_SOURCE_INC_WINCLIP_HXX
+#define INCLUDED_DTRANS_SOURCE_INC_WINCLIP_HXX
 
 #include <sal/types.h>
 
