d105910fb51346558a6909c447d579c8e548b00e
David Tardon <dtardon@redhat.com>
2015-12-24 22:04:58 +0100

    upload librevenge 0.0.3
    Change-Id: I8f2c09b70679aabb5e5f56334e6ba9eedd2d78e1
    (cherry picked from commit befb4af709c3d05719d2878968a36ece1cc87662)
    Reviewed-on: https://gerrit.libreoffice.org/20941
    Reviewed-by: Adolfo Jayme Barrientos <fitojb@ubuntu.com>
    Reviewed-by: Fridrich Strba <fridrich@documentfoundation.org>
    Tested-by: Fridrich Strba <fridrich@documentfoundation.org>

--- external/librevenge/ExternalProject_librevenge.mk
+++ external/librevenge/ExternalProject_librevenge.mk
@@ -34,8 +34,7 @@ $(call gb_ExternalProject_get_state_target,librevenge,build) :
 			--disable-generators \
 			--without-docs \
 			$(if $(verbose),--disable-silent-rules,--enable-silent-rules) \
-			CXXFLAGS="$(CXXFLAGS) $(if $(SYSTEM_BOOST),$(BOOST_CPPFLAGS),-I$(call gb_UnpackedTarball_get_dir,boost)) \
-				-DBOOST_ERROR_CODE_HEADER_ONLY -DBOOST_SYSTEM_NO_DEPRECATED" \
+			CXXFLAGS="$(CXXFLAGS) $(if $(SYSTEM_BOOST),$(BOOST_CPPFLAGS),-I$(call gb_UnpackedTarball_get_dir,boost))" \
 			$(if $(CROSS_COMPILING),--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM)) \
 			$(if $(filter MACOSX,$(OS)),--prefix=/@.__________________________________________________OOO) \
 		&& $(MAKE) \
