171393c7a497690b3c2a8614a6250ca9e3bf676d
Stephan Bergmann <sbergman@redhat.com>
2015-12-24 12:26:29 +0100

    external/libvisio: Avoid undefined behavior
    Change-Id: I454cf3b102986f2e3c1905f5fe0e1b2bb427828e

--- external/libvisio/UnpackedTarball_libvisio.mk
+++ external/libvisio/UnpackedTarball_libvisio.mk
@@ -13,6 +13,10 @@ $(eval $(call gb_UnpackedTarball_set_tarball,libvisio,$(VISIO_TARBALL)))
 
 $(eval $(call gb_UnpackedTarball_set_patchlevel,libvisio,0))
 
+$(eval $(call gb_UnpackedTarball_add_patches,libvisio, \
+    external/libvisio/ubsan.patch \
+))
+
 ifeq ($(COM_IS_CLANG),TRUE)
 ifneq ($(filter -fsanitize=%,$(CC)),)
 $(eval $(call gb_UnpackedTarball_add_patches,libvisio, \
