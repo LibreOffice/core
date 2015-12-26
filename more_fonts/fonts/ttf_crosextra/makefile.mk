534fa5c25363d7bf21e971a11c624ebcee0c1c2b
Pedro Giffuni <pfg@apache.org>
2015-12-26 23:49:16 +0000

    Bring new crosextra font: Caladea
    Caladea is a modern Latin typeface inspired by the contrast, style and
    ornaments of Khmer typefaces and writing styles. Its main objective is to
    be used to write Latin texts in a Khmer context, but it is also an elegant
    choice for all kinds of texts.
    
    Designed by Carolina Giovagnoli and Andres Torresi
    for Huerta Tipografica under an Apache 2.0 License.
    ____
    
    This is specially interesting as it is metrically compatible with
    MS Cambria.
    
    While here sort out some issues:
    - Sadly, we don't really carry an equivalent for "Arial Narrow".
    - There is not need for Arimo in the Windows platform.

---
+++ more_fonts/fonts/ttf_crosextra/makefile.mk
@@ -0,0 +1,61 @@
+#**************************************************************
+#  
+#  Licensed to the Apache Software Foundation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#  
+#    http://www.apache.org/licenses/LICENSE-2.0
+#  
+#  Unless required by applicable law or agreed to in writing,
+#  software distributed under the License is distributed on an
+#  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+#  KIND, either express or implied.  See the License for the
+#  specific language governing permissions and limitations
+#  under the License.
+#  
+#**************************************************************
+
+PRJ=..$/..
+
+PRJNAME=more_fonts
+TARGET=ttf_crosextra
+
+# --- Settings -----------------------------------------------------
+
+.INCLUDE :	settings.mk
+
+.IF "$(WITH_CATA_FONTS)" != "YES"
+
+@all:
+    @echo "category A licensed CrossCore font is disabled."
+
+.ELSE
+
+# --- Files --------------------------------------------------------
+
+TARFILE_NAME=crosextrafonts-20130214
+TARFILE_MD5=368f114c078f94214a308a74c7e991bc
+
+PATCH_FILES=
+
+CONFIGURE_DIR=
+CONFIGURE_ACTION=
+
+BUILD_DIR=
+BUILD_ACTION=
+BUILD_FLAGS=
+
+# --- Targets ------------------------------------------------------
+
+.INCLUDE : set_ext.mk
+.INCLUDE : target.mk
+
+.IF "$(WITH_FONTS)"!="NO"
+.INCLUDE : tg_ext.mk
+.ENDIF
+
+.ENDIF
