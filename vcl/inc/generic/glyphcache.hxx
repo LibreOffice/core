87584ed0f292e79669a74d2040d668f11fdc2c2b
Caolán McNamara <caolanm@redhat.com>
2015-12-30 13:01:35 +0000

    coverity#1346892 Non-virtual destructor
    Change-Id: Ie718df17cc5f12ebc244d3decca4aa7644f6b120

--- vcl/inc/generic/glyphcache.hxx
+++ vcl/inc/generic/glyphcache.hxx
@@ -55,7 +55,7 @@ class VCL_DLLPUBLIC GlyphCache
 {
 public:
     explicit                GlyphCache();
-                            ~GlyphCache();
+    virtual                 ~GlyphCache();
 
     static GlyphCache&      GetInstance();
 
