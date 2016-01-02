dfb73ede137c8232f749f701219d64b134e9e965
Chris Sherlock <chris.sherlock79@gmail.com>
2016-01-03 05:59:04 +1100

    vcl: indent cleanup for outfont.hxx
    Change-Id: Ia35f2ec1b27373243736172f1d91c03bb2622c64

--- vcl/inc/outfont.hxx
+++ vcl/inc/outfont.hxx
@@ -104,35 +104,35 @@ public:
 
 private:
     // device independent variables
-    OUString        maFamilyName;       // Font Family Name
-    OUString        maStyleName;        // Font Style Name
-    FontWeight      meWeight;           // Weight Type
-    FontItalic      meItalic;           // Slant Type
-    FontFamily      meFamily;           // Family Type
-    FontPitch       mePitch;            // Pitch Type
-    FontWidth       meWidthType;        // Width Type
-    bool            mbSymbolFlag;       // Is font a symbol?
+    OUString        maFamilyName;               // Font Family Name
+    OUString        maStyleName;                // Font Style Name
+    FontWeight      meWeight;                   // Weight Type
+    FontItalic      meItalic;                   // Slant Type
+    FontFamily      meFamily;                   // Family Type
+    FontPitch       mePitch;                    // Pitch Type
+    FontWidth       meWidthType;                // Width Type
+    bool            mbSymbolFlag;               // Is font a symbol?
 
     // device dependent variables
-    OUString        maMapNames;         // List of family name aliases separated with ';'
-    int             mnQuality;          // Quality (used when similar fonts compete)
-    bool            mbOrientation;      // true: physical font can be rotated
-    bool            mbDevice;           // true: built in font
-    bool            mbSubsettable;      // true: a subset of the font can be created
-    bool            mbEmbeddable;       // true: the font can be embedded
+    OUString        maMapNames;                 // List of family name aliases separated with ';'
+    int             mnQuality;                  // Quality (used when similar fonts compete)
+    bool            mbOrientation;              // true: physical font can be rotated
+    bool            mbDevice;                   // true: built in font
+    bool            mbSubsettable;              // true: a subset of the font can be created
+    bool            mbEmbeddable;               // true: the font can be embedded
 };
 
 class FontSelectPatternAttributes : public ImplFontAttributes
 {
 public:
-                        FontSelectPatternAttributes( const vcl::Font&, const OUString& rSearchName,
-                            const Size&, float fExactHeight );
+                    FontSelectPatternAttributes( const vcl::Font&, const OUString& rSearchName,
+                                                 const Size&, float fExactHeight );
 #ifdef WNT
-                        FontSelectPatternAttributes( const PhysicalFontFace&, const Size&,
-                            float fExactHeight, int nOrientation, bool bVertical );
+                    FontSelectPatternAttributes( const PhysicalFontFace&, const Size&,
+                                                 float fExactHeight, int nOrientation, bool bVertical );
 #endif
 
-    size_t              hashCode() const;
+    size_t          hashCode() const;
     bool operator==(const FontSelectPatternAttributes& rOther) const;
     bool operator!=(const FontSelectPatternAttributes& rOther) const
     {
@@ -140,35 +140,36 @@ public:
     }
 
 public:
-    OUString            maTargetName;       // name of the font name token that is chosen
-    OUString            maSearchName;       // name of the font that matches best
-    int                 mnWidth;            // width of font in pixel units
-    int                 mnHeight;           // height of font in pixel units
-    float               mfExactHeight;      // requested height (in pixels with subpixel details)
-    int                 mnOrientation;      // text orientation in 3600 system
-    LanguageType        meLanguage;         // text language
-    bool                mbVertical;         // vertical mode of requested font
-    bool                mbNonAntialiased;   // true if antialiasing is disabled
+    OUString        maTargetName;               // name of the font name token that is chosen
+    OUString        maSearchName;               // name of the font that matches best
+    int             mnWidth;                    // width of font in pixel units
+    int             mnHeight;                   // height of font in pixel units
+    float           mfExactHeight;              // requested height (in pixels with subpixel details)
+    int             mnOrientation;              // text orientation in 3600 system
+    LanguageType    meLanguage;                 // text language
+    bool            mbVertical;                 // vertical mode of requested font
+    bool            mbNonAntialiased;           // true if antialiasing is disabled
 
-    bool                mbEmbolden;         // Force emboldening
-    ItalicMatrix        maItalicMatrix;     // Force matrix for slant
+    bool            mbEmbolden;                 // Force emboldening
+    ItalicMatrix    maItalicMatrix;             // Force matrix for slant
 };
 
 class FontSelectPattern : public FontSelectPatternAttributes
 {
 public:
-                        FontSelectPattern( const vcl::Font&, const OUString& rSearchName,
-                            const Size&, float fExactHeight );
+                    FontSelectPattern( const vcl::Font&, const OUString& rSearchName,
+                                       const Size&, float fExactHeight );
 #ifdef WNT
 // ifdeffed to prevent it going into unusedcode.easy
-                        FontSelectPattern( const PhysicalFontFace&, const Size&,
-                            float fExactHeight, int nOrientation, bool bVertical );
+                    FontSelectPattern( const PhysicalFontFace&, const Size&,
+                                       float fExactHeight, int nOrientation, bool bVertical );
 #endif
 
 public: // TODO: change to private
     const PhysicalFontFace* mpFontData;         // a matching PhysicalFontFace object
-    ImplFontEntry*      mpFontEntry;        // pointer to the resulting FontCache entry
-    void copyAttributes(const FontSelectPatternAttributes &rAttributes);
+    ImplFontEntry*  mpFontEntry;                // pointer to the resulting FontCache entry
+
+    void            copyAttributes(const FontSelectPatternAttributes &rAttributes);
 };
 
 // - ImplFontMetricData -
@@ -176,58 +177,59 @@ public: // TODO: change to private
 class ImplFontMetricData : public ImplFontAttributes
 {
 public:
-    explicit ImplFontMetricData( const FontSelectPattern& );
-    void    ImplInitTextLineSize( const OutputDevice* pDev );
-    void    ImplInitAboveTextLineSize();
+    explicit        ImplFontMetricData( const FontSelectPattern& );
+
+    void            ImplInitTextLineSize( const OutputDevice* pDev );
+    void            ImplInitAboveTextLineSize();
 
 public: // TODO: hide members behind accessor methods
     // font instance attributes from the font request
-    long                mnWidth;                    // Reference Width
-    short               mnOrientation;              // Rotation in 1/10 degrees
+    long            mnWidth;                    // Reference Width
+    short           mnOrientation;              // Rotation in 1/10 degrees
 
     // font metrics measured for the font instance
-    long                mnAscent;                   // Ascent
-    long                mnDescent;                  // Descent
-    long                mnIntLeading;               // Internal Leading
-    long                mnExtLeading;               // External Leading
-    int                 mnSlant;                    // Slant (Italic/Oblique)
-    long                mnMinKashida;               // Minimal width of kashida (Arabic)
+    long            mnAscent;                   // Ascent
+    long            mnDescent;                  // Descent
+    long            mnIntLeading;               // Internal Leading
+    long            mnExtLeading;               // External Leading
+    int             mnSlant;                    // Slant (Italic/Oblique)
+    long            mnMinKashida;               // Minimal width of kashida (Arabic)
 
     // font attributes queried from the font instance
-    int                 meFamilyType;               // Font Family Type
-    bool                mbDevice;                   // Flag for Device Fonts
-    bool                mbScalableFont;
-    bool                mbTrueTypeFont;
-    bool                mbKernableFont;
-    bool                mbFullstopCentered;
-    long                mnBulletOffset;             // Offset to position non-print character
+    int             meFamilyType;               // Font Family Type
+    bool            mbDevice;                   // Flag for Device Fonts
+    bool            mbScalableFont;
+    bool            mbTrueTypeFont;
+    bool            mbKernableFont;
+    bool            mbFullstopCentered;
+    long            mnBulletOffset;             // Offset to position non-print character
 
     // font metrics that are usually derived from the measurements
-    long                mnUnderlineSize;            // Lineheight of Underline
-    long                mnUnderlineOffset;          // Offset from Underline to Baseline
-    long                mnBUnderlineSize;           // Height of bold underline
-    long                mnBUnderlineOffset;         // Offset from bold underline to baseline
-    long                mnDUnderlineSize;           // Height of double underline
-    long                mnDUnderlineOffset1;        // Offset from double underline to baseline
-    long                mnDUnderlineOffset2;        // Offset from double underline to baseline
-    long                mnWUnderlineSize;           // Height of WaveLine underline
-    long                mnWUnderlineOffset;         // Offset from WaveLine underline to baseline, but centrered to WaveLine
-    long                mnAboveUnderlineSize;       // Height of single underline (for Vertical Right)
-    long                mnAboveUnderlineOffset;     // Offset from single underline to baseline (for Vertical Right)
-    long                mnAboveBUnderlineSize;      // Height of bold underline (for Vertical Right)
-    long                mnAboveBUnderlineOffset;    // Offset from bold underline to baseline (for Vertical Right)
-    long                mnAboveDUnderlineSize;      // Height of double underline (for Vertical Right)
-    long                mnAboveDUnderlineOffset1;   // Offset from double underline to baseline (for Vertical Right)
-    long                mnAboveDUnderlineOffset2;   // Offset from double underline to baseline (for Vertical Right)
-    long                mnAboveWUnderlineSize;      // Height of WaveLine-strike-out (for Vertical Right)
-    long                mnAboveWUnderlineOffset;    // Offset from WaveLine-strike-out to baseline, but centrered to the WaveLine (for Vertical Right)
-    long                mnStrikeoutSize;            // Height of single strike-out
-    long                mnStrikeoutOffset;          // Offset from single strike-out to baseline
-    long                mnBStrikeoutSize;           // Height of bold strike-out
-    long                mnBStrikeoutOffset;         // Offset of bold strike-out to baseline
-    long                mnDStrikeoutSize;           // Height of double strike-out
-    long                mnDStrikeoutOffset1;        // Offset of double strike-out to baseline
-    long                mnDStrikeoutOffset2;        // Offset of double strike-out to baseline
+    long            mnUnderlineSize;            // Lineheight of Underline
+    long            mnUnderlineOffset;          // Offset from Underline to Baseline
+    long            mnBUnderlineSize;           // Height of bold underline
+    long            mnBUnderlineOffset;         // Offset from bold underline to baseline
+    long            mnDUnderlineSize;           // Height of double underline
+    long            mnDUnderlineOffset1;        // Offset from double underline to baseline
+    long            mnDUnderlineOffset2;        // Offset from double underline to baseline
+    long            mnWUnderlineSize;           // Height of WaveLine underline
+    long            mnWUnderlineOffset;         // Offset from WaveLine underline to baseline, but centrered to WaveLine
+    long            mnAboveUnderlineSize;       // Height of single underline (for Vertical Right)
+    long            mnAboveUnderlineOffset;     // Offset from single underline to baseline (for Vertical Right)
+    long            mnAboveBUnderlineSize;      // Height of bold underline (for Vertical Right)
+    long            mnAboveBUnderlineOffset;    // Offset from bold underline to baseline (for Vertical Right)
+    long            mnAboveDUnderlineSize;      // Height of double underline (for Vertical Right)
+    long            mnAboveDUnderlineOffset1;   // Offset from double underline to baseline (for Vertical Right)
+    long            mnAboveDUnderlineOffset2;   // Offset from double underline to baseline (for Vertical Right)
+    long            mnAboveWUnderlineSize;      // Height of WaveLine-strike-out (for Vertical Right)
+    long            mnAboveWUnderlineOffset;    // Offset from WaveLine-strike-out to baseline, but centrered to the WaveLine (for Vertical Right)
+    long            mnStrikeoutSize;            // Height of single strike-out
+    long            mnStrikeoutOffset;          // Offset from single strike-out to baseline
+    long            mnBStrikeoutSize;           // Height of bold strike-out
+    long            mnBStrikeoutOffset;         // Offset of bold strike-out to baseline
+    long            mnDStrikeoutSize;           // Height of double strike-out
+    long            mnDStrikeoutOffset1;        // Offset of double strike-out to baseline
+    long            mnDStrikeoutOffset2;        // Offset of double strike-out to baseline
 };
 
 // - ImplFontEntry -
@@ -238,24 +240,25 @@ public: // TODO: hide members behind accessor methods
 class VCL_PLUGIN_PUBLIC ImplFontEntry
 {
 public:
-    explicit            ImplFontEntry( const FontSelectPattern& );
-    virtual             ~ImplFontEntry();
+    explicit        ImplFontEntry( const FontSelectPattern& );
+    virtual         ~ImplFontEntry();
 
 public: // TODO: make data members private
     ImplFontCache * m_pFontCache;
-    FontSelectPattern  maFontSelData;      // FontSelectionData
-    ImplFontMetricData  maMetric;           // Font Metric
-    const ConvertChar*  mpConversion;       // used e.g. for StarBats->StarSymbol
-    long                mnLineHeight;
-    sal_uLong               mnRefCount;
-    sal_uInt16              mnSetFontFlags;     // Flags returned by SalGraphics::SetFont()
-    short               mnOwnOrientation;   // text angle if lower layers don't rotate text themselves
-    short               mnOrientation;      // text angle in 3600 system
-    bool                mbInit;             // true if maMetric member is valid
+    FontSelectPattern  maFontSelData;           // FontSelectionData
+    ImplFontMetricData  maMetric;               // Font Metric
+    const ConvertChar*  mpConversion;           // used e.g. for StarBats->StarSymbol
 
-    void                AddFallbackForUnicode( sal_UCS4, FontWeight eWeight, const OUString& rFontName );
-    bool                GetFallbackForUnicode( sal_UCS4, FontWeight eWeight, OUString* pFontName ) const;
-    void                IgnoreFallbackForUnicode( sal_UCS4, FontWeight eWeight, const OUString& rFontName );
+    long            mnLineHeight;
+    sal_uLong       mnRefCount;
+    sal_uInt16      mnSetFontFlags;             // Flags returned by SalGraphics::SetFont()
+    short           mnOwnOrientation;           // text angle if lower layers don't rotate text themselves
+    short           mnOrientation;              // text angle in 3600 system
+    bool            mbInit;                     // true if maMetric member is valid
+
+    void            AddFallbackForUnicode( sal_UCS4, FontWeight eWeight, const OUString& rFontName );
+    bool            GetFallbackForUnicode( sal_UCS4, FontWeight eWeight, OUString* pFontName ) const;
+    void            IgnoreFallbackForUnicode( sal_UCS4, FontWeight eWeight, const OUString& rFontName );
 
 private:
     // cache of Unicode characters and replacement font names
@@ -275,17 +278,17 @@ private:
     sal_Int32   mnLen;
 
 public:
-               ImplTextLineInfo( long nWidth, sal_Int32 nIndex, sal_Int32 nLen )
-               {
-                   if(nIndex == -1 || nIndex == 0x0FFFF || nLen == -1 || nLen == 0x0FFFF)
-                   {
-                        SAL_INFO("sal.rtl.xub",
-                                 "ImplTextLine Info Suspicious arguments nIndex:" << nIndex << " nLen:" << nLen);
-                   }
-                   mnWidth = nWidth;
-                   mnIndex = nIndex;
-                   mnLen   = nLen;
-               }
+   ImplTextLineInfo( long nWidth, sal_Int32 nIndex, sal_Int32 nLen )
+   {
+       if(nIndex == -1 || nIndex == 0x0FFFF || nLen == -1 || nLen == 0x0FFFF)
+       {
+            SAL_INFO("sal.rtl.xub",
+                     "ImplTextLine Info Suspicious arguments nIndex:" << nIndex << " nLen:" << nLen);
+       }
+       mnWidth = nWidth;
+       mnIndex = nIndex;
+       mnLen   = nLen;
+   }
 
     long       GetWidth() const { return mnWidth; }
     sal_Int32  GetIndex() const { return mnIndex; }
@@ -296,25 +299,25 @@ public:
 
 class ImplMultiTextLineInfo
 {
-private:
-    ImplTextLineInfo**  mpLines;
-    sal_Int32           mnLines;
-    sal_Int32           mnSize;
-
 public:
-                        ImplMultiTextLineInfo();
-                        ~ImplMultiTextLineInfo();
+                ImplMultiTextLineInfo();
+                ~ImplMultiTextLineInfo();
 
-    void                AddLine( ImplTextLineInfo* pLine );
-    void                Clear();
+    void        AddLine( ImplTextLineInfo* pLine );
+    void        Clear();
 
-    ImplTextLineInfo*   GetLine( sal_Int32 nLine ) const
+    ImplTextLineInfo* GetLine( sal_Int32 nLine ) const
                             { return mpLines[nLine]; }
-    sal_Int32           Count() const { return mnLines; }
+    sal_Int32   Count() const { return mnLines; }
 
 private:
-                            ImplMultiTextLineInfo( const ImplMultiTextLineInfo& ) = delete;
-    ImplMultiTextLineInfo&  operator=( const ImplMultiTextLineInfo& ) = delete;
+    ImplMultiTextLineInfo( const ImplMultiTextLineInfo& ) = delete;
+    ImplMultiTextLineInfo& operator=( const ImplMultiTextLineInfo& ) = delete;
+
+    ImplTextLineInfo**  mpLines;
+    sal_Int32   mnLines;
+    sal_Int32   mnSize;
+
 };
 
 #endif // INCLUDED_VCL_INC_OUTFONT_HXX
