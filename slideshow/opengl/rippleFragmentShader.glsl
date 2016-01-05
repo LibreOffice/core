97e2cc532644a1a7cbbae049f37bc3ac098e94ac
Emmanuel Gil Peyrot <emmanuel.peyrot@collabora.com>
2016-01-05 15:19:11 +0100

    OpenGL transitions: squashed 5 commits into this one
    vcl: Ignore i965’s shader compiler debug
    
    (cherry picked from commit 928fe134ff6ea85f732b36a1ab11336f1d829531)
    
    slideshow: Fix a few issues in the Glitter transition
    
    Remove an unused variable, add comments, reduce the time a tile stays
    black, and don’t rely on implicit casts of integers into floats.
    
    (cherry picked from commit 22480b20130d10f4691cdf0a658040be7f36e47b)
    
    slideshow: Improve the Ripple transition to match PowerPoint better
    
    (cherry picked from commit 1d411cad5a7d78ead8cffb5da522f1e0fba31187)
    
    slideshow: Improve the Vortex transition to match PowerPoint better
    
    (cherry picked from commit d886fef25c5978dd4b07fa0e3ce2402874a6c29a)
    
    slideshow: Define inverse() to bring back the GLSL version to 1.20
    
    (cherry picked from commit a301da7cb6c562cd21983d9b3b34dc01235a82a5)
    
    Change-Id: Ifd33ae982b762af3ea8d88b2b2de259aabeebc9a
    Reviewed-on: https://gerrit.libreoffice.org/21116
    Reviewed-by: Tor Lillqvist <tml@collabora.com>
    Tested-by: Tor Lillqvist <tml@collabora.com>

--- slideshow/opengl/rippleFragmentShader.glsl
+++ slideshow/opengl/rippleFragmentShader.glsl
@@ -15,21 +15,43 @@ uniform sampler2D leavingSlideTexture;
 uniform sampler2D enteringSlideTexture;
 uniform float time;
 uniform vec2 center;
+uniform float slideRatio;
+
 varying vec2 v_texturePosition;
+
+// This function returns the distance between two points, taking into account the slide ratio.
+float betterDistance(vec2 p1, vec2 p2)
+{
+    p1.x *= slideRatio;
+    p2.x *= slideRatio;
+    return distance(p1, p2);
+}
 
 void main()
 {
-    float d = length(v_texturePosition - center);
-    float w = 0;
-    w = max(w, length(center - vec2(0, 0)));
-    w = max(w, length(center - vec2(1, 0)));
-    w = max(w, length(center - vec2(1, 1)));
-    w = max(w, length(center - vec2(0, 1)));
-    float v = 0.2;
-    float smoothtime = smoothstep(0, 1, time);
-    float a = smoothstep(smoothtime*w-v, smoothtime*w+v, d);
-    a += (0.5 - abs(a-0.5))*sin(d*M_PI*30);
-    gl_FragColor = mix(texture2D(enteringSlideTexture, v_texturePosition), texture2D(leavingSlideTexture, v_texturePosition), a);
+    const float w = 0.5;
+    const float v = 0.1;
+
+    // Distance from this fragment to the center, in slide coordinates.
+    float dist = betterDistance(center, v_texturePosition);
+
+    // We want the ripple to span all of the slide at the end of the transition.
+    float t = time * (sqrt(2.0) * (slideRatio < 1.0 ? 1.0 / slideRatio : slideRatio));
+
+    // Interpolate the distance to the center in fonction of the time.
+    float mixed = smoothstep(t*w-v, t*w+v, dist);
+
+    // Get the displacement offset from the current pixel, for fragments that have been touched by the ripple already.
+    vec2 offset = (v_texturePosition - center) * (sin(dist * 64.0 - time * 16.0) + 0.5) / 32.0;
+    vec2 wavyTexCoord = mix(v_texturePosition + offset, v_texturePosition, time);
+
+    // Get the final position we will sample from.
+    vec2 pos = mix(wavyTexCoord, v_texturePosition, mixed);
+
+    // Sample from the textures and mix that together.
+    vec4 leaving = texture2D(leavingSlideTexture, pos);
+    vec4 entering = texture2D(enteringSlideTexture, pos);
+    gl_FragColor = mix(entering, leaving, mixed);
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
--- slideshow/opengl/vortexVertexShader.glsl
+++ slideshow/opengl/vortexVertexShader.glsl
@@ -28,11 +28,71 @@ uniform float time;
 uniform ivec2 numTiles;
 uniform sampler2D permTexture;
 attribute float tileInfo;
-varying float v_textureSelect;
+uniform float slide;
+
+varying vec4 debug;
+
+#if __VERSION__ < 140
+mat4 inverse(mat4 m)
+{
+    float a00 = m[0][0], a01 = m[0][1], a02 = m[0][2], a03 = m[0][3];
+    float a10 = m[1][0], a11 = m[1][1], a12 = m[1][2], a13 = m[1][3];
+    float a20 = m[2][0], a21 = m[2][1], a22 = m[2][2], a23 = m[2][3];
+    float a30 = m[3][0], a31 = m[3][1], a32 = m[3][2], a33 = m[3][3];
+
+    float b00 = a00 * a11 - a01 * a10;
+    float b01 = a00 * a12 - a02 * a10;
+    float b02 = a00 * a13 - a03 * a10;
+    float b03 = a01 * a12 - a02 * a11;
+    float b04 = a01 * a13 - a03 * a11;
+    float b05 = a02 * a13 - a03 * a12;
+    float b06 = a20 * a31 - a21 * a30;
+    float b07 = a20 * a32 - a22 * a30;
+    float b08 = a20 * a33 - a23 * a30;
+    float b09 = a21 * a32 - a22 * a31;
+    float b10 = a21 * a33 - a23 * a31;
+    float b11 = a22 * a33 - a23 * a32;
+
+    float det = b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06;
+
+    return mat4(
+        a11 * b11 - a12 * b10 + a13 * b09,
+        a02 * b10 - a01 * b11 - a03 * b09,
+        a31 * b05 - a32 * b04 + a33 * b03,
+        a22 * b04 - a21 * b05 - a23 * b03,
+        a12 * b08 - a10 * b11 - a13 * b07,
+        a00 * b11 - a02 * b08 + a03 * b07,
+        a32 * b02 - a30 * b05 - a33 * b01,
+        a20 * b05 - a22 * b02 + a23 * b01,
+        a10 * b10 - a11 * b08 + a13 * b06,
+        a01 * b08 - a00 * b10 - a03 * b06,
+        a30 * b04 - a31 * b02 + a33 * b00,
+        a21 * b02 - a20 * b04 - a23 * b00,
+        a11 * b07 - a10 * b09 - a12 * b06,
+        a00 * b09 - a01 * b07 + a02 * b06,
+        a31 * b01 - a30 * b03 - a32 * b00,
+        a20 * b03 - a21 * b01 + a22 * b00) / det;
+}
+#endif
 
 float snoise(vec2 p)
 {
     return texture2D(permTexture, p).r;
+}
+
+mat4 identityMatrix(void)
+{
+    return mat4(1.0, 0.0, 0.0, 0.0,
+                0.0, 1.0, 0.0, 0.0,
+                0.0, 0.0, 1.0, 0.0,
+                0.0, 0.0, 0.0, 1.0);
+}
+
+mat4 translationMatrix(vec3 axis)
+{
+    mat4 matrix = identityMatrix();
+    matrix[3] = vec4(axis, 1.0);
+    return matrix;
 }
 
 mat4 rotationMatrix(vec3 axis, float angle)
@@ -53,13 +113,6 @@ void main( void )
     vec4 v = vec4(a_position, 1.0);
     vec4 normal = vec4(a_normal, 1.0);
 
-    // Not sure it this is like what it should eventually be; just
-    // experimenting to get at least something.
-
-    // Move the tile on a semicircular path so that it will end up at
-    // the correct place. All tiles move the same direction around the
-    // vertical centre axis.
-
     // Each tile moves during only half of the transition. The letmost
     // tiles start moving at the start and arrive at their end
     // position around time=0.5, when the tiles there (the rightmost
@@ -70,48 +123,68 @@ void main( void )
 
     int tileXIndex = int(mod(int(tileInfo), 256));
     int tileYIndex = int(mod(int(tileInfo) / 256, 256));
-    int vertexIndexInTile = int(mod(int(tileInfo) / (256*256), 256));
 
-    // A semi-random number 0..1, different for neighbouring tiles.
-    float fuzz = snoise(vec2(float(tileXIndex)/(numTiles.x-1), float(tileYIndex)/(numTiles.y-1)));
+    // A semi-random number 0..1, different for neighbouring tiles, to know when they should start moving.
+    float startTimeFuzz = snoise(vec2(float(tileXIndex)/(numTiles.x-1), float(tileYIndex)/(numTiles.y-1)));
 
-    // Semi-random rotation direction, identical for tiles that rotate into each other's location
-    // so that they don't pass through each others in flight, which looks ugly.
-    float direction = (snoise(vec2(floor(abs(float(numTiles.x-1)/2-tileXIndex))/(float(numTiles.x-1)/2), float(tileYIndex)/(numTiles.y-1))) < 0.5 ? -1 : 1);
+    // A semi-random number -1.5..1.5, different for neighbouring tiles, to specify their rotation center.
+    // The additional 0.5 on each side is because we want some tiles to rotate outside.
+    float rotationFuzz = snoise(vec2(float(numTiles.x + tileXIndex)/(numTiles.x-1), float(tileYIndex)/(numTiles.y-1))) * 3.0 - 1.5;
 
-    float startTime = float(tileXIndex)/(numTiles.x-1) * 0.5 + fuzz*0.2;
-    float endTime = min(startTime + 0.5, 1.0);
+    float startTime = float(tileXIndex)/(numTiles.x-1) * 0.2 + startTimeFuzz * 0.2;
+    float endTime = min(startTime + 0.7, 1.0);
 
-    const float ALMOST_ONE = 0.999;
+    bool isLeavingSlide = (slide < 0.5);
+    const vec4 invalidPosition = vec4(-256.0, -256.0, -256.0, -256.0);
 
-    if (time <= startTime)
+    float nTime;
+
+    // Don’t display the tile before or after its rotation, depending on the slide.
+    if (!isLeavingSlide)
     {
-        // Still at start location, nothing needed
-        v_textureSelect = 0;
-    }
-    else if (time > startTime && time <= endTime)
-    {
-        // Moving
-        float rotation = direction * (time - startTime) / (endTime - startTime);
-
-        // Avoid z fighting
-        mat4 matrix = rotationMatrix(vec3(0, 1, 0), max(min(rotation, ALMOST_ONE), -ALMOST_ONE)*M_PI);
-        v = matrix * v;
-        normal = matrix * normal;
-
-        v_textureSelect = float(rotation > 0.5 || rotation < -0.5);
+        if (time < max(0.3, startTime))
+        {
+            gl_Position = invalidPosition;
+            return;
+        }
+        nTime = 1.0 - time;
     }
     else
     {
-        // At end location. Tile is 180 degrees rotated
-
-        // Avoid z fighting
-        mat4 matrix = rotationMatrix(vec3(0, 1, 0), direction*ALMOST_ONE*M_PI);
-        v = matrix * v;
-        normal = matrix * normal;
-
-        v_textureSelect = 1;
+        if (time > endTime)
+        {
+            gl_Position = invalidPosition;
+            return;
+        }
+        nTime = time;
     }
+
+    mat4 transform = identityMatrix();
+    if (nTime > startTime && nTime <= endTime)
+    {
+        // We are in the rotation part.
+        float rotation = -(nTime - startTime) / (endTime - startTime);
+
+        // Translation vector to set the origin of the rotation.
+        vec3 translationVector = vec3(rotationFuzz, 0.0, 0.0);
+
+        // Compute the actual rotation matrix.
+        transform = translationMatrix(translationVector)
+                  * rotationMatrix(vec3(0, 1, 0), clamp(rotation, -1.0, 1.0) * M_PI)
+                  * translationMatrix(-translationVector)
+                  * transform;
+
+        // Add a translation movement to the leaving slide so it doesn’t exactly mirror the entering one.
+        if (isLeavingSlide && nTime > 0.3)
+        {
+            float movement = smoothstep(0.0, 1.0, (nTime - 0.3) * 2.0);
+            transform = translationMatrix(vec3(-movement, 0.0, -0.5 * movement)) * transform;
+        }
+    }
+
+    // Apply our transform operations.
+    v = transform * v;
+    normal = transform * normal;
 
     mat4 modelViewMatrix = u_modelViewMatrix * u_operationsTransformMatrix * u_sceneTransformMatrix * u_primitiveTransformMatrix;
     mat3 normalMatrix = mat3(transpose(inverse(modelViewMatrix)));
