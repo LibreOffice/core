78ad8e11810154ca94215af74657b9c7ec3fa394
Tor Lillqvist <tml@collabora.com>
2016-01-07 15:05:58 +0200

    tdf#96420: Re-introduce SimpleWinLayout
    Fixes lots of problems with glyph layout. For instance the Cambria
    case in tdf#95648. The UniscribeLayout class turned out to be much
    less correct than one might have hoped. 
    
    Use OpenGL glyph caching also for SimpleWinLayout to avoid performance
    decrease.
    
    Corresponding commit in master is 4622689fad7ddff72cd08da9611ccfacdb0aa7bd
    
    Change-Id: Ie3c00304ce58f84c7f54051f4c230b32bfc47d9f

--- vcl/win/source/gdi/salgdi.cxx
+++ vcl/win/source/gdi/salgdi.cxx
@@ -631,6 +631,9 @@ WinSalGraphics::WinSalGraphics(WinSalGraphics::Type eType, bool bScreen, HWND hW
     mhDefPal(0),
     mpStdClipRgnData(NULL),
     mpFontAttrCache(NULL),
+    mpFontKernPairs(NULL),
+    mnFontKernPairCount(0),
+    mbFontKernInit(false),
     mnPenWidth(GSL_PEN_WIDTH)
 {
     if (OpenGLHelper::isVCLOpenGLEnabled() && !mbPrinter)
@@ -660,6 +663,8 @@ WinSalGraphics::~WinSalGraphics()
 
     // delete cache data
     delete [] (BYTE*)mpStdClipRgnData;
+
+    delete [] mpFontKernPairs;
 }
 
 SalGraphicsImpl* WinSalGraphics::GetImpl() const
