bc9879b0f1c35211e3df045c522b8e5e1ead0fd1
Chris Sherlock <chris.sherlock79@gmail.com>
2016-01-08 17:19:09 +1100

    vcl: should be using @tparam for template parameters
    Change-Id: I650448b4c24173a88b594185c2332861288ec753

--- include/vcl/vclptr.hxx
+++ include/vcl/vclptr.hxx
@@ -124,7 +124,7 @@ public:
      *
      * For more details on the design please see vcl/README.lifecycle
      *
-     * @param reference_type must be a subclass of vcl::Window
+     * @tparam reference_type must be a subclass of vcl::Window
      */
     template<typename... Arg> static VclPtr< reference_type > Create(Arg &&... arg)
     {
