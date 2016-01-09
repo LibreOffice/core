8a5aecb804212557c2a970d209ea440b2e46d00e
Chris Sherlock <chris.sherlock79@gmail.com>
2016-01-09 14:41:15 +1100

    vcl: move some Freetype stuff away from the "generic" folder
    Freetype is definitely not "generic" code. It is only used for Unix
    platforms, but is not used for OS X which has it's own font/glyph/text
    system called CoreText and that is implemented elsewhere in our
    codebase.
    
    Change-Id: I30b2756d2cd9a81b6f746988685295d25ba1de44

--- vcl/generic/glyphs/gcach_layout.cxx
+++ vcl/generic/glyphs/gcach_layout.cxx
@@ -17,7 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#include <gcach_ftyp.hxx>
+#include "unx/gcach_ftyp.hxx"
 #include <sallayout.hxx>
 #include <salgdi.hxx>
 #include <scrptrun.h>
