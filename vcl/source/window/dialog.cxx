77654b9cf774d67f0fc62d8d80f90d928af43c01
Ras-al-Ghul <dipankar1995@gmail.com>
2016-01-09 19:05:30 +0530

    tdf#96888 Kill internal vcl dog-tags ..
    Corrected a typo
    
    Change-Id: Icc8ce940a6f97570d37389530f9fd90890ae8055

--- vcl/source/window/dialog.cxx
+++ vcl/source/window/dialog.cxx
@@ -865,10 +865,6 @@ short Dialog::Execute()
 
     VclPtr<vcl::Window> xWindow = this;
 
-#ifdef DBG_UTIL
-    VclPtr<vcl::Window> xDialogParent = mpDialogParent;
-#endif
-
     // Yield util EndDialog is called or dialog gets destroyed
     // (the latter should not happen, but better safe than sorry
     while ( !xWindow->IsDisposed() && mbInExecute )
@@ -877,10 +873,10 @@ short Dialog::Execute()
     ImplEndExecuteModal();
 
 #ifdef DBG_UTIL
-    if( xDialogParent  )
+    if( mpDialogParent  )
     {
-        if( ! xDialogParent->IsDisposed() )
-            xDialogParent.clear();
+        if( ! mpDialogParent->IsDisposed() )
+            mpDialogParent.clear();
         else
             OSL_FAIL( "Dialog::Execute() - Parent of dialog destroyed in Execute()" );
     }
