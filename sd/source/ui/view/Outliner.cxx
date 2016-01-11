714bfcebd471b2dd67bca76fccbeddec762af0ec
Marco Cecchetti <marco.cecchetti@collabora.com>
2016-01-11 16:43:02 +0100

    lool - search all - unit test failure - solved
    Change-Id: I5a74dd60f61fc0fdb88f08383d579b14222aa55d
    Reviewed-on: https://gerrit.libreoffice.org/21366
    Reviewed-by: Marco Cecchetti <mrcekets@gmail.com>
    Tested-by: Marco Cecchetti <mrcekets@gmail.com>

--- sd/source/ui/view/Outliner.cxx
+++ sd/source/ui/view/Outliner.cxx
@@ -606,6 +606,8 @@ void Outliner::Initialize (bool bDirectionIsForward)
 
 bool Outliner::SearchAndReplaceAll()
 {
+    DetectChange();
+
     bool bRet = true;
     // Save the current position to be restored after having replaced all
     // matches.
