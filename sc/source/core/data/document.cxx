b70b579b7cc27a6bd2a348acf474b2c2e3fd7e9a
Jan Holesovsky <kendy@collabora.com>
2016-01-13 11:12:12 +0100

    sc: Make .uno:Insert (inserting a new sheet) always succeed.
    Also, when provided with '0' as the sheet number, treat it as adding at the
    end - which I think is the usual use case one wants to target.
    
    Change-Id: I9e1a1733c2310b8c6bb7ff239351e88bbef09ac7

--- sc/source/core/data/document.cxx
+++ sc/source/core/data/document.cxx
@@ -105,6 +105,8 @@
 #include <memory>
 #include <boost/checked_delete.hpp>
 
+#include <LibreOfficeKit/LibreOfficeKitEnums.h>
+
 #include "mtvelements.hxx"
 
 using ::editeng::SvxBorderLine;
@@ -566,6 +568,9 @@ bool ScDocument::InsertTab(
         aCxt.mnTabDeletedStart = nPos;
         aCxt.mnTabDeletedEnd = nPos;
         SetAllFormulasDirty(aCxt);
+
+        if (GetDrawLayer()->isTiledRendering())
+            GetDrawLayer()->libreOfficeKitCallback(LOK_CALLBACK_DOCUMENT_SIZE_CHANGED, "");
     }
 
     return bValid;
