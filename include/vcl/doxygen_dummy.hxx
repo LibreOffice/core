a669faa1fa88d4d82d3f1d38b652b689ecc4d3a1
Chris Sherlock <chris.sherlock79@gmail.com>
2016-01-15 06:26:41 +1100

    vcl: allow doxygen to see boost::intrusive_ptr
    We are using boost::intrusive_ptr for a number of classes,
    unfortunately by default we cannot see this in the Doxygen
    collaboration diagrams.
    
    However, we can work around this problem by making a dummy
    namespace which we include in a header that is scanned by
    Doxygen, but never included in LibreOffice. To be sure of this,
    however, I have put #ifdef DOXYGEN_ONLY guards around the file
    and defined this in the Doxygen config file.
    
    Change-Id: I18d6956518a49e6006b64e2147023ec8266c8f5c

---
+++ include/vcl/doxygen_dummy.hxx
@@ -0,0 +1,8 @@
+// Do NOT include this file anywhere! This is merely used to make Doxygen see
+// boost intrusive_ptr's - otherwise it doesn't build the collaboration diagram
+// correctly
+#ifdef DOXYGENONLY
+
+namespace boost { template<class T> class intrusive_ptr { T *ptr; }; }
+
+#endif
