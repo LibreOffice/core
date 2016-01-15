73b52e010cc14b8ccffae397197a009276715bb0
melikeyurtoglu <aysemelikeyurtoglu@gmail.com>
2016-01-15 04:16:56 +0200

    tdf#53472 Integral Symbols too small for some integrands
    Change-Id: I1ab550821f0b254a7414246fd72f3882b7140c90
    Signed-off-by: melikeyurtoglu <aysemelikeyurtoglu@gmail.com>

--- starmath/source/node.cxx
+++ starmath/source/node.cxx
@@ -1866,13 +1866,12 @@ void SmOperNode::Arrange(OutputDevice &rDev, const SmFormat &rFormat)
     OSL_ENSURE(pOper, "Sm: missing subnode");
     OSL_ENSURE(pBody, "Sm: missing subnode");
 
+    pBody->Arrange(rDev,rFormat);
+    long nHeight = pBody->GetHeight();
     SmNode *pSymbol = GetSymbol();
-    pSymbol->SetSize(Fraction(CalcSymbolHeight(*pSymbol, rFormat),
-                              pSymbol->GetFont().GetSize().Height()));
-
-    pBody->Arrange(rDev, rFormat);
     pOper->Arrange(rDev, rFormat);
 
+<<<<<<< HEAD
     long  nOrigHeight = GetFont().GetSize().Height(),
           nDist = nOrigHeight
                   * rFormat.GetDistance(DIS_OPERATORSPACE) / 100L;
@@ -1880,9 +1879,16 @@ void SmOperNode::Arrange(OutputDevice &rDev, const SmFormat &rFormat)
     Point aPos = pOper->AlignTo(*pBody, RectPos::Left, RHA_CENTER, /*RVA_CENTERY*/RVA_MID);
     aPos.X() -= nDist;
     pOper->MoveTo(aPos);
+=======
+    pSymbol->AdaptToY(rDev,nHeight);
+    pSymbol->Arrange(rDev, rFormat);
+    Point aPos= pSymbol->AlignTo(*pBody, RP_LEFT, RHA_CENTER, RVA_BASELINE);
+    aPos.Y() = pSymbol->GetTop()+pBody->GetBottom() - pSymbol->GetBottom();
+    pSymbol->MoveTo(aPos);
+>>>>>>> tdf#53472 Integral Symbols too small for some integrands
 
     SmRect::operator = (*pBody);
-    ExtendBy(*pOper, RCP_THIS);
+    ExtendBy(*pSymbol, RCP_THIS);
 }
 
 
