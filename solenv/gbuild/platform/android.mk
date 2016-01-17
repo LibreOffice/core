44ba4d0e95958ce25f6ca1ea51b197c10e6d8f50
Peter Foley <pefoley2@pefoley.com>
2016-01-17 18:13:25 -0500

    LTO fixes for android
    Change-Id: I2d4cedac4081260c5147d8c11904d042c765e3a6

--- solenv/gbuild/platform/android.mk
+++ solenv/gbuild/platform/android.mk
@@ -20,6 +20,11 @@ gb_STDLIBS := \
 
 endif
 
+ifeq ($(ENABLE_LTO),TRUE)
+	AR_FLAGS := rcs --plugin $(shell $(CC) --print-prog-name=liblto_plugin.so)
+	RANLIB := $(RANLIB) --plugin $(shell $(CC) --print-prog-name=liblto_plugin.so)
+endif
+
 # No unit testing can be run
 gb_CppunitTest_CPPTESTPRECOMMAND := :
 
