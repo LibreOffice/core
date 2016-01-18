66603d539c73c0b08809d4ed6c5f6fde6f588a7e
Miklos Vajna <vmiklos@collabora.co.uk>
2016-01-18 22:56:15 +0100

    basebmp: fix Android build
    Change-Id: I3912d9cc27ba13494ac6f140c555a17b3784919d

--- basebmp/StaticLibrary_basebmp.mk
+++ basebmp/StaticLibrary_basebmp.mk
@@ -33,7 +33,6 @@ $(eval $(call gb_StaticLibrary_add_defs,basebmp,\
 
 $(eval $(call gb_StaticLibrary_add_exception_objects,basebmp,\
 	basebmp/source/bitmapdevice \
-	basebmp/source/debug \
 ))
 
 # vim: set noet sw=4 ts=4:
