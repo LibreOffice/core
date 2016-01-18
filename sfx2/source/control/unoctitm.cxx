13e39c62959e1904eb374e07765688ffcfdba017
aybuke <aybuke.147@gmail.com>
2016-01-19 01:34:09 +0200

    tdf#96434 Activate usage data collection from commandline.
    Possible env variable: $ export LO_COLLECT_USAGE=1
    
    Change-Id: Ic253850ca73551bad8b44072164881c5039615c9

--- sfx2/source/control/unoctitm.cxx
+++ sfx2/source/control/unoctitm.cxx
@@ -674,10 +674,16 @@ class theUsageInfo : public rtl::Static<UsageInfo, theUsageInfo> {};
 /// Extracts information about the command + args, and stores that.
 void collectUsageInformation(const util::URL& rURL, const uno::Sequence<beans::PropertyValue>& rArgs)
 {
-    bool bCollecting = officecfg::Office::Common::Misc::CollectUsageInformation::get();
+    static bool bCollecting = officecfg::Office::Common::Misc::CollectUsageInformation::get();
     theUsageInfo::get().setCollecting(bCollecting);
-    if (!bCollecting)
-        return;
+    if (!bCollecting){
+        bCollecting = getenv("LO_COLLECT_USAGE");
+        if(!bCollecting) {
+            bCollecting = 1;
+        }
+        else {
+            return;}
+        }
 
     OUStringBuffer aBuffer;
 
