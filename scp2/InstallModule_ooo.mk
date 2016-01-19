85b46f51928dc17cda1bc974bca9159de9221b4d
Pranav Kant <pranavk@libreoffice.org>
2016-01-19 19:05:40 +0530

    lokdocview: Add scp files for selection handle PNGs
    Change-Id: I4d3eab1f16751a1b3f24a995e3d28322fd2f510b
    Reviewed-on: https://gerrit.libreoffice.org/21606
    Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
    Tested-by: Miklos Vajna <vmiklos@collabora.co.uk>

--- scp2/InstallModule_ooo.mk
+++ scp2/InstallModule_ooo.mk
@@ -102,6 +102,7 @@ $(eval $(call gb_InstallModule_add_scpfiles,scp2/ooo,\
 		scp2/source/ooo/mingw_dlls \
 	) \
     scp2/source/ooo/module_filter \
+    scp2/source/ooo/module_libreofficekit \
 ))
 
 $(eval $(call gb_InstallModule_add_localized_scpfiles,scp2/ooo,\
