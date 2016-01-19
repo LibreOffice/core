352f9fdff619b5a0a1414c29cfc47068165a599c
Stephan Bergmann <sbergman@redhat.com>
2016-01-19 10:43:21 +0100

    -Werror=shift-negative-value (GCC 6)
    Change-Id: Ifc7b1d6675a8e8fb41dd47cc1282059c3790736f

--- basebmp/inc/packedpixeliterator.hxx
+++ basebmp/inc/packedpixeliterator.hxx
@@ -78,7 +78,7 @@ public:
         num_intraword_positions=sizeof(value_type)*8/bits_per_pixel,
         /** Bit mask for one pixel (least significant bits)
          */
-        bit_mask=~(~0 << bits_per_pixel)
+        bit_mask=~(~0u << bits_per_pixel)
     };
 
 private:
@@ -238,7 +238,7 @@ public:
         num_intraword_positions=sizeof(value_type)*8/bits_per_pixel,
         /** Bit mask for one pixel (least significant bits)
          */
-        bit_mask=~(~0 << bits_per_pixel)
+        bit_mask=~(~0u << bits_per_pixel)
     };
 
 private:
@@ -477,7 +477,7 @@ public:
         num_intraword_positions=sizeof(value_type)*8/bits_per_pixel,
         /** Bit mask for one pixel (least significant bits)
          */
-        bit_mask=~(~0 << bits_per_pixel)
+        bit_mask=~(~0u << bits_per_pixel)
     };
 
     // TODO(F2): direction of iteration (ImageIterator can be made to
