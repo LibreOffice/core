52030ced97d18cbf3d0fadbab03199e41a78dcb2
Pranav Kant <pranavk@libreoffice.org>
2016-01-20 14:49:29 +0530

    lokdocview: Center the widget inside the allocation
    Change-Id: I8d7f8ffb1c5ddd07ccf7d56bdf0ccc866c927401
    Reviewed-on: https://gerrit.libreoffice.org/21624
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Pranav Kant <pranavk@gnome.org>
    (cherry picked from commit 070bfedb7c0bc01ada0c0b95622543472ccb4a1c)

--- libreofficekit/source/gtk/lokdocview.cxx
+++ libreofficekit/source/gtk/lokdocview.cxx
@@ -2406,14 +2406,21 @@ static void lok_doc_view_class_init (LOKDocViewClass* pClass)
 SAL_DLLPUBLIC_EXPORT GtkWidget*
 lok_doc_view_new (const gchar* pPath, GCancellable *cancellable, GError **error)
 {
-    return GTK_WIDGET (g_initable_new (LOK_TYPE_DOC_VIEW, cancellable, error, "lopath", pPath == NULL ? LOK_PATH : pPath, NULL));
+    return GTK_WIDGET (g_initable_new (LOK_TYPE_DOC_VIEW, cancellable, error,
+                                       "lopath", pPath == nullptr ? LOK_PATH : pPath,
+                                       "halign", GTK_ALIGN_CENTER,
+                                       nullptr));
 }
 
 SAL_DLLPUBLIC_EXPORT GtkWidget* lok_doc_view_new_from_widget(LOKDocView* pOldLOKDocView)
 {
     LOKDocViewPrivate& pOldPriv = getPrivate(pOldLOKDocView);
     GtkWidget* pNewDocView = GTK_WIDGET(g_initable_new(LOK_TYPE_DOC_VIEW, /*cancellable=*/nullptr, /*error=*/nullptr,
-                                                       "lopath", pOldPriv->m_aLOPath, "lopointer", pOldPriv->m_pOffice, "docpointer", pOldPriv->m_pDocument, NULL));
+                                                       "lopath", pOldPriv->m_aLOPath,
+                                                       "lopointer", pOldPriv->m_pOffice,
+                                                       "docpointer", pOldPriv->m_pDocument,
+                                                       "halign", GTK_ALIGN_CENTER,
+                                                       nullptr));
 
     // No documentLoad(), just a createView().
     LibreOfficeKitDocument* pDocument = lok_doc_view_get_document(LOK_DOC_VIEW(pNewDocView));
