d4d2c7db766f8afc77e752c56cfa1cbc3636088f
Miklos Vajna <vmiklos@collabora.co.uk>
2016-01-20 12:40:07 +0100

    libxmlsec: respect --enable-debug with GCC
    So we compile using either '-O2' or '-O0 -g', instead of '-O2 -g' all
    the time.
    
    Change-Id: Iefc22f38be37ea876c713724657af460eb4c1606

--- external/libxmlsec/ExternalProject_xmlsec.mk
+++ external/libxmlsec/ExternalProject_xmlsec.mk
@@ -55,6 +55,7 @@ $(call gb_ExternalProject_get_state_target,xmlsec,build) :
 		autoreconf \
 		&& ./configure \
 			--with-pic --disable-shared --disable-crypto-dl --without-libxslt --without-gnutls \
+			CFLAGS="$(if $(debug),$(gb_COMPILERNOOPTFLAGS) $(gb_DEBUG_CFLAGS),$(gb_COMPILEROPTFLAGS))" \
 			$(if $(or $(filter-out ANDROID,$(OS)),$(DISABLE_OPENSSL)),--without-openssl,--with-openssl=$(call gb_UnpackedTarball_get_dir,openssl)) \
 			$(if $(filter MACOSX,$(OS)),--prefix=/@.__________________________________________________OOO) \
 			$(if $(SYSTEM_NSS),,$(if $(filter MACOSX,$(OS)),--disable-pkgconfig)) \
