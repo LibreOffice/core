1493f1d1d595689dd63f801fa264481cd1dc12f0
Nusaiba Al-Kindi <noseeba1@gmail.com>
2016-01-21 15:38:03 +0400

    tdf#42788: FORMATTING - Numbering/ordered list
    I changed the default numbering alignment of the Roman(upper/ lower) numbers list to the right
    instead of left with out any changes in the default alignment of other numbring type lists.
    In addition, numbering alignment can be changed by the user and any new list created will not
    be affected by the user choice (using the default settings for the new list).
    
    Change-Id: I7a6b7fb31bf40e26910e90538cd336e767f4c056

--- editeng/source/items/numitem.cxx
+++ editeng/source/items/numitem.cxx
@@ -151,6 +151,7 @@ SvxNumberFormat::SvxNumberFormat( sal_Int16 eType,
                                   SvxNumPositionAndSpaceMode ePositionAndSpaceMode )
     : SvxNumberType(eType),
       eNumAdjust(SVX_ADJUST_LEFT),
+      mbNumAdjustChanged(false),
       nInclUpperLevels(0),
       nStart(1),
       cBullet(SVX_DEF_BULLET),
@@ -243,6 +244,25 @@ SvxNumberFormat::~SvxNumberFormat()
     delete pBulletFont;
 }
 
+void SvxNumberFormat::SetNumberingType(sal_Int16 nSet)
+{
+    if(!mbNumAdjustChanged)
+    {//Romain numbers alignment to right, issue-42788
+        if(nSet == SVX_NUM_ROMAN_UPPER || nSet == SVX_NUM_ROMAN_LOWER)
+            eNumAdjust = SVX_ADJUST_RIGHT;
+        else if (eNumAdjust == SVX_ADJUST_RIGHT && (GetNumberingType() == SVX_NUM_ROMAN_UPPER || GetNumberingType() == SVX_NUM_ROMAN_LOWER))
+            eNumAdjust = SVX_ADJUST_LEFT;
+    }
+
+    SvxNumberType::SetNumberingType(nSet);
+}
+
+void SvxNumberFormat::SetNumAdjust(SvxAdjust eSet)
+{
+    eNumAdjust = eSet;
+    mbNumAdjustChanged = true;
+}
+
 void SvxNumberFormat::Store(SvStream &rStream, FontToSubsFontConverter pConverter)
 {
     if(pConverter && pBulletFont)
@@ -316,6 +336,7 @@ SvxNumberFormat& SvxNumberFormat::operator=( const SvxNumberFormat& rFormat )
 
     SetNumberingType(rFormat.GetNumberingType());
         eNumAdjust          = rFormat.eNumAdjust ;
+        mbNumAdjustChanged  = rFormat.mbNumAdjustChanged;
         nInclUpperLevels    = rFormat.nInclUpperLevels ;
         nStart              = rFormat.nStart ;
         cBullet             = rFormat.cBullet ;
