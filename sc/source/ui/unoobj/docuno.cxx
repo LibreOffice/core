37cb554fb207a8ace162876edae8858258d90d44
Henry Castro <hcastro@collabora.com>
2016-01-22 16:47:51 -0400

    sc lokit: update zoom values to graphic and text selection
    Change-Id: I750f31c5698d9dc5ed589a1812a6993991915dc5
    Reviewed-on: https://gerrit.libreoffice.org/21724
    Reviewed-by: Henry Castro <hcastro@collabora.com>
    Tested-by: Henry Castro <hcastro@collabora.com>

--- sc/source/ui/unoobj/docuno.cxx
+++ sc/source/ui/unoobj/docuno.cxx
@@ -629,7 +629,8 @@ void ScModelObj::setTextSelection(int nType, int nX, int nY)
     ScDrawView* pDrawView = pViewData->GetScDrawView();
 
     // update the aLogicMode in ScViewData to something predictable
-    pViewData->SetZoom(Fraction(1, 1), Fraction(1, 1), true);
+    pViewData->SetZoom(Fraction(mnTilePixelWidth * TWIPS_PER_PIXEL, mnTileTwipWidth),
+                       Fraction(mnTilePixelHeight * TWIPS_PER_PIXEL, mnTileTwipHeight), true);
 
     bool bHandled = false;
 
@@ -785,7 +786,8 @@ void ScModelObj::setGraphicSelection(int nType, int nX, int nY)
     ScGridWindow* pGridWindow = pViewData->GetActiveWin();
 
     // update the aLogicMode in ScViewData to something predictable
-    pViewData->SetZoom(Fraction(1, 1), Fraction(1, 1), true);
+    pViewData->SetZoom(Fraction(mnTilePixelWidth * TWIPS_PER_PIXEL, mnTileTwipWidth),
+                       Fraction(mnTilePixelHeight * TWIPS_PER_PIXEL, mnTileTwipHeight), true);
 
     int nPixelX = nX * pViewData->GetPPTX();
     int nPixelY = nY * pViewData->GetPPTY();
