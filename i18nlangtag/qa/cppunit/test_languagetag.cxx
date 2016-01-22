f31e11b68d1f2b43d0a761dd6da54df20a65d81d
Eike Rathke <erack@redhat.com>
2016-01-22 22:30:39 +0100

    tdf#97315 forward compatibility for [eu-ES] mapping to [eu]
    Commit 72c5e230f9cda8e18f63f7bbc6567487b4c5a5e0 maps [eu] to [eu-ES] for
    5.2 (cherry-picked to 5.1), enable 5.0.5 to read that but still save
    [eu].
    
    Change-Id: I2aa82023a1ffb411c034beceb0bc999885adb026
    Reviewed-on: https://gerrit.libreoffice.org/21727
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Michael Stahl <mstahl@redhat.com>
    (cherry picked from commit 9c9876c8b63fb47814e05141c74a4fc244cfa56c)

--- i18nlangtag/qa/cppunit/test_languagetag.cxx
+++ i18nlangtag/qa/cppunit/test_languagetag.cxx
@@ -665,6 +665,7 @@ static bool checkMapping( const OUString& rStr1, const OUString& rStr2 )
     if (rStr1 == "ku-SY"       ) return rStr2 == "kmr-Latn-SY";
     if (rStr1 == "ku-IQ"       ) return rStr2 == "ckb-IQ";
     if (rStr1 == "ku-IR"       ) return rStr2 == "ckb-IR";
+    if (rStr1 == "eu-ES"       ) return rStr2 == "eu";
     return rStr1 == rStr2;
 }
 
