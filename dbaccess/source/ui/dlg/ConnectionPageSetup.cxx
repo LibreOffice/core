90e18b514f639827091cee0f04892d999046eb18
akki95 <akshaydeepiitr@gmail.com>
2016-01-26 03:52:42 +0530

    tdf#45775 PgSQL new DB allow empty Datasource
    Change-Id: I5d672629ddf7f153f0ad1927f0f6c0ffc545c3ab

--- dbaccess/source/ui/dlg/ConnectionPageSetup.cxx
+++ dbaccess/source/ui/dlg/ConnectionPageSetup.cxx
@@ -138,6 +138,9 @@ namespace dbaui
         //! TODO
         //if (  ::dbaccess::DST_ORACLE_JDBC == m_eType )
         //    m_eType =  ::dbaccess::DST_JDBC;
+        if(m_pCollection->determineType(m_eType) == ::dbaccess::DST_POSTGRES){
+            SetRoadmapStateValue(true);
+        }
 
         OConnectionHelper::implInitControls(_rSet, _bSaveValue);
 
@@ -168,6 +171,8 @@ namespace dbaui
     }
     bool OConnectionTabPageSetup::checkTestConnection()
     {
+        if ( m_pCollection->determineType(m_eType) ==  ::dbaccess::DST_POSTGRES )
+            return true;
         return !m_pConnectionURL->IsVisible() || !m_pConnectionURL->GetTextNoPrefix().isEmpty();
     }
 
