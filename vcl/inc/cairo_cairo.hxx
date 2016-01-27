48d38a419f5642993d027465eacdfaa8d7f59224
Chris Sherlock <chris.sherlock79@gmail.com>
2016-01-27 21:30:59 +1100

    vcl: fix header guard for cairo_cairo.hxx
    Change-Id: Icd1cd3bcfbf65712097a6a90755a3c747ab1da9a

--- vcl/inc/cairo_cairo.hxx
+++ vcl/inc/cairo_cairo.hxx
@@ -17,8 +17,8 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_CANVAS_SOURCE_CAIRO_CAIRO_CAIRO_HXX
-#define INCLUDED_CANVAS_SOURCE_CAIRO_CAIRO_CAIRO_HXX
+#ifndef INCLUDED_VCL_INC_CAIRO_CAIRO_HXX
+#define INCLUDED_VCL_INC_CAIRO_CAIRO_HXX
 
 #include <sal/config.h>
 
