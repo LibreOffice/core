354f537cc12cd649f44ab6d77859adc7e57b143f
Giuseppe Castagno <giuseppe.castagno@acca-esse.eu>
2016-01-29 16:11:26 +0100

    ucb: webdav-curl: Related tdf#95217: Http header names are case insensitive
    [ port of commit e973b342826e54f147251b132c3325d30749e312 ]
    
    Change-Id: I7473625894c023c526a8ffeccd7ec6d67629ff76

--- ucb/source/ucp/webdav-curl/ContentProperties.cxx
+++ ucb/source/ucp/webdav-curl/ContentProperties.cxx
@@ -406,7 +406,7 @@ void ContentProperties::addProperty( const OUString & rName,
         (*m_xProps)[ OUString( "Size" ) ]
             = PropertyValue( uno::makeAny( aValue.toInt64() ), true );
     }
-    else if ( rName == "Content-Length" )
+    else if ( rName.equalsIgnoreAsciiCase( "Content-Length" ) )
     {
         // Do NOT map Content-Length entity header to DAV:getcontentlength!
         // Only DAV resources have this property.
@@ -424,7 +424,7 @@ void ContentProperties::addProperty( const OUString & rName,
         (*m_xProps)[ OUString( "MediaType" ) ]
             = PropertyValue( rValue, true );
     }
-    else if ( rName == "Content-Type" )
+    else if ( rName.equalsIgnoreAsciiCase( "Content-Type" ) )
     {
         // Do NOT map Content-Type entity header to DAV:getcontenttype!
         // Only DAV resources have this property.
@@ -447,7 +447,7 @@ void ContentProperties::addProperty( const OUString & rName,
         (*m_xProps)[ OUString( "DateModified" ) ]
             = PropertyValue( uno::makeAny( aDate ), true );
     }
-    else if ( rName == "Last-Modified" )
+    else if ( rName.equalsIgnoreAsciiCase( "Last-Modified" ) )
     {
         // Do not map Last-Modified entity header to DAV:getlastmodified!
         // Only DAV resources have this property.
