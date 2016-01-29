09730ea0ccfe63982cdb869d5eaa906982283bf1
Giuseppe Castagno <giuseppe.castagno@acca-esse.eu>
2016-01-29 15:39:17 +0100

    ucb: webdav-curl: Related tdf#95217: Force HEAD method in Web access if PROPFIND failed
    [ port of commit d61352f58a7f750d3b0b0a9c2d6498fbb7a6e10d ]
    
    Change-Id: Id945be7a1f71c830f3ab15c54fbf65a32be71a2d
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/123278
    Tested-by: Michael Stahl <michael.stahl@allotropia.de>
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

--- ucb/source/ucp/webdav-curl/webdavcontent.cxx
+++ ucb/source/ucp/webdav-curl/webdavcontent.cxx
@@ -1379,6 +1379,9 @@ uno::Reference< sdbc::XRow > Content::getPropertyValues(
 
         if ( bNetworkAccessAllowed )
         {
+            if( eType != DAV )
+                m_bDidGetOrHead = false;
+
             // All properties obtained already?
             std::vector< OUString > aMissingProps;
             if ( !( xProps
