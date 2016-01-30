84de87edd6a18d3cb3789dd646466565d2f47391
irem <iremsendur1@gmail.com>
2016-01-30 17:34:55 +0200

    tdf#46279 Show restart message after extension installation
    Change-Id: I1b714755d73aba433389dcc9e320d2b0ad58c323

---
+++ desktop/source/deployment/gui/restartdialog.hxx
@@ -0,0 +1,49 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef INCLUDED_SVTOOLS_RESTARTDIALOG_HXX
+#define INCLUDED_SVTOOLS_RESTARTDIALOG_HXX
+
+#include <sal/config.h>
+
+#include <com/sun/star/uno/Reference.hxx>
+#include <svtools/svtdllapi.h>
+
+namespace vcl { class Window; }
+namespace com { namespace sun { namespace star { namespace uno {
+    class XComponentContext;
+} } } }
+
+namespace svtools {
+
+enum RestartReason {
+    RESTART_REASON_JAVA,
+        // "For the selected Java runtime environment to work properly,
+        // %PRODUCTNAME must be restarted."
+    RESTART_REASON_PDF_AS_STANDARD_JOB_FORMAT,
+        // For the modified default print job format to take effect,
+        // %PRODUCTNAME must be restarted.
+    RESTART_REASON_BIBLIOGRAPHY_INSTALL,
+        // "For the bibliography to work properly,
+        // %PRODUCTNAME must be restarted."
+    RESTART_REASON_MAILMERGE_INSTALL
+        // "For the mail merge to work properly,
+        // %PRODUCTNAME must be restarted."
+};
+
+// Must be called with the solar mutex locked:
+SVT_DLLPUBLIC void executeRestartDialog(
+    css::uno::Reference< css::uno::XComponentContext > const & context,
+    vcl::Window * parent, RestartReason reason);
+
+}
+
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
