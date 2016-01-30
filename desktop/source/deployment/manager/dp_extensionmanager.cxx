26da91f65745879232f0628ed0cca5110f8fe8a5
irem <iremsendur1@gmail.com>
2016-01-30 17:34:55 +0200

    tdf#46279  Show restart message after extension installation
    Change-Id: I53c30af80c26ecc2661fb39d70ca0784f5d131bc

--- desktop/source/deployment/manager/dp_extensionmanager.cxx
+++ desktop/source/deployment/manager/dp_extensionmanager.cxx
@@ -51,6 +51,12 @@
 #include "dp_extensionmanager.hxx"
 #include "dp_commandenvironments.hxx"
 #include "dp_properties.hxx"
+<<<<<<< HEAD
+=======
+#include <vcl/layout.hxx>
+#include "../../app/desktop.hrc"
+#include "../../app/desktopresid.hxx"
+>>>>>>> tdf#46279  Show restart message after extension installation
 
 #include <list>
 #include <algorithm>
@@ -599,6 +605,10 @@ bool ExtensionManager::doChecksForAddExtension(
             xAbortChannel, _xCmdEnv, xOldExtension.is() || props.isExtensionUpdate()) == 0;
 
         return bCanInstall;
+        if(bCanInstall == true){
+            ScopedVclPtrInstance<MessageDialog> aWarnBox(nullptr, desktop::DesktopResId(STR_LO_MUST_BE_RESTARTED), VCL_MESSAGE_INFO);
+            aWarnBox->Execute();
+         }
     }
     catch ( const css::deployment::DeploymentException& ) {
         throw;
@@ -621,6 +631,7 @@ bool ExtensionManager::doChecksForAddExtension(
             "Extension Manager: unexpected exception in doChecksForAddExtension",
             static_cast<OWeakObject*>(this));
     }
+    return 0;
 }
 
 // Only add to shared and user repository
