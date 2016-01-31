6564bcd0af8efed58c1c32328c68e3a5bf825e48
Sedat Ak <sedat.ak.bm@gmail.com>
2016-01-31 11:59:58 +0200

    tdf#94698 cleanup Makefiles
    Change-Id: Ic03de94851a16db1f8664258e14254283d2ad497

--- sc/CppunitTest_sc_bugfix_test.mk
+++ sc/CppunitTest_sc_bugfix_test.mk
@@ -64,10 +64,7 @@ $(eval $(call gb_CppunitTest_set_include,sc_bugfix_test,\
     $$(INCLUDE) \
 ))
 
-$(eval $(call gb_CppunitTest_use_api,sc_bugfix_test,\
-    offapi \
-    udkapi \
-))
+$(eval $(call gb_CppunitTest_use_api,sc_bugfix_test,))
 
 $(eval $(call gb_CppunitTest_use_ure,sc_bugfix_test))
 $(eval $(call gb_CppunitTest_use_vcl,sc_bugfix_test))
