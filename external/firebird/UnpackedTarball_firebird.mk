53eb91d00c46ccc8c69a31ccdd33d424bcb9fc6b
Douglas Mencken <dougmencken@gmail.com>
2016-02-02 22:08:25 -0500

    try to fix build of external:firebird
    Change-ID: Ia07a2e34208763228bd78f5b6cbda78c78015d4b

--- external/firebird/UnpackedTarball_firebird.mk
+++ external/firebird/UnpackedTarball_firebird.mk
@@ -14,14 +14,18 @@ $(eval $(call gb_UnpackedTarball_set_tarball,firebird,$(FIREBIRD_TARBALL)))
 $(eval $(call gb_UnpackedTarball_set_patchlevel,firebird,0))
 
 $(eval $(call gb_UnpackedTarball_add_patches,firebird,\
+		external/firebird/Provide-sized-global-delete-operators-when-compiled-.patch \
+		external/firebird/Make-the-generated-code-compatible-with-gcc-6-in-C-1.patch \
         external/firebird/firebird.disable-ib-util-not-found.patch.1 \
 		external/firebird/firebird-Engine12.patch \
 		external/firebird/firebird-rpath.patch.0 \
 		external/firebird/firebird-cloop-compiler.patch.1 \
-		external/firebird/firebird-gcc6.patch.1 \
 		external/firebird/wnt-dbgutil.patch \
 		external/firebird/debug-gfix-failure.patch \
 		external/firebird/libc++.patch \
+		external/firebird/os_utils-O_CLOEXEC.patch \
+		external/firebird/remote-inet-SOL_TCP.patch \
+		external/firebird/my-own-version-of-strnlen.patch \
 ))
 
 ifeq ($(OS)-$(COM),WNT-MSC)
@@ -32,10 +36,20 @@ endif
 
 ifeq ($(OS),MACOSX)
 $(eval $(call gb_UnpackedTarball_add_patches,firebird,\
-	external/firebird/firebird-configure-x86-64-macosx.patch.1 \
 	external/firebird/firebird-macosx.patch.1 \
-	external/firebird/macosx-elcapitan-dyld.patch \
+	external/firebird/firebird-macosx-print-dyldlibpath.patch \
+	external/firebird/InputDevices-darwin.patch \
+))
+ifeq ($(CPUNAME),POWERPC)
+$(eval $(call gb_UnpackedTarball_add_patches,firebird,\
+	external/firebird/lSystemStubs-darwin.patch \
+	external/firebird/prefix.darwin_powerpc.patch \
+	external/firebird/semaphore.h-darwin.patch \
+))
+else
+$(eval $(call gb_UnpackedTarball_add_patches,firebird,\
+	external/firebird/firebird-configure-x86-64-macosx.patch.1 \
 ))
 endif
-
+endif
 # vim: set noet sw=4 ts=4:
