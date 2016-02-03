d9835c81b1f929e524d4c92628811faa25bfc181
coypu <coypu@sdf.org>
2016-02-03 15:32:40 +0200

    Include headers using OpenBSD too
    OpenBSD ports patch: http://cvsweb.openbsd.org/cgi-bin/cvsweb/ports/editors/libreoffice/patches/patch-include_LibreOfficeKit_LibreOfficeKitInit_h
    
    Change-Id: Ia3cebb41ca72bf022cc409814bed575cfbcfbf64
    Reviewed-on: https://gerrit.libreoffice.org/22079
    Reviewed-by: jan iversen <jani@documentfoundation.org>
    Tested-by: jan iversen <jani@documentfoundation.org>
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Michael Stahl <mstahl@redhat.com>

--- include/LibreOfficeKit/LibreOfficeKitInit.h
+++ include/LibreOfficeKit/LibreOfficeKitInit.h
@@ -19,7 +19,7 @@ extern "C"
 
 #if defined(__linux__) || defined (__FreeBSD_kernel__) || defined(_AIX) ||\
     defined(_WIN32) || defined(__APPLE__) || defined (__NetBSD__) ||\
-    defined (__sun)
+    defined (__sun) || defined(__OpenBSD__)
 
 #include <stdio.h>
 #include <stdlib.h>
