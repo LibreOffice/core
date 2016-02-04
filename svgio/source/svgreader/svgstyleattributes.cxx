c5daef6d5f720db6b44bd900ed79c14260a0135d
Xisco Fauli <anistenis@gmail.com>
2016-02-05 00:25:58 +0100

    tdf#97542: SVGIO: First check the current fill attribute
    Differenciate when gradient is defined in the text element or in its parents
    
    Add unit tests
    
    Change-Id: I85b09f72fbb1a3c8d2fa7350b4396b5bf924277f

--- svgio/source/svgreader/svgstyleattributes.cxx
+++ svgio/source/svgreader/svgstyleattributes.cxx
@@ -263,6 +263,9 @@ namespace svgio
                 // dismantle to geometry and add needed primitives
                 const basegfx::BColor* pFill = getFill();
                 const SvgGradientNode* pFillGradient = getSvgGradientNodeFill();
+                const SvgStyleAttributes* pSvgStyleAttributes = getParentStyle();
+                const SvgGradientNode* pParentFillGradient =
+                    pSvgStyleAttributes ? pSvgStyleAttributes->getSvgGradientNodeFill() : nullptr;
                 const SvgPatternNode* pFillPattern = getSvgPatternNodeFill();
                 const basegfx::BColor* pStroke = getStroke();
                 const SvgGradientNode* pStrokeGradient = getSvgGradientNodeStroke();
@@ -303,19 +306,19 @@ namespace svgio
 
                 const bool bStrokeUsed(pStroke || pStrokeGradient || pStrokePattern);
 
+                if(pFill && (!pFillGradient || pParentFillGradient))
+                {
+                    // add the already prepared primitives for single color fill
+                    rTarget.append(rSource);
+                }
                 // add fill. Use geometry even for simple color fill when stroke
                 // is used, else text rendering and the geometry-based stroke will
                 // normally not really match optically due to diverse system text
                 // renderers
-                if(aMergedArea.count() && (pFillGradient || pFillPattern || bStrokeUsed))
+                else if(aMergedArea.count() && (pFillGradient || pFillPattern || bStrokeUsed))
                 {
                     // create text fill content based on geometry
                     add_fill(aMergedArea, rTarget, aMergedArea.getB2DRange());
-                }
-                else if(pFill)
-                {
-                    // add the already prepared primitives for single color fill
-                    rTarget.append(rSource);
                 }
 
                 // add stroke
