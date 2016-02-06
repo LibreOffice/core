02eeb9509f38c746ddd71997df57b15772cf89c4
Markus Mohrhard <markus.mohrhard@googlemail.com>
2016-02-07 00:19:09 +0100

    work on adding crash report UI
    Change-Id: I66f4dca3cd32381ecd52cc36490e7ee1dddf3699
    Reviewed-on: https://gerrit.libreoffice.org/22566
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Markus Mohrhard <markus.mohrhard@googlemail.com>

---
+++ svx/source/dialog/crashreportdlg.hxx
@@ -0,0 +1,36 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef INCLUDED_SVX_SOURCE_DIALOG_CRASHREPORTDLG_HXX
+#define INCLUDED_SVX_SOURCE_DIALOG_CRASHREPORTDLG_HXX
+
+#include <vcl/dialog.hxx>
+#include <vcl/button.hxx>
+
+class CrashReportDialog : public Dialog
+{
+public:
+
+    CrashReportDialog(vcl::Window* pParent);
+
+    ~CrashReportDialog();
+
+    virtual void dispose() override;
+
+private:
+
+    VclPtr<Button> mpBtnSend;
+    VclPtr<Button> mpBtnCancel;
+
+    DECL_LINK_TYPED(BtnHdl, Button*, void);
+};
+
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
