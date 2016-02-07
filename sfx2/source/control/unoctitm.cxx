6d5bbadfb69c1c6b2d3e82e79c97350af2135487
Henry Castro <hcastro@collabora.com>
2016-02-07 18:29:03 -0400

    lok: notify the states of undo and redo toolbar items
    Also included .uno:InsertPage, .uno:DeletePage and
    .uno:DuplicatePage
    
    Change-Id: I91dd2609d6a48be75005a44490614bcf8bcf62e9
    Reviewed-on: https://gerrit.libreoffice.org/22195
    Reviewed-by: Henry Castro <hcastro@collabora.com>
    Tested-by: Henry Castro <hcastro@collabora.com>

--- sfx2/source/control/unoctitm.cxx
+++ sfx2/source/control/unoctitm.cxx
@@ -1115,6 +1115,17 @@ void SfxDispatchController_Impl::InterceptLOKStateChangeEvent(const SfxObjectShe
         aEvent.State >>= nColor;
         aBuffer.append(nColor);
     }
+    else if (aEvent.FeatureURL.Path == "Undo" ||
+             aEvent.FeatureURL.Path == "Redo")
+    {
+        aBuffer.append(aEvent.IsEnabled ? OUString("enabled") : OUString("disabled"));
+    }
+    else if (aEvent.FeatureURL.Path == "InsertPage" ||
+             aEvent.FeatureURL.Path == "DeletePage" ||
+             aEvent.FeatureURL.Path == "DuplicatePage")
+    {
+        aBuffer.append(OUString::boolean(aEvent.IsEnabled));
+    }
     else
     {
         return;
