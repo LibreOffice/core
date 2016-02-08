570d5af407d55c39621575e56c77817dbd429783
Miklos Vajna <vmiklos@collabora.co.uk>
2016-02-08 15:00:21 +0100

    xmlsecurity: initial OOXML support for writing same-document references
    They are kind of a special case for ODF, but OOXML uses these
    exclusively, and then one of them refers to the actual package streams.
    
    Change-Id: I4663eef4bd718a7563effd6cafa790126db6d8c7

--- xmlsecurity/source/helper/xsecctl.hxx
+++ xmlsecurity/source/helper/xsecctl.hxx
@@ -97,6 +97,7 @@
 #define ALGO_RSASHA1            "http://www.w3.org/2000/09/xmldsig#rsa-sha1"
 #define ALGO_RSASHA256 "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256"
 #define ALGO_XMLDSIGSHA1        "http://www.w3.org/2000/09/xmldsig#sha1"
+#define ALGO_XMLDSIGSHA256 "http://www.w3.org/2001/04/xmlenc#sha256"
 #define ALGO_RELATIONSHIP "http://schemas.openxmlformats.org/package/2006/RelationshipTransform"
 
 #define CHAR_FRAGMENT           "#"
