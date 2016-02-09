7672637cb33bcae199d3f0b0b3dcc568375d4976
Michael Meeks <michael.meeks@collabora.com>
2016-02-09 17:15:29 +0000

    vcl: opengl - clear the user buffer after greyscale conversion.
    Change-Id: Ie87fc935bf69b5eb2c620e60d041fb95b544ec96
    Reviewed-on: https://gerrit.libreoffice.org/22243
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>
    Tested-by: Tomaž Vajngerl <quikee@gmail.com>

--- vcl/opengl/salbmp.cxx
+++ vcl/opengl/salbmp.cxx
@@ -932,6 +932,10 @@ bool OpenGLSalBitmap::ConvertToGreyscale()
     mnBits = 8;
     maPalette = Bitmap::GetGreyPalette(256);
 
+    // AllocateUserData will handle the rest.
+    mpUserBuffer.reset();
+    mbDirtyTexture = false;
+
     CHECK_GL_ERROR();
     return true;
 }
