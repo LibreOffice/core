96b5f7e2640dcdb69a4791c9903de54cd4f227d7
Juergen Funk <juergen.funk_ml@cib.de>
2016-02-10 14:54:24 +0100

    tdf#97526 - in copy/paste ignore unwanted page break on first paragraph
    In copy/paste ignore the page break attribute from the first paragraph
    of the document (see also tdf#39400)
    
    Reviewed-on: https://gerrit.libreoffice.org/22270
    Reviewed-by: Oliver Specht <oliver.specht@cib.de>
    Tested-by: Oliver Specht <oliver.specht@cib.de>
    
    Conflicts:
    	sw/source/core/doc/DocumentContentOperationsManager.cxx
    
    Change-Id: I838c21c14647d4692673bd30df320e6704de1455

--- sw/source/core/doc/DocumentContentOperationsManager.cxx
+++ sw/source/core/doc/DocumentContentOperationsManager.cxx
@@ -4172,6 +4172,7 @@ bool DocumentContentOperationsManager::CopyImpl( SwPaM& rPam, SwPosition& rPos,
                           ( !bOneNode && !rPos.nContent.GetIndex() ) );
     bool bCopyBookmarks = true;
     bool bStartIsTextNode = 0 != pSttTextNd;
+    bool bCopyPageSource  = false;
 
     // #i104585# copy outline num rule to clipboard (for ASCII filter)
     if (pDoc->IsClipBoard() && m_rDoc.GetOutlineNumRule())
@@ -4435,9 +4436,23 @@ bool DocumentContentOperationsManager::CopyImpl( SwPaM& rPam, SwPosition& rPos,
                     pCopyPam->GetPoint()->nNode.GetIndex()+1 ]->GetTextNode()))
             {
                 pDestTextNd->SetAttr( aBrkSet );
+                bCopyPageSource = true;
             }
         }
     } while( false );
+
+
+    // it is not possible to make this test when copy from the clipBoard to document
+    //  in this case the PageNum not exist anymore
+    // tdf#39400 and tdf#97526
+    // when copy from document to ClipBoard, and it is from the first page
+    //  and not the source has the page break
+    if (pDoc->IsClipBoard() && (rPam.GetPageNum(pStt == rPam.GetPoint()) == 1) && !bCopyPageSource)
+    {
+        pDestTextNd->ResetAttr(RES_BREAK);        // remove the page-break
+        pDestTextNd->ResetAttr(RES_PAGEDESC);
+    }
+
 
     // Adjust position (in case it was moved / in another node)
     rPos.nContent.Assign( rPos.nNode.GetNode().GetContentNode(),
