cee1b43e706b710819dc03590cf585e1798f801c
Tor Lillqvist <tml@collabora.com>
2016-02-12 16:36:55 +0200

    We need to mention the vcl_opengl_blacklist here
    Otherwise the file won't be included in the installer.
    
    Change-Id: I928144e666377f18efa3a66f1e8c3e4204a708dc
    (cherry picked from commit 98015cdcde77d23b66636591280463ae577246c0)

--- Repository.mk
+++ Repository.mk
@@ -816,6 +816,9 @@ $(eval $(call gb_Helper_register_packages_for_install,ooo,\
 	xmlsec \
 	chart2_opengl_shader \
 	vcl_opengl_shader \
+	$(if $(filter WNT,$(OS)), \
+		vcl_opengl_blacklist \
+	) \
 	$(if $(ENABLE_OPENGL_CANVAS),canvas_opengl_shader) \
 ))
 
