3a24a19f9a397871dae139c1fd7a31f4144ace09
Kohei Yoshida <kohei.yoshida@gmail.com>
2016-02-11 21:51:12 -0500

    Update mdds to 1.1.0.
    Change-Id: I89b9b2e562f5e934df25498f5587a339db23388f

--- external/mdds/UnpackedTarball_mdds.mk
+++ external/mdds/UnpackedTarball_mdds.mk
@@ -11,10 +11,10 @@ $(eval $(call gb_UnpackedTarball_UnpackedTarball,mdds))
 
 $(eval $(call gb_UnpackedTarball_set_tarball,mdds,$(MDDS_TARBALL)))
 
-$(eval $(call gb_UnpackedTarball_set_patchlevel,mdds,3))
+$(eval $(call gb_UnpackedTarball_set_patchlevel,mdds,0))
 
 $(eval $(call gb_UnpackedTarball_add_patches,mdds,\
-	external/mdds/mdds-c++98.patch.0 \
+	external/mdds/0001-Fix-some-build-snuffs.patch \
 ))
 
 # vim: set noet sw=4 ts=4:
