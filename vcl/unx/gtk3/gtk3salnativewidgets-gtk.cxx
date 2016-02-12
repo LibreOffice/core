4cd1c029812155e4e1f80dcf69ea4167edec0130
Caolán McNamara <caolanm@redhat.com>
2016-02-12 12:06:00 +0000

    Related: rhbz#1287593 subissues 1 & 2, pick 'tab' suitable colors from theme
    we don't use these for "real" tabs in tabcontrols in the gtk3 themeing
    but they get used in other places
    
    Change-Id: Ia55b8a43604f624ac82923897a01050fa4aa2bc9

--- vcl/unx/gtk3/gtk3salnativewidgets-gtk.cxx
+++ vcl/unx/gtk3/gtk3salnativewidgets-gtk.cxx
@@ -1940,6 +1940,10 @@ void GtkSalGraphics::updateSettings( AllSettings& rSettings )
         aStyleSet.SetFieldColor( aBackFieldColor );
         // This baby is the default page/paper color
         aStyleSet.SetWindowColor( aBackFieldColor );
+
+        // Tab colors
+        aStyleSet.SetActiveTabColor( aBackFieldColor ); // same as the window color.
+        aStyleSet.SetInactiveTabColor( aBackColor );
     }
 
     // menu disabled entries handling
