58ec66da422e5ac6aec5cbd28403039cded7c7ed
Pranav Kant <pranavk@collabora.com>
2016-02-14 20:44:00 +0530

    lokdocview: Center the widget vertically inside the allocation
    Change-Id: I891519d54212ecd99273a54aca2a44e2d54c1933

--- libreofficekit/source/gtk/lokdocview.cxx
+++ libreofficekit/source/gtk/lokdocview.cxx
@@ -2574,6 +2574,7 @@ lok_doc_view_new (const gchar* pPath, GCancellable *cancellable, GError **error)
     return GTK_WIDGET (g_initable_new (LOK_TYPE_DOC_VIEW, cancellable, error,
                                        "lopath", pPath == nullptr ? LOK_PATH : pPath,
                                        "halign", GTK_ALIGN_CENTER,
+                                       "valign", GTK_ALIGN_CENTER,
                                        nullptr));
 }
 
@@ -2585,6 +2586,7 @@ SAL_DLLPUBLIC_EXPORT GtkWidget* lok_doc_view_new_from_widget(LOKDocView* pOldLOK
                                                        "lopointer", pOldPriv->m_pOffice,
                                                        "docpointer", pOldPriv->m_pDocument,
                                                        "halign", GTK_ALIGN_CENTER,
+                                                       "valign", GTK_ALIGN_CENTER,
                                                        nullptr));
 
     // No documentLoad(), just a createView().
