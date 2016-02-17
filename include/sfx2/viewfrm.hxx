36a0c25fa59df9e16d7cd2be36388803ddcec228
Stephan Bergmann <sbergman@redhat.com>
2016-02-17 14:07:15 +0100

    loplugin:nullptr
    Change-Id: Iadf6c245e4902ef98d5e3de09c1648bdd74644bc

--- include/sfx2/viewfrm.hxx
+++ include/sfx2/viewfrm.hxx
@@ -180,7 +180,7 @@ public:
         The buttons will be added from Right to Left at the right of the info bar. The parent, size
         and position of each button will be changed: only the width will remain unchanged.
       */
-    SfxInfoBarWindow* AppendInfoBar(const OUString& sId, const OUString& sMessage, const basegfx::BColor* pBackgroundColor = 0, const basegfx::BColor* pForegroundColor = 0);
+    SfxInfoBarWindow* AppendInfoBar(const OUString& sId, const OUString& sMessage, const basegfx::BColor* pBackgroundColor = nullptr, const basegfx::BColor* pForegroundColor = nullptr);
     void              RemoveInfoBar(const OUString& sId);
 
     SAL_DLLPRIVATE void SetDowning_Impl();
