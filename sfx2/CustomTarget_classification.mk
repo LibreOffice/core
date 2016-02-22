3a2f9a6869b20d26c7ddfd34a13f869c99071144
Miklos Vajna <vmiklos@collabora.co.uk>
2016-02-22 11:57:00 +0100

    sfx2 classification: add categories example
    This one has 4 choices, it shows how a category can trigger a specific
    header, footer or watermark. Also validate the XML file against the XSD
    as part of the build.
    
    Change-Id: Id6cf3de63e7fadd16366465c7721d6052c7c60ed
    Reviewed-on: https://gerrit.libreoffice.org/22615
    Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
    Tested-by: Jenkins <ci@libreoffice.org>
    (cherry picked from commit c2befb46193a4644155a1beee9197906be1af780)

---
+++ sfx2/CustomTarget_classification.mk
@@ -0,0 +1,28 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_CustomTarget_CustomTarget,sfx2/classification))
+
+sfx2_classification_SRC := $(SRCDIR)/sfx2/classification
+sfx2_classification_WORK := $(call gb_CustomTarget_get_workdir,sfx2/classification)
+sfx2_classification_GEN_example_validated=$(sfx2_classification_WORK)/example.validated
+
+sfx2_classification_XMLLINTCOMMAND := $(call gb_ExternalExecutable_get_command,xmllint)
+
+$(sfx2_classification_GEN_example_validated) : $(sfx2_classification_SRC)/baf.xsd $(sfx2_classification_SRC)/example.xml
+	$(call gb_Output_announce,$(subst $(WORKDIR)/,,$@),build,VAL,1)
+	$(call gb_Helper_abbreviate_dirs,\
+		$(sfx2_classification_XMLLINTCOMMAND) --noout --schema $(sfx2_classification_SRC)/baf.xsd $(sfx2_classification_SRC)/example.xml > $@ 2>&1 \
+		|| (cat $@; false))
+
+$(call gb_CustomTarget_get_target,sfx2/classification) : $(sfx2_classification_GEN_example_validated)
+
+$(sfx2_classification_GEN_example_validated) :| $(call gb_ExternalExecutable_get_dependencies,xmllint) $(sfx2_classification_WORK)/.dir
+
+# vim: set noet sw=4 ts=4:
