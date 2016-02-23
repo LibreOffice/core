9e6e1e5ac38497ae5042963597dd2cdc61d3b8a2
David Ostrovsky <david@ostrovsky.org>
2016-02-23 22:25:06 +0100

    Bump nss to 3.22.1 and nspr to 4.12
    It seems like nss has activated -Werror, even though the code is not
    warning free: [1].  This applies also to the additional patches that
    would need to be patched to be a warning free patch, endless recursion.
    
    To avoid messing around with something like that: [2], we disable
    -Werror option for now. Also note, that [2] doesn't fix all warnings,
    but only part of them so that we let with unsolved non-trivially to
    fix warnings on different supported compilers if we would choose the
    warning-free-compilation-path.
    
    [1] https://groups.google.com/d/topic/mozilla.dev.tech.crypto/TgnF3tIMxJ0/discussion
    [2] https://git.fedorahosted.org/cgit/nss-pem.git/commit/?id=ec2b9a90623b1ec5adb6ebe8d6651b35854786ec
    
    Reviewed-on: https://gerrit.libreoffice.org/22650
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: David Ostrovsky <david@ostrovsky.org>
    (cherry picked from commit 486c91147c587febdacf3fa95d56633dbd703d5f)
    
    Change-Id: I762c03acede6c4eac715d3b0589061eeabbcc7c2
    Reviewed-on: https://gerrit.libreoffice.org/23097
    Reviewed-by: David Ostrovsky <david@ostrovsky.org>
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>

--- external/nss/ExternalProject_nss.mk
+++ external/nss/ExternalProject_nss.mk
@@ -22,6 +22,7 @@ $(call gb_ExternalProject_get_state_target,nss,configure):
 		nspr/configure --includedir=$(call gb_UnpackedTarball_get_dir,nss)/mozilla/dist/out/include \
 			$(if $(CROSS_COMPILING),--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM)) \
 			$(if $(filter MSC-X86_64,$(COM)-$(CPUNAME)),--enable-64bit) \
+			$(if $(filter MSC-INTEL,$(COM)-$(CPUNAME)),--host=i686-pc-cygwin) \
 	,,nss_configure.log)
 
 ifeq ($(OS),WNT)
