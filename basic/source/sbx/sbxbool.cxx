0a505f87700636412119d9590fc3e9f089915498
baltasarq <baltasarq@gmail.com>
2016-02-23 14:29:44 +0100

    Eliminate String defines and create StringId enum class
    This is a [partial] patch for BUG #84938:
    https://bugs.documentfoundation.org/show_bug.cgi?id=84938
    
    The objective is to substitute all apparitions of #defined constants for enum class.
    The victim here is the series of constants STRING_xxx that were located in sbres.hxx
    
    Obviously some other files where the type is used must be changed too.
    
    The patch compiles in my machine, though for some reason it says "Cannot merge".
    
    Change-Id: I63e03cc0e49092aca13b6ad395c6c138437ff67f

--- basic/source/sbx/sbxbool.cxx
+++ basic/source/sbx/sbxbool.cxx
@@ -70,9 +70,9 @@ enum SbxBOOL ImpGetBool( const SbxValues* p )
             nRes = SbxFALSE;
             if ( p->pOUString )
             {
-                if( p->pOUString->equalsIgnoreAsciiCase( SbxRes( String::TRUE ) ) )
+                if( p->pOUString->equalsIgnoreAsciiCase( SbxRes( StringId::TRUE ) ) )
                     nRes = SbxTRUE;
-                else if( !p->pOUString->equalsIgnoreAsciiCase( SbxRes( String::FALSE ) ) )
+                else if( !p->pOUString->equalsIgnoreAsciiCase( SbxRes( StringId::FALSE ) ) )
                 {
                     // it can be convertible to a number
                     bool bError = true;
@@ -174,9 +174,9 @@ void ImpPutBool( SbxValues* p, sal_Int16 n )
         case SbxSTRING:
         case SbxLPSTR:
             if ( !p->pOUString )
-                p->pOUString = new OUString( SbxRes( n ? String::TRUE : String::FALSE ) );
+                p->pOUString = new OUString( SbxRes( n ? StringId::TRUE : StringId::FALSE ) );
             else
-                *p->pOUString = SbxRes( n ? String::TRUE : String::FALSE );
+                *p->pOUString = SbxRes( n ? StringId::TRUE : StringId::FALSE );
             break;
 
         case SbxOBJECT:
