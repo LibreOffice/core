b4d3a0d6744b8c6e5861b8198b31a434a5c2a6f6
Michael Stahl <mstahl@redhat.com>
2016-02-23 15:08:47 +0100

    RepositoryExternal: move that out of gb_LinkTarget__use_breakpad
    otherwise package would be registered multiple times, looks like one
    tinderbox fails due to that.
    
    Change-Id: I3bcbc9fff1e1993ae30cda634a50f815a30bc094

--- RepositoryExternal.mk
+++ RepositoryExternal.mk
@@ -3368,12 +3368,11 @@ endif
 
 $(call gb_LinkTarget_use_external_project,$(1),breakpad)
 
+endef
+
 $(eval $(call gb_Helper_register_packages_for_install,ooo,\
 	breakpad \
 ))
-
-
-endef
 
 # else # SYSTEM_LIBBREAKPAD
 # 
