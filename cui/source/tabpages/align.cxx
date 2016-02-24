7926df8df6ef021c204da5415d9dfaec15952dad
Yousuf Philips <philipz85@hotmail.com>
2016-02-24 21:28:59 +0400

    tdf#97251 Always show the text direction alignment control
    Change-Id: I349ac47e08f9a9d55dd955026988273e126ad4c7

--- cui/source/tabpages/align.cxx
+++ cui/source/tabpages/align.cxx
@@ -208,19 +208,9 @@ AlignmentTabPage::AlignmentTabPage( vcl::Window* pParent, const SfxItemSet& rCor
     // Asian vertical mode
     m_pCbAsianMode->Show( SvtCJKOptions().IsVerticalTextEnabled() );
 
-
-    if( !SvtLanguageOptions().IsCTLFontEnabled() )
-    {
-        m_pBoxDirection->Hide();
-    }
-    else
-    {
-       // CTL frame direction
-       m_pLbFrameDir->InsertEntryValue( CUI_RESSTR( RID_SVXSTR_FRAMEDIR_LTR ), FRMDIR_HORI_LEFT_TOP );
-       m_pLbFrameDir->InsertEntryValue( CUI_RESSTR( RID_SVXSTR_FRAMEDIR_RTL ), FRMDIR_HORI_RIGHT_TOP );
-       m_pLbFrameDir->InsertEntryValue( CUI_RESSTR( RID_SVXSTR_FRAMEDIR_SUPER ), FRMDIR_ENVIRONMENT );
-       m_pBoxDirection->Show();
-    }
+    m_pLbFrameDir->InsertEntryValue( CUI_RESSTR( RID_SVXSTR_FRAMEDIR_LTR ), FRMDIR_HORI_LEFT_TOP );
+    m_pLbFrameDir->InsertEntryValue( CUI_RESSTR( RID_SVXSTR_FRAMEDIR_RTL ), FRMDIR_HORI_RIGHT_TOP );
+    m_pLbFrameDir->InsertEntryValue( CUI_RESSTR( RID_SVXSTR_FRAMEDIR_SUPER ), FRMDIR_ENVIRONMENT );
 
     // This page needs ExchangeSupport.
     SetExchangeSupport();
