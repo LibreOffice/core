7255751d9a03d46766b670d742d276c9db246c4a
baltasarq <baltasarq@gmail.com>
2016-02-24 11:10:39 +0100

    Removing #defines for String_XXX in sbxres.hxx
    This is a [partial] patch for BUG #84938:
    https://bugs.documentfoundation.org/show_bug.cgi?id=84938
    
    The objective is to substitute all apparitions of #defined constants
    for enum class.
    
    The victim here is the series of constants STRING_xxx that were
    located in sbres.hxx and were changed for a StringId enum class.
    Obviously some other files where the type is used must be changed too.
    
    Change-Id: I31fa684eb5eb3508d2025c9a319d5b245cc177af

--- basic/source/sbx/sbxbool.cxx
+++ basic/source/sbx/sbxbool.cxx
@@ -70,9 +70,9 @@ enum SbxBOOL ImpGetBool( const SbxValues* p )
             nRes = SbxFALSE;
             if ( p->pOUString )
             {
-                if( p->pOUString->equalsIgnoreAsciiCase( SbxRes( STRING_TRUE ) ) )
+                if( p->pOUString->equalsIgnoreAsciiCase( SbxRes( StringId::True ) ) )
                     nRes = SbxTRUE;
-                else if( !p->pOUString->equalsIgnoreAsciiCase( SbxRes( STRING_FALSE ) ) )
+                else if( !p->pOUString->equalsIgnoreAsciiCase( SbxRes( StringId::False ) ) )
                 {
                     // it can be convertible to a number
                     bool bError = true;
@@ -174,9 +174,9 @@ void ImpPutBool( SbxValues* p, sal_Int16 n )
         case SbxSTRING:
         case SbxLPSTR:
             if ( !p->pOUString )
-                p->pOUString = new OUString( SbxRes( n ? STRING_TRUE : STRING_FALSE ) );
+                p->pOUString = new OUString( SbxRes( n ? StringId::True : StringId::False ) );
             else
-                *p->pOUString = SbxRes( n ? STRING_TRUE : STRING_FALSE );
+                *p->pOUString = SbxRes( n ? StringId::True : StringId::False );
             break;
 
         case SbxOBJECT:
