b423f5cd8578924623d5349e4d48a35ed60050ec
Michael Stahl <mstahl@redhat.com>
2016-02-26 22:35:26 +0100

    xmloff: tdf#97808: allow empty draw:marker-start/marker-end/stroke-dash
    Apparently these are allowed to be empty, which means "nothing".
    
    (regression from db1d278dcc308c73eb5edebc20481c96e7f479d8)
    
    (cherry picked from commit af57a81d0c28944b424649f024c28f444a1ab2d9)
    Reviewed-on: https://gerrit.libreoffice.org/22723
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>
    (cherry picked from commit a33a394bf3b44f56641ee8fd3bc5e29f1776c777)
    
    fix clang build
    
    Reviewed-on: https://gerrit.libreoffice.org/22745
    Reviewed-by: Michael Stahl <mstahl@redhat.com>
    Tested-by: Michael Stahl <mstahl@redhat.com>
    (cherry picked from commit 0e8e2899cf1b21aecc7bc0cbf7c9e4b775739f44)
    
    Change-Id: I13f7998e0986b26c34929afd40b1b4f0fc9efdff
    Reviewed-on: https://gerrit.libreoffice.org/22770
    Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>

--- sd/qa/unit/sdmodeltestbase.hxx
+++ sd/qa/unit/sdmodeltestbase.hxx
@@ -49,6 +49,7 @@ struct FileFormat
 #define HTML_FORMAT_TYPE ( SfxFilterFlags::EXPORT | SfxFilterFlags::ALIEN )
 #define PDF_FORMAT_TYPE  ( SfxFilterFlags::STARONEFILTER | SfxFilterFlags::ALIEN | SfxFilterFlags::IMPORT | SfxFilterFlags::PREFERED )
 #define FODG_FORMAT_TYPE  (SfxFilterFlags::STARONEFILTER | SfxFilterFlags::OWN | SfxFilterFlags::IMPORT | SfxFilterFlags::EXPORT)
+#define FODP_FORMAT_TYPE  (SfxFilterFlags::STARONEFILTER | SfxFilterFlags::OWN | SfxFilterFlags::IMPORT | SfxFilterFlags::EXPORT)
 
 /** List of file formats we support in Impress unit tests.
 
@@ -66,6 +67,7 @@ FileFormat aFileFormats[] =
     { "html", "graphic_HTML", "graphic_HTML", "", HTML_FORMAT_TYPE },
     { "pdf",  "draw_pdf_import", "pdf_Portable_Document_Format", "", PDF_FORMAT_TYPE },
     { "fodg",  "OpenDocument Drawing Flat XML", "Flat XML ODF Drawing", "", FODG_FORMAT_TYPE },
+    { "fodp",  "OpenDocument Presentation Flat XML", "Flat XML ODF Presentation", "", FODP_FORMAT_TYPE },
     { nullptr, nullptr, nullptr, nullptr, SfxFilterFlags::NONE }
 };
 
@@ -75,6 +77,7 @@ FileFormat aFileFormats[] =
 #define HTML 3
 #define PDF  4
 #define FODG 5
+#define FODP 6
 
 /// Base class for filter tests loading or roundtriping a document, and asserting the document model.
 class SdModelTestBase : public test::BootstrapFixture, public unotest::MacrosTest
