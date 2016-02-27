85fcf15ff41ceb95f46dee586ff7187551be4955
Xisco Faulí <anistenis@gmail.com>
2016-02-27 00:17:25 +0000

    Revert "SVGIO: tdf#97539: clip-path elements might contain..."
    This reverts commit 7b7ffedfa6357b902a5e354d986dac39a3b2f462.
    
    Change-Id: I018830e17cb83c5fe3574b76939673877ac2004d
    Reviewed-on: https://gerrit.libreoffice.org/22726
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Xisco Faulí <anistenis@gmail.com>

--- svgio/source/svgreader/svgstyleattributes.cxx
+++ svgio/source/svgreader/svgstyleattributes.cxx
@@ -2659,23 +2659,6 @@ namespace svgio
             return nullptr;
         }
 
-        OUString SvgStyleAttributes::getClipPathXLink() const
-        {
-            if(!maClipPathXLink.isEmpty())
-            {
-                return maClipPathXLink;
-            }
-
-            const SvgStyleAttributes* pSvgStyleAttributes = getParentStyle();
-
-            if(pSvgStyleAttributes)
-            {
-                return pSvgStyleAttributes->getClipPathXLink();
-            }
-
-            return OUString();
-        }
-
         OUString SvgStyleAttributes::getMarkerStartXLink() const
         {
             if(!maMarkerStartXLink.isEmpty())
