e3f15b3399255f5908062895cab76e32d9aa8ba0
Jaskaran <jvsg1303@gmail.com>
2016-02-28 06:55:25 +0530

    tdf#97667 Added a dump method for SfxItemSet in gridwin.cxx
    This is the first part of solving the task at tdf#97667.
    The goal is to dump SfxItemSet where our cell properties are
    stored.Improved it after inputs from moggi.Also I've commented
    out a few lines in gridwin_dbgutils.cxx That were causing build
    to fail on jenkins.Once the dump function is moved to the DBG_UTIL
    
    Change-Id: I502eaa985b3bf8d296e301d96e42f13d6fd53da5

--- sc/source/ui/inc/gridwin.hxx
+++ sc/source/ui/inc/gridwin.hxx
@@ -439,7 +439,7 @@ protected:
     void ImpDestroyOverlayObjects();
 
 private:
-
+//void dumpCellProperties();   <-- Uncomment this once you put it in DBG_UTIL
 #ifdef DBG_UTIL
     void dumpColumnInformationPixel();
     void dumpColumnInformationHmm();
