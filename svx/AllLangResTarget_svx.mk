1eb60d6e68105fa7ab078cb276f00b6c58989923
Maxim Monastirsky <momonasmon@gmail.com>
2016-02-28 14:30:24 +0200

    Revert "Unused resources"
    Why it doesn't fail in my local build?
    
    This reverts commit cd98e0725ea9b8389297b80d6c8620f4b63f482d.
    
    Change-Id: I1cf8c037cdaaf43f8fe2d42c9e2c8bd40d3608f6

--- svx/AllLangResTarget_svx.mk
+++ svx/AllLangResTarget_svx.mk
@@ -87,4 +87,8 @@ $(eval $(call gb_SrsTarget_add_nonlocalizable_files,svx/res,\
     svx/source/unodraw/unodraw.src \
 ))
 
+$(eval $(call gb_SrsTarget_add_templates,svx/res,\
+	svx/inc/globlmn_tmpl.hrc \
+))
+
 # vim: set noet sw=4 ts=4:
