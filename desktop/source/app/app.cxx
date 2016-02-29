4348b0c27286f01507ce7bb098b88852f05b5304
Michael Stahl <mstahl@redhat.com>
2016-02-29 16:30:48 +0100

    desktop: fix infinite loop with --enable-breakpad
    Change-Id: I81a0405edc29fa7205d00a5cb920b2d0e800602e

--- desktop/source/app/app.cxx
+++ desktop/source/app/app.cxx
@@ -1033,7 +1033,7 @@ bool crashReportInfoExists()
 #if HAVE_FEATURE_BREAKPAD
     std::string path = CrashReporter::getIniFileName();
     std::ifstream aFile(path);
-    while (!aFile.eof())
+    while (aFile.good())
     {
         std::string line;
         std::getline(aFile, line);
