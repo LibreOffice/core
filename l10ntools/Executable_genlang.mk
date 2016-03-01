ec01830749054ead53588479f4d9f49a251ab9f9
Jan Iversen <jani@documentfoundation.org>
2016-03-01 18:07:54 +0100

    genLang project (awareness)
    the genLang project aims at replacing all l10ntools with more
    modern versions, based on C++ and lex.
    
    The current extract works basically as a standalone "find" over the
    source tree. genLang can use that, but also a more efficient way, by
    having translation-worthy files declared in the makefile stubs.
    
    genLang itself is a C++ framework, where each file type is defined as
    a class, making it easy to add new file types.
    
    genLang can easily be adopted to transform the help files into e.g.
    mediawiki format, and later merge a url back into the code.
    
    The project was first developed (solely by the author) in a non
    published branch of OO. This branch was never merged but deleted
    and therefore never published.
    
    The files have been adapted to the LO build system and setup.
    
    The primary commit is just to raise awareness, that this is being
    developed. The following commit, will update the source code to LO 
    standard. Before replacing the old modules a dedicated review will
    be asked for.
    
    
    Change-Id: I4504992474333c476c179903f822bfaf1441cca9

---
+++ l10ntools/Executable_genlang.mk
@@ -0,0 +1,73 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_Executable_Executable,genlang))
+
+$(eval $(call gb_Executable_set_include,genlang,\
+    -I$(SRCDIR)/l10ntools/inc \
+    $$(INCLUDE) \
+))
+
+$(eval $(call gb_Executable_add_scanners,genlang,\
+    l10ntools/source/gLexPo   \
+    l10ntools/source/gLexSrc  \
+    l10ntools/source/gLexXcu  \
+    l10ntools/source/gLexXcs  \
+    l10ntools/source/gLexXrm  \
+    l10ntools/source/gLexXhp  \
+    l10ntools/source/gLexUlf  \
+    l10ntools/source/gLexTree \
+))
+
+$(eval $(call gb_Executable_add_exception_objects,genlang,\
+    l10ntools/source/gLang      \
+    l10ntools/source/gL10nMem   \
+    l10ntools/source/gL10nMemDB \
+    l10ntools/source/gHandler   \
+    l10ntools/source/gConvProp  \
+    l10ntools/source/gConv      \
+    l10ntools/source/gConvDB    \
+    l10ntools/source/gConvPo    \
+    l10ntools/source/gConvSrc   \
+    l10ntools/source/gConvXrm   \
+    l10ntools/source/gConvXhp   \
+    l10ntools/source/gConvXcs   \
+    l10ntools/source/gConvXcu   \
+    l10ntools/source/gConvUlf   \
+    l10ntools/source/gConvTree  \
+))
+
+# vim:set noet sw=4 ts=4:
+
+
+
+
+# localizer for new l10n framework
+APP1TARGET=   genLang 
+APP1OBJS=     $(OBJFILES)
+APP1RPATH=    NONE
+APP1LINKTYPE= STATIC
+
+APP1LIBS= 
+APP1STDLIBS= 
+APP1LIBSALCPPRT=
+
+
+# --- Targets ------------------------------------------------------
+.INCLUDE :  target.mk
+
+$(MISC)$/%_yy.c : %lex.l
+	flex -l -w -8 -o$@ $<
+
+# --- Files --------------------------------------------------------
+
+
+genPO: ALLTAR
+	+$(PERL) $(SOLARENV)/bin/deliver.pl
+
