55c846660f19f03e087c0aa04c8342ee53e21c69
Jan Iversen <jani@documentfoundation.org>
2016-03-01 18:07:54 +0100

    genLang project moved to LO
    the genLang project aims at replacing all l10ntools with more
    modern versions, based on C++ and lex.
    
    The current extract works basically as a standalone "find" over the
    source tree. genLang can use that, but also a more efficient way, by
    having translation-worthy files declared in the makefile stubs.
    
    genLang itself is a C++ framework, where each file type is defined as
    a class, making it easy to add new file types.
    
    genLang can easily be adopted to transform the help files into e.g.
    mediawiki format, and later merge a url back into the code.
    
    The project was first developed (solely by the author) in a non
    published branch of OO. This branch was never merged but deleted
    and therefore never published.
    
    The files have been adapted to the LO build system and setup.
    
    The primary commit is just to raise awareness, that this is being
    developed.
    
    Change-Id: I4504992474333c476c179903f822bfaf1441cca9

---
+++ l10ntools/inc/gLang.hxx
@@ -0,0 +1,164 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#ifndef GLANG_HXX
+#define GLANG_HXX
+#include <string>
+#include <vector>
+
+
+
+/*****************************************************************************
+ ***************************   G L A N G . H X X   ***************************
+ *****************************************************************************
+ * This is the class definition header of the l10n localizer program,
+ * all global classes and their interrelations is defined here
+ *****************************************************************************/
+
+
+
+/*******************   G L O B A L   D E F I N I T I O N   *******************/
+
+
+
+
+
+
+/********************   C L A S S   D E F I N I T I O N   ********************/
+class l10nMem_impl;
+class l10nMem
+{
+  public:
+    l10nMem();
+    ~l10nMem();
+
+    typedef enum
+    {
+      ENTRY_DELETED,
+      ENTRY_ADDED,
+      ENTRY_CHANGED,
+      ENTRY_NORMAL
+    } ENTRY_STATE;
+
+    static void setShowVerbose ();
+    static void setShowDebug   ();
+
+    static int  showError   (const std::string& sText, int iLineNo = 0);
+    static int  showWarning (const std::string& sText, int iLineNo = 0);
+    static void showDebug   (const std::string& sText, int iLineNo = 0);
+    static void showVerbose (const std::string& sText, int iLineNo = 0);
+    bool        isError            ();
+
+    void setModuleName (const std::string& sModuleName);
+    const std::string& getModuleName (void);
+    void setLanguage   (const std::string& sLanguage,
+                        bool               bCreate);
+    void setConvert    (bool               bConvert,
+                        bool               bStrict);
+    void loadEntryKey  (int                iLineNo,
+                        const std::string& sSourceFile,
+                        const std::string& sKey,
+                        const std::string& sOrgText,
+                        const std::string& sText,
+                        bool               bIsFuzzy);
+
+    void setSourceKey  (int                iLineNo,
+                        const std::string& sFilename,
+                        const std::string& sKey,
+                        const std::string& sText,
+                        bool               bMustExist);
+
+    void saveTemplates (const std::string& sTargetDir,
+                        bool               bKid,
+                        bool               bForce);
+    void saveLanguages (const std::string& sTargetDir,
+                        bool               bForce);
+    void dumpMem       (const std::string& sTargetDir);
+
+    int  prepareMerge  ();
+    bool getMergeLang  (std::string& sLang,
+                        std::string& sText);
+    void showNOconvert ();
+
+    void convertToInetString(std::string& sText);
+    void convertFromInetString(std::string& sText);
+};
+
+
+
+/********************   C L A S S   D E F I N I T I O N   ********************/
+class convert_gen
+{
+  public:
+    convert_gen(l10nMem&           cMemory,
+                const std::string& sSourceDir,
+                const std::string& sTargetDir,
+                const std::string& sSourceFile);
+    ~convert_gen();
+
+    // do extract/merge
+    bool execute(const bool bMerge);
+
+    // ONLY po should implement these functions
+    void startSave(const std::string& sLanguage,
+                   const std::string& sFile);
+    void save(const std::string& sFileName,
+              const std::string& sKey,
+              const std::string& sENUStext,
+              const std::string& sText,
+              bool               bFuzzy);
+    void endSave();
+    static bool checkAccess(std::string& sFile);
+    static bool createDir(std::string& sDir, std::string& sFile);
+};
+
+
+
+/********************   C L A S S   D E F I N I T I O N   ********************/
+class handler
+{
+  public:
+    handler();
+    ~handler();
+
+    void checkCommandLine(int argc, char *argv[]);
+    void run();
+
+  private:
+    enum {DO_CONVERT, DO_CONVERT_POT, DO_EXTRACT, DO_EXTRACT_KID, DO_MERGE} meWorkMode;
+    l10nMem                  mcMemory;
+    std::string              msModuleName;
+    std::string              msPoOutDir;
+    std::string              msPoDir;
+    std::string              msSourceDir;
+    std::string              msTargetDir;
+    bool                     mbForceSave;
+    std::vector<std::string> mvSourceFiles;
+    std::vector<std::string> mvLanguages;
+
+    void runConvert(bool bPot);
+    void runExtract(bool bKid);
+    void runMerge();
+
+    void showUsage(std::string& sErr);
+    void showManual();
+    void loadL10MEM(bool onlyTemplates);
+    void readFileWithSources();
+};
+#endif
