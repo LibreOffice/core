b2d679419ab55efa5c8c3d05bc55b45d78ec6bf0
Michael Stahl <mstahl@redhat.com>
2016-03-01 22:04:09 +0100

    sw: tdf#91131 when renaming a SwNumRule, also ...
    ... update the DocumentListsManager::maLists because without that all
    list styles created in the UI will have GetDefaultListStyleName() ==
    "Untitled1" and then deleting one of them deletes all these entries from
    maList, which makes the DocumentListsManager unhappy.
    
    Change-Id: Ic40e561fc540881ab29566a3f5ecdc8a180c97ef
    (cherry picked from commit 69ff012f1a2d10b3284586474f2add4e88eabcfb)

--- sw/source/core/doc/DocumentListsManager.cxx
+++ sw/source/core/doc/DocumentListsManager.cxx
@@ -182,6 +182,13 @@ void DocumentListsManager::trackChangeOfListStyleName( const OUString& sListStyl
         maListStyleLists.erase( sListStyleName );
         maListStyleLists[sNewListStyleName] = pList;
     }
+    for (auto & it : maLists) // tdf#91131 update these references too
+    {
+        if (it.second->GetDefaultListStyleName() == sListStyleName)
+        {
+            it.second->SetDefaultListStyleName(sNewListStyleName);
+        }
+    }
 }
 
 
