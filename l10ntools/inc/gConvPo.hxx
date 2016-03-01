999c68f12f1d95b16a97294949a0e6ba6d3ba259
Jan Iversen <jani@documentfoundation.org>
2016-03-01 18:07:54 +0100

    genLang project (awareness)
    the genLang project aims at replacing all l10ntools with more
    modern versions, based on C++ and lex.
    
    The current extract works basically as a standalone "find" over the
    source tree. genLang can use that, but also a more efficient way, by
    having translation-worthy files declared in the makefile stubs.
    
    genLang itself is a C++ framework, where each file type is defined as
    a class, making it easy to add new file types.
    
    genLang can easily be adopted to transform the help files into e.g.
    mediawiki format, and later merge a url back into the code.
    
    The project was first developed (solely by the author) in a non
    published branch of OO. This branch was never merged but deleted
    and therefore never published.
    
    The files have been adapted to the LO build system and setup.
    
    The primary commit is just to raise awareness, that this is being
    developed. The following commit, will update the source code to LO 
    standard. Before replacing the old modules a dedicated review will
    be asked for.
    
    
    Change-Id: I4504992474333c476c179903f822bfaf1441cca9
    Reviewed-on: https://gerrit.libreoffice.org/22819
    Reviewed-by: jan iversen <jani@documentfoundation.org>
    Tested-by: jan iversen <jani@documentfoundation.org>

---
+++ l10ntools/inc/gConvPo.hxx
@@ -0,0 +1,71 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#ifndef GCONPO_HXX
+#define GCONPO_HXX
+#include "gConv.hxx"
+
+
+
+/*****************************************************************************
+ **************************   G C O N P O . H X X   **************************
+ *****************************************************************************
+ * This is the class header for .po conversion
+ *****************************************************************************/
+
+
+
+/********************   C L A S S   D E F I N I T I O N   ********************/
+class convert_po : public convert_gen_impl
+{
+  public:
+    bool mbExpectId;
+    bool mbExpectStr;
+
+
+    convert_po(l10nMem& crMemory);
+    ~convert_po();
+
+    void startLook ();
+    void setValue  (char *syyText, int iLineCnt);
+    void setFuzzy  ();
+    void setKey    (char *syyText);
+    void setMsgId  ();
+    void setMsgStr ();
+    void handleNL  ();
+
+  private:
+    std::string  msId;
+    std::string  msStr;
+    std::string  msKey;
+    bool         mbFuzzy;
+    std::filebuf outBuffer;
+
+    void execute();
+
+    void startSave(const std::string& sLanguage,
+                   const std::string& sFile);
+    void save(const std::string& sFileName,
+              const std::string& sKey,
+              const std::string& sENUStext,
+              const std::string& sText,
+              bool               bFuzzy);
+    void endSave();
+};
+#endif
---
+++ l10ntools/inc/gConvSrc.hxx
@@ -0,0 +1,75 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#ifndef GCONSRCHXX
+#define GCONSRCHXX
+#include "gConv.hxx"
+
+
+/*****************************************************************************
+ *************************   G C O N S R C . H X X   *************************
+ *****************************************************************************
+ * This is the class header for .src conversion
+ *****************************************************************************/
+
+
+
+/********************   C L A S S   D E F I N I T I O N   ********************/
+class convert_src : public convert_gen_impl
+{
+  public:
+    bool mbExpectValue;
+
+    convert_src(l10nMem& crMemory);
+    ~convert_src();
+
+    void setValue      (char *syyText, char *sbuildValue);
+    void setLang       (char *syyText, bool bEnUs);
+    void setId         (char *syyText, bool bIde);
+    void setText       (char *syyText);
+    void setName       (char *syyText);
+    void setCmd        (char *syyText);
+    void setMacro      (char *syyText);
+    void setList       (char *syyText);
+    void setListItem   (char *syyText, bool bIsStart);
+    void setNL         (char *syyText, bool bMacro);
+    void startBlock    (char *syyText);
+    void stopBlock     (char *syyText);
+
+  private:
+    std::vector<std::string> mcStack;
+    std::string              msValue;
+    std::string              msName;
+    std::string              msTextName;
+    std::string              msCmd;
+    bool                     mbEnUs;
+    bool                     mbExpectName;
+    bool                     mbExpectMacro;
+    bool                     mbAutoPush;
+    bool                     mbValuePresent;
+    bool                     mbInList;
+    bool                     mbInListItem;
+    int                      miListCount;
+    int                      miMacroLevel;
+    void execute();
+    void trim(std::string& sText);
+    void buildKey(std::string& sKey);
+    void insertLanguagePart(std::string& sKey, std::string& sTextType);
+};
+#endif
