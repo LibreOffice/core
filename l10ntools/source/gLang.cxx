fd0676ad5013bd4d2099569f80cb99609c0fe49b
Jan Iversen <jani@documentfoundation.org>
2016-03-01 18:07:54 +0100

    genLang project moved to LO
    the genLang project aims at replacing all l10ntools with more
    modern versions, based on C++ and lex.
    
    The current extract works basically as a standalone "find" over the
    source tree. genLang can use that, but also a more efficient way, by
    having translation-worthy files declared in the makefile stubs.
    
    genLang itself is a C++ framework, where each file type is defined as
    a class, making it easu
    genLang can easily be adopted to transform the help files into e.g.
    mediawiki format, and later merge a url back into the code.
    
    The project was first developed (solely by the author) in a non
    published branch of OO. This branch was never merged but deleted
    and therefore never published.
    
    The files have been adapted to the LO build system and setup.
    
    The primary commit is just to raise awareness, that this is being
    developed.
    
    Change-Id: I4504992474333c476c179903f822bfaf1441cca9

---
+++ l10ntools/source/gLang.cxx
@@ -0,0 +1,48 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#include "gLang.hxx"
+
+
+
+/*****************************************************************************
+ ***************************   G L A N G . C X X   ***************************
+ *****************************************************************************
+ * This is the main of the l10n localizer program, it is C based and call
+ * down to classes for handling.
+ *****************************************************************************/
+
+
+
+
+/**********************   I M P L E M E N T A T I O N   **********************/
+#if defined(UNX) || defined(OS2)
+int main(int argc, char *argv[])
+#else
+int _cdecl main(int argc, char *argv[])
+#endif
+{
+  handler cHandler;
+
+  // check command line (exit if problems)
+  cHandler.checkCommandLine(argc, argv);
+
+  // command line is ok, so execute it
+  cHandler.run();
+}
