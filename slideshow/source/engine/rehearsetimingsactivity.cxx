LibreOffice/core

commit f29c0b2b3e8861909fa2c6c37bf631ab01590541

boost::shared_ptr->std::shared_ptr in slideshow
Change-Id: I27da6bc550488ea65ccdf1d26f8178f803f495d6

slideshow/source/engine/rehearsetimingsactivity.cxx
@@ -103,7 +103,7 @@ public:
 private:
     ::canvas::tools::ElapsedTime    maTimer;
     double                          mnNextTime;
-    boost::weak_ptr<Activity>       mpActivity;
+    std::weak_ptr<Activity>       mpActivity;
     ActivitiesQueue&                mrActivityQueue;
 };
 
@@ -188,10 +188,10 @@ RehearseTimingsActivity::~RehearseTimingsActivity()
     }
 }
 
-boost::shared_ptr<RehearseTimingsActivity> RehearseTimingsActivity::create(
+std::shared_ptr<RehearseTimingsActivity> RehearseTimingsActivity::create(
     const SlideShowContext& rContext )
 {
-    boost::shared_ptr<RehearseTimingsActivity> pActivity(
+    std::shared_ptr<RehearseTimingsActivity> pActivity(
         new RehearseTimingsActivity( rContext ));
 
     pActivity->mpMouseHandler.reset(
@@ -217,7 +217,7 @@ void RehearseTimingsActivity::start()
     for_each_sprite( []( const ::cppcanvas::CustomSpriteSharedPtr& pSprite )
                      { return pSprite->show(); } );
 
-    mrActivitiesQueue.addActivity( shared_from_this() );
+    mrActivitiesQueue.addActivity( std::shared_ptr<Activity>((Activity*)this) );
 
     mpMouseHandler->reset();
     mrEventMultiplexer.addClickHandler(

slideshow/source/engine/shapes/drawinglayeranimation.cxx
@@ -53,10 +53,9 @@
 #include "intrinsicanimationactivity.hxx"
 #include "intrinsicanimationeventhandler.hxx"
 
-#include <boost/weak_ptr.hpp>
-#include <boost/enable_shared_from_this.hpp>
 #include <boost/noncopyable.hpp>
 #include <vector>
+#include <memory>
 
 using namespace com::sun::star;
 using namespace ::slideshow::internal;
@@ -131,7 +130,6 @@ double ScrollTextAnimNode::GetStateAtRelativeTime(
 }
 
 class ActivityImpl : public Activity,
-                     public boost::enable_shared_from_this<ActivityImpl>,
                      private boost::noncopyable
 {
 public:
@@ -139,8 +137,8 @@ public:
 
     ActivityImpl(
         SlideShowContext const& rContext,
-        boost::shared_ptr<WakeupEvent> const& pWakeupEvent,
-        boost::shared_ptr<DrawShape> const& pDrawShape );
+        std::shared_ptr<WakeupEvent> const& pWakeupEvent,
+        std::shared_ptr<DrawShape> const& pDrawShape );
 
     bool enableAnimations();
 
@@ -194,8 +192,8 @@ private:
 
 
     SlideShowContext                            maContext;
-    boost::shared_ptr<WakeupEvent>              mpWakeupEvent;
-    boost::weak_ptr<DrawShape>                  mpParentDrawShape;
+    std::shared_ptr<WakeupEvent>              mpWakeupEvent;
+    std::weak_ptr<DrawShape>                  mpParentDrawShape;
     DrawShapeSharedPtr                          mpDrawShape;
     ShapeAttributeLayerHolder                   maShapeAttrLayer;
     GDIMetaFileSharedPtr                        mpMetaFile;
@@ -736,8 +734,8 @@ bool ActivityImpl::perform()
 
 ActivityImpl::ActivityImpl(
     SlideShowContext const& rContext,
-    boost::shared_ptr<WakeupEvent> const& pWakeupEvent,
-    boost::shared_ptr<DrawShape> const& pParentDrawShape )
+    std::shared_ptr<WakeupEvent> const& pWakeupEvent,
+    std::shared_ptr<DrawShape> const& pParentDrawShape )
     : maContext(rContext),
       mpWakeupEvent(pWakeupEvent),
       mpParentDrawShape(pParentDrawShape),
@@ -770,7 +768,7 @@ ActivityImpl::ActivityImpl(
     // TODO(Q3): Doing this manually, instead of using
     // ShapeSubset. This is because of lifetime issues (ShapeSubset
     // generates circular references to parent shape)
-    mpDrawShape = boost::dynamic_pointer_cast<DrawShape>(
+    mpDrawShape = std::dynamic_pointer_cast<DrawShape>(
         maContext.mpSubsettableShapeManager->getSubsetShape(
             pParentDrawShape,
             scrollTextNode ));
@@ -861,8 +859,7 @@ ActivityImpl::ActivityImpl(
 bool ActivityImpl::enableAnimations()
 {
     mbIsActive = true;
-    return maContext.mrActivitiesQueue.addActivity(
-        shared_from_this() );
+    return maContext.mrActivitiesQueue.addActivity( ActivitySharedPtr(this) );
 }
 
 ActivityImpl::~ActivityImpl()
@@ -936,15 +933,15 @@ void ActivityImpl::end()
 namespace slideshow {
 namespace internal {
 
-boost::shared_ptr<Activity> createDrawingLayerAnimActivity(
+std::shared_ptr<Activity> createDrawingLayerAnimActivity(
     SlideShowContext const& rContext,
-    boost::shared_ptr<DrawShape> const& pDrawShape )
+    std::shared_ptr<DrawShape> const& pDrawShape )
 {
-    boost::shared_ptr<Activity> pActivity;
+    std::shared_ptr<Activity> pActivity;
 
     try
     {
-        boost::shared_ptr<WakeupEvent> const pWakeupEvent(
+        std::shared_ptr<WakeupEvent> const pWakeupEvent(
             new WakeupEvent( rContext.mrEventQueue.getTimer(),
                              rContext.mrActivitiesQueue ) );
         pActivity.reset( new ActivityImpl( rContext, pWakeupEvent, pDrawShape ) );

slideshow/source/engine/shapes/intrinsicanimationactivity.cxx
@@ -28,8 +28,7 @@
 #include "intrinsicanimationeventhandler.hxx"
 
 #include <boost/noncopyable.hpp>
-#include <boost/enable_shared_from_this.hpp>
-#include <boost/weak_ptr.hpp>
+#include <memory>
 
 namespace slideshow
 {
@@ -43,7 +42,6 @@ namespace slideshow
             animations, or GIF animations.
          */
         class IntrinsicAnimationActivity : public Activity,
-                                           public boost::enable_shared_from_this<IntrinsicAnimationActivity>,
                                            private boost::noncopyable
         {
         public:
@@ -83,7 +81,7 @@ namespace slideshow
 
         private:
             SlideShowContext                        maContext;
-            boost::weak_ptr<DrawShape>              mpDrawShape;
+            std::weak_ptr<DrawShape>              mpDrawShape;
             WakeupEventSharedPtr                    mpWakeupEvent;
             IntrinsicAnimationEventHandlerSharedPtr mpListener;
             ::std::vector<double>                   maTimeouts;
@@ -250,8 +248,7 @@ namespace slideshow
         bool IntrinsicAnimationActivity::enableAnimations()
         {
             mbIsActive = true;
-            return maContext.mrActivitiesQueue.addActivity(
-                shared_from_this() );
+            return maContext.mrActivitiesQueue.addActivity( ActivitySharedPtr(this) );
         }
 
 

slideshow/source/engine/transitions/slidechangebase.cxx
@@ -176,7 +176,7 @@ void SlideChangeBase::prefetch( const AnimatableShapeSharedPtr&,
         return;
 
     // register ourselves for view change events
-    mrEventMultiplexer.addViewHandler( shared_from_this() );
+    mrEventMultiplexer.addViewHandler( std::shared_ptr<ViewEventHandler>((ViewEventHandler*)this) );
 
     // init views and create slide bitmaps
     for( const auto& pView : mrViewContainer )
@@ -251,7 +251,7 @@ void SlideChangeBase::end()
     mbSpritesVisible = false;
 
     // remove also from event multiplexer, we're dead anyway
-    mrEventMultiplexer.removeViewHandler( shared_from_this() );
+    mrEventMultiplexer.removeViewHandler( std::shared_ptr<ViewEventHandler>((ViewEventHandler*)this) );
 }
 
 bool SlideChangeBase::operator()( double nValue )

slideshow/source/inc/disposable.hxx
@@ -21,7 +21,7 @@
 #define INCLUDED_SLIDESHOW_SOURCE_INC_DISPOSABLE_HXX
 
 #include <sal/types.h>
-#include <boost/shared_ptr.hpp>
+#include <memory>
 
 
 /* Definition of Disposable interface */
@@ -30,6 +30,14 @@ namespace slideshow
 {
     namespace internal
     {
+        /**
+         * Base class for being a shared pointer, since quite a few of the downstream classes
+         * want to be stored using std::shared_ptr.
+         */
+        class SharedPtrAble : public std::enable_shared_from_this<SharedPtrAble>
+        {
+        };
+
         /** Disposable interface
 
             With ref-counted objects, deleting object networks

commit 037de512546917786c313d23995daaa0521a8e6e

tdf#98175 fix Impress crash
Caused by my commit f29c0b2b3e8861909fa2c6c37bf631ab01590541
"boost::shared_ptr->std::shared_ptr in slideshow"

where I foolishly converted
   shared_from_this()
to
   std::shared_ptr<Activity>((Activity*)this) )
all over the place, which is bad, I should have used
   std::dynamic_pointer_cast<ViewEventHandler>(shared_from_this())

Ah, the joys of C++ memory management.

Change-Id: I9b538e78730c9107870f406cb8ba482d491dd568

slideshow/source/engine/rehearsetimingsactivity.cxx
@@ -217,7 +217,7 @@ void RehearseTimingsActivity::start()
     for_each_sprite( []( const ::cppcanvas::CustomSpriteSharedPtr& pSprite )
                      { return pSprite->show(); } );
 
-    mrActivitiesQueue.addActivity( std::shared_ptr<Activity>(this) );
+    mrActivitiesQueue.addActivity( std::dynamic_pointer_cast<Activity>(shared_from_this()) );
 
     mpMouseHandler->reset();
     mrEventMultiplexer.addClickHandler(

slideshow/source/engine/shapes/drawinglayeranimation.cxx
@@ -859,7 +859,7 @@ ActivityImpl::ActivityImpl(
 bool ActivityImpl::enableAnimations()
 {
     mbIsActive = true;
-    return maContext.mrActivitiesQueue.addActivity( ActivitySharedPtr(this) );
+    return maContext.mrActivitiesQueue.addActivity( std::dynamic_pointer_cast<Activity>(shared_from_this()) );
 }
 
 ActivityImpl::~ActivityImpl()

slideshow/source/engine/shapes/intrinsicanimationactivity.cxx
@@ -248,7 +248,8 @@ namespace slideshow
         bool IntrinsicAnimationActivity::enableAnimations()
         {
             mbIsActive = true;
-            return maContext.mrActivitiesQueue.addActivity( ActivitySharedPtr(this) );
+            return maContext.mrActivitiesQueue.addActivity( std::dynamic_pointer_cast<Activity>(shared_from_this()) );
+
         }
 
 

slideshow/source/engine/transitions/slidechangebase.cxx
@@ -176,7 +176,7 @@ void SlideChangeBase::prefetch( const AnimatableShapeSharedPtr&,
         return;
 
     // register ourselves for view change events
-    mrEventMultiplexer.addViewHandler( std::shared_ptr<ViewEventHandler>(this) );
+    mrEventMultiplexer.addViewHandler( std::dynamic_pointer_cast<ViewEventHandler>(shared_from_this()) );
 
     // init views and create slide bitmaps
     for( const auto& pView : mrViewContainer )
@@ -251,7 +251,7 @@ void SlideChangeBase::end()
     mbSpritesVisible = false;
 
     // remove also from event multiplexer, we're dead anyway
-    mrEventMultiplexer.removeViewHandler( std::shared_ptr<ViewEventHandler>(this) );
+    mrEventMultiplexer.removeViewHandler( std::dynamic_pointer_cast<ViewEventHandler>(shared_from_this()) );
 }
 
 bool SlideChangeBase::operator()( double nValue )

slideshow/source/inc/disposable.hxx
@@ -36,6 +36,8 @@ namespace slideshow
          */
         class SharedPtrAble : public std::enable_shared_from_this<SharedPtrAble>
         {
+        public:
+            virtual ~SharedPtrAble() {}
         };
 
         /** Disposable interface