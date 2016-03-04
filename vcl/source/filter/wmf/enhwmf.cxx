LibreOffice/core

commit 09c722873b2d378d2d155f5f1dd7d8f3fb2012e9

EMF/WMF: fix rendering of pen styles (dash, dot, dashdot, dashdotdot)
Change-Id: I226bac370601b75f2589f7a7c5e8830746b31e2e

vcl/source/filter/wmf/enhwmf.cxx
@@ -730,19 +730,37 @@ sal_Bool EnhWMFReader::ReadEnhWMF()
                             aLineInfo.SetWidth( aSize.Width() );
 
                         sal_Bool bTransparent = sal_False;
-                        sal_uInt16 nDashCount = 0;
-                        sal_uInt16 nDotCount = 0;
                         switch( nStyle & 0xFF )
                         {
                             case PS_DASHDOTDOT :
-                                nDotCount++;
+                                aLineInfo.SetStyle( LINE_DASH );
+                                aLineInfo.SetDashCount( 1 );
+                                aLineInfo.SetDotCount( 2 );
+                                aLineInfo.SetDashLen( 150 );
+                                aLineInfo.SetDotLen( 30 );
+                                aLineInfo.SetDistance( 50 );
+                            break;
                             case PS_DASHDOT :
-                                nDashCount++;
+                                aLineInfo.SetStyle( LINE_DASH );
+                                aLineInfo.SetDashCount( 1 );
+                                aLineInfo.SetDotCount( 1 );
+                                aLineInfo.SetDashLen( 150 );
+                                aLineInfo.SetDotLen( 30 );
+                                aLineInfo.SetDistance( 90 );
+                            break;
                             case PS_DOT :
-                                nDotCount++;
+                                aLineInfo.SetStyle( LINE_DASH );
+                                aLineInfo.SetDashCount( 0 );
+                                aLineInfo.SetDotCount( 1 );
+                                aLineInfo.SetDotLen( 30 );
+                                aLineInfo.SetDistance( 50 );
                             break;
                             case PS_DASH :
-                                nDashCount++;
+                                aLineInfo.SetStyle( LINE_DASH );
+                                aLineInfo.SetDashCount( 1 );
+                                aLineInfo.SetDotCount( 0 );
+                                aLineInfo.SetDashLen( 225 );
+                                aLineInfo.SetDistance( 100 );
                             break;
                             case PS_NULL :
                                 bTransparent = sal_True;
@@ -778,12 +796,6 @@ sal_Bool EnhWMFReader::ReadEnhWMF()
                             break;
                             default :
                                 aLineInfo.SetLineJoin ( basegfx::B2DLINEJOIN_NONE );
-                        }
-                        if ( nDashCount | nDotCount )
-                        {
-                            aLineInfo.SetStyle( LINE_DASH );
-                            aLineInfo.SetDashCount( nDashCount );
-                            aLineInfo.SetDotCount( nDotCount );
                         }
                         pOut->CreateObject( nIndex, GDI_PEN, new WinMtfLineStyle( ReadColor(), aLineInfo, bTransparent ) );
                     }

vcl/source/filter/wmf/winmtf.cxx
@@ -713,14 +713,6 @@ void WinMtfOutput::CreateObject( GDIObjectType eType, void* pStyle )
         {
             Size aSize( ((WinMtfLineStyle*)pStyle)->aLineInfo.GetWidth(), 0 );
             ((WinMtfLineStyle*)pStyle)->aLineInfo.SetWidth( ImplMap( aSize ).Width() );
-            if ( ((WinMtfLineStyle*)pStyle)->aLineInfo.GetStyle() == LINE_DASH )
-            {
-                aSize.Width() += 1;
-                long nDotLen = ImplMap( aSize ).Width();
-                ((WinMtfLineStyle*)pStyle)->aLineInfo.SetDistance( nDotLen );
-                ((WinMtfLineStyle*)pStyle)->aLineInfo.SetDotLen( nDotLen );
-                ((WinMtfLineStyle*)pStyle)->aLineInfo.SetDashLen( nDotLen * 4 );
-            }
         }
     }
     sal_uInt32 nIndex;
@@ -749,14 +741,6 @@ void WinMtfOutput::CreateObject( sal_Int32 nIndex, GDIObjectType eType, void* pS
             {
                 Size aSize( ((WinMtfLineStyle*)pStyle)->aLineInfo.GetWidth(), 0 );
                 ((WinMtfLineStyle*)pStyle)->aLineInfo.SetWidth( ImplMap( aSize ).Width() );
-                if ( ((WinMtfLineStyle*)pStyle)->aLineInfo.GetStyle() == LINE_DASH )
-                {
-                    aSize.Width() += 1;
-                    long nDotLen = ImplMap( aSize ).Width();
-                    ((WinMtfLineStyle*)pStyle)->aLineInfo.SetDistance( nDotLen );
-                    ((WinMtfLineStyle*)pStyle)->aLineInfo.SetDotLen( nDotLen );
-                    ((WinMtfLineStyle*)pStyle)->aLineInfo.SetDashLen( nDotLen * 4 );
-                }
             }
         }
         if ( (sal_uInt32)nIndex >= vGDIObj.size() )

vcl/source/filter/wmf/winwmf.cxx
@@ -749,19 +749,37 @@ void WMFReader::ReadRecordParams( sal_uInt16 nFunc )
                 aLineInfo.SetWidth( nWidth );
 
             sal_Bool bTransparent = sal_False;
-            sal_uInt16 nDashCount = 0;
-            sal_uInt16 nDotCount = 0;
             switch( nStyle & 0xFF )
             {
                 case PS_DASHDOTDOT :
-                    nDotCount++;
+                    aLineInfo.SetStyle( LINE_DASH );
+                    aLineInfo.SetDashCount( 1 );
+                    aLineInfo.SetDotCount( 2 );
+                    aLineInfo.SetDashLen( 150 );
+                    aLineInfo.SetDotLen( 30 );
+                    aLineInfo.SetDistance( 50 );
+                break;
                 case PS_DASHDOT :
-                    nDashCount++;
+                    aLineInfo.SetStyle( LINE_DASH );
+                    aLineInfo.SetDashCount( 1 );
+                    aLineInfo.SetDotCount( 1 );
+                    aLineInfo.SetDashLen( 150 );
+                    aLineInfo.SetDotLen( 30 );
+                    aLineInfo.SetDistance( 90 );
+                break;
                 case PS_DOT :
-                    nDotCount++;
+                    aLineInfo.SetStyle( LINE_DASH );
+                    aLineInfo.SetDashCount( 0 );
+                    aLineInfo.SetDotCount( 1 );
+                    aLineInfo.SetDotLen( 30 );
+                    aLineInfo.SetDistance( 50 );
                 break;
                 case PS_DASH :
-                    nDashCount++;
+                    aLineInfo.SetStyle( LINE_DASH );
+                    aLineInfo.SetDashCount( 1 );
+                    aLineInfo.SetDotCount( 0 );
+                    aLineInfo.SetDashLen( 225 );
+                    aLineInfo.SetDistance( 100 );
                 break;
                 case PS_NULL :
                     bTransparent = sal_True;
@@ -797,12 +815,6 @@ void WMFReader::ReadRecordParams( sal_uInt16 nFunc )
                 break;
                 default :
                     aLineInfo.SetLineJoin ( basegfx::B2DLINEJOIN_NONE );
-            }
-            if ( nDashCount | nDotCount )
-            {
-                aLineInfo.SetStyle( LINE_DASH );
-                aLineInfo.SetDashCount( nDashCount );
-                aLineInfo.SetDotCount( nDotCount );
             }
             pOut->CreateObject( GDI_PEN, new WinMtfLineStyle( ReadColor(), aLineInfo, bTransparent ) );
         }

commit f6bb6a7529adca3dc6da4df14383819d239f1bed

tdf#79679 vcl: dashed lines show as solid lines when importing EMF files
Issue is a regression in commit 09c722873b2d378d2d155f5f1dd7d8f3fb2012e9.
(EMF/WMF: fix rendering of pen styles (dash, dot, dashdot, dashdotdot).

Change-Id: Ie8b5fa396e4fb0f480cb3594c8129a59f472c1b8

vcl/source/filter/wmf/enhwmf.cxx
@@ -911,41 +911,23 @@ bool EnhWMFReader::ReadEnhWMF()
                         if ( aSize.Width() )
                             aLineInfo.SetWidth( aSize.Width() );
 
-                        bool bTransparent = false;
+                        sal_Bool bTransparent = false;
+                        sal_uInt16 nDashCount = 0;
+                        sal_uInt16 nDotCount = 0;
                         switch( nStyle & 0xFF )
                         {
                             case PS_DASHDOTDOT :
-                                aLineInfo.SetStyle( LINE_DASH );
-                                aLineInfo.SetDashCount( 1 );
-                                aLineInfo.SetDotCount( 2 );
-                                aLineInfo.SetDashLen( 150 );
-                                aLineInfo.SetDotLen( 30 );
-                                aLineInfo.SetDistance( 50 );
-                            break;
+                                nDotCount++;
                             case PS_DASHDOT :
-                                aLineInfo.SetStyle( LINE_DASH );
-                                aLineInfo.SetDashCount( 1 );
-                                aLineInfo.SetDotCount( 1 );
-                                aLineInfo.SetDashLen( 150 );
-                                aLineInfo.SetDotLen( 30 );
-                                aLineInfo.SetDistance( 90 );
-                            break;
+                                nDashCount++;
                             case PS_DOT :
-                                aLineInfo.SetStyle( LINE_DASH );
-                                aLineInfo.SetDashCount( 0 );
-                                aLineInfo.SetDotCount( 1 );
-                                aLineInfo.SetDotLen( 30 );
-                                aLineInfo.SetDistance( 50 );
+                                nDotCount++;
                             break;
                             case PS_DASH :
-                                aLineInfo.SetStyle( LINE_DASH );
-                                aLineInfo.SetDashCount( 1 );
-                                aLineInfo.SetDotCount( 0 );
-                                aLineInfo.SetDashLen( 225 );
-                                aLineInfo.SetDistance( 100 );
+                                nDashCount++;
                             break;
                             case PS_NULL :
-                                bTransparent = true;
+                                bTransparent = sal_True;
                                 aLineInfo.SetStyle( LINE_NONE );
                             break;
                             case PS_INSIDEFRAME :

vcl/source/filter/wmf/winmtf.cxx
@@ -727,10 +727,16 @@ void WinMtfOutput::CreateObject( sal_Int32 nIndex, GDIObjectType eType, void* pS
             }
             else if ( eType == GDI_PEN )
             {
-                WinMtfLineStyle* pLineStyle = static_cast<WinMtfLineStyle*>(pStyle);
-                Size aSize(pLineStyle->aLineInfo.GetWidth(), 0);
-                aSize = ImplMap(aSize);
-                pLineStyle->aLineInfo.SetWidth(aSize.Width());
+                Size aSize( ((WinMtfLineStyle*)pStyle)->aLineInfo.GetWidth(), 0 );
+                ((WinMtfLineStyle*)pStyle)->aLineInfo.SetWidth( ImplMap( aSize ).Width() );
+                if ( ((WinMtfLineStyle*)pStyle)->aLineInfo.GetStyle() == LINE_DASH )
+                {
+                    aSize.Width() += 1;
+                    long nDotLen = ImplMap( aSize ).Width();
+                    ((WinMtfLineStyle*)pStyle)->aLineInfo.SetDistance( nDotLen );
+                    ((WinMtfLineStyle*)pStyle)->aLineInfo.SetDotLen( nDotLen );
+                    ((WinMtfLineStyle*)pStyle)->aLineInfo.SetDashLen( nDotLen * 4 );
+                }
             }
         }
         if ( (sal_uInt32)nIndex >= vGDIObj.size() )

vcl/source/filter/wmf/winwmf.cxx
@@ -811,37 +811,19 @@ void WMFReader::ReadRecordParams( sal_uInt16 nFunc )
                 aLineInfo.SetWidth(nWidth);
 
             bool bTransparent = false;
+            sal_uInt16 nDashCount = 0;
+            sal_uInt16 nDotCount = 0;
             switch( nStyle & 0xFF )
             {
                 case PS_DASHDOTDOT :
-                    aLineInfo.SetStyle( LINE_DASH );
-                    aLineInfo.SetDashCount( 1 );
-                    aLineInfo.SetDotCount( 2 );
-                    aLineInfo.SetDashLen( 150 );
-                    aLineInfo.SetDotLen( 30 );
-                    aLineInfo.SetDistance( 50 );
-                break;
+                    nDotCount++;
                 case PS_DASHDOT :
-                    aLineInfo.SetStyle( LINE_DASH );
-                    aLineInfo.SetDashCount( 1 );
-                    aLineInfo.SetDotCount( 1 );
-                    aLineInfo.SetDashLen( 150 );
-                    aLineInfo.SetDotLen( 30 );
-                    aLineInfo.SetDistance( 90 );
-                break;
+                    nDashCount++;
                 case PS_DOT :
-                    aLineInfo.SetStyle( LINE_DASH );
-                    aLineInfo.SetDashCount( 0 );
-                    aLineInfo.SetDotCount( 1 );
-                    aLineInfo.SetDotLen( 30 );
-                    aLineInfo.SetDistance( 50 );
+                    nDotCount++;
                 break;
                 case PS_DASH :
-                    aLineInfo.SetStyle( LINE_DASH );
-                    aLineInfo.SetDashCount( 1 );
-                    aLineInfo.SetDotCount( 0 );
-                    aLineInfo.SetDashLen( 225 );
-                    aLineInfo.SetDistance( 100 );
+                    nDashCount++;
                 break;
                 case PS_NULL :
                     bTransparent = true;