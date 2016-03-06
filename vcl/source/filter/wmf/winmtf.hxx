c9c611fd76499968ad851a2d6594294d5f3f915b
Chris Sherlock <chris.sherlock79@gmail.com>
2016-03-07 10:21:34 +1100

    vcl: fix winmtf.hxx file to include constants
    Change-Id: I3aa97c31d5e20bd506a8090f2613a21b9cc47b0b
    Reviewed-on: https://gerrit.libreoffice.org/22964
    Reviewed-by: Chris Sherlock <chris.sherlock79@gmail.com>
    Tested-by: Chris Sherlock <chris.sherlock79@gmail.com>

--- vcl/source/filter/wmf/winmtf.hxx
+++ vcl/source/filter/wmf/winmtf.hxx
@@ -148,14 +148,15 @@ struct WMF_EXTERNALHEADER;
 #define PS_INSIDEFRAME          6
 #define PS_STYLE_MASK           15
 
-#define PS_ENDCAP_ROUND     0x000
-#define PS_ENDCAP_SQUARE    0x100
-#define PS_ENDCAP_FLAT      0x200
+#define PS_ENDCAP_ROUND      0x000
+#define PS_ENDCAP_SQUARE     0x100
+#define PS_ENDCAP_FLAT       0x200
+#define PS_ENDCAP_STYLE_MASK 0xF00
 
 #define PS_JOIN_ROUND       0x0000
 #define PS_JOIN_BEVEL       0x1000
 #define PS_JOIN_MITER       0x2000
-
+#define PS_JOIN_STYLE_MASK  0xF000
 
 #define ANSI_CHARSET            0
 #define DEFAULT_CHARSET         1
