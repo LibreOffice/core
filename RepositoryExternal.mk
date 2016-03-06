f2fe3b8877ed29514257e9dc99e1de6a9ce45055
Kohei Yoshida <kohei.yoshida@gmail.com>
2016-03-05 20:09:34 -0500

    Update liborcus to 0.11.0.
    Markus did half of this, and I simply carried the torch.
    
    Change-Id: Icd3ee1e4b5bd562faee06dfd9dce52a4bed49a71

--- RepositoryExternal.mk
+++ RepositoryExternal.mk
@@ -3128,7 +3128,7 @@ $(call gb_LinkTarget_set_include,$(1),\
 )
 
 $(call gb_LinkTarget_add_libs,$(1),\
-	-L$(call gb_UnpackedTarball_get_dir,liborcus)/src/liborcus/.libs -lorcus-0.10 \
+       -L$(call gb_UnpackedTarball_get_dir,liborcus)/src/liborcus/.libs -lorcus-0.11 \
 )
 
 $(if $(SYSTEM_BOOST), \
@@ -3147,7 +3147,7 @@ $(call gb_LinkTarget_set_include,$(1),\
 )
 
 $(call gb_LinkTarget_add_libs,$(1),\
-	-L$(call gb_UnpackedTarball_get_dir,liborcus)/src/parser/.libs -lorcus-parser-0.10 \
+	-L$(call gb_UnpackedTarball_get_dir,liborcus)/src/parser/.libs -lorcus-parser-0.11 \
 )
 
 endef
