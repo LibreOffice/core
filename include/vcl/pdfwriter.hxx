2a4211319b1402a67b027d21c6576a88e265d974
Katarina Behrens <Katarina.Behrens@cib.de>
2016-03-08 11:41:26 +0100

    Show cell formula as annotation in exported PDF
    Change-Id: Idca8f6a27453a0f41566098a1720ee345eff3af6

--- include/vcl/pdfwriter.hxx
+++ include/vcl/pdfwriter.hxx
@@ -1058,6 +1058,7 @@ The following structure describes the permissions used in PDF security
     or -1 in which case the current page is used
     */
     void CreateNote( const Rectangle& rRect, const PDFNote& rNote, sal_Int32 nPageNr = -1 );
+    void CreateFormulaAnnotation( const Rectangle& rRect, const PDFNote& rNote, sal_Int32 nPageNr = -1 );
 
     /** begin a new logical structure element
 
