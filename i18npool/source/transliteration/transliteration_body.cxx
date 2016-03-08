987626ee9482407b666abd1d9578aacb410e9935
Stephan Bergmann <sbergman@redhat.com>
2016-03-08 16:03:49 +0100

    Remove dead code
    Change-Id: Iebaf8af5dbf11cd255f67202bbb18f61b1bd39fd

--- i18npool/source/transliteration/transliteration_body.cxx
+++ i18npool/source/transliteration/transliteration_body.cxx
@@ -301,11 +301,6 @@ static OUString transliterate_titlecase_Impl(
             pOffset[i] = nIdx;
         }
     }
-#if OSL_DEBUG_LEVEL > 0
-    const sal_Int32 *pCOffset = offset.getConstArray();
-    (void) pCOffset;
-#endif
-
     return aRes;
 }
 
