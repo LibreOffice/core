5d3448ebc625440d063282037b825b4832dfd6db
Jakub Trzebiatowski <ubap.dev@gmail.com>
2016-03-11 20:58:34 +0100

    tdf#90855 Improve the 'Insert Bookmark' dialog
    implemented:
    - SvSimpleTable
    - display page number
    - displaying text following bookmark
    - go to bookmark
    - selecting multiple bookmarks in TableView or in EditField by separating names using ";"
    - sorting by any column
    
    possible improvements (might need extra work):
    - sorting bookmarks by page should sort them chronologically (consider position on page),
      now they are sorted chronologically only at beginning
    
    It is work in progress
    
    Change-Id: I7523dc066380bc360bd484c88a6f4ba45e867320

--- sw/source/uibase/inc/bookmark.hxx
+++ sw/source/uibase/inc/bookmark.hxx
@@ -21,35 +21,42 @@
 
 #include <svx/stddlg.hxx>
 #include <vcl/fixed.hxx>
+#include <ndtxt.hxx>
 
 #include <vcl/button.hxx>
+#include <svtools/simptabl.hxx>
+#include <pam.hxx>
 
 #include "swlbox.hxx"
+#include "IMark.hxx"
 
 class SwWrtShell;
 class SfxRequest;
 
-class BookmarkCombo : public SwComboBox
+class BookmarkTable : public SvSimpleTable
 {
-    sal_Int32           GetFirstSelEntryPos() const;
-    sal_Int32           GetNextSelEntryPos(sal_Int32  nPos) const;
-    sal_Int32           GetSelEntryPos(sal_Int32  nPos) const;
-
-    virtual bool    PreNotify(NotifyEvent& rNEvt) override;
+    /*virtual bool    PreNotify(NotifyEvent& rNEvt) override;*/
+    SvTreeListEntry*    GetRowByBookmarkName(OUString name);
 public:
-    BookmarkCombo(vcl::Window* pWin, WinBits nStyle);
-
-    sal_Int32           GetSelectEntryCount() const;
-    sal_Int32           GetSelectEntryPos( sal_Int32  nSelIndex = 0 ) const;
+    BookmarkTable(SvSimpleTableContainer& rParent, WinBits nStyle);
+    void                InsertBookmark(sw::mark::IMark* pMark);
+    sw::mark::IMark*    GetBookmarkByName(OUString sName);
+    void                SelectByName(OUString sName);
+    OUString            GetNameProposal();
 
     static const OUString aForbiddenChars;
+    static const OUString sDefaultBookmarkName;
 };
 
 class SwInsertBookmarkDlg: public SvxStandardDialog
 {
-    VclPtr<BookmarkCombo>  m_pBookmarkBox;
-    VclPtr<OKButton>       m_pOkBtn;
-    VclPtr<PushButton>     m_pDeleteBtn;
+    VclPtr<SvSimpleTableContainer>      m_BookmarksContainer;
+    VclPtr<BookmarkTable>               m_pBookmarksBox;
+    VclPtr<Edit>                        m_pEditBox;
+    VclPtr<OKButton>                    m_pOkBtn;
+    VclPtr<PushButton>                  m_pDeleteBtn;
+    VclPtr<PushButton>                  m_pGotoBtn;
+
 
     OUString        sRemoveWarning;
     SwWrtShell      &rSh;
@@ -57,7 +64,11 @@ class SwInsertBookmarkDlg: public SvxStandardDialog
 
     DECL_LINK_TYPED(ModifyHdl, Edit&, void);
     DECL_LINK_TYPED(DeleteHdl, Button*, void);
+    DECL_LINK_TYPED(GotoHdl, Button*, void);
+    DECL_LINK_TYPED(SelectionChangedHdl, SvTreeListBox*, void);
+    DECL_LINK_TYPED(DoubleClickHdl, SvTreeListBox*, bool);
 
+    void GoToSelectedBookmark();
     virtual void Apply() override;
 
 public:
