c3230bd3cc43d2fbd8cb4139ceed2b0520f5b4d8
Markus Mohrhard <markus.mohrhard@googlemail.com>
2016-03-11 09:11:02 +0100

    no need for fancy delete macros for local variables
    Change-Id: Ib81562dda616ab6c9e2ad69c076818a9a2c42085

--- sc/source/core/data/stlpool.cxx
+++ sc/source/core/data/stlpool.cxx
@@ -331,7 +331,7 @@ void ScStyleSheetPool::CreateStandardStyles()
     pHFSetItem = new SvxSetItem( static_cast<const SvxSetItem&>(pSet->Get( ATTR_PAGE_HEADERSET ) ) );
     pSet->Put( *pHFSetItem, ATTR_PAGE_HEADERSET );
     pSet->Put( *pHFSetItem, ATTR_PAGE_FOOTERSET );
-    DELETEZ( pHFSetItem );
+    delete pHFSetItem;
 
     // Header:
     // [empty][\sheet\][empty]
@@ -343,7 +343,7 @@ void ScStyleSheetPool::CreateStandardStyles()
     pHeaderItem->SetCenterArea( *pTxtObj );
     pHeaderItem->SetRightArea ( *pEmptyTxtObj );
     pSet->Put( *pHeaderItem );
-    DELETEZ( pTxtObj );
+    delete pTxtObj;
 
     // Footer:
     // [empty][Page \STR_PAGE\][empty]
@@ -357,7 +357,7 @@ void ScStyleSheetPool::CreateStandardStyles()
     pFooterItem->SetCenterArea( *pTxtObj );
     pFooterItem->SetRightArea ( *pEmptyTxtObj );
     pSet->Put( *pFooterItem );
-    DELETEZ( pTxtObj );
+    delete pTxtObj;
 
     // 2. Report
 
@@ -389,7 +389,7 @@ void ScStyleSheetPool::CreateStandardStyles()
     pHFSet->Put( aBoxInfoItem );
     pSet->Put( *pHFSetItem, ATTR_PAGE_HEADERSET );
     pSet->Put( *pHFSetItem, ATTR_PAGE_FOOTERSET );
-    DELETEZ( pHFSetItem );
+    delete pHFSetItem;
 
     // Footer:
     // [\TABLE\ (\DATA\)][empty][\DATE\, \TIME\]
@@ -401,7 +401,7 @@ void ScStyleSheetPool::CreateStandardStyles()
     pTxtObj = pEdEngine->CreateTextObject();
     pHeaderItem->SetLeftArea( *pTxtObj );
     pHeaderItem->SetCenterArea( *pEmptyTxtObj );
-    DELETEZ( pTxtObj );
+    delete pTxtObj;
     aStr = ", ";
     pEdEngine->SetText( aStr );
     pEdEngine->QuickInsertField( SvxFieldItem(SvxTimeField(), EE_FEATURE_FIELD), ESelection(0,2,0,2) );
@@ -409,7 +409,7 @@ void ScStyleSheetPool::CreateStandardStyles()
                                     ESelection() );
     pTxtObj = pEdEngine->CreateTextObject();
     pHeaderItem->SetRightArea( *pTxtObj );
-    DELETEZ( pTxtObj );
+    delete pTxtObj;
     pSet->Put( *pHeaderItem );
 
     // Footer:
@@ -427,12 +427,12 @@ void ScStyleSheetPool::CreateStandardStyles()
     pFooterItem->SetCenterArea( *pTxtObj );
     pFooterItem->SetRightArea ( *pEmptyTxtObj );
     pSet->Put( *pFooterItem );
-    DELETEZ( pTxtObj );
+    delete pTxtObj;
 
-    DELETEZ( pEmptyTxtObj );
-    DELETEZ( pHeaderItem );
-    DELETEZ( pFooterItem );
-    DELETEZ( pEdEngine );
+    delete pEmptyTxtObj;
+    delete pHeaderItem;
+    delete pFooterItem;
+    delete pEdEngine;
 }
 
 namespace {
