183307cfcd21abb021816b0eb0c9fa55c43af9d5
Mohamed Thabet <thabetx@gmail.com>
2016-03-13 21:01:41 +0200

    tdf#44282 fix missing space for numbered lists in TOC
    check if there is a number, if so add a space after it
    
    Change-Id: I1c9024aeda2048e60526f26b57a466fea90e56ec

--- sw/source/core/tox/ToxTextGenerator.cxx
+++ sw/source/core/tox/ToxTextGenerator.cxx
@@ -182,8 +182,14 @@ ToxTextGenerator::GenerateText(SwDoc* pDoc, const std::vector<SwTOXSortTabBase*>
             switch( aToken.eTokenType )
             {
             case TOKEN_ENTRY_NO:
-                // for TOC numbering
-                rText += GetNumStringOfFirstNode( rBase, aToken.nChapterFormat == CF_NUMBER, static_cast<sal_uInt8>(aToken.nOutlineLevel - 1) ) ;
+                {
+                    // for TOC numbering
+                    OUString numString = GetNumStringOfFirstNode( rBase, aToken.nChapterFormat == CF_NUMBER, static_cast<sal_uInt8>(aToken.nOutlineLevel - 1) ) ;
+                    if (numString.getLength() > 0)
+                    {
+                        rText += numString + " " ;
+                    }
+                }
                 break;
 
             case TOKEN_ENTRY_TEXT: {
