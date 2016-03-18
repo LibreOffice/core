8d515ccd7c25120ee1f8d6d5494c8853f1268162
Jan Holesovsky <kendy@collabora.com>
2016-03-18 15:52:55 +0100

    sc lok bccu#1519: More general solution to detecting cell resizes.
    This covers even the row height updates when the text in the cell changes etc.
    
    Change-Id: Idb570fa2c3c4d3c6bcdb381f8ce07a0311354083

--- sc/source/ui/docshell/docsh3.cxx
+++ sc/source/ui/docshell/docsh3.cxx
@@ -63,6 +63,9 @@
 #include "markdata.hxx"
 #include <memory>
 
+#include <comphelper/lok.hxx>
+#include <LibreOfficeKit/LibreOfficeKitEnums.h>
+
 //          Redraw - Benachrichtigungen
 
 void ScDocShell::PostEditView( ScEditEngineDefaulter* pEditEngine, const ScAddress& rCursorPos )
@@ -159,6 +162,11 @@ void ScDocShell::PostPaint( const ScRangeList& rRanges, sal_uInt16 nPart, sal_uI
     }
 
     Broadcast(ScPaintHint(aPaintRanges.Combine(), nPart));
+
+    // LOK: we are supposed to update the row / columns headers (and actually
+    // the document size too - cell size affects that, obviously)
+    if ((nPart & (PAINT_TOP | PAINT_LEFT)) && comphelper::LibreOfficeKit::isActive() && aDocument.GetDrawLayer())
+        aDocument.GetDrawLayer()->libreOfficeKitCallback(LOK_CALLBACK_DOCUMENT_SIZE_CHANGED, "");
 }
 
 void ScDocShell::PostPaintGridAll()
