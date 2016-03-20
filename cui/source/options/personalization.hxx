41cbc3ba98bb7263f0139b627de7ac562b3c6d15
Susobhan Ghosh <susobhang70@gmail.com>
2016-03-21 03:25:27 +0530

    tdf#88502 Firefox Theme URL in custom theme search
    Persona Dialog added. Search supports Firefox Theme URL.
    Added display of installed theme on personalization dialog.
    Changed placeholder text in search area.
    
    Change-Id: I603c0c292fe5c3e380553b9a2eaab6f4bc0683e1

--- cui/source/options/personalization.hxx
+++ cui/source/options/personalization.hxx
@@ -34,6 +34,7 @@ private:
     VclPtr<ListBox> m_pPersonaList;                    ///< The ListBox to show the list of installed personas
     OUString m_aPersonaSettings;                ///< Header and footer images + color to be set in the settings.
     VclPtr<FixedText> m_pExtensionLabel;               ///< The "select persona installed via extensions" label
+    VclPtr<FixedText> m_pAppliedThemeLabel;        ///< The label for showing applied custom theme
 
     std::vector<OUString> m_vDefaultPersonaSettings;
     std::vector<OUString> m_vExtensionPersonaSettings;
@@ -52,6 +53,8 @@ public:
     virtual void Reset( const SfxItemSet *rSet ) override;
 
     void SetPersonaSettings( const OUString& );
+    void CheckAppliedTheme();
+    void ShowAppliedThemeLabel( OUString& );
 
     void LoadDefaultImages();
     void LoadExtensionThemes();
@@ -118,16 +121,16 @@ private:
 
     VclPtr<SelectPersonaDialog> m_pPersonaDialog;
     OUString m_aURL;
-    bool m_bExecute;
+    bool m_bExecute, m_bDirectURL;
 
     virtual ~SearchAndParseThread();
     virtual void execute() override;
-    void getPreviewFile( const OUString&, OUString *, OUString * );
+    bool getPreviewFile( const OUString&, OUString *, OUString * );
 
 public:
 
     SearchAndParseThread( SelectPersonaDialog* pDialog,
-                          const OUString& rURL );
+                          const OUString& rURL, bool rDirectURL );
 
     void StopExecution() { m_bExecute = false; }
 };
