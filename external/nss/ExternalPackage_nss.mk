2a9e6ecc61b232be08d8b1687874289ca0fe8d43
Michael Stahl <mstahl@redhat.com>
2016-03-22 17:08:35 +0100

    nss: remove nss-pem.patch
    It looks like commit 30b9c6a457de61f3c94431ecc75716f866c3379e made
    this obsolete: by default, curl's configure will pick some OpenSSL
    certificate database in /etc to use, which is in PEM format.
    
    But since every distribution uses a different path in /etc for that,
    it will work only on the same distribution as the build host anyway,
    which makes it mostly useless in practice.
    
    So we don't need our NSS to be able to read PEM format now.
    
    Speaking of which, PEM support was only required for Linux builds
    anyway, not for WNT/Mac builds.
    
    Change-Id: Ib9bc6ec0b7d71c14e9a53454e6efed038072e913
    Reviewed-on: https://gerrit.libreoffice.org/23435
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Giuseppe Castagno <giuseppe.castagno@acca-esse.eu>
    Reviewed-by: Michael Stahl <mstahl@redhat.com>

--- external/nss/ExternalPackage_nss.mk
+++ external/nss/ExternalPackage_nss.mk
@@ -59,22 +59,4 @@ $(eval $(call gb_ExternalPackage_add_files,nss,$(LIBO_LIB_FOLDER),\
 ))
 endif
 
-ifeq ($(SYSTEM_CURL),)
-ifeq ($(OS),IOS)
-# nothing
-else ifeq ($(OS),MACOSX)
-$(eval $(call gb_ExternalPackage_add_files,nss,$(LIBO_LIB_FOLDER),\
-		dist/out/lib/libnsspem.dylib \
-))
-else ifeq ($(OS),WNT)
-$(eval $(call gb_ExternalPackage_add_files,nss,$(LIBO_LIB_FOLDER),\
-		dist/out/lib/nsspem.dll \
-))
-else # OS!=WNT/MACOSX
-$(eval $(call gb_ExternalPackage_add_files,nss,$(LIBO_LIB_FOLDER),\
-		dist/out/lib/libnsspem.so \
-))
-endif
-endif
-
 # vim: set noet sw=4 ts=4:
