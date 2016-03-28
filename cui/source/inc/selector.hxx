7bce46623cde8b62c3986c1cb141196092605044
Jakub Trzebiatowski <ubap.dev@gmail.com>
2016-03-28 20:53:24 +0200

    tdf#45129 Customize Toolbars: Add category "All commands"
    	- adds string "All commands" to resources
    	- moves code responsible for flling functions list to a function
    	- defines SVX_CFGGROUP_ALLFUNCTIONS
            - adds all commands group
    
    Change-Id: I4f03c8bc0af44dba2ef89ddd64b3fac67c5f2695

--- cui/source/inc/selector.hxx
+++ cui/source/inc/selector.hxx
@@ -26,6 +26,7 @@
 #include <svtools/treelistbox.hxx>
 
 #include <com/sun/star/uno/XComponentContext.hpp>
+#include <com/sun/star/frame/DispatchInformation.hpp>
 #include <com/sun/star/frame/XFrame.hpp>
 #include <com/sun/star/container/XNameAccess.hpp>
 #include <com/sun/star/script/browse/XBrowseNode.hpp>
@@ -38,6 +39,7 @@ class SaveInData;
 #define SVX_CFGFUNCTION_SLOT  2
 #define SVX_CFGGROUP_SCRIPTCONTAINER  3
 #define SVX_CFGFUNCTION_SCRIPT 4
+#define SVX_CFGGROUP_ALLFUNCTIONS 5
 
 struct SvxGroupInfo_Impl
 {
@@ -149,6 +151,7 @@ private:
         SvTreeListEntry* _pParentEntry,
         bool _bCheapChildrenOnDemand
     );
+    void    fillFunctionList( const com::sun::star::uno::Sequence< com::sun::star::frame::DispatchInformation >& commands );
 
 protected:
     virtual void    RequestingChildren( SvTreeListEntry *pEntry) override;
