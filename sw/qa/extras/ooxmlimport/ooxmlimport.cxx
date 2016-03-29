226af96743f8ce5f00f34ee726a1218c2274e3a2
Miklos Vajna <vmiklos@collabora.co.uk>
2016-03-29 14:03:00 +0200

    tdf#98882 DOCX import: set default para properties on the Standard para style
    That's what the DOC import does, and that's the reason e.g. the strange
    unwanted crop of the as-char anchored picture doesn't happen there.
    
    This also needs the "reset all existing style properties back to
    default" logic to be adapted: the Standard style has to be reset before
    the default are set, and later it should be left alone, otherwise the
    defaults are lost.
    
    Conflicts:
    	sw/qa/extras/ooxmlimport/ooxmlimport.cxx
    
    (cherry picked from commit eae2331f83bd58bacccd898d60f6c5f54856c036)
    
    Change-Id: Ie422a0b64b80a826fa4f469145a26283fb32d734
    Reviewed-on: https://gerrit.libreoffice.org/23597
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Andras Timar <andras.timar@collabora.com>

--- sw/qa/extras/ooxmlimport/ooxmlimport.cxx
+++ sw/qa/extras/ooxmlimport/ooxmlimport.cxx
@@ -3079,6 +3079,14 @@ DECLARE_OOXMLIMPORT_TEST(testTdf97371, "tdf97371.docx")
     CPPUNIT_ASSERT(nDiff < 10);
 }
 
+DECLARE_OOXMLIMPORT_TEST(testTdf98882, "tdf98882.docx")
+{
+    sal_Int32 nFlyHeight = parseDump("//fly/infos/bounds", "height").toInt32();
+    sal_Int32 nContentHeight = parseDump("//notxt/infos/bounds", "height").toInt32();
+    // The content height was 600, not 360, so the frame and the content height did not match.
+    CPPUNIT_ASSERT_EQUAL(nFlyHeight, nContentHeight);
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
