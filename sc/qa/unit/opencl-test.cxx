6cad2caf44e29bba9c1289b55770891187a3d486
Tor Lillqvist <tml@collabora.com>
2016-03-29 17:47:46 +0300

    Bin pointless comment
    The history of the file can be browsed using the version control system.
    
    Change-Id: Ibf087fb476a91bd36d5cafe62def4120ddece1a1

--- sc/qa/unit/opencl-test.cxx
+++ sc/qa/unit/opencl-test.cxx
@@ -1,7 +1,4 @@
 /* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
-/*
- * This file is added by mcw.
- */
 
 #include <sal/config.h>
 #include <test/bootstrapfixture.hxx>
