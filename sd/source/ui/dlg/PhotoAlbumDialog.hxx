71a23387d3e9c390ff92dedf7641cda73b2bb5dd
Beza Moges <mogesb@berea.edu>
2016-03-30 14:21:49 -0400

    PhotoAlbumDialog cxx and ui edits for impress

--- sd/source/ui/dlg/PhotoAlbumDialog.hxx
+++ sd/source/ui/dlg/PhotoAlbumDialog.hxx
@@ -66,6 +66,7 @@ private:
     VclPtr<ListBox>    pInsTypeCombo;
     VclPtr<CheckBox>   pASRCheck;
     VclPtr<CheckBox>   pCapCheck;
+    VclPtr<CheckBox> pFILLCheck;
 
     SdDrawDocument* pDoc;
     GraphicFilter* mpGraphicFilter;
@@ -84,6 +85,7 @@ private:
         Reference< drawing::XDrawPages > xDrawPages);
 
     static awt::Size createASRSize(const awt::Size& aPicSize, const awt::Size& aMaxSize);
+    static awt::Size createFILLSize(const awt::Size& aPicSize, const awt::Size& aMaxSize);
     void createCaption(const awt::Size& aPageSize);
     static Reference< graphic::XGraphic> createXGraphicFromUrl(const OUString& sUrl,
         Reference< graphic::XGraphicProvider> xProvider);
