b3fd8ccfc92fe07f67619783e7e37ce702af1621
Pranav Kant <pranavk@collabora.com>
2016-03-31 14:47:27 +0530

    lok: Expose context menu
    Change-Id: I0968689630e10f838c075e86357eb36a9a220d0d

--- sc/source/ui/inc/gridwin.hxx
+++ sc/source/ui/inc/gridwin.hxx
@@ -295,7 +295,7 @@ protected:
     virtual void    LoseFocus() override;
 
     virtual void    RequestHelp( const HelpEvent& rEvt ) override;
-    virtual void    Command( const CommandEvent& rCEvt ) override;
+
 
     virtual sal_Int8 AcceptDrop( const AcceptDropEvent& rEvt ) override;
     virtual sal_Int8 ExecuteDrop( const ExecuteDropEvent& rEvt ) override;
@@ -317,6 +317,7 @@ public:
 
     virtual void    MouseButtonDown( const MouseEvent& rMEvt ) override;
     virtual void    MouseButtonUp( const MouseEvent& rMEvt ) override;
+    virtual void    Command( const CommandEvent& rCEvt ) override;
     virtual void    MouseMove( const MouseEvent& rMEvt ) override;
     virtual bool    PreNotify( NotifyEvent& rNEvt ) override;
     virtual void    Tracking( const TrackingEvent& rTEvt ) override;
