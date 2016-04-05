4b4c9184279c2cb5998348b09e084b295de1c8aa
Stephan Bergmann <sbergman@redhat.com>
2016-04-05 13:13:08 +0200

    loplugin:salbool
    Change-Id: I6c1910aeb2866d9646f1ea8e53dbd7a8a351a49d

--- svx/source/form/formcontroller.cxx
+++ svx/source/form/formcontroller.cxx
@@ -1655,7 +1655,7 @@ void FormController::focusGained(const FocusEvent& e) throw( RuntimeException, s
             // check the old control if the content is ok
 #if OSL_DEBUG_LEVEL > 0
             Reference< XBoundControl >  xLockingTest(m_xCurrentControl, UNO_QUERY);
-            sal_Bool bControlIsLocked = xLockingTest.is() && xLockingTest->getLock();
+            bool bControlIsLocked = xLockingTest.is() && xLockingTest->getLock();
             assert(!bControlIsLocked && "FormController::Gained: I'm modified and the current control is locked ? How this ?");
             // normalerweise sollte ein gelocktes Control nicht modified sein, also muss wohl mein bModified aus einem anderen Kontext
             // gesetzt worden sein, was ich nicht verstehen wuerde ...
