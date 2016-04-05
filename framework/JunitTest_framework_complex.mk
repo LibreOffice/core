3e253963b93330dcd9e38c39de6601dbd43f13e4
Akash Jain <akash96j@gmail.com>
2016-04-05 11:58:01 +0530

    Add unit tests for XTitle interface
    Added several unit tests for the XTitle interface.
    Tests are for bugs tdf#96044,tdf#98837,tdf#96896
    
    Change-Id: I9b3990dff02d5d5738024b60e94555199b625a2e
    Reviewed-on: https://gerrit.libreoffice.org/23834
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

--- framework/JunitTest_framework_complex.mk
+++ framework/JunitTest_framework_complex.mk
@@ -62,13 +62,15 @@ $(eval $(call gb_JunitTest_add_sourcefiles,framework_complex,\
     framework/qa/complex/imageManager/_XUIConfigurationPersistence \
     framework/qa/complex/imageManager/_XUIConfiguration \
     framework/qa/complex/api_internal/CheckAPI \
-	framework/qa/complex/dispatches/checkdispatchapi \
+    framework/qa/complex/dispatches/checkdispatchapi \
     framework/qa/complex/dispatches/Interceptor \
     framework/qa/complex/ModuleManager/CheckXModuleManager \
+    framework/qa/complex/XTitle/CheckXTitle \
 ))
 
 $(eval $(call gb_JunitTest_add_classes,framework_complex,\
     complex.dispatches.checkdispatchapi \
+    complex.XTitle.CheckXTitle \
 ))
 # these were disabled in the old build system too, please check
 # carefully before reenabling
