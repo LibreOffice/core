LibreOffice/core

commit 3979afceecb98780549fda60c7f75c8015bb33d8

osl_getSystemTime: use high precision timer of W32, if available
Change-Id: I7029f37639ead7a8b597ff39d1abcd295fab0551

sal/osl/w32/time.c
@@ -35,10 +35,28 @@ sal_Bool SAL_CALL osl_getSystemTime(TimeValue* pTimeVal)
     FILETIME   CurTime, OffTime;
     __int64    Value;
 
+    typedef BOOL (WINAPI *CheckTokenMembership_PROC)( HANDLE, PSID, PBOOL );
+
+    static HMODULE  hModule = NULL;
+    static CheckTokenMembership_PROC    pCheckTokenMembership = NULL;
+
     OSL_ASSERT(pTimeVal != 0);
 
-    GetSystemTime(&SystemTime);
-    SystemTimeToFileTime(&SystemTime, &CurTime);
+    if ( !hModule )
+    {
+        hModule = GetModuleHandleA( "Kernel32.dll" );
+        if ( hModule )
+            pCheckTokenMembership = (CheckTokenMembership_PROC)GetProcAddress( hModule, "GetSystemTimePreciseAsFileTime" );
+    }
+
+    // use ~1 microsecond resolution if available
+    if ( pCheckTokenMembership )
+        GetSystemTimePreciseAsFileTime(&CurTime);
+    else
+    {
+        GetSystemTime(&SystemTime);
+        SystemTimeToFileTime(&SystemTime, &CurTime);
+    }
 
     SystemTime.wYear         = 1970;
     SystemTime.wMonth        = 1;

commit da181c3b1ee92fac3c0c8692a7b7ed1e43041423

sal: GetSystemTimePreciseAsFileTime requires Windows 8
3979afceecb98780549fda60c7f75c8015bb33d8 broke the build on Windows 7.

Change-Id: I142208c062d56d6a3daa8213a877411edde45e55

sal/osl/w32/time.c
@@ -35,10 +35,10 @@ sal_Bool SAL_CALL osl_getSystemTime(TimeValue* pTimeVal)
     FILETIME   CurTime, OffTime;
     __int64    Value;
 
-    typedef BOOL (WINAPI *CheckTokenMembership_PROC)( HANDLE, PSID, PBOOL );
+    typedef VOID (WINAPI *GetSystemTimePreciseAsFileTime_PROC)(LPFILETIME);
 
-    static HMODULE  hModule = NULL;
-    static CheckTokenMembership_PROC    pCheckTokenMembership = NULL;
+    static HMODULE hModule = nullptr;
+    static GetSystemTimePreciseAsFileTime_PROC pGetSystemTimePreciseAsFileTime = nullptr;
 
     OSL_ASSERT(pTimeVal != 0);
 
@@ -46,11 +46,12 @@ sal_Bool SAL_CALL osl_getSystemTime(TimeValue* pTimeVal)
     {
         hModule = GetModuleHandleA( "Kernel32.dll" );
         if ( hModule )
-            pCheckTokenMembership = (CheckTokenMembership_PROC)GetProcAddress( hModule, "GetSystemTimePreciseAsFileTime" );
+            pGetSystemTimePreciseAsFileTime = (GetSystemTimePreciseAsFileTime_PROC)
+                GetProcAddress(hModule, "GetSystemTimePreciseAsFileTime");
     }
 
     // use ~1 microsecond resolution if available
-    if ( pCheckTokenMembership )
+    if (pGetSystemTimePreciseAsFileTime)
         GetSystemTimePreciseAsFileTime(&CurTime);
     else
     {