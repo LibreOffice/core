3bdf162f691004cfc171c5f1ed86dac611e68691
Mark Hung <marklh9@gmail.com>
2016-04-09 23:45:28 +0800

    tdf#48300 bring window to top and restore window when minimized.
    While XWindow->ToTop() in Linux do the both for default ( None ),
    ToTopFlags::RestoreWhenMin has to be specified in Windows in order
    to restore the window, and ToTopFlags::ForegroundTask is necessary
    when ToTopFlags::RestoreWhenMin is specified in order to bring
    non-minimized window to the front.
    
    Change-Id: Ief4a825a18d874a0c35211c9022a4261da6bcf8f
    Reviewed-on: https://gerrit.libreoffice.org/23938
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>

--- framework/source/loadenv/loadenv.cxx
+++ framework/source/loadenv/loadenv.cxx
@@ -1645,7 +1645,7 @@ void LoadEnv::impl_makeFrameWindowVisible(const css::uno::Reference< css::awt::X
         }
 
         if( pWindow->IsVisible() && (bForceFrontAndFocus || bForceToFront) )
-            pWindow->ToTop();
+            pWindow->ToTop( ToTopFlags::RestoreWhenMin | ToTopFlags::ForegroundTask );
         else
             pWindow->Show(true, (bForceFrontAndFocus || bForceToFront) ? ShowFlags::ForegroundTask : ShowFlags::NONE );
     }
