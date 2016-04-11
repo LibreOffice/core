8a36e1a3d9ce23bca2f52bef86b837796b944d7a
Stephan Bergmann <sbergman@redhat.com>
2016-04-11 15:07:28 +0200

    Avoid reserved identifiers
    Change-Id: I0a8dc9591c00188d0a31feb784d14ec53f389f27

--- sal/osl/unx/file_volume.cxx
+++ sal/osl/unx/file_volume.cxx
@@ -133,77 +133,77 @@ oslFileError osl_getVolumeInformation( rtl_uString* ustrDirectoryURL, oslVolumeI
 #ifdef HAVE_STATFS_H
 
 #if defined(FREEBSD) || defined(MACOSX) || defined(OPENBSD) || defined(DRAGONFLY)
-#   define __OSL_STATFS_STRUCT                  struct statfs
-#   define __OSL_STATFS(dir, sfs)               statfs((dir), (sfs))
-#   define __OSL_STATFS_BLKSIZ(a)               ((sal_uInt64)((a).f_bsize))
-#   define __OSL_STATFS_TYPENAME(a)             ((a).f_fstypename)
+#   define OSL_detail_STATFS_STRUCT                  struct statfs
+#   define OSL_detail_STATFS(dir, sfs)               statfs((dir), (sfs))
+#   define OSL_detail_STATFS_BLKSIZ(a)               ((sal_uInt64)((a).f_bsize))
+#   define OSL_detail_STATFS_TYPENAME(a)             ((a).f_fstypename)
 #if defined(OPENBSD)
-#   define __OSL_STATFS_ISREMOTE(a)             (rtl_str_compare((a).f_fstypename, "nfs") == 0)
+#   define OSL_detail_STATFS_ISREMOTE(a)             (rtl_str_compare((a).f_fstypename, "nfs") == 0)
 #else
-#   define __OSL_STATFS_ISREMOTE(a)             (((a).f_type & MNT_LOCAL) == 0)
+#   define OSL_detail_STATFS_ISREMOTE(a)             (((a).f_type & MNT_LOCAL) == 0)
 #endif
 
 /* always return true if queried for the properties of
    the file system. If you think this is wrong under any
    of the target platforms fix it!!!! */
-#   define __OSL_STATFS_IS_CASE_SENSITIVE_FS(a)  (true)
-#   define __OSL_STATFS_IS_CASE_PRESERVING_FS(a) (true)
+#   define OSL_detail_STATFS_IS_CASE_SENSITIVE_FS(a)  (true)
+#   define OSL_detail_STATFS_IS_CASE_PRESERVING_FS(a) (true)
 #endif /* FREEBSD || MACOSX || OPENBSD */
 
 #if defined(NETBSD)
 
 #include <sys/param.h>
 
-#   define __OSL_STATFS_STRUCT              struct statvfs
-#   define __OSL_STATFS(dir, sfs)           statvfs((dir), (sfs))
-#   define __OSL_STATFS_ISREMOTE(a)         (((a).f_flag & ST_LOCAL) == 0)
+#   define OSL_detail_STATFS_STRUCT              struct statvfs
+#   define OSL_detail_STATFS(dir, sfs)           statvfs((dir), (sfs))
+#   define OSL_detail_STATFS_ISREMOTE(a)         (((a).f_flag & ST_LOCAL) == 0)
 
-#   define __OSL_STATFS_BLKSIZ(a)           ((sal_uInt64)((a).f_bsize))
-#   define __OSL_STATFS_TYPENAME(a)         ((a).f_fstypename)
+#   define OSL_detail_STATFS_BLKSIZ(a)           ((sal_uInt64)((a).f_bsize))
+#   define OSL_detail_STATFS_TYPENAME(a)         ((a).f_fstypename)
 
-#   define __OSL_STATFS_IS_CASE_SENSITIVE_FS(a) (strcmp((a).f_fstypename, "msdos") != 0 && strcmp((a).f_fstypename, "ntfs") != 0 && strcmp((a).f_fstypename, "smbfs") != 0)
-#   define __OSL_STATFS_IS_CASE_PRESERVING_FS(a)    (strcmp((a).f_fstypename, "msdos") != 0)
+#   define OSL_detail_STATFS_IS_CASE_SENSITIVE_FS(a) (strcmp((a).f_fstypename, "msdos") != 0 && strcmp((a).f_fstypename, "ntfs") != 0 && strcmp((a).f_fstypename, "smbfs") != 0)
+#   define OSL_detail_STATFS_IS_CASE_PRESERVING_FS(a)    (strcmp((a).f_fstypename, "msdos") != 0)
 #endif /* NETBSD */
 
 #if defined(LINUX)
-#   define __OSL_NFS_SUPER_MAGIC                 0x6969
-#   define __OSL_SMB_SUPER_MAGIC                 0x517B
-#   define __OSL_MSDOS_SUPER_MAGIC               0x4d44
-#   define __OSL_NTFS_SUPER_MAGIC                0x5346544e
-#   define __OSL_STATFS_STRUCT                   struct statfs
-#   define __OSL_STATFS(dir, sfs)                statfs((dir), (sfs))
-#   define __OSL_STATFS_BLKSIZ(a)                ((sal_uInt64)((a).f_bsize))
-#   define __OSL_STATFS_IS_NFS(a)                (__OSL_NFS_SUPER_MAGIC == (a).f_type)
-#   define __OSL_STATFS_IS_SMB(a)                (__OSL_SMB_SUPER_MAGIC == (a).f_type)
-#   define __OSL_STATFS_ISREMOTE(a)              (__OSL_STATFS_IS_NFS((a)) || __OSL_STATFS_IS_SMB((a)))
-#   define __OSL_STATFS_IS_CASE_SENSITIVE_FS(a)  ((__OSL_MSDOS_SUPER_MAGIC != (a).f_type) && (__OSL_NTFS_SUPER_MAGIC != (a).f_type))
-#   define __OSL_STATFS_IS_CASE_PRESERVING_FS(a) ((__OSL_MSDOS_SUPER_MAGIC != (a).f_type))
+#   define OSL_detail_NFS_SUPER_MAGIC                 0x6969
+#   define OSL_detail_SMB_SUPER_MAGIC                 0x517B
+#   define OSL_detail_MSDOS_SUPER_MAGIC               0x4d44
+#   define OSL_detail_NTFS_SUPER_MAGIC                0x5346544e
+#   define OSL_detail_STATFS_STRUCT                   struct statfs
+#   define OSL_detail_STATFS(dir, sfs)                statfs((dir), (sfs))
+#   define OSL_detail_STATFS_BLKSIZ(a)                ((sal_uInt64)((a).f_bsize))
+#   define OSL_detail_STATFS_IS_NFS(a)                (OSL_detail_NFS_SUPER_MAGIC == (a).f_type)
+#   define OSL_detail_STATFS_IS_SMB(a)                (OSL_detail_SMB_SUPER_MAGIC == (a).f_type)
+#   define OSL_detail_STATFS_ISREMOTE(a)              (OSL_detail_STATFS_IS_NFS((a)) || OSL_detail_STATFS_IS_SMB((a)))
+#   define OSL_detail_STATFS_IS_CASE_SENSITIVE_FS(a)  ((OSL_detail_MSDOS_SUPER_MAGIC != (a).f_type) && (OSL_detail_NTFS_SUPER_MAGIC != (a).f_type))
+#   define OSL_detail_STATFS_IS_CASE_PRESERVING_FS(a) ((OSL_detail_MSDOS_SUPER_MAGIC != (a).f_type))
 #endif /* LINUX */
 
 #if defined(SOLARIS)
-#   define __OSL_STATFS_STRUCT                   struct statvfs
-#   define __OSL_STATFS(dir, sfs)                statvfs((dir), (sfs))
-#   define __OSL_STATFS_BLKSIZ(a)                ((sal_uInt64)((a).f_frsize))
-#   define __OSL_STATFS_TYPENAME(a)              ((a).f_basetype)
-#   define __OSL_STATFS_ISREMOTE(a)              (rtl_str_compare((a).f_basetype, "nfs") == 0)
+#   define OSL_detail_STATFS_STRUCT                   struct statvfs
+#   define OSL_detail_STATFS(dir, sfs)                statvfs((dir), (sfs))
+#   define OSL_detail_STATFS_BLKSIZ(a)                ((sal_uInt64)((a).f_frsize))
+#   define OSL_detail_STATFS_TYPENAME(a)              ((a).f_basetype)
+#   define OSL_detail_STATFS_ISREMOTE(a)              (rtl_str_compare((a).f_basetype, "nfs") == 0)
 
 /* always return true if queried for the properties of
    the file system. If you think this is wrong under any
    of the target platforms fix it!!!! */
-#   define __OSL_STATFS_IS_CASE_SENSITIVE_FS(a)  (true)
-#   define __OSL_STATFS_IS_CASE_PRESERVING_FS(a) (true)
+#   define OSL_detail_STATFS_IS_CASE_SENSITIVE_FS(a)  (true)
+#   define OSL_detail_STATFS_IS_CASE_PRESERVING_FS(a) (true)
 #endif /* SOLARIS */
 
-#   define __OSL_STATFS_INIT(a)         (memset(&(a), 0, sizeof(__OSL_STATFS_STRUCT)))
+#   define OSL_detail_STATFS_INIT(a)         (memset(&(a), 0, sizeof(OSL_detail_STATFS_STRUCT)))
 
 #else /* no statfs available */
 
-#   define __OSL_STATFS_STRUCT                   struct dummy {int i;}
-#   define __OSL_STATFS_INIT(a)                  ((void)a)
-#   define __OSL_STATFS(dir, sfs)                (1)
-#   define __OSL_STATFS_ISREMOTE(sfs)            (false)
-#   define __OSL_STATFS_IS_CASE_SENSITIVE_FS(a)  (true)
-#   define __OSL_STATFS_IS_CASE_PRESERVING_FS(a) (true)
+#   define OSL_detail_STATFS_STRUCT                   struct dummy {int i;}
+#   define OSL_detail_STATFS_INIT(a)                  ((void)a)
+#   define OSL_detail_STATFS(dir, sfs)                (1)
+#   define OSL_detail_STATFS_ISREMOTE(sfs)            (false)
+#   define OSL_detail_STATFS_IS_CASE_SENSITIVE_FS(a)  (true)
+#   define OSL_detail_STATFS_IS_CASE_PRESERVING_FS(a) (true)
 #endif /* HAVE_STATFS_H */
 
 static oslFileError osl_psz_getVolumeInformation (
@@ -225,9 +225,9 @@ static oslFileError osl_psz_getVolumeInformation (
             | osl_VolumeInfo_Mask_FileSystemCaseHandling))
         != 0)
     {
-        __OSL_STATFS_STRUCT sfs;
-        __OSL_STATFS_INIT(sfs);
-        if ((__OSL_STATFS(pszDirectory, &sfs)) < 0)
+        OSL_detail_STATFS_STRUCT sfs;
+        OSL_detail_STATFS_INIT(sfs);
+        if ((OSL_detail_STATFS(pszDirectory, &sfs)) < 0)
         {
             oslFileError result = oslTranslateFileError(OSL_FET_ERROR, errno);
             return result;
@@ -236,7 +236,7 @@ static oslFileError osl_psz_getVolumeInformation (
         /* FIXME: how to detect the kind of storage (fixed, cdrom, ...) */
         if (uFieldMask & osl_VolumeInfo_Mask_Attributes)
         {
-            if (__OSL_STATFS_ISREMOTE(sfs))
+            if (OSL_detail_STATFS_ISREMOTE(sfs))
                 pInfo->uAttributes  |= osl_Volume_Attribute_Remote;
 
             pInfo->uValidFields |= osl_VolumeInfo_Mask_Attributes;
@@ -244,21 +244,21 @@ static oslFileError osl_psz_getVolumeInformation (
 
         if (uFieldMask & osl_VolumeInfo_Mask_FileSystemCaseHandling)
         {
-            if (__OSL_STATFS_IS_CASE_SENSITIVE_FS(sfs))
+            if (OSL_detail_STATFS_IS_CASE_SENSITIVE_FS(sfs))
                 pInfo->uAttributes |= osl_Volume_Attribute_Case_Sensitive;
 
-            if (__OSL_STATFS_IS_CASE_PRESERVING_FS(sfs))
+            if (OSL_detail_STATFS_IS_CASE_PRESERVING_FS(sfs))
                 pInfo->uAttributes |= osl_Volume_Attribute_Case_Is_Preserved;
 
             pInfo->uValidFields |= osl_VolumeInfo_Mask_Attributes;
         }
 
-#if defined(__OSL_STATFS_BLKSIZ)
+#if defined(OSL_detail_STATFS_BLKSIZ)
 
         if ((uFieldMask & osl_VolumeInfo_Mask_TotalSpace) ||
             (uFieldMask & osl_VolumeInfo_Mask_UsedSpace))
         {
-            pInfo->uTotalSpace   = __OSL_STATFS_BLKSIZ(sfs);
+            pInfo->uTotalSpace   = OSL_detail_STATFS_BLKSIZ(sfs);
             pInfo->uTotalSpace  *= (sal_uInt64)(sfs.f_blocks);
             pInfo->uValidFields |= osl_VolumeInfo_Mask_TotalSpace;
         }
@@ -266,7 +266,7 @@ static oslFileError osl_psz_getVolumeInformation (
         if ((uFieldMask & osl_VolumeInfo_Mask_FreeSpace) ||
             (uFieldMask & osl_VolumeInfo_Mask_UsedSpace))
         {
-            pInfo->uFreeSpace = __OSL_STATFS_BLKSIZ(sfs);
+            pInfo->uFreeSpace = OSL_detail_STATFS_BLKSIZ(sfs);
 
             if (getuid() == 0)
                 pInfo->uFreeSpace *= (sal_uInt64)(sfs.f_bfree);
@@ -283,16 +283,16 @@ static oslFileError osl_psz_getVolumeInformation (
             pInfo->uValidFields |= osl_VolumeInfo_Mask_UsedSpace;
         }
 
-#endif  /* __OSL_STATFS_BLKSIZ */
+#endif  /* OSL_detail_STATFS_BLKSIZ */
 
-#if defined(__OSL_STATFS_TYPENAME)
+#if defined(OSL_detail_STATFS_TYPENAME)
 
         if (uFieldMask & osl_VolumeInfo_Mask_FileSystemName)
         {
             rtl_string2UString(
                 &(pInfo->ustrFileSystemName),
-                __OSL_STATFS_TYPENAME(sfs),
-                rtl_str_getLength(__OSL_STATFS_TYPENAME(sfs)),
+                OSL_detail_STATFS_TYPENAME(sfs),
+                rtl_str_getLength(OSL_detail_STATFS_TYPENAME(sfs)),
                 osl_getThreadTextEncoding(),
                 OUSTRING_TO_OSTRING_CVTFLAGS);
             OSL_ASSERT(pInfo->ustrFileSystemName != nullptr);
@@ -300,7 +300,7 @@ static oslFileError osl_psz_getVolumeInformation (
             pInfo->uValidFields |= osl_VolumeInfo_Mask_FileSystemName;
         }
 
-#endif /* __OSL_STATFS_TYPENAME */
+#endif /* OSL_detail_STATFS_TYPENAME */
     }
 
     pInfo->uMaxNameLength = 0;
