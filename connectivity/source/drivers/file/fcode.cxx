LibreOffice/core

commit ac9671f94800b647f82b12e718968311a025e87e

tdf#94559: second step to remove rtti.hxx
    replaced use of PTR_CAST, IS_TYPE, ISA in
    chart2, connectivity, editeng, extensions, filter, forms, framework, idl

Change-Id: I6a2f9d8bbccb07088413f7552af4b5af8f0cad99
Reviewed-on: https://gerrit.libreoffice.org/18920
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Oliver Specht <oliver.specht@cib.de>

connectivity/source/drivers/file/fcode.cxx
@@ -189,9 +189,9 @@ void OBoolOperator::Exec(OCodeStack& rCodeStack)
     rCodeStack.pop();
 
     rCodeStack.push(new OOperandResultBOOL(operate(pLeft, pRight)));
-    if (IS_TYPE(OOperandResult,pLeft))
+    if( typeid(OOperandResult) == typeid(*pLeft))
         delete pLeft;
-    if (IS_TYPE(OOperandResult,pRight))
+    if( typeid(OOperandResult) == typeid(*pRight))
         delete pRight;
 }
 
@@ -206,7 +206,8 @@ void OOp_NOT::Exec(OCodeStack& rCodeStack)
     rCodeStack.pop();
 
     rCodeStack.push(new OOperandResultBOOL(operate(pOperand)));
-    if (IS_TYPE(OOperandResult,pOperand))
+
+    if( typeid(OOperandResult) == typeid(*pOperand))
         delete pOperand;
 }
 
@@ -228,7 +229,7 @@ void OOp_ISNULL::Exec(OCodeStack& rCodeStack)
     rCodeStack.pop();
 
     rCodeStack.push(new OOperandResultBOOL(operate(pOperand)));
-    if (IS_TYPE(OOperandResult,pOperand))
+    if( typeid(OOperandResult) == typeid(*pOperand))
         delete pOperand;
 }
 
@@ -340,9 +341,9 @@ void ONumOperator::Exec(OCodeStack& rCodeStack)
     rCodeStack.pop();
 
     rCodeStack.push(new OOperandResultNUM(operate(pLeft->getValue(), pRight->getValue())));
-    if (IS_TYPE(OOperandResult,pLeft))
+    if( typeid(OOperandResult) == typeid(*pLeft))
         delete pLeft;
-    if (IS_TYPE(OOperandResult,pRight))
+    if( typeid(OOperandResult) == typeid(*pRight))
         delete pRight;
 }
 
@@ -380,11 +381,11 @@ void ONthOperator::Exec(OCodeStack& rCodeStack)
         pOperand    = rCodeStack.top();
         rCodeStack.pop();
         assert(pOperand);
-        if (pOperand && !IS_TYPE(OStopOperand,pOperand))
+        if (pOperand && typeid(OStopOperand) != typeid(*pOperand))
             aValues.push_back( pOperand->getValue() );
         aOperands.push_back( pOperand );
     }
-    while (pOperand && !IS_TYPE(OStopOperand,pOperand));
+    while (pOperand && typeid(OStopOperand) != typeid(*pOperand));
 
     rCodeStack.push(new OOperandResult(operate(aValues)));
 
@@ -392,7 +393,7 @@ void ONthOperator::Exec(OCodeStack& rCodeStack)
     ::std::vector<OOperand*>::iterator aEnd = aOperands.end();
     for (; aIter != aEnd; ++aIter)
     {
-        if (IS_TYPE(OOperandResult,*aIter))
+        if (typeid(OOperandResult) != typeid(*(*aIter)))
             delete *aIter;
     }
 }
@@ -404,13 +405,13 @@ void OBinaryOperator::Exec(OCodeStack& rCodeStack)
     OOperand  *pLeft    = rCodeStack.top();
     rCodeStack.pop();
 
-    if ( !rCodeStack.empty() && IS_TYPE(OStopOperand,rCodeStack.top()) )
+    if ( !rCodeStack.empty() && typeid(OStopOperand) == typeid(*rCodeStack.top()) )
         rCodeStack.pop();
 
     rCodeStack.push(new OOperandResult(operate(pLeft->getValue(),pRight->getValue())));
-    if (IS_TYPE(OOperandResult,pRight))
+    if(typeid(OOperandResult) == typeid(*pRight))
         delete pRight;
-    if (IS_TYPE(OOperandResult,pLeft))
+    if(typeid(OOperandResult) == typeid(*pLeft))
         delete pLeft;
 }
 
@@ -421,7 +422,7 @@ void OUnaryOperator::Exec(OCodeStack& rCodeStack)
     rCodeStack.pop();
 
     rCodeStack.push(new OOperandResult(operate(pOperand->getValue())));
-    if (IS_TYPE(OOperandResult,pOperand))
+    if (typeid(OOperandResult) == typeid(*pOperand))
         delete pOperand;
 }
 

connectivity/source/drivers/file/fcomp.cxx
@@ -554,7 +554,7 @@ bool OPredicateInterpreter::evaluate(OCodeList& rCodeList)
 
     for(;aIter != rCodeList.end();++aIter)
     {
-        OOperand* pOperand = PTR_CAST(OOperand,(*aIter));
+        OOperand* pOperand = dynamic_cast<OOperand* >(*aIter);
         if (pOperand)
             m_aStack.push(pOperand);
         else
@@ -568,7 +568,7 @@ bool OPredicateInterpreter::evaluate(OCodeList& rCodeList)
     DBG_ASSERT(pOperand, "StackFehler");
 
     bResult = pOperand->isValid();
-    if (IS_TYPE(OOperandResult,pOperand))
+    if (dynamic_cast<const OOperandResult* >(pOperand) !=  nullptr)
         delete pOperand;
     return bResult;
 }
@@ -581,7 +581,7 @@ void OPredicateInterpreter::evaluateSelection(OCodeList& rCodeList,ORowSetValueD
 
     for(;aIter != rCodeList.end();++aIter)
     {
-        OOperand* pOperand = PTR_CAST(OOperand,(*aIter));
+        OOperand* pOperand = dynamic_cast<OOperand* >(*aIter);
         if (pOperand)
             m_aStack.push(pOperand);
         else
@@ -595,7 +595,7 @@ void OPredicateInterpreter::evaluateSelection(OCodeList& rCodeList,ORowSetValueD
     DBG_ASSERT(pOperand, "StackFehler");
 
     (*_rVal) = pOperand->getValue();
-    if (IS_TYPE(OOperandResult,pOperand))
+    if (dynamic_cast<const OOperandResult* >(pOperand) !=  nullptr)
         delete pOperand;
 }
 

chart2/source/controller/main/ChartController_TextEdit.cxx
@@ -177,7 +177,7 @@ void ChartController::executeDispatch_InsertSpecialCharacter()
         const SfxPoolItem* pItem=0;
         OUString aString;
         if ( pSet && pSet->GetItemState( SID_CHARMAP, true, &pItem) == SfxItemState::SET &&
-             pItem->ISA(SfxStringItem) )
+             dynamic_cast< const SfxStringItem* >(pItem) !=  nullptr )
                 aString = dynamic_cast<const SfxStringItem*>(pItem)->GetValue();
 
         OutlinerView* pOutlinerView = m_pDrawViewWrapper->GetTextEditOutlinerView();

chart2/source/controller/main/ChartController_Window.cxx
@@ -907,7 +907,7 @@ void ChartController::execute_DoubleClick( const Point* pMousePixel )
         {
             // #i12587# support for shapes in chart
             SdrObject* pObj = DrawViewWrapper::getSdrObject( m_aSelection.getSelectedAdditionalShape() );
-            if ( pObj && pObj->ISA( SdrTextObj ) )
+            if ( dynamic_cast< const SdrTextObj* >(pObj) !=  nullptr )
             {
                 bEditText = true;
             }

chart2/source/controller/main/DrawCommandDispatch.cxx
@@ -466,7 +466,7 @@ SdrObject* DrawCommandDispatch::createDefaultObject( const sal_uInt16 nID )
                     case COMMAND_ID_DRAW_LINE:
                     case COMMAND_ID_LINE_ARROW_END:
                         {
-                            if ( pObj->ISA( SdrPathObj ) )
+                            if ( dynamic_cast<const SdrPathObj*>( pObj) !=  nullptr )
                             {
                                 Point aStart = aRect.TopLeft();
                                 Point aEnd = aRect.BottomRight();
@@ -483,7 +483,7 @@ SdrObject* DrawCommandDispatch::createDefaultObject( const sal_uInt16 nID )
                         break;
                     case COMMAND_ID_DRAW_FREELINE_NOFILL:
                         {
-                            if ( pObj->ISA( SdrPathObj ) )
+                            if ( dynamic_cast<const SdrPathObj*>( pObj) !=  nullptr )
                             {
                                 basegfx::B2DPolygon aInnerPoly;
                                 aInnerPoly.append( basegfx::B2DPoint( aRect.Left(), aRect.Bottom() ) );
@@ -504,7 +504,7 @@ SdrObject* DrawCommandDispatch::createDefaultObject( const sal_uInt16 nID )
                     case COMMAND_ID_DRAW_TEXT:
                     case COMMAND_ID_DRAW_TEXT_VERTICAL:
                         {
-                            if ( pObj->ISA( SdrTextObj ) )
+                            if ( dynamic_cast<const SdrTextObj*>( pObj) !=  nullptr )
                             {
                                 SdrTextObj* pTextObj = dynamic_cast< SdrTextObj* >( pObj );
                                 if ( pTextObj )
@@ -528,7 +528,7 @@ SdrObject* DrawCommandDispatch::createDefaultObject( const sal_uInt16 nID )
                     case COMMAND_ID_DRAW_CAPTION:
                     case COMMAND_ID_DRAW_CAPTION_VERTICAL:
                         {
-                            if ( pObj->ISA( SdrCaptionObj ) )
+                            if ( dynamic_cast<const SdrCaptionObj*>( pObj) !=  nullptr )
                             {
                                 bool bIsVertical( COMMAND_ID_DRAW_CAPTION_VERTICAL == nID );
                                 SdrTextObj* pTextObj = dynamic_cast< SdrTextObj* >( pObj );

chart2/source/controller/main/SelectionHelper.cxx
@@ -470,7 +470,7 @@ SelectionHelper::~SelectionHelper()
 bool SelectionHelper::getFrameDragSingles()
 {
     bool bFrameDragSingles = true;//true == green == surrounding handles
-    if( m_pSelectedObj && m_pSelectedObj->ISA(E3dObject) )
+    if( m_pSelectedObj && dynamic_cast<const E3dObject*>( m_pSelectedObj) !=  nullptr )
         bFrameDragSingles = false;
     return bFrameDragSingles;
 }
@@ -592,7 +592,7 @@ bool SelectionHelper::getMarkHandles( SdrHdlList& rHdlList )
     if( m_pMarkObj && m_pMarkObj != m_pSelectedObj)
     {
         rHdlList.Clear();
-        if( m_pMarkObj->ISA(SdrPathObj) )
+        if( dynamic_cast<const SdrPathObj*>( m_pMarkObj) !=  nullptr )
         {
             //if th object is a polygon
             //from each point a handle is generated

chart2/source/view/diagram/VDiagram.cxx
@@ -220,7 +220,7 @@ E3dScene* lcl_getE3dScene( const uno::Reference< drawing::XShape >& xShape )
         if(pSvxShape)
         {
             SdrObject* pObj = pSvxShape->GetSdrObject();
-            if( pObj && pObj->ISA(E3dScene) )
+            if( pObj && dynamic_cast< const E3dScene* >(pObj) !=  nullptr )
                 pRet = static_cast<E3dScene*>(pObj);
         }
     }

connectivity/source/drivers/dbase/DIndexIter.cxx
@@ -132,7 +132,7 @@ ONDXKey* OIndexIterator::GetFirstKey(ONDXPage* pPage, const OOperand& rKey)
 sal_uIntPtr OIndexIterator::GetCompare(bool bFirst)
 {
     ONDXKey* pKey = NULL;
-    sal_Int32 ePredicateType = PTR_CAST(file::OOp_COMPARE,m_pOperator)->getPredicateType();
+    sal_Int32 ePredicateType = dynamic_cast<file::OOp_COMPARE*>(m_pOperator)->getPredicateType();
 
     if (bFirst)
     {

commit 49d320c6202a569f996c27fd824239f5f1f8a036

tdf#97853 connectivity: fix inverted condition
Also in 2 places IS_TYPE was converted to dynamic_cast instead of a
typeid check; use typeid there too, just for consistency.

(regression from ac9671f94800b647f82b12e718968311a025e87e)

Change-Id: I0df47a845eff660c791304c22c97704589ab999f

connectivity/source/drivers/file/fcode.cxx
@@ -366,7 +366,7 @@ void ONthOperator::Exec(OCodeStack& rCodeStack)
     ::std::vector<OOperand*>::const_iterator aEnd = aOperands.end();
     for (; aIter != aEnd; ++aIter)
     {
-        if (typeid(OOperandResult) != typeid(*(*aIter)))
+        if (typeid(OOperandResult) == typeid(*(*aIter)))
             delete *aIter;
     }
 }

connectivity/source/drivers/file/fcomp.cxx
@@ -567,7 +567,7 @@ bool OPredicateInterpreter::evaluate(OCodeList& rCodeList)
     DBG_ASSERT(pOperand, "StackFehler");
 
     bResult = pOperand->isValid();
-    if (dynamic_cast<const OOperandResult* >(pOperand) !=  nullptr)
+    if (typeid(OOperandResult) == typeid(*pOperand))
         delete pOperand;
     return bResult;
 }
@@ -594,7 +594,7 @@ void OPredicateInterpreter::evaluateSelection(OCodeList& rCodeList,ORowSetValueD
     DBG_ASSERT(pOperand, "StackFehler");
 
     (*_rVal) = pOperand->getValue();
-    if (dynamic_cast<const OOperandResult* >(pOperand) !=  nullptr)
+    if (typeid(OOperandResult) == typeid(*pOperand))
         delete pOperand;
 }
 