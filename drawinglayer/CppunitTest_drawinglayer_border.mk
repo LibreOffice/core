d9c888f1d08e3b8f3de28fa37aebcfc2d4a85389
Miklos Vajna <vmiklos@collabora.co.uk>
2016-04-15 15:46:06 +0200

    tdf#99315 VclPixelProcessor2D: fix double border line width
    Regression from commit 2c91cb08d65cd35fa8ef6eaca3677aa82fb58cbe (better
    drawing support for borders of different width, fdo#33634, 2012-04-04),
    the problem is that previously the width of inner/outer double border
    lines got rounded to integer values quite early, but after the commit
    they are kept at a double precision for much longer, which needs pixel
    correction in VclPixelProcessor2D.
    
    Example: if the border with is 1.47, and the line gets moved by 0.2
    pixels, then the inner and outer edge of the line will be 0.2 and 1.67,
    which gets rounded to 0 -> 2 in the pixel processor. Previously the
    input was rounded to 1, so moving by 0.2 resulted in 0.2 -> 1.2, which
    got rounded to 0 -> 1. The result is that sometimes the line width is 1
    pixel wider than expected.
    
    Fix the problem by allowing VclPixelProcessor2D to request pixel
    correction from BorderLinePrimitive2D. It wouldn't be possible to do
    pixel correction only in VclPixelProcessor2D, as it has no idea what to
    correct: it only gets polygons, so it has no idea if e.g. the top of a
    polygon is the outer edge of a top border line or an inner edge of a
    bottom border line.
    
    Conflicts:
    	drawinglayer/source/primitive2d/borderlineprimitive2d.cxx
    
    (cherry picked from commits 1ee570a4e625719f8bf270d372926c0d829ae6f0,
    555c9add26e06030402c73f885de98f4b96826f0,
    304f50684d3ac08e973fd27e6acf3e821394d164 and
    422f10c5d7ebe6f4b778636c9c1eb6dbdf708a27)
    
    Change-Id: I1971f3a952fbcdc598ab46c659e12d976c13cbe6

---
+++ drawinglayer/CppunitTest_drawinglayer_border.mk
@@ -0,0 +1,52 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_CppunitTest_CppunitTest,drawinglayer_border))
+
+$(eval $(call gb_CppunitTest_use_api,drawinglayer_border,\
+	offapi \
+	udkapi \
+))
+
+$(eval $(call gb_CppunitTest_use_libraries,drawinglayer_border, \
+	basegfx \
+	cppu \
+	cppuhelper \
+	sal \
+	salhelper \
+	drawinglayer \
+	vcl \
+	test \
+	tl \
+	$(gb_UWINAPI) \
+))
+
+$(eval $(call gb_CppunitTest_use_externals,drawinglayer_border,\
+	boost_headers \
+	libxml2 \
+))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,drawinglayer_border, \
+	drawinglayer/qa/unit/border \
+))
+
+$(eval $(call gb_CppunitTest_use_ure,drawinglayer_border))
+
+$(eval $(call gb_CppunitTest_use_vcl,drawinglayer_border))
+
+$(eval $(call gb_CppunitTest_use_components,drawinglayer_border,\
+    configmgr/source/configmgr \
+    i18npool/util/i18npool \
+    ucb/source/core/ucb1 \
+    ucb/source/ucp/file/ucpfile1 \
+))
+
+$(eval $(call gb_CppunitTest_use_configuration,drawinglayer_border))
+
+# vim: set noet sw=4 ts=4:
--- drawinglayer/source/primitive2d/borderlineprimitive2d.cxx
+++ drawinglayer/source/primitive2d/borderlineprimitive2d.cxx
@@ -63,7 +63,7 @@ primitive2d::Primitive2DReference makeSolidLinePrimitive(
     const basegfx::B2DVector& rVector, const basegfx::BColor& rColor, double fLineWidth, double fGap)
 {
     const basegfx::B2DVector aPerpendicular = basegfx::getPerpendicular(rVector);
-    const basegfx::B2DVector aLineWidthOffset = ((fLineWidth + 1.0) * 0.5) * aPerpendicular;
+    const basegfx::B2DVector aLineWidthOffset = (fLineWidth * 0.5) * aPerpendicular;
 
     basegfx::B2DPolygon aPolygon;
     aPolygon.append(rStart + aLineWidthOffset);
@@ -163,6 +163,11 @@ primitive2d::Primitive2DReference makeSolidLinePrimitive(
 
         Primitive2DSequence BorderLinePrimitive2D::create2DDecomposition(const geometry::ViewInformation2D& rViewInformation) const
         {
+            return createDecomposition(rViewInformation, false);
+        }
+
+        Primitive2DSequence BorderLinePrimitive2D::createDecomposition(const geometry::ViewInformation2D& rViewInformation, bool bPixelCorrection) const
+        {
             Primitive2DSequence xRetval;
 
             if(!getStart().equal(getEnd()) && ( isInsideUsed() || isOutsideUsed() ) )
@@ -199,8 +204,11 @@ primitive2d::Primitive2DReference makeSolidLinePrimitive(
                         xRetval[0] = makeHairLinePrimitive(
                             getStart(), getEnd(), aVector, getRGBColorLeft(), 0.0);
                     else
+                    {
+                        double fWidth = bPixelCorrection ? std::round(fLeftWidth) : fLeftWidth;
                         xRetval[0] = makeSolidLinePrimitive(
-                            aClipRegion, aTmpStart, aTmpEnd, aVector, getRGBColorLeft(), fLeftWidth, -fLeftWidth/2.0);
+                            aClipRegion, aTmpStart, aTmpEnd, aVector, getRGBColorLeft(), fWidth, -fLeftWidth/2.0);
+                    }
 
                     // "outside" line
 
@@ -208,8 +216,11 @@ primitive2d::Primitive2DReference makeSolidLinePrimitive(
                         xRetval[1] = makeHairLinePrimitive(
                             getStart(), getEnd(), aVector, getRGBColorRight(), fLeftWidth+mfDistance);
                     else
+                    {
+                        double fWidth = bPixelCorrection ? std::round(fRightWidth) : fRightWidth;
                         xRetval[1] = makeSolidLinePrimitive(
-                            aClipRegion, aTmpStart, aTmpEnd, aVector, getRGBColorRight(), fRightWidth, mfDistance+fRightWidth/2.0);
+                            aClipRegion, aTmpStart, aTmpEnd, aVector, getRGBColorRight(), fWidth, mfDistance+fRightWidth/2.0);
+                    }
                 }
                 else
                 {
