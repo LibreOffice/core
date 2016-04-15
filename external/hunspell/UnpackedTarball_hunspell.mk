21579e424eac782438f602dd2e5f7c3073350f7d
Caolán McNamara <caolanm@redhat.com>
2016-04-15 13:07:55 +0100

    test pre 1.4.0 hunspell
    Change-Id: Ied87b41ec02b993fd08ac13f325b9489b6f07456

--- external/hunspell/UnpackedTarball_hunspell.mk
+++ external/hunspell/UnpackedTarball_hunspell.mk
@@ -13,8 +13,6 @@ $(eval $(call gb_UnpackedTarball_set_tarball,hunspell,$(HUNSPELL_TARBALL)))
 
 $(eval $(call gb_UnpackedTarball_add_patches,hunspell,\
 	external/hunspell/hunspell-solaris.patch \
-	external/hunspell/hunspell-windows.patch \
-	external/hunspell/ubsan.patch.0 \
 ))
 
 ifeq ($(COM),MSC)
