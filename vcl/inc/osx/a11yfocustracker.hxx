06a49d7ff98f058788fe4e2263daff51a66d234c
Stephan Bergmann <sbergman@redhat.com>
2016-04-16 18:00:33 +0200

    loplugin:passstuffbyref
    Change-Id: I00ae9a495c69c8394f54c76d61c886229d746579

--- vcl/inc/osx/a11yfocustracker.hxx
+++ vcl/inc/osx/a11yfocustracker.hxx
@@ -41,7 +41,7 @@ class AquaA11yFocusTracker : public rtl::Static< AquaA11yFocusTracker, AquaA11yF
 public:
     AquaA11yFocusTracker();
 
-    css::uno::Reference< css::accessibility::XAccessible > getFocusedObject() { return m_xFocusedObject; };
+    css::uno::Reference< css::accessibility::XAccessible > const & getFocusedObject() { return m_xFocusedObject; };
 
     // sets the currently focus object and notifies the FocusEventListener (if any)
     void setFocusedObject(const css::uno::Reference< css::accessibility::XAccessible >& xAccessible);
