b342b524f0cdbbd804ebe324b672f9a2fc157877
Miklos Vajna <vmiklos@collabora.co.uk>
2016-04-19 11:28:47 +0200

    sc: fix loplugin:passstuffbyref
    (cherry picked from commit 7218011f134250a2ad3e03ff28d5665265c50605)
    
    Change-Id: I6716817b0ca439b63cc7b49eb7a611c2c15a9b00

--- sc/inc/listenercontext.hxx
+++ sc/inc/listenercontext.hxx
@@ -13,7 +13,6 @@
 #include "address.hxx"
 #include "columnspanset.hxx"
 
-#include <boost/noncopyable.hpp>
 #include <memory>
 
 class ScDocument;
@@ -25,12 +24,14 @@ struct ColumnBlockPosition;
 class ColumnBlockPositionSet;
 class ColumnSet;
 
-class StartListeningContext : boost::noncopyable
+class StartListeningContext
 {
     ScDocument& mrDoc;
     std::shared_ptr<ColumnBlockPositionSet> mpSet;
     std::shared_ptr<const ColumnSet> mpColSet;
 public:
+    StartListeningContext(const StartListeningContext&) = delete;
+    const StartListeningContext& operator=(const StartListeningContext&) = delete;
     StartListeningContext(ScDocument& rDoc);
     StartListeningContext(ScDocument& rDoc, const std::shared_ptr<ColumnBlockPositionSet>& pSet);
     void setColumnSet( const std::shared_ptr<const ColumnSet>& pColSet );
@@ -40,7 +41,7 @@ public:
     ColumnBlockPosition* getBlockPosition(SCTAB nTab, SCCOL nCol);
 };
 
-class EndListeningContext : boost::noncopyable
+class EndListeningContext
 {
     ScDocument& mrDoc;
     ColumnSpanSet maSet;
@@ -49,6 +50,8 @@ class EndListeningContext : boost::noncopyable
     ScAddress maPosDelta; // Add this to get the old position prior to the move.
 
 public:
+    EndListeningContext(const EndListeningContext&) = delete;
+    const EndListeningContext& operator=(const EndListeningContext&) = delete;
     EndListeningContext(ScDocument& rDoc, ScTokenArray* pOldCode = nullptr);
     EndListeningContext(ScDocument& rDoc, const std::shared_ptr<ColumnBlockPositionSet>& pSet, ScTokenArray* pOldCode = nullptr);
 
@@ -64,12 +67,14 @@ public:
     void purgeEmptyBroadcasters();
 };
 
-class PurgeListenerAction : public ColumnSpanSet::Action, boost::noncopyable
+class PurgeListenerAction : public ColumnSpanSet::Action
 {
     ScDocument& mrDoc;
     std::unique_ptr<ColumnBlockPosition> mpBlockPos;
 
 public:
+    PurgeListenerAction(const PurgeListenerAction&) = delete;
+    const PurgeListenerAction& operator=(const PurgeListenerAction&) = delete;
     PurgeListenerAction( ScDocument& rDoc );
 
     virtual void startColumn( SCTAB nTab, SCCOL nCol ) override;
