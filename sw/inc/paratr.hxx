d5e07bd49ac6bcc8dc8573228e952da693241997
Miklos Vajna <vmiklos@collabora.co.uk>
2016-04-19 13:55:33 +0200

    drawinglayer, sw: indentation fixes
    Change-Id: Ia84182629f33220a0d85bde00c16f9c26f45e3c5

--- sw/inc/paratr.hxx
+++ sw/inc/paratr.hxx
@@ -71,7 +71,7 @@ private:
     SwFormatDrop & operator= (const SwFormatDrop &) = delete;
 
 protected:
-   virtual void Modify( const SfxPoolItem*, const SfxPoolItem* ) override;
+    virtual void Modify( const SfxPoolItem*, const SfxPoolItem* ) override;
 
 public:
 
