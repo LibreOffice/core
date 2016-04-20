a59454c45b9e01eb2fa0283827f68ed31b3045e1
Jochen Nitschke <j.nitschke+logerrit@ok.de>
2016-04-20 20:10:54 +0200

    remove unused BiffStringFlags
    users are gone since bb5fa5773461e14de153266ccfa36e5f454ff9b8
    
    Change-Id: I8a73fdbec0df467bcaa3dc26fae5c965fd5bc612

--- sc/source/filter/inc/richstring.hxx
+++ sc/source/filter/inc/richstring.hxx
@@ -34,16 +34,6 @@ namespace com { namespace sun { namespace star {
 namespace oox {
 namespace xls {
 
-/** Flags used to specify import/export mode of strings. */
-typedef sal_Int32 BiffStringFlags;
-
-const BiffStringFlags BIFF_STR_DEFAULT      = 0x0000;   /// Default string settings.
-const BiffStringFlags BIFF_STR_FORCEUNICODE = 0x0001;   /// Always use UCS-2 characters (default: try to compress). BIFF8 export only.
-const BiffStringFlags BIFF_STR_8BITLENGTH   = 0x0002;   /// 8-bit string length field (default: 16-bit).
-const BiffStringFlags BIFF_STR_SMARTFLAGS   = 0x0004;   /// Omit flags on empty string (default: read/write always). BIFF8 only.
-const BiffStringFlags BIFF_STR_KEEPFONTS    = 0x0008;   /// Keep old fonts when reading unformatted string (default: clear fonts). Import only.
-const BiffStringFlags BIFF_STR_EXTRAFONTS   = 0x0010;   /// Read trailing rich-string font array (default: nothing). BIFF2-BIFF5 import only.
-
 /** Contains text data and font attributes for a part of a rich formatted string. */
 class RichStringPortion : public WorkbookHelper
 {
