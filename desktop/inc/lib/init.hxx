e6a429770bde5da75239961ae88c06c78cfa5686
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2016-04-19 21:26:42 -0400

    New LOKDocument callback queue to flush certain events lazily on idle
    Change-Id: I36834d6139b5feb0bab2a40c55b379b6f281d45d
    Reviewed-on: https://gerrit.libreoffice.org/24252
    Reviewed-by: Ashod Nakashian <ashnakash@gmail.com>
    Tested-by: Ashod Nakashian <ashnakash@gmail.com>

--- desktop/inc/lib/init.hxx
+++ desktop/inc/lib/init.hxx
@@ -6,14 +6,19 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
+
+#include <map>
+#include <mutex>
+
+#include <boost/shared_ptr.hpp>
+
+#include <osl/thread.h>
+#include <vcl/idle.hxx>
 #include <LibreOfficeKit/LibreOfficeKit.h>
 #include <LibreOfficeKit/LibreOfficeKitEnums.h>
 #include <com/sun/star/frame/XStorable.hpp>
 #include <com/sun/star/lang/XComponent.hpp>
-#include <boost/shared_ptr.hpp>
-#include <map>
 #include "../../source/inc/desktopdllapi.h"
-#include <osl/thread.h>
 
 using namespace css;
 using namespace boost;
@@ -21,12 +26,92 @@ using namespace boost;
 class LOKInteractionHandler;
 
 namespace desktop {
+
+    class CallbackFlushHandler : public Idle
+    {
+    public:
+        explicit CallbackFlushHandler(LibreOfficeKitCallback pCallback, void* pData)
+            : Idle( "lokit idle callback" ),
+              m_pCallback(pCallback),
+              m_pData(pData)
+        {
+            SetPriority(SchedulerPriority::POST_PAINT);
+        }
+
+        virtual ~CallbackFlushHandler()
+        {
+            Stop();
+
+            // Wait for Invoke to finish, if called.
+            std::unique_lock<std::mutex> lock(m_mutex);
+        }
+
+        virtual void Invoke() override
+        {
+            std::unique_lock<std::mutex> lock(m_mutex);
+            flush();
+        }
+
+        static
+        void callback(const int type, const char* payload, void* data)
+        {
+            CallbackFlushHandler* self = reinterpret_cast<CallbackFlushHandler*>(data);
+            if (self)
+            {
+                self->queue(type, payload);
+            }
+        }
+
+        void queue(const int type, const char* payload)
+        {
+            std::unique_lock<std::mutex> lock(m_mutex);
+
+            // TODO: Add more state tracking and prune superfluous notifications.
+            if (type == LOK_CALLBACK_INVALIDATE_TILES || type == LOK_CALLBACK_TEXT_SELECTION)
+            {
+                if (m_queue.empty() || std::get<0>(m_queue.back()) != type)
+                {
+                    m_queue.emplace_back(type, std::string(payload ? payload : "(nil)"));
+
+                    if (!IsActive())
+                    {
+                        Start();
+                    }
+                }
+            }
+            else
+            {
+                m_queue.emplace_back(type, std::string(payload ? payload : "(nil)"));
+                flush();
+            }
+        }
+
+    private:
+        void flush()
+        {
+            if (m_pCallback)
+            {
+                for (auto& pair : m_queue)
+                {
+                    m_pCallback(std::get<0>(pair), std::get<1>(pair).c_str(), m_pData);
+                }
+            }
+
+            m_queue.clear();
+        }
+
+    private:
+        std::vector<std::tuple<int, std::string>> m_queue;
+        LibreOfficeKitCallback m_pCallback;
+        void *m_pData;
+        std::mutex m_mutex;
+    };
+
     struct DESKTOP_DLLPUBLIC LibLODocument_Impl : public _LibreOfficeKitDocument
     {
         uno::Reference<css::lang::XComponent> mxComponent;
         shared_ptr< LibreOfficeKitDocumentClass > m_pDocumentClass;
-        LibreOfficeKitCallback mpCallback;
-        void *mpCallbackData;
+        shared_ptr<CallbackFlushHandler> mpCallbackFlushHandler;
 
         explicit LibLODocument_Impl(const uno::Reference <css::lang::XComponent> &xComponent);
         ~LibLODocument_Impl();
