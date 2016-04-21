a4d7976c99b9ec49202dcac7e4dcb416755415bf
Stephan Bergmann <sbergman@redhat.com>
2016-04-21 09:28:42 +0200

    comphelper::UStringMixLess defaults to bCaseSensitive = true
    ...which then behaves the same as the default std::less<OUString>
    
    Change-Id: If18ef434a752bc77012e665d3621bc319f2897e8

--- dbaccess/source/ui/querydesign/QueryDesignView.cxx
+++ dbaccess/source/ui/querydesign/QueryDesignView.cxx
@@ -461,7 +461,7 @@ namespace
         }
         return BuildJoin(_xConnection, rRh, BuildTable(_xConnection,pLh), &data);
     }
-    typedef ::std::map< OUString,bool,::comphelper::UStringMixLess> tableNames_t;
+    typedef ::std::map< OUString,bool> tableNames_t;
     void addConnectionTableNames( const Reference< XConnection>& _xConnection,
                                   const OQueryTableConnection* const pEntryConn,
                                   tableNames_t &_rTableNames )
