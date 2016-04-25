LibreOffice/core

commit 26b4f5be815bc7c77deb3d38d23b51c9dea9fcd1

java:use System.arrayCopy to copy arrays
instead of a for loop

Change-Id: I3fbd92c5f96970543c2da3843e54a733bf811e72

bean/com/sun/star/comp/beans/OOoBean.java
@@ -1350,8 +1350,7 @@ xLayoutManager.showElement("private:resource/menubar/menubar");
             new com.sun.star.beans.PropertyValue[ nNumArgs + 1 ];
 
         // copy current arguments
-        for ( int n = 0; n < nNumArgs; ++n )
-            aExtendedArguments[n] = aArguments[n];
+        System.arraycopy(aArguments, 0, aExtendedArguments, 0, nNumArgs);
 
         // add new argument
         aExtendedArguments[ nNumArgs ] = aArgument;

odk/source/com/sun/star/lib/loader/Loader.java
@@ -218,9 +218,7 @@ public final class Loader {
                         return;
                     }
                     byte[] buf2 = new byte[2 * n];
-                    for (int i = 0; i < n; ++i) {
-                        buf2[i] = buf[i];
-                    }
+                    System.arraycopy(buf, 0, buf2, 0, n);
                     buf = buf2;
                 }
                 int k = s.read(buf, n, buf.length - n);

qadevOOo/tests/java/ifc/form/binding/_XBindableValue.java
@@ -82,11 +82,10 @@ public class _XBindableValue extends MultiMethodTest {
     }
 
     class MyValueBinding implements XValueBinding {
-        private Type[] TypeArray;
         private final ArrayList<Type> types = new ArrayList<Type>();
 
         public com.sun.star.uno.Type[] getSupportedValueTypes() {
-            return TypeArray;
+            return types.toArray(new Type[types.size()]);
         }
 
         public Object getValue(com.sun.star.uno.Type type)
@@ -101,12 +100,6 @@ public class _XBindableValue extends MultiMethodTest {
 
         public boolean supportsType(com.sun.star.uno.Type type) {
             types.add(type);
-            TypeArray = new Type[types.size()];
-
-            for (int i = 0; i < types.size(); i++) {
-                TypeArray[i] = (Type) types.toArray()[i];
-            }
-
             return true;
         }
     }

commit 0082984fa5b9b11810a83d8dd7253af0ce315fec

bean: fix the OfficeBean SDK example
Regression from 26b4f5be815bc7c77deb3d38d23b51c9dea9fcd1 (java:use
System.arrayCopy to copy arrays, 2015-06-09), as the old code used to handle
the case when aArguments was null.

How to reproduce:

1) Source the SDK environment.
2) cd instdir/sdk/examples/DevelopersGuide/OfficeBean/
3) make OOoBeanViewer.run
4) Click new document... -> Text Document -> NullPointerException

Change-Id: I5521c7fd200f6c74c5f2665f2e4a99f0d6abfeeb

bean/com/sun/star/comp/beans/OOoBean.java
@@ -1349,7 +1349,8 @@ xLayoutManager.showElement("private:resource/menubar/menubar");
             new com.sun.star.beans.PropertyValue[ nNumArgs + 1 ];
 
         // copy current arguments
-        System.arraycopy(aArguments, 0, aExtendedArguments, 0, nNumArgs);
+        if (aArguments != null)
+            System.arraycopy(aArguments, 0, aExtendedArguments, 0, nNumArgs);
 
         // add new argument
         aExtendedArguments[ nNumArgs ] = aArgument;