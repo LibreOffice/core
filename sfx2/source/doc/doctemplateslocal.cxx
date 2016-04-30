3962ae32d442b1a33d78a877690930fc45e990b0
melikeyurtoglu <aysemelikeyurtoglu@gmail.com>
2016-04-30 20:54:57 +0300

    tdf#96890 The file "groupuinames.xml" is invalid XML
    Change-Id: I623d5bbae5a016569f5c1f8174c22ee97db94467
    Signed-off-by: melikeyurtoglu <aysemelikeyurtoglu@gmail.com>
    Reviewed-on: https://gerrit.libreoffice.org/24539
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Markus Mohrhard <markus.mohrhard@googlemail.com>

--- sfx2/source/doc/doctemplateslocal.cxx
+++ sfx2/source/doc/doctemplateslocal.cxx
@@ -63,7 +63,7 @@ void SAL_CALL DocTemplLocaleHelper::WriteGroupLocalizationSequence( const uno::R
     ::comphelper::AttributeList* pRootAttrList = new ::comphelper::AttributeList;
     uno::Reference< xml::sax::XAttributeList > xRootAttrList( pRootAttrList );
     pRootAttrList->AddAttribute(
-        "xmlns",
+        "xmlns:groupinames",
         aCDATAString,
         "http://openoffice.org/2006/groupuinames" );
 
