34fce577f0107c6d0d76bb75b08cbcb814995cce
kadertarlan <kadertarlan1@gmail.com>
2016-05-01 16:01:45 +0300

    tdf#45904 move java based api tests to c++ (AccessibleRelationSet)
    Change-Id: I2d9d72bbc845acf091570df663849495619ee765

--- sw/JunitTest_sw_complex.mk
+++ sw/JunitTest_sw_complex.mk
@@ -25,7 +25,6 @@ $(eval $(call gb_JunitTest_set_defs,sw_complex,\
 ))
 
 $(eval $(call gb_JunitTest_add_sourcefiles,sw_complex,\
-    sw/qa/complex/accessibility/AccessibleRelationSet \
     sw/qa/complex/indeterminateState/CheckIndeterminateState \
     sw/qa/complex/writer/CheckBookmarks \
     sw/qa/complex/writer/TestDocument \
@@ -41,7 +40,6 @@ $(eval $(call gb_JunitTest_use_jars,sw_complex,\
 ))
 
 $(eval $(call gb_JunitTest_add_classes,sw_complex,\
-    complex.accessibility.AccessibleRelationSet \
     complex.writer.CheckBookmarks \
     complex.writer.TextPortionEnumerationTest \
 ))
