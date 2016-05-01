7983c404e83c8223f9192a67fe9c8b3101a699b7
Akshay Deep <akshaydeepiitr@gmail.com>
2016-05-01 13:05:00 +0530

    New Template Manager
    1. Save Mode removed from Template Manager
    2. Context Menu for TemplateViewItems (Handled from LocalView for Local Repos)
    3. 'showAllTemplates()' replacing 'showRootRegion()'
    4. Filter Combobox for templates (Remembers filters also)
    5. Search Filter (Synchronized with Filter ComboBoxes)
    6. Removed Tabs from Template Manager
    7. Removed Buttons from TemplateAbstractView
    8. Unused GtkToolButtons removed
    9. PushButtons in UI
    10. Modal dialog for Import and Move (Works from "All Categories" now too)
    11. ContextMenu for TemplateSearchView
    12. Delete Categories(Folder) in Settings Menu
    13. Save As Template Dialog
    
    Change-Id: I88f6568c35271c17dbd7e6877d50119a8cfe4d60

---
+++ sfx2/source/doc/saveastemplatedlg.cxx
@@ -0,0 +1,169 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <sfx2/saveastemplatedlg.hxx>
+
+#include <comphelper/processfactory.hxx>
+#include <comphelper/string.hxx>
+#include <sfx2/sfxresid.hxx>
+#include <sfx2/doctempl.hxx>
+#include <vcl/edit.hxx>
+#include <vcl/layout.hxx>
+#include <vcl/lstbox.hxx>
+
+#include <com/sun/star/frame/DocumentTemplates.hpp>
+
+#include "doc.hrc"
+
+using namespace ::com::sun::star;
+using namespace ::com::sun::star::frame;
+
+// Class SfxSaveAsTemplateDialog --------------------------------------------------
+
+SfxSaveAsTemplateDialog::SfxSaveAsTemplateDialog( vcl::Window* pParent):
+        ModalDialog(pParent, "SaveAsTemplateDialog", "sfx/ui/templatesaveasdlg.ui"),
+        msSelectedCategory(OUString()),
+        msTemplateName(OUString()),
+        mnRegionPos(0),
+        mpDocTemplates(new SfxDocumentTemplates)
+{
+    get(mpLBCategory, "categorylb");
+    get(mpTemplateNameEdit, "name_entry");
+    get(mpOKButton, "ok");
+
+    initialize();
+    SetCategoryLBEntries(msCategories);
+
+    mpTemplateNameEdit->SetModifyHdl(LINK(this, SfxSaveAsTemplateDialog, TemplateNameEditHdl));
+    mpLBCategory->SetSelectHdl(LINK(this, SfxSaveAsTemplateDialog, SelectCategoryHdl));
+    mpOKButton->SetClickHdl(LINK(this, SfxSaveAsTemplateDialog, OkClickHdl));
+
+    mpOKButton->Disable();
+    mpOKButton->SetText(SfxResId(STR_SAVEDOC).toString());
+}
+
+SfxSaveAsTemplateDialog::~SfxSaveAsTemplateDialog()
+{
+    disposeOnce();
+}
+
+void SfxSaveAsTemplateDialog::dispose()
+{
+    mpLBCategory.clear();
+    mpTemplateNameEdit.clear();
+    mpOKButton.clear();
+
+    ModalDialog::dispose();
+}
+
+void SfxSaveAsTemplateDialog::setDocumentModel(const uno::Reference<frame::XModel> &rModel)
+{
+    m_xModel = rModel;
+}
+
+IMPL_LINK_NOARG_TYPED(SfxSaveAsTemplateDialog, OkClickHdl, Button*, void)
+{
+    ScopedVclPtrInstance< MessageDialog > aQueryDlg(this, OUString(), VCL_MESSAGE_QUESTION, VCL_BUTTONS_YES_NO);
+
+    if(!IsTemplateNameUnique())
+    {
+        OUString sQueryMsg(SfxResId(STR_QMSG_TEMPLATE_OVERWRITE).toString());
+        sQueryMsg = sQueryMsg.replaceFirst("$1",msTemplateName);
+        aQueryDlg->set_primary_text(sQueryMsg.replaceFirst("$2", msSelectedCategory));
+
+        if( aQueryDlg->Execute() == RET_NO )
+            return;
+    }
+
+    if(SaveTemplate())
+        Close();
+    else
+    {
+        OUString sText( SfxResId(STR_ERROR_SAVEAS).toString() );
+        ScopedVclPtrInstance<MessageDialog>::Create(this, sText.replaceFirst("$1", msTemplateName))->Execute();
+    }
+}
+
+IMPL_LINK_NOARG_TYPED(SfxSaveAsTemplateDialog, TemplateNameEditHdl, Edit&, void)
+{
+    msTemplateName = comphelper::string::strip(mpTemplateNameEdit->GetText(), ' ');
+    SelectCategoryHdl(*mpLBCategory);
+}
+
+IMPL_LINK_NOARG_TYPED(SfxSaveAsTemplateDialog, SelectCategoryHdl, ListBox&, void)
+{
+    if(mpLBCategory->GetSelectEntryPos() == 0)
+    {
+        msSelectedCategory = OUString();
+        mpOKButton->Disable();
+    }
+    else
+    {
+        msSelectedCategory = mpLBCategory->GetSelectEntry();
+        mpOKButton->Enable(!msTemplateName.isEmpty());
+    }
+}
+
+void SfxSaveAsTemplateDialog::initialize()
+{
+    sal_uInt16 nCount = mpDocTemplates->GetRegionCount();
+    for (sal_uInt16 i = 0; i < nCount; ++i)
+    {
+        OUString sCategoryName(mpDocTemplates->GetFullRegionName(i));
+        msCategories.push_back(sCategoryName);
+    }
+}
+
+void SfxSaveAsTemplateDialog::SetCategoryLBEntries(std::vector<OUString> aFolderNames)
+{
+    mpLBCategory->InsertEntry(OUString("None"), 0);
+    if (!aFolderNames.empty())
+    {
+        for (size_t i = 0, n = aFolderNames.size(); i < n; ++i)
+            mpLBCategory->InsertEntry(aFolderNames[i], i+1);
+    }
+    mpLBCategory->SelectEntryPos(0);
+}
+
+bool SfxSaveAsTemplateDialog::IsTemplateNameUnique()
+{
+    std::vector<OUString>::iterator it;
+    it=find(msCategories.begin(), msCategories.end(), msSelectedCategory);
+    mnRegionPos = std::distance(msCategories.begin(), it);
+
+    sal_uInt16 nEntries = mpDocTemplates->GetCount(mnRegionPos);
+    for(sal_uInt16 i = 0; i < nEntries; i++)
+    {
+        OUString aName = mpDocTemplates->GetName(mnRegionPos, i);
+        if(aName == msTemplateName)
+            return false;
+    }
+
+    return true;
+}
+
+bool SfxSaveAsTemplateDialog::SaveTemplate()
+{
+    uno::Reference< frame::XStorable > xStorable(m_xModel, uno::UNO_QUERY_THROW );
+
+    uno::Reference< frame::XDocumentTemplates > xTemplates(frame::DocumentTemplates::create(comphelper::getProcessComponentContext()) );
+
+    if (!xTemplates->storeTemplate( msSelectedCategory, msTemplateName, xStorable ))
+        return false;
+
+    sal_uInt16 nDocId = mpDocTemplates->GetCount(mnRegionPos);
+    OUString     sURL = mpDocTemplates->GetTemplateTargetURLFromComponent(msSelectedCategory, msTemplateName);
+
+    if(!mpDocTemplates->InsertTemplate( mnRegionPos, nDocId, msTemplateName, sURL))
+        return false;
+
+    return true;
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
