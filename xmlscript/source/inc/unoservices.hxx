ef9b6580034cc2ca441a7022778da6e715c7a93c
Yeliz Taneroğlu <yeliztaneroglu@gmail.com>
2016-05-01 13:46:53 +0300

    tdf#74608 Constructor function for XMLBasicExporter
    Change-Id: Ib4721b3f67956a8da48851841ef86bb7b79df1e7

--- xmlscript/source/inc/unoservices.hxx
+++ xmlscript/source/inc/unoservices.hxx
@@ -42,14 +42,6 @@ OUString SAL_CALL getImplementationName_DocumentHandlerImpl();
 css::uno::Reference<css::uno::XInterface> SAL_CALL create_DocumentHandlerImpl(
     css::uno::Reference<css::uno::XComponentContext> const & xContext);
 
-css::uno::Sequence<OUString> SAL_CALL
-getSupportedServiceNames_XMLBasicExporter();
-
-OUString SAL_CALL getImplementationName_XMLBasicExporter();
-
-css::uno::Reference<css::uno::XInterface> SAL_CALL create_XMLBasicExporter(
-    css::uno::Reference<css::uno::XComponentContext> const & xContext);
-
 }
 
 #endif
