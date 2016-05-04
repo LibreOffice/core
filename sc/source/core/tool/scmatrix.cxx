b73137aa58fe3a3562ec02f25114d769910b9740
Kohei Yoshida <kohei.yoshida@gmail.com>
2016-05-03 22:35:16 -0400

    Correct value types for the respective function objects.
    Change-Id: I88771e976fe2a40fd4734b57dcf1728162ba559a
    Reviewed-on: https://gerrit.libreoffice.org/24629
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Kohei Yoshida <libreoffice@kohei.us>

--- sc/source/core/tool/scmatrix.cxx
+++ sc/source/core/tool/scmatrix.cxx
@@ -2415,15 +2415,15 @@ void ScMatrixImpl::MatConcat(SCSIZE nMaxCol, SCSIZE nMaxRow, const ScMatrixRef& 
         };
 
     std::function<void(size_t, size_t, bool)> aBoolFunc =
-        [&](size_t nRow, size_t nCol, double nVal)
+        [&](size_t nRow, size_t nCol, bool nVal)
         {
             OUString aStr;
             rFormatter.GetInputLineString( nVal, nKey, aStr);
             aString[get_index(nMaxRow, nMaxCol, nRow, nCol, nRowOffset, nColOffset)] = aString[get_index(nMaxRow, nMaxCol, nRow, nCol, nRowOffset, nColOffset)] + aStr;
         };
 
-    std::function<void(size_t, size_t, svl::SharedString)> aStringFunc =
-        [&](size_t nRow, size_t nCol, svl::SharedString aStr)
+    std::function<void(size_t, size_t, const svl::SharedString&)> aStringFunc =
+        [&](size_t nRow, size_t nCol, const svl::SharedString& aStr)
         {
             aString[get_index(nMaxRow, nMaxCol, nRow, nCol, nRowOffset, nColOffset)] = aString[get_index(nMaxRow, nMaxCol, nRow, nCol, nRowOffset, nColOffset)] + aStr.getString();
         };
@@ -2465,15 +2465,15 @@ void ScMatrixImpl::MatConcat(SCSIZE nMaxCol, SCSIZE nMaxRow, const ScMatrixRef& 
         };
 
     std::function<void(size_t, size_t, bool)> aBoolFunc2 =
-        [&](size_t nRow, size_t nCol, double nVal)
+        [&](size_t nRow, size_t nCol, bool nVal)
         {
             OUString aStr;
             rFormatter.GetInputLineString( nVal, nKey, aStr);
             aSharedString[get_index(nMaxRow, nMaxCol, nRow, nCol, nRowOffset, nColOffset)] = rStringPool.intern(aString[get_index(nMaxRow, nMaxCol, nRow, nCol, nRowOffset, nColOffset)] + aStr);
         };
 
-    std::function<void(size_t, size_t, svl::SharedString)> aStringFunc2 =
-        [&](size_t nRow, size_t nCol, svl::SharedString aStr)
+    std::function<void(size_t, size_t, const svl::SharedString&)> aStringFunc2 =
+        [&](size_t nRow, size_t nCol, const svl::SharedString& aStr)
         {
             aSharedString[get_index(nMaxRow, nMaxCol, nRow, nCol, nRowOffset, nColOffset)] =
                 rStringPool.intern(aString[get_index(nMaxRow, nMaxCol, nRow, nCol, nRowOffset, nColOffset)] + aStr.getString());
