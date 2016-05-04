498e2b924d4f38cba1c5351039424b6e3cf55aaa
Mark Page <aptitude@btconnect.com>
2016-05-04 10:28:01 +0100

    Update the wmf filter to simplify object ownership
    Changed the GDI style structures to use inheritance, thus object deletion no longer requires a static_cast
    Used std::unique_ptr for GDI objects to enforce object ownership
    Modified the WMF Writer to use std::vector, instead of a raw pointer array when processing handles
    
    Change-Id: Ic635ff9d641427b901eb18468529ea6367859b53

--- vcl/source/filter/wmf/emfwr.cxx
+++ vcl/source/filter/wmf/emfwr.cxx
@@ -293,8 +293,7 @@ bool EMFWriter::WriteEMF(const GDIMetaFile& rMtf)
     maVDev->SetMapMode( rMtf.GetPrefMapMode() );
     // don't work with pixel as destination map mode -> higher resolution preferable
     maDestMapMode.SetMapUnit( MAP_100TH_MM );
-    mpHandlesUsed = new bool[ MAXHANDLES ];
-    memset( mpHandlesUsed, 0, MAXHANDLES * sizeof( bool ) );
+    mpHandlesUsed = std::vector<bool>(MAXHANDLES, false);
     mnHandleCount = mnRecordCount = mnRecordPos = mnRecordPlusPos = 0;
     mbRecordOpen = mbRecordPlusOpen = false;
     mbLineChanged = mbFillChanged = mbTextChanged = false;
@@ -365,7 +364,7 @@ bool EMFWriter::WriteEMF(const GDIMetaFile& rMtf)
             .WriteInt32( aMtfSizeLog.Width() * 10 ).WriteInt32( aMtfSizeLog.Height() * 10 ); //use [MS-EMF 2.2.11] HeaderExtension2 Object
 
     m_rStm.Seek( nEndPos );
-    delete[] mpHandlesUsed;
+    mpHandlesUsed.clear();
 
     return( m_rStm.GetError() == ERRCODE_NONE );
 }
@@ -374,7 +373,7 @@ sal_uLong EMFWriter::ImplAcquireHandle()
 {
     sal_uLong nHandle = HANDLE_INVALID;
 
-    for( sal_uLong i = 0; i < MAXHANDLES && ( HANDLE_INVALID == nHandle ); i++ )
+    for( sal_uLong i = 0; i < mpHandlesUsed.size() && ( HANDLE_INVALID == nHandle ); i++ )
     {
         if( !mpHandlesUsed[ i ] )
         {
@@ -391,7 +390,7 @@ sal_uLong EMFWriter::ImplAcquireHandle()
 
 void EMFWriter::ImplReleaseHandle( sal_uLong nHandle )
 {
-    DBG_ASSERT( nHandle && ( nHandle < MAXHANDLES ), "Handle out of range" );
+    DBG_ASSERT( nHandle && ( nHandle < mpHandlesUsed.size() ), "Handle out of range" );
     mpHandlesUsed[ nHandle - 1 ] = false;
 }
 
