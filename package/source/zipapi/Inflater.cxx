7d91753c5bf91a830620651652fe96dbeed77a92
Stephan Bergmann <sbergman@redhat.com>
2016-05-10 17:03:22 +0200

    Add SAL_FALLTHROUGH, where apparently appropriate
    Change-Id: Ieee43fce49b7f07d6b356e8c2e34e9e92b605957

--- package/source/zipapi/Inflater.cxx
+++ package/source/zipapi/Inflater.cxx
@@ -116,6 +116,7 @@ sal_Int32 Inflater::doInflateBytes (Sequence < sal_Int8 >  &rBuffer, sal_Int32 n
     {
         case Z_STREAM_END:
             bFinished = true;
+            SAL_FALLTHROUGH;
         case Z_OK:
             nOffset += nLength - pStream->avail_in;
             nLength = pStream->avail_in;
