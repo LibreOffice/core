aa74c30eddaa72a2c000f46e01d3ef3611dd98fa
Krishna Keshav <princy.krishnakeshav@gmail.com>
2016-05-11 03:42:21 +0530

    Removed 'altstrfunc.cxx' and .hxx
    renamed and moved 'start()' to  it's only call site -
    as 'startsAsciiCaseInsensitive' lingucomponent/source/languageguessing/simpleguesser.cxx.
    moved inline bool to it's only call site -
    as 'isSeparator(const char)' from altstrfunc.hxx to lingucomponent/source/languageguessing/guess.cxx.
    
    Change-Id: Ic624ba3cab90bd454311219966c19fb550388da1
    Reviewed-on: https://gerrit.libreoffice.org/24864
    Reviewed-by: Michael Meeks <michael.meeks@collabora.com>
    Tested-by: Michael Meeks <michael.meeks@collabora.com>

--- lingucomponent/source/languageguessing/simpleguesser.cxx
+++ lingucomponent/source/languageguessing/simpleguesser.cxx
@@ -47,10 +47,25 @@
 
 #include <sal/types.h>
 
-#include "altstrfunc.hxx"
+#include<rtl/character.hxx>
 #include "simpleguesser.hxx"
 
 using namespace std;
+
+static int startsAsciiCaseInsensitive(const std::string &s1, const std::string &s2){
+            size_t i;
+            int ret = 0;
+
+            size_t min = s1.length();
+            if (min > s2.length())
+                min = s2.length();
+
+            for(i = 0; i < min && s2[i] && s1[i] && !ret; i++){
+                    ret =   rtl::toAsciiUpperCase(s1[i]) -  rtl::toAsciiUpperCase(s2[i]);
+                    if(s1[i] == '.' || s2[i] == '.') {ret = 0;}    //. is a neutral character
+            }
+        return ret;
+ }
 
 /**
  * This 3 following structures are from fingerprint.c and textcat.c
@@ -197,7 +212,7 @@ void SimpleGuesser::XableLanguage(const string& lang, char mask)
     for (size_t i=0; i<tables->size; i++)
     {
         string language(fp_Name(tables->fprint[i]));
-        if (start(language,lang) == 0)
+        if (startsAsciiCaseInsensitive(language,lang) == 0)
             tables->fprint_disable[i] = mask;
     }
 }
