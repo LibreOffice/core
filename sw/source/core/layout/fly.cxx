LibreOffice/core

commit c2ea2e6ca99395cc6d9ea8f2edf009252dc87957

coverity#704901 Dereference after null check
Change-Id: I3e2ad2d3dd1713336b1a01cb7a2b1ff28bb833ea

sw/source/core/layout/fly.cxx
@@ -805,6 +805,7 @@ void SwFlyFrm::_UpdateAttr( const SfxPoolItem *pOld, const SfxPoolItem *pNew,
             }
 
         case RES_COL:
+            if (pOld && pNew)
             {
                 ChgColumns( *(const SwFmtCol*)pOld, *(const SwFmtCol*)pNew );
                 const SwFmtFrmSize &rNew = GetFmt()->GetFrmSize();

commit 838994da2af5f069654456dc86c5ffbc4322c37c

Missing break in switch
caused by c2ea2e6ca99395cc6d9ea8f2edf009252dc87957 "coverity#704901 Dereference
after null check"

Change-Id: I7e53493bcb2857ccc840429cd2fd2e802180aa19

sw/source/core/layout/fly.cxx
@@ -818,8 +818,8 @@ void SwFlyFrame::UpdateAttr_( const SfxPoolItem *pOld, const SfxPoolItem *pNew,
                 if ( FrameSizeChg( rNew ) )
                     NotifyDrawObj();
                 rInvFlags |= 0x1A;
-                break;
             }
+            break;
 
         case RES_FRM_SIZE:
         case RES_FMT_CHG: