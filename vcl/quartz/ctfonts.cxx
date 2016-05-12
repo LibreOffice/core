8d711b2dfdd552e1210eb92091317da27fa7a1b8
Stephan Bergmann <sbergman@redhat.com>
2016-05-12 10:38:58 +0200

    Missing SAL_FALLTHROUGH (see comment above)
    Change-Id: I25d7c76462699e0f206e622841744fef9527108b

--- vcl/quartz/ctfonts.cxx
+++ vcl/quartz/ctfonts.cxx
@@ -181,6 +181,7 @@ static void MyCGPathApplierFunc( void* pData, const CGPathElement* pElement )
         {
             break;
         }
+        SAL_FALLTHROUGH;
     case kCGPathElementAddLineToPoint:
         rPolygon.append( basegfx::B2DPoint( +pElement->points[0].x, -pElement->points[0].y ) );
         break;
