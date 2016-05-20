4ee0bf03d41a6814d5c3a6f4d548cff57a850ffd
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2016-05-20 11:50:10 -0400

    bccu#1781: Impress/Comments - selection not updated on delete
    Change-Id: I694d76785fcf6ec9c158c34cbe07f4d1f2c7a41d
    Reviewed-on: https://gerrit.libreoffice.org/25219
    Reviewed-by: Ashod Nakashian <ashnakash@gmail.com>
    Tested-by: Ashod Nakashian <ashnakash@gmail.com>
    (cherry picked from commit b95e4e8681c9cefb4e1d4177192164e041baf9d9)

--- editeng/source/editeng/editeng.cxx
+++ editeng/source/editeng/editeng.cxx
@@ -1349,6 +1349,7 @@ bool EditEngine::PostKeyEvent( const KeyEvent& rKeyEvent, EditView* pEditView, v
     }
 
     pEditView->pImpEditView->SetEditSelection( aCurSel );
+    pEditView->pImpEditView->DrawSelection();
     pImpEditEngine->UpdateSelections();
 
     if ( ( !IsVertical() && ( nCode != KEY_UP ) && ( nCode != KEY_DOWN ) ) ||
