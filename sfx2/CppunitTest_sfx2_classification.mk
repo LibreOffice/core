cc9e6a245277401d2f7e32b7d440b1fc049287da
Miklos Vajna <vmiklos@collabora.co.uk>
2016-05-20 12:35:41 +0200

    Add initial CppunitTest_sfx2_classification
    Change-Id: Id896d10b8e735b4c95e14863428e4061326c3fbc
    Reviewed-on: https://gerrit.libreoffice.org/25193
    Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
    Tested-by: Jenkins <ci@libreoffice.org>
    (cherry picked from commit 626f6cd534a3563bf854b32fd5f46cff65357929)

---
+++ sfx2/CppunitTest_sfx2_classification.mk
@@ -0,0 +1,41 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_CppunitTest_CppunitTest,sfx2_classification))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,sfx2_classification, \
+    sfx2/qa/cppunit/test_classification \
+))
+
+$(eval $(call gb_CppunitTest_use_sdk_api,sfx2_classification))
+
+$(eval $(call gb_CppunitTest_use_libraries,sfx2_classification, \
+	comphelper \
+	cppu \
+	cppuhelper \
+	test \
+	unotest \
+	vcl \
+    sal \
+    sfx \
+	$(gb_UWINAPI) \
+))
+
+$(eval $(call gb_CppunitTest_use_external,sfx2_classification,boost_headers))
+
+$(eval $(call gb_CppunitTest_use_sdk_api,sfx2_classification))
+
+$(eval $(call gb_CppunitTest_use_ure,sfx2_classification))
+$(eval $(call gb_CppunitTest_use_vcl,sfx2_classification))
+
+$(eval $(call gb_CppunitTest_use_rdb,sfx2_classification,services))
+
+$(eval $(call gb_CppunitTest_use_configuration,sfx2_classification))
+
+# vim: set noet sw=4 ts=4:
