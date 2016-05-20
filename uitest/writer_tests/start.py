a32bfc3f27d6bd8d9d383128840a2f8ecd296aa2
Markus Mohrhard <markus.mohrhard@googlemail.com>
2016-05-21 01:24:01 +0200

    uitest: add demo showing how to use page number
    Change-Id: I718f4c6c8f01ceb556b9beadce5af88491c4efe0

--- uitest/writer_tests/start.py
+++ uitest/writer_tests/start.py
@@ -5,7 +5,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 
-from uitest_helper import UITest
+from uitest_helper import UITest, get_state_as_dict
 
 from helper import mkPropertyValues
 
@@ -53,6 +53,27 @@ def type_text(xContext):
     xWriterEdit = xWriterDoc.getChild("writer_edit")
 
     xWriterEdit.executeAction("TYPE", mkPropertyValues({"TEXT": "This is my first writer text written through the UI testing"}))
+
+    time.sleep(2)
+
+    ui_test.close_doc()
+
+def goto_first_page(xContext):
+    xUITest = xContext.ServiceManager.createInstanceWithContext(
+            "org.libreoffice.uitest.UITest", xContext)
+
+    ui_test = UITest(xUITest, xContext)
+
+    ui_test.create_doc_in_start_center("writer")
+
+    xWriterDoc = xUITest.getTopFocusWindow()
+    xWriterEdit = xWriterDoc.getChild("writer_edit")
+
+    state = get_state_as_dict(xWriterEdit)
+    print(state)
+    while state["CurrentPage"] is "1":
+        xWriterEdit.executeAction("TYPE", mkPropertyValues({"KEYCODE": "RETURN"}))
+        state = get_state_as_dict(xWriterEdit)
 
     time.sleep(2)
 
