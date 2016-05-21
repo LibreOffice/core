4bb0bbcd02afcf247cc3eefa53d04df7c81464db
Markus Mohrhard <markus.mohrhard@googlemail.com>
2016-05-22 01:39:09 +0200

    uitest: add writer demo ui tests to makefile
    Change-Id: I4ba2321deb196f22c95db533266d82c541a95f0a

--- uitest/UITest_writer_demo.mk
+++ uitest/UITest_writer_demo.mk
@@ -1,6 +1,5 @@
 # -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
 #
-#
 # This file is part of the LibreOffice project.
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
@@ -8,9 +7,9 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 
-$(eval $(call gb_Module_Module,uitest))
+$(eval $(call gb_UITest_UITest,writer_demo))
 
-$(eval $(call gb_Module_add_uicheck_targets,uitest,\
-    UITest_calc_demo \
-    UITest_writer_demo \
+$(eval $(call gb_UITest_add_modules,writer_demo,$(SRCDIR)/uitest,\
+	writer_tests/writer_demo.txt \
 ))
+# vim: set noet sw=4 ts=4:
