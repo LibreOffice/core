908e46c7116d62718fc377b1e0eb53b39d42dc1f
Markus Mohrhard <markus.mohrhard@googlemail.com>
2016-05-23 15:27:23 +0200

    use the TDF server as target for crash reports
    Change-Id: I0518bbad2f3550f8cac453c98af88a092448b5cd

--- desktop/source/app/crashreport.cxx
+++ desktop/source/app/crashreport.cxx
@@ -45,7 +45,7 @@ void CrashReporter::writeCommonInfo()
     std::ofstream minidump_file(ini_path, std::ios_base::trunc);
     minidump_file << "ProductName=LibreOffice\n";
     minidump_file << "Version=" LIBO_VERSION_DOTTED "\n";
-    minidump_file << "URL=http://127.0.0.1:8000/submit\n";
+    minidump_file << "URL=http://crashreport.libreoffice.org/submit\n";
     minidump_file.close();
 
     updateMinidumpLocation();
