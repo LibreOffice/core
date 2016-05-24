f36ad4058b353b2e1ea8a0b2add36451b9e1f9b2
Jakub Trzebiatowski <ubap.dev@gmail.com>
2016-05-24 16:44:19 +0200

    Sw UNO API Table Styles getByName
    Implementing TableStyle Family getByName
    Implementing TableStyle Family getByIndex
    Implementing SwXTextTableStyle
    
    Change-Id: I256189db8631a713c4aae6b449409bbfaa776f24

--- sw/inc/unostyle.hxx
+++ sw/inc/unostyle.hxx
@@ -246,6 +246,30 @@ protected:
     virtual void Modify( const SfxPoolItem* pOld, const SfxPoolItem *pNew) override;
 
 };
+
+class SwTableAutoFormat;
+
+/// A text table style is a uno api wrapper for a SwTableAutoFormat
+class SwXTextTableStyle : public cppu::WeakImplHelper< css::style::XStyle >
+{
+    OUString m_sTableAutoFormatName;
+    SwDocShell* m_pDocShell;
+
+    SwTableAutoFormat* GetTableAutoFormat();
+public:
+    SwXTextTableStyle(SwDocShell* pDocShell, const OUString& rTableAutoFormatName);
+    // XStyle
+    virtual sal_Bool SAL_CALL isUserDefined() throw (css::uno::RuntimeException, std::exception) override;
+    virtual sal_Bool SAL_CALL isInUse() throw (css::uno::RuntimeException, std::exception) override;
+    virtual OUString SAL_CALL getParentStyle() throw (css::uno::RuntimeException, std::exception) override;
+    virtual void SAL_CALL setParentStyle(const OUString& aParentStyle ) throw (css::container::NoSuchElementException, css::uno::RuntimeException, std::exception) override;
+
+    //XNamed
+    virtual OUString SAL_CALL getName() throw( css::uno::RuntimeException, std::exception ) override;
+    virtual void SAL_CALL setName(const OUString& Name_) throw( css::uno::RuntimeException, std::exception ) override;
+
+    static css::uno::Reference<css::style::XStyle> CreateXTextTableStyle(SwDocShell* pDocShell, const OUString& rTableAutoFormatName);
+};
 #endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
