LibreOffice/core

commit fd205e40c58aa749fbe458d74ca0c2373a1017dc

sw: clean up no longer needed SwViewShell::isTiledRendering()
Change-Id: Icd719c461613f8c59cad81b256678307f69974e1

sw/source/core/crsr/swcrsr.cxx
@@ -52,6 +52,7 @@
 #include <redline.hxx>
 #include <txatbase.hxx>
 #include <memory>
+#include <comphelper/lok.hxx>
 
 using namespace ::com::sun::star::i18n;
 
@@ -1379,7 +1380,7 @@ bool SwCursor::SelectWordWT( SwViewShell* pViewShell, sal_Int16 nWordType, const
                                 nWordType,
                                 bForward ));
 
-            if (pViewShell->isTiledRendering() && aBndry.startPos == aBndry.endPos && nPtPos > 0)
+            if (comphelper::LibreOfficeKit::isActive() && aBndry.startPos == aBndry.endPos && nPtPos > 0)
             {
                 // nPtPos is the end of the paragraph, select the last word then.
                 --nPtPos;

sw/source/core/crsr/viscrs.cxx
@@ -92,7 +92,7 @@ void SwVisibleCursor::Show()
         m_bIsVisible = true;
 
         // display at all?
-        if( m_pCursorShell->VisArea().IsOver( m_pCursorShell->m_aCharRect ) || m_pCursorShell->isTiledRendering() )
+        if( m_pCursorShell->VisArea().IsOver( m_pCursorShell->m_aCharRect ) || comphelper::LibreOfficeKit::isActive() )
             _SetPosAndShow();
     }
 }
@@ -756,7 +756,7 @@ void SwShellTableCursor::FillRects()
 
     bool bStart = true;
     SwRegionRects aReg( GetShell()->VisArea() );
-    if (GetShell()->isTiledRendering())
+    if (comphelper::LibreOfficeKit::isActive())
         aReg = GetShell()->getIDocumentLayoutAccess().GetCurrentLayout()->Frame();
     SwNodes& rNds = GetDoc()->GetNodes();
     SwFrame* pEndFrame = nullptr;

sw/source/core/layout/trvlfrm.cxx
@@ -47,6 +47,7 @@
 
 #include <cfloat>
 #include <swselectionlist.hxx>
+#include <comphelper/lok.hxx>
 
 namespace {
     bool lcl_GetCursorOfst_Objects( const SwPageFrame* pPageFrame, bool bSearchBackground,
@@ -2020,7 +2021,7 @@ void SwRootFrame::CalcFrameRects(SwShellCursor &rCursor)
 
     bool bIgnoreVisArea = true;
     if (pSh)
-        bIgnoreVisArea = pSh->GetViewOptions()->IsPDFExport() || pSh->isTiledRendering();
+        bIgnoreVisArea = pSh->GetViewOptions()->IsPDFExport() || comphelper::LibreOfficeKit::isActive();
 
     // #i12836# enhanced pdf
     SwRegionRects aRegion( !bIgnoreVisArea ?

sw/source/core/view/viewimp.cxx
@@ -35,6 +35,7 @@
 #include <pagepreviewlayout.hxx>
 #include <comcore.hrc>
 #include <svx/svdundo.hxx>
+#include <comphelper/lok.hxx>
 #include <IDocumentLayoutAccess.hxx>
 #include <IDocumentDrawModelAccess.hxx>
 #include <IDocumentDeviceAccess.hxx>
@@ -132,13 +133,13 @@ void SwViewShellImp::DelRegion()
 bool SwViewShellImp::AddPaintRect( const SwRect &rRect )
 {
     // In case of tiled rendering the visual area is the last painted tile -> not interesting.
-    if ( rRect.IsOver( m_pShell->VisArea() ) || m_pShell->isTiledRendering() )
+    if ( rRect.IsOver( m_pShell->VisArea() ) || comphelper::LibreOfficeKit::isActive() )
     {
         if ( !m_pRegion )
         {
             // In case of normal rendering, this makes sure only visible rectangles are painted.
             // Otherwise get the rectangle of the full document, so all paint rectangles are invalidated.
-            const SwRect& rArea = m_pShell->isTiledRendering() ? m_pShell->GetLayout()->Frame() : m_pShell->VisArea();
+            const SwRect& rArea = comphelper::LibreOfficeKit::isActive() ? m_pShell->GetLayout()->Frame() : m_pShell->VisArea();
             m_pRegion = new SwRegionRects( rArea );
         }
         (*m_pRegion) -= rRect;

sw/source/uibase/docvw/PostItMgr.cxx
@@ -889,7 +889,7 @@ void SwPostItMgr::registerLibreOfficeKitCallback(LibreOfficeKitCallback pCallbac
         if (!pPostIt)
             continue;
 
-        pPostIt->GetOutlinerView()->setTiledRendering(mpWrtShell->isTiledRendering());
+        pPostIt->GetOutlinerView()->setTiledRendering(comphelper::LibreOfficeKit::isActive());
         pPostIt->GetOutlinerView()->registerLibreOfficeKitCallback(pCallback, pData);
     }
 }

sw/source/uibase/docvw/SidebarWin.cxx
@@ -618,7 +618,7 @@ void SwSidebarWin::InitControls()
         LibreOfficeKitCallback pCallback = nullptr;
         void* pData = nullptr;
         pDrawModel->getLibreOfficeKitCallback(pCallback, pData);
-        mpOutlinerView->setTiledRendering(mrView.GetWrtShellPtr()->isTiledRendering());
+        mpOutlinerView->setTiledRendering(comphelper::LibreOfficeKit::isActive());
         mpOutlinerView->registerLibreOfficeKitCallback(pCallback, pData);
     }
 

sw/source/uibase/docvw/edtwin.cxx
@@ -3293,7 +3293,7 @@ void SwEditWin::MouseButtonDown(const MouseEvent& _rMEvt)
                             {
                             case nsSelectionType::SEL_GRF:
                                 RstMBDownFlags();
-                                if (!rSh.isTiledRendering())
+                                if (!comphelper::LibreOfficeKit::isActive())
                                 {
                                     GetView().GetViewFrame()->GetBindings().Execute(
                                         FN_FORMAT_GRAFIC_DLG, nullptr, 0,
@@ -3312,7 +3312,7 @@ void SwEditWin::MouseButtonDown(const MouseEvent& _rMEvt)
 
                             case nsSelectionType::SEL_FRM:
                                 RstMBDownFlags();
-                                if (!rSh.isTiledRendering())
+                                if (!comphelper::LibreOfficeKit::isActive())
                                 {
                                     GetView().GetViewFrame()->GetBindings().Execute(
                                         FN_FORMAT_FRAME_DLG, nullptr, 0,
@@ -3331,7 +3331,7 @@ void SwEditWin::MouseButtonDown(const MouseEvent& _rMEvt)
 
                         // if the cursor position was corrected or if a Fly
                         // was selected in ReadOnlyMode, no word selection, except when tiled rendering.
-                        if ((!g_bValidCursorPos || rSh.IsFrameSelected()) && !rSh.isTiledRendering())
+                        if ((!g_bValidCursorPos || rSh.IsFrameSelected()) && !comphelper::LibreOfficeKit::isActive())
                             return;
 
                         SwField *pField;
@@ -3383,7 +3383,7 @@ void SwEditWin::MouseButtonDown(const MouseEvent& _rMEvt)
                         }
                         else
                         {
-                            if (!rSh.SelWrd(&aDocPos) && rSh.isTiledRendering())
+                            if (!rSh.SelWrd(&aDocPos) && comphelper::LibreOfficeKit::isActive())
                                 // Double click did not select any word: try to
                                 // select the current cell in case we are in a
                                 // table.
@@ -3786,7 +3786,7 @@ void SwEditWin::MouseMove(const MouseEvent& _rMEvt)
     }
 
     const Point aOldPt( rSh.VisArea().Pos() );
-    const bool bInsWin = rSh.VisArea().IsInside( aDocPt ) || rSh.isTiledRendering();
+    const bool bInsWin = rSh.VisArea().IsInside( aDocPt ) || comphelper::LibreOfficeKit::isActive();
 
     if( m_pShadCursor && !bInsWin )
         delete m_pShadCursor, m_pShadCursor = nullptr;
@@ -4891,7 +4891,7 @@ void SwEditWin::MouseButtonUp(const MouseEvent& rMEvt)
     if (bCallBase)
         Window::MouseButtonUp(rMEvt);
 
-    if (pSdrView && rMEvt.GetClicks() == 1 && rSh.isTiledRendering())
+    if (pSdrView && rMEvt.GetClicks() == 1 && comphelper::LibreOfficeKit::isActive())
     {
         // When tiled rendering, single click on a shape text starts editing already.
         SdrViewEvent aViewEvent;
@@ -6301,7 +6301,7 @@ void SwEditWin::LogicInvalidate(const Rectangle* pRectangle)
 void SwEditWin::LogicMouseButtonDown(const MouseEvent& rMouseEvent)
 {
     // When we're not doing tiled rendering, then positions must be passed as pixels.
-    assert(m_rView.GetWrtShell().isTiledRendering());
+    assert(comphelper::LibreOfficeKit::isActive());
 
     Point aPoint = GetPointerPosPixel();
     SetLastMousePos(rMouseEvent.GetPosPixel());
@@ -6314,7 +6314,7 @@ void SwEditWin::LogicMouseButtonDown(const MouseEvent& rMouseEvent)
 void SwEditWin::LogicMouseButtonUp(const MouseEvent& rMouseEvent)
 {
     // When we're not doing tiled rendering, then positions must be passed as pixels.
-    assert(m_rView.GetWrtShell().isTiledRendering());
+    assert(comphelper::LibreOfficeKit::isActive());
 
     Point aPoint = GetPointerPosPixel();
     SetLastMousePos(rMouseEvent.GetPosPixel());
@@ -6327,7 +6327,7 @@ void SwEditWin::LogicMouseButtonUp(const MouseEvent& rMouseEvent)
 void SwEditWin::LogicMouseMove(const MouseEvent& rMouseEvent)
 {
     // When we're not doing tiled rendering, then positions must be passed as pixels.
-    assert(m_rView.GetWrtShell().isTiledRendering());
+    assert(comphelper::LibreOfficeKit::isActive());
 
     Point aPoint = GetPointerPosPixel();
     SetLastMousePos(rMouseEvent.GetPosPixel());

sw/source/uibase/wrtsh/wrtsh2.cxx
@@ -63,10 +63,10 @@
 #include <com/sun/star/document/XDocumentProperties.hpp>
 #include <com/sun/star/document/XDocumentPropertiesSupplier.hpp>
 
-#include <xmloff/odffields.hxx>
 #include <memory>
 
 #include <LibreOfficeKit/LibreOfficeKitEnums.h>
+#include <comphelper/lok.hxx>
 
 void SwWrtShell::Insert(SwField &rField)
 {
@@ -472,7 +472,7 @@ void LoadURL( SwViewShell& rVSh, const OUString& rURL, sal_uInt16 nFilter,
         return;
 
     // We are doing tiledRendering, let the client handles the URL loading.
-    if (rVSh.isTiledRendering())
+    if (comphelper::LibreOfficeKit::isActive())
     {
         rVSh.libreOfficeKitCallback(LOK_CALLBACK_HYPERLINK_CLICKED, rURL.toUtf8().getStr());
         return;

commit 5fc68a744d13cce5ab2f9f0c488d5d65746bebe2

bccu#1811 - Writer comment focusing / selection / cursor issue
Partially based on fd205e40c58aa749fbe458d74ca0c2373a1017dc
which caused make desktop.check to fail when viewsh.?xx
changes were included, so left them out.

Change-Id: I5b4576d2385c1812f358a2434b87c8ca62024430

sw/source/core/crsr/swcrsr.cxx
@@ -51,6 +51,8 @@
 #include <statstr.hrc>
 #include <redline.hxx>
 #include <txatbase.hxx>
+#include <memory>
+#include <comphelper/lok.hxx>
 
 using namespace ::com::sun::star::i18n;
 
@@ -1379,7 +1381,7 @@ bool SwCursor::SelectWordWT( SwViewShell* pViewShell, sal_Int16 nWordType, const
                                 nWordType,
                                 bForward ));
 
-            if (pViewShell->isTiledRendering() && aBndry.startPos == aBndry.endPos && nPtPos > 0)
+            if (comphelper::LibreOfficeKit::isActive() && aBndry.startPos == aBndry.endPos && nPtPos > 0)
             {
                 // nPtPos is the end of the paragraph, select the last word then.
                 --nPtPos;

sw/source/core/crsr/viscrs.cxx
@@ -92,7 +92,7 @@ void SwVisCrsr::Show()
         m_bIsVisible = true;
 
         // display at all?
-        if( m_pCrsrShell->VisArea().IsOver( m_pCrsrShell->m_aCharRect ) || m_pCrsrShell->isTiledRendering() )
+        if( m_pCrsrShell->VisArea().IsOver( m_pCrsrShell->m_aCharRect ) || comphelper::LibreOfficeKit::isActive() )
             _SetPosAndShow();
     }
 }
@@ -757,7 +757,7 @@ void SwShellTableCrsr::FillRects()
 
     bool bStart = true;
     SwRegionRects aReg( GetShell()->VisArea() );
-    if (GetShell()->isTiledRendering())
+    if (comphelper::LibreOfficeKit::isActive())
         aReg = GetShell()->getIDocumentLayoutAccess()->GetCurrentLayout()->Frm();
     SwNodes& rNds = GetDoc()->GetNodes();
     SwFrm* pEndFrm = 0;

sw/source/core/layout/trvlfrm.cxx
@@ -47,6 +47,7 @@
 
 #include <cfloat>
 #include <swselectionlist.hxx>
+#include <comphelper/lok.hxx>
 
 namespace {
     bool lcl_GetCrsrOfst_Objects( const SwPageFrm* pPageFrm, bool bSearchBackground,
@@ -2021,7 +2022,7 @@ void SwRootFrm::CalcFrmRects(SwShellCrsr &rCrsr)
 
     bool bIgnoreVisArea = false;
     if (pSh)
-        bIgnoreVisArea = pSh->GetViewOptions()->IsPDFExport() || pSh->isTiledRendering();
+        bIgnoreVisArea = pSh->GetViewOptions()->IsPDFExport() || comphelper::LibreOfficeKit::isActive();
 
     // #i12836# enhanced pdf
     SwRegionRects aRegion( !bIgnoreVisArea ?

sw/source/core/view/viewimp.cxx
@@ -35,6 +35,7 @@
 #include <pagepreviewlayout.hxx>
 #include <comcore.hrc>
 #include <svx/svdundo.hxx>
+#include <comphelper/lok.hxx>
 #include <IDocumentLayoutAccess.hxx>
 #include <IDocumentDrawModelAccess.hxx>
 #include <IDocumentDeviceAccess.hxx>
@@ -133,13 +134,13 @@ void SwViewShellImp::DelRegion()
 bool SwViewShellImp::AddPaintRect( const SwRect &rRect )
 {
     // In case of tiled rendering the visual area is the last painted tile -> not interesting.
-    if ( rRect.IsOver( pSh->VisArea() ) || pSh->isTiledRendering() )
+    if ( rRect.IsOver( pSh->VisArea() ) || comphelper::LibreOfficeKit::isActive() )
     {
         if ( !pRegion )
         {
             // In case of normal rendering, this makes sure only visible rectangles are painted.
             // Otherwise get the rectangle of the full document, so all paint rectangles are invalidated.
-            const SwRect& rArea = pSh->isTiledRendering() ? pSh->GetLayout()->Frm() : pSh->VisArea();
+            const SwRect& rArea = comphelper::LibreOfficeKit::isActive() ? pSh->GetLayout()->Frm() : pSh->VisArea();
             pRegion = new SwRegionRects( rArea );
         }
         (*pRegion) -= rRect;

sw/source/uibase/docvw/PostItMgr.cxx
@@ -889,7 +889,7 @@ void SwPostItMgr::registerLibreOfficeKitCallback(LibreOfficeKitCallback pCallbac
         if (!pPostIt)
             continue;
 
-        pPostIt->GetOutlinerView()->setTiledRendering(mpWrtShell->isTiledRendering());
+        pPostIt->GetOutlinerView()->setTiledRendering(comphelper::LibreOfficeKit::isActive());
         pPostIt->GetOutlinerView()->registerLibreOfficeKitCallback(pCallback, pData, pSearchable);
     }
 }

sw/source/uibase/docvw/SidebarWin.cxx
@@ -618,7 +618,7 @@ void SwSidebarWin::InitControls()
         LibreOfficeKitCallback pCallback = 0;
         void* pData = 0;
         pDrawModel->getLibreOfficeKitCallback(pCallback, pData);
-        mpOutlinerView->setTiledRendering(mrView.GetWrtShellPtr()->isTiledRendering());
+        mpOutlinerView->setTiledRendering(true);
         mpOutlinerView->registerLibreOfficeKitCallback(pCallback, pData, pDrawModel);
     }
 

sw/source/uibase/docvw/edtwin.cxx
@@ -3293,7 +3293,7 @@ void SwEditWin::MouseButtonDown(const MouseEvent& _rMEvt)
                             {
                             case nsSelectionType::SEL_GRF:
                                 RstMBDownFlags();
-                                if (!rSh.isTiledRendering())
+                                if (!comphelper::LibreOfficeKit::isActive())
                                 {
                                     GetView().GetViewFrame()->GetBindings().Execute(
                                         FN_FORMAT_GRAFIC_DLG, 0, 0,
@@ -3312,7 +3312,7 @@ void SwEditWin::MouseButtonDown(const MouseEvent& _rMEvt)
 
                             case nsSelectionType::SEL_FRM:
                                 RstMBDownFlags();
-                                if (!rSh.isTiledRendering())
+                                if (!comphelper::LibreOfficeKit::isActive())
                                 {
                                     GetView().GetViewFrame()->GetBindings().Execute(
                                         FN_FORMAT_FRAME_DLG, 0, 0,
@@ -3331,7 +3331,7 @@ void SwEditWin::MouseButtonDown(const MouseEvent& _rMEvt)
 
                         // if the cursor position was corrected or if a Fly
                         // was selected in ReadOnlyMode, no word selection, except when tiled rendering.
-                        if ((!g_bValidCrsrPos || rSh.IsFrmSelected()) && !rSh.isTiledRendering())
+                        if ((!g_bValidCrsrPos || rSh.IsFrmSelected()) && !comphelper::LibreOfficeKit::isActive())
                             return;
 
                         SwField *pField;
@@ -3383,7 +3383,7 @@ void SwEditWin::MouseButtonDown(const MouseEvent& _rMEvt)
                         }
                         else
                         {
-                            if (!rSh.SelWrd(&aDocPos) && rSh.isTiledRendering())
+                            if (!rSh.SelWrd(&aDocPos) && comphelper::LibreOfficeKit::isActive())
                                 // Double click did not select any word: try to
                                 // select the current cell in case we are in a
                                 // table.
@@ -3786,7 +3786,7 @@ void SwEditWin::MouseMove(const MouseEvent& _rMEvt)
     }
 
     const Point aOldPt( rSh.VisArea().Pos() );
-    const bool bInsWin = rSh.VisArea().IsInside( aDocPt ) || rSh.isTiledRendering();
+    const bool bInsWin = rSh.VisArea().IsInside( aDocPt ) || comphelper::LibreOfficeKit::isActive();
 
     if( m_pShadCrsr && !bInsWin )
         delete m_pShadCrsr, m_pShadCrsr = 0;
@@ -4890,7 +4890,7 @@ void SwEditWin::MouseButtonUp(const MouseEvent& rMEvt)
     if (bCallBase)
         Window::MouseButtonUp(rMEvt);
 
-    if (pSdrView && rMEvt.GetClicks() == 1 && rSh.isTiledRendering())
+    if (pSdrView && rMEvt.GetClicks() == 1 && comphelper::LibreOfficeKit::isActive())
     {
         // When tiled rendering, single click on a shape text starts editing already.
         SdrViewEvent aViewEvent;
@@ -6294,7 +6294,7 @@ void SwEditWin::LogicInvalidate(const Rectangle* pRectangle)
 void SwEditWin::LogicMouseButtonDown(const MouseEvent& rMouseEvent)
 {
     // When we're not doing tiled rendering, then positions must be passed as pixels.
-    assert(m_rView.GetWrtShell().isTiledRendering());
+    assert(comphelper::LibreOfficeKit::isActive());
 
     Point aPoint = GetPointerPosPixel();
     SetLastMousePos(rMouseEvent.GetPosPixel());
@@ -6307,7 +6307,7 @@ void SwEditWin::LogicMouseButtonDown(const MouseEvent& rMouseEvent)
 void SwEditWin::LogicMouseButtonUp(const MouseEvent& rMouseEvent)
 {
     // When we're not doing tiled rendering, then positions must be passed as pixels.
-    assert(m_rView.GetWrtShell().isTiledRendering());
+    assert(comphelper::LibreOfficeKit::isActive());
 
     Point aPoint = GetPointerPosPixel();
     SetLastMousePos(rMouseEvent.GetPosPixel());
@@ -6320,7 +6320,7 @@ void SwEditWin::LogicMouseButtonUp(const MouseEvent& rMouseEvent)
 void SwEditWin::LogicMouseMove(const MouseEvent& rMouseEvent)
 {
     // When we're not doing tiled rendering, then positions must be passed as pixels.
-    assert(m_rView.GetWrtShell().isTiledRendering());
+    assert(comphelper::LibreOfficeKit::isActive());
 
     Point aPoint = GetPointerPosPixel();
     SetLastMousePos(rMouseEvent.GetPosPixel());

sw/source/uibase/wrtsh/wrtsh2.cxx
@@ -67,6 +67,7 @@
 #include <boost/scoped_ptr.hpp>
 
 #include <LibreOfficeKit/LibreOfficeKitEnums.h>
+#include <comphelper/lok.hxx>
 
 void SwWrtShell::Insert(SwField &rField)
 {
@@ -472,7 +473,7 @@ void LoadURL( SwViewShell& rVSh, const OUString& rURL, sal_uInt16 nFilter,
         return;
 
     // We are doing tiledRendering, let the client handles the URL loading.
-    if (rVSh.isTiledRendering())
+    if (comphelper::LibreOfficeKit::isActive())
     {
         rVSh.libreOfficeKitCallback(LOK_CALLBACK_HYPERLINK_CLICKED, rURL.toUtf8().getStr());
         return;

sw/qa/extras/tiledrendering/tiledrendering.cxx
@@ -174,6 +174,7 @@ void SwTiledRenderingTest::callbackImpl(int nType, const char* pPayload)
 
 void SwTiledRenderingTest::testRegisterCallback()
 {
+    comphelper::LibreOfficeKit::setActive();
     SwXTextDocument* pXTextDocument = createDoc("dummy.fodt");
     pXTextDocument->registerCallback(&SwTiledRenderingTest::callback, this);
     SwWrtShell* pWrtShell = pXTextDocument->GetDocShell()->GetWrtShell();
@@ -184,6 +185,7 @@ void SwTiledRenderingTest::testRegisterCallback()
     CPPUNIT_ASSERT(!m_aInvalidation.IsEmpty());
     Rectangle aTopLeft(0, 0, 256*15, 256*15); // 1 px = 15 twips, assuming 96 DPI.
     CPPUNIT_ASSERT(m_aInvalidation.IsOver(aTopLeft));
+    comphelper::LibreOfficeKit::setActive(false);
 }
 
 void SwTiledRenderingTest::testPostKeyEvent()
@@ -203,6 +205,7 @@ void SwTiledRenderingTest::testPostKeyEvent()
 
 void SwTiledRenderingTest::testPostMouseEvent()
 {
+    comphelper::LibreOfficeKit::setActive();
     SwXTextDocument* pXTextDocument = createDoc("dummy.fodt");
     SwWrtShell* pWrtShell = pXTextDocument->GetDocShell()->GetWrtShell();
     pWrtShell->Right(CRSR_SKIP_CHARS, /*bSelect=*/false, 1, /*bBasicCall=*/false);
@@ -216,6 +219,7 @@ void SwTiledRenderingTest::testPostMouseEvent()
     pXTextDocument->postMouseEvent(LOK_MOUSEEVENT_MOUSEBUTTONUP, aStart.getX(), aStart.getY(), 1);
     // The new cursor position must be before the first word.
     CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(0), pShellCrsr->GetPoint()->nContent.GetIndex());
+    comphelper::LibreOfficeKit::setActive(false);
 }
 
 void SwTiledRenderingTest::testSetTextSelection()
@@ -442,6 +446,7 @@ void SwTiledRenderingTest::testSearchTextFrameWrapAround()
 
 void SwTiledRenderingTest::testDocumentSizeChanged()
 {
+    comphelper::LibreOfficeKit::setActive();
     // Get the current document size.
     SwXTextDocument* pXTextDocument = createDoc("2-pages.odt");
     pXTextDocument->registerCallback(&SwTiledRenderingTest::callback, this);
@@ -455,6 +460,7 @@ void SwTiledRenderingTest::testDocumentSizeChanged()
     CPPUNIT_ASSERT_EQUAL(aSize.getWidth(), m_aDocumentSize.getWidth());
     // Document height should be smaller now.
     CPPUNIT_ASSERT(aSize.getHeight() > m_aDocumentSize.getHeight());
+    comphelper::LibreOfficeKit::setActive(false);
 }
 
 void SwTiledRenderingTest::testSearchAll()