809c127860c4b28bb7930ec614d61faf2f9994db
Markus Mohrhard <markus.mohrhard@googlemail.com>
2016-05-26 12:43:23 +0200

    uitest: add more impress tests
    Change-Id: I89bd0af49e530382f0201a108f8798f9a9d689e3

--- uitest/impress_tests/start.py
+++ uitest/impress_tests/start.py
@@ -29,6 +29,86 @@ def start_impress(xContext):
 
     ui_test.create_doc_in_start_center("impress")
 
+    xImpressDoc = xUITest.getTopFocusWindow()
+    print(get_state_as_dict(xImpressDoc))
+    print(xImpressDoc.getChildren())
+
+    xEditWin = xImpressDoc.getChild("impress_win")
+    xEditWin.executeAction("SET", mkPropertyValues({"ZOOM": "200"}))
+
+    print(xEditWin.getChildren())
+
+    time.sleep(5)
+
+    ui_test.close_doc()
+
+def select_page(xContext):
+    xUITest = xContext.ServiceManager.createInstanceWithContext(
+            "org.libreoffice.uitest.UITest", xContext)
+
+    ui_test = UITest(xUITest, xContext)
+
+    ui_test.create_doc_in_start_center("impress")
+
+    xImpressDoc = xUITest.getTopFocusWindow()
+    print(get_state_as_dict(xImpressDoc))
+    print(xImpressDoc.getChildren())
+
+    xEditWin = xImpressDoc.getChild("impress_win")
+
+    time.sleep(10)
+    print(get_state_as_dict(xEditWin))
+
+    xEditWin.executeAction("GOTO", mkPropertyValues({"PAGE": "2"}))
+
+    time.sleep(5)
+
+    print(get_state_as_dict(xEditWin))
+
+    ui_test.close_doc()
+
+def select_text(xContext):
+    xUITest = xContext.ServiceManager.createInstanceWithContext(
+            "org.libreoffice.uitest.UITest", xContext)
+
+    ui_test = UITest(xUITest, xContext)
+
+    ui_test.create_doc_in_start_center("impress")
+
+    xImpressDoc = xUITest.getTopFocusWindow()
+    print(get_state_as_dict(xImpressDoc))
+    print(xImpressDoc.getChildren())
+
+    xEditWin = xImpressDoc.getChild("impress_win")
+
+    time.sleep(10)
+
+    print(get_state_as_dict(xEditWin))
+
+    ui_test.close_doc()
+
+def select_object(xContext):
+    xUITest = xContext.ServiceManager.createInstanceWithContext(
+            "org.libreoffice.uitest.UITest", xContext)
+
+    ui_test = UITest(xUITest, xContext)
+
+    ui_test.create_doc_in_start_center("impress")
+
+    xImpressDoc = xUITest.getTopFocusWindow()
+    print(get_state_as_dict(xImpressDoc))
+    print(xImpressDoc.getChildren())
+
+    xEditWin = xImpressDoc.getChild("impress_win")
+
+    xEditWin.executeAction("SELECT", mkPropertyValues({"OBJECT":"Unnamed Drawinglayer object 1"}))
+    time.sleep(2)
+    xEditWin.executeAction("DESELECT", tuple())
+
+    time.sleep(4)
+
+    print(get_state_as_dict(xEditWin))
+
     ui_test.close_doc()
 
 # vim:set shiftwidth=4 softtabstop=4 expandtab: */
