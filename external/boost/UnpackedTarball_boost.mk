4168359d0fdf1a9509246d007a991bdb574538be
Michael Stahl <mstahl@redhat.com>
2016-05-27 16:24:33 +0200

    boost: warning-patch-ectomy
    Upstream maintainers, with few exceptions, generally don't care about
    warnings in boost headers, hence we re-base our warning fix patches on
    every upgrade, which is a pointless exercise in frustration.
    
    Most of the patches are for GCC/Clang warnings, where we could use
    -isystem to suppress the warnings (with corresponding hacks in the build
    system, because -isystem also disables dependency generation) - but
    clang-cl does not support -isystem.
    
    So generate a bunch of wrapper headers into external/boost/include, that
    disable all known warnings and use #include_next to get the real boost
    header. This allows us to get rid of most of the existing patches.
    
    There is however a bug in GCC that preprocessor warnings like -Wundef
    cannot be disabled with a #pragma, so those patches cannot be removed.
    https://gcc.gnu.org/bugzilla/show_bug.cgi?id=53431
    
    Change-Id: I2992bf4a463015f1140489df867bd80757f84541

--- external/boost/UnpackedTarball_boost.mk
+++ external/boost/UnpackedTarball_boost.mk
@@ -8,76 +8,34 @@
 #
 
 boost_patches :=
-#https://svn.boost.org/trac/boost/ticket/3780
-boost_patches += boost.3780.aliasing.patch
-#http://gcc.gnu.org/bugzilla/show_bug.cgi?id=47679
-boost_patches += boost.gcc47679.patch
-#https://svn.boost.org/trac/boost/ticket/6369
-boost_patches += boost.6369.warnings.patch
+
 #https://svn.boost.org/trac/boost/ticket/6142
 boost_patches += boost.6142.warnings.patch.1
 
-# https://svn.boost.org/trac/boost/ticket/9882
-boost_patches += boost.date_time.Wshadow.warnings.patch
 # https://svn.boost.org/trac/boost/ticket/9885
 boost_patches += boost.multi_array.C4510.warnings.patch
-# https://svn.boost.org/trac/boost/ticket/9888
-boost_patches += boost.multi_array.Wunused-local-typedefs.warnings.patch
-# https://svn.boost.org/trac/boost/ticket/9891
-boost_patches += boost.preprocessor.Wundef.warnings.patch
-# https://svn.boost.org/trac/boost/ticket/9892
-boost_patches += boost.property_tree.Wshadow.warnings.patch.1
-# https://svn.boost.org/trac/boost/ticket/9900
-boost_patches += boost.spirit.Wshadow.warnings.patch
-# https://svn.boost.org/trac/boost/ticket/9901
-boost_patches += boost.spirit.Wunused-local-typedefs.warnings.patch
-# to-do: submit upstream
-boost_patches += boost.spirit.Wunused-parameter.warnings.patch
-boost_patches += boost.date_time.Wshadow.warnings.patch.1
 # https://svn.boost.org/trac/boost/ticket/9903
 boost_patches += boost.utility.Wundef.warnings.patch
 
-# Help static analysis tools (see SAL_UNUSED_PARAMETER in sal/types.h):
-ifeq (GCC,$(COM))
-boost_patches += boost_1_44_0-unused-parameters.patch
-endif
+boost_patches += boost.type_index.Wundef.warnings.patch
 
 boost_patches += boost.auto_link.patch
-boost_patches += boost.wunused.patch
 
 boost_patches += ubsan.patch.0
 boost_patches += rtti.patch.0
 
 # https://svn.boost.org/trac/boost/ticket/11505
 boost_patches += boost_1_59_0.mpl.config.wundef.patch
-# https://svn.boost.org/trac/boost/ticket/11502
-boost_patches += boost_1_59_0.property_tree.wtype-limits.patch
-# https://svn.boost.org/trac/boost/ticket/11507
-boost_patches += boost_1_59_0.property_tree.wshadow.patch
-# https://svn.boost.org/trac/boost/ticket/11508
-boost_patches += boost_1_59_0.iostreams.wshadow.patch
-# https://svn.boost.org/trac/boost/ticket/11509
-boost_patches += boost_1_59_0.iostreams.wunused.patch
-# https://svn.boost.org/trac/boost/ticket/11506
-boost_patches += boost_1_59_0.rational.wshadow.patch
-# https://svn.boost.org/trac/boost/ticket/11511
-boost_patches += boost_1_59_0.multi_array.wshadow.patch
 # https://svn.boost.org/trac/boost/ticket/11501
 boost_patches += boost_1_59_0.property_tree.wreturn-type.patch
 
 boost_patches += clang-cl.patch.0
-boost_patches += gcc6-warnings.patch.0
 
 # This patch was already applied upstream
 # https://github.com/boostorg/math/commit/74ff2db959c5fa75bec770c41ed2951a740fe936
 boost_patches += boost_1_60_0.quadmath.patch
 boost_patches += boost_1_60_0.undef.warning.patch
-boost_patches += boost.wdeprecated-auto_ptr.patch.0
 boost_patches += boost_1_60_0.android.undef.warning.patch
-
-# <https://github.com/boostorg/format/pull/11> "BOOST_FALLTHROUGH to silence
-# Clang -Wimplicit-fallthrough":
-boost_patches += fallthrough.patch.0
 
 $(eval $(call gb_UnpackedTarball_UnpackedTarball,boost))
 
