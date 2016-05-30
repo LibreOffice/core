1e3854bd7c88961f42215ff5d3db32a71dad51ad
Wastack <btomi96@gmail.com>
2016-05-30 18:20:19 +0200

    GSoC Update firebird to 3.0
    Update from Firebird 2.5 to Firebird 3.0.0.
    This certainly not works on Windows and MacOS yet.
    
    Change-Id: I3e708b957ab0f010124a41ee85ae45ce0d7e5918

---
+++ external/libtommath/UnpackedTarball_libtommath.mk
@@ -0,0 +1,22 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_UnpackedTarball_UnpackedTarball,libtommath))
+
+$(eval $(call gb_UnpackedTarball_set_tarball,libtommath,$(LIBTOMMATH_TARBALL)))
+
+$(eval $(call gb_UnpackedTarball_set_patchlevel,libtommath,0))
+
+ifeq ($(OS)-$(COM),WNT-MSC)
+$(eval $(call gb_UnpackedTarball_add_patches,libtommath,\
+	external/libtommath/libtommath-msvc.patch \
+))
+endif
+
+# vim: set noet sw=4 ts=4:
