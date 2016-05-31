538fa0209ab37a656a5ca37ad327c1346a5ea9c7
Szymon Kłos <eszkadev@gmail.com>
2016-05-31 16:57:13 +0200

    notebookbar: working paragraph spacing controls
    Change-Id: I9d2672cd156f2dcc2ee4c544902e9d42632cab70

---
+++ svx/source/sidebar/paragraph/ParaSpacingControl.cxx
@@ -0,0 +1,68 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#include "ParaSpacingWindow.hxx"
+
+#include <editeng/ulspitem.hxx>
+#include <svx/ParaSpacingControl.hxx>
+#include <vcl/toolbox.hxx>
+#include <svl/itempool.hxx>
+
+using namespace svx;
+
+SFX_IMPL_TOOLBOX_CONTROL(ParaULSpacingControl, SvxULSpaceItem);
+
+ParaULSpacingControl::ParaULSpacingControl(sal_uInt16 nSlotId, sal_uInt16 nId, ToolBox& rTbx)
+    : SfxToolBoxControl(nSlotId, nId, rTbx)
+{
+}
+
+ParaULSpacingControl::~ParaULSpacingControl()
+{
+}
+
+void ParaULSpacingControl::StateChanged(sal_uInt16 /*nSID*/, SfxItemState eState,
+                              const SfxPoolItem* /*pState*/)
+{
+    sal_uInt16 nId = GetId();
+    ToolBox& rTbx = GetToolBox();
+    ParaULSpacingWindow* pWindow = static_cast<ParaULSpacingWindow*>(rTbx.GetItemWindow(nId));
+
+    DBG_ASSERT( pWindow, "Control not found!" );
+
+    if(SfxItemState::DISABLED == eState)
+        pWindow->Disable();
+    else
+        pWindow->Enable();
+
+    rTbx.EnableItem(nId, SfxItemState::DISABLED != eState);
+
+    if(!pWindow->IsInitialized())
+        pWindow->Initialize();
+}
+
+VclPtr<vcl::Window> ParaULSpacingControl::CreateItemWindow(vcl::Window* pParent)
+{
+    VclPtr<ParaULSpacingWindow> pWindow = VclPtr<ParaULSpacingWindow>::Create(pParent, m_xFrame);
+    pWindow->Show();
+
+    return pWindow;
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
---
+++ svx/source/sidebar/paragraph/ParaSpacingWindow.hxx
@@ -0,0 +1,124 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+#ifndef INCLUDED_SVX_SOURCE_SIDEBAR_PARAGRAPH_PARASPACINGWINDOW_HXX
+#define INCLUDED_SVX_SOURCE_SIDEBAR_PARAGRAPH_PARASPACINGWINDOW_HXX
+
+#include <editeng/ulspitem.hxx>
+#include <editeng/lrspitem.hxx>
+#include <vcl/builder.hxx>
+#include <vcl/layout.hxx>
+#include <svx/relfld.hxx>
+#include <svtools/unitconv.hxx>
+#include <sfx2/sidebar/ControllerItem.hxx>
+#include <com/sun/star/ui/XContextChangeEventListener.hpp>
+#include <sfx2/sidebar/EnumContext.hxx>
+
+using namespace com::sun::star;
+
+namespace svx {
+
+class ParaULSpacingWindow : public VclVBox, public VclBuilderContainer,
+                            public ::sfx2::sidebar::ControllerItem::ItemUpdateReceiverInterface
+{
+public:
+    explicit ParaULSpacingWindow(vcl::Window* pParent, css::uno::Reference<css::frame::XFrame>& xFrame);
+    virtual ~ParaULSpacingWindow();
+    virtual void dispose() override;
+
+    void SetUnit(FieldUnit eUnit);
+    void SetValue(const SvxULSpaceItem* pItem);
+    void Initialize();
+    bool IsInitialized() const { return (m_pULSpaceControl != nullptr); }
+
+private:
+    VclPtr<SvxRelativeField> m_pAboveSpacing;
+    VclPtr<SvxRelativeField> m_pBelowSpacing;
+
+    SfxMapUnit m_eUnit;
+    SfxMapUnit GetCoreMetric() const;
+    Timer m_aLateInitTimer;
+
+    ::sfx2::sidebar::ControllerItem* m_pULSpaceControl;
+
+    virtual void NotifyItemUpdate(
+        const sal_uInt16 nSId,
+        const SfxItemState eState,
+        const SfxPoolItem* pState,
+        const bool bIsEnabled) override;
+
+    DECL_LINK_TYPED(ModifySpacingHdl, Edit&, void);
+};
+
+class ParaLRSpacingWindow : public VclVBox, public VclBuilderContainer,
+                            public ::sfx2::sidebar::ControllerItem::ItemUpdateReceiverInterface,
+                            public css::ui::XContextChangeEventListener
+{
+public:
+    explicit ParaLRSpacingWindow(vcl::Window* pParent);
+    virtual ~ParaLRSpacingWindow();
+    virtual void dispose() override;
+
+    // XContextChangeEventListener
+    virtual void SAL_CALL notifyContextChangeEvent(const css::ui::ContextChangeEventObject& rEvent)
+        throw (css::uno::RuntimeException, std::exception) override;
+
+    virtual ::css::uno::Any SAL_CALL queryInterface(const ::css::uno::Type&)
+        throw (::css::uno::RuntimeException, ::std::exception) override
+    {
+        return ::css::uno::Any();
+    }
+
+    virtual void SAL_CALL acquire() throw () override {}
+
+    virtual void SAL_CALL disposing(const ::css::lang::EventObject&)
+        throw (::css::uno::RuntimeException, ::std::exception) override {}
+
+    virtual void SAL_CALL release() throw () override {}
+
+
+
+private:
+    VclPtr<SvxRelativeField> m_pBeforeSpacing;
+    VclPtr<SvxRelativeField> m_pAfterSpacing;
+    VclPtr<SvxRelativeField> m_pFLSpacing;
+
+    SfxMapUnit m_eUnit;
+    SfxMapUnit GetCoreMetric() const;
+    Timer m_aLateInitTimer;
+
+    ::sfx2::sidebar::EnumContext maContext;
+    ::sfx2::sidebar::ControllerItem* m_pLRSpaceControl;
+
+    virtual void NotifyItemUpdate(
+        const sal_uInt16 nSId,
+        const SfxItemState eState,
+        const SfxPoolItem* pState,
+        const bool bIsEnabled) override;
+    void SetUnit(FieldUnit eUnit);
+    void SetValue(SfxItemState eState, const SfxPoolItem* pState);
+
+    DECL_LINK_TYPED(ModifySpacingHdl, Edit&, void);
+    DECL_LINK_TYPED(LateInitCallback, Timer *, void);
+};
+
+}
+
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
