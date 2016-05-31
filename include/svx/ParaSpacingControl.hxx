dfb9ebf83157f52822332b361c61b325685ffb07
Szymon Kłos <eszkadev@gmail.com>
2016-05-31 16:57:13 +0200

    [API CHANGE] notebookbar: paragraph spacing controls
    Change-Id: I9d2672cd156f2dcc2ee4c544902e9d42632cab70

---
+++ include/svx/ParaSpacingControl.hxx
@@ -0,0 +1,84 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+#ifndef INCLUDED_SVX_SOURCE_SIDEBAR_PARAGRAPH_PARASPACINGCONTROL_HXX
+#define INCLUDED_SVX_SOURCE_SIDEBAR_PARAGRAPH_PARASPACINGCONTROL_HXX
+
+#include <sfx2/tbxctrl.hxx>
+#include <svx/svxdllapi.h>
+#include <vcl/window.hxx>
+#include <com/sun/star/ui/XContextChangeEventListener.hpp>
+#include <com/sun/star/ui/ContextChangeEventMultiplexer.hpp>
+#include <com/sun/star/ui/XContextChangeEventMultiplexer.hpp>
+#include <sfx2/sidebar/EnumContext.hxx>
+
+namespace svx {
+
+class SVX_DLLPUBLIC ParaULSpacingControl : public SfxToolBoxControl
+{
+public:
+    SFX_DECL_TOOLBOX_CONTROL();
+
+    ParaULSpacingControl(sal_uInt16 nSlotId, sal_uInt16 nId, ToolBox& rTbx);
+    virtual ~ParaULSpacingControl();
+
+    virtual void StateChanged(sal_uInt16 nSID, SfxItemState eState,
+                              const SfxPoolItem* pState) override;
+    virtual VclPtr<vcl::Window> CreateItemWindow(vcl::Window* pParent) override;
+};
+
+class SVX_DLLPUBLIC ParaLRSpacingControl : public SfxToolBoxControl,
+                                    public css::ui::XContextChangeEventListener
+{
+public:
+    SFX_DECL_TOOLBOX_CONTROL();
+
+    ParaLRSpacingControl(sal_uInt16 nSlotId, sal_uInt16 nId, ToolBox& rTbx);
+    virtual ~ParaLRSpacingControl();
+
+    virtual void SAL_CALL dispose()
+        throw (css::uno::RuntimeException, std::exception) override;
+
+    virtual void StateChanged(sal_uInt16 nSID, SfxItemState eState,
+                              const SfxPoolItem* pState) override;
+    virtual VclPtr<vcl::Window> CreateItemWindow(vcl::Window* pParent) override;
+
+    // XContextChangeEventListener
+    virtual void SAL_CALL notifyContextChangeEvent(const css::ui::ContextChangeEventObject& rEvent)
+        throw (css::uno::RuntimeException, std::exception) override;
+
+    virtual ::css::uno::Any SAL_CALL queryInterface(const ::css::uno::Type& aType)
+        throw (::css::uno::RuntimeException, ::std::exception) override;
+
+    virtual void SAL_CALL acquire() throw () override;
+
+    virtual void SAL_CALL disposing(const ::css::lang::EventObject&)
+        throw (::css::uno::RuntimeException, ::std::exception) override;
+
+    virtual void SAL_CALL release() throw () override;
+
+private:
+    css::uno::Reference<css::ui::XContextChangeEventMultiplexer> m_xMultiplexer;
+};
+
+}
+
+#endif
+
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
