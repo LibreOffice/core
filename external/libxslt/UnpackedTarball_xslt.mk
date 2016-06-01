2ae410617c9cb2a196b9d09b8ac51e8b75a71c64
Michael Stahl <mstahl@redhat.com>
2016-06-01 14:02:49 +0200

    libxslt: upgrade to release 1.1.29
    * drop libxslt-freebsd.patch.1
      fixed in new upstream libtool
    * drop libxslt-config-guess.patch.0
      fixed in new upstream autoconf
    * drop libxslt-android.patch
      fixed in new upstream autoconf
    * drop libxslt-configure.patch.1
      fixed upstream
    * drop libxslt-vc15.patch
      fixed upstream
    * drop second hunk of libxslt-vc10.patch
      fixed upstream
    * drop 0001-Fix-for-type-confusion-in-preprocessing-attributes.patch.1
      fixed upstream
    
    Change-Id: I7427725ed6c82da53de12c9e1676e6ce02fd6483
    Reviewed-on: https://gerrit.libreoffice.org/25775
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Michael Stahl <mstahl@redhat.com>
    (cherry picked from commit 35db469326c0608dba7637d892af0004d13f8557)
    Reviewed-on: https://gerrit.libreoffice.org/25987
    Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>

--- external/libxslt/UnpackedTarball_xslt.mk
+++ external/libxslt/UnpackedTarball_xslt.mk
@@ -13,16 +13,10 @@ $(eval $(call gb_UnpackedTarball_set_tarball,xslt,$(LIBXSLT_TARBALL)))
 
 $(eval $(call gb_UnpackedTarball_add_patches,xslt,\
 	external/libxslt/libxslt-config.patch.1 \
-	external/libxslt/libxslt-configure.patch.1 \
-	external/libxslt/libxslt-freebsd.patch.1 \
 	external/libxslt/libxslt-internal-symbols.patch \
-	external/libxslt/libxslt-vc10.patch \
+	external/libxslt/libxslt-msvc.patch.2 \
 	external/libxslt/libxslt-1.1.26-memdump.patch \
-	$(if $(filter ANDROID,$(OS)),external/libxslt/libxslt-android.patch) \
-	external/libxslt/libxslt-config-guess.patch.0 \
 	external/libxslt/rpath.patch.0 \
-	external/libxslt/libxslt-vc15.patch \
-	external/libxslt/0001-Fix-for-type-confusion-in-preprocessing-attributes.patch.1 \
 ))
 
 # vim: set noet sw=4 ts=4:
