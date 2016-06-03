dc8db9f1a7b9c3f9a3e4bacf966446dcaac96177
Markus Mohrhard <markus.mohrhard@googlemail.com>
2016-06-03 12:37:18 +0200

    integrate the minidump upload into the normal LibreOffice
    Change-Id: Id1808ceebf6c54698f7d1656a0ce96bcaece89ac
    Reviewed-on: https://gerrit.libreoffice.org/25863
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Markus Mohrhard <markus.mohrhard@googlemail.com>

--- desktop/source/minidump/minidump_upload.cxx
+++ desktop/source/minidump/minidump_upload.cxx
@@ -7,10 +7,9 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+#include <desktop/minidump.hxx>
 
-#include "minidump.hxx"
-
-#include<iostream>
+#include <iostream>
 
 int main(int argc, char** argv)
 {
