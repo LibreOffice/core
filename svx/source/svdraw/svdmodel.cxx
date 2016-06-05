fd393faa68d5979d319900438a789d40630a56ec
Xisco Fauli <anistenis@gmail.com>
2016-06-06 00:50:58 +0200

    tdf#89329: use unique_ptr for pImpl in svdmodel
    Change-Id: Ied5f57b2cf4004de70d0aec8e1b6b69603428a69
    Reviewed-on: https://gerrit.libreoffice.org/25940
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Noel Grandin <noelgrandin@gmail.com>

--- svx/source/svdraw/svdmodel.cxx
+++ svx/source/svdraw/svdmodel.cxx
@@ -107,7 +107,7 @@ struct SdrModelImpl
 void SdrModel::ImpCtor(SfxItemPool* pPool, ::comphelper::IEmbeddedHelper* _pEmbeddedHelper,
     bool bUseExtColorTable)
 {
-    mpImpl = new SdrModelImpl;
+    mpImpl.reset(new SdrModelImpl);
     mpImpl->mpUndoManager=nullptr;
     mpImpl->mpUndoFactory=nullptr;
     mbInDestruction = false;
@@ -316,7 +316,6 @@ SdrModel::~SdrModel()
     delete mpNumberFormatter;
 
     delete mpImpl->mpUndoFactory;
-    delete mpImpl;
 }
 
 void SdrModel::SetSwapGraphics()
