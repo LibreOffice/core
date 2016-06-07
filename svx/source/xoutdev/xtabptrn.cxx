218650d8008c14bec7a247e8012762ab580f9b6d
Rishabh Kumar <kris.kr296@gmail.com>
2016-06-07 16:58:20 +0530

    Create XPatternList class for Patterns
    Add XPatternList class to add pattern presets.
    Internally bitmaps and patterns are treated the same.
    
    Change-Id: I5cf02f1889090539f7adb826483c91a1ef3c3925

---
+++ svx/source/xoutdev/xtabptrn.cxx
@@ -0,0 +1,59 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#include "svx/XPropertyTable.hxx"
+
+#include <vcl/virdev.hxx>
+#include <svl/itemset.hxx>
+#include <sfx2/docfile.hxx>
+#include <svx/dialogs.hrc>
+#include <svx/dialmgr.hxx>
+#include <svx/xtable.hxx>
+#include <svx/xpool.hxx>
+#include <svx/xbtmpit.hxx>
+
+using namespace com::sun::star;
+
+XBitmapEntry* XPatternList::Remove(long nIndex)
+{
+    return static_cast<XBitmapEntry*>( XPropertyList::Remove(nIndex) );
+}
+
+XBitmapEntry* XPatternList::GetBitmap(long nIndex) const
+{
+    return static_cast<XBitmapEntry*>( XPropertyList::Get(nIndex) );
+}
+
+uno::Reference< container::XNameContainer > XPatternList::createInstance()
+{
+    return uno::Reference< container::XNameContainer >(
+        SvxUnoXBitmapTable_createInstance( this ), uno::UNO_QUERY );
+}
+
+bool XPatternList::Create()
+{
+    return true;
+}
+
+Bitmap XPatternList::CreateBitmapForUI( long /*nIndex*/ )
+{
+    return Bitmap();
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
