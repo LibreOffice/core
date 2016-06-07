3fd25d2fd8dd29a2c775dee2781591074f1f8f34
Jakub Trzebiatowski <ubap.dev@gmail.com>
2016-06-07 12:40:39 +0200

    fdo47267 odf import unit test
    Change-Id: I0f745483245b0e75b250ae1c2752f2e157307f0a
    Reviewed-on: https://gerrit.libreoffice.org/26012
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: jan iversen <jani@documentfoundation.org>

--- sw/qa/extras/odfimport/odfimport.cxx
+++ sw/qa/extras/odfimport/odfimport.cxx
@@ -637,5 +637,10 @@ DECLARE_ODFIMPORT_TEST(testTdf96113, "tdf96113.odt")
     CPPUNIT_ASSERT_EQUAL(sal_Int32(0x00ff00), getProperty<sal_Int32>(getShape(1), "BackColor"));
 }
 
+DECLARE_ODFIMPORT_TEST(testFdo47267, "fdo47267-3.odt")
+{
+    // This was a Style Families getByName() crash
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
