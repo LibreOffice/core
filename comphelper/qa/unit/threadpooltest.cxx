96dd291180fbf7dc890054ba61c33271aff8188a
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2016-06-14 07:19:20 -0400

    tdf#98955 hardware_concurrency not ideal for thread pools
    A new static member getPreferredConcurrency added to
    comphelper::ThreadPool to return a configurable max
    number of threads.
    
    By default the new function returns the hardware_concurrency
    value provided by std::thread. When MAX_CONCURRENCY envar is
    defined, the return value is limited to whatever is set there.
    
    Three call-sites that used std::thread::hardware_concurrency
    have been replaced with getPreferredConcurrency.
    
    Unittests added to cover the functionality of the new member.
    
    Unittests are capped to 4 threads.
    
    Reviewed-on: https://gerrit.libreoffice.org/26254
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Ashod Nakashian <ashnakash@gmail.com>
    (cherry picked from commit 60e75fb276778459f6055360646d879b8c615d83)
    
    Change-Id: I3332e393a88a5ed436316fa712ed920a4b37f4af

---
+++ comphelper/qa/unit/threadpooltest.cxx
@@ -0,0 +1,55 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <comphelper/threadpool.hxx>
+#include "cppunit/TestAssert.h"
+#include "cppunit/TestFixture.h"
+#include "cppunit/extensions/HelperMacros.h"
+#include "cppunit/plugin/TestPlugIn.h"
+
+#include <stdlib.h>
+#include <thread>
+
+class ThreadPoolTest : public CppUnit::TestFixture
+{
+public:
+    void testPreferredConcurrency();
+
+    CPPUNIT_TEST_SUITE(ThreadPoolTest);
+    CPPUNIT_TEST(testPreferredConcurrency);
+    CPPUNIT_TEST_SUITE_END();
+};
+
+void ThreadPoolTest::testPreferredConcurrency() {
+
+    // Check default.
+    auto nThreads = comphelper::ThreadPool::getPreferredConcurrency();
+    sal_Int32 nExpected = 4; // UTs are capped to 4.
+    CPPUNIT_ASSERT_MESSAGE("Expected no more than 4 threads", nExpected >= nThreads);
+
+#ifndef _WIN32_WINNT
+    // The result should be cached, so this should change anything.
+    nThreads = std::thread::hardware_concurrency() * 2;
+    setenv("MAX_CONCURRENCY", std::to_string(nThreads).c_str(), true);
+    nThreads = comphelper::ThreadPool::getPreferredConcurrency();
+    CPPUNIT_ASSERT_MESSAGE("Expected no more than hardware threads",
+                           nThreads <= (sal_Int32)std::thread::hardware_concurrency());
+
+    // Revert and check. Again, nothing should change.
+    unsetenv("MAX_CONCURRENCY");
+    nThreads = comphelper::ThreadPool::getPreferredConcurrency();
+    CPPUNIT_ASSERT_MESSAGE("Expected no more than 4 threads", nExpected >= nThreads);
+#endif
+}
+
+CPPUNIT_TEST_SUITE_REGISTRATION(ThreadPoolTest);
+
+CPPUNIT_PLUGIN_IMPLEMENT();
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
