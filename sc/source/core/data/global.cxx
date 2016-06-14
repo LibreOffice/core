19822c8a2d35570593bf2f39f16cbc40a30b09db
Miklos Vajna <vmiklos@collabora.co.uk>
2016-06-14 15:10:02 +0200

    sc: implement per-view LOK_CALLBACK_HYPERLINK_CLICKED
    The last place that crashed when clicking on hyperlinks.
    
    Change-Id: Ia114d98ada6cf8a3be56a25e48f3e84471c37e66
    Reviewed-on: https://gerrit.libreoffice.org/26265
    Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
    Tested-by: Jenkins <ci@libreoffice.org>

--- sc/source/core/data/global.cxx
+++ sc/source/core/data/global.cxx
@@ -880,10 +880,21 @@ bool ScGlobal::EETextObjEqual( const EditTextObject* pObj1,
 
 void ScGlobal::OpenURL(const OUString& rURL, const OUString& rTarget, const SdrModel* pDrawLayer)
 {
-    if (pDrawLayer && comphelper::LibreOfficeKit::isActive())
+    if (comphelper::LibreOfficeKit::isActive())
     {
-        pDrawLayer->libreOfficeKitCallback(LOK_CALLBACK_HYPERLINK_CLICKED, rURL.toUtf8().getStr());
-        return;
+        if (comphelper::LibreOfficeKit::isViewCallback())
+        {
+            if(SfxViewShell* pViewShell = SfxViewShell::Current())
+                pViewShell->libreOfficeKitViewCallback(LOK_CALLBACK_HYPERLINK_CLICKED, rURL.toUtf8().getStr());
+        }
+        else
+        {
+            if (pDrawLayer)
+            {
+                pDrawLayer->libreOfficeKitCallback(LOK_CALLBACK_HYPERLINK_CLICKED, rURL.toUtf8().getStr());
+                return;
+            }
+        }
     }
 
     // OpenURL is always called in the GridWindow by mouse clicks in some way or another.
