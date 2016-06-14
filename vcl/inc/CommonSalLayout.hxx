63a673e8cb0936a047c69ef49b0f5fa8e3cafa3f
Akash Jain <akash96j@gmail.com>
2016-06-14 14:38:12 +0530

    GSoC: Add new CommonSalLayout class
    Change-Id: Ic11e573da2f5fd6ef931f53ab674f8894815c3b4

---
+++ vcl/inc/CommonSalLayout.hxx
@@ -0,0 +1,68 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#ifdef _WIN32
+#include "win/winlayout.hxx"
+
+#elif defined(MACOSX) || defined(IOS)
+#include <com/sun/star/i18n/XBreakIterator.hpp>
+#include "quartz/ctfonts.hxx"
+
+#else
+#include "unx/freetype_glyphcache.hxx"
+#endif
+
+#include <sallayout.hxx>
+#include <hb-icu.h>
+#include <hb-ot.h>
+
+class CommonSalLayout : public GenericSalLayout
+{
+    hb_face_t* mpHbFace;
+    FontSelectPattern maFontSelData;
+    css::uno::Reference<css::i18n::XBreakIterator> mxBreak;
+#ifdef _WIN32
+    HDC   mhDC;
+    HFONT mhFont;
+#elif defined(MACOSX) || defined(IOS)
+    const CoreTextStyle& mrCoreTextStyle;
+#else
+    ServerFont& mrServerFont;
+#endif
+
+    hb_font_t*              GetHbFont();
+public:
+#if defined(_WIN32)
+    explicit                CommonSalLayout(HDC, WinFontInstance&);
+    virtual void            InitFont() const override;
+#elif defined(MACOSX) || defined(IOS)
+    explicit                CommonSalLayout(const CoreTextStyle&);
+    const CoreTextStyle&    getFontData() const { return mrCoreTextStyle; };
+#else
+    explicit                CommonSalLayout(ServerFont&);
+    const ServerFont&       getFontData() const { return mrServerFont; };
+#endif
+
+    virtual                 ~CommonSalLayout();
+    void                    SetNeedFallback(ImplLayoutArgs&, sal_Int32, bool);
+    void                    AdjustLayout(ImplLayoutArgs&) override;
+    virtual bool            LayoutText(ImplLayoutArgs&) override;
+    virtual void            DrawText( SalGraphics& ) const override;
+    std::shared_ptr<vcl::TextLayoutCache> CreateTextLayoutCache(OUString const&) const override;
+};
