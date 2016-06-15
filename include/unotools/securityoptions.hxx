3c31796bedc77dc55ee645987da0922c17162454
Xisco Fauli <anistenis@gmail.com>
2016-06-15 19:42:05 +0200

    tdf#89329: use shared_ptr for pImpl in securityoptions
    Change-Id: I91c9c9dcbb9df644836c87d4789521d668c2ba49
    Reviewed-on: https://gerrit.libreoffice.org/26319
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Noel Grandin <noelgrandin@gmail.com>

--- include/unotools/securityoptions.hxx
+++ include/unotools/securityoptions.hxx
@@ -25,6 +25,7 @@
 #include <com/sun/star/uno/Sequence.h>
 #include <rtl/ustring.hxx>
 #include <unotools/options.hxx>
+#include <memory>
 
 /*-************************************************************************************************************
     @short          forward declaration to our private date container implementation
@@ -98,17 +99,6 @@ class SAL_WARN_UNUSED UNOTOOLS_DLLPUBLIC SvtSecurityOptions : public utl::detail
         */
 
     public:
-        /*-****************************************************************************************************
-            @short      standard constructor and destructor
-            @descr      This will initialize an instance with default values.
-                        We implement these class with a refcount mechanism! Every instance of this class increase it
-                        at create and decrease it at delete time - but all instances use the same data container!
-                        He is implemented as a static member ...
-
-            @seealso    member m_nRefCount
-            @seealso    member m_pDataContainer
-        *//*-*****************************************************************************************************/
-
          SvtSecurityOptions();
         virtual ~SvtSecurityOptions();
 
@@ -223,17 +213,7 @@ class SAL_WARN_UNUSED UNOTOOLS_DLLPUBLIC SvtSecurityOptions : public utl::detail
     //  private member
 
     private:
-
-        /*Attention
-
-            Don't initialize these static members in these headers!
-            a) Double defined symbols will be detected ...
-            b) and unresolved externals exist at linking time.
-            Do it in your source only.
-         */
-
-        static SvtSecurityOptions_Impl* m_pDataContainer;
-        static sal_Int32                m_nRefCount;
+        std::shared_ptr<SvtSecurityOptions_Impl> m_pImpl;
 
 };      // class SvtSecurityOptions
 
