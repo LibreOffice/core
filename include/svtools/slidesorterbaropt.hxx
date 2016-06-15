bb6500aa74feabcf884801d7ca9ce70b5449cafb
Xisco Fauli <anistenis@gmail.com>
2016-06-15 20:46:16 +0200

    tdf#89329: use shared_ptr for pImpl in slidesorterbaropt
    Change-Id: If5b7fd23448a584d1faebaf0a540e61f091471fa
    Reviewed-on: https://gerrit.libreoffice.org/26331
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Noel Grandin <noelgrandin@gmail.com>

--- include/svtools/slidesorterbaropt.hxx
+++ include/svtools/slidesorterbaropt.hxx
@@ -25,6 +25,7 @@
 #include <osl/mutex.hxx>
 #include <rtl/ustring.hxx>
 #include <unotools/options.hxx>
+#include <memory>
 
 /** forward declaration to our private date container implementation
 
@@ -41,16 +42,6 @@ class SvtSlideSorterBarOptions_Impl;
 class SVT_DLLPUBLIC SvtSlideSorterBarOptions: public utl::detail::Options
 {
     public:
-        /** standard constructor and destructor
-
-            This will initialize an instance with default values.
-            We implement these class with a refcount mechanism! Every instance of this class increase it
-            at create and decrease it at delete time - but all instances use the same data container!
-            He is implemented as a static member ...
-
-            \sa    member m_nRefCount
-            \sa    member m_pDataContainer
-        */
         SvtSlideSorterBarOptions();
         virtual ~SvtSlideSorterBarOptions();
 
@@ -79,17 +70,7 @@ class SVT_DLLPUBLIC SvtSlideSorterBarOptions: public utl::detail::Options
         SVT_DLLPRIVATE static ::osl::Mutex& GetInitMutex();
 
     private:
-
-        /**
-            \attention
-            Don't initialize these static members in these headers!
-            \li Double defined symbols will be detected ...
-            \li and unresolved externals exist at linking time.
-            Do it in your source only.
-        */
-        static SvtSlideSorterBarOptions_Impl* m_pDataContainer    ;
-        static sal_Int32                      m_nRefCount         ;
-
+        std::shared_ptr<SvtSlideSorterBarOptions_Impl> m_pImpl;
 };
 
 #endif
