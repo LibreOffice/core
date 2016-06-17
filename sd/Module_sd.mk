fcd3564077d4cdcbc33ea1fcb6e1567371d10201
Katarina Behrens <Katarina.Behrens@cib.de>
2016-06-17 16:10:26 +0200

    Let's not run this w/ the rest of the tests for now
    Change-Id: Ia5fc46f5030285049acf4836ba4cbd6285cbe5ae

--- sd/Module_sd.mk
+++ sd/Module_sd.mk
@@ -32,7 +32,6 @@ $(eval $(call gb_Module_add_check_targets,sd,\
     CppunitTest_sd_import_tests \
     CppunitTest_sd_export_tests \
     CppunitTest_sd_filters_test \
-    CppunitTest_sd_dialogs_test \
     CppunitTest_sd_misc_tests \
     CppunitTest_sd_html_export_tests \
 ))
