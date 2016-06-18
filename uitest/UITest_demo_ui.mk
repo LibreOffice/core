4d0d3e71cce879c7a882ca3eb2bc4a713c945c7c
Markus Mohrhard <markus.mohrhard@googlemail.com>
2016-06-18 19:37:00 +0200

    uitest: add the demo ui uitest
    That test just shows features of the test and the use of different UI
    objects.
    
    Change-Id: I49e9fa2d42634951ae5a78aa3a9bf79fefacd298

--- uitest/UITest_demo_ui.mk
+++ uitest/UITest_demo_ui.mk
@@ -1,6 +1,5 @@
 # -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
 #
-#
 # This file is part of the LibreOffice project.
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
@@ -8,11 +7,10 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 
-$(eval $(call gb_Module_Module,uitest))
+$(eval $(call gb_UITest_UITest,demo_ui))
 
-$(eval $(call gb_Module_add_uicheck_targets,uitest,\
-    UITest_calc_demo \
-    UITest_writer_demo \
-    UITest_impress_demo \
-    UITest_demo_ui \
+$(eval $(call gb_UITest_add_modules,demo_ui,$(SRCDIR)/uitest,\
+	demo_ui/demo_ui.txt \
 ))
+
+# vim: set noet sw=4 ts=4:
