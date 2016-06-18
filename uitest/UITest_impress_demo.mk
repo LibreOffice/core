e672eb7a08aa839481d13bf4aadaa7456824e0c9
Markus Mohrhard <markus.mohrhard@googlemail.com>
2016-06-18 19:33:01 +0200

    uitest: also add the impress uitest demo to the makefiles
    Change-Id: Id5d5c53b5d51b7c8984c93e8c89b0a71b8c660bd

--- uitest/UITest_impress_demo.mk
+++ uitest/UITest_impress_demo.mk
@@ -1,6 +1,5 @@
 # -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
 #
-#
 # This file is part of the LibreOffice project.
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
@@ -8,10 +7,9 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 
-$(eval $(call gb_Module_Module,uitest))
+$(eval $(call gb_UITest_UITest,impress_demo))
 
-$(eval $(call gb_Module_add_uicheck_targets,uitest,\
-    UITest_calc_demo \
-    UITest_writer_demo \
-    UITest_impress_demo \
+$(eval $(call gb_UITest_add_modules,impress_demo,$(SRCDIR)/uitest,\
+	impress_tests/impress_demo.txt \
 ))
+# vim: set noet sw=4 ts=4:
