9ec875bf691ad3c06723d6a9846bdb34bebedb29
Rishabh Kumar <kris.kr296@gmail.com>
2016-06-22 04:43:18 +0530

    [GSoC] Add new Bitmap tab to Area dialog
    Change-Id: I0eb59e5082da1efa52dcb3b85f6e4129aad3e676

--- cui/source/inc/cuitabarea.hxx
+++ cui/source/inc/cuitabarea.hxx
@@ -44,7 +44,7 @@ class SvxAreaTabDialog : public SfxTabDialog
     sal_uInt16            m_nColorTabPage;
     sal_uInt16            m_nGradientTabPage;
     sal_uInt16            m_nHatchTabPage;
-    //sal_uInt16            m_nBitmapTabPage;
+    sal_uInt16            m_nBitmapTabPage;
     sal_uInt16            m_nPatternTabPage;
 
 private:
@@ -549,6 +549,93 @@ public:
     void    SetColorChgd( ChangeType* pIn ) { m_pnColorListState = pIn; }
 
     virtual void        DataChanged( const DataChangedEvent& rDCEvt ) override;
+};
+
+/************************************************************************/
+
+class SvxBitmapTabPage : public SvxTabPage
+{
+    using TabPage::ActivatePage;
+    using TabPage::DeactivatePage;
+private:
+
+    VclPtr<VclBox>             m_pFillLB;
+    VclPtr<BitmapLB>           m_pLbBitmap;
+    VclPtr<SvxXRectPreview>    m_pCtlBitmapPreview;
+
+    VclPtr<VclBox>             m_pBxBitmap;
+
+    VclPtr<VclFrame>           m_pFlSize;
+    VclPtr<TriStateBox>        m_pTsbOriginal;
+    VclPtr<TriStateBox>        m_pTsbScale;
+    VclPtr<VclGrid>            m_pGridX_Y;
+    VclPtr<FixedText>          m_pFtXSize;
+    VclPtr<MetricField>        m_pMtrFldXSize;
+    VclPtr<FixedText>          m_pFtYSize;
+    VclPtr<MetricField>        m_pMtrFldYSize;
+
+    VclPtr<VclFrame>           m_pFlPosition;
+    VclPtr<SvxRectCtl>         m_pCtlPosition;
+    VclPtr<VclGrid>            m_pGridOffset;
+    VclPtr<MetricField>        m_pMtrFldXOffset;
+    VclPtr<MetricField>        m_pMtrFldYOffset;
+    VclPtr<VclBox>             m_pBxTile;
+    VclPtr<TriStateBox>        m_pTsbTile;
+    VclPtr<TriStateBox>        m_pTsbStretch;
+    VclPtr<VclFrame>           m_pFlOffset;
+    VclPtr<RadioButton>        m_pRbtRow;
+    VclPtr<RadioButton>        m_pRbtColumn;
+    VclPtr<MetricField>        m_pMtrFldOffset;
+
+    const SfxItemSet&   m_rOutAttrs;
+
+    XBitmapListRef        m_pBitmapList;
+    ChangeType*         m_pnBitmapListState;
+    XFillStyleItem      m_aXFStyleItem;
+    XFillBitmapItem     m_aXBitmapItem;
+
+    sal_uInt16 m_nPageType;
+    sal_uInt16 m_nDlgType;
+    sal_Int32  m_nPos;
+
+    bool*               m_pbAreaTP;
+
+    XFillAttrSetItem    m_aXFillAttr;
+    SfxItemSet&         m_rXFSet;
+
+    SfxMapUnit          m_ePoolUnit;
+    FieldUnit           m_eFUnit;
+
+    DECL_LINK_TYPED(SelectDialogTypeHdl_Impl, ListBox&, void);
+    DECL_LINK_TYPED( ModifyBitmapHdl_Impl, ListBox&, void );
+
+    DECL_LINK_TYPED( ModifyTileHdl_Impl, Edit&, void );
+    DECL_LINK_TYPED( ModifyTileClickHdl_Impl, Button*, void );
+    DECL_LINK_TYPED( ClickScaleHdl_Impl, Button*, void );
+    void ClickBitmapHdl_Impl();
+
+public:
+    SvxBitmapTabPage( vcl::Window* pParent, const SfxItemSet& rInAttrs );
+    virtual ~SvxBitmapTabPage();
+    virtual void dispose() override;
+
+    void    Construct();
+
+    static VclPtr<SfxTabPage> Create( vcl::Window*, const SfxItemSet* );
+
+    virtual bool FillItemSet( SfxItemSet* ) override;
+    virtual void Reset( const SfxItemSet * ) override;
+    virtual void ActivatePage( const SfxItemSet& rSet ) override;
+    virtual DeactivateRC DeactivatePage( SfxItemSet* pSet ) override;
+    virtual void PointChanged( vcl::Window* pWindow, RECT_POINT eRP ) override;
+
+    void    SetBitmapList( XBitmapListRef pBmpLst) { m_pBitmapList = pBmpLst; }
+
+    void    SetPageType( sal_uInt16 nInType ) { m_nPageType = nInType; }
+    void    SetDlgType( sal_uInt16 nInType ) { m_nDlgType = nInType; }
+    void    SetPos( sal_uInt16 nInPos ) { m_nPos = nInPos; }
+    void    SetAreaTP( bool* pIn ) { m_pbAreaTP = pIn; }
+    void    SetBmpChgd( ChangeType* pIn ) { m_pnBitmapListState = pIn; }
 };
 
 /************************************************************************/
