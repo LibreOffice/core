LibreOffice/core

commit 9f66db9c474f71f43d7a3667230241fd4fa4183f

sw lok: add LOK_CALLBACK_TEXT_VIEW_SELECTION
So a view can be aware where selections of other views are.

Change-Id: I5026b1ff2b99a4eedfd0bde32a05ceb8e2f424bc
Reviewed-on: https://gerrit.libreoffice.org/26542
Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
Tested-by: Jenkins <ci@libreoffice.org>

libreofficekit/source/gtk/lokdocview.cxx
@@ -346,6 +346,8 @@ callbackTypeToString (int nType)
         return "LOK_CALLBACK_CONTEXT_MENU";
     case LOK_CALLBACK_INVALIDATE_VIEW_CURSOR:
         return "LOK_CALLBACK_INVALIDATE_VIEW_CURSOR";
+    case LOK_CALLBACK_TEXT_VIEW_SELECTION:
+        return "LOK_CALLBACK_TEXT_VIEW_SELECTION";
     }
     return nullptr;
 }
@@ -1173,6 +1175,11 @@ callback (gpointer pData)
         const std::string& rRectangle = aTree.get<std::string>("rectangle");
         priv->m_aViewCursors[nViewId] = payloadToRectangle(pDocView, rRectangle.c_str());
         gtk_widget_queue_draw(GTK_WIDGET(pDocView));
+        break;
+    }
+    case LOK_CALLBACK_TEXT_VIEW_SELECTION:
+    {
+        // TODO: Implement me
         break;
     }
     default:

sw/qa/extras/tiledrendering/tiledrendering.cxx
@@ -551,7 +551,7 @@ public:
 
     ViewCallback()
         : m_bOwnCursorInvalidated(false),
-        m_bViewCursorInvalidated(false)
+          m_bViewCursorInvalidated(false)
     {
     }
 

include/LibreOfficeKit/LibreOfficeKitEnums.h
@@ -316,7 +316,6 @@ typedef enum
      * The size and/or the position of the view cursor changed. A view cursor
      * is a cursor of an other view, the current view can't change it.
      *
-     * Rectangle format is the same as LOK_CALLBACK_INVALIDATE_TILES.
      * The payload format:
      *
      * {
@@ -328,6 +327,21 @@ typedef enum
      * - rectangle uses the format of LOK_CALLBACK_INVALIDATE_VISIBLE_CURSOR
      */
     LOK_CALLBACK_INVALIDATE_VIEW_CURSOR,
+
+    /**
+     * The the text selection in one of the other views has changed.
+     *
+     * The payload format:
+     *
+     * {
+     *     "viewId": "..."
+     *     "selection": "..."
+     * }
+     *
+     * - viewId is a value returned earlier by lok::Document::createView()
+     * - selection uses the format of LOK_CALLBACK_TEXT_SELECTION.
+     */
+    LOK_CALLBACK_TEXT_VIEW_SELECTION,
 
 }
 LibreOfficeKitCallbackType;

include/sfx2/lokhelper.hxx
@@ -13,6 +13,7 @@
 #include <sfx2/dllapi.h>
 #include <cstddef>
 #include <cstdint>
+#include <rtl/string.hxx>
 
 class SfxViewShell;
 
@@ -29,6 +30,9 @@ public:
     static std::uintptr_t getView(SfxViewShell* pViewShell = nullptr);
     /// Get the number of views of the current object shell.
     static std::size_t getViews();
+
+    /// Invoke the LOK callback of all views except pThisView, with a payload of rKey-rPayload.
+    static void notifyOtherViews(SfxViewShell* pThisView, int nType, const OString& rKey, const OString& rPayload);
 };
 
 #endif

sfx2/source/view/lokhelper.cxx
@@ -8,6 +8,9 @@
  */
 
 #include <sfx2/lokhelper.hxx>
+
+#include <boost/property_tree/json_parser.hpp>
+
 #include <sfx2/viewsh.hxx>
 #include <sfx2/request.hxx>
 #include <sfx2/viewfrm.hxx>
@@ -84,6 +87,28 @@ std::size_t SfxLokHelper::getViews()
     }
 
     return nRet;
+}
+
+void SfxLokHelper::notifyOtherViews(SfxViewShell* pThisView, int nType, const OString& rKey, const OString& rPayload)
+{
+    if (SfxLokHelper::getViews() <= 1)
+        return;
+
+    SfxViewShell* pViewShell = SfxViewShell::GetFirst();
+    while (pViewShell)
+    {
+        if (pViewShell != pThisView)
+        {
+            boost::property_tree::ptree aTree;
+            aTree.put("viewId", SfxLokHelper::getView(pThisView));
+            aTree.put(rKey.getStr(), rPayload.getStr());
+            std::stringstream aStream;
+            boost::property_tree::write_json(aStream, aTree);
+            OString aPayload = aStream.str().c_str();
+            pViewShell->libreOfficeKitViewCallback(nType, aPayload.getStr());
+        }
+        pViewShell = SfxViewShell::GetNext(*pViewShell);
+    }
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sw/source/core/crsr/viscrs.cxx
@@ -198,26 +198,7 @@ void SwVisibleCursor::SetPosAndShow()
         Rectangle aSVRect(aRect.Pos().getX(), aRect.Pos().getY(), aRect.Pos().getX() + aRect.SSize().Width(), aRect.Pos().getY() + aRect.SSize().Height());
         OString sRect = aSVRect.toString();
         m_pCursorShell->GetSfxViewShell()->libreOfficeKitViewCallback(LOK_CALLBACK_INVALIDATE_VISIBLE_CURSOR, sRect.getStr());
-
-        if (SfxLokHelper::getViews() > 1)
-        {
-            // Notify other views about the invalidated cursor.
-            SfxViewShell* pViewShell = SfxViewShell::GetFirst();
-            while (pViewShell)
-            {
-                if (pViewShell != m_pCursorShell->GetSfxViewShell())
-                {
-                    boost::property_tree::ptree aTree;
-                    aTree.put("viewId", SfxLokHelper::getView(m_pCursorShell->GetSfxViewShell()));
-                    aTree.put("rectangle", sRect.getStr());
-                    std::stringstream aStream;
-                    boost::property_tree::write_json(aStream, aTree);
-                    OString aPayload = aStream.str().c_str();
-                    pViewShell->libreOfficeKitViewCallback(LOK_CALLBACK_INVALIDATE_VIEW_CURSOR, aPayload.getStr());
-                }
-                pViewShell = SfxViewShell::GetNext(*pViewShell);
-            }
-        }
+        SfxLokHelper::notifyOtherViews(m_pCursorShell->GetSfxViewShell(), LOK_CALLBACK_INVALIDATE_VIEW_CURSOR, "rectangle", sRect);
     }
 
     if ( !m_pCursorShell->IsCursorReadonly()  || m_pCursorShell->GetViewOptions()->IsSelectionInReadonly() )
@@ -417,6 +398,7 @@ void SwSelPaintRects::Show(std::vector<OString>* pSelectionRectangles)
             if (!pSelectionRectangles)
             {
                 GetShell()->GetSfxViewShell()->libreOfficeKitViewCallback(LOK_CALLBACK_TEXT_SELECTION, sRect.getStr());
+                SfxLokHelper::notifyOtherViews(GetShell()->GetSfxViewShell(), LOK_CALLBACK_TEXT_VIEW_SELECTION, "selection", sRect);
             }
             else
                 pSelectionRectangles->push_back(sRect);
@@ -624,6 +606,7 @@ void SwShellCursor::Show()
         }
         OString sRect = comphelper::string::join("; ", aRect);
         GetShell()->GetSfxViewShell()->libreOfficeKitViewCallback(LOK_CALLBACK_TEXT_SELECTION, sRect.getStr());
+        SfxLokHelper::notifyOtherViews(GetShell()->GetSfxViewShell(), LOK_CALLBACK_TEXT_VIEW_SELECTION, "selection", sRect);
     }
 }
 

commit 9a63980b0586f4a241017231f9b1cf7020db90c9

sw: add LOK_CALLBACK_TEXT_VIEW_SELECTION testcase
Fails with 9f66db9c474f71f43d7a3667230241fd4fa4183f (sw lok: add
LOK_CALLBACK_TEXT_VIEW_SELECTION, 2016-06-21) reverted.

Change-Id: Ide21167ce2dc4287b1860b5f03a6975dc9edd4c6

libreofficekit/source/gtk/lokdocview.cxx
@@ -1039,7 +1039,6 @@ callback (gpointer pData)
                       priv->m_aVisibleCursor.y,
                       priv->m_aVisibleCursor.width,
                       priv->m_aVisibleCursor.height);
-        std::cerr << "debug, LOK_CALLBACK_INVALIDATE_VISIBLE_CURSOR: i am " << priv->m_nViewId << ", i got " << pCallback->m_aPayload << " for myself" << std::endl;
         gtk_widget_queue_draw(GTK_WIDGET(pDocView));
     }
     break;

sw/qa/extras/tiledrendering/tiledrendering.cxx
@@ -225,7 +225,7 @@ void SwTiledRenderingTest::testSetTextSelection()
     SwWrtShell* pWrtShell = pXTextDocument->GetDocShell()->GetWrtShell();
     // Move the cursor into the second word.
     pWrtShell->Right(CRSR_SKIP_CHARS, /*bSelect=*/false, 5, /*bBasicCall=*/false);
-    // Create a selection by on the word.
+    // Create a selection on the word.
     pWrtShell->SelWrd();
     SwShellCursor* pShellCursor = pWrtShell->getShellCursor(false);
     // Did we indeed manage to select the second word?
@@ -548,10 +548,14 @@ class ViewCallback
 public:
     bool m_bOwnCursorInvalidated;
     bool m_bViewCursorInvalidated;
+    bool m_bOwnSelectionSet;
+    bool m_bViewSelectionSet;
 
     ViewCallback()
         : m_bOwnCursorInvalidated(false),
-          m_bViewCursorInvalidated(false)
+          m_bViewCursorInvalidated(false),
+          m_bOwnSelectionSet(false),
+          m_bViewSelectionSet(false)
     {
     }
 
@@ -574,6 +578,16 @@ public:
             m_bViewCursorInvalidated = true;
         }
         break;
+        case LOK_CALLBACK_TEXT_SELECTION:
+        {
+            m_bOwnSelectionSet = true;
+        }
+        break;
+        case LOK_CALLBACK_TEXT_VIEW_SELECTION:
+        {
+            m_bViewSelectionSet = true;
+        }
+        break;
         }
     }
 };
@@ -582,7 +596,7 @@ void SwTiledRenderingTest::testViewCursors()
 {
     comphelper::LibreOfficeKit::setActive();
 
-    createDoc("dummy.fodt");
+    SwXTextDocument* pXTextDocument = createDoc("dummy.fodt");
     ViewCallback aView1;
     SfxViewShell::Current()->registerLibreOfficeKitViewCallback(&ViewCallback::callback, &aView1);
     SfxLokHelper::createView();
@@ -594,6 +608,27 @@ void SwTiledRenderingTest::testViewCursors()
     // This failed: the cursor position of view1 was only known to view2 once
     // it changed.
     CPPUNIT_ASSERT(aView2.m_bViewCursorInvalidated);
+
+    // Make sure that aView1 gets a view-only selection notification, while
+    // aView2 gets a real selection notification.
+    aView1.m_bOwnSelectionSet = false;
+    aView1.m_bViewSelectionSet = false;
+    aView2.m_bOwnSelectionSet = false;
+    aView2.m_bViewSelectionSet = false;
+    SwWrtShell* pWrtShell = pXTextDocument->GetDocShell()->GetWrtShell();
+    // Move the cursor into the second word.
+    pWrtShell->Right(CRSR_SKIP_CHARS, /*bSelect=*/false, 5, /*bBasicCall=*/false);
+    // Create a selection on the word.
+    pWrtShell->SelWrd();
+    SwShellCursor* pShellCursor = pWrtShell->getShellCursor(false);
+    // Did we indeed manage to select the second word?
+    CPPUNIT_ASSERT_EQUAL(OUString("bbb"), pShellCursor->GetText());
+    CPPUNIT_ASSERT(!aView1.m_bOwnSelectionSet);
+    // This failed, aView1 did not get notification about selection changes in
+    // aView2.
+    CPPUNIT_ASSERT(aView1.m_bViewSelectionSet);
+    CPPUNIT_ASSERT(aView2.m_bOwnSelectionSet);
+    CPPUNIT_ASSERT(!aView2.m_bViewSelectionSet);
 
     comphelper::LibreOfficeKit::setActive(false);
 }