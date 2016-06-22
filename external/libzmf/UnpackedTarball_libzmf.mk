cb4d71566a90769aaab5e2edcbddcad11542855b
Aleksas Pantechovskis <alex.pantec@gmail.com>
2016-06-22 19:02:29 +0300

    integrate libzmf
    Change-Id: I0c7ea5b56ea4ed4839ff38798c0b915aaca81774

---
+++ external/libzmf/UnpackedTarball_libzmf.mk
@@ -0,0 +1,24 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_UnpackedTarball_UnpackedTarball,libzmf))
+
+$(eval $(call gb_UnpackedTarball_set_tarball,libzmf,$(ZMF_TARBALL)))
+
+$(eval $(call gb_UnpackedTarball_set_patchlevel,libzmf,0))
+
+ifeq ($(COM_IS_CLANG),TRUE)
+ifneq ($(filter -fsanitize=%,$(CC)),)
+$(eval $(call gb_UnpackedTarball_add_patches,libzmf, \
+    external/libzmf/ubsan-visibility.patch \
+))
+endif
+endif
+
+# vim: set noet sw=4 ts=4:
