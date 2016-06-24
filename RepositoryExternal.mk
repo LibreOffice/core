f554d92e06d6eda045e5ea68ea4a008b9029d65a
Jaskaran Singh <jvsg1303@gmail.com>
2016-06-24 19:53:11 +0530

    Switch from orcus-0.11 to orcus-0.12
    Change-Id: I837ed3a30fae6fbe6cb658f1feecce1e88279ae0

--- RepositoryExternal.mk
+++ RepositoryExternal.mk
@@ -3214,7 +3214,7 @@ $(call gb_LinkTarget_set_include,$(1),\
 )
 
 $(call gb_LinkTarget_add_libs,$(1),\
-       -L$(call gb_UnpackedTarball_get_dir,liborcus)/src/liborcus/.libs -lorcus-0.11 \
+       -L$(call gb_UnpackedTarball_get_dir,liborcus)/src/liborcus/.libs -lorcus-0.12 \
 )
 
 $(if $(SYSTEM_BOOST), \
@@ -3233,7 +3233,7 @@ $(call gb_LinkTarget_set_include,$(1),\
 )
 
 $(call gb_LinkTarget_add_libs,$(1),\
-	-L$(call gb_UnpackedTarball_get_dir,liborcus)/src/parser/.libs -lorcus-parser-0.11 \
+	-L$(call gb_UnpackedTarball_get_dir,liborcus)/src/parser/.libs -lorcus-parser-0.12 \
 )
 
 endef
