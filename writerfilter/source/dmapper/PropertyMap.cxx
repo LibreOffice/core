LibreOffice/core

commit 50cb1667020494906afaacb68d4163d1eda527cf

fdo#49940 dmapper: handle m_bTitlePage when m_nBreakType is zero
We used to ignore m_bTitlePage in this case, resulting in wrong
'Default' page style for the first page, instead of 'First Page'.

Change-Id: I1899354fb39db4f0eb663fd5233395f2d4a5e72a

writerfilter/source/dmapper/PropertyMap.cxx
@@ -794,6 +794,19 @@ void SectionPropertyMap::PrepareHeaderFooterProperties( bool bFirstPage )
     operator[]( PropertyDefinition( PROP_BOTTOM_MARGIN, false )) = uno::makeAny( m_nBottomMargin + nFooterHeight );
 }
 
+uno::Reference<beans::XPropertySet> lcl_GetRangeProperties(bool bIsFirstSection, DomainMapper_Impl& rDM_Impl, uno::Reference<text::XTextRange> xStartingRange)
+{
+    uno::Reference< beans::XPropertySet > xRangeProperties;
+    if (bIsFirstSection)
+    {
+        uno::Reference<container::XEnumerationAccess> xEnumAccess(rDM_Impl.GetBodyText(), uno::UNO_QUERY_THROW);
+        uno::Reference<container::XEnumeration> xEnum = xEnumAccess->createEnumeration();
+        xRangeProperties = uno::Reference<beans::XPropertySet>(xEnum->nextElement(), uno::UNO_QUERY_THROW);
+    }
+    else
+        xRangeProperties = uno::Reference<beans::XPropertySet>(xStartingRange, uno::UNO_QUERY_THROW);
+    return xRangeProperties;
+}
 
 void SectionPropertyMap::CloseSectionGroup( DomainMapper_Impl& rDM_Impl )
 {
@@ -834,6 +847,8 @@ void SectionPropertyMap::CloseSectionGroup( DomainMapper_Impl& rDM_Impl )
                                     rDM_Impl.appendTextSectionAfter( m_xStartingRange );
         if( m_nColumnCount > 0 && xSection.is())
             ApplyColumnProperties( xSection );
+        uno::Reference<beans::XPropertySet> xRangeProperties(lcl_GetRangeProperties(m_bIsFirstSection, rDM_Impl, m_xStartingRange));
+        xRangeProperties->setPropertyValue(rPropNameSupplier.GetName(PROP_PAGE_DESC_NAME), uno::makeAny(m_bTitlePage ? m_sFirstPageStyleName : m_sFollowPageStyleName));
     }
     else
     {
@@ -963,15 +978,7 @@ void SectionPropertyMap::CloseSectionGroup( DomainMapper_Impl& rDM_Impl )
         {
             {
                 //now apply this break at the first paragraph of this section
-                uno::Reference< beans::XPropertySet > xRangeProperties;
-                if( m_bIsFirstSection )
-                {
-                    uno::Reference< container::XEnumerationAccess > xEnumAccess( rDM_Impl.GetBodyText( ), uno::UNO_QUERY_THROW );
-                    uno::Reference< container::XEnumeration >  xEnum = xEnumAccess->createEnumeration( );
-                    xRangeProperties = uno::Reference< beans::XPropertySet >( xEnum->nextElement( ), uno::UNO_QUERY_THROW );
-                }
-                else
-                    xRangeProperties = uno::Reference< beans::XPropertySet >( m_xStartingRange, uno::UNO_QUERY_THROW );
+                uno::Reference<beans::XPropertySet> xRangeProperties(lcl_GetRangeProperties(m_bIsFirstSection, rDM_Impl, m_xStartingRange));
             /* break type
             0 - No break 1 - New Colunn 2 - New page 3 - Even page 4 - odd page */
                 xRangeProperties->setPropertyValue(rPropNameSupplier.GetName( PROP_PAGE_DESC_NAME ),

commit a18d7a2afce018234a0acb9cc1b955ae1a57aa0c

tdf#90697 docx - don't change continuous break into page break
As soon as you set PROP_PAGE_DESC_NAME, you are inserting that
style as a page break.  Setting a pagebreak via a continous break
was first introduced in commit 50cb1667020494906afaacb68d4163d1eda527cf
but the unittest for that commit no longer uses this code.
I'm suggesting it be reverted.  It really messes up round-tripping
when continuous breaks are removed/replaced with hard page breaks.

There are a few odd cases where the very first section needs to set the
page break via the continuous break, so it hasn't been eliminated
completely.

Change-Id: I5b0e4bb7612ee9df47e5c49b8c2316dc001b5238

writerfilter/source/dmapper/PropertyMap.cxx
@@ -557,6 +557,18 @@ uno::Reference< beans::XPropertySet > SectionPropertyMap::GetPageStyle(
     return xRet;
 }
 
+void SectionPropertyMap::SwitchToPageStyle( uno::Reference< beans::XPropertySet > const& xStyle, const OUString sPageStyleName )
+throw ( css::beans::UnknownPropertyException,
+        css::beans::PropertyVetoException,
+        css::lang::IllegalArgumentException,
+        css::lang::WrappedTargetException,
+        css::uno::RuntimeException, std::exception )
+{
+    if( xStyle.is() && !sPageStyleName.isEmpty() )
+    {
+        xStyle->setPropertyValue( getPropertyName(PROP_PAGE_DESC_NAME), uno::makeAny(sPageStyleName) );
+    }
+}
 
 void SectionPropertyMap::SetBorder( BorderPosition ePos, sal_Int32 nLineDistance, const table::BorderLine2& rBorderLine, bool bShadow )
 {
@@ -1154,7 +1166,8 @@ void SectionPropertyMap::CloseSectionGroup( DomainMapper_Impl& rDM_Impl )
             {
                 try
                 {
-                    xRangeProperties->setPropertyValue(getPropertyName(PROP_PAGE_DESC_NAME), uno::makeAny(aName));
+                    if( m_bIsFirstSection )
+                        SwitchToPageStyle( xRangeProperties, aName );
                     uno::Reference<beans::XPropertySet> xPageStyle (rDM_Impl.GetPageStyles()->getByName(aName), uno::UNO_QUERY_THROW);
                     HandleMarginsHeaderFooter(rDM_Impl);
                     if (rDM_Impl.IsNewDoc())
@@ -1352,10 +1365,7 @@ void SectionPropertyMap::CloseSectionGroup( DomainMapper_Impl& rDM_Impl )
                 }
 
                 if (xRangeProperties.is() && rDM_Impl.IsNewDoc())
-                    xRangeProperties->setPropertyValue(
-                        getPropertyName( PROP_PAGE_DESC_NAME ),
-                        uno::makeAny( m_bTitlePage ?  m_sFirstPageStyleName
-                                      : m_sFollowPageStyleName ));
+                    SwitchToPageStyle( xRangeProperties, m_bTitlePage ?  m_sFirstPageStyleName : m_sFollowPageStyleName );
 
                 if(m_bPageNoRestart || m_nPageNumber >= 0)
                 {

writerfilter/source/dmapper/PropertyMap.hxx
@@ -293,6 +293,13 @@ public:
                                                                const css::uno::Reference<css::lang::XMultiServiceFactory>& xTextFactory,
                                                                bool bFirst);
 
+    void SwitchToPageStyle( css::uno::Reference<css::beans::XPropertySet> const& xStyle, const OUString sPageStyleName )
+        throw ( css::beans::UnknownPropertyException,
+                css::beans::PropertyVetoException,
+                css::lang::IllegalArgumentException,
+                css::lang::WrappedTargetException,
+                css::uno::RuntimeException, std::exception );
+
     void SetBorder(BorderPosition ePos, sal_Int32 nLineDistance, const css::table::BorderLine2& rBorderLine, bool bShadow);
     void SetBorderParams( sal_Int32 nSet ) { m_nBorderParams = nSet; }
 