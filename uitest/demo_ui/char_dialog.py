02f43b0ad3865ef7af17fe51bc87680ffe881dc0
Markus Mohrhard <markus.mohrhard@googlemail.com>
2016-06-25 10:25:47 +0200

    uitest: add special char widget demo
    Change-Id: I513ae293cfed488723e221a7eec37f103efefa92

---
+++ uitest/demo_ui/char_dialog.py
@@ -0,0 +1,37 @@
+# -*- Mode: python; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+from uitest_helper import UITest
+
+from helper import mkPropertyValues
+from UITestCase import UITestCase
+
+import time
+
+class CharDialogText(UITestCase):
+
+    def test_select_char(self):
+        self.ui_test.create_doc_in_start_center("calc")
+
+        self.ui_test.execute_dialog_through_command(".uno:InsertSymbol")
+        xCharDialog = self.xUITest.getTopFocusWindow()
+        print(xCharDialog.getChildren())
+
+        time.sleep(5)
+
+        xCharSet = xCharDialog.getChild("showcharset")
+
+        xCharSet.executeAction("SELECT", mkPropertyValues({"COLUMN": "2", "ROW": "2"}))
+
+        time.sleep(5)
+
+        xCancelBtn = xCharDialog.getChild("cancel")
+        xCancelBtn.executeAction("CLICK", tuple())
+
+        self.ui_test.close_doc()
+
+# vim:set shiftwidth=4 softtabstop=4 expandtab: */
