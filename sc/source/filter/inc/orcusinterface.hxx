878ed0acb389443bfcf311057a2ff9597dcf813f
Jaskaran Singh <jvsg1303@gmail.com>
2016-06-26 13:13:42 +0530

    Declare font, border, protection orcus interface methods
    Change-Id: I486d861ba3c2c763964cdc3af2312661ef1a533d

--- sc/source/filter/inc/orcusinterface.hxx
+++ sc/source/filter/inc/orcusinterface.hxx
@@ -357,6 +357,13 @@ public:
     virtual void set_font_name(const char* s, size_t n) override;
     virtual void set_font_size(double point) override;
     virtual void set_font_underline(orcus::spreadsheet::underline_t e) override;
+    virtual void set_font_underline_width(orcus::spreadsheet::underline_width_t e) override;
+    virtual void set_font_underline_mode(orcus::spreadsheet::underline_mode_t e) override;
+    virtual void set_font_underline_type(orcus::spreadsheet::underline_type_t e) override;
+    virtual void set_font_underline_color(orcus::spreadsheet::color_elem_t alpha,
+            orcus::spreadsheet::color_elem_t red,
+            orcus::spreadsheet::color_elem_t green,
+            orcus::spreadsheet::color_elem_t blue) override;
     virtual void set_font_color( orcus::spreadsheet::color_elem_t alpha,
             orcus::spreadsheet::color_elem_t red,
             orcus::spreadsheet::color_elem_t green,
@@ -381,11 +388,14 @@ public:
             orcus::spreadsheet::color_elem_t red,
             orcus::spreadsheet::color_elem_t green,
             orcus::spreadsheet::color_elem_t blue) override;
+    virtual void set_border_width(orcus::spreadsheet::border_direction_t dir, orcus::length_t width) override;
     virtual size_t commit_border() override;
 
     // cell protection
     virtual void set_cell_hidden(bool b) override;
     virtual void set_cell_locked(bool b) override;
+    virtual void set_cell_print_content(bool b) override;
+    virtual void set_cell_formula_hidden(bool b) override;
     virtual size_t commit_cell_protection() override;
 
     // number format
--- sc/source/filter/orcus/interface.cxx
+++ sc/source/filter/orcus/interface.cxx
@@ -961,6 +961,29 @@ void ScOrcusStyles::set_font_underline(orcus::spreadsheet::underline_t e)
     }
 }
 
+void ScOrcusStyles::set_font_underline_width(orcus::spreadsheet::underline_width_t /* e */)
+{
+
+}
+
+void ScOrcusStyles::set_font_underline_mode(orcus::spreadsheet::underline_mode_t /* e */)
+{
+
+}
+
+void ScOrcusStyles::set_font_underline_type(orcus::spreadsheet::underline_type_t /* e */)
+{
+
+}
+
+void ScOrcusStyles::set_font_underline_color(orcus::spreadsheet::color_elem_t /*alpha*/,
+            orcus::spreadsheet::color_elem_t /*red*/,
+            orcus::spreadsheet::color_elem_t /*green*/,
+            orcus::spreadsheet::color_elem_t /*blue*/)
+{
+
+}
+
 void ScOrcusStyles::set_font_color(orcus::spreadsheet::color_elem_t alpha,
             orcus::spreadsheet::color_elem_t red,
             orcus::spreadsheet::color_elem_t green,
@@ -1035,6 +1058,11 @@ void ScOrcusStyles::set_border_color(orcus::spreadsheet::border_direction_t dir,
     current_line.maColor = Color(alpha, red, green, blue);
 }
 
+void ScOrcusStyles::set_border_width(orcus::spreadsheet::border_direction_t /* dir */, orcus::length_t /* width */)
+{
+
+}
+
 size_t ScOrcusStyles::commit_border()
 {
     SAL_INFO("sc.orcus.style", "commit border");
@@ -1052,6 +1080,16 @@ void ScOrcusStyles::set_cell_hidden(bool b)
 void ScOrcusStyles::set_cell_locked(bool b)
 {
     maCurrentProtection.mbLocked = b;
+}
+
+void ScOrcusStyles::set_cell_print_content(bool /* b */)
+{
+
+}
+
+void ScOrcusStyles::set_cell_formula_hidden(bool /* b */)
+{
+
 }
 
 size_t ScOrcusStyles::commit_cell_protection()
