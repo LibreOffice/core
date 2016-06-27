34dbeaed44df03a57faa346d1a7e7e0a0205476d
Tor Lillqvist <tml@collabora.com>
2016-06-27 17:26:44 +0300

    tdf#75387: Don't case-fold strings for formula group calculations
    There should be no need for it as far as I see. Possibly it made some
    sense back when we thought we could use OpenCL for operators and
    functions that are supposed to ignore the case of strings they
    handle. But we hopefully should never try to use OpenCL for string
    data nowadays anyway. And actually, the case-folding ended up
    affecting strings handled by the so-called software interpreter. For
    the tdf#75387, the sample sheet worked fine when OpenCL was enabled.
    
    Change-Id: I1c170ebf09551fea0964f394cdb3c65079672257
    (cherry picked from commit f948dce856d51704e1962553198015dbebb6e527)
    Reviewed-on: https://gerrit.libreoffice.org/26746
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Eike Rathke <erack@redhat.com>
    Tested-by: Eike Rathke <erack@redhat.com>
    (cherry picked from commit 593d8c825df9c0e79b2a8bb9a726ffb34b146845)

--- sc/source/core/data/column2.cxx
+++ sc/source/core/data/column2.cxx
@@ -2326,7 +2326,7 @@ bool appendToBlock(
                 rCxt.ensureStrArray(rColArray, nArrayLen);
 
                 for (; itData != itDataEnd; ++itData, ++nPos)
-                    (*rColArray.mpStrArray)[nPos] = itData->getDataIgnoreCase();
+                    (*rColArray.mpStrArray)[nPos] = itData->getData();
             }
             break;
             case sc::element_type_edittext:
@@ -2338,7 +2338,7 @@ bool appendToBlock(
                 for (; itData != itDataEnd; ++itData, ++nPos)
                 {
                     OUString aStr = ScEditUtil::GetString(**itData, pDoc);
-                    (*rColArray.mpStrArray)[nPos] = rPool.intern(aStr).getDataIgnoreCase();
+                    (*rColArray.mpStrArray)[nPos] = rPool.intern(aStr).getData();
                 }
             }
             break;
@@ -2439,7 +2439,7 @@ void copyFirstStringBlock(
             sc::string_block::iterator itEnd = it;
             std::advance(itEnd, nLen);
             for (; it != itEnd; ++it, ++itArray)
-                *itArray = it->getDataIgnoreCase();
+                *itArray = it->getData();
         }
         break;
         case sc::element_type_edittext:
@@ -2453,7 +2453,7 @@ void copyFirstStringBlock(
             {
                 EditTextObject* pText = *it;
                 OUString aStr = ScEditUtil::GetString(*pText, &rDoc);
-                *itArray = rPool.intern(aStr).getDataIgnoreCase();
+                *itArray = rPool.intern(aStr).getData();
             }
         }
         break;
