46047f1ee9b8bf2283561fefcac095e4a3189839
Akshay Deep <akshaydeepiitr@gmail.com>
2016-06-27 16:15:23 +0530

    GSoC Emoji View
    Tab Window
    Add emoji.json to installation directory
    Read the JSON data from code
    Filter emojis according to categories
    
    TODO:
    1. Create toolbar control
    2. Search (probably another patch)
    
    Change-Id: Ia5a577ce41398132872914ebfa499e4d4077d57e

---
+++ include/sfx2/emojicontrol.hxx
@@ -0,0 +1,46 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef INCLUDED_SFX2_INC_EMOJICONTROL_HXX
+#define INCLUDED_SFX2_INC_EMOJICONTROL_HXX
+
+#include <sal/config.h>
+#include <sfx2/dllapi.h>
+#include <vcl/button.hxx>
+#include <vcl/tabctrl.hxx>
+#include <vcl/tabpage.hxx>
+#include <sfx2/tbxctrl.hxx>
+
+class EmojiView;
+enum class FILTER_CATEGORY;
+
+class SFX2_DLLPUBLIC SfxEmojiControl : public SfxPopupWindow
+{
+public:
+    explicit SfxEmojiControl(sal_uInt16 nId);
+
+    virtual ~SfxEmojiControl();
+
+    virtual void dispose() override;
+
+private:
+    void ConvertLabelToUnicode(sal_uInt16 nPageId);
+
+    /// Return filter according to the currently selected tab page.
+    FILTER_CATEGORY getCurrentFilter();
+
+    DECL_LINK_TYPED(ActivatePageHdl, TabControl*, void);
+
+    VclPtr<TabControl>   mpTabControl;
+    VclPtr<EmojiView>    mpEmojiView;
+};
+
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
