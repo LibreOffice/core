92dcd43e5aea4f4c1b8148e9d8a13c165e2a8c48
Katarina Behrens <Katarina.Behrens@cib.de>
2016-06-27 14:22:49 +0200

    Modify tab dialog screenshot id slightly
    use only ID of the current page
    
    Change-Id: I27ef4d2112a047379cec6bc221ba403f1a0371e4

--- sfx2/source/dialog/tabdlg.cxx
+++ sfx2/source/dialog/tabdlg.cxx
@@ -1235,7 +1235,7 @@ OString SfxTabDialog::GetScreenshotId() const
         vcl::Window* pToplevelBox = pActiveTabPage->GetWindow( GetWindowType::FirstChild );
 
         if ( pToplevelBox )
-            aScreenshotId = aScreenshotId + OString("#") + pToplevelBox->GetHelpId();
+            aScreenshotId = pToplevelBox->GetHelpId();
     }
 
     return aScreenshotId;
