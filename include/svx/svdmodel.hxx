7e49e303f800d1508643daa5a00e7f1603e6b73b
Noel Grandin <noel@peralex.com>
2016-06-28 10:44:11 +0200

    loplugin:staticmethods
    Change-Id: I597015df5c12040b1b2a108bcfbe2831fee6ec23

--- include/svx/svdmodel.hxx
+++ include/svx/svdmodel.hxx
@@ -551,8 +551,8 @@ public:
     bool IsWriter() const { return !bMyPool; }
 
     // Used as a fallback in *::ReadUserDataSequence() to process common properties
-    void ReadUserDataSequenceValue(const css::beans::PropertyValue *pValue);
-    void WriteUserDataSequence(css::uno::Sequence < css::beans::PropertyValue >& rValues, bool bBrowse = false);
+    static void ReadUserDataSequenceValue(const css::beans::PropertyValue *pValue);
+    static void WriteUserDataSequence(css::uno::Sequence < css::beans::PropertyValue >& rValues, bool bBrowse = false);
 
     /** returns the numbering type that is used to format page fields in drawing shapes */
     virtual SvxNumType GetPageNumType() const;
