0223781c3fa4f73985cf7aec58924f91c8963a75
Armin Le Grand <Armin.Le.Grand@cib.de>
2016-07-01 14:10:00 +0200

    tdf#50613 add close_path to correctly show closed polygons
    For closed polygons it is essential to add a close_path to
    correctly show the last line join.
    
    Change-Id: Ib6f37bbc5e85133f21a936b186eb0ab12773f7da

--- vcl/unx/generic/gdi/salgdi.cxx
+++ vcl/unx/generic/gdi/salgdi.cxx
@@ -802,6 +802,11 @@ bool X11SalGraphics::drawPolyLine(
                 }
             }
 
+            if(rPolygon.isClosed() && !bNoJoin)
+            {
+                cairo_close_path(cr);
+            }
+
             cairo_stroke(cr);
         }
 
