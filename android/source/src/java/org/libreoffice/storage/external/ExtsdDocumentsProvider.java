66be4feef7e0d3661f01fbb2372700de5eeea070
Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>
2016-07-02 17:20:52 +0200

    android: prevent crash if storage is unavailable
    Change-Id: I350acc1735d5fd22a27434fc5ccf2ab39bf3d2cf

--- android/source/src/java/org/libreoffice/storage/external/ExtsdDocumentsProvider.java
+++ android/source/src/java/org/libreoffice/storage/external/ExtsdDocumentsProvider.java
@@ -59,6 +59,9 @@ public class ExtsdDocumentsProvider implements IExternalDocumentProvider,
         String internalSDPath = internalSD.getAbsolutePath();
 
         for (File option: options) {
+            // Returned paths may be null if a storage device is unavailable.
+            if (null == option) { continue; }
+
             String optionPath = option.getAbsolutePath();
 
             if(optionPath.contains(internalSDPath))
