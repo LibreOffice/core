f9cd6d0efdb3dfa820205a126e4d6adb53db861b
Eike Rathke <erack@redhat.com>
2016-07-04 23:19:41 +0200

    explicitly disable partial cell shift with change-tracking, tdf#73335 related
    ... in ScDocFunc::InsertCells()/DeleteCells() that can be reached via
    API but ChangeTrack produces a SC_CAT_NONE action because the partial
    shift is not handled. UI slots are disabled for this.
    
    Change-Id: Ie0f992b05753bb1467d3f7a80263714bfcc1a2d7
    (cherry picked from commit 9f8e2065c42f1724ac7a24f1bb0531e8c954698a)
    Reviewed-on: https://gerrit.libreoffice.org/26923
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>
    (cherry picked from commit 9fb749f520b94d11ebeaf83c1e913dacb742703b)

--- sc/source/ui/docshell/docfunc.cxx
+++ sc/source/ui/docshell/docfunc.cxx
@@ -1631,6 +1631,16 @@ bool ScDocFunc::InsertCells( const ScRange& rRange, const ScMarkData* pTabMark, 
 {
     ScDocShellModificator aModificator( rDocShell );
 
+    if (rDocShell.GetDocument().GetChangeTrack() &&
+            ((eCmd == INS_CELLSDOWN  && (rRange.aStart.Col() != 0 || rRange.aEnd.Col() != MAXCOL)) ||
+             (eCmd == INS_CELLSRIGHT && (rRange.aStart.Row() != 0 || rRange.aEnd.Row() != MAXROW))))
+    {
+        // We should not reach this via UI disabled slots.
+        assert(bApi);
+        SAL_WARN("sc.ui","ScDocFunc::InsertCells - no change-tracking of partial cell shift");
+        return false;
+    }
+
     ScRange aTargetRange( rRange );
 
     // If insertion is for full cols/rows and after the current
@@ -2092,6 +2102,16 @@ bool ScDocFunc::DeleteCells( const ScRange& rRange, const ScMarkData* pTabMark, 
                              bool bRecord, bool bApi )
 {
     ScDocShellModificator aModificator( rDocShell );
+
+    if (rDocShell.GetDocument().GetChangeTrack() &&
+            ((eCmd == DEL_CELLSUP   && (rRange.aStart.Col() != 0 || rRange.aEnd.Col() != MAXCOL)) ||
+             (eCmd == DEL_CELLSLEFT && (rRange.aStart.Row() != 0 || rRange.aEnd.Row() != MAXROW))))
+    {
+        // We should not reach this via UI disabled slots.
+        assert(bApi);
+        SAL_WARN("sc.ui","ScDocFunc::DeleteCells - no change-tracking of partial cell shift");
+        return false;
+    }
 
     SCCOL nStartCol = rRange.aStart.Col();
     SCROW nStartRow = rRange.aStart.Row();
