7f3e4fe9af218cd7175798a21978e9b802b7a38a
Akshay Deep <akshaydeepiitr@gmail.com>
2016-07-04 14:20:34 +0530

    Template Manager: Merge local view and abstract view
    Change-Id: I789ca15f04d27c1fe1422ff1a81746004b627977

--- sfx2/source/control/recentdocsview.cxx
+++ sfx2/source/control/recentdocsview.cxx
@@ -19,7 +19,7 @@
 
 #include <sax/tools/converter.hxx>
 #include <sfx2/recentdocsview.hxx>
-#include <sfx2/templateabstractview.hxx>
+#include <sfx2/templatelocalview.hxx>
 #include <sfx2/app.hxx>
 #include <sfx2/sfx.hrc>
 #include <sfx2/sfxresid.hxx>
