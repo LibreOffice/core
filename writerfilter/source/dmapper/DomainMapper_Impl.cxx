b2bf34141fdb41abfe421931dcfc093457f30d2d
Caolán McNamara <caolanm@redhat.com>
2016-07-05 10:52:29 +0100

    Resolves: tdf#100770 crash on loading specific docx
    use same safeguards as RemoveLastParagraph does
    
    Change-Id: I43ed4eb28f44654054fd266bc464840af7014fea
    (cherry picked from commit 000263f799d4aa9ad21e63f474def55438ce601e)
    Reviewed-on: https://gerrit.libreoffice.org/26944
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
    (cherry picked from commit 57144fae2bd2296fe9a842ab87addf9dda5ff91a)

--- writerfilter/source/dmapper/DomainMapper_Impl.cxx
+++ writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -328,7 +328,14 @@ void DomainMapper_Impl::RemoveDummyParaForTableInSection()
     SectionPropertyMap* pSectionContext = dynamic_cast< SectionPropertyMap* >( pContext.get() );
     if (!pSectionContext)
         return;
-    uno::Reference< text::XTextCursor > xCursor = GetTopTextAppend()->createTextCursorByRange(pSectionContext->GetStartingRange());
+
+    if (m_aTextAppendStack.empty())
+        return;
+    uno::Reference< text::XTextAppend > xTextAppend = m_aTextAppendStack.top().xTextAppend;
+    if (!xTextAppend.is())
+        return;
+
+    uno::Reference< text::XTextCursor > xCursor = xTextAppend->createTextCursorByRange(pSectionContext->GetStartingRange());
 
     // Remove the extra NumPicBullets from the document,
     // which get attached to the first paragraph in the
@@ -367,7 +374,7 @@ void DomainMapper_Impl::RemoveLastParagraph( )
 {
     if (m_aTextAppendStack.empty())
         return;
-    uno::Reference< text::XTextAppend >  xTextAppend = m_aTextAppendStack.top().xTextAppend;
+    uno::Reference< text::XTextAppend > xTextAppend = m_aTextAppendStack.top().xTextAppend;
     if (!xTextAppend.is())
         return;
     try
