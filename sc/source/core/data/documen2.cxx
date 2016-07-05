9878630244be3101a633e741240f71ec09f56888
Eike Rathke <erack@redhat.com>
2016-07-05 15:11:34 +0200

    Resolves: tdf#90285 do not reuse the same StartListeningContext
    ... that after UpdateInsertTab and related may point into broadcasters'
    ColumnBlockPosition that have been shifted to the next sheet.
    
    Change-Id: I6f657021b6102cec427734a8ff3105f0c4309a7a
    (cherry picked from commit dd7a2c95b86d158be8d0637bdff13b9a0ed9954b)
    Reviewed-on: https://gerrit.libreoffice.org/26959
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>
    (cherry picked from commit dff1d0abc3e66277da76c60943ec6bc92c29b14f)

--- sc/source/core/data/documen2.cxx
+++ sc/source/core/data/documen2.cxx
@@ -827,7 +827,6 @@ bool ScDocument::CopyTab( SCTAB nOldPos, SCTAB nNewPos, const ScMarkData* pOnlyM
 
     sc::AutoCalcSwitch aACSwitch(*this, false);
     sc::RefUpdateInsertTabContext aCxt( *this, nNewPos, 1);
-    sc::StartListeningContext aSLCxt(*this);
 
     if (bValid)
     {
@@ -874,6 +873,7 @@ bool ScDocument::CopyTab( SCTAB nOldPos, SCTAB nNewPos, const ScMarkData* pOnlyM
                     if (*it && it != maTabs.begin()+nOldPos && it != maTabs.begin() + nNewPos)
                         (*it)->UpdateCompile();
                 SetNoListening( false );
+                sc::StartListeningContext aSLCxt(*this);
                 for (TableContainer::iterator it = maTabs.begin(); it != maTabs.end(); ++it)
                     if (*it && it != maTabs.begin()+nOldPos && it != maTabs.begin()+nNewPos)
                         (*it)->StartListeners(aSLCxt, true);
@@ -910,6 +910,7 @@ bool ScDocument::CopyTab( SCTAB nOldPos, SCTAB nNewPos, const ScMarkData* pOnlyM
         maTabs[nOldPos]->UpdateCompile();
         maTabs[nNewPos]->UpdateCompile( true ); //  maybe already compiled in Clone, but used names need recompilation
         SetNoListening( false );
+        sc::StartListeningContext aSLCxt(*this);
         maTabs[nOldPos]->StartListeners(aSLCxt, true);
         maTabs[nNewPos]->StartListeners(aSLCxt, true);
 
