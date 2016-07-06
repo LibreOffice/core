24c405b1590f02d1b0f7955c9868f25067d139fa
Akash Jain <akash96j@gmail.com>
2016-07-06 10:15:49 +0530

    GSoC: Modify DrawServerFontLayout and add DrawSalLayout
    Modify the definiton of the DrawServerFontLayout method.
    Add new DrawSalLayout method which will be used for drawing text
    independent of the platform.
    
    Change-Id: Ie3eefb172b1781c685def1ef549db2538f672a62

--- vcl/source/gdi/CommonSalLayout.cxx
+++ vcl/source/gdi/CommonSalLayout.cxx
@@ -221,9 +221,10 @@ void CommonSalLayout::AdjustLayout(ImplLayoutArgs& rArgs)
     }
 }
 
-void CommonSalLayout::DrawText( SalGraphics& ) const
+void CommonSalLayout::DrawText( SalGraphics& rSalGraphics ) const
 {
     //call platform dependent DrawText functions
+    rSalGraphics.DrawSalLayout( *this );
 }
 
 bool CommonSalLayout::LayoutText(ImplLayoutArgs& rArgs)
