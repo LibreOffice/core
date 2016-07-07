8550366138d576123b9e66a1a7915a04026d79cd
Rishabh Kumar <kris.kr296@yahoo.in>
2016-07-07 20:01:23 +0000

    Revert "Add XBitmapList to SvxPresetListBox"
    This reverts commit a38901295082ff72948a18fe9c82c37973061490.
    
    Change-Id: Ibfee6df54c24baf8b4e9cfc47be3f12e0ebd82a4
    Reviewed-on: https://gerrit.libreoffice.org/27020
    Reviewed-by: Rishabh Kumar <kris.kr296@yahoo.in>
    Tested-by: Rishabh Kumar <kris.kr296@yahoo.in>

--- include/svx/SvxPresetListBox.hxx
+++ include/svx/SvxPresetListBox.hxx
@@ -59,7 +59,6 @@ public:
 
     void FillPresetListBox(XGradientList& pList, sal_uInt32 nStartIndex = 1);
     void FillPresetListBox(XHatchList& pList, sal_uInt32 nStartIndex = 1);
-    void FillPresetListBox(XBitmapList& pList, sal_uInt32 nStartIndex = 1);
     void DrawLayout();
 
 };
