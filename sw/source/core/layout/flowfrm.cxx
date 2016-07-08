b84ed6082b92a2037a539210a912a6048dc6a1df
Caolán McNamara <caolanm@redhat.com>
2016-07-08 16:09:36 +0100

    Resolves: tdf#100813 crash during pagination of particular docx
    Change-Id: Id2c99cc6c5fe4c3a5bcf3c0b3f16b603cdd46239
    (cherry picked from commit f374e01af32c7752b31455642e7d76f2056a2aeb)

--- sw/source/core/layout/flowfrm.cxx
+++ sw/source/core/layout/flowfrm.cxx
@@ -2010,6 +2010,9 @@ bool SwFlowFrame::MoveBwd( bool &rbReformat )
     }
 
     SwFootnoteBossFrame * pOldBoss = m_rThis.FindFootnoteBossFrame();
+    if (!pOldBoss)
+        return false;
+
     SwPageFrame * const pOldPage = pOldBoss->FindPageFrame();
     SwLayoutFrame *pNewUpper = nullptr;
     bool bCheckPageDescs = false;
