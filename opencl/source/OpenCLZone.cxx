f0da69b41b1d8aa963887c743ce2ff981691b8db
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2016-07-08 22:16:27 +0900

    opencl: OpenCLZone, detect CL device change and disable CL on crash
    Guard OpenCL calls with OpenCLZone, so if a OpenCL call crashes we
    detect this and disable OpenCL so next time the user doesn't encounter
    the crash at the same calculation because he has a broken OpenCL
    drivers. Similar has been implemented for OpenGL with good results.
    
    Additionaly we persistently remember a known good OpenCL device ID and
    driver version so we can match this and perform calculation tests when
    they change. This is to ensure that the selected OpenCL device performs
    as we expect. In this commit the calculation tests aren't included yet.
    
    Remove complex static initializer in opencl wrapper library.
    
    Change-Id: I1a8b81ee31298731efcf63dc6a476955afc035e9
    Reviewed-on: https://gerrit.libreoffice.org/27064
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>
    Tested-by: Tomaž Vajngerl <quikee@gmail.com>
    (cherry picked from commit f41eb66302208f384a475fb20c98b6d1b0676cb6)
    Reviewed-on: https://gerrit.libreoffice.org/27099
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Michael Meeks <michael.meeks@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/27144
    Reviewed-by: Jan Holesovsky <kendy@collabora.com>
    Tested-by: Michael Meeks <michael.meeks@collabora.com>

---
+++ opencl/source/OpenCLZone.cxx
@@ -0,0 +1,46 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <opencl/OpenCLZone.hxx>
+
+#include <memory>
+
+#include <officecfg/Office/Common.hxx>
+#include <com/sun/star/util/XFlushable.hpp>
+#include <com/sun/star/configuration/theDefaultProvider.hpp>
+
+// FIXME: templatize me vs. OpenGLZone.
+
+sal_uInt64 volatile OpenCLZone::gnEnterCount = 0;
+sal_uInt64 volatile OpenCLZone::gnLeaveCount = 0;
+
+/**
+ * Called from a signal handler if we get
+ * a crash or hang in some CL code.
+ */
+void OpenCLZone::hardDisable()
+{
+    // protect ourselves from double calling etc.
+    static bool bDisabled = false;
+    if (!bDisabled)
+    {
+        bDisabled = true;
+
+        std::shared_ptr<comphelper::ConfigurationChanges> xChanges(comphelper::ConfigurationChanges::create());
+        officecfg::Office::Common::Misc::UseOpenCL::set(false, xChanges);
+        xChanges->commit();
+
+        // Force synchronous config write
+        auto xConfProvider = css::configuration::theDefaultProvider::get(comphelper::getProcessComponentContext());
+        css::uno::Reference<css::util::XFlushable> xFlushable(xConfProvider, css::uno::UNO_QUERY_THROW);
+        xFlushable->flush();
+    }
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
