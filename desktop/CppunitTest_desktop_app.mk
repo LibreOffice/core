d4bf71af12d3947e76269842eeeae6dcc0f32c63
Mike Kaganski <mike.kaganski@collabora.com>
2016-07-11 06:38:50 +1000

    tdf#100837: Support Office URI Schemes
    This patch adds support for Office URI Schemes (see
    https://msdn.microsoft.com/en-us/library/dn906146).
    This will enable browser (non-CMIS) integration of LibreOffice
    with MS SharePoint server (v.2013 tested).
    
    In this patch, in addition to ms-* schemes, a new scheme is
    introduced: vnd.libreoffice.documentlocation, which is analogous
    to ms-*. Its purpose is to enable flexible configuration of
    server and client, where some types of documents are declared as
    handled by LibreOffice, and other are handled by other software.
    E.g., ODTs may have "libreoffice" scheme, while DOCXs could be
    "ms-word". Client may register LibreOffice to handle both, or to
    handle only "vnd.libreoffice.documentlocation" scheme.
    
    Unit test included.
    
    TODO in a later patch: add a mechanism to register LibreOffice
    to the schemes with OS.
    
    Change-Id: I1c449a211102036f87163058a4c90a93eb32c948

---
+++ desktop/CppunitTest_desktop_app.mk
@@ -0,0 +1,48 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_CppunitTest_CppunitTest,desktop_app))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,desktop_app, \
+    desktop/qa/desktop_app/test_desktop_app \
+))
+
+$(eval $(call gb_CppunitTest_use_externals,desktop_app, \
+    dbus \
+))
+
+$(eval $(call gb_CppunitTest_use_libraries,desktop_app, \
+    comphelper \
+    cppu \
+    cppuhelper \
+    deploymentmisc \
+    editeng \
+    i18nlangtag \
+    sal \
+    salhelper \
+    sb \
+    sfx \
+    svl \
+    svxcore \
+    svt \
+    tk \
+    tl \
+    utl \
+    vcl \
+))
+
+$(eval $(call gb_CppunitTest_use_library_objects,desktop_app, \
+    sofficeapp \
+))
+
+$(eval $(call gb_CppunitTest_use_external,desktop_app,boost_headers))
+
+$(eval $(call gb_CppunitTest_use_sdk_api,desktop_app))
+
+# vim: set noet sw=4 ts=4:
