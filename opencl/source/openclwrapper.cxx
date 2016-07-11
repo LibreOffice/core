87ed55a10f7c610c7bb3a9154c1dec7159fa6b8e
Michael Meeks <michael.meeks@collabora.com>
2016-07-11 15:12:38 +0100

    desktop: validate OpenCL drivers before use.
    OpenCL validation needs to happen before drivers are used in
    anger. This should isolate any crashes, and/or mis-behavior to
    We use app version, CL driver version and file time-stamp to
    trigger re-testing the device. If anything fails: hard disable
    OpenCL.
    
    We use an opencl validation sheet (cl-test.ods) and install it.
    It is a minimal CL set - it requires a very short formula group
    length, and combines several CL functions into few formulae to
    test more.
    
    The sheet structure, in particular the manual squaring / SQRT is
    necessary to stick within the default CL subset, and ensure that
    formulae are CL enabled from the root of the dependency tree up.
    
    Reviewed-on: https://gerrit.libreoffice.org/27131
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Michael Meeks <michael.meeks@collabora.com>
    (cherry picked from commit c44726c48228d9c6a5960e302b1c0bd16b0099c4)
    
    + opencl: bail out early in missing OpenCL case.
    (cherry picked from commit 605a5dc088385ad21c33028d8107125c0316ddb1)
    
    + Remove bogus dependency from opencl to configmgr
    Since f41eb66302208f384a475fb20c98b6d1b0676cb6 "opencl: OpenCLZone, detect CL
    device change and disable CL on crash" vcl links against opencl (so indirectly
    linked against configmgr), which caused CppunitTest_configmgr_unit to include
    the configmgr object files both statically (through
    gb_CppunitTest_use_library_objects) and through the linked-in configmgr dynamic
    library, which in turn caused ASan builds to report an ODR violation for a
    doubly defined 'typeinfo name for configmgr::Access'.
    
    (cherry picked from commit 9c711f05fa10dc70e4257a1f48d43f539353541a)
    
    Change-Id: I18682dbdf9a8ba9c16d52bad4447e9acce97f0a3
    Reviewed-on: https://gerrit.libreoffice.org/27141
    Reviewed-by: Michael Meeks <michael.meeks@collabora.com>
    Tested-by: Michael Meeks <michael.meeks@collabora.com>

--- opencl/source/openclwrapper.cxx
+++ opencl/source/openclwrapper.cxx
@@ -268,6 +268,8 @@ bool initOpenCLAttr( OpenCLEnv * env )
     return false;
 }
 
+}
+
 void releaseOpenCLEnv( GPUEnv *gpuInfo )
 {
     OpenCLZone zone;
@@ -297,6 +299,8 @@ void releaseOpenCLEnv( GPUEnv *gpuInfo )
 
     return;
 }
+
+namespace {
 
 bool buildProgram(const char* buildOption, GPUEnv* gpuInfo, int idx)
 {
@@ -695,7 +699,7 @@ void findDeviceInfoFromDeviceId(cl_device_id aDeviceId, size_t& rDeviceId, size_
 
 bool switchOpenCLDevice(const OUString* pDevice, bool bAutoSelect, bool bForceEvaluation, OUString& rOutSelectedDeviceVersionIDString)
 {
-    if(fillOpenCLInfo().empty())
+    if(fillOpenCLInfo().empty() || getenv("SAL_DISABLE_OPENCL"))
         return false;
 
     cl_device_id pDeviceId = nullptr;
