c5caed66544c11596ba190722873ed0591d7d251
Michael Meeks <michael.meeks@collabora.com>
2016-07-11 15:12:38 +0100

    desktop: validate OpenCL drivers before use.
    OpenCL validation needs to happen before drivers are used in
    anger. This should isolate any crashes, and/or mis-behavior to
    We use app version, CL driver version and file time-stamp to
    trigger re-testing the device. If anything fails: hard disable
    OpenCL.
    
    We use an opencl validation sheet (cl-test.ods) and install it.
    It is a minimal CL set - it requires a very short formula group
    length, and combines several CL functions into few formulae to
    test more.
    
    The sheet structure, in particular the manual squaring / SQRT - is
    necessary to stick within the default CL subset, and ensure that
    formulae are CL enabled from the root of the dependency tree up.
    
    Change-Id: I18682dbdf9a8ba9c16d52bad4447e9acce97f0a3

--- Repository.mk
+++ Repository.mk
@@ -837,6 +837,7 @@ $(eval $(call gb_Helper_register_packages_for_install,ooo,\
 		Pyuno/mailmerge \
 	)) \
 	sfx2_classification \
+    $(if $(filter OPENCL,$(BUILD_TYPE)),sc_opencl_runtimetest) \
 ))
 
 $(eval $(call gb_Helper_register_packages_for_install,ogltrans,\
