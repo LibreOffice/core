3e9adb92c5ecb3817787e2f6693f02014e61247f
Michael Meeks <michael.meeks@collabora.com>
2016-07-12 19:39:33 +0100

    tdf#100883 - opencl impls. that use SEH are still bad.
    Amazingly we fell-back to the old calculation path for
    crashes in older LibreOffices.
    
    Change-Id: Ia182f7a25c5560b68494d5cdd68e02925bfd5845

--- sc/source/core/opencl/formulagroupcl.cxx
+++ sc/source/core/opencl/formulagroupcl.cxx
@@ -4053,6 +4053,7 @@ DynamicKernel* DynamicKernel::create( const ScCalcConfig& rConfig, ScTokenArray&
     catch (...)
     {
         SAL_WARN("sc.opencl", "Dynamic formula compiler: unhandled compiler error");
+        ::opencl::kernelFailures++;
         return nullptr;
     }
     return pDynamicKernel;
@@ -4163,21 +4164,25 @@ public:
         catch (const UnhandledToken& ut)
         {
             SAL_WARN("sc.opencl", "Dynamic formula compiler: unhandled token: " << ut.mMessage << " at " << ut.mFile << ":" << ut.mLineNumber);
+            ::opencl::kernelFailures++;
             return CLInterpreterResult();
         }
         catch (const OpenCLError& oce)
         {
             SAL_WARN("sc.opencl", "Dynamic formula compiler: OpenCL error from " << oce.mFunction << ": " << ::opencl::errorString(oce.mError) << " at " << oce.mFile << ":" << oce.mLineNumber);
+            ::opencl::kernelFailures++;
             return CLInterpreterResult();
         }
         catch (const Unhandled& uh)
         {
             SAL_WARN("sc.opencl", "Dynamic formula compiler: unhandled case at " << uh.mFile << ":" << uh.mLineNumber);
+            ::opencl::kernelFailures++;
             return CLInterpreterResult();
         }
         catch (...)
         {
             SAL_WARN("sc.opencl", "Dynamic formula compiler: unhandled compiler error");
+            ::opencl::kernelFailures++;
             return CLInterpreterResult();
         }
 
