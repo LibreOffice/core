cc49daa85de86e7dec71f7289533d9c977c0bdd2
Henry Castro <hcastro@collabora.com>
2016-07-14 10:59:47 -0400

    sc lok: add more toolbar items
    .uno:SortAscending
    .uno:SortDescending
    
    Change-Id: I3d307b43fb18ffce75b00a0b919b63320bf6db05
    Reviewed-on: https://gerrit.libreoffice.org/27223
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Henry Castro <hcastro@collabora.com>

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -1018,7 +1018,9 @@ static void doc_iniUnoCommands ()
         OUString(".uno:ToggleMergeCells"),
         OUString(".uno:NumberFormatCurrency"),
         OUString(".uno:NumberFormatPercent"),
-        OUString(".uno:NumberFormatDate")
+        OUString(".uno:NumberFormatDate"),
+        OUString(".uno:SortAscending"),
+        OUString(".uno:SortDescending")
     };
 
     util::URL aCommandURL;
