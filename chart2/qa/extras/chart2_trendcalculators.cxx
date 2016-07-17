c46d12d77e990a0d47ffd8532e9054d3f299adc7
Laurent Balland-Poirier <laurent.balland-poirier@laposte.net>
2016-07-17 15:10:51 +0200

    Improve polynomial trendline equation representation
    Insert a blank between x variable and its coefficient
    Aplly also to linear trendline
    
    Change-Id: I7d1e61f12aa1cf98b4181df6a4849df1e5293793

--- chart2/qa/extras/chart2_trendcalculators.cxx
+++ chart2/qa/extras/chart2_trendcalculators.cxx
@@ -152,7 +152,7 @@ void Chart2TrendCalculators::testLinearRegression1()
         xValues[i] = d;
         yValues[i] = - 2.0 * d - 5.0 ;
     }
-    checkCalculator( xValues, yValues, "f(x) = "+ OUString(aMinusSign) +" 2x "+ OUString(aMinusSign) +" 5");
+    checkCalculator( xValues, yValues, "f(x) = "+ OUString(aMinusSign) +" 2 x "+ OUString(aMinusSign) +" 5");
 }
 
 // test y = A x ^ B
@@ -168,7 +168,7 @@ void Chart2TrendCalculators::testPolynomialRegression1()
         xValues[i] = d;
         yValues[i] =  - 2.0 * d * d + 4 * d - 5;
     }
-    OUString sExpectedFormula( "f(x) = "+ OUString(aMinusSign) +" 2x" + OUString( aSuperscriptFigures[2] ) + " + 4x "+ OUString(aMinusSign) +" 5" );
+    OUString sExpectedFormula( "f(x) = "+ OUString(aMinusSign) +" 2 x" + OUString( aSuperscriptFigures[2] ) + " + 4 x "+ OUString(aMinusSign) +" 5" );
     checkCalculator( xValues, yValues, sExpectedFormula );
 }
 
