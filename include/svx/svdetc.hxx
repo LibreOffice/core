d2e4753c3f511cfc6b2932ce60d0bc2e09296f9f
tymyjan <tymyjan@yahoo.co.uk>
2016-07-19 22:40:29 +0200

    tdf#75280 Cleaning up of sal_uIntPtr usage #4a
    Change-Id: I488baed0ffc3fea9c42bad1647a034cd5e9b28bb
    Reviewed-on: https://gerrit.libreoffice.org/27325
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Michael Stahl <mstahl@redhat.com>

--- include/svx/svdetc.hxx
+++ include/svx/svdetc.hxx
@@ -65,7 +65,7 @@ class SVX_DLLPUBLIC SdrEngineDefaults
 {
 friend class SdrAttrObj;
     Color            aFontColor;
-    sal_uIntPtr      nFontHeight;
+    size_t           nFontHeight;
     Fraction         aMapFraction;
 
 private:
@@ -81,7 +81,7 @@ public:
     // see below for further details). The default setting 847/100mm corresponds to about
     // 24 Point. If e.g. one would use Twips (SetMapUnit(MAP_TWIP)) (20 Twip = 1 Point)
     // instead, one would need to set the font height to 480, in order to get a 24 Point height.
-    static sal_uIntPtr      GetFontHeight()                      { return GetDefaults().nFontHeight; }
+    static size_t      GetFontHeight()                      { return GetDefaults().nFontHeight; }
 
     // The MapMode is needed for the global Outliner.
     // Incidentally, every newly instantiated SdrModel is assigned this MapMode by default.
@@ -127,44 +127,44 @@ sal_uInt16* RemoveWhichRange(const sal_uInt16* pOldWhichTable, sal_uInt16 nRange
 class SVX_DLLPUBLIC SvdProgressInfo
 {
 private:
-    sal_uIntPtr m_nSumActionCount; // Sum of all Actions
-    sal_uIntPtr m_nSumCurAction;   // Sum of all handled Actions
+    size_t      m_nSumActionCount; // Sum of all Actions
+    size_t      m_nSumCurAction;   // Sum of all handled Actions
 
-    sal_uIntPtr m_nActionCount;   // Count of Actions in the current object
-    sal_uIntPtr m_nCurAction;     // Count of handled Actions in the current object
+    size_t      m_nActionCount;   // Count of Actions in the current object
+    size_t      m_nCurAction;     // Count of handled Actions in the current object
 
-    sal_uIntPtr m_nInsertCount;   // Count of to-be-inserted Actions in the current object
-    sal_uIntPtr m_nCurInsert;     // Count of already inserted Actions
+    size_t      m_nInsertCount;   // Count of to-be-inserted Actions in the current object
+    size_t      m_nCurInsert;     // Count of already inserted Actions
 
-    sal_uIntPtr m_nObjCount;      // Count of selected objects
-    sal_uIntPtr m_nCurObj;        // Current object
+    size_t      m_nObjCount;      // Count of selected objects
+    size_t      m_nCurObj;        // Current object
 
     Link<void*,bool>  maLink;
 
 public:
     SvdProgressInfo( const Link<void*,bool>& _pLink );
 
-    void Init( sal_uIntPtr _nSumActionCount, sal_uIntPtr _nObjCount );
+    void Init( size_t _nSumActionCount, size_t _nObjCount );
 
     void SetNextObject();
 
-    void SetActionCount( sal_uIntPtr _nActionCount );
-    void SetInsertCount( sal_uIntPtr _nInsertCount );
+    void SetActionCount( size_t _nActionCount );
+    void SetInsertCount( size_t _nInsertCount );
 
-    bool ReportActions( sal_uIntPtr nActionCount );
-    void ReportInserts( sal_uIntPtr nInsertCount );
+    bool ReportActions( size_t nActionCount );
+    void ReportInserts( size_t nInsertCount );
 
-    sal_uIntPtr GetSumCurAction() const { return m_nSumCurAction; };
-    sal_uIntPtr GetObjCount() const { return m_nObjCount; };
-    sal_uIntPtr GetCurObj() const { return m_nCurObj; };
+    size_t      GetSumCurAction() const { return m_nSumCurAction; };
+    size_t      GetObjCount() const { return m_nObjCount; };
+    size_t      GetCurObj() const { return m_nCurObj; };
 
-    sal_uIntPtr GetActionCount() const { return m_nActionCount; };
-    sal_uIntPtr GetCurAction() const { return m_nCurAction; };
+    size_t      GetActionCount() const { return m_nActionCount; };
+    size_t      GetCurAction() const { return m_nCurAction; };
 
-    sal_uIntPtr GetInsertCount() const { return m_nInsertCount; };
-    sal_uIntPtr GetCurInsert() const { return m_nCurInsert; };
+    size_t      GetInsertCount() const { return m_nInsertCount; };
+    size_t      GetCurInsert() const { return m_nCurInsert; };
 
-    void ReportRescales( sal_uIntPtr nRescaleCount );
+    void ReportRescales( size_t nRescaleCount );
 };
 
 
