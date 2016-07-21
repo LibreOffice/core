71aad51b24832a2d0441a12bfa10995a4b4955d2
Caolán McNamara <caolanm@redhat.com>
2016-07-21 21:44:06 +0100

    Resolves: tdf#101054 crash on options->path with non-native file picker
    regression from...
    
    commit 3bbc0574d78d129359638b74612de2f93419eeb0
    Author: Caolán McNamara <caolanm@redhat.com>
    Date:   Tue Jul 5 10:16:51 2016 +0100
    
        Resolves: rhbz#1352835 path options doesn't promptly destroy folder picker
    
    only clear the xFolderPicker immediately in the non-async case
    
    Change-Id: Id6a411424d41262a9379ad3eb7976c8dd666a5cf
    (cherry picked from commit c1bd3156cf66318023f36d81ce809a38072588e4)

--- cui/source/options/optpath.cxx
+++ cui/source/options/optpath.cxx
@@ -730,7 +730,6 @@ IMPL_LINK_TYPED( SvxPathTabPage, DialogClosedHdl, DialogClosedEvent*, pEvt, void
         OUString sURL = xFolderPicker->getDirectory();
         ChangeCurrentEntry( sURL );
     }
-    xFolderPicker.clear();
 }
 
 void SvxPathTabPage::GetPathList(
