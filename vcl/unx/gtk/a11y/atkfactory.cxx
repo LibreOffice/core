27328034f5e2644b4a0287532e762e87ea36c4a0
Caolán McNamara <caolanm@redhat.com>
2016-07-21 11:55:13 +0100

    gtk3-a11y: our eventbox is inside a grid now
    Change-Id: Icb49d6274b09fb426ab6c0f9f67b7c71c4ef9a31

--- vcl/unx/gtk/a11y/atkfactory.cxx
+++ vcl/unx/gtk/a11y/atkfactory.cxx
@@ -109,7 +109,11 @@ wrapper_factory_create_accessible( GObject *obj )
     if (!pEventBox)
         return atk_noop_object_wrapper_new();
 
-    GtkWidget* pTopLevel = gtk_widget_get_parent(pEventBox);
+    GtkWidget* pTopLevelGrid = gtk_widget_get_parent(pEventBox);
+    if (!pTopLevelGrid)
+        return atk_noop_object_wrapper_new();
+
+    GtkWidget* pTopLevel = gtk_widget_get_parent(pTopLevelGrid);
     if (!pTopLevel)
         return atk_noop_object_wrapper_new();
 #else
