ebe04c075649c677b55941e3b4e57fbca150b5db
Caolán McNamara <caolanm@redhat.com>
2016-07-27 21:28:59 +0100

    use div() instead of /0 to content ubsan
    for the same reason as...
    
    commit fb2c146752074b132d665e40343a08dcb2e6672e
    Author: Eike Rathke <erack@redhat.com>
    Date:   Mon Jul 25 13:32:17 2016 +0200
    
        use div() instead of /0 to content ubsan
    
        While we do handle double floating point division by 0 at least on
        IEEE754 systems, ubsan builds mock about.
    
    Change-Id: Id2e34686b053707c83ca535689b693d80b606533

--- sc/source/core/tool/interpr1.cxx
+++ sc/source/core/tool/interpr1.cxx
@@ -1695,7 +1695,7 @@ void ScInterpreter::ScTan()
 
 void ScInterpreter::ScCot()
 {
-    PushDouble( div( 1.0, ::rtl::math::tan(GetDouble())));
+    PushDouble(div(1.0, ::rtl::math::tan(GetDouble())));
 }
 
 void ScInterpreter::ScArcSin()
@@ -1735,7 +1735,7 @@ void ScInterpreter::ScTanHyp()
 
 void ScInterpreter::ScCotHyp()
 {
-    PushDouble(1.0 / tanh(GetDouble()));
+    PushDouble(div(1.0, tanh(GetDouble())));
 }
 
 void ScInterpreter::ScArcSinHyp()
