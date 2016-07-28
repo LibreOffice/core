2e87d50c7b74fba30bd00c5bc9cb3359018f70e9
Caolán McNamara <caolanm@redhat.com>
2016-07-28 13:49:13 +0100

    fftester: empty table manager stack
    Change-Id: Ia7f7ace8130d5dfe290207e0cd3d2e6a43b8ab46
    (cherry picked from commit df9414084b46c1712dc7151f50023438c62301e2)
    Reviewed-on: https://gerrit.libreoffice.org/27633
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
    (cherry picked from commit 3f320c21d27c409aa086d044502acf42f60a1a36)

--- writerfilter/source/dmapper/DomainMapper.cxx
+++ writerfilter/source/dmapper/DomainMapper.cxx
@@ -3068,6 +3068,9 @@ void DomainMapper::lcl_utext(const sal_uInt8 * data_, size_t len)
         return;
     }
 
+    if (!m_pImpl->hasTableManager())
+        return;
+
     try
     {
         m_pImpl->getTableManager().utext(data_, len);
