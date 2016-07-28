ff4217e9e4f90a8dd09e635a35665a64846b4505
Takeshi Abe <tabe@fixedpoint.jp>
2016-07-28 09:03:59 +0900

    tdf#101022 Export Greek symbol to MathML with correct mathvariant
    In StarMath notation "{ital %GAMMA}" is recognized as italic, and
    "{nitalic %iGAMMA}" is non-italic. That is, the ital/nitalic directive
    takes priority over special characters' flavor.
    On the other hand, in MathML a mathvariant attribute given in <mi>
    overwrites inherited value.
    
    This does not handle "bold-italic" case etc. yet.
    
    Change-Id: I9c72dc4472f8cec553417d516d9d82aebd43d15c
    Reviewed-on: https://gerrit.libreoffice.org/27604
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>

---
+++ starmath/CppunitTest_starmath_export.mk
@@ -0,0 +1,89 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_CppunitTest_CppunitTest,starmath_export))
+
+$(eval $(call gb_CppunitTest_set_include,starmath_export,\
+    -I$(SRCDIR)/starmath/inc \
+    $$(INCLUDE) \
+))
+
+$(eval $(call gb_CppunitTest_use_externals,starmath_export,\
+    boost_headers \
+    libxml2 \
+))
+
+$(eval $(call gb_CppunitTest_use_sdk_api,starmath_export))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,starmath_export,\
+    starmath/qa/extras/mmlexport-test \
+))
+
+$(eval $(call gb_CppunitTest_use_libraries,starmath_export,\
+    comphelper \
+    cppu \
+    cppuhelper \
+    editeng \
+    i18nlangtag \
+    i18nutil \
+    msfilter \
+    oox \
+    sal \
+    salhelper \
+    sax \
+    sfx \
+    sm \
+    smd \
+    sot \
+    svl \
+    svt \
+    svx \
+    svxcore \
+    test \
+    tk \
+    tl \
+    unotest \
+    unoxml \
+    utl \
+    vcl \
+    xo \
+    $(gb_UWINAPI) \
+))
+
+$(eval $(call gb_CppunitTest_use_ure,starmath_export))
+$(eval $(call gb_CppunitTest_use_vcl,starmath_export))
+
+$(eval $(call gb_CppunitTest_use_components,starmath_export,\
+    configmgr/source/configmgr \
+    framework/util/fwk \
+    i18npool/util/i18npool \
+    package/source/xstor/xstor \
+    package/util/package2 \
+    sfx2/util/sfx \
+    starmath/util/sm \
+    starmath/util/smd \
+    toolkit/util/tk \
+    ucb/source/core/ucb1 \
+    ucb/source/ucp/file/ucpfile1 \
+    unotools/util/utl \
+    comphelper/util/comphelp \
+    filter/source/config/cache/filterconfig1 \
+    oox/util/oox \
+    sax/source/expatwrap/expwrap \
+    svl/source/fsstor/fsstorage \
+    svl/util/svl \
+    svx/util/svx \
+    unoxml/source/service/unoxml \
+    uui/util/uui \
+    xmloff/util/xo \
+))
+
+$(eval $(call gb_CppunitTest_use_configuration,starmath_export))
+
+# vim: set noet sw=4 ts=4:
--- starmath/Module_starmath.mk
+++ starmath/Module_starmath.mk
@@ -21,6 +21,7 @@ $(eval $(call gb_Module_add_l10n_targets,starmath,\
 ))
 
 $(eval $(call gb_Module_add_check_targets,starmath,\
+    CppunitTest_starmath_export \
     CppunitTest_starmath_import \
     CppunitTest_starmath_qa_cppunit \
 ))
