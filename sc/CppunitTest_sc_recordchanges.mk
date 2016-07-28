7e3925057a97b33727c1245007e639daef156a79
Caolán McNamara <caolanm@redhat.com>
2016-07-28 15:05:56 +0100

    add some uui component dependencies
    Change-Id: Ie191d66d3902281ccf7962d025ecc60e48e20cba

--- sc/CppunitTest_sc_recordchanges.mk
+++ sc/CppunitTest_sc_recordchanges.mk
@@ -96,6 +96,7 @@ $(eval $(call gb_CppunitTest_use_components,sc_recordchanges,\
     unotools/util/utl \
     unoxml/source/rdf/unordf \
     unoxml/source/service/unoxml \
+    uui/util/uui \
     xmloff/util/xo \
 	svtools/util/svt \
 ))
