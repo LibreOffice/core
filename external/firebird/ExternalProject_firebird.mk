0f6da9dd3fe578cf65169a1103b517b0e3a7f2ff
Wastack <btomi96@gmail.com>
2016-07-28 17:16:19 +0200

    WiP GSoC Upgrade firebird to 3.0
    External libtommath added. Does not support MSVC 14.0
    
    Change-Id: I132939bdee745795b22f675e4265e9590079c45f

--- external/firebird/ExternalProject_firebird.mk
+++ external/firebird/ExternalProject_firebird.mk
@@ -15,14 +15,12 @@ $(eval $(call gb_ExternalProject_use_externals,firebird,\
 	boost_headers \
 	icu \
 	libatomic_ops \
+	libtommath \
 ))
 
 $(eval $(call gb_ExternalProject_register_targets,firebird,\
 	build \
 ))
-
-# note: this can intentionally only build against internal atomic_op
-# note: this can intentionally only build against internal tommath
 
 ifneq ($(OS),WNT)
 INVOKE_FPA:="CPU=\$$(EMPTY) $${FB_CPU_ARG}"
@@ -46,23 +44,38 @@ $(call gb_ExternalProject_get_state_target,firebird,build):
 			$(if $(SYSTEM_LIBATOMIC_OPS),$(LIBATOMIC_OPS_CFLAGS), \
 				-I$(call gb_UnpackedTarball_get_dir,libatomic_ops)/src \
 			) \
+			$(if $(SYSTEM_LIBTOMMATH),$(LIBTOMMATH_CFLAGS), \
+				-I$(call gb_UnpackedTarball_get_dir,libtommath) \
+				-L$(call gb_UnpackedTarball_get_dir,libtommath) \
+			) \
+			$(if $(filter WNT,$(OS)), \
+				$(if $(SYSTEM_ICU),$(ICU_CPPFLAGS), \
+					-I$(call gb_UnpackedTarball_get_dir,icu)/source \
+					-I$(call gb_UnpackedTarball_get_dir,icu)/source/i18n \
+					-I$(call gb_UnpackedTarball_get_dir,icu)/source/common \
+					-L$(call gb_UnpackedTarball_get_dir,icu)/source/lib \
+				)) \
 			" \
 		&& export CXXFLAGS=" \
 			$(if $(SYSTEM_BOOST),$(BOOST_CPPFLAGS), \
 				$(BOOST_CPPFLAGS) \
 				-L$(call gb_UnpackedTarball_get_dir,boost)/source/lib \
 			) \
-			$(if $(SYSTEM_ICU),$(ICU_CPPFLAGS), \
-				-I$(call gb_UnpackedTarball_get_dir,icu)/source \
-				-I$(call gb_UnpackedTarball_get_dir,icu)/source/i18n \
-				-I$(call gb_UnpackedTarball_get_dir,icu)/source/common \
-				-L$(call gb_UnpackedTarball_get_dir,icu)/source/lib \
+			$(if $(filter-out WNT,$(OS)), \
+				$(if $(SYSTEM_ICU),$(ICU_CPPFLAGS), \
+					-I$(call gb_UnpackedTarball_get_dir,icu)/source \
+					-I$(call gb_UnpackedTarball_get_dir,icu)/source/i18n \
+					-I$(call gb_UnpackedTarball_get_dir,icu)/source/common \
+					-L$(call gb_UnpackedTarball_get_dir,icu)/source/lib \
+				)) \
+			$(if $(SYSTEM_LIBTOMMATH),$(LIBTOMMATH_CFLAGS), \
+				-L$(call gb_UnpackedTarball_get_dir,libtommath) \
 			) \
-			" \
+		" \
 		&& MAKE=$(MAKE) ./configure \
 			--without-editline \
-			--disable-superserver \
-			--with-system-icu --without-fbsample --without-fbsample-db \
+			--without-fbsample --without-fbsample-db \
+			--with-wire-compress=no \
 			$(if $(filter-out MSC,$(COM)),$(if $(ENABLE_DEBUG),--enable-debug)) \
 			$(if $(CROSS_COMPILING),--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM)) \
 			$(if $(DISABLE_DYNLOADING), \
@@ -71,9 +84,9 @@ $(call gb_ExternalProject_get_state_target,firebird,build):
 				--enable-shared --disable-static \
 			) \
 		&& if [ -n "$${FB_CPU_ARG}" ]; then \
-			   $(MAKE_PRE) $(MAKE) $(INVOKE_FPA) SHELL='$(SHELL)' firebird_embedded $(MAKE_POST); \
+			   $(MAKE_PRE) $(MAKE) $(INVOKE_FPA) SHELL='$(SHELL)' $(MAKE_POST); \
 			else \
-			   $(MAKE_PRE) $(MAKE) SHELL='$(SHELL)' firebird_embedded $(MAKE_POST); \
+			   $(MAKE_PRE) $(MAKE) SHELL='$(SHELL)' $(MAKE_POST); \
 			fi \
 	)
 # vim: set noet sw=4 ts=4:
