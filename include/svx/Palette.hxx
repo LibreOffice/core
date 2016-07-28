684cc1868292576a2620259f839c9cf52bf48a7c
Rishabh Kumar <kris.kr296@gmail.com>
2016-07-28 12:53:03 +0530

    [GSoC] Rework of color tab
    New Features -
    
    1. Multiple Palettes in color tab.
    2. Remember the selected palette.
    
    Change-Id: I36a438a0c282059ddcbda35f934fcd90337fd451

--- include/svx/Palette.hxx
+++ include/svx/Palette.hxx
@@ -29,7 +29,7 @@ class SvFileStream;
 typedef std::pair<Color, OUString> NamedColor;
 typedef std::vector< NamedColor > ColorList;
 
-class Palette
+class SVX_DLLPUBLIC Palette
 {
 public:
     virtual ~Palette();
@@ -43,7 +43,7 @@ public:
 
 // ASE = Adobe Swatch Exchange
 
-class PaletteASE : public Palette
+class SVX_DLLPUBLIC PaletteASE : public Palette
 {
     bool        mbValidPalette;
     OUString    maFPath;
@@ -64,7 +64,7 @@ public:
 
 // GPL - this is *not* GNU Public License, but is the Gimp PaLette
 
-class PaletteGPL : public Palette
+class SVX_DLLPUBLIC PaletteGPL : public Palette
 {
     bool        mbLoadedPalette;
     bool        mbValidPalette;
@@ -89,7 +89,7 @@ public:
 
 // SOC - Star Office Color-table
 
-class PaletteSOC : public Palette
+class SVX_DLLPUBLIC PaletteSOC : public Palette
 {
     bool            mbLoadedPalette;
     OUString        maFPath;
