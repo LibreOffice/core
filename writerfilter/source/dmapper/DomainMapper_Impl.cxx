d00fc303bca7765762a602531b7d3b40ce8f1740
Caolán McNamara <caolanm@redhat.com>
2016-07-29 21:24:26 +0100

    fftester: another empty table manager stack
    Change-Id: If3148cb6e16cff4aad28c4f86467c66ed04bcd05

--- writerfilter/source/dmapper/DomainMapper_Impl.cxx
+++ writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -2013,8 +2013,11 @@ void DomainMapper_Impl::UpdateEmbeddedShapeProps(const uno::Reference< drawing::
 
 void DomainMapper_Impl::PopShapeContext()
 {
-    getTableManager().endLevel();
-    popTableManager();
+    if (hasTableManager())
+    {
+        getTableManager().endLevel();
+        popTableManager();
+    }
     if ( m_aAnchoredStack.size() > 0 )
     {
         // For OLE object replacement shape, the text append context was already removed
