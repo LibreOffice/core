78fc2dd3e85cef9d826a9c9c2e8dc02ab761debe
Caolán McNamara <caolanm@redhat.com>
2016-07-29 08:48:22 +0100

    fftester: null DestinationText
    Change-Id: Icdd71733c4e9f4b36e6e957e4dea772087890faa
    (cherry picked from commit 0dd22165346f4296718656d17ae98ebbfe563719)

--- writerfilter/source/rtftok/rtfdocumentimpl.cxx
+++ writerfilter/source/rtftok/rtfdocumentimpl.cxx
@@ -1212,7 +1212,7 @@ void RTFDocumentImpl::text(OUString& rString)
             rString = rString.copy(0, rString.getLength() - 1);
             bEnd = true;
         }
-        m_aStates.top().pDestinationText->append(rString);
+        m_aStates.top().appendDestinationText(rString);
         if (bEnd)
         {
             // always clear, necessary in case of group-less fonttable
@@ -1318,7 +1318,7 @@ void RTFDocumentImpl::text(OUString& rString)
     case Destination::TOCENTRY:
     case Destination::PROPNAME:
     case Destination::STATICVAL:
-        m_aStates.top().pDestinationText->append(rString);
+        m_aStates.top().appendDestinationText(rString);
         break;
     default:
         bRet = false;
@@ -1347,7 +1347,7 @@ void RTFDocumentImpl::text(OUString& rString)
     // Don't return earlier, a bookmark start has to be in a paragraph group.
     if (m_aStates.top().eDestination == Destination::BOOKMARKSTART)
     {
-        m_aStates.top().pDestinationText->append(rString);
+        m_aStates.top().appendDestinationText(rString);
         return;
     }
 
