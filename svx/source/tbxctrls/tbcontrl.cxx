0cd576e5d62132ce6f012a21dcc5db96b0436ee6
Rishabh Kumar <kris.kr296@gmail.com>
2016-07-31 11:40:38 +0530

    [GSoC] Rework of color tab
    New Features -
    
    1. Multiple Palettes in color tab.
    2. Remember the selected palette.
    3. Recent colors.
    4. Custom colors.
    
    Change-Id: I36a438a0c282059ddcbda35f934fcd90337fd451

--- svx/source/tbxctrls/tbcontrl.cxx
+++ svx/source/tbxctrls/tbcontrl.cxx
@@ -96,6 +96,8 @@
 #include <svx/xflclit.hxx>
 #include <svl/currencytable.hxx>
 #include <svtools/langtab.hxx>
+#include <cppu/unotype.hxx>
+#include <officecfg/Office/Common.hxx>
 
 #define MAX_MRU_FONTNAME_ENTRIES    5
 
@@ -1331,7 +1333,8 @@ SvxColorWindow_Impl::SvxColorWindow_Impl( const OUString&            rCommand,
     {
         mpPaletteListBox->InsertEntry( *it );
     }
-    mpPaletteListBox->SelectEntry(mrPaletteManager.GetPaletteName());
+    sal_Int32 nPos = officecfg::Office::Common::UserColors::PalettePosition::get();
+    mpPaletteListBox->SelectEntryPos( nPos );
     SelectPaletteHdl( *mpPaletteListBox );
 
     mpButtonAutoColor->SetClickHdl( LINK( this, SvxColorWindow_Impl, AutoColorClickHdl ) );
