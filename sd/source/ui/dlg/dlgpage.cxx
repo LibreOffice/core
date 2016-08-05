5693ee7a41fbe4de337cef3b10294edd670be804
Rishabh Kumar <kris.kr296@gmail.com>
2016-08-05 17:57:49 +0530

    [GSoC] Add Transparency tab to background dialog
    Change-Id: I33dffe2435c44116842240e4b481947415317b5b
    Reviewed-on: https://gerrit.libreoffice.org/27906
    Reviewed-by: Yousuf Philips <philipz85@hotmail.com>
    Tested-by: Yousuf Philips <philipz85@hotmail.com>
    Reviewed-by: Rishabh Kumar <kris.kr296@yahoo.in>

--- sd/source/ui/dlg/dlgpage.cxx
+++ sd/source/ui/dlg/dlgpage.cxx
@@ -62,9 +62,13 @@ SdPageDlg::SdPageDlg( SfxObjectShell* pDocSh, vcl::Window* pParent, const SfxIte
 
     mnPage = AddTabPage( "RID_SVXPAGE_PAGE", pFact->GetTabPageCreatorFunc( RID_SVXPAGE_PAGE ), nullptr );
     mnArea = AddTabPage( "RID_SVXPAGE_AREA", pFact->GetTabPageCreatorFunc( RID_SVXPAGE_AREA ), nullptr );
+    mnTransparence = AddTabPage( "RID_SVXPAGE_TRANSPARENCE", pFact->GetTabPageCreatorFunc( RID_SVXPAGE_TRANSPARENCE ), nullptr );
 
     if(!bAreaPage)  // I have to add the page before I remove it !
+    {
         RemoveTabPage( "RID_SVXPAGE_AREA" );
+        RemoveTabPage( "RID_SVXPAGE_TRANSPARENCE" );
+    }
 }
 
 void SdPageDlg::PageCreated(sal_uInt16 nId, SfxTabPage& rPage)
@@ -87,6 +91,12 @@ void SdPageDlg::PageCreated(sal_uInt16 nId, SfxTabPage& rPage)
         aSet.Put (SfxUInt16Item(SID_PAGE_TYPE,0));
         aSet.Put (SfxUInt16Item(SID_DLG_TYPE,1));
         aSet.Put (SfxUInt16Item(SID_TABPAGE_POS,0));
+        rPage.PageCreated(aSet);
+    }
+    else if (nId == mnTransparence)
+    {
+        aSet.Put(SfxUInt16Item(SID_PAGE_TYPE,0));
+        aSet.Put(SfxUInt16Item(SID_DLG_TYPE,1));
         rPage.PageCreated(aSet);
     }
 }
