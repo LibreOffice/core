4a17cb9addab51b73ea095bb6b885b5cada3958b
Mike Kaganski <mike.kaganski@collabora.com>
2016-08-08 21:29:09 +1000

    tdf#101385: don't try to get SharePoint folder outside of allowed range
    for a given site.
    This patch uses the fact that getFolderByServerRelativeUrl API returns
    site's root folder if used with empty path ('').
    Hopefully this isn't otherwise context-dependent, so that it doesn't
    return other folders in some unknown circumstances.
    
    More correct would be instead to use the site's root path directly,
    like '/sites/mysite'. But I don't know a reliable way to filter the
    API part from the base URI.
    
    To make use of the corrected SharePoint repository root id, the patch
    also redirects all cases where "/" object is queried, to session's
    getRootFolder().
    
    libcmis part of the patch will hopefully soon be merged upstream.
    
    Change-Id: I0f3b9930748de3fe4763eb27721739f097b9892d
    Reviewed-on: https://gerrit.libreoffice.org/27987
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: David Tardon <dtardon@redhat.com>
    (cherry picked from commit 1b482aec4e7b6aec86fb20bcda555c4060a80332)
    Reviewed-on: https://gerrit.libreoffice.org/28011
    Reviewed-by: Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>

--- external/libcmis/UnpackedTarball_cmis.mk
+++ external/libcmis/UnpackedTarball_cmis.mk
@@ -17,6 +17,7 @@ $(eval $(call gb_UnpackedTarball_add_patches,cmis, \
 						external/libcmis/libcmis-libxml2_compatibility.patch \
 						external/libcmis/libcmis-fix-google-drive.patch \
 						external/libcmis/libcmis-google-2FA-implementation.patch \
+						external/libcmis/libcmis-sharepoint-repository-root.patch \
 ))
 
 ifeq ($(OS)$(COM),WNTMSC)
