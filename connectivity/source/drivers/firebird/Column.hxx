0a9123152387f7a742481e9f35401270e29ed695
Wastack <btomi96@gmail.com>
2016-08-11 20:03:18 +0200

    tdf#69949 GSoC Firebird implement autoincrement
    Change-Id: I6fe08b575f01b986f0a3c96b341f254279427b68
    Reviewed-on: https://gerrit.libreoffice.org/28062
    Reviewed-by: Lionel Elie Mamane <lionel@mamane.lu>
    Tested-by: Jenkins <ci@libreoffice.org>

---
+++ connectivity/source/drivers/firebird/Column.hxx
@@ -0,0 +1,37 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+#ifndef INCLUDED_CONNECTIVITY_SOURCE_DRIVERS_FIREBIRD_HCOLUMN_HXX
+#define INCLUDED_CONNECTIVITY_SOURCE_DRIVERS_FIREBIRD_HCOLUMN_HXX
+#include <connectivity/TColumnsHelper.hxx>
+#include <connectivity/sdbcx/VColumn.hxx>
+
+namespace connectivity
+{
+    namespace firebird
+    {
+        class Column;
+        typedef sdbcx::OColumn Column_BASE;
+        typedef ::comphelper::OIdPropertyArrayUsageHelper<Column> Column_PROP;
+        class Column : public Column_BASE,
+                           public Column_PROP
+        {
+            OUString m_sAutoIncrement;
+        protected:
+            virtual ::cppu::IPropertyArrayHelper* createArrayHelper( sal_Int32 _nId) const override;
+            virtual ::cppu::IPropertyArrayHelper & SAL_CALL getInfoHelper() override;
+        public:
+            Column();
+            virtual void construct() override;
+            virtual css::uno::Sequence< OUString > SAL_CALL getSupportedServiceNames(  ) throw(css::uno::RuntimeException, std::exception) override;
+        };
+    }
+}
+#endif // INCLUDED_CONNECTIVITY_SOURCE_INC_HSQLDB_HCOLUMNS_HXX
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
--- connectivity/source/drivers/firebird/Tables.hxx
+++ connectivity/source/drivers/firebird/Tables.hxx
@@ -29,6 +29,8 @@ namespace connectivity
             css::uno::Reference< css::sdbc::XDatabaseMetaData >
                 m_xMetaData;
 
+            static OUString createStandardColumnPart(const css::uno::Reference< css::beans::XPropertySet >& xColProp,const css::uno::Reference< com::sun::star::sdbc::XConnection>& _xConnection);
+
             // OCollection
             virtual void impl_refresh()
                 throw(css::uno::RuntimeException) override;
