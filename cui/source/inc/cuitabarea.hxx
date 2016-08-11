4533b8695e172ed56944012bf009e5fbc46f747c
Rishabh Kumar <kris.kr296@gmail.com>
2016-08-12 04:22:28 +0530

    [GSoC] Add new Bitmap controls to bitmap tab
    Change-Id: I0eb59e5082da1efa52dcb3b85f6e4129aad3e676

--- cui/source/inc/cuitabarea.hxx
+++ cui/source/inc/cuitabarea.hxx
@@ -31,9 +31,9 @@
 #include <svx/hexcolorcontrol.hxx>
 #include <svx/SvxColorValueSet.hxx>
 #include <svx/SvxPresetListBox.hxx>
+#include <svx/svdview.hxx>
 
 class SdrModel;
-class SdrView;
 class SvxBitmapCtl;
 
 /************************************************************************/
@@ -532,9 +532,22 @@ class SvxBitmapTabPage : public SvxTabPage
 {
     using TabPage::ActivatePage;
     using TabPage::DeactivatePage;
+    static const sal_uInt16 pBitmapRanges[];
 private:
 
     VclPtr<SvxPresetListBox>   m_pBitmapLB;
+    VclPtr<ListBox>            m_pBitmapStyleLB;
+    VclPtr<VclBox>             m_pSizeBox;
+    VclPtr<MetricField>        m_pBitmapWidth;
+    VclPtr<MetricField>        m_pBitmapHeight;
+    VclPtr<VclBox>             m_pPositionBox;
+    VclPtr<ListBox>            m_pPositionLB;
+    VclPtr<VclBox>             m_pPositionOffBox;
+    VclPtr<MetricField>        m_pPositionOffX;
+    VclPtr<MetricField>        m_pPositionOffY;
+    VclPtr<VclBox>             m_pTileOffBox;
+    VclPtr<ListBox>            m_pTileOffLB;
+    VclPtr<MetricField>        m_pTileOffset;
     VclPtr<PushButton>         m_pBtnImport;
     VclPtr<SvxXRectPreview>    m_pCtlBitmapPreview;
 
@@ -545,6 +558,8 @@ private:
     XFillStyleItem             m_aXFStyleItem;
     XFillBitmapItem            m_aXBitmapItem;
 
+    double                     m_nObjectWidth;
+    double                     m_nObjectHeight;
     sal_uInt16*                m_nPageType;
     sal_uInt16                 m_nDlgType;
     sal_Int32*                 m_nPos;
@@ -553,12 +568,23 @@ private:
 
     XFillAttrSetItem           m_aXFillAttr;
     SfxItemSet&                m_rXFSet;
-
+    const SdrView*             mpView;
+    SfxMapUnit                 mePoolUnit;
+    FieldUnit                  meDlgUnit;
+    Size                       rBitmapSize;
+    Size                       rFilledSize;
+    Size                       rZoomedSize;
     DECL_LINK_TYPED( ModifyBitmapHdl, ValueSet*, void );
+    DECL_LINK_TYPED( ModifyBitmapStyleHdl, ListBox&, void );
+    DECL_LINK_TYPED( ModifyBitmapSizeHdl, Edit&, void );
+    DECL_LINK_TYPED( ModifyBitmapPositionHdl, ListBox&, void );
+    DECL_LINK_TYPED( ModifyPositionOffsetHdl, Edit&, void );
+    DECL_LINK_TYPED( ModifyTileOffsetHdl, Edit&, void );
     DECL_LINK_TYPED( ClickRenameHdl, SvxPresetListBox*, void );
     DECL_LINK_TYPED( ClickDeleteHdl, SvxPresetListBox*, void );
     DECL_LINK_TYPED( ClickImportHdl, Button*, void );
     void ClickBitmapHdl_Impl();
+    void CalculateBitmapPresetSize();
     sal_Int32 SearchBitmapList(const OUString& rBitmapName);
 
 public:
@@ -569,6 +595,7 @@ public:
     void    Construct();
 
     static VclPtr<SfxTabPage> Create( vcl::Window*, const SfxItemSet* );
+    static const sal_uInt16* GetRanges() { return pBitmapRanges; }
 
     virtual bool FillItemSet( SfxItemSet* ) override;
     virtual void Reset( const SfxItemSet * ) override;
