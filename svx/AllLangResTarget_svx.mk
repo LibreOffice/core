a2e4bb69ab89c541d4cd20a5f4af8d2ab36c3244
Susobhan Ghosh <susobhang70@gmail.com>
2016-08-13 19:23:54 +0530

    Use SpacingListbox in sw
    Change-Id: I80ea84b6eaabcf241b313d46695cffdd74174c08

--- svx/AllLangResTarget_svx.mk
+++ svx/AllLangResTarget_svx.mk
@@ -49,6 +49,7 @@ $(eval $(call gb_SrsTarget_add_files,svx/res,\
     svx/source/dialog/passwd.src \
     svx/source/dialog/prtqry.src \
     svx/source/dialog/ruler.src \
+    svx/source/dialog/spacing.src \
     svx/source/dialog/sdstring.src \
     svx/source/dialog/srchdlg.src \
     svx/source/dialog/svxbmpnumvalueset.src \
