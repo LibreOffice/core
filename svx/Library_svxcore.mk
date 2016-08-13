6f437466f96868cb10187c404ed84fb6d3dcc645
Susobhan Ghosh <susobhang70@gmail.com>
2016-08-13 19:23:54 +0530

    tdf#83830: Use SpacingListBox in Header/Footer and Sidebar Wrap Panel
    Change-Id: I80ea84b6eaabcf241b313d46695cffdd74174c08
    Reviewed-on: https://gerrit.libreoffice.org/28104
    Reviewed-by: Katarina Behrens <Katarina.Behrens@cib.de>
    Tested-by: Katarina Behrens <Katarina.Behrens@cib.de>

--- svx/Library_svxcore.mk
+++ svx/Library_svxcore.mk
@@ -120,6 +120,7 @@ $(eval $(call gb_Library_add_exception_objects,svxcore,\
     svx/source/dialog/langbox \
     svx/source/dialog/pagenumberlistbox \
     svx/source/dialog/papersizelistbox \
+    svx/source/dialog/spacinglistbox \
     svx/source/dialog/stddlg \
     svx/source/dialog/svxdlg \
     svx/source/dialog/SvxNumOptionsTabPageHelper \
