93de7661211d19f6fff201bff5625413a453258d
Arnaud Versini <arnaud.versini@gmail.com>
2016-08-15 15:51:18 +0200

    basic: Simplify SbxArray
    Change-Id: Idcc1e35d0a1d80591e2cebdae37a70cf029b022e
    Reviewed-on: https://gerrit.libreoffice.org/28147
    Reviewed-by: Noel Grandin <noelgrandin@gmail.com>
    Tested-by: Noel Grandin <noelgrandin@gmail.com>

--- include/basic/sbx.hxx
+++ include/basic/sbx.hxx
@@ -118,15 +118,13 @@ struct SbxVarEntry;
 
 class BASIC_DLLPUBLIC SbxArray : public SbxBase
 {
-    typedef std::vector<SbxVarEntry> VarEntriesType;
-
 // #100883 Method to set method directly to parameter array
     friend class SbMethod;
     friend class SbClassModuleObject;
     friend SbxObject* cloneTypeObjectImpl( const SbxObject& rTypeObj );
     BASIC_DLLPRIVATE void PutDirect( SbxVariable* pVar, sal_uInt32 nIdx );
 
-    VarEntriesType* mpVarEntries;          // The variables
+    std::vector<SbxVarEntry> mVarEntries;          // The variables
 
 protected:
     SbxDataType eType;            // Data type of the array
