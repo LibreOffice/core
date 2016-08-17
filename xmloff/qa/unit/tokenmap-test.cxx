6c0b64f8a54ec0b219e9af9dc179f23b51b09f9e
Mohammed Abdul Azeem <azeemmysore@gmail.com>
2016-08-17 09:03:18 +0530

    GSoC - Keeping both token lists in xmloff/ consistent:
    Added dummy tokens to keep the lists consistent and
    a test case to check the same.
    
    Change-Id: Ia58f1f77a5100a9fb3c7226fcad1fef1bd5aec39

--- xmloff/qa/unit/tokenmap-test.cxx
+++ xmloff/qa/unit/tokenmap-test.cxx
@@ -14,6 +14,7 @@
 
 #include "xmloff/fasttokenhandler.hxx"
 #include "xmloff/token/tokens.hxx"
+#include <xmloff/xmltoken.hxx>
 
 using namespace std;
 using namespace com::sun::star::uno;
@@ -27,10 +28,13 @@ public:
     TokenmapTest();
 
     void test_roundTrip();
+    void test_listEquality();
 
     CPPUNIT_TEST_SUITE(TokenmapTest);
 
     CPPUNIT_TEST(test_roundTrip);
+    CPPUNIT_TEST(test_listEquality);
+
     CPPUNIT_TEST_SUITE_END();
 
 private:
@@ -52,6 +56,32 @@ void TokenmapTest::test_roundTrip()
         CPPUNIT_ASSERT_MESSAGE("Token name sequence array pointer failed", pChar);
         sal_Int32 ret = token::TokenMap::getTokenFromUTF8( pChar, rUtf8Name.getLength() );
         CPPUNIT_ASSERT_EQUAL_MESSAGE("No roundtrip for token", ret, nToken);
+    }
+}
+
+void TokenmapTest::test_listEquality()
+{
+    //make sure the two token lists stay in sync
+    for ( sal_Int32 nToken = 0; nToken < XML_TOKEN_COUNT; ++nToken )
+    {
+        Sequence< sal_Int8 > rUtf8Name = pTokenMap->getUtf8TokenName(nToken);
+        const OUString& rName = OUString( reinterpret_cast< const char* >(
+                        rUtf8Name.getConstArray() ), rUtf8Name.getLength(), RTL_TEXTENCODING_UTF8 );
+        if ( rName.endsWith("_DUMMY") )
+            continue;
+        const OUString& rTokenName = GetXMLToken( static_cast<xmloff::token::XMLTokenEnum>(nToken) );
+        CPPUNIT_ASSERT_EQUAL(rName, rTokenName);
+    }
+
+    for ( sal_Int32 nToken = xmloff::token::XMLTokenEnum::XML_TOKEN_START + 1;
+            nToken < xmloff::token::XMLTokenEnum::XML_TOKEN_END; ++nToken )
+    {
+        const OUString& rTokenName = GetXMLToken( static_cast<xmloff::token::XMLTokenEnum>(nToken) );
+        Sequence< sal_Int8 > rUtf8Name = pTokenMap->getUtf8TokenName(nToken);
+        const OUString& rName = OUString( reinterpret_cast< const char* >(
+                        rUtf8Name.getConstArray() ), rUtf8Name.getLength(), RTL_TEXTENCODING_UTF8 );
+        if ( !rName.endsWith("_DUMMY") )
+            CPPUNIT_ASSERT_EQUAL(rTokenName, rName);
     }
 }
 
