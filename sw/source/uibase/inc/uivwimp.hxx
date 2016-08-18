fd590397606e944e9a2974a2c62759436a5a5b20
Miklos Vajna <vmiklos@collabora.co.uk>
2016-08-18 10:01:32 +0200

    sw redlining: support per-view authors
    In case there are multiple SwView instances and SetRedlineAuthor() is
    called with a non-empty string on them, switching views will keep the
    SwModule redline author string up to date as expected.
    
    (cherry picked from commit cb9362faad9fe702031c5e657a31b1963ad4d374)
    
    Conflicts:
    	sw/source/uibase/uno/unotxdoc.cxx
    
    Change-Id: I363221049dbacd67d7c8f4ff3e778f8032a3bc43

--- sw/source/uibase/inc/uivwimp.hxx
+++ sw/source/uibase/inc/uivwimp.hxx
@@ -120,6 +120,9 @@ class SwView_Impl
     bool                        m_bEditingPositionSet;
 
 public:
+    /// Redline author that's specific to this view.
+    OUString m_sRedlineAuthor;
+
     SwView_Impl(SwView* pShell);
     ~SwView_Impl();
 
