c4f99f35f40ba59fdab476a2483497a31dc68a24
Markus Mohrhard <markus.mohrhard@googlemail.com>
2016-08-19 16:39:23 +0200

    add files for starting windows process
    Change-Id: Idb2d6a43c8bc86a51fb8e5b937784bedc05a0308

---
+++ onlineupdate/StaticLibrary_winhelper.mk
@@ -0,0 +1,22 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_StaticLibrary_StaticLibrary,winhelper))
+
+$(eval $(call gb_StaticLibrary_set_include,winhelper,\
+	-I$(SRCDIR)/onlineupdate/inc/ \
+	-I$(SRCDIR)/onlineupdate/source/winhelper/ \
+	$$(INCLUDE) \
+))
+
+$(eval $(call gb_StaticLibrary_add_cxxobjects,winhelper,\
+	onlineupdate/source/winhelper/windowsStart \
+))
+
+# vim:set shiftwidth=4 tabstop=4 noexpandtab: */
