016a79ef1a3010e665ed57cebc5126e18cb366aa
Markus Mohrhard <markus.mohrhard@googlemail.com>
2016-08-19 16:39:23 +0200

    add files for starting windows process
    Change-Id: Idb2d6a43c8bc86a51fb8e5b937784bedc05a0308

---
+++ onlineupdate/inc/winhelper/windowsStart.hxx
@@ -0,0 +1,18 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifdef XP_WIN
+#include <windows.h>
+#else
+#include <limits.h>
+#endif
+
+
+BOOL
+WinLaunchChild(const wchar_t *exePath, int argc,
+               char **argv, HANDLE userToken = nullptr,
+               HANDLE *hProcess = nullptr);
+
+wchar_t* MakeCommandLine(int argc, WCHAR **argv);
