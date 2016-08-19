6ffd51f91ae8a12af7eebba94c6087db222ff3e1
Markus Mohrhard <markus.mohrhard@googlemail.com>
2016-08-19 14:36:02 +0200

    start work on the updater service
    Change-Id: Ic0b98c859cf2115195a75e9521c35ef32bd8b833

---
+++ onlineupdate/Executable_update_service.mk
@@ -0,0 +1,32 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_Executable_Executable,update_service))
+
+$(eval $(call gb_Executable_set_include,update_service,\
+	-I$(SRCDIR)/onlineupdate/source/libmar/src/ \
+	-I$(SRCDIR)/onlineupdate/source/libmar/verify/ \
+	-I$(SRCDIR)/onlineupdate/source/libmar/sign/ \
+	$$(INCLUDE) \
+))
+
+$(eval $(call gb_Executable_add_libs,update_service,\
+    ws2_32.lib \
+    Crypt32.lib \
+))
+
+$(eval $(call gb_Executable_add_cxxobjects,update_service,\
+	onlineupdate/source/libmar/sign/nss_secutil \
+	onlineupdate/source/libmar/sign/mar_sign \
+	onlineupdate/source/libmar/verify/cryptox \
+	onlineupdate/source/libmar/verify/mar_verify \
+	onlineupdate/source/libmar/tool/mar \
+))
+
+# vim:set shiftwidth=4 tabstop=4 noexpandtab: */
