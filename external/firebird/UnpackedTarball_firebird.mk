e73dc86924ad33a1522c7e4a284a66dabf54c160
Stephan Bergmann <sbergman@redhat.com>
2016-08-19 15:02:52 +0200

    Why not just drop that $(HOME)/icu54/icu/source/lib nonsense
    Change-Id: I2fed34ae680eb0bc01de90a7ef525cf8446cd940

--- external/firebird/UnpackedTarball_firebird.mk
+++ external/firebird/UnpackedTarball_firebird.mk
@@ -20,6 +20,7 @@ $(eval $(call gb_UnpackedTarball_add_patches,firebird,\
 		external/firebird/firebird-cloop-compiler.patch.1 \
 		external/firebird/firebird-gcc6.patch.1 \
 		external/firebird/wnt-dbgutil.patch \
+		external/firebird/libc++.patch \
 ))
 
 ifeq ($(OS)-$(COM),WNT-MSC)
