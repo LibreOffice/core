751db1f2bffc7059650048c7137161873b1e9c88
Markus Mohrhard <markus.mohrhard@googlemail.com>
2016-08-20 13:35:06 +0200

    add initial version of running the updater
    Change-Id: If21aa08ec4f535d79e6f5cbe6fc1a10626752bea

---
+++ desktop/source/app/updater.hxx
@@ -0,0 +1,19 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef INCLUDED_DESKTOP_SOURCE_APP_UPDATER_HXX
+#define INCLUDED_DESKTOP_SOURCE_APP_UPDATER_HXX
+
+#include <rtl/ustring.hxx>
+
+void Update(const OUString& rInstallDir);
+
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
