a5b29a702e9102bf85d90a40d33cde81cafda45a
Jochen Nitschke <j.nitschke+logerrit@ok.de>
2016-08-25 17:58:38 +0200

    openldap: upgrade to release 2.4.44
    Change-Id: I9af1bedd06410bed17b37adc6213125e0c4f1f29
    Reviewed-on: https://gerrit.libreoffice.org/28393
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Michael Stahl <mstahl@redhat.com>

--- external/openldap/UnpackedTarball_openldap.mk
+++ external/openldap/UnpackedTarball_openldap.mk
@@ -12,7 +12,7 @@ $(eval $(call gb_UnpackedTarball_UnpackedTarball,openldap))
 $(eval $(call gb_UnpackedTarball_set_tarball,openldap,$(OPENLDAP_TARBALL),,openldap))
 
 $(eval $(call gb_UnpackedTarball_add_patches,openldap,\
-	external/openldap/openldap-2.4.31.patch \
+	external/openldap/openldap-2.4.44.patch.1 \
 ))
 
 # vim: set noet sw=4 ts=4:
