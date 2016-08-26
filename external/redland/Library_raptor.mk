acede375dd48fc59910ac6dbfedfc8da5db1a4d9
Michael Stahl <mstahl@redhat.com>
2016-08-26 21:33:16 +0200

    redland: upgrade raptor to 2.0.15, rasqal to 0.9.33, redland to 1.0.17:
    - drop rasqal-pkgconfig.patch.1:
      merged upstream
    - drop most of rasqal-msvc.patch.1:
      merged upstream
    - drop redland-query-rasqal-avoid-assert.patch.1:
      merged upstream
    - drop redland-pkgconfig.patch.1:
      merged upstream
    
    Change-Id: I4f03bed5fc5c4b30151c0c532572e438681da041

--- external/redland/Library_raptor.mk
+++ external/redland/Library_raptor.mk
@@ -35,6 +35,7 @@ $(eval $(call gb_Library_add_generated_cobjects,raptor2,\
 	UnpackedTarball/raptor/src/raptor_abbrev \
 	UnpackedTarball/raptor/src/raptor_avltree \
 	UnpackedTarball/raptor/src/raptor_concepts \
+	UnpackedTarball/raptor/src/raptor_escaped \
 	UnpackedTarball/raptor/src/raptor_general \
 	UnpackedTarball/raptor/src/raptor_guess \
 	UnpackedTarball/raptor/src/raptor_iostream \
@@ -44,6 +45,7 @@ $(eval $(call gb_Library_add_generated_cobjects,raptor2,\
 	UnpackedTarball/raptor/src/raptor_log \
 	UnpackedTarball/raptor/src/raptor_memstr \
 	UnpackedTarball/raptor/src/raptor_namespace \
+	UnpackedTarball/raptor/src/raptor_ntriples \
 	UnpackedTarball/raptor/src/raptor_option \
 	UnpackedTarball/raptor/src/raptor_parse \
 	UnpackedTarball/raptor/src/raptor_qname \
@@ -67,6 +69,7 @@ $(eval $(call gb_Library_add_generated_cobjects,raptor2,\
 	UnpackedTarball/raptor/src/raptor_xml \
 	UnpackedTarball/raptor/src/raptor_xml_writer \
 	UnpackedTarball/raptor/src/snprintf \
+	UnpackedTarball/raptor/src/sort_r \
 	UnpackedTarball/raptor/src/strcasecmp \
 	UnpackedTarball/raptor/src/turtle_common \
 	UnpackedTarball/raptor/src/turtle_lexer \
