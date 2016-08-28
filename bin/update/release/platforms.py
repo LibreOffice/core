e606b4364498029200abe42d56393d4f5a6b2206
Markus Mohrhard <markus.mohrhard@googlemail.com>
2016-08-28 16:13:24 +0200

    change of plans, we are not going to use balrog
    Balrog makes a lot of assumptions about the Mozilla infrastructure that
    we can't clone. As an example the whole admin interface assumes an
    external authenication system, most likely LDAP. Also it would not
    support my plans for language pack and SDK handling.
    
    We will copy some of the ideas of Balrog but will need an own simple
    webservice for that.
    
    Change-Id: I2e31f3ef7887b19edcd248ddbd02a1965f71b3e3

--- bin/update/release/platforms.py
+++
@@ -1,8 +0,0 @@
-def buildbot2bouncer():
-    pass
-
-def buildbot2updatePlatforms(platform):
-    return platform
-
-def buildbot2ftp():
-    pass
--- bin/update/release/versions.py
+++
@@ -1,2 +0,0 @@
-def getPrettyVersion(version):
-    return version
