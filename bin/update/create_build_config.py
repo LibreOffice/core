a2a62b3385c01467317922caa34dfda7d66e182b
Markus Mohrhard <markus.mohrhard@googlemail.com>
2016-08-28 18:09:02 +0200

    also include the platform into the build config
    Change-Id: Ia586e813b33dde0e0b2468c54bed9471c03e8c5a

--- bin/update/create_build_config.py
+++ bin/update/create_build_config.py
@@ -5,16 +5,17 @@ import sys
 import os
 
 def main(argv):
-    if len(argv) < 6:
-        print("Usage: create_build_config.py $PRODUCTNAME $VERSION $BUILDID $UPDATECHANNEL $TARGETDIR")
+    if len(argv) < 7:
+        print("Usage: create_build_config.py $PRODUCTNAME $VERSION $BUILDID $UPDATECHANNEL $PLATFORM $TARGETDIR")
 
     data = { 'productName' : argv[1],
             'version' : argv[2],
             'buildNumber' : argv[3],
-            'updateChannel' : argv[4]
+            'updateChannel' : argv[4],
+            'platform' : argv[5]
             }
 
-    with open(os.path.join(argv[5], "build_config.json"), "w") as f:
+    with open(os.path.join(argv[6], "build_config.json"), "w") as f:
         json.dump(data, f, indent=4)
 
 if __name__ == "__main__":
