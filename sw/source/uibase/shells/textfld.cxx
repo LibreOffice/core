6ab48f1b2d2f41f631154a79092985a6ed987b75
Miklos Vajna <vmiklos@collabora.co.uk>
2016-08-29 17:43:15 +0200

    sw: use SwModule::GetRedlineAuthor() in SwTextShell::ExecField()
    This avoids some code duplication, and also means that the redline
    author set by SwDocShell::SetView() affects the inserted Writer comments
    as well, while those were 'Unknown Author' in the LOK case.
    
    Change-Id: Ib51183302ee6904fdf69fb16f27ecfe6df39e6cb

--- sw/source/uibase/shells/textfld.cxx
+++ sw/source/uibase/shells/textfld.cxx
@@ -70,6 +70,7 @@
 #include <PostItMgr.hxx>
 #include <calbck.hxx>
 #include <memory>
+#include <swmodule.hxx>
 
 using namespace nsSwDocInfoSubType;
 
@@ -381,10 +382,8 @@ void SwTextShell::ExecField(SfxRequest &rReq)
                         sAuthor = pAuthorItem->GetValue();
                     else
                     {
-                        SvtUserOptions aUserOpt;
-                        if( (sAuthor = aUserOpt.GetFullName()).isEmpty())
-                            if( (sAuthor = aUserOpt.GetID()).isEmpty() )
-                                sAuthor = SW_RES( STR_REDLINE_UNKNOWN_AUTHOR );
+                        sal_uInt16 nAuthor = SW_MOD()->GetRedlineAuthor();
+                        sAuthor = SW_MOD()->GetRedlineAuthor(nAuthor);
                     }
 
                     const SvxPostItTextItem* pTextItem = rReq.GetArg<SvxPostItTextItem>(SID_ATTR_POSTIT_TEXT);
