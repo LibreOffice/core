d9daa30f404e1f8ef9070d06a4f334c5cb185106
Markus Mohrhard <markus.mohrhard@googlemail.com>
2016-08-30 05:19:06 +0200

    improve the build system part for generating mar files
    Change-Id: If28cca535da20e4633ce025e39a967820eae6945

---
+++ bin/update/create_full_mar.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import subprocess
+import json
+
+from tools import uncompress_file_to_dir, get_file_info
+
+current_dir_path = os.path.dirname(os.path.realpath(__file__))
+
+def make_mar_name(target_dir, filename_prefix):
+    filename = filename_prefix + "_complete.mar"
+    return os.path.join(target_dir, filename)
+
+def main():
+    print(sys.argv)
+    if len(sys.argv) < 8:
+        print("Usage: create_full_mar_for_languages.py $PRODUCTNAME $WORKDIR $TARGETDIR $TEMPDIR $FILENAMEPREFIX")
+        sys.exit(1)
+
+    certificate_path = sys.argv[7]
+    certificate_name = sys.argv[6]
+    filename_prefix = sys.argv[5]
+    temp_dir = sys.argv[4]
+    target_dir = sys.argv[3]
+    workdir = sys.argv[2]
+    product_name = sys.argv[1]
+
+    tar_dir = os.path.join(workdir, "installation", product_name, "archive", "install", "en-US")
+    tar_file = os.path.join(tar_dir, os.listdir(tar_dir)[0])
+
+    uncompress_dir = uncompress_file_to_dir(tar_file, temp_dir)
+
+    # on linux we should stip the symbols from the libraries
+    if sys.platform.startswith('linux'):
+        subprocess.call('strip -g ' + os.path.join(uncompress_dir, 'program/') + '*', shell=True)
+
+    mar_executable = os.environ.get('MAR', 'mar')
+    mar_file = make_mar_name(target_dir, filename_prefix)
+    subprocess.call([os.path.join(current_dir_path, 'make_full_update.sh'), mar_file, uncompress_dir])
+
+    subprocess.call([mar_executable, '-C', target_dir, '-d', certificate_path, '-n', certificate_name, '-s', mar_file, make_mar_name(target_dir, filename_prefix + '_signed')])
+
+    file_info = { 'complete' : get_file_info(mar_file) }
+
+    with open(os.path.join(target_dir, 'complete_info.json'), "w") as complete_info_file:
+        json.dump(file_info, complete_info_file, indent = 4)
+
+if __name__ == '__main__':
+    main()
---
+++ bin/update/create_full_mar_for_languages.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import subprocess
+import json
+
+from tools import uncompress_file_to_dir, get_file_info
+
+current_dir_path = os.path.dirname(os.path.realpath(__file__))
+
+def make_complete_mar_name(target_dir, filename_prefix, language):
+    filename = filename_prefix + "_" + language + "_complete_langpack.mar"
+    return os.path.join(target_dir, filename)
+
+def create_lang_infos(mar_file_name, language):
+    data = {'lang' : language,
+            'complete' : get_file_info(mar_file_name)
+            }
+    return data
+
+def main():
+    print(sys.argv)
+    if len(sys.argv) < 8:
+        print("Usage: create_full_mar_for_languages.py $PRODUCTNAME $WORKDIR $TARGETDIR $TEMPDIR $FILENAMEPREFIX")
+        sys.exit(1)
+
+    certificate_path = sys.argv[7]
+    certificate_name = sys.argv[6]
+    filename_prefix = sys.argv[5]
+    temp_dir = sys.argv[4]
+    target_dir = sys.argv[3]
+    workdir = sys.argv[2]
+    product_name = sys.argv[1]
+
+    mar_executable = os.environ.get('MAR', 'mar')
+
+    language_pack_dir = os.path.join(workdir, "installation", product_name + "_languagepack", "archive", "install")
+    language_packs = os.listdir(language_pack_dir)
+    lang_infos = []
+    for language in language_packs:
+        if language == 'log':
+            continue
+
+        language_dir = os.path.join(language_pack_dir, language)
+        language_file = os.path.join(language_dir, os.listdir(language_dir)[0])
+
+        directory = uncompress_file_to_dir(language_file, os.path.join(temp_dir, language))
+
+        mar_file_name = make_complete_mar_name(target_dir, filename_prefix, language)
+
+        subprocess.call([os.path.join(current_dir_path, 'make_full_update.sh'), mar_file_name, directory])
+        subprocess.call([mar_executable, '-C', target_dir, '-d', certificate_path, '-n', certificate_name, '-s', mar_file_name, make_complete_mar_name(target_dir, filename_prefix + '_signed', language)])
+
+        lang_infos.append(create_lang_infos(mar_file_name, language))
+
+    with open(os.path.join(target_dir, "complete_lang_info.json"), "w") as language_info_file:
+        json.dump({'languages' : lang_infos}, language_info_file, indent=4)
+
+if __name__ == '__main__':
+    main()
