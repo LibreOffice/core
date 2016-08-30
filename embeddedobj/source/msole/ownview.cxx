5d268dcbaf135e4e03c39960ccc3a22cc75f170d
Stephan Bergmann <sbergman@redhat.com>
2016-08-30 15:27:57 +0200

    loplugin:stringconstant: adapt to improved OUStringLiteral1 (embeddedobj)
    Change-Id: I4d1d29155c09b15514f96a8b53f9865eefc78704

--- embeddedobj/source/msole/ownview.cxx
+++ embeddedobj/source/msole/ownview.cxx
@@ -299,7 +299,7 @@ bool OwnView_Impl::ReadContentsAndGenerateTempFile( const uno::Reference< io::XI
                 aReadSeq[0] == '.'
                )
             {
-                aFileSuffix += OUString( (sal_Unicode) aReadSeq[0] );
+                aFileSuffix += OUStringLiteral1( aReadSeq[0] );
             }
 
         } while( aReadSeq[0] );
