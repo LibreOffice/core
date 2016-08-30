e7bbf98659260ac5756c43ec7513f1fa0833f62e
Stephan Bergmann <sbergman@redhat.com>
2016-08-30 16:19:36 +0200

    Fix placement of EXTRA_ENV_VARS
    Change-Id: Id9ae15aaf24b65abfcb4006490836eae8a1d8db6

--- solenv/gbuild/CppunitTest.mk
+++ solenv/gbuild/CppunitTest.mk
@@ -115,7 +115,8 @@ $(call gb_CppunitTest_get_target,%) :| $(gb_CppunitTest_RUNTIMEDEPS)
 		$(gb_CppunitTest_malloc_check) \
 		$(if $(strip $(PYTHON_URE)),\
 			PYTHONDONTWRITEBYTECODE=1) \
-		$(ICECREAM_RUN) $(gb_CppunitTest_GDBTRACE) $(gb_CppunitTest_VALGRINDTOOL) $(EXTRA_ENV_VARS) $(gb_CppunitTest_CPPTESTCOMMAND) \
+		$(EXTRA_ENV_VARS) \
+		$(ICECREAM_RUN) $(gb_CppunitTest_GDBTRACE) $(gb_CppunitTest_VALGRINDTOOL) $(gb_CppunitTest_CPPTESTCOMMAND) \
 		$(call gb_LinkTarget_get_target,$(call gb_CppunitTest_get_linktarget,$*)) \
 		$(call gb_CppunitTest__make_args) "-env:CPPUNITTESTTARGET=$@" \
 		$(if $(gb_CppunitTest__interactive),, \
