13714d994471a5d53b673d97e4fcf56b5dc3d057
Maxim Monastirsky <momonasmon@gmail.com>
2016-08-30 21:38:37 +0300

    Implement disabling of Menubar under Unity
    This is a combination of 5 commits:
    
    * gtk3: Implement menubar hiding
    
    * tdf#106271 Cannot disable Menubar under Unity
    
    * Destroy menu widget when enabling Unity integration
    
    * Don't use SetDisplayable when enabling Unity mode
    
    * Keep menubar hidden after Unity registrar activated
    
    Change-Id: I3c5e17100622cd8d22c4348cf90a76233dd0fd4c
    Reviewed-on: https://gerrit.libreoffice.org/35206
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>

--- vcl/unx/gtk/gtksalmenu.cxx
+++ vcl/unx/gtk/gtksalmenu.cxx
@@ -484,11 +484,7 @@ GtkSalMenu::~GtkSalMenu()
 
 bool GtkSalMenu::VisibleMenuBar()
 {
-#if GTK_CHECK_VERSION(3,0,0)
-    return mbMenuBar;
-#else
-    return mbMenuBar && bUnityMode;
-#endif
+    return mbMenuBar && (bUnityMode || mpMenuBarContainerWidget);
 }
 
 void GtkSalMenu::InsertItem( SalMenuItem* pSalMenuItem, unsigned nPos )
@@ -539,7 +535,9 @@ void GtkSalMenu::ShowCloseButton(bool bShow)
 {
 #if GTK_CHECK_VERSION(3,0,0)
     assert(mbMenuBar);
-    MenuBar *pVclMenuBar = static_cast<MenuBar*>(mpVCLMenu);
+    if (!mpMenuBarContainerWidget)
+        return;
+
     if (!bShow)
     {
         if (mpCloseButton)
@@ -547,6 +545,7 @@ void GtkSalMenu::ShowCloseButton(bool bShow)
         return;
     }
 
+    MenuBar *pVclMenuBar = static_cast<MenuBar*>(mpVCLMenu);
     mpCloseButton = gtk_button_new();
     g_signal_connect(mpCloseButton, "clicked", G_CALLBACK(CloseMenuBar), pVclMenuBar);
 
@@ -695,6 +694,9 @@ static gboolean MenuBarSignalKey(GtkWidget*, GdkEventKey* pEvent, gpointer menu)
 void GtkSalMenu::CreateMenuBarWidget()
 {
 #if GTK_CHECK_VERSION(3,0,0)
+    if (mpMenuBarContainerWidget)
+        return;
+
     GtkGrid* pGrid = mpFrame->getTopLevelGridWidget();
     mpMenuBarContainerWidget = gtk_grid_new();
 
@@ -710,6 +712,8 @@ void GtkSalMenu::CreateMenuBarWidget()
     g_signal_connect(G_OBJECT(mpMenuBarWidget), "key-press-event", G_CALLBACK(MenuBarSignalKey), this);
 
     gtk_widget_show_all(mpMenuBarContainerWidget);
+
+    ShowCloseButton( static_cast<MenuBar*>(mpVCLMenu)->HasCloseButton() );
 #else
     (void)mpMenuBarContainerWidget;
 #endif
@@ -722,6 +726,7 @@ void GtkSalMenu::DestroyMenuBarWidget()
     {
         gtk_widget_destroy(mpMenuBarContainerWidget);
         mpMenuBarContainerWidget = nullptr;
+        mpCloseButton = nullptr;
     }
 #else
     (void)mpMenuBarContainerWidget;
@@ -1087,15 +1092,41 @@ void GtkSalMenu::Deactivate(const gchar* pCommand)
 
 void GtkSalMenu::EnableUnity(bool bEnable)
 {
-    if (bUnityMode != bEnable)
+    bUnityMode = bEnable;
+
+    MenuBar* pMenuBar(static_cast<MenuBar*>(mpVCLMenu));
+    bool bDisplayable(pMenuBar->IsDisplayable());
+
+    if (bEnable)
     {
-        if (!bEnable)
-            CreateMenuBarWidget();
-        else
-            DestroyMenuBarWidget();
-        bUnityMode = bEnable;
-        static_cast<MenuBar*>(mpVCLMenu)->SetDisplayable(!bEnable);
+        DestroyMenuBarWidget();
+        UpdateFull();
+        if (!bDisplayable)
+            ShowMenuBar(false);
     }
+    else
+    {
+        Update();
+        ShowMenuBar(bDisplayable);
+    }
+
+    pMenuBar->LayoutChanged();
+}
+
+void GtkSalMenu::ShowMenuBar( bool bVisible )
+{
+    // Unity tdf#106271: Can't hide global menu, so empty it instead when user wants to hide menubar,
+    if (bUnityMode)
+    {
+        if (bVisible)
+            Update();
+        else if (mpMenuModel && g_menu_model_get_n_items(G_MENU_MODEL(mpMenuModel)) > 0)
+            g_lo_menu_remove(G_LO_MENU(mpMenuModel), 0);
+    }
+    else if (bVisible)
+        CreateMenuBarWidget();
+    else
+        DestroyMenuBarWidget();
 }
 
 bool GtkSalMenu::IsItemVisible( unsigned nPos )
