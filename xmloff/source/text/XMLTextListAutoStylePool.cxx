97dbbf7282b48eccedf60696ad65cf4e9987c7a9
David Tardon <dtardon@redhat.com>
2016-09-09 13:41:01 +0200

    avoid manual memory management
    Change-Id: Ibc4e7191e5cbc5a14c32b32bab99116173070a87

--- xmloff/source/text/XMLTextListAutoStylePool.cxx
+++ xmloff/source/text/XMLTextListAutoStylePool.cxx
@@ -17,6 +17,8 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
+#include <vector>
+
 #include <tools/debug.hxx>
 #include <tools/solar.h>
 #include <o3tl/sorted_vector.hxx>
@@ -246,14 +248,9 @@ void XMLTextListAutoStylePool::exportXML() const
     if( !nCount )
         return;
 
-    XMLTextListAutoStylePoolEntry_Impl **aExpEntries =
-        new XMLTextListAutoStylePoolEntry_Impl*[nCount];
+    std::vector<XMLTextListAutoStylePoolEntry_Impl*> aExpEntries(nCount);
 
     sal_uInt32 i;
-    for( i=0; i < nCount; i++ )
-    {
-        aExpEntries[i] = nullptr;
-    }
     for( i=0; i < nCount; i++ )
     {
         XMLTextListAutoStylePoolEntry_Impl *pEntry = (*pPool)[i];
@@ -269,7 +266,6 @@ void XMLTextListAutoStylePool::exportXML() const
         aNumRuleExp.exportNumberingRule( pEntry->GetName(), false,
                                          pEntry->GetNumRules() );
     }
-    delete [] aExpEntries;
 }
 
 
