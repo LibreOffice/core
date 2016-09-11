377a8b36a1f9a32c05bc042f31a893719ce02666
Andras Timar <andras.timar@collabora.com>
2016-09-11 14:00:50 +0200

    tdf#101949 update Source Code Pro fonts to 2.030 and 1.050
    Change-Id: Ic314f2b970ea96ec1eb027979723985138638cdb
    Reviewed-on: https://gerrit.libreoffice.org/28819
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Adolfo Jayme Barrientos <fitojb@ubuntu.com>
    (cherry picked from commit 93725af2265d1171bcad27d69db11934b2ae6de5)

--- external/more_fonts/ExternalPackage_sourcecode.mk
+++ external/more_fonts/ExternalPackage_sourcecode.mk
@@ -10,8 +10,20 @@
 $(eval $(call gb_ExternalPackage_ExternalPackage,fonts_sourcecode,font_sourcecode))
 
 $(eval $(call gb_ExternalPackage_add_unpacked_files,fonts_sourcecode,$(LIBO_SHARE_FOLDER)/fonts/truetype,\
+	SourceCodePro-BlackIt.ttf \
+	SourceCodePro-Black.ttf \
+	SourceCodePro-BoldIt.ttf \
 	SourceCodePro-Bold.ttf \
+	SourceCodePro-ExtraLightIt.ttf \
+	SourceCodePro-ExtraLight.ttf \
+	SourceCodePro-It.ttf \
+	SourceCodePro-LightIt.ttf \
+	SourceCodePro-Light.ttf \
+	SourceCodePro-MediumIt.ttf \
+	SourceCodePro-Medium.ttf \
 	SourceCodePro-Regular.ttf \
+	SourceCodePro-SemiboldIt.ttf \
+	SourceCodePro-Semibold.ttf \
 ))
 
 # vim: set noet sw=4 ts=4:
