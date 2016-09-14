ccc3edf4fc4e3a96bbeea159e9a07f01d7721c85
Marco Cecchetti <marco.cecchetti@collabora.com>
2016-09-14 10:26:55 +0200

    Revert "LOK: we use callbacks latch for not missing messages sent very early"
    This reverts commit bbae556d12e4edf7795b0b5643df77fd8cdbdacd.
    
    Change-Id: If1370751b612ff06ef5be13e8a4017af7dc34a7c

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -392,6 +392,7 @@ static void doc_getDocumentSize(LibreOfficeKitDocument* pThis,
                                 long* pHeight);
 static void doc_initializeForRendering(LibreOfficeKitDocument* pThis,
                                        const char* pArguments);
+
 static void doc_registerCallback(LibreOfficeKitDocument* pThis,
                                 LibreOfficeKitCallback pCallback,
                                 void* pData);
@@ -443,9 +444,6 @@ static unsigned char* doc_renderFont(LibreOfficeKitDocument* pThis,
                           int* pFontWidth,
                           int* pFontHeight);
 static char* doc_getPartHash(LibreOfficeKitDocument* pThis, int nPart);
-static void doc_setCallbackLatch(LibreOfficeKitDocument* pThis,
-                                 bool bCallbackLatch);
-
 
 LibLODocument_Impl::LibLODocument_Impl(const uno::Reference <css::lang::XComponent> &xComponent)
     : mxComponent(xComponent)
@@ -491,8 +489,6 @@ LibLODocument_Impl::LibLODocument_Impl(const uno::Reference <css::lang::XCompone
 
         m_pDocumentClass->renderFont = doc_renderFont;
         m_pDocumentClass->getPartHash = doc_getPartHash;
-
-        m_pDocumentClass->setCallbackLatch = doc_setCallbackLatch;
 
         gDocumentClass = m_pDocumentClass;
     }
@@ -2343,19 +2339,6 @@ unsigned char* doc_renderFont(LibreOfficeKitDocument* /*pThis*/,
         }
     }
     return nullptr;
-}
-
-static void doc_setCallbackLatch(LibreOfficeKitDocument* pThis, bool bCallbackLatch)
-{
-    SolarMutexGuard aGuard;
-    LibLODocument_Impl* pDocument = static_cast<LibLODocument_Impl*>(pThis);
-
-    int nView = SfxLokHelper::getView();
-    if (nView < 0)
-        return;
-
-    if (pDocument->mpCallbackFlushHandlers[nView])
-        pDocument->mpCallbackFlushHandlers[nView]->setEventLatch(bCallbackLatch);
 }
 
 static char* lo_getError (LibreOfficeKit *pThis)
