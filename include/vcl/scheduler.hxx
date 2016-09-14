2a28cf15b685536c879904c094d7e5f588308354
Jan-Marek Glogowski <glogow@fbihome.de>
2016-09-14 15:33:54 +0200

    Run Idle tasks immediatly
    There is really no reason to wait a millisecond for an idle.
    
    Change-Id: I7665d5f2e7d6ba3e01290a692bbc8e42c36b9986

--- include/vcl/scheduler.hxx
+++ include/vcl/scheduler.hxx
@@ -45,8 +45,8 @@ class VCL_DLLPUBLIC Scheduler final
     static void ImplStartTimer ( sal_uInt64 nMS, bool bForce, sal_uInt64 nTime );
 
 public:
-    static constexpr sal_uInt64 ImmediateTimeoutMs = 1;
-    static constexpr sal_uInt64 InfiniteTimeoutMs = 1000 * 60 * 60 * 24; // 1 day
+    static constexpr sal_uInt64 ImmediateTimeoutMs = 0;
+    static constexpr sal_uInt64 InfiniteTimeoutMs  = SAL_MAX_UINT64;
 
     static void       ImplDeInitScheduler();
 
