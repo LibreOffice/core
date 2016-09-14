92ba2749b89be74789d0179e1b52019f6a372edd
Jan-Marek Glogowski <glogow@fbihome.de>
2016-09-14 15:33:54 +0200

    Run Idle tasks immediatly
    There is really no reason to wait a millisecond for an idle.
    
    Change-Id: I7665d5f2e7d6ba3e01290a692bbc8e42c36b9986

--- vcl/source/app/scheduler.cxx
+++ vcl/source/app/scheduler.cxx
@@ -138,10 +138,6 @@ void Scheduler::ImplStartTimer(sal_uInt64 nMS, bool bForce, sal_uInt64 nTime)
         rSchedCtx.mpSalTimer->SetCallback(Scheduler::CallbackTaskScheduling);
     }
 
-    if ( nMS > InfiniteTimeoutMs )
-        nMS = InfiniteTimeoutMs;
-    if ( nMS < ImmediateTimeoutMs )
-        nMS = ImmediateTimeoutMs;
     assert(SAL_MAX_UINT64 - nMS >= nTime);
 
     sal_uInt64 nProposedTimeout = nTime + nMS;
