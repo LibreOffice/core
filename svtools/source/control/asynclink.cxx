3d118198f2ca165dd71cc1208fee08bfbc669ae5
Tor Lillqvist <tml@collabora.com>
2016-09-15 18:03:46 +0300

    Make sure some history is preserved for posterity
    Change-Id: Ie74f12ee6fda9f8bcba540d52ce7ec53236998bf

--- svtools/source/control/asynclink.cxx
+++ svtools/source/control/asynclink.cxx
@@ -37,7 +37,7 @@ void AsynchronLink::Call( void* pObj, bool bAllowDoubles )
 {
 #ifdef DBG_UTIL
     if ( !_bInCall )
-        SAL_INFO( "svtools", "Recursives Call. Eher ueber Timer. TLX Fragen" );
+        SAL_INFO( "svtools", "Recursives Call. Eher ueber Timer. TLX Fragen" ); // Do NOT translate. This is a valuable historical artefact.
 #endif
     if( _aLink.IsSet() )
     {
