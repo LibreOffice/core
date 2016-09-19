de2533cdcdce5a065cc10444c63dce81d1d5029e
Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
2016-09-19 17:05:36 +0200

    tdf#102288 TerminationVetoException should only prevent termination
    When using a TerminationVetoException, all windows should be closed,
    but the process should be kept running.
    
    Change-Id: I71b0b57b6035a36f0325c8dea3cd38309408f176

--- framework/source/services/desktop.cxx
+++ framework/source/services/desktop.cxx
@@ -219,9 +219,14 @@ sal_Bool SAL_CALL Desktop::terminate()
 
     aReadLock.clear();
 
-    // Ask normal terminate listener. They could stop terminate without closing any open document.
+    // try to close all open frames.
+    // Allow using of any UI ... because Desktop.terminate() was designed as UI functionality in the past.
+    bool bAllowUI      = true;
+    bool bFramesClosed = impl_closeFrames(bAllowUI);
+
+    // Ask normal terminate listener. They could stop terminating the process.
     Desktop::TTerminateListenerList lCalledTerminationListener;
-    bool                      bVeto = false;
+    bool bVeto = false;
     impl_sendQueryTerminationEvent(lCalledTerminationListener, bVeto);
     if ( bVeto )
     {
@@ -229,10 +234,6 @@ sal_Bool SAL_CALL Desktop::terminate()
         return sal_False;
     }
 
-    // try to close all open frames.
-    // Allow using of any UI ... because Desktop.terminate() was designed as UI functionality in the past.
-    bool bAllowUI      = true;
-    bool bFramesClosed = impl_closeFrames(bAllowUI);
     if ( ! bFramesClosed )
     {
         impl_sendCancelTerminationEvent(lCalledTerminationListener);
