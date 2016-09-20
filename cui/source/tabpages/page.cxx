LibreOffice/core

commit 992fba98f4d0b6ff0d20c15b0ddff09edd37847c

ubsan: 9 is not a valid value for type ´SvxNumType´
which was originally fixed by...

commit d29b75c402ea635b3865501e43c9f349885913af
Author: Miklos Vajna <vmiklos@collabora.co.uk>
Date:   Wed Mar 23 10:35:14 2016 +0100

    svx: extend SvxNumType with BITMAP and CHARS_UPPER_LETTER_N

but was lost again with...

commit d30a4298bdb5ba53cd1fe659f2b742f218a2e527
Author: Noel Grandin <noel@peralex.com>
Date:   Thu Aug 11 15:02:19 2016 +0200

    loplugin:unusedenumconstants in package..svtools

bring it back, but make it more obvious that the elements map
to the othere sequence of numbers.

Maybe we should just change this sal_Int16 and let it take the
whole range ?

Change-Id: Ibfa548c1d37aa078fc682530372727776e45f9cf

include/svx/pageitem.hxx
@@ -21,17 +21,20 @@
 
 #include <svl/poolitem.hxx>
 #include <svx/svxdllapi.h>
-
+#include <com/sun/star/style/NumberingType.hpp>
 
 enum SvxNumType
 {
-    SVX_CHARS_UPPER_LETTER,
-    SVX_CHARS_LOWER_LETTER,
-    SVX_ROMAN_UPPER,
-    SVX_ROMAN_LOWER,
-    SVX_ARABIC,
-    SVX_NUMBER_NONE,
-    SVX_PAGEDESC,
+    SVX_CHARS_UPPER_LETTER = css::style::NumberingType::CHARS_UPPER_LETTER,
+    SVX_CHARS_LOWER_LETTER = css::style::NumberingType::CHARS_LOWER_LETTER,
+    SVX_ROMAN_UPPER = css::style::NumberingType::ROMAN_UPPER,
+    SVX_ROMAN_LOWER = css::style::NumberingType::ROMAN_LOWER,
+    SVX_ARABIC = css::style::NumberingType::ARABIC,
+    SVX_NUMBER_NONE = css::style::NumberingType::NUMBER_NONE,
+    SVX_CHAR_SPECIAL = css::style::NumberingType::CHAR_SPECIAL,
+    SVX_PAGEDESC = css::style::NumberingType::PAGE_DESCRIPTOR,
+    SVX_BITMAP = css::style::NumberingType::BITMAP,
+    SVX_CHARS_UPPER_LETTER_N = css::style::NumberingType::CHARS_UPPER_LETTER_N
 };
 
 /*--------------------------------------------------------------------

commit 5e0edd5fafbc8696fe75d9518a93b59969418c43

Fix "ubsan: 9 is not a valid value for type ´SvxNumType´" once again
...after 1658bd231bc662504a072097dc614c29fe2e116d "loplugin:unusedenumvalues"
broke it again after 992fba98f4d0b6ff0d20c15b0ddff09edd37847c "ubsan: 9 is not a
valid value for type ´SvxNumType´" had fixed it the last time.

This time, acknowledge that SvxNumType can apparently take on rather arbitrary
sal_Int16 values from the css::style::NumberingTypes constants, and make it an
alias for sal_Int16 (whether or not keeping such a trivial alias is good or bad
overall).

Or is the bug elsewhere, and values of type SvxNumType should really only take
on the restricted set of values originally encoded by the enum?  At least the
below UBSan backtrace (from CppunitTest_sw_rtfexport) suggests otherwise, but
then again there's also an SvxExtNumType enum (include/editeng/svxenum.hxx),
encompassing more parts of css::style::NumberingTypes.  Oh, my.

> svx/source/items/pageitem.cxx:49:25: runtime error: load of value 9, which is not a valid value for type 'SvxNumType'
>  #0 0x2aafdaebe669 in SvxPageItem::SvxPageItem(SvxPageItem const&) svx/source/items/pageitem.cxx:49:25
>  #1 0x2aafdaebf194 in SvxPageItem::Clone(SfxItemPool*) const svx/source/items/pageitem.cxx:59:16
>  #2 0x2aaf7fab9b7c in SfxItemPool::Put(SfxPoolItem const&, unsigned short) svl/source/items/itempool.cxx:632:40
>  #3 0x2aaf7fc4794a in SfxItemSet::Put(SfxPoolItem const&, unsigned short) svl/source/items/itemset.cxx:467:56
>  #4 0x2aaf7fb96635 in SfxItemSet::Put(SfxPoolItem const&) include/svl/itemset.hxx:131:42
>  #5 0x2aaf7fb89539 in SfxItemPropertySet::setPropertyValue(SfxItemPropertySimpleEntry const&, com::sun::star::uno::Any const&, SfxItemSet&) const svl/source/items/itemprop.cxx:256:14
>  #6 0x2aaf9ef1a5d4 in void SwXStyle::SetPropertyValue<(unsigned short)1>(SfxItemPropertySimpleEntry const&, SfxItemPropertySet const&, com::sun::star::uno::Any const&, SwStyleBase_Impl&) sw/source/core/unocore/unostyle.cxx:1596:14

cui/source/tabpages/page.cxx
@@ -466,7 +466,7 @@ void SvxPageDescPage::Reset( const SfxItemSet* rSet )
     }
 
     // general page data
-    SvxNumType eNumType = SVX_ARABIC;
+    SvxNumType eNumType = css::style::NumberingType::ARABIC;
     bLandscape = ( mpDefPrinter->GetOrientation() == Orientation::Landscape );
     sal_uInt16 nUse = (sal_uInt16)SVX_PAGE_ALL;
     pItem = GetItem( *rSet, SID_ATTR_PAGE );
@@ -805,10 +805,10 @@ bool SvxPageDescPage::FillItemSet( SfxItemSet* rSet )
 
     //Get the NumType value
     nPos = m_pNumberFormatBox->GetSelectEntryPos();
-    sal_uInt16 nEntryData = (sal_uInt16)reinterpret_cast<sal_uLong>(m_pNumberFormatBox->GetEntryData(nPos));
+    SvxNumType nEntryData = static_cast<SvxNumType>(reinterpret_cast<sal_uLong>(m_pNumberFormatBox->GetEntryData(nPos)));
     if ( m_pNumberFormatBox->IsValueChangedFromSaved() )
     {
-        aPage.SetNumType( (SvxNumType)nEntryData );
+        aPage.SetNumType( nEntryData );
         bMod = true;
     }
 

filter/source/svg/svgexport.cxx
@@ -933,9 +933,9 @@ void SVGFilter::implGenerateMetaData()
         }
 
         // Add a (global) Page Numbering Type attribute for the document
-        // NOTE: at present pSdrModel->GetPageNumType() returns always SVX_ARABIC
+        // NOTE: at present pSdrModel->GetPageNumType() returns always css::style::NumberingType::ARABIC
         // so the following code fragment is pretty unuseful
-        sal_Int32 nPageNumberingType =  SVX_ARABIC;
+        sal_Int32 nPageNumberingType = css::style::NumberingType::ARABIC;
         SvxDrawPage* pSvxDrawPage = SvxDrawPage::getImplementation( mSelectedPages[0] );
         if( pSvxDrawPage )
         {
@@ -946,24 +946,24 @@ void SVGFilter::implGenerateMetaData()
             // That is used by CalcFieldHdl method.
             mVisiblePagePropSet.nPageNumberingType = nPageNumberingType;
         }
-        if( nPageNumberingType != SVX_NUMBER_NONE )
+        if( nPageNumberingType != css::style::NumberingType::NUMBER_NONE )
         {
             OUString sNumberingType;
             switch( nPageNumberingType )
             {
-                case SVX_CHARS_UPPER_LETTER:
+                case css::style::NumberingType::CHARS_UPPER_LETTER:
                     sNumberingType = "alpha-upper";
                     break;
-                case SVX_CHARS_LOWER_LETTER:
+                case css::style::NumberingType::CHARS_LOWER_LETTER:
                     sNumberingType = "alpha-lower";
                     break;
-                case SVX_ROMAN_UPPER:
+                case css::style::NumberingType::ROMAN_UPPER:
                     sNumberingType = "roman-upper";
                     break;
-                case SVX_ROMAN_LOWER:
+                case css::style::NumberingType::ROMAN_LOWER:
                     sNumberingType = "roman-lower";
                     break;
-                case SVX_ARABIC:
+                case css::style::NumberingType::ARABIC:
                     // arabic numbering type is the default, so we do not append any attribute for it
                 default:
                     // in case the numbering type is not handled we fall back on arabic numbering
@@ -1033,7 +1033,7 @@ void SVGFilter::implGenerateMetaData()
 
                             //  Page Number Field
                             xPropSet->getPropertyValue( "IsPageNumberVisible" )  >>= bPageNumberVisibility;
-                            bPageNumberVisibility = bPageNumberVisibility && ( nPageNumberingType != SVX_NUMBER_NONE );
+                            bPageNumberVisibility = bPageNumberVisibility && ( nPageNumberingType != css::style::NumberingType::NUMBER_NONE );
                             if( bPageNumberVisibility ) // visibility default value: 'hidden'
                             {
                                 mpSVGExport->AddAttribute( XML_NAMESPACE_NONE, aOOOAttrPageNumberVisibility, "visible" );
@@ -1379,7 +1379,7 @@ void SVGFilter::implGetPagePropSet( const Reference< XDrawPage > & rxPage )
     mVisiblePagePropSet.bIsDateTimeFieldVisible             = true;
     mVisiblePagePropSet.bIsDateTimeFieldFixed               = true;
     mVisiblePagePropSet.nDateTimeFormat                     = SVXDATEFORMAT_B;
-    mVisiblePagePropSet.nPageNumberingType                  = SVX_ARABIC;
+    mVisiblePagePropSet.nPageNumberingType                  = css::style::NumberingType::ARABIC;
 
     //  We collect info on master page elements visibility, and placeholder text shape content.
     Reference< XPropertySet > xPropSet( rxPage, UNO_QUERY );
@@ -2274,20 +2274,20 @@ IMPL_LINK_TYPED( SVGFilter, CalcFieldHdl, EditFieldInfo*, pInfo, void )
             {
                 switch( mVisiblePagePropSet.nPageNumberingType )
                 {
-                    case SVX_CHARS_UPPER_LETTER:
+                    case css::style::NumberingType::CHARS_UPPER_LETTER:
                         aRepresentation += "QWERTYUIOPASDFGHJKLZXCVBNM";
                         break;
-                    case SVX_CHARS_LOWER_LETTER:
+                    case css::style::NumberingType::CHARS_LOWER_LETTER:
                         aRepresentation += "qwertyuiopasdfghjklzxcvbnm";
                         break;
-                    case SVX_ROMAN_UPPER:
+                    case css::style::NumberingType::ROMAN_UPPER:
                         aRepresentation += "IVXLCDM";
                         break;
-                    case SVX_ROMAN_LOWER:
+                    case css::style::NumberingType::ROMAN_LOWER:
                         aRepresentation += "ivxlcdm";
                         break;
                     // arabic numbering type is the default
-                    case SVX_ARABIC:
+                    case css::style::NumberingType::ARABIC:
                     // in case the numbering type is not handled we fall back on arabic numbering
                     default:
                         aRepresentation += "0123456789";

include/svx/pageitem.hxx
@@ -21,18 +21,8 @@
 
 #include <svl/poolitem.hxx>
 #include <svx/svxdllapi.h>
-#include <com/sun/star/style/NumberingType.hpp>
 
-enum SvxNumType
-{
-    SVX_CHARS_UPPER_LETTER = css::style::NumberingType::CHARS_UPPER_LETTER,
-    SVX_CHARS_LOWER_LETTER = css::style::NumberingType::CHARS_LOWER_LETTER,
-    SVX_ROMAN_UPPER = css::style::NumberingType::ROMAN_UPPER,
-    SVX_ROMAN_LOWER = css::style::NumberingType::ROMAN_LOWER,
-    SVX_ARABIC = css::style::NumberingType::ARABIC,
-    SVX_NUMBER_NONE = css::style::NumberingType::NUMBER_NONE,
-    SVX_PAGEDESC = css::style::NumberingType::PAGE_DESCRIPTOR,
-};
+using SvxNumType = sal_Int16; // css::style::NumberingType constants
 
 /*--------------------------------------------------------------------
   usage of the page

reportdesign/source/core/sdr/RptModel.cxx
@@ -121,8 +121,8 @@ SvxNumType OReportModel::GetPageNumType() const
 {
     uno::Reference< report::XReportDefinition > xReportDefinition( getReportDefinition() );
     if ( xReportDefinition.is() )
-        return (SvxNumType)getStyleProperty<sal_Int16>(xReportDefinition,PROPERTY_NUMBERINGTYPE);
-    return SVX_ARABIC;
+        return getStyleProperty<sal_Int16>(xReportDefinition,PROPERTY_NUMBERINGTYPE);
+    return css::style::NumberingType::ARABIC;
 }
 
 

reportdesign/source/ui/report/ReportController.cxx
@@ -2446,7 +2446,7 @@ void OReportController::openPageDialog(const uno::Reference<report::XSection>& _
                 uno::Reference<beans::XPropertySet> xProp(xPageStyle,uno::UNO_QUERY_THROW);
                 aPageItem.PutValue(xProp->getPropertyValue(PROPERTY_PAGESTYLELAYOUT),MID_PAGE_LAYOUT);
                 aPageItem.SetLandscape(getStyleProperty<bool>(m_xReportDefinition,PROPERTY_ISLANDSCAPE));
-                aPageItem.SetNumType((SvxNumType)getStyleProperty<sal_Int16>(m_xReportDefinition,PROPERTY_NUMBERINGTYPE));
+                aPageItem.SetNumType(getStyleProperty<sal_Int16>(m_xReportDefinition,PROPERTY_NUMBERINGTYPE));
                 pDescriptor->Put(aPageItem);
                 pDescriptor->Put(SvxBrushItem(::Color(getStyleProperty<sal_Int32>(m_xReportDefinition,PROPERTY_BACKCOLOR)),RPTUI_ID_BRUSH));
             }

sc/source/core/tool/editutil.cxx
@@ -750,20 +750,20 @@ static OUString lcl_GetNumStr(sal_Int32 nNo, SvxNumType eType)
     {
         switch( eType )
         {
-        case SVX_CHARS_UPPER_LETTER:
-        case SVX_CHARS_LOWER_LETTER:
+        case css::style::NumberingType::CHARS_UPPER_LETTER:
+        case css::style::NumberingType::CHARS_LOWER_LETTER:
             aTmpStr = lcl_GetCharStr( nNo );
             break;
 
-        case SVX_ROMAN_UPPER:
-        case SVX_ROMAN_LOWER:
+        case css::style::NumberingType::ROMAN_UPPER:
+        case css::style::NumberingType::ROMAN_LOWER:
             if( nNo < 4000 )
-                aTmpStr = SvxNumberFormat::CreateRomanString( nNo, ( eType == SVX_ROMAN_UPPER ) );
+                aTmpStr = SvxNumberFormat::CreateRomanString( nNo, ( eType == css::style::NumberingType::ROMAN_UPPER ) );
             else
                 aTmpStr.clear();
             break;
 
-        case SVX_NUMBER_NONE:
+        case css::style::NumberingType::NUMBER_NONE:
             aTmpStr.clear();
             break;
 
@@ -776,7 +776,7 @@ static OUString lcl_GetNumStr(sal_Int32 nNo, SvxNumType eType)
             break;
         }
 
-        if( SVX_CHARS_UPPER_LETTER == eType )
+        if( css::style::NumberingType::CHARS_UPPER_LETTER == eType )
             aTmpStr = aTmpStr.toAsciiUpperCase();
     }
     return aTmpStr;
@@ -788,7 +788,7 @@ ScHeaderFieldData::ScHeaderFieldData()
         aTime( tools::Time::EMPTY )
 {
     nPageNo = nTotalPages = 0;
-    eNumType = SVX_ARABIC;
+    eNumType = css::style::NumberingType::ARABIC;
 }
 
 ScHeaderEditEngine::ScHeaderEditEngine( SfxItemPool* pEnginePoolP )

sc/source/filter/starcalc/scflt.cxx
@@ -20,6 +20,7 @@
 #include "scitems.hxx"
 #include <editeng/eeitem.hxx>
 
+#include <com/sun/star/style/NumberingType.hpp>
 #include <svx/algitem.hxx>
 #include <editeng/boxitem.hxx>
 #include <editeng/brushitem.hxx>
@@ -859,7 +860,7 @@ void Sc10PageCollection::PutToDoc( ScDocument* pDoc )
         SvxPageItem aPageItem(ATTR_PAGE);
         aPageItem.SetPageUsage( SVX_PAGE_ALL );
         aPageItem.SetLandscape( pPage->Orientation != 1 );
-        aPageItem.SetNumType( SVX_ARABIC );
+        aPageItem.SetNumType( css::style::NumberingType::ARABIC );
         pSet->Put(aPageItem);
 
         pSet->Put(SvxLRSpaceItem( pPage->Left, pPage->Right, 0,0, ATTR_LRSPACE ));

sd/source/core/drawdoc.cxx
@@ -170,7 +170,7 @@ SdDrawDocument::SdDrawDocument(DocumentType eType, SfxObjectShell* pDrDocSh)
 , meLanguage( LANGUAGE_SYSTEM )
 , meLanguageCJK( LANGUAGE_SYSTEM )
 , meLanguageCTL( LANGUAGE_SYSTEM )
-, mePageNumType(SVX_ARABIC)
+, mePageNumType(css::style::NumberingType::ARABIC)
 , mbAllocDocSh(false)
 , meDocType(eType)
 , mpCharClass(nullptr)

sd/source/core/drawdoc4.cxx
@@ -1038,19 +1038,19 @@ OUString SdDrawDocument::CreatePageNumValue(sal_uInt16 nNum) const
 
     switch (mePageNumType)
     {
-        case SVX_CHARS_UPPER_LETTER:
+        case css::style::NumberingType::CHARS_UPPER_LETTER:
             aPageNumValue += OUStringLiteral1( (nNum - 1) % 26 + 'A' );
             break;
-        case SVX_CHARS_LOWER_LETTER:
+        case css::style::NumberingType::CHARS_LOWER_LETTER:
             aPageNumValue += OUStringLiteral1( (nNum - 1) % 26 + 'a' );
             break;
-        case SVX_ROMAN_UPPER:
+        case css::style::NumberingType::ROMAN_UPPER:
             bUpper = true;
             SAL_FALLTHROUGH;
-        case SVX_ROMAN_LOWER:
+        case css::style::NumberingType::ROMAN_LOWER:
             aPageNumValue += SvxNumberFormat::CreateRomanString(nNum, bUpper);
             break;
-        case SVX_NUMBER_NONE:
+        case css::style::NumberingType::NUMBER_NONE:
             aPageNumValue = " ";
             break;
         default:

sd/source/core/sdpage.cxx
@@ -2545,7 +2545,7 @@ const OUString& SdPage::GetName() const
 
             aCreatedPageName = SD_RESSTR(STR_PAGE);
             aCreatedPageName += " ";
-            if( GetModel()->GetPageNumType() == SVX_NUMBER_NONE )
+            if( GetModel()->GetPageNumType() == css::style::NumberingType::NUMBER_NONE )
             {
                 // if the document has number none as a formatting
                 // for page numbers we still default to arabic numbering

sd/source/ui/unoidl/UnoDocumentSettings.cxx
@@ -711,7 +711,7 @@ throw (UnknownPropertyException, PropertyVetoException,
             case HANDLE_PAGENUMFMT:
                 {
                     sal_Int32 nValue = 0;
-                    if( (*pValues >>= nValue ) && (nValue >= SVX_CHARS_UPPER_LETTER ) && (nValue <= SVX_PAGEDESC) )
+                    if( (*pValues >>= nValue ) && (nValue >= css::style::NumberingType::CHARS_UPPER_LETTER ) && (nValue <= css::style::NumberingType::PAGE_DESCRIPTOR) )
                     {
                         pDoc->SetPageNumType((SvxNumType)nValue);
                         bOk = true;

svx/source/items/pageitem.cxx
@@ -23,6 +23,7 @@
 #include <svx/pageitem.hxx>
 #include <editeng/itemtype.hxx>
 #include <svx/unomid.hxx>
+#include <com/sun/star/style/NumberingType.hpp>
 #include <com/sun/star/style/PageStyleLayout.hpp>
 #include <com/sun/star/style/BreakType.hpp>
 #include <svl/itemset.hxx>
@@ -36,7 +37,7 @@ SfxPoolItem* SvxPageItem::CreateDefault() { return new   SvxPageItem(0);}
 
 SvxPageItem::SvxPageItem( const sal_uInt16 nId ) : SfxPoolItem( nId ),
 
-    eNumType    ( SVX_ARABIC ),
+    eNumType    ( css::style::NumberingType::ARABIC ),
     bLandscape  ( false ),
     eUse        ( SVX_PAGE_ALL )
 {
@@ -101,7 +102,7 @@ bool SvxPageItem::GetPresentation
             {
                 rText = aDescName + cpDelimTmp;
             }
-            DBG_ASSERT( eNumType <= SVX_NUMBER_NONE, "enum overflow" );
+            DBG_ASSERT( eNumType <= css::style::NumberingType::NUMBER_NONE, "enum overflow" );
             rText += SVX_RESSTR(RID_SVXITEMS_PAGE_NUM_BEGIN + eNumType) + cpDelimTmp;
             if ( bLandscape )
                 rText += SVX_RESSTR(RID_SVXITEMS_PAGE_LAND_TRUE);
@@ -121,7 +122,7 @@ bool SvxPageItem::GetPresentation
             {
                 rText += aDescName + cpDelimTmp;
             }
-            DBG_ASSERT( eNumType <= SVX_NUMBER_NONE, "enum overflow" );
+            DBG_ASSERT( eNumType <= css::style::NumberingType::NUMBER_NONE, "enum overflow" );
             rText += SVX_RESSTR(RID_SVXITEMS_PAGE_NUM_BEGIN + eNumType) + cpDelimTmp;
             if ( bLandscape )
                 rText += SVX_RESSTR(RID_SVXITEMS_PAGE_LAND_TRUE);

svx/source/svdraw/svdmodel.cxx
@@ -1941,7 +1941,7 @@ void SdrModel::disposeOutliner( SdrOutliner* pOutliner )
 
 SvxNumType SdrModel::GetPageNumType() const
 {
-    return SVX_ARABIC;
+    return css::style::NumberingType::ARABIC;
 }
 
 void SdrModel::ReadUserDataSequenceValue(const css::beans::PropertyValue* pValue)

svx/source/unodraw/UnoGraphicExporter.cxx
@@ -355,19 +355,19 @@ IMPL_LINK_TYPED(GraphicExporter, CalcFieldValueHdl, EditFieldInfo*, pInfo, void)
 
                 switch(mpDoc->GetPageNumType())
                 {
-                    case SVX_CHARS_UPPER_LETTER:
+                    case css::style::NumberingType::CHARS_UPPER_LETTER:
                         aPageNumValue += OUStringLiteral1( (mnPageNumber - 1) % 26 + 'A' );
                         break;
-                    case SVX_CHARS_LOWER_LETTER:
+                    case css::style::NumberingType::CHARS_LOWER_LETTER:
                         aPageNumValue += OUStringLiteral1( (mnPageNumber - 1) % 26 + 'a' );
                         break;
-                    case SVX_ROMAN_UPPER:
+                    case css::style::NumberingType::ROMAN_UPPER:
                         bUpper = true;
                         SAL_FALLTHROUGH;
-                    case SVX_ROMAN_LOWER:
+                    case css::style::NumberingType::ROMAN_LOWER:
                         aPageNumValue += SvxNumberFormat::CreateRomanString(mnPageNumber, bUpper);
                         break;
-                    case SVX_NUMBER_NONE:
+                    case css::style::NumberingType::NUMBER_NONE:
                         aPageNumValue = " ";
                         break;
                     default:

sw/source/core/fields/dbfld.cxx
@@ -867,7 +867,7 @@ bool SwDBSetNumberField::PutValue( const uno::Any& rAny, sal_uInt16 nWhichId )
         {
             sal_Int16 nSet = 0;
             rAny >>= nSet;
-            if(nSet < (sal_Int16) SVX_NUMBER_NONE )
+            if(nSet < css::style::NumberingType::NUMBER_NONE )
                 SetFormat(nSet);
             else {
             }

sw/source/core/fields/expfld.cxx
@@ -986,7 +986,7 @@ bool SwSetExpField::PutValue( const uno::Any& rAny, sal_uInt16 nWhichId )
     case FIELD_PROP_USHORT2:
         {
             rAny >>= nTmp16;
-            if(nTmp16 <= SVX_NUMBER_NONE )
+            if(nTmp16 <= css::style::NumberingType::NUMBER_NONE )
                 SetFormat(nTmp16);
             else {
                 //exception(wrong_value)

sw/source/uibase/sidebar/PageStylesPanel.cxx
@@ -454,8 +454,8 @@ IMPL_LINK_NOARG_TYPED( PageStylesPanel, ModifyColumnCountHdl, ListBox&, void )
 
 IMPL_LINK_NOARG_TYPED( PageStylesPanel, ModifyNumberingHdl, ListBox&, void )
 {
-    sal_uInt16 nEntryData = (sal_uInt16)reinterpret_cast<sal_uLong>(mpNumberSelectLB->GetEntryData(mpNumberSelectLB->GetSelectEntryPos()));
-    mpPageItem->SetNumType((SvxNumType)nEntryData);
+    SvxNumType nEntryData = static_cast<SvxNumType>(reinterpret_cast<sal_uLong>(mpNumberSelectLB->GetEntryData(mpNumberSelectLB->GetSelectEntryPos())));
+    mpPageItem->SetNumType(nEntryData);
     mpBindings->GetDispatcher()->ExecuteList(SID_ATTR_PAGE, SfxCallMode::RECORD, { mpPageItem.get() });
 }
 

sw/source/uibase/utlui/uitool.cxx
@@ -420,7 +420,7 @@ void PageDescToItemSet( const SwPageDesc& rPageDesc, SfxItemSet& rSet)
     aPageItem.SetDescName(rPageDesc.GetName());
     aPageItem.SetPageUsage(lcl_convertUseToSvx(rPageDesc.GetUseOn()));
     aPageItem.SetLandscape(rPageDesc.GetLandscape());
-    aPageItem.SetNumType((SvxNumType)rPageDesc.GetNumType().GetNumberingType());
+    aPageItem.SetNumType(rPageDesc.GetNumType().GetNumberingType());
     rSet.Put(aPageItem);
 
     // Size