LibreOffice/core

commit 86e9ef21a06a9babefb778dfc3e02118d371d70d

sequence->vector in sc
Change-Id: I23edfbacb9ad0fb11ac8e6f789a73367f043eeda
Reviewed-on: https://gerrit.libreoffice.org/23697
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Eike Rathke <erack@redhat.com>
Tested-by: Eike Rathke <erack@redhat.com>

sc/source/core/tool/charthelper.cxx
@@ -206,16 +206,15 @@ uno::Reference< chart2::XChartDocument > ScChartHelper::GetChartFromSdrObject( S
 }
 
 void ScChartHelper::GetChartRanges( const uno::Reference< chart2::XChartDocument >& xChartDoc,
-            uno::Sequence< OUString >& rRanges )
+            std::vector< OUString >& rRanges )
 {
-    rRanges.realloc(0);
+    rRanges.clear();
     uno::Reference< chart2::data::XDataSource > xDataSource( xChartDoc, uno::UNO_QUERY );
     if( !xDataSource.is() )
         return;
 
     uno::Sequence< uno::Reference< chart2::data::XLabeledDataSequence > > aLabeledDataSequences( xDataSource->getDataSequences() );
-    rRanges.realloc(2*aLabeledDataSequences.getLength());
-    sal_Int32 nRealCount=0;
+    rRanges.reserve(2*aLabeledDataSequences.getLength());
     for( sal_Int32 nN=0;nN<aLabeledDataSequences.getLength();nN++)
     {
         uno::Reference< chart2::data::XLabeledDataSequence > xLabeledSequence( aLabeledDataSequences[nN] );
@@ -224,12 +223,15 @@ void ScChartHelper::GetChartRanges( const uno::Reference< chart2::XChartDocument
         uno::Reference< chart2::data::XDataSequence > xLabel( xLabeledSequence->getLabel());
         uno::Reference< chart2::data::XDataSequence > xValues( xLabeledSequence->getValues());
 
-        if( xLabel.is())
-            rRanges[nRealCount++] = xLabel->getSourceRangeRepresentation();
-        if( xValues.is())
-            rRanges[nRealCount++] = xValues->getSourceRangeRepresentation();
+        if (xLabel.is())
+             rRanges.push_back( xLabel->getSourceRangeRepresentation() );
+         else
+             rRanges.push_back( OUString() );
+         if (xValues.is())
+             rRanges.push_back( xValues->getSourceRangeRepresentation() );
+         else
+             rRanges.push_back( OUString() );
     }
-    rRanges.realloc(nRealCount);
 }
 
 void ScChartHelper::SetChartRanges( const uno::Reference< chart2::XChartDocument >& xChartDoc,

sc/inc/charthelper.hxx
@@ -42,7 +42,7 @@ public:
     static void UpdateChartsOnDestinationPage( ScDocument* pDestDoc, const SCTAB nDestTab );
     static css::uno::Reference< css::chart2::XChartDocument > GetChartFromSdrObject( SdrObject* pObject );
     static void GetChartRanges( const css::uno::Reference< css::chart2::XChartDocument >& xChartDoc,
-            css::uno::Sequence< OUString >& rRanges );
+            std::vector< OUString >& rRanges );
     static void SetChartRanges( const css::uno::Reference< css::chart2::XChartDocument >& xChartDoc,
             const css::uno::Sequence< OUString >& rRanges );
 

sc/source/core/data/documen5.cxx
@@ -259,9 +259,9 @@ void ScDocument::GetChartRanges( const OUString& rChartName, ::std::vector< ScRa
     uno::Reference< chart2::XChartDocument > xChartDoc( GetChartByName( rChartName ) );
     if ( xChartDoc.is() )
     {
-        uno::Sequence< OUString > aRangeStrings;
+        std::vector< OUString > aRangeStrings;
         ScChartHelper::GetChartRanges( xChartDoc, aRangeStrings );
-        for( sal_Int32 nN=0; nN<aRangeStrings.getLength(); nN++ )
+        for( size_t nN=0; nN<aRangeStrings.size(); nN++ )
         {
             ScRangeList aRanges;
             aRanges.Parse( aRangeStrings[nN], pSheetNameDoc, ScRefFlags::VALID, pSheetNameDoc->GetAddressConvention() );

commit 476ceebf4bc1b244dcdb34a743ce5c851a959952

tdf#101894 Copying a single sheet with a chart on it garbles the chart
..on the new sheet

Bug introduced with
    commit 86e9ef21a06a9babefb778dfc3e02118d371d70d
    Author: Noel Grandin <noelgrandin@gmail.com>
    Date:   Tue Mar 8 20:41:21 2016 +0200
    sequence->vector in sc

Change-Id: I7a6539005f42c7a7036c60b12a9ff2f77f71ba6b

sc/source/core/tool/charthelper.cxx
@@ -224,13 +224,9 @@ void ScChartHelper::GetChartRanges( const uno::Reference< chart2::XChartDocument
         uno::Reference< chart2::data::XDataSequence > xValues( xLabeledSequence->getValues());
 
         if (xLabel.is())
-             rRanges.push_back( xLabel->getSourceRangeRepresentation() );
-         else
-             rRanges.push_back( OUString() );
-         if (xValues.is())
-             rRanges.push_back( xValues->getSourceRangeRepresentation() );
-         else
-             rRanges.push_back( OUString() );
+            rRanges.push_back( xLabel->getSourceRangeRepresentation() );
+        if (xValues.is())
+            rRanges.push_back( xValues->getSourceRangeRepresentation() );
     }
 }
 