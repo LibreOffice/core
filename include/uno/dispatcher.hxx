4c1823923f47103a29f208b72e96e6e70773f18e
Stephan Bergmann <sbergman@redhat.com>
2016-09-23 18:33:11 +0200

    cid#1371304: Add move semantics
    Change-Id: I60051e720e816b1bd72193cbc028ce7264ba1e6a

--- include/uno/dispatcher.hxx
+++ include/uno/dispatcher.hxx
@@ -58,6 +58,12 @@ public:
     inline UnoInterfaceReference( uno_Interface * pUnoI );
     inline UnoInterfaceReference( UnoInterfaceReference const & ref );
 
+#if defined LIBO_INTERNAL_ONLY
+    UnoInterfaceReference(UnoInterfaceReference && other):
+        m_pUnoI(other.m_pUnoI)
+    { other.m_pUnoI = nullptr; }
+#endif
+
     inline uno_Interface * get() const
         { return m_pUnoI; }
 
@@ -73,6 +79,17 @@ public:
     inline UnoInterfaceReference & operator = (
         uno_Interface * pUnoI )
         { return set( pUnoI ); }
+
+#if defined LIBO_INTERNAL_ONLY
+    UnoInterfaceReference & operator =(UnoInterfaceReference && other) {
+        if (m_pUnoI != nullptr) {
+            (*m_pUnoI->release)(m_pUnoI);
+        }
+        m_pUnoI = other.m_pUnoI;
+        other.m_pUnoI = nullptr;
+        return *this;
+    }
+#endif
 
     inline void dispatch(
         struct _typelib_TypeDescription const * pMemberType,
