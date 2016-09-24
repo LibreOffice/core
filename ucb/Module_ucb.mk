f4cf84b2e343ac051b6d01acab8cf347d7d68222
Giuseppe Castagno <giuseppe.castagno@acca-esse.eu>
2016-09-24 13:35:54 +0200

    tdf#102499: Add WebDAV test to check HTTP reponse status codes retry
    Change-Id: I4ff36df3ac1cc0788322768378fb74f70892f922

--- ucb/Module_ucb.mk
+++ ucb/Module_ucb.mk
@@ -37,6 +37,7 @@ $(eval $(call gb_Module_add_check_targets,ucb,\
     CppunitTest_ucb_webdav_local_neon \
     CppunitTest_ucb_webdav_neon_opts \
     CppunitTest_ucb_webdav_propfindcache \
+    CppunitTest_ucb_webdav_res_access \
 ))
 
 endif
