b1815c2217aec67f62ce9be37791ddaf4b6992ab
Akshay Deep <akshaydeepiitr@gmail.com>
2016-09-26 15:42:01 +0530

    tdf#102238 START CENTER: Templates should be activated with single click
    Change-Id: If38f8fffd959c50b3befd521d2110e4c54e86be5

--- sfx2/source/control/templatedefaultview.cxx
+++ sfx2/source/control/templatedefaultview.cxx
@@ -61,6 +61,27 @@ void TemplateDefaultView::KeyInput( const KeyEvent& rKEvt )
     ThumbnailView::KeyInput(rKEvt);
 }
 
+void TemplateDefaultView::MouseButtonDown( const MouseEvent& rMEvt )
+{
+    if ( !rMEvt.IsLeft() )
+    {
+        Control::MouseButtonDown( rMEvt );
+        return;
+    }
+
+    if( rMEvt.GetClicks() == 1 )
+    {
+        size_t nPos = ImplGetItem(rMEvt.GetPosPixel());
+        ThumbnailViewItem* pItem = ImplGetItem(nPos);
+        TemplateViewItem* pViewItem = dynamic_cast<TemplateViewItem*>(pItem);
+        if(pViewItem)
+            maOpenTemplateHdl.Call(pViewItem);
+        return;
+    }
+
+    TemplateLocalView::MouseButtonDown(rMEvt);
+}
+
 void TemplateDefaultView::createContextMenu()
 {
     ScopedVclPtrInstance<PopupMenu> pItemMenu;
