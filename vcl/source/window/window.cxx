672069fa48dc7f8481fc4e94d131ba5f5211a9e8
Julien Nabet <serval2412@yahoo.fr>
2016-10-01 23:56:40 +0200

    Optimize a bit clear mnemonic labels part
    Change-Id: I2e95baef8f79e8a6644917bdf8f4795580af385e

--- vcl/source/window/window.cxx
+++ vcl/source/window/window.cxx
@@ -395,11 +395,7 @@ void Window::dispose()
     remove_from_all_size_groups();
 
     // clear mnemonic labels
-    std::vector<VclPtr<FixedText> > aMnemonicLabels(list_mnemonic_labels());
-    for (auto aI = aMnemonicLabels.begin(); aI != aMnemonicLabels.end(); ++aI)
-    {
-        remove_mnemonic_label(*aI);
-    }
+    mpWindowImpl->m_aMnemonicLabels.clear();
 
     // hide window in order to trigger the Paint-Handling
     Hide();
