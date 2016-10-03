7169343e0b36990df3ec64cd441e6348e27dca70
Tomaž Vajngerl <tomaz.vajngerl@collabora.com>
2016-10-03 18:39:04 +0200

    vcl: read image from JPEG one scanline at a time
    When reading an image from JPEG file we allocate a large buffer
    to hold the whole raw image before we read it into SalBitmap.
    If the image is large this step will spike the memory usage.
    
    This changes reading so that we allocate only the buffer for
    a scanline and read one scanline at a time.
    
    Change-Id: I405f4a14601e271c0293a2b75b4c9ec6c458d8a9

--- vcl/source/filter/jpeg/JpegReader.hxx
+++ vcl/source/filter/jpeg/JpegReader.hxx
@@ -39,34 +39,34 @@ struct JPEGCreateBitmapParam
     unsigned long density_unit;
     unsigned long X_density;
     unsigned long Y_density;
-    long     bGray;
+    bool bGray;
 
-    long     nAlignedWidth;  // these members will be filled by the
-    bool     bTopDown;      // CreateBitmap method in svtools
+    long nAlignedWidth;  // these members will be filled by the
+    bool bTopDown;      // CreateBitmap method in svtools
 };
 
 class JPEGReader : public GraphicReader
 {
     SvStream&           mrStream;
-    Bitmap              maBmp;
-    Bitmap              maBmp1;
-    BitmapWriteAccess*  mpAcc;
-    BitmapWriteAccess*  mpAcc1;
-    unsigned char *     mpBuffer;
+    Bitmap              maBitmap;
+    Bitmap              maIncompleteAlpha;
+
     long                mnLastPos;
     long                mnFormerPos;
     long                mnLastLines;
     bool                mbSetLogSize;
 
-    Graphic CreateIntermediateGraphic( const Bitmap& rBitmap, long nLines );
-    void    FillBitmap();
+    Graphic CreateIntermediateGraphic(long nLines);
 
 public:
             JPEGReader( SvStream& rStream, void* pCallData, bool bSetLogSize );
     virtual ~JPEGReader() override;
 
-    ReadState   Read( Graphic& rGraphic );
-    unsigned char * CreateBitmap( JPEGCreateBitmapParam& param );
+    ReadState Read(Graphic& rGraphic);
+
+    bool CreateBitmap(JPEGCreateBitmapParam& param);
+
+    Bitmap& GetBitmap() { return maBitmap; }
 };
 
 #endif // INCLUDED_VCL_SOURCE_FILTER_JPEG_JPEGREADER_HXX
