c2b466ead2d27586ee2028e5b4e36c2da5ec35d5
Noel Grandin <noel.grandin@collabora.co.uk>
2016-10-04 09:57:11 +0200

    merge dbaccess::StorageInputStream with dbaccess::StorageXMLInputStream
    Change-Id: I05b54b8a2fe4ff0c8b9f89e3d29e864e8665b7b8

--- dbaccess/source/core/recovery/storagestream.hxx
+++ dbaccess/source/core/recovery/storagestream.hxx
@@ -53,26 +53,6 @@ namespace dbaccess
                                                 m_xOutputStream;
     };
 
-    // StorageInputStream
-    /** convenience wrapper around a stream living in a storage
-    */
-    class StorageInputStream
-    {
-    public:
-        StorageInputStream(
-            const css::uno::Reference< css::embed::XStorage >& i_rParentStorage,
-            const OUString& i_rStreamName
-        );
-        virtual ~StorageInputStream();
-
-    protected:
-        const css::uno::Reference< css::io::XInputStream >&
-                                                getInputStream() const { return m_xInputStream; }
-
-    private:
-              css::uno::Reference< css::io::XInputStream >
-                                                m_xInputStream;
-    };
 
 } // namespace dbaccess
 
