1ab72d8cc91bfc31c01b86b3941564ab3f6e8db9
Justin Luth <justin_luth@sil.org>
2016-10-07 13:44:01 +0300

    move round-tripables to ooxmlexport
    This serves two purposes. It helps to prevent regressions
    and it allows focus on features that don't export properly.
    
    Change-Id: I9d72202c577940ba94855096c5a11d4dec4790a5

---
+++ sw/CppunitTest_sw_ooxmlexport9.mk
@@ -0,0 +1,28 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#*************************************************************************
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+#*************************************************************************
+
+ifeq ($(OS),MACOSX)
+
+$(eval $(call gb_CppunitTest_add_cxxflags,sw_ooxmlimport,\
+    $(gb_OBJCXXFLAGS) \
+))
+
+$(eval $(call gb_CppunitTest_use_system_darwin_frameworks,sw_ooxmlimport,\
+	AppKit \
+))
+
+endif
+
+include $(SRCDIR)/sw/ooxmlexport_setup.mk
+
+$(eval $(call sw_ooxmlexport_test,9))
+
+# vim: set noet sw=4 ts=4:
