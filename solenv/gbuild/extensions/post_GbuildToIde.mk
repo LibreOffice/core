2cda6138274e75a07be738c95dc2e8a4e9a39c6a
Bjoern Michaelsen <bjoern.michaelsen@canonical.com>
2016-10-12 23:42:26 +0200

    add initial json export or gbuild data
    - also add gbuild selftest to test this (and possibly more later)
    
    Change-Id: Ia4ef41095613e596f39d107df700e929579ba45f

---
+++ solenv/gbuild/extensions/post_GbuildToIde.mk
@@ -0,0 +1,50 @@
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+ifneq ($(filter gbuildtoide,$(MAKECMDGOALS)),)
+
+gbuildtoide:
+	true
+
+.PHONY : foo
+foo:
+	true
+
+define gb_Postprocess_register_target
+gbuildtoide : $(call gb_LinkTarget_get_target,$(call gb_Library_get_linktarget,$(3)))
+
+
+$(call gb_LinkTarget_get_target,$(call gb_Library_get_linktarget,$(3))): $(gb_Helper_MISCDUMMY) foo
+
+define gb_LinkTarget__command
+	mkdir -p $(WORKDIR)/GbuildToIde/Library
+	printf '{"LINKTARGET": "%s"' '$$(2)' > $(WORKDIR)/GbuildToIde/$$(2)
+	printf ', "ILIBTARGET": "%s"' '$$(ILIBTARGET)' >> $(WORKDIR)/GbuildToIde/$$(2)
+	printf ', "COBJECTS": "%s"' '$$(COBJECTS)' >> $(WORKDIR)/GbuildToIde/$$(2)
+	printf ', "CXXOBJECTS": "%s"' '$$(CXXOBJECTS)' >> $(WORKDIR)/GbuildToIde/$$(2)
+	printf ', "YACCOBJECTS": "%s"' '$$(YACCOBJECTS)' >> $(WORKDIR)/GbuildToIde/$$(2)
+	printf ', "OBJCOBJECTS": "%s"' '$$(OBJCOBJECTS)' >> $(WORKDIR)/GbuildToIde/$$(2)
+	printf ', "OBJCXXOBJECTS": "%s"' '$$(OBJCXXOBJECTS)' >> $(WORKDIR)/GbuildToIde/$$(2)
+	printf ', "ASMOBJECTS": "%s"' '$$(ASMOBJECTS)' >> $(WORKDIR)/GbuildToIde/$$(2)
+	printf ', "GENCOBJECTS": "%s"' '$$(GENCOBJECTS)' >> $(WORKDIR)/GbuildToIde/$$(2)
+	printf ', "GENCXXOBJECTS": "%s"' '$$(GENCXXOBJECTS)' >> $(WORKDIR)/GbuildToIde/$$(2)
+	printf ', "CFLAGS": "%s"' '$$(T_CFLAGS) $$(T_CFLAGS_APPEND)' >> $(WORKDIR)/GbuildToIde/$$(2)
+	printf ', "CXXFLAGS": "%s"' '$$(T_CXXFLAGS) $$(T_CXXFLAGS_APPEND)' >> $(WORKDIR)/GbuildToIde/$$(2)
+	printf ', "OBJCFLAGS": "%s"' '$$(T_OBJCFLAGS)' >> $(WORKDIR)/GbuildToIde/$$(2)
+	printf ', "OBJCXXFLAGS": "%s"' '$$(T_OBJCXXFLAGS)' >> $(WORKDIR)/GbuildToIde/$$(2)
+	printf ', "DEFS": "%s"' '$$(DEFS)' >> $(WORKDIR)/GbuildToIde/$$(2)
+	printf ', "INCLUDE": "%s"' '$$(INCLUDE)' >> $(WORKDIR)/GbuildToIde/$$(2)
+	printf ', "LINKED_LIBS": "%s"' '$$(LINKED_LIBS)' >> $(WORKDIR)/GbuildToIde/$$(2)
+	printf ', "LINKED_STATIC_LIBS": "%s"' '$$(LINKED_STATIC_LIBS)' >> $(WORKDIR)/GbuildToIde/$$(2)
+	printf '}\n' >> $(WORKDIR)/GbuildToIde/$$(2)
+endef
+
+endef
+
+endif
+# vim: set noet ts=4 sw=4:
