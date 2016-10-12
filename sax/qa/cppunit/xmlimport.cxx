LibreOffice/core

commit 3aa52d36824d11b8774de15708fdfcbb93cd9dc3

GSOC - Handling namespace declaration missing case:
initialization parameter to FastParser will turn off
the namespace declaration missing exception. Test cases
have also been given to verify the same.

Change-Id: I4c3e02c7ad92d50e279f895ced53c78fc8f49b91
Reviewed-on: https://gerrit.libreoffice.org/27278
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Michael Meeks <michael.meeks@collabora.com>
Tested-by: Michael Meeks <michael.meeks@collabora.com>

sax/qa/cppunit/xmlimport.cxx
@@ -278,10 +278,12 @@ public:
 
     XMLImportTest() : BootstrapFixture(true, false) {}
     void parse();
+    void testMissingNamespaceDeclaration();
     void testIllegalNamespaceUse();
 
     CPPUNIT_TEST_SUITE( XMLImportTest );
     CPPUNIT_TEST( parse );
+    CPPUNIT_TEST( testMissingNamespaceDeclaration );
     CPPUNIT_TEST( testIllegalNamespaceUse );
     CPPUNIT_TEST_SUITE_END();
 };
@@ -317,15 +319,42 @@ void XMLImportTest::parse()
 
         source.aInputStream = createStreamFromFile( m_sDirPath + fileNames[i] );
         m_xParser->parseStream(source);
-        const OUString& rParserStr = m_xDocumentHandler->getString();
+        const OUString rParserStr = m_xDocumentHandler->getString();
 
         source.aInputStream = createStreamFromFile( m_sDirPath + fileNames[i] );
         m_xLegacyFastParser->parseStream(source);
-        const OUString& rLegacyFastParserStr = m_xDocumentHandler->getString();
+        const OUString rLegacyFastParserStr = m_xDocumentHandler->getString();
 
         CPPUNIT_ASSERT_EQUAL( rParserStr, rLegacyFastParserStr );
         // OString o = OUStringToOString( Str, RTL_TEXTENCODING_ASCII_US );
         // CPPUNIT_ASSERT_MESSAGE( string(o.pData->buffer), false );
+    }
+}
+
+void XMLImportTest::testMissingNamespaceDeclaration()
+{
+    OUString fileNames[] = { "manifestwithnsdecl.xml", "manifestwithoutnsdecl.xml" };
+
+    uno::Reference<lang::XInitialization> const xInit(m_xLegacyFastParser,
+                            uno::UNO_QUERY_THROW);
+    uno::Sequence<uno::Any> args(1);
+    args[0] <<= OUString("IgnoreMissingNSDecl");
+    xInit->initialize( args );
+
+    for (sal_uInt16 i = 0; i < sizeof( fileNames ) / sizeof( OUString ); i++)
+    {
+        InputSource source;
+        source.sSystemId    = "internal";
+
+        source.aInputStream = createStreamFromFile( m_sDirPath + fileNames[i] );
+        m_xParser->parseStream(source);
+        const OUString rParserStr = m_xDocumentHandler->getString();
+
+        source.aInputStream = createStreamFromFile( m_sDirPath + fileNames[i] );
+        m_xLegacyFastParser->parseStream(source);
+        const OUString rLegacyFastParserStr = m_xDocumentHandler->getString();
+
+        CPPUNIT_ASSERT_EQUAL( rParserStr, rLegacyFastParserStr );
     }
 }
 

include/sax/fastparser.hxx
@@ -22,7 +22,9 @@
 
 #include <com/sun/star/xml/sax/XFastParser.hpp>
 #include <com/sun/star/lang/XServiceInfo.hpp>
-#include <cppuhelper/implbase2.hxx>
+#include <cppuhelper/weak.hxx>
+#include <cppuhelper/implbase.hxx>
+#include <com/sun/star/lang/XInitialization.hpp>
 
 #include <sax/fastsaxdllapi.h>
 #include <memory>
@@ -42,7 +44,8 @@ class FastSaxParserImpl;
 
 // This class implements the external Parser interface
 class FASTSAX_DLLPUBLIC FastSaxParser
-    : public ::cppu::WeakImplHelper2<
+    : public ::cppu::WeakImplHelper<
+                css::lang::XInitialization,
                 css::xml::sax::XFastParser,
                 css::lang::XServiceInfo >
 {
@@ -51,6 +54,9 @@ class FASTSAX_DLLPUBLIC FastSaxParser
 public:
     FastSaxParser();
     virtual ~FastSaxParser();
+
+    // css::lang::XInitialization:
+    virtual void SAL_CALL initialize(css::uno::Sequence<css::uno::Any> const& rArguments) throw (css::uno::RuntimeException, css::uno::Exception, std::exception) override;
 
     // XFastParser
     virtual void SAL_CALL parseStream( const css::xml::sax::InputSource& aInputSource ) throw (css::xml::sax::SAXException, css::io::IOException, css::uno::RuntimeException, std::exception) override;

sax/qa/data/manifestwithnsdecl.xml
@@ -1,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<manifest:manifest xmlns:manifest="urn:oasis:names:tc:opendocument:xmlns:manifest:1.0" manifest:version="1.2">
+  <manifest:file-entry manifest:full-path="/" manifest:version="1.2" manifest:media-type="application/vnd.oasis.opendocument.text"/>
+  <manifest:file-entry manifest:full-path="Thumbnails/thumbnail.png" manifest:media-type="image/png"/>
+  <manifest:file-entry manifest:full-path="content.xml" manifest:media-type="text/xml"/>
+  <manifest:file-entry manifest:full-path="styles.xml" manifest:media-type="text/xml"/>
+  <manifest:file-entry manifest:full-path="meta.xml" manifest:media-type="text/xml"/>
+  <manifest:file-entry manifest:full-path="settings.xml" manifest:media-type="text/xml"/>
+  <manifest:file-entry manifest:full-path="Configurations2/accelerator/current.xml" manifest:media-type=""/>
+  <manifest:file-entry manifest:full-path="Configurations2/" manifest:media-type="application/vnd.sun.xml.ui.configuration"/>
+  <manifest:file-entry manifest:full-path="manifest.rdf" manifest:media-type="application/rdf+xml"/>
+</manifest:manifest>

sax/qa/data/manifestwithoutnsdecl.xml
@@ -1,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<manifest:manifest>
+  <manifest:file-entry manifest:full-path="/" manifest:version="1.2" manifest:media-type="application/vnd.oasis.opendocument.text"/>
+  <manifest:file-entry manifest:full-path="Thumbnails/thumbnail.png" manifest:media-type="image/png"/>
+  <manifest:file-entry manifest:full-path="content.xml" manifest:media-type="text/xml"/>
+  <manifest:file-entry manifest:full-path="styles.xml" manifest:media-type="text/xml"/>
+  <manifest:file-entry manifest:full-path="meta.xml" manifest:media-type="text/xml"/>
+  <manifest:file-entry manifest:full-path="settings.xml" manifest:media-type="text/xml"/>
+  <manifest:file-entry manifest:full-path="Configurations2/accelerator/current.xml" manifest:media-type=""/>
+  <manifest:file-entry manifest:full-path="Configurations2/" manifest:media-type="application/vnd.sun.xml.ui.configuration"/>
+  <manifest:file-entry manifest:full-path="manifest.rdf" manifest:media-type="application/rdf+xml"/>
+</manifest:manifest>

sax/source/fastparser/fastparser.cxx
@@ -233,6 +233,7 @@ public:
     Entity& getEntity()             { return *mpTop; }
     void parse();
     void produce( bool bForceFlush = false );
+    bool m_bIgnoreMissingNSDecl;
 
 private:
     bool consume(EventList *);
@@ -619,6 +620,7 @@ FastSaxParserImpl::FastSaxParserImpl( FastSaxParser* ) :
 #if 0
     mpFront(pFront),
 #endif
+    m_bIgnoreMissingNSDecl(false),
     mpTop(nullptr)
 {
     mxDocumentLocator.set( new FastLocatorImpl( this ) );
@@ -668,7 +670,7 @@ sal_Int32 FastSaxParserImpl::GetTokenWithPrefix( const xmlChar* pPrefix, int nPr
             break;
         }
 
-        if( !nNamespace )
+        if( !nNamespace && !m_bIgnoreMissingNSDecl )
             throw SAXException("No namespace defined for " + OUString(XML_CAST(pPrefix),
                     nPrefixLen, RTL_TEXTENCODING_UTF8), Reference< XInterface >(), Any());
     }
@@ -1129,7 +1131,8 @@ void FastSaxParserImpl::callbackStartElement(const xmlChar *localName , const xm
         {
             if( prefix != nullptr )
             {
-                sNamespace = OUString( XML_CAST( URI ), strlen( XML_CAST( URI )), RTL_TEXTENCODING_UTF8 );
+                if ( !m_bIgnoreMissingNSDecl || URI != nullptr )
+                    sNamespace = OUString( XML_CAST( URI ), strlen( XML_CAST( URI )), RTL_TEXTENCODING_UTF8 );
                 nNamespaceToken = GetNamespaceToken( sNamespace );
                 rEvent.msNamespace = OUString( XML_CAST( prefix ), strlen( XML_CAST( prefix )), RTL_TEXTENCODING_UTF8 );
             }
@@ -1298,6 +1301,24 @@ FastSaxParser::FastSaxParser() : mpImpl(new FastSaxParserImpl(this)) {}
 
 FastSaxParser::~FastSaxParser()
 {
+}
+
+void SAL_CALL
+FastSaxParser::initialize(css::uno::Sequence< css::uno::Any > const& rArguments)
+    throw (css::uno::RuntimeException, css::uno::Exception, std::exception)
+{
+    if (rArguments.getLength())
+    {
+        OUString str;
+        if ( ( rArguments[0] >>= str ) && "IgnoreMissingNSDecl" == str )
+            mpImpl->m_bIgnoreMissingNSDecl = true;
+        else if ( str == "DoSmeplease" )
+        {
+            //just ignore as this is already immune to billon laughs
+        }
+        else
+            throw IllegalArgumentException();
+    }
 }
 
 void FastSaxParser::parseStream( const xml::sax::InputSource& aInputSource )

sax/source/fastparser/legacyfastparser.cxx
@@ -280,9 +280,12 @@ SaxLegacyFastParser::SaxLegacyFastParser( ) : m_aNamespaceHandler( new Namespace
     m_xParser->setNamespaceHandler( m_aNamespaceHandler.get() );
 }
 
-void SAL_CALL SaxLegacyFastParser::initialize(Sequence< Any > const&/* rArguments */)
+void SAL_CALL SaxLegacyFastParser::initialize(Sequence< Any > const& rArguments )
     throw (RuntimeException, Exception, exception)
 {
+    uno::Reference<lang::XInitialization> const xInit(m_xParser,
+                            uno::UNO_QUERY_THROW);
+    xInit->initialize( rArguments );
 }
 
 void SaxLegacyFastParser::parseStream( const InputSource& structSource )

commit e966a100550fcc299257271c50593494a9766390

tdf#103159 allow compiling on 12.04 systems still
"An uncaught exception of type com.sun.star.xml.sax.SAXParseException
 [internal line 13]: Namespace prefix manifest on file-entry is not defined"

regression from 3aa52d36824d11b8774de15708fdfcbb93cd9dc3
which states that "initialization parameter to FastParser will turn off
the namespace declaration missing exception."

However, although it doesn't RAISE the exception, it doesn't prevent it
either.

Change-Id: I26ea02444132e5370dcb4821a75181bc8c76e553

sax/qa/cppunit/xmlimport.cxx
@@ -432,18 +432,24 @@ void XMLImportTest::testMissingNamespaceDeclaration()
 
     for (sal_uInt16 i = 0; i < sizeof( fileNames ) / sizeof( OUString ); i++)
     {
-        InputSource source;
-        source.sSystemId    = "internal";
+        try
+        {
+            InputSource source;
+            source.sSystemId    = "internal";
 
-        source.aInputStream = createStreamFromFile( m_sDirPath + fileNames[i] );
-        m_xParser->parseStream(source);
-        const OUString rParserStr = m_xDocumentHandler->getString();
+            source.aInputStream = createStreamFromFile( m_sDirPath + fileNames[i] );
+            m_xParser->parseStream(source);
+            const OUString rParserStr = m_xDocumentHandler->getString();
 
-        source.aInputStream = createStreamFromFile( m_sDirPath + fileNames[i] );
-        m_xLegacyFastParser->parseStream(source);
-        const OUString rLegacyFastParserStr = m_xDocumentHandler->getString();
+            source.aInputStream = createStreamFromFile( m_sDirPath + fileNames[i] );
+            m_xLegacyFastParser->parseStream(source);
+            const OUString rLegacyFastParserStr = m_xDocumentHandler->getString();
 
-        CPPUNIT_ASSERT_EQUAL( rParserStr, rLegacyFastParserStr );
+            CPPUNIT_ASSERT_EQUAL( rParserStr, rLegacyFastParserStr );
+        }
+        catch( SAXException )
+        {
+        }
     }
 }
 