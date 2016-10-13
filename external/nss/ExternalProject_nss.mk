da5d20562479174504c3795aa9e7ca0856883e81
Giuseppe Castagno <giuseppe.castagno@acca-esse.eu>
2016-10-13 11:10:00 +0200

    Linux: Build NSS in debug mode only in '--enable-dbgutil' mode.
    With this commit, NSS debug mode is enabled only if you compile
    with --enable-dbgutil using ./autogen.sh command.
    
    Only for the Linux platform builds.
    
    Change-Id: Ia8626f36692fd38e8b1dbd774e4bc0ccedc59072
    Reviewed-on: https://gerrit.libreoffice.org/29781
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Giuseppe Castagno <giuseppe.castagno@acca-esse.eu>
    Reviewed-by: Michael Stahl <mstahl@redhat.com>

--- external/nss/ExternalProject_nss.mk
+++ external/nss/ExternalProject_nss.mk
@@ -67,6 +67,7 @@ $(call gb_ExternalProject_get_state_target,nss,build): $(call gb_ExternalProject
 		$(if $(filter MACOSX,$(OS)),\
 			$(if $(filter-out POWERPC,$(CPUNAME)),MACOS_SDK_DIR=$(MACOSX_SDK_PATH)) \
 			NSS_USE_SYSTEM_SQLITE=1) \
+		$(if $(filter LINUX,$(OS)),$(if $(ENABLE_DBGUTIL),,BUILD_OPT=1)) \
 		$(if $(filter SOLARIS,$(OS)),NS_USE_GCC=1) \
 		$(if $(CROSS_COMPILING),\
 			$(if $(filter MACOSXPOWERPC,$(OS)$(CPUNAME)),CPU_ARCH=ppc) \
