6d4f7734ad63998b067f50b6564639a527bf34e3
Stephan Bergmann <sbergman@redhat.com>
2016-10-14 16:59:47 +0200

    clang-cl loplugin: avmedia
    Change-Id: Id9ccbeb100ca9770226d0c8c84dae0a02aa7d2d6
    Reviewed-on: https://gerrit.libreoffice.org/29882
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

--- avmedia/source/win/player.hxx
+++ avmedia/source/win/player.hxx
@@ -56,7 +56,7 @@ class Player : public cppu::BaseMutex,
 public:
 
     explicit Player( const css::uno::Reference< css::lang::XMultiServiceFactory >& rxMgr );
-    ~Player();
+    ~Player() override;
 
     bool                create( const OUString& rURL );
 
@@ -66,29 +66,29 @@ public:
     const IVideoWindow* getVideoWindow() const;
 
     // XPlayer
-    virtual void SAL_CALL start(  ) throw (css::uno::RuntimeException);
-    virtual void SAL_CALL stop(  ) throw (css::uno::RuntimeException);
-    virtual sal_Bool SAL_CALL isPlaying(  ) throw (css::uno::RuntimeException);
-    virtual double SAL_CALL getDuration(  ) throw (css::uno::RuntimeException);
-    virtual void SAL_CALL setMediaTime( double fTime ) throw (css::uno::RuntimeException);
-    virtual double SAL_CALL getMediaTime(  ) throw (css::uno::RuntimeException);
-    virtual void SAL_CALL setPlaybackLoop( sal_Bool bSet ) throw (css::uno::RuntimeException);
-    virtual sal_Bool SAL_CALL isPlaybackLoop(  ) throw (css::uno::RuntimeException);
-    virtual void SAL_CALL setMute( sal_Bool bSet ) throw (css::uno::RuntimeException);
-    virtual sal_Bool SAL_CALL isMute(  ) throw (css::uno::RuntimeException);
-    virtual void SAL_CALL setVolumeDB( sal_Int16 nVolumeDB ) throw (css::uno::RuntimeException);
-    virtual sal_Int16 SAL_CALL getVolumeDB(  ) throw (css::uno::RuntimeException);
-    virtual css::awt::Size SAL_CALL getPreferredPlayerWindowSize(  ) throw (css::uno::RuntimeException);
-    virtual css::uno::Reference< css::media::XPlayerWindow > SAL_CALL createPlayerWindow( const css::uno::Sequence< css::uno::Any >& aArguments ) throw (css::uno::RuntimeException);
-    virtual css::uno::Reference< css::media::XFrameGrabber > SAL_CALL createFrameGrabber(  ) throw (css::uno::RuntimeException);
+    virtual void SAL_CALL start(  ) throw (css::uno::RuntimeException) override;
+    virtual void SAL_CALL stop(  ) throw (css::uno::RuntimeException) override;
+    virtual sal_Bool SAL_CALL isPlaying(  ) throw (css::uno::RuntimeException) override;
+    virtual double SAL_CALL getDuration(  ) throw (css::uno::RuntimeException) override;
+    virtual void SAL_CALL setMediaTime( double fTime ) throw (css::uno::RuntimeException) override;
+    virtual double SAL_CALL getMediaTime(  ) throw (css::uno::RuntimeException) override;
+    virtual void SAL_CALL setPlaybackLoop( sal_Bool bSet ) throw (css::uno::RuntimeException) override;
+    virtual sal_Bool SAL_CALL isPlaybackLoop(  ) throw (css::uno::RuntimeException) override;
+    virtual void SAL_CALL setMute( sal_Bool bSet ) throw (css::uno::RuntimeException) override;
+    virtual sal_Bool SAL_CALL isMute(  ) throw (css::uno::RuntimeException) override;
+    virtual void SAL_CALL setVolumeDB( sal_Int16 nVolumeDB ) throw (css::uno::RuntimeException) override;
+    virtual sal_Int16 SAL_CALL getVolumeDB(  ) throw (css::uno::RuntimeException) override;
+    virtual css::awt::Size SAL_CALL getPreferredPlayerWindowSize(  ) throw (css::uno::RuntimeException) override;
+    virtual css::uno::Reference< css::media::XPlayerWindow > SAL_CALL createPlayerWindow( const css::uno::Sequence< css::uno::Any >& aArguments ) throw (css::uno::RuntimeException) override;
+    virtual css::uno::Reference< css::media::XFrameGrabber > SAL_CALL createFrameGrabber(  ) throw (css::uno::RuntimeException) override;
 
     // XServiceInfo
-    virtual OUString SAL_CALL getImplementationName(  ) throw (css::uno::RuntimeException);
-    virtual sal_Bool SAL_CALL supportsService( const OUString& ServiceName ) throw (css::uno::RuntimeException);
-    virtual css::uno::Sequence< OUString > SAL_CALL getSupportedServiceNames(  ) throw (css::uno::RuntimeException);
+    virtual OUString SAL_CALL getImplementationName(  ) throw (css::uno::RuntimeException) override;
+    virtual sal_Bool SAL_CALL supportsService( const OUString& ServiceName ) throw (css::uno::RuntimeException) override;
+    virtual css::uno::Sequence< OUString > SAL_CALL getSupportedServiceNames(  ) throw (css::uno::RuntimeException) override;
 
     // ::cppu::OComponentHelper
-    virtual void SAL_CALL disposing();
+    virtual void SAL_CALL disposing() override;
 
 private:
 
