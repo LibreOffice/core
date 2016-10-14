2087efa58342ca8042b121c6281941d62a0bbe4c
Bjoern Michaelsen <bjoern.michaelsen@canonical.com>
2016-10-14 15:17:24 +0200

    prep WinResTarget for WNT in testdir
    Change-Id: I04c050dca1212d247c9b11a996ba8f37c0a6492f

--- solenv/qa/python/gbuildtoide.py
+++ solenv/qa/python/gbuildtoide.py
@@ -24,7 +24,7 @@ class CheckGbuildToIde(unittest.TestCase):
 
     def test_gbuildtoide(self):
         os.chdir(os.path.join(os.environ['SRCDIR'], 'solenv', 'qa', 'python', 'selftest'))
-        subprocess.check_call(['make', 'gbuildtoide', 'WORKDIR=%s' % self.tempwork])
+        subprocess.check_call(['make', 'gbuildtoide', 'WORKDIR=\'%s\'' % self.tempwork])
         jsonfiles = os.listdir(os.path.join(self.tempwork, 'GbuildToIde', 'Library'))
         gbuildlibs = []
         for jsonfilename in jsonfiles:
