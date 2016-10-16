747c2523b6ebbeacbe3665d24abbacab469a9fc9
melikeyurtoglu <aysemelikeyurtoglu@gmail.com>
2016-10-16 15:26:43 +0300

    tdf#91222 VclBuilder constructor cleanup
    Change-Id: I9a82ebc57c7e246841fda0262763fa9a7fbd5970
    Signed-off-by: melikeyurtoglu <aysemelikeyurtoglu@gmail.com>

--- sw/source/ui/config/optload.cxx
+++ sw/source/ui/config/optload.cxx
@@ -389,14 +389,8 @@ SwCaptionPreview::SwCaptionPreview(vcl::Window* pParent, WinBits nStyle)
     Init();
 }
 
-VCL_BUILDER_DECL_FACTORY(SwCaptionPreview)
-{
     WinBits nBits = 0;
-    OString sBorder = VclBuilder::extractCustomProperty(rMap);
-    if (!sBorder.isEmpty())
-       nBits |= WB_BORDER;
-    rRet = VclPtr<SwCaptionPreview>::Create(pParent, nBits);
-}
+    VCL_BUILDER_FACTORY_CONSTRUCTOR(SwCaptionPreview, nBits)
 
 void SwCaptionPreview::Init()
 {
