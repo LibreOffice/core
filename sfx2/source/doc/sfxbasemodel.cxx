7b1034367af82b0ab15693b3e6b3584701262fb4
Giuseppe Castagno <giuseppe.castagno@acca-esse.eu>
2016-10-16 10:05:42 +0200

    tdf#103274 (6): Add default XCommandEnvironment reference
    Change-Id: I71d83f1094800c3156f9159b6628e4d53d723b95

--- sfx2/source/doc/sfxbasemodel.cxx
+++ sfx2/source/doc/sfxbasemodel.cxx
@@ -2582,7 +2582,7 @@ bool SfxBaseModel::getBoolPropertyValue( const OUString& rName ) throw ( Runtime
             try
             {
                 ::ucbhelper::Content aContent( pMedium->GetName( ),
-                    Reference<ucb::XCommandEnvironment>(),
+                    utl::UCBContentHelper::getDefaultCommandEnvironment(),
                     comphelper::getProcessComponentContext() );
                 Reference < beans::XPropertySetInfo > xProps = aContent.getProperties();
                 if ( xProps->hasPropertyByName( rName ) )
