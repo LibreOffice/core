aed5e42680caf49068a3f463b571621ef0d87f86
Kohei Yoshida <kohei.yoshida@collabora.com>
2016-10-17 23:19:23 -0400

    tdf#93894: Prohibit grouping when certain token types are present.
    For instance, column / row label tokens don't work correctly in
    grouped cells with the current implementation.
    
    Change-Id: Idf86312ef15fbfd4382aa90ee6d131c671a80683

--- sc/source/core/tool/token.cxx
+++ sc/source/core/tool/token.cxx
@@ -1789,6 +1789,7 @@ ScTokenArray* ScTokenArray::Clone() const
     p->mnHashValue = mnHashValue;
     p->meVectorState = meVectorState;
     p->mbFromRangeName = mbFromRangeName;
+    p->mbShareable = mbShareable;
 
     FormulaToken** pp;
     if( nLen )
