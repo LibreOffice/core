c6f2b268ac422c0b6e77119f7e20bdca3f9524ec
Bjoern Michaelsen <bjoern.michaelsen@canonical.com>
2016-10-19 14:18:55 +0200

    add test for running gbuildtoide on non-build modules
    Change-Id: I85ae65af99490e58e732450a29e2eeeba085d1d4

--- solenv/qa/python/gbuildtoide.py
+++ solenv/qa/python/gbuildtoide.py
@@ -13,10 +13,15 @@ import json
 import os
 import os.path
 import tempfile
+import shutil
 
+makeenvvars = ['MAKEOVERRIDES', 'MAKEFLAGS', 'MAKE_TERMERR', 'MAKE_TERMOUT', 'MAKELEVEL', 'MFLAGS']
 
 class CheckGbuildToIde(unittest.TestCase):
     def setUp(self):
+        for makeenvvar in makeenvvars:
+            if makeenvvar in os.environ:
+                del os.environ[makeenvvar]
         self.tempwork = tempfile.mkdtemp()
         if os.environ['OS'] == 'WNT':
             self.tempworkmixed = self.tempwork.replace('\\','/')
@@ -27,6 +32,7 @@ class CheckGbuildToIde(unittest.TestCase):
         subprocess.check_call(['rm', '-rf', self.tempwork])
 
     def test_gbuildtoide(self):
+        print(os.environ)
         os.chdir(os.path.join(os.environ['SRCDIR'], 'solenv', 'qa', 'python', 'selftest'))
         subprocess.check_call(['make', 'gbuildtoide', 'WORKDIR=%s' % self.tempworkmixed])
         jsonfiles = os.listdir(os.path.join(self.tempwork, 'GbuildToIde', 'Library'))
@@ -64,6 +70,49 @@ class CheckGbuildToIde(unittest.TestCase):
                 self.assertTrue(False)
         self.assertEqual(foundexes, set(['gbuildselftestexe']))
         self.assertEqual(len(foundexes), 1)
+
+class CheckGbuildToIdeModules(unittest.TestCase):
+    def setUp(self):
+        for makeenvvar in makeenvvars:
+            if makeenvvar in os.environ:
+                del os.environ[makeenvvar]
+        self.tempwork = tempfile.mkdtemp()
+        self.tempsrc = tempfile.mkdtemp()
+        self.srcdir = os.environ['SRCDIR']
+        if os.environ['OS'] == 'WNT':
+            self.tempworkmixed = self.tempwork.replace('\\','/')
+            self.tempsrcmixed = self.tempsrc.replace('\\','/')
+            self.srcdirnative = self.srcdir.replace('/','\\')
+        else:
+            self.tempworkmixed = self.tempwork
+            self.tempsrcmixed = self.tempsrc
+            self.srcdirnative = self.srcdir
+        shutil.copyfile(os.path.join(self.srcdirnative, 'config_host.mk'), os.path.join(self.tempsrc, 'config_host.mk'))
+        shutil.copyfile(os.path.join(self.srcdirnative, 'config_host_lang.mk'), os.path.join(self.tempsrc, 'config_host_lang.mk'))
+        shutil.copyfile(os.path.join(self.srcdirnative, 'Repository.mk'), os.path.join(self.tempsrc, 'Repository.mk'))
+        shutil.copyfile(os.path.join(self.srcdirnative, 'RepositoryExternal.mk'), os.path.join(self.tempsrc, 'RepositoryExternal.mk'))
+        shutil.copyfile(os.path.join(self.srcdirnative, 'RepositoryFixes.mk'), os.path.join(self.tempsrc, 'RepositoryFixes.mk'))
+        shutil.copytree(os.path.join(self.srcdirnative, 'config_host'),  os.path.join(self.tempsrc, 'config_host'))
+        shutil.copytree(os.path.join(self.srcdirnative, 'solenv'),  os.path.join(self.tempsrc, 'solenv'))
+
+    def tearDown(self):
+        subprocess.check_call(['rm', '-rf', self.tempsrc])
+        subprocess.check_call(['rm', '-rf', self.tempwork])
+
+    def test_gbuildtoide(self):
+        print(os.environ)
+        blacklisted_modules = ['compilerplugins', 'dictionaries', 'bridges', 'helpcompiler', 'helpcontent2', 'icon-themes', 'sal', 'cppu', 'cppuhelper', 'cpputools', 'extensions', 'external', 'i18npool', 'javaunohelper', 'jurt', 'lingucomponent', 'scaddins', 'solenv', 'stoc', 'tools', 'tranlations', 'writerfilter']
+        modules = ['accessibility', 'android', 'animations', 'apple_remote', 'avmedia', 'basctl', 'basegfx', 'basic', 'bean', 'canvas', 'chart2', 'cli_ure', 'codemaker', 'comphelper', 'cppcanvas', 'cui', 'dbaccess', 'desktop', 'drawinglayer', 'dtrans', 'editeng', 'embeddedobj', 'embedserv', 'eventattacher', 'extras', 'filter', 'forms', 'formula', 'fpicker', 'framework', 'hwpfilter', 'i18nlangtag', 'i18nutil', 'idl', 'idlc', 'instsetoo_native', 'io', 'ios', 'jvmaccess', 'jvmfwk', 'l10ntools', 'librelogo', 'libreofficekit', 'linguistic', 'lotuswordpro', 'mysqlc', 'nlpsolver', 'o3tl', 'odk', 'offapi', 'officecfg', 'onlineupdate', 'oovbaapi', 'oox', 'opencl', 'package', 'postprocess', 'pyuno', 'registry', 'remotebridges', 'reportbuilder', 'reportdesign', 'ridljar', 'rsc', 'salhelper', 'sax', 'sc', 'sccomp', 'scp2', 'scripting', 'sd', 'sdext', 'setup_native', 'sfx2', 'shell', 'slideshow', 'smoketest', 'soltools', 'sot', 'starmath', 'store', 'svgio', 'svl', 'svtools', 'svx', 'sw', 'swext', 'sysui', 'test', 'testtools', 'toolkit', 'tubes', 'ucb', 'ucbhelper', 'udkapi', 'uitest', 'UnoControls', 'unodevtools', 'unoidl', 'unoil', 'unotest', 'unotools', 'unoxml', 'ure', 'uui', 'vbahelper', 'vcl', 'winaccessibility', 'wizards', 'writerperfect', 'xmerge', 'xmlhelp', 'xmloff', 'xmlreader', 'xmlscript', 'xmlsecurity']
+        for module in modules:
+            print("testing gbuildtoide for module: %s" % module)
+            shutil.rmtree(self.tempwork)
+            os.mkdir(self.tempwork)
+            if module != 'solenv':
+                shutil.copytree(os.path.join(os.environ['SRCDIR'], module),  os.path.join(self.tempsrc, module))
+            os.chdir(os.path.join(self.tempsrc, module))
+            subprocess.check_call(['make', 'gbuildtoide', '--debug=v', 'WORKDIR=%s' % self.tempworkmixed, 'SRCDIR=%s' % self.tempsrcmixed])
+            shutil.rmtree(os.path.join(self.tempsrc, module))
+
 
 if __name__ == "__main__":
     unittest.main()
