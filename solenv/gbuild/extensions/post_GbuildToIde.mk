4a04609c00a00e8b8205e9ef98c2f2d71d9575d8
Bjoern Michaelsen <bjoern.michaelsen@canonical.com>
2016-10-19 14:18:55 +0200

    add test for running gbuildtoide on non-build modules
    also reenable test on windows
    
    Change-Id: I85ae65af99490e58e732450a29e2eeeba085d1d4

--- solenv/gbuild/extensions/post_GbuildToIde.mk
+++ solenv/gbuild/extensions/post_GbuildToIde.mk
@@ -19,6 +19,7 @@ foo:
 
 define gb_LinkTarget__command
 mkdir -p $(WORKDIR)/GbuildToIde/$(dir $(2))
+mkdir -p $(WORKDIR)/LinkTarget/$(dir $(2))
 printf '{"LINKTARGET": "%s"' '$(2)' > $(WORKDIR)/GbuildToIde/$(2)
 printf ', "ILIBTARGET": "%s"' '$(ILIBTARGET)' >> $(WORKDIR)/GbuildToIde/$(2)
 printf ', "COBJECTS": "%s"' '$(COBJECTS)' >> $(WORKDIR)/GbuildToIde/$(2)
@@ -46,5 +47,34 @@ gbuildtoide : $(call gb_LinkTarget_get_target,$(call gb_$(2)_get_linktarget,$(3)
 $(call gb_LinkTarget_get_target,$(call gb_$(2)_get_linktarget,$(3))): $(gb_Helper_MISCDUMMY) foo
 endef
 
+gb_LinkTarget_use_static_libraries =
+gb_UnoApiHeadersTarget_get_target = foo
+gb_UnpackedTarball_get_final_target = foo
+gb_LinkTarget__get_headers_check =
+gb_LinkTarget_add_cobject = $(call gb_LinkTarget_get_target,$(1)) : COBJECTS += $(2)
+gb_LinkTarget_add_cxxobject = $(call gb_LinkTarget_get_target,$(1)) : CXXOBJECTS += $(2)
+gb_LinkTarget_use_package =
+gb_LinkTarget_add_sdi_headers =
+gb_LinkTarget_use_external_project =
+gb_LinkTarget_add_scanners =
+gb_LinkTarget_add_grammars =
+gb_LinkTarget__check_srcdir_paths =
+gb_WinResTarget_WinResTarget_init =
+
+define gb_LinkTarget__use_custom_headers
+$(call gb_LinkTarget__add_include,$(1),$(call gb_CustomTarget_get_workdir,$(2)))
+
+endef
+
+define gb_Module_add_target
+$(if $(filter Library_% Executable_%,$(2)),$(call gb_Module__read_targetfile,$(1),$(2),target))
+
+$(if $(filter Library_% Executable_%,$(2)),$(call gb_Module_get_nonl10n_target,$(1)) : $$(gb_Module_CURRENTTARGET))
+
+endef
+
+gb_Module_add_l10n_target =
+
 endif
+
 # vim: set noet ts=4 sw=4:
