85a3c7beabcae4f33d0869f0f2380df750e8fc53
Miklos Vajna <vmiklos@collabora.co.uk>
2016-10-19 08:33:05 +0200

    starmath: clean up redundant virtual keywords in unofilter
    Change-Id: I97af23d22c0b4eb416172e9ebea9544541621cb0

--- starmath/source/unofilter.cxx
+++ starmath/source/unofilter.cxx
@@ -30,28 +30,24 @@ class MathTypeFilter : public cppu::WeakImplHelper
 
 public:
     MathTypeFilter();
-    virtual ~MathTypeFilter() override;
+    ~MathTypeFilter() override;
 
     // XFilter
-    virtual sal_Bool SAL_CALL filter(const uno::Sequence<beans::PropertyValue>& rDescriptor) throw (uno::RuntimeException, std::exception) override;
-    virtual void SAL_CALL cancel() throw (uno::RuntimeException, std::exception) override;
+    sal_Bool SAL_CALL filter(const uno::Sequence<beans::PropertyValue>& rDescriptor) throw (uno::RuntimeException, std::exception) override;
+    void SAL_CALL cancel() throw (uno::RuntimeException, std::exception) override;
 
     // XImporter
-    virtual void SAL_CALL setTargetDocument(const uno::Reference<lang::XComponent>& xDoc) throw (lang::IllegalArgumentException, uno::RuntimeException, std::exception) override;
+    void SAL_CALL setTargetDocument(const uno::Reference<lang::XComponent>& xDoc) throw (lang::IllegalArgumentException, uno::RuntimeException, std::exception) override;
 
     // XServiceInfo
-    virtual OUString SAL_CALL getImplementationName() throw (uno::RuntimeException, std::exception) override;
-    virtual sal_Bool SAL_CALL supportsService(const OUString& ServiceName) throw (uno::RuntimeException, std::exception) override;
-    virtual uno::Sequence<OUString> SAL_CALL getSupportedServiceNames() throw (uno::RuntimeException, std::exception) override;
+    OUString SAL_CALL getImplementationName() throw (uno::RuntimeException, std::exception) override;
+    sal_Bool SAL_CALL supportsService(const OUString& ServiceName) throw (uno::RuntimeException, std::exception) override;
+    uno::Sequence<OUString> SAL_CALL getSupportedServiceNames() throw (uno::RuntimeException, std::exception) override;
 };
 
-MathTypeFilter::MathTypeFilter()
-{
-}
+MathTypeFilter::MathTypeFilter() = default;
 
-MathTypeFilter::~MathTypeFilter()
-{
-}
+MathTypeFilter::~MathTypeFilter() = default;
 
 sal_Bool MathTypeFilter::filter(const uno::Sequence<beans::PropertyValue>& rDescriptor) throw(uno::RuntimeException, std::exception)
 {
