a5236104672ccbb9f7f9c1b4dc70bea95ca5abf6
Stephan Bergmann <sbergman@redhat.com>
2016-10-20 13:28:17 +0200

    external/harfbuzz: Silence clang-cl -Werror,-Wmicrosoft-enum-value
    The code in harfbuzz' src/hb_common.h apparently goes to some length to ensure
    that any value of type hb_tag_t (aka unit32_t) can be transported as a value of
    the hb_script_t enum type.  However, under MSVC any C (or non-fixed C++) enum
    type has an underlying type of int, so _HB_SCRIPT_MAX_VALUE of value 0xFFFFFFFF
    will cause a -Wmicrosoft-enum-value under clang-cl.  To not complicate things
    further, acknowledge that converting between hb_tag_t (an unsigned integer type
    with 32 value bits) and hb_script_t (a two's-complement signed integer type with
    32 value bits) is well-defined under MSVC and drop _HB_SCRIPT_MAX_VALUE (which
    appears to be an otherwise unused implementation detail) there.
    
    Change-Id: Ic03dff64a9dd24683c45347fa78699708c269972

--- external/harfbuzz/UnpackedTarball_harfbuzz.mk
+++ external/harfbuzz/UnpackedTarball_harfbuzz.mk
@@ -15,6 +15,7 @@ $(eval $(call gb_UnpackedTarball_set_patchlevel,harfbuzz,0))
 
 $(eval $(call gb_UnpackedTarball_add_patches,harfbuzz, \
     external/harfbuzz/ubsan.patch \
+    external/harfbuzz/clang-cl.patch \
 ))
 
 ifneq ($(ENABLE_RUNTIME_OPTIMIZATIONS),TRUE)
