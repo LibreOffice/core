4bb615c27b14687b90b04dae8460d89e67be4198
Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
2016-10-20 12:47:40 +0200

    Check for enable safe mode earlier
    Change-Id: I50e48472bb840c70e9a7c11dd84f33c64d6e1422

--- desktop/source/app/appinit.cxx
+++ desktop/source/app/appinit.cxx
@@ -126,9 +126,6 @@ void Desktop::RegisterServices(Reference< XComponentContext > const & context)
         // interpret command line arguments
         CommandLineArgs& rCmdLine = GetCommandLineArgs();
 
-        if (rCmdLine.IsSafeMode() || sfx2::SafeMode::hasFlag())
-            Application::EnableSafeMode();
-
         // Headless mode for FAT Office
         bool bHeadlessMode = rCmdLine.IsHeadless();
         if ( bHeadlessMode )
