12018be8839cfe60a95bd434e5ba9c3f3619353c
Julien Nabet <serval2412@yahoo.fr>
2016-10-23 13:26:49 +0200

    Related tdf#103421: disposeAndClear m_aColorSet (reportdesign)
    m_aColorSet has been declared as VclPtr<ValueSet>
    then initialized with VclPtr<ValueSet>::Create(this, WinBits( WB_ITEMBORDER | WB_NAMEFIELD | WB_3DLOOK | WB_NO_DIRECTSELECT))
    Following vcl/README.lifecycle, we're on the case 2 of "Who owns & disposes what ?"
    so we should use disposeAndClear
    
    Change-Id: I5809b54b8a15c365362c05c35a7499d8f4f69ed2
    Reviewed-on: https://gerrit.libreoffice.org/30176
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
    (cherry picked from commit ef663010434da24ecda35d0f0c26ce1a2b2c0f6c)
    Reviewed-on: https://gerrit.libreoffice.org/30177
    Reviewed-by: Katarina Behrens <Katarina.Behrens@cib.de>

--- reportdesign/source/ui/dlg/Condition.cxx
+++ reportdesign/source/ui/dlg/Condition.cxx
@@ -161,7 +161,7 @@ OColorPopup::~OColorPopup()
 void OColorPopup::dispose()
 {
     disposeBuilder();
-    m_aColorSet.clear();
+    m_aColorSet.disposeAndClear();
     m_pCondition.clear();
     FloatingWindow::dispose();
 }
