2d96f3a582383205cd2f891b1001b11970cec316
Caolán McNamara <caolanm@redhat.com>
2016-10-27 21:39:32 +0100

    coverity#1374314 Uncaught exception
    Change-Id: I07a39774a1372ea26e8d66376c27d3b98229892e

--- forms/source/richtext/richtextmodel.cxx
+++ forms/source/richtext/richtextmodel.cxx
@@ -329,7 +329,7 @@ namespace frm
     }
 
 
-    sal_Bool SAL_CALL ORichTextModel::convertFastPropertyValue( Any& _rConvertedValue, Any& _rOldValue, sal_Int32 _nHandle, const Any& _rValue ) throw( IllegalArgumentException )
+    sal_Bool SAL_CALL ORichTextModel::convertFastPropertyValue( Any& _rConvertedValue, Any& _rOldValue, sal_Int32 _nHandle, const Any& _rValue ) throw( IllegalArgumentException, RuntimeException, std::exception )
     {
         bool bModified = false;
 
--- forms/source/richtext/richtextmodel.hxx
+++ forms/source/richtext/richtextmodel.hxx
@@ -131,7 +131,7 @@ namespace frm
         virtual void SAL_CALL getFastPropertyValue(css::uno::Any& rValue, sal_Int32 nHandle ) const override;
         virtual sal_Bool SAL_CALL convertFastPropertyValue(css::uno::Any& rConvertedValue, css::uno::Any& rOldValue,
                                             sal_Int32 nHandle, const css::uno::Any& rValue )
-                                        throw(css::lang::IllegalArgumentException) override;
+                                        throw(css::lang::IllegalArgumentException, css::uno::RuntimeException, std::exception) override;
         virtual void SAL_CALL setFastPropertyValue_NoBroadcast(sal_Int32 nHandle, const css::uno::Any& rValue) throw ( css::uno::Exception, std::exception) override;
         virtual css::uno::Any getPropertyDefaultByHandle( sal_Int32 nHandle ) const override;
 
