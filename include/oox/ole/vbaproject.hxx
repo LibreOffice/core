LibreOffice/core

commit d4743045a0b320449d07a957463a76bb8b13f939

the cells need to be imported before we handle charts, tdf#81396
Change-Id: Ic0dc47b71e76aa4825a867fc171406d126ae0518
Reviewed-on: https://gerrit.libreoffice.org/19568
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Eike Rathke <erack@redhat.com>
Tested-by: Eike Rathke <erack@redhat.com>

sc/source/filter/oox/workbookfragment.cxx
@@ -52,9 +52,11 @@
 #include "document.hxx"
 #include "docsh.hxx"
 #include "calcconfig.hxx"
+#include "globstr.hrc"
 
 #include <vcl/svapp.hxx>
 #include <vcl/timer.hxx>
+#include <vcl/msgbox.hxx>
 
 #include <oox/core/fastparser.hxx>
 #include <salhelper/thread.hxx>
@@ -67,6 +69,9 @@
 #include <memory>
 
 #include <oox/ole/vbaproject.hxx>
+
+#include <comphelper/processfactory.hxx>
+#include <officecfg/Office/Calc.hxx>
 
 namespace oox {
 namespace xls {
@@ -482,6 +487,19 @@ void WorkbookFragment::finalizeImport()
     // load all worksheets
     importSheetFragments(*this, aSheetFragments);
 
+    sal_Int16 nActiveSheet = getViewSettings().getActiveCalcSheet();
+    getWorksheets().finalizeImport( nActiveSheet );
+
+    // final conversions, e.g. calculation settings and view settings
+    finalizeWorkbookImport();
+    //
+    //stop preventing establishment of listeners as is done in
+    //ScDocShell::AfterXMLLoading() for ods
+    getScDocument().SetInsertingFromOtherDoc(false);
+    getDocImport().finalize();
+
+    recalcFormulaCells();
+
     for( std::vector<WorksheetHelper*>::iterator aIt = maHelpers.begin(), aEnd = maHelpers.end(); aIt != aEnd; ++aIt )
     {
         (*aIt)->finalizeDrawingImport();
@@ -494,12 +512,6 @@ void WorkbookFragment::finalizeImport()
         aIt->first.reset();
     }
 
-    sal_Int16 nActiveSheet = getViewSettings().getActiveCalcSheet();
-    getWorksheets().finalizeImport( nActiveSheet );
-
-    // final conversions, e.g. calculation settings and view settings
-    finalizeWorkbookImport();
-
     OUString aRevHeadersPath = getFragmentPathFromFirstType(CREATE_OFFICEDOC_RELATION_TYPE("revisionHeaders"));
     if (!aRevHeadersPath.isEmpty())
     {
@@ -507,6 +519,61 @@ void WorkbookFragment::finalizeImport()
         rtl::Reference<oox::core::FragmentHandler> xFragment(new RevisionHeadersFragment(*this, aRevHeadersPath));
         importOoxFragment(xFragment, *xParser);
     }
+}
+
+namespace {
+
+ScDocShell& getDocShell(ScDocument& rDoc)
+{
+    return static_cast<ScDocShell&>(*rDoc.GetDocumentShell());
+}
+
+}
+
+void WorkbookFragment::recalcFormulaCells()
+{
+    // Recalculate formula cells.
+    ScDocument& rDoc = getScDocument();
+    ScDocShell& rDocSh = getDocShell(rDoc);
+    Reference< XComponentContext > xContext = comphelper::getProcessComponentContext();
+    ScRecalcOptions nRecalcMode =
+        static_cast<ScRecalcOptions>(officecfg::Office::Calc::Formula::Load::OOXMLRecalcMode::get(xContext));
+    bool bHardRecalc = false;
+    if (nRecalcMode == RECALC_ASK)
+    {
+        if (rDoc.IsUserInteractionEnabled())
+        {
+            // Ask the user if full re-calculation is desired.
+            ScopedVclPtrInstance<QueryBox> aBox(
+                ScDocShell::GetActiveDialogParent(), WinBits(WB_YES_NO | WB_DEF_YES),
+                ScGlobal::GetRscString(STR_QUERY_FORMULA_RECALC_ONLOAD_XLS));
+            aBox->SetCheckBoxText(ScGlobal::GetRscString(STR_ALWAYS_PERFORM_SELECTED));
+
+            sal_Int32 nRet = aBox->Execute();
+            bHardRecalc = nRet == RET_YES;
+
+            if (aBox->GetCheckBoxState())
+            {
+                // Always perform selected action in the future.
+                std::shared_ptr<comphelper::ConfigurationChanges> batch(comphelper::ConfigurationChanges::create());
+                officecfg::Office::Calc::Formula::Load::OOXMLRecalcMode::set(sal_Int32(0), batch);
+                ScFormulaOptions aOpt = SC_MOD()->GetFormulaOptions();
+                aOpt.SetOOXMLRecalcOptions(bHardRecalc ? RECALC_ALWAYS : RECALC_NEVER);
+                /* XXX  is this really supposed to set the ScModule options?
+                 *      Not the ScDocShell options? */
+                SC_MOD()->SetFormulaOptions(aOpt);
+
+                batch->commit();
+            }
+        }
+    }
+    else if (nRecalcMode == RECALC_ALWAYS)
+        bHardRecalc = true;
+
+    if (bHardRecalc)
+        rDocSh.DoHardRecalc(false);
+    else
+        rDoc.CalcFormulaTree(false, true, false);
 }
 
 // private --------------------------------------------------------------------

sc/source/filter/inc/workbookfragment.hxx
@@ -55,6 +55,8 @@ private:
     void                importExternalLinkFragment( ExternalLink& rExtLink );
     void                importPivotCacheDefFragment( const OUString& rRelId, sal_Int32 nCacheId );
 
+    void recalcFormulaCells();
+
 private:
     DefinedNameRef      mxCurrName;
 };

sc/source/filter/oox/workbookhelper.cxx
@@ -660,60 +660,9 @@ void WorkbookGlobals::finalize()
         if (pModel)
             pModel->SetOpenInDesignMode(false);
 
-        //stop preventing establishment of listeners as is done in
-        //ScDocShell::AfterXMLLoading() for ods
-        mpDoc->SetInsertingFromOtherDoc(false);
-        getDocImport().finalize();
-
-        recalcFormulaCells();
     }
 }
 
-void WorkbookGlobals::recalcFormulaCells()
-{
-    // Recalculate formula cells.
-    ScDocument& rDoc = getScDocument();
-    ScDocShell& rDocSh = getDocShell();
-    Reference< XComponentContext > xContext = comphelper::getProcessComponentContext();
-    ScRecalcOptions nRecalcMode =
-        static_cast<ScRecalcOptions>(officecfg::Office::Calc::Formula::Load::OOXMLRecalcMode::get(xContext));
-    bool bHardRecalc = false;
-    if (nRecalcMode == RECALC_ASK)
-    {
-        if (rDoc.IsUserInteractionEnabled())
-        {
-            // Ask the user if full re-calculation is desired.
-            ScopedVclPtrInstance<QueryBox> aBox(
-                ScDocShell::GetActiveDialogParent(), WinBits(WB_YES_NO | WB_DEF_YES),
-                ScGlobal::GetRscString(STR_QUERY_FORMULA_RECALC_ONLOAD_XLS));
-            aBox->SetCheckBoxText(ScGlobal::GetRscString(STR_ALWAYS_PERFORM_SELECTED));
-
-            sal_Int32 nRet = aBox->Execute();
-            bHardRecalc = nRet == RET_YES;
-
-            if (aBox->GetCheckBoxState())
-            {
-                // Always perform selected action in the future.
-                std::shared_ptr<comphelper::ConfigurationChanges> batch(comphelper::ConfigurationChanges::create());
-                officecfg::Office::Calc::Formula::Load::OOXMLRecalcMode::set(sal_Int32(0), batch);
-                ScFormulaOptions aOpt = SC_MOD()->GetFormulaOptions();
-                aOpt.SetOOXMLRecalcOptions(bHardRecalc ? RECALC_ALWAYS : RECALC_NEVER);
-                /* XXX  is this really supposed to set the ScModule options?
-                 *      Not the ScDocShell options? */
-                SC_MOD()->SetFormulaOptions(aOpt);
-
-                batch->commit();
-            }
-        }
-    }
-    else if (nRecalcMode == RECALC_ALWAYS)
-        bHardRecalc = true;
-
-    if (bHardRecalc)
-        rDocSh.DoHardRecalc(false);
-    else
-        rDoc.CalcFormulaTree(false, true, false);
-}
 
 WorkbookHelper::~WorkbookHelper()
 {

commit 42054677cdb1e41e17e768ef6fdb43be11c5ac43

tdf#63846 assign macros after VBA project fully loaded.
fixes regression caused by d4743045a0b320449d07a957463a76bb8b13f939.

see the documentation for VbaMacroAttacherBase, which indicates that
the macros are to be attached after all objects are finished loading.

Made attachMacros() public so it could be called separately since so
many things are inter-dependent in the logic flow.

Change-Id: I11f7f4a1a75d6f77a97e12e3359d6ea8a995f518

include/oox/ole/vbaproject.hxx
@@ -139,6 +139,9 @@ public:
         VbaMacroAttacherBase class. */
     void                registerMacroAttacher( const VbaMacroAttacherRef& rxAttacher );
 
+    /** Attaches VBA macros to objects registered via registerMacroAttacher(). */
+    void                attachMacros();
+
     /** Returns true, if the document contains at least one code module. */
     bool                hasModules() const;
 
@@ -176,9 +179,6 @@ private:
     void                importVba(
                             StorageBase& rVbaPrjStrg,
                             const GraphicHelper& rGraphicHelper );
-
-    /** Attaches VBA macros to objects registered via registerMacroAttacher(). */
-    void                attachMacros();
 
     /** Copies the entire VBA project storage to the passed document model. */
     void                copyStorage( StorageBase& rVbaPrjStrg );

oox/source/ole/vbaproject.cxx
@@ -253,6 +253,8 @@ void VbaProject::importVba( StorageBase& rVbaPrjStrg, const GraphicHelper& rGrap
 {
     readVbaModules( rVbaPrjStrg );
     importModulesAndForms(rVbaPrjStrg, rGraphicHelper );
+    // attach macros to registered objects
+    attachMacros();
 }
 
 void VbaProject::readVbaModules( StorageBase& rVbaPrjStrg )
@@ -502,9 +504,6 @@ void VbaProject::importModulesAndForms( StorageBase& rVbaPrjStrg, const GraphicH
             }
         }
     }
-
-    // attach macros to registered objects
-    attachMacros();
 }
 
 void VbaProject::attachMacros()

sc/source/filter/oox/workbookfragment.cxx
@@ -508,6 +508,9 @@ void WorkbookFragment::finalizeImport()
         rtl::Reference<oox::core::FragmentHandler> xFragment(new RevisionHeadersFragment(*this, aRevHeadersPath));
         importOoxFragment(xFragment, *xParser);
     }
+
+    // attach macros to registered objects now that all objects have been created.
+    getBaseFilter().getVbaProject().attachMacros();
 }
 
 namespace {