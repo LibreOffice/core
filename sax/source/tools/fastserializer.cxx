7c6aaf66a711ebfb9be9469916a3193efc220591
Tamás Zolnai <tamas.zolnai@collabora.com>
2016-10-28 15:24:51 +0000

    tdf#103544: DOCX exp.: Image loss when have a frame anchored to the same para.
    Regression from:
    83d51e5e52688c4c9bc0ad70a511458bb06a242d
    
    Partly revert the commit causes this regression.
    I checked the related bugs (tdf#78590,tdf#80748)
    intended to be fixed by this commit and reverting
    this part does not bring back the corruption.
    I guess something changed in frames' and text boxes'
    import in the meantime, because this MergeMarks::IGNORE
    is useless now.
    
    Change-Id: If17776e8628561961c7ce2a2994e3fc609f75639
    Reviewed-on: https://gerrit.libreoffice.org/30351
    Reviewed-by: Tamás Zolnai <tamas.zolnai@collabora.com>
    Tested-by: Tamás Zolnai <tamas.zolnai@collabora.com>

--- sax/source/tools/fastserializer.cxx
+++ sax/source/tools/fastserializer.cxx
@@ -407,7 +407,7 @@ namespace sax_fastparser {
         // flush, so that we get everything in getData()
         maCachedOutputStream.flush();
 
-        if (maMarkStack.size() == 1 && eMergeType != MergeMarks::IGNORE)
+        if (maMarkStack.size() == 1)
         {
 #ifdef DBG_UTIL
             while (!maMarkStack.top()->m_DebugEndedElements.empty())
@@ -471,8 +471,6 @@ namespace sax_fastparser {
                     topDebugEndedElements,
                     topDebugStartedElements);
                 break;
-            case MergeMarks::IGNORE:
-                break;
         }
 #endif
         if (maMarkStack.empty())
@@ -490,8 +488,6 @@ namespace sax_fastparser {
             case MergeMarks::APPEND:   maMarkStack.top()->append( aMerge );   break;
             case MergeMarks::PREPEND:  maMarkStack.top()->prepend( aMerge );  break;
             case MergeMarks::POSTPONE: maMarkStack.top()->postpone( aMerge ); break;
-            case MergeMarks::IGNORE:   break;
-
         }
     }
 
