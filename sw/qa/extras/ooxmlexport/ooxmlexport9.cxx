6934adbe25f7aaf9646eb0421308a6fdb5ee9013
Tamás Zolnai <tamas.zolnai@collabora.com>
2016-10-29 15:49:18 +0000

    tdf#103753: DOCX: Handle relative positions to left or right margin
    Positioning relative to left or right margin in MS Word
    works the same as the positioning relative to left or right
    page border in LO Writer.
    
    Change-Id: I476a5e9e76f766b7fb7f1c7f4a068af8bb3c8813

--- sw/qa/extras/ooxmlexport/ooxmlexport9.cxx
+++ sw/qa/extras/ooxmlexport/ooxmlexport9.cxx
@@ -18,6 +18,8 @@
 #include <com/sun/star/text/XTextEmbeddedObjectsSupplier.hpp>
 #include <com/sun/star/text/XTextViewCursorSupplier.hpp>
 #include <com/sun/star/graphic/XGraphic.hpp>
+#include <com/sun/star/text/HoriOrientation.hpp>
+#include <com/sun/star/text/RelOrientation.hpp>
 
 class Test : public SwModelTestBase
 {
@@ -83,6 +85,23 @@ DECLARE_OOXMLEXPORT_TEST(testTdf103544, "tdf103544.docx")
     uno::Reference<beans::XPropertySet> xImage(getShape(1), uno::UNO_QUERY);
     auto xGraphic = getProperty<uno::Reference<graphic::XGraphic> >(xImage, "Graphic");
     CPPUNIT_ASSERT(xGraphic.is());
+}
+
+DECLARE_OOXMLEXPORT_TEST(testTdf103573, "tdf103573.docx")
+{
+    // Relative positions to the left or right margin (MS Word naming) was not handled.
+    uno::Reference<beans::XPropertySet> xShapeProperties( getShape(1), uno::UNO_QUERY );
+    sal_Int16 nValue;
+    xShapeProperties->getPropertyValue("HoriOrient") >>= nValue;
+    CPPUNIT_ASSERT_EQUAL_MESSAGE("Not centered horizontally", text::HoriOrientation::CENTER, nValue);
+    xShapeProperties->getPropertyValue("HoriOrientRelation") >>= nValue;
+    CPPUNIT_ASSERT_EQUAL_MESSAGE("Not centered horizontally relatively to left page border", text::RelOrientation::PAGE_LEFT, nValue);
+
+    xShapeProperties.set( getShape(2), uno::UNO_QUERY );
+    xShapeProperties->getPropertyValue("HoriOrient") >>= nValue;
+    CPPUNIT_ASSERT_EQUAL_MESSAGE("Not centered horizontally", text::HoriOrientation::CENTER, nValue);
+    xShapeProperties->getPropertyValue("HoriOrientRelation") >>= nValue;
+    CPPUNIT_ASSERT_EQUAL_MESSAGE("Not centered horizontally relatively to right page border", text::RelOrientation::PAGE_RIGHT, nValue);
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();
