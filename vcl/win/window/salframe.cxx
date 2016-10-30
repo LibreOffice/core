4f38c077c03cbf4172278d9a7eb2d1fdfcd6d9f0
Jan-Marek Glogowski <glogow@fbihome.de>
2016-10-30 00:14:18 +0000

    WIN simplify system timer / LO event handling
    This removes a level of indirection for the timer callback handling.
    It also ensures we just have a single timeout message queued.
    
    Also drops the 16bit MAX duration limit, as CreateTimerQueueTimer
    uses a DWORD for the DueTime parameter, which is always UINT32.
    
    CreateTimerQueueTimer already sets the period to 0, which makes it
    a one-shot timer, but there is also the WT_EXECUTEONLYONCE, which
    enforces the Period parameter to be 0.
    
    Change-Id: I549142394334bdc098f053b42f222b23cf4fcecd

--- vcl/win/window/salframe.cxx
+++ vcl/win/window/salframe.cxx
@@ -5476,8 +5476,6 @@ LRESULT CALLBACK SalFrameWndProc( HWND hWnd, UINT nMsg, WPARAM wParam, LPARAM lP
         return 0;
     }
 
-    bool bCheckTimers = false;
-
     switch( nMsg )
     {
         case WM_MOUSEMOVE:
@@ -5614,11 +5612,11 @@ LRESULT CALLBACK SalFrameWndProc( HWND hWnd, UINT nMsg, WPARAM wParam, LPARAM lP
             rDef = FALSE;
             break;
         case WM_PAINT:
-            bCheckTimers = ImplHandlePaintMsg( hWnd );
+            ImplHandlePaintMsg( hWnd );
             rDef = FALSE;
             break;
         case SAL_MSG_POSTPAINT:
-            bCheckTimers = ImplHandlePostPaintMsg( hWnd, reinterpret_cast<RECT*>(wParam) );
+            ImplHandlePostPaintMsg( hWnd, reinterpret_cast<RECT*>(wParam) );
             rDef = FALSE;
             break;
 
@@ -5837,24 +5835,6 @@ LRESULT CALLBACK SalFrameWndProc( HWND hWnd, UINT nMsg, WPARAM wParam, LPARAM lP
                 rDef = FALSE;
             }
             break;
-    }
-
-    if( bCheckTimers )
-    {
-        SalData* pSalData = GetSalData();
-        if( pSalData->mnNextTimerTime )
-        {
-            DWORD nCurTime = GetTickCount();
-            if( pSalData->mnNextTimerTime < nCurTime )
-            {
-                MSG aMsg;
-                if( ! PeekMessageW( &aMsg, nullptr, WM_PAINT, WM_PAINT, PM_NOREMOVE | PM_NOYIELD ) )
-                {
-                    BOOL const ret = PostMessageW(pSalData->mpFirstInstance->mhComWnd, SAL_MSG_POSTTIMER, 0, nCurTime);
-                    SAL_WARN_IF(0 == ret, "vcl", "ERROR: PostMessage() failed!");
-                }
-            }
-        }
     }
 
     return nRet;
