c3043a3072465c489d3b20991b17d222771305eb
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2016-10-31 13:28:26 +0100

    tdf#103591 icon theme name resolving, ui fixes, prioritize png
    - Prefer "png" over "svg" because for Tango theme we include both
      "png" and "svg" icon, but rendering of "svg" crashes
    - We save the choice of which icons to use into the profile. When
      32px icons were added the values have shifted - "auto" had value
      2 has became "32px" choice and "auto" has the new value 3. In the
      case of the default "auto", we now always showed 32px icons. This
      commit reverts the "auto" value 2 and puts "32px" choice to 3.
    - Name resolving now always removes the icon file extension and
      adds the ".png" and ".svg" extension before resolving. This
      makes it possible to define the name of the icon without the
      file extension.
    
    Change-Id: I05e3913aaee0037692609ced246954b14a13828a
    Reviewed-on: https://gerrit.libreoffice.org/30440
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>
    Tested-by: Tomaž Vajngerl <quikee@gmail.com>

--- include/svtools/imgdef.hxx
+++ include/svtools/imgdef.hxx
@@ -22,10 +22,10 @@
 
 enum SfxSymbolsSize
 {
-    SFX_SYMBOLS_SIZE_SMALL,
-    SFX_SYMBOLS_SIZE_LARGE,
-    SFX_SYMBOLS_SIZE_32,
-    SFX_SYMBOLS_SIZE_AUTO
+    SFX_SYMBOLS_SIZE_SMALL   = 0,
+    SFX_SYMBOLS_SIZE_LARGE   = 1,
+    SFX_SYMBOLS_SIZE_32      = 3, // keep the numbers as they are written into the profile
+    SFX_SYMBOLS_SIZE_AUTO    = 2,
 };
 
 #endif // INCLUDED_SVTOOLS_IMGDEF_HXX
