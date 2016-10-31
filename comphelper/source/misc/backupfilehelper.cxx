3f0ce9d7e5d0c7b901149fce2a571495713903c0
Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
2016-10-31 16:20:47 +0100

    SafeModeDialog: Add link to user profile
    Change-Id: I0096b3d7db51e28eb58c7209109c30b73b382478

--- comphelper/source/misc/backupfilehelper.cxx
+++ comphelper/source/misc/backupfilehelper.cxx
@@ -2205,6 +2205,11 @@ namespace comphelper
         deleteDirRecursively(maUserConfigWorkURL);
     }
 
+    const OUString& BackupFileHelper::getUserProfileURL()
+    {
+        return maUserConfigBaseURL;
+    }
+
     /////////////////// helpers ///////////////////////
 
     const rtl::OUString BackupFileHelper::getPackURL()
