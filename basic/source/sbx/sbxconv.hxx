3db1e9ed4d0d11457684dd3d2c8cacf97d7e3333
Arnaud Versini <arnaud.versini@gmail.com>
2016-11-01 13:50:03 +0100

    BASIC : remove ImpRound and use std::(l)lround instead
    Change-Id: I1e8f58890395bc6bc29f56c258836db3dc52384c
    Reviewed-on: https://gerrit.libreoffice.org/30464
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

--- basic/source/sbx/sbxconv.hxx
+++ basic/source/sbx/sbxconv.hxx
@@ -37,8 +37,7 @@ void ImpGetIntntlSep( sal_Unicode& rcDecimalSep, sal_Unicode& rcThousandSep );
 
 // SBXINT.CXX
 
-double      ImpRound( double );
-sal_Int16       ImpGetInteger( const SbxValues* );
+sal_Int16   ImpGetInteger( const SbxValues* );
 void        ImpPutInteger( SbxValues*, sal_Int16 );
 
 sal_Int64   ImpGetInt64( const SbxValues* );
