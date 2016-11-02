ebca7cf087e6b03161975fd34cba6c771682b86a
Michael Stahl <mstahl@redhat.com>
2016-11-02 14:13:54 +0100

    poppler: build against libjpeg
    Albert Astals Cid points out that --disable-libjpeg causes poppler to
    use internal JPEG code instead, which is not actively maintained.
    
    Add LIBJPEG_CFLAGS / LIBJPEG_LIBS to config_host.mk and patch poppler to
    respect LIBJPEG_LIBS.
    
    Reviewed-on: https://gerrit.libreoffice.org/30496
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Michael Stahl <mstahl@redhat.com>
    (cherry picked from commit eea709f67d91e271e3df37e6c6724b7b5870b1ee)
    
    jpeg-turbo: resolve some conflicts to fix poppler build
    
    poppler uses both windows.h and jpeg headers, so runs into the conflict
    between Win32 definitions of boolean, INT16, INT32, and jpeg's.
    
    Tweak jpeg-turbo headers so it uses the Win32 definitions.
    (cherry picked from commit f2761b62b3d0b45a3e57781a16c8c5c6ee94ded0)
    
    Change-Id: Id19a62a9bd9dc41fee5e8e43e2798e515aacc299
    Reviewed-on: https://gerrit.libreoffice.org/30506
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: David Tardon <dtardon@redhat.com>

--- external/poppler/UnpackedTarball_poppler.mk
+++ external/poppler/UnpackedTarball_poppler.mk
@@ -19,6 +19,7 @@ $(eval $(call gb_UnpackedTarball_add_patches,poppler,\
 	external/poppler/poppler-0.46.0-std-max.patch.1 \
 	external/poppler/poppler-0.46.0-unused-private-field.patch.1 \
 	external/poppler/ubsan.patch.0 \
+	external/poppler/poppler-libjpeg.patch.1 \
 ))
 
 # vim: set noet sw=4 ts=4:
