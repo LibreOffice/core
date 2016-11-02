1bbf7f653b6b159afb0bf2c34dd463f58333852c
Jochen Nitschke <j.nitschke+logerrit@ok.de>
2016-11-02 12:56:44 +0100

    bin nranges.*, move remaining functions into itemset.cxx
    Change-Id: I1b88d98e00415e9c32a48486912d577a00fbfbda
    Reviewed-on: https://gerrit.libreoffice.org/30486
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
    Tested-by: Noel Grandin <noel.grandin@collabora.co.uk>

--- svl/source/items/itemset.cxx
+++ svl/source/items/itemset.cxx
@@ -29,7 +29,6 @@
 #include <svl/itempool.hxx>
 #include <svl/itemiter.hxx>
 #include <svl/whiter.hxx>
-#include <svl/nranges.hxx>
 #include "whassert.hxx"
 
 #include <tools/stream.hxx>
@@ -39,6 +38,90 @@
 #include "poolio.hxx"
 
 static const sal_uInt16 nInitCount = 10; // Single USHORTs => 5 pairs without '0'
+
+namespace
+{
+
+/**
+ * Creates a sal_uInt16-ranges-array in 'rpRanges' using 'nWh1' and 'nWh2' as
+ * first range, 'nNull' as terminator or start of 2nd range and 'pArgs' as
+ * remainder.
+ *
+ * It returns the number of sal_uInt16s which are contained in the described
+ * set of sal_uInt16s.
+ */
+sal_uInt16 InitializeRanges_Impl( sal_uInt16 *&rpRanges, va_list pArgs,
+                               sal_uInt16 nWh1, sal_uInt16 nWh2, sal_uInt16 nNull )
+{
+    sal_uInt16 nSize = 0, nIns = 0;
+    std::vector<sal_uInt16> aNumArr;
+    aNumArr.push_back( nWh1 );
+    aNumArr.push_back( nWh2 );
+    DBG_ASSERT( nWh1 <= nWh2, "Invalid range" );
+    nSize += nWh2 - nWh1 + 1;
+    aNumArr.push_back( nNull );
+    bool bEndOfRange = false;
+    while ( 0 !=
+            ( nIns =
+              sal::static_int_cast< sal_uInt16 >(
+                  va_arg( pArgs, int ) ) ) )
+    {
+        bEndOfRange = !bEndOfRange;
+        if ( bEndOfRange )
+        {
+            const sal_uInt16 nPrev(*aNumArr.rbegin());
+            DBG_ASSERT( nPrev <= nIns, "Invalid range" );
+            nSize += nIns - nPrev + 1;
+        }
+        aNumArr.push_back( nIns );
+    }
+
+    assert( bEndOfRange ); // odd number of WhichIds
+
+    // Now all ranges are present
+    rpRanges = new sal_uInt16[ aNumArr.size() + 1 ];
+    std::copy( aNumArr.begin(), aNumArr.end(), rpRanges);
+    *(rpRanges + aNumArr.size()) = 0;
+
+    return nSize;
+}
+
+/**
+ * Determines the number of sal_uInt16s in a 0-terminated array of pairs of
+ * sal_uInt16s.
+ * The terminating 0 is not included in the count.
+ */
+sal_uInt16 Count_Impl( const sal_uInt16 *pRanges )
+{
+    sal_uInt16 nCount = 0;
+    while ( *pRanges )
+    {
+        nCount += 2;
+        pRanges += 2;
+    }
+    return nCount;
+}
+
+/**
+ * Determines the total number of sal_uInt16s described in a 0-terminated
+ * array of pairs of sal_uInt16s, each representing an range of sal_uInt16s.
+ */
+sal_uInt16 Capacity_Impl( const sal_uInt16 *pRanges )
+{
+    sal_uInt16 nCount = 0;
+
+    if ( pRanges )
+    {
+        while ( *pRanges )
+        {
+            nCount += pRanges[1] - pRanges[0] + 1;
+            pRanges += 2;
+        }
+    }
+    return nCount;
+}
+
+}
 
 /**
  * Ctor for a SfxItemSet with exactly the Which Ranges, which are known to
