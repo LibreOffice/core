4f7874e5725f6b26aa70362ccdc5566a8a94fe8b
David Tardon <dtardon@redhat.com>
2016-11-03 16:36:50 +0100

    fix external deps for cairo
    Change-Id: Iba03576ee9a114ea8dc20d8ff70a953e11c6d3e7

--- external/cairo/ExternalProject_cairo.mk
+++ external/cairo/ExternalProject_cairo.mk
@@ -11,6 +11,11 @@ $(eval $(call gb_ExternalProject_ExternalProject,cairo))
 
 $(eval $(call gb_ExternalProject_use_external_project,cairo,pixman))
 
+$(eval $(call gb_ExternalProject_use_externals,cairo,\
+	png \
+	zlib \
+))
+
 ifeq ($(OS),ANDROID)
 $(eval $(call gb_ExternalProject_use_unpacked,cairo,fontconfig))
 $(eval $(call gb_ExternalProject_use_unpacked,cairo,png))
@@ -40,9 +45,9 @@ $(call gb_ExternalProject_get_state_target,cairo,build) :
 		$(if $(filter ANDROID IOS,$(OS)),CFLAGS="$(if $(debug),-g) $(ZLIB_CFLAGS) $(gb_VISIBILITY_FLAGS)") \
 		$(if $(filter-out ANDROID IOS,$(OS)),CFLAGS="$(if $(debug),-g) $(ZLIB_CFLAGS)" ) \
 		$(if $(filter ANDROID IOS,$(OS)),PKG_CONFIG=./dummy_pkg_config) \
+		LDFLAGS="$(ZLIB_LIBS)" \
 		pixman_CFLAGS="-I$(call gb_UnpackedTarball_get_dir,pixman)/pixman" \
 		pixman_LIBS="-L$(call gb_UnpackedTarball_get_dir,pixman)/pixman/.libs -lpixman-1" \
-		ZLIB3RDLIB="$(if $(SYSTEM_ZLIB),,-L$(WORKDIR)/LinkTarget/StaticLibrary) -lz" \
 		png_REQUIRES="trick_configure_into_using_png_CFLAGS_and_LIBS" \
 		png_CFLAGS="$(LIBPNG_CFLAGS)" png_LIBS="$(LIBPNG_LIBS)" \
 		$(if $(SYSTEM_FREETYPE),,FREETYPE_CFLAGS="-I$(call gb_UnpackedTarball_get_dir,freetype)/include") \
