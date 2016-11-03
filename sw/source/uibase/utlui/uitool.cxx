415f11facfe6c658b10b32f88618e4cb0f158e9d
Miklos Vajna <vmiklos@collabora.co.uk>
2016-11-03 09:16:09 +0100

    tdf#102308 sw redline tooltip: show seconds
    Commit d9dca1eef91faa710112ffd20a2b2d36b48a9287 (tdf#102308 sw: improve
    redline timestamp precision) improved the manage changes dialog to show
    seconds of the redline timestamps, do the same with tooltips, when the
    mouse is hovered over a redline portion.
    
    Change-Id: I6ec42db49cb93d8bce0cdee02e37d871f8e7e2ce

--- sw/source/uibase/utlui/uitool.cxx
+++ sw/source/uibase/utlui/uitool.cxx
@@ -787,7 +787,7 @@ OUString GetAppLangDateTimeString( const DateTime& rDT )
 {
     const SvtSysLocale aSysLocale;
     const LocaleDataWrapper& rAppLclData = aSysLocale.GetLocaleData();
-    OUString sRet = rAppLclData.getDate( rDT ) + " " + rAppLclData.getTime( rDT, false );
+    OUString sRet = rAppLclData.getDate( rDT ) + " " + rAppLclData.getTime( rDT );
     return sRet;
 }
 
