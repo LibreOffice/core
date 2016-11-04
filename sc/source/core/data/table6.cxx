48f97944c9962a42cfb389b7ff74f3144b00a0b3
Michael Meeks <michael.meeks@collabora.com>
2016-11-04 09:46:29 +0000

    tdf#91260 - ReplaceAll should use a columular direction.
    MDDS is columnular, so this shrinks the working set very
    significantly, and should improve performance.
    
    Change-Id: I2aff92b31bfc459e85291059d977541aa65155cf

--- sc/source/core/data/table6.cxx
+++ sc/source/core/data/table6.cxx
@@ -571,6 +571,10 @@ bool ScTable::ReplaceAll(
     else
         GetLastDataPos(nLastCol, nLastRow);
 
+    // tdf#92160 - columnular replace is faster, and more memory efficient.
+    SvxSearchItem aCopyItem(rSearchItem);
+    aCopyItem.SetRowDirection(false);
+
     bool bEverFound = false;
     while (true)
     {
