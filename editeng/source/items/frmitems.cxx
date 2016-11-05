LibreOffice/core

commit e598ab04476a32a08f18e8f0662fafa5f78f1a4a

n#775270: clip pictures instead of scaling they don't fit
Word clips pictures that are bigger than a page instead of scaling them
down. This patch introduces a new compatibility option to allow clipping
a picture in Writer instead of scaling it down.

Change-Id: I4defbee05be81e23ec28a2ed272eaf4e4cc6faf5

sw/source/core/doc/notxtfrm.cxx
@@ -49,6 +49,7 @@
 #include <fesh.hxx>
 #include <doc.hxx>
 #include <flyfrm.hxx>
+#include <flyfrms.hxx>
 #include <frmtool.hxx>
 #include <viewopt.hxx>
 #include <viewimp.hxx>
@@ -294,6 +295,16 @@ void SwNoTxtFrm::Paint(SwRect const& rRect, SwPrintData const*const) const
 
     SwRect aGrfArea( Frm() );
     SwRect aPaintArea( aGrfArea );
+
+    // In case the picture fly frm was clipped, render it with the origin
+    // size instead of scaling it
+    if ( rNoTNd.getIDocumentSettingAccess()->get( IDocumentSettingAccess::CLIPPED_PICTURES ) )
+    {
+        const SwFlyFreeFrm *pFly = dynamic_cast< const SwFlyFreeFrm* >( FindFlyFrm() );
+        if( pFly )
+            aGrfArea = SwRect( Frm().Pos( ), pFly->GetUnclippedFrm( ).SSize( ) );
+    }
+
     aPaintArea._Intersection( aOrigPaint );
 
     SwRect aNormal( Frm().Pos() + Prt().Pos(), Prt().SSize() );

sw/inc/IDocumentSettingAccess.hxx
@@ -84,6 +84,7 @@ namespace com { namespace sun { namespace star { namespace i18n { struct Forbidd
          SMALL_CAPS_PERCENTAGE_66,
          TAB_OVERFLOW,
          UNBREAKABLE_NUMBERINGS,
+         CLIPPED_PICTURES,
          // COMPATIBILITY FLAGS END
 
          BROWSE_MODE,

sw/inc/doc.hxx
@@ -589,6 +589,7 @@ private:
     bool mbSmallCapsPercentage66;
     bool mbTabOverflow;
     bool mbUnbreakableNumberings;
+    bool mbClippedPictures;
 
     bool mbLastBrowseMode                           : 1;
 

sw/source/core/doc/doc.cxx
@@ -199,6 +199,7 @@ bool SwDoc::get(/*[in]*/ DocumentSettingId id) const
         case SMALL_CAPS_PERCENTAGE_66: return mbSmallCapsPercentage66;
         case TAB_OVERFLOW: return mbTabOverflow;
         case UNBREAKABLE_NUMBERINGS: return mbUnbreakableNumberings;
+        case CLIPPED_PICTURES: return mbClippedPictures;
 
         case BROWSE_MODE: return mbLastBrowseMode; // Attention: normally the ViewShell has to be asked!
         case HTML_MODE: return mbHTMLMode;
@@ -343,6 +344,11 @@ void SwDoc::set(/*[in]*/ DocumentSettingId id, /*[in]*/ bool value)
         case UNBREAKABLE_NUMBERINGS:
             mbUnbreakableNumberings = value;
             break;
+
+        case CLIPPED_PICTURES:
+            mbClippedPictures = value;
+            break;
+
          // COMPATIBILITY FLAGS END
 
         case BROWSE_MODE: //can be used temporary (load/save) when no ViewShell is avaiable

sw/source/core/doc/docnew.cxx
@@ -358,6 +358,7 @@ SwDoc::SwDoc()
     mbTabOverflow                           = true;
     mbUnbreakableNumberings                 = false;
     mbFloattableNomargins                   = false;
+    mbClippedPictures                       = false;
 
     //
     // COMPATIBILITY FLAGS END

sw/source/core/inc/flyfrms.hxx
@@ -47,6 +47,8 @@ class SwFlyFreeFrm : public SwFlyFrm
     // #i37068# - flag to prevent move in method <CheckClip(..)>
     bool mbNoMoveOnCheckClip;
 
+    SwRect maUnclippedFrm;
+
     void CheckClip( const SwFmtFrmSize &rSz );  //'Emergency' Clipping.
 
     /** determines, if direct environment of fly frame has 'auto' size
@@ -104,6 +106,14 @@ public:
         {
             return false;
         }
+    }
+
+    inline const SwRect& GetUnclippedFrm( ) const
+    {
+        if ( maUnclippedFrm.HasArea( ) )
+            return maUnclippedFrm;
+        else
+            return Frm();
     }
 
     /** method to determine, if a format on the Writer fly frame is possible

sw/source/core/layout/flylay.cxx
@@ -74,7 +74,8 @@ SwFlyFreeFrm::SwFlyFreeFrm( SwFlyFrmFmt *pFmt, SwFrm* pSib, SwFrm *pAnch ) :
     // #i34753#
     mbNoMakePos( false ),
     // #i37068#
-    mbNoMoveOnCheckClip( false )
+    mbNoMoveOnCheckClip( false ),
+    maUnclippedFrm( )
 {
 }
 
@@ -447,6 +448,7 @@ void SwFlyFreeFrm::CheckClip( const SwFmtFrmSize &rSz )
             // otherwise we'll end up with unwanted side-effects
             const long nPrtHeightDiff = Frm().Height() - Prt().Height();
             const long nPrtWidthDiff  = Frm().Width()  - Prt().Width();
+            maUnclippedFrm = SwRect( Frm() );
             Frm().Height( aFrmRect.Height() );
             Frm().Width ( Max( long(MINLAY), aFrmRect.Width() ) );
             if ( Lower() && Lower()->IsColumnFrm() )

sw/source/filter/ww8/ww8par.cxx
@@ -1536,6 +1536,7 @@ void SwWW8ImplReader::ImportDop()
     rDoc.set(IDocumentSettingAccess::COLLAPSE_EMPTY_CELL_PARA, true);
     rDoc.set(IDocumentSettingAccess::TAB_OVERFLOW, true);
     rDoc.set(IDocumentSettingAccess::UNBREAKABLE_NUMBERINGS, true);
+    rDoc.set(IDocumentSettingAccess::CLIPPED_PICTURES, true);
 
     //
     // COMPATIBILITY FLAGS END

commit d0e2552a8408e4a4dc8e3518364d69e6dd4d9f58

tdf#90070 don't clip flys with borders
regression from commit e598ab04476a32a08f18e8f0662fafa5f78f1a4a
very aggressively forced a new frame size via compat setting
CLIPPED_PICTURES on any fly - not just images.

This only affects MS-format documents, EXCEPT that it is a document
property, so if the file every spent any part of it's life in MS-format,
it will always retain that compatibility setting. That explains
why the problem was intermittent for me - and was hard to reproduce
in a clean document, even though I'd seen it in .ODTs.

bIgnoreLine (ignore the fact that there is no visible line)
was a confusing word choice for "if there is no line,
then return a spacing size of zero". bEvenIfNoLine=false is better.

Change-Id: I50a3bdef3a67339ae517ee6319920651bc56f9be

editeng/source/items/frmitems.cxx
@@ -2421,7 +2421,7 @@ void SvxBoxItem::SetDistance( sal_uInt16 nNew, SvxBoxItemLine nLine )
 }
 
 
-sal_uInt16 SvxBoxItem::CalcLineSpace( SvxBoxItemLine nLine, bool bIgnoreLine ) const
+sal_uInt16 SvxBoxItem::CalcLineSpace( SvxBoxItemLine nLine, bool bEvenIfNoLine ) const
 {
     SvxBorderLine* pTmp = nullptr;
     sal_uInt16 nDist = 0;
@@ -2451,9 +2451,17 @@ sal_uInt16 SvxBoxItem::CalcLineSpace( SvxBoxItemLine nLine, bool bIgnoreLine ) c
     {
         nDist = nDist + pTmp->GetScaledWidth();
     }
-    else if( !bIgnoreLine )
+    else if( !bEvenIfNoLine )
         nDist = 0;
     return nDist;
+}
+
+bool SvxBoxItem::HasBorder( bool bTreatPaddingAsBorder ) const
+{
+    return  CalcLineSpace( SvxBoxItemLine::BOTTOM,   bTreatPaddingAsBorder )
+            || CalcLineSpace( SvxBoxItemLine::RIGHT, bTreatPaddingAsBorder )
+            || CalcLineSpace( SvxBoxItemLine::TOP,   bTreatPaddingAsBorder )
+            || CalcLineSpace( SvxBoxItemLine::LEFT,  bTreatPaddingAsBorder );
 }
 
 // class SvxBoxInfoItem --------------------------------------------------

include/editeng/boxitem.hxx
@@ -109,8 +109,9 @@ public:
     void SetRemoveAdjacentCellBorder( bool bSet ) { bRemoveAdjCellBorder = bSet; }
 
     // Line width plus Space plus inward distance
-    // bIgnoreLine = TRUE -> Also return distance, when no Line is set
-    sal_uInt16  CalcLineSpace( SvxBoxItemLine nLine, bool bIgnoreLine = false ) const;
+    // bEvenIfNoLine = TRUE -> Also return distance, when no Line is set
+    sal_uInt16  CalcLineSpace( SvxBoxItemLine nLine, bool bEvenIfNoLine = false ) const;
+    bool HasBorder( bool bTreatPaddingAsBorder = false ) const;
     static css::table::BorderLine2 SvxLineToLine( const editeng::SvxBorderLine* pLine, bool bConvert );
     static bool LineToSvxLine(const css::table::BorderLine& rLine, editeng::SvxBorderLine& rSvxLine, bool bConvert);
     static bool LineToSvxLine(const css::table::BorderLine2& rLine, editeng::SvxBorderLine& rSvxLine, bool bConvert);

sw/source/core/doc/notxtfrm.cxx
@@ -270,11 +270,23 @@ void SwNoTextFrame::Paint(vcl::RenderContext& rRenderContext, SwRect const& rRec
 
     // In case the picture fly frm was clipped, render it with the origin
     // size instead of scaling it
-    if ( rNoTNd.getIDocumentSettingAccess()->get( DocumentSettingId::CLIPPED_PICTURES ) )
+    if ( pGrfNd && rNoTNd.getIDocumentSettingAccess()->get( DocumentSettingId::CLIPPED_PICTURES ) )
     {
         const SwFlyFreeFrame *pFly = dynamic_cast< const SwFlyFreeFrame* >( FindFlyFrame() );
         if( pFly )
-            aGrfArea = SwRect( Frame().Pos( ), pFly->GetUnclippedFrame( ).SSize( ) );
+        {
+            bool bGetUnclippedFrame=true;
+            const SfxPoolItem* pItem;
+            if( pFly->GetFormat() && SfxItemState::SET == pFly->GetFormat()->GetItemState(RES_BOX, false, &pItem) )
+            {
+                const SvxBoxItem& rBox = *static_cast<const SvxBoxItem*>(pItem);
+                if( rBox.HasBorder( /*bTreatPaddingAsBorder*/true) )
+                    bGetUnclippedFrame = false;
+            }
+
+            if( bGetUnclippedFrame )
+                aGrfArea = SwRect( Frame().Pos( ), pFly->GetUnclippedFrame( ).SSize( ) );
+        }
     }
 
     aPaintArea.Intersection_( aOrigPaint );