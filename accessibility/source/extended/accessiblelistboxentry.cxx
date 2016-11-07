8955c3fde60b0621527e4b91576e49778494f926
Noel Grandin <noel.grandin@collabora.co.uk>
2016-11-07 15:59:37 +0200

    loplugin:oncevar
    Change-Id: I44fb6858eeff14fcbd9fdfbbb0aabd1433b6a27d
    Reviewed-on: https://gerrit.libreoffice.org/30668
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

--- accessibility/source/extended/accessiblelistboxentry.cxx
+++ accessibility/source/extended/accessiblelistboxentry.cxx
@@ -802,8 +802,6 @@ namespace accessibility
         checkActionIndex_Impl( nIndex );
         EnsureIsAlive();
 
-        static const char sActionDesc1[] = "Check";
-        static const char sActionDesc2[] = "UnCheck";
         // sal_Bool bHasButtons = (getListBox()->GetStyle() & WB_HASBUTTONS)!=0;
         SvTreeListEntry* pEntry = getListBox()->GetEntryFromPath( m_aEntryPath );
         SvButtonState state = getListBox()->GetCheckButtonState( pEntry );
@@ -813,9 +811,9 @@ namespace accessibility
             if(getAccessibleRole() == AccessibleRole::CHECK_BOX)
             {
                 if ( state == SvButtonState::Checked )
-                    return OUString(sActionDesc2);
+                    return OUString("UnCheck");
                 else if (state == SvButtonState::Unchecked)
-                    return OUString(sActionDesc1);
+                    return OUString("Check");
             }
             else
             {
