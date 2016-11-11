8909cbeb315e257fbe06dad6caee5db18879b916
Caolán McNamara <caolanm@redhat.com>
2016-11-11 16:45:22 +0000

    coverity#1371239 Missing move assignment operator
    Change-Id: Ie5cddac8b4b0c8eefee82a14c845982600df0455

--- connectivity/source/inc/file/fcode.hxx
+++ connectivity/source/inc/file/fcode.hxx
@@ -44,7 +44,16 @@ namespace connectivity
         class OOO_DLLPUBLIC_FILE OCode
         {
         public:
+            //virtual dtor to allow this to be the root of the class hierarchy
             virtual ~OCode() = default;
+            //but that disables the default move ctor
+            OCode(OCode&&) = default;
+            //but that disables the rest of default ctors
+            OCode(const OCode&) = default;
+            OCode() = default;
+            //and same issue for the assignment operators
+            OCode& operator=(const OCode&) = default;
+            OCode& operator=(OCode&&) = default;
 
             inline static void * SAL_CALL operator new( size_t nSize )
                 { return ::rtl_allocateMemory( nSize ); }
