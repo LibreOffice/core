bb67a3f9847eb364c1d0aea82b97962bfabc3453
Julien Nabet <serval2412@yahoo.fr>
2016-11-11 20:53:49 +0100

    tdf#71251: VARCHAR max length is 32765 for Firebird
    At least for Firebird 2.5 (see http://www.firebirdsql.org/en/firebird-technical-specifications/)
    Let's put the same for CHAR
    
    Cherry-picked from ba8a41bac3765f7b3562d357aeb13f752ea14ba4
    Reviewed-on: https://gerrit.libreoffice.org/30783
    Reviewed-by: Julien Nabet <serval2412@yahoo.fr>
    Tested-by: Julien Nabet <serval2412@yahoo.fr>
    
    Change-Id: I4ab232689093b0418a7c1a9751685f41f2dabbf7
    Reviewed-on: https://gerrit.libreoffice.org/30786
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Lionel Elie Mamane <lionel@mamane.lu>

--- connectivity/source/drivers/firebird/DatabaseMetaData.cxx
+++ connectivity/source/drivers/firebird/DatabaseMetaData.cxx
@@ -903,7 +903,7 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getTypeInfo()
         // SQL_TEXT
         aRow[1] = new ORowSetValueDecorator(OUString("CHAR"));
         aRow[2] = new ORowSetValueDecorator(getColumnTypeFromFBType(SQL_TEXT));
-        aRow[3] = new ORowSetValueDecorator(sal_Int16(32767)); // Prevision = max length
+        aRow[3] = new ORowSetValueDecorator(sal_Int16(32765)); // Prevision = max length
         aRow[6] = new ORowSetValueDecorator(OUString("length")); // Create Params
         aRow[9] = new ORowSetValueDecorator(
                 sal_Int16(ColumnSearch::FULL)); // Searchable
@@ -915,7 +915,7 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getTypeInfo()
         // SQL_VARYING
         aRow[1] = new ORowSetValueDecorator(OUString("VARCHAR"));
         aRow[2] = new ORowSetValueDecorator(getColumnTypeFromFBType(SQL_VARYING));
-        aRow[3] = new ORowSetValueDecorator(sal_Int16(32767)); // Prevision = max length
+        aRow[3] = new ORowSetValueDecorator(sal_Int16(32765)); // Prevision = max length
         aRow[6] = new ORowSetValueDecorator(OUString("length")); // Create Params
         aRow[9] = new ORowSetValueDecorator(
                 sal_Int16(ColumnSearch::FULL)); // Searchable
