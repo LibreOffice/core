3d2655ee883a4b77caffa268d457cdaa3a3ff21b
Marco Cecchetti <marco.cecchetti@collabora.com>
2016-11-17 12:42:34 +0100

    LOK - calc: now PgUp/PgDn offset is used only when PgUp/PgDn are pressed
    Change-Id: I340d747021bc794361d6b8eff76335345b60010c

--- sc/source/ui/view/viewdata.cxx
+++ sc/source/ui/view/viewdata.cxx
@@ -1735,11 +1735,6 @@ SCROW ScViewData::CellsAtY( SCsROW nPosY, SCsROW nDir, ScVSplitPos eWhichY, sal_
 
     if (nScrSizeY == SC_SIZE_NONE) nScrSizeY = (sal_uInt16) aScrSize.Height();
 
-    if (comphelper::LibreOfficeKit::isActive() && m_nLOKPageUpDownOffset > 0)
-    {
-        nScrSizeY = ToPixel( m_nLOKPageUpDownOffset, nPPTY );
-    }
-
     SCROW nY;
 
     if (nDir==1)
