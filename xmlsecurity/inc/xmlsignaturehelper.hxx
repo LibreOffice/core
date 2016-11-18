e6122f621039f6377312bd6992f535e3a9171528
Miklos Vajna <vmiklos@collabora.co.uk>
2016-11-18 14:17:25 +0100

    xmlsecurity: clean up unused mxSecurityController
    Change-Id: Ife64ab3683479baf152357a6167718f13c9b6089

--- xmlsecurity/inc/xmlsignaturehelper.hxx
+++ xmlsecurity/inc/xmlsignaturehelper.hxx
@@ -91,20 +91,13 @@ class XMLSECURITY_DLLPUBLIC XMLSignatureHelper
 {
 private:
     css::uno::Reference< css::uno::XComponentContext > mxCtx;
-    // FIXME: This field appears to be unused (it is only assigned to in the XMLSignatureHelper
-    // constructor), but it can't be removed as it is that very assignment which causes the object
-    // it to be acquired, and otherwise its reference counting will be borked and we get a
-    // crash. This is stupid of course. Probably we should just kill the separate XSecController
-    // class and move its contents inside this class. Nothing else uses XSecController anyway, as
-    // far as I see.
-    css::uno::Reference< css::xml::crypto::sax::XSecurityController > mxSecurityController;
     css::uno::Reference< css::xml::crypto::XUriBinding > mxUriBinding;
 
     std::vector<XMLSignatureCreationResult>
                                 maCreationResults;
     std::vector<XMLSignatureVerifyResult>
                                 maVerifyResults;
-    XSecController*             mpXSecController;
+    css::uno::Reference<XSecController> mpXSecController;
     bool                        mbError;
     bool mbODFPre1_2;
     Link<LinkParamNone*,bool>   maStartVerifySignatureHdl;
