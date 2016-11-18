d89925fc869ceac1c7ae5de4b9df8929c656a348
Miklos Vajna <vmiklos@collabora.co.uk>
2016-11-18 08:12:33 +0100

    starmath: fix loplugin:cppunitassertequals warnings
    Change-Id: I49bddc1e51550c1696a1e93ce22e6b85bb663cd6

--- sc/qa/unit/bugfix-test.cxx
+++ sc/qa/unit/bugfix-test.cxx
@@ -212,8 +212,8 @@ void ScFiltersTest::testTdf91979()
     Point aPos = aViewData.GetScrPos(MAXCOL - 1, 10000, SC_SPLIT_TOPLEFT, true);
     int nColWidth = ScViewData::ToPixel(pDoc->GetColWidth(0, 0), aViewData.GetPPTX());
     int nRowHeight = ScViewData::ToPixel(pDoc->GetRowHeight(0, 0), aViewData.GetPPTY());
-    CPPUNIT_ASSERT(aPos.getX() == (MAXCOL - 1) * nColWidth);
-    CPPUNIT_ASSERT(aPos.getY() == 10000 * nRowHeight);
+    CPPUNIT_ASSERT_EQUAL(static_cast<long>((MAXCOL - 1) * nColWidth), aPos.getX());
+    CPPUNIT_ASSERT_EQUAL(static_cast<long>(10000 * nRowHeight), aPos.getY());
 
     xComponent->dispose();
 }
