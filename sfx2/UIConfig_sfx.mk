331d661c8081f28ba08516ff9d93ddd95739b62e
Miklos Vajna <vmiklos@collabora.co.uk>
2016-11-18 11:28:45 +0100

    sfx2: let .uno:SignPDF result in a warning before editing the doc
    Editing such a document is most probably not what you want, help the
    user to avoid the trouble.
    
    Change-Id: I87d52d89e12658675b580b7dc21fb38fe41dd777

--- sfx2/UIConfig_sfx.mk
+++ sfx2/UIConfig_sfx.mk
@@ -20,6 +20,7 @@ $(eval $(call gb_UIConfig_add_uifiles,sfx,\
 	sfx2/uiconfig/ui/documentfontspage \
 	sfx2/uiconfig/ui/documentinfopage \
 	sfx2/uiconfig/ui/documentpropertiesdialog \
+	sfx2/uiconfig/ui/editdocumentdialog \
 	sfx2/uiconfig/ui/editdurationdialog \
 	sfx2/uiconfig/ui/emojicontrol \
 	sfx2/uiconfig/ui/errorfindemaildialog \
