de378b010c80d16ace019039592b75d34f52a26f
Khaled Hosny <khaledhosny@eglug.org>
2016-11-23 03:55:21 +0200

    Revert "tdf#89870: Add config option for layout engine"
    This reverts commit 6324efd70dfa8c6be84cba1fa29658e3373cbbe3.
    
    The old layout engines will soon be gone.
    
    Change-Id: Iedbcffd18b11ac36ad98a66ac12111058aab19b3

--- vcl/source/gdi/sallayout.cxx
+++ vcl/source/gdi/sallayout.cxx
@@ -34,8 +34,6 @@
 
 #include <i18nlangtag/lang.h>
 
-#include <officecfg/Office/Common.hxx>
-
 #include <tools/debug.hxx>
 #include <vcl/svapp.hxx>
 
@@ -772,8 +770,7 @@ bool SalLayout::IsSpacingGlyph( sal_GlyphId nGlyph )
 
 bool SalLayout::UseCommonLayout()
 {
-    static bool bUse = (getenv("SAL_NO_COMMON_LAYOUT") == nullptr) &&
-                       (officecfg::Office::Common::VCL::TextLayoutEngine::get() == "new");
+    static bool bUse = getenv("SAL_NO_COMMON_LAYOUT") == nullptr;
     return bUse;
 }
 
