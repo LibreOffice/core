9af78ca4dee2781916702960a29ead7e8b0f4aca
David Ostrovsky <david@ostrovsky.org>
2016-11-23 23:53:55 +0100

    Support MSVC 15.0
    New compiler changes quite some stuff:
    
    * Detection done based on different registry key
    * .NET SDK detection done based on different registry key
    * Compiler, include and library location directories changed
    * Some library versions still have 14 in their names. It's unclear
      to me, if this is going to be changed in the final release
    
    Known issues:
    
    * MSBuild 15.0 that is shipped with MSVC 15.0 includes Toolset 14.1
    and has some issues building external projects. Particularly, the
    new SDK 10.0 doesn't seem to be supported out of the box. Some
    tweaks were needed to re-target the existing vcproj files to support
    it. For now use MSBuild from MSVC 14.0 distribution.
    
    * Devenv.exe returns 1 from /upgrade command and thus cannot be used.
    It turns out, that it was optional anyway, omitting this step didn't
    change anything.
    
    Tested with this configuration: [1].
    Status: LO is up and running on MSVC 15.0: [2].
    
    * [1] http://paste.openstack.org/show/590594
    * [2] http://ostrovsky.org/libo/LibreOfficeDev_5.4.0.0.alpha0_Win_x64.msi
    
    Change-Id: I148d7932aff43bbbd07bd493504df974726234c2

--- bridges/inc/except.hxx
+++ bridges/inc/except.hxx
@@ -20,7 +20,7 @@
 #ifndef INCLUDED_BRIDGES_INC_EXCEPT_HXX
 #define INCLUDED_BRIDGES_INC_EXCEPT_HXX
 
-#if _MSC_VER >= 1900 // VC 2015 (and later?)
+#if _MSC_VER >= 1900 // VC 2015/2017 (and later?)
 // extern "C" void** __cdecl __current_exception()
 // is defined in MSVS14.0/VC/crt/src/vcruntime/frame.cpp:
 // return &__vcrt_getptd()->_curexception;
