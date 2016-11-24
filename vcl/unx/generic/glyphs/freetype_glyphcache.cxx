3e653534a3df5fe55a6f38dfa60cd76e6d4792f6
Caolán McNamara <caolanm@redhat.com>
2016-11-24 08:54:11 +0000

    move the FontConfigFontOptions out of FreetypeFontInstance now
    Change-Id: Ic6ff21533a53df16fed4ee086f78ff368cb23afd
    Reviewed-on: https://gerrit.libreoffice.org/31130
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>
    (cherry picked from commit b30e298c3f8d9202a14e0ca08120ccde31e413ee)
    (cherry picked from commit c9462ce4c30a6642826a8363de2a11ed9142b5fb)

--- vcl/unx/generic/glyphs/freetype_glyphcache.cxx
+++ vcl/unx/generic/glyphs/freetype_glyphcache.cxx
@@ -607,10 +607,9 @@ const std::shared_ptr<FontConfigFontOptions>& FreetypeFont::GetFontOptions() con
 {
     if (!mxFontOptions)
     {
-        SAL_WARN("vcl", "this doesn't happen in practice I believe");
         mxFontOptions.reset(GetFCFontOptions(mpFontInfo->GetFontAttributes(), maFontSelData.mnHeight));
+        mxFontOptions->SyncPattern(GetFontFileName(), GetFontFaceIndex(), NeedsArtificialBold());
     }
-    mxFontOptions->SyncPattern(GetFontFileName(), GetFontFaceIndex(), NeedsArtificialBold());
     return mxFontOptions;
 }
 
