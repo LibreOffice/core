8ceb8b2d141a249127822bd511e66dd855fb1e59
Caolán McNamara <caolanm@redhat.com>
2016-11-24 11:43:38 +0000

    Related: coverity#1371267 copy ctor broken, nPagePos not copied
    initially operator= was removed by
    
    commit a0a919d2b541c415ad9b81d2ee91895bf106e9bb
    Date:   Fri Oct 3 10:39:28 2014 +0200
    
        remove SvRefBase::QueryDelete
    
    and then operator= was restored by
    
    commit faa6455995966e41653204779d055b885b27fc4c
    Date:   Fri Oct 10 12:02:32 2014 +0200
    
        add copy constructor for ONDXPagePtr class
    
        which I forgot in commit a0a919d2b541c415ad9b81d2ee91895bf106e9bb
        "remove SvRefBase::QueryDelete"
    
    but nPagePos was missed
    
    Change-Id: I6497934d4a199f5ea94cf2da840c164910e7a826

--- connectivity/source/drivers/dbase/dindexnode.cxx
+++ connectivity/source/drivers/dbase/dindexnode.cxx
@@ -858,6 +858,7 @@ ONDXPagePtr& ONDXPagePtr::operator=(ONDXPagePtr const & rOther)
     }
     ONDXPage * pOldObj = mpPage;
     mpPage = rOther.mpPage;
+    nPagePos = rOther.nPagePos;
     if (pOldObj != nullptr) {
         pOldObj->ReleaseRef();
     }
