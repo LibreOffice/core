8f08d48780dd4877a3949f39b424b8e097ee7e17
Tor Lillqvist <tml@collabora.com>
2016-11-24 11:31:28 +0200

    Bin copy-pasted comment that is meaningless here
    Change-Id: Ie18a1bd4c006a9c7a54dc79747cb7e300315640a

--- xmlsecurity/source/xmlsec/errorcallback.cxx
+++ xmlsecurity/source/xmlsec/errorcallback.cxx
@@ -17,10 +17,6 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-
-/*
- * Implementation of the I/O interfaces based on stream and URI binding
- */
 #include "errorcallback.hxx"
 
 #include "xmlsec-wrapper.h"
