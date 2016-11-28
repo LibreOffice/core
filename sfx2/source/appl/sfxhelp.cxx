bdb5b7300be0247757bda6e365f98408c0fe34dd
pv2k <vidhey96@gmail.com>
2016-11-28 21:48:43 +0530

    tdf#103391 online help functionality added
    opens a dialog box from which user can choose to take online help or cancel it
    
    Change-Id: I8c1278aab88d55901a65c38b15fcc8519360dc59

--- sfx2/source/appl/sfxhelp.cxx
+++ sfx2/source/appl/sfxhelp.cxx
@@ -613,12 +613,24 @@ bool SfxHelp::Start_Impl(const OUString& rURL, const vcl::Window* pWindow, const
 
     if ( !impl_hasHelpInstalled() )
     {
-        if ( impl_showOnlineHelp( aHelpURL ) )
-            return true;
+        ScopedVclPtrInstance< MessageDialog > aQueryBox(const_cast< vcl::Window* >( pWindow ),"onlinehelpmanual","sfx/ui/helpmanual.ui");
+        short OnlineHelpBox = aQueryBox->Execute();
+         if(OnlineHelpBox == RET_OK)
+         {
+             if ( impl_showOnlineHelp( aHelpURL ) )
+                 return true;
+             else
+             {
+                 ScopedVclPtrInstance< NoHelpErrorBox > aErrBox(const_cast< vcl::Window* >( pWindow ));
+                 aErrBox->Execute();
+                 return false;
+             }
+         }
+         else
+         {
+             return false;
+         }
 
-        ScopedVclPtrInstance< NoHelpErrorBox > aErrBox(const_cast< vcl::Window* >( pWindow ));
-        aErrBox->Execute();
-        return false;
     }
 
     Reference < XDesktop2 > xDesktop = Desktop::create( ::comphelper::getProcessComponentContext() );
