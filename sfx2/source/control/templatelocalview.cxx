6053ec9b4b20ecd43dc3bb5a16f7cd338fb86fdd
Akshay Deep <akshaydeepiitr@gmail.com>
2016-11-28 20:21:26 +0530

    tdf#101870 - Template manager thumbnail view control needs a border
    Change-Id: If1773b99aa816b2b45fe16127ff09a646e3702ae

--- sfx2/source/control/templatelocalview.cxx
+++ sfx2/source/control/templatelocalview.cxx
@@ -89,7 +89,7 @@ bool ViewFilter_Application::operator () (const ThumbnailViewItem *pItem)
 }
 
 TemplateLocalView::TemplateLocalView ( vcl::Window* pParent)
-    : ThumbnailView(pParent),
+    : ThumbnailView(pParent, WB_BORDER),
       mnCurRegionId(0),
       maSelectedItem(nullptr),
       mnThumbnailWidth(TEMPLATE_THUMBNAIL_MAX_WIDTH),
