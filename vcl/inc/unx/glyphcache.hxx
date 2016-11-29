ac360e56999b50ec23a6915277131745366842c4
Khaled Hosny <khaledhosny@eglug.org>
2016-11-29 08:17:25 +0200

    Drop assigned but never used parameter
    Change-Id: If043960898b6388df7a5ecf5fe34172ed08bba9f
    Reviewed-on: https://gerrit.libreoffice.org/31344
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Khaled Hosny <khaledhosny@eglug.org>
    (cherry picked from commit 0f4777a19634e64db884f39611be0d5075ea24e3)
    (cherry picked from commit 58740f2b26a34234633b4e756dc1bf3350b2b6eb)

--- vcl/inc/unx/glyphcache.hxx
+++ vcl/inc/unx/glyphcache.hxx
@@ -157,7 +157,7 @@ public:
 
     const FontSelectPattern& GetFontSelData() const      { return maFontSelData; }
 
-    void                    GetFontMetric( ImplFontMetricDataRef&, long& rFactor ) const;
+    void                    GetFontMetric(ImplFontMetricDataRef&) const;
     const unsigned char*    GetTable( const char* pName, sal_uLong* pLength );
     int                     GetEmUnits() const { return maFaceFT->units_per_EM;}
     double                  GetStretch() { return mfStretch; }
