9d1ac5e1ce8c1ce96ee8db43b05a084c859df865
Akshay Deep <akshaydeepiitr@gmail.com>
2016-11-30 05:13:43 +0530

    Remove Noto Emoji font
    Change-Id: Ic43807cde11fff21d78bece6874783ba154655a2

--- external/more_fonts/Module_more_fonts.mk
+++ external/more_fonts/Module_more_fonts.mk
@@ -22,7 +22,6 @@ $(eval $(call gb_Module_add_targets,more_fonts,\
 	ExternalPackage_ptserif \
 	ExternalPackage_sourcecode \
 	ExternalPackage_sourcesans \
-	ExternalPackage_NotoEmoji \
 	UnpackedTarball_caladea \
 	UnpackedTarball_carlito \
 	UnpackedTarball_dejavu \
@@ -34,7 +33,6 @@ $(eval $(call gb_Module_add_targets,more_fonts,\
 	UnpackedTarball_ptserif \
 	UnpackedTarball_sourcecode \
 	UnpackedTarball_sourcesans \
-	UnpackedTarball_NotoEmoji \
 ))
 
 # vim: set noet sw=4 ts=4:
