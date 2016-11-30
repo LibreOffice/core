LibreOffice/core

commit 33603a582bdd173a8e85c78bb0e8ef82874b8e59

convert writer navigator to .ui format
Change-Id: I83f834ea050372362b50de61753c2b3e33c67778

sw/source/uibase/utlui/content.cxx
@@ -766,8 +766,9 @@ void SwContentType::FillMemberList(bool* pbLevelOrVisibilityChanged)
 
 }
 
-SwContentTree::SwContentTree(vcl::Window* pParent, const ResId& rResId)
-    : SvTreeListBox(pParent, rResId)
+SwContentTree::SwContentTree(vcl::Window* pParent, SwNavigationPI* pDialog)
+    : SvTreeListBox(pParent)
+    , m_xDialog(pDialog)
     , m_sSpace(OUString("                    "))
     , m_sRemoveIdx(SW_RES(STR_REMOVE_INDEX))
     , m_sUpdateIdx(SW_RES(STR_UPDATE))
@@ -833,7 +834,13 @@ void SwContentTree::dispose()
     bIsInDrag = false;
     m_aUpdTimer.Stop();
     SetActiveShell(nullptr);
+    m_xDialog.clear();
     SvTreeListBox::dispose();
+}
+
+Size SwContentTree::GetOptimalSize() const
+{
+    return LogicToPixel(Size(110, 112), MapUnit::MapAppFont);
 }
 
 OUString SwContentTree::GetEntryAltText( SvTreeListEntry* pEntry ) const
@@ -1564,11 +1571,11 @@ void SwContentTree::Display( bool bActive )
         m_bIsLastReadOnly = bReadOnly;
         bool bDisable =  pShell == nullptr || bReadOnly;
         SwNavigationPI* pNavi = GetParentWindow();
-        pNavi->m_aContentToolBox->EnableItem(FN_ITEM_UP , !bDisable);
-        pNavi->m_aContentToolBox->EnableItem(FN_ITEM_DOWN, !bDisable);
-        pNavi->m_aContentToolBox->EnableItem(FN_ITEM_LEFT, !bDisable);
-        pNavi->m_aContentToolBox->EnableItem(FN_ITEM_RIGHT, !bDisable);
-        pNavi->m_aContentToolBox->EnableItem(FN_SELECT_SET_AUTO_BOOKMARK, !bDisable);
+        pNavi->m_aContentToolBox->EnableItem(pNavi->m_aContentToolBox->GetItemId("up"), !bDisable);
+        pNavi->m_aContentToolBox->EnableItem(pNavi->m_aContentToolBox->GetItemId("down"), !bDisable);
+        pNavi->m_aContentToolBox->EnableItem(pNavi->m_aContentToolBox->GetItemId("promote"), !bDisable);
+        pNavi->m_aContentToolBox->EnableItem(pNavi->m_aContentToolBox->GetItemId("demote"), !bDisable);
+        pNavi->m_aContentToolBox->EnableItem(pNavi->m_aContentToolBox->GetItemId("reminder"), !bDisable);
     }
     if(pShell)
     {
@@ -1890,7 +1897,8 @@ void SwContentTree::ToggleToRoot()
         }
     }
     m_pConfig->SetRootType( m_nRootType );
-    GetParentWindow()->m_aContentToolBox->CheckItem(FN_SHOW_ROOT, m_bIsRoot);
+    VclPtr<SwNavHelpToolBox> xBox = GetParentWindow()->m_aContentToolBox;
+    xBox->CheckItem(xBox->GetItemId("root"), m_bIsRoot);
 }
 
 bool SwContentTree::HasContentChanged()
@@ -2218,147 +2226,143 @@ void SwContentTree::Notify(SfxBroadcaster & rBC, SfxHint const& rHint)
     }
 }
 
-void SwContentTree::ExecCommand(sal_uInt16 nCmd, bool bModifier)
+void SwContentTree::ExecCommand(const OUString& rCmd, bool bModifier)
 {
-    bool bMove = false;
-    switch( nCmd )
+    const bool bUp = rCmd == "up";
+    const bool bUpDown = bUp || rCmd == "up";
+    const bool bLeft = rCmd == "promote";
+    const bool bLeftRight = bLeft || rCmd == "demote";
+    if (!bUpDown && !bLeftRight)
+        return;
+    if( !GetWrtShell()->GetView().GetDocShell()->IsReadOnly() &&
+            (m_bIsActive ||
+                (m_bIsConstant && m_pActiveShell == GetParentWindow()->GetCreateView()->GetWrtShellPtr())))
     {
-        case FN_ITEM_DOWN:
-        case FN_ITEM_UP:
-            bMove = true;
-            SAL_FALLTHROUGH;
-        case FN_ITEM_LEFT:
-        case FN_ITEM_RIGHT:
-        if( !GetWrtShell()->GetView().GetDocShell()->IsReadOnly() &&
-                (m_bIsActive ||
-                    (m_bIsConstant && m_pActiveShell == GetParentWindow()->GetCreateView()->GetWrtShellPtr())))
+        SwWrtShell* pShell = GetWrtShell();
+        sal_Int8 nActOutlineLevel = m_nOutlineLevel;
+        sal_uInt16 nActPos = pShell->GetOutlinePos(nActOutlineLevel);
+        SvTreeListEntry* pFirstEntry = FirstSelected();
+        if (pFirstEntry && lcl_IsContent(pFirstEntry))
         {
-            SwWrtShell* pShell = GetWrtShell();
-            sal_Int8 nActOutlineLevel = m_nOutlineLevel;
-            sal_uInt16 nActPos = pShell->GetOutlinePos(nActOutlineLevel);
-            SvTreeListEntry* pFirstEntry = FirstSelected();
-            if (pFirstEntry && lcl_IsContent(pFirstEntry))
+            if ( (m_bIsRoot && m_nRootType == ContentTypeId::OUTLINE) ||
+                static_cast<SwContent*>(pFirstEntry->GetUserData())->GetParent()->GetType()
+                                            ==  ContentTypeId::OUTLINE)
             {
-                if ( (m_bIsRoot && m_nRootType == ContentTypeId::OUTLINE) ||
-                    static_cast<SwContent*>(pFirstEntry->GetUserData())->GetParent()->GetType()
-                                                ==  ContentTypeId::OUTLINE)
-                {
-                    nActPos = static_cast<SwOutlineContent*>(pFirstEntry->GetUserData())->GetPos();
-                }
+                nActPos = static_cast<SwOutlineContent*>(pFirstEntry->GetUserData())->GetPos();
             }
-            if ( nActPos < USHRT_MAX &&
-                    ( !bMove || pShell->IsOutlineMovable( nActPos )) )
+        }
+        if ( nActPos < USHRT_MAX &&
+                ( !bUpDown || pShell->IsOutlineMovable( nActPos )) )
+        {
+            pShell->StartAllAction();
+            pShell->GotoOutline( nActPos); // If text selection != box selection
+            pShell->Push();
+            pShell->MakeOutlineSel( nActPos, nActPos,
+                                bModifier);
+            if (bUpDown)
             {
-                pShell->StartAllAction();
-                pShell->GotoOutline( nActPos); // If text selection != box selection
-                pShell->Push();
-                pShell->MakeOutlineSel( nActPos, nActPos,
-                                    bModifier);
-                if( bMove )
+                short nDir = bUp ? -1 : 1;
+                if( !bModifier && ( (nDir == -1 && nActPos > 0) ||
+                    (nDir == 1 && nActPos < GetEntryCount() - 2) ) )
                 {
-                    short nDir = nCmd == FN_ITEM_UP ? -1 : 1;
-                    if( !bModifier && ( (nDir == -1 && nActPos > 0) ||
-                        (nDir == 1 && nActPos < GetEntryCount() - 2) ) )
+                    pShell->MoveOutlinePara( nDir );
+                    // Set cursor back to the current position
+                    pShell->GotoOutline( nActPos + nDir);
+                }
+                else if(bModifier && pFirstEntry)
+                {
+                    sal_uInt16 nActEndPos = nActPos;
+                    SvTreeListEntry* pEntry = pFirstEntry;
+                    const auto nActLevel = static_cast<SwOutlineContent*>(
+                            pFirstEntry->GetUserData())->GetOutlineLevel();
+                    pEntry = Next(pEntry);
+                    while( pEntry && CTYPE_CNT ==
+                        static_cast<SwTypeNumber*>(pEntry->GetUserData())->GetTypeId() )
                     {
-                        pShell->MoveOutlinePara( nDir );
-                        // Set cursor back to the current position
-                        pShell->GotoOutline( nActPos + nDir);
-                    }
-                    else if(bModifier && pFirstEntry)
-                    {
-                        sal_uInt16 nActEndPos = nActPos;
-                        SvTreeListEntry* pEntry = pFirstEntry;
-                        const auto nActLevel = static_cast<SwOutlineContent*>(
-                                pFirstEntry->GetUserData())->GetOutlineLevel();
+                        if(nActLevel >= static_cast<SwOutlineContent*>(
+                            pEntry->GetUserData())->GetOutlineLevel())
+                            break;
                         pEntry = Next(pEntry);
-                        while( pEntry && CTYPE_CNT ==
-                            static_cast<SwTypeNumber*>(pEntry->GetUserData())->GetTypeId() )
+                        nActEndPos++;
+                    }
+                    if(nDir == 1)
+                    {
+                        // If the last entry is to be moved it is over!
+                        if(pEntry && CTYPE_CNT ==
+                            static_cast<SwTypeNumber*>(pEntry->GetUserData())->GetTypeId())
                         {
-                            if(nActLevel >= static_cast<SwOutlineContent*>(
-                                pEntry->GetUserData())->GetOutlineLevel())
-                                break;
-                            pEntry = Next(pEntry);
-                            nActEndPos++;
-                        }
-                        if(nDir == 1)
-                        {
-                            // If the last entry is to be moved it is over!
-                            if(pEntry && CTYPE_CNT ==
-                                static_cast<SwTypeNumber*>(pEntry->GetUserData())->GetTypeId())
+                            // pEntry now points to the following entry of the last
+                            // selected entry.
+                            sal_uInt16 nDest = nActEndPos + 1;
+                            // here needs to found the next record after next.
+                            // The selection must be inserted in front of.
+                            while(pEntry )
                             {
-                                // pEntry now points to the following entry of the last
-                                // selected entry.
-                                sal_uInt16 nDest = nActEndPos + 1;
-                                // here needs to found the next record after next.
-                                // The selection must be inserted in front of.
-                                while(pEntry )
-                                {
-                                    pEntry = Next(pEntry);
-                                    // nDest++ may only executed if pEntry != 0
-                                    if(pEntry && nDest++ &&
-                                       ( nActLevel >= static_cast<SwOutlineContent*>(pEntry->GetUserData())->GetOutlineLevel()||
-                                         CTYPE_CNT != static_cast<SwTypeNumber*>(pEntry->GetUserData())->GetTypeId()))
-                                    {
-                                        nDest--;
-                                        break;
-                                    }
-                                }
-                                nDir = nDest - nActEndPos;
-                                // If no entry was found which corresponds the condition
-                                // of the previously paste, it needs to be pushed slightly less.
-                            }
-                            else
-                                nDir = 0;
-                        }
-                        else
-                        {
-                            sal_uInt16 nDest = nActPos;
-                            pEntry = pFirstEntry;
-                            while(pEntry && nDest )
-                            {
-                                nDest--;
-                                pEntry = Prev(pEntry);
-                                if(pEntry &&
-                                    (nActLevel >= static_cast<SwOutlineContent*>(pEntry->GetUserData())->GetOutlineLevel()||
+                                pEntry = Next(pEntry);
+                                // nDest++ may only executed if pEntry != 0
+                                if(pEntry && nDest++ &&
+                                   ( nActLevel >= static_cast<SwOutlineContent*>(pEntry->GetUserData())->GetOutlineLevel()||
                                      CTYPE_CNT != static_cast<SwTypeNumber*>(pEntry->GetUserData())->GetTypeId()))
                                 {
+                                    nDest--;
                                     break;
                                 }
                             }
-                            nDir = nDest - nActPos;
+                            nDir = nDest - nActEndPos;
+                            // If no entry was found which corresponds the condition
+                            // of the previously paste, it needs to be pushed slightly less.
                         }
-                        if(nDir)
+                        else
+                            nDir = 0;
+                    }
+                    else
+                    {
+                        sal_uInt16 nDest = nActPos;
+                        pEntry = pFirstEntry;
+                        while(pEntry && nDest )
                         {
-                            pShell->MoveOutlinePara( nDir );
-                            //Set cursor back to the current position
-                            pShell->GotoOutline( nActPos + nDir);
+                            nDest--;
+                            pEntry = Prev(pEntry);
+                            if(pEntry &&
+                                (nActLevel >= static_cast<SwOutlineContent*>(pEntry->GetUserData())->GetOutlineLevel()||
+                                 CTYPE_CNT != static_cast<SwTypeNumber*>(pEntry->GetUserData())->GetTypeId()))
+                            {
+                                break;
+                            }
                         }
+                        nDir = nDest - nActPos;
+                    }
+                    if(nDir)
+                    {
+                        pShell->MoveOutlinePara( nDir );
+                        //Set cursor back to the current position
+                        pShell->GotoOutline( nActPos + nDir);
                     }
                 }
-                else
-                {
-                    if( !pShell->IsProtectedOutlinePara() )
-                        pShell->OutlineUpDown( nCmd == FN_ITEM_LEFT ? -1 : 1 );
-                }
+            }
+            else
+            {
+                if( !pShell->IsProtectedOutlinePara() )
+                    pShell->OutlineUpDown(bLeft ? -1 : 1);
+            }
 
-                pShell->ClearMark();
-                pShell->Pop(false); // Cursor is now back at the current superscription.
-                pShell->EndAllAction();
-                if(m_aActiveContentArr[ContentTypeId::OUTLINE])
-                    m_aActiveContentArr[ContentTypeId::OUTLINE]->Invalidate();
-                Display(true);
-                if(!m_bIsRoot)
-                {
-                    const sal_uInt16 nCurrPos = pShell->GetOutlinePos(MAXLEVEL);
-                    SvTreeListEntry* pFirst = First();
+            pShell->ClearMark();
+            pShell->Pop(false); // Cursor is now back at the current superscription.
+            pShell->EndAllAction();
+            if(m_aActiveContentArr[ContentTypeId::OUTLINE])
+                m_aActiveContentArr[ContentTypeId::OUTLINE]->Invalidate();
+            Display(true);
+            if(!m_bIsRoot)
+            {
+                const sal_uInt16 nCurrPos = pShell->GetOutlinePos(MAXLEVEL);
+                SvTreeListEntry* pFirst = First();
 
-                    while( nullptr != (pFirst = Next(pFirst)) && lcl_IsContent(pFirst))
+                while( nullptr != (pFirst = Next(pFirst)) && lcl_IsContent(pFirst))
+                {
+                    if(static_cast<SwOutlineContent*>(pFirst->GetUserData())->GetPos() == nCurrPos)
                     {
-                        if(static_cast<SwOutlineContent*>(pFirst->GetUserData())->GetPos() == nCurrPos)
-                        {
-                            Select(pFirst);
-                            MakeVisible(pFirst);
-                        }
+                        Select(pFirst);
+                        MakeVisible(pFirst);
                     }
                 }
             }
@@ -2995,10 +2999,10 @@ bool SwContentTree::Select( SvTreeListEntry* pEntry, bool bSelect )
         }
     }
     SwNavigationPI* pNavi = GetParentWindow();
-    pNavi->m_aContentToolBox->EnableItem(FN_ITEM_UP ,  bEnable);
-    pNavi->m_aContentToolBox->EnableItem(FN_ITEM_DOWN, bEnable);
-    pNavi->m_aContentToolBox->EnableItem(FN_ITEM_LEFT, bEnable);
-    pNavi->m_aContentToolBox->EnableItem(FN_ITEM_RIGHT,bEnable);
+    pNavi->m_aContentToolBox->EnableItem(pNavi->m_aContentToolBox->GetItemId("up"),  bEnable);
+    pNavi->m_aContentToolBox->EnableItem(pNavi->m_aContentToolBox->GetItemId("down"), bEnable);
+    pNavi->m_aContentToolBox->EnableItem(pNavi->m_aContentToolBox->GetItemId("promote"), bEnable);
+    pNavi->m_aContentToolBox->EnableItem(pNavi->m_aContentToolBox->GetItemId("demote"), bEnable);
 
     return SvTreeListBox::Select(pEntry, bSelect);
 }
@@ -3441,7 +3445,7 @@ void SwContentTree::DataChanged(const DataChangedEvent& rDCEvt)
 
 SwNavigationPI* SwContentTree::GetParentWindow()
 {
-    return static_cast<SwNavigationPI*>(Window::GetParent());
+    return m_xDialog;
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sc/source/ui/navipi/content.cxx
@@ -134,6 +134,11 @@ ScContentTree::ScContentTree(vcl::Window* pParent, ScNavigatorDlg* pNavigatorDlg
     pTmpEntry= nullptr;
 }
 
+Size ScContentTree::GetOptimalSize() const
+{
+    return LogicToPixel(Size(110, 100), MapUnit::MapAppFont);
+}
+
 ScContentTree::~ScContentTree()
 {
     disposeOnce();

extras/source/glade/libreoffice-catalog.xml.in
@@ -745,6 +745,11 @@
     <glade-widget-class title="Sidebar ToolBox" name="sfxlo-SidebarToolBox"
                         generic-name="Sidebar ToolBox" parent="GtkToolbar"
                         icon-name="widget-gtk-toolbar"/>
+
+    <glade-widget-class title="Writer Navigator ToolBox" name="swlo-SwNavHelpToolBox"
+                        generic-name="Writer Navigator ToolBox" parent="GtkToolbar"
+                        icon-name="widget-gtk-toolbar"/>
+
     <glade-widget-class title="Tab Stop Images" name="cuilo-TabWin_Impl"
                         generic-name="TabWin_Impl" parent="GtkDrawingArea"
                         icon-name="widget-gtk-drawingarea"/>

sc/source/ui/inc/content.hxx
@@ -129,6 +129,7 @@ public:
     void     SetNavigatorDlgFlag(bool isInNavigateDlg){ bisInNavigatoeDlg=isInNavigateDlg;};
     virtual void    MouseButtonDown( const MouseEvent& rMEvt ) override;
     virtual void    KeyInput( const KeyEvent& rKEvt ) override;
+    virtual Size    GetOptimalSize() const override;
 
     void    InitWindowBits( bool bButtons );
 

sc/source/ui/navipi/navipi.cxx
@@ -83,11 +83,7 @@ ColumnEdit::~ColumnEdit()
     disposeOnce();
 }
 
-VCL_BUILDER_DECL_FACTORY(ColumnEdit)
-{
-    (void)rMap;
-    rRet = VclPtr<ColumnEdit>::Create(pParent, WB_BORDER | WB_SPIN | WB_REPEAT | WB_RIGHT);
-}
+VCL_BUILDER_FACTORY_ARGS(ColumnEdit, WB_BORDER | WB_SPIN | WB_REPEAT | WB_RIGHT)
 
 bool ColumnEdit::Notify( NotifyEvent& rNEvt )
 {
@@ -264,11 +260,7 @@ RowEdit::~RowEdit()
     disposeOnce();
 }
 
-VCL_BUILDER_DECL_FACTORY(RowEdit)
-{
-    (void)rMap;
-    rRet = VclPtr<RowEdit>::Create(pParent, WB_BORDER | WB_SPIN | WB_REPEAT | WB_RIGHT);
-}
+VCL_BUILDER_FACTORY_ARGS(RowEdit, WB_BORDER | WB_SPIN | WB_REPEAT | WB_RIGHT)
 
 bool RowEdit::Notify( NotifyEvent& rNEvt )
 {
@@ -417,9 +409,9 @@ void ScNavigatorDlg::UpdateButtons()
     aTbxCmd->SetItemImage(nDragModeId, Image(ScResId(nImageId)));
 }
 
-ScNavigatorSettings::ScNavigatorSettings() :
-    mnRootSelected( ScContentId::ROOT ),
-    mnChildSelected( SC_CONTENT_NOCHILD )
+ScNavigatorSettings::ScNavigatorSettings()
+    : mnRootSelected(ScContentId::ROOT)
+    , mnChildSelected(SC_CONTENT_NOCHILD)
 {
 }
 

sw/UIConfig_swriter.mk
@@ -204,6 +204,7 @@ $(eval $(call gb_UIConfig_add_uifiles,modules/swriter,\
 	sw/uiconfig/swriter/ui/mmselectpage \
 	sw/uiconfig/swriter/ui/mmsendmails \
 	sw/uiconfig/swriter/ui/mmsalutationpage \
+	sw/uiconfig/swriter/ui/navigatorpanel \
 	sw/uiconfig/swriter/ui/notebookbar \
 	sw/uiconfig/swriter/ui/notebookbar_groups \
 	sw/uiconfig/swriter/ui/notebookbar_single \

sw/inc/cmdid.h
@@ -115,23 +115,9 @@
 #define FN_NEXT_BOOKMARK        (FN_EDIT + 68)  /*  */
 #define FN_PREV_BOOKMARK        (FN_EDIT + 69)  /*  */
 
-/*Navigator ToolBoxes*/
-#define FN_ITEM_DOWN                 (FN_EDIT + 71)  /**/
-#define FN_ITEM_LEFT                 (FN_EDIT + 72)  /**/
-#define FN_ITEM_RIGHT                (FN_EDIT + 73)  /**/
-#define FN_ITEM_UP                   (FN_EDIT + 74)  /**/
-#define FN_DOWN                      (FN_EDIT + 75)  /**/
-
-#define FN_SELECT_FOOTER             (FN_EDIT + 77)  /**/
-#define FN_SELECT_HEADER             (FN_EDIT + 79)  /**/
 #define FN_PAGENUMBER                (FN_EDIT + 80)  /**/
-#define FN_SELECT_FOOTNOTE           (FN_EDIT + 82)  /**/
-#define FN_SELECT_SET_AUTO_BOOKMARK  (FN_EDIT + 83)  /**/
-#define FN_UP                        (FN_EDIT + 86)  /**/
 
 #define FN_SELECT_PARA               (FN_EDIT + 97) /* select paragraph*/
-
-#define FN_SELECT_CONTENT            (FN_EDIT + 99) /* Navigator - Content Type */
 
 #define FN_UPDATE_ALL_LINKS          (FN_EDIT2 + 24) /* update all links */
 
@@ -178,28 +164,13 @@
 
 #define FN_SET_PAGE             (FN_VIEW + 29)  /* Set page template to paragraph */
 
-// more Navigator
-#define FN_SHOW_CONTENT_BOX     (FN_VIEW + 33)  /**/
-#define FN_SHOW_ROOT            (FN_VIEW + 34)  /**/
-#define FN_DROP_REGION          (FN_VIEW + 35)  /**/
-#define FN_OUTLINE_LEVEL        (FN_VIEW + 36)  /**/
-
 #define FN_PRINT_LAYOUT         (FN_VIEW + 37)  /* print layout */
-
-#define FN_DROP_REGION_LINK     (FN_VIEW + 38)  /**/
-#define FN_DROP_REGION_COPY     (FN_VIEW + 39)  /**/
 
 #define FN_SCROLL_NAVIGATION    (FN_VIEW + 40)  /* Navigation Controller */
 
 #define FN_VIEW_HIDDEN_PARA     (FN_VIEW + 42)  /* View hidden paragraphs */
 #define FN_VIEW_SMOOTH_SCROLL   (FN_VIEW + 43)
 
-#define FN_GLOBAL_SWITCH        (FN_VIEW + 44)  /* Toggle between Global and Content */
-#define FN_GLOBAL_EDIT          (FN_VIEW + 45)  /* edit */
-#define FN_GLOBAL_UPDATE        (FN_VIEW + 46)  /* update */
-#define FN_GLOBAL_OPEN          (FN_VIEW + 47)  /* open */
-#define FN_GLOBAL_SAVE_CONTENT  (FN_VIEW + 48)  /* save content of link */
-#define FN_CREATE_NAVIGATION    (FN_VIEW + 49)  /* create Navigations-Controller */
 #define FN_PREVIEW_ZOOM          (FN_VIEW + 51)  /* create table controller for zoom */
 
 #define FN_SET_MODOPT_TBLNUMFMT  (FN_VIEW + 52) /* Number recognition in tables */

sw/inc/helpid.h
@@ -43,30 +43,7 @@
 #define HID_NAVIGATOR_TREELIST                                  "SW_HID_NAVIGATOR_TREELIST"
 #define HID_NAVIGATOR_TOOLBOX                                   "SW_HID_NAVIGATOR_TOOLBOX"
 #define HID_NAVIGATOR_LISTBOX                                   "SW_HID_NAVIGATOR_LISTBOX"
-#define HID_NAVI_TBX2                                           "SW_HID_NAVI_TBX2"
-#define HID_NAVI_TBX3                                           "SW_HID_NAVI_TBX3"
-#define HID_NAVI_TBX4                                           "SW_HID_NAVI_TBX4"
-#define HID_NAVI_TBX5                                           "SW_HID_NAVI_TBX5"
-#define HID_NAVI_TBX6                                           "SW_HID_NAVI_TBX6"
-#define HID_NAVI_TBX7                                           "SW_HID_NAVI_TBX7"
-#define HID_NAVI_TBX8                                           "SW_HID_NAVI_TBX8"
-#define HID_NAVI_TBX9                                           "SW_HID_NAVI_TBX9"
-#define HID_NAVI_TBX10                                          "SW_HID_NAVI_TBX10"
-#define HID_NAVI_TBX11                                          "SW_HID_NAVI_TBX11"
-#define HID_NAVI_TBX12                                          "SW_HID_NAVI_TBX12"
-#define HID_NAVI_TBX13                                          "SW_HID_NAVI_TBX13"
-#define HID_NAVI_TBX14                                          "SW_HID_NAVI_TBX14"
-#define HID_NAVI_TBX15                                          "SW_HID_NAVI_TBX15"
 #define HID_NAVI_VS                                             "SW_HID_NAVI_VS"
-#define HID_NAVI_TBX16                                          "SW_HID_NAVI_TBX16"
-#define HID_NAVI_TBX17                                          "SW_HID_NAVI_TBX17"
-#define HID_NAVI_TBX18                                          "SW_HID_NAVI_TBX18"
-#define HID_NAVI_TBX19                                          "SW_HID_NAVI_TBX19"
-#define HID_NAVI_TBX20                                          "SW_HID_NAVI_TBX20"
-#define HID_NAVI_TBX21                                          "SW_HID_NAVI_TBX21"
-#define HID_NAVI_TBX22                                          "SW_HID_NAVI_TBX22"
-#define HID_NAVI_TBX23                                          "SW_HID_NAVI_TBX23"
-#define HID_NAVI_TBX24                                          "SW_HID_NAVI_TBX24"
 #define HID_NAVIGATOR_GLOBAL_TOOLBOX                            "SW_HID_NAVIGATOR_GLOBAL_TOOLBOX"
 #define HID_NAVIGATOR_GLOB_TREELIST                             "SW_HID_NAVIGATOR_GLOB_TREELIST"
 #define HID_GLBLTREE_UPDATE                                     "SW_HID_GLBLTREE_UPDATE"
@@ -81,8 +58,6 @@
 #define HID_GLBLTREE_UPD_IDX                                    "SW_HID_GLBLTREE_UPD_IDX"
 #define HID_GLBLTREE_UPD_LINK                                   "SW_HID_GLBLTREE_UPD_LINK"
 #define HID_GLBLTREEUPD_ALL                                     "SW_HID_GLBLTREEUPD_ALL"
-#define HID_NAVI_CONTENT                                        "SW_HID_NAVI_CONTENT"
-#define HID_NAVI_GLOBAL                                         "SW_HID_NAVI_GLOBAL"
 #define HID_SORT_ACTION                                         "SW_HID_SORT_ACTION"
 #define HID_SORT_AUTHOR                                         "SW_HID_SORT_AUTHOR"
 #define HID_SORT_DATE                                           "SW_HID_SORT_DATE"
@@ -172,12 +147,8 @@
 
 // More Help-IDs
 #define HID_EDIT_FORMULA                                        "SW_HID_EDIT_FORMULA"
-#define HID_NAVIGATION_PI                                       "SW_HID_NAVIGATION_PI"
-
 
 #define HID_CALC_TOOLBOX                                        "SW_HID_CALC_TOOLBOX"
-
-
 
 #define HID_AUTOFORMAT_REJECT                                   "SW_HID_AUTOFORMAT_REJECT"
 #define HID_AUTOFORMAT_ACCEPT                                   "SW_HID_AUTOFORMAT_ACCEPT"

commit 86e8d7aa4995bd53e6b3218f85a61459b1c30583

sw: navigator: fix outline Demote Chapter "down" copypasta
(regression from 33603a582bdd173a8e85c78bb0e8ef82874b8e59)

Change-Id: I95a8ebf0315a10595904dace051bbd4dd43f0496

sw/source/uibase/utlui/content.cxx
@@ -2237,7 +2237,7 @@ void SwContentTree::Notify(SfxBroadcaster & rBC, SfxHint const& rHint)
 void SwContentTree::ExecCommand(const OUString& rCmd, bool bModifier)
 {
     const bool bUp = rCmd == "up";
-    const bool bUpDown = bUp || rCmd == "up";
+    const bool bUpDown = bUp || rCmd == "down";
     const bool bLeft = rCmd == "promote";
     const bool bLeftRight = bLeft || rCmd == "demote";
     if (!bUpDown && !bLeftRight)