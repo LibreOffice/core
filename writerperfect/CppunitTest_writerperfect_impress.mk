675a74d5e57af8f6f3a30b69122cbd5faf392a86
David Tardon <dtardon@redhat.com>
2016-12-01 16:18:37 +0100

    rename to better match the purpose
    Change-Id: I7cd8e551c284045fbe782cc9756e283d466fd1bc

--- writerperfect/CppunitTest_writerperfect_impress.mk
+++ writerperfect/CppunitTest_writerperfect_impress.mk
@@ -28,7 +28,7 @@ $(eval $(call gb_CppunitTest_use_libraries,writerperfect_impress,\
 ))
 
 $(eval $(call gb_CppunitTest_use_static_libraries,writerperfect_impress,\
-	writerperfect_importtestbase \
+	writerperfect_filtertestbase \
 ))
 
 $(eval $(call gb_CppunitTest_use_ure,writerperfect_impress))
