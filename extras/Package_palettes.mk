841ef7154ba7b287666767bfa6b48f4d0bb63c10
heiko tietze <tietze.heiko@googlemail.com>
2016-12-01 12:43:26 +0100

    tdf#104050 - Add Breeze palette
    Amendment to gerrit 31012; makefile adjusted
    
    Change-Id: I89b339e49e190f4269a2950799436e94f0aee665
    Reviewed-on: https://gerrit.libreoffice.org/31481
    Reviewed-by: jan iversen <jani@documentfoundation.org>
    Tested-by: jan iversen <jani@documentfoundation.org>

--- extras/Package_palettes.mk
+++ extras/Package_palettes.mk
@@ -11,6 +11,7 @@ $(eval $(call gb_Package_Package,extras_palettes,$(SRCDIR)/extras/source/palette
 
 $(eval $(call gb_Package_add_files,extras_palettes,$(LIBO_SHARE_FOLDER)/palette,\
 	arrowhd.soe \
+	breeze.soc \
 	classic.sog \
 	hatching.soh \
 	html.soc \
