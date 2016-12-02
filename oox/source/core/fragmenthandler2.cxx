d4a341c7902f04fbb063463bb7b9b3c4c3d3c7d0
Mike Kaganski <mike.kaganski@collabora.com>
2016-12-02 03:31:22 +0300

    tdf#104310: Accept x12ac lists and fallbacks in dataValidations
    Change-Id: I42cf20fcfe3ec03ebd09923be509a9d11e0b40da

--- oox/source/core/fragmenthandler2.cxx
+++ oox/source/core/fragmenthandler2.cxx
@@ -76,6 +76,7 @@ bool FragmentHandler2::prepareMceContext( sal_Int32 nElement, const AttributeLis
                 {
                     "p14",
                     "p15",
+                    "x12ac",
                 };
 
                 if (std::find(aSupportedNS.begin(), aSupportedNS.end(), aRequires) != aSupportedNS.end())
