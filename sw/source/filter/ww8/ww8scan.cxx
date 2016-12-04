17f4f35619df8497c0cbbc9b455dcd4201156144
Caolán McNamara <caolanm@redhat.com>
2016-12-04 20:35:05 +0000

    coverity#1394416 another effort at silencing Out-of-bounds read
    Change-Id: Ie43a6be74bb79fa75bd268cc4b11f90f076eca5a

--- sw/source/filter/ww8/ww8scan.cxx
+++ sw/source/filter/ww8/ww8scan.cxx
@@ -2551,7 +2551,7 @@ WW8PLCFx_Fc_FKP::WW8Fkp::WW8Fkp(const WW8Fib& rFib, SvStream* pSt,
                         */
                         bool bExpand = IsExpandableSprm(nSpId);
                         const sal_uInt8* pStartData = aEntry.mpData + 2;
-                        const sal_uInt8* pLastValidDataPos = maRawData + sizeof(maRawData) - sizeof(sal_uInt32);
+                        const sal_uInt8* pLastValidDataPos = maRawData + 512 - sizeof(sal_uInt32);
                         if ((IsReplaceAllSprm(nSpId) || bExpand) && pStartData <= pLastValidDataPos)
                         {
                             sal_uInt32 nCurr = pDataSt->Tell();
