d3c3f15c3266411266c461103f9098e426f42a80
Marina Latini <marina@studiostorti.com>
2016-12-04 14:39:07 +0100

    tdf#102784 Patch 1 - Enhance removetooltip_markups.sh script with checks
    This is the first part of this heasy hack. The script parses all the .ui
    files checking the existence of tooltip_markup and tooltip_text
    properties. If the ui file contains a tooltip_markup property and the
    related tooltip_text one, it removes the tooltip_markup line and add the
    deletion in a log file. If the ui file contains only a tooltip_markup
    propery the script rename it via sed in tooltip_text adding the renaming
    in a log file.
    
    Change-Id: I1f027e11c7a2a91596511e2def5f4b850e14fad4
    Signed-off-by: Marina Latini <marina@studiostorti.com>
    Reviewed-on: https://gerrit.libreoffice.org/31596
    Tested-by: Jenkins <ci@libreoffice.org>
    Tested-by: jan iversen <jani@documentfoundation.org>
    Reviewed-by: jan iversen <jani@documentfoundation.org>

--- bin/removetooltip_markups.sh
+++ bin/removetooltip_markups.sh
@@ -10,15 +10,45 @@
 # Run the script in the core directory to remove all tooltip_markup
 # properties from the .ui files
 
+SED_BIN=`which sed`
+CUT_BIN=`which cut`
+LOG_FILE="modified-$(date +%s).log"
+
 removeTooltipMarkup()
 {
+    LINE=$(grep -n "<property name=\"tooltip_markup\"" $1 | $CUT_BIN -f 1 -d ':')
+    TEXT=$(grep "<property name=\"tooltip_markup\"" $1)
     grep -v "<property name=\"tooltip_markup\"" $1 > temp && mv temp $1
+    echo "removed $TEXT from $1 at line $LINE" >> $LOG_FILE
+}
+
+changeTooltipMarkup()
+{
+   LINE=$(grep -n "<property name=\"tooltip_markup\"" $1 | $CUT_BIN -f 1 -d ':')
+   $SED_BIN "s/tooltip_markup/tooltip_text/g" $i > temp && mv temp $1
+   echo "renamed tooltip_markup from $1 at line $LINE" >> $LOG_FILE
+}
+
+checkTooltipMarkup()
+{
+   TEXT=`grep "<property name=\"tooltip_text\"" $1`
+   MARKUP=`grep "<property name=\"tooltip_markup\"" $1`
+
+   if [[ $MARKUP ]] && [[ $TEXT ]]
+   then
+      removeTooltipMarkup "$1"
+   fi
+   if [[ $MARKUP ]] && [[ ! $TEXT ]]
+   then
+      changeTooltipMarkup "$1"
+   fi
 }
 
 shopt -s globstar
+echo " " > $LOG_FILE
 for i in **/*.ui; do
     echo -n "."
-    removeTooltipMarkup "$i"
+    checkTooltipMarkup "$i"
 done
 
 echo
