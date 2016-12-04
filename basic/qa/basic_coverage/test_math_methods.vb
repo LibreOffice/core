13e91e6e5699741cd3ac0ee2e7929094373b1650
Arnaud Versini <arnaud.versini@gmail.com>
2016-12-04 10:46:27 +0100

    basic: cleanup test_math_methods
    Change-Id: I11b3ca4c55151de7403ff9980ada9958d8ef12dc

--- basic/qa/basic_coverage/test_math_methods.vb
+++ basic/qa/basic_coverage/test_math_methods.vb
@@ -12,62 +12,48 @@ Function doUnitTest as Integer
     ' ABS
     If (Abs(-3.5) <> 3.5) Then
         doUnitTest = 0
-    Else If (Abs(3.5) <> 3.5) Then
+    ElseIf (Abs(3.5) <> 3.5) Then
         doUnitTest = 0
     ' ATN
-    Else If (Atn(1) <> PI/4) Then
+    ElseIf (Atn(1) <> PI/4) Then
         doUnitTest = 0
     ' COS SIN
-    Else If ( Abs(Cos(PI/3) - Sin(PI/6)) > 1E-6 ) Then
+    ElseIf ( Abs(Cos(PI/3) - Sin(PI/6)) > 1E-6 ) Then
         doUnitTest = 0
     ' EXP LOG
-    Else If ( Log( Exp(1) ) <> 1 ) Then
+    ElseIf ( Log( Exp(1) ) <> 1 ) Then
         doUnitTest = 0
     ' FIX
-    Else If (Fix(PI) <> 3) Then
+    ElseIf (Fix(PI) <> 3) Then
         doUnitTest = 0
     ' FRAC
-    Else If ( 3+Frac(PI) <> PI) Then
+    ElseIf ( 3+Frac(PI) <> PI) Then
         doUnitTest = 0
     ' HEX
-    Else If ( Hex(100) <> "64") Then
+    ElseIf ( Hex(100) <> "64") Then
         doUnitTest = 0
     ' INT
-    Else If ( Int(PI) <> 3 ) Then
+    ElseIf ( Int(PI) <> 3 ) Then
         doUnitTest = 0
     ' OCT
-    Else If ( Oct(100) <> "144" ) Then
+    ElseIf ( Oct(100) <> "144" ) Then
         doUnitTest = 0
     ' ROUND
     ' Else If ( Round( PI, 2 ) <> 3.14 ) Then
     '    doUnitTest = 0
     ' RND
-    Else If ( Rnd >= 1 ) Then
+    ElseIf ( Rnd >= 1 ) Then
         doUnitTest = 0
     ' SGN
-    Else If ( Sgn(aVector(3)) <> -1 ) Then
+    ElseIf ( Sgn(aVector(3)) <> -1 ) Then
         doUnitTest = 0
     ' SQR
-    Else If ( Sqr( 4 ) <> 2 ) Then
+    ElseIf ( Sqr( 4 ) <> 2 ) Then
         doUnitTest = 0
     ' TAN
-    Else If ( Abs( Tan(PI/4) - 1 ) > 1E-6 ) Then
+    ElseIf ( Abs( Tan(PI/4) - 1 ) > 1E-6 ) Then
         doUnitTest = 0
     Else
         doUnitTest = 1
     End If
-    End If
-    End If
-    End If
-    End If
-    End If
-    End If
-    End If
-    End If
-    End If
-    End If
-    End If
-    End If
-    End If
-    'End If
 End Function
