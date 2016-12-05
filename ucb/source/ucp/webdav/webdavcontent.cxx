c68347040462ae6c000c0f7564c42553c8c18f98
Caolán McNamara <caolanm@redhat.com>
2016-12-05 15:10:32 +0000

    fix the generally unused non-neon webdav MapDAVException
    Change-Id: I82caa1686a8855c484050f129d5095f95eab7ece
    (cherry picked from commit e100bb57d884d21c6a0e498317fa93b0202dcb64)
    Reviewed-on: https://gerrit.libreoffice.org/34526
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Tor Lillqvist <tml@collabora.com>

--- ucb/source/ucp/webdav/webdavcontent.cxx
+++ ucb/source/ucp/webdav/webdavcontent.cxx
@@ -2028,7 +2028,7 @@ uno::Sequence< uno::Any > Content::setPropertyValues(
             aNewTitle.clear();
 
             // Set error .
-            aRet[ nTitlePos ] <<= MapDAVException( e, true );
+            aRet[ nTitlePos ] = MapDAVException( e, true );
         }
     }
 
