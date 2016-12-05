dce81dc99445b64768eff757470791f40fffe227
Noel Grandin <noel.grandin@collabora.co.uk>
2016-12-05 11:51:01 +0200

    tdf#104405 Clone Formatting ignores vertical text alignment
    Reviewed-on: https://gerrit.libreoffice.org/31623
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
    (cherry picked from commit d36aa2ba3132ce62a370b7260ca620642cbf7dbf)
    
    Change-Id: I56cce69ddb21f7f9336b8f59e181e91306e92019

--- svx/source/table/svdotable.cxx
+++ svx/source/table/svdotable.cxx
@@ -1178,6 +1178,10 @@ const SfxItemSet& SdrTableObj::GetActiveCellItemSet() const
     return getActiveCell()->GetItemSet();
 }
 
+void SdrTableObj::SetMergedItemSetAndBroadcastOnActiveCell(const SfxItemSet& rSet, bool bClearAllItems)
+{
+    return getActiveCell()->SetMergedItemSetAndBroadcast(rSet, bClearAllItems);
+}
 
 void SdrTableObj::setTableStyle( const Reference< XIndexAccess >& xTableStyle )
 {
