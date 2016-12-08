370b0d9495be2f24e384116d65e2d5764ba942fe
Juergen Funk <juergen.funk_ml@cib.de>
2016-12-08 12:25:50 +0100

    Stop question in context of session manager when cancel a frame
    When shutdown or logout, LO make the question for all open
    windows, but when you cancel a window the question for the other
    windows are not necessary, this patch breaks the other questions.
    
    in dependency of tdf#104101
    
    Change-Id: I7119317d392ccd808c67fb57f82b1e990062abf2

--- framework/inc/services/desktop.hxx
+++ framework/inc/services/desktop.hxx
@@ -373,8 +373,21 @@ class Desktop : private cppu::BaseMutex,
 
     private:
 
-        bool m_bIsTerminated;  /// check flag to protect us against dispose before terminate!
-                                    /// see dispose() for further information!
+        /** check flag to protect us against dispose before terminate!
+        *   see dispose() for further information!
+        */
+        bool m_bIsTerminated;
+
+        /** when true, the call came form session manager
+        *   the methode is Desktop::terminateQuickstarterToo()
+        *   this the only one place where set this to true
+        *   In this case,  when one frame break, not make
+        *   question for other, the break of shutdown or logout
+        *   can be only once.
+        *   In Desktop::impl_closeFrames would be test and break
+        *   the loop and reset to false
+        */
+        bool m_bSession;
 
         css::uno::Reference< css::uno::XComponentContext >              m_xContext;               /// reference to factory, which has create this instance
         FrameContainer                                                  m_aChildTaskContainer;    /// array of child tasks (children of desktop are tasks; and tasks are also frames - But pure frames are not accepted!)
