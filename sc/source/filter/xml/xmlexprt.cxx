7b1c8fa23342f847c7bbc99627dbb3fec0a57205
Eike Rathke <erack@redhat.com>
2016-12-10 00:03:42 +0100

    rename to RegisterDefinedStyleNames() to clarify, tdf#104097 follow-up
    Change-Id: Iecb64ab1e25913180bcb53a133f9dd6777ffb17b

--- sc/source/filter/xml/xmlexprt.cxx
+++ sc/source/filter/xml/xmlexprt.cxx
@@ -1834,7 +1834,7 @@ const ScXMLEditAttributeMap& ScXMLExport::GetEditAttributeMap() const
     return *mpEditAttrMap;
 }
 
-void ScXMLExport::RegisterDefinedNames( uno::Reference< css::sheet::XSpreadsheetDocument > & xSpreadDoc )
+void ScXMLExport::RegisterDefinedStyleNames( uno::Reference< css::sheet::XSpreadsheetDocument > & xSpreadDoc )
 {
     ScFormatSaveData* pFormatData = ScModelObj::getImplementation(xSpreadDoc)->GetFormatSaveData();
     auto xAutoStylePool = GetAutoStylePool();
@@ -1941,7 +1941,7 @@ void ScXMLExport::ExportStyles_( bool bUsed )
 {
     Reference <sheet::XSpreadsheetDocument> xSpreadDoc( GetModel(), uno::UNO_QUERY );
     if (xSpreadDoc.is())
-        RegisterDefinedNames( xSpreadDoc);
+        RegisterDefinedStyleNames( xSpreadDoc);
 
     if (!pSharedData)
     {
@@ -2269,7 +2269,7 @@ void ScXMLExport::ExportAutoStyles_()
     if (getExportFlags() & SvXMLExportFlags::CONTENT)
     {
         // Reserve the loaded cell style names.
-        RegisterDefinedNames( xSpreadDoc);
+        RegisterDefinedStyleNames( xSpreadDoc);
 
         //  re-create automatic styles with old names from stored data
         ScSheetSaveData* pSheetData = ScModelObj::getImplementation(xSpreadDoc)->GetSheetSaveData();
