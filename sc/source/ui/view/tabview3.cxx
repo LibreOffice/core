7767008b5223281af2286457e250353863f6ff75
Mike Kaganski <mike.kaganski@collabora.com>
2016-12-11 00:33:07 +0300

    tdf#63593: revert 88f91adf266f19659014df22e09ce6c6761fb6f1
    This patch caused a regression that on creation of a new sheet
    while there's an unfinished edit of a cell, by using "+" in bottom
    left corner, that edit is committed to newly created sheet instead
    of that in which it was performed.
    
    Also, fixing original problem (tdf#42432), it forced the pending
    edit on deleted page to go to an existing page, which could
    overwrite existing data without being noticed (dataloss).
    
    Reverting the patch does not reintroduce the original problem that
    it intended to fix (a crash) in master.
    
    Change-Id: I696a85ec9d08ebb3621ebdbce4d9f71eadcdb2c2
    Reviewed-on: https://gerrit.libreoffice.org/31843
    Reviewed-by: Markus Mohrhard <markus.mohrhard@googlemail.com>
    Tested-by: Markus Mohrhard <markus.mohrhard@googlemail.com>
    (cherry picked from commit 53dbc419b2e886b345a3512cf11e035ff2b7651b)
    (cherry picked from commit de783e6c3eeb89c1c5e08044e0a0aae046165999)

--- sc/source/ui/view/tabview3.cxx
+++ sc/source/ui/view/tabview3.cxx
@@ -1709,6 +1709,7 @@ void ScTabView::SetTabNo( SCTAB nTab, bool bNew, bool bExtendSelection, bool bSa
         // window  (is called from SetCursor)
         UpdateShow();
         aViewData.ResetOldCursor();
+        SetCursor( aViewData.GetCurX(), aViewData.GetCurY(), true );
 
         SfxBindings& rBindings = aViewData.GetBindings();
         ScMarkData& rMark = aViewData.GetMarkData();
@@ -1745,7 +1746,6 @@ void ScTabView::SetTabNo( SCTAB nTab, bool bNew, bool bExtendSelection, bool bSa
             rBindings.Invalidate( FID_TAB_DESELECTALL );
         }
 
-        SetCursor( aViewData.GetCurX(), aViewData.GetCurY(), true );
         bool bUnoRefDialog = pScMod->IsRefDialogOpen() && pScMod->GetCurRefDlgId() == WID_SIMPLE_REF;
 
         // recalc zoom-dependent values (before TabChanged, before UpdateEditViewPos)
