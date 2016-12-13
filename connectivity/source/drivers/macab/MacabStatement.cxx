de7922ad57a9283209ad9443f3f7f34730ce3f53
Stephan Bergmann <sbergman@redhat.com>
2016-12-13 11:06:02 +0100

    -Werror,-Wunused-macros
    Change-Id: Idc81dd30502a1968f2f65386575cac1e029f8c64

--- connectivity/source/drivers/macab/MacabStatement.cxx
+++ connectivity/source/drivers/macab/MacabStatement.cxx
@@ -32,12 +32,6 @@
 #include "resource/sharedresources.hxx"
 #include "resource/macab_res.hrc"
 
-#if OSL_DEBUG_LEVEL > 0
-# define OUtoCStr( x ) ( OUStringToOString ( (x), RTL_TEXTENCODING_ASCII_US).getStr())
-#else /* OSL_DEBUG_LEVEL */
-# define OUtoCStr( x ) ("dummy")
-#endif /* OSL_DEBUG_LEVEL */
-
 using namespace connectivity::macab;
 using namespace com::sun::star::uno;
 using namespace com::sun::star::lang;
