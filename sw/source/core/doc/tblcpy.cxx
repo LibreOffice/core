LibreOffice/core

commit f0fa43ea99ac7564f2cc7ca1d997831808056b68

Revert "Revert "std::list<sal_uLong> to deque""
+ use returned iterator by insert method call
This reverts commit 1a583283d6b58c8951cb52faac82ecb24fdbf1b3.

Change-Id: I02547663230fb7b5959df349c4d395bbadd74c3b
Reviewed-on: https://gerrit.libreoffice.org/30439
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Julien Nabet <serval2412@yahoo.fr>
Tested-by: Julien Nabet <serval2412@yahoo.fr>

sw/source/core/doc/tblcpy.cxx
@@ -45,6 +45,7 @@
 #include <redline.hxx>
 #include <fmtfsize.hxx>
 #include <list>
+#include <deque>
 #include <memory>
 #include <o3tl/make_unique.hxx>
 
@@ -68,7 +69,7 @@ namespace
 
     typedef std::vector< BoxSpanInfo > BoxStructure;
     typedef std::vector< BoxStructure > LineStructure;
-    typedef std::list< sal_uLong > ColumnStructure;
+    typedef std::deque< sal_uLong > ColumnStructure;
 
     struct SubBox
     {
@@ -334,7 +335,7 @@ namespace
         }
         if( rpCol == maCols.end() || *rpCol > rnBorder )
         {
-            maCols.insert( rpCol, rnBorder );
+            rpCol = maCols.insert( rpCol, rnBorder );
             --rpCol;
             incColSpan( nLine, rnCol );
         }

commit 752d2f594d922fef6651e04f92b9c23728792ee6

tdf#104554: fix iterator calculus
and avoid unexpected behaviours when pasting columns between tables

regression from:
https://cgit.freedesktop.org/libreoffice/core/commit/?id=f0fa43ea99ac7564f2cc7ca1d997831808056b68

http://www.cplusplus.com/reference/list/list/insert/
and http://www.cplusplus.com/reference/deque/deque/insert/
helped me to understand my mistake

Change-Id: I6ff6b84a90c7ea8db1e4d7c2d12dd191f96141ae

sw/source/core/doc/tblcpy.cxx
@@ -336,7 +336,6 @@ namespace
         if( rpCol == maCols.end() || *rpCol > rnBorder )
         {
             rpCol = maCols.insert( rpCol, rnBorder );
-            --rpCol;
             incColSpan( nLine, rnCol );
         }
         aInfo.mnColSpan = rnCol - nLeftCol;