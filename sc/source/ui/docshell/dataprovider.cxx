5fbe5e968b866faab360327b00487b0d59cdd4f4
Stephan Bergmann <sbergman@redhat.com>
2016-12-16 15:26:09 +0100

    Work around -Werror,-Wunused-macros (clang-cl)
    Change-Id: Iebe340f9594f0e1847055361cebce5930685efd9

--- sc/source/ui/docshell/dataprovider.cxx
+++ sc/source/ui/docshell/dataprovider.cxx
@@ -11,7 +11,9 @@
 #include <stringutil.hxx>
 
 #if defined(_WIN32)
+#if !defined __ORCUS_STATIC_LIB // avoid -Werror,-Wunused-macros
 #define __ORCUS_STATIC_LIB
+#endif
 #endif
 #include <orcus/csv_parser.hpp>
 
