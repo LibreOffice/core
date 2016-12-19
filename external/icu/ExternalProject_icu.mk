bab8b9b6d91bf727afda2defd2cd6206f6ca0cb0
Caolán McNamara <caolanm@redhat.com>
2016-12-19 21:25:30 +0000

    static icu tweaking
    Change-Id: Ic882b4d1a5b5eaee9e8c38a0c8dbe7cd39590b9c

--- external/icu/ExternalProject_icu.mk
+++ external/icu/ExternalProject_icu.mk
@@ -83,12 +83,10 @@ $(call gb_ExternalProject_get_state_target,icu,build) :
 			$(if $(filter ANDROID EMSCRIPTEN,$(OS)),--disable-strict ac_cv_c_bigendian=no) \
 			$(if $(filter SOLARIS AIX,$(OS)),--disable-64bit-libs) \
 			$(if $(filter TRUE,$(DISABLE_DYNLOADING)),\
-				--enable-static --disable-shared,\
+				--with-data-packaging=static --enable-static --disable-shared --disable-dyload,\
 				--disable-static --enable-shared $(if $(filter ANDROID,$(OS)),--with-library-suffix=lo)) \
 			$(if $(CROSS_COMPILING),--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM)\
 				--with-cross-build=$(WORKDIR_FOR_BUILD)/UnpackedTarball/icu/source) \
-			$(if $(filter IOS,$(OS)), \
-				--with-data-packaging=archive) \
 		&& $(MAKE) \
 		$(if $(filter MACOSX,$(OS)), \
 			&& $(PERL) $(SRCDIR)/solenv/bin/macosx-change-install-names.pl shl \
