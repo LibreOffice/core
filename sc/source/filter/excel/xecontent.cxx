8c685767ba83d762292d45ea961609e657e50306
Markus Mohrhard <markus.mohrhard@googlemail.com>
2016-12-24 13:15:27 +0100

    first stab at replacing hard coded relationships with calls to a method
    Reviewed-on: https://gerrit.libreoffice.org/32409
    Reviewed-by: Markus Mohrhard <markus.mohrhard@googlemail.com>
    Tested-by: Markus Mohrhard <markus.mohrhard@googlemail.com>
    (cherry picked from commit 93e0a48d09456578527d1e498f708edbeae19667)
    
    Change-Id: I4d1fd6ac3d32180972d82c67a81adf4953e3a82d

--- sc/source/filter/excel/xecontent.cxx
+++ sc/source/filter/excel/xecontent.cxx
@@ -44,6 +44,8 @@
 #include "xestyle.hxx"
 #include "xename.hxx"
 #include <rtl/uuid.h>
+#include <oox/token/namespaces.hxx>
+#include <oox/token/relationship.hxx>
 
 using namespace ::oox;
 
@@ -511,7 +513,7 @@ void XclExpHyperlink::WriteEmbeddedData( XclExpStream& rStrm )
 void XclExpHyperlink::SaveXml( XclExpXmlStream& rStrm )
 {
     OUString sId = !msTarget.isEmpty() ? rStrm.addRelation( rStrm.GetCurrentStream()->getOutputStream(),
-            XclXmlUtils::ToOUString( "http://schemas.openxmlformats.org/officeDocument/2006/relationships/hyperlink" ),
+            oox::getRelationship(Relationship::HYPERLINK),
             msTarget, true ) : OUString();
     rStrm.GetCurrentStream()->singleElement( XML_hyperlink,
             XML_ref,                XclXmlUtils::ToOString( maScPos ).getStr(),
