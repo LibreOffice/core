a0610dc7bdac1678422a41dd174bed4cc199314f
Markus Mohrhard <markus.mohrhard@googlemail.com>
2016-12-24 13:15:27 +0100

    first stab at replacing hard coded relationships with calls to a method
    Change-Id: I4d1fd6ac3d32180972d82c67a81adf4953e3a82d

---
+++ oox/source/token/relationship.cxx
@@ -0,0 +1,33 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <oox/token/relationship.hxx>
+
+#include <map>
+
+namespace oox {
+
+OUString getRelationship(Relationship eRelationship)
+{
+    static const std::map<Relationship, OUString> aMap =
+    {
+#include "relationship.inc"
+    };
+
+    auto itr = aMap.find(eRelationship);
+    if (itr != aMap.end())
+        return itr->second;
+
+    SAL_WARN("oox", "could not find an entry for the relationship: " << (int) eRelationship);
+    return OUString();
+}
+
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
