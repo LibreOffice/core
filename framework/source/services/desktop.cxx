ed0f2d099b78d27d58e76d6ae40f850f965a1f3e
Markus Mohrhard <markus.mohrhard@googlemail.com>
2016-12-29 00:52:09 +0100

    process all outstanding events before shutdown, tdf#104969, tdf#104286, tdf#104399
    Change-Id: Id09c3dfdc94c430d5dcb2aebb017f17db80f17e6

--- framework/source/services/desktop.cxx
+++ framework/source/services/desktop.cxx
@@ -63,6 +63,7 @@
 #include <rtl/instance.hxx>
 #include <vcl/svapp.hxx>
 #include <desktop/crashreport.hxx>
+#include <vcl/scheduler.hxx>
 
 #include <tools/errinf.hxx>
 #include <unotools/configmgr.hxx>
@@ -324,6 +325,7 @@ sal_Bool SAL_CALL Desktop::terminate()
         /* UNSAFE AREA ------------------------------------------------------------------------------------- */
 
         impl_sendNotifyTerminationEvent();
+        Scheduler::ProcessEventsToIdle();
 
         if(
             ( bAskQuickStart      ) &&
