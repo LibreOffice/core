a4abfa38baed07e3489af72df304cc3c278be562
Marco Cecchetti <marco.cecchetti@collabora.com>
2017-01-01 18:02:51 +0100

    LOK: Calc: invalidation on insert image
    Change-Id: Ib0ec104b14385473a2a7f7dc2a33558d998996fa

--- sc/source/ui/view/viewdata.cxx
+++ sc/source/ui/view/viewdata.cxx
@@ -2229,6 +2229,12 @@ ScDrawView* ScViewData::GetScDrawView()
     return pView->GetScDrawView();
 }
 
+FmFormView* ScViewData::GetLOKDrawView()
+{
+    OSL_ENSURE( pView, "GetLOKDrawView() without View" );
+    return pView->GetLOKDrawView();
+}
+
 bool ScViewData::IsMinimized()
 {
     OSL_ENSURE( pView, "IsMinimized() without View" );
