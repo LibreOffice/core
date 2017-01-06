40246f81556846793a0c846b39b1eec47efc21ee
Michael Meeks <michael.meeks@collabora.com>
2017-01-06 15:23:30 +0000

    tdf#105151 - be more conservative about EndTracking / Tracking calls.
    We really shouldn't be emitting these into widgets after they are
    disposed - it serves only to confuse things. Restore behaviour to how
    it was before the VclPtr change for this.
    
    Change-Id: Ife666929f8aac0e4431a5e43970769692c11888c
    Reviewed-on: https://gerrit.libreoffice.org/32785
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Michael Meeks <michael.meeks@collabora.com>
    (cherry picked from commit d788d37a16d382d7ff32a19e8d65e5c605ac55dd)

--- vcl/source/window/window2.cxx
+++ vcl/source/window/window2.cxx
@@ -365,7 +365,11 @@ void Window::EndTracking( TrackingEventFlags nFlags )
                                    mpWindowImpl->mpFrameData->mnMouseCode,
                                    mpWindowImpl->mpFrameData->mnMouseCode );
             TrackingEvent   aTEvt( aMEvt, nFlags | TrackingEventFlags::End );
-            Tracking( aTEvt );
+            // CompatTracking effectively
+            if (!mpWindowImpl || mpWindowImpl->mbInDispose)
+                return Window::Tracking( aTEvt );
+            else
+                return Tracking( aTEvt );
         }
     }
 }
