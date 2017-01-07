d126ce58c2eb8c56faaa2163a62c5fe5d4b68690
Caolán McNamara <caolanm@redhat.com>
2017-01-07 20:48:30 +0000

    coverity#705097 Explicit null dereferenced
    Change-Id: I0c2163f91b86d2b48b7bc58b173438d2aa11377d

--- xmloff/source/transform/ChartOASISTContext.cxx
+++ xmloff/source/transform/ChartOASISTContext.cxx
@@ -121,7 +121,7 @@ void XMLChartOASISTransformerContext::StartElement(
         OUString aAttrQName( GetTransformer().GetNamespaceMap().GetQNameByKey(
                                 XML_NAMESPACE_CHART,
                                 GetXMLToken( XML_ADD_IN_NAME ) ) );
-        // coverity[var_deref_model] - pMutableAttrList is assigned in a superset of the enclosing if condition entry logic
+        assert(pMutableAttrList && "coverity[var_deref_model] - pMutableAttrList should be assigned in a superset of the enclosing if condition entry logic");
         pMutableAttrList->AddAttribute( aAttrQName, aAddInName );
     }
 
