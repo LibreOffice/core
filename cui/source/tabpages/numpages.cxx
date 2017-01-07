7a275521a5383f24d9a0c1aea7b61f1b64829190
pv2k <vidhey96@gmail.com>
2017-01-07 17:27:41 +0530

    tdf#46552 fixing preview for bullets/numbering configuration
    Change-Id: I959169428d4ed0498a2b78fc7a6ae0815e780648
    Reviewed-on: https://gerrit.libreoffice.org/32816
    Reviewed-by: Tamás Zolnai <tamas.zolnai@collabora.com>
    Tested-by: Tamás Zolnai <tamas.zolnai@collabora.com>

--- cui/source/tabpages/numpages.cxx
+++ cui/source/tabpages/numpages.cxx
@@ -2544,6 +2544,7 @@ void SvxNumberingPreview::Paint(vcl::RenderContext& rRenderContext, const Rectan
                 const SvxNumberFormat &rFmt = pActNum->GetLevel(nLevel);
                 aNum.GetLevelVal()[ nLevel ] = rFmt.GetStart();
                 sal_uInt16 nXStart( 0 );
+                pVDev->SetFillColor( aBackColor );
 
                 if (rFmt.GetPositionAndSpaceMode() == SvxNumberFormat::LABEL_WIDTH_AND_POSITION)
                 {
