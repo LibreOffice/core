89efbdd440343bd10b03fc80b6e396aea3215e90
Caolán McNamara <caolanm@redhat.com>
2017-01-07 20:43:50 +0000

    coverity#440749 Explicit null dereferenced
    Change-Id: I0927e51d0cdb53b5bd20d4f1cf567cbfb4b377da

--- unoxml/source/dom/node.cxx
+++ unoxml/source/dom/node.cxx
@@ -905,7 +905,7 @@ namespace DOM
                 if (pNew->next != nullptr)
                     pNew->next->prev = pNew;
                 pNew->parent = pOld->parent;
-                // coverity[var_deref_op] pNew->parent cannot be NULL here
+                assert(pNew->parent && "coverity[var_deref_op] pNew->parent cannot be NULL here");
                 if(pNew->parent->children == pOld)
                     pNew->parent->children = pNew;
                 if(pNew->parent->last == pOld)
