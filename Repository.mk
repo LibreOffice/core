d142b3cd50da96eb1f80a0e681bf324bb39f1d0a
Caolán McNamara <caolanm@redhat.com>
2017-01-08 20:54:02 +0000

    add png fuzzer
    Change-Id: I06a92aff55f4e107781b5e7b2b95eb61692a929b

--- Repository.mk
+++ Repository.mk
@@ -98,6 +98,7 @@ $(eval $(call gb_Helper_register_executables_for_install,OOO,brand, \
 	$(call gb_Helper_optional,FUZZERS,giffuzzer) \
 	$(call gb_Helper_optional,FUZZERS,xbmfuzzer) \
 	$(call gb_Helper_optional,FUZZERS,xpmfuzzer) \
+	$(call gb_Helper_optional,FUZZERS,pngfuzzer) \
 	$(if $(filter-out ANDROID IOS MACOSX WNT,$(OS)),oosplash) \
 	soffice_bin \
 	$(if $(filter DESKTOP,$(BUILD_TYPE)),unopkg_bin) \
