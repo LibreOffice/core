5e355dabccdbaa94de8897edc1fd88b16993b493
Stephan Bergmann <sbergman@redhat.com>
2017-01-11 11:26:49 +0100

    Some loplugin:conststringvar/stringconstant improvements: sal
    Change-Id: I8b5d81c2b51f846a24fd1f7f9cdd831f1ea54ea5

--- sal/qa/rtl/process/rtl_Process_Const.h
+++ sal/qa/rtl/process/rtl_Process_Const.h
@@ -27,10 +27,10 @@ extern "C"
 {
 #endif
 
-::rtl::OUString suParam0(RTL_CONSTASCII_USTRINGPARAM("-join"));
-::rtl::OUString suParam1(RTL_CONSTASCII_USTRINGPARAM("-with"));
-::rtl::OUString suParam2(RTL_CONSTASCII_USTRINGPARAM("-child"));
-::rtl::OUString suParam3(RTL_CONSTASCII_USTRINGPARAM("-process"));
+::rtl::OUString suParam0("-join");
+::rtl::OUString suParam1("-with");
+::rtl::OUString suParam2("-child");
+::rtl::OUString suParam3("-process");
 
 #ifdef __cplusplus
 }
