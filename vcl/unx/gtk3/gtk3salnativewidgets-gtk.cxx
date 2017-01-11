bf4255849fad4a0674d75165452659e132d97bda
Caolán McNamara <caolanm@redhat.com>
2017-01-11 10:14:31 +0000

    Resolves: rhbz#1408500 gtk3 toolbar borders too distinct vs native gtk3 ones
    Change-Id: I7ce284d3e6f999e2ac08326dc40b060bf70d7d4b
    (cherry picked from commit 5088b2066651ed133dcfd93388f7ac7187844d33)
    Reviewed-on: https://gerrit.libreoffice.org/32955
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Maxim Monastirsky <momonasmon@gmail.com>
    (cherry picked from commit 4590b0a2a5b75fc4fd6c8fb7fea6d40c4bab664c)

--- vcl/unx/gtk3/gtk3salnativewidgets-gtk.cxx
+++ vcl/unx/gtk3/gtk3salnativewidgets-gtk.cxx
@@ -2300,6 +2300,7 @@ void GtkData::initNWF()
     ImplSVData* pSVData = ImplGetSVData();
     pSVData->maNWFData.mbFlatMenu = true;
     pSVData->maNWFData.mbCheckBoxNeedsErase = true;
+    pSVData->maNWFData.mbDockingAreaAvoidTBFrames = true;
     pSVData->maNWFData.mbCanDrawWidgetAnySize = true;
     pSVData->maNWFData.mbDDListBoxNoTextArea = true;
     pSVData->maNWFData.mbNoFocusRects = true;
