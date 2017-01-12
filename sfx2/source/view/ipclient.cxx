75367918028dffb7a137d29644a7a6e1f7af7c9b
Caolán McNamara <caolanm@redhat.com>
2017-01-12 09:49:36 +0000

    in extremis dump Ole10Native payload and launch system viewer on it
    so embedded plain text documents/source code/etc in word documents can be viewed
    by us under Linux
    
    Change-Id: I19e19619070841fe097c70297adc2e8b96d1c581

--- sfx2/source/view/ipclient.cxx
+++ sfx2/source/view/ipclient.cxx
@@ -932,7 +932,7 @@ ErrCode SfxInPlaceClient::DoVerb( long nVerb )
                 }
                 catch ( embed::UnreachableStateException& )
                 {
-                    if ( nVerb == 0 || nVerb == embed::EmbedVerbs::MS_OLEVERB_OPEN )
+                    if (nVerb == embed::EmbedVerbs::MS_OLEVERB_PRIMARY || nVerb == embed::EmbedVerbs::MS_OLEVERB_OPEN || embed::EmbedVerbs::MS_OLEVERB_SHOW)
                     {
                         // a workaround for the default verb, usually makes sense for alien objects
                         try
