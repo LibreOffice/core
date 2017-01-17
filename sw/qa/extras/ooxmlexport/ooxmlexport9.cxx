LibreOffice/core

commit 992da0d5cf04497bad55637f6a6ebfcdaec03e16

bnc#817956 DOCX import of document background color
See wwSectionManager::SetSegmentToPageDesc(), this has to be applied to
every page style.

Change-Id: Iea2707ae665a55eabda3ed7575cf3658f7af5237

writerfilter/source/dmapper/DomainMapper.cxx
@@ -1445,6 +1445,9 @@ void DomainMapper::lcl_attribute(Id nName, Value & val)
         case NS_ooxml::LN_CT_SdtListItem_value:
             m_pImpl->m_pSdtHelper->getDropDownItems().push_back(sStringValue);
         break;
+        case NS_ooxml::LN_CT_Background_color:
+            m_pImpl->m_oBackgroundColor.reset(nIntValue);
+        break;
         default:
             {
 #if OSL_DEBUG_LEVEL > 0

sw/qa/extras/ooxmlimport/ooxmlimport.cxx
@@ -113,6 +113,7 @@ public:
     void testN592908_Picture();
     void testN779630();
     void testIndentation();
+    void testPageBackground();
 
     CPPUNIT_TEST_SUITE(Test);
 #if !defined(MACOSX) && !defined(WNT)
@@ -194,6 +195,7 @@ void Test::run()
         {"n592908-picture.docx", &Test::testN592908_Picture},
         {"n779630.docx", &Test::testN779630},
         {"indentation.docx", &Test::testIndentation},
+        {"page-background.docx", &Test::testPageBackground},
     };
     header();
     for (unsigned int i = 0; i < SAL_N_ELEMENTS(aMethods); ++i)
@@ -1377,6 +1379,13 @@ void Test::testIndentation()
     uno::Reference<beans::XPropertySet> xPropertySet(getStyles("ParagraphStyles")->getByName("Standard"), uno::UNO_QUERY);
     // This was RL_TB (e.g. right-to-left).
     CPPUNIT_ASSERT_EQUAL(text::WritingMode2::LR_TB, getProperty<sal_Int16>(xPropertySet, "WritingMode"));
+}
+
+void Test::testPageBackground()
+{
+    // The problem was that  <w:background w:color="92D050"/> was ignored.
+    uno::Reference<beans::XPropertySet> xPageStyle(getStyles("PageStyles")->getByName(DEFAULT_STYLE), uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(0x92D050), getProperty<sal_Int32>(xPageStyle, "BackColor"));
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(Test);

writerfilter/source/dmapper/DomainMapper_Impl.hxx
@@ -684,6 +684,9 @@ public:
     bool m_bInTableStyleRunProps;
 
     SdtHelper* m_pSdtHelper;
+
+    /// Document background color, applied to every page style.
+    boost::optional<sal_Int32> m_oBackgroundColor;
 };
 } //namespace dmapper
 } //namespace writerfilter

writerfilter/source/dmapper/PropertyMap.cxx
@@ -889,6 +889,9 @@ void SectionPropertyMap::CloseSectionGroup( DomainMapper_Impl& rDM_Impl )
         operator[]( PropertyDefinition( PROP_LEFT_MARGIN, false )) =  uno::makeAny( m_nLeftMargin  );
         operator[]( PropertyDefinition( PROP_RIGHT_MARGIN, false )) = uno::makeAny( m_nRightMargin );
 
+        if (rDM_Impl.m_oBackgroundColor)
+            operator[](PropertyDefinition(PROP_BACK_COLOR, false)) = uno::makeAny(*rDM_Impl.m_oBackgroundColor);
+
         /*** if headers/footers are available then the top/bottom margins of the
             header/footer are copied to the top/bottom margin of the page
           */

writerfilter/source/ooxml/model.xml
@@ -14997,7 +14997,7 @@
         <ref name="CT_PictureBase"/>
         <optional>
           <attribute name="color">
-            <text/>
+            <ref name="ST_HexColor"/>
             <xs:documentation>Background Color</xs:documentation>
           </attribute>
         </optional>

commit d16db5b923645fdc6613438914124c3229aa10e6

tdf#104150 DOCX import: handle <w:displayBackgroundShape/>
Regression from commit 992da0d5cf04497bad55637f6a6ebfcdaec03e16
(bnc#817956 DOCX import of document background color, 2013-05-27),
<w:background> should be ignored when <w:displayBackgroundShape/> is
missing from settings.xml, it turns out.

This also requires generating the
ooxml:CT_Settings_displayBackgroundShape token from the RTF tokenizer.

Change-Id: I6d7986904cedb952998a87e7648919ae34adc360

writerfilter/source/dmapper/DomainMapper.cxx
@@ -987,7 +987,8 @@ void DomainMapper::lcl_attribute(Id nName, Value & val)
                 m_pImpl->appendGrabBag(m_pImpl->m_aInteropGrabBag, "ooxml:CT_SdtDate_fullDate", sStringValue);
         break;
         case NS_ooxml::LN_CT_Background_color:
-            m_pImpl->m_oBackgroundColor.reset(nIntValue);
+            if (m_pImpl->GetSettingsTable()->GetDisplayBackgroundShape())
+                m_pImpl->m_oBackgroundColor.reset(nIntValue);
         break;
         case NS_ooxml::LN_CT_PageNumber_start:
             if (pSectionContext != nullptr)

writerfilter/source/dmapper/SettingsTable.cxx
@@ -65,6 +65,7 @@ struct SettingsTable_Impl
     bool                m_bSplitPgBreakAndParaMark;
     bool                m_bMirrorMargin;
     bool                m_bProtectForm;
+    bool                m_bDisplayBackgroundShape;
 
     uno::Sequence<beans::PropertyValue> m_pThemeFontLangProps;
 
@@ -91,6 +92,7 @@ struct SettingsTable_Impl
     , m_bSplitPgBreakAndParaMark(false)
     , m_bMirrorMargin(false)
     , m_bProtectForm(false)
+    , m_bDisplayBackgroundShape(false)
     , m_pThemeFontLangProps(3)
     , m_pCurrentCompatSetting(3)
     {}
@@ -274,6 +276,9 @@ void SettingsTable::lcl_sprm(Sprm& rSprm)
     case NS_ooxml::LN_CT_Settings_widowControl:
         m_pImpl->m_bWidowControl = nIntValue;
         break;
+    case NS_ooxml::LN_CT_Settings_displayBackgroundShape:
+        m_pImpl->m_bDisplayBackgroundShape = nIntValue;
+        break;
     default:
     {
 #ifdef DEBUG_WRITERFILTER
@@ -347,6 +352,11 @@ bool SettingsTable::GetSplitPgBreakAndParaMark() const
 bool SettingsTable::GetMirrorMarginSettings() const
 {
     return m_pImpl->m_bMirrorMargin;
+}
+
+bool SettingsTable::GetDisplayBackgroundShape() const
+{
+    return m_pImpl->m_bDisplayBackgroundShape;
 }
 
 bool SettingsTable::GetProtectForm() const

writerfilter/source/dmapper/SettingsTable.hxx
@@ -70,6 +70,7 @@ class SettingsTable : public LoggedProperties, public LoggedTable
     bool GetDoNotUseHTMLParagraphAutoSpacing() const;
     bool GetSplitPgBreakAndParaMark() const;
     bool GetMirrorMarginSettings() const;
+    bool GetDisplayBackgroundShape() const;
     bool GetNoColumnBalance() const;
     bool GetProtectForm() const;
 

writerfilter/source/rtftok/rtfdispatchvalue.cxx
@@ -1432,6 +1432,14 @@ RTFError RTFDocumentImpl::dispatchValue(RTFKeyword nKeyword, int nParam)
             m_aStates.top().aCharacterSprms.set(NS_ooxml::LN_EG_RPrBase_effect, std::make_shared<RTFValue>(nId));
         break;
     }
+    case RTF_VIEWBKSP:
+    {
+        m_aSettingsTableSprms.set(NS_ooxml::LN_CT_Settings_displayBackgroundShape, pIntValue);
+        // Send this token immediately, if it only appears before the first
+        // run, it will be too late, we ignored the background shape already by then.
+        outputSettingsTable();
+        break;
+    }
     default:
     {
         SAL_INFO("writerfilter", "TODO handle value '" << keywordToString(nKeyword) << "'");

writerfilter/source/rtftok/rtfdocumentimpl.cxx
@@ -354,16 +354,20 @@ void RTFDocumentImpl::resolveSubstream(std::size_t nPos, Id nId, OUString& rIgno
     Strm().Seek(nCurrent);
 }
 
+void RTFDocumentImpl::outputSettingsTable()
+{
+    writerfilter::Reference<Properties>::Pointer_t pProp = std::make_shared<RTFReferenceProperties>(m_aSettingsTableAttributes, m_aSettingsTableSprms);
+    RTFReferenceTable::Entries_t aSettingsTableEntries;
+    aSettingsTableEntries.insert(std::make_pair(0, pProp));
+    writerfilter::Reference<Table>::Pointer_t pTable = std::make_shared<RTFReferenceTable>(aSettingsTableEntries);
+    Mapper().table(NS_ooxml::LN_settings_settings, pTable);
+}
+
 void RTFDocumentImpl::checkFirstRun()
 {
     if (m_bFirstRun)
     {
-        // output settings table
-        writerfilter::Reference<Properties>::Pointer_t pProp = std::make_shared<RTFReferenceProperties>(m_aSettingsTableAttributes, m_aSettingsTableSprms);
-        RTFReferenceTable::Entries_t aSettingsTableEntries;
-        aSettingsTableEntries.insert(std::make_pair(0, pProp));
-        writerfilter::Reference<Table>::Pointer_t pTable = std::make_shared<RTFReferenceTable>(aSettingsTableEntries);
-        Mapper().table(NS_ooxml::LN_settings_settings, pTable);
+        outputSettingsTable();
         // start initial paragraph
         m_bFirstRun = false;
         assert(!m_bNeedSect);

writerfilter/source/rtftok/rtfdocumentimpl.hxx
@@ -424,6 +424,8 @@ public:
 
     /// If this is the first run of the document, starts the initial paragraph.
     void checkFirstRun();
+    /// Send NS_ooxml::LN_settings_settings to dmapper.
+    void outputSettingsTable();
     /// If the initial paragraph is started.
     bool getFirstRun()
     {

sw/qa/extras/ooxmlexport/ooxmlexport9.cxx
@@ -197,6 +197,13 @@ DECLARE_OOXMLEXPORT_TEST(testTdf104162, "tdf104162.docx")
     CPPUNIT_ASSERT(xTextFields->hasElements());
 }
 
+DECLARE_OOXMLEXPORT_TEST(testTdf104150, "tdf104150.docx")
+{
+    uno::Reference<beans::XPropertySet> xPageStyle(getStyles("PageStyles")->getByName("Standard"), uno::UNO_QUERY);
+    // This was 0xff0000, i.e. red: background shape wasn't ignored.
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(-1), getProperty<sal_Int32>(xPageStyle, "BackColor"));
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */