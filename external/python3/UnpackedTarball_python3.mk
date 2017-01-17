2d3aacb5cd7487791af37c6062295c2a91168499
Michael Stahl <mstahl@redhat.com>
2017-01-17 15:38:11 +0100

    python3: upgrade to release 3.5.3
    - fixes some minor CVEs
    - drop python-vc2013.patch.1
    - drop python-3.3.3-py17797.patch.1:
      the bug was fixed in MSVC2015 runtime so not relevant
    - drop python-lsan.patch.0:
      fixed upstream
    - ubsan.patch.0:
      drop hunks that were fixed upstream
    - python-3.5.0-tcltk.disable.patch:
      merge into msvc-disable.patch.1
    
    Change-Id: I2aecae446539d28eaf3eb64ee67581596019335d
    Reviewed-on: https://gerrit.libreoffice.org/33225
    Reviewed-by: David Ostrovsky <david@ostrovsky.org>
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Michael Stahl <mstahl@redhat.com>
    (cherry picked from commit 743f9fc86f3d3b6e87bf58c0654bcdccab0ab383)
    Reviewed-on: https://gerrit.libreoffice.org/33262
    Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
    (cherry picked from commit baf09e572b377f569b00e61c10710787b1c151ba)

--- external/python3/UnpackedTarball_python3.mk
+++ external/python3/UnpackedTarball_python3.mk
@@ -19,17 +19,14 @@ $(eval $(call gb_UnpackedTarball_add_patches,python3,\
 	external/python3/i100492-freebsd.patch.1 \
 	$(if $(filter AIX,$(OS)),external/python3/python-3.3.3-aix.patch.1) \
 	external/python3/python-3.3.0-darwin.patch.1 \
-	external/python3/python-3.3.0-msvc-disable.patch.1 \
 	external/python3/python-3.3.0-ssl.patch.1 \
-	external/python3/python-3.3.3-py17797.patch.1 \
+	external/python3/python-3.3.0-msvc-disable.patch.1 \
 	external/python3/python-3.3.0-i42553.patch.2 \
 	external/python3/python-3.3.0-pythreadstate.patch.1 \
 	external/python3/python-3.3.0-clang.patch.1 \
 	external/python3/python-3.3.5-pyexpat-symbols.patch.1 \
-	external/python3/python-lsan.patch.0 \
 	external/python3/ubsan.patch.0 \
 	external/python3/python-3.5.tweak.strip.soabi.patch \
-	external/python3/python-3.5.0-tcltk.disable.patch \
 ))
 
 ifneq ($(filter DRAGONFLY FREEBSD LINUX NETBSD OPENBSD SOLARIS,$(OS)),)
@@ -42,14 +39,6 @@ ifneq ($(ENABLE_RUNTIME_OPTIMIZATIONS),TRUE)
 $(eval $(call gb_UnpackedTarball_add_patches,python3,\
 	external/python3/python-3.3.3-disable-obmalloc.patch.0 \
 ))
-endif
-
-ifeq ($(OS)-$(COM),WNT-MSC)
-ifneq ($(filter 120,$(VCVER)),)
-$(eval $(call gb_UnpackedTarball_add_patches,python3,\
-	external/python3/python-vc2013.patch.1 \
-))
-endif
 endif
 
 ifneq ($(SYSTEM_ZLIB),TRUE)
