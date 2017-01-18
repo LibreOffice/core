b05f85a3fd247baea4f2dd975f2178e04ea5dd6a
Caolán McNamara <caolanm@redhat.com>
2017-01-18 15:35:47 +0000

    Resolves: tdf#98593 buttons in writer show different text at different zooms
    The DrawControlText always renders with a reference device positions but
    we pass in a rectangle which is derived a different way.
    
    So, add a GetControlTextRect which operates on the same reference
    device as DrawControlText so the rectangles match
    
    The rectangle is mapped from pixels to logic and back from logic
    to pixel, so add an argument to store the logic size from
    GetControlTextRect and re-use it without conversion on the cases
    where we pass back the original rectangle
    
    zooming in/out on the button in writer is now stable wrt text shown at all zoom
    levels
    
    Change-Id: Ic581eca67d0ff265e2753ab8b6c40e8fca7e6ae4
    Reviewed-on: https://gerrit.libreoffice.org/33277
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>
    (cherry picked from commit db9ef688edd86b31606e0dd6df6a77732faca49b)
    
    change DrawControlText to return new rect rather than change old one
    
    Change-Id: Id5c80ff263e429d4239a844db216e87a656edb2e
    (cherry picked from commit bc6479e92b56df742dd7339d337ffc78d2f9e4a1)
    
    Ref...DeviceTextLayout::GetTextArray is only called by Ref...DeviceTextLayout
    
    so set it as private to de-confuse this a little
    
    Change-Id: Idde7f78a767227cc2d52e417b6c39d12e1339745
    (cherry picked from commit 3d002fdb8738d9bbc9b04c0270510e349bd58149)
    Reviewed-on: https://gerrit.libreoffice.org/33281
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>

--- vcl/source/control/button.cxx
+++ vcl/source/control/button.cxx
@@ -286,7 +286,7 @@ void Button::ImplDrawAlignedImage(OutputDevice* pDev, Point& rPos,
     }
     else if (bDrawText && !bDrawImage && !bHasSymbol)
     {
-        DrawControlText(*pDev, aOutRect, aText, nTextStyle, nullptr, nullptr);
+        aOutRect = DrawControlText(*pDev, aOutRect, aText, nTextStyle, nullptr, nullptr);
 
         ImplSetFocusRect(aOutRect);
         rSize = aOutRect.GetSize();
@@ -300,6 +300,7 @@ void Button::ImplDrawAlignedImage(OutputDevice* pDev, Point& rPos,
 
     Size aTextSize;
     Size aSymbolSize;
+    Size aDeviceTextSize;
     Size aMax;
     Point aImagePos = rPos;
     Point aTextPos = rPos;
@@ -359,7 +360,7 @@ void Button::ImplDrawAlignedImage(OutputDevice* pDev, Point& rPos,
                 aRect.Bottom() -= (aImageSize.Height() + nImageSep);
             }
 
-            aRect = pDev->GetTextRect(aRect, aText, nTextStyle);
+            aRect = GetControlTextRect(*pDev, aRect, aText, nTextStyle, &aDeviceTextSize);
             aTextSize = aRect.GetSize();
 
             aTSSize.Width()  += aTextSize.Width();
@@ -511,9 +512,9 @@ void Button::ImplDrawAlignedImage(OutputDevice* pDev, Point& rPos,
 
     if (bDrawText)
     {
-        Rectangle       aTOutRect( aTextPos, aTextSize );
-        ImplSetFocusRect( aTOutRect );
-        DrawControlText( *pDev, aTOutRect, aText, nTextStyle, nullptr, nullptr );
+        const Rectangle aTOutRect(aTextPos, aTextSize);
+        ImplSetFocusRect(aTOutRect);
+        DrawControlText(*pDev, aTOutRect, aText, nTextStyle, nullptr, nullptr, &aDeviceTextSize);
     }
     else
     {
