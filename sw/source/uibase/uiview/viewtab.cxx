64f2c78605a1bab8c7caeb14cf387e1aa4dda51b
Caolán McNamara <caolanm@redhat.com>
2017-01-18 12:07:29 +0000

    this SfxItemSet is unused
    Change-Id: I18b587aaef066c9558933d6d557219c1bad24656

--- sw/source/uibase/uiview/viewtab.cxx
+++ sw/source/uibase/uiview/viewtab.cxx
@@ -1510,9 +1510,6 @@ void SwView::StateTabWin(SfxItemSet& rSet)
                     }
                     else
                     {
-                        SfxItemSet aCoreSet2( GetPool(),
-                                                RES_BOX, RES_BOX,
-                                                SID_ATTR_BORDER_INNER, SID_ATTR_BORDER_INNER, 0 );
                         SvxBoxInfoItem aBoxInfo( SID_ATTR_BORDER_INNER );
                         aCoreSet.Put( aBoxInfo );
                         rSh.GetFlyFrameAttr( aCoreSet );
