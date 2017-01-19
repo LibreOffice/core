dfdb0882b497bf7614c2c37daddfc43f569fc9fd
Takeshi Abe <tabe@fixedpoint.jp>
2017-01-19 13:24:19 +0900

    vcl: Fix a small chance of memleaks
    Change-Id: I4fe286486f9cad57c4220d10367442a51f077f00
    Reviewed-on: https://gerrit.libreoffice.org/33294
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Julien Nabet <serval2412@yahoo.fr>
    (cherry picked from commit 9aeccd9ac7b71bfa225e4a2c4dd6692a4659da71)

--- vcl/source/gdi/pdfwriter_impl.cxx
+++ vcl/source/gdi/pdfwriter_impl.cxx
@@ -7435,6 +7435,7 @@ bool PDFWriter::Sign(PDFSignContext& rContext)
     if (!aCertHashContext.get())
     {
         SAL_WARN("vcl.pdfwriter", "HASH_Create() failed");
+        free(pass);
         return false;
     }
     HASH_Begin(aCertHashContext.get());
@@ -7459,6 +7460,7 @@ bool PDFWriter::Sign(PDFSignContext& rContext)
     if (!pEncodedCertificate)
     {
         SAL_WARN("vcl.pdfwriter", "SEC_ASN1EncodeItem() failed");
+        free(pass);
         return false;
     }
 
@@ -7483,6 +7485,7 @@ bool PDFWriter::Sign(PDFSignContext& rContext)
     if (my_SEC_StringToOID(&aOidData.oid, "1.2.840.113549.1.9.16.2.47", 0) != SECSuccess)
     {
         SAL_WARN("vcl.pdfwriter", "my_SEC_StringToOID() failed");
+        free(pass);
         return false;
     }
     aOidData.offset = SEC_OID_UNKNOWN;
@@ -7496,6 +7499,7 @@ bool PDFWriter::Sign(PDFSignContext& rContext)
     if (my_NSS_CMSSignerInfo_AddAuthAttr(cms_signer, &aAttribute) != SECSuccess)
     {
         SAL_WARN("vcl.pdfwriter", "my_NSS_CMSSignerInfo_AddAuthAttr() failed");
+        free(pass);
         return false;
     }
 
