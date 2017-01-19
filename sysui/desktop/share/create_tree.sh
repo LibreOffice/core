ade401e4654ce8b9fdc10d821890f7b755686812
Aleix Pol <aleixpol@kde.org>
2017-01-19 17:31:58 +0100

    Install a libreoffice-kde.appdata.xml file for KDE integration
    On many GNU/Linux distributions packagers will split libreoffice in
    different chunks, one of them being KDE integration. Without this file
    it's not possible for users to find the integration in the Software
    Centers.
    
    Change-Id: I7e4f3009be4f23f2dd38a6d2464476253959413e

--- sysui/desktop/share/create_tree.sh
+++ sysui/desktop/share/create_tree.sh
@@ -86,6 +86,7 @@ mkdir -p "${DESTDIR}/${PREFIXDIR}/share/appdata"
 for i in base calc draw impress writer; do
     cp "${APPDATA_SOURCE_DIR}/libreoffice-${i}.appdata.xml" "${DESTDIR}/${PREFIXDIR}/share/appdata/${PREFIX}-${i}.appdata.xml"
 done
+cp "${APPDATA_SOURCE_DIR}/org.libreoffice.kde.metainfo.xml" "${DESTDIR}/${PREFIXDIR}/share/appdata/org.${PREFIX}.kde.metainfo.xml"
 
 # Generate gobject-introspection files
 if [ -n "$INTROSPECTION_SCANNER" ]; then
