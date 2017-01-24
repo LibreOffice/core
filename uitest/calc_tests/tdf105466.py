efbbf4a7950a63b771877f5e9fc98acb174d55ed
Xisco Fauli <anistenis@gmail.com>
2017-01-24 11:40:29 +0100

    tdf#105466: Add uitest
    Change-Id: I21ba4b716b3b7d2c1c23db816701331ba462b379
    Reviewed-on: https://gerrit.libreoffice.org/33494
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Xisco Faulí <xiscofauli@libreoffice.org>

---
+++ uitest/calc_tests/tdf105466.py
@@ -0,0 +1,31 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+from uitest.framework import UITestCase
+from libreoffice.uno.propertyvalue import mkPropertyValues
+
+class tdf105466(UITestCase):
+
+    def test_changing_conditional_format(self):
+
+        self.ui_test.create_doc_in_start_center("calc")
+
+        self.ui_test.execute_modeless_dialog_through_command(".uno:ConditionalFormatDialog")
+
+        for i in range(0,4):
+            xCondFormatDlg = self.xUITest.getTopFocusWindow()
+            xTypeLstBox = xCondFormatDlg.getChild("type")
+            xTypeLstBox.executeAction("SELECT", mkPropertyValues({"POS": str(i)}))
+
+        xCondFormatDlg = self.xUITest.getTopFocusWindow()
+        xOkBtn = xCondFormatDlg.getChild("ok")
+        xOkBtn.executeAction("CLICK", tuple())
+
+        self.ui_test.close_doc()
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
+
