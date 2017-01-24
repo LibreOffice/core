92d00e5a7ffff0a067838e964dea216bafd84bcd
Kohei Yoshida <kohei.yoshida@collabora.com>
2017-01-23 22:01:37 -0500

    tdf#103927: Share single standard VirtualDevice instance.
    To avoid creating excessive amounts of VirtualDevice instances. Also,
    since we now have VclPtr, we shouldn't need this bOwnerOfRefDev flag.
    
    Change-Id: I97a6f553a178b32bc173b83a6716185d126f97e1

--- editeng/source/editeng/eerdll.cxx
+++ editeng/source/editeng/eerdll.cxx
@@ -70,6 +70,7 @@
 #include <editeng/forbiddencharacterstable.hxx>
 #include <editeng/justifyitem.hxx>
 #include <rtl/instance.hxx>
+#include <tools/mapunit.hxx>
 
 using namespace ::com::sun::star;
 
@@ -83,9 +84,11 @@ EditDLL& EditDLL::Get()
     return theEditDLL::get();
 }
 
-GlobalEditData::GlobalEditData()
+GlobalEditData::GlobalEditData() :
+    ppDefItems(nullptr),
+    mpVirDev(VclPtr<VirtualDevice>::Create())
 {
-    ppDefItems = nullptr;
+    mpVirDev->SetMapMode(MapUnit::MapTwip);
 }
 
 GlobalEditData::~GlobalEditData()
@@ -192,6 +195,11 @@ uno::Reference< linguistic2::XLanguageGuessing > const & GlobalEditData::GetLang
         xLanguageGuesser = linguistic2::LanguageGuessing::create( comphelper::getProcessComponentContext() );
     }
     return xLanguageGuesser;
+}
+
+VclPtr<VirtualDevice> GlobalEditData::GetStdVirtualDevice()
+{
+    return mpVirDev;
 }
 
 EditResId::EditResId(sal_uInt16 nId)
