d2ebae4f969064cdc3176ac2eb520575a8206c0b
Takeshi Abe <tabe@fixedpoint.jp>
2017-01-24 10:26:55 +0900

    Fix typo in comments: absolut -> absolute
    Change-Id: I8aab9004001a9a2ddd54b4c73857b6f9c5b51515

--- sw/source/core/text/widorp.cxx
+++ sw/source/core/text/widorp.cxx
@@ -107,7 +107,7 @@ bool SwTextFrameBreak::IsInside( SwTextMargin &rLine ) const
 
     SwSwapIfSwapped swap(m_pFrame);
     SwRectFnSet aRectFnSet(m_pFrame);
-    // nOrigin is an absolut value, rLine referes to the swapped situation.
+    // nOrigin is an absolute value, rLine referes to the swapped situation.
 
     SwTwips nTmpY;
     if ( m_pFrame->IsVertical() )
