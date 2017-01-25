86dbc822314ce088521b019ff0eae7df2a381f01
Michael Stahl <mstahl@redhat.com>
2017-01-25 17:12:42 +0100

    sw: revert ToX hyperlinks to LO 4.3 behaviour
    Commit 94b296d5416dd71d721ad16216b50bce79e3dc04 changed this to
    potentially insert multiple overlapping hyperlink items.
    
    Unfortunately Writer can only have one RES_TXTATR_INETFMT on any given
    position in the paragraph, so the hyperlink hints inevitably overwrite
    each other.
    
    Revert this to do it the same way as the old code in LO 4.3 did: match
    the last unmatched link-start with the first link-end, and ignore all
    the link-start before the matching one, as well as the link-end after
    the matching one.
    
    This should prevent surprising formatting changes on index update,
    including entries spontaneously turning green, and there is no reason
    why the result of the new way is objectively better than the old one.
    
    Change-Id: I55be9c212c473908428fa8bd6487d136343fe852
    (cherry picked from commit 68f7be01a78a5bad56ddd94c04764131676d6cc0)

--- sw/source/core/tox/ToxLinkProcessor.cxx
+++ sw/source/core/tox/ToxLinkProcessor.cxx
@@ -21,19 +21,18 @@ namespace sw {
 void
 ToxLinkProcessor::StartNewLink(sal_Int32 startPosition, const OUString& characterStyle)
 {
-    m_StartedLinks.push_back(o3tl::make_unique<StartedLink>(
-                startPosition, characterStyle));
+    SAL_INFO_IF(m_pStartedLink, "sw.core", "ToxLinkProcessor: LS without LE");
+    m_pStartedLink = o3tl::make_unique<StartedLink>(
+                startPosition, characterStyle);
 }
 
 void
 ToxLinkProcessor::CloseLink(sal_Int32 endPosition, const OUString& url)
 {
-    StartedLink const startedLink( (m_StartedLinks.empty())
-        ? StartedLink(0, SW_RES(STR_POOLCHR_TOXJUMP))
-        : *m_StartedLinks.back() );
-    if (!m_StartedLinks.empty())
+    if (m_pStartedLink == nullptr)
     {
-        m_StartedLinks.pop_back();
+        SAL_INFO("sw.core", "ToxLinkProcessor: LE without LS");
+        return;
     }
 
     if (url.isEmpty()) {
@@ -41,14 +40,15 @@ ToxLinkProcessor::CloseLink(sal_Int32 endPosition, const OUString& url)
     }
 
     std::unique_ptr<ClosedLink> pClosedLink(
-            new ClosedLink(url, startedLink.mStartPosition, endPosition));
+            new ClosedLink(url, m_pStartedLink->mStartPosition, endPosition));
 
-    const OUString& characterStyle = startedLink.mCharacterStyle;
+    const OUString& characterStyle = m_pStartedLink->mCharacterStyle;
     sal_uInt16 poolId = ObtainPoolId(characterStyle);
     pClosedLink->mINetFormat.SetVisitedFormatAndId(characterStyle, poolId);
     pClosedLink->mINetFormat.SetINetFormatAndId(characterStyle, poolId);
 
     m_ClosedLinks.push_back(std::move(pClosedLink));
+    m_pStartedLink.reset();
 }
 
 sal_uInt16
