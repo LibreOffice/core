ee71c4b28654f402600c184857a4a2727773251d
Jan-Marek Glogowski <glogow@fbihome.de>
2017-01-25 15:53:15 +0100

    Round-robin invoked tasks
    Add some round-robin to the task processing, so equal priority
    (auto) tasks won't always be scheduled, if there are multiple
    tasks with the same priority.
    
    Change-Id: Ice111aa5f85e9181b3ee9799ca4df0d58f210fe9

--- include/vcl/idle.hxx
+++ include/vcl/idle.hxx
@@ -46,6 +46,22 @@ public:
     virtual void  Start() override;
 };
 
+/**
+ * An auto-idle is long running task processing small chunks of data, which
+ * is re-scheduled multiple times.
+ *
+ * Remember to stop the Idle when finished, as it would otherwise busy loop the CPU!
+ *
+ * It probably makes sense to re-implement ReadyForSchedule and UpdateMinPeriod,
+ * in case there is a quick check and it can otherwise sleep.
+ */
+class VCL_DLLPUBLIC AutoIdle : public Idle
+{
+public:
+    AutoIdle( const sal_Char *pDebugName = nullptr );
+};
+
+
 #endif // INCLUDED_VCL_IDLE_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
