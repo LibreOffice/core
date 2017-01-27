1ddf5b0ecb68333e00df72a9e367b709ff664661
Caolán McNamara <caolanm@redhat.com>
2017-01-27 14:56:47 +0000

    ImplClear sets mbSwapOut false, so mbSwapOut is actually always false here
    i.e. failure to swap in does not mean that mbSwapOut is true afterwards, and
    mpSwapFile is discarded. It means the whole thing is thrown away.
    
    Change-Id: I73704866d77078c73d053047a7d2ef6499a3e69d

--- vcl/source/gdi/impgraph.cxx
+++ vcl/source/gdi/impgraph.cxx
@@ -1267,10 +1267,12 @@ bool ImpGraphic::ImplSwapIn( SvStream* xIStm )
             bRet = ImplReadEmbedded( *xIStm );
             mbSwapUnderway = false;
 
-            if( !bRet )
+            if (!bRet)
+            {
+                //throw away swapfile, etc.
                 ImplClear();
-            else
-                mbSwapOut = false;
+            }
+            mbSwapOut = false;
         }
     }
 
