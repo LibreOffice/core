05ce36d2e2bada48ee97d6a7c45e3bcf71dff29a
jan Iversen <jani@documentfoundation.org>
2017-01-29 17:10:36 +0100

    gbuild-to-ide xcode, added targets in menu
    First step in removing make as builder.
    
    Change-Id: I445627df5610a7d0b8bfbabddb66f1273e021b1a

---
+++ osx/janTestLib/janTestLib.h
@@ -0,0 +1,13 @@
+//
+//  janTestLib.h
+//  janTestLib
+//
+//  Created by Jan Iversen on 29/01/2017.
+//  Copyright © 2017 Jan Iversen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface janTestLib : NSObject
+
+@end
