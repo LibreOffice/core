c6315e162ab7e44cd710df3d0d1d7af423bf48bc
Katarina Behrens <Katarina.Behrens@cib.de>
2017-01-30 18:01:18 +0100

    gpg4libre: Download external gpgme and dependent libs
    in particular, libgpg-error and libassuan
    This only downloads and unpacks the tarball. Building them needs
    some work still
    
    Change-Id: I562fd01571929ddfb47a319038f88ea8dbfb4bdd
    Reviewed-on: https://gerrit.libreoffice.org/33712
    Reviewed-by: Siegmund Gorr <siegmund.gorr@cib.de>
    Tested-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>
    Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>

---
+++ external/libassuan/UnpackedTarball_libassuan.mk
@@ -0,0 +1,14 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_UnpackedTarball_UnpackedTarball,libassuan))
+
+$(eval $(call gb_UnpackedTarball_set_tarball,libassuan,$(LIBASSUAN_TARBALL)))
+
+# vim: set noet sw=4 ts=4:
