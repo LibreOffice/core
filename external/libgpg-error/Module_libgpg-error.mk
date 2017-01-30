18ecc504353b0e5f4e47d132bbfae8f49628d1af
Katarina Behrens <Katarina.Behrens@cib.de>
2017-01-30 18:01:18 +0100

    gpg4libre: Download external gpgme and dependent libs
    in particular, libgpg-error and libassuan
    This only downloads and unpacks the tarballs. Building them needs
    some work still
    
    Change-Id: I562fd01571929ddfb47a319038f88ea8dbfb4bdd

---
+++ external/libgpg-error/Module_libgpg-error.mk
@@ -0,0 +1,16 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_Module_Module,libgpg-error))
+
+$(eval $(call gb_Module_add_targets,libgpg-error,\
+	UnpackedTarball_libgpg-error \
+))
+
+# vim: set noet sw=4 ts=4:
