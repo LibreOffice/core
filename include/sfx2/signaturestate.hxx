3ee29be8aab0e1b04930475d337320518e3f61cb
Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
2017-01-31 09:57:05 +0100

    Typo, whitespace fixes
    Change-Id: I5e77ee0df8490706a33d2e8500f130ffa419168d

--- include/sfx2/signaturestate.hxx
+++ include/sfx2/signaturestate.hxx
@@ -31,8 +31,8 @@ enum class SignatureState
     INVALID       = 3,
     // signature is OK, but certificate could not be validated
     NOTVALIDATED  = 4,
-    //signatur and certificate are ok,but not al files are signed, as was the case in
-    //OOo 2.x - OOo 3.1.1. This state is only used  together with document signatures.
+    // signature and certificate are ok, but not all files are signed, as it was the case in
+    // OOo 2.x - OOo 3.1.1. This state is only used together with document signatures.
     PARTIAL_OK    = 5
 };
 
