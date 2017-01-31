c866ace49f4a394185b412559fbcbb67718a2fcf
Stephan Bergmann <sbergman@redhat.com>
2017-01-31 12:30:13 +0100

    Revert "Avoid warning with Clang that doesn't have -Wunused-but-set-parameter"
    This reverts commit d69bc9c939b94718ac9e17a62f62ae7ae87776e5, which isn't needed
    any more now that 01aa52b5e91bac740ad21c31968c834c1530831e "Work around
    -Werror,-Wunknown-pragmas" makes the whole pragma block conditional on
    
      __GNUC__ >= 7
    
    (which Clang does not fulfill).

--- ucb/source/ucp/cmis/cmis_url.cxx
+++ ucb/source/ucp/cmis/cmis_url.cxx
@@ -10,9 +10,7 @@
 #if defined __GNUC__ && __GNUC__ >= 7
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wdeprecated"
-#ifndef __clang__
 #pragma GCC diagnostic ignored "-Wunused-but-set-parameter"
-#endif
 #endif
 #include <libcmis/libcmis.hxx>
 #if defined __GNUC__ && __GNUC__ >= 7
