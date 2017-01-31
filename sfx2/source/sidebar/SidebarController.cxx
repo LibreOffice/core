4d3c883a4882774a62da086a6aa7c095345ace15
Caolán McNamara <caolanm@redhat.com>
2017-01-31 14:29:18 +0000

    Resolves: tdf#104884 print preview replaces the frame controller
    so the sidebar is listening to the old controller which has been disposed[1] so
    when print preview exits and yet another controller replaces the print preview
    one then the sidebar still doesn't listen to the current one.
    
    framework broadcasts COMPONENT_DETACHING/COMPONENT_REATTACHED around these
    changes, so if we listen to them we can keep attached to whatever is the
    current component
    
    [1] note that ContextChangeEventMultipler doesn't inform clients that the
    controller has been disposed, this remains unchanged here
    
    Reviewed-on: https://gerrit.libreoffice.org/33758
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>
    (cherry picked from commit 3e27ba70ce212642026874ba73021930a06cdbbd)
    
    Change-Id: I141509d4a262307afd7dcfc3d77de6cdd6dbfa5f

--- sfx2/source/sidebar/SidebarController.cxx
+++ sfx2/source/sidebar/SidebarController.cxx
@@ -121,6 +121,7 @@ SidebarController::SidebarController (
     mpResourceManager = o3tl::make_unique<ResourceManager>();
 
     registerSidebarForFrame(this, mxFrame->getController());
+    rxFrame->addFrameActionListener(this);
     // Listen for window events.
     mpParentWindow->AddEventListener(LINK(this, SidebarController, WindowEventHandler));
 
@@ -231,6 +232,7 @@ void SAL_CALL SidebarController::disposing()
     if (!xController.is())
         xController = mxCurrentController;
 
+    mxFrame->removeFrameActionListener(this);
     unregisterSidebarForFrame(this, xController);
 
     if (mxReadOnlyModeDispatch.is())
@@ -1307,6 +1309,16 @@ void SidebarController::updateModel(const css::uno::Reference<css::frame::XModel
     mpResourceManager->UpdateModel(xModel);
 }
 
+void SidebarController::frameAction(const css::frame::FrameActionEvent& rEvent)
+{
+    if (rEvent.Frame == mxFrame)
+    {
+        if (rEvent.Action == css::frame::FrameAction_COMPONENT_DETACHING)
+            unregisterSidebarForFrame(this, mxFrame->getController());
+        else if (rEvent.Action == css::frame::FrameAction_COMPONENT_REATTACHED)
+            registerSidebarForFrame(this, mxFrame->getController());
+    }
+}
 
 } } // end of namespace sfx2::sidebar
 
