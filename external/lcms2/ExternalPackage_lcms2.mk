370d1a5d498df3d2ee633cd31485f834292fe143
David Tardon <dtardon@redhat.com>
2017-02-01 10:20:43 +0100

    upload lcms2 2.8
    Change-Id: I8a3b138c051d3cddf25855a635262311669bdddc

--- external/lcms2/ExternalPackage_lcms2.mk
+++ external/lcms2/ExternalPackage_lcms2.mk
@@ -21,7 +21,7 @@ else ifeq ($(COM),MSC)
 $(eval $(call gb_ExternalPackage_add_file,lcms2,$(LIBO_LIB_FOLDER)/lcms2.dll,bin/lcms2.dll))
 endif # $(COM)
 else  # $(OS) != WNT/MACOSX
-$(eval $(call gb_ExternalPackage_add_file,lcms2,$(LIBO_LIB_FOLDER)/liblcms2.so.2,src/.libs/liblcms2.so.2.0.6))
+$(eval $(call gb_ExternalPackage_add_file,lcms2,$(LIBO_LIB_FOLDER)/liblcms2.so.2,src/.libs/liblcms2.so.2.0.8))
 endif # $(OS)
 endif # $(DISABLE_DYNLOADING)
 
