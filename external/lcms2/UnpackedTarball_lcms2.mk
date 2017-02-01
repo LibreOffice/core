8fa67334bdb6d8e5b7545f9a98ffef72cc9ffc49
David Tardon <dtardon@redhat.com>
2017-02-01 10:20:43 +0100

    upload lcms2 2.8
    Change-Id: I8a3b138c051d3cddf25855a635262311669bdddc
    Reviewed-on: https://gerrit.libreoffice.org/33798
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: David Tardon <dtardon@redhat.com>
    (cherry picked from commit 0ada594b5c4cd8490efac86029674187356c56c8)

--- external/lcms2/UnpackedTarball_lcms2.mk
+++ external/lcms2/UnpackedTarball_lcms2.mk
@@ -13,12 +13,8 @@ $(eval $(call gb_UnpackedTarball_set_tarball,lcms2,$(LCMS2_TARBALL)))
 $(eval $(call gb_UnpackedTarball_set_patchlevel,lcms2,3))
 
 $(eval $(call gb_UnpackedTarball_add_patches,lcms2,\
-	external/lcms2/lcms2.patch \
 	external/lcms2/lcms2-2.4-windows.patch \
 	external/lcms2/lcms2-msvc-disable-sse2.patch.1 \
-	external/lcms2/lcms2-vc2013-project.patch \
-	external/lcms2/lcms2-config-guess.patch.0 \
-	external/lcms2/ubsan.patch.0 \
 	external/lcms2/0001-Added-an-extra-check-to-MLU-bounds.patch.1 \
 ))
 
