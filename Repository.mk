c8ff0ec0c628056eb5e927070da90fb3179c8cd7
Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
2017-02-02 17:33:30 +0100

    gpg4libre: List and view GPG keys
    * Add GPG implementation of css::xml::crypto UNO interfaces (part of that is only stub atm)
    * List gpg keys along with other certificates
    * Viewing gpg certificates: Not all properties are implemented yet
    
    Change-Id: I7f60b26efe949a94bf8fe1b8d4d428002c2995b1

--- Repository.mk
+++ Repository.mk
@@ -624,6 +624,7 @@ $(eval $(call gb_Helper_register_libraries_for_install,PLAINLIBS_OOO,ooo, \
 	xmlsecurity \
 	xsec_fw \
 	xsec_xmlsec \
+	$(if $(filter-out MACOSX WNT,$(OS)),xsec_gpg) \
 	xstor \
 	$(if $(filter $(OS),MACOSX), \
 		macab1 \
