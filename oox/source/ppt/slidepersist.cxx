4fd910caec342aa2c78a4f3761acf669deee30bd
Tamás Zolnai <tamas.zolnai@collabora.com>
2017-02-04 00:38:52 +0100

    tdf#31488: Background fill changes its color when saving a PPTX file to ODP
    For a gradient fill we need to generate a name because
    ODP export works with this name.
    In case of shapes it works because when fill attribute
    changes some internal name generation is triggered.
    The same thing doesn't work for slide background
    so generate this name explicitely in oox code.
    
    Change-Id: Ic6ebf37ef3d66a9c274747ca04653363b1fe6d02

--- oox/source/ppt/slidepersist.cxx
+++ oox/source/ppt/slidepersist.cxx
@@ -173,7 +173,10 @@ void SlidePersist::createBackground( const XmlFilterBase& rFilterBase )
         sal_Int32 nPhClr = maBackgroundColor.isUsed() ?
             maBackgroundColor.getColor( rFilterBase.getGraphicHelper() ) : API_RGB_TRANSPARENT;
 
-        ::oox::drawingml::ShapePropertyMap aPropMap( rFilterBase.getModelObjectHelper() );
+        oox::drawingml::ShapePropertyIds aPropertyIds = (oox::drawingml::ShapePropertyInfo::DEFAULT).mrPropertyIds;
+        aPropertyIds[oox::drawingml::ShapeProperty::FillGradient] = PROP_FillGradientName;
+        oox::drawingml::ShapePropertyInfo aPropInfo( aPropertyIds, true, false, true, false );
+        oox::drawingml::ShapePropertyMap aPropMap( rFilterBase.getModelObjectHelper(), aPropInfo );
         mpBackgroundPropertiesPtr->pushToPropMap( aPropMap, rFilterBase.getGraphicHelper(), 0, nPhClr );
         PropertySet( mxPage ).setProperty( PROP_Background, aPropMap.makePropertySet() );
     }
