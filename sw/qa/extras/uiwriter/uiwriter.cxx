6b8b148601b3e2b0e28042dd57b69ef53b112fae
Vitaliy Anderson <vanderson@smartru.com>
2017-02-03 03:58:02 -0800

    tdf#104349, tdf#104668 MS Word compatibility trailing blanks option part 3
    Add unittest
    
    Change-Id: I2c0671b8ba0ee899d2e77e8d29ebbadea09e7a78

--- sw/qa/extras/uiwriter/uiwriter.cxx
+++ sw/qa/extras/uiwriter/uiwriter.cxx
@@ -39,6 +39,7 @@
 #include <unotbl.hxx>
 #include <IMark.hxx>
 #include <IDocumentMarkAccess.hxx>
+#include <IDocumentSettingAccess.hxx>
 #include <pagedesc.hxx>
 #include <postithelper.hxx>
 #include <PostItMgr.hxx>
@@ -227,6 +228,7 @@ public:
     void testTdf104492();
     void testTdf105417();
     void testTdf105625();
+    void testMsWordCompOpt();
 
     CPPUNIT_TEST_SUITE(SwUiWriterTest);
     CPPUNIT_TEST(testReplaceForward);
@@ -348,6 +350,7 @@ public:
     CPPUNIT_TEST(testTdf104492);
     CPPUNIT_TEST(testTdf105417);
     CPPUNIT_TEST(testTdf105625);
+    CPPUNIT_TEST(testMsWordCompOpt);
     CPPUNIT_TEST_SUITE_END();
 
 private:
@@ -4291,6 +4294,37 @@ void SwUiWriterTest::testTdf105625()
     pWrtShell->SttPara();
     pWrtShell->Right(CRSR_SKIP_CHARS, /*bSelect=*/true, 1, /*bBasicCall=*/false);
     CPPUNIT_ASSERT_EQUAL(true, pWrtShell->HasReadonlySel());
+}
+
+void SwUiWriterTest::testMsWordCompOpt()
+{
+    // The option is true in settings.xml
+    SwDoc* pDoc = createDoc( "MsWordCompTrailingBlanksTrue.odt" );
+    CPPUNIT_ASSERT_EQUAL( true, pDoc->getIDocumentSettingAccess().get( DocumentSettingId::MS_WORD_COMP_TRAILING_BLANKS ) );
+    calcLayout();
+    // Check that trailing spaces spans have no width if option is enabled
+    CPPUNIT_ASSERT_EQUAL( OUString( "" ), parseDump( "/root/page/body/txt[2]/Text[4]", "nWidth" ) );
+    CPPUNIT_ASSERT_EQUAL( OUString( "" ), parseDump( "/root/page/body/txt[2]/Text[5]", "nWidth" ) );
+    CPPUNIT_ASSERT_EQUAL( OUString( "" ), parseDump( "/root/page/body/txt[3]/Text[4]", "nWidth" ) );
+    CPPUNIT_ASSERT_EQUAL( OUString( "" ), parseDump( "/root/page/body/txt[3]/Text[5]", "nWidth" ) );
+
+    // The option is false in settings.xml
+    pDoc = createDoc( "MsWordCompTrailingBlanksFalse.odt" );
+    CPPUNIT_ASSERT_EQUAL( false, pDoc->getIDocumentSettingAccess().get( DocumentSettingId::MS_WORD_COMP_TRAILING_BLANKS ) );
+    calcLayout();
+    // Check that trailing spaces spans have width if option is disabled
+    CPPUNIT_ASSERT( OUString( "" ) != parseDump( "/root/page/body/txt[2]/Text[4]", "nWidth" ) );
+    CPPUNIT_ASSERT( OUString( "" ) != parseDump( "/root/page/body/txt[2]/Text[5]", "nWidth" ) );
+    CPPUNIT_ASSERT( OUString( "" ) != parseDump( "/root/page/body/txt[3]/Text[4]", "nWidth" ) );
+    CPPUNIT_ASSERT( OUString( "" ) != parseDump( "/root/page/body/txt[3]/Text[5]", "nWidth" ) );
+
+    // MsWordCompTrailingBlanks option should be false by default in new documents
+    pDoc = createDoc();
+    CPPUNIT_ASSERT_EQUAL( false, pDoc->getIDocumentSettingAccess().get( DocumentSettingId::MS_WORD_COMP_TRAILING_BLANKS ) );
+
+    // The option should be true if a .docx, .doc or .rtf document is opened
+    pDoc = createDoc( "MsWordCompTrailingBlanks.docx" );
+    CPPUNIT_ASSERT_EQUAL( true, pDoc->getIDocumentSettingAccess().get( DocumentSettingId::MS_WORD_COMP_TRAILING_BLANKS ) );
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(SwUiWriterTest);
