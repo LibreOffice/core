LibreOffice/core

commit f4826959c1a5b0e6d2849d4240668b2087582848

new loplugin:unusedenumconstants
These are the simple removals, where it is obviously safe,
the more complex ones will come in separate commits

Change-Id: I7211945a6a5576354b60d9c709940ce9b674f308
Reviewed-on: https://gerrit.libreoffice.org/33828
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/source/gdi/pdfwriter_impl.hxx
@@ -73,14 +73,13 @@ enum class GraphicsStateUpdateFlags {
     TextLineColor         = 0x0010,
     OverlineColor         = 0x0020,
     ClipRegion            = 0x0040,
-    AntiAlias             = 0x0080,
     LayoutMode            = 0x0100,
     TransparentPercent    = 0x0200,
     DigitLanguage         = 0x0400,
     All                   = 0x07ff
 };
 namespace o3tl {
-    template<> struct typed_flags<GraphicsStateUpdateFlags> : is_typed_flags<GraphicsStateUpdateFlags, 0x07ff> {};
+    template<> struct typed_flags<GraphicsStateUpdateFlags> : is_typed_flags<GraphicsStateUpdateFlags, 0x077f> {};
 }
 
 namespace vcl

basctl/source/inc/bastypes.hxx
@@ -136,7 +136,6 @@ public:
 
 enum BasicWindowStatus
 {
-    BASWIN_OK           = 0x00,
     BASWIN_RUNNINGBASIC = 0x01,
     BASWIN_TOBEKILLED   = 0x02,
     BASWIN_SUSPENDED    = 0x04,

cppcanvas/source/inc/canvasgraphichelper.hxx
@@ -50,7 +50,7 @@ namespace cppcanvas
             virtual void                             setTransformation( const ::basegfx::B2DHomMatrix& rMatrix ) override;
             virtual void                             setClip( const ::basegfx::B2DPolyPolygon& rClipPoly ) override;
             virtual void                             setClip() override;
-            virtual void                             setCompositeOp( CompositeOp aOp ) override;
+            virtual void                             setCompositeOp( sal_Int8 aOp ) override;
 
         protected:
             // for our clients

cppcanvas/source/tools/canvasgraphichelper.cxx
@@ -93,9 +93,9 @@ namespace cppcanvas
             return maRenderState;
         }
 
-        void CanvasGraphicHelper::setCompositeOp( CompositeOp aOp )
+        void CanvasGraphicHelper::setCompositeOp( sal_Int8 aOp )
         {
-            maRenderState.CompositeOperation = (sal_Int8)aOp;
+            maRenderState.CompositeOperation = aOp;
         }
 
     }

cui/source/inc/cuitabarea.hxx
@@ -93,7 +93,6 @@ enum class PageType
     Color,
     Shadow,
     Transparence,
-    Unknown = 0xFFF
 };
 
 class SvxAreaTabDialog : public SfxTabDialog

cui/source/options/optcolor.cxx
@@ -50,8 +50,6 @@ namespace
 // list of default groups
 enum Group
 {
-    Group_Unknown = -1,
-
     Group_General,
     Group_Writer,
     Group_Html,

cui/source/tabpages/paragrph.cxx
@@ -92,8 +92,7 @@ enum LineSpaceList
     LLINESPACE_PROP = 4,
     LLINESPACE_MIN  = 5,
     LLINESPACE_DURCH= 6,
-    LLINESPACE_FIX  = 7,
-    LLINESPACE_END
+    LLINESPACE_FIX  = 7
 };
 
 void SetLineSpace_Impl( SvxLineSpacingItem&, int, long lValue = 0 );

dbaccess/source/ui/inc/dbwiz.hxx
@@ -96,15 +96,6 @@ protected:
                         getPageController( TabPage* _pCurrentPage ) const override;
     virtual bool        onFinish() override;
 
-protected:
-
-    enum ApplyResult
-    {
-        AR_LEAVE_MODIFIED,      // something was modified and has successfully been committed
-        AR_LEAVE_UNCHANGED,     // no changes were made
-        AR_KEEP                 // don't leave the page (e.g. because an error occurred)
-    };
-
 private:
     DECL_LINK(OnTypeSelected, OGeneralPage&, void);
 };

commit 56206c8a5e67a5d3ed560d1c1ac4f63dcef1c6e3

vcl: fix inconsistent GraphicsStateUpdateFlags asserts
(regression from f4826959c1a5b0e6d2849d4240668b2087582848)

Change-Id: If530503a667f765c38a6b226bb0627506dbb0900

vcl/source/gdi/pdfwriter_impl.hxx
@@ -76,7 +76,7 @@ enum class GraphicsStateUpdateFlags {
     LayoutMode            = 0x0100,
     TransparentPercent    = 0x0200,
     DigitLanguage         = 0x0400,
-    All                   = 0x07ff
+    All                   = 0x077f
 };
 namespace o3tl {
     template<> struct typed_flags<GraphicsStateUpdateFlags> : is_typed_flags<GraphicsStateUpdateFlags, 0x077f> {};