f1c844fed0fdf898fd6d027529cb2570eb52bec5
brainbreaker <gautamprajapati06@gmail.com>
2017-02-08 01:38:40 +0530

    Explorer File View option in Settings working now
    Explorer file view option in Settings is working now.
    To listen to the preference updates in the app
    a custom class SettingsListernerModel has been added.
    SettingsListenerModel.java file contains the methods
    to instantiate and set OnSettingPreferenceChangedListener
    in the implementing class.
    LibreOfficeUIActivity implements this listener
    and it is triggered by onSharedPreferenceChanged() method
    in SettingsActivity.
    This class can also be used to listen to other preference changes
    in Settings.
    
    Change-Id: I063024df01cf183d2a6211648e32805795af20a9
    Reviewed-on: https://gerrit.libreoffice.org/34012
    Reviewed-by: Aleksandar Stefanović <theonewithideas@gmail.com>
    Tested-by: Aleksandar Stefanović <theonewithideas@gmail.com>

--- android/source/src/java/org/libreoffice/SettingsActivity.java
+++ android/source/src/java/org/libreoffice/SettingsActivity.java
@@ -9,10 +9,9 @@
 package org.libreoffice;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
-
-import org.libreoffice.R;
 
 public class SettingsActivity extends Activity {
     @Override
@@ -25,12 +24,30 @@ public class SettingsActivity extends Activity {
             .commit();
     }
 
-    public static class SettingsFragment extends PreferenceFragment {
+    public static class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-
             addPreferencesFromResource(R.xml.libreoffice_preferences);
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            getPreferenceScreen().getSharedPreferences()
+                    .registerOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            getPreferenceScreen().getSharedPreferences()
+                    .unregisterOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            SettingsListenerModel.getInstance().changePreferenceState(sharedPreferences, key);
         }
     }
 }
