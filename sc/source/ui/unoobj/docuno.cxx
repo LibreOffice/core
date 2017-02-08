21ed2e32bd99f49fa2565ccf8db3c44eadc2445f
Pranav Kant <pranavk@collabora.co.uk>
2017-02-08 20:40:57 +0530

    sc lok: Set appropriate ScViewOptions when annotations are off
    Change-Id: I65b3fea9a33396419dff70b134729594c0dda4db
    (cherry picked from commit c7b84ac9ea14e6c96165e3281c4bea422918c83f)

--- sc/source/ui/unoobj/docuno.cxx
+++ sc/source/ui/unoobj/docuno.cxx
@@ -1053,6 +1053,10 @@ void ScModelObj::initializeForTiledRendering(const css::uno::Sequence<css::beans
     // format
     SvtSaveOptions().SetWarnAlienFormat(false);
 
+    // If annotations are turned off in tiled rendering case
+    if (comphelper::LibreOfficeKit::isActive() && !comphelper::LibreOfficeKit::isTiledAnnotations())
+        ScDocShell::GetViewData()->SetNotesMode(false);
+
     // default tile size in pixels
     mnTilePixelWidth = 256;
     mnTilePixelHeight = 256;
