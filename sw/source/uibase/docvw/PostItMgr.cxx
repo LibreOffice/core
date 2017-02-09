1dcd451009eab84ac6d6389ee6ad9e40e2bb5358
Michael Stahl <mstahl@redhat.com>
2017-02-09 21:33:24 +0100

    tdf#91596 sw: make Format All Comments persistent
    Change-Id: I8f9a20084d2dc8abd7699f64307b81321699498c
    (cherry picked from commit bd269f902b86ecb5dde0043838be14c867ee8bec)

--- sw/source/uibase/docvw/PostItMgr.cxx
+++ sw/source/uibase/docvw/PostItMgr.cxx
@@ -1472,6 +1472,8 @@ void SwPostItMgr::FormatAll(const SfxItemSet &rNewAttr)
         pOLV->SetAttribs(rNewAttr);
         //restore old selection
         pOLV->SetSelection(aOrigSel);
+        // tdf#91596 store updated formatting in SwField
+        (*i)->pPostIt->UpdateData();
     }
 
     mpWrtShell->EndUndo();
