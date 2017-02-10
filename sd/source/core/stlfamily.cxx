71eab1c4b0f7516b57c2bd863335522885fa81f8
Arnold Dumas <arnold@dumas.at>
2017-02-10 20:10:11 +0100

    tdf#89329: Use unique_ptr for pImpl in stlfamily
    Change-Id: I0c9df5809a906dbc761f9a51c3424cebb215d0bc
    Reviewed-on: https://gerrit.libreoffice.org/34132
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

--- sd/source/core/stlfamily.cxx
+++ sd/source/core/stlfamily.cxx
@@ -95,7 +95,6 @@ PresStyleMap& SdStyleFamilyImpl::getStyleSheets()
 SdStyleFamily::SdStyleFamily( const rtl::Reference< SfxStyleSheetPool >& xPool, SfxStyleFamily nFamily )
 : mnFamily( nFamily )
 , mxPool( xPool )
-, mpImpl( nullptr )
 {
 }
 
@@ -111,7 +110,6 @@ SdStyleFamily::SdStyleFamily( const rtl::Reference< SfxStyleSheetPool >& xPool, 
 SdStyleFamily::~SdStyleFamily()
 {
     DBG_ASSERT( !mxPool.is(), "SdStyleFamily::~SdStyleFamily(), dispose me first!" );
-    delete mpImpl;
 }
 
 void SdStyleFamily::throwIfDisposed() const
@@ -462,11 +460,7 @@ void SAL_CALL SdStyleFamily::dispose(  )
     if( mxPool.is() )
         mxPool.clear();
 
-    if( mpImpl )
-    {
-        delete mpImpl;
-        mpImpl = nullptr;
-    }
+    mpImpl.reset();
 }
 
 void SAL_CALL SdStyleFamily::addEventListener( const Reference< XEventListener >&  )
