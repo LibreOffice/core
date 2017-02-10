41b5e39bde9a0a0e7d508e521140f3317007edb8
Andras Timar <andras.timar@collabora.com>
2017-02-10 21:26:17 +0100

    Related: tdf#105707 PPTX import: decode document name from URL
    Change-Id: Ic4825549c6fef1a609a753a10f6224cbcd69b7fc

--- oox/source/ppt/pptimport.cxx
+++ oox/source/ppt/pptimport.cxx
@@ -104,7 +104,7 @@ bool PowerPointImport::importDocument()
     {
         // Construct a warning message.
         INetURLObject aURL(getFileUrl());
-        SfxErrorContext aContext(ERRCTX_SFX_OPENDOC, aURL.getName(INetURLObject::LAST_SEGMENT), nullptr, RID_ERRCTX);
+        SfxErrorContext aContext(ERRCTX_SFX_OPENDOC, aURL.getName(INetURLObject::LAST_SEGMENT, true, INetURLObject::DecodeMechanism::WithCharset), nullptr, RID_ERRCTX);
         OUString aWarning;
         aContext.GetString(ERRCODE_WARNING_MASK, aWarning);
         aWarning += ":\n";
