LibreOffice/core

commit 6657d52417295265367cf3ffe5832b60e3c38011

vcl pdf import: use pdfium instead of draw_pdf_import
Replace creating a full Draw component with direct pdfium library calls.
This also means that the result is now a bitmap, not a metafile for now.

Also decouple HAVE_FEATURE_PDFIMPORT and HAVE_FEATURE_PDFIUM, the first
is the "import PDF into Draw" feature, the second is the "insert PDF as
image" feature.

Change-Id: I72c25642ec84cc831df362e02b1520c6e6d9adcf
Reviewed-on: https://gerrit.libreoffice.org/34217
Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
Tested-by: Jenkins <ci@libreoffice.org>

sw/qa/extras/odfexport/odfexport.cxx
@@ -848,7 +848,7 @@ DECLARE_ODFEXPORT_TEST(testCellUserDefineAttr, "userdefattr-tablecell.odt")
     getUserDefineAttribute(uno::makeAny(xCellC1), "proName", "v3");
 }
 
-#if HAVE_FEATURE_PDFIMPORT
+#if HAVE_FEATURE_PDFIUM
 DECLARE_ODFEXPORT_TEST(testEmbeddedPdf, "embedded-pdf.odt")
 {
     uno::Reference<drawing::XShape> xShape = getShape(1);

config_host/config_features.h.in
@@ -184,4 +184,9 @@
  */
 #define HAVE_FEATURE_NSS 0
 
+/*
+ * Whether pdfium is available
+ */
+#define HAVE_FEATURE_PDFIUM 0
+
 #endif

configure.ac
@@ -10334,6 +10334,7 @@ ENABLE_PDFIUM=
 if test -z "$enable_pdfium" -o "$enable_pdfium" = yes; then
     AC_MSG_RESULT([yes])
     ENABLE_PDFIUM=TRUE
+    AC_DEFINE(HAVE_FEATURE_PDFIUM)
     BUILD_TYPE="$BUILD_TYPE PDFIUM"
 else
     AC_MSG_RESULT([no])

sd/qa/unit/export-tests.cxx
@@ -540,7 +540,7 @@ void SdExportTest::testTdf62176()
 
 void SdExportTest::testEmbeddedPdf()
 {
-#if HAVE_FEATURE_PDFIMPORT
+#if HAVE_FEATURE_PDFIUM
     sd::DrawDocShellRef xShell = loadURL(m_directories.getURLFromSrc("/sd/qa/unit/data/odp/embedded-pdf.odp"), ODP);
     xShell = saveAndReload( xShell.get(), ODP );
     uno::Reference<drawing::XDrawPage> xPage = getPage(0, xShell);

sd/source/ui/view/drviews2.cxx
@@ -927,7 +927,7 @@ void DrawViewShell::FuTemporary(SfxRequest& rReq)
             if( rMarkList.GetMarkCount() == 1 )
             {
                 const SdrGrafObj* pObj = dynamic_cast<const SdrGrafObj*>(rMarkList.GetMark(0)->GetMarkedSdrObj());
-                if (pObj && (pObj->GetGraphicType() == GraphicType::Bitmap || pObj->GetGraphicObject().GetGraphic().getPdfData().hasElements()))
+                if (pObj && pObj->GetGraphicType() == GraphicType::Bitmap)
                 {
                     GraphicObject aGraphicObject(pObj->GetGraphicObject());
                     {

svtools/qa/unit/GraphicObjectTest.cxx
@@ -311,7 +311,7 @@ void GraphicObjectTest::testTdf88935()
 
 void GraphicObjectTest::testPdf()
 {
-#if HAVE_FEATURE_PDFIMPORT
+#if HAVE_FEATURE_PDFIUM
     uno::Reference<lang::XComponent> xComponent = loadFromDesktop(m_directories.getURLFromSrc("svtools/qa/unit/data/pdf.odt"), "com.sun.star.text.TextDocument");
     SwXTextDocument* pTxtDoc = dynamic_cast<SwXTextDocument*>(xComponent.get());
     CPPUNIT_ASSERT(pTxtDoc);

svtools/source/graphic/grfcache.cxx
@@ -240,6 +240,8 @@ bool GraphicCacheEntry::ImplInit( const GraphicObject& rObj )
                 else
                 {
                     mpBmpEx = new BitmapEx( rGraphic.GetBitmapEx() );
+                    if (rGraphic.getPdfData().hasElements())
+                        maPdfData = rGraphic.getPdfData();
                 }
             }
             break;
@@ -247,8 +249,6 @@ bool GraphicCacheEntry::ImplInit( const GraphicObject& rObj )
             case GraphicType::GdiMetafile:
             {
                 mpMtf = new GDIMetaFile( rGraphic.GetGDIMetaFile() );
-                if (rGraphic.getPdfData().hasElements())
-                    maPdfData = rGraphic.getPdfData();
             }
             break;
 
@@ -287,6 +287,8 @@ void GraphicCacheEntry::ImplFillSubstitute( Graphic& rSubstitute )
     else if( mpBmpEx )
     {
         rSubstitute = *mpBmpEx;
+        if (maPdfData.hasElements())
+            rSubstitute.setPdfData(maPdfData);
     }
     else if( mpAnimation )
     {
@@ -295,8 +297,6 @@ void GraphicCacheEntry::ImplFillSubstitute( Graphic& rSubstitute )
     else if( mpMtf )
     {
         rSubstitute = *mpMtf;
-        if (maPdfData.hasElements())
-            rSubstitute.setPdfData(maPdfData);
     }
     else
     {

svx/qa/unit/xoutdev.cxx
@@ -34,7 +34,7 @@ public:
 
 void XOutdevTest::testPdfGraphicExport()
 {
-#if HAVE_FEATURE_PDFIMPORT
+#if HAVE_FEATURE_PDFIUM
     // Import the graphic.
     Graphic aGraphic;
     test::Directories aDirectories;

commit b5ba51d22444349a6f04acfbf67136283ee7dc17

ODT export: test that the PDF replacement image is a bitmap
Fails with commit 6657d52417295265367cf3ffe5832b60e3c38011 (vcl pdf
import: use pdfium instead of draw_pdf_import, 2017-02-13) reverted.

Change-Id: Ibf29df4fdf5b1bb2684b58564b6e1981463f12eb

sw/qa/extras/odfexport/odfexport.cxx
@@ -852,8 +852,26 @@ DECLARE_ODFEXPORT_TEST(testCellUserDefineAttr, "userdefattr-tablecell.odt")
 DECLARE_ODFEXPORT_TEST(testEmbeddedPdf, "embedded-pdf.odt")
 {
     uno::Reference<drawing::XShape> xShape = getShape(1);
-    // This failed, pdf+svm replacement graphics pair didn't survive an ODT roundtrip.
+    // This failed, pdf+png replacement graphics pair didn't survive an ODT roundtrip.
     CPPUNIT_ASSERT(!getProperty<OUString>(xShape, "ReplacementGraphicURL").isEmpty());
+
+    if (mbExported)
+    {
+        uno::Sequence<uno::Any> aArgs(1);
+        aArgs[0] <<= maTempFile.GetURL();
+        uno::Reference<container::XNameAccess> xNameAccess(m_xSFactory->createInstanceWithArguments("com.sun.star.packages.zip.ZipFileAccess", aArgs), uno::UNO_QUERY);
+        bool bHasBitmap = false;
+        for (const auto& rElementName : xNameAccess->getElementNames())
+        {
+            if (rElementName.startsWith("Pictures") && rElementName.endsWith("png"))
+            {
+                bHasBitmap = true;
+                break;
+            }
+        }
+        // This failed, replacement was an svm file.
+        CPPUNIT_ASSERT(bHasBitmap);
+    }
 }
 #endif
 