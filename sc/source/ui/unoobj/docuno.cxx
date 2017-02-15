4d800a54bfa94421712c7dccc5437f50b8bfab9c
Pranav Kant <pranavk@collabora.co.uk>
2017-02-15 14:55:49 +0530

    sc lok: No need to set tiled rendering zoom here
    If we make sure that this code is executed after initializeForRendering
    is called, which is how it should be ideally, we will get the correct
    positions anyway.
    
    Change-Id: I1eb8f32823cb0b02539f077882372a73b6df5851

--- sc/source/ui/unoobj/docuno.cxx
+++ sc/source/ui/unoobj/docuno.cxx
@@ -1004,13 +1004,6 @@ OUString ScModelObj::getPostIts()
         ScGridWindow* pGridWindow = pViewData->GetActiveWin();
         if (pGridWindow)
         {
-            Fraction zoomX = Fraction(long(mnTilePixelWidth * TWIPS_PER_PIXEL), mnTileTwipWidth);
-            Fraction zoomY = Fraction(long(mnTilePixelHeight * TWIPS_PER_PIXEL), mnTileTwipHeight);
-
-            Fraction defaultZoomX = pViewData->GetZoomX();
-            Fraction defaultZoomY = pViewData->GetZoomY();
-            pViewData->SetZoom(zoomX, zoomY, true);
-
             SCCOL nX = aNote.maPos.Col();
             SCROW nY = aNote.maPos.Row();
             Point aScrPos = pViewData->GetScrPos(nX, nY, pViewData->GetActivePart(), true);
@@ -1022,8 +1015,6 @@ OUString ScModelObj::getPostIts()
             double fPPTY = pViewData->GetPPTY();
             Rectangle aRect(Point(aScrPos.getX() / fPPTX, aScrPos.getY() / fPPTY),
                             Size(nSizeXPix / fPPTX, nSizeYPix / fPPTY));
-
-            pViewData->SetZoom(defaultZoomX, defaultZoomY, true);
 
             aAnnotation.put("cellPos", aRect.toString());
         }
