7d04c367158039f18f04b0b9e1157829e5e6e082
Pranav Kant <pranavk@collabora.co.uk>
2017-02-15 14:54:50 +0530

    sc lok: comments: Use tab.rowcolumn string as postit id
    Change-Id: I5eaa7c82239ab65a567a893d76c68468cf116469

--- sc/source/ui/unoobj/docuno.cxx
+++ sc/source/ui/unoobj/docuno.cxx
@@ -982,7 +982,7 @@ OUString ScModelObj::getPostIts()
     if (!pDocShell)
         return OUString();
 
-    const ScDocument& rDoc = pDocShell->GetDocument();
+    ScDocument& rDoc = pDocShell->GetDocument();
     std::vector<sc::NoteEntry> aNotes;
     rDoc.GetAllNoteEntries(aNotes);
 
@@ -990,7 +990,11 @@ OUString ScModelObj::getPostIts()
     for (const sc::NoteEntry& aNote : aNotes)
     {
         boost::property_tree::ptree aAnnotation;
-        aAnnotation.put("id", aNote.maPos.hash());
+        OStringBuffer aBuf;
+        aNote.maPos.Format(aBuf, ScRefFlags::VALID | ScRefFlags::TAB_3D, &rDoc,
+                           ScAddress::Details(formula::FormulaGrammar::AddressConvention::CONV_ODF, aNote.maPos));
+
+        aAnnotation.put("id", aBuf.toString());
         aAnnotation.put("author", aNote.mpNote->GetAuthor());
         aAnnotation.put("dateTime", aNote.mpNote->GetDate());
         aAnnotation.put("text", aNote.mpNote->GetText());
