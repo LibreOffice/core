03feb950308bcbb26a7b1c52fa0ce820be9e6472
jan Iversen <jani@libreoffice.org>
2017-02-19 08:09:08 +0100

    ios LibreOfficeLight
    start project with sidemenu and navigation
    
    the aim of the project is to provide an IOS prototype
    based on work from the online repo and added up with
    native swift work.
    
    The project aims at having a simple editor, where the user
    can correct typos etc. in complex documents. Sharing will
    be done using the iCloud drive.
    
    The project is a proof of concept, to replace the current
    android/ios viewers.
    
    The document will be presented using the webkit (available
    in both IOS and Android) and reusing the Javascripts from online.
    Thereby having one set of files to handle user interface for online,
    android and ios.
    
    The sidebar is intented to be used similar to MUFFIN (notebook bar),
    while the action is for save/print etc.
    
    Change-Id: I307a70b87e6367de2d22c1360a3e9cfa53d373a9
    Reviewed-on: https://gerrit.libreoffice.org/35376
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: jan iversen <jani@libreoffice.org>
    Tested-by: jan iversen <jani@libreoffice.org>

---
+++ ios/experimental/LibreOfficeLight/LibreOfficeLight/LO.swift
@@ -0,0 +1,34 @@
+//
+// This file is part of the LibreOffice project.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+
+// Global variable to reference LibreOffice functions
+var LOinterface = LOkit.init()
+
+
+
+class LOkit
+{
+    init()
+    {
+        
+    }
+
+
+
+    func Initialize()
+    {
+        
+    }
+    
+    func DeleteCurrentDocument()
+    {
+        
+    }
+}
+
