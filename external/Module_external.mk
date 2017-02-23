ea2e1d226ae4f52f7910c91ac4f023d315969d1f
Katarina Behrens <Katarina.Behrens@cib.de>
2017-02-23 12:57:42 +0100

    gpg4libre: Fix build by s/gpgme/gpgmepp/ on multiple places
    Change-Id: Icfe97381395f5554d51cfb52289b3de89abbbb68

--- external/Module_external.mk
+++ external/Module_external.mk
@@ -42,7 +42,7 @@ $(eval $(call gb_Module_add_moduledirs,external,\
 	$(call gb_Helper_optional,FREEHAND,libfreehand) \
 	$(call gb_Helper_optional,FREETYPE,freetype) \
 	$(call gb_Helper_optional,GLM,glm) \
-	$(call gb_Helper_optional,GPGME,gpgme) \
+	$(call gb_Helper_optional,GPGMEPP,gpgme) \
 	$(call gb_Helper_optional,GRAPHITE,graphite) \
 	$(call gb_Helper_optional,HARFBUZZ,harfbuzz) \
 	$(call gb_Helper_optional,HSQLDB,hsqldb) \
