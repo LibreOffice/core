8e6df6a310d8e3c8b5385ef121474ca717a14267
Jeevan <suryamaddu@yahoo.in>
2017-03-05 00:13:03 +0530

    tdf#95416 Removed ../ in filter/source/svg/test files
    Change-Id: I9f281c48ddd78f2a897e26b9af57fef2e89604d3

--- filter/inc/svgreader.hxx
+++ filter/inc/svgreader.hxx
@@ -6,8 +6,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-#ifndef INCLUDED_FILTER_SOURCE_SVG_SVGREADER_HXX
-#define INCLUDED_FILTER_SOURCE_SVG_SVGREADER_HXX
+#ifndef INCLUDED_FILTER_INC_SVGREADER_HXX
+#define INCLUDED_FILTER_INC_SVGREADER_HXX
 
 #include <filter/dllapi.h>
 
