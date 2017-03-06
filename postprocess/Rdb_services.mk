c8267b8b65a2adcad97ea767669dabbdc4b2b27e
Caolán McNamara <caolanm@redhat.com>
2017-03-06 20:35:44 +0000

    oss-fuzz: Module_xmlsecurity has gpg inside ENABLE_NSS
    which is probably wrong, but nothing uses --disable-nss except IOS
    and the static build where nss is not built
    
    Change-Id: Icdd24ffc301715a0227e31f67335c8775806e085

--- postprocess/Rdb_services.mk
+++ postprocess/Rdb_services.mk
@@ -109,9 +109,9 @@ $(eval $(call gb_Rdb_add_components,services,\
 	xmloff/source/transform/xof \
 	xmloff/util/xo \
 	xmlscript/util/xmlscript \
-	$(if $(filter-out WNT MACOSX ANDROID IOS,$(OS)),xmlsecurity/util/xsec_gpg) \
 	$(if $(ENABLE_NSS), \
 		xmlsecurity/util/xmlsecurity \
+		$(if $(filter-out WNT MACOSX ANDROID IOS,$(OS)),xmlsecurity/util/xsec_gpg) \
 		xmlsecurity/util/xsec_xmlsec$(if $(filter WNT,$(OS)),.windows)) \
 	$(if $(ENABLE_COINMP), \
 		sccomp/source/solver/coinmpsolver \
