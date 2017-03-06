003fd6b95033a2a0d28e04dec2e756e4c67f9d1a
Gulsah Kose <gulsah.1004@gmail.com>
2017-03-06 10:22:14 +0300

    tdf#90856 Hide "Format Area" command for line objects in Writer
    Change-Id: Ib90aba380be7eeef3c7418ea85b553079794cf87
    Signed-off-by: Gulsah Kose <gulsah.1004@gmail.com>

--- sw/source/uibase/shells/drwbassh.cxx
+++ sw/source/uibase/shells/drwbassh.cxx
@@ -745,6 +745,31 @@ bool SwDrawBaseShell::Disable(SfxItemSet& rSet, sal_uInt16 nWhich)
     return bDisable;
 }
 
+void SwDrawBaseShell::DisableState( SfxItemSet& rSet )
+{
+    SwWrtShell *pSh = &GetShell();
+    SdrView*    pSdrView = pSh->GetDrawView();
+    const SdrMarkList& rMarkList = pSdrView->GetMarkedObjectList();
+
+    if ( rMarkList.GetMarkCount() == 1 )
+    {
+        SdrObject* pObj = rMarkList.GetMark(0)->GetMarkedSdrObj();
+        sal_uInt16 nObjType = pObj->GetObjIdentifier();
+
+        // If marked object is 2D, disable format area command.
+        if ( nObjType == OBJ_PLIN     ||
+             nObjType == OBJ_LINE     ||
+             nObjType == OBJ_PATHLINE ||
+             nObjType == OBJ_FREELINE ||
+             nObjType == OBJ_EDGE     ||
+             nObjType == OBJ_CARC )
+            rSet.DisableItem( SID_ATTRIBUTES_AREA );
+    }
+
+    Disable(rSet);
+
+}
+
 // Validate of drawing positions
 
 IMPL_LINK(SwDrawBaseShell, ValidatePosition, SvxSwFrameValidation&, rValidation, void )
