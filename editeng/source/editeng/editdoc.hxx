LibreOffice/core

commit 5e10e6937a8d5117555a4dcc9520aca173d75c95

EDITENG : Remove usage of DBG_CTOR and DBG_DTOR.
Valgrind is capable of detecting such bugs. No need for extra macros.

Change-Id: Ifc04e2ec9d27c706868569a3bcb8fbfae0e84c69
Reviewed-on: https://gerrit.libreoffice.org/7524
Reviewed-by: Caolán McNamara <caolanm@redhat.com>
Tested-by: Caolán McNamara <caolanm@redhat.com>

editeng/source/editeng/editdoc.hxx
@@ -40,7 +40,6 @@
 class ImpEditEngine;
 class SvxTabStop;
 
-DBG_NAMEEX( EE_TextPortion )
 
 #define CHARPOSGROW     16
 #define DEFTAB          720
@@ -399,7 +398,6 @@ private:
                 , nRightToLeft( sal_False )
                 , nExtraValue( 0 )
                 {
-                    DBG_CTOR( EE_TextPortion, 0 );
                 }
 
 public:
@@ -411,7 +409,6 @@ public:
                 , nRightToLeft( sal_False )
                 , nExtraValue( 0 )
                 {
-                    DBG_CTOR( EE_TextPortion, 0 );
                 }
 
                 TextPortion( const TextPortion& r )
@@ -422,10 +419,8 @@ public:
                 , nRightToLeft( r.nRightToLeft )
                 , nExtraValue( r.nExtraValue )
                 {
-                    DBG_CTOR( EE_TextPortion, 0 );
                 }
 
-                ~TextPortion()              {   DBG_DTOR( EE_TextPortion, 0 ); delete pExtraInfos; }
 
     sal_uInt16      GetLen() const              { return nLen; }
     void        SetLen( sal_uInt16 nL )         { nLen = nL; }

editeng/source/accessibility/AccessibleImageBullet.cxx
@@ -48,7 +48,6 @@ using namespace ::com::sun::star::accessibility;
 
 namespace accessibility
 {
-    DBG_NAME( AccessibleImageBullet )
 
     AccessibleImageBullet::AccessibleImageBullet ( const uno::Reference< XAccessible >& rParent ) :
         mnParagraphIndex( 0 ),
@@ -64,7 +63,6 @@ namespace accessibility
         mnNotifierClientId(::comphelper::AccessibleEventNotifier::registerClient())
     {
 #ifdef DBG_UTIL
-        DBG_CTOR( AccessibleImageBullet, NULL );
         OSL_TRACE( "Received ID: %d", mnNotifierClientId );
 #endif
 
@@ -85,7 +83,6 @@ namespace accessibility
 
     AccessibleImageBullet::~AccessibleImageBullet()
     {
-        DBG_DTOR( AccessibleImageBullet, NULL );
 
         // sign off from event notifier
         if( getNotifierClientId() != -1 )
@@ -103,14 +100,12 @@ namespace accessibility
 
     uno::Any SAL_CALL AccessibleImageBullet::queryInterface (const uno::Type & rType) throw (uno::RuntimeException)
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
 
         return AccessibleImageBulletInterfaceBase::queryInterface(rType);
     }
 
     uno::Reference< XAccessibleContext > SAL_CALL AccessibleImageBullet::getAccessibleContext(  ) throw (uno::RuntimeException)
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
 
         // We implement the XAccessibleContext interface in the same object
         return uno::Reference< XAccessibleContext > ( this );
@@ -118,14 +113,12 @@ namespace accessibility
 
     sal_Int32 SAL_CALL  AccessibleImageBullet::getAccessibleChildCount() throw (uno::RuntimeException)
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
 
         return 0;
     }
 
     uno::Reference< XAccessible > SAL_CALL  AccessibleImageBullet::getAccessibleChild( sal_Int32 i ) throw (lang::IndexOutOfBoundsException, uno::RuntimeException)
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
         (void)i;
 
         throw lang::IndexOutOfBoundsException("No children available",
@@ -135,28 +128,24 @@ namespace accessibility
 
     uno::Reference< XAccessible > SAL_CALL  AccessibleImageBullet::getAccessibleParent() throw (uno::RuntimeException)
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
 
         return mxParent;
     }
 
     sal_Int32 SAL_CALL  AccessibleImageBullet::getAccessibleIndexInParent() throw (uno::RuntimeException)
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
 
         return mnIndexInParent;
     }
 
     sal_Int16 SAL_CALL  AccessibleImageBullet::getAccessibleRole() throw (uno::RuntimeException)
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
 
         return AccessibleRole::GRAPHIC;
     }
 
     OUString SAL_CALL  AccessibleImageBullet::getAccessibleDescription() throw (uno::RuntimeException)
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
 
         SolarMutexGuard aGuard;
 
@@ -166,7 +155,6 @@ namespace accessibility
 
     OUString SAL_CALL  AccessibleImageBullet::getAccessibleName() throw (uno::RuntimeException)
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
 
         SolarMutexGuard aGuard;
 
@@ -176,7 +164,6 @@ namespace accessibility
 
     uno::Reference< XAccessibleRelationSet > SAL_CALL AccessibleImageBullet::getAccessibleRelationSet() throw (uno::RuntimeException)
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
 
         // no relations, therefore empty
         return uno::Reference< XAccessibleRelationSet >();
@@ -184,7 +171,6 @@ namespace accessibility
 
     uno::Reference< XAccessibleStateSet > SAL_CALL AccessibleImageBullet::getAccessibleStateSet() throw (uno::RuntimeException)
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
 
         SolarMutexGuard aGuard;
 
@@ -199,7 +185,6 @@ namespace accessibility
 
     lang::Locale SAL_CALL AccessibleImageBullet::getLocale() throw (IllegalAccessibleComponentStateException, uno::RuntimeException)
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
 
         SolarMutexGuard aGuard;
 
@@ -212,7 +197,6 @@ namespace accessibility
 
     void SAL_CALL AccessibleImageBullet::addAccessibleEventListener( const uno::Reference< XAccessibleEventListener >& xListener ) throw (uno::RuntimeException)
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
 
         if( getNotifierClientId() != -1 )
             ::comphelper::AccessibleEventNotifier::addEventListener( getNotifierClientId(), xListener );
@@ -220,7 +204,6 @@ namespace accessibility
 
     void SAL_CALL AccessibleImageBullet::removeAccessibleEventListener( const uno::Reference< XAccessibleEventListener >& xListener ) throw (uno::RuntimeException)
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
 
         if( getNotifierClientId() != -1 )
             ::comphelper::AccessibleEventNotifier::removeEventListener( getNotifierClientId(), xListener );
@@ -228,7 +211,6 @@ namespace accessibility
 
     sal_Bool SAL_CALL AccessibleImageBullet::containsPoint( const awt::Point& rPoint ) throw (uno::RuntimeException)
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
 
         SolarMutexGuard aGuard;
 
@@ -244,7 +226,6 @@ namespace accessibility
 
     uno::Reference< XAccessible > SAL_CALL AccessibleImageBullet::getAccessibleAtPoint( const awt::Point& /*aPoint*/ ) throw (uno::RuntimeException)
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
 
         // as we have no children, empty reference
         return uno::Reference< XAccessible >();
@@ -252,7 +233,6 @@ namespace accessibility
 
     awt::Rectangle SAL_CALL AccessibleImageBullet::getBounds(  ) throw (uno::RuntimeException)
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
 
         SolarMutexGuard aGuard;
 
@@ -291,7 +271,6 @@ namespace accessibility
 
     awt::Point SAL_CALL AccessibleImageBullet::getLocation(  ) throw (uno::RuntimeException)
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
 
         SolarMutexGuard aGuard;
 
@@ -302,7 +281,6 @@ namespace accessibility
 
     awt::Point SAL_CALL AccessibleImageBullet::getLocationOnScreen(  ) throw (uno::RuntimeException)
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
 
         SolarMutexGuard aGuard;
 
@@ -329,7 +307,6 @@ namespace accessibility
 
     awt::Size SAL_CALL AccessibleImageBullet::getSize(  ) throw (uno::RuntimeException)
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
 
         SolarMutexGuard aGuard;
 
@@ -340,7 +317,6 @@ namespace accessibility
 
     void SAL_CALL AccessibleImageBullet::grabFocus(  ) throw (uno::RuntimeException)
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
 
         throw uno::RuntimeException("Not focusable",
                                     uno::Reference< uno::XInterface >
@@ -349,7 +325,6 @@ namespace accessibility
 
     sal_Int32 SAL_CALL AccessibleImageBullet::getForeground(  ) throw (::com::sun::star::uno::RuntimeException)
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
 
         // #104444# Added to XAccessibleComponent interface
         svtools::ColorConfig aColorConfig;
@@ -359,7 +334,6 @@ namespace accessibility
 
     sal_Int32 SAL_CALL AccessibleImageBullet::getBackground(  ) throw (::com::sun::star::uno::RuntimeException)
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
 
         // #104444# Added to XAccessibleComponent interface
         Color aColor( Application::GetSettings().GetStyleSettings().GetWindowColor().GetColor() );
@@ -372,21 +346,18 @@ namespace accessibility
 
     OUString SAL_CALL AccessibleImageBullet::getImplementationName (void) throw (uno::RuntimeException)
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
 
         return OUString("AccessibleImageBullet");
     }
 
     sal_Bool SAL_CALL AccessibleImageBullet::supportsService (const OUString& sServiceName) throw (uno::RuntimeException)
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
 
         return cppu::supportsService(this, sServiceName);
     }
 
     uno::Sequence< OUString > SAL_CALL AccessibleImageBullet::getSupportedServiceNames (void) throw (uno::RuntimeException)
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
 
         const OUString sServiceName ("com.sun.star.accessibility.AccessibleContext");
         return uno::Sequence< OUString > (&sServiceName, 1);
@@ -394,28 +365,24 @@ namespace accessibility
 
     OUString SAL_CALL AccessibleImageBullet::getServiceName (void) throw (uno::RuntimeException)
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
 
         return OUString("com.sun.star.accessibility.AccessibleContext");
     }
 
     void AccessibleImageBullet::SetIndexInParent( sal_Int32 nIndex )
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
 
         mnIndexInParent = nIndex;
     }
 
     void AccessibleImageBullet::SetEEOffset( const Point& rOffset )
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
 
         maEEOffset = rOffset;
     }
 
     void AccessibleImageBullet::Dispose()
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
 
         int nClientId( getNotifierClientId() );
 
@@ -443,7 +410,6 @@ namespace accessibility
 
     void AccessibleImageBullet::SetEditSource( SvxEditSource* pEditSource )
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
 
         mpEditSource = pEditSource;
 
@@ -461,7 +427,6 @@ namespace accessibility
 
     void AccessibleImageBullet::FireEvent(const sal_Int16 nEventId, const uno::Any& rNewValue, const uno::Any& rOldValue ) const
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
 
         uno::Reference < XAccessibleContext > xThis( const_cast< AccessibleImageBullet* > (this)->getAccessibleContext() );
 
@@ -474,21 +439,18 @@ namespace accessibility
 
     void AccessibleImageBullet::GotPropertyEvent( const uno::Any& rNewValue, const sal_Int16 nEventId ) const
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
 
         FireEvent( nEventId, rNewValue );
     }
 
     void AccessibleImageBullet::LostPropertyEvent( const uno::Any& rOldValue, const sal_Int16 nEventId ) const
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
 
         FireEvent( nEventId, uno::Any(), rOldValue );
     }
 
     void AccessibleImageBullet::SetState( const sal_Int16 nStateId )
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
 
         ::utl::AccessibleStateSetHelper* pStateSet = static_cast< ::utl::AccessibleStateSetHelper*>(mxStateSet.get());
         if( pStateSet != NULL &&
@@ -501,7 +463,6 @@ namespace accessibility
 
     void AccessibleImageBullet::UnSetState( const sal_Int16 nStateId )
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
 
         ::utl::AccessibleStateSetHelper* pStateSet = static_cast< ::utl::AccessibleStateSetHelper*>(mxStateSet.get());
         if( pStateSet != NULL &&
@@ -514,14 +475,12 @@ namespace accessibility
 
     int AccessibleImageBullet::getNotifierClientId() const
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
 
         return mnNotifierClientId;
     }
 
     void AccessibleImageBullet::SetParagraphIndex( sal_Int32 nIndex )
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
 
         uno::Any aOldDesc;
         uno::Any aOldName;
@@ -551,14 +510,12 @@ namespace accessibility
 
     sal_Int32 AccessibleImageBullet::GetParagraphIndex() const SAL_THROW((uno::RuntimeException))
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
 
         return mnParagraphIndex;
     }
 
     SvxEditSource& AccessibleImageBullet::GetEditSource() const SAL_THROW((uno::RuntimeException))
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
 
         if( mpEditSource )
             return *mpEditSource;
@@ -571,7 +528,6 @@ namespace accessibility
 
     SvxTextForwarder& AccessibleImageBullet::GetTextForwarder() const SAL_THROW((uno::RuntimeException))
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
 
         SvxEditSource& rEditSource = GetEditSource();
         SvxTextForwarder* pTextForwarder = rEditSource.GetTextForwarder();
@@ -593,7 +549,6 @@ namespace accessibility
 
     SvxViewForwarder& AccessibleImageBullet::GetViewForwarder() const SAL_THROW((uno::RuntimeException))
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
 
         SvxEditSource& rEditSource = GetEditSource();
         SvxViewForwarder* pViewForwarder = rEditSource.GetViewForwarder();
@@ -617,7 +572,6 @@ namespace accessibility
 
     const Point& AccessibleImageBullet::GetEEOffset() const
     {
-        DBG_CHKTHIS( AccessibleImageBullet, NULL );
 
         return maEEOffset;
     }

editeng/source/accessibility/AccessibleStaticTextBase.cxx
@@ -102,7 +102,6 @@ namespace accessibility
     //
     //------------------------------------------------------------------------
 
-    DBG_NAME( AccessibleStaticTextBase_Impl );
 
     /** AccessibleStaticTextBase_Impl
 
@@ -123,7 +122,6 @@ namespace accessibility
 
         SvxEditSourceAdapter& GetEditSource() const SAL_THROW((uno::RuntimeException))
         {
-            DBG_CHKTHIS( AccessibleStaticTextBase_Impl, NULL );
 
             return maEditSource;
         }
@@ -133,13 +131,11 @@ namespace accessibility
 
         void SetEventSource( const uno::Reference< XAccessible >& rInterface )
         {
-            DBG_CHKTHIS( AccessibleStaticTextBase_Impl, NULL );
 
             mxThis = rInterface;
         }
         uno::Reference< XAccessible > GetEventSource() const
         {
-            DBG_CHKTHIS( AccessibleStaticTextBase_Impl, NULL );
 
             return mxThis;
         }
@@ -147,7 +143,6 @@ namespace accessibility
         void SetOffset( const Point& );
         Point GetOffset() const
         {
-            DBG_CHKTHIS( AccessibleStaticTextBase_Impl, NULL );
 
             ::osl::MutexGuard aGuard( maMutex ); Point aPoint( maOffset );
             return aPoint;
@@ -165,14 +160,12 @@ namespace accessibility
 
         EPosition                   Index2Internal( sal_Int32 nFlatIndex ) const
         {
-            DBG_CHKTHIS( AccessibleStaticTextBase_Impl, NULL );
 
             return ImpCalcInternal( nFlatIndex, false );
         }
 
         EPosition                   Range2Internal( sal_Int32 nFlatIndex ) const
         {
-            DBG_CHKTHIS( AccessibleStaticTextBase_Impl, NULL );
 
             return ImpCalcInternal( nFlatIndex, true );
         }
@@ -229,7 +222,6 @@ namespace accessibility
         maMutex(),
         maOffset(0,0)
     {
-        DBG_CTOR( AccessibleStaticTextBase_Impl, NULL );
 
         // TODO: this is still somewhat of a hack, all the more since
         // now the maTextParagraph has an empty parent reference set
@@ -237,13 +229,11 @@ namespace accessibility
 
     AccessibleStaticTextBase_Impl::~AccessibleStaticTextBase_Impl()
     {
-        DBG_DTOR( AccessibleStaticTextBase_Impl, NULL );
     }
 
     SAL_WNODEPRECATED_DECLARATIONS_PUSH
     void AccessibleStaticTextBase_Impl::SetEditSource( ::std::auto_ptr< SvxEditSource > pEditSource ) SAL_THROW((uno::RuntimeException))
     {
-        DBG_CHKTHIS( AccessibleStaticTextBase_Impl, NULL );
 
         maEditSource.SetEditSource( pEditSource );
         if( mpTextParagraph )
@@ -253,7 +243,6 @@ namespace accessibility
 
     void AccessibleStaticTextBase_Impl::SetOffset( const Point& rPoint )
     {
-        DBG_CHKTHIS( AccessibleStaticTextBase_Impl, NULL );
 
         // guard against non-atomic access to maOffset data structure
         {
@@ -270,14 +259,12 @@ namespace accessibility
 
     void AccessibleStaticTextBase_Impl::UpdateChildren()
     {
-        DBG_CHKTHIS( AccessibleStaticTextBase_Impl, NULL );
 
         // currently no children
     }
 
     void AccessibleStaticTextBase_Impl::Dispose()
     {
-        DBG_CHKTHIS( AccessibleStaticTextBase_Impl, NULL );
 
         // we're the owner of the paragraph, so destroy it, too
         if( mpTextParagraph )
@@ -298,7 +285,6 @@ namespace accessibility
 
     AccessibleEditableTextPara& AccessibleStaticTextBase_Impl::GetParagraph( sal_Int32 nPara ) const
     {
-        DBG_CHKTHIS( AccessibleStaticTextBase_Impl, NULL );
 
         if( !mpTextParagraph )
             throw lang::DisposedException ("object has been already disposed", mxThis );
@@ -312,7 +298,6 @@ namespace accessibility
 
     sal_Int32 AccessibleStaticTextBase_Impl::GetParagraphCount() const
     {
-        DBG_CHKTHIS( AccessibleStaticTextBase_Impl, NULL );
 
         if( !mpTextParagraph )
             return 0;
@@ -357,7 +342,6 @@ namespace accessibility
 
     EPosition AccessibleStaticTextBase_Impl::ImpCalcInternal( sal_Int32 nFlatIndex, bool bExclusive ) const
     {
-        DBG_CHKTHIS( AccessibleStaticTextBase_Impl, NULL );
 
         if( nFlatIndex < 0 )
             throw lang::IndexOutOfBoundsException("AccessibleStaticTextBase_Impl::Index2Internal: character index out of bounds",
@@ -399,7 +383,6 @@ namespace accessibility
     sal_Bool AccessibleStaticTextBase_Impl::SetSelection( sal_Int32 nStartPara, sal_Int32 nStartIndex,
                                                           sal_Int32 nEndPara, sal_Int32 nEndIndex )
     {
-        DBG_CHKTHIS( AccessibleStaticTextBase_Impl, NULL );
 
         if( !mpTextParagraph )
             return sal_False;
@@ -418,7 +401,6 @@ namespace accessibility
     sal_Bool AccessibleStaticTextBase_Impl::CopyText( sal_Int32 nStartPara, sal_Int32 nStartIndex,
                                                       sal_Int32 nEndPara, sal_Int32 nEndIndex )
     {
-        DBG_CHKTHIS( AccessibleStaticTextBase_Impl, NULL );
 
         if( !mpTextParagraph )
             return sal_False;

editeng/source/editeng/editattr.cxx
@@ -46,7 +46,6 @@
 
 #include "editattr.hxx"
 
-DBG_NAME( EE_EditAttrib )
 
 // -------------------------------------------------------------------------
 // class EditCharAttrib
@@ -54,7 +53,6 @@ DBG_NAME( EE_EditAttrib )
 EditCharAttrib::EditCharAttrib( const SfxPoolItem& rAttr, sal_uInt16 nS, sal_uInt16 nE ) :
     nStart(nS), nEnd(nE), bFeature(false), bEdge(false)
 {
-    DBG_CTOR( EE_EditAttrib, 0 );
     pItem = &rAttr;
 
     DBG_ASSERT( ( rAttr.Which() >= EE_ITEMS_START ) && ( rAttr.Which() <= EE_ITEMS_END ), "EditCharAttrib CTOR: Invalid id!" );
@@ -63,7 +61,6 @@ EditCharAttrib::EditCharAttrib( const SfxPoolItem& rAttr, sal_uInt16 nS, sal_uIn
 
 EditCharAttrib::~EditCharAttrib()
 {
-    DBG_DTOR( EE_EditAttrib, 0 );
 }
 
 void EditCharAttrib::SetFont( SvxFont&, OutputDevice* )

editeng/source/editeng/editdoc.cxx
@@ -161,11 +161,6 @@ sal_Bool IsScriptItemValid( sal_uInt16 nItemId, short nScriptType )
 // Should later be moved to TOOLS/STRING (Current: 303)
 // for Grep: WS_TARGET
 
-DBG_NAME( EE_TextPortion );
-DBG_NAME( EE_EditLine );
-DBG_NAME( EE_ContentNode );
-DBG_NAME( EE_CharAttribList );
-DBG_NAME( EE_ParaPortion )
 
 const SfxItemInfo aItemInfos[EDITITEMCOUNT] = {
         { SID_ATTR_FRAMEDIRECTION, SFX_ITEM_POOLABLE },         // EE_PARA_WRITINGDIR
@@ -532,7 +527,6 @@ void ExtraPortionInfo::SaveOrgDXArray( const sal_Int32* pDXArray, sal_uInt16 nLe
 
 ParaPortion::ParaPortion( ContentNode* pN )
 {
-    DBG_CTOR( EE_ParaPortion, 0 );
 
     pNode               = pN;
     bInvalid            = sal_True;
@@ -548,7 +542,6 @@ ParaPortion::ParaPortion( ContentNode* pN )
 
 ParaPortion::~ParaPortion()
 {
-    DBG_DTOR( EE_ParaPortion, 0 );
 }
 
 void ParaPortion::MarkInvalid( sal_uInt16 nStart, short nDiff )
@@ -948,7 +941,6 @@ EditLine::EditLine() :
     bHangingPunctuation(false),
     bInvalid(true)
 {
-    DBG_CTOR( EE_EditLine, 0 );
 
     nStart = nEnd = 0;
     nStartPortion = 0;  // to be able to tell the difference between a line
@@ -966,7 +958,6 @@ EditLine::EditLine( const EditLine& r ) :
     bHangingPunctuation(r.bHangingPunctuation),
     bInvalid(true)
 {
-    DBG_CTOR( EE_EditLine, 0 );
 
     nEnd = r.nEnd;
     nStart = r.nStart;
@@ -983,7 +974,6 @@ EditLine::EditLine( const EditLine& r ) :
 
 EditLine::~EditLine()
 {
-    DBG_DTOR( EE_EditLine, 0 );
 }
 
 EditLine::CharPosArrayType& EditLine::GetCharPosArray()
@@ -1298,18 +1288,15 @@ sal_Bool operator != ( const EditPaM& r1,  const EditPaM& r2  )
 
 ContentNode::ContentNode( SfxItemPool& rPool ) : aContentAttribs( rPool )
 {
-    DBG_CTOR( EE_ContentNode, 0 );
 }
 
 ContentNode::ContentNode( const OUString& rStr, const ContentAttribs& rContentAttribs ) :
     maString(rStr), aContentAttribs(rContentAttribs)
 {
-    DBG_CTOR( EE_ContentNode, 0 );
 }
 
 ContentNode::~ContentNode()
 {
-    DBG_DTOR( EE_ContentNode, 0 );
 }
 
 void ContentNode::ExpandAttribs( sal_uInt16 nIndex, sal_uInt16 nNew, SfxItemPool& rItemPool )
@@ -2694,12 +2681,10 @@ CharAttribList::CharAttribList()
 , aDefFont()
 , bHasEmptyAttribs(false)
 {
-    DBG_CTOR( EE_CharAttribList, 0 );
 }
 
 CharAttribList::~CharAttribList()
 {
-    DBG_DTOR( EE_CharAttribList, 0 );
 }
 
 void CharAttribList::InsertAttrib( EditCharAttrib* pAttrib )

editeng/source/editeng/editobj.cxx
@@ -56,8 +56,6 @@ using std::endl;
 
 using namespace com::sun::star;
 
-DBG_NAME( EE_EditTextObject )
-DBG_NAME( XEditAttribute )
 
 //--------------------------------------------------------------
 
@@ -72,7 +70,6 @@ XEditAttribute* MakeXEditAttribute( SfxItemPool& rPool, const SfxPoolItem& rItem
 
 XEditAttribute::XEditAttribute( const SfxPoolItem& rAttr, sal_uInt16 nS, sal_uInt16 nE )
 {
-    DBG_CTOR( XEditAttribute, 0 );
     pItem = &rAttr;
     nStart = nS;
     nEnd = nE;
@@ -80,7 +77,6 @@ XEditAttribute::XEditAttribute( const SfxPoolItem& rAttr, sal_uInt16 nS, sal_uIn
 
 XEditAttribute::~XEditAttribute()
 {
-    DBG_DTOR( XEditAttribute, 0 );
     pItem = 0;  // belongs to the Pool.
 }
 

commit 0cbc0181d52433790bb607176ff01dcffc89ce99

ofz: leak in loading ppts
regression from

commit 5e10e6937a8d5117555a4dcc9520aca173d75c95
Date:   Sat Jan 18 10:15:10 2014 +0100

    EDITENG : Remove usage of DBG_CTOR and DBG_DTOR.

Change-Id: Id109f5bb23b1e4a93195548f880973ebc778515f

editeng/source/editeng/editdoc.hxx
@@ -386,7 +386,7 @@ struct ExtraPortionInfo
 class TextPortion
 {
 private:
-    ExtraPortionInfo*   pExtraInfos;
+    std::unique_ptr<ExtraPortionInfo> xExtraInfos;
     sal_Int32           nLen;
     Size                aOutSz;
     PortionKind         nKind;
@@ -396,8 +396,7 @@ private:
 
 public:
                 TextPortion( sal_Int32 nL )
-                : pExtraInfos( nullptr )
-                , nLen( nL )
+                : nLen( nL )
                 , aOutSz( -1, -1 )
                 , nKind( PortionKind::TEXT )
                 , nRightToLeftLevel( 0 )
@@ -406,8 +405,7 @@ public:
                 }
 
                 TextPortion( const TextPortion& r )
-                : pExtraInfos( nullptr )
-                , nLen( r.nLen )
+                : nLen( r.nLen )
                 , aOutSz( r.aOutSz )
                 , nKind( r.nKind )
                 , nRightToLeftLevel( r.nRightToLeftLevel )
@@ -434,8 +432,8 @@ public:
 
     bool           HasValidSize() const        { return aOutSz.Width() != (-1); }
 
-    ExtraPortionInfo*   GetExtraInfos() const { return pExtraInfos; }
-    void                SetExtraInfos( ExtraPortionInfo* p ) { delete pExtraInfos; pExtraInfos = p; }
+    ExtraPortionInfo*   GetExtraInfos() const { return xExtraInfos.get(); }
+    void                SetExtraInfos( ExtraPortionInfo* p ) { xExtraInfos.reset(p); }
 };
 
 