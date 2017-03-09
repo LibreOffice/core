739345e73af9e14d9c666836d0e35a93b1ed12ee
Stephan Bergmann <sbergman@redhat.com>
2017-03-09 11:36:27 +0100

    Add missing CxxClrObject case
    Based on shm_get's (?) patch at <http://pastebin.com/yCghrjWX>.  According to
    SweetShark on IRC, the reason for writing these phony files is:  "IIRC not
    having them resulted in the builds being slow n windows as touching them
    required forks and windows is unionized and introduced the 35-fork-week or
    something."
    
    Change-Id: Ie0e6e2aa4e56ab620325ea55b4513e185db38ae7

--- solenv/bin/concat-deps.c
+++ solenv/bin/concat-deps.c
@@ -1076,6 +1076,11 @@ off_t size;
                     created_line = generate_phony_line(src_relative, "o");
                     rc = generate_phony_file(fn, created_line);
                 }
+                else if(strncmp(src_relative, "CxxClrObject/", 13) == 0)
+                {
+                    created_line = generate_phony_line(src_relative, "o");
+                    rc = generate_phony_file(fn, created_line);
+                }
                 else
                 {
                     fprintf(stderr, "no magic for %s(%s) in %s\n", fn, src_relative, work_dir);
