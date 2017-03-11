441534187d6a999e12e4588728023e01d9578770
Tamás Zolnai <tamas.zolnai@collabora.com>
2017-03-11 15:55:33 +0100

    Use the converted shape rectanle here
    Change-Id: I38092b9ca0de2c73a639588d441eff56eb88e33e

--- oox/source/drawingml/chart/chartdrawingfragment.cxx
+++ oox/source/drawingml/chart/chartdrawingfragment.cxx
@@ -218,8 +218,8 @@ void ChartDrawingFragment::onEndElement()
                     getLimitedValue< sal_Int32, sal_Int64 >( aShapeRectEmu.Height, 0, SAL_MAX_INT32 ) );
 
                 // Set the position and size before calling addShape().
-                mxShape->setPosition(awt::Point(aShapeRectEmu.X, aShapeRectEmu.Y));
-                mxShape->setSize(awt::Size(aShapeRectEmu.Width, aShapeRectEmu.Height));
+                mxShape->setPosition(awt::Point(aShapeRectEmu32.X, aShapeRectEmu32.Y));
+                mxShape->setSize(awt::Size(aShapeRectEmu32.Width, aShapeRectEmu32.Height));
 
                 basegfx::B2DHomMatrix aMatrix;
                 mxShape->addShape( getFilter(), getFilter().getCurrentTheme(), mxDrawPage, aMatrix, mxShape->getFillProperties(), &aShapeRectEmu32 );
