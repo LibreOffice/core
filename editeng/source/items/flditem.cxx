cae16645e17df6a1f87443c00b6abfd26dabd27f
Rosemary Sebastian <rosemaryseb8@gmail.com>
2017-03-16 15:25:26 +0000

    Revert "Implement export and import of author field in fixed format"
    This reverts commit 5de40734af30a8b897c6c5b7f681759e36a25557.
    
    Change-Id: Ib09fb1dc54066cadcd9503c92140d78e7482ae6c
    Reviewed-on: https://gerrit.libreoffice.org/35281
    Reviewed-by: Rosemary Sebastian <rosemaryseb8@gmail.com>
    Tested-by: Rosemary Sebastian <rosemaryseb8@gmail.com>

--- editeng/source/items/flditem.cxx
+++ editeng/source/items/flditem.cxx
@@ -181,16 +181,13 @@ SvxFieldData* SvxFieldData::Create(const uno::Reference<text::XTextContent>& xTe
                     SvxAuthorField* pData = new SvxAuthorField(
                             aFirstName, aLastName, OUString(), bIsFixed ? SVXAUTHORTYPE_FIX : SVXAUTHORTYPE_VAR);
 
-                    if (!bIsFixed)
+                    if (!bFullName)
                     {
-                        if (!bFullName)
-                        {
-                            pData->SetFormat(SVXAUTHORFORMAT_SHORTNAME);
-                        }
-                        else if (nFmt >= SVXAUTHORFORMAT_FULLNAME && nFmt <= SVXAUTHORFORMAT_SHORTNAME)
-                        {
-                            pData->SetFormat(static_cast<SvxAuthorFormat>(nFmt));
-                        }
+                        pData->SetFormat(SVXAUTHORFORMAT_SHORTNAME);
+                    }
+                    else if (nFmt >= SVXAUTHORFORMAT_FULLNAME && nFmt <= SVXAUTHORFORMAT_SHORTNAME)
+                    {
+                        pData->SetFormat(static_cast<SvxAuthorFormat>(nFmt));
                     }
 
                     return pData;
