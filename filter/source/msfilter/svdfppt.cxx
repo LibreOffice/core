5dd86aa6ae22e41f718f7b429b16b63414a373b7
Caolán McNamara <caolanm@redhat.com>
2017-03-16 09:13:13 +0000

    ofz#876 avoid accessing empty rows
    (cherry picked from commit b9892037c303e645ee1a987d80734361700d91ac)
    
    Change-Id: Iba539fb03611bbe5627cc7976c1146d08a2ab5fd
    Reviewed-on: https://gerrit.libreoffice.org/35257
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Michael Stahl <mstahl@redhat.com>
    (cherry picked from commit 2f0e14de089b9da350444bddb5baf6ba9baac338)
    (cherry picked from commit bcdf9b934db7ae5459fefdf4a0798307e01df397)

--- filter/source/msfilter/svdfppt.cxx
+++ filter/source/msfilter/svdfppt.cxx
@@ -7655,6 +7655,10 @@ SdrObject* SdrPowerPointImport::CreateTable( SdrObject* pGroup, sal_uInt32* pTab
                     aColumns.insert( aSnapRect.Left() );
                 }
             }
+
+            if (aRows.empty())
+                return pRet;
+
             sdr::table::SdrTableObj* pTable = new sdr::table::SdrTableObj( pSdrModel );
             pTable->uno_lock();
             Reference< XTable > xTable( pTable->getTable() );
