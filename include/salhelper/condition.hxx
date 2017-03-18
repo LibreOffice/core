4c144817997113cc6ce3d9cacb2bb380c97cb2fa
Chris Sherlock <chris.sherlock79@gmail.com>
2017-03-18 18:32:13 +1100

    salhelper: change oslCondition to osl::Condition
    Condition is deprecated already, but there is no need for the
    us to use the low-level C-API, when in fact there is a C++ fascade
    that calls on this via the C++ abstraction, osl::Condition.
    
    This will make it much easier to switch to using std::condition_variable
    
    Change-Id: Ia362666ff241293e143de0fa1dc0bc3a990bef82

--- include/salhelper/condition.hxx
+++ include/salhelper/condition.hxx
@@ -22,7 +22,7 @@
 #define INCLUDED_SALHELPER_CONDITION_HXX
 
 
-#include <osl/conditn.h>
+#include <osl/conditn.hxx>
 #include <osl/mutex.hxx>
 #include <salhelper/salhelperdllapi.h>
 
@@ -53,8 +53,8 @@ namespace salhelper
         Condition(Condition &) SAL_DELETED_FUNCTION;
         void operator =(Condition &) SAL_DELETED_FUNCTION;
 
-        osl::Mutex&  m_aMutex;
-        oslCondition m_aCondition;
+        osl::Mutex&     m_aMutex;
+        osl::Condition  m_aCondition;
     };
 
 
