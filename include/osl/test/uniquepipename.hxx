bda4fe043b8bd03d647bc9497fc271cd458ac91d
Chris Sherlock <chris.sherlock79@gmail.com>
2017-03-18 12:15:55 +1100

    osl: test/uniquepipename.hxx use <> delimiters for headers
    Change-Id: Idabde42af4eed1000e32d605cf93f359ed218ff0

--- include/osl/test/uniquepipename.hxx
+++ include/osl/test/uniquepipename.hxx
@@ -17,11 +17,12 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#include "sal/types.h"
-#include "cppunit/TestAssert.h"
-#include "osl/process.h"
-#include "rtl/ustring.h"
-#include "rtl/ustring.hxx"
+#include <sal/types.h>
+#include <rtl/ustring.h>
+#include <rtl/ustring.hxx>
+#include <cppunit/TestAssert.h>
+
+#include <osl/process.h>
 
 namespace osl {
 namespace test {
