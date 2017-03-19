550c6ed03a6d5d56ec23ce0d18d081b03226f539
Khaled Hosny <khaledhosny@eglug.org>
2017-03-19 16:55:17 +0200

    Typo
    Change-Id: Icdcf060b8e6d5ecbb08a1915261687026d674c0b

--- vcl/workben/fftester.cxx
+++ vcl/workben/fftester.cxx
@@ -105,7 +105,7 @@ SAL_IMPLEMENT_MAIN_WITH_ARGS(argc, argv)
         setenv("JPEGMEM", "1024M", 1);
         setenv("SAL_WMF_COMPLEXCLIP_VIA_REGION", "1", 1);
         setenv("SAL_DISABLE_PRINTERLIST", "1", 1);
-        setenv("SAL_NO_FONT_LOOPUP", "1", 1);
+        setenv("SAL_NO_FONT_LOOKUP", "1", 1);
 
 try_again:
 
