LibreOffice/core

commit bdaa13a87744e424d3c210fc7f3f9e4f199d8279

comphelper:: fix MSVC hang in ThreadPool::shutdown(), try #2
This takes a different approach than commit
9899ffd244dd367ba69dffe1f21f4f0222064a46.

Change the ThreadPool to automatically shutdown and join all threads
whenever waitUntilDone() is called.  Then start the threads again
in pushTask().

Because the ThreadPool is meant to be used synchronously with
waitUntilDone() called after adding all required tasks, this should
obviate the need to call shutdown() before process exit, as
there won't be any threads running at that point.

Change-Id: I2b8e639004a94cf05ccb4522aa1f0d3dac88a936
Reviewed-on: https://gerrit.libreoffice.org/35510
Reviewed-by: Michael Meeks <michael.meeks@collabora.com>
Tested-by: Jenkins <ci@libreoffice.org>

comphelper/source/misc/threadpool.cxx
@@ -75,21 +75,20 @@ public:
     }
 };
 
-ThreadPool::ThreadPool( sal_Int32 nWorkers ) :
-    mbTerminate( false )
+ThreadPool::ThreadPool(sal_Int32 nWorkers)
+    : mbTerminate(true)
+    , mnWorkers(nWorkers)
 {
-    std::unique_lock< std::mutex > aGuard( maMutex );
-
-    for( sal_Int32 i = 0; i < nWorkers; i++ )
-        maWorkers.push_back( new ThreadWorker( this ) );
-
-    for(rtl::Reference<ThreadWorker> & rpWorker : maWorkers)
-        rpWorker->launch();
 }
 
 ThreadPool::~ThreadPool()
 {
-    shutdown();
+    // note: calling shutdown from global variable dtor blocks forever on Win7
+    // note2: there isn't enough MSVCRT left on exit to call assert() properly
+    // so these asserts just print something to stderr but exit status is
+    // still 0, but hopefully they will be more helpful on non-WNT platforms
+    assert(mbTerminate);
+    assert(maTasks.empty());
 }
 
 struct ThreadPoolStatic : public rtl::StaticWithInit< std::shared_ptr< ThreadPool >,
@@ -136,7 +135,11 @@ void ThreadPool::shutdown()
         return;
 
     std::unique_lock< std::mutex > aGuard( maMutex );
+    shutdownLocked(aGuard);
+}
 
+void ThreadPool::shutdownLocked(std::unique_lock<std::mutex>& aGuard)
+{
     if( maWorkers.empty() )
     { // no threads at all -> execute the work in-line
         ThreadTask *pTask;
@@ -172,6 +175,14 @@ void ThreadPool::shutdown()
 void ThreadPool::pushTask( ThreadTask *pTask )
 {
     std::unique_lock< std::mutex > aGuard( maMutex );
+
+    mbTerminate = false;
+
+    if (maWorkers.size() < mnWorkers && maWorkers.size() <= maTasks.size())
+    {
+        maWorkers.push_back( new ThreadWorker( this ) );
+        maWorkers.back()->launch();
+    }
 
     pTask->mpTag->onTaskPushed();
     maTasks.insert( maTasks.begin(), pTask );
@@ -217,6 +228,14 @@ void ThreadPool::waitUntilDone(const std::shared_ptr<ThreadTaskTag>& rTag)
     }
 
     rTag->waitUntilDone();
+
+    {
+        std::unique_lock< std::mutex > aGuard( maMutex );
+        if (maTasks.empty()) // check if there are still tasks from another tag
+        {
+            shutdownLocked(aGuard);
+        }
+    }
 }
 
 std::shared_ptr<ThreadTaskTag> ThreadPool::createThreadTaskTag()

include/comphelper/threadpool.hxx
@@ -68,7 +68,7 @@ public:
     void        waitUntilDone(const std::shared_ptr<ThreadTaskTag>&);
 
     /// return the number of live worker threads
-    sal_Int32   getWorkerCount() const { return maWorkers.size(); }
+    sal_Int32   getWorkerCount() const { return mnWorkers; }
 
     /// wait until all work is completed, then join all threads
     void        shutdown();
@@ -85,11 +85,13 @@ private:
         @return a new task to perform, or NULL if list empty or terminated
     */
     ThreadTask *popWorkLocked( std::unique_lock< std::mutex > & rGuard, bool bWait );
+    void shutdownLocked(std::unique_lock<std::mutex>&);
 
     /// signalled when all in-progress tasks are complete
     std::mutex              maMutex;
     std::condition_variable maTasksChanged;
     bool                    mbTerminate;
+    std::size_t             mnWorkers;
     std::vector< ThreadTask * >   maTasks;
     std::vector< rtl::Reference< ThreadWorker > > maWorkers;
 };

commit 64c09371d6feed6e87aaa54cde081f001dcfca10

comphelper::ThreadPool: guard against concurrent shutdown/pushTask
To join a thread, the mutex must be released - another thread
in pushTask() could add a new thread to maWorkers at that point,
which must of course not be deleted.

Avoid the problem by transferring ownership of the to-be-deleted
threads to the calling thread.

(regression from bdaa13a87744e424d3c210fc7f3f9e4f199d8279)

Change-Id: I9d4fcfe4cb46a336586b5663934a12d47b2d8ccb

comphelper/source/misc/threadpool.cxx
@@ -157,18 +157,20 @@ void ThreadPool::shutdownLocked(std::unique_lock<std::mutex>& aGuard)
 
     maTasksChanged.notify_all();
 
-    while( !maWorkers.empty() )
+    decltype(maWorkers) aWorkers;
+    std::swap(maWorkers, aWorkers);
+    aGuard.unlock();
+
+    while (!aWorkers.empty())
     {
-        rtl::Reference< ThreadWorker > xWorker = maWorkers.back();
-        maWorkers.pop_back();
-        assert(std::find(maWorkers.begin(), maWorkers.end(), xWorker)
-                == maWorkers.end());
-        aGuard.unlock();
+        rtl::Reference<ThreadWorker> xWorker = aWorkers.back();
+        aWorkers.pop_back();
+        assert(std::find(aWorkers.begin(), aWorkers.end(), xWorker)
+                == aWorkers.end());
         {
             xWorker->join();
             xWorker.clear();
         }
-        aGuard.lock();
     }
 }
 