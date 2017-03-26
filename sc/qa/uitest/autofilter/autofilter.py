23282b476f094c735ecfae4f82798e36ac9cbbc9
Markus Mohrhard <markus.mohrhard@googlemail.com>
2017-03-26 23:10:45 +0200

    add test for multiple row filtering
    Change-Id: I55c898a7bf6796802ee6d04071801fec3fcf218d
    Reviewed-on: https://gerrit.libreoffice.org/35732
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Markus Mohrhard <markus.mohrhard@googlemail.com>

--- sc/qa/uitest/autofilter/autofilter.py
+++ sc/qa/uitest/autofilter/autofilter.py
@@ -48,4 +48,26 @@ class AutofilterTest(UITestCase):
 
         self.ui_test.close_doc()
 
+    def test_filter_multiple_rows(self):
+        doc = self.ui_test.load_file(get_url_for_data_file("autofilter.ods"))
+
+        xGridWin = self.xUITest.getTopFocusWindow().getChild("grid_window")
+        xGridWin.executeAction("LAUNCH", mkPropertyValues({"AUTOFILTER": "", "COL": "2", "ROW": "0"}))
+        xFloatWindow = self.xUITest.getFloatWindow()
+        xCheckListMenu = xFloatWindow.getChild("check_list_menu")
+
+        xTreeList = xCheckListMenu.getChild("check_list_box")
+        xFirstEntry = xTreeList.getChild("1")
+
+        xFirstEntry.executeAction("CLICK", tuple())
+
+        xOkBtn = xFloatWindow.getChild("ok")
+        xOkBtn.executeAction("CLICK", tuple())
+
+        self.assertFalse(is_row_hidden(doc, 0))
+        self.assertFalse(is_row_hidden(doc, 1))
+        self.assertTrue(is_row_hidden(doc, 2))
+        self.assertTrue(is_row_hidden(doc, 3))
+        self.assertFalse(is_row_hidden(doc, 4))
+
 # vim: set shiftwidth=4 softtabstop=4 expandtab:
