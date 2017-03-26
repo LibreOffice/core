128e00114a20ef68cd169790b11041e71db7c707
Markus Mohrhard <markus.mohrhard@googlemail.com>
2017-03-26 23:00:24 +0200

    add test for tdf#106214
    Change-Id: I9998aa40c3831a6033d4d61a6eab90d639ec5aa9
    Reviewed-on: https://gerrit.libreoffice.org/35731
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Markus Mohrhard <markus.mohrhard@googlemail.com>
    Reviewed-on: https://gerrit.libreoffice.org/58353
    Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>
    Tested-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>

--- sc/Module_sc.mk
+++ sc/Module_sc.mk
@@ -107,5 +107,8 @@ $(eval $(call gb_Module_add_perfcheck_targets,sc,\
 	CppunitTest_sc_tablesheetobj \
 ))
 
+$(eval $(call gb_Module_add_uicheck_targets,sc,\
+	UITest_autofilter \
+))
 
 # vim: set noet sw=4 ts=4:
