59af9cbbccee600408c89e872d548d153523ab1f
Mike Kaganski <mike.kaganski@collabora.com>
2017-03-27 17:35:45 +0300

    tdf#106799 WW8: Accept TTP Mark without Cell Mark
    Paragraph mark of inner table (0x0D) sometimes has
    sprmPFInnerTtp, but no sprmPFInnerTableCell. This still counts
    as cell end (at least, MS Word treats it that way).
    
    Unit test included.
    
    Change-Id: I5589cdd486c03ca4567d61882826cc7c245a40c9
    Reviewed-on: https://gerrit.libreoffice.org/35763
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

--- sw/source/filter/ww8/ww8par.cxx
+++ sw/source/filter/ww8/ww8par.cxx
@@ -3673,7 +3673,9 @@ bool SwWW8ImplReader::ReadChar(long nPosCp, long nCpOfs)
                         bParaMark = false;
                     }
                 }
-                else if (m_bWasTabCellEnd)
+                // tdf#106799: We expect TTP marks to be also cell marks,
+                // but sometimes sprmPFInnerTtp comes without sprmPFInnerTableCell
+                else if (m_bWasTabCellEnd || m_bWasTabRowEnd)
                 {
                     TabCellEnd();
                     bParaMark = false;
