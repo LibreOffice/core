8dd1fa09de75e0f2845113c3316630e988c1dd85
Marco Cecchetti <marco.cecchetti@collabora.com>
2017-03-29 13:42:35 +0200

    LOK - Calc: make computation of cell cursor position faster
    Now the computation of the cell cursor position exploits the cached
    row/col positions in HeightHelper/WidthHelper.
    
    That makes navigating through arrow keys independent from the current
    cell cursor position: the cell cursor position is updated at the
    bottom of the document as fast as at the top even for very big
    spreadsheets.
    
    Change-Id: Id369b63e547aedf30c233f75c70d18f32e015edf

--- sc/source/ui/view/viewdata.cxx
+++ sc/source/ui/view/viewdata.cxx
@@ -267,6 +267,8 @@ ScViewDataTable::ScViewDataTable() :
                 nCurY( 0 ),
                 nOldCurX( 0 ),
                 nOldCurY( 0 ),
+                nLOKOldCurX( 0 ),
+                nLOKOldCurY( 0 ),
                 nMaxTiledCol( 20 ),
                 nMaxTiledRow( 50 ),
                 bShowGrid( true ),
@@ -1880,6 +1882,13 @@ Point ScViewData::GetScrPos( SCCOL nWhereX, SCROW nWhereY, ScSplitPos eWhich,
     SCCOL   nX;
 
     long nScrPosX=0;
+    if (bIsTiledRendering)
+    {
+        const auto& rNearest = pThisTab->aWidthHelper.getNearestByIndex(nWhereX - 1);
+        nPosX = rNearest.first + 1;
+        nScrPosX = rNearest.second;
+    }
+
     if (nWhereX >= nPosX)
         for (nX = nPosX; nX < nWhereX && (bAllowNeg || bIsTiledRendering || nScrPosX <= aScrSize.Width()); nX++)
         {
@@ -1911,6 +1920,13 @@ Point ScViewData::GetScrPos( SCCOL nWhereX, SCROW nWhereY, ScSplitPos eWhich,
     SCROW   nY;
 
     long nScrPosY=0;
+    if (bIsTiledRendering)
+    {
+        const auto& rNearest = pThisTab->aHeightHelper.getNearestByIndex(nWhereY - 1);
+        nPosY = rNearest.first + 1;
+        nScrPosY = rNearest.second;
+    }
+
     if (nWhereY >= nPosY)
         for (nY = nPosY; nY < nWhereY && (bAllowNeg || bIsTiledRendering || nScrPosY <= aScrSize.Height()); nY++)
         {
