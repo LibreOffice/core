bf5dd96c326ebc74d79ca0fa6e41fff29464f5bc
Marco Cecchetti <marco.cecchetti@collabora.com>
2017-03-29 13:17:43 +0200

    LOK - Calc: invalidation of cached row/col positions on several cases
    Cached row positions are invalidated when one of the following event
    occurs:
    
    - one or more rows are inserted
    - one or more rows are deleted
    - one or more hidden rows are made visible
    - one or more visible rows are made hidden
    - one row is resized
    - optimal row height is changed
    - the value of the PPTY parameter is updated
    
    The same occurs for cached column positions.
    
    Change-Id: Idf7b8cf1e694142e4fa0e1613d19ee9272b9a1be

--- sc/source/ui/view/viewdata.cxx
+++ sc/source/ui/view/viewdata.cxx
@@ -2502,6 +2502,8 @@ void ScViewData::UpdateScreenZoom( const Fraction& rNewX, const Fraction& rNewY 
 
 void ScViewData::CalcPPT()
 {
+    double nOldPPTX = nPPTX;
+    double nOldPPTY = nPPTY;
     nPPTX = ScGlobal::nScreenPPTX * (double) GetZoomX();
     if (pDocShell)
         nPPTX = nPPTX / pDocShell->GetOutputFactor();   // Factor is printer to screen
@@ -2538,6 +2540,11 @@ void ScViewData::CalcPPT()
             }
         }
     }
+
+    if (nPPTX != nOldPPTX)
+        GetLOKWidthHelper().invalidateByPosition(0L);
+    if (nPPTY != nOldPPTY)
+        GetLOKHeightHelper().invalidateByPosition(0L);
 }
 
 #define SC_OLD_TABSEP   '/'
