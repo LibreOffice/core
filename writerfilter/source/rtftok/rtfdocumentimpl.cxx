c9405c278291562e868944454f4a03a9a3c53905
Caolán McNamara <caolanm@redhat.com>
2017-04-01 16:43:04 +0100

    ubsan: use WrapTextMode_MAKE_FIXED_SIZE instead of -1 as unset flag
    Change-Id: I0d460c5a0c14d078623fb1669a529a411002cad0

--- writerfilter/source/rtftok/rtfdocumentimpl.cxx
+++ writerfilter/source/rtftok/rtfdocumentimpl.cxx
@@ -3272,7 +3272,7 @@ RTFShape::RTFShape()
       nVertOrientRelation(0),
       nHoriOrientRelationToken(0),
       nVertOrientRelationToken(0),
-      nWrap((text::WrapTextMode)-1),
+      nWrap(text::WrapTextMode_MAKE_FIXED_SIZE),
       bInBackground(false)
 {
 }
