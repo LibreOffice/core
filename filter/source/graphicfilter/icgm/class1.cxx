fb41ebff32371ee7a7e07f671f7c769a8bb18718
Caolán McNamara <caolanm@redhat.com>
2017-04-02 16:50:38 +0100

    ofz: more check bounds on read
    Change-Id: I70018ee2ab282c11547f5bf9d81b2ee74c74aa04

--- filter/source/graphicfilter/icgm/class1.cxx
+++ filter/source/graphicfilter/icgm/class1.cxx
@@ -192,6 +192,10 @@ void CGM::ImplDoClass1()
             {
                 sal_uInt32 nCharSetType = ImplGetUI16();
                 sal_uInt32 nSize = ImplGetUI(1);
+
+                if (static_cast<sal_uIntPtr>(mpEndValidSource - (mpSource + mnParaSize)) < nSize)
+                    throw css::uno::Exception("attempt to read past end of input", nullptr);
+
                 pElement->aFontList.InsertCharSet( (CharSetType)nCharSetType, mpSource + mnParaSize, nSize );
                 mnParaSize += nSize;
             }
