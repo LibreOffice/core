f1a53e7a0b388e0a5303fe68dfbb4c60f4c7a0ff
Caolán McNamara <caolanm@redhat.com>
2017-04-04 14:29:00 +0100

    Resolves: tdf#106935 restore traditional toolbar labels
    Change-Id: I00cf16e9ce429f9186cc900a07f4d386e33b8f7b

--- sfx2/source/dialog/templdlg.cxx
+++ sfx2/source/dialog/templdlg.cxx
@@ -2190,18 +2190,15 @@ SfxTemplateDialog_Impl::SfxTemplateDialog_Impl(SfxBindings* pB, SfxTemplatePanel
         if (xUICommands.is())
         {
             uno::Any aCommand = xUICommands->getByName(".uno:StyleApply");
-            OUString sLabel = lcl_GetLabel( aCommand );
-            m_aActionTbR->InsertItem(SID_STYLE_WATERCAN, Image(BitmapEx(SfxResId(RID_SFXBMP_WATERCAN))), sLabel);
+            m_aActionTbR->InsertItem(SID_STYLE_WATERCAN, Image(BitmapEx(SfxResId(RID_SFXBMP_WATERCAN))), SfxResId(STR_STYLE_FILL_FORMAT_MODE).toString());
             m_aActionTbR->SetHelpId(SID_STYLE_WATERCAN, HID_TEMPLDLG_WATERCAN);
 
             aCommand = xUICommands->getByName(".uno:StyleNewByExample");
-            sLabel = lcl_GetLabel( aCommand );
-            m_aActionTbR->InsertItem(SID_STYLE_NEW_BY_EXAMPLE, Image(BitmapEx(SfxResId(RID_SFXBMP_NEW_BY_EXAMPLE))), sLabel);
+            m_aActionTbR->InsertItem(SID_STYLE_NEW_BY_EXAMPLE, Image(BitmapEx(SfxResId(RID_SFXBMP_NEW_BY_EXAMPLE))), SfxResId(STR_STYLE_NEW_STYLE_FROM_SELECTION).toString());
             m_aActionTbR->SetHelpId(SID_STYLE_NEW_BY_EXAMPLE, HID_TEMPLDLG_NEWBYEXAMPLE);
 
             aCommand = xUICommands->getByName(".uno:StyleUpdateByExample");
-            sLabel = lcl_GetLabel( aCommand );
-            m_aActionTbR->InsertItem(SID_STYLE_UPDATE_BY_EXAMPLE, Image(BitmapEx(SfxResId(RID_SFXBMP_UPDATE_BY_EXAMPLE))), sLabel);
+            m_aActionTbR->InsertItem(SID_STYLE_UPDATE_BY_EXAMPLE, Image(BitmapEx(SfxResId(RID_SFXBMP_UPDATE_BY_EXAMPLE))), SfxResId(STR_STYLE_UPDATE_STYLE).toString());
             m_aActionTbR->SetHelpId(SID_STYLE_UPDATE_BY_EXAMPLE, HID_TEMPLDLG_UPDATEBYEXAMPLE);
         }
     }
