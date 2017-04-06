22956b377b1ff916fe1d0b448f7e046f49e63095
Thorsten Behrens <Thorsten.Behrens@CIB.de>
2017-04-06 05:36:10 +0200

    lokit: compile-test a plain C version
    Change-Id: Ibe2108984df8f4fef9ed681941d517ca9ad9479c

---
+++ libreofficekit/qa/unit/test.h
@@ -0,0 +1,17 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef INCLUDED_LIBREOFFICEKIT_QA_TEST_H
+#define INCLUDED_LIBREOFFICEKIT_QA_TEST_H
+
+LibreOfficeKit* compile_test(void);
+
+#endif // INCLUDED_LIBREOFFICEKIT_QA_TEST_H
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
