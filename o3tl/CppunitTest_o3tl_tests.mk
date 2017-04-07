50057a37a877213d935958d5c643fde1434d680c
Stephan Bergmann <sbergman@redhat.com>
2017-04-07 15:59:12 +0200

    Introduce o3tl::string_view.hxx approximation of C++17 <string_view>
    ...and use it in configmgr/source/writemodfile.hxx
    
    Change-Id: Ie683dc21010ed45cc454ff89bea0376994b351f2
    Reviewed-on: https://gerrit.libreoffice.org/36270
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

--- o3tl/CppunitTest_o3tl_tests.mk
+++ o3tl/CppunitTest_o3tl_tests.mk
@@ -31,6 +31,7 @@ $(eval $(call gb_CppunitTest_add_exception_objects,o3tl_tests,\
 	o3tl/qa/test-cow_wrapper \
 	o3tl/qa/test-lru_map \
 	o3tl/qa/test-sorted_vector \
+	o3tl/qa/test-string_view \
 	o3tl/qa/test-typed_flags \
 	o3tl/qa/test-vector_pool \
 ))
