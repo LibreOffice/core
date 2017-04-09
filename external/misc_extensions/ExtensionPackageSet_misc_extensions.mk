dab3923a690f742d0a32532b15f47e2557706c8a
Gabor Kelemen <kelemeng@ubuntu.com>
2017-04-10 00:12:36 +0200

    Remove more remains of bundled extensions
    Change-Id: I22686d1e3b89c017a6e48a4b42cabc5e092b11cc
    Reviewed-on: https://gerrit.libreoffice.org/36396
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Michael Stahl <mstahl@redhat.com>

--- external/misc_extensions/ExtensionPackageSet_misc_extensions.mk
+++ external/misc_extensions/ExtensionPackageSet_misc_extensions.mk
@@ -9,56 +9,8 @@
 
 $(eval $(call gb_ExtensionPackageSet_ExtensionPackageSet,misc_extensions))
 
-ifneq ($(BARCODE_EXTENSION_PACK),)
-$(eval $(call gb_ExtensionPackageSet_add_extension,misc_extensions,Barcode,$(BARCODE_EXTENSION_PACK)))
-endif
-
-ifneq ($(DIAGRAM_EXTENSION_PACK),)
-$(eval $(call gb_ExtensionPackageSet_add_extension,misc_extensions,SmART,$(DIAGRAM_EXTENSION_PACK)))
-endif
-
-ifneq ($(HUNART_EXTENSION_PACK),)
-$(eval $(call gb_ExtensionPackageSet_add_extension,misc_extensions,hunart,$(HUNART_EXTENSION_PACK)))
-endif
-
 ifneq ($(NUMBERTEXT_EXTENSION_PACK),)
 $(eval $(call gb_ExtensionPackageSet_add_extension,misc_extensions,numbertext,$(NUMBERTEXT_EXTENSION_PACK)))
-endif
-
-ifneq ($(SUNTEMPLATES_DE_PACK),)
-$(eval $(call gb_ExtensionPackageSet_add_extension,misc_extensions,Sun_ODF_Template_Pack_de,$(SUNTEMPLATES_DE_PACK)))
-endif
-
-ifneq ($(SUNTEMPLATES_EN_US_PACK),)
-$(eval $(call gb_ExtensionPackageSet_add_extension,misc_extensions,Sun_ODF_Template_Pack_en-US,$(SUNTEMPLATES_EN_US_PACK)))
-endif
-
-ifneq ($(SUNTEMPLATES_ES_PACK),)
-$(eval $(call gb_ExtensionPackageSet_add_extension,misc_extensions,Sun_ODF_Template_Pack_es,$(SUNTEMPLATES_ES_PACK)))
-endif
-
-ifneq ($(SUNTEMPLATES_FR_PACK),)
-$(eval $(call gb_ExtensionPackageSet_add_extension,misc_extensions,Sun_ODF_Template_Pack_fr,$(SUNTEMPLATES_FR_PACK)))
-endif
-
-ifneq ($(SUNTEMPLATES_HU_PACK),)
-$(eval $(call gb_ExtensionPackageSet_add_extension,misc_extensions,Sun_ODF_Template_Pack_hu,$(SUNTEMPLATES_HU_PACK)))
-endif
-
-ifneq ($(SUNTEMPLATES_IT_PACK),)
-$(eval $(call gb_ExtensionPackageSet_add_extension,misc_extensions,Sun_ODF_Template_Pack_it,$(SUNTEMPLATES_IT_PACK)))
-endif
-
-ifneq ($(TYPO_EXTENSION_PACK),)
-$(eval $(call gb_ExtensionPackageSet_add_extension,misc_extensions,typo,$(TYPO_EXTENSION_PACK)))
-endif
-
-ifneq ($(VALIDATOR_EXTENSION_PACK),)
-$(eval $(call gb_ExtensionPackageSet_add_extension,misc_extensions,Validator,$(VALIDATOR_EXTENSION_PACK)))
-endif
-
-ifneq ($(WATCH_WINDOW_EXTENSION_PACK),)
-$(eval $(call gb_ExtensionPackageSet_add_extension,misc_extensions,WatchWindow,$(WATCH_WINDOW_EXTENSION_PACK)))
 endif
 
 # vim: set noet sw=4 ts=4:
