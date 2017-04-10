LibreOffice/core

commit 0fe6f1a196b70f0ba4c948389b2ef9b1e77187b8

use actual UNO enums in vcl..xmlsecurity
Change-Id: Id862544f27ebcbe8363f93dc83192d0f0cddb5cc
Reviewed-on: https://gerrit.libreoffice.org/36041
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

xmlscript/source/xmldlg_imexp/xmldlg_import.cxx
@@ -1070,7 +1070,7 @@ bool ImportContext::importButtonTypeProperty(
             _pImport->XMLNS_DIALOGS_UID, rAttrName ) );
     if (!buttonType.isEmpty())
     {
-        sal_Int16 nButtonType;
+        awt::PushButtonType nButtonType;
         if ( buttonType == "standard" )
         {
             nButtonType = awt::PushButtonType_STANDARD;

vcl/osx/a11ytextattributeswrapper.mm
@@ -141,7 +141,7 @@ using namespace ::com::sun::star::uno;
 
 +(int)convertItalicStyle:(PropertyValue)property {
     int italicStyle = NSUnitalicFontMask;
-    sal_Int16 value = property.Value.get< ::css_awt::FontSlant>();
+    ::css_awt::FontSlant value = property.Value.get< ::css_awt::FontSlant>();
     if ( value == ::css_awt::FontSlant_ITALIC ) {
         italicStyle = NSItalicFontMask;
     }
@@ -279,7 +279,7 @@ SAL_WNODEPRECATED_DECLARATIONS_PUSH
     // 'NSJustifiedTextAlignment' is deprecated: first deprecated in macOS 10.12
     // 'NSLeftTextAlignment' is deprecated: first deprecated in macOS 10.12
     // 'NSRightTextAlignment' is deprecated: first deprecated in macOS 10.12
-                switch(alignment) {
+                switch((css::style::ParagraphAdjust)alignment) {
                     case css::style::ParagraphAdjust_RIGHT : textAlignment = [NSNumber numberWithInteger:NSRightTextAlignment]    ; break;
                     case css::style::ParagraphAdjust_CENTER: textAlignment = [NSNumber numberWithInteger:NSCenterTextAlignment]   ; break;
                     case css::style::ParagraphAdjust_BLOCK : textAlignment = [NSNumber numberWithInteger:NSJustifiedTextAlignment]; break;

vcl/source/app/unohelp.cxx
@@ -192,7 +192,7 @@ css::awt::FontSlant vcl::unohelper::ConvertFontSlant(FontItalic eItalic)
             eRet = css::awt::FontSlant_DONTKNOW;
             break;
         case FontItalic_FORCE_EQUAL_SIZE:
-            eRet = css::awt::FontSlant_MAKE_FIXED_SIZE;
+            eRet = css::awt::FontSlant::FontSlant_MAKE_FIXED_SIZE;
             break;
     }
     return eRet;
@@ -223,7 +223,7 @@ FontItalic vcl::unohelper::ConvertFontSlant(css::awt::FontSlant eSlant)
             //there is no vcl reverse normal
             eRet = ITALIC_NORMAL;
             break;
-        case css::awt::FontSlant_MAKE_FIXED_SIZE:
+        case css::awt::FontSlant::FontSlant_MAKE_FIXED_SIZE:
             eRet = FontItalic_FORCE_EQUAL_SIZE;
             break;
     }

vcl/source/gdi/lineinfo.cxx
@@ -216,7 +216,7 @@ SvStream& WriteLineInfo( SvStream& rOStm, const LineInfo& rLineInfo )
     rOStm.WriteUInt16( static_cast<sal_uInt16>(rLineInfo.mpImplLineInfo->meLineJoin) );
 
     // since version4
-    rOStm.WriteUInt16( rLineInfo.mpImplLineInfo->meLineCap );
+    rOStm.WriteUInt16( (sal_uInt16)rLineInfo.mpImplLineInfo->meLineCap );
 
     return rOStm;
 }

vcl/source/gdi/svmconverter.cxx
@@ -1586,7 +1586,7 @@ sal_uLong SVMConverter::ImplWriteActions( SvStream& rOStm, GDIMetaFile& rMtf,
                     {
                         rOStm.WriteInt16( GDI_LINECAP_ACTION );
                         rOStm.WriteInt32( 6 );
-                        rOStm.WriteInt16( rInfo.GetLineCap() );
+                        rOStm.WriteInt16( (sal_Int16)rInfo.GetLineCap() );
                     }
                 }
 
@@ -1742,7 +1742,7 @@ sal_uLong SVMConverter::ImplWriteActions( SvStream& rOStm, GDIMetaFile& rMtf,
                     {
                         rOStm.WriteInt16( GDI_LINECAP_ACTION );
                         rOStm.WriteInt32( 6 );
-                        rOStm.WriteInt16( rInfo.GetLineCap() );
+                        rOStm.WriteInt16( (sal_Int16)rInfo.GetLineCap() );
                     }
                 }
 

vcl/unx/gtk/a11y/atktextattributes.cxx
@@ -387,7 +387,7 @@ Adjust2Justification(const uno::Any& rAny)
 {
     const gchar * value = nullptr;
 
-    switch( rAny.get<short>() )
+    switch( (style::ParagraphAdjust)rAny.get<short>() )
     {
         case style::ParagraphAdjust_LEFT:
             value = "left";
@@ -419,7 +419,7 @@ Adjust2Justification(const uno::Any& rAny)
 static bool
 Justification2Adjust( uno::Any& rAny, const gchar * value )
 {
-    short nParagraphAdjust;
+    style::ParagraphAdjust nParagraphAdjust;
 
     if( strncmp( value, STRNCMP_PARAM( "left" ) ) == 0 )
         nParagraphAdjust = style::ParagraphAdjust_LEFT;
@@ -432,7 +432,7 @@ Justification2Adjust( uno::Any& rAny, const gchar * value )
     else
         return false;
 
-    rAny <<= nParagraphAdjust;
+    rAny <<= (short)nParagraphAdjust;
     return true;
 }
 

winaccessibility/source/UAccCOM/MAccessible.cxx
@@ -2838,7 +2838,6 @@ void CMAccessible::get_OLECHARFromAny(Any& pAny, OLECHAR* pChar)
     case TypeClass_CONSTANT:
     case TypeClass_CONSTANTS:
     case TypeClass_SINGLETON:
-    case TypeClass_MAKE_FIXED_SIZE:
         break;
     default:
         break;
@@ -3017,7 +3016,7 @@ void CMAccessible::ConvertAnyToVariant(const css::uno::Any &rAnyVal, VARIANT *pv
         case TypeClass_CONSTANT:
         case TypeClass_CONSTANTS:
         case TypeClass_SINGLETON:
-        case TypeClass_MAKE_FIXED_SIZE:
+        case TypeClass::TypeClass_MAKE_FIXED_SIZE:
             // Output the type string, if there is other uno value type.
             pvData->vt = VT_BSTR;
             pvData->bstrVal = SysAllocString(rAnyVal.getValueTypeName().getStr());

writerfilter/source/dmapper/DomainMapper.cxx
@@ -2726,7 +2726,7 @@ void DomainMapper::sprmWithProps( Sprm& rSprm, const PropertyMapPtr& rContext )
         {
             rContext->Insert(PROP_RUBY_TEXT, uno::makeAny(aInfo.sRubyText));
             rContext->Insert(PROP_RUBY_STYLE, uno::makeAny(aInfo.sRubyStyle));
-            rContext->Insert(PROP_RUBY_ADJUST, uno::makeAny<sal_Int16>(ConversionHelper::convertRubyAlign(aInfo.nRubyAlign)));
+            rContext->Insert(PROP_RUBY_ADJUST, uno::makeAny((sal_Int16)ConversionHelper::convertRubyAlign(aInfo.nRubyAlign)));
             m_pImpl->SetRubySprmId(0);
         }
     }

commit 8a007f4f34093ceccb3eec663b7b3d3866592515

tdf#107029 xmlscript: fix enum conversion
Regression from commit 0fe6f1a196b70f0ba4c948389b2ef9b1e77187b8 (use
actual UNO enums in vcl..xmlsecurity, 2017-04-03), it's important when
changing a sal_Int16 to an enum, then arguments of makeAny() calls of
that variable are explicitly casted the previous type to keep
compatibility.

Change-Id: Ib55eeded34033ce536078919c6fad4d571951cd3

xmlscript/source/xmldlg_imexp/xmldlg_import.cxx
@@ -1092,7 +1092,7 @@ bool ImportContext::importButtonTypeProperty(
             throw xml::sax::SAXException( "invalid button-type value!", Reference< XInterface >(), Any() );
         }
 
-        _xControlModel->setPropertyValue( rPropName, makeAny( nButtonType ) );
+        _xControlModel->setPropertyValue( rPropName, makeAny( (sal_Int16)nButtonType ) );
         return true;
     }
     return false;