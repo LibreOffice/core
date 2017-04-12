5b6b0bb47aae0dfafb09f4ccf794a921e3717055
Stephan Bergmann <sbergman@redhat.com>
2017-04-12 14:12:07 +0200

    Remove support for cross-building .msi on Linux
    ...which is no longer needed after 8646ab97dc37c0606b19057686bf3d610f9c15ee
    "Remove MinGW support".  This effectively removes the commits
    8251cd1936af5047c817adf88333fef31031c506 "Call uuidgen without -n when cross-
    compiling", e8ddf693e69ea768e4cb1bd4c0445990149af07d "Cross-compiling-msi-
    related changes; not finished", 60865562c89f2d9a5d157f809e401d725dee9a86 "We
    have to add the path to solver for the msi* tools when cross-compiling", and
    61b1c24615445d7677dbfe4a702d3dd97eaa4939 "More full paths for cross msi* tools"
    (while 8429bd67715a33751f4cfd50cb4be0346d78ee65 "Make the relativisation of the
    path working even on Linux" from amidst them is probably "harmless").
    
    Change-Id: I0b9be32babdf6db83e2093eafd556c875910d92b
    Reviewed-on: https://gerrit.libreoffice.org/36471
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

--- solenv/bin/modules/installer/control.pm
+++ solenv/bin/modules/installer/control.pm
@@ -70,12 +70,6 @@ sub check_system_path
     # All platforms: zip
     # Windows only: "msiinfo.exe", "msidb.exe", "uuidgen.exe", "makecab.exe", "msitran.exe", "expand.exe" for msi database and packaging
 
-    if ($ENV{'CROSS_COMPILING'} eq 'TRUE')
-    {
-        # we build our own msi* etc. tools when cross-compiling
-        $ENV{'PATH'} .= $installer::globals::pathseparator . $ENV{'WORKDIR_FOR_BUILD'} . '/LinkTarget/Executable';
-    }
-
     my $onefile;
     my $error = 0;
     my $pathvariable = $ENV{'PATH'};
@@ -99,10 +93,6 @@ sub check_system_path
     if (($installer::globals::iswin) && ($installer::globals::iswindowsbuild))
     {
         @needed_files_in_path = ("zip.exe", "msiinfo.exe", "msidb.exe", "uuidgen", "makecab.exe", "msitran.exe", "expand.exe");
-    }
-    elsif ($installer::globals::isunix && $installer::globals::packageformat eq 'msi')
-    {
-        @needed_files_in_path = ("zip", "msiinfo.exe", "msidb.exe", "uuidgen", "makecab.exe", "msitran.exe", "cabextract");
     }
     elsif ($installer::globals::iswin)
     {
@@ -167,10 +157,6 @@ sub get_makecab_version
     my $makecabversion = -1;
 
     my $systemcall = "makecab.exe |";
-    if ( $installer::globals::isunix )
-    {
-        $systemcall = "$ENV{'WORKDIR_FOR_BUILD'}/LinkTarget/Executable/makecab.exe |";
-    }
     my @makecaboutput = ();
 
     open (CAB, $systemcall);
