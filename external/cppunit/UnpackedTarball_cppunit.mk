5ca60e76d48dd9a59a519392cc3aba5fd92b4b4d
Markus Mohrhard <markus.mohrhard@googlemail.com>
2017-04-13 05:21:11 +0200

    update cppunit to 1.14.0
    Change-Id: I95fa42f4ef0580734b605df859c1660b29adb8b2

--- external/cppunit/UnpackedTarball_cppunit.mk
+++ external/cppunit/UnpackedTarball_cppunit.mk
@@ -12,13 +12,10 @@ $(eval $(call gb_UnpackedTarball_UnpackedTarball,cppunit))
 $(eval $(call gb_UnpackedTarball_set_tarball,cppunit,$(CPPUNIT_TARBALL),,cppunit))
 
 $(eval $(call gb_UnpackedTarball_add_patches,cppunit,\
-	external/cppunit/coverity.patch \
 	external/cppunit/windows.patch \
 	external/cppunit/unix.patch \
-	external/cppunit/wundef.patch \
 	external/cppunit/CPPUNIT_PLUGIN_EXPORT.patch.0 \
 	external/cppunit/enable-win32-debug.patch \
-	external/cppunit/0001-mark-the-fails-as-no-return.patch.1 \
 	external/cppunit/rtti.patch.0 \
 ))
 ifeq ($(DISABLE_DYNLOADING),TRUE)
