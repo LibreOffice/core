9357c1dab44feb042bd27d9a3f51787b02683186
Chris Sherlock <chris.sherlock79@gmail.com>
2017-04-14 08:42:15 +1000

    tools: svstream.hxx needs only errcode.hxx, not errinf.hxx
    Change-Id: I5dfa117925c6e96a4514372b16e4d0292b3cc6cd

--- sd/source/filter/xml/sdxmlwrp.cxx
+++ sd/source/filter/xml/sdxmlwrp.cxx
@@ -17,6 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
+#include <tools/errinf.hxx>
 #include <rtl/strbuf.hxx>
 #include <com/sun/star/container/XChild.hpp>
 #include <com/sun/star/beans/XPropertySetInfo.hpp>
