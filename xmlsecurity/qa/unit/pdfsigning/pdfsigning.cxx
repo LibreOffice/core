3a002748474083e57703fa3def0080b6712d536c
Miklos Vajna <vmiklos@collabora.co.uk>
2017-04-13 18:09:32 +0200

    vcl PDF import: don't assume larger offset -> newer trailer
    Usually when the PDF file contains incremental updates the updates are
    appended at the end of the document. But this is not required, the
    various trailers can be in any order. Make sure that we look at the last
    trailer (logically last, not the one with the largest file offset) when
    looking for pages.
    
    Change-Id: Idcd85a7c6bbf08c9436dd73933d79cdb683f482c

--- xmlsecurity/qa/unit/pdfsigning/pdfsigning.cxx
+++ xmlsecurity/qa/unit/pdfsigning/pdfsigning.cxx
@@ -395,6 +395,7 @@ void PDFSigningTest::testTokenize()
         "noeol.pdf",
         // File that's intentionally smaller than 1024 bytes.
         "small.pdf",
+        "tdf107149.pdf",
     };
 
     for (const auto& rName : aNames)
@@ -403,6 +404,10 @@ void PDFSigningTest::testTokenize()
         vcl::filter::PDFDocument aDocument;
         // Just make sure the tokenizer finishes without an error, don't look at the signature.
         CPPUNIT_ASSERT(aDocument.Read(aStream));
+
+        if (rName == "tdf107149.pdf")
+            // This failed, page list was empty.
+            CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(1), aDocument.GetPages().size());
     }
 }
 
