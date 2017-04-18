96f34e6f938137dd91b0f4d086f9e8e8802f1575
Jochen Nitschke <j.nitschke+logerrit@ok.de>
2017-04-18 15:23:56 +0200

    extend unit test for INetMIME::scanContentType
    This reverts parts of commit 631b67952909a73ba1851417bd2edbe02ad7be5a
    and commit abc6071b7a8af354a56c91e4caecd8afc79f55cc.
    
    some of the removed fields are usefull,
    m_bConverted should be checked by callers
    
    fixed 2 bugs and added test cases:
    * extended attributes with more than 2 sections were not parsed
    * extended attributes with more than 1 section were not parsed
      if there was an other attribute
    
    Change-Id: I61ab2af7c5151ef1bcd80cc159fa2b99559374a8

--- tools/qa/cppunit/test_inetmime.cxx
+++ tools/qa/cppunit/test_inetmime.cxx
@@ -35,11 +35,13 @@ namespace
     public:
         void test_decodeHeaderFieldBody();
 
-        void test_scanContentType();
+        void test_scanContentType_basic();
+        void test_scanContentType_rfc2231();
 
         CPPUNIT_TEST_SUITE(Test);
         CPPUNIT_TEST(test_decodeHeaderFieldBody);
-        CPPUNIT_TEST(test_scanContentType);
+        CPPUNIT_TEST(test_scanContentType_basic);
+        CPPUNIT_TEST(test_scanContentType_rfc2231);
         CPPUNIT_TEST_SUITE_END();
     };
 
@@ -56,26 +58,108 @@ namespace
         CPPUNIT_ASSERT(testDecode("=?iso-8859-1?B?QUJD?=", "ABC"));
     }
 
-    void Test::test_scanContentType()
+    void Test::test_scanContentType_basic()
     {
         {
             OUString input
-                = "TEST/subTST; parm1*0*=US-ASCII'En'5%25%20; Parm1*1*=of%2010";
+                = "TEST/subTST; parm1=Value1; Parm2=\"unpacked value; %20\"";
+            // non catching scan
+            auto end = INetMIME::scanContentType(input.getStr(), input.getStr()+input.getLength());
+            CPPUNIT_ASSERT_EQUAL(input.getStr()+input.getLength(), end);
+            // catching scan
             OUString type;
             OUString subType;
             INetContentTypeParameterList parameters;
-            auto end = INetMIME::scanContentType(
-                input.getStr(), input.getStr() + input.getLength(), &type,
-                &subType, &parameters);
-            CPPUNIT_ASSERT(end != nullptr);
-            CPPUNIT_ASSERT_EQUAL(OUString(), OUString(end));
+            end = INetMIME::scanContentType(input.getStr(), input.getStr() + input.getLength(),
+                                            &type, &subType, &parameters);
+            CPPUNIT_ASSERT_EQUAL(input.getStr()+input.getLength(), end);
+            CPPUNIT_ASSERT_EQUAL(OUString("test"), type);
+            CPPUNIT_ASSERT_EQUAL(OUString("subtst"), subType);
+            CPPUNIT_ASSERT_EQUAL(
+                INetContentTypeParameterList::size_type(2), parameters.size());
+            auto i = parameters.find("parm1");
+            CPPUNIT_ASSERT(i != parameters.end());
+            CPPUNIT_ASSERT_EQUAL(OString(), i->second.m_sCharset);
+            CPPUNIT_ASSERT_EQUAL(OString(), i->second.m_sLanguage);
+            CPPUNIT_ASSERT_EQUAL(OUString("Value1"), i->second.m_sValue);
+            CPPUNIT_ASSERT(i->second.m_bConverted);
+            i = parameters.find("parm2");
+            CPPUNIT_ASSERT(i != parameters.end());
+            CPPUNIT_ASSERT_EQUAL(OString(), i->second.m_sCharset);
+            CPPUNIT_ASSERT_EQUAL(OString(), i->second.m_sLanguage);
+            CPPUNIT_ASSERT_EQUAL(OUString("unpacked value; %20"), i->second.m_sValue);
+            CPPUNIT_ASSERT(i->second.m_bConverted);
+        }
+    }
+
+    void Test::test_scanContentType_rfc2231()
+    {
+        {
+            // extended parameter, value split in 3 sections
+            OUString input
+                = "TEST/subTST; "
+                  "parm1*0*=US-ASCII'En'5%25%20; "
+                  "Parm1*1*=of%2010;\t"
+                  "parm1*2*=%20%3d%200.5";
+            // non catching scan
+            auto end = INetMIME::scanContentType(input.getStr(), input.getStr()+input.getLength());
+            CPPUNIT_ASSERT_EQUAL(input.getStr()+input.getLength(), end);
+            // catching scan
+            OUString type;
+            OUString subType;
+            INetContentTypeParameterList parameters;
+            end = INetMIME::scanContentType(input.getStr(), input.getStr() + input.getLength(),
+                                            &type, &subType, &parameters);
+            CPPUNIT_ASSERT_EQUAL(input.getStr()+input.getLength(), end);
             CPPUNIT_ASSERT_EQUAL(OUString("test"), type);
             CPPUNIT_ASSERT_EQUAL(OUString("subtst"), subType);
             CPPUNIT_ASSERT_EQUAL(
                 INetContentTypeParameterList::size_type(1), parameters.size());
             auto i = parameters.find("parm1");
             CPPUNIT_ASSERT(i != parameters.end());
-            CPPUNIT_ASSERT_EQUAL(OUString("5% of 10"), i->second.m_sValue);
+            CPPUNIT_ASSERT_EQUAL(OString("us-ascii"), i->second.m_sCharset);
+            CPPUNIT_ASSERT_EQUAL(OString("en"), i->second.m_sLanguage);
+            CPPUNIT_ASSERT_EQUAL(OUString("5% of 10 = 0.5"), i->second.m_sValue);
+            CPPUNIT_ASSERT(i->second.m_bConverted);
+
+            // extended parameters with different value charsets
+            input = "TEST/subTST;"
+                    "parm1*0*=us-ascii'en'value;PARM1*1*=1;"
+                    "parm2*0*=WINDOWS-1250'en-GB'value2%20%80;"
+                    "parm3*0*=UNKNOWN'EN'value3";
+            // non catching scan
+            end = INetMIME::scanContentType(input.getStr(), input.getStr()+input.getLength());
+            CPPUNIT_ASSERT_EQUAL(input.getStr()+input.getLength(), end);
+            // catching scan
+            end = INetMIME::scanContentType(input.getStr(), input.getStr() + input.getLength(),
+                                            &type, &subType, &parameters);
+            CPPUNIT_ASSERT_EQUAL(input.getStr()+input.getLength(), end);
+            CPPUNIT_ASSERT_EQUAL(OUString("test"), type);
+            CPPUNIT_ASSERT_EQUAL(OUString("subtst"), subType);
+            CPPUNIT_ASSERT_EQUAL(
+                INetContentTypeParameterList::size_type(3), parameters.size());
+            i = parameters.find("parm1");
+            CPPUNIT_ASSERT(i != parameters.end());
+            CPPUNIT_ASSERT_EQUAL(OString("us-ascii"), i->second.m_sCharset);
+            CPPUNIT_ASSERT_EQUAL(OString("en"), i->second.m_sLanguage);
+            CPPUNIT_ASSERT_EQUAL(OUString("value1"), i->second.m_sValue);
+            CPPUNIT_ASSERT(i->second.m_bConverted);
+            i = parameters.find("parm2");
+            CPPUNIT_ASSERT(i != parameters.end());
+            CPPUNIT_ASSERT_EQUAL(OString("windows-1250"), i->second.m_sCharset);
+            CPPUNIT_ASSERT_EQUAL(OString("en-gb"), i->second.m_sLanguage);
+/** FIXME
+            // euro sign, windows-1250 x80 is converted to unicode u20AC,
+            OUString value2("value2 \u20AC", std::strlen("value2 \u20AC"), RTL_TEXTENCODING_UNICODE);
+            CPPUNIT_ASSERT_EQUAL(value2, i->second.m_sValue);
+**/
+            CPPUNIT_ASSERT(i->second.m_bConverted);
+            i = parameters.find("parm3");
+            CPPUNIT_ASSERT(i != parameters.end());
+            CPPUNIT_ASSERT_EQUAL(OString("unknown"), i->second.m_sCharset);
+            CPPUNIT_ASSERT_EQUAL(OString("en"), i->second.m_sLanguage);
+            // convert fails
+            CPPUNIT_ASSERT(!i->second.m_bConverted);
         }
     }
 
