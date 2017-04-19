c2ac37c64182a06f80e378b84b46efd5559623af
Michael Stahl <mstahl@redhat.com>
2017-04-19 16:47:56 +0200

    redland: add raptor patch to fix some bugs
    Change-Id: I4e3db61fdfe0b984d36b4f67f8e891da48f79ddb
    (cherry picked from commit 8af6e5e0c4191b12d830b20f72ec3552eea15747)

--- external/redland/UnpackedTarball_raptor.mk
+++ external/redland/UnpackedTarball_raptor.mk
@@ -26,6 +26,7 @@ $(eval $(call gb_UnpackedTarball_add_patches,raptor,\
 	external/redland/raptor/ubsan.patch \
 	$(if $(SYSTEM_LIBXML),,external/redland/raptor/rpath.patch) \
 	external/redland/raptor/xml2-config.patch \
+	external/redland/raptor/0001-Calcualte-max-nspace-declarations-correctly-for-XML-.patch.1 \
 ))
 
 # vim: set noet sw=4 ts=4:
