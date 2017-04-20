d6ca887de38b3989ec1759f69ebcb390af5b6c1d
Eike Rathke <erack@redhat.com>
2017-04-20 22:06:23 +0200

    add ICU changeset-39671 fix for CVE-2017-7867 CVE-2017-7868
    http://bugs.icu-project.org/trac/changeset/39671
    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=213
    https://bugzilla.redhat.com/show_bug.cgi?id=1444101
    
    Reviewed-on: https://gerrit.libreoffice.org/36754
    Reviewed-by: Eike Rathke <erack@redhat.com>
    Tested-by: Jenkins <ci@libreoffice.org>
    (cherry picked from commit c7de8233d15ed0c90fef6c49a54d60cf10119f58)
    
    Backported to older MSVC using the UGLY_SIZEOF_MAPTOUCHARS macro instead
    of sizeof(UTF8Buf::mapToUChars).
    
    Reviewed-on: https://gerrit.libreoffice.org/36776
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Michael Stahl <mstahl@redhat.com>
    (cherry picked from commit 91f5d002884cae1a60768e9caa9d182f41fb7be6)
    (cherry picked from commit 3cdac6bb2defce45342dff04400c7a37bb8a2453)
    
    Change-Id: I4e776ad4fe63c77057b0c823f8672a2b6703346f

--- external/icu/UnpackedTarball_icu.mk
+++ external/icu/UnpackedTarball_icu.mk
@@ -29,6 +29,7 @@ $(eval $(call gb_UnpackedTarball_add_patches,icu,\
 	external/icu/icu.changeset_36727.patch.1 \
 	external/icu/icu.changeset_36801.patch.1 \
 	$(if $(filter-out ANDROID,$(OS)),external/icu/icu4c-icudata-stdlibs.diff) \
+	external/icu/icu4c-changeset-39671.patch.1 \
 ))
 
 # vim: set noet sw=4 ts=4:
