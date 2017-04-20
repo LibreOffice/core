c9deb223767647b6a317a76f1ea676e7f514b723
Eike Rathke <erack@redhat.com>
2017-04-20 22:06:23 +0200

    add ICU changeset-39671 fix for CVE-2017-7867 CVE-2017-7868
    http://bugs.icu-project.org/trac/changeset/39671
    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=213
    https://bugzilla.redhat.com/show_bug.cgi?id=1444101
    
    Reviewed-on: https://gerrit.libreoffice.org/36754
    Reviewed-by: Eike Rathke <erack@redhat.com>
    Tested-by: Jenkins <ci@libreoffice.org>
    (cherry picked from commit c7de8233d15ed0c90fef6c49a54d60cf10119f58)
    
    Backported to older MSVC using the UGLY_SIZEOF_MAPTOUCHARS macro instead
    of sizeof(UTF8Buf::mapToUChars).
    
    Change-Id: I4e776ad4fe63c77057b0c823f8672a2b6703346f
    Reviewed-on: https://gerrit.libreoffice.org/36775
    Reviewed-by: Michael Stahl <mstahl@redhat.com>
    Reviewed-by: Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>
    Tested-by: Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>

--- external/icu/UnpackedTarball_icu.mk
+++ external/icu/UnpackedTarball_icu.mk
@@ -27,6 +27,7 @@ $(eval $(call gb_UnpackedTarball_add_patches,icu,\
 	$(if $(filter-out ANDROID,$(OS)),external/icu/icu4c-icudata-stdlibs.diff) \
 	$(if $(filter EMSCRIPTEN,$(OS)),external/icu/icu4c-emscripten.patch.1) \
 	external/icu/khmerbreakengine.patch \
+	external/icu/icu4c-changeset-39671.patch.1 \
 ))
 
 $(eval $(call gb_UnpackedTarball_add_file,icu,source/data/brkitr/khmerdict.dict,external/icu/khmerdict.dict))
