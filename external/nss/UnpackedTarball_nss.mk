d501f2b013db9225df3d7d7bd8eb27c43790a2bf
Michael Stahl <mstahl@redhat.com>
2017-04-21 14:41:53 +0200

    nss: try to remove nss.windowbuild.patch.0
    It's not obvious to me what problem this solves.  Maybe the problem no
    longer exsists in the new upstream release?
    
    Change-Id: I16762e0024536afb821eac70c6ba3929d5b8927a
    Reviewed-on: https://gerrit.libreoffice.org/36798
    Reviewed-by: Michael Stahl <mstahl@redhat.com>
    Tested-by: Michael Stahl <mstahl@redhat.com>
    (cherry picked from commit 489414f2395b6c8008ebcda7c37a150fd4ed7ad5)

--- external/nss/UnpackedTarball_nss.mk
+++ external/nss/UnpackedTarball_nss.mk
@@ -25,7 +25,6 @@ $(eval $(call gb_UnpackedTarball_add_patches,nss,\
 		external/nss/nss.mingw.patch.3) \
     external/nss/ubsan.patch.0 \
     external/nss/clang-cl.patch.0 \
-    external/nss/nss.windowbuild.patch.0 \
     $(if $(filter IOS,$(OS)), \
         external/nss/nss-chromium-nss-static.patch \
         external/nss/nss-more-static.patch \
