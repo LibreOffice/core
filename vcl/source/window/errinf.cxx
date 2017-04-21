1442cd37d31706ae89356adcf943a4152e72f3e9
Chris Sherlock <chris.sherlock79@gmail.com>
2017-04-21 21:02:04 +1000

    tdf#39468 - vcl: Translate German in errinf.cxx
    Change-Id: I53a0bccc7e040d9e5347d18630f1190c195c7b61

--- vcl/source/window/errinf.cxx
+++ vcl/source/window/errinf.cxx
@@ -285,7 +285,7 @@ ErrorHandlerFlags ErrorHandler::HandleError_Impl(
             {
                 OStringBuffer aStr("Action: ");
                 aStr.append(OUStringToOString(aAction, RTL_TEXTENCODING_ASCII_US));
-                aStr.append("\nFehler: ");
+                aStr.append("\nError: ");
                 aStr.append(OUStringToOString(aErr, RTL_TEXTENCODING_ASCII_US));
                 OSL_FAIL(aStr.getStr());
             }
@@ -308,7 +308,7 @@ ErrorHandlerFlags ErrorHandler::HandleError_Impl(
         }
     }
     OSL_FAIL("Error not handled");
-    // Error 1 is General Error in the Sfx
+    // Error 1 is classified as a General Error in sfx
     if(pInfo->GetErrorCode()!=1)
     {
         HandleError_Impl(1, ErrorHandlerFlags::MAX, bJustCreateString, rError);
