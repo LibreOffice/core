fabad007c60958f2ff87e8f636ff6a798ad1f963
Eike Rathke <erack@redhat.com>
2017-04-22 01:24:19 +0200

    Upgrade to ICU 59.1
    Also regenerated all patches using make icu.genpatch (hence the .1
    suffix that indicates the path level) as some hunks did not apply anyway
    and all now have the correct offset. Using genpatch may have the future
    benefit to yield smaller diffs between different versions of patches.
    Also prefixed all patch names with icu4c- for a cleaner listing.
    New patches introduced are prefixed with icu4c-59-...
    
    Change-Id: Ia83754b0823839887fce1a1d4ed04f8375b113c2
    Reviewed-on: https://gerrit.libreoffice.org/36809
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Eike Rathke <erack@redhat.com>

--- external/libvisio/ExternalProject_libvisio.mk
+++ external/libvisio/ExternalProject_libvisio.mk
@@ -35,7 +35,7 @@ $(call gb_ExternalProject_get_state_target,libvisio,build) :
 			$(if $(ENABLE_DEBUG),--enable-debug,--disable-debug) \
 			--disable-werror \
 			$(if $(verbose),--disable-silent-rules,--enable-silent-rules) \
-			CXXFLAGS="$(CXXFLAGS) $(BOOST_CPPFLAGS)" \
+			CXXFLAGS="$(CXXFLAGS) $(ICU_UCHAR_TYPE) $(BOOST_CPPFLAGS)" \
 			$(if $(CROSS_COMPILING),--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM)) \
 		&& $(MAKE) \
 	)
