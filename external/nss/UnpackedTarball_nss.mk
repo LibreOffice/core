be451f7e8f2ecc799211337eda50323cbda822e5
Michael Stahl <mstahl@redhat.com>
2017-04-21 14:41:53 +0200

    nss: try to remove nss.windowbuild.patch.0
    It's not obvious to me what problem this solves.  Maybe the problem no
    longer exsists in the new upstream release?
    
    Change-Id: I16762e0024536afb821eac70c6ba3929d5b8927a
    Reviewed-on: https://gerrit.libreoffice.org/36798
    Reviewed-by: Michael Stahl <mstahl@redhat.com>
    Tested-by: Michael Stahl <mstahl@redhat.com>

--- external/nss/UnpackedTarball_nss.mk
+++ external/nss/UnpackedTarball_nss.mk
@@ -16,7 +16,6 @@ $(eval $(call gb_UnpackedTarball_add_patches,nss,\
 	external/nss/nss.aix.patch \
 	external/nss/nss-3.13.5-zlib-werror.patch \
 	$(if $(filter WNTMSC,$(OS)$(COM)),external/nss/nss.windows.patch) \
-    external/nss/nss.windowbuild.patch.0 \
 	$(if $(filter MSC-INTEL,$(COM)-$(CPUNAME)), \
 		external/nss/nss.cygwin64.in32bit.patch) \
     $(if $(findstring 120_70,$(VCVER)_$(WINDOWS_SDK_VERSION)), \
