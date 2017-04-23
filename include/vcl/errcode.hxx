89d1361a8de337d83ea75ad3d101f1e501d0bea9
Chris Sherlock <chris.sherlock79@gmail.com>
2017-04-24 01:06:41 +1000

    tools: move errcode.hxx to the vcl module
    Change-Id: I17e5a033de5f0aeb0a726744f7b464cdab6e2ee3

--- include/vcl/errcode.hxx
+++ include/vcl/errcode.hxx
@@ -17,8 +17,8 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_TOOLS_ERRCODE_HXX
-#define INCLUDED_TOOLS_ERRCODE_HXX
+#ifndef INCLUDED_VCL_ERRCODE_HXX
+#define INCLUDED_VCL_ERRCODE_HXX
 
 #ifndef __RSC
 #include <tools/solar.h>
