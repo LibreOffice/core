8f51b4272450b6220f563740538f143c0bc70175
Michael Stahl <mstahl@redhat.com>
2017-04-24 18:21:50 +0200

    tdf#107205 vcl: don't always use D2DWriteTextOutRenderer
    ... because it cannot rotate text.
    
    (regression from commit df556aa47da22f96b3fcd356c12419d3035cba3c)
    
    Change-Id: If9bc6e98b8979c5eb02f1a5cfc12b4d19bdc481b
    (cherry picked from commit 2eb91035c2f60e9dc476336474d51b23b666bb04)
    Reviewed-on: https://gerrit.libreoffice.org/36904
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Andras Timar <andras.timar@collabora.com>

--- vcl/inc/win/saldata.hxx
+++ vcl/inc/win/saldata.hxx
@@ -121,7 +121,9 @@ public:
     std::map< UINT,sal_uInt16 > maVKMap;      // map some dynamic VK_* entries
 
     // must be deleted before exit(), so delete it in DeInitSalData()
-    std::unique_ptr<TextOutRenderer> m_pTextOutRenderer;
+    std::unique_ptr<TextOutRenderer> m_pD2DWriteTextOutRenderer;
+    // tdf#107205 need 2 instances because D2DWrite can't rotate text
+    std::unique_ptr<TextOutRenderer> m_pExTextOutRenderer;
 };
 
 inline void SetSalData( SalData* pData ) { ImplGetSVData()->mpSalData = pData; }
