LibreOffice/core

commit a2c467a58ade9f55e0154b2935c747bb283ebd45

use UnoCursorPointer in SwNavigationMgr
Change-Id: I7c7431edd79cf4527f97c7dc0695d49174b61e2c

sw/source/uibase/inc/navmgr.hxx
@@ -15,12 +15,13 @@
 #include "swtypes.hxx"
 #include "calbck.hxx"
 #include "unocrsr.hxx"
+#include "vcl/svapp.hxx"
 
 class   SwWrtShell;
 struct  SwPosition;
 class SwUnoCrsr;
 
-class SwNavigationMgr : SwClient
+class SwNavigationMgr
 {
 private:
     /*
@@ -32,7 +33,7 @@ private:
      * (e.g. click a link, or double click an entry from the navigator).
      * Every use of the back/forward buttons results in moving the stack pointer within the navigation history
      */
-    typedef ::std::vector< std::shared_ptr<SwUnoCrsr> > Stack_t;
+    typedef ::std::vector< sw::UnoCursorPointer > Stack_t;
     Stack_t m_entries;
     Stack_t::size_type m_nCurrent; /* Current position within the navigation history */
     SwWrtShell & m_rMyShell; /* The active shell within which the navigation occurs */
@@ -44,11 +45,8 @@ public:
     SwNavigationMgr( SwWrtShell & rShell );
     virtual ~SwNavigationMgr()
     {
-        for(auto pEntry : m_entries)
-        {
-            if(pEntry && GetRegisteredIn() == pEntry.get())
-                pEntry->Remove(this);
-        }
+        SolarMutexGuard g;
+        m_entries.clear();
     }
     /* Can we go back in the history ? */
     bool backEnabled() ;
@@ -60,7 +58,6 @@ public:
     void goForward() ;
     /* The method that adds the position pPos to the navigation history */
     bool addEntry(const SwPosition& rPos);
-    void SwClientNotify(const SwModify& rModify, const SfxHint& rHint) SAL_OVERRIDE;
 };
 #endif
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sw/source/uibase/wrtsh/navmgr.cxx
@@ -162,17 +162,15 @@ bool SwNavigationMgr::addEntry(const SwPosition& rPos) {
 
         if (*m_entries.back()->GetPoint() != rPos)
         {
-            std::shared_ptr<SwUnoCrsr> pCursor(m_rMyShell.GetDoc()->CreateUnoCrsr(rPos));
+            sw::UnoCursorPointer pCursor(m_rMyShell.GetDoc()->CreateUnoCrsr(rPos));
             m_entries.push_back(pCursor);
-            pCursor->Add(this);
         }
         bRet = true;
     }
     else {
         if ( (!m_entries.empty() && *m_entries.back()->GetPoint() != rPos) || m_entries.empty() ) {
-            auto pCursor(m_rMyShell.GetDoc()->CreateUnoCrsr(rPos));
+            sw::UnoCursorPointer pCursor(m_rMyShell.GetDoc()->CreateUnoCrsr(rPos));
             m_entries.push_back(pCursor);
-            pCursor->Add(this);
             bRet = true;
         }
         if (m_entries.size() > 1 && *m_entries.back()->GetPoint() == rPos)
@@ -214,16 +212,6 @@ bool SwNavigationMgr::addEntry(const SwPosition& rPos) {
     }
 
     return bRet;
-}
-
-void SwNavigationMgr::SwClientNotify(const SwModify& rModify, const SfxHint& rHint)
-{
-    if(typeid(rHint) == typeid(sw::DocDisposingHint))
-    {
-        m_entries.clear();
-    }
-    else
-        SwClient::SwClientNotify(rModify, rHint);
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sw/inc/unocrsr.hxx
@@ -115,6 +115,13 @@ namespace sw
             {
                 m_pCursor->Add(this);
             }
+            UnoCursorPointer(const UnoCursorPointer& pOther)
+                : SwClient(nullptr)
+                , m_pCursor(pOther.m_pCursor)
+            {
+                if(m_pCursor)
+                    m_pCursor->Add(this);
+            }
             virtual ~UnoCursorPointer() SAL_OVERRIDE
             {
                 if(m_pCursor)
@@ -132,6 +139,13 @@ namespace sw
                 { return *m_pCursor.get(); }
             SwUnoCrsr* operator->() const
                 { return m_pCursor.get(); }
+            UnoCursorPointer& operator=(UnoCursorPointer aOther)
+            {
+                if(aOther.m_pCursor)
+                    aOther.m_pCursor->Add(this);
+                m_pCursor = aOther.m_pCursor;
+                return *this;
+            }
             explicit operator bool() const
                 { return static_cast<bool>(m_pCursor); }
             void reset(std::shared_ptr<SwUnoCrsr> pNew)

commit 8cd1ae22eb2d464516717d767bd3e5f6c2f4bd34

tdf#107427 sw: fix crash with stale entries in SwNavigationMgr
When deleting a header, the sw::UnoCursorPointer of SwNavigationMgr
spontaneously self-destructs, but SwNavigationMgr expects its cursors
to always be alive, so add another SfxListener to remove dying cursors.

(probably regression from a2c467a58ade9f55e0154b2935c747bb283ebd45)

Change-Id: I1055ea3cfc47114dc36002198f1ddffea87d2d85

sw/source/uibase/inc/navmgr.hxx
@@ -21,7 +21,7 @@ class   SwWrtShell;
 struct  SwPosition;
 class SwUnoCursor;
 
-class SwNavigationMgr final
+class SwNavigationMgr final : public SfxListener
 {
 private:
     /*
@@ -43,11 +43,7 @@ private:
 public:
     /* Constructor that initializes the shell to the current shell */
     SwNavigationMgr( SwWrtShell & rShell );
-    ~SwNavigationMgr()
-    {
-        SolarMutexGuard g;
-        m_entries.clear();
-    }
+    ~SwNavigationMgr();
     /* Can we go back in the history ? */
     bool backEnabled() ;
     /* Can we go forward in the history ? */
@@ -58,6 +54,9 @@ public:
     void goForward() ;
     /* The method that adds the position pPos to the navigation history */
     bool addEntry(const SwPosition& rPos);
+    /* to get notified if our cursors self-destruct */
+    virtual void Notify(SfxBroadcaster& rBC, const SfxHint& rHint) override;
 };
 #endif
+
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sw/source/uibase/wrtsh/navmgr.cxx
@@ -46,6 +46,34 @@ SwNavigationMgr::SwNavigationMgr(SwWrtShell & rShell)
 {
 }
 
+SwNavigationMgr::~SwNavigationMgr()
+{
+    SolarMutexGuard g;
+    for (auto & it : m_entries)
+    {
+        EndListening(it.get()->m_aNotifier);
+    }
+    m_entries.clear();
+}
+
+void SwNavigationMgr::Notify(SfxBroadcaster& rBC, const SfxHint& rHint)
+{
+    // our cursors may now spontaneously self-destruct: remove from
+    // m_entries if that happens
+    if (typeid(rHint) == typeid(sw::UnoCursorHint))
+    {
+        for (auto it = m_entries.begin(); it != m_entries.end(); ++it)
+        {
+            if (!it->get() || & rBC == & it->get()->m_aNotifier)
+            {
+                EndListening(rBC);
+                m_entries.erase(it);
+                break;
+            }
+        }
+    }
+}
+
 // This method is used by the navigation shell - defined in sw/source/uibase/inc/navsh.hxx
 // and implemented in sw/source/uibase/shells/navsh.cxx
 // It is called when we want to check if the back button should be enabled or not.
@@ -163,6 +191,7 @@ bool SwNavigationMgr::addEntry(const SwPosition& rPos) {
         if (*m_entries.back()->GetPoint() != rPos)
         {
             sw::UnoCursorPointer pCursor(m_rMyShell.GetDoc()->CreateUnoCursor(rPos));
+            StartListening(pCursor->m_aNotifier);
             m_entries.push_back(pCursor);
         }
         bRet = true;
@@ -170,6 +199,7 @@ bool SwNavigationMgr::addEntry(const SwPosition& rPos) {
     else {
         if ( (!m_entries.empty() && *m_entries.back()->GetPoint() != rPos) || m_entries.empty() ) {
             sw::UnoCursorPointer pCursor(m_rMyShell.GetDoc()->CreateUnoCursor(rPos));
+            StartListening(pCursor->m_aNotifier);
             m_entries.push_back(pCursor);
             bRet = true;
         }