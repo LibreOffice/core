167e46926839ae51f3d33d17f2111bb487fd765c
Caolán McNamara <caolanm@redhat.com>
2017-04-28 15:25:51 +0100

    split these out into a standalone header
    cut down on the chance they get mangled by a non utf-8 editor
    
    Change-Id: I672885c26883275ba57fbfac49a6c761ec00bef1

---
+++ scaddins/source/analysis/deffuncname.hxx
@@ -0,0 +1,631 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#ifndef INCLUDED_SCADDINS_SOURCE_ANALYSIS_DEFFUNCTIONNAME_HXX
+#define INCLUDED_SCADDINS_SOURCE_ANALYSIS_DEFFUNCTIONNAME_HXX
+
+static const char* ANALYSIS_DEFFUNCNAME_Workday[2] =
+{
+    u8"ARBEITSTAG",
+    u8"WORKDAY"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Yearfrac[2] =
+{
+    u8"BRTEILJAHRE",
+    u8"YEARFRAC"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Edate[2] =
+{
+    u8"EDATUM",
+    u8"EDATE"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Weeknum[2] =
+{
+    u8"KALENDERWOCHE",
+    u8"WEEKNUM"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Eomonth[2] =
+{
+    u8"MONATSENDE",
+    u8"EOMONTH"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Networkdays[2] =
+{
+    u8"NETTOARBEITSTAGE",
+    u8"NETWORKDAYS"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Amordegrc[2] =
+{
+    u8"AMORDEGRK",
+    u8"AMORDEGRC"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Amorlinc[2] =
+{
+    u8"AMORLINEARK",
+    u8"AMORLINC"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Accrint[2] =
+{
+    u8"AUFGELZINS",
+    u8"ACCRINT"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Accrintm[2] =
+{
+    u8"AUFGELZINSF",
+    u8"ACCRINTM"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Received[2] =
+{
+    u8"AUSZAHLUNG",
+    u8"RECEIVED"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Disc[2] =
+{
+    u8"DISAGIO",
+    u8"DISC"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Duration[2] =
+{
+    u8"DURATION",
+    u8"DURATION"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Effect[2] =
+{
+    u8"EFFEKTIV",
+    u8"EFFECT"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Cumprinc[2] =
+{
+    u8"KUMKAPITAL",
+    u8"CUMPRINC"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Cumipmt[2] =
+{
+    u8"KUMZINSZ",
+    u8"CUMIPMT"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Price[2] =
+{
+    u8"KURS",
+    u8"PRICE"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Pricedisc[2] =
+{
+    u8"KURSDISAGIO",
+    u8"PRICEDISC"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Pricemat[2] =
+{
+    u8"KURSFÄLLIG",
+    u8"PRICEMAT"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Mduration[2] =
+{
+    u8"MDURATION",
+    u8"MDURATION"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Nominal[2] =
+{
+    u8"NOMINAL",
+    u8"NOMINAL"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Dollarfr[2] =
+{
+    u8"NOTIERUNGBRU",
+    u8"DOLLARFR"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Dollarde[2] =
+{
+    u8"NOTIERUNGDEZ",
+    u8"DOLLARDE"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Yield[2] =
+{
+    u8"RENDITE",
+    u8"YIELD"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Yielddisc[2] =
+{
+    u8"RENDITEDIS",
+    u8"YIELDDISC"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Yieldmat[2] =
+{
+    u8"RENDITEFÄLL",
+    u8"YIELDMAT"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Tbilleq[2] =
+{
+    u8"TBILLÄQUIV",
+    u8"TBILLEQ"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Tbillprice[2] =
+{
+    u8"TBILLKURS",
+    u8"TBILLPRICE"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Tbillyield[2] =
+{
+    u8"TBILLRENDITE",
+    u8"TBILLYIELD"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Oddfprice[2] =
+{
+    u8"UNREGER.KURS",
+    u8"ODDFPRICE"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Oddfyield[2] =
+{
+    u8"UNREGER.REND",
+    u8"ODDFYIELD"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Oddlprice[2] =
+{
+    u8"UNREGLE.KURS",
+    u8"ODDLPRICE"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Oddlyield[2] =
+{
+    u8"UNREGLE.REND",
+    u8"ODDLYIELD"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Xirr[2] =
+{
+    u8"XINTZINSFUSS",
+    u8"XIRR"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Xnpv[2] =
+{
+    u8"XKAPITALWERT",
+    u8"XNPV"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Intrate[2] =
+{
+    u8"ZINSSATZ",
+    u8"INTRATE"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Coupncd[2] =
+{
+    u8"ZINSTERMNZ",
+    u8"COUPNCD"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Coupdays[2] =
+{
+    u8"ZINSTERMTAGE",
+    u8"COUPDAYS"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Coupdaysnc[2] =
+{
+    u8"ZINSTERMTAGNZ",
+    u8"COUPDAYSNC"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Coupdaybs[2] =
+{
+    u8"ZINSTERMTAGVA",
+    u8"COUPDAYBS"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Couppcd[2] =
+{
+    u8"ZINSTERMVZ",
+    u8"COUPPCD"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Coupnum[2] =
+{
+    u8"ZINSTERMZAHL",
+    u8"COUPNUM"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Fvschedule[2] =
+{
+    u8"ZW2",
+    u8"FVSCHEDULE"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Iseven[2] =
+{
+    u8"ISTGERADE",
+    u8"ISEVEN"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Isodd[2] =
+{
+    u8"ISTUNGERADE",
+    u8"ISODD"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Gcd[2] =
+{
+    u8"GGT",
+    u8"GCD"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Lcm[2] =
+{
+    u8"KGV",
+    u8"LCM"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Multinomial[2] =
+{
+    u8"POLYNOMIAL",
+    u8"MULTINOMIAL"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Seriessum[2] =
+{
+    u8"POTENZREIHE",
+    u8"SERIESSUM"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Quotient[2] =
+{
+    u8"QUOTIENT",
+    u8"QUOTIENT"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Mround[2] =
+{
+    u8"VRUNDEN",
+    u8"MROUND"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Sqrtpi[2] =
+{
+    u8"WURZELPI",
+    u8"SQRTPI"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Randbetween[2] =
+{
+    u8"ZUFALLSBEREICH",
+    u8"RANDBETWEEN"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Besseli[2] =
+{
+    u8"BESSELI",
+    u8"BESSELI"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Besselj[2] =
+{
+    u8"BESSELJ",
+    u8"BESSELJ"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Besselk[2] =
+{
+    u8"BESSELK",
+    u8"BESSELK"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Bessely[2] =
+{
+    u8"BESSELY",
+    u8"BESSELY"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Bin2Dec[2] =
+{
+    u8"BININDEZ",
+    u8"BIN2DEC"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Bin2Hex[2] =
+{
+    u8"BININHEX",
+    u8"BIN2HEX"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Bin2Oct[2] =
+{
+    u8"BININOKT",
+    u8"BIN2OCT"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Delta[2] =
+{
+    u8"DELTA",
+    u8"DELTA"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Dec2Bin[2] =
+{
+    u8"DEZINBIN",
+    u8"DEC2BIN"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Dec2Hex[2] =
+{
+    u8"DEZINHEX",
+    u8"DEC2HEX"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Dec2Oct[2] =
+{
+    u8"DEZINOKT",
+    u8"DEC2OCT"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Erf[2] =
+{
+    u8"GAUSSFEHLER",
+    u8"ERF"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Erfc[2] =
+{
+    u8"GAUSSFKOMPL",
+    u8"ERFC"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Gestep[2] =
+{
+    u8"GGANZZAHL",
+    u8"GESTEP"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Hex2Bin[2] =
+{
+    u8"HEXINBIN",
+    u8"HEX2BIN"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Hex2Dec[2] =
+{
+    u8"HEXINDEZ",
+    u8"HEX2DEC"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Hex2Oct[2] =
+{
+    u8"HEXINOKT",
+    u8"HEX2OCT"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Imabs[2] =
+{
+    u8"IMABS",
+    u8"IMABS"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Imaginary[2] =
+{
+    u8"IMAGINÄRTEIL",
+    u8"IMAGINARY"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Impower[2] =
+{
+    u8"IMAPOTENZ",
+    u8"IMPOWER"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Imargument[2] =
+{
+    u8"IMARGUMENT",
+    u8"IMARGUMENT"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Imcos[2] =
+{
+    u8"IMCOS",
+    u8"IMCOS"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Imdiv[2] =
+{
+    u8"IMDIV",
+    u8"IMDIV"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Imexp[2] =
+{
+    u8"IMEXP",
+    u8"IMEXP"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Imconjugate[2] =
+{
+    u8"IMKONJUGIERTE",
+    u8"IMCONJUGATE"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Imln[2] =
+{
+    u8"IMLN",
+    u8"IMLN"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Imlog10[2] =
+{
+    u8"IMLOG10",
+    u8"IMLOG10"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Imlog2[2] =
+{
+    u8"IMLOG2",
+    u8"IMLOG2"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Improduct[2] =
+{
+    u8"IMPRODUKT",
+    u8"IMPRODUCT"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Imreal[2] =
+{
+    u8"IMREALTEIL",
+    u8"IMREAL"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Imsin[2] =
+{
+    u8"IMSIN",
+    u8"IMSIN"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Imsub[2] =
+{
+    u8"IMSUB",
+    u8"IMSUB"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Imsum[2] =
+{
+    u8"IMSUMME",
+    u8"IMSUM"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Imsqrt[2] =
+{
+    u8"IMWURZEL",
+    u8"IMSQRT"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Imtan[2] =
+{
+    u8"IMTAN",
+    u8"IMTAN"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Imsec[2] =
+{
+    u8"IMSEC",
+    u8"IMSEC"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Imcsc[2] =
+{
+    u8"IMCSC",
+    u8"IMCSC"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Imcot[2] =
+{
+    u8"IMCOT",
+    u8"IMCOT"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Imsinh[2] =
+{
+    u8"IMSINH",
+    u8"IMSINH"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Imcosh[2] =
+{
+    u8"IMCOSH",
+    u8"IMCOSH"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Imsech[2] =
+{
+    u8"IMSECH",
+    u8"IMSECH"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Imcsch[2] =
+{
+    u8"IMCSCH",
+    u8"IMCSCH"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Complex[2] =
+{
+    u8"KOMPLEXE",
+    u8"COMPLEX"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Oct2Bin[2] =
+{
+    u8"OKTINBIN",
+    u8"OCT2BIN"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Oct2Dec[2] =
+{
+    u8"OKTINDEZ",
+    u8"OCT2DEC"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Oct2Hex[2] =
+{
+    u8"OKTINHEX",
+    u8"OCT2HEX"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Convert[2] =
+{
+    u8"UMWANDELN",
+    u8"CONVERT"
+};
+
+static const char* ANALYSIS_DEFFUNCNAME_Factdouble[2] =
+{
+    u8"ZWEIFAKULTÄT",
+    u8"FACTDOUBLE"
+};
+
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
