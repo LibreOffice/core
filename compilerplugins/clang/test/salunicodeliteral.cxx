5a4d03131ffe557fed9ab3c8e31a005188c7ea5c
Stephan Bergmann <sbergman@redhat.com>
2017-04-28 17:59:50 +0200

    loplugin:salunicodeliteral
    For the c-char in the u'...' literal, the preceding commits consistently use:
    
    * a simple-escape-sequence if the original code already used one
    * \0 for U+0000
    * the (\ escaped, for ' and \) source character matching U+0020..7E (even if it
      is not a basic source character)
    * a consistently four-digit hexadecimal-escape-sequence otherwise, \xNNNN
    
    For non-surrogate code points, the last case could probably also use \uNNNN
    universal-character-names.  However, for one, it isn't quite clear to me whether
    conversion of such to members of the execution chacacter set in character
    literals (in translation phase 5) is implementation-specific.  And for another,
    the current C++ standard references the dated (no pun intended) ISO/IEC
    10646-1:1993 specification, rather than the current ISO/IEC 10646:2014, and
    requires that a universal-characrer-name designate a character with a specific
    "character short name in ISO/IEC 10646", but I do not find a specification of a
    "short name" in ISO/IEC 10646:2014 and don't have access to ISO/IEC
    10646-1:1993, so am not sure whether that would e.g. cover noncharacters like
    U+FFFF.
    
    (The only exception is one occurrence of u'\x6C' in bestFitOpenSymbolToMSFont,
    filter/source/msfilter/util.cxx, where it is clear from the context that the
    value denotes neither a Unicode code point nor a UTF-16 code unit, but rather an
    index into the Wingdings font glyph table.)
    
    Change-Id: If36b94168428ba1e05977c370aceaa7e90131e90

---
+++ compilerplugins/clang/test/salunicodeliteral.cxx
@@ -0,0 +1,39 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "sal/config.h"
+
+#include "sal/types.h"
+
+#include "salunicodeliteral.hxx"
+
+#define TEST1 'x'
+#define TEST2 sal_Unicode('x')
+
+namespace {
+
+void f(sal_Unicode) {}
+
+}
+
+void test() {
+    f(sal_Unicode('x')); // expected-error {{in LIBO_INTERNAL_ONLY code, replace literal cast to 'sal_Unicode' (aka 'char16_t') with a u'...' char16_t character literal [loplugin:salunicodeliteral]}}
+    f(static_cast<sal_Unicode>('x')); // expected-error {{in LIBO_INTERNAL_ONLY code, replace literal cast to 'sal_Unicode' (aka 'char16_t') with a u'...' char16_t character literal [loplugin:salunicodeliteral]}}
+    f(static_cast<sal_Unicode const>('x')); // expected-error {{in LIBO_INTERNAL_ONLY code, replace literal cast to 'const sal_Unicode' (aka 'const char16_t') with a u'...' char16_t character literal [loplugin:salunicodeliteral]}}
+    f((sal_Unicode) 'x'); // expected-error {{in LIBO_INTERNAL_ONLY code, replace literal cast to 'sal_Unicode' (aka 'char16_t') with a u'...' char16_t character literal [loplugin:salunicodeliteral]}}
+    f(sal_Unicode(('x'))); // expected-error {{in LIBO_INTERNAL_ONLY code, replace literal cast to 'sal_Unicode' (aka 'char16_t') with a u'...' char16_t character literal [loplugin:salunicodeliteral]}}
+    f(sal_Unicode(120)); // expected-error {{in LIBO_INTERNAL_ONLY code, replace literal cast to 'sal_Unicode' (aka 'char16_t') with a u'...' char16_t character literal [loplugin:salunicodeliteral]}}
+    f(sal_Unicode(TEST1));
+    f(TEST2); // expected-error {{in LIBO_INTERNAL_ONLY code, replace literal cast to 'sal_Unicode' (aka 'char16_t') with a u'...' char16_t character literal [loplugin:salunicodeliteral]}}
+    char c = 'x';
+    f(sal_Unicode(c));
+    f(char16_t('x'));
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
