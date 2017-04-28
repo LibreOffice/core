efdbb9791a75a5b2e5de34855bec8407e8c8f53a
Jochen Nitschke <j.nitschke+logerrit@ok.de>
2017-04-28 16:34:00 +0200

    replace 'if ( ) SAL_INFO' with 'SAL_INFO_IF'
    Change-Id: If45b57a3dc6d7ed088fde8835e14abe4bf7378f8
    Reviewed-on: https://gerrit.libreoffice.org/37554
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Eike Rathke <erack@redhat.com>

--- sc/source/core/tool/token.cxx
+++ sc/source/core/tool/token.cxx
@@ -380,11 +380,7 @@ bool ScRawToken::IsValidReference() const
 
 FormulaToken* ScRawToken::CreateToken() const
 {
-#if OSL_DEBUG_LEVEL > 0
-#define IF_NOT_OPCODE_ERROR(o,c) if (eOp!=o) SAL_INFO( "sc.core", #c "::ctor: OpCode " << (int)eOp << " lost, converted to " #o "; maybe inherit from FormulaToken instead!")
-#else
-#define IF_NOT_OPCODE_ERROR(o,c)
-#endif
+#define IF_NOT_OPCODE_ERROR(o,c) SAL_INFO_IF((eOp!=o), "sc.core", #c "::ctor: OpCode " << (int)eOp << " lost, converted to " #o "; maybe inherit from FormulaToken instead!")
     switch ( GetType() )
     {
         case svByte :
