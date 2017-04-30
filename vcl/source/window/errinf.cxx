LibreOffice/core

commit 46a153cbcac1a3e2881b83b5537037321c34797c

vcl: update error codes to defined constants for code clarity
Change-Id: I6e86c45b71fc15d6645501128e7d86f8e179e652
Reviewed-on: https://gerrit.libreoffice.org/36851
Reviewed-by: Chris Sherlock <chris.sherlock79@gmail.com>
Tested-by: Chris Sherlock <chris.sherlock79@gmail.com>

vcl/source/window/errinf.cxx
@@ -259,7 +259,7 @@ bool ErrorHandler::GetErrorString(sal_uInt32 nErrCodeId, OUString& rErrStr)
 */
 DialogMask ErrorHandler::HandleError(sal_uInt32 nErrCodeId, DialogMask nFlags)
 {
-    if(!nErrCodeId || nErrCodeId == ERRCODE_ABORT)
+    if(nErrCodeId != ERRCODE_NONE || nErrCodeId == ERRCODE_ABORT)
         return DialogMask::NONE;
 
     ErrorRegistry &rData = TheErrorRegistry::get();
@@ -327,11 +327,11 @@ DialogMask ErrorHandler::HandleError(sal_uInt32 nErrCodeId, DialogMask nFlags)
     }
 
     OSL_FAIL("Error not handled");
-    // Error 1 is classified as a General Error in sfx
-    if (pInfo->GetErrorCode()!=1)
-        HandleError(1);
+    // Error 1 (ERRCODE_ABORT) is classified as a General Error in sfx
+    if (pInfo->GetErrorCode() != ERRCODE_ABORT)
+        HandleError(ERRCODE_ABORT);
     else
-        OSL_FAIL("Error 1 not handled");
+        OSL_FAIL("ERRCODE_ABORT not handled");
 
     delete pInfo;
     return DialogMask::NONE;

commit 820dff991b119b2cafb5efe5f1a4668351705457

coverity#1405737 (golden) Logically dead code
regression since

commit 46a153cbcac1a3e2881b83b5537037321c34797c
Author: Chris Sherlock <chris.sherlock79@gmail.com>
Date:   Sun Apr 23 18:53:58 2017 +1000

    vcl: update error codes to defined constants for code clarity

    Change-Id: I6e86c45b71fc15d6645501128e7d86f8e179e652
    Reviewed-on: https://gerrit.libreoffice.org/36851
    Reviewed-by: Chris Sherlock <chris.sherlock79@gmail.com>
    Tested-by: Chris Sherlock <chris.sherlock79@gmail.com>

Change-Id: Id1a3f26a84c7641b0b6af71dc842a683d0c1dd0c

vcl/source/window/errinf.cxx
@@ -114,7 +114,7 @@ bool ErrorHandler::GetErrorString(sal_uInt32 nErrCodeId, OUString& rErrStr)
 
 DialogMask ErrorHandler::HandleError(sal_uInt32 nErrCodeId, DialogMask nFlags)
 {
-    if(nErrCodeId != ERRCODE_NONE || nErrCodeId == ERRCODE_ABORT)
+    if (nErrCodeId == ERRCODE_NONE || nErrCodeId == ERRCODE_ABORT)
         return DialogMask::NONE;
 
     ErrorRegistry &rData = TheErrorRegistry::get();