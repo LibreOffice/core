85a162ba600cdee429cc91133504042b46f5236a
Jens Carl <j.carl43@gmx.de>
2017-05-11 05:30:19 +0000

    tdf#39468: Translate German comment/term in sw/source/filter/ww8/wrtww8gr.cxx
    Change-Id: I5760af990e62cff357cdd2b80db0b129a96a857e
    Reviewed-on: https://gerrit.libreoffice.org/37491
    Reviewed-by: Michael Stahl <mstahl@redhat.com>
    Tested-by: Michael Stahl <mstahl@redhat.com>

--- sw/source/filter/ww8/wrtww8gr.cxx
+++ sw/source/filter/ww8/wrtww8gr.cxx
@@ -73,10 +73,10 @@ using namespace ::com::sun::star;
 // TODO:
 // 5. convert the MapModes that Widows can't handle
 
-// OutGrf () is called for every GrafNode in the document. Es wird ein PicLocFc-Sprm
-// eingefuegt, der statt Adresse ein Magic sal_uLong enthaelt. Ausserdem wird
-// in der Graf-Klasse der GrfNode-Ptr gemerkt ( fuers spaetere Ausgeben der
-// Grafiken und Patchen der PicLocFc-Attribute )
+// OutGrf () is called for every GrfNode in the document. A PicLocFc-Sprm
+// will be inserted, which contains a magic number instead of an address.
+// The GrfNode-Ptr is saved in Graf-Class (used later for output of
+// the graphic and patching of the PicLocFc attributes)
 
 void WW8Export::OutputGrfNode( const SwGrfNode& /*rNode*/ )
 {
