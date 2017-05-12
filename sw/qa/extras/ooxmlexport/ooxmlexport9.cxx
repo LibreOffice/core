b3179359e1126882717bae6e4597c65e34fa606d
Vasily Melenchuk <Vasily.Melenchuk@cib.de>
2017-05-12 16:14:51 +0300

    tdf#100072 extra test for DOCX shape import with zero height
    Corresponding bug is already fixed in tdf#107104. However created
    tests do care only for width, but not for height, like we have in
    this testcase.
    
    Change-Id: I6f24e66e3c5e9289e158e0ca043ef01a3ea42880

--- sw/qa/extras/ooxmlexport/ooxmlexport9.cxx
+++ sw/qa/extras/ooxmlexport/ooxmlexport9.cxx
@@ -459,12 +459,22 @@ DECLARE_OOXMLEXPORT_TEST(testTdf106492, "tdf106492.docx")
 
 DECLARE_OOXMLEXPORT_TEST(testTdf107104, "tdf107104.docx")
 {
-    CPPUNIT_ASSERT(getShape(1)->getSize().Width > 0);
-    // This failed: the second arrow was invisible because it had zero width.
-    CPPUNIT_ASSERT(getShape(2)->getSize().Width > 0);
+    // Ensure that shape has non-zero height
+    CPPUNIT_ASSERT(getShape(2)->getSize().Height > 0);
+
+    // Ensure that shape left corner is within page (positive
+    CPPUNIT_ASSERT(getShape(2)->getPosition().X > 0);
 }
 
 DECLARE_OOXMLEXPORT_TEST(testTdf107033, "tdf107033.docx")
+{
+    uno::Reference<beans::XPropertySet> xPageStyle(getStyles("PageStyles")->getByName("Standard"), uno::UNO_QUERY);
+    // This was 0: footnote separator was disabled even in case the document
+    // had no footnotes.
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(25), getProperty<sal_Int32>(xPageStyle, "FootnoteLineRelativeWidth"));
+}
+
+DECLARE_OOXMLEXPORT_TEST(testTdf100072, "tdf100072.docx")
 {
     uno::Reference<beans::XPropertySet> xPageStyle(getStyles("PageStyles")->getByName("Standard"), uno::UNO_QUERY);
     // This was 0: footnote separator was disabled even in case the document
