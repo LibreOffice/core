54f1d6247f8e6f1adcb94ca180e72383851f622c
Yeliz Taneroğlu <yeliztaneroglu@gmail.com>
2017-05-12 19:35:08 +0300

    tdf#105204 fix shellcheck warnings in solenv/bin/mkonedoc.sh
    SC2001: See if you can use ${variable//search/replace} instead.
    SC2116 Useless echo? Instead of 'cmd $(echo foo)', just use 'cmd foo'.
    SC2006: Use $(STATEMENT) instead of legacy `STATEMENT`
    SC2086: Double quote to prevent globbing and word splitting.
    
    Change-Id: Ia9978927138d9cc49d636be98271ffe8854e9e9e

--- solenv/bin/mkonedoc.sh
+++ solenv/bin/mkonedoc.sh
@@ -15,7 +15,7 @@ if [ -d "include/$module" ]; then
     module_include="$module_include include/$module"
 fi
 
-DOXYGEN_INPUT=`printf "%s" "$module/source $module_include"`
+DOXYGEN_INPUT=$(printf "%s" "$module/source $module_include")
 DOXYGEN_OUTPUT="$BASE_OUTPUT/$module"
 DOXYGEN_VERSION="master"
 DOXYGEN_PROJECTNAME="LibreOffice Module $module"
@@ -24,7 +24,7 @@ if [ "$mode" = "tag" ] ; then
     DOXYGEN_REF_TAGFILES=
 else
     DOXYGEN_OUR_TAGFILE="$DOXYGEN_OUTPUT/$module.tags2"
-    DOXYGEN_REF_TAGFILES="$(echo $DOXYGEN_REF_TAGFILES | sed -e "s@$DOXYGEN_OUTPUT/$module.tags@@")"
+    DOXYGEN_REF_TAGFILES="${DOXYGEN_REF_TAGFILES//s@DOXYGEN_OUTPUT/module.tags@@})"
 fi
 
 # export variables referenced in doxygen config file
@@ -38,8 +38,8 @@ export DOXYGEN_PROJECTNAME
 
 
 echo "Calling doxygen mode $mode for $module"
-echo "using tagfiles : $DOXYGEN_REF_TAGFILES" >> $BASE_OUTPUT/$module.log 2>&1
-doxygen "$SRCDIR/solenv/inc/doxygen_$mode.cfg" >> $BASE_OUTPUT/$module.log 2>&1
+echo "using tagfiles : $DOXYGEN_REF_TAGFILES" >> "$BASE_OUTPUT"/"$module".log 2>&1
+doxygen "$SRCDIR/solenv/inc/doxygen_$mode.cfg" >> "$BASE_OUTPUT"/"$module".log 2>&1
 echo "$(date "+%F %T") doxygen $module.$mode rc:$?"
 
 
