a31d35c30ce328a0ce975cea36843f728f78def6
Noel Grandin <noel.grandin@collabora.co.uk>
2017-05-12 12:35:16 +0200

    catch out of range values in strong_int constructor
    Change-Id: Ibcbb873fda6cb82ad8f575673705ba6cb16217e6

--- include/o3tl/strong_int.hxx
+++ include/o3tl/strong_int.hxx
@@ -21,6 +21,8 @@
 #define INCLUDED_O3TL_STRONG_INT_HXX
 
 #include <sal/config.h>
+#include <limits>
+#include <cassert>
 
 namespace o3tl
 {
@@ -40,7 +42,15 @@ template <typename UNDERLYING_TYPE, typename PHANTOM_TYPE>
 struct strong_int
 {
 public:
-    explicit constexpr strong_int(UNDERLYING_TYPE value) : m_value(value) {}
+    explicit constexpr strong_int(long value) : m_value(value)
+    {
+#if HAVE_CXX14_CONSTEXPR
+        // catch attempts to pass in out-of-range values early
+        assert(value >= std::numeric_limits<UNDERLYING_TYPE>::min()
+               && value <= std::numeric_limits<UNDERLYING_TYPE>::max()
+               && "out of range");
+#endif
+    }
     strong_int() : m_value(0) {}
 
     explicit constexpr operator UNDERLYING_TYPE() const { return m_value; }
