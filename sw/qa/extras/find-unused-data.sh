e20f6464cc4047876ce5e6d47510e8038c513234
Yeliz Taneroğlu <yeliztaneroglu@gmail.com>
2017-05-12 18:34:12 +0300

    tdf#105204: ShellCheck nitpicks for find-unused-data.sh
    Double quote to prevent globbing and word splitting.
    
    Change-Id: I09faac27ca5c63a85b9b8cbd4f09821587bf4759
    Reviewed-on: https://gerrit.libreoffice.org/37545
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Chris Sherlock <chris.sherlock79@gmail.com>

--- sw/qa/extras/find-unused-data.sh
+++ sw/qa/extras/find-unused-data.sh
@@ -2,8 +2,8 @@
 
 for i in */data/*
 do
-    file=$(basename $i)
-    if ! git grep -q $file; then
+    file=$(basename "$i")
+    if ! git grep -q "$file"; then
         echo "WARNING: $i is not used, write a testcase for it!"
     fi
 done
