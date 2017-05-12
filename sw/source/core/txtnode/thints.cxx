9dd2529d94ea6ec9217c1f1be807e11bb7aafee2
Michael Stahl <mstahl@redhat.com>
2017-05-12 18:34:17 +0200

    sw: table-in-footnote: delete SwFootnoteFrame if it becomes empty
    Crashed in a11y code with a SwFootnoteFrame that survived a JoinNode and
    subsequent deletion of its reference-containing SwTextFrame and thus had
    a stale "pRef" member; presumably the SwTableFrame needs to delete an
    empty footnote frame like the SwTextFrame does from SwContentFrame::Cut(),
    called from DelFrames(), called from CutImpl().
    
    Change-Id: I5a30357ecd3bf474bfc4a5451de89beb245fb0ae
    (cherry picked from commit c9fb347642729017ad0c613fe26310befd021db8)

--- sw/source/core/txtnode/thints.cxx
+++ sw/source/core/txtnode/thints.cxx
@@ -1398,6 +1398,10 @@ bool SwTextNode::InsertHint( SwTextAttr * const pAttr, const SetAttrMode nMode )
                         SwContentNode* pCNd = rNodes[ nSttIdx ]->GetContentNode();
                         if( nullptr != pCNd )
                             pCNd->DelFrames();
+                        else if (SwTableNode *const pTable = rNodes[nSttIdx]->GetTableNode())
+                        {
+                            pTable->DelFrames();
+                        }
                     }
                 }
 
