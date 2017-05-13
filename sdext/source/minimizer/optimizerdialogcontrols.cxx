LibreOffice/core

commit 5d0e485e827057eee9fb2c997685690b710e7f34

use actual UNO enums in reportdesign..svtools
Change-Id: Idcd916382b87f8542dc6b5cdcb5ecb01d6947203
Reviewed-on: https://gerrit.libreoffice.org/36043
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sdext/source/minimizer/optimizerdialogcontrols.cxx
@@ -86,7 +86,7 @@ OUString InsertSeparator( OptimizerDialog& rOptimizerDialog, const OUString& rCo
 
 
 OUString InsertButton( OptimizerDialog& rOptimizerDialog, const OUString& rControlName, Reference< XActionListener >& xActionListener,
-    sal_Int32 nXPos, sal_Int32 nYPos, sal_Int32 nWidth, sal_Int32 nHeight, sal_Int16 nTabIndex, bool bEnabled, PPPOptimizerTokenEnum nResID, sal_Int16 nPushButtonType )
+    sal_Int32 nXPos, sal_Int32 nYPos, sal_Int32 nWidth, sal_Int32 nHeight, sal_Int16 nTabIndex, bool bEnabled, PPPOptimizerTokenEnum nResID, css::awt::PushButtonType nPushButtonType )
 {
     OUString pNames[] = {
         OUString("Enabled"),

reportdesign/source/core/api/ReportComponent.cxx
@@ -29,7 +29,6 @@
 #include "ReportControlModel.hxx"
 #include <com/sun/star/reflection/XProxyFactory.hpp>
 #include <com/sun/star/text/ParagraphVertAlign.hpp>
-#include <com/sun/star/style/ParagraphAdjust.hpp>
 #include <com/sun/star/i18n/ScriptType.hpp>
 #include <editeng/unolingu.hxx>
 #include <unotools/syslocale.hxx>

reportdesign/source/core/inc/ReportControlModel.hxx
@@ -27,6 +27,7 @@
 #include <com/sun/star/container/XContainer.hpp>
 #include <com/sun/star/lang/IndexOutOfBoundsException.hpp>
 #include <com/sun/star/lang/Locale.hpp>
+#include <com/sun/star/style/ParagraphAdjust.hpp>
 #include <comphelper/uno3.hxx>
 #include <comphelper/interfacecontainer2.hxx>
 
@@ -35,7 +36,7 @@ namespace reportdesign
 {
     struct OFormatProperties
     {
-        ::sal_Int16                                         nAlign;
+        css::style::ParagraphAdjust                         nAlign;
         css::awt::FontDescriptor                            aFontDescriptor;
         css::awt::FontDescriptor                            aAsianFontDescriptor;
         css::awt::FontDescriptor                            aComplexFontDescriptor;

reportdesign/source/core/inc/ReportHelperImpl.hxx
@@ -226,12 +226,12 @@ void SAL_CALL clazz::setCharScaleWidth(::sal_Int16 the_value) \
 sal_Int16 SAL_CALL clazz::getParaAdjust() \
 { \
     ::osl::MutexGuard aGuard(m_aMutex); \
-    return varName.nAlign; \
+    return (sal_Int16)varName.nAlign; \
 } \
  \
 void SAL_CALL clazz::setParaAdjust( sal_Int16 _align ) \
 { \
-    set(PROPERTY_PARAADJUST,_align,varName.nAlign); \
+    set(PROPERTY_PARAADJUST,(css::style::ParagraphAdjust)_align,varName.nAlign); \
 } \
  \
 awt::FontDescriptor SAL_CALL clazz::getFontDescriptor() \

reportdesign/source/core/sdr/RptObject.cxx
@@ -187,29 +187,30 @@ namespace
             {
                 sal_Int16 nTextAlign = 0;
                 lhs >>= nTextAlign;
+                style::ParagraphAdjust eAdjust;
                 switch(nTextAlign)
                 {
                     case awt::TextAlign::LEFT:
-                        nTextAlign = style::ParagraphAdjust_LEFT;
+                        eAdjust = style::ParagraphAdjust_LEFT;
                         break;
                     case awt::TextAlign::CENTER:
-                        nTextAlign = style::ParagraphAdjust_CENTER;
+                        eAdjust = style::ParagraphAdjust_CENTER;
                         break;
                     case awt::TextAlign::RIGHT:
-                        nTextAlign = style::ParagraphAdjust_RIGHT;
+                        eAdjust = style::ParagraphAdjust_RIGHT;
                         break;
                     default:
                         OSL_FAIL("Illegal text alignment value!");
                         break;
                 }
-                aRet <<= (style::ParagraphAdjust)nTextAlign;
+                aRet <<= eAdjust;
             }
             else
             {
                 sal_Int16 nTextAlign = 0;
                 sal_Int16 eParagraphAdjust = 0;
                 lhs >>= eParagraphAdjust;
-                switch(eParagraphAdjust)
+                switch((style::ParagraphAdjust)eParagraphAdjust)
                 {
                     case style::ParagraphAdjust_LEFT:
                     case style::ParagraphAdjust_BLOCK:

reportdesign/source/ui/inspection/GeometryHandler.cxx
@@ -1253,12 +1253,12 @@ uno::Any SAL_CALL GeometryHandler::convertToControlValue(const OUString & Proper
                 ModuleRes aRes(RID_STR_VERTICAL_ALIGN_CONST);
                 ResStringArray aResList(aRes);
                 if (sal_uInt32(nParagraphVertAlign) < aResList.Count())
-                    aControlValue <<= aResList.GetString(nParagraphVertAlign);
+                    aControlValue <<= aResList.GetString((sal_uInt32)nParagraphVertAlign);
             }
             break;
         case PROPERTY_ID_PARAADJUST:
             {
-                sal_Int16 nParagraphAdjust = style::ParagraphAdjust_LEFT;
+                sal_Int16 nParagraphAdjust = (sal_Int16)style::ParagraphAdjust_LEFT;
                 aPropertyValue >>= nParagraphAdjust;
                 ModuleRes aRes(RID_STR_PARAADJUST_CONST);
                 ResStringArray aResList(aRes);

reportdesign/source/ui/report/ReportController.cxx
@@ -859,7 +859,7 @@ FeatureState OReportController::GetState(sal_uInt16 _nId) const
                 ::sal_Int16 nParaAdjust = 0;
                 if ( aReturn.aValue >>= nParaAdjust )
                 {
-                    switch(nParaAdjust)
+                    switch((style::ParagraphAdjust)nParaAdjust)
                     {
                         case style::ParagraphAdjust_LEFT:
                             aReturn.bChecked = _nId == SID_ATTR_PARA_ADJUST_LEFT;
@@ -874,6 +874,7 @@ FeatureState OReportController::GetState(sal_uInt16 _nId) const
                         case style::ParagraphAdjust_CENTER:
                             aReturn.bChecked = _nId == SID_ATTR_PARA_ADJUST_CENTER;
                             break;
+                        default: break;
                     }
                 }
                 aReturn.aValue.clear();
@@ -1481,7 +1482,7 @@ void OReportController::Execute(sal_uInt16 _nId, const Sequence< PropertyValue >
         case SID_ATTR_PARA_ADJUST_RIGHT:
         case SID_ATTR_PARA_ADJUST_BLOCK:
             {
-                sal_Int16 eParagraphAdjust = style::ParagraphAdjust_LEFT;
+                style::ParagraphAdjust eParagraphAdjust = style::ParagraphAdjust_LEFT;
                 switch(_nId)
                 {
                     case SID_ATTR_PARA_ADJUST_LEFT:
@@ -1497,7 +1498,7 @@ void OReportController::Execute(sal_uInt16 _nId, const Sequence< PropertyValue >
                         eParagraphAdjust = style::ParagraphAdjust_BLOCK;
                         break;
                 }
-                impl_setPropertyAtControls_throw(RID_STR_UNDO_ALIGNMENT,PROPERTY_PARAADJUST,uno::makeAny(eParagraphAdjust),aArgs);
+                impl_setPropertyAtControls_throw(RID_STR_UNDO_ALIGNMENT,PROPERTY_PARAADJUST,uno::makeAny((sal_Int16)eParagraphAdjust),aArgs);
 
                 InvalidateFeature(SID_ATTR_PARA_ADJUST_LEFT);
                 InvalidateFeature(SID_ATTR_PARA_ADJUST_CENTER);

sdext/source/pdfimport/tree/drawtreevisiting.cxx
@@ -114,7 +114,7 @@ void DrawXmlEmitter::visit( TextElement& elem, const std::list< Element* >::cons
     {
         for(int i=1; i< elem.Text.getLength(); i++)
         {
-            sal_Int16 nType = xCC->getCharacterDirection( str, i );
+            css::i18n::DirectionProperty nType = (css::i18n::DirectionProperty)xCC->getCharacterDirection( str, i );
             if ( nType == css::i18n::DirectionProperty_RIGHT_TO_LEFT           ||
                  nType == css::i18n::DirectionProperty_RIGHT_TO_LEFT_ARABIC    ||
                  nType == css::i18n::DirectionProperty_RIGHT_TO_LEFT_EMBEDDING ||

commit a10376f28cfb70cbbf65efdfed3ff93579504ff1

tdf#107823: missing buttons in Presentation Minimizer
Regression from https://cgit.freedesktop.org/libreoffice/core/commit/?id=5d0e485e827057eee9fb2c997685690b710e7f34

Change-Id: I1864e7f7ac73fc13af82237f4f7d4dbaaa5733f5

sdext/source/minimizer/optimizerdialogcontrols.cxx
@@ -105,7 +105,7 @@ OUString InsertButton( OptimizerDialog& rOptimizerDialog, const OUString& rContr
         Any( rOptimizerDialog.getString( nResID ) ),
         Any( nXPos ),
         Any( nYPos ),
-        Any( nPushButtonType ),
+        Any( static_cast< sal_Int16 >(nPushButtonType) ),
         Any( (sal_Int16)0 ),
         Any( nTabIndex ),
         Any( nWidth ) };