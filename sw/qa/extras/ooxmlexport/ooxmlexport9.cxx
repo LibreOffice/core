c9b3f79a2761584c81ee7d55ddd11b625e5945cc
Vasily Melenchuk <Vasily.Melenchuk@cib.de>
2017-05-15 13:41:14 +0300

    tdf#100072 zero height of shape's path was causing geometry errors
    DOCX custom geometry shape's path width and height are now used
    independently for scaling calculations.
    
    Reviewed-on: https://gerrit.libreoffice.org/37639
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>
    (cherry picked from commit 5477f7274e4df1210298c0f503a54eabc0f06bfc)
    
    plus:
    
    tdf#100072 extra test for DOCX shape import with zero height
    
    Corresponding bug is already fixed in tdf#107104. However created
    tests do care only for width, but not for height, like we have in
    this testcase.
    
    Reviewed-on: https://gerrit.libreoffice.org/37538
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
    (cherry picked from commit cffc5a04661fc0a84dff9fa5da954236d88a8b38)
    
    Change-Id: If5d5499f402379df125b0f31dd071ca51b2553f1

--- sw/qa/extras/ooxmlexport/ooxmlexport9.cxx
+++ sw/qa/extras/ooxmlexport/ooxmlexport9.cxx
@@ -258,6 +258,23 @@ DECLARE_OOXMLEXPORT_TEST(testTdf107033, "tdf107033.docx")
     CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(25), getProperty<sal_Int32>(xPageStyle, "FootnoteLineRelativeWidth"));
 }
 
+DECLARE_OOXMLEXPORT_TEST(testTdf107889, "tdf107889.docx")
+{
+    uno::Reference<drawing::XDrawPageSupplier> xDrawPageSupplier(mxComponent, uno::UNO_QUERY);
+    uno::Reference<drawing::XDrawPage> xDrawPage = xDrawPageSupplier->getDrawPage();
+    // This was 1, multi-page table was imported as a floating one.
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(0), xDrawPage->getCount());
+}
+
+DECLARE_OOXMLEXPORT_TEST(testTdf100072, "tdf100072.docx")
+{
+    // Ensure that shape has non-zero height
+    CPPUNIT_ASSERT(getShape(1)->getSize().Height > 0);
+
+    // Ensure that shape left corner is within page (positive)
+    CPPUNIT_ASSERT(getShape(1)->getPosition().X > 0);
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
