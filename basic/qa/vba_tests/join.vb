7e8a7d992290c3d4f775863cfc255f12485d4fde
Takeshi Abe <tabe@fixedpoint.jp>
2017-05-17 22:13:39 +0900

    basic: Fix broken unit test for Join()
    Change-Id: Ib9768097d73fe664e24da4c3989141e87342a34e

--- basic/qa/vba_tests/join.vb
+++ basic/qa/vba_tests/join.vb
@@ -6,7 +6,7 @@ Dim result As String
 
 Function doUnitTest() As String
 result = verify_testJoin()
-If failCount <> 0 And passCount > 0 Then
+If failCount <> 0 Or passCount = 0 Then
     doUnitTest = result
 Else
     doUnitTest = "OK"
@@ -23,31 +23,29 @@ Function verify_testJoin() As String
     result = "Test Results" & Chr$(10) & "============" & Chr$(10)
 
     Dim testName As String
-    Dim TestDateTime As Date
-    Dim TestStr As String
-    Dim date1, date2
-    Dim vaArray() As String
+    Dim str1, str2 As String
+    Dim vaArray(2) As String
     testName = "Test Join function"
     On Error GoTo errorHandler
     vaArray(0) = "string1"
     vaArray(1) = "string2"
     vaArray(2) = "string3"
 
-    date2 = "string1 string2 string3"
-    date1 = Join(vaArray)
-    TestLog_ASSERT date1 = date2, "the return Join is: " & date1
+    str2 = "string1 string2 string3"
+    str1 = Join(vaArray)
+    TestLog_ASSERT str1 = str2, "the return Join is: " & str1
 
-    date2 = "string1 string2 string3"
-    date1 = Join(vaArray, " ")
-    TestLog_ASSERT date1 = date2, "the return Join is: " & date1
+    str2 = "string1 string2 string3"
+    str1 = Join(vaArray, " ")
+    TestLog_ASSERT str1 = str2, "the return Join is: " & str1
 
-    date2 = "string1<>string2<>string3"
-    date1 = Join(vaArray, "<>")
-    TestLog_ASSERT date1 = date2, "the return Join is: " & date1
+    str2 = "string1<>string2<>string3"
+    str1 = Join(vaArray, "<>")
+    TestLog_ASSERT str1 = str2, "the return Join is: " & str1
 
-    date2 = "string1string2string3"
-    date1 = Join(vaArray, "")
-    TestLog_ASSERT date1 = date2, "the return Join is: " & date1
+    str2 = "string1string2string3"
+    str1 = Join(vaArray, "")
+    TestLog_ASSERT str1 = str2, "the return Join is: " & str1
 
     result = result & Chr$(10) & "Tests passed: " & passCount & Chr$(10) & "Tests failed: " & failCount & Chr$(10)
     verify_testJoin = result
