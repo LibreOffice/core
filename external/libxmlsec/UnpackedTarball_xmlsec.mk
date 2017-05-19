99a3b8d535af51bc05fc586875fc433f75598ff8
Miklos Vajna <vmiklos@collabora.co.uk>
2017-05-19 12:16:54 +0200

    xmlsec: fix macOS 10.8 build
    This is just a workaround to have an easy backport to libreoffice-5-4; I
    guess the real solution is to move to internal libxml2 on macOS.
    
    Change-Id: I84d7db296ef6aeee3a3adcdeaaaf4330977a3825

--- external/libxmlsec/UnpackedTarball_xmlsec.mk
+++ external/libxmlsec/UnpackedTarball_xmlsec.mk
@@ -15,6 +15,7 @@ xmlsec_patches += xmlsec1-1.2.14_fix_extern_c.patch.1
 xmlsec_patches += xmlsec1-customkeymanage.patch.1
 # Backport of <https://github.com/lsh123/xmlsec/pull/112>.
 xmlsec_patches += xmlsec1-mscrypto-fix-signing-regression.patch.1
+xmlsec_patches += xmlsec1-osx.patch.1
 
 $(eval $(call gb_UnpackedTarball_UnpackedTarball,xmlsec))
 
