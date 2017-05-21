95d5375b63d80c4a89ad96954934ff2e8ae213a2
Chris Sherlock <chris.sherlock79@gmail.com>
2017-05-21 16:06:56 +1000

    osl: remove LLA sigs, fix typos and convert to Doxygen comment
    Change-Id: I07311894c25a906d0f3d64f39684851c7b23d672
    Reviewed-on: https://gerrit.libreoffice.org/37872
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
    Tested-by: Noel Grandin <noel.grandin@collabora.co.uk>

--- sal/qa/osl/file/osl_File.cxx
+++ sal/qa/osl/file/osl_File.cxx
@@ -775,8 +775,8 @@ namespace osl_FileBase
 #endif
     }
 
-    /** LLA: Test for getSystemPathFromFileURL()
-        this test is splitted into 2 different OS tests,
+    /** Test for getSystemPathFromFileURL()
+        this test is split into 2 different OS tests,
         the first function checkUNXBehaviour... runs only on Unix based Systems,
         the second only on windows based systems
         the first parameter are a file URL where we want to get the system path of,
@@ -853,7 +853,7 @@ namespace osl_FileBase
     {
 #if 0
         OString sURL("file://c:/tmp");
-        checkUNXBehaviour_getSystemPathFromFileURL(sURL, osl::FileBase::E_None, "c:/tmp"); // LLA: this is may be a BUG
+        checkUNXBehaviour_getSystemPathFromFileURL(sURL, osl::FileBase::E_None, "c:/tmp"); // this is may be a BUG
         checkWNTBehaviour_getSystemPathFromFileURL(sURL, osl::FileBase::E_INVAL, "");
 #endif
     }
@@ -867,7 +867,7 @@ namespace osl_FileBase
 
     void SystemPath_FileURL::getSystemPathFromFileURL_001_53()
     {
-// LLA: is this a legal file path?
+// is this a legal file path?
         OString sURL("file:///c|/tmp");
         checkUNXBehaviour_getSystemPathFromFileURL(sURL, osl::FileBase::E_None, "/c|/tmp");
         checkWNTBehaviour_getSystemPathFromFileURL(sURL, osl::FileBase::E_None, "c:\\tmp");
@@ -890,7 +890,7 @@ namespace osl_FileBase
     void SystemPath_FileURL::getSystemPathFromFileURL_001_7()
     {
         OString sURL("file:///tmp/../second");
-        checkUNXBehaviour_getSystemPathFromFileURL(sURL, osl::FileBase::E_None, "/tmp/../second"); // LLA: may be a BUG
+        checkUNXBehaviour_getSystemPathFromFileURL(sURL, osl::FileBase::E_None, "/tmp/../second"); // may be a BUG
         checkWNTBehaviour_getSystemPathFromFileURL(sURL, osl::FileBase::E_INVAL, "");
     }
 
@@ -1164,7 +1164,7 @@ namespace osl_FileBase
         void getTempDirURL_001()
         {
 
-            CPPUNIT_ASSERT_EQUAL_MESSAGE("test for getTempDirURL function: excution",
+            CPPUNIT_ASSERT_EQUAL_MESSAGE("test for getTempDirURL function: execution",
                                      osl::FileBase::E_None, nError);
         }
 
@@ -1477,11 +1477,12 @@ namespace osl_FileStatus
 
         }
 
-        // Link is not defined in Windows, and on Linux, we can not get the directory item of the link file
-        // LLA: we have to differ to filesystems, normal filesystems support links (EXT2, ...)
-        //      castrated filesystems don't (FAT, FAT32)
-        //      Windows NT NTFS support links, but the windows api doesn't :-(
+        /** Check if is a valid linked file.
 
+            Link is not defined in Windows, and on Linux, we can not get the directory item of the linked file.
+            We have to defer to filesystems, normal filesystems support links (EXT2, ...), castrated filesystems
+            don't have links (FAT, FAT32) and Windows NT NTFS support links, but the Windows API doesn't :-(
+        */
         void isValid_003()
         {
 #if defined (UNX)
@@ -1656,7 +1657,7 @@ namespace osl_FileStatus
 
         void getFileType_007()
         {
-#if defined(__sun) // Special file is differ in Windows
+#if defined(__sun) // Special file is different in Windows
             nError1 = DirectoryItem::get(aTypeURL2, m_aSpecialItem);
             CPPUNIT_ASSERT_EQUAL(nError1, osl::FileBase::E_None);
 
@@ -2095,7 +2096,7 @@ namespace osl_FileStatus
             deleteTestFile(aTypeURL);
         }
 
-#if (defined UNX)            // Link file is not define in Windows
+#if (defined UNX)            // Link file is not defined in Windows
         void getLinkTargetURL_001()
         {
             // create a link file;
