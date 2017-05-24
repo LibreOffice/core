2e5fd5c73d4e2a2a92f1122cacc72bf046be20f0
Ximeng Zu <uznomis@yahoo.com>
2017-05-24 17:02:17 -0500

    Presentation Mode
    Adding fullscreen presentation mode.
    
    Change-Id: Id07416ce204d1d7dd917fbd33a4d9f5072ac1703

---
+++ android/source/src/java/org/libreoffice/PresentationActivity.java
@@ -0,0 +1,22 @@
+package org.libreoffice;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+
+public class PresentationActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.presentation_mode);
+
+        Intent intent = getIntent();
+        String filePath = intent.getStringExtra(LibreOfficeMainActivity.PRESENTATION_CACHE_PATH);
+
+        WebView wv = (WebView) findViewById(R.id.presentation_view);
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.loadUrl("file://"+filePath);
+    }
+}
