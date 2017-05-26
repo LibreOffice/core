LibreOffice/core

commit 4a340e36e8d3713806719351f455711c1254a178

loplugin:constantparam
add the results files so I can just see the diff in future

Change-Id: Ia20a1aa6418be95ed620719cde340c00b7b053e1
Reviewed-on: https://gerrit.libreoffice.org/37988
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

include/sfx2/objsh.hxx
@@ -458,7 +458,7 @@ public:
 
     // Transfer IFace
     bool                        IsAbortingImport() const;
-    void                        FinishedLoading( SfxLoadedFlags nWhich = SfxLoadedFlags::ALL );
+    void                        FinishedLoading();
 
     virtual void                SetFormatSpecificCompatibilityOptions( const OUString& /*rFilterTypeName*/ ) { /* Do not do anything here; Derived classes must overload to do actual work */ };
 

sfx2/source/doc/objmisc.cxx
@@ -1045,7 +1045,7 @@ void SfxObjectShell::InitOwnModel_Impl()
     }
 }
 
-void SfxObjectShell::FinishedLoading( SfxLoadedFlags nFlags )
+void SfxObjectShell::FinishedLoading()
 {
     std::shared_ptr<const SfxFilter> pFlt = pMedium->GetFilter();
     if( pFlt )
@@ -1055,7 +1055,7 @@ void SfxObjectShell::FinishedLoading( SfxLoadedFlags nFlags )
 
     bool bSetModifiedTRUE = false;
     const SfxStringItem* pSalvageItem = SfxItemSet::GetItem<SfxStringItem>(pMedium->GetItemSet(), SID_DOC_SALVAGE, false);
-    if( ( nFlags & SfxLoadedFlags::MAINDOCUMENT ) && !(pImpl->nLoadedFlags & SfxLoadedFlags::MAINDOCUMENT )
+    if( !(pImpl->nLoadedFlags & SfxLoadedFlags::MAINDOCUMENT )
         && !(pImpl->nFlagsInProgress & SfxLoadedFlags::MAINDOCUMENT ))
     {
         pImpl->nFlagsInProgress |= SfxLoadedFlags::MAINDOCUMENT;
@@ -1083,23 +1083,7 @@ void SfxObjectShell::FinishedLoading( SfxLoadedFlags nFlags )
         pImpl->nFlagsInProgress &= ~SfxLoadedFlags::MAINDOCUMENT;
     }
 
-    if( ( nFlags & SfxLoadedFlags::IMAGES ) && !(pImpl->nLoadedFlags & SfxLoadedFlags::IMAGES )
-        && !(pImpl->nFlagsInProgress & SfxLoadedFlags::IMAGES ))
-    {
-        pImpl->nFlagsInProgress |= SfxLoadedFlags::IMAGES;
-        uno::Reference<document::XDocumentProperties> xDocProps(
-            getDocProperties());
-        const OUString url(xDocProps->getAutoloadURL());
-        sal_Int32 delay(xDocProps->getAutoloadSecs());
-        SetAutoLoad( INetURLObject(url), delay * 1000,
-                     (delay > 0) || !url.isEmpty() );
-        if( !bSetModifiedTRUE && IsEnableSetModified() )
-            SetModified( false );
-        Invalidate( SID_SAVEASDOC );
-        pImpl->nFlagsInProgress &= ~SfxLoadedFlags::IMAGES;
-    }
-
-    pImpl->nLoadedFlags |= nFlags;
+    pImpl->nLoadedFlags |= SfxLoadedFlags::MAINDOCUMENT;
 
     if ( pImpl->nFlagsInProgress == SfxLoadedFlags::NONE )
     {

compilerplugins/clang/constantparam.bitmask.results
@@ -1,0 +1,42 @@
+codemaker/source/javamaker/classfile.hxx:153
+    void codemaker::javamaker::ClassFile::ClassFile(enum codemaker::javamaker::ClassFile::AccessFlags,const class rtl::OString &,const class rtl::OString &,const class rtl::OString &)
+    enum codemaker::javamaker::ClassFile::AccessFlags accessFlags setBits=0x1
+codemaker/source/javamaker/classfile.hxx:168
+    void codemaker::javamaker::ClassFile::addField(enum codemaker::javamaker::ClassFile::AccessFlags,const class rtl::OString &,const class rtl::OString &,unsigned short,const class rtl::OString &)
+    enum codemaker::javamaker::ClassFile::AccessFlags accessFlags setBits=0x1
+cui/source/options/webconninfo.hxx:35
+    void svx::PasswordTable::InsertHeaderItem(unsigned short,const class rtl::OUString &,enum HeaderBarItemBits)
+    enum HeaderBarItemBits nBits setBits=0x211 clearBits=0x1ee
+dbaccess/source/filter/xml/xmlExport.hxx:172
+    void dbaxml::ODBExport::ODBExport(const class com::sun::star::uno::Reference<class com::sun::star::uno::XComponentContext> &,const class rtl::OUString &,enum SvXMLExportFlags)
+    enum SvXMLExportFlags nExportFlag setBits=0x400 clearBits=0x200
+include/framework/framelistanalyzer.hxx:181
+    void framework::FrameListAnalyzer::FrameListAnalyzer(const class com::sun::star::uno::Reference<class com::sun::star::frame::XFramesSupplier> &,const class com::sun::star::uno::Reference<class com::sun::star::frame::XFrame> &,enum FrameAnalyzerFlags)
+    enum FrameAnalyzerFlags eDetectMode setBits=0x4
+include/svl/itemset.hxx:134
+    void SfxItemSet::PutExtended(const class SfxItemSet &,enum SfxItemState,enum SfxItemState)
+    enum SfxItemState eDefaultAs setBits=0x20 clearBits=0xf
+include/svtools/brwbox.hxx:441
+    void BrowseBox::InsertDataColumn(unsigned short,const class rtl::OUString &,long,enum HeaderBarItemBits,unsigned short)
+    enum HeaderBarItemBits nBits setBits=0x410 clearBits=0x3ac
+include/svtools/wizardmachine.hxx:173
+    void svt::OWizardMachine::OWizardMachine(class vcl::Window *,enum WizardButtonFlags)
+    enum WizardButtonFlags _nButtonFlags setBits=0xf
+include/vcl/bitmapex.hxx:82
+    void BitmapEx::SetSizePixel(const class Size &,enum BmpScaleFlag)
+    enum BmpScaleFlag nScaleFlag setBits=0x1
+include/vcl/gdimtf.hxx:194
+    _Bool GDIMetaFile::CreateThumbnail(class BitmapEx &,enum BmpConversion,enum BmpScaleFlag) const
+    enum BmpScaleFlag nScaleFlag setBits=0x1
+sc/inc/rangelst.hxx:45
+    void ScRangeList::Format(class rtl::OUString &,enum ScRefFlags,class ScDocument *,enum formula::FormulaGrammar::AddressConvention,char16_t) const
+    enum ScRefFlags nFlags setBits=0x8000 clearBits=0x7f80
+sc/inc/xmlwrap.hxx:92
+    _Bool ScXMLImportWrapper::Import(enum ImportFlags,unsigned int &)
+    enum ImportFlags nMode setBits=0x1
+xmloff/inc/MetaExportComponent.hxx:32
+    void XMLMetaExportComponent::XMLMetaExportComponent(const class com::sun::star::uno::Reference<class com::sun::star::uno::XComponentContext> &,const class rtl::OUString &,enum SvXMLExportFlags)
+    enum SvXMLExportFlags nFlags setBits=0x1
+xmloff/source/text/XMLAutoTextEventExport.hxx:53
+    void XMLAutoTextEventExport::XMLAutoTextEventExport(const class com::sun::star::uno::Reference<class com::sun::star::uno::XComponentContext> &,const class rtl::OUString &,enum SvXMLExportFlags)
+    enum SvXMLExportFlags nFlags setBits=0xdff clearBits=0x200

compilerplugins/clang/constantparam.py
@@ -43,6 +43,7 @@ constructor_regex = re.compile("^\w+\(\)$")
 tmp1list = list()
 tmp2list = list()
 tmp3list = list()
+tmp4list = list()
 for callInfo, callValues in callDict.iteritems():
     nameAndParams = callInfo[1]
     if len(callValues) != 1:
@@ -81,6 +82,8 @@ for callInfo, callValues in callDict.iteritems():
         if callInfo[2] == "###0" and callValue == "InitAggregate()": continue
         if callValue == "shared_from_this()": continue
         tmp3list.append((sourceLoc, functionSig, callInfo[3] + " " + callInfo[2], callValue))
+    else:
+        tmp4list.append((sourceLoc, functionSig, callInfo[3] + " " + callInfo[2], callValue))
 
 
 # sort results by filename:lineno
@@ -90,22 +93,29 @@ def natural_sort_key(s, _nsre=re.compile('([0-9]+)')):
 tmp1list.sort(key=lambda v: natural_sort_key(v[0]))
 tmp2list.sort(key=lambda v: natural_sort_key(v[0]))
 tmp3list.sort(key=lambda v: natural_sort_key(v[0]))
+tmp4list.sort(key=lambda v: natural_sort_key(v[0]))
 
 # print out the results
-with open("loplugin.constantparam.report-booleans", "wt") as f:
+with open("compilerplugins/clang/constantparam.booleans.results", "wt") as f:
     for v in tmp1list:
         f.write(v[0] + "\n")
         f.write("    " + v[1] + "\n")
         f.write("    " + v[2] + "\n")
         f.write("    " + v[3] + "\n")
-with open("loplugin.constantparam.report-numbers", "wt") as f:
+with open("compilerplugins/clang/constantparam.numbers.results", "wt") as f:
     for v in tmp2list:
         f.write(v[0] + "\n")
         f.write("    " + v[1] + "\n")
         f.write("    " + v[2] + "\n")
         f.write("    " + v[3] + "\n")
-with open("loplugin.constantparam.report-constructors", "wt") as f:
+with open("compilerplugins/clang/constantparam.constructors.results", "wt") as f:
     for v in tmp3list:
+        f.write(v[0] + "\n")
+        f.write("    " + v[1] + "\n")
+        f.write("    " + v[2] + "\n")
+        f.write("    " + v[3] + "\n")
+with open("compilerplugins/clang/constantparam.others.results", "wt") as f:
+    for v in tmp4list:
         f.write(v[0] + "\n")
         f.write("    " + v[1] + "\n")
         f.write("    " + v[2] + "\n")
@@ -170,7 +180,7 @@ for callInfo, callValues in callDict.iteritems():
 tmp2list.sort(key=lambda v: natural_sort_key(v[0]))
 
 # print out the results
-with open("loplugin.constantparam.report-bitmask-params", "wt") as f:
+with open("compilerplugins/clang/constantparam.bitmask.results", "wt") as f:
     for v in tmp2list:
         f.write(v[0] + "\n")
         f.write("    " + v[1] + "\n")

editeng/source/editeng/impedit3.cxx
@@ -830,7 +830,7 @@ bool ImpEditEngine::CreateLines( sal_Int32 nPara, sal_uInt32 nStartPosY )
                 if ( IsFixedCellHeight() )
                     nTextLineHeight = ImplCalculateFontIndependentLineSpacing( aTmpFont.GetFontHeight() );
                 else
-                    nTextLineHeight = aTmpFont.GetPhysTxtSize( GetRefDevice(), OUString() ).Height();
+                    nTextLineHeight = aTmpFont.GetPhysTxtSize( GetRefDevice() ).Height();
                 // Metrics can be greater
                 FormatterFontMetric aTempFormatterMetrics;
                 RecalcFormatterFontMetrics( aTempFormatterMetrics, aTmpFont );
@@ -1339,7 +1339,7 @@ bool ImpEditEngine::CreateLines( sal_Int32 nPara, sal_uInt32 nStartPosY )
             if ( IsFixedCellHeight() )
                 aTextSize.Height() = ImplCalculateFontIndependentLineSpacing( aTmpFont.GetFontHeight() );
             else
-                aTextSize.Height() = aTmpFont.GetPhysTxtSize( pRefDev, OUString() ).Height();
+                aTextSize.Height() = aTmpFont.GetPhysTxtSize( pRefDev ).Height();
             pLine->SetHeight( (sal_uInt16)aTextSize.Height() );
         }
 
@@ -1669,7 +1669,7 @@ void ImpEditEngine::CreateAndInsertEmptyLine( ParaPortion* pParaPortion )
     aTmpFont.SetPhysFont( pRefDev );
 
     TextPortion* pDummyPortion = new TextPortion( 0 );
-    pDummyPortion->GetSize() = aTmpFont.GetPhysTxtSize( pRefDev, OUString() );
+    pDummyPortion->GetSize() = aTmpFont.GetPhysTxtSize( pRefDev );
     if ( IsFixedCellHeight() )
         pDummyPortion->GetSize().Height() = ImplCalculateFontIndependentLineSpacing( aTmpFont.GetFontHeight() );
     pParaPortion->GetTextPortions().Append(pDummyPortion);

commit c1311507bd68728df92829833d4027110fd0031e

fix impress slide context menu
regression from 4a340e36e8d3713806719351f455711c1254a178
"loplugin:constantparam"

Change-Id: I3dc34fd438ba7f057e52e888e3ea8053a0b3ea78

include/sfx2/objsh.hxx
@@ -458,7 +458,7 @@ public:
 
     // Transfer IFace
     bool                        IsAbortingImport() const;
-    void                        FinishedLoading();
+    void                        FinishedLoading( SfxLoadedFlags nWhich = SfxLoadedFlags::ALL );
 
     virtual void                SetFormatSpecificCompatibilityOptions( const OUString& /*rFilterTypeName*/ ) { /* Do not do anything here; Derived classes must overload to do actual work */ };
 

sfx2/source/doc/objmisc.cxx
@@ -1045,7 +1045,7 @@ void SfxObjectShell::InitOwnModel_Impl()
     }
 }
 
-void SfxObjectShell::FinishedLoading()
+void SfxObjectShell::FinishedLoading( SfxLoadedFlags nFlags )
 {
     std::shared_ptr<const SfxFilter> pFlt = pMedium->GetFilter();
     if( pFlt )
@@ -1055,7 +1055,7 @@ void SfxObjectShell::FinishedLoading()
 
     bool bSetModifiedTRUE = false;
     const SfxStringItem* pSalvageItem = SfxItemSet::GetItem<SfxStringItem>(pMedium->GetItemSet(), SID_DOC_SALVAGE, false);
-    if( !(pImpl->nLoadedFlags & SfxLoadedFlags::MAINDOCUMENT )
+    if( ( nFlags & SfxLoadedFlags::MAINDOCUMENT ) && !(pImpl->nLoadedFlags & SfxLoadedFlags::MAINDOCUMENT )
         && !(pImpl->nFlagsInProgress & SfxLoadedFlags::MAINDOCUMENT ))
     {
         pImpl->nFlagsInProgress |= SfxLoadedFlags::MAINDOCUMENT;
@@ -1083,7 +1083,23 @@ void SfxObjectShell::FinishedLoading()
         pImpl->nFlagsInProgress &= ~SfxLoadedFlags::MAINDOCUMENT;
     }
 
-    pImpl->nLoadedFlags |= SfxLoadedFlags::MAINDOCUMENT;
+    if( ( nFlags & SfxLoadedFlags::IMAGES ) && !(pImpl->nLoadedFlags & SfxLoadedFlags::IMAGES )
+        && !(pImpl->nFlagsInProgress & SfxLoadedFlags::IMAGES ))
+    {
+        pImpl->nFlagsInProgress |= SfxLoadedFlags::IMAGES;
+        uno::Reference<document::XDocumentProperties> xDocProps(
+            getDocProperties());
+        const OUString url(xDocProps->getAutoloadURL());
+        sal_Int32 delay(xDocProps->getAutoloadSecs());
+        SetAutoLoad( INetURLObject(url), delay * 1000,
+                     (delay > 0) || !url.isEmpty() );
+        if( !bSetModifiedTRUE && IsEnableSetModified() )
+            SetModified( false );
+        Invalidate( SID_SAVEASDOC );
+        pImpl->nFlagsInProgress &= ~SfxLoadedFlags::IMAGES;
+    }
+
+    pImpl->nLoadedFlags |= nFlags;
 
     if ( pImpl->nFlagsInProgress == SfxLoadedFlags::NONE )
     {