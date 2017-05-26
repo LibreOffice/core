0eba80c9ded1fc7275964b687c24a37ceb704482
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2017-05-26 22:37:06 +0200

    move ITileRenderable implementation to own file
    Change-Id: Ib05ecf5f92e0ae4adf99c4dd10d60e730a95cf45

--- vcl/Library_vcl.mk
+++ vcl/Library_vcl.mk
@@ -329,6 +329,7 @@ $(eval $(call gb_Library_add_exception_objects,vcl,\
     vcl/source/app/IconThemeInfo \
     vcl/source/app/IconThemeScanner \
     vcl/source/app/IconThemeSelector \
+    vcl/source/app/ITiledRenderable \
     vcl/source/app/sound \
     vcl/source/app/stdtext \
     vcl/source/app/svapp \
