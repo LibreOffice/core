6b93207b847bff4e75debaadd8fd476b7a318d85
Paul Trojahn <paul.trojahn@gmail.com>
2017-05-27 22:38:15 +0200

    tdf#45904 Move java XCellSeries test to c++
    Change-Id: Iedc1023b67138727061c2da13bebdb75af370ec6

---
+++ include/test/sheet/xcellseries.hxx
@@ -0,0 +1,30 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef INCLUDED_TEST_SHEET_XCELLSERIES_HXX
+#define INCLUDED_TEST_SHEET_XCELLSERIES_HXX
+
+#include <com/sun/star/uno/XInterface.hpp>
+#include <test/testdllapi.hxx>
+
+namespace apitest {
+
+class OOO_DLLPUBLIC_TEST XCellSeries
+{
+public:
+    virtual css::uno::Reference< css::uno::XInterface > init() = 0;
+    virtual ~XCellSeries(){}
+
+    void testFillAuto();
+    void testFillSeries();
+};
+
+}
+
+#endif // INCLUDED_TEST_SHEET_XCELLSERIES_HXX
