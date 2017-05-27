23fb760a9a3fedcbe24be9fec50d2df18a25d0d7
Paul Trojahn <paul.trojahn@gmail.com>
2017-05-27 22:38:15 +0200

    tdf#45904 Move java XCellSeries test to c++
    Change-Id: Iedc1023b67138727061c2da13bebdb75af370ec6

--- qadevOOo/Jar_OOoRunner.mk
+++ qadevOOo/Jar_OOoRunner.mk
@@ -600,7 +600,6 @@ $(eval $(call gb_Jar_add_sourcefiles,OOoRunner,\
     qadevOOo/tests/java/ifc/sheet/_XCellRangeMovement \
     qadevOOo/tests/java/ifc/sheet/_XCellRangeReferrer \
     qadevOOo/tests/java/ifc/sheet/_XCellRangesQuery \
-    qadevOOo/tests/java/ifc/sheet/_XCellSeries \
     qadevOOo/tests/java/ifc/sheet/_XConsolidatable \
     qadevOOo/tests/java/ifc/sheet/_XConsolidationDescriptor \
     qadevOOo/tests/java/ifc/sheet/_XDatabaseRange \
