450a3dd270392ee8dbbed6a5ab2d4d88e5c1c9fa
Jan Holesovsky <kendy@collabora.com>
2017-05-29 16:55:49 +0200

    lok: Notify about the Formatting Marks changes.
    Change-Id: Ibcbd370c99cf63789637ad6642c91775066ecfad

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -1521,6 +1521,7 @@ static void doc_iniUnoCommands ()
         OUString(".uno:CharBackgroundExt"),
         OUString(".uno:CharFontName"),
         OUString(".uno:Color"),
+        OUString(".uno:ControlCodes"),
         OUString(".uno:DecrementIndent"),
         OUString(".uno:DefaultBullet"),
         OUString(".uno:DefaultNumbering"),
