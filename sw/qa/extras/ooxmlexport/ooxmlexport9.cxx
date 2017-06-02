8a59b30bb1af55f7afd8b98e4b60234f98d84c76
Miklos Vajna <vmiklos@collabora.co.uk>
2017-06-02 18:41:38 +0200

    Related: tdf#108269 DOCM filter: preserve VBA stream
    This means 2 new streams when roundtripping DOCM files that actually
    have macros: word/vbaProject.bin and word/vbaData.xml (+ the relation
    pointing to the second from the first).
    
    Change-Id: Iba24eea4c5bca8f743a53027c71ed2aae48f1934
    Reviewed-on: https://gerrit.libreoffice.org/38360
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>

--- sw/qa/extras/ooxmlexport/ooxmlexport9.cxx
+++ sw/qa/extras/ooxmlexport/ooxmlexport9.cxx
@@ -93,6 +93,18 @@ DECLARE_SW_ROUNDTRIP_TEST(testBadDocm, "bad.docm", nullptr, DocmTest)
     CPPUNIT_ASSERT_EQUAL(OUString("MS Word 2007 XML VBA"), pTextDoc->GetDocShell()->GetMedium()->GetFilter()->GetName());
 }
 
+DECLARE_SW_ROUNDTRIP_TEST(testTdf108269, "tdf108269.docm", nullptr, DocmTest)
+{
+    if (!mbExported)
+        return;
+
+    uno::Reference<packages::zip::XZipFileAccess2> xNameAccess = packages::zip::ZipFileAccess::createWithURL(comphelper::getComponentContext(m_xSFactory), maTempFile.GetURL());
+    // This failed: VBA streams were not roundtripped via the doc-level
+    // grab-bag.
+    CPPUNIT_ASSERT(xNameAccess->hasByName("word/vbaProject.bin"));
+    CPPUNIT_ASSERT(xNameAccess->hasByName("word/vbaData.xml"));
+}
+
 DECLARE_OOXMLEXPORT_TEST(testTdf92045, "tdf92045.docx")
 {
     // This was true, <w:effect w:val="none"/> resulted in setting the blinking font effect.
