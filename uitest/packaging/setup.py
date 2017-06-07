8af868e2879282e85e32cabe824bc0990985fc81
Markus Mohrhard <markus.mohrhard@googlemail.com>
2017-06-08 00:28:35 +0200

    prepare publishing the libreoffice-connection python code
    Change-Id: I730207465e185a0b942b5636cd7ba3bebe7856f5

---
+++ uitest/packaging/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+        name="libreoffice-connection",
+        version="0.0.1",
+        description="Connection code for LibreOffice's pyUNO",
+        long_description="",
+        url="http://www.libreoffice.org",
+        author="The LibreOffice developers",
+        author_email="libreoffice@lists.freedesktop.org",
+        license="MPL2",
+        classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+            "Programming Language :: Python :: 3.2",
+            "Programming Language :: Python :: 3.3",
+            "Programming Language :: Python :: 3.4",
+            "Programming Language :: Python :: 3.5",
+            "Programming Language :: Python :: 3.6",
+            "Topic :: Office/Business :: Office Suites",
+            "Topic :: Software Development :: Libraries",
+            ],
+        keywords="office automation",
+        packages=find_packages(),
+)
