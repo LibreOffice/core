7071589519f3bc63d27941ea38332b4212dde101
Markus Mohrhard <markus.mohrhard@googlemail.com>
2017-06-10 07:30:08 +0200

    improve description
    Change-Id: I59060690b7ed138987df07adf174cf43fadbae77
    Reviewed-on: https://gerrit.libreoffice.org/38624
    Reviewed-by: Markus Mohrhard <markus.mohrhard@googlemail.com>
    Tested-by: Markus Mohrhard <markus.mohrhard@googlemail.com>

---
+++ uitest/packaging/README.rst
@@ -0,0 +1,4 @@
+Connection code for LibreOffice's pyUNO
+=======================================
+
+This code allows out-of-process communication with a LibreOffice instance. The instance can either be started by the script or a connection to a running LibreOffice instance. The code contains several safety checks for hanging and crashed LibreOffice instances.
--- uitest/packaging/setup.py
+++ uitest/packaging/setup.py
@@ -1,10 +1,18 @@
 from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+long_description = f.read()
 
 setup(
         name="libreoffice-connection",
         version="0.0.1",
         description="Connection code for LibreOffice's pyUNO",
-        long_description="",
+        long_description=long_description,
         url="http://www.libreoffice.org",
         author="The LibreOffice developers",
         author_email="libreoffice@lists.freedesktop.org",
