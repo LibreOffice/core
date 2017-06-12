e7e02af3f98696d1bd11e8ec22b2454cae7d0f3f
jan Iversen <jani@libreoffice.org>
2017-06-12 17:58:27 +0200

    iOS patch libetonyek
    support for arm64
    
    Change-Id: Ic8e78aaf6a72fa8bfc6971544414c84c29b0ec53

--- external/libetonyek/UnpackedTarball_libetonyek.mk
+++ external/libetonyek/UnpackedTarball_libetonyek.mk
@@ -19,6 +19,7 @@ $(eval $(call gb_UnpackedTarball_add_patches,libetonyek,\
 	external/libetonyek/rpath.patch \
 	external/libetonyek/silence-libxml.patch \
 	external/libetonyek/0001-fix-brain-fart.patch.1 \
+	external/libetonyek/iOS.patch.0 \
 ))
 
 ifneq ($(OS),MACOSX)
