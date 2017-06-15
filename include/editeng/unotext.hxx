555ef764db5fa90c6792909e863d7b6e694220fc
Varun Dhall <varun.dhall@studentpartner.com>
2017-06-15 21:37:57 +0530

    EditEngine: Making ODF Format default for Copy/Paste
    Change-Id: I6d8f79e8fa888f846ffa468b53f0f7ca25d7478e

--- include/editeng/unotext.hxx
+++ include/editeng/unotext.hxx
@@ -514,9 +514,10 @@ private:
     std::unique_ptr<SvxEditSource>          mpEditSource;
     sal_Int32               mnNextParagraph;
     const SvxUnoTextBase&   mrText;
+    const ESelection        maSelection;
 
 public:
-    SvxUnoTextContentEnumeration( const SvxUnoTextBase& _rText ) throw();
+    SvxUnoTextContentEnumeration( const SvxUnoTextBase& _rText, const ESelection rSel ) throw();
     virtual ~SvxUnoTextContentEnumeration() throw() override;
 
     // css::container::XEnumeration
@@ -547,7 +548,7 @@ protected:
     using SvxUnoTextRangeBase::getPropertyValue;
 
 public:
-    SvxUnoTextContent( const SvxUnoTextBase& rText, sal_Int32 nPara ) throw();
+    SvxUnoTextContent( const SvxUnoTextBase& rText, sal_Int32 nPara, const ESelection rSel ) throw();
     SvxUnoTextContent( const SvxUnoTextContent& rContent ) throw();
     virtual ~SvxUnoTextContent() throw() override;
 
@@ -606,11 +607,12 @@ private:
     css::uno::Reference< css::text::XText > mxParentText;
     const SvxUnoTextBase&    mrParentText;
     sal_Int32                mnParagraph;
-    std::vector<sal_Int32>*  mpPortions;
+    std::vector< rtl::Reference<SvxUnoTextRange> >*  mpPortions;
     sal_uInt16               mnNextPortion;
+    const ESelection         mnSel;
 
 public:
-    SvxUnoTextRangeEnumeration( const SvxUnoTextBase& rText, sal_Int32 nPara ) throw();
+    SvxUnoTextRangeEnumeration( const SvxUnoTextBase& rText, sal_Int32 nPara, const ESelection rSel ) throw();
     virtual ~SvxUnoTextRangeEnumeration() throw() override;
 
     // css::container::XEnumeration
