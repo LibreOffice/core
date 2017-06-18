dca0d62b6de39c16b00f8325ea5f9ab3c7355d2d
Chris Sherlock <chris.sherlock79@gmail.com>
2017-06-18 21:19:04 +1000

    osl: move crash handler signal interception to Desktop
    When InitVCL is called from soffice, it does so early, before potentially
    instantiating any JVM, so there are no problems with establishing our SEGV,
    etc. handlers.  However, when InitVCL is called from a process where a JVM
    is already instantiated (either because it's a Java application, or because
    it's a native process that already instantiated a JVM), we must not install
    SEGV etc. handlers overriding the JVM ones.
    
    Consequently, desktop will handle Unix SEGV, WINCH and ILL signals via the
    function lo_initialize, in the PRE_INIT stage.
    
    Change-Id: I70f0150c89d48f8a3893acb3f58a25b3ef730449

--- include/osl/signal.h
+++ include/osl/signal.h
@@ -100,6 +100,100 @@ SAL_DLLPUBLIC oslSignalAction SAL_CALL osl_raiseSignal(
 SAL_DLLPUBLIC sal_Bool SAL_CALL osl_setErrorReporting(
         sal_Bool bEnable );
 
+#ifdef UNX
+
+#include <signal.h>
+
+#if defined HAVE_VALGRIND_HEADERS
+#include <valgrind/memcheck.h>
+#endif
+
+#define ACT_IGNORE  1
+#define ACT_EXIT    2
+#define ACT_SYSTEM  3
+#define ACT_HIDE    4
+#define ACT_ABORT   5
+
+extern "C" using Handler1 = void (*)(int);
+extern "C" using Handler2 = void (*)(int, siginfo_t *, void *);
+
+typedef struct
+{
+    int Signal;
+    int Action;
+    Handler1 Handler;
+    bool siginfo; // Handler's type is Handler2
+} SignalAction;
+
+static SignalAction Signals[] =
+{
+    { SIGHUP,    ACT_HIDE,   SIG_DFL, false }, /* hangup */
+    { SIGINT,    ACT_EXIT,   SIG_DFL, false }, /* interrupt (rubout) */
+    { SIGQUIT,   ACT_EXIT,   SIG_DFL, false }, /* quit (ASCII FS) */
+    { SIGILL,    ACT_SYSTEM, SIG_DFL, false }, /* illegal instruction (not reset when caught) */
+/* changed from ACT_ABOUT to ACT_SYSTEM to try and get collector to run*/
+    { SIGTRAP,   ACT_ABORT,  SIG_DFL, false }, /* trace trap (not reset when caught) */
+#if ( SIGIOT != SIGABRT )
+    { SIGIOT,    ACT_ABORT,  SIG_DFL, false }, /* IOT instruction */
+#endif
+#if defined(FORCE_DEFAULT_SIGNAL)
+    { SIGABRT,   ACT_SYSTEM, SIG_DFL, false }, /* used by abort, replace SIGIOT in the future */
+#else
+    { SIGABRT,   ACT_ABORT,  SIG_DFL, false }, /* used by abort, replace SIGIOT in the future */
+#endif
+#ifdef SIGEMT
+    { SIGEMT,    ACT_SYSTEM, SIG_DFL, false }, /* EMT instruction */
+/* changed from ACT_ABORT to ACT_SYSTEM to remove handler*/
+/* SIGEMT may also be used by the profiler - so it is probably not a good
+plan to have the new handler use this signal*/
+#endif
+    { SIGFPE,    ACT_ABORT,  SIG_DFL, false }, /* floating point exception */
+    { SIGKILL,   ACT_SYSTEM, SIG_DFL, false }, /* kill (cannot be caught or ignored) */
+    { SIGBUS,    ACT_ABORT,  SIG_DFL, false }, /* bus error */
+#if defined(FORCE_DEFAULT_SIGNAL)
+    { SIGSEGV,   ACT_SYSTEM, SIG_DFL, false }, /* segmentation violation */
+#else
+    { SIGSEGV,   ACT_ABORT,  SIG_DFL, false }, /* segmentation violation */
+#endif
+#ifdef SIGSYS
+    { SIGSYS,    ACT_ABORT,  SIG_DFL, false }, /* bad argument to system call */
+#endif
+    { SIGPIPE,   ACT_HIDE,   SIG_DFL, false }, /* write on a pipe with no one to read it */
+#if defined(FORCE_DEFAULT_SIGNAL)
+    { SIGALRM,   ACT_SYSTEM, SIG_DFL, false }, /* alarm clock */
+#else
+    { SIGALRM,   ACT_EXIT,   SIG_DFL, false }, /* alarm clock */
+#endif
+    { SIGTERM,   ACT_EXIT,   SIG_DFL, false }, /* software termination signal from kill */
+    { SIGUSR1,   ACT_SYSTEM, SIG_DFL, false }, /* user defined signal 1 */
+    { SIGUSR2,   ACT_SYSTEM, SIG_DFL, false }, /* user defined signal 2 */
+    { SIGCHLD,   ACT_SYSTEM, SIG_DFL, false }, /* child status change */
+#ifdef SIGPWR
+    { SIGPWR,    ACT_IGNORE, SIG_DFL, false }, /* power-fail restart */
+#endif
+    { SIGWINCH,  ACT_IGNORE, SIG_DFL, false }, /* window size change */
+    { SIGURG,    ACT_EXIT,   SIG_DFL, false }, /* urgent socket condition */
+#ifdef SIGPOLL
+    { SIGPOLL,   ACT_EXIT,   SIG_DFL, false }, /* pollable event occurred */
+#endif
+    { SIGSTOP,   ACT_SYSTEM, SIG_DFL, false }, /* stop (cannot be caught or ignored) */
+    { SIGTSTP,   ACT_SYSTEM, SIG_DFL, false }, /* user stop requested from tty */
+    { SIGCONT,   ACT_SYSTEM, SIG_DFL, false }, /* stopped process has been continued */
+    { SIGTTIN,   ACT_SYSTEM, SIG_DFL, false }, /* background tty read attempted */
+    { SIGTTOU,   ACT_SYSTEM, SIG_DFL, false }, /* background tty write attempted */
+    { SIGVTALRM, ACT_EXIT,   SIG_DFL, false }, /* virtual timer expired */
+    { SIGPROF,   ACT_SYSTEM, SIG_DFL, false }, /* profiling timer expired */
+/*Change from ACT_EXIT to ACT_SYSTEM for SIGPROF is so that profiling signals do
+not get taken by the new handler - the new handler does not pass on context
+information which causes 'collect' to crash. This is a way of avoiding
+what looks like a bug in the new handler*/
+    { SIGXCPU,   ACT_ABORT,  SIG_DFL, false }, /* exceeded cpu limit */
+    { SIGXFSZ,   ACT_ABORT,  SIG_DFL, false }  /* exceeded file size limit */
+};
+
+void SAL_DLLPUBLIC initSignal(SignalAction &rSignal);
+#endif
+
 #ifdef __cplusplus
 }
 #endif
--- sal/osl/unx/signal.cxx
+++ sal/osl/unx/signal.cxx
@@ -61,234 +61,94 @@
 #include <rtl/digest.h>
 
 #include "file_path_helper.hxx"
-#define ACT_IGNORE  1
-#define ACT_EXIT    2
-#define ACT_SYSTEM  3
-#define ACT_HIDE    4
-#define ACT_ABORT   5
-
-#if defined HAVE_VALGRIND_HEADERS
-#include <valgrind/memcheck.h>
-#endif
 
 namespace
 {
-extern "C" using Handler1 = void (*)(int);
-extern "C" using Handler2 = void (*)(int, siginfo_t *, void *);
-struct SignalAction
-{
-    int Signal;
-    int Action;
-    Handler1 Handler;
-    bool siginfo; // Handler's type is Handler2
-} Signals[] =
-{
-    { SIGHUP,    ACT_HIDE,   SIG_DFL, false }, /* hangup */
-    { SIGINT,    ACT_EXIT,   SIG_DFL, false }, /* interrupt (rubout) */
-    { SIGQUIT,   ACT_EXIT,   SIG_DFL, false }, /* quit (ASCII FS) */
-    { SIGILL,    ACT_SYSTEM, SIG_DFL, false }, /* illegal instruction (not reset when caught) */
-/* changed from ACT_ABOUT to ACT_SYSTEM to try and get collector to run*/
-    { SIGTRAP,   ACT_ABORT,  SIG_DFL, false }, /* trace trap (not reset when caught) */
-#if ( SIGIOT != SIGABRT )
-    { SIGIOT,    ACT_ABORT,  SIG_DFL, false }, /* IOT instruction */
-#endif
-#if defined(FORCE_DEFAULT_SIGNAL)
-    { SIGABRT,   ACT_SYSTEM, SIG_DFL, false }, /* used by abort, replace SIGIOT in the future */
-#else
-    { SIGABRT,   ACT_ABORT,  SIG_DFL, false }, /* used by abort, replace SIGIOT in the future */
-#endif
-#ifdef SIGEMT
-    { SIGEMT,    ACT_SYSTEM, SIG_DFL, false }, /* EMT instruction */
-/* changed from ACT_ABORT to ACT_SYSTEM to remove handler*/
-/* SIGEMT may also be used by the profiler - so it is probably not a good
-plan to have the new handler use this signal*/
-#endif
-    { SIGFPE,    ACT_ABORT,  SIG_DFL, false }, /* floating point exception */
-    { SIGKILL,   ACT_SYSTEM, SIG_DFL, false }, /* kill (cannot be caught or ignored) */
-    { SIGBUS,    ACT_ABORT,  SIG_DFL, false }, /* bus error */
-#if defined(FORCE_DEFAULT_SIGNAL)
-    { SIGSEGV,   ACT_SYSTEM, SIG_DFL, false }, /* segmentation violation */
-#else
-    { SIGSEGV,   ACT_ABORT,  SIG_DFL, false }, /* segmentation violation */
-#endif
-#ifdef SIGSYS
-    { SIGSYS,    ACT_ABORT,  SIG_DFL, false }, /* bad argument to system call */
-#endif
-    { SIGPIPE,   ACT_HIDE,   SIG_DFL, false }, /* write on a pipe with no one to read it */
-#if defined(FORCE_DEFAULT_SIGNAL)
-    { SIGALRM,   ACT_SYSTEM, SIG_DFL, false }, /* alarm clock */
-#else
-    { SIGALRM,   ACT_EXIT,   SIG_DFL, false }, /* alarm clock */
-#endif
-    { SIGTERM,   ACT_EXIT,   SIG_DFL, false }, /* software termination signal from kill */
-    { SIGUSR1,   ACT_SYSTEM, SIG_DFL, false }, /* user defined signal 1 */
-    { SIGUSR2,   ACT_SYSTEM, SIG_DFL, false }, /* user defined signal 2 */
-    { SIGCHLD,   ACT_SYSTEM, SIG_DFL, false }, /* child status change */
-#ifdef SIGPWR
-    { SIGPWR,    ACT_IGNORE, SIG_DFL, false }, /* power-fail restart */
-#endif
-    { SIGWINCH,  ACT_IGNORE, SIG_DFL, false }, /* window size change */
-    { SIGURG,    ACT_EXIT,   SIG_DFL, false }, /* urgent socket condition */
-#ifdef SIGPOLL
-    { SIGPOLL,   ACT_EXIT,   SIG_DFL, false }, /* pollable event occurred */
-#endif
-    { SIGSTOP,   ACT_SYSTEM, SIG_DFL, false }, /* stop (cannot be caught or ignored) */
-    { SIGTSTP,   ACT_SYSTEM, SIG_DFL, false }, /* user stop requested from tty */
-    { SIGCONT,   ACT_SYSTEM, SIG_DFL, false }, /* stopped process has been continued */
-    { SIGTTIN,   ACT_SYSTEM, SIG_DFL, false }, /* background tty read attempted */
-    { SIGTTOU,   ACT_SYSTEM, SIG_DFL, false }, /* background tty write attempted */
-    { SIGVTALRM, ACT_EXIT,   SIG_DFL, false }, /* virtual timer expired */
-    { SIGPROF,   ACT_SYSTEM, SIG_DFL, false }, /* profiling timer expired */
-/*Change from ACT_EXIT to ACT_SYSTEM for SIGPROF is so that profiling signals do
-not get taken by the new handler - the new handler does not pass on context
-information which causes 'collect' to crash. This is a way of avoiding
-what looks like a bug in the new handler*/
-    { SIGXCPU,   ACT_ABORT,  SIG_DFL, false }, /* exceeded cpu limit */
-    { SIGXFSZ,   ACT_ABORT,  SIG_DFL, false }  /* exceeded file size limit */
-};
 const int NoSignals = SAL_N_ELEMENTS(Signals);
 
-bool bSetSEGVHandler = false;
-bool bSetWINCHHandler = false;
-bool bSetILLHandler = false;
-
 void signalHandlerFunction(int, siginfo_t *, void *);
-
-void getExecutableName_Impl (rtl_String ** ppstrProgName)
-{
-    rtl_uString* ustrProgFile = nullptr;
-    osl_getExecutableFile (&ustrProgFile);
-    if (ustrProgFile)
-    {
-        rtl_uString * ustrProgName = nullptr;
-        osl_systemPathGetFileNameOrLastDirectoryPart (ustrProgFile, &ustrProgName);
-        if (ustrProgName != nullptr)
-        {
-            rtl_uString2String (
-                ppstrProgName,
-                rtl_uString_getStr (ustrProgName), rtl_uString_getLength (ustrProgName),
-                osl_getThreadTextEncoding(),
-                OUSTRING_TO_OSTRING_CVTFLAGS);
-            rtl_uString_release (ustrProgName);
-        }
-        rtl_uString_release (ustrProgFile);
-    }
-}
-
-bool is_soffice_Impl()
-{
-    sal_Int32 idx = -1;
-    rtl_String* strProgName = nullptr;
-
-    getExecutableName_Impl (&strProgName);
-    if (strProgName)
-    {
-        idx = rtl_str_indexOfStr (rtl_string_getStr (strProgName), "soffice");
-        rtl_string_release (strProgName);
-    }
-    return (idx != -1);
-}
-
-#if HAVE_FEATURE_BREAKPAD
-bool is_unset_signal(int signal)
-{
-#ifdef DBG_UTIL
-    return (!bSetSEGVHandler && signal == SIGSEGV) ||
-        (!bSetWINCHHandler && signal == SIGWINCH) ||
-        (!bSetILLHandler && signal == SIGILL);
-#else
-    (void) signal;
-    return false;
-#endif
-}
-#endif
-
 }
 
 bool onInitSignal()
 {
-    if (is_soffice_Impl())
+    /* Initialize the rest of the signals */
+    for (SignalAction &rSignal : Signals)
     {
-        // WORKAROUND FOR SEGV HANDLER CONFLICT
-        //
-        // the java jit needs SIGSEGV for proper work
-        // and we need SIGSEGV for the office crashguard
-        //
-        // TEMPORARY SOLUTION:
-        //   the office sets the signal handler during startup
-        //   java can than overwrite it, if needed
-        bSetSEGVHandler = true;
-
-        // WORKAROUND FOR WINCH HANDLER (SEE ABOVE)
-        bSetWINCHHandler = true;
-
-        // WORKAROUND FOR ILLEGAL INSTRUCTION HANDLER (SEE ABOVE)
-        bSetILLHandler = true;
+        // Don't wipe out JVM signal handlers
+        switch (rSignal.Signal)
+        {
+            case SIGSEGV:
+            case SIGWINCH: // caters for Apache, this is the signal for graceful shutdown
+            case SIGILL:
+                break;
+            default:
+                initSignal(rSignal);
+        }
     }
 
-#ifdef DBG_UTIL
-    bSetSEGVHandler = bSetWINCHHandler = bSetILLHandler = false;
-#endif
+    /* Clear signal mask inherited from parent process (on Mac OS X, upon a
+       crash soffice re-execs itself from within the signal handler, so the
+       second soffice would have the guilty signal blocked and would freeze upon
+       encountering a similar crash again) */
+    sigset_t unset;
 
+    if (sigemptyset(&unset) < 0 || pthread_sigmask(SIG_SETMASK, &unset, nullptr) < 0)
+        SAL_WARN("sal.osl", "sigemptyset or pthread_sigmask failed");
+
+    return true;
+}
+
+void initSignal(SignalAction &rSignal)
+{
     struct sigaction act;
     act.sa_sigaction = signalHandlerFunction;
     act.sa_flags = SA_RESTART | SA_SIGINFO;
 
     sigfillset(&(act.sa_mask));
 
-    /* Initialize the rest of the signals */
-    for (SignalAction & rSignal : Signals)
-    {
 #if defined HAVE_VALGRIND_HEADERS
-        if (rSignal.Signal == SIGUSR2 && RUNNING_ON_VALGRIND)
-            rSignal.Action = ACT_IGNORE;
+    if (rSignal.Signal == SIGUSR2 && RUNNING_ON_VALGRIND)
+        rSignal.Action = ACT_IGNORE;
 #endif
 
-        /* hack: stomcatd is attaching JavaVM which does not work with an sigaction(SEGV) */
-        if ((bSetSEGVHandler || rSignal.Signal != SIGSEGV)
-        && (bSetWINCHHandler || rSignal.Signal != SIGWINCH)
-        && (bSetILLHandler   || rSignal.Signal != SIGILL))
+    if (rSignal.Action != ACT_SYSTEM)
+    {
+        if (rSignal.Action == ACT_HIDE)
         {
-            if (rSignal.Action != ACT_SYSTEM)
-            {
-                if (rSignal.Action == ACT_HIDE)
-                {
-                    struct sigaction ign;
+            struct sigaction ign;
 
-                    ign.sa_handler = SIG_IGN;
-                    ign.sa_flags   = 0;
-                    sigemptyset(&ign.sa_mask);
+            ign.sa_handler = SIG_IGN;
+            ign.sa_flags   = 0;
+            sigemptyset(&ign.sa_mask);
 
-                    struct sigaction oact;
-                    if (sigaction(rSignal.Signal, &ign, &oact) == 0) {
-                        rSignal.siginfo = (oact.sa_flags & SA_SIGINFO) != 0;
-                        if (rSignal.siginfo) {
-                            rSignal.Handler = reinterpret_cast<Handler1>(
-                                oact.sa_sigaction);
-                        } else {
-                            rSignal.Handler = oact.sa_handler;
-                        }
-                    } else {
-                        rSignal.Handler = SIG_DFL;
-                        rSignal.siginfo = false;
-                    }
+            struct sigaction oact;
+            if (sigaction(rSignal.Signal, &ign, &oact) == 0) {
+                rSignal.siginfo = (oact.sa_flags & SA_SIGINFO) != 0;
+                if (rSignal.siginfo) {
+                    rSignal.Handler = reinterpret_cast<Handler1>(
+                        oact.sa_sigaction);
+                } else {
+                    rSignal.Handler = oact.sa_handler;
                 }
-                else
-                {
-                    struct sigaction oact;
-                    if (sigaction(rSignal.Signal, &act, &oact) == 0) {
-                        rSignal.siginfo = (oact.sa_flags & SA_SIGINFO) != 0;
-                        if (rSignal.siginfo) {
-                            rSignal.Handler = reinterpret_cast<Handler1>(
-                                oact.sa_sigaction);
-                        } else {
-                            rSignal.Handler = oact.sa_handler;
-                        }
-                    } else {
-                        rSignal.Handler = SIG_DFL;
-                        rSignal.siginfo = false;
-                    }
+            } else {
+                rSignal.Handler = SIG_DFL;
+                rSignal.siginfo = false;
+            }
+        }
+        else
+        {
+            struct sigaction oact;
+            if (sigaction(rSignal.Signal, &act, &oact) == 0) {
+                rSignal.siginfo = (oact.sa_flags & SA_SIGINFO) != 0;
+                if (rSignal.siginfo) {
+                    rSignal.Handler = reinterpret_cast<Handler1>(
+                        oact.sa_sigaction);
+                } else {
+                    rSignal.Handler = oact.sa_handler;
                 }
+            } else {
+                rSignal.Handler = SIG_DFL;
+                rSignal.siginfo = false;
             }
         }
     }
@@ -473,17 +333,17 @@ void signalHandlerFunction(int signal, siginfo_t * info, void * context)
 
 #if HAVE_FEATURE_BREAKPAD
     if ((Info.Signal == osl_Signal_AccessViolation ||
-            Info.Signal == osl_Signal_IntegerDivideByZero ||
-            Info.Signal == osl_Signal_FloatDivideByZero) && !is_unset_signal(signal))
+         Info.Signal == osl_Signal_IntegerDivideByZero ||
+         Info.Signal == osl_Signal_FloatDivideByZero))
     {
-        for (SignalAction & rSignal : Signals)
+        for (SignalAction &rSignal : Signals)
         {
             if (rSignal.Signal == signal)
             {
                 if (rSignal.siginfo)
                 {
-                    (*reinterpret_cast<Handler2>(rSignal.Handler))(
-                        signal, info, context);
+                    (*reinterpret_cast<Handler2>(
+                        rSignal.Handler))(signal, info, context);
                 }
                 else
                 {
