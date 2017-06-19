cec62fe8be219319c28a6661bd39c527610da29e
Katarina Behrens <Katarina.Behrens@cib.de>
2017-06-19 15:08:20 +0200

    gpg4libre: Warn before removing document signatures
    simple version, no "do not show this message again" checkbox
    
    Change-Id: Iba8ac8cda51acac748174c864aa5c205f2efcc8f

--- xmlsecurity/source/dialogs/digitalsignaturesdialog.cxx
+++ xmlsecurity/source/dialogs/digitalsignaturesdialog.cxx
@@ -316,7 +316,14 @@ bool DigitalSignaturesDialog::canAdd()
 
 bool DigitalSignaturesDialog::canRemove()
 {
-    return canAddRemove();
+    if ( maSignatureManager.meSignatureMode == DocumentSignatureMode::Content )
+    {
+        short nDlgRet = ScopedVclPtrInstance<MessageDialog>(
+              nullptr, XMLSEC_RES(STR_XMLSECDLG_QUERY_REALLYREMOVE), VclMessageType::Question, VclButtonsType::YesNo)->Execute();
+        return ( nDlgRet == RET_YES );
+    }
+    else
+        return canAddRemove();
 }
 
 short DigitalSignaturesDialog::Execute()
