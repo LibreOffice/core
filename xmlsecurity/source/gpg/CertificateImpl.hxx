84e699f5b085502738bfa76ec1a188b7bdd32809
Katarina Behrens <Katarina.Behrens@cib.de>
2017-06-19 23:36:12 +0200

    gpg4libre: API Change: add certificate kind (X509 vs. OpenPGP)
    Change-Id: I423bef41f93af9d1b78ee9795be7ec33c3c7ae0c

--- xmlsecurity/source/gpg/CertificateImpl.hxx
+++ xmlsecurity/source/gpg/CertificateImpl.hxx
@@ -23,6 +23,7 @@
 #include <com/sun/star/uno/Sequence.hxx>
 #include <com/sun/star/lang/XUnoTunnel.hpp>
 #include <com/sun/star/uno/SecurityException.hpp>
+#include <com/sun/star/security/CertificateKind.hpp>
 #include <com/sun/star/security/XCertificate.hpp>
 
 #include <key.h>
@@ -77,6 +78,7 @@ public:
 
     /// @see xmlsecurity::Certificate::getSHA256Thumbprint().
     virtual css::uno::Sequence<sal_Int8> getSHA256Thumbprint() override;
+    virtual css::security::CertificateKind getCertificateKind() override;
 
     // Helper methods
     void setCertificate(const GpgME::Key& key);
