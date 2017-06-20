LibreOffice/core

commit 5f8476c00f482ab5c8707de2ebdcd41faa4057fc

fdo#72697 write <loext:sender-initials>
Change-Id: I032bd79aa3ab73889a3ca723fba56ec371a28231

xmloff/source/text/txtflde.cxx
@@ -1756,7 +1756,7 @@ void XMLTextFieldExport::ExportFieldHelper(
         OUString aInitials( GetStringProperty(sPropertyInitials, rPropSet) );
         if( !aInitials.isEmpty() )
         {
-            SvXMLElementExport aCreatorElem( GetExport(), XML_NAMESPACE_TEXT,
+            SvXMLElementExport aCreatorElem( GetExport(), XML_NAMESPACE_LO_EXT,
                                               XML_SENDER_INITIALS, sal_True,
                                               sal_False );
             GetExport().Characters(aInitials);
@@ -2226,10 +2226,20 @@ void XMLTextFieldExport::ExportElement(enum XMLTokenEnum eElementName,
     if (eElementName != XML_TOKEN_INVALID)
     {
         // Element
-        SvXMLElementExport aElem( GetExport(), XML_NAMESPACE_TEXT,
-                                  eElementName, bAddSpace, bAddSpace );
-        // export content
-        GetExport().Characters(sContent);
+        if (eElementName == XML_SENDER_INITIALS)
+        {
+            SvXMLElementExport aElem( GetExport(), XML_NAMESPACE_LO_EXT,
+                                      eElementName, bAddSpace, bAddSpace );
+            // export content
+            GetExport().Characters(sContent);
+        }
+        else
+        {
+            SvXMLElementExport aElem( GetExport(), XML_NAMESPACE_TEXT,
+                                      eElementName, bAddSpace, bAddSpace );
+            // export content
+            GetExport().Characters(sContent);
+        }
     } else {
         // always export content
         GetExport().Characters(sContent);

commit 29f250c428953750ce89dec7ee961723478609c1

Resolves: tdf#107438 write <text:sender-initials> for fields
Only in <office:annotation> context write <loext:sender-initials>

Broken since

    commit 5f8476c00f482ab5c8707de2ebdcd41faa4057fc
    Date:   Fri Dec 13 23:21:46 2013 +0100

and

    commit 49f9694e34d07468cff4a7e4de332f11be3e0dd8
    Date:   Fri Dec 13 23:30:34 2013 +0100

Change-Id: I48b146b779878bf0faa5830a721559d8e864dd33

xmloff/source/text/txtflde.cxx
@@ -2220,23 +2220,10 @@ void XMLTextFieldExport::ExportElement(enum XMLTokenEnum eElementName,
     if (eElementName != XML_TOKEN_INVALID)
     {
         // Element
-        if (eElementName == XML_SENDER_INITIALS)
-        {
-            if (SvtSaveOptions().GetODFDefaultVersion() > SvtSaveOptions::ODFVER_012)
-            {
-                SvXMLElementExport aElem( GetExport(), XML_NAMESPACE_LO_EXT,
-                        eElementName, false, false );
-                // export content
-                GetExport().Characters(sContent);
-            }
-        }
-        else
-        {
-            SvXMLElementExport aElem( GetExport(), XML_NAMESPACE_TEXT,
-                                      eElementName, false, false );
-            // export content
-            GetExport().Characters(sContent);
-        }
+        SvXMLElementExport aElem( GetExport(), XML_NAMESPACE_TEXT,
+                eElementName, false, false );
+        // export content
+        GetExport().Characters(sContent);
     } else {
         // always export content
         GetExport().Characters(sContent);