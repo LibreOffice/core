ca8215d09f4b2e944ac3af1c80495ea4384ac655
Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
2017-06-20 14:49:10 +0200

    gpg4libre: Init xmlsec in one place before creating the gpg/x509 services
    Change-Id: I0308d586344b5e52ad327f6650b62ac0eac7ecb9

--- xmlsecurity/source/gpg/XMLSecurityContext.cxx
+++ xmlsecurity/source/gpg/XMLSecurityContext.cxx
@@ -22,32 +22,10 @@ using namespace css::xml::crypto;
 XMLSecurityContextGpg::XMLSecurityContextGpg()
     : m_nDefaultEnvIndex(-1)
 {
-    // TODO: same code in XMLSecurityContext_NssImpl, not a good idea
-    // prolly to initialize twice ...
-    //Init xmlsec library
-    if( xmlSecInit() < 0 ) {
-        throw RuntimeException() ;
-    }
-
-    //Init xmlsec crypto engine library
-    if( xmlSecCryptoInit() < 0 ) {
-        xmlSecShutdown() ;
-        throw RuntimeException() ;
-    }
-
-    //Enable external stream handlers
-    if( xmlEnableStreamInputCallbacks() < 0 ) {
-        xmlSecCryptoShutdown() ;
-        xmlSecShutdown() ;
-        throw RuntimeException() ;
-    }
 }
 
 XMLSecurityContextGpg::~XMLSecurityContextGpg()
 {
-    xmlDisableStreamInputCallbacks() ;
-    xmlSecCryptoShutdown() ;
-    xmlSecShutdown() ;
 }
 
 sal_Int32 SAL_CALL XMLSecurityContextGpg::addSecurityEnvironment(
