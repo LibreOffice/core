ab50f0b08b22af1e60a0b6ce5e7e8e7d1f665216
Miklos Vajna <vmiklos@collabora.co.uk>
2017-06-21 21:59:11 +0200

    xmlsecurity: implement support for building against system-xmlsec
    The only remaining difference is that in the system-xmlsec case we work
    with the default key manager, not with the one that's only added by our
    xmlsec patches.
    
    This works for me for the uses I know of (see
    <https://lists.freedesktop.org/archives/libreoffice/2017-February/076947.html>
    for the motivation): signing and verifying of different signatures (bad
    signature, good with non-trusted CA, good with trusted CA) with
    software-based certificates all behave as expected.
    
    Change-Id: If3f3e2b8373ab7397db3f98070a5a2ce51fa7c06
    Reviewed-on: https://gerrit.libreoffice.org/39075
    Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
    Tested-by: Jenkins <ci@libreoffice.org>

--- Repository.mk
+++ Repository.mk
@@ -917,7 +917,6 @@ $(eval $(call gb_Helper_register_packages_for_install,ooo,\
 	wizards_basicusr \
 	wizards_properties \
 	wizards_wizardshare \
-	xmlsec \
 	$(if $(ENABLE_HEADLESS),, \
 		chart2_opengl_shader \
 	) \
