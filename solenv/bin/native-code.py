05447e09f511a6a754a3671451b29a3b6b3f458f
brainbreaker <gautamprajapati06@gmail.com>
2017-06-21 17:00:33 +0530

    add factory function mapping for libspelllo archive
    Change-Id: I7cbea9381bc993e7894603c731ab0ac54e80d4b4
    Reviewed-on: https://gerrit.libreoffice.org/39049
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>

--- solenv/bin/native-code.py
+++ solenv/bin/native-code.py
@@ -60,7 +60,8 @@ core_factory_list = [
     ("libodfflatxmllo.a", "odfflatxml_component_getFactory"),
     ("libxmlscriptlo.a", "xmlscript_component_getFactory"),
     ("libmcnttype.a", "mcnttype_component_getFactory"),
-    ("libvcllo.a", "vcl_component_getFactory")
+    ("libvcllo.a", "vcl_component_getFactory"),
+    ("libspelllo.a", "spell_component_getFactory")
     ]
 
 core_constructor_list = [
