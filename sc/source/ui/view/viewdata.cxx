0fed0829a601aaed6134bbc689b394e38c830605
Eike Rathke <erack@redhat.com>
2017-06-21 17:45:46 +0200

    Assert that nTabNo fits maTabData, tdf#108654 related
    Change-Id: I30b95f69a71317896d86e36b1b926c703f1539d1

--- sc/source/ui/view/viewdata.cxx
+++ sc/source/ui/view/viewdata.cxx
@@ -502,6 +502,7 @@ ScViewData::~ScViewData() COVERITY_NOEXCEPT_FALSE
 
 void ScViewData::UpdateCurrentTab()
 {
+    assert(0 <= nTabNo && static_cast<size_t>(nTabNo) < maTabData.size());
     pThisTab = maTabData[nTabNo];
     while (!pThisTab)
     {
