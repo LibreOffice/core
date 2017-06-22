346121398b6fbab233bae3053e14868a50418fb6
jan Iversen <jani@libreoffice.org>
2017-06-22 16:11:51 +0200

    expat-2.2.1 patch to allow compile on macOS and others
    xmlparse.c has a
     #define buffer something
    
    later
     #include something
    
    that happens to use buffer as a parameter.
    
    Change-Id: I7378aa9481b30364097c70317c794c0bcca2f05c
    Reviewed-on: https://gerrit.libreoffice.org/39109
    Reviewed-by: jan iversen <jani@libreoffice.org>
    Tested-by: jan iversen <jani@libreoffice.org>

--- external/expat/UnpackedTarball_expat.mk
+++ external/expat/UnpackedTarball_expat.mk
@@ -14,6 +14,7 @@ $(eval $(call gb_UnpackedTarball_set_tarball,expat,$(EXPAT_TARBALL)))
 $(eval $(call gb_UnpackedTarball_add_patches,expat,\
 	external/expat/expat-winapi.patch \
 	external/expat/expat-iOS.patch \
+	external/expat/expat-2.2.1.patch \
 ))
 
 # This is a bit hackish.
