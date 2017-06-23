77f8987c752b76ead5593d54ffb8f77b1452fb8d
Muhammet Kara <muhammet.kara@pardus.org.tr>
2017-06-23 15:09:10 +0300

    Separate SvxMenuConfigPage from cfg.cxx
    Change-Id: I6e56756628bbbf3900f946715ce8b95bbe78ae03
    Reviewed-on: https://gerrit.libreoffice.org/39341
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Katarina Behrens <Katarina.Behrens@cib.de>
    (cherry picked from commit 091a7f474adac69b1808e5c812952586d253a809)

---
+++ cui/source/inc/SvxMenuConfigPage.hxx
@@ -0,0 +1,87 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+#ifndef INCLUDED_CUI_SOURCE_INC_SVXMENUCONFIGPAGE_HXX
+#define INCLUDED_CUI_SOURCE_INC_SVXMENUCONFIGPAGE_HXX
+
+#endif // INCLUDED_CUI_SOURCE_INC_SVXMENUCONFIGPAGE_HXX
+
+#include <vcl/fixed.hxx>
+#include <vcl/group.hxx>
+#include <vcl/layout.hxx>
+#include <vcl/lstbox.hxx>
+#include <vcl/menubtn.hxx>
+#include <vcl/toolbox.hxx>
+#include <svtools/treelistbox.hxx>
+#include <svtools/svmedit2.hxx>
+#include <svtools/svmedit.hxx>
+#include <com/sun/star/beans/XPropertySet.hpp>
+#include <com/sun/star/container/XIndexContainer.hpp>
+#include <com/sun/star/container/XNameAccess.hpp>
+#include <com/sun/star/ui/XUIConfigurationListener.hpp>
+#include <com/sun/star/ui/XUIConfigurationManager.hpp>
+#include <com/sun/star/ui/XImageManager.hpp>
+#include <com/sun/star/graphic/XGraphicProvider.hpp>
+#include <com/sun/star/frame/XFrame.hpp>
+#include <com/sun/star/uno/XComponentContext.hpp>
+#include <com/sun/star/lang/XSingleComponentFactory.hpp>
+
+#include <sfx2/tabdlg.hxx>
+#include <memory>
+#include <vector>
+#include <vcl/msgbox.hxx>
+
+#include "cfgutil.hxx"
+#include "cfg.hxx" //for SvxConfigPage and SaveInData
+
+class SvxMenuConfigPage : public SvxConfigPage
+{
+private:
+    bool m_bIsMenuBar;
+    DECL_LINK( SelectMenu, ListBox&, void );
+    DECL_LINK( SelectMenuEntry, SvTreeListBox *, void );
+    DECL_LINK( NewMenuHdl, Button *, void );
+    DECL_LINK( MenuSelectHdl, MenuButton *, void );
+    DECL_LINK( EntrySelectHdl, MenuButton *, void );
+    DECL_LINK( AddCommandsHdl, Button *, void );
+    DECL_LINK( AddSeparatorHdl, Button *, void );
+    DECL_LINK( AddSubmenuHdl, Button *, void );
+    DECL_LINK( DeleteCommandHdl, Button *, void );
+    DECL_LINK( AddFunctionHdl, SvxScriptSelectorDialog&, void );
+
+    void            Init() override;
+    void            UpdateButtonStates() override;
+    short           QueryReset() override;
+    void            DeleteSelectedContent() override;
+    void            DeleteSelectedTopLevel() override;
+
+public:
+    SvxMenuConfigPage( vcl::Window *pParent, const SfxItemSet& rItemSet, bool bIsMenuBar = true );
+    virtual ~SvxMenuConfigPage() override;
+    virtual void dispose() override;
+
+    SaveInData* CreateSaveInData(
+        const css::uno::Reference <
+            css::ui::XUIConfigurationManager >&,
+        const css::uno::Reference <
+            css::ui::XUIConfigurationManager >&,
+        const OUString& aModuleId,
+        bool docConfig ) override;
+};
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
