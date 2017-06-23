LibreOffice/core

commit c83a443eb106e426901d0ba8a809eedcd24c42c5

tdf#101821 sw: fix another layout footnote use-after-free
In SwContentFrame::MakeAll() the pPre (previous frame) is deleted in a
call to MoveFwd().  This SwTextFrame is inside of a footnote, and
the SwFlowFrame::CutTree() for whatever reason wants to format all
of the SwTextFrames inside the same SwFootnoteFrame, which causes
the pPre to be joined with another one.

Let's try to avoid that by checking that it's still in the layout after
the call to MoveFwd(); on the one hand it's not obvious that this frame
is important enough that it should be kept alive with ForbidDelete(),
on the other hand i have no idea if simply removing the ValidateSz()
call would introduce new loops or whatever.

Invalid read of size 4
 at 0x414E8D14: SwFrame::IsSctFrame() const (frame.hxx:1018)
 by 0x41A85A29: SwContentFrame::MakeAll(OutputDevice*) (calcmove.cxx:1713)
 by 0x41A7F499: SwFrame::OptPrepareMake() (calcmove.cxx:368)
 by 0x41ADF0CF: SwFrame::OptCalc() const (frame.hxx:892)
 by 0x41ADCC07: SwLayAction::FormatContent_(SwContentFrame const*, SwPageFrame const*) (layact.cxx:1789)
 by 0x41ADC195: SwLayAction::FormatContent(SwPageFrame const*) (layact.cxx:1620)
 by 0x41AD88DE: SwLayAction::InternalAction(OutputDevice*) (layact.cxx:760)
 by 0x41AD7080: SwLayAction::Action(OutputDevice*) (layact.cxx:351)
 by 0x41ADE32E: SwLayIdle::SwLayIdle(SwRootFrame*, SwViewShellImp*) (layact.cxx:2133)
 by 0x41FFC97E: SwViewShell::LayoutIdle() (viewsh.cxx:711)
Address 0x530ccf80 is 160 bytes inside a block of size 264 free'd
 at 0x4C2ED4A: free (vg_replace_malloc.c:530)
 by 0x4E5BCD0: rtl_freeMemory_SYSTEM(void*) (alloc_global.cxx:271)
 by 0x4E5C00A: rtl_freeMemory (alloc_global.cxx:341)
 by 0x4E5AAA0: rtl_cache_free (alloc_cache.cxx:1231)
 by 0xEFC9A6F: FixedMemPool::Free(void*) (mempool.cxx:49)
 by 0x41CA7DFA: SwTextFrame::operator delete(void*, unsigned long) (txtfrm.hxx:377)
 by 0x41C9F7B6: SwTextFrame::~SwTextFrame() (txtfrm.cxx:415)

sw/source/core/layout/calcmove.cxx
@@ -1681,6 +1681,26 @@ void SwContentFrame::MakeAll(vcl::RenderContext* /*pRenderContext*/)
         if ( !bMovedFwd && !MoveFwd( bMakePage, false ) )
             bMakePage = false;
         aRectFnSet.Refresh(this);
+        if (!bMovedFwd && bFootnote && GetIndPrev() != pPre)
+        {   // SwFlowFrame::CutTree() could have formatted and deleted pPre
+            auto const pPrevFootnoteFrame(static_cast<SwFootnoteFrame const*>(GetUpper())->GetMaster());
+            bool bReset = true;
+            if (pPrevFootnoteFrame)
+            {   // use GetIndNext() in case there are sections
+                for (auto p = pPrevFootnoteFrame->Lower(); p; p = p->GetIndNext())
+                {
+                    if (p == pPre)
+                    {
+                        bReset = false;
+                        break;
+                    }
+                }
+            }
+            if (bReset)
+            {
+                pPre = nullptr;
+            }
+        }
 
         // If MoveFwd moves the paragraph to the next page, a following
         // paragraph, which contains footnotes can cause the old upper

commit e10cf1b991006f3099042781df20e4487a5e0597

sw: fix UBSAN error in CppunitTest_sw_odfimport
/sw/source/core/layout/calcmove.cxx:1686:43: runtime error: downcast of address 0x2ae9ba4566c0 which does not point to an object of type 'const SwFootnoteFrame'
0x2ae9ba4566c0: note: object is of type 'SwSectionFrame'

Footnotes can contain sections of course, so use FindFootnoteFrame().

(regression from c83a443eb106e426901d0ba8a809eedcd24c42c5)

Change-Id: I7824c81ba74ed3341f0ba5b14dac89d71f6318db

sw/source/core/layout/calcmove.cxx
@@ -1683,7 +1683,8 @@ void SwContentFrame::MakeAll(vcl::RenderContext* /*pRenderContext*/)
         aRectFnSet.Refresh(this);
         if (!bMovedFwd && bFootnote && GetIndPrev() != pPre)
         {   // SwFlowFrame::CutTree() could have formatted and deleted pPre
-            auto const pPrevFootnoteFrame(static_cast<SwFootnoteFrame const*>(GetUpper())->GetMaster());
+            auto const pPrevFootnoteFrame(static_cast<SwFootnoteFrame const*>(
+                        FindFootnoteFrame())->GetMaster());
             bool bReset = true;
             if (pPrevFootnoteFrame)
             {   // use GetIndNext() in case there are sections