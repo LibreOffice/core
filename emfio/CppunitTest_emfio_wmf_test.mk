48af0b9310023f3789d46d5714498e62728a8d8c
Armin Le Grand <Armin.Le.Grand@cib.de>
2017-06-23 12:16:33 +0200

    emfplus: UnitTests and gerrit build corrections
    Needed to migrate UnitTest for Emf/Wmf import from
    vcl to emfio. Corrected stuff based on gerrit build
    feedback
    
    Change-Id: I7fd2456f814ea19583072ba09730a07e9b9d4061

--- emfio/CppunitTest_emfio_wmf_test.mk
+++ emfio/CppunitTest_emfio_wmf_test.mk
@@ -7,46 +7,39 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 
-$(eval $(call gb_CppunitTest_CppunitTest,vcl_wmf_test))
+$(eval $(call gb_CppunitTest_CppunitTest,emfio))
 
-$(eval $(call gb_CppunitTest_add_exception_objects,vcl_wmf_test, \
-    vcl/qa/cppunit/wmf/wmfimporttest \
+$(eval $(call gb_CppunitTest_set_componentfile,emfio,emfio/emfio))
+
+$(eval $(call gb_CppunitTest_set_include,emfio,\
+    $$(INCLUDE) \
+    -I$(SRCDIR)/emfio/inc \
 ))
 
-$(eval $(call gb_CppunitTest_use_externals,vcl_wmf_test,\
+$(eval $(call gb_CppunitTest_use_externals,emfio,\
     boost_headers \
     libxml2 \
     $(if $(filter PDFIUM,$(BUILD_TYPE)),pdfium) \
 ))
 
-$(eval $(call gb_CppunitTest_set_include,vcl_wmf_test,\
-    $$(INCLUDE) \
-    -I$(SRCDIR)/vcl/inc \
-    -I$(SRCDIR)/vcl/source/filter/wmf \
+$(eval $(call gb_CppunitTest_add_exception_objects,emfio, \
+    emfio/qa/cppunit/wmf/wmfimporttest \
 ))
 
-$(eval $(call gb_CppunitTest_use_library_objects,vcl_wmf_test, \
-    test-setupvcl \
-    vcl \
+$(eval $(call gb_CppunitTest_use_sdk_api,emfio))
+
+$(eval $(call gb_CppunitTest_use_library_objects,emfio, \
+    emfio \
 ))
 
-$(eval $(call gb_CppunitTest_use_externals,vcl_wmf_test,\
-    $(if $(filter LINUX MACOSX %BSD SOLARIS,$(OS)), \
-            curl) \
-    jpeg \
-    $(if $(filter-out IOS WNT,$(OS)), \
-            nss3 \
-            plc4) \
-    libeot \
-))
-
-$(eval $(call gb_CppunitTest_use_libraries,vcl_wmf_test,\
+$(eval $(call gb_CppunitTest_use_libraries,emfio,\
     $(call gb_Helper_optional,BREAKPAD, \
                crashreport) \
     basegfx \
-    comphelper \
+    drawinglayer \
     cppu \
     cppuhelper \
+    comphelper \
     i18nlangtag \
     i18nutil \
     $(if $(filter OPENCL,$(BUILD_TYPE)),opencl) \
@@ -56,15 +49,27 @@ $(eval $(call gb_CppunitTest_use_libraries,vcl_wmf_test,\
     svl \
     svt \
     test \
+    test-setupvcl \
     tl \
     ucbhelper \
     unotest \
+    vcl \
     utl \
     xmlreader \
 ))
 
+$(eval $(call gb_CppunitTest_use_externals,emfio,\
+    $(if $(filter LINUX MACOSX %BSD SOLARIS,$(OS)), \
+            curl) \
+    jpeg \
+    $(if $(filter-out IOS WNT,$(OS)), \
+            nss3 \
+            plc4) \
+    libeot \
+))
+
 ifeq ($(OS),MACOSX)
-$(eval $(call gb_CppunitTest_add_libs,vcl_wmf_test,\
+$(eval $(call gb_CppunitTest_add_libs,emfio,\
     -framework IOKit \
     -F/System/Library/PrivateFrameworks \
     -framework CoreUI \
@@ -73,12 +78,12 @@ $(eval $(call gb_CppunitTest_add_libs,vcl_wmf_test,\
 endif
 
 ifeq ($(ENABLE_JAVA),TRUE)
-$(eval $(call gb_CppunitTest_use_libraries,vcl_wmf_test,\
+$(eval $(call gb_CppunitTest_use_libraries,emfio,\
     jvmaccess \
 ))
 endif
 
-$(eval $(call gb_CppunitTest_use_externals,vcl_wmf_test,\
+$(eval $(call gb_CppunitTest_use_externals,emfio,\
     gio \
     graphite \
     harfbuzz \
@@ -86,30 +91,30 @@ $(eval $(call gb_CppunitTest_use_externals,vcl_wmf_test,\
     lcms2 \
 ))
 ifeq ($(ENABLE_HEADLESS),)
-$(eval $(call gb_CppunitTest_use_externals,vcl_wmf_test,\
+$(eval $(call gb_CppunitTest_use_externals,emfio,\
      epoxy \
  ))
 endif
 
 ifeq ($(OS),MACOSX)
-$(eval $(call gb_CppunitTest_use_system_darwin_frameworks,vcl_wmf_test,\
+$(eval $(call gb_CppunitTest_use_system_darwin_frameworks,emfio,\
     ApplicationServices \
 ))
-$(eval $(call gb_CppunitTest_use_system_darwin_frameworks,vcl_wmf_test,\
+$(eval $(call gb_CppunitTest_use_system_darwin_frameworks,emfio,\
     $(if $(filter X86_64,$(CPUNAME)),,QuickTime) \
     Cocoa \
     Carbon \
     CoreFoundation \
 ))
 ifneq ($(ENABLE_MACOSX_SANDBOX),TRUE)
-$(eval $(call gb_CppunitTest_use_libraries,vcl_wmf_test,\
+$(eval $(call gb_CppunitTest_use_libraries,emfio,\
     AppleRemote \
 ))
 endif
 endif
 
 ifeq ($(USING_X11),TRUE)
-$(eval $(call gb_CppunitTest_use_externals,vcl_wmf_test,\
+$(eval $(call gb_CppunitTest_use_externals,emfio,\
     cairo \
     cups \
     dbus \
@@ -120,14 +125,14 @@ $(eval $(call gb_CppunitTest_use_externals,vcl_wmf_test,\
 endif
 
 ifeq ($(ENABLE_HEADLESS),TRUE)
-$(eval $(call gb_CppunitTest_use_externals,vcl_wmf_test,\
+$(eval $(call gb_CppunitTest_use_externals,emfio,\
     cairo \
     freetype \
     fontconfig \
 ))
 else
 ifeq ($(OS),LINUX)
-$(eval $(call gb_CppunitTest_add_libs,vcl_wmf_test,\
+$(eval $(call gb_CppunitTest_add_libs,emfio,\
     -lm \
     -ldl \
     -lpthread \
@@ -137,12 +142,12 @@ endif
 endif
 
 ifeq ($(OS),ANDROID)
-$(eval $(call gb_CppunitTest_add_libs,vcl_wmf_test,\
+$(eval $(call gb_CppunitTest_add_libs,emfio,\
     -llog \
     -landroid \
     -llo-bootstrap \
 ))
-$(eval $(call gb_CppunitTest_use_externals,vcl_wmf_test,\
+$(eval $(call gb_CppunitTest_use_externals,emfio,\
     cairo \
     fontconfig \
     freetype \
@@ -151,14 +156,14 @@ $(eval $(call gb_CppunitTest_use_externals,vcl_wmf_test,\
 endif
 
 ifeq ($(OS),IOS)
-$(eval $(call gb_CppunitTest_use_system_darwin_frameworks,vcl_wmf_test,\
+$(eval $(call gb_CppunitTest_use_system_darwin_frameworks,emfio,\
     UIKit \
     CoreFoundation \
 ))
 endif
 
 ifeq ($(OS),WNT)
-$(eval $(call gb_CppunitTest_use_system_win32_libs,vcl_wmf_test,\
+$(eval $(call gb_CppunitTest_use_system_win32_libs,emfio,\
     advapi32 \
     crypt32 \
     gdi32 \
@@ -174,11 +179,11 @@ $(eval $(call gb_CppunitTest_use_system_win32_libs,vcl_wmf_test,\
     setupapi \
     shlwapi \
 ))
-#$(eval $(call gb_CppunitTest_add_nativeres,vcl_wmf_test,vcl/salsrc))
+#$(eval $(call gb_CppunitTest_add_nativeres,emfio,vcl/salsrc))
 endif
 
 ifeq ($(OS), $(filter LINUX %BSD SOLARIS, $(OS)))
-$(eval $(call gb_CppunitTest_add_libs,vcl_wmf_test,\
+$(eval $(call gb_CppunitTest_add_libs,emfio,\
     -lm $(DLOPEN_LIBS) \
     -lpthread \
     -lX11 \
@@ -186,29 +191,27 @@ $(eval $(call gb_CppunitTest_add_libs,vcl_wmf_test,\
 ))
 endif
 
-$(eval $(call gb_CppunitTest_use_sdk_api,vcl_wmf_test))
+$(eval $(call gb_CppunitTest_use_ure,emfio))
 
-$(eval $(call gb_CppunitTest_use_ure,vcl_wmf_test))
-
-$(eval $(call gb_CppunitTest_use_components,vcl_wmf_test,\
+$(eval $(call gb_CppunitTest_use_components,emfio,\
     configmgr/source/configmgr \
     i18npool/util/i18npool \
     ucb/source/core/ucb1 \
     unotools/util/utl \
 ))
 
-$(eval $(call gb_CppunitTest_use_configuration,vcl_wmf_test))
+$(eval $(call gb_CppunitTest_use_configuration,emfio))
 
 # See gb_CppunitTest__use_vcl (solenv/gbuild/CppunitTest.mk; headless):
 ifeq ($(USING_X11),TRUE)
-$(call gb_CppunitTest_get_target,vcl_wmf_test): \
+$(call gb_CppunitTest_get_target,emfio): \
     $(call gb_Library_get_target,desktop_detector)
 endif
 
 # Hack to suppress ASan ODR violation warnings about symbols present in both the
 # vcl objects linked into this test library and the vcl library (which gets
 # dynamically loaded during the test):
-$(call gb_CppunitTest_get_target,vcl_wmf_test): \
+$(call gb_CppunitTest_get_target,emfio): \
     EXTRA_ENV_VARS := \
         ASAN_OPTIONS="$${ASAN_OPTIONS+$$ASAN_OPTIONS:}"detect_odr_violation=0
 
