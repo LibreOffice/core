35619f27de69cb3969e6e58f08516bbf18be4012
Chris Sherlock <chris.sherlock79@gmail.com>
2017-06-24 13:09:03 +1000

    tdf#43157 - osl: convert OSL_PRECONDs in w32 & unx file.cxx
    Change-Id: Ic914d9240e6b06b7e6550fe311fd69e310254bd5

--- sal/osl/unx/file.cxx
+++ sal/osl/unx/file.cxx
@@ -195,7 +195,7 @@ FileHandle_Impl::Allocator::Allocator()
     size_t const pagesize = FileHandle_Impl::getpagesize();
     if (pagesize != size_t(-1))
     {
-        m_cache  = rtl_cache_create (
+        m_cache = rtl_cache_create (
             "osl_file_buffer_cache", pagesize, 0, nullptr, nullptr, nullptr, nullptr, nullptr, 0);
         if (m_cache != nullptr)
             m_bufsiz = pagesize;
@@ -209,7 +209,8 @@ FileHandle_Impl::Allocator::~Allocator()
 
 void FileHandle_Impl::Allocator::allocate (sal_uInt8 ** ppBuffer, size_t * pnSize)
 {
-    OSL_PRECOND((nullptr != ppBuffer) && (nullptr != pnSize), "FileHandle_Impl::Allocator::allocate(): contract violation");
+    SAL_WARN_IF((!ppBuffer) || (!pnSize), "sal.osl", "FileHandle_Impl::Allocator::allocate(): contract violation");
+    assert(ppBuffer && pnSize);
     if ((ppBuffer != nullptr) && (pnSize != nullptr))
     {
         *ppBuffer = static_cast< sal_uInt8* >(rtl_cache_alloc(m_cache));
@@ -345,11 +346,11 @@ oslFileError FileHandle_Impl::readAt (
     size_t       nBytesRequested,
     sal_uInt64 * pBytesRead)
 {
-    OSL_PRECOND((m_state & STATE_SEEKABLE), "FileHandle_Impl::readAt(): not seekable");
+    SAL_WARN_IF(!(m_state & STATE_SEEKABLE), "sal.osl", "FileHandle_Impl::readAt(): not seekable");
     if (!(m_state & STATE_SEEKABLE))
         return osl_File_E_SPIPE;
 
-    OSL_PRECOND((m_state & STATE_READABLE), "FileHandle_Impl::readAt(): not readable");
+    SAL_WARN_IF(!(m_state & STATE_READABLE), "sal.osl", "FileHandle_Impl::readAt(): not readable");
     if (!(m_state & STATE_READABLE))
         return osl_File_E_BADF;
 
@@ -393,11 +394,11 @@ oslFileError FileHandle_Impl::writeAt (
     size_t       nBytesToWrite,
     sal_uInt64 * pBytesWritten)
 {
-    OSL_PRECOND((m_state & STATE_SEEKABLE), "FileHandle_Impl::writeAt(): not seekable");
+    SAL_WARN_IF(!(m_state & STATE_SEEKABLE), "sal.osl", "FileHandle_Impl::writeAt(): not seekable");
     if (!(m_state & STATE_SEEKABLE))
         return osl_File_E_SPIPE;
 
-    OSL_PRECOND((m_state & STATE_WRITEABLE), "FileHandle_Impl::writeAt(): not writeable");
+    SAL_WARN_IF(!(m_state & STATE_WRITEABLE), "sal.osl", "FileHandle_Impl::writeAt(): not writeable");
     if (!(m_state & STATE_WRITEABLE))
         return osl_File_E_BADF;
 
