LibreOffice/core

commit ac2d415a52f22caf0012b7d9b17d015aca27db9d

loplugin:oncevar in sc
Change-Id: Ice59e286debb6bd0eb692f2b2b0c2c5087c069c0
Reviewed-on: https://gerrit.libreoffice.org/39239
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Noel Grandin <noel.grandin@collabora.co.uk>

sc/source/filter/xcl97/xcl97rec.cxx
@@ -1586,9 +1586,8 @@ void XclExpSheetProtectOptions::WriteBody( XclExpStream& rStrm )
     sal_uInt16 nBytes = 0x0867;
     rStrm << nBytes;
 
-    unsigned char nZero = 0x00;
     for (int i = 0; i < 9; ++i)
-        rStrm << nZero;
+        rStrm << 0;
 
     nBytes = 0x0200;
     rStrm << nBytes;
@@ -1612,7 +1611,7 @@ XclExpSheetEnhancedProtection::XclExpSheetEnhancedProtection( const XclExpRoot& 
 
 void XclExpSheetEnhancedProtection::WriteBody( XclExpStream& rStrm )
 {
-    sal_uInt16 nRecordType = 0x0868;
+    sal_uInt16 const nRecordType = 0x0868;
     rStrm << nRecordType;                   // frtHeader rt
     rStrm.WriteZeroBytesToRecord(10);       // frtHeader unused
     rStrm << EXC_ISFPROTECTION;             // isf
@@ -1809,8 +1808,7 @@ void XclExpWriteAccess::WriteBody( XclExpStream& rStrm )
         0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
         0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20 };
 
-    std::size_t nDataSize = sizeof(aData);
-    for (std::size_t i = 0; i < nDataSize; ++i)
+    for (std::size_t i = 0; i < sizeof(aData); ++i)
         rStrm << aData[i];
 }
 

commit 7850e70d5e98bbfeb536d9644b618d0782da30a6

Write 0 of type 'unsigned char'
regression introduced with ac2d415a52f22caf0012b7d9b17d015aca27db9d
"loplugin:oncevar in sc"

Change-Id: I1073e7e8b67ddf1dd633f004322c6bc46b9a8052

sc/source/filter/xcl97/xcl97rec.cxx
@@ -1587,7 +1587,7 @@ void XclExpSheetProtectOptions::WriteBody( XclExpStream& rStrm )
     rStrm << nBytes;
 
     for (int i = 0; i < 9; ++i)
-        rStrm << sal_Int32(0);
+        rStrm << static_cast<unsigned char>(0);
 
     nBytes = 0x0200;
     rStrm << nBytes;