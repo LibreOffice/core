df186eddf48172e4b9a36b00bcf2f62250197770
Mike Kaganski <mike.kaganski@collabora.com>
2017-06-27 07:50:12 +0300

    tdf#108806: convert CRLF into space in OOXML text
    Change-Id: I8e2e108a705ecdb55c096a589d83d51c48b0b83c

--- sw/qa/extras/ooxmlimport/ooxmlimport.cxx
+++ sw/qa/extras/ooxmlimport/ooxmlimport.cxx
@@ -1291,6 +1291,16 @@ DECLARE_OOXMLIMPORT_TEST(testTdf108408, "tdf108408.docx")
     CPPUNIT_ASSERT_EQUAL(double(20), getProperty<double>(xRun, "CharHeight"));
 }
 
+DECLARE_OOXMLIMPORT_TEST(testTdf108806, "tdf108806.docx")
+{
+    // tdf#108806:The CRLF in the text contents of XML must be converted to single spaces.
+    CPPUNIT_ASSERT_EQUAL(1, getParagraphs());
+    uno::Reference< text::XTextRange > paragraph = getParagraph(1);
+    CPPUNIT_ASSERT_EQUAL(
+        OUString("First part of a line (before CRLF). Second part of the same line (after CRLF)."),
+        paragraph->getString());
+}
+
 DECLARE_OOXMLIMPORT_TEST(testVmlAdjustments, "vml-adjustments.docx")
 {
     uno::Reference<beans::XPropertySet> xPropertySet(getShape(1), uno::UNO_QUERY);
