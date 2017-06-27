6d5d5cc3fd94d051eeed82653cf4ba6f22ee097b
Christian Barth <Christian.Barth@zoho.com>
2017-06-27 20:56:57 +0200

    tdf#107880: Revert change to ScopedVclPtr<VirtualDevice>
    Revert for change to ScopedVclPtr as this would massivly change
    behavior.
    
    Change-Id: I43105c16ea2ff2a0aaf0424252288f0b971657f0
    Signed-off-by: Christian Barth <Christian.Barth@zoho.com>

--- vcl/source/gdi/impanmvw.cxx
+++ vcl/source/gdi/impanmvw.cxx
@@ -206,7 +206,7 @@ void ImplAnimView::draw( sal_uLong nPos, VirtualDevice* pVDev )
         setMarked( true );
     else if( !mbPause )
     {
-        ScopedVclPtr<VirtualDevice>   pDev;
+        VclPtr<VirtualDevice>   pDev;
         Point                   aPosPix;
         Point                   aBmpPosPix;
         Size                    aSizePix;
@@ -300,6 +300,8 @@ void ImplAnimView::draw( sal_uLong nPos, VirtualDevice* pVDev )
                 pRenderContext->SetClipRegion(*xOldClip);
                 xOldClip.reset();
             }
+
+            pDev.disposeAndClear();
 
             if( pRenderContext->GetOutDevType() == OUTDEV_WINDOW )
                 static_cast<vcl::Window*>( pRenderContext.get() )->Flush();
