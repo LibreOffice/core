c08dd17baf7ea63f10d5311e90c5c6a4c48aac20
Jens Carl <j.carl43@gmx.de>
2017-06-28 08:09:33 +0000

    tdf#45904 Move Java XCellAddressable test to C++
    Change-Id: Ib68c7dd8087f19bd969cf5273c3a6e8664c6bb62
    Reviewed-on: https://gerrit.libreoffice.org/39348
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Markus Mohrhard <markus.mohrhard@googlemail.com>

--- sc/Module_sc.mk
+++ sc/Module_sc.mk
@@ -121,6 +121,7 @@ $(eval $(call gb_Module_add_subsequentcheck_targets,sc,\
 	CppunitTest_sc_check_data_pilot_field \
 	CppunitTest_sc_check_data_pilot_table \
 	CppunitTest_sc_viewpaneobj \
+	CppunitTest_sc_cellobj \
 	CppunitTest_sc_cellcursorobj \
 ))
 
