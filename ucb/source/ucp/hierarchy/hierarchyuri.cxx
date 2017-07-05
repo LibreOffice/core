777fbce9a3fb6924b9481a05130838cb5ffe2bb0
Stephan Bergmann <sbergman@redhat.com>
2017-07-05 15:29:30 +0200

    loplugin:unnecessaryparen
    Change-Id: Id1cec62e84c3ddfe479c0e77212bc87e64c00aa6

--- ucb/source/ucp/hierarchy/hierarchyuri.cxx
+++ ucb/source/ucp/hierarchy/hierarchyuri.cxx
@@ -49,7 +49,7 @@ void HierarchyUri::init() const
         m_aName.clear();
 
         // URI must match at least: <sheme>:
-        if ( ( m_aUri.getLength() < HIERARCHY_URL_SCHEME_LENGTH + 1 ) )
+        if ( m_aUri.getLength() < HIERARCHY_URL_SCHEME_LENGTH + 1 )
         {
             // error, but remember that we did a init().
             m_aPath = "/";
