LibreOffice/core

commit f05d0d05829dd51cb9d8071ac97cc219779ee40a

loplugin:unusedfields in sw part1
Change-Id: Iabbf7f09c0d92b488d7e03f4436f077c5e6e87aa
Reviewed-on: https://gerrit.libreoffice.org/39039
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sw/source/ui/fmtui/tmpdlg.cxx
@@ -83,15 +83,11 @@ SwTemplateDlg::SwTemplateDlg(vcl::Window* pParent,
     , bNewStyle(bNew)
     , m_nIndentsId(0)
     , m_nAlignId(0)
-    , m_nTextFlowId(0)
-    , m_nAsianTypo(0)
     , m_nFontId(0)
     , m_nFontEffectId(0)
     , m_nPositionId(0)
     , m_nAsianLayoutId(0)
-    , m_nTabId(0)
     , m_nOutlineId(0)
-    , m_nDropCapsId(0)
     , m_nBackgroundId(0)
     , m_nAreaId(0)
     , m_nTransparenceId(0)
@@ -105,12 +101,8 @@ SwTemplateDlg::SwTemplateDlg(vcl::Window* pParent,
     , m_nHeaderId(0)
     , m_nFooterId(0)
     , m_nPageId(0)
-    , m_nFootNoteId(0)
-    , m_nTextGridId(0)
-    , m_nSingleId(0)
     , m_nBulletId(0)
     , m_nNumId(0)
-    , m_nBmpId(0)
     , m_nNumOptId(0)
     , m_nNumPosId(0)
 {
@@ -165,11 +157,11 @@ SwTemplateDlg::SwTemplateDlg(vcl::Window* pParent,
 
             OSL_ENSURE(pFact->GetTabPageCreatorFunc(RID_SVXPAGE_EXT_PARAGRAPH), "GetTabPageCreatorFunc fail!");
             OSL_ENSURE(pFact->GetTabPageRangesFunc(RID_SVXPAGE_EXT_PARAGRAPH), "GetTabPageRangesFunc fail!");
-            m_nTextFlowId = AddTabPage("textflow", pFact->GetTabPageCreatorFunc(RID_SVXPAGE_EXT_PARAGRAPH),        pFact->GetTabPageRangesFunc(RID_SVXPAGE_EXT_PARAGRAPH) );
+            AddTabPage("textflow", pFact->GetTabPageCreatorFunc(RID_SVXPAGE_EXT_PARAGRAPH),        pFact->GetTabPageRangesFunc(RID_SVXPAGE_EXT_PARAGRAPH) );
 
             OSL_ENSURE(pFact->GetTabPageCreatorFunc(RID_SVXPAGE_PARA_ASIAN), "GetTabPageCreatorFunc fail!");
             OSL_ENSURE(pFact->GetTabPageRangesFunc(RID_SVXPAGE_PARA_ASIAN), "GetTabPageRangesFunc fail!");
-            m_nAsianTypo = AddTabPage("asiantypo",  pFact->GetTabPageCreatorFunc(RID_SVXPAGE_PARA_ASIAN),       pFact->GetTabPageRangesFunc(RID_SVXPAGE_PARA_ASIAN) );
+            AddTabPage("asiantypo",  pFact->GetTabPageCreatorFunc(RID_SVXPAGE_PARA_ASIAN),       pFact->GetTabPageRangesFunc(RID_SVXPAGE_PARA_ASIAN) );
 
             OSL_ENSURE(pFact->GetTabPageCreatorFunc( RID_SVXPAGE_CHAR_NAME ), "GetTabPageCreatorFunc fail!");
             OSL_ENSURE(pFact->GetTabPageRangesFunc( RID_SVXPAGE_CHAR_NAME ), "GetTabPageRangesFunc fail!");
@@ -193,12 +185,10 @@ SwTemplateDlg::SwTemplateDlg(vcl::Window* pParent,
 
             OSL_ENSURE(pFact->GetTabPageCreatorFunc(RID_SVXPAGE_TABULATOR), "GetTabPageCreatorFunc fail!");
             OSL_ENSURE(pFact->GetTabPageRangesFunc(RID_SVXPAGE_TABULATOR), "GetTabPageRangesFunc fail!");
-            m_nTabId = AddTabPage("tabs", pFact->GetTabPageCreatorFunc(RID_SVXPAGE_TABULATOR),        pFact->GetTabPageRangesFunc(RID_SVXPAGE_TABULATOR) );
+            AddTabPage("tabs", pFact->GetTabPageCreatorFunc(RID_SVXPAGE_TABULATOR),        pFact->GetTabPageRangesFunc(RID_SVXPAGE_TABULATOR) );
 
-            m_nOutlineId = AddTabPage("outline", SwParagraphNumTabPage::Create,
-                                    SwParagraphNumTabPage::GetRanges);
-            m_nDropCapsId = AddTabPage("dropcaps", SwDropCapsPage::Create,
-                           SwDropCapsPage::GetRanges );
+            AddTabPage("outline", SwParagraphNumTabPage::Create, SwParagraphNumTabPage::GetRanges);
+            AddTabPage("dropcaps", SwDropCapsPage::Create, SwDropCapsPage::GetRanges );
 
             // add Area and Transparence TabPages
             m_nAreaId = AddTabPage("area", pFact->GetTabPageCreatorFunc( RID_SVXPAGE_AREA ), pFact->GetTabPageRangesFunc( RID_SVXPAGE_AREA ));
@@ -289,12 +279,8 @@ SwTemplateDlg::SwTemplateDlg(vcl::Window* pParent,
                 m_nBorderId = AddTabPage("borders",
                                 pFact->GetTabPageCreatorFunc( RID_SVXPAGE_BORDER ), pFact->GetTabPageRangesFunc( RID_SVXPAGE_BORDER ) );
                 m_nColumnId = AddTabPage("columns", SwColumnPage::Create, SwColumnPage::GetRanges );
-                m_nFootNoteId = AddTabPage("footnotes",
-                                                SwFootNotePage::Create,
-                                                SwFootNotePage::GetRanges );
-                m_nTextGridId = AddTabPage("textgrid",
-                                                SwTextGridPage::Create,
-                                                SwTextGridPage::GetRanges );
+                AddTabPage("footnotes", SwFootNotePage::Create, SwFootNotePage::GetRanges );
+                AddTabPage("textgrid", SwTextGridPage::Create, SwTextGridPage::GetRanges );
                 SvtCJKOptions aCJKOptions;
                 if(!aCJKOptions.IsAsianTypographyEnabled())
                     RemoveTabPage("textgrid");
@@ -311,10 +297,10 @@ SwTemplateDlg::SwTemplateDlg(vcl::Window* pParent,
         // numbering styles
         case SfxStyleFamily::Pseudo:
         {
-            m_nSingleId = AddTabPage("numbering", RID_SVXPAGE_PICK_SINGLE_NUM);
+            AddTabPage("numbering", RID_SVXPAGE_PICK_SINGLE_NUM);
             m_nBulletId = AddTabPage("bullets", RID_SVXPAGE_PICK_BULLET);
             m_nNumId = AddTabPage("outline", RID_SVXPAGE_PICK_NUM);
-            m_nBmpId = AddTabPage("graphics", RID_SVXPAGE_PICK_BMP);
+            AddTabPage("graphics", RID_SVXPAGE_PICK_BMP);
             m_nNumOptId = AddTabPage("customize", RID_SVXPAGE_NUM_OPTIONS );
             m_nNumPosId = AddTabPage("position", RID_SVXPAGE_NUM_POSITION );
         }

sw/inc/view.hxx
@@ -607,9 +607,9 @@ public:
 
     // store MailMerge data while "Back to Mail Merge Wizard" FloatingWindow is active
     // or to support printing
-    void SetMailMergeConfigItem(std::shared_ptr<SwMailMergeConfigItem>& rConfigItem, sal_uInt16 nRestart);
+    void SetMailMergeConfigItem(std::shared_ptr<SwMailMergeConfigItem>& rConfigItem);
     std::shared_ptr<SwMailMergeConfigItem> GetMailMergeConfigItem() const;
-    std::shared_ptr<SwMailMergeConfigItem> EnsureMailMergeConfigItem(const SfxItemSet* pArgs = nullptr, sal_uInt16 nWizardRestartPage = 0);
+    std::shared_ptr<SwMailMergeConfigItem> EnsureMailMergeConfigItem(const SfxItemSet* pArgs = nullptr);
 
     void ExecFormatPaintbrush(SfxRequest &);
     void StateFormatPaintbrush(SfxItemSet &);

sw/source/ui/dbui/mmresultdialogs.cxx
@@ -828,7 +828,7 @@ IMPL_LINK(SwMMResultPrintDialog, PrintHdl_Impl, Button*, pButton, void)
     sPages += " - ";
     sPages += OUString::number(documentEndPageNumber(xConfigItem.get(), nEnd - 1));
 
-    pTargetView->SetMailMergeConfigItem(xConfigItem, 0);
+    pTargetView->SetMailMergeConfigItem(xConfigItem);
     if(m_pTempPrinter)
     {
         SfxPrinter *const pDocumentPrinter = pTargetView->GetWrtShell()

sw/source/ui/index/cnttab.cxx
@@ -298,7 +298,7 @@ SwMultiTOXTabDialog::SwMultiTOXTabDialog(vcl::Window* pParent, const SfxItemSet&
     SfxAbstractDialogFactory* pFact = SfxAbstractDialogFactory::Create();
     OSL_ENSURE(pFact, "Dialog creation failed!");
     m_nSelectId = AddTabPage("index", SwTOXSelectTabPage::Create, nullptr);
-    m_nStylesId = AddTabPage("styles", SwTOXStylesTabPage::Create, nullptr);
+    AddTabPage("styles", SwTOXStylesTabPage::Create, nullptr);
     m_nColumnId = AddTabPage("columns", SwColumnPage::Create, nullptr);
     m_nBackGroundId = AddTabPage("background", pFact->GetTabPageCreatorFunc( RID_SVXPAGE_BACKGROUND ), nullptr);
     m_nEntriesId = AddTabPage("entries", SwTOXEntryTabPage::Create, nullptr);

sw/source/uibase/app/apphdl.cxx
@@ -245,7 +245,7 @@ void SwModule::StateOther(SfxItemSet &rSet)
 // start field dialog
 void NewXForms( SfxRequest& rReq ); // implementation: below
 
-std::shared_ptr<SwMailMergeConfigItem> SwView::EnsureMailMergeConfigItem(const SfxItemSet* pArgs, sal_uInt16 nWizardRestartPage)
+std::shared_ptr<SwMailMergeConfigItem> SwView::EnsureMailMergeConfigItem(const SfxItemSet* pArgs)
 {
     // create if it does not exist yet
     std::shared_ptr<SwMailMergeConfigItem> xMMConfig = GetMailMergeConfigItem();
@@ -302,7 +302,7 @@ std::shared_ptr<SwMailMergeConfigItem> SwView::EnsureMailMergeConfigItem(const S
             }
         }
 
-        SetMailMergeConfigItem(xMMConfig, nWizardRestartPage);
+        SetMailMergeConfigItem(xMMConfig);
     }
     return xMMConfig;
 }
@@ -448,7 +448,7 @@ void SwMailMergeWizardExecutor::ExecuteMailMergeWizard( const SfxItemSet * pArgs
     acquire();
 
     // create if it does not exist yet
-    std::shared_ptr<SwMailMergeConfigItem> xMMConfig = m_pView->EnsureMailMergeConfigItem(pArgs, m_pWizard ? m_pWizard->GetRestartPage() : 0);
+    std::shared_ptr<SwMailMergeConfigItem> xMMConfig = m_pView->EnsureMailMergeConfigItem(pArgs);
 
     SwAbstractDialogFactory* pFact = SwAbstractDialogFactory::Create();
     m_pWizard = pFact->CreateMailMergeWizard(*m_pView, xMMConfig);

sw/source/uibase/inc/SwXFilterOptions.hxx
@@ -41,7 +41,6 @@ class SwXFilterOptions : public ::cppu::WeakImplHelper<
 
     css::uno::Reference< css::io::XInputStream > xInputStream;
     css::uno::Reference< css::lang::XComponent > xModel;
-    bool            bExport;
 
 public:
                             SwXFilterOptions();

sw/source/uibase/inc/navipi.hxx
@@ -94,7 +94,6 @@ class SwNavigationPI : public PanelLayout,
     Size        m_aExpandedSize;
 
     bool    m_bIsZoomedIn : 1;
-    bool    m_bPageCtrlsVisible : 1;
     bool    m_bGlobalMode : 1;
 
     bool IsZoomedIn() const {return m_bIsZoomedIn;}

sw/source/uibase/inc/swuicnttab.hxx
@@ -69,7 +69,6 @@ class SwMultiTOXTabDialog : public SfxTabDialog
     SwWrtShell&             m_rWrtShell;
 
     sal_uInt16              m_nSelectId;
-    sal_uInt16              m_nStylesId;
     sal_uInt16              m_nColumnId;
     sal_uInt16              m_nBackGroundId;
     sal_uInt16              m_nEntriesId;

commit 480ba09f17e4a07ca0f1a498467514aacf5ce681

tdf#108829 Cannot set a numbering style to a paragraph style
regression from
    commit f05d0d05829dd51cb9d8071ac97cc219779ee40a
    loplugin:unusedfields in sw part1

Change-Id: Ic7049c5288b2ec875936be0784611f5c7e6a4788

sw/source/ui/fmtui/tmpdlg.cxx
@@ -187,7 +187,7 @@ SwTemplateDlg::SwTemplateDlg(vcl::Window* pParent,
             OSL_ENSURE(pFact->GetTabPageRangesFunc(RID_SVXPAGE_TABULATOR), "GetTabPageRangesFunc fail!");
             AddTabPage("tabs", pFact->GetTabPageCreatorFunc(RID_SVXPAGE_TABULATOR),        pFact->GetTabPageRangesFunc(RID_SVXPAGE_TABULATOR) );
 
-            AddTabPage("outline", SwParagraphNumTabPage::Create, SwParagraphNumTabPage::GetRanges);
+            m_nOutlineId = AddTabPage("outline", SwParagraphNumTabPage::Create, SwParagraphNumTabPage::GetRanges);
             AddTabPage("dropcaps", SwDropCapsPage::Create, SwDropCapsPage::GetRanges );
 
             // add Area and Transparence TabPages