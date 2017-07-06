46ba298934d1765365018349f26f8a732cefba63
Jens Carl <j.carl43@gmx.de>
2017-07-06 07:33:52 +0000

    tdf#45904 Remove/disable obsolete _XSheetAnnotations Java tests
    The _XSheetAnnotations test is already written in C++ since commit
    654e275fc673b28ac264f10282a4cd3a9f61a45d.
    
    Change-Id: Ie4d8459f503289cd9fd1c52b9121377826921bc3
    Reviewed-on: https://gerrit.libreoffice.org/39619
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Markus Mohrhard <markus.mohrhard@googlemail.com>

--- qadevOOo/Jar_OOoRunner.mk
+++ qadevOOo/Jar_OOoRunner.mk
@@ -630,7 +630,6 @@ $(eval $(call gb_Jar_add_sourcefiles,OOoRunner,\
     qadevOOo/tests/java/ifc/sheet/_XScenarios \
     qadevOOo/tests/java/ifc/sheet/_XScenariosSupplier \
     qadevOOo/tests/java/ifc/sheet/_XSheetAnnotationAnchor \
-    qadevOOo/tests/java/ifc/sheet/_XSheetAnnotations \
     qadevOOo/tests/java/ifc/sheet/_XSheetAnnotationsSupplier \
     qadevOOo/tests/java/ifc/sheet/_XSheetAuditing \
     qadevOOo/tests/java/ifc/sheet/_XSheetCellCursor \
