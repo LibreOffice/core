c8c4f13e6410ce48976e54ec7ecf67dbe25363f0
Justin Luth <justin_luth@sil.org>
2017-07-06 09:46:06 -0400

    tdf#77219 writerfilter: allow shapes to be in front of text
    For LO4.3, commit 2496eaa5c4535b88b44e4ac034aae6af0c08de0e
    forced every shape to be "in background". That is not correct.
    Word appears to use negative z-indexes to indicate behind-text
    zordering and positive numbers for in-front placement.
    
    Change-Id: I4083ae67ef76152e1c0a894d810950d249ac13b4

--- writerfilter/source/dmapper/DomainMapper_Impl.cxx
+++ writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -2022,6 +2022,7 @@ void DomainMapper_Impl::PushShapeContext( const uno::Reference< drawing::XShape 
                         xShapePropertySet->setPropertyValue( "ZOrder", uno::makeAny(pZOrderHelper->findZOrder(zOrder)));
                         pZOrderHelper->addItem(xShapePropertySet, zOrder);
                         checkZOrderStatus = true;
+
                     }
                     if(checkBtLrStatus && checkZOrderStatus)
                         break;
@@ -2067,7 +2068,7 @@ void DomainMapper_Impl::PushShapeContext( const uno::Reference< drawing::XShape 
                         aGrabBag[i].Value >>= zOrder;
                         xShapePropertySet->setPropertyValue( "ZOrder", uno::makeAny(pZOrderHelper->findZOrder(zOrder)));
                         pZOrderHelper->addItem(xShapePropertySet, zOrder);
-                        xShapePropertySet->setPropertyValue(getPropertyName( PROP_OPAQUE ), uno::makeAny( false ) );
+                        xShapePropertySet->setPropertyValue(getPropertyName( PROP_OPAQUE ), uno::makeAny( zOrder >= 0 ) );
                         checkZOrderStatus = true;
                     }
                     else if ( aGrabBag[i].Name == "TxbxHasLink" )
