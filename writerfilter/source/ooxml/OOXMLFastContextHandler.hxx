f9ef58cd0efe9d51447c0f2a8a29fe1cb22ce30b
Mike Kaganski <mike.kaganski@collabora.com>
2017-07-07 11:33:34 +0300

    tdf#108995: take xml:space attribute into account
    See paragraph 2.10 of XML 1.0 specification and 17.3.3.31 of ECMA-376-1:2016
    
    Change-Id: I7f19d3b9cf2ccce88a5fa03022beeb99facc04fe

--- writerfilter/source/ooxml/OOXMLFastContextHandler.hxx
+++ writerfilter/source/ooxml/OOXMLFastContextHandler.hxx
@@ -221,6 +221,9 @@ protected:
     void startAction();
     void endAction();
 
+    // 2.10 of XML 1.0 specification
+    virtual bool IsPreserveSpace() const;
+
     const css::uno::Reference< css::uno::XComponentContext >& getComponentContext() { return m_xContext;}
 
     bool inPositionV;
@@ -253,8 +256,13 @@ public:
 
     void handleHyperlink();
 
+protected:
+    virtual bool IsPreserveSpace() const override;
+
 private:
     mutable OOXMLPropertySet::Pointer_t mpPropertySetAttrs;
+    bool mbPreserveSpace    : 1;
+    bool mbPreserveSpaceSet : 1;
 };
 
 class OOXMLFastContextHandlerProperties : public OOXMLFastContextHandler
