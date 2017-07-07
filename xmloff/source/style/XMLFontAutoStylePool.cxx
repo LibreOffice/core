317a931018c420b23feb351b8526f244ba4e987b
Arnaud Versini <arnaud.versini@gmail.com>
2017-07-07 10:38:11 +0200

    Make xmloff build again.
    Change-Id: I8b2266b08881348ba0f2d590bdc99a447ff02b7b

--- xmloff/source/style/XMLFontAutoStylePool.cxx
+++ xmloff/source/style/XMLFontAutoStylePool.cxx
@@ -17,6 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
+#include <functionnal>
 #include <o3tl/sorted_vector.hxx>
 #include <tools/fontenum.hxx>
 #include <xmloff/xmlnmspe.hxx>
