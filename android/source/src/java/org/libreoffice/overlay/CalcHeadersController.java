61d4b9f76d0f69b5c062311f13ff66b94f0595e8
Ximeng Zu <uznomis@yahoo.com>
2017-07-12 09:20:56 -0500

    [Android Viewer] Add header funcs to Calc
    Added insert/delete/hide/show/optimal width or height
    /adjust width or heigth  to Calc. These options
    show in a floating menu near the headers when the
    user taps on the headers. Also added selection on
    headers, i.e., user can drag on headers to select
    multiple columns/rows.
    
    Change-Id: I7e1994d1fa81d80c110def035c2c065e838b49ac

--- android/source/src/java/org/libreoffice/overlay/CalcHeadersController.java
+++ android/source/src/java/org/libreoffice/overlay/CalcHeadersController.java
@@ -1,8 +1,18 @@
 package org.libreoffice.overlay;
 
+import android.content.Context;
+import android.graphics.PointF;
 import android.graphics.RectF;
+import android.graphics.drawable.ColorDrawable;
+import android.support.design.widget.Snackbar;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.PopupWindow;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -39,8 +49,111 @@ public class CalcHeadersController {
             @Override
             public void onClick(View v) {
                 LOKitShell.sendEvent(new LOEvent(LOEvent.UNO_COMMAND, ".uno:SelectAll"));
+                if (mCalcColumnHeadersView == null) return;
+                mCalcColumnHeadersView.showHeaderPopup(new PointF());
             }
         });
+    }
+
+    public void setupHeaderPopupView() {
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        String[] rowOrColumn = {"Row","Column"};
+        CalcHeadersView[] headersViews= {mCalcRowHeadersView, mCalcColumnHeadersView};
+        for (int i = 0; i < rowOrColumn.length; i++) {
+            // create popup window
+            final String tempName = rowOrColumn[i];
+            final CalcHeadersView tempView = headersViews[i];
+            final View headerPopupView = inflater.inflate(R.layout.calc_header_popup, null);
+            final PopupWindow popupWindow = new PopupWindow(headerPopupView, LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+            popupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+                @Override
+                public void onDismiss() {
+                    headerPopupView.findViewById(R.id.calc_header_popup_optimal_length_dialog).setVisibility(View.GONE);
+                    popupWindow.setFocusable(false);
+                }
+            });
+            popupWindow.setOutsideTouchable(true);
+            popupWindow.setBackgroundDrawable(new ColorDrawable());
+            popupWindow.setAnimationStyle(android.R.style.Animation_Dialog);
+            tempView.setHeaderPopupWindow(popupWindow);
+            // set up child views in the popup window
+            headerPopupView.findViewById(R.id.calc_header_popup_insert).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    LOKitShell.sendEvent(new LOEvent(LOEvent.UNO_COMMAND, ".uno:Insert"+tempName+"s"));
+                    tempView.dismissPopupWindow();
+                    mContext.setDocumentChanged(true);
+                }
+            });
+            headerPopupView.findViewById(R.id.calc_header_popup_delete).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    LOKitShell.sendEvent(new LOEvent(LOEvent.UNO_COMMAND, ".uno:Delete"+tempName+"s"));
+                    tempView.dismissPopupWindow();
+                    mContext.setDocumentChanged(true);
+                }
+            });
+            headerPopupView.findViewById(R.id.calc_header_popup_hide).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    LOKitShell.sendEvent(new LOEvent(LOEvent.UNO_COMMAND, ".uno:Hide"+tempName));
+                    tempView.dismissPopupWindow();
+                    mContext.setDocumentChanged(true);
+                }
+            });
+            headerPopupView.findViewById(R.id.calc_header_popup_show).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    LOKitShell.sendEvent(new LOEvent(LOEvent.UNO_COMMAND, ".uno:Show"+tempName));
+                    tempView.dismissPopupWindow();
+                    mContext.setDocumentChanged(true);
+                }
+            });
+            headerPopupView.findViewById(R.id.calc_header_popup_optimal_length).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    View view = headerPopupView.findViewById(R.id.calc_header_popup_optimal_length_dialog);
+                    if (view.getVisibility() == View.VISIBLE) {
+                        view.setVisibility(View.GONE);
+                        popupWindow.setFocusable(false);
+                        popupWindow.update();
+                    } else {
+                        popupWindow.dismiss();
+                        view.setVisibility(View.VISIBLE);
+                        popupWindow.setFocusable(true);
+                        popupWindow.showAtLocation(tempView, Gravity.CENTER, 0, 0);
+                        LOKitShell.getMainHandler().post(new Runnable() {
+                            @Override
+                            public void run() {
+                                Snackbar.make(tempView, R.string.calc_alert_double_click_optimal_length, Snackbar.LENGTH_LONG).show();
+                            }
+                        });
+                    }
+                }
+            });
+            headerPopupView.findViewById(R.id.calc_header_popup_optimal_length_button).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String text = ((EditText)headerPopupView.findViewById(R.id.calc_header_popup_optimal_length_text)).getText().toString();
+                    tempView.sendOptimalLengthRequest(text);
+                    tempView.dismissPopupWindow();
+                    mContext.setDocumentChanged(true);
+                }
+            });
+            headerPopupView.findViewById(R.id.calc_header_popup_adjust_length).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mContext.getDocumentOverlay().showAdjustLengthLine(tempView == mCalcRowHeadersView, tempView);
+                    tempView.dismissPopupWindow();
+                    mContext.setDocumentChanged(true);
+                }
+            });
+            ((Button)headerPopupView.findViewById(R.id.calc_header_popup_adjust_length))
+                    .setText(tempView == mCalcRowHeadersView ? R.string.calc_adjust_height : R.string.calc_adjust_width);
+            ((Button)headerPopupView.findViewById(R.id.calc_header_popup_optimal_length))
+                    .setText(tempView == mCalcRowHeadersView ? R.string.calc_optimal_height : R.string.calc_optimal_width);
+
+        }
     }
 
     public void setHeaders(String headers) {
