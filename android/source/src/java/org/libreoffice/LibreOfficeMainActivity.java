2cba4ea9672f1c308f7c5fa09ca775b77cdebf53
Ximeng Zu <uznomis@yahoo.com>
2017-07-11 22:34:00 -0500

    [Android Viewer] Fix crash after save dialog
    Fixed the crash after save dialog caused by
    a possible null pointer related to presentation
    mode.
    
    Change-Id: I2fcf254f4382eaa5b565e25679486a5158d56eac
    Reviewed-on: https://gerrit.libreoffice.org/39847
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

--- android/source/src/java/org/libreoffice/LibreOfficeMainActivity.java
+++ android/source/src/java/org/libreoffice/LibreOfficeMainActivity.java
@@ -414,7 +414,7 @@ public class LibreOfficeMainActivity extends AppCompatActivity implements Settin
                 // noinspection ResultOfMethodCallIgnored
                 mTempFile.delete();
             }
-            if (mTempSlideShowFile.exists()) {
+            if (mTempSlideShowFile != null && mTempSlideShowFile.exists()) {
                 // noinspection ResultOfMethodCallIgnored
                 mTempSlideShowFile.delete();
             }
