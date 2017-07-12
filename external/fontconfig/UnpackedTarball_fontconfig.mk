cb1df642538dd023a0e0ed3ba8f9733df8189dec
David Tardon <dtardon@redhat.com>
2017-07-12 08:41:54 +0200

    upload fontconfig 2.12.4
    Reviewed-on: https://gerrit.libreoffice.org/39850
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: David Tardon <dtardon@redhat.com>
    (cherry picked from commit fc8badada7b9946dc9987238e3e90737d64a3830)
    
    Change-Id: I151ce38c094d208c113cf0bd8a9f5322891d7bcd
    (cherry picked from commit e1103156e11c968992c398a0d0fddc5a6d18e08b)

--- external/fontconfig/UnpackedTarball_fontconfig.mk
+++ external/fontconfig/UnpackedTarball_fontconfig.mk
@@ -13,8 +13,6 @@ $(eval $(call gb_UnpackedTarball_set_tarball,fontconfig,$(FONTCONFIG_TARBALL),,f
 
 $(eval $(call gb_UnpackedTarball_add_patches,fontconfig,\
 	external/fontconfig/fontconfig-2.12.1.patch.1 \
-	external/fontconfig/fontconfig-glibc-limits.patch.1 \
-	external/fontconfig/0001-Fix-the-build-issue-with-gperf-3.1.patch.1 \
 ))
 
 # vim: set noet sw=4 ts=4:
