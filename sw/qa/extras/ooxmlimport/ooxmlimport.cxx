b85c798c7f5cd85f814122c76b3bb4f13f8b54be
Tamás Zolnai <tamas.zolnai@collabora.com>
2017-07-12 16:07:10 +0200

    tdf#109053: DOCX: Multipage table is not imported properly
    An other use case when converting to a "floating table" is
    not a good idea. In this case we can check whether next to
    the table anything fits in the text area. If not then we
    can avoid floating table conversion.
    
    Reviewed-on: https://gerrit.libreoffice.org/39811
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Tamás Zolnai <tamas.zolnai@collabora.com>
    (cherry picked from commit fc55711f01af172eb3a034454405fa941454c781)
    
    Change-Id: I798a2f4c7a9dfe6aecbe4a73e3162b49ea5f0adc
    Reviewed-on: https://gerrit.libreoffice.org/39930
    Reviewed-by: Andras Timar <andras.timar@collabora.com>
    Tested-by: Andras Timar <andras.timar@collabora.com>

--- sw/qa/extras/ooxmlimport/ooxmlimport.cxx
+++ sw/qa/extras/ooxmlimport/ooxmlimport.cxx
@@ -1484,6 +1484,14 @@ DECLARE_OOXMLIMPORT_TEST(testTdf108545_embeddedDocxIcon, "tdf108545_embeddedDocx
     CPPUNIT_ASSERT_EQUAL(embed::Aspects::MSOLE_ICON, xSupplier->getAspect());
 }
 
+
+DECLARE_OOXMLIMPORT_TEST(testTdf109053, "tdf109053.docx")
+{
+    // Table was imported into a text frame which led to a one page document
+    // Originally the table takes two pages, so Writer should import it accordingly.
+    CPPUNIT_ASSERT_EQUAL(getPages(), 2);
+}
+
 // tests should only be added to ooxmlIMPORT *if* they fail round-tripping in ooxmlEXPORT
 
 CPPUNIT_PLUGIN_IMPLEMENT();
