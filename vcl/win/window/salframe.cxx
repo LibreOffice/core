3c727d5d7ce782c4642cf8d58950229e44f93600
Mike Kaganski <mike.kaganski@collabora.com>
2017-07-13 08:55:27 +0200

    tdf#109085: allow correct shutdown when user logs off on Windows
    When system shutdown/user logoff happens, Windows sends two
    messages: WM_QUERYENDSESSION and WM_ENDSESSION. The first allows
    application to stop ending the session. The second notifies the
    application of the actual result (i.e. if the session is actually
    ending, or if some application prevented the shutdown).
    
    After the latter, application doesn't get any normal shutdown
    messages (e.g. WM_CLOSE). As we don't do any cleanup in WM_ENDSESSION
    handler, this ends up in program improperly terminated, telling
    users that their files need repair, etc.
    
    Microsoft requires both end session messages to be processed as fast
    as possible. There is 5-second built-in delay, after which Windows
    asks user to terminate applications that prevent the shutdown.
    
    But we must close properly, so this patch handles both messages
    (WM_QUERYENDSESSION and WM_ENDSESSION) in SalComWndProc to handle
    cases when for some reason, there were no SALFRAME windows open,
    and emulates the normal shutdown sequence there. It terminates the
    application after that by exit(0).
    
    See:
    https://msdn.microsoft.com/en-us/library/windows/desktop/aa376890
    https://msdn.microsoft.com/en-us/library/windows/desktop/aa376889
    
    Change-Id: Ibf389b5a7a0272572b0dec6f567428e7395ee0b6

--- vcl/win/window/salframe.cxx
+++ vcl/win/window/salframe.cxx
@@ -4029,18 +4029,6 @@ static void ImplHandleCloseMsg( HWND hWnd )
         }
 }
 
-static long ImplHandleShutDownMsg( HWND hWnd )
-{
-    long nRet = 0;
-    WinSalFrame* pFrame = ProcessOrDeferMessage( hWnd, 0, 0, DeferPolicy::Blocked );
-    if ( pFrame )
-    {
-        nRet = pFrame->CallCallback( SalEvent::Shutdown, nullptr );
-        ImplSalYieldMutexRelease();
-    }
-    return nRet;
-}
-
 static void ImplHandleSettingsChangeMsg( HWND hWnd, UINT nMsg,
                                          WPARAM wParam, LPARAM lParam )
 {
@@ -5447,7 +5435,6 @@ LRESULT CALLBACK SalFrameWndProc( HWND hWnd, UINT nMsg, WPARAM wParam, LPARAM lP
 {
     LRESULT     nRet = 0;
     static int  bInWheelMsg = FALSE;
-    static int  bInQueryEnd = FALSE;
 
     // By WM_CRETAE we connect the frame with the window handle
     if ( nMsg == WM_CREATE )
@@ -5690,41 +5677,9 @@ LRESULT CALLBACK SalFrameWndProc( HWND hWnd, UINT nMsg, WPARAM wParam, LPARAM lP
             break;
 
         case WM_QUERYENDSESSION:
-            if( !bInQueryEnd )
-            {
-                // handle queryendsession only once
-                bInQueryEnd = TRUE;
-                nRet = LRESULT(!ImplHandleShutDownMsg( hWnd ));
-                rDef = FALSE;
-
-                // Issue #16314#: ImplHandleShutDownMsg causes a PostMessage in case of allowing shutdown.
-                // This posted message was never processed and cause Windows XP to hang after log off
-                // if there are multiple sessions and the current session wasn't the first one started.
-                // So if shutdown is allowed we assume that a post message was done and retrieve all
-                // messages in the message queue and dispatch them before we return control to the system.
-
-                if ( nRet )
-                {
-                    MSG msg;
-
-                    while( PeekMessage( &msg, nullptr, 0, 0, PM_REMOVE ) )
-                    {
-                        DispatchMessage( &msg );
-                    }
-                }
-            }
-            else
-            {
-                ImplSalYieldMutexAcquireWithWait();
-                ImplSalYieldMutexRelease();
-                rDef = TRUE;
-            }
-            break;
-
         case WM_ENDSESSION:
-            if( !wParam )
-                bInQueryEnd = FALSE; // no shutdown: allow query again
-            nRet = FALSE;
+            // tdf#109085: Process it synchronously in SalComWndProc
+            nRet = SendMessageW(GetSalData()->mpFirstInstance->mhComWnd, nMsg, wParam, lParam);
             rDef = FALSE;
             break;
 
