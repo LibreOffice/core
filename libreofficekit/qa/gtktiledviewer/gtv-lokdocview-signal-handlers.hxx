f059293344ba93d76fcc6e64015923dfa639c4bf
Pranav Kant <pranavk@collabora.co.uk>
2017-07-13 19:34:41 +0530

    Modernize gtktiledviewer; use GApplication
    Put all the UI content in UI XML file.
    
    Unfortunately, lots of boilerplate code because
    G_DECLARE_* macros are available only since glib 2.44
    
    Change-Id: Idc74ba8565d482c28abd00b6f6f75646ab3d40b9
    Reviewed-on: https://gerrit.libreoffice.org/39913
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: pranavk <pranavk@collabora.co.uk>

---
+++ libreofficekit/qa/gtktiledviewer/gtv-lokdocview-signal-handlers.hxx
@@ -0,0 +1,34 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef GTV_LOKDOCVIEW_SIGNAL_HANDLERS_H
+#define GTV_LOKDOCVIEW_SIGNAL_HANDLERS_H
+
+#include <gtk/gtk.h>
+
+namespace LOKDocViewSigHandlers {
+    void editChanged(LOKDocView* pDocView, gboolean bWasEdit, gpointer);
+    void commandChanged(LOKDocView* pDocView, char* pPayload, gpointer);
+    void commandResult(LOKDocView*, char*, gpointer);
+    void searchNotFound(LOKDocView*, char*, gpointer);
+    void searchResultCount(LOKDocView*, char*, gpointer);
+    void partChanged(LOKDocView*, int, gpointer);
+    void hyperlinkClicked(LOKDocView*, char*, gpointer);
+    void cursorChanged(LOKDocView* pDocView, gint nX, gint nY, gint nWidth, gint nHeight, gpointer);
+    void addressChanged(LOKDocView* pDocView, char* pPayload, gpointer);
+    void formulaChanged(LOKDocView* pDocView, char* pPayload, gpointer);
+    void passwordRequired(LOKDocView* pDocView, char* pUrl, gboolean bModify, gpointer);
+    void comment(LOKDocView* pDocView, gchar* pComment, gpointer);
+
+    gboolean configureEvent(GtkWidget* pWidget, GdkEventConfigure* pEvent, gpointer pData);
+}
+
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
