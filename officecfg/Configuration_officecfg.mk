55baf61c08630e0c09a87d99519211467818ef8c
Michael Stahl <mstahl@redhat.com>
2017-07-14 15:31:20 +0200

    officecfg: increase GraphicManager/TotalCacheSize again for 64-bit
    There's a bit of a problem with XFillBitmapItem, which contains a
    GraphicObject that can't be swapped because it's a poolable item.
    
    Generally contemptorary hardware has enough RAM that we can easily
    increase the cache size to 400M (effectively there's another factor
    of 2x), but on legacy 32-bit platforms the address space is getting
    scarce, so keep the existing size for those.
    
    Change-Id: I8437f4e8c5421f8ec20e94e4cdf64f867d7760ca

--- officecfg/Configuration_officecfg.mk
+++ officecfg/Configuration_officecfg.mk
@@ -84,6 +84,7 @@ $(eval $(call gb_Configuration_add_spool_modules,registry,officecfg/registry/dat
 	org/openoffice/Office/Common-macosx.xcu \
 	org/openoffice/Office/Common-wnt.xcu \
 	org/openoffice/Office/Common-UseOOoFileDialogs.xcu \
+	org/openoffice/Office/Common-32bit.xcu \
 	org/openoffice/Office/Jobs-impress.xcu \
 	org/openoffice/Office/Linguistic-ForceDefaultLanguage.xcu \
 	org/openoffice/Office/ProtocolHandler-impress.xcu \
