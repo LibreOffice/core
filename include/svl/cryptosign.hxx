13fbaa13fd7750f1ee3c3dca26f9f30e76da57e4
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2017-07-14 22:21:00 -0400

    svl: move byte-array signing from vcl
    Signing a generic byte-array can (and will be) used
    by more than the existing PDF signing code, hence
    the move into comphelper from vcl and ourside of
    the PDF-specific logic.
    
    Change-Id: I7257b5218c6ba37960c6a013746eb387917a23a4

---
+++ include/svl/cryptosign.hxx
@@ -0,0 +1,71 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <config_features.h>
+#include <sal/types.h>
+
+#include <memory>
+#include <vector>
+
+#include <rtl/strbuf.hxx>
+#include <svl/svldllapi.h>
+#include "com/sun/star/uno/Reference.hxx"
+
+namespace com {
+namespace sun {
+namespace star {
+namespace security {
+    class XCertificate; }
+}}}
+
+namespace svl {
+
+namespace crypto {
+
+/// Helper to cryptographically sign and verify
+/// arbitrary data blocks.
+class SVL_DLLPUBLIC Signing
+{
+public:
+
+    Signing(const css::uno::Reference<css::security::XCertificate>& xCertificate) :
+        m_xCertificate(xCertificate)
+    {
+    }
+
+    /// Add a range to sign.
+    /// Note: for efficiency this takes a naked pointer, which must remain valid
+    /// until this object is discarded.
+    void AddDataRange(void* pData, sal_Int32 size)
+    {
+        m_dataBlocks.emplace_back(pData, size);
+    }
+
+    void SetSignTSA(const OUString& tsa) { m_aSignTSA = tsa; }
+    void SetSignPassword(const OUString& password) { m_aSignPassword = password;; }
+
+    /// Signs one or more data blocks (as a single, contiguous, array).
+    /// Returns the signature (in PKCS#7 format) as string (hex).
+    bool Sign(OStringBuffer& rCMSHexBuffer);
+
+private:
+    /// The certificate to use for signing.
+    const css::uno::Reference<css::security::XCertificate> m_xCertificate;
+
+    /// Data blocks (pointer-size pairs).
+    std::vector<std::pair<void*, sal_Int32>> m_dataBlocks;
+    OUString m_aSignTSA;
+    OUString m_aSignPassword;
+};
+
+}
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
+
