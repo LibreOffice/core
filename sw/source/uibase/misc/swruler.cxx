028f44faead4326ae0ffcfe6675080b320f0eba3
Aditya Dewan <iit2015097@iiita.ac.in>
2017-07-15 21:47:14 +0530

    implementing callback for ruler invalidation
    adding commands to fetch and changee ruler state
    '.uno:RulerState' and '.uno:RulerStateChange'
    
    Change-Id: I981fafe0afbe3e3a3e618a2b059404b28955ee97

--- sw/source/uibase/misc/swruler.cxx
+++ sw/source/uibase/misc/swruler.cxx
@@ -24,6 +24,8 @@
 #include <vcl/window.hxx>
 #include <vcl/settings.hxx>
 #include "strings.hrc"
+#include <comphelper/lok.hxx>
+#include <LibreOfficeKit/LibreOfficeKitEnums.h>
 
 #define CONTROL_BORDER_WIDTH    1
 
@@ -246,12 +248,22 @@ void SwCommentRuler::MouseButtonDown( const MouseEvent& rMEvt )
     Invalidate();
 }
 
+void SwCommentRuler::NotifyKit()
+{
+    if (!comphelper::LibreOfficeKit::isActive())
+        return;
+
+    const std::string test = SvxRuler::CreateJsonNotification();
+    mpViewShell->GetSfxViewShell()->libreOfficeKitViewCallback(LOK_CALLBACK_RULER_UPDATE, test.c_str());
+}
+
 void SwCommentRuler::Update()
 {
     tools::Rectangle aPreviousControlRect = GetCommentControlRegion();
     SvxRuler::Update();
     if (aPreviousControlRect != GetCommentControlRegion())
         Invalidate();
+    NotifyKit();
 }
 
 void SwCommentRuler::UpdateCommentHelpText()
