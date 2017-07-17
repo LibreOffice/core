75d5767e68400b70ab55811b72198efc8f121686
Stephan Bergmann <sbergman@redhat.com>
2017-07-17 17:20:53 +0200

    loplugin:oncevar: extensions (clang-cl)
    Change-Id: Id82847d72a106976339fd64685a9029900a862bd

--- extensions/source/update/check/updatecheck.cxx
+++ extensions/source/update/check/updatecheck.cxx
@@ -883,17 +883,17 @@ UpdateCheck::install()
         OUString aInstallImage(m_aImageName);
         osl::FileBase::getSystemPathFromFileURL(aInstallImage, aInstallImage);
 
-        OUString aParameter;
         sal_Int32 nFlags;
 #if (defined LINUX || defined __sun)
         nFlags = 42;
-        aParameter = getBaseInstallation();
+        OUString aParameter = getBaseInstallation();
         if( !aParameter.isEmpty() )
             osl::FileBase::getSystemPathFromFileURL(aParameter, aParameter);
 
         aParameter += " &";
 #else
         nFlags = c3s::SystemShellExecuteFlags::DEFAULTS;
+        OUString const aParameter;
 #endif
 
         rtl::Reference< UpdateCheckConfig > rModel = UpdateCheckConfig::get( m_xContext );
