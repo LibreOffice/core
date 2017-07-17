e25d58625adb8b110b6c1643ab9665c0a8182107
Szymon Kłos <szymon.klos@collabora.com>
2017-07-17 19:45:16 +0200

    Embedded documents: show title in menu entries
    Change-Id: I478d81798e6f1e2d96e570cb6788a438c6a0be62

--- embeddedobj/source/general/intercept.cxx
+++ embeddedobj/source/general/intercept.cxx
@@ -155,7 +155,7 @@ Interceptor::addStatusListener(
         aStateEvent.FeatureDescriptor = "Close and Return";
         aStateEvent.IsEnabled = true;
         aStateEvent.Requery = false;
-        aStateEvent.State <<= ( "($2) " + m_pDocHolder->GetTitle() );
+        aStateEvent.State <<= ( "($2) " + m_pDocHolder->GetContainerName() );
         Control->statusChanged(aStateEvent);
 
 
