8c3d3507a0a630925b5b0ccb8bee57790aab208d
Jan Holesovsky <kendy@collabora.com>
2017-07-19 08:35:26 +0200

    comphelper: Allow initializer lists for Sequences of NamedValues.
    This is particularly useful for creation of sequences that are later
    unwrapped using comphelper::SequenceAsHashMap.
    
    Eg.
    
        uno::Sequence<uno::Any> aArguments(comphelper::InitAnySequence(
        {
            {"SomethingNamed", uno::makeAny(true)},
        }));
    
        Reference<XExporter> xExporter(aFactory->createInstanceWithArguments(..., aArguments), UNO_QUERY);
    
    and in the implementation where the arguments are consumed:
    
        comphelper::SequenceAsHashMap aArgumentsMap(rArguments);
        mbSomething = aArgumentsMap.getUnpackedValueOrDefault("SomethingNamed", false);
    
    Change-Id: Ib1135078a99ca08f50bf51184f2ec7d13f5e6b4d
    Reviewed-on: https://gerrit.libreoffice.org/40201
    Reviewed-by: Jan Holesovsky <kendy@collabora.com>
    Tested-by: Jan Holesovsky <kendy@collabora.com>

--- include/comphelper/propertysequence.hxx
+++ include/comphelper/propertysequence.hxx
@@ -14,10 +14,12 @@
 #include <initializer_list>
 #include <com/sun/star/uno/Any.hxx>
 #include <com/sun/star/uno/Sequence.hxx>
+#include <com/sun/star/beans/NamedValue.hpp>
 #include <com/sun/star/beans/PropertyValue.hpp>
 
 namespace comphelper
 {
+    /// Init list for property sequences.
     inline css::uno::Sequence< css::beans::PropertyValue > InitPropertySequence(
         ::std::initializer_list< ::std::pair< OUString, css::uno::Any > > vInit)
     {
@@ -29,6 +31,23 @@ namespace comphelper
             vResult[nCount].Handle = -1;
             vResult[nCount].Value = aEntry.second;
             // State is default-initialized to DIRECT_VALUE
+            ++nCount;
+        }
+        return vResult;
+    }
+
+    /// Init list for property sequences that wrap the NamedValues in Anys.
+    ///
+    /// This is particularly useful for creation of sequences that are later
+    /// unwrapped using comphelper::SequenceAsHashMap.
+    inline css::uno::Sequence< css::uno::Any > InitAnySequence(
+        ::std::initializer_list< ::std::pair< OUString, css::uno::Any > > vInit)
+    {
+        css::uno::Sequence<css::uno::Any> vResult{static_cast<sal_Int32>(vInit.size())};
+        size_t nCount{0};
+        for(const auto& aEntry : vInit)
+        {
+            vResult[nCount] <<= css::beans::NamedValue(aEntry.first, aEntry.second);
             ++nCount;
         }
         return vResult;
