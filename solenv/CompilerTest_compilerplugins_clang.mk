17a8aadf764061b5be766e8edff3f01a12fe0993
Miklos Vajna <vmiklos@collabora.co.uk>
2017-07-19 10:42:06 +0200

    CompilerTest_compilerplugins_clang: disable test failing with clang-3.8
    10:28 <@vmiklos> noelgrandin: clang-3.8 is unhappy for the unusedfields plugin: https://pastebin.com/raw/MJ30aKcs
    10:30 <@noelgrandin> vmiklos: ... probably best to just disable the unusedfields solenv testing in the .mk file
    
    Change-Id: I1873f1f76d7a7b0438002ffda9180abe582e07d7

--- solenv/CompilerTest_compilerplugins_clang.mk
+++ solenv/CompilerTest_compilerplugins_clang.mk
@@ -31,10 +31,12 @@ $(eval $(call gb_CompilerTest_add_exception_objects,compilerplugins_clang, \
     compilerplugins/clang/test/unnecessaryoverride-dtor \
     compilerplugins/clang/test/unnecessaryparen \
     compilerplugins/clang/test/unoany \
-    compilerplugins/clang/test/unusedfields \
     compilerplugins/clang/test/useuniqueptr \
     compilerplugins/clang/test/vclwidgets \
 ))
+
+# FIXME Fails with clang-3.8.
+#    compilerplugins/clang/test/unusedfields \
 
 $(eval $(call gb_CompilerTest_use_externals,compilerplugins_clang, \
     boost_headers \
