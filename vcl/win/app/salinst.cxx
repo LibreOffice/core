caae38ebb3536c37ab8bb2de4710b8d599f34235
Jan-Marek Glogowski <glogow@fbihome.de>
2017-07-19 15:48:39 +0200

    tdf#109123 WIN Run instant timerout with low priority
    This busy-lock happens, because user messages have a higher priority
    then some system messages. What happens:
    
    1. The main system loop picks up the LO scheduler
    2. The idle worker (IW) is started
    3. IW checks using AnyInput( VCL_INPUT_ANY ) for system events
    4. A system event is found
    5. The LO scheduler gets posted again
    6. The main system loop picks up the LO scheduler instead of the
       system message => goto 2
    
    Normally it's suggested to use WM_TIMER in this case, as these messages
    are supposed to have the lowest priority. But this doesn't work, if
    you use PostMessage to generate them and SetTimer doesn't accept a
    0ms timeout. At least PeakMessage also picks up the WM_TIMER message
    before the system message, probably because PostMessage is somehow
    related to the threads queue - who knows.
    
    In the end this implements a manual, low priority event, which is checked
    at the end of the ImplSalYield function. It just runs, if there is
    nothing else to do. We still have to emit the timer callback event,
    as ImplSalYield may wait in GetMessage, but wParam now indicates, if
    it's a wakeup and can be ignored. We use the same event, so it's
    easier to filter.
    
    Thanks to Mike Kaganski for the missing information and ideas for the
    final implementation.
    
    Change-Id: Ib8e4f214ab8d3731d5594d68f38f46982c2eb36d

--- vcl/win/app/salinst.cxx
+++ vcl/win/app/salinst.cxx
@@ -322,6 +322,7 @@ SalData::SalData()
     mpDitherLow = nullptr;      // Dither mapping table
     mpDitherHigh = nullptr;     // Dither mapping table
     mnTimerId = nullptr;        // windows timer id
+    mbOnIdleRunScheduler = false; // if yield is idle, run the scheduler
     mhSalObjMsgHook = nullptr;  // hook to get interesting msg for SalObject
     mhWantLeaveMsg = nullptr;   // window handle, that want a MOUSELEAVE message
     mpMouseLeaveTimer = nullptr; // Timer for MouseLeave Test
@@ -565,19 +566,38 @@ ImplSalYield( bool bWait, bool bHandleAllCurrentEvents )
 {
     MSG aMsg;
     bool bWasMsg = false, bOneEvent = false;
+    SalData *const pSalData = GetSalData();
 
     int nMaxEvents = bHandleAllCurrentEvents ? 100 : 1;
     do
     {
-        if ( PeekMessageW( &aMsg, nullptr, 0, 0, PM_REMOVE ) )
+        bOneEvent = PeekMessageW( &aMsg, nullptr, 0, 0, PM_REMOVE );
+        if ( bOneEvent )
         {
-            TranslateMessage( &aMsg );
-            ImplSalDispatchMessage( &aMsg );
-
-            bOneEvent = bWasMsg = true;
+            bWasMsg = true;
+            if ( !(aMsg.message == SAL_MSG_TIMER_CALLBACK && 1 == aMsg.wParam) )
+            {
+                TranslateMessage( &aMsg );
+                ImplSalDispatchMessage( &aMsg );
+            }
+            else
+            {
+                // This is just the scheduler wakeup message, in case we're
+                // waiting in GetMessageW
+                // So we can just drop it, but we have to fix the accounting!
+                assert( pSalData->mbOnIdleRunScheduler );
+                ++nMaxEvents;
+            }
         }
         else
-            bOneEvent = false;
+        {
+            if ( nMaxEvents && pSalData->mbOnIdleRunScheduler )
+            {
+                pSalData->mbOnIdleRunScheduler = false;
+                EmitTimerCallback();
+                bOneEvent = true;
+            }
+        }
     } while( --nMaxEvents && bOneEvent );
 
     // Also check that we don't wait when application already has quit
@@ -585,8 +605,12 @@ ImplSalYield( bool bWait, bool bHandleAllCurrentEvents )
     {
         if ( GetMessageW( &aMsg, nullptr, 0, 0 ) )
         {
-            TranslateMessage( &aMsg );
-            ImplSalDispatchMessage( &aMsg );
+            // Ignore the scheduler wakeup message
+            if ( !(aMsg.message == SAL_MSG_TIMER_CALLBACK && 1 == aMsg.wParam) )
+            {
+                TranslateMessage( &aMsg );
+                ImplSalDispatchMessage( &aMsg );
+            }
         }
     }
     return bWasMsg;
@@ -708,7 +732,8 @@ LRESULT CALLBACK SalComWndProc( HWND, UINT nMsg, WPARAM wParam, LPARAM lParam, i
             while ( PeekMessageW(&aMsg, nullptr, SAL_MSG_TIMER_CALLBACK,
                                  SAL_MSG_TIMER_CALLBACK, PM_REMOVE) )
                 assert( "Multiple timer messages in queue" );
-            EmitTimerCallback();
+            if ( 0 == wParam )
+                EmitTimerCallback();
             break;
     }
 
