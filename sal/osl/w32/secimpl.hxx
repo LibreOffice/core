9df7952d237d77fb3acfab6ee2822e8391780af3
Chris Sherlock <chris.sherlock79@gmail.com>
2017-07-22 20:17:25 +1000

    osl: remove sig in comment
    Change-Id: I7e02e463105cf65fcc47ce5e3d5f9476ff778cce

--- sal/osl/w32/secimpl.hxx
+++ sal/osl/w32/secimpl.hxx
@@ -30,7 +30,7 @@ typedef struct {
     HANDLE m_hProfile;
     HANDLE m_hToken;
     sal_Unicode   m_User[USER_BUFFER_SIZE];
-/* ts: extension by fileserver login */
+/* extension by fileserver login */
     NETRESOURCEW *m_pNetResource;
 } oslSecurityImpl;
 
