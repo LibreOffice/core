7f2348d84c7abd886da9cf949085fd3a8b51049c
Caolán McNamara <caolanm@redhat.com>
2017-07-23 21:47:02 +0100

    pQLine2 will be non-zero in any case that nHeight is non-zero
    Change-Id: I5c836552df2122049dc88ca724444b29ae2c74e9
    (cherry picked from commit 13f80252de289b666b5df126e1ec8a9c99445bac)
    (cherry picked from commit bfaf9b9dc59cd38a1480da61daf949736759b9ed)

--- vcl/source/gdi/bitmap3.cxx
+++ vcl/source/gdi/bitmap3.cxx
@@ -731,6 +731,8 @@ bool Bitmap::ImplConvertDown(sal_uInt16 nBitCount, Color* pExtColor)
                 }
             }
 
+            assert(pQLine2 || nHeight == 0);
+
             for (long nY = 0; nY < nHeight; nY++, nYTmp++)
             {
                 // first pixel in the line
