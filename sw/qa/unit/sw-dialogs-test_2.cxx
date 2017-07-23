9263470903a46fa030fb941ee024bf24f2b98e59
Markus Mohrhard <markus.mohrhard@googlemail.com>
2017-07-24 00:09:31 +0200

    screenshots: the screenshots now need a initialized SwModule
    Change-Id: I44ea8630a874300e9163d71bd5094a5941d2a419

--- sw/qa/unit/sw-dialogs-test_2.cxx
+++ sw/qa/unit/sw-dialogs-test_2.cxx
@@ -16,6 +16,7 @@
 #include <vcl/abstdlg.hxx>
 
 #include <swabstdlg.hxx>
+#include <swdll.hxx>
 
 using namespace ::com::sun::star;
 
@@ -56,6 +57,7 @@ SwDialogsTest2::SwDialogsTest2()
 void SwDialogsTest2::setUp()
 {
     ScreenshotTest::setUp();
+    SwGlobals::ensure();
     // Make sure the swui library's global pSwResMgr is initialized
     // (alternatively to dynamically loading the library, SwCreateDialogFactory
     // could be declared in an include file and this CppunitTest link against
