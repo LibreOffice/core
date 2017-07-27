e7d324dd6f0126e948fea3df76763ac069cac91a
Muhammet Kara <muhammetk@gmail.com>
2017-07-27 21:49:53 +0300

    WiP: Convert UI of Customize Dialog to the new design
    Change-Id: Ib602e0a4b9419d16adaa4ff6aa23b74097a3b88d

--- cui/source/inc/cfg.hxx
+++ cui/source/inc/cfg.hxx
@@ -383,31 +383,36 @@ protected:
 
     // the top section of the tab page where top level menus and toolbars
     //  are displayed in a listbox
-    VclPtr<VclFrame>                           m_pTopLevel;
-    VclPtr<FixedText>                          m_pTopLevelLabel;
+    //VclPtr<VclFrame>                           m_pTopLevel;
+    //VclPtr<FixedText>                          m_pTopLevelLabel;
     VclPtr<ListBox>                            m_pTopLevelListBox;
-    VclPtr<PushButton>                         m_pNewTopLevelButton;
-    VclPtr<MenuButton>                         m_pModifyTopLevelButton;
+    //VclPtr<PushButton>                         m_pNewTopLevelButton;
+    //VclPtr<MenuButton>                         m_pModifyTopLevelButton;
 
-    VclPtr<FixedText>                          m_pToolbarStyleLabel;
-    VclPtr<RadioButton>                        m_pIconsOnlyRB;
-    VclPtr<RadioButton>                        m_pTextOnlyRB;
-    VclPtr<RadioButton>                        m_pIconsAndTextRB;
+    //VclPtr<FixedText>                          m_pToolbarStyleLabel;
+    //VclPtr<RadioButton>                        m_pIconsOnlyRB;
+    //VclPtr<RadioButton>                        m_pTextOnlyRB;
+    //VclPtr<RadioButton>                        m_pIconsAndTextRB;
 
     // the contents section where the contents of the selected
     // menu or toolbar are displayed
     VclPtr<VclFrame>                           m_pContents;
-    VclPtr<FixedText>                          m_pContentsLabel;
+    //VclPtr<FixedText>                          m_pContentsLabel;
     VclPtr<VclContainer>                       m_pEntries;
     VclPtr<SvTreeListBox>                      m_pContentsListBox;
 
-    VclPtr<PushButton>                         m_pAddCommandsButton;
-    VclPtr<PushButton>                         m_pAddSeparatorButton;
-    VclPtr<PushButton>                         m_pAddSubmenuButton;
-    VclPtr<MenuButton>                         m_pModifyCommandButton;
-    VclPtr<PushButton>                         m_pDeleteCommandButton;
+    // the functions section where functions in the
+    // selected category will be displayed
+    VclPtr<VclContainer>                       m_pFunctions;
+    VclPtr<SvTreeListBox>                      m_pFunctionsListBox;
+
+    //VclPtr<PushButton>                         m_pAddCommandsButton;
+    //VclPtr<PushButton>                         m_pAddSeparatorButton;
+    //VclPtr<PushButton>                         m_pAddSubmenuButton;
+    //VclPtr<MenuButton>                         m_pModifyCommandButton;
+    //VclPtr<PushButton>                         m_pDeleteCommandButton;
     // Resets the top level toolbar to default settings
-    VclPtr<PushButton>                         m_pResetTopLevelButton;
+    //VclPtr<PushButton>                         m_pResetTopLevelButton;
 
     VclPtr<PushButton>                         m_pMoveUpButton;
     VclPtr<PushButton>                         m_pMoveDownButton;
