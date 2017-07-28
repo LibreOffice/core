632d6052526304e59897abc0ef199b9689980820
Muhammet Kara <muhammet.kara@pardus.org.tr>
2017-07-28 17:11:28 +0300

    WiP: Convert UI of Customize Dialog to the new design
    * Elements of the old design have been removed from both
    the .ui file and the .cxx/.hxx files.
    
    * New elements for the new design have been added to the
    .ui file.
    
    Change-Id: Ib602e0a4b9419d16adaa4ff6aa23b74097a3b88d

--- cui/source/inc/SvxMenuConfigPage.hxx
+++ cui/source/inc/SvxMenuConfigPage.hxx
@@ -53,14 +53,6 @@ private:
     bool m_bIsMenuBar;
     DECL_LINK( SelectMenu, ListBox&, void );
     DECL_LINK( SelectMenuEntry, SvTreeListBox *, void );
-    DECL_LINK( NewMenuHdl, Button *, void );
-    DECL_LINK( MenuSelectHdl, MenuButton *, void );
-    DECL_LINK( EntrySelectHdl, MenuButton *, void );
-    DECL_LINK( AddCommandsHdl, Button *, void );
-    DECL_LINK( AddSeparatorHdl, Button *, void );
-    DECL_LINK( AddSubmenuHdl, Button *, void );
-    DECL_LINK( DeleteCommandHdl, Button *, void );
-    DECL_LINK( AddFunctionHdl, SvxScriptSelectorDialog&, void );
 
     void            Init() override;
     void            UpdateButtonStates() override;
