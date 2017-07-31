LibreOffice/core

commit ae86557de53a8762924f18c78a556e2d6881002a

sd pptm: Add a separate PPTM filter.
Similarly to commit 697173f6fdfae581022cfdb5ec5171c5a3be58f0, we have to
preserve the correct content-type which is not the same for PPTX and PPTM.

This commit also changes sdfilt.component to use the constructor feature.

Change-Id: I4b334540ec07a53d308e6b9dd6403294842bddf8
Reviewed-on: https://gerrit.libreoffice.org/40195
Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
Tested-by: Miklos Vajna <vmiklos@collabora.co.uk>

sd/source/filter/eppt/pptx-epptooxml.cxx
@@ -26,6 +26,7 @@
 #include <epptdef.hxx>
 #include <oox/export/shapes.hxx>
 
+#include <comphelper/sequenceashashmap.hxx>
 #include <cppuhelper/implementationentry.hxx>
 #include <cppuhelper/factory.hxx>
 #include <sax/fshelper.hxx>
@@ -91,17 +92,15 @@ using ::com::sun::star::beans::XPropertySet;
 using ::com::sun::star::beans::XPropertySetInfo;
 using ::com::sun::star::container::XIndexAccess;
 using ::sax_fastparser::FSHelperPtr;
-
+using namespace oox;
+using namespace oox::drawingml;
+using namespace oox::core;
 
 #if OSL_DEBUG_LEVEL > 1
 void dump_pset(Reference< XPropertySet > const & rXPropSet);
 #endif
 
 #define IDS(x) OString(OStringLiteral(#x " ") + OString::number( mnShapeIdMax++ )).getStr()
-
-namespace oox {
-    using namespace drawingml;
-    namespace core {
 
 class PowerPointShapeExport : public ShapeExport
 {
@@ -313,8 +312,8 @@ ShapeExport& PowerPointShapeExport::WriteUnknownShape( const Reference< XShape >
     return *this;
 }
 
-PowerPointExport::PowerPointExport( const Reference< XComponentContext > & rxCtxt  )
-    : XmlFilterBase(rxCtxt)
+PowerPointExport::PowerPointExport(const Reference< XComponentContext > & rContext, const uno::Sequence<uno::Any>& rArguments)
+    : XmlFilterBase(rContext)
     , PPTWriterBase()
     , mnLayoutFileIdMax(1)
     , mnSlideIdMax(1 << 8)
@@ -323,6 +322,9 @@ PowerPointExport::PowerPointExport( const Reference< XComponentContext > & rxCtx
     , mbCreateNotes(false)
 {
     memset( mLayoutInfo, 0, sizeof(mLayoutInfo) );
+
+    comphelper::SequenceAsHashMap aArgumentsMap(rArguments);
+    mbPptm = aArgumentsMap.getUnpackedValueOrDefault("IsPPTM", false);
 }
 
 PowerPointExport::~PowerPointExport()
@@ -357,8 +359,12 @@ bool PowerPointExport::exportDocument() throw (css::uno::RuntimeException, std::
 
     addRelation( "http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument", "ppt/presentation.xml" );
 
-    mPresentationFS = openFragmentStreamWithSerializer( "ppt/presentation.xml",
-                                                    "application/vnd.openxmlformats-officedocument.presentationml.presentation.main+xml" );
+    // PPTM needs a different media type for the presentation.xml stream.
+    OUString aMediaType("application/vnd.openxmlformats-officedocument.presentationml.presentation.main+xml");
+    if (mbPptm)
+        aMediaType = "application/vnd.ms-powerpoint.presentation.macroEnabled.main+xml";
+
+    mPresentationFS = openFragmentStreamWithSerializer("ppt/presentation.xml", aMediaType);
 
     addRelation( mPresentationFS->getOutputStream(),
                  "http://schemas.openxmlformats.org/officeDocument/2006/relationships/theme",
@@ -2288,50 +2294,17 @@ bool PowerPointExport::ImplCreateMainNotes()
     return true;
 }
 
-OUString SAL_CALL PowerPointExport_getImplementationName() throw()
-{
-    return OUString( "com.sun.star.comp.Impress.oox.PowerPointExport" );
-}
-
-uno::Sequence< OUString > SAL_CALL PowerPointExport_getSupportedServiceNames() throw()
-{
-    return Sequence< OUString >();
-}
-
-uno::Reference< uno::XInterface > SAL_CALL PowerPointExport_createInstance(const uno::Reference< XComponentContext > & rxCtxt ) throw( uno::Exception )
-{
-    return static_cast<cppu::OWeakObject*>(new PowerPointExport( rxCtxt ));
-}
-
 OUString PowerPointExport::getImplementationName() throw (css::uno::RuntimeException, std::exception)
 {
-    return PowerPointExport_getImplementationName();
-}
-}
+    return OUString("com.sun.star.comp.Impress.oox.PowerPointExport");
 }
 
 // UNO component
-
-static const struct cppu::ImplementationEntry g_entries[] =
+extern "C" SAL_DLLPUBLIC_EXPORT uno::XInterface* SAL_CALL
+css_comp_Impress_oox_PowerPointExport(uno::XComponentContext* rxCtxt,
+                                      uno::Sequence<css::uno::Any> const& rArguments)
 {
-    {
-        oox::core::PowerPointExport_createInstance,
-        oox::core::PowerPointExport_getImplementationName,
-       oox::core::PowerPointExport_getSupportedServiceNames,
-        cppu::createSingleComponentFactory,
-        nullptr , 0
-    },
-    { nullptr, nullptr, nullptr, nullptr, nullptr, 0 }
-};
-
-extern "C"
-{
-
-SAL_DLLPUBLIC_EXPORT void* SAL_CALL sdfilt_component_getFactory( const sal_Char* pImplName, void* pServiceManager, void* pRegistryKey )
-{
-    return cppu::component_getFactoryHelper( pImplName, pServiceManager, pRegistryKey , g_entries );
-}
-
+    return cppu::acquire(new PowerPointExport(rxCtxt, rArguments));
 }
 
 #if OSL_DEBUG_LEVEL > 1

filter/Configuration_filter.mk
@@ -598,6 +598,7 @@ $(eval $(call filter_Configuration_add_types,fcfg_langpack,fcfg_impress_types.xc
 	MS_PowerPoint_2007_XML \
 	MS_PowerPoint_2007_XML_AutoPlay \
 	MS_PowerPoint_2007_XML_Template \
+	MS_PowerPoint_2007_XML_VBA \
 	impress_OOXML_Presentation \
 	impress_OOXML_Presentation_Template \
 	impress_OOXML_Presentation_AutoPlay \
@@ -622,6 +623,7 @@ $(eval $(call filter_Configuration_add_filters,fcfg_langpack,fcfg_impress_filter
 	impress_MS_PowerPoint_2007_XML \
 	impress_MS_PowerPoint_2007_XML_AutoPlay \
 	impress_MS_PowerPoint_2007_XML_Template \
+	impress_MS_PowerPoint_2007_XML_VBA \
 	impress_OOXML \
 	impress_OOXML_Template \
 	impress_OOXML_AutoPlay \

filter/source/config/fragments/filters/impress_MS_PowerPoint_2007_XML_VBA.xcu
@@ -15,16 +15,16 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
 -->
-<node oor:name="Impress MS PowerPoint 2007 XML" oor:op="replace">
+<node oor:name="Impress MS PowerPoint 2007 XML VBA" oor:op="replace">
     <prop oor:name="Flags"><value>IMPORT EXPORT ALIEN 3RDPARTYFILTER PREFERRED ENCRYPTION PASSWORDTOMODIFY SUPPORTSSIGNING</value></prop>
     <prop oor:name="UIComponent"/>
     <prop oor:name="FilterService"><value>com.sun.star.comp.oox.ppt.PowerPointImport</value></prop>
-    <prop oor:name="UserData"/>
+    <prop oor:name="UserData"><value>macro-enabled</value></prop>
     <prop oor:name="FileFormatVersion"/>
-    <prop oor:name="Type"><value>MS PowerPoint 2007 XML</value></prop>
+    <prop oor:name="Type"><value>MS PowerPoint 2007 XML VBA</value></prop>
     <prop oor:name="TemplateName"/>
     <prop oor:name="DocumentService"><value>com.sun.star.presentation.PresentationDocument</value></prop>
     <prop oor:name="UIName">
-        <value xml:lang="en-US">Microsoft PowerPoint 2007-2013 XML</value>
+        <value xml:lang="en-US">Microsoft PowerPoint 2007-2013 XML VBA</value>
     </prop>
 </node>

filter/source/config/fragments/types/MS_PowerPoint_2007_XML.xcu
@@ -18,7 +18,7 @@
 <node oor:name="MS PowerPoint 2007 XML" oor:op="replace" >
     <prop oor:name="DetectService"><value>com.sun.star.comp.oox.FormatDetector</value></prop>
     <prop oor:name="URLPattern"/>
-    <prop oor:name="Extensions"><value>pptx pptm</value></prop>
+    <prop oor:name="Extensions"><value>pptx</value></prop>
     <prop oor:name="MediaType"><value>application/vnd.openxmlformats-officedocument.presentationml.presentation</value></prop>
     <prop oor:name="Preferred"><value>true</value></prop>
     <prop oor:name="PreferredFilter"><value>Impress MS PowerPoint 2007 XML</value></prop>

filter/source/config/fragments/types/MS_PowerPoint_2007_XML_VBA.xcu
@@ -1,3 +1,4 @@
+
 <!--
  * This file is part of the LibreOffice project.
  *
@@ -15,16 +16,13 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
 -->
-<node oor:name="Impress MS PowerPoint 2007 XML" oor:op="replace">
-    <prop oor:name="Flags"><value>IMPORT EXPORT ALIEN 3RDPARTYFILTER PREFERRED ENCRYPTION PASSWORDTOMODIFY SUPPORTSSIGNING</value></prop>
-    <prop oor:name="UIComponent"/>
-    <prop oor:name="FilterService"><value>com.sun.star.comp.oox.ppt.PowerPointImport</value></prop>
-    <prop oor:name="UserData"/>
-    <prop oor:name="FileFormatVersion"/>
-    <prop oor:name="Type"><value>MS PowerPoint 2007 XML</value></prop>
-    <prop oor:name="TemplateName"/>
-    <prop oor:name="DocumentService"><value>com.sun.star.presentation.PresentationDocument</value></prop>
-    <prop oor:name="UIName">
-        <value xml:lang="en-US">Microsoft PowerPoint 2007-2013 XML</value>
-    </prop>
+<node oor:name="MS PowerPoint 2007 XML VBA" oor:op="replace" >
+    <prop oor:name="DetectService"><value>com.sun.star.comp.oox.FormatDetector</value></prop>
+    <prop oor:name="URLPattern"/>
+    <prop oor:name="Extensions"><value>pptm</value></prop>
+    <prop oor:name="MediaType"><value>application/vnd.ms-powerpoint.presentation.macroEnabled.main+xml</value></prop>
+    <prop oor:name="Preferred"><value>true</value></prop>
+    <prop oor:name="PreferredFilter"><value>Impress MS PowerPoint 2007 XML VBA</value></prop>
+    <prop oor:name="UIName"><value xml:lang="en-US">Microsoft PowerPoint 2007-2013 XML VBA</value></prop>
+    <prop oor:name="ClipboardFormat"/>
 </node>

oox/source/core/filterdetect.cxx
@@ -197,9 +197,11 @@ OUString FilterDetectDocHandler::getFilterNameFromContentType( const OUString& r
     if ( rContentType == "application/vnd.ms-excel.sheet.binary.macroEnabled.main" )
         return OUString( "MS Excel 2007 Binary" );
 
-    if( rContentType == "application/vnd.openxmlformats-officedocument.presentationml.presentation.main+xml" ||
-        rContentType == "application/vnd.ms-powerpoint.presentation.macroEnabled.main+xml" )
+    if (rContentType == "application/vnd.openxmlformats-officedocument.presentationml.presentation.main+xml")
         return OUString( "MS PowerPoint 2007 XML" );
+
+    if (rContentType == "application/vnd.ms-powerpoint.presentation.macroEnabled.main+xml")
+        return OUString( "MS PowerPoint 2007 XML VBA" );
 
     if( rContentType == "application/vnd.openxmlformats-officedocument.presentationml.slideshow.main+xml" ||
         rContentType == "application/vnd.ms-powerpoint.slideshow.macroEnabled.main+xml" )

oox/source/ppt/pptimport.cxx
@@ -21,6 +21,7 @@
 
 #include <com/sun/star/lang/XMultiServiceFactory.hpp>
 #include <com/sun/star/uno/XComponentContext.hpp>
+#include <comphelper/propertysequence.hxx>
 #include <osl/diagnose.h>
 #include <vcl/msgbox.hxx>
 #include <vcl/svapp.hxx>
@@ -175,14 +176,23 @@ sal_Bool SAL_CALL PowerPointImport::filter( const Sequence< PropertyValue >& rDe
     if( XmlFilterBase::filter( rDescriptor ) )
         return true;
 
-    if( isExportFilter() ) {
-        Reference< XExporter > xExporter( Reference<css::lang::XMultiServiceFactory>(getComponentContext()->getServiceManager(), UNO_QUERY_THROW)->createInstance( "com.sun.star.comp.Impress.oox.PowerPointExport" ), UNO_QUERY );
+    if (isExportFilter())
+    {
+        uno::Sequence<uno::Any> aArguments(comphelper::InitAnySequence(
+        {
+            {"IsPPTM", uno::makeAny(exportVBA())},
+        }));
 
-        if( xExporter.is() ) {
+        Reference<css::lang::XMultiServiceFactory> aFactory(getComponentContext()->getServiceManager(), UNO_QUERY_THROW);
+        Reference< XExporter > xExporter(aFactory->createInstanceWithArguments("com.sun.star.comp.Impress.oox.PowerPointExport", aArguments), UNO_QUERY);
+
+        if (xExporter.is())
+        {
             Reference< XComponent > xDocument( getModel(), UNO_QUERY );
             Reference< XFilter > xFilter( xExporter, UNO_QUERY );
 
-            if( xFilter.is() ) {
+            if (xFilter.is())
+            {
                 xExporter->setSourceDocument( xDocument );
                 if( xFilter->filter( rDescriptor ) )
                     return true;

sd/source/filter/eppt/epptooxml.hxx
@@ -39,8 +39,6 @@ namespace oox {
     }
 namespace core {
 
-class PowerPointShapeExport;
-
 struct LayoutInfo
 {
     std::vector< sal_Int32 > mnFileIdArray;
@@ -65,7 +63,7 @@ class PowerPointExport : public XmlFilterBase, public PPTWriterBase
     friend class PowerPointShapeExport;
 public:
 
-    PowerPointExport( const css::uno::Reference< css::uno::XComponentContext > & rxCtxt  );
+    PowerPointExport(const css::uno::Reference<css::uno::XComponentContext> & rContext, const css::uno::Sequence<css::uno::Any>& rArguments);
 
     virtual ~PowerPointExport() override;
 
@@ -133,6 +131,9 @@ private:
     void AddLayoutIdAndRelation( const ::sax_fastparser::FSHelperPtr& pFS, sal_Int32 nLayoutFileId );
 
     virtual OUString SAL_CALL getImplementationName() throw (css::uno::RuntimeException, std::exception) override;
+
+    /// Should we export as .pptm, ie. do we contain macros?
+    bool mbPptm;
 
     ::sax_fastparser::FSHelperPtr mPresentationFS;
 

commit 1db8c89f5c3c6f8f2ab047eb23ca2187c43b36a3

Windows build fix: ambigous symbol
Caused by:
ae86557de53a8762924f18c78a556e2d6881002a

These namespaces were removed from the cxx file, but not from the header.

Change-Id: I5bae177638f0c9d404aac0fa4577689965ef5c26

sd/source/filter/eppt/pptx-epptooxml.cxx
@@ -106,6 +106,10 @@ void dump_pset(Reference< XPropertySet > const & rXPropSet);
 
 #define IDS(x) OString(OStringLiteral(#x " ") + OString::number( mnShapeIdMax++ )).getStr()
 
+namespace oox {
+    using namespace drawingml;
+    namespace core {
+
 class PowerPointShapeExport : public ShapeExport
 {
     PowerPointExport&   mrExport;
@@ -2330,6 +2334,9 @@ bool PowerPointExport::ImplCreateMainNotes()
 OUString PowerPointExport::getImplementationName() throw (css::uno::RuntimeException, std::exception)
 {
     return OUString("com.sun.star.comp.Impress.oox.PowerPointExport");
+}
+
+}
 }
 
 // UNO component