6924b18ad65115c0190135d9fb71cf1ebcd24b0b
Andrea Gelmini <andrea.gelmini@gelma.net>
2017-08-02 18:19:35 +0200

    Fix typos
    Change-Id: Ib7f43bf17ab8b9fb9d961a908d7620b50826a975

--- uitest/uitest/test.py
+++ uitest/uitest/test.py
@@ -198,7 +198,7 @@ class UITest(object):
         with EventListener(self._xContext, ["DialogExecute", "ModelessDialogExecute"]) as event:
             thread.start()
             time_ = 0
-            # we are not necessarily opeinging a dialog, so wait much longer
+            # we are not necessarily opening a dialog, so wait much longer
             while time_ < 10 * MAX_WAIT:
                 if event.executed:
                     xDlg = self._xUITest.getTopFocusWindow()
