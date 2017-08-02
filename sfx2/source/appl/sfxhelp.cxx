7f34b77e59a9151b01347dcf4d7003c2da6ef475
Olivier Hallot <olivier.hallot@libreoffice.org>
2017-08-02 01:06:01 -0300

    tdf#97745 rename index,html to LOHelp.html
    Index.html is not a good name on a website that is called
    from LibreOffice. Use LOHelp.html instead to handle
    special case of URLs comming from LibreOffice help call.
    
    Change-Id: I0f33e56c0e6eb81deebeaedf6e24b9c3c791a68c

--- sfx2/source/appl/sfxhelp.cxx
+++ sfx2/source/appl/sfxhelp.cxx
@@ -530,7 +530,7 @@ static bool impl_showOnlineHelp( const OUString& rURL )
     if ( rURL.getLength() <= aInternal.getLength() || !rURL.startsWith(aInternal) )
         return false;
 
-    OUString aHelpLink( "http://helponline.libreoffice.org/index.html?"  );
+    OUString aHelpLink( "http://helponline.libreoffice.org/LOHelp.html?"  );
 
     aHelpLink += rURL.copy( aInternal.getLength() );
     aHelpLink = aHelpLink.replaceAll("%2F","/");
