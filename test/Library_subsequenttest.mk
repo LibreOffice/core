b4f543d6776aed8629278968ea1f040326edbe20
Jens Carl <j.carl43@gmx.de>
2017-08-05 04:31:57 +0000

    tdf#45904 Move Java XSpreadsheets tests to C++
    Change-Id: If311e2c8ca89528760b14d977bca76e14164e6e3

--- test/Library_subsequenttest.mk
+++ test/Library_subsequenttest.mk
@@ -59,6 +59,7 @@ $(eval $(call gb_Library_add_exception_objects,subsequenttest,\
     test/source/sheet/xnamedranges \
     test/source/sheet/xprintareas \
     test/source/sheet/xspreadsheetdocument \
+    test/source/sheet/xspreadsheets \
     test/source/sheet/xspreadsheets2 \
     test/source/sheet/xsheetannotation \
     test/source/sheet/xsheetannotations \
