f262577c6a1e3c50e43f5c22b04df1e8953c0b88
Jens Carl <j.carl43@gmx.de>
2017-08-06 07:10:36 +0000

    tdf#45904 Move Java _XViewSplitable tests to C++
    Change-Id: Ib9cf1c53947d10260673e1e1088bbfbaa30efbc9

---
+++ sc/qa/extras/sctabviewobj.cxx
@@ -0,0 +1,92 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <test/calc_unoapi_test.hxx>
+#include <test/sheet/xviewsplitable.hxx>
+
+#include <com/sun/star/lang/XComponent.hpp>
+#include <com/sun/star/frame/XModel.hpp>
+#include <com/sun/star/sheet/XSpreadsheetDocument.hpp>
+#include <com/sun/star/sheet/XViewSplitable.hpp>
+#include <com/sun/star/uno/XInterface.hpp>
+
+using namespace css;
+using namespace css::uno;
+
+namespace sc_apitest {
+
+#define NUMBER_OF_TESTS 1
+
+class ScTabViewObj : public CalcUnoApiTest, public apitest::XViewSplitable
+{
+public:
+    ScTabViewObj();
+
+    virtual uno::Reference< uno::XInterface > init() override;
+    virtual void setUp() override;
+    virtual void tearDown() override;
+
+    CPPUNIT_TEST_SUITE(ScTabViewObj);
+    // XViewSplitable
+    CPPUNIT_TEST(testSplit);
+
+    CPPUNIT_TEST_SUITE_END();
+
+private:
+
+    static sal_Int32 nTest;
+    static uno::Reference< lang::XComponent > mxComponent;
+};
+
+sal_Int32 ScTabViewObj::nTest = 0;
+uno::Reference< lang::XComponent > ScTabViewObj::mxComponent;
+
+ScTabViewObj::ScTabViewObj()
+    : CalcUnoApiTest("/sc/qa/extras/testdocuments")
+{
+}
+
+uno::Reference< uno::XInterface > ScTabViewObj::init()
+{
+    if (!mxComponent.is())
+        mxComponent = loadFromDesktop("private:factory/scalc");
+
+    uno::Reference< sheet::XSpreadsheetDocument > xSheetDoc(mxComponent, uno::UNO_QUERY_THROW);
+    CPPUNIT_ASSERT_MESSAGE("no calc document", xSheetDoc.is());
+
+    uno::Reference< frame::XModel > xModel(xSheetDoc, uno::UNO_QUERY_THROW);
+    uno::Reference< sheet::XViewSplitable > xViewSplitable(xModel->getCurrentController(), uno::UNO_QUERY_THROW);
+
+    return xViewSplitable;
+}
+
+void ScTabViewObj::setUp()
+{
+    nTest++;
+    CalcUnoApiTest::setUp();
+}
+
+void ScTabViewObj::tearDown()
+{
+    if (nTest == NUMBER_OF_TESTS)
+    {
+        closeDocument(mxComponent);
+        mxComponent.clear();
+    }
+
+    CalcUnoApiTest::tearDown();
+}
+
+CPPUNIT_TEST_SUITE_REGISTRATION(ScTabViewObj);
+
+} // end namespace
+
+CPPUNIT_PLUGIN_IMPLEMENT();
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
