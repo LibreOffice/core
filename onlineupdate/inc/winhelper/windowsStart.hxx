a5cda5603c3f71851a987c24d36433ce269c66f8
Markus Mohrhard <markus.mohrhard@googlemail.com>
2017-08-07 09:34:21 +0200

    updater: use the correct define check
    Change-Id: I79f47da401732e39bbce778d5b3971d6024a7935
    Reviewed-on: https://gerrit.libreoffice.org/40835
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Markus Mohrhard <markus.mohrhard@googlemail.com>

--- onlineupdate/inc/winhelper/windowsStart.hxx
+++ onlineupdate/inc/winhelper/windowsStart.hxx
@@ -3,10 +3,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifdef XP_WIN
+#ifdef _WIN32
 #include <windows.h>
-#else
-#include <limits.h>
 #endif
 
 
