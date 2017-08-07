f14b6e46c4a7ce51298d9a7c4447f25bb54b715f
Caolán McNamara <caolanm@redhat.com>
2017-08-07 19:24:37 +0100

    ofz#2877: crash in SVTB16Short
    sal_uInt16 wraparound
    
    Change-Id: Ifd791bdd5f1b96576fdd4ca6665bb972fb8b1e4c

--- sw/source/filter/ww8/ww8scan.cxx
+++ sw/source/filter/ww8/ww8scan.cxx
@@ -3501,7 +3501,7 @@ bool WW8PLCFx_SEPX::Find4Sprms(sal_uInt16 nId1,sal_uInt16 nId2,sal_uInt16 nId3,s
     bool bFound = false;
 
     sal_uInt8* pSp = pSprms;
-    sal_uInt16 i=0;
+    size_t i = 0;
     while (i + maSprmParser.MinSprmLen() <= nSprmSiz)
     {
         // Sprm found?
