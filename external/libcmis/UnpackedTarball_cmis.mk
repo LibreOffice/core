e845507bc22a166ec172a4b4d9da120a16f8a964
Michael Stahl <mstahl@redhat.com>
2017-08-09 17:53:44 +0200

    libcmis: configure curl to only allow redirects to HTTP/HTTPS
    Change-Id: I77e90ca955dc1249d259bf01cb107d5b317d8045

--- external/libcmis/UnpackedTarball_cmis.mk
+++ external/libcmis/UnpackedTarball_cmis.mk
@@ -20,6 +20,7 @@ $(eval $(call gb_UnpackedTarball_add_patches,cmis, \
 						external/libcmis/libcmis-fix-google-drive-2.patch \
 						external/libcmis/libcmis-sharepoint-repository-root.patch \
 						external/libcmis/libcmis-fix-error-handling.patch \
+						external/libcmis/libcmis-curl-redirects.patch.1 \
 ))
 
 ifeq ($(OS),WNT)
