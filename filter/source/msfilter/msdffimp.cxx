2b562f7531d097def1cf312f13df2fe9e9789ebe
Szymon Kłos <szymon.klos@collabora.com>
2017-08-09 12:15:53 +0200

    tdf#111480 Circular text was imported horizontally
    Change-Id: I84a6c3a30fd1dddf104ad8bf037a5e0b2a90d376
    Reviewed-on: https://gerrit.libreoffice.org/40912
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>

--- filter/source/msfilter/msdffimp.cxx
+++ filter/source/msfilter/msdffimp.cxx
@@ -4407,7 +4407,7 @@ SdrObject* SvxMSDffManager::ImportShape( const DffRecordHeader& rHd, SvStream& r
                         aFont.SetFamilyName( aFontName );
                         auto nTextWidth = pOut->GetTextWidth( aObjectText );
 
-                        if ( nTextWidth )
+                        if ( nTextWidth && aObjData.eShapeType == mso_sptTextPlainText )
                         {
                             fRatio = aFont.GetFontSize().Height();
                             fRatio /= nTextWidth;
