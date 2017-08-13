93e6fed38a71ff53703a0b7bb64b457dac1ab2a3
Khaled Hosny <khaledhosny@eglug.org>
2017-08-13 23:03:43 +0200

    tdf#109142: Update to HarfBuzz 1.4.8
    Should fix the Tibetan long U issue.
    
    Also reverts this commit as depending on auto-detection for a required
    feature is not a very good idea. The underlying issue is fixed upstream:
    
    commit 5985bed1764358a515c378c80bb60b4d35bad6b1
    Author: jan Iversen <jani@libreoffice.org>
    Date:   Mon Jun 12 14:51:42 2017 +0200
    
        iOS, harfbuzz libcoretext not found
    
        changed --with-libcoretext
        from "yes" to "auto" for IOS and MACOSX
    
    which seems to be a misguided attemp to fix the build breakage
    introduced in:
    
    commit b3559e5c6ce036b1a4fe96c0ffc935e0c94a7b10
    Author: jan Iversen <jani@libreoffice.org>
    Date:   Sun Jun 11 19:35:53 2017 +0200
    
        iOS, patch harfbuzz
    
        Add support for arm64
    
    Also had to drop ubsan.patch as it no longer applies, hopefully it is no
    longer needed, otherwise it is always good idea to submit such patches
    upstream.
    
    Change-Id: I91643c542c0a5a00f36d5564052787eedef8c9cf

--- external/harfbuzz/ExternalProject_harfbuzz.mk
+++ external/harfbuzz/ExternalProject_harfbuzz.mk
@@ -36,8 +36,9 @@ $(call gb_ExternalProject_get_state_target,harfbuzz,build) :
 			--with-fontconfig=no \
 			--with-cairo=no \
 			--with-glib=no \
+			--with-ucdn=no \
 			--with-graphite2=yes \
-			$(if $(filter IOS MACOSX,$(OS)),--with-coretext=auto) \
+			$(if $(filter IOS MACOSX,$(OS)),--with-coretext=yes) \
 			$(if $(verbose),--disable-silent-rules,--enable-silent-rules) \
 			--libdir=$(call gb_UnpackedTarball_get_dir,harfbuzz/src/.libs) \
 			$(if $(CROSS_COMPILING),--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM)) \
