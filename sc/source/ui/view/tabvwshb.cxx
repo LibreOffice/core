LibreOffice/core

commit 528632660b72b105345945c13c5b68060d94a91b

convert ErrCode to strong typedef
would have preferred to re-use o3tl::strong_int, of which this
is a modified copy, but there are lots of convenience accessors
which are nice to define on the class.

Change-Id: I301b807aaf02fbced3bf75de1e1692cde6c0340a
Reviewed-on: https://gerrit.libreoffice.org/38497
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sc/source/ui/view/tabvwshb.cxx
@@ -158,7 +158,7 @@ void ScTabViewShell::ActivateObject( SdrOle2Obj* pObj, long nVerb )
         if ( !pClient )
             pClient = new ScClient( this, pWin, GetSdrView()->GetModel(), pObj );
 
-        if ( !(nErr & ERRCODE_ERROR_MASK) && xObj.is() )
+        if ( !nErr.IgnoreWarning() && xObj.is() )
         {
             tools::Rectangle aRect = pObj->GetLogicRect();
 

svtools/source/misc/ehdl.cxx
@@ -139,7 +139,7 @@ static DialogMask aWndFunc(
 }
 
 
-SfxErrorHandler::SfxErrorHandler(sal_uInt16 nIdP, sal_uLong lStartP, sal_uLong lEndP, ResMgr *pMgrP) :
+SfxErrorHandler::SfxErrorHandler(sal_uInt16 nIdP, ErrCode lStartP, ErrCode lEndP, ResMgr *pMgrP) :
 
     lStart(lStartP), lEnd(lEndP), nId(nIdP), pMgr(pMgrP), pFreeMgr( nullptr )
 
@@ -167,7 +167,7 @@ bool SfxErrorHandler::CreateString(const ErrorInfo *pErr, OUString &rStr) const
     */
 
 {
-    sal_uLong nErrCode = pErr->GetErrorCode() & ERRCODE_ERROR_MASK;
+    ErrCode nErrCode = pErr->GetErrorCode().IgnoreWarning();
     if( nErrCode>=lEnd || nErrCode<=lStart )
         return false;
     if(GetErrorString(nErrCode, rStr))
@@ -213,7 +213,7 @@ void SfxErrorHandler::GetClassString(sal_uLong lClassId, OUString &rStr)
     }
 }
 
-bool SfxErrorHandler::GetErrorString(sal_uLong lErrId, OUString &rStr) const
+bool SfxErrorHandler::GetErrorString(ErrCode lErrId, OUString &rStr) const
 
 /*  [Description]
 
@@ -229,7 +229,7 @@ bool SfxErrorHandler::GetErrorString(sal_uLong lErrId, OUString &rStr) const
     rStr = RID_ERRHDL_CLASS;
 
     ResStringArray aEr(ResId(nId, *pMgr));
-    sal_uInt32 nErrIdx = aEr.FindIndex((sal_uInt16)lErrId);
+    sal_uInt32 nErrIdx = aEr.FindIndex((sal_uInt16)(sal_uInt32)lErrId);
     if (nErrIdx != RESARRAY_INDEX_NOTFOUND)
     {
         rStr = rStr.replaceAll("$(ERROR)", aEr.GetString(nErrIdx));
@@ -241,7 +241,7 @@ bool SfxErrorHandler::GetErrorString(sal_uLong lErrId, OUString &rStr) const
     if( bRet )
     {
         OUString aErrStr;
-        GetClassString(lErrId & ERRCODE_CLASS_MASK,
+        GetClassString((sal_uInt32)lErrId & ERRCODE_CLASS_MASK,
                        aErrStr);
         if(!aErrStr.isEmpty())
             aErrStr += ".\n";
@@ -271,7 +271,7 @@ SfxErrorContext::SfxErrorContext(
 }
 
 
-bool SfxErrorContext::GetString(sal_uInt32 nErrId, OUString &rStr)
+bool SfxErrorContext::GetString(ErrCode nErrId, OUString &rStr)
 
 /*  [Description]
 
@@ -305,7 +305,7 @@ bool SfxErrorContext::GetString(sal_uInt32 nErrId, OUString &rStr)
 
         if ( bRet )
         {
-            sal_uInt16 nId = ( nErrId & ERRCODE_WARNING_MASK ) ? ERRCTX_WARNING : ERRCTX_ERROR;
+            sal_uInt16 nId = nErrId.IsWarning() ? ERRCTX_WARNING : ERRCTX_ERROR;
             ResStringArray aEr(ResId(RID_ERRCTX, *pMgr));
             rStr = rStr.replaceAll("$(ERR)", aEr.GetString(nId));
         }

uui/source/iahndl-errorhandler.cxx
@@ -156,11 +156,11 @@ UUIInteractionHelper::handleErrorHandlerRequest(
             = { RID_ERRHDL,
                 RID_SVXERRCODE,
                 RID_UUI_ERRHDL };
-        ErrCode nErrorId = nErrorCode & ~ERRCODE_WARNING_MASK;
-        Source eSource = nErrorId < ERRCODE_AREA_LIB1 ?
+        ErrCode nErrorId = nErrorCode.IgnoreWarning();
+        Source eSource = nErrorId < ErrCode(ERRCODE_AREA_LIB1) ?
             SOURCE_DEFAULT :
-            nErrorId >= ERRCODE_AREA_SVX
-            && nErrorId <= ERRCODE_AREA_SVX_END ?
+            nErrorId >= ErrCode(ERRCODE_AREA_SVX)
+            && nErrorId <= ErrCode(ERRCODE_AREA_SVX_END) ?
             SOURCE_SVX :
             SOURCE_UUI;
 

commit 3c69bb60bfdc87ba10d77c2822b82ba8d3a0551f

fix some ErrCode IgnoreWarning() usage
In some places I used the new IgnoreWarning() method, and that
was not a faithful translation of the previous code.

Regression from commit 528632660b72b105345945c13c5b68060d94a91b
"convert ErrCode to strong typedef"

Change-Id: Ie53f7a6b1830e5977510a0c32926e0c70c42dcd3

sc/source/ui/view/tabvwshb.cxx
@@ -156,7 +156,7 @@ void ScTabViewShell::ActivateObject( SdrOle2Obj* pObj, long nVerb )
         if ( !pClient )
             pClient = new ScClient( this, pWin, GetSdrView()->GetModel(), pObj );
 
-        if ( !nErr.IgnoreWarning() && xObj.is() )
+        if ( (sal_uInt32(nErr) & ERRCODE_ERROR_MASK) == 0 && xObj.is() )
         {
             tools::Rectangle aRect = pObj->GetLogicRect();
 

svtools/source/misc/ehdl.cxx
@@ -158,7 +158,7 @@ bool SfxErrorHandler::CreateString(const ErrorInfo *pErr, OUString &rStr) const
     */
 
 {
-    ErrCode nErrCode = pErr->GetErrorCode().IgnoreWarning();
+    ErrCode nErrCode(sal_uInt32(pErr->GetErrorCode()) & ERRCODE_ERROR_MASK);
     if( nErrCode>=lEnd || nErrCode<=lStart )
         return false;
     if(GetErrorString(nErrCode, rStr))

uui/source/iahndl-errorhandler.cxx
@@ -159,7 +159,7 @@ UUIInteractionHelper::handleErrorHandlerRequest(
             = { getRID_ERRHDL(),
                 getRID_SVXERRCODE(),
                 RID_UUI_ERRHDL };
-        ErrCode nErrorId = nErrorCode.IgnoreWarning();
+        ErrCode nErrorId(sal_uInt32(nErrorCode) & ~ERRCODE_WARNING_MASK);
         Source eSource = nErrorId < ErrCode(ERRCODE_AREA_SVX) ?
             SOURCE_DEFAULT :
             nErrorId >= ErrCode(ERRCODE_AREA_SVX)