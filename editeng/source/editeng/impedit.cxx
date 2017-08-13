5b3b0a91edc4c468c7c9c132cd066c0d25de9706
Noel Grandin <noelgrandin@gmail.com>
2017-08-13 13:37:28 +0200

    tdf#110988 Middle click does not paste primary selection
    regression from commit 65b7b6322b662785bf032e66c76abc36c9a2bb0e
    "loplugin:unusedenumconstants read-only constants in vcl"
    
    Change-Id: I262dd4c9cd1dc6eca82521f5fd9fbfc952fb7745
    Reviewed-on: https://gerrit.libreoffice.org/41111
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

--- editeng/source/editeng/impedit.cxx
+++ editeng/source/editeng/impedit.cxx
@@ -1349,7 +1349,13 @@ bool ImpEditView::MouseButtonUp( const MouseEvent& rMouseEvent )
     nExtraCursorFlags = GetCursorFlags::NONE;
     bClickedInSelection = false;
 
-    if ( rMouseEvent.IsLeft() && GetEditSelection().HasRange() )
+    if ( rMouseEvent.IsMiddle() && !bReadOnly &&
+         ( GetWindow()->GetSettings().GetMouseSettings().GetMiddleButtonAction() == MouseMiddleButtonAction::PasteSelection ) )
+    {
+        Reference<css::datatransfer::clipboard::XClipboard> aClipBoard(GetWindow()->GetPrimarySelection());
+        Paste( aClipBoard );
+    }
+    else if ( rMouseEvent.IsLeft() && GetEditSelection().HasRange() )
     {
         Reference<css::datatransfer::clipboard::XClipboard> aClipBoard(GetWindow()->GetPrimarySelection());
         CutCopy( aClipBoard, false );
