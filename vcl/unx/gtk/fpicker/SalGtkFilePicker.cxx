92b83918055de5553187f23a2c281c3c5eaaef55
Caolán McNamara <caolanm@redhat.com>
2017-08-15 16:07:47 +0100

    Resolves: tdf#111760 cancel file dialog on terminate
    schedule cancel dialog in query termination, take ownership of the desktop and
    re-schedule terminate to be re-attempted after the dialog is cancelled
    
    Change-Id: I5c061b47959baba138c6c23aaae365f28e9cf167

--- vcl/unx/gtk/fpicker/SalGtkFilePicker.cxx
+++ vcl/unx/gtk/fpicker/SalGtkFilePicker.cxx
@@ -888,10 +888,14 @@ sal_Int16 SAL_CALL SalGtkFilePicker::execute()
         awt::Toolkit::create(m_xContext),
         UNO_QUERY_THROW );
 
+    uno::Reference< frame::XDesktop > xDesktop(
+        frame::Desktop::create(m_xContext),
+        UNO_QUERY_THROW );
+
     GtkWindow *pParent = RunDialog::GetTransientFor();
     if (pParent)
         gtk_window_set_transient_for(GTK_WINDOW(m_pDialog), pParent);
-    RunDialog* pRunDialog = new RunDialog(m_pDialog, xToolkit);
+    RunDialog* pRunDialog = new RunDialog(m_pDialog, xToolkit, xDesktop);
     uno::Reference < awt::XTopWindowListener > xLifeCycle(pRunDialog);
     while( GTK_RESPONSE_NO == btn )
     {
@@ -984,7 +988,7 @@ sal_Int16 SAL_CALL SalGtkFilePicker::execute()
                             if (pParent)
                                 gtk_window_set_transient_for(GTK_WINDOW(dlg), pParent);
 #endif
-                            RunDialog* pAnotherDialog = new RunDialog(dlg, xToolkit);
+                            RunDialog* pAnotherDialog = new RunDialog(dlg, xToolkit, xDesktop);
                             uno::Reference < awt::XTopWindowListener > xAnotherLifeCycle(pAnotherDialog);
                             btn = pAnotherDialog->run();
 
