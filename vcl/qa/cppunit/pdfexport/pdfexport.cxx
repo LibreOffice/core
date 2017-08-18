8eb8e427d1e0757fe0add304cfdbe2d6aae4d9c9
Caolán McNamara <caolanm@redhat.com>
2017-08-18 16:26:34 +0100

    trivial WaE when pdfium is disabled
    Change-Id: I43a5061c9549a0aff217407ebd3cc5baf68ac60d
    Reviewed-on: https://gerrit.libreoffice.org/41334
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

--- vcl/qa/cppunit/pdfexport/pdfexport.cxx
+++ vcl/qa/cppunit/pdfexport/pdfexport.cxx
@@ -33,8 +33,6 @@ using namespace ::com::sun::star;
 namespace
 {
 
-const char* const DATA_DIRECTORY = "/vcl/qa/cppunit/pdfexport/data/";
-
 /// Tests the PDF export filter.
 class PdfExportTest : public test::BootstrapFixture, public unotest::MacrosTest
 {
@@ -121,6 +119,8 @@ void PdfExportTest::tearDown()
 }
 
 #if HAVE_FEATURE_PDFIUM
+
+const char* const DATA_DIRECTORY = "/vcl/qa/cppunit/pdfexport/data/";
 
 void PdfExportTest::load(const OUString& rFile, vcl::filter::PDFDocument& rDocument)
 {
