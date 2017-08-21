LibreOffice/core

commit d347c2403605c5aa3ddd98fb605366914acab79f

convert std::map::insert to std::map::emplace
which is considerably less verbose

Change-Id: Ifa373e8eb09e39bd6c8d3578641610a6055a187b
Reviewed-on: https://gerrit.libreoffice.org/40978
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

ucb/source/ucp/webdav/DAVSessionFactory.cxx
@@ -57,7 +57,7 @@ rtl::Reference< DAVSession > DAVSessionFactory::createDAVSession(
         std::unique_ptr< DAVSession > xElement(
             new SerfSession( this, inUri, *m_xProxyDecider.get() ) );
 
-        aIt = m_aMap.insert( Map::value_type( inUri, xElement.get() ) ).first;
+        aIt = m_aMap.emplace(  inUri, xElement.get() ) ).first;
         aIt->second->m_aContainerIt = aIt;
         xElement.release();
         return aIt->second;

ucb/source/ucp/webdav-neon/DAVSessionFactory.cxx
@@ -70,7 +70,7 @@ rtl::Reference< DAVSession > DAVSessionFactory::createDAVSession(
         std::unique_ptr< DAVSession > xElement(
             new NeonSession( this, inUri, rFlags, *m_xProxyDecider.get() ) );
 
-        aIt = m_aMap.insert( Map::value_type( inUri, xElement.get() ) ).first;
+        aIt = m_aMap.emplace( inUri, xElement.get() ).first;
         aIt->second->m_aContainerIt = aIt;
         xElement.release();
         return aIt->second;

commit 0e35b7738d9f276c0566df0f2cc0f1eed7900d6c

fix build regression from d347c2403605c5aa3ddd98fb605366914acab79f
Change-Id: Ia9e017732814d3f5b1f2efdaef45d96aa22daa46

ucb/source/ucp/webdav/DAVSessionFactory.cxx
@@ -57,7 +57,7 @@ rtl::Reference< DAVSession > DAVSessionFactory::createDAVSession(
         std::unique_ptr< DAVSession > xElement(
             new SerfSession( this, inUri, *m_xProxyDecider.get() ) );
 
-        aIt = m_aMap.emplace(  inUri, xElement.get() ) ).first;
+        aIt = m_aMap.emplace(  inUri, xElement.get() ).first;
         aIt->second->m_aContainerIt = aIt;
         xElement.release();
         return aIt->second;