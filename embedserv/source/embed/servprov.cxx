19efa825090f1b41bcbc2f6d84ee0102cf4bb259
Stephan Bergmann <sbergman@redhat.com>
2017-08-21 11:12:02 +0200

    loplugin:unnecessaryparen (clang-cl)
    Change-Id: I11a535a5271ec15965877986f15adc0804c00a4f

--- embedserv/source/embed/servprov.cxx
+++ embedserv/source/embed/servprov.cxx
@@ -71,13 +71,13 @@ CurThreadData::~CurThreadData()
 bool CurThreadData::setData(void *pData)
 {
     OSL_ENSURE( m_hKey, "No thread key!" );
-    return (osl_setThreadKeyData(m_hKey, pData));
+    return osl_setThreadKeyData(m_hKey, pData);
 }
 
 void *CurThreadData::getData()
 {
     OSL_ENSURE( m_hKey, "No thread key!" );
-    return (osl_getThreadKeyData(m_hKey));
+    return osl_getThreadKeyData(m_hKey);
 }
 
 void o2u_attachCurrentThread()
