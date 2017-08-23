a6192af8dcf8866118669a4b71c25f6a7688d95d
Tor Lillqvist <tml@collabora.com>
2017-08-23 16:33:50 +0300

    Check whether it is the multi-threaded case explicitly
    Change-Id: I96bed3ebd90af9a8ab2c17b678384cf340ba2802

--- sc/inc/formulacell.hxx
+++ sc/inc/formulacell.hxx
@@ -150,7 +150,7 @@ public:
                         SCITP_FROM_ITERATION,
                         SCITP_CLOSE_ITERATION_CIRCLE
                     };
-    void            InterpretTail( ScInterpretTailParameter, bool bSingleThreaded );
+    void            InterpretTail( ScInterpretTailParameter );
 
     void            HandleStuffAfterParallelCalculation();
 
