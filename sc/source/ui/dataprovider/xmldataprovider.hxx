e439ace4495d5ea0be06de007f1484a239bfe6a4
Markus Mohrhard <markus.mohrhard@googlemail.com>
2017-08-23 07:26:31 +0200

    external data: initial work on xml data provider
    Change-Id: Ic54d91d935c451d1a6cde61f85e6b8a77975f7cd

---
+++ sc/source/ui/dataprovider/xmldataprovider.hxx
@@ -0,0 +1,48 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef INCLUDED_SC_SOURCE_UI_DATAPROVIDER_XMLDATAPROVIDER_HXX
+#define INCLUDED_SC_SOURCE_UI_DATAPROVIDER_XMLDATAPROVIDER_HXX
+
+#include "dataprovider.hxx"
+
+namespace sc {
+
+class XMLFetchThread;
+
+class XMLDataProvider : public DataProvider
+{
+    OUString maURL;
+    OUString maID;
+    ScDocument* mpDocument;
+    ScDBDataManager* mpDBDataManager;
+    rtl::Reference<XMLFetchThread> mxXMLFetchThread;
+
+    std::unique_ptr<ScDocument> mpDoc;
+    Idle maIdle;
+
+public:
+
+    XMLDataProvider(ScDocument* pDoc, const OUString& rURL, ScDBDataManager* pDBManager, const OUString& rID);
+    virtual ~XMLDataProvider();
+
+    virtual void Import() override;
+
+    virtual const OUString& GetURL() const override;
+
+    virtual std::map<OUString, OUString> getDataSourcesForURL(const OUString& rURL);
+
+    DECL_LINK( ImportFinishedHdl, Timer*, void );
+};
+
+}
+
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
