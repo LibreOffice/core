e8ddee994e945b9ea8247801bae6b9b7ee18437f
Mike Kaganski <mike.kaganski@collabora.com>
2017-08-23 09:09:57 +0300

    tdf#111964: only trim XML whitespace
    OUString::trim() uses rtl_uString_newTrim, which relies upon
    rtl_ImplIsWhitespace. The latter treats as whitespaces not only
    characters with values less than or equal to 32, but also Unicode
    General Punctuation area Space and some Control characters. Thus,
    using OUString::trim() is incorrect when the goal is to trim XML
    whitespace, which is defined as one of 0x09, 0x0A, 0x0D, 0x20.
    
    The comments for OUString::trim() and rtl_uString_newTrim are
    corrected to describe which characters are considered whitespace.
    
    A unit test included.
    
    Change-Id: I45a132be923a52dcd5a4c35aeecb53d423b49fec
    Reviewed-on: https://gerrit.libreoffice.org/41444
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
    Tested-by: Mike Kaganski <mike.kaganski@collabora.com>

--- sw/qa/extras/ooxmlexport/ooxmlexport9.cxx
+++ sw/qa/extras/ooxmlexport/ooxmlexport9.cxx
@@ -564,6 +564,16 @@ DECLARE_OOXMLEXPORT_TEST(tdf112169, "tdf112169.odt")
     // LO crashed while export because of chararacter background color handling
 }
 
+DECLARE_OOXMLEXPORT_TEST(testTdf111964, "tdf111964.docx")
+{
+    xmlDocPtr pXmlDoc = parseExport("word/document.xml");
+    if (!pXmlDoc)
+        return;
+    // Unicode spaces that are not XML whitespace must not be trimmed
+    const sal_Unicode sWSReference [] { 0x2002, 0x2002, 0x2002, 0x2002, 0x2002, 0 };
+    assertXPathContent(pXmlDoc, "/w:document/w:body/w:p/w:r[4]/w:t", sWSReference);
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
