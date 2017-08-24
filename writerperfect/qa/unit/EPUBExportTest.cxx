a3e5762d56b68fb8ffdbf39f20c288a02077bcfc
Miklos Vajna <vmiklos@collabora.co.uk>
2017-08-24 18:08:11 +0200

    EPUB export: make sure that the mimetype stream is not compressed
    Similar to ODF, the spec mandates this, and recent enough epubcheck
    validator asserts this.
    
    Also backport 2 libepubgen commits that fix other validator errors
    around missing mimetypes / malformed URLs.
    
    Change-Id: I29f0524465a30d26585cea92ec27bd336f6a17d8

--- writerperfect/qa/unit/EPUBExportTest.cxx
+++ writerperfect/qa/unit/EPUBExportTest.cxx
@@ -30,14 +30,18 @@ class EPUBExportTest : public test::BootstrapFixture, public unotest::MacrosTest
 {
     uno::Reference<uno::XComponentContext> mxComponentContext;
     uno::Reference<lang::XComponent> mxComponent;
+    utl::TempFile maTempFile;
 
 public:
     virtual void setUp() override;
     virtual void tearDown() override;
+    void createDoc(const OUString &rFile);
     void testOutlineLevel();
+    void testMimetype();
 
     CPPUNIT_TEST_SUITE(EPUBExportTest);
     CPPUNIT_TEST(testOutlineLevel);
+    CPPUNIT_TEST(testMimetype);
     CPPUNIT_TEST_SUITE_END();
 };
 
@@ -57,26 +61,46 @@ void EPUBExportTest::tearDown()
     test::BootstrapFixture::tearDown();
 }
 
-void EPUBExportTest::testOutlineLevel()
+void EPUBExportTest::createDoc(const OUString &rFile)
 {
     // Import the bugdoc and export as EPUB.
-    OUString aURL = m_directories.getURLFromSrc(DATA_DIRECTORY) + "outline-level.fodt";
+    OUString aURL = m_directories.getURLFromSrc(DATA_DIRECTORY) + rFile;
     mxComponent = loadFromDesktop(aURL);
     CPPUNIT_ASSERT(mxComponent.is());
 
     uno::Reference<frame::XStorable> xStorable(mxComponent, uno::UNO_QUERY);
-    utl::TempFile aTempFile;
-    aTempFile.EnableKillingFile();
+    maTempFile.EnableKillingFile();
     utl::MediaDescriptor aMediaDescriptor;
     aMediaDescriptor["FilterName"] <<= OUString("EPUB");
-    xStorable->storeToURL(aTempFile.GetURL(), aMediaDescriptor.getAsConstPropertyValueList());
+    xStorable->storeToURL(maTempFile.GetURL(), aMediaDescriptor.getAsConstPropertyValueList());
+}
+
+void EPUBExportTest::testOutlineLevel()
+{
+    createDoc("outline-level.fodt");
 
     // Make sure that the output is split into two.
-    uno::Reference<packages::zip::XZipFileAccess2> xNameAccess = packages::zip::ZipFileAccess::createWithURL(mxComponentContext, aTempFile.GetURL());
+    uno::Reference<packages::zip::XZipFileAccess2> xNameAccess = packages::zip::ZipFileAccess::createWithURL(mxComponentContext, maTempFile.GetURL());
     CPPUNIT_ASSERT(xNameAccess->hasByName("OEBPS/sections/section0001.xhtml"));
     // This failed, output was a single section.
     CPPUNIT_ASSERT(xNameAccess->hasByName("OEBPS/sections/section0002.xhtml"));
     CPPUNIT_ASSERT(!xNameAccess->hasByName("OEBPS/sections/section0003.xhtml"));
+}
+
+void EPUBExportTest::testMimetype()
+{
+    createDoc("hello.fodt");
+
+    // Check that the mime type is written uncompressed at the expected location.
+    SvFileStream aFileStream(maTempFile.GetURL(), StreamMode::READ);
+    SvMemoryStream aMemoryStream;
+    aMemoryStream.WriteStream(aFileStream);
+    OString aExpected("application/epub+zip");
+    CPPUNIT_ASSERT(aMemoryStream.GetSize() > static_cast<sal_uInt64>(38 + aExpected.getLength()));
+
+    OString aActual(static_cast<const char *>(aMemoryStream.GetBuffer()) + 38, aExpected.getLength());
+    // This failed: actual data was some garbage, not the uncompressed mime type.
+    CPPUNIT_ASSERT_EQUAL(aExpected, aActual);
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(EPUBExportTest);
