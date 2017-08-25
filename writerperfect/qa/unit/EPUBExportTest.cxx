3a1d6a2bccd05e5859ab44593609d11c39fc518a
Miklos Vajna <vmiklos@collabora.co.uk>
2017-08-25 14:10:01 +0200

    EPUB export: allow setting a custom version
    By parsing the FilterData key of the media descriptor, similar to how
    the PDF export does it.
    
    The default is still EPUB3, but this now exposes the ability of
    libepubgen to emit EPUB2 if wanted.
    
    Change-Id: I23834fa28db9b01ef4cce4a142331b1cc9ecb3c2
    Reviewed-on: https://gerrit.libreoffice.org/41566
    Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
    Tested-by: Jenkins <ci@libreoffice.org>

--- writerperfect/qa/unit/EPUBExportTest.cxx
+++ writerperfect/qa/unit/EPUBExportTest.cxx
@@ -12,11 +12,14 @@
 #include <com/sun/star/packages/zip/ZipFileAccess.hpp>
 
 #include <comphelper/processfactory.hxx>
+#include <comphelper/propertysequence.hxx>
 #include <cppuhelper/implbase.hxx>
 #include <test/bootstrapfixture.hxx>
+#include <test/xmltesttools.hxx>
 #include <unotest/macros_test.hxx>
 #include <unotools/mediadescriptor.hxx>
 #include <unotools/tempfile.hxx>
+#include <unotools/ucbstreamhelper.hxx>
 
 using namespace ::com::sun::star;
 
@@ -26,22 +29,25 @@ namespace
 char const DATA_DIRECTORY[] = "/writerperfect/qa/unit/data/writer/epubexport/";
 
 /// Tests the EPUB export filter.
-class EPUBExportTest : public test::BootstrapFixture, public unotest::MacrosTest
+class EPUBExportTest : public test::BootstrapFixture, public unotest::MacrosTest, public XmlTestTools
 {
     uno::Reference<uno::XComponentContext> mxComponentContext;
     uno::Reference<lang::XComponent> mxComponent;
     utl::TempFile maTempFile;
 
 public:
-    virtual void setUp() override;
-    virtual void tearDown() override;
-    void createDoc(const OUString &rFile);
+    void setUp() override;
+    void tearDown() override;
+    void registerNamespaces(xmlXPathContextPtr &pXmlXpathCtx) override;
+    void createDoc(const OUString &rFile, const uno::Sequence<beans::PropertyValue> &rFilterData);
     void testOutlineLevel();
     void testMimetype();
+    void testEPUB2();
 
     CPPUNIT_TEST_SUITE(EPUBExportTest);
     CPPUNIT_TEST(testOutlineLevel);
     CPPUNIT_TEST(testMimetype);
+    CPPUNIT_TEST(testEPUB2);
     CPPUNIT_TEST_SUITE_END();
 };
 
@@ -61,7 +67,12 @@ void EPUBExportTest::tearDown()
     test::BootstrapFixture::tearDown();
 }
 
-void EPUBExportTest::createDoc(const OUString &rFile)
+void EPUBExportTest::registerNamespaces(xmlXPathContextPtr &pXmlXpathCtx)
+{
+    xmlXPathRegisterNs(pXmlXpathCtx, BAD_CAST("opf"), BAD_CAST("http://www.idpf.org/2007/opf"));
+}
+
+void EPUBExportTest::createDoc(const OUString &rFile, const uno::Sequence<beans::PropertyValue> &rFilterData)
 {
     // Import the bugdoc and export as EPUB.
     OUString aURL = m_directories.getURLFromSrc(DATA_DIRECTORY) + rFile;
@@ -72,12 +83,13 @@ void EPUBExportTest::createDoc(const OUString &rFile)
     maTempFile.EnableKillingFile();
     utl::MediaDescriptor aMediaDescriptor;
     aMediaDescriptor["FilterName"] <<= OUString("EPUB");
+    aMediaDescriptor["FilterData"] <<= rFilterData;
     xStorable->storeToURL(maTempFile.GetURL(), aMediaDescriptor.getAsConstPropertyValueList());
 }
 
 void EPUBExportTest::testOutlineLevel()
 {
-    createDoc("outline-level.fodt");
+    createDoc("outline-level.fodt", {});
 
     // Make sure that the output is split into two.
     uno::Reference<packages::zip::XZipFileAccess2> xNameAccess = packages::zip::ZipFileAccess::createWithURL(mxComponentContext, maTempFile.GetURL());
@@ -89,7 +101,7 @@ void EPUBExportTest::testOutlineLevel()
 
 void EPUBExportTest::testMimetype()
 {
-    createDoc("hello.fodt");
+    createDoc("hello.fodt", {});
 
     // Check that the mime type is written uncompressed at the expected location.
     SvFileStream aFileStream(maTempFile.GetURL(), StreamMode::READ);
@@ -101,6 +113,24 @@ void EPUBExportTest::testMimetype()
     OString aActual(static_cast<const char *>(aMemoryStream.GetBuffer()) + 38, aExpected.getLength());
     // This failed: actual data was some garbage, not the uncompressed mime type.
     CPPUNIT_ASSERT_EQUAL(aExpected, aActual);
+}
+
+void EPUBExportTest::testEPUB2()
+{
+    uno::Sequence<beans::PropertyValue> aFilterData(comphelper::InitPropertySequence(
+    {
+        // Explicitly request EPUB2.
+        {"EPUBVersion", uno::makeAny(static_cast<sal_Int32>(20))}
+    }));
+    createDoc("hello.fodt", aFilterData);
+
+    uno::Reference<packages::zip::XZipFileAccess2> xNameAccess = packages::zip::ZipFileAccess::createWithURL(mxComponentContext, maTempFile.GetURL());
+    uno::Reference<io::XInputStream> xInputStream(xNameAccess->getByName("OEBPS/content.opf"), uno::UNO_QUERY);
+    std::shared_ptr<SvStream> pStream(utl::UcbStreamHelper::CreateStream(xInputStream, true));
+    xmlDocPtr pXmlDoc = parseXmlStream(pStream.get());
+    // This was 3.0, EPUBVersion filter option was ignored and we always emitted EPUB3.
+    assertXPath(pXmlDoc, "/opf:package", "version", "2.0");
+    xmlFreeDoc(pXmlDoc);
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(EPUBExportTest);
