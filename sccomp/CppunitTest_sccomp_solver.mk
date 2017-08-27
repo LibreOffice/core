c0ae901bc6acef7cf160723d0e4b5ebe3979bac2
Rene Engelhard <rene@debian.org>
2017-08-27 16:15:31 +0200

    sccomp_lpsolver -> sccomp_solver
    This test also tests the CoinMP solver, no _lpsolver is misleading
    
    Change-Id: If56de3aa84ec80b1888aadeb940e08e65083c9e9

---
+++ sccomp/CppunitTest_sccomp_solver.mk
@@ -0,0 +1,47 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_CppunitTest_CppunitTest,sccomp_solver))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,sccomp_solver,\
+	sccomp/qa/unit/solver \
+))
+
+$(eval $(call gb_CppunitTest_use_externals,sccomp_solver,\
+	boost_headers \
+))
+
+$(eval $(call gb_CppunitTest_add_defs,sccomp_solver,\
+	$(if $(ENABLE_COINMP), -DENABLE_COINMP) \
+	$(if $(ENABLE_LPSOLVE), -DENABLE_LPSOLVE) \
+))
+
+$(eval $(call gb_CppunitTest_use_libraries,sccomp_solver,\
+	cppu \
+	sal \
+	test \
+	unotest \
+))
+
+$(eval $(call gb_CppunitTest_set_include,sccomp_solver,\
+	-I$(SRCDIR)/sc/inc \
+	$$(INCLUDE) \
+))
+
+
+$(eval $(call gb_CppunitTest_use_sdk_api,sccomp_solver))
+
+$(eval $(call gb_CppunitTest_use_ure,sccomp_solver))
+$(eval $(call gb_CppunitTest_use_vcl,sccomp_solver))
+
+$(eval $(call gb_CppunitTest_use_rdb,sccomp_solver,services))
+
+$(eval $(call gb_CppunitTest_use_configuration,sccomp_solver))
+
+# vim: set noet sw=4 ts=4:
