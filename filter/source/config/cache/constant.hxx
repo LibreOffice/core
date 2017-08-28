c214aad2e4b7d58d4a44ba9823474c12189b9d79
Jochen Nitschke <j.nitschke+logerrit@ok.de>
2017-08-28 05:31:07 +0200

    filterconfig: drop unused debug macros
    logging functionality went in
    commit 0056fe28c7bd8e72cd5bdd335f098d00cd0d5d11
    Date:   Mon Feb 7 20:11:01 2011 +0100
        Remove lots of dead code
    
    it was dead since initial commit in 2004
    
    Change-Id: Idd4393854014e997e4e044dc75c16a76dae23e95

--- filter/source/config/cache/constant.hxx
+++ filter/source/config/cache/constant.hxx
@@ -19,8 +19,6 @@
 #ifndef INCLUDED_FILTER_SOURCE_CONFIG_CACHE_CONSTANT_HXX
 #define INCLUDED_FILTER_SOURCE_CONFIG_CACHE_CONSTANT_HXX
 
-#include "macros.hxx"
-
 /*  disable impl_loadOnDemand function of BaseContainer for certain
     functions, where it the feature "impl_loadItemOnDemand() of class FilterCache
     can be used instead of loadAll()!*/
