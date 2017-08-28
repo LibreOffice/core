8be388e5a07eb249d851fd3324b5d75ad22e32c4
Caolán McNamara <caolanm@redhat.com>
2017-08-28 10:17:56 +0100

    ofz#3110 GeneratePLCF only generated word6 sized missing entries
    BTE was 2 bytes in word 7- but 4 bytes in word 8+
    
    Change-Id: I24007d26fccc5edc104320bd2eb8f9c62399c988

--- sw/source/filter/ww8/ww8scan.cxx
+++ sw/source/filter/ww8/ww8scan.cxx
@@ -2147,7 +2147,7 @@ void WW8PLCF::GeneratePLCF(SvStream& rSt, sal_Int32 nPN, sal_Int32 ncpN)
 
     if (!failure)
     {
-        size_t nSiz = 6 * nIMax + 4;
+        size_t nSiz = (4 + nStru) * nIMax + 4;
         size_t nElems = ( nSiz + 3 ) / 4;
         pPLCF_PosArray.reset( new sal_Int32[ nElems ] ); // Pointer to Pos-array
 
@@ -2202,7 +2202,7 @@ void WW8PLCF::GeneratePLCF(SvStream& rSt, sal_Int32 nPN, sal_Int32 ncpN)
         for (sal_Int32 i = 0; i < ncpN; ++i)         // construct PNs
         {
             ShortToSVBT16(static_cast<sal_uInt16>(nPN + i), p);
-            p+=2;
+            p += nStru;
         }
     }
 
