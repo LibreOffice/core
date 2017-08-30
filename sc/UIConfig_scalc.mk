f6c2bac838ab74c4b7d9b0bf4f656a8ec14dceb9
andreas kainz <kainz.a@gmail.com>
2017-08-31 00:19:40 +0200

    tdf#107158 Groupedbar variant of the Notebookbar for Calc
    Change-Id: I8f6a086a0895d54cce6243f65c3f47f7cc4cf50e

--- sc/UIConfig_scalc.mk
+++ sc/UIConfig_scalc.mk
@@ -146,6 +146,8 @@ $(eval $(call gb_UIConfig_add_uifiles,modules/scalc,\
 	sc/uiconfig/scalc/ui/namerangesdialog \
 	sc/uiconfig/scalc/ui/notebookbar \
 	sc/uiconfig/scalc/ui/notebookbar_groups \
+	sc/uiconfig/scalc/ui/notebookbar_groupedbar_full \
+	sc/uiconfig/scalc/ui/notebookbar_groupedbar_compact \
 	sc/uiconfig/scalc/ui/managenamesdialog \
 	sc/uiconfig/scalc/ui/mergecellsdialog \
 	sc/uiconfig/scalc/ui/movecopysheet \
