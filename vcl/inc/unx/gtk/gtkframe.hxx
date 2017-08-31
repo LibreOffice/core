c4840d6794db76eafa852a6c02d8948a561ced37
Caolán McNamara <caolanm@redhat.com>
2017-08-31 21:08:17 +0100

    remove some casting
    Change-Id: Idf6c82b0c609d8680e2017602bfe9a93acb093b9

--- vcl/inc/unx/gtk/gtkframe.hxx
+++ vcl/inc/unx/gtk/gtkframe.hxx
@@ -55,7 +55,7 @@ class GtkSalGraphics;
 class GtkSalDisplay;
 
 #if GTK_CHECK_VERSION(3,0,0)
-typedef ::Window GdkNativeWindow;
+typedef sal_uIntPtr GdkNativeWindow;
 #define GDK_WINDOW_XWINDOW(o) GDK_WINDOW_XID(o)
 #define gdk_set_sm_client_id(i) gdk_x11_set_sm_client_id(i)
 #define gdk_window_foreign_new_for_display(a,b) gdk_x11_window_foreign_new_for_display(a,b)
