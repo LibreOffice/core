5b43b2eb176fc3e2113b03fec3e9f3d8cfe9e0da
Dennis Francis <dennis.francis@collabora.co.uk>
2017-08-31 14:22:15 +0530

    tdf#111974: Ensure sufficient space for all sheets in FormulaBuffer...
    and not just for the number of sheet fragments. This is important
    because we changed the internal storage of FormulaBuffer from based
    on std::map to std::vector in d17a83fa549f828f29e6939b16ba8b568a75f95e
    
    Added simple unit test that loads the bugdoc in
    subsequent_filters-test.cxx
    
    Reviewed-on: https://gerrit.libreoffice.org/41792
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Dennis Francis <dennis.francis@collabora.co.uk>
    (cherry picked from commit 20169423d492b58ab93fb0e8e9d2918c0baebc92)
    
    Reviewed-on: https://gerrit.libreoffice.org/41841
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Eike Rathke <erack@redhat.com>
    
     Conflicts:
    	sc/qa/unit/subsequent_filters-test.cxx
    
    Backported.
    
    Change-Id: I7320dd537266e540d585d3fccfbb8665bba457c7

--- sc/qa/unit/subsequent_filters-test.cxx
+++ sc/qa/unit/subsequent_filters-test.cxx
@@ -240,6 +240,7 @@ public:
     void testTdf100458();
     void testTdf100709XLSX();
     void testTdf97598XLSX();
+    void testTdf111974XLSM();
 
     CPPUNIT_TEST_SUITE(ScFiltersTest);
     CPPUNIT_TEST(testBooleanFormatXLSX);
@@ -357,6 +358,7 @@ public:
     CPPUNIT_TEST(testTdf100458);
     CPPUNIT_TEST(testTdf100709XLSX);
     CPPUNIT_TEST(testTdf97598XLSX);
+    CPPUNIT_TEST(testTdf111974XLSM);
 
     CPPUNIT_TEST_SUITE_END();
 
@@ -3758,6 +3760,14 @@ void ScFiltersTest::testColumnStyle2XLSX()
         CPPUNIT_ASSERT_EQUAL(OUString("Linux Biolinum G"), aName);
     }
 
+    xDocSh->DoClose();
+}
+
+void ScFiltersTest::testTdf111974XLSM()
+{
+    // Would crash without the fix on loading
+    ScDocShellRef xDocSh = loadDoc("tdf111974.", FORMAT_XLSM);
+    CPPUNIT_ASSERT_MESSAGE("Failed to open doc", xDocSh.is());
     xDocSh->DoClose();
 }
 
