3576aa7a7328b04faf222451d73681db7c1d9c54
Caolán McNamara <caolanm@redhat.com>
2017-08-31 13:16:25 +0100

    Resolves: tdf#112123 check m_xAutoStyles like in similar methods
    Change-Id: I099da789a0e64fd862d09bb462ccb699e1dc8b9f
    Reviewed-on: https://gerrit.libreoffice.org/41753
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Michael Stahl <mstahl@redhat.com>

--- xmloff/source/text/txtimp.cxx
+++ xmloff/source/text/txtimp.cxx
@@ -2430,6 +2430,9 @@ SvXMLImportContext *XMLTextImportHelper::CreateTableChildContext(
 sal_Int32 XMLTextImportHelper::GetDataStyleKey(const OUString& sStyleName,
                                                bool* pIsSystemLanguage )
 {
+    if (!m_xImpl->m_xAutoStyles.is())
+        return -1;
+
     const SvXMLStyleContext* pStyle =
         static_cast<SvXMLStylesContext *>(m_xImpl->m_xAutoStyles.get())->
                   FindStyleChildContext( XML_STYLE_FAMILY_DATA_STYLE,
