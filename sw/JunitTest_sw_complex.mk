7cee698f50f623a5fcbd971f6c3f389205a122dd
jmzambon <jeanmarczambon@gmail.com>
2017-09-03 19:09:57 +0200

    tdf#97362: TextPortionEnumerationTest partially migrated to python (last part)
    Convert tests:
    - test_meta_xtextattach_toxmark
    - test_meta_xtextattach_refmark
    - test_meta_xtextattach_textfield
    - test_meta_xtextattach_footnote
    - test_meta_xtextattach_meta
    - do_meta_xtextattach
    - test_metafield_xtextfield
    - test_metafield_xpropertyset
    - test_load_store
    - test_load_store_xmlid
    
    Change-Id: I471329e8088b533eedec6a55c521ed313f1960b4

--- sw/JunitTest_sw_complex.mk
+++ sw/JunitTest_sw_complex.mk
@@ -28,7 +28,6 @@ $(eval $(call gb_JunitTest_add_sourcefiles,sw_complex,\
     sw/qa/complex/accessibility/AccessibleRelationSet \
     sw/qa/complex/indeterminateState/CheckIndeterminateState \
     sw/qa/complex/writer/TestDocument \
-    sw/qa/complex/writer/TextPortionEnumerationTest \
 ))
 
 $(eval $(call gb_JunitTest_use_jars,sw_complex,\
@@ -41,7 +40,6 @@ $(eval $(call gb_JunitTest_use_jars,sw_complex,\
 
 $(eval $(call gb_JunitTest_add_classes,sw_complex,\
     complex.accessibility.AccessibleRelationSet \
-    complex.writer.TextPortionEnumerationTest \
 ))
 
 # vim: set noet sw=4 ts=4:
