6721e7c7e60dd810e3b1a5a3b094ca6ed332fc33
Grzegorz Araminowicz <g.araminowicz@gmail.com>
2017-09-03 17:36:50 +0200

    SmartArt: separate unit tests
    Change-Id: I9bf522caae155de354c2d28dd92d85e4c016aa14

--- sd/Module_sd.mk
+++ sd/Module_sd.mk
@@ -30,6 +30,7 @@ $(eval $(call gb_Module_add_check_targets,sd,\
 	$(if $(and $(filter $(COM),MSC),$(MERGELIBS)),, \
 		CppunitTest_sd_uimpress) \
     CppunitTest_sd_import_tests \
+    CppunitTest_sd_import_tests_smartart \
     CppunitTest_sd_export_ooxml1 \
     CppunitTest_sd_export_ooxml2 \
     CppunitTest_sd_export_tests \
