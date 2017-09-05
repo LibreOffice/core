688b30f33510a7b8c5989daa86cba9252031142c
Tor Lillqvist <tml@collabora.com>
2017-09-05 11:20:22 +0300

    Revert "Make two slightly mysterious static local variables thread_local"
    Temporarily revert until somebody can figure out what to do with the
    obsolete Mac toolchain on some tinderbox.
    
    This reverts commit 5f847b3d197a96e5c2bab3d9129d3dd9d5b75873.

--- sc/source/core/data/formulacell.cxx
+++ sc/source/core/data/formulacell.cxx
@@ -2658,8 +2658,8 @@ sc::MatrixEdge ScFormulaCell::GetMatrixEdge( ScAddress& rOrgPos ) const
         case ScMatrixMode::Formula :
         case ScMatrixMode::Reference :
         {
-            static thread_local SCCOL nC;
-            static thread_local SCROW nR;
+            static SCCOL nC;
+            static SCROW nR;
             ScAddress aOrg;
             if ( !GetMatrixOrigin( aOrg ) )
                 return sc::MatrixEdge::Nothing;
