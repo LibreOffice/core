812c19e173a6246ad3f683b5546d795415fe1c50
Michael Stahl <mstahl@redhat.com>
2017-09-05 17:24:14 +0200

    libxml2: upgrade to release 2.9.5
    * drop ubsan.patch.0: presumably fixed upstream
    * drop 0001-* CVE fixes: fixed upstream
    
    Change-Id: I3e2a53b5ef82ef8edd85e812fd5dee67ab60db94
    Reviewed-on: https://gerrit.libreoffice.org/41951
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Michael Stahl <mstahl@redhat.com>
    (cherry picked from commit 2b329599183107735e1a7b6aad516452eb6fa6a6)

--- external/libxml2/ExternalPackage_xml2.mk
+++ external/libxml2/ExternalPackage_xml2.mk
@@ -21,7 +21,7 @@ else # COM=MSC
 $(eval $(call gb_ExternalPackage_add_file,xml2,$(LIBO_URE_LIB_FOLDER)/libxml2.dll,win32/bin.msvc/libxml2.dll))
 endif
 else # OS!=WNT
-$(eval $(call gb_ExternalPackage_add_file,xml2,$(LIBO_URE_LIB_FOLDER)/libxml2.so.2,.libs/libxml2.so.2.9.4))
+$(eval $(call gb_ExternalPackage_add_file,xml2,$(LIBO_URE_LIB_FOLDER)/libxml2.so.2,.libs/libxml2.so.2.9.5))
 endif
 endif # DISABLE_DYNLOADING
 
