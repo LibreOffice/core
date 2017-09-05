45bc0b931eb2569bf267d226b67e58b7b8390529
Caolán McNamara <caolanm@redhat.com>
2017-09-05 13:06:20 +0100

    Resolves: tdf#112180: avoid crash with specific ttf
    Change-Id: I8cde147279173bffec0c991eb7676f5d4641138d
    Reviewed-on: https://gerrit.libreoffice.org/41939
    Reviewed-by: Michael Stahl <mstahl@redhat.com>
    Tested-by: Jenkins <ci@libreoffice.org>

--- vcl/win/gdi/salfont.cxx
+++ vcl/win/gdi/salfont.cxx
@@ -87,9 +87,9 @@ RawFontData::RawFontData( HDC hDC, DWORD nTableTag )
 {
     // get required size in bytes
     mnByteCount = ::GetFontData( hDC, nTableTag, 0, nullptr, 0 );
-    if( mnByteCount == GDI_ERROR )
-        return;
-    else if( !mnByteCount )
+    if (mnByteCount == GDI_ERROR)
+        mnByteCount = 0;
+    if (!mnByteCount)
         return;
 
     // allocate the array
