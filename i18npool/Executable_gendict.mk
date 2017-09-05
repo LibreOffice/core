d763f23f13d66ddccbf4f5ade511d37240cbc442
jan iversen <jani@libreoffice.org>
2017-09-05 21:04:13 +0200

    Revert "iOS, Executable_gendict.mk contained work ifneq"
    break android build, will try another idea.
    
    This reverts commit 9915e8d250639348ca06622ffae840f207be8eec.
    
    Change-Id: I4e4e73733febd8d3bb7469de76163e8d16b7a5c7
    Reviewed-on: https://gerrit.libreoffice.org/41958
    Reviewed-by: jan iversen <jani@libreoffice.org>
    Tested-by: jan iversen <jani@libreoffice.org>

--- i18npool/Executable_gendict.mk
+++ i18npool/Executable_gendict.mk
@@ -14,7 +14,7 @@ $(eval $(call gb_Executable_use_libraries,gendict,\
 ))
 
 ifeq ($(gb_Side),build)
-ifeq ($(shell grep OS=IOS $(BUILDDIR)/config_host.mk),)
+ifneq ($(shell grep OS=IOS $(BUILDDIR)/config_host.mk),)
 $(eval $(call gb_Executable_add_cxxflags,gendict,\
 	-DDICT_JA_ZH_IN_DATAFILE \
 ))
