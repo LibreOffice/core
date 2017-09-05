5a0d302d335e52c44ffcbcbeb6fa587ef5f95384
Michael Stahl <mstahl@redhat.com>
2017-09-05 17:24:14 +0200

    libxml2: upgrade to release 2.9.5
    * drop ubsan.patch.0: presumably fixed upstream
    * drop 0001-* CVE fixes: fixed upstream
    
    Change-Id: I3e2a53b5ef82ef8edd85e812fd5dee67ab60db94
    Reviewed-on: https://gerrit.libreoffice.org/41951
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Michael Stahl <mstahl@redhat.com>
    (cherry picked from commit 2b329599183107735e1a7b6aad516452eb6fa6a6)

--- external/libxml2/UnpackedTarball_xml2.mk
+++ external/libxml2/UnpackedTarball_xml2.mk
@@ -17,12 +17,6 @@ $(eval $(call gb_UnpackedTarball_add_patches,xml2,\
 	external/libxml2/libxml2-vc10.patch \
 	$(if $(filter ANDROID,$(OS)),external/libxml2/libxml2-android.patch) \
 	external/libxml2/libxml2-icu.patch.0 \
-	external/libxml2/ubsan.patch.0 \
-	external/libxml2/0001-Fix-type-confusion-in-xmlValidateOneNamespace.patch.1 \
-	external/libxml2/0001-Prevent-unwanted-external-entity-reference.patch.1 \
-	external/libxml2/0001-Increase-buffer-space-for-port-in-HTTP-redirect-supp.patch.1 \
-	external/libxml2/0001-Fix-buffer-size-checks-in-xmlSnprintfElementContent.patch.1 \
-	external/libxml2/0001-Fix-handling-of-parameter-entity-references.patch.1 \
 ))
 
 # vim: set noet sw=4 ts=4:
