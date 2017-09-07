4d13f63dd7fed3437b9b493eb6cf2ef429d84aa7
Szymon Kłos <szymon.klos@collabora.com>
2017-09-07 13:48:01 +0200

    tdf#112247 correct the size only for Watermark
    Change-Id: I770cd36d5f8bca0f2fa97b7ad4afaaf539bf5f1f
    Reviewed-on: https://gerrit.libreoffice.org/42052
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>
    (cherry picked from commit 91d909ac8ec53ea135401babe8227f9c41b779b8)
    Reviewed-on: https://gerrit.libreoffice.org/42058
    Reviewed-by: Andras Timar <andras.timar@collabora.com>
    Tested-by: Andras Timar <andras.timar@collabora.com>
    (cherry picked from commit 60ba6063a2a9063ba1bd39ceb322d613d2129628)

--- sw/source/filter/ww8/wrtw8esh.cxx
+++ sw/source/filter/ww8/wrtw8esh.cxx
@@ -761,12 +761,15 @@ void PlcDrawObj::WritePlc( WW8Export& rWrt ) const
                 {
                     aRect = pObj->GetLogicRect();
 
-                    // We have to export original size with padding
-                    const SfxItemSet& rSet = pObj->GetMergedItemSet();
-                    long nHeight = aRect.GetHeight();
-                    if (const SdrMetricItem* pItem = static_cast<const SdrMetricItem*>(rSet.GetItem(SDRATTR_TEXT_UPPERDIST)))
-                        nHeight += pItem->GetValue();
-                    aRect.SetSize(Size(aRect.GetWidth(), nHeight));
+                    // We have to export Watermark original size with padding
+                    if (pObj->GetName().match("PowerPlusWaterMarkObject"))
+                    {
+                        const SfxItemSet& rSet = pObj->GetMergedItemSet();
+                        long nHeight = aRect.GetHeight();
+                        if (const SdrMetricItem* pItem = static_cast<const SdrMetricItem*>(rSet.GetItem(SDRATTR_TEXT_UPPERDIST)))
+                            nHeight += pItem->GetValue();
+                        aRect.SetSize(Size(aRect.GetWidth(), nHeight));
+                    }
                 }
             }
 
