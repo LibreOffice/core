b301dd1092284a60a119bc3ecbd0e21370a25269
Jens Carl <j.carl43@gmx.de>
2017-09-08 08:48:08 +0000

    tdf#45904: Move _XSubTotalDescriptor Java tests to C++
    Change-Id: Ie1ec8879972f77c5278992aadf9324f755859d96

--- sc/Module_sc.mk
+++ sc/Module_sc.mk
@@ -128,6 +128,7 @@ $(eval $(call gb_Module_add_subsequentcheck_targets,sc,\
 	CppunitTest_sc_cellcursorobj \
 	CppunitTest_sc_tabviewobj \
 	CppunitTest_sc_arealinkobj \
+	CppunitTest_sc_subtotaldescriptorbaseobj \
 	CppunitTest_sc_datapilotitemobj \
 	CppunitTest_sc_subtotalfieldobj \
 	CppunitTest_sc_arealinksobj \
---
+++ test/source/sheet/xsubtotaldescriptor.cxx
@@ -0,0 +1,47 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <test/sheet/xsubtotaldescriptor.hxx>
+
+#include <com/sun/star/sheet/GeneralFunction.hpp>
+#include <com/sun/star/sheet/SubTotalColumn.hpp>
+#include <com/sun/star/sheet/XSubTotalDescriptor.hpp>
+
+#include <com/sun/star/uno/Reference.hxx>
+#include <com/sun/star/uno/Sequence.hxx>
+
+#include <cppunit/extensions/HelperMacros.h>
+
+using namespace com::sun::star;
+using namespace com::sun::star::uno;
+
+namespace apitest {
+
+void XSubTotalDescriptor::testAddNew()
+{
+    uno::Reference< sheet::XSubTotalDescriptor > xSTD(init(), uno::UNO_QUERY_THROW);
+
+    uno::Sequence< sheet::SubTotalColumn > xCols;
+    xCols.realloc(1);
+    xCols[0].Column = 5;
+    xCols[0].Function = sheet::GeneralFunction_SUM;
+
+    CPPUNIT_ASSERT_NO_THROW_MESSAGE("Unable to add column", xSTD->addNew(xCols, 1));
+}
+
+void XSubTotalDescriptor::testClear()
+{
+    uno::Reference< sheet::XSubTotalDescriptor > xSTD(init(), UNO_QUERY_THROW);
+
+    CPPUNIT_ASSERT_NO_THROW_MESSAGE("Unable to clear XSubTotalDescriptor", xSTD->clear());
+}
+
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
