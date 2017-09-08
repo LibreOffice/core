46ebc2cfe7cc9ebdc1fe705cb14dac0b5cc3f3e7
Jens Carl <j.carl43@gmx.de>
2017-09-08 08:48:08 +0000

    tdf#45904: Move _XSubTotalDescriptor Java tests to C++
    Change-Id: Ie1ec8879972f77c5278992aadf9324f755859d96

---
+++ include/test/sheet/xsubtotaldescriptor.hxx
@@ -0,0 +1,34 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef INCLUDED_TEST_SHEET_XSUBTOTALDESCRIPTOR_HXX
+#define INCLUDED_TEST_SHEET_XSUBTOTALDESCRIPTOR_HXX
+
+#include <com/sun/star/uno/Reference.hxx>
+#include <com/sun/star/uno/XInterface.hpp>
+#include <test/testdllapi.hxx>
+
+namespace apitest {
+
+class OOO_DLLPUBLIC_TEST XSubTotalDescriptor
+{
+public:
+    virtual css::uno::Reference< css::uno::XInterface > init() = 0;
+
+    void testAddNew();
+    void testClear();
+
+protected:
+    ~XSubTotalDescriptor() {}
+};
+
+}
+#endif // INCLUDED_TEST_SHEET_XSUBTOTALDESCRIPTOR_HXX
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
