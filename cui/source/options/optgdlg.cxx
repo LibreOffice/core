044fd7f8146b6ce4325cad6ff8cbe898fc9db9e6
Andreas Brandner <Andreas.Brandner@cib.de>
2017-09-08 18:51:21 +0200

    tdf#108406 offline help-images helpimg are no longer selectable as iconset
    Change-Id: I6733ed2a04c01c288de2cd6af938a6c6ff1b4481

--- cui/source/options/optgdlg.cxx
+++ cui/source/options/optgdlg.cxx
@@ -660,6 +660,9 @@ OfaViewTabPage::OfaViewTabPage(vcl::Window* pParent, const SfxItemSet& rSet)
     StyleSettings aStyleSettings = Application::GetSettings().GetStyleSettings();
     mInstalledIconThemes = aStyleSettings.GetInstalledIconThemes();
 
+    mInstalledIconThemes.erase(std::remove_if(mInstalledIconThemes.begin(), mInstalledIconThemes.end(),
+                                [](vcl::IconThemeInfo theme) { return theme.GetDisplayName() == "Helpimg";}), mInstalledIconThemes.end());
+
     // Start with the automatically chosen icon theme
     OUString autoThemeId = aStyleSettings.GetAutomaticallyChosenIconTheme();
     const vcl::IconThemeInfo& autoIconTheme = vcl::IconThemeInfo::FindIconThemeById(mInstalledIconThemes, autoThemeId);
