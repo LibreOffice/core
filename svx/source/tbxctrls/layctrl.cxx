0a0656354c2f747afe28e856579a7b1984de7764
Caolán McNamara <caolanm@redhat.com>
2017-09-12 12:12:19 +0100

    Resolves: tdf#106163 safe fix for wrong final row/col selection in rtl
    Change-Id: I773de03768b0a5b28e6b4f63bd65dd270dc975cd

--- svx/source/tbxctrls/layctrl.cxx
+++ svx/source/tbxctrls/layctrl.cxx
@@ -161,6 +161,8 @@ void TableWindow::dispose()
 void TableWindow::MouseMove( const MouseEvent& rMEvt )
 {
     SfxPopupWindow::MouseMove( rMEvt );
+    if (IsInCleanUp())
+        return;
     Point aPos = rMEvt.GetPosPixel();
     Point aMousePos( aPos );
 
