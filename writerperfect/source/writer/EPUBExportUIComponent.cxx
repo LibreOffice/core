e6746a005419235d20074d696a1f8fadbda0a003
Miklos Vajna <vmiklos@collabora.co.uk>
2017-09-12 14:07:07 +0200

    EPUB export: initial UI component
    EPUBExportUIComponent::execute() still needs to launch an actual dialog,
    but otherwise it has enough functionality that only tweaking
    maFilterData is necessary in the UNO component to pass custom filter
    options to the exporter.
    
    Change-Id: I95af024f5babd66a5aa0b446550f4f0fec45ef43
    Reviewed-on: https://gerrit.libreoffice.org/42204
    Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
    Tested-by: Jenkins <ci@libreoffice.org>

---
+++ writerperfect/source/writer/EPUBExportUIComponent.cxx
@@ -0,0 +1,83 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "EPUBExportUIComponent.hxx"
+
+#include <com/sun/star/ui/dialogs/ExecutableDialogResults.hpp>
+
+#include <comphelper/sequence.hxx>
+#include <cppuhelper/supportsservice.hxx>
+
+using namespace com::sun::star;
+
+namespace writerperfect
+{
+
+EPUBExportUIComponent::EPUBExportUIComponent(const uno::Reference<uno::XComponentContext> &/*xContext*/)
+{
+}
+
+uno::Sequence<beans::PropertyValue> EPUBExportUIComponent::getPropertyValues()
+{
+    maMediaDescriptor["FilterData"] <<= maFilterData.getAsConstPropertyValueList();
+    return maMediaDescriptor.getAsConstPropertyValueList();
+}
+
+void EPUBExportUIComponent::setPropertyValues(const uno::Sequence<beans::PropertyValue> &rProperties)
+{
+    maMediaDescriptor.clear();
+    maMediaDescriptor << rProperties;
+    auto it = maMediaDescriptor.find("FilterData");
+    if (it != maMediaDescriptor.end())
+    {
+        uno::Sequence<beans::PropertyValue> aFilterData;
+        if (it->second >>= aFilterData)
+        {
+            maFilterData.clear();
+            maFilterData << aFilterData;
+        }
+    }
+}
+
+OUString EPUBExportUIComponent::getImplementationName()
+{
+    return OUString("com.sun.star.comp.Writer.EPUBExportUIComponent");
+}
+
+sal_Bool EPUBExportUIComponent::supportsService(const OUString &rServiceName)
+{
+    return cppu::supportsService(this, rServiceName);
+}
+
+uno::Sequence<OUString> EPUBExportUIComponent::getSupportedServiceNames()
+{
+    uno::Sequence<OUString> aRet =
+    {
+        OUString("com.sun.star.ui.dialogs.FilterOptionsDialog")
+    };
+    return aRet;
+}
+
+void EPUBExportUIComponent::setTitle(const OUString &/*rTitle*/)
+{
+}
+
+sal_Int16 EPUBExportUIComponent::execute()
+{
+    return ui::dialogs::ExecutableDialogResults::OK;
+}
+
+extern "C" SAL_DLLPUBLIC_EXPORT uno::XInterface *SAL_CALL com_sun_star_comp_Writer_EPUBExportUIComponent_get_implementation(uno::XComponentContext *pCtx, uno::Sequence<uno::Any> const &)
+{
+    return cppu::acquire(new EPUBExportUIComponent(pCtx));
+}
+
+} // namespace writerperfect
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
