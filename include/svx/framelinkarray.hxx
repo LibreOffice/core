0a59a23dec5071c333e5fda635d90abd729f8726
Armin Le Grand <Armin.Le.Grand@cib.de>
2017-09-13 10:29:29 +0200

    borderline: Added merge BorderLinePrimitive2D
    Added BorderLinePrimitive2D merges for Writer and fixes.

--- include/svx/framelinkarray.hxx
+++ include/svx/framelinkarray.hxx
@@ -327,6 +327,11 @@ private:
     ArrayImplPtr        mxImpl;
 };
 
+// helper to merge B2DPrimitive(s) in rSource and add to rTarget
+void SVX_DLLPUBLIC HelperMergeInB2DPrimitiveArray(
+    const drawinglayer::primitive2d::Primitive2DContainer& rSource,
+    drawinglayer::primitive2d::Primitive2DContainer& rTarget);
+
 }
 }
 
