52eb9c7a3fa9e88e175904410d77fb01c0bd74ae
Tor Lillqvist <tml@collabora.com>
2017-09-14 21:11:27 +0300

    Handle also the case where a range extends an existing one with overlap
    Check for a new range being joined Extending an existing one in any of
    four directions (up, right, down, left), within a tab (sheet), that
    is. And add unit test for this.
    
    Reviewed-on: https://gerrit.libreoffice.org/42304
    Reviewed-by: Eike Rathke <erack@redhat.com>
    Tested-by: Eike Rathke <erack@redhat.com>
    (cherry picked from commit 596efaad51735a130e7b7bd27dbc34dc07f32f68)
    
    Change-Id: I4bd0525c2837f8b4b9d5a8967e0d5d661c6a5e2f

--- sc/qa/unit/rangelst_test.cxx
+++ sc/qa/unit/rangelst_test.cxx
@@ -44,6 +44,8 @@ public:
     void testDeleteArea_0Ranges();
     void testJoin_Case1();
     void testJoin_Case2();
+    void testJoin_Case3();
+    void testJoin_Case4();
     void testGetIntersectedRange();
 
     void testUpdateReference_DeleteRow();
@@ -72,6 +74,8 @@ public:
     CPPUNIT_TEST(testDeleteArea_0Ranges);
     CPPUNIT_TEST(testJoin_Case1);
     CPPUNIT_TEST(testJoin_Case2);
+    CPPUNIT_TEST(testJoin_Case3);
+    CPPUNIT_TEST(testJoin_Case4);
     CPPUNIT_TEST(testUpdateReference_DeleteRow);
     CPPUNIT_TEST(testUpdateReference_DeleteLastRow);
     CPPUNIT_TEST(testUpdateReference_DeleteCol);
@@ -422,6 +426,59 @@ void Test::testJoin_Case2()
 
     CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(1), aList.size());
     CPPUNIT_ASSERT_EQUAL(ScRange(1,1,0,9,3,0), *aList[0]);
+}
+
+void Test::testJoin_Case3()
+{
+    ScRangeList aList;
+    aList.Join(ScRange(1,1,0,6,6,0));
+    aList.Join(ScRange(3,3,0,4,4,0));
+
+    // The second one should have been swallowed by the first one
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(1), aList.size());
+    CPPUNIT_ASSERT_EQUAL(ScRange(1,1,0,6,6,0), *aList[0]);
+
+    // Add a disjoint one
+    aList.Join(ScRange(8,8,0,9,9,0));
+
+    // Should be two ones now
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(2), aList.size());
+    // The first one should still be as is
+    CPPUNIT_ASSERT_EQUAL(ScRange(1,1,0,6,6,0), *aList[0]);
+    // Ditto for the second one
+    CPPUNIT_ASSERT_EQUAL(ScRange(8,8,0,9,9,0), *aList[1]);
+}
+
+void Test::testJoin_Case4()
+{
+    ScRangeList aList;
+    aList.Join(ScRange(1,1,0,2,6,0));
+    // Join a range that overlaps it and extends it vertically
+    aList.Join(ScRange(1,4,0,2,8,0));
+
+    // The one range in the list should have been extended
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(1), aList.size());
+    CPPUNIT_ASSERT_EQUAL(ScRange(1,1,0,2,8,0), *aList[0]);
+
+    // Join a range that overlaps it and extends it horizontally
+    aList.Join(ScRange(2,1,0,4,8,0));
+
+    // Again, should have just been extended
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(1), aList.size());
+    CPPUNIT_ASSERT_EQUAL(ScRange(1,1,0,4,8,0), *aList[0]);
+
+    // And then the same but on top / to the left of existing range
+    ScRangeList aList2;
+    aList2.Join(ScRange(4,4,0,8,8,0));
+    aList2.Join(ScRange(4,1,0,8,6,0));
+
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(1), aList2.size());
+    CPPUNIT_ASSERT_EQUAL(ScRange(4,1,0,8,8,0), *aList2[0]);
+
+    aList2.Join(ScRange(1,1,0,6,8,0));
+
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(1), aList2.size());
+    CPPUNIT_ASSERT_EQUAL(ScRange(1,1,0,8,8,0), *aList2[0]);
 }
 
 void Test::testUpdateReference_DeleteRow()
