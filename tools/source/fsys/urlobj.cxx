LibreOffice/core

commit 43a6b59539ad573436f43303e9fbe17c12dc9c84

simplify some OUString compareTo calls
to either startsWith or == or !=

Change-Id: Ie4b4662f5b8e4532cbc1ab36910389e0b3d41ef0
Reviewed-on: https://gerrit.libreoffice.org/39750
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

tools/source/fsys/urlobj.cxx
@@ -3694,7 +3694,7 @@ bool INetURLObject::operator ==(INetURLObject const & rObject) const
                 default:
                     return false;
             }
-            return aPath1.compareTo(aPath2, nLength) == 0;
+            return aPath2.startsWith(aPath1);
         }
 
         default:

comphelper/source/misc/backupfilehelper.cxx
@@ -452,14 +452,14 @@ namespace
 
         bool isSameExtension(const ExtensionInfoEntry& rComp) const
         {
-            return (maRepository == rComp.maRepository && 0 == maName.compareTo(rComp.maName));
+            return (maRepository == rComp.maRepository && maName == rComp.maName);
         }
 
         bool operator<(const ExtensionInfoEntry& rComp) const
         {
             if (maRepository == rComp.maRepository)
             {
-                if (0 == maName.compareTo(rComp.maName))
+                if (maName == rComp.maName)
                 {
                     return mbEnabled < rComp.mbEnabled;
                 }

comphelper/source/misc/storagehelper.cxx
@@ -446,16 +446,13 @@ bool OStorageHelper::PathHasSegment( const OUString& aPath, const OUString& aSeg
 
     if ( !aSegment.isEmpty() && nPathLen >= nSegLen )
     {
-        OUString aEndSegment = "/"
-                             + aSegment;
-
-        OUString aInternalSegment = aEndSegment
-                                  + "/";
+        OUString aEndSegment = "/" + aSegment;
+        OUString aInternalSegment = aEndSegment + "/";
 
         if ( aPath.indexOf( aInternalSegment ) >= 0 )
             bResult = true;
 
-        if ( !bResult && !aPath.compareTo( aSegment, nSegLen ) )
+        if ( !bResult && aPath.startsWith( aSegment ) )
         {
             if ( nPathLen == nSegLen || aPath[nSegLen] == '/' )
                 bResult = true;

connectivity/source/drivers/firebird/DatabaseMetaData.cxx
@@ -1513,9 +1513,9 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getImportedKeys(
 
         // deferrability is currently not supported, but may be supported in the future.
         /*
-        aCurrentRow[14] = (xRow->getString(5).compareTo(OUString("NO") == 0 ?
+        aCurrentRow[14] = (xRow->getString(5) == "NO" ?
                           new ORowSetValueDecorator(Deferrability::NONE)
-                        : (xRow->getString(6).compareTo(OUString("NO") == 0 ?
+                        : (xRow->getString(6) == "NO" ?
                             new ORowSetValueDecorator(Deferrability::INITIALLY_IMMEDIATE)
                           : new ORowSetValueDecorator(Deferrability::INITIALLY_DEFERRED));
         */

cui/source/dialogs/screenshotannotationdlg.cxx
@@ -364,7 +364,7 @@ IMPL_LINK_NOARG(ScreenshotAnnotationDlg_Impl, saveButtonHandler, Button*, void)
                 INetURLObject aConfirmedURL(aConfirmedName);
                 OUString aCurrentExtension(aConfirmedURL.getExtension());
 
-                if (!aCurrentExtension.isEmpty() && 0 != aCurrentExtension.compareTo("png"))
+                if (!aCurrentExtension.isEmpty() && aCurrentExtension != "png")
                 {
                     aConfirmedURL.removeExtension();
                     aCurrentExtension.clear();

filter/source/svg/svgwriter.cxx
@@ -1146,7 +1146,7 @@ bool SVGTextWriter::nextTextPortion()
                                 sFieldName = pNames->copy( sServicePrefix.getLength() );
                                 break;
                             }
-                            else if( 0 == pNames->compareTo( sPresentationServicePrefix, sPresentationServicePrefix.getLength() ) )
+                            else if( pNames->startsWith( sPresentationServicePrefix ) )
                             {
                                 // TextField found => postfix is field type!
                                 sFieldName = pNames->copy( sPresentationServicePrefix.getLength() );

framework/source/uielement/notebookbarmenucontroller.cxx
@@ -272,11 +272,11 @@ void SAL_CALL NotebookbarMenuController::itemActivated( const css::awt::MenuEven
         return;
 
     // Entries are enabled only when Notebookbar mode is active
-    bool bActive = ( comphelper::getString( aModesNode.getNodeValue( "Active" ) ).compareTo("Notebookbar") == 0 );
+    bool bActive = comphelper::getString( aModesNode.getNodeValue( "Active" ) ) == "Notebookbar";
 
     for ( int i = 0; i < m_xPopupMenu->getItemCount(); ++i )
     {
-        m_xPopupMenu->checkItem( i+1, ( aActive.compareTo( m_xPopupMenu->getCommand( i+1 ) ) == 0 ) );
+        m_xPopupMenu->checkItem( i+1, aActive == m_xPopupMenu->getCommand( i+1 ) );
         m_xPopupMenu->enableItem( i+1, bActive );
     }
 }

framework/source/uielement/toolbarmodemenucontroller.cxx
@@ -283,7 +283,7 @@ void SAL_CALL ToolbarModeMenuController::itemActivated( const css::awt::MenuEven
     OUString aMode = comphelper::getString( aModesNode.getNodeValue( "Active" ) );
 
     for ( int i = 0; i < m_xPopupMenu->getItemCount(); ++i )
-        m_xPopupMenu->checkItem( i+1, (aMode.compareTo( m_xPopupMenu->getCommand( i+1 ) ) == 0) );
+        m_xPopupMenu->checkItem( i+1, aMode == m_xPopupMenu->getCommand( i+1 ) );
 }
 
 // XPopupMenuController

commit b12e9ec1c9796fe62cb744ac82008146323348ea

Fix INetURLObject::operator==() when comparing paths
Regression from

    commit 43a6b59539ad573436f43303e9fbe17c12dc9c84
    Date:   Mon Jul 10 12:42:24 2017 +0200

        simplify some OUString compareTo calls

        to either startsWith or == or !=

aPath2.startsWith(aPath1) is not the same as
aPath1.compareTo(aPath2, nLength) == 0
if aPath1 is longer than aPath2 ...

The consequence was a broken Macro Security Trusted Location that never
matched.

Change-Id: I6b7adab42982c020f3677bf24f18edd14210ffdf

tools/source/fsys/urlobj.cxx
@@ -3694,7 +3694,7 @@ bool INetURLObject::operator ==(INetURLObject const & rObject) const
                 default:
                     return false;
             }
-            return aPath2.startsWith(aPath1);
+            return aPath1.compareTo(aPath2, nLength) == 0;
         }
 
         default: