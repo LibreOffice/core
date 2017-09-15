LibreOffice/core

commit 2e0a25ce2b87d3a4bbf944025fc3720933fb391d

loplugin:constantparam
Change-Id: Ic87f0cc863490858b7cc7e74a7f90ec31992dd53
Reviewed-on: https://gerrit.libreoffice.org/42089
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/source/edit/texteng.cxx
@@ -2440,24 +2440,13 @@ bool TextEngine::Read( SvStream& rInput, const TextSelection* pSel )
     return rInput.GetError() == ERRCODE_NONE;
 }
 
-bool TextEngine::Write( SvStream& rOutput, const TextSelection* pSel, bool bHTML )
+bool TextEngine::Write( SvStream& rOutput )
 {
     TextSelection aSel;
-    if ( pSel )
-        aSel = *pSel;
-    else
-    {
-        const sal_uInt32 nParas = static_cast<sal_uInt32>(mpDoc->GetNodes().size());
-        TextNode* pNode = mpDoc->GetNodes()[ nParas - 1 ];
-        aSel.GetStart() = TextPaM( 0, 0 );
-        aSel.GetEnd() = TextPaM( nParas-1, pNode->GetText().getLength() );
-    }
-
-    if ( bHTML )
-    {
-        rOutput.WriteLine( "<HTML>" );
-        rOutput.WriteLine( "<BODY>" );
-    }
+    const sal_uInt32 nParas = static_cast<sal_uInt32>(mpDoc->GetNodes().size());
+    TextNode* pSelNode = mpDoc->GetNodes()[ nParas - 1 ];
+    aSel.GetStart() = TextPaM( 0, 0 );
+    aSel.GetEnd() = TextPaM( nParas-1, pSelNode->GetText().getLength() );
 
     for ( sal_uInt32 nPara = aSel.GetStart().GetPara(); nPara <= aSel.GetEnd().GetPara(); ++nPara  )
     {
@@ -2469,35 +2458,22 @@ bool TextEngine::Write( SvStream& rOutput, const TextSelection* pSel, bool bHTML
             ? aSel.GetEnd().GetIndex() : pNode->GetText().getLength();
 
         OUStringBuffer aText;
-        if ( !bHTML )
+        aText = "<P STYLE=\"margin-bottom: 0cm\">";
+
+        if ( nStartPos == nEndPos )
         {
-            aText = pNode->GetText().copy( nStartPos, nEndPos-nStartPos );
+            // Empty lines will be removed by Writer
+            aText.append( "<BR>" );
         }
         else
         {
-            aText = "<P STYLE=\"margin-bottom: 0cm\">";
-
-            if ( nStartPos == nEndPos )
-            {
-                // Empty lines will be removed by Writer
-                aText.append( "<BR>" );
-            }
-            else
-            {
-                // Text before Attribute
-                aText.append( pNode->GetText().copy( nStartPos, nEndPos-nStartPos ) );
-            }
-
-            aText.append( "</P>" );
+            // Text before Attribute
+            aText.append( pNode->GetText().copy( nStartPos, nEndPos-nStartPos ) );
         }
+
+        aText.append( "</P>" );
         rOutput.WriteLine(OUStringToOString(aText.makeStringAndClear(),
             rOutput.GetStreamCharSet()));
-    }
-
-    if ( bHTML )
-    {
-        rOutput.WriteLine( "</BODY>" );
-        rOutput.WriteLine( "</HTML>" );
     }
 
     return rOutput.GetError() == ERRCODE_NONE;

compilerplugins/clang/constantparam.constructors.results
@@ -52,15 +52,15 @@ connectivity/source/inc/calc/CTable.hxx:60
     ""
 connectivity/source/inc/dbase/DTable.hxx:149
     void connectivity::dbase::ODbaseTable::ODbaseTable(class connectivity::sdbcx::OCollection *,class connectivity::dbase::ODbaseConnection *,const class rtl::OUString &,const class rtl::OUString &,const class rtl::OUString &,const class rtl::OUString &,const class rtl::OUString &)
+    const class rtl::OUString & Description
+    ""
+connectivity/source/inc/dbase/DTable.hxx:149
+    void connectivity::dbase::ODbaseTable::ODbaseTable(class connectivity::sdbcx::OCollection *,class connectivity::dbase::ODbaseConnection *,const class rtl::OUString &,const class rtl::OUString &,const class rtl::OUString &,const class rtl::OUString &,const class rtl::OUString &)
     const class rtl::OUString & SchemaName
     ""
 connectivity/source/inc/dbase/DTable.hxx:149
     void connectivity::dbase::ODbaseTable::ODbaseTable(class connectivity::sdbcx::OCollection *,class connectivity::dbase::ODbaseConnection *,const class rtl::OUString &,const class rtl::OUString &,const class rtl::OUString &,const class rtl::OUString &,const class rtl::OUString &)
     const class rtl::OUString & CatalogName
-    ""
-connectivity/source/inc/dbase/DTable.hxx:149
-    void connectivity::dbase::ODbaseTable::ODbaseTable(class connectivity::sdbcx::OCollection *,class connectivity::dbase::ODbaseConnection *,const class rtl::OUString &,const class rtl::OUString &,const class rtl::OUString &,const class rtl::OUString &,const class rtl::OUString &)
-    const class rtl::OUString & Description
     ""
 connectivity/source/inc/flat/ETable.hxx:80
     void connectivity::flat::OFlatTable::OFlatTable(class connectivity::sdbcx::OCollection *,class connectivity::flat::OFlatConnection *,const class rtl::OUString &,const class rtl::OUString &,const class rtl::OUString &,const class rtl::OUString &,const class rtl::OUString &)
@@ -81,18 +81,6 @@ connectivity/source/inc/hsqldb/HView.hxx:40
 connectivity/source/inc/OColumn.hxx:72
     void connectivity::OColumn::OColumn(const class rtl::OUString &,const class rtl::OUString &,int,int,int,int,int)
     const class rtl::OUString & _aTableName
-    ""
-connectivity/source/inc/writer/WTable.hxx:79
-    void connectivity::writer::OWriterTable::OWriterTable(class connectivity::sdbcx::OCollection *,class connectivity::writer::OWriterConnection *,const class rtl::OUString &,const class rtl::OUString &,const class rtl::OUString &,const class rtl::OUString &,const class rtl::OUString &)
-    const class rtl::OUString & SchemaName
-    ""
-connectivity/source/inc/writer/WTable.hxx:79
-    void connectivity::writer::OWriterTable::OWriterTable(class connectivity::sdbcx::OCollection *,class connectivity::writer::OWriterConnection *,const class rtl::OUString &,const class rtl::OUString &,const class rtl::OUString &,const class rtl::OUString &,const class rtl::OUString &)
-    const class rtl::OUString & CatalogName
-    ""
-connectivity/source/inc/writer/WTable.hxx:79
-    void connectivity::writer::OWriterTable::OWriterTable(class connectivity::sdbcx::OCollection *,class connectivity::writer::OWriterConnection *,const class rtl::OUString &,const class rtl::OUString &,const class rtl::OUString &,const class rtl::OUString &,const class rtl::OUString &)
-    const class rtl::OUString & Description
     ""
 dbaccess/source/core/api/RowSetRow.hxx:41
     void dbaccess::ORowSetOldRowHelper::ORowSetOldRowHelper(const class rtl::Reference<class connectivity::ORowVector<class connectivity::ORowSetValue> > &)
@@ -250,27 +238,11 @@ include/editeng/brushitem.hxx:60
     void SvxBrushItem::SvxBrushItem(class SvStream &,unsigned short,unsigned short)
     unsigned short nWhich
     Which()
-include/editeng/bulletitem.hxx:64
-    void SvxBulletItem::SvxBulletItem(class SvStream &,unsigned short)
-    unsigned short nWhich
-    Which()
 include/editeng/colritem.hxx:42
     void SvxColorItem::SvxColorItem(class SvStream &,const unsigned short)
     const unsigned short nId
     Which()
-include/editeng/colritem.hxx:81
-    void SvxBackgroundColorItem::SvxBackgroundColorItem(class SvStream &,const unsigned short)
-    const unsigned short nId
-    Which()
-include/editeng/flditem.hxx:72
-    void SvxFieldItem::SvxFieldItem(class SvxFieldData *,const unsigned short)
-    const unsigned short nId
-    Which()
-include/editeng/rsiditem.hxx:23
-    void SvxRsidItem::SvxRsidItem(class SvStream &,unsigned short)
-    unsigned short nId
-    Which()
-include/editeng/scripttypeitem.hxx:53
+include/editeng/scripttypeitem.hxx:37
     const class SfxPoolItem * SvxScriptSetItem::GetItemOfScript(unsigned short,const class SfxItemSet &,enum SvtScriptType)
     unsigned short nSlotId
     Which()
@@ -346,10 +318,6 @@ include/sfx2/dinfdlg.hxx:90
     void SfxDocumentInfoItem::SfxDocumentInfoItem(const class rtl::OUString &,const class com::sun::star::uno::Reference<class com::sun::star::document::XDocumentProperties> &,const class com::sun::star::uno::Sequence<struct com::sun::star::document::CmisProperty> &,_Bool,_Bool)
     _Bool bUseThumbnailSave
     IsUseThumbnailSave()
-include/sfx2/mailmodelapi.hxx:89
-    enum SfxMailModel::SendMailResult SfxMailModel::AttachDocument(const class rtl::OUString &,const class com::sun::star::uno::Reference<class com::sun::star::uno::XInterface> &,const class rtl::OUString &)
-    const class rtl::OUString & sDocumentType
-    ""
 include/sfx2/objsh.hxx:630
     void SfxObjectShell::DoDraw(class OutputDevice *,const class Point &,const class Size &,const class JobSetup &,unsigned short)
     const class JobSetup & rSetup
@@ -362,9 +330,25 @@ include/svl/aeitem.hxx:54
     void SfxAllEnumItem::SfxAllEnumItem(unsigned short,class SvStream &)
     unsigned short nWhich
     Which()
+include/svl/eitem.hxx:103
+    void SfxBoolItem::SfxBoolItem(unsigned short,class SvStream &)
+    unsigned short nWhich
+    Which()
 include/svl/int64item.hxx:22
     void SfxInt64Item::SfxInt64Item(unsigned short,class SvStream &)
     unsigned short nWhich
+    Which()
+include/svl/intitem.hxx:52
+    void SfxInt16Item::SfxInt16Item(unsigned short,class SvStream &)
+    unsigned short nWhich
+    Which()
+include/svl/intitem.hxx:119
+    void SfxInt32Item::SfxInt32Item(unsigned short,class SvStream &)
+    unsigned short which
+    Which()
+include/svl/intitem.hxx:139
+    void SfxUInt32Item::SfxUInt32Item(unsigned short,class SvStream &)
+    unsigned short which
     Which()
 include/svl/lckbitem.hxx:33
     void SfxLockBytesItem::SfxLockBytesItem(unsigned short,class SvStream &)
@@ -378,8 +362,8 @@ include/svl/slstitm.hxx:41
     void SfxStringListItem::SfxStringListItem(unsigned short,class SvStream &)
     unsigned short nWhich
     Which()
-include/svl/szitem.hxx:34
-    void SfxSizeItem::SfxSizeItem(unsigned short,const class Size &)
+include/svl/stritem.hxx:37
+    void SfxStringItem::SfxStringItem(unsigned short,class SvStream &)
     unsigned short nWhich
     Which()
 include/svl/visitem.hxx:39
@@ -394,22 +378,6 @@ include/svtools/ehdl.hxx:37
     void SfxErrorContext::SfxErrorContext(unsigned short,class vcl::Window *,const struct std::pair<const char *, class ErrCode> *,const class std::locale &)
     const class std::locale & rResLocaleP
     SvtResLocale()
-include/svtools/imapobj.hxx:73
-    void IMapObject::AppendCERNURL(class rtl::OStringBuffer &,const class rtl::OUString &) const
-    const class rtl::OUString & rBaseURL
-    ""
-include/svtools/imapobj.hxx:75
-    void IMapObject::AppendNCSAURL(class rtl::OStringBuffer &,const class rtl::OUString &) const
-    const class rtl::OUString & rBaseURL
-    ""
-include/svtools/imapobj.hxx:93
-    void IMapObject::Write(class SvStream &,const class rtl::OUString &) const
-    const class rtl::OUString & rBaseURL
-    ""
-include/svtools/imapobj.hxx:94
-    void IMapObject::Read(class SvStream &,const class rtl::OUString &)
-    const class rtl::OUString & rBaseURL
-    ""
 include/svtools/transfer.hxx:313
     class com::sun::star::uno::Any TransferableDataHelper::GetAny(enum SotClipboardFormatId,const class rtl::OUString &) const
     const class rtl::OUString & rDestDoc
@@ -418,58 +386,26 @@ include/svtools/transfer.hxx:351
     class com::sun::star::uno::Sequence<signed char> TransferableDataHelper::GetSequence(enum SotClipboardFormatId,const class rtl::OUString &)
     const class rtl::OUString & rDestDoc
     ""
-include/svtools/treelistbox.hxx:762
+include/svtools/treelistbox.hxx:752
     void SvTreeListBox::ShowFocusRect(const class SvTreeListEntry *)
     const class SvTreeListEntry * pEntry
     FirstSelected()
-include/svx/charmap.hxx:135
-    void SvxShowCharSet::DrawChars_Impl(class OutputDevice &,int,int)
-    int n2
-    LastInView()
-include/svx/charmap.hxx:135
+include/svx/charmap.hxx:151
     void SvxShowCharSet::DrawChars_Impl(class OutputDevice &,int,int)
     int n1
     FirstInView()
-include/svx/chrtitem.hxx:87
-    void SvxChartRegressItem::SvxChartRegressItem(class SvStream &,unsigned short)
-    unsigned short nId
-    Which()
-include/svx/chrtitem.hxx:101
-    void SvxChartTextOrderItem::SvxChartTextOrderItem(class SvStream &,unsigned short)
-    unsigned short nId
-    Which()
-include/svx/chrtitem.hxx:118
-    void SvxChartKindErrorItem::SvxChartKindErrorItem(class SvStream &,unsigned short)
-    unsigned short nId
-    Which()
-include/svx/chrtitem.hxx:134
-    void SvxChartIndicateItem::SvxChartIndicateItem(class SvStream &,unsigned short)
-    unsigned short nId
-    Which()
-include/svx/sdangitm.hxx:31
-    void SdrAngleItem::SdrAngleItem(unsigned short,class SvStream &)
-    unsigned short nId
-    Which()
-include/svx/sdmetitm.hxx:33
-    void SdrMetricItem::SdrMetricItem(unsigned short,class SvStream &)
-    unsigned short nId
-    Which()
+include/svx/charmap.hxx:151
+    void SvxShowCharSet::DrawChars_Impl(class OutputDevice &,int,int)
+    int n2
+    LastInView()
 include/svx/sdr/animation/scheduler.hxx:54
     _Bool sdr::animation::CompareEvent::operator()(class sdr::animation::Event *const &,class sdr::animation::Event *const &) const
     class sdr::animation::Event *const & lhs
     Compare()
-include/svx/sdynitm.hxx:33
-    void SdrYesNoItem::SdrYesNoItem(unsigned short,class SvStream &)
-    unsigned short nId
-    Which()
 include/svx/svdoole2.hxx:163
     void SdrOle2Obj::SetGraphicToObj(const class Graphic &,const class rtl::OUString &)
     const class rtl::OUString & aMediaType
     ""
-include/svx/sxsiitm.hxx:27
-    void SdrScaleItem::SdrScaleItem(unsigned short,class SvStream &)
-    unsigned short nId
-    Which()
 include/test/primitive2dxmldump.hxx:39
     struct _xmlDoc * Primitive2dXmlDump::dumpAndParse(const class drawinglayer::primitive2d::Primitive2DContainer &,const class rtl::OUString &)
     const class rtl::OUString & rTempStreamName
@@ -478,15 +414,15 @@ include/vcl/btndlg.hxx:72
     void ButtonDialog::SetButtonHelpText(unsigned short,const class rtl::OUString &)
     const class rtl::OUString & rText
     ""
-include/vcl/dockwin.hxx:36
+include/vcl/dockwin.hxx:37
     void DockingData::DockingData(const class Point &,const class tools::Rectangle &,_Bool)
     _Bool b
     IsFloatingMode()
-include/vcl/dockwin.hxx:47
+include/vcl/dockwin.hxx:48
     void EndDockingData::EndDockingData(const class tools::Rectangle &,_Bool,_Bool)
     _Bool bCancelled
     IsDockingCanceled()
-include/vcl/dockwin.hxx:47
+include/vcl/dockwin.hxx:48
     void EndDockingData::EndDockingData(const class tools::Rectangle &,_Bool,_Bool)
     _Bool b
     IsFloatingMode()
@@ -510,19 +446,19 @@ include/vcl/virdev.hxx:130
     _Bool VirtualDevice::SetOutputSizePixelScaleOffsetAndBuffer(const class Size &,const class Fraction &,const class Point &,unsigned char *)
     const class Point & rNewOffset
     Point()
-include/xmloff/txtparae.hxx:455
+include/xmloff/txtparae.hxx:454
     void XMLTextParagraphExport::exportTextStyles(_Bool,_Bool)
     _Bool bProg
     IsShowProgress()
-include/xmloff/txtparae.hxx:505
+include/xmloff/txtparae.hxx:504
     void XMLTextParagraphExport::collectTextAutoStyles(const class com::sun::star::uno::Reference<class com::sun::star::text::XText> &,const class com::sun::star::uno::Reference<class com::sun::star::text::XTextSection> &,_Bool)
     _Bool bIsProgress
     IsShowProgress()
-include/xmloff/txtparae.hxx:535
+include/xmloff/txtparae.hxx:534
     void XMLTextParagraphExport::exportText(const class com::sun::star::uno::Reference<class com::sun::star::text::XText> &,const class com::sun::star::uno::Reference<class com::sun::star::text::XTextSection> &,_Bool)
     _Bool bIsProgress
     IsShowProgress()
-include/xmloff/xmlexp.hxx:279
+include/xmloff/xmlexp.hxx:280
     void SvXMLExport::SvXMLExport(const class com::sun::star::uno::Reference<class com::sun::star::uno::XComponentContext> &,const class rtl::OUString &,const class rtl::OUString &,const class com::sun::star::uno::Reference<class com::sun::star::xml::sax::XDocumentHandler> &,const class com::sun::star::uno::Reference<class com::sun::star::frame::XModel> &,const enum FieldUnit,enum SvXMLExportFlags)
     const class rtl::OUString & implementationName
     ""
@@ -578,8 +514,8 @@ lotuswordpro/source/filter/lwpoverride.hxx:419
     void LwpIndentOverride::OverrideUseRelative(_Bool)
     _Bool use
     IsUseRelative()
-opencl/source/opencl_device.cxx:494
-    void opencl::(anonymous namespace)::LogWriter::text(const class rtl::OString &)
+opencl/source/opencl_device.cxx:492
+    void (anonymous namespace)::LogWriter::text(const class rtl::OString &)
     const class rtl::OString & rText
     ""
 reportdesign/source/ui/dlg/Condition.hxx:154
@@ -790,7 +726,7 @@ sw/source/filter/ww8/wrtww8.hxx:265
     void WW8_WrPlcSepx::WriteSepx(class SvStream &) const
     class SvStream & rStrm
     Strm()
-sw/source/filter/ww8/ww8scan.hxx:1532
+sw/source/filter/ww8/ww8scan.hxx:1531
     void WW8Fib::Write(class SvStream &)
     class SvStream & rStrm
     Strm()

cui/source/customize/CommandCategoryListBox.cxx
@@ -36,8 +36,8 @@
 #include <o3tl/make_unique.hxx>
 #include <i18nutil/searchopt.hxx>
 
-CommandCategoryListBox::CommandCategoryListBox(vcl::Window* pParent, WinBits nStyle)
-    : ListBox( pParent, nStyle)
+CommandCategoryListBox::CommandCategoryListBox(vcl::Window* pParent)
+    : ListBox( pParent, WB_BORDER | WB_DROPDOWN)
 {
     SetDropDownLineCount(25);
 

cui/source/inc/CommandCategoryListBox.hxx
@@ -37,7 +37,7 @@ class CommandCategoryListBox : public ListBox
     i18nutil::SearchOptions2 m_searchOptions;
 
 public:
-    CommandCategoryListBox( vcl::Window* pParent, WinBits nBits = WB_BORDER | WB_DROPDOWN );
+    CommandCategoryListBox( vcl::Window* pParent );
     virtual ~CommandCategoryListBox() override;
     virtual void dispose() override;
     void ClearAll();

editeng/source/editeng/eerdll.cxx
@@ -150,7 +150,7 @@ DefItems::DefItems()
     // Features
     rDefItems[51] = new SfxVoidItem( EE_FEATURE_TAB );
     rDefItems[52] = new SfxVoidItem( EE_FEATURE_LINEBR );
-    rDefItems[53] = new SvxCharSetColorItem( Color( COL_RED ), RTL_TEXTENCODING_DONTKNOW, EE_FEATURE_NOTCONV );
+    rDefItems[53] = new SvxCharSetColorItem( Color( COL_RED ), EE_FEATURE_NOTCONV );
     rDefItems[54] = new SvxFieldItem( SvxFieldData(), EE_FEATURE_FIELD );
 
     assert(EDITITEMCOUNT == 55 && "ITEMCOUNT changed, adjust DefItems!");

editeng/source/items/textitem.cxx
@@ -1813,11 +1813,10 @@ SvxCharSetColorItem::SvxCharSetColorItem( const sal_uInt16 nId ) :
 
 
 SvxCharSetColorItem::SvxCharSetColorItem( const Color& rCol,
-                                          const rtl_TextEncoding _eFrom,
                                           const sal_uInt16 nId ) :
     SvxColorItem( rCol, nId ),
 
-    eFrom( _eFrom )
+    eFrom( RTL_TEXTENCODING_DONTKNOW )
 {
 }
 

commit 7ab76d2bd9578c2680733bd66fd50902027e0c58

Fix BASIC storage that stored an HTML <P> element
&lt;P STYLE=&quot;margin-bottom: 0cm&quot;&gt;
ended up in the XML stream,

Regression from

    commit 2e0a25ce2b87d3a4bbf944025fc3720933fb391d
    Date:   Fri Sep 8 08:31:42 2017 +0200

        loplugin:constantparam

that removed/kept the wrong path of !bHTML/bHTML

Change-Id: I29ad9c4e7dff7a2d5537f5e52e7316aff7347c7a

vcl/source/edit/texteng.cxx
@@ -2457,23 +2457,8 @@ bool TextEngine::Write( SvStream& rOutput )
         const sal_Int32 nEndPos = nPara == aSel.GetEnd().GetPara()
             ? aSel.GetEnd().GetIndex() : pNode->GetText().getLength();
 
-        OUStringBuffer aText;
-        aText = "<P STYLE=\"margin-bottom: 0cm\">";
-
-        if ( nStartPos == nEndPos )
-        {
-            // Empty lines will be removed by Writer
-            aText.append( "<BR>" );
-        }
-        else
-        {
-            // Text before Attribute
-            aText.append( pNode->GetText().copy( nStartPos, nEndPos-nStartPos ) );
-        }
-
-        aText.append( "</P>" );
-        rOutput.WriteLine(OUStringToOString(aText.makeStringAndClear(),
-            rOutput.GetStreamCharSet()));
+        const OUString aText = pNode->GetText().copy( nStartPos, nEndPos-nStartPos );
+        rOutput.WriteLine(OUStringToOString(aText, rOutput.GetStreamCharSet()));
     }
 
     return rOutput.GetError() == ERRCODE_NONE;