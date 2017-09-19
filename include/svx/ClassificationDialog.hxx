71ee09947d5a71105d64fd225bb3672dfa7ce834
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2017-09-19 15:37:04 +0200

    TSCP: add advanced classification dialog
    This adds a advanced classification dialog, which enables the user
    to manually construct a header/footer message from classification
    properties in cases where the user needs more control. All the
    text is inserted as fields into the end document and can be changed
    by changing the document properties. The simple classification
    still functions as it did before, which is what most users will
    want to use in this case.
    
    The BAF policy has been extended with new xml extension elements:
    - loext:Marking
    - loext:IntellectualPropertyPart
    - loext:IntellectualPropertyPartNumber
    
    They are used to fill the values in the advanced classification
    dialog.
    
    Change-Id: Ie2d638d69b8a9b0799cff9c2b785eb789f8af1d5
    Reviewed-on: https://gerrit.libreoffice.org/42474
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>
    Tested-by: Tomaž Vajngerl <quikee@gmail.com>

---
+++ include/svx/ClassificationDialog.hxx
@@ -0,0 +1,63 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#ifndef INCLUDED_SVX_CLASSIFICATIONDIALOG_HXX
+#define INCLUDED_SVX_CLASSIFICATIONDIALOG_HXX
+
+#include <sal/config.h>
+#include <vcl/dialog.hxx>
+#include <vcl/button.hxx>
+#include <vcl/lstbox.hxx>
+#include <vcl/edit.hxx>
+#include <svx/svxdllapi.h>
+#include <svx/ClassificationEditView.hxx>
+#include <sfx2/classificationhelper.hxx>
+#include <svx/ClassificationField.hxx>
+
+namespace svx {
+
+class SVX_DLLPUBLIC ClassificationDialog : public ModalDialog
+{
+private:
+    VclPtr<ClassificationEditView> m_pEditWindow;
+    VclPtr<PushButton> m_pBoldButton;
+    VclPtr<ListBox> m_pClassificationListBox;
+    VclPtr<ListBox> m_pInternationalClassificationListBox;
+    VclPtr<ListBox> m_pMarkingListBox;
+    VclPtr<ListBox> m_pIntellectualPropertyPartListBox;
+    VclPtr<ListBox> m_pIntellectualPropertyPartNumberListBox;
+    VclPtr<PushButton> m_pIntellectualPropertyPartAddButton;
+    VclPtr<Edit> m_pIntellectualPropertyPartEdit;
+
+    SfxClassificationHelper maHelper;
+    SfxClassificationHelper maInternationalHelper;
+
+    DECL_LINK(ButtonClicked, Button*, void);
+    DECL_LINK(SelectClassificationHdl, ListBox&, void);
+    DECL_LINK(SelectMarkingHdl, ListBox&, void);
+    DECL_LINK(SelectIPPartNumbersHdl, ListBox&, void);
+    DECL_LINK(DoubleClickIPPartHdl, ListBox&, void);
+
+    void insertField(ClassificationType eType, OUString const & rString);
+
+public:
+    ClassificationDialog(vcl::Window* pParent);
+    virtual ~ClassificationDialog() override;
+    virtual void dispose() override;
+
+    std::vector<ClassificationResult> getResult();
+    void setupValues(std::vector<ClassificationResult> const & rInput);
+};
+
+} // end svx namespace
+
+#endif // INCLUDED_SVX_CLASSIFICATIONDIALOG_HXX
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
---
+++ include/svx/ClassificationField.hxx
@@ -0,0 +1,67 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#ifndef INCLUDED_SVX_CLASSIFICATIONFIELD_HXX
+#define INCLUDED_SVX_CLASSIFICATIONFIELD_HXX
+
+#include <sal/config.h>
+#include <svx/svxdllapi.h>
+#include <editeng/flditem.hxx>
+
+namespace svx {
+
+enum class ClassificationType
+{
+    CATEGORY,
+    MARKING,
+    TEXT,
+    INTELLECTUAL_PROPERTY_PART
+};
+
+class SVX_DLLPUBLIC ClassificationField : public SvxFieldData
+{
+public:
+    ClassificationType meType;
+    OUString msDescription;
+
+    ClassificationField(ClassificationType eType, OUString const & sDescription)
+        : SvxFieldData()
+        , meType(eType)
+        , msDescription(sDescription)
+    {}
+
+    ClassificationField* Clone() const override
+    {
+        return new ClassificationField(meType, msDescription);
+    }
+
+    bool operator==(const SvxFieldData& rOther) const override
+    {
+        if (typeid(rOther) != typeid(*this))
+            return false;
+
+        const ClassificationField& rOtherField = static_cast<const ClassificationField&>(rOther);
+        return (meType == rOtherField.meType &&
+                msDescription == rOtherField.msDescription);
+    }
+};
+
+struct SVX_DLLPUBLIC ClassificationResult
+{
+    ClassificationType meType;
+    OUString msString;
+    sal_Int32 mnParagraph;
+};
+
+} // end svx namespace
+
+#endif // INCLUDED_SVX_CLASSIFICATIONFIELD_HXX
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
