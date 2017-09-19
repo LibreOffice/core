6c78ed7673ec42749654fd5e1052def3b6a95215
Eike Rathke <erack@redhat.com>
2017-09-19 21:54:01 +0200

    Ditch now unused GraphicExportDialog, GraphicExportOptionsDialog
    That was a dead end anyway, trying to partly reimplement
    SvFilterOptionsDialog ExportDialog, instead of implementing the
    necessary bits to use that one which has everything.
    
    Change-Id: Icde7422f2c2d7e26c07dfe921a4abda41e222b09

--- svtools/Library_svt.mk
+++ svtools/Library_svt.mk
@@ -149,7 +149,6 @@ $(eval $(call gb_Library_add_exception_objects,svt,\
     svtools/source/edit/svmedit2 \
     svtools/source/edit/textwindowpeer \
     svtools/source/filter/SvFilterOptionsDialog \
-    svtools/source/filter/GraphicExportOptionsDialog \
     svtools/source/filter/DocumentToGraphicRenderer \
     svtools/source/filter/exportdialog \
     svtools/source/graphic/descriptor \
