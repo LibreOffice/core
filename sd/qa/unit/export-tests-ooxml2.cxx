c0a61832965aff202d72a2bf51556c5c6b6d594b
Szymon Kłos <szymon.klos@collabora.com>
2017-09-20 16:29:54 +0200

    tdf#112089 add charts to the ShapeMap
    Charts weren't added to the collection what resulted
    in missing ids during export (target field).
    
    Change-Id: Ie96a4e33a52c5448342a3c3dcec0d8e8b244e270
    Reviewed-on: https://gerrit.libreoffice.org/42552
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>

--- sd/qa/unit/export-tests-ooxml2.cxx
+++ sd/qa/unit/export-tests-ooxml2.cxx
@@ -112,6 +112,7 @@ public:
     void testTdf112552();
     void testTdf112557();
     void testTdf112334();
+    void testTdf112089();
 
     CPPUNIT_TEST_SUITE(SdOOXMLExportTest2);
 
@@ -151,6 +152,7 @@ public:
     CPPUNIT_TEST(testTdf112552);
     CPPUNIT_TEST(testTdf112557);
     CPPUNIT_TEST(testTdf112334);
+    CPPUNIT_TEST(testTdf112089);
 
     CPPUNIT_TEST_SUITE_END();
 
@@ -1142,6 +1144,20 @@ void SdOOXMLExportTest2::testTdf112334()
 
     OUString sAttributeName = getXPathContent(pXmlDocContent, "/p:sld/p:timing/p:tnLst/p:par/p:cTn/p:childTnLst/p:seq/p:cTn/p:childTnLst/p:par/p:cTn/p:childTnLst/p:par/p:cTn/p:childTnLst/p:par/p:cTn/p:childTnLst/p:animClr[1]/p:cBhvr/p:attrNameLst/p:attrName");
     CPPUNIT_ASSERT_EQUAL(OUString("style.color"), sAttributeName);
+}
+
+void SdOOXMLExportTest2::testTdf112089()
+{
+    ::sd::DrawDocShellRef xDocShRef = loadURL(m_directories.getURLFromSrc("sd/qa/unit/data/pptx/tdf112089.pptx"), PPTX);
+    utl::TempFile tempFile;
+    xDocShRef = saveAndReload(xDocShRef.get(), PPTX, &tempFile);
+    xDocShRef->DoClose();
+
+    xmlDocPtr pXmlDocContent = parseExport(tempFile, "ppt/slides/slide1.xml");
+
+    OUString sID = getXPath(pXmlDocContent, "/p:sld/p:cSld/p:spTree/p:graphicFrame/p:nvGraphicFramePr/p:cNvPr", "id");
+    OUString sTarget = getXPath(pXmlDocContent, "/p:sld/p:timing/p:tnLst/p:par/p:cTn/p:childTnLst/p:seq/p:cTn/p:childTnLst/p:par/p:cTn/p:childTnLst/p:par/p:cTn/p:childTnLst/p:par/p:cTn/p:childTnLst/p:set/p:cBhvr/p:tgtEl/p:spTgt", "spid");
+    CPPUNIT_ASSERT_EQUAL(sID, sTarget);
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(SdOOXMLExportTest2);
