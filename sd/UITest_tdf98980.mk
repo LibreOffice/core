36973c38362eae5ef692303c5450d864de04bd46
Serge Krot <Serge.Krot@cib.de>
2017-09-20 11:38:26 +0200

    tdf#98980 Added ui unit test (unfinished)
    Change-Id: I1c7f7f37f0f976e7d106e949f4f8f865850f7c46

---
+++ sd/UITest_tdf98980.mk
@@ -0,0 +1,15 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_UITest_UITest,SlideLayoutChangingTest))
+
+$(eval $(call gb_UITest_add_modules,SlideLayoutChangingTest,$(SRCDIR)/sd/qa/uitest,\
+	tdf98980/ \
+))
+# vim: set noet sw=4 ts=4:
