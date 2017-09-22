1b4f801aeb29b3c052a2bcb3a9af81ac3a1909f2
Tamás Zolnai <tamas.zolnai@collabora.com>
2017-09-22 16:24:06 +0200

    tdf#112552: Shape's gray background is lost after saving to PPTX
    Reviewed-on: https://gerrit.libreoffice.org/42598
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Tamás Zolnai <tamas.zolnai@collabora.com>
    (cherry picked from commit 31919b8909fa7b34412dd52c3d4dff17bc5b6fab)
    
    Change-Id: I30f371ad301eede82ddcece4d91ffcd32e164115

--- include/oox/export/drawingml.hxx
+++ include/oox/export/drawingml.hxx
@@ -44,6 +44,7 @@
 #endif
 
 class Graphic;
+class SdrObjCustomShape;
 
 namespace com { namespace sun { namespace star {
 namespace awt {
@@ -57,6 +58,8 @@ namespace beans {
 }
 namespace drawing {
     class XShape;
+    struct EnhancedCustomShapeParameterPair;
+    struct EnhancedCustomShapeParameter;
 }
 namespace style {
     struct LineSpacing;
@@ -207,7 +210,9 @@ public:
     void WritePresetShape( const char* pShape , std::vector< std::pair<sal_Int32,sal_Int32>> & rAvList );
     void WritePresetShape( const char* pShape );
     void WritePresetShape( const char* pShape, MSO_SPT eShapeType, bool bPredefinedHandlesUsed, sal_Int32 nAdjustmentsWhichNeedsToBeConverted, const css::beans::PropertyValue& rProp );
-    bool WriteCustomGeometry( const css::uno::Reference<css::drawing::XShape>& rXShape );
+    bool WriteCustomGeometry( const css::uno::Reference<css::drawing::XShape>& rXShape, const SdrObjCustomShape* pShape );
+    void WriteCustomGeometryPoint(const css::drawing::EnhancedCustomShapeParameterPair& rParamPair, const SdrObjCustomShape* pShape);
+    static sal_Int32 GetCustomGeometryPointValue(const css::drawing::EnhancedCustomShapeParameter& rParam, const SdrObjCustomShape* pShape);
     void WritePolyPolygon( const tools::PolyPolygon& rPolyPolygon );
     void WriteFill( const css::uno::Reference< css::beans::XPropertySet >& xPropSet );
     void WriteShapeStyle( const css::uno::Reference< css::beans::XPropertySet >& rXPropSet );
