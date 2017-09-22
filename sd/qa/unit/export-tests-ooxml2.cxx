e4d1f5ab3aeb18e6bb1d31967992170e4408316a
Tamás Zolnai <tamas.zolnai@collabora.com>
2017-09-22 16:02:00 +0200

    tdf#112552: Shape's gray background is lost after saving to PPTX
    Reviewed-on: https://gerrit.libreoffice.org/42598
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Tamás Zolnai <tamas.zolnai@collabora.com>
    (cherry picked from commit 31919b8909fa7b34412dd52c3d4dff17bc5b6fab)
    
    Change-Id: I30f371ad301eede82ddcece4d91ffcd32e164115

--- sd/qa/unit/export-tests-ooxml2.cxx
+++ sd/qa/unit/export-tests-ooxml2.cxx
@@ -107,6 +107,7 @@ public:
     void testTdf111518();
     void testTdf106867();
     void testTdf112280();
+    void testTdf112552();
 
     CPPUNIT_TEST_SUITE(SdOOXMLExportTest2);
 
@@ -137,6 +138,7 @@ public:
     CPPUNIT_TEST(testTdf111518);
     CPPUNIT_TEST(testTdf106867);
     CPPUNIT_TEST(testTdf112280);
+    CPPUNIT_TEST(testTdf112552);
 
     CPPUNIT_TEST_SUITE_END();
 
@@ -853,6 +855,21 @@ void SdOOXMLExportTest2::testTdf112280()
     xmlDocPtr pXmlDocContent = parseExport(tempFile, "ppt/slides/slide1.xml");
     assertXPath(pXmlDocContent, "/p:sld/p:timing/p:tnLst/p:par/p:cTn/p:childTnLst/p:seq/p:cTn/p:childTnLst/p:par/p:cTn/p:childTnLst/p:par/p:cTn/p:childTnLst/p:par/p:cTn/p:childTnLst/p:animRot",
             "by", "21600000");
+}
+
+void SdOOXMLExportTest2::testTdf112552()
+{
+    // Background fill was not displayed, but it was because of the wrong geometry
+    ::sd::DrawDocShellRef xDocShRef = loadURL(m_directories.getURLFromSrc("/sd/qa/unit/data/odp/tdf112552.odp"), ODP);
+    utl::TempFile tempFile;
+    xDocShRef = saveAndReload(xDocShRef.get(), PPTX, &tempFile);
+
+    xmlDocPtr pXmlDocContent = parseExport(tempFile, "ppt/slides/slide1.xml");
+    assertXPath(pXmlDocContent, "/p:sld/p:cSld/p:spTree/p:sp/p:spPr/a:custGeom/a:pathLst/a:path", "w", "21600");
+    assertXPath(pXmlDocContent, "/p:sld/p:cSld/p:spTree/p:sp/p:spPr/a:custGeom/a:pathLst/a:path", "h", "21600");
+    assertXPath(pXmlDocContent, "/p:sld/p:cSld/p:spTree/p:sp/p:spPr/a:custGeom/a:pathLst/a:path/a:lnTo[1]/a:pt", "x", "21600");
+    assertXPath(pXmlDocContent, "/p:sld/p:cSld/p:spTree/p:sp/p:spPr/a:custGeom/a:pathLst/a:path/a:lnTo[1]/a:pt", "y", "0");
+    xDocShRef->DoClose();
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(SdOOXMLExportTest2);
