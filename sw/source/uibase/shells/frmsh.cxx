8e38d11ab903bcb8226780638973dec0449801b0
Armin Le Grand <Armin.Le.Grand@cib.de>
2017-09-22 15:28:33 +0200

    RotGrfFlyFrame: Initial adaptions
    To allow free rotation of Graphic FlyFrames in Writer,
    several adaptions are necessary. This change takes care
    of all needed changes to internally support a freely
    definable rotation angle for that case. Save/Load round
    trip is working, the graphic does no longer get modified
    and added in 90-degree-changed state to the object, the
    original will be preserved. Support for needed slot in
    core/ui is implemented. Rotation can be applied from
    Menus/Toolbars in the known 90/180 degree steps. Added
    a slot/Button/command to reset rotation in these cases.
    Added support in Sidebar to rotate using the rotation
    wheel and/or numeric field. These fields and support added
    to Image TabPage, too, fully functional.
    Missing now is a solution for displaying the rotated
    Graphic. For now, it just gets rotated, but this will not
    be the final state of this change.
    
    Change-Id: I6f3b85ebb5be2b4ad3311c536d54f27a37a494e7
    365287ecd6525b1972e8436d61332f7121d88649
    RotGrfFlyFrame: Linux build adaptions
    
    (cherry picked from commit 3db7fbfe024fbc9324afaba3a37cfae20cbf17c9)

--- sw/source/uibase/shells/frmsh.cxx
+++ sw/source/uibase/shells/frmsh.cxx
@@ -84,6 +84,11 @@
 #include <svx/drawitem.hxx>
 #include <memory>
 
+#define SwFrameShell
+#include <sfx2/msg.hxx>
+#include <swslots.hxx>
+#include <grfatr.hxx>
+
 using ::editeng::SvxBorderLine;
 using namespace ::com::sun::star;
 using namespace ::com::sun::star::uno;
@@ -101,10 +106,6 @@ static const SwFrameFormat* lcl_GetFrameFormatByName(SwWrtShell& rSh, const OUSt
     }
     return nullptr;
 }
-
-#define SwFrameShell
-#include <sfx2/msg.hxx>
-#include "swslots.hxx"
 
 SFX_IMPL_INTERFACE(SwFrameShell, SwBaseShell)
 
@@ -361,6 +362,22 @@ void SwFrameShell::Execute(SfxRequest &rReq)
             {
                 aNewSize.setHeight( static_cast< const SfxUInt32Item* >(pItem)->GetValue() );
                 bApplyNewSize = true;
+            }
+
+            // RotGrfFlyFrame: Get Value and disable is in SwGrfShell::GetAttrStateForRotation, but the
+            // value setter uses SID_ATTR_TRANSFORM and a group of three values. Rotation is
+            // added now, so use it in this central place. Do no forget to convert angle from
+            // 100th degrees in SID_ATTR_TRANSFORM_ANGLE to 10th degrees in RES_GRFATR_ROTATION
+            if (pArgs && SfxItemState::SET == pArgs->GetItemState(SID_ATTR_TRANSFORM_ANGLE, false, &pItem))
+            {
+                const sal_uInt32 nNewRot(static_cast<const SfxUInt32Item*>(pItem)->GetValue() / 10);
+                SfxItemSet aSet(rSh.GetAttrPool(), RES_GRFATR_ROTATION, RES_GRFATR_ROTATION, 0, 0 );
+                rSh.GetCurAttr(aSet);
+                const SwRotationGrf& rRotation = static_cast<const SwRotationGrf&>(aSet.Get(RES_GRFATR_ROTATION));
+                const sal_uInt32 nOldRot(rRotation.GetValue());
+
+                // RotGrfFlyFrame: Rotation change here, SwFlyFrameAttrMgr aMgr is available
+                aMgr.SetRotation(nOldRot, nNewRot, rRotation.GetUnrotatedSize());
             }
 
             if ( bApplyNewSize )
