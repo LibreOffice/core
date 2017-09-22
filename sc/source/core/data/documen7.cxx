15c768aceb913af67645ae89f9c8565822eaf68d
Tor Lillqvist <tml@collabora.com>
2017-09-22 18:55:37 +0300

    Add "mutation guard" API for ScDocument
    USe by adding a ScMutationGuard object to a section of code that
    shouldn't be run during threaded calculation of a formula group in any
    of the calculation threads. There are currently several "classes" of
    mutation as bits of ScMutationGuardFlags, althouh I am not sure
    whether that will be useful, so far I use just the one same (CORE) in
    all cases.
    
    Currently implemented using mutexes. Possibly a simple bool field in
    ScDocument would be enough, the multiple flags and mutexes might be
    over-complicating it? Maybe I misunderstood what I want.
    
    Add such mutation guards for some fields of ScDocument.
    
    Change-Id: If2a8223c49d36143f2984e0449798271f2b6440d

--- sc/source/core/data/documen7.cxx
+++ sc/source/core/data/documen7.cxx
@@ -361,6 +361,7 @@ void ScDocument::PutInFormulaTree( ScFormulaCell* pCell )
     OSL_ENSURE( pCell, "PutInFormulaTree: pCell Null" );
     RemoveFromFormulaTree( pCell );
     // append
+    ScMutationGuard aGuard(this, ScMutationGuardFlags::CORE);
     if ( pEOFormulaTree )
         pEOFormulaTree->SetNext( pCell );
     else
@@ -373,6 +374,7 @@ void ScDocument::PutInFormulaTree( ScFormulaCell* pCell )
 
 void ScDocument::RemoveFromFormulaTree( ScFormulaCell* pCell )
 {
+    ScMutationGuard aGuard(this, ScMutationGuardFlags::CORE);
     OSL_ENSURE( pCell, "RemoveFromFormulaTree: pCell Null" );
     ScFormulaCell* pPrev = pCell->GetPrevious();
     assert(pPrev != pCell);                 // pointing to itself?!?
@@ -429,6 +431,7 @@ void ScDocument::CalcFormulaTree( bool bOnlyForced, bool bProgressBar, bool bSet
     if ( IsCalculatingFormulaTree() )
         return ;
 
+    ScMutationGuard aGuard(this, ScMutationGuardFlags::CORE);
     mpFormulaGroupCxt.reset();
     bCalculatingFormulaTree = true;
 
