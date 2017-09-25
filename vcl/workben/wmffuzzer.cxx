1e1f2e677fbf6c8043318450640f54c892699d55
Caolán McNamara <caolanm@redhat.com>
2017-09-25 09:05:42 +0100

    Direct-leak fix effort
    Change-Id: If267c2d40b9e511a8e13be34bb7ba09048a736c5

--- vcl/workben/wmffuzzer.cxx
+++ vcl/workben/wmffuzzer.cxx
@@ -23,6 +23,10 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
     SvMemoryStream aStream(const_cast<uint8_t*>(data), size, StreamMode::READ);
     GDIMetaFile aGDIMetaFile;
     (void)ReadWindowMetafile(aStream, aGDIMetaFile);
+    //fontconfigs alloc mechanism is too complicated for lsan/valgrind so
+    //force the fontconfig options to be released now, they are demand loaded
+    //so will be recreated if necessary
+    SvpSalGraphics::getPlatformGlyphCache().ClearFontOptions();
     return 0;
 }
 
