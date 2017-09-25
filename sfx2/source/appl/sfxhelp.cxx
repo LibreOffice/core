288baabaf42d039a1f22c60bcdc53a8916ca9f22
Olivier Hallot <olivier.hallot@libreoffice.org>
2017-09-25 15:06:50 -0300

    Simplify call to external helponline
    add indentation
    
    chnage helponline.l.org -> help.l.org for good.
    
    Change-Id: Ie52a0f0a716c56da1a9553a805e25ebcabcb1490

--- sfx2/source/appl/sfxhelp.cxx
+++ sfx2/source/appl/sfxhelp.cxx
@@ -54,6 +54,7 @@
 #include <vcl/layout.hxx>
 #include <svtools/ehdl.hxx>
 #include <svtools/sfxecode.hxx>
+#include "openuriexternally.hxx"
 
 #include "newhelp.hxx"
 #include <sfx2/objsh.hxx>
@@ -530,16 +531,13 @@ static bool impl_showOnlineHelp( const OUString& rURL )
     if ( rURL.getLength() <= aInternal.getLength() || !rURL.startsWith(aInternal) )
         return false;
 
-    OUString aHelpLink( "http://helponline.libreoffice.org/help.html?"  );
+    OUString aHelpLink( "https://help.libreoffice.org/help.html?"  );
 
     aHelpLink += rURL.copy( aInternal.getLength() );
     aHelpLink = aHelpLink.replaceAll("%2F","/");
     try
     {
-        Reference< XSystemShellExecute > xSystemShell(
-                SystemShellExecute::create(::comphelper::getProcessComponentContext()) );
-
-        xSystemShell->execute( aHelpLink, OUString(), SystemShellExecuteFlags::URIS_ONLY );
+        sfx2::openUriExternally(aHelpLink, false);
         return true;
     }
     catch (const Exception&)
