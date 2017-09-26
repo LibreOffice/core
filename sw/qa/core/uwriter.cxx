3b27fa90814aecffe35e6b2214894c783c5d10a1
Stephan Bergmann <sbergman@redhat.com>
2017-09-26 14:38:52 +0200

    Better way to detect Apple Xcode 9 Clang
    ...without also accidentally affecting any upstream Clang 9.0
    
    Change-Id: Ie52413e2e53a2ce01f55b97677fe9878d498b076
    Reviewed-on: https://gerrit.libreoffice.org/42809
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

--- sw/qa/core/uwriter.cxx
+++ sw/qa/core/uwriter.cxx
@@ -1412,8 +1412,10 @@ void SwDocTest::testTableAutoFormats()
     //Get m_aRotateAngle
     CPPUNIT_ASSERT( bool( pLoadAF->GetBoxFormat(0).m_aRotateAngle == aRAngle ) );
 #if !((defined(__clang__) && __clang_major__ == 4 && __clang_minor__ == 0) \
-      || (defined __APPLE__ && defined __clang__ && __clang_major__ == 9 \
-          && __clang_minor__ == 0))
+      || (defined __APPLE__ && defined __clang__ \
+          && defined __apple_build_version__ \
+          && __apple_build_version__ >= 9000000 \
+          && __apple_build_version__ <= 9000099))
         // Temporary fix for mysterious problem with Clang in F26 and Xcode 9
     //Get m_aRotateMode
     SvxRotateModeItem aRMode = aBoxAF.m_aRotateMode;
