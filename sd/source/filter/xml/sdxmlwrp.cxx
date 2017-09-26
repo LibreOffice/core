cae335091776738fdb2dfb295033803279d30409
Caolán McNamara <caolanm@redhat.com>
2017-09-26 10:20:11 +0100

    oom in fodpfuzzer
    Change-Id: Iba6ce63f21f9dfe7187eea81c6d76fad12ccc498

--- sd/source/filter/xml/sdxmlwrp.cxx
+++ sd/source/filter/xml/sdxmlwrp.cxx
@@ -1049,6 +1049,8 @@ extern "C" SAL_DLLPUBLIC_EXPORT bool SAL_CALL TestImportFODP(SvStream &rStream)
     bool ret = xFilter->filter(aArgs);
     xDocSh->SetLoading(SfxLoadedFlags::ALL);
 
+    xDocSh->DoClose();
+
     return ret;
 }
 
