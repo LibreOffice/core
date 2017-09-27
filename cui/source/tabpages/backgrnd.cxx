09d346305d9ac07ea10dc1dd8392f870c86a9e65
Noel Grandin <noel.grandin@collabora.co.uk>
2017-09-27 11:36:18 +0200

    simplify Bitmap operator==/IsEqual (stage1)
    It looks like operator== is actually only useful internally. So inline
    that, and rename the other call sites to use IsEqual().
    
    As a second stage, I will rename IsEqual to operator==, the intention
    being to make it obvious how the call sites are modified.
    
    Change-Id: I37f2920a8cafaffb25e8c5c16e6559546206684b
    Reviewed-on: https://gerrit.libreoffice.org/42846
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
    (cherry picked from commit 404630c6052919c1a0b5dab405084c2a39fc2f5c)

--- cui/source/tabpages/backgrnd.cxx
+++ cui/source/tabpages/backgrnd.cxx
@@ -713,7 +713,7 @@ bool SvxBackgroundTabPage::FillItemSet( SfxItemSet* rCoreSet )
                 {
                     const Graphic* pGraphic = rOldItem.GetGraphic();
                     if (pGraphic)
-                        bModifyBrush = pGraphic->GetBitmap() != aBgdGraphic.GetBitmap();
+                        bModifyBrush = !pGraphic->GetBitmap().IsEqual(aBgdGraphic.GetBitmap());
                 }
                 if (bModifyBrush)
                 {
