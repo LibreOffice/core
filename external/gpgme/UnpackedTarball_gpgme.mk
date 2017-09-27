db04019c55f706dcbde1ca4fa52070b005280f86
David Ostrovsky <david@ostrovsky.org>
2017-09-27 08:57:36 +0200

    gpg4libre: build gpgme natively on windows
    Change-Id: I0b77ba0c1b8f84affa483181e26a26beaf39e878

--- external/gpgme/UnpackedTarball_gpgme.mk
+++ external/gpgme/UnpackedTarball_gpgme.mk
@@ -17,5 +17,6 @@ $(eval $(call gb_UnpackedTarball_add_patches,gpgme, \
     external/gpgme/find-libgpg-error-libassuan.patch \
     external/gpgme/fix-autoconf-macros.patch \
     external/gpgme/add-minimal-keyexport.patch \
+    $(if $(filter MSC,$(COM)),external/gpgme/w32-build-fixes.patch.1) \
 ))
 # vim: set noet sw=4 ts=4:
