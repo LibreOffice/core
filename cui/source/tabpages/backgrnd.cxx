f5b16fc3aa7540759e6e959dbf664dd83d2f87e4
Noel Grandin <noel.grandin@collabora.co.uk>
2017-09-27 14:22:45 +0200

    simplify Bitmap operator==/IsEqual (stage2)
    Now that we've sorted out which bits are rightfully internal (see commit
    7a4064cf87d23bffac987b3e39bf87d9b4d158fc), and which bits need a
    comparison operator, rename IsEqual to operator==.
    
    Change-Id: Iec229a7571d76ee18fbc0a4fe672cc2e50a44948
    Reviewed-on: https://gerrit.libreoffice.org/42851
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
    (cherry picked from commit 09917566b6ce86ea65a246f2055b5d1f41df4e76)
    Reviewed-on: https://gerrit.libreoffice.org/45456
    Reviewed-by: Ashod Nakashian <ashnakash@gmail.com>
    Tested-by: Ashod Nakashian <ashnakash@gmail.com>
    (cherry picked from commit f641e9d9f472110ccb61c01210e8acf280ad9bf3)

--- cui/source/tabpages/backgrnd.cxx
+++ cui/source/tabpages/backgrnd.cxx
@@ -716,7 +716,7 @@ bool SvxBackgroundTabPage::FillItemSet( SfxItemSet* rCoreSet )
                 {
                     const Graphic* pGraphic = rOldItem.GetGraphic();
                     if (pGraphic)
-                        bModifyBrush = !pGraphic->GetBitmap().IsEqual(aBgdGraphic.GetBitmap());
+                        bModifyBrush = pGraphic->GetBitmap() != aBgdGraphic.GetBitmap();
                 }
                 if (bModifyBrush)
                 {
