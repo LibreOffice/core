1047b426ed7ab0ee7a1b3ecf01adab18eb040de2
Markus Mohrhard <markus.mohrhard@googlemail.com>
2017-09-28 18:37:56 +0200

    updater: make sure the target dir exists while writing update info
    Change-Id: I6cafdb28468d0023a0a1ea8cc2c051e9b84a1c6c

--- bin/update/make_incremental_update.sh
+++ bin/update/make_incremental_update.sh
@@ -102,10 +102,10 @@ fi
 workdir="$newdir.work"
 updatemanifestv2="$workdir/updatev2.manifest"
 updatemanifestv3="$workdir/updatev3.manifest"
-echo "updatev2.manifest" >> $workdir/files.txt
-echo "updatev3.manifest" >> $workdir/files.txt
 
 mkdir -p "$workdir"
+echo "updatev2.manifest" >> $workdir/files.txt
+echo "updatev3.manifest" >> $workdir/files.txt
 
 # Generate a list of all files in the target directory.
 pushd "$olddir"
