28dbeab6d8b35e4dffb4f444b85e3b27716aed66
Jan-Marek Glogowski <glogow@fbihome.de>
2017-09-29 21:02:17 +0200

    Convert tick-based timer events to versioned ones
    Instead of storing the system ticks in the timer event message
    simply store a version.
    
    Moves the version handling code into a VersionedEvent class,
    inherited by WinSalTimer and AquaSalTimer.
    
    Change-Id: I5add85031d36b3424a26a9ef798294cbfb00b2e4

--- vcl/inc/osx/saltimer.h
+++ vcl/inc/osx/saltimer.h
@@ -41,10 +41,9 @@ public:
     ~ReleasePoolHolder() { [mpPool release]; }
 };
 
-class AquaSalTimer : public SalTimer
+class AquaSalTimer final : public SalTimer, protected VersionedEvent
 {
     NSTimer    *m_pRunningTimer;
-    sal_Int32   m_nTimerStartTicks;  ///< system ticks at timer start % SAL_MAX_INT32
 
     void queueDispatchTimerEvent( bool bAtStart );
     void callTimerCallback();
