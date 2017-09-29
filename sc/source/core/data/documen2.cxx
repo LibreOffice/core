390782b70672b60b374a955112ba4debae621e50
Tor Lillqvist <tml@collabora.com>
2017-09-29 17:01:03 +0300

    Move nInterpreterTableOpLevel back to ScDocument
    Change-Id: I4de0051d9fa5de9147954c6021d47076145a3e59

--- sc/source/core/data/documen2.cxx
+++ sc/source/core/data/documen2.cxx
@@ -176,6 +176,7 @@ ScDocument::ScDocument( ScDocumentMode eMode, SfxObjectShell* pDocShell ) :
         nXMLImportedFormulaCount( 0 ),
         nInterpretLevel(0),
         nMacroInterpretLevel(0),
+        nInterpreterTableOpLevel(0),
         nSrcVer( SC_CURRENT_VERSION ),
         nFormulaTrackCount(0),
         eHardRecalcState(HardRecalcState::OFF),
