LibreOffice/core

commit 12b2803f196b897ffb901b0eebe271c5ee43d03e

drop unused PRESOBJ_IMAGE
Change-Id: I8fa011bea423e961a3ebe7cd8687e4bce7c4cd77

sd/source/core/sdpage.cxx
@@ -189,7 +189,6 @@ SdrObject* SdPage::GetPresObj(PresObjKind eObjKind, int nIndex, bool bFuzzySearc
                 case PRESOBJ_ORGCHART:
                 case PRESOBJ_TABLE:
                 case PRESOBJ_CALC:
-                case PRESOBJ_IMAGE:
                 case PRESOBJ_MEDIA:
                     bFound = true;
                     break;

compilerplugins/clang/unusedenumconstants.py
@@ -111,6 +111,7 @@ for d in definitionSet:
          "include/svx/msdffdef.hxx",
          "include/svtools/rtftoken.h", # RTF_TOKEN_IDS
          "starmath/source/mathtype.hxx", # MathType::MTOKENS
+         "sd/source/filter/eppt/epptbase.hxx", # PPTExTextAttr
         # unit test code
          "cppu/source/uno/check.cxx",
         # general weird nonsense going on
@@ -150,6 +151,9 @@ for d in definitionSet:
          "include/svx/sxekitm.hxx", # SdrEdgeKind (from UI)
          "include/svx/paraprev.hxx", # SvxPrevLineSpace (from UI)
          "include/svx/ctredlin.hxx", # SvxRedlinDateMode (from UI)
+         "sd/source/ui/inc/animobjs.hxx", # BitmapAdjustment (from UI)
+         "sd/source/ui/dlg/PhotoAlbumDialog.hxx", # SlideImageLayout (from UI)
+         "sd/inc/pres.hxx", # AutoLayout (from UI)
         # represents constants from an external API
          "opencl/inc/opencl_device_selection.h",
          "vcl/inc/sft.hxx",

sd/inc/pres.hxx
@@ -30,7 +30,6 @@ enum PresObjKind
     PRESOBJ_CHART,
     PRESOBJ_ORGCHART,
     PRESOBJ_TABLE,
-    PRESOBJ_IMAGE,
     PRESOBJ_PAGE,
     PRESOBJ_HANDOUT,
     PRESOBJ_NOTES,

sd/source/ui/unoidl/unopage.cxx
@@ -1490,7 +1490,6 @@ Reference< drawing::XShape >  SdGenericDrawPage::CreateShape(SdrObject *pObj) co
                 break;
             // coverity[dead_error_begin] - following conditions exist to avoid compiler warning
             case PRESOBJ_NONE:
-            case PRESOBJ_IMAGE:
             case PRESOBJ_MAX:
                 break;
             }

sd/source/ui/view/drawview.cxx
@@ -550,7 +550,6 @@ void DrawView::DeleteMarked()
                     case PRESOBJ_ORGCHART:
                     case PRESOBJ_TABLE:
                     case PRESOBJ_CALC:
-                    case PRESOBJ_IMAGE:
                     case PRESOBJ_MEDIA:
                         ePresObjKind = PRESOBJ_OUTLINE;
                         break;

sd/source/ui/view/sdview5.cxx
@@ -33,7 +33,6 @@ static bool implIsMultiPresObj( PresObjKind eKind )
     case PRESOBJ_CHART:
     case PRESOBJ_ORGCHART:
     case PRESOBJ_TABLE:
-    case PRESOBJ_IMAGE:
     case PRESOBJ_MEDIA:
         return true;
     default:

commit 9619efc2bd8adf844ada049486760bddec2d98e2

tdf#112836 master view, bottom areas are wrongly positioned
Bug was introduced in
    commit 12b2803f196b897ffb901b0eebe271c5ee43d03e
    "drop unused PRESOBJ_IMAGE"

Change-Id: I50be74b505a674439bd8b67545b89a9660a85a33

sd/source/core/sdpage.cxx
@@ -900,7 +900,7 @@ const char* const PresObjKindVector[] = {
     "PRESOBJ_NONE", "PRESOBJ_TITLE", "PRESOBJ_OUTLINE",
     "PRESOBJ_TEXT" ,"PRESOBJ_GRAPHIC" , "PRESOBJ_OBJECT",
     "PRESOBJ_CHART", "PRESOBJ_ORGCHART", "PRESOBJ_TABLE",
-    "PRESOBJ_IMAGE", "PRESOBJ_PAGE", "PRESOBJ_HANDOUT",
+    "PRESOBJ_PAGE", "PRESOBJ_HANDOUT",
     "PRESOBJ_NOTES","PRESOBJ_HEADER", "PRESOBJ_FOOTER",
     "PRESOBJ_DATETIME", "PRESOBJ_SLIDENUMBER", "PRESOBJ_CALC",
     "PRESOBJ_MEDIA", "PRESOBJ_MAX"