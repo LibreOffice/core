c985ad5f7b479706935459630c5a59ccae6fa8b7
Miklos Vajna <vmiklos@collabora.co.uk>
2017-10-03 17:44:01 +0200

    tdf#112860 sw: avoid shortcut for tables in SwFrame::GetNextSctLeaf()
    As the comment says the shortcut only works if no columns or pages are
    between the section frames, while for tables there are definitely table,
    row and column frames. This resulted later in
    SwFrame::IsFootnoteAllowed() a nullptr dereference, as the frame was
    still marked as in-table, but wasn't in a table anymore.
    
    Change-Id: I4191c77d20c6ccc096156fae15ca7c22cc5b68b6
    Reviewed-on: https://gerrit.libreoffice.org/43091
    Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
    Tested-by: Jenkins <ci@libreoffice.org>

--- sw/source/core/layout/ftnfrm.cxx
+++ sw/source/core/layout/ftnfrm.cxx
@@ -776,6 +776,7 @@ bool SwFrame::IsFootnoteAllowed() const
     {
         // no footnotes in repeated headlines
         const SwTabFrame *pTab = const_cast<SwFrame*>(this)->ImplFindTabFrame();
+        assert(pTab);
         if ( pTab->IsFollow() )
             return !pTab->IsInHeadline( *this );
     }
