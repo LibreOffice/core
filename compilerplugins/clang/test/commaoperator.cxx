4fc52078f6afa4368b2f4de3cd700e474b7a417f
Stephan Bergmann <sbergman@redhat.com>
2017-10-06 08:55:26 +0200

    Improve performance of loplugin:commaoperator
    ...by avoiding calls to parentStmt, thereby also improving the precision of
    exactly which comma operators to ignore (which turned up a handful more finds).
    Also added tests.
    
    Change-Id: Ie74f824fd7f54131aab09b59086452fb4f3ff827
    Reviewed-on: https://gerrit.libreoffice.org/43181
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

---
+++ compilerplugins/clang/test/commaoperator.cxx
@@ -0,0 +1,27 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+bool f();
+
+struct S { ~S(); };
+
+int main() {
+    f(), f(), f(); // expected-error {{comma operator hides code [loplugin:commaoperator]}}
+    (f(), f());
+    for (
+        f(), f();
+        f(), f(); // expected-error {{comma operator hides code [loplugin:commaoperator]}}
+        f(), f())
+        f(), f(); // expected-error {{comma operator hides code [loplugin:commaoperator]}}
+    S s;
+    (s = S(), s = S(), s = S());
+    for (s = S(), f(); f(); s = S(), f()) {}
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
