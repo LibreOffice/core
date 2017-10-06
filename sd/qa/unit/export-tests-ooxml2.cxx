54293c1fae0c774c72a8564a25844aecf6d40f7f
Szymon Kłos <szymon.klos@collabora.com>
2017-10-06 16:04:25 +0200

    SmartArt: export text rotation in the PPTX
    Change-Id: I74bfc172fe7434d14de468ecfd3412636e53780c
    Reviewed-on: https://gerrit.libreoffice.org/43202
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/44080
    Reviewed-by: Andras Timar <andras.timar@collabora.com>
    Tested-by: Andras Timar <andras.timar@collabora.com>

--- sd/qa/unit/export-tests-ooxml2.cxx
+++ sd/qa/unit/export-tests-ooxml2.cxx
@@ -108,6 +108,7 @@ public:
     void testTdf105739();
     void testTdf112552();
     void testTdf112647();
+    void testSmartartRotation2();
 
     CPPUNIT_TEST_SUITE(SdOOXMLExportTest2);
 
@@ -137,6 +138,7 @@ public:
     CPPUNIT_TEST(testTdf105739);
     CPPUNIT_TEST(testTdf112552);
     CPPUNIT_TEST(testTdf112647);
+    CPPUNIT_TEST(testSmartartRotation2);
 
     CPPUNIT_TEST_SUITE_END();
 
@@ -825,6 +827,17 @@ void SdOOXMLExportTest2::testTdf112647()
     CPPUNIT_ASSERT_EQUAL(sal_Int16(css::style::LineSpacingMode::FIX), aLineSpacing.Mode);
     CPPUNIT_ASSERT_EQUAL(sal_Int16(2117), aLineSpacing.Height);
     xDocShRef->DoClose();
+}
+
+void SdOOXMLExportTest2::testSmartartRotation2()
+{
+    ::sd::DrawDocShellRef xDocShRef = loadURL(m_directories.getURLFromSrc("sd/qa/unit/data/pptx/smartart-rotation2.pptx"), PPTX);
+    utl::TempFile tempFile;
+    xDocShRef = saveAndReload(xDocShRef.get(), PPTX, &tempFile);
+    xDocShRef->DoClose();
+
+    xmlDocPtr pXmlDocContent = parseExport(tempFile, "ppt/slides/slide1.xml");
+    assertXPath(pXmlDocContent, "/p:sld/p:cSld/p:spTree/p:sp[3]/p:txBody/a:bodyPr", "rot", "10800000");
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(SdOOXMLExportTest2);
