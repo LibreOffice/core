e054b898e7510388d613e4f918afce53aa5444c7
Noel Grandin <noel.grandin@collabora.co.uk>
2017-10-06 15:58:13 +0200

    drop use of tools::SvRef in xmloff
    instead of mixing and matching rtl::Reference and tools::SvRef, lets
    just use rtl::Reference
    
    Change-Id: I5d77c8a662e1455f97935e5f4a99e484e8cad332
    Reviewed-on: https://gerrit.libreoffice.org/43205
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
    Tested-by: Noel Grandin <noel.grandin@collabora.co.uk>

--- xmloff/source/forms/elementimport.hxx
+++ xmloff/source/forms/elementimport.hxx
@@ -512,7 +512,7 @@ namespace xmloff
         void implSelectCurrentItem();
         void implDefaultSelectCurrentItem();
     };
-    typedef tools::SvRef<OListAndComboImport> OListAndComboImportRef;
+    typedef rtl::Reference<OListAndComboImport> OListAndComboImportRef;
 
     //= OListOptionImport
     /** helper class for importing a single &lt;form:option&gt; element.
