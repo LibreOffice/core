9bc7de59a438087a3b57973126730cb48b06ca79
Julien Nabet <serval2412@yahoo.fr>
2017-10-08 20:40:05 +0200

    Remove extra line added by mistake
    Change-Id: I75141d99744bf3133b6c64aec9a56db13f836d0c
    Reviewed-on: https://gerrit.libreoffice.org/43264
    Reviewed-by: Julien Nabet <serval2412@yahoo.fr>
    Tested-by: Julien Nabet <serval2412@yahoo.fr>

--- connectivity/source/drivers/odbc/OResultSet.cxx
+++ connectivity/source/drivers/odbc/OResultSet.cxx
@@ -166,7 +166,6 @@ OResultSet::~OResultSet()
 {
     setStmtOption<SQLLEN*, SQL_IS_POINTER>(SQL_ATTR_FETCH_BOOKMARK_PTR, nullptr);
     setStmtOption<SQLUSMALLINT*, SQL_IS_POINTER>(SQL_ATTR_ROW_STATUS_PTR, nullptr);
-
 }
 
 void OResultSet::construct()
