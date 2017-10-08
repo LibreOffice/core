LibreOffice/core

commit 404630c6052919c1a0b5dab405084c2a39fc2f5c

simplify Bitmap operator==/IsEqual (stage1)
It looks like operator== is actually only useful internally. So inline
that, and rename the other call sites to use IsEqual().

As a second stage, I will rename IsEqual to operator==, the intention
being to make it obvious how the call sites are modified.

Change-Id: I37f2920a8cafaffb25e8c5c16e6559546206684b
Reviewed-on: https://gerrit.libreoffice.org/42846
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

include/vcl/bitmap.hxx
@@ -224,8 +224,8 @@ public:
     Bitmap&                 operator=( const Bitmap& rBitmap );
     Bitmap&                 operator=( Bitmap&& rBitmap );
     inline bool             operator!() const;
-    inline bool             operator==( const Bitmap& rBitmap ) const;
-    inline bool             operator!=( const Bitmap& rBitmap ) const;
+    bool                    operator==( const Bitmap& rBitmap ) const = delete;
+    bool                    operator!=( const Bitmap& rBitmap ) const = delete;
 
     bool                    IsEqual( const Bitmap& rBmp ) const;
 
@@ -730,16 +730,6 @@ private:
 inline bool Bitmap::operator!() const
 {
     return( mxImpBmp == nullptr );
-}
-
-inline bool Bitmap::operator==( const Bitmap& rBitmap ) const
-{
-    return( rBitmap.mxImpBmp == mxImpBmp );
-}
-
-inline bool Bitmap::operator!=( const Bitmap& rBitmap ) const
-{
-    return( rBitmap.mxImpBmp != mxImpBmp );
 }
 
 inline bool Bitmap::IsEmpty() const

vcl/source/gdi/bitmapex.cxx
@@ -193,7 +193,7 @@ bool BitmapEx::operator==( const BitmapEx& rBitmapEx ) const
     if( eTransparent != rBitmapEx.eTransparent )
         return false;
 
-    if( aBitmap != rBitmapEx.aBitmap )
+    if( !aBitmap.IsEqual(rBitmapEx.aBitmap) )
         return false;
 
     if( aBitmapSize != rBitmapEx.aBitmapSize )
@@ -205,7 +205,7 @@ bool BitmapEx::operator==( const BitmapEx& rBitmapEx ) const
     if( eTransparent == TransparentType::Color )
         return aTransparentColor == rBitmapEx.aTransparentColor;
 
-    return( ( aMask == rBitmapEx.aMask ) && ( bAlpha == rBitmapEx.bAlpha ) );
+    return aMask.IsEqual(rBitmapEx.aMask) && bAlpha == rBitmapEx.bAlpha;
 }
 
 bool BitmapEx::IsEmpty() const

cui/source/tabpages/backgrnd.cxx
@@ -683,7 +683,7 @@ bool SvxBackgroundTabPage::FillItemSet( SfxItemSet* rCoreSet )
                 {
                     const Graphic* pGraphic = rOldItem.GetGraphic();
                     if (pGraphic)
-                        bModifyBrush = pGraphic->GetBitmap() != aBgdGraphic.GetBitmap();
+                        bModifyBrush = !pGraphic->GetBitmap().IsEqual(aBgdGraphic.GetBitmap());
                 }
                 if (bModifyBrush)
                 {

include/vcl/alpha.hxx
@@ -44,8 +44,9 @@ public:
     AlphaMask&  operator=( const AlphaMask& rAlphaMask ) { return static_cast<AlphaMask&>( Bitmap::operator=( rAlphaMask ) ); }
     AlphaMask&  operator=( AlphaMask&& rAlphaMask ) { return static_cast<AlphaMask&>( Bitmap::operator=( std::move(rAlphaMask) ) ); }
     bool        operator!() const { return Bitmap::operator!(); }
-    bool        operator==( const AlphaMask& rAlphaMask ) const { return Bitmap::operator==( rAlphaMask ); }
-    bool        operator!=( const AlphaMask& rAlphaMask ) const { return Bitmap::operator!=( rAlphaMask ); }
+    bool        operator==( const AlphaMask& rAlphaMask ) const = delete;
+    bool        operator!=( const AlphaMask& rAlphaMask ) const = delete;
+    bool        IsEqual( const AlphaMask& rAlphaMask ) const { return Bitmap::IsEqual(rAlphaMask); }
 
     void        SetPrefMapMode( const MapMode& rMapMode ) { Bitmap::SetPrefMapMode( rMapMode ); }
 

vcl/source/gdi/bitmap.cxx
@@ -779,7 +779,7 @@ bool Bitmap::CopyPixel( const tools::Rectangle& rRectDst,
 
     if( !aRectDst.IsEmpty() )
     {
-        if( pBmpSrc && ( *pBmpSrc != *this ) )
+        if( pBmpSrc && ( pBmpSrc->mxImpBmp != mxImpBmp ) )
         {
             Bitmap*         pSrc = const_cast<Bitmap*>(pBmpSrc);
             const Size      aCopySizePix( pSrc->GetSizePixel() );
@@ -958,7 +958,7 @@ bool Bitmap::CopyPixel_AlphaOptimized( const tools::Rectangle& rRectDst, const t
 
     if( !aRectDst.IsEmpty() )
     {
-        if( pBmpSrc && ( *pBmpSrc != *this ) )
+        if( pBmpSrc && ( pBmpSrc->mxImpBmp != mxImpBmp ) )
         {
             Bitmap*         pSrc = const_cast<Bitmap*>(pBmpSrc);
             const Size      aCopySizePix( pSrc->GetSizePixel() );

commit 78f7298488a995a7f394d2d1028d403fc96b5a16

tdf#112939 Color on drop-down color chooser is always black
regression from
    commit 404630c6052919c1a0b5dab405084c2a39fc2f5c
    simplify Bitmap operator==/IsEqual (stage1)

Change-Id: Ifabbfb28e0ccbe28b19db7f2c50e6d6f55569f9a

include/vcl/bitmap.hxx
@@ -226,6 +226,8 @@ public:
     inline bool             operator!() const;
     bool                    operator==( const Bitmap& rBitmap ) const;
     bool                    operator!=( const Bitmap& rBitmap ) const { return !operator==(rBitmap); }
+    // only compares if we are using the same internal copy-on-write Impl class
+    bool                    ShallowEquals( const Bitmap& rBitmap ) const { return mxImpBmp == rBitmap.mxImpBmp; }
 
     inline bool             IsEmpty() const;
     void                    SetEmpty();

vcl/source/gdi/bitmapex.cxx
@@ -193,7 +193,7 @@ bool BitmapEx::operator==( const BitmapEx& rBitmapEx ) const
     if( eTransparent != rBitmapEx.eTransparent )
         return false;
 
-    if( aBitmap != rBitmapEx.aBitmap )
+    if( !aBitmap.ShallowEquals(rBitmapEx.aBitmap) )
         return false;
 
     if( aBitmapSize != rBitmapEx.aBitmapSize )
@@ -205,7 +205,7 @@ bool BitmapEx::operator==( const BitmapEx& rBitmapEx ) const
     if( eTransparent == TransparentType::Color )
         return aTransparentColor == rBitmapEx.aTransparentColor;
 
-    return aMask == rBitmapEx.aMask && bAlpha == rBitmapEx.bAlpha;
+    return aMask.ShallowEquals(rBitmapEx.aMask) && bAlpha == rBitmapEx.bAlpha;
 }
 
 bool BitmapEx::IsEmpty() const