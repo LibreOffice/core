734b816b6cb8b79cedd402d5db8be470608b31ce
Caolán McNamara <caolanm@redhat.com>
2017-10-09 16:10:43 +0100

    shrink bmp fuzzer
    Change-Id: I9119c61f80631bb57bc8eade552a4d88b0c699df

--- vcl/Executable_bmpfuzzer.mk
+++ vcl/Executable_bmpfuzzer.mk
@@ -33,7 +33,6 @@ $(eval $(call gb_Executable_use_libraries,bmpfuzzer,\
 $(eval $(call gb_Executable_use_static_libraries,bmpfuzzer,\
     findsofficepath \
     ulingu \
-    fuzzer_core \
     fuzzerstubs \
 ))
 
