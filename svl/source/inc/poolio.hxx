f618287ca2b369c4bc354bf5c82ccc18cf7b39d3
Takeshi Abe <tabe@fixedpoint.jp>
2017-10-10 18:38:39 +0900

    svl: Drop dead constants
    which were gone at b021353dd62c3d8c9ee0281753b88f6304a2514d
    and 75933b220d48bceff25b07cfc4b55c70a2e24917.
    
    Change-Id: Ibfff9a4b0c5f834c1daaedeeff8e48d8eb923cb3

--- svl/source/inc/poolio.hxx
+++ svl/source/inc/poolio.hxx
@@ -34,8 +34,6 @@ class SfxItemPoolUser;
 #define DELETEZ(pPtr) { delete pPtr; pPtr = 0; }
 #endif
 
-static const sal_uInt32 SFX_ITEMS_DIRECT  = 0xffffffff;
-static const sal_uInt32 SFX_ITEMS_NULL    = 0xfffffff0;  // instead StoreSurrogate
 static const sal_uInt32 SFX_ITEMS_DEFAULT = 0xfffffffe;
 
 /**
