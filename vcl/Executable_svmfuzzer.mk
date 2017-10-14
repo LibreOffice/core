71ac6dd8bf10504b94ca5c50ce17db92871c1a4b
Caolán McNamara <caolanm@redhat.com>
2017-10-14 20:55:46 +0100

    shrink svm fuzzer
    Change-Id: I6b8c678374f6101b25abe3efd81e77696e4ff1cc

--- vcl/Executable_svmfuzzer.mk
+++ vcl/Executable_svmfuzzer.mk
@@ -33,7 +33,6 @@ $(eval $(call gb_Executable_use_libraries,svmfuzzer,\
 $(eval $(call gb_Executable_use_static_libraries,svmfuzzer,\
     findsofficepath \
     ulingu \
-    fuzzer_core \
     fuzzerstubs \
 ))
 
