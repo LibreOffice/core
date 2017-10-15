f8b9116e710891cbd8674fe838f04e41346d83b8
Caolán McNamara <caolanm@redhat.com>
2017-10-15 17:14:42 +0100

    shrink psd fuzzer
    Change-Id: Ic47710ec80c6c71b48ffb10449398348bec2189b

--- vcl/Executable_psdfuzzer.mk
+++ vcl/Executable_psdfuzzer.mk
@@ -33,7 +33,6 @@ $(eval $(call gb_Executable_use_libraries,psdfuzzer,\
 $(eval $(call gb_Executable_use_static_libraries,psdfuzzer,\
     findsofficepath \
     ulingu \
-    fuzzer_core \
     fuzzerstubs \
 ))
 
