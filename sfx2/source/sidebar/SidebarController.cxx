98f35356a4a88d60ceccb037a4595778503c50f4
Jan-Marek Glogowski <glogow@fbihome.de>
2017-10-17 19:52:58 +0200

    tdf#99537 set expand for re-used sidebar panels
    When re-using a panel, reset the expand state from the stored
    state in the PanelContexDescriptor.
    
    Change-Id: I00851982bbd36cacdc57459bb6e5be9fcc719e81
    Reviewed-on: https://gerrit.libreoffice.org/43466
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Yousuf Philips <philipz85@hotmail.com>
    Tested-by: Yousuf Philips <philipz85@hotmail.com>
    Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>
    (cherry picked from commit ce2e94a98f5fc85bef7623a0227b3a95d8093288)

--- sfx2/source/sidebar/SidebarController.cxx
+++ sfx2/source/sidebar/SidebarController.cxx
@@ -619,6 +619,7 @@ void SidebarController::CreatePanels(const ::rtl::OUString& rDeckId, const Conte
         if (pPanel != nullptr)
         {
             aNewPanels[nWriteIndex] = pPanel;
+            pPanel->SetExpanded( rPanelContexDescriptor.mbIsInitiallyVisible );
             ++nWriteIndex;
         }
         else
