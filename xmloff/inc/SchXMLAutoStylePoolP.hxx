dc298b0d447108ad6ee92cad565361676192e71e
Stephan Bergmann <sbergman@redhat.com>
2017-10-17 16:20:44 +0200

    Move SchXMLAutoStylePoolP.hxx next to SchXMLExport.hxx including it
    Change-Id: I21a805d90f04432a864ec2567fd707da27bc7be3

--- xmloff/inc/SchXMLAutoStylePoolP.hxx
+++ xmloff/inc/SchXMLAutoStylePoolP.hxx
@@ -16,8 +16,8 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
-#ifndef INCLUDED_XMLOFF_SOURCE_CHART_SCHXMLAUTOSTYLEPOOLP_HXX
-#define INCLUDED_XMLOFF_SOURCE_CHART_SCHXMLAUTOSTYLEPOOLP_HXX
+#ifndef INCLUDED_XMLOFF_INC_SCHXMLAUTOSTYLEPOOLP_HXX
+#define INCLUDED_XMLOFF_INC_SCHXMLAUTOSTYLEPOOLP_HXX
 
 #include <xmloff/xmlaustp.hxx>
 
@@ -41,6 +41,6 @@ public:
     virtual ~SchXMLAutoStylePoolP() override;
 };
 
-#endif // INCLUDED_XMLOFF_SOURCE_CHART_SCHXMLAUTOSTYLEPOOLP_HXX
+#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
