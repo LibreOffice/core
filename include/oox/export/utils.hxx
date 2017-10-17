4b32d2104506715b1e0775a36190640c278dd9f6
Szymon Kłos <szymon.klos@collabora.com>
2017-10-17 22:14:38 +0200

    PPTX export: remember color schemes in theme
    Remember color scheme from loaded pptx file
    or use default values.
    
    Change-Id: Icb69c51603afc5f332c20c75e4ed5f659f4b5614
    Reviewed-on: https://gerrit.libreoffice.org/43470
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>

--- include/oox/export/utils.hxx
+++ include/oox/export/utils.hxx
@@ -25,9 +25,17 @@
 #include <sal/types.h>
 
 inline OString I32S_(sal_Int32 x) { return OString::number(x); }
+inline OString I32SHEX_(sal_Int32 x)
+{
+    OString aStr = OString::number(x, 16);
+    while (aStr.getLength() < 6)
+        aStr = OString("0") + aStr;
+    return aStr.getStr();
+}
 inline OString I64S_(sal_Int64 x) { return OString::number(x); }
 #define I32S(x) I32S_(x).getStr()
 #define I64S(x) I64S_(x).getStr()
+#define I32SHEX(x) I32SHEX_(x).getStr()
 #define IS(x) OString::number( x ).getStr()
 #define BS(x) (x ? "1":"0")
 #define USS(x) OUStringToOString( x, RTL_TEXTENCODING_UTF8 ).getStr()
