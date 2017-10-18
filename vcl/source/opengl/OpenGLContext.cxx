836c4e629f5c386256a2d4c38deb6f395e71aab1
Michael Meeks <michael.meeks@collabora.com>
2017-10-18 15:24:44 +0100

    tdf#113225 - reset framebuffer count when resetting OpenGLContext
    Otherwise we can fail to allocate a new one when we need it post reset.
    
    http://crashreport.libreoffice.org/stats/crash_details/e4f26191-15d5-441a-868f-9ada21ef4424
    
    Change-Id: Icc5ec6c51338e18a0a1bc890f56670e8fe9c73ea

--- vcl/source/opengl/OpenGLContext.cxx
+++ vcl/source/opengl/OpenGLContext.cxx
@@ -404,6 +404,7 @@ void OpenGLContext::reset()
             delete pFramebuffer;
             pFramebuffer = pPrevFramebuffer;
         }
+        mnFramebufferCount = 0;
         mpFirstFramebuffer = nullptr;
         mpLastFramebuffer = nullptr;
     }
