4200896e36f259a60b3d3288e13ff08d2fda861c
Jens Carl <j.carl43@gmx.de>
2017-10-20 03:27:37 +0000

    tdf#45904: Move Java _XSheetLinkable tests to C++
    Change-Id: I0f99c3b4a74748d7f1c73ef584170ae84b08cd9a
    Reviewed-on: https://gerrit.libreoffice.org/43582
    Reviewed-by: Markus Mohrhard <markus.mohrhard@googlemail.com>
    Tested-by: Markus Mohrhard <markus.mohrhard@googlemail.com>

--- qadevOOo/Jar_OOoRunner.mk
+++ qadevOOo/Jar_OOoRunner.mk
@@ -627,7 +627,6 @@ $(eval $(call gb_Jar_add_sourcefiles,OOoRunner,\
     qadevOOo/tests/java/ifc/sheet/_XSheetFilterable \
     qadevOOo/tests/java/ifc/sheet/_XSheetFilterableEx \
     qadevOOo/tests/java/ifc/sheet/_XSheetFilterDescriptor \
-    qadevOOo/tests/java/ifc/sheet/_XSheetLinkable \
     qadevOOo/tests/java/ifc/sheet/_XSpreadsheetView \
     qadevOOo/tests/java/ifc/style/_CharacterProperties \
     qadevOOo/tests/java/ifc/style/_CharacterPropertiesAsian \
