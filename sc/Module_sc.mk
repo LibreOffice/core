ba93d6aeb73cba64f037a0bd88d96fedb90f3f2f
Jens Carl <j.carl43@gmx.de>
2017-10-20 22:39:21 +0000

    tdf#45904: Move Java _XSheetFilterDescriptor tests to C++
    Change-Id: If9bbef199f2c698f9ffeb3d57423b66ebd439a94
    Reviewed-on: https://gerrit.libreoffice.org/43635
    Reviewed-by: Markus Mohrhard <markus.mohrhard@googlemail.com>
    Tested-by: Markus Mohrhard <markus.mohrhard@googlemail.com>

--- sc/Module_sc.mk
+++ sc/Module_sc.mk
@@ -133,6 +133,7 @@ $(eval $(call gb_Module_add_subsequentcheck_targets,sc,\
 	CppunitTest_sc_subtotalfieldobj \
 	CppunitTest_sc_arealinksobj \
 	CppunitTest_sc_cellrangesobj \
+	CppunitTest_sc_filterdescriptorbaseobj \
 ))
 
 $(eval $(call gb_Module_add_perfcheck_targets,sc,\
