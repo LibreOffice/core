f8945509e386694fce057adff20de69a333d83ce
Jan-Marek Glogowski <glogow@fbihome.de>
2017-10-21 13:50:30 +0000

    KF5 initial VCL plugin
    Something that compiles, basically just interface stubs.
    All used Svp classes don't use any cairo.
    
    Change-Id: I9a8858c930989438cc2a3f3346c01a7abc579d62

---
+++ vcl/unx/kf5/Kf5Frame.hxx
@@ -0,0 +1,86 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#pragma once
+
+#include <salframe.hxx>
+
+class Kf5Instance;
+
+class VCL_DLLPUBLIC Kf5Frame : public SalFrame
+{
+public:
+    Kf5Frame( Kf5Instance* pInstance,
+              SalFrame* pParent,
+              SalFrameStyleFlags nSalFrameStyle );
+    virtual ~Kf5Frame() override;
+
+    virtual SalGraphics*        AcquireGraphics() override;
+    virtual void                ReleaseGraphics( SalGraphics* pGraphics ) override;
+
+    virtual bool                PostEvent(ImplSVEvent* pData) override;
+
+    virtual void                SetTitle( const OUString& rTitle ) override;
+    virtual void                SetIcon( sal_uInt16 nIcon ) override;
+    virtual void                SetMenu( SalMenu* pMenu ) override;
+    virtual void                DrawMenuBar() override;
+
+    virtual void                SetExtendedFrameStyle( SalExtStyle nExtStyle ) override;
+    virtual void                Show( bool bVisible, bool bNoActivate = false ) override;
+    virtual void                SetMinClientSize( long nWidth, long nHeight ) override;
+    virtual void                SetMaxClientSize( long nWidth, long nHeight ) override;
+    virtual void                SetPosSize( long nX, long nY, long nWidth, long nHeight, sal_uInt16 nFlags ) override;
+    virtual void                GetClientSize( long& rWidth, long& rHeight ) override;
+    virtual void                GetWorkArea( tools::Rectangle& rRect ) override;
+    virtual SalFrame*           GetParent() const override;
+    virtual void                SetWindowState( const SalFrameState* pState ) override;
+    virtual bool                GetWindowState( SalFrameState* pState ) override;
+    virtual void                ShowFullScreen( bool bFullScreen, sal_Int32 nDisplay ) override;
+    virtual void                StartPresentation( bool bStart ) override;
+    virtual void                SetAlwaysOnTop( bool bOnTop ) override;
+    virtual void                ToTop( SalFrameToTop nFlags ) override;
+    virtual void                SetPointer( PointerStyle ePointerStyle ) override;
+    virtual void                CaptureMouse( bool bMouse ) override;
+    virtual void                SetPointerPos( long nX, long nY ) override;
+    virtual void                Flush() override;
+    virtual void                Flush( const tools::Rectangle& rRect ) override;
+    virtual void                SetInputContext( SalInputContext* pContext ) override;
+    virtual void                EndExtTextInput( EndExtTextInputFlags nFlags ) override;
+    virtual OUString            GetKeyName( sal_uInt16 nKeyCode ) override;
+    virtual bool                MapUnicodeToKeyCode( sal_Unicode aUnicode, LanguageType aLangType, vcl::KeyCode& rKeyCode ) override;
+    virtual LanguageType        GetInputLanguage() override;
+    virtual void                UpdateSettings( AllSettings& rSettings ) override;
+    virtual void                Beep() override;
+    virtual const SystemEnvData* GetSystemData() const override;
+    virtual SalPointerState     GetPointerState() override;
+    virtual KeyIndicatorState   GetIndicatorState() override;
+    virtual void                SimulateKeyPress( sal_uInt16 nKeyCode ) override;
+    virtual void                SetParent( SalFrame* pNewParent ) override;
+    virtual bool                SetPluginParent( SystemParentData* pNewParent ) override;
+    virtual void                ResetClipRegion() override;
+    virtual void                BeginSetClipRegion( sal_uLong nRects ) override;
+    virtual void                UnionClipRegion( long nX, long nY, long nWidth, long nHeight ) override;
+    virtual void                EndSetClipRegion() override;
+
+    virtual void                SetScreenNumber( unsigned int ) override;
+    virtual void                SetApplicationID(const OUString &) override;
+
+};
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
---
+++ vcl/unx/kf5/Kf5Instance.hxx
@@ -0,0 +1,75 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#pragma once
+
+#include <unx/geninst.h>
+
+class SalYieldMutex;
+class SalFrame;
+
+class Kf5Instance : public SalGenericInstance
+{
+public:
+    explicit Kf5Instance( SalYieldMutex* pMutex );
+    virtual ~Kf5Instance() override;
+
+    virtual SalFrame*          CreateFrame( SalFrame* pParent, SalFrameStyleFlags nStyle ) override;
+    virtual SalFrame*          CreateChildFrame( SystemParentData* pParent, SalFrameStyleFlags nStyle ) override;
+    virtual void               DestroyFrame( SalFrame* pFrame ) override;
+
+    virtual SalObject*         CreateObject( SalFrame* pParent, SystemWindowData* pWindowData, bool bShow ) override;
+    virtual void               DestroyObject( SalObject* pObject ) override;
+
+    virtual SalVirtualDevice*  CreateVirtualDevice( SalGraphics* pGraphics, long &nDX, long &nDY,
+                                   DeviceFormat eFormat, const SystemGraphicsData *pData = nullptr ) override;
+
+    virtual SalInfoPrinter* CreateInfoPrinter( SalPrinterQueueInfo* pQueueInfo,
+                                               ImplJobSetup* pSetupData ) override;
+    virtual void            DestroyInfoPrinter( SalInfoPrinter* pPrinter ) override;
+    virtual SalPrinter*     CreatePrinter( SalInfoPrinter* pInfoPrinter ) override;
+    virtual void            DestroyPrinter( SalPrinter* pPrinter ) override;
+    virtual void            GetPrinterQueueInfo( ImplPrnQueueList* pList ) override;
+    virtual void            GetPrinterQueueState( SalPrinterQueueInfo* pInfo ) override;
+    virtual void            DeletePrinterQueueInfo( SalPrinterQueueInfo* pInfo ) override;
+    virtual OUString        GetDefaultPrinter() override;
+    virtual void            PostPrintersChanged() override;
+
+    virtual SalTimer*       CreateSalTimer() override;
+    virtual SalSystem*      CreateSalSystem() override;
+    virtual SalBitmap*      CreateSalBitmap() override;
+
+    virtual bool            DoYield(bool bWait, bool bHandleAllCurrentEvents) override;
+    virtual bool            AnyInput( VclInputFlags nType ) override;
+
+    virtual SalSession*     CreateSalSession() override;
+
+    virtual OpenGLContext*  CreateOpenGLContext() override;
+
+    virtual OUString        GetConnectionIdentifier() override;
+
+    virtual void            AddToRecentDocumentList( const OUString& rFileUrl, const OUString& rMimeType,
+                                                     const OUString& rDocumentService ) override;
+
+    virtual GenPspGraphics *CreatePrintGraphics() override;
+
+    virtual bool            IsMainThread() const override;
+};
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
