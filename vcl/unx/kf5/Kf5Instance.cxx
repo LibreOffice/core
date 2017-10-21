7cf778c4f37b5eecfc11db5d5d7e70cd8e66c748
Jan-Marek Glogowski <glogow@fbihome.de>
2017-10-21 14:24:52 +0000

    KF5 add event processing code
    Change-Id: Ida7c4858cb770d1ce2d05aa6adc332c89bddd795

--- vcl/unx/kf5/Kf5Instance.cxx
+++ vcl/unx/kf5/Kf5Instance.cxx
@@ -17,11 +17,13 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#include <QtCore/QMetaType>
+#include "Kf5Instance.hxx"
+#include <Kf5Instance.moc>
+
 #include <QtCore/QThread>
 #include <QtWidgets/QApplication>
+#include <QtCore/QAbstractEventDispatcher>
 
-#include "Kf5Instance.hxx"
 #include "Kf5Frame.hxx"
 #include "Kf5Data.hxx"
 #include "Kf5Timer.hxx"
@@ -35,7 +37,14 @@
 
 Kf5Instance::Kf5Instance( SalYieldMutex* pMutex )
     : SalGenericInstance( pMutex )
+    , m_postUserEventId( -1 )
 {
+    m_postUserEventId = QEvent::registerEventType();
+
+    // this one needs to be blocking, so that the handling in main thread
+    // is processed before the thread emitting the signal continues
+    connect( this, SIGNAL( ImplYieldSignal( bool, bool )),
+             this, SLOT( ImplYield( bool, bool )), Qt::BlockingQueuedConnection );
 }
 
 Kf5Instance::~Kf5Instance()
@@ -92,9 +101,48 @@ SalBitmap* Kf5Instance::CreateSalBitmap()
     return new SvpSalBitmap();
 }
 
+bool Kf5Instance::ImplYield( bool bWait, bool bHandleAllCurrentEvents )
+{
+    bool wasEvent = DispatchUserEvents( bHandleAllCurrentEvents );
+    if ( !bHandleAllCurrentEvents && wasEvent )
+        return true;
+
+    /**
+     * Quoting the Qt docs: [QAbstractEventDispatcher::processEvents] processes
+     * pending events that match flags until there are no more events to process.
+     */
+    QAbstractEventDispatcher* dispatcher = QAbstractEventDispatcher::instance( qApp->thread());
+    if ( bWait && !wasEvent )
+        wasEvent = dispatcher->processEvents( QEventLoop::WaitForMoreEvents );
+    else
+        wasEvent = dispatcher->processEvents( QEventLoop::AllEvents ) || wasEvent;
+    return wasEvent;
+}
+
 bool Kf5Instance::DoYield(bool bWait, bool bHandleAllCurrentEvents)
 {
-    return false;
+    bool bWasEvent = false;
+    if( qApp->thread() == QThread::currentThread() )
+    {
+        bWasEvent = ImplYield( bWait, bHandleAllCurrentEvents );
+        if ( bWasEvent )
+            m_aWaitingYieldCond.set();
+    }
+    else
+    {
+        {
+            SolarMutexReleaser aReleaser;
+            bWasEvent = Q_EMIT ImplYieldSignal( false, bHandleAllCurrentEvents );
+        }
+        if ( !bWasEvent && bWait )
+        {
+            m_aWaitingYieldCond.reset();
+            SolarMutexReleaser aReleaser;
+            m_aWaitingYieldCond.wait();
+            bWasEvent = true;
+        }
+    }
+    return bWasEvent;
 }
 
 bool Kf5Instance::AnyInput( VclInputFlags nType )
@@ -124,6 +172,16 @@ OpenGLContext* Kf5Instance::CreateOpenGLContext()
 bool Kf5Instance::IsMainThread() const
 {
     return qApp->thread() != QThread::currentThread();
+}
+
+void Kf5Instance::TriggerUserEventProcessing()
+{
+    QApplication::postEvent(this, new QEvent(QEvent::Type( m_postUserEventId )));
+}
+
+void Kf5Instance::ProcessEvent( SalUserEvent aEvent )
+{
+    aEvent.m_pFrame->CallCallback( aEvent.m_nEvent, aEvent.m_pData );
 }
 
 extern "C" {
