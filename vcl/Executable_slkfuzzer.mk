600039bc7bd5a4b9b0bdec4f2ce7c83f6d7445d8
Caolán McNamara <caolanm@redhat.com>
2017-10-22 20:44:08 +0100

    shrink slk fuzzer
    Change-Id: I1f9a6afaa2ae0d235eb6c40ae8b2a3b9ed929972

--- vcl/Executable_slkfuzzer.mk
+++ vcl/Executable_slkfuzzer.mk
@@ -34,7 +34,6 @@ $(eval $(call gb_Executable_use_libraries,slkfuzzer,\
 $(eval $(call gb_Executable_use_static_libraries,slkfuzzer,\
     findsofficepath \
     ulingu \
-    fuzzer_calc \
     fuzzerstubs \
 ))
 
