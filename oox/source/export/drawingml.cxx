f68c1a0f03b974839b02cbff400f1a2785489c87
Szymon Kłos <szymon.klos@collabora.com>
2017-10-23 11:20:34 +0200

    tdf#113263 Revert "PPTX export: correct position for shape in group"
    This reverts commit fd3383556881d6a9d73e1a3027c079cf9bfddecb.
    
    Change-Id: Ia9befead3b45975d91215cea584b32afadb5bd52
    Reviewed-on: https://gerrit.libreoffice.org/43711
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>

--- oox/source/export/drawingml.cxx
+++ oox/source/export/drawingml.cxx
@@ -1300,15 +1300,6 @@ void DrawingML::WriteShapeTransformation( const Reference< XShape >& rXShape, sa
         aPos.X -= aParentPos.X;
         aPos.Y -= aParentPos.Y;
     }
-    else if (m_xParent.is())
-    {
-        SdrObject* pShape = GetSdrObjectFromXShape(rXShape);
-        if (pShape)
-        {
-            aPos.X = pShape->GetRelativePos().getX();
-            aPos.Y = pShape->GetRelativePos().getY();
-        }
-    }
 
     if ( aSize.Width < 0 )
         aSize.Width = 1000;
