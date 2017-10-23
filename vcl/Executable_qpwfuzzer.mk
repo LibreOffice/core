2bdd2425664b6314e312e13679d4ccd3c093afb2
Caolán McNamara <caolanm@redhat.com>
2017-10-23 09:21:45 +0100

    shrink qpw fuzzer
    Change-Id: If7038a3e720a987aaa3575646444aaf8dad555c2

--- vcl/Executable_qpwfuzzer.mk
+++ vcl/Executable_qpwfuzzer.mk
@@ -34,7 +34,6 @@ $(eval $(call gb_Executable_use_libraries,qpwfuzzer,\
 $(eval $(call gb_Executable_use_static_libraries,qpwfuzzer,\
     findsofficepath \
     ulingu \
-    fuzzer_calc \
     fuzzerstubs \
 ))
 
