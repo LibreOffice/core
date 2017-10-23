c1e2bcca09c325b47136cb730749d39c1af99a99
Caolán McNamara <caolanm@redhat.com>
2017-10-23 13:36:44 +0100

    need some more services in calc fuzzers
    Change-Id: I8ddca8e1f12a42d70f00ade8e179179fdf567589

--- vcl/workben/qpwfuzzer.cxx
+++ vcl/workben/qpwfuzzer.cxx
@@ -23,6 +23,7 @@ void * com_sun_star_i18n_BreakIterator_get_implementation( void *, void * );
 void * com_sun_star_comp_framework_Desktop_get_implementation( void *, void * );
 void * com_sun_star_i18n_CharacterClassification_Unicode_get_implementation( void *, void * );
 void * com_sun_star_i18n_CharacterClassification_get_implementation( void *, void * );
+void * com_sun_star_i18n_Collator_get_implementation( void *, void * );
 void * com_sun_star_i18n_NativeNumberSupplier_get_implementation( void *, void * );
 void * com_sun_star_i18n_NumberFormatCodeMapper_get_implementation( void *, void * );
 void * com_sun_star_i18n_Transliteration_get_implementation( void *, void * );
@@ -50,6 +51,7 @@ lo_get_constructor_map(void)
         { "com_sun_star_comp_framework_Desktop_get_implementation", com_sun_star_comp_framework_Desktop_get_implementation },
         { "com_sun_star_i18n_CharacterClassification_Unicode_get_implementation", com_sun_star_i18n_CharacterClassification_Unicode_get_implementation },
         { "com_sun_star_i18n_CharacterClassification_get_implementation", com_sun_star_i18n_CharacterClassification_get_implementation },
+        { "com_sun_star_i18n_Collator_get_implementation", com_sun_star_i18n_Collator_get_implementation },
         { "com_sun_star_i18n_NativeNumberSupplier_get_implementation", com_sun_star_i18n_NativeNumberSupplier_get_implementation },
         { "com_sun_star_i18n_NumberFormatCodeMapper_get_implementation", com_sun_star_i18n_NumberFormatCodeMapper_get_implementation },
         { "com_sun_star_i18n_Transliteration_get_implementation", com_sun_star_i18n_Transliteration_get_implementation },
