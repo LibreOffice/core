37eeb4767b7bad61436e645007780a040720476b
jan Iversen <jani@libreoffice.org>
2017-10-25 15:10:59 +0200

    iOS, soffice not needed for iOS
    Removed the IPC part of init for iOS.
    
    Lo_init() completes for iOS without errors.
    
    Change-Id: I7ce38758883bebea1de6ac149ba8768593c7cb78

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -3280,6 +3280,7 @@ static bool initialize_uno(const OUString& aAppProgramURL)
     return true;
 }
 
+#ifndef IOS
 static void lo_startmain(void*)
 {
     osl_setThreadName("lo_startmain");
@@ -3291,6 +3292,7 @@ static void lo_startmain(void*)
 
     Application::ReleaseSolarMutex();
 }
+#endif
 
 static bool bInitialized = false;
 
@@ -3482,12 +3484,16 @@ static int lo_initialize(LibreOfficeKit* pThis, const char* pAppPath, const char
         if (eStage != PRE_INIT)
         {
             SAL_INFO("lok", "Enabling RequestHandler");
+#ifdef IOS
+            RequestHandler::Enable(true);
+#else
             RequestHandler::Enable(false);
             SAL_INFO("lok", "Starting soffice_main");
             RequestHandler::SetReady(false);
             pLib->maThread = osl_createThread(lo_startmain, nullptr);
             SAL_INFO("lok", "Waiting for RequestHandler");
             RequestHandler::WaitForReady();
+#endif
             SAL_INFO("lok", "RequestHandler ready -- continuing");
         }
 
