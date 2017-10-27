ee6dad51150fd53d19f882edfefa879e18f9897d
Katarina Behrens <Katarina.Behrens@cib.de>
2017-10-27 18:02:35 +0200

    tdf#79077: copy 'paper from printer setup' toggle
    into printer properties dialog, it is rather badly accessible
    (almost invisible) in File > Print > Options tabpage
    
    Reviewed-on: https://gerrit.libreoffice.org/43956
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Katarina Behrens <Katarina.Behrens@cib.de>
    (cherry picked from commit 75fc4934c3aafcd83d9bbee63d8d24cbddae8ea0)
    
    Conflicts:
    	vcl/uiconfig/ui/printerpaperpage.ui
    	vcl/unx/generic/print/prtsetup.hxx
    
    Change-Id: I18d538a20dc207c359ae1e7c2c914a68daf58f31

--- vcl/unx/generic/print/prtsetup.cxx
+++ vcl/unx/generic/print/prtsetup.cxx
@@ -163,6 +163,7 @@ RTSPaperPage::RTSPaperPage(RTSDialog* pParent)
     : TabPage(pParent->m_pTabControl, "PrinterPaperPage", "vcl/ui/printerpaperpage.ui")
     , m_pParent( pParent )
 {
+    get(m_pCbFromSetup, "papersizefromsetup");
     get(m_pPaperText, "paperft");
     get(m_pPaperBox, "paperlb");
     get(m_pOrientBox, "orientlb");
@@ -175,6 +176,8 @@ RTSPaperPage::RTSPaperPage(RTSDialog* pParent)
     m_pOrientBox->SetSelectHdl( LINK( this, RTSPaperPage, SelectHdl ) );
     m_pDuplexBox->SetSelectHdl( LINK( this, RTSPaperPage, SelectHdl ) );
     m_pSlotBox->SetSelectHdl( LINK( this, RTSPaperPage, SelectHdl ) );
+    m_pCbFromSetup->SetToggleHdl( LINK( this, RTSPaperPage, CheckBoxHdl ) );
+
 
     sal_Int32 nPos = 0;
 
@@ -200,6 +203,7 @@ void RTSPaperPage::dispose()
 {
     m_pParent.clear();
     m_pPaperText.clear();
+    m_pCbFromSetup.clear();
     m_pPaperBox.clear();
     m_pOrientBox.clear();
     m_pDuplexText.clear();
@@ -253,9 +257,11 @@ void RTSPaperPage::update()
         m_pSlotBox->Enable( false );
     }
 
+    if ( m_pParent->m_aJobData.m_bPapersizeFromSetup )
+        m_pCbFromSetup->Check( m_pParent->m_aJobData.m_bPapersizeFromSetup );
     // disable those, unless user wants to use papersize from printer prefs
     // as they have no influence on what's going to be printed anyway
-    if (!m_pParent->m_aJobData.m_bPapersizeFromSetup)
+    else
     {
         m_pPaperBox->Enable( false );
         m_pOrientBox->Enable( false );
@@ -294,6 +300,14 @@ IMPL_LINK_TYPED( RTSPaperPage, SelectHdl, ListBox&, rBox, void )
     m_pParent->SetDataModified( true );
 }
 
+IMPL_LINK_TYPED( RTSPaperPage, CheckBoxHdl, CheckBox&, /*cBox*/, void )
+{
+    bool bFromSetup = m_pCbFromSetup->IsChecked();
+    m_pParent->m_aJobData.m_bPapersizeFromSetup = bFromSetup;
+    m_pPaperBox->Enable( bFromSetup );
+    m_pOrientBox->Enable( bFromSetup );
+    m_pParent->SetDataModified( true );
+}
 /*
  * RTSDevicePage
  */
