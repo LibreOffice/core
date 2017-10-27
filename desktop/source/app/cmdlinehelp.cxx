fa6d8eebd1574213670dbedc79441d1a1118cd86
Stephan Bergmann <sbergman@redhat.com>
2017-10-27 19:27:29 +0200

    loplugin:includeform: desktop (Windows)
    Change-Id: I245c58a3d97eb0d60e7063e9c34c77d33739801d

--- desktop/source/app/cmdlinehelp.cxx
+++ desktop/source/app/cmdlinehelp.cxx
@@ -30,9 +30,9 @@
 #if !defined WIN32_LEAN_AND_MEAN
 # define WIN32_LEAN_AND_MEAN
 #endif
-#include "windows.h"
-#include "io.h"
-#include "fcntl.h"
+#include <windows.h>
+#include <io.h>
+#include <fcntl.h>
 #endif
 
 namespace desktop
