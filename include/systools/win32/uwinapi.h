6092fd425fe68590bffb49c0390e4ecabf9d9fbd
Stephan Bergmann <sbergman@redhat.com>
2017-10-27 19:25:16 +0200

    loplugin:includeform: systools (Windows)
    Change-Id: If0eee76e3f2844718718906da403e3c09a4688f8

--- include/systools/win32/uwinapi.h
+++ include/systools/win32/uwinapi.h
@@ -20,7 +20,7 @@
 #ifndef INCLUDED_SYSTOOLS_WIN32_UWINAPI_H
 #define INCLUDED_SYSTOOLS_WIN32_UWINAPI_H
 
-#include <sal/macros.h>
+#include "sal/macros.h"
 
 #include "prewin.h"
 #include "postwin.h"
