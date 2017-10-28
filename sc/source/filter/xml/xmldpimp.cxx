d5c7c8d8bde74e89db18212ccd48f972f1b49e3b
Tamás Zolnai <tamas.zolnai@collabora.com>
2017-10-28 08:02:10 +0200

    tdf#113268: Pivot table: Missing popup button after opening a ODS
    Wrong row number was calculated here. This ++nCurRow is usefull
    only when headerlayout flag is set. It's a MSO compatibility flag
    so it's not there by default in LO created tables.
    
    Reviewed-on: https://gerrit.libreoffice.org/43552
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Tamás Zolnai <tamas.zolnai@collabora.com>
    (cherry picked from commit 291874c09c4bcde61f2c6808f2df6079f38d74e0)
    
    Change-Id: Id7989d898f2647f1ba45ed95e0aa615e3b4fa311
    Reviewed-on: https://gerrit.libreoffice.org/43974
    Reviewed-by: Andras Timar <andras.timar@collabora.com>
    Tested-by: Andras Timar <andras.timar@collabora.com>

--- sc/source/filter/xml/xmldpimp.cxx
+++ sc/source/filter/xml/xmldpimp.cxx
@@ -345,6 +345,7 @@ void ScXMLDataPilotTableContext::SetButtons()
     aGeometry.setPageFieldCount(mnPageFieldCount);
     aGeometry.setDataFieldCount(mnDataFieldCount);
     aGeometry.setDataLayoutType(toFieldType(mnDataLayoutType));
+    aGeometry.setHeaderLayout(bHeaderGridLayout);
 
     std::vector<const ScDPSaveDimension*> aRowDims, aColDims, aPageDims;
     pDPSave->GetAllDimensionsByOrientation(sheet::DataPilotFieldOrientation_ROW, aRowDims);
