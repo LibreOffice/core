2437c5daf5620cf63b4dc40cc696805aff782509
David Tardon <dtardon@redhat.com>
2017-10-28 10:56:01 +0200

    libe-book: pass optimization flags to configure
    Change-Id: I3d2833eccacc11d6183c0c13ca18a8d9deab5e02
    Reviewed-on: https://gerrit.libreoffice.org/43981
    Reviewed-by: David Tardon <dtardon@redhat.com>
    Tested-by: David Tardon <dtardon@redhat.com>

--- external/libebook/ExternalProject_libebook.mk
+++ external/libebook/ExternalProject_libebook.mk
@@ -38,7 +38,8 @@ $(call gb_ExternalProject_get_state_target,libebook,build) :
 			$(if $(verbose),--disable-silent-rules,--enable-silent-rules) \
 			--disable-werror \
 			--disable-weffc \
-			CXXFLAGS="$(CXXFLAGS) $(ICU_UCHAR_TYPE) $(BOOST_CPPFLAGS) \
+			CXXFLAGS="$(gb_CXXFLAGS) $(if $(ENABLE_OPTIMIZED),$(gb_COMPILEROPTFLAGS),$(gb_COMPILERNOOPTFLAGS))" \
+			CPPFLAGS="$(CPPFLAGS) $(ICU_UCHAR_TYPE) $(BOOST_CPPFLAGS) \
 				-DBOOST_ERROR_CODE_HEADER_ONLY -DBOOST_SYSTEM_NO_DEPRECATED" \
 			XML_CFLAGS="$(LIBXML_CFLAGS)" \
 			XML_LIBS="$(LIBXML_LIBS)" \
