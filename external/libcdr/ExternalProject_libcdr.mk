93e44b9a099f87d26cc445aa76e4e279082cafb3
David Tardon <dtardon@redhat.com>
2017-10-28 10:50:37 +0200

    libcdr: pass optimization flags to configure
    Change-Id: Ic852290149a018d272ff727ab5f504825b16cbba

--- external/libcdr/ExternalProject_libcdr.mk
+++ external/libcdr/ExternalProject_libcdr.mk
@@ -36,7 +36,8 @@ $(call gb_ExternalProject_get_state_target,libcdr,build) :
 			--disable-werror \
 			--disable-weffc \
 			$(if $(verbose),--disable-silent-rules,--enable-silent-rules) \
-			CXXFLAGS="$(CXXFLAGS) $(ICU_UCHAR_TYPE) $(BOOST_CPPFLAGS)" \
+			CXXFLAGS="$(gb_CXXFLAGS) $(if $(ENABLE_OPTIMIZED),$(gb_COMPILEROPTFLAGS),$(gb_COMPILERNOOPTFLAGS))" \
+			CPPFLAGS="$(CPPFLAGS) $(ICU_UCHAR_TYPE) $(BOOST_CPPFLAGS)" \
 			$(if $(CROSS_COMPILING),--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM)) \
 		&& $(MAKE) \
 	)
