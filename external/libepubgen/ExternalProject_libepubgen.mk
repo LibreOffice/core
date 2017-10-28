e6af915fae27a19b6ac79a9f76fb8b04361d6f4b
David Tardon <dtardon@redhat.com>
2017-10-28 11:00:17 +0200

    libepubgen: pass optimization flags to configure
    Change-Id: I50e0c7bfe89249c421b8e03f57445ce1bf51ecb4
    Reviewed-on: https://gerrit.libreoffice.org/43982
    Reviewed-by: David Tardon <dtardon@redhat.com>
    Tested-by: David Tardon <dtardon@redhat.com>

--- external/libepubgen/ExternalProject_libepubgen.mk
+++ external/libepubgen/ExternalProject_libepubgen.mk
@@ -31,7 +31,8 @@ $(call gb_ExternalProject_get_state_target,libepubgen,build) :
 			$(if $(ENABLE_DEBUG),--enable-debug,--disable-debug) \
 			--disable-werror \
 			$(if $(verbose),--disable-silent-rules,--enable-silent-rules) \
-			CXXFLAGS="$(CXXFLAGS) $(BOOST_CPPFLAGS)" \
+			CXXFLAGS="$(gb_CXXFLAGS) $(if $(ENABLE_OPTIMIZED),$(gb_COMPILEROPTFLAGS),$(gb_COMPILERNOOPTFLAGS))" \
+			CPPFLAGS="$(CPPFLAGS) $(BOOST_CPPFLAGS)" \
 			$(if $(CROSS_COMPILING),--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM)) \
 		&& $(MAKE) \
 	)
