e7e8dab5ab0c0b5016cc57b0719a5d984859d2cb
Jan-Marek Glogowski <glogow@fbihome.de>
2017-10-30 20:22:56 +0100

    QT5 unify Graphics constructors
    Change-Id: I85ce73e0e79927fa9233230bc4a9134db4c513dc

--- vcl/qt5/Qt5Graphics.cxx
+++ vcl/qt5/Qt5Graphics.cxx
@@ -26,19 +26,12 @@
 
 #include <QtGui/QImage>
 
-Qt5Graphics::Qt5Graphics( Qt5Frame *pFrame )
+Qt5Graphics::Qt5Graphics( Qt5Frame *pFrame, QImage *pQImage )
     : m_pFrame( pFrame )
-    , m_pQImage( nullptr )
-    , m_pFontCollection( nullptr )
-    , m_pFontData{ nullptr, }
-    , m_pTextStyle{ nullptr, }
-    , m_aTextColor( MAKE_SALCOLOR(0x00, 0x00, 0x00) )
-{
-}
-
-Qt5Graphics::Qt5Graphics( QImage *pQImage )
-    : m_pFrame( nullptr )
     , m_pQImage( pQImage )
+    , m_aLineColor( MAKE_SALCOLOR(0x00, 0x00, 0x00) )
+    , m_aFillColor( MAKE_SALCOLOR(0xFF, 0xFF, 0XFF) )
+    , m_eCompositionMode( QPainter::CompositionMode_SourceOver )
     , m_pFontCollection( nullptr )
     , m_pFontData{ nullptr, }
     , m_pTextStyle{ nullptr, }
