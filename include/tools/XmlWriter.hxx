56b60744ae8dd97d93cb3d2b9ecf5d106c38dfc9
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2017-11-01 12:46:35 +0900

    Extract XmlWriter and XmlWalker from opencl into tools
    In opencl we read and writer the profile xml with custom classes
    XmlWriter and XmlWalker for reading. This classes are useful in
    other places (very similar XmlWriter is used in test), so extract
    the code from opencl and move it to a more common place - tools.
    
    Refactoring of other usages will follow.
    
    Change-Id: I8363e87b7c30083d299080adec3f99cb33ebe4cc

---
+++ include/tools/XmlWriter.hxx
@@ -0,0 +1,58 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef INCLUDED_TOOLS_XMLWRITER_HXX
+#define INCLUDED_TOOLS_XMLWRITER_HXX
+
+#include <tools/toolsdllapi.h>
+#include <tools/stream.hxx>
+#include <memory>
+
+namespace tools {
+
+struct XmlWriterImpl;
+
+/**
+ * XmlWriter writes a XML to a SvStream. It uses libxml2 for writing but hides
+ * all the internal libxml2 workings and uses types that are native for LO
+ * development.
+ *
+ * The codepage used for XML is always "utf-8" and the output is indented so it
+ * is easier to read.
+ *
+ */
+class TOOLS_DLLPUBLIC XmlWriter final
+{
+private:
+    std::unique_ptr<XmlWriterImpl> mpImpl;
+public:
+
+    XmlWriter(SvStream* pStream);
+
+    ~XmlWriter();
+
+    bool startDocument();
+    void endDocument();
+
+    void startElement(const OString& sName);
+    void endElement();
+
+    void attribute(const OString& sTagName, const OString& aValue);
+    void attribute(const OString& sTagName, const OUString& aValue);
+    void attribute(const OString& sTagName, sal_Int32 aNumber);
+
+    void content(const OString& sValue);
+    void content(const OUString& sValue);
+};
+
+} // end tools namespace
+
+#endif // INCLUDED_TOOLS_XMLWRITER_HXX
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
---
+++ tools/source/xml/XmlWalker.cxx
@@ -0,0 +1,112 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <tools/XmlWalker.hxx>
+#include <o3tl/make_unique.hxx>
+
+#include <libxml/tree.h>
+#include <libxml/parser.h>
+#include <libxml/xmlstring.h>
+#include <vector>
+
+namespace tools {
+
+struct XmlWalkerImpl
+{
+    XmlWalkerImpl()
+        : mpDocPtr(nullptr)
+        , mpRoot(nullptr)
+        , mpCurrent(nullptr)
+    {}
+
+    xmlDocPtr  mpDocPtr;
+    xmlNodePtr mpRoot;
+    xmlNodePtr mpCurrent;
+
+    std::vector<xmlNodePtr> mpStack;
+};
+
+XmlWalker::XmlWalker()
+    : mpImpl(o3tl::make_unique<XmlWalkerImpl>())
+{}
+
+XmlWalker::~XmlWalker()
+{
+    if (mpImpl)
+        xmlFreeDoc(mpImpl->mpDocPtr);
+}
+
+bool XmlWalker::open(SvStream* pStream)
+{
+    std::size_t nSize = pStream->remainingSize();
+    std::vector<sal_uInt8> aBuffer(nSize + 1);
+    pStream->ReadBytes(aBuffer.data(), nSize);
+    aBuffer[nSize] = 0;
+    mpImpl->mpDocPtr = xmlParseDoc(reinterpret_cast<xmlChar*>(aBuffer.data()));
+    if (mpImpl->mpDocPtr == nullptr)
+        return false;
+    mpImpl->mpRoot = xmlDocGetRootElement(mpImpl->mpDocPtr);
+    mpImpl->mpCurrent = mpImpl->mpRoot;
+    mpImpl->mpStack.push_back(mpImpl->mpCurrent);
+    return true;
+}
+
+OString XmlWalker::name()
+{
+    return OString(reinterpret_cast<const char*>(mpImpl->mpCurrent->name));
+}
+
+OString XmlWalker::content()
+{
+    OString aContent;
+    if (mpImpl->mpCurrent->xmlChildrenNode != nullptr)
+    {
+        xmlChar* pContent = xmlNodeListGetString(mpImpl->mpDocPtr, mpImpl->mpCurrent->xmlChildrenNode, 1);
+        aContent = OString(reinterpret_cast<const char*>(pContent));
+        xmlFree(pContent);
+    }
+    return aContent;
+}
+
+void XmlWalker::children()
+{
+    mpImpl->mpStack.push_back(mpImpl->mpCurrent);
+    mpImpl->mpCurrent = mpImpl->mpCurrent->xmlChildrenNode;
+}
+
+void XmlWalker::parent()
+{
+    mpImpl->mpCurrent = mpImpl->mpStack.back();
+    mpImpl->mpStack.pop_back();
+}
+
+OString XmlWalker::attribute(OString sName)
+{
+    xmlChar* xmlName = xmlCharStrdup(sName.getStr());
+    xmlChar* xmlAttribute = xmlGetProp(mpImpl->mpCurrent, xmlName);
+    OString aAttributeContent = OString(reinterpret_cast<const char*>(xmlAttribute));
+    xmlFree(xmlAttribute);
+    xmlFree(xmlName);
+
+    return aAttributeContent;
+}
+
+void XmlWalker::next()
+{
+    mpImpl->mpCurrent = mpImpl->mpCurrent->next;
+}
+
+bool XmlWalker::isValid() const
+{
+    return mpImpl->mpCurrent != nullptr;
+}
+
+} // end tools namespace
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
