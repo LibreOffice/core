27cff3630307b7f217df5af915a8c151412fb803
David Tardon <dtardon@redhat.com>
2017-11-01 19:23:37 +0100

    ICU_UCHAR_TYPE is not needed for C++11 projects
    Change-Id: I089af1a67044ab9cfd767e767a15d1a66014ce2f

--- external/libcdr/ExternalProject_libcdr.mk
+++ external/libcdr/ExternalProject_libcdr.mk
@@ -37,7 +37,7 @@ $(call gb_ExternalProject_get_state_target,libcdr,build) :
 			--disable-weffc \
 			$(if $(verbose),--disable-silent-rules,--enable-silent-rules) \
 			CXXFLAGS="$(gb_CXXFLAGS) $(if $(ENABLE_OPTIMIZED),$(gb_COMPILEROPTFLAGS),$(gb_COMPILERNOOPTFLAGS))" \
-			CPPFLAGS="$(CPPFLAGS) $(ICU_UCHAR_TYPE) $(BOOST_CPPFLAGS)" \
+			CPPFLAGS="$(CPPFLAGS) $(BOOST_CPPFLAGS)" \
 			$(if $(CROSS_COMPILING),--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM)) \
 		&& $(MAKE) \
 	)
