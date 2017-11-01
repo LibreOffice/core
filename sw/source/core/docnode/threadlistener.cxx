b085d3561a84d2590ec52bec4a020566955c9689
Stephan Bergmann <sbergman@redhat.com>
2017-11-01 18:13:54 +0100

    threadmanager.hxx is only included from sw/source/core/docnode
    ...but itself includes sw/source/core/docnode/cancellablejob.hxx, which only
    happens to work because of -I$(dir $(3)) in gb_CObject__command_pattern (in
    solenv/gbuild/platform/com_{GCC,MSC}_class.mk).  Clean that up by moving the
    file.
    
    Change-Id: I69dfb80460edc66309cdb6f08427a47cc2420f34
    Reviewed-on: https://gerrit.libreoffice.org/44173
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

--- sw/source/core/docnode/threadlistener.cxx
+++ sw/source/core/docnode/threadlistener.cxx
@@ -17,7 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 #include <threadlistener.hxx>
-#include <threadmanager.hxx>
+#include "threadmanager.hxx"
 
 /** helper class to observe threads
 
