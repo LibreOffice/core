LibreOffice/core

commit e57a5905fb2975307af654710430d0a876dbd061

Use mdds' event callback to count formula blocks in each column.
And use it to speed up certain formula related operations.

Change-Id: I43b1d860d6b665556624ba7bc716826799919015
Reviewed-on: https://gerrit.libreoffice.org/27261
Reviewed-by: Kohei Yoshida <libreoffice@kohei.us>
Tested-by: Kohei Yoshida <libreoffice@kohei.us>

sc/inc/mtvelements.hxx
@@ -35,6 +35,7 @@
 #include <unordered_map>
 
 class ScDocument;
+class ScColumn;
 struct ScRefCellValue;
 
 namespace sc {
@@ -86,6 +87,17 @@ MDDS_MTV_DEFINE_ELEMENT_CALLBACKS(SharedString, sc::element_type_string, SharedS
 
 namespace sc {
 
+class CellStoreEvent
+{
+    ScColumn* mpCol;
+public:
+    CellStoreEvent();
+    CellStoreEvent(ScColumn* pCol);
+
+    void element_block_acquired(const mdds::mtv::base_element_block* block);
+    void element_block_released(const mdds::mtv::base_element_block* block);
+};
+
 /// Cell note container
 typedef mdds::mtv::custom_block_func1<sc::cellnote_block> CNoteFunc;
 typedef mdds::multi_type_vector<CNoteFunc> CellNoteStoreType;
@@ -100,7 +112,7 @@ typedef mdds::multi_type_vector<CTAttrFunc> CellTextAttrStoreType;
 
 /// Cell container
 typedef mdds::mtv::custom_block_func3<sc::string_block, sc::edittext_block, sc::formula_block> CellFunc;
-typedef mdds::multi_type_vector<CellFunc> CellStoreType;
+typedef mdds::multi_type_vector<CellFunc, CellStoreEvent> CellStoreType;
 
 /**
  * Store position data for column array storage.

sc/source/core/data/column.cxx
@@ -84,13 +84,16 @@ ScColumn::ScColumn() :
     maCellTextAttrs(MAXROWCOUNT),
     maCellNotes(MAXROWCOUNT),
     maBroadcasters(MAXROWCOUNT),
-    maCells(MAXROWCOUNT),
+    maCellsEvent(this),
+    maCells(maCellsEvent),
     nCol( 0 ),
     nTab( 0 ),
     pAttrArray( nullptr ),
     pDocument( nullptr ),
-    mbDirtyGroups(true)
+    mbDirtyGroups(true),
+    mnBlkCountFormula(0)
 {
+    maCells.resize(MAXROWCOUNT);
 }
 
 ScColumn::~ScColumn()

sc/source/core/data/mtvelements.cxx
@@ -11,8 +11,43 @@
 #include "globalnames.hxx"
 #include "document.hxx"
 #include "cellvalue.hxx"
+#include "column.hxx"
 
 namespace sc {
+
+CellStoreEvent::CellStoreEvent() : mpCol(nullptr) {}
+
+CellStoreEvent::CellStoreEvent(ScColumn* pCol) : mpCol(pCol) {}
+
+void CellStoreEvent::element_block_acquired(const mdds::mtv::base_element_block* block)
+{
+    if (!mpCol)
+        return;
+
+    switch (mdds::mtv::get_block_type(*block))
+    {
+        case sc::element_type_formula:
+            ++mpCol->mnBlkCountFormula;
+            break;
+        default:
+            ;
+    }
+}
+
+void CellStoreEvent::element_block_released(const mdds::mtv::base_element_block* block)
+{
+    if (!mpCol)
+        return;
+
+    switch (mdds::mtv::get_block_type(*block))
+    {
+        case sc::element_type_formula:
+            --mpCol->mnBlkCountFormula;
+            break;
+        default:
+            ;
+    }
+}
 
 ColumnBlockPositionSet::ColumnBlockPositionSet(ScDocument& rDoc) : mrDoc(rDoc) {}
 

sc/inc/column.hxx
@@ -130,6 +130,8 @@ class ScColumn
     // Broadcasters for formula cells.
     sc::BroadcasterStoreType maBroadcasters;
 
+    sc::CellStoreEvent maCellsEvent;
+
     // Cell values.
     sc::CellStoreType maCells;
 
@@ -139,6 +141,8 @@ class ScColumn
     ScAttrArray*          pAttrArray;
     ScDocument*           pDocument;
     bool mbDirtyGroups;     /// formula groups are dirty.
+
+    size_t mnBlkCountFormula;
 
 friend class ScDocument;                    // for FillInfo
 friend class ScTable;
@@ -158,6 +162,7 @@ friend class sc::ColumnSpanSet;
 friend class sc::EditTextIterator;
 friend class sc::CellValues;
 friend class sc::TableValues;
+friend class sc::CellStoreEvent;
 
     ScColumn(const ScColumn&) = delete;
     ScColumn& operator= (const ScColumn&) = delete;
@@ -339,6 +344,7 @@ public:
 
     bool SetFormulaCells( SCROW nRow, std::vector<ScFormulaCell*>& rCells );
 
+    bool HasFormulaCell() const;
     bool HasFormulaCell( SCROW nRow1, SCROW nRow2 ) const;
 
     void CloneFormulaCell(

sc/source/core/data/column4.cxx
@@ -1142,6 +1142,11 @@ void ScColumn::CollectFormulaCells( std::vector<ScFormulaCell*>& rCells, SCROW n
     sc::ProcessFormula(maCells.begin(), maCells, nRow1, nRow2, aFunc);
 }
 
+bool ScColumn::HasFormulaCell() const
+{
+    return mnBlkCountFormula != 0;
+}
+
 namespace {
 
 struct FindAnyFormula
@@ -1156,8 +1161,14 @@ struct FindAnyFormula
 
 bool ScColumn::HasFormulaCell( SCROW nRow1, SCROW nRow2 ) const
 {
+    if (!mnBlkCountFormula)
+        return false;
+
     if (nRow2 < nRow1 || !ValidRow(nRow1) || !ValidRow(nRow2))
         return false;
+
+    if (nRow1 == 0 && nRow2 == MAXROW)
+        return HasFormulaCell();
 
     FindAnyFormula aFunc;
     std::pair<sc::CellStoreType::const_iterator, size_t> aRet =
@@ -1344,6 +1355,9 @@ void ScColumn::StartListeningFormulaCells(
     sc::StartListeningContext& rStartCxt, sc::EndListeningContext& rEndCxt,
     SCROW nRow1, SCROW nRow2 )
 {
+    if (!HasFormulaCell())
+        return;
+
     StartListeningFormulaCellsHandler aFunc(rStartCxt, rEndCxt);
     sc::ProcessBlock(maCells.begin(), maCells, aFunc, nRow1, nRow2);
 }
@@ -1352,6 +1366,9 @@ void ScColumn::EndListeningFormulaCells(
     sc::EndListeningContext& rCxt, SCROW nRow1, SCROW nRow2,
     SCROW* pStartRow, SCROW* pEndRow )
 {
+    if (!HasFormulaCell())
+        return;
+
     EndListeningFormulaCellsHandler aFunc(rCxt);
     sc::ProcessBlock(maCells.begin(), maCells, aFunc, nRow1, nRow2);
 

commit 423df1fa929784c14e3a133c06468589fe9269cd

Resolves: tdf#111428 implement CellStoreEvent::swap() in ScColumn::SwapCol()
CellStoreEvent remembered the original ScColumn* and corresponding
ScColumn::mnBlkCountFormula, hence after inserting a column to the
left the quick check of ScColumn::HasFormulaCell() whether there
are any formula cells worked on the swapped in column (originally
the one to the right) that happens to be empty in the scenario.
Things worked correctly by accident if the next column to the
right already contained a formula cell.

Regression from

    commit e57a5905fb2975307af654710430d0a876dbd061
    Date:   Sat Jul 16 20:01:48 2016 -0400

Change-Id: If993856ceee657736f516a81c293506041a6b7eb

sc/inc/mtvelements.hxx
@@ -97,6 +97,8 @@ public:
 
     void element_block_acquired(const mdds::mtv::base_element_block* block);
     void element_block_released(const mdds::mtv::base_element_block* block);
+
+    void swap(CellStoreEvent& r);
 };
 
 /// Cell note container

sc/source/core/data/column.cxx
@@ -1895,6 +1895,10 @@ void ScColumn::SwapCol(ScColumn& rCol)
     maCellTextAttrs.swap(rCol.maCellTextAttrs);
     maCellNotes.swap(rCol.maCellNotes);
 
+    // Swap all CellStoreEvent mdds event_func related.
+    maCells.event_handler().swap( rCol.maCells.event_handler());
+    std::swap( mnBlkCountFormula, rCol.mnBlkCountFormula);
+
     // notes update caption
     UpdateNoteCaptions(0, MAXROW);
     rCol.UpdateNoteCaptions(0, MAXROW);

sc/source/core/data/mtvelements.cxx
@@ -54,6 +54,11 @@ void CellStoreEvent::element_block_released(const mdds::mtv::base_element_block*
     }
 }
 
+void CellStoreEvent::swap(CellStoreEvent& r)
+{
+    std::swap( mpCol, r.mpCol);
+}
+
 ColumnBlockPositionSet::ColumnBlockPositionSet(ScDocument& rDoc) : mrDoc(rDoc) {}
 
 ColumnBlockPosition* ColumnBlockPositionSet::getBlockPosition(SCTAB nTab, SCCOL nCol)