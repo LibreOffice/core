8571ee1373851338ce94624828666a6ce3597236
Jens Carl <j.carl43@gmx.de>
2017-11-02 00:19:40 +0000

    Remove obsolete sc_condformats unit test
    Remove the special snowflake sc_condformats because it's
    now covert by xsheetconditionalentries (see commit
    44e9640748791f602edb22cbc499200283466e1e).
    
    Change-Id: Ibdae392533605b56cf9b9c28f8c419ea0d32c0d8

--- sc/Module_sc.mk
+++ sc/Module_sc.mk
@@ -57,7 +57,6 @@ endif
 endif
 
 $(eval $(call gb_Module_add_slowcheck_targets,sc, \
-	CppunitTest_sc_condformats \
 	CppunitTest_sc_new_cond_format_api \
 	CppunitTest_sc_subsequent_filters_test \
 	CppunitTest_sc_subsequent_export_test \
