e0dc344395393c8a9364952a5d241c12fa8b8f54
Caolán McNamara <caolanm@redhat.com>
2017-11-02 17:23:00 +0000

    Resolves: tdf#113160 changing all warning dialogs to non-modal is unsafe
    existing code doesn't expect that so stuff crashes
    
    partial revert of...
    
    commit db6b703d391838c481fd090065f6d329edcd4efa
    Date:   Thu Aug 24 18:32:38 2017 +0200
    
        Allow non-modal Dialogs during FileImport/Load
    
    Change-Id: I152feb849186cf035664a700d3f94ee049cdf6d3
    Reviewed-on: https://gerrit.libreoffice.org/44227
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>
    
    Related: tdf#113160 set a temporary dialog parent during type detection
    
    to get warning dialogs that don't block the existing windows but whose
    lifecycle can be controlled to avoid crashes during exit
    
    Change-Id: I57965301c3d8a031acb33e83bf7715fe132385d0
    Reviewed-on: https://gerrit.libreoffice.org/45044
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>
    Reviewed-on: https://gerrit.libreoffice.org/45400
    Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>
    Tested-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>

--- vcl/source/window/dialog.cxx
+++ vcl/source/window/dialog.cxx
@@ -406,7 +406,7 @@ void Dialog::ImplInit( vcl::Window* pParent, WinBits nStyle, InitFlag eFlag )
     // Now, all Dialogs are per default system windows !!!
     nStyle |= WB_SYSTEMWINDOW;
 
-    if (InitFlag::NoParent == eFlag || InitFlag::NoParentCentered == eFlag)
+    if (InitFlag::NoParent == eFlag)
     {
         pParent = nullptr;
     }
@@ -992,7 +992,7 @@ long Dialog::GetResult() const
     return mpDialogImpl->mnResult;
 }
 
-void Dialog::EndAllDialogs( vcl::Window* pParent )
+void Dialog::EndAllDialogs( vcl::Window const * pParent )
 {
     ImplSVData* pSVData = ImplGetSVData();
     Dialog* pTempModDialog;
@@ -1007,14 +1007,6 @@ void Dialog::EndAllDialogs( vcl::Window* pParent )
         }
         pModDialog = pTempModDialog;
     }
-}
-
-bool Dialog::AreDialogsOpen()
-{
-    ImplSVData* pSVData = ImplGetSVData();
-    Dialog* pModDialog = pSVData->maWinData.mpLastExecuteDlg;
-
-    return (nullptr != pModDialog);
 }
 
 void Dialog::SetModalInputMode( bool bModal )
