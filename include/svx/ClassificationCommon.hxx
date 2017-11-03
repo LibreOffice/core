40918d0321f9e9a958fa80f309213e199cf21bce
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2017-11-03 21:57:18 +0900

    TSCP: add a full text. value of the header/footer as doc. property
    Change-Id: I4742066c34802017790c1c21ec04a7760d4fe1f7
    Reviewed-on: https://gerrit.libreoffice.org/44279
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>
    Tested-by: Tomaž Vajngerl <quikee@gmail.com>

---
+++ include/svx/ClassificationCommon.hxx
@@ -0,0 +1,37 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#ifndef INCLUDED_SVX_CLASSIFICATIONCOMMON_HXX
+#define INCLUDED_SVX_CLASSIFICATIONCOMMON_HXX
+
+#include <sal/config.h>
+#include <svx/svxdllapi.h>
+#include <svx/ClassificationField.hxx>
+#include <vector>
+#include <sfx2/classificationhelper.hxx>
+#include <com/sun/star/beans/XPropertyContainer.hpp>
+
+namespace svx {
+namespace classification {
+
+SVX_DLLPUBLIC OUString convertClassificationResultToString(std::vector<svx::ClassificationResult> const & rResults);
+
+SVX_DLLPUBLIC bool addOrInsertDocumentProperty(css::uno::Reference<css::beans::XPropertyContainer> const & rxPropertyContainer,
+                                               OUString const & rsKey, OUString const & rsValue);
+
+SVX_DLLPUBLIC void insertFullTextualRepresentationAsDocumentProperty(css::uno::Reference<css::beans::XPropertyContainer> const & rxPropertyContainer,
+                                                                     sfx::ClassificationKeyCreator const & rKeyCreator,
+                                                                     std::vector<svx::ClassificationResult> const & rResults);
+
+}} // end svx::classification namespace
+
+#endif // INCLUDED_SVX_CLASSIFICATIONCOMMON_HXX
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
