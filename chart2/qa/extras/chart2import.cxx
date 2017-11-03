LibreOffice/core

commit f3121049828596b369e3ea844355d61666e49795

use index as real index and not token, tdf#90511
At least in calc a theme index is a zero based index into the clrScheme.
A map is unsiutable for that task so let us use a vector and still allow
to get them by their tokens.

Change-Id: I09d56acaf22c3ed16387aae95c36382302c5a17e

oox/source/drawingml/clrscheme.cxx
@@ -43,6 +43,21 @@ void ClrMap::setColorMap( sal_Int32 nClrToken, sal_Int32 nMappedClrToken )
     maClrMap[ nClrToken ] = nMappedClrToken;
 }
 
+struct find_by_token
+{
+    find_by_token(sal_Int32 token):
+        m_token(token)
+    {
+    }
+
+    bool operator()(const std::pair<sal_Int32, sal_Int32>& r)
+    {
+        return r.first == m_token;
+    }
+
+private:
+    sal_Int32 m_token;
+};
 
 bool ClrScheme::getColor( sal_Int32 nSchemeClrToken, sal_Int32& rColor ) const
 {
@@ -54,15 +69,27 @@ bool ClrScheme::getColor( sal_Int32 nSchemeClrToken, sal_Int32& rColor ) const
         case XML_tx1 : nSchemeClrToken = XML_dk1; break;
         case XML_tx2 : nSchemeClrToken = XML_dk2; break;
     }
-    std::map < sal_Int32, sal_Int32 >::const_iterator aIter( maClrScheme.find( nSchemeClrToken ) );
+
+    auto aIter = std::find_if(maClrScheme.begin(), maClrScheme.end(), find_by_token(nSchemeClrToken) );
+
     if ( aIter != maClrScheme.end() )
-        rColor = (*aIter).second;
+        rColor = aIter->second;
+
     return aIter != maClrScheme.end();
 }
 
 void ClrScheme::setColor( sal_Int32 nSchemeClrToken, sal_Int32 nColor )
 {
-    maClrScheme[ nSchemeClrToken ] = nColor;
+    maClrScheme.push_back(std::pair<sal_Int32, sal_Int32>(nSchemeClrToken, nColor));
+}
+
+bool ClrScheme::getColorByIndex(size_t nIndex, sal_Int32& rColor) const
+{
+    if (nIndex >= maClrScheme.size())
+        return false;
+
+    rColor = maClrScheme[nIndex].second;
+    return true;
 }
 
 } }

include/oox/drawingml/clrscheme.hxx
@@ -42,12 +42,15 @@ typedef std::shared_ptr< ClrMap > ClrMapPtr;
 
 class OOX_DLLPUBLIC ClrScheme
 {
-    std::map < sal_Int32, sal_Int32 > maClrScheme;
+    std::vector< std::pair<sal_Int32, sal_Int32> > maClrScheme;
 
 public:
 
     bool     getColor( sal_Int32 nSchemeClrToken, sal_Int32& rColor ) const;
     void     setColor( sal_Int32 nSchemeClrToken, sal_Int32 nColor );
+
+    bool     getColorByIndex(size_t nIndex,
+            sal_Int32& rColor) const;
 };
 
 typedef std::shared_ptr< ClrScheme > ClrSchemePtr;

sc/source/filter/inc/themebuffer.hxx
@@ -37,6 +37,8 @@ public:
     /** Returns the theme color with the specified token identifier. */
     sal_Int32           getColorByToken( sal_Int32 nToken ) const;
 
+    sal_Int32           getColorByIndex(size_t nIndex) const;
+
     /** Returns the default font data for the current file type. */
     inline const FontModel& getDefaultFontModel() const { return *mxDefFontModel; }
 

sc/source/filter/oox/condformatbuffer.cxx
@@ -245,7 +245,7 @@ void DataBarRule::importColor( const AttributeList& rAttribs )
     else if( rAttribs.hasAttribute( XML_theme ) )
     {
         sal_uInt32 nThemeIndex = rAttribs.getUnsigned( XML_theme, 0 );
-        nColor = getTheme().getColorByToken( nThemeIndex );
+        nColor = getTheme().getColorByIndex( nThemeIndex );
     }
 
     ::Color aColor = RgbToRgbComponents( nColor );

sc/source/filter/oox/themebuffer.cxx
@@ -56,6 +56,12 @@ sal_Int32 ThemeBuffer::getColorByToken( sal_Int32 nToken ) const
     return getClrScheme().getColor( nToken, nColor ) ? nColor : API_RGB_TRANSPARENT;
 }
 
+sal_Int32 ThemeBuffer::getColorByIndex(size_t nIndex) const
+{
+    sal_Int32 nColor = 0;
+    return getClrScheme().getColorByIndex(nIndex, nColor) ? nColor : API_RGB_TRANSPARENT;
+}
+
 } // namespace xls
 } // namespace oox
 

commit 15c9b6e2bea6b817453bf83b889e897cfd60bb30

tdf#105517: overwrite existing scheme colors in vector
Theme overrides stopped working once ClrScheme::maClrScheme was
changed to vector, and colors were always appended to it.

Regression from f3121049828596b369e3ea844355d61666e49795.

Change-Id: Iae850dcabf57b12d8a564e84acf38d9988cfe963

oox/source/drawingml/clrscheme.cxx
@@ -82,7 +82,11 @@ bool ClrScheme::getColor( sal_Int32 nSchemeClrToken, sal_Int32& rColor ) const
 
 void ClrScheme::setColor( sal_Int32 nSchemeClrToken, sal_Int32 nColor )
 {
-    maClrScheme.emplace_back(nSchemeClrToken, nColor);
+    const auto aIter = std::find_if(maClrScheme.begin(), maClrScheme.end(), find_by_token(nSchemeClrToken) );
+    if ( aIter != maClrScheme.end() )
+        aIter->second = nColor;
+    else
+        maClrScheme.emplace_back(nSchemeClrToken, nColor);
 }
 
 bool ClrScheme::getColorByIndex(size_t nIndex, sal_Int32& rColor) const

chart2/qa/extras/chart2import.cxx
@@ -62,6 +62,7 @@ public:
     void testFdo54361();
     void testFdo54361_1();
     void testTdf86624(); // manually placed legends
+    void testTdf105517();
     void testTdf106217();
     void testAutoBackgroundXLSX();
     void testChartAreaStyleBackgroundXLSX();
@@ -129,6 +130,7 @@ public:
     CPPUNIT_TEST(testFdo54361);
     CPPUNIT_TEST(testFdo54361_1);
     CPPUNIT_TEST(testTdf86624);
+    CPPUNIT_TEST(testTdf105517);
     CPPUNIT_TEST(testTdf106217);
     CPPUNIT_TEST(testAutoBackgroundXLSX);
     CPPUNIT_TEST(testChartAreaStyleBackgroundXLSX);
@@ -745,6 +747,34 @@ void Chart2ImportTest::testTdf86624()
     awt::Point aPos = xLegend->getPosition();
     CPPUNIT_ASSERT(aPos.X > 5000); // real value for me is above 8000 but before bug fix is below 1000
     CPPUNIT_ASSERT(aPos.Y > 4000); // real value for ms is above 7000
+}
+
+void Chart2ImportTest::testTdf105517()
+{
+    load("/chart2/qa/extras/data/pptx/", "tdf105517.pptx");
+    Reference<chart2::XChartDocument> xChartDoc(getChartDocFromDrawImpress(0, 0), uno::UNO_QUERY);
+    CPPUNIT_ASSERT(xChartDoc.is());
+
+    Reference<chart2::XCoordinateSystemContainer> xCoordContainer(xChartDoc->getFirstDiagram(), uno::UNO_QUERY);
+    CPPUNIT_ASSERT(xCoordContainer.is());
+    Reference<chart2::XChartTypeContainer> xChartTypeContainer(xCoordContainer->getCoordinateSystems()[0], uno::UNO_QUERY);
+    CPPUNIT_ASSERT(xChartTypeContainer.is());
+    Reference<chart2::XDataSeriesContainer> xDSContainer(xChartTypeContainer->getChartTypes()[0], uno::UNO_QUERY);
+    CPPUNIT_ASSERT(xDSContainer.is());
+    Reference<beans::XPropertySet> xPropSet1(xDSContainer->getDataSeries()[0], uno::UNO_QUERY);
+    CPPUNIT_ASSERT(xPropSet1.is());
+
+    long lineColor;
+    xPropSet1->getPropertyValue("Color") >>= lineColor;
+    // incorrect line color was 0x4a7ebb due to not handling themeOverride
+    CPPUNIT_ASSERT_EQUAL(lineColor, long(0xeaa700));
+
+    Reference<beans::XPropertySet> xPropSet2(xDSContainer->getDataSeries()[1], uno::UNO_QUERY);
+    CPPUNIT_ASSERT(xPropSet2.is());
+
+    xPropSet2->getPropertyValue("Color") >>= lineColor;
+    // incorrect line color was 0x98b855
+    CPPUNIT_ASSERT_EQUAL(lineColor, long(0x1e69a8));
 }
 
 void Chart2ImportTest::testTdf106217()