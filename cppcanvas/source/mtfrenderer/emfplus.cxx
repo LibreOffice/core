LibreOffice/core

commit f115ec6ce1ff4bf6e68ceb441f716a4c70ecfdf6

EMF+: Do not ignore source rectangle of the image to be rendered.
Change-Id: I4f861c7653c89d8f1ca73696e885f33f7bfc3b06

cppcanvas/source/mtfrenderer/emfplus.cxx
@@ -1826,6 +1826,7 @@ namespace cppcanvas
                                 sal_Int32 aCount;
 
                                 ReadRectangle (rMF, sx, sy, sw, sh);
+                                Rectangle aSource(Point(sx, sy), Size(sw, sh));
 
                                 SAL_INFO("cppcanvas.emf", "EMF+ " << (type == EmfPlusRecordTypeDrawImagePoints ? "DrawImagePoints" : "DrawImage") << " source rectangle: " << sx << "," << sy << " " << sw << "x" << sh);
 
@@ -1866,6 +1867,7 @@ namespace cppcanvas
 
                                 if (bValid) {
                                     BitmapEx aBmp( image.graphic.GetBitmapEx () );
+                                    aBmp.Crop( aSource );
 
                                     Size aSize( aBmp.GetSizePixel() );
                                     SAL_INFO("cppcanvas.emf", "EMF+ bitmap size: " << aSize.Width() << "x" << aSize.Height());

commit b3d47a1a2f4a5c5ad6c5eb9d34304cb8dac85622

EMF+: Restrict cropping to bitmap types
Some EMF+ metafiles were incorrectly cropped.

Regression from f115ec6ce1ff4bf6e68ceb441f716a4c70ecfdf6.

Change-Id: I1bba0263cb999a0ed11feb25aaefc7b0416460be

cppcanvas/source/mtfrenderer/emfplus.cxx
@@ -2166,7 +2166,9 @@ namespace cppcanvas
 
                                 if (bValid) {
                                     BitmapEx aBmp( image.graphic.GetBitmapEx () );
-                                    aBmp.Crop( aSource );
+                                    if (image.type == 1) { // Bitmap
+                                      aBmp.Crop( aSource );
+                                    }
 
                                     Size aSize( aBmp.GetSizePixel() );
                                     SAL_INFO("cppcanvas.emf", "EMF+ bitmap size: " << aSize.Width() << "x" << aSize.Height());