0d9123e158829849985857247ed45bda6bf02488
Bartosz Kosiorek <gang65@poczta.onet.pl>
2017-11-05 00:02:49 +0100

    tdf#113635 WIP Implement proper reading of the EmfPlusRegionNode
    Change-Id: I668e5892701b979f09bcf5bbce44a43226676192

--- drawinglayer/source/tools/emfpregion.cxx
+++ drawinglayer/source/tools/emfpregion.cxx
@@ -36,6 +36,7 @@
 #include <basegfx/polygon/b2dpolypolygontools.hxx>
 #include <vcl/canvastools.hxx>
 #include "emfpregion.hxx"
+#include "emfppath.hxx"
 
 using namespace ::com::sun::star;
 using namespace ::basegfx;
@@ -43,8 +44,8 @@ using namespace ::basegfx;
 namespace emfplushelper
 {
     EMFPRegion::EMFPRegion()
-        : parts(0)
-        , combineMode(nullptr)
+        : count(0)
+        , regionNodeDataType(nullptr)
         , initialState(0)
         , ix(0.0)
         , iy(0.0)
@@ -55,37 +56,103 @@ namespace emfplushelper
 
     EMFPRegion::~EMFPRegion()
     {
-        if (combineMode)
+        if (regionNodeDataType)
         {
-            delete[] combineMode;
-            combineMode = nullptr;
+            delete[] regionNodeDataType;
+            regionNodeDataType = nullptr;
         }
     }
 
-    void EMFPRegion::Read(SvStream& s)
+    void EMFPRegion::ReadRegionNode(SvStream& s)
+    {
+        sal_uInt32 dataType;
+        s.ReadUInt32(dataType);
+        SAL_INFO("drawinglayer", "EMF+\t Region node data type 0x" << std::hex << dataType << std::dec);
+
+        switch (dataType)
+        {
+        case RegionNodeDataTypeAnd:
+        {
+            ReadRegionNode(s);
+            ReadRegionNode(s);
+            break;
+        }
+        case RegionNodeDataTypeOr:
+        {
+            ReadRegionNode(s);
+            ReadRegionNode(s);
+            break;
+        }
+        case RegionNodeDataTypeXor:
+        {
+            ReadRegionNode(s);
+            ReadRegionNode(s);
+            break;
+        }
+        case RegionNodeDataTypeExclude:
+        {
+            ReadRegionNode(s);
+            ReadRegionNode(s);
+            break;
+        }
+        case RegionNodeDataTypeComplement:
+        {
+            ReadRegionNode(s);
+            ReadRegionNode(s);
+            break;
+        }
+        case RegionNodeDataTypeRect:
+        {
+            s.ReadFloat(ix).ReadFloat(iy).ReadFloat(iw).ReadFloat(ih);
+            SAL_INFO("drawinglayer", "EMF+\t\t RegionNodeDataTypeRect x:" << ix << ", y:" << iy << ", width:" << iw << ", height:" << ih);
+            break;
+        }
+        case RegionNodeDataTypePath:
+        {
+            sal_Int32 pathLength;
+            s.ReadInt32(pathLength);
+            SAL_INFO("drawinglayer", "EMF+\t\t RegionNodeDataTypePath, Path Length: " << pathLength << " bytes");
+
+            sal_uInt32 header, pathFlags;
+            sal_Int32 points;
+
+            s.ReadUInt32(header).ReadInt32(points).ReadUInt32(pathFlags);
+            SAL_INFO("drawinglayer", "EMF+\t\t header: 0x" << std::hex << header << " points: " << std::dec << points << " additional flags: 0x" << std::hex << pathFlags << std::dec);
+
+            EMFPPath path(points);
+            path.Read(s, pathFlags);
+            break;
+        }
+        case RegionNodeDataTypeEmpty:
+        {
+            SAL_INFO("drawinglayer", "EMF+\t\t RegionNodeDataTypeEmpty");
+
+            break;
+        }
+        case RegionNodeDataTypeInfinite:
+        {
+            SAL_INFO("drawinglayer", "EMF+\t\t RegionNodeDataTypeInfinite");
+            break;
+        }
+        default:
+        {
+            SAL_WARN("drawinglayer", "EMF+\t\t Unhandled region type: 0x" << std::hex << dataType << std::dec);
+        }
+        }
+    }
+
+    void EMFPRegion::ReadRegion(SvStream& s)
     {
         sal_uInt32 header;
-        s.ReadUInt32(header).ReadInt32(parts);
-        SAL_INFO("drawinglayer", "EMF+\tregion");
-        SAL_INFO("drawinglayer", "EMF+\theader: 0x" << std::hex << header << " parts: " << parts << std::dec);
+        s.ReadUInt32(header).ReadUInt32(count);
+        // An array should be RegionNodeCount+1 of EmfPlusRegionNode objects.
+        SAL_WARN("drawinglayer", "EMF+\t EmfPlusRegion TODO");
+        SAL_INFO("drawinglayer", "EMF+\t version: 0x" << std::hex << header << std::dec << ", region node count: " << count);
 
-        if (parts)
-        {
-            if (parts<0 || sal_uInt32(parts)>SAL_MAX_INT32 / sizeof(sal_Int32))
-            {
-                parts = SAL_MAX_INT32 / sizeof(sal_Int32);
-            }
+        regionNodeDataType = new sal_uInt32[count];
 
-            combineMode = new sal_Int32[parts];
-
-            for (int i = 0; i < parts; i++)
-            {
-                s.ReadInt32(combineMode[i]);
-                SAL_INFO("drawinglayer", "EMF+\tcombine mode [" << i << "]: 0x" << std::hex << combineMode[i] << std::dec);
-            }
-        }
-
-        s.ReadInt32(initialState);
+        ReadRegionNode(s);
+        s.ReadUInt32(initialState);
         SAL_INFO("drawinglayer", "EMF+\tinitial state: 0x" << std::hex << initialState << std::dec);
     }
 }
