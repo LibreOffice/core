f74d0b89958b99b57ebc23ebd5ad92947d60faa5
Thomas Beck <thomas.beck@cib.de>
2017-11-06 13:35:45 +0100

    tdf#108947 Added Unittest.
    Added different branches for Windows/Linux because of differences in
    linebreaks.
    
    Change-Id: I17397155292e173bf698fa2680aaaf2711c064cf

--- sw/qa/extras/rtfimport/rtfimport.cxx
+++ sw/qa/extras/rtfimport/rtfimport.cxx
@@ -1175,6 +1175,24 @@ DECLARE_RTFIMPORT_TEST(testImportHeaderFooter, "tdf108055.rtf")
     CPPUNIT_ASSERT_EQUAL(OUString("Footer uneven"), aActual);
 }
 
+DECLARE_RTFIMPORT_TEST(testTdf108947, "tdf108947.rtf")
+{
+    //Check page count
+    CPPUNIT_ASSERT_EQUAL(2, getPages());
+
+    //Check if Headers/Footers contain what they should in this document
+    uno::Reference<text::XText> xHeaderTextRight = getProperty< uno::Reference<text::XText> >(getStyles("PageStyles")->getByName("Default Style"), "HeaderTextRight");
+    OUString aActual = xHeaderTextRight->getString();
+    CPPUNIT_ASSERT_EQUAL(OUString(""), aActual);
+
+    OUString aExpected = SAL_NEWLINE_STRING;
+    aExpected += SAL_NEWLINE_STRING;
+    aExpected += "Header Page 2 ?";
+    uno::Reference<text::XText> xHeaderTextLeft = getProperty< uno::Reference<text::XText> >(getStyles("PageStyles")->getByName("Default Style"), "HeaderTextLeft");
+    aActual = xHeaderTextLeft->getString();
+    CPPUNIT_ASSERT_EQUAL(aExpected, aActual);
+}
+
 DECLARE_RTFIMPORT_TEST(testWatermark, "watermark.rtf")
 {
     Size aExpectedSize(14965, 7482);
