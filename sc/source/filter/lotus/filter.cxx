43b94e8d855576518a01c586a8d5495a1077ca8b
Caolán McNamara <caolanm@redhat.com>
2017-11-07 12:24:44 +0000

    ofz 1-2-3 mem leak
    Change-Id: I72b633b27578815fb0951e2bfac755fce51203f1

--- sc/source/filter/lotus/filter.cxx
+++ sc/source/filter/lotus/filter.cxx
@@ -43,21 +43,35 @@ generate_Opcodes(LotusContext &rContext, SvStream& aStream,
     OPCODE_FKT *pOps;
     int         nOps;
 
+    ErrCode nErr = ERRCODE_NONE;
+
     switch (rContext.eTyp)
     {
         case eWK_1:
         case eWK_2:
             pOps = LotusContext::pOpFkt;
             nOps = FKT_LIMIT;
-        break;
+            break;
         case eWK123:
             pOps = LotusContext::pOpFkt123;
             nOps = FKT_LIMIT123;
-        break;
-        case eWK3:      return SCERR_IMPORT_NI;
-        case eWK_Error: return SCERR_IMPORT_FORMAT;
-        default:        return SCERR_IMPORT_UNKNOWN_WK;
-     }
+            break;
+        case eWK3:
+            nErr = SCERR_IMPORT_NI;
+            break;
+        case eWK_Error:
+            nErr = SCERR_IMPORT_FORMAT;
+            break;
+        default:
+            nErr = SCERR_IMPORT_UNKNOWN_WK;
+            break;
+    }
+
+    if (nErr != ERRCODE_NONE)
+    {
+        MemDelete(rContext);
+        return nErr;
+    }
 
     // #i76299# seems that SvStream::IsEof() does not work correctly
     aStream.Seek( STREAM_SEEK_TO_END );
@@ -72,7 +86,10 @@ generate_Opcodes(LotusContext &rContext, SvStream& aStream,
         if( nOpcode == LOTUS_EOF )
             rContext.bEOF = true;
         else if( nOpcode == LOTUS_FILEPASSWD )
-            return SCERR_IMPORT_FILEPASSWD;
+        {
+            nErr = SCERR_IMPORT_FILEPASSWD;
+            break;
+        }
         else if( nOpcode < nOps )
             pOps[ nOpcode ] (rContext, aStream, nLength);
         else if (rContext.eTyp == eWK123 && nOpcode == LOTUS_PATTERN)
@@ -101,9 +118,10 @@ generate_Opcodes(LotusContext &rContext, SvStream& aStream,
 
     MemDelete(rContext);
 
-    rContext.pDoc->CalcAfterLoad();
+    if (nErr == ERRCODE_NONE)
+        rContext.pDoc->CalcAfterLoad();
 
-    return ERRCODE_NONE;
+    return nErr;
 }
 
 WKTYP ScanVersion(SvStream& aStream)
