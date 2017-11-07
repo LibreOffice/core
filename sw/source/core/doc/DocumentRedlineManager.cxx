3c3c38fa0150ec6c56f0de3bae7c97864ec62c30
Caolán McNamara <caolanm@redhat.com>
2017-11-07 09:13:06 +0000

    ofz#4066 Bad-cast
    and a cluster of others around here. This pRedl has been deleted at this point.
    
    This doesn't matter for most users, seeing as !LibreOfficeKit::isActive() is
    the usual case so the deleted pRedl isn't fully accesssed.
    
    Because the deleted pRedl won't be found in GetRedlineTable when deleted,
    rework this a little to avoid the problem.
    
    Reviewed-on: https://gerrit.libreoffice.org/44392
    Reviewed-by: Michael Stahl <mstahl@redhat.com>
    Tested-by: Jenkins <ci@libreoffice.org>
    (cherry picked from commit f0074dd8c6e0040d7f331ba4a42ea6193c0eac99)
    
    Change-Id: I5c2f405cdae0b0c804ee7452629a14206516962d

--- sw/source/core/doc/DocumentRedlineManager.cxx
+++ sw/source/core/doc/DocumentRedlineManager.cxx
@@ -1216,7 +1216,7 @@ bool DocumentRedlineManager::AppendRedline( SwRangeRedline* pNewRedl, bool bCall
                             delete pNewRedl;
                             pNewRedl = nullptr;
                             if (eCmpPos == POS_INSIDE)
-                                pRedl->MaybeNotifyModification();
+                                MaybeNotifyRedlineModification(pRedl, &m_rDoc);
                             break;
 
                         case POS_OUTSIDE:
