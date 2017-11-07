LibreOffice/core

commit dc28e90d200a839d4017d548217ee5ce8a23f848

fdo#84938: convert IMPORT_ constants to 'enum class'
Change-Id: Idaa8f07c62b3ba93c27ca5fe286720254baac10d

sc/source/filter/xml/XMLTableShapeResizer.cxx
@@ -95,7 +95,7 @@ void ScMyOLEFixer::CreateChartListener(ScDocument* pDoc,
         //for loading binary files e.g.
         //if we have the flat filter we need to set the dirty flag thus the visible charts get repainted
         //otherwise the charts keep their first visual representation which was created at a moment where the calc itself was not loaded completely and is therefore incorrect
-        if( (rImport.getImportFlags() & IMPORT_ALL) == IMPORT_ALL )
+        if( rImport.getImportFlags() & SvXMLImportFlags::ALL )
             pCL->SetDirty( true );
         else
         {

include/xmloff/xmlimp.hxx
@@ -56,6 +56,7 @@
 #include <com/sun/star/xml/sax/XFastDocumentHandler.hpp>
 #include <com/sun/star/xml/sax/XFastContextHandler.hpp>
 #include <com/sun/star/xml/sax/XFastAttributeList.hpp>
+#include <o3tl/typed_flags_set.hxx>
 
 namespace com { namespace sun { namespace star {
     namespace frame { class XModel; }
@@ -82,17 +83,24 @@ namespace xmloff {
     class RDFaImportHelper;
 }
 
-#define IMPORT_META         0x0001
-#define IMPORT_STYLES       0x0002
-#define IMPORT_MASTERSTYLES 0x0004
-#define IMPORT_AUTOSTYLES   0x0008
-#define IMPORT_CONTENT      0x0010
-#define IMPORT_SCRIPTS      0x0020
-#define IMPORT_SETTINGS     0x0040
-#define IMPORT_FONTDECLS    0x0080
-#define IMPORT_EMBEDDED     0x0100
-#define IMPORT_OOO_NAMESPACES   0x0100
-#define IMPORT_ALL          0xffff
+enum class SvXMLImportFlags {
+    NONE            = 0x0000,
+    META            = 0x0001,
+    STYLES          = 0x0002,
+    MASTERSTYLES    = 0x0004,
+    AUTOSTYLES      = 0x0008,
+    CONTENT         = 0x0010,
+    SCRIPTS         = 0x0020,
+    SETTINGS        = 0x0040,
+    FONTDECLS       = 0x0080,
+    EMBEDDED        = 0x0100,
+    OOO_NAMESPACES  = 0x0100,
+    ALL             = 0xffff
+};
+namespace o3tl
+{
+    template<> struct typed_flags<SvXMLImportFlags> : is_typed_flags<SvXMLImportFlags, 0xffff> {};
+}
 
 
 
@@ -148,7 +156,7 @@ class XMLOFF_DLLPUBLIC SvXMLImport : public ::cppu::WeakImplHelper7<
 
     SAL_DLLPRIVATE void _InitCtor();
 
-    sal_uInt16  mnImportFlags;
+    SvXMLImportFlags  mnImportFlags;
     sal_uInt16  mnErrorFlags;
     std::set< OUString > embeddedFontUrlsKnown;
 
@@ -199,7 +207,7 @@ public:
     SvXMLImport(
         const ::com::sun::star::uno::Reference< ::com::sun::star::uno::XComponentContext >& xContext,
         OUString const & implementationName,
-        sal_uInt16 nImportFlags = IMPORT_ALL ) throw();
+        SvXMLImportFlags nImportFlags = SvXMLImportFlags::ALL ) throw();
 
     virtual ~SvXMLImport() throw();
 
@@ -369,7 +377,7 @@ public:
     const SvXMLStylesContext *GetStyles() const;
     const SvXMLStylesContext *GetAutoStyles() const;
 
-    sal_uInt16  getImportFlags() const { return mnImportFlags; }
+    SvXMLImportFlags  getImportFlags() const { return mnImportFlags; }
     bool    IsFormsSupported() const { return mbIsFormsSupported; }
     OUString GetAbsoluteReference(const OUString& rValue) const;
 

linguistic/source/convdicxml.hxx
@@ -84,7 +84,7 @@ public:
 
     //!!  see comment for pDic member
     ConvDicXMLImport( ConvDic *pConvDic ) :
-        SvXMLImport ( comphelper::getProcessComponentContext(), "com.sun.star.lingu2.ConvDicXMLImport", IMPORT_ALL ),
+        SvXMLImport ( comphelper::getProcessComponentContext(), "com.sun.star.lingu2.ConvDicXMLImport", SvXMLImportFlags::ALL ),
         pDic        ( pConvDic )
     {
         nLanguage       = LANGUAGE_NONE;

reportdesign/source/filter/xml/xmlfilter.cxx
@@ -283,7 +283,7 @@ sal_Int32 ReadThroughComponent(
 
 uno::Reference< uno::XInterface > ORptImportHelper::create(uno::Reference< uno::XComponentContext > const & xContext)
 {
-    return static_cast< XServiceInfo* >(new ORptFilter(xContext, IMPORT_SETTINGS ));
+    return static_cast< XServiceInfo* >(new ORptFilter(xContext, SvXMLImportFlags::SETTINGS ));
 }
 
 OUString ORptImportHelper::getImplementationName_Static(  ) throw (RuntimeException)
@@ -300,8 +300,8 @@ Sequence< OUString > ORptImportHelper::getSupportedServiceNames_Static(  ) throw
 
 Reference< XInterface > ORptContentImportHelper::create(const Reference< XComponentContext > & xContext)
 {
-    return static_cast< XServiceInfo* >(new ORptFilter(xContext,IMPORT_AUTOSTYLES |   IMPORT_CONTENT | IMPORT_SCRIPTS |
-        IMPORT_FONTDECLS ));
+    return static_cast< XServiceInfo* >(new ORptFilter(xContext, SvXMLImportFlags::AUTOSTYLES | SvXMLImportFlags::CONTENT | SvXMLImportFlags::SCRIPTS |
+        SvXMLImportFlags::FONTDECLS ));
 }
 
 OUString ORptContentImportHelper::getImplementationName_Static(  ) throw (RuntimeException)
@@ -320,8 +320,8 @@ Sequence< OUString > ORptContentImportHelper::getSupportedServiceNames_Static(  
 Reference< XInterface > ORptStylesImportHelper::create(Reference< XComponentContext > const & xContext)
 {
     return static_cast< XServiceInfo* >(new ORptFilter(xContext,
-        IMPORT_STYLES | IMPORT_MASTERSTYLES | IMPORT_AUTOSTYLES |
-        IMPORT_FONTDECLS ));
+        SvXMLImportFlags::STYLES | SvXMLImportFlags::MASTERSTYLES | SvXMLImportFlags::AUTOSTYLES |
+        SvXMLImportFlags::FONTDECLS ));
 }
 
 OUString ORptStylesImportHelper::getImplementationName_Static(  ) throw (RuntimeException)
@@ -340,7 +340,7 @@ Sequence< OUString > ORptStylesImportHelper::getSupportedServiceNames_Static(  )
 Reference< XInterface > ORptMetaImportHelper::create(Reference< XComponentContext > const & xContext)
 {
     return static_cast< XServiceInfo* >(new ORptFilter(xContext,
-        IMPORT_META));
+        SvXMLImportFlags::META));
 }
 
 OUString ORptMetaImportHelper::getImplementationName_Static(  ) throw (RuntimeException)
@@ -358,7 +358,7 @@ Sequence< OUString > ORptMetaImportHelper::getSupportedServiceNames_Static(  ) t
 
 // - ORptFilter -
 
-ORptFilter::ORptFilter( const uno::Reference< XComponentContext >& _rxContext,sal_uInt16 nImportFlags )
+ORptFilter::ORptFilter( const uno::Reference< XComponentContext >& _rxContext, SvXMLImportFlags nImportFlags )
     :SvXMLImport(_rxContext, getImplementationName_Static(), nImportFlags)
 {
     GetMM100UnitConverter().SetCoreMeasureUnit(util::MeasureUnit::MM_100TH);
@@ -1033,7 +1033,7 @@ SvXMLImportContext* ORptFilter::CreateMetaContext(const OUString& rLocalName,con
 {
     SvXMLImportContext* pContext = NULL;
 
-    if ( (getImportFlags() & IMPORT_META) )
+    if ( (getImportFlags() & SvXMLImportFlags::META) )
     {
         uno::Reference<document::XDocumentPropertiesSupplier> xDPS(GetModel(), uno::UNO_QUERY_THROW);
         pContext = new SvXMLMetaDocumentContext(*this,XML_NAMESPACE_OFFICE, rLocalName,xDPS->getDocumentProperties());

reportdesign/source/filter/xml/xmlfilter.hxx
@@ -117,7 +117,7 @@ protected:
     virtual ~ORptFilter()  throw();
 public:
 
-    ORptFilter( const Reference< XComponentContext >& _rxContext, sal_uInt16 nImportFlags = IMPORT_ALL );
+    ORptFilter( const Reference< XComponentContext >& _rxContext, SvXMLImportFlags nImportFlags = SvXMLImportFlags::ALL );
 
     // XFilter
     virtual sal_Bool SAL_CALL filter( const Sequence< PropertyValue >& rDescriptor ) throw(RuntimeException, std::exception) SAL_OVERRIDE;

sc/source/filter/xml/xmlimprt.cxx
@@ -127,7 +127,7 @@ uno::Reference< uno::XInterface > SAL_CALL ScXMLImport_createInstance(
 {
     // #110680#
     // return (cppu::OWeakObject*)new ScXMLImport(IMPORT_ALL);
-    return (cppu::OWeakObject*)new ScXMLImport( comphelper::getComponentContext(rSMgr), ScXMLImport_getImplementationName(), IMPORT_ALL );
+    return (cppu::OWeakObject*)new ScXMLImport( comphelper::getComponentContext(rSMgr), ScXMLImport_getImplementationName(), SvXMLImportFlags::ALL );
 }
 
 OUString SAL_CALL ScXMLImport_Meta_getImplementationName() throw()
@@ -146,7 +146,7 @@ uno::Reference< uno::XInterface > SAL_CALL ScXMLImport_Meta_createInstance(
 {
     // #110680#
     // return (cppu::OWeakObject*)new ScXMLImport(IMPORT_META);
-    return (cppu::OWeakObject*)new ScXMLImport( comphelper::getComponentContext(rSMgr), ScXMLImport_Meta_getImplementationName(), IMPORT_META );
+    return (cppu::OWeakObject*)new ScXMLImport( comphelper::getComponentContext(rSMgr), ScXMLImport_Meta_getImplementationName(), SvXMLImportFlags::META );
 }
 
 OUString SAL_CALL ScXMLImport_Styles_getImplementationName() throw()
@@ -164,8 +164,8 @@ uno::Reference< uno::XInterface > SAL_CALL ScXMLImport_Styles_createInstance(
     const uno::Reference< lang::XMultiServiceFactory > & rSMgr ) throw( uno::Exception )
 {
     // #110680#
-    // return (cppu::OWeakObject*)new ScXMLImport(IMPORT_STYLES|IMPORT_AUTOSTYLES|IMPORT_MASTERSTYLES|IMPORT_FONTDECLS);
-    return (cppu::OWeakObject*)new ScXMLImport( comphelper::getComponentContext(rSMgr), ScXMLImport_Styles_getImplementationName(), IMPORT_STYLES|IMPORT_AUTOSTYLES|IMPORT_MASTERSTYLES|IMPORT_FONTDECLS);
+    // return (cppu::OWeakObject*)new ScXMLImport(SvXMLImportFlagsSTYLES|SvXMLImportFlags::AUTOSTYLES|SvXMLImportFlags::MASTERSTYLES|SvXMLImportFlags::FONTDECLS);
+    return (cppu::OWeakObject*)new ScXMLImport( comphelper::getComponentContext(rSMgr), ScXMLImport_Styles_getImplementationName(), SvXMLImportFlags::STYLES|SvXMLImportFlags::AUTOSTYLES|SvXMLImportFlags::MASTERSTYLES|SvXMLImportFlags::FONTDECLS);
 }
 
 OUString SAL_CALL ScXMLImport_Content_getImplementationName() throw()
@@ -183,8 +183,8 @@ uno::Reference< uno::XInterface > SAL_CALL ScXMLImport_Content_createInstance(
     const uno::Reference< lang::XMultiServiceFactory > & rSMgr ) throw( uno::Exception )
 {
     // #110680#
-    // return (cppu::OWeakObject*)new ScXMLImport(IMPORT_META|IMPORT_STYLES|IMPORT_MASTERSTYLES|IMPORT_AUTOSTYLES|IMPORT_CONTENT|IMPORT_SCRIPTS|IMPORT_SETTINGS|IMPORT_FONTDECLS);
-    return (cppu::OWeakObject*)new ScXMLImport( comphelper::getComponentContext(rSMgr), ScXMLImport_Content_getImplementationName(), IMPORT_AUTOSTYLES|IMPORT_CONTENT|IMPORT_SCRIPTS|IMPORT_FONTDECLS);
+    // return (cppu::OWeakObject*)new ScXMLImport(SvXMLImportFlags::META|SvXMLImportFlags::STYLES|SvXMLImportFlags::MASTERSTYLES|SvXMLImportFlags::AUTOSTYLES|SvXMLImportFlags::CONTENT|SvXMLImportFlags::SCRIPTS|SvXMLImportFlags::SETTINGS|SvXMLImportFlags::FONTDECLS);
+    return (cppu::OWeakObject*)new ScXMLImport( comphelper::getComponentContext(rSMgr), ScXMLImport_Content_getImplementationName(), SvXMLImportFlags::AUTOSTYLES|SvXMLImportFlags::CONTENT|SvXMLImportFlags::SCRIPTS|SvXMLImportFlags::FONTDECLS);
 }
 
 OUString SAL_CALL ScXMLImport_Settings_getImplementationName() throw()
@@ -202,8 +202,8 @@ uno::Reference< uno::XInterface > SAL_CALL ScXMLImport_Settings_createInstance(
     const uno::Reference< lang::XMultiServiceFactory > & rSMgr ) throw( uno::Exception )
 {
     // #110680#
-    // return (cppu::OWeakObject*)new ScXMLImport(IMPORT_SETTINGS);
-    return (cppu::OWeakObject*)new ScXMLImport( comphelper::getComponentContext(rSMgr), ScXMLImport_Settings_getImplementationName(), IMPORT_SETTINGS );
+    // return (cppu::OWeakObject*)new ScXMLImport(SvXMLImportFlags::SETTINGS);
+    return (cppu::OWeakObject*)new ScXMLImport( comphelper::getComponentContext(rSMgr), ScXMLImport_Settings_getImplementationName(), SvXMLImportFlags::SETTINGS );
 }
 
 const SvXMLTokenMap& ScXMLImport::GetTableRowCellAttrTokenMap()
@@ -356,19 +356,19 @@ SvXMLImportContext *ScXMLDocContext_Impl::CreateChildContext( sal_uInt16 nPrefix
     switch( rTokenMap.Get( nPrefix, rLocalName ) )
     {
     case XML_TOK_DOC_FONTDECLS:
-        if (GetScImport().getImportFlags() & IMPORT_FONTDECLS)
+        if (GetScImport().getImportFlags() & SvXMLImportFlags::FONTDECLS)
             pContext = GetScImport().CreateFontDeclsContext(nPrefix, rLocalName, xAttrList);
         break;
     case XML_TOK_DOC_STYLES:
-        if (GetScImport().getImportFlags() & IMPORT_STYLES)
+        if (GetScImport().getImportFlags() & SvXMLImportFlags::STYLES)
             pContext = GetScImport().CreateStylesContext( rLocalName, xAttrList, false);
         break;
     case XML_TOK_DOC_AUTOSTYLES:
-        if (GetScImport().getImportFlags() & IMPORT_AUTOSTYLES)
+        if (GetScImport().getImportFlags() & SvXMLImportFlags::AUTOSTYLES)
             pContext = GetScImport().CreateStylesContext( rLocalName, xAttrList, true);
         break;
     case XML_TOK_DOC_MASTERSTYLES:
-        if (GetScImport().getImportFlags() & IMPORT_MASTERSTYLES)
+        if (GetScImport().getImportFlags() & SvXMLImportFlags::MASTERSTYLES)
             pContext = new ScXMLMasterStylesContext( GetImport(), nPrefix, rLocalName,
             xAttrList );
         break;
@@ -376,16 +376,16 @@ SvXMLImportContext *ScXMLDocContext_Impl::CreateChildContext( sal_uInt16 nPrefix
         DBG_WARNING("XML_TOK_DOC_META: should not have come here, maybe document is invalid?");
         break;
     case XML_TOK_DOC_SCRIPTS:
-        if (GetScImport().getImportFlags() & IMPORT_SCRIPTS)
+        if (GetScImport().getImportFlags() & SvXMLImportFlags::SCRIPTS)
             pContext = GetScImport().CreateScriptContext( rLocalName );
         break;
     case XML_TOK_DOC_BODY:
-        if (GetScImport().getImportFlags() & IMPORT_CONTENT)
+        if (GetScImport().getImportFlags() & SvXMLImportFlags::CONTENT)
             pContext = new ScXMLBodyContext_Impl( GetScImport(), nPrefix,
             rLocalName, xAttrList );
         break;
     case XML_TOK_DOC_SETTINGS:
-        if (GetScImport().getImportFlags() & IMPORT_SETTINGS)
+        if (GetScImport().getImportFlags() & SvXMLImportFlags::SETTINGS)
             pContext = new XMLDocumentSettingsContext(GetScImport(), nPrefix, rLocalName, xAttrList );
         break;
     }
@@ -1980,7 +1980,7 @@ SvXMLImportContext *ScXMLImport::CreateContext( sal_uInt16 nPrefix,
 
 ScXMLImport::ScXMLImport(
     const ::com::sun::star::uno::Reference< ::com::sun::star::uno::XComponentContext > xContext,
-    OUString const & implementationName, sal_uInt16 nImportFlag)
+    OUString const & implementationName, SvXMLImportFlags nImportFlag)
 :   SvXMLImport( xContext, implementationName, nImportFlag ),
     pDoc( NULL ),
     pChangeTrackingImportHelper(NULL),
@@ -2289,7 +2289,7 @@ SvXMLImportContext *ScXMLImport::CreateMetaContext(
 {
     SvXMLImportContext* pContext = NULL;
 
-    if (getImportFlags() & IMPORT_META)
+    if (getImportFlags() & SvXMLImportFlags::META)
     {
         uno::Reference<document::XDocumentPropertiesSupplier> xDPS(
             GetModel(), uno::UNO_QUERY_THROW);
@@ -2431,7 +2431,7 @@ void ScXMLImport::InsertStyles()
     GetStyles()->CopyStylesToDoc(true);
 
     // if content is going to be loaded with the same import, set bLatinDefaultStyle flag now
-    if ( getImportFlags() & IMPORT_CONTENT )
+    if ( getImportFlags() & SvXMLImportFlags::CONTENT )
         ExamineDefaultStyle();
 }
 
@@ -2987,11 +2987,11 @@ throw( ::com::sun::star::xml::sax::SAXException, ::com::sun::star::uno::RuntimeE
 
     // if content and styles are loaded with separate imports,
     // set bLatinDefaultStyle flag at the start of the content import
-    sal_uInt16 nFlags = getImportFlags();
-    if ( ( nFlags & IMPORT_CONTENT ) && !( nFlags & IMPORT_STYLES ) )
+    SvXMLImportFlags nFlags = getImportFlags();
+    if ( ( nFlags & SvXMLImportFlags::CONTENT ) && !( nFlags & SvXMLImportFlags::STYLES ) )
         ExamineDefaultStyle();
 
-    if (getImportFlags() & IMPORT_CONTENT)
+    if (getImportFlags() & SvXMLImportFlags::CONTENT)
     {
         if (GetModel().is())
         {
@@ -3178,7 +3178,7 @@ void SAL_CALL ScXMLImport::endDocument()
           std::exception)
 {
     ScXMLImport::MutexGuard aGuard(*this);
-    if (getImportFlags() & IMPORT_CONTENT)
+    if (getImportFlags() & SvXMLImportFlags::CONTENT)
     {
         if (GetModel().is())
         {

sc/source/filter/xml/xmlimprt.hxx
@@ -982,7 +982,7 @@ public:
     // #110680#
     ScXMLImport(
         const ::com::sun::star::uno::Reference< ::com::sun::star::uno::XComponentContext > xContext,
-        OUString const & implementationName, sal_uInt16 nImportFlag);
+        OUString const & implementationName, SvXMLImportFlags nImportFlag);
 
     virtual ~ScXMLImport() throw();
 

starmath/source/mathmlimport.cxx
@@ -397,7 +397,7 @@ sal_uLong SmXMLImportWrapper::ReadThroughComponent(
 
 SmXMLImport::SmXMLImport(
     const ::com::sun::star::uno::Reference< ::com::sun::star::uno::XComponentContext > xContext,
-    OUString const & implementationName, sal_uInt16 nImportFlags)
+    OUString const & implementationName, SvXMLImportFlags nImportFlags)
 :   SvXMLImport( xContext, implementationName, nImportFlags ),
     pPresLayoutElemTokenMap(0),
     pPresLayoutAttrTokenMap(0),
@@ -439,7 +439,7 @@ uno::Reference< uno::XInterface > SAL_CALL SmXMLImport_createInstance(
     const uno::Reference< lang::XMultiServiceFactory > & rSMgr)
     throw( uno::Exception )
 {
-    return (cppu::OWeakObject*)new SmXMLImport(comphelper::getComponentContext(rSMgr), SmXMLImport_getImplementationName(), IMPORT_ALL);
+    return (cppu::OWeakObject*)new SmXMLImport(comphelper::getComponentContext(rSMgr), SmXMLImport_getImplementationName(), SvXMLImportFlags::ALL);
 }
 
 
@@ -461,7 +461,7 @@ uno::Reference< uno::XInterface > SAL_CALL SmXMLImportMeta_createInstance(
     const uno::Reference< lang::XMultiServiceFactory > & rSMgr)
 throw( uno::Exception )
 {
-    return (cppu::OWeakObject*)new SmXMLImport( comphelper::getComponentContext(rSMgr), SmXMLImportMeta_getImplementationName(), IMPORT_META );
+    return (cppu::OWeakObject*)new SmXMLImport( comphelper::getComponentContext(rSMgr), SmXMLImportMeta_getImplementationName(), SvXMLImportFlags::META );
 }
 
 
@@ -483,7 +483,7 @@ uno::Reference< uno::XInterface > SAL_CALL SmXMLImportSettings_createInstance(
     const uno::Reference< lang::XMultiServiceFactory > & rSMgr)
     throw( uno::Exception )
 {
-    return (cppu::OWeakObject*)new SmXMLImport( comphelper::getComponentContext(rSMgr), SmXMLImportSettings_getImplementationName(), IMPORT_SETTINGS );
+    return (cppu::OWeakObject*)new SmXMLImport( comphelper::getComponentContext(rSMgr), SmXMLImportSettings_getImplementationName(), SvXMLImportFlags::SETTINGS );
 }
 
 sal_Int64 SAL_CALL SmXMLImport::getSomething(

commit fe70628989fcbac6b43eea118b63b590dda39b79

tdf#31231: properly check for SvXMLImportFlags::ALL
A regression from commit dc28e90d200a839d4017d548217ee5ce8a23f848

Change-Id: If7fde863e7adffe281a77d83372fd768812ccfa7

sc/source/filter/xml/XMLTableShapeResizer.cxx
@@ -93,7 +93,7 @@ void ScMyOLEFixer::CreateChartListener(ScDocument* pDoc,
         //for loading binary files e.g.
         //if we have the flat filter we need to set the dirty flag thus the visible charts get repainted
         //otherwise the charts keep their first visual representation which was created at a moment where the calc itself was not loaded completely and is therefore incorrect
-        if( rImport.getImportFlags() & SvXMLImportFlags::ALL )
+        if( (rImport.getImportFlags() & SvXMLImportFlags::ALL) == SvXMLImportFlags::ALL )
             pCL->SetDirty( true );
         else
         {