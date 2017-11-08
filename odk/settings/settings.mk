71183661e66d2e2897c999f39772a324c28a3bf7
Rene Engelhard <rene@debian.org>
2017-11-08 19:47:17 +0100

    fix also odk/settings/settings.mk wrt Java 9 to find jawt
    since we can't use HAVE_JAVA= here since it's in a different environment and
    we don't know the Java version just do the -L. Doesn't do harm if Java
    has the "old" path
    
    Change-Id: I2f912c417597d3b14e570f362f2cfc2df5954108

--- odk/settings/settings.mk
+++ odk/settings/settings.mk
@@ -376,7 +376,7 @@ COMP_LINK_FLAGS=$(LIBRARY_LINK_FLAGS)
 
 EXE_LINK_FLAGS=-Wl,--allow-shlib-undefined -Wl,-export-dynamic -Wl,-z,defs -Wl,--no-whole-archive
 LINK_LIBS=-L"$(OUT)/lib" -L"$(OO_SDK_HOME)/lib" -L"$(OO_SDK_URE_LIB_DIR)"
-LINK_JAVA_LIBS=-L"$(OO_SDK_JAVA_HOME)/jre/lib/$(JAVA_PROC_TYPE)"
+LINK_JAVA_LIBS=-L"$(OO_SDK_JAVA_HOME)/jre/lib/$(JAVA_PROC_TYPE)" -L"$(OO_SDK_JAVA_HOME)/lib"
 
 URE_MISC=$(OFFICE_PROGRAM_PATH)
 
