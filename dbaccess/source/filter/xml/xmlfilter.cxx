4cc31f50ffb84786f9aa888bc57d62cd9642a45c
Mike Kaganski <mike.kaganski@collabora.com>
2017-11-08 14:22:44 +0200

    ODBFilter::filter: Also leave window wait on exception
    Change-Id: I6bb759c583e15f229bc2afa178a1d1d90d8315ef

--- dbaccess/source/filter/xml/xmlfilter.cxx
+++ dbaccess/source/filter/xml/xmlfilter.cxx
@@ -244,29 +244,40 @@ css::uno::Reference< css::uno::XInterface >
     return static_cast< XServiceInfo* >(new ODBFilter( comphelper::getComponentContext(_rxORB)));
 }
 
+namespace {
+class FocusWindowWaitGuard
+{
+public:
+    FocusWindowWaitGuard()
+    {
+        SolarMutexGuard aGuard;
+        vcl::Window* pFocusWindow = Application::GetFocusWindow();
+        mxWindow = VCLUnoHelper::GetInterface(pFocusWindow);
+        if (pFocusWindow)
+            pFocusWindow->EnterWait();
+    }
+    ~FocusWindowWaitGuard()
+    {
+        if (mxWindow.is())
+        {
+            SolarMutexGuard aGuard;
+            VclPtr<vcl::Window> pFocusWindow = VCLUnoHelper::GetWindow(mxWindow);
+            if (pFocusWindow)
+                pFocusWindow->LeaveWait();
+        }
+    }
+private:
+    uno::Reference< css::awt::XWindow > mxWindow;
+};
+}
 
 sal_Bool SAL_CALL ODBFilter::filter( const Sequence< PropertyValue >& rDescriptor )
 {
-    uno::Reference< css::awt::XWindow > xWindow;
-    {
-        SolarMutexGuard aGuard;
-        vcl::Window*     pFocusWindow = Application::GetFocusWindow();
-        xWindow = VCLUnoHelper::GetInterface( pFocusWindow );
-        if( pFocusWindow )
-            pFocusWindow->EnterWait();
-    }
+    FocusWindowWaitGuard aWindowFocusGuard;
     bool    bRet = false;
 
     if ( GetModel().is() )
         bRet = implImport( rDescriptor );
-
-    if ( xWindow.is() )
-    {
-        SolarMutexGuard aGuard;
-        VclPtr<vcl::Window> pFocusWindow = VCLUnoHelper::GetWindow( xWindow );
-        if ( pFocusWindow )
-            pFocusWindow->LeaveWait();
-    }
 
     return bRet;
 }
