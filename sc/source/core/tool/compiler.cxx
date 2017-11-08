82094d1a77bc5ad8d732f640c1a05a6b5339b666
Caolán McNamara <caolanm@redhat.com>
2017-11-08 17:05:59 +0000

    ofz: bad mnRangeOpPosInSymbol
    Change-Id: Ieae5a81a1b475fd56cce76c43dab11c2d9fcbe6c
    Reviewed-on: https://gerrit.libreoffice.org/44499
    Reviewed-by: Eike Rathke <erack@redhat.com>
    Tested-by: Jenkins <ci@libreoffice.org>
    (cherry picked from commit f17640a95b4c65809f8573c778a293c836a847cd)

--- sc/source/core/tool/compiler.cxx
+++ sc/source/core/tool/compiler.cxx
@@ -2678,6 +2678,8 @@ Label_MaskStateMachine:
         {
             SetError( FormulaError::StringOverflow );
             nLen = MAXSTRLEN-1;
+            if (mnRangeOpPosInSymbol > nLen)
+                mnRangeOpPosInSymbol = -1;
         }
         lcl_UnicodeStrNCpy( cSymbol, aSymbol.getStr(), nLen );
         pSym = &cSymbol[nLen];
