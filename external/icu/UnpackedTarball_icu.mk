7637c1534b81e58459292d202c49b58f50ab7688
Eike Rathke <erack@redhat.com>
2017-11-08 17:17:44 +0100

    Add ICU changeset-40324 fix for CVE-2017-14952
    (cherry picked from commit cebe1279665960b732c9cb05c18e481c33e778bb)
    
     Conflicts:
    	external/icu/UnpackedTarball_icu.mk
    
    Change-Id: Ia457669c5ec6ef5c568f4550c44ef5df32a4be66
    Reviewed-on: https://gerrit.libreoffice.org/44492
    Reviewed-by: Michael Stahl <mstahl@redhat.com>
    Tested-by: Michael Stahl <mstahl@redhat.com>

--- external/icu/UnpackedTarball_icu.mk
+++ external/icu/UnpackedTarball_icu.mk
@@ -27,6 +27,7 @@ $(eval $(call gb_UnpackedTarball_add_patches,icu,\
 	$(if $(filter-out ANDROID,$(OS)),external/icu/icu4c-icudata-stdlibs.diff) \
 	external/icu/khmerbreakengine.patch \
 	external/icu/icu4c-changeset-39671.patch.1 \
+	external/icu/icu4c-changeset-40324.patch.1 \
 ))
 
 $(eval $(call gb_UnpackedTarball_add_file,icu,source/data/brkitr/khmerdict.dict,external/icu/khmerdict.dict))
