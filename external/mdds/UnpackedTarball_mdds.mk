0b4d0c87fe201b7cc7f49b2dc14c134c95558a30
Kohei Yoshida <kohei.yoshida@gmail.com>
2017-11-10 18:13:52 -0500

    Update mdds to 1.3.1.
    This newer version incorporates the change in ofz-4048.patch.1.
    
    Change-Id: Iaccbf2694bbd752f9949c491219aa5ab8caa45fe
    Reviewed-on: https://gerrit.libreoffice.org/44615
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Kohei Yoshida <libreoffice@kohei.us>

--- external/mdds/UnpackedTarball_mdds.mk
+++ external/mdds/UnpackedTarball_mdds.mk
@@ -14,7 +14,6 @@ $(eval $(call gb_UnpackedTarball_set_tarball,mdds,$(MDDS_TARBALL)))
 $(eval $(call gb_UnpackedTarball_set_patchlevel,mdds,0))
 
 $(eval $(call gb_UnpackedTarball_add_patches,mdds,\
-	external/mdds/ofz-4048.patch.1 \
 ))
 
 # vim: set noet sw=4 ts=4:
