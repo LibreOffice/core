c66ac9aecb9758a5eea43b12ca7152dfd4037705
Caolán McNamara <caolanm@redhat.com>
2017-11-13 10:47:59 +0000

    Resolves: tdf#113366 Contour editor does not open
    reportedly
    
    This reverts commit 15869ea2dcd8a6b33044d08eef6fcaf8ca54579d.
    
    Change-Id: I930567f8dd9701a14ab02f42f89a629949453f22

--- vcl/source/window/floatwin.cxx
+++ vcl/source/window/floatwin.cxx
@@ -596,6 +596,11 @@ void FloatingWindow::LogicInvalidate(const tools::Rectangle* /*pRectangle*/)
 
 void FloatingWindow::StateChanged( StateChangedType nType )
 {
+    if (nType == StateChangedType::InitShow)
+    {
+        DoInitialLayout();
+    }
+
     SystemWindow::StateChanged( nType );
     Dialog* pParentDlg = GetParentDialog();
     if (pParentDlg && nType == StateChangedType::InitShow && IsVisible())
