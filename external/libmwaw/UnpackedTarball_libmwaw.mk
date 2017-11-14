40de00e5cbd7278b89f1f155896223864f91d1af
David Tardon <dtardon@redhat.com>
2017-11-14 20:07:09 +0100

    upload libmwaw 0.3.13
    Change-Id: I9af801efd81bbe64117de52359491c05069c1947

--- external/libmwaw/UnpackedTarball_libmwaw.mk
+++ external/libmwaw/UnpackedTarball_libmwaw.mk
@@ -37,8 +37,4 @@ $(eval $(call gb_UnpackedTarball_add_patches,libmwaw, \
 ))
 endif
 
-$(eval $(call gb_UnpackedTarball_add_patches,libmwaw, \
-    external/libmwaw/0001-make-MWAW_FALLTHROUGH-decl.-more-verbose-again.patch.1 \
-))
-
 # vim: set noet sw=4 ts=4:
