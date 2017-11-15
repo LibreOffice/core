c9ef21e861de90c5ba484a742da9249f9a04d8a0
Mark Vecsernyes <kurtaazz@gmail.com>
2017-11-15 19:47:59 +0100

    tdf#111740 fix: Wrong preview of graphic bullet symbol in Impress
    Change-Id: I43f23077ef8b692262c4e18ba89729e80a40b788

--- cui/source/tabpages/numpages.cxx
+++ cui/source/tabpages/numpages.cxx
@@ -2233,7 +2233,7 @@ void SvxNumOptionsTabPage::EditModifyHdl_Impl( Edit* pEdit )
 }
 
 static sal_uInt16 lcl_DrawGraphic(VirtualDevice* pVDev, const SvxNumberFormat &rFmt, sal_uInt16 nXStart,
-                        sal_uInt16 nYStart, sal_uInt16 nDivision)
+                        sal_uInt16 nYMiddle, sal_uInt16 nDivision)
 {
     const SvxBrushItem* pBrushItem = rFmt.GetBrush();
     sal_uInt16 nRet = 0;
@@ -2246,7 +2246,7 @@ static sal_uInt16 lcl_DrawGraphic(VirtualDevice* pVDev, const SvxNumberFormat &r
             aGSize.Width() /= nDivision;
             nRet = (sal_uInt16)aGSize.Width();
             aGSize.Height() /= nDivision;
-            pGrf->Draw( pVDev, Point(nXStart,nYStart - ( aGSize.Height() / 2) ),
+            pGrf->Draw( pVDev, Point(nXStart,nYMiddle - ( aGSize.Height() / 2) ),
                     pVDev->PixelToLogic( aGSize ) );
         }
     }
@@ -2329,6 +2329,7 @@ void SvxNumberingPreview::Paint(vcl::RenderContext& rRenderContext, const ::tool
         if (pActNum->GetLevelCount() < 10)
             nXStep /= 2;
         sal_uInt16 nYStart = 4;
+        sal_uInt16 nYMiddle;
         // the whole height mustn't be used for a single level
         sal_uInt16 nYStep = sal::static_int_cast<sal_uInt16>((aSize.Height() - 6)/ (pActNum->GetLevelCount() > 1 ? pActNum->GetLevelCount() : 5));
 
@@ -2392,13 +2393,17 @@ void SvxNumberingPreview::Paint(vcl::RenderContext& rRenderContext, const ::tool
                 }
 
                 sal_uInt16 nBulletWidth = 0;
+
+
                 if (SVX_NUM_BITMAP == (rFmt.GetNumberingType() &(~LINK_TOKEN)))
                 {
-                    nBulletWidth = rFmt.IsShowSymbol() ? lcl_DrawGraphic(pVDev.get(), rFmt, nNumberXPos, nYStart, nWidthRelation) : 0;
+                    nYMiddle = nYStart + ( nFontHeight / 2 );
+                    nBulletWidth = rFmt.IsShowSymbol() ? lcl_DrawGraphic(pVDev.get(), rFmt, nNumberXPos, nYMiddle, nWidthRelation) : 0;
                 }
                 else if (SVX_NUM_CHAR_SPECIAL == rFmt.GetNumberingType())
                 {
-                    nBulletWidth =  rFmt.IsShowSymbol() ? lcl_DrawBullet(pVDev.get(), rFmt, nNumberXPos, nYStart, aStdFont.GetFontSize()) : 0;
+                    nYMiddle = nYStart + ( nFontHeight / 2 );
+                    nBulletWidth =  rFmt.IsShowSymbol() ? lcl_DrawBullet(pVDev.get(), rFmt, nNumberXPos, nYMiddle, aStdFont.GetFontSize()) : 0;
                 }
                 else
                 {
@@ -2513,7 +2518,8 @@ void SvxNumberingPreview::Paint(vcl::RenderContext& rRenderContext, const ::tool
                 {
                     if (rFmt.IsShowSymbol())
                     {
-                        nTextOffset = lcl_DrawGraphic(pVDev.get(), rFmt, nXStart, nYStart + ( nFontHeight / 2 ), nWidthRelation);
+                        nYMiddle = nYStart + ( nFontHeight / 2 );
+                        nTextOffset = lcl_DrawGraphic(pVDev.get(), rFmt, nXStart, nYMiddle, nWidthRelation);
                         nTextOffset = nTextOffset + nXStep;
                     }
                 }
