283aa46962948cf4f7244c875d2071b12e10d9b3
Miklos Vajna <vmiklos@collabora.co.uk>
2017-11-16 11:24:05 +0100

    vcl: fix typo
    Change-Id: I696e3986348034b61b3bbf13fa6db39f2a8f43e0

--- vcl/inc/pdfread.hxx
+++ vcl/inc/pdfread.hxx
@@ -18,7 +18,7 @@ namespace vcl
 
 /// Imports a PDF stream into rGraphic as a GDIMetaFile.
 VCL_DLLPUBLIC bool ImportPDF(SvStream& rStream, Bitmap& rBitmap,
-                             css::uno::Sequence<sal_Int8>& rPdfFata,
+                             css::uno::Sequence<sal_Int8>& rPdfData,
                              sal_uInt64 nPos = STREAM_SEEK_TO_BEGIN,
                              sal_uInt64 nSize = STREAM_SEEK_TO_END);
 VCL_DLLPUBLIC bool ImportPDF(SvStream& rStream, Graphic& rGraphic);
