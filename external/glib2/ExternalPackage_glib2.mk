daee5fc5569c1807f5c8dae502d305eb06141e8b
Thorsten Behrens <Thorsten.Behrens@CIB.de>
2017-11-17 04:37:29 +0100

    gpg4libre: add glib2 dependency for Windows
    Change-Id: Ib6b5393323a4dbfe94ea89689ac9170f8afa3899
    Reviewed-on: https://gerrit.libreoffice.org/44856
    Reviewed-by: Siegmund Gorr <siegmund.gorr@cib.de>
    Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>
    Tested-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>

---
+++ external/glib2/ExternalPackage_glib2.mk
@@ -0,0 +1,14 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_ExternalPackage_ExternalPackage,glib2,glib2))
+
+$(eval $(call gb_ExternalPackage_use_external_project,glib2,glib2))
+
+# vim: set noet sw=4 ts=4:
---
+++ external/glib2/Module_glib2.mk
@@ -0,0 +1,18 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_Module_Module,glib2))
+
+$(eval $(call gb_Module_add_targets,glib2,\
+	ExternalPackage_glib2 \
+	ExternalProject_glib2 \
+	UnpackedTarball_glib2 \
+))
+
+# vim: set noet sw=4 ts=4:
