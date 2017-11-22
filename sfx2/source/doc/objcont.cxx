LibreOffice/core

commit 389da66dfc96d06c407bff156c4ea21e940c5e06

remove unused uno::Reference vars
found by temporarily marking Reference as SAL_WARN_UNUSED.

Change-Id: I18809b62654467f890016adcc92576980ced393b
Reviewed-on: https://gerrit.libreoffice.org/37511
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sfx2/source/doc/objcont.cxx
@@ -385,7 +385,6 @@ void SfxObjectShell::UpdateFromTemplate_Impl(  )
         return;
 
     // only for own storage formats
-    uno::Reference< embed::XStorage > xDocStor = pFile->GetStorage();
     if ( !pFile->GetFilter() || !pFile->GetFilter()->IsOwnFormat() )
         return;
 

commit e8da485c0abb705f305f65a130c02fc5b638fa93

tdf#113935 Switching from read-only to edit mode slow
Regression introduced by

    commit 389da66dfc96d06c407bff156c4ea21e940c5e06
    remove unused uno::Reference vars

I'm guessing this variable keeps some kind of cache alive which prevents
us from re-parsing the PDF file when we switch to edit mode - which is
clearly what we are doing when I remove the line.

Change-Id: Iea2cf9640d876028a78806e717b930d4a063dc9b

sfx2/source/doc/objcont.cxx
@@ -383,6 +383,11 @@ void SfxObjectShell::UpdateFromTemplate_Impl(  )
         // update only for documents loaded from the local file system
         return;
 
+    // tdf#113935 - do not remove this line - somehow, it makes the process
+    // of switching from viewing a read-only document to opening it in writable
+    // mode much faster.
+    uno::Reference< embed::XStorage > xDocStor = pFile->GetStorage();
+
     // only for own storage formats
     if ( !pFile->GetFilter() || !pFile->GetFilter()->IsOwnFormat() )
         return;