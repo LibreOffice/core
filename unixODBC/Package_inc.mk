19c13a1a1cc3f275cd2b11bb82afe0a3b75392d6
Damjan Jovanovic <damjan@apache.org>
2017-11-27 05:09:40 +0000

    Port main/unixOBDC to gbuild.
    Patch by: me

---
+++ unixODBC/Package_inc.mk
@@ -0,0 +1,30 @@
+#**************************************************************
+#  
+#  Licensed to the Apache Software Foundation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#  
+#    http://www.apache.org/licenses/LICENSE-2.0
+#  
+#  Unless required by applicable law or agreed to in writing,
+#  software distributed under the License is distributed on an
+#  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+#  KIND, either express or implied.  See the License for the
+#  specific language governing permissions and limitations
+#  under the License.
+#  
+#**************************************************************
+
+
+
+$(eval $(call gb_Package_Package,unixODBC_inc,$(SRCDIR)/unixODBC))
+
+$(eval $(call gb_Package_add_file,unixODBC_inc,inc/external/odbc/iodbcunix.h,inc/iodbcunix.h))
+$(eval $(call gb_Package_add_file,unixODBC_inc,inc/external/odbc/sql.h,inc/sql.h))
+$(eval $(call gb_Package_add_file,unixODBC_inc,inc/external/odbc/sqlext.h,inc/sqlext.h))
+$(eval $(call gb_Package_add_file,unixODBC_inc,inc/external/odbc/sqltypes.h,inc/sqltypes.h))
+$(eval $(call gb_Package_add_file,unixODBC_inc,inc/external/odbc/sqlucode.h,inc/sqlucode.h))
