e5927b78eea34e28e0c26bd9fe77b4fc7f7287a6
Caolán McNamara <caolanm@redhat.com>
2017-11-28 16:56:41 +0000

    add xlsx fuzzer
    Change-Id: Icce1317dc3ccfb598ce07ee61eddd873f1f98222

--- Repository.mk
+++ Repository.mk
@@ -138,6 +138,7 @@ $(eval $(call gb_Helper_register_executables_for_install,OOO,brand, \
 	$(call gb_Helper_optional,FUZZERS,wksfuzzer) \
 	$(call gb_Helper_optional,FUZZERS,diffuzzer) \
 	$(call gb_Helper_optional,FUZZERS,docxfuzzer) \
+	$(call gb_Helper_optional,FUZZERS,xlsxfuzzer) \
 	$(if $(filter-out ANDROID IOS MACOSX WNT,$(OS)),oosplash) \
 	soffice_bin \
 	$(if $(filter DESKTOP,$(BUILD_TYPE)),unopkg_bin) \
---
+++ vcl/Executable_xlsxfuzzer.mk
@@ -0,0 +1,49 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+include $(SRCDIR)/vcl/commonfuzzer.mk
+
+$(eval $(call gb_Executable_Executable,xlsxfuzzer))
+
+$(eval $(call gb_Executable_use_api,xlsxfuzzer,\
+    offapi \
+    udkapi \
+))
+
+$(eval $(call gb_Executable_use_externals,xlsxfuzzer,\
+	$(fuzzer_externals) \
+))
+
+$(eval $(call gb_Executable_set_include,xlsxfuzzer,\
+    $$(INCLUDE) \
+    -I$(SRCDIR)/vcl/inc \
+))
+
+$(eval $(call gb_Executable_use_libraries,xlsxfuzzer,\
+    $(fuzzer_calc_libraries) \
+    $(fuzzer_core_libraries) \
+))
+
+$(eval $(call gb_Executable_use_static_libraries,xlsxfuzzer,\
+    findsofficepath \
+    ulingu \
+    fuzzer_calc \
+    fuzzerstubs \
+))
+
+$(eval $(call gb_Executable_add_exception_objects,xlsxfuzzer,\
+	vcl/workben/xlsxfuzzer \
+))
+
+$(eval $(call gb_Executable_add_libs,xlsxfuzzer,\
+	-lFuzzingEngine \
+))
+
+# vim: set noet sw=4 ts=4:
