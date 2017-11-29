da6aad10897300dc2dbfc0141ad40e7097c6f6f4
Caolán McNamara <caolanm@redhat.com>
2017-11-29 10:44:09 +0000

    add pptx fuzzer
    Change-Id: I50495496914929f9279a455d1acbffab833bf3f5

---
+++ vcl/Executable_pptxfuzzer.mk
@@ -0,0 +1,49 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+include $(SRCDIR)/vcl/commonfuzzer.mk
+
+$(eval $(call gb_Executable_Executable,pptxfuzzer))
+
+$(eval $(call gb_Executable_use_api,pptxfuzzer,\
+    offapi \
+    udkapi \
+))
+
+$(eval $(call gb_Executable_use_externals,pptxfuzzer,\
+	$(fuzzer_externals) \
+))
+
+$(eval $(call gb_Executable_set_include,pptxfuzzer,\
+    $$(INCLUDE) \
+    -I$(SRCDIR)/vcl/inc \
+))
+
+$(eval $(call gb_Executable_use_libraries,pptxfuzzer,\
+    $(fuzzer_draw_libraries) \
+    $(fuzzer_core_libraries) \
+))
+
+$(eval $(call gb_Executable_use_static_libraries,pptxfuzzer,\
+    findsofficepath \
+    ulingu \
+    fuzzer_draw \
+    fuzzerstubs \
+))
+
+$(eval $(call gb_Executable_add_exception_objects,pptxfuzzer,\
+	vcl/workben/pptxfuzzer \
+))
+
+$(eval $(call gb_Executable_add_libs,pptxfuzzer,\
+	-lFuzzingEngine \
+))
+
+# vim: set noet sw=4 ts=4:
