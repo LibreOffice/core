ff03b6ca2b28e84d89bd5f962cd3cf6c5111ab56
Heiko Tietze <tietze.heiko@gmail.com>
2017-11-29 11:09:35 +0100

    tdf#113343 Floating charmap window must not have a context menu
    Change-Id: I76c567047b0033ae766ab6c5161c96416e478e8e

--- include/sfx2/charwin.hxx
+++ include/sfx2/charwin.hxx
@@ -27,7 +27,6 @@ class SFX2_DLLPUBLIC SvxCharView : public Control
 {
 public:
     SvxCharView(vcl::Window* pParent);
-
     void            SetFont( const vcl::Font& rFont );
     void            SetText( const OUString& rText ) override;
     void            InsertCharToDoc();
@@ -55,6 +54,7 @@ private:
     long            mnY;
     Point           maPosition;
     vcl::Font       maFont;
+    bool            bIsFloating;
 
     Link<SvxCharView*, void> maMouseClickHdl;
     Link<SvxCharView*, void> maClearClickHdl;
