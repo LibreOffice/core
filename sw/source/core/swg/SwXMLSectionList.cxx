LibreOffice/core

commit 346abc6785117ad0a3353eec2bc2f4d1b8db9fb7

sw: move SvXMLSectionListContext out of header file
Change-Id: I18417484a35d67bde3ec2690d834589c3751005f

sw/source/core/swg/SwXMLSectionList.cxx
@@ -18,12 +18,30 @@
  */
 
 #include <SwXMLSectionList.hxx>
+#include <xmloff/xmlictxt.hxx>
 #include <xmloff/nmspmap.hxx>
 #include <xmloff/xmlnmspe.hxx>
 #include <vector>
 
 using namespace ::com::sun::star;
 using namespace ::xmloff::token;
+
+class SvXMLSectionListContext : public SvXMLImportContext
+{
+private:
+    SwXMLSectionList & m_rImport;
+
+public:
+    SvXMLSectionListContext(SwXMLSectionList& rImport,
+           sal_uInt16 nPrefix,
+           const OUString& rLocalName,
+           const uno::Reference<xml::sax::XAttributeList> & xAttrList);
+    virtual SvXMLImportContextRef CreateChildContext(sal_uInt16 nPrefix,
+           const OUString& rLocalName,
+           const uno::Reference<xml::sax::XAttributeList> & xAttrList) override;
+    virtual ~SvXMLSectionListContext() override;
+};
+
 
 SwXMLSectionList::SwXMLSectionList(
     const uno::Reference< uno::XComponentContext >& rContext,
@@ -80,7 +98,7 @@ SvXMLSectionListContext::SvXMLSectionListContext(
    const OUString& rLocalName,
    const uno::Reference<   xml::sax::XAttributeList > & ) :
    SvXMLImportContext ( rImport, nPrefix, rLocalName ),
-   rLocalRef(rImport)
+   m_rImport(rImport)
 {
 }
 
@@ -101,15 +119,15 @@ SvXMLImportContextRef SvXMLSectionListContext::CreateChildContext(
         {
             const OUString& rAttrName = xAttrList->getNameByIndex( i );
             OUString aLocalName;
-            sal_uInt16 nPrefx = rLocalRef.GetNamespaceMap().GetKeyByAttrName( rAttrName, &aLocalName);
+            sal_uInt16 nPrefx = m_rImport.GetNamespaceMap().GetKeyByAttrName(rAttrName, &aLocalName);
             if (XML_NAMESPACE_TEXT == nPrefx && IsXMLToken ( aLocalName, XML_NAME ) )
                 sName = xAttrList->getValueByIndex( i );
         }
         if ( !sName.isEmpty() )
-            rLocalRef.rSectionList.push_back( new OUString(sName) );
+            m_rImport.rSectionList.push_back( new OUString(sName) );
     }
 
-    pContext = new SvXMLSectionListContext (rLocalRef, nPrefix, rLocalName, xAttrList);
+    pContext = new SvXMLSectionListContext(m_rImport, nPrefix, rLocalName, xAttrList);
     return pContext;
 }
 SvXMLSectionListContext::~SvXMLSectionListContext()

sw/inc/SwXMLSectionList.hxx
@@ -19,7 +19,6 @@
 #ifndef INCLUDED_SW_INC_SWXMLSECTIONLIST_HXX
 #define INCLUDED_SW_INC_SWXMLSECTIONLIST_HXX
 
-#include <xmloff/xmlictxt.hxx>
 #include <xmloff/xmlimp.hxx>
 
 class SwXMLSectionList : public SvXMLImport
@@ -39,21 +38,6 @@ public:
 
     virtual ~SwXMLSectionList ( )
         throw() override;
-};
-
-class SvXMLSectionListContext : public SvXMLImportContext
-{
-private:
-    SwXMLSectionList & rLocalRef;
-public:
-    SvXMLSectionListContext ( SwXMLSectionList& rImport,
-                           sal_uInt16 nPrefix,
-                           const OUString& rLocalName,
-                           const css::uno::Reference< css::xml::sax::XAttributeList > & xAttrList );
-    virtual SvXMLImportContextRef CreateChildContext( sal_uInt16 nPrefix,
-                           const OUString& rLocalName,
-                           const css::uno::Reference< css::xml::sax::XAttributeList > & xAttrList ) override;
-    virtual ~SvXMLSectionListContext() override;
 };
 
 #endif

commit f8f0ca62d88aa84a8cc95b808275ee459757221a

Remove unnecessary override in SvXMLSectionListContext dtor
Fix clang [loplugin:unnecessaryoverride] error introduced with
commit 346abc6785117ad0a3353eec2bc2f4d1b8db9fb7.

Change-Id: I72ab13179ba770b95613fda9649580259b9240d7

sw/source/core/swg/SwXMLSectionList.cxx
@@ -39,7 +39,7 @@ public:
     virtual SvXMLImportContextRef CreateChildContext(sal_uInt16 nPrefix,
            const OUString& rLocalName,
            const uno::Reference<xml::sax::XAttributeList> & xAttrList) override;
-    virtual ~SvXMLSectionListContext() override;
+    virtual ~SvXMLSectionListContext();
 };
 
 class SwXMLParentContext : public SvXMLImportContext