LibreOffice/core

commit de2ac128da025502c533f8cede5862e054dd9c44

loplugin:useuniqueptr in dbaccess
Change-Id: Iecf167f2018537476107d0850bef6f6e8ae344a4
Reviewed-on: https://gerrit.libreoffice.org/41494
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

dbaccess/source/core/dataaccess/databasedocument.hxx
@@ -167,11 +167,11 @@ class ODatabaseDocument :public ModelDependentComponent             // ModelDepe
     typedef std::map< OUString, css::uno::Reference< css::frame::XUntitledNumbers > > TNumberedController;
     css::uno::Reference< css::ui::XUIConfigurationManager2>                                     m_xUIConfigurationManager;
 
-    ::comphelper::OInterfaceContainerHelper2                                                           m_aModifyListeners;
-    ::comphelper::OInterfaceContainerHelper2                                                           m_aCloseListener;
-    ::comphelper::OInterfaceContainerHelper2                                                           m_aStorageListeners;
+    ::comphelper::OInterfaceContainerHelper2                                                    m_aModifyListeners;
+    ::comphelper::OInterfaceContainerHelper2                                                    m_aCloseListener;
+    ::comphelper::OInterfaceContainerHelper2                                                    m_aStorageListeners;
 
-    DocumentEvents*                                                                             m_pEventContainer;
+    rtl::Reference<DocumentEvents>                                                              m_pEventContainer;
     ::rtl::Reference< DocumentEventExecutor >                                                   m_pEventExecutor;
     DocumentEventNotifier                                                                       m_aEventNotifier;
 

dbaccess/source/core/dataaccess/databasedocument.cxx
@@ -192,9 +192,6 @@ ODatabaseDocument::~ODatabaseDocument()
         acquire();
         dispose();
     }
-
-    delete m_pEventContainer;
-    m_pEventContainer = nullptr;
 }
 
 Any SAL_CALL ODatabaseDocument::queryInterface( const Type& _rType )
@@ -2003,7 +2000,7 @@ Reference< provider::XScriptProvider > SAL_CALL ODatabaseDocument::getScriptProv
 Reference< XNameReplace > SAL_CALL ODatabaseDocument::getEvents(  )
 {
     DocumentGuard aGuard( *this, DocumentGuard::MethodUsedDuringInit );
-    return m_pEventContainer;
+    return m_pEventContainer.get();
 }
 
 Reference< XInterface > ODatabaseDocument::getThis() const

dbaccess/source/ui/inc/unoadmin.hxx
@@ -42,7 +42,7 @@ protected:
     SfxItemPool*            m_pItemPool;            // item pool for the item set for the dialog
     std::vector<SfxPoolItem*>*
                             m_pItemPoolDefaults;    // pool defaults
-    ::dbaccess::ODsnTypeCollection*
+    std::unique_ptr<::dbaccess::ODsnTypeCollection>
                             m_pCollection;          // datasource type collection
 
     css::uno::Any           m_aInitialSelection;

dbaccess/source/ui/uno/unoadmin.cxx
@@ -45,11 +45,9 @@ ODatabaseAdministrationDialog::ODatabaseAdministrationDialog(const Reference< XC
     ,m_pDatasourceItems(nullptr)
     ,m_pItemPool(nullptr)
     ,m_pItemPoolDefaults(nullptr)
-    ,m_pCollection(nullptr)
 {
-
-    m_pCollection = new ::dbaccess::ODsnTypeCollection(_rxORB);
-    ODbAdminDialog::createItemSet(m_pDatasourceItems, m_pItemPool, m_pItemPoolDefaults, m_pCollection);
+    m_pCollection.reset( new ::dbaccess::ODsnTypeCollection(_rxORB) );
+    ODbAdminDialog::createItemSet(m_pDatasourceItems, m_pItemPool, m_pItemPoolDefaults, m_pCollection.get());
 }
 
 ODatabaseAdministrationDialog::~ODatabaseAdministrationDialog()
@@ -63,10 +61,6 @@ ODatabaseAdministrationDialog::~ODatabaseAdministrationDialog()
             ODbAdminDialog::destroyItemSet(m_pDatasourceItems, m_pItemPool, m_pItemPoolDefaults);
         }
     }
-
-    delete m_pCollection;
-    m_pCollection = nullptr;
-
 }
 
 void ODatabaseAdministrationDialog::implInitialize(const Any& _rValue)

commit ec505e8d935b69452c43a5dad0fa2869411052c7

Don't prevent ODatabaseDocument refcount from dropping to zero
...preventing the dtor from ever being called.  (DocumentEvents forwards its
acquire/release calls to its m_pData->rParent, i.e., the ODatabaseDocument, for
better or worse.)  This caused ODatabaseDocument instances to be leaked during
e.g. JunitTest_dbaccess_complex.  Regression introduced with
de2ac128da025502c533f8cede5862e054dd9c44 "loplugin:useuniqueptr in dbaccess".

Change-Id: Ida073c7e576b88e0d1d1a90253445e946e6eac99

compilerplugins/clang/refcounting.cxx
@@ -479,6 +479,8 @@ bool RefCounting::VisitFieldDecl(const FieldDecl * fieldDecl) {
             << fieldDecl->getSourceRange();
     }
 
+// Not in general (dbaccess::DocumentEvents, dbaccess/source/core/dataaccess/databasedocument.hxx):
+#if 0
     if (!firstTemplateParamType.isNull() && containsXInterfaceSubclass(firstTemplateParamType))
     {
         report(
@@ -490,6 +492,7 @@ bool RefCounting::VisitFieldDecl(const FieldDecl * fieldDecl) {
             << fieldDecl->getParent()
             << fieldDecl->getSourceRange();
     }
+#endif
 
     checkUnoReference(
         fieldDecl->getType(), fieldDecl,

dbaccess/source/core/dataaccess/databasedocument.hxx
@@ -22,6 +22,7 @@
 #include <sal/config.h>
 
 #include <map>
+#include <memory>
 
 #include <ModelImpl.hxx>
 #include "documenteventnotifier.hxx"
@@ -171,7 +172,7 @@ class ODatabaseDocument :public ModelDependentComponent             // ModelDepe
     ::comphelper::OInterfaceContainerHelper2                                                    m_aCloseListener;
     ::comphelper::OInterfaceContainerHelper2                                                    m_aStorageListeners;
 
-    rtl::Reference<DocumentEvents>                                                              m_pEventContainer;
+    std::unique_ptr<DocumentEvents>                                                             m_pEventContainer;
     ::rtl::Reference< DocumentEventExecutor >                                                   m_pEventExecutor;
     DocumentEventNotifier                                                                       m_aEventNotifier;
 

compilerplugins/clang/test/refcounting.cxx
@@ -13,11 +13,16 @@
 #include <boost/intrusive_ptr.hpp>
 #include <com/sun/star/uno/XInterface.hpp>
 
+// expected-no-diagnostics
+
 struct Foo
 {
+// Not in general (dbaccess::DocumentEvents, dbaccess/source/core/dataaccess/databasedocument.hxx):
+#if 0
     std::unique_ptr<css::uno::XInterface> m_foo1; // expected-error {{XInterface subclass 'com::sun::star::uno::XInterface' being managed via smart pointer, should be managed via uno::Reference, parent is 'Foo' [loplugin:refcounting]}}
     std::shared_ptr<css::uno::XInterface> m_foo2; // expected-error {{XInterface subclass 'com::sun::star::uno::XInterface' being managed via smart pointer, should be managed via uno::Reference, parent is 'Foo' [loplugin:refcounting]}}
     boost::intrusive_ptr<css::uno::XInterface> m_foo3; // expected-error {{XInterface subclass 'com::sun::star::uno::XInterface' being managed via smart pointer, should be managed via uno::Reference, parent is 'Foo' [loplugin:refcounting]}}
+#endif
 };
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */