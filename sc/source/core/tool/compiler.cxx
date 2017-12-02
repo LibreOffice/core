c8556086ad8560240d6e91cfd3fefad396288e36
Eike Rathke <erack@redhat.com>
2017-12-02 15:32:06 +0100

    ofz#4484 another stab at mnRangeOpPosInSymbol
    Commit cd0b749cc4b4f268d26639fb3c6cbf2128b2d7df still missed it by
    one position behind string length.
    
    Also move the check to end of i18n symbol parsing for all
    constellations.
    
     Conflicts:
    	sc/source/core/tool/compiler.cxx
    
    Change-Id: I7b5e1375fe711dcc3f659e558702de78893d25a5
    Reviewed-on: https://gerrit.libreoffice.org/45719
    Tested-by: Eike Rathke <erack@redhat.com>
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>

--- sc/source/core/tool/compiler.cxx
+++ sc/source/core/tool/compiler.cxx
@@ -2683,9 +2683,9 @@ Label_MaskStateMachine:
         {
             SetError( FormulaError::StringOverflow );
             nLen = MAXSTRLEN-1;
-            if (mnRangeOpPosInSymbol > nLen)
-                mnRangeOpPosInSymbol = -1;
         }
+        if (mnRangeOpPosInSymbol >= nLen)
+            mnRangeOpPosInSymbol = -1;
         lcl_UnicodeStrNCpy( cSymbol, aSymbol.getStr(), nLen );
         pSym = &cSymbol[nLen];
     }
