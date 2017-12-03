3e4fadaa5b36e37317dbfb4ce2f68640be12c9e1
Zdeněk Crhonek <zcrhonek@gmail.com>
2017-12-03 13:32:44 +0100

    VB tests (3)-don't pass whe all tests fail
    Change-Id: I7dd99a43a3d41c8ca31a070028a19bd032d06435

--- basic/qa/vba_tests/sgn.vb
+++ basic/qa/vba_tests/sgn.vb
@@ -6,7 +6,7 @@ Dim result As String
 
 Function doUnitTest() As String
 result = verify_SGN()
-If failCount <> 0 And passCount > 0 Then
+If failCount <> 0 or passCount = 0 Then
     doUnitTest = result
 Else
     doUnitTest = "OK"
@@ -23,30 +23,28 @@ Function verify_SGN() As String
     result = "Test Results" & Chr$(10) & "============" & Chr$(10)
 
     Dim testName As String
-    Dim TestDateTime As Date
-    Dim TestStr As String
     Dim date1, date2
     testName = "Test SGN function"
     On Error GoTo errorHandler
 
     date2 = 0
-    date1 = DateValue(0)
+    date1 = sgn(0)
     TestLog_ASSERT date1 = date2, "the return SGN is: " & date1
 
     date2 = -1
-    date1 = DateValue(-1)
+    date1 = sgn(-1)
     TestLog_ASSERT date1 = date2, "the return SGN is: " & date1
 
     date2 = 1
-    date1 = DateValue(1)
+    date1 = sgn(1)
     TestLog_ASSERT date1 = date2, "the return SGN is: " & date1
 
     date2 = 1
-    date1 = DateValue(50)
+    date1 = sgn(50)
     TestLog_ASSERT date1 = date2, "the return SGN is: " & date1
 
     date2 = -1
-    date1 = DateValue(-50)
+    date1 = sgn(-50)
     TestLog_ASSERT date1 = date2, "the return SGN is: " & date1
 
     result = result & Chr$(10) & "Tests passed: " & passCount & Chr$(10) & "Tests failed: " & failCount & Chr$(10)
