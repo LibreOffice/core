e3dd484b1219f5c7e0f223f8a38b7a003297ccad
Zdeněk Crhonek <zcrhonek@gmail.com>
2017-12-03 13:32:44 +0100

    VB tests (3)-don't pass whe all tests fail
    Change-Id: I7dd99a43a3d41c8ca31a070028a19bd032d06435

--- basic/qa/vba_tests/rgb.vb
+++ basic/qa/vba_tests/rgb.vb
@@ -7,7 +7,7 @@ Dim result As String
 
 Function doUnitTest() As String
 result = verify_testRGB()
-If failCount <> 0 And passCount > 0 Then
+If failCount <> 0 or passCount = 0 Then
     doUnitTest = result
 Else
     doUnitTest = "OK"
