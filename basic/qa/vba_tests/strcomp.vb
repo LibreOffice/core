4de48bcd285019d2ca7f5f39c330249d7d47c202
Zdeněk Crhonek <zcrhonek@gmail.com>
2017-12-04 08:04:52 +0100

    VBA tests (4) - don't pass when all tests fail
    Change-Id: Ib4f9b5df1cffe104ba27be95183d075f24f28bb3

--- basic/qa/vba_tests/strcomp.vb
+++ basic/qa/vba_tests/strcomp.vb
@@ -6,7 +6,7 @@ Dim result As String
 
 Function doUnitTest() As String
 result = verify_testSTRcomp()
-If failCount <> 0 And passCount > 0 Then
+If failCount <> 0 or passCount = 0 Then
     doUnitTest = result
 Else
     doUnitTest = "OK"
@@ -23,7 +23,6 @@ Function verify_testSTRcomp() As String
     result = "Test Results" & Chr$(10) & "============" & Chr$(10)
 
     Dim testName As String
-    Dim TestDateTime As Date
     Dim TestStr, TestStr1, TestStr2 As String
     Dim date1, date2
     testName = "Test STRcomp function"
