f84a0277115120cb5fb91a3f369e97f6708fc7b6
Thorsten Behrens <Thorsten.Behrens@CIB.de>
2017-12-04 16:17:18 +0100

    gpg4libre: add gpgme config header where gpgme.h is included
    Also pull in config_lgpl.h in that case.
    
    Squashed in is this fix, too:
    
      android: fix gpgme build
    
      No need to include config_lgpl.h when gpgme is disabled.
    
    Change-Id: Iae4e56ee8023066581c562c66305875a0fe15f37
    Reviewed-on: https://gerrit.libreoffice.org/45809
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>
    (cherry picked from commit 5daa364922ba75442e2aa834095e9bef16d141d9)

--- xmlsecurity/source/gpg/SecurityEnvironment.cxx
+++ xmlsecurity/source/gpg/SecurityEnvironment.cxx
@@ -7,6 +7,8 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+#include <config_gpgme.h>
+
 #include "SecurityEnvironment.hxx"
 #include "CertificateImpl.hxx"
 
--- xmlsecurity/source/gpg/xmlsignature_gpgimpl.cxx
+++ xmlsecurity/source/gpg/xmlsignature_gpgimpl.cxx
@@ -17,6 +17,8 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
+#include <config_gpgme.h>
+
 #include <sal/config.h>
 #include <rtl/uuid.h>
 #include <cppuhelper/supportsservice.hxx>
