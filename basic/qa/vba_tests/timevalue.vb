f3654d1d7abed676eac48d0576739d49b3a62460
Zdeněk Crhonek <zcrhonek@gmail.com>
2017-12-04 08:04:52 +0100

    VBA tests (4) - don't pass when all tests fail
    Change-Id: Ib4f9b5df1cffe104ba27be95183d075f24f28bb3

--- basic/qa/vba_tests/timevalue.vb
+++ basic/qa/vba_tests/timevalue.vb
@@ -6,7 +6,7 @@ Dim result As String
 
 Function doUnitTest() As String
 result = verify_testTimeValue()
-If failCount <> 0 And passCount > 0 Then
+If failCount <> 0 or passCount = 0 Then
     doUnitTest = result
 Else
     doUnitTest = "OK"
@@ -21,8 +21,7 @@ Function verify_testTimeValue() As String
     result = "Test Results" & Chr$(10) & "============" & Chr$(10)
 
     Dim testName As String
-    Dim TestDateTime As Date
-    Dim TestStr As String
+
     Dim date1, date2 As Date   'variables for test
     testName = "Test TimeValue function"
     On Error GoTo errorHandler
