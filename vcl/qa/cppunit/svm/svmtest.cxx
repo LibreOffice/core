acec1ad7c1ae681d1c89f8c1f8b0dec7a320ac3c
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2017-12-03 23:35:26 -0500

    vcl-svp: Store 24-bit images in 3-byte pixels
    This adds support in headless rendering for more
    compact 24-bit RGB image storage in 3-byte pixels
    instead of 4 bytes.
    
    There is a conversion necessary to accomodate Cairo,
    which requires 4-byte pixels, but that is relatively.
    
    Early tests show no loss of performance at runtime.
    
    Unit tests updated since the bitmap memory bytes
    are crc-ed, which obviously are different in size
    if not in content.
    
    (cherry picked from commit 354ad875092fd0c3b12f232950375206ec5d66a6)
    Reviewed-on: https://gerrit.libreoffice.org/46679
    Reviewed-by: Jan Holesovsky <kendy@collabora.com>
    Tested-by: Jan Holesovsky <kendy@collabora.com>
    (cherry picked from commit 11adb51f2553dc4a838c8668d94909771d70e1ab)
    
    Change-Id: I3919f7c56d14d09e67f163f035b4c7683b49087c
    Reviewed-on: https://gerrit.libreoffice.org/47009
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Ashod Nakashian <ashnakash@gmail.com>
    (cherry picked from commit 33761c69e9d3fa16e7a5b176200d183dc3236627)

--- vcl/qa/cppunit/svm/svmtest.cxx
+++ vcl/qa/cppunit/svm/svmtest.cxx
@@ -759,14 +759,14 @@ void SvmTest::checkBitmaps(const GDIMetaFile& rMetaFile)
     xmlDocPtr pDoc = dumpMeta(rMetaFile);
 
 #ifdef LINUX
-    assertXPathAttrs(pDoc, "/metafile/bmp[1]", {{"x", "1"}, {"y", "2"}, {"crc", "7932585a"}});
+    assertXPathAttrs(pDoc, "/metafile/bmp[1]", {{"x", "1"}, {"y", "2"}, {"crc", "b8dee5da"}});
     assertXPathAttrs(pDoc, "/metafile/bmpscale[1]", {
-        {"x", "1"}, {"y", "2"}, {"width", "3"}, {"height", "4"}, {"crc", "08bfca1f"}
+        {"x", "1"}, {"y", "2"}, {"width", "3"}, {"height", "4"}, {"crc", "281fc589"}
     });
     assertXPathAttrs(pDoc, "/metafile/bmpscalepart[1]", {
         {"destx", "1"}, {"desty", "2"}, {"destwidth", "3"}, {"destheight", "4"},
         {"srcx", "2"},  {"srcy", "1"},  {"srcwidth", "4"},  {"srcheight", "3"},
-        {"crc", "abd45514"}
+        {"crc", "5e01ddcc"}
     });
 #else
     assertXPathAttrs(pDoc, "/metafile/bmp[1]", {{"x", "1"}, {"y", "2"}, {"crc", "b8dee5da"}});
@@ -815,16 +815,16 @@ void SvmTest::checkBitmapExs(const GDIMetaFile& rMetaFile)
 
 #ifdef LINUX
     assertXPathAttrs(pDoc, "/metafile/bmpex[1]", {
-        {"x", "1"}, {"y", "2"}, {"crc", "7932585a"}, {"transparenttype", "bitmap"}
+        {"x", "1"}, {"y", "2"}, {"crc", "b8dee5da"}, {"transparenttype", "bitmap"}
     });
     assertXPathAttrs(pDoc, "/metafile/bmpexscale[1]", {
         {"x", "1"}, {"y", "2"}, {"width", "3"}, {"height", "4"},
-        {"crc", "08bfca1f"}, {"transparenttype", "bitmap"}
+        {"crc", "281fc589"}, {"transparenttype", "bitmap"}
     });
     assertXPathAttrs(pDoc, "/metafile/bmpexscalepart[1]", {
         {"destx", "1"}, {"desty", "2"}, {"destwidth", "3"}, {"destheight", "4"},
         {"srcx", "2"},  {"srcy", "1"},  {"srcwidth", "4"},  {"srcheight", "3"},
-        {"crc", "abd45514"}, {"transparenttype", "bitmap"}
+        {"crc", "5e01ddcc"}, {"transparenttype", "bitmap"}
     });
 #else
     assertXPathAttrs(pDoc, "/metafile/bmpex[1]", {
