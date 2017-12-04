c058eafb6c284484520e2d0b4388f5860944aadb
Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
2017-12-04 16:37:14 +0100

    tdf#83877 Add dialog to generate signature line
    Change-Id: Iacc929edf1469c7e37707f8d9518cd07642bb9dd

--- sw/inc/cmdid.h
+++ sw/inc/cmdid.h
@@ -209,6 +209,7 @@
 
 #define FN_INSERT_IDX_ENTRY_DLG (FN_INSERT + 35)    /* insert index entry */
 #define FN_INSERT_FRAME_INTERACT_NOCOL (FN_INSERT + 36) /*insert interactive non column frame*/
+#define FN_INSERT_SIGNATURELINE (FN_INSERT + 37)    /*insert signature line*/
 
 #define FN_TOOL_ANCHOR          (FN_INSERT + 49)    /* Draw Anchor for object */
 #define FN_TOOL_ANCHOR_PAGE     (FN_INSERT + 50)    /* anchor Draw object to page */
