986ded91b6f031dd4e0356f8d53fbe2cbe1724f0
Zdeněk Crhonek <zcrhonek@gmail.com>
2017-12-04 08:04:52 +0100

    VBA tests (4) - don't pass when all tests fail
    Change-Id: Ib4f9b5df1cffe104ba27be95183d075f24f28bb3

--- basic/qa/vba_tests/year.vb
+++ basic/qa/vba_tests/year.vb
@@ -6,7 +6,7 @@ Dim result As String
 
 Function doUnitTest() As String
 result = verify_testYear()
-If failCount <> 0 And passCount > 0 Then
+If failCount <> 0 or passCount = 0 Then
     doUnitTest = result
 Else
     doUnitTest = "OK"
@@ -23,14 +23,12 @@ Function verify_testYear() As String
     result = "Test Results" & Chr$(10) & "============" & Chr$(10)
 
     Dim testName As String
-    Dim TestDateTime As Date
-    Dim TestStr As String
     Dim date1, date2
     testName = "Test Year function"
     On Error GoTo errorHandler
 
     date2 = 1969
-    date1 = Year("12.2.1969")
+    date1 = Year("12/2/1969")
     TestLog_ASSERT date1 = date2, "the return Year is: " & date1
 
     date2 = 1900
