097d12bd738467765ae9aa0f4129cf3153510241
Caolán McNamara <caolanm@redhat.com>
2017-12-05 15:09:12 +0000

    coverity#1421089 seems to be really reporting missing move ctors/assignments
    Change-Id: I434eebac395bbb53a0c586a43568f64ec3fb8448

--- sc/source/core/data/cellvalue.cxx
+++ sc/source/core/data/cellvalue.cxx
@@ -244,6 +244,27 @@ ScCellValue::ScCellValue( const ScCellValue& r ) : meType(r.meType), mfValue(r.m
     }
 }
 
+ScCellValue::ScCellValue(ScCellValue&& r)
+    : meType(r.meType)
+    , mfValue(r.mfValue)
+{
+    switch (r.meType)
+    {
+        case CELLTYPE_STRING:
+            mpString = r.mpString;
+        break;
+        case CELLTYPE_EDIT:
+            mpEditText = r.mpEditText;
+        break;
+        case CELLTYPE_FORMULA:
+            mpFormula = r.mpFormula;
+        break;
+        default:
+            ;
+    }
+    r.meType = CELLTYPE_NONE;
+}
+
 ScCellValue::~ScCellValue()
 {
     clear();
@@ -489,6 +510,33 @@ ScCellValue& ScCellValue::operator= ( const ScCellValue& r )
 {
     ScCellValue aTmp(r);
     swap(aTmp);
+    return *this;
+}
+
+ScCellValue& ScCellValue::operator=(ScCellValue&& rCell)
+{
+    clear();
+
+    meType = rCell.meType;
+    mfValue = rCell.mfValue;
+    switch (rCell.meType)
+    {
+        case CELLTYPE_STRING:
+            mpString = rCell.mpString;
+        break;
+        case CELLTYPE_EDIT:
+            mpEditText = rCell.mpEditText;
+        break;
+        case CELLTYPE_FORMULA:
+            mpFormula = rCell.mpFormula;
+        break;
+        default:
+            ;
+    }
+    //we don't need to reset mpString/mpEditText/mpFormula if we
+    //set meType to NONE as the ScCellValue dtor keys off the meType
+    rCell.meType = CELLTYPE_NONE;
+
     return *this;
 }
 
