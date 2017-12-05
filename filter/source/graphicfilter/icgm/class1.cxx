df3d9f52543b7a3cae2943813a4948ef5bd0b86a
ekuiitr <jhaekansh80@gmail.com>
2017-12-05 15:26:32 +0800

    tdf#75280 Cleaning up of sal_uIntPtr usage
    Change-Id: Ia8fd1b3a600a12d0fcbedf2fc87747063eb7d090

--- filter/source/graphicfilter/icgm/class1.cxx
+++ filter/source/graphicfilter/icgm/class1.cxx
@@ -178,7 +178,7 @@ void CGM::ImplDoClass1()
             {
                 sal_uInt32 nSize = ImplGetUI(1);
 
-                if (static_cast<sal_uIntPtr>(mpEndValidSource - (mpSource + mnParaSize)) < nSize)
+                if (static_cast<sal_uInt32>(mpEndValidSource - (mpSource + mnParaSize)) < nSize)
                     throw css::uno::Exception("attempt to read past end of input", nullptr);
 
                 pElement->aFontList.InsertName( mpSource + mnParaSize, nSize );
@@ -193,7 +193,7 @@ void CGM::ImplDoClass1()
                 sal_uInt32 nCharSetType = ImplGetUI16();
                 sal_uInt32 nSize = ImplGetUI(1);
 
-                if (static_cast<sal_uIntPtr>(mpEndValidSource - (mpSource + mnParaSize)) < nSize)
+                if (static_cast<sal_uInt32>(mpEndValidSource - (mpSource + mnParaSize)) < nSize)
                     throw css::uno::Exception("attempt to read past end of input", nullptr);
 
                 pElement->aFontList.InsertCharSet( (CharSetType)nCharSetType, mpSource + mnParaSize, nSize );
