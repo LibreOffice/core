6030c822c968be79b493de70c68a0bd51760e52f
Yousuf Philips <philipz85@hotmail.com>
2017-12-06 16:15:35 +0400

    tdf#103080 Dont package Open Sans and PT_Serif fonts
    Change-Id: Iac1d572f19372465e9cc369454480d9b621bcd66
    Reviewed-on: https://gerrit.libreoffice.org/45169
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Yousuf Philips <philipz85@hotmail.com>
    (cherry picked from commit 0ce173b50fd12342979cf3f8f9b2d92267552060)
    Reviewed-on: https://gerrit.libreoffice.org/46617

--- Repository.mk
+++ Repository.mk
@@ -955,8 +955,6 @@ $(eval $(call gb_Helper_register_packages_for_install,ooo_fonts,\
 		fonts_liberation_narrow \
 		fonts_libertineg \
 		fonts_noto \
-		fonts_opensans \
-		fonts_ptserif \
 		fonts_sourcecode \
 		fonts_sourcesans \
 	) \
