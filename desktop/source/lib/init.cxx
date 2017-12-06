cdac186e0408b8e2f6f6c1fbacc1c6659a8b5b96
Pranav Kant <pranavk@collabora.co.uk>
2017-12-06 17:32:53 +0530

    lokdialog: State change events for dialogs uno commands
    Change-Id: I92f60e717cd2a0695c8b86b103076d26c0c22cef
    (cherry picked from commit 98da350c7c9bcb96937f1397a34c2c68ede44011)

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -1741,7 +1741,13 @@ static void doc_iniUnoCommands ()
         OUString(".uno:NextTrackedChange"),
         OUString(".uno:PreviousTrackedChange"),
         OUString(".uno:AcceptAllTrackedChanges"),
-        OUString(".uno:RejectAllTrackedChanges")
+        OUString(".uno:RejectAllTrackedChanges"),
+        OUString(".uno:TableDialog"),
+        OUString(".uno:FormatCellDialog"),
+        OUString(".uno:FontDialog"),
+        OUString(".uno:ParagraphDialog"),
+        OUString(".uno:OutlineBullet"),
+        OUString(".uno:InsertIndexesEntry")
     };
 
     util::URL aCommandURL;
