7ab0ddd29fa404ff4bcdf0159de8820fa0c147ff
Caolán McNamara <caolanm@redhat.com>
2017-12-07 16:37:13 +0000

    coverity#1425723 silence 'Wrapper object use after free'
    Reviewed-on: https://gerrit.libreoffice.org/46035
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>
    (cherry picked from commit 05f97ef127ec6ee0cc159fc74b4fecc34d9c684c)
    
    Change-Id: Ibcc0e5f28910bdd15c94579d754de3698ac8beb1

--- sc/source/filter/xml/xmlcondformat.cxx
+++ sc/source/filter/xml/xmlcondformat.cxx
@@ -324,7 +324,16 @@ void ScXMLConditionalFormatContext::EndElement()
                 }
             }
         }
+    }
 
+    sal_uLong nIndex = pDoc->AddCondFormat(pFormat.release(), nTab);
+    ScConditionalFormat* pInsertedFormat = pDoc->GetCondFormList(nTab)->GetFormat(nIndex);
+    assert(pInsertedFormat && pInsertedFormat->GetKey() == nIndex);
+
+    mrParent.mvCondFormatData.push_back( { pInsertedFormat, nTab } );
+
+    if (bEligibleForCache)
+    {
         // Not found in cache, replace oldest cache entry
         sal_Int64 nOldestAge = -1;
         size_t nIndexOfOldest = 0;
@@ -336,17 +345,11 @@ void ScXMLConditionalFormatContext::EndElement()
                 nIndexOfOldest = (&aCacheEntry - &mrParent.maCache.front());
             }
         }
-        mrParent.maCache[nIndexOfOldest].mpFormat = pFormat.get();
+        mrParent.maCache[nIndexOfOldest].mpFormat = pInsertedFormat;
         mrParent.maCache[nIndexOfOldest].mbSingleRelativeReference = bSingleRelativeReference;
         mrParent.maCache[nIndexOfOldest].mpTokens.reset(pTokens);
         mrParent.maCache[nIndexOfOldest].mnAge = 0;
     }
-
-    sal_uLong nIndex = pDoc->AddCondFormat(pFormat.get(), nTab);
-    (void) nIndex; // Avoid 'unused variable' warning when assert() expands to empty
-    assert(pFormat->GetKey() == nIndex);
-
-    mrParent.mvCondFormatData.push_back( { pFormat.release(), nTab } );
 }
 
 ScXMLConditionalFormatContext::~ScXMLConditionalFormatContext()
