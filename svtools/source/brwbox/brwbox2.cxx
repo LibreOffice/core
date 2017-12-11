3f9faf0fb356cdd7a03ea2da2d5b0fcefbd11b56
Takeshi Abe <tabe@fixedpoint.jp>
2017-12-11 22:28:32 +0900

    svtools: Simplify BrowserDataWin with std::unique_ptr
    Change-Id: Ib63b60c1f5326cfc1b163164ae5a783ea8ba1919

--- svtools/source/brwbox/brwbox2.cxx
+++ svtools/source/brwbox/brwbox2.cxx
@@ -27,6 +27,7 @@
 #include <tools/multisel.hxx>
 #include <tools/fract.hxx>
 #include <algorithm>
+#include <memory>
 
 using namespace ::com::sun::star::datatransfer;
 
@@ -234,7 +235,7 @@ void BrowseBox::ToggleSelection()
     bNotToggleSel = true;
 
     // accumulate areas of rows to highlight
-    RectangleList aHighlightList;
+    std::vector<std::unique_ptr<tools::Rectangle>> aHighlightList;
     long nLastRowInRect = 0; // for the CFront
 
     // don't highlight handle column
@@ -259,18 +260,16 @@ void BrowseBox::ToggleSelection()
         if ( aHighlightList.size() && nLastRowInRect == ( nRow - 1 ) )
             aHighlightList[ 0 ]->Union( aAddRect );
         else
-            aHighlightList.insert( aHighlightList.begin(), new tools::Rectangle( aAddRect ) );
+            aHighlightList.emplace( aHighlightList.begin(), new tools::Rectangle( aAddRect ) );
         nLastRowInRect = nRow;
     }
 
     // unhighlight the old selection (if any)
-    for ( size_t i = aHighlightList.size(); i > 0; )
+    while ( !aHighlightList.empty() )
     {
-        tools::Rectangle *pRect = aHighlightList[ --i ];
-        pDataWin->Invalidate( *pRect );
-        delete pRect;
+        pDataWin->Invalidate( *aHighlightList.back() );
+        aHighlightList.pop_back();
     }
-    aHighlightList.clear();
 
     // unhighlight old column selection (if any)
     for ( long nColId = pColSel ? pColSel->FirstSelected() : BROWSER_ENDOFSELECTION;
