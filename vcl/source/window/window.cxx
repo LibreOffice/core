fdd878943c2582deabd45d80943bbabc4ab82646
Caolán McNamara <caolanm@redhat.com>
2017-12-12 14:17:11 +0000

    tdf#114408 set correct tooltip rectangle
    The original code tried to get the position of the window relative
    to its parent and then ask the parent to adjust that position to
    a position relative to the toplevel. But the position to use in
    that case should have be WindowImpl::nX/WindowImpl::nY not GetPosPixel()
    
    Much easier to just directly ask the the current window for its position
    relative to the toplevel.
    
    Change-Id: Iff202f052bc651a8c0e3a57ff3322e5e2a3e4b9a

--- vcl/source/window/window.cxx
+++ vcl/source/window/window.cxx
@@ -1861,7 +1861,7 @@ void Window::RequestHelp( const HelpEvent& rHEvt )
         {
             Point aPos = GetPosPixel();
             if ( ImplGetParent() && !ImplIsOverlapWindow() )
-                aPos = ImplGetParent()->OutputToScreenPixel( aPos );
+                aPos = OutputToScreenPixel(Point(0, 0));
             tools::Rectangle   aRect( aPos, GetSizePixel() );
 
             Help::ShowBalloon( this, rHEvt.GetMousePosPixel(), aRect, rStr );
@@ -1876,7 +1876,7 @@ void Window::RequestHelp( const HelpEvent& rHEvt )
         {
             Point aPos = GetPosPixel();
             if ( ImplGetParent() && !ImplIsOverlapWindow() )
-                aPos = ImplGetParent()->OutputToScreenPixel( aPos );
+                aPos = OutputToScreenPixel(Point(0, 0));
             tools::Rectangle   aRect( aPos, GetSizePixel() );
             OUString      aHelpText;
             if ( !rStr.isEmpty() )
