447975a785574a2590b4e70287018c9ca2f23820
serdarot5 <serdarot5@gmail.com>
2017-12-12 09:58:31 +0300

    Shape text incorrectly turned tdf#104290
    Change-Id: I7558f8ee274a3c12543c4512cca2cf5e50255291

--- sc/qa/unit/subsequent_filters-test.cxx
+++ sc/qa/unit/subsequent_filters-test.cxx
@@ -66,11 +66,19 @@
 #include <stlpool.hxx>
 
 #include <orcusfiltersimpl.hxx>
+<<<<<<< 8af88c48060d2b07255793c9be65f4d085b7b48b
 #include <orcusfilters.hxx>
 #include <filter.hxx>
 #include <orcusinterface.hxx>
 #include <generalfunction.hxx>
 
+=======
+#include "orcusfilters.hxx"
+#include "filter.hxx"
+#include "orcusinterface.hxx"
+#include "generalfunction.hxx"
+#include <comphelper/sequenceashashmap.hxx>
+>>>>>>> Shape text incorrectly turned tdf#104290
 #include <com/sun/star/drawing/XDrawPageSupplier.hpp>
 #include <com/sun/star/drawing/XControlShape.hpp>
 #include <com/sun/star/sheet/XSpreadsheetDocument.hpp>
@@ -228,6 +236,7 @@ public:
     void testTdf110440XLSX();
     void testTdf111974XLSM();
     void testTdf83672XLSX();
+    void testTdf104290XLSX();
 
     void testPageScalingXLSX();
     void testActiveXCheckboxXLSX();
@@ -350,6 +359,7 @@ public:
     CPPUNIT_TEST(testTdf110440XLSX);
     CPPUNIT_TEST(testTdf111974XLSM);
     CPPUNIT_TEST(testTdf83672XLSX);
+    CPPUNIT_TEST(testTdf104290XLSX);
 
     CPPUNIT_TEST(testPageScalingXLSX);
     CPPUNIT_TEST(testActiveXCheckboxXLSX);
@@ -3478,6 +3488,25 @@ void ScFiltersTest::testTdf83672XLSX()
     sal_Int32 nRotate = 0;
     xShapeProperties->getPropertyValue("RotateAngle") >>= nRotate;
     CPPUNIT_ASSERT(nRotate != 0);
+    xDocSh->DoClose();
+}
+void ScFiltersTest::testTdf104290XLSX()
+{
+    ScDocShellRef xDocSh = loadDoc("tdf104290.", FORMAT_XLSX);
+    CPPUNIT_ASSERT_MESSAGE("Failed to load tdf104290 .xlsx", xDocSh.is());
+    uno::Reference< drawing::XDrawPagesSupplier > xDoc(
+        xDocSh->GetModel(), uno::UNO_QUERY_THROW );
+    uno::Reference< drawing::XDrawPage > xPage(
+        xDoc->getDrawPages()->getByIndex(0), uno::UNO_QUERY_THROW );
+    uno::Reference< drawing::XShape > xShape(
+        xPage->getByIndex(0), uno::UNO_QUERY_THROW );
+    CPPUNIT_ASSERT_MESSAGE( "failed to load shape", xShape.is() );
+    uno::Reference< beans::XPropertySet > xShapeProperties(
+        xShape, uno::UNO_QUERY );
+    sal_Int32 nTextRotate = 0;
+    comphelper::SequenceAsHashMap geom(xShapeProperties->getPropertyValue("CustomShapeGeometry"));
+    geom["TextRotateAngle"]>>=nTextRotate;
+    CPPUNIT_ASSERT(nTextRotate == 0);
     xDocSh->DoClose();
 }
 
