135c160d41e68c8ba4178684cce6fb88b1f57d78
Stephan Bergmann <sbergman@redhat.com>
2017-12-12 08:20:46 +0100

    loplugin:salcall
    Change-Id: I38be4ffc8e93cdc567fbaaf826ce791fa79bbfaf

--- svx/source/dialog/crashreportui.cxx
+++ svx/source/dialog/crashreportui.cxx
@@ -75,7 +75,7 @@ css::uno::Any SAL_CALL CrashReportUI::dispatchWithReturnValue(const css::util::U
 
 }
 
-extern "C" SAL_DLLPUBLIC_EXPORT css::uno::XInterface * SAL_CALL
+extern "C" SAL_DLLPUBLIC_EXPORT css::uno::XInterface *
 com_sun_star_comp_svx_CrashReportUI_get_implementation(
     css::uno::XComponentContext *context,
     css::uno::Sequence<css::uno::Any> const &)
