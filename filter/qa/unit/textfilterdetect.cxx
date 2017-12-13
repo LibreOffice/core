23f8f6882ee02554d29cd4409bc94c9da2dd5f02
Miklos Vajna <vmiklos@collabora.co.uk>
2017-12-13 09:49:41 +0100

    tdf#114428 filter: recognize XHTML with XML declaration as HTML
    The problem was the additional
    
    	<?xml version="1.0" encoding="utf-8"?>
    
    XML declaration before the usual
    
    	<!DOCTYPE html ...
    
    line, just ignore it.
    
    Change-Id: I294aae5504b40b42f76da00fef645d0d89009da9
    Reviewed-on: https://gerrit.libreoffice.org/46324
    Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
    Tested-by: Jenkins <ci@libreoffice.org>
    (cherry picked from commit 4af729f31c64c09c76ea8bcfa5067092571b92de)

---
+++ filter/qa/unit/textfilterdetect.cxx
@@ -0,0 +1,63 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <com/sun/star/document/XExtendedFilterDetection.hpp>
+#include <com/sun/star/io/XInputStream.hpp>
+
+#include <comphelper/processfactory.hxx>
+#include <comphelper/propertyvalue.hxx>
+#include <test/bootstrapfixture.hxx>
+#include <unotools/mediadescriptor.hxx>
+#include <unotools/streamwrap.hxx>
+
+using namespace com::sun::star;
+
+namespace
+{
+/// Test class for PlainTextFilterDetect.
+class TextFilterDetectTest : public test::BootstrapFixture
+{
+public:
+    void testTdf114428();
+
+    CPPUNIT_TEST_SUITE(TextFilterDetectTest);
+    CPPUNIT_TEST(testTdf114428);
+    CPPUNIT_TEST_SUITE_END();
+};
+
+char const DATA_DIRECTORY[] = "/filter/qa/unit/data/";
+
+void TextFilterDetectTest::testTdf114428()
+{
+    uno::Reference<uno::XComponentContext> xComponentContext
+        = comphelper::getComponentContext(getMultiServiceFactory());
+    uno::Reference<document::XExtendedFilterDetection> xDetect(
+        getMultiServiceFactory()->createInstance("com.sun.star.comp.filters.PlainTextFilterDetect"),
+        uno::UNO_QUERY);
+    OUString aURL = m_directories.getURLFromSrc(DATA_DIRECTORY) + "tdf114428.xhtml";
+    SvFileStream aStream(aURL, StreamMode::READ);
+    uno::Reference<io::XInputStream> xStream(new utl::OStreamWrapper(aStream));
+    uno::Sequence<beans::PropertyValue> aDescriptor
+        = { comphelper::makePropertyValue("DocumentService",
+                                          OUString("com.sun.star.text.TextDocument")),
+            comphelper::makePropertyValue("InputStream", xStream),
+            comphelper::makePropertyValue("TypeName", OUString("generic_HTML")) };
+    xDetect->detect(aDescriptor);
+    utl::MediaDescriptor aMediaDesc(aDescriptor);
+    OUString aFilterName = aMediaDesc.getUnpackedValueOrDefault("FilterName", OUString());
+    // This was empty, XML declaration caused HTML detect to not handle XHTML.
+    CPPUNIT_ASSERT_EQUAL(OUString("HTML (StarWriter)"), aFilterName);
+}
+
+CPPUNIT_TEST_SUITE_REGISTRATION(TextFilterDetectTest);
+}
+
+CPPUNIT_PLUGIN_IMPLEMENT();
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
