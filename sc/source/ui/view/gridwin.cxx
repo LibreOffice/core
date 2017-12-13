e07e4fcc10c424eeea726a151ab90c49aaf75797
Marco Cecchetti <marco.cecchetti@collabora.com>
2017-12-13 18:02:01 +0100

    lok: sc: append the col, row position to the cell cursor message
    The payload changes from: "x, y, width, height"
    to: "x, y, width, height, column, row".
    This feature is exploited by online starting from the 3.0 release.
    This change is compatible with older online release:
    the column, row coordinates are just ignored.
    
    Change-Id: I7f8c0e3685c57ff20520ee068b215037f6137389

--- sc/source/ui/view/gridwin.cxx
+++ sc/source/ui/view/gridwin.cxx
@@ -5565,11 +5565,15 @@ OString ScGridWindow::getCellCursor(const Fraction& rZoomX, const Fraction& rZoo
     if (nSizeYPix == 0)
         nSizeYPix = 1;
 
-    long nSizeXTw = rtl::math::round(nSizeXPix / fPPTX);
-    long nSizeYTw = rtl::math::round(nSizeYPix / fPPTY);
+    long nPosXTw = rtl::math::round(aScrPos.getX() / fPPTX);
+    long nPosYTw = rtl::math::round(aScrPos.getY() / fPPTY);
+    // look at Rectangle( const Point& rLT, const Size& rSize ) for the '- 1'
+    long nSizeXTw = rtl::math::round(nSizeXPix / fPPTX) - 1;
+    long nSizeYTw = rtl::math::round(nSizeYPix / fPPTY) - 1;
 
-    tools::Rectangle aRect(Point(rtl::math::round(aScrPos.getX() / fPPTX), rtl::math::round(aScrPos.getY() / fPPTY)),
-                    Size(nSizeXTw, nSizeYTw));
+    std::stringstream ss;
+    ss << nPosXTw << ", " << nPosYTw << ", " << nSizeXTw << ", " << nSizeYTw << ", "
+       << nX << ", " << nY;
 
     pViewData->SetZoom(defaultZoomX, defaultZoomY, true);
 
@@ -5580,7 +5584,7 @@ OString ScGridWindow::getCellCursor(const Fraction& rZoomX, const Fraction& rZoo
     pViewData->GetLOKHeightHelper().insert(nY - 1, aScrPos.getY());
     pViewData->SetLOKOldCurY(nY);
 
-    return aRect.toString();
+    return ss.str().c_str();
 }
 
 void ScGridWindow::updateLibreOfficeKitCellCursor(const SfxViewShell* pOtherShell) const
