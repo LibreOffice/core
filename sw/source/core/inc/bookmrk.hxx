c66b2fabd1928b73ca0f65d884971bec55185313
Bernhard Widl <bernhard.widl@cib.de>
2017-12-13 10:24:47 +0100

    extended bookmarks dialog
    Change-Id: I9466398dc2c889caed2be2849a18ea6f06b67e2a

--- sw/source/core/inc/bookmrk.hxx
+++ sw/source/core/inc/bookmrk.hxx
@@ -174,6 +174,14 @@ namespace sw {
                 { m_sShortName = rShortName; }
             virtual void SetKeyCode(const vcl::KeyCode& rCode) override
                 { m_aCode = rCode; }
+            virtual bool IsHidden() const override
+                { return m_bHidden; }
+            virtual const OUString& GetHideCondition() const override
+                { return m_sHideCondition; }
+            virtual void Hide(bool rHide) override
+                { m_bHidden = rHide; }
+            virtual void SetHideCondition(const OUString& rHideCondition) override
+                { m_sHideCondition = rHideCondition; }
 
             // ::sfx2::Metadatable
             virtual ::sfx2::IXmlIdRegistry& GetRegistry() override;
@@ -185,6 +193,8 @@ namespace sw {
         private:
             vcl::KeyCode m_aCode;
             OUString m_sShortName;
+            bool m_bHidden;
+            OUString m_sHideCondition;
         };
 
         class Fieldmark
