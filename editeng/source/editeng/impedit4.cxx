985cc331778dc4ccb10be2f145e0b00d52c9935f
Noel Grandin <noel.grandin@collabora.co.uk>
2017-12-14 11:06:52 +0200

    sal_uIntPtr->VclPtr<OutputDevice> in XParaPortionList
    who knows we had to hide a pointer behind an integer type, but it's been
    this way since forever
    
    Change-Id: I9738e1137752cc8226877e2cf0291967b45dbf0e

--- editeng/source/editeng/impedit4.cxx
+++ editeng/source/editeng/impedit4.cxx
@@ -1185,7 +1185,7 @@ EditSelection ImpEditEngine::InsertTextObject( const EditTextObject& rTextObject
             && ( pPortionInfo->GetStretchX() == nStretchX )
             && ( pPortionInfo->GetStretchY() == nStretchY ) )
     {
-        if ( ( pPortionInfo->GetRefDevPtr() == reinterpret_cast<sal_uIntPtr>(GetRefDevice()) ) ||
+        if ( ( pPortionInfo->GetRefDevPtr() == GetRefDevice() ) ||
              ( ( pPortionInfo->GetRefDevType() == OUTDEV_VIRDEV ) &&
                ( GetRefDevice()->GetOutDevType() == OUTDEV_VIRDEV ) ) )
         bUsePortionInfo = true;
