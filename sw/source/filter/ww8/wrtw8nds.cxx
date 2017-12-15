558f571c3bc345c889023e093651f3941791e389
Tamas Bunth <tamas.bunth@collabora.co.uk>
2017-12-15 01:00:00 +0100

    no section break after split paragraph
    Change-Id: I08aa10527d6e5d8950a592334d01186cbd8cd355

--- sw/source/filter/ww8/wrtw8nds.cxx
+++ sw/source/filter/ww8/wrtw8nds.cxx
@@ -2685,7 +2685,7 @@ void MSWordExportBase::OutputTextNode( SwTextNode& rNode )
                 PrepareNewPageDesc( rNode.GetpSwAttrSet(), rNode, nullptr , pNextPageDesc);
             }
         }
-        else
+        else if (!bNeedParaSplit)
         {
             // else check if section break needed after the paragraph
             AttrOutput().SectionBreaks(rNode);
