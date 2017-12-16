00bf13f3e4b7b72ceb65bb3d4538461b7d5b70e4
Eike Rathke <erack@redhat.com>
2017-12-16 22:52:09 +0100

    Type svIndex does not necessarily mean range name, tdf#114251 related
    Change-Id: I5e7930b7f93ee44fb0d6b4c86f261afde8ed2ccc
    Reviewed-on: https://gerrit.libreoffice.org/46615
    Reviewed-by: Eike Rathke <erack@redhat.com>
    Tested-by: Jenkins <ci@libreoffice.org>
    (cherry picked from commit ecac94f3a6872538c19827603b52f1ed7e671a4c)

--- sc/source/core/data/grouptokenconverter.cxx
+++ sc/source/core/data/grouptokenconverter.cxx
@@ -241,6 +241,13 @@ bool ScGroupTokenConverter::convert( const ScTokenArray& rCode, sc::FormulaLogge
             break;
             case svIndex:
             {
+                if (p->GetOpCode() != ocName)
+                {
+                    // May be DB-range or TableRef
+                    mrGroupTokens.AddToken(*p);
+                    break;
+                }
+
                 // Named range.
                 ScRangeName* pNames = mrDoc.GetRangeName();
                 if (!pNames)
