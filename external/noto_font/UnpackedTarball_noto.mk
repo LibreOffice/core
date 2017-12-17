49cc6bc8300223d9cbb7f1e9444ac7eca44cca92
Andras Timar <andras.timar@collabora.com>
2017-12-17 09:12:59 +0100

    reduce the size of Noto fonts package (484M->101M)
    * do not install UI font variants
    * do not install monospaced CJK fonts
    * install Region-specific Subset OpenType/CFF (Subset OTF) for CJK
    	* this is the logical choice on Linux:
    	  see: https://www.google.com/get/noto/help/cjk/
    * keep only 2 weights, Regular and Bold for CJK fonts
    
    Change-Id: I292c7a656a55b76a5c07c4fb24529ac36b5bc34b
    Reviewed-on: https://gerrit.libreoffice.org/46637
    Reviewed-by: Michael Meeks <michael.meeks@collabora.com>
    Tested-by: Michael Meeks <michael.meeks@collabora.com>

--- external/noto_font/UnpackedTarball_noto.mk
+++ external/noto_font/UnpackedTarball_noto.mk
@@ -11,4 +11,36 @@ $(eval $(call gb_UnpackedTarball_UnpackedTarball,font_noto))
 
 $(eval $(call gb_UnpackedTarball_set_tarball,font_noto,$(FONT_NOTO_TARBALL),0))
 
+$(eval $(call gb_UnpackedTarball_UnpackedTarball,font_notosansjp))
+
+$(eval $(call gb_UnpackedTarball_set_tarball,font_notosansjp,$(FONT_NOTO_SANS_JP_TARBALL),0))
+
+$(eval $(call gb_UnpackedTarball_UnpackedTarball,font_notosanskr))
+
+$(eval $(call gb_UnpackedTarball_set_tarball,font_notosanskr,$(FONT_NOTO_SANS_KR_TARBALL),0))
+
+$(eval $(call gb_UnpackedTarball_UnpackedTarball,font_notosanssc))
+
+$(eval $(call gb_UnpackedTarball_set_tarball,font_notosanssc,$(FONT_NOTO_SANS_SC_TARBALL),0))
+
+$(eval $(call gb_UnpackedTarball_UnpackedTarball,font_notosanstc))
+
+$(eval $(call gb_UnpackedTarball_set_tarball,font_notosanstc,$(FONT_NOTO_SANS_TC_TARBALL),0))
+
+$(eval $(call gb_UnpackedTarball_UnpackedTarball,font_notoserifjp))
+
+$(eval $(call gb_UnpackedTarball_set_tarball,font_notoserifjp,$(FONT_NOTO_SERIF_JP_TARBALL),0))
+
+$(eval $(call gb_UnpackedTarball_UnpackedTarball,font_notoserifkr))
+
+$(eval $(call gb_UnpackedTarball_set_tarball,font_notoserifkr,$(FONT_NOTO_SERIF_KR_TARBALL),0))
+
+$(eval $(call gb_UnpackedTarball_UnpackedTarball,font_notoserifsc))
+
+$(eval $(call gb_UnpackedTarball_set_tarball,font_notoserifsc,$(FONT_NOTO_SERIF_SC_TARBALL),0))
+
+$(eval $(call gb_UnpackedTarball_UnpackedTarball,font_notoseriftc))
+
+$(eval $(call gb_UnpackedTarball_set_tarball,font_notoseriftc,$(FONT_NOTO_SERIF_TC_TARBALL),0))
+
 # vim: set noet sw=4 ts=4:
