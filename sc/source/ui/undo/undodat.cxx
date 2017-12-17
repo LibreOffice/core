adffe88eaa4608ce52676568598cbc4199599ee7
Marco Cecchetti <marco.cecchetti@collabora.com>
2017-12-17 14:08:50 +0100

    lok: sc: missing header invalidation on undo an autofilter action
    Change-Id: Ibfa7602221ed0c115ad469bd8a61e62baaff0bdf
    Reviewed-on: https://gerrit.libreoffice.org/46715
    Reviewed-by: Marco Cecchetti <mrcekets@gmail.com>
    Tested-by: Marco Cecchetti <mrcekets@gmail.com>
    (cherry picked from commit b0cecbfb03dba67f01d1411a1e8b7402c2ca90ce)

--- sc/source/ui/undo/undodat.cxx
+++ sc/source/ui/undo/undodat.cxx
@@ -817,6 +817,7 @@ void ScUndoQuery::Undo()
 
     // invalidate cache positions and update cursor and selection
     pViewShell->OnLOKShowHideColRow(/*bColumns*/ false, aQueryParam.nRow1 - 1);
+    ScTabViewShell::notifyAllViewsHeaderInvalidation(ROW_HEADER, nTab);
 
     //  Paint
 
