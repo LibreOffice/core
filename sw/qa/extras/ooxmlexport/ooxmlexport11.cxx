LibreOffice/core

commit c72a1a74b5b1064fc9cdf9994b11fce26d866e26

Related: tdf#112211 DOCX import: fix handling of missing first ind in <w:lvl>
Usually a DOCX numbering definition has multiple levels, each level
containing a <w:ind ... w:hanging="..."/> element. When this is missing,
we should default to the Word default, not to the Writer one.

This makes the DOCX version of tdf#106953 imported correctly, in
preparation of dropping the original fix that helped RTF only.

[ The DOC version of the bugdoc is still not imported correctly. ]

Change-Id: Ib7fc1de55316a73188c023665a585ac7056341f7
Reviewed-on: https://gerrit.libreoffice.org/42447
Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
Tested-by: Jenkins <ci@libreoffice.org>

writerfilter/source/dmapper/NumberingManager.cxx
@@ -304,6 +304,9 @@ uno::Sequence< beans::PropertyValue > ListLevel::GetLevelProperties( )
         boost::optional<PropertyMap::Property> aProp = getProperty(rReadId);
         if (aProp)
             aNumberingProperties.emplace_back( getPropertyName(aProp->first), 0, aProp->second, beans::PropertyState_DIRECT_VALUE );
+        else if (rReadId == PROP_FIRST_LINE_INDENT)
+            // Writer default is -360 twips, Word default seems to be 0.
+            aNumberingProperties.emplace_back("FirstLineIndent", 0, uno::makeAny(static_cast<sal_Int32>(0)), beans::PropertyState_DIRECT_VALUE);
     }
 
     boost::optional<PropertyMap::Property> aPropFont = getProperty(PROP_CHAR_FONT_NAME);

sw/qa/extras/ooxmlexport/ooxmlexport.cxx
@@ -845,6 +845,13 @@ DECLARE_OOXMLEXPORT_TEST(testTdf44986, "tdf44986.docx")
     CPPUNIT_ASSERT_EQUAL(sal_Int32(1), getProperty< uno::Sequence<text::TableColumnSeparator> >(xTableRows->getByIndex(0), "TableColumnSeparators").getLength());
 }
 
+DECLARE_OOXMLEXPORT_TEST(testTdf106953, "tdf106953.docx")
+{
+    uno::Reference<container::XIndexAccess> xRules = getProperty< uno::Reference<container::XIndexAccess> >(getStyles("NumberingStyles")->getByName("WWNum1"), "NumberingRules");
+    // This was -635, so the tab of the numbering expanded to a small value instead of matching Word's larger value.
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(0), comphelper::SequenceAsHashMap(xRules->getByIndex(0))["FirstLineIndent"].get<sal_Int32>());
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

commit 174deb08da22916b9b708f9d7144eaa039968191

tdf#113547 DOCX import: fix handling of stub numbering overrides
Regression from commit c72a1a74b5b1064fc9cdf9994b11fce26d866e26
(Related: tdf#112211 DOCX import: fix handling of missing first ind in
<w:lvl>, 2017-09-19), now that a numbering level can have default
properties, we need to differentiate between the case when the level has
real properties or it has only default properties.

Merging of properties is not necessary when the abstract numbering has
properties, but the "override" has default properties only.

Change-Id: Idbcbd0f66932514612e11fe82377f2b68ac9e403

writerfilter/source/dmapper/NumberingManager.cxx
@@ -119,6 +119,12 @@ void ListLevel::SetValue( Id nId, sal_Int32 nValue )
         default:
             OSL_FAIL( "this line should never be reached");
     }
+    m_bHasValues = true;
+}
+
+bool ListLevel::HasValues() const
+{
+    return m_bHasValues;
 }
 
 void ListLevel::SetParaStyle( const std::shared_ptr< StyleSheetEntry >& pStyle )
@@ -467,9 +473,9 @@ uno::Sequence< uno::Sequence< beans::PropertyValue > > ListDef::GetPropertyValue
     for ( sal_Int32 i = 0; i < nThisCount && i < nAbstractCount; i++ )
     {
         uno::Sequence< beans::PropertyValue > level = aThis[i];
-        if ( level.hasElements() )
+        if (level.hasElements() && GetLevel(i)->HasValues())
         {
-            // If the element contains something, merge it
+            // If the element contains something, merge it, but ignore stub overrides.
             lcl_mergeProperties( level, aAbstract[i] );
         }
     }

writerfilter/source/dmapper/NumberingManager.hxx
@@ -52,6 +52,7 @@ class ListLevel : public PropertyMap
     sal_Int32                                     m_nTabstop;
     std::shared_ptr< StyleSheetEntry >          m_pParaStyle;
     bool                                          m_outline;
+    bool m_bHasValues = false;
 
 public:
 
@@ -80,6 +81,8 @@ public:
     const OUString& GetBulletChar( ) { return m_sBulletChar; };
     const std::shared_ptr< StyleSheetEntry >& GetParaStyle( ) { return m_pParaStyle; };
     bool isOutlineNumbering() const { return m_outline; }
+    /// Determines if SetValue() was called at least once.
+    bool HasValues() const;
 
     // UNO mapping functions
 

sw/qa/extras/ooxmlexport/ooxmlexport11.cxx
@@ -170,6 +170,17 @@ DECLARE_OOXMLEXPORT_TEST(testTdf113183, "tdf113183.docx")
                                                 "HoriOrientPosition"));
 }
 
+DECLARE_OOXMLEXPORT_TEST(testTdf113547, "tdf113547.docx")
+{
+    uno::Reference<beans::XPropertySet> xPropertySet(
+        getStyles("NumberingStyles")->getByName("WWNum1"), uno::UNO_QUERY);
+    uno::Reference<container::XIndexAccess> xLevels(
+        xPropertySet->getPropertyValue("NumberingRules"), uno::UNO_QUERY);
+    comphelper::SequenceAsHashMap aProps(xLevels->getByIndex(0)); // 1st level
+    // This was 0, first-line left margin of the numbering was lost.
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(-635), aProps["FirstLineIndent"].get<sal_Int32>());
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */