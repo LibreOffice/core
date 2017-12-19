be39e51740be1224aa1ee7d7f6241883189435fa
Bernhard Widl <bernhard.widl@cib.de>
2017-12-19 21:21:55 +0100

    write new bookmark attributes to odt (unfinished)
    Change-Id: Ib1df7a4c1477693aa2d0ec067635cdcbd393cebd

--- sw/source/ui/misc/bookmark.cxx
+++ sw/source/ui/misc/bookmark.cxx
@@ -208,7 +208,7 @@ IMPL_LINK_NOARG(SwInsertBookmarkDlg, RenameHdl, Button*, void)
 IMPL_LINK_NOARG(SwInsertBookmarkDlg, InsertHdl, Button*, void)
 {
     OUString sBookmark = m_pEditBox->GetText();
-    rSh.SetBookmark(vcl::KeyCode(), sBookmark);
+    rSh.SetBookmark(vcl::KeyCode(), sBookmark, m_pHideCB->IsChecked(), m_pConditionED->GetText());
     rReq.AppendItem(SfxStringItem(FN_INSERT_BOOKMARK, sBookmark));
     rReq.Done();
     if (!rReq.IsDone())
