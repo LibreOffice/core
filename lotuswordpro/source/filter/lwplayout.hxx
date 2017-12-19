b181d66c350416fed0a284b9d8db9c02a25c8dc5
Caolán McNamara <caolanm@redhat.com>
2017-12-19 09:18:54 +0000

    ofz#4689 avoid recurse to death
    Change-Id: Ie578a767cd8646c4e67c302a67b46ce1f1637122
    Reviewed-on: https://gerrit.libreoffice.org/46772
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>

--- lotuswordpro/source/filter/lwplayout.hxx
+++ lotuswordpro/source/filter/lwplayout.hxx
@@ -494,6 +494,7 @@ protected:
     void Read() override;
 protected:
     bool m_bGettingWrapType;
+    bool m_bGettingLayoutRelativity;
     sal_uInt8 m_nWrapType;
     sal_uInt8 m_nBuoyancy;
     sal_Int32 m_nBaseLineOffset;
