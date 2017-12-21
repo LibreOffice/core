09cb05dccf89f69bf3b648d898293e7ddf93ad0e
Yousuf Philips <philipz85@hotmail.com>
2017-12-21 18:57:02 +0400

    tdf#113532 Add Arabic fonts into default installation
    Includes these fonts
    * Amiri
    * KACSTOffice, KACSTBook
    * Reem Kufi
    * Scheherazade
    
    Change-Id: I2071c4c379b2dc88a205e2c284ae0a65cfdc76c9

--- external/more_fonts/Module_more_fonts.mk
+++ external/more_fonts/Module_more_fonts.mk
@@ -11,11 +11,13 @@ $(eval $(call gb_Module_Module,more_fonts))
 
 $(eval $(call gb_Module_add_targets,more_fonts,\
 	ExternalPackage_alef \
+	ExternalPackage_amiri \
 	ExternalPackage_caladea \
 	ExternalPackage_carlito \
 	ExternalPackage_culmus \
 	ExternalPackage_dejavu \
 	ExternalPackage_gentium \
+	ExternalPackage_kacst \
 	ExternalPackage_liberation \
 	ExternalPackage_liberation_narrow \
 	ExternalPackage_libertineg \
@@ -23,13 +25,17 @@ $(eval $(call gb_Module_add_targets,more_fonts,\
 	ExternalPackage_sourcecode \
 	ExternalPackage_sourcesans \
 	ExternalPackage_noto \
+	ExternalPackage_reem \
+	ExternalPackage_scheherazade \
 	ExternalPackage_EmojiOne_Color \
 	UnpackedTarball_alef \
+	UnpackedTarball_amiri \
 	UnpackedTarball_caladea \
 	UnpackedTarball_carlito \
 	UnpackedTarball_culmus \
 	UnpackedTarball_dejavu \
 	UnpackedTarball_gentium \
+	UnpackedTarball_kacst \
 	UnpackedTarball_liberation \
 	UnpackedTarball_liberation_narrow \
 	UnpackedTarball_libertineg \
@@ -37,6 +43,8 @@ $(eval $(call gb_Module_add_targets,more_fonts,\
 	UnpackedTarball_sourcecode \
 	UnpackedTarball_sourcesans \
 	UnpackedTarball_noto \
+	UnpackedTarball_reem \
+	UnpackedTarball_scheherazade \
 	UnpackedTarball_EmojiOne_Color \
 ))
 
