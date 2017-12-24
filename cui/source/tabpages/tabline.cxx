22ee0a3d95ebf6de2394c732f091c07ace2ad1d7
ekuiitr <jhaekansh80@gmail.com>
2017-12-24 22:51:43 +0800

    tdf#111723 Fixed -  Dialog does not remember the last opened tab page.
    Dialog remember the last used tab page
    
    Change-Id: Ife6d0eb9ea13d2a0534f316db001dbb3b6a716eb

--- cui/source/tabpages/tabline.cxx
+++ cui/source/tabpages/tabline.cxx
@@ -93,8 +93,6 @@ SvxLineTabDialog::SvxLineTabDialog
     m_nStyleTabPage = AddTabPage( "RID_SVXPAGE_LINE_DEF", SvxLineDefTabPage::Create, nullptr);
     m_nEndTabPage = AddTabPage( "RID_SVXPAGE_LINEEND_DEF", SvxLineEndDefTabPage::Create, nullptr);
 
-    SetCurPageId( "RID_SVXPAGE_LINE" );
-
     CancelButton& rBtnCancel = GetCancelButton();
     rBtnCancel.SetClickHdl( LINK( this, SvxLineTabDialog, CancelHdlImpl ) );
 }
