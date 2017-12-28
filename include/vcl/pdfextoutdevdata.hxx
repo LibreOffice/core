LibreOffice/core

commit 76ec54e8c9f3580450bca85236a4f5af0c328588

tdf#97662 - Try to preserve original compressed JPEGs harder.
Avoiding de-compressing and re-compressing them saves lots of time too.

Change-Id: Ie8eb68554627581b2f0584a55bbbdb43c9482bed
Reviewed-on: https://gerrit.libreoffice.org/22219
Reviewed-by: Michael Meeks <michael.meeks@collabora.com>
Tested-by: Michael Meeks <michael.meeks@collabora.com>

include/vcl/pdfextoutdevdata.hxx
@@ -86,6 +86,8 @@ class VCL_DLLPUBLIC PDFExtOutDevData : public ExtOutDevData
     bool                        mbExportNDests; //i56629
     sal_Int32                   mnFormsFormat;
     sal_Int32                   mnPage;
+    sal_Int32                   mnCompressionQuality;
+    sal_Int32                   mnMaxImageResolution;
     css::lang::Locale           maDocLocale;
 
     PageSyncData*               mpPageSyncData;
@@ -102,8 +104,6 @@ public:
     void ResetSyncData();
 
     void PlayGlobalActions( PDFWriter& rWriter );
-
-
 
     bool    GetIsExportNotes() const { return mbExportNotes;}
     void        SetIsExportNotes( const bool bExportNotes );
@@ -135,10 +135,16 @@ public:
     sal_Int32   GetCurrentPageNumber() const { return mnPage;}
     void        SetCurrentPageNumber( const sal_Int32 nPage );
 
-    bool    GetIsLosslessCompression() const { return mbUseLosslessCompression;}
+    bool        GetIsLosslessCompression() const { return mbUseLosslessCompression;}
     void        SetIsLosslessCompression( const bool bLosslessCompression );
 
-    bool    GetIsReduceImageResolution() const { return mbReduceImageResolution;}
+    sal_Int32   GetCompressionQuality() const { return mnCompressionQuality; }
+    void        SetCompressionQuality( const sal_Int32 nQuality );
+
+    sal_Int32   GetMaxImageResolution() const { return mnMaxImageResolution; }
+    void        SetMaxImageResolution( const sal_Int32 nQuality );
+
+    bool        GetIsReduceImageResolution() const { return mbReduceImageResolution;}
     void        SetIsReduceImageResolution( const bool bReduceImageResolution );
 
     const css::lang::Locale& GetDocumentLocale() const { return maDocLocale;}
@@ -176,9 +182,15 @@ public:
         rOutputRect, e.g. for cropped graphics.
      */
     void        EndGroup( const Graphic&    rGraphic,
-                          sal_uInt8             nTransparency,
+                          sal_uInt8         nTransparency,
                           const Rectangle&  rOutputRect,
                           const Rectangle&  rVisibleOutputRect );
+
+    /// Detect if stream is compressed enough to avoid de-compress / scale & re-compress
+    bool        HasAdequateCompression( const Graphic &rGraphic,
+                                        const Rectangle &rOutputRect,
+                                        const Rectangle &rVisibleOutputRect ) const;
+
 //--->i56629
     /** Create a new named destination to be used in a link to this document from another PDF document
  (see PDF spec 1.4, 8.2.1)

vcl/source/gdi/pdfextoutdevdata.cxx
@@ -305,6 +305,7 @@ struct PageSyncData
     void PushAction( const OutputDevice& rOutDev, const PDFExtOutDevDataSync::Action eAct );
     bool PlaySyncPageAct( PDFWriter& rWriter, sal_uInt32& rCurGDIMtfAction, const PDFExtOutDevData& rOutDevData );
 };
+
 void PageSyncData::PushAction( const OutputDevice& rOutDev, const PDFExtOutDevDataSync::Action eAct )
 {
     GDIMetaFile* pMtf = rOutDev.GetConnectMetaFile();
@@ -403,13 +404,14 @@ bool PageSyncData::PlaySyncPageAct( PDFWriter& rWriter, sal_uInt32& rCurGDIMtfAc
                     }
                     else if ( aBeg->eAct == PDFExtOutDevDataSync::EndGroupGfxLink )
                     {
-                        if ( rOutDevData.GetIsLosslessCompression() && !rOutDevData.GetIsReduceImageResolution() )
+                        Graphic& rGraphic = mGraphics.front();
+                        if ( rGraphic.IsLink() &&
+                             rGraphic.GetLink().GetType() == GFX_LINK_TYPE_NATIVE_JPG &&
+                             mParaRects.size() >= 2 )
                         {
-                            Graphic& rGraphic = mGraphics.front();
-                            if ( rGraphic.IsLink() && rGraphic.GetLink().GetType() == GFX_LINK_TYPE_NATIVE_JPG )
-                            {
-                                mbGroupIgnoreGDIMtfActions = true;
-                            }
+                            mbGroupIgnoreGDIMtfActions =
+                                rOutDevData.HasAdequateCompression(
+                                        rGraphic, mParaRects[0], mParaRects[1]);
                         }
                         break;
                     }
@@ -504,6 +506,8 @@ PDFExtOutDevData::PDFExtOutDevData( const OutputDevice& rOutDev ) :
     mbExportNDests          ( false ),
     mnFormsFormat           ( 0 ),
     mnPage                  ( -1 ),
+    mnCompressionQuality    ( 90 ),
+    mnMaxImageResolution    ( 300 ),
     mpPageSyncData          ( nullptr ),
     mpGlobalSyncData        ( new GlobalSyncData() )
 {
@@ -527,6 +531,14 @@ void PDFExtOutDevData::SetCurrentPageNumber( const sal_Int32 nPage )
 void PDFExtOutDevData::SetIsLosslessCompression( const bool bUseLosslessCompression )
 {
     mbUseLosslessCompression = bUseLosslessCompression;
+}
+void PDFExtOutDevData::SetCompressionQuality( const sal_Int32 nQuality )
+{
+    mnCompressionQuality = nQuality;
+}
+void PDFExtOutDevData::SetMaxImageResolution( const sal_Int32 nMaxImageResolution )
+{
+    mnMaxImageResolution = nMaxImageResolution;
 }
 void PDFExtOutDevData::SetIsReduceImageResolution( const bool bReduceImageResolution )
 {
@@ -748,7 +760,7 @@ void PDFExtOutDevData::BeginGroup()
 }
 
 void PDFExtOutDevData::EndGroup( const Graphic&     rGraphic,
-                                 sal_uInt8              nTransparency,
+                                 sal_uInt8          nTransparency,
                                  const Rectangle&   rOutputRect,
                                  const Rectangle&   rVisibleOutputRect )
 {
@@ -757,6 +769,49 @@ void PDFExtOutDevData::EndGroup( const Graphic&     rGraphic,
     mpPageSyncData->mParaInts.push_back( nTransparency );
     mpPageSyncData->mParaRects.push_back( rOutputRect );
     mpPageSyncData->mParaRects.push_back( rVisibleOutputRect );
+}
+
+// Avoids expensive de-compression and re-compression of large images.
+bool PDFExtOutDevData::HasAdequateCompression( const Graphic &rGraphic,
+                                               const Rectangle & /* rOutputRect */,
+                                               const Rectangle & /* rVisibleOutputRect */ ) const
+{
+    bool bReduceResolution = false;
+
+    assert( rGraphic.IsLink() && rGraphic.GetLink().GetType() == GFX_LINK_TYPE_NATIVE_JPG );
+
+    // small items better off as PNG anyway
+    if ( rGraphic.GetSizePixel().Width() < 32 &&
+         rGraphic.GetSizePixel().Height() < 32 )
+        return false;
+
+    // FIXME: ideally we'd also pre-empt the DPI related scaling too.
+
+    Size aSize = rGraphic.GetSizePixel();
+    sal_Int32 nCurrentRatio = (100 * aSize.Width() * aSize.Height() * 4) /
+                               rGraphic.GetLink().GetDataSize();
+
+    if ( GetIsLosslessCompression() )
+        return !bReduceResolution && !GetIsReduceImageResolution();
+    else
+    {
+        static const struct {
+            sal_Int32 mnQuality;
+            sal_Int32 mnRatio;
+        } aRatios[] = { // minium tolerable compression ratios
+            { 100, 400 }, { 95, 700 }, { 90, 1000 }, { 85, 1200 },
+            { 80, 1500 }, { 75, 1700 }
+        };
+        sal_Int32 nTargetRatio = 10000;
+        for ( size_t i = 0 ; i < SAL_N_ELEMENTS( aRatios ); ++i )
+        {
+            if ( mnCompressionQuality > aRatios[i].mnQuality )
+                break;
+            nTargetRatio = aRatios[i].mnRatio;
+        }
+
+        return nCurrentRatio > nTargetRatio;
+    }
 }
 
 }

filter/source/pdf/pdfexport.cxx
@@ -818,6 +818,8 @@ bool PDFExport::Export( const OUString& rFile, const Sequence< PropertyValue >& 
                 pPDFExtOutDevData->SetIsExportBookmarks( mbExportBookmarks );
                 pPDFExtOutDevData->SetIsExportHiddenSlides( mbExportHiddenSlides );
                 pPDFExtOutDevData->SetIsLosslessCompression( mbUseLosslessCompression );
+                pPDFExtOutDevData->SetCompressionQuality( mnQuality );
+                pPDFExtOutDevData->SetMaxImageResolution( mnMaxImageResolution );
                 pPDFExtOutDevData->SetIsReduceImageResolution( mbReduceImageResolution );
                 pPDFExtOutDevData->SetIsExportNamedDestinations( mbExportBmkToDest );
 

commit a7bdc4809d8f7ed543801b04f244d377b14ab3c0

tdf#105954 use Reduce image resolution setting during PDF export
regression from 76ec54e8c9f3580450bca85236a4f5af0c328588

Change-Id: I98c5cb1419541451d586e0dd1798e64fa391b3f3

include/vcl/pdfextoutdevdata.hxx
@@ -180,7 +180,7 @@ public:
                           const tools::Rectangle&  rVisibleOutputRect );
 
     /// Detect if stream is compressed enough to avoid de-compress / scale & re-compress
-    bool        HasAdequateCompression( const Graphic &rGraphic ) const;
+    bool        HasAdequateCompression( const Graphic &rGraphic, const tools::Rectangle &rOutputRect ) const;
 
 //--->i56629
     /** Create a new named destination to be used in a link to this document from another PDF document

vcl/source/gdi/pdfextoutdevdata.cxx
@@ -24,6 +24,7 @@
 #include <vcl/dllapi.h>
 #include <basegfx/polygon/b2dpolygon.hxx>
 #include <basegfx/polygon/b2dpolygontools.hxx>
+#include <basegfx/vector/b2dsize.hxx>
 
 #include <set>
 #include <memory>
@@ -404,13 +405,13 @@ bool PageSyncData::PlaySyncPageAct( PDFWriter& rWriter, sal_uInt32& rCurGDIMtfAc
                             GfxLinkType eType = rGraphic.GetLink().GetType();
                             if ( eType == GfxLinkType::NativeJpg )
                             {
-                                mbGroupIgnoreGDIMtfActions = rOutDevData.HasAdequateCompression(rGraphic);
+                                mbGroupIgnoreGDIMtfActions = rOutDevData.HasAdequateCompression(rGraphic, mParaRects.front());
                                 if ( !mbGroupIgnoreGDIMtfActions )
                                     mCurrentGraphic = rGraphic;
                             }
                             else if ( eType == GfxLinkType::NativePng || eType == GfxLinkType::NativePdf )
                             {
-                                if ( eType == GfxLinkType::NativePdf || rOutDevData.HasAdequateCompression(rGraphic) )
+                                if ( eType == GfxLinkType::NativePdf || rOutDevData.HasAdequateCompression(rGraphic, mParaRects.front()) )
                                     mCurrentGraphic = rGraphic;
                             }
                         }
@@ -797,7 +798,7 @@ void PDFExtOutDevData::EndGroup( const Graphic&     rGraphic,
 }
 
 // Avoids expensive de-compression and re-compression of large images.
-bool PDFExtOutDevData::HasAdequateCompression( const Graphic &rGraphic ) const
+bool PDFExtOutDevData::HasAdequateCompression( const Graphic &rGraphic, const tools::Rectangle &rOutputRect ) const
 {
     assert(rGraphic.IsLink() &&
            (rGraphic.GetLink().GetType() == GfxLinkType::NativeJpg ||
@@ -817,7 +818,13 @@ bool PDFExtOutDevData::HasAdequateCompression( const Graphic &rGraphic ) const
     if (GetIsLosslessCompression())
         return !GetIsReduceImageResolution();
 
-    // FIXME: ideally we'd also pre-empt the DPI related scaling too.
+    assert(rOutputRect.Right() - rOutputRect.Left() != 0);
+    // TEMPORARY!!
+    const long nTargetDPI = 300;
+    const long nActualDPI = aSize.Width() * 1440 / (rOutputRect.Right() - rOutputRect.Left());
+    if (nTargetDPI < nActualDPI)
+        return false;
+
     sal_Int32 nCurrentRatio = (100 * aSize.Width() * aSize.Height() * 4) /
                                rGraphic.GetLink().GetDataSize();
 