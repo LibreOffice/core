1632166329e6c14d885229c358a6c60dd2528244
Zdeněk Crhonek <zcrhonek@gmail.com>
2017-12-31 08:49:04 +0100

    uitest - writer Insert Horizontal Line
    Change-Id: Ia44e18650b72ffe8d8d263f62dc6d5a31d1b0dc3
    Reviewed-on: https://gerrit.libreoffice.org/47215
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Markus Mohrhard <markus.mohrhard@googlemail.com>

---
+++ sw/qa/uitest/writer_tests/horizontalLine.py
@@ -0,0 +1,42 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+from uitest.framework import UITestCase
+from libreoffice.uno.propertyvalue import mkPropertyValues
+from uitest.uihelper.common import get_state_as_dict, type_text
+from uitest.debug import sleep
+
+class WriterInsertHorizontalLine(UITestCase):
+
+    def test_insert_horizontal_line(self):
+        self.ui_test.create_doc_in_start_center("writer")
+        xWriterDoc = self.xUITest.getTopFocusWindow()
+        xWriterEdit = xWriterDoc.getChild("writer_edit")
+
+        type_text(xWriterEdit, "Test horizontal line")     #write the text
+
+        self.xUITest.executeCommand(".uno:StyleApply?Style:string=Horizontal%20Line&FamilyName:string=ParagraphStyles") #insert horizontal line
+
+        self.ui_test.execute_dialog_through_command(".uno:EditStyle")  #open style dialog
+        xDialog = self.xUITest.getTopFocusWindow()
+        xStyleNametxt = xDialog.getChild("namerw")
+        self.assertEqual(get_state_as_dict(xStyleNametxt)["Text"], "Horizontal Line") #check style name
+        xCancBtn = xDialog.getChild("cancel")
+        self.ui_test.close_dialog_through_button(xCancBtn)
+
+        self.xUITest.executeCommand(".uno:Undo")
+        self.xUITest.executeCommand(".uno:Redo")
+
+        self.ui_test.execute_dialog_through_command(".uno:EditStyle")  #open style dialog
+        xDialog = self.xUITest.getTopFocusWindow()
+        xStyleNametxt = xDialog.getChild("namerw")
+        self.assertEqual(get_state_as_dict(xStyleNametxt)["Text"], "Horizontal Line")  #check style name
+        xCancBtn = xDialog.getChild("cancel")
+        self.ui_test.close_dialog_through_button(xCancBtn)
+
+        self.ui_test.close_doc()
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
