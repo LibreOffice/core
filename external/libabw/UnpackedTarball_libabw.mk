25d56b07116b176b1c56b998ce052a26e14e2a28
David Tardon <dtardon@redhat.com>
2018-01-02 09:54:19 +0100

    upload libabw 0.1.2
    Change-Id: I972ff9e0dbb73f6a38c886e1acd03cc4d62da2ec

--- external/libabw/UnpackedTarball_libabw.mk
+++ external/libabw/UnpackedTarball_libabw.mk
@@ -13,7 +13,9 @@ $(eval $(call gb_UnpackedTarball_set_tarball,libabw,$(ABW_TARBALL)))
 
 $(eval $(call gb_UnpackedTarball_set_patchlevel,libabw,0))
 
-$(eval $(call gb_UnpackedTarball_update_autoconf_configs,libabw))
+$(eval $(call gb_UnpackedTarball_add_patches,libabw, \
+    external/libabw/libabw-msvc.patch.1 \
+))
 
 ifeq ($(COM_IS_CLANG),TRUE)
 ifneq ($(filter -fsanitize=%,$(CC)),)
