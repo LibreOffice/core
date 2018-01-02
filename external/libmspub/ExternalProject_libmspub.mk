2a92e524d4ca2cd2caf2cd4298867f218dc2c1fd
David Tardon <dtardon@redhat.com>
2018-01-02 13:40:57 +0100

    upload libmspub 0.1.3
    Reviewed-on: https://gerrit.libreoffice.org/47256
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: David Tardon <dtardon@redhat.com>
    (cherry picked from commit a3488acbeb3b618cf5b6b1a93608c58d0eb9d646)
    
    Change-Id: I1f759bf914e4a61fc943098f04661789d374ffdb

--- external/libmspub/ExternalProject_libmspub.mk
+++ external/libmspub/ExternalProject_libmspub.mk
@@ -36,8 +36,7 @@ $(call gb_ExternalProject_get_state_target,libmspub,build) :
 			--disable-weffc \
 			$(if $(verbose),--disable-silent-rules,--enable-silent-rules) \
 			CXXFLAGS="$(gb_CXXFLAGS) $(if $(ENABLE_OPTIMIZED),$(gb_COMPILEROPTFLAGS),$(gb_COMPILERNOOPTFLAGS))" \
-			CPPFLAGS="$(CPPFLAGS) $(ICU_UCHAR_TYPE) $(BOOST_CPPFLAGS) \
-				-DBOOST_ERROR_CODE_HEADER_ONLY -DBOOST_SYSTEM_NO_DEPRECATED" \
+			CPPFLAGS="$(CPPFLAGS) $(ICU_UCHAR_TYPE) $(BOOST_CPPFLAGS)" \
 			$(if $(CROSS_COMPILING),--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM)) \
 		&& $(MAKE) \
 	)
