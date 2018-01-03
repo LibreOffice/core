5eb3bb20c292ee1d8576d2b7a190509e5d79cd9d
Miklos Vajna <vmiklos@collabora.co.uk>
2018-01-03 13:34:46 +0100

    tdf#113357 cui: fix duplicate 'Formatted text [Richtext]' paste option
    Prefer RTF when we have both RICHTEXT and RTF.
    
    Change-Id: Ib4133ae4fdecc32429d89b56b0c9466dd3451522

---
+++ cui/qa/uitest/dialogs/pastedlg.py
@@ -0,0 +1,46 @@
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+from libreoffice.uno.propertyvalue import mkPropertyValues
+from uitest.framework import UITestCase
+from uitest.uihelper.common import get_state_as_dict
+
+
+# Test for SvPasteObjectDialog.
+class Test(UITestCase):
+
+    def testGetFormat(self):
+        # Copy a string in Impress.
+        self.ui_test.create_doc_in_start_center("impress")
+        template = self.xUITest.getTopFocusWindow()
+        self.ui_test.close_dialog_through_button(template.getChild("cancel"))
+        doc = self.xUITest.getTopFocusWindow()
+        editWin = doc.getChild("impress_win")
+        # Select the title shape.
+        editWin.executeAction("TYPE", mkPropertyValues({"KEYCODE": "TAB"}))
+        editWin.executeAction("TYPE", mkPropertyValues({"TEXT": "t"}))
+        self.xUITest.executeCommand(".uno:SelectAll")
+        self.xUITest.executeCommand(".uno:Copy")
+
+        # Now use paste special to see what formats are offered.
+        self.ui_test.execute_dialog_through_command(".uno:PasteSpecial")
+        pasteSpecial = self.xUITest.getTopFocusWindow()
+        formats = pasteSpecial.getChild("list")
+        entryCount = int(get_state_as_dict(formats)["EntryCount"])
+        items = []
+        for index in range(entryCount):
+            formats.executeAction("SELECT", mkPropertyValues({"POS": str(index)}))
+            items.append(get_state_as_dict(formats)["SelectEntryText"])
+
+        # Make sure there is no RTF vs Richtext duplication.
+        self.assertTrue("Formatted text [RTF]" in items)
+        self.assertFalse("Formatted text [Richtext]" in items)
+
+        # Close the dialog and the document.
+        self.ui_test.close_dialog_through_button(pasteSpecial.getChild("cancel"))
+        self.ui_test.close_doc()
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
