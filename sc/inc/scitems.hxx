010830d40c225c07fe74fe914d7e5915636a0191
Noel Grandin <noel.grandin@collabora.co.uk>
2018-01-05 08:46:26 +0200

    SCITEM_PRINTWARN is unused
    and mpPoolDefaults doesn't need to be a pointer
    
    Change-Id: I02735b22466711d7260d2ff0cbc6bf0cab5d83b2

--- sc/inc/scitems.hxx
+++ sc/inc/scitems.hxx
@@ -41,10 +41,9 @@
 #define SCITEM_PIVOTDATA            1106
 #define SCITEM_SOLVEDATA            1107
 #define SCITEM_USERLIST             1108
-#define SCITEM_PRINTWARN            1109
-#define SCITEM_CONDFORMATDLGDATA    1110
+#define SCITEM_CONDFORMATDLGDATA    1109
 
-#define MSGPOOL_END                 1110
+#define MSGPOOL_END                 1109
 
 // Item-IDs for attributes:
 
