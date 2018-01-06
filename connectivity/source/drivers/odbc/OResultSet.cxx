LibreOffice/core

commit 12d5e57dcac22c288ef23075b82e3e3e87929912

tdf#112947: fix write to free'd memory (odbc)
suggested by Nick Gorham

Change-Id: I3579caf5192df6e460dea3a5b3728196d75a65f4
Reviewed-on: https://gerrit.libreoffice.org/43234
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

connectivity/source/drivers/odbc/OResultSet.cxx
@@ -164,6 +164,7 @@ OResultSet::OResultSet(SQLHANDLE _pStatementHandle ,OStatement_Base* pStmt) :   
 
 OResultSet::~OResultSet()
 {
+    setStmtOption<SQLUSMALLINT*, SQL_IS_POINTER>(SQL_ATTR_ROW_STATUS_PTR, nullptr);
 }
 
 void OResultSet::construct()

commit 098bb73f054d005c4a11d4847c3c3536242dd8c4

tdf#114495: fix crash in odbc resultset dtr
See bt https://bugs.documentfoundation.org/attachment.cgi?id=138913

Regression from https://cgit.freedesktop.org/libreoffice/core/commit/?id=12d5e57dcac22c288ef23075b82e3e3e87929912

Change-Id: Ic079b87370207a1e51d747e250caea2bfcbc8c17

connectivity/source/drivers/odbc/OResultSet.cxx
@@ -164,7 +164,8 @@ OResultSet::OResultSet(SQLHANDLE _pStatementHandle ,OStatement_Base* pStmt) :   
 
 OResultSet::~OResultSet()
 {
-    setStmtOption<SQLUSMALLINT*, SQL_IS_POINTER>(SQL_ATTR_ROW_STATUS_PTR, nullptr);
+    if (m_xStatement)
+        setStmtOption<SQLUSMALLINT*, SQL_IS_POINTER>(SQL_ATTR_ROW_STATUS_PTR, nullptr);
 }
 
 void OResultSet::construct()