38365f071d78fa751311fa1938284419851ce376
Tamas Bunth <tamas.bunth@collabora.co.uk>
2018-01-08 15:57:39 +0100

    Add HSQLDB schema import
    It can be enabled by initializing the DBACCESS_HSQL_MIGRATION variable.
    
    Create new library "dbahsql" which is responsible for migrating the
    embedded hsql database to any database covered by sdbc.
    
    The hsqldb schema is stored in a file named "script" in form of SQL
    statements. The SQL statements used by DBMS's differ mostly by the
    defined types. Because of that, only the create statements need to be
    parsed, alter statements will work (with a little luck) without actually
    modifying it.
    
    User / security settings which can occur in the script file (e.g. GRANT
    statements) are dropped. Statements starting with SET are also dropped
    (they are hsql specific stuff)
    
    Change-Id: I6a22942e8a9a76765f80e50f0ad68f4d72e1ff9d

---
+++ dbaccess/source/filter/hsqldb/columndef.cxx
@@ -0,0 +1,41 @@
+
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#include "columndef.hxx"
+#include <com/sun/star/sdbc/DataType.hpp>
+
+namespace dbahsql
+{
+using namespace css::sdbc;
+
+ColumnDefinition::ColumnDefinition(const OUString& sName, sal_Int32 eType,
+                                   const std::vector<sal_Int32> aParams, bool bPrimary,
+                                   sal_Int32 nAutoIncr, bool bNullable)
+    : m_sName(sName)
+    , m_eType(eType)
+    , m_aParams(aParams)
+    , m_bPrimaryKey(bPrimary)
+    , m_nAutoIncrement(nAutoIncr)
+    , m_bNullable(bNullable)
+{
+}
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
