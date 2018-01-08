a60ccf6a44f4d6a3b160afb36b74ccedc4e0dff2
Mert Tümer <merttumer7@gmail.com>
2018-01-08 12:07:44 +0300

    tdf#107026 render the new page without reopen the document on Android Viewer
    The patch was sent for the ULAKBIM/Pardus project.
    Signed-off-by: Mert Tümer <merttumer7@gmail.com>
    
    Change-Id: I91c902bfd1acc5cc70ad30f16e0719e7a325702b

--- android/source/src/java/org/libreoffice/LOKitThread.java
+++ android/source/src/java/org/libreoffice/LOKitThread.java
@@ -169,6 +169,15 @@ class LOKitThread extends Thread {
         String partPageRectString = ((LOKitTileProvider) mTileProvider).getPartPageRectangles();
         List<RectF> partPageRectangles = mInvalidationHandler.convertPayloadToRectangles(partPageRectString);
         mContext.getDocumentOverlay().setPartPageRectangles(partPageRectangles);
+
+        /* render the new page */
+        mTileProvider.updatePageCount();
+        mLayerClient.setPageRect(0, 0, mTileProvider.getPageWidth(), mTileProvider.getPageHeight());
+        mViewportMetrics = mLayerClient.getViewportMetrics();
+        mLayerClient.setViewportMetrics(mViewportMetrics);
+        mLayerClient.forceRedraw();
+        mLayerClient.forceRender();
+        LOKitShell.moveViewportTo(mContext, new PointF(mContext.getCurrentCursorPosition().left, mContext.getCurrentCursorPosition().top), mLayerClient.getViewportMetrics().getZoomFactor());
     }
 
     private void updateZoomConstraints() {
--- android/source/src/java/org/libreoffice/TileProvider.java
+++ android/source/src/java/org/libreoffice/TileProvider.java
@@ -59,6 +59,11 @@ public interface TileProvider {
      */
     int getCurrentPartNumber();
 
+     /**
+      *  Updates the page count to render
+     */
+    void updatePageCount();
+
     /**
      * Get the total number of parts.
      */
