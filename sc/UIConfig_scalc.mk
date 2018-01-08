1eaf02599a03e03d2731aa07d75d1fcce23511de
vikasmahato <vikasmahato0@gmail.com>
2018-01-08 20:54:08 +0530

    tdf#36943
     Clicking 'Edit Points' button in Calc doesnt show 'Edit Points' toolbar
    
    Change-Id: Iba95869a2b7783dac9dbd02788663c3be9a2c96d

--- sc/UIConfig_scalc.mk
+++ sc/UIConfig_scalc.mk
@@ -48,6 +48,7 @@ $(eval $(call gb_UIConfig_add_toolbarfiles,modules/scalc,\
 	sc/uiconfig/scalc/toolbar/arrowsbar \
 	sc/uiconfig/scalc/toolbar/arrowshapes \
 	sc/uiconfig/scalc/toolbar/basicshapes \
+	sc/uiconfig/scalc/toolbar/bezierobjectbar \
 	sc/uiconfig/scalc/toolbar/calloutshapes \
 	sc/uiconfig/scalc/toolbar/classificationbar \
 	sc/uiconfig/scalc/toolbar/colorbar \
