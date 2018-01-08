074e739f44b2d3c041a4ff29b2a1f594e43dd241
David Tardon <dtardon@redhat.com>
2018-01-08 20:30:43 +0100

    upload libpagemaker 0.0.4
    Change-Id: Idc4b7eaa3331ee3831f7d27ca66663e23c30b8c9

--- external/libpagemaker/ExternalProject_libpagemaker.mk
+++ external/libpagemaker/ExternalProject_libpagemaker.mk
@@ -34,8 +34,7 @@ $(call gb_ExternalProject_get_state_target,libpagemaker,build) :
 			--disable-weffc \
 			$(if $(verbose),--disable-silent-rules,--enable-silent-rules) \
 			CXXFLAGS="$(gb_CXXFLAGS) $(if $(ENABLE_OPTIMIZED),$(gb_COMPILEROPTFLAGS),$(gb_COMPILERNOOPTFLAGS))" \
-			CPPFLAGS="$(CPPFLAGS) $(BOOST_CPPFLAGS) \
-				-DBOOST_ERROR_CODE_HEADER_ONLY -DBOOST_SYSTEM_NO_DEPRECATED" \
+			CPPFLAGS="$(CPPFLAGS) $(BOOST_CPPFLAGS)" \
 			$(if $(CROSS_COMPILING),--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM)) \
 		&& $(MAKE) \
 	)
