0cff17a62d16f295284cdb17040a03b176ca7cf0
Zdeněk Crhonek <zcrhonek@gmail.com>
2018-01-09 23:25:46 +0100

    uitest - writer Document properties; tdf#49683; tdf#81457
    Change-Id: If4a202a7b8ae9be11b8f8f05e50eb3202c5c655d
    Reviewed-on: https://gerrit.libreoffice.org/47685
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Markus Mohrhard <markus.mohrhard@googlemail.com>

---
+++ sw/qa/uitest/writer_tests/tdf81457.py
@@ -0,0 +1,35 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+from uitest.framework import UITestCase
+from libreoffice.uno.propertyvalue import mkPropertyValues
+from uitest.path import get_srcdir_url
+from uitest.uihelper.common import get_state_as_dict
+import time
+from uitest.debug import sleep
+from uitest.uihelper.common import select_pos
+
+def get_url_for_data_file(file_name):
+   return get_srcdir_url() + "/sw/qa/uitest/writer_tests/data/" + file_name
+
+class tdf81457(UITestCase):
+
+#tdf 81457
+   def test_open_documentProperties_tdf81457(self):
+        writer_doc = self.ui_test.load_file(get_url_for_data_file("tdf81457.odt"))
+        self.ui_test.execute_dialog_through_command(".uno:SetDocumentProperties")
+        xDialog = self.xUITest.getTopFocusWindow()
+        xTabs = xDialog.getChild("tabcontrol")
+        select_pos(xTabs, "2")     #tab Custom properties
+        xOkBtn = xDialog.getChild("ok")
+        xOkBtn.executeAction("CLICK", tuple())
+
+        self.ui_test.close_doc()
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
