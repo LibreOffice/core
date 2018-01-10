26a3659c3695feba606ed63cf487420bdd1c1b7a
Milian Wolff <milian.wolff@kdab.com>
2018-01-10 16:26:10 +0100

    Extend build system to support linking against KDE Frameworks 5
    Pass --enable-kde5 to autogen.sh to enable this feature. Then
    add kde5 to the list of externals to link against KF5. Note that
    we have to disable the kde5 vclplug, as that does not compile in
    it's current form. I will introduce other code that depends on
    KF5 though which will leverage this feature.
    
    Change-Id: I17e434a53ac769000b0f805b1f41cdc5c2c84ee2

--- vcl/Module_vcl.mk
+++ vcl/Module_vcl.mk
@@ -81,12 +81,13 @@ $(eval $(call gb_Module_add_targets,vcl,\
     Library_vclplug_kde4 \
 ))
 endif
-ifneq ($(ENABLE_KDE5),)
-$(eval $(call gb_Module_add_targets,vcl,\
-    CustomTarget_kde5_moc \
-    Library_vclplug_kde5 \
-))
-endif
+# NOTE: does not compile
+# ifneq ($(ENABLE_KDE5),)
+# $(eval $(call gb_Module_add_targets,vcl,\
+#     CustomTarget_kde5_moc \
+#     Library_vclplug_kde5 \
+# ))
+# endif
 ifneq ($(ENABLE_QT5),)
 $(eval $(call gb_Module_add_targets,vcl,\
     CustomTarget_qt5_moc \
