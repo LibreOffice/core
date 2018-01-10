4d78cf97d7b1629556df68fc461922fda930d9ec
Milian Wolff <milian.wolff@kdab.com>
2018-01-10 16:26:10 +0100

    Extend build system to support linking against KDE Frameworks 5
    Pass --enable-kde5 to autogen.sh to enable this feature. Then
    add kde5 to the list of externals to link against KF5. I will
    introduce other code that depends on KF5 though which will
    leverage this feature.
    
    Change-Id: I17e434a53ac769000b0f805b1f41cdc5c2c84ee2
    Reviewed-on: https://gerrit.libreoffice.org/47715
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>

--- scp2/Module_scp2.mk
+++ scp2/Module_scp2.mk
@@ -39,7 +39,7 @@ $(eval $(call gb_Module_add_targets,scp2,\
 	$(if $(filter TRUE,$(ENABLE_EVOAB2) $(ENABLE_GIO) $(ENABLE_GTK) $(ENABLE_GTK3)),\
 		InstallModule_gnome \
 	) \
-	$(if $(filter TRUE,$(ENABLE_KDE4) $(ENABLE_QT5)),\
+	$(if $(filter TRUE,$(ENABLE_KDE4) $(ENABLE_QT5) $(ENABLE_KDE5)),\
 		InstallModule_kde \
 	) \
 ))
