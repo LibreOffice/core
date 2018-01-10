183fd9a58ae3eeb749b78e6f4ca5783c79b43acb
Marco Cecchetti <marco.cecchetti@collabora.com>
2018-01-10 17:31:35 +0100

    Revert "lok: sc: copy / paste confusion -- workaround"
    This reverts commit f0684222540b96d8366ce175c522b64472361b80.
    
    This workaround is no more needed because of commit 1b7a827
    
    Change-Id: Icd13b245d5965c2b2ece326f7f5705cf06cf1b64
    Reviewed-on: https://gerrit.libreoffice.org/47761
    Reviewed-by: Marco Cecchetti <mrcekets@gmail.com>
    Tested-by: Marco Cecchetti <mrcekets@gmail.com>

--- sc/source/ui/view/viewfun3.cxx
+++ sc/source/ui/view/viewfun3.cxx
@@ -227,14 +227,6 @@ bool ScViewFunc::CopyToClip( ScDocument* pClipDoc, const ScRangeList& rRanges, b
             // and lose the 'if' above
             aClipParam.setSourceDocID( pDoc->GetDocumentID() );
 
-            // This is only a workaround, which doesn't allow to paste content
-            // in one view which has been copied in a different view.
-            // TODO: implement a solution providing one clipboard per view
-            if (comphelper::LibreOfficeKit::isActive())
-            {
-                aClipParam.setSourceView(GetViewData().GetViewShell());
-            }
-
             if (SfxObjectShell* pObjectShell = pDoc->GetDocumentShell())
             {
                 // Copy document properties from pObjectShell to pClipDoc (to its clip options, as it has no object shell).
@@ -874,20 +866,6 @@ bool ScViewFunc::PasteFromClip( InsertDeleteFlags nFlags, ScDocument* pClipDoc,
 
     if (GetViewData().SelectionForbidsCellFill())
         return false;
-
-    // This is only a workaround, which doesn't allow to paste content
-    // in one view which has been copied in a different view.
-    // TODO: implement a solution providing one clipboard per view
-    if (comphelper::LibreOfficeKit::isActive())
-    {
-        ScTabViewShell* pThisView = GetViewData().GetViewShell();
-        ScTabViewShell* pSourceView = dynamic_cast<ScTabViewShell*>(pClipDoc->GetClipParam().getSourceView());
-
-        if (pThisView && pSourceView && pThisView != pSourceView)
-        {
-            return false;
-        }
-    }
 
     //  undo: save all or no content
     InsertDeleteFlags nContFlags = InsertDeleteFlags::NONE;
