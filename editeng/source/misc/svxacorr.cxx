86c20ecd7cd675991e30b068c46e768e17937fc0
Fyodor Yemelyanenko <fyodor_e@hotmail.com>
2018-01-10 10:27:08 +1000

    tdf#83260 editeng sw: avoid accessing dead nodes in AutoCorrect
    When change tracking is ON (Redlining) and Edit->Change Tracking->Show==OFF
    Autocorrection and Undo operations crash LO.
    
    Change-Id: I616f2de143b78fc83483a6589cfa1dd1ab61675a

--- editeng/source/misc/svxacorr.cxx
+++ editeng/source/misc/svxacorr.cxx
@@ -1382,7 +1382,7 @@ void SvxAutoCorrect::DoAutoCorrect( SvxAutoCorrDoc& rDoc, const OUString& rTxt,
 
                     if( IsAutoCorrFlag( ChgToEnEmDash ) )
                     {
-                        FnChgToEnEmDash( rDoc, rTxt, nCapLttrPos, nEnd, eLang );
+                        FnChgToEnEmDash( rDoc, aPara, nCapLttrPos, nEnd, eLang );
                     }
                 }
                 break;
