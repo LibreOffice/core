6f4b9e30580f3cb91591b0e33f799da2a69b1ae4
Mert Tümer <merttumer7@gmail.com>
2018-01-12 15:12:16 +0300

    [Pardus] tdf#108446 - Shape is not changed after save
    This patch is sponsored by ULAKBIM/PARDUS project.
    Signed-off-by: Mert Tümer <merttumer7@gmail.com>
    
    Change-Id: I7ee137b2332f799e4301cc3507df4a7b86af74b9

--- android/source/src/java/org/libreoffice/LibreOfficeMainActivity.java
+++ android/source/src/java/org/libreoffice/LibreOfficeMainActivity.java
@@ -115,7 +115,7 @@ public class LibreOfficeMainActivity extends AppCompatActivity implements Settin
     private boolean isKeyboardOpen = false;
     private boolean isFormattingToolbarOpen = false;
     private boolean isSearchToolbarOpen = false;
-    private boolean isDocumentChanged = false;
+    private static boolean isDocumentChanged = false;
     public boolean isNewDocument = false;
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -757,7 +757,7 @@ public class LibreOfficeMainActivity extends AppCompatActivity implements Settin
         return mIsSpreadsheet;
     }
 
-    public void setDocumentChanged (boolean changed) {
+    public static void setDocumentChanged (boolean changed) {
         isDocumentChanged = changed;
     }
 
