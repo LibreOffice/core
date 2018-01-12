8145696c9e52456755801e03cea3a09887824d74
Jan Holesovsky <kendy@collabora.com>
2018-01-12 11:57:22 +0000

    lokdialog: Allow closing the Format Cell dialogs in any order.
    Includes also lots of infrastructural changes, making the
    conversion of the rest of the dialogs much easier.
    
    StartExecuteAsync should be used in-place of StartExecuteModal
    and the latter removed from the code-base incrementally. More
    common code from Dialog::Execute should be moved to
    ImplStartExecuteModal in a next step, as this is used more widely.
    
    Change-Id: Idb2c1ec790e38f582438471a0419a56cdcf1439d

--- vcl/source/window/window.cxx
+++ vcl/source/window/window.cxx
@@ -2479,6 +2479,8 @@ bool Window::IsCallHandlersOnInputDisabled() const
 
 void Window::EnableInput( bool bEnable, bool bChild )
 {
+    if (!mpWindowImpl)
+        return;
 
     bool bNotify = (bEnable != mpWindowImpl->mbInputDisabled);
     if ( mpWindowImpl->mpBorderWindow )
@@ -2542,6 +2544,9 @@ void Window::EnableInput( bool bEnable, bool bChild )
 
 void Window::EnableInput( bool bEnable, const vcl::Window* pExcludeWindow )
 {
+    if (!mpWindowImpl)
+        return;
+
     EnableInput( bEnable );
 
     // pExecuteWindow is the first Overlap-Frame --> if this
