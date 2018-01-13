360903b3c31f317ef04d8a585ac0c3e77c38d80f
Jim Raykowski <raykowj@gmail.com>
2018-01-13 13:57:19 -0900

    uitest for tdf#114724 headings outline tracking in Navigator
    Change-Id: I4390c3d33d69ddd93880545f58d0ef81469c960d

--- sw/source/uibase/uitest/uiobject.cxx
+++ sw/source/uibase/uitest/uiobject.cxx
@@ -12,6 +12,8 @@
 #include <edtwin.hxx>
 #include <view.hxx>
 #include <wrtsh.hxx>
+#include <navipi.hxx>
+#include <sfx2/sidebar/Sidebar.hxx>
 
 SwEditWinUIObject::SwEditWinUIObject(const VclPtr<SwEditWin>& xEditWin):
     WindowUIObject(xEditWin),
@@ -96,6 +98,19 @@ void SwEditWinUIObject::execute(const OUString& rAction,
             getWrtShell(mxEditWin).SelectText(nStartPos, nEndPos);
         }
     }
+    else if (rAction == "SIDEBAR")
+    {
+        SfxViewFrame* pViewFrm = SfxViewFrame::Current();
+        DBG_ASSERT(pViewFrm, "SwEditWinUIObject::execute: no viewframe");
+        pViewFrm->ShowChildWindow(SID_SIDEBAR);
+
+        if (rParameters.find("PANEL") != rParameters.end())
+        {
+            auto itr = rParameters.find("PANEL");
+            OUString aVal = itr->second;
+            ::sfx2::sidebar::Sidebar::ShowPanel(aVal, pViewFrm->GetFrame().GetFrameInterface());
+        }
+    }
     else
         WindowUIObject::execute(rAction, rParameters);
 }
@@ -111,5 +126,44 @@ std::unique_ptr<UIObject> SwEditWinUIObject::create(vcl::Window* pWindow)
     assert(pEditWin);
     return std::unique_ptr<UIObject>(new SwEditWinUIObject(pEditWin));
 }
+
+SwNavigationPIUIObject::SwNavigationPIUIObject(const VclPtr<SwNavigationPI>& xSwNavigationPI):
+    WindowUIObject(xSwNavigationPI),
+    mxSwNavigationPI(xSwNavigationPI)
+{
+}
+
+StringMap SwNavigationPIUIObject::get_state()
+{
+    StringMap aMap = WindowUIObject::get_state();
+
+    aMap["selectioncount"] = OUString::number(mxSwNavigationPI->m_aContentTree->GetSelectionCount());
+
+    return aMap;
+}
+
+void SwNavigationPIUIObject::execute(const OUString& rAction,
+        const StringMap& rParameters)
+{
+    if (rAction == "ROOT")
+    {
+        mxSwNavigationPI->m_aContentToolBox->TriggerItem(mxSwNavigationPI->m_aContentToolBox->GetItemId("root"));
+    }
+    else
+        WindowUIObject::execute(rAction, rParameters);
+}
+
+std::unique_ptr<UIObject> SwNavigationPIUIObject::create(vcl::Window* pWindow)
+{
+    SwNavigationPI* pSwNavigationPI = dynamic_cast<SwNavigationPI*>(pWindow);
+    assert(pSwNavigationPI);
+    return std::unique_ptr<UIObject>(new SwNavigationPIUIObject(pSwNavigationPI));
+}
+
+OUString SwNavigationPIUIObject::get_name() const
+{
+    return OUString("SwNavigationPIUIObject");
+}
+
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
