6339072a5638995925ea403e71f01a454fdac74e
Michael Stahl <mstahl@redhat.com>
2018-01-16 17:13:22 +0100

    comphelper: put some include guards around stark naked header
    Change-Id: I407dc215ea6948518d0d744c73ff2de717063962

--- include/comphelper/hash.hxx
+++ include/comphelper/hash.hxx
@@ -7,6 +7,9 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+#ifndef INCLUDED_COMPHELPER_HASH_HXX
+#define INCLUDED_COMPHELPER_HASH_HXX
+
 #include <comphelper/comphelperdllapi.h>
 
 #include <memory>
@@ -44,5 +47,7 @@ public:
 };
 
 }
+
+#endif // INCLUDED_COMPHELPER_HASH_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
