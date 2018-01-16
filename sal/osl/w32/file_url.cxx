cfaadaf83cd45df68245a0dfbd55c523d81c7673
Stephan Bergmann <sbergman@redhat.com>
2018-01-16 09:04:05 +0100

    Missing include
    Change-Id: I3bcc4ff11e37afe29b847ff65f722c7b0ba2c0b0

--- sal/osl/w32/file_url.cxx
+++ sal/osl/w32/file_url.cxx
@@ -17,6 +17,10 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
+#include <sal/config.h>
+
+#include <algorithm>
+
 #include <systools/win32/uwinapi.h>
 
 #include "file_url.hxx"
