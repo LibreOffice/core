dd317d594d7f3c9073753d90f0927038301d47e6
Noel Grandin <noel.grandin@collabora.co.uk>
2018-01-17 17:09:14 +0200

    loplugin:useuniqueptr in XMLTextFieldExport
    Change-Id: I2b3e1ec5454bc3486fb41b010091adc549500daf

--- xmloff/inc/txtflde.hxx
+++ xmloff/inc/txtflde.hxx
@@ -151,9 +151,9 @@ class XMLTextFieldExport final
     SvXMLExport& rExport;
 
     /// store used text field master names (NULL means: don't collect)
-    ::std::map<
+    std::unique_ptr< ::std::map<
             css::uno::Reference< css::text::XText >,
-            ::std::set< OUString > > *
+            ::std::set< OUString > > >
         pUsedMasters;
 
 public:
@@ -481,7 +481,7 @@ private:
     const OUString sPropertyTooltip;
     const OUString sPropertyTextRange;
 
-    XMLPropertyState* pCombinedCharactersPropertyState;
+    std::unique_ptr<XMLPropertyState> pCombinedCharactersPropertyState;
 
 };
 
