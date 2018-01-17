94058881fc3bfd08929bf5021a77d54396f9ddc4
Justin Luth <justin_luth@sil.org>
2018-01-17 12:13:00 +0300

    writerfilter: remove redundant sStyleIdentifierI
    Originally, StyleIdentiferI was the string name, and
    StyleIdentifierD was an integer. That changed a long
    time ago.
    
    Both identifiers are only assigned a value once, and
    both are given identical values at the same time.
    Thus, one is completely redundant and is being removed.
    
    Change-Id: Ia7e9bbeec53110fa8e56cdfb13b9871486f335fa

--- writerfilter/source/dmapper/StyleSheetTable.hxx
+++ writerfilter/source/dmapper/StyleSheetTable.hxx
@@ -53,8 +53,7 @@ class StyleSheetEntry
 {
     std::vector<css::beans::PropertyValue> m_aInteropGrabBag;
 public:
-    OUString sStyleIdentifierI;
-    OUString sStyleIdentifierD;
+    OUString sStyleIdentifierD;   // WW8 name
     bool            bIsDefaultStyle;
     bool            bInvalidHeight;
     bool            bHasUPE; //universal property expansion
