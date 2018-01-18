LibreOffice/core

commit e68b6e349c31ac1376e90218013e1e7612f2b522

Optimize plain-text import.
Cost of managing rsids is very significant >50% of import time, new
Rsids will be generated for changes anyway.
Also avoid un-necessary splitting of paragraphs on new-line.

Change-Id: Ib959306f187f93bd3a85ead11fd2342b99d4ece3
Reviewed-on: https://gerrit.libreoffice.org/36938
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Michael Meeks <michael.meeks@collabora.com>

sw/source/filter/ascii/parasc.cxx
@@ -481,7 +481,7 @@ sal_uLong SwASCIIParser::ReadChars()
         {
             // We found a CR/LF, thus save the text
             InsertText( OUString( pLastStt ));
-            pDoc->getIDocumentContentOperations().SplitNode( *pPam->GetPoint(), false );
+            pDoc->getIDocumentContentOperations().AppendTextNode( *pPam->GetPoint() );
             pLastStt = pStt;
             nLineLen = 0;
         }
@@ -498,8 +498,6 @@ sal_uLong SwASCIIParser::ReadChars()
 void SwASCIIParser::InsertText( const OUString& rStr )
 {
     pDoc->getIDocumentContentOperations().InsertString( *pPam, rStr );
-    pDoc->UpdateRsid( *pPam, rStr.getLength() );
-    pDoc->UpdateParRsid( pPam->GetPoint()->nNode.GetNode().GetTextNode() );
 
     if( pItemSet && g_pBreakIt && nScript != ( SvtScriptType::LATIN |
                                              SvtScriptType::ASIAN |

commit 5d7e7f0490cd37d7852d746bb6921164781ad1f5

tdf:115088: Pasting multi-line text from external source behaves strangely
Regression from:
e68b6e349c31ac1376e90218013e1e7612f2b522

AppendTextNode might be sufficient for import (when there is no
content in the document), but for clipboard operations better to
sue the original SplitNode method.

Change-Id: I7de3e9bc9c5356acb0b4cc43927a0d39079522b2

sw/source/filter/ascii/parasc.cxx
@@ -472,7 +472,10 @@ ErrCode SwASCIIParser::ReadChars()
         {
             // We found a CR/LF, thus save the text
             InsertText( OUString( pLastStt ));
-            pDoc->getIDocumentContentOperations().AppendTextNode( *pPam->GetPoint() );
+            if(bNewDoc)
+                pDoc->getIDocumentContentOperations().AppendTextNode( *pPam->GetPoint() );
+            else
+                pDoc->getIDocumentContentOperations().SplitNode( *pPam->GetPoint(), false );
             pLastStt = pStt;
             nLineLen = 0;
         }