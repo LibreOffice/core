c221405cb0e5d412e34dab2aabdf037d4b048ce2
Zdeněk Crhonek <zcrhonek@gmail.com>
2018-01-18 07:41:37 +0100

    uitest - writer insert Page Footer; insert Envelope
    Change-Id: Ifd931998607edbb526ec756f80b2bad13f541d63
    Reviewed-on: https://gerrit.libreoffice.org/48086
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Markus Mohrhard <markus.mohrhard@googlemail.com>

---
+++ sw/qa/uitest/writer_tests/insertEnvelope.py
@@ -0,0 +1,55 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+from uitest.framework import UITestCase
+from uitest.debug import sleep
+from uitest.path import get_srcdir_url
+from libreoffice.uno.propertyvalue import mkPropertyValues
+from uitest.uihelper.common import get_state_as_dict, type_text
+
+#envaddresspage.ui
+
+class WriterInsertEnvelope(UITestCase):
+
+    def test_insert_envelope(self):
+        self.ui_test.create_doc_in_start_center("writer")
+        document = self.ui_test.get_component()
+        self.ui_test.execute_dialog_through_command(".uno:InsertEnvelope")
+
+        xDialog = self.xUITest.getTopFocusWindow()
+
+        xAddrTxt= xDialog.getChild("addredit")
+        xSenderTxt = xDialog.getChild("senderedit")
+        xSenderCheckBox = xDialog.getChild("sender")
+
+        xAddrTxt.executeAction("SELECT", mkPropertyValues({"FROM": "1", "TO": "200"}))
+        xAddrTxt.executeAction("BACKSPACE", tuple())
+        xAddrTxt.executeAction("TYPE", mkPropertyValues({"KEYCODE":"CTRL+A"}))
+        xAddrTxt.executeAction("BACKSPACE", tuple())
+        xAddrTxt.executeAction("TYPE", mkPropertyValues({"TEXT":"Address"}))
+
+        xSenderTxt.executeAction("SELECT", mkPropertyValues({"FROM": "1", "TO": "200"}))
+        xSenderTxt.executeAction("BACKSPACE", tuple())
+        xSenderTxt.executeAction("TYPE", mkPropertyValues({"KEYCODE":"CTRL+A"}))
+        xSenderTxt.executeAction("BACKSPACE", tuple())
+        xSenderTxt.executeAction("TYPE", mkPropertyValues({"TEXT":"Sender"}))
+
+        xInsBtn = xDialog.getChild("user")
+        xInsBtn.executeAction("CLICK", tuple())
+
+        self.ui_test.execute_dialog_through_command(".uno:InsertEnvelope")
+        xDialog = self.xUITest.getTopFocusWindow()
+        xAddrTxt= xDialog.getChild("addredit")
+        xSenderTxt = xDialog.getChild("senderedit")
+        self.assertEqual(get_state_as_dict(xAddrTxt)["Text"], "Address")
+        self.assertEqual(get_state_as_dict(xSenderTxt)["Text"], "Sender")
+
+        xcancelBtn = xDialog.getChild("cancel")
+        self.ui_test.close_dialog_through_button(xcancelBtn)
+
+        self.ui_test.close_doc()
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
---
+++ sw/qa/uitest/writer_tests/insertPageFooter.py
@@ -0,0 +1,75 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+from uitest.framework import UITestCase
+from uitest.debug import sleep
+from uitest.path import get_srcdir_url
+
+class WriterInsertPageFooter(UITestCase):
+
+    def insert_footer(self):
+        document = self.ui_test.get_component()
+
+        self.assertEqual(
+            document.StyleFamilies.PageStyles.Standard.FooterIsOn, False)
+
+        self.xUITest.executeCommand(
+            ".uno:InsertPageFooter?PageStyle:string=Default%20Style&On:bool=true")
+
+        self.assertEqual(
+            document.StyleFamilies.PageStyles.Standard.FooterIsOn, True)
+
+    def delete_footer(self):
+        document = self.ui_test.get_component()
+
+        self.assertEqual(
+            document.StyleFamilies.PageStyles.Standard.FooterIsOn, True)
+
+        self.ui_test.execute_dialog_through_command(
+            ".uno:InsertPageFooter?PageStyle:string=Default%20Style&On:bool=false")
+
+        xDialog = self.xUITest.getTopFocusWindow()
+
+        xOption = xDialog.getChild("yes")
+        xOption.executeAction("CLICK", tuple())
+
+        self.assertEqual(
+            document.StyleFamilies.PageStyles.Standard.FooterIsOn, False)
+
+    def test_footer(self):
+        self.ui_test.create_doc_in_start_center("writer")
+
+        self.insert_footer()
+
+        self.delete_footer()
+
+        self.ui_test.close_doc()
+
+    def test_tdf107427(self):
+        self.ui_test.create_doc_in_start_center("writer")
+
+        self.insert_footer()
+
+        self.ui_test.execute_dialog_through_command(".uno:InsertTable")
+
+        xInsertDlg = self.xUITest.getTopFocusWindow()
+
+        xOkBtn = xInsertDlg.getChild("ok")
+        xOkBtn.executeAction("CLICK", tuple())
+
+        document = self.ui_test.get_component()
+
+        tables = document.getTextTables()
+        self.assertEqual(len(tables[0].getRows()), 2)
+        self.assertEqual(len(tables[0].getColumns()), 2)
+
+        self.xUITest.executeCommand(".uno:SelectAll")
+
+        self.delete_footer()
+
+        self.ui_test.close_doc()
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
