f72cd9bd5a51afd531de914bf81ab041b2cb92b7
Pranav Kant <pranavk@collabora.co.uk>
2018-01-18 19:06:23 +0530

    lokdialog: No modal mode in LOK
    ... otherwise the cursor in the document goes invisible as soon as the
    dialog is launched.
    
    Change-Id: I77bc30879550b354e1a9593db3c2db7ad3c68193
    Reviewed-on: https://gerrit.libreoffice.org/48126
    Reviewed-by: pranavk <pranavk@collabora.co.uk>
    Tested-by: pranavk <pranavk@collabora.co.uk>
    (cherry picked from commit 0e8d0fe001ceecea95ad7115b835527e13252a17)

--- sfx2/source/view/viewfrm.cxx
+++ sfx2/source/view/viewfrm.cxx
@@ -67,6 +67,7 @@
 #include <rtl/ustrbuf.hxx>
 
 #include <unotools/ucbhelper.hxx>
+#include <comphelper/lok.hxx>
 #include <comphelper/processfactory.hxx>
 #include <comphelper/namedvaluecollection.hxx>
 #include <comphelper/docpasswordrequest.hxx>
@@ -2249,6 +2250,10 @@ OUString SfxViewFrame::GetActualPresentationURL_Impl() const
 
 void SfxViewFrame::SetModalMode( bool bModal )
 {
+    // no real modality for LOK
+    if (comphelper::LibreOfficeKit::isActive())
+        return;
+
     m_pImpl->bModal = bModal;
     if ( m_xObjSh.is() )
     {
