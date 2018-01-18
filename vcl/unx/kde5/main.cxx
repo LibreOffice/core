62811b032226d9c88c4dd8d8d16418fa077d89e1
Katarina Behrens <Katarina.Behrens@cib.de>
2018-01-18 10:31:55 +0100

    Band aid fix build --with-kde5
    Change-Id: Ifbcfdbb74bffaf88296351ec72536e16a4073b60

--- vcl/unx/kde5/main.cxx
+++ vcl/unx/kde5/main.cxx
@@ -17,7 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#include <QtGui/QApplication>
+#include <QtWidgets/QApplication>
 
 #include "KDE5Data.hxx"
 #include "KDE5SalInstance.hxx"
