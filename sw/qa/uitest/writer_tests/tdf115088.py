3c9b620efc314050246bef9b7ef9c6daeb482ae1
Zdeněk Crhonek <zcrhonek@gmail.com>
2018-01-19 20:38:34 +0100

    add uitest for bug tdf#115088
    Change-Id: Ic13f399cd489755ce97e026a9259c96c8aceb45e
    Reviewed-on: https://gerrit.libreoffice.org/48220
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Markus Mohrhard <markus.mohrhard@googlemail.com>

---
+++ sw/qa/uitest/writer_tests/tdf115088.py
@@ -0,0 +1,48 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from uitest.framework import UITestCase
+from libreoffice.uno.propertyvalue import mkPropertyValues
+from uitest.uihelper.common import get_state_as_dict, type_text
+import time
+from uitest.debug import sleep
+
+class tdf115088(UITestCase):
+
+   def test_tdf115088(self):
+        self.ui_test.create_doc_in_start_center("writer")
+        document = self.ui_test.get_component()
+        xWriterDoc = self.xUITest.getTopFocusWindow()
+        xWriterEdit = xWriterDoc.getChild("writer_edit")
+
+        type_text(xWriterEdit, "1")
+        xWriterEdit.executeAction("TYPE", mkPropertyValues({"KEYCODE": "RETURN"}))
+        type_text(xWriterEdit, "1")
+
+        selection = self.xUITest.executeCommand(".uno:SelectAll")  #select whole text
+        self.xUITest.executeCommand(".uno:Cut")   #cut  text
+        type_text(xWriterEdit, "test")
+        self.xUITest.executeCommand(".uno:GoLeft")
+        self.xUITest.executeCommand(".uno:GoLeft")
+        self.xUITest.executeCommand(".uno:GoLeft")
+        self.xUITest.executeCommand(".uno:GoLeft")
+        self.xUITest.executeCommand(".uno:PasteUnformatted")
+        self.assertEqual(document.Text.String[0:2], "1\n")
+
+        self.ui_test.close_doc()
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
+
+
+
+
+
+
+
+
+
+
+
+
