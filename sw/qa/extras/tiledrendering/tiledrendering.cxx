cc2ee044d9fc34d112842ca6e4150567238c7e4f
Stephan Bergmann <sbergman@redhat.com>
2018-01-19 09:05:45 +0100

    Blind fix for sal_Int32 = long
    ...after 252ce90e39648c47bcfdb39547d0ac82bacdbae6 "sw lokit: add page syles to
    header & footer"
    
    Change-Id: Ia0b3f8194d889fc1022da08bac06c85cbb22dd3b

--- sw/qa/extras/tiledrendering/tiledrendering.cxx
+++ sw/qa/extras/tiledrendering/tiledrendering.cxx
@@ -1953,7 +1953,7 @@ void checkPageHeaderOrFooter(const SfxViewShell* pViewShell, sal_uInt16 nWhich, 
     pListItem->GetStringList(aSeq);
     if (bValue)
     {
-        CPPUNIT_ASSERT_EQUAL(1, aSeq.getLength());
+        CPPUNIT_ASSERT_EQUAL(sal_Int32(1), aSeq.getLength());
         CPPUNIT_ASSERT_EQUAL(OUString("Default Style"), aSeq[0]);
     }
     else
