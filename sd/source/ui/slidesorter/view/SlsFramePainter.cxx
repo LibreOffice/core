4d06269ea4b1de63c2a32adc21f94814d07f7701
Chris Sherlock <chris.sherlock79@gmail.com>
2018-01-21 04:48:50 +1100

    vcl: remove BitmapColor Color() operator
    BitmapColor has a Color() operator. However, this is confusing and
    tends to hide that the two classes aren't the same. I have converted this
    to GetColor().
    
    Change-Id: I0be2dcb3fc420e7be9c8d04330e7a3fe69a5412a

--- sd/source/ui/slidesorter/view/SlsFramePainter.cxx
+++ sd/source/ui/slidesorter/view/SlsFramePainter.cxx
@@ -82,7 +82,8 @@ void FramePainter::AdaptColor (
     Bitmap::ScopedReadAccess pReadAccess(aBitmap);
     if (!pReadAccess)
         return;
-    const Color aSourceColor = pReadAccess->GetColor(0,0);
+    const BitmapColor aBmpSourceColor = pReadAccess->GetColor(0,0);
+    const Color aSourceColor = aBmpSourceColor.GetColor();
     pReadAccess.reset();
 
     // Erase the center bitmap.
