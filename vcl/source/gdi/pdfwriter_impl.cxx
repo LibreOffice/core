LibreOffice/core

commit 64d624b65124ac02d8ee59b135593fd9d8eb9067

Fix typos
Change-Id: I9a5940027423ff0791fa7da0b79b617412ce6b86
Reviewed-on: https://gerrit.libreoffice.org/21209
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Ashod Nakashian <ashnakash@gmail.com>

vcl/source/gdi/pdfwriter_impl.cxx
@@ -3768,7 +3768,7 @@ sal_Int32 PDFWriterImpl::createToUnicodeCMap( sal_uInt8* pEncoding,
     }
     aContents.append( "endbfchar\n"
                       "endcmap\n"
-                      "CMapName currentdict /CMap defineresource pop\n"
+                      "CMapName currentdict /CMap define resource pop\n"
                       "end\n"
                       "end\n" );
 #ifndef DEBUG_DISABLE_PDFCOMPRESSION
@@ -5570,7 +5570,7 @@ bool PDFWriterImpl::emitWidgetAnnotations()
             else if( rWidget.m_nTextStyle & DrawTextFlags::Right )
                 aLine.append( "/Q 2\n" );
         }
-        // appearance charactristics for terminal fields
+        // appearance characteristics for terminal fields
         // which are supposed to have an appearance constructed
         // by the viewer application
         if( !rWidget.m_aMKDict.isEmpty() )
@@ -7772,7 +7772,7 @@ sal_Int32 PDFWriterImpl::emitOutputIntent()
     appendLiteralStringEncrypt( aComment ,nOIObject, aLine );
     aLine.append("/DestOutputProfile ");
     aLine.append( nICCObject );
-    aLine.append( " 0 R>>\nendobj\n\n" );;
+    aLine.append( " 0 R>>\nendobj\n\n" );
     if ( !writeBuffer( aLine.getStr(), aLine.getLength() ) ) return 0;
 
     return nOIObject;
@@ -12257,7 +12257,7 @@ sal_Int32 PDFWriterImpl::setLinkURL( sal_Int32 nLinkId, const OUString& rURL )
     if (!m_xTrans.is())
     {
         uno::Reference< uno::XComponentContext > xContext( comphelper::getProcessComponentContext() );
-        m_xTrans = util::URLTransformer::create(xContext);;
+        m_xTrans = util::URLTransformer::create(xContext);
     }
 
     util::URL aURL;

commit d2a614196559b6993a5a71ad34f9288704d27873

its defineresource not define resource
regression from

commit 64d624b65124ac02d8ee59b135593fd9d8eb9067
Date:   Sat Jan 9 22:55:28 2016 +0100

    Fix typos

    Change-Id: I9a5940027423ff0791fa7da0b79b617412ce6b86
    Reviewed-on: https://gerrit.libreoffice.org/21209
    Tested-by: Jenkins <ci@libreoffice.org>

Change-Id: Ifdf45e97b81523c84c73b93d14ed75b7b6909f77

vcl/source/gdi/pdfwriter_impl.cxx
@@ -3058,7 +3058,7 @@ sal_Int32 PDFWriterImpl::createToUnicodeCMap( sal_uInt8 const * pEncoding,
     }
     aContents.append( "endbfchar\n"
                       "endcmap\n"
-                      "CMapName currentdict /CMap define resource pop\n"
+                      "CMapName currentdict /CMap defineresource pop\n"
                       "end\n"
                       "end\n" );
     SvMemoryStream aStream;