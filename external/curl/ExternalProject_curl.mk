f406d38638119fef40aa99b9412d515fb979855c
Michael Stahl <mstahl@redhat.com>
2018-01-24 11:44:26 +0100

    curl: upgrade to release 7.58.0
    * fixes 2 CVEs
    * disable some new optional dependencies
    
    Change-Id: If7725d126e68de04b67969a83c0ea08573a43679
    Reviewed-on: https://gerrit.libreoffice.org/48493
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Michael Stahl <mstahl@redhat.com>
    (cherry picked from commit 5e3799a0c8a92918b9e1868c942f8918ff61c003)
    Reviewed-on: https://gerrit.libreoffice.org/48539
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>

--- external/curl/ExternalProject_curl.mk
+++ external/curl/ExternalProject_curl.mk
@@ -43,10 +43,11 @@ $(call gb_ExternalProject_get_state_target,curl,build):
 			$(if $(filter IOS MACOSX,$(OS)),\
 				--with-darwinssl,\
 				$(if $(ENABLE_NSS),--with-nss$(if $(SYSTEM_NSS),,="$(call gb_UnpackedTarball_get_dir,nss)/dist/out"),--without-nss)) \
-			--without-ssl --without-gnutls --without-polarssl --without-cyassl --without-axtls \
+			--without-ssl --without-gnutls --without-polarssl --without-cyassl --without-axtls --without-mbedtls \
 			--enable-ftp --enable-http --enable-ipv6 \
 			--without-libidn2 --without-libpsl --without-librtmp \
 			--without-libssh2 --without-metalink --without-nghttp2 \
+			--without-libssh --without-brotli \
 			--disable-ares \
 			--disable-dict --disable-file --disable-gopher --disable-imap \
 			--disable-ldap --disable-ldaps --disable-manual --disable-pop3 \
