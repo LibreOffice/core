0553ef7642fec066bf1d8e463f2dca035ab21443
Vasily Melenchuk <Vasily.Melenchuk@cib.de>
2018-01-25 16:00:07 +0300

    Support for webdav:// and webdavs:// schemes from command line
    We have currently support for WebDAV schemes as vnd.sun.star.webdav://
    but it is not user and tools friendly.
    
    Some related refactoring in ucb to reuse existing code instead of
    manual parsing of URLs.
    
    Change-Id: Ib14411796d6cf11afd80d5132abe67d4e46a42e1

--- ucb/CppunitTest_ucb_webdav_res_access.mk
+++ ucb/CppunitTest_ucb_webdav_res_access.mk
@@ -24,6 +24,7 @@ $(eval $(call gb_CppunitTest_use_libraries,ucb_webdav_res_access, \
 	salhelper \
 	test \
 	ucbhelper \
+	tl \
 ))
 
 $(eval $(call gb_CppunitTest_use_library_objects,ucb_webdav_res_access, \
