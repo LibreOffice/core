LibreOffice/core

commit 18765b9fa739337d2d891513f6e2fb7c3ce23b50

tdf#114306 fix unexpected page break in row-spanned table
If a para gets moved off to another page, it never gets
moved back. Make IsMoveable() more symmetric, add condition
to MoveBwd to also claim table content back.

Change-Id: I5366eb824f0ef7016599c777786cbdf42f65b9b5
Reviewed-on: https://gerrit.libreoffice.org/46021
Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>
Tested-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>

sw/qa/extras/uiwriter/uiwriter.cxx
@@ -289,6 +289,7 @@ public:
     void testTdf99689TableOfTables();
     void testTdf113790();
     void testTdf108048();
+    void testTdf114306();
 
     CPPUNIT_TEST_SUITE(SwUiWriterTest);
     CPPUNIT_TEST(testReplaceForward);
@@ -458,6 +459,7 @@ public:
     CPPUNIT_TEST(testTdf99689TableOfTables);
     CPPUNIT_TEST(testTdf113790);
     CPPUNIT_TEST(testTdf108048);
+    CPPUNIT_TEST(testTdf114306);
     CPPUNIT_TEST_SUITE_END();
 
 private:
@@ -5241,6 +5243,19 @@ void SwUiWriterTest::testTdf112025()
 
     uno::Reference<beans::XPropertySet> xStyle(getStyles("PageStyles")->getByName("Standard"), uno::UNO_QUERY);
     CPPUNIT_ASSERT_EQUAL(true, getProperty<bool>(xStyle, "IsLandscape"));
+}
+
+void SwUiWriterTest::testTdf114306()
+{
+    load(DATA_DIRECTORY, "fdo114306.odt");
+    xmlDocPtr pXmlDoc = parseLayoutDump();
+
+    // There are 2 long paragraphs in cell A1.
+    // A part of paragraph 2 should flow over to the second page but
+    // *not* the whole paragraph. There should be 2 paragraphs on
+    // page 1 and 1 paragraph on page 2.
+    assertXPath(pXmlDoc, "/root/page[1]/body/tab[1]/row[1]/cell[1]/txt", 2);
+    assertXPath(pXmlDoc, "/root/page[2]/body/tab[1]/row[1]/cell[1]/txt", 1);
 }
 
 void SwUiWriterTest::testTdf108524()

sw/source/core/layout/findfrm.cxx
@@ -1303,8 +1303,14 @@ bool SwFrame::IsMoveable( const SwLayoutFrame* _pLayoutFrame ) const
                   _pLayoutFrame->IsInDocBody() ||
                   _pLayoutFrame->IsInFootnote() )
         {
+            // If IsMovable() is called before a MoveFwd() the method
+            // may return false if there is no NextCellLeaf. If
+            // IsMovable() is called before a MoveBwd() the method may
+            // return false if there is no PrevCellLeaf.
             if ( _pLayoutFrame->IsInTab() && !IsTabFrame() &&
-                 ( !IsContentFrame() || !const_cast<SwFrame*>(this)->GetNextCellLeaf() ) )
+                 ( !IsContentFrame() || (!const_cast<SwFrame*>(this)->GetNextCellLeaf()
+                                      && !const_cast<SwFrame*>(this)->GetPrevCellLeaf()) )
+                )
             {
                 bRetVal = false;
             }

sw/source/core/layout/flowfrm.cxx
@@ -2021,14 +2021,19 @@ bool SwFlowFrame::MoveBwd( bool &rbReformat )
         const SwLayoutFrame* pUpperFrame = m_rThis.GetUpper();
         while ( pUpperFrame )
         {
-            if ( pUpperFrame->IsTabFrame() )
+            if ( pUpperFrame->IsTabFrame() || pUpperFrame->IsRowFrame() )
             {
                 return false;
             }
             // If the text frame is a follow-section-in-table, that can move
             // backward as well.
             bool bIsFollowSection = pUpperFrame->IsSctFrame() && static_cast<const SwSectionFrame*>(pUpperFrame)->GetPrecede();
-            if ( ( pUpperFrame->IsColumnFrame() && pUpperFrame->IsInSct() ) || bIsFollowSection )
+
+            // If the text frame is a follow-in-table, that can move
+            // backward as well.
+            bool bIsFollow = const_cast<SwLayoutFrame*>(pUpperFrame)->GetPrevCellLeaf();
+
+            if ( ( pUpperFrame->IsColumnFrame() && pUpperFrame->IsInSct() ) || bIsFollowSection || bIsFollow )
             {
                 break;
             }

commit 22c822d6c2dadacdcbb8bed219635a97fa327cc5

tdf#114306 fix crash caused by special document
Regression from 18765b9fa739337d2d891513f6e2fb7c3ce23b50

Change-Id: Ie00435aa4bffa3d2e49896aea6894ae999a5536a

sw/source/core/layout/tabfrm.cxx
@@ -1589,7 +1589,32 @@ static void lcl_RecalcRow( SwRowFrame& rRow, long nBottom )
         {
             // #115759# - force another format of the
             // lowers, if at least one of it was invalid.
+
+            // tdf#114306 writer may crash because rRow points to a deleted SwRowFrame
+            SwRowFrame* pRow = &rRow;
+            OSL_ENSURE(pRow->GetUpper() && pRow->GetUpper()->IsTabFrame(), "No table");
+            SwTabFrame* pTab = static_cast<SwTabFrame*>(pRow->GetUpper());
+
             bCheck = SwContentFrame::CalcLowers( &rRow, rRow.GetUpper(), nBottom, true );
+
+            bool bRowStillExists = false;
+            SwFrame* pTestRow = pTab->Lower();
+
+            while (pTestRow)
+            {
+                if (pTestRow == pRow)
+                {
+                    bRowStillExists = true;
+                    break;
+                }
+                pTestRow = pTestRow->GetNext();
+            }
+
+            if (!bRowStillExists)
+            {
+                SAL_WARN("sw.layout", "no row anymore at " << pRow);
+                return;
+            }
 
             // NEW TABLES
             // First we calculate the cells with row span of < 1, afterwards

sw/qa/extras/uiwriter/uiwriter.cxx
@@ -294,6 +294,7 @@ public:
     void testTdf113790();
     void testTdf108048();
     void testTdf114306();
+    void testTdf114306_2();
     void testTdf113481();
     void testTdf115013();
     void testTdf114536();
@@ -471,6 +472,7 @@ public:
     CPPUNIT_TEST(testTdf113790);
     CPPUNIT_TEST(testTdf108048);
     CPPUNIT_TEST(testTdf114306);
+    CPPUNIT_TEST(testTdf114306_2);
     CPPUNIT_TEST(testTdf113481);
     CPPUNIT_TEST(testTdf115013);
     CPPUNIT_TEST(testTdf114536);
@@ -5289,6 +5291,17 @@ void SwUiWriterTest::testTdf114306()
     assertXPath(pXmlDoc, "/root/page[1]/body/tab[1]/row[1]/cell[1]/txt", 2);
     assertXPath(pXmlDoc, "/root/page[2]/body/tab[1]/row[1]/cell[1]/txt", 1);
 }
+
+void SwUiWriterTest::testTdf114306_2()
+{
+    // tdf#114306 fix unexpected page break in row-spanned table
+    // load regression document without writer crash
+    load(DATA_DIRECTORY, "fdo114306_2.odt");
+
+    // correct number of pages
+    CPPUNIT_ASSERT_EQUAL(4, getPages());
+}
+
 
 // During insert of the document with list inside into the main document inside the list
 // we should merge both lists into one, when they have the same list properties