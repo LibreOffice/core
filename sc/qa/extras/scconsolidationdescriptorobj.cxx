5b5c1beb0008b52bd856ee32652396b555c6eb4a
Jens Carl <j.carl43@gmx.de>
2018-01-27 01:40:45 +0000

    Remove shared mxComponent in scconsolidationdescriptorobj
    Change-Id: I4b71673fa19459eeb7ea97bb291a62f327d69958

--- sc/qa/extras/scconsolidationdescriptorobj.cxx
+++ sc/qa/extras/scconsolidationdescriptorobj.cxx
@@ -23,8 +23,6 @@ using namespace com::sun::star;
 
 namespace sc_apitest
 {
-#define NUMBER_OF_TESTS 12
-
 class ScConsolidationDescriptorObj : public CalcUnoApiTest, public apitest::XConsolidationDescriptor
 {
 public:
@@ -53,12 +51,8 @@ public:
     CPPUNIT_TEST_SUITE_END();
 
 private:
-    static sal_Int32 nTest;
-    static uno::Reference<lang::XComponent> mxComponent;
+    uno::Reference<lang::XComponent> mxComponent;
 };
-
-sal_Int32 ScConsolidationDescriptorObj::nTest = 0;
-uno::Reference<lang::XComponent> ScConsolidationDescriptorObj::mxComponent;
 
 ScConsolidationDescriptorObj::ScConsolidationDescriptorObj()
     : CalcUnoApiTest("/sc/qa/extras/testdocuments")
@@ -67,10 +61,6 @@ ScConsolidationDescriptorObj::ScConsolidationDescriptorObj()
 
 uno::Reference<uno::XInterface> ScConsolidationDescriptorObj::init()
 {
-    // create a calc document
-    if (!mxComponent.is())
-        mxComponent = loadFromDesktop("private:factory/scalc");
-
     uno::Reference<sheet::XSpreadsheetDocument> xDoc(mxComponent, UNO_QUERY_THROW);
     CPPUNIT_ASSERT_MESSAGE("no calc document", xDoc.is());
 
@@ -80,19 +70,14 @@ uno::Reference<uno::XInterface> ScConsolidationDescriptorObj::init()
 
 void ScConsolidationDescriptorObj::setUp()
 {
-    nTest++;
-    CPPUNIT_ASSERT(nTest <= NUMBER_OF_TESTS);
     CalcUnoApiTest::setUp();
+    // create a calc document
+    mxComponent = loadFromDesktop("private:factory/scalc");
 }
 
 void ScConsolidationDescriptorObj::tearDown()
 {
-    if (nTest == NUMBER_OF_TESTS)
-    {
-        closeDocument(mxComponent);
-        mxComponent.clear();
-    }
-
+    closeDocument(mxComponent);
     CalcUnoApiTest::tearDown();
 }
 
