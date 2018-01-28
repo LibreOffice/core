d13c5b153b7798ed18062b67c5d4185590b5b22e
Zdeněk Crhonek <zcrhonek@gmail.com>
2018-01-28 21:43:40 +0100

    uitest for tdf#99386
    Change-Id: I1b9d03da4e5dff6149c98057976adc1cdcdb1770
    Reviewed-on: https://gerrit.libreoffice.org/48802
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Markus Mohrhard <markus.mohrhard@googlemail.com>

---
+++ sc/qa/uitest/calc_tests/tdf99386.py
@@ -0,0 +1,38 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+from uitest.framework import UITestCase
+from uitest.uihelper.common import get_state_as_dict
+from libreoffice.uno.propertyvalue import mkPropertyValues
+from uitest.debug import sleep
+from uitest.uihelper.calc import enter_text_to_cell
+from uitest.path import get_srcdir_url
+from libreoffice.calc.document import get_cell_by_position
+import time
+
+def get_url_for_data_file(file_name):
+    return get_srcdir_url() + "/sc/qa/uitest/calc_tests/data/" + file_name
+
+class tdf99386(UITestCase):
+
+    def test_td99386_undo_merged_cell_needs_hard_recalculate(self):
+
+        calc_doc = self.ui_test.load_file(get_url_for_data_file("tdf99386.ods"))
+
+        xCalcDoc = self.xUITest.getTopFocusWindow()
+        xGridWindow = xCalcDoc.getChild("grid_window")
+        document = self.ui_test.get_component()
+
+        xGridWindow.executeAction("SELECT", mkPropertyValues({"RANGE": "A1:B1"})) #select cells
+        self.xUITest.executeCommand(".uno:ToggleMergeCells") # merge cells
+        self.xUITest.executeCommand(".uno:Undo")
+
+        self.assertEqual(get_cell_by_position(document, 0, 1, 1).getString(), "This")
+
+        self.ui_test.close_doc()
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
