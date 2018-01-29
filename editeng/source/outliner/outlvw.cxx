780edd38cabdb98b3a752d997907ce217dea218c
Noel Grandin <noel.grandin@collabora.co.uk>
2018-01-29 11:12:08 +0200

    loplugin:useuniqueptr in Outliner
    Change-Id: I68568dd61ef6ba99fcb0ac386170bfe67ab2bbd7

--- editeng/source/outliner/outlvw.cxx
+++ editeng/source/outliner/outlvw.cxx
@@ -52,7 +52,7 @@ using namespace ::com::sun::star;
 OutlinerView::OutlinerView( Outliner* pOut, vcl::Window* pWin )
 {
     pOwner                      = pOut;
-    pEditView.reset( new EditView( pOut->pEditEngine, pWin ) );
+    pEditView.reset( new EditView( pOut->pEditEngine.get(), pWin ) );
 }
 
 OutlinerView::~OutlinerView()
