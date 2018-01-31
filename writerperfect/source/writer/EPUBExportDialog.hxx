70d9d0ba67d3b1bce372587e2a46fed1dc62391d
Miklos Vajna <vmiklos@collabora.co.uk>
2018-01-31 22:06:29 +0100

    EPUB export: can use std::move() here
    And get rid of an easy to avoid include if we are at it.
    
    Change-Id: I63cb4628a3a7038aab91c66f592920923a31d7a6

--- writerperfect/source/writer/EPUBExportDialog.hxx
+++ writerperfect/source/writer/EPUBExportDialog.hxx
@@ -27,7 +27,7 @@ namespace writerperfect
 class EPUBExportDialog : public ModalDialog
 {
 public:
-    EPUBExportDialog(vcl::Window *pParent, comphelper::SequenceAsHashMap &rFilterData, css::uno::Reference<css::uno::XComponentContext> xContext, const css::uno::Reference<css::lang::XComponent> &xDocument);
+    EPUBExportDialog(vcl::Window *pParent, comphelper::SequenceAsHashMap &rFilterData, css::uno::Reference<css::uno::XComponentContext> xContext, css::uno::Reference<css::lang::XComponent> xDocument);
     ~EPUBExportDialog() override;
     void dispose() override;
 
