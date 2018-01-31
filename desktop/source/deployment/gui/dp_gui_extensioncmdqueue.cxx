be13e756d64c461681c79457b5de4a22b6ec00d4
Mike Kaganski <mike.kaganski@collabora.com>
2018-01-31 12:39:29 +0300

    desktop: MSVC: pragma warning: make more specific, remove obsolete
    Change-Id: I61ed9700c904b7499166d32e4ffda69811d64a8b
    Reviewed-on: https://gerrit.libreoffice.org/48973
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

--- desktop/source/deployment/gui/dp_gui_extensioncmdqueue.cxx
+++ desktop/source/deployment/gui/dp_gui_extensioncmdqueue.cxx
@@ -81,17 +81,10 @@
 #include <memory>
 
 #ifdef _WIN32
-#if defined _MSC_VER
-#pragma warning (push, 1)
-#pragma warning (disable: 4005)
-#endif
 #if !defined WIN32_LEAN_AND_MEAN
 # define WIN32_LEAN_AND_MEAN
 #endif
 #include <windows.h>
-#if defined _MSC_VER
-#pragma warning (pop)
-#endif
 #include <objbase.h>
 #endif
 
