ba582f9217c5ba86ea6c8b09119a685776ae824e
Mike Kaganski <mike.kaganski@collabora.com>
2018-01-31 20:41:53 +0300

    odk: MSVC: pragma warning: make more specific, remove obsolete
    Change-Id: Ie5957edb3954507505a7df9fad9f8da6b87b09d0

--- odk/source/unowinreg/win/unowinreg.cxx
+++ odk/source/unowinreg/win/unowinreg.cxx
@@ -18,16 +18,10 @@
  */
 
 
-#if defined _MSC_VER
-#pragma warning(push, 1)
-#endif
 #if !defined WIN32_LEAN_AND_MEAN
 # define WIN32_LEAN_AND_MEAN
 #endif
 #include <windows.h>
-#if defined _MSC_VER
-#pragma warning(pop)
-#endif
 
 #include <jni.h>
 
