254b03b3700047076a7de565e6ccb05971cc1147
Katarina Behrens <Katarina.Behrens@cib.de>
2018-02-01 12:08:34 +0100

    Don't recycle DocumentInserter, create a new one every time
    After all, everyone else using DocumentInserter across the codebase
    does the same.
    
    For KDE4, an attempt to reuse the same filepicker dialog sporadically
    crashes in the depths of (old-ish, possibly buggy) libQt due to nested
    event loop and/or recursive repaints.
    
    (crash scenario: Calc > Sheet > Link to external data > Browse > double-click
    a file > Browse again > Kaboom!)
    
    Change-Id: Ib494203abd07873919b8394a442f09c5329d237c

--- sc/source/ui/miscdlgs/linkarea.cxx
+++ sc/source/ui/miscdlgs/linkarea.cxx
@@ -83,8 +83,7 @@ void ScLinkedAreaDlg::dispose()
 
 IMPL_LINK_NOARG(ScLinkedAreaDlg, BrowseHdl, Button*, void)
 {
-    if ( !pDocInserter )
-        pDocInserter = new sfx2::DocumentInserter(this, ScDocShell::Factory().GetFactoryName());
+    pDocInserter.reset( new sfx2::DocumentInserter(this, ScDocShell::Factory().GetFactoryName()) );
     pDocInserter->StartExecuteModal( LINK( this, ScLinkedAreaDlg, DialogClosedHdl ) );
 }
 
