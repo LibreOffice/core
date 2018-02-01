22f6e25987744da57644be95f59b45c1ff51a5d2
Jens Carl <j.carl43@gmx.de>
2018-02-01 22:36:08 +0000

    tdf#45904 Move _SheetCellRange Java tests to C++
    Change-Id: Ia4ebe9d0418e33d245437a49c7ce9551b4a06e82
    Reviewed-on: https://gerrit.libreoffice.org/49124
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Jens Carl <j.carl43@gmx.de>

--- test/Library_subsequenttest.mk
+++ test/Library_subsequenttest.mk
@@ -54,6 +54,7 @@ $(eval $(call gb_Library_add_exception_objects,subsequenttest,\
 	test/source/sheet/spreadsheetviewsettings \
 	test/source/sheet/subtotaldescriptor \
 	test/source/sheet/sheetcell \
+	test/source/sheet/sheetcellrange \
 	test/source/sheet/tableautoformat \
 	test/source/sheet/tablevalidation \
 	test/source/sheet/xarealink \
