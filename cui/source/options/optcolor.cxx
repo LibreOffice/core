296ebed05273c22aca0922d010af1ec8915c45a7
ekuiitr <jhaekansh80@gmail.com>
2018-02-01 20:30:35 +0530

    tdf#113853 - Traveling with tab in Options -> Aplication colors behaves weird
    This patch fix the weird behavior. Currently selected item is visible
    when the view is scrolled and page is return back to start after whole page is
    travelled
    
    Change-Id: I56f57134384d3cf4b04fb4e08de3d014f7ddff5f

--- cui/source/options/optcolor.cxx
+++ cui/source/options/optcolor.cxx
@@ -938,25 +938,16 @@ IMPL_LINK(ColorConfigCtrl_Impl, ColorHdl, SvxColorListBox&, rBox, void)
 IMPL_LINK(ColorConfigCtrl_Impl, ControlFocusHdl, Control&, rCtrl, void)
 {
     // determine whether a control is completely visible
-    // and make it visible
     long aCtrlPosY = rCtrl.GetPosPixel().Y();
     unsigned const nWinHeight = m_pScrollWindow->GetSizePixel().Height();
     unsigned const nEntryHeight = m_pScrollWindow->GetEntryHeight();
     if ((GetFocusFlags::Tab & rCtrl.GetGetFocusFlags()) &&
-        (aCtrlPosY < 0 || nWinHeight < aCtrlPosY + nEntryHeight)
+        (aCtrlPosY < 0 || nWinHeight < aCtrlPosY + nEntryHeight || nWinHeight > aCtrlPosY + nEntryHeight)
     ) {
         long nThumbPos = m_pVScroll->GetThumbPos();
-        if (nWinHeight < aCtrlPosY + nEntryHeight)
         {
             //scroll down
-            nThumbPos += 2;
-        }
-        else
-        {
-            //scroll up
-            nThumbPos -= 2;
-            if(nThumbPos < 0)
-                nThumbPos = 0;
+            nThumbPos = aCtrlPosY/(nEntryHeight+10); //nEntryHeight will be different for different plateforms which will change nThumbPos accordingly.
         }
         m_pVScroll->SetThumbPos(nThumbPos);
         ScrollHdl(m_pVScroll);
