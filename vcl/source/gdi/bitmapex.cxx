e365f19e6f9d069700297983a3d3557b465589e9
Chris Sherlock <chris.sherlock79@gmail.com>
2018-02-03 13:50:53 +1100

    vcl: BitmapColor now inherits from Color
    Change-Id: Idb114bc33f68f01c88f03c12ec3e2373886f3721

--- vcl/source/gdi/bitmapex.cxx
+++ vcl/source/gdi/bitmapex.cxx
@@ -334,7 +334,11 @@ bool BitmapEx::Invert()
         bRet = aBitmap.Invert();
 
         if (bRet && (eTransparent == TransparentType::Color))
-            aTransparentColor = BitmapColor(aTransparentColor).Invert().GetColor();
+        {
+            BitmapColor aColor(aTransparentColor);
+            aColor.Invert();
+            aTransparentColor = BitmapColor(aColor);
+        }
     }
 
     return bRet;
@@ -725,8 +729,7 @@ sal_uInt8 BitmapEx::GetTransparency(sal_Int32 nX, sal_Int32 nY) const
 
                     if(pRead)
                     {
-                        const BitmapColor aBmpColor = pRead->GetColor(nY, nX);
-                        const Color aColor = aBmpColor.GetColor();
+                        const BitmapColor aColor = pRead->GetColor(nY, nX);
 
                         // If color is not equal to TransparentColor, we are not transparent
                         if (aColor != aTransparentColor)
