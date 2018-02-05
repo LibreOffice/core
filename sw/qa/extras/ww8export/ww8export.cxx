453bdd11aefc0fb198e1c3f7b30042f3c70a1991
Michael Stahl <mstahl@redhat.com>
2018-02-05 16:53:03 +0100

    sw: disable testTableKeep on WNT
    Fails from time to time, like so:
    https://tinderbox.libreoffice.org/cgi-bin/gunzip.cgi?tree=MASTER&brief-log=1517842001.29090#err2
    
    Change-Id: Ia33193fbfa05da58d31bcc5008beea32c1feb2fe
    (cherry picked from commit b2a053160c4c8eb0acd66b74c52dbda23a654bda)

--- sw/qa/extras/ww8export/ww8export.cxx
+++ sw/qa/extras/ww8export/ww8export.cxx
@@ -1295,7 +1295,7 @@ DECLARE_WW8EXPORT_TEST(testCommentExport, "comment-export.odt")
     }
 }
 
-#if !defined(MACOSX)
+#if !defined(MACOSX) && !defined(_WIN32)
 #if !TEST_FONTS_MISSING
 DECLARE_WW8EXPORT_TEST(testTableKeep, "tdf91083.odt")
 {
