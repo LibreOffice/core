575a2b0e25eedd7c113c376f57980bc9ce2f3825
Caolán McNamara <caolanm@redhat.com>
2018-02-07 12:19:10 +0000

    add sft fuzzer
    Change-Id: I0ee43e78b5366f2c23fb0bac9a103af987138411
    Reviewed-on: https://gerrit.libreoffice.org/49372
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>

---
+++ vcl/Executable_sftfuzzer.mk
@@ -0,0 +1,47 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+include $(SRCDIR)/vcl/commonfuzzer.mk
+
+$(eval $(call gb_Executable_Executable,sftfuzzer))
+
+$(eval $(call gb_Executable_use_api,sftfuzzer,\
+    offapi \
+    udkapi \
+))
+
+$(eval $(call gb_Executable_use_externals,sftfuzzer,\
+	$(fuzzer_externals) \
+))
+
+$(eval $(call gb_Executable_set_include,sftfuzzer,\
+    $$(INCLUDE) \
+    -I$(SRCDIR)/vcl/inc \
+))
+
+$(eval $(call gb_Executable_use_libraries,sftfuzzer,\
+    $(fuzzer_core_libraries) \
+))
+
+$(eval $(call gb_Executable_use_static_libraries,sftfuzzer,\
+    findsofficepath \
+    ulingu \
+    fuzzerstubs \
+))
+
+$(eval $(call gb_Executable_add_exception_objects,sftfuzzer,\
+    vcl/workben/sftfuzzer \
+))
+
+$(eval $(call gb_Executable_add_libs,sftfuzzer,\
+    -lFuzzingEngine \
+))
+
+# vim: set noet sw=4 ts=4:
