487b7f9fc5bfd16672e9f54b412ff6b608a51d76
Jim Raykowski <raykowj@gmail.com>
2018-02-06 22:50:02 -0900

    tdf#108227 Set table style so it is highlighted in Sidebar styles list
    Change-Id: I930085839c9ab7297e492c1463b54d41c087361c

--- sw/source/uibase/shells/basesh.cxx
+++ sw/source/uibase/shells/basesh.cxx
@@ -2711,6 +2711,8 @@ void SwBaseShell::InsertTable( SfxRequest& _rRequest )
 
                 if( pTAFormat == nullptr )
                     rSh.SetTableStyle( SwStyleNameMapper::GetUIName( RES_POOLTABSTYLE_DEFAULT, OUString() ) );
+                else
+                    rSh.SetTableStyle( aAutoName );
 
                 rSh.EndAllAction();
                 rTempView.AutoCaption(TABLE_CAP);
