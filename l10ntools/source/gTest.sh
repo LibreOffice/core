f1828e5bc000708f46eba5f55653258750e52df3
Stephan Bergmann <sbergman@redhat.com>
2018-02-07 20:56:29 +0100

    Remove dead Executable_genlang
    ...originally introduce with 999c68f12f1d95b16a97294949a0e6ba6d3ba259 "genLang
    project (awareness)", but apparently never took off.
    
    Change-Id: I6f61271a75d96750dea63de596b7745c2f589b83

--- l10ntools/source/gTest.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-S=C:/Users/jani/opensource/libreoffice/lode/dev/work
-I=$S/instdir
-W=$S/workdir
-PATH="C:\Users\jani\opensource\libreoffice\lode\dev\work\instdir\program;C:\Users\jani\opensource\libreoffice\lode\dev\work\instdir\program;$PATH"
-./workdir/LinkTarget/Executable/xrmex.exe  -i readlicense_oo/docs/readme.xrm -o workdir/jan2/docs
