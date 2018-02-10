fd6d1941ffdb40b3497a6a5bfd030114fffa7cd6
Michael Meeks <michael.meeks@collabora.com>
2018-02-11 00:01:44 +0100

    lok: enable pre-loading with Vcl's builder
    Cache vclbuilder's loaded modules globally & simplify logic.
    
    Pre-populate that cache on preload for LOK vs. a somewhat
    inelegant pre-canned list of dialog libraries.
    
    Change-Id: I86d936862a41495fd37908f3ee7eb2e0c363d651

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -98,6 +98,8 @@
 #include <svl/undo.hxx>
 #include <unotools/datetime.hxx>
 #include <i18nlangtag/languagetag.hxx>
+#include <vcl/builder.hxx>
+#include <vcl/abstdlg.hxx>
 
 #include <app.hxx>
 
@@ -3811,7 +3813,9 @@ static int lo_initialize(LibreOfficeKit* pThis, const char* pAppPath, const char
                 // 2) comphelper::setProcessServiceFactory(xSFactory);
                 // 3) InitVCL()
                 aService->initialize({css::uno::makeAny<OUString>("preload")});
-
+                // Force load some modules
+                VclBuilder::preload();
+                VclAbstractDialogFactory::Create();
                 preloadData();
 
                 // Release Solar Mutex, lo_startmain thread should acquire it.
