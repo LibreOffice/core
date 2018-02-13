1ac016797a72ff946b385a31ba002a42d4f3fd23
Mike Kaganski <mike.kaganski@collabora.com>
2018-02-13 11:03:03 +0300

    tdf#94341: Set child transparent mode for ToolBox
    Push buttons don't rely on their parent's child transparent mode
    since i#38498. This allows buttons to have proper transparency
    mode, despite toolboxes don't set their children transparency mode
    at all (even being transparent themselves).
    
    However, when other controls are placed to toolboxes, as FixedText,
    they check parent's child transparent mode, and then use parent's
    background for them. For transparent toolboxes this makes the
    background to be NONE, and thus on invalidation of such FixedText,
    neither parent's area, nor FixedText's is filled to remove previous
    text before outputting new text.
    
    This patch sets ToolBox child transparent mode depending on its own
    transparency. Also, FixedText now checks if background applied in
    ApplySettings is empty, and if it is, sets transparency mode despite
    parent's setting.
    
    Change-Id: Id424cbad894bb42a88a4b027f2a964d6e542f115
    Reviewed-on: https://gerrit.libreoffice.org/49621
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
    (cherry picked from commit 179fbaf3687fc40d75a2570639191f797f03ce4e)

--- vcl/source/window/toolbox.cxx
+++ vcl/source/window/toolbox.cxx
@@ -1298,6 +1298,8 @@ void ToolBox::ImplInitSettings(bool bFont, bool bForeground, bool bBackground)
                 SetParentClipMode();
             }
         }
+
+        EnableChildTransparentMode(IsPaintTransparent());
     }
 }
 
