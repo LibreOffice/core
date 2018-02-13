495ad1e211a43b45f35620408707b404cc3a2058
Stephan Bergmann <sbergman@redhat.com>
2018-02-13 14:39:04 +0100

    Fix !LIBO_INTERNAL_ONLY code (Extension_test-active)
    ...by reverting the broken part of 2c05d758b2b62c9df413e2514fb3cd233d0f3ec7 "add
    << operator for css::uno::Exception"
    
    Change-Id: I1b625dad2050b7549338f148c016a515e42adeb6

--- desktop/test/deployment/active/active_native.cxx
+++ desktop/test/deployment/active/active_native.cxx
@@ -267,7 +267,7 @@ extern "C" SAL_DLLPUBLIC_EXPORT sal_Bool SAL_CALL component_writeInfo(
     } catch (const css::uno::Exception & e) {
         SAL_INFO(
             "desktop.test",
-            "active_native component_writeInfo: " << e);
+            "active_native component_writeInfo exception: " << e.Message);
         return false;
     }
     return true;
