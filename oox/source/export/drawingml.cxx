2717ff0d895424f58b553f169a45cd1998d2482f
Szymon Kłos <szymon.klos@collabora.com>
2018-02-13 17:47:23 +0100

    PPTX export scale for TextFitToSize
    MSO requires to save fontScale attirbute to have
    all text properly after presentation is opened.
    These values are overrided after first modification.
    Scalling to fit fream is calculated in svx and
    difficult to access here so compromise is used.
    There is no warranty that size will be correct,
    but will fit the frame.
    
    Change-Id: I73657fdd663b540b436747cfeeef3c76e8fe388c

--- oox/source/export/drawingml.cxx
+++ oox/source/export/drawingml.cxx
@@ -2535,11 +2535,28 @@ void DrawingML::WriteText( const Reference< XInterface >& rXIface, const OUStrin
         }
         if (GetDocumentType() == DOCUMENT_PPTX)
         {
+            const sal_Int32 MAX_SCALE_VAL = 100000;
+            sal_Int32 nFontScale = MAX_SCALE_VAL;
+            SvxShapeText* pTextShape = dynamic_cast<SvxShapeText*>(rXIface.get());
+            if (pTextShape)
+            {
+                SdrTextObj* pTextObject = dynamic_cast<SdrTextObj*>(pTextShape->GetSdrObject());
+                if (pTextObject)
+                {
+                    Size aTextSize = pTextObject->GetTextSize();
+                    awt::Size aFrameSize = pTextShape->getSize();
+                    double fRatio = (double)aTextSize.getHeight() / aFrameSize.Height;
+                    nFontScale = (sal_Int32)(fRatio * 100) * 1000;
+                }
+            }
+
             TextFitToSizeType eFit = TextFitToSizeType_NONE;
             if (GETA(TextFitToSize))
                 mAny >>= eFit;
+
             if (eFit == TextFitToSizeType_AUTOFIT)
-                mpFS->singleElementNS(XML_a, XML_normAutofit, FSEND);
+                mpFS->singleElementNS(XML_a, XML_normAutofit, XML_fontScale,
+                    nFontScale < MAX_SCALE_VAL ? I32S(nFontScale) : nullptr, FSEND);
         }
         mpFS->endElementNS((nXmlNamespace ? nXmlNamespace : XML_a), XML_bodyPr);
     }
