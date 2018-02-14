LibreOffice/core

commit 017f250764ec7b4ecb82ac19f5b3f68cadf1bf56

Ensure WakeUpThread is joined before exit
Change-Id: If50fe94875b29043c75b581bf39ca9deea59dbe3

framework/source/helper/statusindicatorfactory.cxx
@@ -55,7 +55,6 @@ const char PROGRESS_RESOURCE[] = "private:resource/progressbar/progressbar";
 
 StatusIndicatorFactory::StatusIndicatorFactory(const css::uno::Reference< css::uno::XComponentContext >& xContext)
     : m_xContext          (xContext )
-    , m_pWakeUp           (0        )
     , m_bAllowReschedule  (false)
     , m_bAllowParentShow  (false)
     , m_bDisableReschedule(false)
@@ -542,21 +541,18 @@ void StatusIndicatorFactory::impl_startWakeUpThread()
     if (m_bDisableReschedule)
         return;
 
-    if (!m_pWakeUp)
+    if (!m_pWakeUp.is())
     {
         m_pWakeUp = new WakeUpThread(this);
-        m_pWakeUp->create();
     }
 }
 
 void StatusIndicatorFactory::impl_stopWakeUpThread()
 {
     osl::MutexGuard g(m_mutex);
-    if (m_pWakeUp)
+    if (m_pWakeUp.is())
     {
-        // Thread kill itself after terminate()!
-        m_pWakeUp->terminate();
-        m_pWakeUp = 0;
+        m_pWakeUp->stop();
     }
 }
 

framework/source/helper/wakeupthread.cxx
@@ -19,44 +19,41 @@
 
 #include <sal/config.h>
 
-#include <osl/conditn.hxx>
+#include <com/sun/star/uno/Reference.hxx>
+#include <com/sun/star/util/XUpdatable.hpp>
+#include <osl/mutex.hxx>
+#include <osl/time.h>
 
-// include files of own module
 #include <helper/wakeupthread.hxx>
 
-namespace framework{
-
-WakeUpThread::WakeUpThread(const css::uno::Reference< css::util::XUpdatable >& xListener)
-    : m_xListener   (xListener)
-{
-}
-
-void SAL_CALL WakeUpThread::run()
-{
-    osl_setThreadName("framework::WakeUpThread");
-
-    ::osl::Condition aSleeper;
-
-    TimeValue aTime;
-    aTime.Seconds = 0;
-    aTime.Nanosec = 25000000; // 25 msec
-
-    while(schedule())
-    {
-        aSleeper.reset();
-        aSleeper.wait(&aTime);
-
-        css::uno::Reference< css::util::XUpdatable > xListener(m_xListener);
-        if (xListener.is())
-            xListener->update();
+void framework::WakeUpThread::execute() {
+    for (;;) {
+        TimeValue t{0, 25000000}; // 25 msec
+        condition_.wait(&t);
+        {
+            osl::MutexGuard g(mutex_);
+            if (terminate_) {
+                break;
+            }
+        }
+        css::uno::Reference<css::util::XUpdatable> up(updatable_);
+        if (up.is()) {
+            up->update();
+        }
     }
 }
 
-void SAL_CALL WakeUpThread::onTerminated()
-{
-    delete this;
-}
+framework::WakeUpThread::WakeUpThread(
+    css::uno::Reference<css::util::XUpdatable> const & updatable):
+    Thread("WakeUpThread"), updatable_(updatable), terminate_(false)
+{}
 
-} // namespace framework
+void framework::WakeUpThread::stop() {
+    {
+        osl::MutexGuard g(mutex_);
+        terminate_ = true;
+    }
+    join();
+}
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

framework/inc/helper/statusindicatorfactory.hxx
@@ -173,7 +173,7 @@ class StatusIndicatorFactory : public  ::cppu::WeakImplHelper4<
 
         /** Notify us if a fix time is over. We use it to implement an
             intelligent "Reschedule" ... */
-        WakeUpThread* m_pWakeUp;
+        rtl::Reference<WakeUpThread> m_pWakeUp;
 
         /** Our WakeUpThread calls us in our interface method "XUpdatable::update().
             There we set this member m_bAllowReschedule to sal_True. Next time if our impl_reschedule()

framework/inc/helper/wakeupthread.hxx
@@ -20,57 +20,38 @@
 #ifndef INCLUDED_FRAMEWORK_INC_HELPER_WAKEUPTHREAD_HXX
 #define INCLUDED_FRAMEWORK_INC_HELPER_WAKEUPTHREAD_HXX
 
-// include files of own module
+#include <sal/config.h>
 
-#include <macros/generic.hxx>
-
-#include <general.h>
-
-// include UNO interfaces
-
-#include <com/sun/star/util/XUpdatable.hpp>
-
-// include all others
+#include <com/sun/star/uno/Reference.hxx>
 #include <cppuhelper/weakref.hxx>
-#include <osl/thread.hxx>
+#include <osl/conditn.hxx>
+#include <osl/mutex.hxx>
+#include <sal/types.h>
+#include <salhelper/thread.hxx>
+
+namespace com { namespace sun { namespace star { namespace util {
+    class XUpdatable;
+} } } }
 
 namespace framework{
 
-/** @short  implements a "sleeping" thread, which try to sleep
-            without a using cpu consumption :-) */
-class WakeUpThread : public ::osl::Thread
-{
+class WakeUpThread: public salhelper::Thread {
+    css::uno::WeakReference<css::util::XUpdatable> updatable_;
+    osl::Condition condition_;
 
-    // member
-    private:
+    osl::Mutex mutex_;
+    bool terminate_;
 
-        /** @short  this listener will be notified if this thread
-                    waked up. */
-        css::uno::WeakReference< css::util::XUpdatable > m_xListener;
+    void execute() SAL_OVERRIDE;
 
-    // interface
-    public:
+public:
+    WakeUpThread(css::uno::Reference<css::util::XUpdatable> const & updatable);
 
-        /** @short  Register a new listener on this thread.
-
-            @descr  The listener is holded as a weak reference.
-                    If the thread detects, that no listener exists ...
-                    he will terminate itself.
-         */
-        WakeUpThread(const css::uno::Reference< css::util::XUpdatable >& xListener);
-
-        /** @descr  The thread waits on a condition using a fix timeout value.
-                    If the thread wakes up he notify the internal set listener.
-                    The listener can use this "timeout" info for it's own purpose.
-                    The thread itself will wait on the condition again.
-         */
-        virtual void SAL_CALL run() SAL_OVERRIDE;
-
-        virtual void SAL_CALL onTerminated() SAL_OVERRIDE;
+    void stop();
 };
 
-} // namespace framework
+}
 
-#endif // INCLUDED_FRAMEWORK_INC_HELPER_WAKEUPTHREAD_HXX
+#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

commit c8523e148706fa38d9d6af5936e9d1ca406808e4

tdf#108005: Problems with progress bar while saving document
...as StatusIndicatorFactory::m_pWakeUp is still non-null after it has already
been used while loading the document.  Regression introduced with
017f250764ec7b4ecb82ac19f5b3f68cadf1bf56 "Ensure WakeUpThread is joined before
exit".

(Also, WakeUpThread::stop could take up to 25 msec longer than necessary, as it
failed to set condition_, so would always have waited for WakeUpThread::execute
to finish its next 25 msec condition_.wait call.)

Change-Id: Ic7fe15f1b90f3b8292cd2ceccdc2719a00743d44

framework/source/helper/statusindicatorfactory.cxx
@@ -18,6 +18,7 @@
  */
 
 #include <algorithm>
+#include <utility>
 #include <helper/statusindicatorfactory.hxx>
 #include <helper/statusindicator.hxx>
 #include <helper/vclstatusindicator.hxx>
@@ -547,7 +548,7 @@ void StatusIndicatorFactory::impl_stopWakeUpThread()
     rtl::Reference<WakeUpThread> wakeUp;
     {
         osl::MutexGuard g(m_mutex);
-        wakeUp = m_pWakeUp;
+        std::swap(wakeUp, m_pWakeUp);
     }
     if (wakeUp.is())
     {

framework/source/helper/wakeupthread.cxx
@@ -53,6 +53,7 @@ void framework::WakeUpThread::stop() {
         osl::MutexGuard g(mutex_);
         terminate_ = true;
     }
+    condition_.set();
     join();
 }
 