415bf9d178c2318602547c5b55ee576eeedb9300
Kohei Yoshida <kohei.yoshida@gmail.com>
2018-02-14 21:04:28 -0500

    Update orcus to 0.13.3.
    This will be the new baseline for master, since, though this change
    is a fully API-compatible, one bug fix wrt the alpha value handling
    unfortunately would break some unit tests when using an earlier
    version.
    
    Change-Id: I5a17ce4085d3311a165748154d107d12b95e2c71

--- external/liborcus/UnpackedTarball_liborcus.mk
+++ external/liborcus/UnpackedTarball_liborcus.mk
@@ -18,10 +18,6 @@ $(eval $(call gb_UnpackedTarball_update_autoconf_configs,liborcus))
 $(eval $(call gb_UnpackedTarball_add_patches,liborcus,\
 	external/liborcus/0001-workaround-a-linking-problem-on-windows.patch \
 	external/liborcus/rpath.patch.0 \
-	external/liborcus/0001-Alpha-value-of-0-means-fully-transparent.-I-m-sure-2.patch \
-	external/liborcus/0002-We-are-supposed-to-use-the-foreground-color-for-soli.patch \
-	external/liborcus/0001-xls-xml-Pick-up-border-colors.patch \
-	external/liborcus/0001-xls-xml-Import-hidden-row-and-column-flags.patch \
 ))
 
 ifeq ($(OS),WNT)
