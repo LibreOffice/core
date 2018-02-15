LibreOffice/core

commit a8ea79b10bfc4851d092270f02fa8c817c9bc0d9

sdext: disable MSVC 2017 warnings from boost headers
Change-Id: I6eb816f155a222ca4299fd425c79d38bf2d464dc

sdext/source/pdfimport/pdfparse/pdfparse.cxx
@@ -20,12 +20,23 @@
 
 #include <pdfparse.hxx>
 
+// boost using obsolete stuff
+#if defined(_MSC_VER)
+#pragma warning(push)
+#pragma warning(disable:4996)
+#endif
+
 // workaround windows compiler: do not include multi_pass.hpp
 #include <boost/spirit/include/classic_core.hpp>
 #include <boost/spirit/include/classic_utility.hpp>
 #include <boost/spirit/include/classic_error_handling.hpp>
 #include <boost/spirit/include/classic_file_iterator.hpp>
 #include <boost/bind.hpp>
+
+#if defined(_MSC_VER)
+#pragma warning(pop)
+#endif
+
 #include <string.h>
 
 #include <rtl/strbuf.hxx>

commit 7e241cda123e8afac329445121c555586020ae93

sdext: suppress another MSVC warning
Somehow a8ea79b10bfc4851d092270f02fa8c817c9bc0d9 caused another warning
to appear in the MSVC 2015 that is used in jenkins builds.  Whyever.

Change-Id: Iba73de5739078972129d0a2de7d8a6437c12b2e0

sdext/source/pdfimport/pdfparse/pdfparse.cxx
@@ -24,6 +24,7 @@
 #if defined(_MSC_VER)
 #pragma warning(push)
 #pragma warning(disable:4996)
+#pragma warning(disable:4503)
 #endif
 
 // workaround windows compiler: do not include multi_pass.hpp