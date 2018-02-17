97854fe56e1324265ce74f0dcb74214110d2f52a
Shubham Verma <lifemeansmore008@gmail.com>
2018-02-17 16:52:15 +0530

    tdf#112689 : Replace chained O(U)StringBuffer::append() with operator+
    Change-Id: I119d33b171da7024daab5c4ca4488ceb495eab2f

--- basctl/source/basicide/basicbox.cxx
+++ basctl/source/basicide/basicbox.cxx
@@ -408,8 +408,7 @@ void LanguageBox::FillBox()
             OUString sLanguage = SvtLanguageTable::GetLanguageString( eLangType );
             if ( bIsDefault )
             {
-                sLanguage += " ";
-                sLanguage += m_sDefaultLanguageStr;
+                sLanguage += " " + m_sDefaultLanguageStr;
             }
             sal_Int32 nPos = InsertEntry( sLanguage );
             SetEntryData( nPos, new LanguageEntry( pLocale[i], bIsDefault ) );
