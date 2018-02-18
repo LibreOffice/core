60f1490af9b29ac2df8e9e9411e9d41b472f9113
Kacper Kasper <kacperkasper@gmail.com>
2018-02-18 15:15:56 +0100

    Buildsystem changes to recognize Haiku.
    Change-Id: I219d556f8e124cfe426cc1ac3c54da34eb7ef790
    Reviewed-on: https://gerrit.libreoffice.org/49925
    Reviewed-by: Michael Meeks <michael.meeks@collabora.com>
    Tested-by: Jenkins <ci@libreoffice.org>

---
+++ solenv/gbuild/platform/HAIKU_INTEL_GCC.mk
@@ -0,0 +1,16 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+#please make generic modifications to unxgcc.mk
+
+gb_CPUDEFS := -DX86
+
+include $(GBUILDDIR)/platform/unxgcc.mk
+
+# vim: set noet sw=4:
