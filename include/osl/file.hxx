LibreOffice/core

commit a9739f2dec197793fa9400dd95be49494dc85264

osl: document file.hxx, swap order of E_ROFS and E_SPIPE to match file.h
Change-Id: I085852e7c9fed285117f426e775f7ca2c123e4c0
Reviewed-on: https://gerrit.libreoffice.org/37594
Reviewed-by: Chris Sherlock <chris.sherlock79@gmail.com>
Tested-by: Chris Sherlock <chris.sherlock79@gmail.com>

include/osl/file.hxx
@@ -53,53 +53,53 @@ class FileBase
 public:
 
     enum RC {
-        E_None         = osl_File_E_None,
-        E_PERM         = osl_File_E_PERM,
-        E_NOENT        = osl_File_E_NOENT,
-        E_SRCH         = osl_File_E_SRCH,
-        E_INTR         = osl_File_E_INTR,
-        E_IO           = osl_File_E_IO,
-        E_NXIO         = osl_File_E_NXIO,
-        E_2BIG         = osl_File_E_2BIG,
-        E_NOEXEC       = osl_File_E_NOEXEC,
-        E_BADF         = osl_File_E_BADF,
-        E_CHILD        = osl_File_E_CHILD,
-        E_AGAIN        = osl_File_E_AGAIN,
-        E_NOMEM        = osl_File_E_NOMEM,
-        E_ACCES        = osl_File_E_ACCES,
-        E_FAULT        = osl_File_E_FAULT,
-        E_BUSY         = osl_File_E_BUSY,
-        E_EXIST        = osl_File_E_EXIST,
-        E_XDEV         = osl_File_E_XDEV,
-        E_NODEV        = osl_File_E_NODEV,
-        E_NOTDIR       = osl_File_E_NOTDIR,
-        E_ISDIR        = osl_File_E_ISDIR,
-        E_INVAL        = osl_File_E_INVAL,
-        E_NFILE        = osl_File_E_NFILE,
-        E_MFILE        = osl_File_E_MFILE,
-        E_NOTTY        = osl_File_E_NOTTY,
-        E_FBIG         = osl_File_E_FBIG,
-        E_NOSPC        = osl_File_E_NOSPC,
-        E_SPIPE        = osl_File_E_SPIPE,
-        E_ROFS         = osl_File_E_ROFS,
-        E_MLINK        = osl_File_E_MLINK,
-        E_PIPE         = osl_File_E_PIPE,
-        E_DOM          = osl_File_E_DOM,
-        E_RANGE        = osl_File_E_RANGE,
-        E_DEADLK       = osl_File_E_DEADLK,
-        E_NAMETOOLONG  = osl_File_E_NAMETOOLONG,
-        E_NOLCK        = osl_File_E_NOLCK,
-        E_NOSYS        = osl_File_E_NOSYS,
-        E_NOTEMPTY     = osl_File_E_NOTEMPTY,
-        E_LOOP         = osl_File_E_LOOP,
-        E_ILSEQ        = osl_File_E_ILSEQ,
-        E_NOLINK       = osl_File_E_NOLINK,
-        E_MULTIHOP     = osl_File_E_MULTIHOP,
-        E_USERS        = osl_File_E_USERS,
-        E_OVERFLOW     = osl_File_E_OVERFLOW,
-        E_NOTREADY     = osl_File_E_NOTREADY,
-        E_invalidError = osl_File_E_invalidError,   /* unmapped error: always last entry in enum! */
-        E_TIMEDOUT     = osl_File_E_TIMEDOUT,
+        E_None         = osl_File_E_None,           //< on success
+        E_PERM         = osl_File_E_PERM,           //< operation not permitted
+        E_NOENT        = osl_File_E_NOENT,          //< no such file or directory
+        E_SRCH         = osl_File_E_SRCH,           //< no process matches the PID
+        E_INTR         = osl_File_E_INTR,           //< function call was interrupted
+        E_IO           = osl_File_E_IO,             //< I/O error occured
+        E_NXIO         = osl_File_E_NXIO,           //< no such device or address
+        E_2BIG         = osl_File_E_2BIG,           //< argument list too long
+        E_NOEXEC       = osl_File_E_NOEXEC,         //< invalid executable file format
+        E_BADF         = osl_File_E_BADF,           //< bad file descriptor
+        E_CHILD        = osl_File_E_CHILD,          //< there are no child processes
+        E_AGAIN        = osl_File_E_AGAIN,          //< resource temp unavailable, try again later
+        E_NOMEM        = osl_File_E_NOMEM,          //< no memory available
+        E_ACCES        = osl_File_E_ACCES,          //< file permissions do not allow operation
+        E_FAULT        = osl_File_E_FAULT,          //< bad address; an invalid pointer detected
+        E_BUSY         = osl_File_E_BUSY,           //< resource busy
+        E_EXIST        = osl_File_E_EXIST,          //< file exists where should only be created
+        E_XDEV         = osl_File_E_XDEV,           //< improper link across file systems detected
+        E_NODEV        = osl_File_E_NODEV,          //< wrong device type specified
+        E_NOTDIR       = osl_File_E_NOTDIR,         //< file isn't a directory where one is needed
+        E_ISDIR        = osl_File_E_ISDIR,          //< file is a directory, invalid operation
+        E_INVAL        = osl_File_E_INVAL,          //< invalid argument to library function
+        E_NFILE        = osl_File_E_NFILE,          //< too many distinct file openings
+        E_MFILE        = osl_File_E_MFILE,          //< process has too many distinct files open
+        E_NOTTY        = osl_File_E_NOTTY,          //< inappropriate I/O control operation
+        E_FBIG         = osl_File_E_FBIG,           //< file too large
+        E_NOSPC        = osl_File_E_NOSPC,          //< no space left on device, write failed
+        E_SPIPE        = osl_File_E_ROFS,           //< invalid seek operation (such as on pipe)
+        E_ROFS         = osl_File_E_SPIPE,          //< illegal modification to read-only filesystem
+        E_MLINK        = osl_File_E_MLINK,          //< too many links to file
+        E_PIPE         = osl_File_E_PIPE,           //< broken pipe; no process reading from other end of pipe
+        E_DOM          = osl_File_E_DOM,            //< domain error (mathematical error)
+        E_RANGE        = osl_File_E_RANGE,          //< range error (mathematical error)
+        E_DEADLK       = osl_File_E_DEADLK,         //< deadlock avoided
+        E_NAMETOOLONG  = osl_File_E_NAMETOOLONG,    //< filename too long
+        E_NOLCK        = osl_File_E_NOLCK,          //< no locks available
+        E_NOSYS        = osl_File_E_NOSYS,          //< function not implemented
+        E_NOTEMPTY     = osl_File_E_NOTEMPTY,       //< directory not empty
+        E_LOOP         = osl_File_E_LOOP,           //< too many levels of symbolic links found during name lookup
+        E_ILSEQ        = osl_File_E_ILSEQ,          //< invalid or incomplete byte sequence of multibyte char found
+        E_NOLINK       = osl_File_E_NOLINK,         //< link has been severed
+        E_MULTIHOP     = osl_File_E_MULTIHOP,       //< remote resource is not directly available
+        E_USERS        = osl_File_E_USERS,          //< file quote system is confused as there are too many users
+        E_OVERFLOW     = osl_File_E_OVERFLOW,       //< value too large for defined data type
+        E_NOTREADY     = osl_File_E_NOTREADY,       //< device not ready
+        E_invalidError = osl_File_E_invalidError,   //< unmapped error: always last entry in enum!
+        E_TIMEDOUT     = osl_File_E_TIMEDOUT,       //< socket operation timed out
         E_NETWORK      = osl_File_E_NETWORK
     };
 

commit 0e9787047d031d47f3da8b13c703dab786afec78

tdf#115383: revert mixup of E_ROFS and E_SPIPE in file.hxx
Regression from a9739f2dec197793fa9400dd95be49494dc85264

Change-Id: I1b55b0201888a63bd2439be8c0421f450d609018

include/osl/file.hxx
@@ -80,8 +80,8 @@ public:
         E_NOTTY        = osl_File_E_NOTTY,          ///< inappropriate I/O control operation
         E_FBIG         = osl_File_E_FBIG,           ///< file too large
         E_NOSPC        = osl_File_E_NOSPC,          ///< no space left on device, write failed
-        E_SPIPE        = osl_File_E_ROFS,           ///< invalid seek operation (such as on pipe)
-        E_ROFS         = osl_File_E_SPIPE,          ///< illegal modification to read-only filesystem
+        E_SPIPE        = osl_File_E_SPIPE,          ///< invalid seek operation (such as on pipe)
+        E_ROFS         = osl_File_E_ROFS,           ///< illegal modification to read-only filesystem
         E_MLINK        = osl_File_E_MLINK,          ///< too many links to file
         E_PIPE         = osl_File_E_PIPE,           ///< broken pipe; no process reading from other end of pipe
         E_DOM          = osl_File_E_DOM,            ///< domain error (mathematical error)