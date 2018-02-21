970915b37bc23785b741aa12c5dc95c5130d8d81
Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
2018-02-21 09:27:25 +0100

    Use long path prefix in osl_getFileStatus
    When installing an extension e.g., paths can get very long and they
    hit the 255 char limit, thus the installation fails.
    So we need to prefix the path with the long file name prefix
    for windows api calls to succeed.
    
    Change-Id: Ie62644192ba40a9d4802772cd9837fc84fae947a

--- sal/osl/w32/file_dirvol.cxx
+++ sal/osl/w32/file_dirvol.cxx
@@ -1573,9 +1573,16 @@ oslFileError SAL_CALL osl_getFileStatus(
         break;
     }
 
+    // Provide a long path version of the file name, because when
+    // the path is longer than 255 chars, windows api calls expect this prefix.
+    OUString sFullPath(pItemImpl->m_pFullPath);
+
+    if (sFullPath.getLength() >= 260 && isalpha(sFullPath[0]) && sFullPath[1] == ':')
+        sFullPath = "\\\\?\\" + sFullPath;
+
     if ( uFieldMask & osl_FileStatus_Mask_Validate )
     {
-        HANDLE  hFind = FindFirstFileW( o3tl::toW(rtl_uString_getStr( pItemImpl->m_pFullPath )), &pItemImpl->FindData );
+        HANDLE  hFind = FindFirstFileW( o3tl::toW(sFullPath.getStr() ), &pItemImpl->FindData );
 
         if ( hFind != INVALID_HANDLE_VALUE )
             FindClose( hFind );
@@ -1647,7 +1654,7 @@ oslFileError SAL_CALL osl_getFileStatus(
         if ( !pItemImpl->bFullPathNormalized )
         {
             ::osl::LongPathBuffer< sal_Unicode > aBuffer( MAX_LONG_PATH );
-            sal_uInt32 nNewLen = GetCaseCorrectPathName( o3tl::toW(rtl_uString_getStr( pItemImpl->m_pFullPath )),
+            sal_uInt32 nNewLen = GetCaseCorrectPathName( o3tl::toW( sFullPath.getStr() ),
                                                          o3tl::toW( aBuffer ),
                                                          aBuffer.getBufSizeInSymbols(),
                                                          true );
@@ -1659,7 +1666,7 @@ oslFileError SAL_CALL osl_getFileStatus(
             }
         }
 
-        oslFileError error = osl_getFileURLFromSystemPath( pItemImpl->m_pFullPath, &pStatus->ustrFileURL );
+        oslFileError error = osl_getFileURLFromSystemPath( sFullPath.pData, &pStatus->ustrFileURL );
         if (error != osl_File_E_None)
             return error;
         pStatus->uValidFields |= osl_FileStatus_Mask_FileURL;
