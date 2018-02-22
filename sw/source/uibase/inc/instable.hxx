c24e515a6a2daabd88b8ad5eeb651b3c907b2693
Furkan Tokac <furkantokac34@gmail.com>
2018-02-22 23:17:59 +0200

    tdf#113558: Replace 'AutoFormat' button in insert table dialog.
    Writer > Table > Insert Table... > AutoFormat part is moved to
    "Insert Table..." dialog box with realtime style preview.
    
    Change-Id: I72ed46401fb053156a4787450fc974951923d049

--- sw/source/uibase/inc/instable.hxx
+++ sw/source/uibase/inc/instable.hxx
@@ -19,48 +19,64 @@
 #ifndef INCLUDED_SW_SOURCE_UIBASE_INC_INSTABLE_HXX
 #define INCLUDED_SW_SOURCE_UIBASE_INC_INSTABLE_HXX
 
-#include <rtl/ustring.hxx>
-#include <vcl/button.hxx>
-#include <vcl/field.hxx>
-#include <vcl/edit.hxx>
-#include <vcl/layout.hxx>
 #include <sfx2/basedlgs.hxx>
-#include "actctrl.hxx"
+#include <sal/types.h>
+#include <rtl/ustring.hxx>
+#include <tools/link.hxx>
+#include <vcl/vclreferencebase.hxx>
+#include <vcl/button.hxx>
+#include <vcl/edit.hxx>
+#include <vcl/field.hxx>
+#include <vcl/layout.hxx>
 
-class SwWrtShell;
-class SwTableAutoFormat;
-class SwView;
-struct SwInsertTableOptions;
+#include "wrtsh.hxx"
+#include "autoformatpreview.hxx"
+#include <view.hxx>
+#include <tblafmt.hxx>
+#include <itabenum.hxx>
 
 class SwInsTableDlg : public SfxModalDialog
 {
-    VclPtr<Edit>           m_pNameEdit;
     TextFilter      m_aTextFilter;
+    VclPtr<Edit>    m_pNameEdit;
 
-    VclPtr<NumericField>   m_pColNF;
-    VclPtr<NumericField>   m_pRowNF;
+    VclPtr<NumericField>    m_pColNF;
+    VclPtr<NumericField>    m_pRowNF;
 
-    VclPtr<CheckBox>       m_pHeaderCB;
-    VclPtr<CheckBox>       m_pRepeatHeaderCB;
-    VclPtr<NumericField>   m_pRepeatHeaderNF;
-    VclPtr<VclContainer>   m_pRepeatGroup;
+    VclPtr<CheckBox>        m_pHeaderCB;
+    VclPtr<CheckBox>        m_pRepeatHeaderCB;
+    VclPtr<NumericField>    m_pRepeatHeaderNF;
+    VclPtr<VclContainer>    m_pRepeatGroup;
 
-    VclPtr<CheckBox>       m_pDontSplitCB;
-    VclPtr<CheckBox>       m_pBorderCB;
+    VclPtr<CheckBox>        m_pDontSplitCB;
 
-    VclPtr<PushButton>     m_pInsertBtn;
-    VclPtr<PushButton>     m_pAutoFormatBtn;
+    VclPtr<PushButton>      m_pInsertBtn;
+
+    VclPtr<ListBox>         m_pLbFormat;
+
+    VclPtr<AutoFormatPreview> m_pWndPreview;
 
     SwWrtShell*     pShell;
-    SwTableAutoFormat* pTAutoFormat;
+    SwTableAutoFormatTable* pTableTable;
+    SwTableAutoFormat*      pTAutoFormat;
+
+    sal_uInt8       lbIndex;
+    sal_uInt8       tbIndex;
+    sal_uInt8       minTableIndexInLb;
+    sal_uInt8       maxTableIndexInLb;
     sal_Int64       nEnteredValRepeatHeaderNF;
 
+    // Returns 255 if mapping is not possible.
+    // This means there cannot be more than 255 autotable style.
+    sal_uInt8 lbIndexToTableIndex( const sal_uInt8 listboxIndex );
+    void InitAutoTableFormat();
+
+    DECL_LINK( SelFormatHdl, ListBox&, void );
     DECL_LINK( ModifyName, Edit&, void );
     DECL_LINK( ModifyRowCol, Edit&, void );
-    DECL_LINK( AutoFormatHdl, Button*, void );
-    DECL_LINK( OKHdl, Button*, void);
-    DECL_LINK( CheckBoxHdl, Button*, void);
-    DECL_LINK( ReapeatHeaderCheckBoxHdl, Button*, void);
+    DECL_LINK( OKHdl, Button*, void );
+    DECL_LINK( CheckBoxHdl, Button*, void );
+    DECL_LINK( ReapeatHeaderCheckBoxHdl, Button*, void );
     DECL_LINK( ModifyRepeatHeaderNF_Hdl, Edit&, void );
 
 public:
