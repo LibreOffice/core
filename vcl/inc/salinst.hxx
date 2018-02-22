eb9e6c87622ef5436abf75a87f4868d5d0e826a1
Tor Lillqvist <tml@collabora.com>
2018-02-22 12:40:26 +0200

    tdf#103690: Set up notifications only after VCL has been initialised
    On some Macs, it seems that LibreOffice (or any app?) gets an
    NSApplicationDidChangeScreenParametersNotification as soon as it has
    started and asked for such a notification. Our handler for that
    notification assumes that VCL is initialised. Thus we should not ask
    for such notifications before VCL has been initialised.
    
    I could not reproduce the reported crash with an unmodified
    LibreOffice, only after inserting a sleep after the notifications had
    been set up. But I am fairly sure this change fixes the problem.
    
    Change-Id: I18d342eb7dc0c77cb7fc8623756bead65a1bd329
    Reviewed-on: https://gerrit.libreoffice.org/50164
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Tor Lillqvist <tml@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/50197
    Reviewed-by: Michael Meeks <michael.meeks@collabora.com>
    Tested-by: Michael Meeks <michael.meeks@collabora.com>
    (cherry picked from commit 42a5a054b70c7850ca47074c2d5dd249de3202b4)
    Reviewed-on: https://gerrit.libreoffice.org/50209
    Tested-by: Tor Lillqvist <tml@collabora.com>

--- vcl/inc/salinst.hxx
+++ vcl/inc/salinst.hxx
@@ -196,6 +196,10 @@ void DeInitSalData();                       // called from Application-Dtor
 
 void InitSalMain();
 
+#ifdef MACOSX
+void postInitVCLinitNSApp();
+#endif
+
 #endif // INCLUDED_VCL_INC_SALINST_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
