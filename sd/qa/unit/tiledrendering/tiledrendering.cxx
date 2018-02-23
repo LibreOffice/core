ea68707c2fed39eb00fdc8c24b0360005b1f1c0e
Miklos Vajna <vmiklos@collabora.co.uk>
2018-02-23 12:30:02 +0100

    tdf#115873 sd navigator: adapt IsEqualToDoc() to Fill() further
    SdPageObjsTLB::Fill() populates the navigator tree list box,
    SdPageObjsTLB::IsEqualToDoc() determintes if it has to be refreshed or
    not.
    
    Commit f3c68cdf8f6a0273c62b493552f78af0138a44e8 (tdf#115873 sd
    navigator: allow selecting but not focusing on objects, 2018-02-21)
    already brought IsEqualToDoc() closer to Fill() with using the same
    iteration mode for the SdrObjects, but that's not enough.
    
    Fill() uses flat iteration, then checks for group shapes explicitly and
    visits them recursively. Change IsEqualToDoc() to do the same, this way
    selecting "Rectangle 3" in the testcase won't result in a jump back to
    "Slide 1" (as an effect of IsEqualToDoc() returning false for an up to
    date tree list box).
    
    Change-Id: If2543cbc282af06ba43d4804e7ed455c8b9828cd
    Reviewed-on: https://gerrit.libreoffice.org/50234
    Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
    Tested-by: Jenkins <ci@libreoffice.org>
    (cherry picked from commit 1f159a4df7dcf9c4b1a35d16aee2303b8fa34b27)
    Reviewed-on: https://gerrit.libreoffice.org/50346
    Reviewed-by: Andras Timar <andras.timar@collabora.com>

--- sd/qa/unit/tiledrendering/tiledrendering.cxx
+++ sd/qa/unit/tiledrendering/tiledrendering.cxx
@@ -103,6 +103,7 @@ public:
     void testMultiViewInsertDeletePage();
     void testTdf115783();
     void testTdf115873();
+    void testTdf115873Group();
 
     CPPUNIT_TEST_SUITE(SdTiledRenderingTest);
     CPPUNIT_TEST(testRegisterCallback);
@@ -141,6 +142,7 @@ public:
     CPPUNIT_TEST(testMultiViewInsertDeletePage);
     CPPUNIT_TEST(testTdf115783);
     CPPUNIT_TEST(testTdf115873);
+    CPPUNIT_TEST(testTdf115873Group);
 
     CPPUNIT_TEST_SUITE_END();
 
@@ -1883,6 +1885,21 @@ void SdTiledRenderingTest::testTdf115873()
     // This failed, single-click did not result in a shape selection (only
     // double-click did).
     CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(1), rMarkList.GetMarkCount());
+}
+
+void SdTiledRenderingTest::testTdf115873Group()
+{
+    // Initialize the navigator.
+    SdXImpressDocument* pXImpressDocument = createDoc("tdf115873-group.fodp");
+    SfxViewShell* pViewShell = SfxViewShell::Current();
+    CPPUNIT_ASSERT(pViewShell);
+    SfxBindings& rBindings = pViewShell->GetViewFrame()->GetBindings();
+    ScopedVclPtrInstance<SdNavigatorWin> pNavigator(nullptr, &rBindings);
+    pNavigator->InitTreeLB(pXImpressDocument->GetDoc());
+    VclPtr<SdPageObjsTLB> pObjects = pNavigator->GetObjects();
+    // This failed, Fill() and IsEqualToDoc() were out of sync for group
+    // shapes.
+    CPPUNIT_ASSERT(pObjects->IsEqualToDoc(pXImpressDocument->GetDoc()));
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(SdTiledRenderingTest);
