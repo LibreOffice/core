196499cdf9b541496c97eb493f58c92f3b996758
Zdeněk Crhonek <zcrhonek@gmail.com>
2018-02-23 20:57:51 +0100

    uitest for bug tdf#51352
    Change-Id: I5ad054c7226c43c60dd0770ff7f7bd7bba34c93d

---
+++ sw/qa/uitest/writer_tests/tdf51352.py
@@ -0,0 +1,53 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+from uitest.framework import UITestCase
+from libreoffice.uno.propertyvalue import mkPropertyValues
+from uitest.debug import sleep
+
+class tdf51352(UITestCase):
+
+    def test_convert_nested_table_to_text_tdf51352(self):
+
+        self.ui_test.create_doc_in_start_center("writer")
+        xWriterDoc = self.xUITest.getTopFocusWindow()
+        xWriterEdit = xWriterDoc.getChild("writer_edit")
+        document = self.ui_test.get_component()
+        #1. New a text document
+        #2. Insert a table and then in one cell create another table
+        #-insert a table (by default 2x2)
+        self.ui_test.execute_dialog_through_command(".uno:InsertTable")
+        xDialog = self.xUITest.getTopFocusWindow()
+        xOkBtn = xDialog.getChild("ok")
+        xOkBtn.executeAction("CLICK", tuple())
+        #-put the cursor inside first cell top left for example
+        #-insert a inner table (by default 2x2) inside this cell
+        self.ui_test.execute_dialog_through_command(".uno:InsertTable")
+        xDialog = self.xUITest.getTopFocusWindow()
+        xOkBtn = xDialog.getChild("ok")
+        xOkBtn.executeAction("CLICK", tuple())
+        #3. Select the first table
+        xWriterEdit.executeAction("TYPE", mkPropertyValues({"KEYCODE": "RIGHT"}))
+        xWriterEdit.executeAction("TYPE", mkPropertyValues({"KEYCODE": "RIGHT"}))
+        xWriterEdit.executeAction("TYPE", mkPropertyValues({"KEYCODE": "RIGHT"}))
+        xWriterEdit.executeAction("TYPE", mkPropertyValues({"KEYCODE": "RIGHT"}))
+        xWriterEdit.executeAction("TYPE", mkPropertyValues({"KEYCODE": "RIGHT"}))
+        self.assertEqual(document.TextTables.getCount(), 2)
+        #4. From menu "Table->Covert->Table to Text
+        self.ui_test.execute_dialog_through_command(".uno:ConvertTableToText")
+        xDialog = self.xUITest.getTopFocusWindow()
+        xOkBtn = xDialog.getChild("ok")
+        xOkBtn.executeAction("CLICK", tuple())
+        self.assertEqual(document.TextTables.getCount(), 0)
+        #5. Undo/Redo
+        self.xUITest.executeCommand(".uno:Undo")
+        self.assertEqual(document.TextTables.getCount(), 2)
+        self.xUITest.executeCommand(".uno:Redo")
+        self.assertEqual(document.TextTables.getCount(), 0)
+        self.ui_test.close_doc()
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
+
