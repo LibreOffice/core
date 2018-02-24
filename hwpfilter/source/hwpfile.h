b434bda77878e87f1eb8efeaab3dd0e5f5c4f420
Caolán McNamara <caolanm@redhat.com>
2018-02-24 20:43:12 +0000

    forcepoint #8 ensure ColumnDef lifetime
    Change-Id: Idb0c7b1530dc57f4d7c14751f1b76caecc3b03a6
    Reviewed-on: https://gerrit.libreoffice.org/50290
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>

--- hwpfilter/source/hwpfile.h
+++ hwpfilter/source/hwpfile.h
@@ -70,11 +70,10 @@ class   HStream;
 struct ColumnInfo{
     int start_page;
     bool bIsSet;
-    ColumnDef *coldef;
+    std::shared_ptr<ColumnDef> xColdef;
     explicit ColumnInfo(int num){
         start_page = num;
         bIsSet = false;
-        coldef = nullptr;
     }
 };
 
@@ -211,7 +210,7 @@ class DLLEXPORT HWPFile
         void AddBox(FBox *);
         void AddPage(){ m_nCurrentPage++;}
         void AddColumnInfo();
-        void SetColumnDef(ColumnDef *coldef);
+        void SetColumnDef(std::shared_ptr<ColumnDef>&);
         void AddParaShape(std::shared_ptr<ParaShape>&);
         void AddCharShape(std::shared_ptr<CharShape>&);
         void AddFBoxStyle(FBoxStyle *);
