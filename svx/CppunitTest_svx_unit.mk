ff0f1298633f625c2016fe03ed9c71cd7f8d32d1
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2018-02-26 12:29:58 +0900

    Fix exporting bitmap table + test
    Change-Id: I0577de02000c6aeb45bf1e950b9212beadacb05b

--- svx/CppunitTest_svx_unit.mk
+++ svx/CppunitTest_svx_unit.mk
@@ -13,19 +13,30 @@ $(eval $(call gb_CppunitTest_use_external,svx_unit,boost_headers))
 
 $(eval $(call gb_CppunitTest_use_sdk_api,svx_unit))
 
+$(eval $(call gb_CppunitTest_set_include,svx_unit,\
+    -I$(SRCDIR)/svx/source/inc \
+    -I$(SRCDIR)/svx/inc \
+    $$(INCLUDE) \
+))
+
 $(eval $(call gb_CppunitTest_add_exception_objects,svx_unit, \
 	svx/qa/unit/svdraw/test_SdrTextObject \
 	svx/qa/unit/xoutdev \
+	svx/qa/unit/XTableImportExportTest \
 ))
 
 $(eval $(call gb_CppunitTest_use_libraries,svx_unit, \
 	sal \
 	sfx \
 	svxcore \
+	svx \
 	tl \
 	unotest \
 	vcl \
 	utl \
+	comphelper \
+	cppuhelper \
+	cppu \
 ))
 
 $(eval $(call gb_CppunitTest_use_sdk_api,svx_unit))
