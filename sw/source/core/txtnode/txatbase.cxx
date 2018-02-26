feb87052fc83be32bbcde5166768997ccc5059ec
Rico Tzschichholz <ricotz@ubuntu.com>
2018-02-26 09:12:48 +0100

    sw: Fix build with older boost::optional like 1.54
    Change-Id: I2256a52e2017bfda04b3a2395f82b0aff0aa5b22

--- sw/source/core/txtnode/txatbase.cxx
+++ sw/source/core/txtnode/txatbase.cxx
@@ -100,21 +100,21 @@ void SwTextAttr::dumpAsXml(xmlTextWriterPtr pWriter) const
         {
             pWhich = "character format";
             if (SwCharFormat* pCharFormat = GetCharFormat().GetCharFormat())
-                oValue = "name: " + OUStringToOString(pCharFormat->GetName(), RTL_TEXTENCODING_UTF8);
+                oValue = OString("name: " + OUStringToOString(pCharFormat->GetName(), RTL_TEXTENCODING_UTF8));
             break;
         }
     case RES_TXTATR_INETFMT:
         {
             pWhich = "inet format";
             const SwFormatINetFormat& rFormat = GetINetFormat();
-            oValue = "url: " + rFormat.GetValue().toUtf8();
+            oValue = OString("url: " + rFormat.GetValue().toUtf8());
             break;
         }
     case RES_TXTATR_CJK_RUBY:
         {
             pWhich = "ruby";
             const SwFormatRuby& rFormat = GetRuby();
-            oValue = "rubytext: " + rFormat.GetText().toUtf8();
+            oValue = OString("rubytext: " + rFormat.GetText().toUtf8());
             break;
         }
     case RES_TXTATR_META:
