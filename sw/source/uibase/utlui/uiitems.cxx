ac2ed426777d54948b5cf9a4f808ded1fe6e0faf
Noel Grandin <noel.grandin@collabora.co.uk>
2018-02-27 13:06:31 +0200

    tdf#115432 footnote separator position remain in the left side
    regression from
        commit 7e9857c2935bb2533806db4e71c6cd1e171c3478
        "templatize SvXMLEnumMapEntry"
    
    Change-Id: Id29e7c07be637adcdc7fe624a1673beca9829d4d

--- sw/source/uibase/utlui/uiitems.cxx
+++ sw/source/uibase/utlui/uiitems.cxx
@@ -18,6 +18,7 @@
  */
 
 #include <editeng/itemtype.hxx>
+#include <o3tl/any.hxx>
 #include <unosett.hxx>
 
 #include <swtypes.hxx>
@@ -86,7 +87,7 @@ bool SwPageFootnoteInfoItem::QueryValue( Any& rVal, sal_uInt8 nMemberId ) const
             rVal <<= static_cast<sal_Int8>(static_cast<long>(aTmp));
         }
         break;
-        case MID_LINE_ADJUST       :     rVal <<= static_cast<sal_Int16>(aFootnoteInfo.GetAdj());break;//text::HorizontalAdjust
+        case MID_LINE_ADJUST       :     rVal <<= aFootnoteInfo.GetAdj();break;//text::HorizontalAdjust
         case MID_LINE_TEXT_DIST    :     rVal <<= static_cast<sal_Int32>(convertTwipToMm100(aFootnoteInfo.GetTopDist()));break;
         case MID_LINE_FOOTNOTE_DIST:     rVal <<= static_cast<sal_Int32>(convertTwipToMm100(aFootnoteInfo.GetBottomDist()));break;
         case MID_FTN_LINE_STYLE    :
@@ -156,12 +157,8 @@ bool SwPageFootnoteInfoItem::PutValue(const Any& rVal, sal_uInt8 nMemberId)
         break;
         case MID_LINE_ADJUST       :
         {
-            sal_Int16 nSet = 0;
-            rVal >>= nSet;
-            if(nSet >= 0 && nSet < 3) //text::HorizontalAdjust
-                aFootnoteInfo.SetAdj(static_cast<css::text::HorizontalAdjust>(nSet));
-            else
-                bRet = false;
+            text::HorizontalAdjust nSet = *o3tl::forceAccess<text::HorizontalAdjust>(rVal);
+            aFootnoteInfo.SetAdj(nSet);
         }
         break;
         case MID_FTN_LINE_STYLE:
