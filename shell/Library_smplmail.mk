f05e9233904b48cf71a95e01a6f0d0c862f5a532
Mike Kaganski <mike.kaganski@collabora.com>
2018-02-27 23:40:15 +0300

    tdf#116074: Don't block on sending email interactively
    When sending e-mail using a MAPI mail client that doesn't recognize
    MAPI_DIALOG_MODELESS flag, and doesn't return from MAPISendMail until
    message compose dialog is closed (like MS Outlook 2010 and older),
    waiting for the senddoc process blocks UI, which is unexpected and
    prevents users from copying stuff from documents to the mail body.
    
    Waiting for senddoc process completion is used for two things:
    1. To serialize sending multiple mails (e.g., using mailmerge);
    2. To show error in case when it failed.
    
    This patch allows to avoid blocking the UI in case when compose UI is
    requested - i.e., user interaction with the mail client is expected,
    and serialization is not required. In this case, the senddoc process
    will show the error message itself -> no need for main application to
    wait for its return. The error message now includes actual error code.
    
    To avoid cases when closing main program would remove temporary
    attachment files before they were used by mail client, they are
    copied to base temporary directory (instead of default session
    temporary directory that gets deleted upon program shutdown).
    senddoc cleans up its temporaries itself.
    
    The temporary attachment files are copied to files with ASCII-only
    filenames, and their original filenames are passed to mail clients
    using MAPI. This allows to avoid cases when the filenames contain
    characters outside of current Windows codepage, and the mail client
    does not support Unicode MAPI, thus receiving wrong filename and
    erroring out from the send.
    
    Reviewed-on: https://gerrit.libreoffice.org/50826
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
    (cherry picked from commit 2f061dad7f875f704e3744fc5780c1d145b22e9f)
    
    Change-Id: I4a517bd7a797e76e4c0b7ea48bb1a7b652741a81

--- shell/Library_smplmail.mk
+++ shell/Library_smplmail.mk
@@ -18,7 +18,10 @@ $(eval $(call gb_Library_use_system_win32_libs,smplmail,\
 $(eval $(call gb_Library_use_libraries,smplmail,\
 	cppu \
 	cppuhelper \
+	i18nlangtag \
 	sal \
+	tl \
+	utl \
 ))
 
 $(eval $(call gb_Library_set_componentfile,smplmail,shell/source/win32/simplemail/smplmail))
