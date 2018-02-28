639a056847c65c7833295022e3951912cd747981
Miklos Vajna <vmiklos@collabora.co.uk>
2018-02-28 12:00:38 +0100

    sw HTML export: ReqIf implies we write a fragment
    Test that for a very simple input we roundtrip perfectly (not counting
    whitespace changes).
    
    Change-Id: I89cfbe1da594f8a5343444ca20a107021d6a1a37

--- sw/source/filter/html/css1atr.cxx
+++ sw/source/filter/html/css1atr.cxx
@@ -3061,6 +3061,10 @@ static Writer& OutCSS1_SvxFormatBreak_SwFormatPDesc_SvxFormatKeep( Writer& rWrt,
         }
     }
 
+    if (rHTMLWrt.mbSkipHeaderFooter)
+        // No page break when writing only a fragment.
+        return rWrt;
+
     if( pBreakBefore )
         rHTMLWrt.OutCSS1_PropertyAscii( sCSS1_P_page_break_before,
                                         pBreakBefore );
