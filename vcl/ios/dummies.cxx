f5c00cd780dab434c96bd2760821e1565475fc0f
jan Iversen <jani@libreoffice.org>
2018-03-05 09:32:35 +0100

    vcl, make ios/dummies compile
    Point .... were not defined
    
    Change-Id: I725b3058d44d527ca2d3201060e4f467fd69c78d

--- vcl/ios/dummies.cxx
+++ vcl/ios/dummies.cxx
@@ -16,12 +16,13 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
-
+#include <vcl/svapp.hxx>
 #include "salprn.hxx"
 #include "headless/svpgdi.hxx"
 #include "headless/svpinst.hxx"
 #include "unx/fontmanager.hxx"
 #include "unx/gendata.hxx"
+
 
 SalPrinter* SvpSalInstance::CreatePrinter( SalInfoPrinter* /* pInfoPrinter */ )
 {
