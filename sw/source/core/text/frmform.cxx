59b1bb96acd646de3f22fb6b23bd59a1b42e2976
Tamás Zolnai <tamas.zolnai@collabora.com>
2018-03-05 04:47:34 +0100

    bccu#3100: Writer: Change tracking boxes aren't repositioned with editing
    Emit a notification related to the node position change, so the track changes
    comment box is repositioned when the text node changes it's position.
    
    Change-Id: I34b5ec453b3dc43895865f15d71aaead46ae6f24

--- sw/source/core/text/frmform.cxx
+++ sw/source/core/text/frmform.cxx
@@ -52,6 +52,8 @@
 #include "portab.hxx"
 #include <editeng/lrspitem.hxx>
 #include <editeng/tstpitem.hxx>
+#include <redline.hxx>
+#include <comphelper/lok.hxx>
 
 // Tolerance in formatting and text output
 #define SLOPPY_TWIPS    5
@@ -341,6 +343,25 @@ bool SwTextFrame::CalcFollow( const sal_Int32 nTextOfst )
     }
 
     return false;
+}
+
+void SwTextFrame::MakePos()
+{
+    SwFrame::MakePos();
+    // Inform LOK clients about change in position of redlines (if any)
+    if(comphelper::LibreOfficeKit::isActive())
+    {
+        const SwTextNode& rTextNode = *GetTextNode();
+        const SwRedlineTable& rTable = rTextNode.getIDocumentRedlineAccess().GetRedlineTable();
+        for (SwRedlineTable::size_type nRedlnPos = 0; nRedlnPos < rTable.size(); ++nRedlnPos)
+        {
+            SwRangeRedline* pRedln = rTable[nRedlnPos];
+            if (rTextNode.GetIndex() == pRedln->GetPoint()->nNode.GetNode().GetIndex())
+            {
+                pRedln->MaybeNotifyRedlinePositionModification(getFrameArea().Top());
+            }
+        }
+    }
 }
 
 void SwTextFrame::AdjustFrame( const SwTwips nChgHght, bool bHasToFit )
