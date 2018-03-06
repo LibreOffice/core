12278d018d7a0ad92f219c1040d3c135fd166638
Caolán McNamara <caolanm@redhat.com>
2018-03-06 09:53:37 +0000

    ofz: timeout, reduce warning limit further
    Change-Id: I9c8fd5ec55c446e4deb6feee800dd4a09fbea75f
    Reviewed-on: https://gerrit.libreoffice.org/50811
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>

--- vcl/source/filter/jpeg/jpegc.cxx
+++ vcl/source/filter/jpeg/jpegc.cxx
@@ -72,7 +72,7 @@ extern "C" void outputMessage (j_common_ptr cinfo)
 
 static int GetWarningLimit()
 {
-    return utl::ConfigManager::IsFuzzing() ? 10 : 1000;
+    return utl::ConfigManager::IsFuzzing() ? 5 : 1000;
 }
 
 extern "C" void emitMessage (j_common_ptr cinfo, int msg_level)
