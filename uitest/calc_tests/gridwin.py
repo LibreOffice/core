b8f2691a32b73f7f748b97a5dd6098313070a8fc
Markus Mohrhard <markus.mohrhard@googlemail.com>
2018-03-07 01:36:12 +0100

    uitest: add demo showing how to select different sheet
    Change-Id: I1ddc8e9855832df7176d768e73d062e7f601a62c

--- uitest/calc_tests/gridwin.py
+++ uitest/calc_tests/gridwin.py
@@ -6,6 +6,7 @@
 #
 
 from libreoffice.uno.propertyvalue import mkPropertyValues
+from uitest.uihelper.common import get_state_as_dict
 
 from uitest.framework import UITestCase
 
@@ -27,5 +28,21 @@ class GridWinTest(UITestCase):
         xGridWindow.executeAction("DESELECT", tuple())
 
         self.ui_test.close_doc()
+
+    def test_select_sheet(self):
+
+        self.ui_test.create_doc_in_start_center("calc")
+        xCalcDoc = self.xUITest.getTopFocusWindow()
+        xGridWindow = xCalcDoc.getChild("grid_window")
+
+        for i in range(3):
+            self.ui_test.execute_dialog_through_command(".uno:Insert")
+            current_dialog = self.xUITest.getTopFocusWindow()
+
+            xOkButton = current_dialog.getChild("ok")
+            self.ui_test.close_dialog_through_button(xOkButton)
+
+        xGridWindow.executeAction("SELECT", mkPropertyValues({"TABLE": "2"}))
+        self.assertEqual(get_state_as_dict(xGridWindow)["SelectedTable"], "2")
 
 # vim: set shiftwidth=4 softtabstop=4 expandtab:
