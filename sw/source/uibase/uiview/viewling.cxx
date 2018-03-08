5a31537cb2f32ee01098bccbbaaa0f4e183d4850
Jan Holesovsky <kendy@collabora.com>
2018-03-08 17:26:27 +0100

    Revert "lok: Don't freeze the LibreOfficeKit via the spell-checking popup..."
    We want to re-introduce the spell-checking context menu, and use dialog
    tunneling for that.
    
    This reverts commit eb5e45782b0e805455d383e9d020aae5bb70d619.
    
    Change-Id: I068585b9e70409fe3e64f2d7039fb4a23af08738

--- sw/source/uibase/uiview/viewling.cxx
+++ sw/source/uibase/uiview/viewling.cxx
@@ -24,7 +24,6 @@
 #include <com/sun/star/linguistic2/ProofreadingResult.hpp>
 #include <com/sun/star/i18n/TextConversionOption.hpp>
 #include <linguistic/lngprops.hxx>
-#include <comphelper/lok.hxx>
 #include <comphelper/processfactory.hxx>
 #include <comphelper/propertyvalue.hxx>
 #include <comphelper/scopeguard.hxx>
@@ -731,13 +730,9 @@ bool SwView::ExecSpellPopup(const Point& rPt)
 
                 OUString sMenuName  = bUseGrammarContext ?
                     OUString("private:resource/GrammarContextMenu") : OUString("private:resource/SpellContextMenu");
-                if (comphelper::LibreOfficeKit::isActive())
+                if(TryContextMenuInterception( *pPopup, sMenuName, pMenu, aEvent ))
                 {
-                    // TODO for LOK, we'll need to convert the spelling popup menu to
-                    // something much more sfx2-based & non-modal...
-                }
-                else if (TryContextMenuInterception( *pPopup, sMenuName, pMenu, aEvent ))
-                {
+
                     //! happy hacking for context menu modifying extensions of this
                     //! 'custom made' menu... *sigh* (code copied from sfx2 and framework)
                     if ( pMenu )
