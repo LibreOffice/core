5ef0f1dad06ceb137ff2436e37f6939ad8de191e
Aron Budea <aron.budea@collabora.com>
2018-03-09 03:54:27 +0100

    tdf#116301: write correct content type for diagramDrawing
    According to MS-ODRAWXML the correct one is:
    application/vnd.ms-office.drawingml.diagramDrawing+xml
    
    Change-Id: If988ad234742da42634a52bbe176dbb5b763af46

--- sw/source/filter/ww8/docxsdrexport.cxx
+++ sw/source/filter/ww8/docxsdrexport.cxx
@@ -1331,7 +1331,7 @@ void DocxSdrExport::writeDiagram(const SdrObject* sdrObject, const SwFrameFormat
         uno::Reference<io::XOutputStream> xDrawingOutputStream
             = m_pImpl->m_rExport.GetFilter().openFragmentStream(
                 "word/" + drawingFileName,
-                "application/vnd.openxmlformats-officedocument.drawingml.diagramDrawing+xml");
+                "application/vnd.ms-office.drawingml.diagramDrawing+xml");
         writer->setOutputStream(xDrawingOutputStream);
         serializer->serialize(
             uno::Reference<xml::sax::XDocumentHandler>(writer, uno::UNO_QUERY_THROW),
