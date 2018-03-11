42dcdfe80872b6b30f3bb0ae4b69113acd051229
Zdeněk Crhonek <zcrhonek@gmail.com>
2018-03-11 09:11:08 +0100

    uitest - Calc Paste Only
    Change-Id: I4d858b34609058fb86fe70244236f6cca32a4c31

---
+++ sc/qa/uitest/calc_tests/CalcPasteOnly.py
@@ -0,0 +1,46 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+from libreoffice.uno.propertyvalue import mkPropertyValues
+
+from uitest.framework import UITestCase
+from libreoffice.calc.document import get_column
+from uitest.path import get_srcdir_url
+from libreoffice.calc.document import get_cell_by_position
+from uitest.uihelper.calc import enter_text_to_cell
+import time
+from uitest.debug import sleep
+
+class CalcPasteOnly(UITestCase):
+
+    def test_paste_only(self):
+        calc_doc = self.ui_test.create_doc_in_start_center("calc")
+        xTopWindow = self.xUITest.getTopFocusWindow()
+        document = self.ui_test.get_component()
+        gridwin = xTopWindow.getChild("grid_window")
+
+        enter_text_to_cell(gridwin, "A1", "=SUM(A2:A3)")
+        gridwin.executeAction("SELECT", mkPropertyValues({"CELL": "A1"}))
+        self.xUITest.executeCommand(".uno:Copy")
+
+        gridwin.executeAction("SELECT", mkPropertyValues({"CELL": "C1"}))
+        self.xUITest.executeCommand(".uno:PasteOnlyText")
+        self.assertEqual(get_cell_by_position(document, 0, 2, 0).getString(), "")
+
+        gridwin.executeAction("SELECT", mkPropertyValues({"CELL": "D1"}))
+        self.xUITest.executeCommand(".uno:PasteOnlyValue")
+        self.assertEqual(get_cell_by_position(document, 0, 3, 0).getString(), "0")
+        self.assertEqual(get_cell_by_position(document, 0, 3, 0).getValue(), 0)
+
+        gridwin.executeAction("SELECT", mkPropertyValues({"CELL": "E1"}))
+        self.xUITest.executeCommand(".uno:PasteOnlyFormula")
+        self.assertEqual(get_cell_by_position(document, 0, 4, 0).getString(), "0")
+        self.assertEqual(get_cell_by_position(document, 0, 4, 0).getValue(), 0)
+        self.assertEqual(get_cell_by_position(document, 0, 4, 0).getFormula(), "=SUM(E2:E3)")
+
+        self.ui_test.close_doc()
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
