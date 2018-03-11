cab04bc39b5164ea74216cd849c3af5f5b298f79
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2018-03-11 23:45:38 +0900

    extract common components to one place for sc unoapi tests
    The declared components in a test of uno api don't change much
    or at all, so instead of duplicating this in every test, define
    it in a common file use when needed. Some more common things
    could be extracted in a similar way.
    
    Change-Id: I07ae641afca55ffaf3c5c93624bfaa2143590886
    Reviewed-on: https://gerrit.libreoffice.org/51079
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

--- sc/CppunitTest_sc_cellrangesobj.mk
+++ sc/CppunitTest_sc_cellrangesobj.mk
@@ -64,41 +64,8 @@ $(eval $(call gb_CppunitTest_use_ure,sc_cellrangesobj))
 $(eval $(call gb_CppunitTest_use_vcl,sc_cellrangesobj))
 
 $(eval $(call gb_CppunitTest_use_components,sc_cellrangesobj,\
-	basic/util/sb \
-	comphelper/util/comphelp \
-	configmgr/source/configmgr \
-	dbaccess/util/dba \
-	filter/source/config/cache/filterconfig1 \
-	filter/source/storagefilterdetect/storagefd \
-	forms/util/frm \
-	framework/util/fwk \
-	i18npool/source/search/i18nsearch \
-	i18npool/util/i18npool \
-	oox/util/oox \
-	package/source/xstor/xstor \
-	package/util/package2 \
-	sax/source/expatwrap/expwrap \
-	scripting/source/basprov/basprov \
-	scripting/util/scriptframe \
-	sc/util/sc \
-	sc/util/scd \
-	sc/util/scfilt \
-	$(call gb_Helper_optional,SCRIPTING, \
-		sc/util/vbaobj) \
-	sfx2/util/sfx \
-	sot/util/sot \
-	svl/source/fsstor/fsstorage \
-	toolkit/util/tk \
-	ucb/source/core/ucb1 \
-	ucb/source/ucp/file/ucpfile1 \
-	ucb/source/ucp/tdoc/ucptdoc1 \
-	unotools/util/utl \
-	unoxml/source/rdf/unordf \
-	unoxml/source/service/unoxml \
-	uui/util/uui \
-	vcl/vcl.common \
-	xmloff/util/xo \
-	svtools/util/svt \
+    $(sc_unoapi_common_components) \
+    i18npool/source/search/i18nsearch \
 ))
 
 $(eval $(call gb_CppunitTest_use_configuration,sc_cellrangesobj))
