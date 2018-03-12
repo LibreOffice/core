be1aba56c3c5657fba83bff3106fd90c4bf3e3b1
Caolán McNamara <caolanm@redhat.com>
2018-03-12 10:24:55 +0000

    forcepoint #24 back() on empty vector
    Thanks to Antti Levomäki and Christian Jalio from Forcepoint.
    
    Change-Id: Idb6723b53a1ae8aaca80847bfe643bc4abaedd21

--- writerfilter/source/dmapper/DomainMapperTableManager.hxx
+++ writerfilter/source/dmapper/DomainMapperTableManager.hxx
@@ -95,6 +95,7 @@ public:
     virtual void endOfRowAction() override;
 
     IntVectorPtr getCurrentGrid( );
+    bool hasCurrentSpans() const;
     IntVectorPtr getCurrentSpans( );
     IntVectorPtr getCurrentCellWidths( );
 
