1912551d9f145e32574a147831934d46735ce7ff
Miklos Vajna <vmiklos@collabora.co.uk>
2018-03-12 16:30:55 +0100

    sw XHTML export: always write images as PNG in ReqIF mode
    JPG image would have to be JPG + PNG fallback, at which point it's
    easier to just write PNG.
    
    Change-Id: I6e4f0759244d29735fdcda8e050d198f118c737d

--- sw/source/filter/html/wrthtml.hxx
+++ sw/source/filter/html/wrthtml.hxx
@@ -655,7 +655,8 @@ Writer& OutHTML_Image( Writer&, const SwFrameFormat& rFormat,
                        Graphic const & rGraphic, const OUString& rAlternateText,
                        const Size& rRealSize, HtmlFrmOpts nFrameOpts,
                        const sal_Char *pMarkType,
-                       const ImageMap *pGenImgMap = nullptr );
+                       const ImageMap *pGenImgMap = nullptr,
+                       const OUString& rMimeType = OUString() );
 
 Writer& OutHTML_BulletImage( Writer& rWrt, const sal_Char *pTag,
                              const SvxBrushItem* pBrush,
