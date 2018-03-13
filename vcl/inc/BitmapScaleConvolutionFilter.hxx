520417d149862591d58fbc1a1e67812e6b3543cf
Chris Sherlock <chris.sherlock79@gmail.com>
2018-03-13 15:38:44 +1100

    vcl: migrated BitmapProcessor and scale classes to BitmapFilterEx classes
    Change-Id: I0530824cdefb3c714d2664988f887d123e471840

---
+++ vcl/inc/BitmapScaleConvolutionFilter.hxx
@@ -0,0 +1,82 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#ifndef VCL_INC_BITMAPSCALECONVOLUTIONFILTER_HXX
+#define VCL_INC_BITMAPSCALECONVOLUTIONFILTER_HXX
+
+#include <vcl/BitmapFilter.hxx>
+
+#include "ResampleKernel.hxx"
+
+namespace vcl
+{
+class VCL_DLLPUBLIC BitmapScaleConvolutionFilter : public BitmapFilter
+{
+public:
+    BitmapScaleConvolutionFilter(const double& rScaleX, const double& rScaleY)
+        : mrScaleX(rScaleX)
+        , mrScaleY(rScaleY)
+    {
+    }
+
+    virtual BitmapEx execute(BitmapEx const& rBitmap) override;
+
+protected:
+    Kernel* mpKernel;
+
+private:
+    double mrScaleX;
+    double mrScaleY;
+};
+
+class VCL_DLLPUBLIC BitmapScaleBilinearFilter : public BitmapScaleConvolutionFilter
+{
+public:
+    BitmapScaleBilinearFilter(const double& rScaleX, const double& rScaleY)
+        : BitmapScaleConvolutionFilter(rScaleX, rScaleY)
+    {
+        mpKernel = new BilinearKernel();
+    }
+};
+
+class VCL_DLLPUBLIC BitmapScaleBicubicFilter : public BitmapScaleConvolutionFilter
+{
+public:
+    BitmapScaleBicubicFilter(const double& rScaleX, const double& rScaleY)
+      : BitmapScaleConvolutionFilter(rScaleX, rScaleY)
+    {
+        mpKernel = new BicubicKernel();
+    }
+};
+
+class VCL_DLLPUBLIC BitmapScaleLanczos3Filter : public BitmapScaleConvolutionFilter
+{
+public:
+    BitmapScaleLanczos3Filter(const double& rScaleX, const double& rScaleY)
+        : BitmapScaleConvolutionFilter(rScaleX, rScaleY)
+    {
+        mpKernel = new Lanczos3Kernel();
+    }
+};
+
+}
+
+#endif // VCL_INC_BITMAPSCALECONVOLUTIONFILTER_HXX
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
