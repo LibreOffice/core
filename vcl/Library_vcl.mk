686a2c0aae8b58d51f89acdb87ef3331a8cc92a9
Chris Sherlock <chris.sherlock79@gmail.com>
2018-03-14 02:38:07 +1100

    vcl: move BitmapEx::Dither() to BitmapDitheringFilter class
    Change-Id: Ieb9eeb1d6873fd4bde0de8438dddd18fe2bb1c4a

--- vcl/Library_vcl.mk
+++ vcl/Library_vcl.mk
@@ -312,6 +312,13 @@ $(eval $(call gb_Library_add_exception_objects,vcl,\
     vcl/source/graphic/UnoGraphicTransformer \
     vcl/source/bitmap/bitmap \
     vcl/source/bitmap/bitmapfilter \
+    vcl/source/bitmap/BitmapSmoothenFilter \
+    vcl/source/bitmap/BitmapGaussianSeparableBlurFilter \
+    vcl/source/bitmap/BitmapDitheringFilter \
+    vcl/source/bitmap/BitmapLightenFilter \
+    vcl/source/bitmap/BitmapDisabledImageFilter \
+    vcl/source/bitmap/BitmapColorizeFilter \
+    vcl/source/bitmap/bitmappaint \
     vcl/source/bitmap/BitmapSobelGreyFilter \
     vcl/source/bitmap/BitmapSolarizeFilter \
     vcl/source/bitmap/BitmapSepiaFilter \
@@ -322,13 +329,6 @@ $(eval $(call gb_Library_add_exception_objects,vcl,\
     vcl/source/bitmap/BitmapConvolutionMatrixFilter \
     vcl/source/bitmap/BitmapMedianFilter \
     vcl/source/bitmap/BitmapInterpolateScaleFilter \
-    vcl/source/bitmap/BitmapLightenFilter \
-    vcl/source/bitmap/BitmapDisabledImageFilter \
-    vcl/source/bitmap/BitmapColorizeFilter \
-    vcl/source/bitmap/bitmappaint \
-    vcl/source/bitmap/BitmapGaussianSeparableBlurFilter \
-    vcl/source/bitmap/BitmapSeparableUnsharpenFilter \
-    vcl/source/bitmap/BitmapSmoothenFilter \
 	vcl/source/bitmap/BitmapFastScaleFilter \
     vcl/source/bitmap/BitmapScaleSuperFilter \
     vcl/source/bitmap/BitmapScaleConvolutionFilter \
@@ -336,6 +336,7 @@ $(eval $(call gb_Library_add_exception_objects,vcl,\
     vcl/source/bitmap/BitmapColorQuantizationFilter \
     vcl/source/bitmap/BitmapSimpleColorQuantizationFilter \
     vcl/source/bitmap/BitmapMedianColorQuantizationFilter \
+    vcl/source/bitmap/BitmapSeparableUnsharpenFilter \
     vcl/source/bitmap/BitmapTools \
     vcl/source/bitmap/checksum \
     vcl/source/image/Image \
