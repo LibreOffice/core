293d8ad21edcd6825e1951759244bcdeae93784e
Chris Sherlock <chris.sherlock79@gmail.com>
2018-03-14 02:38:07 +1100

    vcl: move BitmapEx::Dither() to BitmapDitheringFilter class
    Change-Id: Ieb9eeb1d6873fd4bde0de8438dddd18fe2bb1c4a

--- vcl/source/gdi/bitmapex.cxx
+++ vcl/source/gdi/bitmapex.cxx
@@ -583,11 +583,6 @@ bool BitmapEx::Erase( const Color& rFillColor )
     return bRet;
 }
 
-bool BitmapEx::Dither()
-{
-    return !!maBitmap && maBitmap.Dither();
-}
-
 void BitmapEx::Replace( const Color& rSearchColor, const Color& rReplaceColor )
 {
     if (!!maBitmap)
