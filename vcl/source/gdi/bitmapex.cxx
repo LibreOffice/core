6631cc554557c56000e0b87d595493b36e6fb7da
Chris Sherlock <chris.sherlock79@gmail.com>
2018-03-13 15:38:44 +1100

    vcl: migrated BitmapProcessor and scale classes to BitmapFilterEx classes
    Change-Id: I0530824cdefb3c714d2664988f887d123e471840
    Reviewed-on: https://gerrit.libreoffice.org/52898
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

--- vcl/source/gdi/bitmapex.cxx
+++ vcl/source/gdi/bitmapex.cxx
@@ -19,11 +19,12 @@
 
 #include <rtl/crc.h>
 #include <rtl/strbuf.hxx>
-
 #include <o3tl/any.hxx>
 #include <tools/debug.hxx>
 #include <unotools/resmgr.hxx>
 #include <tools/stream.hxx>
+#include <basegfx/matrix/b2dhommatrixtools.hxx>
+
 #include <vcl/ImageTree.hxx>
 #include <vcl/salbtype.hxx>
 #include <vcl/outdev.hxx>
@@ -35,16 +36,16 @@
 #include <vcl/virdev.hxx>
 #include <vcl/settings.hxx>
 
-#include <image.h>
-#include <basegfx/matrix/b2dhommatrixtools.hxx>
-
 // BitmapEx::Create
 #include <salbmp.hxx>
 #include <salinst.hxx>
 #include <svdata.hxx>
-#include <com/sun/star/beans/XFastPropertySet.hpp>
-#include <memory>
 #include <bitmapwriteaccess.hxx>
+#include <image.h>
+
+#include <com/sun/star/beans/XFastPropertySet.hpp>
+
+#include <memory>
 
 using namespace ::com::sun::star;
 
@@ -118,6 +119,7 @@ BitmapEx::BitmapEx( const Bitmap& rBmp, const Bitmap& rMask ) :
         meTransparent    ( !rMask ? TransparentType::NONE : TransparentType::Bitmap ),
         mbAlpha          ( false )
 {
+    SAL_INFO("vcl.gdi", "Bitmap size: " << maBitmapSize);
     // Ensure a mask is exactly one bit deep
     if( !!maMask && maMask.GetBitCount() != 1 )
     {
