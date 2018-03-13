097e5bc6d1fe8859e973f0026ea9a3d8114ae31c
Chris Sherlock <chris.sherlock79@gmail.com>
2018-03-14 01:50:13 +1100

    vcl: move BitmapEx::ReduceColors() to BitmapColorQuantizationFilter class
    Change-Id: Ieb21df9eb1a4b938753bd9f762f2aa5379e17523

--- include/vcl/bitmapex.hxx
+++ include/vcl/bitmapex.hxx
@@ -94,16 +94,6 @@ public:
     BitmapChecksum      GetChecksum() const;
 
 public:
-
-    /** Reduce number of colors for the bitmap using the POPULAR algorithm
-
-        @param nNewColorCount
-        Maximal number of bitmap colors after the reduce operation
-
-        @return true, if the color reduction operation was completed successfully.
-     */
-    bool                ReduceColors( sal_uInt16 nNewColorCount );
-
     /** Apply a dither algorithm to the bitmap
 
         This method dithers the bitmap inplace, i.e. a true color
