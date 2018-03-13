c25e1328c0873b56f2fe5f4edc9e257021a11b7e
Chris Sherlock <chris.sherlock79@gmail.com>
2018-03-13 15:38:44 +1100

    vcl: migrated scaling functions BitmapFilter classes
    Change-Id: I0aa193cd4ee588f724df2c9fe70de7157fabc4e8

--- svx/source/unodraw/UnoGraphicExporter.cxx
+++ svx/source/unodraw/UnoGraphicExporter.cxx
@@ -43,6 +43,7 @@
 #include <vcl/svapp.hxx>
 #include <vcl/virdev.hxx>
 #include <vcl/FilterConfigItem.hxx>
+#include <vcl/BitmapScaleFilter.hxx>
 #include <svl/outstrm.hxx>
 #include <svx/sdr/contact/objectcontactofobjlistpainter.hxx>
 #include <svx/sdr/contact/viewobjectcontact.hxx>
@@ -827,7 +828,11 @@ bool GraphicExporter::GetGraphic( ExportSettings const & rSettings, Graphic& aGr
                         {
                             BitmapEx aBmpEx( aGraphic.GetBitmapEx() );
                             // export: use highest quality
-                            aBmpEx.Scale( Size( rSettings.mnWidth, rSettings.mnHeight ), BmpScaleFlag::Lanczos );
+
+                            BitmapFilter::Filter(aBmpEx,
+                                    BitmapScaleFilter(Size(rSettings.mnWidth, rSettings.mnHeight),
+                                        BmpScaleFlag::Lanczos));
+
                             aGraphic = aBmpEx;
                         }
 
