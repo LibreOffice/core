19cc1cb2b3d3088d9dae3ec69b7eeb7d42571207
Chris Sherlock <chris.sherlock79@gmail.com>
2018-03-14 02:38:07 +1100

    vcl: move BitmapEx::Dither() to BitmapDitheringFilter class
    Change-Id: Ieb9eeb1d6873fd4bde0de8438dddd18fe2bb1c4a

--- vcl/Library_vcl.mk
+++ vcl/Library_vcl.mk
@@ -311,6 +311,14 @@ $(eval $(call gb_Library_add_exception_objects,vcl,\
     vcl/source/graphic/UnoGraphicTransformer \
     vcl/source/bitmap/bitmap \
     vcl/source/bitmap/bitmapfilter \
+    vcl/source/bitmap/BitmapSmoothenFilter \
+    vcl/source/bitmap/BitmapGaussianSeparableBlurFilter \
+    vcl/source/bitmap/BitmapDitheringFilter \
+    vcl/source/bitmap/BitmapLightenFilter \
+    vcl/source/bitmap/BitmapDisabledImageFilter \
+    vcl/source/bitmap/BitmapColorizeFilter \
+    vcl/source/bitmap/BitmapColorAdjustFilter \
+    vcl/source/bitmap/bitmappaint \
     vcl/source/bitmap/BitmapSobelGreyFilter \
     vcl/source/bitmap/BitmapSolarizeFilter \
     vcl/source/bitmap/BitmapSepiaFilter \
@@ -321,14 +329,6 @@ $(eval $(call gb_Library_add_exception_objects,vcl,\
     vcl/source/bitmap/BitmapConvolutionMatrixFilter \
     vcl/source/bitmap/BitmapMedianFilter \
     vcl/source/bitmap/BitmapInterpolateScaleFilter \
-    vcl/source/bitmap/BitmapLightenFilter \
-    vcl/source/bitmap/BitmapDisabledImageFilter \
-    vcl/source/bitmap/BitmapColorizeFilter \
-    vcl/source/bitmap/BitmapColorAdjustFilter \
-    vcl/source/bitmap/bitmappaint \
-    vcl/source/bitmap/BitmapGaussianSeparableBlurFilter \
-    vcl/source/bitmap/BitmapSeparableUnsharpenFilter \
-    vcl/source/bitmap/BitmapSmoothenFilter \
 	vcl/source/bitmap/BitmapFastScaleFilter \
     vcl/source/bitmap/BitmapScaleSuperFilter \
     vcl/source/bitmap/BitmapScaleConvolutionFilter \
