2f1d4927fcb56cecd9c2cc6585af82bd07508414
Chris Sherlock <chris.sherlock79@gmail.com>
2018-03-13 18:20:19 +1100

    vcl: move BitmapEx::Convert() to BitmapFilter
    Change-Id: I3edcaea6c2406922febb567ad564aeabc4e56efc

---
+++ include/vcl/BitmapConverter.hxx
@@ -0,0 +1,32 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#ifndef INCLUDED_INCLUDE_VCL_BITMAPCONVERTER_HXX
+#define INCLUDED_INCLUDE_VCL_BITMAPCONVERTER_HXX
+
+#include <vcl/bitmap.hxx>
+#include <vcl/bitmapex.hxx>
+#include <vcl/BitmapFilter.hxx>
+
+class VCL_DLLPUBLIC BitmapConverter : public BitmapFilter
+{
+public:
+    BitmapConverter(BmpConversion eConversion)
+        : meConversion(eConversion) {}
+
+    virtual BitmapEx execute(BitmapEx const & rBitmapEx);
+
+private:
+    BmpConversion meConversion;
+};
+
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
