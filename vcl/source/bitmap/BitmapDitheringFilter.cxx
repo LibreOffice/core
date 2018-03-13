d00e9621e51349634ed8e1bdd56dddb5996fb44d
Chris Sherlock <chris.sherlock79@gmail.com>
2018-03-14 02:38:07 +1100

    vcl: move BitmapEx::Dither() to BitmapDitheringFilter class
    Change-Id: I56f3c7cd84577ca8ec954017c5d51a9d8dd34c22

---
+++ vcl/source/bitmap/BitmapDitheringFilter.cxx
@@ -0,0 +1,23 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#include "BitmapDitheringFilter.hxx"
+
+BitmapEx BitmapDitheringFilter::execute(BitmapEx const& rBitmapEx)
+{
+    Bitmap aBitmap = rBitmapEx.GetBitmap();
+
+    if (aBitmap.Dither())
+        return rBitmapEx;
+
+    return BitmapEx();
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
