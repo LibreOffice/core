6a9326803c01f4c9bc7da855053ce4e80646fad8
Xisco Fauli <xiscofauli@libreoffice.org>
2018-03-14 10:19:47 +0100

    tdf#115539, tdf#116238: Don't display the notebookbar in presentation mode
    Change-Id: Ifc64d9a3b5c93b83fa238737375e7a58b2ae86db
    Reviewed-on: https://gerrit.libreoffice.org/51262
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>

--- sd/source/ui/view/drviewse.cxx
+++ sd/source/ui/view/drviewse.cxx
@@ -737,12 +737,8 @@ void DrawViewShell::FuSupport(SfxRequest& rReq)
         case SID_PRESENTATION_CURRENT_SLIDE:
         case SID_REHEARSE_TIMINGS:
         {
-            sfx2::SfxNotebookBar::LockNotebookBar();
-
             slideshowhelp::ShowSlideShow(rReq, *GetDoc());
             rReq.Ignore ();
-
-            sfx2::SfxNotebookBar::UnlockNotebookBar();
         }
         break;
 
@@ -1609,6 +1605,7 @@ namespace slideshowhelp
         Reference< XPresentation2 > xPresentation( rDoc.getPresentation() );
         if( xPresentation.is() )
         {
+            sfx2::SfxNotebookBar::LockNotebookBar();
             if (SID_REHEARSE_TIMINGS == rReq.GetSlot())
                 xPresentation->rehearseTimings();
             else if (rDoc.getPresentationSettings().mbCustomShow)
@@ -1638,6 +1635,7 @@ namespace slideshowhelp
 
                 xPresentation->startWithArguments( aArguments );
             }
+            sfx2::SfxNotebookBar::UnlockNotebookBar();
         }
     }
 }
