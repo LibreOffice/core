950accef75440fd7c79bb54705dbb43b0b5a21e6
Jens Carl <j.carl43@gmx.de>
2018-03-14 08:31:03 +0000

    Create UNO API property testers for common types
    Create UNO API property testers for common, so it's possible to share/use
    them between all the UNO API service property tests.
    Testers are available for the types 'boolean', 'long', 'short', and
    'string'.
    
    Change-Id: Ia36dba98c774695b3ba6ac9d9917af9fb20efdba

--- test/Library_test.mk
+++ test/Library_test.mk
@@ -47,6 +47,7 @@ $(eval $(call gb_Library_add_exception_objects,test,\
     test/source/mtfxmldump \
     test/source/primitive2dxmldump \
     test/source/screenshot_test \
+    test/source/unoapi_property_testers \
 ))
 
 # vim: set noet sw=4 ts=4:
