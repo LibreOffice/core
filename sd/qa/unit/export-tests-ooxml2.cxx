fc1d36ed9fbf4e93e8e8ba56cce5c4914f99b3f6
Szymon Kłos <szymon.klos@collabora.com>
2018-03-14 18:59:49 +0100

    tdf#90627 Don't save empty conditions
    Change-Id: I5a953c1b0fff7bf41a59360a8f52596264ddf266

--- sd/qa/unit/export-tests-ooxml2.cxx
+++ sd/qa/unit/export-tests-ooxml2.cxx
@@ -135,6 +135,7 @@ public:
     void testTdf111789();
     /// SmartArt animated elements
     void testTdf104792();
+    void testTdf90627();
 
     CPPUNIT_TEST_SUITE(SdOOXMLExportTest2);
 
@@ -194,6 +195,7 @@ public:
     CPPUNIT_TEST(testBulletsAsImage);
     CPPUNIT_TEST(testTdf111789);
     CPPUNIT_TEST(testTdf104792);
+    CPPUNIT_TEST(testTdf90627);
 
     CPPUNIT_TEST_SUITE_END();
 
@@ -1550,6 +1552,19 @@ void SdOOXMLExportTest2::testTdf104792()
 
     xmlDocPtr pXmlDocContent = parseExport(tempFile, "ppt/slides/slide1.xml");
     assertXPath(pXmlDocContent, "/p:sld/p:timing/p:tnLst/p:par/p:cTn/p:childTnLst[1]/p:seq/p:cTn/p:childTnLst[1]/p:par[1]/p:cTn/p:childTnLst[1]/p:par/p:cTn/p:childTnLst[1]/p:par/p:cTn/p:childTnLst[1]/p:set/p:cBhvr/p:tgtEl/p:spTgt", 1);
+
+    xDocShRef->DoClose();
+}
+
+void SdOOXMLExportTest2::testTdf90627()
+{
+    ::sd::DrawDocShellRef xDocShRef = loadURL(m_directories.getURLFromSrc("sd/qa/unit/data/odp/tdf90627.odp"), ODP);
+    utl::TempFile tempFile;
+    xDocShRef = saveAndReload(xDocShRef.get(), PPTX, &tempFile);
+
+    xmlDocPtr pXmlDocContent = parseExport(tempFile, "ppt/slides/slide1.xml");
+    // Don't export empty conditions
+    assertXPath(pXmlDocContent, "/p:sld/p:timing/p:tnLst/p:par/p:cTn/p:childTnLst[1]/p:seq/p:cTn/p:childTnLst[1]/p:par[2]/p:cTn/p:childTnLst[1]/p:par/p:cTn/p:childTnLst[1]/p:par/p:cTn/p:endCondLst", 0);
 
     xDocShRef->DoClose();
 }
