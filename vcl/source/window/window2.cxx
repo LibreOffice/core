b2428496de5cd579f4cdb0f149e2cebb8b12dd4d
Pranav Kant <pranavk@collabora.co.uk>
2018-03-14 14:55:05 +0530

    lokdialog: Invalidate upon window size change
    This problem can be seen with some of the dialogs like chart dialogs
    where not all of the area which should be invalidated is invalidated.
    The reason being that their container gets resized but not
    lok-invalidated.
    
    Change-Id: I12eb68e8644ca67b2666badb7b772b92a145c624
    Reviewed-on: https://gerrit.libreoffice.org/51264
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: pranavk <pranavk@collabora.co.uk>

--- vcl/source/window/window2.cxx
+++ vcl/source/window/window2.cxx
@@ -1392,6 +1392,12 @@ void Window::queue_resize(StateChangedType eReason)
         if (pBorderWindow)
             pBorderWindow->Resize();
     }
+
+    if (VclPtr<vcl::Window> pParent = GetParentWithLOKNotifier())
+    {
+        if (GetParentDialog())
+            LogicInvalidate(nullptr);
+    }
 }
 
 namespace
