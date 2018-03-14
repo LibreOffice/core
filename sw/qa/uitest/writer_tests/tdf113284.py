f456d2b79d06785356f64e87dbf98fdc4cb064fc
Zdeněk Crhonek <zcrhonek@gmail.com>
2018-03-14 17:12:50 +0100

    uitest for bug tdf#113284
    Change-Id: If4e9875344da2525b5f7bdb658ad0096d923ead7

---
+++ sw/qa/uitest/writer_tests/tdf113284.py
@@ -0,0 +1,40 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from uitest.framework import UITestCase
+from libreoffice.uno.propertyvalue import mkPropertyValues
+from uitest.uihelper.common import get_state_as_dict
+import time
+from uitest.path import get_srcdir_url
+from uitest.debug import sleep
+
+def get_url_for_data_file(file_name):
+    return get_srcdir_url() + "/sw/qa/uitest/writer_tests/data/" + file_name
+
+class tdf113284(UITestCase):
+
+   def test_tdf113284(self):
+    writer_doc = self.ui_test.load_file(get_url_for_data_file("tdf113284.odt"))
+    xWriterDoc = self.xUITest.getTopFocusWindow()
+    xWriterEdit = xWriterDoc.getChild("writer_edit")
+
+    sleep(2)  #we need this sleep,because without this is pagecount counted without index. Load index takes time probably.
+    self.ui_test.execute_dialog_through_command(".uno:GotoPage")
+    xDialog = self.xUITest.getTopFocusWindow()
+    xPageText = xDialog.getChild("page")
+    xPageText.executeAction("TYPE", mkPropertyValues({"TEXT":"70"})) # goto last page
+    xOkBtn = xDialog.getChild("ok")
+    self.ui_test.close_dialog_through_button(xOkBtn)
+
+    self.assertEqual(get_state_as_dict(xWriterEdit)["CurrentPage"], "70")
+    self.ui_test.execute_dialog_through_command(".uno:EditCurIndex")  #open index dialog
+    xDiagIndex = self.xUITest.getTopFocusWindow()
+    xOkBtn = xDiagIndex.getChild("ok")
+    self.ui_test.close_dialog_through_button(xOkBtn)   # close dialog with OK
+
+    self.assertEqual(get_state_as_dict(xWriterEdit)["CurrentPage"], "66")    #page 66 start of the Index
+
+    self.ui_test.close_doc()
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
