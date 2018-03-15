ecfc35ace3be6c8f95b84ab9efc8eaead8fd4dc5
Noel Grandin <noel.grandin@collabora.co.uk>
2018-03-15 14:18:06 +0200

    move swffilter use of BitmapEx::GetAlpha inside vcl
    Change-Id: I22f489a26b8566ddc00c0c77f95127adb8eb2489

--- include/vcl/bitmapex.hxx
+++ include/vcl/bitmapex.hxx
@@ -95,6 +95,11 @@ public:
 
 public:
 
+    /**
+     * @brief extract the bitmap and alpha data separately. Used by the SWF filter.
+     */
+    void GetSplitData( std::vector<sal_uInt8>& rvColorData, std::vector<sal_uInt8>& rvAlphaData ) const;
+
     /** Convert bitmap format
 
         @param eConversion
