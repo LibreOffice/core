8984fee9a9f90c09c3d123628cfc69d6f40f28ff
Armin Le Grand <Armin.Le.Grand@cib.de (CIB)>
2018-03-17 23:11:46 +0100

    SOSAW080: Adapted some comments to make more readable
    Change-Id: I446685a49a6d67aedd01cfbbd5e87b07f97a4d7b

--- sd/source/ui/view/drviews2.cxx
+++ sd/source/ui/view/drviews2.cxx
@@ -521,7 +521,7 @@ public:
                 continue;
 
             SdrRectObj* pObject = new SdrRectObj(
-                *m_rDrawViewShell.GetDoc(), // TTTT shbe ref
+                *m_rDrawViewShell.GetDoc(), // TTTT should be reference
                 OBJ_TEXT);
             pObject->SetMergedItem(makeSdrTextAutoGrowWidthItem(true));
             pObject->SetOutlinerParaObject(pOutliner->CreateParaObject());
