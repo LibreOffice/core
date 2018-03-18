6489bf1eff4ce74b35f7b2f8e8d64dbc56c7eee9
Zdeněk Crhonek <zcrhonek@gmail.com>
2018-03-18 10:07:09 +0100

    uitest for bug tdf#78068 and tdf#114992
    Change-Id: Ia98164b8cd07b5d4feb113052fc6bf4ae24b5192
    Reviewed-on: https://gerrit.libreoffice.org/51490
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Markus Mohrhard <markus.mohrhard@googlemail.com>

---
+++ sc/qa/uitest/calc_tests/tdf114992.py
@@ -0,0 +1,31 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+from uitest.framework import UITestCase
+from uitest.path import get_srcdir_url
+#Bug:  Delete a sheet in a calc document and LO crashes
+def get_url_for_data_file(file_name):
+    return get_srcdir_url() + "/sc/qa/uitest/calc_tests/data/" + file_name
+
+class tdf114992(UITestCase):
+    def test_tdf114992_delete_sheet_crash(self):
+        calc_doc = self.ui_test.load_file(get_url_for_data_file("tdf114992.ods"))
+        xCalcDoc = self.xUITest.getTopFocusWindow()
+        gridwin = xCalcDoc.getChild("grid_window")
+        document = self.ui_test.get_component()
+
+        self.ui_test.execute_dialog_through_command(".uno:Remove")  #delete sheet
+        xDialog = self.xUITest.getTopFocusWindow()
+        xOKButton = xDialog.getChild("yes")
+        xOKButton.executeAction("CLICK", tuple())
+
+        self.assertEqual(document.Sheets.getCount(), 1)
+        self.xUITest.executeCommand(".uno:Undo")
+        self.assertEqual(document.Sheets.getCount(), 2)
+
+        self.ui_test.close_doc()
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
---
+++ sw/qa/uitest/writer_tests/tdf78068.py
@@ -0,0 +1,36 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from uitest.framework import UITestCase
+from libreoffice.uno.propertyvalue import mkPropertyValues
+from uitest.uihelper.common import get_state_as_dict, type_text
+
+import time
+from uitest.debug import sleep
+from libreoffice.uno.propertyvalue import mkPropertyValues
+from uitest.uihelper.common import select_pos
+
+class tdf78068(UITestCase):
+
+   def test_tdf78068_format_paragraph_crash(self):
+        self.ui_test.create_doc_in_start_center("writer")
+        document = self.ui_test.get_component()
+        xWriterDoc = self.xUITest.getTopFocusWindow()
+        xWriterEdit = xWriterDoc.getChild("writer_edit")
+        #- add some text
+        type_text(xWriterEdit, "Test")
+        #- go to Format > Paragraph
+        self.ui_test.execute_dialog_through_command(".uno:ParagraphDialog")
+        xDialog = self.xUITest.getTopFocusWindow()
+        xTabs = xDialog.getChild("tabcontrol")
+        select_pos(xTabs, "0")
+        xOK = xDialog.getChild("ok")
+        xOK.executeAction("CLICK", tuple()) #close dialog
+
+        self.assertEqual(document.Text.String[0:4], "Test")
+        self.ui_test.close_doc()
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
