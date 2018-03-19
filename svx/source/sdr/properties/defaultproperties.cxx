e0930919f4aa7d847f56e857dfc0320f2dc2b455
Armin Le Grand <Armin.Le.Grand@cib.de (CIB)>
2018-03-19 17:42:29 +0100

    SOSAW080: Added Scale to DefaultProperties
    If a SdrModel change happens in DefaultProperties copy
    constructor (used from Clone()), potentially a Scale
    for the SfxItems has to be done.
    
    Change-Id: If9e646242cf89f6fde1315046952252a2c429779

--- svx/source/sdr/properties/defaultproperties.cxx
+++ svx/source/sdr/properties/defaultproperties.cxx
@@ -57,13 +57,29 @@ namespace sdr
         {
             if(rProps.mpItemSet)
             {
-                // TTTT Clone may be to another model and thus another ItemPool
+                // Clone may be to another model and thus another ItemPool.
                 // SfxItemSet supports that thus we are able to Clone all
                 // SfxItemState::SET items to the target pool.
                 mpItemSet.reset(
                     rProps.mpItemSet->Clone(
                         true,
                         &rObj.getSdrModelFromSdrObject().GetItemPool()));
+
+                // React on ModelChange: If metric has changed, scale items.
+                // As seen above, clone is supported, but scale is not included,
+                // thus: TTTT maybe add scale to SfxItemSet::Clone() (?)
+                if(&rObj.getSdrModelFromSdrObject() != &GetSdrObject().getSdrModelFromSdrObject())
+                {
+                    const MapUnit aOldUnit(GetSdrObject().getSdrModelFromSdrObject().GetScaleUnit());
+                    const MapUnit aNewUnit(rObj.getSdrModelFromSdrObject().GetScaleUnit());
+                    const bool bScaleUnitChanged(aNewUnit != aOldUnit);
+
+                    if(bScaleUnitChanged)
+                    {
+                        const Fraction aMetricFactor(GetMapFactor(aOldUnit, aNewUnit).X());
+                        Scale(aMetricFactor);
+                    }
+                }
 
                 // do not keep parent info, this may be changed by later constructors.
                 // This class just copies the ItemSet, ignore parent.
