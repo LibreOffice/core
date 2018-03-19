e4bf7cbc082a428dbef65f0d92cb0cdbda7af16e
Abhyudaya Sharma <sharmaabhyudaya@gmail.com>
2018-03-19 21:50:12 +0530

    tdf#45904 Move _CaptionShape Java tests to C++
    Change-Id: I66fd554a542912784cf730e28cc223591ad5c723
    Reviewed-on: https://gerrit.libreoffice.org/51575
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Jens Carl <j.carl43@gmx.de>

--- test/Library_subsequenttest.mk
+++ test/Library_subsequenttest.mk
@@ -47,6 +47,7 @@ $(eval $(call gb_Library_add_exception_objects,subsequenttest,\
 	test/source/container/xnamecontainer \
 	test/source/container/xnamereplace \
 	test/source/container/xnamed \
+	test/source/drawing/captionshape \
 	test/source/sheet/cellarealink \
 	test/source/sheet/cellproperties \
 	test/source/sheet/databaseimportdescriptor \
