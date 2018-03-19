LibreOffice/core

commit 6529cd54c29c5800340530a1b8182c341fbeeafb

don't use heap for elements in ScRangeList
no need to store small objects like this out of line.

Also
 - add move constructor and move assignment operator
 - drop Assign method since it now has the same
   signature as push_back

Change-Id: I9a8647d3a11f24166a83d399a358a2bce3b2cb79
Reviewed-on: https://gerrit.libreoffice.org/50899
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Eike Rathke <erack@redhat.com>

sc/source/core/data/drwlayer.cxx
@@ -1426,8 +1426,8 @@ static bool lcl_IsAllInRange( const ::std::vector< ScRangeList >& rRangesVector,
         const ScRangeList& rRanges = *aIt;
         for ( size_t i = 0, nCount = rRanges.size(); i < nCount; i++ )
         {
-            ScRange aRange = *rRanges[ i ];
-            if ( !rClipRange.In( aRange ) )
+            const ScRange & rRange = rRanges[ i ];
+            if ( !rClipRange.In( rRange ) )
             {
                 return false;   // at least one range is not valid
             }
@@ -1448,13 +1448,13 @@ static bool lcl_MoveRanges( ::std::vector< ScRangeList >& rRangesVector, const S
         ScRangeList& rRanges = *aIt;
         for ( size_t i = 0, nCount = rRanges.size(); i < nCount; i++ )
         {
-            ScRange* pRange = rRanges[ i ];
-            if ( rSourceRange.In( *pRange ) )
+            ScRange aRange = rRanges[ i ];
+            if ( rSourceRange.In( aRange ) )
             {
                 SCCOL nDiffX = rDestPos.Col() - rSourceRange.aStart.Col();
                 SCROW nDiffY = rDestPos.Row() - rSourceRange.aStart.Row();
                 SCTAB nDiffZ = rDestPos.Tab() - rSourceRange.aStart.Tab();
-                if (!pRange->Move( nDiffX, nDiffY, nDiffZ, aErrorRange))
+                if (!aRange.Move( nDiffX, nDiffY, nDiffZ, aErrorRange))
                 {
                     assert(!"can't move range");
                 }

sc/source/ui/view/viewfun2.cxx
@@ -330,7 +330,7 @@ static ScAutoSum lcl_GetAutoSumForColumnRange( ScDocument* pDoc, ScRangeList& rR
         bool bContinue = false;
         do
         {
-            rRangeList.Append( ScRange( nCol, nStartRow, nTab, nCol, nEndRow, nTab ) );
+            rRangeList.push_back( ScRange( nCol, nStartRow, nTab, nCol, nEndRow, nTab ) );
             nEndRow = static_cast< SCROW >( nExtend );
             bContinue = lcl_FindNextSumEntryInColumn( pDoc, nCol, nEndRow /*inout*/, nTab, nExtend /*out*/, aStart.Row() );
             if ( bContinue )
@@ -346,7 +346,7 @@ static ScAutoSum lcl_GetAutoSumForColumnRange( ScDocument* pDoc, ScRangeList& rR
         {
             --nStartRow;
         }
-        rRangeList.Append( ScRange( nCol, nStartRow, nTab, nCol, nEndRow, nTab ) );
+        rRangeList.push_back( ScRange( nCol, nStartRow, nTab, nCol, nEndRow, nTab ) );
         if (eSum == ScAutoSumNone)
             eSum = ScAutoSumData;
     }
@@ -375,7 +375,7 @@ static ScAutoSum lcl_GetAutoSumForRowRange( ScDocument* pDoc, ScRangeList& rRang
         bool bContinue = false;
         do
         {
-            rRangeList.Append( ScRange( nStartCol, nRow, nTab, nEndCol, nRow, nTab ) );
+            rRangeList.push_back( ScRange( nStartCol, nRow, nTab, nEndCol, nRow, nTab ) );
             nEndCol = static_cast< SCCOL >( nExtend );
             bContinue = lcl_FindNextSumEntryInRow( pDoc, nEndCol /*inout*/, nRow, nTab, nExtend /*out*/, aStart.Col() );
             if ( bContinue )
@@ -391,7 +391,7 @@ static ScAutoSum lcl_GetAutoSumForRowRange( ScDocument* pDoc, ScRangeList& rRang
         {
             --nStartCol;
         }
-        rRangeList.Append( ScRange( nStartCol, nRow, nTab, nEndCol, nRow, nTab ) );
+        rRangeList.push_back( ScRange( nStartCol, nRow, nTab, nEndCol, nRow, nTab ) );
         if (eSum == ScAutoSumNone)
             eSum = ScAutoSumData;
     }
@@ -476,7 +476,7 @@ bool ScViewFunc::GetAutoSumArea( ScRangeList& rRangeList )
                         --nStartCol;
                 }
             }
-            rRangeList.Append(
+            rRangeList.push_back(
                 ScRange( nStartCol, nStartRow, nTab, nEndCol, nEndRow, nTab ) );
             if ( eSum == ScAutoSumSum )
             {
@@ -665,7 +665,7 @@ bool ScViewFunc::AutoSum( const ScRange& rRange, bool bSubTotal, bool bSetCursor
                 if ( (eSum = lcl_GetAutoSumForColumnRange( pDoc, aRangeList, aRange )) != ScAutoSumNone )
                 {
                     if (++nRowSums == 1)
-                        nRowSumsStartCol = aRangeList[0]->aStart.Col();
+                        nRowSumsStartCol = aRangeList[0].aStart.Col();
                     const OUString aFormula = GetAutoSumFormula(
                         aRangeList, bSubTotal, ScAddress(nCol, nInsRow, nTab));
                     EnterData( nCol, nInsRow, nTab, aFormula );
@@ -702,7 +702,7 @@ bool ScViewFunc::AutoSum( const ScRange& rRange, bool bSubTotal, bool bSetCursor
                 if ( (eSum = lcl_GetAutoSumForRowRange( pDoc, aRangeList, aRange )) != ScAutoSumNone )
                 {
                     if (++nColSums == 1)
-                        nColSumsStartRow = aRangeList[0]->aStart.Row();
+                        nColSumsStartRow = aRangeList[0].aStart.Row();
                     const OUString aFormula = GetAutoSumFormula( aRangeList, bSubTotal, ScAddress(nInsCol, nRow, nTab) );
                     EnterData( nInsCol, nRow, nTab, aFormula );
                 }
@@ -749,15 +749,14 @@ OUString ScViewFunc::GetAutoSumFormula( const ScRangeList& rRangeList, bool bSub
     if(!rRangeList.empty())
     {
         ScRangeList aRangeList = rRangeList;
-        const ScRange* pFirst = aRangeList.front();
         size_t ListSize = aRangeList.size();
-        for ( size_t i = 0; i < ListSize; ++i )
+        for ( size_t i = 1; i < ListSize; ++i )
         {
-            const ScRange* p = aRangeList[i];
-            if (p != pFirst)
+            const ScRange & r = aRangeList[i];
+            if (i != 0)
                 pArray->AddOpCode(ocSep);
             ScComplexRefData aRef;
-            aRef.InitRangeRel(*p, rAddr);
+            aRef.InitRangeRel(r, rAddr);
             pArray->AddDoubleReference(aRef);
         }
     }
@@ -996,8 +995,8 @@ void ScViewFunc::SetPrintRanges( bool bEntireSheet, const OUString* pPrint,
                 rMark.FillRangeListWithMarks( pList.get(), false );
                 for (size_t i = 0, n = pList->size(); i < n; ++i)
                 {
-                    ScRange* pR = (*pList)[i];
-                    rDoc.AddPrintRange(nTab, *pR);
+                    const ScRange & rR = (*pList)[i];
+                    rDoc.AddPrintRange(nTab, rR);
                 }
             }
         }
@@ -1391,7 +1390,7 @@ void ScViewFunc::FillAuto( FillDir eDir, SCCOL nStartCol, SCROW nStartRow,
                 default:
                     break;
             }
-            aChangeRanges.Append( aChangeRange );
+            aChangeRanges.push_back( aChangeRange );
             HelperNotifyChanges::Notify(*pModelObj, aChangeRanges);
         }
     }
@@ -1863,7 +1862,7 @@ bool ScViewFunc::SearchAndReplace( const SvxSearchItem* pSearchItem,
                 rMark.ResetMark();
                 for (size_t i = 0, n = aMatchedRanges.size(); i < n; ++i)
                 {
-                    const ScRange& r = *aMatchedRanges[i];
+                    const ScRange& r = aMatchedRanges[i];
                     if (r.aStart.Tab() == nTab)
                         rMark.SetMultiMarkArea(r);
                 }
@@ -2762,7 +2761,7 @@ void ScViewFunc::MoveTable(
                 break;  // for
             }
             ScRange aRange( 0, 0, TheTabs[j], MAXCOL, MAXROW, TheTabs[j] );
-            aParam.maRanges.Append(aRange);
+            aParam.maRanges.push_back(aRange);
         }
         pDoc->SetClipParam(aParam);
         if ( nErrVal > 0 )

commit d94052af4034a2b5d27325edc6dd61acde06b46c

tdf#116421 autosum is not working
regression from
   commit 6529cd54c29c5800340530a1b8182c341fbeeafb
   don't use heap for elements in ScRangeList

The change in lcl_MoveRanges is not part of the fix, just something else
I noticed when reviewing the problematic commit

Change-Id: I958937f92a84f6e8e7bec607705c3a958d70ad3c

sc/source/core/data/drwlayer.cxx
@@ -1511,13 +1511,13 @@ static bool lcl_MoveRanges( ::std::vector< ScRangeList >& rRangesVector, const S
         ScRangeList& rRanges = *aIt;
         for ( size_t i = 0, nCount = rRanges.size(); i < nCount; i++ )
         {
-            ScRange aRange = rRanges[ i ];
-            if ( rSourceRange.In( aRange ) )
+            ScRange & rRange = rRanges[ i ];
+            if ( rSourceRange.In( rRange ) )
             {
                 SCCOL nDiffX = rDestPos.Col() - rSourceRange.aStart.Col();
                 SCROW nDiffY = rDestPos.Row() - rSourceRange.aStart.Row();
                 SCTAB nDiffZ = rDestPos.Tab() - rSourceRange.aStart.Tab();
-                if (!aRange.Move( nDiffX, nDiffY, nDiffZ, aErrorRange))
+                if (!rRange.Move( nDiffX, nDiffY, nDiffZ, aErrorRange))
                 {
                     assert(!"can't move range");
                 }

sc/source/ui/view/viewfun2.cxx
@@ -750,7 +750,7 @@ OUString ScViewFunc::GetAutoSumFormula( const ScRangeList& rRangeList, bool bSub
     {
         ScRangeList aRangeList = rRangeList;
         size_t ListSize = aRangeList.size();
-        for ( size_t i = 1; i < ListSize; ++i )
+        for ( size_t i = 0; i < ListSize; ++i )
         {
             const ScRange & r = aRangeList[i];
             if (i != 0)