2e889e07ec6400de7ec78bac7dd0bfe6e745ac5b
Winfried Donkers <winfrieddonkers@libreoffice.org>
2018-03-21 16:57:36 +0100

    tdf#97977 make MODE.MULT fully compatible with Excel.
    Also, improve efficiency of function.
    
    Change-Id: Id01542bdd997e0cacfdfd16683d1ac832d22fa35

--- sc/source/core/inc/interpre.hxx
+++ sc/source/core/inc/interpre.hxx
@@ -932,8 +932,7 @@ private:
     void GetSortArray( sal_uInt8 nParamCount, ::std::vector<double>& rSortArray, ::std::vector<long>* pIndexOrder, bool bConvertTextInArray, bool bAllowEmptyArray );
     static void QuickSort(::std::vector<double>& rSortArray, ::std::vector<long>* pIndexOrder);
     void ScModalValue();
-    void ScModalValue_MS();
-    void ScModalValue_Multi();
+    void ScModalValue_MS( bool bSingle );
     void ScAveDev();
     void ScAggregate();
     void ScDevSq();
