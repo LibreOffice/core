LibreOffice/core

commit 2ccde70d60d3a5074faf49260e8fe0ccdb91ff26

teach redundantcast plugin about functional casts
Change-Id: Iac8ccd17d9e46ebb2cb55db7adb06c469bbd4ea0
Reviewed-on: https://gerrit.libreoffice.org/37910
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Noel Grandin <noel.grandin@collabora.co.uk>

sw/source/core/layout/paintfrm.cxx
@@ -5107,7 +5107,7 @@ void PaintCharacterBorder(
 
         if( aShadow.GetLocation() != SvxShadowLocation::NONE )
         {
-            lcl_PaintShadow( SwRect(aAlignedRect), aAlignedRect, aShadow,
+            lcl_PaintShadow( aAlignedRect, aAlignedRect, aShadow,
                              false, bTop, bBottom, bLeft, bRight, gProp);
         }
     }

commit 64b009e611b7b46b3a2b51b51f2313e28c2802aa

tdf#116525 writer, no shadow around box
regression from

    commit 2ccde70d60d3a5074faf49260e8fe0ccdb91ff26
    teach redundantcast plugin about functional casts

because we are now passing the same variable as a both an in-parameter
and an out-parameter

Change-Id: I6454103fbc46530180cde5f725273e062eb2e25d

sw/source/core/layout/paintfrm.cxx
@@ -4755,7 +4755,7 @@ void PaintCharacterBorder(
 
         if( aShadow.GetLocation() != SvxShadowLocation::NONE )
         {
-            lcl_PaintShadow( aAlignedRect, aAlignedRect, aShadow,
+            lcl_PaintShadow( rPaintArea, aAlignedRect, aShadow,
                              false, bTop, bBottom, bLeft, bRight, gProp);
         }
     }