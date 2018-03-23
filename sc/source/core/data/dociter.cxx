LibreOffice/core

commit bad266fa06294f1dacec11ec02dfc6ae4ec8cdc4

ScQueryEntry::GetSearchTextPtr() with SearchType, tdf#72196
Change-Id: I1c9c5c6271927bb2d2ac9309e9a6ca36016237f4

sc/source/core/data/dociter.cxx
@@ -1296,9 +1296,9 @@ bool ScQueryCellIterator::FindEqualOrSortedLastInRange( SCCOL& nFoundCol,
     SetStopOnMismatch( true ); // assume sorted keys
     SetTestEqualCondition( true );
     bIgnoreMismatchOnLeadingStrings = bIgnoreMismatchOnLeadingStringsP;
-    bool bRegExp = mpParam->eSearchType == utl::SearchParam::SRCH_REGEXP &&
+    bool bLiteral = mpParam->eSearchType == utl::SearchParam::SRCH_NORMAL &&
         mpParam->GetEntry(0).GetQueryItem().meType == ScQueryEntry::ByString;
-    bool bBinary = !bRegExp && mpParam->bByRow && (mpParam->GetEntry(0).eOp ==
+    bool bBinary = bLiteral && mpParam->bByRow && (mpParam->GetEntry(0).eOp ==
             SC_LESS_EQUAL || mpParam->GetEntry(0).eOp == SC_GREATER_EQUAL);
     bool bFound = false;
     if (bBinary)
@@ -1413,7 +1413,7 @@ bool ScQueryCellIterator::FindEqualOrSortedLastInRange( SCCOL& nFoundCol,
         maCurPos = aPosSave;
         return true;
     }
-    if ( (bSearchForEqualAfterMismatch || mpParam->eSearchType == utl::SearchParam::SRCH_REGEXP) &&
+    if ( (bSearchForEqualAfterMismatch || mpParam->eSearchType != utl::SearchParam::SRCH_NORMAL) &&
             StoppedOnMismatch() )
     {
         // Assume found entry to be the last value less than respectively

sc/inc/queryentry.hxx
@@ -23,13 +23,9 @@
 #include "address.hxx"
 #include "global.hxx"
 #include <svl/sharedstring.hxx>
+#include <unotools/textsearch.hxx>
 
 #include <vector>
-
-namespace utl {
-    class SearchParam;
-    class TextSearch;
-}
 
 /**
  * Each instance of this struct represents a single filtering criteria.
@@ -55,15 +51,15 @@ struct SC_DLLPUBLIC ScQueryEntry
     SCCOLROW        nField;
     ScQueryOp       eOp;
     ScQueryConnect  eConnect;
-    mutable utl::SearchParam* pSearchParam;       ///< if RegExp, not saved
-    mutable utl::TextSearch*  pSearchText;        ///< if RegExp, not saved
+    mutable utl::SearchParam* pSearchParam;       ///< if Wildcard or RegExp, not saved
+    mutable utl::TextSearch*  pSearchText;        ///< if Wildcard or RegExp, not saved
 
     ScQueryEntry();
     ScQueryEntry(const ScQueryEntry& r);
     ~ScQueryEntry();
 
-    /// creates pSearchParam and pSearchText if necessary, always RegExp!
-    utl::TextSearch* GetSearchTextPtr( bool bCaseSens ) const;
+    /// creates pSearchParam and pSearchText if necessary
+    utl::TextSearch* GetSearchTextPtr( utl::SearchParam::SearchType eSearchType, bool bCaseSens ) const;
 
     QueryItemsType& GetQueryItems() { return maQueryItems;}
     const QueryItemsType& GetQueryItems() const { return maQueryItems;}

sc/source/core/data/dpcache.cxx
@@ -537,7 +537,7 @@ bool ScDPCache::ValidQuery( SCROW nRow, const ScQueryParam &rParam) const
                 sal_Int32 nStart = 0;
                 sal_Int32 nEnd   = aCellStr.getLength();
 
-                bool bMatch = (bool) rEntry.GetSearchTextPtr( rParam.bCaseSens )
+                bool bMatch = (bool) rEntry.GetSearchTextPtr( rParam.eSearchType, rParam.bCaseSens )
                               ->SearchForward( aCellStr, &nStart, &nEnd );
                 // from 614 on, nEnd is behind the found text
                 if (bMatch && bMatchWholeCell

sc/source/core/data/table3.cxx
@@ -2423,12 +2423,12 @@ public:
             {
                 nEnd = 0;
                 nStart = aCellStr.getLength();
-                bMatch = rEntry.GetSearchTextPtr( mrParam.bCaseSens )
+                bMatch = rEntry.GetSearchTextPtr( mrParam.eSearchType, mrParam.bCaseSens )
                     ->SearchBackward(aCellStr.getString(), &nStart, &nEnd);
             }
             else
             {
-                bMatch = rEntry.GetSearchTextPtr( mrParam.bCaseSens )
+                bMatch = rEntry.GetSearchTextPtr( mrParam.eSearchType, mrParam.bCaseSens )
                     ->SearchForward(aCellStr.getString(), &nStart, &nEnd);
             }
             if ( bMatch && bMatchWholeCell

sc/source/core/tool/compare.cxx
@@ -140,7 +140,7 @@ double CompareFunc( const Compare& rComp, CompareOptions* pOptions )
             {
                 sal_Int32 nStart = 0;
                 sal_Int32 nStop  = rCell1.maStr.getLength();
-                bool bMatch = rEntry.GetSearchTextPtr(
+                bool bMatch = rEntry.GetSearchTextPtr( pOptions->eSearchType,
                         !rComp.mbIgnoreCase)->SearchForward(
                             rCell1.maStr.getString(), &nStart, &nStop);
                 if (bMatch && pOptions->bMatchWholeCell && (nStart != 0 || nStop != rCell1.maStr.getLength()))

sc/source/core/tool/queryentry.cxx
@@ -164,13 +164,13 @@ bool ScQueryEntry::operator==( const ScQueryEntry& r ) const
     // do not compare pSearchParam and pSearchText!
 }
 
-utl::TextSearch* ScQueryEntry::GetSearchTextPtr( bool bCaseSens ) const
+utl::TextSearch* ScQueryEntry::GetSearchTextPtr( utl::SearchParam::SearchType eSearchType, bool bCaseSens ) const
 {
     if ( !pSearchParam )
     {
         OUString aStr = maQueryItems[0].maString.getString();
         pSearchParam = new utl::SearchParam(
-            aStr, utl::SearchParam::SRCH_REGEXP, bCaseSens, false, false);
+            aStr, eSearchType, bCaseSens, false, false);
         pSearchText = new utl::TextSearch( *pSearchParam, *ScGlobal::pCharClass );
     }
     return pSearchText;

commit c53235ba9aab844c4b16ecf7a28429e256bace6d

Enable binary search for numeric values again
... as used in LOOKUP and VLOOKUP.

This was disabled as a fallout from

    commit bad266fa06294f1dacec11ec02dfc6ae4ec8cdc4
    CommitDate: Mon Feb 22 21:30:28 2016 +0100

        ScQueryEntry::GetSearchTextPtr() with SearchType, tdf#72196

that changed the !bRegExp condition (which includes numeric) to
bLiteral (which covers only text string) searches.

Change-Id: I265286ffe75bc7cd9e660ab82b0e647d82865eb9

sc/source/core/data/dociter.cxx
@@ -1288,8 +1288,9 @@ bool ScQueryCellIterator::FindEqualOrSortedLastInRange( SCCOL& nFoundCol,
     bIgnoreMismatchOnLeadingStrings = true;
     bool bLiteral = mpParam->eSearchType == utl::SearchParam::SearchType::Normal &&
         mpParam->GetEntry(0).GetQueryItem().meType == ScQueryEntry::ByString;
-    bool bBinary = bLiteral && mpParam->bByRow && (mpParam->GetEntry(0).eOp ==
-            SC_LESS_EQUAL || mpParam->GetEntry(0).eOp == SC_GREATER_EQUAL);
+    bool bBinary = mpParam->bByRow &&
+        (bLiteral || mpParam->GetEntry(0).GetQueryItem().meType == ScQueryEntry::ByValue) &&
+        (mpParam->GetEntry(0).eOp == SC_LESS_EQUAL || mpParam->GetEntry(0).eOp == SC_GREATER_EQUAL);
     bool bFound = false;
     if (bBinary)
     {