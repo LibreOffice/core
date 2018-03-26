7a1c5d90cb6b17d2f7535d32f3aa44570bdafd31
Jens Carl <j.carl43@gmx.de>
2018-03-26 07:13:25 +0000

    tdf#45904 Move _XDDELink Java test to C++
    Change-Id: Ic7a23088ee10f2d92d05354131e3066ec098a7b9

---
+++ sc/qa/extras/scddelinkobj.cxx
@@ -0,0 +1,100 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <test/calc_unoapi_test.hxx>
+#include <test/sheet/xddelink.hxx>
+
+#include <com/sun/star/beans/XPropertySet.hpp>
+#include <com/sun/star/container/XIndexAccess.hpp>
+#include <com/sun/star/container/XNameAccess.hpp>
+#include <com/sun/star/lang/XComponent.hpp>
+#include <com/sun/star/lang/XMultiServiceFactory.hpp>
+#include <com/sun/star/sheet/XDDELink.hpp>
+#include <com/sun/star/sheet/XSpreadsheet.hpp>
+#include <com/sun/star/sheet/XSpreadsheetDocument.hpp>
+#include <com/sun/star/sheet/XSpreadsheets.hpp>
+#include <com/sun/star/uno/XInterface.hpp>
+
+#include <com/sun/star/uno/Reference.hxx>
+
+using namespace css;
+using namespace css::uno;
+using namespace com::sun::star;
+
+namespace sc_apitest
+{
+class ScDDELinkObj : public CalcUnoApiTest, public apitest::XDDELink
+{
+public:
+    ScDDELinkObj();
+
+    virtual uno::Reference<uno::XInterface> init() override;
+    virtual void setUp() override;
+    virtual void tearDown() override;
+
+    CPPUNIT_TEST_SUITE(ScDDELinkObj);
+
+    // XDDELink
+    CPPUNIT_TEST(testGetApplication);
+    CPPUNIT_TEST(testGetItem);
+    CPPUNIT_TEST(testGetTopic);
+
+    CPPUNIT_TEST_SUITE_END();
+
+private:
+    uno::Reference<lang::XComponent> mxComponent;
+};
+
+ScDDELinkObj::ScDDELinkObj()
+    : CalcUnoApiTest("/sc/qa/extras/testdocuments")
+{
+}
+
+uno::Reference<uno::XInterface> ScDDELinkObj::init()
+{
+    uno::Reference<sheet::XSpreadsheetDocument> xDoc(mxComponent, UNO_QUERY_THROW);
+    CPPUNIT_ASSERT_MESSAGE("no calc document", xDoc.is());
+
+    uno::Reference<sheet::XSpreadsheets> xSheets(xDoc->getSheets(), UNO_QUERY_THROW);
+    uno::Reference<container::XIndexAccess> xIA(xSheets, UNO_QUERY_THROW);
+    uno::Reference<sheet::XSpreadsheet> xSheet(xIA->getByIndex(0), UNO_QUERY_THROW);
+
+    xSheet->getCellByPosition(5, 5)->setFormula(
+        "=DDE(\"soffice\";\"ScDDELinksObj.ods\";\"Sheet.A1\"");
+    xSheet->getCellByPosition(1, 4)->setFormula(
+        "=DDE(\"soffice\";\"ScDDELinksObj.ods\";\"Sheet.A1\"");
+
+    uno::Reference<beans::XPropertySet> xPropSet(xDoc, UNO_QUERY_THROW);
+    uno::Any aDDELinks = xPropSet->getPropertyValue("DDELinks");
+    uno::Reference<container::XNameAccess> xNA(aDDELinks, UNO_QUERY_THROW);
+    uno::Sequence<OUString> sLinkNames = xNA->getElementNames();
+    uno::Reference<sheet::XDDELink> xDDELink(xNA->getByName(sLinkNames[0]), UNO_QUERY_THROW);
+    return xDDELink;
+}
+
+void ScDDELinkObj::setUp()
+{
+    CalcUnoApiTest::setUp();
+    // create a calc document
+    mxComponent = loadFromDesktop("private:factory/scalc");
+}
+
+void ScDDELinkObj::tearDown()
+{
+    closeDocument(mxComponent);
+    CalcUnoApiTest::tearDown();
+}
+
+CPPUNIT_TEST_SUITE_REGISTRATION(ScDDELinkObj);
+
+} // namespace sc_apitest
+
+CPPUNIT_PLUGIN_IMPLEMENT();
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
