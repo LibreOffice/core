681f8c7f616aa1987cac6bfcb128a926cc7ec502
Mark Hung <marklh9@gmail.com>
2018-03-28 07:26:08 +0800

    tdf#115623: patch libepubgen to support writing-mode.
    Backport three patches from libepubgen master branch to support
    exporting style:writing-mode as body CSS styles.
    
    1. Support writing-mode for reflowable layout method.
    https://sourceforge.net/p/libepubgen/code/ci/9a284081eea4a95235a6d6a6a50cbe3f7ad323ba/
    
    2. Always keep page properties when splitting.
    https://sourceforge.net/p/libepubgen/code/ci/0318031b9094b9180d1d391d0ca31a782b016e99/
    
    3. Ensure page properties in the page span work
    https://sourceforge.net/p/libepubgen/code/ci/1f602fcaa74fc9dbc6457019d11c602ff4040a4e/
    
    Change-Id: I9033cb1f5fcbfedb423308fb29b9bd4d6d7d7a43

--- external/libepubgen/UnpackedTarball_libepubgen.mk
+++ external/libepubgen/UnpackedTarball_libepubgen.mk
@@ -10,6 +10,12 @@
 epubgen_patches :=
 # Backport of <https://sourceforge.net/p/libepubgen/code/ci/006848cb62225647c418d5143d4e88a9d73829da/>.
 epubgen_patches += libepubgen-epub3.patch.1
+# Backport of <https://sourceforge.net/p/libepubgen/code/ci/9a284081eea4a95235a6d6a6a50cbe3f7ad323ba/>.
+epubgen_patches += 0001-Support-writing-mode-for-reflowable-layout-method.patch.1
+# Backport of <https://sourceforge.net/p/libepubgen/code/ci/0318031b9094b9180d1d391d0ca31a782b016e99/>.
+epubgen_patches += 0002-Always-keep-page-properties-when-splitting-the-HTML-.patch.1
+# Backport of <https://sourceforge.net/p/libepubgen/code/ci/1f602fcaa74fc9dbc6457019d11c602ff4040a4e/>.
+epubgen_patches += 0003-Ensure-page-properties-in-the-page-span-works.patch.1
 
 ifeq ($(COM_IS_CLANG),TRUE)
 ifneq ($(filter -fsanitize=%,$(CC)),)
