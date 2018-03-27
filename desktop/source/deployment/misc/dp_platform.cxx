LibreOffice/core

commit 2d2294fb82bd4f0d1d805405631aeb1acbc36cbb

Add x64 Windows here, too

desktop/source/deployment/misc/dp_platform.cxx
@@ -38,6 +38,7 @@
 
 #define PLATFORM_ALL                "all"
 #define PLATFORM_WIN_X86            "windows_x86"
+#define PLATFORM_WIN_X86_64         "windows_x86_64"
 #define PLATFORM_LINUX_X86          "linux_x86"
 #define PLATFORM_LINUX_X86_64       "linux_x86_64"
 #define PLATFORM_KFREEBSD_X86       "kfreebsd_x86"
@@ -133,6 +134,8 @@ namespace
             ret = true;
         else if (token.equals(OUSTR(PLATFORM_WIN_X86)))
             ret = checkOSandCPU(OUSTR("Windows"), OUSTR("x86"));
+        else if (token.equals(OUSTR(PLATFORM_WIN_X86_64)))
+            ret = checkOSandCPU(OUSTR("Windows"), OUSTR("x86_64"));
         else if (token.equals(OUSTR(PLATFORM_LINUX_X86)))
             ret = checkOSandCPU(OUSTR("Linux"), OUSTR("x86"));
         else if (token.equals(OUSTR(PLATFORM_LINUX_X86_64)))

commit 2d31694161cffdbd4d4fdbe8ab5f3ff7883f0a92

Fix typo ("x86_64" -> "X86_64")
...introduced with 2d2294fb82bd4f0d1d805405631aeb1acbc36cbb "Add x64 Windows
here, too".  As reported by Richard Webb at
<https://lists.freedesktop.org/archives/libreoffice/2018-March/079870.html>
"Re: Manifest.xml for 64bit LO".

Change-Id: I1681908012e73e520bebd79b8149788358ea49a3

desktop/source/deployment/misc/dp_platform.cxx
@@ -77,7 +77,7 @@ namespace
         else if (token == "windows_x86")
             ret = checkOSandCPU("Windows", "x86");
         else if (token == "windows_x86_64")
-            ret = checkOSandCPU("Windows", "x86_64");
+            ret = checkOSandCPU("Windows", "X86_64");
         else if (token == "solaris_sparc")
             ret = checkOSandCPU("Solaris", "SPARC");
         else if (token == "solaris_sparc64")