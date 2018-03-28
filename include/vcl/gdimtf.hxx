7dd6b165e0398fc667bbfc7df95b94519a367c56
Caolán McNamara <caolanm@redhat.com>
2018-03-28 08:53:20 +0100

    ofz#7165 set a recursion limit for svm in svm
    Change-Id: Id9089986012588690b6d5e33cd71d094ef2357dd

--- include/vcl/gdimtf.hxx
+++ include/vcl/gdimtf.hxx
@@ -38,7 +38,7 @@ namespace tools {
     class PolyPolygon;
 }
 class Gradient;
-
+struct ImplMetaReadData;
 
 #define GDI_METAFILE_END                (size_t(0xFFFFFFFF))
 
@@ -185,7 +185,7 @@ public:
 
     // Stream-operators write (still) the old format
     // and read both the old and the new format
-    friend VCL_DLLPUBLIC SvStream& ReadGDIMetaFile( SvStream& rIStm, GDIMetaFile& rGDIMetaFile );
+    friend VCL_DLLPUBLIC SvStream& ReadGDIMetaFile(SvStream& rIStm, GDIMetaFile& rGDIMetaFile, ImplMetaReadData* pReadData = nullptr);
     friend VCL_DLLPUBLIC SvStream& WriteGDIMetaFile( SvStream& rOStm, const GDIMetaFile& rGDIMetaFile );
 
     /// Creates an antialiased thumbnail
