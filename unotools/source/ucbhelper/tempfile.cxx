d7a8fa7adf2b2b098a1e38cd7002c928d015b489
Stephan Bergmann <sbergman@redhat.com>
2018-03-29 16:39:50 +0200

    Remove bogus assert
    ...that had been added with f0215dcc9e5ba47d4b7d5b9759f1bd774942d388 "unotools:
    assert if TempFile::GetURL() fails due to missing file UCP".  But when SRCDIR is
    a read-only tree, JunitTest_sc_unoapi_2 fails with that assert firing when
    TempFile::GetURL is called from SfxMedium::CreateTempFile
    (sfx2/source/doc/docfile.cxx), which in turn is prepared to handle an empty
    return value from GetURL, and the test succeeds after removing the assert.  So
    it looks like that assert was not precise enough to only fire in the scenarios
    where it was intended to fire.
    
    Change-Id: I333e9026ee14e9d1140b1b475793a5b15ededd70
    Reviewed-on: https://gerrit.libreoffice.org/52085
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

--- unotools/source/ucbhelper/tempfile.cxx
+++ unotools/source/ucbhelper/tempfile.cxx
@@ -409,7 +409,6 @@ OUString TempFile::GetFileName() const
 
 OUString const & TempFile::GetURL() const
 {
-    assert(!aName.isEmpty() && "TempFile::GetURL failed: unit test is leaking temp files, add the ucpfile1 component!");
     return aName;
 }
 
