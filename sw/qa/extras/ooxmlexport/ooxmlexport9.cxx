22adee816d967e64cd84e9784ad5e97311526729
Justin Luth <justin_luth@sil.org>
2018-03-30 21:22:44 +0300

    tdf#106062 ooxmlimport: skip fake tab only on hanging indent
    Export has changed, so that it only exports a tab when the
    footnote paragraph has a hanging indent. Adjusting the import
    code to match that change.
    
    Please test with MSO before flagging this patch as a regression.
    Certainly there will be some documents previously saved by LO
    which will now, in LO, show an extra tab character after the footnote.
    Any previously saved document without a hanging indent will display
    this extra tab. However, MSO has always seen that extra tab, so
    these patches are enhancing compatibility.
    
    This patch corrects several incorrect assumptions:
    -The paragraph style is not necessarily "Footnote".
    -The paragraph may have directly defined a hanging margin.
    -An aesthetic tab is needed on a hanging indent, not a defined margin.
    
    Change-Id: Ieaa76448ce202d92efdb8d1fc04ba2674ed120ba

--- sw/qa/extras/ooxmlexport/ooxmlexport9.cxx
+++ sw/qa/extras/ooxmlexport/ooxmlexport9.cxx
@@ -917,6 +917,15 @@ DECLARE_OOXMLEXPORT_TEST(testTdf105095, "tdf105095.docx")
     CPPUNIT_ASSERT_EQUAL( OUString("\tfootnote"), xTextRange->getString() );
 }
 
+DECLARE_OOXMLEXPORT_TEST(testTdf106062_nonHangingFootnote, "tdf106062_nonHangingFootnote.odt")
+{
+    uno::Reference<text::XFootnotesSupplier> xFootnotesSupplier(mxComponent, uno::UNO_QUERY);
+    uno::Reference<container::XIndexAccess> xFootnotes(xFootnotesSupplier->getFootnotes(), uno::UNO_QUERY);
+    uno::Reference<text::XTextRange> xTextRange(xFootnotes->getByIndex(0), uno::UNO_QUERY);
+    // This failed, tab between the footnote number and the footnote content was lost on import.
+    CPPUNIT_ASSERT_MESSAGE( "Footnote starts with a tab", xTextRange->getString().startsWith("\t") );
+}
+
 DECLARE_OOXMLEXPORT_TEST( testActiveXCheckbox, "activex_checkbox.docx" )
 {
     uno::Reference<drawing::XControlShape> xControlShape( getShape(1), uno::UNO_QUERY );
