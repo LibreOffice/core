e0767c32125abc2de7e7edb1fa9c2655926eb16c
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2018-04-04 17:51:26 +0900

    vcl: Add a internal (memory) manager for Graphic objects
    This adds vcl::graphic::Manager which is a manager singleton that
    tracks all the creation fo graphic objects and swaps them out on
    a time and allocation basis.
    
    Time based - every number of seconds it looks for Graphics that
    weren't used for a time.
    
    Allocation based - when creating a new Graphic and the total of
    Graphic uses more than the total amount of memory for Graphics
    defined in configuration, it tries to release the Graphics that
    weren't used for a time.
    
    Change-Id: I5dbf74db4a6455d32c4abcbad7be21c7f0534642

---
+++ vcl/inc/graphic/Manager.hxx
@@ -0,0 +1,71 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef INCLUDED_VCL_INC_GRAPHIC_MANAGER_HXX
+#define INCLUDED_VCL_INC_GRAPHIC_MANAGER_HXX
+
+#include <sal/types.h>
+#include <rtl/ustring.hxx>
+#include <vcl/bitmapex.hxx>
+#include <vcl/animate.hxx>
+#include <vcl/vectorgraphicdata.hxx>
+#include <vcl/metaact.hxx>
+#include <vcl/timer.hxx>
+
+#include <officecfg/Office/Common.hxx>
+#include <unotools/configmgr.hxx>
+
+#include <memory>
+#include <chrono>
+#include <unordered_set>
+
+class ImpGraphic;
+
+namespace vcl
+{
+namespace graphic
+{
+class Manager final
+{
+private:
+    std::unordered_set<ImpGraphic*> m_pImpGraphicList;
+    sal_Int64 mnTotalCacheSize;
+    sal_Int64 mnUsedSize;
+    Timer maSwapOutTimer;
+
+    Manager();
+
+    void registerGraphic(std::shared_ptr<ImpGraphic>& rImpGraphic, OUString const& rsContext);
+
+    DECL_LINK(SwapOutTimerHandler, Timer*, void);
+
+public:
+    static Manager& get();
+
+    void swappedIn(const ImpGraphic* pImpGraphic);
+    void swappedOut(const ImpGraphic* pImpGraphic);
+
+    void reduceGraphicMemory();
+    void changeExisting(const ImpGraphic* pImpGraphic, sal_Int64 nOldSize);
+    void unregisterGraphic(ImpGraphic* pImpGraphic);
+
+    std::shared_ptr<ImpGraphic> copy(std::shared_ptr<ImpGraphic> const& pImpGraphic);
+    std::shared_ptr<ImpGraphic> newInstance();
+    std::shared_ptr<ImpGraphic> newInstance(const Bitmap& rBitmap);
+    std::shared_ptr<ImpGraphic> newInstance(const BitmapEx& rBitmapEx);
+    std::shared_ptr<ImpGraphic> newInstance(const VectorGraphicDataPtr& rVectorGraphicDataPtr);
+    std::shared_ptr<ImpGraphic> newInstance(const Animation& rAnimation);
+    std::shared_ptr<ImpGraphic> newInstance(const GDIMetaFile& rMtf);
+};
+}
+} // end namespace vcl::graphic
+
+#endif // INCLUDED_VCL_INC_GRAPHIC_MANAGER_HXX
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
