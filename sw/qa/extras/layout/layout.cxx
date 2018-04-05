05ed69af14af8f138f69941c7c6186b39c742eb4
Miklos Vajna <vmiklos@collabora.co.uk>
2018-04-05 18:04:25 +0200

    tdf#116830 sw: handle cell background for BACKGROUND_PARA_OVER_DRAWINGS
    Commit 18b3feb8bef06bf7b126fd0bc743e19479cb8026 (n#778133 sw: add
    BackgroundParaOverDrawings compat flag, 2012-09-12) added a layout
    compat flag that is meant to make Writer behave the same as Word when it
    comes to the order of various backgrounds.
    
    The original fix was that in case there are background shapes and also
    paragraph backgrounds, then this flag can opt in to have the paragraph
    backgrounds on top of background shapes.
    
    Cell backgrounds behave the same way in Word, so extend the behavior of
    the compat flag to cover not only the background of text frames but also
    of cell frames.
    
    (The test could be hacked into CppunitTest_sw_uiwriter, but 1) this is
    layout, not UI and 2) that one started to get rather large.)
    
    (cherry picked from commit 09954c381895f340c9b73af64b0833d8b4689c06)
    
    Change-Id: Ifca1851217000f6daac50e6c9562c042aca6ff7d
    Reviewed-on: https://gerrit.libreoffice.org/52982
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Michael Stahl <Michael.Stahl@cib.de>

---
+++ sw/qa/extras/layout/layout.cxx
@@ -0,0 +1,68 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <swmodeltestbase.hxx>
+#include <test/mtfxmldump.hxx>
+
+static char const DATA_DIRECTORY[] = "/sw/qa/extras/layout/data/";
+
+/// Test to assert layout / rendering result of Writer.
+class SwLayoutWriter : public SwModelTestBase
+{
+public:
+    void testTdf116830();
+
+    CPPUNIT_TEST_SUITE(SwLayoutWriter);
+    CPPUNIT_TEST(testTdf116830);
+    CPPUNIT_TEST_SUITE_END();
+
+private:
+    SwDoc* createDoc(const char* pName = nullptr);
+};
+
+SwDoc* SwLayoutWriter::createDoc(const char* pName)
+{
+    load(DATA_DIRECTORY, pName);
+
+    SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument*>(mxComponent.get());
+    CPPUNIT_ASSERT(pTextDoc);
+    return pTextDoc->GetDocShell()->GetDoc();
+}
+
+void SwLayoutWriter::testTdf116830()
+{
+    SwDoc* pDoc = createDoc("tdf116830.odt");
+    SwDocShell* pShell = pDoc->GetDocShell();
+
+    // Dump the rendering of the first page as an XML file.
+    std::shared_ptr<GDIMetaFile> xMetaFile = pShell->GetPreviewMetaFile();
+    MetafileXmlDump dumper;
+    xmlDocPtr pXmlDoc = dumper.dumpAndParse(*xMetaFile);
+    CPPUNIT_ASSERT(pXmlDoc);
+
+    // Assert that the yellow rectangle (cell background) is painted after the
+    // polypolygon (background shape).
+    // Background shape: 1.1.1.2
+    // Cell background: 1.1.1.3
+    assertXPath(
+        pXmlDoc,
+        "/metafile/push[1]/push[1]/push[1]/push[2]/push[1]/push[1]/fillcolor[@color='#729fcf']", 1);
+    assertXPath(pXmlDoc, "/metafile/push[1]/push[1]/push[1]/push[2]/push[1]/push[1]/polypolygon",
+                1);
+
+    // This failed: cell background was painted before the background shape.
+    assertXPath(pXmlDoc,
+                "/metafile/push[1]/push[1]/push[1]/push[3]/push[1]/fillcolor[@color='#ffff00']", 1);
+    assertXPath(pXmlDoc, "/metafile/push[1]/push[1]/push[1]/push[3]/push[1]/rect", 1);
+}
+
+CPPUNIT_TEST_SUITE_REGISTRATION(SwLayoutWriter);
+CPPUNIT_PLUGIN_IMPLEMENT();
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
