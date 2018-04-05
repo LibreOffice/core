60fcb4cdfd947787b64bc8942f7eb695cfe4c3e6
Miklos Vajna <vmiklos@collabora.co.uk>
2018-04-05 18:04:25 +0200

    tdf#116830 sw: handle cell background for BACKGROUND_PARA_OVER_DRAWINGS
    Commit 18b3feb8bef06bf7b126fd0bc743e19479cb8026 (n#778133 sw: add
    BackgroundParaOverDrawings compat flag, 2012-09-12) added a layout
    compat flag that is meant to make Writer behave the same as Word when it
    comes to the order of various backgrounds.
    
    The original fix was that in case there are background shapes and also
    paragraph backgrounds, then this flag can opt in to have the paragraph
    backgrounds on top of background shapes.
    
    Cell backgrounds behave the same way in Word, so extend the behavior of
    the compat flag to cover not only the background of text frames but also
    of cell frames.
    
    (The test could be hacked into CppunitTest_sw_uiwriter, but 1) this is
    layout, not UI and 2) that one started to get rather large.)
    
    (cherry picked from commit 09954c381895f340c9b73af64b0833d8b4689c06)
    
    Change-Id: Ifca1851217000f6daac50e6c9562c042aca6ff7d

--- sw/source/core/layout/paintfrm.cxx
+++ sw/source/core/layout/paintfrm.cxx
@@ -6557,7 +6557,9 @@ void SwFrame::PaintBackground( const SwRect &rRect, const SwPageFrame *pPage,
     if( IsTextFrame() || IsSctFrame() )
         aPaintRect = UnionFrame( true );
 
-    if ( (!bOnlyTextBackground || IsTextFrame()) && aPaintRect.IsOver( rRect ) )
+    // bOnlyTextBackground means background that's on top of background shapes,
+    // this includes both text and cell frames.
+    if ( (!bOnlyTextBackground || IsTextFrame() || IsCellFrame()) && aPaintRect.IsOver( rRect ) )
     {
         if ( bBack || bPageFrame || !bLowerMode )
         {
