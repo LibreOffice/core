948a42999a769a16c6b88b6ea673c8df3254e7fd
Chris Sherlock <chris.sherlock79@gmail.com>
2018-04-07 00:36:54 +1000

    vcl: maBitmapSize = aSize to SetPixelSize(aSize)
    Change-Id: Ic30b3bbb38756e880ab7bf4c76d94e5f4c787988

--- vcl/source/gdi/impgraph.cxx
+++ vcl/source/gdi/impgraph.cxx
@@ -1038,7 +1038,7 @@ bool ImpGraphic::ImplReadEmbedded( SvStream& rIStm )
                 maEx = maVectorGraphicData->getReplacement();
             }
 
-            maEx.maBitmapSize = aSize;
+            maEx.SetPixelSize(aSize);
 
             if( aMapMode != MapMode() )
             {
