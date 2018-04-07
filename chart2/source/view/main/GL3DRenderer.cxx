2a9f7b65450a8444b9e52c60550cfcf248828e1f
Noel Grandin <noelgrandin@gmail.com>
2018-04-07 08:45:51 +0200

    comment out some debug code
    left over from
        commit f9f8d930a17eb14365f914425c35df5a78f648ca
        Date:   Wed May 28 01:18:10 2014 +0200
        enable picking again
    
    Change-Id: Icce19c247512d61d7c255940fb5115e30c04e045

--- chart2/source/view/main/GL3DRenderer.cxx
+++ chart2/source/view/main/GL3DRenderer.cxx
@@ -2235,9 +2235,8 @@ void OpenGL3DRenderer::SetPickingMode(bool bPickingMode)
 
 Color OpenGL3DRenderer::GetPixelColorFromPoint(long nX, long nY)
 {
-    static sal_uInt32 nId = 0;
-    OUString aFileName = "/home/moggi/work/picking_" + OUString::number(nId++) + ".png";
-    OpenGLHelper::renderToFile(m_iWidth, m_iHeight, aFileName);
+//    OUString aFileName = "/home/moggi/work/picking_" + OUString::number(nId++) + ".png";
+//    OpenGLHelper::renderToFile(m_iWidth, m_iHeight, aFileName);
     std::unique_ptr<sal_uInt8[]> buf(new sal_uInt8[4]);
     glReadPixels(nX, m_iHeight-nY, 1, 1, GL_BGRA, GL_UNSIGNED_BYTE, buf.get());
     Color aColor(255-buf[3], buf[2], buf[1], buf[0]);
