c0e285d840373a26cee98deaccb521c1f8f1efdb
Miklos Vajna <vmiklos@collabora.co.uk>
2018-04-10 13:59:16 +0200

    dbaccess: fix --without-java build
    Change-Id: I9c1ac64745301deda1501806534f13c1527a33b3
    Reviewed-on: https://gerrit.libreoffice.org/52670
    Reviewed-by: Heiko Tietze <tietze.heiko@gmail.com>
    Tested-by: Heiko Tietze <tietze.heiko@gmail.com>
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>

--- dbaccess/CppunitTest_dbaccess_embeddeddb_performancetest.mk
+++ dbaccess/CppunitTest_dbaccess_embeddeddb_performancetest.mk
@@ -22,7 +22,6 @@ $(eval $(call gb_CppunitTest_use_libraries,dbaccess_embeddeddb_performancetest, 
     dbaxml \
     dbtools \
     firebird_sdbc \
-    jvmfwk \
     sal \
     subsequenttest \
     utl \
@@ -48,7 +47,6 @@ $(eval $(call gb_CppunitTest_use_components,dbaccess_embeddeddb_performancetest,
     configmgr/source/configmgr \
     connectivity/source/cpool/dbpool2 \
     connectivity/source/drivers/firebird/firebird_sdbc \
-    connectivity/source/drivers/jdbc/jdbc \
     connectivity/source/manager/sdbc2 \
     dbaccess/util/dba \
     dbaccess/source/filter/xml/dbaxml \
