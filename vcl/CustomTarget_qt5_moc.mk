783cecd82779abb141a04bc59896555ae8631062
Katarina Behrens <Katarina.Behrens@cib.de>
2018-04-10 12:40:38 +0200

    Move qt5 headers into vcl top-level include
    they will be used by kde5 vcl plugin
    
    Change-Id: Ibcece683802c7a448b165da4c2515408d641927c

--- vcl/CustomTarget_qt5_moc.mk
+++ vcl/CustomTarget_qt5_moc.mk
@@ -16,7 +16,7 @@ $(call gb_CustomTarget_get_target,vcl/qt5) : \
 	$(call gb_CustomTarget_get_workdir,vcl/qt5)/Qt5Widget.moc \
 
 $(call gb_CustomTarget_get_workdir,vcl/qt5)/%.moc : \
-		$(SRCDIR)/vcl/qt5/%.hxx \
+		$(SRCDIR)/vcl/inc/qt5/%.hxx \
 		| $(call gb_CustomTarget_get_workdir,vcl/qt5)/.dir
 	$(call gb_Output_announce,$(subst $(WORKDIR)/,,$@),$(true),MOC,1)
 	$(MOC5) $< -o $@
