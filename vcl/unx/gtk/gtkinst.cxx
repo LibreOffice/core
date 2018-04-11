28f2941680f850e88228e45699ac612ce036cf16
Caolán McNamara <caolanm@redhat.com>
2018-04-11 11:06:37 +0100

    Related: rhbz#1396729 use cairo_surface_create_similar
    where we can
    
    Change-Id: If6fd729a9cbf834faef33586b5bd886aad2fbe1d
    Reviewed-on: https://gerrit.libreoffice.org/52726
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>
    (cherry picked from commit b524de950c6eb0bc61d05d41fe69b67ab59b16c6)
    
    cairo_surface_create_similar_image is >= cairo 1.12.0
    
    (cherry picked from commit 2ca4b505b25e13c9f422c28252f5b7533b8e3270)
    
    Change-Id: I1805e5680beff6c632016686aa661efe25a8c2f8
    Reviewed-on: https://gerrit.libreoffice.org/53021
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>

--- vcl/unx/gtk/gtkinst.cxx
+++ vcl/unx/gtk/gtkinst.cxx
@@ -336,7 +336,7 @@ SalVirtualDevice* GtkInstance::CreateVirtualDevice( SalGraphics *pG,
     (void) pGd;
     SvpSalGraphics *pSvpSalGraphics = dynamic_cast<SvpSalGraphics*>(pG);
     assert(pSvpSalGraphics);
-    SvpSalVirtualDevice* pNew = new SvpSalVirtualDevice(eFormat, pSvpSalGraphics->getScale());
+    SvpSalVirtualDevice* pNew = new SvpSalVirtualDevice(eFormat, pSvpSalGraphics->getSurface());
     pNew->SetSize( nDX, nDY );
     return pNew;
 #else
