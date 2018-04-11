adc10e62ddc4a65f6b1fecbcfc7c6560489e3aaa
Miklos Vajna <vmiklos@collabora.co.uk>
2018-04-11 11:51:12 +0200

    sw XHTML export: fix missing namespace prefix of lists
    Unordered lists, but fixes ordered lists as a side effect, too.
    
    Change-Id: I1752522e906975b112e6938ffc5456e53a165996
    Reviewed-on: https://gerrit.libreoffice.org/52724
    Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
    Tested-by: Jenkins <ci@libreoffice.org>

--- sw/source/filter/html/htmlnumwriter.cxx
+++ sw/source/filter/html/htmlnumwriter.cxx
@@ -182,6 +182,7 @@ Writer& OutHTML_NumBulListStart( SwHTMLWriter& rWrt,
 
         rWrt.m_aBulletGrfs[i].clear();
         OString sOut = "<";
+        sOut += rWrt.GetNamespace();
         const SwNumFormat& rNumFormat = rInfo.GetNumRule()->Get( i );
         sal_Int16 eType = rNumFormat.GetNumberingType();
         if( SVX_NUM_CHAR_SPECIAL == eType )
