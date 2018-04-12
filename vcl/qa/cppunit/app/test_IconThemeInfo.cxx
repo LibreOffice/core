dc842f099cb3d6b3f547d57861ae999833a06535
heiko tietze <tietze.heiko@gmail.com>
2018-04-12 12:20:17 +0200

    tdf#75398 - Replace hicontrast by Sifr
    Change-Id: Ib9ba1e2203766f3afd3147d823e564b15d14dc65

--- vcl/qa/cppunit/app/test_IconThemeInfo.cxx
+++ vcl/qa/cppunit/app/test_IconThemeInfo.cxx
@@ -36,12 +36,6 @@ class IconThemeInfoTest : public CppUnit::TestFixture
     ThemeIdIsDetectedFromFileNameWithUnderscore();
 
     void
-    DisplayNameForHicontrastIsHighContrast();
-
-    void
-    DisplayNameForTango_testingIsTangoTesting();
-
-    void
     ExceptionIsThrownWhenIdCannotBeDetermined1();
 
     void
@@ -53,8 +47,6 @@ class IconThemeInfoTest : public CppUnit::TestFixture
     CPPUNIT_TEST(ThemeIdIsDetectedFromFileNameWithUnderscore);
     CPPUNIT_TEST(ImagesZipIsNotValid);
     CPPUNIT_TEST(ImagesOxygenZipIsValid);
-    CPPUNIT_TEST(DisplayNameForHicontrastIsHighContrast);
-    CPPUNIT_TEST(DisplayNameForTango_testingIsTangoTesting);
     CPPUNIT_TEST(ExceptionIsThrownWhenIdCannotBeDetermined1);
     CPPUNIT_TEST(ExceptionIsThrownWhenIdCannotBeDetermined2);
 
@@ -119,24 +111,6 @@ IconThemeInfoTest::ExceptionIsThrownWhenIdCannotBeDetermined2()
         thrown = true;
     }
     CPPUNIT_ASSERT_EQUAL_MESSAGE("Exception was thrown", true, thrown);
-}
-
-void
-IconThemeInfoTest::DisplayNameForHicontrastIsHighContrast()
-{
-    OUString const id("hicontrast");
-    OUString const expected("High Contrast");
-    OUString displayName = vcl::IconThemeInfo::ThemeIdToDisplayName(id);
-    CPPUNIT_ASSERT_EQUAL(expected, displayName);
-}
-
-void
-IconThemeInfoTest::DisplayNameForTango_testingIsTangoTesting()
-{
-    OUString const id("tango_testing");
-    OUString const expected("Tango Testing");
-    OUString displayName = vcl::IconThemeInfo::ThemeIdToDisplayName(id);
-    CPPUNIT_ASSERT_EQUAL(expected, displayName);
 }
 
 // Put the test suite in the registry
