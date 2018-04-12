0ebf27da5953c4cf965ce9354541702dafe0c021
heiko tietze <tietze.heiko@gmail.com>
2018-04-12 18:02:46 +0200

    Remove tango_testing from repository
    Abandonned project
    
    Change-Id: I72a80658aa868ca445c4122c32d0c744b340c828

--- vcl/source/app/IconThemeInfo.cxx
+++ vcl/source/app/IconThemeInfo.cxx
@@ -21,8 +21,6 @@ const OUStringLiteral vcl::IconThemeInfo::HIGH_CONTRAST_ID("hicontrast");
 namespace {
 
 static const OUStringLiteral HIGH_CONTRAST_DISPLAY_NAME("High Contrast");
-static const OUStringLiteral TANGO_TESTING_ID("tango_testing");
-static const OUStringLiteral TANGO_TESTING_DISPLAY_NAME("Tango Testing");
 static const OUStringLiteral BREEZE_DARK_ID("breeze_dark");
 static const OUStringLiteral BREEZE_DARK_DISPLAY_NAME("Breeze Dark");
 static const OUStringLiteral SIFR_DARK_ID("sifr_dark");
@@ -130,9 +128,6 @@ IconThemeInfo::ThemeIdToDisplayName(const OUString& themeId)
     // special cases
     if (themeId.equalsIgnoreAsciiCase(HIGH_CONTRAST_ID)) {
         return HIGH_CONTRAST_DISPLAY_NAME;
-    }
-    else if (themeId.equalsIgnoreAsciiCase(TANGO_TESTING_ID)) {
-        return TANGO_TESTING_DISPLAY_NAME;
     }
     else if (themeId.equalsIgnoreAsciiCase(BREEZE_DARK_ID)) {
         return BREEZE_DARK_DISPLAY_NAME;
