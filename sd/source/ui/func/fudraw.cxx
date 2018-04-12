f57830da7a50330327dd87c363a1ad81227b2004
Vasily Melenchuk <Vasily.Melenchuk@cib.de>
2018-04-12 18:06:54 +0300

    sd: hyperlink hint was not always shown
    Change-Id: Ic885688ce0f757c3230a5e355a4eef8da71dbda1

--- sd/source/ui/func/fudraw.cxx
+++ sd/source/ui/func/fudraw.cxx
@@ -791,6 +791,35 @@ bool FuDraw::SetHelpText(SdrObject* pObj, const Point& rPosPixel, const SdrViewE
             }
         }
     }
+    else if (rVEvt.pURLField)
+    {
+        /**************************************************************
+        * URL-Field
+        **************************************************************/
+        OUString aURL = INetURLObject::decode(rVEvt.pURLField->GetURL(), INetURLObject::DecodeMechanism::WithCharset);
+
+        SvtSecurityOptions aSecOpt;
+        if (aSecOpt.IsOptionSet(SvtSecurityOptions::EOption::CtrlClickHyperlink))
+        {
+            // Hint about Ctrl-click to open hyperlink, but need to detect "Ctrl" key for MacOs
+            vcl::KeyCode aCode(KEY_SPACE);
+            vcl::KeyCode aModifiedCode(KEY_SPACE, KEY_MOD1);
+            OUString aModStr(aModifiedCode.GetName());
+            aModStr = aModStr.replaceFirst(aCode.GetName(), "");
+            aModStr = aModStr.replaceAll("+", "");
+
+            OUString aCtrlClickHlinkStr = SdResId(STR_CTRLCLICKHYPERLINK);
+
+            aCtrlClickHlinkStr = aCtrlClickHlinkStr.replaceAll("%s", aModStr);
+
+            aHelpText = aCtrlClickHlinkStr + aURL;
+        }
+        else
+        {
+            // Hint about just clicking hyperlink
+            aHelpText = SdResId(STR_CLICKHYPERLINK) + aURL;
+        }
+    }
     else if (dynamic_cast< GraphicDocShell *>( mpDocSh ) ==  nullptr && SdDrawDocument::GetAnimationInfo(pObj))
     {
         SdAnimationInfo* pInfo = SdDrawDocument::GetAnimationInfo(pObj);
@@ -896,36 +925,6 @@ bool FuDraw::SetHelpText(SdrObject* pObj, const Point& rPosPixel, const SdrViewE
             break;
             default:
                 break;
-        }
-    }
-    else if (rVEvt.pURLField)
-    {
-        /**************************************************************
-        * URL-Field
-        **************************************************************/
-        OUString aURL = INetURLObject::decode( rVEvt.pURLField->GetURL(), INetURLObject::DecodeMechanism::WithCharset );
-
-        SvtSecurityOptions aSecOpt;
-        if (aSecOpt.IsOptionSet(SvtSecurityOptions::EOption::CtrlClickHyperlink))
-        {
-            // Hint about Ctrl-click to open hyperlink
-            // But need to detect "Ctrl" key for MacOs
-            vcl::KeyCode aCode(KEY_SPACE);
-            vcl::KeyCode aModifiedCode(KEY_SPACE, KEY_MOD1);
-            OUString aModStr(aModifiedCode.GetName());
-            aModStr = aModStr.replaceFirst(aCode.GetName(), "");
-            aModStr = aModStr.replaceAll("+", "");
-
-            OUString aCtrlClickHlinkStr = SdResId(STR_CTRLCLICKHYPERLINK);
-
-            aCtrlClickHlinkStr = aCtrlClickHlinkStr.replaceAll("%s", aModStr);
-
-            aHelpText = aCtrlClickHlinkStr + aURL;
-        }
-        else
-        {
-            // Hint about just clicking hyperlink
-            aHelpText = SdResId(STR_CLICKHYPERLINK) + aURL;
         }
     }
 
