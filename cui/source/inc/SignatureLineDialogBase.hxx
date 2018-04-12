e7e5617d3e76841e8b14a171eaf575217f5a4498
Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
2018-04-12 17:29:56 +0200

    tdf#83877 Signatureline dialog: Move shared stuff to common base
    Change-Id: I0af888f7b8cd3b7d58665a4b0b1ef18a7f9e98c2

---
+++ cui/source/inc/SignatureLineDialogBase.hxx
@@ -0,0 +1,33 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+#ifndef INCLUDED_CUI_INC_SIGNATURELINEDIALOGBASE_HXX
+#define INCLUDED_CUI_INC_SIGNATURELINEDIALOGBASE_HXX
+
+#include <vcl/weld.hxx>
+
+#include <com/sun/star/frame/XModel.hpp>
+#include <com/sun/star/uno/Reference.hxx>
+
+class SignatureLineDialogBase : public weld::GenericDialogController
+{
+public:
+    SignatureLineDialogBase(weld::Widget* pParent, css::uno::Reference<css::frame::XModel> xModel,
+                            const OUString& rUIFile, const OString& rDialogId);
+
+    short execute();
+
+protected:
+    css::uno::Reference<css::frame::XModel> m_xModel;
+    OUString getSignatureImage();
+    virtual void Apply() = 0;
+};
+
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
