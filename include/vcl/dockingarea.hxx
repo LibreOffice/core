cb1756e8a51f43ab8657bf8dada7c42150cc6fd1
Noel Grandin <noel.grandin@collabora.co.uk>
2018-04-13 09:59:38 +0200

    loplugin:useuniqueptr in DockingAreaWindow
    Change-Id: I20fb393cfe4a129bc1007d00df5671c784ae85e4

--- include/vcl/dockingarea.hxx
+++ include/vcl/dockingarea.hxx
@@ -21,6 +21,7 @@
 #define INCLUDED_VCL_DOCKINGAREA_HXX
 
 #include <vcl/window.hxx>
+#include <memory>
 
 //  A simple container for docked toolbars
 //  - its main purpose is theming support
@@ -29,7 +30,7 @@ class VCL_DLLPUBLIC DockingAreaWindow : public vcl::Window
     class ImplData;
 
 private:
-    ImplData*       mpImplData;
+    std::unique_ptr<ImplData> mpImplData;
 
                     DockingAreaWindow (const DockingAreaWindow &) = delete;
                     DockingAreaWindow & operator= (const DockingAreaWindow &) = delete;
