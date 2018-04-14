34ffe14e035f3b2a0519b27bc4798eff1ad358e4
Tamas Bunth <tamas.bunth@collabora.co.uk>
2018-04-14 19:46:43 +0200

    tdf#116982 dbahsql: fix autoincrementation
    Fix autoincrementation issue by parsing and processing statements like
    "ALTER TABLE <name> ALTER COLUMN <col_name> RESTART WITH <num>"
    
    Change-Id: Ib175d64343d05d4b2cfc9a4cf944d0d0478adbcc

---
+++ dbaccess/source/filter/hsqldb/alterparser.cxx
@@ -0,0 +1,48 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#include <comphelper/string.hxx>
+#include "alterparser.hxx"
+#include "utils.hxx"
+
+namespace dbahsql
+{
+void AlterStmtParser::parse(const OUString& sSql)
+{
+    if (!sSql.startsWith("ALTER"))
+    {
+        SAL_WARN("dbaccess", "Not an ALTER statement");
+        return;
+    }
+
+    m_sTableName = utils::getTableNameFromStmt(sSql);
+    auto words = comphelper::string::split(sSql, sal_Unicode(u' '));
+
+    if (words[3] == "ALTER" && words[4] == "COLUMN")
+        m_sColumnName = words[5];
+    if (words[6] == "RESTART" && words[7] == "WITH")
+    {
+        m_eAction = AlterAction::IDENTITY_RESTART;
+        m_nIdentityParam = words[8].toInt32();
+    }
+}
+
+} // namespace dbahsql
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
---
+++ dbaccess/source/filter/hsqldb/fbalterparser.hxx
@@ -0,0 +1,30 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef INCLUDED_DBACCESS_SOURCE_FILTER_HSQLDB_FBALTERPARSER_HXX
+#define INCLUDED_DBACCESS_SOURCE_FILTER_HSQLDB_FBALTERPARSER_HXX
+
+#include "alterparser.hxx"
+
+namespace dbahsql
+{
+class SAL_DLLPUBLIC_EXPORT FbAlterStmtParser : public AlterStmtParser
+{
+public:
+    /**
+     * Compose the result of the parser to statements of Firebird dialect
+     */
+    virtual OUString compose() const override;
+};
+
+} // dbahsql
+
+#endif // INCLUDED_DBACCESS_SOURCE_FILTER_HSQLDB_FBALTERPARSER_HXX
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
