ef54107dfa0af8494d7bc7335213171dded67fd3
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2018-04-14 11:40:18 -0400

    svx: correct the positioning of PDF Paths and the stroke width
    Change-Id: I5b150721cc1b61b028f282062c1466ef6a67fcae
    (cherry picked from commit 37a40d8025ac70f84937de2b1d0f596d08088cbd)

--- external/pdfium/UnpackedTarball_pdfium.mk
+++ external/pdfium/UnpackedTarball_pdfium.mk
@@ -21,6 +21,7 @@ pdfium_patches += 0004-svx-support-PDF-text-color.patch.2
 pdfium_patches += 0005-svx-support-Paths-in-PDFs-while-importing.patch.2
 pdfium_patches += 0006-svx-improve-path-importing-from-PDF.patch.2
 pdfium_patches += 0007-svx-improved-text-importing-from-PDF.patch.2
+pdfium_patches += 0008-svx-correct-the-positioning-of-PDF-Paths-and-the-str.patch.2
 
 $(eval $(call gb_UnpackedTarball_UnpackedTarball,pdfium))
 
