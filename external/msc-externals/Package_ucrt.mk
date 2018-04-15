8fb7fb51d79d02230602f2b47356978869d4edf1
Mike Kaganski <mike.kaganski@collabora.com>
2018-04-15 23:24:42 +0300

    Install UCRT from MSUs, not using nested VC Redist install
    Using nested install is bad because (1) MS advises against it (though it
    most possibly doesn't relate to our specific case, when we install the
    vc redist exe package in UI part, so actually only a single MSI session
    is active at any time); (2) because it adds some extra interactions
    (user sees something "unrelated" being installed, which raises concerns;
    additional admin authentication required); and (3) because it runs in
    InstallUISequence, thus only installing the UCRT when doing interactive
    installation (unattended installs, including GPO, need to install UCRT
    separately).
    
    This patch aims to incorporate the original UCRT MSU (Windows Update)
    packages (https://support.microsoft.com/en-us/help/2999226) available as
    a zip archive from
    https://www.microsoft.com/en-us/download/details.aspx?id=48234
    - the same as used in VC redists for VS 2015 and 2017. This obsoletes
    the separate installation of the redist; since we also have the redist
    as merge module in our MSI, that is enough (and removes redundancy).
    The MSUs are installed using wusa.exe in a custom action (deferred,
    non-impersonating).
    
    As a small bonus, embedding MSUs instead of redist EXE allows us to
    shrink the size of installer a little (~10 MB).
    
    As deferred custom actions cannot access current installer database,
    we workaround this by using initial immediate impersonating action to
    extract the binaries into a temporary location. To ensure that the file
    gets removed upon completion (both successful and failed), we use an
    additional cleanup action.
    
    Commit 61b1d631331551b43bc7d619be33bfbfeff7cad6 is effectively reverted.
    
    This commit also includes changes from commits from master:
    8faa1bc61fa8f09365d483364aea2b1c2751b587
    1f8a3657216e44796cb94087450552aa977ebdae
    378c1576d2890625ebbd18ec9ccff560eeb1619f
    
    Change-Id: I1529356fdcc67ff24b232c01ddf8bb3a31bb00bd

--- external/msc-externals/Package_ucrt.mk
+++ external/msc-externals/Package_ucrt.mk
@@ -7,10 +7,15 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 
-$(eval $(call gb_Package_Package,vcredist_exe,$(VCREDIST_DIR)))
+$(eval $(call gb_Package_Package,ucrt,$(UCRT_REDISTDIR)))
 
-$(eval $(call gb_Package_add_files,vcredist_exe,$(LIBO_ETC_FOLDER),\
-	$(VCREDIST_EXE) \
+$(eval $(call gb_Package_add_files,ucrt,$(LIBO_ETC_FOLDER),\
+    Windows6.1-KB2999226-x64.msu \
+    Windows6.1-KB2999226-x86.msu \
+    Windows8.1-KB2999226-x64.msu \
+    Windows8.1-KB2999226-x86.msu \
+    Windows8-RT-KB2999226-x64.msu \
+    Windows8-RT-KB2999226-x86.msu \
 ))
 
 # vim:set shiftwidth=4 tabstop=4 noexpandtab:
