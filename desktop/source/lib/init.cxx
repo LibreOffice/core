bf1acecf4f7ffd21c6ff3d0534c35811cdf753d2
Muhammet Kara <muhammet.kara@pardus.org.tr>
2018-04-15 12:33:03 +0300

    cppcheck: variableScope & unreadVariable
    Change-Id: Iaa3adc54d547e243b977a562fa4dbc2b9b9c6592

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -2841,7 +2841,6 @@ static char* getStyles(LibreOfficeKitDocument* pThis, const char* pCommand)
     // Header & Footer Styles
     {
         OUString sName;
-        bool bIsPhysical;
         boost::property_tree::ptree aChild;
         boost::property_tree::ptree aChildren;
         const OUString sPageStyles("PageStyles");
@@ -2850,6 +2849,7 @@ static char* getStyles(LibreOfficeKitDocument* pThis, const char* pCommand)
 
         if (xStyleFamilies->hasByName(sPageStyles) && (xStyleFamilies->getByName(sPageStyles) >>= xContainer))
         {
+            bool bIsPhysical;
             uno::Sequence<OUString> aSeqNames = xContainer->getElementNames();
             for (sal_Int32 itName = 0; itName < aSeqNames.getLength(); itName++)
             {
