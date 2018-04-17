5d5a936e720e30dc25db87f0b7386a18a20c438b
Mike Kaganski <mike.kaganski@collabora.com>
2018-04-18 00:42:17 +0300

    Windows: Test if a filepath redirects to a WebDAV resource
    In Windows, filesystem redirectors can map WebDAV resources to UNC
    paths, or to drive-based "local" paths; so a WebDAV URI of the form
    "http://WebDADServer/root/directory/File.ext" may be accessed using
    "\\WebDADServer\root\directory\File.ext" or "Z:\directory\File.ext".
    
    When using these paths, failure to create a lockfile aside the opened
    document should not be considered an error; so this patch checks for
    this.
    
    Change-Id: I1de55b66447dc91d22b6d2b5b121de96bf32e4ee

---
+++ include/tools/fileutil.hxx
@@ -0,0 +1,24 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef INCLUDED_TOOLS_FILEUTIL_HXX
+#define INCLUDED_TOOLS_FILEUTIL_HXX
+
+#include <tools/toolsdllapi.h>
+
+class INetURLObject;
+
+namespace tools
+{
+TOOLS_DLLPUBLIC bool IsMappedWebDAVPath(const INetURLObject& aURL);
+}
+
+#endif // INCLUDED_TOOLS_FILEUTIL_HXX
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
