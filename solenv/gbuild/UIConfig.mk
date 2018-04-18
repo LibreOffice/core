c24c2d1e41b8123d205f69415a4f934b365408a7
Samuel Thibault <sthibault@hypra.fr>
2018-04-18 16:26:02 +0200

    gla11y: Add dependency on .ui files
    During the gla11y work, the dependency of UIA rules on the .ui files
    got lost, this adds it so that modifying a .ui file triggers checking
    accessibility with gla11y.
    
    Change-Id: I62103ca1783c2f532d7647384c1add1b557b37a1
    Reviewed-on: https://gerrit.libreoffice.org/53116
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>
    Tested-by: Jenkins <ci@libreoffice.org>

--- solenv/gbuild/UIConfig.mk
+++ solenv/gbuild/UIConfig.mk
@@ -258,6 +258,7 @@ $(call gb_UIConfig_get_imagelist_target,$(1)) : $(call gb_UIImageListTarget_get_
 $(call gb_UIConfig_get_clean_target,$(1)) : $(call gb_UIImageListTarget_get_clean_target,$(2))
 
 $(call gb_UIConfig_get_a11yerrors_target,$(1)) : UIFILES += $(SRCDIR)/$(2).ui
+$(call gb_UIConfig_get_a11yerrors_target,$(1)) : $(SRCDIR)/$(2).ui
 
 endef
 
