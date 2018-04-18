ed4c090502ed7e3a44cf18f4fb899bf6ce37ab15
Chris Sherlock <chris.sherlock79@gmail.com>
2018-04-19 07:02:34 +1000

    vcl: ImplMedianFilter() -> BitmapMedianFilter
    Change-Id: I72a0546c11d6ef8a8a4eb467d566d639c88dc8b9

---
+++ include/vcl/BitmapMedianFilter.hxx
@@ -0,0 +1,28 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#ifndef INCLUDED_VCL_BITMAPMEDIANFILTER_HXX
+#define INCLUDED_VCL_BITMAPMEDIANFILTER_HXX
+
+#include <vcl/BitmapFilter.hxx>
+
+class BitmapEx;
+
+class VCL_DLLPUBLIC BitmapMedianFilter : public BitmapFilter
+{
+public:
+    BitmapMedianFilter() {}
+
+    virtual BitmapEx execute(BitmapEx const& rBitmapEx) override;
+};
+
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
