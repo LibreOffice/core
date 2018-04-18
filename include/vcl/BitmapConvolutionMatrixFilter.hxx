a4e28e727f06f3e11cc0798a2edb71cea9fae4f5
Chris Sherlock <chris.sherlock79@gmail.com>
2018-04-18 21:06:15 +1000

    vcl: ImplConvolute3() -> BitmapConvolutionMatrixFilter
    Change-Id: I0203e98d29192ef098719c0a297b967710b8729a

---
+++ include/vcl/BitmapConvolutionMatrixFilter.hxx
@@ -0,0 +1,38 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#ifndef INCLUDED_VCL_BITMAPCONVOLUTIONMATRIXFILTER_HXX
+#define INCLUDED_VCL_BITMAPCONVOLUTIONMATRIXFILTER_HXX
+
+#include <vcl/BitmapFilter.hxx>
+
+class BitmapEx;
+
+/** Filter image based on a 3x3 convolution matrix
+ */
+class VCL_DLLPUBLIC BitmapConvolutionMatrixFilter : public BitmapFilter
+{
+public:
+    BitmapConvolutionMatrixFilter(const long* pMatrix)
+        : mpMatrix(pMatrix)
+    {
+    }
+
+    ~BitmapConvolutionMatrixFilter() override { delete mpMatrix; }
+
+    virtual BitmapEx execute(BitmapEx const& rBitmapEx) override;
+
+private:
+    const long* mpMatrix;
+};
+
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
