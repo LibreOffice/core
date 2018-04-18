9de8bb5d61b0873c7bddf98d701a9f7e6c455cd4
Chris Sherlock <chris.sherlock79@gmail.com>
2018-04-18 21:19:58 +1000

    vcl: create BitmapSmoothenFilter
    Change-Id: I5259035e18daada3cd8963f045731a0a65150718

---
+++ vcl/source/bitmap/BitmapSmoothenFilter.cxx
@@ -0,0 +1,36 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#include <vcl/bitmap.hxx>
+#include <vcl/bitmapex.hxx>
+#include <vcl/bitmapaccess.hxx>
+#include <vcl/BitmapGaussianSeparableBlurFilter.hxx>
+#include <vcl/BitmapSeparableUnsharpenFilter.hxx>
+#include <vcl/BitmapSmoothenFilter.hxx>
+
+#include <bitmapwriteaccess.hxx>
+
+BitmapEx BitmapSmoothenFilter::execute(BitmapEx const& rBitmapEx)
+{
+    BitmapEx aBitmapEx(rBitmapEx);
+    bool bRet = false;
+
+    if (mfRadius > 0.0) // Blur for positive values of mnRadius
+        bRet = BitmapFilter::Filter(aBitmapEx, BitmapGaussianSeparableBlurFilter(mfRadius));
+    else if (mfRadius < 0.0) // Unsharpen mask for negative values of mnRadius
+        bRet = BitmapFilter::Filter(aBitmapEx, BitmapSeparableUnsharpenFilter(mfRadius));
+
+    if (bRet)
+        return BitmapEx(rBitmapEx);
+
+    return BitmapEx();
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
