2241c1392af8c889193910065fb991e4ecf4d790
Stephan Bergmann <sbergman@redhat.com>
2018-04-19 13:59:16 +0200

    tdf#95843: Wait for fire_glxtest_process also in --headless mode
    Discussed with mmeeks on IRC that fire_glxtest_process is probably called as
    early as possible so that its reuslt is ready by the time it is needed in the
    non-headless case.  So best fix for headless is probably to just wait for the
    sub-process at an opportune point, instead of redesigning the whole mess so that
    fire_glxtest_process would only be called once its result is actually needed.
    
    Change-Id: I4ea9c9d54b83c9695a3b72317e68fed0c410da0e

--- desktop/inc/app.hxx
+++ desktop/inc/app.hxx
@@ -183,6 +183,7 @@ OUString ReplaceStringHookProc(const OUString& rStr);
 
 #if defined( UNX ) && !defined MACOSX && !defined IOS && !defined ANDROID && !defined LIBO_HEADLESS
 bool fire_glxtest_process();
+void reap_glxtest_process();
 #endif
 
 #endif // INCLUDED_DESKTOP_INC_APP_HXX
