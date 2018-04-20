d27659d31abd934051390a58ba5678c7bbfc96fc
Chris Sherlock <chris.sherlock79@gmail.com>
2018-04-20 21:00:46 +1000

    vcl: ImplEmbossGrey() -> BitmapEmbossGreyFilter
    Change-Id: I6e541e9ca9cf61dfa8df9638a4ba4b8bd1d3ad71

---
+++ include/vcl/BitmapEmbossGreyFilter.hxx
@@ -0,0 +1,36 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#ifndef INCLUDED_VCL_BITMAPEMBOSSGREYFILTER_HXX
+#define INCLUDED_VCL_BITMAPEMBOSSGREYFILTER_HXX
+
+#include <vcl/BitmapFilter.hxx>
+
+class BitmapEx;
+
+class VCL_DLLPUBLIC BitmapEmbossGreyFilter : public BitmapFilter
+{
+public:
+    BitmapEmbossGreyFilter(sal_uInt16 nAzimuthAngle100, sal_uInt16 nElevationAngle100)
+        : mnAzimuthAngle100(nAzimuthAngle100)
+        , mnElevationAngle100(nElevationAngle100)
+    {
+    }
+
+    virtual BitmapEx execute(BitmapEx const& rBitmapEx) override;
+
+private:
+    sal_uInt16 mnAzimuthAngle100;
+    sal_uInt16 mnElevationAngle100;
+};
+
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
