f8bd9fc4cef5d4fb6eb232b390caae5c22fb7abd
Chris Sherlock <chris.sherlock79@gmail.com>
2018-04-20 20:32:23 +1000

    vcl: ImplSepia -> BitmapSepiaFilter
    Change-Id: I96a4072bf919bd37b30c01ab16d98779c76717ab

--- vcl/Library_vcl.mk
+++ vcl/Library_vcl.mk
@@ -314,6 +314,7 @@ $(eval $(call gb_Library_add_exception_objects,vcl,\
     vcl/source/bitmap/bitmapfilter \
     vcl/source/bitmap/BitmapSobelGreyFilter \
     vcl/source/bitmap/BitmapSolarizeFilter \
+    vcl/source/bitmap/BitmapSepiaFilter \
     vcl/source/bitmap/BitmapPopArtFilter \
     vcl/source/bitmap/BitmapConvolutionMatrixFilter \
     vcl/source/bitmap/BitmapMedianFilter \
