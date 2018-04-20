a0255968e1c1934ce124dbbb5d5dbd72cee44509
Chris Sherlock <chris.sherlock79@gmail.com>
2018-04-20 22:22:44 +1000

    vcl: get rid of Bitmap{Ex}::Filter() and Animation::Filter()
    Change-Id: I510d7b286df732712aa9206b0a7c7910af34c83f

--- vcl/source/gdi/bitmapex.cxx
+++ vcl/source/gdi/bitmapex.cxx
@@ -609,11 +609,6 @@ bool BitmapEx::Adjust( short nLuminancePercent, short nContrastPercent,
                                         fGamma, bInvert, msoBrightness );
 }
 
-bool BitmapEx::Filter( BmpFilter eFilter, const BmpFilterParam* pFilterParam )
-{
-    return !!maBitmap && maBitmap.Filter( eFilter, pFilterParam );
-}
-
 void BitmapEx::Draw( OutputDevice* pOutDev, const Point& rDestPt ) const
 {
     pOutDev->DrawBitmapEx( rDestPt, *this );
