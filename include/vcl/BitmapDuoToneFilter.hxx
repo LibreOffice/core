9e3e9a59850df5ddf6c60b6700cec5b8ebe7b834
Chris Sherlock <chris.sherlock79@gmail.com>
2018-04-20 21:13:55 +1000

    vcl: ImplDuoTone() -> BitmapDuoToneFilter
    Change-Id: If779cf4033948601997a932839eaa10a874de1b3

---
+++ include/vcl/BitmapDuoToneFilter.hxx
@@ -0,0 +1,36 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#ifndef INCLUDED_VCL_BITMAPDUOTONEFILTER_HXX
+#define INCLUDED_VCL_BITMAPDUOTONEFILTER_HXX
+
+#include <vcl/BitmapFilter.hxx>
+
+class BitmapEx;
+
+class VCL_DLLPUBLIC BitmapDuoToneFilter : public BitmapFilter
+{
+public:
+    BitmapDuoToneFilter(sal_uLong nColorOne, sal_uLong nColorTwo)
+        : mnColorOne(nColorOne)
+        , mnColorTwo(nColorTwo)
+    {
+    }
+
+    virtual BitmapEx execute(BitmapEx const& rBitmapEx) override;
+
+private:
+    sal_uLong mnColorOne;
+    sal_uLong mnColorTwo;
+};
+
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
--- vcl/Library_vcl.mk
+++ vcl/Library_vcl.mk
@@ -318,6 +318,7 @@ $(eval $(call gb_Library_add_exception_objects,vcl,\
     vcl/source/bitmap/BitmapMosaicFilter \
     vcl/source/bitmap/BitmapEmbossGreyFilter \
     vcl/source/bitmap/BitmapPopArtFilter \
+    vcl/source/bitmap/BitmapDuoToneFilter \
     vcl/source/bitmap/BitmapConvolutionMatrixFilter \
     vcl/source/bitmap/BitmapMedianFilter \
     vcl/source/bitmap/BitmapInterpolateScaleFilter \
