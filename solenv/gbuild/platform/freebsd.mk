00df62bded7abff051b3defcc56ad0731693c8cd
Don Lewis <truckman@apache.org>
2018-04-21 23:17:08 +0000

    Unbreak build on FreeBSD with clang 6.0 and newer.  Clang 6 changed
    the its default C++ standard from C++98 to C++14 and our old code
    is not prepared for that.  Avoid the problem by adding the -std=gnu++98
    compiler flag.
    
    Add the -fstack-protector compiler flag on FreeBSD INTEL and X86_64.

--- solenv/gbuild/platform/freebsd.mk
+++ solenv/gbuild/platform/freebsd.mk
@@ -92,6 +92,7 @@ gb_CXXFLAGS := \
 	-fno-use-cxa-atexit \
 	-fvisibility-inlines-hidden \
 	-fvisibility=hidden \
+	-std=gnu++98 \
 	-pipe
 ifeq ($(COM),CLANG)
 gb_CXXFLAGS += -DHAVE_STL_INCLUDE_PATH
@@ -140,6 +141,12 @@ gb_LinkTarget_LDFLAGS += \
 	-Wl,--dynamic-list-cpp-typeinfo \
 	-Wl,-Bsymbolic-functions \
 
+endif
+
+ifneq ($(filter $(CPUNAME),INTEL X86_64),)
+gb_CFLAGS += -fstack-protector
+gb_CXXFLAGS += -fstack-protector
+gb_LinkTarget_LDFLAGS += -fstack-protector
 endif
 
 ifeq ($(gb_DEBUGLEVEL),2)
--- solenv/inc/unxfbsd.mk
+++ solenv/inc/unxfbsd.mk
@@ -105,10 +105,15 @@ CFLAGSEXCEPTIONS=-fexceptions -fno-enforce-eh-specs
 CFLAGS_NO_EXCEPTIONS=-fno-exceptions
 
 # -fpermissive should be removed as soon as possible
-CFLAGSCXX= -pipe $(ARCH_FLAGS) 
+CFLAGSCXX= -pipe $(ARCH_FLAGS) -std=gnu++98
 .IF "$(HAVE_GCC_VISIBILITY_FEATURE)" == "TRUE"
 CFLAGSCXX += -fvisibility-inlines-hidden
 .ENDIF # "$(HAVE_GCC_VISIBILITY_FEATURE)" == "TRUE"
+
+.IF "$(CPUNAME)" == "INTEL" || "$(CPUNAME)" == "X86_64"
+CFLAGSCC += -fstack-protector
+CFLAGSCXX += -fstack-protector
+.ENDIF
 
 # Compiler flags for compiling static object in multi threaded environment with graphical user interface
 CFLAGSOBJGUIMT=
