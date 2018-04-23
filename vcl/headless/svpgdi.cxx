9c1f9ab9af9af0e292e2505cdca7559fee7fae5a
Katarina Behrens <Katarina.Behrens@cib.de>
2018-04-23 10:14:02 +0200

    drawBitmap that takes BitmapBuffer as input
    Change-Id: Icc0f63fac38b1476100d790a99065ad29d17bc6a

--- vcl/headless/svpgdi.cxx
+++ vcl/headless/svpgdi.cxx
@@ -1204,6 +1204,12 @@ void SvpSalGraphics::drawBitmap(const SalTwoRect& rTR, const SalBitmap& rSourceB
     copySource(rTR, source);
 }
 
+void SvpSalGraphics::drawBitmap(const SalTwoRect& rTR, BitmapBuffer* pBuffer)
+{
+    cairo_surface_t* source = createCairoSurface( pBuffer );
+    copySource(rTR, source);
+}
+
 void SvpSalGraphics::drawBitmap( const SalTwoRect& rTR,
                                  const SalBitmap& rSourceBitmap,
                                  const SalBitmap& rTransparentBitmap )
