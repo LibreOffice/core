aea5306d3ffa32c368ac73353dc9515c7e89ebe4
Andras Timar <andras.timar@collabora.com>
2018-04-23 16:41:51 +0200

    Optionally bundle even more Google Noto fonts
    Change-Id: I6c08476710ab541ff9b9407f5d874dbb038990df

---
+++ external/noto_font/ExternalPackage_notoserifsc.mk
@@ -0,0 +1,17 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_ExternalPackage_ExternalPackage,fonts_notoserifsc,font_notoserifsc))
+
+$(eval $(call gb_ExternalPackage_add_unpacked_files,fonts_notoserifsc,$(LIBO_SHARE_FOLDER)/fonts/truetype,\
+	NotoSerifSC-Bold.otf \
+	NotoSerifSC-Regular.otf \
+))
+
+# vim: set noet sw=4 ts=4:
