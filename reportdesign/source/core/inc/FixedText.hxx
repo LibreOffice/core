b4ba8dc9ef1635c75b363838b6016d3851387020
Noel Grandin <noel.grandin@collabora.co.uk>
2018-04-24 16:27:17 +0200

    tdf#117161 ReportBuilder horizontal align wrong when editing
    regression from
    	commit 5d0e485e827057eee9fb2c997685690b710e7f34
    	use actual UNO enums in reportdesign..svtools
    
    Also make some of the "template<> set" property helpers only fire on
    actual property change
    
    Change-Id: I930fd255d287c3c7e5b064823fd1e8d4b665eae0
    Reviewed-on: https://gerrit.libreoffice.org/53412
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

--- reportdesign/source/core/inc/FixedText.hxx
+++ reportdesign/source/core/inc/FixedText.hxx
@@ -51,6 +51,22 @@ namespace reportdesign
         OFixedText(const OFixedText&) = delete;
         OFixedText& operator=(const OFixedText&) = delete;
 
+        // internally, we store PROPERTY_PARAADJUST as css::style::ParagraphAdjust, but externally the property is visible as a sal_Int16
+        void set(  const OUString& _sProperty
+                  ,sal_Int16 Value
+                  ,css::style::ParagraphAdjust& _member)
+        {
+            BoundListeners l;
+            {
+                ::osl::MutexGuard aGuard(m_aMutex);
+                if ( static_cast<sal_Int16>(_member) != Value )
+                {
+                    prepareSet(_sProperty, css::uno::makeAny(static_cast<sal_Int16>(_member)), css::uno::makeAny(Value), &l);
+                    _member = static_cast<css::style::ParagraphAdjust>(Value);
+                }
+            }
+            l.notify();
+        }
         template <typename T> void set(  const OUString& _sProperty
                                         ,const T& Value
                                         ,T& _member)
