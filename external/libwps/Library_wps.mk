2e6d981434e2d2b4425211f3449b8c343a25621e
David Tardon <dtardon@redhat.com>
2018-04-25 07:02:54 +0200

    upload libwps 0.4.9
    Change-Id: I9571d91d6ce81d7e8752f24c3220f858a5303986

--- external/libwps/Library_wps.mk
+++ external/libwps/Library_wps.mk
@@ -39,6 +39,7 @@ $(eval $(call gb_Library_add_generated_exception_objects,wps,\
 	UnpackedTarball/libwps/src/lib/LotusSpreadsheet \
 	UnpackedTarball/libwps/src/lib/LotusStyleManager \
 	UnpackedTarball/libwps/src/lib/MSWrite \
+	UnpackedTarball/libwps/src/lib/Multiplan \
 	UnpackedTarball/libwps/src/lib/Quattro \
 	UnpackedTarball/libwps/src/lib/QuattroDos \
 	UnpackedTarball/libwps/src/lib/QuattroDosChart \
