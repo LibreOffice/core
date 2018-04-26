55a4cc54de25c56129370cd4182a14fc3a1987a6
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2018-04-26 22:04:27 +0900

    [API CHANGE] revert and deprecate *BackGraphicURL add *BackGraphic
    *BackGraphicURL include the following properties:
    - BackGraphicURL
    - FooterBackGraphicURL
    - HeaderBackGraphicURL
    - ParaBackGraphicURL
    
    This were removed, but for backwards compatibility this commit
    adds them back again and depreactes them in the UNO API. The
    behaviour also changes as internal vnd.sun.star.GraphicObject
    scheme URLs aren't supported so this properties can only be set
    and only if a external URL is provided. If getting such a property
    then a RuntimeException will be thrown.
    
    [ Miklos Vajna: fixed up sw/qa and writerfilter bits. ]
    
    Change-Id: If60011837da96197b576bfe2671ecafccad736c7

--- qadevOOo/tests/java/mod/_sw/ParagraphStyle.java
+++ qadevOOo/tests/java/mod/_sw/ParagraphStyle.java
@@ -119,7 +119,10 @@ public class ParagraphStyle extends TestCase  {
 
         XPropertySet xStyleProp = UnoRuntime.queryInterface(XPropertySet.class, oMyStyle);
         short exclude = PropertyAttribute.READONLY;
-        String[] names = utils.getFilteredPropertyNames(xStyleProp, (short)0, exclude);
+
+        String[] skipPropetiesNamed = { "ParaBackGraphicURL" };
+
+        String[] names = utils.getFilteredPropertyNames(xStyleProp, (short)0, exclude, skipPropetiesNamed);
         tEnv.addObjRelation("PropertyNames", names);
 
         return tEnv;
