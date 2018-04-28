2ab182ee5a37472efaa90cd5b36163adb08be76b
Mike Kaganski <mike.kaganski@collabora.com>
2018-04-28 09:16:15 +0300

    WinUserInfoBe: a new config backend for user data provided by Windows
    This backend provides Active Directory details for current user. If
    the system is not part of Active Directory domain, GetUserNameEx is
    used instead, so at least user (given) name is configured.
    
    Unlike ldap backend, this one doesn't need connection configuration,
    as the used API works with current user security context.
    
    Change-Id: I74bcc79591a658dc5121df1f09caa15d272fbe12

---
+++ extensions/Library_WinUserInfoBe.mk
@@ -0,0 +1,38 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+#
+
+$(eval $(call gb_Library_Library,WinUserInfoBe))
+
+$(eval $(call gb_Library_set_componentfile,WinUserInfoBe,extensions/source/config/WinUserInfo/WinUserInfoBe))
+
+$(eval $(call gb_Library_use_sdk_api,WinUserInfoBe))
+
+$(eval $(call gb_Library_add_exception_objects,WinUserInfoBe,\
+	extensions/source/config/WinUserInfo/componentdef \
+	extensions/source/config/WinUserInfo/WinUserInfoBe \
+))
+
+$(eval $(call gb_Library_use_libraries,WinUserInfoBe,\
+	comphelper \
+	cppuhelper \
+	cppu \
+	sal \
+))
+
+$(eval $(call gb_Library_use_system_win32_libs,WinUserInfoBe,\
+	Ole32 \
+	OleAut32 \
+	Activeds \
+	Adsiid \
+	Secur32 \
+))
+
+# vim:set noet sw=4 ts=4:
