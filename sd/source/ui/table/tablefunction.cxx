3a469fb03b40b4af7719a2c5f51b4c7f18b8beea
Caolán McNamara <caolanm@redhat.com>
2018-04-30 10:41:16 +0100

    weld SvxNewTableDialog
    Change-Id: I33189708a2f1b63080cbafc94fe288f8abe60830

--- sd/source/ui/table/tablefunction.cxx
+++ sd/source/ui/table/tablefunction.cxx
@@ -124,7 +124,7 @@ void DrawViewShell::FuTable(SfxRequest& rReq)
         if( (nColumns == 0) || (nRows == 0) )
         {
             SvxAbstractDialogFactory* pFact = SvxAbstractDialogFactory::Create();
-            ScopedVclPtr<SvxAbstractNewTableDialog> pDlg( pFact ? pFact->CreateSvxNewTableDialog() : nullptr);
+            ScopedVclPtr<SvxAbstractNewTableDialog> pDlg( pFact ? pFact->CreateSvxNewTableDialog(rReq.GetFrameWeld()) : nullptr);
 
             if( !pDlg.get() || (pDlg->Execute() != RET_OK) )
                 break;
