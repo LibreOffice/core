e208c8f32b475db77ac6e0b39b7f59c0a3a83449
Rene Engelhard <rene@debian.org>
2018-04-30 12:13:21 +0200

    build-examples_common.mk: add mode lines and license header
    Change-Id: I61db4968af4770817606da0c9c2d0622d7db4812

--- odk/build-examples_common.mk
+++ odk/build-examples_common.mk
@@ -1,3 +1,12 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t; fill-column: 100 -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
 define odk_build-examples_test
 
 $(eval $(call gb_CustomTarget_CustomTarget,$(1)))
@@ -50,3 +59,5 @@ $(call gb_CustomTarget_get_workdir,$(1))/setsdkenv: \
 .PHONY: $(call gb_CustomTarget_get_target,$(1))
 
 endef
+
+# vim: set noet sw=4 ts=4:
