e3b29065dce16cd077e53f900aafd1fa767ea1b7
Noel Grandin <noel.grandin@collabora.co.uk>
2018-05-02 10:29:25 +0200

    loplugin:useuniqueptr in SdrPaintView
    Change-Id: I10c9db25dfb0cce7d0815cdc8aff290b3d0c0026

--- include/svx/svdpntv.hxx
+++ include/svx/svdpntv.hxx
@@ -34,6 +34,7 @@
 #include <svtools/optionsdrawinglayer.hxx>
 #include <unotools/options.hxx>
 #include <vcl/idle.hxx>
+#include <memory>
 
 
 // Pre defines
@@ -133,7 +134,7 @@ private:
     // the SdrModel this view was created with, unchanged during lifetime
     SdrModel&                   mrSdrModelFromSdrView;
 
-    SdrPageView*                mpPageView;
+    std::unique_ptr<SdrPageView> mpPageView;
 protected:
     SdrModel*                   mpModel;
 #ifdef DBG_UTIL
@@ -313,7 +314,7 @@ public:
     virtual void HideSdrPage();
 
     // Iterate over all registered PageViews
-    SdrPageView* GetSdrPageView() const { return mpPageView; }
+    SdrPageView* GetSdrPageView() const { return mpPageView.get(); }
 
     // A SdrView can be displayed on multiple Windows at the same time
     virtual void AddWindowToPaintView(OutputDevice* pNewWin, vcl::Window* pWindow);
