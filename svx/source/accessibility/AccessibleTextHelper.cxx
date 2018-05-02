LibreOffice/core

commit 891e41fac81fbd8d5cdb277b26639abfd25a7143

dead code in AccessibleTextHelper_Impl::UpdateVisibleChildren
ever since
    commit 60f11adb950e4f9645cc9ecb0f5af8235cc97366
    Date:   Wed Nov 27 13:03:45 2013 +0000
    Integrate branch of IAccessible2

Change-Id: Ie175277badc07cf79eadbeef5a75857b914876d9
Reviewed-on: https://gerrit.libreoffice.org/52370
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svx/source/accessibility/AccessibleTextHelper.cxx
@@ -757,58 +757,28 @@ namespace accessibility
         try
         {
             SvxTextForwarder& rCacheTF = GetTextForwarder();
-            SvxViewForwarder& rCacheVF = GetViewForwarder();
-
-            tools::Rectangle aViewArea = rCacheVF.GetVisArea();
-
-            if( IsActive() )
-            {
-                // maybe the edit view scrolls, adapt aViewArea
-                tools::Rectangle aEditViewArea = GetEditViewForwarder().GetVisArea();
-                aViewArea += aEditViewArea.TopLeft();
-
-                // now determine intersection
-                aViewArea.Intersection( aEditViewArea );
-            }
-
-            tools::Rectangle aTmpBB, aParaBB;
-            bool bFirstChild = true;
-            sal_Int32 nCurrPara;
             sal_Int32 nParas=rCacheTF.GetParagraphCount();
 
-            mnFirstVisibleChild = -1;
+            mnFirstVisibleChild = nParas ? 0 : -1;
             mnLastVisibleChild = -2;
 
-            for( nCurrPara=0; nCurrPara<nParas; ++nCurrPara )
+            if (mxFrontEnd.is() && bBroadcastEvents)
             {
-                DBG_ASSERT(nCurrPara >= 0,
-                           "AccessibleTextHelper_Impl::UpdateVisibleChildren: index value overflow");
-
-                aTmpBB = rCacheTF.GetParaBounds( nCurrPara );
-
-                // convert to screen coordinates
-                aParaBB = ::accessibility::AccessibleEditableTextPara::LogicToPixel( aTmpBB, rCacheTF.GetMapMode(), rCacheVF );
-                // at least partially visible
-                if( bFirstChild )
+                for( sal_Int32 nCurrPara=0; nCurrPara<nParas; ++nCurrPara )
                 {
-                    bFirstChild = false;
-                    mnFirstVisibleChild = nCurrPara;
-                }
-
-                mnLastVisibleChild = nCurrPara;
-
-                // child not yet created?
-                ::accessibility::AccessibleParaManager::WeakChild aChild( maParaManager.GetChild(nCurrPara) );
-                if( aChild.second.Width == 0 &&
-                    aChild.second.Height == 0 &&
-                    mxFrontEnd.is() &&
-                    bBroadcastEvents )
-                {
-                    GotPropertyEvent( uno::makeAny( maParaManager.CreateChild( nCurrPara - mnFirstVisibleChild,
-                                                                               mxFrontEnd, GetEditSource(), nCurrPara ).first ),
-                                      AccessibleEventId::CHILD );
+                    // child not yet created?
+                    ::accessibility::AccessibleParaManager::WeakChild aChild( maParaManager.GetChild(nCurrPara) );
+                    if( aChild.second.Width == 0 &&
+                        aChild.second.Height == 0 )
+                    {
+                        GotPropertyEvent( uno::makeAny( maParaManager.CreateChild( nCurrPara - mnFirstVisibleChild,
+                                                                                   mxFrontEnd, GetEditSource(), nCurrPara ).first ),
+                                          AccessibleEventId::CHILD );
+                    }
                 }
             }
+
+            mnLastVisibleChild = nParas - 1;
         }
         catch( const uno::Exception& )
         {

commit 1a6a08e32e687ec5ede98d456819e5641b3ff242

tdf#117205 impress crash when moving an item in a list
regression from
   commit 891e41fac81fbd8d5cdb277b26639abfd25a7143 (patch)
   dead code in AccessibleTextHelper_Impl::UpdateVisibleChildren

Change-Id: I0db104fcde91597dc71b6cae6c547a0cedac4af2

svx/source/accessibility/AccessibleTextHelper.cxx
@@ -759,12 +759,15 @@ namespace accessibility
             SvxTextForwarder& rCacheTF = GetTextForwarder();
             sal_Int32 nParas=rCacheTF.GetParagraphCount();
 
-            mnFirstVisibleChild = nParas ? 0 : -1;
+            mnFirstVisibleChild = -1;
             mnLastVisibleChild = -2;
 
-            if (mxFrontEnd.is() && bBroadcastEvents)
+            for( sal_Int32 nCurrPara=0; nCurrPara<nParas; ++nCurrPara )
             {
-                for( sal_Int32 nCurrPara=0; nCurrPara<nParas; ++nCurrPara )
+                if (nCurrPara == 0)
+                    mnFirstVisibleChild = nCurrPara;
+                mnLastVisibleChild = nCurrPara;
+                if (mxFrontEnd.is() && bBroadcastEvents)
                 {
                     // child not yet created?
                     ::accessibility::AccessibleParaManager::WeakChild aChild( maParaManager.GetChild(nCurrPara) );
@@ -777,8 +780,6 @@ namespace accessibility
                     }
                 }
             }
-
-            mnLastVisibleChild = nParas - 1;
         }
         catch( const uno::Exception& )
         {