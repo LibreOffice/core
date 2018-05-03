644c645860bf0bc4df9d652845e0276ff98d10f1
Chris Sherlock <chris.sherlock79@gmail.com>
2018-05-04 03:40:11 +1000

    tools: document and test sanitiseMm100ToTwip()
    Change-Id: I2f6349c679a714e5168ae3c5eccd054c5522bbc9

---
+++ tools/qa/cppunit/test_100mm2twips.cxx
@@ -0,0 +1,55 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#include <sal/types.h>
+#include <sal/log.hxx>
+
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/plugin/TestPlugIn.h>
+
+#include <tools/helpers.hxx>
+
+#include <cassert>
+
+namespace tools
+{
+class cm2TwipsTest : public CppUnit::TestFixture
+{
+public:
+    void testConvert()
+    {
+        sal_Int32 nActual = sanitiseMm100ToTwip(255);
+        sal_Int32 nExpected = 145;
+        CPPUNIT_ASSERT_EQUAL(nExpected, nActual);
+
+        nActual = sanitiseMm100ToTwip(-255);
+        nExpected = -145;
+        CPPUNIT_ASSERT_EQUAL(nExpected, nActual);
+    }
+
+    CPPUNIT_TEST_SUITE(cm2TwipsTest);
+    CPPUNIT_TEST(testConvert);
+    CPPUNIT_TEST_SUITE_END();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(cm2TwipsTest);
+} // namespace tools
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
