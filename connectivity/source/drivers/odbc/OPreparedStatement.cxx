ad35d05fe867c743322c310b68ee45186677150c
Noel Grandin <noel.grandin@collabora.co.uk>
2018-05-03 13:52:47 +0200

    loplugin:useuniqueptr in OPreparedStatement
    Change-Id: Iea0a8c92bbf7820b5f9e2ab79e7df662e8280b2d

--- connectivity/source/drivers/odbc/OPreparedStatement.cxx
+++ connectivity/source/drivers/odbc/OPreparedStatement.cxx
@@ -64,7 +64,6 @@ namespace
 OPreparedStatement::OPreparedStatement( OConnection* _pConnection,const OUString& sql)
     :OStatement_BASE2(_pConnection)
     ,numParams(0)
-    ,boundParams(nullptr)
     ,m_bPrepared(false)
 {
     m_sSqlStatement = sql;
@@ -688,10 +687,7 @@ void OPreparedStatement::initBoundParam ()
 
     if (numParams > 0)
     {
-        // Allocate an array of bound parameter objects
-
-        boundParams = new OBoundParam[numParams];
-
+        boundParams.reset(new OBoundParam[numParams]);
     }
 }
 
@@ -853,8 +849,7 @@ void OPreparedStatement::setStream(
 void OPreparedStatement::FreeParams()
 {
     numParams = 0;
-    delete [] boundParams;
-    boundParams = nullptr;
+    boundParams.reset();
 }
 
 void OPreparedStatement::setFastPropertyValue_NoBroadcast(sal_Int32 nHandle,const Any& rValue)
