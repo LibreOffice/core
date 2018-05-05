af87174f879bfe6daaaac7a2fabd4f74abc754d2
Chris Sherlock <chris.sherlock79@gmail.com>
2018-05-06 01:32:23 +1000

    test: create unit tests for Config
    Change-Id: Iaf92f93e169cf7367e3b9fc521f237413a268493

---
+++ tools/qa/cppunit/test_config.cxx
@@ -0,0 +1,190 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <test/bootstrapfixture.hxx>
+
+#include <osl/file.hxx>
+#include <rtl/ustring.hxx>
+
+#include <tools/config.hxx>
+
+class ToolsConfigTest : public test::BootstrapFixture
+{
+public:
+    ToolsConfigTest()
+        : BootstrapFixture(true, false)
+    {
+    }
+
+    virtual void setUp() override
+    {
+        maConfigFile = m_directories.getURLFromSrc("/tools/qa/data/");
+        maConfigFile += "testconfig.ini";
+    }
+
+    void testHasGroup()
+    {
+        Config aConfig(maConfigFile);
+        CPPUNIT_ASSERT(aConfig.HasGroup("TestGroup"));
+        CPPUNIT_ASSERT(aConfig.HasGroup("TestGroup2"));
+    }
+
+    void testGetGroup()
+    {
+        Config aConfig(maConfigFile);
+        CPPUNIT_ASSERT_EQUAL(OString(""), aConfig.GetGroup());
+
+        CPPUNIT_ASSERT_EQUAL(OString("TestGroup"), aConfig.GetGroupName(0));
+        CPPUNIT_ASSERT_EQUAL(OString("TestGroup2"), aConfig.GetGroupName(1));
+        CPPUNIT_ASSERT_EQUAL(OString(""), aConfig.GetGroupName(2));
+    }
+
+    void testSetGroup()
+    {
+        Config aConfig(maConfigFile);
+
+        aConfig.SetGroup(OString("TestGroup"));
+        CPPUNIT_ASSERT_EQUAL(OString("TestGroup"), aConfig.GetGroup());
+
+        // so this is a quirk of Config - you can set the group name,
+        // but it might not exist so you really should first check if
+        // it exists via HasGroup()
+        aConfig.SetGroup(OString("TestGroupA"));
+        CPPUNIT_ASSERT(!aConfig.HasGroup("TestGroupA"));
+        CPPUNIT_ASSERT_EQUAL(OString("TestGroupA"), aConfig.GetGroup());
+    }
+
+    void testDeleteGroup()
+    {
+        OUString aConfigFileBackup(maConfigFile + OUString(".bak"));
+        osl::File::copy(maConfigFile, aConfigFileBackup);
+
+        {
+            Config aConfig(maConfigFile);
+
+            aConfig.DeleteGroup(OString("TestGroup"));
+            CPPUNIT_ASSERT(!aConfig.HasGroup("TestGroup"));
+            CPPUNIT_ASSERT_EQUAL(OString("TestGroup2"), aConfig.GetGroupName(0));
+
+            sal_uInt16 nExpected = 1;
+            sal_uInt16 nActual = aConfig.GetGroupCount();
+            CPPUNIT_ASSERT_EQUAL(nExpected, nActual);
+        }
+
+        osl::File::copy(aConfigFileBackup, maConfigFile);
+
+        {
+            Config aConfig(maConfigFile);
+
+            CPPUNIT_ASSERT(!aConfig.HasGroup("NonExistentTestGroup"));
+            aConfig.DeleteGroup(OString("NonExistentTestGroup"));
+            CPPUNIT_ASSERT_EQUAL(OString("TestGroup"), aConfig.GetGroupName(0));
+
+            sal_uInt16 nExpected = 2;
+            sal_uInt16 nActual = aConfig.GetGroupCount();
+            CPPUNIT_ASSERT_EQUAL(nExpected, nActual);
+        }
+
+        osl::File::move(aConfigFileBackup, maConfigFile);
+    }
+
+    void testGetGroupCount()
+    {
+        Config aConfig(maConfigFile);
+        sal_uInt16 nExpected = 2;
+        sal_uInt16 nActual = aConfig.GetGroupCount();
+        CPPUNIT_ASSERT_EQUAL(nExpected, nActual);
+    }
+
+    void testReadKey()
+    {
+        Config aConfig(maConfigFile);
+        aConfig.SetGroup(OString("TestGroup"));
+        CPPUNIT_ASSERT_EQUAL(OString("testvalue"), aConfig.ReadKey(OString("testkey")));
+        CPPUNIT_ASSERT_EQUAL(OString(), aConfig.ReadKey(OString("nonexistenttestkey")));
+        CPPUNIT_ASSERT_EQUAL(OString("notexists"),
+                             aConfig.ReadKey(OString("nonexistenttestkey"), OString("notexists")));
+
+        aConfig.SetGroup(OString("TestGroup2"));
+        CPPUNIT_ASSERT_EQUAL(OString("testvalue"), aConfig.ReadKey(OString("testkey2")));
+        CPPUNIT_ASSERT_EQUAL(OString(), aConfig.ReadKey(OString("nonexistenttestkey")));
+        CPPUNIT_ASSERT_EQUAL(OString("notexists"),
+                             aConfig.ReadKey(OString("nonexistenttestkey"), OString("notexists")));
+    }
+
+    void testGetKeyName()
+    {
+        Config aConfig(maConfigFile);
+        aConfig.SetGroup(OString("TestGroup"));
+        CPPUNIT_ASSERT_EQUAL(OString("testkey"), aConfig.GetKeyName(0));
+
+        aConfig.SetGroup(OString("TestGroup2"));
+        CPPUNIT_ASSERT_EQUAL(OString("testkey2"), aConfig.GetKeyName(0));
+    }
+
+    void testWriteDeleteKey()
+    {
+        OUString aConfigFileBackup(maConfigFile + OUString(".bak"));
+        osl::File::copy(maConfigFile, aConfigFileBackup);
+
+        Config aConfig(maConfigFile);
+        aConfig.SetGroup(OString("TestGroup"));
+        aConfig.WriteKey(OString("testkey_new"), OString("testvalue"));
+
+        sal_uInt16 nExpected = 2;
+        sal_uInt16 nActual = aConfig.GetKeyCount();
+        CPPUNIT_ASSERT_EQUAL(nExpected, nActual);
+        CPPUNIT_ASSERT_EQUAL(OString("testvalue"), aConfig.ReadKey(OString("testkey_new")));
+
+        aConfig.DeleteKey(OString("testkey_new"));
+        nExpected = 1;
+        CPPUNIT_ASSERT_EQUAL(nExpected, nActual);
+        CPPUNIT_ASSERT_EQUAL(OString(), aConfig.ReadKey(OString("testkey_new")));
+
+        aConfig.SetGroup(OString("TestGroup2"));
+        aConfig.WriteKey(OString("testkey_new"), OString("testvalue"));
+
+        nActual = aConfig.GetKeyCount();
+        CPPUNIT_ASSERT_EQUAL(nExpected, nActual);
+        CPPUNIT_ASSERT_EQUAL(OString("testvalue"), aConfig.ReadKey(OString("testkey_new")));
+
+        aConfig.DeleteKey(OString("testkey_new"));
+        nExpected = 1;
+        CPPUNIT_ASSERT_EQUAL(nExpected, nActual);
+        CPPUNIT_ASSERT_EQUAL(OString(), aConfig.ReadKey(OString("testkey_new")));
+
+        aConfig.SetGroup(OString("TestGroup3"));
+        aConfig.WriteKey(OString("testkey_new_group3"), OString("testvalue"));
+
+        nExpected = 1;
+        nActual = aConfig.GetKeyCount();
+        CPPUNIT_ASSERT_EQUAL(nExpected, nActual);
+        CPPUNIT_ASSERT_EQUAL(OString("testvalue"), aConfig.ReadKey(OString("testkey_new_group3")));
+
+        osl::File::move(aConfigFileBackup, maConfigFile);
+    }
+
+    CPPUNIT_TEST_SUITE(ToolsConfigTest);
+    CPPUNIT_TEST(testHasGroup);
+    CPPUNIT_TEST(testGetGroup);
+    CPPUNIT_TEST(testSetGroup);
+    CPPUNIT_TEST(testDeleteGroup);
+    CPPUNIT_TEST(testReadKey);
+    CPPUNIT_TEST(testGetGroupCount);
+    CPPUNIT_TEST_SUITE_END();
+
+private:
+    OUString maConfigFile;
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(ToolsConfigTest);
+
+CPPUNIT_PLUGIN_IMPLEMENT();
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
