db6463609a5ea9e85bf340789bc222dc71c6ebfb
Chris Sherlock <chris.sherlock79@gmail.com>
2018-05-06 14:07:57 +1000

    tools: unit test for Time
    Change-Id: Iee1e24d6ed039aea093c759c80aec8a06215e534

---
+++ tools/qa/cppunit/test_time.cxx
@@ -0,0 +1,121 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <iostream>
+
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+#include <tools/time.hxx>
+
+namespace tools
+{
+class TimeTest : public CppUnit::TestFixture
+{
+public:
+    void testZeroTime()
+    {
+        Time aTime(Time::TimeInitEmpty::EMPTY);
+        CPPUNIT_ASSERT(!aTime.GetTime());
+    }
+
+    void testTime()
+    {
+        {
+            Time aTime(23, 59, 59, 999999999);
+            sal_uInt16 nExpectedHour = 23;
+            CPPUNIT_ASSERT_EQUAL(nExpectedHour, aTime.GetHour());
+            sal_uInt16 nExpectedMinute = 59;
+            CPPUNIT_ASSERT_EQUAL(nExpectedMinute, aTime.GetMin());
+            sal_uInt16 nExpectedSecond = 59;
+            CPPUNIT_ASSERT_EQUAL(nExpectedSecond, aTime.GetSec());
+            sal_Int32 nExpectedMillisecond = 999 + 59000 + 3540000 + 8280000;
+            CPPUNIT_ASSERT_EQUAL(nExpectedMillisecond, aTime.GetMSFromTime());
+            sal_uInt32 nExpectedNanoSecond = 999999999;
+            CPPUNIT_ASSERT_EQUAL(nExpectedNanoSecond, aTime.GetNanoSec());
+        }
+
+        {
+            Time aTime(23, 59, 59);
+            sal_uInt16 nExpectedHour = 23;
+            CPPUNIT_ASSERT_EQUAL(nExpectedHour, aTime.GetHour());
+            sal_uInt16 nExpectedMinute = 59;
+            CPPUNIT_ASSERT_EQUAL(nExpectedMinute, aTime.GetMin());
+            sal_uInt16 nExpectedSecond = 59;
+            CPPUNIT_ASSERT_EQUAL(nExpectedSecond, aTime.GetSec());
+            sal_Int32 nExpectedMillisecond = 59000 + 3540000 + 8280000;
+            CPPUNIT_ASSERT_EQUAL(nExpectedMillisecond, aTime.GetMSFromTime());
+            sal_uInt32 nExpectedNanoSecond = 0;
+            CPPUNIT_ASSERT_EQUAL(nExpectedNanoSecond, aTime.GetNanoSec());
+        }
+
+        {
+            Time aTime(23, 59);
+            sal_uInt16 nExpectedHour = 23;
+            CPPUNIT_ASSERT_EQUAL(nExpectedHour, aTime.GetHour());
+            sal_uInt16 nExpectedMinute = 59;
+            CPPUNIT_ASSERT_EQUAL(nExpectedMinute, aTime.GetMin());
+            sal_uInt16 nExpectedSecond = 0;
+            CPPUNIT_ASSERT_EQUAL(nExpectedSecond, aTime.GetSec());
+            sal_Int32 nExpectedMillisecond = 3540000 + 8280000;
+            CPPUNIT_ASSERT_EQUAL(nExpectedMillisecond, aTime.GetMSFromTime());
+            sal_uInt32 nExpectedNanoSecond = 0;
+            CPPUNIT_ASSERT_EQUAL(nExpectedNanoSecond, aTime.GetNanoSec());
+        }
+    }
+
+    void testMakeTimeFromMS()
+    {
+        Time aExpectedTime(3, 17, 59, 999000000);
+        sal_Int32 nExpectedMilliseconds = 11879999;
+
+        Time aTime(Time::TimeInitEmpty::EMPTY);
+        aTime.MakeTimeFromMS(nExpectedMilliseconds);
+
+        CPPUNIT_ASSERT_EQUAL(aExpectedTime, aTime);
+    }
+
+    void testMakeTimeFromNS()
+    {
+        Time aExpectedTime(3, 17, 59, 999999999);
+        long nExpectedNanoseconds = 11879999999999;
+
+        Time aTime(Time::TimeInitEmpty::EMPTY);
+        aTime.MakeTimeFromNS(nExpectedNanoseconds);
+
+        CPPUNIT_ASSERT_EQUAL(aExpectedTime, aTime);
+    }
+
+    void testGetTimeInDays()
+    {
+        Time aTime(12, 0);
+        double fExpectedDays = 0.5;
+
+        CPPUNIT_ASSERT_EQUAL(fExpectedDays, aTime.GetTimeInDays());
+    }
+
+    void testIsEqualIgnoreNS()
+    {
+        Time aTime(23, 59, 59, 999999999);
+        CPPUNIT_ASSERT(aTime.IsEqualIgnoreNanoSec(Time(23, 59, 59)));
+    }
+
+    CPPUNIT_TEST_SUITE(TimeTest);
+    CPPUNIT_TEST(testZeroTime);
+    CPPUNIT_TEST(testMakeTimeFromMS);
+    CPPUNIT_TEST(testMakeTimeFromNS);
+    CPPUNIT_TEST(testGetTimeInDays);
+    CPPUNIT_TEST(testIsEqualIgnoreNS);
+    CPPUNIT_TEST_SUITE_END();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(TimeTest);
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
