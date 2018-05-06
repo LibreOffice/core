ed565447ef96e312b4c776d60b2d76bcbb8ab26a
Mike Kaganski <mike.kaganski@collabora.com>
2018-05-06 12:01:47 +0300

    ww8export: Fix testTableKeep
    The tables in the test .ODT contain a cell with text "With merged cells".
    The text is supposed to wrap, so that cell height includes the two lines.
    The width of the cell is 32.72 mm. When it is exported to .DOC, due to
    rounding errors, the width becomes 32.76 mm. It happens that the text
    width is too close to these numbers, so depending on rounding when text
    layouting, it may start to not wrap in .DOC.
    
    This just adds an extra space character to the line, to stop depend on
    rounding errors.
    
    This also reverts commits b2a053160c4c8eb0acd66b74c52dbda23a654bda and
    a30a36b3c70e64b62add9f3c5376202b9682851f, which had workarounded this
    issue.
    
    Change-Id: Ifdb7a042f78a1a3cce3a385e9db865a1b8c7b134

--- sw/qa/extras/ww8export/ww8export.cxx
+++ sw/qa/extras/ww8export/ww8export.cxx
@@ -1293,7 +1293,6 @@ DECLARE_WW8EXPORT_TEST(testCommentExport, "comment-export.odt")
     }
 }
 
-#if !defined(MACOSX) && !defined(_WIN32)
 #if !TEST_FONTS_MISSING
 DECLARE_WW8EXPORT_TEST(testTableKeep, "tdf91083.odt")
 {
@@ -1301,7 +1300,6 @@ DECLARE_WW8EXPORT_TEST(testTableKeep, "tdf91083.odt")
     CPPUNIT_ASSERT_EQUAL( OUString("Row 1"), parseDump("/root/page[3]/body/tab[1]/row[2]/cell[1]/txt[1]") );
     CPPUNIT_ASSERT_EQUAL( OUString("Row 1"), parseDump("/root/page[6]/body/tab[1]/row[2]/cell[1]/txt[1]") );
 }
-#endif
 #endif
 
 DECLARE_WW8EXPORT_TEST(testTdf76349_textboxMargins, "tdf76349_textboxMargins.doc")
