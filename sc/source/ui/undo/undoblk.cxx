94526129dc7c56474017a93b9edbd875283dc618
Henry Castro <hcastro@collabora.com>
2018-05-06 22:40:05 -0400

    tdf#117228: crash in SfxItemSet::GetItemState...
    (unsigned short, bool, SfxPoolItem const**) when pasting comment of closed document
    
    Re-work commit 1b7a8277aa3e9f73ccdf15e933a1ee3b42849a44.
    
    In the tiled rendering case, each view has its own clipboard,
    but  not in desktop version which it has a shared clipboard each view.
    
    Change-Id: I57b1ab81e4c141829dbad899330e5c22204c384a

--- sc/source/ui/undo/undoblk.cxx
+++ sc/source/ui/undo/undoblk.cxx
@@ -1132,7 +1132,7 @@ void ScUndoPaste::Repeat(SfxRepeatTarget& rTarget)
     {
         ScTabViewShell* pViewSh = static_cast<ScTabViewTarget&>(rTarget).GetViewShell();
         // keep a reference in case the clipboard is changed during PasteFromClip
-        rtl::Reference<ScTransferObj> pOwnClip = ScTransferObj::GetOwnClipboard( pViewSh->GetActiveWin() );
+        const ScTransferObj* pOwnClip = ScTransferObj::GetOwnClipboard(pViewSh->GetClipData());
         if (pOwnClip)
         {
             pViewSh->PasteFromClip( nFlags, pOwnClip->GetDocument(),
