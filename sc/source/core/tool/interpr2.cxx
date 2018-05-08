903c3eb00bc6f94e20edc0a04c5ff962d61bcca3
Takeshi Abe <tabe@fixedpoint.jp>
2018-05-08 16:35:49 +0900

    sc: Avoid looking up system clock twice to get current datetime
    Change-Id: I904067260c15263f8169f095809e642d0738f6bb

--- sc/source/core/tool/interpr2.cxx
+++ sc/source/core/tool/interpr2.cxx
@@ -112,9 +112,8 @@ void ScInterpreter::ScGetActDate()
 void ScInterpreter::ScGetActTime()
 {
     nFuncFmtType = SvNumFormatType::DATETIME;
-    Date aActDate( Date::SYSTEM );
-    long nDiff = aActDate - pFormatter->GetNullDate();
-    tools::Time aActTime( tools::Time::SYSTEM );
+    DateTime aActTime( DateTime::SYSTEM );
+    long nDiff = aActTime - pFormatter->GetNullDate();
     double fTime = aActTime.GetHour()    / static_cast<double>(::tools::Time::hourPerDay)   +
                    aActTime.GetMin()     / static_cast<double>(::tools::Time::minutePerDay) +
                    aActTime.GetSec()     / static_cast<double>(::tools::Time::secondPerDay) +
