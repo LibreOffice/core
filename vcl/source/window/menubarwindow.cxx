c4e76e740a562f0e1ba209f0413f3c91a47cbcc0
Maxim Monastirsky <momonasmon@gmail.com>
2018-05-09 23:52:00 +0300

    tdf#117440 Can't move focus to disposed window
    xSaveFocusId is the already disposed comments floating window,
    so it can't accept focus anymore. Moreover, if closing the
    menu by clicking its title, the focus results nowhere, and no
    keyboard interaction works at all (including e.g. F6 or F10).
    
    This patch makes the focus switch back to the document, like
    it was in earlier LO versions. And does it in a way that
    doesn't break tdf#115227.
    
    Change-Id: I1d473557c10faf65b5b1fa917f5a5d66d0de8656

--- vcl/source/window/menubarwindow.cxx
+++ vcl/source/window/menubarwindow.cxx
@@ -514,20 +514,25 @@ void MenuBarWindow::ChangeHighlightItem( sal_uInt16 n, bool bSelectEntry, bool b
         ImplGetSVData()->maWinData.mbNoDeactivate = false;
         if( !ImplGetSVData()->maWinData.mbNoSaveFocus )
         {
-            VclPtr<vcl::Window> xTempFocusId = xSaveFocusId;
+            VclPtr<vcl::Window> xTempFocusId;
+            if (xSaveFocusId && !xSaveFocusId->isDisposed())
+                xTempFocusId = xSaveFocusId;
             xSaveFocusId = nullptr;
+
             if (bAllowRestoreFocus)
             {
                 // tdf#115227 the popup is already killed, so temporarily set us as the
                 // focus window, so we could avoid sending superfluous activate events
                 // to top window listeners.
-                ImplGetSVData()->maWinData.mpFocusWin = this;
-                Window::EndSaveFocus(xTempFocusId);
-                assert(xTempFocusId == nullptr || ImplGetSVData()->maWinData.mpFocusWin != this);
+                if (xTempFocusId || bDefaultToDocument)
+                    ImplGetSVData()->maWinData.mpFocusWin = this;
+
+                // #105406# restore focus to document if we could not save focus before
+                if (!xTempFocusId && bDefaultToDocument)
+                    GrabFocusToDocument();
+                else
+                    Window::EndSaveFocus(xTempFocusId);
             }
-            // #105406# restore focus to document if we could not save focus before
-            if (bDefaultToDocument && xTempFocusId == nullptr && bAllowRestoreFocus)
-                GrabFocusToDocument();
         }
     }
 
