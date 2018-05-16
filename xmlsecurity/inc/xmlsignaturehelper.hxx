62f676d1eec79294db098a72b6204d72286bcbee
Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
2018-05-16 16:34:35 +0200

    tdf#83877 Write SignatureLineId to ODF & OOXML signatures
    Change-Id: I483a3b7895cdcb10ef9d6dacf167ed0f8db7e723

--- xmlsecurity/inc/xmlsignaturehelper.hxx
+++ xmlsecurity/inc/xmlsignaturehelper.hxx
@@ -129,6 +129,7 @@ public:
 
     void        SetDateTime( sal_Int32 nSecurityId, const Date& rDate, const tools::Time& rTime );
     void SetDescription(sal_Int32 nSecurityId, const OUString& rDescription);
+    void SetSignatureLineId(sal_Int32 nSecurityId, const OUString& rSignatureLineId);
 
     void        AddForSigning( sal_Int32 securityId, const OUString& uri, bool bBinary, bool bXAdESCompliantIfODF );
     void        CreateAndWriteSignature( const css::uno::Reference< css::xml::sax::XDocumentHandler >& xDocumentHandler, bool bXAdESCompliantIfODF );
