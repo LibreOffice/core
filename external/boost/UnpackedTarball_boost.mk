bca5abfdd56cecfaf500d4f8fa0a2b015f034e06
Tor Lillqvist <tml@collabora.com>
2018-05-16 18:08:59 +0300

    Don't ever attempt to initialise a std::locale with a UTF-8 locale on Windows
    As I said in 2ff121f298e64ff204621220622472fe697d599d, passing a
    POSIX-style locale name to the std::locale constructor on Windows is
    decidedly odd, and apparently boost adds insult to injury by even
    really attempting to use a UTF-8 one if that is what our code thinks
    it wants. Or something.
    
    This commit avoids the ton of
    "warn:unotools.i18n:7996:3544:unotools/source/i18n/resmgr.cxx:123: CRT
    Report Hook: ASSERT: f:\dd\vctools\crt\crtw32\stdcpp\xmbtowc.c(89) :
    Assertion failed: ploc->_Mbcurmax == 1 || ploc->_Mbcurmax == 2" we
    otherwise get when built with --enable-dbgutil and a current VS2015
    (or 2017?).
    
    Change-Id: I3a774dff7b12d9960f553c9f242e0463c6d13d5d
    Reviewed-on: https://gerrit.libreoffice.org/54444
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Tor Lillqvist <tml@collabora.com>

--- external/boost/UnpackedTarball_boost.mk
+++ external/boost/UnpackedTarball_boost.mk
@@ -37,6 +37,8 @@ boost_patches += clang-cl.patch.0
 boost_patches += boost_1_60_0.undef.warning.patch
 boost_patches += boost_1_63_0.undef.warning.patch.1
 
+boost_patches += windows-no-utf8-locales.patch.0
+
 $(eval $(call gb_UnpackedTarball_UnpackedTarball,boost))
 
 $(eval $(call gb_UnpackedTarball_set_tarball,boost,$(BOOST_TARBALL)))
