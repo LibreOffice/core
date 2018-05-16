56fddbb4df7fabeb4170c7f851c34f722f1424b7
Andras Timar <andras.timar@collabora.com>
2018-05-16 11:03:54 +0200

    build fix after ea7fba0ff338098dea5292557c418b7d8c8a100b
    Change-Id: I8885d3c97c3ae11d027e044f01e7bb2a2fcc8968
    (cherry picked from commit 302cb9372c8185b8e003f508b587965d710e2911)

--- sc/qa/unit/tiledrendering/tiledrendering.cxx
+++ sc/qa/unit/tiledrendering/tiledrendering.cxx
@@ -1632,8 +1632,8 @@ void ScTiledRenderingTest::testFilterDlg()
     CPPUNIT_ASSERT_EQUAL(false, pView1->GetViewFrame()->GetDispatcher()->IsLocked());
 
     KeyEvent aEvent(27, KEY_ESCAPE, 0);
-    Application::PostKeyEvent(VclEventId::WindowKeyInput, pRefWindow->GetWindow(), &aEvent);
-    Application::PostKeyEvent(VclEventId::WindowKeyUp, pRefWindow->GetWindow(), &aEvent);
+    Application::PostKeyEvent(VCLEVENT_WINDOW_KEYINPUT, pRefWindow->GetWindow(), &aEvent);
+    Application::PostKeyEvent(VCLEVENT_WINDOW_KEYUP, pRefWindow->GetWindow(), &aEvent);
 
     Scheduler::ProcessEventsToIdle();
     CPPUNIT_ASSERT_EQUAL(false, pView2->GetViewFrame()->GetDispatcher()->IsLocked());
