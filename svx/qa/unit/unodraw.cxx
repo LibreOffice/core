1717b2b15a2d90906c20874d6da25a7e5722fb06
Miklos Vajna <vmiklos@collabora.co.uk>
2018-05-18 12:26:37 +0200

    tdf#117614 svx: make drawing.GraphicExportFilter work with sw images
    A Writer image has an underlying SdrObject, but GetSdrObjectFromXShape()
    won't work for it, also we can't an sw SdrObject into an XShape because
    SvxDrawPage::CreateShape() has no idea how to handle sw's
    SdrInventor::Swg inventor.
    
    Fix the problem by just getting the Graphic of the Writer image and
    improve GraphicExportFilter to be able to work with just a Graphic as
    well.
    
    (cherry picked from commit aca5e1ba7f33c8913304b5f90ef478f316e64263)
    
    Conflicts:
    	svx/CppunitTest_svx_unit.mk
    	svx/source/unodraw/UnoGraphicExporter.cxx
    
    Change-Id: I3c9b3005366fcc87815597a27df3cb8a99a8876c
    Reviewed-on: https://gerrit.libreoffice.org/54688
    Reviewed-by: Andras Timar <andras.timar@collabora.com>
    Tested-by: Andras Timar <andras.timar@collabora.com>

---
+++ svx/qa/unit/unodraw.cxx
@@ -0,0 +1,89 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <cppunit/TestAssert.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+#include <com/sun/star/drawing/GraphicExportFilter.hpp>
+#include <com/sun/star/drawing/XDrawPageSupplier.hpp>
+#include <com/sun/star/frame/Desktop.hpp>
+
+#include <comphelper/processfactory.hxx>
+#include <comphelper/propertysequence.hxx>
+#include <test/bootstrapfixture.hxx>
+#include <unotest/macros_test.hxx>
+#include <unotools/tempfile.hxx>
+
+using namespace ::com::sun::star;
+
+namespace
+{
+char const DATA_DIRECTORY[] = "/svx/qa/unit/data/";
+
+/// Tests for svx/source/unodraw/ code.
+class UnodrawTest : public test::BootstrapFixture, public unotest::MacrosTest
+{
+    uno::Reference<uno::XComponentContext> mxComponentContext;
+    uno::Reference<lang::XComponent> mxComponent;
+
+public:
+    void testWriterGraphicExport();
+
+    void setUp() override;
+    void tearDown() override;
+
+    CPPUNIT_TEST_SUITE(UnodrawTest);
+    CPPUNIT_TEST(testWriterGraphicExport);
+    CPPUNIT_TEST_SUITE_END();
+};
+
+void UnodrawTest::setUp()
+{
+    test::BootstrapFixture::setUp();
+
+    mxComponentContext.set(comphelper::getComponentContext(getMultiServiceFactory()));
+    mxDesktop.set(frame::Desktop::create(mxComponentContext));
+}
+
+void UnodrawTest::tearDown()
+{
+    if (mxComponent.is())
+        mxComponent->dispose();
+
+    test::BootstrapFixture::tearDown();
+}
+
+void UnodrawTest::testWriterGraphicExport()
+{
+    // Load a document with a Writer picture in it.
+    OUString aURL = m_directories.getURLFromSrc(DATA_DIRECTORY) + "unodraw-writer-image.odt";
+    mxComponent = loadFromDesktop(aURL);
+    uno::Reference<drawing::XDrawPageSupplier> xDrawPageSupplier(mxComponent, uno::UNO_QUERY);
+    uno::Reference<drawing::XDrawPage> xDrawPage = xDrawPageSupplier->getDrawPage();
+    uno::Reference<lang::XComponent> xShape(xDrawPage->getByIndex(0), uno::UNO_QUERY);
+
+    // Export it as JPEG.
+    uno::Reference<drawing::XGraphicExportFilter> xExportFilter
+        = drawing::GraphicExportFilter::create(mxComponentContext);
+    // This resulted in a css::lang::IllegalArgumentException for a Writer
+    // picture.
+    xExportFilter->setSourceDocument(xShape);
+
+    utl::TempFile aTempFile;
+    aTempFile.EnableKillingFile();
+    uno::Sequence<beans::PropertyValue> aProperties(
+        comphelper::InitPropertySequence({ { "URL", uno::Any(aTempFile.GetURL()) },
+                                           { "MediaType", uno::Any(OUString("image/jpeg")) } }));
+    CPPUNIT_ASSERT(xExportFilter->filter(aProperties));
+}
+
+CPPUNIT_TEST_SUITE_REGISTRATION(UnodrawTest);
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
