6479217a710a0e3b4b0b2a60ef83edd530db6253
Rene Engelhard <rene@rene-engelhard.de>
2018-05-18 14:31:35 +0200

    remove Linux ("UNX") systray "Quickstarter"
    Change-Id: Ie0e8b8b7ad59ee640d6b195dfae1a7cf745056fd

--- sfx2/Library_sfx.mk
+++ sfx2/Library_sfx.mk
@@ -44,14 +44,6 @@ $(eval $(call gb_Library_add_defs,sfx,\
     $(if $(filter TRUE,$(ENABLE_CUPS)),-DENABLE_CUPS) \
 ))
 
-ifeq ($(ENABLE_SYSTRAY_GTK),TRUE)
-$(eval $(call gb_Library_add_defs,sfx,\
-    -DENABLE_QUICKSTART_APPLET \
-    -DENABLE_SYSTRAY_GTK \
-    -DPLUGIN_NAME=libqstart_gtk$(gb_Library_OOOEXT) \
-))
-endif
-
 $(eval $(call gb_Library_use_libraries,sfx,\
     basegfx \
     comphelper \
