de9620dd4539397715dab3102d256f9d634fcb23
Vasily Melenchuk <Vasily.Melenchuk@cib.de>
2018-05-21 10:58:49 +0300

    tdf#97231: potential crash fixed
    blob2 pointer can be invalidated during marshalString() call,
    because it also adds new element in data vector and thus later
    access to blob2 can cause crash.
    
    Change-Id: I2de519c363193f34b249e7250a016397b7420882
    Reviewed-on: https://gerrit.libreoffice.org/54613
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>

--- basic/source/runtime/dllmgr-x64.cxx
+++ basic/source/runtime/dllmgr-x64.cxx
@@ -319,12 +319,12 @@ ErrCode marshal(
                 break;
             case SbxSTRING:
                 {
-                    std::vector< char > * blob2 = data.newBlob();
                     void * p;
                     ErrCode e = marshalString(variable, special, data, &p);
                     if (e != ERRCODE_NONE) {
                         return e;
                     }
+                    std::vector< char >* blob2 = data.newBlob();
                     add(*blob2, p, 8, 0);
                     add(blob, address(*blob2), 8, offset);
                     break;
