50d8fbbe9e2dac476e24f2de6692d45f655e524f
Julien Nabet <serval2412@yahoo.fr>
2018-05-21 18:56:52 +0200

    tdf#117732: fix reading of time
    Change-Id: Ie67b50d899c4684f6fadd0c42337f54ef952078e

--- dbaccess/source/filter/hsqldb/rowinputbinary.cxx
+++ dbaccess/source/filter/hsqldb/rowinputbinary.cxx
@@ -333,7 +333,8 @@ std::vector<Any> HsqlRowInputStream::readOneRow(const ColumnTypeVector& nColType
             {
                 sal_Int64 value = 0;
                 m_pStream->ReadInt64(value);
-                css::util::Time time((value % 1000) * 1000000, value / 1000, 0, 0, true);
+                // no need to make more calculus since value is in nanosec
+                css::util::Time time(value, 0, 0, 0, true);
                 aData.push_back(makeAny(time));
             }
             break;
