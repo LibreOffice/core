LibreOffice/core

commit f7a011c45b0b88cb3c3ea9206cb851375266523d

loplugin:unusedfields in sc
Change-Id: I04aee9574b97d6120fe20be97b2c0fb8187fb260
Reviewed-on: https://gerrit.libreoffice.org/54453
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sc/source/filter/xcl97/XclExpChangeTrack.cxx
@@ -1409,9 +1409,7 @@ void EndXmlElement::SaveXml( XclExpXmlStream& rStrm )
 XclExpChangeTrack::XclExpChangeTrack( const XclExpRoot& rRoot ) :
     XclExpRoot( rRoot ),
     aActionStack(),
-    pTabIdBuffer( nullptr ),
-    pHeader( nullptr ),
-    bValidGUID( false )
+    pTabIdBuffer( nullptr )
 {
     OSL_ENSURE( GetOldRoot().pTabId, "XclExpChangeTrack::XclExpChangeTrack - root data incomplete" );
     if( !GetOldRoot().pTabId )
@@ -1449,7 +1447,7 @@ XclExpChangeTrack::XclExpChangeTrack( const XclExpRoot& rRoot ) :
     // build record list
     if (GetOutput() == EXC_OUTPUT_BINARY)
     {
-        pHeader = new XclExpChTrHeader;
+        XclExpChTrHeader* pHeader = new XclExpChTrHeader; // header record for last GUID
         maRecList.push_back( std::unique_ptr<ExcRecord>(pHeader) );
         maRecList.push_back( std::unique_ptr<ExcRecord>( new XclExpChTr0x0195 ) );
         maRecList.push_back( std::unique_ptr<ExcRecord>( new XclExpChTr0x0194( *pTempChangeTrack ) ) );
@@ -1458,6 +1456,7 @@ XclExpChangeTrack::XclExpChangeTrack( const XclExpRoot& rRoot ) :
         DateTime aLastDateTime( DateTime::EMPTY );
         sal_uInt32 nIndex = 1;
         sal_Int32 nLogNumber = 1;
+        sal_uInt8 aGUID[ 16 ]; // GUID for action info records
         while( !aActionStack.empty() )
         {
             XclExpChTrAction* pAction = aActionStack.top();
@@ -1467,6 +1466,7 @@ XclExpChangeTrack::XclExpChangeTrack( const XclExpRoot& rRoot ) :
                 (pAction->GetUsername() != sLastUsername) ||
                 (pAction->GetDateTime() != aLastDateTime) )
             {
+                bool bValidGUID;
                 lcl_GenerateGUID( aGUID, bValidGUID );
                 sLastUsername = pAction->GetUsername();
                 aLastDateTime = pAction->GetDateTime();
@@ -1494,6 +1494,7 @@ XclExpChangeTrack::XclExpChangeTrack( const XclExpRoot& rRoot ) :
         sal_uInt32 nIndex = 1;
         sal_Int32 nLogNumber = 1;
         XclExpXmlChTrHeader* pCurHeader = nullptr;
+        sal_uInt8 aGUID[ 16 ]; // GUID for action info records
 
         while (!aActionStack.empty())
         {
@@ -1504,6 +1505,7 @@ XclExpChangeTrack::XclExpChangeTrack( const XclExpRoot& rRoot ) :
                 (pAction->GetUsername() != sLastUsername) ||
                 (pAction->GetDateTime() != aLastDateTime) )
             {
+                bool bValidGUID;
                 lcl_GenerateGUID( aGUID, bValidGUID );
                 sLastUsername = pAction->GetUsername();
                 aLastDateTime = pAction->GetDateTime();

sc/inc/formulaiter.hxx
@@ -33,7 +33,6 @@ class ScFormulaCell;
 class ScDetectiveRefIter
 {
 private:
-    ScTokenArray* pCode;
     formula::FormulaTokenArrayPlainIterator maIter;
     ScAddress aPos;
 public:

sc/inc/funcdesc.hxx
@@ -249,7 +249,6 @@ public:
 private:
     ::std::vector<const ScFuncDesc*> aFunctionList; /**< List of functions */
     ::std::vector<const ScFuncDesc*>::iterator aFunctionListIter; /**< position in function list */
-    sal_Int32  nMaxFuncNameLen; /**< Length of longest function name */
 };
 
 /**

sc/source/core/data/formulaiter.cxx
@@ -26,8 +26,7 @@
 using namespace formula;
 
 ScDetectiveRefIter::ScDetectiveRefIter( ScFormulaCell* pCell ) :
-    pCode(pCell->GetCode()),
-    maIter(*pCode),
+    maIter(*pCell->GetCode()),
     aPos(pCell->aPos)
 {
 }

sc/source/core/data/funcdesc.cxx
@@ -414,8 +414,9 @@ bool ScFuncDesc::compareByName(const ScFuncDesc* a, const ScFuncDesc* b)
 
 // class ScFunctionList:
 ScFunctionList::ScFunctionList()
-    : nMaxFuncNameLen(0)
 {
+    sal_Int32  nMaxFuncNameLen = 0; // Length of longest function name
+
     // See ScFuncDescCore definition for format details.
     // This list must be sorted in order of the opcode, dbgutil builds enable _GLIBCXX_DEBUG
     // which will concept check that the list is sorted on first use to ensure this holds

sc/source/core/data/table3.cxx
@@ -259,7 +259,6 @@ private:
     std::unique_ptr<RowsType> mpRows; /// row-wise data table for sort by row operation.
 
     std::vector<std::unique_ptr<ScSortInfo[]>> mvppInfo;
-    SCSIZE          nCount;
     SCCOLROW        nStart;
     SCCOLROW        mnLastIndex; /// index of last non-empty cell position.
 
@@ -273,11 +272,12 @@ public:
 
     ScSortInfoArray( sal_uInt16 nSorts, SCCOLROW nInd1, SCCOLROW nInd2 ) :
         mvppInfo(nSorts),
-        nCount( nInd2 - nInd1 + 1 ), nStart( nInd1 ),
+        nStart( nInd1 ),
         mnLastIndex(nInd2),
         mbKeepQuery(false),
         mbUpdateRefs(false)
     {
+        SCSIZE nCount( nInd2 - nInd1 + 1 );
         if (nSorts)
         {
             for ( sal_uInt16 nSort = 0; nSort < nSorts; nSort++ )

sc/source/core/tool/scmatrix.cxx
@@ -1909,11 +1909,10 @@ struct ArrayMul
 template<typename Op>
 class MergeDoubleArrayFunc
 {
-    std::vector<double>& mrArray;
     std::vector<double>::iterator miPos;
     double mfNaN;
 public:
-    MergeDoubleArrayFunc(std::vector<double>& rArray) : mrArray(rArray), miPos(mrArray.begin())
+    MergeDoubleArrayFunc(std::vector<double>& rArray) : miPos(rArray.begin())
     {
         mfNaN = CreateDoubleError( FormulaError::ElementNaN);
     }

sc/source/filter/dif/difimp.cxx
@@ -225,12 +225,11 @@ ErrCode ScFormatFilterPluginImpl::ScImportDif(SvStream& rIn, ScDocument* pDoc, c
         return ERRCODE_NONE;
 }
 
-DifParser::DifParser( SvStream& rNewIn, const ScDocument& rDoc, rtl_TextEncoding e )
+DifParser::DifParser( SvStream& rNewIn, const ScDocument& rDoc, rtl_TextEncoding eCharSet )
     : fVal(0.0)
     , nVector(0)
     , nVal(0)
     , nNumFormat(0)
-    , eCharSet(e)
     , pNumFormatter(rDoc.GetFormatTable())
     , rIn(rNewIn)
 {

commit 11e968c0dbb6a4eb139a22bfb5241352c85a86dd

Conditional jump or move depends on uninitialised value
Regression introduced with f7a011c45b0b88cb3c3ea9206cb851375266523d
"loplugin:unusedfields in sc" e.g. during `VALGRIND=memcheck make
CppunitTest_sc_subsequent_export_test
CPPUNIT_TEST_NAME=ScExportTest::testTrackChangesSimpleXLSX`:

> ==16928== Conditional jump or move depends on uninitialised value(s)
> ==16928==    at 0x3C72975F: lcl_GenerateGUID(unsigned char*, bool&) (/sc/source/filter/xcl97/XclExpChangeTrack.cxx:87)
> ==16928==    by 0x3C731303: XclExpChangeTrack::XclExpChangeTrack(XclExpRoot const&) (/sc/source/filter/xcl97/XclExpChangeTrack.cxx:1470)
> ==16928==    by 0x3C18B6B1: ExcDocument::ReadDoc() (/sc/source/filter/excel/excdoc.cxx:788)
> ==16928==    by 0x3C1FC902: ExportBiff5::Write() (/sc/source/filter/excel/expop2.cxx:112)
> ==16928==    by 0x3C1A1506: lcl_ExportExcelBiff(SfxMedium&, ScDocument*, SvStream*, bool, unsigned short) (/sc/source/filter/excel/excel.cxx:181)
> ==16928==    by 0x3C1A1044: ScFormatFilterPluginImpl::ScExportExcel5(SfxMedium&, ScDocument*, ExportFormatExcel, unsigned short) (/sc/source/filter/excel/excel.cxx:219)
> ==16928==    by 0x2D947526: ScDocShell::ConvertTo(SfxMedium&) (/sc/source/ui/docshell/docsh.cxx:2325)
> ==16928==    by 0x2FA06539: SfxObjectShell::SaveTo_Impl(SfxMedium&, SfxItemSet const*) (/sfx2/source/doc/objstor.cxx:1510)
> ==16928==    by 0x2FA0A91D: SfxObjectShell::DoSaveAs(SfxMedium&) (/sfx2/source/doc/objstor.cxx:1897)
> ==16928==    by 0x2EFD30D7: ScBootstrapFixture::saveAndReload(ScDocShell*, rtl::OUString const&, rtl::OUString const&, rtl::OUString const&, SfxFilterFlags) (/sc/qa/unit/helper/qahelper.cxx:664)
> ==16928==    by 0x2EFD381D: ScBootstrapFixture::saveAndReload(ScDocShell*, int) (/sc/qa/unit/helper/qahelper.cxx:686)
> ==16928==    by 0x29A6ADEA: ScExportTest::testTrackChangesSimpleXLSX() (/sc/qa/unit/subsequent_export-test.cxx:2615)

Change-Id: I08f6a8e4a3827440f8b908791555e4b2226be12d

sc/source/filter/xcl97/XclExpChangeTrack.cxx
@@ -1457,6 +1457,7 @@ XclExpChangeTrack::XclExpChangeTrack( const XclExpRoot& rRoot ) :
         sal_uInt32 nIndex = 1;
         sal_Int32 nLogNumber = 1;
         sal_uInt8 aGUID[ 16 ]; // GUID for action info records
+        bool bValidGUID = false;
         while( !aActionStack.empty() )
         {
             XclExpChTrAction* pAction = aActionStack.top();
@@ -1466,7 +1467,6 @@ XclExpChangeTrack::XclExpChangeTrack( const XclExpRoot& rRoot ) :
                 (pAction->GetUsername() != sLastUsername) ||
                 (pAction->GetDateTime() != aLastDateTime) )
             {
-                bool bValidGUID;
                 lcl_GenerateGUID( aGUID, bValidGUID );
                 sLastUsername = pAction->GetUsername();
                 aLastDateTime = pAction->GetDateTime();
@@ -1495,6 +1495,7 @@ XclExpChangeTrack::XclExpChangeTrack( const XclExpRoot& rRoot ) :
         sal_Int32 nLogNumber = 1;
         XclExpXmlChTrHeader* pCurHeader = nullptr;
         sal_uInt8 aGUID[ 16 ]; // GUID for action info records
+        bool bValidGUID = false;
 
         while (!aActionStack.empty())
         {
@@ -1505,7 +1506,6 @@ XclExpChangeTrack::XclExpChangeTrack( const XclExpRoot& rRoot ) :
                 (pAction->GetUsername() != sLastUsername) ||
                 (pAction->GetDateTime() != aLastDateTime) )
             {
-                bool bValidGUID;
                 lcl_GenerateGUID( aGUID, bValidGUID );
                 sLastUsername = pAction->GetUsername();
                 aLastDateTime = pAction->GetDateTime();