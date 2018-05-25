e56423ec796dec9db636abd10017771151f5a35d
Dennis Francis <dennis.francis@collabora.co.uk>
2018-05-25 12:09:41 +0530

    Do not do threaded group calc if...
    the dependency computation resulted in a breach of
    recursion limit of MAXRECURSION. In such a case,
    the formula-group we are trying to thread is at the
    bottom of recursion guard stack and the dependencies
    (up the recursion guard stack) are yet to be computed,
    so the formula-group is not safe for threading.
    
    This patch fixes a crash in tdf#115034/1 when threading
    is enabled.
    
    Change-Id: I81737d3dd00f4e5cc782e9f030c18e36f6337a1b

--- sc/source/core/data/formulacell.cxx
+++ sc/source/core/data/formulacell.cxx
@@ -4360,8 +4360,9 @@ bool ScFormulaCell::InterpretFormulaGroup()
         return false;
 
     auto aScope = sc::FormulaLogger::get().enterGroup(*pDocument, *this);
+    ScRecursionHelper& rRecursionHelper = pDocument->GetRecursionHelper();
 
-    if (pDocument->GetRecursionHelper().GetRecursionCount())
+    if (rRecursionHelper.GetRecursionCount())
     {
         // Do not attempt to interpret a group when calculations are already
         // running, otherwise we may run into a circular reference hell. See
@@ -4398,7 +4399,7 @@ bool ScFormulaCell::InterpretFormulaGroup()
     // ScFormulaCell::InterpretFormulaGroup() must never be called through
     // anything else than ScFormulaCell::Interpret(), same as
     // ScFormulaCell::InterpretTail()
-    RecursionCounter aRecursionCounter( pDocument->GetRecursionHelper(), this);
+    RecursionCounter aRecursionCounter( rRecursionHelper, this);
 
     if (!bThreadingProhibited && !ScCalcConfig::isOpenCLEnabled() &&
         pCode->IsEnabledForThreading() &&
@@ -4412,7 +4413,16 @@ bool ScFormulaCell::InterpretFormulaGroup()
         // Disable or hugely enlarge subset for S/W group
         // threading interpreter
 
-        if (!aCalculator.DoIt())
+        bool bOKToThread = aCalculator.DoIt();
+
+        if (rRecursionHelper.IsInRecursionReturn())
+        {
+            mxGroup->meCalcState = sc::GroupCalcDisabled;
+            aScope.addMessage("Recursion limit reached, cannot thread this formula group now");
+            return false;
+        }
+
+        if (!bOKToThread)
         {
             mxGroup->meCalcState = sc::GroupCalcDisabled;
             aScope.addMessage("could not do new dependencies calculation thing");
@@ -4476,7 +4486,6 @@ bool ScFormulaCell::InterpretFormulaGroup()
             nThreadCount /= 2;
 
         SAL_INFO("sc.threaded", "Running " << nThreadCount << " threads");
-
         {
             assert(!pDocument->IsThreadedGroupCalcInProgress());
             pDocument->SetThreadedGroupCalcInProgress(true);
