4d8ac5ca4b08fa9c031f2dce87152e54317e040f
heiko tietze <tietze.heiko@gmail.com>
2018-05-25 11:45:29 +0200

    tdf#116916 - Icon themes missing on LibreOffice Vanilla from macOS appstore
    Update of blacklist for $WITH_THEMES
    Fallback to Tango for ancient/unknown DE, Colibre only on Windows
    MPL vs. non-MPL on macOS
    
    Change-Id: Ibea9e9429a79911d632b54fa4aa9649003830aa3

--- vcl/source/app/IconThemeSelector.cxx
+++ vcl/source/app/IconThemeSelector.cxx
@@ -11,12 +11,13 @@
 
 #include <vcl/IconThemeScanner.hxx>
 #include <vcl/IconThemeInfo.hxx>
+#include <config_mpl.h>
 
 #include <algorithm>
 
 namespace vcl {
 
-/*static*/ const OUStringLiteral IconThemeSelector::FALLBACK_ICON_THEME_ID("colibre");
+/*static*/ const OUStringLiteral IconThemeSelector::FALLBACK_ICON_THEME_ID("tango");
 
 namespace {
 
@@ -51,20 +52,31 @@ IconThemeSelector::IconThemeSelector()
 IconThemeSelector::GetIconThemeForDesktopEnvironment(const OUString& desktopEnvironment)
 {
     OUString r;
-    if ( desktopEnvironment.equalsIgnoreAsciiCase("kde4") ||
-         desktopEnvironment.equalsIgnoreAsciiCase("kde5") ||
-         desktopEnvironment.equalsIgnoreAsciiCase("macosx") ) {
-        r = "breeze";
-    }
-    else
-    if ( desktopEnvironment.equalsIgnoreAsciiCase("gnome") ||
-         desktopEnvironment.equalsIgnoreAsciiCase("mate") ||
-         desktopEnvironment.equalsIgnoreAsciiCase("unity") ) {
-        r = "elementary";
-    }
-    else {
-        r = FALLBACK_ICON_THEME_ID; //effective also on Windows
-    }
+    #ifdef WNT
+      r = "colibre";
+    #else
+      if ( desktopEnvironment.equalsIgnoreAsciiCase("kde4") ||
+           desktopEnvironment.equalsIgnoreAsciiCase("kde5") ) {
+          r = "breeze";
+      }
+      if ( desktopEnvironment.equalsIgnoreAsciiCase("macosx") ) {
+         #if MPL_HAVE_SUBSET
+           r = "sifr";
+         #else
+           r = "breeze";
+         #endif
+      }
+      else
+      if ( desktopEnvironment.equalsIgnoreAsciiCase("gnome") ||
+           desktopEnvironment.equalsIgnoreAsciiCase("mate") ||
+           desktopEnvironment.equalsIgnoreAsciiCase("unity") ) {
+          r = "elementary";
+      }
+      else {
+          r = FALLBACK_ICON_THEME_ID; //effective also on Windows
+      }
+    #endif
+
     return r;
 }
 
