LibreOffice/core

commit e87cc12eaf53efa9b221eae7167ea15bc7896752

tdf#106390 Intersect the table borders with upper frames.
Also removes dead code because SwTabFramePainter::Insert()
is always called with a cell frame and IsTabFrame() always
returns false.

Change-Id: I2505d876d20e44ded1faf760bc3b7b1d34b0fd8d
Reviewed-on: https://gerrit.libreoffice.org/54684
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>

sw/source/core/layout/paintfrm.cxx
@@ -2729,10 +2729,16 @@ void SwTabFramePainter::Insert( const SwFrame& rFrame, const SvxBoxItem& rBoxIte
 {
     // build 4 line entries for the 4 borders:
     SwRect aBorderRect = rFrame.getFrameArea();
-    if ( rFrame.IsTabFrame() )
+    // Frame area of a table might be larger than the containing frame
+    // so we have to intersect the border rect with upper frames til
+    // the first frame that is not part of a table.
+    const SwLayoutFrame *pUpper = rFrame.GetUpper();
+    while(pUpper)
     {
-        aBorderRect = rFrame.getFramePrintArea();
-        aBorderRect.Pos() += rFrame.getFrameArea().Pos();
+        aBorderRect.Intersection(pUpper->getFrameArea());
+        if (!pUpper->IsInTab())
+            break;
+        pUpper = pUpper->GetUpper();
     }
 
     bool const bBottomAsTop(lcl_IsFirstRowInFollowTableWithoutRepeatedHeadlines(

sw/qa/extras/layout/layout.cxx
@@ -19,6 +19,7 @@ public:
     void testTdf116830();
     void testTdf116925();
     void testTdf117028();
+    void testTdf106390();
     void testTdf116848();
     void testTdf117245();
 
@@ -26,6 +27,7 @@ public:
     CPPUNIT_TEST(testTdf116830);
     CPPUNIT_TEST(testTdf116925);
     CPPUNIT_TEST(testTdf117028);
+    CPPUNIT_TEST(testTdf106390);
     CPPUNIT_TEST(testTdf116848);
     CPPUNIT_TEST(testTdf117245);
     CPPUNIT_TEST_SUITE_END();
@@ -110,6 +112,24 @@ void SwLayoutWriter::testTdf117028()
 
     // Make sure the text is still rendered.
     assertXPathContent(pXmlDoc, "//textarray/text", "Hello");
+}
+
+void SwLayoutWriter::testTdf106390()
+{
+    SwDoc* pDoc = createDoc("tdf106390.odt");
+    SwDocShell* pShell = pDoc->GetDocShell();
+
+    // Dump the rendering of the first page as an XML file.
+    std::shared_ptr<GDIMetaFile> xMetaFile = pShell->GetPreviewMetaFile();
+    MetafileXmlDump dumper;
+    xmlDocPtr pXmlDoc = dumper.dumpAndParse(*xMetaFile);
+    CPPUNIT_ASSERT(pXmlDoc);
+    sal_Int32 nBottom = getXPath(pXmlDoc, "//sectrectclipregion", "bottom").toInt32();
+
+    // No end point of line segments shall go below the bottom of the clipping area.
+    const OString sXPath = "//polyline/point[@y>" + OString::number(nBottom) + "]";
+
+    assertXPath(pXmlDoc, sXPath, 0);
 }
 
 void SwLayoutWriter::testTdf116848()

commit 3df979118c2a5c6173cda4b318cb97e905e95380

tdf#106390: further improves sw table border handling.
This patch fix regression caused by e87cc12eaf53 to allow
table borders rendered in the left or right page margin.

Also add another test case (testTableExtrusion2) to assure
inner table can't have wider border than outer table.

Change-Id: I7a86b379dee08fb2f23385183a42d0ebde007370

sw/source/core/layout/paintfrm.cxx
@@ -2308,8 +2308,8 @@ class SwTabFramePainter
     const SwTabFrame& mrTabFrame;
 
     void Insert( SwLineEntry&, bool bHori );
-    void Insert( const SwFrame& rFrame, const SvxBoxItem& rBoxItem );
-    void HandleFrame( const SwLayoutFrame& rFrame );
+    void Insert(const SwFrame& rFrame, const SvxBoxItem& rBoxItem, const SwRect &rPaintArea);
+    void HandleFrame(const SwLayoutFrame& rFrame, const SwRect& rPaintArea);
     void FindStylesForLine( const Point&,
                             const Point&,
                             svx::frame::Style*,
@@ -2324,10 +2324,11 @@ public:
 SwTabFramePainter::SwTabFramePainter( const SwTabFrame& rTabFrame )
     : mrTabFrame( rTabFrame )
 {
-    HandleFrame( rTabFrame );
+    SwRect aPaintArea = rTabFrame.GetUpper()->GetPaintArea();
+    HandleFrame(rTabFrame, aPaintArea);
 }
 
-void SwTabFramePainter::HandleFrame( const SwLayoutFrame& rLayoutFrame )
+void SwTabFramePainter::HandleFrame(const SwLayoutFrame& rLayoutFrame, const SwRect& rPaintArea)
 {
     // Add border lines of cell frames. Skip covered cells. Skip cells
     // in special row span row, which do not have a negative row span:
@@ -2341,7 +2342,7 @@ void SwTabFramePainter::HandleFrame( const SwLayoutFrame& rLayoutFrame )
             SwBorderAttrAccess aAccess( SwFrame::GetCache(), &rLayoutFrame );
             const SwBorderAttrs& rAttrs = *aAccess.Get();
             const SvxBoxItem& rBox = rAttrs.GetBox();
-            Insert( rLayoutFrame, rBox );
+            Insert(rLayoutFrame, rBox, rPaintArea);
         }
     }
 
@@ -2351,7 +2352,7 @@ void SwTabFramePainter::HandleFrame( const SwLayoutFrame& rLayoutFrame )
     {
         const SwLayoutFrame* pLowerLayFrame = dynamic_cast<const SwLayoutFrame*>(pLower);
         if ( pLowerLayFrame && !pLowerLayFrame->IsTabFrame() )
-            HandleFrame( *pLowerLayFrame );
+            HandleFrame(*pLowerLayFrame, rPaintArea);
 
         pLower = pLower->GetNext();
     }
@@ -2725,21 +2726,12 @@ static bool lcl_IsFirstRowInFollowTableWithoutRepeatedHeadlines(
         && rBoxItem.GetBottom());
 }
 
-void SwTabFramePainter::Insert( const SwFrame& rFrame, const SvxBoxItem& rBoxItem )
+void SwTabFramePainter::Insert(const SwFrame& rFrame, const SvxBoxItem& rBoxItem, const SwRect& rPaintArea)
 {
     // build 4 line entries for the 4 borders:
     SwRect aBorderRect = rFrame.getFrameArea();
-    // Frame area of a table might be larger than the containing frame
-    // so we have to intersect the border rect with upper frames til
-    // the first frame that is not part of a table.
-    const SwLayoutFrame *pUpper = rFrame.GetUpper();
-    while(pUpper)
-    {
-        aBorderRect.Intersection(pUpper->getFrameArea());
-        if (!pUpper->IsInTab())
-            break;
-        pUpper = pUpper->GetUpper();
-    }
+
+    aBorderRect.Intersection(rPaintArea);
 
     bool const bBottomAsTop(lcl_IsFirstRowInFollowTableWithoutRepeatedHeadlines(
                 mrTabFrame, rFrame, rBoxItem));

sw/qa/extras/layout/layout.cxx
@@ -20,6 +20,8 @@ public:
     void testTdf116925();
     void testTdf117028();
     void testTdf106390();
+    void testTableExtrusion1();
+    void testTableExtrusion2();
     void testTdf116848();
     void testTdf117245();
 
@@ -28,6 +30,8 @@ public:
     CPPUNIT_TEST(testTdf116925);
     CPPUNIT_TEST(testTdf117028);
     CPPUNIT_TEST(testTdf106390);
+    CPPUNIT_TEST(testTableExtrusion1);
+    CPPUNIT_TEST(testTableExtrusion2);
     CPPUNIT_TEST(testTdf116848);
     CPPUNIT_TEST(testTdf117245);
     CPPUNIT_TEST_SUITE_END();
@@ -128,6 +132,43 @@ void SwLayoutWriter::testTdf106390()
 
     // No end point of line segments shall go below the bottom of the clipping area.
     const OString sXPath = "//polyline/point[@y>" + OString::number(nBottom) + "]";
+
+    assertXPath(pXmlDoc, sXPath, 0);
+}
+
+void SwLayoutWriter::testTableExtrusion1()
+{
+    SwDoc* pDoc = createDoc("table-extrusion1.odt");
+    SwDocShell* pShell = pDoc->GetDocShell();
+
+    // Dump the rendering of the first page as an XML file.
+    std::shared_ptr<GDIMetaFile> xMetaFile = pShell->GetPreviewMetaFile();
+    MetafileXmlDump dumper;
+    xmlDocPtr pXmlDoc = dumper.dumpAndParse(*xMetaFile);
+    CPPUNIT_ASSERT(pXmlDoc);
+    sal_Int32 nRight = getXPath(pXmlDoc, "//sectrectclipregion", "right").toInt32();
+
+    // Expect table borders in right page margin.
+    const OString sXPath = "//polyline/point[@x>" + OString::number(nRight) + "]";
+
+    assertXPath(pXmlDoc, sXPath, 4);
+}
+
+void SwLayoutWriter::testTableExtrusion2()
+{
+    SwDoc* pDoc = createDoc("table-extrusion2.odt");
+    SwDocShell* pShell = pDoc->GetDocShell();
+
+    // Dump the rendering of the first page as an XML file.
+    std::shared_ptr<GDIMetaFile> xMetaFile = pShell->GetPreviewMetaFile();
+    MetafileXmlDump dumper;
+    xmlDocPtr pXmlDoc = dumper.dumpAndParse(*xMetaFile);
+    CPPUNIT_ASSERT(pXmlDoc);
+    // End point position of the outer table.
+    sal_Int32 nX = getXPath(pXmlDoc, "(//polyline[1]/point)[2]", "x").toInt32();
+
+    // Do not allow inner table extrude outer table.
+    const OString sXPath = "//polyline/point[@x>" + OString::number(nX) + "]";
 
     assertXPath(pXmlDoc, sXPath, 0);
 }