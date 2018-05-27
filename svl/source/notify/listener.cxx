79ac607dfbd76e67e5018c57289b214a38b29316
Noel Grandin <noelgrandin@gmail.com>
2018-05-27 19:26:10 +0200

    WIP use std::vector for SvtListener
    std::unordered_set is less efficient for a set composed of pointers
    that is almost always zero or one sized.
    Keep the vector sorted to make removals and uniqueness checking
    cheaper.
    Instrumenting a build and running 'make check' says that the set size
    is always < 300 elements (and that seems to be mostly one use case in sc)
    
    Change-Id: I6e254f691892b1ee07515604456b5be3f03d38dd

--- svl/source/notify/listener.cxx
+++ svl/source/notify/listener.cxx
@@ -43,20 +43,19 @@ SvtListener::~SvtListener() COVERITY_NOEXCEPT_FALSE
 
 bool SvtListener::StartListening( SvtBroadcaster& rBroadcaster )
 {
-    std::pair<BroadcastersType::iterator, bool> r =
-        maBroadcasters.insert(&rBroadcaster);
-    if (r.second)
-    {
-        // This is a new broadcaster.
-        rBroadcaster.Add(this);
-    }
-    return r.second;
+    auto it = std::lower_bound(maBroadcasters.begin(), maBroadcasters.end(), &rBroadcaster);
+    if (it != maBroadcasters.end() && *it == &rBroadcaster)
+        return false;
+    // This is a new broadcaster.
+    maBroadcasters.insert(it, &rBroadcaster);
+    rBroadcaster.Add(this);
+    return true;
 }
 
 bool SvtListener::EndListening( SvtBroadcaster& rBroadcaster )
 {
-    BroadcastersType::iterator it = maBroadcasters.find(&rBroadcaster);
-    if (it == maBroadcasters.end())
+    auto it = std::lower_bound(maBroadcasters.begin(), maBroadcasters.end(), &rBroadcaster);
+    if (it == maBroadcasters.end() || *it != &rBroadcaster)
         // Not listening to this broadcaster.
         return false;
 
@@ -67,8 +66,8 @@ bool SvtListener::EndListening( SvtBroadcaster& rBroadcaster )
 
 void SvtListener::EndListeningAll()
 {
-    BroadcastersType::iterator it = maBroadcasters.begin();
-    BroadcastersType::const_iterator itEnd = maBroadcasters.end();
+    std::vector<SvtBroadcaster*>::iterator it = maBroadcasters.begin();
+    std::vector<SvtBroadcaster*>::const_iterator itEnd = maBroadcasters.end();
     for (; it != itEnd; ++it)
     {
         SvtBroadcaster& rBC = **it;
@@ -80,10 +79,9 @@ void SvtListener::EndListeningAll()
 
 void SvtListener::CopyAllBroadcasters( const SvtListener& r )
 {
-    BroadcastersType aCopy(r.maBroadcasters);
-    maBroadcasters.swap(aCopy);
-    BroadcastersType::iterator it = maBroadcasters.begin();
-    BroadcastersType::const_iterator itEnd = maBroadcasters.end();
+    maBroadcasters = r.maBroadcasters;
+    std::vector<SvtBroadcaster*>::iterator it = maBroadcasters.begin();
+    std::vector<SvtBroadcaster*>::const_iterator itEnd = maBroadcasters.end();
     for (; it != itEnd; ++it)
     {
         SvtBroadcaster* p = *it;
