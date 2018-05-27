3dd0105d1c237fd5af619d6153802b3549678c80
Julien Nabet <serval2412@yahoo.fr>
2018-05-27 21:07:28 +0200

    DBase: synchronize DB types known on Calc with those known on Base
    by adding 0x31, VisualFoxProAuto on array of valid marks in lcl_MayBeDBase
    See https://opengrok.libreoffice.org/xref/core/connectivity/source/inc/dbase/DTable.hxx#40
    and https://opengrok.libreoffice.org/xref/core/include/connectivity/dbtools.hxx#823
    
    Yes, it should be great to have only 1 enum
    
    Change-Id: Ic3fb74c838aab5e744cbe1d70ad8ca4ce2c00b8d
    Reviewed-on: https://gerrit.libreoffice.org/54894
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

--- sc/source/ui/unoobj/scdetect.cxx
+++ sc/source/ui/unoobj/scdetect.cxx
@@ -191,7 +191,7 @@ static bool lcl_MayBeDBase( SvStream& rStream )
     // Look for dbf marker, see connectivity/source/inc/dbase/DTable.hxx
     // DBFType for values.
     const sal_uInt8 nValidMarks[] = {
-        0x03, 0x04, 0x05, 0x30, 0x43, 0xB3, 0x83, 0x8b, 0x8e, 0xf5 };
+        0x03, 0x04, 0x05, 0x30, 0x31, 0x43, 0xB3, 0x83, 0x8b, 0x8e, 0xf5 };
     sal_uInt8 nMark;
     rStream.Seek(STREAM_SEEK_TO_BEGIN);
     rStream.ReadUChar( nMark );
