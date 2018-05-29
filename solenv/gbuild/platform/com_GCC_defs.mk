359da8455b9ca28d420d38e6b741201c4eda0ee5
Michael Stahl <Michael.Stahl@cib.de>
2018-05-29 11:29:31 +0200

    gbuild: avoid -Wunused-macros with clang and icecream
    Apparently the preprocessor leaves in the #define but expands the uses,
    so all macros are reported as unused.
    
    Reviewed-on: https://gerrit.libreoffice.org/54992
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Michael Stahl <Michael.Stahl@cib.de>
    (cherry picked from commit a404d5525256e3a59948ec4a1894e70179b7d783)
    
    gbuild: avoid -Wunused-macros with clang and icecream+ccache
    
    On Fedora, the recommended way to use icecream with ccache is to set
    CCACHE_PREFIX=icecc - but then $CC does not indicate that icecream is
    used.
    
    Reviewed-on: https://gerrit.libreoffice.org/56815
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <Michael.Stahl@cib.de>
    (cherry picked from commit fa503091cce61b0288645efeeab0937b11fe5403)
    
    gbuild: don't use -Wunused-macros with sccache
    
    In at least soltools, jurt and gperf generated files, build with GCC and
    sccache 0.2.16-alpha.0 reports spurious -Werror=implicit-fallthrough=
    due to comments and these go away by configuring sccache with
    rewrite_includes_only = true.
    
    But his results in
    cc1: error: ‘-fdirectives-only’ is incompatible with ‘-Wunused-macros’
    so disable that like for clang-with-icecream builds.
    
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/111716
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
    (cherry picked from commit 6fb9f368c6824a8ff7bc5bc5cf66fc2df7d055b7)
    
    Change-Id: I6dec38e86aa6e22591d7a700a8daddf3fed88b16
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/113086
    Tested-by: Michael Stahl <michael.stahl@allotropia.de>
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

--- solenv/gbuild/platform/com_GCC_defs.mk
+++ solenv/gbuild/platform/com_GCC_defs.mk
@@ -55,7 +55,7 @@ gb_CFLAGS_COMMON := \
 	-Wstrict-prototypes \
 	-Wundef \
 	-Wunreachable-code \
-	-Wunused-macros \
+	$(if $(or $(and $(COM_IS_CLANG),$(or $(findstring icecc,$(CC)),$(findstring icecc,$(CCACHE_PREFIX)))),$(findstring sccache,$(CC))),,-Wunused-macros) \
 	-finput-charset=UTF-8 \
 	-fmessage-length=0 \
 	-fno-common \
@@ -69,7 +69,7 @@ gb_CXXFLAGS_COMMON := \
 	-Wextra \
 	-Wundef \
 	-Wunreachable-code \
-	-Wunused-macros \
+	$(if $(or $(and $(COM_IS_CLANG),$(or $(findstring icecc,$(CXX)),$(findstring icecc,$(CCACHE_PREFIX)))),$(findstring sccache,$(CXX))),,-Wunused-macros) \
 	-finput-charset=UTF-8 \
 	-fmessage-length=0 \
 	-fno-common \
