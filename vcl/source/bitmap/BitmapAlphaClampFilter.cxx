8b0ded99e5f8d71c9b763db264ac445b5f665234
Noel Grandin <noel.grandin@collabora.co.uk>
2018-05-30 15:06:39 +0200

    move some CanvasHelper::implDrawBitmap inside vcl
    part of making mask and alpha internal details of Bitmap/BitmapEx
    
    Change-Id: I87ca24af18a29f5eb8a5761c5d95ae2806d97e77
    Reviewed-on: https://gerrit.libreoffice.org/55078
    Reviewed-by: Michael Meeks <michael.meeks@collabora.com>
    Tested-by: Jenkins <ci@libreoffice.org>

---
+++ vcl/source/bitmap/BitmapAlphaClampFilter.cxx
@@ -0,0 +1,47 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#include <vcl/bitmap.hxx>
+#include <vcl/bitmapex.hxx>
+#include <vcl/BitmapAlphaClampFilter.hxx>
+#include <vcl/bitmapaccess.hxx>
+
+#include <bitmapwriteaccess.hxx>
+
+BitmapEx BitmapAlphaClampFilter::execute(BitmapEx const& rBitmapEx)
+{
+    if (!rBitmapEx.IsTransparent())
+        return rBitmapEx;
+
+    AlphaMask aBitmapAlpha(rBitmapEx.GetAlpha());
+    {
+        AlphaScopedWriteAccess pWriteAlpha(aBitmapAlpha);
+        const Size aSize(rBitmapEx.GetSizePixel());
+
+        for (long nY = 0; nY < aSize.Height(); ++nY)
+        {
+            Scanline pScanAlpha = pWriteAlpha->GetScanline(nY);
+
+            for (long nX = 0; nX < aSize.Width(); ++nX)
+            {
+                BitmapColor aBitmapAlphaValue(pWriteAlpha->GetPixelFromData(pScanAlpha, nX));
+                if (aBitmapAlphaValue.GetIndex() > mcThreshold)
+                {
+                    aBitmapAlphaValue.SetIndex(255);
+                    pWriteAlpha->SetPixelOnData(pScanAlpha, nX, aBitmapAlphaValue);
+                }
+            }
+        }
+    }
+
+    return BitmapEx(rBitmapEx.GetBitmap(), aBitmapAlpha);
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
