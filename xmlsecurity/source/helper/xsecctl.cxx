89ad980588127cd65f51978c822f1a22cdc1dd99
Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
2018-05-30 15:13:20 +0200

    Move signature line to signed properties
    So that a modification will result in an invalid signature.
    
    Change-Id: Ib40e0b941d56e29c134993e1d41f496aa183bd7f

--- xmlsecurity/source/helper/xsecctl.cxx
+++ xmlsecurity/source/helper/xsecctl.cxx
@@ -40,7 +40,6 @@
 #include <xmloff/attrlist.hxx>
 #include <rtl/math.hxx>
 #include <rtl/ref.hxx>
-#include <svx/xoutbmp.hxx>
 #include <unotools/datetime.hxx>
 #include <comphelper/ofopxmlhelper.hxx>
 #include <sax/tools/converter.hxx>
@@ -857,66 +856,6 @@ void XSecController::exportSignature(
                         "dc:date");
                 }
                 xDocumentHandler->endElement( "SignatureProperty" );
-
-                if (!signatureInfo.ouSignatureLineId.isEmpty()
-                    && signatureInfo.aValidSignatureImage.is()
-                    && signatureInfo.aInvalidSignatureImage.is())
-                {
-                    pAttributeList = new SvXMLAttributeList();
-                    pAttributeList->AddAttribute(
-                        "xmlns:loext",
-                        "urn:org:documentfoundation:names:experimental:office:xmlns:loext:1.0");
-                    pAttributeList->AddAttribute("Target", "#" + signatureInfo.ouSignatureId);
-
-                    xDocumentHandler->startElement(
-                        "SignatureProperty",
-                        cssu::Reference<cssxs::XAttributeList>(pAttributeList));
-                    {
-                        xDocumentHandler->startElement(
-                            "loext:SignatureLine",
-                            cssu::Reference<cssxs::XAttributeList>(new SvXMLAttributeList()));
-
-                        {
-                            // Write SignatureLineId element
-                            xDocumentHandler->startElement(
-                                "loext:SignatureLineId",
-                                cssu::Reference<cssxs::XAttributeList>(new SvXMLAttributeList()));
-                            xDocumentHandler->characters(signatureInfo.ouSignatureLineId);
-                            xDocumentHandler->endElement("loext:SignatureLineId");
-                        }
-
-                        {
-                            // Write SignatureLineId element
-                            xDocumentHandler->startElement(
-                                "loext:SignatureLineValidImage",
-                                cssu::Reference<cssxs::XAttributeList>(new SvXMLAttributeList()));
-
-                            OUString aGraphicInBase64;
-                            Graphic aGraphic(signatureInfo.aValidSignatureImage);
-                            if (!XOutBitmap::GraphicToBase64(aGraphic, aGraphicInBase64, false))
-                                SAL_WARN("xmlsecurity.helper", "could not convert graphic to base64");
-
-                            xDocumentHandler->characters(aGraphicInBase64);
-                            xDocumentHandler->endElement("loext:SignatureLineValidImage");
-                        }
-
-                        {
-                            // Write SignatureLineId element
-                            xDocumentHandler->startElement(
-                                "loext:SignatureLineInvalidImage",
-                                cssu::Reference<cssxs::XAttributeList>(new SvXMLAttributeList()));
-                            OUString aGraphicInBase64;
-                            Graphic aGraphic(signatureInfo.aInvalidSignatureImage);
-                            if (!XOutBitmap::GraphicToBase64(aGraphic, aGraphicInBase64, false))
-                                SAL_WARN("xmlsecurity.helper", "could not convert graphic to base64");
-                            xDocumentHandler->characters(aGraphicInBase64);
-                            xDocumentHandler->endElement("loext:SignatureLineInvalidImage");
-                        }
-
-                        xDocumentHandler->endElement("loext:SignatureLine");
-                    }
-                    xDocumentHandler->endElement("SignatureProperty");
-                }
             }
 
             // Write signature description.
