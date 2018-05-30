e76c31f9400eb2337874bc045b15abd1c98530ce
László Németh <nemeth@numbertext.org>
2018-05-30 17:28:17 +0200

    tdf#115007: add import/export to NatNum12 (number names)
    supporting all the libnumbertext library functions by
    using loext:transliteration-formula to store these function
    names (ordinal, ordinal-number, ordinal-feminine, etc.
    currency codes, also their possible combinations,
    depending from the given libnumbertext language module).
    
    Change-Id: I20f93c9d16778f142067a56d53b336d0acbe2d92

--- xmloff/source/core/xmltoken.cxx
+++ xmloff/source/core/xmltoken.cxx
@@ -3308,6 +3308,9 @@ namespace xmloff { namespace token {
         TOKEN( "newline",                         XML_NEWLINE ),
         TOKEN( "creator-initials",                XML_CREATOR_INITIALS ),
 
+        // tdf#115007 to spell out numbers, currencies, dates and cross references
+        TOKEN( "transliteration-formula",         XML_TRANSLITERATION_FORMULA ),
+
 #if OSL_DEBUG_LEVEL > 0
         { 0, nullptr, nullptr,                       XML_TOKEN_END }
 #else
