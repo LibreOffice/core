0e63a045dd29355b2afaa77312f7c6f122df63c5
Nickson Thanda <nicksonthanda10@msn.com>
2018-06-03 22:35:14 +0100

    tdf#114875 Added an option to not show "No offline help installed" popup
    Change-Id: I8067c0c374af57f5ad8e6f5fffd3a9783521824f

--- cui/source/options/optgdlg.cxx
+++ cui/source/options/optgdlg.cxx
@@ -260,6 +260,7 @@ OfaMiscTabPage::OfaMiscTabPage(vcl::Window* pParent, const SfxItemSet& rSet)
     : SfxTabPage(pParent, "OptGeneralPage", "cui/ui/optgeneralpage.ui", &rSet)
 {
     get(m_pExtHelpCB, "exthelp");
+    get(m_pPopUpHelpCB,"popuphelp");
     if (!lcl_HasSystemFilePicker())
         get<VclContainer>("filedlgframe")->Hide();
 #if ! ENABLE_GTK
@@ -324,6 +325,7 @@ void OfaMiscTabPage::dispose()
     m_pCollectUsageInfo.clear();
     m_pQuickStarterFrame.clear();
     m_pQuickLaunchCB.clear();
+    m_pPopUpHelpCB.clear();
     SfxTabPage::dispose();
 }
 
@@ -338,6 +340,8 @@ bool OfaMiscTabPage::FillItemSet( SfxItemSet* rSet )
     std::shared_ptr<comphelper::ConfigurationChanges> batch(comphelper::ConfigurationChanges::create());
 
     SvtHelpOptions aHelpOptions;
+    if ( m_pPopUpHelpCB->IsChecked() != (m_pPopUpHelpCB->GetSavedValue() == TRISTATE_TRUE))
+        aHelpOptions.SetOfflineHelpPopUp( m_pPopUpHelpCB->IsChecked());
     if ( m_pExtHelpCB->IsChecked() != (m_pExtHelpCB->GetSavedValue() == TRISTATE_TRUE) )
         aHelpOptions.SetExtendedHelp( m_pExtHelpCB->IsChecked() );
 
@@ -392,7 +396,8 @@ void OfaMiscTabPage::Reset( const SfxItemSet* rSet )
     SvtHelpOptions aHelpOptions;
     m_pExtHelpCB->Check( aHelpOptions.IsHelpTips() && aHelpOptions.IsExtendedHelp() );
     m_pExtHelpCB->SaveValue();
-
+    m_pPopUpHelpCB->Check( aHelpOptions.IsOfflineHelpPopUp());
+    m_pPopUpHelpCB->SaveValue();
     SvtMiscOptions aMiscOpt;
     m_pFileDlgCB->Check( !aMiscOpt.UseSystemFileDialog() );
     m_pFileDlgCB->SaveValue();
