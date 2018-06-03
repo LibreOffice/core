LibreOffice/core

commit 2a39163aef4211c9d19cb1faee7f55d3718355b6

Rework code to avoid getTokenCount/getToken
Change-Id: I46985c3edda9509f0a014ea351016dce599bda4a

vcl/unx/gtk/fpicker/SalGtkFilePicker.cxx
@@ -41,7 +41,6 @@
 #include <com/sun/star/ui/dialogs/TemplateDescription.hpp>
 #include <com/sun/star/ui/dialogs/ControlActions.hpp>
 #include <com/sun/star/uno/Any.hxx>
-#include <comphelper/string.hxx>
 #include <unx/gtk/gtkdata.hxx>
 #include <unx/gtk/gtkinst.hxx>
 
@@ -705,16 +704,23 @@ namespace
 
 bool lcl_matchFilter( const rtl::OUString& rFilter, const rtl::OUString& rExt )
 {
-    const int nCount = comphelper::string::getTokenCount( rFilter, ';' );
+    const sal_Int32 nBegin = rFilter.indexOf(rExt);
 
-    for ( int n = 0; n != nCount; ++n )
-    {
-        const rtl::OUString aToken = rFilter.getToken( n, ';' );
-        if ( aToken == rExt )
-            return true;
-    }
+    if (nBegin<0) // not found
+        return false;
 
-    return false;
+    const sal_Unicode cSep{';'};
+
+    // Check if the found occurrence is an exact match: left side
+    if (nBegin>0 && rFilter[nBegin-1]!=cSep)
+        return false;
+
+    // Check if the found occurrence is an exact match: right side
+    const sal_Int32 nEnd = nBegin + rExt.getLength();
+    if (nEnd<rFilter.getLength() && rFilter[nEnd]!=cSep)
+        return false;
+
+    return true;
 }
 
 }

commit 3c78a19ed66b44ebb2db7b160fa92a010a3c42aa

Try harder to find a matching file extension
Potential regression from 2a39163aef4211c9d19cb1faee7f55d3718355b6

Change-Id: I67f1f11bd52a1dbf0f77a35df7ad556437ccd39b

vcl/unx/gtk/fpicker/SalGtkFilePicker.cxx
@@ -705,23 +705,32 @@ namespace
 
 bool lcl_matchFilter( const rtl::OUString& rFilter, const rtl::OUString& rExt )
 {
-    const sal_Int32 nBegin = rFilter.indexOf(rExt);
+    const sal_Unicode cSep {';'};
+    sal_Int32 nIdx {0};
 
-    if (nBegin<0) // not found
-        return false;
+    for (;;)
+    {
+        const sal_Int32 nBegin = rFilter.indexOf(rExt, nIdx);
 
-    const sal_Unicode cSep{';'};
+        if (nBegin<0) // not found
+            break;
 
-    // Check if the found occurrence is an exact match: left side
-    if (nBegin>0 && rFilter[nBegin-1]!=cSep)
-        return false;
+        // Let nIdx point to end of matched string, useful in order to
+        // check string boundaries and also for a possible next iteration
+        nIdx = nBegin + rExt.getLength();
 
-    // Check if the found occurrence is an exact match: right side
-    const sal_Int32 nEnd = nBegin + rExt.getLength();
-    if (nEnd<rFilter.getLength() && rFilter[nEnd]!=cSep)
-        return false;
+        // Check if the found occurrence is an exact match: right side
+        if (nIdx<rFilter.getLength() && rFilter[nIdx]!=cSep)
+            continue;
 
-    return true;
+        // Check if the found occurrence is an exact match: left side
+        if (nBegin>0 && rFilter[nBegin-1]!=cSep)
+            continue;
+
+        return true;
+    }
+
+    return false;
 }
 
 }