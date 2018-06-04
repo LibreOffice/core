5ff7ab890b3a4f6eeee33566c6d0900b9f9fd4d5
Miklos Vajna <vmiklos@collabora.co.uk>
2018-06-04 21:25:38 +0200

    libxmlsec: drop not needed xmlsec1-customkeymanage.patch.1
    This was added in commit ebd1b95bb5f9235d1dba1b840fd746c9b53320d2
    (INTEGRATION: CWS xmlsec08 (1.1.2); FILE ADDED; 2005-03-10) without any
    real commit message to explain why this is necessary.
    
    system-xmlsec (not containing this patch) works fine for our XML signing
    purposes with software certificates, and just recently I learned that
    even hardware-based certificates work fine without this patch, so it can
    go away.
    
    I assume this was a refactor to allow some new feature as a next step,
    but that feature was never implemented.
    
    Reviewed-on: https://gerrit.libreoffice.org/55296
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
    (cherry picked from commit f06004c03bbd076767a570180b7fc239064713e6)
    
    Conflicts:
    	external/xmlsec/UnpackedTarball_xmlsec.mk
    
    Change-Id: I31639230483cd34b14b35fd41b4fcd8284476138

--- external/xmlsec/UnpackedTarball_xmlsec.mk
+++ external/xmlsec/UnpackedTarball_xmlsec.mk
@@ -11,7 +11,6 @@ xmlsec_patches :=
 xmlsec_patches += xmlsec1-configure.patch.1
 xmlsec_patches += xmlsec1-vc.patch.1
 xmlsec_patches += xmlsec1-1.2.14_fix_extern_c.patch.1
-xmlsec_patches += xmlsec1-customkeymanage.patch.1
 
 $(eval $(call gb_UnpackedTarball_UnpackedTarball,xmlsec))
 
