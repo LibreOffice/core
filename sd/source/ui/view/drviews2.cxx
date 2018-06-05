92f0922f320d7cb1cb1e735d2c20dd4f1ec0705d
Caolán McNamara <caolanm@redhat.com>
2018-06-05 10:59:23 +0100

    tdf#117940 setting resize shape while text edit active not recorded in undo
    Change-Id: I0887a1caea96f3efe897c304cde0520d59da6c59
    Reviewed-on: https://gerrit.libreoffice.org/55326
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
    (cherry picked from commit 66b4e7ee179cba8ec2f1229ebc6eb48343fae72a)

--- sd/source/ui/view/drviews2.cxx
+++ sd/source/ui/view/drviews2.cxx
@@ -1650,6 +1650,8 @@ void DrawViewShell::FuTemporary(SfxRequest& rReq)
 
         case SID_TEXTATTR_DLG:
         {
+            if (mpDrawView->IsTextEdit())
+                mpDrawView->SdrEndTextEdit();
             SetCurrentFunction( FuTextAttrDlg::Create( this, GetActiveWindow(), mpDrawView, GetDoc(), rReq ) );
             Cancel();
             rReq.Ignore ();
