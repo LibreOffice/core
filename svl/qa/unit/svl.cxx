deabdfb385315388433bb97242dab38d3f273740
László Németh <nemeth@numbertext.org>
2018-06-07 14:26:42 +0200

    tdf#115007 add NatNum12 formatting to dates
    to support variants of preposition, suffixation,
    article or their combinations. For example, Catalan
    "de març"/"d'abril", English "1st of May"/"First of
    May", Hungarian "május 1-je/május 2-a" (May 1/2) or
    Turkish "2018'de/2019'da" (in 2018/2019) usage can
    be automatic in templates and mail merge.
    
    When the date format contains more than a date keyword,
    it needs to specify in the NatNum12 argument which date
    element has got special formatting (described by
    libnumbertext formatting codes, and an optional
    initial capitalize/upper/title argument):
    
    '[NatNum12 ordinal-number]D'              -> "1st"
    '[NatNum12 D=ordinal-number]D" of "MMMM'  -> "1st of April"
    '[NatNum12 D=ordinal]D" of "MMMM'         -> "first of April"
    '[NatNum12 YYYY=title year,D=capitalize ordinal]
     D"of "MMMM", "YYYY' -> "First of April, Nineteen Ninety"
    
    Note: set only for YYYY, MMMM, M, DDDD, D and NNN/AAAA
    in date formats. It's possible to extend this for other
    keywords and date + time combinations, according to
    the possible language requirements.
    
    Note 2: l10n date formats can use the new NatNum12 date
    formats from formatindex=60, see FormatElement in
    i18npool/source/localedata/ XML files and FormatElement
    specification:
    https://opengrok.libreoffice.org/xref/core/i18npool/source/localedata/data/locale.dtd#223
    
    Change-Id: I598849f1492f4012e83cef9293773badbff16206
    Reviewed-on: https://gerrit.libreoffice.org/55613
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: László Németh <nemeth@numbertext.org>

--- svl/qa/unit/svl.cxx
+++ svl/qa/unit/svl.cxx
@@ -1403,6 +1403,15 @@ void Test::testUserDefinedNumberFormats()
         sCode = "[NatNum12 upper ordinal-number]0";
         sExpected = "12345TH";
         checkPreviewString(aFormatter, sCode, 12345, eLang, sExpected);
+        sCode = "[NatNum12 D=ordinal-number]D\" of \"MMMM";
+        sExpected = "2nd of January";
+        checkPreviewString(aFormatter, sCode, M_PI, eLang, sExpected);
+        sCode = "[NatNum12 D=ordinal-number,YYYY=year]D\" of \"MMMM\", \"YYYY";
+        sExpected = "2nd of January, nineteen hundred";
+        checkPreviewString(aFormatter, sCode, M_PI, eLang, sExpected);
+        sCode = "[NatNum12 YYYY=year, D=ordinal]D\" of \"MMMM\", \"YYYY";
+        sExpected = "second of January, nineteen hundred";
+        checkPreviewString(aFormatter, sCode, M_PI, eLang, sExpected);
 #endif
     }
     {  // tdf#105968 engineering format with value rounded up to next magnitude
