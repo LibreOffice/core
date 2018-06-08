LibreOffice/core

commit b11188835d3b87cd9d2a8cdb3da204cfda5d3e6e

DOC import: lazy-read images
At least JPEG files are now only loaded when the user scrolls to the
relevant page.

Also fix the root cause of the EMF lazy-read problem and remove the
previous workarounds.

Change-Id: I9699927282b99bcb71a0d271a20bbfd56a361ee8
Reviewed-on: https://gerrit.libreoffice.org/53219
Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
Tested-by: Jenkins <ci@libreoffice.org>

filter/source/msfilter/msdffimp.cxx
@@ -6545,7 +6545,14 @@ bool SvxMSDffManager::GetBLIPDirect( SvStream& rBLIPStream, Graphic& rData, tool
         else
         {   // and unleash our filter
             GraphicFilter& rGF = GraphicFilter::GetGraphicFilter();
-            nRes = rGF.ImportGraphic( rData, "", *pGrStream );
+            Graphic aGraphic = rGF.ImportUnloadedGraphic(*pGrStream);
+            if (aGraphic)
+            {
+                rData = aGraphic;
+                nRes = ERRCODE_NONE;
+            }
+            else
+                nRes = rGF.ImportGraphic( rData, "", *pGrStream );
 
             // SJ: I40472, sometimes the aspect ratio (aMtfSize100) does not match and we get scaling problems,
             // then it is better to use the prefsize that is stored within the metafile. Bug #72846# for what the

oox/source/helper/graphichelper.cxx
@@ -244,7 +244,7 @@ Reference< XGraphic > GraphicHelper::importGraphic( const Reference< XInputStrea
         aArgs[ 1 ].Name = "LazyRead";
         aArgs[ 1 ].Value <<= true;
 
-        if ( pExtHeader )
+        if ( pExtHeader && pExtHeader->mapMode > 0 )
         {
             aArgs.realloc( aArgs.getLength() + 1 );
             Sequence< PropertyValue > aFilterData( 3 );
@@ -341,11 +341,6 @@ Reference< XGraphic > GraphicHelper::importEmbeddedGraphic( const OUString& rStr
         EmbeddedGraphicMap::const_iterator aIt = maEmbeddedGraphics.find( rStreamName );
         if( aIt == maEmbeddedGraphics.end() )
         {
-            // TODO make lazy-load work for EMF as well.
-            WmfExternal aHeader;
-            if (rStreamName.endsWith(".emf") && !pExtHeader)
-                pExtHeader = &aHeader;
-
             xGraphic = importGraphic(mxStorage->openInputStream(rStreamName), pExtHeader);
             if( xGraphic.is() )
                 maEmbeddedGraphics[ rStreamName ] = xGraphic;

sw/CppunitTest_sw_ww8import.mk
@@ -22,6 +22,7 @@ $(eval $(call gb_CppunitTest_use_libraries,sw_ww8import, \
     sal \
     test \
     unotest \
+    vcl \
     sfx \
     sw \
 	utl \

sw/qa/extras/ww8import/ww8import.cxx
@@ -116,6 +116,14 @@ DECLARE_WW8IMPORT_TEST( testTdf105570, "tdf105570.doc" )
     CPPUNIT_ASSERT_EQUAL( sal_uInt16(0), pTableNd->GetTable().GetRowsToRepeat() );
 }
 
+DECLARE_OOXMLIMPORT_TEST(testImageLazyRead, "image-lazy-read.doc")
+{
+    auto xGraphic = getProperty<uno::Reference<graphic::XGraphic>>(getShape(1), "Graphic");
+    Graphic aGraphic(xGraphic);
+    // This failed, import loaded the graphic, it wasn't lazy-read.
+    CPPUNIT_ASSERT(!aGraphic.isAvailable());
+}
+
 DECLARE_WW8IMPORT_TEST(testTdf106799, "tdf106799.doc")
 {
     sal_Int32 const nCellWidths[3][4] = { { 9530, 0, 0, 0 },{ 2382, 2382, 2382, 2384 },{ 2382, 2382, 2382, 2384 } };

vcl/source/filter/graphicfilter.cxx
@@ -1572,6 +1572,7 @@ Graphic GraphicFilter::ImportUnloadedGraphic(SvStream& rIStream)
             nGraphicContentSize = nStreamLength;
             pGraphicContent.reset(new sal_uInt8[nGraphicContentSize]);
 
+            rIStream.Seek(nStreamBegin);
             rIStream.ReadBytes(pGraphicContent.get(), nStreamLength);
 
             if (!rIStream.GetError())

vcl/source/graphic/UnoGraphicProvider.cxx
@@ -355,9 +355,6 @@ uno::Reference< ::graphic::XGraphic > SAL_CALL GraphicProvider::queryGraphic( co
         }
     }
 
-    if (bLazyRead && aFilterData.hasElements())
-        bLazyRead = false;
-
     SolarMutexGuard g;
 
     if( xIStm.is() )
@@ -396,7 +393,10 @@ uno::Reference< ::graphic::XGraphic > SAL_CALL GraphicProvider::queryGraphic( co
             aExtHeader.mapMode = nExtMapMode;
             WmfExternal *pExtHeader = nullptr;
             if ( nExtMapMode > 0 )
+            {
                 pExtHeader = &aExtHeader;
+                bLazyRead = false;
+            }
 
             ErrCode error = ERRCODE_NONE;
             if (bLazyRead)

commit dcfa1269033035eb6b88f8c522c52db4c8ea6f5d

tdf#118037 PPT import: fix lost crop of graphic
Regression from commit b11188835d3b87cd9d2a8cdb3da204cfda5d3e6e (DOC
import: lazy-read images, 2018-04-20).

Change-Id: Ie2155c971d94153352a9372a9795cbb9d820dd40

filter/source/msfilter/msdffimp.cxx
@@ -3719,6 +3719,9 @@ static void lcl_ApplyCropping( const DffPropSet& rPropSet, SfxItemSet* pSet, Gra
         BitmapEx    aCropBitmap;
         sal_uInt32  nTop( 0 ),  nBottom( 0 ), nLeft( 0 ), nRight( 0 );
 
+        // Cropping has to be applied on a loaded graphic.
+        rGraf.makeAvailable();
+
         if ( pSet ) // use crop attributes ?
             aCropSize = lcl_GetPrefSize(rGraf, MapMode(MapUnit::Map100thMM));
         else

sd/qa/unit/import-tests.cxx
@@ -75,6 +75,7 @@
 #include <com/sun/star/table/XTableRows.hpp>
 #include <com/sun/star/style/NumberingType.hpp>
 #include <com/sun/star/frame/Desktop.hpp>
+#include <com/sun/star/text/GraphicCrop.hpp>
 
 #include <stlpool.hxx>
 #include <comphelper/processfactory.hxx>
@@ -187,6 +188,7 @@ public:
 
     bool checkPattern(sd::DrawDocShellRef const & rDocRef, int nShapeNumber, std::vector<sal_uInt8>& rExpected);
     void testPatternImport();
+    void testPptCrop();
 
     CPPUNIT_TEST_SUITE(SdImportTest);
 
@@ -268,6 +270,7 @@ public:
     CPPUNIT_TEST(testTdf116899);
     CPPUNIT_TEST(testTdf77747);
     CPPUNIT_TEST(testTdf116266);
+    CPPUNIT_TEST(testPptCrop);
 
     CPPUNIT_TEST_SUITE_END();
 };
@@ -630,6 +633,24 @@ void SdImportTest::testFdo68594()
     CPPUNIT_ASSERT_MESSAGE( "no color item", pC != nullptr);
     // Color should be black
     CPPUNIT_ASSERT_EQUAL_MESSAGE( "Placeholder color mismatch", sal_uInt32(0), sal_uInt32(pC->GetValue()) );
+
+    xDocShRef->DoClose();
+}
+
+void SdImportTest::testPptCrop()
+{
+    sd::DrawDocShellRef xDocShRef
+        = loadURL(m_directories.getURLFromSrc("/sd/qa/unit/data/ppt/crop.ppt"), PPT);
+
+    uno::Reference<beans::XPropertySet> xPropertySet(
+        getShapeFromPage(/*nShape=*/1, /*nPage=*/0, xDocShRef));
+    text::GraphicCrop aCrop;
+    xPropertySet->getPropertyValue("GraphicCrop") >>= aCrop;
+    // These were all 0, lazy-loading broke cropping.
+    CPPUNIT_ASSERT_GREATER(static_cast<sal_Int32>(0), aCrop.Top);
+    CPPUNIT_ASSERT_GREATER(static_cast<sal_Int32>(0), aCrop.Bottom);
+    CPPUNIT_ASSERT_GREATER(static_cast<sal_Int32>(0), aCrop.Left);
+    CPPUNIT_ASSERT_GREATER(static_cast<sal_Int32>(0), aCrop.Right);
 
     xDocShRef->DoClose();
 }