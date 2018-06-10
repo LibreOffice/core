c590153eaafff0d332cde9c5f22ffe306fa5d817
Rene Engelhard <rene@debian.org>
2018-06-10 10:33:46 +0200

    fix CppunitTest_dbaccess_hsqlbinary_import
    at least with system-hsqldb.
    
    add missing connectivity/source/drivers/hsqldb/hsqldb to
    gb_CppunitTest_use_components
    
    Change-Id: I6e845dadd67276b137734f68ba922a1d5890c809

--- dbaccess/CppunitTest_dbaccess_hsqlbinary_import.mk
+++ dbaccess/CppunitTest_dbaccess_hsqlbinary_import.mk
@@ -69,6 +69,7 @@ $(eval $(call gb_CppunitTest_use_components,dbaccess_hsql_binary_import,\
     basic/util/sb \
     comphelper/util/comphelp \
     configmgr/source/configmgr \
+    connectivity/source/drivers/hsqldb/hsqldb \
     connectivity/source/drivers/firebird/firebird_sdbc \
     connectivity/source/manager/sdbc2 \
     dbaccess/util/dba \
