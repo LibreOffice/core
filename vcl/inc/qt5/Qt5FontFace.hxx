812cae2f0b96c3d95d267b83727dd5a2114e611a
Kacper Kasper <kacperkasper@gmail.com>
2018-06-10 05:59:31 +0200

    qt5: hold LogicalFontInstance with rtl::Reference
    Build fix after c4c56de1b0e62ec866b519b2b24c5e805f0a86d3.
    
    Change-Id: I49303528ccbe49894a0fbae3c24d3f3d08c016eb
    Reviewed-on: https://gerrit.libreoffice.org/55547
    Tested-by: Jenkins <ci@libreoffice.org>
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

--- vcl/inc/qt5/Qt5FontFace.hxx
+++ vcl/inc/qt5/Qt5FontFace.hxx
@@ -47,7 +47,8 @@ public:
     bool GetFontCapabilities(vcl::FontCapabilities& rFontCapabilities) const;
     bool HasChar(sal_uInt32 cChar) const;
 
-    LogicalFontInstance* CreateFontInstance(const FontSelectPattern& rFSD) const override;
+    rtl::Reference<LogicalFontInstance>
+    CreateFontInstance(const FontSelectPattern& rFSD) const override;
 
 protected:
     Qt5FontFace(const Qt5FontFace&);
