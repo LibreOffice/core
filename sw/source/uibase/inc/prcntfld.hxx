8570288319c806bce218f73978c7435c31bc4103
Caolán McNamara <caolanm@redhat.com>
2018-06-11 21:18:57 +0100

    weld SwTableColumnPage
    Change-Id: I2e92de5899bdee7c5a8a7c29c3a0f407cbb5a1d3

--- sw/source/uibase/inc/prcntfld.hxx
+++ sw/source/uibase/inc/prcntfld.hxx
@@ -124,6 +124,7 @@ public:
     bool has_focus() const { return m_pField->has_focus(); }
     void save_value() { m_pField->save_value(); }
     bool get_value_changed_from_saved() const { return m_pField->get_value_changed_from_saved(); }
+    void set_text(const OUString& rStr) { m_pField->set_text(rStr); }
     void SetMetricFieldMin(int nNewMin) { m_pField->set_min(nNewMin, FUNIT_NONE); }
     void SetMetricFieldMax(int nNewMax) { m_pField->set_max(nNewMax, FUNIT_NONE); }
 
