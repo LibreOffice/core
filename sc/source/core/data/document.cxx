fd5082c756ac0faec79fe8b4c0aaafc9e6fac90a
Luboš Luňák <l.lunak@collabora.com>
2018-06-11 10:28:28 +0200

    move SetNumberFormat() calls out of calc threads
    As it modifies ScAttrArray, which is not thread-safe (has std::vector
    per each column, so multiple threads may try resize it etc.). So if
    threaded calculation is done, delay the calls to the main thread.
    
    Change-Id: I3d87665c0dd0d40f0c2efbcf8958240ee5580233
    Reviewed-on: https://gerrit.libreoffice.org/55602
    Reviewed-by: Michael Meeks <michael.meeks@collabora.com>
    Tested-by: Jenkins
    (cherry picked from commit dc046b7367bf2bcb5b3b883731723da7c92f4cca)

--- sc/source/core/data/document.cxx
+++ sc/source/core/data/document.cxx
@@ -3693,6 +3693,7 @@ sal_uInt32 ScDocument::GetNumberFormat( const ScInterpreterContext& rContext, co
 
 void ScDocument::SetNumberFormat( const ScAddress& rPos, sal_uInt32 nNumberFormat )
 {
+    assert(!IsThreadedGroupCalcInProgress());
     SCTAB nTab = rPos.Tab();
     if (!TableExists(nTab))
         return;
@@ -6752,6 +6753,17 @@ void ScDocumentThreadSpecific::SetupFromNonThreadedData(const ScDocumentThreadSp
 void ScDocumentThreadSpecific::MergeBackIntoNonThreadedData(ScDocumentThreadSpecific& /*rNonThreadedData*/)
 {
     // What about recursion helper and lookup cache?
+}
+
+void ScDocument::MergeBackIntoNonThreadedContext(ScInterpreterContext& threadedContext)
+{
+    // Move data from a context used by a calculation thread to the main thread's context.
+    // Called from the main thread after the calculation thread has already finished.
+    assert(!IsThreadedGroupCalcInProgress());
+    maInterpreterContext.maDelayedSetNumberFormat.insert(
+        maInterpreterContext.maDelayedSetNumberFormat.end(),
+        std::make_move_iterator(threadedContext.maDelayedSetNumberFormat.begin()),
+        std::make_move_iterator(threadedContext.maDelayedSetNumberFormat.end()));
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
