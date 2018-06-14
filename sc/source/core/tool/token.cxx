LibreOffice/core

commit fb4ad5b0c766440e1a47d991c6cbc9cb017ada59

do not even consider ScTokenArray for OpenCL if it's disabled
If sc/qa/unit/data/ods/functions.ods is run with OpenCL disabled
(and the mnOpenCLMinimumFormulaGroupSize check disabled too), then
ScTokenArray::CheckToken() considers the token for OpenCL, and if
the token is ocExternal, it passes all the related OpenCL checks,
and thus ScTokenArray::CheckForThreading() is never called.
However, since OpenCL is disabled, then actually the threaded code
will be executed, since the blacklisting for ocExternal has not been
done.

Change-Id: Ib4d4d63d3925746f6e13da0611ac869e9a094b5b
Reviewed-on: https://gerrit.libreoffice.org/55676
Reviewed-by: Michael Meeks <michael.meeks@collabora.com>
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Tor Lillqvist <tml@collabora.com>

sc/source/core/tool/token.cxx
@@ -1820,9 +1820,18 @@ void ScTokenArray::GenHash()
 
 void ScTokenArray::ResetVectorState()
 {
-    meVectorState = FormulaVectorEnabled;
-    mbOpenCLEnabled = true;
-    mbThreadingEnabled = !ScCalcConfig::isOpenCLEnabled() && ScCalcConfig::isThreadingEnabled();
+    if(ScCalcConfig::isOpenCLEnabled())
+    {
+        meVectorState = FormulaVectorEnabled;
+        mbOpenCLEnabled = true;
+        mbThreadingEnabled = false;
+    }
+    else
+    {
+        meVectorState = FormulaVectorDisabled;
+        mbOpenCLEnabled = false;
+        mbThreadingEnabled = ScCalcConfig::isThreadingEnabled();
+    }
 }
 
 bool ScTokenArray::IsFormulaVectorDisabled() const
@@ -1888,11 +1897,9 @@ bool ScTokenArray::IsValidReference( ScRange& rRange, const ScAddress& rPos ) co
 
 ScTokenArray::ScTokenArray() :
     FormulaTokenArray(),
-    mnHashValue(0),
-    meVectorState(FormulaVectorEnabled),
-    mbOpenCLEnabled(true),
-    mbThreadingEnabled(!ScCalcConfig::isOpenCLEnabled() && ScCalcConfig::isThreadingEnabled())
+    mnHashValue(0)
 {
+    ResetVectorState();
 }
 
 ScTokenArray::~ScTokenArray()
@@ -1930,9 +1937,7 @@ bool ScTokenArray::EqualTokens( const ScTokenArray* pArr2) const
 void ScTokenArray::Clear()
 {
     mnHashValue = 0;
-    meVectorState = FormulaVectorEnabled;
-    mbOpenCLEnabled = true;
-    mbThreadingEnabled = !ScCalcConfig::isOpenCLEnabled() && ScCalcConfig::isThreadingEnabled();
+    ResetVectorState();
     FormulaTokenArray::Clear();
 }
 

commit c3b4188e231ddee82b635df38d37756c9a364c71

do not effectively disable Calc's SoftwareInterpreter
Broken by fb4ad5b0c766440. I find the code unclear about the exact intention
of the settings, but it appears that ScCalcConfig's isOpenCLEnabled() and
isSwInterpreterEnabled() are two independent settings and SwInterpreter
just acts as a fallback to normal OpenCL. And, according to how code
in ScFormulaCell::InterpretFormulaGroup() handles it, SwInterpreter
has the lowest priority.

Change-Id: I8d779bbf55480d5c2c9a0918ab0b7ec74079c451

sc/source/core/tool/token.cxx
@@ -1820,7 +1820,12 @@ void ScTokenArray::GenHash()
 
 void ScTokenArray::ResetVectorState()
 {
-    if(ScCalcConfig::isOpenCLEnabled())
+    // The preference order is (according to ScFormulaCell::InterpretFormulaGroup()):
+    // - OpenCL
+    // - threading
+    // - SoftwareInterpreter
+    if(ScCalcConfig::isOpenCLEnabled()
+        || (!ScCalcConfig::isThreadingEnabled() && ScCalcConfig::isSwInterpreterEnabled()))
     {
         meVectorState = FormulaVectorEnabled;
         mbOpenCLEnabled = true;