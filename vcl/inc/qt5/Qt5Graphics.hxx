3c2423eaab0138d2940d055ab70fcc1292c0d62b
Stephan Bergmann <sbergman@redhat.com>
2018-06-14 16:08:10 +0200

    Adapt --enable-qt5
    ...to 8447d31e529985ef7fc71933f0e55685530f9fc9 "return SalBitmap using
    std::shared_ptr"
    
    Change-Id: I7ef70eda5c2f5931b421852d938ae54d6e133e6d

--- vcl/inc/qt5/Qt5Graphics.hxx
+++ vcl/inc/qt5/Qt5Graphics.hxx
@@ -120,7 +120,8 @@ public:
     virtual void drawMask(const SalTwoRect& rPosAry, const SalBitmap& rSalBitmap,
                           Color nMaskColor) override;
 
-    virtual SalBitmap* getBitmap(long nX, long nY, long nWidth, long nHeight) override;
+    virtual std::shared_ptr<SalBitmap> getBitmap(long nX, long nY, long nWidth,
+                                                 long nHeight) override;
     virtual Color getPixel(long nX, long nY) override;
 
     virtual void invert(long nX, long nY, long nWidth, long nHeight, SalInvert nFlags) override;
