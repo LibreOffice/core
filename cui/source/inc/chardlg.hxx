f05b00948ca041f4f467d837448c16053dda4655
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2018-06-15 20:08:18 +0200

    tdf#58941 Manipulate with font features using FontFeatureDialog
    This adds a font feature dialog activated on the Character page,
    which adds a way to manipulate the available font features for the
    current selected font.
    
    Change-Id: Icd0eda31b235d3cc8ef6eaee582fb78b4b346d78

--- cui/source/inc/chardlg.hxx
+++ cui/source/inc/chardlg.hxx
@@ -26,6 +26,7 @@
 #include <svx/colorbox.hxx>
 #include <svx/langbox.hxx>
 #include <vcl/layout.hxx>
+#include <vcl/button.hxx>
 #include <memory>
 
 // forward ---------------------------------------------------------------
@@ -82,6 +83,7 @@ private:
     VclPtr<FixedText>          m_pWestFontLanguageFT;
     VclPtr<SvxLanguageComboBox> m_pWestFontLanguageLB;
     VclPtr<FixedText>          m_pWestFontTypeFT;
+    VclPtr<PushButton>         m_pWestFontFeaturesButton;
 
     VclPtr<VclContainer>       m_pEastFrame;
     VclPtr<FixedText>          m_pEastFontNameFT;
@@ -93,6 +95,7 @@ private:
     VclPtr<FixedText>          m_pEastFontLanguageFT;
     VclPtr<SvxLanguageBox>     m_pEastFontLanguageLB;
     VclPtr<FixedText>          m_pEastFontTypeFT;
+    VclPtr<PushButton>         m_pEastFontFeaturesButton;
 
     VclPtr<VclContainer>       m_pCTLFrame;
     VclPtr<FixedText>          m_pCTLFontNameFT;
@@ -104,6 +107,7 @@ private:
     VclPtr<FixedText>          m_pCTLFontLanguageFT;
     VclPtr<SvxLanguageBox>     m_pCTLFontLanguageLB;
     VclPtr<FixedText>          m_pCTLFontTypeFT;
+    VclPtr<PushButton>         m_pCTLFontFeaturesButton;
 
     std::unique_ptr<SvxCharNamePage_Impl>   m_pImpl;
 
@@ -137,6 +141,8 @@ private:
     DECL_LINK( FontModifyEditHdl_Impl, Edit&, void );
     DECL_LINK( FontModifyListBoxHdl_Impl, ListBox&, void );
     DECL_LINK( FontModifyComboBoxHdl_Impl, ComboBox&, void );
+    DECL_LINK(FontFeatureButtonClicked, Button*, void);
+
     void FontModifyHdl_Impl(void const *);
 
 public:
