c754f0bf48c5e9b4f85c22bf2f12e181a1976422
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2018-06-15 18:50:00 +0200

    tdf#58941 implement retrieving font features and structures
    This adds the following:
    - Retrieving of features using HarfBuzz
    - Retrieving of feature definitions from graphite fonts
    - Adds description texts of OpenType features
    - Tests for retrieving features (Linux Libertine G graphite font)
      and making sure the features and definitions are what we
      expect.
    
    Change-Id: I51573fe086e59c6228cb546bbfac95ac53824c47

---
+++ vcl/inc/font/OpenTypeFeatureDefinitonList.hxx
@@ -0,0 +1,47 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef INCLUDED_VCL_INC_FONT_OPENTYPEFEATUREDEFINITONLIST_HXX
+#define INCLUDED_VCL_INC_FONT_OPENTYPEFEATUREDEFINITONLIST_HXX
+
+#include <vcl/dllapi.h>
+#include <vcl/font/Feature.hxx>
+#include <rtl/instance.hxx>
+#include <memory>
+#include <vector>
+#include <unordered_map>
+
+namespace vcl
+{
+namespace font
+{
+class VCL_DLLPUBLIC OpenTypeFeatureDefinitonListPrivate
+{
+private:
+    std::vector<FeatureDefinition> m_aFeatureDefinition;
+    std::unordered_map<sal_uInt32, size_t> m_aCodeToIndex;
+
+    void init();
+
+public:
+    OpenTypeFeatureDefinitonListPrivate();
+    FeatureDefinition getDefinition(sal_uInt32 nFeatureCode);
+};
+
+class VCL_DLLPUBLIC OpenTypeFeatureDefinitonList
+    : public rtl::Static<OpenTypeFeatureDefinitonListPrivate, OpenTypeFeatureDefinitonList>
+{
+};
+
+} // end font namespace
+} // end vcl namespace
+
+#endif // INCLUDED_VCL_INC_FONT_OPENTYPEFEATUREDEFINITONLIST_HXX
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
---
+++ vcl/source/font/FeatureCollector.cxx
@@ -0,0 +1,141 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <font/FeatureCollector.hxx>
+#include <font/OpenTypeFeatureDefinitonList.hxx>
+#include <o3tl/make_unique.hxx>
+
+#include <hb-ot.h>
+#include <hb-graphite2.h>
+
+namespace vcl
+{
+namespace font
+{
+bool FeatureCollector::collectGraphiteFeatureDefinition(vcl::font::Feature& rFeature)
+{
+    gr_face* grFace = hb_graphite2_face_get_gr_face(m_pHbFace);
+    if (grFace == nullptr)
+        return false;
+
+    bool bFound = false;
+
+    gr_uint16 nUILanguage = 0x0409;
+
+    gr_uint16 nNumberOfFeatures = gr_face_n_fref(grFace);
+    gr_feature_val* pFeatures = gr_face_featureval_for_lang(grFace, rFeature.m_aID.m_aLanguageCode);
+
+    if (pFeatures)
+    {
+        for (gr_uint16 i = 0; i < nNumberOfFeatures; ++i)
+        {
+            const gr_feature_ref* pFeatureRef = gr_face_fref(grFace, i);
+            gr_uint32 nFeatureCode = gr_fref_id(pFeatureRef);
+
+            if (nFeatureCode == rFeature.m_aID.m_aFeatureCode)
+            {
+                gr_uint32 nLabelLength = 0;
+                void* pLabel = gr_fref_label(pFeatureRef, &nUILanguage, gr_utf8, &nLabelLength);
+                OUString sLabel(OUString::createFromAscii(static_cast<char*>(pLabel)));
+                gr_label_destroy(pLabel);
+
+                std::vector<vcl::font::FeatureParameter> aParameters;
+                gr_uint16 nNumberOfValues = gr_fref_n_values(pFeatureRef);
+                for (gr_uint16 j = 0; j < nNumberOfValues; ++j)
+                {
+                    if (gr_fref_value(pFeatureRef, j))
+                    {
+                        gr_uint32 nValueLabelLength = 0;
+                        void* pValueLabel = gr_fref_value_label(pFeatureRef, j, &nUILanguage,
+                                                                gr_utf8, &nValueLabelLength);
+                        OUString sValueLabel(
+                            OUString::createFromAscii(static_cast<char*>(pValueLabel)));
+                        aParameters.emplace_back(sal_uInt32(j), sValueLabel);
+                        gr_label_destroy(pValueLabel);
+                    }
+                }
+
+                auto eFeatureParameterType = vcl::font::FeatureParameterType::ENUM;
+
+                // Check if the parameters are boolean
+                if (aParameters.size() == 1 && aParameters[0].getDescription() == "True")
+                {
+                    eFeatureParameterType = vcl::font::FeatureParameterType::BOOL;
+                    aParameters.clear();
+                }
+                rFeature.m_aDefinition = vcl::font::FeatureDefinition(
+                    rFeature.m_aID.m_aFeatureCode, sLabel, eFeatureParameterType, aParameters);
+                bFound = true;
+            }
+        }
+    }
+    gr_featureval_destroy(pFeatures);
+    return bFound;
+}
+
+void FeatureCollector::collectForLanguage(hb_tag_t aTableTag, sal_uInt32 nScript,
+                                          hb_tag_t aScriptTag, sal_uInt32 nLanguage,
+                                          hb_tag_t aLanguageTag)
+{
+    sal_uInt32 nFeatureCount = hb_ot_layout_language_get_feature_tags(
+        m_pHbFace, aTableTag, nScript, nLanguage, 0, nullptr, nullptr);
+    std::vector<hb_tag_t> aFeatureTags(nFeatureCount);
+    hb_ot_layout_language_get_feature_tags(m_pHbFace, aTableTag, nScript, nLanguage, 0,
+                                           &nFeatureCount, aFeatureTags.data());
+    aFeatureTags.resize(nFeatureCount);
+
+    for (hb_tag_t aFeatureTag : aFeatureTags)
+    {
+        m_rFontFeatures.emplace_back();
+        vcl::font::Feature& rFeature = m_rFontFeatures.back();
+        rFeature.m_aID = { aFeatureTag, aScriptTag, aLanguageTag };
+        bool bResult = collectGraphiteFeatureDefinition(rFeature);
+        if (!bResult)
+        {
+            FeatureDefinition aDefinition
+                = OpenTypeFeatureDefinitonList::get().getDefinition(aFeatureTag);
+            if (aDefinition)
+            {
+                rFeature.m_aDefinition = vcl::font::FeatureDefinition(aDefinition);
+            }
+        }
+    }
+}
+
+void FeatureCollector::collectForScript(hb_tag_t aTableTag, sal_uInt32 nScript, hb_tag_t aScriptTag)
+{
+    collectForLanguage(aTableTag, nScript, aScriptTag, HB_OT_LAYOUT_DEFAULT_LANGUAGE_INDEX,
+                       HB_OT_TAG_DEFAULT_LANGUAGE);
+
+    sal_uInt32 nLanguageCount
+        = hb_ot_layout_script_get_language_tags(m_pHbFace, aTableTag, nScript, 0, nullptr, nullptr);
+    std::vector<hb_tag_t> aLanguageTags(nLanguageCount);
+    hb_ot_layout_script_get_language_tags(m_pHbFace, aTableTag, nScript, 0, &nLanguageCount,
+                                          aLanguageTags.data());
+    aLanguageTags.resize(nLanguageCount);
+    for (sal_uInt32 nLanguage = 0; nLanguage < nLanguageCount; ++nLanguage)
+        collectForLanguage(aTableTag, nScript, aScriptTag, nLanguage, aLanguageTags[nLanguage]);
+}
+
+void FeatureCollector::collectForTable(hb_tag_t aTableTag)
+{
+    sal_uInt32 nScriptCount
+        = hb_ot_layout_table_get_script_tags(m_pHbFace, aTableTag, 0, nullptr, nullptr);
+    std::vector<hb_tag_t> aScriptTags(nScriptCount);
+    hb_ot_layout_table_get_script_tags(m_pHbFace, aTableTag, 0, &nScriptCount, aScriptTags.data());
+    aScriptTags.resize(nScriptCount);
+
+    for (sal_uInt32 nScript = 0; nScript < nScriptCount; ++nScript)
+        collectForScript(aTableTag, nScript, aScriptTags[nScript]);
+}
+
+} // end namespace font
+} // end namespace vcl
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
