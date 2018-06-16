f220bce256a799e0df45e2419f59e8714b9972e9
Maxim Monastirsky <momonasmon@gmail.com>
2018-06-17 00:10:27 +0300

    gtk3: Correctly mirror popovers in rtl ui
    e.g. formula prompt or fill series in Calc. The reason is that for
    this particular vcl::Window, HasMirroredGraphics() != IsRTLEnabled().
    
    Change-Id: Icd4bb2d22ba77d64c32b243d9c07745824e5e558
    Reviewed-on: https://gerrit.libreoffice.org/55936
    Tested-by: Jenkins
    Reviewed-by: Maxim Monastirsky <momonasmon@gmail.com>
    (cherry picked from commit 6fcc60b49215acb28edac46bb605767840abd122)
    Reviewed-on: https://gerrit.libreoffice.org/56384

--- vcl/inc/salframe.hxx
+++ vcl/inc/salframe.hxx
@@ -246,13 +246,13 @@ public:
     }
 
     // return !0 to indicate popovers are shown natively, 0 otherwise
-    virtual void*           ShowPopover(const OUString& /*rHelpText*/, const tools::Rectangle& /*rHelpArea*/, QuickHelpFlags /*nFlags*/)
+    virtual void*           ShowPopover(const OUString& /*rHelpText*/, vcl::Window* /*pParent*/, const tools::Rectangle& /*rHelpArea*/, QuickHelpFlags /*nFlags*/)
     {
         return nullptr;
     }
 
     // return true to indicate popovers are shown natively, false otherwise
-    virtual bool            UpdatePopover(void* /*nId*/, const OUString& /*rHelpText*/, const tools::Rectangle& /*rHelpArea*/)
+    virtual bool            UpdatePopover(void* /*nId*/, const OUString& /*rHelpText*/, vcl::Window* /*pParent*/, const tools::Rectangle& /*rHelpArea*/)
     {
         return false;
     }
