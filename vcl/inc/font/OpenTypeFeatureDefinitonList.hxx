1f163ed5780f3dd4516a1c28fb5e41dc30f9e26e
Khaled Hosny <khaledhosny@eglug.org>
2018-06-17 15:48:14 +0200

    Skip required features from the features dialog
    Many OpenType features are required features that are applied to certain
    context determined by the layout engine, or should always be on. There
    is no value in presenting these features to the user and they just
    clutter the dialog for fonts that usually have many of such features
    (e.g. Arabic or Indic fonts).
    
    Change-Id: Idc3d7d3a9eb1c4f693ea6fdf9f26f0264b910f91

--- vcl/inc/font/OpenTypeFeatureDefinitonList.hxx
+++ vcl/inc/font/OpenTypeFeatureDefinitonList.hxx
@@ -26,12 +26,14 @@ class VCL_DLLPUBLIC OpenTypeFeatureDefinitonListPrivate
 private:
     std::vector<FeatureDefinition> m_aFeatureDefinition;
     std::unordered_map<sal_uInt32, size_t> m_aCodeToIndex;
+    std::vector<sal_uInt32> m_aRequiredFeatures;
 
     void init();
 
 public:
     OpenTypeFeatureDefinitonListPrivate();
     FeatureDefinition getDefinition(sal_uInt32 nFeatureCode);
+    bool isRequired(sal_uInt32 nFeatureCode);
 };
 
 class VCL_DLLPUBLIC OpenTypeFeatureDefinitonList
