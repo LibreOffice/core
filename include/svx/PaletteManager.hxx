f849f8ceec354f94a6ff5bdaf496d315760a7a90
Caolán McNamara <caolanm@redhat.com>
2018-06-18 21:30:10 +0100

    weld SvxColorTabPage
    Change-Id: I5dc6f949edcb34aa110dfa9415e2ac886d0dfa4c

--- include/svx/PaletteManager.hxx
+++ include/svx/PaletteManager.hxx
@@ -56,7 +56,9 @@ public:
     PaletteManager& operator=(const PaletteManager&) = delete;
     void        LoadPalettes();
     void        ReloadColorSet(SvxColorValueSet& rColorSet);
+    void        ReloadColorSet(ColorValueSet& rColorSet);
     void        ReloadRecentColorSet(SvxColorValueSet& rColorSet);
+    void        ReloadRecentColorSet(ColorValueSet& rColorSet);
     std::vector<OUString> GetPaletteList();
     void        SetPalette( sal_Int32 nPos );
     sal_Int32   GetPalette();
