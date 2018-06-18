489840b595b159c3dccc15818e073e2a42fc3e62
Jens Carl <j.carl43@gmx.de>
2018-06-18 03:03:45 +0000

    Remove obsolete (cargo-cult copied) dependencies
    Change-Id: Ibf4de00b63579aff9faf4cae657824a81c1c80dc
    Reviewed-on: https://gerrit.libreoffice.org/55976
    Tested-by: Jenkins
    Reviewed-by: Jens Carl <j.carl43@gmx.de>

--- sc/CppunitTest_sc_spreadsheetsettings.mk
+++ sc/CppunitTest_sc_spreadsheetsettings.mk
@@ -18,43 +18,14 @@ $(eval $(call gb_CppunitTest_add_exception_objects,sc_spreadsheetsettings, \
 ))
 
 $(eval $(call gb_CppunitTest_use_libraries,sc_spreadsheetsettings, \
-	basegfx \
-	comphelper \
 	cppu \
-	cppuhelper \
-	drawinglayer \
-	editeng \
-	for \
-	forui \
-	i18nlangtag \
-	msfilter \
-	oox \
 	sal \
-	salhelper \
-	sax \
-	sb \
-	sc \
-	sfx \
-	sot \
 	subsequenttest \
-	svl \
-	svt \
-	svx \
-	svxcore \
 	test \
-	tk \
-	tl \
-	ucbhelper \
 	unotest \
-	utl \
-	vbahelper \
-	vcl \
-	xo \
 ))
 
 $(eval $(call gb_CppunitTest_set_include,sc_spreadsheetsettings,\
-	-I$(SRCDIR)/sc/source/ui/inc \
-	-I$(SRCDIR)/sc/inc \
 	$$(INCLUDE) \
 ))
 
