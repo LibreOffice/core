LibreOffice/core

commit 9a6527a98fb968b3fe6bc293ff7520a9480d43d0

stringToDouble() do not parse separator without digit as 0.0
Occurred in CSV import without "detect special numbers" activated for data like
,.,
where the . dot resulted in a numeric cell value 0

Change-Id: Ie715d7a8ed02196b59968a92919ad286b3bedf64

sal/rtl/math.cxx
@@ -694,6 +694,7 @@ inline double stringToDouble(CharT const * pBegin, CharT const * pEnd,
         while (p != pEnd && (*p == CharT('0') || *p == cGroupSeparator))
             ++p;
 
+        CharT const * pFirstSignificant = p;
         long nValExp = 0;       // carry along exponent of mantissa
 
         // integer part of mantissa
@@ -741,7 +742,19 @@ inline double stringToDouble(CharT const * pBegin, CharT const * pEnd,
             if ( fFrac != 0.0 )
                 fVal += rtl::math::pow10Exp( fFrac, nFracExp );
             else if ( nValExp < 0 )
+            {
+                if (pFirstSignificant + 1 == p)
+                {
+                    // No digit at all, only separator(s) without integer or
+                    // fraction part. Bail out. No number. No error.
+                    if (pStatus != nullptr)
+                        *pStatus = eStatus;
+                    if (pParsedEnd != nullptr)
+                        *pParsedEnd = pBegin;
+                    return fVal;
+                }
                 nValExp = 0;    // no digit other than 0 after decimal point
+            }
         }
 
         if ( nValExp > 0 )

commit 1221fdb9dec4cbaabf1e8ca606f0d45267868c1e

Resolves: tdf#118073 one leading 0 is significant for 0.
Regression from

    commit 9a6527a98fb968b3fe6bc293ff7520a9480d43d0
    CommitDate: Mon Jun 27 21:57:52 2016 +0200

        stringToDouble() do not parse separator without digit as 0.0

Change-Id: I9d90aedc324ef0938297224297d89817e3fd1e90

sal/rtl/math.cxx
@@ -817,7 +817,7 @@ inline double stringToDouble(CharT const * pBegin, CharT const * pEnd,
             ++p;
         }
 
-        CharT const * pFirstSignificant = p;
+        CharT const * pFirstSignificant = ((p > pBegin && *(p-1) == CharT('0')) ? p-1 : p);
         long nValExp = 0;       // carry along exponent of mantissa
 
         // integer part of mantissa