e084f748ad476cdd64822afcc53fff26f82819a8
Caolán McNamara <caolanm@redhat.com>
2018-06-19 16:40:38 +0100

    weld SwWatermarkDialog
    Change-Id: Iff3ddfb4dd75088e39ea7675b085f1bbde2c2045

--- sw/qa/uitest/writer_tests/watermark.py
+++ sw/qa/uitest/writer_tests/watermark.py
@@ -40,8 +40,8 @@ class watermark(UITestCase):
         xTransparency = xDialog.getChild("Transparency")
 
         self.assertEqual(get_state_as_dict(xTextInput)["Text"], "Watermark")
-        self.assertEqual(get_state_as_dict(xAngle)["Text"], "46")
-        self.assertEqual(get_state_as_dict(xTransparency)["Text"], "51")
+        self.assertEqual(get_state_as_dict(xAngle)["Text"], "46°")
+        self.assertEqual(get_state_as_dict(xTransparency)["Text"], "51%")
 
         xCancBtn = xDialog.getChild("cancel")
         self.ui_test.close_dialog_through_button(xCancBtn)
