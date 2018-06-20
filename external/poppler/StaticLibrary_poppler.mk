df46a8beb12639b0cdf6a23d2c1107ffd7f7c86c
Michael Stahl <Michael.Stahl@cib.de>
2018-06-20 16:10:44 +0200

    poppler: upgrade to release 0.66.0
    Unfortunately the autotools build system was removed in 0.60.0, and the
    CMake one is a bit overeager in linking against system libraries that
    happen to be installed on the build machine, and we currently don't
    require CMake anyway, so avoid the problems by using a gbuild makefile
    and a patch to add the generated header files.
    
    Remove all current patches:
    
    poppler-notests.patch.1: obsolete (autotools build system)
    
    poppler-mac-fake.patch.1: presumed obsolete
    
    ubsan.patch.0: presumably fixed differently upstream in
    https://cgit.freedesktop.org/poppler/poppler/commit/?id=f43cb73939f85952d83afc87a6dc638dc1ae311b
    
    poppler-libjpeg.patch.1: obsolete (autotools build system; there appears
    to be no way to do this with CMake?)
    
    0001-Fix-building-with-old-clang.patch.1: merged upstream
    
    Change-Id: I271c87ab3ec25cc9d1e8fafb04dae7c004acc350
    (cherry picked from commit 5f8426a077ec0986a4b6cf8a58515ad5496f78e0)
    Reviewed-on: https://gerrit.libreoffice.org/56259
    Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>
    Tested-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>

---
+++ external/poppler/StaticLibrary_poppler.mk
@@ -0,0 +1,128 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t; fill-column: 100 -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_StaticLibrary_StaticLibrary,poppler))
+
+$(eval $(call gb_StaticLibrary_use_unpacked,poppler,poppler))
+
+$(eval $(call gb_StaticLibrary_use_external,poppler,jpeg))
+
+$(eval $(call gb_StaticLibrary_set_warnings_not_errors,poppler))
+
+$(eval $(call gb_StaticLibrary_set_include,poppler,\
+	-I$(WORKDIR)/UnpackedTarball/poppler \
+	-I$(WORKDIR)/UnpackedTarball/poppler/fofi \
+	-I$(WORKDIR)/UnpackedTarball/poppler/goo \
+	-I$(WORKDIR)/UnpackedTarball/poppler/poppler \
+	$$(INCLUDE) \
+))
+
+ifeq ($(OS),WNT)
+$(eval $(call gb_StaticLibrary_add_defs,poppler,\
+	-DWIN32_LEAN_AND_MEAN \
+	-D_CRT_SECURE_NO_WARNINGS \
+))
+$(eval $(call gb_StaticLibrary_add_cxxflags,poppler,\
+	/Zc:wchar_t- \
+))
+endif
+
+$(eval $(call gb_StaticLibrary_set_generated_cxx_suffix,poppler,cc))
+
+$(eval $(call gb_StaticLibrary_add_generated_exception_objects,poppler,\
+	UnpackedTarball/poppler/goo/gfile \
+	UnpackedTarball/poppler/goo/gmempp \
+	UnpackedTarball/poppler/goo/GooList \
+	UnpackedTarball/poppler/goo/GooTimer \
+	UnpackedTarball/poppler/goo/GooString \
+	UnpackedTarball/poppler/goo/gmem \
+	UnpackedTarball/poppler/goo/FixedPoint \
+	UnpackedTarball/poppler/goo/NetPBMWriter \
+	UnpackedTarball/poppler/goo/PNGWriter \
+	UnpackedTarball/poppler/goo/TiffWriter \
+	UnpackedTarball/poppler/goo/JpegWriter \
+	UnpackedTarball/poppler/goo/ImgWriter \
+	UnpackedTarball/poppler/goo/gstrtod \
+	UnpackedTarball/poppler/goo/grandom \
+	UnpackedTarball/poppler/goo/glibc \
+	UnpackedTarball/poppler/goo/glibc_strtok_r \
+	UnpackedTarball/poppler/fofi/FoFiBase \
+	UnpackedTarball/poppler/fofi/FoFiEncodings \
+	UnpackedTarball/poppler/fofi/FoFiTrueType \
+	UnpackedTarball/poppler/fofi/FoFiType1 \
+	UnpackedTarball/poppler/fofi/FoFiType1C \
+	UnpackedTarball/poppler/fofi/FoFiIdentifier \
+	UnpackedTarball/poppler/poppler/Annot \
+	UnpackedTarball/poppler/poppler/Array \
+	UnpackedTarball/poppler/poppler/BuiltinFont \
+	UnpackedTarball/poppler/poppler/BuiltinFontTables \
+	UnpackedTarball/poppler/poppler/CachedFile \
+	UnpackedTarball/poppler/poppler/Catalog \
+	UnpackedTarball/poppler/poppler/CharCodeToUnicode \
+	UnpackedTarball/poppler/poppler/CMap \
+	UnpackedTarball/poppler/poppler/DateInfo \
+	UnpackedTarball/poppler/poppler/Decrypt \
+	UnpackedTarball/poppler/poppler/Dict \
+	UnpackedTarball/poppler/poppler/Error \
+	UnpackedTarball/poppler/poppler/FileSpec \
+	UnpackedTarball/poppler/poppler/FontEncodingTables \
+	UnpackedTarball/poppler/poppler/Form \
+	UnpackedTarball/poppler/poppler/FontInfo \
+	UnpackedTarball/poppler/poppler/Function \
+	UnpackedTarball/poppler/poppler/Gfx \
+	UnpackedTarball/poppler/poppler/GfxFont \
+	UnpackedTarball/poppler/poppler/GfxState \
+	UnpackedTarball/poppler/poppler/GlobalParams \
+	UnpackedTarball/poppler/poppler/Hints \
+	UnpackedTarball/poppler/poppler/JArithmeticDecoder \
+	UnpackedTarball/poppler/poppler/JBIG2Stream \
+	UnpackedTarball/poppler/poppler/Lexer \
+	UnpackedTarball/poppler/poppler/Link \
+	UnpackedTarball/poppler/poppler/Linearization \
+	UnpackedTarball/poppler/poppler/LocalPDFDocBuilder \
+	UnpackedTarball/poppler/poppler/MarkedContentOutputDev \
+	UnpackedTarball/poppler/poppler/NameToCharCode \
+	UnpackedTarball/poppler/poppler/Object \
+	UnpackedTarball/poppler/poppler/OptionalContent \
+	UnpackedTarball/poppler/poppler/Outline \
+	UnpackedTarball/poppler/poppler/OutputDev \
+	UnpackedTarball/poppler/poppler/Page \
+	UnpackedTarball/poppler/poppler/PageTransition \
+	UnpackedTarball/poppler/poppler/Parser \
+	UnpackedTarball/poppler/poppler/PDFDoc \
+	UnpackedTarball/poppler/poppler/PDFDocEncoding \
+	UnpackedTarball/poppler/poppler/PDFDocFactory \
+	UnpackedTarball/poppler/poppler/PopplerCache \
+	UnpackedTarball/poppler/poppler/ProfileData \
+	UnpackedTarball/poppler/poppler/PreScanOutputDev \
+	UnpackedTarball/poppler/poppler/PSTokenizer \
+	UnpackedTarball/poppler/poppler/SignatureInfo \
+	UnpackedTarball/poppler/poppler/Stream \
+	UnpackedTarball/poppler/poppler/StructTreeRoot \
+	UnpackedTarball/poppler/poppler/StructElement \
+	UnpackedTarball/poppler/poppler/UnicodeMap \
+	UnpackedTarball/poppler/poppler/UnicodeMapFuncs \
+	UnpackedTarball/poppler/poppler/UnicodeTypeTable \
+	UnpackedTarball/poppler/poppler/UTF \
+	UnpackedTarball/poppler/poppler/XRef \
+	UnpackedTarball/poppler/poppler/PSOutputDev \
+	UnpackedTarball/poppler/poppler/TextOutputDev \
+	UnpackedTarball/poppler/poppler/PageLabelInfo \
+	UnpackedTarball/poppler/poppler/SecurityHandler \
+	UnpackedTarball/poppler/poppler/StdinCachedFile \
+	UnpackedTarball/poppler/poppler/StdinPDFDocBuilder \
+	UnpackedTarball/poppler/poppler/Sound \
+	UnpackedTarball/poppler/poppler/ViewerPreferences \
+	UnpackedTarball/poppler/poppler/XpdfPluginAPI \
+	UnpackedTarball/poppler/poppler/Movie \
+	UnpackedTarball/poppler/poppler/Rendition \
+	UnpackedTarball/poppler/poppler/DCTStream \
+))
+
+# vim: set noet sw=4 ts=4:
--- external/poppler/UnpackedTarball_poppler.mk
+++ external/poppler/UnpackedTarball_poppler.mk
@@ -12,12 +12,8 @@ $(eval $(call gb_UnpackedTarball_UnpackedTarball,poppler))
 $(eval $(call gb_UnpackedTarball_set_tarball,poppler,$(POPPLER_TARBALL),,poppler))
 
 $(eval $(call gb_UnpackedTarball_add_patches,poppler,\
-	external/poppler/poppler-notests.patch.1 \
 	$(if $(filter MSC-120,$(COM)-$(VCVER)),external/poppler/poppler-snprintf.patch.1) \
-	external/poppler/poppler-mac-fake.patch.1 \
-	external/poppler/ubsan.patch.0 \
-	external/poppler/poppler-libjpeg.patch.1 \
-	external/poppler/0001-Fix-building-with-old-clang.patch.1 \
+	external/poppler/poppler-config.patch.1 \
 ))
 
 # vim: set noet sw=4 ts=4:
