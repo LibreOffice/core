5b2d9b0cd0ef8ea44ced8a3479fa521b74058edc
Luboš Luňák <l.lunak@collabora.com>
2018-06-21 10:35:39 +0200

    fix xmlsec for --with-system-nss
    Change-Id: I95e720c9ee5774d87bb4eb8388f8a302ffed8a9b
    Reviewed-on: https://gerrit.libreoffice.org/56222
    Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
    Tested-by: Jenkins

--- external/xmlsec/ExternalProject_xmlsec.mk
+++ external/xmlsec/ExternalProject_xmlsec.mk
@@ -43,6 +43,8 @@ $(call gb_ExternalProject_get_state_target,xmlsec,build) :
 			--without-openssl \
 			$(if $(filter MACOSX,$(OS)),--prefix=/@.__________________________________________________OOO) \
 			$(if $(SYSTEM_NSS),,$(if $(filter MACOSX,$(OS)),--disable-pkgconfig)) \
+			$(if $(SYSTEM_NSS),,--with-nss=$(call gb_UnpackedTarball_get_dir,nss)/dist/public) \
+			$(if $(SYSTEM_NSS),,--with-nspr=$(call gb_UnpackedTarball_get_dir,nss)/dist/out) \
 			$(if $(CROSS_COMPILING),--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM)) \
 			$(if $(SYSBASE),CFLAGS="-I$(SYSBASE)/usr/include" \
 			LDFLAGS="-L$(SYSBASE)/usr/lib $(if $(filter-out LINUX FREEBSD,$(OS)),",-Wl$(COMMA)-z$(COMMA)origin -Wl$(COMMA)-rpath$(COMMA)\\"\$$\$$ORIGIN)) \
