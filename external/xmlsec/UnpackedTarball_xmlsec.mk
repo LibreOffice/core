5b42a17dc99fba2ccf8dd8d0a8e0e4e836e30120
Miklos Vajna <vmiklos@collabora.co.uk>
2018-06-22 09:13:04 +0200

    libxmlsec windows: enable ECDSA support
    It is provided by the mscng backend.
    
    Change-Id: I1b7e6baf4ba37f7990d1dd685e8d867834e8212d
    Reviewed-on: https://gerrit.libreoffice.org/56271
    Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
    Tested-by: Jenkins

--- external/xmlsec/UnpackedTarball_xmlsec.mk
+++ external/xmlsec/UnpackedTarball_xmlsec.mk
@@ -11,6 +11,8 @@ xmlsec_patches :=
 xmlsec_patches += xmlsec1-configure.patch.1
 xmlsec_patches += xmlsec1-vc.patch.1
 xmlsec_patches += xmlsec1-1.2.14_fix_extern_c.patch.1
+# Backport of <https://github.com/lsh123/xmlsec/pull/192>.
+xmlsec_patches += mscng-fixes.patch.1
 
 $(eval $(call gb_UnpackedTarball_UnpackedTarball,xmlsec))
 
