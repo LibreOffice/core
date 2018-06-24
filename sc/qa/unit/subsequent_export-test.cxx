c553a45d8a03455a5c7e29eebcf5597b69fbc74e
Laurent BP <laurent.balland-poirier@laposte.net>
2018-06-24 17:22:54 +0200

    tdf#118324 Add QA unit test for import/export ODS
    This commit test integer number format with '?' and '0'
    for number, fraction, scientific and engineering
    Need https://gerrit.libreoffice.org/56352/
    
    Change-Id: I5cd821acec2121a9edb442d4ab9bd33b790ac87a

--- sc/qa/unit/subsequent_export-test.cxx
+++ sc/qa/unit/subsequent_export-test.cxx
@@ -208,6 +208,7 @@ public:
     void testHiddenRepeatedRowsODS();
     void testHyperlinkTargetFrameODS();
     void testOpenDocumentAsReadOnly();
+    void testNumberFormatODS();
 
     CPPUNIT_TEST_SUITE(ScExportTest);
     CPPUNIT_TEST(test);
@@ -315,6 +316,7 @@ public:
     CPPUNIT_TEST(testHiddenRepeatedRowsODS);
     CPPUNIT_TEST(testHyperlinkTargetFrameODS);
     CPPUNIT_TEST(testOpenDocumentAsReadOnly);
+    CPPUNIT_TEST(testNumberFormatODS);
 
     CPPUNIT_TEST_SUITE_END();
 
@@ -4033,6 +4035,30 @@ void ScExportTest::testOpenDocumentAsReadOnly()
     CPPUNIT_ASSERT(xDocSh2->IsSecurityOptOpenReadOnly());
     xDocSh->DoClose();
     xDocSh2->DoClose();
+}
+
+void ScExportTest::testNumberFormatODS()
+{
+    ScDocShellRef xDocSh = loadDoc("testIntegerNumFormat.", FORMAT_FODS);
+    CPPUNIT_ASSERT_MESSAGE("Failed to load doc", xDocSh.is() );
+
+    xDocSh = saveAndReload( &(*xDocSh), FORMAT_ODS);
+    ScDocument& rDoc = xDocSh->GetDocument();
+    sal_uInt32 nNumberFormat;
+    const sal_Int32 nCountFormats = 17;
+    const OUString aExpectedFormatStr[nCountFormats] = { "\"format=\"000000", "\"format=\"??????", "\"format=\"??0000",
+                "\"format=\"000,000", "\"format=\"???,???", "\"format=\"??0,000",
+                "\"format=\"000\" \"?/?", "\"format=\"???\" \"?/?", "\"format=\"?00\" \"?/?",
+                "\"format=\"0,000\" \"?/?", "\"format=\"?,???\" \"?/?", "\"format=\"?,?00\" \"?/?",
+                "\"format=\"0E+00", "\"format=\"?E+00", "\"format=\"000E+00", "\"format=\"???E+00", "\"format=\"?00E+00" };
+    for ( sal_Int32 i = 0 ; i < nCountFormats ; i++)
+    {
+        rDoc.GetNumberFormat(i+1, 2, 0, nNumberFormat);
+        const SvNumberformat* pNumberFormat = rDoc.GetFormatTable()->GetEntry(nNumberFormat);
+        const OUString& rFormatStr = pNumberFormat->GetFormatstring();
+
+        CPPUNIT_ASSERT_EQUAL_MESSAGE("Number format modified during export/import", aExpectedFormatStr[i], rFormatStr);
+    }
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(ScExportTest);
