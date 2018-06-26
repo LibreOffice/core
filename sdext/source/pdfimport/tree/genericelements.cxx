LibreOffice/core

commit a9ef943769b06e6bdffe7326f288b27e08a95698

loplugin:useuniqueptr in sdext
Change-Id: I0870d4b1e85465b07e3d9cdb05520fcb37dfc267
Reviewed-on: https://gerrit.libreoffice.org/50659
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sdext/source/pdfimport/tree/genericelements.cxx
@@ -34,9 +34,6 @@ namespace pdfi
 
 Element::~Element()
 {
-    for (auto const& child : Children)
-        delete child;
-    Children.clear();
 }
 
 void Element::applyToChildren( ElementTreeVisitor& rVisitor )
@@ -45,7 +42,7 @@ void Element::applyToChildren( ElementTreeVisitor& rVisitor )
         (*it)->visitedBy( rVisitor, it );
 }
 
-void Element::setParent( std::list<Element*>::iterator const & el, Element* pNewParent )
+void Element::setParent( std::list<std::unique_ptr<Element>>::iterator const & el, Element* pNewParent )
 {
     if( pNewParent )
     {
@@ -95,32 +92,32 @@ void Element::emitStructure( int nLevel)
 }
 #endif
 
-void ListElement::visitedBy( ElementTreeVisitor& visitor, const std::list< Element* >::const_iterator& )
+void ListElement::visitedBy( ElementTreeVisitor& visitor, const std::list< std::unique_ptr<Element> >::const_iterator& )
 {
     // this is only an inner node
     applyToChildren(visitor);
 }
 
 void HyperlinkElement::visitedBy( ElementTreeVisitor&                          rVisitor,
-                                  const std::list< Element* >::const_iterator& rParentIt )
+                                  const std::list< std::unique_ptr<Element> >::const_iterator& rParentIt )
 {
     rVisitor.visit(*this,rParentIt);
 }
 
 void TextElement::visitedBy( ElementTreeVisitor&                          rVisitor,
-                             const std::list< Element* >::const_iterator& rParentIt )
+                             const std::list< std::unique_ptr<Element> >::const_iterator& rParentIt )
 {
     rVisitor.visit(*this,rParentIt);
 }
 
 void FrameElement::visitedBy( ElementTreeVisitor&                          rVisitor,
-                              const std::list< Element* >::const_iterator& rParentIt )
+                              const std::list< std::unique_ptr<Element> >::const_iterator& rParentIt )
 {
     rVisitor.visit(*this,rParentIt);
 }
 
 void ImageElement::visitedBy( ElementTreeVisitor&                          rVisitor,
-                              const std::list< Element* >::const_iterator& rParentIt)
+                              const std::list< std::unique_ptr<Element> >::const_iterator& rParentIt)
 {
     rVisitor.visit( *this, rParentIt);
 }
@@ -153,7 +150,7 @@ void PolyPolyElement::updateGeometry()
 }
 
 void PolyPolyElement::visitedBy( ElementTreeVisitor&                          rVisitor,
-                                 const std::list< Element* >::const_iterator& rParentIt)
+                                 const std::list< std::unique_ptr<Element> >::const_iterator& rParentIt)
 {
     rVisitor.visit( *this, rParentIt);
 }
@@ -183,22 +180,22 @@ void PolyPolyElement::emitStructure( int nLevel)
 #endif
 
 void ParagraphElement::visitedBy( ElementTreeVisitor&                          rVisitor,
-                                  const std::list< Element* >::const_iterator& rParentIt )
+                                  const std::list< std::unique_ptr<Element> >::const_iterator& rParentIt )
 {
     rVisitor.visit(*this,rParentIt);
 }
 
 bool ParagraphElement::isSingleLined( PDFIProcessor const & rProc ) const
 {
-    std::list< Element* >::const_iterator it = Children.begin();
+    auto it = Children.begin();
     TextElement* pText = nullptr, *pLastText = nullptr;
     while( it != Children.end() )
     {
         // a paragraph containing subparagraphs cannot be single lined
-        if( dynamic_cast< ParagraphElement* >(*it) != nullptr )
+        if( dynamic_cast< ParagraphElement* >(it->get()) != nullptr )
             return false;
 
-        pText = dynamic_cast< TextElement* >(*it);
+        pText = dynamic_cast< TextElement* >(it->get());
         if( pText )
         {
             const FontAttributes& rFont = rProc.getFont( pText->FontId );
@@ -223,9 +220,9 @@ bool ParagraphElement::isSingleLined( PDFIProcessor const & rProc ) const
 double ParagraphElement::getLineHeight( PDFIProcessor& rProc ) const
 {
     double line_h = 0;
-    for( std::list< Element* >::const_iterator it = Children.begin(); it != Children.end(); ++it )
+    for( auto it = Children.begin(); it != Children.end(); ++it )
     {
-        ParagraphElement* pPara = dynamic_cast< ParagraphElement* >(*it);
+        ParagraphElement* pPara = dynamic_cast< ParagraphElement* >(it->get());
         TextElement* pText = nullptr;
         if( pPara )
         {
@@ -233,7 +230,7 @@ double ParagraphElement::getLineHeight( PDFIProcessor& rProc ) const
             if( lh > line_h )
                 line_h = lh;
         }
-        else if( (pText = dynamic_cast< TextElement* >( *it )) != nullptr )
+        else if( (pText = dynamic_cast< TextElement* >( it->get() )) != nullptr )
         {
             const FontAttributes& rFont = rProc.getFont( pText->FontId );
             double lh = pText->h;
@@ -249,22 +246,20 @@ double ParagraphElement::getLineHeight( PDFIProcessor& rProc ) const
 TextElement* ParagraphElement::getFirstTextChild() const
 {
     TextElement* pText = nullptr;
-    for( std::list< Element* >::const_iterator it = Children.begin();
+    for( auto it = Children.begin();
          it != Children.end() && ! pText; ++it )
     {
-        pText = dynamic_cast<TextElement*>(*it);
+        pText = dynamic_cast<TextElement*>(it->get());
     }
     return pText;
 }
 
 PageElement::~PageElement()
 {
-    delete HeaderElement;
-    delete FooterElement;
 }
 
 void PageElement::visitedBy( ElementTreeVisitor&                          rVisitor,
-                             const std::list< Element* >::const_iterator& rParentIt )
+                             const std::list< std::unique_ptr<Element> >::const_iterator& rParentIt )
 {
      rVisitor.visit(*this, rParentIt);
 }
@@ -272,24 +267,24 @@ void PageElement::visitedBy( ElementTreeVisitor&                          rVisit
 void PageElement::updateParagraphGeometry( Element* pEle )
 {
     // update geometry of children
-    for( std::list< Element* >::iterator it = pEle->Children.begin();
+    for( auto it = pEle->Children.begin();
          it != pEle->Children.end(); ++it )
     {
-        updateParagraphGeometry( *it );
+        updateParagraphGeometry( it->get() );
     }
     // if this is a paragraph itself, then update according to children geometry
     if( dynamic_cast<ParagraphElement*>(pEle) )
     {
-        for( std::list< Element* >::iterator it = pEle->Children.begin();
+        for( auto it = pEle->Children.begin();
              it != pEle->Children.end(); ++it )
         {
             Element* pChild = nullptr;
-            TextElement* pText = dynamic_cast<TextElement*>(*it);
+            TextElement* pText = dynamic_cast<TextElement*>(it->get());
             if( pText )
                 pChild = pText;
             else
             {
-                ParagraphElement* pPara = dynamic_cast<ParagraphElement*>(*it);
+                ParagraphElement* pPara = dynamic_cast<ParagraphElement*>(it->get());
                 if( pPara )
                     pChild = pPara;
             }
@@ -299,18 +294,18 @@ void PageElement::updateParagraphGeometry( Element* pEle )
     }
 }
 
-bool PageElement::resolveHyperlink( const std::list<Element*>::iterator& link_it, std::list<Element*>& rElements )
+bool PageElement::resolveHyperlink( const std::list<std::unique_ptr<Element>>::iterator& link_it, std::list<std::unique_ptr<Element>>& rElements )
 {
-    HyperlinkElement* pLink = dynamic_cast<HyperlinkElement*>(*link_it);
+    HyperlinkElement* pLink = dynamic_cast<HyperlinkElement*>(link_it->get());
     if( ! pLink ) // sanity check
         return false;
 
-    for( std::list<Element*>::iterator it = rElements.begin(); it != rElements.end(); ++it )
+    for( auto it = rElements.begin(); it != rElements.end(); ++it )
     {
         if( (*it)->x >= pLink->x && (*it)->x + (*it)->w <= pLink->x + pLink->w &&
             (*it)->y >= pLink->y && (*it)->y + (*it)->h <= pLink->y + pLink->h )
         {
-            TextElement* pText = dynamic_cast<TextElement*>(*it);
+            TextElement* pText = dynamic_cast<TextElement*>(it->get());
             if( pText )
             {
                 if( pLink->Children.empty() )
@@ -320,7 +315,7 @@ bool PageElement::resolveHyperlink( const std::list<Element*>::iterator& link_it
                     pLink->Parent = (*it)->Parent;
                 }
                 // move text element into hyperlink
-                std::list<Element*>::iterator next = it;
+                auto next = it;
                 ++next;
                 Element::setParent( it, pLink );
                 it = next;
@@ -330,13 +325,13 @@ bool PageElement::resolveHyperlink( const std::list<Element*>::iterator& link_it
             // a link can contain multiple text elements or a single frame
             if( ! pLink->Children.empty() )
                 continue;
-            if( dynamic_cast<ParagraphElement*>(*it)  )
+            if( dynamic_cast<ParagraphElement*>(it->get())  )
             {
                 if( resolveHyperlink( link_it, (*it)->Children ) )
                     break;
                 continue;
             }
-            FrameElement* pFrame = dynamic_cast<FrameElement*>(*it);
+            FrameElement* pFrame = dynamic_cast<FrameElement*>(it->get());
             if( pFrame )
             {
                 // insert the hyperlink before the frame
@@ -357,7 +352,6 @@ void PageElement::resolveHyperlinks()
     {
         if( ! resolveHyperlink( Hyperlinks.Children.begin(), Children ) )
         {
-            delete Hyperlinks.Children.front();
             Hyperlinks.Children.pop_front();
         }
     }
@@ -373,10 +367,10 @@ void PageElement::resolveUnderlines( PDFIProcessor const & rProc )
     // FIXME: currently the algorithm used is quadratic
     // this could be solved by some sorting beforehand
 
-    std::list< Element* >::iterator poly_it = Children.begin();
+    auto poly_it = Children.begin();
     while( poly_it != Children.end() )
     {
-        PolyPolyElement* pPoly = dynamic_cast< PolyPolyElement* >(*poly_it);
+        PolyPolyElement* pPoly = dynamic_cast< PolyPolyElement* >(poly_it->get());
         if( ! pPoly || ! pPoly->Children.empty() )
         {
             ++poly_it;
@@ -413,10 +407,10 @@ void PageElement::resolveUnderlines( PDFIProcessor const & rProc )
             u_y = r_x; r_x = l_x; l_x = u_y;
         }
         u_y = aPoly.getB2DPoint(0).getY();
-        for( std::list< Element*>::iterator it = Children.begin();
+        for( auto it = Children.begin();
              it != Children.end(); ++it )
         {
-            Element* pEle = *it;
+            Element* pEle = it->get();
             if( pEle->y <= u_y && pEle->y + pEle->h*1.1 >= u_y )
             {
                 // first: is the element underlined completely ?
@@ -450,7 +444,7 @@ void PageElement::resolveUnderlines( PDFIProcessor const & rProc )
         }
         if( bRemovePoly )
         {
-            std::list< Element* >::iterator next_it = poly_it;
+            auto next_it = poly_it;
             ++next_it;
             Children.erase( poly_it );
             delete pPoly;
@@ -466,7 +460,7 @@ DocumentElement::~DocumentElement()
 }
 
 void DocumentElement::visitedBy( ElementTreeVisitor&                          rVisitor,
-                                 const std::list< Element* >::const_iterator& rParentIt)
+                                 const std::list< std::unique_ptr<Element> >::const_iterator& rParentIt)
 {
     rVisitor.visit(*this, rParentIt);
 }

sdext/source/pdfimport/inc/genericelements.hxx
@@ -77,7 +77,7 @@ namespace pdfi
             : x( 0 ), y( 0 ), w( 0 ), h( 0 ), StyleId( -1 ), Parent( pParent )
         {
             if( pParent )
-                pParent->Children.push_back( this );
+                pParent->Children.emplace_back( this );
         }
 
     public:
@@ -87,7 +87,7 @@ namespace pdfi
             To be implemented by every tree node that needs to be
             visitable.
          */
-        virtual void visitedBy( ElementTreeVisitor&, const std::list< Element* >::const_iterator& rParentIt ) = 0;
+        virtual void visitedBy( ElementTreeVisitor&, const std::list< std::unique_ptr<Element> >::const_iterator& rParentIt ) = 0;
         /// Apply visitor to all children
         void applyToChildren( ElementTreeVisitor& );
         /// Union element geometry with given element
@@ -100,18 +100,18 @@ namespace pdfi
         /** el must be a valid dereferenceable iterator of el->Parent->Children
             pNewParent must not be NULL
         */
-        static void setParent( std::list<Element*>::iterator const & el, Element* pNewParent );
+        static void setParent( std::list<std::unique_ptr<Element>>::iterator const & el, Element* pNewParent );
 
         double              x, y, w, h;
         sal_Int32           StyleId;
         Element*            Parent;
-        std::list<Element*> Children;
+        std::list<std::unique_ptr<Element>> Children;
     };
 
     struct ListElement : public Element
     {
         ListElement() : Element( nullptr ) {}
-        virtual void visitedBy( ElementTreeVisitor&, const std::list< Element* >::const_iterator& ) override;
+        virtual void visitedBy( ElementTreeVisitor&, const std::list< std::unique_ptr<Element> >::const_iterator& ) override;
     };
 
     struct HyperlinkElement : public Element
@@ -121,7 +121,7 @@ namespace pdfi
         HyperlinkElement( Element* pParent, const OUString& rURI )
         : Element( pParent ), URI( rURI ) {}
     public:
-        virtual void visitedBy( ElementTreeVisitor&, const std::list< Element* >::const_iterator& ) override;
+        virtual void visitedBy( ElementTreeVisitor&, const std::list< std::unique_ptr<Element> >::const_iterator& ) override;
 
         OUString URI;
     };
@@ -166,7 +166,7 @@ namespace pdfi
         : DrawElement( pParent, nGCId ) {}
 
     public:
-        virtual void visitedBy( ElementTreeVisitor&, const std::list< Element* >::const_iterator& ) override;
+        virtual void visitedBy( ElementTreeVisitor&, const std::list< std::unique_ptr<Element> >::const_iterator& ) override;
     };
 
     struct TextElement : public GraphicalElement
@@ -177,7 +177,7 @@ namespace pdfi
         : GraphicalElement( pParent, nGCId ), FontId( nFontId ) {}
 
     public:
-        virtual void visitedBy( ElementTreeVisitor&, const std::list< Element* >::const_iterator& ) override;
+        virtual void visitedBy( ElementTreeVisitor&, const std::list< std::unique_ptr<Element> >::const_iterator& ) override;
 
         OUStringBuffer Text;
         sal_Int32           FontId;
@@ -190,7 +190,7 @@ namespace pdfi
         explicit ParagraphElement( Element* pParent ) : Element( pParent ), Type( Normal ), bRtl( false ) {}
 
     public:
-        virtual void visitedBy( ElementTreeVisitor&, const std::list< Element* >::const_iterator& rParentIt ) override;
+        virtual void visitedBy( ElementTreeVisitor&, const std::list< std::unique_ptr<Element> >::const_iterator& rParentIt ) override;
 
         // returns true only if only a single line is contained
         bool isSingleLined( PDFIProcessor const & rProc ) const;
@@ -213,7 +213,7 @@ namespace pdfi
                          const basegfx::B2DPolyPolygon& rPolyPoly,
                          sal_Int8 nAction );
     public:
-        virtual void visitedBy( ElementTreeVisitor&, const std::list< Element* >::const_iterator& rParentIt ) override;
+        virtual void visitedBy( ElementTreeVisitor&, const std::list< std::unique_ptr<Element> >::const_iterator& rParentIt ) override;
 
         void updateGeometry();
 
@@ -233,7 +233,7 @@ namespace pdfi
         : DrawElement( pParent, nGCId ), Image( nImage ) {}
 
     public:
-        virtual void visitedBy( ElementTreeVisitor&, const std::list< Element* >::const_iterator& ) override;
+        virtual void visitedBy( ElementTreeVisitor&, const std::list< std::unique_ptr<Element> >::const_iterator& ) override;
 
         ImageId Image;
     };
@@ -249,11 +249,11 @@ namespace pdfi
         {}
     private:
         // helper method for resolveHyperlinks
-        bool resolveHyperlink( const std::list<Element*>::iterator& link_it, std::list<Element*>& rElements );
+        bool resolveHyperlink( const std::list<std::unique_ptr<Element>>::iterator& link_it, std::list<std::unique_ptr<Element>>& rElements );
     public:
         virtual ~PageElement() override;
 
-        virtual void visitedBy( ElementTreeVisitor&, const std::list< Element* >::const_iterator& rParentIt ) override;
+        virtual void visitedBy( ElementTreeVisitor&, const std::list< std::unique_ptr<Element> >::const_iterator& rParentIt ) override;
 
         static void updateParagraphGeometry( Element* pEle );
         void resolveHyperlinks();
@@ -266,8 +266,8 @@ namespace pdfi
         double         BottomMargin;
         double         LeftMargin;
         double         RightMargin;
-        Element*       HeaderElement;
-        Element*       FooterElement;
+        std::unique_ptr<Element> HeaderElement;
+        std::unique_ptr<Element> FooterElement;
     };
 
     struct DocumentElement : public Element
@@ -278,7 +278,7 @@ namespace pdfi
     public:
         virtual ~DocumentElement() override;
 
-        virtual void visitedBy( ElementTreeVisitor&, const std::list< Element* >::const_iterator& ) override;
+        virtual void visitedBy( ElementTreeVisitor&, const std::list< std::unique_ptr<Element> >::const_iterator& ) override;
     };
 
     // this class is the differentiator of document types: it will create

sdext/source/pdfimport/inc/treevisiting.hxx
@@ -43,14 +43,14 @@ namespace pdfi
      */
     struct ElementTreeVisitor
     {
-        virtual void visit( HyperlinkElement&, const std::list< Element* >::const_iterator& ) = 0;
-        virtual void visit( TextElement&, const std::list< Element* >::const_iterator&  ) = 0;
-        virtual void visit( ParagraphElement&, const std::list< Element* >::const_iterator&  ) = 0;
-        virtual void visit( FrameElement&, const std::list< Element* >::const_iterator&  ) = 0;
-        virtual void visit( PolyPolyElement&, const std::list< Element* >::const_iterator&  ) = 0;
-        virtual void visit( ImageElement&, const std::list< Element* >::const_iterator&  ) = 0;
-        virtual void visit( PageElement&, const std::list< Element* >::const_iterator&  ) = 0;
-        virtual void visit( DocumentElement&, const std::list< Element* >::const_iterator&  ) = 0;
+        virtual void visit( HyperlinkElement&, const std::list< std::unique_ptr<Element> >::const_iterator& ) = 0;
+        virtual void visit( TextElement&, const std::list< std::unique_ptr<Element> >::const_iterator&  ) = 0;
+        virtual void visit( ParagraphElement&, const std::list< std::unique_ptr<Element> >::const_iterator&  ) = 0;
+        virtual void visit( FrameElement&, const std::list< std::unique_ptr<Element> >::const_iterator&  ) = 0;
+        virtual void visit( PolyPolyElement&, const std::list< std::unique_ptr<Element> >::const_iterator&  ) = 0;
+        virtual void visit( ImageElement&, const std::list< std::unique_ptr<Element> >::const_iterator&  ) = 0;
+        virtual void visit( PageElement&, const std::list< std::unique_ptr<Element> >::const_iterator&  ) = 0;
+        virtual void visit( DocumentElement&, const std::list< std::unique_ptr<Element> >::const_iterator&  ) = 0;
         virtual ~ElementTreeVisitor() {}
     };
     typedef std::shared_ptr<ElementTreeVisitor> ElementTreeVisitorSharedPtr;

sdext/source/pdfimport/tree/drawtreevisiting.hxx
@@ -44,14 +44,14 @@ namespace pdfi
             m_rProcessor(rProcessor)
         {}
 
-        virtual void visit( HyperlinkElement&, const std::list< Element* >::const_iterator& ) override;
-        virtual void visit( TextElement&, const std::list< Element* >::const_iterator&  ) override;
-        virtual void visit( ParagraphElement&, const std::list< Element* >::const_iterator&  ) override;
-        virtual void visit( FrameElement&, const std::list< Element* >::const_iterator&  ) override;
-        virtual void visit( PolyPolyElement&, const std::list< Element* >::const_iterator&  ) override;
-        virtual void visit( ImageElement&, const std::list< Element* >::const_iterator&  ) override;
-        virtual void visit( PageElement&, const std::list< Element* >::const_iterator&  ) override;
-        virtual void visit( DocumentElement&, const std::list< Element* >::const_iterator&  ) override;
+        virtual void visit( HyperlinkElement&, const std::list< std::unique_ptr<Element> >::const_iterator& ) override;
+        virtual void visit( TextElement&, const std::list< std::unique_ptr<Element> >::const_iterator&  ) override;
+        virtual void visit( ParagraphElement&, const std::list< std::unique_ptr<Element> >::const_iterator&  ) override;
+        virtual void visit( FrameElement&, const std::list< std::unique_ptr<Element> >::const_iterator&  ) override;
+        virtual void visit( PolyPolyElement&, const std::list< std::unique_ptr<Element> >::const_iterator&  ) override;
+        virtual void visit( ImageElement&, const std::list< std::unique_ptr<Element> >::const_iterator&  ) override;
+        virtual void visit( PageElement&, const std::list< std::unique_ptr<Element> >::const_iterator&  ) override;
+        virtual void visit( DocumentElement&, const std::list< std::unique_ptr<Element> >::const_iterator&  ) override;
     };
 
     class DrawXmlFinalizer : public ElementTreeVisitor
@@ -67,14 +67,14 @@ namespace pdfi
             m_rProcessor(rProcessor)
         {}
 
-        virtual void visit( HyperlinkElement&, const std::list< Element* >::const_iterator& ) override;
-        virtual void visit( TextElement&, const std::list< Element* >::const_iterator&  ) override;
-        virtual void visit( ParagraphElement&, const std::list< Element* >::const_iterator&  ) override;
-        virtual void visit( FrameElement&, const std::list< Element* >::const_iterator&  ) override;
-        virtual void visit( PolyPolyElement&, const std::list< Element* >::const_iterator&  ) override;
-        virtual void visit( ImageElement&, const std::list< Element* >::const_iterator&  ) override;
-        virtual void visit( PageElement&, const std::list< Element* >::const_iterator&  ) override;
-        virtual void visit( DocumentElement&, const std::list< Element* >::const_iterator&  ) override;
+        virtual void visit( HyperlinkElement&, const std::list< std::unique_ptr<Element> >::const_iterator& ) override;
+        virtual void visit( TextElement&, const std::list< std::unique_ptr<Element> >::const_iterator&  ) override;
+        virtual void visit( ParagraphElement&, const std::list< std::unique_ptr<Element> >::const_iterator&  ) override;
+        virtual void visit( FrameElement&, const std::list< std::unique_ptr<Element> >::const_iterator&  ) override;
+        virtual void visit( PolyPolyElement&, const std::list< std::unique_ptr<Element> >::const_iterator&  ) override;
+        virtual void visit( ImageElement&, const std::list< std::unique_ptr<Element> >::const_iterator&  ) override;
+        virtual void visit( PageElement&, const std::list< std::unique_ptr<Element> >::const_iterator&  ) override;
+        virtual void visit( DocumentElement&, const std::list< std::unique_ptr<Element> >::const_iterator&  ) override;
     };
 
     class DrawXmlEmitter : public ElementTreeVisitor
@@ -100,14 +100,14 @@ namespace pdfi
             m_bWriteDrawDocument(eDocType==DRAW_DOC)
         {}
 
-        virtual void visit( HyperlinkElement&, const std::list< Element* >::const_iterator& ) override;
-        virtual void visit( TextElement&, const std::list< Element* >::const_iterator&  ) override;
-        virtual void visit( ParagraphElement&, const std::list< Element* >::const_iterator&  ) override;
-        virtual void visit( FrameElement&, const std::list< Element* >::const_iterator&  ) override;
-        virtual void visit( PolyPolyElement&, const std::list< Element* >::const_iterator&  ) override;
-        virtual void visit( ImageElement&, const std::list< Element* >::const_iterator&  ) override;
-        virtual void visit( PageElement&, const std::list< Element* >::const_iterator&  ) override;
-        virtual void visit( DocumentElement&, const std::list< Element* >::const_iterator&  ) override;
+        virtual void visit( HyperlinkElement&, const std::list< std::unique_ptr<Element> >::const_iterator& ) override;
+        virtual void visit( TextElement&, const std::list< std::unique_ptr<Element> >::const_iterator&  ) override;
+        virtual void visit( ParagraphElement&, const std::list< std::unique_ptr<Element> >::const_iterator&  ) override;
+        virtual void visit( FrameElement&, const std::list< std::unique_ptr<Element> >::const_iterator&  ) override;
+        virtual void visit( PolyPolyElement&, const std::list< std::unique_ptr<Element> >::const_iterator&  ) override;
+        virtual void visit( ImageElement&, const std::list< std::unique_ptr<Element> >::const_iterator&  ) override;
+        virtual void visit( PageElement&, const std::list< std::unique_ptr<Element> >::const_iterator&  ) override;
+        virtual void visit( DocumentElement&, const std::list< std::unique_ptr<Element> >::const_iterator&  ) override;
     };
 }
 

sdext/source/pdfimport/tree/pdfiprocessor.cxx
@@ -543,7 +543,7 @@ void PDFIProcessor::emit( XmlEmitter&               rEmitter,
         rVisitorFactory.createOptimizingVisitor(*this));
     // FIXME: localization
     startIndicator( " " );
-    m_pDocument->visitedBy( *optimizingVisitor, std::list<Element*>::const_iterator());
+    m_pDocument->visitedBy( *optimizingVisitor, std::list<std::unique_ptr<Element>>::const_iterator());
 
 #if OSL_DEBUG_LEVEL > 0
     m_pDocument->emitStructure( 0 );
@@ -555,7 +555,7 @@ void PDFIProcessor::emit( XmlEmitter&               rEmitter,
         rVisitorFactory.createStyleCollectingVisitor(aStyles,*this));
     // FIXME: localization
 
-    m_pDocument->visitedBy( *finalizingVisitor, std::list<Element*>::const_iterator() );
+    m_pDocument->visitedBy( *finalizingVisitor, std::list<std::unique_ptr<Element>>::const_iterator() );
 
     EmitContext aContext( rEmitter, aStyles, m_aImages, *this, m_xStatusIndicator, m_xContext );
     ElementTreeVisitorSharedPtr aEmittingVisitor(
@@ -589,7 +589,7 @@ void PDFIProcessor::emit( XmlEmitter&               rEmitter,
     // emit style list
     aStyles.emit( aContext, *aEmittingVisitor );
 
-    m_pDocument->visitedBy( *aEmittingVisitor, std::list<Element*>::const_iterator() );
+    m_pDocument->visitedBy( *aEmittingVisitor, std::list<std::unique_ptr<Element>>::const_iterator() );
     aContext.rEmitter.endTag( "office:document" );
     endIndicator();
 }
@@ -625,7 +625,7 @@ void PDFIProcessor::endIndicator()
         m_xStatusIndicator->end();
 }
 
-static bool lr_tb_sort( Element* pLeft, Element* pRight )
+static bool lr_tb_sort( std::unique_ptr<Element> const & pLeft, std::unique_ptr<Element> const & pRight )
 {
     // Ensure irreflexivity (which could be compromised if h or w is negative):
     if (pLeft == pRight)
@@ -637,9 +637,9 @@ static bool lr_tb_sort( Element* pLeft, Element* pRight )
     // of the same order as font height whereas the real paint area
     // of text is usually smaller
     double fudge_factor_left = 0.0, fudge_factor_right = 0.0;
-    if( dynamic_cast< TextElement* >(pLeft) )
+    if( dynamic_cast< TextElement* >(pLeft.get()) )
         fudge_factor_left = 0.1;
-    if (dynamic_cast< TextElement* >(pRight))
+    if (dynamic_cast< TextElement* >(pRight.get()))
         fudge_factor_right = 0.1;
 
     // Allow negative height

sdext/source/pdfimport/tree/style.cxx
@@ -201,7 +201,7 @@ void StyleContainer::impl_emitStyle( sal_Int32           nStyleId,
             rContext.rEmitter.write( rStyle.Contents );
         if( rStyle.ContainedElement )
             rStyle.ContainedElement->visitedBy( rContainedElemVisitor,
-                                                std::list<Element*>::iterator() );
+                                                std::list<std::unique_ptr<Element>>::iterator() );
         rContext.rEmitter.endTag( rStyle.Name.getStr() );
     }
 }

commit 3ef38c752439b28ea9b540465d71d4572133ce03

tdf#118374: fix crash from loplugin:useuniqueptr in sdext
See bt:
https://bugs.documentfoundation.org/attachment.cgi?id=143105

Regression from:
https://cgit.freedesktop.org/libreoffice/core/commit/?id=a9ef943769b06e6bdffe7326f288b27e08a95698

Change-Id: I285f00cb6ede3d952a794b60ff64586fe2224e5f

sdext/source/pdfimport/tree/genericelements.cxx
@@ -447,7 +447,6 @@ void PageElement::resolveUnderlines( PDFIProcessor const & rProc )
             auto next_it = poly_it;
             ++next_it;
             Children.erase( poly_it );
-            delete pPoly;
             poly_it = next_it;
         }
         else

sdext/source/pdfimport/tree/writertreevisiting.cxx
@@ -419,7 +419,6 @@ void WriterXmlOptimizer::visit( PolyPolyElement& elem, const std::list< std::uni
 #else
         elem.Parent->Children.erase(next_it);
 #endif
-        delete pNext;
     }
 }
 
@@ -819,7 +818,6 @@ void WriterXmlOptimizer::optimizeTextElements(Element& rParent)
                     pCur->Children.splice( pCur->Children.end(), pNext->Children );
                     // get rid of the now useless element
                     rParent.Children.erase( next );
-                    delete pNext;
                     bConcat = true;
                 }
             }