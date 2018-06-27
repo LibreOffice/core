dc952845ca93a38868fee47a02b1ca1ed4c14f88
Stephan Bergmann <sbergman@redhat.com>
2018-06-27 18:31:45 +0200

    Suspiciously, copy ops of Bundle-derived classes didn't copy Bundle::mnColor
    Lets assume that that was an oversight, and change all those user-provided
    copy assignment ops into implicitly-defined ones (that will now also copy
    Bundle::mnColor).  Avoids -Wdeprecated-copy with GCC trunk towards GCC 9, as the
    copy ctors were already left implicitly-defined (and so already copied
    Bundle::mnColor).
    
    Change-Id: I43e323b55d059c7afecaddab0d777b6df47182c8
    Reviewed-on: https://gerrit.libreoffice.org/56539
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

--- filter/source/graphicfilter/icgm/bundles.cxx
+++ filter/source/graphicfilter/icgm/bundles.cxx
@@ -36,50 +36,6 @@ void Bundle::SetColor( sal_uInt32 nColor )
     mnColor = nColor;
 }
 
-
-LineBundle& LineBundle::operator=( const LineBundle& rSource )
-{
-    SetIndex( rSource.GetIndex() );
-    eLineType = rSource.eLineType;
-    nLineWidth = rSource.nLineWidth;
-    return *this;
-};
-
-MarkerBundle& MarkerBundle::operator=( const MarkerBundle& rSource )
-{
-    SetIndex( rSource.GetIndex() );
-    eMarkerType = rSource.eMarkerType;
-    nMarkerSize = rSource.nMarkerSize;
-    return *this;
-};
-
-EdgeBundle& EdgeBundle::operator=( const EdgeBundle& rSource )
-{
-    SetIndex( rSource.GetIndex() );
-    eEdgeType = rSource.eEdgeType;
-    nEdgeWidth = rSource.nEdgeWidth;
-    return *this;
-};
-
-TextBundle& TextBundle::operator=( const TextBundle& rSource )
-{
-    SetIndex( rSource.GetIndex() );
-    nTextFontIndex = rSource.nTextFontIndex;
-    eTextPrecision = rSource.eTextPrecision;
-    nCharacterExpansion = rSource.nCharacterExpansion;
-    nCharacterSpacing = rSource.nCharacterSpacing;
-    return *this;
-};
-
-FillBundle& FillBundle::operator=( const FillBundle& rSource )
-{
-    SetIndex( rSource.GetIndex() );
-    eFillInteriorStyle = rSource.eFillInteriorStyle;
-    nFillPatternIndex = rSource.nFillPatternIndex;
-    nFillHatchIndex = rSource.nFillHatchIndex;
-    return *this;
-};
-
 FontEntry::FontEntry() :
     pFontName       ( nullptr ),
     eCharSetType    ( CST_CCOMPLETE ),
