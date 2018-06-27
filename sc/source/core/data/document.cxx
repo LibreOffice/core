LibreOffice/core

commit b6b1ded0a0539e7be1b5338de378a3276a6ff445

sc: replace ScCaptionPtr with std::shared_ptr, tdf#117997, tdf#117228
Change-Id: I9b6a2c2504c9ce060906ac3bf156721709fef2f3
Reviewed-on: https://gerrit.libreoffice.org/55490
Tested-by: Jenkins
Reviewed-by: Eike Rathke <erack@redhat.com>

sc/source/core/data/document.cxx
@@ -99,6 +99,7 @@
 #include <tokenarray.hxx>
 #include <tokenstringcontext.hxx>
 #include <compressedarray.hxx>
+#include <docsh.hxx>
 
 #include <formula/vectortoken.hxx>
 
@@ -2183,7 +2184,7 @@ void ScDocument::CopyToClip(const ScClipParam& rClipParam,
     if (!pClipDoc)
     {
         SAL_WARN("sc", "CopyToClip: no ClipDoc");
-        pClipDoc = ScModule::GetClipDoc();
+        pClipDoc = static_cast<ScDocShell*>(mpShell)->GetClipDoc();
     }
 
     if (mpShell->GetMedium())
@@ -2283,7 +2284,7 @@ void ScDocument::CopyTabToClip(SCCOL nCol1, SCROW nRow1,
         if (!pClipDoc)
         {
             SAL_WARN("sc", "CopyTabToClip: no ClipDoc");
-            pClipDoc = ScModule::GetClipDoc();
+            pClipDoc = static_cast<ScDocShell*>(mpShell)->GetClipDoc();
         }
 
         if (mpShell->GetMedium())
@@ -2570,7 +2571,7 @@ bool ScDocument::IsClipboardSource() const
     if (bIsClip)
         return false;
 
-    ScDocument* pClipDoc = ScModule::GetClipDoc();
+    ScDocument* pClipDoc = static_cast<ScDocShell*>(mpShell)->GetClipDoc();
     return pClipDoc && pClipDoc->bIsClip && pClipDoc->mxPoolHelper.is() && mxPoolHelper.is() &&
             mxPoolHelper->GetDocPool() == pClipDoc->mxPoolHelper->GetDocPool();
 }
@@ -2807,7 +2808,7 @@ void ScDocument::CopyFromClip( const ScRange& rDestRange, const ScMarkData& rMar
     if (!pClipDoc)
     {
         OSL_FAIL("CopyFromClip: no ClipDoc");
-        pClipDoc = ScModule::GetClipDoc();
+        pClipDoc = static_cast<ScDocShell*>(mpShell)->GetClipDoc();
     }
 
     if (!pClipDoc->bIsClip || !pClipDoc->GetTableCount())

sc/inc/postit.hxx
@@ -36,110 +36,6 @@ class ScDocument;
 namespace tools { class Rectangle; }
 struct ScCaptionInitData;
 
-/** Some desperate attempt to fight against the caption object ownership mess,
-    to which none of shared/weak/plain pointer is a cure.
- */
-class ScCaptionPtr
-{
-public:
-    ScCaptionPtr();
-    explicit ScCaptionPtr( SdrCaptionObj* p );
-    ScCaptionPtr( const ScCaptionPtr& r );
-    ScCaptionPtr( ScCaptionPtr&& r );
-    ~ScCaptionPtr();
-
-    ScCaptionPtr& operator=( const ScCaptionPtr& r );
-    ScCaptionPtr& operator=( ScCaptionPtr&& r );
-    explicit operator bool() const    { return mpCaption != nullptr; }
-    const SdrCaptionObj* get() const        { return mpCaption; }
-    SdrCaptionObj* get()        { return mpCaption; }
-    const SdrCaptionObj* operator->() const { return mpCaption; }
-    SdrCaptionObj* operator->() { return mpCaption; }
-    const SdrCaptionObj& operator*() const  { return *mpCaption; }
-    SdrCaptionObj& operator*()  { return *mpCaption; }
-
-    // Does not default to nullptr to make it visually obvious where such is used.
-    void reset( SdrCaptionObj* p );
-
-    /** Insert to draw page. The caption object is owned by the draw page then.
-     */
-    void insertToDrawPage( SdrPage& rDrawPage );
-
-    /** Remove from draw page. The caption object is not owned anymore by the
-        draw page then.
-     */
-    void removeFromDrawPage( SdrPage& rDrawPage );
-
-    /** Remove from draw page and free caption object if no Undo recording.
-     */
-    void removeFromDrawPageAndFree( bool bIgnoreUndo = false );
-
-    /** Release all management of the SdrCaptionObj* in all instances of this
-        list and dissolve. The SdrCaptionObj pointer returned is ready to be
-        managed elsewhere.
-     */
-    SdrCaptionObj* release();
-
-    /** Forget the SdrCaptionObj pointer in this one instance.
-        Decrements a use count but does not destroy the object, it's up to the
-        caller to manage this mess..
-     */
-    void forget();
-
-    /** Flag that this instance is in Undo, so drawing layer owns it. */
-    void setNotOwner();
-
-    oslInterlockedCount getRefs() const;
-
-private:
-
-    struct Head
-    {
-        ScCaptionPtr*       mpFirst;        ///< first in list
-        oslInterlockedCount mnRefs;         ///< use count
-
-        Head() = delete;
-        explicit Head( ScCaptionPtr* );
-    };
-
-    Head*                 mpHead;       ///< points to the "master" entry
-    mutable ScCaptionPtr* mpNext;       ///< next in list
-    SdrCaptionObj*        mpCaption;    ///< the caption object, managed by head master
-    bool                  mbNotOwner;   ///< whether this caption object is owned by something else, e.g. held in Undo
-                                            /* TODO: can that be moved to Head?
-                                             * It's unclear when to reset, so
-                                             * each instance has its own flag.
-                                             * The last reference count
-                                             * decrement automatically has the
-                                             * then current state available.
-                                             * */
-
-    void newHead();             //< Allocate a new Head and init.
-    void incRef() const;
-    bool decRef() const;        //< @returns <TRUE/> if the last reference was decremented.
-    void decRefAndDestroy();    //< Destroys caption object if the last reference was decremented.
-
-    /** Remove from current list and close gap.
-
-        Usually there are only very few instances, so maintaining a doubly
-        linked list isn't worth memory/performance wise and a simple walk does
-        it.
-     */
-    void removeFromList();
-
-    /** Replace this instance with pNew in a list, if any.
-
-        Used by move-ctor and move assignment operator.
-     */
-    void replaceInList( ScCaptionPtr* pNew );
-
-    /** Dissolve list when the caption object is released or gone. */
-    void dissolve();
-
-    /** Just clear everything, while dissolving the list. */
-    void clear();
-};
-
 /** Internal data for a cell annotation. */
 struct SC_DLLPUBLIC ScNoteData
 {
@@ -148,7 +44,7 @@ struct SC_DLLPUBLIC ScNoteData
     OUString     maDate;             /// Creation date of the note.
     OUString     maAuthor;           /// Author of the note.
     ScCaptionInitDataRef mxInitData;        /// Initial data for invisible notes without SdrObject.
-    ScCaptionPtr        mxCaption;          /// Drawing object representing the cell note.
+    std::shared_ptr< SdrCaptionObj >    m_pCaption;          /// Drawing object representing the cell note.
     bool                mbShown;            /// True = note is visible.
 
     explicit            ScNoteData( bool bShown = false );
@@ -235,12 +131,12 @@ public:
         contains initial caption data needed to construct a caption object.
         The SdrCaptionObj* returned is still managed by the underlying
         ScNoteData::ScCaptionPtr and must not be stored elsewhere. */
-    SdrCaptionObj*      GetCaption() const { return maNoteData.mxCaption.get();}
+    const std::shared_ptr< SdrCaptionObj>& GetCaption() const { return maNoteData.m_pCaption; }
     /** Returns the caption object of this note. Creates the caption object, if
         the note contains initial caption data instead of the caption.
         The SdrCaptionObj* returned is still managed by the underlying
         ScNoteData::ScCaptionPtr and must not be stored elsewhere. */
-    SdrCaptionObj*      GetOrCreateCaption( const ScAddress& rPos ) const;
+    const std::shared_ptr< SdrCaptionObj>& GetOrCreateCaption( const ScAddress& rPos ) const;
 
     /** Forgets the pointer to the note caption object.
 
@@ -268,7 +164,7 @@ private:
     /** Creates the caption object from initial caption data if existing. */
     void                CreateCaptionFromInitData( const ScAddress& rPos ) const;
     /** Creates a new caption object at the passed cell position, clones passed existing caption. */
-    void                CreateCaption( const ScAddress& rPos, const SdrCaptionObj* pCaption = nullptr );
+    void                CreateCaption( const ScAddress& rPos, const std::shared_ptr< SdrCaptionObj >& pCaption );
     /** Removes the caption object from the drawing layer, if this note is its owner. */
     void                RemoveCaption();
 
@@ -283,7 +179,7 @@ class SC_DLLPUBLIC ScNoteUtil
 public:
 
     /** Creates and returns a caption object for a temporary caption. */
-    static ScCaptionPtr CreateTempCaption( ScDocument& rDoc, const ScAddress& rPos,
+    static std::shared_ptr< SdrCaptionObj > CreateTempCaption( ScDocument& rDoc, const ScAddress& rPos,
                             SdrPage& rDrawPage, const OUString& rUserText,
                             const tools::Rectangle& rVisRect, bool bTailFront );
 

sc/inc/scmod.hxx
@@ -147,8 +147,6 @@ public:
     void                SetDragJump(
         ScDocument* pLocalDoc, const OUString& rTarget, const OUString& rText );
 
-    static ScDocument*  GetClipDoc();       // called from document - should be removed later
-
     //  X selection:
     ScSelectionTransferObj* GetSelectionTransfer() const    { return m_pSelTransfer; }
     void                SetSelectionTransfer( ScSelectionTransferObj* pNew );

sc/qa/unit/ucalc.cxx
@@ -5339,7 +5339,7 @@ void Test::testNoteLifeCycle()
 
     // Re-insert the note back to the same place.
     m_pDoc->SetNote(aPos, pNote);
-    SdrCaptionObj* pCaption = pNote->GetOrCreateCaption(aPos);
+    SdrCaptionObj* pCaption = pNote->GetOrCreateCaption(aPos).get();
     CPPUNIT_ASSERT_MESSAGE("Failed to create a caption object.", pCaption);
     CPPUNIT_ASSERT_EQUAL_MESSAGE("This caption should belong to the drawing layer of the document.",
                            m_pDoc->GetDrawLayer(), static_cast<ScDrawLayer*>(&pCaption->getSdrModelFromSdrObject()));
@@ -5355,7 +5355,7 @@ void Test::testNoteLifeCycle()
     ScPostIt* pClipNote = aClipDoc.GetNote(aPos);
     CPPUNIT_ASSERT_MESSAGE("Failed to copy note to the clipboard.", pClipNote);
     CPPUNIT_ASSERT_EQUAL_MESSAGE("Note on the clipboard should share the same caption object from the original.",
-                           pCaption, pClipNote->GetCaption());
+                           pCaption, pClipNote->GetCaption().get());
 
 
     // Move B2 to B3 with note, which creates an ScUndoDragDrop, and Undo.
@@ -5363,7 +5363,7 @@ void Test::testNoteLifeCycle()
     ScAddress aOrigPos(aPos);
     ScAddress aMovePos(1,2,0);
     ScPostIt* pOrigNote = m_pDoc->GetNote(aOrigPos);
-    const SdrCaptionObj* pOrigCaption = pOrigNote->GetOrCreateCaption(aOrigPos);
+    const SdrCaptionObj* pOrigCaption = pOrigNote->GetOrCreateCaption(aOrigPos).get();
     bool const bCut = true;       // like Drag&Drop
     bool bRecord = true;    // record Undo
     bool const bPaint = false;    // don't care about
@@ -5381,7 +5381,7 @@ void Test::testNoteLifeCycle()
     // The caption object should not be identical, it was newly created upon
     // Drop from clipboard.
     // pOrigCaption is a dangling pointer.
-    const SdrCaptionObj* pMoveCaption = pMoveNote->GetOrCreateCaption(aMovePos);
+    const SdrCaptionObj* pMoveCaption = pMoveNote->GetOrCreateCaption(aMovePos).get();
     CPPUNIT_ASSERT_MESSAGE("Captions identical after move.", pOrigCaption != pMoveCaption);
 
     SfxUndoManager* pUndoMgr = m_pDoc->GetUndoManager();
@@ -5396,7 +5396,7 @@ void Test::testNoteLifeCycle()
 
     // The caption object still should not be identical.
     // pMoveCaption is a dangling pointer.
-    pOrigCaption = pOrigNote->GetOrCreateCaption(aOrigPos);
+    pOrigCaption = pOrigNote->GetOrCreateCaption(aOrigPos).get();
     CPPUNIT_ASSERT_MESSAGE("Captions identical after move undo.", pOrigCaption != pMoveCaption);
 
 
@@ -5405,7 +5405,7 @@ void Test::testNoteLifeCycle()
     ScAddress aPosB4(1,3,0);
     ScPostIt* pNoteB4 = m_pDoc->GetOrCreateNote(aPosB4);
     CPPUNIT_ASSERT_MESSAGE("Failed to insert cell comment at B4.", pNoteB4);
-    const SdrCaptionObj* pCaptionB4 = pNoteB4->GetOrCreateCaption(aPosB4);
+    const SdrCaptionObj* pCaptionB4 = pNoteB4->GetOrCreateCaption(aPosB4).get();
     ScCellMergeOption aCellMergeOption(1,3,2,3);
     rDocFunc.MergeCells( aCellMergeOption, true /*bContents*/, bRecord, bApi, false /*bEmptyMergedCells*/ );
 
@@ -5417,7 +5417,7 @@ void Test::testNoteLifeCycle()
     // at B4 after the merge and not cloned nor recreated during Undo.
     ScPostIt* pUndoNoteB4 = m_pDoc->GetNote(aPosB4);
     CPPUNIT_ASSERT_MESSAGE("No cell comment at B4 after Undo.", pUndoNoteB4);
-    const SdrCaptionObj* pUndoCaptionB4 = pUndoNoteB4->GetCaption();
+    const SdrCaptionObj* pUndoCaptionB4 = pUndoNoteB4->GetCaption().get();
     CPPUNIT_ASSERT_EQUAL_MESSAGE("Captions not identical after Merge Undo.", pCaptionB4, pUndoCaptionB4);
 
 
@@ -5433,7 +5433,7 @@ void Test::testNoteLifeCycle()
         ScAddress aPosB5(1,4,0);
         ScPostIt* pOtherNoteB5 = pDoc2->GetOrCreateNote(aPosB5);
         CPPUNIT_ASSERT_MESSAGE("Failed to insert cell comment at B5.", pOtherNoteB5);
-        const SdrCaptionObj* pOtherCaptionB5 = pOtherNoteB5->GetOrCreateCaption(aPosB5);
+        const SdrCaptionObj* pOtherCaptionB5 = pOtherNoteB5->GetOrCreateCaption(aPosB5).get();
         CPPUNIT_ASSERT_MESSAGE("No caption at B5.", pOtherCaptionB5);
 
         ScDocument aClipDoc2(SCDOCMODE_CLIP);
@@ -5451,7 +5451,7 @@ void Test::testNoteLifeCycle()
         pasteFromClip( m_pDoc, aPosB5, &aClipDoc2); // should not crash... tdf#104967
         ScPostIt* pNoteB5 = m_pDoc->GetNote(aPosB5);
         CPPUNIT_ASSERT_MESSAGE("Failed to paste cell comment at B5.", pNoteB5);
-        const SdrCaptionObj* pCaptionB5 = pNoteB5->GetOrCreateCaption(aPosB5);
+        const SdrCaptionObj* pCaptionB5 = pNoteB5->GetOrCreateCaption(aPosB5).get();
         CPPUNIT_ASSERT_MESSAGE("No caption at pasted B5.", pCaptionB5);
         // Do not test if  pCaptionB5 != pOtherCaptionB5  because since pDoc2
         // has been closed and the caption been deleted objects *may* be

sc/source/core/data/documen3.cxx
@@ -76,6 +76,7 @@
 #include <refupdatecontext.hxx>
 #include <scopetools.hxx>
 #include <filterentries.hxx>
+#include <docsh.hxx>
 
 #include <globalnames.hxx>
 #include <LibreOfficeKit/LibreOfficeKitEnums.h>
@@ -1093,7 +1094,7 @@ void ScDocument::UpdateReference(
     // After moving, no clipboard move ref-updates are possible
     if (rCxt.meMode != URM_COPY && IsClipboardSource())
     {
-        ScDocument* pClipDoc = ScModule::GetClipDoc();
+        ScDocument* pClipDoc = static_cast<ScDocShell*>(mpShell)->GetClipDoc();
         if (pClipDoc)
             pClipDoc->GetClipParam().mbCutMode = false;
     }

sc/source/core/tool/detfunc.cxx
@@ -1425,7 +1425,7 @@ void ScDetectiveFunc::UpdateAllComments( ScDocument& rDoc )
                 {
                     ScPostIt* pNote = rDoc.GetNote( pData->maStart );
                     // caption should exist, we iterate over drawing objects...
-                    OSL_ENSURE( pNote && (pNote->GetCaption() == pObject), "ScDetectiveFunc::UpdateAllComments - invalid cell note" );
+                    OSL_ENSURE( pNote && (pNote->GetCaption().get() == pObject), "ScDetectiveFunc::UpdateAllComments - invalid cell note" );
                     if( pNote )
                     {
                         ScCommentData aData( rDoc, pModel );

sc/source/filter/excel/xeescher.cxx
@@ -1196,7 +1196,7 @@ XclExpNote::XclExpNote(const XclExpRoot& rRoot, const ScAddress& rScPos,
             // TODO: additional text
             if( pScNote )
             {
-                if( SdrCaptionObj* pCaption = pScNote->GetOrCreateCaption( maScPos ) )
+                if( SdrCaptionObj* pCaption = pScNote->GetOrCreateCaption( maScPos ).get() )
                 {
                     lcl_GetFromTo( rRoot, pCaption->GetLogicRect(), maScPos.Tab(), maCommentFrom, maCommentTo );
                     if( const OutlinerParaObject* pOPO = pCaption->GetOutlinerParaObject() )

commit 2c642cc17e715914d7b5f9046b1c767e41cb15f5

Null deref in ScDocument::IsClipboardSource
Appears to be a regression introduced with
b6b1ded0a0539e7be1b5338de378a3276a6ff445 "sc: replace ScCaptionPtr with
std::shared_ptr, tdf#117997, tdf#117228".  Reported e.g. as
<https://ci.libreoffice.org//job/lo_ubsan/954/console> during
CppunitTest_sc_subsequent_export_test and CppunitTest_sc_ucalc:

> /sc/source/core/data/document.cxx:2574:28: runtime error: member call on null pointer of type 'ScDocShell'
>     #0 0x2b9971542d76 in ScDocument::IsClipboardSource() const /sc/source/core/data/document.cxx:2574:28
>     #1 0x2b99722b7762 in ScFormulaCell::ScFormulaCell(ScFormulaCell const&, ScDocument&, ScAddress const&, ScCloneFlags) /sc/source/core/data/formulacell.cxx:844:14
>     #2 0x2b996fee35b0 in ScCellValue::assign(ScCellValue const&, ScDocument&, ScCloneFlags) /sc/source/core/data/cellvalue.cxx:395:29
>     #3 0x2b997312db22 in ScChangeTrack::Clone(ScDocument*) const /sc/source/core/tool/chgtrack.cxx:4565:25
>     #4 0x2b99cd1728be in XclExpChangeTrack::CreateTempChangeTrack() /sc/source/filter/xcl97/XclExpChangeTrack.cxx:1561:12
>     #5 0x2b99cd169a74 in XclExpChangeTrack::XclExpChangeTrack(XclExpRoot const&) /sc/source/filter/xcl97/XclExpChangeTrack.cxx:1418:39
>     #6 0x2b99cab087c0 in ExcDocument::ReadDoc() /sc/source/filter/excel/excdoc.cxx:788:41
>     #7 0x2b99cadddc2e in ExportBiff5::Write() /sc/source/filter/excel/expop2.cxx:112:5
>     #8 0x2b99cab8ee83 in lcl_ExportExcelBiff(SfxMedium&, ScDocument*, SvStream*, bool, unsigned short) /sc/source/filter/excel/excel.cxx:181:16
>     #9 0x2b99cab8d342 in ScFormatFilterPluginImpl::ScExportExcel5(SfxMedium&, ScDocument*, ExportFormatExcel, unsigned short) /sc/source/filter/excel/excel.cxx:219:16
>     #10 0x2b99768df9d2 in ScDocShell::ConvertTo(SfxMedium&) /sc/source/ui/docshell/docsh.cxx:2419:30
>     #11 0x2b998a7e2550 in SfxObjectShell::SaveTo_Impl(SfxMedium&, SfxItemSet const*) /sfx2/source/doc/objstor.cxx:1513:19

sc/source/core/data/document.cxx
@@ -2568,7 +2568,7 @@ void ScDocument::SetClipParam(const ScClipParam& rParam)
 
 bool ScDocument::IsClipboardSource() const
 {
-    if (bIsClip)
+    if (bIsClip || mpShell == nullptr)
         return false;
 
     ScDocument* pClipDoc = static_cast<ScDocShell*>(mpShell)->GetClipDoc();