fcf87da871aff68201792825a001f3cbe2e68479
Justin Luth <justin_luth@sil.org>
2018-06-27 16:19:57 +0300

    tdf#118361 sw ooxmlimport: set footnote separator alignment
    If RTL is defined in the default (Normal) paragraph style,
    then the footnote separator should be right-aligned.
    The alignment is not a property that is stored
    with the footnote separator definition, and it is not a
    page property like it is in LO.
    The reverse needs to happen for LTR default paragraph style.
    
    Based on my MSWORD observations, the only place where you can
    indirectly set RightToLeft is in the "Normal" paragraph style.
    That option is disabled in all of the other styles.
    
    In LibreOffice, this is typically handled by the locale properties.
    The default PAGE style's Footnotes - Position is RIGHT for
    RTL locales, but LEFT for LTR locales.
    
    It is kinda interesting that no one from a RTL locale complained
    about wrongly-right-aligned footnote separator
    from imported LTR .docx's.
    
    Change-Id: I07d26640116cb12cb9157cd1d902b4c774dbba26

--- writerfilter/source/dmapper/PropertyIds.cxx
+++ writerfilter/source/dmapper/PropertyIds.cxx
@@ -172,6 +172,7 @@ OUString getPropertyName( PropertyIds eId )
         case PROP_FOOTER_IS_SHARED     :    sName = "FooterIsShared"; break;
         case PROP_FOOTER_IS_ON         :    sName = "FooterIsOn"; break;
         case PROP_FOOTNOTE_COUNTING    :    sName = "FootnoteCounting"; break;
+        case PROP_FOOTNOTE_LINE_ADJUST :    sName = "FootnoteLineAdjust"; break;
         case PROP_WIDTH                :    sName = "Width"; break;
         case PROP_HEIGHT               :    sName = "Height"; break;
         case PROP_TEXT_COLUMNS         :    sName = "TextColumns"; break;
