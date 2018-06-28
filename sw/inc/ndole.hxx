ff597693035d0f60dc53f884e3c456b1048d92ab
Noel Grandin <noel.grandin@collabora.co.uk>
2018-06-28 14:07:14 +0200

    loplugin:useuniqueptr in SwOLEObj
    Change-Id: I36b14c31bfdf66b158dd2cf8f6a7a125d52cddb5
    Reviewed-on: https://gerrit.libreoffice.org/56627
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

--- sw/inc/ndole.hxx
+++ sw/inc/ndole.hxx
@@ -46,7 +46,7 @@ class SW_DLLPUBLIC SwOLEObj
     // eventually buffered data if it is a chart OLE
     drawinglayer::primitive2d::Primitive2DContainer     m_aPrimitive2DSequence;
     basegfx::B2DRange                                   m_aRange;
-    DeflateData*                                  m_pDeflateData;
+    std::unique_ptr<DeflateData>                        m_pDeflateData;
 
     SwOLEObj( const SwOLEObj& rObj ) = delete;
 
