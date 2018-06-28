066917dcb517f114742dac602d3b7c290ab90999
Bartosz Kosiorek <gang65@poczta.onet.pl>
2018-06-28 09:52:50 +0200

    tdf#117335 Fix displaying GIF images
    To fix regression the sal_uInt16 was used instead of sal_uInt8.
    Verified on macOS High Sierra 10.13.5.
    I also checked if with this fix, there is no issue with displaying EMF (tdf#113197)
    Sample document from tdf#113197 is displaying correctly.
    
    Change-Id: I6504717d831a193b0a878ced2f335d34a993aed6
    Reviewed-on: https://gerrit.libreoffice.org/56578
    Tested-by: Jenkins
    Reviewed-by: Armin Le Grand <Armin.Le.Grand@cib.de>
    Reviewed-by: Bartosz Kosiorek <gang65@poczta.onet.pl>
    (cherry picked from commit 054a3586bb4808728a5cd58ea8a867539c08e55c)
    Reviewed-on: https://gerrit.libreoffice.org/56668
    Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>
    (cherry picked from commit 34bba6eaa404f5a081dcc28f9cca7fd55a10fe0c)

--- vcl/quartz/salbmp.cxx
+++ vcl/quartz/salbmp.cxx
@@ -451,13 +451,13 @@ class ImplPixelFormat8 : public ImplPixelFormat
 private:
     sal_uInt8* pData;
     const BitmapPalette& mrPalette;
-    const sal_uInt8 mnPaletteCount;
+    const sal_uInt16 mnPaletteCount;
 
 public:
     explicit ImplPixelFormat8( const BitmapPalette& rPalette )
         : pData(nullptr)
         , mrPalette(rPalette)
-        , mnPaletteCount(static_cast< sal_uInt8 >(rPalette.GetEntryCount()))
+        , mnPaletteCount(rPalette.GetEntryCount())
         {
         }
     virtual void StartLine( sal_uInt8* pLine ) override { pData = pLine; }
@@ -489,7 +489,7 @@ class ImplPixelFormat4 : public ImplPixelFormat
 private:
     sal_uInt8* pData;
     const BitmapPalette& mrPalette;
-    const sal_uInt8 mnPaletteCount;
+    const sal_uInt16 mnPaletteCount;
     sal_uInt32 mnX;
     sal_uInt32 mnShift;
 
@@ -497,7 +497,7 @@ public:
     explicit ImplPixelFormat4( const BitmapPalette& rPalette )
         : pData(nullptr)
         , mrPalette(rPalette)
-        , mnPaletteCount(static_cast< sal_uInt8 >(rPalette.GetEntryCount()))
+        , mnPaletteCount(rPalette.GetEntryCount())
         , mnX(0)
         , mnShift(0)
         {
@@ -545,14 +545,14 @@ class ImplPixelFormat1 : public ImplPixelFormat
 private:
     sal_uInt8* pData;
     const BitmapPalette& mrPalette;
-    const sal_uInt8 mnPaletteCount;
+    const sal_uInt16 mnPaletteCount;
     sal_uInt32 mnX;
 
 public:
     explicit ImplPixelFormat1( const BitmapPalette& rPalette )
         : pData(nullptr)
         , mrPalette(rPalette)
-        , mnPaletteCount(static_cast< sal_uInt8 >(rPalette.GetEntryCount()))
+        , mnPaletteCount(rPalette.GetEntryCount())
         , mnX(0)
         {
         }
