9d7826c42ed7f008fe839b9acfc62bf514a1d0cd
Jens Carl <j.carl43@gmx.de>
2018-06-29 03:08:42 +0000

    Remove obsolete (cargo-cult copied) dependencies
    Change-Id: I0229c3221a2b4d1bc0b8fa3a0347c9e97136f333

--- sc/CppunitTest_sc_tablesheetsobj.mk
+++ sc/CppunitTest_sc_tablesheetsobj.mk
@@ -11,51 +11,22 @@
 
 $(eval $(call gb_CppunitTest_CppunitTest,sc_tablesheetsobj))
 
-$(eval $(call gb_CppunitTest_add_exception_objects,sc_tablesheetsobj, \
-    sc/qa/extras/sctablesheetsobj \
-))
-
 $(eval $(call gb_CppunitTest_use_external,sc_tablesheetsobj,boost_headers))
 
+$(eval $(call gb_CppunitTest_add_exception_objects,sc_tablesheetsobj, \
+	sc/qa/extras/sctablesheetsobj \
+))
+
 $(eval $(call gb_CppunitTest_use_libraries,sc_tablesheetsobj, \
-    basegfx \
-    comphelper \
-    cppu \
-    cppuhelper \
-    drawinglayer \
-    editeng \
-    for \
-    forui \
-    i18nlangtag \
-    msfilter \
-    oox \
-    sal \
-    salhelper \
-    sax \
-    sb \
-    sc \
-    sfx \
-    sot \
-    subsequenttest \
-    svl \
-    svt \
-    svx \
-    svxcore \
+	cppu \
+	sal \
+	subsequenttest \
 	test \
-    tk \
-    tl \
-    ucbhelper \
 	unotest \
-    utl \
-    vbahelper \
-    vcl \
-    xo \
 ))
 
 $(eval $(call gb_CppunitTest_set_include,sc_tablesheetsobj,\
-    -I$(SRCDIR)/sc/source/ui/inc \
-    -I$(SRCDIR)/sc/inc \
-    $$(INCLUDE) \
+	$$(INCLUDE) \
 ))
 
 $(eval $(call gb_CppunitTest_use_sdk_api,sc_tablesheetsobj))
@@ -64,7 +35,7 @@ $(eval $(call gb_CppunitTest_use_ure,sc_tablesheetsobj))
 $(eval $(call gb_CppunitTest_use_vcl,sc_tablesheetsobj))
 
 $(eval $(call gb_CppunitTest_use_components,sc_tablesheetsobj,\
-    $(sc_unoapi_common_components) \
+	$(sc_unoapi_common_components) \
 ))
 
 $(eval $(call gb_CppunitTest_use_configuration,sc_tablesheetsobj))
