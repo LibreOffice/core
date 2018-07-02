d04937fb26aa903a52ce463e1fac12f3b5c0c661
Manuj Vashist <manujvashist@gmail.com>
2018-07-02 17:14:11 +0530

    added Text transformation in data provider dlg
    Change-Id: I69d8aae9289ad6d1cbdf92307c16299a533fcd56

--- sc/UIConfig_scalc.mk
+++ sc/UIConfig_scalc.mk
@@ -156,6 +156,7 @@ $(eval $(call gb_UIConfig_add_uifiles,modules/scalc,\
 	sc/uiconfig/scalc/ui/managenamesdialog \
 	sc/uiconfig/scalc/ui/mergecellsdialog \
 	sc/uiconfig/scalc/ui/mergecolumnentry \
+	sc/uiconfig/scalc/ui/texttransformationentry \
 	sc/uiconfig/scalc/ui/movecopysheet \
 	sc/uiconfig/scalc/ui/movingaveragedialog \
 	sc/uiconfig/scalc/ui/multipleoperationsdialog \
