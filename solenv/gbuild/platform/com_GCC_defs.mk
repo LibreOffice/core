d84a5aa3d49b8126012d3241763d8d04438c0839
Michael Stahl <Michael.Stahl@cib.de>
2018-07-02 16:55:26 +0200

    gbuild: avoid -Wunused-macros with clang and icecream+ccache
    On Fedora, the recommended way to use icecream with ccache is to set
    CCACHE_PREFIX=icecc - but then $CC does not indicate that icecream is
    used.
    
    Change-Id: Ie757e6c00b07df7664c368c0e9f9c9bc599f3651
    Reviewed-on: https://gerrit.libreoffice.org/56815
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <Michael.Stahl@cib.de>
    (cherry picked from commit fa503091cce61b0288645efeeab0937b11fe5403)

--- solenv/gbuild/platform/com_GCC_defs.mk
+++ solenv/gbuild/platform/com_GCC_defs.mk
@@ -55,7 +55,7 @@ gb_CFLAGS_COMMON := \
 	-Wstrict-prototypes \
 	-Wundef \
 	-Wunreachable-code \
-	$(if $(and $(COM_IS_CLANG),$(findstring icecc,$(CC))),,-Wunused-macros) \
+	$(if $(and $(COM_IS_CLANG),$(or $(findstring icecc,$(CC)),$(findstring icecc,$(CCACHE_PREFIX)))),,-Wunused-macros) \
 	-finput-charset=UTF-8 \
 	-fmessage-length=0 \
 	-fno-common \
@@ -69,7 +69,7 @@ gb_CXXFLAGS_COMMON := \
 	-Wextra \
 	-Wundef \
 	-Wunreachable-code \
-	$(if $(and $(COM_IS_CLANG),$(findstring icecc,$(CXX))),,-Wunused-macros) \
+	$(if $(and $(COM_IS_CLANG),$(or $(findstring icecc,$(CC)),$(findstring icecc,$(CCACHE_PREFIX)))),,-Wunused-macros) \
 	-finput-charset=UTF-8 \
 	-fmessage-length=0 \
 	-fno-common \
