457acbfa304ac8bda0755c9ca8f1e1e22e490ac8
Miklos Vajna <vmiklos@collabora.co.uk>
2018-07-03 17:18:55 +0200

    UITest_writer_tests: split this into 4 parts
    Before: make -sr -j2 UITest_writer_tests UITest_writer_tests2 -> 5m9,863s
    After: make -sr -j4 UITest_writer_tests UITest_writer_tests2 UITest_writer_tests3 UITest_writer_tests4 -> 2m21,734s
    
    Again just assuming that each .py file means the same amount of CPU cost
    (which may not be true exactly).
    
    Change-Id: I9c4144c40eb1116d4b999bbba68e2fbef440af7b
    Reviewed-on: https://gerrit.libreoffice.org/56880
    Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
    Tested-by: Jenkins

--- sw/Module_sw.mk
+++ sw/Module_sw.mk
@@ -135,6 +135,8 @@ $(eval $(call gb_Module_add_screenshot_targets,sw,\
 $(eval $(call gb_Module_add_uicheck_targets,sw,\
 	UITest_writer_tests \
 	UITest_writer_tests2 \
+	UITest_writer_tests3 \
+	UITest_writer_tests4 \
 ))
 endif
 
