770b6a14425b4c4b5ab0c598630f9b2b8514db18
Jan-Marek Glogowski <glogow@fbihome.de>
2018-07-06 09:44:21 +0200

    loplugin:externandnotdefined + loplugin:unreffun
    Change-Id: I364cfdb3b26096d986b651bd3e0673fa3dbf5f83

--- vcl/qa/cppunit/complextext.cxx
+++ vcl/qa/cppunit/complextext.cxx
@@ -9,9 +9,6 @@
 
 #include <ostream>
 #include <vector>
-#if !defined(_WIN32)
-std::ostream& operator<<(std::ostream& rStream, const std::vector<long>& rVec);
-#endif
 
 #include <unotest/filters-test.hxx>
 #include <test/bootstrapfixture.hxx>
@@ -25,8 +22,8 @@ std::ostream& operator<<(std::ostream& rStream, const std::vector<long>& rVec);
 
 #include <config_test.h>
 
-#if !defined(_WIN32)
-std::ostream& operator<<(std::ostream& rStream, const std::vector<long>& rVec)
+#if !defined(_WIN32) && !TEST_FONTS_MISSING
+static std::ostream& operator<<(std::ostream& rStream, const std::vector<long>& rVec)
 {
     rStream << "{ ";
     for (size_t i = 0; i < rVec.size() - 1; i++)
@@ -42,8 +39,10 @@ class VclComplexTextTest : public test::BootstrapFixture
 public:
     VclComplexTextTest() : BootstrapFixture(true, false) {}
 
+#if !TEST_FONTS_MISSING
     /// Play with font measuring etc.
     void testArabic();
+#endif
 #if defined(_WIN32)
     void testTdf95650(); // Windows-only issue
 #endif
