124f3126b2a20154416408168946844deadde856
Noel Grandin <noel.grandin@collabora.co.uk>
2018-07-06 13:12:05 +0200

    various loplugin in qt5
    loplugin:includeform
    loplugin:staticaccess
    loplugin:loopvartoosmall
    loplugin:redundantcast
    
    Change-Id: Ibd4848f0c6076d5ae95fff2853e1f6e0ba1c083b

--- vcl/qt5/Qt5Bitmap.cxx
+++ vcl/qt5/Qt5Bitmap.cxx
@@ -146,7 +146,7 @@ bool Qt5Bitmap::Create(const SalBitmap& rSalBmp, sal_uInt16 nNewBitCount)
         sal_uInt32 nWidth = pBitmap->m_aSize.Height() / 2;
         bool isOdd(0 != pBitmap->m_aSize.Height() % 2);
 
-        for (sal_uInt32 h = 0; h < pBitmap->m_aSize.Height(); ++h)
+        for (long h = 0; h < pBitmap->m_aSize.Height(); ++h)
         {
             sal_uInt8* buffer_data = buffer_data_pos;
             buffer_data_pos += pBitmap->m_nScanline;
