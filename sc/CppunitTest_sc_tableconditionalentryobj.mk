fadc32c3bc7baaefd1538605a3900b73e8d95638
Jens Carl <j.carl43@gmx.de>
2018-07-06 04:38:48 +0000

    Remove obsolete (cargo-cult copied) dependencies
    Change-Id: I72d34c875951a8b4a349d92b0d893fdaecd718e7
    Reviewed-on: https://gerrit.libreoffice.org/57033
    Tested-by: Jenkins
    Reviewed-by: Jens Carl <j.carl43@gmx.de>

--- sc/CppunitTest_sc_tableconditionalentryobj.mk
+++ sc/CppunitTest_sc_tableconditionalentryobj.mk
@@ -18,42 +18,14 @@ $(eval $(call gb_CppunitTest_add_exception_objects,sc_tableconditionalentryobj, 
 ))
 
 $(eval $(call gb_CppunitTest_use_libraries,sc_tableconditionalentryobj, \
-	basegfx \
-	comphelper \
 	cppu \
-	cppuhelper \
-	drawinglayer \
-	editeng \
-	for \
-	forui \
-	i18nlangtag \
-	msfilter \
-	oox \
 	sal \
-	salhelper \
-	sax \
-	sb \
-	sc \
-	sfx \
-	sot \
 	subsequenttest \
-	svl \
-	svt \
-	svx \
-	svxcore \
 	test \
-	tk \
-	tl \
-	ucbhelper \
 	unotest \
-	utl \
-	vbahelper \
-	vcl \
-	xo \
 ))
 
 $(eval $(call gb_CppunitTest_set_include,sc_tableconditionalentryobj,\
-	-I$(SRCDIR)/sc/source/ui/inc \
 	-I$(SRCDIR)/sc/inc \
 	$$(INCLUDE) \
 ))
@@ -64,7 +36,7 @@ $(eval $(call gb_CppunitTest_use_ure,sc_tableconditionalentryobj))
 $(eval $(call gb_CppunitTest_use_vcl,sc_tableconditionalentryobj))
 
 $(eval $(call gb_CppunitTest_use_components,sc_tableconditionalentryobj,\
-    $(sc_unoapi_common_components) \
+	$(sc_unoapi_common_components) \
 ))
 
 $(eval $(call gb_CppunitTest_use_configuration,sc_tableconditionalentryobj))
