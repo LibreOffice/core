6b2a864306c85d63b5e27e1bdbac7510196599c5
Michael Stahl <Michael.Stahl@cib.de>
2018-07-09 17:57:13 +0200

    tdf#118578 sw: allow inserting only TextDocument
    There are 3 sw document services: TextDocument, WebDocument and
    GlobalDocument.
    
    The current logic in SwView::InsertDoc() and
    SwView_Impl::StartDocumentInserter() is to request a filter with the
    same document service as the existing target document, so you can insert
    only a GlobalDocument into a GlobalDocument, which doesn't make much
    sense.
    
    As it happens there are 2 different HTML import filters: "HTML" and
    "HTML (StarWriter)", the latter using the TextDocument service.
    
    So just hard-code to allow TextDocument regardless of the target
    document.
    
    (regression from 805fd1ca343d6295b8114a24cc29bdac332f266d)
    
    Change-Id: I7dc0dd4789fba8b61625ca8eae74a864aea383b0

--- sw/source/uibase/uiview/view2.cxx
+++ sw/source/uibase/uiview/view2.cxx
@@ -2093,7 +2093,8 @@ long SwView::InsertDoc( sal_uInt16 nSlotId, const OUString& rFileName, const OUS
     else
     {
         m_pViewImpl->StartDocumentInserter(
-            pDocSh->GetFactory().GetFactoryName(),
+            // tdf#118578 allow inserting any Writer document except GlobalDoc
+            SwDocShell::Factory().GetFactoryName(),
             LINK( this, SwView, DialogClosedHdl ),
             nSlotId
         );
