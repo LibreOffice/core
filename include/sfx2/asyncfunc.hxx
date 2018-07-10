432c46d539b7c5984e104cd71a5a0cd88b88337a
Henry Castro <hcastro@collabora.com>
2018-07-09 20:20:10 -0400

    tdf#117895: "Edit document properties before saving"...
    option leaves just-saved document modified; changes are not saved
    
    Change-Id: Icad48fe1edcfb4c10c40f297326c23110144df53
    Reviewed-on: https://gerrit.libreoffice.org/57211
    Tested-by: Jenkins
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

---
+++ include/sfx2/asyncfunc.hxx
@@ -0,0 +1,39 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef INCLUDED_SFX2_ASYNCFUNC_HXX
+#define INCLUDED_SFX2_ASYNCFUNC_HXX
+
+#include <functional>
+
+#include <com/sun/star/lang/XUnoTunnel.hpp>
+#include <cppuhelper/implbase.hxx>
+
+class AsyncFunc : public cppu::WeakImplHelper<css::lang::XUnoTunnel>
+{
+private:
+    std::function<void()> m_pAsyncFunc;
+
+public:
+    AsyncFunc(const std::function<void()>&);
+    virtual ~AsyncFunc() override;
+
+    static const css::uno::Sequence<sal_Int8>& getUnoTunnelId();
+    static AsyncFunc* getImplementation(const css::uno::Reference<css::uno::XInterface>&);
+
+    void Execute();
+
+    //XUnoTunnel
+    virtual sal_Int64 SAL_CALL
+    getSomething(const css::uno::Sequence<sal_Int8>& aIdentifier) override;
+};
+
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
