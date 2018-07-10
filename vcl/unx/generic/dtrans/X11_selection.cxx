LibreOffice/core

commit d0f44d8ba7e87aa263008d3cfc4e68294d783162

tdf#84323 - sal - add sane sleep interface: cleanup osl_waitThread
Replace osl_waitThread by osl::Thread::wait.
Use std::chrono instead of TimeValue.

Change-Id: I71691d014feeeb0c5d0ba29d048bda8e25e6e7dd
Reviewed-on: https://gerrit.libreoffice.org/57130
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/unx/generic/dtrans/X11_selection.cxx
@@ -943,11 +943,7 @@ bool SelectionManager::getPasteData( Atom selection, Atom type, Sequence< sal_In
             }
             else
             {
-                TimeValue aTVal;
-                aTVal.Seconds = 0;
-                aTVal.Nanosec = 100000000;
-                aGuard.clear();
-                osl_waitThread( &aTVal );
+                osl::Thread::wait(std::chrono::milliseconds(100));
                 aGuard.reset();
             }
             if( bHandle )
@@ -3369,15 +3365,12 @@ void SelectionManager::dragDoDispatch()
 #if OSL_DEBUG_LEVEL > 1
     fprintf( stderr, "begin executeDrag dispatching\n" );
 #endif
-    TimeValue aTVal;
-    aTVal.Seconds = 0;
-    aTVal.Nanosec = 200000000;
     oslThread aThread = m_aDragExecuteThread;
     while( m_xDragSourceListener.is() && ( ! m_bDropSent || time(nullptr)-m_nDropTimeout < 5 ) && osl_scheduleThread( aThread ) )
     {
         // let the thread in the run method do the dispatching
         // just look occasionally here whether drop timed out or is completed
-        osl_waitThread( &aTVal );
+        osl::Thread::wait(std::chrono::milliseconds(200));
     }
 #if OSL_DEBUG_LEVEL > 1
     fprintf( stderr, "end executeDrag dispatching\n" );

canvas/source/directx/dx_9rm.cxx
@@ -27,7 +27,7 @@
 #include <basegfx/vector/b2dsize.hxx>
 #include <basegfx/vector/b2isize.hxx>
 #include <com/sun/star/lang/NoSupportException.hpp>
-#include <osl/thread.h>
+#include <osl/thread.hxx>
 #include <osl/time.h>
 #include <tools/diagnose_ex.h>
 #include <vcl/syschild.hxx>
@@ -880,10 +880,7 @@ namespace dxcanvas
                             return true;
                     }
 
-                    TimeValue aTimeout;
-                    aTimeout.Seconds=1;
-                    aTimeout.Nanosec=0;
-                    osl_waitThread(&aTimeout);
+                    osl::Thread::wait(std::chrono::seconds(1));
                 }
                 while(hr == D3DERR_DEVICELOST);
 

io/test/stm/pumptest.cxx
@@ -35,7 +35,7 @@
 #include <uno/mapping.hxx>
 #include <cppuhelper/implbase.hxx>
 #include <cppuhelper/factory.hxx>
-#include <osl/thread.h>
+#include <osl/thread.hxx>
 #include <list>
 
 
@@ -50,8 +50,7 @@ using namespace ::com::sun::star::test;
 
 static void mywait()
 {
-    TimeValue a = { 0, 10000 };
-    osl_waitThread( &a );
+    osl::Thread::wait(std::chrono::microseconds(10));
     osl_yieldThread();
     osl_yieldThread();
 }

io/test/testconnection.cxx
@@ -127,8 +127,7 @@ void testConnection( const OUString &sConnectionDescription  ,
             try
             {
                 // Why is this wait necessary ????
-                TimeValue value = {1,0};
-                osl_waitThread( &value );
+                osl::Thread::wait(std::chrono::seconds(1));
                 r = rConnector->connect( sConnectionDescription );
                 OSL_ASSERT( r.is() );
                 doWrite( r );
@@ -238,8 +237,7 @@ int SAL_CALL main( int argc, char * argv[] )
     MyThread thread( rAcceptor , OUString("socket,host=localhost,port=2001") );
     thread.create();
 
-    TimeValue value = {0,1};
-    osl_waitThread( &value );
+    osl::Thread::wait(std::chrono::nanoseconds(1));
     try
     {
         rAcceptor->accept( OUString("socket,host=localhost,port=2001") );

package/source/zipapi/ZipOutputStream.cxx
@@ -154,8 +154,7 @@ void ZipOutputStream::reduceScheduledThreadsToGivenNumberOrLess(sal_Int32 nThrea
 
         if(static_cast< sal_Int32 >(m_aEntries.size()) > nThreads)
         {
-            const TimeValue aTimeValue(0, 100000);
-            osl_waitThread(&aTimeValue);
+            osl::Thread::wait(std::chrono::microseconds(100));
         }
     }
 }

sal/qa/osl/mutex/osl_Mutex.cxx
@@ -34,10 +34,7 @@ namespace ThreadHelper
 {
     void thread_sleep_tenth_sec(sal_uInt32 _nTenthSec)
     {
-        TimeValue nTV;
-        nTV.Seconds = _nTenthSec/10;
-        nTV.Nanosec = ( (_nTenthSec%10 ) * 100000000 );
-        osl_waitThread(&nTV);
+        osl::Thread::wait(std::chrono::milliseconds(_nTenthSec * 100));
     }
     void thread_sleep( sal_uInt32 _nSec )
     {

sal/qa/osl/pipe/osl_Pipe.cxx
@@ -743,10 +743,7 @@ namespace osl_StreamPipe
         /// print statement in thread process must use fflush() to force display.
         fflush(stdout);
 
-        TimeValue nTV;
-        nTV.Seconds = _nSec;
-        nTV.Nanosec = 0;
-        osl_waitThread(&nTV);
+        osl::Thread::wait(std::chrono::seconds(_nSec));
     }
     // test read/write & send/recv data to pipe
 

sal/qa/osl/process/osl_Thread.cxx
@@ -180,14 +180,7 @@ namespace ThreadHelper
 {
     void thread_sleep_tenth_sec(sal_Int32 _nTenthSec)
     {
-#ifdef _WIN32
-        Sleep(_nTenthSec * 100 );
-#else
-        TimeValue nTV;
-        nTV.Seconds = static_cast<sal_uInt32>( _nTenthSec/10 );
-        nTV.Nanosec = ( (_nTenthSec%10 ) * 100000000 );
-        osl_waitThread(&nTV);
-#endif
+        osl::Thread::wait(std::chrono::milliseconds(_nTenthSec * 100));
     }
 
     void outputPriority(oslThreadPriority const& _aPriority)

commit ef1d1fd3cd86c137cf3a654da066f9efcf5aa6f7

tdf#118661 LibreOffice remains in background after closing it
regression from
    commit d0f44d8ba7e87aa263008d3cfc4e68294d783162
    tdf#84323 - sal - add sane sleep interface: cleanup osl_waitThread

Change-Id: I55c2fa6c8993cc6268b1c5d9331c665c401e8efe

vcl/unx/generic/dtrans/X11_selection.cxx
@@ -943,6 +943,7 @@ bool SelectionManager::getPasteData( Atom selection, Atom type, Sequence< sal_In
             }
             else
             {
+                aGuard.clear();
                 osl::Thread::wait(std::chrono::milliseconds(100));
                 aGuard.reset();
             }