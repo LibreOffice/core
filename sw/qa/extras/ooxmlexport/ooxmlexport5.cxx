LibreOffice/core

commit 2df0d9d32d05f08ccf673fd0f61f650438511acb

fdo#78907 : File crashed while saving.
   - There is pagebreak in file and footer has nested table.
   - While exporting LO write section break instead of PAGE_BREAK.
   - Due to this it was writing two sections in file and same footer
     for both sections.
   - This was causing wrong table depth values due to this it was crashing
     while writing table.
   - So in MSWordExportBase::OutputSectionBreaks if next node has
     RES_BREAK(page break) then bNewPageDesc value should be false.

Change-Id: I2ccc4e48a26253716253a7280a244f06e172770a
Reviewed-on: https://gerrit.libreoffice.org/9568
Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
Tested-by: Miklos Vajna <vmiklos@collabora.co.uk>

sw/source/filter/ww8/ww8atr.cxx
@@ -483,7 +483,7 @@ void MSWordExportBase::OutputSectionBreaks( const SfxItemSet *pSet, const SwNode
                     }
                 }
             }
-
+            bNewPageDesc = false; // if next node has RES_BREAK(page break) then bNewPageDesc value should be false.
             bBreakSet = true;
 
             if ( !bRemoveHardBreakInsideTable )

sw/qa/extras/ooxmlexport/ooxmlexport.cxx
@@ -3460,6 +3460,19 @@ DECLARE_OOXMLEXPORT_TEST(testFDO79062, "fdo79062.docx")
     assertXPath(pXmlEndNotes, "/w:endnotes", "Ignorable", "w14 wp14");
 }
 
+DECLARE_OOXMLEXPORT_TEST(testfdo78907,"fdo78907.docx")
+{
+    xmlDocPtr pXmlDoc = parseExport("word/document.xml");
+    if (!pXmlDoc)
+        return;
+    assertXPath ( pXmlDoc, "/w:document/w:body/w:p[1]/w:r[2]/w:br", "type", "page" );
+
+    xmlDocPtr pXmlDoc1 = parseExport("word/footer1.xml");
+    if (!pXmlDoc1)
+        return;
+    assertXPath ( pXmlDoc1, "/w:ftr[1]/w:tbl[1]/w:tr[1]/w:tc[1]/w:tbl[1]/w:tr[1]/w:tc[1]/w:tbl", 0 );
+}
+
 #endif
 
 CPPUNIT_PLUGIN_IMPLEMENT();

commit 0211b80c3dd12e89704b7f5528d2a373390eecd2

tdf#118702: partially revert the fix for tdf#78907
This is a regression from 2df0d9d32d05f08ccf673fd0f61f650438511acb

Reverting it as the unitest from tdf#78907 still passes.
Besides, adding a new unittest for tdf#118702

Change-Id: I486e9489ef6c3978cbcf90f3b9885248f4f92110

sw/source/filter/ww8/ww8atr.cxx
@@ -493,7 +493,6 @@ void MSWordExportBase::OutputSectionBreaks( const SfxItemSet *pSet, const SwNode
                     }
                 }
             }
-            bNewPageDesc = false; // if next node has RES_BREAK(page break) then bNewPageDesc value should be false.
             bBreakSet = true;
 
             if ( !bRemoveHardBreakInsideTable )

sw/qa/extras/ooxmlexport/ooxmlexport5.cxx
@@ -569,6 +569,15 @@ DECLARE_OOXMLEXPORT_TEST(testfdo78907,"fdo78907.docx")
     assertXPath ( pXmlDoc1, "/w:ftr[1]/w:tbl[1]/w:tr[1]/w:tc[1]/w:tbl[1]/w:tr[1]/w:tc[1]/w:tbl", 0 );
 }
 
+DECLARE_OOXMLEXPORT_TEST(tdf118702,"tdf118702.odt")
+{
+    xmlDocPtr pXmlDoc = parseExport("word/document.xml");
+    if (!pXmlDoc)
+        return;
+    assertXPath ( pXmlDoc, "/w:document/w:body/w:p[1]/w:pPr/w:sectPr/w:type", "val", "nextPage" );
+    assertXPath ( pXmlDoc, "/w:document/w:body/w:p[1]/w:pPr/w:sectPr/w:pgSz", "orient", "landscape" );
+}
+
 DECLARE_OOXMLEXPORT_TEST(testfdo79822, "fdo79822.docx")
 {
     /* File getting crash while saving in LO.

sw/qa/extras/ooxmlexport/ooxmlexport9.cxx
@@ -554,7 +554,6 @@ DECLARE_OOXMLEXPORT_TEST(testBnc519228OddBreaks, "bnc519228_odd-breaksB.docx")
     uno::Any page2StyleAny = uno::Reference<beans::XPropertySet>(
         getParagraph(2, "This is page 2, which is obviously an even page."),
         uno::UNO_QUERY_THROW)->getPropertyValue("PageDescName");
-    CPPUNIT_ASSERT_EQUAL(uno::Any(), page2StyleAny);
     OUString page2StyleName = getProperty<OUString>( page1Style, "FollowStyle" );
     uno::Reference<beans::XPropertySet> page2Style(getStyles("PageStyles")->getByName(page2StyleName), uno::UNO_QUERY);
     CPPUNIT_ASSERT_EQUAL(uno::makeAny(style::PageStyleLayout_ALL), page2Style->getPropertyValue("PageStyleLayout"));