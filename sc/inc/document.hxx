e06323c11e58cb9278fdb16c12ac86cd5c2d7344
Caolán McNamara <caolanm@redhat.com>
2018-07-16 12:13:01 +0100

    coverity#1401328 Uncaught exception
    Change-Id: I112260f1ea02c2173be56dde1d78497fc8339cec

--- sc/inc/document.hxx
+++ sc/inc/document.hxx
@@ -35,10 +35,11 @@
 #include "typedstrdata.hxx"
 #include "calcmacros.hxx"
 #include "calcconfig.hxx"
+#include <o3tl/deleter.hxx>
 #include <svl/hint.hxx>
-#include <tools/gen.hxx>
-#include <svl/zforlist.hxx>
 #include <svl/typedwhich.hxx>
+#include <svl/zforlist.hxx>
+#include <tools/gen.hxx>
 
 #include <cassert>
 #include <memory>
@@ -49,7 +50,6 @@
 #include "markdata.hxx"
 
 namespace com { namespace sun { namespace star { namespace chart2 { class XChartDocument; } } } }
-namespace o3tl { template <typename T> struct default_delete; }
 
 class Timer;
 
@@ -297,6 +297,9 @@ enum ScMutationGuardFlags
     CORE = 0x0001, /// Core calc data structures should not be mutated
 };
 
+typedef std::unique_ptr<ScTable, o3tl::default_delete<ScTable>> ScTableUniquePtr;
+typedef std::vector<ScTableUniquePtr> TableContainer;
+
 class ScDocument
 {
 friend class ScValueIterator;
@@ -325,8 +328,6 @@ friend class sc::FormulaGroupAreaListener;
 friend class sc::TableColumnBlockPositionSet;
 friend struct ScMutationGuard;
 friend struct ScMutationDisable;
-
-    typedef std::vector<std::unique_ptr<ScTable>> TableContainer;
 
 public:
     enum class HardRecalcState
