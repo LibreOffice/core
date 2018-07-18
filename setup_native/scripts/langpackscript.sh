77be081f0985cd24d2ed6533c84827964806bd0b
Andrea Gelmini <andrea.gelmini@gelma.net>
2018-07-18 11:29:49 +0200

    Fix typos
    Change-Id: Id31aa22a98f7520a37e624cbb1dc4d949246c0fb

--- setup_native/scripts/langpackscript.sh
+++ setup_native/scripts/langpackscript.sh
@@ -113,7 +113,7 @@ mkdir $outdir
 
 #diskSpace=`df -k $outdir | $tail_prog -1 | awk '{if ( $4 ~ /%/) { print $3 } else { print $4 } }'`
 #if [ $diskSpace -lt $diskSpaceRequired ]; then
-#    printf "You will need atleast %s kBytes of Disk Free\n" $diskSpaceRequired
+#    printf "You will need at least %s kBytes of Disk Free\n" $diskSpaceRequired
 #    printf "Please free up the required Disk Space and try again\n"
 #    exit 3
 #fi
