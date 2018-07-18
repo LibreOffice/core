95b00f1bfa341003af23e150d316d943d47909cf
Jens Carl <j.carl43@gmx.de>
2018-07-18 03:13:26 +0000

    Remove obsolete (cargo-cult copied) dependencies
    Change-Id: I73ea0f836852dc40c81f167d409ca0294100676e
    Reviewed-on: https://gerrit.libreoffice.org/57610
    Tested-by: Jenkins
    Reviewed-by: Jens Carl <j.carl43@gmx.de>

--- sc/CppunitTest_sc_recordchanges.mk
+++ sc/CppunitTest_sc_recordchanges.mk
@@ -14,48 +14,19 @@ $(eval $(call gb_CppunitTest_CppunitTest,sc_recordchanges))
 $(eval $(call gb_CppunitTest_use_external,sc_recordchanges,boost_headers))
 
 $(eval $(call gb_CppunitTest_add_exception_objects,sc_recordchanges, \
-    sc/qa/extras/recordchanges-test \
+	sc/qa/extras/recordchanges-test \
 ))
 
 $(eval $(call gb_CppunitTest_use_libraries,sc_recordchanges, \
-    basegfx \
-    comphelper \
-    cppu \
-    cppuhelper \
-    drawinglayer \
-    editeng \
-    for \
-    forui \
-    i18nlangtag \
-    msfilter \
-    oox \
-    sal \
-    salhelper \
-    sax \
-    sb \
-    sc \
-    sfx \
-    sot \
-    subsequenttest \
-    svl \
-    svt \
-    svx \
-    svxcore \
+	cppu \
+	sal \
+	subsequenttest \
 	test \
-    tk \
-    tl \
-    ucbhelper \
 	unotest \
-    utl \
-    vbahelper \
-    vcl \
-    xo \
 ))
 
 $(eval $(call gb_CppunitTest_set_include,sc_recordchanges,\
-    -I$(SRCDIR)/sc/source/ui/inc \
-    -I$(SRCDIR)/sc/inc \
-    $$(INCLUDE) \
+	$$(INCLUDE) \
 ))
 
 $(eval $(call gb_CppunitTest_use_sdk_api,sc_recordchanges))
@@ -64,7 +35,7 @@ $(eval $(call gb_CppunitTest_use_ure,sc_recordchanges))
 $(eval $(call gb_CppunitTest_use_vcl,sc_recordchanges))
 
 $(eval $(call gb_CppunitTest_use_components,sc_recordchanges,\
-    $(sc_unoapi_common_components) \
+	$(sc_unoapi_common_components) \
 ))
 
 $(eval $(call gb_CppunitTest_use_configuration,sc_recordchanges))
