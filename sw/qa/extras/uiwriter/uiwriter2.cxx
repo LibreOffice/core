27b158a408dc161605e9a92d54cf6c0b1fb5909f
Miklos Vajna <vmiklos@collabora.co.uk>
2018-07-19 15:32:59 +0200

    tdf#101534 sw: fix lost indent attributes when copying into numbered paragraph
    Commit aa91dc0ccbe53ffcec9dbfec2265de057775d4e3 (CWS-TOOLING: integrate
    CWS sw32numbf01, 2009-09-08) replaced the SwDoc::ReplaceNumRule() call
    with SwDoc::SetNumRule(), as the later handles list ids. This required a
    decision if resetting of the indent attributes is needed or not, but it
    did not document why resetting was chosen.
    
    On the other hand, the current bugdoc shows why resetting is a bad idea:
    this way if you paste into a numbered paragraph which has a non-zero
    paragraph indent inherited from the numbering + zero indent as direct
    formatting, then this later is lost during paste.
    
    Conflicts:
    	sw/qa/extras/uiwriter/uiwriter2.cxx
    
    (cherry picked from commit 340eaa6aa15c18cc2696a4a304350810cb36565c)
    
    Change-Id: Ie5dcf6ed86c240fd61c4d5bfed886910aea3648f
    Reviewed-on: https://gerrit.libreoffice.org/57770
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
    Tested-by: Miklos Vajna <vmiklos@collabora.co.uk>
    (cherry picked from commit 499c31d83f3ad6356678ae9103e7e0748ff45ad7)

---
+++ sw/qa/extras/uiwriter/uiwriter2.cxx
@@ -0,0 +1,60 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <swmodeltestbase.hxx>
+#include <com/sun/star/awt/FontSlant.hpp>
+#include <swdtflvr.hxx>
+#include <wrtsh.hxx>
+
+namespace
+{
+char const DATA_DIRECTORY[] = "/sw/qa/extras/uiwriter/data2/";
+}
+
+/// Second set of tests asserting the behavior of Writer user interface shells.
+class SwUiWriterTest2 : public SwModelTestBase
+{
+public:
+    void testTdf101534();
+
+    CPPUNIT_TEST_SUITE(SwUiWriterTest2);
+    CPPUNIT_TEST(testTdf101534);
+    CPPUNIT_TEST_SUITE_END();
+};
+
+void SwUiWriterTest2::testTdf101534()
+{
+    // Copy the first paragraph of the document.
+    load(DATA_DIRECTORY, "tdf101534.fodt");
+    SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument*>(mxComponent.get());
+    CPPUNIT_ASSERT(pTextDoc);
+    SwWrtShell* pWrtShell = pTextDoc->GetDocShell()->GetWrtShell();
+    pWrtShell->EndPara(/*bSelect=*/true);
+    rtl::Reference<SwTransferable> pTransfer = new SwTransferable(*pWrtShell);
+    pTransfer->Copy();
+
+    // Go to the second paragraph, assert that we have margins as direct
+    // formatting.
+    pWrtShell->Down(/*bSelect=*/false);
+    SfxItemSet aSet(pWrtShell->GetAttrPool(), RES_LR_SPACE, RES_LR_SPACE);
+    pWrtShell->GetCurAttr(aSet);
+    CPPUNIT_ASSERT(aSet.HasItem(RES_LR_SPACE));
+
+    // Make sure that direct formatting is preserved during paste.
+    pWrtShell->EndPara(/*bSelect=*/false);
+    TransferableDataHelper aHelper(pTransfer.get());
+    SwTransferable::Paste(*pWrtShell, aHelper);
+    pWrtShell->GetCurAttr(aSet);
+    // This failed, direct formatting was lost.
+    CPPUNIT_ASSERT(aSet.HasItem(RES_LR_SPACE));
+}
+
+CPPUNIT_TEST_SUITE_REGISTRATION(SwUiWriterTest2);
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
