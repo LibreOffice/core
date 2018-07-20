a8500a22ca24ee02f16d17f3b9ef157067032364
Markus Mohrhard <markus.mohrhard@googlemail.com>
2018-07-21 01:57:27 +0200

    fix shutdown crash when SfxApplication has already been destroyed
    See e.g. http://crashreport.libreoffice.org/stats/crash_details/dc1ae6ff-923e-44f6-99a9-84893039efd7
    
    Change-Id: I3b5a24bfb62977ca922bf3bececb6676691bf6ea

--- sfx2/source/doc/objxtor.cxx
+++ sfx2/source/doc/objxtor.cxx
@@ -312,13 +312,13 @@ SfxObjectShell::~SfxObjectShell()
     DELETEZ( pImpl->pReloadTimer );
 
     SfxApplication *pSfxApp = SfxGetpApp();
-    if ( USHRT_MAX != pImpl->nVisualDocumentNumber )
+    if ( USHRT_MAX != pImpl->nVisualDocumentNumber && pSfxApp )
         pSfxApp->ReleaseIndex(pImpl->nVisualDocumentNumber);
 
     // Destroy Basic-Manager
     pImpl->aBasicManager.reset( nullptr );
 
-    if ( pSfxApp->GetDdeService() )
+    if ( pSfxApp && pSfxApp->GetDdeService() )
         pSfxApp->RemoveDdeTopic( this );
 
     pImpl->pBaseModel.set( nullptr );
