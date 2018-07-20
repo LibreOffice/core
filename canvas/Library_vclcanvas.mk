41e92a9f39dabe7681b9e3719b10b24748e54ce4
Miklos Vajna <vmiklos@collabora.co.uk>
2018-07-20 14:09:36 +0200

    tdf#118107 canvas opengl: avoid assertion failure with negative widths
    In case an Impress shape has a "Fade in and Swivel" animation attached,
    canvas calls OutputDevice::DrawBitmapEx() with a negative width. This
    results in a call to OutputDevice::DrawDeviceAlphaBitmap(), which
    asserts that the height/width is not negative.
    
    Fix the problem by transforming the bitmap before calling
    OutputDevice::DrawBitmapEx() in the GL case, similarly to how "complex"
    transformations are handled already.
    
    Change-Id: I65ccc8a984132c5921d6096bfe9c7a8fcfacd8dd

--- canvas/Library_vclcanvas.mk
+++ canvas/Library_vclcanvas.mk
@@ -21,7 +21,10 @@ $(eval $(call gb_Library_Library,vclcanvas))
 
 $(eval $(call gb_Library_set_componentfile,vclcanvas,canvas/source/vcl/vclcanvas))
 
-$(eval $(call gb_Library_use_external,vclcanvas,boost_headers))
+$(eval $(call gb_Library_use_externals,vclcanvas,\
+	boost_headers \
+	epoxy \
+))
 
 $(eval $(call gb_Library_use_sdk_api,vclcanvas))
 
