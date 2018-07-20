05780aea430f36c63b642067be8ff2efc1b7497b
Bartosz Kosiorek <gang65@poczta.onet.pl>
2018-07-20 20:59:01 +0200

    tdf#118150 Fix displaying of chart where Legend is set manually
    By default Legend's position is automatic and it is located
    at the right side of the chart.
    Unfortunately even if layout with manualLayout is set:
    http://www.datypic.com/sc/ooxml/e-draw-chart_manualLayout-1.html
    the empty space for the automatic legend on the right side of the chart,
    was left.
    This is issue with opening of the OOXML charts by LibreOffice,
    as MS Word is opening the same document properly.
    
    This patch fixes issue with wrong chart displaying, when manual position
    of the legend is set.
    
    Change-Id: Id5363d48b049c469b6761b43cecfbb56514bf7d8
    Reviewed-on: https://gerrit.libreoffice.org/57788
    Reviewed-by: Markus Mohrhard <markus.mohrhard@googlemail.com>
    Tested-by: Jenkins
    (cherry picked from commit bfbf276c9e1b7da55b613fade763fad56a1cd6fe)

--- oox/source/drawingml/chart/titleconverter.cxx
+++ oox/source/drawingml/chart/titleconverter.cxx
@@ -213,14 +213,13 @@ void LegendConverter::convertFromModel( const Reference< XDiagram >& rxDiagram )
             case XML_r:
                 eLegendPos = cssc2::LegendPosition_LINE_END;
                 eLegendExpand = cssc::ChartLegendExpansion_HIGH;
-                break;
+            break;
             case XML_tr:    // top-right not supported
                 eLegendPos = LegendPosition_CUSTOM;
                 eRelPos.Primary = 1;
                 eRelPos.Secondary =0;
                 eRelPos.Anchor = Alignment_TOP_RIGHT;
                 bTopRight=true;
-
             break;
             case XML_t:
                 eLegendPos = cssc2::LegendPosition_PAGE_START;
@@ -236,9 +235,12 @@ void LegendConverter::convertFromModel( const Reference< XDiagram >& rxDiagram )
         if( mrModel.mxLayout.get() )
         {
             LayoutConverter aLayoutConv( *this, *mrModel.mxLayout );
-            // manual size needs ChartLegendExpansion_CUSTOM
+            // manual size needs ChartLegendExpansion_CUSTOM and LegendPosition_CUSTOM (tdf#118150)
             if( aLayoutConv.convertFromModel( aPropSet ) )
+            {
+                eLegendPos = cssc2::LegendPosition_CUSTOM;
                 eLegendExpand = cssc::ChartLegendExpansion_CUSTOM;
+            }
             bManualLayout = !aLayoutConv.getAutoLayout();
         }
 
