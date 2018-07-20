e794d6ef9e1283b57ebd3fc57189c5732eace9b6
Szymon Kłos <szymon.klos@collabora.com>
2018-07-20 17:21:43 +0200

    tdf#116350 Import preset text geometry (text effects)
    "Font effect" implementation, instead of normal
    text, content is converted to "fontwork".
    
    Change-Id: I5d02c7faedb66a4b919e64ae1b830bffb69984c1

--- oox/source/drawingml/textbodycontext.cxx
+++ oox/source/drawingml/textbodycontext.cxx
@@ -25,6 +25,7 @@
 #include <drawingml/textliststylecontext.hxx>
 #include <drawingml/textfield.hxx>
 #include <drawingml/textfieldcontext.hxx>
+#include <oox/drawingml/shape.hxx>
 #include <oox/token/namespaces.hxx>
 #include <oox/token/tokens.hxx>
 
@@ -37,158 +38,181 @@ using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star::text;
 using namespace ::com::sun::star::xml::sax;
 
-namespace oox { namespace drawingml {
-
+namespace oox
+{
+namespace drawingml
+{
 // CT_TextParagraph
 class TextParagraphContext : public ContextHandler2
 {
 public:
-    TextParagraphContext( ContextHandler2Helper const & rParent, TextParagraph& rPara );
+    TextParagraphContext(ContextHandler2Helper const& rParent, TextParagraph& rPara);
 
-    virtual ContextHandlerRef onCreateContext( sal_Int32 aElementToken, const AttributeList& rAttribs ) override;
+    virtual ContextHandlerRef onCreateContext(sal_Int32 aElementToken,
+                                              const AttributeList& rAttribs) override;
 
 protected:
     TextParagraph& mrParagraph;
 };
 
-TextParagraphContext::TextParagraphContext( ContextHandler2Helper const & rParent, TextParagraph& rPara )
-: ContextHandler2( rParent )
-, mrParagraph( rPara )
+TextParagraphContext::TextParagraphContext(ContextHandler2Helper const& rParent,
+                                           TextParagraph& rPara)
+    : ContextHandler2(rParent)
+    , mrParagraph(rPara)
 {
     mbEnableTrimSpace = false;
 }
 
-ContextHandlerRef TextParagraphContext::onCreateContext( sal_Int32 aElementToken, const AttributeList& rAttribs )
+ContextHandlerRef TextParagraphContext::onCreateContext(sal_Int32 aElementToken,
+                                                        const AttributeList& rAttribs)
 {
     // EG_TextRun
-    switch( aElementToken )
+    switch (aElementToken)
     {
-        case A_TOKEN( r ):      // "CT_RegularTextRun" Regular Text Run.
-        case W_TOKEN( r ):
+        case A_TOKEN(r): // "CT_RegularTextRun" Regular Text Run.
+        case W_TOKEN(r):
         {
-            TextRunPtr pRun( new TextRun );
-            mrParagraph.addRun( pRun );
-            return new RegularTextRunContext( *this, pRun );
+            TextRunPtr pRun(new TextRun);
+            mrParagraph.addRun(pRun);
+            return new RegularTextRunContext(*this, pRun);
         }
-        case A_TOKEN( br ): // "CT_TextLineBreak" Soft return line break (vertical tab).
+        case A_TOKEN(br): // "CT_TextLineBreak" Soft return line break (vertical tab).
         {
-            TextRunPtr pRun( new TextRun );
+            TextRunPtr pRun(new TextRun);
             pRun->setLineBreak();
-            mrParagraph.addRun( pRun );
-            return new RegularTextRunContext( *this, pRun );
+            mrParagraph.addRun(pRun);
+            return new RegularTextRunContext(*this, pRun);
         }
-        case A_TOKEN( fld ):    // "CT_TextField" Text Field.
+        case A_TOKEN(fld): // "CT_TextField" Text Field.
         {
-            std::shared_ptr< TextField > pField( new TextField );
-            mrParagraph.addRun( pField );
-            return new TextFieldContext( *this, rAttribs, *pField );
+            std::shared_ptr<TextField> pField(new TextField);
+            mrParagraph.addRun(pField);
+            return new TextFieldContext(*this, rAttribs, *pField);
         }
-        case A_TOKEN( pPr ):
-        case W_TOKEN( pPr ):
-            return new TextParagraphPropertiesContext( *this, rAttribs, mrParagraph.getProperties() );
-        case A_TOKEN( endParaRPr ):
-            return new TextCharacterPropertiesContext( *this, rAttribs, mrParagraph.getEndProperties() );
-        case W_TOKEN( sdt ):
-        case W_TOKEN( sdtContent ):
+        case A_TOKEN(pPr):
+        case W_TOKEN(pPr):
+            return new TextParagraphPropertiesContext(*this, rAttribs, mrParagraph.getProperties());
+        case A_TOKEN(endParaRPr):
+            return new TextCharacterPropertiesContext(*this, rAttribs,
+                                                      mrParagraph.getEndProperties());
+        case W_TOKEN(sdt):
+        case W_TOKEN(sdtContent):
             return this;
-        case W_TOKEN( del ):
-        break;
-        case W_TOKEN( ins ):
+        case W_TOKEN(del):
+            break;
+        case W_TOKEN(ins):
             return this;
-        break;
+            break;
         case OOX_TOKEN(a14, m):
             return CreateLazyMathBufferingContext(*this, mrParagraph);
-        break;
+            break;
         default:
-            SAL_WARN("oox", "TextParagraphContext::onCreateContext: unhandled element: " << getBaseToken(aElementToken));
+            SAL_WARN("oox", "TextParagraphContext::onCreateContext: unhandled element: "
+                                << getBaseToken(aElementToken));
     }
 
     return nullptr;
 }
 
-RegularTextRunContext::RegularTextRunContext( ContextHandler2Helper const & rParent, TextRunPtr const & pRunPtr )
-: ContextHandler2( rParent )
-, mpRunPtr( pRunPtr )
-, mbIsInText( false )
+RegularTextRunContext::RegularTextRunContext(ContextHandler2Helper const& rParent,
+                                             TextRunPtr const& pRunPtr)
+    : ContextHandler2(rParent)
+    , mpRunPtr(pRunPtr)
+    , mbIsInText(false)
 {
 }
 
-void RegularTextRunContext::onEndElement( )
+void RegularTextRunContext::onEndElement()
 {
-    switch( getCurrentElement() )
+    switch (getCurrentElement())
     {
-        case A_TOKEN( t ):
-        case W_TOKEN( t ):
+        case A_TOKEN(t):
+        case W_TOKEN(t):
         {
             mbIsInText = false;
             break;
         }
-        case A_TOKEN( r ):
-        break;
+        case A_TOKEN(r):
+            break;
     }
 }
 
-void RegularTextRunContext::onCharacters( const OUString& aChars )
+void RegularTextRunContext::onCharacters(const OUString& aChars)
 {
-    if( mbIsInText )
+    if (mbIsInText)
     {
         mpRunPtr->getText() += aChars;
     }
 }
 
-ContextHandlerRef RegularTextRunContext::onCreateContext( sal_Int32 aElementToken, const AttributeList& rAttribs)
+ContextHandlerRef RegularTextRunContext::onCreateContext(sal_Int32 aElementToken,
+                                                         const AttributeList& rAttribs)
 {
-    switch( aElementToken )
+    switch (aElementToken)
     {
-        case A_TOKEN( rPr ):    // "CT_TextCharPropertyBag" The text char properties of this text run.
-        case W_TOKEN( rPr ):
-            return new TextCharacterPropertiesContext( *this, rAttribs, mpRunPtr->getTextCharacterProperties() );
-        case A_TOKEN( t ):      // "xsd:string" minOccurs="1" The actual text string.
-        case W_TOKEN( t ):
+        case A_TOKEN(rPr): // "CT_TextCharPropertyBag" The text char properties of this text run.
+        case W_TOKEN(rPr):
+            return new TextCharacterPropertiesContext(*this, rAttribs,
+                                                      mpRunPtr->getTextCharacterProperties());
+        case A_TOKEN(t): // "xsd:string" minOccurs="1" The actual text string.
+        case W_TOKEN(t):
             mbIsInText = true;
-        break;
-        case W_TOKEN( drawing ):
-        break;
+            break;
+        case W_TOKEN(drawing):
+            break;
         default:
-            SAL_WARN("oox", "RegularTextRunContext::onCreateContext: unhandled element: " << getBaseToken(aElementToken));
-        break;
+            SAL_WARN("oox", "RegularTextRunContext::onCreateContext: unhandled element: "
+                                << getBaseToken(aElementToken));
+            break;
     }
 
     return this;
 }
 
-TextBodyContext::TextBodyContext( ContextHandler2Helper const & rParent, TextBody& rTextBody )
-: ContextHandler2( rParent )
-, mrTextBody( rTextBody )
+TextBodyContext::TextBodyContext(ContextHandler2Helper const& rParent, TextBody& rTextBody)
+    : ContextHandler2(rParent)
+    , mrTextBody(rTextBody)
 {
 }
 
-ContextHandlerRef TextBodyContext::onCreateContext( sal_Int32 aElementToken, const AttributeList& rAttribs )
+TextBodyContext::TextBodyContext(ContextHandler2Helper const& rParent, ShapePtr pShapePtr)
+    : TextBodyContext(rParent, *pShapePtr->getTextBody())
 {
-    switch( aElementToken )
+    mpShapePtr = pShapePtr;
+}
+
+ContextHandlerRef TextBodyContext::onCreateContext(sal_Int32 aElementToken,
+                                                   const AttributeList& rAttribs)
+{
+    switch (aElementToken)
     {
-        case A_TOKEN( bodyPr ):     // CT_TextBodyPropertyBag
-            return new TextBodyPropertiesContext( *this, rAttribs, mrTextBody.getTextProperties() );
-        case A_TOKEN( lstStyle ):   // CT_TextListStyle
-            return new TextListStyleContext( *this, mrTextBody.getTextListStyle() );
-        case A_TOKEN( p ):          // CT_TextParagraph
-        case W_TOKEN( p ):
-            return new TextParagraphContext( *this, mrTextBody.addParagraph() );
-        case W_TOKEN( sdt ):
-        case W_TOKEN( sdtContent ):
+        case A_TOKEN(bodyPr): // CT_TextBodyPropertyBag
+            if (mpShapePtr)
+                return new TextBodyPropertiesContext(*this, rAttribs, mpShapePtr);
+            else
+                return new TextBodyPropertiesContext(*this, rAttribs,
+                                                     mrTextBody.getTextProperties());
+        case A_TOKEN(lstStyle): // CT_TextListStyle
+            return new TextListStyleContext(*this, mrTextBody.getTextListStyle());
+        case A_TOKEN(p): // CT_TextParagraph
+        case W_TOKEN(p):
+            return new TextParagraphContext(*this, mrTextBody.addParagraph());
+        case W_TOKEN(sdt):
+        case W_TOKEN(sdtContent):
             return this;
-        case W_TOKEN( sdtPr ):
-        case W_TOKEN( sdtEndPr ):
-        break;
-        case W_TOKEN( tbl ):
-        break;
+        case W_TOKEN(sdtPr):
+        case W_TOKEN(sdtEndPr):
+            break;
+        case W_TOKEN(tbl):
+            break;
         default:
-            SAL_WARN("oox", "TextBodyContext::onCreateContext: unhandled element: " << getBaseToken(aElementToken));
+            SAL_WARN("oox", "TextBodyContext::onCreateContext: unhandled element: "
+                                << getBaseToken(aElementToken));
     }
 
     return nullptr;
 }
-
-} }
+}
+}
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
