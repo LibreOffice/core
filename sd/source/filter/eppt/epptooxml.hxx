39088a51bd7287626d02ebcb103504db2d715659
Mark Hung <marklh9@gmail.com>
2018-07-21 10:20:51 +0800

    tdf#118835 export animClr element. ( handle AnimateColor ).
    Implement PowerPointExport::WriteAnimateColorColor to export
    to, from, by elements of the animClr element.
    
    Change-Id: I0fb81f8838656c127610a9a10654e3e51106bea8

--- sd/source/filter/eppt/epptooxml.hxx
+++ sd/source/filter/eppt/epptooxml.hxx
@@ -112,6 +112,8 @@ private:
     void WriteAnimationNodeEffect( const ::sax_fastparser::FSHelperPtr& pFS, const css::uno::Reference< css::animations::XAnimationNode >& rXNode, sal_Int32 nXmlNodeType, bool bMainSeqChild );
     void WriteAnimationNodeCommand(const ::sax_fastparser::FSHelperPtr& pFS, const css::uno::Reference< css::animations::XAnimationNode >& rXNode, sal_Int32 nXmlNodeType, bool bMainSeqChild );
     void WriteAnimationNodeCommonPropsStart( const ::sax_fastparser::FSHelperPtr& pFS, const css::uno::Reference< css::animations::XAnimationNode >& rXNode, bool bSingle, bool bMainSeqChild );
+
+    static void WriteAnimateColorColor(const ::sax_fastparser::FSHelperPtr& pFS, const css::uno::Any& rAny, sal_Int32 nToken);
     static void WriteAnimationProperty( const ::sax_fastparser::FSHelperPtr& pFS, const css::uno::Any& rAny, sal_Int32 nToken = 0 );
     void WriteAnimationTarget( const ::sax_fastparser::FSHelperPtr& pFS, const css::uno::Any& rTarget );
     bool WriteComments( sal_uInt32 nPageNum );
