LibreOffice/core

commit 0f166ef5240ee155bf2544e6bc3c985b1e3bf646

cppcheck - fix variableScope in some files
Reduced the scope of some variables in source.

Change-Id: Ib3b521ca5df8b9b254b51eab5f1d7040d6cfd846
Reviewed-on: https://gerrit.libreoffice.org/52974
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

editeng/source/misc/svxacorr.cxx
@@ -2569,14 +2569,13 @@ void SvxAutoCorrectLanguageLists::PutText( const OUString& rShort,
 
     MakeUserStorage_Impl();
 
-    bool bRet = false;
     OUString sLong;
     try
     {
         uno::Reference < embed::XStorage > xStg = comphelper::OStorageHelper::GetStorageFromURL( sUserAutoCorrFile, embed::ElementModes::READWRITE );
-        bRet = rAutoCorrect.PutText( xStg, sUserAutoCorrFile, rShort, rShell, sLong );
         xStg = nullptr;
 
+        bool bRet = rAutoCorrect.PutText( xStg, sUserAutoCorrFile, rShort, rShell, sLong );
         // Update the word list
         if( bRet )
         {

dbaccess/source/ui/querydesign/QueryDesignView.cxx
@@ -640,13 +640,11 @@ namespace
                     {
                         // we have to look if we have alias.* here but before we have to check if the column doesn't already exist
                         OTableFieldDescRef  aInfo = new OTableFieldDesc();
-                        bool bFound = false;
                         for (auto const& table : rTabList)
                         {
                             OQueryTableWindow* pTabWin = static_cast<OQueryTableWindow*>(table.second.get());
 
-                            bFound = pTabWin->ExistsField( rFieldName, aInfo );
-                            if ( bFound )
+                            if ( pTabWin->ExistsField( rFieldName, aInfo ) )
                             {
                                 rFieldName = aInfo->GetField();
                                 break;

hwpfilter/source/hwpreader.cxx
@@ -122,11 +122,10 @@ extern "C" SAL_DLLPUBLIC_EXPORT bool TestImportHWP(SvStream &rStream)
     {
         std::unique_ptr<HStream> stream(new HStream);
         byte aData[32768];
-        std::size_t nRead;
 
         while (true)
         {
-            nRead = rStream.ReadBytes(aData, 32768);
+            std::size_t nRead = rStream.ReadBytes(aData, 32768);
             if (nRead == 0)
                 break;
             stream->addData(aData, static_cast<int>(nRead));

libreofficekit/qa/tilebench/tilebench.cxx
@@ -104,12 +104,6 @@ int main( int argc, char* argv[] )
         return 1;
     }
 
-    // Use realistic dimensions, similar to the Online client.
-    long nTilePixelWidth = 512;
-    long nTilePixelHeight = 512;
-    long nTileTwipWidth = 3840;
-    long nTileTwipHeight = 3840;
-
     aTimes.emplace_back("initialization");
     // coverity[tainted_string] - build time test tool
     Office *pOffice = lok_cpp_init(argv[1]);
@@ -152,6 +146,12 @@ int main( int argc, char* argv[] )
             free (pName);
         }
         aTimes.emplace_back();
+
+        // Use realistic dimensions, similar to the Online client.
+        long nTilePixelWidth = 512;
+        long nTilePixelHeight = 512;
+        long nTileTwipWidth = 3840;
+        long nTileTwipHeight = 3840;
 
         // Estimate the maximum tiles based on the number of parts requested, if Writer.
         if (pDocument->getDocumentType() == LOK_DOCTYPE_TEXT)

commit ce62fcc53c763e51565f791c3b9ca4a3b9179001

tdf#118517: Use xStg before resetting it
Regression from 0f166ef5240ee155bf2544e6bc3c985b1e3bf646
Change-Id: I083951700895a00be8d4407871c27804ca3e5f30

editeng/source/misc/svxacorr.cxx
@@ -2568,9 +2568,9 @@ void SvxAutoCorrectLanguageLists::PutText( const OUString& rShort,
     try
     {
         uno::Reference < embed::XStorage > xStg = comphelper::OStorageHelper::GetStorageFromURL( sUserAutoCorrFile, embed::ElementModes::READWRITE );
+        bool bRet = rAutoCorrect.PutText( xStg, sUserAutoCorrFile, rShort, rShell, sLong );
         xStg = nullptr;
 
-        bool bRet = rAutoCorrect.PutText( xStg, sUserAutoCorrFile, rShort, rShell, sLong );
         // Update the word list
         if( bRet )
         {