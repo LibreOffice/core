79d31ff21d87148fe9db3a8e96110fc06c182299
Noel Grandin <noel.grandin@collabora.co.uk>
2018-07-24 11:21:29 +0200

    loplugin:useuniqueptr in SwInputWindow
    Change-Id: If9ea84c6402269483d74a64b4b79a1f47f01c88d

--- sw/source/uibase/ribbar/inputwin.cxx
+++ sw/source/uibase/ribbar/inputwin.cxx
@@ -143,7 +143,7 @@ void SwInputWindow::dispose()
         pView->GetHRuler().SetActive();
         pView->GetVRuler().SetActive();
     }
-    delete pMgr;
+    pMgr.reset();
     if(pWrtShell)
         pWrtShell->EndSelTableCells();
 
@@ -215,7 +215,7 @@ void SwInputWindow::ShowWin()
 
         // Edit current field
         OSL_ENSURE(pMgr == nullptr, "FieldManager not deleted");
-        pMgr = new SwFieldMgr;
+        pMgr.reset(new SwFieldMgr);
 
         // Form should always begin with "=" , so set here
         OUString sEdit('=');
