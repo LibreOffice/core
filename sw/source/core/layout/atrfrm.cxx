a57d9f2e9ddcea682bb6e5ce2d2125bc9eeb730c
Michael Stahl <Michael.Stahl@cib.de>
2018-07-26 16:12:57 +0200

    sw: assert in SwFrameFormat::MakeFrames()
    This is an oddity, where the base class has an unimplemented
    MakeFrames() but an implemented DelFrames(), where the latter is relied
    upon by subclasses that do override MakeFrames().
    
    Not sure if there are subclasses that don't override MakeFrames()
    but which still require this base class DelFrames().
    
    Change-Id: I5bc8a8d5cb99c9ce80fc14ef33a038e9d88eaecd
    (cherry picked from commit c3f9076e70871be6140d566369ec0b49fb4a6c0e)

--- sw/source/core/layout/atrfrm.cxx
+++ sw/source/core/layout/atrfrm.cxx
@@ -2690,7 +2690,7 @@ void SwFrameFormat::DelFrames()
 
 void SwFrameFormat::MakeFrames()
 {
-    OSL_ENSURE( false, "Sorry not implemented." );
+    assert(false); // unimplemented in base class
 }
 
 SwRect SwFrameFormat::FindLayoutRect( const bool bPrtArea, const Point* pPoint ) const
