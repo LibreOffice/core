c20eb29560fa9d9e552c799203156c0742451eba
Eike Rathke <erack@redhat.com>
2018-07-26 15:00:09 +0200

    Related: tdf#91502 handle FORMULA() pCur->IsInForceArray() as well
    ... so using it in SUMPRODUCT() and other ForceArray context works
    as expected.
    
    Change-Id: I60197301edcd2c24f8cb0c5fcf4dc4699d9a6165
    Reviewed-on: https://gerrit.libreoffice.org/58118
    Reviewed-by: Eike Rathke <erack@redhat.com>
    Tested-by: Jenkins
    (cherry picked from commit 2008a520e429bc2581001d61b63ff934c2255be0)
    Reviewed-on: https://gerrit.libreoffice.org/58175
    Reviewed-by: Markus Mohrhard <markus.mohrhard@googlemail.com>

--- sc/source/core/tool/interpr1.cxx
+++ sc/source/core/tool/interpr1.cxx
@@ -2759,7 +2759,7 @@ void ScInterpreter::ScFormula()
     switch ( GetStackType() )
     {
         case svDoubleRef :
-            if (bMatrixFormula)
+            if (bMatrixFormula || pCur->IsInForceArray())
             {
                 SCCOL nCol1, nCol2;
                 SCROW nRow1, nRow2;
