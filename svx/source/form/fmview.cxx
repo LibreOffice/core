e5ee79b5c089e72e6b24c04c7c820d706bd39993
Armin Le Grand <Armin.Le.Grand@cib.de>
2018-07-27 13:38:46 +0200

    tdf#118963 Hand over a SdrModel to createControlLabelPair
    Change-Id: I34a35ff0700d14474fa9946851812c25c4eb4bc1
    Reviewed-on: https://gerrit.libreoffice.org/58187
    Tested-by: Jenkins
    Reviewed-by: Armin Le Grand <Armin.Le.Grand@cib.de>

--- svx/source/form/fmview.cxx
+++ svx/source/form/fmview.cxx
@@ -566,13 +566,13 @@ FmFormObj* FmFormView::getMarkedGrid() const
 void FmFormView::createControlLabelPair( OutputDevice const * _pOutDev, sal_Int32 _nXOffsetMM, sal_Int32 _nYOffsetMM,
     const Reference< XPropertySet >& _rxField, const Reference< XNumberFormats >& _rxNumberFormats,
     sal_uInt16 _nControlObjectID, SdrInventor _nInventor, sal_uInt16 _nLabelObjectID,
-    SdrPage* _pLabelPage, SdrPage* _pControlPage, SdrModel* _pModel, SdrUnoObj*& _rpLabel, SdrUnoObj*& _rpControl )
+    SdrPage* _pLabelPage, SdrPage* _pControlPage, SdrModel& _rModel, SdrUnoObj*& _rpLabel, SdrUnoObj*& _rpControl )
 {
     FmXFormView::createControlLabelPair(
         *_pOutDev, _nXOffsetMM, _nYOffsetMM,
         _rxField, _rxNumberFormats,
         _nControlObjectID, "", _nInventor, _nLabelObjectID,
-        _pLabelPage, _pControlPage, _pModel,
+        _pLabelPage, _pControlPage, _rModel,
         _rpLabel, _rpControl
     );
 }
