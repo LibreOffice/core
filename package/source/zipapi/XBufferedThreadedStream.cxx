d50586c3db3edf38045a97694349f0d9551012d9
Caolán McNamara <caolanm@redhat.com>
2018-07-28 16:33:22 +0100

    ofz#9597 rethrown IOException not caught by catch IOException
    under google oss-fuzz, asan + clang version 6.0.0 (trunk 315613)
    
    when using our own exception thrower, but builtin C++11 support works
    
    also, ofz#9266, ofz#9591, ofz#9597, ofz#9622, etc
    
    Change-Id: I29c692da880b49268284de2f61be07fa94f33bc6

--- package/source/zipapi/XBufferedThreadedStream.cxx
+++ package/source/zipapi/XBufferedThreadedStream.cxx
@@ -31,7 +31,7 @@ private:
         catch (const css::uno::Exception &e)
         {
             SAL_WARN("package", "Unexpected " << e );
-            mxStream.saveException(cppu::getCaughtException());
+            mxStream.saveException(std::current_exception());
         }
 
         mxStream.setTerminateThread();
@@ -106,8 +106,8 @@ const Buffer& XBufferedThreadedStream::getNextBlock()
         if( maPendingBuffers.empty() )
         {
             maInUseBuffer = Buffer();
-            if (maSavedException.hasValue())
-                cppu::throwException(maSavedException);
+            if (maSavedException)
+                std::rethrow_exception(maSavedException);
         }
         else
         {
