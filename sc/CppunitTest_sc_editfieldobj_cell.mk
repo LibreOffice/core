6b3521a3bc576092d4974a1e86ab4f674ec6a41f
Jens Carl <j.carl43@gmx.de>
2018-07-29 20:15:10 +0000

    Remove obsolete (cargo-cult copied) dependencies
    Change-Id: I828bf75fef031758b354149945f719f61a9caf91

--- sc/CppunitTest_sc_editfieldobj_cell.mk
+++ sc/CppunitTest_sc_editfieldobj_cell.mk
@@ -14,48 +14,19 @@ $(eval $(call gb_CppunitTest_CppunitTest,sc_editfieldobj_cell))
 $(eval $(call gb_CppunitTest_use_external,sc_editfieldobj_cell,boost_headers))
 
 $(eval $(call gb_CppunitTest_add_exception_objects,sc_editfieldobj_cell, \
-    sc/qa/extras/sceditfieldobj-cell \
+	sc/qa/extras/sceditfieldobj-cell \
 ))
 
 $(eval $(call gb_CppunitTest_use_libraries,sc_editfieldobj_cell, \
-    basegfx \
-    comphelper \
-    cppu \
-    cppuhelper \
-    drawinglayer \
-    editeng \
-    for \
-    forui \
-    i18nlangtag \
-    msfilter \
-    oox \
-    sal \
-    salhelper \
-    sax \
-    sb \
-    sc \
-    sfx \
-    sot \
-    subsequenttest \
-    svl \
-    svt \
-    svx \
-    svxcore \
-    test \
-    tk \
-    tl \
-    ucbhelper \
-    unotest \
-    utl \
-    vbahelper \
-    vcl \
-    xo \
+	cppu \
+	sal \
+	subsequenttest \
+	test \
+	unotest \
 ))
 
 $(eval $(call gb_CppunitTest_set_include,sc_editfieldobj_cell,\
-    -I$(SRCDIR)/sc/source/ui/inc \
-    -I$(SRCDIR)/sc/inc \
-    $$(INCLUDE) \
+	$$(INCLUDE) \
 ))
 
 $(eval $(call gb_CppunitTest_use_sdk_api,sc_editfieldobj_cell))
@@ -64,7 +35,7 @@ $(eval $(call gb_CppunitTest_use_ure,sc_editfieldobj_cell))
 $(eval $(call gb_CppunitTest_use_vcl,sc_editfieldobj_cell))
 
 $(eval $(call gb_CppunitTest_use_components,sc_editfieldobj_cell,\
-    $(sc_unoapi_common_components) \
+	$(sc_unoapi_common_components) \
 ))
 
 $(eval $(call gb_CppunitTest_use_configuration,sc_editfieldobj_cell))
