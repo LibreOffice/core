d942bdc9c3df2f5bb2edd406e2a6677e305cdeeb
Caolán McNamara <caolanm@redhat.com>
2018-08-01 08:55:23 +0100

    forcepoint#55 invalid iterator increment
    Change-Id: I151602f645a48404d93e0ae64a9e1d2a6ba2bc4b

--- sc/source/core/data/column3.cxx
+++ sc/source/core/data/column3.cxx
@@ -3080,6 +3080,8 @@ public:
                 if (xCurGrp)
                 {
                     // Move to the cell after the last cell of the current group.
+                    if (xCurGrp->mnLength > std::distance(it, itEnd))
+                        throw css::lang::IllegalArgumentException();
                     std::advance(it, xCurGrp->mnLength);
                     nRow += xCurGrp->mnLength;
                 }
