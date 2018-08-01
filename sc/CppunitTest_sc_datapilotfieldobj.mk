816ddf8b111d34b4a02e5974b62e729bceac9309
Jens Carl <j.carl43@gmx.de>
2018-08-01 05:30:57 +0000

    Remove obsolete (cargo-cult copied) dependencies
    Change-Id: I9edc18f94a33587bbb0a3bd11b4df66b7844a028

--- sc/CppunitTest_sc_datapilotfieldobj.mk
+++ sc/CppunitTest_sc_datapilotfieldobj.mk
@@ -14,48 +14,19 @@ $(eval $(call gb_CppunitTest_CppunitTest,sc_datapilotfieldobj))
 $(eval $(call gb_CppunitTest_use_external,sc_datapilotfieldobj,boost_headers))
 
 $(eval $(call gb_CppunitTest_add_exception_objects,sc_datapilotfieldobj, \
-    sc/qa/extras/scdatapilotfieldobj \
+	sc/qa/extras/scdatapilotfieldobj \
 ))
 
 $(eval $(call gb_CppunitTest_use_libraries,sc_datapilotfieldobj, \
-    basegfx \
-    comphelper \
-    cppu \
-    cppuhelper \
-    drawinglayer \
-    editeng \
-    for \
-    forui \
-    i18nlangtag \
-    msfilter \
-    oox \
-    sal \
-    salhelper \
-    sax \
-    sb \
-    sc \
-    sfx \
-    sot \
-    subsequenttest \
-    svl \
-    svt \
-    svx \
-    svxcore \
+	cppu \
+	sal \
+	subsequenttest \
 	test \
-    tk \
-    tl \
-    ucbhelper \
 	unotest \
-    utl \
-    vbahelper \
-    vcl \
-    xo \
 ))
 
 $(eval $(call gb_CppunitTest_set_include,sc_datapilotfieldobj,\
-    -I$(SRCDIR)/sc/source/ui/inc \
-    -I$(SRCDIR)/sc/inc \
-    $$(INCLUDE) \
+	$$(INCLUDE) \
 ))
 
 $(eval $(call gb_CppunitTest_use_sdk_api,sc_datapilotfieldobj))
@@ -64,7 +35,7 @@ $(eval $(call gb_CppunitTest_use_ure,sc_datapilotfieldobj))
 $(eval $(call gb_CppunitTest_use_vcl,sc_datapilotfieldobj))
 
 $(eval $(call gb_CppunitTest_use_components,sc_datapilotfieldobj,\
-    $(sc_unoapi_common_components) \
+	$(sc_unoapi_common_components) \
 ))
 
 $(eval $(call gb_CppunitTest_use_configuration,sc_datapilotfieldobj))
