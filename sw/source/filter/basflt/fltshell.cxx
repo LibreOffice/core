2efe42f9d918eac459a1b370040691dd9eacbdc0
Andrea Gelmini <andrea.gelmini@gelma.net>
2018-08-03 11:41:11 +0200

    Fix typos
    Change-Id: I7f90b7672665be0d8d5e47c6456433b06f88d5e0

--- sw/source/filter/basflt/fltshell.cxx
+++ sw/source/filter/basflt/fltshell.cxx
@@ -123,7 +123,7 @@ SwFltStackEntry::~SwFltStackEntry()
 void SwFltStackEntry::SetEndPos(const SwPosition& rEndPos)
 {
     // Release attribute and keep track of end
-    // Everything with sal_uInt16s, lest the inserting of new text at
+    // Everything with sal_uInt16s, let the inserting of new text at
     // the cursor position moves the attribute's range
     // That's not the desired behavior!
     bOpen = false;                  // release and remember END
