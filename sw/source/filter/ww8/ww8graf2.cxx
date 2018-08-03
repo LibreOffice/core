1a6a05adf8b0f2620c0798e8c6811b08c80d0dea
Caolán McNamara <caolanm@redhat.com>
2018-08-03 09:57:24 +0100

    forcepoint#70 give all escher client data objects a common parent class
    and make NotifyFreeObj a virtual method of SvxMSDffClientData, finding
    the sc case where the client data was neither SvxMSDffImportData nor
    ProcessData.
    
    make the sc case a XclImpDrawObjClientData whose NotifyFreeObj is a noop
    
    Change-Id: I07422e7a3415114674bb1e3c1ef120299adf2dc8
    Reviewed-on: https://gerrit.libreoffice.org/58552
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <Michael.Stahl@cib.de>

--- sw/source/filter/ww8/ww8graf2.cxx
+++ sw/source/filter/ww8/ww8graf2.cxx
@@ -561,7 +561,7 @@ SwFrameFormat* SwWW8ImplReader::ImportGraf(SdrTextObj const * pTextObj,
 
             tools::Rectangle aClientRect( 0,0, aPD.nWidth,  aPD.nHeight);
             SvxMSDffImportData aData( aClientRect );
-            pObject = m_xMSDffManager->ImportObj(*m_pDataStream, &aData, aClientRect, tools::Rectangle(), /*nCalledByGroup*/0, /*pShapeId*/nullptr );
+            pObject = m_xMSDffManager->ImportObj(*m_pDataStream, aData, aClientRect, tools::Rectangle(), /*nCalledByGroup*/0, /*pShapeId*/nullptr );
             if (pObject)
             {
                 // for the frame
