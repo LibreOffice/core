511625cdaf30ce380c0cabac974271251ae1277c
Jens Carl <j.carl43@gmx.de>
2018-08-04 18:03:50 +0000

    Remove obsolete (cargo-cult copied) dependencies
    Change-Id: I8603a7e219c90095ee971c806f9dd21c1ba9f701

--- sc/CppunitTest_sc_consolidationdescriptorobj.mk
+++ sc/CppunitTest_sc_consolidationdescriptorobj.mk
@@ -18,43 +18,14 @@ $(eval $(call gb_CppunitTest_add_exception_objects,sc_consolidationdescriptorobj
 ))
 
 $(eval $(call gb_CppunitTest_use_libraries,sc_consolidationdescriptorobj, \
-	basegfx \
-	comphelper \
 	cppu \
-	cppuhelper \
-	drawinglayer \
-	editeng \
-	for \
-	forui \
-	i18nlangtag \
-	msfilter \
-	oox \
 	sal \
-	salhelper \
-	sax \
-	sb \
-	sc \
-	sfx \
-	sot \
 	subsequenttest \
-	svl \
-	svt \
-	svx \
-	svxcore \
 	test \
-	tk \
-	tl \
-	ucbhelper \
 	unotest \
-	utl \
-	vbahelper \
-	vcl \
-	xo \
 ))
 
 $(eval $(call gb_CppunitTest_set_include,sc_consolidationdescriptorobj,\
-	-I$(SRCDIR)/sc/source/ui/inc \
-	-I$(SRCDIR)/sc/inc \
 	$$(INCLUDE) \
 ))
 
@@ -64,7 +35,7 @@ $(eval $(call gb_CppunitTest_use_ure,sc_consolidationdescriptorobj))
 $(eval $(call gb_CppunitTest_use_vcl,sc_consolidationdescriptorobj))
 
 $(eval $(call gb_CppunitTest_use_components,sc_consolidationdescriptorobj,\
-    $(sc_unoapi_common_components) \
+	$(sc_unoapi_common_components) \
 ))
 
 $(eval $(call gb_CppunitTest_use_configuration,sc_consolidationdescriptorobj))
