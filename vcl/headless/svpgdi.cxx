c4e3b75718a9953e4f22736d553ba279efd45b7f
Caolán McNamara <caolanm@redhat.com>
2018-08-04 15:30:46 +0100

    crashtesting: bubble surface creation failure notification upwards
    e.g. on converting moz858095-2.svg to odg
    
    Change-Id: I1a222f1745cff9c4cb225f6c0c216072fb17b4f7
    Reviewed-on: https://gerrit.libreoffice.org/58587
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>
    (cherry picked from commit dbf34be458a2b5723084d1ed99390b744cee6053)

--- vcl/headless/svpgdi.cxx
+++ vcl/headless/svpgdi.cxx
@@ -1323,6 +1323,11 @@ SalBitmap* SvpSalGraphics::getBitmap( long nX, long nY, long nWidth, long nHeigh
     }
 
     cairo_surface_t* target = SvpSalGraphics::createCairoSurface(pBitmap->GetBuffer());
+    if (!target)
+    {
+        SAL_WARN("vcl.gdi", "SvpSalGraphics::getBitmap, cannot create cairo surface");
+        return nullptr;
+    }
     cairo_t* cr = cairo_create(target);
 
     SalTwoRect aTR(nX, nY, nWidth, nHeight, 0, 0, nWidth, nHeight);
