3da04639866158008bb23b37113dfe3d717616d4
Jens Carl <j.carl43@gmx.de>
2018-08-04 16:22:55 +0000

    Remove obsolete (cargo-cult copied) dependencies
    Change-Id: I19258e12eab781e7835c362326d6f969228ae56f

--- sc/CppunitTest_sc_databaserangeobj.mk
+++ sc/CppunitTest_sc_databaserangeobj.mk
@@ -14,48 +14,19 @@ $(eval $(call gb_CppunitTest_CppunitTest,sc_databaserangeobj))
 $(eval $(call gb_CppunitTest_use_external,sc_databaserangeobj,boost_headers))
 
 $(eval $(call gb_CppunitTest_add_exception_objects,sc_databaserangeobj, \
-    sc/qa/extras/scdatabaserangeobj \
+	sc/qa/extras/scdatabaserangeobj \
 ))
 
 $(eval $(call gb_CppunitTest_use_libraries,sc_databaserangeobj, \
-    basegfx \
-    comphelper \
-    cppu \
-    cppuhelper \
-    drawinglayer \
-    editeng \
-    for \
-    forui \
-    i18nlangtag \
-    msfilter \
-    oox \
-    sal \
-    salhelper \
-    sax \
-    sb \
-    sc \
-    sfx \
-    sot \
-    subsequenttest \
-    svl \
-    svt \
-    svx \
-    svxcore \
+	cppu \
+	sal \
+	subsequenttest \
 	test \
-    tk \
-    tl \
-    ucbhelper \
 	unotest \
-    utl \
-    vbahelper \
-    vcl \
-    xo \
 ))
 
 $(eval $(call gb_CppunitTest_set_include,sc_databaserangeobj,\
-    -I$(SRCDIR)/sc/source/ui/inc \
-    -I$(SRCDIR)/sc/inc \
-    $$(INCLUDE) \
+	$$(INCLUDE) \
 ))
 
 $(eval $(call gb_CppunitTest_use_sdk_api,sc_databaserangeobj))
@@ -64,7 +35,7 @@ $(eval $(call gb_CppunitTest_use_ure,sc_databaserangeobj))
 $(eval $(call gb_CppunitTest_use_vcl,sc_databaserangeobj))
 
 $(eval $(call gb_CppunitTest_use_components,sc_databaserangeobj,\
-    $(sc_unoapi_common_components) \
+	$(sc_unoapi_common_components) \
 ))
 
 $(eval $(call gb_CppunitTest_use_configuration,sc_databaserangeobj))
