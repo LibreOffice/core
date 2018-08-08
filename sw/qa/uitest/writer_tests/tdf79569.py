88d82b7da7f3b362c62494875ad359158be964d8
Stephan Bergmann <sbergman@redhat.com>
2018-08-08 11:34:21 +0200

    Temporarily sprinkle test code with debug print statements
    ...to hopefully find out why <https://ci.libreoffice.org//job/lo_ubsan/996/
    consoleFull#-14523843999567f988-cbcf-4519-af05-6000b834f13f> still fails in
    UITest_writer_tests after c57191e0c45f9735a33953d6b95d54b0e10c876f "Try to make
    test_tdf81457_table_merge_undo deterministic".
    
    Change-Id: I142f89c508eb66a82e656b0ef32f595796b547fc
    Reviewed-on: https://gerrit.libreoffice.org/58728
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

--- sw/qa/uitest/writer_tests/tdf79569.py
+++ sw/qa/uitest/writer_tests/tdf79569.py
@@ -5,6 +5,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 
+from sys import stderr
 from uitest.framework import UITestCase
 from libreoffice.uno.propertyvalue import mkPropertyValues
 from uitest.debug import sleep
@@ -22,26 +23,47 @@ class tdf79569(UITestCase):
         document = self.ui_test.get_component()
         toolkit_ex = self.xContext.ServiceManager.createInstanceWithContext(
             "com.sun.star.awt.Toolkit", self.xContext) # supports css.awt.XToolkitExperimental
+        print("DEBUG.1", file=stderr);
         self.xUITest.executeCommand(".uno:GoDown")
+        print("DEBUG.2", file=stderr);
         toolkit_ex.processEventsToIdle()
+        print("DEBUG.3", file=stderr);
         self.xUITest.executeCommand(".uno:GoDown")
+        print("DEBUG.4", file=stderr);
         toolkit_ex.processEventsToIdle()
+        print("DEBUG.5", file=stderr);
         xWriterEdit.executeAction("TYPE", mkPropertyValues({"KEYCODE": "CTRL+END"}))
+        print("DEBUG.6", file=stderr);
         toolkit_ex.processEventsToIdle()
+        print("DEBUG.7", file=stderr);
         self.xUITest.executeCommand(".uno:GoRight")
+        print("DEBUG.8", file=stderr);
         toolkit_ex.processEventsToIdle()
+        print("DEBUG.9", file=stderr);
         xWriterEdit.executeAction("TYPE", mkPropertyValues({"KEYCODE": "CTRL+END"}))
+        print("DEBUG.10", file=stderr);
         toolkit_ex.processEventsToIdle()
+        print("DEBUG.11", file=stderr);
         xWriterEdit.executeAction("TYPE", mkPropertyValues({"KEYCODE": "SHIFT+RIGHT"}))
+        print("DEBUG.12", file=stderr);
         toolkit_ex.processEventsToIdle()
+        print("DEBUG.13", file=stderr);
         self.xUITest.executeCommand(".uno:MergeCells")
+        print("DEBUG.14", file=stderr);
         toolkit_ex.processEventsToIdle()
+        print("DEBUG.15", file=stderr);
         self.xUITest.executeCommand(".uno:Undo")
+        print("DEBUG.16", file=stderr);
         toolkit_ex.processEventsToIdle()
+        print("DEBUG.17", file=stderr);
         self.xUITest.executeCommand(".uno:Redo")
+        print("DEBUG.18", file=stderr);
         toolkit_ex.processEventsToIdle()
+        print("DEBUG.19", file=stderr);
         self.xUITest.executeCommand(".uno:Undo")
+        print("DEBUG.20", file=stderr);
         toolkit_ex.processEventsToIdle()
+        print("DEBUG.21", file=stderr);
 
         self.assertEqual(document.TextTables.getCount(), 1)
 
