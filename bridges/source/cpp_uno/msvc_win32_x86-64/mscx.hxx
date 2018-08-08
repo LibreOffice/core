8313116f962ac87e103ea22aafb58700b8c695da
Mike Kaganski <mike.kaganski@collabora.com>
2018-08-08 12:42:32 +0200

    Don't use internal __CxxDetectRethrow: it has side effects
    Since the __CxxDetectRethrow may increment __ProcessingThrow, which then
    must be decremented in __CxxUnregisterExceptionObject, and the latter does
    many other funny things with exception handling CRT machinery, we cannot
    use those internal functions (neither alone, nor together), or we end up
    breaking runtime's expectations: the runtime code checks __ProcessingThrow
    left and right, expecting its non-0 value to indicate "we are unwinding...
    possibly called from a dtor()". In this case, e.g., std::current_exception
    returns nullptr inside catch block.
    
    This creates our own copy of __CxxDetectRethrow, which does not mangle the
    global state, and just performs the same checks. This is a dirty hack, and
    it relies on current layout of the exception description layout - so must
    be synchronized in the event of changes!
    
    Change-Id: I2c475fbc2468073b796c7e9d0f4dfcd315896489
    Reviewed-on: https://gerrit.libreoffice.org/58730
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

--- bridges/source/cpp_uno/msvc_win32_x86-64/mscx.hxx
+++ bridges/source/cpp_uno/msvc_win32_x86-64/mscx.hxx
@@ -47,11 +47,5 @@ void mscx_raiseException(
 
 }
 
-//TODO: Work around missing __CxxDetectRethrow in clang-cl for now (predefined
-// in cl, <www.geoffchappell.com/studies/msvc/language/predefined/index.html>):
-#if defined __clang__
-extern "C" int __cdecl __CxxDetectRethrow(void *);
-#endif
-
 #endif
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
