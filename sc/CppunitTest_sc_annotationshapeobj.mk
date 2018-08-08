cdf82727e3725e612f1789c7c610f40f2390cb79
Jens Carl <j.carl43@gmx.de>
2018-08-08 03:39:07 +0000

    Remove obsolete (cargo-cult copied) dependencies
    Change-Id: Ie2be68fe9d106b5f370bc8927fcf65d16496ee3e

--- sc/CppunitTest_sc_annotationshapeobj.mk
+++ sc/CppunitTest_sc_annotationshapeobj.mk
@@ -14,48 +14,19 @@ $(eval $(call gb_CppunitTest_CppunitTest,sc_annotationshapeobj))
 $(eval $(call gb_CppunitTest_use_external,sc_annotationshapeobj,boost_headers))
 
 $(eval $(call gb_CppunitTest_add_exception_objects,sc_annotationshapeobj, \
-    sc/qa/extras/scannotationshapeobj \
+	sc/qa/extras/scannotationshapeobj \
 ))
 
 $(eval $(call gb_CppunitTest_use_libraries,sc_annotationshapeobj, \
-    basegfx \
-    comphelper \
-    cppu \
-    cppuhelper \
-    drawinglayer \
-    editeng \
-    for \
-    forui \
-    i18nlangtag \
-    msfilter \
-    oox \
-    sal \
-    salhelper \
-    sax \
-    sb \
-    sc \
-    sfx \
-    sot \
-    subsequenttest \
-    svl \
-    svt \
-    svx \
-    svxcore \
-    test \
-    tk \
-    tl \
-    ucbhelper \
-    unotest \
-    utl \
-    vbahelper \
-    vcl \
-    xo \
+	cppu \
+	sal \
+	subsequenttest \
+	test \
+	unotest \
 ))
 
 $(eval $(call gb_CppunitTest_set_include,sc_annotationshapeobj,\
-    -I$(SRCDIR)/sc/source/ui/inc \
-    -I$(SRCDIR)/sc/inc \
-    $$(INCLUDE) \
+	$$(INCLUDE) \
 ))
 
 $(eval $(call gb_CppunitTest_use_sdk_api,sc_annotationshapeobj))
@@ -64,7 +35,7 @@ $(eval $(call gb_CppunitTest_use_ure,sc_annotationshapeobj))
 $(eval $(call gb_CppunitTest_use_vcl,sc_annotationshapeobj))
 
 $(eval $(call gb_CppunitTest_use_components,sc_annotationshapeobj,\
-    $(sc_unoapi_common_components) \
+	$(sc_unoapi_common_components) \
 ))
 
 $(eval $(call gb_CppunitTest_use_configuration,sc_annotationshapeobj))
