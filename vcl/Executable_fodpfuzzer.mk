c33d52ffe9011b7624af317508337e31422a9f0b
Stephan Bergmann <sbergman@redhat.com>
2018-08-09 12:29:20 +0000

    Various fuzzers need pdffilter_component_getFactory
    Change-Id: I59c5499586c16768afde822cd6d580540d1bcd73

--- vcl/Executable_fodpfuzzer.mk
+++ vcl/Executable_fodpfuzzer.mk
@@ -29,6 +29,7 @@ $(eval $(call gb_Executable_set_include,fodpfuzzer,\
 $(eval $(call gb_Executable_use_libraries,fodpfuzzer,\
     $(fuzzer_draw_libraries) \
     $(fuzzer_core_libraries) \
+    pdffilter \
 ))
 
 $(eval $(call gb_Executable_use_static_libraries,fodpfuzzer,\
