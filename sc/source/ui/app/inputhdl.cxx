9292736199f31690cb4e67d9378305386af4f642
Caolán McNamara <caolanm@redhat.com>
2018-08-15 09:11:26 +0100

    Resolves: tdf#118609 crash in calc after some manipulation through basic
    Change-Id: I518b15359b29c845604d2e9cde64bdb637078ba1

--- sc/source/ui/app/inputhdl.cxx
+++ sc/source/ui/app/inputhdl.cxx
@@ -1908,7 +1908,7 @@ void ScInputHandler::SetInputWindow(  ScInputWindow* pNew )
 
 void ScInputHandler::StopInputWinEngine( bool bAll )
 {
-    if (pInputWin)
+    if (pInputWin && !pInputWin->IsDisposed())
         pInputWin->StopEditEngine( bAll );
 
     pTopView = nullptr; // invalid now
