4adea88305ff22483bdc925fe782a3422a6da813
Noel Grandin <noel.grandin@collabora.co.uk>
2018-08-15 15:11:25 +0200

    loplugin:returnconstant in OPropertyBrowserController
    Change-Id: Id5e225dbe5a1c2f27c1032cdaab35a01c78ede81
    Reviewed-on: https://gerrit.libreoffice.org/59136
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

--- extensions/source/propctrlr/propcontroller.cxx
+++ extensions/source/propctrlr/propcontroller.cxx
@@ -361,16 +361,14 @@ namespace pcr
         if (!pParentWin)
             throw RuntimeException("The frame is invalid. Unable to extract the container window.",*this);
 
-        if ( Construct( pParentWin ) )
+        Construct( pParentWin );
+        try
         {
-            try
-            {
-                m_xFrame->setComponent( VCLUnoHelper::GetInterface( m_pView ), this );
-            }
-            catch( const Exception& )
-            {
-                OSL_FAIL( "OPropertyBrowserController::attachFrame: caught an exception!" );
-            }
+            m_xFrame->setComponent( VCLUnoHelper::GetInterface( m_pView ), this );
+        }
+        catch( const Exception& )
+        {
+            OSL_FAIL( "OPropertyBrowserController::attachFrame: caught an exception!" );
         }
 
         startContainerWindowListening();
@@ -669,7 +667,7 @@ namespace pcr
     }
 
 
-    bool OPropertyBrowserController::Construct(vcl::Window* _pParentWin)
+    void OPropertyBrowserController::Construct(vcl::Window* _pParentWin)
     {
         DBG_ASSERT(!haveView(), "OPropertyBrowserController::Construct: already have a view!");
         DBG_ASSERT(_pParentWin, "OPropertyBrowserController::Construct: invalid parent window!");
@@ -690,8 +688,6 @@ namespace pcr
         impl_initializeView_nothrow();
 
         m_pView->Show();
-
-        return true;
     }
 
 
