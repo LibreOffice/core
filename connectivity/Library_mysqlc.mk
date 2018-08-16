47ede49d0a3bb0c149a451e959a10f107ae5e24a
Tamas Bunth <tamas.bunth@collabora.co.uk>
2018-08-16 16:35:17 +0200

    Moving mysqlc into connectivity as a library
    Additionally I erased the remains of the old mysql C++ connector.
    
    Also update the code where the clang plugins were unhappy after moving
    the mysqlc module.
    
    Remove mysql-cpp-connector external.
    
    Change-Id: I771cc7afacf5012fc349afb6b36828fb0fee5a66

---
+++ connectivity/Library_mysqlc.mk
@@ -0,0 +1,75 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, you can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_Library_Library,mysqlc))
+
+$(eval $(call gb_Library_use_externals,mysqlc,\
+	boost_headers \
+	mariadb-connector-c \
+))
+
+ifeq ($(OS)-$(SYSTEM_MARIADB_CONNECTOR_C),MACOSX-)
+$(eval $(call gb_Library_use_external,mysqlc,iconv))
+endif
+ifeq ($(OS)-$(SYSTEM_MARIADB_CONNECTOR_C),WNT-)
+$(eval $(call gb_Library_use_external,mysqlc,iconv))
+endif
+
+$(eval $(call gb_Library_set_include,firebird_sdbc,\
+	-I$(SRCDIR)/connectivity/inc \
+	-I$(SRCDIR)/connectivity/source/inc \
+	$$(INCLUDE) \
+	-I$(WORKDIR)/YaccTarget/connectivity/source/parse \
+))
+
+ifeq ($(SYSTEM_MYSQL_CONNECTOR_CPP),)
+$(eval $(call gb_Library_add_libs,mysqlc,\
+	$(if $(filter-out WNT,$(OS)),$(if $(filter MACOSX SOLARIS,$(OS)),-lz -lm,\
+	-rdynamic -lz -lcrypt -lm)) \
+	$(if $(filter LINUX,$(OS)),-lpthread -ldl,) \
+))
+endif
+
+$(eval $(call gb_Library_use_sdk_api,mysqlc))
+
+$(eval $(call gb_Library_use_libraries,mysqlc,\
+	cppu \
+	sal \
+	salhelper \
+	cppuhelper \
+))
+
+$(eval $(call gb_Library_add_defs,mysqlc,\
+	-DCPPDBC_EXPORTS \
+	-DCPPCONN_LIB_BUILD \
+	-DMARIADBC_VERSION_MAJOR=$(MARIADBC_MAJOR) \
+	-DMARIADBC_VERSION_MINOR=$(MARIADBC_MINOR) \
+	-DMARIADBC_VERSION_MICRO=$(MARIADBC_MICRO) \
+	$(if $(SYSTEM_MYSQL_CONNECTOR_CPP),,\
+	-DCPPCONN_LIB=\"$(call gb_Library_get_runtime_filename,mysqlcppconn)\") \
+	$(if $(BUNDLE_MARIADB_CONNECTOR_C),-DBUNDLE_MARIADB=\"$(LIBMARIADB)\") \
+))
+
+$(eval $(call gb_Library_add_exception_objects,mysqlc,\
+	connectivity/source/drivers/mysqlc/mysqlc_driver \
+	connectivity/source/drivers/mysqlc/mysqlc_services \
+	connectivity/source/drivers/mysqlc/mysqlc_connection \
+	connectivity/source/drivers/mysqlc/mysqlc_resultset \
+	connectivity/source/drivers/mysqlc/mysqlc_prepared_resultset \
+	connectivity/source/drivers/mysqlc/mysqlc_resultsetmetadata \
+	connectivity/source/drivers/mysqlc/mysqlc_statement \
+	connectivity/source/drivers/mysqlc/mysqlc_preparedstatement \
+	connectivity/source/drivers/mysqlc/mysqlc_databasemetadata \
+	connectivity/source/drivers/mysqlc/mysqlc_types \
+	connectivity/source/drivers/mysqlc/mysqlc_general \
+))
+
+$(eval $(call gb_Library_set_componentfile,mysqlc,connectivity/source/drivers/mysqlc/mysqlc))
+
+# vim: set noet sw=4 ts=4:
