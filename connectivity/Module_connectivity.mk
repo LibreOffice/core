490c6cd0ed877a0bd22a7592b42d70edd83114ca
Tamas Bunth <tamas.bunth@collabora.co.uk>
2018-08-16 16:35:17 +0200

    Moving mysqlc into connectivity as a library
    Additionally I erased the remains of the old mysql C++ connector.
    
    Also update the code where the clang plugins were unhappy after moving
    the mysqlc module.
    
    Remove mysql-cpp-connector external.
    
    Change-Id: I771cc7afacf5012fc349afb6b36828fb0fee5a66
    Reviewed-on: https://gerrit.libreoffice.org/59200
    Tested-by: Jenkins
    Reviewed-by: Tamás Bunth <btomi96@gmail.com>
    Reviewed-on: https://gerrit.libreoffice.org/59713
    Reviewed-by: Andras Timar <andras.timar@collabora.com>
    Tested-by: Andras Timar <andras.timar@collabora.com>

--- connectivity/Module_connectivity.mk
+++ connectivity/Module_connectivity.mk
@@ -20,7 +20,6 @@ $(eval $(call gb_Module_add_targets,connectivity,\
 	Configuration_calc \
 	Configuration_dbase \
 	Configuration_flat \
-	Configuration_mysql \
 	Configuration_odbc \
 	Configuration_writer \
 	Library_calc \
@@ -28,7 +27,6 @@ $(eval $(call gb_Module_add_targets,connectivity,\
 	Library_dbpool2 \
 	Library_file \
 	Library_flat \
-	Library_mysql \
 	$(if $(filter ANDROID IOS,$(OS)),,Library_odbc) \
 	Library_sdbc2 \
 	Library_writer \
@@ -78,6 +76,13 @@ ifeq ($(ENABLE_FIREBIRD_SDBC),TRUE)
 $(eval $(call gb_Module_add_targets,connectivity,\
 	Configuration_firebird \
 	Library_firebird_sdbc \
+))
+endif
+
+ifeq ($(ENABLE_MARIADBC),TRUE)
+$(eval $(call gb_Module_add_targets,connectivity,\
+	Configuration_mysql \
+	Library_mysqlc \
 ))
 endif
 
