b10d1366422059f4b0cf222d56938a5409b60e6a
Jan-Marek Glogowski <glogow@fbihome.de>
2018-08-17 23:10:00 +0200

    tdf#116370 cleanup Writer idle job handing
    This prevents the start of the idle job, while processing itself,
    so the fixed WinSalInstance::AnyInput of commit 3bf6c97029d2
    ("tdf#112975 WIN correctly handle VclInputFlags::OTHER") won't
    report the timer events of the re-started idle job to process.
    
    Fixes the early abort of the background job, which resulted in
    the busy loop of the reported bug and this strange printing
    behaviour.
    
    P.S. I'm not sure, why this was just broken on Windows.
    
    Change-Id: I6503dcd925c9a0ed843e794a31eea32a4a4b2889
    Reviewed-on: https://gerrit.libreoffice.org/59279
    Tested-by: Jenkins
    Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>
    (cherry picked from commit 401cba4c20fbc930f034168872642428d7459218)
    Reviewed-on: https://gerrit.libreoffice.org/59315
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>

--- sw/source/core/inc/rootfrm.hxx
+++ sw/source/core/inc/rootfrm.hxx
@@ -245,7 +245,7 @@ public:
         // May be NULL if called from SfxBaseModel::dispose
         // (this happens in the build test 'rtfexport').
         if (pCurrShell != nullptr)
-            pCurrShell->GetDoc()->getIDocumentTimerAccess().StartBackgroundJobs();
+            pCurrShell->GetDoc()->getIDocumentTimerAccess().StartIdling();
     }
     bool IsIdleFormat()  const { return mbIdleFormat; }
     void ResetIdleFormat()     { mbIdleFormat = false; }
@@ -261,7 +261,7 @@ public:
             // May be NULL if called from SfxBaseModel::dispose
             // (this happens in the build test 'rtfexport').
             if (pCurrShell != nullptr)
-                pCurrShell->GetDoc()->getIDocumentTimerAccess().StartBackgroundJobs();
+                pCurrShell->GetDoc()->getIDocumentTimerAccess().StartIdling();
         }
     }
 
