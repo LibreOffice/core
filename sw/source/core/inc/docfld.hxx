307f7aab446a8d51d23de37b52280e667a0d7817
Jan-Marek Glogowski <glogow@fbihome.de>
2018-08-17 23:10:00 +0200

    tdf#116370 cleanup Writer idle job handing
    This prevents the start of the idle job, while processing itself,
    so the fixed WinSalInstance::AnyInput of commit 3bf6c97029d2
    ("tdf#112975 WIN correctly handle VclInputFlags::OTHER") won't
    report the timer events of the re-started idle job to process.
    
    Fixes the early abort of the background job, which resulted in
    the busy loop of the reported bug and this strange printing
    behaviour.
    
    P.S. I'm not sure, why this was just broken on Windows.
    
    Change-Id: I6503dcd925c9a0ed843e794a31eea32a4a4b2889
    Reviewed-on: https://gerrit.libreoffice.org/59279
    Tested-by: Jenkins
    Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>
    (cherry picked from commit 401cba4c20fbc930f034168872642428d7459218)

--- sw/source/core/inc/docfld.hxx
+++ sw/source/core/inc/docfld.hxx
@@ -169,7 +169,7 @@ public:
 
         if (b)
         {
-            pDocument->getIDocumentTimerAccess().StartBackgroundJobs();
+            pDocument->getIDocumentTimerAccess().StartIdling();
         }
     }
 
