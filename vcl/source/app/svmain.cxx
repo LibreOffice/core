LibreOffice/core

commit 9cceba9a928cf3b3447f293020be2fe76c035ed5

make DBG_TESTSOLARMUTEX available in assert builds
where our QA people are more likely to trigger it

Change-Id: I4ce7c8c72e7e21f2296c0f9cc9f019aaef32ed0b
Reviewed-on: https://gerrit.libreoffice.org/58170
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/source/app/svmain.cxx
@@ -367,7 +367,7 @@ bool InitVCL()
     // Set exception handler
     pExceptionHandler = osl_addSignalHandler(VCLExceptionSignal_impl, nullptr);
 
-#ifdef DBG_UTIL
+#ifndef NDEBUG
     DbgGUIInitSolarMutexCheck();
 #endif
 

include/tools/debug.hxx
@@ -35,7 +35,8 @@
     standard assert.
 */
 
-#ifdef DBG_UTIL
+#ifndef NDEBUG
+// we want the solar mutex checking to be enabled in the assert-enabled builds that the QA people use
 
 typedef void (*DbgTestSolarMutexProc)();
 
@@ -49,7 +50,6 @@ do                             \
 } while(false)
 
 #else
-// NO DBG_UTIL
 
 #define DBG_TESTSOLARMUTEX() ((void)0)
 

tools/source/debug/debug.cxx
@@ -45,7 +45,7 @@
 #include <cxxabi.h>
 #endif
 
-#ifdef DBG_UTIL
+#ifndef NDEBUG
 
 struct DebugData
 {

commit 71a0ceef64cac54e01c0fef43bca72ec271e6db2

fix crash on Windows on assert enabled builds
regression from
    commit 9cceba9a928cf3b3447f293020be2fe76c035ed5
    make DBG_TESTSOLARMUTEX available in assert builds

Change-Id: Ifbedc3c16f341a12ba026fc598fb5386da91c50c

vcl/source/app/svmain.cxx
@@ -493,7 +493,7 @@ void DeInitVCL()
     }
     pSVData->mpDefaultWin.disposeAndClear();
 
-#ifdef DBG_UTIL
+#ifndef NDEBUG
     DbgGUIDeInitSolarMutexCheck();
 #endif
 