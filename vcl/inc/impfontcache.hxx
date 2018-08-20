4d627c999647a71dce94f8942177a70cc060c56d
Caolán McNamara <caolanm@redhat.com>
2018-08-20 11:12:36 +0100

    these just use the base FontSelectPatternAttributes
    Change-Id: I0c5ffe571c2cf37ec7d20d4d3ae965227cd72b7e

--- vcl/inc/impfontcache.hxx
+++ vcl/inc/impfontcache.hxx
@@ -37,8 +37,8 @@ private:
     LogicalFontInstance* mpLastHitCacheEntry; ///< keeps the last hit cache entry
 
     // cache of recently used font instances
-    struct IFSD_Equal { bool operator()( const FontSelectPattern&, const FontSelectPattern& ) const; };
-    struct IFSD_Hash { size_t operator()( const FontSelectPattern& ) const; };
+    struct IFSD_Equal { bool operator()( const FontSelectPatternAttributes&, const FontSelectPatternAttributes& ) const; };
+    struct IFSD_Hash { size_t operator()( const FontSelectPatternAttributes& ) const; };
     typedef std::unordered_map<FontSelectPattern, rtl::Reference<LogicalFontInstance>, IFSD_Hash, IFSD_Equal> FontInstanceList;
     FontInstanceList    maFontInstanceList;
 
