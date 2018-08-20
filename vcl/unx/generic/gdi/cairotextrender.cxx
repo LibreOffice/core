c4af0e1f65683a5647684b97f9d48d451e7072ec
Caolán McNamara <caolanm@redhat.com>
2018-08-20 14:21:27 +0100

    only need FontSelectPatternAttributes here
    Change-Id: I3e7595a952c7e95c82091cc483f13a0e96c13532

--- vcl/unx/generic/gdi/cairotextrender.cxx
+++ vcl/unx/generic/gdi/cairotextrender.cxx
@@ -198,7 +198,7 @@ void CairoTextRender::DrawTextLayout(const GenericSalLayout& rLayout, const SalG
     if (cairo_glyphs.empty())
         return;
 
-    const FontSelectPattern& rFSD = rInstance.GetFontSelectPattern();
+    const FontSelectPatternAttributes& rFSD = rInstance.GetFontSelectPattern();
     int nHeight = rFSD.mnHeight;
     int nWidth = rFSD.mnWidth ? rFSD.mnWidth : nHeight;
     if (nWidth == 0 || nHeight == 0)
