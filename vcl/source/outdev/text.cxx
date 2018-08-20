ae814d8b00364da8c1d3b12d91fd781081033ce8
Caolán McNamara <caolanm@redhat.com>
2018-08-20 12:55:39 +0100

    can use FontSelectPatternAttributes here
    Change-Id: I7702c8a763ff9c624ef9c64f80c3e8457a027d40
    Reviewed-on: https://gerrit.libreoffice.org/59324
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>

--- vcl/source/outdev/text.cxx
+++ vcl/source/outdev/text.cxx
@@ -218,7 +218,7 @@ bool OutputDevice::ImplDrawRotateText( SalLayout& rSalLayout )
     if( !pVDev->SetOutputSizePixel( aBoundRect.GetSize() ) )
         return false;
 
-    const FontSelectPattern& rPattern = mpFontInstance->GetFontSelectPattern();
+    const FontSelectPatternAttributes& rPattern = mpFontInstance->GetFontSelectPattern();
     vcl::Font aFont( GetFont() );
     aFont.SetOrientation( 0 );
     aFont.SetFontSize( Size( rPattern.mnWidth, rPattern.mnHeight ) );
