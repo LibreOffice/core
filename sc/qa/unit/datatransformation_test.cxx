67f163ffbd747b324578bc0c197dff0b6a2c5879
Vikas Mahato <vikasmahato0@gmail.com>
2018-08-21 23:58:58 +0530

    Added date time transformations - follow-up
    Change-Id: I130feda7c835b067a542736ad6fadc79cabc4f41

--- sc/qa/unit/datatransformation_test.cxx
+++ sc/qa/unit/datatransformation_test.cxx
@@ -606,7 +606,7 @@ void ScDataTransformationTest::testGetEndOfYear()
 void ScDataTransformationTest::testGetMonth()
 {
     m_pDoc->SetValue(2, 0, 0, 20);
-    m_pDoc->SetValue(2, 1, 0, 3342.44);
+    m_pDoc->SetValue(2, 1, 0, 43248.5624189815);
     m_pDoc->SetValue(2, 2, 0, 955.05);
     m_pDoc->SetValue(2, 3, 0, 4890.22);
 
@@ -614,7 +614,7 @@ void ScDataTransformationTest::testGetMonth()
     aTransform.Transform(*m_pDoc);
 
     CPPUNIT_ASSERT_DOUBLES_EQUAL(1, m_pDoc->GetValue(2, 0, 0), 0);
-    CPPUNIT_ASSERT_DOUBLES_EQUAL(2, m_pDoc->GetValue(2, 1, 0), 0);
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(5, m_pDoc->GetValue(2, 1, 0), 0);
     CPPUNIT_ASSERT_DOUBLES_EQUAL(8, m_pDoc->GetValue(2, 2, 0), 0);
     CPPUNIT_ASSERT_DOUBLES_EQUAL(5, m_pDoc->GetValue(2, 3, 0), 0);
 }
@@ -622,8 +622,8 @@ void ScDataTransformationTest::testGetMonth()
 void ScDataTransformationTest::testGetMonthName()
 {
     m_pDoc->SetValue(2, 0, 0, 20);
-    m_pDoc->SetValue(2, 1, 0, 3342.44);
-    m_pDoc->SetValue(2, 2, 0, 955.05);
+    m_pDoc->SetValue(2, 1, 0, 34);
+    m_pDoc->SetValue(2, 2, 0, 70);
     m_pDoc->SetValue(2, 3, 0, 4890.22);
 
     sc:: DateTimeTransformation aTransform({2}, sc::DATETIME_TRANSFORMATION_TYPE::MONTH_NAME);
@@ -631,7 +631,7 @@ void ScDataTransformationTest::testGetMonthName()
 
     CPPUNIT_ASSERT_EQUAL(OUString("January"), m_pDoc->GetString(2, 0, 0));
     CPPUNIT_ASSERT_EQUAL(OUString("February"), m_pDoc->GetString(2, 1, 0));
-    CPPUNIT_ASSERT_EQUAL(OUString("August"), m_pDoc->GetString(2, 2, 0));
+    CPPUNIT_ASSERT_EQUAL(OUString("March"), m_pDoc->GetString(2, 2, 0));
     CPPUNIT_ASSERT_EQUAL(OUString("May"), m_pDoc->GetString(2, 3, 0));
 }
 
@@ -774,7 +774,7 @@ void ScDataTransformationTest::testGetTime()
     aTransform.Transform(*m_pDoc);
 
     CPPUNIT_ASSERT_EQUAL(OUString("01:29:53 PM"), m_pDoc->GetString(2, 0, 0));
-    CPPUNIT_ASSERT_EQUAL(OUString("07:23:09 AM"), m_pDoc->GetString(2, 1, 0));
+    CPPUNIT_ASSERT_EQUAL(OUString("07:23:10 AM"), m_pDoc->GetString(2, 1, 0));
     CPPUNIT_ASSERT_EQUAL(OUString("09:34:40 AM"), m_pDoc->GetString(2, 2, 0));
     CPPUNIT_ASSERT_EQUAL(OUString("10:09:49 AM"), m_pDoc->GetString(2, 3, 0));
 }
