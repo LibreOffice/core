ff6ea30f73fbc6edb85090f98dec8b0c97e02ef4
Noel Grandin <noel.grandin@collabora.co.uk>
2018-08-21 12:34:29 +0200

    tdf#119282 Area tab, Pattern color change
    regression from
    
        commit ccd316d1cb310734848bd20244f509024b549b8c
    	use VirtualDevice in createHistorical8x8FromArray
    
    Unfortunately this means moving BitmapWriteAccess back to /include.I
    tried to detect this thing by counting the number of independent colors
    in the bitmap, but that didn't work. It is used from more than one
    place, and appears to be very determined that it needs a 2-color palette
    to work properly.
    
    Change-Id: Ie3f312e83a9c61b9785657270396a3c437807009

--- vcl/inc/bitmapwriteaccess.hxx
+++ vcl/inc/bitmapwriteaccess.hxx
@@ -11,85 +11,12 @@
 #ifndef INCLUDED_VCL_INC_BITMAPWRITEACCESS_HXX
 #define INCLUDED_VCL_INC_BITMAPWRITEACCESS_HXX
 
-#include <vcl/alpha.hxx>
-#include <vcl/bitmap.hxx>
-#include <vcl/bitmapaccess.hxx>
-#include <boost/optional.hpp>
+#include <vcl/bitmapwriteaccess.hxx>
 
-typedef vcl::ScopedBitmapAccess<BitmapWriteAccess, Bitmap, &Bitmap::AcquireWriteAccess>
-    BitmapScopedWriteAccess;
-
-typedef vcl::ScopedBitmapAccess<BitmapWriteAccess, AlphaMask, &AlphaMask::AcquireWriteAccess>
-    AlphaScopedWriteAccess;
-
-class VCL_DLLPUBLIC BitmapWriteAccess : public BitmapReadAccess
-{
-public:
-    BitmapWriteAccess(Bitmap& rBitmap);
-    virtual ~BitmapWriteAccess() override;
-
-    void CopyScanline(long nY, const BitmapReadAccess& rReadAcc);
-    void CopyScanline(long nY, ConstScanline aSrcScanline, ScanlineFormat nSrcScanlineFormat,
-                      sal_uLong nSrcScanlineSize);
-
-    void CopyBuffer(const BitmapReadAccess& rReadAcc);
-
-    void SetPalette(const BitmapPalette& rPalette)
-    {
-        assert(mpBuffer && "Access is not valid!");
-
-        mpBuffer->maPalette = rPalette;
-    }
-
-    void SetPaletteEntryCount(sal_uInt16 nCount)
-    {
-        assert(mpBuffer && "Access is not valid!");
-
-        mpBuffer->maPalette.SetEntryCount(nCount);
-    }
-
-    void SetPaletteColor(sal_uInt16 nColor, const BitmapColor& rBitmapColor)
-    {
-        assert(mpBuffer && "Access is not valid!");
-        assert(HasPalette() && "Bitmap has no palette!");
-
-        mpBuffer->maPalette[nColor] = rBitmapColor;
-    }
-
-    void SetPixel(long nY, long nX, const BitmapColor& rBitmapColor)
-    {
-        assert(mpBuffer && "Access is not valid!");
-        assert(nX < mpBuffer->mnWidth && "x-coordinate out of range!");
-        assert(nY < mpBuffer->mnHeight && "y-coordinate out of range!");
-
-        mFncSetPixel(GetScanline(nY), nX, rBitmapColor, maColorMask);
-    }
-
-    void SetPixelIndex(long nY, long nX, sal_uInt8 cIndex)
-    {
-        SetPixel(nY, nX, BitmapColor(cIndex));
-    }
-
-    void SetLineColor(const Color& rColor);
-
-    void SetFillColor();
-    void SetFillColor(const Color& rColor);
-
-    void Erase(const Color& rColor);
-
-    void DrawLine(const Point& rStart, const Point& rEnd);
-
-    void FillRect(const tools::Rectangle& rRect);
-    void DrawRect(const tools::Rectangle& rRect);
-
-private:
-    boost::optional<BitmapColor> mpLineColor;
-    boost::optional<BitmapColor> mpFillColor;
-
-    BitmapWriteAccess() = delete;
-    BitmapWriteAccess(const BitmapWriteAccess&) = delete;
-    BitmapWriteAccess& operator=(const BitmapWriteAccess&) = delete;
-};
+/**
+  * Left this header here as a redirection because we intend to move this code back inside vcl again
+  * once we figure out how to untangle some svx/ code.
+  */
 
 #endif
 
