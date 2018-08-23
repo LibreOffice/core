LibreOffice/core

commit de8f6b25de6fbe813fe172542e7eff1596b37335

loplugin:unused-returns in vcl
Change-Id: I507320900a47f604d17ed7d402d531a7cbdf744e
Reviewed-on: https://gerrit.libreoffice.org/48331
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/unx/generic/print/glyphset.cxx
@@ -58,15 +58,15 @@ GlyphSet::~GlyphSet ()
     /* FIXME delete the glyphlist ??? */
 }
 
-bool
+void
 GlyphSet::GetGlyphID (
                       sal_GlyphId nGlyph,
                       unsigned char* nOutGlyphID,
                       sal_Int32* nOutGlyphSetID
                      )
 {
-    return    LookupGlyphID (nGlyph, nOutGlyphID, nOutGlyphSetID)
-           || AddGlyphID    (nGlyph, nOutGlyphID, nOutGlyphSetID);
+    if (LookupGlyphID(nGlyph, nOutGlyphID, nOutGlyphSetID))
+           AddGlyphID(nGlyph, nOutGlyphID, nOutGlyphSetID);
 }
 
 bool

commit e1247924c17e363141825dba6fc9bbe123d85041

tdf#119357 add the glyph, if the lookup failed
Regression from commit de8f6b25de6f ("loplugin:unused-returns in
vcl") changing the logic in hunk:

-    return    LookupGlyphID (nGlyph, nOutGlyphID, nOutGlyphSetID)
-           || AddGlyphID    (nGlyph, nOutGlyphID, nOutGlyphSetID);
+    if (LookupGlyphID(nGlyph, nOutGlyphID, nOutGlyphSetID))
+           AddGlyphID(nGlyph, nOutGlyphID, nOutGlyphSetID);

Change-Id: Id643120e1cf5e26b3ffda933d07893048493f089

vcl/unx/generic/print/glyphset.cxx
@@ -65,8 +65,8 @@ GlyphSet::GetGlyphID (
                       sal_Int32* nOutGlyphSetID
                      )
 {
-    if (LookupGlyphID(nGlyph, nOutGlyphID, nOutGlyphSetID))
-           AddGlyphID(nGlyph, nOutGlyphID, nOutGlyphSetID);
+    if (!LookupGlyphID(nGlyph, nOutGlyphID, nOutGlyphSetID))
+        AddGlyphID(nGlyph, nOutGlyphID, nOutGlyphSetID);
 }
 
 bool