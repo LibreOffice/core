LibreOffice/core

commit 2ade07126971b79c92f729fae5709f2e2e2b495c

fdo#62044 RTF import: don't overwrite existing styles when pasting
Change-Id: I80a83caebc8fa3f038cf2ff080c6c6ec8e93fb70

writerfilter/source/dmapper/StyleSheetTable.cxx
@@ -275,19 +275,21 @@ struct StyleSheetTable_Impl
     PropertyMapPtr                          m_pCurrentProps;
     StringPairMap_t                         m_aStyleNameMap;
     ListCharStylePropertyVector_t           m_aListCharStylePropertyVector;
+    bool                                    m_bIsNewDoc;
 
-    StyleSheetTable_Impl(DomainMapper& rDMapper, uno::Reference< text::XTextDocument> xTextDocument);
+    StyleSheetTable_Impl(DomainMapper& rDMapper, uno::Reference< text::XTextDocument> xTextDocument, bool bIsNewDoc);
 
     OUString HasListCharStyle( const PropertyValueVector_t& rCharProperties );
 };
 
 
-StyleSheetTable_Impl::StyleSheetTable_Impl(DomainMapper& rDMapper, uno::Reference< text::XTextDocument> xTextDocument ) :
+StyleSheetTable_Impl::StyleSheetTable_Impl(DomainMapper& rDMapper, uno::Reference< text::XTextDocument> xTextDocument, bool bIsNewDoc ) :
             m_rDMapper( rDMapper ),
             m_xTextDocument( xTextDocument ),
             m_pCurrentEntry(),
             m_pDefaultParaProps(new PropertyMap),
-            m_pDefaultCharProps(new PropertyMap)
+            m_pDefaultCharProps(new PropertyMap),
+            m_bIsNewDoc(bIsNewDoc)
 {
     //set font height default to 10pt
     uno::Any aVal = uno::makeAny( double(10.) );
@@ -342,10 +344,10 @@ OUString StyleSheetTable_Impl::HasListCharStyle( const PropertyValueVector_t& rP
 }
 
 
-StyleSheetTable::StyleSheetTable(DomainMapper& rDMapper, uno::Reference< text::XTextDocument> xTextDocument)
+StyleSheetTable::StyleSheetTable(DomainMapper& rDMapper, uno::Reference< text::XTextDocument> xTextDocument, bool bIsNewDoc)
 : LoggedProperties(dmapper_logger, "StyleSheetTable")
 , LoggedTable(dmapper_logger, "StyleSheetTable")
-, m_pImpl( new StyleSheetTable_Impl(rDMapper, xTextDocument) )
+, m_pImpl( new StyleSheetTable_Impl(rDMapper, xTextDocument, bIsNewDoc) )
 {
 }
 
@@ -722,7 +724,8 @@ void StyleSheetTable::ApplyStyleSheets( FontTablePtr rFontTable )
                     uno::Reference< container::XNameContainer > xStyles = bParaStyle ? xParaStyles : xCharStyles;
                     uno::Reference< style::XStyle > xStyle;
                     OUString sConvertedStyleName = ConvertStyleName( pEntry->sStyleName );
-                    if(xStyles->hasByName( sConvertedStyleName ))
+                    // When pasting, don't update existing styles.
+                    if(xStyles->hasByName( sConvertedStyleName ) && m_pImpl->m_bIsNewDoc)
                         xStyles->getByName( sConvertedStyleName ) >>= xStyle;
                     else
                     {

sw/qa/extras/rtfimport/data/fdo62044-paste.rtf
@@ -1,0 +1,7 @@
+{\rtf
+{\stylesheet
+{\s14\fs36 Heading1;}
+}
+from impress
+\par
+}

sw/qa/extras/rtfimport/data/fdo62044.rtf
@@ -1,0 +1,7 @@
+{\rtf1
+{\stylesheet
+{\s1\fs20 Heading 1;}
+}
+\s1 this is heading 1
+\par
+}

sw/qa/extras/rtfimport/rtfimport.cxx
@@ -138,6 +138,7 @@ public:
     void testN818997();
     void testFdo64671();
     void testPageBackground();
+    void testFdo62044();
 
     CPPUNIT_TEST_SUITE(Test);
 #if !defined(MACOSX) && !defined(WNT)
@@ -265,6 +266,7 @@ void Test::run()
         {"n818997.rtf", &Test::testN818997},
         {"fdo64671.rtf", &Test::testFdo64671},
         {"page-background.rtf", &Test::testPageBackground},
+        {"fdo62044.rtf", &Test::testFdo62044},
     };
     header();
     for (unsigned int i = 0; i < SAL_N_ELEMENTS(aMethods); ++i)
@@ -1255,6 +1257,18 @@ void Test::testPageBackground()
     // The problem was that \background was ignored.
     uno::Reference<beans::XPropertySet> xPageStyle(getStyles("PageStyles")->getByName(DEFAULT_STYLE), uno::UNO_QUERY);
     CPPUNIT_ASSERT_EQUAL(sal_Int32(0x92D050), getProperty<sal_Int32>(xPageStyle, "BackColor"));
+}
+
+void Test::testFdo62044()
+{
+    // The problem was that RTF import during copy&paste did not ignore existing paragraph styles.
+    uno::Reference<text::XTextDocument> xTextDocument(mxComponent, uno::UNO_QUERY);
+    uno::Reference<text::XTextRange> xText(xTextDocument->getText(), uno::UNO_QUERY);
+    uno::Reference<text::XTextRange> xEnd = xText->getEnd();
+    paste("fdo62044-paste.rtf", xEnd);
+
+    uno::Reference<beans::XPropertySet> xPropertySet(getStyles("ParagraphStyles")->getByName("Heading 1"), uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(10.f, getProperty<float>(xPropertySet, "CharHeight")); // Was 18, i.e. reset back to original value.
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(Test);

writerfilter/source/dmapper/DomainMapper_Impl.hxx
@@ -481,7 +481,7 @@ public:
     StyleSheetTablePtr GetStyleSheetTable()
     {
         if(!m_pStyleSheetTable)
-            m_pStyleSheetTable.reset(new StyleSheetTable( m_rDMapper, m_xTextDocument ));
+            m_pStyleSheetTable.reset(new StyleSheetTable( m_rDMapper, m_xTextDocument, m_bIsNewDoc ));
         return m_pStyleSheetTable;
     }
     ListsManager::Pointer GetListTable();

writerfilter/source/dmapper/StyleSheetTable.hxx
@@ -81,7 +81,7 @@ class StyleSheetTable :
 
 public:
     StyleSheetTable( DomainMapper& rDMapper,
-                        ::com::sun::star::uno::Reference< ::com::sun::star::text::XTextDocument> xTextDocument );
+                        ::com::sun::star::uno::Reference< ::com::sun::star::text::XTextDocument> xTextDocument, bool bIsNewDoc );
     virtual ~StyleSheetTable();
 
     void ApplyStyleSheets( FontTablePtr rFontTable );

commit 8859da05bb36eb6cf61138ba7253a9d9a749bd39

Resolves: tdf#118382 crash on paste of rtf
into an existing document since....

commit deaa16bd24fba4a3a6b072ab2b94542a7dbd41c6
Author: Serge Krot <Serge.Krot@cib.de>
Date:   Mon Jan 29 12:17:06 2018 +0100

    tdf#72942 Do not overwrite styles from the inserted doc

    During inserting of the text from the external document
    we should not change original default styles in
    currently opened document.

because missing styles aren't created, so there's exceptons
thrown on their use.

I see that the m_bIsNewDoc flag is already checked for since...

commit 5ebe826caf75e8d00656b3333385bf0bbc05cb27
Author: Miklos Vajna <vmiklos@collabora.co.uk>
Date:   Fri Oct 4 11:02:59 2013 +0200

    fdo#69384 fix impress -> writer copy&paste

    Regression from 2ade07126971b79c92f729fae5709f2e2e2b495c (fdo#62044 RTF
    import: don't overwrite existing styles when pasting, 2013-06-04),
    during paste, if existing style was found, then the intention was to
    skip that style, but instead we tried to create one.

in the body of the following loop, which should mean that existing
styles are left alone and only missing styles created if we remove
this hunk of the tdf#72942 commit

Change-Id: Ia4aedb5e309c991490656061aa20d11ff24f4f9e

writerfilter/source/dmapper/StyleSheetTable.cxx
@@ -911,13 +911,6 @@ uno::Sequence< OUString > PropValVector::getNames()
 
 void StyleSheetTable::ApplyStyleSheets( const FontTablePtr& rFontTable )
 {
-    if (!m_pImpl->m_bIsNewDoc)
-    {
-        // tdf#72942: do not import styles from document being inserted,
-        // while they corrupts original styles in master document
-        return;
-    }
-
     try
     {
         uno::Reference< style::XStyleFamiliesSupplier > xStylesSupplier( m_pImpl->m_xTextDocument, uno::UNO_QUERY_THROW );