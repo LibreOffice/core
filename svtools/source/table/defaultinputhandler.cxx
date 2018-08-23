LibreOffice/core

commit 0fb4ae8767fa5ff791cd42934b4215011a269eb0

Modernize a bit svtools
by using for-range loops
+ use empty() instead of comparing begin and end iterator

Change-Id: I41c3ae0c9032d6349afb113d457cb78862ae9dcf
Reviewed-on: https://gerrit.libreoffice.org/49416
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

svtools/source/table/defaultinputhandler.cxx
@@ -87,19 +87,16 @@ namespace svt { namespace table
 
             // ask all other handlers
             bool handled = false;
-            for (   MouseFunctions::iterator handler = i_impl.aMouseFunctions.begin();
-                    ( handler != i_impl.aMouseFunctions.end() ) && !handled;
-                    ++handler
-                )
+            for (auto const& mouseFunction : i_impl.aMouseFunctions)
             {
-                if ( *handler == i_impl.pActiveFunction )
+                if (mouseFunction == i_impl.pActiveFunction)
                     // we already invoked this function
                     continue;
 
-                switch ( (handler->get()->*i_handlerMethod)( i_control, i_event ) )
+                switch ( (mouseFunction.get()->*i_handlerMethod)( i_control, i_event ) )
                 {
                 case ActivateFunction:
-                    i_impl.pActiveFunction = *handler;
+                    i_impl.pActiveFunction = mouseFunction;
                     handled = true;
                     break;
                 case ContinueFunction:

svtools/source/brwbox/brwbox1.cxx
@@ -220,11 +220,8 @@ void BrowseBox::InsertHandleColumn( sal_uLong nWidth )
 #if OSL_DEBUG_LEVEL > 0
     OSL_ENSURE( ColCount() == 0 || pCols[0]->GetId() != HandleColumnId , "BrowseBox::InsertHandleColumn: there is already a handle column" );
     {
-        BrowserColumns::iterator iCol = pCols.begin();
-        const BrowserColumns::iterator colsEnd = pCols.end();
-        if ( iCol != colsEnd )
-            for (++iCol; iCol != colsEnd; ++iCol)
-                OSL_ENSURE( (*iCol)->GetId() != HandleColumnId, "BrowseBox::InsertHandleColumn: there is a non-Handle column with handle ID" );
+        for (auto const col : pCols)
+            OSL_ENSURE( col->GetId() != HandleColumnId, "BrowseBox::InsertHandleColumn: there is a non-Handle column with handle ID" );
     }
 #endif
 
@@ -253,9 +250,8 @@ void BrowseBox::InsertDataColumn( sal_uInt16 nItemId, const OUString& rText,
 
 #if OSL_DEBUG_LEVEL > 0
     {
-        const BrowserColumns::iterator colsEnd = pCols.end();
-        for (BrowserColumns::iterator iCol = pCols.begin(); iCol != colsEnd; ++iCol)
-            OSL_ENSURE( (*iCol)->GetId() != nItemId, "BrowseBox::InsertDataColumn: duplicate column Id" );
+        for (auto const& col : pCols)
+            OSL_ENSURE( col->GetId() != nItemId, "BrowseBox::InsertDataColumn: duplicate column Id" );
     }
 #endif
 

svtools/source/config/extcolorcfg.cxx
@@ -392,30 +392,27 @@ void ExtendedColorConfig_Impl::ImplCommit()
                    + m_sLoadedScheme;
     const OUString s_sSep("/");
 
-    TComponents::iterator aIter = m_aConfigValues.begin();
-    TComponents::iterator aEnd = m_aConfigValues.end();
-    for( ;aIter != aEnd;++aIter )
+    for (auto const& configValue : m_aConfigValues)
     {
-        if ( ConfigItem::AddNode(sBase, aIter->first) )
+        if ( ConfigItem::AddNode(sBase, configValue.first) )
         {
             OUString sNode = sBase
                            + s_sSep
-                           + aIter->first
+                           + configValue.first
             //ConfigItem::AddNode(sNode, sColorEntries);
                            + s_sSep
                            + sColorEntries;
 
-            uno::Sequence < beans::PropertyValue > aPropValues(aIter->second.first.size());
+            uno::Sequence < beans::PropertyValue > aPropValues(configValue.second.first.size());
             beans::PropertyValue* pPropValues = aPropValues.getArray();
-            TConfigValues::iterator aConIter = aIter->second.first.begin();
-            TConfigValues::iterator aConEnd  = aIter->second.first.end();
-            for (; aConIter != aConEnd; ++aConIter,++pPropValues)
+            for (auto const& elem : configValue.second.first)
             {
-                pPropValues->Name = sNode + s_sSep + aConIter->first;
-                ConfigItem::AddNode(sNode, aConIter->first);
+                pPropValues->Name = sNode + s_sSep + elem.first;
+                ConfigItem::AddNode(sNode, elem.first);
                 pPropValues->Name += sColor;
-                pPropValues->Value <<= aConIter->second.getColor();
+                pPropValues->Value <<= elem.second.getColor();
                 // the default color will never be changed
+                ++pPropValues;
             }
             SetSetProperties("ExtendedColorScheme/ColorSchemes", aPropValues);
         }

svtools/source/contnr/svtabbx.cxx
@@ -1153,10 +1153,12 @@ sal_Int32 SvHeaderTabListBox::GetFieldIndexAtPoint(sal_Int32 _nRow,sal_Int32 _nC
     MetricVector aRects;
     if ( GetGlyphBoundRects(Point(0,0), sText, 0, sText.getLength(), aRects) )
     {
-        for (MetricVector::iterator aIter = aRects.begin(); aIter != aRects.end(); ++aIter)
+        sal_Int32 nPos = 0;
+        for (auto const& rectangle : aRects)
         {
-            if( aIter->IsInside(_rPoint) )
-                return aIter - aRects.begin();
+            if( rectangle.IsInside(_rPoint) )
+                return nPos;
+            ++nPos;
         }
     }
 

svtools/source/control/calendar.cxx
@@ -824,23 +824,21 @@ void Calendar::ImplUpdateSelection( IntDateSet* pOld )
 {
     IntDateSet*  pNew = mpSelectTable;
 
-    for ( IntDateSet::const_iterator it = pOld->begin(); it != pOld->end(); ++it )
+    for (auto const& nKey : *pOld)
     {
-        sal_Int32 nKey = *it;
-        if ( pNew->find( nKey ) == pNew->end() )
+        if ( pNew->find(nKey) == pNew->end() )
         {
-            Date aTempDate( nKey );
-            ImplUpdateDate( aTempDate );
+            Date aTempDate(nKey);
+            ImplUpdateDate(aTempDate);
         }
     }
 
-    for ( IntDateSet::const_iterator it = pNew->begin(); it != pNew->end(); ++it )
+    for (auto const& nKey : *pNew)
     {
-        sal_Int32 nKey = *it;
-        if ( pOld->find( nKey ) == pOld->end() )
+        if ( pOld->find(nKey) == pOld->end() )
         {
-            Date aTempDate( nKey );
-            ImplUpdateDate( aTempDate );
+            Date aTempDate(nKey);
+            ImplUpdateDate(aTempDate);
         }
     }
 }

svtools/source/control/ctrlbox.cxx
@@ -175,11 +175,11 @@ long BorderWidthImpl::GuessWidth( long nLine1, long nLine2, long nGap )
     if ( (!bInvalid) && (!aToCompare.empty()) )
     {
         nWidth = *aToCompare.begin();
-        std::vector< double >::iterator pIt = aToCompare.begin();
-        while ( pIt != aToCompare.end() && !bInvalid )
+        for (auto const& elem : aToCompare)
         {
-            bInvalid = ( nWidth != *pIt );
-            ++pIt;
+            bInvalid = ( nWidth != elem );
+            if (bInvalid)
+                break;
         }
         nWidth = bInvalid ?  0.0 : nLine1 + nLine2 + nGap;
     }

svtools/source/control/inettbc.cxx
@@ -227,13 +227,11 @@ IMPL_LINK_NOARG( SvtMatchContext_Impl, Select_Impl, void*, void )
     // insert all completed strings into the listbox
     pBox->Clear();
 
-    for(std::vector<OUString>::iterator i = aCompletions.begin(); i != aCompletions.end(); ++i)
+    for (auto const& completion : aCompletions)
     {
-        OUString sCompletion(*i);
-
         // convert the file into an URL
         OUString sURL;
-        osl::FileBase::getFileURLFromSystemPath( sCompletion, sURL );
+        osl::FileBase::getFileURLFromSystemPath(completion, sURL);
             // note: if this doesn't work, we're not interested in: we're checking the
             // untouched sCompletion then
 
@@ -250,7 +248,7 @@ IMPL_LINK_NOARG( SvtMatchContext_Impl, Select_Impl, void*, void )
             }
         }
 
-        pBox->InsertEntry( sCompletion );
+        pBox->InsertEntry(completion);
     }
 
     if( !bNoSelection && !aCompletions.empty() && !bValidCompletionsFiltered )

svtools/source/control/roadmap.cxx
@@ -233,9 +233,9 @@ void ORoadmap::dispose()
 {
     HL_Vector aItemsCopy = m_pImpl->getHyperLabels();
     m_pImpl->getHyperLabels().clear();
-    for ( HL_Vector::iterator i = aItemsCopy.begin(); i != aItemsCopy.end(); ++i )
+    for (auto const& itemCopy : aItemsCopy)
     {
-        delete *i;
+        delete itemCopy;
     }
     if ( ! m_pImpl->isComplete() )
         delete m_pImpl->InCompleteHyperLabel;
@@ -295,12 +295,9 @@ void ORoadmap::SetRoadmapInteractive(bool _bInteractive)
     m_pImpl->setInteractive( _bInteractive );
 
     const HL_Vector& rItems = m_pImpl->getHyperLabels();
-    for (   HL_Vector::const_iterator i = rItems.begin();
-            i != rItems.end();
-            ++i
-        )
+    for (auto const& item : rItems)
     {
-        (*i)->SetInteractive( _bInteractive );
+        item->SetInteractive( _bInteractive );
     }
 }
 
@@ -330,15 +327,11 @@ void ORoadmap::UpdatefollowingHyperLabels(ItemIndex _nIndex)
     const HL_Vector& rItems = m_pImpl->getHyperLabels();
     if ( _nIndex < static_cast<ItemIndex>(rItems.size()) )
     {
-        for (   HL_Vector::const_iterator i = rItems.begin() + _nIndex;
-                i != rItems.end();
-                ++i, ++_nIndex
-            )
+        for (auto const& item : rItems)
         {
-            RoadmapItem* pItem = *i;
-
-            pItem->SetIndex( _nIndex );
-            pItem->SetPosition( GetPreviousHyperLabel( _nIndex ) );
+            item->SetIndex( _nIndex );
+            item->SetPosition( GetPreviousHyperLabel( _nIndex ) );
+            ++_nIndex;
         }
     }
     if ( ! m_pImpl->isComplete() )
@@ -410,12 +403,11 @@ void ORoadmap::ChangeRoadmapItemLabel( ItemId _nID, const OUString& _sLabel )
     pItem->Update( pItem->GetIndex(), _sLabel );
 
     const HL_Vector& rItems = m_pImpl->getHyperLabels();
-    for (   HL_Vector::const_iterator i = rItems.begin();
-            i != rItems.end();
-            ++i
-        )
+    size_t nPos = 0;
+    for (auto const& item : rItems)
     {
-        (*i)->SetPosition( GetPreviousHyperLabel( i - rItems.begin() ) );
+        item->SetPosition( GetPreviousHyperLabel(nPos) );
+        ++nPos;
     }
 }
 
@@ -430,14 +422,11 @@ RoadmapItem* ORoadmap::GetByID(ItemId _nID)
 {
     ItemId nLocID = 0;
     const HL_Vector& rItems = m_pImpl->getHyperLabels();
-    for ( HL_Vector::const_iterator i = rItems.begin();
-          i != rItems.end();
-          ++i
-        )
+    for (auto const& item : rItems)
     {
-        nLocID = (*i)->GetID();
+        nLocID = item->GetID();
         if ( nLocID == _nID )
-            return *i;
+            return item;
     }
     return nullptr;
 }
@@ -493,12 +482,9 @@ RoadmapTypes::ItemId ORoadmap::GetPreviousAvailableItemId(ItemIndex _nNewIndex)
 void ORoadmap::DeselectOldRoadmapItems()
 {
     const HL_Vector& rItems = m_pImpl->getHyperLabels();
-    for (   HL_Vector::const_iterator i = rItems.begin();
-            i != rItems.end();
-            ++i
-        )
+    for (auto const& item : rItems)
     {
-        (*i)->ToggleBackgroundColor( COL_TRANSPARENT );
+        item->ToggleBackgroundColor( COL_TRANSPARENT );
     }
 }
 
@@ -586,13 +572,10 @@ void ORoadmap::DrawHeadline(vcl::RenderContext& rRenderContext)
 RoadmapItem* ORoadmap::GetByPointer(vcl::Window const * pWindow)
 {
     const HL_Vector& rItems = m_pImpl->getHyperLabels();
-    for (   HL_Vector::const_iterator i = rItems.begin();
-            i != rItems.end();
-            ++i
-        )
+    for (auto const& item : rItems)
     {
-        if ( (*i)->Contains( pWindow ) )
-            return *i;
+        if ( item->Contains( pWindow ) )
+            return item;
     }
     return nullptr;
 }

commit 72b38698b549d100f7ba33cd136b9c5ff8a9e08a

tdf#119427: fix selectionChanged event
Regression from 0fb4ae8767fa5ff791cd42934b4215011a269eb0
Thank you Drew for the bibisection!

Change-Id: Id444172d534374d71765bd4aca71b012cc2352f1

svtools/source/table/defaultinputhandler.cxx
@@ -89,6 +89,8 @@ namespace svt { namespace table
             bool handled = false;
             for (auto const& mouseFunction : i_impl.aMouseFunctions)
             {
+                if (handled)
+                    break;
                 if (mouseFunction == i_impl.pActiveFunction)
                     // we already invoked this function
                     continue;