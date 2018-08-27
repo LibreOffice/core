a8f4c18954848c24b11f6abeee49968c26af86be
Miklos Vajna <vmiklos@collabora.co.uk>
2018-08-27 09:15:16 +0200

    tdf#119309 xmlsecurity xades: missing XML attribute on idSignedProperties ref
    The AdES validator at
    <https://ec.europa.eu/cefdigital/DSS/webapp-demo/validation> recently
    learned to deal with ODF files, this improves its output, so that
    "Qualification Signature" section is no longer just a red "N/A" but an
    orange "Indeterminate QESig".
    
    Change-Id: I5f47b935f1dbfa4e2eee4654db31403479cb571d
    Reviewed-on: https://gerrit.libreoffice.org/59633
    Tested-by: Jenkins
    Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
    (cherry picked from commit ea3a5036d23081b6e8eb38a399ff8ef5acd8adc7)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/113091
    Tested-by: Michael Stahl <michael.stahl@allotropia.de>
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

--- xmlsecurity/source/helper/xsecctl.cxx
+++ xmlsecurity/source/helper/xsecctl.cxx
@@ -661,6 +661,14 @@ void XSecController::exportSignature(
                     pAttributeList->AddAttribute(
                         "URI",
                         "#" + refInfor.ouURI);
+
+                    if (bXAdESCompliantIfODF && refInfor.ouURI == "idSignedProperties")
+                    {
+                        // The reference which points to the SignedProperties
+                        // shall have this specific type.
+                        pAttributeList->AddAttribute("Type",
+                                                     "http://uri.etsi.org/01903#SignedProperties");
+                    }
                 }
 
                 xDocumentHandler->startElement( "Reference", cssu::Reference< cssxs::XAttributeList > (pAttributeList) );
