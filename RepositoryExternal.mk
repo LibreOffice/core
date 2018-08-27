1d0727a104d76210814f41c1169df318e40c9d80
Kohei Yoshida <kohei.yoshida@gmail.com>
2018-08-27 11:06:17 -0400

    Update orcus to 0.14.0.
    And make all necessary adjustments for the new version of orcus.
    
    Change-Id: I0dc207162a3ddfaad6da198a3d13b65f530757d5
    Reviewed-on: https://gerrit.libreoffice.org/59884
    Tested-by: Jenkins
    Reviewed-by: Kohei Yoshida <libreoffice@kohei.us>

--- RepositoryExternal.mk
+++ RepositoryExternal.mk
@@ -3295,7 +3295,7 @@ $(call gb_LinkTarget_set_include,$(1),\
 )
 
 $(call gb_LinkTarget_add_libs,$(1),\
-       -L$(call gb_UnpackedTarball_get_dir,liborcus)/src/liborcus/.libs -lorcus-0.13 \
+       -L$(call gb_UnpackedTarball_get_dir,liborcus)/src/liborcus/.libs -lorcus-0.14 \
 )
 
 $(if $(SYSTEM_BOOST), \
@@ -3314,7 +3314,7 @@ $(call gb_LinkTarget_set_include,$(1),\
 )
 
 $(call gb_LinkTarget_add_libs,$(1),\
-	-L$(call gb_UnpackedTarball_get_dir,liborcus)/src/parser/.libs -lorcus-parser-0.13 \
+	-L$(call gb_UnpackedTarball_get_dir,liborcus)/src/parser/.libs -lorcus-parser-0.14 \
 )
 
 endef
