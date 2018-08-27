c3c4847d359ad74f0ba45a36c28ecb75f4694ca1
Mike Kaganski <mike.kaganski@collabora.com>
2018-08-27 12:34:12 +0300

    tdf#119143: introduce tentative directional embedding import support
    ECMA-376-1:2016 states that w:dir is functionally equivalent to LRE/RLE+PDF
    pair around the enclosed runs. So this patch does just that.
    
    Change-Id: Ibf9775338cc38a3bbc38a42a33fc64ae787b478f
    Reviewed-on: https://gerrit.libreoffice.org/59643
    Tested-by: Jenkins
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

--- sw/qa/extras/ooxmlexport/ooxmlexport12.cxx
+++ sw/qa/extras/ooxmlexport/ooxmlexport12.cxx
@@ -777,6 +777,26 @@ DECLARE_OOXMLEXPORT_TEST(testTdf73547, "tdf73547-dash.docx")
     CPPUNIT_ASSERT_DOUBLES_EQUAL(35000.0, nSp, 5000.0); // was 100000
 }
 
+DECLARE_OOXMLEXPORT_TEST(testTdf119143, "tdf119143.docx")
+{
+    // The runs inside <w:dir> were ignored
+    const OUString sParaText = getParagraph(1)->getString();
+    CPPUNIT_ASSERT_EQUAL(
+        OUString::fromUtf8(
+            u8"عندما يريد العالم أن يتكلّم ‬ ، فهو يتحدّث "
+            u8"بلغة "
+            u8"يونيكود. تسجّل الآن لحضور المؤتمر الدولي العاشر "
+            u8"ليونيكود (Unicode Conference)، الذي سيعقد في 10-12 "
+            u8"آذار 1997 بمدينة مَايِنْتْس، ألمانيا. و سيجمع المؤتمر "
+            u8"بين خبراء من كافة قطاعات الصناعة على الشبكة "
+            u8"العالمية انترنيت ويونيكود، حيث ستتم، على الصعيدين "
+            u8"الدولي والمحلي على حد سواء مناقشة سبل استخدام "
+            u8"يونكود في النظم القائمة وفيما يخص التطبيقات "
+            u8"الحاسوبية، الخطوط، تصميم النصوص والحوسبة متعددة "
+            u8"اللغات."),
+        sParaText);
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
