1bea09c744bc1c58b05147b52e29ca80dc46688f
Tor Lillqvist <tml@iki.fi>
2018-08-28 15:08:45 +0300

    We do want to initialise m_MainThread on iOS, too
    (Original commit message below, but note that the talk about
    LibreOfficeLight is irrelevant here in this branch.)
    
    This doesn't make the LibreOfficeLight app work much better, though.
    But at least it doesn't hang its event loop completely, some 10 s
    timer keeps firing. (But nothing sane shows up in the app UI, even if
    some welcome.odt document apparently does get loaded. Clearly
    something is waiting for something else that never happens...)
    
    (cherry picked from commit 31703196388b455edda1c40fd2b3b536b315848f)
    
    Change-Id: Ieba22a5e1418d48a7dd6cacda526aca69cced4c3
    
    Signed-off-by: Michael Meeks <michael.meeks@collabora.com>

--- vcl/headless/svpinst.cxx
+++ vcl/headless/svpinst.cxx
@@ -69,8 +69,8 @@ SvpSalInstance::SvpSalInstance( SalYieldMutex *pMutex )
     m_aTimeout.tv_usec      = 0;
     m_nTimeoutMS            = 0;
 
-#ifndef IOS
     m_MainThread = osl::Thread::getCurrentIdentifier();
+#ifndef IOS
     CreateWakeupPipe(true);
 #endif
     if( s_pDefaultInstance == nullptr )
