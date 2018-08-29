0aa7c4c4564bdb8663b6ffaa27e33ad627263c23
Caolán McNamara <caolanm@redhat.com>
2018-08-29 17:05:28 +0100

    add SfxStyleDialogController
    Change-Id: I5b55b8adb46d894f87dcf68c97f6fd56059354a2

--- sw/source/uibase/app/docst.cxx
+++ sw/source/uibase/app/docst.cxx
@@ -320,7 +320,7 @@ void SwDocShell::ExecStyleSheet( SfxRequest& rReq )
                 sParent = static_cast<const SfxStringItem*>(pItem)->GetValue();
 
             if (sName.isEmpty() && m_xBasePool.get())
-                sName = SfxStyleDialog::GenerateUnusedName(*m_xBasePool);
+                sName = SfxStyleDialogController::GenerateUnusedName(*m_xBasePool);
 
             Edit(sName, sParent, nFamily, nMask, true, OString(), nullptr, &rReq, nSlot);
         }
