94fb125be60d0f27319770c1c157f3ebc643389a
Thorsten Behrens <Thorsten.Behrens@CIB.de>
2018-08-30 06:03:12 +0200

    sw: add unit test for the XHTML part of tdf#109202
    Change-Id: Ifb1b5b30e1c3586be75ff01f7bc0612a6b8b1f8d

---
+++ sw/CppunitTest_sw_xhtmlexport.mk
@@ -0,0 +1,59 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#*************************************************************************
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+#*************************************************************************
+
+$(eval $(call gb_CppunitTest_CppunitTest,sw_xhtmlexport))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,sw_xhtmlexport, \
+    sw/qa/extras/htmlexport/xhtmlexport \
+))
+
+$(eval $(call gb_CppunitTest_use_libraries,sw_xhtmlexport, \
+    comphelper \
+    cppu \
+	cppuhelper \
+	i18nlangtag \
+    sal \
+    sfx \
+    sw \
+    test \
+	tl \
+    unotest \
+    utl \
+    vcl \
+))
+
+$(eval $(call gb_CppunitTest_use_externals,sw_xhtmlexport,\
+	boost_headers \
+    libxml2 \
+))
+
+$(eval $(call gb_CppunitTest_set_include,sw_xhtmlexport,\
+    -I$(SRCDIR)/sw/inc \
+    -I$(SRCDIR)/sw/source/core/inc \
+    -I$(SRCDIR)/sw/source/uibase/inc \
+	-I$(SRCDIR)/sw/qa/extras/inc \
+    $$(INCLUDE) \
+))
+
+$(eval $(call gb_CppunitTest_use_api,sw_xhtmlexport,\
+	udkapi \
+	offapi \
+	oovbaapi \
+))
+
+$(eval $(call gb_CppunitTest_use_ure,sw_xhtmlexport))
+$(eval $(call gb_CppunitTest_use_vcl,sw_xhtmlexport))
+
+$(eval $(call gb_CppunitTest_use_rdb,sw_xhtmlexport,services))
+
+$(eval $(call gb_CppunitTest_use_configuration,sw_xhtmlexport))
+
+# vim: set noet sw=4 ts=4:
