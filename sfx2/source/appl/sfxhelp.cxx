eac4bd8e3c84ba4afe153e42cc82b263f62b5e11
Caolán McNamara <caolanm@redhat.com>
2018-08-30 13:45:12 +0100

    tdf#119579 skip floating windows as potential parent for missing help dialog
    Change-Id: Ibcee9a6aab0b04bf52f3e75a46f52d98864eee4c
    Reviewed-on: https://gerrit.libreoffice.org/59808
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>

--- sfx2/source/appl/sfxhelp.cxx
+++ sfx2/source/appl/sfxhelp.cxx
@@ -726,6 +726,21 @@ static bool impl_showOfflineHelp( const OUString& rURL )
     return false;
 }
 
+namespace
+{
+    // tdf#119579 skip floating windows as potential parent for missing help dialog
+    const vcl::Window* GetBestParent(const vcl::Window* pWindow)
+    {
+        while (pWindow)
+        {
+            if (pWindow->IsSystemWindow() && pWindow->GetType() != WindowType::FLOATINGWINDOW)
+                break;
+            pWindow = pWindow->GetParent();
+        }
+        return pWindow;
+    }
+}
+
 bool SfxHelp::Start_Impl(const OUString& rURL, const vcl::Window* pWindow, const OUString& rKeyword)
 {
     OUStringBuffer aHelpRootURL("vnd.sun.star.help://");
@@ -830,6 +845,8 @@ bool SfxHelp::Start_Impl(const OUString& rURL, const vcl::Window* pWindow, const
         {
             SvtHelpOptions aHelpOptions;
             bool bShowOfflineHelpPopUp = aHelpOptions.IsOfflineHelpPopUp();
+
+            pWindow = GetBestParent(pWindow);
 
             if(bShowOfflineHelpPopUp)
             {
