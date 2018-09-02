LibreOffice/core

commit 9ceeb4619ba762c47589023d99c43c774caab441

HSQLDB Migration: overwrite backup xml
Change-Id: I080568f2d278e8a7153188497e3987217d9d188f
Reviewed-on: https://gerrit.libreoffice.org/56464
Tested-by: Jenkins
Reviewed-by: Tamás Bunth <btomi96@gmail.com>

dbaccess/source/core/dataaccess/datasource.cxx
@@ -599,8 +599,12 @@ Reference< XConnection > ODatabaseSource::buildLowLevelConnection(const OUString
         {
             // back up content xml file if migration was successful
             Reference<XStorage> xRootStorage = m_pImpl->getOrCreateRootStorage();
+
+            constexpr char BACKUP_XML_NAME[] = "content_before_migration.xml";
+            if(xRootStorage->isStreamElement(BACKUP_XML_NAME))
+                xRootStorage->removeElement(BACKUP_XML_NAME);
             xRootStorage->copyElementTo("content.xml", xRootStorage,
-                    "content_before_migration.xml");
+                BACKUP_XML_NAME);
 
             m_pImpl->m_sConnectURL = "sdbc:embedded:firebird";
         }

commit 8a2213ff21d7483c3f3881abca9200544ce30745

tdf#118750: fix migration assistant
by catching throw if backup content file doesn't exist

Regression from 9ceeb4619ba762c47589023d99c43c774caab441

Change-Id: I68bb27763573217a9dff0e263cb8be88e15255ad

dbaccess/source/core/dataaccess/datasource.cxx
@@ -621,8 +621,15 @@ Reference< XConnection > ODatabaseSource::buildLowLevelConnection(const OUString
             Reference<XStorage> xRootStorage = m_pImpl->getOrCreateRootStorage();
 
             constexpr char BACKUP_XML_NAME[] = "content_before_migration.xml";
-            if(xRootStorage->isStreamElement(BACKUP_XML_NAME))
-                xRootStorage->removeElement(BACKUP_XML_NAME);
+            try
+            {
+                if(xRootStorage->isStreamElement(BACKUP_XML_NAME))
+                    xRootStorage->removeElement(BACKUP_XML_NAME);
+            }
+            catch (NoSuchElementException&)
+            {
+                SAL_INFO("dbaccess", "No file content_before_migration.xml found" );
+            }
             xRootStorage->copyElementTo("content.xml", xRootStorage,
                 BACKUP_XML_NAME);
 