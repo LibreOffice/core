LibreOffice/core

commit 4600b07c1d787f959618d9ecf54161e4ea4ffa61

tdf#102195: don't exclude everything between first and last subset
When subsets are non-contiguous, we need to include the parts between
subsets.

Change-Id: I28214dccc75e6a6af5c65397b2126049a65bf79f
Reviewed-on: https://gerrit.libreoffice.org/56571
Tested-by: Jenkins
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

slideshow/source/engine/shapes/drawshapesubsetting.cxx
@@ -164,50 +164,65 @@ namespace slideshow
             mbNodeTreeInitialized = true;
         }
 
-        void DrawShapeSubsetting::updateSubsetBounds( const SubsetEntry& rSubsetEntry )
+        void DrawShapeSubsetting::excludeSubset(sal_Int32 nExcludedStart, sal_Int32 nExcludedEnd)
         {
-            // TODO(F1): This removes too much from non-contiguous subsets
-            mnMinSubsetActionIndex = ::std::min(
-                mnMinSubsetActionIndex,
-                rSubsetEntry.mnStartActionIndex );
-            mnMaxSubsetActionIndex = ::std::max(
-                mnMaxSubsetActionIndex,
-                rSubsetEntry.mnEndActionIndex );
+            // If current subsets are empty, fill it with initial range
+            initCurrentSubsets();
+            if (maCurrentSubsets.empty())
+            {
+                // Non-subsetting mode (not a subset of anything; child subsets subtract content)
+                maCurrentSubsets.emplace_back(0, maActionClassVector.size());
+            }
+
+            slideshow::internal::VectorOfDocTreeNodes aNodesToAppend;
+            for (auto i = maCurrentSubsets.begin(); i != maCurrentSubsets.end();)
+            {
+                if (i->getStartIndex() < nExcludedStart)
+                {
+                    if (i->getEndIndex() > nExcludedStart)
+                    {
+                        // Some overlap -> append new node (if required), and correct this node's end
+                        if (i->getEndIndex() > nExcludedEnd)
+                        {
+                            aNodesToAppend.emplace_back(nExcludedEnd, i->getEndIndex());
+                        }
+                        i->setEndIndex(nExcludedStart);
+                    }
+                    ++i;
+                }
+                else if (i->getStartIndex() < nExcludedEnd)
+                {
+                    if (i->getEndIndex() > nExcludedEnd)
+                    {
+                        // Partial overlap; change the node's start
+                        i->setStartIndex(nExcludedEnd);
+                        ++i;
+                    }
+                    else
+                    {
+                        // Node is fully inside the removed range: erase it
+                        i = maCurrentSubsets.erase(i);
+                    }
+                }
+                else
+                {
+                    // Node is fully outside (after) excluded range
+                    ++i;
+                }
+            }
+
+            maCurrentSubsets.insert(maCurrentSubsets.end(), aNodesToAppend.begin(),
+                                    aNodesToAppend.end());
         }
 
         void DrawShapeSubsetting::updateSubsets()
         {
             maCurrentSubsets.clear();
+            initCurrentSubsets();
 
-            if( !maSubsetShapes.empty() )
+            for (const auto& rSubsetShape : maSubsetShapes)
             {
-                if( maSubset.isEmpty() )
-                {
-                    // non-subsetted node, with some child subsets
-                    // that subtract from it
-                    maCurrentSubsets.emplace_back( 0,
-                                                             mnMinSubsetActionIndex );
-                    maCurrentSubsets.emplace_back( mnMaxSubsetActionIndex,
-                                                             maActionClassVector.size() );
-                }
-                else
-                {
-                    // subsetted node, from which some further child
-                    // subsets subtract content
-                    maCurrentSubsets.emplace_back( maSubset.getStartIndex(),
-                                                             mnMinSubsetActionIndex );
-                    maCurrentSubsets.emplace_back( mnMaxSubsetActionIndex,
-                                                             maSubset.getEndIndex() );
-                }
-            }
-            else
-            {
-                // no further child subsets, simply add our subset (if any)
-                if( !maSubset.isEmpty() )
-                {
-                    // subsetted node, without any subset children
-                    maCurrentSubsets.push_back( maSubset );
-                }
+                excludeSubset(rSubsetShape.mnStartActionIndex, rSubsetShape.mnEndActionIndex);
             }
         }
 
@@ -220,8 +235,6 @@ namespace slideshow
             mpMtf(),
             maSubset(),
             maSubsetShapes(),
-            mnMinSubsetActionIndex( SAL_MAX_INT32 ),
-            mnMaxSubsetActionIndex(0),
             maCurrentSubsets(),
             mbNodeTreeInitialized( false )
         {
@@ -233,8 +246,6 @@ namespace slideshow
             mpMtf( rMtf ),
             maSubset( rShapeSubset ),
             maSubsetShapes(),
-            mnMinSubsetActionIndex( SAL_MAX_INT32 ),
-            mnMaxSubsetActionIndex(0),
             maCurrentSubsets(),
             mbNodeTreeInitialized( false )
         {
@@ -250,8 +261,6 @@ namespace slideshow
             mpMtf.reset();
             maSubset.reset();
             maSubsetShapes.clear();
-            mnMinSubsetActionIndex = SAL_MAX_INT32;
-            mnMaxSubsetActionIndex = 0;
             maCurrentSubsets.clear();
             mbNodeTreeInitialized = false;
         }
@@ -326,9 +335,7 @@ namespace slideshow
 
                 maSubsetShapes.insert( aEntry );
 
-                // update cached subset borders
-                updateSubsetBounds( aEntry );
-                updateSubsets();
+                excludeSubset(aEntry.mnStartActionIndex, aEntry.mnEndActionIndex);
             }
         }
 
@@ -380,18 +387,9 @@ namespace slideshow
             // part of this shape that is visible, i.e. not displayed
             // in subset shapes)
 
-
-            // init bounds
-            mnMinSubsetActionIndex = SAL_MAX_INT32;
-            mnMaxSubsetActionIndex = 0;
-
             // TODO(P2): This is quite expensive, when
             // after every subset effect end, we have to scan
             // the whole shape set
-
-            // determine new subset range
-            for( const auto& rSubsetShape : maSubsetShapes )
-                updateSubsetBounds( rSubsetShape );
 
             updateSubsets();
 

slideshow/source/engine/shapes/drawshapesubsetting.hxx
@@ -202,7 +202,7 @@ namespace slideshow
             typedef ::std::set< SubsetEntry >       ShapeSet;
 
             void ensureInitializedNodeTree() const;
-            void updateSubsetBounds( const SubsetEntry& rSubsetEntry );
+            void excludeSubset(sal_Int32 nExcludedStart, sal_Int32 nExcludedEnd);
             void updateSubsets();
             void initCurrentSubsets();
             void reset();
@@ -225,12 +225,6 @@ namespace slideshow
 
             /// the list of subset shapes spawned from this one.
             ShapeSet                            maSubsetShapes;
-
-            /// caches minimal subset index from maSubsetShapes
-            sal_Int32                           mnMinSubsetActionIndex;
-
-            /// caches maximal subset index from maSubsetShapes
-            sal_Int32                           mnMaxSubsetActionIndex;
 
             /** Current number of subsets to render (calculated from
                 maSubset and mnMin/MaxSubsetActionIndex).

slideshow/source/inc/doctreenode.hxx
@@ -90,6 +90,7 @@ namespace slideshow
             bool                isEmpty() const { return mnStartIndex == mnEndIndex; }
 
             sal_Int32           getStartIndex() const { return mnStartIndex; }
+            void                setStartIndex( sal_Int32 nIndex ) { mnStartIndex = nIndex; }
             sal_Int32           getEndIndex() const { return mnEndIndex; }
             void                setEndIndex( sal_Int32 nIndex ) { mnEndIndex = nIndex; }
 

commit 7a7b5697fa095fb1642fb7a83b36e166594ba038

tdf#119146: fix populating maCurrentSubsets
Regression from commit 4600b07c1d787f959618d9ecf54161e4ea4ffa61

Before the mentioned commit, creating a subset X, and then adding
sub-subsets until all the subset is excluded, had left the subset X
with two *empty* nodes in maCurrentSubsets, 1st of them having both
mnStartIndex and mnEndIndex equal to maSubset.mnStartIndex, and 2nd
having both mnStartIndex and mnEndIndex equal to maSubset.mnEndIndex.

The commit above changed that to adding maSubset to maCurrentSubsets
in the beginning of each exclusion, which finally resulted in a messy
maCurrentSubsets with multiple duplicating nodes effectively covering
all maSubset. That resulted in the item as a whole handled *besides*
the sub-subsets.

This commit (1) avoids adding extra maSubset to maCurrentSubsets, and
(2) adds the two empty nodes in case all the subset has been excluded
by its sub-subsets.

Change-Id: I7fc5f2d0b6ecf4f9247351da3a2746032150557b

slideshow/source/engine/shapes/drawshapesubsetting.cxx
@@ -214,6 +214,25 @@ namespace slideshow
 
             maCurrentSubsets.insert(maCurrentSubsets.end(), aNodesToAppend.begin(),
                                     aNodesToAppend.end());
+            // Excluding subsets must not leave an absolutely empty maCurrentSubsets, because it
+            // would mean "non-subsetting" mode unconditionally, with whole object added to subsets.
+            // So to indicate a subset with all parts excluded, add two empty subsets (starting and
+            // ending).
+            if (maCurrentSubsets.empty())
+            {
+                if (maSubset.isEmpty())
+                {
+                    maCurrentSubsets.emplace_back(0, 0);
+                    maCurrentSubsets.emplace_back(maActionClassVector.size(),
+                                                  maActionClassVector.size());
+                }
+                else
+                {
+                    maCurrentSubsets.emplace_back(maSubset.getStartIndex(),
+                                                  maSubset.getStartIndex());
+                    maCurrentSubsets.emplace_back(maSubset.getEndIndex(), maSubset.getEndIndex());
+                }
+            }
         }
 
         void DrawShapeSubsetting::updateSubsets()
@@ -276,10 +295,10 @@ namespace slideshow
 
         void DrawShapeSubsetting::initCurrentSubsets()
         {
-            // only add subset to vector, if it's not empty - that's
+            // only add subset to vector, if vector is empty, and subset is not empty - that's
             // because the vector's content is later literally used
             // for e.g. painting.
-            if( !maSubset.isEmpty() )
+            if (maCurrentSubsets.empty() && !maSubset.isEmpty())
                 maCurrentSubsets.push_back( maSubset );
         }
 