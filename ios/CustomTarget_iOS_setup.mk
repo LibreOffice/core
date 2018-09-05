59b9fc4776940863d905de14e25d74421f7de603
Tor Lillqvist <tml@iki.fi>
2018-09-05 13:53:06 +0300

    Put the lib names one per line to match what ld's -filelist expects
    Change-Id: I7454c10a1547db796554f45f2d630af81a916c55

--- ios/CustomTarget_iOS_setup.mk
+++ ios/CustomTarget_iOS_setup.mk
@@ -104,7 +104,8 @@ $(IOSGEN)/native-code.h: $(BUILDDIR)/config_host.mk \
 	&& echo "buildid=$(BUILDID)" \
 	    ) > $(IOSRES)/program/versionrc
 
-	$(SRCDIR)/bin/lo-all-static-libs >$(WORKDIR)/ios-all-static-libs.list
+	$(SRCDIR)/bin/lo-all-static-libs | sed -e 's/ /\
+/g' >$(WORKDIR)/ios-all-static-libs.list
 
 #- clean ios  -----------------------------------------------------------------
 $(call gb_CustomTarget_get_clean_target,ios/iOS_setup):
