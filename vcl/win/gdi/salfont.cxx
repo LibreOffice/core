7cb3e475f2fb0162c7290414509de4fb8fe57e30
Jan-Marek Glogowski <glogow@fbihome.de>
2018-09-06 14:04:25 +0200

    WIN add SalGraphics* to WinFontInstance
    HFONT lookup in ImplDoSetFont depends on the mbVirDev of the
    WinSalGraphics. Since we need too look up HFONTs for SalLayout
    without changing the corresponding SalGraphics, add a pointer
    to the WinFontInstance.
    
    Change-Id: Idb6573ce7267f0019c2183be47621d0eaef8e57b
    Reviewed-on: https://gerrit.libreoffice.org/60093
    Tested-by: Jenkins
    Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>

--- vcl/win/gdi/salfont.cxx
+++ vcl/win/gdi/salfont.cxx
@@ -934,13 +934,10 @@ void WinSalGraphics::SetFont(LogicalFontInstance* pFont, int nFallbackLevel)
     HFONT hNewFont = pFontInstance->GetHFONT();
     if (!hNewFont)
     {
-        float fFontScale = 1.0;
-        hNewFont = ImplDoSetFont(pFont->GetFontSelectPattern(), pFont->GetFontFace(), fFontScale, hOldFont);
-        mpWinFontEntry[ nFallbackLevel ]->SetHFONT(hNewFont);
-        mpWinFontEntry[ nFallbackLevel ]->SetScale(fFontScale);
+        pFontInstance->SetGraphics(this);
+        hNewFont = pFontInstance->GetHFONT();
     }
-    else
-        hOldFont = ::SelectFont( getHDC(), hNewFont );
+    hOldFont = ::SelectFont(getHDC(), hNewFont);
 
     // keep default font
     if( !mhDefFont )
@@ -1588,7 +1585,7 @@ ScopedFont::ScopedFont(WinSalGraphics & rData): m_rData(rData), m_hOrigFont(null
     if (m_rData.mpWinFontEntry[0])
     {
         m_hOrigFont = m_rData.mpWinFontEntry[0]->GetHFONT();
-        m_rData.mpWinFontEntry[0]->UnsetHFONT();
+        m_rData.mpWinFontEntry[0]->SetHFONT(nullptr);
     }
 }
 
