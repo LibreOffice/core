c0a1b9a5c6b1067f43c1b09e27a88e6d01e84d98
Rahul Gurung <gurungrahul2@gmail.com>
2018-09-06 15:07:01 +0530

    tdf#45904 Move _XTableChart.java to C++
    Change-Id: I35270b57a96caea54b04dc5a66081f17a6fd6626

---
+++ test/source/table/xtablechart.cxx
@@ -0,0 +1,51 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+* This file is part of the LibreOffice project.
+*
+* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+#include <test/cppunitasserthelper.hxx>
+#include <test/table/xtablechart.hxx>
+
+#include <com/sun/star/table/CellRangeAddress.hpp>
+#include <com/sun/star/table/XTableChart.hpp>
+
+#include <com/sun/star/uno/Reference.hxx>
+#include <cppunit/extensions/HelperMacros.h>
+
+using namespace com::sun::star;
+using namespace com::sun::star::uno;
+
+namespace apitest
+{
+void XTableChart::testSetGetHasColumnHeaders()
+{
+    uno::Reference<table::XTableChart> xTableChart(init(), UNO_QUERY_THROW);
+    xTableChart->setHasColumnHeaders(true);
+    CPPUNIT_ASSERT_MESSAGE("Successfully able to Set and Get Column Headers",
+                           xCell->getHasColumnHeaders());
+}
+
+void XTableChart::testSetGetHasRowHeaders()
+{
+    uno::Reference<table::XTableChart> xTableChart(init(), UNO_QUERY_THROW);
+    xTableChart->setHasRowHeaders(true);
+    CPPUNIT_ASSERT_MESSAGE("Successfully able to Set and Get Row Headers",
+                           xCell->getHasRowHeaders());
+}
+
+void xTableChart::testSetGetRanges()
+{
+    uno::Reference<table::XTableChart> xTableChart(init(), UNO_QUERY_THROW);
+    table::CellRangeAddress aRanges = xTableChart->getRanges();
+    CPPUNIT_ASSERT_MESSAGE("Successfully able to Get Ranges", aRanges)
+    table::CellRangeAddress aRanges2 = xTableChart->getRanges();
+    aRanges2[0].EndRow = 1;
+    CPPUNIT_ASSERT_EQUAL_MESSAGE("Successfully able to Set Values", aRanges2, aRanges);
+}
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
