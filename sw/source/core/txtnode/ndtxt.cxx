3230baca58db9c78109a3c208afd7d19fe912f16
Michael Stahl <Michael.Stahl@cib.de>
2018-09-06 12:58:18 +0200

    sw_redlinehide_2: update flys and footnotes on redline ops
    Re-use some code that is already used elsewhere; for the
    UpdateFramesForAddDeleteRedline(), the same code as for switching
    the layout to Hide mode should work, for
    UpdateFramesForRemoveDeleteRedline() use the code that is used for
    SwTextNode::SplitContentNode().
    
    Change-Id: I97601bfb63478cc999cf7017da0225b2dc62ad37

--- sw/source/core/txtnode/ndtxt.cxx
+++ sw/source/core/txtnode/ndtxt.cxx
@@ -365,13 +365,18 @@ static void lcl_ChangeFootnoteRef( SwTextNode &rNode )
     }
 }
 
-namespace {
+namespace sw {
 
 // check if there are flys on the existing frames (now on "pNode")
 // that need to be moved to the new frames of "this"
 void MoveMergedFlysAndFootnotes(std::vector<SwTextFrame*> const& rFrames,
-        SwTextNode const& rFirstNode, SwTextNode const& rSecondNode)
+        SwTextNode const& rFirstNode, SwTextNode & rSecondNode,
+        bool isSplitNode)
 {
+    if (!isSplitNode)
+    {
+        lcl_ChangeFootnoteRef(rSecondNode);
+    }
     for (sal_uLong nIndex = rSecondNode.GetIndex() + 1; ; ++nIndex)
     {
         SwNode *const pTmp(rSecondNode.GetNodes()[nIndex]);
@@ -600,7 +605,7 @@ SwTextNode *SwTextNode::SplitContentNode(const SwPosition & rPos,
         }
         if (eOldMergeFlag != SwNode::Merge::None)
         {
-            MoveMergedFlysAndFootnotes(frames, *pNode, *this);
+            MoveMergedFlysAndFootnotes(frames, *pNode, *this, true);
         }
     }
     else
@@ -731,7 +736,7 @@ SwTextNode *SwTextNode::SplitContentNode(const SwPosition & rPos,
 
         if (bRecreateThis)
         {
-            MoveMergedFlysAndFootnotes(frames, *pNode, *this);
+            MoveMergedFlysAndFootnotes(frames, *pNode, *this, true);
         }
     }
 
