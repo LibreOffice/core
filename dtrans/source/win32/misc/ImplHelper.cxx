LibreOffice/core

commit 0dd2e602e5c1b46e82abc6051677aeaab1d265b8

Drop check for Windows versions we don't support
Since we dropped support of Vista and below in master toward 6.0,
those checks are needless. Removing the code that only worked in
older versions, and streamlining the resulting code.

Also, use kernel32.dll version for Windows version, instead of
deprecated GetVersionEx, and inconvenient VersionHelpers. Since both
GetVersion(Ex) and VersionHelpers (based on VerifyVersionInfo) are
subject to manifest-based behavior since Windows 8.1, this move will
hopefully result in more reliable OS version detection.

Change-Id: I3edd8fc1843e64b6a65bd3a126be6a085511f13c
Reviewed-on: https://gerrit.libreoffice.org/42905
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
Tested-by: Mike Kaganski <mike.kaganski@collabora.com>

vcl/opengl/win/WinDeviceInfo.cxx
@@ -20,6 +20,7 @@
 #include <setupapi.h>
 #include <algorithm>
 #include <cstdint>
+#include <memory>
 
 #include <osl/file.hxx>
 #include <rtl/bootstrap.hxx>
@@ -149,14 +150,14 @@ uint32_t ParseIDFromDeviceID(const OUString &key, const char *prefix, int length
 // OS version in 16.16 major/minor form
 // based on http://msdn.microsoft.com/en-us/library/ms724834(VS.85).aspx
 enum {
-    kWindowsUnknown = 0,
-    kWindowsXP =         0x00050001,
+    kWindowsUnknown    = 0,
+    kWindowsXP         = 0x00050001,
     kWindowsServer2003 = 0x00050002,
-    kWindowsVista =      0x00060000,
-    kWindows7 =          0x00060001,
-    kWindows8 =          0x00060002,
-    kWindows8_1 =        0x00060003,
-    kWindows10 =         0x000A0000  // Major 10 Minor 0
+    kWindowsVista      = 0x00060000,
+    kWindows7          = 0x00060001,
+    kWindows8          = 0x00060002,
+    kWindows8_1        = 0x00060003,
+    kWindows10         = 0x000A0000  // Major 10 Minor 0
 };
 
 
@@ -189,23 +190,38 @@ int32_t WindowsOSVersion()
 {
     static int32_t winVersion = kWindowsUnknown;
 
-    OSVERSIONINFO vinfo;
-
     if (winVersion == kWindowsUnknown)
     {
-        vinfo.dwOSVersionInfoSize = sizeof (vinfo);
-#pragma warning(push)
-#pragma warning(disable:4996)
-        SAL_WNODEPRECATED_DECLARATIONS_PUSH
-        if (!GetVersionEx(&vinfo))
-        SAL_WNODEPRECATED_DECLARATIONS_POP
+        // GetVersion(Ex) and VersionHelpers (based on VerifyVersionInfo) API are
+        // subject to manifest-based behavior since Windows 8.1, so give wrong results.
+        // Another approach would be to use NetWkstaGetInfo, but that has some small
+        // reported delays (some milliseconds), and might get slower in domains with
+        // poor network connections.
+        // So go with a solution described at https://msdn.microsoft.com/en-us/library/ms724429
+        HINSTANCE hLibrary = LoadLibraryW(L"kernel32.dll");
+        if (hLibrary != nullptr)
         {
-#pragma warning(pop)
-            winVersion = kWindowsUnknown;
-        }
-        else
-        {
-            winVersion = int32_t(vinfo.dwMajorVersion << 16) + vinfo.dwMinorVersion;
+            wchar_t szPath[MAX_PATH];
+            DWORD dwCount = GetModuleFileNameW(hLibrary, szPath, SAL_N_ELEMENTS(szPath));
+            FreeLibrary(hLibrary);
+            if (dwCount != 0 && dwCount < SAL_N_ELEMENTS(szPath))
+            {
+                dwCount = GetFileVersionInfoSizeW(szPath, NULL);
+                if (dwCount != 0)
+                {
+                    std::unique_ptr<char> ver(new char[dwCount]);
+                    if (GetFileVersionInfoW(szPath, 0, dwCount, ver.get()) != FALSE)
+                    {
+                        void* pBlock = nullptr;
+                        UINT dwBlockSz = 0;
+                        if (VerQueryValueW(ver.get(), L"\\", &pBlock, &dwBlockSz) != FALSE && dwBlockSz >= sizeof(VS_FIXEDFILEINFO))
+                        {
+                            VS_FIXEDFILEINFO *vinfo = reinterpret_cast<VS_FIXEDFILEINFO *>(pBlock);
+                            winVersion = int32_t(vinfo->dwProductVersionMS);
+                        }
+                    }
+                }
+            }
         }
     }
 

avmedia/source/win/player.cxx
@@ -26,9 +26,6 @@
 #include <control.h>
 #include <uuids.h>
 #include <evcode.h>
-#ifdef _WIN32_WINNT_WINBLUE
-#include <VersionHelpers.h>
-#endif
 #if defined _MSC_VER
 #pragma warning(pop)
 #endif
@@ -66,22 +63,6 @@ LRESULT CALLBACK MediaPlayerWndProc_2( HWND hWnd,UINT nMsg, WPARAM nPar1, LPARAM
         bProcessed = false;
 
     return( bProcessed ? 0 : DefWindowProc( hWnd, nMsg, nPar1, nPar2 ) );
-}
-
-
-bool isWindowsVistaOrHigher()
-{
-// the Win32 SDK 8.1 deprecates GetVersionEx()
-#ifdef _WIN32_WINNT_WINBLUE
-    return IsWindowsVistaOrGreater();
-#else
-    // POST: return true if we are at least on Windows Vista
-    OSVERSIONINFO osvi;
-    ZeroMemory(&osvi, sizeof(osvi));
-    osvi.dwOSVersionInfoSize = sizeof(osvi);
-    GetVersionEx(&osvi);
-    return  osvi.dwMajorVersion >= 6;
-#endif
 }
 
 
@@ -166,13 +147,6 @@ bool Player::create( const OUString& rURL )
         // Don't use the overlay mixer on Windows Vista
         // It disables the desktop composition as soon as RenderFile is called
         // also causes some other problems: video rendering is not reliable
-        if( !isWindowsVistaOrHigher() && SUCCEEDED( CoCreateInstance( CLSID_OverlayMixer, nullptr, CLSCTX_INPROC_SERVER, IID_IBaseFilter, reinterpret_cast<void**>(&mpOMF) ) ) )
-        {
-            mpGB->AddFilter( mpOMF, L"com_sun_star_media_OverlayMixerFilter" );
-
-            if( !SUCCEEDED( mpOMF->QueryInterface( IID_IDDrawExclModeVideo, reinterpret_cast<void**>(&mpEV) ) ) )
-                mpEV = nullptr;
-        }
 
         if( SUCCEEDED( hR = mpGB->RenderFile( reinterpret_cast<LPCWSTR>(rURL.getStr()), nullptr ) ) &&
             SUCCEEDED( hR = mpGB->QueryInterface( IID_IMediaControl, reinterpret_cast<void**>(&mpMC) ) ) &&

fpicker/source/win32/filepicker/FPentry.cxx
@@ -44,9 +44,6 @@ static Reference< XInterface > SAL_CALL createInstance(
 {
     Reference< XInterface > xDlg;
 
-    if (!IsWindowsVistaOrNewer())
-        std::abort(); // not supported
-
     xDlg.set(static_cast<XFilePicker2*>(
             new ::fpicker::win32::vista::VistaFilePicker(rServiceManager)));
 

fpicker/source/win32/misc/WinImplHelper.cxx
@@ -24,10 +24,6 @@
 #include <rtl/ustrbuf.hxx>
 #include <com/sun/star/uno/Sequence.hxx>
 
-#ifdef _WIN32_WINNT_WINBLUE
-#include <VersionHelpers.h>
-#endif
-
 using ::com::sun::star::lang::IllegalArgumentException;
 using ::com::sun::star::uno::Reference;
 using ::com::sun::star::uno::XInterface;
@@ -50,32 +46,6 @@ const sal_Unicode   AMPERSAND_SIGN = L'&';
 // Windows 95       VER_PLATFORM_WIN32_WINDOWS  4       0
 // Windows 98       VER_PLATFORM_WIN32_WINDOWS  4       10
 // Windows ME       VER_PLATFORM_WIN32_WINDOWS  4       90
-
-
-// determine if we are running under Vista or newer OS
-
-bool SAL_CALL IsWindowsVistaOrNewer()
-{
-// the Win32 SDK 8.1 deprecates GetVersionEx()
-#ifdef _WIN32_WINNT_WINBLUE
-    return IsWindowsVistaOrGreater();
-#else
-    OSVERSIONINFO osvi;
-    osvi.dwOSVersionInfoSize = sizeof(osvi);
-
-    if(!GetVersionEx(&osvi))
-        return false;
-
-    bool bRet = (VER_PLATFORM_WIN32_NT == osvi.dwPlatformId) &&
-                (osvi.dwMajorVersion >= 6);
-
-    bRet = bRet &&
-        (osvi.dwMinorVersion >=
-         sal::static_int_cast< unsigned int >(0));
-
-    return bRet;
-#endif
-}
 
 
 void Replace( const OUString& aLabel, sal_Unicode OldChar, sal_Unicode NewChar, OUStringBuffer& aBuffer )

fpicker/source/win32/misc/WinImplHelper.hxx
@@ -37,8 +37,6 @@
 #include <com/sun/star/uno/Any.hxx>
 #include <com/sun/star/lang/IllegalArgumentException.hpp>
 
-bool SAL_CALL IsWindowsVistaOrNewer();
-
 // converts a soffice label to a windows label
 // the following rules for character replacements
 // will be done:

setup_native/source/win32/customactions/shellextensions/startmenuicon.cxx
@@ -29,37 +29,9 @@ extern "C" UINT __stdcall InstallStartmenuFolderIcon( MSIHANDLE handle )
 {
     std::wstring sOfficeMenuFolder = GetMsiPropertyW( handle, L"OfficeMenuFolder" );
     std::wstring sDesktopFile = sOfficeMenuFolder + L"Desktop.ini";
-    std::wstring sIconFile = GetMsiPropertyW( handle, L"INSTALLLOCATION" ) + L"program\\soffice.exe";
 
-// the Win32 SDK 8.1 deprecates GetVersionEx()
-#ifdef _WIN32_WINNT_WINBLUE
-    bool const bIsVistaOrLater = IsWindowsVistaOrGreater();
-#else
-    OSVERSIONINFOW osverinfo;
-    osverinfo.dwOSVersionInfoSize = sizeof(osverinfo);
-    GetVersionExW( &osverinfo );
-    bool const bIsVistaOrLater = (osverinfo.dwMajorVersion >= 6);
-#endif
-
-    if (!bIsVistaOrLater)
-    {
-        WritePrivateProfileStringW(
-            L".ShellClassInfo",
-            L"IconFile",
-            sIconFile.c_str(),
-            sDesktopFile.c_str() );
-
-        WritePrivateProfileStringW(
-            L".ShellClassInfo",
-            L"IconIndex",
-            L"0",
-            sDesktopFile.c_str() );
-    }
-    // else
-    // {
-    //     // at the moment there exists no Vista Icon, so we use the default folder icon.
-    //     // add the icon into desktop/util/verinfo.rc
-    // }
+    // at the moment there exists no Vista Icon, so we use the default folder icon.
+    // add the icon into desktop/util/verinfo.rc
 
     // The value '0' is to avoid a message like "You Are Deleting a System Folder" warning when deleting or moving the folder.
     WritePrivateProfileStringW(

shell/inc/utilities.hxx
@@ -64,12 +64,6 @@ std::wstring UTF8ToWString(const std::string& String);
 std::wstring GetResString(int ResId);
 
 
-/** Returns whether we are running
-    on Windows XP or not
-*/
-bool is_windows_xp_or_above();
-
-
 /** helper function to judge if the string is only has spaces.
     @returns
         <TRUE>if the provided string contains only but at least one space

shell/source/win32/shlxthandler/propsheets/listviewbuilder.cxx
@@ -50,10 +50,7 @@
 list_view_builder_ptr create_list_view_builder(
     HWND hwnd_lv, const std::wstring& col1, const std::wstring& col2)
 {
-    if (is_windows_xp_or_above())
-        return list_view_builder_ptr(new winxp_list_view_builder(hwnd_lv, col1, col2));
-    else
-        return list_view_builder_ptr(new list_view_builder(hwnd_lv, col1, col2));
+    return list_view_builder_ptr(new list_view_builder(hwnd_lv, col1, col2));
 }
 
 
@@ -64,7 +61,9 @@ list_view_builder::list_view_builder(
     row_index_(-1),
     hwnd_list_view_(hwnd_list_view),
     column1_title_(column1_title),
-    column2_title_(column2_title)
+    column2_title_(column2_title),
+    group_count_(-1),
+    row_count_(0)
 {
 }
 
@@ -121,73 +120,11 @@ void list_view_builder::setup_list_view()
     header = GetResString(IDS_PROPERTY_VALUE);
     lvc.pszText = const_cast<wchar_t*>(header.c_str());
     ListView_InsertColumnW(hwnd_list_view_, 1, &lvc);
+    ListView_EnableGroupView(hwnd_list_view_, TRUE);
 }
 
 
-void list_view_builder::insert_group(const std::wstring& /*title*/)
-{
-    insert_item(L"", L"", false);
-}
-
-
-void list_view_builder::insert_item(const std::wstring& title, const std::wstring& value, bool is_editable)
-{
-    LVITEMW lvi;
-
-    lvi.iItem      = ++row_index_;
-    lvi.iSubItem   = 0;
-    lvi.mask       = LVIF_TEXT;
-    lvi.state      = 0;
-    lvi.cchTextMax = static_cast<int>(title.size() + 1);
-    lvi.stateMask  = 0;
-    lvi.pszText    = const_cast<wchar_t*>(title.c_str());
-
-    if (title.length() > 0)
-    {
-        lvi.mask |= LVIF_IMAGE;
-
-        if (is_editable)
-            lvi.iImage = 4;
-        else
-            lvi.iImage = 3;
-    }
-
-    ListView_InsertItemW(hwnd_list_view_, &lvi);
-
-    lvi.mask     = LVIF_TEXT;
-    lvi.iSubItem = 1;
-    lvi.pszText  = const_cast<wchar_t*>(value.c_str());
-
-    ListView_SetItemW(hwnd_list_view_, &lvi);
-}
-
-
-HWND list_view_builder::get_list_view() const
-{
-    return hwnd_list_view_;
-}
-
-
-winxp_list_view_builder::winxp_list_view_builder(
-    HWND hwnd_list_view,
-    const std::wstring& column1_title,
-    const std::wstring& column2_title) :
-    list_view_builder(hwnd_list_view, column1_title, column2_title),
-    group_count_(-1),
-    row_count_(0)
-{
-}
-
-
-void winxp_list_view_builder::setup_list_view()
-{
-    list_view_builder::setup_list_view();
-
-    ListView_EnableGroupView(get_list_view(), TRUE);
-}
-
-
-void winxp_list_view_builder::insert_group(const std::wstring& name)
+void list_view_builder::insert_group(const std::wstring& name)
 {
     LVGROUP lvg;
 
@@ -205,8 +142,7 @@ void winxp_list_view_builder::insert_group(const std::wstring& name)
 }
 
 
-void winxp_list_view_builder::insert_item(
-    const std::wstring& title, const std::wstring& value, bool is_editable)
+void list_view_builder::insert_item(const std::wstring& title, const std::wstring& value, bool is_editable)
 {
     LVITEMW lvi;
 
@@ -237,6 +173,12 @@ void winxp_list_view_builder::insert_item(
     ListView_SetItemW(get_list_view(), &lvi);
 
     row_count_++;
+}
+
+
+HWND list_view_builder::get_list_view() const
+{
+    return hwnd_list_view_;
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

commit 684c1770c58a56651b58a3bebc718f15dd9d79a0

WIN fix std::unique_ptr array allocs
regression from commit 0dd2e602e5c1 ("Drop check for Windows
versions we don't support")

Change-Id: I8b70c7da7db60af52bfac12314a21602ede8bfc0

dtrans/source/win32/misc/ImplHelper.cxx
@@ -95,7 +95,7 @@ OUString getWinCPFromLocaleId( LCID lcid, LCTYPE lctype )
 
     if ( nResult )
     {
-        std::unique_ptr<wchar_t> buff( new wchar_t[nResult] );
+        std::unique_ptr<wchar_t[]> buff( new wchar_t[nResult] );
         // Now get the actual data
         nResult = GetLocaleInfoW( lcid, lctype, buff.get(), nResult );
 

vcl/opengl/win/WinDeviceInfo.cxx
@@ -211,7 +211,7 @@ int32_t WindowsOSVersion()
                 dwCount = GetFileVersionInfoSizeW(szPath, nullptr);
                 if (dwCount != 0)
                 {
-                    std::unique_ptr<char> ver(new char[dwCount]);
+                    std::unique_ptr<char[]> ver(new char[dwCount]);
                     if (GetFileVersionInfoW(szPath, 0, dwCount, ver.get()) != FALSE)
                     {
                         void* pBlock = nullptr;