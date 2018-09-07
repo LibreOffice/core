ca56724ed07a182b19a79b6d03cd76a0bb4856af
Mike Kaganski <mike.kaganski@collabora.com>
2018-09-07 17:11:48 +0300

    tdf#119238: keep replaced file's identity when renaming docfile
    Regression from 2157a3536f97ff5ae7c82611a801fef7e3708983
    
      sfx2 store: try rename before copying
    
      Rename is cheaper then copying the content over manually, so try that
      first.
    
    On Windows, we need to keep the file's creation time (which is kept in FS).
    WinAPI has ReplaceFileW specifically for this, and it keeps ACLs of the
    original file, and otherwise makes the changed file not a separate entry,
    but updated old file from system's PoV.
    
    Eventually, we could try to restructure creating backup copies (e.g., for
    documents when configured so) to take advantage of this function being able
    to do that.
    
    Change-Id: I6001a2a3af5e10bc010f5ef129f4bb6f83ee1581

--- include/osl/file.hxx
+++ include/osl/file.hxx
@@ -1298,6 +1298,39 @@ public:
         return static_cast< RC >( osl_moveFile( ustrSourceFileURL.pData, ustrDestFileURL.pData ) );
     }
 
+    /** Move a file to a new destination or renames it, taking old file's identity (if exists).
+
+        Moves or renames a file, replacing an existing file if exist. If the old file existed,
+        moved file creation time (on FSes which keep files' creation time) is set to old one's
+        (to keep the old file's identity) - currently this is only relevant to Windows, on other
+        platforms, this is equivalent to move().
+
+        @param[in] ustrSourceFileURL
+        Full qualified URL of the source file.
+
+        @param[in] ustrDestFileURL
+        Full qualified URL of the destination file.
+
+        @retval E_None on success
+        @retval E_INVAL the format of the parameters was not valid
+        @retval E_NOMEM not enough memory for allocating structures
+        @retval E_ACCES permission denied
+        @retval E_PERM operation not permitted
+        @retval E_NAMETOOLONG file name too long
+        @retval E_NOENT no such file
+        @retval E_ROFS read-only file system
+        @retval E_BUSY device or resource busy
+
+        @see copy()
+
+    @since LibreOffice 6.2
+*/
+    static RC replace(const ::rtl::OUString& ustrSourceFileURL,
+                      const ::rtl::OUString& ustrDestFileURL)
+    {
+        return static_cast<RC>(osl_replaceFile(ustrSourceFileURL.pData, ustrDestFileURL.pData));
+    }
+
     /** Remove a regular file.
 
         @param[in] ustrFileURL
