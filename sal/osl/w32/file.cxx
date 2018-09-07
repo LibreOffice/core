dbe4c6647784fb84982faf353f07f34aaa41b4bd
Mike Kaganski <mike.kaganski@collabora.com>
2018-09-07 17:11:48 +0300

    tdf#119238: keep replaced file's identity when renaming docfile
    Regression from 2157a3536f97ff5ae7c82611a801fef7e3708983
    
      sfx2 store: try rename before copying
    
      Rename is cheaper then copying the content over manually, so try that
      first.
    
    On Windows, we need to keep the file's dentity, including metadata (e.g.,
    creation time, which is kept in FS). WinAPI has ReplaceFileW specifically
    for this, and it keeps ACLs of the original file, and otherwise makes the
    changed file not a separate entry, but updated old file from system's PoV.
    
    Eventually, we could try to restructure creating backup copies (e.g., for
    documents when configured so) to take advantage of this function being able
    to do that.
    
    Change-Id: I6001a2a3af5e10bc010f5ef129f4bb6f83ee1581

--- sal/osl/w32/file.cxx
+++ sal/osl/w32/file.cxx
@@ -1098,4 +1098,38 @@ oslFileError SAL_CALL osl_moveFile(rtl_uString* strPath, rtl_uString *strDestPat
     return error;
 }
 
+oslFileError SAL_CALL osl_replaceFile(rtl_uString* strPath, rtl_uString* strDestPath)
+{
+    rtl_uString *strSysPath = nullptr, *strSysDestPath = nullptr;
+    oslFileError    error = osl_getSystemPathFromFileURL_(strPath, &strSysPath, false);
+
+    if (error == osl_File_E_None)
+        error = osl_getSystemPathFromFileURL_(strDestPath, &strSysDestPath, false);
+
+    if (error == osl_File_E_None)
+    {
+        LPCWSTR src = o3tl::toW(rtl_uString_getStr(strSysPath));
+        LPCWSTR dst = o3tl::toW(rtl_uString_getStr(strSysDestPath));
+
+        if (!ReplaceFileW(dst, src, nullptr,
+                          REPLACEFILE_WRITE_THROUGH | REPLACEFILE_IGNORE_MERGE_ERRORS
+                              | REPLACEFILE_IGNORE_ACL_ERRORS,
+                          nullptr, nullptr))
+        {
+            DWORD dwError = GetLastError();
+            if (dwError == ERROR_FILE_NOT_FOUND) // no strDestPath file?
+                error = osl_moveFile(strPath, strDestPath);
+            else
+                error = oslTranslateFileError(dwError);
+        }
+    }
+
+    if (strSysPath)
+        rtl_uString_release(strSysPath);
+    if (strSysDestPath)
+        rtl_uString_release(strSysDestPath);
+
+    return error;
+}
+
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
