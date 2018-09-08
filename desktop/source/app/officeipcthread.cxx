1fb3a433264e34fc48e351d21d93a7bb4f431113
Noel Grandin <noel.grandin@collabora.co.uk>
2018-09-08 09:26:12 +0200

    clang-tidy bugprone-unused-raii in RequestHandler
    The message from clang-tidy is:
        warning: object destroyed immediately after creation; did you mean
    to name the object?
    
    The guard in
    RequestHandler::ExecuteCmdLineRequests
    comes from
    commit cf333a878ceed18d0343520a2c65be69fc433b1f
    Date:   Sun Jan 21 22:10:09 2018 +0300
    tdf#38915: set cProcessed condition on any process outcome
    and I'm sure it's intention was to set the flag on exit from the
    function, not immediately.
    
    Reviewed-on: https://gerrit.libreoffice.org/60183
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
    Tested-by: Jenkins
    (cherry picked from commit 867ee21fe4c0c9216aea57850efec4de3c3fc554)
    
    Change-Id: Ibf874a5774770df00b9db7f673554e7ffda55072
    Reviewed-on: https://gerrit.libreoffice.org/66168
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

--- desktop/source/app/officeipcthread.cxx
+++ desktop/source/app/officeipcthread.cxx
@@ -1324,7 +1324,7 @@ bool RequestHandler::ExecuteCmdLineRequests(
     osl::ClearableMutexGuard aGuard( GetMutex() );
 
     // ensure that Processed flag (if exists) is signaled in any outcome
-    ConditionSetGuard(aRequest.pcProcessed);
+    ConditionSetGuard aSetGuard(aRequest.pcProcessed);
 
     static std::vector<DispatchWatcher::DispatchRequest> aDispatchList;
 
