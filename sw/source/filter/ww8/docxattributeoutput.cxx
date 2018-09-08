3f891acb225145083a4ee58b11b3d77dd174817a
Justin Luth <justin_luth@sil.org>
2018-09-08 12:11:01 +0300

    tdf#82177 docx export: eliminate invalid tcPr insideV/H borders
    better fix than cecf71c18da5430c10daa8522d38d5144edefc14 since:
    
    17.4.25 insideV (Table Cell Inside Vertical Edges Border)
    This element specifies the border which shall be displayed
    on all interior vertical edges of the current group of table cells.
    [Note: Although individual table cells have no concept of an
    internal edge, which would render this property useless in
    most cases, it is used to determine the cell borders to apply to a specific
    group of cells as part of table conditional formatting in a table style,
    for example, the inside vertical edges on the set of cells in the header
    row. end note]
    
    So, I interpret this as insideV/H having meaning only within
    table styles, and not when directly applied to a cell. The only documents
    I've seen with insideV/H directly applied to a cell seem to have been
    created by LO - which dumps them everywhere, redundantly.
    
    Tablestyle cell groupings are handled elsewhere via grabbag dumps,
    so insideV/H borders can be eliminated from TableCellProperties.
    
    Change-Id: I128417f0a0b485c85ede463daacb8feabc457302

--- sw/source/filter/ww8/docxattributeoutput.cxx
+++ sw/source/filter/ww8/docxattributeoutput.cxx
@@ -3044,7 +3044,7 @@ static OutputBorderOptions lcl_getTableCellBorderOptions(bool bEcma)
     rOptions.tag = XML_tcBorders;
     rOptions.bUseStartEnd = !bEcma;
     rOptions.bWriteTag = true;
-    rOptions.bWriteInsideHV = true;
+    rOptions.bWriteInsideHV = false;
     rOptions.bWriteDistance = false;
 
     return rOptions;
@@ -3067,9 +3067,7 @@ static void impl_borders( FSHelperPtr const & pSerializer,
                           const SvxBoxItem& rBox,
                           const OutputBorderOptions& rOptions,
                           std::map<SvxBoxItemLine,
-                          css::table::BorderLine2> &rTableStyleConf,
-                          const bool bIsLastColumn = false,
-                          const bool bIsLastRow = false )
+                          css::table::BorderLine2> &rTableStyleConf )
 {
     static const SvxBoxItemLine aBorders[] =
     {
@@ -3161,14 +3159,14 @@ static void impl_borders( FSHelperPtr const & pSerializer,
                 bWriteInsideV = true;
         }
     }
-    if (bWriteInsideH && !bIsLastRow)
+    if (bWriteInsideH)
     {
         const table::BorderLine2 *aStyleProps = nullptr;
         if( rTableStyleConf.find( SvxBoxItemLine::BOTTOM ) != rTableStyleConf.end() )
             aStyleProps = &rTableStyleConf[ SvxBoxItemLine::BOTTOM ];
         impl_borderLine( pSerializer, XML_insideH, rBox.GetLine(SvxBoxItemLine::BOTTOM), 0, false, aStyleProps );
     }
-    if (bWriteInsideV && !bIsLastColumn)
+    if (bWriteInsideV)
     {
         const table::BorderLine2 *aStyleProps = nullptr;
         if( rTableStyleConf.find( SvxBoxItemLine::RIGHT ) != rTableStyleConf.end() )
@@ -3237,8 +3235,6 @@ void DocxAttributeOutput::TableCellProperties( ww8::WW8TableNodeInfoInner::Point
     const SwTableBox *pTableBox = pTableTextNodeInfoInner->getTableBox( );
 
     bool bEcma = GetExport().GetFilter().getVersion( ) == oox::core::ECMA_DIALECT;
-    const bool bIsLastColumn = pTableTextNodeInfoInner->isEndOfLine();
-    const bool bIsLastRow = pTableTextNodeInfoInner->isFinalRow();
 
     // Output any table cell redlines if there are any attached to this specific cell
     TableCellRedline( pTableTextNodeInfoInner );
@@ -3298,7 +3294,7 @@ void DocxAttributeOutput::TableCellProperties( ww8::WW8TableNodeInfoInner::Point
     const SvxBoxItem& rDefaultBox = (*tableFirstCells.rbegin())->getTableBox( )->GetFrameFormat( )->GetBox( );
     {
         // The cell borders
-        impl_borders( m_pSerializer, rBox, lcl_getTableCellBorderOptions(bEcma), m_aTableStyleConf, bIsLastColumn, bIsLastRow );
+        impl_borders( m_pSerializer, rBox, lcl_getTableCellBorderOptions(bEcma), m_aTableStyleConf );
     }
 
     TableBackgrounds( pTableTextNodeInfoInner );
