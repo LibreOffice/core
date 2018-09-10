fb841f76fc9d887cfff0301ddaeb51a0995c243b
Stephan Bergmann <sbergman@redhat.com>
2018-09-10 13:30:56 +0200

    Missing include
    Change-Id: I7c0616ffefb00d45342013acc28460dcf76476cb

--- avmedia/source/vlc/vlcuno.cxx
+++ avmedia/source/vlc/vlcuno.cxx
@@ -17,6 +17,9 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
+#include <sal/config.h>
+
+#include <com/sun/star/lang/XSingleServiceFactory.hpp>
 #include <comphelper/processfactory.hxx>
 #include <officecfg/Office/Common.hxx>
 #include <sal/log.hxx>
