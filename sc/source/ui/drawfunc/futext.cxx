LibreOffice/core

commit be48eb2e82a3d8891ee84145567e2b89884f1fd6

return std::unique_ptr from SdrMakeOutliner
and some of its callers

Change-Id: I121a7810e3e35e76da4ffe5fc5405a7bf86cb66d
Reviewed-on: https://gerrit.libreoffice.org/53728
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sc/source/ui/drawfunc/futext.cxx
@@ -184,7 +184,7 @@ bool FuText::MouseButtonDown(const MouseEvent& rMEvt)
                 nullptr;
             if (pObj)
             {
-                SdrOutliner* pO = MakeOutliner();
+                std::unique_ptr<SdrOutliner> pO = MakeOutliner();
                 lcl_UpdateHyphenator( *pO, pObj );
 
                 //  vertical flag:
@@ -197,10 +197,11 @@ bool FuText::MouseButtonDown(const MouseEvent& rMEvt)
                 pO->SetVertical( bVertical );
 
                 //!?? the default values are not correct when result is without outliner ???!?
-                if ( pView->SdrBeginTextEdit(pObj, pPV, pWindow, true, pO) )
+                auto pUndoManager = &pO->GetUndoManager();
+                if ( pView->SdrBeginTextEdit(pObj, pPV, pWindow, true, pO.release()) )
                 {
                     // subscribe EditEngine-UndoManager
-                    pViewShell->SetDrawTextUndo( &pO->GetUndoManager() );
+                    pViewShell->SetDrawTextUndo( pUndoManager );
 
                     OutlinerView* pOLV = pView->GetTextEditOutlinerView();
                     if ( pOLV->MouseButtonDown(rMEvt) )
@@ -578,7 +579,7 @@ void FuText::SetInEditMode(SdrObject* pObj, const Point* pMousePixel,
 
             if ( pObj->HasTextEdit() )
             {
-                SdrOutliner* pO = MakeOutliner();
+                std::unique_ptr<SdrOutliner> pO = MakeOutliner();
                 lcl_UpdateHyphenator( *pO, pObj );
 
                 //  vertical flag:
@@ -592,7 +593,8 @@ void FuText::SetInEditMode(SdrObject* pObj, const Point* pMousePixel,
                 pO->SetVertical( bVertical );
 
                 //!??  without returned Outliner the defaults are not correct ???!?
-                if ( pView->SdrBeginTextEdit(pObj, pPV, pWindow, true, pO) )
+                auto pUndoManager = &pO->GetUndoManager();
+                if ( pView->SdrBeginTextEdit(pObj, pPV, pWindow, true, pO.release()) )
                 {
                     //  Toggle out of paste mode if we are in it, otherwise
                     //  pressing return in this object will instead go to the
@@ -601,7 +603,7 @@ void FuText::SetInEditMode(SdrObject* pObj, const Point* pMousePixel,
                     pViewShell->UpdateCopySourceOverlay();
 
                     //  EditEngine-UndoManager anmelden
-                    pViewShell->SetDrawTextUndo( &pO->GetUndoManager() );
+                    pViewShell->SetDrawTextUndo( pUndoManager );
 
                     pView->SetEditMode();
 

sd/source/ui/func/futext.cxx
@@ -1055,7 +1055,7 @@ void FuText::SetInEditMode(const MouseEvent& rMEvt, bool bQuickDrag)
                  nSdrObjKind == OBJ_OUTLINETEXT || !mxTextObj->IsEmptyPresObj() ) )
             {
                 // create new outliner (owned by SdrObjEditView)
-                SdrOutliner* pOutl = SdrMakeOutliner(OutlinerMode::OutlineObject, *mpDoc);
+                std::unique_ptr<SdrOutliner> pOutl = SdrMakeOutliner(OutlinerMode::OutlineObject, *mpDoc);
 
                 if (bEmptyOutliner)
                     mpView->SdrEndTextEdit(true);
@@ -1076,7 +1076,7 @@ void FuText::SetInEditMode(const MouseEvent& rMEvt, bool bQuickDrag)
                         pTextObj->setActiveText( pTextObj->CheckTextHit(aPnt ) );
                     }
 
-                    if (mpView->SdrBeginTextEdit(pTextObj, pPV, mpWindow, true, pOutl) && mxTextObj->GetObjInventor() == SdrInventor::Default)
+                    if (mpView->SdrBeginTextEdit(pTextObj, pPV, mpWindow, true, pOutl.release()) && mxTextObj->GetObjInventor() == SdrInventor::Default)
                     {
                         //tdf#102293 flush overlay before going on to pass clicks down to
                         //the outline view which will want to paint selections

include/svx/svdetc.hxx
@@ -55,7 +55,7 @@ namespace com { namespace sun { namespace star { namespace lang {
  * The resulting default font height, however, stays the same (the logical
  * font height is converted).
  */
-SVX_DLLPUBLIC SdrOutliner* SdrMakeOutliner(OutlinerMode nOutlinerMode, SdrModel& rMod);
+SVX_DLLPUBLIC std::unique_ptr<SdrOutliner> SdrMakeOutliner(OutlinerMode nOutlinerMode, SdrModel& rMod);
 
 /**
  * Global default settings for the DrawingEngine.

include/svx/svdmodel.hxx
@@ -543,7 +543,7 @@ public:
 
     void ReformatAllTextObjects();
 
-    SdrOutliner* createOutliner( OutlinerMode nOutlinerMode );
+    std::unique_ptr<SdrOutliner> createOutliner( OutlinerMode nOutlinerMode );
     void disposeOutliner( SdrOutliner* pOutliner );
 
     bool IsWriter() const { return !bMyPool; }

sc/source/ui/drawfunc/futext2.cxx
@@ -24,10 +24,10 @@
 #include <futext.hxx>
 #include <tabvwsh.hxx>
 
-SdrOutliner* FuText::MakeOutliner()
+std::unique_ptr<SdrOutliner> FuText::MakeOutliner()
 {
     ScViewData& rViewData = pViewShell->GetViewData();
-    SdrOutliner* pOutl = SdrMakeOutliner(OutlinerMode::OutlineObject, *pDrDoc);
+    std::unique_ptr<SdrOutliner> pOutl = SdrMakeOutliner(OutlinerMode::OutlineObject, *pDrDoc);
 
     rViewData.UpdateOutlinerFlags(*pOutl);
 

sc/source/ui/inc/futext.hxx
@@ -53,7 +53,7 @@ public:
     virtual SdrObject* CreateDefaultObject(const sal_uInt16 nID, const tools::Rectangle& rRectangle) override;
 
 private:
-    SdrOutliner* MakeOutliner();
+    std::unique_ptr<SdrOutliner> MakeOutliner();
 };
 
 #endif

sd/source/ui/func/fuexpand.cxx
@@ -190,7 +190,7 @@ void FuExpandPage::DoExecute( SfxRequest& )
 
                     if( pOutlinerParaObject->GetDepth(0) != -1 )
                     {
-                        SdrOutliner* pTempOutl = SdrMakeOutliner(OutlinerMode::TitleObject, *mpDoc);
+                        std::unique_ptr<SdrOutliner> pTempOutl = SdrMakeOutliner(OutlinerMode::TitleObject, *mpDoc);
 
                         pTempOutl->SetText( *pOutlinerParaObject );
 
@@ -199,7 +199,6 @@ void FuExpandPage::DoExecute( SfxRequest& )
                         pTempOutl->SetDepth( pTempOutl->GetParagraph( 0 ), -1 );
 
                         pOutlinerParaObject = pTempOutl->CreateParaObject();
-                        delete pTempOutl;
                     }
 
                     pTextObj->SetOutlinerParaObject(pOutlinerParaObject);
@@ -218,7 +217,7 @@ void FuExpandPage::DoExecute( SfxRequest& )
                         // create structuring text objects
                         OutlinerParaObject* pOPO = pOutl->CreateParaObject(++nParaPos, nChildCount);
 
-                        SdrOutliner* pTempOutl = SdrMakeOutliner(OutlinerMode::OutlineObject, *mpDoc);
+                        std::unique_ptr<SdrOutliner> pTempOutl = SdrMakeOutliner(OutlinerMode::OutlineObject, *mpDoc);
                         pTempOutl->SetText( *pOPO );
 
                         sal_Int32 nParaCount2 = pTempOutl->GetParagraphCount();
@@ -232,7 +231,7 @@ void FuExpandPage::DoExecute( SfxRequest& )
 
                         delete pOPO;
                         pOPO = pTempOutl->CreateParaObject();
-                        delete pTempOutl;
+                        pTempOutl.reset();
 
                         pOutlineObj->SetOutlinerParaObject( pOPO );
                         pOutlineObj->SetEmptyPresObj(false);

sd/source/ui/view/sdview.cxx
@@ -659,7 +659,7 @@ bool View::SdrBeginTextEdit(
         EventMultiplexerEventId::BeginTextEdit, static_cast<void*>(pObj) );
 
     if( pOutl==nullptr && pObj )
-        pOutl = SdrMakeOutliner(OutlinerMode::TextObject, pObj->getSdrModelFromSdrObject());
+        pOutl = SdrMakeOutliner(OutlinerMode::TextObject, pObj->getSdrModelFromSdrObject()).release();
 
     // make draw&impress specific initialisations
     if( pOutl )

commit 87d3ac959a2b093075e096a1f08ac53518d773e3

tdf#119793 Crash on redo of rectangle movement and text
regression from
   commit be48eb2e82a3d8891ee84145567e2b89884f1fd6
   return std::unique_ptr from SdrMakeOutliner

Change-Id: Iec4421558ed29121973c87b9363da0303f71e203

sc/source/ui/drawfunc/futext.cxx
@@ -197,11 +197,11 @@ bool FuText::MouseButtonDown(const MouseEvent& rMEvt)
                 pO->SetVertical( bVertical );
 
                 //!?? the default values are not correct when result is without outliner ???!?
-                auto pUndoManager = &pO->GetUndoManager();
+                auto pOTemp = pO.get();
                 if ( pView->SdrBeginTextEdit(pObj, pPV, pWindow, true, pO.release()) )
                 {
                     // subscribe EditEngine-UndoManager
-                    rViewShell.SetDrawTextUndo( pUndoManager );
+                    rViewShell.SetDrawTextUndo( &pOTemp->GetUndoManager() );
 
                     OutlinerView* pOLV = pView->GetTextEditOutlinerView();
                     if ( pOLV->MouseButtonDown(rMEvt) )
@@ -592,7 +592,7 @@ void FuText::SetInEditMode(SdrObject* pObj, const Point* pMousePixel,
                 pO->SetVertical( bVertical );
 
                 //!??  without returned Outliner the defaults are not correct ???!?
-                auto pUndoManager = &pO->GetUndoManager();
+                auto pOTemp = pO.get();
                 if ( pView->SdrBeginTextEdit(pObj, pPV, pWindow, true, pO.release()) )
                 {
                     //  Toggle out of paste mode if we are in it, otherwise
@@ -602,7 +602,7 @@ void FuText::SetInEditMode(SdrObject* pObj, const Point* pMousePixel,
                     rViewShell.UpdateCopySourceOverlay();
 
                     //  EditEngine-UndoManager anmelden
-                    rViewShell.SetDrawTextUndo( pUndoManager );
+                    rViewShell.SetDrawTextUndo( &pOTemp->GetUndoManager() );
 
                     pView->SetEditMode();
 