7722b6739bdc83161ef6092fefba0998c399fb4a
Jan-Marek Glogowski <glogow@fbihome.de>
2018-09-13 12:23:36 +0200

    Unify sal plugin loaders
    Change-Id: Ic099761eaff80349e985ccf62e3f4aa6b2e98022

--- vcl/inc/win/saldata.hxx
+++ vcl/inc/win/saldata.hxx
@@ -118,7 +118,6 @@ public:
     std::set< HMENU >       mhMenuSet;              // keeps track of menu handles created by VCL, used by IsKnownMenuHandle()
     std::map< UINT,sal_uInt16 > maVKMap;      // map some dynamic VK_* entries
 
-    // must be deleted before exit(), so delete it in DeInitSalData()
     std::unique_ptr<TextOutRenderer> m_pD2DWriteTextOutRenderer;
     // tdf#107205 need 2 instances because D2DWrite can't rotate text
     std::unique_ptr<TextOutRenderer> m_pExTextOutRenderer;
