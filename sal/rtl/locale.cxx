LibreOffice/core

commit 92b0714c409bd3cffcefd338371ee000fa1b5805

new

sal/util/sal.map
@@ -1,0 +1,505 @@
+UDK_2_0_0 {
+    global:
+        osl_acceptConnectionOnSocket;
+        osl_acceptPipe;
+        osl_acquireSocket;
+        osl_acquirePipe;
+        osl_acquireMutex;
+        osl_acquireSemaphore;
+        osl_addSignalHandler;
+        osl_addToSocketSet;
+        osl_assertFailedLine;
+        osl_awakeThread;
+        osl_bindAddrToSocket;
+        osl_breakDebug;
+        osl_checkCondition;
+        osl_clearSocketSet;
+        osl_closeDirectory;
+        osl_closePipe;
+        osl_closeProfile;
+        osl_connectSocketTo;
+        osl_copyHostAddr;
+        osl_copySocketAddr;
+        osl_createCondition;
+        osl_createEmptySocketAddr;
+        osl_createHostAddr;
+        osl_createHostAddrByAddr;
+        osl_createHostAddrByName;
+        osl_createInetSocketAddr;
+        osl_createMutex;
+        osl_createPipe;
+        osl_createSemaphore;
+        osl_createSocket;
+        osl_createSocketSet;
+        osl_createSuspendedThread;
+        osl_createThread;
+        osl_createThreadKey;
+        osl_decrementInterlockedCount;
+        osl_demultiplexSocketEvents;
+        osl_destroyCondition;
+        osl_destroyHostAddr;
+        osl_destroyMutex;
+        osl_destroySemaphore;
+        osl_destroySocketAddr;
+        osl_destroySocketSet;
+        osl_destroyThread;
+        osl_destroyThreadKey;
+        osl_enableNonBlockingMode;
+        osl_executeProcess;
+        osl_flushProfile;
+        osl_freeProcessHandle;
+        osl_freeSecurityHandle;
+        osl_freeThreadHandle;
+        osl_getAddrOfSocketAddr;
+        osl_getCommandArg;
+        osl_getCommandArgCount;
+        osl_getConfigDir;
+        osl_getCurrentSecurity;
+        osl_getDirectoryItem;
+        osl_getDottedInetAddrOfSocketAddr;
+        osl_getEnvironment;
+        osl_getEthernetAddress;
+        osl_getExecutableFile;
+        osl_getFamilyOfSocketAddr;
+        osl_getFileStatus;
+        osl_getFileURLFromNormalizedPath;
+        osl_getGlobalMutex;
+        osl_getHomeDir;
+        osl_getHostnameOfHostAddr;
+        osl_getHostnameOfSocketAddr;
+        osl_getInetPortOfSocketAddr;
+        osl_getLastPipeError;
+        osl_getLastSocketError;
+        osl_getLastSocketErrorDescription;
+        osl_getLocalAddrOfSocket;
+        osl_getLocalHostname;
+        osl_getNextDirectoryItem;
+        osl_getNormalizedPathFromFileURL;
+        osl_getPeerAddrOfSocket;
+        osl_getProcess;
+        osl_getProcessInfo;
+        osl_getProfileName;
+        osl_getProfileSectionEntries;
+        osl_getProfileSections;
+        osl_getServicePort;
+        osl_getSocketAddrOfHostAddr;
+        osl_getSocketOption;
+        osl_getSocketType;
+        osl_getSymbol;
+        osl_getSystemPathFromNormalizedPath;
+        osl_getSystemTime;
+        osl_getThreadIdentifier;
+        osl_getThreadKeyData;
+        osl_getThreadPriority;
+        osl_getThreadTextEncoding;
+        osl_getUserIdent;
+        osl_getUserName;
+        osl_getVolumeInformation;
+        osl_incrementInterlockedCount;
+        osl_initModuleLogging;
+        osl_isAdministrator;
+        osl_isEqualSocketAddr;
+        osl_isExceptionPending;
+        osl_isInSocketSet;
+        osl_isNonBlockingMode;
+        osl_isReceiveReady;
+        osl_isSendReady;
+        osl_isThreadRunning;
+        osl_joinProcess;
+        osl_joinWithThread;
+        osl_listenOnSocket;
+        osl_loadModule;
+        osl_loginUser;
+        osl_loginUserOnFileServer;
+        osl_normalizePath;
+        osl_openDirectory;
+        osl_openProfile;
+        osl_raiseSignal;
+        osl_readProfileBool;
+        osl_readProfileIdent;
+        osl_readProfileString;
+        osl_readPipe;
+        osl_readSocket;
+        osl_receiveFromSocket;
+        osl_receivePipe;
+        osl_receiveSocket;
+        osl_releaseDirectoryItem;
+        osl_releaseMutex;
+        osl_releasePipe;
+        osl_releaseSemaphore;
+        osl_releaseSocket;
+        osl_removeFromSocketSet;
+        osl_removeProfileEntry;
+        osl_removeSignalHandler;
+        osl_reportError;
+        osl_resetCondition;
+        osl_resolveHostname;
+        osl_resumeThread;
+        osl_scheduleThread;
+        osl_searchNormalizedPath;
+        osl_sendPipe;
+        osl_sendSocket;
+        osl_sendToSocket;
+        osl_setCondition;
+        osl_setFileAttributes;
+        osl_setFileTime;
+        osl_setInetPortOfSocketAddr;
+        osl_setAddrOfSocketAddr;
+        osl_setSocketOption;
+        osl_setThreadKeyData;
+        osl_setThreadPriority;
+        osl_setThreadTextEncoding;
+        osl_shutdownSocket;
+        osl_sleepThread;
+        osl_suspendThread;
+        osl_terminateProcess;
+        osl_terminateThread;
+        osl_trace;
+        osl_tryToAcquireMutex;
+        osl_tryToAcquireSemaphore;
+        osl_unloadLoggedModules;
+        osl_unloadModule;
+        osl_waitCondition;
+        osl_waitThread;
+        osl_writePipe;
+        osl_writeSocket;
+        osl_writeProfileBool;
+        osl_writeProfileIdent;
+        osl_writeProfileString;
+        osl_yieldThread;
+        osl_loadUserProfile;
+        osl_unloadUserProfile;
+        osl_sendResourcePipe;
+        osl_receiveResourcePipe;
+        rtl_allocateMemory;
+        rtl_compareMemory;
+        rtl_copyMemory;
+        rtl_fillMemory;
+        rtl_freeMemory;
+        rtl_moveMemory;
+        rtl_reallocateMemory;
+        rtl_zeroMemory;
+        rtl_findInMemory;
+        rtl_char_getNumericValue;
+        rtl_char_getType;
+        rtl_char_isDefined;
+        rtl_char_isDigit;
+        rtl_char_isISOControl;
+        rtl_char_isIdentifierIgnorable;
+        rtl_char_isJavaIdentifierPart;
+        rtl_char_isJavaIdentifierStart;
+        rtl_char_isLetter;
+        rtl_char_isLetterOrDigit;
+        rtl_char_isLowerCase;
+        rtl_char_isSpaceChar;
+        rtl_char_isTitleCase;
+        rtl_char_isUnicodeIdentifierPart;
+        rtl_char_isUnicodeIdentifierStart;
+        rtl_char_isUpperCase;
+        rtl_char_isWhitespace;
+        rtl_char_toLowerCase;
+        rtl_char_toTitleCase;
+        rtl_char_toUpperCase;
+        rtl_hashentry_destroy;
+        rtl_hashfunc;
+        rtl_hashtable_add;
+        rtl_hashtable_destroy;
+        rtl_hashtable_find;
+        rtl_hashtable_grow;
+        rtl_hashtable_init;
+        rtl_locale_equals;
+        rtl_locale_getCountry;
+        rtl_locale_getDefault;
+        rtl_locale_getLanguage;
+        rtl_locale_getVariant;
+        rtl_locale_hashCode;
+        rtl_locale_register;
+        rtl_locale_setDefault;
+        rtl_str_compare;
+        rtl_str_compare_WithLength;
+        rtl_str_equalsIgnoreCase;
+        rtl_str_equalsIgnoreCase_WithLength;
+        rtl_str_getLength;
+        rtl_str_hashCode;
+        rtl_str_hashCode_WithLength;
+        rtl_str_indexOfChar;
+        rtl_str_indexOfChar_WithLength;
+        rtl_str_indexOfStr;
+        rtl_str_indexOfStr_WithLength;
+        rtl_str_lastIndexOfChar;
+        rtl_str_lastIndexOfChar_WithLength;
+        rtl_str_lastIndexOfStr;
+        rtl_str_lastIndexOfStr_WithLength;
+        rtl_str_replaceChar;
+        rtl_str_replaceChar_WithLength;
+        rtl_str_toAsciiLowerCase;
+        rtl_str_toAsciiLowerCase_WithLength;
+        rtl_str_toAsciiUpperCase;
+        rtl_str_toAsciiUpperCase_WithLength;
+        rtl_str_trim;
+        rtl_str_trim_WithLength;
+        rtl_str_valueOfBoolean;
+        rtl_str_valueOfChar;
+        rtl_str_valueOfInt32;
+        rtl_str_valueOfInt64;
+        rtl_str_valueOfDouble;
+        rtl_str_valueOfFloat;
+        rtl_str_toBoolean;
+        rtl_str_toInt32;
+        rtl_str_toInt64;
+        rtl_str_toDouble;
+        rtl_str_toFloat;
+        rtl_string_acquire;
+        rtl_string_assign;
+        rtl_string_getLength;
+        rtl_string_getStr;
+        rtl_string_getToken;
+        rtl_string_getTokenCount;
+        rtl_string_new;
+        rtl_string_newConcat;
+        rtl_string_newFromStr;
+        rtl_string_newFromStr_WithLength;
+        rtl_string_newFromString;
+        rtl_string_newReplace;
+        rtl_string_newReplaceStrAt;
+        rtl_string_newToLowerCase;
+        rtl_string_newToUpperCase;
+        rtl_string_newTrim;
+        rtl_string_new_WithLength;
+        rtl_string_release;
+        rtl_stringbuffer_ensureCapacity;
+        rtl_stringbuffer_insert;
+        rtl_stringbuffer_newFromStr_WithLength;
+        rtl_stringbuffer_newFromStringBuffer;
+        rtl_ustr_compare;
+        rtl_ustr_compare_WithLength;
+        rtl_ustr_shortenedCompare_WithLength;
+        rtl_ustr_ascii_compare;
+        rtl_ustr_ascii_compare_WithLength;
+        rtl_ustr_ascii_shortenedCompare_WithLength;
+        rtl_ustr_asciil_reverseCompare_WithLength;
+        rtl_ustr_equalsIgnoreCase;
+        rtl_ustr_equalsIgnoreCase_WithLength;
+        rtl_ustr_getLength;
+        rtl_ustr_hashCode;
+        rtl_ustr_hashCode_WithLength;
+        rtl_ustr_indexOfChar;
+        rtl_ustr_indexOfChar_WithLength;
+        rtl_ustr_indexOfStr;
+        rtl_ustr_indexOfStr_WithLength;
+        rtl_ustr_lastIndexOfChar;
+        rtl_ustr_lastIndexOfChar_WithLength;
+        rtl_ustr_lastIndexOfStr;
+        rtl_ustr_lastIndexOfStr_WithLength;
+        rtl_ustr_replaceChar;
+        rtl_ustr_replaceChar_WithLength;
+        rtl_ustr_toAsciiLowerCase;
+        rtl_ustr_toAsciiLowerCase_WithLength;
+        rtl_ustr_toAsciiUpperCase;
+        rtl_ustr_toAsciiUpperCase_WithLength;
+        rtl_ustr_trim;
+        rtl_ustr_trim_WithLength;
+        rtl_ustr_valueOfBoolean;
+        rtl_ustr_valueOfChar;
+        rtl_ustr_valueOfInt32;
+        rtl_ustr_valueOfInt64;
+        rtl_ustr_valueOfDouble;
+        rtl_ustr_valueOfFloat;
+        rtl_ustr_toBoolean;
+        rtl_ustr_toInt32;
+        rtl_ustr_toInt64;
+        rtl_ustr_toDouble;
+        rtl_ustr_toFloat;
+        rtl_uString_acquire;
+        rtl_uString_assign;
+        rtl_uString_getLength;
+        rtl_uString_getStr;
+        rtl_uString_getToken;
+        rtl_uString_getTokenCount;
+        rtl_uString_new;
+        rtl_uString_newConcat;
+        rtl_uString_newFromStr;
+        rtl_uString_newFromStr_WithLength;
+        rtl_uString_newFromAscii;
+        rtl_uString_newFromString;
+        rtl_uString_newReplace;
+        rtl_uString_newReplaceStrAt;
+        rtl_uString_newToLowerCase;
+        rtl_uString_newToUpperCase;
+        rtl_uString_newTrim;
+        rtl_uString_new_WithLength;
+        rtl_uString_release;
+        rtl_uStringbuffer_ensureCapacity;
+        rtl_uStringbuffer_insert;
+        rtl_uStringbuffer_insert_ascii;
+        rtl_uStringbuffer_newFromStr_WithLength;
+        rtl_uStringbuffer_newFromStringBuffer;
+        rtl_string2UString;
+        rtl_uString2String;
+        rtl_createTextToUnicodeConverter;
+        rtl_destroyTextToUnicodeConverter;
+        rtl_createTextToUnicodeContext;
+        rtl_destroyTextToUnicodeContext;
+        rtl_resetTextToUnicodeContext;
+        rtl_convertTextToUnicode;
+        rtl_createUnicodeToTextConverter;
+        rtl_destroyUnicodeToTextConverter;
+        rtl_createUnicodeToTextContext;
+        rtl_destroyUnicodeToTextContext;
+        rtl_resetUnicodeToTextContext;
+        rtl_convertUnicodeToText;
+        rtl_getTextEncodingInfo;
+        rtl_getTextEncodingFromWindowsCharset;
+        rtl_getTextEncodingFromPCCodePage;
+        rtl_getTextEncodingFromMacTextEncoding;
+        rtl_getTextEncodingFromUnixCharset;
+        rtl_getTextEncodingFromMimeCharset;
+        rtl_getBestWindowsCharsetFromTextEncoding;
+        rtl_getBestPCCodePageFromTextEncoding;
+        rtl_getBestMacTextEncodingFromTextEncoding;
+        rtl_getBestUnixCharsetFromTextEncoding;
+        rtl_getBestMimeCharsetFromTextEncoding;
+        rtl_allocateZeroMemory;
+        rtl_freeZeroMemory;
+        rtl_crc32;
+        rtl_digest_create;
+        rtl_digest_createMD2;
+        rtl_digest_createMD5;
+        rtl_digest_createSHA;
+        rtl_digest_createSHA1;
+        rtl_digest_destroy;
+        rtl_digest_destroyMD2;
+        rtl_digest_destroyMD5;
+        rtl_digest_destroySHA;
+        rtl_digest_destroySHA1;
+        rtl_digest_get;
+        rtl_digest_getMD2;
+        rtl_digest_getMD5;
+        rtl_digest_getSHA;
+        rtl_digest_getSHA1;
+        rtl_digest_queryAlgorithm;
+        rtl_digest_queryLength;
+        rtl_digest_update;
+        rtl_digest_updateMD2;
+        rtl_digest_updateMD5;
+        rtl_digest_updateSHA;
+        rtl_digest_updateSHA1;
+        rtl_digest_MD2;
+        rtl_digest_MD5;
+        rtl_digest_SHA;
+        rtl_digest_SHA1;
+
+        rtl_digest_createHMAC_MD5;
+        rtl_digest_destroyHMAC_MD5;
+        rtl_digest_getHMAC_MD5;
+        rtl_digest_initHMAC_MD5;
+        rtl_digest_updateHMAC_MD5;
+
+        rtl_digest_createHMAC_SHA1;
+        rtl_digest_destroyHMAC_SHA1;
+        rtl_digest_getHMAC_SHA1;
+        rtl_digest_initHMAC_SHA1;
+        rtl_digest_updateHMAC_SHA1;
+
+        rtl_digest_PBKDF2;
+
+        rtl_random_createPool;
+        rtl_random_destroyPool;
+        rtl_random_addBytes;
+        rtl_random_getBytes;
+
+        rtl_createUuid;
+        rtl_createNamedUuid;
+        rtl_compareUuid;
+        rtl_getGlobalProcessId;
+
+        osl_openFile;
+        osl_setFilePos;
+        osl_getFilePos;
+        osl_setFileSize;
+        osl_readFile;
+        osl_writeFile;
+        osl_closeFile;
+        osl_createDirectoryItemFromHandle;
+        osl_createDirectory;
+        osl_removeDirectory;
+        osl_removeFile;
+        osl_copyFile;
+        osl_moveFile;
+
+        osl_closeSocket;
+        osl_createInetBroadcastAddr;
+
+        rtl_cipher_create;
+        rtl_cipher_createBF;
+        rtl_cipher_init;
+        rtl_cipher_initBF;
+        rtl_cipher_encode;
+        rtl_cipher_encodeBF;
+        rtl_cipher_decode;
+        rtl_cipher_decodeBF;
+        rtl_cipher_destroy;
+        rtl_cipher_destroyBF;
+
+        rtl_byte_sequence_reference2One;
+        rtl_byte_sequence_realloc;
+        rtl_byte_sequence_acquire;
+        rtl_byte_sequence_release;
+        rtl_byte_sequence_construct;
+        rtl_byte_sequence_constructNoDefault;
+        rtl_byte_sequence_constructFromArray;
+        rtl_byte_sequence_assign;
+        rtl_byte_sequence_equals;
+        rtl_byte_sequence_getConstArray;
+        rtl_byte_sequence_getLength;
+
+        osl_acquireDirectoryItem;
+
+        osl_socket_createSocketLayer;
+        osl_socket_pushSocketLayer;
+        osl_socket_popSocketLayer;
+        osl_socket_deleteSocketLayer;
+
+        osl_socket_create;
+        osl_socket_delete;
+        osl_socket_getLastError;
+        osl_socket_getOption;
+        osl_socket_setOption;
+        osl_socket_accept;
+        osl_socket_accept_handshake;
+        osl_socket_bind;
+        osl_socket_connect;
+        osl_socket_connect_handshake;
+        osl_socket_close;
+        osl_socket_listen;
+        osl_socket_recv;
+        osl_socket_send;
+        osl_socket_shutdown;
+
+        osl_socket_getDefaultSocketMethods;
+        osl_socket_getSocketMethods;
+
+        osl_getDateTimeFromTimeValue;
+        osl_getTimeValueFromDateTime;
+        osl_getLocalTimeFromSystemTime;
+        osl_getSystemTimeFromLocalTime;
+        osl_getAbsolutePath;
+
+        osl_unmountVolumeDevice;
+        osl_automountVolumeDevice;
+        osl_releaseVolumeDeviceHandle;
+        osl_getVolumeDeviceMountPath;
+        osl_acquireVolumeDeviceHandle;
+
+        osl_setDebugMessageFunc;
+        osl_readLine;
+        osl_getGlobalTimer;
+        osl_getCanonicalName;
+        osl_getTextEncodingFromLocale;
+
+        rtl_getUriCharClass;
+        rtl_uriEncode;
+        rtl_uriDecode;
+    local:
+        *;
+};

commit 17be43c988138fc9656366a9792449692d38d067

These rtl_hash* functions appear to never have been exported from sal
They were introduced with 9399c662f36c385b0c705eb34e636a9aec450282 "initial
import" without any trace of being exported DLLPUBLIC-style, and were probably
included in error in the initial sal/util/sal.map when that was introduced in
92b0714c409bd3cffcefd338371ee000fa1b5805 "new".

That means the functions themselves can be moved from extern "C" to an unnamed
namespace (and the resulting loplugin:salbool warnings be fixed).

Change-Id: Ida99540edce9560e69081f507e41db2af34966fb

sal/rtl/locale.cxx
@@ -47,7 +47,9 @@ static RTL_HASHTABLE* g_pLocaleTable = nullptr;
 
 static rtl_Locale* g_pDefaultLocale = nullptr;
 
-extern "C" void rtl_hashentry_destroy(RTL_HASHENTRY* entry)
+namespace {
+
+void rtl_hashentry_destroy(RTL_HASHENTRY* entry)
 {
     rtl_uString_release(entry->Entry->Language);
     rtl_uString_release(entry->Entry->Country);
@@ -60,7 +62,7 @@ extern "C" void rtl_hashentry_destroy(RTL_HASHENTRY* entry)
     free(entry);
 }
 
-extern "C" void rtl_hashtable_destroy(RTL_HASHTABLE* table)
+void rtl_hashtable_destroy(RTL_HASHTABLE* table)
 {
     sal_Int32 size = 0;
 
@@ -81,7 +83,7 @@ extern "C" void rtl_hashtable_destroy(RTL_HASHTABLE* table)
     free(table);
 }
 
-extern "C" void rtl_hashtable_init(RTL_HASHTABLE** table, sal_Int8 sizeIndex)
+void rtl_hashtable_init(RTL_HASHTABLE** table, sal_Int8 sizeIndex)
 {
     sal_Int32 nSize = RTL_HASHTABLE_SIZE[sizeIndex];
 
@@ -102,14 +104,14 @@ extern "C" void rtl_hashtable_init(RTL_HASHTABLE** table, sal_Int8 sizeIndex)
     }
 }
 
-extern "C" sal_Int32 rtl_hashfunc(RTL_HASHTABLE* table, sal_Int32 key)
+sal_Int32 rtl_hashfunc(RTL_HASHTABLE* table, sal_Int32 key)
 {
     return (static_cast<sal_uInt32>(key) % table->Size);
 }
 
-extern "C" sal_Bool rtl_hashtable_grow(RTL_HASHTABLE** table);
+bool rtl_hashtable_grow(RTL_HASHTABLE** table);
 
-extern "C" rtl_Locale* rtl_hashtable_add(RTL_HASHTABLE** table, rtl_Locale* value)
+rtl_Locale* rtl_hashtable_add(RTL_HASHTABLE** table, rtl_Locale* value)
 {
     sal_Int32 key = 0;
 
@@ -138,7 +140,7 @@ extern "C" rtl_Locale* rtl_hashtable_add(RTL_HASHTABLE** table, rtl_Locale* valu
     return nullptr;
 }
 
-sal_Bool rtl_hashtable_grow(RTL_HASHTABLE** table)
+bool rtl_hashtable_grow(RTL_HASHTABLE** table)
 {
     RTL_HASHTABLE* pNewTable = nullptr;
     sal_Int32 i = 0;
@@ -175,7 +177,7 @@ sal_Bool rtl_hashtable_grow(RTL_HASHTABLE** table)
     return true;
 }
 
-extern "C" sal_Bool rtl_hashtable_find(RTL_HASHTABLE * table, sal_Int32 key, sal_Int32 hashCode, rtl_Locale** pValue)
+bool rtl_hashtable_find(RTL_HASHTABLE * table, sal_Int32 key, sal_Int32 hashCode, rtl_Locale** pValue)
 {
     if (!table)
         return false;
@@ -200,6 +202,8 @@ extern "C" sal_Bool rtl_hashtable_find(RTL_HASHTABLE * table, sal_Int32 key, sal
     }
 
     return true;
+}
+
 }
 
 void rtl_locale_init()

sal/util/sal.map
@@ -196,13 +196,6 @@ UDK_3_0_0 {
         rtl_reallocateMemory;
         rtl_zeroMemory;
         rtl_findInMemory;
-        rtl_hashentry_destroy;
-        rtl_hashfunc;
-        rtl_hashtable_add;
-        rtl_hashtable_destroy;
-        rtl_hashtable_find;
-        rtl_hashtable_grow;
-        rtl_hashtable_init;
         rtl_locale_equals;
         rtl_locale_getCountry;
         rtl_locale_getDefault;