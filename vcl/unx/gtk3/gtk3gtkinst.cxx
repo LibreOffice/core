5304eb8992b5ee7d1e72884856a88212db3f252c
Caolán McNamara <caolanm@redhat.com>
2018-09-14 15:39:14 +0100

    weld SvxTabulatorTabPage
    Change-Id: Ife45f59e63309046e532fcb4377e66a887d11f34

--- vcl/unx/gtk3/gtk3gtkinst.cxx
+++ vcl/unx/gtk3/gtk3gtkinst.cxx
@@ -5012,6 +5012,8 @@ public:
         gtk_entry_set_completion(GTK_ENTRY(m_pEntry->getWidget()), nullptr);
     }
 
+    virtual void grab_focus() override { m_xEntry->grab_focus(); }
+
     virtual void connect_focus_in(const Link<Widget&, void>& rLink) override
     {
         m_xEntry->connect_focus_in(rLink);
@@ -5027,7 +5029,6 @@ public:
         GtkWidget* pWidget = m_pEntry->getWidget();
         g_signal_handler_disconnect(pWidget, m_nKeyPressSignalId);
     }
-
 };
 
 class GtkInstanceExpander : public GtkInstanceContainer, public virtual weld::Expander
