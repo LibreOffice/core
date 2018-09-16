0e82806ed6841c0a6919f97660ed4622c89d2338
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2018-09-16 17:27:35 -0400

    LOK: support for ordering/moving parts
    Currently reordering of slides is only supported
    for presentations, although it is provisioned for
    spreadsheets as well.
    
    Change-Id: I6c35066d6a5ef7586d34a8e8b89db69a20b86572
    Reviewed-on: https://gerrit.libreoffice.org/69612
    Reviewed-by: Ashod Nakashian <ashnakash@gmail.com>
    Tested-by: Ashod Nakashian <ashnakash@gmail.com>
    Reviewed-on: https://gerrit.libreoffice.org/73495
    Tested-by: Jenkins
    Reviewed-by: Jan Holesovsky <kendy@collabora.com>

--- sd/source/ui/inc/unomodel.hxx
+++ sd/source/ui/inc/unomodel.hxx
@@ -265,6 +265,8 @@ public:
     virtual OUString getPostIts() override;
     /// @see vcl::ITiledRenderable::selectPart().
     virtual void selectPart(int nPart, int nSelect) override;
+    /// @see vcl::ITiledRenderable::moveSelectedParts().
+    virtual void moveSelectedParts(int nPosition, bool bDuplicate) override;
     /// @see vcl::ITiledRenderable::getPartInfo().
     virtual OUString getPartInfo(int nPart) override;
 
