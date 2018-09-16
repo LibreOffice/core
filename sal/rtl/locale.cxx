c62ac8f9e96b84792f5183651780aeb12d02a284
Noel Grandin <noelgrandin@gmail.com>
2018-09-16 09:10:22 +0200

    use std::unordered_map in locale
    instead of hand-coded hash table
    
    Change-Id: I1c52f98b5a72609f7cd8086227017a486d97520e

--- sal/rtl/locale.cxx
+++ sal/rtl/locale.cxx
@@ -23,202 +23,20 @@
 #include <osl/diagnose.h>
 
 #include <rtllifecycle.h>
+#include <unordered_map>
 
-static const sal_Int32 RTL_HASHTABLE_SIZE[] =
-{
-    7, 31, 127, 251, 509, 1021, 2039, 4093
-};
 
-struct RTL_HASHENTRY
-{
-    rtl_Locale* Entry;
-    RTL_HASHENTRY* Next;
-};
-
-struct RTL_HASHTABLE
-{
-    sal_Int8        iSize;
-    sal_Int32       Size;
-    sal_Int32       Elements;
-    RTL_HASHENTRY** Table;
-};
-
-static RTL_HASHTABLE* g_pLocaleTable = nullptr;
+static std::unordered_map<sal_Int32, rtl_Locale*> g_pLocaleTable;
 
 static rtl_Locale* g_pDefaultLocale = nullptr;
 
-namespace {
-
-void rtl_hashentry_destroy(RTL_HASHENTRY* entry)
-{
-    rtl_uString_release(entry->Entry->Language);
-    rtl_uString_release(entry->Entry->Country);
-    rtl_uString_release(entry->Entry->Variant);
-
-    if (entry->Next)
-        rtl_hashentry_destroy(entry->Next);
-
-    free(entry->Entry);
-    free(entry);
-}
-
-void rtl_hashtable_destroy(RTL_HASHTABLE* table)
-{
-    sal_Int32 size = 0;
-
-    if (!table)
-        return;
-
-    size = table->Size;
-
-    while (size)
-    {
-        if (table->Table[size - 1])
-            rtl_hashentry_destroy(table->Table[size - 1]);
-
-        size--;
-    }
-
-    free(table->Table);
-    free(table);
-}
-
-void rtl_hashtable_init(RTL_HASHTABLE** table, sal_Int8 sizeIndex)
-{
-    sal_Int32 nSize = RTL_HASHTABLE_SIZE[sizeIndex];
-
-    if (*table)
-        rtl_hashtable_destroy(*table);
-
-    *table = static_cast< RTL_HASHTABLE* >(malloc(sizeof(RTL_HASHTABLE)));
-
-    (*table)->iSize = sizeIndex;
-    (*table)->Size = nSize;
-    (*table)->Elements = 0;
-    (*table)->Table = static_cast< RTL_HASHENTRY** >(malloc((*table)->Size * sizeof(RTL_HASHENTRY*)));
-
-    while (nSize)
-    {
-        (*table)->Table[nSize - 1] = nullptr;
-        nSize--;
-    }
-}
-
-sal_Int32 rtl_hashfunc(RTL_HASHTABLE* table, sal_Int32 key)
-{
-    return (static_cast<sal_uInt32>(key) % table->Size);
-}
-
-bool rtl_hashtable_grow(RTL_HASHTABLE** table);
-
-rtl_Locale* rtl_hashtable_add(RTL_HASHTABLE** table, rtl_Locale* value)
-{
-    sal_Int32 key = 0;
-
-    if (!(*table))
-        return nullptr;
-
-    if ((*table)->Elements > ((*table)->Size / 2))
-        rtl_hashtable_grow(table);
-
-    key = rtl_hashfunc(*table, value->HashCode);
-
-    RTL_HASHENTRY **pEntry = &(*table)->Table[key];
-    while (*pEntry)
-    {
-        if (value->HashCode == (*pEntry)->Entry->HashCode)
-            return (*pEntry)->Entry;
-
-        pEntry = &(*pEntry)->Next;
-    }
-
-    RTL_HASHENTRY *newEntry = static_cast< RTL_HASHENTRY* >(malloc(sizeof(RTL_HASHENTRY)));
-    newEntry->Entry = value;
-    newEntry->Next = nullptr;
-    *pEntry = newEntry;
-    (*table)->Elements++;
-    return nullptr;
-}
-
-bool rtl_hashtable_grow(RTL_HASHTABLE** table)
-{
-    RTL_HASHTABLE* pNewTable = nullptr;
-    sal_Int32 i = 0;
-
-    rtl_hashtable_init(&pNewTable, static_cast<sal_Int8>((*table)->iSize + 1));
-
-    while (i < (*table)->Size)
-    {
-        if ((*table)->Table[i])
-        {
-            RTL_HASHENTRY *pNext;
-            RTL_HASHENTRY *pEntry = (*table)->Table[i];
-
-            rtl_hashtable_add(&pNewTable, pEntry->Entry);
-
-            while (pEntry->Next)
-            {
-                rtl_hashtable_add(&pNewTable, pEntry->Next->Entry);
-                pNext = pEntry->Next;
-                free(pEntry);
-                pEntry = pNext;
-            }
-
-            free(pEntry);
-        }
-
-        i++;
-    }
-
-    free((*table)->Table);
-    free(*table);
-    (*table) = pNewTable;
-
-    return true;
-}
-
-bool rtl_hashtable_find(RTL_HASHTABLE * table, sal_Int32 key, sal_Int32 hashCode, rtl_Locale** pValue)
-{
-    if (!table)
-        return false;
-
-    if (table->Table[key])
-    {
-        RTL_HASHENTRY *pEntry = table->Table[key];
-
-        while (pEntry && hashCode != pEntry->Entry->HashCode)
-        {
-            pEntry = pEntry->Next;
-        }
-
-        if (pEntry)
-            *pValue = pEntry->Entry;
-        else
-            return false;
-    }
-    else
-    {
-        return false;
-    }
-
-    return true;
-}
-
-}
-
 void rtl_locale_init()
 {
-    OSL_ASSERT(!g_pLocaleTable);
-    rtl_hashtable_init(&g_pLocaleTable, 1);
 }
 
 void rtl_locale_fini()
 {
-    if (g_pLocaleTable)
-    {
-        rtl_hashtable_destroy (g_pLocaleTable);
-        g_pLocaleTable = nullptr;
-    }
+    g_pLocaleTable.clear();
 }
 
 rtl_Locale * SAL_CALL rtl_locale_register(const sal_Unicode * language, const sal_Unicode * country, const sal_Unicode * variant)
@@ -229,7 +47,6 @@ rtl_Locale * SAL_CALL rtl_locale_register(const sal_Unicode * language, const sa
     rtl_uString* sVariant = nullptr;
     rtl_Locale *newLocale = nullptr;
     sal_Int32 hashCode = -1;
-    sal_Int32 key = 0;
 
     if (!country)
         country = &c;
@@ -238,14 +55,12 @@ rtl_Locale * SAL_CALL rtl_locale_register(const sal_Unicode * language, const sa
         variant = &c;
 
     ensureLocaleSingleton();
-    if (!g_pLocaleTable)
-        return nullptr;
 
     hashCode = rtl_ustr_hashCode(language) ^ rtl_ustr_hashCode(country) ^ rtl_ustr_hashCode(variant);
-    key = rtl_hashfunc(g_pLocaleTable, hashCode);
 
-    if (rtl_hashtable_find(g_pLocaleTable, key, hashCode, &newLocale))
-        return newLocale;
+    auto it = g_pLocaleTable.find(hashCode);
+    if (it != g_pLocaleTable.end())
+        return it->second;
 
     rtl_uString_newFromStr(&sLanguage, language);
     rtl_uString_newFromStr(&sCountry, country);
@@ -258,7 +73,7 @@ rtl_Locale * SAL_CALL rtl_locale_register(const sal_Unicode * language, const sa
     newLocale->Variant = sVariant;
     newLocale->HashCode = hashCode;
 
-    rtl_hashtable_add(&g_pLocaleTable, newLocale);
+    g_pLocaleTable.insert(it, { hashCode, newLocale });
 
     return newLocale;
 }
