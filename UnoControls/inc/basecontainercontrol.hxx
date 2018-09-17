be090c9eb6fb90f9d5f35f2c21737ed3dee5b29b
Noel Grandin <noel.grandin@collabora.co.uk>
2018-09-17 13:12:21 +0200

    loplugin:useuniqueptr in BaseContainerControl
    Change-Id: If78893d47d2b504b01652575579c6972a3863a09

--- UnoControls/inc/basecontainercontrol.hxx
+++ UnoControls/inc/basecontainercontrol.hxx
@@ -152,10 +152,8 @@ protected:
 private:
     void impl_activateTabControllers();
 
-    void impl_cleanMemory();
-
     // list of pointer of "struct IMPL_ControlInfo" to hold child-controls
-    ::std::vector< IMPL_ControlInfo* > maControlInfoList;
+    ::std::vector< std::unique_ptr<IMPL_ControlInfo> > maControlInfoList;
 
     // list of references of XTabController to hold tab-order in this container
     css::uno::Sequence< css::uno::Reference< css::awt::XTabController > >  m_xTabControllerList;
