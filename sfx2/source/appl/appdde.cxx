f40acf7998d6e47631a68c87cc60d3088f0005ef
Stephan Bergmann <sbergman@redhat.com>
2018-09-17 18:31:59 +0200

    Missing include (Windows, --disable-pch)
    Change-Id: I825230daf223ff94ab1dbc19d81d478fbf464f56

--- sfx2/source/appl/appdde.cxx
+++ sfx2/source/appl/appdde.cxx
@@ -44,6 +44,7 @@
 #include <helper.hxx>
 #include <sfx2/docfile.hxx>
 #include <comphelper/processfactory.hxx>
+#include <com/sun/star/ucb/CommandAbortedException.hpp>
 #include <com/sun/star/ucb/IllegalIdentifierException.hpp>
 #include <com/sun/star/ucb/ContentCreationException.hpp>
 
