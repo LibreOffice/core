1ff27bccf4b83cec9fd108442b47ee46a72db837
Luboš Luňák <l.lunak@collabora.com>
2018-09-18 15:00:38 +0200

    avoid usually needless large allocation for formula token array
    Change-Id: I855af060e1aeb91bccfc923ca567ad34d64be757

--- formula/source/core/api/token.cxx
+++ formula/source/core/api/token.cxx
@@ -776,8 +776,20 @@ FormulaToken* FormulaTokenArray::Add( FormulaToken* t )
         return nullptr;
     }
 
+// Allocating an array of size FORMULA_TOKENS is simple, but that results in relatively large
+// allocations that malloc() implementations usually do not handle as efficiently as smaller
+// sizes (not only in terms of memory usage but also speed). Since most token arrays are going
+// to be small, start with a small array and resize only if needed.
+#define MAX_FAST_TOKENS 32
     if( !pCode )
-        pCode.reset(new FormulaToken*[ FORMULA_MAXTOKENS ]);
+        pCode.reset(new FormulaToken*[ MAX_FAST_TOKENS ]);
+    if( nLen == MAX_FAST_TOKENS )
+    {
+        FormulaToken** tmp = new FormulaToken*[ FORMULA_MAXTOKENS ];
+        std::copy(&pCode[0], &pCode[MAX_FAST_TOKENS], tmp);
+        pCode.reset(tmp);
+    }
+#undef MAX_FAST_TOKENS
     if( nLen < FORMULA_MAXTOKENS - 1 )
     {
         CheckToken(*t);
