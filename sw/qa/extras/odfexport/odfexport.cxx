dfeb0bacac04364737a52803b4b4732a58702d38
Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
2018-09-19 12:51:00 +0200

    tdf#118502 Fix loading of eps images with replacement graphic
    Change-Id: Iaf9c8e2ed72115e1f82d2541ae2a1d4803795a46
    Reviewed-on: https://gerrit.libreoffice.org/60752
    Tested-by: Jenkins
    Reviewed-by: Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
    (cherry picked from commit 088af44063c3e2d1d0e7744a9f9bd7b37ff58a75)
    Reviewed-on: https://gerrit.libreoffice.org/60768
    Reviewed-by: Michael Stahl <Michael.Stahl@cib.de>
    (cherry picked from commit 005be0cd5ea4a16860ad595d03ab133daf709a33)

--- sw/qa/extras/odfexport/odfexport.cxx
+++ sw/qa/extras/odfexport/odfexport.cxx
@@ -1790,6 +1790,14 @@ DECLARE_ODFEXPORT_TEST(testTdf77961, "tdf77961.odt")
     CPPUNIT_ASSERT_EQUAL( false , getProperty<bool>(xStyle, "GridPrint"));
 }
 
+DECLARE_ODFEXPORT_TEST(tdf118502, "tdf118502.odt")
+{
+    uno::Reference<drawing::XShape> xShape = getShape(1);
+    // Make sure the replacement graphic is still there
+    // (was gone because the original graphic was not recognized during load)
+    CPPUNIT_ASSERT(!getProperty<OUString>(xShape, "ReplacementGraphicURL").isEmpty());
+}
+
 #endif
 
 CPPUNIT_PLUGIN_IMPLEMENT();
