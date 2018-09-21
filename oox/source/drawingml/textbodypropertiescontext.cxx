78b11422fa15f7d690b8a5398b03b7ce6897c49d
Miklos Vajna <vmiklos@collabora.co.uk>
2018-09-21 11:50:57 +0200

    tdf#120028 PPTX import: map shapes with multiple columns to table shapes
    This is a combination of 3 commits.
    
    1st commit:
    
    Longer term the core Impress shape has to be improved so that it can have
    text in multiple columns.
    
    Shorter term, map text with multiple columns to table shapes, that gives
    correct layout in many cases and requires changes to the import filter
    only.
    
    (cherry picked from commit aef569ed83a3ccc02639e5b2a1c7cc131ba262fc)
    
    Commit #2:
    
    Related: tdf#120028 PPTX import: fix font size of multi-col shape text
    
    Normal shapes have a TextFitToSize property for this, but table cell
    content has to be scaled manually, as the rendering has no automatic
    support for them. Luckily the file format has the info we need to scale
    at import time.
    
    (cherry picked from commit c8b2849d140677f7b35523096eb2bc715b3dc507)
    
    Commit #3:
    
    Related: tdf#120028 PPTX import: fix font color of multi-col shape text
    
    Not just font color, anything defined via list styles were not imported.
    
    (cherry picked from commit 800bf1071a30955ff70373b2fb5886eadce59dee)
    
    Conflicts:
    	oox/inc/drawingml/textbodyproperties.hxx
    
    Reviewed-on: https://gerrit.libreoffice.org/61244
    Tested-by: Jenkins
    Reviewed-by: Andras Timar <andras.timar@collabora.com>
    (cherry picked from commit 6f1fb474694876bc4ffeb552dcc7bed8f32cd221)
    
    Change-Id: Id7942b16882ab1f083ded2f0e577e8c9e128697c

--- oox/source/drawingml/textbodypropertiescontext.cxx
+++ oox/source/drawingml/textbodypropertiescontext.cxx
@@ -90,7 +90,7 @@ TextBodyPropertiesContext::TextBodyPropertiesContext( ContextHandler2Helper cons
 //   sal_Int32 nVertOverflow =  rAttribs.getToken( XML_vertOverflow, XML_overflow );
 
     // ST_TextColumnCount
-//   sal_Int32 nNumCol = rAttribs.getInteger( XML_numCol, 1 );
+    mrTextBodyProp.mnNumCol = rAttribs.getInteger( XML_numCol, 1 );
 
     // ST_Angle
     mrTextBodyProp.moRotation = rAttribs.getInteger( XML_rot );
@@ -155,9 +155,12 @@ ContextHandlerRef TextBodyPropertiesContext::onCreateContext( sal_Int32 aElement
                 mrTextBodyProp.maPropertyMap.setProperty( PROP_TextAutoGrowHeight, false);   // CT_TextNoAutofit
                 break;
             case A_TOKEN( normAutofit ):    // CT_TextNormalAutofit
+            {
                 mrTextBodyProp.maPropertyMap.setProperty( PROP_TextFitToSize, TextFitToSizeType_AUTOFIT);
                 mrTextBodyProp.maPropertyMap.setProperty( PROP_TextAutoGrowHeight, false);
+                mrTextBodyProp.mnFontScale = rAttribs.getInteger(XML_fontScale, 100000);
                 break;
+            }
             case A_TOKEN( spAutoFit ):
                 {
                     const sal_Int32 tVert = mrTextBodyProp.moVert.get( XML_horz );
