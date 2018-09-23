9ffe5b87fc08ca53d81bd2e964fa1ec6df4315cb
Rene Engelhard <rene@debian.org>
2018-09-23 19:40:59 +0200

    tdf#72987 split firebird test into a regression test and a "new" test
    former with endianness and latter with the new fbk format.
    Add new endianness-independent firebird test odbs
    
    Change-Id: I29be2e6916fcca74744211dba04463376fb6b8d5

--- dbaccess/CppunitTest_dbaccess_firebird_regression_test.mk
+++ dbaccess/CppunitTest_dbaccess_firebird_regression_test.mk
@@ -7,15 +7,15 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 
-$(eval $(call gb_CppunitTest_CppunitTest,dbaccess_firebird))
+$(eval $(call gb_CppunitTest_CppunitTest,dbaccess_firebird_regression))
 
-$(eval $(call gb_CppunitTest_use_external,dbaccess_firebird,boost_headers))
+$(eval $(call gb_CppunitTest_use_external,dbaccess_firebird_regression,boost_headers))
 
-$(eval $(call gb_CppunitTest_add_exception_objects,dbaccess_firebird, \
-    dbaccess/qa/unit/firebird \
+$(eval $(call gb_CppunitTest_add_exception_objects,dbaccess_firebird_regression, \
+    dbaccess/qa/unit/firebird-regression \
 ))
 
-$(eval $(call gb_CppunitTest_use_libraries,dbaccess_firebird, \
+$(eval $(call gb_CppunitTest_use_libraries,dbaccess_firebird_regression, \
     comphelper \
     cppu \
     dbaxml \
@@ -29,16 +29,16 @@ $(eval $(call gb_CppunitTest_use_libraries,dbaccess_firebird, \
     xo \
 ))
 
-$(eval $(call gb_CppunitTest_use_api,dbaccess_firebird,\
+$(eval $(call gb_CppunitTest_use_api,dbaccess_firebird_regression,\
     offapi \
     oovbaapi \
     udkapi \
 ))
 
-$(eval $(call gb_CppunitTest_use_ure,dbaccess_firebird))
-$(eval $(call gb_CppunitTest_use_vcl,dbaccess_firebird))
+$(eval $(call gb_CppunitTest_use_ure,dbaccess_firebird_regression))
+$(eval $(call gb_CppunitTest_use_vcl,dbaccess_firebird_regression))
 
-$(eval $(call gb_CppunitTest_use_components,dbaccess_firebird,\
+$(eval $(call gb_CppunitTest_use_components,dbaccess_firebird_regression,\
     basic/util/sb \
     comphelper/util/comphelp \
     configmgr/source/configmgr \
@@ -65,6 +65,6 @@ $(eval $(call gb_CppunitTest_use_components,dbaccess_firebird,\
     xmloff/util/xo \
 ))
 
-$(eval $(call gb_CppunitTest_use_configuration,dbaccess_firebird))
+$(eval $(call gb_CppunitTest_use_configuration,dbaccess_firebird_regression))
 
 # vim: set noet sw=4 ts=4:
