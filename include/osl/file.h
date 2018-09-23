LibreOffice/core

commit 2157a3536f97ff5ae7c82611a801fef7e3708983

sfx2 store: try rename before copying
Rename is cheaper then copying the content over manually, so try that
first.

Change-Id: Ieb1d03e39501c1565dae7e3290e318a09ee18965
Reviewed-on: https://gerrit.libreoffice.org/47612
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>

sfx2/source/doc/docfile.cxx
@@ -1721,29 +1721,36 @@ void SfxMedium::TransactedTransferForFS_Impl( const INetURLObject& aSource,
 
             try
             {
-                if( bOverWrite && ::utl::UCBContentHelper::IsDocument( aDest.GetMainURL( INetURLObject::DecodeMechanism::NONE ) ) )
+                OUString aSourceMainURL = aSource.GetMainURL(INetURLObject::DecodeMechanism::NONE);
+                OUString aDestMainURL = aDest.GetMainURL(INetURLObject::DecodeMechanism::NONE);
+                if (comphelper::isFileUrl(aDestMainURL) && osl::File::move(aSourceMainURL, aDestMainURL) == osl::FileBase::E_None)
+                    bResult = true;
+                else
                 {
-                    if( pImpl->m_aBackupURL.isEmpty() )
-                        DoInternalBackup_Impl( aOriginalContent );
-
-                    if( !pImpl->m_aBackupURL.isEmpty() )
+                    if (bOverWrite && ::utl::UCBContentHelper::IsDocument(aDestMainURL))
                     {
-                        Reference< XInputStream > aTempInput = aTempCont.openStream();
-                        bTransactStarted = true;
-                        aOriginalContent.setPropertyValue( "Size", uno::makeAny( (sal_Int64)0 ) );
-                        aOriginalContent.writeStream( aTempInput, bOverWrite );
-                        bResult = true;
+                        if( pImpl->m_aBackupURL.isEmpty() )
+                            DoInternalBackup_Impl( aOriginalContent );
+
+                        if( !pImpl->m_aBackupURL.isEmpty() )
+                        {
+                            Reference< XInputStream > aTempInput = aTempCont.openStream();
+                            bTransactStarted = true;
+                            aOriginalContent.setPropertyValue( "Size", uno::makeAny( (sal_Int64)0 ) );
+                            aOriginalContent.writeStream( aTempInput, bOverWrite );
+                            bResult = true;
+                        }
+                        else
+                        {
+                            pImpl->m_eError = ERRCODE_SFX_CANTCREATEBACKUP;
+                        }
                     }
                     else
                     {
-                        pImpl->m_eError = ERRCODE_SFX_CANTCREATEBACKUP;
+                        Reference< XInputStream > aTempInput = aTempCont.openStream();
+                        aOriginalContent.writeStream( aTempInput, bOverWrite );
+                        bResult = true;
                     }
-                }
-                else
-                {
-                    Reference< XInputStream > aTempInput = aTempCont.openStream();
-                    aOriginalContent.writeStream( aTempInput, bOverWrite );
-                    bResult = true;
                 }
             }
             catch ( const css::ucb::CommandAbortedException& )

commit bcae324aef2288c85c5f9477f5c9659a6fff6992

tdf#119238: keep replaced file's identity when renaming docfile
Regression from 2157a3536f97ff5ae7c82611a801fef7e3708983

  sfx2 store: try rename before copying

  Rename is cheaper then copying the content over manually, so try that
  first.

On Windows, we need to keep the file's creation time (which is kept in FS).
WinAPI has ReplaceFileW specifically for this, and it keeps ACLs of the
original file, and otherwise makes the changed file not a separate entry,
but updated old file from system's PoV.

Eventually, we could try to restructure creating backup copies (e.g., for
documents when configured so) to take advantage of this function being able
to do that.

Change-Id: I6001a2a3af5e10bc010f5ef129f4bb6f83ee1581

include/osl/file.h
@@ -1634,6 +1634,37 @@ SAL_DLLPUBLIC oslFileError SAL_CALL osl_createTempFile(
     oslFileHandle* pHandle,
     rtl_uString**  ppustrTempFileURL);
 
+/** Move a file to a new destination or renames it, taking old file's identity (if exists).
+
+    Moves or renames a file, replacing an existing file if exist. If the old file existed,
+    moved file creation time (on FSes which keep files' creation time) is set to old one's
+    (to keep the old file's identity) - currently this is only relevant to Windows, on other
+    platforms, this is equivalent to osl_moveFile.
+
+    @param[in] pustrSourceFileURL
+    Full qualified URL of the source file.
+
+    @param[in] pustrDestFileURL
+    Full qualified URL of the destination file.
+
+    @retval osl_File_E_None on success
+    @retval osl_File_E_INVAL the format of the parameters was not valid
+    @retval osl_File_E_NOMEM not enough memory for allocating structures
+    @retval osl_File_E_ACCES permission denied
+    @retval osl_File_E_PERM operation not permitted
+    @retval osl_File_E_NAMETOOLONG file name too long
+    @retval osl_File_E_NOENT no such file
+    @retval osl_File_E_ROFS read-only file system
+    @retval osl_File_E_BUSY if the implementation internally requires resources that are
+        (temporarily) unavailable
+
+    @see osl_copyFile()
+
+    @since LibreOffice 6.1
+*/
+SAL_DLLPUBLIC oslFileError SAL_CALL osl_replaceFile(rtl_uString* pustrSourceFileURL,
+                                                    rtl_uString* pustrDestFileURL);
+
 #ifdef __cplusplus
 }
 #endif

include/osl/file.hxx
@@ -1298,6 +1298,39 @@ public:
         return static_cast< RC >( osl_moveFile( ustrSourceFileURL.pData, ustrDestFileURL.pData ) );
     }
 
+    /** Move a file to a new destination or renames it, taking old file's identity (if exists).
+
+        Moves or renames a file, replacing an existing file if exist. If the old file existed,
+        moved file creation time (on FSes which keep files' creation time) is set to old one's
+        (to keep the old file's identity) - currently this is only relevant to Windows, on other
+        platforms, this is equivalent to move().
+
+        @param[in] ustrSourceFileURL
+        Full qualified URL of the source file.
+
+        @param[in] ustrDestFileURL
+        Full qualified URL of the destination file.
+
+        @retval E_None on success
+        @retval E_INVAL the format of the parameters was not valid
+        @retval E_NOMEM not enough memory for allocating structures
+        @retval E_ACCES permission denied
+        @retval E_PERM operation not permitted
+        @retval E_NAMETOOLONG file name too long
+        @retval E_NOENT no such file
+        @retval E_ROFS read-only file system
+        @retval E_BUSY device or resource busy
+
+        @see copy()
+
+    @since LibreOffice 6.1
+*/
+    static RC replace(const ::rtl::OUString& ustrSourceFileURL,
+                      const ::rtl::OUString& ustrDestFileURL)
+    {
+        return static_cast<RC>(osl_replaceFile(ustrSourceFileURL.pData, ustrDestFileURL.pData));
+    }
+
     /** Remove a regular file.
 
         @param[in] ustrFileURL

sal/osl/unx/file_misc.cxx
@@ -604,6 +604,11 @@ oslFileError SAL_CALL osl_moveFile( rtl_uString* ustrFileURL, rtl_uString* ustrD
     return oslDoMoveFile( srcPath, destPath );
 }
 
+oslFileError SAL_CALL osl_replaceFile(rtl_uString* strPath, rtl_uString* strDestPath)
+{
+    return osl_moveFile(ustrFileURL, ustrDestURL);
+}
+
 oslFileError SAL_CALL osl_copyFile( rtl_uString* ustrFileURL, rtl_uString* ustrDestURL )
 {
     char srcPath[PATH_MAX];

sal/osl/w32/file.cxx
@@ -1098,4 +1098,34 @@ oslFileError SAL_CALL osl_moveFile(rtl_uString* strPath, rtl_uString *strDestPat
     return error;
 }
 
+oslFileError SAL_CALL osl_replaceFile(rtl_uString* strPath, rtl_uString* strDestPath)
+{
+    rtl_uString *strSysPath = nullptr, *strSysDestPath = nullptr;
+    oslFileError    error = osl_getSystemPathFromFileURL_(strPath, &strSysPath, false);
+
+    if (error == osl_File_E_None)
+        error = osl_getSystemPathFromFileURL_(strDestPath, &strSysDestPath, false);
+
+    if (error == osl_File_E_None)
+    {
+        LPCWSTR src = o3tl::toW(rtl_uString_getStr(strSysPath));
+        LPCWSTR dst = o3tl::toW(rtl_uString_getStr(strSysDestPath));
+
+        if (ReplaceFileW(dst, src, nullptr,
+                         REPLACEFILE_WRITE_THROUGH | REPLACEFILE_IGNORE_MERGE_ERRORS
+                             | REPLACEFILE_IGNORE_ACL_ERRORS,
+                         nullptr, nullptr))
+            error = osl_File_E_None;
+        else
+            error = oslTranslateFileError(GetLastError());
+    }
+
+    if (strSysPath)
+        rtl_uString_release(strSysPath);
+    if (strSysDestPath)
+        rtl_uString_release(strSysDestPath);
+
+    return error;
+}
+
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sfx2/source/doc/docfile.cxx
@@ -1845,7 +1845,8 @@ void SfxMedium::TransactedTransferForFS_Impl( const INetURLObject& aSource,
                 OUString aDestMainURL = aDest.GetMainURL(INetURLObject::DecodeMechanism::NONE);
 
                 sal_uInt64 nAttributes = GetDefaultFileAttributes(aDestMainURL);
-                if (IsFileMovable(aDestMainURL) && osl::File::move(aSourceMainURL, aDestMainURL) == osl::FileBase::E_None)
+                if (IsFileMovable(aDestMainURL)
+                    && osl::File::replace(aSourceMainURL, aDestMainURL) == osl::FileBase::E_None)
                 {
                     if (nAttributes)
                         // Adjust attributes, source might be created with