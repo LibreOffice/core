LibreOffice/core

commit c57f10b67fceef47d304d74dce93be7389a7720c

tdf#111306: Hide background widget in sidebar in master and notes view
Background is lost if it's changed from the sidebar in master or
notes view, thus hide the widget for now, until the real issue is fixed.

Keep the insert image button visible as it works.

The background can be changed from the page properties dialog

Change-Id: Ic3f6116cb0997024096343cb20ec40ac07307918
Reviewed-on: https://gerrit.libreoffice.org/57440
Tested-by: Jenkins
Reviewed-by: Xisco Faulí <xiscofauli@libreoffice.org>

sd/source/ui/sidebar/SlideBackground.cxx
@@ -244,11 +244,11 @@ void SlideBackground::HandleContextChange(
             mpMasterSlide->Disable();
             mpDspMasterBackground->Disable();
             mpDspMasterObjects->Disable();
-            mpFillStyle->Show();
-            mpBackgroundLabel->Show();
+            mpFillStyle->Hide();
+            mpBackgroundLabel->Hide();
             mpInsertImage->Show();
         }
-        else if ( maContext == maImpressHandoutContext )
+        else if ( maContext == maImpressHandoutContext  || maContext == maImpressNotesContext )
         {
             mpCloseMaster->Hide();
             mpEditMaster->Hide();
@@ -270,17 +270,7 @@ void SlideBackground::HandleContextChange(
             mpBackgroundLabel->Show();
             mpInsertImage->Show();
         }
-        else if (maContext == maImpressNotesContext)
-        {
-            mpCloseMaster->Hide();
-            mpEditMaster->Hide();
-            mpMasterSlide->Disable();
-            mpDspMasterBackground->Disable();
-            mpDspMasterObjects->Disable();
-            mpFillStyle->Show();
-            mpBackgroundLabel->Show();
-            mpInsertImage->Hide();
-        }
+
         // Need to do a relayouting, otherwise the panel size is not updated after show / hide controls
         sfx2::sidebar::Panel* pPanel = dynamic_cast<sfx2::sidebar::Panel*>(GetParent());
         if(pPanel)
@@ -296,7 +286,7 @@ void SlideBackground::Update()
 {
     eFillStyle nPos = static_cast<eFillStyle>(mpFillStyle->GetSelectedEntryPos());
 
-    if(maContext == maImpressHandoutContext)
+    if(maContext != maImpressOtherContext)
         nPos = NONE;
 
     SfxObjectShell* pSh = SfxObjectShell::Current();

commit 6a21ed2eecb8e15fbb7bd7e68eeaa3685cdf7094

tdf#120069: Do not crash changing background color in draw
Regression from c57f10b67fceef47d304d74dce93be7389a7720c,
where I disabled the master background in Impress but not in Draw,
and both share some common code.

Besides, disable master background in draw as well. Follow-up of bug
tdf#111306

Change-Id: I772382b5ed8eff3a35d001ea498810613137d172

sd/source/ui/sidebar/SlideBackground.cxx
@@ -278,6 +278,17 @@ void SlideBackground::HandleContextChange(
     else if ( IsDraw() )
     {
         mpMasterLabel->SetText(SdResId(STR_MASTERPAGE_LABEL));
+
+        if (maContext == maDrawOtherContext)
+        {
+            mpFillStyle->Show();
+            mpBackgroundLabel->Show();
+        }
+        else if (maContext == maDrawMasterContext)
+        {
+            mpFillStyle->Hide();
+            mpBackgroundLabel->Hide();
+        }
     }
 }
 
@@ -285,7 +296,7 @@ void SlideBackground::Update()
 {
     eFillStyle nPos = static_cast<eFillStyle>(mpFillStyle->GetSelectedEntryPos());
 
-    if(maContext != maImpressOtherContext)
+    if(maContext != maImpressOtherContext && maContext != maDrawOtherContext)
         nPos = NONE;
 
     SfxObjectShell* pSh = SfxObjectShell::Current();