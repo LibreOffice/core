e86df5ca4bd01f73e2d750a96abada751a637c8d
Michael Stahl <Michael.Stahl@cib.de>
2018-09-24 15:26:58 +0200

    sw_redlinehide_2: add unit tests with fly frames
    ... in the body, in header and in footnotes.
    
    Change-Id: Id66911008fe3b68ae9f8dbc24b7d6fee61324f0e

--- sw/CppunitTest_sw_layoutwriter.mk
+++ sw/CppunitTest_sw_layoutwriter.mk
@@ -44,6 +44,7 @@ $(eval $(call gb_CppunitTest_use_externals,sw_layoutwriter,\
 $(eval $(call gb_CppunitTest_set_include,sw_layoutwriter,\
     -I$(SRCDIR)/sw/inc \
     -I$(SRCDIR)/sw/source/core/inc \
+    -I$(SRCDIR)/sw/source/uibase/inc \
     -I$(SRCDIR)/sw/qa/extras/inc \
     $$(INCLUDE) \
 ))
