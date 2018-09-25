5ca6b9db3368a0fa4d8466ccb27e68c1349c4457
Vasily Melenchuk <vasily.melenchuk@cib.de>
2018-09-25 11:21:55 +0300

    sw: new unit test for XAutoTextGroup and rdf::URI
    Change-Id: I989ff797ddcd4ce77ac9b87fde2a2f8f75dbbc6b

--- sw/Module_sw.mk
+++ sw/Module_sw.mk
@@ -97,6 +97,7 @@ $(eval $(call gb_Module_add_slowcheck_targets,sw,\
     CppunitTest_sw_accessible_relation_set \
     CppunitTest_sw_apitests \
     CppunitTest_sw_unowriter \
+    CppunitTest_sw_unoapi \
 ))
 
 ifneq ($(DISABLE_GUI),TRUE)
