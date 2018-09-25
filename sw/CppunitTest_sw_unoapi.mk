9a89a75052c7327187cd5788dbaf8891a0a95cac
Vasily Melenchuk <vasily.melenchuk@cib.de>
2018-09-25 11:21:55 +0300

    sw: new unit test for XAutoTextGroup and rdf::URI
    Change-Id: I989ff797ddcd4ce77ac9b87fde2a2f8f75dbbc6b
    Reviewed-on: https://gerrit.libreoffice.org/60961
    Tested-by: Jenkins
    Reviewed-by: Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
    Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>

---
+++ sw/CppunitTest_sw_unoapi.mk
@@ -0,0 +1,62 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#*************************************************************************
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+#*************************************************************************
+
+$(eval $(call gb_CppunitTest_CppunitTest,sw_unoapi))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,sw_unoapi, \
+	sw/qa/unit/unoapi \
+))
+
+$(eval $(call gb_CppunitTest_use_libraries,sw_unoapi, \
+	comphelper \
+	cppu \
+	cppuhelper \
+	editeng \
+	i18nlangtag \
+	sal \
+	sfx \
+	svl \
+	svt \
+	sw \
+	test \
+	tl \
+	unotest \
+	utl \
+	vcl \
+))
+
+$(eval $(call gb_CppunitTest_use_externals,sw_unoapi,\
+	boost_headers \
+	libxml2 \
+))
+
+$(eval $(call gb_CppunitTest_set_include,sw_unoapi,\
+	-I$(SRCDIR)/sw/inc \
+	-I$(SRCDIR)/sw/source/core/inc \
+	-I$(SRCDIR)/sw/source/uibase/inc \
+	-I$(SRCDIR)/sw/qa/extras/inc \
+	$$(INCLUDE) \
+))
+
+$(eval $(call gb_CppunitTest_use_api,sw_unoapi,\
+	udkapi \
+	offapi \
+	oovbaapi \
+))
+
+$(eval $(call gb_CppunitTest_use_ure,sw_unoapi))
+$(eval $(call gb_CppunitTest_use_vcl,sw_unoapi))
+
+$(eval $(call gb_CppunitTest_use_rdb,sw_unoapi,services))
+
+$(eval $(call gb_CppunitTest_use_configuration,sw_unoapi))
+
+# vim: set noet sw=4 ts=4:
