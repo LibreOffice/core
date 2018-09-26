92bdc59ac61ca8f4315d37962c1bd4432684d0a2
Caolán McNamara <caolanm@redhat.com>
2018-09-26 10:53:14 +0100

    drop now unused SvxFontListBox
    Change-Id: I1ad6c5b98d7bd75c42e14177c417af6ad4ebbeda

--- include/svx/fontlb.hxx
+++ include/svx/fontlb.hxx
@@ -51,44 +51,6 @@ public:
                        const SvViewDataEntry* pView, const SvTreeListEntry& rEntry) override;
 };
 
-
-/** A list box supporting formatted string entries. */
-class SAL_WARN_UNUSED SVX_DLLPUBLIC SvxFontListBox : public SvTabListBox
-{
-private:
-    // The following members are used to store additional parameters for InitEntry().
-    vcl::Font                   maEntryFont;    /// Current entry font used in InitEntry().
-    const Color*                mpEntryColor;   /// Current entry color used in InitEntry().
-    bool                        mbUseFont;      /// true = Use maEntryFont/mpEntryColor in InitEntry().
-
-public:
-    SvxFontListBox(vcl::Window* pParent, WinBits nStyle);
-
-    /** Inserts a list entry and sets the font used for this entry.
-        @param pColor  The font color. NULL = use default listbox text color. */
-    void                        InsertFontEntry(
-                                    const OUString& rString, const vcl::Font& rFont, const Color* pColor = nullptr );
-
-    /** Selects an entry specified by its position in the list box. */
-    void                        SelectEntryPos( sal_uLong nPos );
-    /** Removes a selection. */
-    void                        SetNoSelection();
-
-    /** Returns the position of the entry currently selected or TREELIST_APPEND.
-    */
-    sal_uLong                   GetSelectedEntryPos() const;
-
-protected:
-    /** Initializes a new SvLBoxFontString entry.
-        @descr  Uses current value of maEntryFont to set the entry font (if mbUseFont is true). */
-    virtual void                InitEntry(
-                                    SvTreeListEntry* pEntry,
-                                    const OUString& rEntryText,
-                                    const Image& rCollImg,
-                                    const Image& rExpImg,
-                                    SvLBoxButtonKind eButtonKind) override;
-};
-
 #endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
