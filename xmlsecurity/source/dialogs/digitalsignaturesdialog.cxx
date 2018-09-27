89a60912bba7ffd6f65ea99f4664f343c5025c95
Cor Nouws <oolst@nouenoff.nl>
2018-09-27 21:53:12 +0200

    tdf#116085 make sure Certificate Manager is found with GPG4win v3
    Change-Id: I3a7ab7ec046a6bbff4b4a020e76356ffd454bab8
    Reviewed-on: https://gerrit.libreoffice.org/61061
    Tested-by: Jenkins
    Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>

--- xmlsecurity/source/dialogs/digitalsignaturesdialog.cxx
+++ xmlsecurity/source/dialogs/digitalsignaturesdialog.cxx
@@ -493,9 +493,11 @@ IMPL_STATIC_LINK(DigitalSignaturesDialog, CertMgrButtonHdl, Button*, pButton, vo
     // FIXME: call GpgME::dirInfo("bindir") somewhere in
     // SecurityEnvironmentGpg or whatnot
     // FIXME: perhaps poke GpgME for uiserver, and hope it returns something useful?
-    const OUString aGUIServers[] = { OUString("kleopatra.exe"), OUString("launch-gpa.exe"), OUString("gpa.exe"),
-                                     OUString("bin\\kleopatra.exe"), OUString("bin\\launch-gpa.exe"), OUString("bin\\gpa.exe") };
-    const char* const cPath = "C:\\Program Files (x86)\\GNU\\GnuPG";
+    const OUString aGUIServers[] = { OUString("Gpg4win\\kleopatra.exe"), OUString("GNU\\GnuPG\\kleopatra.exe"),
+                                     OUString("GNU\\GnuPG\\launch-gpa.exe"), OUString("GNU\\GnuPG\\gpa.exe"),
+                                     OUString("GNU\\GnuPG\\bin\\kleopatra.exe"), OUString("GNU\\GnuPG\\bin\\launch-gpa.exe"),
+                                     OUString("GNU\\GnuPG\\bin\\gpa.exe") };
+    const char* const cPath = "C:\\Program Files (x86)";
 #else
     const OUString aGUIServers[] = { OUString("kleopatra"), OUString("seahorse"),  OUString("gpa"), OUString("kgpg") };
     const char* cPath = getenv("PATH");
