185687b7796066493324ec117967409009b5d497
Caolán McNamara <caolanm@redhat.com>
2018-09-27 22:30:33 +0100

    wip: weld SaveDialog
    Change-Id: I80fdcb800642dbb850072a259d0f742e8c6cbb20

--- include/svtools/imagemgr.hxx
+++ include/svtools/imagemgr.hxx
@@ -121,6 +121,7 @@ private:
 public:
     SVT_DLLPUBLIC static OUString GetImageId( const INetURLObject& rURL );
     SVT_DLLPUBLIC static Image  GetImage( const INetURLObject& rURL, bool bBig = false );
+    SVT_DLLPUBLIC static OUString GetFileImageId( const INetURLObject& rURL );
     SVT_DLLPUBLIC static Image  GetFileImage( const INetURLObject& rURL );
     SVT_DLLPUBLIC static Image  GetImageNoDefault( const INetURLObject& rURL, bool bBig = false );
     SVT_DLLPUBLIC static Image  GetFolderImage( const svtools::VolumeInfo& rInfo );
