d0897cd1991c51bc7eecbc1c17dc8ed35069eaeb
Serge Krot <Serge.Krot@cib.de>
2018-09-28 16:02:34 +0200

    sw: new unit test for XCloseable
    Change-Id: I1c89abffcc985d6f5b2e0f570fcb82601d923b5e

--- sw/PythonTest_sw_python.mk
+++ sw/PythonTest_sw_python.mk
@@ -19,6 +19,7 @@ $(eval $(call gb_PythonTest_add_modules,sw_python,$(SRCDIR)/sw/qa/python,\
 	check_index \
 	check_flies \
 	check_fields \
+	check_xcloseable \
 	check_cross_references \
 	check_named_property_values \
 	check_indexed_property_values \
