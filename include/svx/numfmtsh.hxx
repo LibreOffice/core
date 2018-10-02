88209081a0eae5d273baa710c8083ccabaf47f57
Eike Rathke <erack@redhat.com>
2018-10-02 20:11:15 +0200

    Related: tdf#119613 tdf#118127 suppress duplicated builtins, rework All cat
    In number formatter dialog:
    
    Some locales may define duplicated formats, specifically date
    formats, for all builtin formats <50 must be present and default
    formats may be equal to ISO formats and what not. Try to suppress
    duplicated formats, preferring those that are essential (i.e.
    used as current, default or edit formats). This is done only per
    category, not in the All category.
    
    Example is [en-ZA] English (South Africa) that now has ISO 8601
    also as default date(+time) formats.
    
    The All category still listed the since long deprecated old
    currency formats including the worrying CCC format that follows
    the current default currency. Eliminate that crap, unless one
    matches the current format.
    
    Also, additional formats defined in locale data and user defined
    formats were never listed in the All category, do that.
    
    Change-Id: I6e6b9bfecc34d51e89930f348f7afbe5e3c41738

--- include/svx/numfmtsh.hxx
+++ include/svx/numfmtsh.hxx
@@ -197,15 +197,20 @@ private:
     bool                    bIsDefaultValNum;
 
     SVX_DLLPRIVATE short FillEntryList_Impl( std::vector<OUString>& rList );
-    SVX_DLLPRIVATE void  FillEListWithStd_Impl( std::vector<OUString>& rList,sal_uInt16 aPrivCat, short &Pos);
+    SVX_DLLPRIVATE void  FillEListWithStd_Impl( std::vector<OUString>& rList, SvNumFormatType eCategory, short &Pos,
+                                                bool bSuppressDuplicates = false );
     SVX_DLLPRIVATE short FillEListWithFormats_Impl( std::vector<OUString>& rList,short nSelPos,
                                                        NfIndexTableOffset eOffsetStart,
-                                                       NfIndexTableOffset eOffsetEnd);
-    SVX_DLLPRIVATE short FillEListWithDateTime_Impl( std::vector<OUString>& rList,short nSelPos);
+                                                       NfIndexTableOffset eOffsetEnd,
+                                                       bool bSuppressDuplicates );
+    SVX_DLLPRIVATE short FillEListWithDateTime_Impl( std::vector<OUString>& rList,short nSelPos,
+                                                     bool bSuppressDuplicates );
     SVX_DLLPRIVATE short FillEListWithCurrency_Impl( std::vector<OUString>& rList,short nSelPos);
     SVX_DLLPRIVATE short FillEListWithSysCurrencys( std::vector<OUString>& rList,short nSelPos);
     SVX_DLLPRIVATE short FillEListWithUserCurrencys( std::vector<OUString>& rList,short nSelPos);
-    SVX_DLLPRIVATE short FillEListWithUsD_Impl( std::vector<OUString>& rList, sal_uInt16 nPrivCat, short Pos );
+    SVX_DLLPRIVATE short FillEListWithUsD_Impl( std::vector<OUString>& rList, SvNumFormatType eCategory, short Pos );
+
+    SVX_DLLPRIVATE bool IsEssentialFormat_Impl( SvNumFormatType eType, sal_uInt32 nKey );
 
     SVX_DLLPRIVATE ::std::vector<sal_uInt32>::iterator GetRemoved_Impl( size_t nKey );
     SVX_DLLPRIVATE bool                                IsRemoved_Impl( size_t nKey );
