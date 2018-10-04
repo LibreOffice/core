28a962ff1566a15906c289f586ab8fcbcfb4d1ee
Stephan Bergmann <sbergman@redhat.com>
2018-10-04 10:27:54 +0200

    Avoid -Werror=stringop-truncation
    ... "‘strncpy’ output truncated before terminating nul copying as many bytes
    from a string as its length", as reported at
    <https://lists.freedesktop.org/archives/libreoffice/2018-October/081109.html>
    "LO build fails -Werror=stringop-truncation in _lex.c".  Not adding the
    terminating NUL appears to be intentional here, as the s->inp buffer is
    terminated (through s->inl) with EOB bytes after the if/else blocks.
    
    Change-Id: I5a8559e620b7e34ee27cbcd0f836432deb8cba90
    Reviewed-on: https://gerrit.libreoffice.org/61355
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

--- soltools/cpp/_lex.c
+++ soltools/cpp/_lex.c
@@ -668,7 +668,7 @@ Source *
         len = strlen(str);
         s->inb = domalloc(len + 4);
         s->inp = s->inb;
-        strncpy((char *) s->inp, str, len);
+        memcpy((char *) s->inp, str, len);
     }
     else
     {
