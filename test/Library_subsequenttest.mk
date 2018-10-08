0583fed337f770b8f51e989f6004a5baffca7839
Rahul Gurung <gurungrahul2@gmail.com>
2018-10-08 20:39:08 +0530

    tdf#45904 Move _XTableChartsSupplier Java Tests to C++
    Change-Id: I4cd0b25cefce60e8253df65d5136d15d690dbc38

--- test/Library_subsequenttest.mk
+++ test/Library_subsequenttest.mk
@@ -150,6 +150,7 @@ $(eval $(call gb_Library_add_exception_objects,subsequenttest,\
 	test/source/table/xcell \
 	test/source/table/xcellcursor \
 	test/source/table/xcolumnrowrange \
+	test/source/table/xtablechartssupplier \
 	test/source/text/xtext \
 	test/source/text/xtextfield \
 	test/source/text/xtextcontent \
