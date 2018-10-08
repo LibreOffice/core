18c628ae6ef2664db40ef46e21064f3ac4713e5c
Jan-Marek Glogowski <glogow@fbihome.de>
2018-10-08 10:39:57 +0000

    Fix emfio_wmf_test VCL dependency
    This unit test sometimes fails on Windows with the message:
    "no suitable windowing system found, exiting."
    
    Currently the test just depends on desktop_detector and manually
    sets up VCL duplicating a lot of code and even library symbols.
    The original Gerrit change describes a lot of failures in its
    comments (change 28322, version 16). Now that Windows and MacOS
    were converted to VCL plugins, depending on desktop_detector is
    not sufficient anymore.
    
    This reverts a lot of VCL test setup from commit 20f6a6b159c6
    ("tdf#99402: fix Metafile Font handling"). Seems now a simple
    gb_CppunitTest_use_vcl is enough.
    
    Change-Id: Ia941ded91358231021741ff4a5d96e2014ac6c80
    Reviewed-on: https://gerrit.libreoffice.org/61497
    Tested-by: Jenkins
    Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>

--- emfio/CppunitTest_emfio_wmf_test.mk
+++ emfio/CppunitTest_emfio_wmf_test.mk
@@ -17,176 +17,20 @@ $(eval $(call gb_CppunitTest_set_include,emfio_wmf,\
 $(eval $(call gb_CppunitTest_use_externals,emfio_wmf,\
     boost_headers \
     libxml2 \
-    $(if $(filter PDFIUM,$(BUILD_TYPE)),pdfium) \
 ))
 
 $(eval $(call gb_CppunitTest_add_exception_objects,emfio_wmf, \
     emfio/qa/cppunit/wmf/wmfimporttest \
 ))
 
-$(eval $(call gb_CppunitTest_use_sdk_api,emfio_wmf))
-
 $(eval $(call gb_CppunitTest_use_libraries,emfio_wmf,\
-    $(call gb_Helper_optional,BREAKPAD, \
-               crashreport) \
-    basegfx \
-    drawinglayer \
-    cppu \
-    cppuhelper \
-    comphelper \
     emfio \
-    i18nlangtag \
-    i18nutil \
-    $(if $(filter OPENCL,$(BUILD_TYPE)),opencl) \
     sal \
-    salhelper \
-    sot \
-    svl \
-    svt \
     test \
-    test-setupvcl \
     tl \
-    ucbhelper \
     unotest \
     vcl \
-    utl \
-    xmlreader \
 ))
-
-$(eval $(call gb_CppunitTest_use_externals,emfio_wmf,\
-    $(if $(filter LINUX MACOSX %BSD SOLARIS,$(OS)), \
-            curl) \
-    libjpeg \
-    $(if $(filter-out IOS WNT,$(OS)), \
-            nss3 \
-            plc4) \
-    libeot \
-))
-
-ifeq ($(OS),MACOSX)
-$(eval $(call gb_CppunitTest_add_libs,emfio_wmf,\
-    -framework IOKit \
-    -F/System/Library/PrivateFrameworks \
-    -framework CoreUI \
-    -lobjc \
-))
-endif
-
-ifeq ($(ENABLE_JAVA),TRUE)
-$(eval $(call gb_CppunitTest_use_libraries,emfio_wmf,\
-    jvmaccess \
-))
-endif
-
-$(eval $(call gb_CppunitTest_use_externals,emfio_wmf,\
-    gio \
-    graphite \
-    harfbuzz \
-    icuuc \
-    lcms2 \
-))
-ifeq ($(DISABLE_GUI),)
-$(eval $(call gb_CppunitTest_use_externals,emfio_wmf,\
-     epoxy \
- ))
-endif
-
-ifeq ($(OS),MACOSX)
-$(eval $(call gb_CppunitTest_use_system_darwin_frameworks,emfio_wmf,\
-    ApplicationServices \
-))
-$(eval $(call gb_CppunitTest_use_system_darwin_frameworks,emfio_wmf,\
-    $(if $(filter X86_64,$(CPUNAME)),,QuickTime) \
-    Cocoa \
-    Carbon \
-    CoreFoundation \
-))
-ifneq ($(ENABLE_MACOSX_SANDBOX),TRUE)
-$(eval $(call gb_CppunitTest_use_libraries,emfio_wmf,\
-    AppleRemote \
-))
-endif
-endif
-
-ifeq ($(USING_X11),TRUE)
-$(eval $(call gb_CppunitTest_use_externals,emfio_wmf,\
-    cairo \
-    cups \
-    dbus \
-    fontconfig \
-    freetype \
-    valgrind \
-))
-endif
-
-ifeq ($(DISABLE_GUI),TRUE)
-$(eval $(call gb_CppunitTest_use_externals,emfio_wmf,\
-    cairo \
-    freetype \
-    fontconfig \
-))
-else
-ifeq ($(OS),LINUX)
-$(eval $(call gb_CppunitTest_add_libs,emfio_wmf,\
-    -lm \
-    -ldl \
-    -lpthread \
-    -lX11 \
-))
-endif
-endif
-
-ifeq ($(OS),ANDROID)
-$(eval $(call gb_CppunitTest_add_libs,emfio_wmf,\
-    -llog \
-    -landroid \
-    -llo-bootstrap \
-))
-$(eval $(call gb_CppunitTest_use_externals,emfio_wmf,\
-    cairo \
-    fontconfig \
-    freetype \
-    expat \
-))
-endif
-
-ifeq ($(OS),IOS)
-$(eval $(call gb_CppunitTest_use_system_darwin_frameworks,emfio_wmf,\
-    UIKit \
-    CoreFoundation \
-))
-endif
-
-ifeq ($(OS),WNT)
-$(eval $(call gb_CppunitTest_use_system_win32_libs,emfio_wmf,\
-    advapi32 \
-    crypt32 \
-    gdi32 \
-    gdiplus \
-    imm32 \
-    mpr \
-    ole32 \
-    shell32 \
-    usp10 \
-    uuid \
-    version \
-    winspool \
-    setupapi \
-    shlwapi \
-))
-#$(eval $(call gb_CppunitTest_add_nativeres,emfio,vcl/salsrc))
-endif
-
-ifeq ($(OS), $(filter LINUX %BSD SOLARIS, $(OS)))
-$(eval $(call gb_CppunitTest_add_libs,emfio_wmf,\
-    -lm $(DLOPEN_LIBS) \
-    -lpthread \
-    -lX11 \
-    -lXext \
-))
-endif
-
-$(eval $(call gb_CppunitTest_use_ure,emfio_wmf))
 
 $(eval $(call gb_CppunitTest_use_components,emfio_wmf,\
     configmgr/source/configmgr \
@@ -197,18 +41,8 @@ $(eval $(call gb_CppunitTest_use_components,emfio_wmf,\
 ))
 
 $(eval $(call gb_CppunitTest_use_configuration,emfio_wmf))
-
-# See gb_CppunitTest__use_vcl (solenv/gbuild/CppunitTest.mk; headless):
-ifeq ($(USING_X11),TRUE)
-$(call gb_CppunitTest_get_target,emfio_wmf): \
-    $(call gb_Library_get_target,desktop_detector)
-endif
-
-# Hack to suppress ASan ODR violation warnings about symbols present in both the
-# vcl objects linked into this test library and the vcl library (which gets
-# dynamically loaded during the test):
-$(call gb_CppunitTest_get_target,emfio_wmf): \
-    EXTRA_ENV_VARS := \
-        ASAN_OPTIONS="$${ASAN_OPTIONS+$$ASAN_OPTIONS:}"detect_odr_violation=0
+$(eval $(call gb_CppunitTest_use_sdk_api,emfio_wmf))
+$(eval $(call gb_CppunitTest_use_ure,emfio_wmf))
+$(eval $(call gb_CppunitTest_use_vcl,emfio_wmf))
 
 # vim: set noet sw=4 ts=4:
