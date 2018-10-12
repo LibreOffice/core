60962fac81581a340c6c12cc94f6905f05e27df4
Armin Le Grand <Armin.Le.Grand@cib.de>
2018-10-12 11:13:09 +0200

    WIP Refactor calc non-linear ViewToDevice transform
    CAUTION! This is WORK IN PROGRESS and not finished yet.
    DO NOT INTEGRATE YET.
    All palces that need revision are marked using '//Z'
    
    First step: Add interfaces to get a possible GridOffset
    at ViewObjectContact. There it belongs, we have a view-
    dependent offset per object and view. Add mechanisms to
    create on-demand and reach back to the view (aka calc's
    derivation of it)
    
    Second step: Implement the on-demand creation, adapt to
    use it in ViewObjectContact::getPrimitive2DSequence, add
    stuff to reset on zoom change, disable temporarily old
    mechanism -> paint already works. Need to adapt the
    places from old mechanism where the GridOffset was used,
    but no longer the geometry creations.
    
    Third step: Isolated and disabled old mechanism (by
    already removing SetGridOffset). Marked all places that
    possibly need change with '//Z' tag. Main work now will
    be to adapt in the SdrView implementations in svx to know
    about having a SdrObject-dependent ViewTransformation
    at all (currently not known, was hard-coded at some places
    from the old code, ViewTransformation set as MapMode at
    a target OutputDevice, not member at SdrView at all...)
    
    Fourth step: Adapt the Handles and OverlayObjects to
    use an evtl. existing GridOffset. The mechanism is that
    the SdrHdl(s) can be seen as Model-Objects, these get
    converted to OverlayObjects in the ::CreateB2dIAObject()
    implementations, for all SdrMarkView and SdrPageView,
    so this is the place where the ObjectContact is known
    (the SdrPageWindow *is* a ObjectContact) and the view-
    dependent GridOffset can be calculated per-SdrObject.
    I modified OverlayObject to be able to work with a
    set Offset that embeds the created visualization using
    this additionally.
    Handles get now correctly set and have a working HitTest
    (due to that already using the primitives). Some inter-
    action stuff already working, some will need more
    adaption. We simply have no concept for this stuff...
    Refactored to not get dependencies to SdrObject in
    ObjectContact
    
    Fifth Step: Make HitTest work by adding the View-And-
    Object dependent GridOffset in the View when HitTest
    is triggered. This is in SdrMarkView::CheckSingleSdrObjectHit
    where pObj->GetCurrentBoundRect() is used that gets the
    view-independent form. To make HitTest work, add a possible
    GridOffset.
    Since this will be necessary more often in SdrView hierarchy,
    added a tooling method (getPossibleGridOffsetForSdrObject)
    at that level after checking that at that level will be
    reachable at all potential spots.
    Inside that method the correct ObjectContact will be identified
    and the object-specific offset requested there.
    
    Change-Id: Ia76c06901ad745d48bf1b5009905fbe517ade5ac

--- include/svx/svdmrkv.hxx
+++ include/svx/svdmrkv.hxx
@@ -109,7 +109,7 @@ protected:
     sdr::ViewSelection          maSdrViewSelection;
 
     tools::Rectangle            maMarkedObjRect;
-    tools::Rectangle            maMarkedObjRectNoOffset;
+//Z    tools::Rectangle            maMarkedObjRectNoOffset;
     tools::Rectangle            maMarkedPointsRect;
     tools::Rectangle            maMarkedGluePointsRect;
 
@@ -129,6 +129,12 @@ protected:
 
     // flag to completely disable handles at the view
     bool                        mbMarkHandlesHidden : 1;
+
+    // Helper to get the possible SdrObject-specific GridOffset from ObjectContact
+    bool getPossibleGridOffsetForSdrObject(
+        basegfx::B2DVector& rOffset,
+        const SdrObject* pObj,
+        const SdrPageView* pPV) const;
 
 private:
     SVX_DLLPRIVATE void ImpClearVars();
@@ -402,7 +408,9 @@ public:
     const tools::Rectangle& GetMarkedGluePointsRect() const; // Enclosing rectangle of all marked glue points
     const tools::Rectangle& GetAllMarkedRect() const { return GetMarkedObjRect(); }
     tools::Rectangle GetAllMarkedBoundRect() const { return GetMarkedObjBoundRect(); }
-    Point GetGridOffset() const;
+
+//Z    Point GetGridOffset() const;
+    const Point& GetGridOffsetForView() const;
 
     // Will be always called, if the list of marked objects might be changed.
     // If you override this method, be sure that you call the
