3460e91ac853964015215ec75be7cf63361ca0e9
Luboš Luňák <l.lunak@collabora.com>
2018-10-12 13:42:17 +0200

    blacklist svExternalName for calc threading
    It's the same like svExternal(Single|Double)Ref, there's a lot of thread-unsafe
    code involved.
    
    Change-Id: Ief8bbbd9393788c374215412ee5236ce6bb551fa
    Reviewed-on: https://gerrit.libreoffice.org/63176
    Reviewed-by: Michael Meeks <michael.meeks@collabora.com>
    Tested-by: Jenkins

--- sc/source/core/tool/token.cxx
+++ sc/source/core/tool/token.cxx
@@ -1333,6 +1333,7 @@ void ScTokenArray::CheckForThreading( const FormulaToken& r )
         {
             case svExternalDoubleRef:
             case svExternalSingleRef:
+            case svExternalName:
                 SAL_INFO("sc.core.formulagroup", "opcode ocPush: variable type " << StackVarEnumToString(r.GetType())
                     << " disables threaded calculation of formula group");
                 mbThreadingEnabled = false;
