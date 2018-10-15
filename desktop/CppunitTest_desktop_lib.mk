d2242cb1b88cc7d7af1990965925d19557b9f347
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2018-10-15 10:15:32 +0200

    lokit: test for the new signing API functions
    Change-Id: I3ab2f3986955eb82451704fc2386b64f208da554
    Reviewed-on: https://gerrit.libreoffice.org/61782
    Tested-by: Jenkins
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

--- desktop/CppunitTest_desktop_lib.mk
+++ desktop/CppunitTest_desktop_lib.mk
@@ -29,6 +29,7 @@ $(eval $(call gb_CppunitTest_use_libraries,desktop_lib, \
 	test \
 	unotest \
 	utl \
+	tl \
 	vcl \
 ))
 
@@ -48,37 +49,7 @@ $(eval $(call gb_CppunitTest_use_ure,desktop_lib))
 
 $(eval $(call gb_CppunitTest_use_vcl,desktop_lib))
 
-$(eval $(call gb_CppunitTest_use_components,desktop_lib,\
-    comphelper/util/comphelp \
-    configmgr/source/configmgr \
-    dtrans/util/mcnttype \
-    filter/source/config/cache/filterconfig1 \
-    filter/source/storagefilterdetect/storagefd \
-    framework/util/fwk \
-    i18npool/util/i18npool \
-    package/source/xstor/xstor \
-    package/util/package2 \
-    sax/source/expatwrap/expwrap \
-    sfx2/util/sfx \
-    svl/source/fsstor/fsstorage \
-    svtools/util/svt \
-    sw/util/sw \
-    sw/util/swd \
-    sc/util/sc \
-    sc/util/scd \
-    sd/util/sd \
-    sd/util/sdd \
-    toolkit/util/tk \
-    ucb/source/core/ucb1 \
-    ucb/source/ucp/file/ucpfile1 \
-    unoxml/source/service/unoxml \
-    uui/util/uui \
-    vcl/vcl.common \
-    xmloff/util/xo \
-    i18npool/source/search/i18nsearch \
-    filter/source/graphic/graphicfilter \
-    linguistic/source/lng \
-))
+$(eval $(call gb_CppunitTest_use_rdb,desktop_lib,services))
 
 $(eval $(call gb_CppunitTest_use_configuration,desktop_lib))
 
