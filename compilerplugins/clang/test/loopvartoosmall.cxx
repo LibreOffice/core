f4e4231286a2e914e3d37f41db330a9680400047
Tamás Zolnai <tamas.zolnai@collabora.com>
2018-10-15 14:55:30 +0200

    Relicense LoopVarTooSmall plugin to use LLVM license
    Change-Id: I9e8cf6d7c2474f8c4c624dd9040890997c43f788
    Reviewed-on: https://gerrit.libreoffice.org/61789
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>
    Reviewed-by: Tamás Zolnai <tamas.zolnai@collabora.com>
    Tested-by: Tamás Zolnai <tamas.zolnai@collabora.com>

--- compilerplugins/clang/test/loopvartoosmall.cxx
+++ compilerplugins/clang/test/loopvartoosmall.cxx
@@ -2,10 +2,12 @@
 /*
  * This file is part of the LibreOffice project.
  *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- */
+ * Based on LLVM/Clang.
+ *
+ * This file is distributed under the University of Illinois Open Source
+ * License. See LICENSE.TXT for details.
+ *
+*/
 
 #include <cstdint>
 
