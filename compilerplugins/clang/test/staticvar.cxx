6ee4375763854e43e549aee5a35520def2e215a2
Noel Grandin <noel.grandin@collabora.co.uk>
2018-10-16 10:40:16 +0200

    new loplugin staticvar
    looks for variables that can be declared const and static i.e. they can
    be stored in the read-only linker segment and shared between different
    processes
    
    Change-Id: I577fb2070604003e56fb44f8a02c9684070311cf
    Reviewed-on: https://gerrit.libreoffice.org/61817
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

---
+++ compilerplugins/clang/test/staticvar.cxx
@@ -0,0 +1,80 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <config_clang.h>
+#include <sal/config.h>
+#include <rtl/ustring.hxx>
+
+struct S1
+{
+    int x, y;
+};
+
+S1 const& f1(int a)
+{
+    static S1 s1[]{
+        // expected-error@-1 {{var should be static const, or whitelisted [loplugin:staticvar]}}
+        { 1, 1 }
+    };
+    // no warning expected
+    const S1 s2[]{ { a, 1 } };
+    (void)s2;
+    return s1[0];
+}
+
+struct S2
+{
+    OUString x;
+};
+
+#if CLANG_VERSION >= 50000 // probably something to do with how OUString initialisers work
+S2 const& f2()
+{
+    static S2 const s1[]{
+        // expected-error@-1 {{static const var requires runtime initialization? [loplugin:staticvar]}}
+        { "xxx" }
+    };
+    return s1[0];
+}
+#endif
+
+// no warning expected
+S2 const& f3()
+{
+    static S2 s1[]{ { "xxx" } };
+    return s1[0];
+}
+
+// no warning expected
+struct S4
+{
+    OUStringLiteral const cName;
+    bool const bCanBeVisible;
+};
+S4 const& f4()
+{
+    static const S4 s1[] = {
+        { OUStringLiteral("/DocColor"), false },
+    };
+    return s1[0];
+}
+
+struct S5
+{
+    bool const bCanBeVisible;
+};
+void f5(bool b)
+{
+    const S5 s1[] = {
+        { b },
+    };
+    (void)s1;
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
