LibreOffice/core

commit 0f4b3cb7d3d68906de316a64dcec281da2a641bd

if we have a number formatter then use it, dammit..
Change-Id: I2d70bed4d9cf6f0ef4556a327c51fc5c10fcd2be

chart2/source/tools/DiagramHelper.cxx
@@ -1243,16 +1243,25 @@ sal_Int32 DiagramHelper::getDateTimeInputNumberFormat( const Reference< util::XN
 sal_Int32 DiagramHelper::getPercentNumberFormat( const Reference< util::XNumberFormatsSupplier >& xNumberFormatsSupplier )
 {
     sal_Int32 nRet=-1;
-    Reference< util::XNumberFormats > xNumberFormats( xNumberFormatsSupplier->getNumberFormats() );
-    if( xNumberFormats.is() )
+    const LanguageTag& rLanguageTag = Application::GetSettings().GetLanguageTag();
+    NumberFormatterWrapper aNumberFormatterWrapper( xNumberFormatsSupplier );
+    SvNumberFormatter* pNumFormatter = aNumberFormatterWrapper.getSvNumberFormatter();
+    if( pNumFormatter )
     {
-        bool bCreate = true;
-        const LocaleDataWrapper& rLocaleDataWrapper = Application::GetSettings().GetLocaleDataWrapper();
-        Sequence<sal_Int32> aKeySeq = xNumberFormats->queryKeys( util::NumberFormat::PERCENT,
-            rLocaleDataWrapper.getLanguageTag().getLocale(), bCreate );
-        if( aKeySeq.getLength() )
+        nRet = pNumFormatter->GetStandardFormat( util::NumberFormat::PERCENT, rLanguageTag.getLanguageType() );
+    }
+    else
+    {
+        Reference< util::XNumberFormats > xNumberFormats( xNumberFormatsSupplier->getNumberFormats() );
+        if( xNumberFormats.is() )
         {
-            nRet = aKeySeq[0];
+            bool bCreate = true;
+            Sequence<sal_Int32> aKeySeq = xNumberFormats->queryKeys( util::NumberFormat::PERCENT,
+                    rLanguageTag.getLocale(), bCreate );
+            if( aKeySeq.getLength() )
+            {
+                nRet = aKeySeq[0];
+            }
         }
     }
     return nRet;

commit 19af4ffa1420ff8e88d00330de642ddb36e8de2e

Resolves: tdf#115182 default Chart percentage format is integer
Regression from

    commit 0f4b3cb7d3d68906de316a64dcec281da2a641bd
    CommitDate: Mon Aug 10 17:12:45 2015 +0200

Change-Id: Ib0c2c862af9399ab2af5178d01510aa5e4fff97c

chart2/source/tools/DiagramHelper.cxx
@@ -1182,7 +1182,7 @@ sal_Int32 DiagramHelper::getPercentNumberFormat( const Reference< util::XNumberF
     SvNumberFormatter* pNumFormatter = aNumberFormatterWrapper.getSvNumberFormatter();
     if( pNumFormatter )
     {
-        nRet = pNumFormatter->GetStandardFormat( SvNumFormatType::PERCENT, rLanguageTag.getLanguageType() );
+        nRet = pNumFormatter->GetFormatIndex( NF_PERCENT_INT, rLanguageTag.getLanguageType() );
     }
     else
     {
@@ -1193,6 +1193,10 @@ sal_Int32 DiagramHelper::getPercentNumberFormat( const Reference< util::XNumberF
                     rLanguageTag.getLocale(), true/*bCreate*/ );
             if( aKeySeq.getLength() )
             {
+                // This *assumes* the sequence is sorted as in
+                // NfIndexTableOffset and the first format is the integer 0%
+                // format by chance.. which usually is the case, but.. anyway,
+                // we usually also have a number formatter so don't reach here.
                 nRet = aKeySeq[0];
             }
         }