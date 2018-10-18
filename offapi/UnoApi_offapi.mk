db4b4bdfc9b5dd7c4110668897a5eeb1910fc31a
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2018-10-18 10:27:16 +0200

    xmlsecurity: UNO interface to create cert. and private key
    new XCertificateCreator interface which can be used to create a
    certificate and a associated private key, both in DER format
    
    Change-Id: I24caf053f1f2b7357a836047b88b4ce418470eac
    Reviewed-on: https://gerrit.libreoffice.org/61912
    Tested-by: Jenkins
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>
    (cherry picked from commit bdde44d5525caf2240a6ea900336ac973121f145)

--- offapi/UnoApi_offapi.mk
+++ offapi/UnoApi_offapi.mk
@@ -4226,6 +4226,7 @@ $(eval $(call gb_UnoApi_add_idlfiles,offapi,com/sun/star/xml/crypto,\
 	CipherID \
 	DigestID \
 	SecurityOperationStatus \
+	XCertificateCreator \
 	XCipherContext \
 	XCipherContextSupplier \
 	XDigestContext \
