b18634ee8928ea23081451544d3e42e238caf6c9
Jan-Marek Glogowski <glogow@fbihome.de>
2018-10-18 13:40:09 +0000

    Fix NSS parallel build
    This is just a minimal solution and obviously not fully parallel.
    We still have to walk the tree twice:
    1. copy all needed headers to dist (export export_private)
    2. the parallel build (libs)
    
    The fixes are:
    * Add a few rm and @$(MAKE_OBJDIR) calls to various targets
    * Serialize minimal steps in nss_build_all
      => Build libs of nss/lib and nss/cmd/lib before all other
      => drop the minimal all target
    * Make $(SHARED_LIBRARY) depend on $(LIBRARY), otherwise dll lib
      creation on Windows builds a static lib, instead of the dll one
    * Drop all "export:: private_export" rules. We run this before
      the build and duplicate targets break the header install
    
    Change-Id: I1c35985c90d2d63990e04a0d2bb9d8ae921a2182

--- external/nss/ExternalProject_nss.mk
+++ external/nss/ExternalProject_nss.mk
@@ -23,7 +23,7 @@ $(call gb_ExternalProject_get_state_target,nss,build): $(call gb_ExternalExecuta
 		$(if $(filter X86_64,$(CPUNAME)),USE_64=1) \
 		LIB="$(ILIB)" \
 		XCFLAGS="-arch:SSE $(SOLARINC)" \
-		$(MAKE) -j1 nss_build_all RC="rc.exe $(SOLARINC)" \
+		$(MAKE) nss_build_all RC="rc.exe $(SOLARINC)" \
 			NSINSTALL='$(call gb_ExternalExecutable_get_command,python) $(SRCDIR)/external/nss/nsinstall.py' \
 	,nss)
 
@@ -45,7 +45,7 @@ $(call gb_ExternalProject_get_state_target,nss,build): $(call gb_ExternalExecuta
 			$(if $(filter iOS-ARM,$(OS)-$(CPUNAME)),CPU_ARCH=arm) \
 			NSPR_CONFIGURE_OPTS="--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM)") \
 		NSDISTMODE=copy \
-		$(MAKE) -j1 AR="$(AR)" \
+		$(MAKE) AR="$(AR)" \
 			RANLIB="$(RANLIB)" \
 			NMEDIT="$(NM)edit" \
 			COMMA=$(COMMA) \
