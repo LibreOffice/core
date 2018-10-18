54234d8898566b122fa7980cc18af29a93fba3be
Stephan Bergmann <sbergman@redhat.com>
2018-10-18 21:34:11 +0200

    Turn ScGridWin::AutoFilterMode into scoped enum
    ...to avoid -Werror,-Wshadow from Clang trunk with
    <https://reviews.llvm.org/D52400> "Improve -Wshadow warnings with enumerators",
    warning about shadowing of SortAscending/Descending in enum SvSortMode in
    include/svtools/treelist.hxx
    
    Change-Id: I7022d3c6338c5ff50edf6362577f89f6609cd3e9

--- sc/source/ui/inc/gridwin.hxx
+++ sc/source/ui/inc/gridwin.hxx
@@ -300,7 +300,7 @@ protected:
     virtual void    StartDrag( sal_Int8 nAction, const Point& rPosPixel ) override;
 
 public:
-    enum AutoFilterMode { Normal, Top10, Custom, Empty, NonEmpty, SortAscending, SortDescending };
+    enum class AutoFilterMode { Normal, Top10, Custom, Empty, NonEmpty, SortAscending, SortDescending };
 
     ScGridWindow( vcl::Window* pParent, ScViewData* pData, ScSplitPos eWhichPos );
     virtual ~ScGridWindow() override;
