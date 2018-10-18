c4c57f3a46651236b52ef3abfeeaa47449a5d7a1
Tor Lillqvist <tml@collabora.com>
2018-10-18 14:30:42 +0300

    Introduce UnixErrnoString() and use it in sal/osl/unx
    The UnixErrnoString() function returns the symbolic name of an errno
    value, like "ENOENT". For now this is local to sal/osl/unx.
    
    If it can't figure out the symbolic name, it returns it as a number
    followed by the cleartext description (as from strerror()) in
    parentheses.
    
    Rationale why to use this and not strerror(): This is intended to be
    used in SAL_INFO() and SAL_WARN(). Such messages are intended to be
    read by developers, not end-users. Developers are (or should be)
    familiar with symbolic errno names in code anyway. The symbolic names
    of errno values are (or should be) instantly recognizable as such,
    they all start with E and are in UPPERCASE.
    
    strerror() can be localised although in LibreOffice it apparently
    isn't as there allegedly aren't setlocale() calls. But, anyway, the
    error strings might be less familiar to a developer than the symbolc
    errno names that one uses when coding.
    
    When encountering an unfamiliar error string the developer might want
    to add special handling for that error case in the code. They would
    need a reverse mapping from error string to errno value, by manually
    searching <errno.h>, looking at the comments there, hoping the match
    what strerror() produces, to find the corresponding symbolic errno
    value.
    
    Change-Id: Idc11595d528e8432a32bf474e6791f4ea7262a1e

---
+++ sal/osl/unx/unixerrnostring.hxx
@@ -0,0 +1,27 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef INCLUDED_SAL_OSL_UNX_UNIXERRNOSTRING_HXX
+#define INCLUDED_SAL_OSL_UNX_UNIXERRNOSTRING_HXX
+
+#include <string>
+
+// Return the symbolic name of an errno value, like "ENOENT".
+
+// Rationale why to use this and not strerror(): This is intended to be used in SAL_INFO() and
+// SAL_WARN(). Such messages are intended to be read by developers, not end-users. Developers are
+// (or should be) familiar with symbolic errno names in code anyway. strerror() is localized and the
+// localised error strings might be less familiar to a developer that happens to run a localised
+// environment.
+
+std::string UnixErrnoString(int nErrno);
+
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
