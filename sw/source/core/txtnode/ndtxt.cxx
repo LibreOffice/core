ff21c9e264215e550678b36f36fb808acc13508d
Michael Stahl <Michael.Stahl@cib.de>
2018-10-19 15:08:01 +0200

    sw_redlinehide_3: add flag to SwNodeNum
    Disable the NumRule/DocumentListsItemManager manipulations on the
    second tree; only the "main" tree does that.
    
    Change-Id: I0da8ced53f8d55758e3c02fd24b9253bbf603b38

--- sw/source/core/txtnode/ndtxt.cxx
+++ sw/source/core/txtnode/ndtxt.cxx
@@ -3961,7 +3961,7 @@ SwNodeNum* SwTextNode::CreateNum() const
 {
     if ( !mpNodeNum )
     {
-        mpNodeNum = new SwNodeNum( const_cast<SwTextNode*>(this) );
+        mpNodeNum = new SwNodeNum( const_cast<SwTextNode*>(this), false );
     }
     return mpNodeNum;
 }
@@ -4258,7 +4258,7 @@ void SwTextNode::AddToList()
     if (pList)
     {
         assert(!mpNodeNum);
-        mpNodeNum = new SwNodeNum(this);
+        mpNodeNum = new SwNodeNum(this, false);
         pList->InsertListItem(*mpNodeNum, false, GetAttrListLevel());
     }
 }
