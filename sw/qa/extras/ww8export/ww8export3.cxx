dc0112fc560858d3ff555fc02befd5455706388d
Justin Luth <justin.luth@collabora.com>
2018-10-20 19:18:02 +0300

    tdf79435 doc/docx: round-trip legacy input formfields
    GrabBag the settings which LO doesn't implement
    (which is all of them) so that the document
    round-trips without losing the config settings.
    
    Change-Id: I00de6c483af68073634430dd74fd445e981573ab

--- sw/qa/extras/ww8export/ww8export3.cxx
+++ sw/qa/extras/ww8export/ww8export3.cxx
@@ -11,6 +11,7 @@
 
 #include <com/sun/star/beans/XPropertySet.hpp>
 #include <com/sun/star/container/XIndexAccess.hpp>
+#include <com/sun/star/text/XFormField.hpp>
 #include <com/sun/star/text/XTextFramesSupplier.hpp>
 #include <com/sun/star/text/XTextTable.hpp>
 #include <com/sun/star/text/XTextTablesSupplier.hpp>
@@ -31,6 +32,58 @@ public:
         return OString(filename).endsWith(".doc");
     }
 };
+
+DECLARE_WW8EXPORT_TEST(testTdf79435_legacyInputFields, "tdf79435_legacyInputFields.docx")
+{
+    //using .docx input file to verify cross-format compatability.
+    uno::Reference<text::XFormField> xFormField;
+    xFormField = getProperty< uno::Reference<text::XFormField> >(getRun(getParagraph(5), 3), "Bookmark");
+    uno::Reference<container::XNameContainer> xParameters(xFormField->getParameters());
+
+    OUString sTmp;
+    xParameters->getByName("EntryMacro") >>= sTmp;
+    CPPUNIT_ASSERT_EQUAL(OUString("test"), sTmp);
+    xParameters->getByName("Help") >>= sTmp;
+    CPPUNIT_ASSERT_EQUAL(OUString("F1Help"), sTmp);
+    xParameters->getByName("ExitMacro") >>= sTmp;
+    CPPUNIT_ASSERT_EQUAL(OUString("test"), sTmp);
+    xParameters->getByName("Description") >>= sTmp;
+    CPPUNIT_ASSERT_EQUAL(OUString("StatusHelp"), sTmp);
+    xParameters->getByName("Content") >>= sTmp;
+    CPPUNIT_ASSERT_EQUAL(OUString("Camelcase"), sTmp);
+    xParameters->getByName("Format") >>= sTmp;
+    CPPUNIT_ASSERT_EQUAL(OUString("TITLE CASE"), sTmp);
+
+    sal_uInt16 nMaxLength = 0;
+    xParameters->getByName("MaxLength") >>= nMaxLength;
+    CPPUNIT_ASSERT_EQUAL_MESSAGE("Max Length", sal_uInt16(10), nMaxLength);
+
+    // too bad this is based on character runs - just found try trial and error.
+    xFormField = getProperty< uno::Reference<text::XFormField> >(getRun(getParagraph(6), 2), "Bookmark");
+    xParameters.set(xFormField->getParameters());
+    xParameters->getByName("Type") >>= sTmp;
+    CPPUNIT_ASSERT_EQUAL(OUString("calculated"), sTmp);
+
+    xFormField = getProperty< uno::Reference<text::XFormField> >(getRun(getParagraph(7), 2), "Bookmark");
+    xParameters.set(xFormField->getParameters());
+    xParameters->getByName("Type") >>= sTmp;
+    CPPUNIT_ASSERT_EQUAL(OUString("currentDate"), sTmp);
+
+    xFormField = getProperty< uno::Reference<text::XFormField> >(getRun(getParagraph(7), 6), "Bookmark");
+    xParameters.set(xFormField->getParameters());
+    xParameters->getByName("Type") >>= sTmp;
+    CPPUNIT_ASSERT_EQUAL(OUString("currentTime"), sTmp);
+
+    xFormField = getProperty< uno::Reference<text::XFormField> >(getRun(getParagraph(8), 2), "Bookmark");
+    xParameters.set(xFormField->getParameters());
+    xParameters->getByName("Type") >>= sTmp;
+    CPPUNIT_ASSERT_EQUAL(OUString("number"), sTmp);
+
+    xFormField = getProperty< uno::Reference<text::XFormField> >(getRun(getParagraph(8), 6), "Bookmark");
+    xParameters.set(xFormField->getParameters());
+    xParameters->getByName("Type") >>= sTmp;
+    CPPUNIT_ASSERT_EQUAL(OUString("date"), sTmp);
+}
 
 DECLARE_WW8EXPORT_TEST(testTdf120225_textControlCrossRef, "tdf120225_textControlCrossRef.doc")
 {
