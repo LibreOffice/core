32853f5ccc88e2141d67e9eac68c01d22234ce77
Noel Grandin <noel.grandin@collabora.co.uk>
2018-10-23 12:19:57 +0200

    DSID_DATASOURCE_UNO is dead
    ever since it's introduction in
        commit 1519f106cad10c9832089a8bcad63e6890f7135b
        Date:   Mon Feb 5 12:57:41 2001 +0000
        #83430# +DSID_DATASOURCE_UNO
    
    Change-Id: If9573e259c8897c6cd5cce649bfde7ff3aef7f77

--- dbaccess/source/ui/inc/dsitems.hxx
+++ dbaccess/source/ui/inc/dsitems.hxx
@@ -48,43 +48,42 @@ typedef sal_Int32   ItemID;
 #define DSID_PARAMETERNAMESUBST     22
 #define DSID_CONN_PORTNUMBER        23
 #define DSID_SUPPRESSVERSIONCL      24          // meta data: sal_True if the data source described by the set is to-be-deleted
-#define DSID_DATASOURCE_UNO         25          // meta data: OPropertySetItem, the data source the set represents
-#define DSID_CONN_SHUTSERVICE       26
-#define DSID_CONN_DATAINC           27
-#define DSID_CONN_CACHESIZE         28
-#define DSID_CONN_CTRLUSER          29
-#define DSID_CONN_CTRLPWD           30
-#define DSID_USECATALOG             31          // should the driver use the catalog name when the database is filebased
-#define DSID_CONN_HOSTNAME          32
-#define DSID_CONN_LDAP_BASEDN       33
-#define DSID_CONN_LDAP_PORTNUMBER   34
-#define DSID_CONN_LDAP_ROWCOUNT     35
-#define DSID_SQL92CHECK             36
-#define DSID_AUTOINCREMENTVALUE     37
-#define DSID_AUTORETRIEVEVALUE      38
-#define DSID_AUTORETRIEVEENABLED    39
-#define DSID_APPEND_TABLE_ALIAS     40
-#define DSID_MYSQL_PORTNUMBER       41
-#define DSID_IGNOREDRIVER_PRIV      42
-#define DSID_BOOLEANCOMPARISON      43
-#define DSID_ORACLE_PORTNUMBER      44
-#define DSID_ENABLEOUTERJOIN        45
-#define DSID_CATALOG                46
-#define DSID_SCHEMA                 47
-#define DSID_INDEXAPPENDIX          48
-#define DSID_CONN_LDAP_USESSL       49
-#define DSID_DOCUMENT_URL           50
-#define DSID_DOSLINEENDS            51
-#define DSID_DATABASENAME           52
-#define DSID_AS_BEFORE_CORRNAME     53
-#define DSID_CHECK_REQUIRED_FIELDS  54
-#define DSID_IGNORECURRENCY         55
-#define DSID_CONN_SOCKET            56
-#define DSID_ESCAPE_DATETIME        57
-#define DSID_NAMED_PIPE             58
-#define DSID_PRIMARY_KEY_SUPPORT    59
-#define DSID_MAX_ROW_SCAN           60
-#define DSID_RESPECTRESULTSETTYPE   61
+#define DSID_CONN_SHUTSERVICE       25
+#define DSID_CONN_DATAINC           26
+#define DSID_CONN_CACHESIZE         27
+#define DSID_CONN_CTRLUSER          28
+#define DSID_CONN_CTRLPWD           29
+#define DSID_USECATALOG             30          // should the driver use the catalog name when the database is filebased
+#define DSID_CONN_HOSTNAME          31
+#define DSID_CONN_LDAP_BASEDN       32
+#define DSID_CONN_LDAP_PORTNUMBER   33
+#define DSID_CONN_LDAP_ROWCOUNT     34
+#define DSID_SQL92CHECK             35
+#define DSID_AUTOINCREMENTVALUE     36
+#define DSID_AUTORETRIEVEVALUE      37
+#define DSID_AUTORETRIEVEENABLED    38
+#define DSID_APPEND_TABLE_ALIAS     39
+#define DSID_MYSQL_PORTNUMBER       40
+#define DSID_IGNOREDRIVER_PRIV      41
+#define DSID_BOOLEANCOMPARISON      42
+#define DSID_ORACLE_PORTNUMBER      43
+#define DSID_ENABLEOUTERJOIN        44
+#define DSID_CATALOG                45
+#define DSID_SCHEMA                 46
+#define DSID_INDEXAPPENDIX          47
+#define DSID_CONN_LDAP_USESSL       48
+#define DSID_DOCUMENT_URL           49
+#define DSID_DOSLINEENDS            50
+#define DSID_DATABASENAME           51
+#define DSID_AS_BEFORE_CORRNAME     52
+#define DSID_CHECK_REQUIRED_FIELDS  53
+#define DSID_IGNORECURRENCY         54
+#define DSID_CONN_SOCKET            55
+#define DSID_ESCAPE_DATETIME        56
+#define DSID_NAMED_PIPE             57
+#define DSID_PRIMARY_KEY_SUPPORT    58
+#define DSID_MAX_ROW_SCAN           59
+#define DSID_RESPECTRESULTSETTYPE   60
     // don't forget to adjust DSID_LAST_ITEM_ID below!
 
 // item range. Adjust this if you introduce new items above
