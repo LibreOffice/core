5c3b187098cb815d6b72ee04464ebbac5e8f4a90
Serge Krot <Serge.Krot@cib.de>
2018-10-24 15:38:17 +0200

    sc: fix: range/step calculation for progress bar
    Change-Id: I733e4003b65b410d44d9a1132be4e9e10ac24c3e
    Reviewed-on: https://gerrit.libreoffice.org/62305
    Tested-by: Jenkins
    Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>
    
    Conflicts:
    	sc/source/core/data/table1.cxx

--- sc/inc/column.hxx
+++ sc/inc/column.hxx
@@ -369,7 +369,8 @@ public:
     ScFormulaCell * const * GetFormulaCellBlockAddress( SCROW nRow, size_t& rBlockSize ) const;
     CellType    GetCellType( SCROW nRow ) const;
     SCSIZE      GetCellCount() const;
-    sal_uInt32 GetWeightedCount() const;
+    sal_uLong GetWeightedCount() const;
+    sal_uLong GetWeightedCount(SCROW nStartRow, SCROW nEndRow) const;
     sal_uInt32 GetCodeCount() const;       // RPN-Code in formulas
     FormulaError  GetErrCode( SCROW nRow ) const;
 
