b97f3988183dab39b2b0231d9fd0e55d078ac0b5
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2018-10-24 11:29:32 +0200

    lok: trigger sign. verification in getSignatureState + update test
    As the certificate chain can be added after the document was
    opened, we need to trigger signature verification every time the
    LOK API method getSignatureState is called.
    
    In addition update the tests so that they check the status of a
    document that's not signed, a document that was signed but the
    certificate chain is not available so the verification fails and
    later adding the certificate chain and the verification returns
    an OK status.
    
    Change-Id: I44578d0cece5bfc4a2e43fbbcd68b5ea1ccbc38b
    Reviewed-on: https://gerrit.libreoffice.org/62276
    Tested-by: Jenkins
    Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
    (cherry picked from commit 28a698db6f604137443053144dde94c9e553c0ef)

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -3803,6 +3803,8 @@ static int doc_getSignatureState(LibreOfficeKitDocument* pThis)
     if (!pObjectShell)
         return int(SignatureState::UNKNOWN);
 
+    pObjectShell->RecheckSignature(false);
+
     return int(pObjectShell->GetDocumentSignatureState());
 }
 
