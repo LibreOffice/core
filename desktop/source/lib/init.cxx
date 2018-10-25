7a228e8dda93e1e0398e4b34bb564c58b7c47f36
Tor Lillqvist <tml@collabora.com>
2018-10-25 15:29:26 +0300

    The 2nd parameter to lo_initialize() is a URL so use such naming consistently
    Change-Id: Id4e7f8556d6910ab43b0f7b8ba09e7be8b1ec1d1

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -5162,14 +5162,14 @@ static int lo_initialize(LibreOfficeKit* pThis, const char* pAppPath, const char
 }
 
 SAL_DLLPUBLIC_EXPORT
-LibreOfficeKit *libreofficekit_hook_2(const char* install_path, const char* user_profile_path)
+LibreOfficeKit *libreofficekit_hook_2(const char* install_path, const char* user_profile_url)
 {
     if (!gImpl)
     {
         SAL_INFO("lok", "Create libreoffice object");
 
         gImpl = new LibLibreOffice_Impl();
-        if (!lo_initialize(gImpl, install_path, user_profile_path))
+        if (!lo_initialize(gImpl, install_path, user_profile_url))
         {
             lo_destroy(gImpl);
         }
@@ -5184,9 +5184,9 @@ LibreOfficeKit *libreofficekit_hook(const char* install_path)
 }
 
 SAL_JNI_EXPORT
-int lok_preinit(const char* install_path, const char* user_profile_path)
+int lok_preinit(const char* install_path, const char* user_profile_url)
 {
-    return lo_initialize(nullptr, install_path, user_profile_path);
+    return lo_initialize(nullptr, install_path, user_profile_url);
 }
 
 static void lo_destroy(LibreOfficeKit* pThis)
