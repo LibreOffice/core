a2729ecfa302c1397f80c7db01777b6e3657034f
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2018-10-27 10:36:51 -0400

    LOK: sidebar: send close notification on disposing sidebar
    Change-Id: Ia8f197d07ba4b6a089571e9ca23b4697d04e7734

--- sfx2/source/sidebar/SidebarController.cxx
+++ sfx2/source/sidebar/SidebarController.cxx
@@ -1138,11 +1138,6 @@ IMPL_LINK(SidebarController, OnMenuItemSelected, Menu*, pMenu, bool)
 
 void SidebarController::RequestCloseDeck()
 {
-    if (comphelper::LibreOfficeKit::isActive())
-    {
-        mpCurrentDeck->GetLOKNotifier()->notifyWindow(mpCurrentDeck->GetLOKWindowId(), "close");
-    }
-
     mbIsDeckRequestedOpen = false;
     UpdateDeckOpenState();
 
