75e4f4855e7020f255cfee8dd64f8ffbfe2a348e
Katarina Behrens <Katarina.Behrens@cib.de>
2018-11-01 13:57:56 +0100

    tdf#119856: thread-proof creating frames and setting menus
    This finally enables opening a new frame and setting its menu from
    an extension, but it is still far from stable, loads of threading
    landmines like this all over the code
    
    Change-Id: Icf4b67796b0669425ecb7c2c142c21e184024534
    Reviewed-on: https://gerrit.libreoffice.org/62737
    Tested-by: Jenkins
    Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>
    
    Conflicts:
    	vcl/unx/kde5/KDE5SalInstance.hxx

--- vcl/unx/kde5/KDE5SalInstance.cxx
+++ vcl/unx/kde5/KDE5SalInstance.cxx
@@ -46,12 +46,20 @@ KDE5SalInstance::KDE5SalInstance(std::unique_ptr<SalYieldMutex> pMutex)
     pSVData->maAppData.mpToolkitName = new OUString("kde5");
 
     KDE5SalData::initNWF();
+
+    connect(this, &KDE5SalInstance::createFrameSignal, this, &KDE5SalInstance::CreateFrame,
+            Qt::BlockingQueuedConnection);
     connect(this, &KDE5SalInstance::createFilePickerSignal, this,
             &KDE5SalInstance::createFilePicker, Qt::BlockingQueuedConnection);
 }
 
 SalFrame* KDE5SalInstance::CreateFrame(SalFrame* pParent, SalFrameStyleFlags nState)
 {
+    if (!IsMainThread())
+    {
+        SolarMutexReleaser aReleaser;
+        return Q_EMIT createFrameSignal(pParent, nState);
+    }
     return new KDE5SalFrame(static_cast<KDE5SalFrame*>(pParent), nState, true);
 }
 
