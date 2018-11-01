3d670a497aa1437df70dab2a06ba8464c5ac24ff
Justin Luth <justin.luth@collabora.com>
2018-11-01 15:38:58 +0300

    tdf#118244 pdfexport: radio buttons use groupname now
    The previous implementation grouped radio buttons if their
    object name was the same. Likely this is a very old implementation,
    because the current radio buttons have a groupname property which
    links them together (although that too needed fixing in doc/docx),
    and their object names are unique.
    
    Edge cases tested:
    -groupID of 0 works fine - doesn't have to be 1-based.
    -empty group name works fine.
    -writer, calc,
    
    Change-Id: I84aebdac18b9edfa5ffcbfb23c15d0f37fcd47d1

--- vcl/qa/cppunit/pdfexport/pdfexport.cxx
+++ vcl/qa/cppunit/pdfexport/pdfexport.cxx
@@ -94,6 +94,7 @@ public:
     void testTdf99680();
     void testTdf99680_2();
     void testTdf108963();
+    void testTdf118244_radioButtonGroup();
 #if HAVE_MORE_FONTS
     /// Test writing ToUnicode CMAP for LTR ligatures.
     void testTdf115117_1();
@@ -132,6 +133,7 @@ public:
     CPPUNIT_TEST(testTdf99680);
     CPPUNIT_TEST(testTdf99680_2);
     CPPUNIT_TEST(testTdf108963);
+    CPPUNIT_TEST(testTdf118244_radioButtonGroup);
 #if HAVE_MORE_FONTS
     CPPUNIT_TEST(testTdf115117_1);
     CPPUNIT_TEST(testTdf115117_1a);
@@ -870,6 +872,41 @@ void PdfExportTest::testTdf108963()
     }
 
     CPPUNIT_ASSERT_EQUAL(1, nYellowPathCount);
+}
+
+void PdfExportTest::testTdf118244_radioButtonGroup()
+{
+    vcl::filter::PDFDocument aDocument;
+    load("tdf118244_radioButtonGroup.odt", aDocument);
+
+    // The document has one page.
+    std::vector<vcl::filter::PDFObjectElement*> aPages = aDocument.GetPages();
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(1), aPages.size());
+
+    // There are two radio buttons.
+    auto pAnnots = dynamic_cast<vcl::filter::PDFArrayElement*>(aPages[0]->Lookup("Annots"));
+    CPPUNIT_ASSERT(pAnnots);
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(2), pAnnots->GetElements().size());
+
+    sal_uInt32 nRadioGroups = 0;
+    for ( const auto& aElement : aDocument.GetElements() )
+    {
+        auto pObject = dynamic_cast<vcl::filter::PDFObjectElement*>(aElement.get());
+        if ( !pObject )
+            continue;
+        auto pType = dynamic_cast<vcl::filter::PDFNameElement*>(pObject->Lookup("FT"));
+        if ( pType && pType->GetValue() == "Btn" )
+        {
+            auto pKids = dynamic_cast<vcl::filter::PDFArrayElement*>(pObject->Lookup("Kids"));
+            // Only one group, containing both buttons.
+            if ( pKids )
+            {
+                ++nRadioGroups;
+                CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(2), pKids->GetElements().size());
+            }
+        }
+    }
+    CPPUNIT_ASSERT_EQUAL_MESSAGE("# of radio groups", sal_uInt32(1), nRadioGroups);
 }
 
 #if HAVE_MORE_FONTS
