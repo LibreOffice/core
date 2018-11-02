ad54ffb44f76e8294b9f8974409fcf821e523bf9
Tor Lillqvist <tml@collabora.com>
2018-11-02 22:58:16 +0200

    Start of work in progress on making dialogs work in the iOS app
    Build the swui library for non-DESKTOP platforms, too. Handle fallout
    for !HAVE_FEATURE_DBCONNECTIVITY and !HAVE_FEATURE_AVMEDIA (both of
    which features we for now don't want to bother with in the iOS app).
    
    Make VclAbstractDialogFactory::Create() do its thing also on
    non-DESKTOP.
    
    This commit just causes more code to be compiled for the non-DESKTOP
    case, dialogs in general surely don't actually work yet in the iOS
    app. For instance:
    
    vcl/source/window/builder.cxx:2060: probably need to implement sfxlo-CustomPropertiesControl or add a makesfxlo-CustomPropertiesControl function
    
    Change-Id: I579efba605f519dcbf407b675be88c7c6ee0f19b
    Reviewed-on: https://gerrit.libreoffice.org/62865
    Reviewed-by: Tor Lillqvist <tml@collabora.com>
    Tested-by: Tor Lillqvist <tml@collabora.com>

--- cui/source/options/treeopt.cxx
+++ cui/source/options/treeopt.cxx
@@ -297,7 +297,9 @@ VclPtr<SfxTabPage> CreateGeneralTabPage( sal_uInt16 nId, vcl::Window* pParent, c
         case RID_SVXPAGE_INET_PROXY:                fnCreate = &SvxProxyTabPage::Create; break;
         case RID_SVXPAGE_INET_SECURITY:             fnCreate = &SvxSecurityTabPage::Create; break;
         case RID_SVXPAGE_INET_MAIL:                 fnCreate = &SvxEMailTabPage::Create; break;
+#if HAVE_FEATURE_DESKTOP
         case RID_SVXPAGE_PERSONALIZATION:           fnCreate = &SvxPersonalizationTabPage::Create; break;
+#endif
         case RID_SVXPAGE_COLORCONFIG:               fnCreate = &SvxColorOptionsTabPage::Create; break;
         case RID_OFAPAGE_HTMLOPT:                   fnCreate = &OfaHtmlTabPage::Create; break;
         case SID_OPTFILTER_MSOFFICE:                fnCreate = &OfaMSFilterTabPage::Create; break;
