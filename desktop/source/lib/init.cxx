d04f9d0a73f167e741c29ad68320e2f49570ccfe
Tor Lillqvist <tml@collabora.com>
2018-11-02 23:39:44 +0200

    On iOS, lok_init_2() links directly to libreofficekit_hook_2()
    Thus no need for any special __attribute__ ((visibility("default")))
    dance.
    
    Change-Id: I398d588a0165fd616ea0d259bbf2cab1e1a07ccb
    Reviewed-on: https://gerrit.libreoffice.org/62867
    Reviewed-by: Tor Lillqvist <tml@collabora.com>
    Tested-by: Tor Lillqvist <tml@collabora.com>

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -4118,19 +4118,7 @@ static int lo_initialize(LibreOfficeKit* pThis, const char* pAppPath, const char
     return bInitialized;
 }
 
-// Undo our clever trick of having SAL_DLLPUBLIC_EXPORT actually not
-// meaning what is says in for the DISABLE_DYNLOADING case. See
-// <sal/types.h>. Normally, when building just one big dylib (Android)
-// or executable (iOS), most of our "public" symbols don't need to be
-// visible outside that resulting dylib/executable. But
-// libreofficekit_hook must be exported for dlsym() to find it,
-// though, at least on iOS.
-
-#if defined(__GNUC__) && defined(DISABLE_DYNLOADING)
-__attribute__ ((visibility("default")))
-#else
 SAL_DLLPUBLIC_EXPORT
-#endif
 LibreOfficeKit *libreofficekit_hook_2(const char* install_path, const char* user_profile_path)
 {
     if (!gImpl)
@@ -4146,11 +4134,7 @@ LibreOfficeKit *libreofficekit_hook_2(const char* install_path, const char* user
     return static_cast<LibreOfficeKit*>(gImpl);
 }
 
-#if defined(__GNUC__) && defined(DISABLE_DYNLOADING)
-__attribute__ ((visibility("default")))
-#else
 SAL_DLLPUBLIC_EXPORT
-#endif
 LibreOfficeKit *libreofficekit_hook(const char* install_path)
 {
     return libreofficekit_hook_2(install_path, nullptr);
