9b2c3627441ea70b7be04b6cfd5469739cf2395a
Mark Hung <marklh9@gmail.com>
2018-11-04 00:45:20 +0800

    tdf#37636 Render textlines with colors.
    Create a new helepr class, TextLinesHelper, to handle
    textline colors and overall size.
    
    Change-Id: I8635199819443429f7be1754a298e9a21ef17f8c

---
+++ cppcanvas/source/mtfrenderer/textlineshelper.cxx
@@ -0,0 +1,127 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <com/sun/star/rendering/XCanvas.hpp>
+#include <com/sun/star/rendering/StrokeAttributes.hpp>
+#include <com/sun/star/rendering/PathJoinType.hpp>
+#include <basegfx/polygon/b2dpolypolygontools.hxx>
+#include <basegfx/utils/canvastools.hxx>
+#include <outdevstate.hxx>
+#include "textlineshelper.hxx"
+#include "mtftools.hxx"
+
+using namespace ::com::sun::star;
+
+namespace
+{
+void initLineStyleWaveline(sal_uInt32 nLineStyle, bool& bIsWaveline, bool& bIsBold)
+{
+    bIsWaveline = nLineStyle == LINESTYLE_DOUBLEWAVE || nLineStyle == LINESTYLE_SMALLWAVE
+                  || nLineStyle == LINESTYLE_BOLDWAVE || nLineStyle == LINESTYLE_WAVE;
+    bIsBold = nLineStyle == LINESTYLE_BOLDWAVE;
+}
+}
+
+namespace cppcanvas
+{
+namespace internal
+{
+TextLinesHelper::TextLinesHelper(const CanvasSharedPtr& rCanvas, const OutDevState& rState)
+    : mpCanvas(rCanvas)
+    , mbIsOverlineColorSet(rState.isTextOverlineColorSet)
+    , maOverlineColor(rState.textOverlineColor)
+    , mbIsUnderlineColorSet(rState.isTextLineColorSet)
+    , maUnderlineColor(rState.textLineColor)
+    , mbOverlineWaveline(false)
+    , mbUnderlineWaveline(false)
+    , mbOverlineWavelineBold(false)
+    , mbUnderlineWavelineBold(false)
+{
+}
+
+void TextLinesHelper::init(double nLineWidth, const tools::TextLineInfo& rLineInfo)
+{
+    ::basegfx::B2DRange aRange; // default is empty.
+    ::basegfx::B2DPolyPolygon aOverline, aUnderline, aStrikeout;
+    tools::createTextLinesPolyPolygon(0.0, nLineWidth, rLineInfo, aOverline, aUnderline,
+                                      aStrikeout);
+
+    mxOverline.clear();
+    mxUnderline.clear();
+    mxStrikeout.clear();
+
+    uno::Reference<rendering::XGraphicDevice> xDevice = mpCanvas->getUNOCanvas()->getDevice();
+
+    if (aOverline.count())
+    {
+        aRange.expand(::basegfx::utils::getRange(aOverline));
+        mxOverline = ::basegfx::unotools::xPolyPolygonFromB2DPolyPolygon(xDevice, aOverline);
+    }
+
+    if (aUnderline.count())
+    {
+        aRange.expand(::basegfx::utils::getRange(aUnderline));
+        mxUnderline = ::basegfx::unotools::xPolyPolygonFromB2DPolyPolygon(xDevice, aUnderline);
+    }
+
+    if (aStrikeout.count())
+    {
+        aRange.expand(::basegfx::utils::getRange(aStrikeout));
+        mxStrikeout = ::basegfx::unotools::xPolyPolygonFromB2DPolyPolygon(xDevice, aStrikeout);
+    }
+
+    maOverallSize = aRange.getRange();
+
+    initLineStyleWaveline(rLineInfo.mnOverlineStyle, mbOverlineWaveline, mbOverlineWavelineBold);
+
+    initLineStyleWaveline(rLineInfo.mnUnderlineStyle, mbUnderlineWaveline, mbUnderlineWavelineBold);
+}
+
+void TextLinesHelper::render(const rendering::RenderState& rRenderState, bool bNormalText) const
+{
+    const rendering::ViewState& rViewState(mpCanvas->getViewState());
+    const uno::Reference<rendering::XCanvas>& xCanvas(mpCanvas->getUNOCanvas());
+    rendering::StrokeAttributes aStrokeAttributes;
+    aStrokeAttributes.JoinType = rendering::PathJoinType::ROUND;
+
+    if (mxOverline.is())
+    {
+        rendering::RenderState aLocalState(rRenderState);
+        if (bNormalText && mbIsOverlineColorSet)
+            aLocalState.DeviceColor = maOverlineColor;
+
+        if (mbOverlineWaveline)
+        {
+            aStrokeAttributes.StrokeWidth = mbOverlineWavelineBold ? 2.0 : 1.0;
+            xCanvas->strokePolyPolygon(mxOverline, rViewState, aLocalState, aStrokeAttributes);
+        }
+        else
+            xCanvas->fillPolyPolygon(mxOverline, rViewState, aLocalState);
+    }
+
+    if (mxUnderline.is())
+    {
+        rendering::RenderState aLocalState(rRenderState);
+        if (bNormalText && mbIsUnderlineColorSet)
+            aLocalState.DeviceColor = maUnderlineColor;
+        if (mbUnderlineWaveline)
+        {
+            aStrokeAttributes.StrokeWidth = mbUnderlineWavelineBold ? 2.0 : 1.0;
+            xCanvas->strokePolyPolygon(mxUnderline, rViewState, aLocalState, aStrokeAttributes);
+        }
+        else
+            xCanvas->fillPolyPolygon(mxUnderline, rViewState, aLocalState);
+    }
+
+    if (mxStrikeout.is())
+        xCanvas->fillPolyPolygon(mxStrikeout, rViewState, rRenderState);
+}
+}
+}
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
---
+++ cppcanvas/source/mtfrenderer/textlineshelper.hxx
@@ -0,0 +1,87 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+#ifndef INCLUDED_CPPCANVAS_SOURCE_MTFRENDERER_TEXTLINESHELPER_HXX
+#define INCLUDED_CPPCANVAS_SOURCE_MTFRENDERER_TEXTLINESHELPER_HXX
+
+#include <com/sun/star/uno/Sequence.hxx>
+#include <com/sun/star/uno/Reference.hxx>
+#include <basegfx/vector/b2dsize.hxx>
+#include <canvasgraphichelper.hxx>
+
+namespace com
+{
+namespace sun
+{
+namespace star
+{
+namespace rendering
+{
+class XPolyPolygon2D;
+}
+}
+}
+}
+
+namespace cppcanvas
+{
+namespace tools
+{
+struct TextLineInfo;
+}
+
+namespace internal
+{
+struct OutDevState;
+
+class TextLinesHelper
+{
+    const CanvasSharedPtr mpCanvas;
+    css::uno::Reference<css::rendering::XPolyPolygon2D> mxOverline;
+    css::uno::Reference<css::rendering::XPolyPolygon2D> mxUnderline;
+    css::uno::Reference<css::rendering::XPolyPolygon2D> mxStrikeout;
+
+    ::basegfx::B2DSize maOverallSize;
+
+    bool mbIsOverlineColorSet;
+    const css::uno::Sequence<double> maOverlineColor;
+
+    bool mbIsUnderlineColorSet;
+    const css::uno::Sequence<double> maUnderlineColor;
+
+    bool mbOverlineWaveline;
+    bool mbUnderlineWaveline;
+
+    bool mbOverlineWavelineBold;
+    bool mbUnderlineWavelineBold;
+
+public:
+    TextLinesHelper(const CanvasSharedPtr& rCanvas, const OutDevState& rState);
+
+    ::basegfx::B2DSize getOverallSize() const { return maOverallSize; }
+
+    /** Init textlines with specified linewdith and TextLineInfo.
+     */
+    void init(double nLineWidth, const tools::TextLineInfo& rLineInfo);
+
+    /** Fill the textlines with colors.
+        OutDevState::textUnderlineColor.
+
+        @param rRenderState
+        Used to invoke XCanvas::fillPolyPolygon.
+
+        @param bNormalText
+        Use overline color and underline color if the value is true, ignore those
+        colors otherwise ( typical case is to render the shadow ).
+     */
+    void render(const css::rendering::RenderState& rRenderState, bool bNormalText) const;
+};
+}
+}
+#endif // INCLUDED_CPPCANVAS_SOURCE_MTFRENDERER_TEXTLINESHELPER_HXX
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
