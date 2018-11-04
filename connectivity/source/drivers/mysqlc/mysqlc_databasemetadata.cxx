LibreOffice/core

commit 39e0bbfdf77cf9653ec68ffcb4374d929838b2a8

mysqlc: filter schema in getColumns
Filter schema in XDatabaseMetaData::getColumns so it would show correct
results in case there are more tables with the same name in different
schemas.

Change-Id: I8a986a43a8b049db3615d7ec2585ce68ebe3340e
Reviewed-on: https://gerrit.libreoffice.org/62696
Tested-by: Jenkins
Reviewed-by: Tamás Bunth <btomi96@gmail.com>

connectivity/source/drivers/mysqlc/mysqlc_databasemetadata.cxx
@@ -696,12 +696,12 @@ Reference<XResultSet>
 }
 
 Reference<XResultSet> SAL_CALL ODatabaseMetaData::getColumns(const Any& /*catalog*/,
-                                                             const OUString& /*schemaPattern*/,
+                                                             const OUString& schemaPattern,
                                                              const OUString& tableNamePattern,
                                                              const OUString& columnNamePattern)
 {
-    OUStringBuffer queryBuf("SELECT TABLE_CATALOG AS TABLE_CAT, " // 1
-                            "TABLE_SCHEMA AS TABLE_SCHEM, " // 2
+    OUStringBuffer queryBuf("SELECT TABLE_CATALOG, " // 1
+                            "TABLE_SCHEMA, " // 2
                             "TABLE_NAME, " // 3
                             "COLUMN_NAME, " // 4
                             "DATA_TYPE, " // 5
@@ -720,7 +720,6 @@ Reference<XResultSet> SAL_CALL ODatabaseMetaData::getColumns(const Any& /*catalo
                             "COLUMN_TYPE " // 14
                             "FROM INFORMATION_SCHEMA.COLUMNS "
                             "WHERE (1 = 1) ");
-
     if (!tableNamePattern.isEmpty())
     {
         OUString sAppend;
@@ -729,6 +728,15 @@ Reference<XResultSet> SAL_CALL ODatabaseMetaData::getColumns(const Any& /*catalo
         else
             sAppend = "AND TABLE_NAME = '%' ";
         queryBuf.append(sAppend.replaceAll("%", tableNamePattern));
+    }
+    if (!schemaPattern.isEmpty())
+    {
+        OUString sAppend;
+        if (tableNamePattern.match("%"))
+            sAppend = "AND TABLE_SCHEMA LIKE '%' ";
+        else
+            sAppend = "AND TABLE_SCHEMA = '%' ";
+        queryBuf.append(sAppend.replaceAll("%", schemaPattern));
     }
     if (!columnNamePattern.isEmpty())
     {

commit 82af7aa9863a30b9e80ea00e27c60affb86b2225

coverity#1440839: COPY_PASTE_ERROR
Regression from 39e0bbfdf77cf9653ec68ffcb4374d929838b2a8

Change-Id: Ibb0dea1a78ccf3ef446bbbaad28945f1ac8bdf48

connectivity/source/drivers/mysqlc/mysqlc_databasemetadata.cxx
@@ -732,7 +732,7 @@ Reference<XResultSet> SAL_CALL ODatabaseMetaData::getColumns(const Any& /*catalo
     if (!schemaPattern.isEmpty())
     {
         OUString sAppend;
-        if (tableNamePattern.match("%"))
+        if (schemaPattern.match("%"))
             sAppend = "AND TABLE_SCHEMA LIKE '%' ";
         else
             sAppend = "AND TABLE_SCHEMA = '%' ";