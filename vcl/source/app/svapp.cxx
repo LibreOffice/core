82a5c7b35cfe3b17e8fbd1f29666ac24fb9e1804
Andras Timar <andras.timar@collabora.com>
2018-11-05 18:17:05 +0100

    LOK: update main thread
    Change-Id: I75df6ae9f9fb41b08a8774ce026f79fdd120b629

--- vcl/source/app/svapp.cxx
+++ vcl/source/app/svapp.cxx
@@ -1383,6 +1383,13 @@ void Application::SetHelp( Help* pHelp )
     ImplGetSVData()->maAppData.mpHelp = pHelp;
 }
 
+void Application::UpdateMainThread()
+{
+    ImplSVData* pSVData = ImplGetSVData();
+    if (pSVData)
+        pSVData->mnMainThreadId = osl::Thread::getCurrentIdentifier();
+}
+
 Help* Application::GetHelp()
 {
     return ImplGetSVData()->maAppData.mpHelp;
