295526c3a9bb52b3beebfd9eeb490838077b1472
Szymon Kłos <szymon.klos@collabora.com>
2018-11-06 09:43:11 +0100

    Keep reference mode if only one dialog closed
    Change-Id: I9cf7988da7f83c28a0919c69ace131ee93054486
    Reviewed-on: https://gerrit.libreoffice.org/79466
    Tested-by: Jenkins
    Reviewed-by: Michael Meeks <michael.meeks@collabora.com>

--- sc/source/ui/app/scmod.cxx
+++ sc/source/ui/app/scmod.cxx
@@ -1504,7 +1504,16 @@ void ScModule::SetRefDialog( sal_uInt16 nId, bool bVis, SfxViewFrame* pViewFrm )
         //if ( pViewFrm )
         //  pViewFrm->GetBindings().Update();       // to avoid trouble in LockDispatcher
 
-        m_nCurRefDlgId = bVis ? nId : 0 ;             // before SetChildWindow
+        // before SetChildWindow
+        if ( comphelper::LibreOfficeKit::isActive() )
+        {
+            if ( bVis )
+                m_nCurRefDlgId = nId;
+        }
+        else
+        {
+            m_nCurRefDlgId = bVis ? nId : 0;
+        }
 
         if ( pViewFrm )
         {
@@ -1758,7 +1767,13 @@ void ScModule::EndReference()
     //FIXME: ShowRefFrame at InputHdl, if the Function AutoPilot is open?
     if ( m_nCurRefDlgId )
     {
-        SfxChildWindow* pChildWnd = lcl_GetChildWinFromAnyView( m_nCurRefDlgId );
+        SfxChildWindow* pChildWnd = nullptr;
+
+        if ( comphelper::LibreOfficeKit::isActive() )
+            pChildWnd = lcl_GetChildWinFromCurrentView( m_nCurRefDlgId );
+        else
+            pChildWnd = lcl_GetChildWinFromAnyView( m_nCurRefDlgId );
+
         OSL_ENSURE( pChildWnd, "NoChildWin" );
         if ( pChildWnd )
         {
