LibreOffice/core

commit 16b2b4f27acb83fc651b8484dead53ebd0e269e1

Simplify containers iterations in sw/source/ui*
Use range-based loop or replace with STL functions.

Change-Id: I0d690e873f720a68f04991674ce84ec590231fd0
Reviewed-on: https://gerrit.libreoffice.org/62432
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sw/source/uibase/docvw/frmsidebarwincontainer.cxx
@@ -104,16 +104,12 @@ bool SwFrameSidebarWinContainer::remove( const SwFrame& rFrame,
     if ( aFrameIter != mpFrameSidebarWinContainer->end() )
     {
         SidebarWinContainer& rSidebarWinContainer = (*aFrameIter).second;
-        for ( SidebarWinContainer::iterator aIter = rSidebarWinContainer.begin();
-              aIter != rSidebarWinContainer.end();
-              ++aIter )
+        auto aIter = std::find_if(rSidebarWinContainer.begin(), rSidebarWinContainer.end(),
+            [&rSidebarWin](const SidebarWinContainer::value_type& rEntry) { return rEntry.second == &rSidebarWin; });
+        if ( aIter != rSidebarWinContainer.end() )
         {
-            if ( (*aIter).second == &rSidebarWin )
-            {
-                rSidebarWinContainer.erase( aIter );
-                bRemoved = true;
-                break;
-            }
+            rSidebarWinContainer.erase( aIter );
+            bRemoved = true;
         }
     }
 
@@ -141,22 +137,12 @@ sw::annotation::SwAnnotationWin* SwFrameSidebarWinContainer::get( const SwFrame&
 
     FrameKey aFrameKey( &rFrame );
     FrameSidebarWinContainer::iterator aFrameIter = mpFrameSidebarWinContainer->find( aFrameKey );
-    if ( aFrameIter != mpFrameSidebarWinContainer->end() )
+    if ( aFrameIter != mpFrameSidebarWinContainer->end() && nIndex >= 0 )
     {
         SidebarWinContainer& rSidebarWinContainer = (*aFrameIter).second;
-        sal_Int32 nCounter( nIndex );
-        for ( SidebarWinContainer::iterator aIter = rSidebarWinContainer.begin();
-              nCounter >= 0 && aIter != rSidebarWinContainer.end();
-              ++aIter )
-        {
-            if ( nCounter == 0 )
-            {
-                pRet = (*aIter).second;
-                break;
-            }
-
-            --nCounter;
-        }
+        auto aIter = rSidebarWinContainer.begin();
+        std::advance(aIter, nIndex);
+        pRet = (*aIter).second;
     }
 
     return pRet;
@@ -172,11 +158,9 @@ void SwFrameSidebarWinContainer::getAll( const SwFrame& rFrame,
     if ( aFrameIter != mpFrameSidebarWinContainer->end() )
     {
         SidebarWinContainer& rSidebarWinContainer = (*aFrameIter).second;
-        for ( SidebarWinContainer::iterator aIter = rSidebarWinContainer.begin();
-              aIter != rSidebarWinContainer.end();
-              ++aIter )
+        for ( const auto& rEntry : rSidebarWinContainer )
         {
-            pSidebarWins->push_back( (*aIter).second );
+            pSidebarWins->push_back( rEntry.second );
         }
     }
 }

sw/source/ui/chrdlg/pardlg.cxx
@@ -222,8 +222,8 @@ void SwParaDlg::PageCreated(const OString& rId, SfxTabPage& rPage)
             aNames.insert(pBase->GetName());
             pBase = pPool->Next();
         }
-        for(std::set<OUString>::const_iterator it = aNames.begin(); it != aNames.end(); ++it)
-            rBox.append_text(*it);
+        for(const auto& rName : aNames)
+            rBox.append_text(rName);
     }
     // inits for Area and Transparency TabPages
     // The selection attribute lists (XPropertyList derivates, e.g. XColorList for

sw/source/ui/config/optcomp.cxx
@@ -269,20 +269,17 @@ IMPL_LINK_NOARG(SwCompatibilityOptPage, UseAsDefaultHdl, Button*, void)
     std::unique_ptr<weld::MessageDialog> xQueryBox(xBuilder->weld_message_dialog("QueryDefaultCompatDialog"));
     if (xQueryBox->run() == RET_YES)
     {
-        for ( vector< SvtCompatibilityEntry >::iterator pItem = m_pImpl->m_aList.begin();
-              pItem != m_pImpl->m_aList.end(); ++pItem )
+        auto pItem = std::find_if(m_pImpl->m_aList.begin(), m_pImpl->m_aList.end(),
+            [](const SvtCompatibilityEntry& rItem) { return rItem.isDefaultEntry(); });
+        if (pItem != m_pImpl->m_aList.end())
         {
-            if ( pItem->isDefaultEntry() )
+            const sal_Int32 nCount = m_pOptionsLB->GetEntryCount();
+            for ( sal_Int32 i = 0; i < nCount; ++i )
             {
-                const sal_Int32 nCount = m_pOptionsLB->GetEntryCount();
-                for ( sal_Int32 i = 0; i < nCount; ++i )
-                {
-                    bool bChecked = m_pOptionsLB->IsChecked(static_cast< sal_uLong >( i ));
+                bool bChecked = m_pOptionsLB->IsChecked(static_cast< sal_uLong >( i ));
 
-                    int nCoptIdx = i + 2; /* Consider "Name" & "Module" indexes */
-                    pItem->setValue<bool>( SvtCompatibilityEntry::Index(nCoptIdx), bChecked );
-                }
-                break;
+                int nCoptIdx = i + 2; /* Consider "Name" & "Module" indexes */
+                pItem->setValue<bool>( SvtCompatibilityEntry::Index(nCoptIdx), bChecked );
             }
         }
 
@@ -331,9 +328,8 @@ sal_uLong SwCompatibilityOptPage::GetDocumentOptions() const
 void SwCompatibilityOptPage::WriteOptions()
 {
     m_aConfigItem.Clear();
-    for ( vector< SvtCompatibilityEntry >::const_iterator pItem = m_pImpl->m_aList.begin();
-          pItem != m_pImpl->m_aList.end(); ++pItem )
-        m_aConfigItem.AppendItem(*pItem);
+    for ( const auto& rItem : m_pImpl->m_aList )
+        m_aConfigItem.AppendItem(rItem);
 }
 
 VclPtr<SfxTabPage> SwCompatibilityOptPage::Create( TabPageParent pParent, const SfxItemSet* rAttrSet )

sw/source/ui/config/optpage.cxx
@@ -808,14 +808,13 @@ void SwStdFontTabPage::Reset( const SfxItemSet* rSet)
         }
 
         // insert to listboxes
-        for( std::set< OUString >::const_iterator it = aFontNames.begin();
-             it != aFontNames.end(); ++it )
+        for( const auto& rFontName : aFontNames )
         {
-            m_pStandardBox->InsertEntry( *it );
-            m_pTitleBox->InsertEntry( *it );
-            m_pListBox->InsertEntry( *it );
-            m_pLabelBox->InsertEntry( *it );
-            m_pIdxBox->InsertEntry( *it );
+            m_pStandardBox->InsertEntry( rFontName );
+            m_pTitleBox->InsertEntry( rFontName );
+            m_pListBox->InsertEntry( rFontName );
+            m_pLabelBox->InsertEntry( rFontName );
+            m_pIdxBox->InsertEntry( rFontName );
         }
     }
     if(SfxItemState::SET == rSet->GetItemState(FN_PARAM_STDFONTS, false, &pItem))

sw/source/ui/dbui/createaddresslistdialog.cxx
@@ -120,11 +120,11 @@ SwAddressControl_Impl::~SwAddressControl_Impl()
 
 void SwAddressControl_Impl::dispose()
 {
-    for(auto aTextIter = m_aFixedTexts.begin(); aTextIter != m_aFixedTexts.end(); ++aTextIter)
-        aTextIter->disposeAndClear();
+    for(auto& rText : m_aFixedTexts)
+        rText.disposeAndClear();
     m_aFixedTexts.clear();
-    for(auto aEditIter = m_aEdits.begin(); aEditIter != m_aEdits.end(); ++aEditIter)
-        aEditIter->disposeAndClear();
+    for(auto& rEdit : m_aEdits)
+        rEdit.disposeAndClear();
     m_aEdits.clear();
     m_pScrollBar.disposeAndClear();
     m_pWindow.disposeAndClear();
@@ -137,28 +137,24 @@ void SwAddressControl_Impl::SetData(SwCSVData& rDBData)
     //when the address data is updated then remove the controls an build again
     if(!m_aFixedTexts.empty())
     {
-        for(auto aTextIter = m_aFixedTexts.begin(); aTextIter != m_aFixedTexts.end(); ++aTextIter)
-            aTextIter->disposeAndClear();
+        for(auto& rText : m_aFixedTexts)
+            rText.disposeAndClear();
         m_aFixedTexts.clear();
-        for(auto aEditIter = m_aEdits.begin(); aEditIter != m_aEdits.end(); ++aEditIter)
-            aEditIter->disposeAndClear();
+        for(auto& rEdit : m_aEdits)
+            rEdit.disposeAndClear();
         m_aEdits.clear();
         m_bNoDataSet = true;
     }
     //now create appropriate controls
-
-    std::vector< OUString >::iterator    aHeaderIter;
 
     long nFTXPos = m_pWindow->LogicToPixel(Point(RSC_SP_CTRL_X, RSC_SP_CTRL_X), MapMode(MapUnit::MapAppFont)).X();
     long nFTHeight = m_pWindow->LogicToPixel(Size(RSC_BS_CHARHEIGHT, RSC_BS_CHARHEIGHT), MapMode(MapUnit::MapAppFont)).Height();
     long nFTWidth = 0;
 
     //determine the width of the FixedTexts
-    for(aHeaderIter = m_pData->aDBColumnHeaders.begin();
-                aHeaderIter != m_pData->aDBColumnHeaders.end();
-                ++aHeaderIter)
+    for(const auto& rHeader : m_pData->aDBColumnHeaders)
     {
-        sal_Int32 nTemp = m_pWindow->GetTextWidth(*aHeaderIter);
+        sal_Int32 nTemp = m_pWindow->GetTextWidth(rHeader);
         if(nTemp > nFTWidth)
           nFTWidth = nTemp;
     }
@@ -178,9 +174,7 @@ void SwAddressControl_Impl::SetData(SwCSVData& rDBData)
     Edit* pLastEdit = nullptr;
     sal_Int32 nVisibleLines = 0;
     sal_Int32 nLines = 0;
-    for(aHeaderIter = m_pData->aDBColumnHeaders.begin();
-                aHeaderIter != m_pData->aDBColumnHeaders.end();
-                ++aHeaderIter, nEDYPos += m_nLineHeight, nFTYPos += m_nLineHeight, nLines++)
+    for(const auto& rHeader : m_pData->aDBColumnHeaders)
     {
         VclPtr<FixedText> pNewFT = VclPtr<FixedText>::Create(m_pWindow, WB_RIGHT);
         VclPtr<Edit> pNewED = VclPtr<Edit>::Create(m_pWindow, WB_BORDER);
@@ -194,13 +188,16 @@ void SwAddressControl_Impl::SetData(SwCSVData& rDBData)
         if(nEDYPos + nEDHeight < m_aWinOutputSize.Height())
             ++nVisibleLines;
 
-        pNewFT->SetText(*aHeaderIter);
+        pNewFT->SetText(rHeader);
 
         pNewFT->Show();
         pNewED->Show();
         m_aFixedTexts.push_back(pNewFT);
         m_aEdits.push_back(pNewED);
         pLastEdit = pNewED;
+        nEDYPos += m_nLineHeight;
+        nFTYPos += m_nLineHeight;
+        nLines++;
     }
     //scrollbar adjustment
     if(pLastEdit)
@@ -251,11 +248,12 @@ void SwAddressControl_Impl::SetCurrentDataSet(sal_uInt32 nSet)
         if(m_pData->aDBData.size() > m_nCurrentDataSet)
         {
             sal_uInt32 nIndex = 0;
-            for(auto aEditIter = m_aEdits.begin(); aEditIter != m_aEdits.end(); ++aEditIter, ++nIndex)
+            for(auto& rEdit : m_aEdits)
             {
                 OSL_ENSURE(nIndex < m_pData->aDBData[m_nCurrentDataSet].size(),
                             "number of columns doesn't match number of Edits");
-                (*aEditIter)->SetText(m_pData->aDBData[m_nCurrentDataSet][nIndex]);
+                rEdit->SetText(m_pData->aDBData[m_nCurrentDataSet][nIndex]);
+                ++nIndex;
             }
         }
     }
@@ -381,9 +379,9 @@ void SwAddressControl_Impl::Resize()
     {
         long nNewEditSize = aSize.Width() - (*m_aEdits.begin())->GetPosPixel().X() - nScrollBarWidth - 6;
 
-        for(auto aEditIter = m_aEdits.begin(); aEditIter != m_aEdits.end(); ++aEditIter)
+        for(auto& rEdit : m_aEdits)
         {
-            (*aEditIter)->SetSizePixel(Size(nNewEditSize, (*aEditIter)->GetSizePixel().Height()));
+            rEdit->SetSizePixel(Size(nNewEditSize, rEdit->GetSizePixel().Height()));
         }
     }
 
@@ -552,11 +550,8 @@ IMPL_LINK_NOARG(SwCreateAddressListDialog, FindHdl_Impl, Button*, void)
     {
         m_xFindDlg.reset(new SwFindEntryDialog(this));
         weld::ComboBox& rColumnBox = m_xFindDlg->GetFieldsListBox();
-        std::vector< OUString >::iterator    aHeaderIter;
-        for(aHeaderIter = m_pCSVData->aDBColumnHeaders.begin();
-                    aHeaderIter != m_pCSVData->aDBColumnHeaders.end();
-                    ++aHeaderIter)
-            rColumnBox.append_text(*aHeaderIter);
+        for(const auto& rHeader : m_pCSVData->aDBColumnHeaders)
+            rColumnBox.append_text(rHeader);
         rColumnBox.set_active(0);
         m_xFindDlg->show();
     }
@@ -579,11 +574,8 @@ IMPL_LINK_NOARG(SwCreateAddressListDialog, CustomizeHdl_Impl, Button*, void)
     {
         weld::ComboBox& rColumnBox = m_xFindDlg->GetFieldsListBox();
         rColumnBox.clear();
-        std::vector< OUString >::iterator    aHeaderIter;
-        for(aHeaderIter = m_pCSVData->aDBColumnHeaders.begin();
-                    aHeaderIter != m_pCSVData->aDBColumnHeaders.end();
-                    ++aHeaderIter)
-            rColumnBox.append_text(*aHeaderIter);
+        for(const auto& rHeader : m_pCSVData->aDBColumnHeaders)
+            rColumnBox.append_text(rHeader);
     }
 }
 
@@ -643,9 +635,9 @@ IMPL_LINK_NOARG(SwCreateAddressListDialog, OkHdl_Impl, Button*, void)
         lcl_WriteValues(&(m_pCSVData->aDBColumnHeaders), pStream);
 
         std::vector< std::vector< OUString > >::iterator aDataIter;
-        for( aDataIter = m_pCSVData->aDBData.begin(); aDataIter != m_pCSVData->aDBData.end(); ++aDataIter)
+        for(const auto& rData : m_pCSVData->aDBData)
         {
-            lcl_WriteValues(&(*aDataIter), pStream);
+            lcl_WriteValues(&rData, pStream);
         }
         aMedium.Commit();
         EndDialog(RET_OK);

sw/source/ui/dbui/customizeaddresslistdialog.cxx
@@ -46,11 +46,8 @@ SwCustomizeAddressListDialog::SwCustomizeAddressListDialog(
     m_xUpPB->connect_clicked(aUpDownLk);
     m_xDownPB->connect_clicked(aUpDownLk);
 
-    std::vector< OUString >::iterator aHeaderIter;
-
-    for(aHeaderIter = m_xNewData->aDBColumnHeaders.begin();
-                aHeaderIter != m_xNewData->aDBColumnHeaders.end(); ++aHeaderIter)
-        m_xFieldsLB->append_text(*aHeaderIter);
+    for (const auto& rHeader : m_xNewData->aDBColumnHeaders)
+        m_xFieldsLB->append_text(rHeader);
 
     m_xFieldsLB->select(0);
     UpdateButtons();
@@ -97,9 +94,8 @@ IMPL_LINK(SwCustomizeAddressListDialog, AddRenameHdl_Impl, weld::Button&, rButto
             //add the new column
             m_xNewData->aDBColumnHeaders.insert(m_xNewData->aDBColumnHeaders.begin() + nPos, sNew);
             //add a new entry into all data arrays
-            std::vector< std::vector< OUString > >::iterator aDataIter;
-            for( aDataIter = m_xNewData->aDBData.begin(); aDataIter != m_xNewData->aDBData.end(); ++aDataIter)
-                aDataIter->insert(aDataIter->begin() + nPos, OUString());
+            for (auto& rData : m_xNewData->aDBData)
+                rData.insert(rData.begin() + nPos, OUString());
 
         }
 
@@ -118,9 +114,8 @@ IMPL_LINK_NOARG(SwCustomizeAddressListDialog, DeleteHdl_Impl, weld::Button&, voi
     //remove the column
     m_xNewData->aDBColumnHeaders.erase(m_xNewData->aDBColumnHeaders.begin() + nPos);
     //remove the data
-    std::vector< std::vector< OUString > >::iterator aDataIter;
-    for( aDataIter = m_xNewData->aDBData.begin(); aDataIter != m_xNewData->aDBData.end(); ++aDataIter)
-        aDataIter->erase(aDataIter->begin() + nPos);
+    for (auto& rData : m_xNewData->aDBData)
+        rData.erase(rData.begin() + nPos);
 
     UpdateButtons();
 }
@@ -141,12 +136,11 @@ IMPL_LINK(SwCustomizeAddressListDialog, UpDownHdl_Impl, weld::Button&, rButton, 
     OUString sHeader = m_xNewData->aDBColumnHeaders[nOldPos];
     m_xNewData->aDBColumnHeaders.erase(m_xNewData->aDBColumnHeaders.begin() + nOldPos);
     m_xNewData->aDBColumnHeaders.insert(m_xNewData->aDBColumnHeaders.begin() + nPos, sHeader);
-    std::vector< std::vector< OUString > >::iterator aDataIter;
-    for( aDataIter = m_xNewData->aDBData.begin(); aDataIter != m_xNewData->aDBData.end(); ++aDataIter)
+    for (auto& rData : m_xNewData->aDBData)
     {
-        OUString sData = (*aDataIter)[nOldPos];
-        aDataIter->erase(aDataIter->begin() + nOldPos);
-        aDataIter->insert(aDataIter->begin() + nPos, sData);
+        OUString sData = rData[nOldPos];
+        rData.erase(rData.begin() + nOldPos);
+        rData.insert(rData.begin() + nPos, sData);
     }
 
     UpdateButtons();
@@ -181,15 +175,8 @@ IMPL_LINK(SwAddRenameEntryDialog, ModifyHdl_Impl, weld::Entry&, rEdit, void)
 
     if(!bFound)
     {
-        std::vector< OUString >::const_iterator aHeaderIter;
-        for(aHeaderIter = m_rCSVHeader.begin();
-                    aHeaderIter != m_rCSVHeader.end();
-                    ++aHeaderIter)
-            if(*aHeaderIter == sEntry)
-            {
-                bFound = true;
-                break;
-            }
+        bFound = std::any_of(m_rCSVHeader.begin(), m_rCSVHeader.end(),
+            [&sEntry](const OUString& rHeader) { return rHeader == sEntry; });
     }
     m_xOK->set_sensitive(!bFound);
 }

sw/source/ui/dbui/mmaddressblockpage.cxx
@@ -696,9 +696,8 @@ IMPL_LINK(SwCustomizeAddressBlockDialog, SelectionChangedHdl_Impl, AddressMultiL
         }
         m_pFieldCB->Clear();
         if(pVector) {
-            std::vector<OUString>::iterator    aIterator;
-            for( aIterator = pVector->begin(); aIterator != pVector->end(); ++aIterator)
-                m_pFieldCB->InsertEntry(*aIterator);
+            for (const auto& rItem : *pVector)
+                m_pFieldCB->InsertEntry(rItem);
         }
         m_pFieldCB->SetText(sSelect);
         m_pFieldCB->Enable();
@@ -970,12 +969,12 @@ SwAssignFieldsControl::~SwAssignFieldsControl()
 
 void SwAssignFieldsControl::dispose()
 {
-    for(auto aFIIter = m_aFieldNames.begin(); aFIIter != m_aFieldNames.end(); ++aFIIter)
-        aFIIter->disposeAndClear();
-    for(auto aLBIter = m_aMatches.begin(); aLBIter != m_aMatches.end(); ++aLBIter)
-        aLBIter->disposeAndClear();
-    for(auto aFIIter = m_aPreviews.begin(); aFIIter != m_aPreviews.end(); ++aFIIter)
-        aFIIter->disposeAndClear();
+    for(auto& rFIItem : m_aFieldNames)
+        rFIItem.disposeAndClear();
+    for(auto& rLBItem : m_aMatches)
+        rLBItem.disposeAndClear();
+    for(auto& rFIItem : m_aPreviews)
+        rFIItem.disposeAndClear();
 
     m_aFieldNames.clear();
     m_aMatches.clear();
@@ -1015,17 +1014,17 @@ void SwAssignFieldsControl::Resize()
     long nControlHeight = std::max(m_aFieldNames[0]->get_preferred_size().Height(),
                                    m_aMatches[0]->get_preferred_size().Height());
 
-    for(auto aFIIter = m_aFieldNames.begin(); aFIIter != m_aFieldNames.end(); ++aFIIter)
-        (*aFIIter)->SetSizePixel(Size(nColWidth - 6, nControlHeight));
-    for(auto aLBIter = m_aMatches.begin(); aLBIter != m_aMatches.end(); ++aLBIter)
+    for(auto& rFIItem : m_aFieldNames)
+        rFIItem->SetSizePixel(Size(nColWidth - 6, nControlHeight));
+    for(auto& rLBItem : m_aMatches)
     {
-        long nPosY = (*aLBIter)->GetPosPixel().Y();
-        (*aLBIter)->SetPosSizePixel(Point(nColWidth, nPosY), Size(nColWidth - 6, nControlHeight));
+        long nPosY = rLBItem->GetPosPixel().Y();
+        rLBItem->SetPosSizePixel(Point(nColWidth, nPosY), Size(nColWidth - 6, nControlHeight));
     }
-    for(auto aFIIter = m_aPreviews.begin(); aFIIter != m_aPreviews.end(); ++aFIIter)
+    for(auto& rFIItem : m_aPreviews)
     {
-        long nPosY = (*aFIIter)->GetPosPixel().Y();
-        (*aFIIter)->SetPosSizePixel(Point(2 * nColWidth + 6, nPosY), Size(nColWidth, nControlHeight));
+        long nPosY = rFIItem->GetPosPixel().Y();
+        rFIItem->SetPosSizePixel(Point(2 * nColWidth + 6, nPosY), Size(nColWidth, nControlHeight));
     }
 }
 
@@ -1086,12 +1085,12 @@ IMPL_LINK(SwAssignFieldsControl, ScrollHdl_Impl, ScrollBar*, pScroll, void)
     long nMove = m_nFirstYPos - (*m_aMatches.begin())->GetPosPixel().Y() - (nThumb * m_nYOffset);
 
     SetUpdateMode(false);
-    for(auto aFIIter = m_aFieldNames.begin(); aFIIter != m_aFieldNames.end(); ++aFIIter)
-        lcl_Move(*aFIIter, nMove);
-    for(auto aLBIter = m_aMatches.begin(); aLBIter != m_aMatches.end(); ++aLBIter)
-        lcl_Move(*aLBIter, nMove);
-    for(auto aFIIter = m_aPreviews.begin(); aFIIter != m_aPreviews.end(); ++aFIIter)
-        lcl_Move(*aFIIter, nMove);
+    for(auto& rFIItem : m_aFieldNames)
+        lcl_Move(rFIItem, nMove);
+    for(auto& rLBItem : m_aMatches)
+        lcl_Move(rLBItem, nMove);
+    for(auto& rFIItem : m_aPreviews)
+        lcl_Move(rFIItem, nMove);
     SetUpdateMode(true);
 }
 
@@ -1117,14 +1116,11 @@ IMPL_LINK(SwAssignFieldsControl, MatchHdl_Impl, ListBox&, rBox, void)
             }
         }
     }
-    sal_Int32 nIndex = 0;
-    for(auto aLBIter = m_aMatches.begin(); aLBIter != m_aMatches.end(); ++aLBIter, ++nIndex)
+    auto aLBIter = std::find(m_aMatches.begin(), m_aMatches.end(), &rBox);
+    if(aLBIter != m_aMatches.end())
     {
-        if(*aLBIter == &rBox)
-        {
-            m_aPreviews[nIndex]->SetText(sPreview);
-            break;
-        }
+        auto nIndex = static_cast<sal_Int32>(std::distance(m_aMatches.begin(), aLBIter));
+        m_aPreviews[nIndex]->SetText(sPreview);
     }
     m_aModifyHdl.Call(nullptr);
 }
@@ -1134,14 +1130,11 @@ IMPL_LINK(SwAssignFieldsControl, GotFocusHdl_Impl, Control&, rControl, void)
     ListBox* pBox = static_cast<ListBox*>(&rControl);
     if(GetFocusFlags::Tab & pBox->GetGetFocusFlags())
     {
-        sal_Int32 nIndex = 0;
-        for(auto aLBIter = m_aMatches.begin(); aLBIter != m_aMatches.end(); ++aLBIter, ++nIndex)
+        auto aLBIter = std::find(m_aMatches.begin(), m_aMatches.end(), pBox);
+        if(aLBIter != m_aMatches.end())
         {
-            if(*aLBIter == pBox)
-            {
-                MakeVisible(nIndex);
-                break;
-            }
+            auto nIndex = static_cast<sal_Int32>(std::distance(m_aMatches.begin(), aLBIter));
+            MakeVisible(nIndex);
         }
     }
 }
@@ -1212,12 +1205,11 @@ uno::Sequence< OUString > SwAssignFieldsDialog::CreateAssignments()
             m_rConfigItem.GetDefaultAddressHeaders().size());
     OUString* pAssignments = aAssignments.getArray();
     sal_Int32 nIndex = 0;
-    for(auto aLBIter = m_pFieldsControl->m_aMatches.begin();
-                aLBIter != m_pFieldsControl->m_aMatches.end();
-                    ++aLBIter, ++nIndex)
+    for(const auto& rLBItem : m_pFieldsControl->m_aMatches)
     {
-        const OUString sSelect = (*aLBIter)->GetSelectedEntry();
+        const OUString sSelect = rLBItem->GetSelectedEntry();
         pAssignments[nIndex] = (m_sNone != sSelect) ? sSelect : OUString();
+        ++nIndex;
     }
     return aAssignments;
 }

sw/source/ui/dialog/ascfldlg.cxx
@@ -204,10 +204,9 @@ SwAsciiFilterDlg::SwAsciiFilterDlg( weld::Window* pParent, SwDocShell& rDocSh,
             }
 
             // insert into listbox
-            for( std::set< OUString >::const_iterator it = aFontNames.begin();
-                 it != aFontNames.end(); ++it )
+            for( const auto& rFontName : aFontNames )
             {
-                m_xFontLB->append_text(*it);
+                m_xFontLB->append_text(rFontName);
             }
 
             if( aOpt.GetFontName().isEmpty() )

commit 0a12fb597c82e1aad79cdda45d6bc2a380ecec3f

tdf#121179 FILEOPEN: Crash opening a certain file (gtk/gtk3)
regression from
    commit 16b2b4f27acb83fc651b8484dead53ebd0e269e1
    Simplify containers iterations in sw/source/ui*

Change-Id: Id77c41acf1cb891ffa23ca1300633031e367d0d1

sw/source/uibase/docvw/frmsidebarwincontainer.cxx
@@ -142,7 +142,8 @@ sw::annotation::SwAnnotationWin* SwFrameSidebarWinContainer::get( const SwFrame&
         SidebarWinContainer& rSidebarWinContainer = (*aFrameIter).second;
         auto aIter = rSidebarWinContainer.begin();
         std::advance(aIter, nIndex);
-        pRet = (*aIter).second;
+        if (aIter != rSidebarWinContainer.end())
+            pRet = (*aIter).second;
     }
 
     return pRet;