LibreOffice/core

commit f74b888244fcefa7c03fa25db4d42c839ebbf642

simplify PlusHdl logic
like we did with the AddToHdlList stuff, all the client code cares about
is fetching the entire list

Change-Id: Id3cefa5f316a3f979a276c64f9125943d0981842
Reviewed-on: https://gerrit.libreoffice.org/60813
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svx/source/svdraw/svdopath.cxx
@@ -2012,80 +2012,48 @@ void SdrPathObj::AddToHdlList(SdrHdlList& rHdlList) const
     }
 }
 
-sal_uInt32 SdrPathObj::GetPlusHdlCount(const SdrHdl& rHdl) const
+void SdrPathObj::AddToPlusHdlList(SdrHdlList& rHdlList, SdrHdl& rHdl) const
 {
     // keep old stuff to be able to keep old SdrHdl stuff, too
     const XPolyPolygon aOldPathPolygon(GetPathPoly());
-    sal_uInt16 nCnt = 0;
     sal_uInt16 nPnt = static_cast<sal_uInt16>(rHdl.GetPointNum());
     sal_uInt16 nPolyNum = static_cast<sal_uInt16>(rHdl.GetPolyNum());
 
-    if(nPolyNum < aOldPathPolygon.Count())
+    if (nPolyNum>=aOldPathPolygon.Count())
+        return;
+
+    const XPolygon& rXPoly = aOldPathPolygon[nPolyNum];
+    sal_uInt16 nPntMax = rXPoly.GetPointCount();
+
+    if (nPntMax<=0)
+        return;
+
+    nPntMax--;
+    for (sal_uInt32 nPlusNum = 0; nPlusNum <= nPntMax; ++nPlusNum)
     {
-        const XPolygon& rXPoly = aOldPathPolygon[nPolyNum];
-        sal_uInt16 nPntMax = rXPoly.GetPointCount();
-        if (nPntMax>0)
+        SdrHdl* pHdl=new SdrHdlBezWgt(&rHdl);
+        pHdl->SetPolyNum(rHdl.GetPolyNum());
+
+        if (nPnt==0 && IsClosed()) nPnt=nPntMax;
+        if (nPnt>0 && rXPoly.GetFlags(nPnt-1)==PolyFlags::Control && nPlusNum==0)
         {
-            nPntMax--;
-            if (nPnt<=nPntMax)
+            pHdl->SetPos(rXPoly[nPnt-1]);
+            pHdl->SetPointNum(nPnt-1);
+        }
+        else
+        {
+            if (nPnt==nPntMax && IsClosed()) nPnt=0;
+            if (nPnt<rXPoly.GetPointCount()-1 && rXPoly.GetFlags(nPnt+1)==PolyFlags::Control)
             {
-                if (rXPoly.GetFlags(nPnt)!=PolyFlags::Control)
-                {
-                    if (nPnt==0 && IsClosed()) nPnt=nPntMax;
-                    if (nPnt>0 && rXPoly.GetFlags(nPnt-1)==PolyFlags::Control) nCnt++;
-                    if (nPnt==nPntMax && IsClosed()) nPnt=0;
-                    if (nPnt<nPntMax && rXPoly.GetFlags(nPnt+1)==PolyFlags::Control) nCnt++;
-                }
+                pHdl->SetPos(rXPoly[nPnt+1]);
+                pHdl->SetPointNum(nPnt+1);
             }
         }
+
+        pHdl->SetSourceHdlNum(rHdl.GetSourceHdlNum());
+        pHdl->SetPlusHdl(true);
+        rHdlList.AddHdl(pHdl);
     }
-
-    return nCnt;
-}
-
-SdrHdl* SdrPathObj::GetPlusHdl(const SdrHdl& rHdl, sal_uInt32 nPlusNum) const
-{
-    // keep old stuff to be able to keep old SdrHdl stuff, too
-    const XPolyPolygon aOldPathPolygon(GetPathPoly());
-    SdrHdl* pHdl = nullptr;
-    sal_uInt16 nPnt = static_cast<sal_uInt16>(rHdl.GetPointNum());
-    sal_uInt16 nPolyNum = static_cast<sal_uInt16>(rHdl.GetPolyNum());
-
-    if (nPolyNum<aOldPathPolygon.Count())
-    {
-        const XPolygon& rXPoly = aOldPathPolygon[nPolyNum];
-        sal_uInt16 nPntMax = rXPoly.GetPointCount();
-
-        if (nPntMax>0)
-        {
-            nPntMax--;
-            if (nPnt<=nPntMax)
-            {
-                pHdl=new SdrHdlBezWgt(&rHdl);
-                pHdl->SetPolyNum(rHdl.GetPolyNum());
-
-                if (nPnt==0 && IsClosed()) nPnt=nPntMax;
-                if (nPnt>0 && rXPoly.GetFlags(nPnt-1)==PolyFlags::Control && nPlusNum==0)
-                {
-                    pHdl->SetPos(rXPoly[nPnt-1]);
-                    pHdl->SetPointNum(nPnt-1);
-                }
-                else
-                {
-                    if (nPnt==nPntMax && IsClosed()) nPnt=0;
-                    if (nPnt<rXPoly.GetPointCount()-1 && rXPoly.GetFlags(nPnt+1)==PolyFlags::Control)
-                    {
-                        pHdl->SetPos(rXPoly[nPnt+1]);
-                        pHdl->SetPointNum(nPnt+1);
-                    }
-                }
-
-                pHdl->SetSourceHdlNum(rHdl.GetSourceHdlNum());
-                pHdl->SetPlusHdl(true);
-            }
-        }
-    }
-    return pHdl;
 }
 
 // dragging

include/svx/svdobj.hxx
@@ -521,8 +521,7 @@ public:
     /// An object that returns true from HasSpacialDrag() must provide these
     /// methods (incl. FillHdlList()).
     virtual sal_uInt32 GetHdlCount() const;
-    virtual sal_uInt32 GetPlusHdlCount(const SdrHdl& rHdl) const;
-    virtual SdrHdl* GetPlusHdl(const SdrHdl& rHdl, sal_uInt32 nPlNum) const;
+    virtual void AddToPlusHdlList(SdrHdlList& rHdlList, SdrHdl& rHdl) const;
     virtual void AddToHdlList(SdrHdlList& rHdlList) const;
     virtual void addCropHandles(SdrHdlList& rTarget) const;
 

include/svx/svdopath.hxx
@@ -87,8 +87,7 @@ public:
     virtual void NbcSetSnapRect(const tools::Rectangle& rRect) override;
     virtual sal_uInt32 GetHdlCount() const override;
     virtual void AddToHdlList(SdrHdlList& rHdlList) const override;
-    virtual sal_uInt32 GetPlusHdlCount(const SdrHdl& rHdl) const override;
-    virtual SdrHdl* GetPlusHdl(const SdrHdl& rHdl, sal_uInt32 nPlNum) const override;
+    virtual void AddToPlusHdlList(SdrHdlList& rHdlList, SdrHdl& rHdl) const override;
 
     // special drag methods
     virtual bool hasSpecialDrag() const override;

include/svx/svdovirt.hxx
@@ -77,8 +77,7 @@ public:
 
     virtual basegfx::B2DPolyPolygon TakeXorPoly() const override;
     virtual sal_uInt32 GetHdlCount() const override;
-    virtual sal_uInt32 GetPlusHdlCount(const SdrHdl& rHdl) const override;
-    virtual SdrHdl* GetPlusHdl(const SdrHdl& rHdl, sal_uInt32 nPlNum) const override;
+    virtual void AddToPlusHdlList(SdrHdlList& rHdlList, SdrHdl& rHdl) const override;
     virtual void AddToHdlList(SdrHdlList& rHdlList) const override;
 
     // special drag methods

sd/source/ui/animations/motionpathtag.cxx
@@ -915,18 +915,17 @@ void MotionPathTag::addCustomHandles( SdrHdlList& rHandlerList )
 
                     if( mrView.IsPlusHandlesAlwaysVisible() || bSelected )
                     {
-                        sal_uInt32 nPlusHdlCnt=mpPathObj->GetPlusHdlCount(*pSmartHdl);
+                        SdrHdlList plusList(nullptr);
+                        mpPathObj->AddToPlusHdlList(plusList, *pSmartHdl);
+                        sal_uInt32 nPlusHdlCnt=plusList.GetHdlCount();
                         for (sal_uInt32 nPlusNum=0; nPlusNum<nPlusHdlCnt; nPlusNum++)
                         {
-                            SdrHdl* pPlusHdl = mpPathObj->GetPlusHdl(*pSmartHdl,nPlusNum);
-                            if (pPlusHdl!=nullptr)
-                            {
-                                pPlusHdl->SetObj(mpPathObj);
-                                pPlusHdl->SetPageView(mrView.GetSdrPageView());
-                                pPlusHdl->SetPlusHdl(true);
-                                rHandlerList.AddHdl(pPlusHdl);
-                            }
+                            SdrHdl* pPlusHdl = plusList.GetHdl(nPlusNum);
+                            pPlusHdl->SetObj(mpPathObj);
+                            pPlusHdl->SetPageView(mrView.GetSdrPageView());
+                            pPlusHdl->SetPlusHdl(true);
                         }
+                        plusList.MoveTo(rHandlerList);
                     }
                 }
             }

svx/source/svdraw/svdmrkv.cxx
@@ -918,18 +918,17 @@ void SdrMarkView::SetMarkHandles(SfxViewShell* pOtherShell)
                         pHdl->SetSelected(bSelected);
                         if (mbPlusHdlAlways || bSelected)
                         {
-                            sal_uInt32 nPlusHdlCnt=pObj->GetPlusHdlCount(*pHdl);
+                            SdrHdlList plusList(nullptr);
+                            pObj->AddToPlusHdlList(plusList, *pHdl);
+                            sal_uInt32 nPlusHdlCnt=plusList.GetHdlCount();
                             for (sal_uInt32 nPlusNum=0; nPlusNum<nPlusHdlCnt; nPlusNum++)
                             {
-                                SdrHdl* pPlusHdl=pObj->GetPlusHdl(*pHdl,nPlusNum);
-                                if (pPlusHdl!=nullptr)
-                                {
-                                    pPlusHdl->SetObj(pObj);
-                                    pPlusHdl->SetPageView(pPV);
-                                    pPlusHdl->SetPlusHdl(true);
-                                    maHdlList.AddHdl(pPlusHdl);
-                                }
+                                SdrHdl* pPlusHdl=plusList.GetHdl(nPlusNum);
+                                pPlusHdl->SetObj(pObj);
+                                pPlusHdl->SetPageView(pPV);
+                                pPlusHdl->SetPlusHdl(true);
                             }
+                            plusList.MoveTo(maHdlList);
                         }
                     }
                 }

svx/source/svdraw/svdmrkv1.cxx
@@ -152,18 +152,17 @@ bool SdrMarkView::ImpMarkPoint(SdrHdl* pHdl, SdrMark* pMark, bool bUnmark)
     {
         if (!bUnmark)
         {
-            sal_uInt32 nCount(pObj->GetPlusHdlCount(*pHdl));
+            SdrHdlList plusList(nullptr);
+            pObj->AddToPlusHdlList(plusList, *pHdl);
+            sal_uInt32 nCount(plusList.GetHdlCount());
             for (sal_uInt32 i=0; i<nCount; i++)
             {
-                SdrHdl* pPlusHdl=pObj->GetPlusHdl(*pHdl,i);
-                if (pPlusHdl!=nullptr)
-                {
-                    pPlusHdl->SetObj(pObj);
-                    pPlusHdl->SetPageView(pMark->GetPageView());
-                    pPlusHdl->SetPlusHdl(true);
-                    maHdlList.AddHdl(pPlusHdl);
-                }
+                SdrHdl* pPlusHdl=plusList.GetHdl(i);
+                pPlusHdl->SetObj(pObj);
+                pPlusHdl->SetPageView(pMark->GetPageView());
+                pPlusHdl->SetPlusHdl(true);
             }
+            plusList.MoveTo(maHdlList);
         }
         else
         {

svx/source/svdraw/svdobj.cxx
@@ -1194,14 +1194,8 @@ void SdrObject::AddToHdlList(SdrHdlList& rHdlList) const
     }
 }
 
-sal_uInt32 SdrObject::GetPlusHdlCount(const SdrHdl& /*rHdl*/) const
+void SdrObject::AddToPlusHdlList(SdrHdlList&, SdrHdl&) const
 {
-    return 0L;
-}
-
-SdrHdl* SdrObject::GetPlusHdl(const SdrHdl& /*rHdl*/, sal_uInt32 /*nPlNum*/) const
-{
-    return nullptr;
 }
 
 void SdrObject::addCropHandles(SdrHdlList& /*rTarget*/) const

commit 7b6c9ded54ec534ce3d94f108e8ebfc53311e0ae

tdf120965 Phantom control point line in Bézier curve
regression from
    commit f74b888244fcefa7c03fa25db4d42c839ebbf642
    simplify PlusHdl logic
restore the structure of the original code here, which is completely
opaque to me

Change-Id: I7b9e5732bd7ed0a92627503ea3c7d2d320a8a33b

svx/source/svdraw/svdopath.cxx
@@ -2021,14 +2021,33 @@ void SdrPathObj::AddToPlusHdlList(SdrHdlList& rHdlList, SdrHdl& rHdl) const
 
     if (nPntMax<=0)
         return;
-
     nPntMax--;
-    for (sal_uInt32 nPlusNum = 0; nPlusNum <= nPntMax; ++nPlusNum)
+    if (nPnt>nPntMax)
+        return;
+
+    // calculate the number of plus points
+    sal_uInt16 nCnt = 0;
+    if (rXPoly.GetFlags(nPnt)!=PolyFlags::Control)
     {
+        if (nPnt==0 && IsClosed())
+            nPnt=nPntMax;
+        if (nPnt>0 && rXPoly.GetFlags(nPnt-1)==PolyFlags::Control)
+            nCnt++;
+        if (nPnt==nPntMax && IsClosed())
+            nPnt=0;
+        if (nPnt<nPntMax && rXPoly.GetFlags(nPnt+1)==PolyFlags::Control)
+            nCnt++;
+    }
+
+    // construct the plus points
+    for (sal_uInt32 nPlusNum = 0; nPlusNum < nCnt; ++nPlusNum)
+    {
+        nPnt = static_cast<sal_uInt16>(rHdl.GetPointNum());
         std::unique_ptr<SdrHdl> pHdl(new SdrHdlBezWgt(&rHdl));
         pHdl->SetPolyNum(rHdl.GetPolyNum());
 
-        if (nPnt==0 && IsClosed()) nPnt=nPntMax;
+        if (nPnt==0 && IsClosed())
+            nPnt=nPntMax;
         if (nPnt>0 && rXPoly.GetFlags(nPnt-1)==PolyFlags::Control && nPlusNum==0)
         {
             pHdl->SetPos(rXPoly[nPnt-1]);
@@ -2036,7 +2055,8 @@ void SdrPathObj::AddToPlusHdlList(SdrHdlList& rHdlList, SdrHdl& rHdl) const
         }
         else
         {
-            if (nPnt==nPntMax && IsClosed()) nPnt=0;
+            if (nPnt==nPntMax && IsClosed())
+                nPnt=0;
             if (nPnt<rXPoly.GetPointCount()-1 && rXPoly.GetFlags(nPnt+1)==PolyFlags::Control)
             {
                 pHdl->SetPos(rXPoly[nPnt+1]);