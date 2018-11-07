LibreOffice/core

commit 9ec8bf8f22fe74884185492ef2576ce79b41e4f1

add SvStream::TellEnd
and simplify callsites to use it instead of the current
    "seek to end, find pos, seek back to original pos"
pattern

Change-Id: Ib5828868f73c341891efc759af8bd4695ae2f33c
Reviewed-on: https://gerrit.libreoffice.org/61738
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

include/tools/stream.hxx
@@ -273,6 +273,7 @@ public:
     sal_uInt64      Seek( sal_uInt64 nPos );
     sal_uInt64      SeekRel( sal_Int64 nPos );
     sal_uInt64      Tell() const { return m_nBufFilePos + m_nBufActualPos;  }
+    sal_uInt64      TellEnd() { return Tell() + remainingSize();  }
     // length between current (Tell()) pos and end of stream
     virtual sal_uInt64 remainingSize();
     void            Flush();

sot/source/sdstor/storage.cxx
@@ -177,10 +177,7 @@ void SotStorageStream::SetSize(sal_uInt64 const nNewSize)
 
 sal_uInt32 SotStorageStream::GetSize() const
 {
-    sal_uInt64 nPos = Tell();
-    const_cast<SotStorageStream *>(this)->Seek( STREAM_SEEK_TO_END );
-    sal_uInt64 nSize = Tell();
-    const_cast<SotStorageStream *>(this)->Seek( nPos );
+    sal_uInt64 nSize = const_cast<SotStorageStream*>(this)->TellEnd();
     return nSize;
 }
 

sw/source/filter/ww8/ww8scan.cxx
@@ -6264,9 +6264,7 @@ void WW8Fib::WriteHeader(SvStream& rStrm)
     sal_uInt8 *pData = pDataPtr.get();
     memset( pData, 0, nUnencryptedHdr );
 
-    const sal_uInt64 nPos = rStrm.Tell();
-    m_cbMac = rStrm.Seek( STREAM_SEEK_TO_END );
-    rStrm.Seek(nPos);
+    m_cbMac = rStrm.TellEnd();
 
     Set_UInt16( pData, m_wIdent );
     Set_UInt16( pData, m_nFib );
@@ -6351,9 +6349,7 @@ void WW8Fib::Write(SvStream& rStrm)
     sal_uInt8 *pData = pDataPtr.get();
     memset( pData, 0, m_fcMin - nUnencryptedHdr );
 
-    const sal_uInt64 nPos = rStrm.Tell();
-    m_cbMac = rStrm.Seek( STREAM_SEEK_TO_END );
-    rStrm.Seek(nPos);
+    m_cbMac = rStrm.TellEnd();
 
     // ignore 2 longs, because they are unimportant
     pData += 2 * sizeof( sal_Int32);

commit 9dcf430b5205ed0b3b1a25f9978e54bfaf8f6aa9

tdf#120657 Pie chart saved as DOC doesn't preview in MSO
regression from

    commit 9ec8bf8f22fe74884185492ef2576ce79b41e4f1
    add SvStream::TellEnd

the problem is that in this stack trace

    sot/source/sdstor/stgelem.hxx:130
    sot/source/sdstor/stgdir.cxx:300
    sot/source/sdstor/stg.cxx:245
    sot/source/sdstor/storage.cxx:187
    tools/source/stream/stream.cxx:1908
    sw/source/filter/ww8/ww8scan.cxx:6267

The StgEntry returns a size of 0. If we do something else to the
SotStorageStream first, then it materialises a temporary stream, and
that stream returns a valid size.

Also implement suggestions from mike kaganski:

IMO, it would be more robust to implement this new function as virtual
(instead of the remainingSize()), and reimplement the latter as
non-virtual one using the new one and doing the necessary correction.

Change-Id: Id1462d918570a684ec67099c30181436a27d3c6a

include/sot/storage.hxx
@@ -56,7 +56,7 @@ public:
     void                CopyTo( SotStorageStream * pDestStm );
     bool                Commit();
     bool                SetProperty( const OUString& rName, const css::uno::Any& rValue );
-    virtual sal_uInt64 remainingSize() override;
+    virtual sal_uInt64  TellEnd() override;
 };
 
 class  BaseStorage;

include/tools/stream.hxx
@@ -272,9 +272,9 @@ public:
     sal_uInt64      Seek( sal_uInt64 nPos );
     sal_uInt64      SeekRel( sal_Int64 nPos );
     sal_uInt64      Tell() const { return m_nBufFilePos + m_nBufActualPos;  }
-    sal_uInt64      TellEnd();
+    virtual sal_uInt64 TellEnd();
     // length between current (Tell()) pos and end of stream
-    virtual sal_uInt64 remainingSize();
+    sal_uInt64      remainingSize();
     void            Flush();
     // next Tell() <= nSize
     bool            SetStreamSize( sal_uInt64 nSize );
@@ -669,7 +669,7 @@ public:
 
     void            ObjectOwnsMemory( bool bOwn ) { bOwnsData = bOwn; }
     void            SetResizeOffset( std::size_t nNewResize ) { nResize = nNewResize; }
-    virtual sal_uInt64 remainingSize() override { FlushBuffer(true); return GetEndOfData() - Tell(); }
+    virtual sal_uInt64 TellEnd() override { FlushBuffer(true); return nEndOfData; }
 };
 
 #endif

sot/source/sdstor/storage.cxx
@@ -181,12 +181,12 @@ sal_uInt32 SotStorageStream::GetSize() const
     return nSize;
 }
 
-sal_uInt64 SotStorageStream::remainingSize()
+sal_uInt64 SotStorageStream::TellEnd()
 {
     if (pOwnStm)
-        return pOwnStm->GetSize() - Tell();
+        return pOwnStm->GetSize();
 
-    return SvStream::remainingSize();
+    return SvStream::TellEnd();
 }
 
 void SotStorageStream::CopyTo( SotStorageStream * pDestStm )

sw/source/filter/ww8/ww8scan.cxx
@@ -6264,7 +6264,9 @@ void WW8Fib::WriteHeader(SvStream& rStrm)
     sal_uInt8 *pData = pDataPtr.get();
     memset( pData, 0, nUnencryptedHdr );
 
-    m_cbMac = rStrm.TellEnd();
+    const sal_uInt64 nPos = rStrm.Tell();
+    m_cbMac = rStrm.Seek( STREAM_SEEK_TO_END );
+    rStrm.Seek(nPos);
 
     Set_UInt16( pData, m_wIdent );
     Set_UInt16( pData, m_nFib );

tools/source/stream/stream.cxx
@@ -1425,10 +1425,19 @@ bool checkSeek(SvStream &rSt, sal_uInt64 nOffset)
 sal_uInt64 SvStream::remainingSize()
 {
     sal_uInt64 const nCurr = Tell();
-    sal_uInt64 const nEnd = Seek(STREAM_SEEK_TO_END);
+    sal_uInt64 const nEnd = TellEnd();
     sal_uInt64 nMaxAvailable = nEnd > nCurr ? (nEnd-nCurr) : 0;
     Seek(nCurr);
     return nMaxAvailable;
+}
+
+sal_uInt64 SvStream::TellEnd()
+{
+    FlushBuffer(true);
+    sal_uInt64 const nCurr = Tell();
+    sal_uInt64 const nEnd = Seek(STREAM_SEEK_TO_END);
+    Seek(nCurr);
+    return nEnd;
 }
 
 void SvStream::Flush()
@@ -1901,11 +1910,6 @@ void SvMemoryStream::SetSize(sal_uInt64 const nNewSize)
 
     long nDiff = static_cast<long>(nNewSize) - static_cast<long>(nSize);
     ReAllocateMemory( nDiff );
-}
-
-sal_uInt64 SvStream::TellEnd()
-{
-    return Tell() + remainingSize();
 }
 
 //Create a OString of nLen bytes from rStream