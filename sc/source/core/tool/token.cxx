8eca52762db45af859116623f48594ff9f7194e8
Luboš Luňák <l.lunak@collabora.com>
2018-11-08 15:08:48 +0100

    blacklist svMatrix for calc threaded calculation
    If a formula contains a matrix, interpreting it will actually result
    in modifying it in some ways (e.g. ScInterpreter::PopRangeMatrix() calls
    SetErrorInterpreter() on the matrix).
    Testcase: rhbz#1645905
    
    Change-Id: I63742449ba0e4e375d6e459dc7bccd7e6b62920e
    Reviewed-on: https://gerrit.libreoffice.org/63187
    Reviewed-by: Michael Meeks <michael.meeks@collabora.com>
    Tested-by: Jenkins

--- sc/source/core/tool/token.cxx
+++ sc/source/core/tool/token.cxx
@@ -1367,6 +1367,7 @@ void ScTokenArray::CheckForThreading( const FormulaToken& r )
             case svExternalDoubleRef:
             case svExternalSingleRef:
             case svExternalName:
+            case svMatrix:
                 SAL_INFO("sc.core.formulagroup", "opcode ocPush: variable type " << StackVarEnumToString(r.GetType())
                     << " disables threaded calculcation of formula group");
                 mbThreadingEnabled = false;
