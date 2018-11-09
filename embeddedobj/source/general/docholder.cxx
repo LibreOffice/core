LibreOffice/core

commit 45519023aa44940a68f40643fdff6551a2b7adb3

Resolves: tdf#119997 toolbar layout unlock doesn't refresh ui
because lock was taken when toolbar layout wasn't set yet

Change-Id: I695493d4fb637210c72b9ce53af9f089c79b0c7b
Reviewed-on: https://gerrit.libreoffice.org/61849
Tested-by: Jenkins
Tested-by: Xisco Faulí <xiscofauli@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sd/source/ui/view/ToolBarManager.cxx
@@ -59,12 +59,10 @@ class ToolBarRules;
 
 /** Lock of the frame::XLayoutManager.
 */
-class LayouterLock
+struct LayouterLock
 {
-public:
     explicit LayouterLock (const Reference<frame::XLayoutManager>& rxLayouter);
     ~LayouterLock();
-private:
     Reference<frame::XLayoutManager> mxLayouter;
 };
 
@@ -579,6 +577,10 @@ void ToolBarManager::Implementation::SetValid (bool bValid)
                 Reference<beans::XPropertySet> xFrameProperties (xFrame, UNO_QUERY_THROW);
                 Any aValue (xFrameProperties->getPropertyValue("LayoutManager"));
                 aValue >>= mxLayouter;
+                // tdf#119997 if mpSynchronousLayouterLock was created before mxLayouter was
+                // set then update it now that its available
+                if (mpSynchronousLayouterLock && !mpSynchronousLayouterLock->mxLayouter)
+                    mpSynchronousLayouterLock->mxLayouter = mxLayouter;
             }
             catch (const RuntimeException&)
             {
@@ -821,11 +823,14 @@ void ToolBarManager::Implementation::Update (
             // Note that the lock count may have been increased since
             // entering this method.  In that case one of the next
             // UnlockUpdate() calls will post the UpdateCallback.
-            if (mnPendingUpdateCall==nullptr && mnLockCount==0)
+            if (mnLockCount==0)
             {
                 mpAsynchronousLayouterLock = std::move(pLocalLayouterLock);
-                mnPendingUpdateCall = Application::PostUserEvent(
-                    LINK(this,ToolBarManager::Implementation,UpdateCallback));
+                if (mnPendingUpdateCall==nullptr)
+                {
+                    mnPendingUpdateCall = Application::PostUserEvent(
+                        LINK(this,ToolBarManager::Implementation,UpdateCallback));
+                }
             }
         }
         else

commit 954e259a5c370ea79583b72e5575096534174a5d

Fix sd toolbar manager lock underflows
Regressions from commit 45519023aa44 ("Resolves: tdf#119997
toolbar layout unlock doesn't refresh ui"), commit c5977a89c28b
("Resolves: tdf#96451 do magic to enable embedded chart sidebar
only for chart") and commit faed29cac93f ("for now show chart
sidebar").

It also adds an assert for the underflow, as nobody cared for
the underflow warnings since some years.

Change-Id: I86bb093987f084c85a7b640a846dfe0aefea9e48

embeddedobj/source/general/docholder.cxx
@@ -691,8 +691,8 @@ bool DocumentHolder::ShowUI( const uno::Reference< css::frame::XLayoutManager >&
         if ( xOwnLM.is() && xDocAreaAcc.is() )
         {
             // make sure that lock state of LM is correct even if an exception is thrown in between
-            bool bUnlock = false;
-            bool bLock = false;
+            bool bUnlockContainerLM = false;
+            bool bLockOwnLM = false;
             try
             {
                 // take over the control over the containers window
@@ -706,18 +706,14 @@ bool DocumentHolder::ShowUI( const uno::Reference< css::frame::XLayoutManager >&
                     // this must be done after merging menus as we won't get the container menu otherwise
                     xContainerLM->setDockingAreaAcceptor( uno::Reference < ui::XDockingAreaAcceptor >() );
 
-                    bool bIsChart = false;
                     uno::Reference< lang::XServiceInfo> xServiceInfo(m_xComponent, uno::UNO_QUERY);
-                    if (xServiceInfo.is() && xServiceInfo->supportsService("com.sun.star.chart2.ChartDocument"))
-                        bIsChart = true;
-                    // prevent further changes at this LM
-                    // TODO: moggi: why is this necessary?
-                    if (!bIsChart)
+                    if (!xServiceInfo.is() || !xServiceInfo->supportsService("com.sun.star.chart2.ChartDocument"))
                     {
-                        xContainerLM->setVisible( false );
+                        // prevent further changes at this LM
+                        xContainerLM->setVisible(false);
                         xContainerLM->lock();
+                        bUnlockContainerLM = true;
                     }
-                    bUnlock = true;
 
                     // by unlocking the LM each layout change will now resize the containers window; pending layouts will be processed now
                     xOwnLM->setVisible( true );
@@ -727,7 +723,7 @@ bool DocumentHolder::ShowUI( const uno::Reference< css::frame::XLayoutManager >&
                         xSupp->setActiveFrame( m_xFrame );
 
                     xOwnLM->unlock();
-                    bLock = true;
+                    bLockOwnLM = true;
                     bResult = true;
 
                     // TODO/LATER: The following action should be done only if the window is not hidden
@@ -746,7 +742,7 @@ bool DocumentHolder::ShowUI( const uno::Reference< css::frame::XLayoutManager >&
                         xSupp->setActiveFrame( nullptr );
 
                     // remove control about containers window from own LM
-                    if ( bLock )
+                    if (bLockOwnLM)
                         xOwnLM->lock();
                     xOwnLM->setVisible( false );
                     xOwnLM->setDockingAreaAcceptor( uno::Reference< css::ui::XDockingAreaAcceptor >() );
@@ -762,7 +758,7 @@ bool DocumentHolder::ShowUI( const uno::Reference< css::frame::XLayoutManager >&
                     // reestablish control of containers window
                     xContainerLM->setDockingAreaAcceptor( xDocAreaAcc );
                     xContainerLM->setVisible( true );
-                    if ( bUnlock )
+                    if (bUnlockContainerLM)
                         xContainerLM->unlock();
                 }
                 catch( const uno::Exception& ) {}
@@ -805,8 +801,12 @@ bool DocumentHolder::HideUI( const uno::Reference< css::frame::XLayoutManager >&
                 xMerge->removeMergedMenuBar();
 
                 xContainerLM->setDockingAreaAcceptor( xDocAreaAcc );
-                xContainerLM->setVisible( true );
-                xContainerLM->unlock();
+                uno::Reference< lang::XServiceInfo> xServiceInfo(m_xComponent, uno::UNO_QUERY);
+                if (!xServiceInfo.is() || !xServiceInfo->supportsService("com.sun.star.chart2.ChartDocument"))
+                {
+                    xContainerLM->setVisible(true);
+                    xContainerLM->unlock();
+                }
 
                 xContainerLM->doLayout();
                 bResult = true;

sd/source/ui/view/ToolBarManager.cxx
@@ -60,11 +60,13 @@ class ToolBarRules;
 
 /** Lock of the frame::XLayoutManager.
 */
-struct LayouterLock
+class LayouterLock
 {
+    Reference<frame::XLayoutManager> mxLayouter;
+public:
     explicit LayouterLock (const Reference<frame::XLayoutManager>& rxLayouter);
     ~LayouterLock();
-    Reference<frame::XLayoutManager> mxLayouter;
+    bool is() const { return mxLayouter.is(); }
 };
 
 /** Store a list of tool bars for each of the tool bar groups.  From
@@ -577,8 +579,8 @@ void ToolBarManager::Implementation::SetValid (bool bValid)
                 aValue >>= mxLayouter;
                 // tdf#119997 if mpSynchronousLayouterLock was created before mxLayouter was
                 // set then update it now that its available
-                if (mpSynchronousLayouterLock && !mpSynchronousLayouterLock->mxLayouter)
-                    mpSynchronousLayouterLock->mxLayouter = mxLayouter;
+                if (mpSynchronousLayouterLock && !mpSynchronousLayouterLock->is())
+                    mpSynchronousLayouterLock.reset(new LayouterLock(mxLayouter));
             }
             catch (const RuntimeException&)
             {

sfx2/source/appl/workwin.cxx
@@ -543,6 +543,7 @@ void SfxWorkWindow::Lock_Impl( bool bLock )
     if ( m_nLock<0 )
     {
         OSL_FAIL("Lock count underflow!");
+        assert(m_nLock >= 0);
         m_nLock = 0;
     }
 