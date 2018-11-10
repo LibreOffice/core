f179a1ee833598d3510e8b36835836d08767a8b7
Tamás Zolnai <tamas.zolnai@collabora.com>
2018-11-10 18:57:26 +0100

    Introduce client-server message for requesting the selected shape as SVG
    It works for Impress only now.
    
    Change-Id: I95e3e37ae7df49b567108f6d6467038b715e886d
    (cherry picked from commit 1a5b2d2b6f28ec33c5e100fbc5b0c3438df6b006)

--- include/LibreOfficeKit/LibreOfficeKit.h
+++ include/LibreOfficeKit/LibreOfficeKit.h
@@ -347,6 +347,11 @@ struct _LibreOfficeKitDocumentClass
     /// @see lok::Document::getSignatureState().
     int (*getSignatureState) (LibreOfficeKitDocument* pThis);
 
+    /// @see lok::Document::renderShapeSelection
+    void (*renderShapeSelection) (LibreOfficeKitDocument* pThis,
+                                  char*& pOutput,
+                                  size_t& nOutputSize);
+
 #endif // defined LOK_USE_UNSTABLE_API || defined LIBO_INTERNAL_ONLY
 };
 
