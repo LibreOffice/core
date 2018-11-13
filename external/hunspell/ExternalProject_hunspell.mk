afd4f48d164ca72a024159e0c6b7a49147351f64
Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>
2018-11-13 13:26:02 +0100

    hunspell: no need for autoreconf when not patching configure
    Also fix some bitrot... The LIBS statement was only active for the
    autoreconf call, while where it would have mattered is the configure
    call itself. Since it was built wihout those for all the time, delete
    it.
    
    also no need for explicit shell call, as it is by default executed in a
    shell.
    
    also fix missing comma in if statement for ENABLE_OPTIMIZED.
    
    Change-Id: I7ced587bcc3488f19100e89b5b02730b9dd86d5f
    Reviewed-on: https://gerrit.libreoffice.org/63329
    Tested-by: Jenkins
    Reviewed-by: Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>

--- external/hunspell/ExternalProject_hunspell.mk
+++ external/hunspell/ExternalProject_hunspell.mk
@@ -23,14 +23,11 @@ endif
 
 $(call gb_ExternalProject_get_state_target,hunspell,build):
 	$(call gb_ExternalProject_run,build,\
-		$(if $(filter iOS MACOSX,$(OS)),ACLOCAL="aclocal -I $(SRCDIR)/m4/mac") \
-		LIBS="$(gb_STDLIBS) $(LIBS)" \
-		autoreconf && \
-		$(SHELL) ./configure --disable-shared --disable-nls --with-pic \
+		./configure --disable-shared --disable-nls --with-pic \
 			$(if $(CROSS_COMPILING),--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM))\
 			$(if $(filter AIX,$(OS)),CFLAGS="-D_LINUX_SOURCE_COMPAT") \
 			$(if $(hunspell_CPPFLAGS),CPPFLAGS='$(hunspell_CPPFLAGS)') \
-			CXXFLAGS="$(CXXFLAGS) $(if $(ENABLE_OPTIMIZED) $(gb_COMPILEROPTFLAGS),$(gb_COMPILERNOOPTFLAGS)) $(if $(debug),$(gb_DEBUGINFO_FLAGS) $(gb_DEBUG_CXXFLAGS))" \
+			CXXFLAGS="$(CXXFLAGS) $(if $(ENABLE_OPTIMIZED),$(gb_COMPILEROPTFLAGS),$(gb_COMPILERNOOPTFLAGS)) $(if $(debug),$(gb_DEBUGINFO_FLAGS) $(gb_DEBUG_CXXFLAGS))" \
 		&& cd src/hunspell && $(MAKE) \
 	)
 
