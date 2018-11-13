a6a48eeef16e473be14642469cd922f177f54998
Stephan Bergmann <sbergman@redhat.com>
2018-11-13 13:05:01 +0100

    tdf#121325: Replace all of given length, even if replacement is shorter
    Both LO and MS Basic restrict the amount of replacement text ("If the Length
    parameter in the <emph>Mid statement</emph> is less than the length of the text
    that you want to replace, the text is reduced to the specified length." in
    helpcontent2/source/text/sbasic/shared/03120306.xhp, resp. "The number of
    characters replaced is always less than or equal to the number of characters in
    Target." at <https://docs.microsoft.com/en-us/dotnet/visual-basic/
    language-reference/statements/mid-statement>).
    
    But cc20344010e94eda22fee662aab966d395a0796a "tdf#111313: Honor bWriteNoLenParam
    in !bCompatibility, too" had introduced a regression (in the non--compatibility-
    mode case), restricting the amount of replaced text to be no more than the
    amount of replacement text, even if the given length argument was larger.
    (Which had already regressed in the past, see
    <https://bugs.documentfoundation.org/show_bug.cgi?id=62090> "Mid statement
    doesn't work as expected".)
    
    Added test cases now.
    
    Change-Id: I21d4409f49a2437eb0e1a1e200561d803c42a24c
    Reviewed-on: https://gerrit.libreoffice.org/63328
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

---
+++ basic/qa/basic_coverage/test_mid_replace_more.vb
@@ -0,0 +1,17 @@
+'
+' This file is part of the LibreOffice project.
+'
+' This Source Code Form is subject to the terms of the Mozilla Public
+' License, v. 2.0. If a copy of the MPL was not distributed with this
+' file, You can obtain one at http://mozilla.org/MPL/2.0/.
+'
+
+Function doUnitTest as Integer
+    s = "The fox jumps"
+    Mid(s, 5, 3, "duck")
+    If (s = "The duc jumps") Then
+        doUnitTest = 1
+    Else
+        doUnitTest = 0
+    End If
+End Function
