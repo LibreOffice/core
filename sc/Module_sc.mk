48f5732a5dfdab3dd848137b12445174e4990b58
Jens Carl <j.carl43@gmx.de>
2018-11-16 22:03:39 +0000

    tdf#45904 Move XEnumeration Java tests to C++
    Move XEnumeration Java tests to C++ for
    ScIndexEnumeration_TableColumnsEnumeration.
    
    Change-Id: Id4a3f78942b3bed2a09f617b961507a01374f455

--- sc/Module_sc.mk
+++ sc/Module_sc.mk
@@ -136,6 +136,7 @@ $(eval $(call gb_Module_add_subsequentcheck_targets,sc,\
 	CppunitTest_sc_headerfootercontentobj \
 	CppunitTest_sc_indexenumeration_ddelinksenumeration \
 	CppunitTest_sc_indexenumeration_scenariosenumeration \
+	CppunitTest_sc_indexenumeration_tablecolumnsenumeration \
 	CppunitTest_sc_indexenumeration_tablerowsenumeration \
 	CppunitTest_sc_indexenumeration_textfieldenumeration \
 	CppunitTest_sc_importdescriptorbaseobj \
