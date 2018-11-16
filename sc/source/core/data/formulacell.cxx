43922f1aaa3d7bced4b02f6662c5c12ee698132f
Luboš Luňák <l.lunak@collabora.com>
2018-11-16 19:42:03 +0100

    make sure opencl is not used from withing ocTableOp
    Table operations do some tricks with reusing a cell with different values,
    and this confuses opencl handling, which operates on a group as a whole.
    Possibly relevant only with SC_FORCE_CALCULATION=opencl,
    sc_subsequent_filters_test fails in testDataTableOneVarXLSX().
    
    Change-Id: I55e0d238061c8a45a2388ccf6c769ff6a1fddbcc
    Reviewed-on: https://gerrit.libreoffice.org/64230
    Tested-by: Jenkins
    Reviewed-by: Luboš Luňák <l.lunak@collabora.com>

--- sc/source/core/data/formulacell.cxx
+++ sc/source/core/data/formulacell.cxx
@@ -4756,6 +4756,10 @@ bool ScFormulaCell::InterpretFormulaGroupOpenCL(sc::FormulaLogger::GroupScope& a
         return false;
     }
 
+    // TableOp does tricks with using a cell with different values, just bail out.
+    if(pDocument->IsInInterpreterTableOp())
+        return false;
+
     if (bDependencyCheckFailed)
         return false;
 
