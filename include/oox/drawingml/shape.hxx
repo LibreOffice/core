c3ec68d14f5b003e7dcbf085afe7be3ec3f20ebc
Miklos Vajna <vmiklos@collabora.com>
2018-11-16 17:34:46 +0100

    oox smartart, accent process: add support for zorder offsets
    The oox::drawingml::Shape -> css::drawing::XShape converter doesn't
    support ZOrder, so just give each drawingml::Shape a default ZOrder.
    This way the offsets can be applied, and sorting can move the shapes to
    their correct place.
    
    This makes parent text of the bugdoc readable.
    
    Change-Id: Ib87a096fba66aad4a4f35d19473ea88dab340fd0

--- include/oox/drawingml/shape.hxx
+++ include/oox/drawingml/shape.hxx
@@ -206,6 +206,14 @@ public:
     const LinkedTxbxAttr&     getLinkedTxbxAttributes() { return maLinkedTxbxAttr; };
     bool                isLinkedTxbx() { return mbHasLinkedTxbx; };
 
+    void setZOrder(sal_Int32 nZOrder) { mnZOrder = nZOrder; }
+
+    sal_Int32 getZOrder() const { return mnZOrder; }
+
+    void setZOrderOff(sal_Int32 nZOrderOff) { mnZOrderOff = nZOrderOff; }
+
+    sal_Int32 getZOrderOff() const { return mnZOrderOff; }
+
 protected:
 
     css::uno::Reference< css::drawing::XShape > const &
@@ -319,6 +327,12 @@ private:
     bool                            mbHasLinkedTxbx; // this text box has linked text box ?
 
     css::uno::Sequence<css::beans::PropertyValue> maDiagramDoms;
+
+    /// Z-Order.
+    sal_Int32 mnZOrder = 0;
+
+    /// Z-Order offset.
+    sal_Int32 mnZOrderOff = 0;
 };
 
 } }
