35d548e414903c9f513c0899a2bab475ff56d0f0
Caolán McNamara <caolanm@redhat.com>
2018-11-17 15:33:54 +0000

    weld writer numbering styles dialog
    Change-Id: I697fa74a21acec550fcacd2a8d8468dfb21afcac
    Reviewed-on: https://gerrit.libreoffice.org/63495
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>

--- sw/UIConfig_swriter.mk
+++ sw/UIConfig_swriter.mk
@@ -112,6 +112,7 @@ $(eval $(call gb_UIConfig_add_uifiles,modules/swriter,\
 	sw/uiconfig/swriter/ui/columndialog \
 	sw/uiconfig/swriter/ui/columnpage \
 	sw/uiconfig/swriter/ui/columnwidth \
+	sw/uiconfig/swriter/ui/comboboxfragment \
 	sw/uiconfig/swriter/ui/conditionpage \
 	sw/uiconfig/swriter/ui/converttexttable \
 	sw/uiconfig/swriter/ui/createaddresslist \
