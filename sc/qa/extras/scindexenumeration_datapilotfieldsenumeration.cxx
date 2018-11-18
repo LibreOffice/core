2cc66408fdde0a70823921e142e94a49c6fae397
Jens Carl <j.carl43@gmx.de>
2018-11-18 02:38:35 +0000

    tdf#45904 Move XEnumeration Java tests to C++
    Move XEnumeration Java tests to C++
    for ScIndexEnumeration_DataPilotFieldsEnumeration.
    
    Change-Id: Iac7c60c3971756bcc47fc68a4aa86b442af266c5

---
+++ sc/qa/extras/scindexenumeration_datapilotfieldsenumeration.cxx
@@ -0,0 +1,113 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <test/calc_unoapi_test.hxx>
+#include <test/container/xenumeration.hxx>
+
+#include <com/sun/star/container/XEnumerationAccess.hpp>
+#include <com/sun/star/container/XIndexAccess.hpp>
+#include <com/sun/star/lang/XComponent.hpp>
+#include <com/sun/star/sheet/XDataPilotDescriptor.hpp>
+#include <com/sun/star/sheet/XDataPilotTables.hpp>
+#include <com/sun/star/sheet/XDataPilotTablesSupplier.hpp>
+#include <com/sun/star/sheet/XSpreadsheet.hpp>
+#include <com/sun/star/sheet/XSpreadsheetDocument.hpp>
+#include <com/sun/star/sheet/XSpreadsheets.hpp>
+#include <com/sun/star/table/CellAddress.hpp>
+#include <com/sun/star/table/CellRangeAddress.hpp>
+#include <com/sun/star/uno/XInterface.hpp>
+
+#include <com/sun/star/uno/Reference.hxx>
+
+#include <rtl/string.hxx>
+
+using namespace css;
+using namespace css::uno;
+
+namespace sc_apitest
+{
+class ScIndexEnumeration_DataPilotFieldsEnumeration : public CalcUnoApiTest,
+                                                      public apitest::XEnumeration
+{
+public:
+    ScIndexEnumeration_DataPilotFieldsEnumeration();
+
+    virtual uno::Reference<uno::XInterface> init() override;
+    virtual void setUp() override;
+    virtual void tearDown() override;
+
+    CPPUNIT_TEST_SUITE(ScIndexEnumeration_DataPilotFieldsEnumeration);
+
+    // XEnumeration
+    CPPUNIT_TEST(testHasMoreElements);
+    CPPUNIT_TEST(testNextElement);
+
+    CPPUNIT_TEST_SUITE_END();
+
+private:
+    uno::Reference<lang::XComponent> m_xComponent;
+};
+
+ScIndexEnumeration_DataPilotFieldsEnumeration::ScIndexEnumeration_DataPilotFieldsEnumeration()
+    : CalcUnoApiTest("/sc/qa/extras/testdocuments")
+{
+}
+
+uno::Reference<uno::XInterface> ScIndexEnumeration_DataPilotFieldsEnumeration::init()
+{
+    uno::Reference<sheet::XSpreadsheetDocument> xDoc(m_xComponent, uno::UNO_QUERY_THROW);
+    CPPUNIT_ASSERT_MESSAGE("no calc document", xDoc.is());
+
+    uno::Reference<sheet::XSpreadsheets> xSheets(xDoc->getSheets(), uno::UNO_QUERY_THROW);
+    uno::Reference<container::XIndexAccess> xIA(xSheets, uno::UNO_QUERY_THROW);
+    uno::Reference<sheet::XSpreadsheet> xSheet0(xIA->getByIndex(0), uno::UNO_QUERY_THROW);
+
+    for (auto i = 1; i < 4; ++i)
+    {
+        xSheet0->getCellByPosition(i, 0)->setFormula("Col" + OUString::number(i));
+        xSheet0->getCellByPosition(0, i)->setFormula("Row" + OUString::number(i));
+    }
+
+    for (auto x = 1; x < 4; ++x)
+        for (auto y = 1; y < 4; ++y)
+            xSheet0->getCellByPosition(x, y)->setValue(x * (y + 1));
+
+    uno::Reference<sheet::XDataPilotTablesSupplier> xDPTS(xSheet0, uno::UNO_QUERY_THROW);
+    uno::Reference<sheet::XDataPilotTables> xDPT(xDPTS->getDataPilotTables(), uno::UNO_QUERY_THROW);
+    uno::Reference<sheet::XDataPilotDescriptor> xDPD(xDPT->createDataPilotDescriptor(),
+                                                     uno::UNO_QUERY_THROW);
+    xDPD->setSourceRange(table::CellRangeAddress(0, 0, 0, 4, 4));
+    xDPT->insertNewByName("DataPilotTable", table::CellAddress(0, 5, 5), xDPD);
+
+    uno::Reference<container::XEnumerationAccess> xEA(xDPD->getDataPilotFields(),
+                                                      uno::UNO_QUERY_THROW);
+
+    return xEA->createEnumeration();
+}
+
+void ScIndexEnumeration_DataPilotFieldsEnumeration::setUp()
+{
+    CalcUnoApiTest::setUp();
+    m_xComponent = loadFromDesktop("private:factory/scalc");
+    CPPUNIT_ASSERT_MESSAGE("no component", m_xComponent.is());
+}
+
+void ScIndexEnumeration_DataPilotFieldsEnumeration::tearDown()
+{
+    closeDocument(m_xComponent);
+    CalcUnoApiTest::tearDown();
+}
+
+CPPUNIT_TEST_SUITE_REGISTRATION(ScIndexEnumeration_DataPilotFieldsEnumeration);
+
+} // namespace sc_apitest
+
+CPPUNIT_PLUGIN_IMPLEMENT();
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
