37455f8f0109ac838e2dde64118d7eb736958526
Jens Carl <j.carl43@gmx.de>
2018-11-19 08:28:10 +0000

    tdf#45904 Move XEnumeration Java tests to C++
    Move XEnumeration Java tests to C++
    for ScIndexEnumeration_CellAnnotationsEnumeration.
    
    Change-Id: I06ad265c09fa4c29e056527c79c0470634abaabc
    Reviewed-on: https://gerrit.libreoffice.org/63549
    Tested-by: Jenkins
    Reviewed-by: Jens Carl <j.carl43@gmx.de>

---
+++ sc/CppunitTest_sc_indexenumeration_cellannotationsenumeration.mk
@@ -0,0 +1,43 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#*************************************************************************
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+#*************************************************************************
+
+$(eval $(call gb_CppunitTest_CppunitTest,sc_indexenumeration_cellannotationsenumeration))
+
+$(eval $(call gb_CppunitTest_use_external,sc_indexenumeration_cellannotationsenumeration,boost_headers))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,sc_indexenumeration_cellannotationsenumeration, \
+	sc/qa/extras/scindexenumeration_cellannotationsenumeration \
+))
+
+$(eval $(call gb_CppunitTest_use_libraries,sc_indexenumeration_cellannotationsenumeration, \
+	cppu \
+	sal \
+	subsequenttest \
+	test \
+	unotest \
+))
+
+$(eval $(call gb_CppunitTest_set_include,sc_indexenumeration_cellannotationsenumeration,\
+	$$(INCLUDE) \
+))
+
+$(eval $(call gb_CppunitTest_use_sdk_api,sc_indexenumeration_cellannotationsenumeration))
+
+$(eval $(call gb_CppunitTest_use_ure,sc_indexenumeration_cellannotationsenumeration))
+$(eval $(call gb_CppunitTest_use_vcl,sc_indexenumeration_cellannotationsenumeration))
+
+$(eval $(call gb_CppunitTest_use_components,sc_indexenumeration_cellannotationsenumeration,\
+    $(sc_unoapi_common_components) \
+))
+
+$(eval $(call gb_CppunitTest_use_configuration,sc_indexenumeration_cellannotationsenumeration))
+
+# vim: set noet sw=4 ts=4:
