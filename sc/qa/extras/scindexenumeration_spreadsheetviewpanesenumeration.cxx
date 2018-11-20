2fd32ab0f5a8df8ef553414d6bd7971b3f474ecd
Jens Carl <j.carl43@gmx.de>
2018-11-20 06:56:51 +0000

    tdf#45904 Move XEnumeration Java tests to C++
    Move XEnumeration Java tests to C++
    for ScIndexEnumeration_SpreadsheetViewPanesEnumeration.
    
    Change-Id: I03c05cd894bf84816618ac28deb01ecb2fd8779e

---
+++ sc/qa/extras/scindexenumeration_spreadsheetviewpanesenumeration.cxx
@@ -0,0 +1,91 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <test/calc_unoapi_test.hxx>
+#include <test/container/xenumeration.hxx>
+
+#include <com/sun/star/container/XEnumerationAccess.hpp>
+#include <com/sun/star/container/XIndexAccess.hpp>
+#include <com/sun/star/frame/XController.hpp>
+#include <com/sun/star/frame/XModel.hpp>
+#include <com/sun/star/lang/XComponent.hpp>
+#include <com/sun/star/sheet/XSpreadsheetDocument.hpp>
+#include <com/sun/star/uno/XInterface.hpp>
+
+#include <com/sun/star/uno/Reference.hxx>
+
+using namespace css;
+using namespace css::uno;
+
+namespace sc_apitest
+{
+class ScIndexEnumeration_SpreadsheetViewPanesEnumeration : public CalcUnoApiTest,
+                                                           public apitest::XEnumeration
+{
+public:
+    ScIndexEnumeration_SpreadsheetViewPanesEnumeration();
+
+    virtual uno::Reference<uno::XInterface> init() override;
+    virtual void setUp() override;
+    virtual void tearDown() override;
+
+    CPPUNIT_TEST_SUITE(ScIndexEnumeration_SpreadsheetViewPanesEnumeration);
+
+    // XEnumeration
+    CPPUNIT_TEST(testHasMoreElements);
+    CPPUNIT_TEST(testNextElement);
+
+    CPPUNIT_TEST_SUITE_END();
+
+private:
+    uno::Reference<lang::XComponent> m_xComponent;
+};
+
+ScIndexEnumeration_SpreadsheetViewPanesEnumeration::
+    ScIndexEnumeration_SpreadsheetViewPanesEnumeration()
+    : CalcUnoApiTest("/sc/qa/extras/testdocuments")
+{
+}
+
+uno::Reference<uno::XInterface> ScIndexEnumeration_SpreadsheetViewPanesEnumeration::init()
+{
+    uno::Reference<sheet::XSpreadsheetDocument> xDoc(m_xComponent, uno::UNO_QUERY_THROW);
+    CPPUNIT_ASSERT_MESSAGE("no calc document", xDoc.is());
+
+    uno::Reference<frame::XModel> xModel(xDoc, uno::UNO_QUERY_THROW);
+    uno::Reference<frame::XController> xController(xModel->getCurrentController(),
+                                                   uno::UNO_QUERY_THROW);
+    uno::Reference<container::XIndexAccess> xIA(xController, uno::UNO_QUERY_THROW);
+    xIA->getByIndex(0);
+
+    uno::Reference<container::XEnumerationAccess> xEA(xIA, uno::UNO_QUERY_THROW);
+
+    return xEA->createEnumeration();
+}
+
+void ScIndexEnumeration_SpreadsheetViewPanesEnumeration::setUp()
+{
+    CalcUnoApiTest::setUp();
+    m_xComponent = loadFromDesktop("private:factory/scalc");
+    CPPUNIT_ASSERT_MESSAGE("no component", m_xComponent.is());
+}
+
+void ScIndexEnumeration_SpreadsheetViewPanesEnumeration::tearDown()
+{
+    closeDocument(m_xComponent);
+    CalcUnoApiTest::tearDown();
+}
+
+CPPUNIT_TEST_SUITE_REGISTRATION(ScIndexEnumeration_SpreadsheetViewPanesEnumeration);
+
+} // namespace sc_apitest
+
+CPPUNIT_PLUGIN_IMPLEMENT();
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
