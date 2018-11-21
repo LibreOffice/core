fdabb2a6706789fd88dd53d280976d7f78dd1ff2
Ilhan Yesil <ilhanyesil@gmx.de>
2018-11-21 13:52:08 +0100

    tdf#121569 Use of "Save as" under Windows without reopen dialog
    Check if we are under Windows when adding extensions for filter.
    
    Change-Id: Iab624dd2e8fd7caab1a66fb6470c10120ef6cb2c

--- sfx2/source/dialog/filtergrouping.cxx
+++ sfx2/source/dialog/filtergrouping.cxx
@@ -1149,11 +1149,13 @@ namespace sfx2
         if ( sRet.indexOf( "(*.*)" ) == -1 )
         {
             OUString sExt = _rExtension;
+#if !defined(_WIN32) // don't remove "*" under Windows
             if ( !_bForOpen )
             {
                 // show '*' in extensions only when opening a document
                 sExt = sExt.replaceAll("*", "");
             }
+#endif
             sRet += " (";
             sRet += sExt;
             sRet += ")";
