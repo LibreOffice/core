52caecc6c54f0adcff18b2e58ae7b5e0ecfa09a9
Ilhan Yesil <ilhanyesil@gmx.de>
2018-11-21 13:52:08 +0100

    tdf#121569 Use of "Save as" under Windows without reopen dialog
    Don't filter extensions in the standard way, if we are under Windows
    and adding extensions for filter. For this, a new interface
    XFilePicker4 derived from XFilePicker3 is introduced to allow own
    filter methods. If file picker is not derived from the new
    interface, standard filtering of the extension will occur.
    
    Change-Id: Iab624dd2e8fd7caab1a66fb6470c10120ef6cb2c

--- offapi/UnoApi_offapi.mk
+++ offapi/UnoApi_offapi.mk
@@ -4065,6 +4065,7 @@ $(eval $(call gb_UnoApi_add_idlfiles,offapi,com/sun/star/ui/dialogs,\
 	XFilePicker \
 	XFilePicker2 \
 	XFilePicker3 \
+	XFilePicker4 \
 	XFilePickerControlAccess \
 	XFilePickerListener \
 	XFilePickerNotifier \
