db9f978e4a2aa38c52ed42681dc2eff3e9849fa9
Xisco Faulí <xiscofauli@libreoffice.org>
2018-11-21 18:04:05 +0100

    Revert "Notebookbar:Disabled appearence of context toolbars"
    This reverts commit 2c317751c4b7661c10bd9eb11094febeccd1b816.
    
    So far, two crashes has been found related to this commit: tdf#121591
    and tdf#119945 which can be reproduced with or without the notebook enabled.
    Besides, the author seems no to be inactive now...
    
    Change-Id: Ie1ff88afc59abea176f6ae9d459a835aef39c57a
    Reviewed-on: https://gerrit.libreoffice.org/63736
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>

--- sfx2/source/appl/workwin.cxx
+++ sfx2/source/appl/workwin.cxx
@@ -55,7 +55,6 @@
 #include <com/sun/star/lang/DisposedException.hpp>
 #include <type_traits>
 #include <unordered_map>
-#include <sfx2/notebookbar/SfxNotebookBar.hxx>
 
 using namespace ::com::sun::star;
 using namespace ::com::sun::star::uno;
@@ -1204,7 +1203,7 @@ void SfxWorkWindow::UpdateObjectBars_Impl2()
 
         // Is a ToolBox required in this context ?
         bool bModesMatching = (nUpdateMode != SfxVisibilityFlags::Invisible) && ((nTbxMode & nUpdateMode) == nUpdateMode);
-        if ( bDestroy || sfx2::SfxNotebookBar::IsActive())
+        if ( bDestroy )
         {
             OUString aTbxId = g_aTbxTypeName + GetResourceURLFromToolbarId(eId);
             xLayoutManager->destroyElement( aTbxId );
