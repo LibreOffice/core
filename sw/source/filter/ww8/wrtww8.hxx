f8e57c838fa5f2d642e23502176486f9147c8fdd
Serge Krot <Serge.Krot@cib.de>
2018-11-22 10:05:47 +0100

    tdf#121561: sw: DOCX: add std/stdPr/stdContent around TOC
    During export into DOCX from ODT we need to do it
    because in this case the TOC title will be recognized
    inside MS Word as part of the TOC.
    
    Later we could add support of these keywords in LO import
    in order to detect TOC title from DOCX input.
    
    Added unit test for export.
    
    Change-Id: I7135e91dc04d4c0501e6074a046fc473e041f014

--- sw/source/filter/ww8/wrtww8.hxx
+++ sw/source/filter/ww8/wrtww8.hxx
@@ -843,6 +843,7 @@ protected:
 
     /// Output SwSectionNode
     void OutputSectionNode( const SwSectionNode& );
+    void UpdateTocSectionNodeProperties(const SwSectionNode& rSectionNode);
 
     virtual void AppendSection( const SwPageDesc *pPageDesc, const SwSectionFormat* pFormat, sal_uLong nLnNum ) = 0;
 
