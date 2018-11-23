97c218ba7cc443cb4c87110cea8c24ff4230da3a
Tamás Zolnai <tamas.zolnai@collabora.com>
2018-11-23 13:01:02 +0100

    lok: Right rotation calculation in case of delta angle
    Change-Id: Ia7d0a573c1902fba3fc1e69f425bbf2dc93a1c82

--- svx/source/svdraw/svdedtv1.cxx
+++ svx/source/svdraw/svdedtv1.cxx
@@ -1506,7 +1506,7 @@ void SdrEditView::SetGeoAttrToMarked(const SfxItemSet& rAttr)
 
     // rotation
     if (SfxItemState::SET == rAttr.GetItemState(SID_ATTR_TRANSFORM_DELTA_ANGLE, true, &pPoolItem)) {
-        nRotateAngle = static_cast<const SfxInt32Item*>(pPoolItem)->GetValue() + nOldRotateAngle;
+        nRotateAngle = static_cast<const SfxInt32Item*>(pPoolItem)->GetValue();
         bRotate = (nRotateAngle != 0);
     }
 
