996c8d9726543cb3bd69025ef6314a3f7ff7ef2e
Jan-Marek Glogowski <glogow@fbihome.de>
2018-11-23 13:44:28 +0100

    Default all tests to run with the svp plugin
    Otherwise a test can already require a windowed VCL plugin by
    calling gb_CppunitTest_use_vcl_non_headless(_with_windows)?.
    
    For compatibility on unix --headless still implies the use of the
    svp plugin, but now a SAL_USE_VCLPLUGIN will override it.
    
    All the explicit SAL_USE_VCLPLUGIN=svp are not needed, as this is
    now included in the gb_TEST_ENV_VARS variable and gengal already
    calls Application::EnableConsoleOnly().
    
    Change-Id: I6b4e75282aa88d747db87d60ffe6c8f282187c5f

--- solenv/gbuild/UITest.mk
+++ solenv/gbuild/UITest.mk
@@ -70,9 +70,6 @@ else
 		TestUserDir="$(call gb_Helper_make_url,$(dir $(call gb_UITest_get_target,$*)))" \
 		PYTHONDONTWRITEBYTECODE=0 \
 		$(gb_TEST_ENV_VARS) \
-		$(if $(filter-out MACOSX WNT,$(OS_FOR_BUILD)),$(if $(DISABLE_GUI),, \
-			SAL_USE_VCLPLUGIN=svp \
-		)) \
 		$(gb_UITest_COMMAND) \
 		--soffice="$(gb_UITest_SOFFICEARG)" \
 		--userdir=$(call gb_Helper_make_url,$(dir $(call gb_UITest_get_target,$*))user) \
