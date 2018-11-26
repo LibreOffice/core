52fb1434c48b4577dd1ce6240bd57c624c3ce35d
Michael Meeks <michael.meeks@collabora.com>
2018-11-26 14:00:08 +0000

    Use HiDPI scaling to load scaled images.
    We render these at apparently the same pixel size as normal images,
    but the underlying canvas is larger so these then end up pixel-matching
    the true underlying grid.
    
    Change-Id: Ic4b749127e9c81da78d06b34d9f88c5635dc64b9

--- vcl/source/image/Image.cxx
+++ vcl/source/image/Image.cxx
@@ -116,8 +116,7 @@ void Image::Draw(OutputDevice* pOutDev, const Point& rPos, DrawImageFlags nStyle
 
     Size aOutSize = pSize ? *pSize : pOutDev->PixelToLogic(aBitmapSizePixel);
 
-    // FIXME: do the HiDPI scaling fun here [!] =)
-    BitmapEx aRenderBmp = mpImplData->getBitmapEx(!!(nStyle & DrawImageFlags::Disable));
+    BitmapEx aRenderBmp = mpImplData->getBitmapExForHiDPI(!!(nStyle & DrawImageFlags::Disable));
 
     if (!(nStyle & DrawImageFlags::Disable) &&
         (nStyle & (DrawImageFlags::ColorTransform | DrawImageFlags::Highlight |
@@ -154,7 +153,7 @@ void Image::Draw(OutputDevice* pOutDev, const Point& rPos, DrawImageFlags nStyle
         aRenderBmp = aTempBitmapEx;
     }
 
-    pOutDev->DrawBitmapEx(rPos, aOutSize, aSrcPos, aBitmapSizePixel, aRenderBmp);
+    pOutDev->DrawBitmapEx(rPos, aOutSize, aRenderBmp);
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
