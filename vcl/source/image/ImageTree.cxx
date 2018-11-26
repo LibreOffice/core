f9171ff71c8fd9eb0835179d9474800f21a78043
Michael Meeks <michael.meeks@collabora.com>
2018-11-26 13:24:03 +0000

    Add explicit API scaling via ImageTree API.
    Also add image scale to in-memory cache lookup.
    
    Change-Id: I1e84b922d4a9ab2f1723c5cb8a72f295c6940504

--- vcl/source/image/ImageTree.cxx
+++ vcl/source/image/ImageTree.cxx
@@ -37,9 +37,17 @@ std::shared_ptr<SvMemoryStream> ImageTree::getImageStream(OUString const & rName
 
 bool ImageTree::loadImage(OUString const & rName, OUString const & rStyle,
                           BitmapEx & rBitmap, bool bLocalized,
+                          sal_Int32 nScalePercentage,
                           const ImageLoadFlags eFlags)
 {
-    return mpImplImageTree->loadImage(rName, rStyle, rBitmap, bLocalized, eFlags);
+    return mpImplImageTree->loadImage(rName, rStyle, rBitmap, bLocalized, eFlags, nScalePercentage);
+}
+
+bool ImageTree::loadImage(OUString const & rName, OUString const & rStyle,
+                          BitmapEx & rBitmap, bool bLocalized,
+                          const ImageLoadFlags eFlags)
+{
+    return loadImage(rName, rStyle, rBitmap, bLocalized, -1, eFlags);
 }
 
 css::uno::Reference<css::container::XNameAccess> const & ImageTree::getNameAccess()
