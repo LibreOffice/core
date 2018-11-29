10881c6163ad37d4cfbe0670525a5ce300fdb0a7
Tor Lillqvist <tml@collabora.com>
2018-11-29 15:33:31 +0200

    We need svt, too, in the iOS app, for the image insertion feature
    Change-Id: I5a656ab159eec44a537fc22da03763913372bc8f

--- solenv/bin/native-code.py
+++ solenv/bin/native-code.py
@@ -61,7 +61,8 @@ core_factory_list = [
     ("libxmlscriptlo.a", "xmlscript_component_getFactory"),
     ("libmcnttype.a", "mcnttype_component_getFactory"),
     ("libvcllo.a", "vcl_component_getFactory"),
-    ("libspelllo.a", "spell_component_getFactory")
+    ("libspelllo.a", "spell_component_getFactory"),
+    ("libsvtlo.a", "svt_component_getFactory")
     ]
 
 core_constructor_list = [
