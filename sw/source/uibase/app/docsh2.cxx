1f634ca8e2cdb283bd0d61dc4a397bd959c60c03
Caolán McNamara <caolanm@redhat.com>
2018-12-03 16:27:28 +0000

    weld OfaAutoCorrDlg
    Change-Id: Iba217a2998de57bf963872f9b8c1cf38694eb935

--- sw/source/uibase/app/docsh2.cxx
+++ sw/source/uibase/app/docsh2.cxx
@@ -444,7 +444,7 @@ void SwDocShell::Execute(SfxRequest& rReq)
                 aSet.Put( *static_cast<const SfxBoolItem*>(pOpenSmartTagOptionsItem) );
 
             SfxAbstractDialogFactory* pFact = SfxAbstractDialogFactory::Create();
-            VclPtr<SfxAbstractTabDialog> pDlg = pFact->CreateAutoCorrTabDialog(&GetView()->GetViewFrame()->GetWindow(), &aSet);
+            VclPtr<SfxAbstractTabDialog> pDlg = pFact->CreateAutoCorrTabDialog(GetView()->GetViewFrame()->GetWindow().GetFrameWeld(), &aSet);
             pDlg->Execute();
             pDlg.disposeAndClear();
 
