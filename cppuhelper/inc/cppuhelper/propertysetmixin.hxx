72d67ce637c7785ee08462c02dd7666593e5dfc7
Damjan Jovanovic <damjan@apache.org>
2018-12-03 17:55:55 +0000

    Also export PropertySetMixinImpl::BoundListeners.
    Patch by: me

--- cppuhelper/inc/cppuhelper/propertysetmixin.hxx
+++ cppuhelper/inc/cppuhelper/propertysetmixin.hxx
@@ -129,7 +129,7 @@ protected:
        @descr See <code>cppu::PropertySetMixinImpl::prepareSet</code> for
        further details.
     */
-    class BoundListeners {
+    class CPPUHELPER_DLLPUBLIC BoundListeners {
     public:
         /**
            @short The constructor.
