418ff25bba255390895c0edc662153ed3056ca68
Mike Kaganski <mike.kaganski@collabora.com>
2018-12-04 10:00:51 +0300

    tdf#34171: check foreign lockfiles to tell who has locked document
    MS Office (Word/Excel/PowerPoint) lockfiles are supported now.
    Note that Excel does *not* create lockfiles for pre-OOXML files.
    
    Change-Id: I7b99012f4bd60ab3821fb91d5166a286031b7e93

--- include/sfx2/docfile.hxx
+++ include/sfx2/docfile.hxx
@@ -295,7 +295,7 @@ public:
 
 private:
     enum class ShowLockResult { NoLock, Succeeded, Try };
-    ShowLockResult      ShowLockedDocumentDialog(const LockFileEntry& aData, bool bIsLoading, bool bOwnLock, bool bHandleSysLocked);
+    ShowLockResult      ShowLockedDocumentDialog(const OUString& aURL, const LockFileEntry& aData, bool bIsLoading, bool bOwnLock, bool bHandleSysLocked);
     enum class MessageDlg { LockFileIgnore, LockFileCorrupt };
     bool                ShowLockFileProblemDialog(MessageDlg nWhichDlg);
 
