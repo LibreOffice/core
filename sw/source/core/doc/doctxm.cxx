736a38419f49d4b96bd845f5767a028df59e4cff
Serge Krot <Serge.Krot@cib.de>
2018-12-06 19:11:58 +0100

    sw: DOCX: recognize TOC title during import
    Change-Id: Ifa4fb59858d61580f76e3d104aa4caa6b5902d1b
    Reviewed-on: https://gerrit.libreoffice.org/64735
    Tested-by: Jenkins
    Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>
    
    tdf#121561: sw: DOCX: add std/stdPr/stdContent around TOC
    
    During export into DOCX from ODT we need to do it
    because in this case the TOC title will be recognized
    inside MS Word as part of the TOC.
    
    Later we could add support of these keywords in LO import
    in order to detect TOC title from DOCX input.
    
    Added unit test for export.
    
    Change-Id: I7135e91dc04d4c0501e6074a046fc473e041f014
    Reviewed-on: https://gerrit.libreoffice.org/63786
    Tested-by: Jenkins
    Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>
    Reviewed-on: https://gerrit.libreoffice.org/66307
    Reviewed-by: Michael Stahl <Michael.Stahl@cib.de>

--- sw/source/core/doc/doctxm.cxx
+++ sw/source/core/doc/doctxm.cxx
@@ -333,14 +333,22 @@ const SwTOXMark& SwDoc::GotoTOXMark( const SwTOXMark& rCurTOXMark,
 }
 
 SwTOXBaseSection* SwDoc::InsertTableOf( const SwPosition& rPos,
-                                                const SwTOXBase& rTOX,
-                                                const SfxItemSet* pSet,
-                                                bool bExpand )
+                                        const SwTOXBase& rTOX,
+                                        const SfxItemSet* pSet,
+                                        bool bExpand)
+{
+    SwPaM aPam( rPos );
+    return InsertTableOf( aPam, rTOX, pSet, bExpand );
+}
+
+SwTOXBaseSection* SwDoc::InsertTableOf( const SwPaM& aPam,
+                                        const SwTOXBase& rTOX,
+                                        const SfxItemSet* pSet,
+                                        bool bExpand )
 {
     GetIDocumentUndoRedo().StartUndo( SwUndoId::INSTOX, nullptr );
 
     OUString sSectNm = GetUniqueTOXBaseName( *rTOX.GetTOXType(), rTOX.GetTOXName() );
-    SwPaM aPam( rPos );
     SwSectionData aSectionData( TOX_CONTENT_SECTION, sSectNm );
     SwTOXBaseSection *const pNewSection = dynamic_cast<SwTOXBaseSection *>(
         InsertSwSection( aPam, aSectionData, & rTOX, pSet, false ));
