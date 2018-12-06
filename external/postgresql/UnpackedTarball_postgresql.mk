792508616642bc1334f09c6bf99d06ad712aed78
Stephan Bergmann <sbergman@redhat.com>
2018-12-06 17:15:08 +0100

    Use gb_UnpackedTarball_update_autoconf_configs in external/postgresql
    Change-Id: I56c68b74daa701e0e27ca4b661ece12bf20a8776

--- external/postgresql/UnpackedTarball_postgresql.mk
+++ external/postgresql/UnpackedTarball_postgresql.mk
@@ -15,7 +15,6 @@ $(eval $(call gb_UnpackedTarball_set_patchlevel,postgresql,3))
 
 $(eval $(call gb_UnpackedTarball_add_patches,postgresql, \
 	external/postgresql/postgresql-libs-leak.patch \
-	external/postgresql/postgresql-9.2.1-autoreconf.patch \
 	external/postgresql/postgresql-9.2.1-libreoffice.patch \
 ))
 
@@ -24,5 +23,7 @@ $(eval $(call gb_UnpackedTarball_add_patches,postgresql, \
 	external/postgresql/internal-zlib.patch.1 \
 ))
 endif
+
+$(eval $(call gb_UnpackedTarball_update_autoconf_configs,postgresql))
 
 # vim: set noet sw=4 ts=4:
