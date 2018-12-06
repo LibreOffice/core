130076536650d5b46f724e6f25928ae3005cc247
tagezi <lera.goncharuk@gmail.com>
2018-12-06 16:59:28 +0300

    Rename files for ToolBox ComboBoxes in BasicIDE
    Change-Id: Ie9e1949cc9ac474bffe8201d12b9ce7dc2276076

--- basctl/source/basicide/ComboBox.cxx
+++ basctl/source/basicide/ComboBox.cxx
@@ -19,10 +19,8 @@
 
 #include <strings.hrc>
 #include <basidesh.hxx>
-
-#include "basicbox.hxx"
+#include <ComboBox.hxx>
 #include <iderdll.hxx>
-
 #include <localizationmgr.hxx>
 #include <managelang.hxx>
 
