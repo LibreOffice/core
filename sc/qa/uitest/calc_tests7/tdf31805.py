23b6c876180b5fc7b124f8116a350a9b9066e1f6
Zdeněk Crhonek <zcrhonek@gmail.com>
2018-12-11 16:35:31 +0100

    uitest for bug tdf#31805
    Change-Id: I21790869be9c87e923cb4d87b58b2dc4b143fb25
    Reviewed-on: https://gerrit.libreoffice.org/64971
    Tested-by: Jenkins
    Reviewed-by: Zdenek Crhonek <zcrhonek@gmail.com>

---
+++ sc/qa/uitest/calc_tests7/tdf31805.py
@@ -0,0 +1,47 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+from uitest.framework import UITestCase
+from uitest.uihelper.common import get_state_as_dict
+from uitest.uihelper.common import select_pos
+from uitest.uihelper.calc import enter_text_to_cell
+from libreoffice.calc.document import get_cell_by_position
+from libreoffice.uno.propertyvalue import mkPropertyValues
+import org.libreoffice.unotest
+import pathlib
+
+#Bug 31805 - Deleting a column or row from merged cells
+def get_url_for_data_file(file_name):
+    return pathlib.Path(org.libreoffice.unotest.makeCopyFromTDOC(file_name)).as_uri()
+
+class tdf31805(UITestCase):
+    def test_tdf31805_delete_column_merged(self):
+        calc_doc = self.ui_test.load_file(get_url_for_data_file("tdf31805.ods"))
+        xCalcDoc = self.xUITest.getTopFocusWindow()
+        gridwin = xCalcDoc.getChild("grid_window")
+        document = self.ui_test.get_component()
+        #1. open "sample.ods"
+        #2. click 'G1'
+        #3. Menu 'edit -> Celete Cell - Delete entire Column'
+        #expected: Column with cell containing "g" will be deleted,
+        #     a merged cell range will remainlect row 1
+
+        gridwin.executeAction("SELECT", mkPropertyValues({"CELL": "G1"}))
+        self.assertEqual(get_cell_by_position(document, 0, 2, 4).getIsMerged(), True)
+        self.xUITest.executeCommand(".uno:DeleteColumns")
+        #verify. C5:F11 should be merged
+        #isMerged   returns true if this cell is merged with another cell.
+        self.assertEqual(get_cell_by_position(document, 0, 2, 4).getIsMerged(), True)      #C5
+        self.assertEqual(get_cell_by_position(document, 0, 6, 4).getIsMerged(), False)      #G5
+        gridwin.executeAction("SELECT", mkPropertyValues({"CELL": "A11"}))
+        self.xUITest.executeCommand(".uno:DeleteRows")
+        self.assertEqual(get_cell_by_position(document, 0, 2, 4).getIsMerged(), True)      #C5
+        gridwin.executeAction("SELECT", mkPropertyValues({"CELL": "D1"}))
+        self.xUITest.executeCommand(".uno:DeleteColumns")
+        self.assertEqual(get_cell_by_position(document, 0, 2, 4).getIsMerged(), True)      #C5
+        self.ui_test.close_doc()
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
