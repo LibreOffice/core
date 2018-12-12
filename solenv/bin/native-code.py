fcb2685ac23f6a1a5fb03f2ec431f8f7c43db58d
Caolán McNamara <caolanm@redhat.com>
2018-12-12 11:50:06 +0000

    weld SwCaptionDialog
    Change-Id: I5babfbf3489f88c5d6cdeffbd514d86368f04fa9
    Reviewed-on: https://gerrit.libreoffice.org/65014
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>
    (cherry picked from commit 72ebf150ab1f35a65de7c99e0227af77bae72be9)
    Reviewed-on: https://gerrit.libreoffice.org/65078

--- solenv/bin/native-code.py
+++ solenv/bin/native-code.py
@@ -508,7 +508,6 @@ custom_widgets = [
     'SvxTextEncodingBox',
     'SvxTextEncodingBox',
     'SwAddressPreview',
-    'SwCaptionPreview',
     'SwFieldRefTreeListBox',
     'SwGlTreeListBox',
     'SwIdxTreeListBox',
