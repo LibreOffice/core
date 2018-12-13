37b3de9296909a64bbac0c6c649f0698ac09c555
Vasily Melenchuk <vasily.melenchuk@cib.de>
2018-12-13 15:47:38 +0100

    tdf#99438: sc: support for .xltx export
    Added support for content types for template and template
    with macros (.xltm, but no user interface for this yet).
    
    Fixed preferred extension for template: by default it is .xltx
    and not .xltm (macro-enabled), because MS Excel is very strict
    about matching of content-type and extension here.
    
    Fixed wrong namespace in FilterService for ExcelFilter.
    
    Change-Id: Ie717d31d72203601324860f069918d75082add4a

--- sc/source/filter/excel/xestream.cxx
+++ sc/source/filter/excel/xestream.cxx
@@ -915,10 +915,11 @@ sax_fastparser::FSHelperPtr XclXmlUtils::WriteFontData( sax_fastparser::FSHelper
     return pStream;
 }
 
-XclExpXmlStream::XclExpXmlStream( const uno::Reference< XComponentContext >& rCC, bool bExportVBA )
+XclExpXmlStream::XclExpXmlStream( const uno::Reference< XComponentContext >& rCC, bool bExportVBA, bool bExportTemplate )
     : XmlFilterBase( rCC ),
       mpRoot( nullptr ),
-      mbExportVBA(bExportVBA)
+      mbExportVBA(bExportVBA),
+      mbExportTemplate(bExportTemplate)
 {
 }
 
@@ -1080,11 +1081,29 @@ bool XclExpXmlStream::exportDocument()
         ScDocShell::GetViewData()->WriteExtOptions( mpRoot->GetExtDocOptions() );
 
     OUString const workbook = "xl/workbook.xml";
-    const char* pWorkbookContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet.main+xml";
-
-
+    const char* pWorkbookContentType = nullptr;
     if (mbExportVBA)
-        pWorkbookContentType = "application/vnd.ms-excel.sheet.macroEnabled.main+xml";
+    {
+        if (mbExportTemplate)
+        {
+            pWorkbookContentType = "application/vnd.ms-excel.template.macroEnabled.main+xml";
+        }
+        else
+        {
+            pWorkbookContentType = "application/vnd.ms-excel.sheet.macroEnabled.main+xml";
+        }
+    }
+    else
+    {
+        if (mbExportTemplate)
+        {
+            pWorkbookContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.template.main+xml";
+        }
+        else
+        {
+            pWorkbookContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet.main+xml";
+        }
+    }
 
     PushStream( CreateOutputStream( workbook, workbook,
                                     uno::Reference <XOutputStream>(),
