61ef391dd1ca423e5733ce8ec53cfd11495856d5
Bjoern Michaelsen <bjoern.michaelsen@libreoffice.org>
2018-12-13 19:34:15 +0100

    A LibreOffice Hello World!
    This prints "Hello World!" and the "named diamond" from the video at
    https://www.youtube.com/watch?v=ggmspIvaWjI
    
    To try this on Linux:
    $ make
      ^ have a fully build LibreOffice
    $ cd sal
      ^ go into the "sal" module
    $ make Executable_helloworld
      ^ build the Hello World program
    $ LD_LIBRARY_PATH=../instdir/program/ ../workdir/LinkTarget/Executable/helloworld
      ^ run the hello world program (on Linux)
    
    Change-Id: Ia8158db5476b591da2f4f536ed419a51ad32df5e

---
+++ sal/Executable_helloworld.mk
@@ -0,0 +1,21 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_Executable_Executable,helloworld))
+
+$(eval $(call gb_Executable_use_libraries,helloworld,\
+	sal \
+))
+
+$(eval $(call gb_Executable_add_exception_objects,helloworld,\
+	sal/helloworld/helloworld \
+))
+
+# vim: set noet sw=4 ts=4:
---
+++ sal/helloworld/helloworld.cxx
@@ -0,0 +1,49 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <iostream>
+#include <sal/main.h>
+#include <sal/main.h>
+#include <rtl/string.hxx>
+
+using namespace std;
+using namespace rtl;
+
+// rtl::OString is the LibreOffice string to do stuff like in the example video
+// you can find docs about it here:
+// https://api.libreoffice.org/docs/cpp/ref/a00148.html
+//
+// note: In "real world" LibreOffice code, you will much more likely use rtl::OUString
+// which handles Unicode properly too (e.g. for Umlauts or ~any non-ASCII character):
+// https://api.libreoffice.org/docs/cpp/ref/a00152.html
+//
+void namedDiamond(OString& name) {
+    for (int i = 0; i < name.getLength(); i++)
+    {
+        cout << name.copy(0, i + 1) << endl;
+    }
+    for (int i = 0; i < name.getLength(); i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+        cout << name.copy(i) << endl;
+    }
+}
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    OString name = "Bjoern";
+    namedDiamond(name);
+    return 0;
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
