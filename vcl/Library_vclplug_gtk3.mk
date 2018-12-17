ea8a46aa7d46c19b8bf09a415fc8125038a91e01
Michael Weghorn <m.weghorn@posteo.de>
2018-12-17 15:35:58 +0100

    Use 'gb_Library_set_include' for GTK3_CFLAGS
    This among others makes sure that the '-I'/'-isystem' compiler
    flags for the gtk3 includes end up in the 'INCLUDE' section
    in 'workdir/GbuildToJson/Library/libvclplug_gtk3lo.so' when
    that file is generated by 'gbuildtojson'.
    
    That file is used by 'gbuild-to-ide' to generate IDE-specific
    config files.
    Having the gtk3 headers in the 'INCLUDE' section makes sure that
    the includes are properly taken into account when generating
    the IDE-specific files, and the included files can thus
    actually be found.
    
    Also, there's no need to set '$$(INCLUDE)' via
    'gb_Library_add_cxxflags', since the flags are already set via
    'gb_Library_set_include', so drop the 'gb_Library_add_cxxflags'
    part altogether.
    
    Change-Id: Ibf198be9372ce2bec4c8a16d8530edf860733a57

--- vcl/Library_vclplug_gtk3.mk
+++ vcl/Library_vclplug_gtk3.mk
@@ -27,14 +27,10 @@ $(eval $(call gb_Library_add_cxxflags,vclplug_gtk3, \
 
 $(eval $(call gb_Library_set_include,vclplug_gtk3,\
     $$(INCLUDE) \
+    $$(GTK3_CFLAGS) \
     -I$(SRCDIR)/vcl/inc \
     -I$(SRCDIR)/vcl/unx \
     -I$(SRCDIR)/vcl/unx/gtk3 \
-))
-
-$(eval $(call gb_Library_add_cxxflags,vclplug_gtk3,\
-    $$(INCLUDE) \
-    $$(GTK3_CFLAGS) \
 ))
 
 $(eval $(call gb_Library_add_defs,vclplug_gtk3,\
