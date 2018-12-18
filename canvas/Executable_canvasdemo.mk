a4749a48677b26c8b9ba9cf36f5c9bf62ed9ed8a
Jan-Marek Glogowski <glogow@fbihome.de>
2018-12-18 18:32:13 +0000

    Convert canvasdemo to gbuild
    Change-Id: Ieb3b1846a5a31915297cdc80053dba3d1656aba2
    Reviewed-on: https://gerrit.libreoffice.org/65365
    Tested-by: Jenkins
    Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>

---
+++ canvas/Executable_canvasdemo.mk
@@ -0,0 +1,57 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_Executable_Executable,canvasdemo))
+
+$(eval $(call gb_Executable_use_api,canvasdemo,\
+    offapi \
+    udkapi \
+))
+
+$(eval $(call gb_Executable_use_externals,canvasdemo,\
+    boost_headers \
+))
+
+$(eval $(call gb_Executable_set_include,canvasdemo,\
+    $$(INCLUDE) \
+))
+
+$(eval $(call gb_Executable_use_libraries,canvasdemo,\
+    basegfx \
+    comphelper \
+    cppu \
+    cppuhelper \
+    tl \
+    sal \
+    salhelper \
+    vcl \
+))
+
+$(eval $(call gb_Executable_add_exception_objects,canvasdemo,\
+    canvas/workben/canvasdemo \
+))
+
+$(eval $(call gb_Executable_use_static_libraries,canvasdemo,\
+    vclmain \
+))
+
+ifeq ($(OS), $(filter LINUX %BSD SOLARIS, $(OS)))
+$(eval $(call gb_Executable_add_libs,canvasdemo,\
+    -lm $(DLOPEN_LIBS) \
+    -lpthread \
+    -lX11 \
+))
+
+$(eval $(call gb_Executable_use_static_libraries,canvasdemo,\
+    glxtest \
+))
+endif
+
+# vim: set noet sw=4 ts=4:
