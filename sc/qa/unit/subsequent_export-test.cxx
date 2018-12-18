8afeb9aa5fafb34c0af20429dce1936c06ca06cd
Katarina Behrens <Katarina.Behrens@cib.de>
2018-12-18 11:12:49 +0100

    Resetting all fields for all dims corrupts pivot cache
    test case is exporting ooo55266-3 (contains data grouped in numerical
    intervals) to xlsx and without closing the document, opening filter on
    1st pivot table (kaboom!)
    
    ClearGroupFields corrupts the cache bc it resets Field.mpGroup items
    for all dimensions, not just the one present in ScDPDimensionSaveData
    (all this happening in ScDPCollection::SheetCaches::getCache).
    Consequently, accessing or rebuilding pivot cache may crash bc mpGroup
    now points nowhere.
    
    I split and renamed ScDPCache::ClearGroupFields into 2 parts, one of
    them clears maGroupFields, the other resets mpGroup ptrs in maFields.
    When adding data to cache, the former is used (bc group ptrs get reset
    almost immediately afterwards)
    
    Reviewed-on: https://gerrit.libreoffice.org/65329
    Tested-by: Jenkins
    Reviewed-by: Eike Rathke <erack@redhat.com>
    Reviewed-by: Kohei Yoshida <libreoffice@kohei.us>
    Reviewed-by: Katarina Behrens <Katarina.Behrens@cib.de>
    (cherry picked from commit f70d29fd91d232f0b030f0f76bd23bd2919eb868)
    
    Change-Id: I96e8d234a17da0f3cc65c0625aa47b12284b98b8

--- sc/qa/unit/subsequent_export-test.cxx
+++ sc/qa/unit/subsequent_export-test.cxx
@@ -43,6 +43,8 @@
 #include <validat.hxx>
 #include <attrib.hxx>
 #include <global.hxx>
+#include <scmod.hxx>
+#include <dpcache.hxx>
 #include <dpobject.hxx>
 
 #include <svx/svdoole2.hxx>
@@ -211,6 +213,7 @@ public:
 
     void testTdf118990();
     void testTdf121612();
+    void testPivotCacheAfterExportXLSX();
 
     CPPUNIT_TEST_SUITE(ScExportTest);
     CPPUNIT_TEST(test);
@@ -322,6 +325,7 @@ public:
 
     CPPUNIT_TEST(testTdf118990);
     CPPUNIT_TEST(testTdf121612);
+    CPPUNIT_TEST(testPivotCacheAfterExportXLSX);
 
     CPPUNIT_TEST_SUITE_END();
 
@@ -4086,6 +4090,35 @@ void ScExportTest::testTdf121612()
     ScDPCollection* pDPColl = rDoc.GetDPCollection();
     CPPUNIT_ASSERT(pDPColl);
     CPPUNIT_ASSERT_EQUAL(size_t(1), pDPColl->GetCount());
+}
+
+void ScExportTest::testPivotCacheAfterExportXLSX()
+{
+    ScDocShellRef xDocSh = loadDoc("numgroup_example.", FORMAT_ODS);
+    CPPUNIT_ASSERT(xDocSh.is());
+
+    // export only
+    std::shared_ptr<utl::TempFile> pTemp = ScBootstrapFixture::saveAs(xDocSh.get(), FORMAT_XLSX);
+
+    ScDocument& rDoc = xDocSh->GetDocument();
+    CPPUNIT_ASSERT(rDoc.HasPivotTable());
+
+    // Two pivot tables
+    ScDPCollection* pDPColl = rDoc.GetDPCollection();
+    CPPUNIT_ASSERT(pDPColl);
+    CPPUNIT_ASSERT_EQUAL(size_t(2), pDPColl->GetCount());
+
+    // One cache
+    ScDPCollection::SheetCaches& rSheetCaches = pDPColl->GetSheetCaches();
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(1), rSheetCaches.size());
+    const ScDPCache* pCache = rSheetCaches.getExistingCache(ScRange(0, 0, 0, 3, 30, 0));
+    CPPUNIT_ASSERT_MESSAGE("Pivot cache is expected for A1:D31 on the first sheet.", pCache);
+
+    // See if XLSX export didn't damage group info of the 1st pivot table
+    const ScDPNumGroupInfo* pInfo = pCache->GetNumGroupInfo(1);
+    CPPUNIT_ASSERT_MESSAGE("No number group info :(", pInfo);
+
+    xDocSh->DoClose();
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(ScExportTest);
