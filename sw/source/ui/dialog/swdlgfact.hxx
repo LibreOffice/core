a250deec66dc28f3ce878868e947eef664bfef11
Caolán McNamara <caolanm@redhat.com>
2018-12-18 15:16:04 +0000

    weld SwMultiTOXTabDialog
    Change-Id: Ic1ae4cfe43bdf31d511e9ac0f65a821c53ab59eb

--- sw/source/ui/dialog/swdlgfact.hxx
+++ sw/source/ui/dialog/swdlgfact.hxx
@@ -513,7 +513,15 @@ public:
 class SwMultiTOXTabDialog;
 class AbstractMultiTOXTabDialog_Impl : public AbstractMultiTOXTabDialog
 {
-    DECL_ABSTDLG_BASE(AbstractMultiTOXTabDialog_Impl,SwMultiTOXTabDialog)
+protected:
+    std::shared_ptr<SwMultiTOXTabDialog> m_xDlg;
+public:
+    explicit AbstractMultiTOXTabDialog_Impl(std::unique_ptr<SwMultiTOXTabDialog> p)
+        : m_xDlg(std::move(p))
+    {
+    }
+    virtual short Execute() override;
+    virtual bool StartExecuteAsync(VclAbstractDialog::AsyncContext &rCtx) override;
     virtual CurTOXType          GetCurrentTOXType() const override ;
     virtual SwTOXDescription&   GetTOXDescription(CurTOXType eTOXTypes) override;
     //from SfxTabDialog
@@ -700,7 +708,7 @@ public:
                                                 const SfxItemSet* pSwItemSet,
                                                 SwWrtShell &) override;
     virtual VclPtr<AbstractMultiTOXTabDialog>  CreateMultiTOXTabDialog(
-                                                vcl::Window* pParent, const SfxItemSet& rSet,
+                                                weld::Window* pParent, const SfxItemSet& rSet,
                                                 SwWrtShell &rShell,
                                                 SwTOXBase* pCurTOX,
                                                 bool bGlobal) override;
