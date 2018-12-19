LibreOffice/core

commit 067a53bbcd209d7458b04142f989e1ff7ec1f785

#i101821# find correct frame and SwPageFrame to print (or PDF export) selections

sw/source/core/view/vprint.cxx
@@ -776,16 +776,32 @@ SwDoc * ViewShell::CreatePrtDoc( SfxPrinter* pPrt, SfxObjectShellRef &rDocShellR
         pActCrsr = dynamic_cast<SwShellCrsr*>(pActCrsr->GetPrev());
     }
 
-    // Die Y-Position der ersten Selektion
-    const Point aSelPoint = pFESh->IsTableMode() ?
-                            pFESh->GetTableCrsr()->GetSttPos() :
-                            pFirstCrsr->GetSttPos();
+    Point aSelPoint;
+    if( pFESh->IsTableMode() )
+    {
+        SwShellTableCrsr* pShellTblCrsr = pFESh->GetTableCrsr();
+
+        const SwCntntNode* pCntntNode = pShellTblCrsr->GetNode()->GetCntntNode();
+        const SwCntntFrm *pCntntFrm = pCntntNode ? pCntntNode->GetFrm( 0, pShellTblCrsr->Start() ) : 0;
+        if( pCntntFrm )
+        {
+            SwRect aCharRect;
+            SwCrsrMoveState aTmpState( MV_NONE );
+            pCntntFrm->GetCharRect( aCharRect, *pShellTblCrsr->Start(), &aTmpState );
+            aSelPoint = Point( aCharRect.Left(), aCharRect.Top() );
+        }
+    }
+    else
+    {
+       aSelPoint = pFirstCrsr->GetSttPos();
+    }
 
     const SwPageFrm* pPage = GetLayout()->GetPageAtPos( aSelPoint );
+    ASSERT( pPage, "no page found!" );
 
-    // und ihren Seitendescribtor
-    const SwPageDesc* pPageDesc = pPrtDoc->FindPageDescByName(
-                                        pPage->GetPageDesc()->GetName() );
+    // get page descriptor - fall back to the first one if pPage could not be found
+    const SwPageDesc* pPageDesc = pPage ? pPrtDoc->FindPageDescByName(
+        pPage->GetPageDesc()->GetName() ) : &pPrtDoc->_GetPageDesc( (sal_uInt16)0 );
 
     if( !pFESh->IsTableMode() && pActCrsr->HasMark() )
     {   // Am letzten Absatz die Absatzattribute richten:
@@ -868,15 +884,32 @@ SwDoc * ViewShell::FillPrtDoc( SwDoc *pPrtDoc, const SfxPrinter* pPrt)
 
     // Die Y-Position der ersten Selektion
     // Die Y-Position der ersten Selektion
-    const Point aSelPoint = pFESh->IsTableMode() ?
-                            pFESh->GetTableCrsr()->GetSttPos() :
-                            pFirstCrsr->GetSttPos();
+    Point aSelPoint;
+    if( pFESh->IsTableMode() )
+    {
+        SwShellTableCrsr* pShellTblCrsr = pFESh->GetTableCrsr();
+
+        const SwCntntNode* pCntntNode = pShellTblCrsr->GetNode()->GetCntntNode();
+        const SwCntntFrm *pCntntFrm = pCntntNode ? pCntntNode->GetFrm( 0, pShellTblCrsr->Start() ) : 0;
+        if( pCntntFrm )
+        {
+            SwRect aCharRect;
+            SwCrsrMoveState aTmpState( MV_NONE );
+            pCntntFrm->GetCharRect( aCharRect, *pShellTblCrsr->Start(), &aTmpState );
+            aSelPoint = Point( aCharRect.Left(), aCharRect.Top() );
+        }
+    }
+    else
+    {
+       aSelPoint = pFirstCrsr->GetSttPos();
+    }
 
     const SwPageFrm* pPage = GetLayout()->GetPageAtPos( aSelPoint );
+    ASSERT( pPage, "no page found!" );
 
-    // und ihren Seitendescribtor
-    const SwPageDesc* pPageDesc = pPrtDoc->FindPageDescByName(
-                                pPage->GetPageDesc()->GetName() );
+    // get page descriptor - fall back to the first one if pPage could not be found
+    const SwPageDesc* pPageDesc = pPage ? pPrtDoc->FindPageDescByName(
+        pPage->GetPageDesc()->GetName() ) : &pPrtDoc->_GetPageDesc( (sal_uInt16)0 );
 
     if( !pFESh->IsTableMode() && pActCrsr->HasMark() )
     {   // Am letzten Absatz die Absatzattribute richten:

commit 0b5e05815cc498663cbf3281f17c5e3651cb06aa

sw: fix assert in SwViewShell::FillPrtDoc()
The point node isn't necessarily the start node; this will assert in
SwTextFrame::MapModelToView: Assertion `SwFrame::GetDep() == pNode' failed
when selecting a table and then File->Print->Print Selection.

Thanks Samuel for the hint.

(regression from 067a53bbcd209d7458b04142f989e1ff7ec1f785)

Change-Id: I7f58b79e068e8b7817261e81f999721ebfcfdcf1

sw/source/core/view/vprint.cxx
@@ -358,7 +358,8 @@ void SwViewShell::FillPrtDoc( SwDoc *pPrtDoc, const SfxPrinter* pPrt)
     {
         SwShellTableCursor* pShellTableCursor = pFESh->GetTableCursor();
 
-        const SwContentNode* pContentNode = pShellTableCursor->GetNode().GetContentNode();
+        const SwContentNode *const pContentNode =
+            pShellTableCursor->Start()->nNode.GetNode().GetContentNode();
         const SwContentFrame *const pContentFrame = pContentNode ? pContentNode->getLayoutFrame(GetLayout(), pShellTableCursor->Start()) : nullptr;
         if( pContentFrame )
         {