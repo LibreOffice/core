8f92d59b473bace53c59243f0a4b050eb17dfacf
Caolán McNamara <caolanm@redhat.com>
2018-12-23 17:46:48 +0000

    tdf#122273 modal dialog on modeless dialog needs special handling
    Change-Id: Idb5111b3f795f55c4b5644b5bcc6145106ff1392

--- sfx2/source/appl/sfxhelp.cxx
+++ sfx2/source/appl/sfxhelp.cxx
@@ -1009,6 +1009,32 @@ namespace
     }
 }
 
+void SfxHelp::incBusy(const vcl::Window* pParent)
+{
+    // lock any toplevel windows from being closed until busy is over
+    // ensure any dialogs are reset before entering
+    vcl::Window *xTopWin = Application::GetFirstTopLevelWindow();
+    while (xTopWin)
+    {
+        if (xTopWin != pParent)
+            xTopWin->IncModalCount();
+        xTopWin = Application::GetNextTopLevelWindow(xTopWin);
+    }
+}
+
+void SfxHelp::decBusy(const vcl::Window* pParent)
+{
+    // unlock any toplevel windows from being closed until busy is over
+    // ensure any dialogs are reset before entering
+    vcl::Window *xTopWin = Application::GetFirstTopLevelWindow();
+    while (xTopWin)
+    {
+        if (xTopWin != pParent)
+            xTopWin->DecModalCount();
+        xTopWin = Application::GetNextTopLevelWindow(xTopWin);
+    }
+}
+
 bool SfxHelp::Start_Impl(const OUString& rURL, const vcl::Window* pWindow, const OUString& rKeyword)
 {
     OUStringBuffer aHelpRootURL("vnd.sun.star.help://");
@@ -1117,6 +1143,7 @@ bool SfxHelp::Start_Impl(const OUString& rURL, const vcl::Window* pWindow, const
 
             if(bShowOfflineHelpPopUp)
             {
+                incBusy(pWindow);
                 std::unique_ptr<weld::Builder> xBuilder(Application::CreateBuilder(pWindow ? pWindow->GetFrameWeld() : nullptr, "sfx/ui/helpmanual.ui"));
                 std::unique_ptr<weld::MessageDialog> xQueryBox(xBuilder->weld_message_dialog("onlinehelpmanual"));
                 std::unique_ptr<weld::CheckButton> m_xHideOfflineHelpCB(xBuilder->weld_check_button("hidedialog"));
@@ -1127,6 +1154,7 @@ bool SfxHelp::Start_Impl(const OUString& rURL, const vcl::Window* pWindow, const
                 short OnlineHelpBox = xQueryBox->run();
                 bShowOfflineHelpPopUp = OnlineHelpBox != RET_OK;
                 aHelpOptions.SetOfflineHelpPopUp(!m_xHideOfflineHelpCB->get_state());
+                decBusy(pWindow);
             }
             if(!bShowOfflineHelpPopUp)
             {
@@ -1134,8 +1162,10 @@ bool SfxHelp::Start_Impl(const OUString& rURL, const vcl::Window* pWindow, const
                     return true;
                 else
                 {
+                    incBusy(pWindow);
                     NoHelpErrorBox aErrBox(pWindow ? pWindow->GetFrameWeld() : nullptr);
                     aErrBox.run();
+                    decBusy(pWindow);
                     return false;
                 }
             }
