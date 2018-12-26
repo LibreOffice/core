LibreOffice/core

commit c135f51b050dfa7ef095fe8a5b73cde37219a8dd

loplugin:returnconstant in XclExpAutofilter
Change-Id: Ibd57de5b8309048ec29e869f3058b0a8bb26df71
Reviewed-on: https://gerrit.libreoffice.org/59134
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sc/source/filter/excel/excrecds.cxx
@@ -702,7 +702,10 @@ bool XclExpAutofilter::AddEntry( const ScQueryEntry& rEntry )
         return true;
 
     if (GetOutput() != EXC_OUTPUT_BINARY && rItems.size() > 1)
-        return AddMultiValueEntry(rEntry);
+    {
+        AddMultiValueEntry(rEntry);
+        return true;
+    }
 
     bool bConflict = false;
     OUString  sText;
@@ -809,15 +812,13 @@ bool XclExpAutofilter::AddEntry( const ScQueryEntry& rEntry )
     return bConflict;
 }
 
-bool XclExpAutofilter::AddMultiValueEntry( const ScQueryEntry& rEntry )
+void XclExpAutofilter::AddMultiValueEntry( const ScQueryEntry& rEntry )
 {
     meType = MultiValue;
     const ScQueryEntry::QueryItemsType& rItems = rEntry.GetQueryItems();
     ScQueryEntry::QueryItemsType::const_iterator itr = rItems.begin(), itrEnd = rItems.end();
     for (; itr != itrEnd; ++itr)
         maMultiValues.push_back(itr->maString.getString());
-
-    return false;
 }
 
 void XclExpAutofilter::WriteBody( XclExpStream& rStrm )

sc/source/filter/inc/excrecds.hxx
@@ -393,7 +393,7 @@ public:
 
     bool                    HasCondition() const;
     bool                    AddEntry( const ScQueryEntry& rEntry );
-    bool                    AddMultiValueEntry( const ScQueryEntry& rEntry );
+    void                    AddMultiValueEntry( const ScQueryEntry& rEntry );
 
     virtual void            SaveXml( XclExpXmlStream& rStrm ) override;
 };

commit 60b89a46aaa704b1b2cd988cba7b19480b638c48

tdf#122331 Autofilter disappeared after export to XLSX
regression from
    commit c135f51b050dfa7ef095fe8a5b73cde37219a8dd
    loplugin:returnconstant in XclExpAutofilter

Change-Id: Iea7ac925aff47699292b4c3d758e99cd2d081560

sc/source/filter/excel/excrecds.cxx
@@ -666,7 +666,7 @@ bool XclExpAutofilter::AddEntry( const ScQueryEntry& rEntry )
     if (GetOutput() != EXC_OUTPUT_BINARY && rItems.size() > 1)
     {
         AddMultiValueEntry(rEntry);
-        return true;
+        return false;
     }
 
     bool bConflict = false;