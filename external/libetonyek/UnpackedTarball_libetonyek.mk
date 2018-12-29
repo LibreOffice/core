6212d5688946ad6db6d0c3fabf05579d162c0895
David Tardon <dtardon@redhat.com>
2018-12-29 09:34:05 +0100

    upload libetonyek 0.1.9
    Change-Id: Ib29e1a622e25731731512a695443ac2c530d30c2
    Reviewed-on: https://gerrit.libreoffice.org/65701
    Tested-by: Jenkins
    Reviewed-by: David Tardon <dtardon@redhat.com>
    (cherry picked from commit c6e73f3e5bfd5b1cca94f985b5299857d0238ae3)
    Reviewed-on: https://gerrit.libreoffice.org/65712
    Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>

--- external/libetonyek/UnpackedTarball_libetonyek.mk
+++ external/libetonyek/UnpackedTarball_libetonyek.mk
@@ -20,8 +20,6 @@ $(eval $(call gb_UnpackedTarball_add_patches,libetonyek,\
 	external/libetonyek/ubsan.patch \
 	external/libetonyek/rpath.patch \
 	external/libetonyek/warnings.patch \
-	external/libetonyek/initial-creator.patch.1 \
-	external/libetonyek/forcepoint.parsepath.null.deref.patch \
 ))
 
 ifneq ($(OS),MACOSX)
