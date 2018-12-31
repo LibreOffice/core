b9a400f44ea35345fe8f3adff22e5c6cc062ab7f
Patrik Vas <vas.patrik.d@gmail.com>
2018-12-31 15:16:40 +0100

    Fix scale bug in Bitmap Area dialog
    Change-Id: Id8886b7972e707698b539ad5787a26d6a64198ca

--- cui/source/tabpages/tpbitmap.cxx
+++ cui/source/tabpages/tpbitmap.cxx
@@ -329,7 +329,7 @@ void SvxBitmapTabPage::Reset( const SfxItemSet* rAttrs )
             m_bLogicalSize = false;
         }
 
-        ClickScaleHdl(*m_xTsbScale);
+        //ClickScaleHdl(*m_xTsbScale);
     }
     else
         m_xTsbScale->set_state(TRISTATE_INDET);
@@ -359,6 +359,8 @@ void SvxBitmapTabPage::Reset( const SfxItemSet* rAttrs )
         }
     }
     m_xTsbScale->set_state(eRelative);
+    ClickScaleHdl(*m_xTsbScale);
+
 
     if(rBitmapSize.Width() > 0 && rBitmapSize.Height() > 0)
     {
