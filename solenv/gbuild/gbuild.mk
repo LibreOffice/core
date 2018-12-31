9406ba46107674509d2460fa9cb2a73f50178424
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2018-12-31 12:22:39 +0100

    for tests MOZILLA_CERTIFICATE_FOLDER should be set to empty not 0
    0 means it has a value (string "0") but we check for an empty
    string. The end result is the same however.
    
    Change-Id: I01fed189eaf37673fdb254884c5e7382914b5211

--- solenv/gbuild/gbuild.mk
+++ solenv/gbuild/gbuild.mk
@@ -245,7 +245,7 @@ gb_GLOBALDEFS := $(sort $(gb_GLOBALDEFS))
 # * Cap the number of threads unittests use:
 gb_TEST_ENV_VARS := MAX_CONCURRENCY=4
 # * Disable searching for certificates by default:
-gb_TEST_ENV_VARS += MOZILLA_CERTIFICATE_FOLDER=0
+gb_TEST_ENV_VARS += MOZILLA_CERTIFICATE_FOLDER=
 # Avoid hanging if the cups daemon requests a password:
 gb_TEST_ENV_VARS += SAL_DISABLE_SYNCHRONOUS_PRINTER_DETECTION=1
 ifeq (,$(SAL_USE_VCLPLUGIN))
