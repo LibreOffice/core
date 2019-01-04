27770228c1292ae014dcf2b0aa59b3c6e857a351
Tamás Zolnai <tamas.zolnai@collabora.com>
2019-01-04 02:51:36 +0100

    Unfloat: Add some tests about the visibility of the unfloat button
    Reviewed-on: https://gerrit.libreoffice.org/65820
    Tested-by: Jenkins
    Reviewed-by: Tamás Zolnai <tamas.zolnai@collabora.com>
    (cherry picked from commit 9c4a374baa4d18dec4066e547d76a40501b20d45)
    
    Change-Id: Id0bc6e5be5a55480233afeae44eccac24fa01434

--- sw/source/core/inc/flyfrm.hxx
+++ sw/source/core/inc/flyfrm.hxx
@@ -24,6 +24,7 @@
 #include <vector>
 #include <frmfmt.hxx>
 #include <anchoredobject.hxx>
+#include <swdllapi.h>
 
 class SwPageFrame;
 class SwFormatFrameSize;
@@ -57,7 +58,7 @@ bool CalcClipRect( const SdrObject *pSdrObj, SwRect &rRect, bool bMove = true );
 
     #i26791# - inherit also from <SwAnchoredFlyFrame>
 */
-class SwFlyFrame : public SwLayoutFrame, public SwAnchoredObject
+class SW_DLLPUBLIC SwFlyFrame : public SwLayoutFrame, public SwAnchoredObject
 {
     // is allowed to lock, implemented in frmtool.cxx
     friend void AppendObjs   ( const SwFrameFormats *, sal_uLong, SwFrame *, SwPageFrame *, SwDoc* );
@@ -270,9 +271,9 @@ public:
     void InvalidateContentPos();
 
     void SelectionHasChanged(SwFEShell* pShell);
+    bool IsShowUnfloatButton(SwWrtShell* pWrtSh) const;
 
 private:
-    bool IsShowUnfloatButton(SwWrtShell* pWrtSh) const;
     void UpdateUnfloatButton(SwWrtShell* pWrtSh, bool bShow) const;
     void PaintDecorators() const;
 };
