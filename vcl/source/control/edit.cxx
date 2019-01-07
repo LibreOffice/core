d09ca3697bfbff8c6851a1088c594d7d6426bd93
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2019-01-07 07:29:03 -0500

    Revert "lok: custom widgets: render editbox borders"
    Looks like this is no longer needed to correctly render
    editbox borders. Not clear what fixed the issue.
    
    This reverts commit 9c5bc82fdfa67d0149806a0b55962edc7925dd0f.

--- vcl/source/control/edit.cxx
+++ vcl/source/control/edit.cxx
@@ -57,7 +57,6 @@
 
 #include <comphelper/processfactory.hxx>
 #include <comphelper/string.hxx>
-#include <comphelper/lok.hxx>
 
 #include <sot/exchange.hxx>
 #include <sot/formats.hxx>
@@ -1001,7 +1000,7 @@ void Edit::ImplClearBackground(vcl::RenderContext& rRenderContext, const tools::
     }
 }
 
-void Edit::ImplPaintBorder(vcl::RenderContext& rRenderContext, long nXStart, long nXEnd)
+void Edit::ImplPaintBorder(vcl::RenderContext const & rRenderContext, long nXStart, long nXEnd)
 {
     // this is not needed when double-buffering
     if (SupportsDoubleBuffering())
@@ -1063,17 +1062,7 @@ void Edit::ImplPaintBorder(vcl::RenderContext& rRenderContext, long nXStart, lon
             }
             else
             {
-                // For some mysterious reaon, in headless/svp rendering,
-                // pBorder has bad clipping region (shows as 1x1@0,0),
-                // and therefore doesn't render anything at all.
-                // In the case that we know we're in headless/svp, we
-                // render directly on the current context (the edit control).
-                // But if we (the editbox) are part of a more complex control
-                // (e.g. spinbox), we render not (i.e. we let pBorder pretend).
-                if (!mbIsSubEdit && comphelper::LibreOfficeKit::isActive())
-                    pBorder->Paint(rRenderContext, tools::Rectangle());
-                else
-                    pBorder->Paint(*pBorder, tools::Rectangle());
+                pBorder->Paint(*pBorder, tools::Rectangle());
             }
         }
     }
