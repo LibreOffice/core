4341966c04d85d004cca95c38fbee74977e3e896
Brian Fraser <andthebrain@softfrog.ca>
2019-01-07 09:30:14 -0800

    tdf#122454 Impress: Correct d'n'd animation selection in Windows
    Change-Id: I0f17ff657beb2a5b3dfed91856ec45ddfca793a9

--- sd/source/ui/animations/CustomAnimationList.cxx
+++ sd/source/ui/animations/CustomAnimationList.cxx
@@ -700,11 +700,13 @@ sal_Int8 CustomAnimationList::ExecuteDrop( const ExecuteDropEvent& /*rEvt*/ )
         ret = DND_ACTION_MOVE;
     }
 
+    // NOTE: Don't call SvTreeListBox::ExecuteDrop(...) because all required
+    //       move operations have been completed here to update the model.
     return ret;
 }
 
 // D'n'D #6: Cleanup (regardless of if we were target of drop or not)
-void CustomAnimationList::DragFinished( sal_Int8 nDropAction )
+void CustomAnimationList::DragFinished( sal_Int8 /*nDropAction*/ )
 {
     mpDndEffectDragging = nullptr;
     mpDndEffectInsertBefore = nullptr;
@@ -714,7 +716,9 @@ void CustomAnimationList::DragFinished( sal_Int8 nDropAction )
     // Can hit this without running ExecuteDrop(...) when drag canceled.
     mpMainSequence->rebuild();
 
-    SvTreeListBox::DragFinished( nDropAction );
+    // Note: Don't call SvTreeListBox::DragFinished(...) because we don't call
+    //       SvTreeListBox::ExecuteDrop(...) which sets variables that are
+    //       needed in its DragFinished(...) method.
 }
 
 VCL_BUILDER_FACTORY(CustomAnimationList)
