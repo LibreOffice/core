LibreOffice/core

commit 457acbfa304ac8bda0755c9ca8f1e1e22e490ac8

UITest_writer_tests: split this into 4 parts
Before: make -sr -j2 UITest_writer_tests UITest_writer_tests2 -> 5m9,863s
After: make -sr -j4 UITest_writer_tests UITest_writer_tests2 UITest_writer_tests3 UITest_writer_tests4 -> 2m21,734s

Again just assuming that each .py file means the same amount of CPU cost
(which may not be true exactly).

Change-Id: I9c4144c40eb1116d4b999bbba68e2fbef440af7b
Reviewed-on: https://gerrit.libreoffice.org/56880
Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
Tested-by: Jenkins

sw/qa/uitest/writer_tests/insertCaption.py
@@ -1,76 +1,0 @@
-# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
-#
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-from uitest.framework import UITestCase
-from libreoffice.uno.propertyvalue import mkPropertyValues
-from uitest.uihelper.common import get_state_as_dict
-import time
-from uitest.debug import sleep
-from uitest.uihelper.common import select_pos
-
-class insertCaption(UITestCase):
-
-   def test_insert_caption(self):
-        self.ui_test.create_doc_in_start_center("writer")
-        document = self.ui_test.get_component()
-        self.ui_test.execute_dialog_through_command(".uno:InsertFrame")   #  insert frame
-        xDialogFr = self.xUITest.getTopFocusWindow()
-
-        xWidth = xDialogFr.getChild("width")
-        xWidth.executeAction("UP", tuple())
-        xWidth.executeAction("UP", tuple())
-
-        xHeight = xDialogFr.getChild("height")
-        xHeight.executeAction("UP", tuple())
-        xHeight.executeAction("UP", tuple())
-
-        xOkBtn=xDialogFr.getChild("ok")
-        xOkBtn.executeAction("CLICK", tuple())
-
-        self.assertEqual(document.TextFrames.getCount(), 1)
-
-        self.ui_test.execute_dialog_through_command(".uno:InsertCaptionDialog")   #  caption
-        xDialogCaption = self.xUITest.getTopFocusWindow()
-
-        xCapt = xDialogCaption.getChild("caption_edit")
-        xCapt.executeAction("TYPE", mkPropertyValues({"TEXT":"Caption"}))
-
-        xOkBtn=xDialogCaption.getChild("ok")
-        xOkBtn.executeAction("CLICK", tuple())
-
-        xFrame = document.TextFrames.getByIndex(0)
-
-        self.assertEqual(document.TextFrames.getByIndex(0).Text.String, "\nText 1: Caption")
-
-        self.ui_test.execute_dialog_through_command(".uno:InsertCaptionDialog")   # 2nd caption
-        xDialogCaption = self.xUITest.getTopFocusWindow()
-        xCapt = xDialogCaption.getChild("caption_edit")
-        xCapt.executeAction("TYPE", mkPropertyValues({"TEXT":"Caption2"}))
-        xSep = xDialogCaption.getChild("separator_edit")
-        xSep.executeAction("TYPE", mkPropertyValues({"TEXT":"-"}))
-
-        xOkBtn=xDialogCaption.getChild("ok")
-        xOkBtn.executeAction("CLICK", tuple())
-
-        self.assertEqual(document.TextFrames.getByIndex(0).Text.String, "\nText 1: Caption\nText 2-: Caption2")
-
-        self.ui_test.execute_dialog_through_command(".uno:InsertCaptionDialog")   # 3. caption
-        xDialogCaption = self.xUITest.getTopFocusWindow()
-        xCapt = xDialogCaption.getChild("caption_edit")
-        xCapt.executeAction("TYPE", mkPropertyValues({"TEXT":"Caption3"}))
-        xSep = xDialogCaption.getChild("separator_edit")
-        xSep.executeAction("TYPE", mkPropertyValues({"TEXT":"-"}))
-        xPos = xDialogCaption.getChild("position")
-        select_pos(xPos, "1")
-
-        xOkBtn=xDialogCaption.getChild("ok")
-        xOkBtn.executeAction("CLICK", tuple())
-
-        self.assertEqual(document.TextFrames.getByIndex(0).Text.String, "\nText 1: Caption\nText 2-: Caption2\nText 3--: Caption3")
-
-        self.ui_test.close_doc()
-
-# vim: set shiftwidth=4 softtabstop=4 expandtab:

sw/Module_sw.mk
@@ -135,6 +135,8 @@ $(eval $(call gb_Module_add_screenshot_targets,sw,\
 $(eval $(call gb_Module_add_uicheck_targets,sw,\
 	UITest_writer_tests \
 	UITest_writer_tests2 \
+	UITest_writer_tests3 \
+	UITest_writer_tests4 \
 ))
 endif
 

sw/UITest_writer_tests3.mk
@@ -1,0 +1,16 @@
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call 	gb_UITest_UITest,writer_tests3))
+
+$(eval $(call gb_UITest_add_modules,writer_tests3,$(SRCDIR)/sw/qa/uitest,\
+	writer_tests3/ \
+))
+
+$(eval $(call gb_UITest_set_defs,writer_tests3, \
+    TDOC="$(SRCDIR)/sw/qa/uitest/writer_tests/data" \
+))

sw/UITest_writer_tests4.mk
@@ -1,0 +1,16 @@
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call 	gb_UITest_UITest,writer_tests4))
+
+$(eval $(call gb_UITest_add_modules,writer_tests4,$(SRCDIR)/sw/qa/uitest,\
+	writer_tests4/ \
+))
+
+$(eval $(call gb_UITest_set_defs,writer_tests4, \
+    TDOC="$(SRCDIR)/sw/qa/uitest/writer_tests/data" \
+))

commit ef48bdbe038734599e57438d7265008785665c54

sw: restore accidentally deleted/disabled UITests
(regression from 457acbfa304ac8bda0755c9ca8f1e1e22e490ac8)

Change-Id: I2e437748d5a98c3b3eef501134ef9442f8fc3451

sw/qa/uitest/writer_tests/insertCaption.py
@@ -1,0 +1,76 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from uitest.framework import UITestCase
+from libreoffice.uno.propertyvalue import mkPropertyValues
+from uitest.uihelper.common import get_state_as_dict
+import time
+from uitest.debug import sleep
+from uitest.uihelper.common import select_pos
+
+class insertCaption(UITestCase):
+
+   def test_insert_caption(self):
+        self.ui_test.create_doc_in_start_center("writer")
+        document = self.ui_test.get_component()
+        self.ui_test.execute_dialog_through_command(".uno:InsertFrame")   #  insert frame
+        xDialogFr = self.xUITest.getTopFocusWindow()
+
+        xWidth = xDialogFr.getChild("width")
+        xWidth.executeAction("UP", tuple())
+        xWidth.executeAction("UP", tuple())
+
+        xHeight = xDialogFr.getChild("height")
+        xHeight.executeAction("UP", tuple())
+        xHeight.executeAction("UP", tuple())
+
+        xOkBtn=xDialogFr.getChild("ok")
+        xOkBtn.executeAction("CLICK", tuple())
+
+        self.assertEqual(document.TextFrames.getCount(), 1)
+
+        self.ui_test.execute_dialog_through_command(".uno:InsertCaptionDialog")   #  caption
+        xDialogCaption = self.xUITest.getTopFocusWindow()
+
+        xCapt = xDialogCaption.getChild("caption_edit")
+        xCapt.executeAction("TYPE", mkPropertyValues({"TEXT":"Caption"}))
+
+        xOkBtn=xDialogCaption.getChild("ok")
+        xOkBtn.executeAction("CLICK", tuple())
+
+        xFrame = document.TextFrames.getByIndex(0)
+
+        self.assertEqual(document.TextFrames.getByIndex(0).Text.String, "\nText 1: Caption")
+
+        self.ui_test.execute_dialog_through_command(".uno:InsertCaptionDialog")   # 2nd caption
+        xDialogCaption = self.xUITest.getTopFocusWindow()
+        xCapt = xDialogCaption.getChild("caption_edit")
+        xCapt.executeAction("TYPE", mkPropertyValues({"TEXT":"Caption2"}))
+        xSep = xDialogCaption.getChild("separator_edit")
+        xSep.executeAction("TYPE", mkPropertyValues({"TEXT":"-"}))
+
+        xOkBtn=xDialogCaption.getChild("ok")
+        xOkBtn.executeAction("CLICK", tuple())
+
+        self.assertEqual(document.TextFrames.getByIndex(0).Text.String, "\nText 1: Caption\nText 2-: Caption2")
+
+        self.ui_test.execute_dialog_through_command(".uno:InsertCaptionDialog")   # 3. caption
+        xDialogCaption = self.xUITest.getTopFocusWindow()
+        xCapt = xDialogCaption.getChild("caption_edit")
+        xCapt.executeAction("TYPE", mkPropertyValues({"TEXT":"Caption3"}))
+        xSep = xDialogCaption.getChild("separator_edit")
+        xSep.executeAction("TYPE", mkPropertyValues({"TEXT":"-"}))
+        xPos = xDialogCaption.getChild("position")
+        select_pos(xPos, "1")
+
+        xOkBtn=xDialogCaption.getChild("ok")
+        xOkBtn.executeAction("CLICK", tuple())
+
+        self.assertEqual(document.TextFrames.getByIndex(0).Text.String, "\nText 1: Caption\nText 2-: Caption2\nText 3--: Caption3")
+
+        self.ui_test.close_doc()
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab: