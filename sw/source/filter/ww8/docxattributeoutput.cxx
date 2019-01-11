24613d7abf820aff639a276a1819ada8d83e9063
Tamás Zolnai <tamas.zolnai@collabora.com>
2019-01-11 19:58:02 +0100

    tdf#122658: Empty date form field is not exported correctly to DOCX file
    We need to export date format and also text content
    in case of empty date field. Otherwise the exported
    date field will be lost during import into LO Writer
    or MSO Word.
    
    Change-Id: I5cf65bedba010f64ca8f56262057f3cce32b0943
    Reviewed-on: https://gerrit.libreoffice.org/66194
    Tested-by: Jenkins
    Reviewed-by: Tamás Zolnai <tamas.zolnai@collabora.com>

--- sw/source/filter/ww8/docxattributeoutput.cxx
+++ sw/source/filter/ww8/docxattributeoutput.cxx
@@ -5037,7 +5037,13 @@ void DocxAttributeOutput::WritePostponedFormControl(const SdrObject* pObject)
             }
         }
         else
+        {
             aContentText = xPropertySet->getPropertyValue("HelpText").get<OUString>();
+            if(aContentText.isEmpty())
+                aContentText = " "; // Need to write out something to have it imported by MS Word
+            if(sDateFormat.isEmpty())
+                sDateFormat = "dd/MM/yyyy"; // Need to set date format even if there is no date set
+        }
 
         // output component
 
