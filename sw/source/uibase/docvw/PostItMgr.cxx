LibreOffice/core

commit 9b67b6a6d4b912f1e515d3884fa02c1be8aa6921

sw_redlinehide_4b: consider the comments in the margin
SwPostItMgr needs to hide the comments that are in delete redlines.

Also notify SwPostItMgr when redlines are created/removed.

SwPostItMgr is owned by the ViewShell but there is currently only one
layout so SwRootFrame still needs to broadcast to all of them.

Also an EndListening call was missing in
SwPostItMgr::CheckForRemovedPostIts() which caused asserts.

Change-Id: Ic0fccde50f6fdaa449afb535476c00c41ba94287

sw/source/uibase/docvw/PostItMgr.cxx
@@ -52,6 +52,7 @@
 #include <ndtxt.hxx>
 #include <redline.hxx>
 #include <docary.hxx>
+#include <rootfrm.hxx>
 #include <SwRewriter.hxx>
 #include <tools/color.hxx>
 #include <unotools/datetime.hxx>
@@ -240,13 +241,15 @@ SwPostItMgr::~SwPostItMgr()
 
 void SwPostItMgr::CheckForRemovedPostIts()
 {
+    IDocumentRedlineAccess const& rIDRA(mpWrtShell->getIDocumentRedlineAccess());
     bool bRemoved = false;
     auto currentIt = mvPostItFields.begin();
     while(currentIt != mvPostItFields.end())
     {
         auto it = currentIt++;
-        if ( !(*it)->UseElement() )
+        if (!(*it)->UseElement(*mpWrtShell->GetLayout(), rIDRA))
         {
+            EndListening(const_cast<SfxBroadcaster&>(*(*it)->GetBroadCaster()));
             SwSidebarItem* p = *it;
             currentIt = mvPostItFields.erase(std::remove(mvPostItFields.begin(), mvPostItFields.end(), *it), mvPostItFields.end());
             if (GetActiveSidebarWin() == p->pPostIt)
@@ -531,9 +534,10 @@ bool SwPostItMgr::CalcRects()
     PreparePageContainer();
     if ( !mvPostItFields.empty() )
     {
+        IDocumentRedlineAccess const& rIDRA(mpWrtShell->getIDocumentRedlineAccess());
         for (auto const& pItem : mvPostItFields)
         {
-            if ( !pItem->UseElement() )
+            if (!pItem->UseElement(*mpWrtShell->GetLayout(), rIDRA))
             {
                 OSL_FAIL("PostIt is not in doc or other wrong use");
                 bRepair = true;
@@ -887,10 +891,11 @@ void SwPostItMgr::LayoutPostIts()
 
         if (!ShowNotes())
         {       // we do not want to see the notes anymore -> Options-Writer-View-Notes
+            IDocumentRedlineAccess const& rIDRA(mpWrtShell->getIDocumentRedlineAccess());
             bool bRepair = false;
             for (auto const& postItField : mvPostItFields)
             {
-                if ( !postItField->UseElement() )
+                if (!postItField->UseElement(*mpWrtShell->GetLayout(), rIDRA))
                 {
                     OSL_FAIL("PostIt is not in doc!");
                     bRepair = true;
@@ -1298,8 +1303,13 @@ void SwPostItMgr::AddPostIts(bool bCheckExistence, bool bFocus)
     {
         if ( pSwFormatField->GetTextField())
         {
-            if ( pSwFormatField->IsFieldInDoc() )
+            IDocumentRedlineAccess const& rIDRA(mpWrtShell->getIDocumentRedlineAccess());
+            if (pSwFormatField->IsFieldInDoc()
+                && (!mpWrtShell->GetLayout()->IsHideRedlines()
+                    || !sw::IsFieldDeletedInModel(rIDRA, *pSwFormatField->GetTextField())))
+            {
                 InsertItem(pSwFormatField,bCheckExistence,bFocus);
+            }
         }
         pSwFormatField = aIter.Next();
     }

sw/inc/postithelper.hxx
@@ -31,6 +31,7 @@ class SwRootFrame;
 class SwPostItMgr;
 class SwEditWin;
 class SwFrame;
+class IDocumentRedlineAccess;
 namespace sw { namespace annotation {
     class SwAnnotationWin;
 } }
@@ -115,7 +116,7 @@ public:
     SwSidebarItem & operator =(SwSidebarItem &&) = default;
 
     virtual SwPosition GetAnchorPosition() const = 0;
-    virtual bool UseElement() = 0;
+    virtual bool UseElement(SwRootFrame const&, IDocumentRedlineAccess const&) = 0;
     virtual const SwFormatField& GetFormatField() const = 0;
     virtual const SfxBroadcaster* GetBroadCaster() const = 0;
     virtual VclPtr<sw::annotation::SwAnnotationWin> GetSidebarWindow( SwEditWin& rEditWin,
@@ -134,7 +135,7 @@ public:
     }
 
     virtual SwPosition GetAnchorPosition() const override;
-    virtual bool UseElement() override;
+    virtual bool UseElement(SwRootFrame const&, IDocumentRedlineAccess const&) override;
     virtual const SwFormatField& GetFormatField() const override
     {
         return mrFormatField;

sw/source/core/doc/DocumentRedlineManager.cxx
@@ -21,6 +21,8 @@
 #include <rootfrm.hxx>
 #include <txtfrm.hxx>
 #include <doc.hxx>
+#include <docsh.hxx>
+#include <fmtfld.hxx>
 #include <IDocumentUndoRedo.hxx>
 #include <IDocumentFieldsAccess.hxx>
 #include <IDocumentState.hxx>
@@ -166,6 +168,10 @@ void UpdateFramesForAddDeleteRedline(SwDoc & rDoc, SwPaM const& rPam)
     }
     // fields last - SwGetRefField::UpdateField requires up-to-date frames
     UpdateFieldsForRedline(rDoc.getIDocumentFieldsAccess()); // after footnotes
+
+    // update SwPostItMgr / notes in the margin
+    rDoc.GetDocShell()->Broadcast(
+            SwFormatFieldHint(nullptr, SwFormatFieldHintWhich::REMOVED) );
 }
 
 void UpdateFramesForRemoveDeleteRedline(SwDoc & rDoc, SwPaM const& rPam)
@@ -225,6 +231,10 @@ void UpdateFramesForRemoveDeleteRedline(SwDoc & rDoc, SwPaM const& rPam)
     }
     // fields last - SwGetRefField::UpdateField requires up-to-date frames
     UpdateFieldsForRedline(rDoc.getIDocumentFieldsAccess()); // after footnotes
+
+    // update SwPostItMgr / notes in the margin
+    rDoc.GetDocShell()->Broadcast(
+            SwFormatFieldHint(nullptr, SwFormatFieldHintWhich::INSERTED) );
 }
 
 } // namespace sw

sw/source/core/fields/postithelper.cxx
@@ -31,6 +31,7 @@
 #include <txtfrm.hxx>
 #include <tabfrm.hxx>
 #include <IDocumentRedlineAccess.hxx>
+#include <IDocumentFieldsAccess.hxx>
 #include <redline.hxx>
 #include <scriptinfo.hxx>
 #include <editeng/charhiddenitem.hxx>
@@ -142,9 +143,12 @@ SwPosition SwAnnotationItem::GetAnchorPosition() const
     return aPos;
 }
 
-bool SwAnnotationItem::UseElement()
+bool SwAnnotationItem::UseElement(SwRootFrame const& rLayout,
+        IDocumentRedlineAccess const& rIDRA)
 {
-    return mrFormatField.IsFieldInDoc();
+    return mrFormatField.IsFieldInDoc()
+        && (!rLayout.IsHideRedlines()
+            || !sw::IsFieldDeletedInModel(rIDRA, *mrFormatField.GetTextField()));
 }
 
 VclPtr<sw::annotation::SwAnnotationWin> SwAnnotationItem::GetSidebarWindow(

sw/source/core/layout/wsfrm.cxx
@@ -4518,6 +4518,13 @@ void SwRootFrame::SetHideRedlines(bool const bHideRedlines)
     rIDFA.UpdateExpFields(nullptr, false);
     rIDFA.UpdateRefFields();
 
+    // update SwPostItMgr / notes in the margin
+    // note: as long as all shells share layout, broadcast to all shells!
+    rDoc.GetDocShell()->Broadcast( SwFormatFieldHint(nullptr, bHideRedlines
+            ? SwFormatFieldHintWhich::REMOVED
+            : SwFormatFieldHintWhich::INSERTED) );
+
+
 //    InvalidateAllContent(SwInvalidateFlags::Size); // ??? TODO what to invalidate?  this is the big hammer
 }
 

commit 69b6de7836b74c9e89e4d92deb8ec1eec7188ab8

sw_redlinehide: fix infinite loop when deleting comments
The problem is that sw::UpdateFramesForAddDeleteRedline() sends a
SwFormatFieldHintWhich::REMOVED event in order to remove any comments
that may have been deleted from the document view.

Unfortunately there's another listener in class FieldDocWatchingStack
that is used during various SwPostItMgr::Delete* functions, which will
effectively start over from scratch if it receives this event, so it
is an infinite loop.

Avoid this by simply ignoring any redline-deleted fields; deleting them
again won't provide any benefit anyway.

This can be seen with UITest
deleteAllComments.DeleteAllComments.test_comment_trackchanges.

(regression from 9b67b6a6d4b912f1e515d3884fa02c1be8aa6921)

Change-Id: Ie1d5676d1d79a51e2e4a01a3e282f8739ff08899

sw/source/uibase/docvw/PostItMgr.cxx
@@ -1381,6 +1381,28 @@ public:
     }
 };
 
+class IsFieldNotDeleted : public FilterFunctor
+{
+private:
+    IDocumentRedlineAccess const& m_rIDRA;
+    FilterFunctor const& m_rNext;
+
+public:
+    IsFieldNotDeleted(IDocumentRedlineAccess const& rIDRA,
+            FilterFunctor & rNext)
+        : m_rIDRA(rIDRA)
+        , m_rNext(rNext)
+    {
+    }
+    bool operator()(const SwFormatField* pField) const override
+    {
+        if (!m_rNext(pField))
+            return false;
+        if (!pField->GetTextField())
+            return false;
+        return !sw::IsFieldDeletedInModel(m_rIDRA, *pField->GetTextField());
+    }
+};
 
 //Manages the passed in vector by automatically removing entries if they are deleted
 //and automatically adding entries if they appear in the document and match the
@@ -1491,7 +1513,9 @@ void SwPostItMgr::Delete(const OUString& rAuthor)
     mpWrtShell->StartUndo( SwUndoId::DELETE, &aRewriter );
 
     IsPostitFieldWithAuthorOf aFilter(rAuthor);
-    FieldDocWatchingStack aStack(mvPostItFields, *mpView->GetDocShell(), aFilter);
+    IDocumentRedlineAccess const& rIDRA(mpWrtShell->getIDocumentRedlineAccess());
+    IsFieldNotDeleted aFilter2(rIDRA, aFilter);
+    FieldDocWatchingStack aStack(mvPostItFields, *mpView->GetDocShell(), aFilter2);
     while (const SwFormatField* pField = aStack.pop())
     {
         if (mpWrtShell->GotoField(*pField))
@@ -1518,7 +1542,9 @@ void SwPostItMgr::Delete(sal_uInt32 nPostItId)
     mpWrtShell->StartUndo( SwUndoId::DELETE, &aRewriter );
 
     IsPostitFieldWithPostitId aFilter(nPostItId);
-    FieldDocWatchingStack aStack(mvPostItFields, *mpView->GetDocShell(), aFilter);
+    IDocumentRedlineAccess const& rIDRA(mpWrtShell->getIDocumentRedlineAccess());
+    IsFieldNotDeleted aFilter2(rIDRA, aFilter);
+    FieldDocWatchingStack aStack(mvPostItFields, *mpView->GetDocShell(), aFilter2);
     const SwFormatField* pField = aStack.pop();
     if (pField && mpWrtShell->GotoField(*pField))
         mpWrtShell->DelRight();
@@ -1539,8 +1565,10 @@ void SwPostItMgr::Delete()
     mpWrtShell->StartUndo( SwUndoId::DELETE, &aRewriter );
 
     IsPostitField aFilter;
+    IDocumentRedlineAccess const& rIDRA(mpWrtShell->getIDocumentRedlineAccess());
+    IsFieldNotDeleted aFilter2(rIDRA, aFilter);
     FieldDocWatchingStack aStack(mvPostItFields, *mpView->GetDocShell(),
-        aFilter);
+        aFilter2);
     while (const SwFormatField* pField = aStack.pop())
     {
         if (mpWrtShell->GotoField(*pField))