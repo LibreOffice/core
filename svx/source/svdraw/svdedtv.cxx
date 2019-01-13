a8cdc713dc77542b0e5158c27dc06be514424791
Johnny_M <klasse@partyheld.de>
2019-01-13 14:55:19 +0100

    Translate German variable names
    Please feel free to suggest a better name, if desired
    
    Change-Id: I116b30f3734a96fe30a95a30514551983e0258ae

--- svx/source/svdraw/svdedtv.cxx
+++ svx/source/svdraw/svdedtv.cxx
@@ -633,10 +633,10 @@ void SdrEditView::CheckPossibilities()
         m_bPossibilitiesDirty=false;
 
         if (m_bReadOnly) {
-            bool bMerker1=m_bGrpEnterPossible;
+            bool bBuffer1=m_bGrpEnterPossible;
             ImpResetPossibilityFlags();
             m_bReadOnly=true;
-            m_bGrpEnterPossible=bMerker1;
+            m_bGrpEnterPossible=bBuffer1;
         }
         if (m_bMoveAllowed) {
             // Don't allow moving glued connectors.
