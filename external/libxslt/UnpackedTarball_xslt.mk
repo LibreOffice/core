e112260713a2f5fbd9846aa751221f0767c5d5ca
Michael Stahl <Michael.Stahl@cib.de>
2019-01-14 15:52:00 +0100

    libxslt: upgrade to release 1.1.33
    Reviewed-on: https://gerrit.libreoffice.org/66309
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <Michael.Stahl@cib.de>
    (cherry picked from commit 3f720ac5ff65ddb2c0a06e48d0e2e9bec29f3a43)
    Reviewed-on: https://gerrit.libreoffice.org/66364
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>
    (cherry picked from commit efe7b735104343e5e0182e1b004aca9c7b79a901)
    (cherry picked from commit 70e2ca96778af1b7734aa9dde9627567cd210bc4)
    
    Change-Id: Ifc64dae74df341e107857e43223ead04b9c1061e

--- external/libxslt/UnpackedTarball_xslt.mk
+++ external/libxslt/UnpackedTarball_xslt.mk
@@ -11,9 +11,11 @@ $(eval $(call gb_UnpackedTarball_UnpackedTarball,xslt))
 
 $(eval $(call gb_UnpackedTarball_set_tarball,xslt,$(LIBXSLT_TARBALL)))
 
-$(eval $(call gb_UnpackedTarball_add_patches,xslt,\
+$(eval $(call gb_UnpackedTarball_update_autoconf_configs,xslt))
+
+$(eval $(call gb_UnpackedTarball_add_patches,libxslt,\
 	external/libxslt/libxslt-config.patch.1 \
-	external/libxslt/libxslt-internal-symbols.patch \
+	external/libxslt/libxslt-internal-symbols.patch.1 \
 	external/libxslt/libxslt-msvc.patch.2 \
 	external/libxslt/libxslt-1.1.26-memdump.patch \
 	external/libxslt/rpath.patch.0 \
