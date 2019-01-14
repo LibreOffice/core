0734a010a1e331a5800deb38c857c1abe2a241cc
Justin Luth <justin_luth@sil.org>
2019-01-14 22:18:46 +0300

    tdf#101826 ww8import: don't convert XATTR back and forth: textboxes
    In the case of textbox import, the properties were being
    converted into RES_BACKGROUND and then back to XATTR again.
    Just copy the XATTR properties to the new FlySet instead.
    
    Can you tell, this is an exploratory patch?
    
    Change-Id: Ib65b3d9097d0a56dbe205b419d052af53d0132c8

--- sw/source/filter/ww8/ww8graf.cxx
+++ sw/source/filter/ww8/ww8graf.cxx
@@ -1683,6 +1683,27 @@ void SwWW8ImplReader::MatchSdrItemsIntoFlySet( SdrObject const * pSdrObj,
             rFlySet.Put( *pPoolItem );
         }
 
+    bool bSkipResBackground = false;
+    SfxItemPool* pPool = rFlySet.GetPool();
+    if ( pPool )
+    {
+        for(sal_uInt16 i = XATTR_START; i < XATTR_END; ++i)
+        {
+            SfxItemPool* pAttrPool = pPool->GetMasterPool();
+            if ( pAttrPool && !pAttrPool->IsInRange(i) )
+                pAttrPool = pAttrPool->GetSecondaryPool();
+            if ( !pAttrPool )
+                continue;
+
+            if( SfxItemState::SET == rOldSet.GetItemState(i, false, &pPoolItem) )
+            {
+                rFlySet.Put( *pPoolItem );
+                if ( i == XATTR_FILLSTYLE )
+                    bSkipResBackground = true;
+            }
+        }
+    }
+
     // now calculate the borders and build the box: The unit is needed for the
     // frame SIZE!
     SvxBoxItem aBox(sw::util::ItemGet<SvxBoxItem>(rFlySet, RES_BOX));
@@ -1809,7 +1830,7 @@ void SwWW8ImplReader::MatchSdrItemsIntoFlySet( SdrObject const * pSdrObj,
 
     // Separate transparency
     eState = rOldSet.GetItemState(XATTR_FILLTRANSPARENCE, true, &pItem);
-    if (eState == SfxItemState::SET)
+    if (!bSkipResBackground && eState == SfxItemState::SET)
     {
         sal_uInt16 nRes = WW8ITEMVALUE(rOldSet, XATTR_FILLTRANSPARENCE,
             XFillTransparenceItem);
@@ -1820,7 +1841,7 @@ void SwWW8ImplReader::MatchSdrItemsIntoFlySet( SdrObject const * pSdrObj,
 
     // Background: SvxBrushItem
     eState = rOldSet.GetItemState(XATTR_FILLSTYLE, true, &pItem);
-    if (eState == SfxItemState::SET)
+    if (!bSkipResBackground && eState == SfxItemState::SET)
     {
         const drawing::FillStyle eFill = static_cast<const XFillStyleItem*>(pItem)->GetValue();
 
