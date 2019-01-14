1fa954b42490e65d2abb3c6692f308e93ea78802
Caolán McNamara <caolanm@redhat.com>
2019-01-14 15:55:12 +0000

    SolarMutexGuard needed here
    assert seen in dbgutil
    
    Change-Id: I09cab9e4fa4ce81d71518d87c4a571a467ead52d

--- vcl/unx/gtk3/gtk3gtkinst.cxx
+++ vcl/unx/gtk3/gtk3gtkinst.cxx
@@ -3017,6 +3017,7 @@ private:
 
     static gboolean launch_overflow_switch_page(GtkInstanceNotebook* pThis)
     {
+        SolarMutexGuard aGuard;
         pThis->signal_overflow_switch_page();
         return false;
     }
