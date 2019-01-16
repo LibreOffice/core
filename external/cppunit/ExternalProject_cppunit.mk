a0e326f5e109a1dc3402b4102ad730de6feeba96
himajin100000 <himajin100000@gmail.com>
2019-01-16 16:34:36 +0900

    Minimum Supported Version is VS2017
    Change-Id: Id771f1fe0d8c6702a52836f6229a944d259fed4c
    Reviewed-on: https://gerrit.libreoffice.org/66424
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <Michael.Stahl@cib.de>

--- external/cppunit/ExternalProject_cppunit.mk
+++ external/cppunit/ExternalProject_cppunit.mk
@@ -18,7 +18,6 @@ $(call gb_ExternalProject_get_state_target,cppunit,build) :
 	$(call gb_ExternalProject_run,build,\
 	    PROFILEFLAGS="$(if $(MSVC_USE_DEBUG_RUNTIME),Debug,Release) \
 		/p:Platform=$(if $(filter INTEL,$(CPUNAME)),Win32,x64) \
-			$(if $(filter 140,$(VCVER)),/p:PlatformToolset=v140 /p:VisualStudioVersion=14.0 /ToolsVersion:14.0) \
 			$(if $(filter 150,$(VCVER)),/p:PlatformToolset=v141 /p:VisualStudioVersion=15.0 /ToolsVersion:15.0) \
 			$(if $(filter 150-10,$(VCVER)-$(WINDOWS_SDK_VERSION)),/p:WindowsTargetPlatformVersion=$(UCRTVERSION))" \
 		&& msbuild.exe cppunit_dll.vcxproj /p:Configuration=$${PROFILEFLAGS}  \
