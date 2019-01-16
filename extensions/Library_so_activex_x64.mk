27f6a132fc1b8ee06fedc8d9603c52720f325ed4
himajin100000 <himajin100000@gmail.com>
2019-01-16 16:34:36 +0900

    Minimum Supported Version is VS2017
    Change-Id: Id771f1fe0d8c6702a52836f6229a944d259fed4c

--- extensions/Library_so_activex_x64.mk
+++ extensions/Library_so_activex_x64.mk
@@ -50,9 +50,7 @@ $(eval $(call gb_Library_use_system_win32_libs,so_activex_x64,\
 ))
 
 $(eval $(call gb_Library_add_libs,so_activex_x64,\
-	$(if $(filter 140,$(VCVER)),\
-		$(ATL_LIB)/amd64/atls.lib, \
-		$(subst /x86,/x64,$(ATL_LIB)/atls.lib)) \
-))
+	$(subst /x86,/x64,$(ATL_LIB)/atls.lib)) \
+)
 
 # vim:set noet sw=4 ts=4:
