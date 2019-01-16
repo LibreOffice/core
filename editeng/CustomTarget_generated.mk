d8106b6db488a4439f7bb781920c92a056b91ba6
Stephan Bergmann <sbergman@redhat.com>
2019-01-17 00:00:46 +0100

    Non-standard sed -i option is different on macOS than with GNU sed
    While with GNU sed the optional backup suffix is given directly as part of the
    -i[SUFFIX] (or --in-place[=SUFFIX]) argument, on macOS it must unconditionally
    be given as a (potentially empty) argument following the -i argument.  That
    means that on macOS the -e that happens to follow the -i in these sed
    invocations (and that happens to not be necessary to introduce the following
    script in these invocations) was mistaken as the backup suffix, causing creation
    of pointless workdir/CustomTarget/*/generated/tokens.cxx-e backup files.
    
    Change-Id: Icd865c76139afa348d03813eb83653afd7221a0e
    Reviewed-on: https://gerrit.libreoffice.org/66491
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

--- editeng/CustomTarget_generated.mk
+++ editeng/CustomTarget_generated.mk
@@ -19,8 +19,8 @@ $(editeng_INC)/tokens.hxx $(editeng_INC)/tokens.gperf : $(editeng_SRC)/tokens.tx
 
 $(editeng_INC)/tokens.cxx : $(editeng_INC)/tokens.gperf
 	$(GPERF) --compare-strncmp --readonly-tables --output-file=$(editeng_INC)/tokens.cxx $(editeng_INC)/tokens.gperf
-	sed -i -e "s/(char\*)0/(char\*)0, XML_TOKEN_INVALID/g" $(editeng_INC)/tokens.cxx
-	sed -i -e "/^#line/d" $(editeng_INC)/tokens.cxx
+	sed -i $(if $(filter MACOSX,$(OS)),'') -e "s/(char\*)0/(char\*)0, XML_TOKEN_INVALID/g" $(editeng_INC)/tokens.cxx
+	sed -i $(if $(filter MACOSX,$(OS)),'') -e "/^#line/d" $(editeng_INC)/tokens.cxx
 
 $(call gb_CustomTarget_get_target,editeng/generated) : $(editeng_INC)/tokens.cxx
 
