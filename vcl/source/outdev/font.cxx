a214f12c20ae76ddbe307554aae1873eb590306b
Matteo Casalin <matteo.casalin@yahoo.com>
2019-01-19 08:57:54 +0100

    Index is not needed for single getToken call
    Change-Id: I503e6e96a6a61b45e741f2131107ecaef8566b83
    Reviewed-on: https://gerrit.libreoffice.org/66640
    Tested-by: Jenkins
    Reviewed-by: Matteo Casalin <matteo.casalin@yahoo.com>

--- vcl/source/outdev/font.cxx
+++ vcl/source/outdev/font.cxx
@@ -878,8 +878,7 @@ vcl::Font OutputDevice::GetDefaultFont( DefaultFontType nType, LanguageType eLan
                 if( !pOutDev )
                 {
                     SAL_WARN_IF(!utl::ConfigManager::IsFuzzing(), "vcl.gdi", "No default window has been set for the application - we really shouldn't be able to get here");
-                    sal_Int32 nIndex = 0;
-                    aFont.SetFamilyName( aSearch.getToken( 0, ';', nIndex ) );
+                    aFont.SetFamilyName( aSearch.getToken( 0, ';' ) );
                 }
                 else
                 {
