c7c3b3105cffa047721f00b3ca56a1ef4ed8b8aa
Andras Timar <andras.timar@collabora.com>
2019-01-23 23:57:42 +0000

    don't build MariaDB Connector with MPLv2 subset
    Change-Id: I77159776e2f6625cdd23d1b11b36485325da2a3c

--- postprocess/Rdb_services.mk
+++ postprocess/Rdb_services.mk
@@ -252,7 +252,9 @@ $(eval $(call gb_Rdb_add_components,services,\
 		connectivity/source/drivers/firebird/firebird_sdbc \
 	) \
 	connectivity/source/drivers/flat/flat \
-	connectivity/source/drivers/mysqlc/mysqlc \
+	$(if $(filter $(ENABLE_MARIADBC),TRUE), \
+		connectivity/source/drivers/mysqlc/mysqlc \
+	) \
 	$(if $(filter MACOSX,$(OS)), \
 		connectivity/source/drivers/macab/macab1 \
 	) \
