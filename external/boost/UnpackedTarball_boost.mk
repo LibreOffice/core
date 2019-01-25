8c89ca3dd81c835a70dbf7d39372c611aeba4abc
Jan-Marek Glogowski <glogow@fbihome.de>
2019-01-25 21:56:41 +0100

    Just define the __clang_major___WORKAROUND_GUARD
    Change-Id: I84e969005ac2a96da449b5434ee5fdb37fc9b432

--- external/boost/UnpackedTarball_boost.mk
+++ external/boost/UnpackedTarball_boost.mk
@@ -40,8 +40,6 @@ boost_patches += msvc2017.patch.0
 
 boost_patches += sse.patch.0
 
-boost_patches += Wundef.patch.0
-
 $(eval $(call gb_UnpackedTarball_UnpackedTarball,boost))
 
 $(eval $(call gb_UnpackedTarball_set_tarball,boost,$(BOOST_TARBALL)))
