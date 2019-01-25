92e994cd879be51b7b46db7789f69c51523c6533
Jan Holesovsky <kendy@collabora.com>
2019-01-25 14:40:40 +0100

    sw: Ctrl + shape insertion was not setting up the layer.
    The consequence was that the shapes were inserted into the background
    layer.
    
    The problem was particularly visible with the LOK, but reproducible on
    the desktop too.
    
    Includes unit test.
    
    Change-Id: I16137331f944101e25679ec8c4a034bea658bc57

--- sw/source/core/frmedt/feshview.cxx
+++ sw/source/core/frmedt/feshview.cxx
@@ -3142,6 +3142,7 @@ long SwFEShell::GetSectionWidth( SwFormat const & rFormat ) const
             }
         }
         SdrPageView* pPageView = pDrawView->GetSdrPageView();
+        SdrCreateView::SetupObjLayer(pPageView, pDrawView->GetActiveLayer(), pObj);
         pDrawView->InsertObjectAtView(pObj, *pPageView);
     }
     ImpEndCreate();
