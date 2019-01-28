9b3493fec722b8cdd65dda22c52cc549ce46b308
Miklos Vajna <vmiklos@collabora.com>
2019-01-28 17:18:34 +0100

    tdf#122942 sw: update shape insert UI for the AddVerticalFrameOffsets option
    Regression from commit 50223ea6e212b60b7d33839c2753c5601fb50f95
    (tdf#98987 sw: add AddVerticalFrameOffsets compat mode, 2016-03-31),
    SwFEShell::ImpEndCreate() was not adapted to call
    SwTextFrame::GetBaseVertOffsetForFly() when determining the vertical
    position of the inserted shape.
    
    The call can be unconditional, the returned value is already 0 when the
    DocumentSettingId::ADD_VERTICAL_FLY_OFFSETS compat setting is false.
    
    Change-Id: Iec6af5a6d1ff3466e08377853cc8ca84f33a76d1

--- sw/qa/extras/uiwriter/uiwriter2.cxx
+++ sw/qa/extras/uiwriter/uiwriter2.cxx
@@ -29,6 +29,7 @@
 #include <sortedobjs.hxx>
 #include <anchoredobject.hxx>
 #include <swtypes.hxx>
+#include <fmtornt.hxx>
 
 namespace
 {
@@ -57,6 +58,7 @@ public:
     void testUnfloating();
     void testTdf122893();
     void testTdf122901();
+    void testTdf122942();
 
     CPPUNIT_TEST_SUITE(SwUiWriterTest2);
     CPPUNIT_TEST(testRedlineMoveInsertInDelete);
@@ -76,6 +78,7 @@ public:
     CPPUNIT_TEST(testUnfloating);
     CPPUNIT_TEST(testTdf122893);
     CPPUNIT_TEST(testTdf122901);
+    CPPUNIT_TEST(testTdf122942);
     CPPUNIT_TEST_SUITE_END();
 
 private:
@@ -787,6 +790,37 @@ void SwUiWriterTest2::testTdf122901()
     CPPUNIT_ASSERT_EQUAL(sal_Int32(101), getProperty<sal_Int32>(getParagraph(1), "ParaTopMargin"));
     CPPUNIT_ASSERT_EQUAL(sal_Int32(101),
                          getProperty<sal_Int32>(getParagraph(1), "ParaBottomMargin"));
+}
+
+void SwUiWriterTest2::testTdf122942()
+{
+    load(DATA_DIRECTORY, "tdf122942.odt");
+    SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument*>(mxComponent.get());
+    SwWrtShell* pWrtShell = pTextDoc->GetDocShell()->GetWrtShell();
+
+    // Do the moral equivalent of mouse button down, move and up.
+    // Start creating a custom shape that overlaps with the rounded rectangle
+    // already present in the document.
+    Point aStartPos(8000, 3000);
+    pWrtShell->BeginCreate(static_cast<sal_uInt16>(OBJ_CUSTOMSHAPE), aStartPos);
+
+    // Set its size.
+    Point aMovePos(10000, 5000);
+    pWrtShell->MoveCreate(aMovePos);
+
+    // Finish creation.
+    pWrtShell->EndCreate(SdrCreateCmd::ForceEnd);
+
+    // Make sure that the shape is inserted.
+    SwDoc* pDoc = pWrtShell->GetDoc();
+    const SwFrameFormats& rFormats = *pDoc->GetSpzFrameFormats();
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(2), rFormats.size());
+
+    // Without the accompanying fix in place, this test would have failed with
+    // 'Expected less than: 0; Actual  : 1030', i.e. the shape was below the
+    // paragraph mark, not above it.
+    const SwFormatVertOrient& rVert = rFormats[1]->GetVertOrient();
+    CPPUNIT_ASSERT_LESS(static_cast<SwTwips>(0), rVert.GetPos());
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(SwUiWriterTest2);
