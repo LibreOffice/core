87873a682f0d0c3173ac815385484fa9ea92b883
Michael Stahl <Michael.Stahl@cib.de>
2019-01-28 14:31:12 +0100

    libgpg-error: use custom soname and symbol version
    The hope is that this should allow both system's libgpg-error.so.0 and
    LO's bundled libgpg-error-lo.so.0 to be loaded by soffice.bin without
    unintended hilarity.
    
    Change-Id: I94498097a847b9756de86051798cb4ce022f6c83
    Reviewed-on: https://gerrit.libreoffice.org/67012
    Tested-by: Jenkins
    Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>

--- external/libgpg-error/ExternalPackage_libgpg-error.mk
+++ external/libgpg-error/ExternalPackage_libgpg-error.mk
@@ -15,7 +15,7 @@ ifneq ($(DISABLE_DYNLOADING),TRUE)
 
 ifeq ($(OS),LINUX)
 
-$(eval $(call gb_ExternalPackage_add_file,libgpg-error,$(LIBO_LIB_FOLDER)/libgpg-error.so.0,src/.libs/libgpg-error.so.0.22.0))
+$(eval $(call gb_ExternalPackage_add_file,libgpg-error,$(LIBO_LIB_FOLDER)/libgpg-error-lo.so.0,src/.libs/libgpg-error-lo.so.0.22.0))
 
 else ifeq ($(OS),MACOSX)
 
