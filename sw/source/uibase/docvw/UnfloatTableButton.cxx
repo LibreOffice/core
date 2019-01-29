20297a108191b60840b2f2e3fa0d2dab3ec5ea01
Tamás Zolnai <tamas.zolnai@collabora.com>
2019-01-29 19:05:56 +0100

    Unfloat: Fix some issues with unfloat operation
    - Use the correct method to remove the frame, so DOC
    export will work correctly.
    - Remove pagedesc attribute of the first text node if
    the table is moved before it to avoid to generate a page
    break.
    
    Change-Id: I02802c4d25c947ec140b846c594848117815f1ce

--- sw/source/uibase/docvw/UnfloatTableButton.cxx
+++ sw/source/uibase/docvw/UnfloatTableButton.cxx
@@ -11,7 +11,10 @@
 #include <HeaderFooterWin.hxx>
 
 #include <edtwin.hxx>
+#include <view.hxx>
+#include <wrtsh.hxx>
 #include <strings.hrc>
+#include <fmtpdsc.hxx>
 #include <vcl/metric.hxx>
 #include <vcl/settings.hxx>
 #include <viewopt.hxx>
@@ -19,10 +22,12 @@
 #include <flyfrm.hxx>
 #include <tabfrm.hxx>
 #include <txtfrm.hxx>
+#include <pagefrm.hxx>
 #include <ndindex.hxx>
 #include <swtable.hxx>
 #include <IDocumentState.hxx>
 #include <IDocumentUndoRedo.hxx>
+#include <IDocumentLayoutAccess.hxx>
 #include <drawinglayer/primitive2d/textprimitive2d.hxx>
 #include <drawinglayer/processor2d/baseprocessor2d.hxx>
 #include <drawinglayer/attribute/fontattribute.hxx>
@@ -105,12 +110,28 @@ void UnfloatTableButton::MouseButtonDown(const MouseEvent& /*rMEvt*/)
 
     SwDoc& rDoc = pTextFrame->GetDoc();
 
+    // When we move the table before the first text node, we need to clear RES_PAGEDESC attribute
+    // of the text node otherwise LO will create a page break after the table
+    if (pTextFrame->GetTextNodeFirst())
+    {
+        SfxItemSet aSet(GetEditWin()->GetView().GetWrtShell().GetAttrPool(),
+                        svl::Items<RES_PAGEDESC, RES_PAGEDESC>{});
+        aSet.Put(SwFormatPageDesc(nullptr));
+        SwPaM aPaM(*pTextFrame->GetTextNodeFirst());
+        rDoc.getIDocumentContentOperations().InsertItemSet(aPaM, aSet, SetAttrMode::DEFAULT,
+                                                           GetPageFrame()->getRootFrame());
+    }
+
     // Move the table outside of the text frame
     SwNodeRange aRange(*pTableNode, 0, *pTableNode->EndOfSectionNode(), 1);
     rDoc.getIDocumentContentOperations().MoveNodeRange(aRange, aInsertPos, SwMoveFlags::DEFAULT);
 
     // Remove the floating table's frame
-    SwFrame::DestroyFrame(pFlyFrame);
+    SwFlyFrameFormat* pFrameFormat = pFlyFrame->GetFormat();
+    if (pFrameFormat)
+    {
+        rDoc.getIDocumentLayoutAccess().DelLayoutFormat(pFrameFormat);
+    }
 
     rDoc.getIDocumentState().SetModified();
 
