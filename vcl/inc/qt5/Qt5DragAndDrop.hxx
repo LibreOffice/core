fc67d2000631bb83812fecbf99e4179ccdac1487
Stephan Bergmann <sbergman@redhat.com>
2019-01-30 13:27:33 +0100

    loplugin:includeform
    Change-Id: I8ac8b1cbbf94f405ffef0037b6957b35d44a8f91

--- vcl/inc/qt5/Qt5DragAndDrop.hxx
+++ vcl/inc/qt5/Qt5DragAndDrop.hxx
@@ -11,7 +11,7 @@
 #pragma once
 
 #include <cppuhelper/compbase.hxx>
-#include <Qt5Clipboard.hxx>
+#include "Qt5Clipboard.hxx"
 
 #include <com/sun/star/datatransfer/dnd/XDragSource.hpp>
 #include <com/sun/star/datatransfer/dnd/XDropTarget.hpp>
