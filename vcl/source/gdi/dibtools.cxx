55e95ffa19dc68f8a6923af831e44088d07dade3
Caolán McNamara <caolanm@redhat.com>
2019-01-30 13:36:01 +0000

    Resolves: tdf#122958 bmps with weird compression values that work in mso
    Change-Id: Ie1887288cba7c1d56b807dbc9ddb886b9d20ff33

--- vcl/source/gdi/dibtools.cxx
+++ vcl/source/gdi/dibtools.cxx
@@ -937,6 +937,18 @@ bool ImplReadDIBBody(SvStream& rIStm, Bitmap& rBmp, AlphaMask* pBmpAlpha, sal_uL
                 return false;
             break;
         }
+        default:
+            // tdf#122958 invalid compression value used
+            if (aHeader.nCompression & 0x000F)
+            {
+                // lets assume that there was an error in the generating application
+                // and allow through as COMPRESS_NONE if the bottom byte is 0
+                SAL_WARN( "vcl", "bad bmp compression scheme: " << aHeader.nCompression << ", rejecting bmp");
+                return false;
+            }
+            else
+                SAL_WARN( "vcl", "bad bmp compression scheme: " << aHeader.nCompression << ", assuming meant to be COMPRESS_NONE");
+        [[fallthrough]];
         case BITFIELDS:
             break;
         case ZCOMPRESS:
@@ -950,8 +962,6 @@ bool ImplReadDIBBody(SvStream& rIStm, Bitmap& rBmp, AlphaMask* pBmpAlpha, sal_uL
                 return false;
             break;
         }
-        default:
-            return false;
     }
 
     const Size aSizePixel(aHeader.nWidth, aHeader.nHeight);
