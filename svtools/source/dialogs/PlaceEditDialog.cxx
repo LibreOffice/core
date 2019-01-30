4172b3b6a137d5b36f7d2fc19509d1ca55a1caf8
Aditya <adityasahu1511@gmail.com>
2019-01-31 03:50:55 +0530

    tdf#119812: Hide "Windows Share" server type in Windows OS.
    The "Windows Share" server type is non-functional in Windows OS but
    still shown in the File Services (PlaceEditDialog) dialog.
    This patch removes its entry from dialog on Windows only so that no more 
    non-functional service can be created.
    
    Change-Id: I440c637dd2a566f820a6e8fac826f35cabb9c05f

--- svtools/source/dialogs/PlaceEditDialog.cxx
+++ svtools/source/dialogs/PlaceEditDialog.cxx
@@ -223,10 +223,17 @@ void PlaceEditDialog::InitDetails( )
     xSshDetails->setChangeHdl( LINK( this, PlaceEditDialog, EditHdl ) );
     m_aDetailsContainers.push_back(xSshDetails);
 
+    // Remove Windows Share entry from dialog on Windows OS, where it's non-functional
+#if defined(_WIN32)
+    // nPos is the position of first item that is pre-defined in svtools/uiconfig/ui/placeedit.ui,
+    // after other CMIS types were inserted
+    m_xLBServerType->remove(nPos + 3);
+#else
     // Create Windows Share control
     std::shared_ptr<DetailsContainer> xSmbDetails(std::make_shared<SmbDetailsContainer>(this));
     xSmbDetails->setChangeHdl( LINK( this, PlaceEditDialog, EditHdl ) );
     m_aDetailsContainers.push_back(xSmbDetails);
+#endif
 
     // Set default to first value
     m_xLBServerType->set_active(0);
