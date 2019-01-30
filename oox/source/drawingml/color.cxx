f4999d0e3eae63f9b00c7e809f55a99fe596e8e2
Miklos Vajna <vmiklos@collabora.com>
2019-01-30 17:42:39 +0100

    Related: tdf#94238 PPTX import: handle subset of radial gradient fill
    Handle the case when the horizontal center is at 50%. Other cases are
    still unhandled, those are more complex.
    
    After fixing the style, center and border, the gradient fill looks
    similar to how PowerPoint renders it.
    
    Change-Id: I419da70482de37031aa2c7fc735692019d7665f5
    Reviewed-on: https://gerrit.libreoffice.org/67175
    Reviewed-by: Jan Holesovsky <kendy@collabora.com>
    Tested-by: Jan Holesovsky <kendy@collabora.com>

--- oox/source/drawingml/color.cxx
+++ oox/source/drawingml/color.cxx
@@ -425,6 +425,14 @@ sal_Int32 Color::getColorTransformationToken( const OUString& sName )
     return XML_TOKEN_INVALID;
 }
 
+bool Color::equals(const Color& rOther, const GraphicHelper& rGraphicHelper, sal_Int32 nPhClr) const
+{
+    if (getColor(rGraphicHelper, nPhClr) != rOther.getColor(rGraphicHelper, nPhClr))
+        return false;
+
+    return getTransparency() == rOther.getTransparency();
+}
+
 void Color::clearTransparence()
 {
     mnAlpha = MAX_PERCENT;
