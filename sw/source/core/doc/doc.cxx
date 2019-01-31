3ad845075c1eb1c5dda55434f85b1eba1dcd5182
Szymon Kłos <szymon.klos@collabora.com>
2019-01-31 11:32:27 +0100

    tdf#122198 don't remove db after close
    Change-Id: Ib652174148e6997c1aabe0251a62612f28243d6e
    Reviewed-on: https://gerrit.libreoffice.org/67200
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>
    Tested-by: Szymon Kłos <szymon.klos@collabora.com>

--- sw/source/core/doc/doc.cxx
+++ sw/source/core/doc/doc.cxx
@@ -489,6 +489,8 @@ void SwDoc::ChgDBData(const SwDBData& rNewData)
     {
         maDBData = rNewData;
         getIDocumentState().SetModified();
+        if (m_pDBManager)
+            m_pDBManager->CommitLastRegistrations();
     }
     getIDocumentFieldsAccess().GetSysFieldType(SwFieldIds::DatabaseName)->UpdateFields();
 }
