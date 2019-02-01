c9b560be86e122f54892b4ea26360c4dd86df103
Noel Grandin <noel.grandin@collabora.co.uk>
2019-02-01 15:15:16 +0100

    tdf#50916 Makes numbers of columns dynamic.
    With this commit we are making numbers of columns
    dynamic, but the number of maximum supported
    columns will be the same (1024).
    Such approach will allow us to check issues
    (eg. performance, LO format etc.), and improve it.
    
    Increasing number of maximum columns, will be done
    in separate commit.
    
    Change-Id: Ibac4101e9ffc05e3548eca1c198f6319ac7ff9aa

--- sc/source/core/data/table5.cxx
+++ sc/source/core/data/table5.cxx
@@ -1082,7 +1082,7 @@ void ScTable::StartListening( const ScAddress& rAddress, SvtListener* pListener 
     if (!ValidCol(rAddress.Col()))
         return;
 
-    aCol[rAddress.Col()].StartListening( *pListener, rAddress.Row() );
+    CreateColumnIfNotExists(rAddress.Col()).StartListening( *pListener, rAddress.Row() );
 }
 
 void ScTable::EndListening( const ScAddress& rAddress, SvtListener* pListener )
@@ -1090,7 +1090,8 @@ void ScTable::EndListening( const ScAddress& rAddress, SvtListener* pListener )
     if (!ValidCol(rAddress.Col()))
         return;
 
-    aCol[rAddress.Col()].EndListening( *pListener, rAddress.Row() );
+    if (rAddress.Col() < aCol.size())
+        aCol[rAddress.Col()].EndListening( *pListener, rAddress.Row() );
 }
 
 void ScTable::StartListening( sc::StartListeningContext& rCxt, const ScAddress& rAddress, SvtListener& rListener )
