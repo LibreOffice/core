8f92f7fdc40e832f315e28c2e7c167a60311eaef
Noel Grandin <noel.grandin@collabora.co.uk>
2019-02-05 12:24:35 +0200

    simplify SfxEventNamesList
    no need to store elements of vector on the heap
    
    Change-Id: I314372b8f81b117c07676263c0c3481b51374f33

--- include/sfx2/evntconf.hxx
+++ include/sfx2/evntconf.hxx
@@ -35,9 +35,9 @@ class SvxMacroTableDtor;
 
 struct SFX2_DLLPUBLIC SfxEventName
 {
-    SvMacroItemId const mnId;
-    OUString const      maEventName;
-    OUString const      maUIName;
+    SvMacroItemId mnId;
+    OUString      maEventName;
+    OUString      maUIName;
 
             SfxEventName( SvMacroItemId nId,
                              const OUString& rEventName,
@@ -50,7 +50,7 @@ struct SFX2_DLLPUBLIC SfxEventName
 class SFX2_DLLPUBLIC SfxEventNamesList
 {
 private:
-    ::std::vector< std::unique_ptr<SfxEventName> > aEventNamesList;
+    ::std::vector< SfxEventName > aEventNamesList;
 
 public:
     SfxEventNamesList() {}
@@ -60,10 +60,10 @@ public:
 
     size_t size() const { return aEventNamesList.size(); };
 
-    SfxEventName* at( size_t Index ) const
-        { return Index < aEventNamesList.size() ? aEventNamesList[ Index ].get() : nullptr; }
+    SfxEventName& at( size_t Index ) { return aEventNamesList[ Index ]; }
+    SfxEventName const & at( size_t Index ) const { return aEventNamesList[ Index ]; }
 
-    void push_back( std::unique_ptr<SfxEventName> Item ) { aEventNamesList.push_back( std::move(Item) ); }
+    void push_back( SfxEventName Item ) { aEventNamesList.push_back( std::move(Item) ); }
 };
 
 class SFX2_DLLPUBLIC SfxEventNamesItem : public SfxPoolItem
