4fa0ce3a4cbd161537b10ff66aad272e531a6fb6
Noel Grandin <noelgrandin@gmail.com>
2019-02-05 20:27:56 +0200

    hidpi cursors for macOS
    Change-Id: I39bdc93048733627a54c4f7b4b2e7df4f073ef25

---
+++ vcl/inc/cursor_hotspots.hxx
@@ -0,0 +1,169 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef INCLUDED_VCL_INC_CURSOR_HOTSPOTS_HXX
+#define INCLUDED_VCL_INC_CURSOR_HOTSPOTS_HXX
+
+#define help_curs_x_hot 0
+#define help_curs_y_hot 0
+#define pen_curs_x_hot 3
+#define pen_curs_y_hot 27
+#define nodrop_curs_x_hot 9
+#define nodrop_curs_y_hot 9
+#define magnify_curs_x_hot 12
+#define magnify_curs_y_hot 13
+#define rotate_curs_x_hot 15
+#define rotate_curs_y_hot 15
+#define hshear_curs_x_hot 15
+#define hshear_curs_y_hot 15
+#define vshear_curs_x_hot 15
+#define vshear_curs_y_hot 15
+#define drawline_curs_x_hot 7
+#define drawline_curs_y_hot 7
+#define drawrect_curs_x_hot 7
+#define drawrect_curs_y_hot 7
+#define drawpolygon_curs_x_hot 7
+#define drawpolygon_curs_y_hot 7
+#define drawbezier_curs_x_hot 7
+#define drawbezier_curs_y_hot 7
+#define drawarc_curs_x_hot 7
+#define drawarc_curs_y_hot 7
+#define drawpie_curs_x_hot 7
+#define drawpie_curs_y_hot 7
+#define drawcirclecut_curs_x_hot 7
+#define drawcirclecut_curs_y_hot 7
+#define drawellipse_curs_x_hot 7
+#define drawellipse_curs_y_hot 7
+#define drawconnect_curs_x_hot 7
+#define drawconnect_curs_y_hot 7
+#define drawtext_curs_x_hot 8
+#define drawtext_curs_y_hot 8
+#define mirror_curs_x_hot 14
+#define mirror_curs_y_hot 12
+#define crook_curs_x_hot 15
+#define crook_curs_y_hot 14
+#define crop_curs_x_hot 9
+#define crop_curs_y_hot 9
+#define movepoint_curs_x_hot 0
+#define movepoint_curs_y_hot 0
+#define movebezierweight_curs_x_hot 0
+#define movebezierweight_curs_y_hot 0
+#define drawfreehand_curs_x_hot 8
+#define drawfreehand_curs_y_hot 8
+#define drawcaption_curs_x_hot 8
+#define drawcaption_curs_y_hot 8
+#define movedata_curs_x_hot 1
+#define movedata_curs_y_hot 1
+#define copydata_curs_x_hot 1
+#define copydata_curs_y_hot 1
+#define linkdata_curs_x_hot 1
+#define linkdata_curs_y_hot 1
+#define movedlnk_curs_x_hot 1
+#define movedlnk_curs_y_hot 1
+#define copydlnk_curs_x_hot 1
+#define copydlnk_curs_y_hot 1
+#define movefile_curs_x_hot 9
+#define movefile_curs_y_hot 9
+#define copyfile_curs_x_hot 9
+#define copyfile_curs_y_hot 9
+#define linkfile_curs_x_hot 9
+#define linkfile_curs_y_hot 9
+#define moveflnk_curs_x_hot 9
+#define moveflnk_curs_y_hot 9
+#define copyflnk_curs_x_hot 9
+#define copyflnk_curs_y_hot 9
+#define movefiles_curs_x_hot 8
+#define movefiles_curs_y_hot 9
+#define copyfiles_curs_x_hot 8
+#define copyfiles_curs_y_hot 9
+
+#define chart_curs_x_hot 15
+#define chart_curs_y_hot 16
+#define detective_curs_x_hot 12
+#define detective_curs_y_hot 13
+#define pivotcol_curs_x_hot 7
+#define pivotcol_curs_y_hot 5
+#define pivotfld_curs_x_hot 8
+#define pivotfld_curs_y_hot 7
+#define pivotrow_curs_x_hot 8
+#define pivotrow_curs_y_hot 7
+#define pivotdel_curs_x_hot 9
+#define pivotdel_curs_y_hot 8
+
+#define chain_curs_x_hot 0
+#define chain_curs_y_hot 2
+#define chainnot_curs_x_hot 2
+#define chainnot_curs_y_hot 2
+
+#define ase_curs_x_hot 19
+#define ase_curs_y_hot 16
+#define asn_curs_x_hot 16
+#define asn_curs_y_hot 12
+#define asne_curs_x_hot 21
+#define asne_curs_y_hot 10
+#define asns_curs_x_hot 15
+#define asns_curs_y_hot 15
+#define asnswe_curs_x_hot 15
+#define asnswe_curs_y_hot 15
+#define asnw_curs_x_hot 10
+#define asnw_curs_y_hot 10
+#define ass_curs_x_hot 15
+#define ass_curs_y_hot 19
+#define asse_curs_x_hot 21
+#define asse_curs_y_hot 21
+#define assw_curs_x_hot 10
+#define assw_curs_y_hot 21
+#define asw_curs_x_hot 12
+#define asw_curs_y_hot 15
+#define aswe_curs_x_hot 15
+#define aswe_curs_y_hot 15
+#define nullcurs_x_hot 2
+#define nullcurs_y_hot 2
+
+#define fill_curs_x_hot 10
+#define fill_curs_y_hot 22
+#define vertcurs_curs_x_hot 8
+#define vertcurs_curs_y_hot 8
+#define tblsele_curs_x_hot 14
+#define tblsele_curs_y_hot 8
+#define tblsels_curs_x_hot 7
+#define tblsels_curs_y_hot 14
+#define tblselse_curs_x_hot 14
+#define tblselse_curs_y_hot 14
+#define tblselw_curs_x_hot 1
+#define tblselw_curs_y_hot 8
+#define tblselsw_curs_x_hot 1
+#define tblselsw_curs_y_hot 14
+#define hidewhitespace_curs_x_hot 0
+#define hidewhitespace_curs_y_hot 10
+#define showwhitespace_curs_x_hot 0
+#define showwhitespace_curs_y_hot 10
+
+#define wait_curs_x_hot 10
+#define wait_curs_y_hot 10
+#define nwsize_curs_x_hot 10
+#define nwsize_curs_y_hot 10
+#define nesize_curs_x_hot 10
+#define nesize_curs_y_hot 10
+#define swsize_curs_x_hot 10
+#define swsize_curs_y_hot 10
+#define sesize_curs_x_hot 10
+#define sesize_curs_y_hot 10
+#define window_nwsize_curs_x_hot 10
+#define window_nwsize_curs_y_hot 10
+#define window_nesize_curs_x_hot 10
+#define window_nesize_curs_y_hot 10
+#define window_swsize_curs_x_hot 10
+#define window_swsize_curs_y_hot 10
+#define window_sesize_curs_x_hot 10
+#define window_sesize_curs_y_hot 10
+
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
--- vcl/unx/gtk3/gtk3gtkdata.cxx
+++ vcl/unx/gtk3/gtk3gtkdata.cxx
@@ -38,6 +38,7 @@
 #include <unx/salobj.h>
 #include <unx/geninst.h>
 #include <bitmaps.hlst>
+#include <cursor_hotspots.hxx>
 #include <osl/thread.h>
 #include <osl/process.h>
 
@@ -57,136 +58,6 @@
 #include <chrono>
 
 
-#define nodrop_curs_x_hot 9
-#define nodrop_curs_y_hot 9
-#define magnify_curs_x_hot 12
-#define magnify_curs_y_hot 13
-#define rotate_curs_x_hot 15
-#define rotate_curs_y_hot 15
-#define hshear_curs_x_hot 15
-#define hshear_curs_y_hot 15
-#define vshear_curs_x_hot 15
-#define vshear_curs_y_hot 15
-#define drawline_curs_x_hot 7
-#define drawline_curs_y_hot 7
-#define drawrect_curs_x_hot 7
-#define drawrect_curs_y_hot 7
-#define drawpolygon_curs_x_hot 7
-#define drawpolygon_curs_y_hot 7
-#define drawbezier_curs_x_hot 7
-#define drawbezier_curs_y_hot 7
-#define drawarc_curs_x_hot 7
-#define drawarc_curs_y_hot 7
-#define drawpie_curs_x_hot 7
-#define drawpie_curs_y_hot 7
-#define drawcirclecut_curs_x_hot 7
-#define drawcirclecut_curs_y_hot 7
-#define drawellipse_curs_x_hot 7
-#define drawellipse_curs_y_hot 7
-#define drawconnect_curs_x_hot 7
-#define drawconnect_curs_y_hot 7
-#define drawtext_curs_x_hot 8
-#define drawtext_curs_y_hot 8
-#define mirror_curs_x_hot 14
-#define mirror_curs_y_hot 12
-#define crook_curs_x_hot 15
-#define crook_curs_y_hot 14
-#define crop_curs_x_hot 9
-#define crop_curs_y_hot 9
-#define movepoint_curs_x_hot 0
-#define movepoint_curs_y_hot 0
-#define movebezierweight_curs_x_hot 0
-#define movebezierweight_curs_y_hot 0
-#define drawfreehand_curs_x_hot 8
-#define drawfreehand_curs_y_hot 8
-#define drawcaption_curs_x_hot 8
-#define drawcaption_curs_y_hot 8
-#define movedata_curs_x_hot 1
-#define movedata_curs_y_hot 1
-#define copydata_curs_x_hot 1
-#define copydata_curs_y_hot 1
-#define linkdata_curs_x_hot 1
-#define linkdata_curs_y_hot 1
-#define movedlnk_curs_x_hot 1
-#define movedlnk_curs_y_hot 1
-#define copydlnk_curs_x_hot 1
-#define copydlnk_curs_y_hot 1
-#define movefile_curs_x_hot 9
-#define movefile_curs_y_hot 9
-#define copyfile_curs_x_hot 9
-#define copyfile_curs_y_hot 9
-#define linkfile_curs_x_hot 9
-#define linkfile_curs_y_hot 9
-#define moveflnk_curs_x_hot 9
-#define moveflnk_curs_y_hot 9
-#define copyflnk_curs_x_hot 9
-#define copyflnk_curs_y_hot 9
-#define movefiles_curs_x_hot 8
-#define movefiles_curs_y_hot 9
-#define copyfiles_curs_x_hot 8
-#define copyfiles_curs_y_hot 9
-
-#define chart_curs_x_hot 15
-#define chart_curs_y_hot 16
-#define detective_curs_x_hot 12
-#define detective_curs_y_hot 13
-#define pivotcol_curs_x_hot 7
-#define pivotcol_curs_y_hot 5
-#define pivotfld_curs_x_hot 8
-#define pivotfld_curs_y_hot 7
-#define pivotrow_curs_x_hot 8
-#define pivotrow_curs_y_hot 7
-#define pivotdel_curs_x_hot 9
-#define pivotdel_curs_y_hot 8
-
-#define chain_curs_x_hot 0
-#define chain_curs_y_hot 2
-#define chainnot_curs_x_hot 2
-#define chainnot_curs_y_hot 2
-
-#define ase_curs_x_hot 19
-#define ase_curs_y_hot 16
-#define asn_curs_x_hot 16
-#define asn_curs_y_hot 12
-#define asne_curs_x_hot 21
-#define asne_curs_y_hot 10
-#define asns_curs_x_hot 15
-#define asns_curs_y_hot 15
-#define asnswe_curs_x_hot 15
-#define asnswe_curs_y_hot 15
-#define asnw_curs_x_hot 10
-#define asnw_curs_y_hot 10
-#define ass_curs_x_hot 15
-#define ass_curs_y_hot 19
-#define asse_curs_x_hot 21
-#define asse_curs_y_hot 21
-#define assw_curs_x_hot 10
-#define assw_curs_y_hot 21
-#define asw_curs_x_hot 12
-#define asw_curs_y_hot 15
-#define aswe_curs_x_hot 15
-#define aswe_curs_y_hot 15
-#define nullcurs_x_hot 2
-#define nullcurs_y_hot 2
-
-#define fill_curs_x_hot 10
-#define fill_curs_y_hot 22
-#define vertcurs_curs_x_hot 8
-#define vertcurs_curs_y_hot 8
-#define tblsele_curs_x_hot 14
-#define tblsele_curs_y_hot 8
-#define tblsels_curs_x_hot 7
-#define tblsels_curs_y_hot 14
-#define tblselse_curs_x_hot 14
-#define tblselse_curs_y_hot 14
-#define tblselw_curs_x_hot 1
-#define tblselw_curs_y_hot 8
-#define tblselsw_curs_x_hot 1
-#define tblselsw_curs_y_hot 14
-#define hidewhitespace_curs_x_hot 0
-#define hidewhitespace_curs_y_hot 10
-#define showwhitespace_curs_x_hot 0
-#define showwhitespace_curs_y_hot 10
 
 using namespace vcl_sal;
 
