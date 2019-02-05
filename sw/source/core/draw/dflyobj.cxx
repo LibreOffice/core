3acdcfba60874bd88428ae5b744b14b51637d6a3
Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
2019-02-05 15:52:35 +0100

    tdf#122607 Fix layout invalidation in headless mode
    When converting pdf, textbox got out of place.
    Frame was not invalidated since visible area was 0 which led to
    InvalidateWindows not being called.
    
    This also reintroduces the the CalcLayout() call removed in
    1ecca673b40fedc53db125e332b087d1c120a254.
    That commit turned out to fix the problem only in some cases.
    
    Change-Id: I3f620b2f2db2c4a6e5bf279b33e5c93697e4e2d4

--- sw/source/core/draw/dflyobj.cxx
+++ sw/source/core/draw/dflyobj.cxx
@@ -514,7 +514,7 @@ void SwVirtFlyDrawObj::wrap_DoPaintObject(
             // if there's no viewport set, all fly-frames will be painted,
             // which is slow, wastes memory, and can cause other trouble.
             (void) rViewInformation; // suppress "unused parameter" warning
-            assert(comphelper::LibreOfficeKit::isActive() || !rViewInformation.getViewport().isEmpty());
+            assert(Application::IsHeadlessModeEnabled() || !rViewInformation.getViewport().isEmpty());
             if ( !m_pFlyFrame->IsFlyInContentFrame() )
             {
                 // it is also necessary to restore the VCL MapMode from ViewInformation since e.g.
