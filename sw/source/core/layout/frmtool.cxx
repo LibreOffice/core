e5a979d30aca1721981d0292e5ebfce1022b173b
Caolán McNamara <caolanm@redhat.com>
2019-02-06 12:34:26 +0000

    Resolves: tdf#123163 avoid null deref
    one RemoveDrawObjFromPage is already null-deref protected,
    do the same for the other
    
    Change-Id: Ica5bb2df4a8dbffd8397df76b673adab75bb7aef

--- sw/source/core/layout/frmtool.cxx
+++ sw/source/core/layout/frmtool.cxx
@@ -2406,7 +2406,8 @@ static void lcl_RemoveObjsFromPage( SwFrame* _pFrame )
         {
             if (pObj->GetFrameFormat().GetAnchor().GetAnchorId() != RndStdIds::FLY_AS_CHAR)
             {
-                pObj->GetPageFrame()->RemoveDrawObjFromPage(
+                if (SwPageFrame *pPg = pObj->GetPageFrame())
+                    pPg->RemoveDrawObjFromPage(
                                 *static_cast<SwAnchoredDrawObject*>(pObj) );
             }
         }
@@ -2747,8 +2748,8 @@ static void lcl_Regist( SwPageFrame *pPage, const SwFrame *pAnch )
             if ( pPage != pObj->GetPageFrame() )
             {
                 // #i28701#
-                if ( pObj->GetPageFrame() )
-                    pObj->GetPageFrame()->RemoveDrawObjFromPage( *pObj );
+                if (SwPageFrame *pPg = pObj->GetPageFrame())
+                    pPg->RemoveDrawObjFromPage( *pObj );
                 pPage->AppendDrawObjToPage( *pObj );
             }
         }
