b63ce6236c8e56bd0bbdb3ff6315a88929b86418
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2019-02-08 21:30:39 +0100

    add PngImageFilter that uses libpng for PNG loading
    This adds loading of PNG images that uses libpng instead of our
    own solution. It always loaded the image as either RGB or RGBA
    image and if the source PNG is using something else, libpng
    converts to either RGB or RGBA.
    
    In addition this adds tests for loading of various PNG files to
    make sure the resulting bitmaps are using pixel data as expected.
    (especially needed to check the RGBA bitmaps)
    
    Change-Id: I194321caf76c2ec2365bb6075c79c5e84983658a

---
+++ vcl/inc/filter/PngImageReader.hxx
@@ -0,0 +1,39 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#include <vcl/graph.hxx>
+#include <vcl/dllapi.h>
+#include <com/sun/star/task/XStatusIndicator.hpp>
+#include <tools/stream.hxx>
+#include <vcl/bitmapex.hxx>
+
+#ifndef INCLUDED_VCL_SOURCE_FILTER_PNG_PNGIMAGEREADER_HXX
+#define INCLUDED_VCL_SOURCE_FILTER_PNG_PNGIMAGEREADER_HXX
+
+namespace vcl
+{
+class VCL_DLLPUBLIC PngImageReader
+{
+    SvStream& mrStream;
+    css::uno::Reference<css::task::XStatusIndicator> mxStatusIndicator;
+
+public:
+    PngImageReader(SvStream& rStream);
+
+    virtual ~PngImageReader() {}
+
+    bool read(BitmapEx& rBitmap);
+};
+
+} // namespace vcl
+
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
