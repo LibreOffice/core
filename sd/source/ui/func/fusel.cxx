8d4bb1ea2a1bd9e9e877146fd18bf6533d4437dd
Matteo Casalin <matteo.casalin@yahoo.com>
2019-02-10 16:35:04 +0100

    Use indexed getToken()
    Change-Id: I0f4e864ed27a5a9e071489c74f72859118627b08

--- sd/source/ui/func/fusel.cxx
+++ sd/source/ui/func/fusel.cxx
@@ -1418,8 +1418,9 @@ bool FuSelection::AnimateObj(SdrObject* pObj, const Point& rPos)
                         // aMacro has got following format:
                         // "Macroname.Modulname.Libname.Documentname" or
                         // "Macroname.Modulname.Libname.Applicationname"
-                        OUString aMacroName = aMacro.getToken(0, '.');
-                        OUString aModulName = aMacro.getToken(1, '.');
+                        sal_Int32 nIdx{ 0 };
+                        const OUString aMacroName = aMacro.getToken(0, '.', nIdx);
+                        const OUString aModulName = aMacro.getToken(0, '.', nIdx);
 
                         // In this moment the Call-method only
                         // resolves modulename+macroname
