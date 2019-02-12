af053aa10a25381ca98b79134e98047e0bb36ed6
Matthias Seidel <mseidel@apache.org>
2019-02-12 10:40:56 +0000

    Removed whitespace, added vim modeline

--- officecfg/registry/data/org/openoffice/Office/UI/makefile.mk
+++ officecfg/registry/data/org/openoffice/Office/UI/makefile.mk
@@ -1,5 +1,5 @@
 #**************************************************************
-#  
+#
 #  Licensed to the Apache Software Foundation (ASF) under one
 #  or more contributor license agreements.  See the NOTICE file
 #  distributed with this work for additional information
@@ -7,16 +7,16 @@
 #  to you under the Apache License, Version 2.0 (the
 #  "License"); you may not use this file except in compliance
 #  with the License.  You may obtain a copy of the License at
-#  
+#
 #    http://www.apache.org/licenses/LICENSE-2.0
-#  
+#
 #  Unless required by applicable law or agreed to in writing,
 #  software distributed under the License is distributed on an
 #  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 #  KIND, either express or implied.  See the License for the
 #  specific language governing permissions and limitations
 #  under the License.
-#  
+#
 #**************************************************************
 
 
@@ -26,11 +26,10 @@ PRJNAME=officecfg
 TARGET=data_ooOUI
 PACKAGE=org.openoffice.Office.UI
 
-.INCLUDE :  settings.mk
-.INCLUDE :  $(PRJ)$/util$/makefile.pmk
+.INCLUDE : settings.mk
+.INCLUDE : $(PRJ)$/util$/makefile.pmk
 
 # --- Targets ------------------------------------------------------
-
 
 LOCALIZEDFILES= \
     BasicIDECommands.xcu \
@@ -72,5 +71,6 @@ XCUFILES= \
     Factories.xcu \
     GlobalSettings.xcu
 
-.INCLUDE :  target.mk
+.INCLUDE : target.mk
 
+# vim: set noet ts=4 sw=4:
