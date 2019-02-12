2c023de589f9caf2e2a731e484e100e0cc974143
Muzaffer Kadir YILMAZ <muzafferkadiryilmaz@gmail.com>
2019-02-12 15:41:28 +0300

    Convert variables use Tell() function sal_uLong to sal_uInt64
    Change-Id: I0eb235703df5a227158ee9ea9dc3a76534d98349

--- sc/source/core/tool/rechead.cxx
+++ sc/source/core/tool/rechead.cxx
@@ -27,7 +27,7 @@ ScMultipleReadHeader::ScMultipleReadHeader(SvStream& rNewStream) :
 {
     sal_uInt32 nDataSize;
     rStream.ReadUInt32( nDataSize );
-    sal_uLong nDataPos = rStream.Tell();
+    sal_uInt64 nDataPos = rStream.Tell();
     nTotalEnd = nDataPos + nDataSize;
     nEntryEnd = nTotalEnd;
 
@@ -72,7 +72,7 @@ ScMultipleReadHeader::~ScMultipleReadHeader()
 
 void ScMultipleReadHeader::EndEntry()
 {
-    sal_uLong nPos = rStream.Tell();
+    sal_uInt64 nPos = rStream.Tell();
     OSL_ENSURE( nPos <= nEntryEnd, "read too much" );
     if ( nPos != nEntryEnd )
     {
@@ -86,7 +86,7 @@ void ScMultipleReadHeader::EndEntry()
 
 void ScMultipleReadHeader::StartEntry()
 {
-    sal_uLong nPos = rStream.Tell();
+    sal_uInt64 nPos = rStream.Tell();
     sal_uInt32 nEntrySize;
     (*pMemStream).ReadUInt32( nEntrySize );
 
@@ -117,7 +117,7 @@ ScMultipleWriteHeader::ScMultipleWriteHeader(SvStream& rNewStream) :
 
 ScMultipleWriteHeader::~ScMultipleWriteHeader()
 {
-    sal_uLong nDataEnd = rStream.Tell();
+    sal_Int64 nDataEnd = rStream.Tell();
 
     rStream.WriteUInt16( SCID_SIZES );
     rStream.WriteUInt32( aMemStream.Tell() );
@@ -126,7 +126,7 @@ ScMultipleWriteHeader::~ScMultipleWriteHeader()
     if ( nDataEnd - nDataPos != nDataSize )                 // matched default ?
     {
         nDataSize = nDataEnd - nDataPos;
-        sal_uLong nPos = rStream.Tell();
+        sal_uInt64 nPos = rStream.Tell();
         rStream.Seek(nDataPos-sizeof(sal_uInt32));
         rStream.WriteUInt32( nDataSize );                               // record size at the beginning
         rStream.Seek(nPos);
