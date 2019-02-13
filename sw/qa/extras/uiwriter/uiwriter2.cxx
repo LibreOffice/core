8843b3a75c326652f13fd67bd78647e3a07c7f95
Patrick Jaap <patrick.jaap@tu-dresden.de>
2019-02-13 14:58:29 +0100

    Fix: Use correct relative table width in docx export
    It removes the HACK where the doc model was changed.
    We introduce a pointer to a sorrounding frame: an indicator
    telling us that the table was placed in a frame to make it
    floating.
    
    During export, remove the frame and use the relative width of the
    surrounding frame.
    
    Change-Id: I43b45d9a9e67ee755782c9f18df22ae1d4014689

--- sw/qa/extras/uiwriter/uiwriter2.cxx
+++ sw/qa/extras/uiwriter/uiwriter2.cxx
@@ -61,6 +61,7 @@ public:
     void testTdf122942();
     void testTdf52391();
     void testTdf101873();
+    void testTableWidth();
 
     CPPUNIT_TEST_SUITE(SwUiWriterTest2);
     CPPUNIT_TEST(testRedlineMoveInsertInDelete);
@@ -83,6 +84,7 @@ public:
     CPPUNIT_TEST(testTdf122942);
     CPPUNIT_TEST(testTdf52391);
     CPPUNIT_TEST(testTdf101873);
+    CPPUNIT_TEST(testTableWidth);
     CPPUNIT_TEST_SUITE_END();
 
 private:
@@ -912,6 +914,23 @@ void SwUiWriterTest2::testTdf101873()
     // Actual:", i.e. searching for "something" failed, even if it was inserted above.
     SwShellCursor* pShellCursor = pWrtShell->getShellCursor(false);
     CPPUNIT_ASSERT_EQUAL(OUString("something"), pShellCursor->GetText());
+}
+
+void SwUiWriterTest2::testTableWidth()
+{
+    load(DATA_DIRECTORY, "frame_size_export.docx");
+
+    uno::Reference<frame::XStorable> xStorable(mxComponent, uno::UNO_QUERY);
+    utl::MediaDescriptor aMediaDescriptor;
+    aMediaDescriptor["FilterName"] <<= OUString("Office Open XML Text");
+    xStorable->storeToURL(maTempFile.GetURL(), aMediaDescriptor.getAsConstPropertyValueList());
+
+    // after exporting: table width was overwritten in the doc model
+    uno::Reference<text::XTextTablesSupplier> xTablesSupplier(mxComponent, uno::UNO_QUERY);
+    uno::Reference<container::XIndexAccess> xTables(xTablesSupplier->getTextTables(),
+                                                    uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(sal_Int16(100),
+                         getProperty<sal_Int16>(xTables->getByIndex(0), "RelativeWidth"));
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(SwUiWriterTest2);
