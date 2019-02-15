0a6fb9e13add20b9b88e235e22bc68b94357ce75
Jan Holesovsky <kendy@collabora.com>
2019-02-15 11:46:36 +0100

    android: Separate the Android-specific setup from the actual LOK init.
    Change-Id: I433376dfea0a43c63827ba15308a614f3466fb71
    Reviewed-on: https://gerrit.libreoffice.org/67875
    Tested-by: Jenkins
    Reviewed-by: Jan Holesovsky <kendy@collabora.com>

--- include/LibreOfficeKit/LibreOfficeKitInit.h
+++ include/LibreOfficeKit/LibreOfficeKitInit.h
@@ -249,13 +249,13 @@ typedef LibreOfficeKit *(LokHookFunction2)( const char *install_path, const char
 
 typedef int             (LokHookPreInit)  ( const char *install_path, const char *user_profile_url );
 
-#if defined(IOS)
+#if defined(IOS) || defined(ANDROID)
 LibreOfficeKit *libreofficekit_hook_2(const char* install_path, const char* user_profile_path);
 #endif
 
 static LibreOfficeKit *lok_init_2( const char *install_path,  const char *user_profile_url )
 {
-#if !defined(IOS)
+#if !defined(IOS) && !defined(ANDROID)
     void *dlhandle;
     char *imp_lib;
     LokHookFunction *pSym;
