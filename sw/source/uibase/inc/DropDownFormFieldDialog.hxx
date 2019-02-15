117da8dab05986380db586f1fae6a2f0af9ce389
Tamás Zolnai <tamas.zolnai@collabora.com>
2019-02-15 19:23:53 +0100

    MSForms: Introduce a properties dialog for Drop-down form field
    - Dialog created similar to the edit dialog of Input field
    - On the dialog, the user can edit the list of the drop down field
    - This dialog is only for editing of the field, so the user can't select
    an item from the list to display in the field.
    
    Change-Id: I6222aba9b211afeb0e9d10d97a49347921ff7353
    Reviewed-on: https://gerrit.libreoffice.org/67909
    Tested-by: Jenkins
    Reviewed-by: Tamás Zolnai <tamas.zolnai@collabora.com>

---
+++ sw/source/uibase/inc/DropDownFormFieldDialog.hxx
@@ -0,0 +1,69 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef INCLUDED_SW_SOURCE_UIBASE_INC_DROPDOWNFORMFIELDDIALOG_HXX
+#define INCLUDED_SW_SOURCE_UIBASE_INC_DROPDOWNFORMFIELDDIALOG_HXX
+
+#include <vcl/weld.hxx>
+#include "actctrl.hxx"
+
+namespace sw
+{
+namespace mark
+{
+class IFieldmark;
+}
+} // namespace sw
+
+/// Dialog to specify the properties of drop-down form field
+namespace sw
+{
+class DropDownFormFieldDialog : public weld::GenericDialogController
+{
+private:
+    mark::IFieldmark* m_pDropDownField;
+    bool m_bListHasChanged;
+
+    std::unique_ptr<weld::Entry> m_xListItemEntry;
+    std::unique_ptr<weld::Button> m_xListAddButton;
+
+    std::unique_ptr<weld::TreeView> m_xListItemsTreeView;
+
+    std::unique_ptr<weld::Button> m_xListRemoveButton;
+    std::unique_ptr<weld::Button> m_xListUpButton;
+    std::unique_ptr<weld::Button> m_xListDownButton;
+
+    DECL_LINK(ListChangedHdl, weld::TreeView&, void);
+    DECL_LINK(KeyPressedHdl, const KeyEvent&, bool);
+    DECL_LINK(KeyReleasedHdl, const KeyEvent&, bool);
+    DECL_LINK(ButtonPushedHdl, weld::Button&, void);
+
+    void InitControls();
+    void AppendItemToList();
+    void UpdateButtons();
+    void Apply();
+
+public:
+    DropDownFormFieldDialog(weld::Window* pParent, mark::IFieldmark* pDropDownField);
+    virtual ~DropDownFormFieldDialog() override;
+
+    virtual short run() override
+    {
+        short nRet = GenericDialogController::run();
+        if (nRet == RET_OK)
+            Apply();
+        return nRet;
+    }
+};
+
+} // namespace sw
+
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
