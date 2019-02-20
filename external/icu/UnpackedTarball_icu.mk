8a6f33e3a41c131f6c01fc3d428c7e682c031d0a
Michael Stahl <Michael.Stahl@cib.de>
2019-02-20 16:28:15 +0100

    icu: fix CVE-2018-18928
    Eike says that no LO code should use ICU number parser/formatter, but
    meanwhile ICU is also used in the externals firebird, harfbuzz,
    hunspell, libcdr, libebook, libfreehand, libmspub, libqxp, libivsio,
    libxml2, libamz, pdfium, xmlsec, so let's just patch it to be sure.
    
    Change-Id: I3e1a76d7ceefadbe3c514ad7f1384a4daa196f36

--- external/icu/UnpackedTarball_icu.mk
+++ external/icu/UnpackedTarball_icu.mk
@@ -38,6 +38,7 @@ $(eval $(call gb_UnpackedTarball_add_patches,icu,\
 	external/icu/icu4c-61-werror-shadow.patch.1 \
 	external/icu/gcc9.patch \
 	external/icu/char8_t.patch \
+	external/icu/CVE-2018-18928.patch.2 \
 ))
 
 $(eval $(call gb_UnpackedTarball_add_file,icu,source/data/brkitr/khmerdict.dict,external/icu/khmerdict.dict))
