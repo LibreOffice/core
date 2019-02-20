6676952d319e032a631bfe9d8ce23ebcb4c92f5b
Gülşah Köse <gulsah.kose@collabora.com>
2019-02-20 19:06:33 +0300

    tdf#123090 Handle removed column with gridSpan.
    This is a combination of 3 commits.
    (cherry picked from commit 48ef20f2039d1a300a4324072e9b712c9994b406)
    (cherry picked from commit 00e89430a2f8cd1f9ec702a7583a1e4c886a2b46)
    (cherry picked from commit 1f0206d940cd8f7fb627a59cfe4165c0bfebaf46)
    
    Change-Id: Ic6fa6f335623e2114fc8bea76dc54833284d2a02
    Reviewed-on: https://gerrit.libreoffice.org/68154
    Tested-by: Jenkins
    Reviewed-by: Andras Timar <andras.timar@collabora.com>

--- svx/source/table/tablecontroller.cxx
+++ svx/source/table/tablecontroller.cxx
@@ -1821,7 +1821,6 @@ void SvxTableController::MergeRange( sal_Int32 nFirstCol, sal_Int32 nFirstRow, s
             }
 
             xRange->merge();
-            mxTable->optimize();
             mbHasJustMerged = true;
             setSelectedCells( maCursorFirstPos, maCursorFirstPos );
 
