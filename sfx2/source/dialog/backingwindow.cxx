8e41b0bd92a023d54e2737e2e0f0a134868e6367
Jim Raykowski <raykowj@gmail.com>
2019-02-21 16:04:04 -0900

    tdf#102005 Fix F6 regression in start center
    Change-Id: I152a79b79e75bf87c437dd4dd2976c73198e6b52

--- sfx2/source/dialog/backingwindow.cxx
+++ sfx2/source/dialog/backingwindow.cxx
@@ -410,15 +410,18 @@ bool BackingWindow::PreNotify(NotifyEvent& rNEvt)
             }
             else // F6
             {
-                if(mpAllRecentThumbnails->IsVisible())
+                if( mpAllButtonsBox->HasChildPathFocus() )
                 {
-                    mpAllRecentThumbnails->GrabFocus();
-                    return true;
-                }
-                else if(mpLocalView->IsVisible())
-                {
-                    mpLocalView->GrabFocus();
-                    return true;
+                    if(mpAllRecentThumbnails->IsVisible())
+                    {
+                        mpAllRecentThumbnails->GrabFocus();
+                        return true;
+                    }
+                    else if(mpLocalView->IsVisible())
+                    {
+                        mpLocalView->GrabFocus();
+                        return true;
+                    }
                 }
             }
         }
