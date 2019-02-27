9450b8fc67e83e072475d7ae0fc4150b2c938900
Katarina Behrens <Katarina.Behrens@cib.de>
2019-02-27 09:40:50 +0100

    Unbreak internal DnD by using null MimeData
    Regression from 67b0fcea9d748c4dd8101036cbb2c587d65f0bf7 (which
    implements external DnD). QMimeData of internal DnD is never null,
    it always contains internal MimeType (but empty byte array)
    
    Change-Id: Ie12e9c3b7a3c7cebfd98064bb2fafec27c20a442
    Reviewed-on: https://gerrit.libreoffice.org/68673
    Tested-by: Jenkins
    Reviewed-by: Katarina Behrens <Katarina.Behrens@cib.de>
    (cherry picked from commit 14dca9a2197f1b43bf547ee47517e76e58309b93)

--- vcl/qt5/Qt5Frame.cxx
+++ vcl/qt5/Qt5Frame.cxx
@@ -29,6 +29,7 @@
 #include <Qt5Menu.hxx>
 #include <Qt5DragAndDrop.hxx>
 
+#include <QtCore/QMimeData>
 #include <QtCore/QPoint>
 #include <QtCore/QSize>
 #include <QtGui/QIcon>
@@ -1110,7 +1111,7 @@ void Qt5Frame::draggingStarted(const int x, const int y, Qt::DropActions eAction
     aEvent.SourceActions = toVclDropActions(eActions);
 
     css::uno::Reference<css::datatransfer::XTransferable> xTransferable;
-    if (pQMimeData)
+    if (!pQMimeData->hasFormat(sInternalMimeType))
         xTransferable = new Qt5DnDTransferable(pQMimeData);
     else
         xTransferable = Qt5DragSource::m_ActiveDragSource->GetTransferable();
@@ -1142,7 +1143,7 @@ void Qt5Frame::dropping(const int x, const int y, const QMimeData* pQMimeData)
     aEvent.SourceActions = css::datatransfer::dnd::DNDConstants::ACTION_MOVE;
 
     css::uno::Reference<css::datatransfer::XTransferable> xTransferable;
-    if (pQMimeData)
+    if (!pQMimeData->hasFormat(sInternalMimeType))
         xTransferable = new Qt5DnDTransferable(pQMimeData);
     else
         xTransferable = Qt5DragSource::m_ActiveDragSource->GetTransferable();
