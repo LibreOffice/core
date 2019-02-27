22794a8542331269fdac59f39090ddf070c506e8
Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
2019-02-27 08:40:04 +0100

    tdf#123728 Always print on main thread to avoid deadlocks
    Using lambda doesn't work on this branch, thus code differs from master commit.
    
    Reviewed-on: https://gerrit.libreoffice.org/68421
    Tested-by: Jenkins
    Reviewed-by: Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
    (cherry picked from commit 110803068b75ffd895a9b1139aee2248d30f085b)
    
    Change-Id: Ia8c2600aee42c3063e18032d1e485ad417ec4639

--- sfx2/source/doc/sfxbasemodel.cxx
+++ sfx2/source/doc/sfxbasemodel.cxx
@@ -84,6 +84,7 @@
 #include <comphelper/numberedcollection.hxx>
 #include <unotools/ucbstreamhelper.hxx>
 #include <unotools/ucbhelper.hxx>
+#include <vcl/threadex.hxx>
 
 #include <sfx2/sfxbasecontroller.hxx>
 #include <sfx2/viewfac.hxx>
@@ -1426,13 +1427,23 @@ void SAL_CALL SfxBaseModel::setPrinter(const Sequence< beans::PropertyValue >& r
         m_pData->m_xPrintable->setPrinter( rPrinter );
 }
 
+static bool ImplPrintStatic(const Reference<view::XPrintable>& m_xPrintable,
+                            const Sequence<beans::PropertyValue>& rOptions)
+{
+    m_xPrintable->print(rOptions);
+    return true;
+}
+
 void SAL_CALL SfxBaseModel::print(const Sequence< beans::PropertyValue >& rOptions)
         throw (lang::IllegalArgumentException, RuntimeException, std::exception)
 {
     SfxModelGuard aGuard( *this );
 
     if ( impl_getPrintHelper() )
-        m_pData->m_xPrintable->print( rOptions );
+    {
+        // tdf#123728 Always print on main thread to avoid deadlocks
+        vcl::solarthread::syncExecute(std::bind(&ImplPrintStatic, m_pData->m_xPrintable, rOptions));
+    }
 }
 
 
