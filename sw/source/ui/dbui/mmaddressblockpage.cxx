98cbfec9e29fdc3bb1891cd2546244276ed42d76
Caolán McNamara <caolanm@redhat.com>
2019-02-28 14:52:50 +0000

    weld SwAddressListDialog
    Change-Id: I3ae15af8a2c7475394dfc5f6e54b89950d2238c0

--- sw/source/ui/dbui/mmaddressblockpage.cxx
+++ sw/source/ui/dbui/mmaddressblockpage.cxx
@@ -168,16 +168,16 @@ IMPL_LINK_NOARG(SwMailMergeAddressBlockPage, AddressListHdl_Impl, Button*, void)
 {
     try
     {
-        ScopedVclPtrInstance< SwAddressListDialog > xAddrDialog(this);
-        if(RET_OK == xAddrDialog->Execute())
+        SwAddressListDialog aAddrDialog(this);
+        if (RET_OK == aAddrDialog.run())
         {
             SwMailMergeConfigItem& rConfigItem = m_pWizard->GetConfigItem();
             rConfigItem.SetCurrentConnection(
-                            xAddrDialog->GetSource(),
-                            xAddrDialog->GetConnection(),
-                            xAddrDialog->GetColumnsSupplier(),
-                            xAddrDialog->GetDBData());
-            OUString sFilter = xAddrDialog->GetFilter();
+                            aAddrDialog.GetSource(),
+                            aAddrDialog.GetConnection(),
+                            aAddrDialog.GetColumnsSupplier(),
+                            aAddrDialog.GetDBData());
+            OUString sFilter = aAddrDialog.GetFilter();
             rConfigItem.SetFilter( sFilter );
             InsertDataHdl_Impl(nullptr);
             GetWizard()->UpdateRoadmap();
