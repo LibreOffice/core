205e2d6c31b1861111b70a704516580761a75099
Caolán McNamara <caolanm@redhat.com>
2019-02-28 10:19:32 +0000

    address list doesn't refresh on pressing 'delete'
    m_nCurrentDataSet doesn't change, so it doesn't refill it, even
    though m_nCurrentDataSet was deleted so it no refers to a new
    entry
    
    Change-Id: I0c68f407a23a28a7e55fb3cf691e5f07e5ed6729
    Reviewed-on: https://gerrit.libreoffice.org/68497
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>

--- sw/source/ui/dbui/createaddresslistdialog.cxx
+++ sw/source/ui/dbui/createaddresslistdialog.cxx
@@ -84,7 +84,8 @@ public:
     void        SetData(SwCSVData& rDBData);
 
     void        SetCurrentDataSet(sal_uInt32 nSet);
-    sal_uInt32  GetCurrentDataSet() const { return m_nCurrentDataSet;}
+    void        CurrentDataSetInvalidated() { m_nCurrentDataSet = std::numeric_limits<sal_uInt32>::max(); }
+    sal_uInt32  GetCurrentDataSet() const { return m_nCurrentDataSet; }
     void        SetCursorTo(std::size_t nElement);
     virtual void Resize() override;
 };
@@ -543,6 +544,7 @@ IMPL_LINK_NOARG(SwCreateAddressListDialog, DeleteHdl_Impl, Button*, void)
         m_pCSVData->aDBData[0].assign(m_pCSVData->aDBData[0].size(), OUString());
         m_pDeletePB->Enable(false);
     }
+    m_pAddressControl->CurrentDataSetInvalidated();
     m_pAddressControl->SetCurrentDataSet(nCurrent);
     m_pSetNoNF->SetMax(m_pCSVData->aDBData.size());
     UpdateButtons();
