6626d213c74be3f9ae46c70e3179fad35c5d2901
Gülşah Köse <gulsah.kose@collabora.com>
2019-03-02 01:38:50 +0300

    tdf#122590 handle ooxml cfRule element for type=cellIs.
    Change-Id: If08b7a86f68f34b8d143f219ddc59413ea622daf

--- sc/source/filter/oox/extlstcontext.cxx
+++ sc/source/filter/oox/extlstcontext.cxx
@@ -20,11 +20,13 @@
 #include <document.hxx>
 #include <worksheetfragment.hxx>
 #include <workbookfragment.hxx>
+#include <stylesbuffer.hxx>
 
 #include <rangeutl.hxx>
 #include <sal/log.hxx>
 
 using ::oox::core::ContextHandlerRef;
+using ::oox::xls::CondFormatBuffer;
 
 namespace oox {
 namespace xls {
@@ -79,6 +81,7 @@ void ExtCfRuleContext::onStartElement( const AttributeList& rAttribs )
 ExtConditionalFormattingContext::ExtConditionalFormattingContext(WorksheetContextBase& rFragment):
     WorksheetContextBase(rFragment)
 {
+    isPreviousElementF = false;
 }
 
 ContextHandlerRef ExtConditionalFormattingContext::onCreateContext(sal_Int32 nElement, const AttributeList& rAttribs)
@@ -119,12 +122,18 @@ ContextHandlerRef ExtConditionalFormattingContext::onCreateContext(sal_Int32 nEl
             maEntries.push_back(std::make_unique<ScIconSetFormat>(pDoc));
             return new IconSetContext(*this, mpCurrentRule.get());
         }
+        else if (aType == "cellIs")
+        {
+            sal_Int32 aToken = rAttribs.getToken( XML_operator, XML_TOKEN_INVALID );
+            eOperator =  CondFormatBuffer::convertToInternalOperator(aToken);
+            return this;
+        }
         else
         {
             SAL_WARN("sc", "unhandled XLS14_TOKEN(cfRule) with type: " << aType);
         }
     }
-    else if (nElement == XM_TOKEN(sqref))
+    else if (nElement == XM_TOKEN(sqref) || nElement == XM_TOKEN(f))
     {
         return this;
     }
@@ -132,26 +141,38 @@ ContextHandlerRef ExtConditionalFormattingContext::onCreateContext(sal_Int32 nEl
     return nullptr;
 }
 
-void ExtConditionalFormattingContext::onStartElement(const AttributeList& /*rAttribs*/)
+void ExtConditionalFormattingContext::onStartElement(const AttributeList& /*Attribs*/)
 {
-    switch (getCurrentElement())
-    {
-        case XM_TOKEN(sqref):
-        {
-        }
-        break;
-    }
 }
 
 void ExtConditionalFormattingContext::onCharacters(const OUString& rCharacters)
 {
-    aChars = rCharacters;
+    switch (getCurrentElement())
+    {
+        case XM_TOKEN(f):
+        {
+            aChars = rCharacters;
+            isPreviousElementF = true;
+        }
+        break;
+        case XM_TOKEN(sqref):
+        {
+            aChars = rCharacters;
+        }
+        break;
+    }
+
 }
 
 void ExtConditionalFormattingContext::onEndElement()
 {
     switch (getCurrentElement())
     {
+        case XM_TOKEN(f):
+        {
+            expr = aChars;
+        }
+        break;
         case XM_TOKEN(sqref):
         {
             ScRangeList aRange;
@@ -167,8 +188,22 @@ void ExtConditionalFormattingContext::onEndElement()
                 aRange[i].aEnd.SetTab(nTab);
             }
 
+            if(isPreviousElementF)
+            {
+
+                rPos = aRange.GetTopLeftCorner();
+                rStyle = getStyles().createDxfStyle(0);
+
+                ScCondFormatEntry* pEntry = new ScCondFormatEntry(eOperator, expr, "", pDoc,
+                                                                  rPos, rStyle, "", "",
+                                                                  formula::FormulaGrammar::GRAM_OOXML ,
+                                                                  formula::FormulaGrammar::GRAM_OOXML );
+                maEntries.push_back(std::unique_ptr<ScFormatEntry>(pEntry));
+            }
+
             std::vector< std::unique_ptr<ExtCfCondFormat> >& rExtFormats =  getCondFormats().importExtCondFormat();
             rExtFormats.push_back(std::make_unique<ExtCfCondFormat>(aRange, maEntries));
+            isPreviousElementF = false;
         }
         break;
         default:
