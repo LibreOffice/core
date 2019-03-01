6a65b448744323bc02cb57d7dad7bf6d4544d731
Muhammet Kara <muhammet.kara@collabora.com>
2019-03-01 21:56:31 +0300

    WiP: Auto-redact - First stab
    Change-Id: If3db62e50994670143785b6727fdcf1ccd4c6f8e

--- sfx2/source/doc/SfxRedactionHelper.cxx
+++ sfx2/source/doc/SfxRedactionHelper.cxx
@@ -10,6 +10,7 @@
 #include <SfxRedactionHelper.hxx>
 
 #include <com/sun/star/drawing/XDrawPagesSupplier.hpp>
+#include <com/sun/star/drawing/LineStyle.hpp>
 #include <com/sun/star/lang/XMultiServiceFactory.hpp>
 #include <com/sun/star/graphic/XGraphic.hpp>
 #include <com/sun/star/frame/XLayoutManager.hpp>
@@ -31,12 +32,17 @@
 
 #include <svtools/DocumentToGraphicRenderer.hxx>
 
+#include <tools/gen.hxx>
+
 #include <vcl/gdimtf.hxx>
 #include <vcl/graph.hxx>
 #include <sal/log.hxx>
 
 #include <vcl/wmf.hxx>
 #include <vcl/gdimetafiletools.hxx>
+#include <vcl/metaact.hxx>
+#include <vcl/outdev.hxx>
+#include <vcl/vcllayout.hxx>
 
 using namespace ::com::sun::star;
 using namespace ::com::sun::star::lang;
@@ -105,7 +111,308 @@ void setPageMargins(uno::Reference<beans::XPropertySet>& xPageProperySet,
     xPageProperySet->setPropertyValue("BorderLeft", css::uno::makeAny(aPageMargins.nLeft));
     xPageProperySet->setPropertyValue("BorderRight", css::uno::makeAny(aPageMargins.nRight));
 }
+
+// #i10613# Extracted from ImplCheckRect::ImplCreate
+tools::Rectangle ImplCalcActionBounds(const MetaAction& rAct, const OutputDevice& rOut,
+                                      const OUString& sSubString, const sal_Int32& nStrPos)
+{
+    tools::Rectangle aActionBounds;
+
+    switch (rAct.GetType())
+    {
+            /*case MetaActionType::PIXEL:
+            aActionBounds = tools::Rectangle(static_cast<const MetaPixelAction&>(rAct).GetPoint(),
+                                             Size(1, 1));
+            break;
+
+        case MetaActionType::POINT:
+            aActionBounds = tools::Rectangle(static_cast<const MetaPointAction&>(rAct).GetPoint(),
+                                             Size(1, 1));
+            break;
+
+        case MetaActionType::LINE:
+        {
+            const MetaLineAction& rMetaLineAction = static_cast<const MetaLineAction&>(rAct);
+            aActionBounds
+                = tools::Rectangle(rMetaLineAction.GetStartPoint(), rMetaLineAction.GetEndPoint());
+            aActionBounds.Justify();
+            const long nLineWidth(rMetaLineAction.GetLineInfo().GetWidth());
+            if (nLineWidth)
+            {
+                const long nHalfLineWidth((nLineWidth + 1) / 2);
+                aActionBounds.AdjustLeft(-nHalfLineWidth);
+                aActionBounds.AdjustTop(-nHalfLineWidth);
+                aActionBounds.AdjustRight(nHalfLineWidth);
+                aActionBounds.AdjustBottom(nHalfLineWidth);
+            }
+            break;
+        }
+
+        case MetaActionType::RECT:
+            aActionBounds = static_cast<const MetaRectAction&>(rAct).GetRect();
+            break;
+
+        case MetaActionType::ROUNDRECT:
+            aActionBounds
+                = tools::Polygon(static_cast<const MetaRoundRectAction&>(rAct).GetRect(),
+                                 static_cast<const MetaRoundRectAction&>(rAct).GetHorzRound(),
+                                 static_cast<const MetaRoundRectAction&>(rAct).GetVertRound())
+                      .GetBoundRect();
+            break;
+
+        case MetaActionType::ELLIPSE:
+        {
+            const tools::Rectangle& rRect = static_cast<const MetaEllipseAction&>(rAct).GetRect();
+            aActionBounds
+                = tools::Polygon(rRect.Center(), rRect.GetWidth() >> 1, rRect.GetHeight() >> 1)
+                      .GetBoundRect();
+            break;
+        }
+
+        case MetaActionType::ARC:
+            aActionBounds = tools::Polygon(static_cast<const MetaArcAction&>(rAct).GetRect(),
+                                           static_cast<const MetaArcAction&>(rAct).GetStartPoint(),
+                                           static_cast<const MetaArcAction&>(rAct).GetEndPoint(),
+                                           PolyStyle::Arc)
+                                .GetBoundRect();
+            break;
+
+        case MetaActionType::PIE:
+            aActionBounds = tools::Polygon(static_cast<const MetaPieAction&>(rAct).GetRect(),
+                                           static_cast<const MetaPieAction&>(rAct).GetStartPoint(),
+                                           static_cast<const MetaPieAction&>(rAct).GetEndPoint(),
+                                           PolyStyle::Pie)
+                                .GetBoundRect();
+            break;
+
+        case MetaActionType::CHORD:
+            aActionBounds
+                = tools::Polygon(static_cast<const MetaChordAction&>(rAct).GetRect(),
+                                 static_cast<const MetaChordAction&>(rAct).GetStartPoint(),
+                                 static_cast<const MetaChordAction&>(rAct).GetEndPoint(),
+                                 PolyStyle::Chord)
+                      .GetBoundRect();
+            break;
+
+        case MetaActionType::POLYLINE:
+        {
+            const MetaPolyLineAction& rMetaPolyLineAction
+                = static_cast<const MetaPolyLineAction&>(rAct);
+            aActionBounds = rMetaPolyLineAction.GetPolygon().GetBoundRect();
+            const long nLineWidth(rMetaPolyLineAction.GetLineInfo().GetWidth());
+            if (nLineWidth)
+            {
+                const long nHalfLineWidth((nLineWidth + 1) / 2);
+                aActionBounds.AdjustLeft(-nHalfLineWidth);
+                aActionBounds.AdjustTop(-nHalfLineWidth);
+                aActionBounds.AdjustRight(nHalfLineWidth);
+                aActionBounds.AdjustBottom(nHalfLineWidth);
+            }
+            break;
+        }
+
+        case MetaActionType::POLYGON:
+            aActionBounds = static_cast<const MetaPolygonAction&>(rAct).GetPolygon().GetBoundRect();
+            break;
+
+        case MetaActionType::POLYPOLYGON:
+            aActionBounds
+                = static_cast<const MetaPolyPolygonAction&>(rAct).GetPolyPolygon().GetBoundRect();
+            break;
+
+        case MetaActionType::BMP:
+            aActionBounds = tools::Rectangle(
+                static_cast<const MetaBmpAction&>(rAct).GetPoint(),
+                rOut.PixelToLogic(
+                    static_cast<const MetaBmpAction&>(rAct).GetBitmap().GetSizePixel()));
+            break;
+
+        case MetaActionType::BMPSCALE:
+            aActionBounds
+                = tools::Rectangle(static_cast<const MetaBmpScaleAction&>(rAct).GetPoint(),
+                                   static_cast<const MetaBmpScaleAction&>(rAct).GetSize());
+            break;
+
+        case MetaActionType::BMPSCALEPART:
+            aActionBounds
+                = tools::Rectangle(static_cast<const MetaBmpScalePartAction&>(rAct).GetDestPoint(),
+                                   static_cast<const MetaBmpScalePartAction&>(rAct).GetDestSize());
+            break;
+
+        case MetaActionType::BMPEX:
+            aActionBounds = tools::Rectangle(
+                static_cast<const MetaBmpExAction&>(rAct).GetPoint(),
+                rOut.PixelToLogic(
+                    static_cast<const MetaBmpExAction&>(rAct).GetBitmapEx().GetSizePixel()));
+            break;
+
+        case MetaActionType::BMPEXSCALE:
+            aActionBounds
+                = tools::Rectangle(static_cast<const MetaBmpExScaleAction&>(rAct).GetPoint(),
+                                   static_cast<const MetaBmpExScaleAction&>(rAct).GetSize());
+            break;
+
+        case MetaActionType::BMPEXSCALEPART:
+            aActionBounds = tools::Rectangle(
+                static_cast<const MetaBmpExScalePartAction&>(rAct).GetDestPoint(),
+                static_cast<const MetaBmpExScalePartAction&>(rAct).GetDestSize());
+            break;
+
+        case MetaActionType::MASK:
+            aActionBounds = tools::Rectangle(
+                static_cast<const MetaMaskAction&>(rAct).GetPoint(),
+                rOut.PixelToLogic(
+                    static_cast<const MetaMaskAction&>(rAct).GetBitmap().GetSizePixel()));
+            break;
+
+        case MetaActionType::MASKSCALE:
+            aActionBounds
+                = tools::Rectangle(static_cast<const MetaMaskScaleAction&>(rAct).GetPoint(),
+                                   static_cast<const MetaMaskScaleAction&>(rAct).GetSize());
+            break;
+
+        case MetaActionType::MASKSCALEPART:
+            aActionBounds
+                = tools::Rectangle(static_cast<const MetaMaskScalePartAction&>(rAct).GetDestPoint(),
+                                   static_cast<const MetaMaskScalePartAction&>(rAct).GetDestSize());
+            break;
+
+        case MetaActionType::GRADIENT:
+            aActionBounds = static_cast<const MetaGradientAction&>(rAct).GetRect();
+            break;
+
+        case MetaActionType::GRADIENTEX:
+            aActionBounds
+                = static_cast<const MetaGradientExAction&>(rAct).GetPolyPolygon().GetBoundRect();
+            break;
+
+        case MetaActionType::HATCH:
+            aActionBounds
+                = static_cast<const MetaHatchAction&>(rAct).GetPolyPolygon().GetBoundRect();
+            break;
+
+        case MetaActionType::WALLPAPER:
+            aActionBounds = static_cast<const MetaWallpaperAction&>(rAct).GetRect();
+            break;
+
+        case MetaActionType::Transparent:
+            aActionBounds
+                = static_cast<const MetaTransparentAction&>(rAct).GetPolyPolygon().GetBoundRect();
+            break;
+
+        case MetaActionType::FLOATTRANSPARENT:
+            aActionBounds
+                = tools::Rectangle(static_cast<const MetaFloatTransparentAction&>(rAct).GetPoint(),
+                                   static_cast<const MetaFloatTransparentAction&>(rAct).GetSize());
+            break;
+
+        case MetaActionType::EPS:
+            aActionBounds = tools::Rectangle(static_cast<const MetaEPSAction&>(rAct).GetPoint(),
+                                             static_cast<const MetaEPSAction&>(rAct).GetSize());
+            break;
+
+        case MetaActionType::TEXT:
+        {
+            const MetaTextAction& rTextAct = static_cast<const MetaTextAction&>(rAct);
+            const OUString aString(rTextAct.GetText().copy(rTextAct.GetIndex(), rTextAct.GetLen()));
+
+            if (!aString.isEmpty())
+            {
+                const Point aPtLog(rTextAct.GetPoint());
+
+                // #105987# Use API method instead of Impl* methods
+                // #107490# Set base parameter equal to index parameter
+                rOut.GetTextBoundRect(aActionBounds, rTextAct.GetText(), rTextAct.GetIndex(),
+                                      rTextAct.GetIndex(), rTextAct.GetLen());
+                aActionBounds.Move(aPtLog.X(), aPtLog.Y());
+            }
+        }
+        break;*/
+
+        case MetaActionType::TEXTARRAY:
+        {
+            const MetaTextArrayAction& rTextAct = static_cast<const MetaTextArrayAction&>(rAct);
+            const OUString aString(rTextAct.GetText().copy(rTextAct.GetIndex(), rTextAct.GetLen()));
+
+            if (!aString.isEmpty())
+            {
+                // #105987# ImplLayout takes everything in logical coordinates
+                std::unique_ptr<SalLayout> pSalLayout1 = rOut.ImplLayout(
+                    aString, 0, nStrPos, rTextAct.GetPoint(), 0, rTextAct.GetDXArray());
+                std::unique_ptr<SalLayout> pSalLayout2
+                    = rOut.ImplLayout(aString, 0, nStrPos + sSubString.getLength(),
+                                      rTextAct.GetPoint(), 0, rTextAct.GetDXArray());
+                if (pSalLayout2)
+                {
+                    tools::Rectangle aBoundRect2(
+                        const_cast<OutputDevice&>(rOut).ImplGetTextBoundRect(*pSalLayout2));
+                    aActionBounds = rOut.PixelToLogic(aBoundRect2);
+                }
+                if (pSalLayout1 && nStrPos > 0)
+                {
+                    tools::Rectangle aBoundRect1(
+                        const_cast<OutputDevice&>(rOut).ImplGetTextBoundRect(*pSalLayout1));
+                    aActionBounds.SetLeft(rOut.PixelToLogic(aBoundRect1).getX()
+                                          + rOut.PixelToLogic(aBoundRect1).getWidth());
+                }
+
+                // FIXME: Is this really needed?
+                aActionBounds.SetTop(aActionBounds.getY() + 100);
+            }
+        }
+        break;
+
+            /*case MetaActionType::TEXTRECT:
+            aActionBounds = static_cast<const MetaTextRectAction&>(rAct).GetRect();
+            break;
+
+        case MetaActionType::STRETCHTEXT:
+        {
+            const MetaStretchTextAction& rTextAct = static_cast<const MetaStretchTextAction&>(rAct);
+            const OUString aString(rTextAct.GetText().copy(rTextAct.GetIndex(), rTextAct.GetLen()));
+
+            // #i16195# Literate copy from TextArray action, the
+            // semantics for the ImplLayout call are copied from the
+            // OutDev::DrawStretchText() code. Unfortunately, also in
+            // this case, public outdev methods such as GetTextWidth()
+            // don't provide enough info.
+            if (!aString.isEmpty())
+            {
+                // #105987# ImplLayout takes everything in logical coordinates
+                std::unique_ptr<SalLayout> pSalLayout
+                    = rOut.ImplLayout(rTextAct.GetText(), rTextAct.GetIndex(), rTextAct.GetLen(),
+                                      rTextAct.GetPoint(), rTextAct.GetWidth());
+                if (pSalLayout)
+                {
+                    tools::Rectangle aBoundRect(
+                        const_cast<OutputDevice&>(rOut).ImplGetTextBoundRect(*pSalLayout));
+                    aActionBounds = rOut.PixelToLogic(aBoundRect);
+                }
+            }
+        }
+        break;
+
+        case MetaActionType::TEXTLINE:
+            SAL_WARN("sfx.doc", "MetaActionType::TEXTLINE not supported");
+            break;*/
+
+        default:
+            break;
+    }
+
+    if (!aActionBounds.IsEmpty())
+    {
+        // fdo#40421 limit current action's output to clipped area
+        if (rOut.IsClipRegion())
+            return rOut.GetClipRegion().GetBoundRect().Intersection(aActionBounds);
+        else
+            return aActionBounds;
+    }
+    else
+        return tools::Rectangle(0, 0, 0, 0);
 }
+
+} // End of anon namespace
 
 void SfxRedactionHelper::getPageMetaFilesFromDoc(std::vector<GDIMetaFile>& aMetaFiles,
                                                  std::vector<::Size>& aPageSizes,
@@ -184,6 +491,13 @@ void SfxRedactionHelper::addPagesToDraw(uno::Reference<XComponent>& xComponent,
             awt::Size(rGDIMetaFile.GetPrefSize().Width(), rGDIMetaFile.GetPrefSize().Height()));
 
         xPage->add(xShape);
+
+        // Search for the redaction strings, and get the rectangle coordinates
+        std::vector<::tools::Rectangle> aRedactionRectangles;
+        searchInMetaFile("deployment", rGDIMetaFile, aRedactionRectangles, xComponent);
+
+        // Add the redaction rectangles to the page
+        addRedactionRectToPage(xComponent, xPage, aRedactionRectangles);
     }
 
     // Remove the extra page at the beginning
@@ -332,6 +646,74 @@ SfxRedactionHelper::getPageMarginsForCalc(css::uno::Reference<css::frame::XModel
     xPageProperties->getPropertyValue("BottomMargin") >>= aPageMargins.nBottom;
 
     return aPageMargins;
+}
+
+void SfxRedactionHelper::searchInMetaFile(const rtl::OUString& sSearchTerm, const GDIMetaFile& rMtf,
+                                          std::vector<::tools::Rectangle>& aRedactionRectangles,
+                                          uno::Reference<XComponent>& xComponent)
+{
+    MetaAction* pCurrAct;
+
+    // Watch for TEXTARRAY actions.
+    // They contain the text of paragraphes.
+    for (pCurrAct = const_cast<GDIMetaFile&>(rMtf).FirstAction(); pCurrAct;
+         pCurrAct = const_cast<GDIMetaFile&>(rMtf).NextAction())
+    {
+        if (pCurrAct->GetType() == MetaActionType::TEXTARRAY)
+        {
+            MetaTextArrayAction* pMetaTextArrayAction = static_cast<MetaTextArrayAction*>(pCurrAct);
+
+            //sal_Int32 aIndex = pMetaTextArrayAction->GetIndex();
+            //sal_Int32 aLength = pMetaTextArrayAction->GetLen();
+            //Point aPoint = pMetaTextArrayAction->GetPoint();
+            OUString sText = pMetaTextArrayAction->GetText();
+            sal_Int32 nFoundIndex = sText.indexOf(sSearchTerm);
+
+            // If found the string, add the corresponding rectangle to the collection
+            if (nFoundIndex >= 0)
+            {
+                OutputDevice* pOutputDevice
+                    = SfxObjectShell::GetShellFromComponent(xComponent)->GetDocumentRefDev();
+                tools::Rectangle aNewRect(ImplCalcActionBounds(
+                    *pMetaTextArrayAction, *pOutputDevice, sSearchTerm, nFoundIndex));
+
+                aRedactionRectangles.push_back(aNewRect);
+            }
+        }
+    }
+}
+
+void SfxRedactionHelper::addRedactionRectToPage(
+    uno::Reference<XComponent>& xComponent, uno::Reference<drawing::XDrawPage>& xPage,
+    const std::vector<::tools::Rectangle>& aNewRectangles)
+{
+    if (!xComponent.is() || !xPage.is())
+        return;
+
+    if (aNewRectangles.empty())
+        return;
+
+    uno::Reference<css::lang::XMultiServiceFactory> xFactory(xComponent, uno::UNO_QUERY);
+
+    for (auto const& aNewRectangle : aNewRectangles)
+    {
+        uno::Reference<drawing::XShape> xRectShape(
+            xFactory->createInstance("com.sun.star.drawing.RectangleShape"), uno::UNO_QUERY);
+        uno::Reference<beans::XPropertySet> xRectShapeProperySet(xRectShape, uno::UNO_QUERY);
+
+        xRectShapeProperySet->setPropertyValue("Name",
+                                               uno::Any(OUString("RectangleRedactionShape")));
+        xRectShapeProperySet->setPropertyValue("FillTransparence",
+                                               css::uno::makeAny(static_cast<sal_Int16>(50)));
+        xRectShapeProperySet->setPropertyValue("FillColor", css::uno::makeAny(COL_GRAY7));
+        xRectShapeProperySet->setPropertyValue(
+            "LineStyle", css::uno::makeAny(css::drawing::LineStyle::LineStyle_NONE));
+
+        xRectShape->setSize(awt::Size(aNewRectangle.GetWidth(), aNewRectangle.GetHeight()));
+        xRectShape->setPosition(awt::Point(aNewRectangle.getX(), aNewRectangle.getY()));
+
+        xPage->add(xRectShape);
+    }
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
