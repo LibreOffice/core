871adca2177cc462786d4144f389dfd08d2475f4
Tamas Bunth <tamas.bunth@collabora.co.uk>
2019-03-04 16:54:06 +0100

    single source file for licensing info with conditional text
    ... for easier maintenance. This commit only introduces the
    framework. Follow up commits will update the license file.
    Advantages:
    * single source, multiple outputs (fodt, html, txt)
    * conditional text, i.e. output will not include license
      terms of components that are not configured in the build
    
    Change-Id: I0ffad41119228a0a26fd1b6ce19930ca6085adb2

--- readlicense_oo/Package_files.mk
+++ readlicense_oo/Package_files.mk
@@ -9,26 +9,12 @@
 
 $(eval $(call gb_Package_Package,readlicense_oo_files,$(SRCDIR)/readlicense_oo/license))
 
-# LICENSE (upper case) is copied without EOL conversion
-# license.txt is converted, prior to copy, see Package_license.mk
-ifneq ($(OS),WNT)
-ifneq ($(OS),MACOSX)
-$(eval $(call gb_Package_add_file,readlicense_oo_files,LICENSE,LICENSE))
-else
-$(eval $(call gb_Package_add_file,readlicense_oo_files,Resources/LICENSE,LICENSE))
-endif
-endif
-
 ifneq ($(OS),MACOSX)
 $(eval $(call gb_Package_add_file,readlicense_oo_files,NOTICE,NOTICE))
-$(eval $(call gb_Package_add_file,readlicense_oo_files,LICENSE.fodt,LICENSE.fodt))
 $(eval $(call gb_Package_add_file,readlicense_oo_files,CREDITS.fodt,CREDITS.fodt))
-$(eval $(call gb_Package_add_file,readlicense_oo_files,LICENSE.html,LICENSE.html))
 else
 $(eval $(call gb_Package_add_file,readlicense_oo_files,Resources/NOTICE,NOTICE))
-$(eval $(call gb_Package_add_file,readlicense_oo_files,Resources/LICENSE.fodt,LICENSE.fodt))
 $(eval $(call gb_Package_add_file,readlicense_oo_files,Resources/CREDITS.fodt,CREDITS.fodt))
-$(eval $(call gb_Package_add_file,readlicense_oo_files,Resources/LICENSE.html,LICENSE.html))
 endif
 
 # vim: set noet sw=4 ts=4:
