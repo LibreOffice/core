4f782860dccc41ffa9728b30f9cd77533833c952
Tamas Bunth <tamas.bunth@collabora.co.uk>
2019-03-04 16:54:06 +0100

    single source file for licensing info with conditional text
    ... for easier maintenance. This commit only introduces the
    framework. Follow up commits will update the license file.
    Advantages:
    * single source, multiple outputs (fodt, html, txt)
    * conditional text, i.e. output will not include license
      terms of components that are not configured in the build
    
    Change-Id: I0ffad41119228a0a26fd1b6ce19930ca6085adb2

--- readlicense_oo/CustomTarget_license.mk
+++ readlicense_oo/CustomTarget_license.mk
@@ -10,15 +10,72 @@
 $(eval $(call gb_CustomTarget_CustomTarget,readlicense_oo/license))
 
 readlicense_oo_DIR := $(call gb_CustomTarget_get_workdir,readlicense_oo/license)
+readlicense_oo_LICENSE_xml := $(SRCDIR)/readlicense_oo/license/license.xml
 
+$(call gb_CustomTarget_get_target,readlicense_oo/license) : $(readlicense_oo_DIR)/license.html \
+	$(readlicense_oo_DIR)/license.fodt
+
+ifeq ($(OS),WNT)
 $(call gb_CustomTarget_get_target,readlicense_oo/license) : $(readlicense_oo_DIR)/license.txt
+else
+$(call gb_CustomTarget_get_target,readlicense_oo/license) : $(readlicense_oo_DIR)/LICENSE
+endif
 
+$(readlicense_oo_DIR)/license.html : \
+		$(SRCDIR)/readlicense_oo/license/license_html.xsl \
+		$(readlicense_oo_LICENSE_xml) \
+		| $(readlicense_oo_DIR)/.dir \
+		  $(call gb_ExternalExecutable_get_dependencies,xsltproc)
+	$(call gb_Output_announce,$(subst $(WORKDIR)/,,$@),$(true),XSL,1)
+	$(call gb_Helper_abbreviate_dirs, \
+		$(call gb_ExternalExecutable_get_command,xsltproc) --nonet --novalid -o $@ \
+			--stringparam build_type "$(BUILD_TYPE)" \
+			--stringparam os "$(OS)" \
+			--stringparam themes "$(WITH_THEMES)" \
+			$(if $(MPL_SUBSET),,--stringparam no_mpl_subset no_mpl_subset) \
+			$< \
+			$(readlicense_oo_LICENSE_xml) \
+		$(if $(filter WNT,$(OS)), \
+			&& $(gb_AWK) 'sub("$$","\r")' $@ > $@.tmp \
+			&& mv $@.tmp $@ \
+		) \
+	)
+
+$(readlicense_oo_DIR)/LICENSE : \
+		$(SRCDIR)/readlicense_oo/license/license_plain_text.xsl \
+		$(readlicense_oo_LICENSE_xml) \
+		| $(readlicense_oo_DIR)/.dir \
+		  $(call gb_ExternalExecutable_get_dependencies,xsltproc)
+	$(call gb_Output_announce,$(subst $(WORKDIR)/,,$@),$(true),XSL,1)
+	$(call gb_Helper_abbreviate_dirs, \
+		$(call gb_ExternalExecutable_get_command,xsltproc) --nonet --novalid -o $@ \
+			--stringparam build_type "$(BUILD_TYPE)" \
+			--stringparam os "$(OS)" \
+			--stringparam themes "$(WITH_THEMES)" \
+			$(if $(MPL_SUBSET),,--stringparam no_mpl_subset no_mpl_subset) \
+			$< \
+			$(readlicense_oo_LICENSE_xml) \
+	)
+
+ifeq ($(OS),WNT)
 $(readlicense_oo_DIR)/license.txt : \
-		$(SRCDIR)/readlicense_oo/license/LICENSE \
+		$(readlicense_oo_DIR)/LICENSE \
 		| $(readlicense_oo_DIR)/.dir
 	$(call gb_Output_announce,$(subst $(WORKDIR)/,,$@),$(true),AWK,1)
 	$(call gb_Helper_abbreviate_dirs, \
 		$(gb_AWK) 'sub("$$","\r")' $< > $@.tmp && mv $@.tmp $@ \
 	)
+endif
+
+$(readlicense_oo_DIR)/license.fodt : \
+		$(readlicense_oo_DIR)/license.html \
+		$(call gb_Postprocess_get_target,AllLibraries) \
+		$(call gb_Postprocess_get_target,AllExecutables) \
+		$(call gb_Postprocess_get_target,AllModuleTests) \
+		$(call gb_Postprocess_get_target,AllModuleSlowtests) \
+		| $(readlicense_oo_DIR)/.dir
+	$(call gb_Output_announce,$(subst $(WORKDIR)/,,$@),$(true),SOF,1)
+	$(INSTDIR)/$(if $(filter MACOSX,$(OS)),$(PRODUCTNAME_WITHOUT_SPACES).app/Contents/MacOS,program)/soffice$(if $(filter WNT,$(OS)),.exe) \
+		--convert-to fodt:"OpenDocument Text Flat XML" --outdir $(readlicense_oo_DIR) $<
 
 # vim:set shiftwidth=4 tabstop=4 noexpandtab:
