47dabf793e226c196f91a40d6b44c35d380a71ff
Caolán McNamara <caolanm@redhat.com>
2019-03-05 13:25:16 +0000

    Resolves: tdf#123815 null terminator included in string
    regression from...
    
    commit 18d636063fd7be165e7888af49372a6e2b851776
    Author: Caolán McNamara <caolanm@redhat.com>
    Date:   Mon Jun 19 09:46:31 2017 +0100
    
        ofz: use OString instead of bare char*
    
    this is no use to the reporter though, as the document format is
    10 and 11 is the oldest the filter will open
    
    Change-Id: I4cdfe92e37f047493a6eab95f0e8e9f5dced1a35

--- lotuswordpro/source/filter/tocread.cxx
+++ lotuswordpro/source/filter/tocread.cxx
@@ -291,7 +291,9 @@ CBenTOCReader::ReadTOC()
                         return Err;
                     }
 
-                    OString sName(sBuffer, Length);
+                    OString sName;
+                    if (Length)
+                        sName = OString(sBuffer, Length - 1);
 
                     CUtListElmt * pPrevNamedObjectListElmt;
                     if (FindNamedObject(&cpContainer->GetNamedObjects(),
