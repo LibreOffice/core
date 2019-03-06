114176935b6fbb846e27955f59d730663486f755
Katarina Behrens <Katarina.Behrens@cib.de>
2019-03-06 17:22:21 +0100

    Tentative fix of tagged PDF export of artifacts
    quote PDF standard, chapter 14.8.2.2.2:
    " ... to aid in text reflow, artifacts should be defined with property
    lists whenever possible."
    
    So instead of 'Artifact BMC ... EMC', we export
    'Artifact BDC propertyList ... EMC' where property list is empty
    
    This is also how an example at
    https://www.w3.org/WAI/WCAG20/Techniques/working-examples/PDF4/decorative-image.pdf
    implements things
    
    Change-Id: I06e9a9119dd28ee80136393528fdd59d75f16951

--- vcl/source/gdi/pdfwriter_impl.cxx
+++ vcl/source/gdi/pdfwriter_impl.cxx
@@ -10750,7 +10750,7 @@ void PDFWriterImpl::beginStructureElementMCSeq()
              )
     {
         OStringBuffer aLine( 128 );
-        aLine.append( "/Artifact BMC\n" );
+        aLine.append( "/Artifact <<>>BDC\n" );
         writeBuffer( aLine.getStr(), aLine.getLength() );
         // mark element MC sequence as open
         m_aStructure[ m_nCurrentStructElement ].m_bOpenMCSeq = true;
