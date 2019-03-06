LibreOffice/core

commit fe3d5766fa3c42f6cf8d1ea47af820e0b1c1cf48

tdf#116195 swap a compatibility value
There was a minor bug in the compat flag, leading
to a regression from commit 8d62b79f168180c6992eb483ec864d473050635f

Change-Id: I1e468e665a583ef15b6e474c3adb32f1dcf98f46
Reviewed-on: https://gerrit.libreoffice.org/52674
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>

sw/source/filter/xml/xmlimp.cxx
@@ -1620,7 +1620,7 @@ void SwXMLImport::SetConfigurationSettings(const Sequence < PropertyValue > & aC
     if (!bSubtractFlysAnchoredAtFlys)
         xProps->setPropertyValue("SubtractFlysAnchoredAtFlys", makeAny(true));
 
-    if ( !bDisableOffPagePositioning )
+    if ( bDisableOffPagePositioning )
         xProps->setPropertyValue("DisableOffPagePositioning", makeAny(true));
 
     SwDoc *pDoc = getDoc();

sw/qa/extras/odfimport/odfimport.cxx
@@ -902,5 +902,13 @@ DECLARE_ODFIMPORT_TEST(testTdf108482, "tdf108482.odt")
     );
 }
 
+DECLARE_ODFIMPORT_TEST(testTdf116195, "tdf116195.odt")
+{
+    // The image was set to zero height due to a regression
+    CPPUNIT_ASSERT_EQUAL(
+        sal_Int32(12960), parseDump("/root/page/anchored/fly/notxt/infos/bounds", "height").toInt32()
+    );
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

commit 03376d1cf70894cba22cc4381260ce67a62066c6

tdf#116195 sw: remove DisableOffPagePositioning handling from ODF import
SwXMLImport::SetConfigurationSettings() needs to handle only those
settings for which the default is different vs. old OOo/LO versions;
DisableOffPagePositioning however defaults to false, so we can just rely
on that default.  The inverted bDisableOffPagePositioning check is
wrong.

(regression from fe3d5766fa3c42f6cf8d1ea47af820e0b1c1cf48)

Change-Id: I300fa597f58b586b49089bb555a00a6923862abe

sw/source/filter/xml/xmlimp.cxx
@@ -1356,7 +1356,6 @@ void SwXMLImport::SetConfigurationSettings(const Sequence < PropertyValue > & aC
     aExcludeWhenNotLoadingUserSettings.insert("TabOverMargin");
     aExcludeWhenNotLoadingUserSettings.insert("PropLineSpacingShrinksFirstLine");
     aExcludeWhenNotLoadingUserSettings.insert("SubtractFlysAnchoredAtFlys");
-    aExcludeWhenNotLoadingUserSettings.insert("DisableOffPagePositioning");
 
     sal_Int32 nCount = aConfigProps.getLength();
     const PropertyValue* pValues = aConfigProps.getConstArray();
@@ -1391,7 +1390,6 @@ void SwXMLImport::SetConfigurationSettings(const Sequence < PropertyValue > & aC
     bool bTabOverMarginValue = false;
     bool bPropLineSpacingShrinksFirstLine = false;
     bool bSubtractFlysAnchoredAtFlys = false;
-    bool bDisableOffPagePositioning = false;
 
     const PropertyValue* currentDatabaseDataSource = nullptr;
     const PropertyValue* currentDatabaseCommand = nullptr;
@@ -1487,8 +1485,6 @@ void SwXMLImport::SetConfigurationSettings(const Sequence < PropertyValue > & aC
                     bPropLineSpacingShrinksFirstLine = true;
                 else if (pValues->Name == "SubtractFlysAnchoredAtFlys")
                     bSubtractFlysAnchoredAtFlys = true;
-                else if (pValues->Name == "DisableOffPagePositioning")
-                    bDisableOffPagePositioning = true;
             }
             catch( Exception& )
             {
@@ -1656,9 +1652,6 @@ void SwXMLImport::SetConfigurationSettings(const Sequence < PropertyValue > & aC
 
     if (!bSubtractFlysAnchoredAtFlys)
         xProps->setPropertyValue("SubtractFlysAnchoredAtFlys", makeAny(true));
-
-    if ( bDisableOffPagePositioning )
-        xProps->setPropertyValue("DisableOffPagePositioning", makeAny(true));
 
     SwDoc *pDoc = getDoc();
     SfxPrinter *pPrinter = pDoc->getIDocumentDeviceAccess().getPrinter( false );