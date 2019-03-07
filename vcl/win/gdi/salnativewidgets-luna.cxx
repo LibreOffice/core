LibreOffice/core

commit b160db926b574b7e9d6696d49dbbce8dd289aade

tdf#96947 vcl opengl win: fix background of menu items w/ check/radio marks
Extract the actually used bounding box from the rendering code and reuse
that for OpenGL texture purposes, so the available and the used size
can't mismatch.

The background itself is almost invisible, but the borders were lost due
to this size difference.

Change-Id: I31b3e5aa0c0106461b90bfaab3a84d45b33afd71
Reviewed-on: https://gerrit.libreoffice.org/54298
Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
Tested-by: Jenkins <ci@libreoffice.org>

vcl/win/gdi/salnativewidgets-luna.cxx
@@ -498,6 +498,31 @@ static void impl_drawAeroToolbar( HDC hDC, RECT rc, bool bHorizontal )
     }
 }
 
+/**
+ * Gives the actual rectangle used for rendering by ControlType::MenuPopup's
+ * ControlPart::MenuItemCheckMark or ControlPart::MenuItemRadioMark.
+ */
+static tools::Rectangle GetMenuPopupMarkRegion(const ImplControlValue& rValue)
+{
+    tools::Rectangle aRet;
+
+    const MenupopupValue& rMVal(static_cast<const MenupopupValue&>(rValue));
+    aRet.SetTop(rMVal.maItemRect.Top());
+    aRet.SetBottom(rMVal.maItemRect.Bottom() + 1); // see below in drawNativeControl
+    if (AllSettings::GetLayoutRTL())
+    {
+        aRet.SetRight(rMVal.maItemRect.Right() + 1);
+        aRet.SetLeft(aRet.Right() - (rMVal.getNumericVal() - rMVal.maItemRect.Left()));
+    }
+    else
+    {
+        aRet.SetRight(rMVal.getNumericVal());
+        aRet.SetLeft(rMVal.maItemRect.Left());
+    }
+
+    return aRet;
+}
+
 bool ImplDrawNativeControl( HDC hDC, HTHEME hTheme, RECT rc,
                             ControlType nType,
                             ControlPart nPart,
@@ -1088,19 +1113,11 @@ bool ImplDrawNativeControl( HDC hDC, HTHEME hTheme, RECT rc,
                     RECT aBGRect = rc;
                     if( aValue.getType() == ControlType::MenuPopup )
                     {
-                        const MenupopupValue& rMVal( static_cast<const MenupopupValue&>(aValue) );
-                        aBGRect.top    = rMVal.maItemRect.Top();
-                        aBGRect.bottom = rMVal.maItemRect.Bottom()+1; // see below in drawNativeControl
-                        if( AllSettings::GetLayoutRTL() )
-                        {
-                            aBGRect.right = rMVal.maItemRect.Right()+1;
-                            aBGRect.left = aBGRect.right - (rMVal.getNumericVal()-rMVal.maItemRect.Left());
-                        }
-                        else
-                        {
-                            aBGRect.right = rMVal.getNumericVal();
-                            aBGRect.left  = rMVal.maItemRect.Left();
-                        }
+                        tools::Rectangle aRectangle = GetMenuPopupMarkRegion(aValue);
+                        aBGRect.top = aRectangle.Top();
+                        aBGRect.left = aRectangle.Left();
+                        aBGRect.bottom = aRectangle.Bottom();
+                        aBGRect.right = aRectangle.Right();
                         rc = aBGRect;
                     }
                     iState = (nState & ControlState::ENABLED) ? MCB_NORMAL : MCB_DISABLED;
@@ -1163,6 +1180,14 @@ bool WinSalGraphics::drawNativeControl( ControlType nType,
             keySize = rNativeBoundingRegion.GetSize();
         }
     }
+
+    if (pImpl && nType == ControlType::MenuPopup && (nPart == ControlPart::MenuItemCheckMark || nPart == ControlPart::MenuItemRadioMark))
+    {
+        cacheRect = GetMenuPopupMarkRegion(aValue);
+        buttonRect = cacheRect;
+        keySize = cacheRect.GetSize();
+    }
+
 
     ControlCacheKey aControlCacheKey(nType, nPart, nState, keySize);
     if (pImpl != nullptr && pImpl->TryRenderCachedNativeControl(aControlCacheKey, buttonRect.Left(), buttonRect.Top()))

commit 236a517c11418c2aa3a6bf3bbe6359f4e2fe59eb

tdf#119947 vcl opengl win: fix fontwork popup menu
Regression from commit b160db926b574b7e9d6696d49dbbce8dd289aade
(tdf#96947 vcl opengl win: fix background of menu items w/ check/radio
marks, 2018-05-14), the problem was that GetMenuPopupMarkRegion() did an
unconditional downcast from ImplControlValue to MenupopupValue.  This
looked safe, but when the extracted function was used in
WinSalGraphics::drawNativeControl(), then that argument was not safe to
downcast.

Fix the problem by switching to safe downcast, and returning an empty
rectangle (instead of a bogus one) on error.

This way the old bug stays fixed but fontwork popup menu works again as
well.

Change-Id: I2b69b9f2e6823850892ac73df78dac5c2f04ec03

vcl/win/gdi/salnativewidgets-luna.cxx
@@ -496,18 +496,21 @@ static tools::Rectangle GetMenuPopupMarkRegion(const ImplControlValue& rValue)
 {
     tools::Rectangle aRet;
 
-    const MenupopupValue& rMVal(static_cast<const MenupopupValue&>(rValue));
-    aRet.SetTop(rMVal.maItemRect.Top());
-    aRet.SetBottom(rMVal.maItemRect.Bottom() + 1); // see below in drawNativeControl
+    auto pMVal = dynamic_cast<const MenupopupValue*>(&rValue);
+    if (!pMVal)
+        return aRet;
+
+    aRet.SetTop(pMVal->maItemRect.Top());
+    aRet.SetBottom(pMVal->maItemRect.Bottom() + 1); // see below in drawNativeControl
     if (AllSettings::GetLayoutRTL())
     {
-        aRet.SetRight(rMVal.maItemRect.Right() + 1);
-        aRet.SetLeft(aRet.Right() - (rMVal.getNumericVal() - rMVal.maItemRect.Left()));
+        aRet.SetRight(pMVal->maItemRect.Right() + 1);
+        aRet.SetLeft(aRet.Right() - (pMVal->getNumericVal() - pMVal->maItemRect.Left()));
     }
     else
     {
-        aRet.SetRight(rMVal.getNumericVal());
-        aRet.SetLeft(rMVal.maItemRect.Left());
+        aRet.SetRight(pMVal->getNumericVal());
+        aRet.SetLeft(pMVal->maItemRect.Left());
     }
 
     return aRet;
@@ -1173,9 +1176,13 @@ bool WinSalGraphics::drawNativeControl( ControlType nType,
 
     if (pImpl && nType == ControlType::MenuPopup && (nPart == ControlPart::MenuItemCheckMark || nPart == ControlPart::MenuItemRadioMark))
     {
-        cacheRect = GetMenuPopupMarkRegion(aValue);
-        buttonRect = cacheRect;
-        keySize = cacheRect.GetSize();
+        tools::Rectangle aRectangle = GetMenuPopupMarkRegion(aValue);
+        if (!aRectangle.IsEmpty())
+        {
+            cacheRect = GetMenuPopupMarkRegion(aValue);
+            buttonRect = cacheRect;
+            keySize = cacheRect.GetSize();
+        }
     }
 
 