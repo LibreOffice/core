11eb46f5ae705fc14ae48121a9961bf990600608
Khaled Hosny <khaledhosny@eglug.org>
2019-03-08 14:56:15 +0200

    Don'tbuild HarfBuzz with Core Text support on macOS/iOS
    HarfBuzz 2.x has native support for AAT fonts which is, according to Chrome
    developers, sgnificantly faster that HarfBuzz Core Text integration.
    
    Change-Id: I4d5e861a1958402a6e3ccb720b10f40828c3db6a

--- external/harfbuzz/ExternalProject_harfbuzz.mk
+++ external/harfbuzz/ExternalProject_harfbuzz.mk
@@ -38,7 +38,6 @@ $(call gb_ExternalProject_get_state_target,harfbuzz,build) :
 			--with-glib=no \
 			--with-ucdn=no \
 			--with-graphite2=yes \
-			$(if $(filter iOS MACOSX,$(OS)),--with-coretext=yes) \
 			$(if $(verbose),--disable-silent-rules,--enable-silent-rules) \
 			--libdir=$(call gb_UnpackedTarball_get_dir,harfbuzz/src/.libs) \
 			$(if $(CROSS_COMPILING),--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM)) \
