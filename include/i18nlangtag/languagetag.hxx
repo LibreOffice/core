4c67df150f8d0f1285c8090a8c00e285fb947c4a
Noel Grandin <noel.grandin@collabora.co.uk>
2019-03-08 10:37:51 +0200

    out of line some heavily repeated functions
    Change-Id: Icd9f7ebab89d6c2b166b6f42fd7682e89127fd51
    Reviewed-on: https://gerrit.libreoffice.org/68903
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

--- include/i18nlangtag/languagetag.hxx
+++ include/i18nlangtag/languagetag.hxx
@@ -107,6 +107,8 @@ public:
      */
     explicit LanguageTag( const rtl_Locale & rLocale );
 
+    ~LanguageTag();
+
     /** Obtain BCP 47 language tag.
 
         @param bResolveSystem
--- include/vcl/ITiledRenderable.hxx
+++ include/vcl/ITiledRenderable.hxx
@@ -31,56 +31,7 @@ namespace vcl
      * by css, it might turn out to be worth mapping some of these missing cursors
      * to available cursors?
      */
-#ifdef _MSC_VER
-#pragma warning(push)
-#pragma warning( disable : 4592)
-#endif
-    static const std::map <PointerStyle, OString> gaLOKPointerMap {
-    { PointerStyle::Arrow, "default" },
-    // PointerStyle::Null ?
-    { PointerStyle::Wait, "wait" },
-    { PointerStyle::Text, "text" },
-    { PointerStyle::Help, "help" },
-    { PointerStyle::Cross, "crosshair" },
-    { PointerStyle::Fill, "fill" },
-    { PointerStyle::Move, "move" },
-    { PointerStyle::NSize, "n-resize" },
-    { PointerStyle::SSize, "s-resize" },
-    { PointerStyle::WSize, "w-resize" },
-    { PointerStyle::ESize, "e-resize" },
-    { PointerStyle::NWSize, "ne-resize" },
-    { PointerStyle::NESize, "ne-resize" },
-    { PointerStyle::SWSize, "sw-resize" },
-    { PointerStyle::SESize, "se-resize" },
-    // WindowNSize through WindowSESize
-    { PointerStyle::HSplit, "col-resize" },
-    { PointerStyle::VSplit, "row-resize" },
-    { PointerStyle::HSizeBar, "col-resize" },
-    { PointerStyle::VSizeBar, "row-resize" },
-    { PointerStyle::Hand, "grab" },
-    { PointerStyle::RefHand, "pointer" },
-    // Pen, Magnify, Fill, Rotate
-    // HShear, VShear
-    // Mirror, Crook, Crop, MovePoint, MoveBezierWeight
-    // MoveData
-    { PointerStyle::CopyData, "copy" },
-    { PointerStyle::LinkData, "alias" },
-    // MoveDataLink, CopyDataLink
-    //MoveFile, CopyFile, LinkFile
-    // MoveFileLink, CopyFileLink, MoveFiless, CopyFiles
-    { PointerStyle::NotAllowed, "not-allowed" },
-    // DrawLine through DrawCaption
-    // Chart, Detective, PivotCol, PivotRow, PivotField, Chain, ChainNotAllowed
-    // TimeEventMove, TimeEventSize
-    // AutoScrollN through AutoScrollNSWE
-    // Airbrush
-    { PointerStyle::TextVertical, "vertical-text" }
-    // Pivot Delete, TabSelectS through TabSelectSW
-    // PaintBrush, HideWhiteSpace, ShowWhiteSpace
-    };
-#ifdef _MSC_VER
-#pragma warning(pop)
-#endif
+    extern const std::map <PointerStyle, OString> gaLOKPointerMap;
 
 
 class VCL_DLLPUBLIC ITiledRenderable
