LibreOffice/core

commit 173069a1f6ac060a06f04908a94a1a8420461795

tdf#120711 fix paragraph join with change tracking
a DOC import regression caused by "tdf#119571 change
tracking: show layout changes at paragraph join"
(commit 1bbbe57dfc0b43d6b5444798d77dcdf5e4e76e49)

Change-Id: Id9c1efd4f4133a5b1860f33182312f95f5459da0
Reviewed-on: https://gerrit.libreoffice.org/63680
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>
(cherry picked from commit 836cbf3680afd9198abf3a64be698f8fe256dd5a)
Reviewed-on: https://gerrit.libreoffice.org/63699

sw/source/core/doc/DocumentRedlineManager.cxx
@@ -1835,7 +1835,7 @@ DocumentRedlineManager::AppendRedline(SwRangeRedline* pNewRedl, bool const bCall
                     if (pDelNode != nullptr && pTextNode != nullptr && pDelNode != pTextNode)
                         pTextNode->CopyCollFormat( *pDelNode );
                 }
-                else
+                else if ( bCallDelete && nsRedlineType_t::REDLINE_DELETE == pNewRedl->GetType() )
                 {
                     // tdf#119571 update the style of the joined paragraph
                     // after a partially deleted paragraph to show its correct style

sw/qa/extras/ww8export/ww8export3.cxx
@@ -158,6 +158,13 @@ DECLARE_WW8EXPORT_TEST(testTdf94009_zeroPgMargin, "tdf94009_zeroPgMargin.odt")
     CPPUNIT_ASSERT_EQUAL(sal_Int32(0), getProperty<sal_Int32>(defaultStyle, "TopMargin"));
 }
 
+DECLARE_WW8EXPORT_TEST(testTdf120711_joinedParagraphWithChangeTracking, "tdf120711.doc")
+{
+    sal_Int16   numFormat = getNumberingTypeOfParagraph(5);
+    // last paragraph is not a list item
+    CPPUNIT_ASSERT(style::NumberingType::CHAR_SPECIAL != numFormat);
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

commit c67d6b51168b56edc21ce87d993f2197e1575b25

tdf#123054 track changes: fix paragraph style regression
caused by the commit 173069a1f6ac060a06f04908a94a1a8420461795
"fix paragraph join with change tracking" and commit
1bbbe57dfc0b43d6b5444798d77dcdf5e4e76e49 "change tracking:
show layout changes at paragraph join".

Change-Id: I05203255b04094ae118fd4967398045d6e0ad9fa

sw/source/core/doc/DocumentRedlineManager.cxx
@@ -1916,37 +1916,40 @@ DocumentRedlineManager::AppendRedline(SwRangeRedline* pNewRedl, bool const bCall
             }
             else
             {
-                if (pStt->nContent == 0)
+                if ( bCallDelete && nsRedlineType_t::REDLINE_DELETE == pNewRedl->GetType() )
                 {
-                    // tdf#54819 to keep the style of the paragraph
-                    // after the fully deleted paragraphs (normal behaviour
-                    // of editing without change tracking), we copy its style
-                    // to the first removed paragraph.
-                    SwTextNode* pDelNode = pStt->nNode.GetNode().GetTextNode();
-                    SwTextNode* pTextNode = pEnd->nNode.GetNode().GetTextNode();
-                    if (pDelNode != nullptr && pTextNode != nullptr && pDelNode != pTextNode)
-                        pTextNode->CopyCollFormat( *pDelNode );
-                }
-                else if ( bCallDelete && nsRedlineType_t::REDLINE_DELETE == pNewRedl->GetType() )
-                {
-                    // tdf#119571 update the style of the joined paragraph
-                    // after a partially deleted paragraph to show its correct style
-                    // in "Show changes" mode, too. All removed paragraphs
-                    // get the style of the first (partially deleted) paragraph
-                    // to avoid text insertion with bad style in the deleted
-                    // area later.
-                    SwContentNode* pDelNd = pStt->nNode.GetNode().GetContentNode();
-                    SwContentNode* pTextNd = pEnd->nNode.GetNode().GetContentNode();
-                    SwTextNode* pDelNode = pStt->nNode.GetNode().GetTextNode();
-                    SwTextNode* pTextNode;
-                    SwNodeIndex aIdx( pEnd->nNode.GetNode() );
-
-                    while (pDelNode != nullptr && pTextNd != nullptr && pDelNd->GetIndex() < pTextNd->GetIndex())
+                    if ( pStt->nContent == 0 )
                     {
-                        pTextNode = pTextNd->GetTextNode();
-                        if (pTextNode && pDelNode != pTextNode )
-                            pDelNode->CopyCollFormat( *pTextNode );
-                        pTextNd = SwNodes::GoPrevious( &aIdx );
+                        // tdf#54819 to keep the style of the paragraph
+                        // after the fully deleted paragraphs (normal behaviour
+                        // of editing without change tracking), we copy its style
+                        // to the first removed paragraph.
+                        SwTextNode* pDelNode = pStt->nNode.GetNode().GetTextNode();
+                        SwTextNode* pTextNode = pEnd->nNode.GetNode().GetTextNode();
+                        if (pDelNode != nullptr && pTextNode != nullptr && pDelNode != pTextNode)
+                            pTextNode->CopyCollFormat( *pDelNode );
+                    }
+                    else
+                    {
+                        // tdf#119571 update the style of the joined paragraph
+                        // after a partially deleted paragraph to show its correct style
+                        // in "Show changes" mode, too. All removed paragraphs
+                        // get the style of the first (partially deleted) paragraph
+                        // to avoid text insertion with bad style in the deleted
+                        // area later.
+                        SwContentNode* pDelNd = pStt->nNode.GetNode().GetContentNode();
+                        SwContentNode* pTextNd = pEnd->nNode.GetNode().GetContentNode();
+                        SwTextNode* pDelNode = pStt->nNode.GetNode().GetTextNode();
+                        SwTextNode* pTextNode;
+                        SwNodeIndex aIdx( pEnd->nNode.GetNode() );
+
+                        while (pDelNode != nullptr && pTextNd != nullptr && pDelNd->GetIndex() < pTextNd->GetIndex())
+                        {
+                            pTextNode = pTextNd->GetTextNode();
+                            if (pTextNode && pDelNode != pTextNode )
+                                pDelNode->CopyCollFormat( *pTextNode );
+                            pTextNd = SwNodes::GoPrevious( &aIdx );
+                        }
                     }
                 }
                 bool const ret = mpRedlineTable->Insert( pNewRedl );