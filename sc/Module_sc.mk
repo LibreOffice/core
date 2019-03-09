a5ceae59b50aa94688b8886b87d5a6d8ec7c4a78
Jens Carl <j.carl43@gmx.de>
2019-03-09 02:58:56 +0000

    tdf#45904 Move XEnumerationAccess Java test to C++
    Move XEnumerationAccess Java test to C++ for ScChartsObj.
    
    Change-Id: Ia9afb9fcaa5b81b140f47f309166f09af779ca52

--- sc/Module_sc.mk
+++ sc/Module_sc.mk
@@ -118,6 +118,7 @@ $(eval $(call gb_Module_add_subsequentcheck_targets,sc,\
 	CppunitTest_sc_cellsenumeration \
 	CppunitTest_sc_chart2dataprovider \
 	CppunitTest_sc_chartobj \
+	CppunitTest_sc_chartsobj \
 	CppunitTest_sc_check_data_pilot_field \
 	CppunitTest_sc_check_data_pilot_table \
 	CppunitTest_sc_check_xcell_ranges_query \
