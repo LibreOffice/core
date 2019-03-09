12fb6e9e1a0ccdab412832a9e9a7e0cdd7c312be
Jens Carl <j.carl43@gmx.de>
2019-03-09 02:58:56 +0000

    tdf#45904 Move XEnumerationAccess Java test to C++
    Move XEnumerationAccess Java test to C++ for ScChartsObj.
    
    Change-Id: Ia9afb9fcaa5b81b140f47f309166f09af779ca52
    Reviewed-on: https://gerrit.libreoffice.org/68953
    Tested-by: Jenkins
    Reviewed-by: Jens Carl <j.carl43@gmx.de>

---
+++ sc/CppunitTest_sc_chartsobj.mk
@@ -0,0 +1,44 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#*************************************************************************
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+#*************************************************************************
+$(eval $(call gb_CppunitTest_CppunitTest,sc_chartsobj))
+
+$(eval $(call gb_CppunitTest_use_external,sc_chartsobj,boost_headers))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,sc_chartsobj, \
+	sc/qa/extras/scchartsobj \
+))
+
+$(eval $(call gb_CppunitTest_use_libraries,sc_chartsobj, \
+	comphelper \
+	cppu \
+	cppuhelper \
+	sal \
+	subsequenttest \
+	test \
+	unotest \
+))
+
+$(eval $(call gb_CppunitTest_set_include,sc_chartsobj,\
+	$$(INCLUDE) \
+))
+
+$(eval $(call gb_CppunitTest_use_sdk_api,sc_chartsobj))
+
+$(eval $(call gb_CppunitTest_use_ure,sc_chartsobj))
+$(eval $(call gb_CppunitTest_use_vcl,sc_chartsobj))
+
+$(eval $(call gb_CppunitTest_use_rdb,sc_chartsobj,services))
+
+$(eval $(call gb_CppunitTest_use_components,sc_chartsobj))
+
+$(eval $(call gb_CppunitTest_use_configuration,sc_chartsobj))
+
+# vim: set noet sw=4 ts=4:
---
+++ sc/qa/extras/scchartsobj.cxx
@@ -0,0 +1,159 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <test/calc_unoapi_test.hxx>
+#include <test/container/xenumerationaccess.hxx>
+
+#include <com/sun/star/awt/Rectangle.hpp>
+#include <com/sun/star/container/XIndexAccess.hpp>
+#include <com/sun/star/lang/XComponent.hpp>
+#include <com/sun/star/sheet/XCellRangeAddressable.hpp>
+#include <com/sun/star/sheet/XSpreadsheet.hpp>
+#include <com/sun/star/sheet/XSpreadsheetDocument.hpp>
+#include <com/sun/star/sheet/XSpreadsheets.hpp>
+#include <com/sun/star/table/CellRangeAddress.hpp>
+#include <com/sun/star/table/XCell.hpp>
+#include <com/sun/star/table/XCellRange.hpp>
+#include <com/sun/star/table/XTableCharts.hpp>
+#include <com/sun/star/table/XTableChartsSupplier.hpp>
+#include <com/sun/star/uno/XInterface.hpp>
+
+#include <com/sun/star/uno/Reference.hxx>
+#include <com/sun/star/uno/Sequence.hxx>
+
+using namespace css;
+
+namespace sc_apitest
+{
+class ScChartsObj : public CalcUnoApiTest, public apitest::XEnumerationAccess
+{
+public:
+    ScChartsObj();
+
+    virtual uno::Reference<uno::XInterface> init() override;
+    virtual void setUp() override;
+    virtual void tearDown() override;
+
+    CPPUNIT_TEST_SUITE(ScChartsObj);
+
+    // XEnumerationAccess
+    CPPUNIT_TEST(testCreateEnumeration);
+
+    CPPUNIT_TEST_SUITE_END();
+
+private:
+    uno::Reference<lang::XComponent> m_xComponent;
+};
+
+ScChartsObj::ScChartsObj()
+    : CalcUnoApiTest("/sc/qa/extras/testdocuments")
+{
+}
+
+uno::Reference<uno::XInterface> ScChartsObj::init()
+{
+    uno::Reference<sheet::XSpreadsheetDocument> xDoc(m_xComponent, uno::UNO_QUERY_THROW);
+    uno::Reference<sheet::XSpreadsheets> xSheets(xDoc->getSheets(), uno::UNO_QUERY_THROW);
+    uno::Reference<container::XIndexAccess> xIA(xSheets, uno::UNO_QUERY_THROW);
+    uno::Reference<sheet::XSpreadsheet> xSheet0(xIA->getByIndex(0), uno::UNO_QUERY_THROW);
+
+    xSheet0->getCellByPosition(1, 0)->setFormula("JAN");
+    xSheet0->getCellByPosition(2, 0)->setFormula("FEB");
+    xSheet0->getCellByPosition(3, 0)->setFormula("MAR");
+    xSheet0->getCellByPosition(4, 0)->setFormula("APR");
+    xSheet0->getCellByPosition(5, 0)->setFormula("MAY");
+    xSheet0->getCellByPosition(6, 0)->setFormula("JUN");
+    xSheet0->getCellByPosition(7, 0)->setFormula("JUL");
+    xSheet0->getCellByPosition(8, 0)->setFormula("AUG");
+    xSheet0->getCellByPosition(9, 0)->setFormula("SEP");
+    xSheet0->getCellByPosition(10, 0)->setFormula("OCT");
+    xSheet0->getCellByPosition(11, 0)->setFormula("NOV");
+    xSheet0->getCellByPosition(12, 0)->setFormula("DEC");
+    xSheet0->getCellByPosition(13, 0)->setFormula("SUM");
+
+    xSheet0->getCellByPosition(0, 1)->setFormula("Smith");
+    xSheet0->getCellByPosition(1, 1)->setValue(42);
+    xSheet0->getCellByPosition(2, 1)->setValue(58.9);
+    xSheet0->getCellByPosition(3, 1)->setValue(-66.5);
+    xSheet0->getCellByPosition(4, 1)->setValue(43.4);
+    xSheet0->getCellByPosition(5, 1)->setValue(44.5);
+    xSheet0->getCellByPosition(6, 1)->setValue(45.3);
+    xSheet0->getCellByPosition(7, 1)->setValue(-67.3);
+    xSheet0->getCellByPosition(8, 1)->setValue(30.5);
+    xSheet0->getCellByPosition(9, 1)->setValue(23.2);
+    xSheet0->getCellByPosition(10, 1)->setValue(-97.3);
+    xSheet0->getCellByPosition(11, 1)->setValue(22.4);
+    xSheet0->getCellByPosition(11, 1)->setValue(23.5);
+    xSheet0->getCellByPosition(13, 1)->setFormula("SUM(B2:M2");
+
+    xSheet0->getCellByPosition(0, 2)->setFormula("Jones");
+    xSheet0->getCellByPosition(1, 2)->setValue(21);
+    xSheet0->getCellByPosition(2, 2)->setValue(40.9);
+    xSheet0->getCellByPosition(3, 2)->setValue(-57.5);
+    xSheet0->getCellByPosition(4, 2)->setValue(-23.4);
+    xSheet0->getCellByPosition(5, 2)->setValue(34.5);
+    xSheet0->getCellByPosition(6, 2)->setValue(59.3);
+    xSheet0->getCellByPosition(7, 2)->setValue(27.3);
+    xSheet0->getCellByPosition(8, 2)->setValue(-38.5);
+    xSheet0->getCellByPosition(9, 2)->setValue(43.2);
+    xSheet0->getCellByPosition(10, 2)->setValue(57.3);
+    xSheet0->getCellByPosition(11, 2)->setValue(25.4);
+    xSheet0->getCellByPosition(11, 2)->setValue(28.5);
+    xSheet0->getCellByPosition(13, 2)->setFormula("SUM(B3:M3");
+
+    xSheet0->getCellByPosition(0, 3)->setFormula("Brown");
+    xSheet0->getCellByPosition(1, 3)->setValue(31.45);
+    xSheet0->getCellByPosition(2, 3)->setValue(-20.9);
+    xSheet0->getCellByPosition(3, 3)->setValue(-117.5);
+    xSheet0->getCellByPosition(4, 3)->setValue(23.4);
+    xSheet0->getCellByPosition(5, 3)->setValue(-114.5);
+    xSheet0->getCellByPosition(6, 3)->setValue(115.3);
+    xSheet0->getCellByPosition(7, 3)->setValue(-171.3);
+    xSheet0->getCellByPosition(8, 3)->setValue(89.5);
+    xSheet0->getCellByPosition(9, 3)->setValue(41.2);
+    xSheet0->getCellByPosition(10, 3)->setValue(71.3);
+    xSheet0->getCellByPosition(11, 3)->setValue(25.4);
+    xSheet0->getCellByPosition(11, 3)->setValue(38.5);
+    xSheet0->getCellByPosition(13, 3)->setFormula("SUM(A4:L4");
+
+    uno::Reference<table::XCellRange> xCellRange0(xSheet0, uno::UNO_QUERY_THROW);
+    uno::Reference<table::XCellRange> xCellRange1(xCellRange0->getCellRangeByName("A1:N4"),
+                                                  uno::UNO_QUERY_THROW);
+    uno::Reference<sheet::XCellRangeAddressable> xCRA(xCellRange1, uno::UNO_QUERY_THROW);
+
+    uno::Sequence<table::CellRangeAddress> aCRA(1);
+    aCRA[0] = xCRA->getRangeAddress();
+
+    uno::Reference<table::XTableChartsSupplier> xTCS(xSheet0, uno::UNO_QUERY_THROW);
+    uno::Reference<table::XTableCharts> xTC = xTCS->getCharts();
+    xTC->addNewByName("ScChartsObj", awt::Rectangle(500, 3000, 25000, 11000), aCRA, true, true);
+
+    return xTC;
+}
+
+void ScChartsObj::setUp()
+{
+    CalcUnoApiTest::setUp();
+    // create calc document
+    m_xComponent = loadFromDesktop("private:factory/scalc");
+}
+
+void ScChartsObj::tearDown()
+{
+    closeDocument(m_xComponent);
+    CalcUnoApiTest::tearDown();
+}
+
+CPPUNIT_TEST_SUITE_REGISTRATION(ScChartsObj);
+
+} // namespace sc_apitest
+
+CPPUNIT_PLUGIN_IMPLEMENT();
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
