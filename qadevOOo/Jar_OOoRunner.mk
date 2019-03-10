a2ae61ee38410579487f8d5729eb8348eece702d
Jens Carl <j.carl43@gmx.de>
2019-03-10 06:46:03 +0000

    tdf#45904 Move XNameAccess Java tests to C++
    Move XNameAccess Java tests to C++ for ScDataPilotFieldGroupObj
    and fixes i#46730.
    
    Change-Id: I4374774d52c93f381ccf9e71f735b0dab33bc2a4
    Reviewed-on: https://gerrit.libreoffice.org/68999
    Tested-by: Jenkins
    Reviewed-by: Jens Carl <j.carl43@gmx.de>

--- qadevOOo/Jar_OOoRunner.mk
+++ qadevOOo/Jar_OOoRunner.mk
@@ -960,7 +960,6 @@ $(eval $(call gb_Jar_add_sourcefiles,OOoRunner,\
     qadevOOo/tests/java/mod/_sc/ScCellSearchObj \
     qadevOOo/tests/java/mod/_sc/ScCellTextCursor \
     qadevOOo/tests/java/mod/_sc/ScDatabaseRangeObj \
-    qadevOOo/tests/java/mod/_sc/ScDataPilotFieldGroupObj \
     qadevOOo/tests/java/mod/_sc/ScDataPilotItemObj \
     qadevOOo/tests/java/mod/_sc/ScDocumentConfiguration \
     qadevOOo/tests/java/mod/_sc/ScDrawPageObj \
