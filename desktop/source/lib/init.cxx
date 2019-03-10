e44628e398bdf3c734b3079f11382a0fa4e34c05
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2019-03-10 12:21:33 -0400

    LOK: don't segfault when reporting error
    Change-Id: Ia6d614eb15b15f43ddd3e0b5742b888060dfd581
    Reviewed-on: https://gerrit.libreoffice.org/69069
    Reviewed-by: Andras Timar <andras.timar@collabora.com>
    Tested-by: Andras Timar <andras.timar@collabora.com>

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -3133,7 +3133,7 @@ static void doc_postUnoCommand(LibreOfficeKitDocument* pThis, const char* pComma
     else
         bResult = comphelper::dispatchCommand(aCommand, comphelper::containerToSequence(aPropertyValuesVector));
 
-    if (!bResult)
+    if (!bResult && gImpl)
     {
         SetLastExceptionMsg("Failed to dispatch the .uno: command");
     }
