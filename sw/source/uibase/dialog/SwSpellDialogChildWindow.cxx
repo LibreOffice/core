35027d2e9fff6e674609fe0659c1ebb8b79c2c1a
Vaibhav <vaibhavr@iitbhilai.ac.in>
2019-03-17 19:42:23 +0530

    tdf#86731: Add the dictionary not available message
    The patch enables a message or notification box which notifies
    the user with the message *No dictionary available*
    in case there is no dictionary available for any content.
    
    Change-Id: I61fa01ea469acb345637ab86da98a8de8f8a9762
    Reviewed-on: https://gerrit.libreoffice.org/69366
    Tested-by: Jenkins
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>

--- sw/source/uibase/dialog/SwSpellDialogChildWindow.cxx
+++ sw/source/uibase/dialog/SwSpellDialogChildWindow.cxx
@@ -406,10 +406,10 @@ The code below would only be part of the solution.
             }
         }
         bool bNoDictionaryAvailable = pWrtShell->GetDoc()->IsDictionaryMissing();
-        if( aRet.empty() && bCloseMessage && !bNoDictionaryAvailable )
+        if( aRet.empty() && bCloseMessage )
         {
             LockFocusNotification( true );
-            OUString sInfo( SwResId( STR_SPELLING_COMPLETED ) );
+            OUString sInfo( SwResId( bNoDictionaryAvailable ? STR_DICTIONARY_UNAVAILABLE : STR_SPELLING_COMPLETED ) );
             vcl::Window* pThisWindow = GetWindow();
             // #i84610#
             std::unique_ptr<weld::MessageDialog> xBox(
