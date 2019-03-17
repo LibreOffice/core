f6db1e2308d697ca4402e4b3c77078ded085e98e
Vaibhav <vaibhavr@iitbhilai.ac.in>
2019-03-17 19:42:23 +0530

    tdf#86731: Add the dictionary not available message
    The patch enables a message or notification box which notifies
    the user with the message *No dictionary available*
    in case there is no dictionary available for any content.
    
    Change-Id: I61fa01ea469acb345637ab86da98a8de8f8a9762

--- sw/source/uibase/dialog/SwSpellDialogChildWindow.cxx
+++ sw/source/uibase/dialog/SwSpellDialogChildWindow.cxx
@@ -405,24 +405,45 @@ The code below would only be part of the solution.
                     bCloseMessage = false; // no closing message if a wrap around has been denied
             }
         }
+        // bNoDictionaryAvailable returns true if no dictionary is available for any of the user's content 
         bool bNoDictionaryAvailable = pWrtShell->GetDoc()->IsDictionaryMissing();
-        if( aRet.empty() && bCloseMessage && !bNoDictionaryAvailable )
-        {
-            LockFocusNotification( true );
-            OUString sInfo( SwResId( STR_SPELLING_COMPLETED ) );
-            vcl::Window* pThisWindow = GetWindow();
-            // #i84610#
-            std::unique_ptr<weld::MessageDialog> xBox(
-                Application::CreateMessageDialog( pThisWindow->GetFrameWeld(),
-                                                  VclMessageType::Info,
-                                                  VclButtonsType::Ok,
-                                                  sInfo ) );
-            xBox->run();
-            LockFocusNotification( false );
-            // take care that the now valid selection is stored
-            LoseFocus();
-            if( pThisWindow )
-                pThisWindow->GrabFocus();
+        if( aRet.empty() && bCloseMessage )
+        {   
+            if(bNoDictionaryAvailable)
+            {
+                LockFocusNotification( true );
+                OUString sInfo( SwResId( "No dictionary is available" ) );
+                vcl::Window* pThisWindow = GetWindow();
+                std::unique_ptr<weld::MessageDialog> xBox(
+                    Application::CreateMessageDialog( pThisWindow->GetFrameWeld(),
+                                                      VclMessageType::Info,
+                                                      VclButtonsType::Ok,
+                                                      sInfo ) );
+                xBox->run();
+                LockFocusNotification( false );
+                // take care that the now valid selection is stored
+                LoseFocus();
+                if( pThisWindow )
+                    pThisWindow->GrabFocus();
+            }
+            else
+            {   
+                LockFocusNotification( true );
+                OUString sInfo( SwResId( STR_SPELLING_COMPLETED ) );
+                vcl::Window* pThisWindow = GetWindow();
+                // #i84610#
+                std::unique_ptr<weld::MessageDialog> xBox(
+                    Application::CreateMessageDialog( pThisWindow->GetFrameWeld(),
+                                                      VclMessageType::Info,
+                                                      VclButtonsType::Ok,
+                                                      sInfo ) );
+                xBox->run();
+                LockFocusNotification( false );
+                // take care that the now valid selection is stored
+                LoseFocus();
+                if( pThisWindow )
+                    pThisWindow->GrabFocus();
+            }
         }
     }
     return aRet;
