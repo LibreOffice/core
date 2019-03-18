LibreOffice/core

commit b2c1474c1dc93b69f0ede03fc5c9ab496c669955

SwTxtNode::IsIgnoredCharFmtForNumbering: ignore RES_CHRATR_COLOR
And also in SwTxtFormatter::NewNumberPortion(), use
SwTxtNode::IsIgnoredCharFmtForNumbering(), via
checkApplyParagraphMarkFormatToNumbering().  Otherwise the color of the
paragraph mark is inherited by the numbering portion, which is not what
IDocumentSettingAccess::APPLY_PARAGRAPH_MARK_FORMAT_TO_NUMBERING
(mirroring Word's behavior) is supposed to do.

Change-Id: I5d8df9b404916cc4a4405bf796d971ede59e6111

sw/source/core/text/txtfld.cxx
@@ -52,6 +52,7 @@
 #include "flddat.hxx"
 #include "fmtautofmt.hxx"
 #include <IDocumentSettingAccess.hxx>
+#include <svl/itemiter.hxx>
 
 static bool lcl_IsInBody( SwFrm *pFrm )
 {
@@ -427,7 +428,27 @@ static void checkApplyParagraphMarkFormatToNumbering( SwFont* pNumFnt, SwTxtForm
                 && hint->GetStart() == *hint->GetEnd() && hint->GetStart() == node->Len())
             {
                 boost::shared_ptr<SfxItemSet> pSet(hint->GetAutoFmt().GetStyleHandle());
-                pNumFnt->SetDiffFnt( pSet.get(), pIDSA );
+
+                // Check each item and in case it should be ignored, then clear it.
+                boost::shared_ptr<SfxItemSet> pCleanedSet;
+                if (pSet.get())
+                {
+                    pCleanedSet.reset(pSet->Clone());
+
+                    SfxItemIter aIter(*pSet);
+                    const SfxPoolItem* pItem = aIter.GetCurItem();
+                    while (true)
+                    {
+                        if (SwTxtNode::IsIgnoredCharFmtForNumbering(pItem->Which()))
+                            pCleanedSet->ClearItem(pItem->Which());
+
+                        if (aIter.IsAtEnd())
+                            break;
+
+                        pItem = aIter.NextItem();
+                    }
+                }
+                pNumFnt->SetDiffFnt(pCleanedSet.get(), pIDSA);
             }
         }
     }

sw/source/core/txtnode/thints.cxx
@@ -1781,7 +1781,7 @@ void SwTxtNode::DelSoftHyph( const sal_Int32 nStt, const sal_Int32 nEnd )
 
 bool SwTxtNode::IsIgnoredCharFmtForNumbering(const sal_uInt16 nWhich)
 {
-    return (nWhich ==  RES_CHRATR_UNDERLINE || nWhich == RES_CHRATR_BACKGROUND || nWhich == RES_CHRATR_ESCAPEMENT);
+    return (nWhich ==  RES_CHRATR_UNDERLINE || nWhich == RES_CHRATR_COLOR || nWhich == RES_CHRATR_BACKGROUND || nWhich == RES_CHRATR_ESCAPEMENT);
 }
 
 //In MS Word, following properties of the paragraph end position wont affect the formatting of bullets, so we ignore them:

sw/qa/extras/ooxmlexport/ooxmlexport.cxx
@@ -596,6 +596,9 @@ DECLARE_OOXMLEXPORT_TEST(testNumOverrideLvltext, "num-override-lvltext.docx")
     uno::Reference<container::XIndexAccess> xRules = getProperty< uno::Reference<container::XIndexAccess> >(getStyles("NumberingStyles")->getByName("WWNum1"), "NumberingRules");
     // This was 1, i.e. the numbering on the second level was "1", not "1.1".
     CPPUNIT_ASSERT_EQUAL(sal_Int16(2), comphelper::SequenceAsHashMap(xRules->getByIndex(1))["ParentNumbering"].get<sal_Int16>());
+
+    // The paragraph marker's red font color was inherited by the number portion, this was ff0000.
+    CPPUNIT_ASSERT_EQUAL(OUString("00000a"), parseDump("//Special[@nType='POR_NUMBER']/pFont", "color"));
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();

commit 42da575fb7e43d62f1d7386f08ecb7db695f2969

tdf#120548 sw ApplyParagraphMarkFormatToNumbering: fix handling of font color
Regression from commit b2c1474c1dc93b69f0ede03fc5c9ab496c669955
(SwTxtNode::IsIgnoredCharFmtForNumbering: ignore RES_CHRATR_COLOR,
2014-11-20), where the problem was that unconditionally ignoring or not
ignoring the font color is not correct.

Re-examining the test document from the above commit, it has an explicit
font color set in the numbering, while this bugdoc doesn't have it.

So make applying the paragraph mark font color to the numbering
conditional if the numbering already has a color set, this makes both
cases work correctly.

Change-Id: I43a6dec7d3a77689e2acbdc9d3671e79a9c4cac8

sw/source/core/text/txtfld.cxx
@@ -412,7 +412,9 @@ SwLinePortion *SwTextFormatter::NewExtraPortion( SwTextFormatInfo &rInf )
  * character than can be configured to be shown). However, in practice MSO also uses it as direct formatting
  * for numbering in that paragraph. I don't know if the problem is in the spec or in MSWord.
  */
-static void checkApplyParagraphMarkFormatToNumbering( SwFont* pNumFnt, SwTextFormatInfo& rInf, const IDocumentSettingAccess* pIDSA )
+static void checkApplyParagraphMarkFormatToNumbering(SwFont* pNumFnt, SwTextFormatInfo& rInf,
+                                                     const IDocumentSettingAccess* pIDSA,
+                                                     const SwAttrSet* pFormat)
 {
     if( !pIDSA->get(DocumentSettingId::APPLY_PARAGRAPH_MARK_FORMAT_TO_NUMBERING ))
         return;
@@ -446,6 +448,8 @@ static void checkApplyParagraphMarkFormatToNumbering( SwFont* pNumFnt, SwTextFor
                 while (true)
                 {
                     if (SwTextNode::IsIgnoredCharFormatForNumbering(pItem->Which()))
+                        pCleanedSet->ClearItem(pItem->Which());
+                    else if (pFormat && pFormat->HasItem(pItem->Which()))
                         pCleanedSet->ClearItem(pItem->Which());
 
                     if (aIter.IsAtEnd())
@@ -552,7 +556,7 @@ SwNumberPortion *SwTextFormatter::NewNumberPortion( SwTextFormatInfo &rInf ) con
                 if( pFormat )
                     pNumFnt->SetDiffFnt( pFormat, pIDSA );
 
-                checkApplyParagraphMarkFormatToNumbering( pNumFnt.get(), rInf, pIDSA );
+                checkApplyParagraphMarkFormatToNumbering(pNumFnt.get(), rInf, pIDSA, pFormat);
 
                 if ( pFormatFnt )
                 {
@@ -608,7 +612,7 @@ SwNumberPortion *SwTextFormatter::NewNumberPortion( SwTextFormatInfo &rInf ) con
                     if( pFormat )
                         pNumFnt->SetDiffFnt( pFormat, pIDSA );
 
-                    checkApplyParagraphMarkFormatToNumbering( pNumFnt.get(), rInf, pIDSA );
+                    checkApplyParagraphMarkFormatToNumbering(pNumFnt.get(), rInf, pIDSA, pFormat);
 
                     // we do not allow a vertical font
                     pNumFnt->SetVertical( pNumFnt->GetOrientation(), m_pFrame->IsVertical() );

sw/source/core/txtnode/thints.cxx
@@ -1760,7 +1760,8 @@ void SwTextNode::DelSoftHyph( const sal_Int32 nStt, const sal_Int32 nEnd )
 
 bool SwTextNode::IsIgnoredCharFormatForNumbering(const sal_uInt16 nWhich)
 {
-    return (nWhich ==  RES_CHRATR_UNDERLINE || nWhich == RES_CHRATR_COLOR || nWhich == RES_CHRATR_BACKGROUND || nWhich == RES_CHRATR_ESCAPEMENT);
+    return (nWhich == RES_CHRATR_UNDERLINE || nWhich == RES_CHRATR_BACKGROUND
+            || nWhich == RES_CHRATR_ESCAPEMENT);
 }
 
 //In MS Word, following properties of the paragraph end position won't affect the formatting of bullets, so we ignore them:

sw/qa/extras/ooxmlimport/ooxmlimport2.cxx
@@ -103,6 +103,14 @@ DECLARE_OOXMLIMPORT_TEST(testGroupShapeFontName, "groupshape-fontname.docx")
         getProperty<OUString>(getRun(getParagraphOfText(1, xText), 1), "CharFontNameAsian"));
 }
 
+DECLARE_OOXMLIMPORT_TEST(testTdf120548, "tdf120548.docx")
+{
+    // Without the accompanying fix in place, this test would have failed with 'Expected: 00ff0000;
+    // Actual: ffffffff', i.e. the numbering portion was black, not red.
+    CPPUNIT_ASSERT_EQUAL(OUString("00ff0000"),
+                         parseDump("//Special[@nType='PortionType::Number']/SwFont", "color"));
+}
+
 DECLARE_OOXMLIMPORT_TEST(test120551, "tdf120551.docx")
 {
     auto nHoriOrientPosition = getProperty<sal_Int32>(getShape(1), "HoriOrientPosition");