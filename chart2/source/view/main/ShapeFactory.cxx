72d3f9ebae436c73a7890454f095e65bbdc0b989
Mike Kaganski <mike.kaganski@collabora.com>
2019-03-19 21:27:20 +0300

    tdf#123504: 0 and 360 are different angles in charts
    This partly reverts commit 81302f33073e7629d724ed269f1fa21dad29e141.
    
    Change-Id: I40cbe739eb4497b8217aca56a2c3661ed1e491f4
    Reviewed-on: https://gerrit.libreoffice.org/69440
    Tested-by: Jenkins
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
    (cherry picked from commit 166a4989a0d1e5a6271c66bceb73a27970afc882)

--- chart2/source/view/main/ShapeFactory.cxx
+++ chart2/source/view/main/ShapeFactory.cxx
@@ -877,7 +877,11 @@ uno::Reference< drawing::XShape >
     if( !xTarget.is() )
         return nullptr;
 
-    fUnitCircleWidthAngleDegree = NormAngle360(fUnitCircleWidthAngleDegree);
+    // tdf#123504: both 0 and 360 are valid and different values here!
+    while (fUnitCircleWidthAngleDegree > 360)
+        fUnitCircleWidthAngleDegree -= 360.0;
+    while (fUnitCircleWidthAngleDegree < 0)
+        fUnitCircleWidthAngleDegree += 360.0;
 
     //create shape
     uno::Reference< drawing::XShape > xShape(
@@ -926,9 +930,10 @@ uno::Reference< drawing::XShape >
     if( !xTarget.is() )
         return nullptr;
 
-    while(fUnitCircleWidthAngleDegree>360)
+    // tdf#123504: both 0 and 360 are valid and different values here!
+    while (fUnitCircleWidthAngleDegree > 360)
         fUnitCircleWidthAngleDegree -= 360.0;
-    while(fUnitCircleWidthAngleDegree<0)
+    while (fUnitCircleWidthAngleDegree < 0)
         fUnitCircleWidthAngleDegree += 360.0;
 
     //create shape
