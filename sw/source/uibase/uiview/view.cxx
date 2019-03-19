f6bdfa3754744fafc6bee8a07d706897a487e035
Henry Castro <hcastro@collabora.com>
2019-03-19 16:17:09 -0400

    tdf#124162: Crash when trying to accept changes
    Reviewed-on: https://gerrit.libreoffice.org/69441
    Tested-by: Jenkins
    Reviewed-by: Aron Budea <aron.budea@collabora.com>
    (cherry picked from commit 2ac8d81a00bc9061f88dfd13a6a08873d92a5b86)
    
    Change-Id: If4a4034a891d6539b183f6083c0711b05ef7692d
    Reviewed-on: https://gerrit.libreoffice.org/69451
    Reviewed-by: Henry Castro <hcastro@collabora.com>
    Reviewed-by: Tor Lillqvist <tml@collabora.com>
    Tested-by: Tor Lillqvist <tml@collabora.com>

--- sw/source/uibase/uiview/view.cxx
+++ sw/source/uibase/uiview/view.cxx
@@ -1692,10 +1692,10 @@ void SwView::Notify( SfxBroadcaster& rBC, const SfxHint& rHint )
             case SfxHintId::RedlineChanged:
                 {
                     sal_uInt16 aSlotRedLine[] = {
-                        FN_REDLINE_NEXT_CHANGE,
-                        FN_REDLINE_PREV_CHANGE,
                         FN_REDLINE_ACCEPT_DIRECT,
                         FN_REDLINE_REJECT_DIRECT,
+                        FN_REDLINE_NEXT_CHANGE,
+                        FN_REDLINE_PREV_CHANGE,
                         FN_REDLINE_ACCEPT_ALL,
                         FN_REDLINE_REJECT_ALL,
                         0
