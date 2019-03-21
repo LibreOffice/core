1c45f8e2690bc40af39708e2590cb5253ca21112
heiko tietze <tietze.heiko@gmail.com>
2019-03-21 10:14:07 +0100

    tdf#124238 - Show a Tip-Of-The-Day dialog on startup
    New dialog
    
    todo:
    * makefile for resource
    * strings check-up
    * illustration images
    
    Change-Id: If1e501de26eb5a9c20a59e621f9e805c3b5e2cf8

--- include/vcl/weld.hxx
+++ include/vcl/weld.hxx
@@ -1071,6 +1071,7 @@ class VCL_DLLPUBLIC Image : virtual public Widget
 {
 public:
     virtual void set_from_icon_name(const OUString& rIconName) = 0;
+    virtual void set_image(VirtualDevice* pDevice) = 0;
 };
 
 class VCL_DLLPUBLIC Calendar : virtual public Widget
