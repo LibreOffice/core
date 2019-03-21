3526a576cd74ae33e6cc0722c91a5178f8886caf
Jan Holesovsky <kendy@collabora.com>
2019-03-21 14:18:35 +0100

    lok: Keep the temporary user profile set during the entire preload.
    Otherwise the core likes to crash due to the keyboard accellerators
    being read, but the configuration is missing.
    
    Change-Id: I94193f47462227b5975a49ce11709d7bf415a519
    Reviewed-on: https://gerrit.libreoffice.org/69507
    Reviewed-by: Andras Timar <andras.timar@collabora.com>
    Tested-by: Andras Timar <andras.timar@collabora.com>

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -4442,9 +4442,6 @@ static void preloadData()
     }
     std::cerr << "\n";
 
-    // Set user profile's path back to the original one
-    rtl::Bootstrap::set("UserInstallation", sUserPath);
-
     css::uno::Reference< css::ui::XAcceleratorConfiguration > xGlobalCfg;
     xGlobalCfg = css::ui::GlobalAcceleratorConfiguration::create(
         comphelper::getProcessComponentContext());
@@ -4484,6 +4481,9 @@ static void preloadData()
         nLang = MsLangId::resolveSystemLanguageByScriptType(LanguageTag::convertToLanguageType(aLocale, false), COMPLEX);
         OutputDevice::GetDefaultFont(DefaultFontType::CTL_SPREADSHEET, nLang, GetDefaultFontFlags::OnlyOne);
     }
+
+    // Set user profile's path back to the original one
+    rtl::Bootstrap::set("UserInstallation", sUserPath);
 }
 
 static int lo_initialize(LibreOfficeKit* pThis, const char* pAppPath, const char* pUserProfileUrl)
