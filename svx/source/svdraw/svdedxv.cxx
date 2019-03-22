8c5c911b67d9ae71e97f05a991534e20c8454977
Henry Castro <hcastro@collabora.com>
2019-03-22 11:56:20 -0400

    tdf#124167: Changing text properties doesn't make presentation...
    modified right away
    
    when the attributes of edit engine has changed also broadcast the
    changes
    
    Change-Id: I28c67a4167e0b5653b6e9b645069830aa5575412
    Reviewed-on: https://gerrit.libreoffice.org/69564
    Tested-by: Jenkins
    Reviewed-by: Henry Castro <hcastro@collabora.com>
    (cherry picked from commit dd8010c037a06763b3554739c01c9029988ad84f)

--- svx/source/svdraw/svdedxv.cxx
+++ svx/source/svdraw/svdedxv.cxx
@@ -2159,6 +2159,10 @@ bool SdrObjEditView::SetAttributes(const SfxItemSet& rSet, bool bReplaceAll)
             }
             pTextEditOutlinerView->SetAttribs(rSet);
 
+            Outliner* pTEOutliner = pTextEditOutlinerView->GetOutliner();
+            if (mpModel && pTEOutliner && pTEOutliner->IsModified())
+                mpModel->SetChanged();
+
 #ifdef DBG_UTIL
             if (mpItemBrowser!=nullptr)
                 mpItemBrowser->SetDirty();
