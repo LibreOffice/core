7d1e7649fc319ceadfd79990ecb48cf2773c7288
Greg Veldman <git@gregv.net>
2019-03-24 18:44:31 +0000

    In LibreOffice core commit d4d3766, several variables were changed to be declared as const.  However, on platforms not excluded by NO_PTHREAD_PRIORITY, osl_thread_priority_init_Impl() then tries later to assign values to these.  This leads to compiler errors on platforms so affected, including FreeBSD.  Revert as necessary on affected platforms, also using NO_PTHREAD_PRIORITY to match the code in osl_thread_priority_init_Impl().
    Change-Id: I0e968231e7c1be9771844222a7bab1f0fcb51a0e

--- sal/osl/unx/thread.cxx
+++ sal/osl/unx/thread.cxx
@@ -88,11 +88,11 @@ typedef struct osl_thread_impl_st
 
 struct osl_thread_priority_st
 {
-    int const m_Highest;
-    int const m_Above_Normal;
-    int const m_Normal;
-    int const m_Below_Normal;
-    int const m_Lowest;
+    int m_Highest;
+    int m_Above_Normal;
+    int m_Normal;
+    int m_Below_Normal;
+    int m_Lowest;
 };
 
 #define OSL_THREAD_PRIORITY_INITIALIZER { 127, 96, 64, 32, 0 }
@@ -110,7 +110,7 @@ static void osl_thread_textencoding_init_Impl();
 struct osl_thread_global_st
 {
     pthread_once_t                    m_once;
-    struct osl_thread_priority_st const m_priority;
+    struct osl_thread_priority_st     m_priority;
     struct osl_thread_textencoding_st m_textencoding;
 };
 
