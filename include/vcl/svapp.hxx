6b476080da0678faa59606ef814760bd4235de24
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2019-03-25 18:05:35 +0900

    tdf#124146 add (general) gesture event support to VCL
    Change-Id: I766930bb35071442e132b91477cd3d55e8f00f48
    Reviewed-on: https://gerrit.libreoffice.org/69655
    Tested-by: Jenkins
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

--- include/vcl/svapp.hxx
+++ include/vcl/svapp.hxx
@@ -63,6 +63,7 @@ class WorkWindow;
 class NotifyEvent;
 class KeyEvent;
 class MouseEvent;
+class GestureEvent;
 struct ImplSVEvent;
 struct ConvertData;
 
@@ -746,6 +747,8 @@ public:
      @param     pMouseEvent     Mouse event to send
     */
     static ImplSVEvent *        PostMouseEvent( VclEventId nEvent, vcl::Window *pWin, MouseEvent const * pMouseEvent );
+
+    static ImplSVEvent* PostGestureEvent(VclEventId nEvent, vcl::Window* pWin, GestureEvent const * pGestureEvent);
 
     /** Remove mouse and keypress events from a window... any also zoom and scroll events
      if the platform supports it.
