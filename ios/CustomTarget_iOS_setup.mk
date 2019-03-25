71b8eaf5a6f5f779d3c227801b16a635fd9c129c
Tor Lillqvist <tml@collabora.com>
2019-03-25 18:25:52 +0200

    Just include all fonts from instdir/share/fonts in the iOS app
    Change-Id: I7c98fb1cbf6523e39111c774a5ef3af20bba73b6

--- ios/CustomTarget_iOS_setup.mk
+++ ios/CustomTarget_iOS_setup.mk
@@ -51,7 +51,7 @@ $(IOSGEN)/native-code.h: $(BUILDDIR)/config_host.mk $(SRCDIR)/ios/CustomTarget_i
 	cp -R $(INSTDIR)/share/theme_definitions $(IOSRES)/share
 	cp -R $(INSTDIR)/share/template $(IOSRES)/share
 	mkdir -p $(IOSRES)/share/fonts/truetype
-	cp $(INSTDIR)/share/fonts/truetype/Liberation* $(IOSRES)/share/fonts/truetype
+	cp -R $(INSTDIR)/share/fonts/truetype $(IOSRES)/share/fonts
 
 	# Set up rc, the "inifile". See getIniFileName_Impl().
 	echo '[Bootstrap]' > $(IOSRES)/rc
