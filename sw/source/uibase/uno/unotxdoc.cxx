97ff2b5fb8d3f6812c7d2f0a59d1accbff03c468
Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
2019-03-25 16:35:40 +0100

    tdf#124025 Update formula before printing
    Reintroduce the UpdateFields call, but guard it so that
    it does not modify the doc (as it was before)
    
    Change-Id: I23e814f0b5d900d145dc82272034f03e66c079ed
    Reviewed-on: https://gerrit.libreoffice.org/69696
    Tested-by: Jenkins
    Reviewed-by: Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
    (cherry picked from commit 055480aac88e4688619e9a7815e0a8b0ab97a5a1)
    Reviewed-on: https://gerrit.libreoffice.org/69727
    Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>

--- sw/source/uibase/uno/unotxdoc.cxx
+++ sw/source/uibase/uno/unotxdoc.cxx
@@ -2621,23 +2621,25 @@ sal_Int32 SAL_CALL SwXTextDocument::getRendererCount(
             // since printing now also use the API for PDF export this option
             // should be set for printing as well ...
             pViewShell->SetPDFExportOption( true );
-            bool bOrigStatus = pRenderDocShell->IsEnableSetModified();
-            // check configuration: shall update of printing information in DocInfo set the document to "modified"?
-            bool bStateChanged = false;
-            if ( bOrigStatus && !SvtPrintWarningOptions().IsModifyDocumentOnPrintingAllowed() )
-            {
-                pRenderDocShell->EnableSetModified( false );
-                bStateChanged = true;
-            }
-
-            if( bStateChanged )
-                pRenderDocShell->EnableSetModified();
 
             // there is some redundancy between those two function calls, but right now
             // there is no time to sort this out.
             //TODO: check what exactly needs to be done and make just one function for that
             pViewShell->CalcLayout();
             pViewShell->CalcPagesForPrint( pViewShell->GetPageCount() );
+
+
+            // #122919# Force field update before PDF export, but after layout init (tdf#121962)
+            bool bStateChanged = false;
+            // check configuration: shall update of printing information in DocInfo set the document to "modified"?
+            if ( pRenderDocShell->IsEnableSetModified() && !SvtPrintWarningOptions().IsModifyDocumentOnPrintingAllowed() )
+            {
+                pRenderDocShell->EnableSetModified( false );
+                bStateChanged = true;
+            }
+            pViewShell->SwViewShell::UpdateFields(true);
+            if( bStateChanged )
+                pRenderDocShell->EnableSetModified();
 
             pViewShell->SetPDFExportOption( false );
 
