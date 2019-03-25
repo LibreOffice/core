LibreOffice/core

commit 6be7e2e9dd8027d284f1b00ef6e3b4654eec7d79

pass SdrUndoAction around by std::unique_ptr
convert the LINK we use here to std::function, since LINK
does not currently handle std::unique_ptr

Change-Id: I9df80352e612445e5f5ca513d7d4196d65589778
Reviewed-on: https://gerrit.libreoffice.org/59804
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

chart2/source/controller/main/ChartController.cxx
@@ -73,6 +73,7 @@
 
 #include <sal/log.hxx>
 #include <svx/sidebar/SelectionChangeHandler.hxx>
+#include <svx/svdundo.hxx>
 #include <toolkit/awt/vclxwindow.hxx>
 #include <toolkit/helper/vclunohelper.hxx>
 #include <vcl/svapp.hxx>
@@ -1433,7 +1434,7 @@ void SAL_CALL ChartController::modified(
     //todo? update menu states ?
 }
 
-IMPL_LINK( ChartController, NotifyUndoActionHdl, SdrUndoAction*, pUndoAction, void )
+void ChartController::NotifyUndoActionHdl( std::unique_ptr<SdrUndoAction> pUndoAction )
 {
     ENSURE_OR_RETURN_VOID( pUndoAction, "invalid Undo action" );
 
@@ -1463,7 +1464,8 @@ DrawModelWrapper* ChartController::GetDrawModelWrapper()
             m_pDrawModelWrapper = pProvider->getDrawModelWrapper();
         if ( m_pDrawModelWrapper.get() )
         {
-            m_pDrawModelWrapper->getSdrModel().SetNotifyUndoActionHdl( LINK( this, ChartController, NotifyUndoActionHdl ) );
+            m_pDrawModelWrapper->getSdrModel().SetNotifyUndoActionHdl(
+                std::bind(&ChartController::NotifyUndoActionHdl, this, std::placeholders::_1) );
         }
     }
     return m_pDrawModelWrapper.get();

basctl/source/basicide/baside3.cxx
@@ -83,7 +83,7 @@ DialogWindow::DialogWindow(DialogWindowLayout* pParent, ScriptDocument const& rD
     InitSettings();
 
     m_pEditor->GetModel().SetNotifyUndoActionHdl(
-        LINK(this, DialogWindow, NotifyUndoActionHdl)
+        &DialogWindow::NotifyUndoActionHdl
     );
 
     SetHelpId( HID_BASICIDE_DIALOGWINDOW );
@@ -217,13 +217,11 @@ void DialogWindow::Command( const CommandEvent& rCEvt )
 }
 
 
-IMPL_STATIC_LINK(
-    DialogWindow, NotifyUndoActionHdl, SdrUndoAction *, pUndoAction, void )
+void DialogWindow::NotifyUndoActionHdl( std::unique_ptr<SdrUndoAction> )
 {
     // #i120515# pUndoAction needs to be deleted, this hand over is an ownership
     // change. As long as it does not get added to the undo manager, it needs at
     // least to be deleted.
-    delete pUndoAction;
 }
 
 void DialogWindow::DoInit()

basctl/source/inc/baside3.hxx
@@ -72,7 +72,7 @@ protected:
     virtual void        Command( const CommandEvent& rCEvt ) override;
     virtual void        LoseFocus() override;
 
-    DECL_STATIC_LINK( DialogWindow, NotifyUndoActionHdl, SdrUndoAction *, void );
+    static void         NotifyUndoActionHdl( std::unique_ptr<SdrUndoAction> );
     virtual void        DoInit() override;
     virtual void        DoScroll( ScrollBar* pCurScrollBar ) override;
     virtual void        DataChanged( const DataChangedEvent& rDCEvt ) override;

chart2/source/controller/inc/ChartController.hxx
@@ -332,7 +332,7 @@ public:
     void SetAndApplySelection(const css::uno::Reference<css::drawing::XShape>& rxShape);
     void StartTextEdit( const Point* pMousePixel = nullptr );
 
-    DECL_LINK( NotifyUndoActionHdl, SdrUndoAction*, void );
+    void NotifyUndoActionHdl( std::unique_ptr<SdrUndoAction> );
 
     css::uno::Reference<css::uno::XInterface> const & getChartView();
 

include/svx/svdmodel.hxx
@@ -20,6 +20,7 @@
 #ifndef INCLUDED_SVX_SVDMODEL_HXX
 #define INCLUDED_SVX_SVDMODEL_HXX
 
+#include <functional>
 #include <memory>
 #include <com/sun/star/uno/Sequence.hxx>
 #include <cppuhelper/weakref.hxx>
@@ -183,7 +184,7 @@ private:
 protected:
     std::vector<SdrPage*> maMaPag;     // master pages
     std::vector<SdrPage*> maPages;
-    Link<SdrUndoAction*,void>  aUndoLink;  // link to a NotifyUndo-Handler
+    std::function<void(std::unique_ptr<SdrUndoAction>)> aUndoLink;  // link to a NotifyUndo-Handler
     Size           aMaxObjSize; // e.g. for auto-growing text
     Fraction       aObjUnit;   // description of the coordinate units for ClipBoard, Drag&Drop, ...
     MapUnit        eObjUnit;   // see above
@@ -522,7 +523,7 @@ public:
     //   void NotifyUndoActionHdl(SfxUndoAction* pUndoAction);
     // When calling the handler ownership is transferred;
     // The UndoAction belongs to the Handler, not the SdrModel.
-    void        SetNotifyUndoActionHdl(const Link<SdrUndoAction*,void>& rLink)  { aUndoLink=rLink; }
+    void        SetNotifyUndoActionHdl(const std::function<void(std::unique_ptr<SdrUndoAction>)>& rLink)  { aUndoLink=rLink; }
 
     /** application can set its own undo manager, BegUndo, EndUndo and AddUndoAction
         calls are routed to this interface if given */

sc/inc/drwlayer.hxx
@@ -134,7 +134,7 @@ public:
     void            BeginCalcUndo(bool bDisableTextEditUsesCommonUndoManager);
     std::unique_ptr<SdrUndoGroup> GetCalcUndo();
     bool            IsRecording() const         { return bRecording; }
-    void            AddCalcUndo( SdrUndoAction* pUndo );
+    void            AddCalcUndo( std::unique_ptr<SdrUndoAction> pUndo );
 
     void            MoveArea( SCTAB nTab, SCCOL nCol1,SCROW nRow1, SCCOL nCol2,SCROW nRow2,
                                 SCCOL nDx,SCROW nDy, bool bInsDel, bool bUpdateNoteCaptionPos );

sc/source/core/data/documen9.cxx
@@ -40,6 +40,7 @@
 #include <sfx2/printer.hxx>
 #include <unotools/saveopt.hxx>
 #include <unotools/pathoptions.hxx>
+#include <o3tl/make_unique.hxx>
 
 #include <document.hxx>
 #include <docoptio.hxx>
@@ -90,7 +91,7 @@ void ScDocument::TransferDrawPage(ScDocument* pSrcDoc, SCTAB nSrcPos, SCTAB nDes
                 pNewPage->InsertObject( pNewObject );
 
                 if (mpDrawLayer->IsRecording())
-                    mpDrawLayer->AddCalcUndo( new SdrUndoInsertObj( *pNewObject ) );
+                    mpDrawLayer->AddCalcUndo( o3tl::make_unique<SdrUndoInsertObj>( *pNewObject ) );
 
                 pOldObject = aIter.Next();
             }

sc/source/core/data/drwlayer.cxx
@@ -396,7 +396,7 @@ bool ScDrawLayer::ScAddPage( SCTAB nTab )
     ScDrawPage* pPage = static_cast<ScDrawPage*>(AllocPage( false ));
     InsertPage(pPage, static_cast<sal_uInt16>(nTab));
     if (bRecording)
-        AddCalcUndo(new SdrUndoNewPage(*pPage));
+        AddCalcUndo(o3tl::make_unique<SdrUndoNewPage>(*pPage));
 
     ResetTab(nTab, pDoc->GetTableCount()-1);
     return true;        // inserted
@@ -411,7 +411,7 @@ void ScDrawLayer::ScRemovePage( SCTAB nTab )
     if (bRecording)
     {
         SdrPage* pPage = GetPage(static_cast<sal_uInt16>(nTab));
-        AddCalcUndo(new SdrUndoDelPage(*pPage));        // Undo-Action becomes the page owner
+        AddCalcUndo(o3tl::make_unique<SdrUndoDelPage>(*pPage));        // Undo-Action becomes the page owner
         RemovePage( static_cast<sal_uInt16>(nTab) );    // just deliver, not deleting
     }
     else
@@ -472,7 +472,7 @@ void ScDrawLayer::ScCopyPage( sal_uInt16 nOldPos, sal_uInt16 nNewPos )
             }
 
             if (bRecording)
-                AddCalcUndo( new SdrUndoInsertObj( *pNewObject ) );
+                AddCalcUndo( o3tl::make_unique<SdrUndoInsertObj>( *pNewObject ) );
 
             pOldObject = aIter.Next();
         }
@@ -562,7 +562,7 @@ void ScDrawLayer::MoveCells( SCTAB nTab, SCCOL nCol1,SCROW nRow1, SCCOL nCol2,SC
                     pNoRotatedAnchor->maEnd = pData->maEnd;
                 }
 
-                AddCalcUndo( new ScUndoObjData( pObj, aOldStt, aOldEnd, pData->maStart, pData->maEnd ) );
+                AddCalcUndo( o3tl::make_unique<ScUndoObjData>( pObj, aOldStt, aOldEnd, pData->maStart, pData->maEnd ) );
                 RecalcPos( pObj, *pData, bNegativePage, bUpdateNoteCaptionPos );
             }
         }
@@ -797,7 +797,7 @@ void ScDrawLayer::RecalcPos( SdrObject* pObj, ScDrawObjData& rData, bool bNegati
         if ( pObj->GetLogicRect() != aRect )
         {
             if (bRecording)
-                AddCalcUndo( new SdrUndoGeoObj( *pObj ) );
+                AddCalcUndo( o3tl::make_unique<SdrUndoGeoObj>( *pObj ) );
             rData.setShapeRect(GetDocument(), lcl_makeSafeRectangle(aRect));
             pObj->SetLogicRect(rData.getShapeRect());
         }
@@ -829,7 +829,7 @@ void ScDrawLayer::RecalcPos( SdrObject* pObj, ScDrawObjData& rData, bool bNegati
             if ( pObj->GetPoint( 0 ) != aStartPos )
             {
                 if (bRecording)
-                    AddCalcUndo( new SdrUndoGeoObj( *pObj ) );
+                    AddCalcUndo( o3tl::make_unique<SdrUndoGeoObj>( *pObj ) );
 
                 rData.setShapeRect(GetDocument(), lcl_UpdateCalcPoly(aCalcPoly, 0, aStartPos));
                 pObj->SetPoint( aStartPos, 0 );
@@ -845,7 +845,7 @@ void ScDrawLayer::RecalcPos( SdrObject* pObj, ScDrawObjData& rData, bool bNegati
                 if ( pObj->GetPoint( 1 ) != aEndPos )
                 {
                     if (bRecording)
-                        AddCalcUndo( new SdrUndoGeoObj( *pObj ) );
+                        AddCalcUndo( o3tl::make_unique<SdrUndoGeoObj>( *pObj ) );
 
                     rData.setShapeRect(GetDocument(), lcl_UpdateCalcPoly(aCalcPoly, 1, aEndPos));
                     pObj->SetPoint( aEndPos, 1 );
@@ -867,7 +867,7 @@ void ScDrawLayer::RecalcPos( SdrObject* pObj, ScDrawObjData& rData, bool bNegati
             if ( pObj->GetPoint( 1 ) != aEndPos )
             {
                 if (bRecording)
-                    AddCalcUndo( new SdrUndoGeoObj( *pObj ) );
+                    AddCalcUndo( o3tl::make_unique<SdrUndoGeoObj>( *pObj ) );
 
                 rData.setShapeRect(GetDocument(), lcl_UpdateCalcPoly(aCalcPoly, 1, aEndPos));
                 pObj->SetPoint( aEndPos, 1 );
@@ -885,7 +885,7 @@ void ScDrawLayer::RecalcPos( SdrObject* pObj, ScDrawObjData& rData, bool bNegati
                 if ( pObj->GetPoint( 0 ) != aStartPos )
                 {
                     if (bRecording)
-                        AddCalcUndo( new SdrUndoGeoObj( *pObj ) );
+                        AddCalcUndo( o3tl::make_unique<SdrUndoGeoObj>( *pObj ) );
 
                     rData.setShapeRect(GetDocument(), lcl_UpdateCalcPoly(aCalcPoly, 0, aStartPos));
                     pObj->SetPoint( aStartPos, 0 );
@@ -973,7 +973,7 @@ void ScDrawLayer::RecalcPos( SdrObject* pObj, ScDrawObjData& rData, bool bNegati
                 tools::Rectangle aOld(pObj->GetSnapRect());
 
                 if (bRecording)
-                    AddCalcUndo( new SdrUndoGeoObj( *pObj ) );
+                    AddCalcUndo( o3tl::make_unique<SdrUndoGeoObj>( *pObj ) );
                 long nOldWidth = aOld.GetWidth();
                 long nOldHeight = aOld.GetHeight();
                 if (pObj->IsPolyObj() && nOldWidth && nOldHeight)
@@ -1001,7 +1001,7 @@ void ScDrawLayer::RecalcPos( SdrObject* pObj, ScDrawObjData& rData, bool bNegati
             if ( pObj->GetRelativePos() != aPos )
             {
                 if (bRecording)
-                    AddCalcUndo( new SdrUndoGeoObj( *pObj ) );
+                    AddCalcUndo( o3tl::make_unique<SdrUndoGeoObj>( *pObj ) );
                 pObj->SetRelativePos( aPos );
             }
         }
@@ -1185,17 +1185,15 @@ bool ScDrawLayer::GetPrintArea( ScRange& rRange, bool bSetHor, bool bSetVer ) co
     return bAny;
 }
 
-void ScDrawLayer::AddCalcUndo( SdrUndoAction* pUndo )
+void ScDrawLayer::AddCalcUndo( std::unique_ptr<SdrUndoAction> pUndo )
 {
     if (bRecording)
     {
         if (!pUndoGroup)
             pUndoGroup.reset(new SdrUndoGroup(*this));
 
-        pUndoGroup->AddAction( pUndo );
+        pUndoGroup->AddAction( pUndo.release() );
     }
-    else
-        delete pUndo;
 }
 
 void ScDrawLayer::BeginCalcUndo(bool bDisableTextEditUsesCommonUndoManager)
@@ -1353,7 +1351,7 @@ void ScDrawLayer::DeleteObjectsInArea( SCTAB nTab, SCCOL nCol1,SCROW nRow1,
 
         if (bRecording)
             for (size_t i=1; i<=nDelCount; ++i)
-                AddCalcUndo( new SdrUndoRemoveObj( *ppObj[nDelCount-i] ) );
+                AddCalcUndo( o3tl::make_unique<SdrUndoRemoveObj>( *ppObj[nDelCount-i] ) );
 
         for (size_t i=1; i<=nDelCount; ++i)
             pPage->RemoveObject( ppObj[nDelCount-i]->GetOrdNum() );
@@ -1416,7 +1414,7 @@ void ScDrawLayer::DeleteObjectsInSelection( const ScMarkData& rMark )
 
                 if (bRecording)
                     for (size_t i=1; i<=nDelCount; ++i)
-                        AddCalcUndo( new SdrUndoRemoveObj( *ppObj[nDelCount-i] ) );
+                        AddCalcUndo( o3tl::make_unique<SdrUndoRemoveObj>( *ppObj[nDelCount-i] ) );
 
                 for (size_t i=1; i<=nDelCount; ++i)
                     pPage->RemoveObject( ppObj[nDelCount-i]->GetOrdNum() );
@@ -1660,7 +1658,7 @@ void ScDrawLayer::CopyFromClip( ScDrawLayer* pClipModel, SCTAB nSourceTab, const
 
             pDestPage->InsertObject( pNewObject );
             if (bRecording)
-                AddCalcUndo( new SdrUndoInsertObj( *pNewObject ) );
+                AddCalcUndo( o3tl::make_unique<SdrUndoInsertObj>( *pNewObject ) );
 
             //#i110034# handle chart data references (after InsertObject)
 
@@ -1756,7 +1754,7 @@ void ScDrawLayer::MirrorRTL( SdrObject* pObj )
         Point aRef1( 0, 0 );
         Point aRef2( 0, 1 );
         if (bRecording)
-            AddCalcUndo( new SdrUndoGeoObj( *pObj ) );
+            AddCalcUndo( o3tl::make_unique<SdrUndoGeoObj>( *pObj ) );
         pObj->Mirror( aRef1, aRef2 );
     }
     else
@@ -1767,7 +1765,7 @@ void ScDrawLayer::MirrorRTL( SdrObject* pObj )
         tools::Rectangle aObjRect = pObj->GetLogicRect();
         Size aMoveSize( -(aObjRect.Left() + aObjRect.Right()), 0 );
         if (bRecording)
-            AddCalcUndo( new SdrUndoMoveObj( *pObj, aMoveSize ) );
+            AddCalcUndo( o3tl::make_unique<SdrUndoMoveObj>( *pObj, aMoveSize ) );
         pObj->Move( aMoveSize );
     }
 }

commit 62bbc0331604913928bac103ac034ebc17415fdf

tdf#124112 Insert drawing object in chart crashes LibreOffice
regression from
    commit 6be7e2e9dd8027d284f1b00ef6e3b4654eec7d79
    Date:   Thu Aug 30 13:54:33 2018 +0200
    pass SdrUndoAction around by std::unique_ptr
Looks like previously this memory was just leaked, so fix that and the
crash

Change-Id: I5eb5c104dccd82cef30af533ce1fe8546ced0ea0

chart2/source/controller/main/ChartController.cxx
@@ -1428,7 +1428,7 @@ void ChartController::NotifyUndoActionHdl( std::unique_ptr<SdrUndoAction> pUndoA
         {
             const Reference< document::XUndoManagerSupplier > xSuppUndo( getModel(), uno::UNO_QUERY_THROW );
             const Reference< document::XUndoManager > xUndoManager( xSuppUndo->getUndoManager(), uno::UNO_QUERY_THROW );
-            const Reference< document::XUndoAction > xAction( new impl::ShapeUndoElement( *pUndoAction ) );
+            const Reference< document::XUndoAction > xAction( new impl::ShapeUndoElement( std::move(pUndoAction) ) );
             xUndoManager->addUndoAction( xAction );
         }
         catch( const uno::Exception& )

chart2/source/controller/main/UndoActions.cxx
@@ -85,10 +85,10 @@ void SAL_CALL UndoElement::redo(  )
 
 // = ShapeUndoElement
 
-ShapeUndoElement::ShapeUndoElement( SdrUndoAction& i_sdrUndoAction )
+ShapeUndoElement::ShapeUndoElement( std::unique_ptr<SdrUndoAction> xSdrUndoAction )
     :ShapeUndoElement_MBase()
     ,ShapeUndoElement_TBase( m_aMutex )
-    ,m_pAction( &i_sdrUndoAction )
+    ,m_xAction( std::move(xSdrUndoAction) )
 {
 }
 
@@ -98,23 +98,23 @@ ShapeUndoElement::~ShapeUndoElement()
 
 OUString SAL_CALL ShapeUndoElement::getTitle()
 {
-    if ( !m_pAction )
+    if ( !m_xAction )
         throw DisposedException( OUString(), *this );
-    return m_pAction->GetComment();
+    return m_xAction->GetComment();
 }
 
 void SAL_CALL ShapeUndoElement::undo(  )
 {
-    if ( !m_pAction )
+    if ( !m_xAction )
         throw DisposedException( OUString(), *this );
-    m_pAction->Undo();
+    m_xAction->Undo();
 }
 
 void SAL_CALL ShapeUndoElement::redo(  )
 {
-    if ( !m_pAction )
+    if ( !m_xAction )
         throw DisposedException( OUString(), *this );
-    m_pAction->Redo();
+    m_xAction->Redo();
 }
 
 void SAL_CALL ShapeUndoElement::disposing()

chart2/source/controller/main/UndoActions.hxx
@@ -89,7 +89,7 @@ class ShapeUndoElement  :public ShapeUndoElement_MBase
                         ,public ShapeUndoElement_TBase
 {
 public:
-    explicit ShapeUndoElement( SdrUndoAction& i_sdrUndoAction );
+    explicit ShapeUndoElement( std::unique_ptr<SdrUndoAction> xSdrUndoAction );
 
     // XUndoAction
     virtual OUString SAL_CALL getTitle() override;
@@ -103,7 +103,7 @@ protected:
     virtual ~ShapeUndoElement() override;
 
 private:
-    SdrUndoAction*  m_pAction;
+    std::unique_ptr<SdrUndoAction> m_xAction;
 };
 
 } // namespace impl