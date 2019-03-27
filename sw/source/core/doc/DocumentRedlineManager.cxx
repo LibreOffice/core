c29e6deb7987b75200a2379cea0d6ec919ec9724
Henry Castro <hcastro@collabora.com>
2019-03-27 14:41:54 -0400

    tdf#120338: The paragraph formatting changes are not undone, part 1
    Rejecting paragraph formatting is not implemented yet.
    "Reject All" command is affected because the changes were not removed
    
    Change-Id: Ic4af1def97025643ecbc5cf0752cd06d9b94c74a
    Reviewed-on: https://gerrit.libreoffice.org/69865
    Tested-by: Jenkins
    Reviewed-by: Henry Castro <hcastro@collabora.com>

--- sw/source/core/doc/DocumentRedlineManager.cxx
+++ sw/source/core/doc/DocumentRedlineManager.cxx
@@ -2656,6 +2656,7 @@ bool DocumentRedlineManager::RejectRedline( const SwPaM& rPam, bool bCallDelete 
 
 void DocumentRedlineManager::AcceptAllRedline(bool bAccept)
 {
+    bool bSuccess = true;
     OUString sUndoStr;
     IDocumentUndoRedo& rUndoMgr = m_rDoc.GetIDocumentUndoRedo();
 
@@ -2672,12 +2673,12 @@ void DocumentRedlineManager::AcceptAllRedline(bool bAccept)
         rUndoMgr.StartUndo(bAccept ? SwUndoId::ACCEPT_REDLINE : SwUndoId::REJECT_REDLINE, &aRewriter);
     }
 
-    while (!mpRedlineTable->empty())
+    while (!mpRedlineTable->empty() && bSuccess)
     {
         if (bAccept)
-            AcceptRedline(mpRedlineTable->size() - 1, true);
+            bSuccess = AcceptRedline(mpRedlineTable->size() - 1, true);
         else
-            RejectRedline(mpRedlineTable->size() - 1, true);
+            bSuccess = RejectRedline(mpRedlineTable->size() - 1, true);
     }
 
     if (!sUndoStr.isEmpty())
