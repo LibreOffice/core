ef5804a60123b9b98390cbcea73026e01cccb036
Katarina Behrens <Katarina.Behrens@cib.de>
2019-03-27 09:32:47 +0100

    tdf#124026: map window resizing-related cursors to their Qt equivalents
    there is no direct Qt equivalent of X11 xc_[top|bottom|left|right]_side
    cursors, but those come at least close
    
    Change-Id: Ifd442c46c08c71b1e1b9ddac42628b38eefee47f
    Reviewed-on: https://gerrit.libreoffice.org/69797
    Tested-by: Jenkins
    Reviewed-by: Katarina Behrens <Katarina.Behrens@cib.de>
    (cherry picked from commit 167812fe0d77b993427e7dca112f319a92942a3c)

--- vcl/qt5/Qt5Data.cxx
+++ vcl/qt5/Qt5Data.cxx
@@ -204,12 +204,10 @@ QCursor& Qt5Data::getCursor(PointerStyle ePointerStyle)
             MAP_BUILTIN(PointerStyle::NESize, Qt::SizeBDiagCursor);
             MAP_BUILTIN(PointerStyle::SWSize, Qt::SizeBDiagCursor);
             MAP_BUILTIN(PointerStyle::SESize, Qt::SizeFDiagCursor);
-#if 0
-            MAP_BUILTIN( PointerStyle::WindowNSize, GDK_TOP_SIDE );
-            MAP_BUILTIN( PointerStyle::WindowSSize, GDK_BOTTOM_SIDE );
-            MAP_BUILTIN( PointerStyle::WindowWSize, GDK_LEFT_SIDE );
-            MAP_BUILTIN( PointerStyle::WindowESize, GDK_RIGHT_SIDE );
-#endif
+            MAP_BUILTIN(PointerStyle::WindowNSize, Qt::SizeVerCursor);
+            MAP_BUILTIN(PointerStyle::WindowSSize, Qt::SizeVerCursor);
+            MAP_BUILTIN(PointerStyle::WindowWSize, Qt::SizeHorCursor);
+            MAP_BUILTIN(PointerStyle::WindowESize, Qt::SizeHorCursor);
             MAP_BUILTIN(PointerStyle::WindowNWSize, Qt::SizeFDiagCursor);
             MAP_BUILTIN(PointerStyle::WindowNESize, Qt::SizeBDiagCursor);
             MAP_BUILTIN(PointerStyle::WindowSWSize, Qt::SizeBDiagCursor);
@@ -299,7 +297,8 @@ QCursor& Qt5Data::getCursor(PointerStyle ePointerStyle)
         if (!pCursor)
         {
             pCursor = new QCursor(Qt::ArrowCursor);
-            SAL_WARN("vcl.qt5", "pointer " << static_cast<int>(ePointerStyle) << "not implemented");
+            SAL_WARN("vcl.qt5",
+                     "pointer " << static_cast<int>(ePointerStyle) << " not implemented");
         }
 
         m_aCursors[ePointerStyle].reset(pCursor);
