659be567fba36081c100502e8c4a9b204333777f
Tor Lillqvist <tml@collabora.com>
2019-03-29 16:32:01 +0200

    Use the colibre icon theme in Online and apps
    Change-Id: Idb79b3d4b1f14712f2e03d17fca6bec56f317edf

--- vcl/source/app/IconThemeSelector.cxx
+++ vcl/source/app/IconThemeSelector.cxx
@@ -7,6 +7,8 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+#include <comphelper/lok.hxx>
+
 #include <vcl/IconThemeSelector.hxx>
 
 #include <vcl/IconThemeInfo.hxx>
@@ -50,6 +52,9 @@ IconThemeSelector::IconThemeSelector()
 /*static*/ OUString
 IconThemeSelector::GetIconThemeForDesktopEnvironment(const OUString& desktopEnvironment)
 {
+    if (comphelper::LibreOfficeKit::isActive())
+        return OUString("colibre");
+
 #ifdef _WIN32
     (void)desktopEnvironment;
     return OUString("colibre");
