b4f5c9c362e221cfb159c633c70b26ba86cdfec1
tundet <tundeth@gmail.com>
2019-03-29 10:22:06 +0100

    tdf#115192 XLSX export: fix hyperlink inserted to shape
    Hyperlink inserted to shape didn't work after export, because
    the LO wrote TargetMode="External" code incorrectly in the xml.
    
    test_115192.xlsx: Test file from Excel 2016.
    
    Change-Id: I3a9d39c1aa5de5af6d1ca43d16c45a5401ec6f3d

--- sc/qa/unit/subsequent_export-test.cxx
+++ sc/qa/unit/subsequent_export-test.cxx
@@ -211,6 +211,7 @@ public:
     void testTdf121612();
     void testPivotCacheAfterExportXLSX();
     void testTdf114969XLSX();
+    void testTdf115192XLSX();
 
     void testXltxExport();
 
@@ -329,6 +330,7 @@ public:
     CPPUNIT_TEST(testTdf121612);
     CPPUNIT_TEST(testPivotCacheAfterExportXLSX);
     CPPUNIT_TEST(testTdf114969XLSX);
+    CPPUNIT_TEST(testTdf115192XLSX);
 
     CPPUNIT_TEST(testXltxExport);
 
@@ -4170,6 +4172,18 @@ void ScExportTest::testTdf114969XLSX()
     CPPUNIT_ASSERT(pDoc);
     assertXPath(pDoc, "/x:worksheet/x:hyperlinks/x:hyperlink[1]", "location", "'1.1.1.1'!C1");
     assertXPath(pDoc, "/x:worksheet/x:hyperlinks/x:hyperlink[2]", "location", "'1.1.1.1'!C2");
+}
+
+void ScExportTest::testTdf115192XLSX()
+{
+    ScDocShellRef xDocSh = loadDoc("test_115192.", FORMAT_XLSX);
+    CPPUNIT_ASSERT(xDocSh.is());
+
+    xmlDocPtr pDoc = XPathHelper::parseExport2(*this, *xDocSh, m_xSFactory, "xl/drawings/_rels/drawing1.xml.rels", FORMAT_XLSX);
+    CPPUNIT_ASSERT(pDoc);
+    assertXPath(pDoc, "/r:Relationships/r:Relationship[@Id='rId1']", "TargetMode", "External");
+    assertXPathNoAttribute(pDoc, "/r:Relationships/r:Relationship[@Id='rId2']", "TargetMode");
+    assertXPath(pDoc, "/r:Relationships/r:Relationship[@Id='rId3']", "TargetMode", "External");
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(ScExportTest);
