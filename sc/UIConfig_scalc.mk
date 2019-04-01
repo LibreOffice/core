7f7d12d2f2aca326772dcfadbcbfd17f115b8e9f
Gagandeep Singh <deepgagan231197@gmail.com>
2019-04-01 16:50:18 +0530

    bug tdf#120697
    add drop down containing formulas in autosum button in calc
    
    Change-Id: I1da8116ba2f6073bec153979282161fbaa286427

--- sc/UIConfig_scalc.mk
+++ sc/UIConfig_scalc.mk
@@ -88,6 +88,7 @@ $(eval $(call gb_UIConfig_add_uifiles,modules/scalc,\
 	sc/uiconfig/scalc/ui/allheaderfooterdialog \
 	sc/uiconfig/scalc/ui/analysisofvariancedialog \
 	sc/uiconfig/scalc/ui/autoformattable \
+	sc/uiconfig/scalc/ui/autosum \
 	sc/uiconfig/scalc/ui/cellprotectionpage \
 	sc/uiconfig/scalc/ui/changesourcedialog \
 	sc/uiconfig/scalc/ui/chardialog \
--- sc/source/ui/inc/tabvwsh.hxx
+++ sc/source/ui/inc/tabvwsh.hxx
@@ -21,6 +21,7 @@
 #define INCLUDED_SC_SOURCE_UI_INC_TABVWSH_HXX
 
 #include <formula/errorcodes.hxx>
+#include <formula/opcode.hxx>
 #include <svx/fmshell.hxx>
 #include <sfx2/viewsh.hxx>
 #include <editeng/svxenum.hxx>
@@ -375,7 +376,7 @@ public:
     bool IsActive() const { return bIsActive; }
     OUString GetFormula(const ScAddress& rAddress);
     bool    UseSubTotal(ScRangeList* pRangeList);
-    const   OUString DoAutoSum(bool& rRangeFinder, bool& rSubTotal);
+    const   OUString DoAutoSum(bool& rRangeFinder, bool& rSubTotal, const OpCode eCode);
 
     // ugly hack to call Define Names from Manage Names
     void    SwitchBetweenRefDialogs(SfxModelessDialogController* pDialog);
