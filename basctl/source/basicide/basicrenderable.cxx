abc6cb7ec6c7f4f9921c25be8a14982d20601243
Andrea Gelmini <andrea.gelmini@gelma.net>
2019-04-01 07:30:47 +0000

    Fix typo
    Change-Id: I8f5828e0e9887aaee928e4ff794bd21399fa5abc

--- basctl/source/basicide/basicrenderable.cxx
+++ basctl/source/basicide/basicrenderable.cxx
@@ -64,7 +64,7 @@ Renderable::Renderable (BaseWindow* pWin)
                                                    aHelpIds, aPrintContentName,
                                                    aChoices, 0);
 
-    // create a an Edit dependent on "Pages" selected
+    // create an Edit dependent on "Pages" selected
     vcl::PrinterOptionsHelper::UIControlOptions aPageRangeOpt(aPrintContentName, 1, true);
     m_aUIProperties[2].Value = setEditControlOpt("pagerange", OUString(),
                                                  OUString(), "PageRange",
