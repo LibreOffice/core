LibreOffice/core

commit fa5f4a1d5fb2b04ee416dd72eb4aaf0b42ca5cba

tdf#124149 add bigger 32px icons for border line and shadow
Reviewed-on: https://gerrit.libreoffice.org/69925
Tested-by: Jenkins
Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>
(cherry picked from commit 92990b12f900ac0a12ce006f63b8c3d0f09b6b2f)

Change-Id: I7ea382f3ba648657d2a6d009bf90dcc3be879c63

cui/source/tabpages/border.cxx
@@ -48,6 +48,7 @@
 #include <sal/macros.h>
 #include <com/sun/star/lang/XServiceInfo.hpp>
 #include "borderconn.hxx"
+#include <comphelper/lok.hxx>
 
 using namespace ::editeng;
 using ::com::sun::star::uno::Reference;
@@ -140,45 +141,82 @@ SvxBorderTabPage::SvxBorderTabPage(vcl::Window* pParent, const SfxItemSet& rCore
     get(m_pRemoveAdjcentCellBordersCB, "rmadjcellborders");
     get(m_pRemoveAdjcentCellBordersFT, "rmadjcellbordersft");
 
-    static const OUStringLiteral pnBorderImgIds[] =
+    static std::vector<OUStringLiteral> aBorderImageIds;
+
+    if (aBorderImageIds.empty())
     {
-        RID_SVXBMP_CELL_NONE,
-        RID_SVXBMP_CELL_ALL,
-        RID_SVXBMP_CELL_LR,
-        RID_SVXBMP_CELL_TB,
-        RID_SVXBMP_CELL_L,
-        RID_SVXBMP_CELL_DIAG,
-        RID_SVXBMP_HOR_NONE,
-        RID_SVXBMP_HOR_OUTER,
-        RID_SVXBMP_HOR_HOR,
-        RID_SVXBMP_HOR_ALL,
-        RID_SVXBMP_HOR_OUTER2,
-        RID_SVXBMP_VER_NONE,
-        RID_SVXBMP_VER_OUTER,
-        RID_SVXBMP_VER_VER,
-        RID_SVXBMP_VER_ALL,
-        RID_SVXBMP_VER_OUTER2,
-        RID_SVXBMP_TABLE_NONE,
-        RID_SVXBMP_TABLE_OUTER,
-        RID_SVXBMP_TABLE_OUTERH,
-        RID_SVXBMP_TABLE_ALL,
-        RID_SVXBMP_TABLE_OUTER2
-    };
+        if (comphelper::LibreOfficeKit::isActive())
+        {
+            aBorderImageIds.insert(aBorderImageIds.end(), {
+                RID_SVXBMP_CELL_NONE_32,
+                RID_SVXBMP_CELL_ALL_32,
+                RID_SVXBMP_CELL_LR_32,
+                RID_SVXBMP_CELL_TB_32,
+                RID_SVXBMP_CELL_L_32,
+                RID_SVXBMP_CELL_DIAG_32
+            });
+        }
+        else
+        {
+            aBorderImageIds.insert(aBorderImageIds.end(), {
+                RID_SVXBMP_CELL_NONE,
+                RID_SVXBMP_CELL_ALL,
+                RID_SVXBMP_CELL_LR,
+                RID_SVXBMP_CELL_TB,
+                RID_SVXBMP_CELL_L,
+                RID_SVXBMP_CELL_DIAG
+            });
+        }
+        aBorderImageIds.insert(aBorderImageIds.end(), {
+            RID_SVXBMP_HOR_NONE,
+            RID_SVXBMP_HOR_OUTER,
+            RID_SVXBMP_HOR_HOR,
+            RID_SVXBMP_HOR_ALL,
+            RID_SVXBMP_HOR_OUTER2,
+            RID_SVXBMP_VER_NONE,
+            RID_SVXBMP_VER_OUTER,
+            RID_SVXBMP_VER_VER,
+            RID_SVXBMP_VER_ALL,
+            RID_SVXBMP_VER_OUTER2,
+            RID_SVXBMP_TABLE_NONE,
+            RID_SVXBMP_TABLE_OUTER,
+            RID_SVXBMP_TABLE_OUTERH,
+            RID_SVXBMP_TABLE_ALL,
+            RID_SVXBMP_TABLE_OUTER2
+        });
+    }
 
-    for (size_t i = 0; i < SAL_N_ELEMENTS(pnBorderImgIds); ++i)
-        m_aBorderImgVec.emplace_back(pnBorderImgIds[i]);
+    for (auto const & rImageId : aBorderImageIds)
+        m_aBorderImgVec.emplace_back(StockImage::Yes, rImageId);
 
-    static const OUStringLiteral pnShadowImgIds[SVX_BORDER_SHADOW_COUNT] =
+    static std::vector<OUStringLiteral> aShadowImageIds;
+    if (aShadowImageIds.empty())
     {
-        RID_SVXBMP_SHADOWNONE,
-        RID_SVXBMP_SHADOW_BOT_RIGHT,
-        RID_SVXBMP_SHADOW_TOP_RIGHT,
-        RID_SVXBMP_SHADOW_BOT_LEFT,
-        RID_SVXBMP_SHADOW_TOP_LEFT
-    };
+        if (comphelper::LibreOfficeKit::isActive())
+        {
+            aShadowImageIds.insert(aShadowImageIds.end(), {
+                RID_SVXBMP_SHADOWNONE_32,
+                RID_SVXBMP_SHADOW_BOT_RIGHT_32,
+                RID_SVXBMP_SHADOW_TOP_RIGHT_32,
+                RID_SVXBMP_SHADOW_BOT_LEFT_32,
+                RID_SVXBMP_SHADOW_TOP_LEFT_32
+            });
+        }
+        else
+        {
+            aShadowImageIds.insert(aShadowImageIds.end(), {
+                RID_SVXBMP_SHADOWNONE,
+                RID_SVXBMP_SHADOW_BOT_RIGHT,
+                RID_SVXBMP_SHADOW_TOP_RIGHT,
+                RID_SVXBMP_SHADOW_BOT_LEFT,
+                RID_SVXBMP_SHADOW_TOP_LEFT
+            });
+        }
+    }
 
-    for (size_t i = 0; i < SAL_N_ELEMENTS(pnShadowImgIds); ++i)
-        m_aShadowImgVec.emplace_back(pnShadowImgIds[i]);
+    for (auto const & rImageId : aShadowImageIds)
+        m_aShadowImgVec.emplace_back(StockImage::Yes, rImageId);
+
     assert(m_aShadowImgVec.size() == SVX_BORDER_SHADOW_COUNT);
 
     if ( GetDPIScaleFactor() > 1 )

cui/inc/bitmaps.hlst
@@ -11,11 +11,17 @@
 #define INCLUDED_CUI_INC_BITMAPS_HRC
 
 #define RID_SVXBMP_CELL_NONE            "svx/res/pr01.png"
+#define RID_SVXBMP_CELL_NONE_32         "svx/res/border_cell_none_32.png"
 #define RID_SVXBMP_CELL_ALL             "svx/res/pr02.png"
+#define RID_SVXBMP_CELL_ALL_32          "svx/res/border_cell_all_32.png"
 #define RID_SVXBMP_CELL_LR              "svx/res/pr03.png"
+#define RID_SVXBMP_CELL_LR_32           "svx/res/border_cell_lr_32.png"
 #define RID_SVXBMP_CELL_TB              "svx/res/pr04.png"
+#define RID_SVXBMP_CELL_TB_32           "svx/res/border_cell_tb_32.png"
 #define RID_SVXBMP_CELL_L               "svx/res/pr05.png"
+#define RID_SVXBMP_CELL_L_32            "svx/res/border_cell_l_32.png"
 #define RID_SVXBMP_CELL_DIAG            "svx/res/pr06.png"
+#define RID_SVXBMP_CELL_DIAG_32         "svx/res/border_cell_diag_32.png"
 #define RID_SVXBMP_HOR_NONE             "svx/res/pr07.png"
 #define RID_SVXBMP_HOR_OUTER            "svx/res/pr08.png"
 #define RID_SVXBMP_HOR_HOR              "svx/res/pr09.png"
@@ -31,11 +37,17 @@
 #define RID_SVXBMP_TABLE_OUTERH         "svx/res/pr019.png"
 #define RID_SVXBMP_TABLE_ALL            "svx/res/pr020.png"
 #define RID_SVXBMP_TABLE_OUTER2         "svx/res/pr021.png"
+
 #define RID_SVXBMP_SHADOWNONE           "svx/res/sh01.png"
+#define RID_SVXBMP_SHADOWNONE_32        "svx/res/shadow_none_32.png"
 #define RID_SVXBMP_SHADOW_BOT_RIGHT     "svx/res/sh02.png"
+#define RID_SVXBMP_SHADOW_BOT_RIGHT_32  "svx/res/shadow_bottom_right_32.png"
 #define RID_SVXBMP_SHADOW_BOT_LEFT      "svx/res/sh03.png"
+#define RID_SVXBMP_SHADOW_BOT_LEFT_32   "svx/res/shadow_bottom_left_32.png"
 #define RID_SVXBMP_SHADOW_TOP_RIGHT     "svx/res/sh04.png"
+#define RID_SVXBMP_SHADOW_TOP_RIGHT_32  "svx/res/shadow_top_right_32.png"
 #define RID_SVXBMP_SHADOW_TOP_LEFT      "svx/res/sh05.png"
+#define RID_SVXBMP_SHADOW_TOP_LEFT_32   "svx/res/shadow_top_left_32.png"
 
 #define RID_SVXBMP_BOTTOMLOCK           "svx/res/lo01.png"
 #define RID_SVXBMP_TOPLOCK              "svx/res/lo02.png"

commit f710c70b83a597ac13895954093f12d78dcc2ed7

fix build caused by fa5f4a1d5fb2b04ee416dd72eb4aaf0b42ca5cba
Change-Id: I503ec0ffa995a29a4164552ca1560b61fcd1bfd0

cui/source/tabpages/border.cxx
@@ -187,7 +187,7 @@ SvxBorderTabPage::SvxBorderTabPage(vcl::Window* pParent, const SfxItemSet& rCore
     }
 
     for (auto const & rImageId : aBorderImageIds)
-        m_aBorderImgVec.emplace_back(StockImage::Yes, rImageId);
+        m_aBorderImgVec.emplace_back(rImageId);
 
     static std::vector<OUStringLiteral> aShadowImageIds;
     if (aShadowImageIds.empty())
@@ -215,7 +215,7 @@ SvxBorderTabPage::SvxBorderTabPage(vcl::Window* pParent, const SfxItemSet& rCore
     }
 
     for (auto const & rImageId : aShadowImageIds)
-        m_aShadowImgVec.emplace_back(StockImage::Yes, rImageId);
+        m_aShadowImgVec.emplace_back(rImageId);
 
     assert(m_aShadowImgVec.size() == SVX_BORDER_SHADOW_COUNT);
 