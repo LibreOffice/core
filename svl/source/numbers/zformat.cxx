b105ba5032aa953fc78f99c7b9e24e3db7c2b9aa
Takeshi Abe <tabe@fixedpoint.jp>
2019-04-01 11:44:32 +0900

    Introduce next Japanese gengou era 'Reiwa'
    starting from 2019-05-01, which has been announced officially.
    
    This fills the provisional slot acknowledged at
    cacbb0faef77ae8462de9ff5c7307a6a2e28b2bb.
    
    Change-Id: Ifb12e6afaad4c66d455f664b46ec946e80324e87

--- svl/source/numbers/zformat.cxx
+++ svl/source/numbers/zformat.cxx
@@ -3476,7 +3476,7 @@ void SvNumberformat::ImpAppendEraG( OUStringBuffer& OutString,
             cEra = 'H';
             break;
         case 5:
-            cEra = 'N';     /* TODO: the real one letter era name is not known yet (2018-07-26) */
+            cEra = 'R';
             break;
         default:
             cEra = '?';
