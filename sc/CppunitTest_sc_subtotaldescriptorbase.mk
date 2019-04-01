65d6265e800f12e7ed609bb07bcac730b34fed38
Jens Carl <j.carl43@gmx.de>
2019-04-01 03:58:12 +0000

    Rename ScSubTotalDescriptorBaseObj to ScSubTotalDescriptorBase
    Correct class name to mirror the name of the object we are testing
    against.
    
    Change-Id: If26dc045f67df7725043f1b0ccd0d3c8ba73f4ed

---
+++ sc/CppunitTest_sc_subtotaldescriptorbase.mk
@@ -0,0 +1,46 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#*************************************************************************
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+#*************************************************************************
+
+$(eval $(call gb_CppunitTest_CppunitTest,sc_subtotaldescriptorbase))
+
+$(eval $(call gb_CppunitTest_use_external,sc_subtotaldescriptorbase,boost_headers))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,sc_subtotaldescriptorbase, \
+	sc/qa/extras/scsubtotaldescriptorbase \
+))
+
+$(eval $(call gb_CppunitTest_use_libraries,sc_subtotaldescriptorbase, \
+	cppu \
+	sal \
+	subsequenttest \
+	test \
+	unotest \
+))
+
+$(eval $(call gb_CppunitTest_set_include,sc_subtotaldescriptorbase,\
+	$$(INCLUDE) \
+))
+
+$(eval $(call gb_CppunitTest_use_api,sc_subtotaldescriptorbase,\
+	offapi \
+	udkapi \
+))
+
+$(eval $(call gb_CppunitTest_use_ure,sc_subtotaldescriptorbase))
+$(eval $(call gb_CppunitTest_use_vcl,sc_subtotaldescriptorbase))
+
+$(eval $(call gb_CppunitTest_use_components,sc_subtotaldescriptorbase,\
+	$(sc_unoapi_common_components) \
+))
+
+$(eval $(call gb_CppunitTest_use_configuration,sc_subtotaldescriptorbase))
+
+# vim: set noet sw=4 ts=4:
