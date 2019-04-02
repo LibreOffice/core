cfa605dddb6c22f64be852c4dd38760ca3cad946
Andrea Gelmini <andrea.gelmini@gelma.net>
2019-04-02 23:37:42 +0200

    Fix typo
    Change-Id: I794be89bed433e813feaecffce9464d087ddde61

--- UnoControls/source/controls/framecontrol.cxx
+++ UnoControls/source/controls/framecontrol.cxx
@@ -74,13 +74,13 @@ Any SAL_CALL FrameControl::queryInterface( const Type& rType )
     Reference< XInterface > xDel = BaseControl::impl_getDelegator();
     if ( xDel.is() )
     {
-        // If an delegator exist, forward question to his queryInterface.
-        // Delegator will ask his own queryAggregation!
+        // If a delegator exists, forward question to its queryInterface.
+        // Delegator will ask its own queryAggregation!
         aReturn = xDel->queryInterface( rType );
     }
     else
     {
-        // If an delegator unknown, forward question to own queryAggregation.
+        // If a delegator is unknown, forward question to own queryAggregation.
         aReturn = queryAggregation( rType );
     }
 
