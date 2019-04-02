b1881e85172f642a28b38be817ec499947ae6029
Tor Lillqvist <tml@collabora.com>
2019-04-02 14:02:51 +0300

    Warn when getFileURLFromSystemPath() fails, not when it succeeds
    Also, display the name that the call failed for in the warning message.
    
    Change-Id: I0b6b7baca1d534b5bada5286b2b0e5b8e94f1832

--- sfx2/source/doc/docfile.cxx
+++ sfx2/source/doc/docfile.cxx
@@ -763,9 +763,9 @@ bool SfxMedium::IsStorage()
     {
         OUString aURL;
         if ( osl::FileBase::getFileURLFromSystemPath( pImpl->m_aName, aURL )
-             == osl::FileBase::E_None )
+             != osl::FileBase::E_None )
         {
-            SAL_WARN( "sfx.doc", "Physical name not convertible!");
+            SAL_WARN( "sfx.doc", "Physical name '" << pImpl->m_aName << "' not convertible to file URL");
         }
         pImpl->bIsStorage = SotStorage::IsStorageFile( aURL ) && !SotStorage::IsOLEStorage( aURL);
         if ( !pImpl->bIsStorage )
