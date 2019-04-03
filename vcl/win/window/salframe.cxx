810456f8f09cb1e0fc5be164c715008fc07a20ac
Miklos Vajna <vmiklos@collabora.com>
2019-04-03 12:34:38 +0200

    tdf#114316 vcl opengl windows: fix missing context menu in full-screen mode
    Full-screen mode on Windows used to work by measuring the space needed
    by window caption (title) and borders, then positioning and sizing the
    window in a way, so that the caption and borders are not visible.
    
    This approach breaks at least in the OpenGL case where a large enough
    negative position results in rendering errors.
    
    Fix the problem by explicitly requesting the window to have no caption,
    so we render less outside the screen (30 pixels -> 8 pixels in my case),
    which makes the "exit fullscreen" toolbar appear, also the context menu
    is visible.
    
    (cherry picked from commit c31734d0e49a778cffb1fdc3cd04adb45270e1da)
    
    Change-Id: I6cf2b9774b505d3887b958a6a018b5ae84bbe4bc

--- vcl/win/window/salframe.cxx
+++ vcl/win/window/salframe.cxx
@@ -842,6 +842,7 @@ WinSalFrame::WinSalFrame()
     mbBorder            = false;
     mbFixBorder         = false;
     mbSizeBorder        = false;
+    mbFullScreenCaption = false;
     mbFullScreen        = false;
     mbPresentation      = false;
     mbInShow            = false;
@@ -1849,6 +1850,15 @@ void WinSalFrame::ShowFullScreen( bool bFullScreen, sal_Int32 nDisplay )
         if ( !(GetWindowStyle( mhWnd ) & WS_VISIBLE) )
             mnShowState = SW_SHOW;
 
+        // Save caption state.
+        mbFullScreenCaption = mbCaption;
+        if (mbCaption)
+        {
+            DWORD nStyle = GetWindowStyle(mhWnd);
+            SetWindowStyle(mhWnd, nStyle & ~WS_CAPTION);
+            mbCaption = false;
+        }
+
         // set window to screen size
         ImplSalFrameFullScreenPos( this, true );
     }
@@ -1863,6 +1873,14 @@ void WinSalFrame::ShowFullScreen( bool bFullScreen, sal_Int32 nDisplay )
         if ( mbFullScreenToolWin )
             SetWindowExStyle( mhWnd, GetWindowExStyle( mhWnd ) | WS_EX_TOOLWINDOW );
         mbFullScreenToolWin = false;
+
+        // Restore caption state.
+        if (mbFullScreenCaption)
+        {
+            DWORD nStyle = GetWindowStyle(mhWnd);
+            SetWindowStyle(mhWnd, nStyle | WS_CAPTION);
+        }
+        mbCaption = mbFullScreenCaption;
 
         SetWindowPos( mhWnd, nullptr,
                       maFullScreenRect.left,
