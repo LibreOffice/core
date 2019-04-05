LibreOffice/core

commit 5b2f1243bd44b450da26c8344b3ee459f8e88f6c

tdf#122927 prevent SolarMutex deadlock on join()
The AsyncRequests thread can be blocked in a SolarMutexGuard, so
we have to release the mutex before calling join().

But there is also a static AsyncRequests object inside the
VistaFilePickerEventHandler::impl_sendEvent function, which will
just be cleaned up at DLL atexit.

"Luckily" it won't ever run, so it doesn't need to be joined and
we can use isRunning to detect it, prevent releasing a non-
existing SolarMutex at this point and so won't crash LO on exit.

Change-Id: I0c56b89a11f96be54e82b756c5e18a058b04a41d
Reviewed-on: https://gerrit.libreoffice.org/66901
Tested-by: Jenkins
Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>

fpicker/source/win32/asyncrequests.cxx
@@ -75,7 +75,14 @@ AsyncRequests::~AsyncRequests()
     aLock.clear();
     // <- SYNCHRONIZED
 
-    join();
+    // The static AsyncRequests aNotify in VistaFilePickerEventHandler::impl_sendEvent
+    // is destructed at DLL atexit. But it won't run, so needs no join and release of
+    // the already destructed SolarMutex, which would crash LO on exit.
+    if (isRunning())
+    {
+        SolarMutexReleaser aReleaser;
+        join();
+    }
 }
 
 void AsyncRequests::triggerJobExecution()

fpicker/source/win32/VistaFilePickerEventHandler.cxx
@@ -289,6 +289,7 @@ public:
 void VistaFilePickerEventHandler::impl_sendEvent(  EEventType eEventType,
                                                  ::sal_Int16  nControlID)
 {
+    // See special handling in ~AsyncRequests for this static
     static AsyncRequests aNotify(RequestHandlerRef(new AsyncPickerEvents()));
 
     ::cppu::OInterfaceContainerHelper* pContainer = m_lListener.getContainer( cppu::UnoType<css::ui::dialogs::XFilePickerListener>::get());

commit 221090eb5d4c5a58fdb815ab6da210a0b220f8ba

tdf#123502: Make sure to hold the mutex before releasing it
Fixes a regression after commit 5b2f1243bd44b450da26c8344b3ee459f8e88f6c

Change-Id: Ie95bd3423a25f210f0bbb2a88288c7c60a528ea0

fpicker/source/win32/asyncrequests.cxx
@@ -80,6 +80,8 @@ AsyncRequests::~AsyncRequests()
     // the already destructed SolarMutex, which would crash LO on exit.
     if (isRunning())
     {
+        // tdf#123502: make sure we actually hold the mutex before releasing it
+        SolarMutexGuard aGuard;
         SolarMutexReleaser aReleaser;
         join();
     }