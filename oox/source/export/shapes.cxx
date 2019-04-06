4774eedbe083786557d14df8ee787edcf769bc58
Julien Nabet <serval2412@yahoo.fr>
2019-04-06 11:19:07 +0200

    Fix 6a9cf9ba2d37fee9b7c2f190b347e0d7c4a2676a
    Change-Id: If025c21f3961372f861c8bb9df722fbdae5cc3cb

--- oox/source/export/shapes.cxx
+++ oox/source/export/shapes.cxx
@@ -1165,7 +1165,7 @@ void ShapeExport::WriteGraphicObjectShapePart( const Reference< XShape >& xShape
         mpURLTransformer->isExternalURL(sURL));
 
         mpFS->singleElementNS( XML_a, XML_hlinkClick,
-        FSNS( XML_r,XML_id ), USS( sRelId ),
+        FSNS( XML_r,XML_id ), OUStringToOString( sRelId, RTL_TEXTENCODING_UTF8 ).getStr(),
         FSEND );
     }
     pFS->endElementNS(mnXmlNamespace, XML_cNvPr);
