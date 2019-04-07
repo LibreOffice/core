741720027bf2fe40a4a313e72acf4e7f275c8900
Mike Kaganski <mike.kaganski@collabora.com>
2019-04-07 15:29:35 +0100

    [API CHANGE] Asserts to never clear already cleared guard
    ... which could help catch copy-paste errors when wrong guard is cleared
    second time.
    
    Also an assert added that when resetting, there's something to reset
    (i.e., no descendant class had cleared protected pResetT, making reset
    impossible, and thus actually unable to guard anything).
    
    framework/source/layoutmanager/layoutmanager.cxx: made sure to not call
    clear() second time
    
    framework/source/layoutmanager/toolbarlayoutmanager.cxx: restored lock
    lost in commit 777bc22ca6490a4300f30fc1b45287dce789a36f
    
    forms/source/misc/InterfaceContainer.cxx: removed a leftover from commit
    a19cd21e3c03559877428315bebc0ceaf367a461 which reduced guarded scope
    
    forms/source/component/DatabaseForm.cxx: fixed clear-reset sequence
    broken from the initial commit bf4154eb5307ec8c35f000fd1df39ef3abb2eb6d
    
    Change-Id: Ibab6660c79561eee31faf3e6c1128ab141a7e8a3

--- include/osl/mutex.hxx
+++ include/osl/mutex.hxx
@@ -175,11 +175,9 @@ namespace osl
         */
         void clear()
         {
-            if(pT)
-            {
-                pT->release();
-                pT = NULL;
-            }
+            assert(pT);
+            pT->release();
+            pT = NULL;
         }
     };
 
@@ -213,11 +211,9 @@ namespace osl
         */
         void reset()
         {
-            if( pResetT )
-            {
-                this->pT = pResetT;
-                this->pT->acquire();
-            }
+            assert(!this->pT);
+            this->pT = pResetT;
+            this->pT->acquire();
         }
     };
 
