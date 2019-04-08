5a12df9506d040e700e73c260a9a89168e71bf16
heiko tietze <tietze.heiko@gmail.com>
2019-04-08 14:35:46 +0200

    Resolves: tdf#124552 - Sentence selection UNO action
    iUNO command SelectParagraph / FN_SELECT_PARAGRAPH added
    executing SelPara()
    
    Change-Id: Idcd528cf2915b30a59a9916dcff14d60d01dcd74

--- sw/source/uibase/shells/txtcrsr.cxx
+++ sw/source/uibase/shells/txtcrsr.cxx
@@ -154,6 +154,10 @@ void SwTextShell::ExecMove(SfxRequest &rReq)
         case FN_SELECT_WORD:
             bRet = rSh.SelNearestWrd();
             break;
+        case FN_SELECT_PARAGRAPH:
+            rSh.SelPara( nullptr );
+            bRet = true;
+            break;
         case SID_SELECTALL:
             rSh.SelAll();
             bRet = true;
