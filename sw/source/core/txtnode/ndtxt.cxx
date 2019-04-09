12dc419768d99b34a59f7568cab337b5162bf3e5
Michael Stahl <Michael.Stahl@cib.de>
2019-04-09 17:15:50 +0200

    tdf#124586 sw_redlinehide: fix crash in SwTextNode::NumRuleChgd()
    (regression from c180c9447256588fe5e7991e06642883574760ae)
    
    Change-Id: Ie3c935ee5dd42187ca8ad2b28406b80e63c0d1e3
    Reviewed-on: https://gerrit.libreoffice.org/70467
    Reviewed-by: Michael Stahl <Michael.Stahl@cib.de>
    Tested-by: Michael Stahl <Michael.Stahl@cib.de>
    (cherry picked from commit 7587e39014c1b6ed505e9c10d9f0f0803fc986ac)

--- sw/source/core/txtnode/ndtxt.cxx
+++ sw/source/core/txtnode/ndtxt.cxx
@@ -2870,7 +2870,10 @@ void SwTextNode::NumRuleChgd()
         if ( pNumRule && pNumRule != GetNum()->GetNumRule() )
         {
             mpNodeNum->ChangeNumRule( *pNumRule );
-            mpNodeNumRLHidden->ChangeNumRule( *pNumRule );
+            if (mpNodeNumRLHidden)
+            {
+                mpNodeNumRLHidden->ChangeNumRule(*pNumRule);
+            }
         }
     }
 
