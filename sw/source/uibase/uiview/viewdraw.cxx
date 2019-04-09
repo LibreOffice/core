34790afec30024569a66108fa4d875ab054ab938
Henry Castro <hcastro@collabora.com>
2019-04-09 16:33:40 -0400

    sw lok: fix "Cannot deselect the shape after inserting the first in a...
    document"
    
    Change-Id: I976318fe299306b65190b4f5ae0ed2565830c6f7
    Reviewed-on: https://gerrit.libreoffice.org/70475
    Tested-by: Jenkins
    Reviewed-by: Jan Holesovsky <kendy@collabora.com>
    Reviewed-by: Henry Castro <hcastro@collabora.com>
    (cherry picked from commit 304425807e9172ac7a85d5f17fceba2735218409)
    Reviewed-on: https://gerrit.libreoffice.org/79090
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Andras Timar <andras.timar@collabora.com>

--- sw/source/uibase/uiview/viewdraw.cxx
+++ sw/source/uibase/uiview/viewdraw.cxx
@@ -346,6 +346,10 @@ void SwView::ExecDraw(SfxRequest& rReq)
             {
                 m_pWrtShell->GotoObj(true);
             }
+            else if (dynamic_cast<ConstCustomShape*>(pTempFuncPtr))
+            {
+                pTempFuncPtr->CreateDefaultObject();
+            }
             else
             {
                 pTempFuncPtr->CreateDefaultObject();
