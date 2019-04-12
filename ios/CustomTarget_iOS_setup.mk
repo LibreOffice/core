7811a0e581ca34f59daf4210c568f818d8c9dfdd
Tor Lillqvist <tml@collabora.com>
2019-04-12 15:24:05 +0300

    tdf#124449: We need also share/gallery for the iOS app
    I wonder wheter we should just include all of instdir/share. Seems
    that I have to add more and more of it all the time anyway. I don't
    remember why I thought (many years ago) that an app would need just a
    subset. (Maybe I thought that an app would not, at least not
    initially, have functionality that would use most of the stuff in
    shre. But that has changed now.)
    
    Change-Id: I62f935e3ab9c4709373fad11ed120ecca033b4aa

--- ios/CustomTarget_iOS_setup.mk
+++ ios/CustomTarget_iOS_setup.mk
@@ -71,6 +71,7 @@ $(IOSGEN)/native-code.h: $(BUILDDIR)/config_host.mk \
 	cp -R $(INSTDIR)/share/template $(IOSRES)/share
 	mkdir -p $(IOSRES)/share/fonts
 	cp -R $(INSTDIR)/share/fonts/truetype $(IOSRES)/share/fonts
+	cp -R $(INSTDIR)/share/gallery $(IOSRES)/share
 	cp $(SRCDIR)/ios/welcome.odt $(IOSRES)
 
 	# Set up rc (the "inifile", fundamentalrc, unorc, bootstraprc and versionrc.
