51fb5ce0774c463caad7bcf9a6851161a56d60f7
Michael Stahl <Michael.Stahl@cib.de>
2019-04-15 17:11:51 +0200

    tdf#123855 sw_redlinehide: fix GetExpandText() not expanding footnotes
    ... due to change of default argument; the XHTML export is calling it
    from SwXTextPortion::getString().  This is complicated a bit by a
    bunch of changes to GetExpandText() callers.
    
    (regression from bf488abbf46da51d17c3ad7ccf9e39005a55c2d7)
    
    Change-Id: I0b1e10e17c8f3824d6fa1f21fc74cc59b310474f
    Reviewed-on: https://gerrit.libreoffice.org/70791
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <Michael.Stahl@cib.de>
    (cherry picked from commit f32ddd38ef777598901981491ad57bd37efe69e8)
    Reviewed-on: https://gerrit.libreoffice.org/70809
    Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>

--- sw/source/core/crsr/crstrvl.cxx
+++ sw/source/core/crsr/crstrvl.cxx
@@ -1296,7 +1296,7 @@ bool SwCursorShell::GetContentAtPos( const Point& rPt,
             if( pONd )
             {
                 rContentAtPos.eContentAtPos = IsAttrAtPos::Outline;
-                rContentAtPos.sStr = sw::GetExpandTextMerged(GetLayout(), *pONd, true, false, ExpandMode(0));
+                rContentAtPos.sStr = sw::GetExpandTextMerged(GetLayout(), *pONd, true, false, ExpandMode::ExpandFootnote);
                 bRet = true;
             }
         }
