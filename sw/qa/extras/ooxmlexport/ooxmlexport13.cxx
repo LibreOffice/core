e674d53162a127949dbae271352e5fd96e741115
Vasily Melenchuk <vasily.melenchuk@cib.de>
2019-04-15 17:22:04 +0300

    docx export: implement text-input field export
    It is expected that <text:text-input> will be converted to
    {FORMTEXT} MS field, but practical implementation was
    missing.
    
    Change-Id: Ifd17a523e253555a240f853bea5f21572d90d853
    Reviewed-on: https://gerrit.libreoffice.org/70786
    Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>
    Tested-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>
    (cherry picked from commit b01a4437c42b3cd8484c7d7c16a2d09190c1ee97)

--- sw/qa/extras/ooxmlexport/ooxmlexport13.cxx
+++ sw/qa/extras/ooxmlexport/ooxmlexport13.cxx
@@ -70,6 +70,18 @@ DECLARE_OOXMLEXPORT_TEST(testFrameSizeExport, "floating-tables-anchor.docx")
     assertXPath(pXmlDoc, "/w:document/w:body/w:tbl[1]/w:tblPr/w:tblW", "w", "4000");
 }
 
+DECLARE_OOXMLEXPORT_TEST(testTextInput, "textinput.odt")
+{
+    xmlDocPtr pXmlDoc = parseExport("word/document.xml");
+    if (!pXmlDoc)
+        return;
+    // Ensure we have a formfield
+    assertXPathContent(pXmlDoc, "/w:document/w:body/w:p/w:r[3]/w:instrText", " FORMTEXT ");
+    // and it's content is not gone
+    assertXPathContent(pXmlDoc, "/w:document/w:body/w:p/w:r[5]/w:t", "SomeText");
+}
+
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
