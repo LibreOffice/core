4f19bc0491c11a93420cfb2d9aa4e118a180858f
Michael Weghorn <m.weghorn@posteo.de>
2019-04-16 16:48:58 +0200

    gtk3_kde5: Increase fpicker process timeout
    In some observed cases, 100 ms were not enough for the separate
    'lo_kde5filepicker' process to terminate properly, in particular
    on slow or busy hardware.
    
    Give the process more time to avoid killing it unnecessarily,
    which could e.g. lead to file dialog settings/state not properly
    being saved by the native KDE Plasma QFileDialog (plasma-integration,
    e.g. in 'KDEPlatformFileDialogHelper::saveSize'), so that the last
    used mode or size would not be remembered.
    
    Change-Id: I0a58c1425940fbfaecb5012a24fd9a0222cbaccb

--- vcl/unx/gtk3_kde5/gtk3_kde5_filepicker_ipc.cxx
+++ vcl/unx/gtk3_kde5/gtk3_kde5_filepicker_ipc.cxx
@@ -139,9 +139,13 @@ Gtk3KDE5FilePickerIpc::~Gtk3KDE5FilePickerIpc()
         return;
 
     sendCommand(Commands::Quit);
-    TimeValue timeValue(std::chrono::milliseconds(100));
+    TimeValue timeValue(std::chrono::milliseconds(2000));
     if (osl_joinProcessWithTimeout(m_process, &timeValue) != osl_Process_E_None)
+    {
+        SAL_WARN("vcl.gtk3_kde5",
+                 "File picker process did not finish in time and will be terminated.");
         osl_terminateProcess(m_process);
+    }
 
     if (m_inputWrite)
         osl_closeFile(m_inputWrite);
