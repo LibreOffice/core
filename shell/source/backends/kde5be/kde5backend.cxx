85be8807c9b6f3248467aa29ba01e006e96dfaff
Katarina Behrens <Katarina.Behrens@cib.de>
2019-04-16 09:47:31 +0200

    Stop qt event loop after KDE settings have been read
    Two use-cases here in kde5backend
    1) kde or qt vclplug has already started qt event loop => just use this
    loop to read KDE settings
    2) no qt event loop runs (we're most likely in gtk3_kde5 vclplug) =>
    start a new event loop, read the settings and stop it
    
    In case 2) letting qt event loop run means subsequently all UI ops
    need to happen in main thread. This is problematic to enforce in
    non-qt-based vclplugs
    
    In both cases, cache those settings for future use.
    
    Change-Id: Ifa203f4cdb9a753db808f945762fb131ee83393c

--- shell/source/backends/kde5be/kde5backend.cxx
+++ shell/source/backends/kde5be/kde5backend.cxx
@@ -121,6 +121,7 @@ private:
     }
 
     bool enabled_;
+    std::map<OUString, css::beans::Optional<css::uno::Any>> m_KDESettings;
 };
 
 OString getDisplayArg()
@@ -148,10 +149,28 @@ OString getExecutable()
     return OUStringToOString(aBin, osl_getThreadTextEncoding());
 }
 
+void readKDESettings(std::map<OUString, css::beans::Optional<css::uno::Any>>& rSettings)
+{
+    std::vector<OUString> aKeys
+        = { "EnableATToolSupport",  "ExternalMailer",       "SourceViewFontHeight",
+            "SourceViewFontName",   "WorkPathVariable",     "ooInetFTPProxyName",
+            "ooInetFTPProxyPort",   "ooInetHTTPProxyName",  "ooInetHTTPProxyPort",
+            "ooInetHTTPSProxyName", "ooInetHTTPSProxyPort", "ooInetNoProxy",
+            "ooInetProxyType" };
+
+    for (OUString& aKey : aKeys)
+    {
+        css::beans::Optional<css::uno::Any> aValue = kde5access::getValue(aKey);
+        std::pair<OUString, css::beans::Optional<css::uno::Any>> elem
+            = std::make_pair(aKey, aValue);
+        rSettings.insert(elem);
+    }
+}
+
 // init the QApplication when we load the kde5backend into a non-Qt vclplug (e.g. Gtk3KDE5)
 // TODO: use a helper process to read these values without linking to Qt directly?
 // TODO: share this code somehow with Qt5Instance.cxx?
-void initQApp()
+void initQApp(std::map<OUString, css::beans::Optional<css::uno::Any>>& rSettings)
 {
     const auto aDisplay = getDisplayArg();
     int nFakeArgc = aDisplay.isEmpty() ? 2 : 3;
@@ -169,12 +188,14 @@ void initQApp()
         unsetenv("SESSION_MANAGER");
     }
 
-    auto app = new QApplication(nFakeArgc, pFakeArgv);
+    QApplication* app = new QApplication(nFakeArgc, pFakeArgv);
     QObject::connect(app, &QObject::destroyed, app, [nFakeArgc, pFakeArgv]() {
         for (int i = 0; i < nFakeArgc; ++i)
             delete pFakeArgv[i];
         delete[] pFakeArgv;
     });
+
+    readKDESettings(rSettings);
 
     if (session_manager != nullptr)
     {
@@ -183,7 +204,8 @@ void initQApp()
         free(session_manager);
     }
 
-    QApplication::setQuitOnLastWindowClosed(false);
+    // kill this w/ fire to stop qt event loop
+    delete app;
 }
 
 Service::Service()
@@ -192,10 +214,15 @@ Service::Service()
     css::uno::Reference<css::uno::XCurrentContext> context(css::uno::getCurrentContext());
     if (context.is())
     {
-        if (!qApp)
+        if (!qApp) // no qt event loop yet
         {
-            initQApp();
+            // so we start one and read KDE settings
+            initQApp(m_KDESettings);
         }
+        else // someone else (most likely kde/qt vclplug) has started qt event loop
+            // all that is left to do is to read KDE settings
+            readKDESettings(m_KDESettings);
+
         OUString desktop;
         context->getValueByName("system.desktop-environment") >>= desktop;
         enabled_ = desktop == "KDE5" && qApp != nullptr;
@@ -218,8 +245,12 @@ css::uno::Any Service::getPropertyValue(OUString const& PropertyName)
         || PropertyName == "ooInetHTTPSProxyPort" || PropertyName == "ooInetNoProxy"
         || PropertyName == "ooInetProxyType")
     {
-        return css::uno::makeAny(enabled_ ? kde5access::getValue(PropertyName)
-                                          : css::beans::Optional<css::uno::Any>());
+        std::map<OUString, css::beans::Optional<css::uno::Any>>::iterator it
+            = m_KDESettings.find(PropertyName);
+        if (it != m_KDESettings.end())
+            return css::uno::makeAny(it->second);
+        else
+            return css::uno::makeAny(css::beans::Optional<css::uno::Any>());
     }
     else if (PropertyName == "givenname" || PropertyName == "sn"
              || PropertyName == "TemplatePathVariable")
