e2190fa056fea36dd0fc1ae16af409b0e9776a69
Justin Luth <justin.luth@collabora.com>
2019-04-16 14:14:03 +0300

    tdf#123636 writerfilter: split new-line also if PAGE_BREAK
    All of the other cases (COLUMN_BREAK and non-empty line) split
    the paragraph, so why not here?  These documents show it is needed.
    (I see that I was the one who added COLUMN_BREAK for newLine a
    few years ago. So now I have proof documents for the other
    case as well.)
    
    At first I was trying to be safer by splitting only if
    GetSplitPgBreakAndParaMark() since that worked with the unit test
    that I designed. However, it didn't fix the original bug document,
    so I added both documents as tests since they act differently.
    
    Change-Id: I27399780c909663f9a2b21974a5b385bea67f9ec

--- writerfilter/source/dmapper/DomainMapper.cxx
+++ writerfilter/source/dmapper/DomainMapper.cxx
@@ -3292,6 +3292,17 @@ void DomainMapper::lcl_utext(const sal_uInt8 * data_, size_t len)
             {
                 if (m_pImpl->isBreakDeferred(PAGE_BREAK))
                 {
+                    /* If PAGEBREAK appears in first paragraph of the section or
+                     * after first run of any paragraph then need to split paragraph
+                     * to handle it properly.
+                     */
+                    if (m_pImpl->GetIsFirstParagraphInSection() || !m_pImpl->IsFirstRun())
+                    {
+                        m_pImpl->m_bIsSplitPara = true;
+                        m_pImpl->finishParagraph( m_pImpl->GetTopContextOfType(CONTEXT_PARAGRAPH) );
+                        lcl_startParagraphGroup();
+                    }
+
                     if (m_pImpl->GetSettingsTable()->GetSplitPgBreakAndParaMark())
                     {
                         pContext->Insert(PROP_BREAK_TYPE, uno::makeAny(style::BreakType_PAGE_BEFORE));
