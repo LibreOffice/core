1e917af27d18d934f3e9122aa98cd950b74ca246
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2019-04-17 12:24:17 +0900

    tdf#124032 fix Edit background drawing
    IsPaintTransparent() is preventing to paint the background but
    not sure why this is needed. removing this condition doesn't
    seem to have any ill effects.
    
    Change-Id: I5ac54e208e4f1c9941beb4012aa44182d21dbed9
    Reviewed-on: https://gerrit.libreoffice.org/70855
    Tested-by: Jenkins
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

--- vcl/source/control/edit.cxx
+++ vcl/source/control/edit.cxx
@@ -329,6 +329,7 @@ void Edit::ImplInit(vcl::Window* pParent, WinBits nStyle)
     SetCursor( new vcl::Cursor );
 
     SetPointer( PointerStyle::Text );
+    ApplySettings(*this);
 
     uno::Reference< datatransfer::dnd::XDragGestureListener> xDGL( mxDnDListener, uno::UNO_QUERY );
     uno::Reference< datatransfer::dnd::XDragGestureRecognizer > xDGR = GetDragGestureRecognizer();
@@ -382,7 +383,7 @@ void Edit::ApplySettings(vcl::RenderContext& rRenderContext)
     Color aTextColor = rStyleSettings.GetFieldTextColor();
     ApplyControlForeground(rRenderContext, aTextColor);
 
-    if (ImplUseNativeBorder(rRenderContext, GetStyle()) || IsPaintTransparent())
+    if (ImplUseNativeBorder(rRenderContext, GetStyle()))
     {
         // Transparent background
         rRenderContext.SetBackground();
