LibreOffice/core

commit 48314f25241e014a634dd5371543b90137ffd2bc

improve function-local statics in basic..cui
Change-Id: If737e8478f6f1c8fffb060ce132d80e0f07ef8ee
Reviewed-on: https://gerrit.libreoffice.org/63701
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

connectivity/source/drivers/firebird/DatabaseMetaData.cxx
@@ -805,10 +805,9 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getTypeInfo()
     ODatabaseMetaDataResultSet* pResultSet =
             new ODatabaseMetaDataResultSet(ODatabaseMetaDataResultSet::eTypeInfo);
     uno::Reference< XResultSet > xResultSet = pResultSet;
-    static ODatabaseMetaDataResultSet::ORows aResults;
-
-    if(aResults.empty())
+    static ODatabaseMetaDataResultSet::ORows aResults = [&]()
     {
+        ODatabaseMetaDataResultSet::ORows tmp;
         ODatabaseMetaDataResultSet::ORow aRow(19);
 
         // Common data
@@ -838,7 +837,7 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getTypeInfo()
         aRow[12] = new ORowSetValueDecorator(false); // Autoincrement
         aRow[14] = ODatabaseMetaDataResultSet::get0Value(); // Minimum scale
         aRow[15] = ODatabaseMetaDataResultSet::get0Value(); // Max scale
-        aResults.push_back(aRow);
+        tmp.push_back(aRow);
 
         // Varchar
         aRow[1] = new ORowSetValueDecorator(OUString("VARCHAR"));
@@ -850,7 +849,7 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getTypeInfo()
         aRow[12] = new ORowSetValueDecorator(false); // Autoincrement
         aRow[14] = ODatabaseMetaDataResultSet::get0Value(); // Minimum scale
         aRow[15] = ODatabaseMetaDataResultSet::get0Value(); // Max scale
-        aResults.push_back(aRow);
+        tmp.push_back(aRow);
 
         // Binary (CHAR)
         // It is distinguished from Text type by its character set
@@ -862,7 +861,7 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getTypeInfo()
                 sal_Int16(ColumnSearch::NONE)); // Searchable
         aRow[14] = ODatabaseMetaDataResultSet::get0Value(); // Minimum scale
         aRow[15] = ODatabaseMetaDataResultSet::get0Value(); // Max scale
-        aResults.push_back(aRow);
+        tmp.push_back(aRow);
 
         // Varbinary (VARCHAR)
         aRow[1] = new ORowSetValueDecorator(OUString("VARCHAR"));
@@ -882,12 +881,12 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getTypeInfo()
         aRow[12] = new ORowSetValueDecorator(false); // Autoincrement
         aRow[14] = ODatabaseMetaDataResultSet::get0Value(); // Minimum scale
         aRow[15] = ODatabaseMetaDataResultSet::get0Value(); // Max scale
-        aResults.push_back(aRow);
+        tmp.push_back(aRow);
 
         // Longvarbinary (SQL_BLOB)
         // Distinguished from simple blob with a user-defined subtype.
         aRow[2] = new ORowSetValueDecorator(DataType::LONGVARBINARY);
-        aResults.push_back(aRow);
+        tmp.push_back(aRow);
 
         // Integer Types common
         {
@@ -902,17 +901,17 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getTypeInfo()
         aRow[1] = new ORowSetValueDecorator(OUString("SMALLINT"));
         aRow[2] = new ORowSetValueDecorator(DataType::SMALLINT);
         aRow[3] = new ORowSetValueDecorator(sal_Int16(5)); // Prevision
-        aResults.push_back(aRow);
+        tmp.push_back(aRow);
         // Integer (SQL_LONG)
         aRow[1] = new ORowSetValueDecorator(OUString("INTEGER"));
         aRow[2] = new ORowSetValueDecorator(DataType::INTEGER);
         aRow[3] = new ORowSetValueDecorator(sal_Int16(10)); // Precision
-        aResults.push_back(aRow);
+        tmp.push_back(aRow);
         // Bigint (SQL_INT64)
         aRow[1] = new ORowSetValueDecorator(OUString("BIGINT"));
         aRow[2] = new ORowSetValueDecorator(DataType::BIGINT);
         aRow[3] = new ORowSetValueDecorator(sal_Int16(20)); // Precision
-        aResults.push_back(aRow);
+        tmp.push_back(aRow);
 
         // Decimal Types common
         {
@@ -928,14 +927,14 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getTypeInfo()
         aRow[3] = new ORowSetValueDecorator(sal_Int16(15)); // Precision
         aRow[14] = new ORowSetValueDecorator(sal_Int16(1)); // Minimum scale
         aRow[15] = new ORowSetValueDecorator(sal_Int16(15)); // Max scale
-        aResults.push_back(aRow);
+        tmp.push_back(aRow);
         // Decimal
         aRow[1] = new ORowSetValueDecorator(OUString("DECIMAL"));
         aRow[2] = new ORowSetValueDecorator(DataType::DECIMAL);
         aRow[3] = new ORowSetValueDecorator(sal_Int16(15)); // Precision
         aRow[14] = new ORowSetValueDecorator(sal_Int16(1)); // Minimum scale
         aRow[15] = new ORowSetValueDecorator(sal_Int16(15)); // Max scale
-        aResults.push_back(aRow);
+        tmp.push_back(aRow);
 
         aRow[6] = new ORowSetValueDecorator(); // Create Params
         // Float (SQL_FLOAT)
@@ -944,14 +943,14 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getTypeInfo()
         aRow[3] = new ORowSetValueDecorator(sal_Int16(7)); // Precision
         aRow[14] = new ORowSetValueDecorator(sal_Int16(1)); // Minimum scale
         aRow[15] = new ORowSetValueDecorator(sal_Int16(7)); // Max scale
-        aResults.push_back(aRow);
+        tmp.push_back(aRow);
         // Double (SQL_DOUBLE)
         aRow[1] = new ORowSetValueDecorator(OUString("DOUBLE PRECISION"));
         aRow[2] = new ORowSetValueDecorator(DataType::DOUBLE);
         aRow[3] = new ORowSetValueDecorator(sal_Int16(15)); // Precision
         aRow[14] = new ORowSetValueDecorator(sal_Int16(1)); // Minimum scale
         aRow[15] = new ORowSetValueDecorator(sal_Int16(15)); // Max scale
-        aResults.push_back(aRow);
+        tmp.push_back(aRow);
 
         // TODO: no idea whether D_FLOAT corresponds to an sql type
 
@@ -965,7 +964,7 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getTypeInfo()
         aRow[12] = new ORowSetValueDecorator(false); // Autoincrement
         aRow[14] = ODatabaseMetaDataResultSet::get0Value(); // Minimum scale
         aRow[15] = ODatabaseMetaDataResultSet::get0Value(); // Max scale
-        aResults.push_back(aRow);
+        tmp.push_back(aRow);
 
         // SQL_TYPE_TIME
         aRow[1] = new ORowSetValueDecorator(OUString("TIME"));
@@ -989,7 +988,7 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getTypeInfo()
         aRow[12] = new ORowSetValueDecorator(false); // Autoincrement
         aRow[14] = ODatabaseMetaDataResultSet::get0Value(); // Minimum scale
         aRow[15] = ODatabaseMetaDataResultSet::get0Value(); // Max scale
-        aResults.push_back(aRow);
+        tmp.push_back(aRow);
 
         // SQL_BLOB
         aRow[1] = new ORowSetValueDecorator(OUString("BLOB"));
@@ -1001,7 +1000,7 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getTypeInfo()
         aRow[12] = new ORowSetValueDecorator(false); // Autoincrement
         aRow[14] = ODatabaseMetaDataResultSet::get0Value(); // Minimum scale
         aRow[15] = ODatabaseMetaDataResultSet::get0Value(); // Max scale
-        aResults.push_back(aRow);
+        tmp.push_back(aRow);
 
         // SQL_BOOLEAN
         aRow[1] = new ORowSetValueDecorator(OUString("BOOLEAN"));
@@ -1013,9 +1012,9 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getTypeInfo()
         aRow[12] = new ORowSetValueDecorator(false); // Autoincrement
         aRow[14] = ODatabaseMetaDataResultSet::get0Value(); // Minimum scale
         aRow[15] = ODatabaseMetaDataResultSet::get0Value(); // Max scale
-        aResults.push_back(aRow);
-
-    }
+        tmp.push_back(aRow);
+        return tmp;
+    }();
     pResultSet->setRows(aResults);
     return xResultSet;
 }

basic/source/comp/basiccharclass.cxx
@@ -34,9 +34,7 @@ bool BasicCharClass::isLetter( sal_Unicode c )
 
 bool BasicCharClass::isLetterUnicode( sal_Unicode c )
 {
-  static CharClass* pCharClass = nullptr;
-  if( pCharClass == nullptr )
-    pCharClass = new CharClass( Application::GetSettings().GetLanguageTag() );
+  static CharClass* pCharClass = new CharClass( Application::GetSettings().GetLanguageTag() );
   // can we get pCharClass to accept a sal_Unicode instead of this waste?
   return pCharClass->isLetter( OUString(c), 0 );
 }

basic/source/runtime/methods.cxx
@@ -129,14 +129,7 @@ static long GetDayDiff( const Date& rDate );
 
 static const CharClass& GetCharClass()
 {
-    static bool bNeedsInit = true;
-    static LanguageTag aLanguageTag( LANGUAGE_SYSTEM);
-    if( bNeedsInit )
-    {
-        bNeedsInit = false;
-        aLanguageTag = Application::GetSettings().GetLanguageTag();
-    }
-    static CharClass aCharClass( aLanguageTag );
+    static CharClass aCharClass( Application::GetSettings().GetLanguageTag() );
     return aCharClass;
 }
 
@@ -169,11 +162,7 @@ OUString getFullPath( const OUString& aRelPath )
 // TODO: -> SbiGlobals
 static uno::Reference< ucb::XSimpleFileAccess3 > const & getFileAccess()
 {
-    static uno::Reference< ucb::XSimpleFileAccess3 > xSFI;
-    if( !xSFI.is() )
-    {
-        xSFI = ucb::SimpleFileAccess::create( comphelper::getProcessComponentContext() );
-    }
+    static uno::Reference< ucb::XSimpleFileAccess3 > xSFI = ucb::SimpleFileAccess::create( comphelper::getProcessComponentContext() );
     return xSFI;
 }
 

basic/source/runtime/methods1.cxx
@@ -70,13 +70,7 @@ using namespace com::sun::star::uno;
 
 static Reference< XCalendar4 > const & getLocaleCalendar()
 {
-    static Reference< XCalendar4 > xCalendar;
-    if( !xCalendar.is() )
-    {
-        Reference< XComponentContext > xContext = getProcessComponentContext();
-        xCalendar = LocaleCalendar2::create(xContext);
-    }
-
+    static Reference< XCalendar4 > xCalendar = LocaleCalendar2::create(getProcessComponentContext());
     static css::lang::Locale aLastLocale;
     static bool bNeedsInit = true;
 

basic/source/runtime/runtime.cxx
@@ -1337,14 +1337,12 @@ void SbiRuntime::StepCompare( SbxOperator eOp )
     // I dumbly follow the pattern :-/
     if ( bVBAEnabled && ( p1->IsNull() || p2->IsNull() ) )
     {
-        static SbxVariable* pNULL = nullptr;
-
-        if( !pNULL )
-        {
-            pNULL = new SbxVariable;
-            pNULL->PutNull();
-            pNULL->AddFirstRef();
-        }
+        static SbxVariable* pNULL = [&]() {
+            SbxVariable* p = new SbxVariable;
+            p->PutNull();
+            p->AddFirstRef();
+            return p;
+        }();
         PushVar( pNULL );
     }
     else if( p2->Compare( eOp, *p1 ) )

comphelper/source/misc/backupfilehelper.cxx
@@ -2044,29 +2044,25 @@ namespace comphelper
 
     const std::vector< OUString >& BackupFileHelper::getCustomizationDirNames()
     {
-        static std::vector< OUString > aDirNames;
-
-        if (aDirNames.empty())
+        static std::vector< OUString > aDirNames =
         {
-            aDirNames.emplace_back("config");     // UI config stuff
-            aDirNames.emplace_back("registry");   // most of the registry stuff
-            aDirNames.emplace_back("psprint");    // not really needed, can be abandoned
-            aDirNames.emplace_back("store");      // not really needed, can be abandoned
-            aDirNames.emplace_back("temp");       // not really needed, can be abandoned
-            aDirNames.emplace_back("pack");       // own backup dir
-        }
+            "config",     // UI config stuff
+            "registry",   // most of the registry stuff
+            "psprint",    // not really needed, can be abandoned
+            "store",      // not really needed, can be abandoned
+            "temp",       // not really needed, can be abandoned
+            "pack"       // own backup dir
+        };
 
         return aDirNames;
     }
 
     const std::vector< OUString >& BackupFileHelper::getCustomizationFileNames()
     {
-        static std::vector< OUString > aFileNames;
-
-        if (aFileNames.empty())
+        static std::vector< OUString > aFileNames =
         {
-            aFileNames.emplace_back("registrymodifications.xcu"); // personal registry stuff
-        }
+            "registrymodifications.xcu" // personal registry stuff
+        };
 
         return aFileNames;
     }

comphelper/source/misc/threadpool.cxx
@@ -109,8 +109,7 @@ ThreadPool& ThreadPool::getSharedOptimalPool()
 
 sal_Int32 ThreadPool::getPreferredConcurrency()
 {
-    static sal_Int32 ThreadCount = 0;
-    if (ThreadCount == 0)
+    static sal_Int32 ThreadCount = [&]()
     {
         const sal_Int32 nHardThreads = std::max(std::thread::hardware_concurrency(), 1U);
         sal_Int32 nThreads = nHardThreads;
@@ -122,8 +121,8 @@ sal_Int32 ThreadPool::getPreferredConcurrency()
         }
 
         nThreads = std::min(nHardThreads, nThreads);
-        ThreadCount = std::max<sal_Int32>(nThreads, 1);
-    }
+        return std::max<sal_Int32>(nThreads, 1);
+    }();
 
     return ThreadCount;
 }

configmgr/source/lock.cxx
@@ -27,9 +27,7 @@ namespace configmgr {
 
 std::shared_ptr<osl::Mutex> const & lock()
 {
-    static std::shared_ptr<osl::Mutex> theLock;
-    if (!theLock.get())
-        theLock.reset(new osl::Mutex);
+    static std::shared_ptr<osl::Mutex> theLock(new osl::Mutex);
     return theLock;
 }
 

commit 42feb447bbb553e40db1e9d410957c5d999daf11

Fix use of variable before its lifetime begins
This has been broken with 48314f25241e014a634dd5371543b90137ffd2bc "improve
function-local statics in basic..cui" (and has been found with a new plugin to
be commited).

Change-Id: I518360e3273af9db4a8560a89bc8a789410c8033

connectivity/source/drivers/firebird/DatabaseMetaData.cxx
@@ -976,7 +976,7 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getTypeInfo()
         aRow[12] = new ORowSetValueDecorator(false); // Autoincrement
         aRow[14] = ODatabaseMetaDataResultSet::get0Value(); // Minimum scale
         aRow[15] = ODatabaseMetaDataResultSet::get0Value(); // Max scale
-        aResults.push_back(aRow);
+        tmp.push_back(aRow);
 
         // SQL_TYPE_DATE
         aRow[1] = new ORowSetValueDecorator(OUString("DATE"));