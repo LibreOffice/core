04bab17562c5cf460ad93bcee8725393b8472b88
Mike Kaganski <mike.kaganski@collabora.com>
2019-04-17 18:02:49 +0300

    Add a test to check XPath testing facilities in XmlTestTools
    ... to guarantee that e.g. getXPath will always fail for non-empty
    non-existing attribute
    
    Change-Id: I22cf932eda020abf16608341fcd1769b35478883

---
+++ test/CppunitTest_test_xpath.mk
@@ -0,0 +1,27 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_CppunitTest_CppunitTest,test_xpath))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,test_xpath, \
+    test/qa/cppunit/test_xpath \
+))
+
+$(eval $(call gb_CppunitTest_use_externals,test_xpath,\
+    libxml2 \
+))
+
+$(eval $(call gb_CppunitTest_use_libraries,test_xpath, \
+    sal \
+    test \
+))
+
+$(eval $(call gb_CppunitTest_use_sdk_api,test_xpath))
+
+# vim: set noet sw=4 ts=4:
---
+++ test/qa/cppunit/test_xpath.cxx
@@ -0,0 +1,40 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <sal/types.h>
+#include <test/xmltesttools.hxx>
+#include <unotest/bootstrapfixturebase.hxx>
+
+class TestXPath : public CppUnit::TestFixture, public XmlTestTools
+{
+};
+
+CPPUNIT_TEST_FIXTURE(TestXPath, test_getXPath)
+{
+    const xmlChar s_xml[] = "<?xml version='1.0' encoding='UTF-8' standalone='yes'?>"
+                            "<xml><item attrib='val'>text</item></xml>";
+    xmlDocPtr pTable = xmlParseDoc(s_xml);
+    CPPUNIT_ASSERT(pTable);
+    // Must get existing element content without errors
+    CPPUNIT_ASSERT_ASSERTION_PASS(getXPath(pTable, "/xml/item", ""));
+    // Must error out when getting non-existing element
+    CPPUNIT_ASSERT_ASSERTION_FAIL(getXPath(pTable, "/xml/no_item", ""));
+    // Must get existing attribute value correctly
+    CPPUNIT_ASSERT_ASSERTION_PASS(getXPath(pTable, "/xml/item", "attrib"));
+    // Must fail when requested non-empty attribute doesn't exist
+    CPPUNIT_ASSERT_ASSERTION_FAIL(getXPath(pTable, "/xml/item", "no_attrib"));
+    // Must return empty string if not asking an attribute, regardless what is its content
+    CPPUNIT_ASSERT_EQUAL(OUString(), getXPath(pTable, "/xml/item", ""));
+    // Must properly return attribute content
+    CPPUNIT_ASSERT_EQUAL(OUString("val"), getXPath(pTable, "/xml/item", "attrib"));
+}
+
+CPPUNIT_PLUGIN_IMPLEMENT();
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
