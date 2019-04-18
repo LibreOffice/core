b2f419740375bc13cd6e869ddf0d9cbd9065c49c
Andrés Maldonado <amaldonado@linagora.com>
2019-04-18 15:24:50 +0200

    tdf#124824 FILEOPEN: Error rendering page break between two tables in a RTF
    This patch is adapted from an old unpublished Linagora patch by mderoucy@linagora.com
    
    Change-Id: I49bd9a9797ef42cd110259bd0dd4d4747300da71

--- writerfilter/source/rtftok/rtfdocumentimpl.cxx
+++ writerfilter/source/rtftok/rtfdocumentimpl.cxx
@@ -264,6 +264,7 @@ RTFDocumentImpl::RTFDocumentImpl(uno::Reference<uno::XComponentContext> const& x
     , m_bNeedCrOrig(false)
     , m_bNeedPar(true)
     , m_bNeedFinalPar(false)
+    , m_bNeedTableBreak(false)
     , m_nNestedCells(0)
     , m_nTopLevelCells(0)
     , m_nInheritingCells(0)
@@ -700,6 +701,8 @@ void RTFDocumentImpl::sectBreak(bool bFinal)
     }
     m_bNeedPar = false;
     m_bNeedSect = false;
+
+    m_bNeedTableBreak = true;
 }
 
 Color RTFDocumentImpl::getColorTable(sal_uInt32 nIndex)
