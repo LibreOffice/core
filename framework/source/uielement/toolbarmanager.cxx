LibreOffice/core

commit 1a5b12aa5da2c718848d3cc5d9bce7bfcdeacf54

optimise find/insert pattern
if we're doing a find/insert on a set or a map, it is better to just do
a conditional insert/emplace operation than triggering two lookups.

Change-Id: I80da5097f5a89fe30fa348ce5b6e747c34287a8d
Reviewed-on: https://gerrit.libreoffice.org/70937
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

framework/source/uielement/toolbarmanager.cxx
@@ -1042,16 +1042,14 @@ void ToolBarManager::FillToolbar( const Reference< XIndexAccess >& rItemContaine
                     // Fill command map. It stores all our commands and from what
                     // image manager we got our image. So we can decide if we have to use an
                     // image from a notification message.
-                    CommandToInfoMap::iterator pIter = m_aCommandMap.find( aCommandURL );
-                    if ( pIter == m_aCommandMap.end())
+                    auto pIter = m_aCommandMap.emplace( aCommandURL, aCmdInfo );
+                    if ( pIter.second )
                     {
                         aCmdInfo.nId = nId;
-                        const CommandToInfoMap::value_type aValue( aCommandURL, aCmdInfo );
-                        m_aCommandMap.insert( aValue );
                     }
                     else
                     {
-                        pIter->second.aIds.push_back( nId );
+                        pIter.first->second.aIds.push_back( nId );
                     }
 
                     if ( !bIsVisible )
@@ -1197,16 +1195,15 @@ void ToolBarManager::FillOverflowToolbar( ToolBox const * pParent )
             // Fill command map. It stores all our commands and from what
             // image manager we got our image. So we can decide if we have to use an
             // image from a notification message.
-            CommandToInfoMap::iterator pIter = m_aCommandMap.find( aCommandURL );
-            if ( pIter == m_aCommandMap.end())
+            auto pIter = m_aCommandMap.emplace( aCommandURL, aCmdInfo );
+            if ( pIter.second )
             {
                 aCmdInfo.nId = nId;
                 const CommandToInfoMap::value_type aValue( aCommandURL, aCmdInfo );
-                m_aCommandMap.insert( aValue );
             }
             else
             {
-                pIter->second.aIds.push_back( nId );
+                pIter.first->second.aIds.push_back( nId );
             }
         }
         else

basctl/source/basicide/documentenumeration.cxx
@@ -124,11 +124,10 @@ namespace basctl { namespace docs {
                         // those
                         continue;
 
-                    if ( aEncounteredModels.find( xModel ) != aEncounteredModels.end() )
+                    if ( !aEncounteredModels.insert( xModel ).second )
                         // there might be multiple frames for the same model
                         // handle it only once
                         continue;
-                    aEncounteredModels.insert( xModel );
 
                     // create a DocumentDescriptor
                     DocumentDescriptor aDescriptor;

bridges/source/cpp_uno/shared/vtablefactory.cxx
@@ -155,11 +155,12 @@ sal_Int32 VtableFactory::BaseOffset::calculate(
     typelib_InterfaceTypeDescription * type, sal_Int32 offset)
 {
     OUString name(type->aBase.pTypeName);
-    if (m_map.find(name) == m_map.end()) {
+    auto it = m_map.find(name);
+    if (it == m_map.end()) {
         for (sal_Int32 i = 0; i < type->nBaseTypes; ++i) {
             offset = calculate(type->ppBaseTypes[i], offset);
         }
-        m_map.insert({name, offset});
+        m_map.insert(it, {name, offset});
         typelib_typedescription_complete(
             reinterpret_cast< typelib_TypeDescription ** >(&type));
         offset += bridges::cpp_uno::shared::getLocalFunctions(type);

compilerplugins/clang/cstylecast.cxx
@@ -603,8 +603,7 @@ bool CStyleCast::rewriteArithmeticCast(CStyleCastExpr const * expr, char const *
         secondBegin = l;
         ++secondLen;
     }
-    if (rewritten_.find(firstBegin) == rewritten_.end()) {
-        rewritten_.insert(firstBegin);
+    if (rewritten_.insert(firstBegin).second) {
         if (!replaceText(firstBegin, firstLen, functional ? "" : "static_cast<")) {
             if (isDebugMode()) {
                 report(
@@ -630,8 +629,7 @@ bool CStyleCast::rewriteArithmeticCast(CStyleCastExpr const * expr, char const *
         }
     }
     if (third.isValid()) {
-        if (rewritten_.find(third) == rewritten_.end()) {
-            rewritten_.insert(third);
+        if (rewritten_.insert(third).second) {
             if (!insertTextBefore(third, "(")) {
                 //TODO: roll back
                 if (isDebugMode()) {

compilerplugins/clang/memoryvar.cxx
@@ -130,8 +130,7 @@ bool MemoryVar::VisitCXXDeleteExpr(const CXXDeleteExpr *deleteExpr)
 
     SourceLocation loc = varDecl->getLocation();
 
-    if (maVarUsesSet.find(loc) == maVarUsesSet.end()) {
-        maVarUsesSet.insert(loc);
+    if (maVarUsesSet.insert(loc).second) {
         maVarDeclSourceRangeMap[loc] = varDecl->getSourceRange();
         maVarDeleteSourceRangeMap[loc] = declRefExpr->getSourceRange();
     }

configmgr/source/xcsparser.cxx
@@ -267,24 +267,18 @@ void XcsParser::endElement(xmlreader::XmlReader const & reader) {
                 switch (state_) {
                 case STATE_TEMPLATES:
                     {
-                        NodeMap::iterator i(data_.templates.find(top.name));
-                        if (i == data_.templates.end()) {
-                            data_.templates.insert(
-                                NodeMap::value_type(top.name, top.node));
-                        } else {
-                            merge(i->second, top.node);
+                        auto itPair = data_.templates.insert({top.name, top.node});
+                        if (!itPair.second) {
+                            merge(itPair.first->second, top.node);
                         }
                     }
                     break;
                 case STATE_COMPONENT:
                     {
                         NodeMap & components = data_.getComponents();
-                        NodeMap::iterator i(components.find(top.name));
-                        if (i == components.end()) {
-                            components.insert(
-                                NodeMap::value_type(top.name, top.node));
-                        } else {
-                            merge(i->second, top.node);
+                        auto itPair = components.insert({top.name, top.node});
+                        if (!itPair.second) {
+                            merge(itPair.first->second, top.node);
                         }
                         state_ = STATE_COMPONENT_DONE;
                     }

cui/source/dialogs/FontFeaturesDialog.cxx
@@ -71,9 +71,8 @@ void FontFeaturesDialog::initialize()
     for (vcl::font::Feature const& rFontFeature : rFontFeatures)
     {
         sal_uInt32 nFontFeatureCode = rFontFeature.m_aID.m_aFeatureCode;
-        if (aDoneFeatures.find(nFontFeatureCode) != aDoneFeatures.end())
+        if (!aDoneFeatures.insert(nFontFeatureCode).second)
             continue;
-        aDoneFeatures.insert(nFontFeatureCode);
         rFilteredFontFeatures.push_back(rFontFeature);
     }
 

package/source/zipapi/ZipFile.cxx
@@ -1065,8 +1065,7 @@ void ZipFile::recover()
                                     aEntry.nSize = 0;
                                 }
 
-                                if ( aEntries.find( aEntry.sPath ) == aEntries.end() )
-                                    aEntries[aEntry.sPath] = aEntry;
+                                aEntries.emplace( aEntry.sPath, aEntry );
                             }
                         }
                     }

commit 73f255bacb7412e58b2cfb67aab5c3e461cfaf8f

tdf#124886 Toolbar button icons are off by one
regression from
    commit 1a5b12aa5da2c718848d3cc5d9bce7bfcdeacf54
    Date:   Thu Apr 18 15:13:19 2019 +0200
    optimise find/insert pattern

Change-Id: I24edd5a89beed392c86058cd60155caf94bcf9ca

framework/source/uielement/toolbarmanager.cxx
@@ -1046,6 +1046,7 @@ void ToolBarManager::FillToolbar( const Reference< XIndexAccess >& rItemContaine
                     if ( pIter.second )
                     {
                         aCmdInfo.nId = nId;
+                        pIter.first->second.nId = nId;
                     }
                     else
                     {
@@ -1199,7 +1200,7 @@ void ToolBarManager::FillOverflowToolbar( ToolBox const * pParent )
             if ( pIter.second )
             {
                 aCmdInfo.nId = nId;
-                const CommandToInfoMap::value_type aValue( aCommandURL, aCmdInfo );
+                pIter.first->second.nId = nId;
             }
             else
             {