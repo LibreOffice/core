LibreOffice/core

commit 59339dec1ce56213dc74a06af2f0d35ac1c534d7

tdf#105150 PPTX import: try harder to handle <p:sp useBgFill="1">
The bugdoc has two shapes. The red rectangle is supposed to be covered
by the other shape, but it wasn't as the fill style was set to none. The
reason for that was we ignored useBgFill, unless the slide had an
explicit fill style. Assume that in that case a white solid fill is the
default.

Change-Id: Iea20e216e44e13db68887f285c10bc1a8feacf0a
Reviewed-on: https://gerrit.libreoffice.org/32786
Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
Tested-by: Jenkins <ci@libreoffice.org>

oox/source/ppt/pptshapegroupcontext.cxx
@@ -101,7 +101,15 @@ ContextHandlerRef PPTShapeGroupContext::onCreateContext( sal_Int32 aElementToken
             std::shared_ptr<PPTShape> pShape( new PPTShape( meShapeLocation, "com.sun.star.drawing.CustomShape" ) );
             if( rAttribs.getBool( XML_useBgFill, false ) )
             {
-                const oox::drawingml::FillPropertiesPtr pBackgroundPropertiesPtr = mpSlidePersistPtr->getBackgroundProperties();
+                oox::drawingml::FillPropertiesPtr pBackgroundPropertiesPtr = mpSlidePersistPtr->getBackgroundProperties();
+                if (!pBackgroundPropertiesPtr)
+                {
+                    // The shape wants a background, but the slide doesn't have
+                    // one: default to white.
+                    pBackgroundPropertiesPtr.reset(new oox::drawingml::FillProperties());
+                    pBackgroundPropertiesPtr->moFillType = XML_solidFill;
+                    pBackgroundPropertiesPtr->maFillColor.setSrgbClr(0xFFFFFF);
+                }
                 if ( pBackgroundPropertiesPtr ) {
                     pShape->getFillProperties().assignUsed( *pBackgroundPropertiesPtr );
                 }

sd/qa/unit/import-tests.cxx
@@ -134,6 +134,7 @@ public:
     void testTdf104201();
     void testTdf103477();
     void testTdf104445();
+    void testTdf105150();
 
     CPPUNIT_TEST_SUITE(SdImportTest);
 
@@ -192,6 +193,7 @@ public:
     CPPUNIT_TEST(testTdf104201);
     CPPUNIT_TEST(testTdf103477);
     CPPUNIT_TEST(testTdf104445);
+    CPPUNIT_TEST(testTdf105150);
 
     CPPUNIT_TEST_SUITE_END();
 };
@@ -1638,6 +1640,18 @@ void SdImportTest::testTdf103477()
     CPPUNIT_ASSERT(pNumFmt);
     CPPUNIT_ASSERT_EQUAL_MESSAGE( "Bullet's color is wrong!", sal_uInt32(0x000000), pNumFmt->GetNumRule()->GetLevel(1).GetBulletColor().GetColor());
 
+    xDocShRef->DoClose();
+}
+
+void SdImportTest::testTdf105150()
+{
+    sd::DrawDocShellRef xDocShRef = loadURL(m_directories.getURLFromSrc("sd/qa/unit/data/pptx/tdf105150.pptx"), PPTX);
+    const SdrPage* pPage = GetPage(1, xDocShRef);
+    const SdrObject* pObj = pPage->GetObj(1);
+    auto& rFillStyleItem = dynamic_cast<const XFillStyleItem&>(pObj->GetMergedItem(XATTR_FILLSTYLE));
+    // This was drawing::FillStyle_NONE, <p:sp useBgFill="1"> was ignored when
+    // the slide didn't have an explicit background fill.
+    CPPUNIT_ASSERT_EQUAL(drawing::FillStyle_SOLID, rFillStyleItem.GetValue());
     xDocShRef->DoClose();
 }
 

commit f8a998067b7bd032c2b1079ffca071c6455ed337

tdf#123684 PPTX import: fix wrong background color for <p:sp useBgFill="1">
Regression from commit 59339dec1ce56213dc74a06af2f0d35ac1c534d7
(tdf#105150 PPTX import: try harder to handle <p:sp useBgFill="1">,
2017-01-06), the problem was that we gave a white solid fill to a shape
which is meant to be transparent.

Fix the problem by limiting the scope of the mentioned commit to solid
colors only, and also extend to code to look for background fill from
the masterpage as well. This allows not hardcoding the white solid fill
and leaves the fill style of shapes as transparent where the slide
background is a bitmap or other more complex fill type.

Change-Id: I0063e88d510250652d2b14856df3bd431681422d

oox/source/ppt/pptshapegroupcontext.cxx
@@ -106,13 +106,27 @@ ContextHandlerRef PPTShapeGroupContext::onCreateContext( sal_Int32 aElementToken
                 oox::drawingml::FillPropertiesPtr pBackgroundPropertiesPtr = mpSlidePersistPtr->getBackgroundProperties();
                 if (!pBackgroundPropertiesPtr)
                 {
-                    // The shape wants a background, but the slide doesn't have
-                    // one: default to white.
-                    pBackgroundPropertiesPtr.reset(new oox::drawingml::FillProperties);
-                    pBackgroundPropertiesPtr->moFillType = XML_solidFill;
-                    pBackgroundPropertiesPtr->maFillColor.setSrgbClr(0xFFFFFF);
+                    // The shape wants a background, but the slide doesn't have one.
+                    SlidePersistPtr pMaster = mpSlidePersistPtr->getMasterPersist();
+                    if (pMaster)
+                    {
+                        oox::drawingml::FillPropertiesPtr pMasterBackground
+                            = pMaster->getBackgroundProperties();
+                        if (pMasterBackground)
+                        {
+                            if (pMasterBackground->moFillType.has()
+                                && pMasterBackground->moFillType.get() == XML_solidFill)
+                            {
+                                // Master has a solid background, use that.
+                                pBackgroundPropertiesPtr = pMasterBackground;
+                            }
+                        }
+                    }
                 }
-                pShape->getFillProperties().assignUsed( *pBackgroundPropertiesPtr );
+                if (pBackgroundPropertiesPtr)
+                {
+                    pShape->getFillProperties().assignUsed(*pBackgroundPropertiesPtr);
+                }
             }
             pShape->setModelId(rAttribs.getString( XML_modelId ).get());
             return new PPTShapeContext( *this, mpSlidePersistPtr, mpGroupShapePtr, pShape );

sd/qa/unit/import-tests.cxx
@@ -169,6 +169,7 @@ public:
     void testTdf104445();
     void testTdf105150();
     void testTdf105150PPT();
+    void testTdf123684();
     void testTdf100926();
     void testTdf89064();
     void testTdf108925();
@@ -259,6 +260,7 @@ public:
     CPPUNIT_TEST(testTdf104445);
     CPPUNIT_TEST(testTdf105150);
     CPPUNIT_TEST(testTdf105150PPT);
+    CPPUNIT_TEST(testTdf123684);
     CPPUNIT_TEST(testTdf100926);
     CPPUNIT_TEST(testPatternImport);
     CPPUNIT_TEST(testTdf89064);
@@ -1769,6 +1771,20 @@ void SdImportTest::testTdf105150()
     // This was drawing::FillStyle_NONE, <p:sp useBgFill="1"> was ignored when
     // the slide didn't have an explicit background fill.
     CPPUNIT_ASSERT_EQUAL(drawing::FillStyle_SOLID, rFillStyleItem.GetValue());
+    xDocShRef->DoClose();
+}
+
+void SdImportTest::testTdf123684()
+{
+    sd::DrawDocShellRef xDocShRef
+        = loadURL(m_directories.getURLFromSrc("sd/qa/unit/data/pptx/tdf123684.pptx"), PPTX);
+    const SdrPage* pPage = GetPage(1, xDocShRef);
+    const SdrObject* pObj = pPage->GetObj(0);
+    auto& rFillStyleItem
+        = dynamic_cast<const XFillStyleItem&>(pObj->GetMergedItem(XATTR_FILLSTYLE));
+    // Without the accompanying fix in place, this test would have failed with 'Expected: 0; Actual:
+    // 1', i.e. the shape's fill was FillStyle_SOLID, making the text of the shape unreadable.
+    CPPUNIT_ASSERT_EQUAL(drawing::FillStyle_NONE, rFillStyleItem.GetValue());
     xDocShRef->DoClose();
 }
 