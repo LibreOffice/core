9a8211c0036d9226fe03dce321134b678f255466
Caolán McNamara <caolanm@redhat.com>
2019-04-24 11:38:53 +0100

    ofz#14422 null deref
    Change-Id: Icd00e2aaa5932564668cd12ce4ee63aecc34419a

--- oox/source/drawingml/diagram/diagramlayoutatoms.cxx
+++ oox/source/drawingml/diagram/diagramlayoutatoms.cxx
@@ -1290,6 +1290,12 @@ bool LayoutNode::setupShape( const ShapePtr& rShape, const dgm::Point* pPresNode
                 continue;
             }
 
+            if (!aDataNode2->second->mpShape)
+            {
+                //busted, skip it
+                continue;
+            }
+
             rShape->setDataNodeType(aDataNode2->second->mnType);
 
             if (rItem.mnDepth == 0)
