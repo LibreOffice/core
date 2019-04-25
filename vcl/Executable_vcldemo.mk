a2638984a4b6c1400271420f549a509c369ed29e
Stephan Bergmann <sbergman@redhat.com>
2019-04-25 11:48:39 +0200

    Cut down on -pthread/-lpthread proliferation
    Building against libstdc++ effectively always requires -pthread anyway (as
    various standard C++ headers require it, see the comment added to
    solenv/gbuild/platform/unxgcc.mk), so many explicit uses of -pthread/-lpthread
    can be removed.
    
    Doing a (partial) test build on Linux with Clang -stdlib=libc++ suggests that
    libc++ indeed doesn't need -pthread as libstdc++ does.
    
    The remaining uses of -pthread/-lpthread are mostly in configure.ac for the
    various BSDs (which somebody else might want to clean up now), and related to
    external projects.  I tried to be careful to remove -pthread/-lpthread from
    makefiles only when C++ object files are involved (so -pthread will now be
    included on the link command line by default).
    
    Change-Id: I936e082839cb9a434bd273ce5a1f187a4245dfa1
    Reviewed-on: https://gerrit.libreoffice.org/71291
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

--- vcl/Executable_vcldemo.mk
+++ vcl/Executable_vcldemo.mk
@@ -53,7 +53,6 @@ $(eval $(call gb_Executable_use_static_libraries,vcldemo,\
 ifeq ($(OS), $(filter LINUX %BSD SOLARIS, $(OS)))
 $(eval $(call gb_Executable_add_libs,vcldemo,\
 	-lm $(DLOPEN_LIBS) \
-	-lpthread \
     -lX11 \
 ))
 
