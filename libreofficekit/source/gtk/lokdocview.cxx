2fea258f1886ac735fc4da3a6ea3ebf64458bb84
Michael Meeks <michael.meeks@collabora.com>
2019-04-26 21:40:05 +0100

    lok: handle missing callback.
    Change-Id: I13a200c9c9d8b0a8fde6dd15a71fe1a665d04e8d
    Reviewed-on: https://gerrit.libreoffice.org/71432
    Tested-by: Jenkins
    Reviewed-by: Michael Meeks <michael.meeks@collabora.com>
    (cherry picked from commit 85dc12a99b455126e8deae5637bf63c48ce8938e)

--- libreofficekit/source/gtk/lokdocview.cxx
+++ libreofficekit/source/gtk/lokdocview.cxx
@@ -1354,6 +1354,8 @@ callback (gpointer pData)
     case LOK_CALLBACK_INVALIDATE_HEADER:
         g_signal_emit(pCallback->m_pDocView, doc_view_signals[INVALIDATE_HEADER], 0, pCallback->m_aPayload.c_str());
         break;
+    case LOK_CALLBACK_CONTEXT_CHANGED:
+        break; // TODO
     default:
         g_assert(false);
         break;
