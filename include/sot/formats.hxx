615202cab983f1f13407f6515aeb047e4e09f6ed
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2019-04-26 20:17:00 +0900

    tdf#118977 support PDF documents as a clipboard format
    On macOS it is possible to copy from a PDF a subset of a document,
    which will be transported as a new PDF document containing the
    subset.
    LibreOffice didn't support PDF as a valid clipboard format and
    previously it also didn't support showing PDFs inside the document,
    so in such cases it copy-pasted a low resolution bitmap. The result
    wasn't good.
    
    As we are now able to display PDF documents as Graphic in LO, we
    can also support this use-case. This adds support for the PDF
    documents as a clipboard format in general and to the macOS
    backend. This commit only adds support for Writer.
    
    Change-Id: Ib982b55391b390ae06974b4ad836e376dd722a4c
    Reviewed-on: https://gerrit.libreoffice.org/71364
    Tested-by: Jenkins
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

--- include/sot/formats.hxx
+++ include/sot/formats.hxx
@@ -174,8 +174,9 @@ enum class SotClipboardFormatId : sal_uInt32
     JPEG                   = 144,
     RICHTEXT               = 145,
     STRING_TSVC            = 146,
+    PDF                    = 147,
     // the point at which we start allocating "runtime" format IDs
-    USER_END  = STRING_TSVC
+    USER_END  = PDF
 };
 
 /** Make it easier to iterate over format IDs */
