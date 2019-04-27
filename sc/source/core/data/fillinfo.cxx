LibreOffice/core

commit 7282014e362a1529a36c88eb308df8ed359c2cfa

tdf#50916 Makes numbers of columns dynamic.
With this commit we are making numbers of columns
dynamic, but the number of maximum supported
columns will be the same (1024).
Such approach will allow us to check issues
(eg. performance, LO format etc.), and improve it.

Increasing number of maximum columns, will be done
in separate commit.

Change-Id: Ibac4101e9ffc05e3548eca1c198f6319ac7ff9aa
Reviewed-on: https://gerrit.libreoffice.org/44802
Tested-by: Jenkins
Reviewed-by: Bartosz Kosiorek <gang65@poczta.onet.pl>

sc/source/core/data/fillinfo.cxx
@@ -442,7 +442,7 @@ void ScDocument::FillInfo(
     {
         SCCOL nX = (nArrCol>0) ? nArrCol-1 : MAXCOL+1;                    // negative -> invalid
 
-        if ( ValidCol(nX) )
+        if ( ValidCol(nX) && nX < maTabs[nTab]->GetAllocatedColumnsCount() )
         {
             // #i58049#, #i57939# Hidden columns must be skipped here, or their attributes
             // will disturb the output

sc/inc/address.hxx
@@ -67,6 +67,7 @@ const SCSIZE   SCSIZE_MAX   = ::std::numeric_limits<SCSIZE>::max();
 // Count values
 const SCROW       MAXROWCOUNT    = MAXROWCOUNT_DEFINE;
 const SCCOL       MAXCOLCOUNT    = MAXCOLCOUNT_DEFINE;
+const SCCOL       INITIALCOLCOUNT = 64;
 /// limiting to 10000 for now, problem with 32 bit builds for now
 const SCTAB       MAXTABCOUNT    = 10000;
 const SCCOLROW    MAXCOLROWCOUNT = MAXROWCOUNT;

sc/inc/colcontainer.hxx
@@ -28,10 +28,9 @@
 
 class ScColContainer
 {
-    typedef std::vector<std::unique_ptr<ScColumn>> ScColumnVector;
-    ScColumnVector    aCols;
-
 public:
+    typedef std::vector<std::unique_ptr<ScColumn>> ScColumnVector;
+
     ScColContainer( const size_t nSize );
     ~ScColContainer() COVERITY_NOEXCEPT_FALSE;
 
@@ -73,6 +72,9 @@ public:
 
     ScColumnVector::const_iterator begin() const { return aCols.begin(); }
     ScColumnVector::const_iterator end() const { return aCols.end(); }
+
+private:
+    ScColumnVector    aCols;
 };
 
 

sc/inc/dociter.hxx
@@ -370,7 +370,7 @@ class ScDocAttrIterator             // all attribute areas
 private:
     ScDocument*     pDoc;
     SCTAB           nTab;
-    SCCOL const     nEndCol;
+    SCCOL           nEndCol;
     SCROW const     nStartRow;
     SCROW const     nEndRow;
     SCCOL           nCol;
@@ -391,7 +391,7 @@ class ScAttrRectIterator            // all attribute areas, including areas stre
 private:
     ScDocument*     pDoc;
     SCTAB           nTab;
-    SCCOL const     nEndCol;
+    SCCOL           nEndCol;
     SCROW const     nStartRow;
     SCROW const     nEndRow;
     SCCOL           nIterStartCol;
@@ -423,7 +423,7 @@ class ScHorizontalCellIterator      // walk through all non empty cells in an ar
     ScDocument*     pDoc;
     SCTAB           mnTab;
     SCCOL const           nStartCol;
-    SCCOL const           nEndCol;
+    SCCOL                 nEndCol;
     SCROW const           nStartRow;
     SCROW const           nEndRow;
     SCCOL           mnCol;
@@ -482,7 +482,7 @@ private:
     SCTAB                   nTab;
     SCCOL const             nStartCol;
     SCROW const             nStartRow;
-    SCCOL const             nEndCol;
+    SCCOL                   nEndCol;
     SCROW const             nEndRow;
 
     std::unique_ptr<SCROW[]>  pNextEnd;

sc/inc/document.hxx
@@ -786,6 +786,8 @@ public:
     ScRangePairListRef& GetColNameRangesRef() { return xColNameRanges; }
     ScRangePairListRef& GetRowNameRangesRef() { return xRowNameRanges; }
 
+    SC_DLLPUBLIC SCCOL ClampToAllocatedColumns(SCTAB nTab, SCCOL nCol) const;
+
     SC_DLLPUBLIC ScDBCollection* GetDBCollection() const { return pDBCollection.get();}
     void                         SetDBCollection( std::unique_ptr<ScDBCollection> pNewDBCollection,
                                                   bool bRemoveAutoFilter = false );

sc/inc/table.hxx
@@ -20,6 +20,7 @@
 #ifndef INCLUDED_SC_INC_TABLE_HXX
 #define INCLUDED_SC_INC_TABLE_HXX
 
+#include <algorithm>
 #include <vector>
 #include <tools/gen.hxx>
 #include <tools/color.hxx>
@@ -151,7 +152,7 @@ class ScTable
 private:
     typedef ::std::vector< ScRange > ScRangeVec;
 
-    ScColContainer  aCol;
+    mutable ScColContainer aCol;
 
     OUString aName;
     OUString aCodeName;
@@ -273,14 +274,14 @@ public:
 
     ScOutlineTable* GetOutlineTable()               { return pOutlineTable.get(); }
 
-    ScColumn& CreateColumnIfNotExists( const SCCOL nScCol )
+    ScColumn& CreateColumnIfNotExists( const SCCOL nScCol ) const
     {
         if ( nScCol >= aCol.size() )
             CreateColumnIfNotExistsImpl(nScCol);
         return aCol[nScCol];
     }
     // out-of-line the cold part of the function
-    void CreateColumnIfNotExistsImpl( const SCCOL nScCol );
+    void CreateColumnIfNotExistsImpl( const SCCOL nScCol ) const;
     sal_uLong       GetCellCount() const;
     sal_uLong       GetWeightedCount() const;
     sal_uLong       GetWeightedCount(SCROW nStartRow, SCROW nEndRow) const;
@@ -443,9 +444,11 @@ public:
 
     CellType    GetCellType( const ScAddress& rPos ) const
                     {
-                        return ValidColRow(rPos.Col(),rPos.Row()) ?
-                            aCol[rPos.Col()].GetCellType( rPos.Row() ) :
-                            CELLTYPE_NONE;
+                        if (!ValidColRow(rPos.Col(),rPos.Row()))
+                            return CELLTYPE_NONE;
+                        if (rPos.Col() >= aCol.size())
+                            return CELLTYPE_NONE;
+                        return aCol[rPos.Col()].GetCellType( rPos.Row() );
                     }
     CellType    GetCellType( SCCOL nCol, SCROW nRow ) const;
     ScRefCellValue GetCellValue( SCCOL nCol, SCROW nRow ) const;
@@ -1072,6 +1075,8 @@ public:
     static void UpdateSearchItemAddressForReplace( const SvxSearchItem& rSearchItem, SCCOL& rCol, SCROW& rRow );
 
     ScColumnsRange GetColumnsRange(SCCOL begin, SCCOL end) const;
+    SCCOL ClampToAllocatedColumns(SCCOL nCol) const { return std::min(nCol, static_cast<SCCOL>(aCol.size() - 1)); }
+    SCCOL GetAllocatedColumnsCount() const { return aCol.size(); }
 
 private:
 

sc/source/core/data/columnspanset.cxx
@@ -131,6 +131,7 @@ void ColumnSpanSet::scan(
     if (!pTab)
         return;
 
+    nCol2 = pTab->ClampToAllocatedColumns(nCol2);
     for (SCCOL nCol = nCol1; nCol <= nCol2; ++nCol)
     {
         ColumnType& rCol = getColumn(nTab, nCol);
@@ -181,7 +182,7 @@ void ColumnSpanSet::executeColumnAction(ScDocument& rDoc, ColumnAction& ac) cons
             continue;
 
         const TableType& rTab = *maTables[nTab];
-        for (size_t nCol = 0; nCol < rTab.size(); ++nCol)
+        for (SCCOL nCol = 0; nCol < static_cast<SCCOL>(rTab.size()); ++nCol)
         {
             if (!rTab[nCol])
                 continue;
@@ -190,7 +191,7 @@ void ColumnSpanSet::executeColumnAction(ScDocument& rDoc, ColumnAction& ac) cons
             if (!pTab)
                 continue;
 
-            if (!ValidCol(nCol))
+            if (!ValidCol(nCol) || nCol >= pTab->GetAllocatedColumnsCount())
             {
                 // End the loop.
                 nCol = rTab.size();
@@ -357,12 +358,13 @@ void RangeColumnSpanSet::executeColumnAction(ScDocument& rDoc, sc::ColumnSpanSet
 {
     for (SCTAB nTab = range.aStart.Tab(); nTab <= range.aEnd.Tab(); ++nTab)
     {
-        for (SCCOL nCol = range.aStart.Col(); nCol <= range.aEnd.Col(); ++nCol)
-        {
-            ScTable* pTab = rDoc.FetchTable(nTab);
-            if (!pTab)
-                continue;
+        ScTable* pTab = rDoc.FetchTable(nTab);
+        if (!pTab)
+            continue;
 
+        SCCOL nEndCol = pTab->ClampToAllocatedColumns(range.aEnd.Col());
+        for (SCCOL nCol = range.aStart.Col(); nCol <= nEndCol; ++nCol)
+        {
             if (!ValidCol(nCol))
                 break;
 

commit 62086df3b084acef18326f279bd18019da654ba2

tdf#124997: only skip ScColumn*-related part when calculating widths
Otherwise, not-yet-allocated columns have STD_COL_WIDTH (1280), while
normal columns have widths like 82 (pixel), making the oversized grid
look as if the columns not having vertical lines at all.

Regression after commit 7282014e362a1529a36c88eb308df8ed359c2cfa

Change-Id: Ibb37c0c621d009bda2c5c0176cf5d3b238c45964

sc/source/core/data/fillinfo.cxx
@@ -433,7 +433,7 @@ void ScDocument::FillInfo(
     {
         SCCOL nX = (nArrCol>0) ? nArrCol-1 : MAXCOL+1;                    // negative -> invalid
 
-        if ( ValidCol(nX) && nX < maTabs[nTab]->GetAllocatedColumnsCount() )
+        if (ValidCol(nX))
         {
             // #i58049#, #i57939# Hidden columns must be skipped here, or their attributes
             // will disturb the output
@@ -447,184 +447,190 @@ void ScDocument::FillInfo(
 
                 pRowInfo[0].pCellInfo[nArrCol].nWidth = nThisWidth;           //TODO: this should be enough
 
-                ScColumn* pThisCol = &maTabs[nTab]->aCol[nX];                   // Column data
-
-                nArrRow = 1;
-                // Iterate between rows nY1 and nY2 and pick up non-empty
-                // cells that are not hidden.
-                RowInfoFiller aFunc(*this, nTab, pRowInfo, nArrCol, nArrRow);
-                sc::ParseAllNonEmpty(
-                    pThisCol->maCells.begin(), pThisCol->maCells, nRow1, nRow2, aFunc);
-
-                if (nX+1 >= nCol1)                                // Attribute/Blockmark from nX1-1
+                if (nX < maTabs[nTab]->GetAllocatedColumnsCount())
                 {
-                    ScAttrArray* pThisAttrArr = pThisCol->pAttrArray.get();       // Attribute
-                    nArrRow = 0;
+                    ScColumn* pThisCol = &maTabs[nTab]->aCol[nX]; // Column data
 
-                    SCROW nCurRow=nRow1;                  // single rows
-                    if (nCurRow>0)
-                        --nCurRow;                      // 1 more on top
-                    else
-                        nArrRow = 1;
+                    nArrRow = 1;
+                    // Iterate between rows nY1 and nY2 and pick up non-empty
+                    // cells that are not hidden.
+                    RowInfoFiller aFunc(*this, nTab, pRowInfo, nArrCol, nArrRow);
+                    sc::ParseAllNonEmpty(pThisCol->maCells.begin(), pThisCol->maCells, nRow1, nRow2,
+                                         aFunc);
 
-                    SCROW nThisRow;
-                    SCSIZE nIndex;
-                    if ( pThisAttrArr->Count() )
-                        (void) pThisAttrArr->Search( nCurRow, nIndex );
-                    else
-                        nIndex = 0;
-
-                    do
+                    if (nX + 1 >= nCol1) // Attribute/Blockmark from nX1-1
                     {
-                        const ScPatternAttr* pPattern = nullptr;
-                        if ( pThisAttrArr->Count() )
-                        {
-                            nThisRow = pThisAttrArr->mvData[nIndex].nEndRow;              // End of range
-                            pPattern = pThisAttrArr->mvData[nIndex].pPattern;
-                        }
+                        ScAttrArray* pThisAttrArr = pThisCol->pAttrArray.get(); // Attribute
+                        nArrRow = 0;
+
+                        SCROW nCurRow = nRow1; // single rows
+                        if (nCurRow > 0)
+                            --nCurRow; // 1 more on top
                         else
-                        {
-                            nThisRow = MAXROW;
-                            pPattern = GetDefPattern();
-                        }
+                            nArrRow = 1;
 
-                        const SvxBrushItem* pBackground = &pPattern->GetItem(ATTR_BACKGROUND);
-                        const SvxBoxItem* pLinesAttr = &pPattern->GetItem(ATTR_BORDER);
-
-                        const SvxLineItem* pTLBRLine = &pPattern->GetItem( ATTR_BORDER_TLBR );
-                        const SvxLineItem* pBLTRLine = &pPattern->GetItem( ATTR_BORDER_BLTR );
-
-                        const SvxShadowItem* pShadowAttr = &pPattern->GetItem(ATTR_SHADOW);
-                        if (pShadowAttr != pDefShadow)
-                            bAnyShadow = true;
-
-                        const ScMergeAttr* pMergeAttr = &pPattern->GetItem(ATTR_MERGE);
-                        bool bMerged = ( pMergeAttr != pDefMerge && *pMergeAttr != *pDefMerge );
-                        ScMF nOverlap = pPattern->GetItemSet().
-                                                        Get(ATTR_MERGE_FLAG).GetValue();
-                        bool bHOverlapped(nOverlap & ScMF::Hor);
-                        bool bVOverlapped(nOverlap & ScMF::Ver);
-                        bool bAutoFilter(nOverlap & ScMF::Auto);
-                        bool bPivotButton(nOverlap & ScMF::Button);
-                        bool bScenario(nOverlap & ScMF::Scenario);
-                        bool bPivotPopupButton(nOverlap & ScMF::ButtonPopup);
-                        bool bFilterActive(nOverlap & ScMF::HiddenMember);
-                        if (bMerged||bHOverlapped||bVOverlapped)
-                            bAnyMerged = true;                              // internal
-
-                        bool bHidden, bHideFormula;
-                        if (bTabProtect)
-                        {
-                            const ScProtectionAttr& rProtAttr = pPattern->GetItem(ATTR_PROTECTION);
-                            bHidden = rProtAttr.GetHideCell();
-                            bHideFormula = rProtAttr.GetHideFormula();
-                        }
+                        SCROW nThisRow;
+                        SCSIZE nIndex;
+                        if (pThisAttrArr->Count())
+                            (void)pThisAttrArr->Search(nCurRow, nIndex);
                         else
-                            bHidden = bHideFormula = false;
-
-                        const ScCondFormatIndexes& rCondFormats = pPattern->GetItem(ATTR_CONDITIONAL).GetCondFormatData();
-                        bool bContainsCondFormat = !rCondFormats.empty();
+                            nIndex = 0;
 
                         do
                         {
-                            SCROW nLastHiddenRow = -1;
-                            bool bRowHidden = RowHidden(nCurRow, nTab, nullptr, &nLastHiddenRow);
-                            if ( nArrRow==0 || !bRowHidden )
+                            const ScPatternAttr* pPattern = nullptr;
+                            if (pThisAttrArr->Count())
                             {
-                                if ( GetPreviewCellStyle( nX, nCurRow, nTab  ) != nullptr )
-                                    bAnyPreview = true;
-                                RowInfo* pThisRowInfo = &pRowInfo[nArrRow];
-                                if (pBackground != pDefBackground)          // Column background == Default ?
-                                    pThisRowInfo->bEmptyBack = false;
-                                if (bContainsCondFormat)
-                                    pThisRowInfo->bEmptyBack = false;
-                                if (bAutoFilter)
-                                    pThisRowInfo->bAutoFilter = true;
-                                if (bPivotButton || bPivotPopupButton)
-                                    pThisRowInfo->bPivotButton = true;
-
-                                CellInfo* pInfo = &pThisRowInfo->pCellInfo[nArrCol];
-                                pInfo->pBackground  = pBackground;
-                                pInfo->pPatternAttr = pPattern;
-                                pInfo->bMerged      = bMerged;
-                                pInfo->bHOverlapped = bHOverlapped;
-                                pInfo->bVOverlapped = bVOverlapped;
-                                pInfo->bAutoFilter  = bAutoFilter;
-                                pInfo->bPivotButton  = bPivotButton;
-                                pInfo->bPivotPopupButton = bPivotPopupButton;
-                                pInfo->bFilterActive = bFilterActive;
-                                pInfo->pLinesAttr   = pLinesAttr;
-                                pInfo->mpTLBRLine   = pTLBRLine;
-                                pInfo->mpBLTRLine   = pBLTRLine;
-                                pInfo->pShadowAttr  = pShadowAttr;
-                                // nWidth is no longer set individually
-
-                                if (bScenario)
-                                {
-                                    pInfo->pBackground = ScGlobal::GetButtonBrushItem();
-                                    pThisRowInfo->bEmptyBack = false;
-                                }
-
-                                if (bContainsCondFormat && pCondFormList)
-                                {
-                                    bAnyCondition |= handleConditionalFormat(*pCondFormList, rCondFormats, pInfo, pStlPool, ScAddress(nX, nCurRow, nTab),
-                                            bHidden, bHideFormula, bTabProtect);
-                                }
-
-                                if (bHidden || (bFormulaMode && bHideFormula && pInfo->maCell.meType == CELLTYPE_FORMULA))
-                                    pInfo->bEmptyCellText = true;
-
-                                ++nArrRow;
+                                nThisRow = pThisAttrArr->mvData[nIndex].nEndRow; // End of range
+                                pPattern = pThisAttrArr->mvData[nIndex].pPattern;
                             }
-                            else if (nLastHiddenRow >= 0)
+                            else
                             {
-                                nCurRow = nLastHiddenRow;
-                                if (nCurRow > nThisRow)
-                                    nCurRow = nThisRow;
+                                nThisRow = MAXROW;
+                                pPattern = GetDefPattern();
                             }
-                            ++nCurRow;
-                        }
-                        while (nCurRow <= nThisRow && nCurRow <= nYExtra);
-                        ++nIndex;
-                    }
-                    while ( nIndex < pThisAttrArr->Count() && nThisRow < nYExtra );
 
-                    if (pMarkData && pMarkData->IsMultiMarked())
-                    {
-                        //  Block marks
-                        ScMarkArray aThisMarkArr(pMarkData->GetMarkArray( nX ));
-                        nArrRow = 1;
-                        nCurRow = nRow1;                                      // single rows
+                            const SvxBrushItem* pBackground = &pPattern->GetItem(ATTR_BACKGROUND);
+                            const SvxBoxItem* pLinesAttr = &pPattern->GetItem(ATTR_BORDER);
 
-                        if ( aThisMarkArr.Search( nRow1, nIndex ) )
-                        {
+                            const SvxLineItem* pTLBRLine = &pPattern->GetItem(ATTR_BORDER_TLBR);
+                            const SvxLineItem* pBLTRLine = &pPattern->GetItem(ATTR_BORDER_BLTR);
+
+                            const SvxShadowItem* pShadowAttr = &pPattern->GetItem(ATTR_SHADOW);
+                            if (pShadowAttr != pDefShadow)
+                                bAnyShadow = true;
+
+                            const ScMergeAttr* pMergeAttr = &pPattern->GetItem(ATTR_MERGE);
+                            bool bMerged = (pMergeAttr != pDefMerge && *pMergeAttr != *pDefMerge);
+                            ScMF nOverlap = pPattern->GetItemSet().Get(ATTR_MERGE_FLAG).GetValue();
+                            bool bHOverlapped(nOverlap & ScMF::Hor);
+                            bool bVOverlapped(nOverlap & ScMF::Ver);
+                            bool bAutoFilter(nOverlap & ScMF::Auto);
+                            bool bPivotButton(nOverlap & ScMF::Button);
+                            bool bScenario(nOverlap & ScMF::Scenario);
+                            bool bPivotPopupButton(nOverlap & ScMF::ButtonPopup);
+                            bool bFilterActive(nOverlap & ScMF::HiddenMember);
+                            if (bMerged || bHOverlapped || bVOverlapped)
+                                bAnyMerged = true; // internal
+
+                            bool bHidden, bHideFormula;
+                            if (bTabProtect)
+                            {
+                                const ScProtectionAttr& rProtAttr
+                                    = pPattern->GetItem(ATTR_PROTECTION);
+                                bHidden = rProtAttr.GetHideCell();
+                                bHideFormula = rProtAttr.GetHideFormula();
+                            }
+                            else
+                                bHidden = bHideFormula = false;
+
+                            const ScCondFormatIndexes& rCondFormats
+                                = pPattern->GetItem(ATTR_CONDITIONAL).GetCondFormatData();
+                            bool bContainsCondFormat = !rCondFormats.empty();
+
                             do
                             {
-                                nThisRow=aThisMarkArr.pData[nIndex].nRow;      // End of range
+                                SCROW nLastHiddenRow = -1;
+                                bool bRowHidden
+                                    = RowHidden(nCurRow, nTab, nullptr, &nLastHiddenRow);
+                                if (nArrRow == 0 || !bRowHidden)
+                                {
+                                    if (GetPreviewCellStyle(nX, nCurRow, nTab) != nullptr)
+                                        bAnyPreview = true;
+                                    RowInfo* pThisRowInfo = &pRowInfo[nArrRow];
+                                    if (pBackground
+                                        != pDefBackground) // Column background == Default ?
+                                        pThisRowInfo->bEmptyBack = false;
+                                    if (bContainsCondFormat)
+                                        pThisRowInfo->bEmptyBack = false;
+                                    if (bAutoFilter)
+                                        pThisRowInfo->bAutoFilter = true;
+                                    if (bPivotButton || bPivotPopupButton)
+                                        pThisRowInfo->bPivotButton = true;
 
+                                    CellInfo* pInfo = &pThisRowInfo->pCellInfo[nArrCol];
+                                    pInfo->pBackground = pBackground;
+                                    pInfo->pPatternAttr = pPattern;
+                                    pInfo->bMerged = bMerged;
+                                    pInfo->bHOverlapped = bHOverlapped;
+                                    pInfo->bVOverlapped = bVOverlapped;
+                                    pInfo->bAutoFilter = bAutoFilter;
+                                    pInfo->bPivotButton = bPivotButton;
+                                    pInfo->bPivotPopupButton = bPivotPopupButton;
+                                    pInfo->bFilterActive = bFilterActive;
+                                    pInfo->pLinesAttr = pLinesAttr;
+                                    pInfo->mpTLBRLine = pTLBRLine;
+                                    pInfo->mpBLTRLine = pBLTRLine;
+                                    pInfo->pShadowAttr = pShadowAttr;
+                                    // nWidth is no longer set individually
+
+                                    if (bScenario)
+                                    {
+                                        pInfo->pBackground = ScGlobal::GetButtonBrushItem();
+                                        pThisRowInfo->bEmptyBack = false;
+                                    }
+
+                                    if (bContainsCondFormat && pCondFormList)
+                                    {
+                                        bAnyCondition |= handleConditionalFormat(
+                                            *pCondFormList, rCondFormats, pInfo, pStlPool,
+                                            ScAddress(nX, nCurRow, nTab), bHidden, bHideFormula,
+                                            bTabProtect);
+                                    }
+
+                                    if (bHidden
+                                        || (bFormulaMode && bHideFormula
+                                            && pInfo->maCell.meType == CELLTYPE_FORMULA))
+                                        pInfo->bEmptyCellText = true;
+
+                                    ++nArrRow;
+                                }
+                                else if (nLastHiddenRow >= 0)
+                                {
+                                    nCurRow = nLastHiddenRow;
+                                    if (nCurRow > nThisRow)
+                                        nCurRow = nThisRow;
+                                }
+                                ++nCurRow;
+                            } while (nCurRow <= nThisRow && nCurRow <= nYExtra);
+                            ++nIndex;
+                        } while (nIndex < pThisAttrArr->Count() && nThisRow < nYExtra);
+
+                        if (pMarkData && pMarkData->IsMultiMarked())
+                        {
+                            //  Block marks
+                            ScMarkArray aThisMarkArr(pMarkData->GetMarkArray(nX));
+                            nArrRow = 1;
+                            nCurRow = nRow1; // single rows
+
+                            if (aThisMarkArr.Search(nRow1, nIndex))
+                            {
                                 do
                                 {
-                                    if ( !RowHidden( nCurRow,nTab ) )
+                                    nThisRow = aThisMarkArr.pData[nIndex].nRow; // End of range
+
+                                    do
                                     {
-                                        ++nArrRow;
-                                    }
-                                    ++nCurRow;
-                                }
-                                while (nCurRow <= nThisRow && nCurRow <= nRow2);
-                                ++nIndex;
+                                        if (!RowHidden(nCurRow, nTab))
+                                        {
+                                            ++nArrRow;
+                                        }
+                                        ++nCurRow;
+                                    } while (nCurRow <= nThisRow && nCurRow <= nRow2);
+                                    ++nIndex;
+                                } while (nIndex < aThisMarkArr.nCount && nThisRow < nRow2);
                             }
-                            while ( nIndex < aThisMarkArr.nCount && nThisRow < nRow2 );
                         }
                     }
-                }
-                else                                    // columns in front
-                {
-                    for (nArrRow=1; nArrRow+1<nArrCount; nArrRow++)
+                    else // columns in front
                     {
-                        RowInfo* pThisRowInfo = &pRowInfo[nArrRow];
-                        CellInfo* pInfo = &pThisRowInfo->pCellInfo[nArrCol];
+                        for (nArrRow = 1; nArrRow + 1 < nArrCount; nArrRow++)
+                        {
+                            RowInfo* pThisRowInfo = &pRowInfo[nArrRow];
+                            CellInfo* pInfo = &pThisRowInfo->pCellInfo[nArrCol];
 
-                        pInfo->nWidth       = nThisWidth;           //TODO: or check only 0 ??
+                            pInfo->nWidth = nThisWidth; //TODO: or check only 0 ??
+                        }
                     }
                 }
             }