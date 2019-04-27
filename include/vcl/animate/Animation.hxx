9afa666120d8bb045f3569a77475a58c368c03a1
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2019-04-27 21:48:24 +0900

    move Animation, AnimationBitmap out from clang-format blacklist
    Change-Id: I95fd2d4e164bf04c4457b3e20401270227e21a8f
    Reviewed-on: https://gerrit.libreoffice.org/71424
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>
    Tested-by: Tomaž Vajngerl <quikee@gmail.com>

--- include/vcl/animate/Animation.hxx
+++ include/vcl/animate/Animation.hxx
@@ -31,102 +31,85 @@ class ImplAnimView;
 class VCL_DLLPUBLIC Animation
 {
 public:
-                    Animation();
-                    Animation( const Animation& rAnimation );
-                    ~Animation();
+    Animation();
+    Animation(const Animation& rAnimation);
+    ~Animation();
 
-    Animation&      operator=( const Animation& rAnimation );
-    bool            operator==( const Animation& rAnimation ) const;
-    bool            operator!=( const Animation& rAnimation ) const
-                        { return !(*this==rAnimation); }
+    Animation& operator=(const Animation& rAnimation);
+    bool operator==(const Animation& rAnimation) const;
+    bool operator!=(const Animation& rAnimation) const { return !(*this == rAnimation); }
 
-    void            Clear();
+    void Clear();
 
-    bool            Start(
-                        OutputDevice* pOutDev,
-                        const Point& rDestPt,
-                        const Size& rDestSz,
-                        long nExtraData,
-                        OutputDevice* pFirstFrameOutDev);
+    bool Start(OutputDevice* pOutDev, const Point& rDestPt, const Size& rDestSz, long nExtraData,
+               OutputDevice* pFirstFrameOutDev);
 
-    void            Stop( OutputDevice* pOutDev = nullptr, long nExtraData = 0 );
+    void Stop(OutputDevice* pOutDev = nullptr, long nExtraData = 0);
 
-    void            Draw( OutputDevice* pOutDev, const Point& rDestPt ) const;
-    void            Draw( OutputDevice* pOutDev, const Point& rDestPt, const Size& rDestSz ) const;
+    void Draw(OutputDevice* pOutDev, const Point& rDestPt) const;
+    void Draw(OutputDevice* pOutDev, const Point& rDestPt, const Size& rDestSz) const;
 
-    bool            IsInAnimation() const { return mbIsInAnimation; }
-    bool            IsTransparent() const;
+    bool IsInAnimation() const { return mbIsInAnimation; }
+    bool IsTransparent() const;
 
-    const Size&     GetDisplaySizePixel() const { return maGlobalSize; }
-    void            SetDisplaySizePixel( const Size& rSize ) { maGlobalSize = rSize; }
+    const Size& GetDisplaySizePixel() const { return maGlobalSize; }
+    void SetDisplaySizePixel(const Size& rSize) { maGlobalSize = rSize; }
 
     const BitmapEx& GetBitmapEx() const { return maBitmapEx; }
-    void            SetBitmapEx( const BitmapEx& rBmpEx ) { maBitmapEx = rBmpEx; }
+    void SetBitmapEx(const BitmapEx& rBmpEx) { maBitmapEx = rBmpEx; }
 
-    sal_uInt32      GetLoopCount() const { return mnLoopCount; }
-    void            SetLoopCount(const sal_uInt32 nLoopCount);
-    void            ResetLoopCount();
+    sal_uInt32 GetLoopCount() const { return mnLoopCount; }
+    void SetLoopCount(const sal_uInt32 nLoopCount);
+    void ResetLoopCount();
 
-    void            SetNotifyHdl( const Link<Animation*,void>& rLink ) { maNotifyLink = rLink; }
-    const Link<Animation*,void>& GetNotifyHdl() const { return maNotifyLink; }
+    void SetNotifyHdl(const Link<Animation*, void>& rLink) { maNotifyLink = rLink; }
+    const Link<Animation*, void>& GetNotifyHdl() const { return maNotifyLink; }
 
-    std::vector< std::unique_ptr<AnimationBitmap> >& GetAnimationFrames() { return maList; }
-    size_t          Count() const { return maList.size(); }
-    bool            Insert( const AnimationBitmap& rAnimationBitmap );
-    const AnimationBitmap&
-                    Get( sal_uInt16 nAnimation ) const;
-    void            Replace( const AnimationBitmap& rNewAnimationBmp, sal_uInt16 nAnimation );
+    std::vector<std::unique_ptr<AnimationBitmap>>& GetAnimationFrames() { return maList; }
+    size_t Count() const { return maList.size(); }
+    bool Insert(const AnimationBitmap& rAnimationBitmap);
+    const AnimationBitmap& Get(sal_uInt16 nAnimation) const;
+    void Replace(const AnimationBitmap& rNewAnimationBmp, sal_uInt16 nAnimation);
 
-    sal_uLong       GetSizeBytes() const;
-    BitmapChecksum  GetChecksum() const;
-
-public:
-
-    void            Convert( BmpConversion eConversion );
-    bool            ReduceColors( sal_uInt16 nNewColorCount );
-
-    bool            Invert();
-    void            Mirror( BmpMirrorFlags nMirrorFlags );
-    void            Adjust(
-                        short nLuminancePercent,
-                        short nContrastPercent,
-                        short nChannelRPercent,
-                        short nChannelGPercent,
-                        short nChannelBPercent,
-                        double fGamma = 1.0,
-                        bool bInvert = false );
-
-    friend VCL_DLLPUBLIC SvStream& ReadAnimation( SvStream& rIStream, Animation& rAnimation );
-    friend VCL_DLLPUBLIC SvStream& WriteAnimation( SvStream& rOStream, const Animation& rAnimation );
+    sal_uLong GetSizeBytes() const;
+    BitmapChecksum GetChecksum() const;
 
 public:
+    void Convert(BmpConversion eConversion);
+    bool ReduceColors(sal_uInt16 nNewColorCount);
 
-    SAL_DLLPRIVATE static void
-                    ImplIncAnimCount() { mnAnimCount++; }
-    SAL_DLLPRIVATE static void
-                    ImplDecAnimCount() { mnAnimCount--; }
-    SAL_DLLPRIVATE sal_uLong
-                    ImplGetCurPos() const { return mnPos; }
+    bool Invert();
+    void Mirror(BmpMirrorFlags nMirrorFlags);
+    void Adjust(short nLuminancePercent, short nContrastPercent, short nChannelRPercent,
+                short nChannelGPercent, short nChannelBPercent, double fGamma = 1.0,
+                bool bInvert = false);
+
+    friend VCL_DLLPUBLIC SvStream& ReadAnimation(SvStream& rIStream, Animation& rAnimation);
+    friend VCL_DLLPUBLIC SvStream& WriteAnimation(SvStream& rOStream, const Animation& rAnimation);
+
+public:
+    SAL_DLLPRIVATE static void ImplIncAnimCount() { mnAnimCount++; }
+    SAL_DLLPRIVATE static void ImplDecAnimCount() { mnAnimCount--; }
+    SAL_DLLPRIVATE sal_uLong ImplGetCurPos() const { return mnPos; }
 
 private:
     SAL_DLLPRIVATE static sal_uLong mnAnimCount;
 
-    std::vector< std::unique_ptr<AnimationBitmap> > maList;
-    std::vector< std::unique_ptr<ImplAnimView> >    maViewList;
+    std::vector<std::unique_ptr<AnimationBitmap>> maList;
+    std::vector<std::unique_ptr<ImplAnimView>> maViewList;
 
-    Link<Animation*,void> maNotifyLink;
-    BitmapEx        maBitmapEx;
-    Timer           maTimer;
-    Size            maGlobalSize;
-    sal_uInt32      mnLoopCount;
-    sal_uInt32      mnLoops;
-    size_t          mnPos;
-    bool            mbIsInAnimation;
-    bool            mbLoopTerminated;
+    Link<Animation*, void> maNotifyLink;
+    BitmapEx maBitmapEx;
+    Timer maTimer;
+    Size maGlobalSize;
+    sal_uInt32 mnLoopCount;
+    sal_uInt32 mnLoops;
+    size_t mnPos;
+    bool mbIsInAnimation;
+    bool mbLoopTerminated;
 
-    SAL_DLLPRIVATE void ImplRestartTimer( sal_uLong nTimeout );
-    DECL_DLLPRIVATE_LINK( ImplTimeoutHdl, Timer*, void );
-
+    SAL_DLLPRIVATE void ImplRestartTimer(sal_uLong nTimeout);
+    DECL_DLLPRIVATE_LINK(ImplTimeoutHdl, Timer*, void);
 };
 
 #endif // INCLUDED_VCL_ANIMATE_ANIMATION_HXX
