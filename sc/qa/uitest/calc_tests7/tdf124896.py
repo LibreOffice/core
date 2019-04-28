debb9eb01e2877e7863cb56f67261f8baff678af
Zdeněk Crhonek <zcrhonek@gmail.com>
2019-04-28 20:20:15 +0200

    uitest for bug tdf#124896
    Change-Id: Iacaf5e19bb20dee471ebdc53977170312cf0fc5f

---
+++ sc/qa/uitest/calc_tests7/tdf124896.py
@@ -0,0 +1,39 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+from uitest.framework import UITestCase
+from uitest.uihelper.common import get_state_as_dict
+from uitest.uihelper.common import select_pos
+from uitest.uihelper.calc import enter_text_to_cell
+from libreoffice.calc.document import get_cell_by_position
+from libreoffice.uno.propertyvalue import mkPropertyValues
+from uitest.uihelper.common import get_state_as_dict, type_text
+from uitest.debug import sleep
+import org.libreoffice.unotest
+import pathlib
+
+def get_url_for_data_file(file_name):
+    return pathlib.Path(org.libreoffice.unotest.makeCopyFromTDOC(file_name)).as_uri()
+
+# Bug 124896 - CRASH: deleting sheet
+
+class tdf124896(UITestCase):
+   def test_tdf124896_crash_delete_sheet(self):
+    calc_doc = self.ui_test.load_file(get_url_for_data_file("tdf124896.ods"))
+    xCalcDoc = self.xUITest.getTopFocusWindow()
+    gridwin = xCalcDoc.getChild("grid_window")
+    document = self.ui_test.get_component()
+
+    # Remove X Bar R Chart (2) sheet
+    self.ui_test.execute_dialog_through_command(".uno:Remove")  #delete sheet
+    xDialog = self.xUITest.getTopFocusWindow()
+    xOKButton = xDialog.getChild("yes")
+    xOKButton.executeAction("CLICK", tuple())
+    #verify; no crashes
+    self.assertEqual(document.Sheets.getCount(), 2)
+
+    self.ui_test.close_doc()
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
