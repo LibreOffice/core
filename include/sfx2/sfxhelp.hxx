c61e7b9940cb30800d6f1000727f9cfd5de9fa5e
Stephan Bergmann <sbergman@redhat.com>
2019-04-30 10:38:38 +0200

    Support "Preview in Web Browser" in Flatpak mode
    ...by storing the temporary HTML document in a location that can be accessed by
    the browser running outside the Flatpak sandbox.  This reuses and extends the
    mechanism already in place for the new HTML-based help in Flatpak mode (see
    72b936d70b7eaa6d9f5f911b27e3c955382de967 "Enable --help=html for flatpak").
    
    This fixes <https://github.com/flathub/org.libreoffice.LibreOffice/issues/85>
    "“Preview in Web Browser” does not work in Flatpak version".
    
    Change-Id: I5f73fd89139ffe6b8ab0dc501154b4f054a0ae5c
    Reviewed-on: https://gerrit.libreoffice.org/71570
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

--- include/sfx2/sfxhelp.hxx
+++ include/sfx2/sfxhelp.hxx
@@ -50,8 +50,6 @@ public:
     static OUString         GetCurrentModuleIdentifier();
     // Check for built-in help
     static bool             IsHelpInstalled();
-
-    static void removeFlatpakHelpTemporaryDirectory();
 };
 
 #endif // INCLUDED_SFX2_SFXHELP_HXX
