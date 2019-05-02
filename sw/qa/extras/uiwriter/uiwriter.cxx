LibreOffice/core

commit 214e6ed5286566e689a4ef13a9115e2ad38fff57

tdf#123259 sw_redlinehide: fix reanchoring of drawing objects
... in SplitNode.

The problem is that first the new anchor position is set in the
SwFrameFormat, then SwDrawContact::DisconnectFromLayout() is called
(implicitly from its SwClientNotify()).

This then cause the a11y wrapper to be disposed and an event to be sent,
which then ATKListener::notifyEvent() immediately processes by
retrieving all of the children of the anchor SwTextFrame.

At this point, we get an assert from SwTextFrame::MapModelToView,
because the layout anchor frame is still the old one, but the model
format already has the new position, so the frame can't map the
anchor position.

Avoid this by explicitly disconnecting from the layout before setting
the anchor on the SwFrameFormat.

Change-Id: Iba8960729dd041e13de4963d1b2ab6b223c8a427
Reviewed-on: https://gerrit.libreoffice.org/68880
Tested-by: Jenkins
Reviewed-by: Michael Stahl <Michael.Stahl@cib.de>
(cherry picked from commit 468ac1bdf1dcdc1f0d679ae2a7bf75102333c0af)
Reviewed-on: https://gerrit.libreoffice.org/68910
Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>

sw/source/core/txtnode/atrflyin.cxx
@@ -34,6 +34,7 @@
 #include <flyfrms.hxx>
 #include <objectformatter.hxx>
 #include <calbck.hxx>
+#include <dcontact.hxx>
 
 SwFormatFlyCnt::SwFormatFlyCnt( SwFrameFormat *pFrameFormat )
     : SfxPoolItem( RES_TXTATR_FLYCNT ),
@@ -193,6 +194,13 @@ void SwTextFlyCnt::SetAnchor( const SwTextNode *pNode )
     else
     {
         assert(!pFormat->IsModifyLocked()); // need to notify anchor node
+        if (RES_DRAWFRMFMT == pFormat->Which())
+        {
+            if (SdrObject const*const pObj = pFormat->FindSdrObject())
+            {   // tdf#123259 disconnect with *old* anchor position
+                static_cast<SwDrawContact*>(::GetUserCall(pObj))->DisconnectFromLayout();
+            }
+        }
         pFormat->SetFormatAttr( aAnchor );  // only set the anchor
     }
 

commit 536dad5cef5dae4c9f44416d320e43a39aaae189

tdf#125039 sw_redlinehide: keep shape selected when changing anchor type
Avoid the problem by not moving the drawing object to the invisible
layer, which isn't necessary because it will be inserted immediately
anyway.

(regression from 214e6ed5286566e689a4ef13a9115e2ad38fff57)

Change-Id: I9936de990d1db56fa4c00c37d05b4ed2923790ff

sw/source/core/txtnode/atrflyin.cxx
@@ -198,7 +198,7 @@ void SwTextFlyCnt::SetAnchor( const SwTextNode *pNode )
         {
             if (SdrObject const*const pObj = pFormat->FindSdrObject())
             {   // tdf#123259 disconnect with *old* anchor position
-                static_cast<SwDrawContact*>(::GetUserCall(pObj))->DisconnectFromLayout();
+                static_cast<SwDrawContact*>(::GetUserCall(pObj))->DisconnectFromLayout(false);
             }
         }
         pFormat->SetFormatAttr( aAnchor );  // only set the anchor

sw/qa/extras/uiwriter/uiwriter.cxx
@@ -179,6 +179,7 @@ public:
     void testTdf98512();
     void testShapeTextboxSelect();
     void testShapeTextboxDelete();
+    void testAnchorChangeSelection();
     void testCp1000071();
     void testShapeTextboxVertadjust();
     void testShapeTextboxAutosize();
@@ -384,6 +385,7 @@ public:
     CPPUNIT_TEST(testTdf98512);
     CPPUNIT_TEST(testShapeTextboxSelect);
     CPPUNIT_TEST(testShapeTextboxDelete);
+    CPPUNIT_TEST(testAnchorChangeSelection);
     CPPUNIT_TEST(testCp1000071);
     CPPUNIT_TEST(testShapeTextboxVertadjust);
     CPPUNIT_TEST(testShapeTextboxAutosize);
@@ -1277,6 +1279,25 @@ void SwUiWriterTest::testShapeTextboxDelete()
     nActual = pPage->GetObjCount();
     // Both (not only the shape) should be removed by now (the textbox wasn't removed, so this was 1).
     CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(0), nActual);
+}
+
+void SwUiWriterTest::testAnchorChangeSelection()
+{
+    SwDoc* pDoc = createDoc("test_anchor_as_character.odt");
+    SwWrtShell* pWrtShell = pDoc->GetDocShell()->GetWrtShell();
+    SdrPage* pPage = pDoc->getIDocumentDrawModelAccess().GetDrawModel()->GetPage(0);
+    SdrObject* pObject = pPage->GetObj(0);
+    CPPUNIT_ASSERT(pObject);
+
+    // Then select it.
+    pWrtShell->SelectObj(Point(), 0, pObject);
+    const SdrMarkList& rMarkList = pWrtShell->GetDrawView()->GetMarkedObjectList();
+    CPPUNIT_ASSERT_EQUAL(pObject, rMarkList.GetMark(0)->GetMarkedSdrObj());
+
+    pWrtShell->ChgAnchor(RndStdIds::FLY_AS_CHAR);
+
+    // tdf#125039 shape must still be selected, extensions depend on that
+    CPPUNIT_ASSERT_EQUAL(pObject, rMarkList.GetMark(0)->GetMarkedSdrObj());
 }
 
 void SwUiWriterTest::testCp1000071()