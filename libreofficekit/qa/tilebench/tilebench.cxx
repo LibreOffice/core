ee4f69f286446ec88c6682aaac96bdec793e760b
Jan Holesovsky <kendy@collabora.com>
2019-05-02 11:14:40 +0200

    lok: We changed the (unstable) ABI, but let's allow the older C++ API.
    Helps building the Online before the appropriate patch gets merged.
    
    Change-Id: I9983d546bb0cb7fb472806622a0a10e3a4d3ab87

--- libreofficekit/qa/tilebench/tilebench.cxx
+++ libreofficekit/qa/tilebench/tilebench.cxx
@@ -286,7 +286,7 @@ static void testDialog( Document *pDocument, const char *uno_cmd )
     }
 
     aTimes.emplace_back("post close dialog");
-    pDocument->postWindow(nDialogId, LOK_WINDOW_CLOSE, nullptr);
+    pDocument->postWindow(nDialogId, LOK_WINDOW_CLOSE);
     aTimes.emplace_back();
 
     pDocument->destroyView(view);
