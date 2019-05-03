9ce86af4417191c28a22b9264fe5cbe3bda901d4
Caolán McNamara <caolanm@redhat.com>
2019-05-03 11:57:09 +0100

    Related: tdf#124942 upgrade epoxy to 1.5.3
    https://github.com/anholt/libepoxy/issues/180 sounds very similar and
    1.5.3 apparently fixes that
    
    Change-Id: I009f5bc82f9e8326a7028ed29d86733cce649d15

--- external/epoxy/UnpackedTarball_epoxy.mk
+++ external/epoxy/UnpackedTarball_epoxy.mk
@@ -11,6 +11,18 @@ $(eval $(call gb_UnpackedTarball_UnpackedTarball,epoxy))
 
 $(eval $(call gb_UnpackedTarball_set_tarball,epoxy,$(EPOXY_TARBALL)))
 
+$(eval $(call gb_ExternalProject_use_external_project,lxml,python3))
+
+epoxy_PYTHON := $(call gb_ExternalExecutable_get_command,python)
+
+# previous versions of epoxy bundled the output, but now it has to be generated
+$(eval $(call gb_UnpackedTarball_set_pre_action,epoxy,\
+	$(epoxy_PYTHON) ./src/gen_dispatch.py --srcdir src --includedir include/epoxy registry/gl.xml && \
+	$(epoxy_PYTHON) ./src/gen_dispatch.py --srcdir src --includedir include/epoxy registry/glx.xml && \
+	$(epoxy_PYTHON) ./src/gen_dispatch.py --srcdir src --includedir include/epoxy registry/egl.xml && \
+	$(epoxy_PYTHON) ./src/gen_dispatch.py --srcdir src --includedir include/epoxy registry/wgl.xml \
+))
+
 $(eval $(call gb_UnpackedTarball_set_patchlevel,epoxy,0))
 
 $(eval $(call gb_UnpackedTarball_add_patches,epoxy, \
