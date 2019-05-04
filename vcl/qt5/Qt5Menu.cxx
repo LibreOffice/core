8a26857b9c7b5d2158239958f499edd010fa95c7
Jan-Marek Glogowski <glogow@fbihome.de>
2019-05-04 00:03:15 +0000

    tdf#123549 Qt5 implement Qt5Menu::ShowCloseButton
    This includes some not-so-nice lifetime handling of the button
    "clicked" connection handling. I decided to keep the code in
    one place, simply always forcing a disconnect on show, instead
    of a more "optimized" code version in SetFrame.
    
    First we try to get the icon from the system theme, but use LO's
    own icon theme as a fallback.
    
    Change-Id: Ic0459623ec907b9a54bef4670bf65cf587cd47ea

--- vcl/qt5/Qt5Menu.cxx
+++ vcl/qt5/Qt5Menu.cxx
@@ -21,6 +21,9 @@
 #include <vcl/pngwrite.hxx>
 #include <tools/stream.hxx>
 
+#include <strings.hrc>
+#include <bitmaps.hlst>
+
 Qt5Menu::Qt5Menu(bool bMenuBar)
     : mpVCLMenu(nullptr)
     , mpParentSalMenu(nullptr)
@@ -422,6 +425,7 @@ void Qt5Menu::DoFullMenuUpdate(Menu* pMenuBar)
 {
     // clear action groups since menu is rebuilt
     ResetAllActionGroups();
+    ShowCloseButton(false);
 
     for (sal_Int32 nItem = 0; nItem < static_cast<sal_Int32>(GetItemCount()); nItem++)
     {
@@ -508,21 +512,7 @@ void Qt5Menu::SetItemImage(unsigned, SalMenuItem* pItem, const Image& rImage)
     if (!pAction)
         return;
 
-    QImage aImage;
-
-    if (!!rImage)
-    {
-        SvMemoryStream aMemStm;
-        vcl::PNGWriter aWriter(rImage.GetBitmapEx());
-        aWriter.Write(aMemStm);
-
-        if (!aImage.loadFromData(static_cast<const uchar*>(aMemStm.GetData()), aMemStm.TellEnd()))
-        {
-            return;
-        }
-    }
-
-    pAction->setIcon(QPixmap::fromImage(aImage));
+    pAction->setIcon(QPixmap::fromImage(toQImage(rImage)));
 }
 
 void Qt5Menu::SetAccelerator(unsigned, SalMenuItem* pItem, const vcl::KeyCode&,
@@ -606,6 +596,49 @@ void Qt5Menu::NativeItemText(OUString& rItemText)
     rItemText = rItemText.replaceAll("&", "&&");
 
     rItemText = rItemText.replace('~', '&');
+}
+
+void Qt5Menu::slotCloseDocument()
+{
+    MenuBar* pVclMenuBar = static_cast<MenuBar*>(mpVCLMenu.get());
+    if (pVclMenuBar)
+        Application::PostUserEvent(pVclMenuBar->GetCloseButtonClickHdl());
+}
+
+void Qt5Menu::ShowCloseButton(bool bShow)
+{
+    if (!mpQMenuBar)
+        return;
+
+    QPushButton* pButton = static_cast<QPushButton*>(mpQMenuBar->cornerWidget(Qt::TopRightCorner));
+    if (!pButton)
+    {
+        QIcon aIcon;
+        if (QIcon::hasThemeIcon("window-close-symbolic"))
+            aIcon = QIcon::fromTheme("window-close-symbolic");
+        else
+            aIcon = QIcon(
+                QPixmap::fromImage((toQImage(Image(StockImage::Yes, SV_RESID_BITMAP_CLOSEDOC)))));
+        pButton = new QPushButton(mpQMenuBar);
+        pButton->setIcon(aIcon);
+        pButton->setFlat(true);
+        pButton->setToolTip(toQString(VclResId(SV_HELPTEXT_CLOSEDOCUMENT)));
+        mpQMenuBar->setCornerWidget(pButton, Qt::TopRightCorner);
+    }
+
+    if (bShow)
+    {
+        // The mpQMenuBar is used in multiple Qt5Menu. If one Qt5Menu is deleted, the clicked button
+        // connection is severed. The reconnect could be handled in SetFrame, but ShowCloseButton is
+        // called so seldomly, that I decided to keep the reconnect in this function in one place. As
+        // we don't know the connection state, we unconditionally remove it, so slotCloseDocument
+        // isn't called multiple times on click.
+        pButton->disconnect(SIGNAL(clicked(bool)));
+        connect(pButton, &QPushButton::clicked, this, &Qt5Menu::slotCloseDocument);
+        pButton->show();
+    }
+    else
+        pButton->hide();
 }
 
 Qt5MenuItem::Qt5MenuItem(const SalItemParams* pItemData)
