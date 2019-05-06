f7e1f0cec19d2770e398e064e5950379ae6d32ff
Adrien Ollier <adr.ollier@hotmail.fr>
2019-05-06 22:58:27 +0200

    inverts the tests for performance optimization
    Change-Id: Ib65ec7fd957ab19c703d31da9eb74390126d2d04
    Signed-off-by: Adrien Ollier <adr.ollier@hotmail.fr>

--- vcl/source/animate/Animation.cxx
+++ vcl/source/animate/Animation.cxx
@@ -115,13 +115,14 @@ bool Animation::IsTransparent() const
     // as the application (?) does not invalidate on non-transparent
     // graphics due to performance reasons.
 
-    return std::any_of(maList.begin(), maList.end(),
-                       [&aRect](const std::unique_ptr<AnimationBitmap>& pAnim) -> bool {
-                           return pAnim->meDisposal == Disposal::Back
-                                  && tools::Rectangle{ pAnim->maPositionPixel, pAnim->maSizePixel }
-                                         != aRect;
-                       })
-           || maBitmapEx.IsTransparent();
+    return maBitmapEx.IsTransparent()
+           || std::any_of(maList.begin(), maList.end(),
+                          [&aRect](const std::unique_ptr<AnimationBitmap>& pAnim) -> bool {
+                              return pAnim->meDisposal == Disposal::Back
+                                     && tools::Rectangle{ pAnim->maPositionPixel,
+                                                          pAnim->maSizePixel }
+                                            != aRect;
+                          });
 }
 
 sal_uLong Animation::GetSizeBytes() const
