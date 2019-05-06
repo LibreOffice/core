LibreOffice/core

commit 4d382636b0b1c555af2b98f2f41b4776fd7b5ffb

qt5: Add basic a11y support
Widget tree is displayed and basic role mapping is available.
Still needs quite some work (positions, actions, texts, etc.)

Change-Id: I9d26a762f1d9684f33bbb80a384cf2a0b8a905c0
Reviewed-on: https://gerrit.libreoffice.org/60853
Tested-by: Jenkins
Reviewed-by: Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>

vcl/inc/qt5/Qt5Widget.hxx
@@ -36,8 +36,6 @@ class Qt5Widget : public QWidget
 {
     Q_OBJECT
 
-    Qt5Frame* m_pFrame;
-
     bool handleKeyEvent(QKeyEvent*, bool);
     void handleMouseButtonEvent(QMouseEvent*, bool);
 
@@ -57,6 +55,7 @@ class Qt5Widget : public QWidget
 
 public:
     Qt5Widget(Qt5Frame& rFrame, Qt::WindowFlags f = Qt::WindowFlags());
+    Qt5Frame* m_pFrame;
 };
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

vcl/qt5/Qt5AccessibleWidget.cxx
@@ -1,0 +1,511 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#include <Qt5AccessibleWidget.hxx>
+#include <Qt5AccessibleWidget.moc>
+
+#include <QtGui/QAccessibleInterface>
+
+#include <Qt5Frame.hxx>
+#include <Qt5Tools.hxx>
+#include <Qt5VclWindow.hxx>
+#include <Qt5Widget.hxx>
+
+#include <com/sun/star/accessibility/AccessibleRole.hpp>
+
+#include <sal/log.hxx>
+#include <vcl/popupmenuwindow.hxx>
+
+using namespace css::accessibility;
+
+Qt5AccessibleWidget::Qt5AccessibleWidget(Qt5Widget* pFrame, vcl::Window* pWindow)
+    : m_pFrame(pFrame)
+    , m_pWindow(pWindow)
+{
+}
+
+Qt5AccessibleWidget::Qt5AccessibleWidget(vcl::Window* pWindow)
+    : m_pWindow(pWindow)
+{
+}
+
+QWindow* Qt5AccessibleWidget::window() const { return nullptr; }
+
+int Qt5AccessibleWidget::childCount() const
+{
+    if (!m_pWindow.get())
+        return 0;
+
+    return m_pWindow->GetAccessibleChildWindowCount();
+}
+
+int Qt5AccessibleWidget::indexOfChild(const QAccessibleInterface* /* child */) const { return 0; }
+QVector<QPair<QAccessibleInterface*, QAccessible::Relation>>
+    Qt5AccessibleWidget::relations(QAccessible::Relation /* match */) const
+{
+    return QVector<QPair<QAccessibleInterface*, QAccessible::Relation>>();
+}
+
+QAccessibleInterface* Qt5AccessibleWidget::focusChild() const
+{
+    if (m_pWindow->HasChildPathFocus())
+        return QAccessible::queryAccessibleInterface(
+            new Qt5VclWindow(Application::GetFocusWindow()));
+    return QAccessible::queryAccessibleInterface(object());
+}
+
+QRect Qt5AccessibleWidget::rect() const
+{
+    if (!m_pWindow.get())
+        return QRect();
+
+    SolarMutexGuard aSolarGuard;
+
+    // TODO: This seems to return a relative position (to the parent window).
+    // Needs to be absolute instead.
+    Point aPoint(m_pWindow->GetPosPixel());
+    Size aSize(m_pWindow->GetSizePixel());
+
+    return QRect(aPoint.X(), aPoint.Y(), aSize.Width(), aSize.Height());
+}
+
+QAccessibleInterface* Qt5AccessibleWidget::parent() const
+{
+    if (!m_pWindow)
+        return QAccessible::queryAccessibleInterface(nullptr);
+
+    return QAccessible::queryAccessibleInterface(
+        new Qt5VclWindow(m_pWindow->GetAccessibleParentWindow()));
+}
+QAccessibleInterface* Qt5AccessibleWidget::child(int index) const
+{
+    if (!m_pWindow)
+        return QAccessible::queryAccessibleInterface(nullptr);
+
+    return QAccessible::queryAccessibleInterface(
+        new Qt5VclWindow(m_pWindow->GetAccessibleChildWindow(index)));
+}
+
+QString Qt5AccessibleWidget::text(QAccessible::Text text) const
+{
+    if (!m_pWindow.get())
+        return QString();
+
+    SolarMutexGuard aSolarGuard;
+
+    switch (text)
+    {
+        case QAccessible::Name:
+            return toQString(m_pWindow->GetAccessibleName());
+        case QAccessible::Description:
+        case QAccessible::DebugDescription:
+            return toQString(m_pWindow->GetAccessibleDescription());
+        case QAccessible::Value:
+        case QAccessible::Help:
+        case QAccessible::Accelerator:
+        case QAccessible::UserText:
+        default:
+            return QString("Unknown");
+    }
+}
+QAccessible::Role Qt5AccessibleWidget::role() const
+{
+    if (!m_pWindow.get())
+        return QAccessible::NoRole;
+
+    switch (m_pWindow->GetAccessibleRole())
+    {
+        case AccessibleRole::UNKNOWN:
+            return QAccessible::NoRole;
+
+        case AccessibleRole::ALERT:
+            return QAccessible::AlertMessage;
+
+        case AccessibleRole::COLUMN_HEADER:
+            return QAccessible::ColumnHeader;
+
+        case AccessibleRole::CANVAS:
+            return QAccessible::Canvas;
+
+        case AccessibleRole::CHECK_BOX:
+            return QAccessible::CheckBox;
+
+        case AccessibleRole::CHECK_MENU_ITEM:
+            return QAccessible::MenuItem;
+
+        case AccessibleRole::COLOR_CHOOSER:
+            return QAccessible::ColorChooser;
+
+        case AccessibleRole::COMBO_BOX:
+            return QAccessible::ComboBox;
+
+        case AccessibleRole::DATE_EDITOR:
+            return QAccessible::EditableText;
+
+        case AccessibleRole::DESKTOP_ICON:
+            return QAccessible::Graphic;
+
+        case AccessibleRole::DESKTOP_PANE:
+        case AccessibleRole::DIRECTORY_PANE:
+            return QAccessible::Pane;
+
+        case AccessibleRole::DIALOG:
+            return QAccessible::Dialog;
+
+        case AccessibleRole::DOCUMENT:
+            return QAccessible::Document;
+
+        case AccessibleRole::EMBEDDED_OBJECT:
+            return QAccessible::UserRole;
+
+        case AccessibleRole::END_NOTE:
+            return QAccessible::Note;
+
+        case AccessibleRole::FILLER:
+            return QAccessible::Whitespace;
+
+        case AccessibleRole::FONT_CHOOSER:
+            return QAccessible::UserRole;
+
+        case AccessibleRole::FOOTER:
+            return QAccessible::Footer;
+
+        case AccessibleRole::FOOTNOTE:
+            return QAccessible::Note;
+
+        case AccessibleRole::FRAME: // top-level window with title bar
+            return QAccessible::Window;
+
+        case AccessibleRole::GLASS_PANE:
+            return QAccessible::UserRole;
+
+        case AccessibleRole::GRAPHIC:
+            return QAccessible::Graphic;
+
+        case AccessibleRole::GROUP_BOX:
+            return QAccessible::Grouping;
+
+        case AccessibleRole::HEADER:
+            return QAccessible::UserRole;
+
+        case AccessibleRole::HEADING:
+            return QAccessible::Heading;
+
+        case AccessibleRole::HYPER_LINK:
+            return QAccessible::Link;
+
+        case AccessibleRole::ICON:
+            return QAccessible::Graphic;
+
+        case AccessibleRole::INTERNAL_FRAME:
+            return QAccessible::UserRole;
+
+        case AccessibleRole::LABEL:
+            return QAccessible::StaticText;
+
+        case AccessibleRole::LAYERED_PANE:
+            return QAccessible::Pane;
+
+        case AccessibleRole::LIST:
+            return QAccessible::List;
+
+        case AccessibleRole::LIST_ITEM:
+            return QAccessible::ListItem;
+
+        case AccessibleRole::MENU:
+        case AccessibleRole::MENU_BAR:
+            return QAccessible::MenuBar;
+
+        case AccessibleRole::MENU_ITEM:
+            return QAccessible::MenuItem;
+
+        case AccessibleRole::OPTION_PANE:
+            return QAccessible::Pane;
+
+        case AccessibleRole::PAGE_TAB:
+            return QAccessible::PageTab;
+
+        case AccessibleRole::PAGE_TAB_LIST:
+            return QAccessible::PageTabList;
+
+        case AccessibleRole::PANEL:
+            return QAccessible::Pane;
+
+        case AccessibleRole::PARAGRAPH:
+            return QAccessible::Paragraph;
+
+        case AccessibleRole::PASSWORD_TEXT:
+            return QAccessible::EditableText;
+
+        case AccessibleRole::POPUP_MENU:
+            return QAccessible::PopupMenu;
+
+        case AccessibleRole::PUSH_BUTTON:
+            return QAccessible::Button;
+
+        case AccessibleRole::PROGRESS_BAR:
+            return QAccessible::ProgressBar;
+
+        case AccessibleRole::RADIO_BUTTON:
+            return QAccessible::RadioButton;
+
+        case AccessibleRole::RADIO_MENU_ITEM:
+            return QAccessible::MenuItem;
+
+        case AccessibleRole::ROW_HEADER:
+            return QAccessible::RowHeader;
+
+        case AccessibleRole::ROOT_PANE:
+            return QAccessible::Pane;
+
+        case AccessibleRole::SCROLL_BAR:
+            return QAccessible::ScrollBar;
+
+        case AccessibleRole::SCROLL_PANE:
+            return QAccessible::Pane;
+
+        case AccessibleRole::SHAPE:
+            return QAccessible::Graphic;
+
+        case AccessibleRole::SEPARATOR:
+            return QAccessible::Separator;
+
+        case AccessibleRole::SLIDER:
+            return QAccessible::Slider;
+
+        case AccessibleRole::SPIN_BOX:
+            return QAccessible::SpinBox;
+
+        case AccessibleRole::SPLIT_PANE:
+            return QAccessible::Pane;
+
+        case AccessibleRole::STATUS_BAR:
+            return QAccessible::StatusBar;
+
+        case AccessibleRole::TABLE:
+            return QAccessible::Table;
+
+        case AccessibleRole::TABLE_CELL:
+            return QAccessible::Cell;
+
+        case AccessibleRole::TEXT:
+            return QAccessible::EditableText;
+
+        case AccessibleRole::TEXT_FRAME:
+            return QAccessible::UserRole;
+
+        case AccessibleRole::TOGGLE_BUTTON:
+            return QAccessible::Button;
+
+        case AccessibleRole::TOOL_BAR:
+            return QAccessible::ToolBar;
+
+        case AccessibleRole::TOOL_TIP:
+            return QAccessible::ToolTip;
+
+        case AccessibleRole::TREE:
+            return QAccessible::Tree;
+
+        case AccessibleRole::VIEW_PORT:
+            return QAccessible::UserRole;
+
+        case AccessibleRole::BUTTON_DROPDOWN:
+            return QAccessible::Button;
+
+        case AccessibleRole::BUTTON_MENU:
+            return QAccessible::Button;
+
+        case AccessibleRole::CAPTION:
+            return QAccessible::StaticText;
+
+        case AccessibleRole::CHART:
+            return QAccessible::Chart;
+
+        case AccessibleRole::EDIT_BAR:
+            return QAccessible::Equation;
+
+        case AccessibleRole::FORM:
+            return QAccessible::Form;
+
+        case AccessibleRole::IMAGE_MAP:
+            return QAccessible::Graphic;
+
+        case AccessibleRole::NOTE:
+            return QAccessible::Note;
+
+        case AccessibleRole::RULER:
+            return QAccessible::UserRole;
+
+        case AccessibleRole::SECTION:
+            return QAccessible::Section;
+
+        case AccessibleRole::TREE_ITEM:
+            return QAccessible::TreeItem;
+
+        case AccessibleRole::TREE_TABLE:
+            return QAccessible::Tree;
+
+        case AccessibleRole::COMMENT:
+            return QAccessible::Note;
+
+        case AccessibleRole::COMMENT_END:
+            return QAccessible::UserRole;
+
+        case AccessibleRole::DOCUMENT_PRESENTATION:
+            return QAccessible::Document;
+
+        case AccessibleRole::DOCUMENT_SPREADSHEET:
+            return QAccessible::Document;
+
+        case AccessibleRole::DOCUMENT_TEXT:
+            return QAccessible::Document;
+
+        case AccessibleRole::STATIC:
+            return QAccessible::StaticText;
+
+        /* Ignore window objects for sub-menus, combo- and list boxes,
+         *  which are exposed as children of their parents.
+         */
+        case AccessibleRole::WINDOW: // top-level window without title bar
+        {
+            SolarMutexGuard aSolarGuard;
+            WindowType type = WindowType::WINDOW;
+            bool parentIsMenuFloatingWindow = false;
+
+            vcl::Window* pParent = m_pWindow->GetParent();
+            if (pParent)
+            {
+                type = pParent->GetType();
+                parentIsMenuFloatingWindow = pParent->IsMenuFloatingWindow();
+            }
+
+            if ((WindowType::LISTBOX != type) && (WindowType::COMBOBOX != type)
+                && (WindowType::MENUBARWINDOW != type) && !parentIsMenuFloatingWindow)
+            {
+                return QAccessible::Window;
+            }
+        }
+            SAL_FALLTHROUGH;
+
+        default:
+        {
+            SolarMutexGuard aSolarGuard;
+            vcl::Window* pChild = m_pWindow->GetWindow(GetWindowType::FirstChild);
+            if (pChild)
+            {
+                if (WindowType::HELPTEXTWINDOW == pChild->GetType())
+                {
+                    return QAccessible::HelpBalloon;
+                }
+                else if (m_pWindow->GetType() == WindowType::BORDERWINDOW
+                         && pChild->GetType() == WindowType::FLOATINGWINDOW)
+                {
+                    PopupMenuFloatingWindow* p = dynamic_cast<PopupMenuFloatingWindow*>(pChild);
+                    if (p && p->IsPopupMenu() && p->GetMenuStackLevel() == 0)
+                    {
+                        return QAccessible::PopupMenu;
+                    }
+                }
+            }
+            break;
+        }
+    }
+    return QAccessible::NoRole;
+}
+QAccessible::State Qt5AccessibleWidget::state() const
+{
+    QAccessible::State state;
+    return state;
+}
+
+QColor Qt5AccessibleWidget::foregroundColor() const { return QColor(); }
+QColor Qt5AccessibleWidget::backgroundColor() const { return QColor(); }
+
+void* Qt5AccessibleWidget::interface_cast(QAccessible::InterfaceType /* t */)
+{
+    /* if (t == QAccessible::ActionInterface)
+        return static_cast<QAccessibleActionInterface*>(this); */
+    return nullptr;
+}
+
+// QAccessibleActionInterface
+/* QStringList Qt5AccessibleWidget::actionNames() const
+{
+    qDebug("Qt5AccessibleWidget::actionNames");
+    QStringList actionNames;
+    return actionNames;
+}
+void Qt5AccessibleWidget::doAction(const QString& actionName)
+{
+    qDebug("Qt5AccessibleWidget::doAction");
+}
+QStringList Qt5AccessibleWidget::keyBindingsForAction(const QString& actionName) const
+{
+    qDebug("Qt5AccessibleWidget::keyBindingsForAction");
+    return QStringList();
+} */
+
+bool Qt5AccessibleWidget::isValid() const { return true; }
+
+QObject* Qt5AccessibleWidget::object() const { return nullptr; }
+
+void Qt5AccessibleWidget::setText(QAccessible::Text t, const QString& text)
+{
+    if (!m_pWindow)
+        return;
+
+    switch (t)
+    {
+        case QAccessible::Name:
+            m_pWindow->SetAccessibleName(toOUString(text));
+            break;
+        case QAccessible::Description:
+        case QAccessible::DebugDescription:
+            m_pWindow->SetAccessibleDescription(toOUString(text));
+            break;
+        case QAccessible::Value:
+        case QAccessible::Help:
+        case QAccessible::Accelerator:
+        case QAccessible::UserText:
+            break;
+    }
+}
+
+QAccessibleInterface* Qt5AccessibleWidget::childAt(int /* x */, int /* y */) const
+{
+    return nullptr;
+}
+
+QAccessibleInterface* Qt5AccessibleWidget::customFactory(const QString& classname, QObject* object)
+{
+    if (classname == QLatin1String("Qt5Widget") && object && object->isWidgetType())
+    {
+        return new Qt5AccessibleWidget(static_cast<Qt5Widget*>(object),
+                                       (static_cast<Qt5Widget*>(object))->m_pFrame->GetWindow());
+    }
+    if (classname == QLatin1String("Qt5VclWindow") && object)
+    {
+        if (dynamic_cast<Qt5VclWindow*>(object) != nullptr)
+            return new Qt5AccessibleWidget((static_cast<Qt5VclWindow*>(object))->m_pWindow);
+    }
+
+    return nullptr;
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

vcl/CustomTarget_qt5_moc.mk
@@ -10,12 +10,14 @@
 $(eval $(call gb_CustomTarget_CustomTarget,vcl/qt5))
 
 $(call gb_CustomTarget_get_target,vcl/qt5) : \
+	$(call gb_CustomTarget_get_workdir,vcl/qt5)/Qt5AccessibleWidget.moc \
 	$(call gb_CustomTarget_get_workdir,vcl/qt5)/Qt5FilePicker.moc \
 	$(call gb_CustomTarget_get_workdir,vcl/qt5)/Qt5Frame.moc \
 	$(call gb_CustomTarget_get_workdir,vcl/qt5)/Qt5Instance.moc \
 	$(call gb_CustomTarget_get_workdir,vcl/qt5)/Qt5MainWindow.moc \
 	$(call gb_CustomTarget_get_workdir,vcl/qt5)/Qt5Menu.moc \
 	$(call gb_CustomTarget_get_workdir,vcl/qt5)/Qt5Timer.moc \
+	$(call gb_CustomTarget_get_workdir,vcl/qt5)/Qt5VclWindow.moc \
 	$(call gb_CustomTarget_get_workdir,vcl/qt5)/Qt5Widget.moc \
 
 $(call gb_CustomTarget_get_workdir,vcl/qt5)/%.moc : \

vcl/Library_vclplug_qt5.mk
@@ -80,6 +80,7 @@ $(eval $(call gb_Library_add_libs,vclplug_qt5,\
 endif
 
 $(eval $(call gb_Library_add_exception_objects,vclplug_qt5,\
+    vcl/qt5/Qt5AccessibleWidget \
     vcl/qt5/Qt5Bitmap \
     vcl/qt5/Qt5Data \
     vcl/qt5/Qt5FilePicker \
@@ -101,6 +102,7 @@ $(eval $(call gb_Library_add_exception_objects,vclplug_qt5,\
     vcl/qt5/Qt5Timer \
     vcl/qt5/Qt5Tools \
     vcl/qt5/Qt5VirtualDevice \
+    vcl/qt5/Qt5VclWindow \
     vcl/qt5/Qt5Widget \
 ))
 

commit abd2e51e059b3c34897f1dceb859ed7dd7cd3c6d

Qt5 make Qt5Widdget's frame a private reference
Kind of a regression from commit 4d382636b0b1 ("qt5: Add basic
a11y support"), which made it public for a single call.

Change-Id: I631a861a98388223770cfca2704c3ddee6a0a8a0

vcl/inc/qt5/Qt5Widget.hxx
@@ -42,6 +42,8 @@ class Qt5Widget : public QWidget
 {
     Q_OBJECT
 
+    Qt5Frame& m_rFrame;
+
     bool handleKeyEvent(QKeyEvent*, bool);
     void handleMouseButtonEvent(QMouseEvent*, bool);
 
@@ -71,7 +73,8 @@ public slots:
 
 public:
     Qt5Widget(Qt5Frame& rFrame, Qt::WindowFlags f = Qt::WindowFlags());
-    Qt5Frame* m_pFrame;
+
+    Qt5Frame& getFrame() const { return m_rFrame; }
     void startDrag(sal_Int8 nSourceActions);
 };
 

vcl/qt5/Qt5AccessibleWidget.cxx
@@ -718,7 +718,7 @@ QAccessibleInterface* Qt5AccessibleWidget::customFactory(const QString& classnam
     if (classname == QLatin1String("Qt5Widget") && object && object->isWidgetType())
     {
         Qt5Widget* pWidget = static_cast<Qt5Widget*>(object);
-        vcl::Window* pWindow = pWidget->m_pFrame->GetWindow();
+        vcl::Window* pWindow = pWidget->getFrame().GetWindow();
 
         if (pWindow)
             return new Qt5AccessibleWidget(pWindow->GetAccessible());

vcl/qt5/Qt5Widget.cxx
@@ -49,12 +49,12 @@
 void Qt5Widget::paintEvent(QPaintEvent* pEvent)
 {
     QPainter p(this);
-    if (!m_pFrame->m_bNullRegion)
-        p.setClipRegion(m_pFrame->m_aRegion);
+    if (!m_rFrame.m_bNullRegion)
+        p.setClipRegion(m_rFrame.m_aRegion);
 
-    if (m_pFrame->m_bUseCairo)
+    if (m_rFrame.m_bUseCairo)
     {
-        cairo_surface_t* pSurface = m_pFrame->m_pSurface.get();
+        cairo_surface_t* pSurface = m_rFrame.m_pSurface.get();
         cairo_surface_flush(pSurface);
 
         QImage aImage(cairo_image_surface_get_data(pSurface), size().width(), size().height(),
@@ -62,55 +62,55 @@ void Qt5Widget::paintEvent(QPaintEvent* pEvent)
         p.drawImage(pEvent->rect().topLeft(), aImage, pEvent->rect());
     }
     else
-        p.drawImage(pEvent->rect().topLeft(), *m_pFrame->m_pQImage, pEvent->rect());
+        p.drawImage(pEvent->rect().topLeft(), *m_rFrame.m_pQImage, pEvent->rect());
 }
 
 void Qt5Widget::resizeEvent(QResizeEvent* pEvent)
 {
-    if (m_pFrame->m_bUseCairo)
+    if (m_rFrame.m_bUseCairo)
     {
         int width = size().width();
         int height = size().height();
 
-        if (m_pFrame->m_pSvpGraphics)
+        if (m_rFrame.m_pSvpGraphics)
         {
             cairo_surface_t* pSurface
                 = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, width, height);
             cairo_surface_set_user_data(pSurface, SvpSalGraphics::getDamageKey(),
-                                        &m_pFrame->m_aDamageHandler, nullptr);
-            m_pFrame->m_pSvpGraphics->setSurface(pSurface, basegfx::B2IVector(width, height));
-            UniqueCairoSurface old_surface(m_pFrame->m_pSurface.release());
-            m_pFrame->m_pSurface.reset(pSurface);
+                                        &m_rFrame.m_aDamageHandler, nullptr);
+            m_rFrame.m_pSvpGraphics->setSurface(pSurface, basegfx::B2IVector(width, height));
+            UniqueCairoSurface old_surface(m_rFrame.m_pSurface.release());
+            m_rFrame.m_pSurface.reset(pSurface);
 
             int min_width = qMin(pEvent->oldSize().width(), pEvent->size().width());
             int min_height = qMin(pEvent->oldSize().height(), pEvent->size().height());
 
             SalTwoRect rect(0, 0, min_width, min_height, 0, 0, min_width, min_height);
 
-            m_pFrame->m_pSvpGraphics->copySource(rect, old_surface.get());
+            m_rFrame.m_pSvpGraphics->copySource(rect, old_surface.get());
         }
     }
     else
     {
         QImage* pImage = nullptr;
 
-        if (m_pFrame->m_pQImage)
+        if (m_rFrame.m_pQImage)
             pImage = new QImage(
-                m_pFrame->m_pQImage->copy(0, 0, pEvent->size().width(), pEvent->size().height()));
+                m_rFrame.m_pQImage->copy(0, 0, pEvent->size().width(), pEvent->size().height()));
         else
         {
             pImage = new QImage(size(), Qt5_DefaultFormat32);
             pImage->fill(Qt::transparent);
         }
 
-        m_pFrame->m_pQt5Graphics->ChangeQImage(pImage);
-        m_pFrame->m_pQImage.reset(pImage);
+        m_rFrame.m_pQt5Graphics->ChangeQImage(pImage);
+        m_rFrame.m_pQImage.reset(pImage);
     }
 
-    m_pFrame->maGeometry.nWidth = size().width();
-    m_pFrame->maGeometry.nHeight = size().height();
+    m_rFrame.maGeometry.nWidth = size().width();
+    m_rFrame.maGeometry.nHeight = size().height();
 
-    m_pFrame->CallCallback(SalEvent::Resize, nullptr);
+    m_rFrame.CallCallback(SalEvent::Resize, nullptr);
 }
 
 void Qt5Widget::handleMouseButtonEvent(QMouseEvent* pEvent, bool bReleased)
@@ -141,7 +141,7 @@ void Qt5Widget::handleMouseButtonEvent(QMouseEvent* pEvent, bool bReleased)
         nEventType = SalEvent::MouseButtonUp;
     else
         nEventType = SalEvent::MouseButtonDown;
-    m_pFrame->CallCallback(nEventType, &aEvent);
+    m_rFrame.CallCallback(nEventType, &aEvent);
 }
 
 void Qt5Widget::mousePressEvent(QMouseEvent* pEvent) { handleMouseButtonEvent(pEvent, false); }
@@ -159,7 +159,7 @@ void Qt5Widget::mouseMoveEvent(QMouseEvent* pEvent)
     aEvent.mnCode = GetKeyModCode(pEvent->modifiers()) | GetMouseModCode(pEvent->buttons());
     aEvent.mnButton = 0;
 
-    m_pFrame->CallCallback(SalEvent::MouseMove, &aEvent);
+    m_rFrame.CallCallback(SalEvent::MouseMove, &aEvent);
     pEvent->accept();
 }
 
@@ -187,7 +187,7 @@ void Qt5Widget::wheelEvent(QWheelEvent* pEvent)
     aEvent.mnNotchDelta = nDelta > 0 ? 1 : -1;
     aEvent.mnScrollLines = 3;
 
-    m_pFrame->CallCallback(SalEvent::WheelMouse, &aEvent);
+    m_rFrame.CallCallback(SalEvent::WheelMouse, &aEvent);
     pEvent->accept();
 }
 
@@ -214,7 +214,7 @@ void Qt5Widget::dragMoveEvent(QDragMoveEvent* event)
 {
     QPoint point = event->pos();
 
-    m_pFrame->draggingStarted(point.x(), point.y(), event->possibleActions(), event->mimeData());
+    m_rFrame.draggingStarted(point.x(), point.y(), event->possibleActions(), event->mimeData());
     QWidget::dragMoveEvent(event);
 }
 
@@ -222,22 +222,22 @@ void Qt5Widget::dropEvent(QDropEvent* event)
 {
     QPoint point = event->pos();
 
-    m_pFrame->dropping(point.x(), point.y(), event->mimeData());
+    m_rFrame.dropping(point.x(), point.y(), event->mimeData());
     QWidget::dropEvent(event);
 }
 
-void Qt5Widget::moveEvent(QMoveEvent*) { m_pFrame->CallCallback(SalEvent::Move, nullptr); }
+void Qt5Widget::moveEvent(QMoveEvent*) { m_rFrame.CallCallback(SalEvent::Move, nullptr); }
 
 void Qt5Widget::showEvent(QShowEvent*)
 {
-    QSize aSize(m_pFrame->GetQWidget()->size());
+    QSize aSize(m_rFrame.GetQWidget()->size());
     SalPaintEvent aPaintEvt(0, 0, aSize.width(), aSize.height(), true);
-    m_pFrame->CallCallback(SalEvent::Paint, &aPaintEvt);
+    m_rFrame.CallCallback(SalEvent::Paint, &aPaintEvt);
 }
 
 void Qt5Widget::closeEvent(QCloseEvent* /*pEvent*/)
 {
-    m_pFrame->CallCallback(SalEvent::Close, nullptr);
+    m_rFrame.CallCallback(SalEvent::Close, nullptr);
 }
 
 static sal_uInt16 GetKeyCode(int keyval, Qt::KeyboardModifiers modifiers)
@@ -399,9 +399,9 @@ bool Qt5Widget::handleKeyEvent(QKeyEvent* pEvent, bool bDown)
 
     bool bStopProcessingKey;
     if (bDown)
-        bStopProcessingKey = m_pFrame->CallCallback(SalEvent::KeyInput, &aEvent);
+        bStopProcessingKey = m_rFrame.CallCallback(SalEvent::KeyInput, &aEvent);
     else
-        bStopProcessingKey = m_pFrame->CallCallback(SalEvent::KeyUp, &aEvent);
+        bStopProcessingKey = m_rFrame.CallCallback(SalEvent::KeyUp, &aEvent);
     return bStopProcessingKey;
 }
 
@@ -431,12 +431,9 @@ void Qt5Widget::keyReleaseEvent(QKeyEvent* pEvent)
         pEvent->accept();
 }
 
-void Qt5Widget::focusInEvent(QFocusEvent*) { m_pFrame->CallCallback(SalEvent::GetFocus, nullptr); }
+void Qt5Widget::focusInEvent(QFocusEvent*) { m_rFrame.CallCallback(SalEvent::GetFocus, nullptr); }
 
-void Qt5Widget::focusOutEvent(QFocusEvent*)
-{
-    m_pFrame->CallCallback(SalEvent::LoseFocus, nullptr);
-}
+void Qt5Widget::focusOutEvent(QFocusEvent*) { m_rFrame.CallCallback(SalEvent::LoseFocus, nullptr); }
 
 void Qt5Widget::showTooltip(const OUString& rTooltip)
 {
@@ -446,7 +443,7 @@ void Qt5Widget::showTooltip(const OUString& rTooltip)
 
 Qt5Widget::Qt5Widget(Qt5Frame& rFrame, Qt::WindowFlags f)
     : QWidget(Q_NULLPTR, f)
-    , m_pFrame(&rFrame)
+    , m_rFrame(rFrame)
 {
     create();
     setMouseTracking(true);
@@ -480,13 +477,13 @@ void Qt5Widget::inputMethodEvent(QInputMethodEvent* pEvent)
 
     if (!pEvent->commitString().isNull())
     {
-        vcl::DeletionListener aDel(m_pFrame);
+        vcl::DeletionListener aDel(&m_rFrame);
         aInputEvent.maText = toOUString(pEvent->commitString());
         aInputEvent.mnCursorPos = aInputEvent.maText.getLength();
-        m_pFrame->CallCallback(SalEvent::ExtTextInput, &aInputEvent);
+        m_rFrame.CallCallback(SalEvent::ExtTextInput, &aInputEvent);
         pEvent->accept();
         if (!aDel.isDeleted())
-            m_pFrame->CallCallback(SalEvent::EndExtTextInput, nullptr);
+            m_rFrame.CallCallback(SalEvent::EndExtTextInput, nullptr);
     }
     else
     {
@@ -534,7 +531,7 @@ void Qt5Widget::inputMethodEvent(QInputMethodEvent* pEvent)
             }
         }
 
-        m_pFrame->CallCallback(SalEvent::ExtTextInput, &aInputEvent);
+        m_rFrame.CallCallback(SalEvent::ExtTextInput, &aInputEvent);
         pEvent->accept();
     }
 }
@@ -546,7 +543,7 @@ QVariant Qt5Widget::inputMethodQuery(Qt::InputMethodQuery property) const
         case Qt::ImCursorRectangle:
         {
             SalExtTextInputPosEvent aPosEvent;
-            m_pFrame->CallCallback(SalEvent::ExtTextInputPos, &aPosEvent);
+            m_rFrame.CallCallback(SalEvent::ExtTextInputPos, &aPosEvent);
             return QVariant(
                 QRect(aPosEvent.mnX, aPosEvent.mnY, aPosEvent.mnWidth, aPosEvent.mnHeight));
         }