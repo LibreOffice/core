8c2304b1cbddb0603af3d8274109637d6422d4dc
Andrea Gelmini <andrea.gelmini@gelma.net>
2019-05-08 10:13:33 +0000

    Fix typo
    Change-Id: I3292e784ff82c5e71d9b3b931f21db963d681016

--- dbaccess/source/ui/querydesign/SelectionBrowseBox.cxx
+++ dbaccess/source/ui/querydesign/SelectionBrowseBox.cxx
@@ -786,7 +786,7 @@ bool OSelectionBrowseBox::saveField(OUString& _sFieldName ,OTableFieldDescRef co
                 // and the function is different to count
                 clearEntryFunctionField(_sFieldName,aSelEntry,_bListAction,nColumnId);
             }
-            // do we have a aggregate function and only a function?
+            // do we have an aggregate function and only a function?
             else if ( SQL_ISRULE(pColumnRef,general_set_fct) )
             {
                 OUString sLocalizedFunctionName;
@@ -1481,7 +1481,7 @@ void OSelectionBrowseBox::InsertColumn(const OTableFieldDescRef& pEntry, sal_uIn
 
     // remember the column id of the current position
     sal_uInt16 nColumnId = GetColumnId(_nColumnPosition);
-    // put at the end of the list if to small or to big,
+    // put at the end of the list if too small or too big,
     if ((_nColumnPosition == BROWSER_INVALIDID) || (_nColumnPosition >= getFields().size()))   // append the field
     {
         if (FindFirstFreeCol(_nColumnPosition) == nullptr)  // no more free columns
