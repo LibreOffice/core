51239fde4182aed2e4121b44731f0c1ce437c26a
Andrea Gelmini <andrea.gelmini@gelma.net>
2019-05-08 10:13:04 +0000

    Fix typo
    Change-Id: I305e152b6d6e71eba8d568b66c80b53e3bf27db0
    Reviewed-on: https://gerrit.libreoffice.org/72262
    Tested-by: Jenkins
    Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

--- desktop/source/offacc/acceptor.cxx
+++ desktop/source/offacc/acceptor.cxx
@@ -106,7 +106,7 @@ void Acceptor::run()
 
             // accept connection
             Reference< XConnection > rConnection = m_rAcceptor->accept( m_aConnectString );
-            // if we return without a valid connection we mus assume that the acceptor
+            // if we return without a valid connection we must assume that the acceptor
             // is destructed so we break out of the run method terminating the thread
             if (! rConnection.is()) break;
             OUString aDescription = rConnection->getDescription();
