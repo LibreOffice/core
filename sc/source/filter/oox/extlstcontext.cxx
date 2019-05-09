e40b44573f8dd727142978462cb87dab1fa29665
Dennis Francis <dennis.francis@collabora.com>
2019-05-09 18:50:22 +0530

    crashtesting: crash on importing tdf#123420-2.xlsx
    since
    
    commit c2f1c68ffb6dfa1ce7de09dcc428d6c53549e88d
    Date:   Fri Apr 19 23:15:53 2019 +0530
    
        tdf#122590: follow-up : import x14:cfRule priorities
    
    Fix is to import the priorities for iconSet x14:cfRule too.
    
    Thanks to Caolán McNamara for notifying me about the regression.
    
    Change-Id: Ib5b703a6911ab6480d42ac1e004a144043d3ad24
    Reviewed-on: https://gerrit.libreoffice.org/72035
    Tested-by: Jenkins
    Reviewed-by: Dennis Francis <dennis.francis@collabora.com>
    (cherry picked from commit ea5f1ec4eec4aa609000223aea1bc9ce202de2c5)
    Reviewed-on: https://gerrit.libreoffice.org/72120
    Reviewed-by: Andras Timar <andras.timar@collabora.com>
    Tested-by: Andras Timar <andras.timar@collabora.com>

--- sc/source/filter/oox/extlstcontext.cxx
+++ sc/source/filter/oox/extlstcontext.cxx
@@ -106,6 +106,7 @@ ContextHandlerRef ExtConditionalFormattingContext::onCreateContext(sal_Int32 nEl
         OUString aType = rAttribs.getString(XML_type, OUString());
         OUString aId = rAttribs.getString(XML_id, OUString());
         nPriority = rAttribs.getInteger( XML_priority, -1 );
+        maPriorities.push_back(nPriority);
 
         if (aType == "dataBar")
         {
@@ -181,7 +182,6 @@ void ExtConditionalFormattingContext::onEndElement()
         case XM_TOKEN(f):
         {
             rFormulas.push_back(aChars);
-            maPriorities.push_back(nPriority);
         }
         break;
         case XLS14_TOKEN( cfRule ):
@@ -226,9 +226,7 @@ void ExtConditionalFormattingContext::onEndElement()
             std::vector< std::unique_ptr<ExtCfCondFormat> >& rExtFormats =  getCondFormats().importExtCondFormat();
             rExtFormats.push_back(o3tl::make_unique<ExtCfCondFormat>(aRange, maEntries, &maPriorities));
 
-            if (isPreviousElementF)
-                maPriorities.clear();
-
+            maPriorities.clear();
             isPreviousElementF = false;
         }
         break;
