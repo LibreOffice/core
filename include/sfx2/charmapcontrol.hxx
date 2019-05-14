6c8519f14aa446820d735384d8ac190100f0ea60
Jim Raykowski <raykowj@gmail.com>
2019-05-13 18:14:04 -0800

    tdf#123793 Follow up fix for kb nav of special chars popup
    Static variable use to determine if initializing can focus properties of
    recent and favorite controls is needed only allows init to ever happen
    once. This patch changes use to a member variable. This corrects the
    problem of kb navigation only works the the first time the popup is shown
    and tab is used to navigate.
    
    Change-Id: I2bbb66071b582c9a4282c79d801b4a36c375ed2d
    Reviewed-on: https://gerrit.libreoffice.org/72264
    Tested-by: Jenkins
    Reviewed-by: Jim Raykowski <raykowj@gmail.com>
    (cherry picked from commit e8a1db012fbd185f52f38806bcac8bb9917c51c3)

--- include/sfx2/charmapcontrol.hxx
+++ include/sfx2/charmapcontrol.hxx
@@ -50,6 +50,7 @@ private:
     std::deque<OUString>   maFavCharList;
     std::deque<OUString>   maFavCharFontList;
     VclPtr<Button>         maDlgBtn;
+    bool                   mbNeedsInit = true;
 
     DECL_LINK(CharClickHdl, SvxCharViewControl*, void);
     DECL_STATIC_LINK(SfxCharmapCtrl, FocusHdl, Control&, void);
