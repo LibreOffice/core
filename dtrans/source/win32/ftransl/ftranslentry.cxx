fe6ceb37860b4a33d50f4dedc012b82b6ecd0013
Andrea Gelmini <andrea.gelmini@gelma.net>
2019-05-14 21:25:36 +0000

    Fix typo
    Change-Id: I282902c410ab93017b3aea46f88f60c246ca9894

--- dtrans/source/win32/ftransl/ftranslentry.cxx
+++ dtrans/source/win32/ftransl/ftranslentry.cxx
@@ -38,7 +38,7 @@ using namespace ::com::sun::star::datatransfer;
 namespace
 {
 
-    // functions to create a new Clipboad instance; is needed by factory helper implementation
+    // functions to create a new Clipboard instance; is needed by factory helper implementation
     // @param rServiceManager - service manager, useful if the component needs other uno services
     // so we should give it to every UNO-Implementation component
 
