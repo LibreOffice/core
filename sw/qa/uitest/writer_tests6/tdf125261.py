2cdbd04f2e574b4cafdeed316c8a43962ca8336d
Zdeněk Crhonek <zcrhonek@gmail.com>
2019-05-15 20:26:12 +0200

    uitest for bug tdf#125261
    Change-Id: I282f4f3a07fece65bd482c7664aedd44d5852a4e

---
+++ sw/qa/uitest/writer_tests6/tdf125261.py
@@ -0,0 +1,37 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+from uitest.framework import UITestCase
+from uitest.uihelper.common import get_state_as_dict
+from uitest.uihelper.common import select_pos
+from uitest.uihelper.calc import enter_text_to_cell
+from libreoffice.calc.document import get_cell_by_position
+from libreoffice.uno.propertyvalue import mkPropertyValues
+from uitest.uihelper.common import get_state_as_dict, type_text
+from uitest.debug import sleep
+# import org.libreoffice.unotest
+# import pathlib
+from uitest.path import get_srcdir_url
+def get_url_for_data_file(file_name):
+#    return pathlib.Path(org.libreoffice.unotest.makeCopyFromTDOC(file_name)).as_uri()
+    return get_srcdir_url() + "/sw/qa/uitest/writer_tests/data/" + file_name
+
+#Bug 125261 - Applying autocorrect to text with URLs causes Writer to hang
+
+class tdf125261(UITestCase):
+   def test_tdf125261_crash_autocorrect_text_with_URL(self):
+        writer_doc = self.ui_test.load_file(get_url_for_data_file("tdf125261.odt"))
+        document = self.ui_test.get_component()
+        xWriterDoc = self.xUITest.getTopFocusWindow()
+        xWriterEdit = xWriterDoc.getChild("writer_edit")
+        #apply autocorrect StartAutoCorrect
+        self.xUITest.executeCommand(".uno:AutoFormatApply")
+
+        self.xUITest.executeCommand(".uno:Undo")
+
+        self.assertEqual(document.Text.String[0:4], "This")
+        self.ui_test.close_doc()
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
