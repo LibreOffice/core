90ace7be71d41257e29a28fa2e7c0adf8f84050d
Todor Balabanov <todor.balabanov@gmail.com>
2019-05-15 19:54:21 +0300

    Range for DE scaling factor was implemented.
    Change-Id: I5b8d3cd69a6138d7eebf37c299626019b32d639a
    Reviewed-on: https://gerrit.libreoffice.org/72373
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>
    Tested-by: Tomaž Vajngerl <quikee@gmail.com>

--- nlpsolver/ThirdParty/EvolutionarySolver/src/net/adaptivebox/deps/behavior/DEGTBehavior.java
+++ nlpsolver/ThirdParty/EvolutionarySolver/src/net/adaptivebox/deps/behavior/DEGTBehavior.java
@@ -41,7 +41,10 @@ public class DEGTBehavior extends AbsGTBehavior implements ILibEngine {
   private static final int DVNum = 2;
 
   //scale constant: (0, 1.2], normally be 0.5
-  public double FACTOR = 0.5;
+  public double MIN_FACTOR = 0.5;
+
+  //scale constant: (0, 1.2], normally be 0.5
+  public double MAX_FACTOR = 0.5;
 
   //crossover constant: [0, 1], normally be 0.1 or 0.9
   public double CR = 0.9;
@@ -70,7 +73,7 @@ public class DEGTBehavior extends AbsGTBehavior implements ILibEngine {
       delta += (i % 2 == 0 ? +1D : -1D) * differenceVectors[i].getLocation()[index];
     }
 
-    trialVector[index] = globalVector[index] + FACTOR * delta;
+    trialVector[index] = globalVector[index] + RandomGenerator.doubleRangeRandom(MIN_FACTOR, MAX_FACTOR) * delta;
   }
 
   @Override
