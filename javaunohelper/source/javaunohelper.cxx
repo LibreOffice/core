LibreOffice/core

commit 2d22f8bb15c12752f6c763cde330d255d8be9545

tdf#42949 Fix IWYU warnings in javaunohelper/
Found with bin/find-unneeded-includes
Only removal proposals are dealt with here.

Change-Id: I16c46b6640a349b886fe934f5634f2ba79d64887
Reviewed-on: https://gerrit.libreoffice.org/71934
Tested-by: Jenkins
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

javaunohelper/source/javaunohelper.cxx
@@ -20,7 +20,6 @@
 #include <sal/config.h>
 
 #include <osl/module.hxx>
-#include <osl/thread.h>
 
 #include <uno/environment.hxx>
 #include <uno/lbnames.h>
@@ -28,13 +27,10 @@
 
 #include <cppuhelper/factory.hxx>
 
-#include <com/sun/star/beans/XPropertySet.hpp>
 #include <com/sun/star/lang/XMultiServiceFactory.hpp>
-#include <com/sun/star/lang/XServiceInfo.hpp>
 #include <com/sun/star/registry/XRegistryKey.hpp>
 
 #include <jni.h>
-#include <jvmaccess/virtualmachine.hxx>
 #include <jvmaccess/unovirtualmachine.hxx>
 
 #include "juhx-export-functions.hxx"

javaunohelper/IwyuFilter_javaunohelper.yaml
@@ -1,0 +1,16 @@
+---
+assumeFilename: javaunohelper/source/vm.cxx
+blacklist:
+    javaunohelper/source/bootstrap.cxx:
+    # Needed for direct member access
+    - com/sun/star/uno/XComponentContext.hpp
+    - jvmaccess/unovirtualmachine.hxx
+    # Needed to avoid loplugin:unreffun error
+    - juhx-export-functions.hxx
+    javaunohelper/source/javaunohelper.cxx:
+    # Needed for direct member access
+    - com/sun/star/lang/XMultiServiceFactory.hpp
+    - com/sun/star/registry/XRegistryKey.hpp
+    - jvmaccess/unovirtualmachine.hxx
+    # Needed to avoid loplugin:unreffun error
+    - juhx-export-functions.hxx

javaunohelper/source/bootstrap.cxx
@@ -22,7 +22,6 @@
 
 #include <osl/diagnose.h>
 
-#include <rtl/alloc.h>
 #include <rtl/bootstrap.hxx>
 #include <rtl/string.hxx>
 
@@ -33,10 +32,9 @@
 #include <cppuhelper/bootstrap.hxx>
 
 #include <com/sun/star/lang/XComponent.hpp>
-#include <com/sun/star/lang/XSingleComponentFactory.hpp>
+#include <com/sun/star/uno/XComponentContext.hpp>
 
 #include <jni.h>
-#include <jvmaccess/virtualmachine.hxx>
 #include <jvmaccess/unovirtualmachine.hxx>
 #include <tools/diagnose_ex.h>
 

commit f4780ebbc58000f4f4f52d23a9dd698c90d24bf1

javaunohelper: blind fix for Android build broken by 2d22f8bb15c1
Change-Id: Ie56197b3f4a90b8f9510a1c29d0388f5f70e1d16

javaunohelper/source/javaunohelper.cxx
@@ -40,6 +40,10 @@
 #include "juhx-export-functions.hxx"
 #include "vm.hxx"
 
+#ifdef DISABLE_DYNLOADING
+#include <osl/thread.h>
+#endif
+
 using namespace ::com::sun::star;
 using namespace ::com::sun::star::uno;
 