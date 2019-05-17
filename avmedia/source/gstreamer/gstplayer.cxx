LibreOffice/core

commit 18138417485aeba6c52d935c616dba829b24ffd8

avmedia: set gstreamer gst_video_overlay size and position.
Have video fit to the media object size on the slide,
allow more complicated interactions.

Change-Id: Ice1fa4b521176ad7ed7f7d1d2b13e617e8282390
Reviewed-on: https://gerrit.libreoffice.org/67978
Tested-by: Jenkins
Reviewed-by: Caolán McNamara <caolanm@redhat.com>
Tested-by: Caolán McNamara <caolanm@redhat.com>

avmedia/source/gstreamer/gstplayer.cxx
@@ -487,7 +487,13 @@ GstBusSyncReply Player::processSyncMessage( GstMessage *message )
             mpXOverlay = GST_VIDEO_OVERLAY( GST_MESSAGE_SRC( message ) );
             g_object_ref( G_OBJECT ( mpXOverlay ) );
             if ( mnWindowID != 0 )
+            {
                 gst_video_overlay_set_window_handle( mpXOverlay, mnWindowID );
+
+                if (maArea.Width > 0 && maArea.Height > 0)
+                    gst_video_overlay_set_render_rectangle(mpXOverlay, maArea.X, maArea.Y, maArea.Width, maArea.Height);
+            }
+
             return GST_BUS_DROP;
         }
     }
@@ -894,7 +900,12 @@ uno::Reference< ::media::XPlayerWindow > SAL_CALL Player::createPlayerWindow( co
     ::osl::MutexGuard aGuard(m_aMutex);
 
     uno::Reference< ::media::XPlayerWindow >    xRet;
-    awt::Size                                   aSize( getPreferredPlayerWindowSize() );
+    awt::Size                                   aSize;
+
+    if (rArguments.getLength() > 1 && (rArguments[1] >>= maArea))
+        aSize = awt::Size(maArea.Width, maArea.Height);
+    else
+        aSize = getPreferredPlayerWindowSize();
 
     if( mbFakeVideo )
         preparePlaybin( maURL, nullptr );
@@ -912,6 +923,14 @@ uno::Reference< ::media::XPlayerWindow > SAL_CALL Player::createPlayerWindow( co
             sal_IntPtr pIntPtr = 0;
             rArguments[ 2 ] >>= pIntPtr;
             SystemChildWindow *pParentWindow = reinterpret_cast< SystemChildWindow* >( pIntPtr );
+
+            if (pParentWindow)
+            {
+                Point aPoint = pParentWindow->GetPosPixel();
+                maArea.X = aPoint.getX();
+                maArea.Y = aPoint.getY();
+            }
+
             const SystemEnvData* pEnvData = pParentWindow ? pParentWindow->GetSystemData() : nullptr;
             OSL_ASSERT(pEnvData);
             if (pEnvData)

avmedia/source/gstreamer/gstplayer.hxx
@@ -101,6 +101,8 @@ private:
     int                     mnWidth;
     int                     mnHeight;
 
+    css::awt::Rectangle     maArea;     // Area of the player window.
+
     guint                   mnWatchID;
     bool                    mbWatchID;
 

commit 430acff785e22fd33eeb79183ce77cc59b036f28

tdf#124027: use ID of the embedded window and fix position of overlay
1) after many trials and errors what LibO apparently expects here is
the ID of QWindow parent of [embedded] system child window

2) the position of the video overlay has already been translated
relative to the top-left corner of the slide (see bugfix of tdf#42873
how) so let's not translate it 2nd time, now relative to the embedded
window (regression from 18138417485aeba6c52d935c616dba829b24ffd8)

Change-Id: Ie4c6d14a50959c0fdd04e745918d4889c9da45ec

avmedia/source/gstreamer/gstplayer.cxx
@@ -926,13 +926,6 @@ uno::Reference< ::media::XPlayerWindow > SAL_CALL Player::createPlayerWindow( co
             rArguments[ 2 ] >>= pIntPtr;
             SystemChildWindow *pParentWindow = reinterpret_cast< SystemChildWindow* >( pIntPtr );
 
-            if (pParentWindow)
-            {
-                Point aPoint = pParentWindow->GetPosPixel();
-                maArea.X = aPoint.getX();
-                maArea.Y = aPoint.getY();
-            }
-
             const SystemEnvData* pEnvData = pParentWindow ? pParentWindow->GetSystemData() : nullptr;
             OSL_ASSERT(pEnvData);
             if (pEnvData)

vcl/qt5/Qt5Object.cxx
@@ -40,7 +40,7 @@ Qt5Object::Qt5Object(Qt5Frame* pParent, bool bShow)
         m_pQWidget->show();
 
     m_aSystemData.nSize = sizeof(SystemEnvData);
-    m_aSystemData.aWindow = pParent->GetQWidget()->winId();
+    m_aSystemData.aWindow = m_pQWindow->winId();
     m_aSystemData.aShellWindow = reinterpret_cast<sal_IntPtr>(this);
     //m_aSystemData.pSalFrame = this;
     //m_aSystemData.pWidget = m_pQWidget;