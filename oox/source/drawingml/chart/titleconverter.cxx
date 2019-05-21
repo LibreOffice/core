7ea8112e33761f947635c0ce8b93f583c88c1e8e
Tamás Zolnai <tamas.zolnai@collabora.com>
2019-05-21 12:05:33 +0200

    tdf#122765: Legend interferes with pie chart after pptx import
    Revert the commit caused this regression:
    0fc41c53dfbd21e526fb0ad68a6651693c4a2ecd
    
    The original issue does not come back with
    reverting this commit.
    
    Reviewed-on: https://gerrit.libreoffice.org/72679
    Tested-by: Jenkins
    Reviewed-by: Tamás Zolnai <tamas.zolnai@collabora.com>
    (cherry picked from commit 10609749126ca76eaf12904d4cce9cc5a16d8405)
    
    Add unit test for tdf#118150.
    
    Reviewed-on: https://gerrit.libreoffice.org/72678
    Tested-by: Jenkins
    Reviewed-by: Tamás Zolnai <tamas.zolnai@collabora.com>
    (cherry picked from commit a703b4d8842261f55f489c28352df1f53a9b070a)
    
    Fix outdated comment.
    
    Reviewed-on: https://gerrit.libreoffice.org/72697
    Reviewed-by: Tamás Zolnai <tamas.zolnai@collabora.com>
    Tested-by: Tamás Zolnai <tamas.zolnai@collabora.com>
    (cherry picked from commit c43534d5e8d33f73ee4ba70867065be675302579)
    
    Change-Id: I666c4f92e3b70b416ec6da7a704298d207451649
    cea2c8aacb36e843dad67a056d07d6495fbbb17a
    1be6e4cf52ccd385d59f85d9d5fa5b8a47caf4f1
    Reviewed-on: https://gerrit.libreoffice.org/72761
    Reviewed-by: Xisco Faulí <xiscofauli@libreoffice.org>
    Tested-by: Xisco Faulí <xiscofauli@libreoffice.org>
    Tested-by: Jenkins
    Reviewed-by: Andras Timar <andras.timar@collabora.com>

--- oox/source/drawingml/chart/titleconverter.cxx
+++ oox/source/drawingml/chart/titleconverter.cxx
@@ -235,10 +235,9 @@ void LegendConverter::convertFromModel( const Reference< XDiagram >& rxDiagram )
         if( mrModel.mxLayout.get() )
         {
             LayoutConverter aLayoutConv( *this, *mrModel.mxLayout );
-            // manual size needs ChartLegendExpansion_CUSTOM and LegendPosition_CUSTOM (tdf#118150)
+            // manual size needs ChartLegendExpansion_CUSTOM
             if( aLayoutConv.convertFromModel( aPropSet ) )
             {
-                eLegendPos = cssc2::LegendPosition_CUSTOM;
                 eLegendExpand = cssc::ChartLegendExpansion_CUSTOM;
             }
             bManualLayout = !aLayoutConv.getAutoLayout();
