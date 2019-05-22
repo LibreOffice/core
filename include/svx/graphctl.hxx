ab02ad51c18638bab5db57af8164ca616f517e68
Caolán McNamara <caolanm@redhat.com>
2019-05-22 16:33:50 +0100

    remove now unused GraphCtrl
    Change-Id: I6d4aca59d5f9e24521876c36853b663b65a2d200

--- include/svx/graphctl.hxx
+++ include/svx/graphctl.hxx
@@ -27,21 +27,6 @@
 #include <svx/svdview.hxx>
 #include <svx/svdobj.hxx>
 
-class GraphCtrl;
-
-class GraphCtrlUserCall : public SdrObjUserCall
-{
-    GraphCtrl& rWin;
-
-public:
-
-    GraphCtrlUserCall(GraphCtrl& rGraphWin)
-        : rWin(rGraphWin)
-    {}
-
-    virtual void Changed(const SdrObject& rObj, SdrUserCallType eType, const tools::Rectangle& rOldBoundRect) override;
-};
-
 class SvxGraphCtrl;
 
 class SvxGraphCtrlUserCall : public SdrObjUserCall
@@ -58,84 +43,6 @@ public:
 };
 
 class SvxGraphCtrlAccessibleContext;
-
-class SVX_DLLPUBLIC GraphCtrl : public Control
-{
-    friend class GraphCtrlView;
-    friend class GraphCtrlUserCall;
-
-    Graphic             aGraphic;
-    Idle                aUpdateIdle;
-    Link<GraphCtrl*,void>  aMousePosLink;
-    Link<GraphCtrl*,void>  aGraphSizeLink;
-    Link<GraphCtrl*,void>  aUpdateLink;
-    MapMode const          aMap100;
-    Size                aGraphSize;
-    Point               aMousePos;
-    std::unique_ptr<GraphCtrlUserCall> pUserCall;
-    SdrObjKind          eObjKind;
-    sal_uInt16          nPolyEdit;
-    bool                bEditMode;
-    bool                mbSdrMode;
-    bool                mbInIdleUpdate;
-
-                        DECL_LINK( UpdateHdl, Timer*, void );
-
-    rtl::Reference<SvxGraphCtrlAccessibleContext> mpAccContext;
-
-protected:
-
-    std::unique_ptr<SdrModel>  pModel;
-    std::unique_ptr<SdrView>   pView;
-
-    virtual void        Paint( vcl::RenderContext& rRenderContext, const tools::Rectangle& rRect ) override;
-    virtual void        Resize() override;
-    virtual void        KeyInput(const KeyEvent& rKEvt) override;
-    virtual void        MouseButtonDown(const MouseEvent& rMEvt) override;
-    virtual void        MouseButtonUp(const MouseEvent& rMEvt) override;
-    virtual void        MouseMove(const MouseEvent& rMEvt) override;
-
-    virtual void        InitSdrModel();
-
-    virtual void        SdrObjCreated( const SdrObject& rObj );
-    virtual void        SdrObjChanged( const SdrObject& rObj );
-    virtual void        MarkListHasChanged();
-
-    SdrObjUserCall* GetSdrUserCall() { return pUserCall.get(); }
-
-public:
-
-    GraphCtrl( vcl::Window* pParent, WinBits nStyle );
-    virtual ~GraphCtrl() override;
-    virtual void dispose() override;
-
-    void                SetGraphic( const Graphic& rGraphic, bool bNewModel = true );
-    const Graphic&      GetGraphic() const { return aGraphic; }
-    const Size&         GetGraphicSize() const { return aGraphSize; }
-
-    const Point&        GetMousePos() const { return aMousePos; }
-
-    void                SetEditMode( const bool bEditMode );
-
-    void                SetPolyEditMode( const sal_uInt16 nPolyEdit );
-    sal_uInt16          GetPolyEditMode() const { return nPolyEdit; }
-
-    void                SetObjKind( const SdrObjKind eObjKind );
-
-    SdrModel*           GetSdrModel() const { return pModel.get(); }
-    SdrView*            GetSdrView() const { return pView.get(); }
-    SdrObject*          GetSelectedSdrObject() const;
-    bool                IsChanged() const { return mbSdrMode && pModel->IsChanged(); }
-
-    void                SetMousePosLink( const Link<GraphCtrl*,void>& rLink ) { aMousePosLink = rLink; }
-
-    void                SetGraphSizeLink( const Link<GraphCtrl*,void>& rLink ) { aGraphSizeLink = rLink; }
-
-    void                SetUpdateLink( const Link<GraphCtrl*,void>& rLink ) { aUpdateLink = rLink; }
-    void                QueueIdleUpdate();
-
-    void                SetSdrMode(bool b);
-};
 
 class SVX_DLLPUBLIC SvxGraphCtrl : public weld::CustomWidgetController
 {
@@ -220,26 +127,6 @@ public:
 
     Point               GetPositionInDialog() const;
     virtual css::uno::Reference< css::accessibility::XAccessible > CreateAccessible() override;
-};
-
-class GraphCtrlView : public SdrView
-{
-    GraphCtrl& rGraphCtrl;
-
-protected:
-
-    virtual void MarkListHasChanged() override
-    {
-        SdrView::MarkListHasChanged();
-        rGraphCtrl.MarkListHasChanged();
-    }
-
-public:
-    GraphCtrlView(SdrModel& rSdrModel, GraphCtrl* pWindow)
-        : SdrView(rSdrModel, pWindow)
-        , rGraphCtrl(*pWindow)
-    {
-    }
 };
 
 class SvxGraphCtrlView : public SdrView
