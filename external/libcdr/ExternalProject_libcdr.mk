c0927799b12350f87b2999df2c224816474519fb
Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>
2019-05-22 12:20:34 +0200

    libcdr & libqxp: follow main configure's --diable-dependency-tracking
    Change-Id: I363a02d7115ea54bb4aedb38071a249e145ee471
    Reviewed-on: https://gerrit.libreoffice.org/72742
    Tested-by: Jenkins
    Reviewed-by: Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>
    (cherry picked from commit ae03c889c900c8ae0d015e096d1e90b197bb1f5d)

--- external/libcdr/ExternalProject_libcdr.mk
+++ external/libcdr/ExternalProject_libcdr.mk
@@ -35,6 +35,7 @@ $(call gb_ExternalProject_get_state_target,libcdr,build) :
 			--disable-debug \
 			--disable-werror \
 			--disable-weffc \
+			$(if $(gb_FULLDEPS),,--disable-dependency-tracking) \
 			$(if $(verbose),--disable-silent-rules,--enable-silent-rules) \
 			CXXFLAGS="$(gb_CXXFLAGS) $(if $(ENABLE_OPTIMIZED),$(gb_COMPILEROPTFLAGS),$(gb_COMPILERNOOPTFLAGS))" \
 			CPPFLAGS="$(CPPFLAGS) $(BOOST_CPPFLAGS)" \
