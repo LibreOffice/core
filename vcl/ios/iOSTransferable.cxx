a0a4d142121c3aafdc97dcd3677a87f1439907b3
Tor Lillqvist <tml@collabora.com>
2019-05-24 01:51:00 +0300

    tdf#124752: Add system clipboard interface for iOS
    Based on the corresponding macOS code. Work in progress. The image
    support ifdeffed out still (because it uses some macOS specific APIs
    for which I couldn't right away find the equivalent iOS ones).
    
    I made it much simpler than the macOS code. I dropped the keeping of a
    local in-process clipboard completely. Firstly, as far as I see, the
    iOS clipboard API (UIPasteboard etc) does not even offer the
    possibility to separately offer some formats and actually provide the
    data on request. Secondly, we must be prepared anyway that the system
    can kill an iOS app at any stage while the user is using some other
    app, so we need to make sure everything that is copied goes onto the
    system clipboard right away anyway.
    
    I had to disable the copying of HTML to the clipboard as that lead to
    a mysterious assertion failure. See comment in
    DataFlavorMapper::openOfficeToSystemFlavor(). But RTF seems to work
    well, too. I assume RTF is what gets used for cross-application
    copy/paste (and cross-device, even, through Apple's Universal
    Clipboard thing, where you can copy/paste between your Macs and iOS
    devices on the same network).
    
    I am not sure how relevant the various application/x-openoffice-foo
    formats are.
    
    Change-Id: I174495e33d86fc3990996c229243c05d6cbfcda7

---
+++ vcl/ios/iOSTransferable.cxx
@@ -0,0 +1,174 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#include <sal/config.h>
+
+#include <com/sun/star/datatransfer/UnsupportedFlavorException.hpp>
+#include <com/sun/star/lang/IllegalArgumentException.hpp>
+#include <sal/types.h>
+#include <osl/diagnose.h>
+
+#include "iOSTransferable.hxx"
+
+#include "DataFlavorMapping.hxx"
+
+using namespace std;
+using namespace osl;
+using namespace cppu;
+using namespace com::sun::star::uno;
+using namespace com::sun::star::datatransfer;
+using namespace com::sun::star::lang;
+
+namespace
+{
+bool isValidFlavor(const DataFlavor& aFlavor)
+{
+    size_t len = aFlavor.MimeType.getLength();
+    Type dtype = aFlavor.DataType;
+    return ((len > 0)
+            && ((dtype == cppu::UnoType<Sequence<sal_Int8>>::get())
+                || (dtype == cppu::UnoType<OUString>::get())));
+}
+
+bool cmpAllContentTypeParameter(const Reference<XMimeContentType>& xLhs,
+                                const Reference<XMimeContentType>& xRhs)
+{
+    Sequence<OUString> xLhsFlavors = xLhs->getParameters();
+    Sequence<OUString> xRhsFlavors = xRhs->getParameters();
+
+    // Stop here if the number of parameters is different already
+    if (xLhsFlavors.getLength() != xRhsFlavors.getLength())
+        return false;
+
+    try
+    {
+        OUString pLhs;
+        OUString pRhs;
+
+        for (sal_Int32 i = 0; i < xLhsFlavors.getLength(); i++)
+        {
+            pLhs = xLhs->getParameterValue(xLhsFlavors[i]);
+            pRhs = xRhs->getParameterValue(xLhsFlavors[i]);
+
+            if (!pLhs.equalsIgnoreAsciiCase(pRhs))
+            {
+                return false;
+            }
+        }
+    }
+    catch (IllegalArgumentException&)
+    {
+        return false;
+    }
+
+    return true;
+}
+
+} // unnamed namespace
+
+iOSTransferable::iOSTransferable(const Reference<XMimeContentTypeFactory>& rXMimeCntFactory,
+                                 std::shared_ptr<DataFlavorMapper> pDataFlavorMapper,
+                                 UIPasteboard* pasteboard)
+    : mrXMimeCntFactory(rXMimeCntFactory)
+    , mDataFlavorMapper(pDataFlavorMapper)
+    , mPasteboard(pasteboard)
+{
+    [mPasteboard retain];
+
+    initClipboardItemList();
+}
+
+iOSTransferable::~iOSTransferable() { [mPasteboard release]; }
+
+Any SAL_CALL iOSTransferable::getTransferData(const DataFlavor& aFlavor)
+{
+    if (!isValidFlavor(aFlavor) || !isDataFlavorSupported(aFlavor))
+    {
+        throw UnsupportedFlavorException("Unsupported data flavor",
+                                         static_cast<XTransferable*>(this));
+    }
+
+    bool bInternal(false);
+    NSString* sysFormat = (aFlavor.MimeType.startsWith("image/png"))
+                              ? DataFlavorMapper::openOfficeImageToSystemFlavor(mPasteboard)
+                              : mDataFlavorMapper->openOfficeToSystemFlavor(aFlavor, bInternal);
+    DataProviderPtr_t dp;
+
+    NSData* sysData = [mPasteboard dataForPasteboardType:sysFormat];
+    dp = DataFlavorMapper::getDataProvider(sysFormat, sysData);
+
+    if (dp.get() == nullptr)
+    {
+        throw UnsupportedFlavorException("Unsupported data flavor",
+                                         static_cast<XTransferable*>(this));
+    }
+
+    return dp->getOOoData();
+}
+
+Sequence<DataFlavor> SAL_CALL iOSTransferable::getTransferDataFlavors() { return mFlavorList; }
+
+sal_Bool SAL_CALL iOSTransferable::isDataFlavorSupported(const DataFlavor& aFlavor)
+{
+    for (sal_Int32 i = 0; i < mFlavorList.getLength(); i++)
+        if (compareDataFlavors(aFlavor, mFlavorList[i]))
+            return true;
+
+    return false;
+}
+
+void iOSTransferable::initClipboardItemList()
+{
+    NSArray* pboardFormats = [mPasteboard pasteboardTypes];
+
+    if (pboardFormats == nullptr)
+    {
+        throw RuntimeException("Cannot get clipboard data", static_cast<XTransferable*>(this));
+    }
+
+    mFlavorList = mDataFlavorMapper->typesArrayToFlavorSequence(pboardFormats);
+}
+
+/* Compares two DataFlavors. Returns true if both DataFlavor have the same media type
+   and the number of parameter and all parameter values do match otherwise false
+   is returned.
+ */
+bool iOSTransferable::compareDataFlavors(const DataFlavor& lhs, const DataFlavor& rhs)
+{
+    try
+    {
+        Reference<XMimeContentType> xLhs(mrXMimeCntFactory->createMimeContentType(lhs.MimeType));
+        Reference<XMimeContentType> xRhs(mrXMimeCntFactory->createMimeContentType(rhs.MimeType));
+
+        if (!xLhs->getFullMediaType().equalsIgnoreAsciiCase(xRhs->getFullMediaType())
+            || !cmpAllContentTypeParameter(xLhs, xRhs))
+        {
+            return false;
+        }
+    }
+    catch (IllegalArgumentException&)
+    {
+        OSL_FAIL("Invalid content type detected");
+        return false;
+    }
+
+    return true;
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
