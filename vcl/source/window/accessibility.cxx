LibreOffice/core

commit cca4d9ab39499562614f0f778a2fffe5a40fde88

convert WB_PASSWORD to a bool field on Edit
Change-Id: Ie1e03bcdf57e6ba3e2a6ba8596d70a2d7a88eaba
Reviewed-on: https://gerrit.libreoffice.org/53520
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/source/window/accessibility.cxx
@@ -318,7 +318,7 @@ sal_uInt16 Window::getDefaultAccessibleRole() const
 
         case WindowType::PATTERNFIELD:
         case WindowType::CALCINPUTLINE:
-        case WindowType::EDIT: nRole = ( GetStyle() & WB_PASSWORD ) ? accessibility::AccessibleRole::PASSWORD_TEXT : accessibility::AccessibleRole::TEXT; break;
+        case WindowType::EDIT: nRole = static_cast<Edit const *>(this)->IsPassword() ? accessibility::AccessibleRole::PASSWORD_TEXT : accessibility::AccessibleRole::TEXT; break;
 
         case WindowType::PATTERNBOX:
         case WindowType::NUMERICBOX:

accessibility/source/standard/vclxaccessibleedit.cxx
@@ -214,7 +214,7 @@ sal_Int16 VCLXAccessibleEdit::implGetAccessibleRole(  )
 {
     sal_Int16 nRole;
     VclPtr< Edit > pEdit = GetAs< Edit >();
-    if ( pEdit && ( ( pEdit->GetStyle() & WB_PASSWORD ) || pEdit->GetEchoChar() ) )
+    if ( pEdit && ( pEdit->IsPassword() || pEdit->GetEchoChar() ) )
         nRole = AccessibleRole::PASSWORD_TEXT;
     else if ( pEdit && ( pEdit->GetStyle() & WB_READONLY ) )
         nRole = AccessibleRole::LABEL;

include/svtools/ivctrl.hxx
@@ -176,7 +176,7 @@ public:
 #define WB_NOVSCROLL            WB_DRAG
 #define WB_NOSELECTION          WB_REPEAT
 #define WB_NODRAGSELECTION      WB_PATHELLIPSIS
-#define WB_SMART_ARRANGE        WB_PASSWORD
+#define WB_SMART_ARRANGE        0x01000000 // used to be WB_PASSWORD
 #define WB_ALIGN_TOP            WB_TOP
 #define WB_ALIGN_LEFT           WB_LEFT
 #define WB_NOCOLUMNHEADER       WB_CENTER

include/tools/wintypes.hxx
@@ -195,7 +195,6 @@ WinBits const WB_NOMULTILINE =          0x10000000;
 WinBits const WB_EARLYTOGGLE =          SAL_CONST_INT64(0x4000000000);
 
 // Window-Bits for Edit
-WinBits const WB_PASSWORD =             0x01000000;
 WinBits const WB_READONLY =             0x02000000;
 WinBits const WB_NOHIDESELECTION =      SAL_CONST_INT64(0x1000000000);
 

include/vcl/edit.hxx
@@ -93,7 +93,8 @@ private:
                         mbClickedInSelection:1,
                         mbIsSubEdit:1,
                         mbActivePopup:1,
-                        mbForceControlBackground:1;
+                        mbForceControlBackground:1,
+                        mbPassword;
     Link<Edit&,void>    maModifyHdl;
     Link<Edit&,void>    maUpdateDataHdl;
     Link<Edit&,void>    maAutocompleteHdl;
@@ -275,6 +276,9 @@ public:
     static Size GetMinimumEditSize();
 
     void SetForceControlBackground(bool b) { mbForceControlBackground = b; }
+
+    void SetPassword(bool b)  { mbPassword = b; }
+    bool IsPassword() const { return mbPassword; }
 };
 
 #endif // INCLUDED_VCL_EDIT_HXX

vcl/source/control/edit.cxx
@@ -206,11 +206,9 @@ bool Edit::set_property(const OString &rKey, const OUString &rValue)
     }
     else if (rKey == "visibility")
     {
-        WinBits nBits = GetStyle();
-        nBits &= ~WB_PASSWORD;
+        mbPassword = false;
         if (!toBool(rValue))
-            nBits |= WB_PASSWORD;
-        SetStyle(nBits);
+            mbPassword = true;
     }
     else if (rKey == "placeholder-text")
         SetPlaceholderText(rValue);
@@ -281,6 +279,7 @@ void Edit::ImplInitEditData()
     mbActivePopup           = false;
     mbIsSubEdit             = false;
     mbForceControlBackground = false;
+    mbPassword              = false;
     mpDDInfo                = nullptr;
     mpIMEInfos              = nullptr;
     mcEchoChar              = 0;
@@ -431,7 +430,7 @@ long Edit::ImplGetExtraYOffset() const
 
 OUString Edit::ImplGetText() const
 {
-    if ( mcEchoChar || (GetStyle() & WB_PASSWORD) )
+    if ( mcEchoChar || mbPassword )
     {
         sal_Unicode cEchoChar;
         if ( mcEchoChar )
@@ -1406,7 +1405,7 @@ bool Edit::ImplHandleKeyEvent( const KeyEvent& rKEvt )
         {
             case KeyFuncType::CUT:
             {
-                if ( !mbReadOnly && maSelection.Len() && !(GetStyle() & WB_PASSWORD) )
+                if ( !mbReadOnly && maSelection.Len() && !mbPassword )
                 {
                     Cut();
                     ImplModified();
@@ -1417,7 +1416,7 @@ bool Edit::ImplHandleKeyEvent( const KeyEvent& rKEvt )
 
             case KeyFuncType::COPY:
             {
-                if ( !(GetStyle() & WB_PASSWORD) )
+                if ( !mbPassword )
                 {
                     Copy();
                     bDone = true;
@@ -2564,7 +2563,7 @@ OUString Edit::GetSelected() const
 
 void Edit::Cut()
 {
-    if ( !(GetStyle() & WB_PASSWORD ) )
+    if ( !mbPassword )
     {
         Copy();
         ReplaceSelected( OUString() );
@@ -2573,7 +2572,7 @@ void Edit::Cut()
 
 void Edit::Copy()
 {
-    if ( !(GetStyle() & WB_PASSWORD ) )
+    if ( !mbPassword )
     {
         css::uno::Reference<css::datatransfer::clipboard::XClipboard> aClipboard(GetClipboard());
         ImplCopy( aClipboard );
@@ -2811,7 +2810,7 @@ void Edit::dragGestureRecognized( const css::datatransfer::dnd::DragGestureEvent
     SolarMutexGuard aVclGuard;
 
     if ( !IsTracking() && maSelection.Len() &&
-         !(GetStyle() & WB_PASSWORD) && (!mpDDInfo || !mpDDInfo->bStarterOfDD) ) // no repeated D&D
+         !mbPassword && (!mpDDInfo || !mpDDInfo->bStarterOfDD) ) // no repeated D&D
     {
         Selection aSel( maSelection );
         aSel.Justify();

commit d1e8941682d20b7cfe0c95ebdd41da3db48788ff

A CALCINPUTLINE is not an Edit
Under ASan, clicking into the Calc "input line" control (in the toolbars) caused

> ==26138==ERROR: AddressSanitizer: heap-buffer-overflow on address 0x616000107a4c at pc 0x7fc890d792cb bp 0x7fff4cab6d90 sp 0x7fff4cab6d88
> READ of size 1 at 0x616000107a4c thread T0
>  #0 in Edit::IsPassword() const at include/vcl/edit.hxx:285:38
>  #1 in vcl::Window::getDefaultAccessibleRole() const at vcl/source/window/accessibility.cxx:327:73
[...]
> 0x616000107a4c is located 116 bytes to the right of 600-byte region [0x616000107780,0x6160001079d8)
> allocated by thread T0 here:
>  #0 in operator new(unsigned long) at /data/sbergman/github.com/llvm/llvm-project/compiler-rt/lib/asan/asan_new_delete.cc:105:3
>  #1 in VclPtr<ScInputBarGroup> VclPtr<ScInputBarGroup>::Create<vcl::Window*&, ScTabViewShell*&>(vcl::Window*&, ScTabViewShell*&) at include/vcl/vclptr.hxx:129:42
>  #2 in lcl_chooseRuntimeImpl(vcl::Window*, SfxBindings const*) at sc/source/ui/app/inputwin.cxx:157:12

This looks like it got broken with cca4d9ab39499562614f0f778a2fffe5a40fde88
"convert WB_PASSWORD to a bool field on Edit" assuming that a Window of type
CALCINPUTLINE derives from Edit, even though ScInputBarGroup does not.

Change-Id: I1457c6cd07477ee0fa868c3949105aa96116a366

vcl/source/window/accessibility.cxx
@@ -323,8 +323,9 @@ sal_uInt16 Window::getDefaultAccessibleRole() const
         case WindowType::MULTILINEEDIT: nRole = accessibility::AccessibleRole::SCROLL_PANE; break;
 
         case WindowType::PATTERNFIELD:
-        case WindowType::CALCINPUTLINE:
         case WindowType::EDIT: nRole = static_cast<Edit const *>(this)->IsPassword() ? accessibility::AccessibleRole::PASSWORD_TEXT : accessibility::AccessibleRole::TEXT; break;
+
+        case WindowType::CALCINPUTLINE: nRole = accessibility::AccessibleRole::TEXT; break;
 
         case WindowType::PATTERNBOX:
         case WindowType::NUMERICBOX: