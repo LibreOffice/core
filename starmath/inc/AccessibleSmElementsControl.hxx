334e9b8b79086d688c76d62b0dec7fb13ff59454
Jan-Marek Glogowski <glogow@fbihome.de>
2019-05-26 14:53:33 +0000

    tdf#65587 SM implement ElementControl accessibility
    This took me ages to implement. I'm still not 100% sure, it works
    correct. One main problem was the loop where the ATK listener will
    rebuild the list of children based on the current child count.
    
    Then there is the "broken" SPI bridge behaviour. I could actually
    test this with the gtk3 backend just fine, if I started LO *after*
    the accerciser. Otherwise the displayed tree will be really broken
    and the add and remove child events won't be correctly processed,
    because some Windows in the hierarchy will return a negative
    parent index. And generally the accerciser has various problems,
    with most result in Python backtraces an inconsistent app state,
    but even SIGSEGV happened a few time. Already have some patches
    for the easy reproducible ones.
    
    No idea what will happen on any other setup then Linux with ATK.
    
    Change-Id: I3280fd8622966be74e3833621952d95a2671d214

---
+++ starmath/inc/AccessibleSmElementsControl.hxx
@@ -0,0 +1,119 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+#ifndef INCLUDED_STARMATH_INC_ACCESSIBLESMELEMENTSCONTROL_HXX
+#define INCLUDED_STARMATH_INC_ACCESSIBLESMELEMENTSCONTROL_HXX
+
+#include <comphelper/accessiblecomponenthelper.hxx>
+#include <com/sun/star/accessibility/XAccessibleSelection.hpp>
+#include <com/sun/star/lang/XServiceInfo.hpp>
+#include <cppuhelper/implbase3.hxx>
+#include <vcl/vclptr.hxx>
+
+#include <vector>
+
+class AccessibleSmElement;
+class SmElementsControl;
+
+typedef ::cppu::ImplHelper3<css::lang::XServiceInfo, css::accessibility::XAccessible,
+                            css::accessibility::XAccessibleSelection>
+    AccessibleSmElementsControl_BASE;
+
+typedef std::vector<rtl::Reference<AccessibleSmElement>> SmElementList;
+
+class AccessibleSmElementsControl final : public comphelper::OAccessibleComponentHelper,
+                                          public AccessibleSmElementsControl_BASE
+{
+private:
+    SmElementList m_aAccessibleChildren;
+    VclPtr<SmElementsControl> m_pControl;
+
+    AccessibleSmElement* GetItem_Impl(sal_uInt16 _nPos);
+    void ReleaseFocus_Impl(sal_uInt16 _nPos);
+    sal_Int32 implGetAccessibleChildCount();
+    void ReleaseItems(bool bNotifyRemoval);
+    void UpdateFocus(sal_uInt16);
+    inline void TestControl();
+
+    ~AccessibleSmElementsControl() override;
+    void SAL_CALL disposing() override;
+    css::awt::Rectangle implGetBounds() override;
+
+public:
+    AccessibleSmElementsControl(SmElementsControl& rControl);
+
+    void ReleaseAllItems(bool bNotify);
+    void AddAllItems();
+    inline void AcquireFocus();
+    inline void ReleaseFocus(sal_uInt16);
+
+    DECLARE_XINTERFACE()
+    DECLARE_XTYPEPROVIDER()
+
+    // XAccessible
+    css::uno::Reference<css::accessibility::XAccessibleContext>
+        SAL_CALL getAccessibleContext() override;
+
+    // XServiceInfo
+    OUString SAL_CALL getImplementationName() override;
+    sal_Bool SAL_CALL supportsService(const OUString& ServiceName) override;
+    css::uno::Sequence<OUString> SAL_CALL getSupportedServiceNames() override;
+
+    // XAccessibleComponent
+    sal_Bool SAL_CALL containsPoint(const css::awt::Point& aPoint) override;
+    css::uno::Reference<css::accessibility::XAccessible>
+        SAL_CALL getAccessibleAtPoint(const css::awt::Point& aPoint) override;
+    css::awt::Rectangle SAL_CALL getBounds() override;
+    css::awt::Point SAL_CALL getLocation() override;
+    css::awt::Point SAL_CALL getLocationOnScreen() override;
+    css::awt::Size SAL_CALL getSize() override;
+    void SAL_CALL grabFocus() override;
+    sal_Int32 SAL_CALL getForeground() override;
+    sal_Int32 SAL_CALL getBackground() override;
+
+    // XAccessibleContext
+    sal_Int32 SAL_CALL getAccessibleChildCount() override;
+    css::uno::Reference<css::accessibility::XAccessible>
+        SAL_CALL getAccessibleChild(sal_Int32 i) override;
+    css::uno::Reference<css::accessibility::XAccessible> SAL_CALL getAccessibleParent() override;
+    sal_Int16 SAL_CALL getAccessibleRole() override;
+    OUString SAL_CALL getAccessibleDescription() override;
+    OUString SAL_CALL getAccessibleName() override;
+    css::uno::Reference<css::accessibility::XAccessibleRelationSet>
+        SAL_CALL getAccessibleRelationSet() override;
+    css::uno::Reference<css::accessibility::XAccessibleStateSet>
+        SAL_CALL getAccessibleStateSet() override;
+
+    // XAccessibleSelection
+    void SAL_CALL selectAccessibleChild(sal_Int32 nChildIndex) override;
+    sal_Bool SAL_CALL isAccessibleChildSelected(sal_Int32 nChildIndex) override;
+    void SAL_CALL clearAccessibleSelection() override;
+    void SAL_CALL selectAllAccessibleChildren() override;
+    sal_Int32 SAL_CALL getSelectedAccessibleChildCount() override;
+    css::uno::Reference<css::accessibility::XAccessible>
+        SAL_CALL getSelectedAccessibleChild(sal_Int32 nSelectedChildIndex) override;
+    void SAL_CALL deselectAccessibleChild(sal_Int32 nChildIndex) override;
+};
+
+void AccessibleSmElementsControl::AcquireFocus() { UpdateFocus(SAL_MAX_UINT16); }
+
+void AccessibleSmElementsControl::ReleaseFocus(sal_uInt16 nPos) { UpdateFocus(nPos); }
+
+#endif // INCLUDED_STARMATH_INC_ACCESSIBLESMELEMENTSCONTROL_HXX
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
---
+++ starmath/source/AccessibleSmElementsControl.cxx
@@ -0,0 +1,457 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#include <AccessibleSmElementsControl.hxx>
+#include <AccessibleSmElement.hxx>
+#include <ElementsDockingWindow.hxx>
+#include <smmod.hxx>
+
+#include <comphelper/accessiblewrapper.hxx>
+#include <comphelper/processfactory.hxx>
+#include <comphelper/types.hxx>
+#include <com/sun/star/accessibility/AccessibleEventId.hpp>
+#include <com/sun/star/accessibility/AccessibleRole.hpp>
+#include <com/sun/star/accessibility/AccessibleStateType.hpp>
+#include <com/sun/star/lang/IndexOutOfBoundsException.hpp>
+#include <cppuhelper/supportsservice.hxx>
+#include <cppuhelper/typeprovider.hxx>
+#include <toolkit/helper/convert.hxx>
+#include <unotools/accessiblerelationsethelper.hxx>
+#include <unotools/accessiblestatesethelper.hxx>
+#include <vcl/svapp.hxx>
+#include <vcl/vclevent.hxx>
+
+using namespace ::com::sun::star;
+using namespace ::com::sun::star::accessibility;
+using OContextEntryGuard = ::comphelper::OContextEntryGuard;
+using OExternalLockGuard = ::comphelper::OExternalLockGuard;
+
+// AccessibleSmElementsControl
+
+AccessibleSmElementsControl::AccessibleSmElementsControl(SmElementsControl& rControl)
+    : m_pControl(&rControl)
+{
+}
+
+AccessibleSmElementsControl::~AccessibleSmElementsControl() {}
+
+void AccessibleSmElementsControl::UpdateFocus(sal_uInt16 nPos)
+{
+    const bool bSetFocus = (nPos == SAL_MAX_UINT16);
+
+    // submit events only if the widget has the focus to avoid sending events due to mouse move
+    if (!m_pControl || (bSetFocus && !m_pControl->HasFocus()))
+        return;
+
+    if (bSetFocus)
+        nPos = m_pControl->itemHighlighted() - m_pControl->itemOffset();
+
+    if (nPos < m_aAccessibleChildren.size())
+    {
+        const auto& rxChild = m_aAccessibleChildren[nPos];
+        if (rxChild.is())
+            rxChild->SetFocus(bSetFocus);
+    }
+}
+
+void AccessibleSmElementsControl::ReleaseAllItems(bool bNotifyRemoval)
+{
+    if (m_aAccessibleChildren.empty())
+        return;
+
+    m_aAccessibleChildren.clear();
+    if (!bNotifyRemoval)
+        return;
+
+    // The original toolbox accessibility code uses individual NAME_CHANGED
+    // events in a loop. We can't do this, because on each remove event the
+    // list of known children is rebuild. But since we rely on the child
+    // count of the SmElementsControl, we'll always have no or all items.
+    // In the latter case this would automatically recreate all items!
+    assert(m_pControl && m_pControl->itemCount() == 0);
+    NotifyAccessibleEvent(AccessibleEventId::INVALIDATE_ALL_CHILDREN, uno::Any(), uno::Any());
+}
+
+void AccessibleSmElementsControl::AddAllItems()
+{
+    assert(m_pControl);
+    if (!m_pControl)
+        return;
+
+    uno::Any aNewName(getAccessibleName());
+    NotifyAccessibleEvent(AccessibleEventId::NAME_CHANGED, uno::Any(), aNewName);
+
+    // register the new items
+    sal_uInt16 nCount = getAccessibleChildCount();
+    for (sal_uInt16 i = 0; i < nCount; ++i)
+    {
+        uno::Any aNewValue;
+        aNewValue <<= getAccessibleChild(static_cast<sal_Int32>(i));
+        NotifyAccessibleEvent(AccessibleEventId::CHILD, uno::Any(), aNewValue);
+    }
+}
+
+IMPLEMENT_FORWARD_XINTERFACE2(AccessibleSmElementsControl, comphelper::OAccessibleComponentHelper,
+                              AccessibleSmElementsControl_BASE)
+
+IMPLEMENT_FORWARD_XTYPEPROVIDER2(AccessibleSmElementsControl,
+                                 comphelper::OAccessibleComponentHelper,
+                                 AccessibleSmElementsControl_BASE)
+
+// XAccessible
+uno::Reference<XAccessibleContext> AccessibleSmElementsControl::getAccessibleContext()
+{
+    return this;
+}
+
+// XComponent
+void AccessibleSmElementsControl::disposing()
+{
+    comphelper::OAccessibleComponentHelper::disposing();
+    m_aAccessibleChildren.clear();
+}
+
+void AccessibleSmElementsControl::grabFocus()
+{
+    SolarMutexGuard aGuard;
+    if (!m_pControl)
+        throw uno::RuntimeException();
+
+    m_pControl->GrabFocus();
+}
+
+sal_Int32 AccessibleSmElementsControl::getForeground()
+{
+    SolarMutexGuard aGuard;
+
+    if (!m_pControl)
+        throw uno::RuntimeException();
+    return static_cast<sal_Int32>(m_pControl->GetTextColor());
+}
+
+sal_Int32 AccessibleSmElementsControl::getBackground()
+{
+    SolarMutexGuard aGuard;
+
+    if (!m_pControl)
+        throw uno::RuntimeException();
+    Color nCol = m_pControl->GetControlBackground();
+    return static_cast<sal_Int32>(nCol);
+}
+
+// XServiceInfo
+OUString AccessibleSmElementsControl::getImplementationName()
+{
+    return OUString("com.sun.star.comp.toolkit.AccessibleSmElementsControl");
+}
+
+sal_Bool AccessibleSmElementsControl::supportsService(const OUString& rServiceName)
+{
+    return cppu::supportsService(this, rServiceName);
+}
+
+uno::Sequence<OUString> AccessibleSmElementsControl::getSupportedServiceNames()
+{
+    return { "com.sun.star.accessibility.AccessibleContext",
+             "com.sun.star.accessibility.AccessibleComponent",
+             "com.sun.star.accessibility.AccessibleSelection",
+             "com.sun.star.accessibility.AccessibleSmElementsControl" };
+}
+
+// XAccessibleContext
+sal_Int32 AccessibleSmElementsControl::getAccessibleChildCount()
+{
+    comphelper::OExternalLockGuard aGuard(this);
+    sal_Int32 nCount = 0;
+    if (m_pControl)
+    {
+        nCount = m_pControl->itemCount();
+        if (m_aAccessibleChildren.size() != sal_uInt16(nCount))
+            m_aAccessibleChildren.resize(nCount);
+        if (m_pControl->scrollbarAccessible().is())
+            nCount++;
+    }
+    return nCount;
+}
+
+uno::Reference<XAccessible> AccessibleSmElementsControl::getAccessibleChild(sal_Int32 i)
+{
+    comphelper::OExternalLockGuard aGuard(this);
+
+    if (i < 0 || i >= getAccessibleChildCount())
+        throw lang::IndexOutOfBoundsException();
+
+    // first child may be the scrollbar
+    sal_uInt16 c(i);
+    uno::Reference<XAccessible> xScrollbar = m_pControl->scrollbarAccessible();
+    if (xScrollbar.is())
+    {
+        if (c == 0)
+            return xScrollbar;
+        c--;
+    }
+
+    assert(c < m_aAccessibleChildren.size());
+    rtl::Reference<AccessibleSmElement> xChild = m_aAccessibleChildren[c];
+    const sal_uInt16 nItemId = m_pControl->itemOffset() + c;
+    if (xChild.is() && xChild->itemId() != nItemId)
+        xChild.clear();
+    if (!xChild.is())
+    {
+        sal_uInt16 nHighlightItemId = m_pControl->itemHighlighted();
+        AccessibleSmElement* pChild = new AccessibleSmElement(m_pControl, nItemId, i);
+        if (pChild->itemId() == nHighlightItemId)
+            pChild->SetFocus(true);
+        m_aAccessibleChildren[c] = pChild;
+        xChild = pChild;
+    }
+    return xChild.get();
+}
+
+uno::Reference<XAccessible> AccessibleSmElementsControl::getAccessibleParent()
+{
+    SolarMutexGuard aGuard;
+    if (!m_pControl)
+        throw uno::RuntimeException();
+
+    vcl::Window* pAccParent = m_pControl->GetAccessibleParentWindow();
+    assert(pAccParent);
+    return pAccParent ? pAccParent->GetAccessible() : uno::Reference<XAccessible>();
+}
+
+uno::Reference<XAccessible>
+AccessibleSmElementsControl::getAccessibleAtPoint(const awt::Point& rPoint)
+{
+    comphelper::OExternalLockGuard aGuard(this);
+
+    uno::Reference<XAccessible> xAccessible;
+    if (m_pControl)
+    {
+        sal_uInt16 nPos = m_pControl->itemAtPos(VCLPoint(rPoint));
+        nPos -= m_pControl->itemOffset();
+        if (nPos <= m_aAccessibleChildren.size())
+            xAccessible = getAccessibleChild(nPos);
+    }
+    return xAccessible;
+}
+
+sal_Int16 AccessibleSmElementsControl::getAccessibleRole() { return AccessibleRole::SCROLL_PANE; }
+
+OUString AccessibleSmElementsControl::getAccessibleDescription() { return OUString(); }
+
+OUString AccessibleSmElementsControl::getAccessibleName()
+{
+    SolarMutexGuard aGuard;
+    OUString aName;
+    if (m_pControl)
+        aName = SmResId(m_pControl->elementSetId().getStr());
+    return aName;
+}
+
+// XAccessibleSelection
+void AccessibleSmElementsControl::selectAccessibleChild(sal_Int32 nChildIndex)
+{
+    OExternalLockGuard aGuard(this);
+
+    if ((!m_pControl) || nChildIndex < 0
+        || static_cast<size_t>(nChildIndex) >= m_aAccessibleChildren.size())
+        throw lang::IndexOutOfBoundsException();
+
+    m_pControl->setItemHighlighted(nChildIndex);
+}
+
+sal_Bool AccessibleSmElementsControl::isAccessibleChildSelected(sal_Int32 nChildIndex)
+{
+    OExternalLockGuard aGuard(this);
+    if ((!m_pControl) || nChildIndex < 0
+        || static_cast<size_t>(nChildIndex) >= m_aAccessibleChildren.size())
+        throw lang::IndexOutOfBoundsException();
+
+    return (m_pControl->itemHighlighted() == nChildIndex);
+}
+
+void AccessibleSmElementsControl::clearAccessibleSelection()
+{
+    OExternalLockGuard aGuard(this);
+    if (m_pControl)
+        m_pControl->setItemHighlighted(SAL_MAX_UINT16);
+}
+
+void AccessibleSmElementsControl::selectAllAccessibleChildren()
+{
+    // intentionally empty
+}
+
+sal_Int32 AccessibleSmElementsControl::getSelectedAccessibleChildCount()
+{
+    OExternalLockGuard aGuard(this);
+
+    sal_Int32 nRet = 0;
+    if (m_pControl
+        && (m_pControl->itemHighlighted() - m_pControl->itemOffset()) < getAccessibleChildCount())
+        nRet = 1;
+    return nRet;
+}
+
+uno::Reference<XAccessible>
+AccessibleSmElementsControl::getSelectedAccessibleChild(sal_Int32 nSelectedChildIndex)
+{
+    OExternalLockGuard aGuard(this);
+    if (nSelectedChildIndex != 0 || !m_pControl)
+        throw lang::IndexOutOfBoundsException();
+    return getAccessibleChild(m_pControl->itemHighlighted() - m_pControl->itemOffset());
+}
+
+void AccessibleSmElementsControl::deselectAccessibleChild(sal_Int32 nChildIndex)
+{
+    OExternalLockGuard aGuard(this);
+    if (nChildIndex != 0 || nChildIndex >= getAccessibleChildCount())
+        throw lang::IndexOutOfBoundsException();
+    clearAccessibleSelection(); // there can be just one selected child
+}
+
+// XAccessibleComponent
+static awt::Point lcl_GetLocationOnScreen(vcl::Window const* m_pControl)
+{
+    awt::Point aPos;
+    if (m_pControl)
+    {
+        tools::Rectangle aRect = m_pControl->GetWindowExtentsRelative(nullptr);
+        aPos.X = aRect.Left();
+        aPos.Y = aRect.Top();
+    }
+    return aPos;
+}
+
+static awt::Rectangle lcl_GetBounds(vcl::Window const* m_pControl)
+{
+    // !! see VCLXAccessibleComponent::implGetBounds()
+
+    //! the coordinates returned are relative to the parent window !
+    //! Thus the top-left point may be different from (0, 0) !
+
+    awt::Rectangle aBounds;
+    if (m_pControl)
+    {
+        tools::Rectangle aRect = m_pControl->GetWindowExtentsRelative(nullptr);
+        aBounds.X = aRect.Left();
+        aBounds.Y = aRect.Top();
+        aBounds.Width = aRect.GetWidth();
+        aBounds.Height = aRect.GetHeight();
+
+        vcl::Window* pParent = m_pControl->GetAccessibleParentWindow();
+        if (pParent)
+        {
+            tools::Rectangle aParentRect = pParent->GetWindowExtentsRelative(nullptr);
+            awt::Point aParentScreenLoc(aParentRect.Left(), aParentRect.Top());
+            aBounds.X -= aParentScreenLoc.X;
+            aBounds.Y -= aParentScreenLoc.Y;
+        }
+    }
+    return aBounds;
+}
+
+void AccessibleSmElementsControl::TestControl()
+{
+    if (!m_pControl)
+        throw uno::RuntimeException();
+    assert(m_pControl->GetParent()->GetAccessible() == getAccessibleParent());
+}
+
+awt::Rectangle AccessibleSmElementsControl::implGetBounds()
+{
+    SolarMutexGuard aGuard;
+    TestControl();
+    return lcl_GetBounds(m_pControl);
+}
+
+awt::Rectangle AccessibleSmElementsControl::getBounds() { return implGetBounds(); }
+
+sal_Bool AccessibleSmElementsControl::containsPoint(const awt::Point& aPoint)
+{
+    SolarMutexGuard aGuard;
+    TestControl();
+    Size aSz(m_pControl->GetSizePixel());
+    return aPoint.X >= 0 && aPoint.Y >= 0 && aPoint.X < aSz.Width() && aPoint.Y < aSz.Height();
+}
+
+awt::Point AccessibleSmElementsControl::getLocation()
+{
+    SolarMutexGuard aGuard;
+    TestControl();
+    awt::Rectangle aRect(lcl_GetBounds(m_pControl));
+    return awt::Point(aRect.X, aRect.Y);
+}
+
+awt::Point AccessibleSmElementsControl::getLocationOnScreen()
+{
+    SolarMutexGuard aGuard;
+    TestControl();
+    return lcl_GetLocationOnScreen(m_pControl);
+}
+
+awt::Size AccessibleSmElementsControl::getSize()
+{
+    SolarMutexGuard aGuard;
+    TestControl();
+
+    Size aSz(m_pControl->GetSizePixel());
+#if OSL_DEBUG_LEVEL > 0 && !defined NDEBUG
+    awt::Rectangle aRect(lcl_GetBounds(m_pControl));
+    Size aSz2(aRect.Width, aRect.Height);
+    assert(aSz == aSz2 && "mismatch in width");
+#endif
+    return awt::Size(aSz.Width(), aSz.Height());
+}
+
+uno::Reference<XAccessibleRelationSet> AccessibleSmElementsControl::getAccessibleRelationSet()
+{
+    uno::Reference<XAccessibleRelationSet> xRelSet = new utl::AccessibleRelationSetHelper();
+    return xRelSet; // empty relation set
+}
+
+uno::Reference<XAccessibleStateSet> AccessibleSmElementsControl::getAccessibleStateSet()
+{
+    SolarMutexGuard aGuard;
+    ::utl::AccessibleStateSetHelper* pStateSet = new ::utl::AccessibleStateSetHelper;
+
+    uno::Reference<XAccessibleStateSet> xStateSet(pStateSet);
+
+    if (!m_pControl)
+        pStateSet->AddState(AccessibleStateType::DEFUNC);
+    else
+    {
+        pStateSet->AddState(AccessibleStateType::ENABLED);
+        pStateSet->AddState(AccessibleStateType::FOCUSABLE);
+        if (m_pControl->HasFocus())
+            pStateSet->AddState(AccessibleStateType::FOCUSED);
+        if (m_pControl->IsActive())
+            pStateSet->AddState(AccessibleStateType::ACTIVE);
+        if (m_pControl->IsVisible())
+            pStateSet->AddState(AccessibleStateType::SHOWING);
+        if (m_pControl->IsReallyVisible())
+            pStateSet->AddState(AccessibleStateType::VISIBLE);
+        if (COL_TRANSPARENT != m_pControl->GetBackground().GetColor())
+            pStateSet->AddState(AccessibleStateType::OPAQUE);
+    }
+
+    return xStateSet;
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
