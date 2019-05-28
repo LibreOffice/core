6c2e4b311648c0ec0981a88bc1e550b4f053f5f2
Shubham Goyal <22shubh22@gmail.com>
2019-05-28 12:49:07 +0530

    Qr code Integerate
    This is similar to file in hunspell external lib.
    
    Change-Id: Iaa9225a72d15806c929d30951cefd3f3fee8960e

---
+++ external/qrcodegen/ExternalProject_qrcodegen.mk
@@ -0,0 +1,34 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_ExternalProject_ExternalProject,qrcodegen))
+
+$(eval $(call gb_ExternalProject_register_targets,qrcodegen,\
+	build \
+))
+
+qrcodegen_CPPCLAGS=$(CPPFLAGS)
+
+ifneq (,$(filter ANDROID DRAGONFLY FREEBSD iOS LINUX NETBSD OPENBSD,$(OS)))
+ifneq (,$(gb_ENABLE_DBGUTIL))
+qrcodegen_CPPFLAGS+=-D_GLIBCXX_DEBUG
+endif
+endif
+
+$(call gb_ExternalProject_get_state_target,qrcodegen,build):
+	$(call gb_ExternalProject_run,build,\
+		./configure --disable-shared --disable-nls --with-pic \
+			$(if $(CROSS_COMPILING),--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM))\
+			$(if $(filter AIX,$(OS)),CFLAGS="-D_LINUX_SOURCE_COMPAT") \
+			$(if $(qrcodegen_CPPFLAGS),CPPFLAGS='$(qrcodegen_CPPFLAGS)') \
+			CXXFLAGS="$(CXXFLAGS) $(if $(ENABLE_OPTIMIZED),$(gb_COMPILEROPTFLAGS),$(gb_COMPILERNOOPTFLAGS)) $(if $(debug),$(gb_DEBUGINFO_FLAGS))" \
+		&& cd cpp && $(MAKE) \
+	)
+
+# vim: set noet sw=4 ts=4:
