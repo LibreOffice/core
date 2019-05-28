deb0e1e113256dfd0307e4b86c0c3f8394123e26
Tamás Zolnai <tamas.zolnai@collabora.com>
2019-05-28 17:32:44 +0200

    tdf#122765: Legend interferes with pie chart after pptx import, second part
    Revert of:
    2903c5f59d257b6ab94f32df64513f2edf94fb0b
    
    Reverting this commit does not lead to the original bug
    to come back, so we can revert it without a problem.
    
    Change-Id: I244a6b9451c47e1094db8a77c71b6696e0c464cc
    Reviewed-on: https://gerrit.libreoffice.org/73139
    Reviewed-by: Tamás Zolnai <tamas.zolnai@collabora.com>
    Tested-by: Tamás Zolnai <tamas.zolnai@collabora.com>
    (cherry picked from commit 63d17d01da0f9e3a7554e4327244ee4457b7188d)
    Reviewed-on: https://gerrit.libreoffice.org/73160
    Reviewed-by: Andras Timar <andras.timar@collabora.com>
    Tested-by: Andras Timar <andras.timar@collabora.com>

--- chart2/source/view/main/ChartView.cxx
+++ chart2/source/view/main/ChartView.cxx
@@ -1619,7 +1619,6 @@ awt::Rectangle ChartView::impl_createDiagramAndContent( const CreateShapeParam2D
     uno::Reference< drawing::XShapes > xSeriesTargetInFrontOfAxis(nullptr);
     uno::Reference< drawing::XShapes > xSeriesTargetBehindAxis(nullptr);
     VDiagram aVDiagram(xDiagram, aPreferredAspectRatio, nDimensionCount);
-    bool bIsPieOrDonut = lcl_IsPieOrDonut(xDiagram);
     {//create diagram
         aVDiagram.init(rParam.mxDiagramWithAxesShapes, m_xShapeFactory);
         aVDiagram.createShapes(
@@ -1628,7 +1627,7 @@ awt::Rectangle ChartView::impl_createDiagramAndContent( const CreateShapeParam2D
 
         xSeriesTargetInFrontOfAxis = aVDiagram.getCoordinateRegion();
         // It is preferable to use full size than minimum for pie charts
-        if (!bIsPieOrDonut && !rParam.mbUseFixedInnerSize)
+        if (!rParam.mbUseFixedInnerSize)
             aVDiagram.reduceToMimimumSize();
     }
 
@@ -1656,6 +1655,7 @@ awt::Rectangle ChartView::impl_createDiagramAndContent( const CreateShapeParam2D
 
     //use first coosys only so far; todo: calculate for more than one coosys if we have more in future
     //todo: this is just a workaround at the moment for pie and donut labels
+    bool bIsPieOrDonut = lcl_IsPieOrDonut(xDiagram);
     if( !bIsPieOrDonut && (!rVCooSysList.empty()) )
     {
         VCoordinateSystem* pVCooSys = rVCooSysList[0];
