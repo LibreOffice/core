LibreOffice/core

commit 497e40ad03c27837978551ba15491c3fb2a0bf53

improve refcounting loplugin
to find ref-counted classes being managed via other smart pointer
classes.
Hopefully prevent needing fixes like
642ae256ea5b8083ba0b3c097ca8ea52304b9cdb
"ChangedUIEventListener is refcounted, mustn't be helt by unique_ptr"

Change-Id: I6b0c5f8f87ce3546a8a1104ce1000470c09459bd
Reviewed-on: https://gerrit.libreoffice.org/39378
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

connectivity/source/commontools/TIndex.cxx
@@ -37,7 +37,7 @@ OIndexHelper::OIndexHelper( OTableHelper* _pTable) : connectivity::sdbcx::OIndex
 {
     construct();
     std::vector< OUString> aVector;
-    m_pColumns.reset( new OIndexColumns(this,m_aMutex,aVector) );
+    m_pColumns = new OIndexColumns(this,m_aMutex,aVector);
 }
 
 OIndexHelper::OIndexHelper( OTableHelper* _pTable,
@@ -93,7 +93,7 @@ void OIndexHelper::refreshColumns()
     if(m_pColumns)
         m_pColumns->reFill(aVector);
     else
-        m_pColumns.reset( new OIndexColumns(this,m_aMutex,aVector) );
+        m_pColumns = new OIndexColumns(this,m_aMutex,aVector);
 }
 
 

connectivity/source/commontools/TKey.cxx
@@ -100,7 +100,7 @@ void OTableKeyHelper::refreshColumns()
     if ( m_pColumns )
         m_pColumns->reFill(aVector);
     else
-        m_pColumns.reset( new OKeyColumnsHelper(this,m_aMutex,aVector) );
+        m_pColumns = new OKeyColumnsHelper(this,m_aMutex,aVector);
 }
 
 

connectivity/source/drivers/dbase/DIndex.cxx
@@ -103,7 +103,7 @@ void ODbaseIndex::refreshColumns()
     if(m_pColumns)
         m_pColumns->reFill(aVector);
     else
-        m_pColumns.reset( new ODbaseIndexColumns(this,m_aMutex,aVector) );
+        m_pColumns = new ODbaseIndexColumns(this,m_aMutex,aVector);
 }
 
 Sequence< sal_Int8 > ODbaseIndex::getUnoTunnelImplementationId()

include/connectivity/sdbcx/VGroup.hxx
@@ -56,7 +56,7 @@ namespace connectivity
                         public ODescriptor
         {
         protected:
-            std::unique_ptr<OUsers>    m_pUsers;
+            rtl::Reference<OUsers>    m_pUsers;
 
             using OGroup_BASE::rBHelper;
 

include/connectivity/sdbcx/VIndex.hxx
@@ -57,7 +57,7 @@ namespace connectivity
             bool            m_IsPrimaryKeyIndex;
             bool            m_IsClustered;
 
-            std::unique_ptr<OCollection>    m_pColumns;
+            rtl::Reference<OCollection>    m_pColumns;
 
             using ODescriptor_BASE::rBHelper;
             virtual void refreshColumns() override;

include/connectivity/sdbcx/VKey.hxx
@@ -69,7 +69,7 @@ namespace connectivity
         {
         protected:
             std::shared_ptr<KeyProperties>   m_aProps;
-            std::unique_ptr<OCollection>     m_pColumns;
+            rtl::Reference<OCollection>      m_pColumns;
 
             using ODescriptor_BASE::rBHelper;
             // OPropertyArrayUsageHelper

include/connectivity/sdbcx/VUser.hxx
@@ -53,7 +53,7 @@ namespace connectivity
                         public ODescriptor
         {
         protected:
-            std::unique_ptr<OGroups>  m_pGroups;
+            rtl::Reference<OGroups>  m_pGroups;
 
             using OUser_BASE::rBHelper;
 

compilerplugins/clang/refcounting.cxx
@@ -386,28 +386,67 @@ bool RefCounting::VisitFieldDecl(const FieldDecl * fieldDecl) {
         return true;
     }
 
-    std::string aParentName = fieldDecl->getParent()->getQualifiedNameAsString();
+    // check for dodgy code managing ref-counted stuff with shared_ptr or unique_ptr or similar stuff
+    QualType firstTemplateParamType;
+    if (auto recordType = fieldDecl->getType()->getUnqualifiedDesugaredType()->getAs<RecordType>()) {
+        auto recordDeclName = recordType->getDecl()->getName();
+        if (recordDeclName.find("unique_ptr") != StringRef::npos
+            || recordDeclName.find("shared_ptr") != StringRef::npos
+            || recordDeclName.find("intrusive_ptr") != StringRef::npos) // boost
+        {
+            auto templateDecl = dyn_cast<ClassTemplateSpecializationDecl>(recordType->getDecl());
+            if (templateDecl && templateDecl->getTemplateArgs().size() > 0)
+                firstTemplateParamType = templateDecl->getTemplateArgs()[0].getAsType();
+        }
+    }
 
     if (containsSvRefBaseSubclass(fieldDecl->getType().getTypePtr())) {
         report(
             DiagnosticsEngine::Warning,
-            "SvRefBase subclass being directly heap managed, should be managed via tools::SvRef, "
-            + fieldDecl->getType().getAsString()
-            + ", parent is " + aParentName,
+            "SvRefBase subclass %0 being directly heap managed, should be managed via tools::SvRef, "
+            ", parent is %1",
             fieldDecl->getLocation())
-          << fieldDecl->getSourceRange();
+            << fieldDecl->getType()
+            << fieldDecl->getParent()
+            << fieldDecl->getSourceRange();
+    }
+
+    if (!firstTemplateParamType.isNull() && containsSvRefBaseSubclass(firstTemplateParamType.getTypePtr()))
+    {
+        report(
+            DiagnosticsEngine::Warning,
+            "SvRefBase subclass %0 being managed via smart pointer, should be managed via tools::SvRef, "
+            "parent is %1",
+            fieldDecl->getLocation())
+            << firstTemplateParamType
+            << fieldDecl->getParent()
+            << fieldDecl->getSourceRange();
     }
 
     if (containsSalhelperReferenceObjectSubclass(fieldDecl->getType().getTypePtr())) {
         report(
             DiagnosticsEngine::Warning,
-            "salhelper::SimpleReferenceObject subclass being directly heap managed, should be managed via rtl::Reference, "
-            + fieldDecl->getType().getAsString()
-            + ", parent is " + aParentName,
+            "salhelper::SimpleReferenceObject subclass %0 being directly heap managed, should be managed via rtl::Reference, "
+            "parent is %1",
             fieldDecl->getLocation())
-          << fieldDecl->getSourceRange();
+            << fieldDecl->getType()
+            << fieldDecl->getParent()
+            << fieldDecl->getSourceRange();
     }
 
+    if (!firstTemplateParamType.isNull() && containsSalhelperReferenceObjectSubclass(firstTemplateParamType.getTypePtr()))
+    {
+        report(
+            DiagnosticsEngine::Warning,
+            "salhelper::SimpleReferenceObject subclass %0 being managed via smart pointer, should be managed via rtl::Reference, "
+            "parent is %1",
+            fieldDecl->getLocation())
+            << firstTemplateParamType
+            << fieldDecl->getParent()
+            << fieldDecl->getSourceRange();
+    }
+
+    std::string aParentName = fieldDecl->getParent()->getQualifiedNameAsString();
     if ( aParentName == "com::sun::star::uno::BaseReference"
          || aParentName == "cppu::detail::element_alias"
          // this is playing some kind of game to avoid circular references
@@ -419,11 +458,24 @@ bool RefCounting::VisitFieldDecl(const FieldDecl * fieldDecl) {
     if (containsXInterfaceSubclass(fieldDecl->getType())) {
         report(
             DiagnosticsEngine::Warning,
-            "XInterface subclass being directly heap managed, should be managed via uno::Reference, "
-            + fieldDecl->getType().getAsString()
-            + ", parent is " + aParentName,
+            "XInterface subclass %0 being directly heap managed, should be managed via uno::Reference, "
+            "parent is %1",
             fieldDecl->getLocation())
-          << fieldDecl->getSourceRange();
+            << fieldDecl->getType()
+            << fieldDecl->getParent()
+            << fieldDecl->getSourceRange();
+    }
+
+    if (!firstTemplateParamType.isNull() && containsXInterfaceSubclass(firstTemplateParamType))
+    {
+        report(
+            DiagnosticsEngine::Warning,
+            "XInterface subclass %0 being managed via smart pointer, should be managed via uno::Reference, "
+            "parent is %1",
+            fieldDecl->getLocation())
+            << firstTemplateParamType
+            << fieldDecl->getParent()
+            << fieldDecl->getSourceRange();
     }
 
     checkUnoReference(fieldDecl->getType(), fieldDecl, aParentName, "field");

commit 66223a4eb5ecba94f1e13eb8d8b6a198823cdee5

connectivity: fix memory leaks caused by OConnection::acquire()
Followup to 58f121ef2e680697e10453add43bab9b771d153a;
OConnection must not be held by rtl::Reference as that creates a cycle.

(regression from 497e40ad03c27837978551ba15491c3fb2a0bf53)

Change-Id: Ibd56d335e3e2631c5a57ea435f1035e89868a5a6

connectivity/source/commontools/TIndex.cxx
@@ -37,7 +37,7 @@ OIndexHelper::OIndexHelper( OTableHelper* _pTable) : connectivity::sdbcx::OIndex
 {
     construct();
     std::vector< OUString> aVector;
-    m_pColumns = new OIndexColumns(this,m_aMutex,aVector);
+    m_pColumns.reset(new OIndexColumns(this,m_aMutex,aVector));
 }
 
 OIndexHelper::OIndexHelper( OTableHelper* _pTable,
@@ -93,7 +93,7 @@ void OIndexHelper::refreshColumns()
     if(m_pColumns)
         m_pColumns->reFill(aVector);
     else
-        m_pColumns = new OIndexColumns(this,m_aMutex,aVector);
+        m_pColumns.reset(new OIndexColumns(this,m_aMutex,aVector));
 }
 
 

connectivity/source/commontools/TKey.cxx
@@ -100,7 +100,7 @@ void OTableKeyHelper::refreshColumns()
     if ( m_pColumns )
         m_pColumns->reFill(aVector);
     else
-        m_pColumns = new OKeyColumnsHelper(this,m_aMutex,aVector);
+        m_pColumns.reset(new OKeyColumnsHelper(this,m_aMutex,aVector));
 }
 
 

connectivity/source/drivers/dbase/DIndex.cxx
@@ -101,7 +101,7 @@ void ODbaseIndex::refreshColumns()
     if(m_pColumns)
         m_pColumns->reFill(aVector);
     else
-        m_pColumns = new ODbaseIndexColumns(this,m_aMutex,aVector);
+        m_pColumns.reset(new ODbaseIndexColumns(this,m_aMutex,aVector));
 }
 
 Sequence< sal_Int8 > ODbaseIndex::getUnoTunnelImplementationId()

include/connectivity/sdbcx/VGroup.hxx
@@ -51,7 +51,8 @@ namespace connectivity
                         public ODescriptor
         {
         protected:
-            rtl::Reference<OUsers>    m_pUsers;
+            // no Reference! see OCollection::acquire
+            std::unique_ptr<OUsers> m_pUsers;
 
             using OGroup_BASE::rBHelper;
 

include/connectivity/sdbcx/VIndex.hxx
@@ -53,7 +53,8 @@ namespace connectivity
             bool            m_IsPrimaryKeyIndex;
             bool            m_IsClustered;
 
-            rtl::Reference<OCollection>    m_pColumns;
+            // no Reference! see OCollection::acquire
+            std::unique_ptr<OCollection> m_pColumns;
 
             using ODescriptor_BASE::rBHelper;
             virtual void refreshColumns() override;

include/connectivity/sdbcx/VKey.hxx
@@ -68,7 +68,8 @@ namespace connectivity
         {
         protected:
             std::shared_ptr<KeyProperties>   m_aProps;
-            rtl::Reference<OCollection>      m_pColumns;
+            // no Reference! see OCollection::acquire
+            std::unique_ptr<OCollection> m_pColumns;
 
             using ODescriptor_BASE::rBHelper;
             // OPropertyArrayUsageHelper

include/connectivity/sdbcx/VUser.hxx
@@ -51,7 +51,8 @@ namespace connectivity
                         public ODescriptor
         {
         protected:
-            rtl::Reference<OGroups>  m_pGroups;
+            // no Reference! see OCollection::acquire
+            std::unique_ptr<OGroups> m_pGroups;
 
             using OUser_BASE::rBHelper;
 