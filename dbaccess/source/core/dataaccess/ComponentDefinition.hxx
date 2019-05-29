616ed2365434da0008b3be93b86fd49e0052f1aa
Michael Stahl <Michael.Stahl@cib.de>
2019-05-29 14:42:06 +0200

    dbaccess: fix more memory leaks caused by circular Reference
    Followup to 96ae2a3300811897c24cccb20f8c2faf382483df; more overridden
    acquire() creating cycles in dbaccess.
    
    (regression from 2660d24a07866e083c5135ea263030f3e3a2e729)
    
    Reviewed-on: https://gerrit.libreoffice.org/73158
    Reviewed-by: Michael Stahl <Michael.Stahl@cib.de>
    Tested-by: Michael Stahl <Michael.Stahl@cib.de>
    (cherry picked from commit 7d582d1f9767c0ebd5e319f2fd11711e60cd72fa)
    
    Change-Id: I134343b3afbcd5ee3f71212ec18e551455eaee5b
    Reviewed-on: https://gerrit.libreoffice.org/73253
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

--- dbaccess/source/core/dataaccess/ComponentDefinition.hxx
+++ dbaccess/source/core/dataaccess/ComponentDefinition.hxx
@@ -85,7 +85,8 @@ class OComponentDefinition  :public OContentHelper
                             ,public ::comphelper::OPropertyArrayUsageHelper< OComponentDefinition >
 {
 protected:
-    rtl::Reference< OColumns >     m_xColumns;
+    // no Reference! see OCollection::acquire
+    std::unique_ptr<OColumns> m_pColumns;
     rtl::Reference<OColumnPropertyListener> m_xColumnPropertyListener;
     bool                        m_bTable;
 
