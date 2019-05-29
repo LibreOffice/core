1064cd82fbd84e641807d091b62969ce13c6fe8d
Caolán McNamara <caolanm@redhat.com>
2019-05-29 12:41:16 +0100

    remove newly unused SfxTabDialog
    Change-Id: Iebbfe2ab66ff6e058c90c85de747f549cbc36aa5

--- sfx2/Library_sfx.mk
+++ sfx2/Library_sfx.mk
@@ -291,7 +291,6 @@ $(eval $(call gb_Library_add_exception_objects,sfx,\
     sfx2/source/statbar/stbitem \
     sfx2/source/styles/StyleManager \
     sfx2/source/toolbox/tbxitem \
-    sfx2/source/uitest/sfx_uiobject \
     sfx2/source/view/classificationcontroller \
     sfx2/source/view/classificationhelper \
     sfx2/source/view/frame \
