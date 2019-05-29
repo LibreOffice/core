LibreOffice/core

commit 497e40ad03c27837978551ba15491c3fb2a0bf53

improve refcounting loplugin
to find ref-counted classes being managed via other smart pointer
classes.
Hopefully prevent needing fixes like
642ae256ea5b8083ba0b3c097ca8ea52304b9cdb
"ChangedUIEventListener is refcounted, mustn't be helt by unique_ptr"

Change-Id: I6b0c5f8f87ce3546a8a1104ce1000470c09459bd
Reviewed-on: https://gerrit.libreoffice.org/39378
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

dbaccess/source/core/api/TableDeco.cxx
@@ -566,7 +566,7 @@ void ODBTableDecorator::refreshColumns()
         OContainerMediator* pMediator = new OContainerMediator( pCol, m_xColumnDefinitions );
         m_xColumnMediator = pMediator;
         pCol->setMediator( pMediator );
-        m_pColumns.reset( pCol );
+        m_pColumns = pCol;
     }
     else
         m_pColumns->reFill(aVector);

dbaccess/source/core/inc/TableDeco.hxx
@@ -74,7 +74,7 @@ namespace dbaccess
     // <properties>
         mutable sal_Int32                                         m_nPrivileges;
     // </properties>
-        std::unique_ptr<::connectivity::sdbcx::OCollection>       m_pColumns;
+        rtl::Reference<::connectivity::sdbcx::OCollection>       m_pColumns;
 
         // IColumnFactory
         virtual OColumn*    createColumn(const OUString& _rName) const override;

compilerplugins/clang/refcounting.cxx
@@ -386,28 +386,67 @@ bool RefCounting::VisitFieldDecl(const FieldDecl * fieldDecl) {
         return true;
     }
 
-    std::string aParentName = fieldDecl->getParent()->getQualifiedNameAsString();
+    // check for dodgy code managing ref-counted stuff with shared_ptr or unique_ptr or similar stuff
+    QualType firstTemplateParamType;
+    if (auto recordType = fieldDecl->getType()->getUnqualifiedDesugaredType()->getAs<RecordType>()) {
+        auto recordDeclName = recordType->getDecl()->getName();
+        if (recordDeclName.find("unique_ptr") != StringRef::npos
+            || recordDeclName.find("shared_ptr") != StringRef::npos
+            || recordDeclName.find("intrusive_ptr") != StringRef::npos) // boost
+        {
+            auto templateDecl = dyn_cast<ClassTemplateSpecializationDecl>(recordType->getDecl());
+            if (templateDecl && templateDecl->getTemplateArgs().size() > 0)
+                firstTemplateParamType = templateDecl->getTemplateArgs()[0].getAsType();
+        }
+    }
 
     if (containsSvRefBaseSubclass(fieldDecl->getType().getTypePtr())) {
         report(
             DiagnosticsEngine::Warning,
-            "SvRefBase subclass being directly heap managed, should be managed via tools::SvRef, "
-            + fieldDecl->getType().getAsString()
-            + ", parent is " + aParentName,
+            "SvRefBase subclass %0 being directly heap managed, should be managed via tools::SvRef, "
+            ", parent is %1",
             fieldDecl->getLocation())
-          << fieldDecl->getSourceRange();
+            << fieldDecl->getType()
+            << fieldDecl->getParent()
+            << fieldDecl->getSourceRange();
+    }
+
+    if (!firstTemplateParamType.isNull() && containsSvRefBaseSubclass(firstTemplateParamType.getTypePtr()))
+    {
+        report(
+            DiagnosticsEngine::Warning,
+            "SvRefBase subclass %0 being managed via smart pointer, should be managed via tools::SvRef, "
+            "parent is %1",
+            fieldDecl->getLocation())
+            << firstTemplateParamType
+            << fieldDecl->getParent()
+            << fieldDecl->getSourceRange();
     }
 
     if (containsSalhelperReferenceObjectSubclass(fieldDecl->getType().getTypePtr())) {
         report(
             DiagnosticsEngine::Warning,
-            "salhelper::SimpleReferenceObject subclass being directly heap managed, should be managed via rtl::Reference, "
-            + fieldDecl->getType().getAsString()
-            + ", parent is " + aParentName,
+            "salhelper::SimpleReferenceObject subclass %0 being directly heap managed, should be managed via rtl::Reference, "
+            "parent is %1",
             fieldDecl->getLocation())
-          << fieldDecl->getSourceRange();
+            << fieldDecl->getType()
+            << fieldDecl->getParent()
+            << fieldDecl->getSourceRange();
     }
 
+    if (!firstTemplateParamType.isNull() && containsSalhelperReferenceObjectSubclass(firstTemplateParamType.getTypePtr()))
+    {
+        report(
+            DiagnosticsEngine::Warning,
+            "salhelper::SimpleReferenceObject subclass %0 being managed via smart pointer, should be managed via rtl::Reference, "
+            "parent is %1",
+            fieldDecl->getLocation())
+            << firstTemplateParamType
+            << fieldDecl->getParent()
+            << fieldDecl->getSourceRange();
+    }
+
+    std::string aParentName = fieldDecl->getParent()->getQualifiedNameAsString();
     if ( aParentName == "com::sun::star::uno::BaseReference"
          || aParentName == "cppu::detail::element_alias"
          // this is playing some kind of game to avoid circular references
@@ -419,11 +458,24 @@ bool RefCounting::VisitFieldDecl(const FieldDecl * fieldDecl) {
     if (containsXInterfaceSubclass(fieldDecl->getType())) {
         report(
             DiagnosticsEngine::Warning,
-            "XInterface subclass being directly heap managed, should be managed via uno::Reference, "
-            + fieldDecl->getType().getAsString()
-            + ", parent is " + aParentName,
+            "XInterface subclass %0 being directly heap managed, should be managed via uno::Reference, "
+            "parent is %1",
             fieldDecl->getLocation())
-          << fieldDecl->getSourceRange();
+            << fieldDecl->getType()
+            << fieldDecl->getParent()
+            << fieldDecl->getSourceRange();
+    }
+
+    if (!firstTemplateParamType.isNull() && containsXInterfaceSubclass(firstTemplateParamType))
+    {
+        report(
+            DiagnosticsEngine::Warning,
+            "XInterface subclass %0 being managed via smart pointer, should be managed via uno::Reference, "
+            "parent is %1",
+            fieldDecl->getLocation())
+            << firstTemplateParamType
+            << fieldDecl->getParent()
+            << fieldDecl->getSourceRange();
     }
 
     checkUnoReference(fieldDecl->getType(), fieldDecl, aParentName, "field");

compilerplugins/clang/test/refcounting.cxx
@@ -1,0 +1,19 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <memory>
+#include <com/sun/star/uno/XInterface.hpp>
+
+struct Foo
+{
+    std::unique_ptr<css::uno::XInterface> m_foo1; // expected-error {{XInterface subclass 'com::sun::star::uno::XInterface' being managed via smart pointer, should be managed via uno::Reference, parent is 'Foo' [loplugin:refcounting]}}
+    std::shared_ptr<css::uno::XInterface> m_foo2; // expected-error {{XInterface subclass 'com::sun::star::uno::XInterface' being managed via smart pointer, should be managed via uno::Reference, parent is 'Foo' [loplugin:refcounting]}}
+};
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */

connectivity/source/commontools/TIndex.cxx
@@ -37,7 +37,7 @@ OIndexHelper::OIndexHelper( OTableHelper* _pTable) : connectivity::sdbcx::OIndex
 {
     construct();
     std::vector< OUString> aVector;
-    m_pColumns.reset( new OIndexColumns(this,m_aMutex,aVector) );
+    m_pColumns = new OIndexColumns(this,m_aMutex,aVector);
 }
 
 OIndexHelper::OIndexHelper( OTableHelper* _pTable,
@@ -93,7 +93,7 @@ void OIndexHelper::refreshColumns()
     if(m_pColumns)
         m_pColumns->reFill(aVector);
     else
-        m_pColumns.reset( new OIndexColumns(this,m_aMutex,aVector) );
+        m_pColumns = new OIndexColumns(this,m_aMutex,aVector);
 }
 
 

connectivity/source/commontools/TKey.cxx
@@ -100,7 +100,7 @@ void OTableKeyHelper::refreshColumns()
     if ( m_pColumns )
         m_pColumns->reFill(aVector);
     else
-        m_pColumns.reset( new OKeyColumnsHelper(this,m_aMutex,aVector) );
+        m_pColumns = new OKeyColumnsHelper(this,m_aMutex,aVector);
 }
 
 

connectivity/source/drivers/ado/AGroup.cxx
@@ -79,7 +79,7 @@ void OAdoGroup::refreshUsers()
     if(m_pUsers)
         m_pUsers->reFill(aVector);
     else
-        m_pUsers.reset( new OUsers(m_pCatalog,m_aMutex,aVector,aUsers,isCaseSensitive()) );
+        m_pUsers = new OUsers(m_pCatalog,m_aMutex,aVector,aUsers,isCaseSensitive());
 }
 
 Sequence< sal_Int8 > OAdoGroup::getUnoTunnelImplementationId()

connectivity/source/drivers/ado/AIndex.cxx
@@ -68,7 +68,7 @@ void OAdoIndex::refreshColumns()
     if ( m_pColumns )
         m_pColumns->reFill(aVector);
     else
-        m_pColumns.reset( new OColumns(*this,m_aMutex,aVector,aColumns,isCaseSensitive(),m_pConnection) );
+        m_pColumns = new OColumns(*this,m_aMutex,aVector,aColumns,isCaseSensitive(),m_pConnection);
 }
 
 

commit b01ee83a797cc2928bd7099b8550a8dc603e5bf4

tdf#114596 dbaccess: fix mysterious dataloss bug (part 2)
Same problem as in 96ae2a3300811897c24cccb20f8c2faf382483df

Regression from 497e40ad03c27837978551ba15491c3fb2a0bf53

Change-Id: I00e7bf3559e688e7fbc5429ace2b5c18221c9890

dbaccess/source/core/api/TableDeco.cxx
@@ -553,7 +553,7 @@ void ODBTableDecorator::refreshColumns()
         OContainerMediator* pMediator = new OContainerMediator( pCol, m_xColumnDefinitions );
         m_xColumnMediator = pMediator;
         pCol->setMediator( pMediator );
-        m_pColumns = pCol;
+        m_pColumns.reset(pCol);
     }
     else
         m_pColumns->reFill(aVector);

dbaccess/source/core/inc/TableDeco.hxx
@@ -71,10 +71,11 @@ namespace dbaccess
         css::uno::Reference< css::sdbc::XDatabaseMetaData >       m_xMetaData;
         css::uno::Reference< css::util::XNumberFormatsSupplier >  m_xNumberFormats;
 
-    // <properties>
+        // <properties>
         mutable sal_Int32                                         m_nPrivileges;
-    // </properties>
-        rtl::Reference<::connectivity::sdbcx::OCollection>       m_pColumns;
+        // </properties>
+        // note: this thing uses the ref-count of "this", see OCollection::acquire()!
+        std::unique_ptr<::connectivity::sdbcx::OCollection>       m_pColumns;
 
         // IColumnFactory
         virtual OColumn*    createColumn(const OUString& _rName) const override;