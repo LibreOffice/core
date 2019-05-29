6cd9d977799aaed7047be5ef3d2f113b564191ec
Caolán McNamara <caolanm@redhat.com>
2019-05-29 12:41:16 +0100

    remove newly unused SfxTabDialog
    Change-Id: Iebbfe2ab66ff6e058c90c85de747f549cbc36aa5

--- cui/source/options/optjava.hxx
+++ cui/source/options/optjava.hxx
@@ -98,7 +98,7 @@ private:
     DECL_LINK(        StartFolderPickerHdl, void *, void );
     DECL_LINK(        DialogClosedHdl, css::ui::dialogs::DialogClosedEvent*, void );
 
-    DECL_LINK(        ExpertConfigHdl_Impl, Button*, void);
+    DECL_STATIC_LINK(SvxJavaOptionsPage, ExpertConfigHdl_Impl, Button*, void);
 
     void                    ClearJavaInfo();
     void                    ClearJavaList();
