f0340d58cc0f2808ce8003099ff2a0495360f267
Szymon Kłos <szymon.klos@collabora.com>
2019-05-31 19:21:19 +0200

    Don't send false 'INPLACE' messages
    Change-Id: I8eb9a77c7e6b9242f18ac5f0634091c88bf40ec0
    Reviewed-on: https://gerrit.libreoffice.org/80695
    Tested-by: Jenkins
    Reviewed-by: Michael Meeks <michael.meeks@collabora.com>

--- sfx2/source/view/ipclient.cxx
+++ sfx2/source/view/ipclient.cxx
@@ -336,6 +336,13 @@ void SAL_CALL SfxInPlaceClient_Impl::activatingInplace()
 {
     if ( !m_pClient || !m_pClient->GetViewShell() )
         throw uno::RuntimeException();
+
+    if ( comphelper::LibreOfficeKit::isActive() )
+    {
+        if ( SfxViewShell* pViewShell = m_pClient->GetViewShell() )
+            pViewShell->libreOfficeKitViewCallback( LOK_CALLBACK_GRAPHIC_SELECTION, "INPLACE" );
+    }
+
 }
 
 
