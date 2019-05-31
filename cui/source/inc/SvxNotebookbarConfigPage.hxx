40ee5285fc0cd088c8a1f84600573d6ba92e8a4d
Sumit Chauhan <sumitcn25@gmail.com>
2019-05-31 14:59:54 +0530

    UI for the Notebookbar Customization tab
    This patch generates the UI for the notebookbar customization tab.
    Patch is working and one can show/hide the UIItem by clicking on the
    checkbox.
    
    Change-Id: I4923c57e6443474f178a72922646619f07b5ffc1

---
+++ cui/source/inc/SvxNotebookbarConfigPage.hxx
@@ -0,0 +1,85 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+#ifndef INCLUDED_CUI_SOURCE_INC_SVXNOTEBOOKBARCONFIGPAGE_HXX
+#define INCLUDED_CUI_SOURCE_INC_SVXNOTEBOOKBARCONFIGPAGE_HXX
+
+#include <libxml/xmlmemory.h>
+#include <libxml/parser.h>
+#include <vcl/weld.hxx>
+#include <com/sun/star/beans/XPropertySet.hpp>
+#include <com/sun/star/container/XIndexContainer.hpp>
+#include <com/sun/star/container/XNameAccess.hpp>
+#include <com/sun/star/ui/XUIConfigurationListener.hpp>
+#include <com/sun/star/ui/XUIConfigurationManager.hpp>
+#include <com/sun/star/ui/XImageManager.hpp>
+#include <com/sun/star/frame/XFrame.hpp>
+#include <com/sun/star/uno/XComponentContext.hpp>
+#include <com/sun/star/lang/XSingleComponentFactory.hpp>
+
+#include <sfx2/tabdlg.hxx>
+#include <memory>
+#include <vector>
+
+//#include "cfgutil.hxx"
+#include "cfg.hxx" //for SvxConfigPage and SaveInData
+
+class SvxNotebookbarConfigPage : public SvxConfigPage
+{
+private:
+    void UpdateButtonStates() override;
+    short QueryReset() override;
+    void Init() override;
+    void DeleteSelectedContent() override;
+    void DeleteSelectedTopLevel() override;
+    virtual void SelectElement() override;
+
+public:
+    struct NotebookbarEntries
+    {
+        OUString sUIItemID;
+        OUString sActionName;
+        OUString sVisibleValue;
+        int nPos;
+    };
+    SvxNotebookbarConfigPage(TabPageParent pParent, const SfxItemSet& rItemSet);
+    virtual ~SvxNotebookbarConfigPage() override;
+    SaveInData* CreateSaveInData(const css::uno::Reference<css::ui::XUIConfigurationManager>&,
+                                 const css::uno::Reference<css::ui::XUIConfigurationManager>&,
+                                 const OUString& aModuleId, bool docConfig) override;
+    static void FillFunctionsList(std::vector<NotebookbarEntries>& aEntries);
+    static void searchNodeandAttribute(std::vector<NotebookbarEntries>& aEntries, xmlNode* pNodePtr,
+                                       int nPos);
+    static void getNodeValue(xmlNode* pNodePtr, NotebookbarEntries& aNodeEntries);
+};
+
+class SvxNotebookbarEntriesListBox final : public SvxMenuEntriesListBox
+{
+    void ChangedVisibility(int nRow);
+    typedef std::pair<int, int> row_col;
+    DECL_LINK(CheckButtonHdl, const row_col&, void);
+    DECL_LINK(KeyInputHdl, const KeyEvent&, bool);
+
+public:
+    SvxNotebookbarEntriesListBox(std::unique_ptr<weld::TreeView> xControl, SvxConfigPage* pPg);
+    virtual ~SvxNotebookbarEntriesListBox() override;
+};
+
+#endif // INCLUDED_CUI_SOURCE_INC_SVXNOTEBOOKBARCONFIGPAGE_HXX
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
