7ae354290af7f3fcd9c5031e437af0037e61e984
Miklos Vajna <vmiklos@collabora.com>
2019-05-31 14:56:46 +0200

    tdf#125550 vcl menu bar / floating window: fix text color
    Regression from commit e8d5b8beb5958147235ff955ed38c47b51d860ff
    (tdf#113714 vcl menu bar window: avoid flicker, 2019-05-20), the problem
    was that a freshly created VirtualDevice doesn't have the default text
    color, so we need to initialize that explicitly based on the render
    context text color.
    
    Also introduce a BufferDevice to do this initialization, instead of
    fixing this in MenuBarWindow::Paint(), then copy&pasting the fix to
    MenuFloatingWindow::Paint().
    
    Change-Id: Ib171cd52e7cabe0bc3c639821f558d8303039fe6
    Reviewed-on: https://gerrit.libreoffice.org/73269
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Tested-by: Jenkins
    (cherry picked from commit 42bf893a8479f70d7d8f00e03105ce15e8545f8b)
    Reviewed-on: https://gerrit.libreoffice.org/73314
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>
    Tested-by: Michael Weghorn <m.weghorn@posteo.de>

--- vcl/Library_vcl.mk
+++ vcl/Library_vcl.mk
@@ -121,6 +121,7 @@ $(eval $(call gb_Library_add_exception_objects,vcl,\
     vcl/source/window/accel \
     vcl/source/window/accmgr \
     vcl/source/window/brdwin \
+    vcl/source/window/bufferdevice \
     vcl/source/window/accessibility \
     vcl/source/window/legacyaccessibility \
     vcl/source/window/clipping \
