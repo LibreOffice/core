3e27cea44ccb2a34bd96c03ed2f279324a145ea6
Michael Meeks <michael.meeks@collabora.com>
2019-05-31 14:38:34 +0100

    Avoid odd crash where OLE object can't generate an image preview.
    Change-Id: Ib77eca6907aa09383a96f3c9de6f7a7c4799c3bc

--- sw/source/filter/html/htmlplug.cxx
+++ sw/source/filter/html/htmlplug.cxx
@@ -1503,6 +1503,12 @@ Writer& OutHTML_FrameFormatOLENodeGrf( Writer& rWrt, const SwFrameFormat& rFrame
         return rWrt;
     }
 
+    if ( !pOLENd->GetGraphic() )
+    {
+        SAL_WARN("sw.html", "Unexpected missing OLE fallback graphic");
+        return rWrt;
+    }
+
     Graphic aGraphic( *pOLENd->GetGraphic() );
 
     SwDocShell* pDocSh = rHTMLWrt.m_pDoc->GetDocShell();
