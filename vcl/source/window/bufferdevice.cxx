6b2ef897ebaafba05b8dafa5c53981b8e2e02fba
Miklos Vajna <vmiklos@collabora.com>
2019-05-31 14:56:46 +0200

    tdf#125550 vcl menu bar / floating window: fix text color
    Regression from commit e8d5b8beb5958147235ff955ed38c47b51d860ff
    (tdf#113714 vcl menu bar window: avoid flicker, 2019-05-20), the problem
    was that a freshly created VirtualDevice doesn't have the default text
    color, so we need to initialize that explicitly based on the render
    context text color.
    
    Also introduce a BufferDevice to do this initialization, instead of
    fixing this in MenuBarWindow::Paint(), then copy&pasting the fix to
    MenuFloatingWindow::Paint().
    
    Change-Id: Ib171cd52e7cabe0bc3c639821f558d8303039fe6
    Reviewed-on: https://gerrit.libreoffice.org/73269
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Tested-by: Jenkins
    (cherry picked from commit 42bf893a8479f70d7d8f00e03105ce15e8545f8b)

---
+++ vcl/source/window/bufferdevice.cxx
@@ -0,0 +1,36 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "bufferdevice.hxx"
+
+namespace vcl
+{
+BufferDevice::BufferDevice(const VclPtr<vcl::Window>& pWindow, vcl::RenderContext& rRenderContext)
+    : m_pBuffer(VclPtr<VirtualDevice>::Create())
+    , m_pWindow(pWindow)
+    , m_rRenderContext(rRenderContext)
+{
+    m_pBuffer->SetOutputSizePixel(pWindow->GetOutputSizePixel(), false);
+    m_pBuffer->SetTextColor(rRenderContext.GetTextColor());
+    m_pBuffer->DrawOutDev(Point(0, 0), pWindow->GetOutputSizePixel(), Point(0, 0),
+                          pWindow->GetOutputSizePixel(), rRenderContext);
+}
+
+BufferDevice::~BufferDevice()
+{
+    m_rRenderContext.DrawOutDev(Point(0, 0), m_pWindow->GetOutputSizePixel(), Point(0, 0),
+                                m_pWindow->GetOutputSizePixel(), *m_pBuffer);
+}
+
+vcl::RenderContext* BufferDevice::operator->() { return m_pBuffer.get(); }
+
+vcl::RenderContext& BufferDevice::operator*() { return *m_pBuffer; }
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
