33ca4dbcc91164a3725263d8a8f61d9f2866da8f
Jan-Marek Glogowski <glogow@fbihome.de>
2019-06-02 20:17:49 +0000

    Qt5/KDE5 always use either Qt5 or KDE5 pickers
    And use RunInMain for both SalInstances and for both picker types,
    as there is no reason to assume just the file and not the folder
    picker can be called from the non-GUI thread.
    
    Little drawback is the inclusion of Qt5FilePicker header in the
    Qt5Instance header, as Qt's enums aren't forward-declarable.
    
    Change-Id: Ie170d247a76134df9aff835393c71c9d6e907d32
    Reviewed-on: https://gerrit.libreoffice.org/73416
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>
    Tested-by: Jenkins
    Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>
    (cherry picked from commit ca69a1e17782d88580f2449e0252be52c26cae42)
    Reviewed-on: https://gerrit.libreoffice.org/73721

--- vcl/qt5/Qt5Instance.cxx
+++ vcl/qt5/Qt5Instance.cxx
@@ -394,18 +394,32 @@ void Qt5Instance::ProcessEvent(SalUserEvent aEvent)
     aEvent.m_pFrame->CallCallback(aEvent.m_nEvent, aEvent.m_pData);
 }
 
+Qt5FilePicker* Qt5Instance::createPicker(QFileDialog::FileMode eMode)
+{
+    if (!IsMainThread())
+    {
+        SolarMutexGuard g;
+        Qt5FilePicker* pPicker;
+        RunInMainThread(std::function([&, this]() { pPicker = createPicker(eMode); }));
+        assert(pPicker);
+        return pPicker;
+    }
+
+    return new Qt5FilePicker(eMode);
+}
+
 css::uno::Reference<css::ui::dialogs::XFilePicker2>
 Qt5Instance::createFilePicker(const css::uno::Reference<css::uno::XComponentContext>&)
 {
     return css::uno::Reference<css::ui::dialogs::XFilePicker2>(
-        new Qt5FilePicker(QFileDialog::ExistingFile));
+        createPicker(QFileDialog::ExistingFile));
 }
 
 css::uno::Reference<css::ui::dialogs::XFolderPicker2>
 Qt5Instance::createFolderPicker(const css::uno::Reference<css::uno::XComponentContext>&)
 {
     return css::uno::Reference<css::ui::dialogs::XFolderPicker2>(
-        new Qt5FilePicker(QFileDialog::Directory));
+        createPicker(QFileDialog::Directory));
 }
 
 css::uno::Reference<css::uno::XInterface>
