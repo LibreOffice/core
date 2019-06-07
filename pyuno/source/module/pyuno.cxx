0ab8fbc16023df240827f4ab4566d75f0c6796fe
Stephan Bergmann <sbergman@redhat.com>
2019-06-07 15:57:13 +0200

    Adapt to upcoming Python 3.8
    ...which changed PyTypeObject in <https://github.com/python/cpython/commit/
    aacc77fbd77640a8f03638216fa09372cc21673d> "bpo-36974: implement PEP 590
    (GH-13185)".
    
    Change-Id: I687ec38aeda05d0747b9ed08221db75a758bed51
    Reviewed-on: https://gerrit.libreoffice.org/73664
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>
    (cherry picked from commit 2bd585f31d7abb066e3f53d9b29c822af20aea69)
    Reviewed-on: https://gerrit.libreoffice.org/73778

--- pyuno/source/module/pyuno.cxx
+++ pyuno/source/module/pyuno.cxx
@@ -1614,7 +1614,11 @@ static PyTypeObject PyUNOType =
     sizeof (PyUNO),
     0,
     PyUNO_del,
-    nullptr,
+#if PY_VERSION_HEX >= 0x03080000
+    0, // Py_ssize_t tp_vectorcall_offset
+#else
+    nullptr, // printfunc tp_print
+#endif
     PyUNO_getattr,
     PyUNO_setattr,
     /* this type does not exist in Python 3: (cmpfunc) */ nullptr,
@@ -1660,6 +1664,9 @@ static PyTypeObject PyUNOType =
 #endif
 #if PY_VERSION_HEX >= 0x03040000
     , nullptr
+#if PY_VERSION_HEX >= 0x03080000
+    , nullptr // vectorcallfunc tp_vectorcall
+#endif
 #endif
 };
 
