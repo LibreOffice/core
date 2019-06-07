LibreOffice/core

commit edc85fb55f8adc30a1416c6c854c89097060fb21

clang-tidy performance-unnecessary-copy-init in idl..reportdesign
Change-Id: I4b2eda375bbfe1ba35a45168515d885935adcb76
Reviewed-on: https://gerrit.libreoffice.org/62217
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

jvmfwk/plugins/sunmajor/pluginlib/util.cxx
@@ -733,9 +733,8 @@ void bubbleSortVersion(vector<rtl::Reference<VendorBase> >& vec)
             }
             if(nCmp == 1) // cur > next
             {
-                rtl::Reference<VendorBase> less = next;
                 vec.at(j-1)= cur;
-                vec.at(j)= less;
+                vec.at(j)= next;
             }
         }
         ++cIter;

idl/source/prj/database.cxx
@@ -284,7 +284,7 @@ SvMetaType * SvIdlDataBase::ReadKnownType( SvTokenStream & rInStm )
 
     if( rTok.IsIdentifier() )
     {
-        OString aName = rTok.GetString();
+        const OString& aName = rTok.GetString();
         for( const auto& aType : GetTypeList() )
         {
             if( aType->GetName() == aName )

l10ntools/source/pocheck.cxx
@@ -45,7 +45,7 @@ static void checkStyleNames(const OString& aLanguage)
 
         if( !aPoEntry.isFuzzy() && aPoEntry.getMsgCtxt().startsWith("STR_POOLCOLL") )
         {
-            OString aMsgStr = aPoEntry.getMsgStr();
+            const OString& aMsgStr = aPoEntry.getMsgStr();
             if( aMsgStr.isEmpty() )
                 continue;
             if( aLocalizedStyleNames.find(aMsgStr) == aLocalizedStyleNames.end() )
@@ -57,7 +57,7 @@ static void checkStyleNames(const OString& aLanguage)
         }
         if( !aPoEntry.isFuzzy() && aPoEntry.getMsgCtxt().startsWith("STR_POOLNUMRULE") )
         {
-            OString aMsgStr = aPoEntry.getMsgStr();
+            const OString& aMsgStr = aPoEntry.getMsgStr();
             if( aMsgStr.isEmpty() )
                 continue;
             if( aLocalizedNumStyleNames.find(aMsgStr) == aLocalizedNumStyleNames.end() )
@@ -163,7 +163,7 @@ static void checkFunctionNames(const OString& aLanguage)
             break;
         if( !aPoEntry.isFuzzy() && aPoEntry.getMsgCtxt() == "RID_STRLIST_FUNCTION_NAMES" )
         {
-            OString aMsgStr = aPoEntry.getMsgStr();
+            const OString& aMsgStr = aPoEntry.getMsgStr();
             if( aMsgStr.isEmpty() )
                 continue;
             if( aLocalizedCoreFunctionNames.find(aMsgStr) == aLocalizedCoreFunctionNames.end() )

l10ntools/source/xrmmerge.cxx
@@ -405,12 +405,11 @@ void XRMResMerge::WorkOnDesc(
             OString sDescFilename = GetAttribute ( rOpenTag, "xlink:href" );
             for( size_t n = 0; n < aLanguages.size(); n++ ){
                 sCur = aLanguages[ n ];
-                OString sContent;
+                OString sText;
                 if ( !sCur.equalsIgnoreAsciiCase("en-US")  &&
-                    ( pEntrys->GetText( sContent, sCur, true )) &&
-                    !sContent.isEmpty())
+                    ( pEntrys->GetText( sText, sCur, true )) &&
+                    !sText.isEmpty())
                 {
-                    OString sText( sContent );
                     OString sAdditionalLine( "\n        " );
                     sAdditionalLine += rOpenTag;
                     OString sSearch = sLangAttribute;
@@ -498,7 +497,7 @@ void XRMResMerge::EndOfText(
                     !sContent.isEmpty() &&
                     helper::isWellFormedXML( sContent ))
                 {
-                    OString sText( sContent );
+                    const OString& sText( sContent );
                     OString sAdditionalLine( "\n        " );
                     sAdditionalLine += rOpenTag;
                     OString sSearch = sLangAttribute;

lingucomponent/source/lingutil/lingutil.cxx
@@ -112,7 +112,7 @@ static void GetOldStyleDicsInDir(
                     continue;
 
                 // Thus we first get the language of the dictionary
-                OUString aLocaleName(aLangTag.getBcp47());
+                const OUString& aLocaleName(aLangTag.getBcp47());
 
                 if (aDicLangInUse.insert(aLocaleName).second)
                 {

linguistic/source/convdicxml.cxx
@@ -329,9 +329,8 @@ void ConvDicXMLExport::ExportContent_()
     for (auto const& elem : rDic.aFromLeft)
         aKeySet.insert( elem.first );
 
-    for (auto const& elem : aKeySet)
+    for (const OUString& aLeftText : aKeySet)
     {
-        OUString aLeftText(elem);
         AddAttribute( XML_NAMESPACE_TCD, "left-text", aLeftText );
         if (rDic.pConvPropType) // property-type list available?
         {
@@ -348,10 +347,10 @@ void ConvDicXMLExport::ExportContent_()
                 "entry" , true, true );
 
         pair< ConvMap::iterator, ConvMap::iterator > aRange =
-                rDic.aFromLeft.equal_range(elem);
+                rDic.aFromLeft.equal_range(aLeftText);
         for (auto aIt = aRange.first;  aIt != aRange.second;  ++aIt)
         {
-            DBG_ASSERT( elem == (*aIt).first, "key <-> entry mismatch" );
+            DBG_ASSERT( aLeftText == (*aIt).first, "key <-> entry mismatch" );
             OUString aRightText( (*aIt).second );
             SvXMLElementExport aEntryRightText( *this, XML_NAMESPACE_TCD,
                     "right-text" , true, false );

oox/source/drawingml/diagram/layoutatomvisitors.cxx
@@ -220,7 +220,7 @@ void ShapeTemplateVisitor::visit(ShapeAtom& rAtom)
         return;
     }
 
-    ShapePtr pCurrShape(rAtom.getShapeTemplate());
+    const ShapePtr& pCurrShape(rAtom.getShapeTemplate());
 
     // TODO(F3): cloned shape shares all properties by reference,
     // don't change them!

oox/source/export/vmlexport.cxx
@@ -666,7 +666,7 @@ void VMLExport::Commit( EscherPropertyContainer& rProps, const tools::Rectangle&
                             XFastAttributeListRef(pAttrListSignatureLine));
 
                         // Get signature line graphic
-                        const uno::Reference<graphic::XGraphic> xGraphic
+                        const uno::Reference<graphic::XGraphic>& xGraphic
                             = pSdrGrafObj->getSignatureLineUnsignedGraphic();
                         Graphic aGraphic(xGraphic);
 

commit 838a5d6c115c46048da2e29afcf038baff949530

Fix swapping of vector elements
...which had been broken with edc85fb55f8adc30a1416c6c854c89097060fb21
"clang-tidy performance-unnecessary-copy-init in idl..reportdesign" (and could
cause "Tools - Options... - LibreOffice - Advanced - Java Options" to
erroneously list a single JRE multiple times, instead of listing different
JREs).

Change-Id: I6799903be39a5c6aa601131fad6eab3edea8d9e4

jvmfwk/plugins/sunmajor/pluginlib/util.cxx
@@ -731,8 +731,7 @@ void bubbleSortVersion(vector<rtl::Reference<VendorBase> >& vec)
             }
             if(nCmp == 1) // cur > next
             {
-                vec.at(j-1)= cur;
-                vec.at(j)= next;
+                std::swap(vec.at(j-1), vec.at(j));
             }
         }
         ++cIter;