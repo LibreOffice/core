54b588724005c502590a2f43ea42f6b2fb8fb1f2
Miklos Vajna <vmiklos@collabora.com>
2019-06-07 11:12:31 +0200

    tdf#117347 sw form controls: fix disappearing FixedText when editing TextField
    This is nominally a regression from commit
    af11abf3626e12d2b4b7dd9d255c6c71bf84cd4b (SwViewShell::ImplEndAction:
    still paint directly when non-double-buffering, 2015-09-01), but that
    just uncovered an already existing problem introduced earlier in commit
    2b26c1796d0a05f47cfb01d79ee4f69344efbbb2 (tdf#92577 sw rendercontext:
    paint transparent from controls conditionally, 2015-07-06).
    
    Given that the first commit (as a side-effect) solved the problem the
    second commit tried to solve, it's safe to just revert this.
    
    (Sadly we have no way to test incremental paints with a reasonable
    amount of effort, so no testcase.)
    
    (cherry picked from commit 00dfa6dc890dbbc8140fe613599becb5e4c55486)
    
    Change-Id: Ia59efbf45a0cd5af3580914e1dcee74d89068f03
    Reviewed-on: https://gerrit.libreoffice.org/73658
    Tested-by: Jenkins
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    (cherry picked from commit 802e3a960e166b24e18da04a821862620047f37f)
    Reviewed-on: https://gerrit.libreoffice.org/73751
    Reviewed-by: Xisco Faulí <xiscofauli@libreoffice.org>

--- sw/source/core/view/viewsh.cxx
+++ sw/source/core/view/viewsh.cxx
@@ -432,8 +432,8 @@ void SwViewShell::ImplEndAction( const bool bIdleEnd )
                             DLPostPaint2(true);
                         }
                     }
-                    else
-                        lcl_PaintTransparentFormControls(*this, aRect); // i#107365
+
+                    lcl_PaintTransparentFormControls(*this, aRect); // i#107365
                 }
 
                 delete pRegion;
