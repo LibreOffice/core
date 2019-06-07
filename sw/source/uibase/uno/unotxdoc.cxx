fd3e15079852cefbc5f0cdcdc06f5fd579a9f7e7
Scott Clarke <scott.clarke@codethink.co.uk>
2019-06-07 15:28:42 +0100

    Add storage and serialisation for the Resolved flag
    Change-Id: I0278588dc533378104e544d05b60583c85271f6a
    Co-authored-by: Jim MacArthur <jim.macarthur@codethink.co.uk>

--- sw/source/uibase/uno/unotxdoc.cxx
+++ sw/source/uibase/uno/unotxdoc.cxx
@@ -3370,6 +3370,7 @@ OUString SwXTextDocument::getPostIts()
         aAnnotation.put("parent", pWin->CalcParent());
         aAnnotation.put("author", pField->GetPar1().toUtf8().getStr());
         aAnnotation.put("text", pField->GetPar2().toUtf8().getStr());
+        aAnnotation.put("resolved", pField->GetResolved() ? "true" : "false");
         aAnnotation.put("dateTime", utl::toISO8601(pField->GetDateTime().GetUNODateTime()));
         aAnnotation.put("anchorPos", aSVRect.toString());
         aAnnotation.put("textRange", sRects.getStr());
