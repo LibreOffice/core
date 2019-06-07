LibreOffice/core

commit af11abf3626e12d2b4b7dd9d255c6c71bf84cd4b

SwViewShell::ImplEndAction: still paint directly when non-double-buffering
It turns out that in this case it hurts performance if we go via
invaliation-then-paint instead of direct painting when double-buffering
is not enabled.

Related commits:

- beb4aa21d61f0d66392d596be86fb57b4b167239 (SwViewShell::ImplEndAction:
  avoid direct paint, 2015-06-29) and
  c9175a1bd3249ad573ae6827bf19963a3ebe2fbc (SwViewShell::ImplEndAction:
  avoid direct PaintDesktop(), 2015-07-03) globally avoided direct paints
- 222f10e773ba51a19880be1b798990260c198147 (tdf#93096 sw: fix selection
  with keyboard outside current view, 2015-08-18) restored direct paint
  when non-double-buffering for the second commit, this one does the
  same for the first

Change-Id: Ida0c6917a8cdec74209bd64813c7876d4fa61b8a

sw/source/core/view/viewsh.cxx
@@ -419,7 +419,10 @@ void SwViewShell::ImplEndAction( const bool bIdleEnd )
 
                             if ( bPaintsFromSystem )
                                 PaintDesktop(*GetOut(), aRect);
-                            pCurrentLayout->GetCurrShell()->InvalidateWindows(aRect.SVRect());
+                            if (!isTiledRendering())
+                                pCurrentLayout->Paint( *mpOut, aRect );
+                            else
+                                pCurrentLayout->GetCurrShell()->InvalidateWindows(aRect.SVRect());
 
                             // #i75172# end DrawingLayer paint
                             DLPostPaint2(true);

commit ba7976dee48fe02dcde82311e39387feb4e94e5c

tdf#117347 sw form controls: fix disappearing FixedText when editing TextField
This is nominally a regression from commit
af11abf3626e12d2b4b7dd9d255c6c71bf84cd4b (SwViewShell::ImplEndAction:
still paint directly when non-double-buffering, 2015-09-01), but that
just uncovered an already existing problem introduced earlier in commit
2b26c1796d0a05f47cfb01d79ee4f69344efbbb2 (tdf#92577 sw rendercontext:
paint transparent from controls conditionally, 2015-07-06).

Given that the first commit (as a side-effect) solved the problem the
second commit tried to solve, it's safe to just revert this.

(Sadly we have no way to test incremental paints with a reasonable
amount of effort, so no testcase.)

Change-Id: Ia59efbf45a0cd5af3580914e1dcee74d89068f03

sw/source/core/view/viewsh.cxx
@@ -430,8 +430,8 @@ void SwViewShell::ImplEndAction( const bool bIdleEnd )
                             DLPostPaint2(true);
                         }
                     }
-                    else
-                        lcl_PaintTransparentFormControls(*this, aRect); // i#107365
+
+                    lcl_PaintTransparentFormControls(*this, aRect); // i#107365
                 }
             }
             if( bShowCursor )