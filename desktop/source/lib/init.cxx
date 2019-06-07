1c9e4ae43ec01379f25de9dbc4f2d2c33db7264c
Scott Clarke <scott.clarke@codethink.co.uk>
2019-06-07 15:28:42 +0100

    Add storage and serialisation for the Resolved flag
    Change-Id: Id3751e1b388b570f0a99018cb87366b2cff77ff5
    Co-authored-by: Jim MacArthur <jim.macarthur@codethink.co.uk>

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -2137,6 +2137,7 @@ static void doc_iniUnoCommands ()
         OUString(".uno:InsertAnnotation"),
         OUString(".uno:DeleteAnnotation"),
         OUString(".uno:ReplyComment"),
+        OUString(".uno:ResolveComment"),
         OUString(".uno:InsertRowsBefore"),
         OUString(".uno:InsertRowsAfter"),
         OUString(".uno:InsertColumnsBefore"),
