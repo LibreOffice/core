a7894d0747cebae44774decd22f9c48160679ac2
Shubham Goyal <22shubh22@gmail.com>
2019-06-09 11:28:12 +0530

    wip:Qr code Dialog Box.
    Change-Id: I1cee6f0e7fac585de880a9ac34e3bc441a4b7390

---
+++ cui/source/dialogs/QrCodeGenDialogBase.cxx
@@ -0,0 +1,39 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <QrCodeGenDialogBase.hxx>
+
+#include <utility>
+#include <vcl/weld.hxx>
+
+using namespace css;
+using namespace css::uno;
+using namespace css::frame;
+
+QrCodeGenDialogBase::QrCodeGenDialogBase(weld::Widget* pParent, Reference<XModel> xModel,
+                                                 const OUString& rUIFile, const OString& rDialogId)
+    : GenericDialogController(pParent, rUIFile, rDialogId)
+    , m_xModel(std::move(xModel))
+{
+}
+
+short QrCodeGenDialogBase::run()
+{
+    short nRet = GenericDialogController::run();
+    if (nRet == RET_OK)
+        Apply();
+    return nRet;
+}
+
+OUString QrCodeGenDialogBase::getCDataString(const OUString& rString)
+{
+    return "<![CDATA[" + rString + "]]>";
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
