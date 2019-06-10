53fa488104b8723ba6de2063338e14544cb13da3
Andrea Gelmini <andrea.gelmini@gelma.net>
2019-06-10 16:56:30 +0000

    Fix typo
    Change-Id: I7c6f8478b95fa3904b1cec0cf5dbfc6885fd2810
    Reviewed-on: https://gerrit.libreoffice.org/74002
    Tested-by: Jenkins
    Reviewed-by: Jens Carl <j.carl43@gmx.de>

--- framework/qa/complex/api_internal/CheckAPI.java
+++ framework/qa/complex/api_internal/CheckAPI.java
@@ -41,7 +41,7 @@ import com.sun.star.uno.UnoRuntime;
 
 /**
  * This test executes the API tests internally in LibreOffice. Prerequisite is
- * that a OOoRunner.jar is registered inside of LibreOffice. Adjust the joblist
+ * that an OOoRunner.jar is registered inside of LibreOffice. Adjust the joblist
  * inside of the CheckAPI.props to determine which tests will be executed.
  */
 public class CheckAPI  {
