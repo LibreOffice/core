999996e7b5c297db267ca7ee9d9e35a3e25fa72f
Grzegorz Araminowicz <grzegorz.araminowicz@collabora.com>
2019-06-11 08:31:18 +0200

    SmartArt: support ForEach references
    ForEach 'ref' parameter causes specified ForEach node to be used instead.
    Used to create recursive structures like organisation charts.
    
    Change-Id: Iee61b2e103759355b59beb8d3f33eb3cce47c590
    Reviewed-on: https://gerrit.libreoffice.org/74271
    Tested-by: Jenkins
    Reviewed-by: Grzegorz Araminowicz <grzegorz.araminowicz@collabora.com>

--- oox/source/drawingml/diagram/layoutatomvisitors.cxx
+++ oox/source/drawingml/diagram/layoutatomvisitors.cxx
@@ -48,6 +48,13 @@ void ShapeCreationVisitor::visit(AlgAtom& rAtom)
 
 void ShapeCreationVisitor::visit(ForEachAtom& rAtom)
 {
+    if (!rAtom.getRef().isEmpty())
+    {
+        if (LayoutAtomPtr pRefAtom = rAtom.getRefAtom())
+            pRefAtom->accept(*this);
+        return;
+    }
+
     if (rAtom.iterator().mnAxis == XML_followSib)
     {
         // If the axis is the follow sibling, then the last atom should not be
@@ -273,6 +280,13 @@ void ShapeLayoutingVisitor::visit(AlgAtom& rAtom)
 
 void ShapeLayoutingVisitor::visit(ForEachAtom& rAtom)
 {
+    if (!rAtom.getRef().isEmpty())
+    {
+        if (LayoutAtomPtr pRefAtom = rAtom.getRefAtom())
+            pRefAtom->accept(*this);
+        return;
+    }
+
     defaultVisit(rAtom);
 }
 
