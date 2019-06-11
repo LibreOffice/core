LibreOffice/core

commit 705a8c226aee3e68db492083b7cf8b704335328b

WW8PLCFMan::AdjustEnds deeply flawed concept wrt change tracking
The whole idea of clipping the char attributes to before the cr that word uses
as the end of para marker is flawed from especially the perspective of
redlining which is a char property in word.

If the redline encompasses the newline in order to state that it is deleted,
then if the prop is clipped to before that newline then the end-of-para doesn't
get marked as deleted

For now just remove the character attributes clipping from here to be as
conservative as possible.

Hopefully the ordering of processing start pap before start chp and end chp
before end pap and the other million improvements in the parser that came about
after AdjustEnds was created avoids whatever problems were trying to be worked
around here.

Change-Id: I5a72e462db2fff60f52b12c2125ea6ac363de695

sw/source/filter/ww8/ww8scan.cxx
@@ -4235,20 +4235,15 @@ void WW8PLCFMan::AdjustEnds( WW8PLCFxDesc& rDesc )
             nLineEnd = pPap->nEndPos;// nLineEnd zeigt *hinter* das <CR>
             pPap->nEndPos--;        // Absatzende um 1 Zeichen verkuerzen
 
-            // gibt es bereits ein CharAttr-Ende das auf das jetzige
-            // Absatzende zeigt ?  ... dann auch um 1 Zeichen verkuerzen
-            if (pChp->nEndPos == nLineEnd)
-                pChp->nEndPos--;
-
             // gibt es bereits ein Sep-Ende, das auf das jetzige Absatzende
             // zeigt ?  ... dann auch um 1 Zeichen verkuerzen
             if( pSep->nEndPos == nLineEnd )
                 pSep->nEndPos--;
         }
     }
-    else if ( (&rDesc == pChp) || (&rDesc == pSep) )
+    else if (&rDesc == pSep)
     {
-        // Char Adjust oder Sep Adjust Wenn Ende Char-Attr == Absatzende ...
+        // Sep Adjust Wenn Ende Char-Attr == Absatzende ...
         if( (rDesc.nEndPos == nLineEnd) && (rDesc.nEndPos > rDesc.nStartPos) )
             rDesc.nEndPos--;            // ... dann um 1 Zeichen verkuerzen
     }

sw/qa/extras/ww8export/ww8export.cxx
@@ -349,13 +349,20 @@ DECLARE_WW8EXPORT_TEST(testRedlineExport1, "redline-export-1.odt")
     uno::Reference<text::XTextRange> xParagraph = getParagraph(1);
     uno::Reference<container::XEnumerationAccess> xRunEnumAccess(xParagraph, uno::UNO_QUERY);
     uno::Reference<container::XEnumeration> xRunEnum = xRunEnumAccess->createEnumeration();
-    //there must be no redline information on the first line before or after reloading
+    //there must be no redline information on the first line before and after reloading
     while (xRunEnum->hasMoreElements())
     {
         uno::Reference<text::XTextRange> xRun(xRunEnum->nextElement(), uno::UNO_QUERY);
         CPPUNIT_ASSERT_EQUAL(false, hasProperty(xRun, "RedlineType"));
     }
 }
+
+DECLARE_WW8EXPORT_TEST(testRedlineExport2, "redline-export-2.odt")
+{
+    //there must be redline information on the first portion of the third paragraph before and after reloading
+    CPPUNIT_ASSERT_EQUAL(true, hasProperty(getRun(getParagraph(3), 1), "RedlineType"));
+}
+
 
 CPPUNIT_PLUGIN_IMPLEMENT();
 

commit 5d87d09fbdd3c89d364cbfc09c03bb406bb4cb9f

tdf#103967: submit jluth proposal + unittests
See https://bugs.documentfoundation.org/show_bug.cgi?id=103967#c11

Regression from 705a8c226aee3e68db492083b7cf8b704335328b

1. Unittest from 705a8c226aee3e68db492083b7cf8b704335328b still passes
2. Add unittest for tdf#104027
3. Add unittest for tdf#103967
4. Add unittest for tdf#125602
5. Add unittest for tdf#114629

This also fixes tdf#106705 but I don't know how to test it

Change-Id: I0330cbe09233bb7bfbe12af9c93d94d9c2bfa6c1

sw/source/filter/ww8/ww8scan.cxx
@@ -4808,10 +4808,9 @@ void WW8PLCFMan::AdjustEnds( WW8PLCFxDesc& rDesc )
                 m_pSep->nEndPos--;
         }
     }
-    else if (&rDesc == m_pSep)
+    else if ((&rDesc == m_pSep) || (&rDesc == m_pChp))
     {
-        // Sep Adjust if end Char-Attr == paragraph end ...
-        if( (rDesc.nEndPos == m_nLineEnd) && (rDesc.nEndPos > rDesc.nStartPos) )
+        if( (rDesc.nEndPos == m_nLineEnd) && (rDesc.nEndPos > rDesc.nStartPos) && !HasCharSprm(NS_sprm::sprmCDttmRMarkDel).pSprm)
             rDesc.nEndPos--;            // ... then shorten by one character
     }
 }

sw/qa/extras/ww8export/ww8export.cxx
@@ -288,6 +288,32 @@ DECLARE_WW8EXPORT_TEST(testN816603, "n816603.doc")
     CPPUNIT_ASSERT(getPages() > 1);
 }
 
+DECLARE_WW8EXPORT_TEST(testTdf103967, "tdf103967.doc")
+{
+    // Bugdoc was 1 page in Writer and 2 in Word
+    CPPUNIT_ASSERT(getPages() == 2);
+}
+
+DECLARE_WW8EXPORT_TEST(testTdf104027, "tdf104027.doc")
+{
+    // Bugdoc was 2 pages in Writer and 1 in Word
+    CPPUNIT_ASSERT(getPages() == 1);
+}
+
+DECLARE_WW8EXPORT_TEST(testTdf125602, "tdf125602.doc")
+{
+    //Font size is 11, not 22
+    CPPUNIT_ASSERT_EQUAL(11.0f, getProperty<float>(getRun(getParagraph(1), 1), "CharHeight"));
+    CPPUNIT_ASSERT_EQUAL(OUString("Calibri"), getProperty<OUString>(getRun(getParagraph(1), 1), "CharFontName"));
+}
+
+DECLARE_WW8EXPORT_TEST(testTdf114629, "tdf114629.doc")
+{
+    //Font name is Arial, not Wingdings
+    CPPUNIT_ASSERT_EQUAL(10.0f, getProperty<float>(getRun(getParagraph(1), 1), "CharHeight"));
+    CPPUNIT_ASSERT_EQUAL(OUString("Arial"), getProperty<OUString>(getRun(getParagraph(1), 1), "CharFontName"));
+}
+
 DECLARE_WW8EXPORT_TEST(testPageBorder, "page-border.doc")
 {
     // Page border was missing (LineWidth was 0), due to wrong interpretation of pgbApplyTo.