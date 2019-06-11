fc8a35ab89c5043cb8819ef6a8f7e5373d4e625e
Jim MacArthur <jim.macarthur@codethink.co.uk>
2019-06-11 13:08:55 +0100

    AnnotationWin: Fix for GetTopReplyNote
    This allows GetTopReplyNote to work when 'this' is already
    the top reply note in a thread.
    
    Change-Id: Ic4cd5d18bdd022bd22bc97a0b0257639ef3f027d
    Reviewed-on: https://gerrit.libreoffice.org/74450
    Tested-by: Jenkins
    Reviewed-by: Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>

--- sw/source/uibase/docvw/AnnotationWin2.cxx
+++ sw/source/uibase/docvw/AnnotationWin2.cxx
@@ -1425,7 +1425,7 @@ void SwAnnotationWin::SetViewState(ViewState bViewState)
 
 SwAnnotationWin* SwAnnotationWin::GetTopReplyNote()
 {
-    SwAnnotationWin* pTopNote = nullptr;
+    SwAnnotationWin* pTopNote = this;
     SwAnnotationWin* pSidebarWin = IsFollow() ? mrMgr.GetNextPostIt(KEY_PAGEUP, this) : nullptr;
     while (pSidebarWin)
     {
