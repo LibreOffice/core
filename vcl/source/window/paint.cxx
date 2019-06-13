LibreOffice/core

commit e5bb5c52aeaeb60c8592b004efd88c30242e03a1

add RenderContext to Window::PushPaintHelper, use Erase correctly
Change-Id: Icc684a2dcfb4d92752b09768e5a908636910f302

vcl/source/window/paint.cxx
@@ -121,11 +121,12 @@ void PaintHelper::DoPaint(const vcl::Region* pRegion)
     if (!pWindowImpl->maInvalidateRegion.IsEmpty())
     {
         m_pWindow->BeginPaint();
-        m_pWindow->PushPaintHelper(this);
 
         // double-buffering - so far an experimental feature
         if (officecfg::Office::Common::Misc::ExperimentalMode::get())
         {
+            m_pWindow->PushPaintHelper(this, *m_pWindow);
+
             ScopedVclPtrInstance<VirtualDevice> pDevice;
 
             // transfer various settings
@@ -186,6 +187,8 @@ void PaintHelper::DoPaint(const vcl::Region* pRegion)
         else
         {
             // direct painting
+            m_pWindow->PushPaintHelper(this, *m_pWindow);
+            m_pWindow->ApplySettings(*m_pWindow);
             m_pWindow->Paint(*m_pWindow, m_aPaintRect);
         }
 
@@ -348,7 +351,7 @@ void RenderTools::DrawSelectionBackground(vcl::RenderContext& rRenderContext, vc
     rRenderContext.Pop(); // LINECOLOR | FILLCOLOR
 }
 
-void Window::PushPaintHelper(PaintHelper *pHelper)
+void Window::PushPaintHelper(PaintHelper *pHelper, vcl::RenderContext& rRenderContext)
 {
     pHelper->SetPop();
 
@@ -361,49 +364,48 @@ void Window::PushPaintHelper(PaintHelper *pHelper)
     // restore Paint-Region
     vcl::Region &rPaintRegion = pHelper->GetPaintRegion();
     rPaintRegion = mpWindowImpl->maInvalidateRegion;
-    Rectangle   aPaintRect = rPaintRegion.GetBoundRect();
+    Rectangle aPaintRect = rPaintRegion.GetBoundRect();
 
     // - RTL - re-mirror paint rect and region at this window
-    if( ImplIsAntiparallel() )
+    if (ImplIsAntiparallel())
     {
-        const OutputDevice *pOutDev = GetOutDev();
-        pOutDev->ReMirror( aPaintRect );
-        pOutDev->ReMirror( rPaintRegion );
+        rRenderContext.ReMirror(aPaintRect);
+        rRenderContext.ReMirror(rPaintRegion);
     }
-    aPaintRect = ImplDevicePixelToLogic( aPaintRect);
+    aPaintRect = ImplDevicePixelToLogic(aPaintRect);
     mpWindowImpl->mpPaintRegion = &rPaintRegion;
     mpWindowImpl->maInvalidateRegion.SetEmpty();
 
-    if ( (pHelper->GetPaintFlags() & IMPL_PAINT_ERASE) && IsBackground() )
+    if ((pHelper->GetPaintFlags() & IMPL_PAINT_ERASE) && rRenderContext.IsBackground())
     {
-        if ( IsClipRegion() )
+        if (rRenderContext.IsClipRegion())
         {
-            vcl::Region aOldRegion = GetClipRegion();
-            SetClipRegion();
-            Erase();
-            SetClipRegion( aOldRegion );
+            vcl::Region aOldRegion = rRenderContext.GetClipRegion();
+            rRenderContext.SetClipRegion();
+            Erase(rRenderContext);
+            rRenderContext.SetClipRegion(aOldRegion);
         }
         else
-            Erase();
+            Erase(rRenderContext);
     }
 
     // #98943# trigger drawing of toolbox selection after all childern are painted
-    if( mpWindowImpl->mbDrawSelectionBackground )
+    if (mpWindowImpl->mbDrawSelectionBackground)
         pHelper->SetSelectionRect(aPaintRect);
     pHelper->SetPaintRect(aPaintRect);
 }
 
 void Window::PopPaintHelper(PaintHelper *pHelper)
 {
-    if ( mpWindowImpl->mpWinData )
+    if (mpWindowImpl->mpWinData)
     {
-        if ( mpWindowImpl->mbFocusVisible )
-            ImplInvertFocus( *(mpWindowImpl->mpWinData->mpFocusRect) );
+        if (mpWindowImpl->mbFocusVisible)
+            ImplInvertFocus(*(mpWindowImpl->mpWinData->mpFocusRect));
     }
     mpWindowImpl->mbInPaint = false;
     mbInitClipRegion = true;
     mpWindowImpl->mpPaintRegion = NULL;
-    if ( mpWindowImpl->mpCursor )
+    if (mpWindowImpl->mpCursor)
         mpWindowImpl->mpCursor->ImplResume(pHelper->GetRestoreCursor());
 }
 
@@ -906,7 +908,6 @@ void Window::Paint(vcl::RenderContext& /*rRenderContext*/, const Rectangle& rRec
 
 void Window::SetPaintTransparent( bool bTransparent )
 {
-
     // transparency is not useful for frames as the background would have to be provided by a different frame
     if( bTransparent && mpWindowImpl->mbFrame )
         return;
@@ -1305,7 +1306,7 @@ void Window::ImplPaintToDevice( OutputDevice* i_pTargetOutDev, const Point& i_rP
 
     // background
     if( ! IsPaintTransparent() && IsBackground() && ! (GetParentClipMode() & PARENTCLIPMODE_NOCLIP ) )
-        Erase();
+        Erase(*this);
     // foreground
     Paint(*this, aPaintRect);
     // put a pop action to metafile

include/vcl/window.hxx
@@ -544,8 +544,8 @@ protected:
     SAL_DLLPRIVATE WindowResHeader      ImplLoadResHeader( const ResId& rResId );
     SAL_DLLPRIVATE void                 ImplLoadRes( const ResId& rResId );
 
-    SAL_DLLPRIVATE void                 PushPaintHelper(PaintHelper *pHelper);
-    SAL_DLLPRIVATE void                 PopPaintHelper(PaintHelper *pHelper);
+    SAL_DLLPRIVATE void                 PushPaintHelper(PaintHelper* pHelper, vcl::RenderContext& rRenderContext);
+    SAL_DLLPRIVATE void                 PopPaintHelper(PaintHelper* pHelper);
 
 private:
 

commit 9c9ec6ca56a8ebefcaf792e257217343f255cb13

tdf#92079 vcl: fix missing image background on dialog from basic
Regression from commit e5bb5c52aeaeb60c8592b004efd88c30242e03a1 (add
RenderContext to Window::PushPaintHelper, use Erase correctly,
2015-05-18), the problem was the unconditional ApplySettings() call in
the direct painting case.

It's not clear what was the intent of that addition, since
vcl::Window::ApplySettings() is virtual, and the default implementation
is empty. But at least the Dialog subclass sets the background there,
leading to the loss of the original bitmap background.

Fix the problem by explicitly restoring the old background after
ApplySettings() at least in the bitmap case. (Wallpaper is
copy-on-write, so no problem to make a copy before ApplySettings()
unconditionally.)

Change-Id: I89768a1331f9dc1ea4508336723484d112156338

vcl/source/window/paint.cxx
@@ -296,7 +296,10 @@ void PaintHelper::DoPaint(const vcl::Region* pRegion)
         else
         {
             // direct painting
+            Wallpaper aBackground = m_pWindow->GetBackground();
             m_pWindow->ApplySettings(*m_pWindow);
+            if (aBackground.IsBitmap())
+                m_pWindow->SetBackground(aBackground);
             m_pWindow->PushPaintHelper(this, *m_pWindow);
             m_pWindow->Paint(*m_pWindow, m_aPaintRect);
         }