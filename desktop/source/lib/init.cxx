5e969f13b1f5483e163729e7d3185f7e8552a341
Jan Holesovsky <kendy@collabora.com>
2019-06-14 18:38:51 +0200

    android: Unregistering in runLoop() is important even on Android.
    Otherwise we get crashes in online Kit when attempting to shutdown
    reasonably cleanly and the poll kicks in.
    
    Change-Id: I9a1744b42fec9f8ffddc6094773bda943caedf2a
    Reviewed-on: https://gerrit.libreoffice.org/74064
    Tested-by: Jenkins
    Reviewed-by: Jan Holesovsky <kendy@collabora.com>

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -5172,7 +5172,7 @@ static void lo_runLoop(LibreOfficeKit* /*pThis*/,
                        LibreOfficeKitWakeCallback pWakeCallback,
                        void* pData)
 {
-#ifdef IOS // Maybe ANDROID, too?
+#if defined(IOS) || defined(ANDROID)
     Application::GetSolarMutex().acquire();
 #endif
 
@@ -5183,7 +5183,7 @@ static void lo_runLoop(LibreOfficeKit* /*pThis*/,
         Application::UpdateMainThread();
         soffice_main();
     }
-#ifdef IOS // ANDROID, too?
+#if defined(IOS) || defined(ANDROID)
     vcl::lok::unregisterPollCallbacks();
     Application::ReleaseSolarMutex();
 #endif
