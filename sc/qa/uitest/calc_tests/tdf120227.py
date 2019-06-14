712c47e3f55529666b9c671fdc7641aa758e58a9
Artur Neumann <artur@jankaritech.com>
2019-06-14 14:49:23 +0545

    UI test for tdf#120227
    test to check if Format > Page does crash Calc
    
    Change-Id: Ia019ac1a1f4dabbf88754a35e881c936f85d2df7

---
+++ sc/qa/uitest/calc_tests/tdf120227.py
@@ -0,0 +1,19 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+from uitest.framework import UITestCase
+
+class tdf120227(UITestCase):
+    def test_column_width(self):
+        calc_doc = self.ui_test.create_doc_in_start_center("calc")
+        xCalcDoc = self.xUITest.getTopFocusWindow()
+        gridwin = xCalcDoc.getChild("grid_window")
+        document = self.ui_test.get_component()
+        self.xUITest.executeCommand(".uno:PageFormatDialog")
+
+        self.ui_test.close_doc()
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
