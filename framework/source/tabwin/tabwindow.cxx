b9c00b3c5d90b80248577403ce56c8668a51b315
Andrea Gelmini <andrea.gelmini@gelma.net>
2019-06-14 22:42:49 +0000

    Fix typo
    Change-Id: I7e6be2abe50487df7da5486ad9b98aabc67b1caa
    Reviewed-on: https://gerrit.libreoffice.org/74592
    Tested-by: Jenkins
    Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

--- framework/source/tabwin/tabwindow.cxx
+++ framework/source/tabwin/tabwindow.cxx
@@ -835,7 +835,7 @@ css::uno::Reference< css::beans::XPropertySetInfo > SAL_CALL TabWindow::getPrope
 const css::uno::Sequence< css::beans::Property > TabWindow::impl_getStaticPropertyDescriptor()
 {
     // Create property array to initialize sequence!
-    // Table of all predefined properties of this class. Its used from OPropertySetHelper-class!
+    // Table of all predefined properties of this class. It's used from OPropertySetHelper-class!
     // Don't forget to change the defines (see begin of this file), if you add, change or delete a property in this list!!!
     // It's necessary for methods of OPropertySetHelper.
     // ATTENTION:
