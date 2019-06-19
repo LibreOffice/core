33895de82b9031d7f501fe1eaa71864cc979c694
Artur Neumann <artur@jankaritech.com>
2019-06-19 12:15:08 +0545

    UI tests opening all dialogs in calc and see if it crashes
    Open a dialog, close it by the given button e.g. "cancel" or "close".
    If the dialog has an enabled "OK" button open the dialog again
    and try to close it also using the OK button.
    For every dialog a complete new document is opened to ensure test-separation,
    otherwise one action could hamper the next action.
    E.g. ".uno:Protect" would hamper any edit action.
    
    For dialogs that already have other tests a "cancel/close" test was added to
    the previous test. As far as possible opening new documents was avoided in those
    cases. And in some cases a simple check was added to check if the "cancel" button
    does not change anything.
    
    Change-Id: I727cad43603770245f27baf752a1985f9a754847

--- sc/qa/uitest/pageFormat/tdf123508.py
+++ sc/qa/uitest/pageFormat/tdf123508.py
@@ -57,8 +57,8 @@ class tdf123508(UITestCase):
     self.assertEqual(get_state_as_dict(spinEDSCALEPAGEWIDTH)["Text"], "2")
     self.assertEqual(get_state_as_dict(spinEDSCALEPAGEHEIGHT)["Text"], "2")
 
-    xOKBtn = xDialog.getChild("ok")
-    self.ui_test.close_dialog_through_button(xOKBtn)
+    xCancelBtn = xDialog.getChild("cancel")
+    self.ui_test.close_dialog_through_button(xCancelBtn)
 
     self.ui_test.close_doc()
 # vim: set shiftwidth=4 softtabstop=4 expandtab:
--- sc/qa/uitest/range_name/tdf119954.py
+++ sc/qa/uitest/range_name/tdf119954.py
@@ -78,5 +78,11 @@ class tdf119954(UITestCase):
         self.assertEqual(get_cell_by_position(document, 0, 1, 2).getFormula(), "")
         self.assertEqual(get_cell_by_position(document, 0, 1, 1).getFormula(), "")
 
+        # check cancel button
+        self.ui_test.execute_modeless_dialog_through_command(".uno:DefineDBName")
+        xDefineNameDlg = self.xUITest.getTopFocusWindow()
+        xCancelBtn = xDefineNameDlg.getChild("cancel")
+        self.ui_test.close_dialog_through_button(xCancelBtn)
+
         self.ui_test.close_doc()
 # vim: set shiftwidth=4 softtabstop=4 expandtab:
