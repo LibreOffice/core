e969f039d1988c7bc908386dbcbd250410bbd3f8
Miklos Vajna <vmiklos@collabora.com>
2019-06-20 16:58:06 +0200

    sw comments: allow ranges to be created which cover only the placeholder char
    First, the old condition was a bit tricky: it wanted to avoid the case
    when only the placeholder character is covered, but given that start and
    end can be the same for collapsed ranges, this was true for comments
    without a range, too. In practice what remains blacklisted is just the
    case when the PaM has a mark, but it's the same as the point.
    
    The change has two motivations:
    
    1) ODF can have '<office:annotation/>...<office:annotation-end/>', where
    '...' may be content which has no document position itself in the Writer
    implementation, like at-char anchored images. This change avoids loosing
    the range of such annotations during ODF roundtrip.
    
    2) This starts adding support for comments on objects which don't have
    an own document position, though UI for that is still missing.
    
    (cherry picked from commit 34c008380b9c17a78b9fde8cfa2102ab14c2392b)
    
    Conflicts:
    	sw/qa/extras/unowriter/unowriter.cxx
    
    Change-Id: If151b8e00e37e07830c0582b8f0920a91a787363
    Reviewed-on: https://gerrit.libreoffice.org/75035
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
    Tested-by: Miklos Vajna <vmiklos@collabora.com>

--- sw/qa/extras/unowriter/unowriter.cxx
+++ sw/qa/extras/unowriter/unowriter.cxx
@@ -123,6 +123,22 @@ DECLARE_UNOAPI_TEST_FILE(testRenderablePagePosition, "renderable-page-position.o
     CPPUNIT_ASSERT_GREATER(aPosition1.Y, aPosition2.Y);
 }
 
+DECLARE_UNOAPI_TEST(testImageCommentAtChar)
+{
+    // Load a document with an at-char image in it (and a comment on the image).
+    load(mpTestDocumentPath, "image-comment-at-char.odt");
+    SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument*>(mxComponent.get());
+    CPPUNIT_ASSERT(pTextDoc);
+    SwDoc* pDoc = pTextDoc->GetDocShell()->GetDoc();
+
+    // Verify that we have an annotation mark (comment with a text range) in the document.
+    // Without the accompanying fix in place, this test would have failed, as comments lost their
+    // ranges on load when their range only covered the placeholder character of the comment (which
+    // is also the anchor position of the image).
+    IDocumentMarkAccess* pMarks = pDoc->getIDocumentMarkAccess();
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(1), pMarks->getAnnotationMarksCount());
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
