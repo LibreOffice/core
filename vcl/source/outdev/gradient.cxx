LibreOffice/core

commit 710f3c63af2d8bee7d5e860614145799a3f0e491

tdf#125670 just check gradient clipping on drawing
Otherwise the gradient won't be commited to the Metafile, as
mbOutputClipped will be true, because the output device will
have just one pixel size.

Change-Id: I73084eb715ee6313f6478eded24feb9abfc411f3
Reviewed-on: https://gerrit.libreoffice.org/73423
Tested-by: Jenkins
Reviewed-by: Caolán McNamara <caolanm@redhat.com>
Tested-by: Caolán McNamara <caolanm@redhat.com>

vcl/source/outdev/gradient.cxx
@@ -50,12 +50,6 @@ void OutputDevice::DrawGradient( const tools::PolyPolygon& rPolyPoly,
 {
     assert(!is_double_buffered_window());
 
-    if ( mbInitClipRegion )
-        InitClipRegion();
-
-    if ( mbOutputClipped )
-        return;
-
     if ( rPolyPoly.Count() && rPolyPoly[ 0 ].GetSize() )
     {
         if ( mnDrawMode & ( DrawModeFlags::BlackGradient | DrawModeFlags::WhiteGradient | DrawModeFlags::SettingsGradient) )
@@ -106,6 +100,9 @@ void OutputDevice::DrawGradient( const tools::PolyPolygon& rPolyPoly,
 
                 if( mbInitClipRegion )
                     InitClipRegion();
+
+                if ( mbOutputClipped )
+                    return;
 
                 // try to draw gradient natively
                 bDrawn = mpGraphics->DrawGradient( aClixPolyPoly, aGradient );

commit f389d01bf51fe7df0619fcedc16ac81fa2fee58b

tdf#125777 restore old RenderContext before return
Regression from commit 710f3c63af2d ("tdf#125670 just check gradient
clipping on drawing")

Change-Id: I6acf23675414983bd07639703f43aa934c0a48ed

vcl/source/outdev/gradient.cxx
@@ -101,8 +101,11 @@ void OutputDevice::DrawGradient( const tools::PolyPolygon& rPolyPoly,
                 if( mbInitClipRegion )
                     InitClipRegion();
 
-                if ( mbOutputClipped )
+                if (mbOutputClipped)
+                {
+                    Pop();
                     return;
+                }
 
                 // try to draw gradient natively
                 bDrawn = mpGraphics->DrawGradient( aClixPolyPoly, aGradient );