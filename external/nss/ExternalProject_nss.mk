e08493d9cd40e7c6f1e78f474bc8948d99eb43dd
Jan-Marek Glogowski <glogow@fbihome.de>
2019-06-26 18:09:19 +0200

    Fix NSS parallel build; new attempt
    This is just a minimal solution and obviously not fully parallel.
    We still have to walk the tree twice:
    1. copy all needed headers to dist (export export_private)
    2. the parallel build (libs)
    
    The changes are:
    * Get rid of all MAKE_OBJDIR calls and replace them with an
      order-only-prerequisites on OBJDIR. While at it rename it to
      MAKE_DIR. The calls sites must be hidden as @$(MAKE_DIR),
      because of the strange way the shared libraries are build.
    * Serialize steps for the nss_build_all and all targets
      => the export target needs nsinstall, so we have to build that
      => just export all headers in advance for the build
      => no real way to track dependencies, so just list them
    * Make $(SHARED_LIBRARY) depend on $(LIBRARY), otherwise dll lib
      creation on Windows builds a static lib, instead of the dll one
    
    Change-Id: If327317f623bd94e19e0bc04990ccfdc95c48971

--- external/nss/ExternalProject_nss.mk
+++ external/nss/ExternalProject_nss.mk
@@ -16,14 +16,16 @@ $(eval $(call gb_ExternalProject_register_targets,nss,\
 ))
 
 ifeq ($(OS),WNT)
+$(call gb_ExternalProject_get_state_target,nss,build): CROSS_CC_DIR := $(subst /cl.exe,,$(subst /x64/,/x86/,$(CXX_X64_BINARY)))
 $(call gb_ExternalProject_get_state_target,nss,build): $(call gb_ExternalExecutable_get_dependencies,python)
-	$(call gb_ExternalProject_run,build,\
+	$(if $(filter X86_64,$(CPUNAME)),USE_64=1,$(if $(CXX_X64_BINARY), \
+		export PATH="$(CROSS_CC_DIR);$(subst ;;,,$(subst $(CROSS_CC_DIR),,$(subst HostX86,HostX64,$(PATH))))")); \
+	$(call gb_ExternalProject_run,build, \
 		$(if $(MSVC_USE_DEBUG_RUNTIME),USE_DEBUG_RTL=1,BUILD_OPT=1) \
 		MOZ_MSVCVERSION=9 OS_TARGET=WIN95 \
-		$(if $(filter X86_64,$(CPUNAME)),USE_64=1) \
 		LIB="$(ILIB)" \
 		XCFLAGS="-arch:SSE $(SOLARINC)" \
-		$(MAKE) -j1 nss_build_all RC="rc.exe $(SOLARINC)" \
+		$(MAKE) nss_build_all RC="rc.exe $(SOLARINC)" \
 			NSINSTALL='$(call gb_ExternalExecutable_get_command,python) $(SRCDIR)/external/nss/nsinstall.py' \
 			NSS_DISABLE_GTESTS=1 \
 	,nss)
@@ -46,7 +48,7 @@ $(call gb_ExternalProject_get_state_target,nss,build): $(call gb_ExternalExecuta
 			$(if $(filter iOS-ARM,$(OS)-$(CPUNAME)),CPU_ARCH=arm) \
 			NSPR_CONFIGURE_OPTS="--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM)") \
 		NSDISTMODE=copy \
-		$(MAKE) -j1 AR="$(AR)" \
+		$(MAKE) AR="$(AR)" \
 			RANLIB="$(RANLIB)" \
 			NMEDIT="$(NM)edit" \
 			COMMA=$(COMMA) \
