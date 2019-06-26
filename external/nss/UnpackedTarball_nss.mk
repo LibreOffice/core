ae7c0190b68ad8854d9cec9596774d3ca30f978e
Jan-Marek Glogowski <glogow@fbihome.de>
2019-06-26 18:09:19 +0200

    Allow a much more parallel NSS build
    This solution is almost fully parallel. There are still three not-
    fixed / not fixable problems, which just have a minimal impact:
    
    1. the global nsinstall dependency
    2. the header export for the build
    3. the child -> parent directory dependencies inside lib/ckfw
    
    All of them are handled in the prepare_build target.
    
    Please look into the individual patches for more information.
    
    Change-Id: If327317f623bd94e19e0bc04990ccfdc95c48971

--- external/nss/UnpackedTarball_nss.mk
+++ external/nss/UnpackedTarball_nss.mk
@@ -12,34 +12,42 @@ $(eval $(call gb_UnpackedTarball_UnpackedTarball,nss))
 $(eval $(call gb_UnpackedTarball_set_tarball,nss,$(NSS_TARBALL)))
 
 $(eval $(call gb_UnpackedTarball_add_patches,nss,\
-	external/nss/nss.patch \
-	external/nss/nss.aix.patch \
-	external/nss/nss-3.13.5-zlib-werror.patch \
-	external/nss/nss_macosx.patch \
-	external/nss/nss-win32-make.patch.1 \
-	$(if $(filter WNT,$(OS)),external/nss/nss.windows.patch \
-        external/nss/nss.nowerror.patch \
-		external/nss/nss.vs2015.patch) \
+    external/nss/nss.patch \
+    external/nss/nss.aix.patch \
+    external/nss/nss-3.13.5-zlib-werror.patch \
+    external/nss/nss_macosx.patch \
+    external/nss/nss-win32-make.patch.1 \
     external/nss/ubsan.patch.0 \
     external/nss/clang-cl.patch.0 \
+    external/nss/nss.vs2015.patch \
+    external/nss/nss.vs2015.pdb.patch \
+    external/nss/nss.bzmozilla1238154.patch \
+    external/nss/macos-dlopen.patch.0 \
+    external/nss/0001-Introduce-SHARED_LIBRARY_NAME.patch.1 \
+    external/nss/0002-Handle-parallel-PROGRAM-and-PROGRAMS.patch.1 \
+    external/nss/0003-Drop-Windows-XP-compatibility.patch.1 \
+    external/nss/0004-Add-dist-install-to-generating-targets.patch.1 \
+    external/nss/0005-Don-t-delete-directories.patch.1 \
+    external/nss/0006-Parallelize-part-of-the-NSS-build.patch.1 \
+    external/nss/0007-Drop-recursive-private_exports.patch.1 \
+    external/nss/0008-Use-eval-templates-for-export-and-freebl.patch.1 \
+    external/nss/0009-Drop-double-colon-usage-and-add-directory-depends.patch.1 \
     $(if $(filter iOS,$(OS)), \
         external/nss/nss-ios.patch) \
-	$(if $(filter MSC-INTEL,$(COM)-$(CPUNAME)), \
-		external/nss/nss.cygwin64.in32bit.patch) \
-	$(if $(filter WNT,$(OS)), \
-		external/nss/nss.vs2015.pdb.patch) \
-	$(if $(filter WNT,$(OS)), \
-    	external/nss/nss.utf8bom.patch.1) \
-	$(if $(filter ANDROID,$(OS)), \
-		external/nss/nss-android.patch.1) \
-	external/nss/nss.bzmozilla1238154.patch \
-    external/nss/macos-dlopen.patch.0 \
+    $(if $(filter ANDROID,$(OS)), \
+        external/nss/nss-android.patch.1) \
+    $(if $(filter MSC-INTEL,$(COM)-$(CPUNAME)), \
+        external/nss/nss.cygwin64.in32bit.patch) \
+    $(if $(filter WNT,$(OS)), \
+        external/nss/nss.windows.patch \
+        external/nss/nss.nowerror.patch \
+        external/nss/nss.utf8bom.patch.1) \
 ))
 
 ifeq ($(COM_IS_CLANG),TRUE)
 ifneq ($(filter -fsanitize=%,$(CC)),)
 $(eval $(call gb_UnpackedTarball_add_patches,nss,\
-	external/nss/asan.patch.1 \
+    external/nss/asan.patch.1 \
 ))
 endif
 endif
