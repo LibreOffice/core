LibreOffice/core

commit 56b2cf0c10d9caa01ebae1d80465e342d046a85c

sw-collapse-empty-table-par-like-html.diff:
n#376690

sw/source/core/text/porrst.cxx
@@ -58,6 +58,8 @@
 #include <IDocumentSettingAccess.hxx>
 #include <IDocumentDeviceAccess.hxx>
 
+#include <crsrsh.hxx>
+
 /*************************************************************************
  *                      class SwTmpEndPortion
  *************************************************************************/
@@ -228,6 +230,20 @@ SwLinePortion *SwArrowPortion::Compress() { return this; }
 
 SwTwips SwTxtFrm::EmptyHeight() const
 {
+    if (IsCollapse()) {
+        ViewShell *pSh = GetShell();
+        if ( pSh->IsA( TYPE(SwCrsrShell) ) ) {
+            SwCrsrShell *pCrSh=(SwCrsrShell*)pSh;
+            SwCntntFrm *pCurrFrm=pCrSh->GetCurrFrm();
+            if (pCurrFrm==(SwCntntFrm*)this) {
+                // do nothing
+            } else {
+                return 1;
+            }
+        } else {
+            return 1;
+        }
+    }
     ASSERT( ! IsVertical() || ! IsSwapped(),"SwTxtFrm::EmptyHeight with swapped frame" );
 
     SwFont *pFnt;

sw/inc/IDocumentSettingAccess.hxx
@@ -82,6 +82,7 @@ namespace com { namespace sun { namespace star { namespace i18n { struct Forbidd
          TAB_AT_LEFT_INDENT_FOR_PARA_IN_LIST,
          // <--
      INVERT_BORDER_SPACING,
+         COLLAPSE_EMPTY_CELL_PARA,
          // COMPATIBILITY FLAGS END
 
          BROWSE_MODE,

sw/inc/doc.hxx
@@ -604,6 +604,7 @@ private:
     bool mbTabRelativeToIndent                      : 1;   // #i24363# tab stops relative to indent
     bool mbProtectForm                              : 1;
     bool mbInvertBorderSpacing                      : 1;
+    bool mbCollapseEmptyCellPara                    : 1;
     bool mbTabAtLeftIndentForParagraphsInList;             // OD 2008-06-05 #i89181# - see above
 
     // #i78591#

sw/inc/ndtxt.hxx
@@ -856,6 +856,8 @@ public:
     virtual ::com::sun::star::uno::Reference<
         ::com::sun::star::rdf::XMetadatable > MakeUnoObject();
 
+    bool IsCollapse() const;
+
     DECL_FIXEDMEMPOOL_NEWDEL(SwTxtNode)
 };
 

sw/source/core/crsr/callnk.cxx
@@ -43,9 +43,14 @@
 #include <doc.hxx>
 #include <frmfmt.hxx>
 #include <txtfrm.hxx>
+#include <tabfrm.hxx>
+#include <rowfrm.hxx>
+#include <fmtfsize.hxx>
 #include <ndtxt.hxx>
 #include <flyfrm.hxx>
 #include <breakit.hxx>
+
+#include<vcl/window.hxx>
 
 
 SwCallLink::SwCallLink( SwCrsrShell & rSh, ULONG nAktNode, xub_StrLen nAktCntnt,
@@ -96,6 +101,51 @@ SwCallLink::~SwCallLink()
     SwCntntNode * pCNd = pCurCrsr->GetCntntNode();
     if( !pCNd )
         return;
+
+    bool bUpdatedTable = false;
+    SwFrm *myFrm=pCNd->GetFrm();
+    if (myFrm!=NULL)
+    {
+        // We need to emulated a change of the row height in order
+        // to have the complete row redrawn
+        SwRowFrm* pRow = myFrm->FindRowFrm( );
+        if ( pRow )
+        {
+            const SwTableLine* pLine = pRow->GetTabLine( );
+            SwFmtFrmSize pSize = pLine->GetFrmFmt( )->GetFrmSize( );
+            pRow->Modify( NULL, &pSize );
+
+            bUpdatedTable = true;
+        }
+    }
+
+    const SwDoc *pDoc=rShell.GetDoc();
+    const SwCntntNode *pNode = NULL;
+    if ( ( pDoc != NULL && nNode < pDoc->GetNodes( ).Count( ) ) )
+    {
+        pNode = pDoc->GetNodes()[nNode]->GetCntntNode();
+    }
+    if ( pNode != NULL )
+    {
+        SwFrm *myFrm2=pNode->GetFrm();
+        if (myFrm2!=NULL)
+        {
+            // We need to emulated a change of the row height in order
+            // to have the complete row redrawn
+            SwRowFrm* pRow = myFrm2->FindRowFrm();
+            if ( pRow )
+            {
+                const SwTableLine* pLine = pRow->GetTabLine( );
+                SwFmtFrmSize pSize = pLine->GetFrmFmt( )->GetFrmSize( );
+                pRow->Modify( NULL, &pSize );
+
+                bUpdatedTable = true;
+            }
+        }
+    }
+
+    if ( bUpdatedTable )
+        rShell.GetWin( )->Invalidate( 0 );
 
     xub_StrLen nCmp, nAktCntnt = pCurCrsr->GetPoint()->nContent.GetIndex();
     USHORT nNdWhich = pCNd->GetNodeType();

sw/source/core/doc/doc.cxx
@@ -198,6 +198,7 @@ bool SwDoc::get(/*[in]*/ DocumentSettingId id) const
         case TAB_AT_LEFT_INDENT_FOR_PARA_IN_LIST: return mbTabAtLeftIndentForParagraphsInList;
         // <--
     case INVERT_BORDER_SPACING: return mbInvertBorderSpacing;
+        case COLLAPSE_EMPTY_CELL_PARA: return mbCollapseEmptyCellPara;
          // COMPATIBILITY FLAGS END
 
         case BROWSE_MODE: return mbBrowseMode;
@@ -324,6 +325,9 @@ void SwDoc::set(/*[in]*/ DocumentSettingId id, /*[in]*/ bool value)
     case INVERT_BORDER_SPACING:
         mbInvertBorderSpacing = value;
     break;
+        case COLLAPSE_EMPTY_CELL_PARA:
+            mbCollapseEmptyCellPara = value;
+         break;
          // COMPATIBILITY FLAGS END
 
         case BROWSE_MODE:

sw/source/core/doc/docnew.cxx
@@ -384,6 +384,7 @@ SwDoc::SwDoc() :
     mbTabAtLeftIndentForParagraphsInList    = false;        // hidden
     // <--
     mbInvertBorderSpacing                   = false;        // hidden
+    mbCollapseEmptyCellPara                 = true;        // hidden
 
     //
     // COMPATIBILITY FLAGS END

sw/source/core/inc/frame.hxx
@@ -624,6 +624,7 @@ public:
     SwRootFrm           *FindRootFrm();
     SwPageFrm           *FindPageFrm();
     SwFrm               *FindColFrm();
+    SwRowFrm            *FindRowFrm();
     SwFtnBossFrm        *FindFtnBossFrm( BOOL bFootnotes = FALSE );
     SwTabFrm            *ImplFindTabFrm();
     SwFtnFrm            *ImplFindFtnFrm();
@@ -910,6 +911,9 @@ public:
 
     // FME 2007-08-30 #i81146# new loop control
     void ValidateThisAndAllLowers( const USHORT nStage );
+
+public:
+    bool IsCollapse() const;
 };
 
 inline BOOL SwFrm::IsInDocBody() const

commit 30578a02ed1a05691990c3e15ffbe6b796dc1af5

tdf#125751 sw: don't start recursive layout in SwTextFrame::EmptyHeight()
Not sure if this has anything to do with the bug, but i noticed it while
debugging...

This is the only place that calls GetCurrFrame() *during* formatting,
so make sure we don't do stupid recursive things.

(regression from 56b2cf0c10d9caa01ebae1d80465e342d046a85c)

Change-Id: Icd05f8d634bc9de4d5d16824d30075397e6c9960

sw/source/core/text/porrst.cxx
@@ -210,7 +210,8 @@ SwTwips SwTextFrame::EmptyHeight() const
         SwViewShell *pSh = getRootFrame()->GetCurrShell();
         if ( dynamic_cast<const SwCursorShell*>( pSh ) !=  nullptr ) {
             SwCursorShell *pCrSh = static_cast<SwCursorShell*>(pSh);
-            SwContentFrame *pCurrFrame=pCrSh->GetCurrFrame();
+            // this is called during formatting so avoid recursive layout
+            SwContentFrame const*const pCurrFrame = pCrSh->GetCurrFrame(false);
             if (pCurrFrame==static_cast<SwContentFrame const *>(this)) {
                 // do nothing
             } else {