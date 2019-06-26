35eecde7a118a27781b6f801d1f97a0bcf00ee01
Noel Grandin <noel.grandin@collabora.co.uk>
2019-06-26 15:19:36 +0200

    tdf#125706 Fields slow down down load, part1
    The RDF stuff is sloooooooow, so (a) add some caching and (b) fold a
    very hot UNO_QUERY call.
    
    To add the caching we need to add a new UNO interface, since
    XEnumeration is not amenable to being cached.
    
    This takes the time from 7s to 5s for me.
    
    Change-Id: I207387e975b4f107834edd0974134c481fb4012d

--- unoxml/source/rdf/librdf_repository.cxx
+++ unoxml/source/rdf/librdf_repository.cxx
@@ -51,6 +51,7 @@
 #include <com/sun/star/rdf/BlankNode.hpp>
 #include <com/sun/star/rdf/URI.hpp>
 #include <com/sun/star/rdf/Literal.hpp>
+#include <com/sun/star/rdf/XNamedGraph2.hpp>
 
 #include <rtl/ref.hxx>
 #include <rtl/strbuf.hxx>
@@ -646,7 +647,7 @@ librdf_QuerySelectResult::getBindingNames()
  */
 class librdf_NamedGraph:
     public ::cppu::WeakImplHelper<
-        rdf::XNamedGraph>
+        rdf::XNamedGraph2>
 {
 public:
     librdf_NamedGraph(librdf_Repository * i_pRep,
@@ -678,6 +679,11 @@ public:
             const uno::Reference< rdf::XResource > & i_xSubject,
             const uno::Reference< rdf::XURI > & i_xPredicate,
             const uno::Reference< rdf::XNode > & i_xObject) override;
+    // css::rdf::XNamedGraph2
+    virtual uno::Sequence<uno::Reference< rdf::XURI >> SAL_CALL getStatementsObjects(
+            const uno::Reference< rdf::XResource > & i_xSubject,
+            const uno::Reference< rdf::XURI > & i_xPredicate,
+            const uno::Reference< rdf::XNode > & i_xObject) override;
 
 private:
 
@@ -688,6 +694,23 @@ private:
     uno::WeakReference< rdf::XRepository > const m_wRep;
     librdf_Repository *const m_pRep;
     uno::Reference< rdf::XURI > const m_xName;
+
+    struct CacheKey
+    {
+        uno::Reference< rdf::XResource > xSubject;
+        uno::Reference< rdf::XURI > xPredicate;
+        uno::Reference< rdf::XNode > xObject;
+
+        bool operator<(const CacheKey& other) const
+        {
+            return xSubject.get() < other.xSubject.get()
+                && xPredicate.get() < other.xPredicate.get()
+                && xObject.get() < other.xObject.get();
+        }
+    };
+    // Querying is rather slow, so cache the results.
+    // But cache them weakly, because they could be large objects like whole documents.
+    std::map<CacheKey, std::vector<uno::WeakReference<rdf::XURI>>> m_aStatementsCache;
 };
 
 
@@ -729,6 +752,7 @@ void SAL_CALL librdf_NamedGraph::clear()
         throw lang::WrappedTargetRuntimeException( ex.Message,
                         *this, anyEx );
     }
+    m_aStatementsCache.clear();
 }
 
 void SAL_CALL librdf_NamedGraph::addStatement(
@@ -743,6 +767,7 @@ void SAL_CALL librdf_NamedGraph::addStatement(
     }
     m_pRep->addStatementGraph_NoLock(
             i_xSubject, i_xPredicate, i_xObject, m_xName);
+    m_aStatementsCache.clear();
 }
 
 void SAL_CALL librdf_NamedGraph::removeStatements(
@@ -757,6 +782,7 @@ void SAL_CALL librdf_NamedGraph::removeStatements(
     }
     m_pRep->removeStatementsGraph_NoLock(
             i_xSubject, i_xPredicate, i_xObject, m_xName);
+    m_aStatementsCache.clear();
 }
 
 uno::Reference< container::XEnumeration > SAL_CALL
@@ -770,10 +796,63 @@ librdf_NamedGraph::getStatements(
         throw rdf::RepositoryException(
             "librdf_NamedGraph::getStatements: repository is gone", *this);
     }
-    return m_pRep->getStatementsGraph_NoLock(
+    auto ret = m_pRep->getStatementsGraph_NoLock(
             i_xSubject, i_xPredicate, i_xObject, m_xName);
+    return ret;
 }
 
+uno::Sequence<uno::Reference<rdf::XURI>> SAL_CALL
+librdf_NamedGraph::getStatementsObjects(
+    const uno::Reference< rdf::XResource > & i_xSubject,
+    const uno::Reference< rdf::XURI > & i_xPredicate,
+    const uno::Reference< rdf::XNode > & i_xObject)
+{
+    CacheKey key { i_xSubject, i_xPredicate, i_xObject };
+    auto it = m_aStatementsCache.find(key);
+    if (it != m_aStatementsCache.end())
+    {
+        // check if any of the weakreferences is now gone
+        std::vector<uno::Reference<rdf::XURI>> ret;
+        bool foundAll = true;
+        for (const uno::WeakReference<rdf::XURI> & rWeakUri : it->second)
+        {
+            uno::Reference<rdf::XURI> xUri = rWeakUri;
+            if (!xUri.is())
+            {
+                foundAll = false;
+                break;
+            }
+            ret.push_back(xUri);
+        }
+        if (foundAll)
+            return comphelper::containerToSequence(ret);
+        // one of the weak results is gone, need to rebuild it
+        m_aStatementsCache.erase(it);
+    }
+    uno::Reference< rdf::XRepository > xRep( m_wRep );
+    if (!xRep.is()) {
+        throw rdf::RepositoryException(
+            "librdf_NamedGraph::getStatements: repository is gone", *this);
+    }
+    uno::Reference<container::XEnumeration> xEnum = m_pRep->getStatementsGraph_NoLock(
+            i_xSubject, i_xPredicate, i_xObject, m_xName);
+
+    std::vector<uno::WeakReference<rdf::XURI>> weakUris;
+    std::vector<uno::Reference<rdf::XURI>> uris;
+    while (xEnum->hasMoreElements())
+    {
+        rdf::Statement stmt;
+        if (!(xEnum->nextElement() >>= stmt))
+            throw uno::RuntimeException();
+        const uno::Reference<rdf::XURI> xPart(stmt.Object, uno::UNO_QUERY);
+        if (!xPart.is())
+            continue;
+        uris.push_back(xPart);
+        weakUris.push_back(xPart);
+    }
+    m_aStatementsCache.emplace(key, weakUris);
+    return comphelper::containerToSequence(uris);
+}
 
 std::shared_ptr<librdf_world> librdf_Repository::m_pWorld;
 sal_uInt32 librdf_Repository::m_NumInstances = 0;
