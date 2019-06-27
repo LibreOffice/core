d3b81a27e2bb7c8a3de8cd5fabe8d3734b5a3d3d
Jens Carl <j.carl43@gmx.de>
2019-06-26 20:39:49 -0700

    tdf#45904 Move XElementAccess Java tests to C++
    Move XElementAccess Java tests to C++ for ScTabViewObj.
    
    Change-Id: Iee151264033bee61a6a88ec0acb932ae069ede56
    Reviewed-on: https://gerrit.libreoffice.org/74774
    Tested-by: Jenkins
    Reviewed-by: Jens Carl <j.carl43@gmx.de>

--- sc/qa/extras/sctabviewobj.cxx
+++ sc/qa/extras/sctabviewobj.cxx
@@ -8,6 +8,7 @@
  */
 
 #include <test/calc_unoapi_test.hxx>
+#include <test/container/xelementaccess.hxx>
 #include <test/container/xenumerationaccess.hxx>
 #include <test/container/xindexaccess.hxx>
 #include <test/sheet/spreadsheetviewsettings.hxx>
@@ -18,13 +19,16 @@
 #include <test/sheet/xviewsplitable.hxx>
 
 #include <com/sun/star/container/XIndexAccess.hpp>
-#include <com/sun/star/lang/XComponent.hpp>
 #include <com/sun/star/frame/XModel.hpp>
-#include <com/sun/star/sheet/XSpreadsheetDocument.hpp>
+#include <com/sun/star/lang/XComponent.hpp>
 #include <com/sun/star/sheet/XSpreadsheet.hpp>
-
+#include <com/sun/star/sheet/XSpreadsheetDocument.hpp>
+#include <com/sun/star/sheet/XViewPane.hpp>
 #include <com/sun/star/uno/XInterface.hpp>
+
 #include <com/sun/star/uno/Reference.hxx>
+
+#include <cppu/unotype.hxx>
 
 using namespace css;
 using namespace css::uno;
@@ -35,6 +39,7 @@ class ScTabViewObj : public CalcUnoApiTest,
                      public apitest::SpreadsheetViewSettings,
                      public apitest::XActivationBroadcaster,
                      public apitest::XCellRangeReferrer,
+                     public apitest::XElementAccess,
                      public apitest::XEnumerationAccess,
                      public apitest::XIndexAccess,
                      public apitest::XSpreadsheetView,
@@ -62,6 +67,10 @@ public:
     //Disabled till it's clear why it fails on some machines.
     //CPPUNIT_TEST(testGetReferredCells);
 
+    // XElementAccess
+    CPPUNIT_TEST(testGetElementType);
+    CPPUNIT_TEST(testHasElements);
+
     // XEnumerationAccess
     CPPUNIT_TEST(testCreateEnumeration);
 
@@ -86,6 +95,7 @@ private:
 
 ScTabViewObj::ScTabViewObj()
     : CalcUnoApiTest("/sc/qa/extras/testdocuments")
+    , XElementAccess(cppu::UnoType<sheet::XViewPane>::get())
     , XIndexAccess(1)
 {
 }
