3f04c8405db7916f32f59bface80ddf97ab699a0
Miklos Vajna <vmiklos@collabora.com>
2019-07-01 16:08:10 +0200

    sw comments on frames: fix UI to be able to annotate with at-char anchors
    We used to just comment the doc model position at the end of the
    paragraph of the image anchor. Change this, so that in case an at-char
    anchored image is commented, first we insert the comment (which creates
    a placeholder character), and then we set the annotation range to cover
    exactly that placeholder character.
    
    This results in a layout that shows a commented image.
    
    (cherry picked from commit 5cb34f1c38be72047be1c4ed8357c5e87134c6a2)
    
    Conflicts:
    	sw/qa/extras/uiwriter/uiwriter2.cxx
    	sw/source/uibase/inc/fldmgr.hxx
    
    Change-Id: Ie21d62ddb9f55f3e06ee39d9eccfaa0446ce7845

--- sw/source/uibase/fldui/fldmgr.cxx
+++ sw/source/uibase/fldui/fldmgr.cxx
@@ -1489,7 +1489,7 @@ bool SwFieldMgr::InsertField(
     // insert
     pCurShell->StartAllAction();
 
-    pCurShell->Insert( *pField );
+    pCurShell->Insert(*pField, rData.m_pAnnotationRange.get());
 
     if (TYP_INPUTFLD == rData.m_nTypeId)
     {
