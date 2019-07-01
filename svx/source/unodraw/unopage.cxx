f7c48b38092e8e07b9407f83746fc5a30261d328
Katarina Behrens <Katarina.Behrens@cib.de>
2019-07-01 18:20:02 +0200

    speed-up shape import if shapes need z-order rearranging
    setting z-order individually on each shape and broadcasting is
    O(n^2) (remaining shapes also need reordering) and this is bad
    bad bad for performance
    
    Change-Id: Ic9c9137a097f6ff524192693910f221885f77cc4

--- svx/source/unodraw/unopage.cxx
+++ svx/source/unodraw/unopage.cxx
@@ -27,6 +27,7 @@
 #include <osl/mutex.hxx>
 #include <sfx2/dispatch.hxx>
 #include <comphelper/classids.hxx>
+#include <comphelper/sequence.hxx>
 #include <cppuhelper/supportsservice.hxx>
 
 #include <sfx2/objsh.hxx>
@@ -328,6 +329,12 @@ void SAL_CALL SvxDrawPage::remove( const Reference< drawing::XShape >& xShape )
     }
 
     mpModel->SetChanged();
+}
+
+void SvxDrawPage::sort( const css::uno::Sequence< sal_Int32 >& sortOrder )
+{
+    auto newOrder = comphelper::sequenceToContainer<std::vector<sal_Int32>>(sortOrder);
+    mpPage->sort(newOrder);
 }
 
 // css::container::XIndexAccess
