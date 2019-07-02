7d32994ad9dc45c7846664ec5ccb03bb796bb071
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2019-07-02 14:48:31 +0900

    tdf#125655 disbale quick edit mode for iOS and Android
    When clicking (tapping) on text in text based objects (shape with
    text or a text frame), it automatically enters editing mode,
    which is not a desired behaviour in a touch based environment.
    This behaviour comes from a quick edit mode, which is enabled by
    default on the desktop. For Andorid and iOS this is now disabled
    when LOK is initialized.
    For online this change might also make sense (if the user would
    use a mobile device), however this is not implemented now as this
    would require more than just changing the global configuration,
    as it needs to behave differently depending on the view/user.
    
    Change-Id: Ib4a71cba0aeb4238a5d5b708801a07b39fb5f316
    Reviewed-on: https://gerrit.libreoffice.org/74986
    Tested-by: Jenkins
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -157,6 +157,7 @@
 
 #include "lokinteractionhandler.hxx"
 #include "lokclipboard.hxx"
+#include <officecfg/Office/Impress.hxx>
 
 using namespace css;
 using namespace vcl;
@@ -5125,6 +5126,16 @@ static int lo_initialize(LibreOfficeKit* pThis, const char* pAppPath, const char
     {
         comphelper::ThreadPool::getSharedOptimalPool().shutdown();
     }
+
+// Turn off quick editing on IOS and ANDROID
+#if defined IOS || defined ANDROID
+    if (officecfg::Office::Impress::Misc::TextObject::QuickEditing::get())
+    {
+        std::shared_ptr<comphelper::ConfigurationChanges> batch(comphelper::ConfigurationChanges::create());
+        officecfg::Office::Impress::Misc::TextObject::QuickEditing::set(false, batch);
+        batch->commit();
+    }
+#endif
 
     return bInitialized;
 }
