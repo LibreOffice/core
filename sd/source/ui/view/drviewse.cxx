5b979f757db154a04871145aea3bb2f57ec3c40a
Xisco Fauli <xiscofauli@libreoffice.org>
2019-07-02 17:36:50 +0200

    tdf#126197: EndTextEdit on all views before delete/cut shape
    Change-Id: I3da93e5c72ee6f6f99120758e870d654e01a0ec7
    Reviewed-on: https://gerrit.libreoffice.org/75001
    Tested-by: Jenkins
    Reviewed-by: Xisco Faulí <xiscofauli@libreoffice.org>
    (cherry picked from commit 80db66c4f345ac469943f871308b14b7f5ed0a11)
    Reviewed-on: https://gerrit.libreoffice.org/75079
    (cherry picked from commit e79dbb8c6c4d2cf1ce2cf0ce67c53ed4bb09898b)

--- sd/source/ui/view/drviewse.cxx
+++ sd/source/ui/view/drviewse.cxx
@@ -806,6 +806,10 @@ void DrawViewShell::FuSupport(SfxRequest& rReq)
             }
             else
             {
+                //tdf#126197: EndTextEdit in all views if current one is not in TextEdit
+                if ( !mpDrawView->IsTextEdit() )
+                    mpDrawView->EndTextEditAllViews();
+
                 if(HasCurrentFunction())
                 {
                     GetCurrentFunction()->DoCut();
@@ -980,6 +984,7 @@ void DrawViewShell::FuSupport(SfxRequest& rReq)
             }
             else
             {
+                mpDrawView->EndTextEditAllViews();
                 FuDeleteSelectedObjects();
             }
             rReq.Ignore ();
