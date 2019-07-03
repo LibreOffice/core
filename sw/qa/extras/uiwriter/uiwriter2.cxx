ab0c754d3ce256f2d9bdb646e35bfb425f941daf
Miklos Vajna <vmiklos@collabora.com>
2019-07-03 14:05:05 +0200

    sw comments on frames: delete comment of frame when deleting frame
    Also group the two actions together, so only 1 undo action is visible to
    the user, not 2.
    
    (cherry picked from commit 86fd893e32ef7a737b2c4b60e0938146b102fc07)
    
    Conflicts:
    	sw/qa/extras/uiwriter/uiwriter2.cxx
    	sw/source/uibase/wrtsh/delete.cxx
    
    Change-Id: Idba5a63b1653e96db4f9567a38b3f4ca740eb1df

--- sw/qa/extras/uiwriter/uiwriter2.cxx
+++ sw/qa/extras/uiwriter/uiwriter2.cxx
@@ -12,6 +12,7 @@
 #include <swdtflvr.hxx>
 #include <wrtsh.hxx>
 #include <redline.hxx>
+#include <svl/stritem.hxx>
 #include <flyfrms.hxx>
 #include <UndoManager.hxx>
 #include <edtwin.hxx>
@@ -539,6 +540,18 @@ void SwUiWriterTest2::testImageCommentAtChar()
                          getProperty<OUString>(getRun(xPara, 4), "TextPortionType"));
     CPPUNIT_ASSERT_EQUAL(OUString("Text"),
                          getProperty<OUString>(getRun(xPara, 5), "TextPortionType"));
+
+    // Insert content to the comment, and select the image again.
+    SfxStringItem aItem(FN_INSERT_STRING, "x");
+    pView->GetViewFrame()->GetDispatcher()->ExecuteList(FN_INSERT_STRING, SfxCallMode::SYNCHRON,
+                                                        { &aItem });
+    pView->GetViewFrame()->GetDispatcher()->Execute(FN_CNTNT_TO_NEXT_FRAME, SfxCallMode::SYNCHRON);
+    // Now delete the image.
+    pView->GetViewFrame()->GetDispatcher()->Execute(SID_DELETE, SfxCallMode::SYNCHRON);
+    // Without the accompanying fix in place, this test would have failed with 'Expected: 0; Actual:
+    // 1', i.e. the comment of the image was not deleted when the image was deleted.
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(0),
+                         pDoc->getIDocumentMarkAccess()->getAnnotationMarksCount());
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(SwUiWriterTest2);
