edde0cf64cf6418d9a4cb0b0adb187173f0c63eb
Xisco Fauli <xiscofauli@libreoffice.org>
2019-07-05 18:57:39 +0200

    tdf#126184: Restore hack deleted in d1027af3c74529827d53e8cf7b0d42a0ee47d1ba
    Change-Id: I634ec50b5f5578b604956e3fbc45d197352bd320

--- svx/source/customshapes/EnhancedCustomShape2d.cxx
+++ svx/source/customshapes/EnhancedCustomShape2d.cxx
@@ -2542,6 +2542,22 @@ void EnhancedCustomShape2d::CreateSubPath(
 
     if(aNewB2DPolyPolygon.count())
     {
+        if( !bLineGeometryNeededOnly )
+        {
+            // hack aNewB2DPolyPolygon to fill logic rect - this is
+            // needed to produce gradient fills that look like mso
+            // still needed for tdf#126183, tdf#116940 or tdf#122955
+            aNewB2DPolygon.clear();
+            aNewB2DPolygon.append(basegfx::B2DPoint(0,0));
+            aNewB2DPolygon.setClosed(true);
+            aNewB2DPolyPolygon.append(aNewB2DPolygon);
+
+            aNewB2DPolygon.clear();
+            aNewB2DPolygon.append(basegfx::B2DPoint(aLogicRect.GetWidth(),
+                                                    aLogicRect.GetHeight()));
+            aNewB2DPolygon.setClosed(true);
+            aNewB2DPolyPolygon.append(aNewB2DPolygon);
+        }
         // #i37011#
         bool bForceCreateTwoObjects(false);
 
