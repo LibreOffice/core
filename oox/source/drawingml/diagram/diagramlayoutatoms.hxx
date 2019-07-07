91939fc8f53d2e9b09abdfdcbec67a2df127d955
Grzegorz Araminowicz <grzegorz.araminowicz@collabora.com>
2019-07-07 11:31:22 +0200

    Revert "SmartArt: support multiple levels of shapes in LayoutNodes"
    As we have presentation node - shape mapping, keeping shape level information is no longer needed.
    
    This reverts commit 596a03b65e1b870be671ea1a44f4fba9fc66e4ae.
    
    Change-Id: Ibde1b4afde41778304138253c1548422c5b173c3

--- oox/source/drawingml/diagram/diagramlayoutatoms.hxx
+++ oox/source/drawingml/diagram/diagramlayoutatoms.hxx
@@ -161,8 +161,7 @@ public:
     void addParam( sal_Int32 nType, sal_Int32 nVal )
         { maMap[nType]=nVal; }
     void layoutShape( const ShapePtr& rShape,
-                      const std::vector<Constraint>& rConstraints,
-                      sal_Int32 nShapeLevel );
+                      const std::vector<Constraint>& rConstraints );
 
     void setAspectRatio(double fAspectRatio) { mfAspectRatio = fAspectRatio; }
 
@@ -237,7 +236,6 @@ class LayoutNode
 {
 public:
     typedef std::map<sal_Int32, OUString> VarMap;
-    typedef std::map<sal_Int32, std::vector<ShapePtr>> ShapeLevelMap;
 
     LayoutNode(const Diagram& rDgm) : LayoutAtom(*this), mrDgm(rDgm), mnChildOrder(0) {}
     const Diagram& getDiagram() const
@@ -255,10 +253,10 @@ public:
         { mpExistingShape = pShape; }
     const ShapePtr& getExistingShape() const
         { return mpExistingShape; }
-    const ShapeLevelMap& getNodeShapes() const
+    const std::vector<ShapePtr> & getNodeShapes() const
         { return mpNodeShapes; }
-    void addNodeShape(const ShapePtr& pShape, sal_Int32 nLevel)
-        { mpNodeShapes[nLevel].push_back(pShape); }
+    void addNodeShape(const ShapePtr& pShape)
+        { mpNodeShapes.push_back(pShape); }
 
     bool setupShape( const ShapePtr& rShape,
                      const dgm::Point* pPresNode ) const;
@@ -271,7 +269,7 @@ private:
     OUString                     msMoveWith;
     OUString                     msStyleLabel;
     ShapePtr                     mpExistingShape;
-    ShapeLevelMap                mpNodeShapes;
+    std::vector<ShapePtr>        mpNodeShapes;
     sal_Int32                    mnChildOrder;
 };
 
