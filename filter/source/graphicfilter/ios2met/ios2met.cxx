f6b87a7d64cd7c979ebbe032166ef256d4f0d844
Noel Grandin <noel.grandin@collabora.co.uk>
2019-07-08 12:25:02 +0200

    use BitmapEx in OS2METReader
    Change-Id: I03750087174488f1debb2c33e55effe956a59e1d
    Reviewed-on: https://gerrit.libreoffice.org/75213
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

--- filter/source/graphicfilter/ios2met/ios2met.cxx
+++ filter/source/graphicfilter/ios2met/ios2met.cxx
@@ -246,7 +246,7 @@ struct OSFont {
 struct OSBitmap {
     OSBitmap * pSucc;
     sal_uInt32 nID;
-    Bitmap     aBitmap;
+    BitmapEx   aBitmapEx;
 
     // required during reading of the bitmap:
     SvStream * pBMP; // pointer to temporary Windows-BMP file or NULL
@@ -990,7 +990,7 @@ void OS2METReader::ReadBitBlt()
     while (pB!=nullptr && pB->nID!=nID) pB=pB->pSucc;
     if (pB!=nullptr) {
         SetRasterOp(aAttr.ePatMix);
-        pVirDev->DrawBitmap(aP1,aSize,pB->aBitmap);
+        pVirDev->DrawBitmapEx(aP1,aSize,pB->aBitmapEx);
     }
 }
 
@@ -2486,7 +2486,7 @@ void OS2METReader::ReadField(sal_uInt16 nFieldType, sal_uInt16 nFieldSize)
             }
             pBitmapList->pBMP->Seek(0);
 
-            ReadDIB(pBitmapList->aBitmap, *(pBitmapList->pBMP), false);
+            ReadDIBBitmapEx(pBitmapList->aBitmapEx, *(pBitmapList->pBMP), false);
 
             if (pBitmapList->pBMP->GetError()!=ERRCODE_NONE) {
                 pOS2MET->SetError(SVSTREAM_FILEFORMAT_ERROR);
