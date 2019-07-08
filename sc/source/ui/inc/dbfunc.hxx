cb68e5e51fa0c4b8006e46f60d1488c75237bc12
Andrea Gelmini <andrea.gelmini@gelma.net>
2019-07-08 13:38:18 +0000

    Fix typo
    Uhm... maybe missed traslantion from German?
    
    Anyway, this just to spot it to smarter ones
    
    Change-Id: Icba3458d9acf9d123a73973c749c94aebd518881

--- sc/source/ui/inc/dbfunc.hxx
+++ sc/source/ui/inc/dbfunc.hxx
@@ -112,7 +112,7 @@ public:
     void            HideMarkedOutlines( bool bRecord = true );
     bool            OutlinePossible(bool bHide);
 
-    void            UpdateCharts(bool bAllCharts);      // Default: am Cursor
+    void            UpdateCharts(bool bAllCharts);      // Default: at the Cursor
 
     static sal_uInt16   DoUpdateCharts( const ScAddress& rPos, ScDocument* pDoc, bool bAllCharts );
 
