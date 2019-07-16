b09f1be99f21bf56bd65981e82d2ec1dba4b3e9d
Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
2019-07-16 14:19:25 +0200

    tdf#126427 Fix deck highlighting after activating via UNO API
    Change-Id: I4ff7dfa156905abcba0df8e6bfbddc9e7d37cbbb
    Reviewed-on: https://gerrit.libreoffice.org/75723
    Tested-by: Jenkins
    Reviewed-by: Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
    (cherry picked from commit 5a6671e8d8dc65f2bbb0880f19fd8c8c8e426b6d)

--- sfx2/source/sidebar/SidebarController.cxx
+++ sfx2/source/sidebar/SidebarController.cxx
@@ -560,8 +560,6 @@ void SidebarController::OpenThenToggleDeck (
     if (mnSavedSidebarWidth < nRequestedWidth)
         SetChildWindowWidth(nRequestedWidth);
 
-    mpTabBar->Invalidate();
-    mpTabBar->HighlightDeck(rsDeckId);
     collectUIInformation(rsDeckId);
 }
 
@@ -574,8 +572,7 @@ void SidebarController::OpenThenSwitchToDeck (
         pSplitWindow->FadeIn();
     RequestOpenDeck();
     SwitchToDeck(rsDeckId);
-    mpTabBar->Invalidate();
-    mpTabBar->HighlightDeck(rsDeckId);
+
 }
 
 void SidebarController::SwitchToDefaultDeck()
@@ -715,6 +712,7 @@ void SidebarController::SwitchToDeck (
 
         msCurrentDeckId = rDeckDescriptor.msId;
     }
+    mpTabBar->Invalidate();
     mpTabBar->HighlightDeck(msCurrentDeckId);
 
     // Determine the panels to display in the deck.
