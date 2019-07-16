55df612be5e3e2541f8e42f5122febfcffc77d68
Caolán McNamara <caolanm@redhat.com>
2019-07-16 11:50:36 +0100

    cid#1448419 Resource leak
    Change-Id: I1ca12ee7097ba2f4e644d3ff91cff1a49af49cbe
    Reviewed-on: https://gerrit.libreoffice.org/75713
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>

--- sd/source/ui/func/fumorph.cxx
+++ sd/source/ui/func/fumorph.cxx
@@ -380,8 +380,8 @@ void FuMorph::ImpInsertPolygons(
         return;
 
     SfxItemSet      aSet( aSet1 );
-    SdrObjGroup*    pObjGroup = new SdrObjGroup(mpView->getSdrModelFromSdrView());
-    SdrObjList*     pObjList = pObjGroup->GetSubList();
+    std::unique_ptr<SdrObjGroup, SdrObjectFreeOp> xObjGroup(new SdrObjGroup(mpView->getSdrModelFromSdrView()));
+    SdrObjList*     pObjList = xObjGroup->GetSubList();
     const size_t    nCount = rPolyPolyList3D.size();
     const double    fStep = 1. / ( nCount + 1 );
     const double    fDelta = nEndLineWidth - nStartLineWidth;
@@ -434,7 +434,7 @@ void FuMorph::ImpInsertPolygons(
             pObj2->CloneSdrObject(pObj2->getSdrModelFromSdrObject()) );
 
         mpView->DeleteMarked();
-        mpView->InsertObjectAtView( pObjGroup, *pPageView, SdrInsertFlags:: SETDEFLAYER );
+        mpView->InsertObjectAtView(xObjGroup.release(), *pPageView, SdrInsertFlags:: SETDEFLAYER);
     }
 }
 
