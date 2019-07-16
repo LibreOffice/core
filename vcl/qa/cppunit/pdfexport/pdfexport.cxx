d497cecb76c4e1aa07c3e74f167205e7318a8a38
Miklos Vajna <vmiklos@collabora.com>
2019-07-16 23:32:31 +0200

    external: update pdfium to 3849
    Reviewed-on: https://gerrit.libreoffice.org/75736
    Tested-by: Jenkins
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    (cherry picked from commit 0ee0ca3036629b69bf20b448d74991fd133f08ac)
    
    Change-Id: I616f57bd9de72b078500a290bf9ff89c71773f26

--- vcl/qa/cppunit/pdfexport/pdfexport.cxx
+++ vcl/qa/cppunit/pdfexport/pdfexport.cxx
@@ -317,7 +317,7 @@ void PdfExportTest::testTdf105461()
             continue;
 
         unsigned int nRed = 0, nGreen = 0, nBlue = 0, nAlpha = 0;
-        FPDFPath_GetFillColor(pPdfPageObject, &nRed, &nGreen, &nBlue, &nAlpha);
+        FPDFPageObj_GetFillColor(pPdfPageObject, &nRed, &nGreen, &nBlue, &nAlpha);
         if (Color(nRed, nGreen, nBlue) == COL_YELLOW)
             ++nYellowPathCount;
     }
@@ -372,7 +372,7 @@ void PdfExportTest::testTdf107868()
             continue;
 
         unsigned int nRed = 0, nGreen = 0, nBlue = 0, nAlpha = 0;
-        FPDFPath_GetFillColor(pPdfPageObject, &nRed, &nGreen, &nBlue, &nAlpha);
+        FPDFPageObj_GetFillColor(pPdfPageObject, &nRed, &nGreen, &nBlue, &nAlpha);
         if (Color(nRed, nGreen, nBlue) == COL_WHITE)
             ++nWhitePathCount;
     }
@@ -807,7 +807,7 @@ void PdfExportTest::testTdf108963()
             continue;
 
         unsigned int nRed = 0, nGreen = 0, nBlue = 0, nAlpha = 0;
-        FPDFPath_GetFillColor(pPdfPageObject, &nRed, &nGreen, &nBlue, &nAlpha);
+        FPDFPageObj_GetFillColor(pPdfPageObject, &nRed, &nGreen, &nBlue, &nAlpha);
         if (Color(nRed, nGreen, nBlue) == COL_YELLOW)
         {
             ++nYellowPathCount;
