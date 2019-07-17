78b430216bc2a857db9768d0081475a7bed84e4c
Michael Stahl <Michael.Stahl@cib.de>
2019-07-17 17:40:50 +0200

    sw: missing SolarMutexGuard in SwXFrame::attach()
    Causes assert in a test i'm adding...
    
    Change-Id: I5be24aa9683ec0d31494c0c97270d1d58692f764
    Reviewed-on: https://gerrit.libreoffice.org/75796
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <Michael.Stahl@cib.de>
    (cherry picked from commit 8dfe166a879a17bd8c2e0859d027ff923258a9d8)

--- sw/source/core/unocore/unoframe.cxx
+++ sw/source/core/unocore/unoframe.cxx
@@ -3042,6 +3042,8 @@ void SwXFrame::attachToRange(const uno::Reference< text::XTextRange > & xTextRan
 
 void SwXFrame::attach(const uno::Reference< text::XTextRange > & xTextRange)
 {
+    SolarMutexGuard g;
+
     SwFrameFormat* pFormat;
     if(IsDescriptor())
         attachToRange(xTextRange);
