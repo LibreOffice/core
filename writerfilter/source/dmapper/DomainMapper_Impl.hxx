d91209f354163133eaaac0cf6e708f647d1aff17
László Németh <nemeth@numbertext.org>
2019-07-18 09:49:00 +0200

    Revert "tdf#123627 DOCX import: fix relative hyperlinks to documents"
    This reverts commit 217a80fd205c7f61794c863898cb7cfd1a17e78f.
    
    Change-Id: Ic6c9c68dff800f0969187bdd46b51f8e71c0c618
    Reviewed-on: https://gerrit.libreoffice.org/75825
    Reviewed-by: László Németh <nemeth@numbertext.org>
    Tested-by: László Németh <nemeth@numbertext.org>

--- writerfilter/source/dmapper/DomainMapper_Impl.hxx
+++ writerfilter/source/dmapper/DomainMapper_Impl.hxx
@@ -27,6 +27,7 @@
 #include <com/sun/star/text/XTextFrame.hpp>
 #include <com/sun/star/style/TabStop.hpp>
 #include <com/sun/star/container/XNameContainer.hpp>
+#include <unotools/saveopt.hxx>
 #include <queue>
 #include <stack>
 #include <tuple>
@@ -406,6 +407,7 @@ public:
 private:
     SourceDocumentType const                                                        m_eDocumentType;
     DomainMapper&                                                                   m_rDMapper;
+    SvtSaveOptions const                                                            m_aSaveOpt;
     OUString m_aBaseUrl;
     css::uno::Reference<css::text::XTextDocument> m_xTextDocument;
     css::uno::Reference<css::beans::XPropertySet> m_xDocumentSettings;
