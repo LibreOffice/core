a13ed891405da93bfe884bd76c3adf4cab820e47
Noel Grandin <noel.grandin@collabora.co.uk>
2019-07-19 16:31:37 +0200

    simplify ImageList
    we don't need an Impl pattern here
    
    Change-Id: I93d0c5904a3aee8844987c7e36c19319d071d2f4
    Reviewed-on: https://gerrit.libreoffice.org/75960
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
    (cherry picked from commit 825e3f66497c673dd7ac11a8fce830874288977d)

--- framework/source/uiconfiguration/ImageList.hxx
+++ framework/source/uiconfiguration/ImageList.hxx
@@ -20,7 +20,20 @@
 #ifndef FRAMEWORK_SOURCE_UICONFIGURATION_IMAGELIST_HXX
 #define FRAMEWORK_SOURCE_UICONFIGURATION_IMAGELIST_HXX
 
-struct ImplImageList;
+#include <vcl/bitmapex.hxx>
+#include <unordered_map>
+#include <vector>
+
+// Images identified by either name, or by id
+struct ImageAryData
+{
+    OUString                maName;
+    sal_uInt16              mnId;
+    BitmapEx                maBitmapEx;
+
+    bool IsLoadable() { return maBitmapEx.IsEmpty() && !maName.isEmpty(); }
+};
+
 
 class ImageList
 {
@@ -49,15 +62,17 @@ public:
     OUString        GetImageName( sal_uInt16 nPos ) const;
     void            GetImageNames( ::std::vector< OUString >& rNames ) const;
 
-    bool            operator==( const ImageList& rImageList ) const;
-    bool            operator!=( const ImageList& rImageList ) const { return !(ImageList::operator==( rImageList )); }
-
 private:
 
-    std::shared_ptr<ImplImageList> mpImplData;
+    std::vector< std::unique_ptr<ImageAryData> >   maImages;
+    std::unordered_map< OUString, ImageAryData * > maNameHash;
+    OUString               maPrefix;
+    Size                   maImageSize;
 
-    void    ImplInit( sal_uInt16 nItems, const Size &rSize );
     sal_uInt16  ImplGetImageId( const OUString& rImageName ) const;
+    void ImplAddImage( const OUString &aName, sal_uInt16 nId, const BitmapEx &aBitmapEx );
+    void ImplRemoveImage( sal_uInt16 nPos );
+    void ImplLoad(ImageAryData&) const;
 };
 
 #endif // INCLUDED_VCL_IMAGE_HXX
