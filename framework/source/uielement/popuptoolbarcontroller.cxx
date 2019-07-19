LibreOffice/core

commit 15f0d61206dddee12159b68aeab5c860e3d5aff4

use Image(OUString) instead of Image(Bitmap(OUString))
which benefits LOOL since we can delay creating the image until
we know the dpi setting of the display we are going to write to.

Achieved by

    perl -pi -w -e "s/\bImage\s*\(\s*BitmapEx\s*\((\w+)\s*\)\s*\)/Image\(\1\)/g" $( git grep -lw "BitmapEx" )

followed by

    git grep -nP '\bImage\s*\(\s*BitmapEx\s*\('

followed by commenting out the BitmapEx(OUString) constructor and seeing
what needed adjusting.

Change-Id: I3224e11937d720fa484b0d659d25673a9e809267
Reviewed-on: https://gerrit.libreoffice.org/64760
Tested-by: Jenkins
Reviewed-by: Michael Meeks <michael.meeks@collabora.com>

framework/source/uielement/popuptoolbarcontroller.cxx
@@ -492,11 +492,11 @@ void SaveToolbarController::updateImage()
     else if ( m_bModified )
     {
         if (eImageType == vcl::ImageType::Size26)
-            aImage = Image(BitmapEx(BMP_SAVEMODIFIED_LARGE));
+            aImage = Image(StockImage::Yes, BMP_SAVEMODIFIED_LARGE);
         else if (eImageType == vcl::ImageType::Size32)
-            aImage = Image(BitmapEx(BMP_SAVEMODIFIED_EXTRALARGE));
+            aImage = Image(StockImage::Yes, BMP_SAVEMODIFIED_EXTRALARGE);
         else
-            aImage = Image(BitmapEx(BMP_SAVEMODIFIED_SMALL));
+            aImage = Image(StockImage::Yes, BMP_SAVEMODIFIED_SMALL);
     }
 
     if ( !aImage )

commit 662cc05e37d2e7b0637d317a3a454f24c150ddbf

tdf#126293 Save icon changes size when document gets modified
regression from
    commit 15f0d61206dddee12159b68aeab5c860e3d5aff4
    Date:   Fri Dec 7 10:29:02 2018 +0200
    use Image(OUString) instead of Image(Bitmap(OUString))

Change-Id: I9a66b815ec5c401c1ed81c3c8119c69bf77a0454

framework/source/uielement/popuptoolbarcontroller.cxx
@@ -501,6 +501,18 @@ void SaveToolbarController::updateImage()
             aImage = Image(StockImage::Yes, BMP_SAVEMODIFIED_EXTRALARGE);
         else
             aImage = Image(StockImage::Yes, BMP_SAVEMODIFIED_SMALL);
+
+        // not sure why this is not being automatically scaled by the StockImage::Yes
+        sal_Int32 nScalePercentage = Application::GetDefaultDevice()->GetDPIScalePercentage();
+        if (nScalePercentage > 100)
+        {
+            double aScaleFactor(nScalePercentage / 100.0);
+            BitmapEx aScaledBitmapEx = aImage.GetBitmapEx();
+            Size aSizePixel = aImage.GetSizePixel();
+            aScaledBitmapEx.Scale( Size(aSizePixel.getWidth() * aScaleFactor, aSizePixel.getHeight() * aScaleFactor),
+                                   BmpScaleFlag::Fast);
+            aImage = Image(aScaledBitmapEx);
+        }
     }
 
     if ( !aImage )