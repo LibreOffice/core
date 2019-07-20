357acfc15f21e9b964f75ac9dab456347f1e8439
Chris Sherlock <chris.sherlock79@gmail.com>
2019-07-21 05:38:23 +1000

    tdf#74702: vcl - override AnimationRenderer::drawToIndex()
    I have introduced a seam into OutputDevice - a factory function that
    creates an AnimationRenderer instance. By overriding the behaviour in
    Window. I can then introduce a new class WindowAnimationRenderer, which
    contains a specialized drawToIndex() function that caters to Windows.
    
    There are some other areas that will need this later also.
    
    Change-Id: I0ed8014926ac83ef98ab081776a6d8c10127127c

--- vcl/source/animate/AnimationRenderers.cxx
+++ vcl/source/animate/AnimationRenderers.cxx
@@ -18,15 +18,15 @@
  */
 
 #include <vcl/animate/Animation.hxx>
+#include <vcl/animate/AnimationRenderer.hxx>
 #include <vcl/outdev.hxx>
 
 #include <AnimationData.hxx>
-#include <AnimationRenderer.hxx>
 
-void Animation::ClearAnimationRenderers() { maAnimationRenderers.clear(); }
+void AnimationRenderers::ClearAnimationRenderers() { maAnimationRenderers.clear(); }
 
-bool Animation::CanRepaintRenderers(OutputDevice* pOut, sal_uLong nCallerId, const Point& rDestPt,
-                                    const Size& rDestSz)
+bool AnimationRenderers::RepaintRenderers(OutputDevice* pOut, sal_uLong nCallerId,
+                                          const Point& rDestPt, const Size& rDestSz)
 {
     AnimationRenderer* pRenderer;
 
@@ -52,9 +52,9 @@ bool Animation::CanRepaintRenderers(OutputDevice* pOut, sal_uLong nCallerId, con
     return false;
 }
 
-bool Animation::NoRenderersAreAvailable() { return maAnimationRenderers.empty(); }
+bool AnimationRenderers::NoRenderersAreAvailable() { return maAnimationRenderers.empty(); }
 
-std::vector<std::unique_ptr<AnimationData>> Animation::CreateAnimationDataItems()
+std::vector<std::unique_ptr<AnimationData>> AnimationRenderers::CreateAnimationDataItems()
 {
     std::vector<std::unique_ptr<AnimationData>> aAnimationDataItems;
 
@@ -66,15 +66,25 @@ std::vector<std::unique_ptr<AnimationData>> Animation::CreateAnimationDataItems(
     return aAnimationDataItems;
 }
 
-void Animation::CreateDefaultRenderer(Animation* pAnim, OutputDevice* pOut, const Point& rDestPt,
-                                      const Size& rDestSz, sal_uLong nCallerId,
-                                      OutputDevice* pFirstFrameOutDev)
+void AnimationRenderers::CreateDefaultRenderer(Animation* pAnim, OutputDevice* pOut,
+                                               const Point& rDestPt, const Size& rDestSz,
+                                               sal_uLong nCallerId, OutputDevice* pFirstFrameOutDev)
 {
     maAnimationRenderers.emplace_back(
-        new AnimationRenderer(pAnim, pOut, rDestPt, rDestSz, nCallerId, pFirstFrameOutDev));
+        pOut->CreateAnimationRenderer(pAnim, pOut, rDestPt, rDestSz, nCallerId, pFirstFrameOutDev));
 }
 
-void Animation::PopulateRenderers()
+void AnimationRenderers::RemoveAnimationInstance(OutputDevice* pOut, sal_uLong nCallerId)
+{
+    maAnimationRenderers.erase(
+        std::remove_if(maAnimationRenderers.begin(), maAnimationRenderers.end(),
+                       [=](const std::unique_ptr<AnimationRenderer>& pAnimView) -> bool {
+                           return pAnimView->matches(pOut, nCallerId);
+                       }),
+        maAnimationRenderers.end());
+}
+
+void AnimationRenderers::PopulateRenderers(Animation* pAnim)
 {
     AnimationRenderer* pRenderer;
 
@@ -82,8 +92,8 @@ void Animation::PopulateRenderers()
     {
         if (!pItem->pAnimationRenderer)
         {
-            pRenderer = new AnimationRenderer(this, pItem->pOutDev, pItem->aStartOrg,
-                                              pItem->aStartSize, pItem->nCallerId);
+            pRenderer = pItem->pOutDev->CreateAnimationRenderer(
+                pAnim, pItem->pOutDev, pItem->aStartOrg, pItem->aStartSize, pItem->nCallerId);
 
             maAnimationRenderers.push_back(std::unique_ptr<AnimationRenderer>(pRenderer));
         }
@@ -97,7 +107,7 @@ void Animation::PopulateRenderers()
     }
 }
 
-void Animation::DeleteUnmarkedRenderers()
+void AnimationRenderers::DeleteUnmarkedRenderers()
 {
     AnimationRenderer* pRenderer;
 
@@ -109,7 +119,7 @@ void Animation::DeleteUnmarkedRenderers()
     }
 }
 
-bool Animation::ResetMarkedRenderers()
+bool AnimationRenderers::ResetMarkedRenderers()
 {
     bool bIsGloballyPaused = true;
 
@@ -124,30 +134,15 @@ bool Animation::ResetMarkedRenderers()
     return bIsGloballyPaused;
 }
 
-bool Animation::IsTimeoutSetup() { return maTimeoutNotifier.IsSet(); }
-
-bool Animation::SendTimeout()
-{
-    if (IsTimeoutSetup())
-    {
-        maTimeoutNotifier.Call(this);
-        PopulateRenderers();
-        DeleteUnmarkedRenderers();
-        return ResetMarkedRenderers();
-    }
-
-    return false;
-}
-
-void Animation::PaintRenderers()
+void AnimationRenderers::PaintRenderers(sal_uLong nFrameIndex)
 {
     for (auto& rRenderer : maAnimationRenderers)
     {
-        rRenderer->draw(mnFrameIndex);
+        rRenderer->draw(nFrameIndex);
     }
 }
 
-void Animation::EraseMarkedRenderers()
+void AnimationRenderers::EraseMarkedRenderers()
 {
     for (size_t i = 0; i < maAnimationRenderers.size();)
     {
@@ -157,54 +152,6 @@ void Animation::EraseMarkedRenderers()
             maAnimationRenderers.erase(maAnimationRenderers.begin() + i);
         else
             i++;
-    }
-}
-
-AnimationBitmap* Animation::GetNextFrameBitmap()
-{
-    const size_t nAnimCount = maAnimationFrames.size();
-
-    bool bIsFrameAtEnd = mnFrameIndex >= maAnimationFrames.size();
-    mnFrameIndex++;
-
-    AnimationBitmap* pCurrentFrameBmp
-        = bIsFrameAtEnd ? nullptr : maAnimationFrames[mnFrameIndex].get();
-
-    if (!pCurrentFrameBmp)
-    {
-        if (mnLoops == 1)
-        {
-            Stop();
-            mbLoopTerminated = true;
-            mnFrameIndex = nAnimCount - 1;
-            maBitmapEx = maAnimationFrames[mnFrameIndex]->maBitmapEx;
-        }
-        else
-        {
-            if (mnLoops)
-                mnLoops--;
-
-            mnFrameIndex = 0;
-            pCurrentFrameBmp = maAnimationFrames[mnFrameIndex].get();
-        }
-    }
-
-    return pCurrentFrameBmp;
-}
-
-void Animation::RenderNextFrame()
-{
-    AnimationBitmap* pCurrentFrameBmp = GetNextFrameBitmap();
-    if (pCurrentFrameBmp)
-    {
-        PaintRenderers();
-        EraseMarkedRenderers();
-
-        // stop or restart timer
-        if (maAnimationRenderers.empty())
-            Stop();
-        else
-            RestartTimer(pCurrentFrameBmp->mnWait);
     }
 }
 
