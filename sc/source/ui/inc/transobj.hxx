168602b99cdae97d262c2515cfc1e38dd2b4344f
Michael Meeks <michael.meeks@collabora.com>
2019-07-20 11:15:05 +0100

    lok: minimal implementation of isComplex for calc for now.
    Change-Id: Ic4141d30a0ff48a9ec5ecc8e6119d9d779c4a0fc

--- sc/source/ui/inc/transobj.hxx
+++ sc/source/ui/inc/transobj.hxx
@@ -76,6 +76,7 @@ public:
     virtual bool        WriteObject( tools::SvRef<SotStorageStream>& rxOStm, void* pUserObject, sal_uInt32 nUserObjectId,
                                         const css::datatransfer::DataFlavor& rFlavor ) override;
     virtual void        DragFinished( sal_Int8 nDropAction ) override;
+    virtual sal_Bool SAL_CALL isComplex() override;
 
     ScDocument*         GetDocument() const     { return m_pDoc.get(); }        // owned by ScTransferObj
     const ScRange&      GetRange() const        { return m_aBlock; }
