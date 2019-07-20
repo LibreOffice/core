9464654ff79bc366049f5fdbada5067ba0a7f1ca
Michael Meeks <michael.meeks@collabora.com>
2019-07-20 11:05:49 +0100

    lok: cleanup clipboards associated with a view on destruction.
    Also defer destruction potentially indefinitely for bad users who
    quit without destroying views, documents, shutting down nicely etc.
    
    Change-Id: Ieea6ad00b2983d372b745179bfe3b884c3c64eb0

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -1571,6 +1571,8 @@ static void doc_destroy(LibreOfficeKitDocument *pThis)
 
     SolarMutexGuard aGuard;
 
+    LOKClipboardFactory::releaseClipboardForView(-1);
+
     LibLODocument_Impl *pDocument = static_cast<LibLODocument_Impl*>(pThis);
     delete pDocument;
 }
@@ -4280,6 +4282,8 @@ static void doc_destroyView(SAL_UNUSED_PARAMETER LibreOfficeKitDocument* /*pThis
 
     SolarMutexGuard aGuard;
     SetLastExceptionMsg();
+
+    LOKClipboardFactory::releaseClipboardForView(nId);
 
     SfxLokHelper::destroyView(nId);
 }
