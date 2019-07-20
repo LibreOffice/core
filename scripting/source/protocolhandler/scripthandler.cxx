3932dc0426c108e2b7ec37c28c7ee442de8f1417
Noel Grandin <noel.grandin@collabora.co.uk>
2019-07-20 13:28:22 +0200

    loplugin:referencecasting in scripting
    Change-Id: I056ae11c6cda0a1550a8fe363b069359feafcd59

--- scripting/source/protocolhandler/scripthandler.cxx
+++ scripting/source/protocolhandler/scripthandler.cxx
@@ -93,8 +93,7 @@ Reference< XDispatch > SAL_CALL ScriptProtocolHandler::queryDispatch(
     // get scheme of url
 
     Reference< uri::XUriReferenceFactory > xFac = uri::UriReferenceFactory::create( m_xContext );
-    Reference<  uri::XUriReference > uriRef(
-        xFac->parse( aURL.Complete ), UNO_QUERY );
+    Reference<  uri::XUriReference > uriRef = xFac->parse( aURL.Complete );
     if ( uriRef.is() )
     {
         if ( uriRef->getScheme() == "vnd.sun.star.script" )
@@ -324,13 +323,12 @@ ScriptProtocolHandler::getScriptInvocation()
         }
         else
         {
-            Reference< XFrame > xFrame( m_xFrame.get(), UNO_QUERY );
-            if ( xFrame.is() )
+            if ( m_xFrame.is() )
             {
                 SfxFrame* pFrame = nullptr;
                 for ( pFrame = SfxFrame::GetFirst(); pFrame; pFrame = SfxFrame::GetNext( *pFrame ) )
                 {
-                    if ( pFrame->GetFrameInterface() == xFrame )
+                    if ( pFrame->GetFrameInterface() == m_xFrame )
                         break;
                 }
                 SfxObjectShell* pDocShell = pFrame ? pFrame->GetCurrentDocument() : SfxObjectShell::Current();
