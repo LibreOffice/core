0b0ade31dc0a9782b0241ec95d03f027682b69e0
Chris Sherlock <chris.sherlock79@gmail.com>
2019-07-21 01:15:14 +1000

    vcl: extract Animation function IsTimeoutSetup()
    Change-Id: Ia8dca597ce4918b45e7e599bdaa022f6f644bdd8

--- include/vcl/animate/Animation.hxx
+++ include/vcl/animate/Animation.hxx
@@ -116,6 +116,7 @@ private:
     SAL_DLLPRIVATE void PopulateRenderers();
     SAL_DLLPRIVATE void DeleteUnmarkedRenderers();
     SAL_DLLPRIVATE bool ResetMarkedRenderers();
+    SAL_DLLPRIVATE bool IsTimeoutSetup();
 
     SAL_DLLPRIVATE void ImplRestartTimer(sal_uLong nTimeout);
     DECL_DLLPRIVATE_LINK(ImplTimeoutHdl, Timer*, void);
