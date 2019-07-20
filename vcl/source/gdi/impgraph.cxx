38a2a2ed20386e08dfcfa52b895b713de7b73ac5
Chris Sherlock <chris.sherlock79@gmail.com>
2019-07-20 13:41:40 +1000

    vcl: rename mnExtraData to nCallerId
    Change-Id: I56e36f7cb7a81b8e2724c28c8773c7907d723adb

--- vcl/source/gdi/impgraph.cxx
+++ vcl/source/gdi/impgraph.cxx
@@ -1179,21 +1179,21 @@ void ImpGraphic::ImplDraw( OutputDevice* pOutDev,
 }
 
 void ImpGraphic::ImplStartAnimation( OutputDevice* pOutDev, const Point& rDestPt,
-                                     const Size& rDestSize, long nExtraData,
+                                     const Size& rDestSize, long nCallerId,
                                      OutputDevice* pFirstFrameOutDev )
 {
     ensureAvailable();
 
     if( ImplIsSupportedGraphic() && !ImplIsSwapOut() && mpAnimation )
-        mpAnimation->Start( pOutDev, rDestPt, rDestSize, nExtraData, pFirstFrameOutDev );
+        mpAnimation->Start( pOutDev, rDestPt, rDestSize, nCallerId, pFirstFrameOutDev );
 }
 
-void ImpGraphic::ImplStopAnimation( OutputDevice* pOutDev, long nExtraData )
+void ImpGraphic::ImplStopAnimation( OutputDevice* pOutDev, long nCallerId )
 {
     ensureAvailable();
 
     if( ImplIsSupportedGraphic() && !ImplIsSwapOut() && mpAnimation )
-        mpAnimation->Stop( pOutDev, nExtraData );
+        mpAnimation->Stop( pOutDev, nCallerId );
 }
 
 void ImpGraphic::ImplSetAnimationNotifyHdl( const Link<Animation*,void>& rLink )
