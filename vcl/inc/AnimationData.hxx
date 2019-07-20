7bd495e886c017b7a84e5af56f7ef7ca3e0f4637
Chris Sherlock <chris.sherlock79@gmail.com>
2019-07-21 04:31:12 +1000

    vcl: make AnimationData.hxx a module local header
    Change-Id: Iec393740029a96cbbcf98f19865c2bf334697cf5

--- vcl/inc/AnimationData.hxx
+++ vcl/inc/AnimationData.hxx
@@ -17,8 +17,8 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_VCL_ANIMATE_ANIMATIONDATA_HXX
-#define INCLUDED_VCL_ANIMATE_ANIMATIONDATA_HXX
+#ifndef INCLUDED_VCL_INC_ANIMATIONDATA_HXX
+#define INCLUDED_VCL_INC_ANIMATIONDATA_HXX
 
 #include <tools/gen.hxx>
 
@@ -38,4 +38,4 @@ struct AnimationData
     AnimationData();
 };
 
-#endif // INCLUDED_VCL_ANIMATE_ANIMATIONDATA_HXX
+#endif // INCLUDED_VCL_INC_ANIMATIONDATA_HXX
