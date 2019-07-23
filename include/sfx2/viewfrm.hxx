da92217a074cbebdb5204473bc40a725ff5d2e7a
Tamás Zolnai <tamas.zolnai@collabora.com>
2019-07-23 19:38:56 +0200

    lok: Support per-view help data
    Used for showing tooltip windows in LO online.
    
    Change-Id: I8b6a7272c75025e717923c839fa8fd9f4cab2903
    Reviewed-on: https://gerrit.libreoffice.org/84717
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Andras Timar <andras.timar@collabora.com>

--- include/sfx2/viewfrm.hxx
+++ include/sfx2/viewfrm.hxx
@@ -48,6 +48,7 @@ class SFX2_DLLPUBLIC SfxViewFrame: public SfxShell, public SfxListener
     SfxObjectShellRef           m_xObjSh;
     std::unique_ptr<SfxDispatcher> m_pDispatcher;
     SfxBindings*                m_pBindings;
+    ImplSVHelpData*             m_pHelpData;
     sal_uInt16                  m_nAdjustPosPixelLock;
 
 private:
