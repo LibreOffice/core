17fa4153a2f0205333af8119243679a9a1e9bae9
Tamás Zolnai <tamas.zolnai@collabora.com>
2019-07-23 19:38:56 +0200

    lok: Support per-view help data
    Used for showing tooltip windows in LO online.
    
    Change-Id: I8b6a7272c75025e717923c839fa8fd9f4cab2903
    Reviewed-on: https://gerrit.libreoffice.org/84717
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Andras Timar <andras.timar@collabora.com>

--- sfx2/source/view/viewfrm.cxx
+++ sfx2/source/view/viewfrm.cxx
@@ -1618,6 +1618,7 @@ SfxViewFrame::SfxViewFrame
 )
     : m_pImpl( new SfxViewFrame_Impl( rFrame ) )
     , m_pBindings( new SfxBindings )
+    , m_pHelpData(CreateSVHelpData())
     , m_nAdjustPosPixelLock( 0 )
 {
 
@@ -1660,6 +1661,9 @@ SfxViewFrame::~SfxViewFrame()
 
     // Delete Member
     KillDispatcher_Impl();
+
+    DestroySVHelpData(m_pHelpData);
+    m_pHelpData = nullptr;
 }
 
 // Remove and delete the Dispatcher.
@@ -3291,6 +3295,8 @@ void SfxViewFrame::UpdateDocument_Impl()
 
 void SfxViewFrame::SetViewFrame( SfxViewFrame* pFrame )
 {
+    if(pFrame)
+        SetSVHelpData(pFrame->m_pHelpData);
     SfxGetpApp()->SetViewFrame_Impl( pFrame );
 }
 
