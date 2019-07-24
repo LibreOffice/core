9c4945a6767c298258f8aab053841242dc4f7afc
Noel Grandin <noel.grandin@collabora.co.uk>
2019-07-24 10:32:25 +0200

    sd lok: Remove the .uno:LeftPaneImpress call from sd lok initialization.
    this is combination of
    
    commit dd15e149326f5270ab9ceb5f21e32b25b96c0a44
    Author: Ashod Nakashian <ashod.nakashian@collabora.co.uk>
    Date:   Sun Nov 11 23:37:24 2018 -0500
    LOK: sd: initialize slide-sorter in sd
    (Change-Id: I4cb6ce6d961b4ba4d542c14cb37370788cf75e45)
    
    and
    
    commit 7e291eedbad335bf8bbc8a17cc3d633bf66d0e90
    Author: Jan Holesovsky <kendy@collabora.com>
    Date:   Thu Apr 25 05:54:15 2019 +0200
    (Change-Id: I86b23b484674ad4c6a75246ee6186ad9b828931f)
    
    sd lok: Remove the .uno:LeftPaneImpress call from sd lok initialization.
    
    According to my testing, this makes no difference and the moving of
    slides and multiple selection behaves the same way with or without this.
    
    So let's return to not activating the SlideSorterBar, because otherwise
    we are doing a lot of pixel bashing behind the scenes unnecessarily.
    
    Having said that, if we want to activate it at some stage, it is better
    to do something like the following (+ remove the explicit LOK-only if's
    from svtools/source/config/slidesorterbaropt.cxx):
    
      SvtSlideSorterBarOptions().SetVisibleImpressView(true);
    
    instead (and potentially we can also make it floating not to occupy the
    space in the main view by
    
      GetWindow()->SetFloatingMode(true);
    
    in the LOK case in sd/source/ui/dlg/PaneChildWindows.cxxi).
    
    Change-Id: I719bc4ca5e43fce9949d494149b65ffd8587c601
    Reviewed-on: https://gerrit.libreoffice.org/76220
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -2729,8 +2729,6 @@ static void doc_initializeForRendering(LibreOfficeKitDocument* pThis,
     if (pDoc)
     {
         doc_iniUnoCommands();
-        // Create the SlideSorter which is used for multiselection and reordering.
-        doc_postUnoCommand(pThis, ".uno:LeftPaneImpress", nullptr, false);
         pDoc->initializeForTiledRendering(
                 comphelper::containerToSequence(jsonToPropertyValuesVector(pArguments)));
     }
