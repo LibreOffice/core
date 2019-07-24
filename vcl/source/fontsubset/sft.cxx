0d1a4235ec652da4c71d3ab99f8c9feba74ecf4f
Caolán McNamara <caolanm@redhat.com>
2019-07-24 11:37:16 +0100

    cid#1448301 silence Untrusted loop bound
    Change-Id: Ib5b592fdcf234a78747058eb3cdc7efc5d9cddc6

--- vcl/source/fontsubset/sft.cxx
+++ vcl/source/fontsubset/sft.cxx
@@ -436,6 +436,7 @@ static int GetSimpleTTOutline(TrueTypeFont const *ttf, sal_uInt32 glyphID, Contr
         pa[i++].flags = static_cast<sal_uInt32>(flag);
         if (flag & 8) {                                     /*- repeat flag */
             n = *p++;
+            // coverity[tainted_data] - i > lastPoint extra checks the n loop bound
             for (j=0; j<n; j++) {
                 if (i > lastPoint) {                        /*- if the font is really broken */
                     free(pa);
