d48b942f3ce9bda4707bde7cc9c8d9feed2d71dc
Tor Lillqvist <tml@collabora.com>
2019-07-25 14:35:22 +0300

    Skip these patches for iOS, too
    Change-Id: I18f4531bc78f8abe6e82d0bfb6102e2a8ad48a25
    Reviewed-on: https://gerrit.libreoffice.org/76317
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Tor Lillqvist <tml@collabora.com>
    Tested-by: Tor Lillqvist <tml@collabora.com>

--- external/libetonyek/UnpackedTarball_libetonyek.mk
+++ external/libetonyek/UnpackedTarball_libetonyek.mk
@@ -24,9 +24,11 @@ $(eval $(call gb_UnpackedTarball_add_patches,libetonyek,\
 
 ifneq ($(OS),MACOSX)
 ifneq ($(OS),WNT)
+ifneq ($(OS),iOS)
 $(eval $(call gb_UnpackedTarball_add_patches,libetonyek,\
 	external/libetonyek/libetonyek-bundled-soname.patch.0 \
 ))
+endif
 endif
 endif
 
