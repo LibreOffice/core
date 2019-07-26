d141f07015107cfa8b3949aa53a5ca60fe914fd7
Caolán McNamara <caolanm@redhat.com>
2019-07-26 09:22:53 +0100

    cid#1448362 silence Dereference null return value
    Change-Id: I6b24a10bb2cd67d9750702a43d15162fb5347d76

--- sd/source/ui/func/fupage.cxx
+++ sd/source/ui/func/fupage.cxx
@@ -370,7 +370,9 @@ const SfxItemSet* FuPage::ExecuteDialog(weld::Window* pParent, SfxRequest& rReq)
             }
 
             // if the background for this page was set to invisible, the background-object has to be deleted, too.
-            if( ( pTempSet->GetItem<XFillStyleItem>( XATTR_FILLSTYLE )->GetValue() == drawing::FillStyle_NONE ) ||
+            const XFillStyleItem* pTempFillStyleItem = pTempSet->GetItem<XFillStyleItem>(XATTR_FILLSTYLE);
+            assert(pTempFillStyleItem);
+            if( ( pTempFillStyleItem->GetValue() == drawing::FillStyle_NONE ) ||
                 ( ( pTempSet->GetItemState( XATTR_FILLSTYLE ) == SfxItemState::DEFAULT ) &&
                     ( aMergedAttr.GetItem<XFillStyleItem>( XATTR_FILLSTYLE )->GetValue() == drawing::FillStyle_NONE ) ) )
                 mbPageBckgrdDeleted = true;
