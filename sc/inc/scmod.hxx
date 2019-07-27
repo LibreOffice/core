1a27f60b75907ef6ae633fde067073495f45d18e
Caolán McNamara <caolanm@redhat.com>
2019-07-27 11:46:17 +0100

    cid#1401328 Uncaught exception
    Change-Id: I1c4bdd4afec30d7d52054d8c9bd4958191c480f8

--- sc/inc/scmod.hxx
+++ sc/inc/scmod.hxx
@@ -87,7 +87,7 @@ class SAL_DLLPUBLIC_RTTI ScModule: public SfxModule, public SfxListener, public 
     ScMessagePool*      m_pMessagePool;
     // there is no global InputHandler anymore, each View has its own
     ScInputHandler*     m_pRefInputHandler;
-    std::unique_ptr<ScViewCfg>        m_pViewCfg;
+    std::unique_ptr<ScViewCfg, o3tl::default_delete<ScViewCfg>> m_pViewCfg;
     std::unique_ptr<ScDocCfg, o3tl::default_delete<ScDocCfg>> m_pDocCfg;
     std::unique_ptr<ScAppCfg, o3tl::default_delete<ScAppCfg>> m_pAppCfg;
     std::unique_ptr<ScDefaultsCfg>    m_pDefaultsCfg;
