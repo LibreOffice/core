dec9dec4d2a51612c301019f4becd41227b56df4
Aron Budea <aron.budea@collabora.com>
2019-07-28 05:51:40 +0200

    tdf#125800: accessing cond format props in UNO throws error
    These conditions were always evaluated to true: OR should
    become AND when negated.
    
    Regression from 8c637b47d9de4b3a64c33a9c2ffe7ed220be2467
    
    Change-Id: I87038319ed27eff2bed2412da49e01f73ec0a79c
    Reviewed-on: https://gerrit.libreoffice.org/76493
    Reviewed-by: Gülşah Köse <gulsah.kose@collabora.com>
    Reviewed-by: Aron Budea <aron.budea@collabora.com>
    Tested-by: Aron Budea <aron.budea@collabora.com>
    (cherry picked from commit 9ad8f83912e6cf14d7b3b3e1cb2a79d819247c82)
    Reviewed-on: https://gerrit.libreoffice.org/76705
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

--- sc/source/ui/unoobj/fmtuno.cxx
+++ sc/source/ui/unoobj/fmtuno.cxx
@@ -159,7 +159,7 @@ ScTableConditionalFormat::ScTableConditionalFormat(
                 {
                     ScCondFormatEntryItem aItem;
                     const ScFormatEntry* pFrmtEntry = pFormat->GetEntry(i);
-                    if(pFrmtEntry->GetType() != ScFormatEntry::Type::Condition ||
+                    if(pFrmtEntry->GetType() != ScFormatEntry::Type::Condition &&
                        pFrmtEntry->GetType() != ScFormatEntry::Type::ExtCondition)
                         continue;
 
