120bbd100f98f3e375d4a374f8d6835fb17fa1ad
Chris Sherlock <chris.sherlock79@gmail.com>
2019-07-28 19:16:53 +1000

    vcl: test ImplAnimView
    Change-Id: I1f17dce097810e4507a9feae1675ff486260657a

---
+++ vcl/CppunitTest_vcl_animation.mk
@@ -0,0 +1,43 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_CppunitTest_CppunitTest,vcl_animation))
+
+$(eval $(call gb_CppunitTest_set_include,vcl_animation,\
+    -I$(SRCDIR)/vcl/inc \
+    $$(INCLUDE) \
+))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,vcl_animation, \
+	vcl/qa/cppunit/implanimview \
+))
+
+$(eval $(call gb_CppunitTest_use_libraries,vcl_animation, \
+	test \
+	unotest \
+	vcl \
+))
+
+$(eval $(call gb_CppunitTest_use_externals,vcl_animation, \
+	boost_headers \
+))
+
+$(eval $(call gb_CppunitTest_use_sdk_api,vcl_animation))
+
+$(eval $(call gb_CppunitTest_use_ure,vcl_animation))
+$(eval $(call gb_CppunitTest_use_vcl,vcl_animation))
+
+$(eval $(call gb_CppunitTest_use_components,vcl_animation,\
+	configmgr/source/configmgr \
+	i18npool/util/i18npool \
+))
+
+$(eval $(call gb_CppunitTest_use_configuration,vcl_animation))
+
+# vim: set noet sw=4 ts=4:
