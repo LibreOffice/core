a643c1fa95899f89e2add8c7ae89eed7b4fca224
Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
2019-07-29 10:09:11 +0200

    Expand collapsed sidebar when calling XSidebarProvider::showDecks
    This UNO call did not work correctly when the sidebar was completely collapsed.
    
    Change-Id: I3de0b89d95aa81287c36911bbd52a9941d9886b3
    Reviewed-on: https://gerrit.libreoffice.org/76531
    Tested-by: Jenkins
    Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>

--- sfx2/source/sidebar/SidebarController.cxx
+++ sfx2/source/sidebar/SidebarController.cxx
@@ -565,10 +565,6 @@ void SidebarController::OpenThenToggleDeck (
 void SidebarController::OpenThenSwitchToDeck (
     const OUString& rsDeckId)
 {
-    SfxSplitWindow* pSplitWindow = GetSplitWindow();
-    if ( pSplitWindow && !pSplitWindow->IsFadeIn() )
-        // tdf#83546 Collapsed sidebar should expand first
-        pSplitWindow->FadeIn();
     RequestOpenDeck();
     SwitchToDeck(rsDeckId);
 
@@ -1122,6 +1118,11 @@ void SidebarController::RequestCloseDeck()
 
 void SidebarController::RequestOpenDeck()
 {
+    SfxSplitWindow* pSplitWindow = GetSplitWindow();
+    if ( pSplitWindow && !pSplitWindow->IsFadeIn() )
+        // tdf#83546 Collapsed sidebar should expand first
+        pSplitWindow->FadeIn();
+
     mbIsDeckRequestedOpen = true;
     UpdateDeckOpenState();
 }
