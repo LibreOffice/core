e752b13e640f22d4808e0b69c090ae0dca6880d9
Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
2019-07-30 11:18:45 +0200

    tdf#126508 Fix crash when changing icon style
    Regression from b58aa94f1f365c746135470bceb97cc182c289bc
    
    Change-Id: I253447c197d8b333e26f243e26794e775f3edcee

--- sfx2/source/sidebar/SidebarController.cxx
+++ sfx2/source/sidebar/SidebarController.cxx
@@ -656,6 +656,8 @@ void SidebarController::SwitchToDeck (
     const DeckDescriptor& rDeckDescriptor,
     const Context& rContext)
 {
+    maFocusManager.Clear();
+
     const bool bForceNewDeck ((mnRequestedForceFlags&SwitchFlag_ForceNewDeck)!=0);
     const bool bForceNewPanels ((mnRequestedForceFlags&SwitchFlag_ForceNewPanels)!=0);
     mnRequestedForceFlags = SwitchFlag_NoForce;
