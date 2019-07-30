20a4abc865a4aef2ec74153ca00ff5cdde79e0d8
Xisco Fauli <xiscofauli@libreoffice.org>
2019-07-30 15:51:58 +0200

    tdf#126605: Do not end TextEdit when changing an attribute
    otherwise, pTextEditOutlinerView becomes nullptr in
    svx/source/svdraw/svdedxv.cxx:2184
    
    Regression from e6c7a018a0cfee395ce2886d41c908a2447ef5cc
    
    Change-Id: I9d0645f637dc92b50d01682d119c0db60238e921

---
+++ sd/qa/uitest/impress_tests/tdf126605.py
@@ -0,0 +1,28 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from uitest.framework import UITestCase
+from libreoffice.uno.propertyvalue import mkPropertyValues
+
+class tdf126605(UITestCase):
+
+   def test_run(self):
+        self.ui_test.create_doc_in_start_center("impress")
+        xTemplateDlg = self.xUITest.getTopFocusWindow()
+        xCancelBtn = xTemplateDlg.getChild("cancel")
+        self.ui_test.close_dialog_through_button(xCancelBtn)
+
+        xDoc = self.xUITest.getTopFocusWindow()
+        xEdit = xDoc.getChild("impress_win")
+        xEdit.executeAction("TYPE", mkPropertyValues({"TEXT":"test"}))
+
+        # Without the accompanying fix in place, it would crash at this point
+        self.xUITest.executeCommand(".uno:ParaRightToLeft")
+        self.xUITest.executeCommand(".uno:ParaLeftToRight")
+
+        self.ui_test.close_doc()
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
