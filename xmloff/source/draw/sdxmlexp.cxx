cffa757fa067f5ca2622d91e09e64121fba5510e
Miklos Vajna <vmiklos@collabora.com>
2019-07-31 20:50:05 +0200

    xmloff: create XMLImpressStylesExportOOO instances with an uno constructor
    See tdf#74608 for motivation.
    
    Change-Id: I6b99c5975eacbaba5dbec1ae679c9e5726bdfd6b

--- xmloff/source/draw/sdxmlexp.cxx
+++ xmloff/source/draw/sdxmlexp.cxx
@@ -2657,8 +2657,17 @@ com_sun_star_comp_Impress_XMLOasisSettingsExporter_get_implementation(
                                          SvXMLExportFlags::OASIS | SvXMLExportFlags::SETTINGS));
 }
 
+extern "C" SAL_DLLPUBLIC_EXPORT uno::XInterface*
+com_sun_star_comp_Impress_XMLStylesExporter_get_implementation(
+    uno::XComponentContext* pCtx, uno::Sequence<uno::Any> const& /*rSeq*/)
+{
+    return cppu::acquire(new SdXMLExport(pCtx, "XMLImpressStylesExportOOO", false,
+                                         SvXMLExportFlags::STYLES | SvXMLExportFlags::MASTERSTYLES
+                                             | SvXMLExportFlags::AUTOSTYLES
+                                             | SvXMLExportFlags::FONTDECLS));
+}
+
 SERVICE( XMLImpressExportOOO, "com.sun.star.comp.Impress.XMLExporter", "XMLImpressExportOOO", false, SvXMLExportFlags::META|SvXMLExportFlags::STYLES|SvXMLExportFlags::MASTERSTYLES|SvXMLExportFlags::AUTOSTYLES|SvXMLExportFlags::CONTENT|SvXMLExportFlags::SCRIPTS|SvXMLExportFlags::SETTINGS|SvXMLExportFlags::FONTDECLS|SvXMLExportFlags::EMBEDDED );
-SERVICE( XMLImpressStylesExportOOO, "com.sun.star.comp.Impress.XMLStylesExporter", "XMLImpressStylesExportOOO", false, SvXMLExportFlags::STYLES|SvXMLExportFlags::MASTERSTYLES|SvXMLExportFlags::AUTOSTYLES|SvXMLExportFlags::FONTDECLS );
 SERVICE( XMLImpressContentExportOOO, "com.sun.star.comp.Impress.XMLContentExporter", "XMLImpressContentExportOOO", false, SvXMLExportFlags::AUTOSTYLES|SvXMLExportFlags::CONTENT|SvXMLExportFlags::SCRIPTS|SvXMLExportFlags::FONTDECLS );
 SERVICE( XMLImpressMetaExportOOO, "com.sun.star.comp.Impress.XMLMetaExporter", "XMLImpressMetaExportOOO", false, SvXMLExportFlags::META );
 SERVICE( XMLImpressSettingsExportOOO, "com.sun.star.comp.Impress.XMLSettingsExporter", "XMLImpressSettingsExportOOO", false, SvXMLExportFlags::SETTINGS );
