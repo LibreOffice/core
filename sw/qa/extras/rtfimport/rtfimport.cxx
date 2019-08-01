3472c353d0ca03680b80b2d04b99e99cc48f0011
László Németh <nemeth@numbertext.org>
2019-08-01 17:06:47 +0200

    tdf#123702 RTF/DOCX default 1" left/right page margins
    since MSO 2007: now 1440 twips = 2540 1/100 mm (it was
    1800 twips = 3175 1/100 mm).
    
    Changing the default value fixes the layout of the
    documents based on RTF templates without explicit margins.
    
    Change-Id: I0395fb7cdd6ba176f266c8f0a9309ba48a047da3
    Reviewed-on: https://gerrit.libreoffice.org/76812
    Tested-by: Jenkins
    Reviewed-by: László Németh <nemeth@numbertext.org>
    (cherry picked from commit 2550b380e8f81528aa2dde5790c3b607c068ee1a)

--- sw/qa/extras/rtfimport/rtfimport.cxx
+++ sw/qa/extras/rtfimport/rtfimport.cxx
@@ -436,7 +436,7 @@ DECLARE_RTFIMPORT_TEST(testFdo79319, "fdo79319.rtf")
     // the thin horizontal rule was imported as a big fat rectangle
     uno::Reference<drawing::XShape> xShape(getShape(1), uno::UNO_QUERY);
     CPPUNIT_ASSERT_EQUAL(sal_Int16(100), getProperty<sal_Int16>(xShape, "RelativeWidth"));
-    CPPUNIT_ASSERT_DOUBLES_EQUAL(sal_Int32(15238), xShape->getSize().Width, 10);
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(sal_Int32(16508), xShape->getSize().Width, 10);
     CPPUNIT_ASSERT_DOUBLES_EQUAL(sal_Int32(53), xShape->getSize().Height, 10);
 #if 0
     CPPUNIT_ASSERT_EQUAL(text::VertOrientation::CENTER, getProperty<sal_Int16>(xShape, "VertOrient"));
