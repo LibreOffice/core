7e8b570c6166c345bacbe40d6e98fa73bcc20f70
Caolán McNamara <caolanm@redhat.com>
2019-08-03 16:22:51 +0100

    ofz#16191 add en_LK locale data stub
    Change-Id: I0a4fd74e9f50e5d62d2bbd294a42cd272af90e44

--- vcl/StaticLibrary_fuzzerstubs.mk
+++ vcl/StaticLibrary_fuzzerstubs.mk
@@ -30,6 +30,7 @@ $(eval $(call gb_StaticLibrary_add_exception_objects,fuzzerstubs,\
     vcl/workben/localestub/localedata_en_IE \
     vcl/workben/localestub/localedata_en_IN \
     vcl/workben/localestub/localedata_en_JM \
+    vcl/workben/localestub/localedata_en_LK \
     vcl/workben/localestub/localedata_en_MW \
     vcl/workben/localestub/localedata_en_MY \
     vcl/workben/localestub/localedata_en_NA \
