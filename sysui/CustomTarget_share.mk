LibreOffice/core

commit 634844354ee6ed884128086a80c3ee32c889d8c9

sysui: fix rpm errors in freedesktop-menus (4.14.1)
RPM build errors:
    Explicit %attr() mode not applicable to symlink: /workdir/CustomTarget/sysui/rpm/libreofficedev/freedesktop/usr/bin/libreofficedev6.1
    Installed (but unpackaged) file(s) found:
   /usr/local/lib/girepository-1.0/LOKDocView-0.1.typelib
   /usr/share/gir-1.0/LOKDocView-0.1.gir

The LOKDocView problem turned out to be the result of the first
incremental build after adding --with-package-format, and previously it
was avoided with a if in configure; moving the commands out of
create_tree.sh should be more obvious and reliable though.

Change-Id: I69c1566e26eeaa1d8bf88a3650a78da6ddfb5a3b
Reviewed-on: https://gerrit.libreoffice.org/76596
Tested-by: Jenkins
Reviewed-by: Michael Stahl <Michael.Stahl@cib.de>

configure.ac
@@ -10431,10 +10431,7 @@ if test "x$enable_gtk3" = "xyes"; then
     if test "x$ENABLE_GTK3" = "xTRUE"; then
         AC_DEFINE(ENABLE_GTK3)
         R="gtk3"
-        dnl Avoid installed by unpackaged files for now.
-        if test -z "$PKGFORMAT"; then
-            GOBJECT_INTROSPECTION_CHECK(INTROSPECTION_REQUIRED_VERSION)
-        fi
+        GOBJECT_INTROSPECTION_CHECK(INTROSPECTION_REQUIRED_VERSION)
     else
         AC_MSG_ERROR([gtk3 or dependent libraries of the correct versions, not found])
     fi

sysui/CustomTarget_share.mk
@@ -127,6 +127,9 @@ $(eval $(call gb_CustomTarget_register_targets,sysui/share,\
 		$(product)/openoffice.keys \
 		$(product)/openoffice.sh \
 		$(product)/create_tree.sh \
+		$(if $(INTROSPECTION_SCANNER),\
+			$(product)/LOKDocView-0.1.gir \
+			$(product)/LOKDocView-0.1.typelib) \
 		$(product)/launcherlist) \
 ))
 
@@ -171,6 +174,30 @@ $(share_WORKDIR)/%/create_tree.sh: $(share_SRCDIR)/share/create_tree.sh \
 	cat $< >> $@
 	chmod 774 $@
 
+# Generate gobject-introspection files
+# These are *not* packaged in rpms because there's no good place to put them
+# where the system will actually find them and where it won't conflict with a
+# distro packaged LO; on Fedora 30 at least there's no /opt path in
+# $XDG_DATA_DIRS
+ifneq ($(INTROSPECTION_SCANNER),)
+
+$(share_WORKDIR)/%/LOKDocView-0.1.gir: \
+		$(call gb_Library_get_target,libreofficekitgtk)
+	mkdir -p $(dir $@)
+	g-ir-scanner "${SRCDIR}/include/LibreOfficeKit/LibreOfficeKitGtk.h" \
+				 "${SRCDIR}/libreofficekit/source/gtk/lokdocview.cxx" \
+                 `${PKG_CONFIG} --cflags gobject-introspection-1.0 gtk+-3.0` \
+				 -I"${SRCDIR}/include/" \
+                 --include=GLib-2.0 --include=GObject-2.0 --include=Gio-2.0 \
+                 --library=libreofficekitgtk --library-path="${INSTDIR}/program" \
+                 --include=Gdk-3.0 --include=GdkPixbuf-2.0 --include=Gtk-3.0 \
+                 --namespace=LOKDocView --nsversion=0.1 --identifier-prefix=LOKDoc --symbol-prefix=lok_doc \
+				 --output="$@" --warn-all --no-libtool
+
+$(share_WORKDIR)/%/LOKDocView-0.1.typelib: $(share_WORKDIR)/%/LOKDocView-0.1.gir
+	g-ir-compiler "$<" --output="$@"
+
+endif
 
 $(share_WORKDIR)/%/launcherlist: $(LAUNCHERS)
 	mkdir -p $(dir $@)

sysui/desktop/freedesktop/freedesktop-menus.spec
@@ -394,7 +394,7 @@ done
 # compat symlinks
 %attr(0755,root,root) /opt/%unixfilename
 %endif
-%attr(0755, root, root) /usr/bin/*
+/usr/bin/*
 /usr/share/applications/%unixfilename-base.desktop
 /usr/share/applications/%unixfilename-calc.desktop
 /usr/share/applications/%unixfilename-draw.desktop

sysui/desktop/share/create_tree.sh
@@ -83,18 +83,3 @@ for i in base calc draw impress writer; do
 done
 cp "${APPDATA_SOURCE_DIR}/org.libreoffice.kde.metainfo.xml" "${DESTDIR}/${PREFIXDIR}/share/appdata/org.${PREFIX}.kde.metainfo.xml"
 
-# Generate gobject-introspection files
-if [ -n "$INTROSPECTION_SCANNER" ]; then
-    mkdir -p "${DESTDIR}/${PREFIXDIR}/share/gir-1.0"
-    g-ir-scanner "${SRCDIR}/include/LibreOfficeKit/LibreOfficeKitGtk.h" "${SRCDIR}/libreofficekit/source/gtk/lokdocview.cxx" \
-                 `${PKG_CONFIG} --cflags gobject-introspection-1.0 gtk+-3.0` -I"${SRCDIR}/include/" \
-                 --include=GLib-2.0 --include=GObject-2.0 --include=Gio-2.0 \
-                 --library=libreofficekitgtk --library-path="${INSTDIR}/program" \
-                 --include=Gdk-3.0 --include=GdkPixbuf-2.0 --include=Gtk-3.0 \
-                 --namespace=LOKDocView --nsversion=0.1 --identifier-prefix=LOKDoc --symbol-prefix=lok_doc \
-                 --output="${DESTDIR}/${PREFIXDIR}/share/gir-1.0/LOKDocView-0.1.gir" --warn-all --no-libtool
-
-    mkdir -p "${DESTDIR}/${LIBDIR}/girepository-1.0"
-    g-ir-compiler "${DESTDIR}/${PREFIXDIR}/share/gir-1.0/LOKDocView-0.1.gir" \
-                  --output="${DESTDIR}/${LIBDIR}/girepository-1.0/LOKDocView-0.1.typelib"
-fi

commit 20ca741223cacb7784af3b466c94f9513f8d612f

Introduce explicit --enable-introspection
4b4c7e76e6c2ea0d47a42a5107352d3ad7341fbf "Only build LOKDocView-0.1.gir when
necessary" had erroneously assumed that LOKDocView-0.1.{gir,typelib} need to be
built when PKGFORMAT contains "deb" or "rpm".  But instead, they need to be
built only for some 3rd-party Linux distro builds, never for TDF builds.

Make that explicit with a new --enable-introspection, which those 3rd-party
Linux distros will now have to specify (probably along with other fixes to where
they pick up those LOKDocView-0.1.{gir,typelib} files after
634844354ee6ed884128086a80c3ee32c889d8c9 "sysui: fix rpm errors in
freedesktop-menus (4.14.1)" had moved them around).

That way, builds that broke after 634844354ee6ed884128086a80c3ee32c889d8c9
"sysui: fix rpm errors in freedesktop-menus (4.14.1)" (like my ASan+UBSan one
that 4b4c7e76e6c2ea0d47a42a5107352d3ad7341fbf "Only build LOKDocView-0.1.gir
when necessary" had tried to fix) can be fixed with an (implicit)
--disable-introspection.

This commit contains a revert of 4b4c7e76e6c2ea0d47a42a5107352d3ad7341fbf "Only
build LOKDocView-0.1.gir when necessary", which it supersedes.

Change-Id: Idb618e3353da7d68a2e552b0f290775c02327733

configure.ac
@@ -1228,6 +1228,11 @@ libo_FUZZ_ARG_ENABLE(gtk3,
         [Determines whether to use Gtk+ 3.0 vclplug on platforms where Gtk+ 3.0 is available.]),
 ,test "${enable_gtk3+set}" = set || enable_gtk3=yes)
 
+AC_ARG_ENABLE(introspection,
+    AS_HELP_STRING([--enable-introspection],
+        [Generate files for GObject introspection.  Requires --enable-gtk3.  (Typically used by
+         Linux distributions.)]))
+
 AC_ARG_ENABLE(split-app-modules,
     AS_HELP_STRING([--enable-split-app-modules],
         [Split file lists for app modules, e.g. base, calc.
@@ -10431,7 +10436,6 @@ if test "x$enable_gtk3" = "xyes"; then
     if test "x$ENABLE_GTK3" = "xTRUE"; then
         AC_DEFINE(ENABLE_GTK3)
         R="gtk3"
-        GOBJECT_INTROSPECTION_CHECK(INTROSPECTION_REQUIRED_VERSION)
     else
         AC_MSG_ERROR([gtk3 or dependent libraries of the correct versions, not found])
     fi
@@ -10449,6 +10453,14 @@ fi
 AC_SUBST(GTK3_LIBS)
 AC_SUBST(GTK3_CFLAGS)
 AC_SUBST(ENABLE_GTK3)
+
+if test "$enable_introspection" = yes; then
+    if test "$ENABLE_GTK3" = TRUE; then
+        GOBJECT_INTROSPECTION_REQUIRE(INTROSPECTION_REQUIRED_VERSION)
+    else
+        AC_MSG_ERROR([--enable-gtk3-introspection requires --enable-gtk3])
+    fi
+fi
 
 ENABLE_GTK=""
 if test "x$enable_gtk" = "xyes"; then

sysui/CustomTarget_share.mk
@@ -127,9 +127,9 @@ $(eval $(call gb_CustomTarget_register_targets,sysui/share,\
 		$(product)/openoffice.keys \
 		$(product)/openoffice.sh \
 		$(product)/create_tree.sh \
-		$(if $(filter deb rpm,$(PKGFORMAT)),$(if $(INTROSPECTION_SCANNER),\
+		$(if $(INTROSPECTION_SCANNER),\
 			$(product)/LOKDocView-0.1.gir \
-			$(product)/LOKDocView-0.1.typelib)) \
+			$(product)/LOKDocView-0.1.typelib) \
 		$(product)/launcherlist) \
 ))
 