LibreOffice/core

commit d425658bd9fd8e315e4931afb544bc845da0360e

pass OSQLParseNode around by unique_ptr
Change-Id: I8ffb9e04614472c3645d24bebdc88f91059d12ad
Reviewed-on: https://gerrit.libreoffice.org/65436
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

connectivity/source/parse/sqlbison.y
@@ -4494,10 +4494,10 @@ void setParser(OSQLParser* _pParser)
 	xxx_pGLOBAL_SQLPARSER = _pParser;
 }
 
-void OSQLParser::setParseTree(OSQLParseNode * pNewParseTree)
+void OSQLParser::setParseTree(OSQLParseNode* pNewParseTree)
 {
 	::osl::MutexGuard aGuard(getMutex());
-	m_pParseTree = pNewParseTree;
+	m_pParseTree.reset(pNewParseTree);
 }
 
 
@@ -4561,7 +4561,7 @@ static OUString delComment( const OUString& rQuery )
     return aBuf.makeStringAndClear();
 }
 
-OSQLParseNode* OSQLParser::parseTree(OUString& rErrorMessage,
+std::unique_ptr<OSQLParseNode> OSQLParser::parseTree(OUString& rErrorMessage,
                                      const OUString& rStatement,
                                      bool bInternational)
 {
@@ -4609,7 +4609,7 @@ OSQLParseNode* OSQLParser::parseTree(OUString& rErrorMessage,
 
         SAL_WARN_IF(!m_pParseTree, "connectivity.parse",
             "OSQLParser: Parser did not create ParseTree");
-		return m_pParseTree;
+		return std::move(m_pParseTree);
 	}
 }
 

connectivity/source/parse/sqlnode.cxx
@@ -1159,7 +1159,7 @@ OUString OSQLParser::stringToDouble(const OUString& _rValue,sal_Int16 _nScale)
 }
 
 
-OSQLParseNode* OSQLParser::predicateTree(OUString& rErrorMessage, const OUString& rStatement,
+std::unique_ptr<OSQLParseNode> OSQLParser::predicateTree(OUString& rErrorMessage, const OUString& rStatement,
                                          const Reference< css::util::XNumberFormatter > & xFormatter,
                                          const Reference< XPropertySet > & xField,
                                          bool bUseRealName)
@@ -1309,14 +1309,13 @@ OSQLParseNode* OSQLParser::predicateTree(OUString& rErrorMessage, const OUString
 
         // Instead, the parse method sets the member pParseTree and simply returns that
         OSL_ENSURE(m_pParseTree != nullptr,"OSQLParser: Parser did not return a ParseTree!");
-        return m_pParseTree;
+        return std::move(m_pParseTree);
     }
 }
 
 
 OSQLParser::OSQLParser(const css::uno::Reference< css::uno::XComponentContext >& rxContext, const IParseContext* _pContext)
     :m_pContext(_pContext)
-    ,m_pParseTree(nullptr)
     ,m_pData( new OSQLParser_Data )
     ,m_nFormatKey(0)
     ,m_nDateFormatKey(0)

connectivity/source/commontools/ParameterSubstitution.cxx
@@ -83,12 +83,11 @@ namespace connectivity
                 OSQLParser aParser( m_xContext );
                 OUString sErrorMessage;
                 OUString sNewSql;
-                OSQLParseNode* pNode = aParser.parseTree(sErrorMessage,_sText);
+                std::unique_ptr<OSQLParseNode> pNode = aParser.parseTree(sErrorMessage,_sText);
                 if(pNode)
                 {   // special handling for parameters
-                    OSQLParseNode::substituteParameterNames(pNode);
+                    OSQLParseNode::substituteParameterNames(pNode.get());
                     pNode->parseNodeToStr( sNewSql, xConnection );
-                    delete pNode;
                     sRet = sNewSql;
                 }
             }

connectivity/source/commontools/predicateinput.cxx
@@ -123,9 +123,9 @@ namespace dbtools
     }
 
 
-    OSQLParseNode* OPredicateInputController::implPredicateTree(OUString& _rErrorMessage, const OUString& _rStatement, const Reference< XPropertySet > & _rxField) const
+    std::unique_ptr<OSQLParseNode> OPredicateInputController::implPredicateTree(OUString& _rErrorMessage, const OUString& _rStatement, const Reference< XPropertySet > & _rxField) const
     {
-        OSQLParseNode* pReturn = const_cast< OSQLParser& >( m_aParser ).predicateTree( _rErrorMessage, _rStatement, m_xFormatter, _rxField );
+        std::unique_ptr<OSQLParseNode> pReturn = const_cast< OSQLParser& >( m_aParser ).predicateTree( _rErrorMessage, _rStatement, m_xFormatter, _rxField );
         if ( !pReturn )
         {   // is it a text field ?
             sal_Int32 nType = DataType::OTHER;
@@ -242,7 +242,7 @@ namespace dbtools
             // parse the string
             OUString sError;
             OUString sTransformedText( _rPredicateValue );
-            OSQLParseNode* pParseNode = implPredicateTree( sError, sTransformedText, _rxField );
+            std::unique_ptr<OSQLParseNode> pParseNode = implPredicateTree( sError, sTransformedText, _rxField );
             if ( _pErrorMessage ) *_pErrorMessage = sError;
 
             if ( pParseNode )
@@ -258,7 +258,6 @@ namespace dbtools
                     rParseContext.getPreferredLocale(), static_cast<sal_Char>(nDecSeparator), &rParseContext
                 );
                 _rPredicateValue = sTransformedText;
-                delete pParseNode;
 
                 bSuccess = true;
             }
@@ -279,9 +278,9 @@ namespace dbtools
             // (dbaccess/source/ui/dlg/paramdialog.cxx). I do not fully understand this .....
 
             OUString sError;
-            OSQLParseNode* pParseNode = implPredicateTree( sError, _rPredicateValue, _rxField );
+            std::unique_ptr<OSQLParseNode> pParseNode = implPredicateTree( sError, _rPredicateValue, _rxField );
 
-            implParseNode(pParseNode, true) >>= sReturn;
+            implParseNode(std::move(pParseNode), true) >>= sReturn;
         }
 
         return sReturn;
@@ -325,10 +324,10 @@ namespace dbtools
         pColumn->setFunction(true);
         pColumn->setRealName(sField);
 
-        OSQLParseNode* pParseNode = implPredicateTree( sError, _rPredicateValue, xColumn );
+        std::unique_ptr<OSQLParseNode> pParseNode = implPredicateTree( sError, _rPredicateValue, xColumn );
         if(pParseNode)
         {
-            implParseNode(pParseNode, true) >>= sReturn;
+            implParseNode(std::move(pParseNode), true) >>= sReturn;
         }
         return sReturn;
     }
@@ -344,22 +343,21 @@ namespace dbtools
             // (dbaccess/source/ui/dlg/paramdialog.cxx). I do not fully understand this .....
 
             OUString sError;
-            OSQLParseNode* pParseNode = implPredicateTree( sError, _rPredicateValue, _rxField );
+            std::unique_ptr<OSQLParseNode> pParseNode = implPredicateTree( sError, _rPredicateValue, _rxField );
 
-            return implParseNode(pParseNode, false);
+            return implParseNode(std::move(pParseNode), false);
         }
 
         return Any();
     }
 
-    Any OPredicateInputController::implParseNode(OSQLParseNode* pParseNode, bool _bForStatementUse) const
+    Any OPredicateInputController::implParseNode(std::unique_ptr<OSQLParseNode> pParseNode, bool _bForStatementUse) const
     {
         if ( ! pParseNode )
             return Any();
         else
         {
             OUString sReturn;
-            std::shared_ptr<OSQLParseNode> xTakeOwnership(pParseNode);
             OSQLParseNode* pOdbcSpec = pParseNode->getByRule( OSQLParseNode::odbc_fct_spec );
             if ( pOdbcSpec )
             {

connectivity/source/drivers/ado/APreparedStatement.cxx
@@ -61,15 +61,14 @@ OPreparedStatement::OPreparedStatement( OConnection* _pConnection, const OUStrin
     OSQLParser aParser(comphelper::getComponentContext(_pConnection->getDriver()->getORB()));
     OUString sErrorMessage;
     OUString sNewSql;
-    OSQLParseNode* pNode = aParser.parseTree(sErrorMessage,sql);
+    std::unique_ptr<OSQLParseNode> pNode = aParser.parseTree(sErrorMessage,sql);
     if(pNode)
     {   // special handling for parameters
         //  we recursive replace all occurrences of ? in the statement and
         //  replace them with name like "parame" */
         sal_Int32 nParameterCount = 0;
-        replaceParameterNodeName(pNode,"parame",nParameterCount);
+        replaceParameterNodeName(pNode.get(), "parame", nParameterCount);
         pNode->parseNodeToStr( sNewSql, _pConnection );
-        delete pNode;
     }
     else
         sNewSql = sql;

connectivity/source/drivers/file/FStatement.cxx
@@ -367,7 +367,7 @@ void OStatement_Base::setOrderbyColumn( OSQLParseNode const * pColumnRef,
 void OStatement_Base::construct(const OUString& sql)
 {
     OUString aErr;
-    m_pParseTree = m_aParser.parseTree(aErr,sql);
+    m_pParseTree = m_aParser.parseTree(aErr,sql).release();
     if(!m_pParseTree)
         throw SQLException(aErr,*this,OUString(),0,Any());
 

connectivity/source/drivers/macab/MacabStatement.cxx
@@ -403,7 +403,7 @@ Reference< XResultSet > SAL_CALL MacabCommonStatement::executeQuery(
     Reference< XResultSet > xRS = pResult;
     OUString aErr;
 
-    m_pParseTree = m_aParser.parseTree(aErr, sql);
+    m_pParseTree = m_aParser.parseTree(aErr, sql).release();
     if (m_pParseTree == nullptr)
         throw SQLException(aErr, *this, aErr, 0, Any());
 

connectivity/source/drivers/mork/MStatement.cxx
@@ -115,7 +115,7 @@ OCommonStatement::StatementType OCommonStatement::parseSql( const OUString& sql 
 {
     OUString aErr;
 
-    m_pParseTree.reset( m_aParser.parseTree(aErr,sql) );
+    m_pParseTree = m_aParser.parseTree(aErr,sql);
 
     if(m_pParseTree)
     {

commit 7e860a0080164560fc3de790564de986f554562b

tdf#126588 crash in saving view
regression from
    commit d425658bd9fd8e315e4931afb544bc845da0360e
    Date:   Wed Dec 19 16:10:51 2018 +0200
    pass OSQLParseNode around by unique_ptr

We have a OSQLNode being deleted at
connectivity::OSQLParseNodesContainer::clearAndDelete() at
connectivity/source/parse/sqlnode.cxx:2781
connectivity::OSQLParser::parseTree(rtl::OUString&, rtl::OUString
const&, bool)
(this=0x38eba90, rErrorMessage="syntax error, unexpected UNION,
expecting $end", rStatement="CREATE VIEW \"View1\" AS SELECT
\"Tab1\".\"Name\" AS \"Name1\", \"Tab1\".\"Geburtsdatum\" AS
\"Geburtsdatum1\", 1 AS \"MonatZahl1\",
COALESCE(\"Tab1\".\"MonatZaehler\",999) AS \"MonatZaehler1\", 'Januar'
AS \"Monat1\", \"Tab2"..., bInternational=false)
    at workdir/YaccTarget/connectivity/source/parse/sqlbison.cxx:10914
namespace)::parseStatement_throwError(connectivity::OSQLParser&,
rtl::OUString const&,
com::sun::star::uno::Reference<com::sun::star::uno::XInterface> const&)
(_rParser=..., _rStatement="CREATE VIEW \"View1\" AS SELECT
\"Tab1\".\"Name\" AS \"Name1\", \"Tab1\".\"Geburtsdatum\" AS
\"Geburtsdatum1\", 1 AS \"MonatZahl1\",
COALESCE(\"Tab1\".\"MonatZaehler\",999) AS \"MonatZaehler1\", 'Januar'
AS \"Monat1\", \"Tab2"..., _rxContext=uno::Reference to
(dbaccess::OSingleSelectQueryComposer *) 0x38eb8d0)
    at dbaccess/source/core/api/SingleSelectQueryComposer.cxx:106

and then again at
std::unique_ptr<connectivity::OSQLParseNode,
std::default_delete<connectivity::OSQLParseNode>
>::operator=(decltype(nullptr))
(this=0x38eba90) at connectivity/source/parse/sqlnode.cxx:1500

Change-Id: I292627a06369208e0010743063c7eb5a38921a19

connectivity/source/parse/sqlbison.y
@@ -4597,6 +4597,7 @@ std::unique_ptr<OSQLParseNode> OSQLParser::parseTree(OUString& rErrorMessage,
 
 		// clear the garbage collector
 		(*s_pGarbageCollector)->clearAndDelete();
+		m_pParseTree.release(); // because the garbage collector deleted it
 		return nullptr;
 	}
 	else

connectivity/source/parse/sqlnode.cxx
@@ -1293,6 +1293,7 @@ std::unique_ptr<OSQLParseNode> OSQLParser::predicateTree(OUString& rErrorMessage
 
         // clear the garbage collector
         (*s_pGarbageCollector)->clearAndDelete();
+        m_pParseTree.release(); // because the garbage collector deleted it
         return nullptr;
     }
     else