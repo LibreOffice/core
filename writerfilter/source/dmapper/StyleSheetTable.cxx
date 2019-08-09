3b44cae0613d31f88018f119c58b1b70bd2271e2
Justin Luth <justin_luth@sil.org>
2019-08-09 19:16:10 +0300

    tdf#118936 writerfilter: MultiPropertySet retry on exception
    Applying the same logic as in PropertyMap - if the efficient
    multipropertyset fails, then re-apply each property one
    at a time.
    
    The "assert" idea of identifying and fixing invalid properties
    failed spectacularly with RTF - where paragraph properties
    seem to be routinely applied to character styles. No docx
    examples were found - unless they were the novell examples
    which I don't have access to.
    
    Change-Id: I4b733d52de93dfb6beb1da35e0813fd1b0396da7

--- writerfilter/source/dmapper/StyleSheetTable.cxx
+++ writerfilter/source/dmapper/StyleSheetTable.cxx
@@ -861,6 +861,7 @@ public:
     void Insert(const beans::PropertyValue& rVal);
     uno::Sequence< uno::Any > getValues();
     uno::Sequence< OUString > getNames();
+    std::vector<beans::PropertyValue> getProperties() { return m_aValues; };
 };
 
 void PropValVector::Insert(const beans::PropertyValue& rVal)
@@ -1109,8 +1110,19 @@ void StyleSheetTable::ApplyStyleSheets( const FontTablePtr& rFontTable )
                         try
                         {
                             uno::Reference< beans::XMultiPropertySet > xMultiPropertySet( xStyle, uno::UNO_QUERY_THROW);
-                            xMultiPropertySet->setPropertyValues( aSortedPropVals.getNames(), aSortedPropVals.getValues() );
-
+                            try
+                                xMultiPropertySet->setPropertyValues( aSortedPropVals.getNames(), aSortedPropVals.getValues() );
+                            catch ( const uno::Exception& )
+                            {
+                                uno::Reference<beans::XPropertySet> xPropertySet(xStyle, uno::UNO_QUERY_THROW);
+                                for ( const beans::PropertyValue& rValue : aSortedPropVals.getProperties() )
+                                {
+                                    try
+                                       xPropertySet->setPropertyValue( rValue.Name, rValue.Value );
+                                    catch ( const uno::Exception& )
+                                        SAL_WARN( "writerfilter", "StyleSheetTable::ApplyStyleSheets could not set property " << rValue.Name );
+                                }
+                            }
                             // Duplicate MSWord's single footnote reference into Footnote Characters and Footnote anchor
                             if( pEntry->sStyleName.equalsIgnoreAsciiCase("footnote reference")
                                 || pEntry->sStyleName.equalsIgnoreAsciiCase("endnote reference") )
@@ -1135,7 +1147,6 @@ void StyleSheetTable::ApplyStyleSheets( const FontTablePtr& rFontTable )
                                 aMessage += ": " + aUnknownPropertyException.Message;
 
                             SAL_WARN("writerfilter", aMessage);
-                            assert (false && "SERIOUS: remaining alphabetically sorted properties were lost");
 #else
                             (void) rWrapped;
 #endif
