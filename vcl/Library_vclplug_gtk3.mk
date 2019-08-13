23772723c6162a6ae69d0c9fa479c978e6d79ca2
Caolán McNamara <caolanm@redhat.com>
2019-08-13 10:37:50 +0100

    drop gtk2 support
    Change-Id: Ie838cabfecfef7e3225c1555536d5c9cf3b43f15

--- vcl/Library_vclplug_gtk3.mk
+++ vcl/Library_vclplug_gtk3.mk
@@ -46,7 +46,6 @@ $(eval $(call gb_Library_use_sdk_api,vclplug_gtk3))
 
 $(eval $(call gb_Library_add_libs,vclplug_gtk3,\
 	$(GTK3_LIBS) \
-	$(GTHREAD_LIBS) \
 	-lX11 \
 	-lXext \
 	-lSM \
@@ -97,10 +96,13 @@ $(eval $(call gb_Library_add_exception_objects,vclplug_gtk3,\
     vcl/unx/gtk3/a11y/gtk3atkvalue \
     vcl/unx/gtk3/a11y/gtk3atkwindow \
     vcl/unx/gtk3/a11y/gtk3atkwrapper \
+    vcl/unx/gtk3/fpicker/resourceprovider \
+    vcl/unx/gtk3/fpicker/SalGtkFilePicker \
+    vcl/unx/gtk3/fpicker/SalGtkFolderPicker \
+    vcl/unx/gtk3/fpicker/SalGtkPicker \
     vcl/unx/gtk3/gtk3gtkdata \
     vcl/unx/gtk3/gtk3gtkinst \
     vcl/unx/gtk3/gtk3gtksys \
-    vcl/unx/gtk3/gtk3fpicker \
     vcl/unx/gtk3/cairo_gtk3_cairo \
     vcl/unx/gtk3/gtk3gtkprintwrapper \
     vcl/unx/gtk3/gtk3salnativewidgets-gtk \
