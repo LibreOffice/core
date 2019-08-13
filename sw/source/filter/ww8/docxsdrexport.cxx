8866b0dd521deba1e2bb2520394ff10b6fcb1d03
Tamás Zolnai <tamas.zolnai@collabora.com>
2019-08-13 14:27:05 +0200

    Writer: Saving a damaged file as DOCX crashes
    Change-Id: I42de376c69d0b1caa0a03622506d1bf6385ca57e

--- sw/source/filter/ww8/docxsdrexport.cxx
+++ sw/source/filter/ww8/docxsdrexport.cxx
@@ -1383,14 +1383,17 @@ void DocxSdrExport::writeDMLTextFrame(ww8::Frame const* pParentFrame, int nAncho
     // We need to init padding to 0, if it's not set.
     // In LO the default is 0 and so ins attributes are not set when padding is 0
     // but in MSO the default is 254 / 127, so we need to set 0 padding explicitly
-    if (!m_pImpl->getBodyPrAttrList()->hasAttribute(XML_lIns))
-        m_pImpl->getBodyPrAttrList()->add(XML_lIns, OString::number(0));
-    if (!m_pImpl->getBodyPrAttrList()->hasAttribute(XML_tIns))
-        m_pImpl->getBodyPrAttrList()->add(XML_tIns, OString::number(0));
-    if (!m_pImpl->getBodyPrAttrList()->hasAttribute(XML_rIns))
-        m_pImpl->getBodyPrAttrList()->add(XML_rIns, OString::number(0));
-    if (!m_pImpl->getBodyPrAttrList()->hasAttribute(XML_bIns))
-        m_pImpl->getBodyPrAttrList()->add(XML_bIns, OString::number(0));
+    if (m_pImpl->getBodyPrAttrList())
+    {
+        if (!m_pImpl->getBodyPrAttrList()->hasAttribute(XML_lIns))
+            m_pImpl->getBodyPrAttrList()->add(XML_lIns, OString::number(0));
+        if (!m_pImpl->getBodyPrAttrList()->hasAttribute(XML_tIns))
+            m_pImpl->getBodyPrAttrList()->add(XML_tIns, OString::number(0));
+        if (!m_pImpl->getBodyPrAttrList()->hasAttribute(XML_rIns))
+            m_pImpl->getBodyPrAttrList()->add(XML_rIns, OString::number(0));
+        if (!m_pImpl->getBodyPrAttrList()->hasAttribute(XML_bIns))
+            m_pImpl->getBodyPrAttrList()->add(XML_bIns, OString::number(0));
+    }
 
     sax_fastparser::XFastAttributeListRef xBodyPrAttrList(m_pImpl->getBodyPrAttrList());
     m_pImpl->setBodyPrAttrList(nullptr);
