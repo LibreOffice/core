3ccaa17d9df7731014db41157ff4376d6f8c8fd3
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2019-08-14 18:08:18 +0900

    tdf#tdf#122529 support table selection and column/row changing
    Change-Id: Ife7cff14d91ffc84c95c040f0b42319e3d6194b4

--- sw/inc/crsrsh.hxx
+++ sw/inc/crsrsh.hxx
@@ -272,6 +272,7 @@ private:
 
     SAL_DLLPRIVATE const SwRangeRedline* GotoRedline_( SwRedlineTable::size_type nArrPos, bool bSelect );
 
+    SAL_DLLPRIVATE void sendLOKCursorUpdates();
 protected:
 
     inline SwMoveFnCollection const & MakeFindRange( SwDocPositions, SwDocPositions, SwPaM* ) const;
