LibreOffice/core

commit c70b86588fe56f3e1077f3f904c379afda70c6be

Gtk3 inform LO of drag leave
Actually the correct code was added in commit 0b354d18ccfc
("Related: tdf#93054 gtk3: implement enough dnd to be dragged
into.."), but never enabled and then removed in commit c3074cb57843
("vcl: remove dead code). It's at least needed, so D'n'D between
two LO Calc windows correctly removes the pre-drop cell rectangle,
when the mouse leaves the 2nd window without doing the drop.

Commit references:
* 0b354d18ccfc05e7c2582f851d9201e2aa353d7d
* c3074cb5784329eaf558c2f58d96a64085b42d48

Change-Id: I55461f0c87cdef7a03fc261b363dc82642a3933d
Reviewed-on: https://gerrit.libreoffice.org/77568
Reviewed-by: Caolán McNamara <caolanm@redhat.com>
Tested-by: Jenkins
Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>

vcl/unx/gtk3/gtk3gtkframe.cxx
@@ -3610,6 +3610,10 @@ void GtkDropTarget::signalDragLeave(GtkWidget* pWidget, GdkDragContext* /*contex
 {
     m_bInDrag = false;
     gtk_drag_unhighlight(pWidget);
+
+    css::datatransfer::dnd::DropTargetEvent aEvent;
+    aEvent.Source = static_cast<css::datatransfer::dnd::XDropTarget*>(this);
+    fire_dragExit(aEvent);
 }
 
 void GtkSalFrame::signalDestroy( GtkWidget* pObj, gpointer frame )

commit ca80d2ff70426ab823c28dbeec168c056e04973a

Gtk3 defer LO dragExit handling to an idle
LO expects to handle either a drop or an exit event. Quoting the
Gtk+ docs: "Likewise, the “drag-leave” signal is also emitted
before the ::drag-drop signal, for instance to allow cleaning up
of a preview item created in the “drag-motion” signal handler."

Since we don't know if there will be a drop-event following the
leave event, we have to defer the "fire_draExit" to an idle, which
will be removed by the drop event, before it can be processed.

Fixes D'n'D between two Writer windows. For whatever reason it
works correct in Calc, which probably needs further investigation.

Regression from commit c70b86588fe5 ("Gtk3 inform LO of drag
leave").

Change-Id: Ia59dcc8b20efd442ac5efc895602e5cc432cc379
Regression: c70b86588fe56f3e1077f3f904c379afda70c6be

vcl/unx/gtk3/gtk3gtkframe.cxx
@@ -3438,6 +3438,9 @@ gboolean GtkSalFrame::signalDragDrop(GtkWidget* pWidget, GdkDragContext* context
 
 gboolean GtkDropTarget::signalDragDrop(GtkWidget* pWidget, GdkDragContext* context, gint x, gint y, guint time)
 {
+    // remove the deferred dragExit, as we'll do a drop
+    assert(g_idle_remove_by_data(this));
+
     css::datatransfer::dnd::DropTargetDropEvent aEvent;
     aEvent.Source = static_cast<css::datatransfer::dnd::XDropTarget*>(this);
     aEvent.Context = new GtkDropTargetDropContext(context, time);
@@ -3606,14 +3609,24 @@ void GtkSalFrame::signalDragLeave(GtkWidget *pWidget, GdkDragContext *context, g
     pThis->m_pDropTarget->signalDragLeave(pWidget, context, time);
 }
 
+static gboolean lcl_deferred_dragExit(gpointer user_data)
+{
+    GtkDropTarget* pThis = static_cast<GtkDropTarget*>(user_data);
+    css::datatransfer::dnd::DropTargetEvent aEvent;
+    aEvent.Source = static_cast<css::datatransfer::dnd::XDropTarget*>(pThis);
+    pThis->fire_dragExit(aEvent);
+    return FALSE;
+}
+
 void GtkDropTarget::signalDragLeave(GtkWidget* pWidget, GdkDragContext* /*context*/, guint /*time*/)
 {
     m_bInDrag = false;
     gtk_drag_unhighlight(pWidget);
-
-    css::datatransfer::dnd::DropTargetEvent aEvent;
-    aEvent.Source = static_cast<css::datatransfer::dnd::XDropTarget*>(this);
-    fire_dragExit(aEvent);
+    // defer fire_dragExit, since gtk also sends a drag-leave before the drop, while
+    // LO expect to either handle the drop or the exit... at least in Writer.
+    // but since we don't know there will be a drop following the leave, defer the
+    // exit handling to an idle.
+    g_idle_add(lcl_deferred_dragExit, this);
 }
 
 void GtkSalFrame::signalDestroy( GtkWidget* pObj, gpointer frame )