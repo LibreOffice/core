d3c8973f54037f915e12cd038a6a76501d237ea4
Tamás Zolnai <tamas.zolnai@collabora.com>
2019-08-20 15:01:34 +0200

    tdf#126792: DOCX legacy drop-downs are only supposed to hold 25 items, GUI
    Implement GUI part. Do not allow to add more items to the drop-down
    field if the count of items reached the 25 limit.
    
    Change-Id: I126edc500bc18e2f0f4b6864775f89be6801f887
    Reviewed-on: https://gerrit.libreoffice.org/77843
    Tested-by: Jenkins
    Reviewed-by: Tamás Zolnai <tamas.zolnai@collabora.com>

--- sw/source/uibase/inc/DropDownFormFieldDialog.hxx
+++ sw/source/uibase/inc/DropDownFormFieldDialog.hxx
@@ -26,6 +26,7 @@ namespace sw
 class DropDownFormFieldDialog : public weld::GenericDialogController
 {
 private:
+    weld::Widget* const m_pParent;
     mark::IFieldmark* m_pDropDownField;
     bool m_bListHasChanged;
 
