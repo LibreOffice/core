ba58219a05ca5b00eb188f19791a07b5e5b51d56
Khaled Hosny <khaledhosny@eglug.org>
2019-08-20 10:33:40 +0200

    Update HarfBbuzz to 2.6.0
    Change-Id: I7983dd10fe6599a2473caf0da04a0df3e63e9b2a

--- external/harfbuzz/UnpackedTarball_harfbuzz.mk
+++ external/harfbuzz/UnpackedTarball_harfbuzz.mk
@@ -15,10 +15,4 @@ $(eval $(call gb_UnpackedTarball_update_autoconf_configs,harfbuzz))
 
 $(eval $(call gb_UnpackedTarball_set_patchlevel,harfbuzz,0))
 
-# * external/harfbuzz/msvc.patch sent upstream as <https://github.com/harfbuzz/harfbuzz/pull/1605>
-#   "Fix hb_atomic_* variants based on C++11 atomics":
-$(eval $(call gb_UnpackedTarball_add_patches,harfbuzz, \
-    external/harfbuzz/msvc.patch \
-))
-
 # vim: set noet sw=4 ts=4:
