LibreOffice/core

commit 86b14cc8bd910651cef422a0f7408adc0fc51a17

tdf#119617 Fix export of Autofit property of shapes to PPTX
With this patch the "Resize shape to fit text" property
(TextAutoGrowHeight-->spAutofit/noAutofit) will be exported
correctly to PPTX format.

Change-Id: I5fa975c6390a17bad30c360b3b17aa944bbe72af
Reviewed-on: https://gerrit.libreoffice.org/60043
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

oox/source/export/drawingml.cxx
@@ -2469,6 +2469,12 @@ void DrawingML::WriteText( const Reference< XInterface >& rXIface, const OUStrin
                 mpFS->singleElementNS(XML_a, XML_normAutofit, XML_fontScale,
                     ( nFontScale < MAX_SCALE_VAL && nFontScale > 0 ) ? I32S(nFontScale) : nullptr, FSEND);
             }
+            else
+            {
+                bool bTextAutoGrowHeight = false;
+                GET(bTextAutoGrowHeight, TextAutoGrowHeight);
+                mpFS->singleElementNS(XML_a, (bTextAutoGrowHeight ? XML_spAutoFit : XML_noAutofit), FSEND);
+            }
         }
         mpFS->endElementNS((nXmlNamespace ? nXmlNamespace : XML_a), XML_bodyPr);
     }

sd/qa/unit/export-tests-ooxml2.cxx
@@ -172,6 +172,7 @@ public:
     void testTdf107608();
     void testTdf111786();
     void testFontScale();
+    void testShapeAutofitPPTX();
     void testTdf115394();
     void testTdf115394Zero();
     void testTdf115005();
@@ -251,6 +252,7 @@ public:
     CPPUNIT_TEST(testTdf107608);
     CPPUNIT_TEST(testTdf111786);
     CPPUNIT_TEST(testFontScale);
+    CPPUNIT_TEST(testShapeAutofitPPTX);
     CPPUNIT_TEST(testTdf115394);
     CPPUNIT_TEST(testTdf115394Zero);
     CPPUNIT_TEST(testTdf115005);
@@ -1555,6 +1557,20 @@ void SdOOXMLExportTest2::testFontScale()
         CPPUNIT_ASSERT_EQUAL(OUString("73000"), sScale);
 
     xDocShRef->DoClose();
+}
+
+void SdOOXMLExportTest2::testShapeAutofitPPTX()
+{
+    sd::DrawDocShellRef xDocShRef = loadURL(m_directories.getURLFromSrc("/sd/qa/unit/data/pptx/testShapeAutofit.pptx"), PPTX);
+    utl::TempFile tempFile;
+    xDocShRef = saveAndReload(xDocShRef.get(), PPTX, &tempFile);
+    xmlDocPtr pXmlDocContent = parseExport(tempFile, "ppt/slides/slide1.xml");
+    CPPUNIT_ASSERT(pXmlDocContent);
+
+    // TextAutoGrowHeight --> "Resize shape to fit text" --> true
+    assertXPath(pXmlDocContent, "/p:sld/p:cSld/p:spTree/p:sp[1]/p:txBody/a:bodyPr/a:spAutoFit", 1);
+    // TextAutoGrowHeight --> "Resize shape to fit text" --> false
+    assertXPath(pXmlDocContent, "/p:sld/p:cSld/p:spTree/p:sp[2]/p:txBody/a:bodyPr/a:noAutofit", 1);
 }
 
 void SdOOXMLExportTest2::testTdf115394()

commit 46e7fabddeb28e812c2e4d5f3000a7f203addaa5

tdf#127030 PPTX Shape Export: fix legacy shapes size
Classical/legacy shapes do not obey the TextAutoGrowHeight and
"Resize shape to fit text"options.
Regression from commit: 86b14cc8bd910651cef422a0f7408adc0fc51a17
(tdf#119617 Fix export of Autofit property of shapes to PPTX)

Change-Id: Iab67db113edc61f4c94a89ec1bb8df0736c26f21

oox/source/export/drawingml.cxx
@@ -2786,8 +2786,9 @@ void DrawingML::WriteText( const Reference< XInterface >& rXIface, const OUStrin
             }
             else
             {
+                // tdf#127030: Only custom shapes obey the TextAutoGrowHeight option.
                 bool bTextAutoGrowHeight = false;
-                if (GetProperty(rXPropSet, "TextAutoGrowHeight"))
+                if (dynamic_cast<SvxCustomShape*>(rXIface.get()) && GetProperty(rXPropSet, "TextAutoGrowHeight"))
                     mAny >>= bTextAutoGrowHeight;
                 mpFS->singleElementNS(XML_a, (bTextAutoGrowHeight ? XML_spAutoFit : XML_noAutofit));
             }

sd/qa/unit/export-tests-ooxml2.cxx
@@ -180,6 +180,7 @@ public:
     void testTdf111786();
     void testFontScale();
     void testShapeAutofitPPTX();
+    void testLegacyShapeAutofitPPTX();
     void testTdf115394();
     void testTdf115394Zero();
     void testTdf115005();
@@ -274,6 +275,7 @@ public:
     CPPUNIT_TEST(testTdf111786);
     CPPUNIT_TEST(testFontScale);
     CPPUNIT_TEST(testShapeAutofitPPTX);
+    CPPUNIT_TEST(testLegacyShapeAutofitPPTX);
     CPPUNIT_TEST(testTdf115394);
     CPPUNIT_TEST(testTdf115394Zero);
     CPPUNIT_TEST(testTdf115005);
@@ -1626,6 +1628,22 @@ void SdOOXMLExportTest2::testShapeAutofitPPTX()
     assertXPath(pXmlDocContent, "/p:sld/p:cSld/p:spTree/p:sp[1]/p:txBody/a:bodyPr/a:spAutoFit", 1);
     // TextAutoGrowHeight --> "Resize shape to fit text" --> false
     assertXPath(pXmlDocContent, "/p:sld/p:cSld/p:spTree/p:sp[2]/p:txBody/a:bodyPr/a:noAutofit", 1);
+}
+
+void SdOOXMLExportTest2::testLegacyShapeAutofitPPTX()
+{
+    sd::DrawDocShellRef xDocShRef = loadURL(m_directories.getURLFromSrc("/sd/qa/unit/data/odp/testLegacyShapeAutofit.odp"), ODP);
+    utl::TempFile tempFile;
+    xDocShRef = saveAndReload(xDocShRef.get(), PPTX, &tempFile);
+    xmlDocPtr pXmlDocContent = parseExport(tempFile, "ppt/slides/slide1.xml");
+    CPPUNIT_ASSERT(pXmlDocContent);
+
+    // Text in a legacy rectangle
+    assertXPath(pXmlDocContent, "/p:sld/p:cSld/p:spTree/p:sp[1]/p:txBody/a:bodyPr/a:noAutofit", 1);
+    // Text in (closed) Polygon
+    assertXPath(pXmlDocContent, "/p:sld/p:cSld/p:spTree/p:sp[2]/p:txBody/a:bodyPr/a:noAutofit", 1);
+    // Text in a legacy ellipse
+    assertXPath(pXmlDocContent, "/p:sld/p:cSld/p:spTree/p:sp[3]/p:txBody/a:bodyPr/a:noAutofit", 1);
 }
 
 void SdOOXMLExportTest2::testTdf115394()