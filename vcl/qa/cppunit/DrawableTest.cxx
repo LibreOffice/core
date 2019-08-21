ffe45297918eec29d8e7cb84017ad52bd8678f40
Chris Sherlock <chris.sherlock79@gmail.com>
2019-08-22 09:40:09 +1000

    vcl: introduce OutputDevice::Draw() function
    OutputDevice::Draw() is a command object pattern, which decouples the
    drawing logic from OutpuDevice. It calls upon a Drawable command
    object.
    
    Also, each time we draw to an OutputDevice, we follow the same basic
    algorithm:
    
    1. Add action to GDI metafile
    2. Can we draw? No - exit
    3. Initialize the clipping region if possible
    4. Initialize the object's color
    5. Initialize the object's fill color
    6. Acquire the SalGraphics instance
    7. Do the actual drawing!
    8. Paint alpha virtual device
    
    The only thing that varies is step 7 - the actual drawing. Thus I
    now provide overridable functions that provide default behaviour,
    an abstract function DrawCommand where the drawing is implemented,
    and I make execute run these steps in the right order.
    
    Now all someone who wants to implement a Drawable object has to do is
    to override the functions they wish to vary, and perform their
    drawing in DrawCommand. They no longer have to provide scaffolding
    to make it work!
    
    Things to note:
    - each of these steps (except the actual drawing) may be optional,
      so I have put an overridable check function for each
    - on some occassions we rely on the underlying command object to
      undertake all these steps and we don't want to dupliate them - so
      I provide an option to disable these scaffolding steps entirely
    
    Change-Id: If7b58aaef4e64dc2047c0f804abd1848d367a6c2

---
+++ vcl/qa/cppunit/DrawableTest.cxx
@@ -0,0 +1,153 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <config_features.h>
+
+#include <cppunit/TestAssert.h>
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/plugin/TestPlugIn.h>
+
+#include <sal/log.hxx>
+
+#include <vcl/drawables/Drawable.hxx>
+#include <vcl/virdev.hxx>
+
+enum EnumScaffolding
+{
+    Scaffolding,
+    NoScaffolding
+};
+
+namespace vcl
+{
+class FakeDrawable : public Drawable
+{
+public:
+    FakeDrawable(EnumScaffolding eScaffolding)
+        : Drawable(eScaffolding == Scaffolding ? true : false)
+        , meScaffolding(eScaffolding)
+        , mbDidAddAction(false)
+        , mbDidInitClipRegion(false)
+        , mbDidInitLineColor(false)
+        , mbDidInitFillColor(false)
+        , mbDidUseAlpha(false)
+    {
+    }
+
+    bool DidAddAction() { return mbDidAddAction; }
+    bool DidInitClipRegion() { return mbDidInitClipRegion; }
+    bool DidInitLineColor() { return mbDidInitLineColor; }
+    bool DidInitFillColor() { return mbDidInitFillColor; }
+    bool DidUseAlpha() { return mbDidUseAlpha; }
+
+protected:
+    bool ShouldAddAction() const override;
+    bool CanDraw(OutputDevice*) const override;
+    bool ShouldInitClipRegion() const override;
+    bool ShouldInitLineColor() const override;
+    bool ShouldInitFillColor() const override;
+    bool UseAlphaVirtDev() const override;
+
+    bool DrawCommand(OutputDevice* const) const override;
+
+private:
+    EnumScaffolding meScaffolding;
+
+    mutable bool mbDidAddAction;
+    mutable bool mbDidInitClipRegion;
+    mutable bool mbDidInitLineColor;
+    mutable bool mbDidInitFillColor;
+    mutable bool mbDidUseAlpha;
+};
+
+bool FakeDrawable::DrawCommand(OutputDevice* const) const { return true; }
+
+bool FakeDrawable::ShouldAddAction() const
+{
+    mbDidAddAction = (meScaffolding == Scaffolding);
+    return true;
+}
+
+bool FakeDrawable::CanDraw(OutputDevice*) const { return true; }
+
+bool FakeDrawable::ShouldInitClipRegion() const
+{
+    mbDidInitClipRegion = (meScaffolding == Scaffolding);
+    return true;
+}
+
+bool FakeDrawable::ShouldInitLineColor() const
+{
+    mbDidInitLineColor = (meScaffolding == Scaffolding);
+    return true;
+}
+
+bool FakeDrawable::ShouldInitFillColor() const
+{
+    mbDidInitFillColor = (meScaffolding == Scaffolding);
+    return true;
+}
+
+bool FakeDrawable::UseAlphaVirtDev() const
+{
+    mbDidUseAlpha = (meScaffolding == Scaffolding);
+    return true;
+}
+
+} // namespace vcl
+
+namespace
+{
+class DrawableTest : public CppUnit::TestFixture
+{
+    void testScaffolding();
+    void testNoScaffolding();
+
+    CPPUNIT_TEST_SUITE(DrawableTest);
+    CPPUNIT_TEST(testScaffolding);
+    CPPUNIT_TEST(testNoScaffolding);
+    CPPUNIT_TEST_SUITE_END();
+};
+
+void DrawableTest::testScaffolding()
+{
+    VirtualDevice aRenderContext;
+    vcl::FakeDrawable aFakeDrawable(EnumScaffolding::Scaffolding);
+
+    aRenderContext.Draw(aFakeDrawable);
+
+    CPPUNIT_ASSERT_MESSAGE("Did not add action", aFakeDrawable.DidAddAction());
+    CPPUNIT_ASSERT_MESSAGE("Did not initialize line color", aFakeDrawable.DidInitLineColor());
+    CPPUNIT_ASSERT_MESSAGE("Did not initialize fill color", aFakeDrawable.DidInitFillColor());
+    CPPUNIT_ASSERT_MESSAGE("Did not initialize clipping region", aFakeDrawable.DidInitClipRegion());
+    CPPUNIT_ASSERT_MESSAGE("Did not use alpha VirtualDevice", aFakeDrawable.DidUseAlpha());
+}
+
+void DrawableTest::testNoScaffolding()
+{
+    VirtualDevice aRenderContext;
+    vcl::FakeDrawable aFakeDrawable(EnumScaffolding::NoScaffolding);
+
+    aRenderContext.Draw(aFakeDrawable);
+
+    CPPUNIT_ASSERT_MESSAGE("Added action", !aFakeDrawable.DidAddAction());
+    CPPUNIT_ASSERT_MESSAGE("Initialized line color", !aFakeDrawable.DidInitLineColor());
+    CPPUNIT_ASSERT_MESSAGE("Initialized fill color", !aFakeDrawable.DidInitFillColor());
+    CPPUNIT_ASSERT_MESSAGE("Initialized clipping region", !aFakeDrawable.DidInitClipRegion());
+    CPPUNIT_ASSERT_MESSAGE("Used alpha VirtualDevice", !aFakeDrawable.DidUseAlpha());
+}
+
+} // namespace
+
+CPPUNIT_TEST_SUITE_REGISTRATION(DrawableTest);
+
+CPPUNIT_PLUGIN_IMPLEMENT();
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
