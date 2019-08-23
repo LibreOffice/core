3780d305ae1607458029af3c9452bad251490461
Tomáš Chvátal <tchvatal@suse.com>
2019-08-23 09:44:01 +0200

    Fix old boost build for good
    With the previous approach the code could lead to crashes in
    the flowfrm.cxx
    
    Change-Id: I3b56ed46db9d37a606a1cd793a20b8aff22db6e2
    Reviewed-on: https://gerrit.libreoffice.org/78001
    Reviewed-by: Tomáš Chvátal <tchvatal@suse.com>
    Tested-by: Tomáš Chvátal <tchvatal@suse.com>
    Reviewed-by: Michael Stahl <Michael.Stahl@cib.de>

--- sw/source/core/layout/flowfrm.cxx
+++ sw/source/core/layout/flowfrm.cxx
@@ -2517,7 +2517,11 @@ bool SwFlowFrame::MoveBwd( bool &rbReformat )
 
         {
             auto const pOld = m_rThis.GetUpper();
+#if BOOST_VERSION < 105600
+            std::list<SwFrameDeleteGuard> g;
+#else
             ::boost::optional<SwFrameDeleteGuard> g;
+#endif
             if (m_rThis.GetUpper()->IsCellFrame())
             {
                 // note: IsFollowFlowRow() is never set for new-style tables
@@ -2528,7 +2532,7 @@ bool SwFlowFrame::MoveBwd( bool &rbReformat )
                 {
                     // lock follow-flow-row (similar to sections above)
 #if BOOST_VERSION < 105600
-                    g.reset(SwFrameDeleteGuard(m_rThis.GetUpper()->GetUpper()));
+                    g.emplace_back(m_rThis.GetUpper()->GetUpper());
 #else
                     g.emplace(m_rThis.GetUpper()->GetUpper());
 #endif
