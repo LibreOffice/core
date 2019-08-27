LibreOffice/core

commit df905f41a33d0d7d2cde7fad80c5b3dc3c837c10

loplugin:constmethod in sfx2
Change-Id: Ic129a8e136dc3f3b0a36fdb83e7aa71027ada59a
Reviewed-on: https://gerrit.libreoffice.org/78190
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sfx2/source/appl/app.cxx
@@ -520,7 +520,7 @@ SfxApplication::ChooseScript(weld::Window *pParent)
     return aScriptURL;
 }
 
-void SfxApplication::MacroOrganizer(weld::Window* pParent, sal_Int16 nTabId)
+void SfxApplication::MacroOrganizer(const weld::Window* pParent, sal_Int16 nTabId)
 {
 #if !HAVE_FEATURE_SCRIPTING
     (void) pParent;

include/sfx2/app.hxx
@@ -146,7 +146,7 @@ public:
     // Basic/Scripting
     static bool                 IsXScriptURL( const OUString& rScriptURL );
     static OUString             ChooseScript(weld::Window *pParent);
-    static void                 MacroOrganizer(weld::Window* pParent, sal_Int16 nTabId);
+    static void                 MacroOrganizer(const weld::Window* pParent, sal_Int16 nTabId);
     static ErrCode              CallBasic( const OUString&, BasicManager*, SbxArray *pArgs, SbxValue *pRet );
     static ErrCode              CallAppBasic( const OUString& i_macroName )
                                 { return CallBasic( i_macroName, SfxApplication::GetBasicManager(), nullptr, nullptr ); }

include/sfx2/brokenpackageint.hxx
@@ -37,7 +37,7 @@ public:
     ~RequestPackageReparation();
     RequestPackageReparation( const RequestPackageReparation&) = delete;
     RequestPackageReparation& operator=( const RequestPackageReparation&) = delete;
-    bool    isApproved();
+    bool    isApproved() const;
     css::uno::Reference < css::task::XInteractionRequest > GetRequest();
 };
 

include/sfx2/checkin.hxx
@@ -24,8 +24,8 @@ public:
     SfxCheckinDialog(weld::Window* pParent);
     virtual ~SfxCheckinDialog() override;
 
-    OUString GetComment();
-    bool IsMajor();
+    OUString GetComment() const;
+    bool IsMajor() const;
 };
 
 #endif

include/sfx2/childwin.hxx
@@ -171,8 +171,8 @@ public:
     void                SetAlignment(SfxChildAlignment eAlign);
     virtual void        Hide();
     virtual void        Show( ShowFlags nFlags );
-    sal_uInt16          GetPosition();
-    sal_uInt16          GetType()
+    sal_uInt16          GetPosition() const;
+    sal_uInt16          GetType() const
                         { return nType; }
 
     void                CreateContext( sal_uInt16 nContextId, SfxBindings& );
@@ -197,7 +197,7 @@ public:
     bool                WantsFocus() const;
 
     virtual bool        QueryClose();
-    const css::uno::Reference< css::frame::XFrame >&   GetFrame();
+    const css::uno::Reference< css::frame::XFrame >&   GetFrame() const;
     void                SetFrame( const css::uno::Reference< css::frame::XFrame > & );
 
     SAL_DLLPRIVATE static void InitializeChildWinFactory_Impl(sal_uInt16, SfxChildWinInfo&);

include/sfx2/classificationhelper.hxx
@@ -56,7 +56,7 @@ public:
     SfxClassificationHelper(const css::uno::Reference<css::document::XDocumentProperties>& xDocumentProperties, bool bUseLocalizedPolicy = true);
     ~SfxClassificationHelper();
     /// Get the currently selected category for eType.
-    const OUString& GetBACName(SfxClassificationPolicyType eType);
+    const OUString& GetBACName(SfxClassificationPolicyType eType) const;
     /// Return all possible valid category names, based on the policy.
     std::vector<OUString> GetBACNames();
     /// Return all possible valid category identifiers, based on the policy.
@@ -85,9 +85,9 @@ public:
     bool HasDocumentFooter();
     void UpdateInfobar(SfxViewFrame& rViewFrame);
 
-    std::vector<OUString> const & GetMarkings();
-    std::vector<OUString> const & GetIntellectualPropertyParts();
-    std::vector<OUString> const & GetIntellectualPropertyPartNumbers();
+    std::vector<OUString> const & GetMarkings() const;
+    std::vector<OUString> const & GetIntellectualPropertyParts() const;
+    std::vector<OUString> const & GetIntellectualPropertyPartNumbers() const;
 
     /// Does a best-effort conversion of rType to SfxClassificationPolicyType.
     static SfxClassificationPolicyType stringToPolicyType(const OUString& rType);

include/sfx2/dinfdlg.hxx
@@ -340,7 +340,7 @@ struct CustomPropertyLine
     DECL_LINK(EditLoseFocusHdl, weld::Widget&, void);
     DECL_LINK(BoxLoseFocusHdl, weld::Widget&, void);
 
-    void DoTypeHdl(weld::ComboBox& rBox);
+    void DoTypeHdl(const weld::ComboBox& rBox);
 
     void Clear();
     void Hide();
@@ -401,7 +401,7 @@ public:
 
     void                BoxLoseFocus(CustomPropertyLine* pLine);
     void                EditLoseFocus(CustomPropertyLine* pLine);
-    void                Remove(CustomPropertyLine* pLine);
+    void                Remove(const CustomPropertyLine* pLine);
 };
 
 // class CustomPropertiesControl -----------------------------------------

include/sfx2/docfile.hxx
@@ -142,7 +142,7 @@ public:
     ErrCode             GetErrorCode() const;
     ErrCode             GetError() const
                         { return GetErrorCode().IgnoreWarning(); }
-    ErrCode const &     GetLastStorageCreationState();
+    ErrCode const &     GetLastStorageCreationState() const;
 
     void                SetError(ErrCode nError);
 
@@ -203,8 +203,8 @@ public:
 
     OUString            GetBaseURL( bool bForSaving=false );
     void                SetInCheckIn( bool bInCheckIn );
-    bool                IsInCheckIn( );
-    bool                IsSkipImages( );
+    bool                IsInCheckIn( ) const;
+    bool                IsSkipImages( ) const;
 
     SAL_DLLPRIVATE bool HasStorage_Impl() const;
 
@@ -238,7 +238,7 @@ public:
 
     SAL_DLLPRIVATE void SetLongName(const OUString &rName);
     SAL_DLLPRIVATE const OUString & GetLongName() const;
-    SAL_DLLPRIVATE bool IsPreview_Impl();
+    SAL_DLLPRIVATE bool IsPreview_Impl() const;
     SAL_DLLPRIVATE void ClearBackup_Impl();
     SAL_DLLPRIVATE void SetPhysicalName_Impl(const OUString& rName);
     SAL_DLLPRIVATE void CanDisposeStorage_Impl( bool bDisposeStorage );
@@ -279,7 +279,7 @@ public:
     // the following two methods must be used and make sense only during saving currently
     // TODO/LATER: in future the signature state should be controlled by the medium not by the document
     //             in this case the methods will be used generally, and might need to be renamed
-    SAL_DLLPRIVATE SignatureState GetCachedSignatureState_Impl();
+    SAL_DLLPRIVATE SignatureState GetCachedSignatureState_Impl() const;
     SAL_DLLPRIVATE void       SetCachedSignatureState_Impl( SignatureState nState );
 
     void SetHasEmbeddedObjects(bool bHasEmbeddedObjects);

commit 7f797f0cf49d680a70c35eef08dc312bb6b4065e

Fix other signature of basicide_macro_organizer
...after 97710225f015c8716c80e55ecdb78832f2edc731 "sfx2: fix DISABLE_DYNLOADING
build" had changed it to 'void const *' but the concurrently submitted
036e635678aedc414e2d55217e4f7f8359ed1b9a "Fix signature of
basicide_macro_organizer" fixed the underlying issue that had been introduced
with df905f41a33d0d7d2cde7fad80c5b3dc3c837c10 "loplugin:constmethod in sfx2" and
had caused DISABLE_DYNLOADING builds to fail

Change-Id: If5d8c3289d7c77f5e6526bf7f4e0c48292dc5f71

sfx2/source/appl/app.cxx
@@ -421,7 +421,7 @@ extern "C" { static void thisModule() {} }
 #else
 
 extern "C" long basicide_handle_basic_error(void const*);
-extern "C" void basicide_macro_organizer(void const*, sal_Int16);
+extern "C" void basicide_macro_organizer(void*, sal_Int16);
 
 #endif
 