45ee0f7b300a67fbec0fabbbc7b8621304b4c14b
Juergen Funk <juergen.funk_ml@cib.de>
2019-08-27 09:22:17 +0200

    Printing in main thread
    the wollmux has change the dialogs form swing to the LO-Dialogs,
    and when printig with MS-PDF then LO hang on this line.
    The reason is wollmux run in it own thread, but this thread has
    no win-message-bump, when the printer-driver would open the
    FileSave-Dialog then it hangs, because the message-bump
    is missing in this thread.
    
    I belive, the swing has made it own win-message-bump for this
    thread.
    
    Change-Id: Ia5131464dcc5f166133a3302d29550e945dd1026

--- vcl/win/gdi/salprn.cxx
+++ vcl/win/gdi/salprn.cxx
@@ -52,6 +52,8 @@
 #include <com/sun/star/lang/XInitialization.hpp>
 #include <comphelper/processfactory.hxx>
 
+#include <vcl/threadex.hxx>
+
 #include <malloc.h>
 
 #include <winspool.h>
@@ -1502,8 +1504,8 @@ bool WinSalPrinter::StartJob( const OUString* pFileName,
     else
         aInfo.lpszOutput = nullptr;
 
-    // start Job
-    int nRet = lcl_StartDocW( hDC, &aInfo, this );
+    // start Job, in the main thread
+    int nRet = vcl::solarthread::syncExecute([hDC, this, &aInfo]() -> int { return lcl_StartDocW(hDC, &aInfo, this); });
 
     if ( nRet <= 0 )
     {
