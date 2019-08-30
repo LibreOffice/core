7a6f4c19dea65686b68d8e326fe53dc665954854
Rene Engelhard <rene@debian.org>
2019-08-30 21:00:55 +0200

    add -latomic configure check...
    and move the flag to com_GCC_defs.mk
    
    (copied from https://github.com/zelcash/zelcash/blob/master/build-aux/m4/l_atomic.m4)
    
    Follow-up to 2b1ce38f0d1feeec11e6c45b03eebeddb2f571ee and
    b11763dbaa0c7f427ea47abe9b98995cb49a8595 to generalize it
    
    Change-Id: I8879a72d730cc08a72c2d8b132ff9f5d2efe7b9f

--- solenv/gbuild/platform/LINUX_GODSON_GCC.mk
+++ solenv/gbuild/platform/LINUX_GODSON_GCC.mk
@@ -13,6 +13,4 @@ gb_COMPILEROPTFLAGS := -Os
 
 include $(GBUILDDIR)/platform/linux.mk
 
-gb_LinkTarget_LDFLAGS += -Wl,-no-as-needed -latomic -Wl,--as-needed
-
 # vim: set noet sw=4:
--- solenv/gbuild/platform/LINUX_POWERPC_GCC.mk
+++ solenv/gbuild/platform/LINUX_POWERPC_GCC.mk
@@ -12,6 +12,4 @@ gb_CPUDEFS += -DPPC
 
 include $(GBUILDDIR)/platform/linux.mk
 
-gb_LinkTarget_LDFLAGS += -Wl,--no-as-needed -latomic -Wl,--as-needed
-
 # vim: set noet sw=4:
