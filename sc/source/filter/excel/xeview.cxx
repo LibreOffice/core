LibreOffice/core

commit 3707e2a7b8b05617b0afb75374f866c967593861

tdf#122624 XLSX OLE in DOCX: export view positions
of the visible sheet of an embedded spreadsheet,
instead of exporting always the first column and row.

Change-Id: Iaf87bfc5b7658cd4d64cb9c83c50bbf66fd6e7f0
Reviewed-on: https://gerrit.libreoffice.org/67982
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

sc/source/filter/excel/xeview.cxx
@@ -419,6 +419,20 @@ void XclExpTabViewSettings::SaveXml( XclExpXmlStream& rStrm )
 {
     sax_fastparser::FSHelperPtr& rWorksheet = rStrm.GetCurrentStream();
     rWorksheet->startElement( XML_sheetViews, FSEND );
+
+    // handle missing viewdata at embedded XLSX OLE objects
+    if (maData.mbSelected)
+    {
+        SCCOL nPosLeft = rStrm.GetRoot().GetDoc().GetPosLeft();
+        SCROW nPosTop = rStrm.GetRoot().GetDoc().GetPosTop();
+        if (nPosLeft > 0 || nPosTop > 0)
+        {
+            ScAddress aLeftTop(nPosLeft, nPosTop, 0);
+            XclExpAddressConverter& rAddrConv = GetAddressConverter();
+            maData.maFirstXclPos = rAddrConv.CreateValidAddress( aLeftTop, false );
+        }
+    }
+
     rWorksheet->startElement( XML_sheetView,
             // OOXTODO: XML_windowProtection,
             XML_showFormulas,               ToPsz( maData.mbShowFormulas ),

sc/source/ui/unoobj/docuno.cxx
@@ -2443,12 +2443,18 @@ uno::Reference< container::XIndexAccess > SAL_CALL ScModelObj::getViewData(  )
             xRet.set( xCont, uno::UNO_QUERY_THROW );
 
             uno::Sequence< beans::PropertyValue > aSeq;
-            aSeq.realloc(1);
+            aSeq.realloc(3);
             OUString sName;
             pDocShell->GetDocument().GetName( pDocShell->GetDocument().GetVisibleTab(), sName );
             OUString sOUName(sName);
             aSeq[0].Name = SC_ACTIVETABLE;
             aSeq[0].Value <<= sOUName;
+            SCCOL nPosLeft = pDocShell->GetDocument().GetPosLeft();
+            aSeq[1].Name = SC_POSITIONLEFT;
+            aSeq[1].Value <<= nPosLeft;
+            SCROW nPosTop = pDocShell->GetDocument().GetPosTop();
+            aSeq[2].Name = SC_POSITIONTOP;
+            aSeq[2].Value <<= nPosTop;
             xCont->insertByIndex( 0, uno::makeAny( aSeq ) );
         }
     }

sw/qa/extras/ooxmlexport/ooxmlexport12.cxx
@@ -967,6 +967,7 @@ DECLARE_OOXMLEXPORT_TEST(testTdf122594, "tdf122594.docx")
     CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(1), xIndexAccess->getCount());
 
     uno::Sequence<beans::PropertyValue> aSeq;
+    sal_Int32 nCheck = 0;
     if (xIndexAccess->getByIndex(0) >>= aSeq)
     {
         sal_Int32 nCount(aSeq.getLength());
@@ -977,11 +978,31 @@ DECLARE_OOXMLEXPORT_TEST(testTdf122594, "tdf122594.docx")
             {
                 OUString sTabName;
                 if (aSeq[i].Value >>= sTabName)
+                {
                     // Sheet2, not Sheet1
                     CPPUNIT_ASSERT_EQUAL(OUString("Munka2"), sTabName);
+                    nCheck++;
+                }
+            }
+            // tdf#122624 column and row viewarea positions
+            else if (sName == "PositionLeft")
+            {
+                sal_Int32 nPosLeft;
+                aSeq[i].Value >>= nPosLeft;
+                CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(1), nPosLeft);
+                nCheck++;
+            }
+            else if (sName == "PositionTop")
+            {
+                sal_Int32 nPosTop;
+                aSeq[i].Value >>= nPosTop;
+                CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(1), nPosTop);
+                nCheck++;
             }
         }
     }
+
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(3), nCheck);
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();

commit 898a665cb7d8f8b2a95df5deee046ecc0578e719

tdf#127110 XLSX export: fix bad view position
Update SheetView attribute topLeftCell only
in the case of embedded OLE sheets.

Regression from commit 3707e2a7b8b05617b0afb75374f866c967593861
"tdf#122624 XLSX OLE in DOCX: export view positions".

Change-Id: I9ea57ff1531d710bff34df12fb7e0bb1e3edbea5

sc/source/filter/excel/xeview.cxx
@@ -285,7 +285,8 @@ sal_uInt16 lclGetXclZoom( long nScZoom, sal_uInt16 nDefXclZoom )
 
 XclExpTabViewSettings::XclExpTabViewSettings( const XclExpRoot& rRoot, SCTAB nScTab ) :
     XclExpRoot( rRoot ),
-    mnGridColorId( XclExpPalette::GetColorIdFromIndex( EXC_COLOR_WINDOWTEXT ) )
+    mnGridColorId( XclExpPalette::GetColorIdFromIndex( EXC_COLOR_WINDOWTEXT ) ),
+    mbHasTabSettings(false)
 {
     // *** sheet flags ***
 
@@ -304,6 +305,7 @@ XclExpTabViewSettings::XclExpTabViewSettings( const XclExpRoot& rRoot, SCTAB nSc
 
     if( const ScExtTabSettings* pTabSett = GetExtDocOptions().GetTabSettings( nScTab ) )
     {
+        mbHasTabSettings = true;
         const ScExtTabSettings& rTabSett = *pTabSett;
         XclExpAddressConverter& rAddrConv = GetAddressConverter();
 
@@ -419,7 +421,7 @@ void XclExpTabViewSettings::SaveXml( XclExpXmlStream& rStrm )
     rWorksheet->startElement(XML_sheetViews);
 
     // handle missing viewdata at embedded XLSX OLE objects
-    if (maData.mbSelected)
+    if( !mbHasTabSettings && maData.mbSelected )
     {
         SCCOL nPosLeft = rStrm.GetRoot().GetDoc().GetPosLeft();
         SCROW nPosTop = rStrm.GetRoot().GetDoc().GetPosTop();

sc/source/filter/inc/xeview.hxx
@@ -159,6 +159,7 @@ private:
 private:
     XclTabViewData      maData;             /// All view settings for a sheet.
     sal_uInt32          mnGridColorId;      /// Color identifier for grid color.
+    bool                mbHasTabSettings;   /// It's false for embedded OLE spreadsheets.
 };
 
 #endif