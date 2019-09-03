LibreOffice/core

commit 23c0b3e13f88d6d71d528777d4f86f50c0766672

tdf#107074 uncommenting createLabels for the axis fixes the issue
Quick fix for this bug seems to be to call createLabels again
after creating shapes. The code to position axis label seems to
be quite messy it would be better to greatly refactor the code if
we want to do any enhancement to this code in the future.

Change-Id: I2bc2c3d503944002c538d2d2a5119102c927f3b7
Reviewed-on: https://gerrit.libreoffice.org/37122
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

chart2/source/view/axes/VCartesianAxis.cxx
@@ -1846,7 +1846,7 @@ void VCartesianAxis::createShapes()
         }
     }
 
-    //createLabels();
+    createLabels();
 }
 
 } //namespace chart

commit 42fe488f92f5319d007528198c835f1fb87421b6

tdf#127304 Chart: fix multilevel axis labels rotation
If the first level of horizontal category axis labels
are vertically rotated, then other levels should not be
rotated.

Regression from commit: 23c0b3e13f88d6d71d528777d4f86f50c0766672
(tdf#107074 uncommenting createLabels for the axis fixes the issue)

Change-Id: Ia1978fdc345fa154cff2ea03d2a94475f73398a6

chart2/source/view/axes/VCartesianAxis.cxx
@@ -757,8 +757,8 @@ bool VCartesianAxis::createTextShapes(
         }
     }
 
-     // Stores an array of text label strings in case of a normal
-     // (non-complex) category axis.
+    // Stores an array of text label strings in case of a normal
+    // (non-complex) category axis.
     const uno::Sequence<OUString>* pCategories = nullptr;
     if( m_bUseTextLabels && !m_aAxisProperties.m_bComplexCategories )
         pCategories = &m_aTextLabels;
@@ -1680,10 +1680,12 @@ void VCartesianAxis::createLabels()
             {
                 aComplexProps.bLineBreakAllowed = true;
                 aComplexProps.bOverlapAllowed = aComplexProps.fRotationAngleDegree != 0.0;
-                //Only the first level of complex vertical category axis labels orientation should be horizontal
-                if( nTextLevel > 0 && m_aAxisProperties.m_bSwapXAndY )
+                if( nTextLevel > 0 )
                 {
-                    aComplexProps.fRotationAngleDegree = 90.0;
+                    if( m_aAxisProperties.m_bSwapXAndY )
+                        aComplexProps.fRotationAngleDegree = 90.0;
+                    else
+                        aComplexProps.fRotationAngleDegree = 0.0;
                 }
             }
             AxisLabelProperties& rAxisLabelProperties =  m_aAxisProperties.m_bComplexCategories ? aComplexProps : m_aAxisLabelProperties;

sw/qa/extras/layout/layout.cxx
@@ -2320,6 +2320,27 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf126244)
                 "900");
 }
 
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf127304)
+{
+    SwDoc* pDoc = createDoc("tdf127304.odt");
+    SwDocShell* pShell = pDoc->GetDocShell();
+
+    // Dump the rendering of the first page as an XML file.
+    std::shared_ptr<GDIMetaFile> xMetaFile = pShell->GetPreviewMetaFile();
+    MetafileXmlDump dumper;
+    xmlDocPtr pXmlDoc = dumpAndParse(dumper, *xMetaFile);
+    CPPUNIT_ASSERT(pXmlDoc);
+    // Test the first level of horizontal category axis labels orientation. The first level orientation should be vertical.
+    assertXPath(pXmlDoc, "/metafile/push[1]/push[1]/push[1]/push[3]/push[1]/font[1]", "orientation",
+                "900");
+    // Test the second level of horizontal category axis labels orientation. The second level orientation should be horizontal.
+    assertXPath(pXmlDoc, "/metafile/push[1]/push[1]/push[1]/push[3]/push[1]/font[5]", "orientation",
+                "0");
+    // Test the third level of horizontal category axis labels orientation. The third level orientation should be horizontal.
+    assertXPath(pXmlDoc, "/metafile/push[1]/push[1]/push[1]/push[3]/push[1]/font[7]", "orientation",
+                "0");
+}
+
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf124796)
 {
     SwDoc* pDoc = createDoc("tdf124796.odt");