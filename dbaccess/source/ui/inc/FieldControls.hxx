8c66efa030e98cfdf5da20be368566d64e43c5d1
Caolán McNamara <caolanm@redhat.com>
2019-09-05 12:22:38 +0100

    weld OCopyTableWizard cluster
    Change-Id: I52547defb0e7ba328ec20d41a955e545556ffd16
    note: get the toplevel parent by getting it from the XInteractionHandler
    Reviewed-on: https://gerrit.libreoffice.org/78751
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>

--- dbaccess/source/ui/inc/FieldControls.hxx
+++ dbaccess/source/ui/inc/FieldControls.hxx
@@ -27,58 +27,92 @@
 namespace dbaui
 {
 
-    class OPropColumnEditCtrl : public OSQLNameEdit
+    class OPropColumnEditCtrl : public OSQLNameEntry
     {
         short                m_nPos;
         OUString             m_strHelpText;
     public:
-        OPropColumnEditCtrl(vcl::Window* pParent, OUString const & _rAllowedChars, const char* pHelpId, short nPosition, WinBits nWinStyle);
-
-        virtual bool IsModified() const override { return IsValueChangedFromSaved(); }
+        OPropColumnEditCtrl(std::unique_ptr<weld::Entry> xEntry, OUString const & _rAllowedChars, const char* pHelpId, short nPosition);
 
         short GetPos() const { return m_nPos; }
         const OUString& GetHelp() const { return m_strHelpText; }
     };
 
-    class OPropEditCtrl :   public Edit
+    class OPropEditCtrl : public OWidgetBase
     {
+        std::unique_ptr<weld::Entry> m_xEntry;
         short                m_nPos;
         OUString             m_strHelpText;
 
     public:
-        OPropEditCtrl(vcl::Window* pParent, const char* pHelpId, short nPosition, WinBits nWinStyle);
+        OPropEditCtrl(std::unique_ptr<weld::Entry> xEntry, const char* pHelpId, short nPosition);
 
-        virtual bool IsModified() const override { return IsValueChangedFromSaved(); }
+        void set_text(const OUString& rText) { m_xEntry->set_text(rText); }
+        OUString get_text() const { return m_xEntry->get_text(); }
+        void set_editable(bool bEditable) { m_xEntry->set_editable(bEditable); }
+
+        virtual void save_value() override { m_xEntry->save_value(); }
+        virtual bool get_value_changed_from_saved() const override { return m_xEntry->get_value_changed_from_saved(); }
 
         short GetPos() const { return m_nPos; }
         const OUString& GetHelp() const { return m_strHelpText; }
     };
 
-    class OPropNumericEditCtrl : public NumericField
+    class OPropNumericEditCtrl : public OWidgetBase
     {
+        std::unique_ptr<weld::SpinButton> m_xSpinButton;
         short     m_nPos;
         OUString  m_strHelpText;
 
     public:
-        OPropNumericEditCtrl(vcl::Window* pParent, const char* pHelpId, short nPosition, WinBits nWinStyle);
+        OPropNumericEditCtrl(std::unique_ptr<weld::SpinButton> xSpinButton, const char* pHelpId, short nPosition);
 
-        virtual bool IsModified() const override { return IsValueChangedFromSaved(); }
+        void set_text(const OUString& rText) { m_xSpinButton->set_text(rText); }
+        OUString get_text() const { return m_xSpinButton->get_text(); }
+
+        virtual void save_value() override { m_xSpinButton->save_value(); }
+        virtual bool get_value_changed_from_saved() const override { return m_xSpinButton->get_value_changed_from_saved(); }
+        void set_digits(int nLen) { m_xSpinButton->set_digits(nLen); }
+        void set_min(int nMin) { m_xSpinButton->set_min(nMin); }
+        void set_max(int nMax) { m_xSpinButton->set_max(nMax); }
+        void set_range(int nMin, int nMax) { m_xSpinButton->set_range(nMin, nMax); }
+        int get_value() const { return m_xSpinButton->get_value(); }
 
         short GetPos() const { return m_nPos; }
         const OUString& GetHelp() const { return m_strHelpText; }
 
-        void SetSpecialReadOnly(bool _bReadOnly);
+        void set_editable(bool bEditable) { m_xSpinButton->set_editable(bEditable); }
     };
 
-    class OPropListBoxCtrl : public ListBox
+    class OPropListBoxCtrl : public OWidgetBase
     {
+        std::unique_ptr<weld::ComboBox> m_xComboBox;
         short     m_nPos;
         OUString  m_strHelpText;
 
     public:
-        OPropListBoxCtrl(vcl::Window* pParent, const char* pHelpId, short nPosition, WinBits nWinStyle);
+        OPropListBoxCtrl(std::unique_ptr<weld::ComboBox> xComboBox, const char* pHelpId, short nPosition);
+        virtual ~OPropListBoxCtrl() override
+        {
+            m_xComboBox->clear();
+        }
 
-        bool IsModified() const { return IsValueChangedFromSaved(); }
+        virtual void save_value() override { m_xComboBox->save_value(); }
+        virtual bool get_value_changed_from_saved() const override { return m_xComboBox->get_value_changed_from_saved(); }
+
+        weld::ComboBox& GetComboBox() { return *m_xComboBox; }
+
+        OUString get_active_text() const { return m_xComboBox->get_active_text(); }
+        void set_active_text(const OUString &rText) { m_xComboBox->set_active_text(rText); }
+
+        int get_active() const { return m_xComboBox->get_active(); }
+        void set_active(int nPos) { m_xComboBox->set_active(nPos); }
+
+        int get_count() const { return m_xComboBox->get_count(); }
+
+        void append_text(const OUString &rText) { m_xComboBox->append_text(rText); }
+        void remove_text(const OUString &rText) { m_xComboBox->remove_text(rText); }
+        int find_text(const OUString &rText) const { return m_xComboBox->find_text(rText); }
 
         short GetPos() const { return m_nPos; }
         const OUString& GetHelp() const { return m_strHelpText; }
