21884ae9954d1c33ea9daf200be9afbde139ef30
Luboš Luňák <l.lunak@collabora.com>
2019-09-12 15:38:05 +0200

    compiler check for rtl::OUStringConcat instances
    Something like auto str = "string" + OUString::number( 10 ); will
    not be OUString but actually rtl::OUStringConcat (which gets implicitly
    converted to OUString, but not with auto). Since those refer to temporaries
    from the expression, they should not outlive the expression.
    
    Change-Id: Ib4cde4b38befb3d49927d0cf01c52ebb2d36df89

--- solenv/CompilerTest_compilerplugins_clang.mk
+++ solenv/CompilerTest_compilerplugins_clang.mk
@@ -69,7 +69,8 @@ $(eval $(call gb_CompilerTest_add_exception_objects,compilerplugins_clang, \
     compilerplugins/clang/test/staticvar \
     compilerplugins/clang/test/stdfunction \
     compilerplugins/clang/test/stringbuffer \
-    compilerplugins/clang/test/stringconcat \
+    compilerplugins/clang/test/stringconcatauto \
+    compilerplugins/clang/test/stringconcatliterals \
     compilerplugins/clang/test/stringconstant \
     compilerplugins/clang/test/stringloop \
     compilerplugins/clang/test/typedefparam \
