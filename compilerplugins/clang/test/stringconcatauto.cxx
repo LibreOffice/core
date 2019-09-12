2ca7919c463b25501ca1de4dc5a1615b57a665f6
Luboš Luňák <l.lunak@collabora.com>
2019-09-12 15:38:05 +0200

    compiler check for rtl::OUStringConcat instances
    Something like auto str = "string" + OUString::number( 10 ); will
    not be OUString but actually rtl::OUStringConcat (which gets implicitly
    converted to OUString, but not with auto). Since those refer to temporaries
    from the expression, they should not outlive the expression.
    
    Change-Id: Ib4cde4b38befb3d49927d0cf01c52ebb2d36df89

---
+++ compilerplugins/clang/test/stringconcatauto.cxx
@@ -0,0 +1,42 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * Based on LLVM/Clang.
+ *
+ * This file is distributed under the University of Illinois Open Source
+ * License. See LICENSE.TXT for details.
+ *
+ */
+
+#include <rtl/ustring.hxx>
+
+void foo()
+{
+    auto str1 = "str1" + OUString::number( 10 );
+    // expected-error-re@-1 {{creating a variable of type 'rtl::OUStringConcat<{{.*}}>' will make it reference temporaries}}
+    // expected-note@-2 {{use OUString instead}}
+    OUString str2 = "str2" + OUString::number( 20 ) + "ing";
+    const auto& str3 = "str3" + OUString::number( 30 );
+    // expected-error-re@-1 {{creating a variable of type 'const rtl::OUStringConcat<{{.*}}> &' will make it reference temporaries}}
+    // expected-note@-2 {{use OUString instead}}
+    const auto str4 = "str4" + OString::number( 40 );
+    // expected-error-re@-1 {{creating a variable of type 'const rtl::OStringConcat<{{.*}}>' will make it reference temporaries}}
+    // expected-note@-2 {{use OString instead}}
+    (void) str1;
+    (void) str2;
+    (void) str3;
+    (void) str4;
+}
+
+struct A
+{
+    auto bar()
+    // expected-error-re@-1 {{returning a variable of type 'rtl::OStringConcat<{{.*}}>' will make it reference temporaries}}
+    // expected-note@-2 {{use OString instead}}
+    {
+        return "bar" + OString::number( 110 );
+    }
+};
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
