4215061730007a106e09483924e6f94c8c56c3d1
Caolán McNamara <caolanm@redhat.com>
2019-09-16 09:34:00 +0100

    latest expat
    Change-Id: Ia98c9718ccd8e18b5f56851027bde944164f05c4

--- external/expat/StaticLibrary_expat.mk
+++ external/expat/StaticLibrary_expat.mk
@@ -43,7 +43,6 @@ $(eval $(call gb_StaticLibrary_add_defs,expat,\
 endif
 
 $(eval $(call gb_StaticLibrary_add_generated_cobjects,expat,\
-	UnpackedTarball/expat/lib/loadlibrary \
 	UnpackedTarball/expat/lib/xmlparse \
 	UnpackedTarball/expat/lib/xmlrole \
 	UnpackedTarball/expat/lib/xmltok \
