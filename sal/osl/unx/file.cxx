bdcf58bdb178402bad25ea1459af3611648eb196
Stephan Bergmann <sbergman@redhat.com>
2019-09-16 10:24:11 +0200

    -Werror=volatile (GCC 10 trunk)
    "error: compound assignment with ‘volatile’-qualified left operand is
    deprecated" in C++20 mode
    
    Change-Id: I62825237a2f4caf359f5f116ab4097ae6b9376e6
    Reviewed-on: https://gerrit.libreoffice.org/78975
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

--- sal/osl/unx/file.cxx
+++ sal/osl/unx/file.cxx
@@ -1181,13 +1181,13 @@ oslFileError SAL_CALL osl_mapFile(
             volatile sal_uInt8 c = 0;
             while (nSize > nPageSize)
             {
-                c ^= pData[0];
+                c = c ^ pData[0];
                 pData += nPageSize;
                 nSize -= nPageSize;
             }
 
             if (nSize > 0)
-                c^= pData[0];
+                c = c ^ pData[0];
         }
     }
 
