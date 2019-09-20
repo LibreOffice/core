LibreOffice/core

commit 1f6b7030cbdc81e8e408e3a13bfcd01fcbdd7550

tdf#95848 sw: DOCX export: crude implementation of abstractNum mapping
The abstractNum needs to correspond to a SwList, not to a SwNumRule as
it is currently implemented.

Add a mapping to MSWordExportBase for "overriding" numbering
definitions; these are added to m_pUsedNumTable, which appears to be
necessary to interact with DuplicateNumRule(), but here we just add
nullpointers, because we don't need to modify the SwNumrule, and neither
do we want the vector to double-delete it.

The mapping is created while iterating over the document, in
AttributeOutputBase::ParaNumRule().

It turns out that this approach would work for WW8 too as
DuplicateNumRule() was originally added for that format but it won't
work easily for RTF; in the DOCX case the WriteNumbering() is called
after the main text and footnotes/endnotes, but with RTF it's the
other way around :(

Change-Id: Ia0409f5ad0b2e089005024ef7f61850a06d4dcbe
Reviewed-on: https://gerrit.libreoffice.org/78607
Tested-by: Jenkins
Reviewed-by: Michael Stahl <Michael.Stahl@cib.de>

sw/source/filter/ww8/wrtw8num.cxx
@@ -70,6 +70,31 @@ sal_uInt16 MSWordExportBase::DuplicateNumRule( const SwNumRule *pRule, sal_uInt8
     return nNumId;
 }
 
+// Ideally we want to map SwList to w:abstractNum and SwNumRule to w:num
+// The current approach is to keep exporting every SwNumRule to
+// 1 w:abstractNum and 1 w:num, and then add extra w:num via this function
+// that reference an existing w:abstractNum and may override its formatting;
+// of course this will end up exporting some w:num that aren't actually used.
+sal_uInt16 MSWordExportBase::OverrideNumRule(
+        SwNumRule const& rExistingRule,
+        SwNumRule const& rAbstractRule)
+{
+    assert(&rExistingRule != &rAbstractRule);
+    auto const numdef = GetNumberingId(rExistingRule);
+    auto const absnumdef = GetNumberingId(rAbstractRule);
+    auto const mapping = std::make_pair(numdef, absnumdef);
+
+    auto it = m_OverridingNumsR.find(mapping);
+    if (it == m_OverridingNumsR.end())
+    {
+        it = m_OverridingNumsR.insert(std::make_pair(mapping, m_pUsedNumTable->size())).first;
+        m_OverridingNums.insert(std::make_pair(m_pUsedNumTable->size(), mapping));
+        m_pUsedNumTable->push_back(nullptr); // dummy, it's unique_ptr...
+        ++m_nUniqueList; // counter for DuplicateNumRule...
+    }
+    return it->second;
+}
+
 sal_uInt16 MSWordExportBase::GetNumberingId( const SwNumRule& rNumRule )
 {
     if ( !m_pUsedNumTable )
@@ -179,9 +204,17 @@ void MSWordExportBase::NumberingDefinitions()
     // Write static data of SwNumRule - LSTF
     for ( sal_uInt16 n = 0; n < nCount; ++n )
     {
-        const SwNumRule& rRule = *(*m_pUsedNumTable)[ n ];
-
-        AttrOutput().NumberingDefinition( n + 1, rRule );
+        const SwNumRule *const pRule = (*m_pUsedNumTable)[ n ];
+        if (pRule)
+        {
+            AttrOutput().NumberingDefinition(n + 1, *pRule);
+        }
+        else
+        {
+            auto it = m_OverridingNums.find(n);
+            assert(it != m_OverridingNums.end());
+            AttrOutput().OverrideNumberingDefinition(n + 1, it->second.second + 1);
+        }
     }
 }
 
@@ -357,6 +390,11 @@ void MSWordExportBase::AbstractNumberingDefinitions()
 
     for( n = 0; n < nCount; ++n )
     {
+        if (nullptr == (*m_pUsedNumTable)[ n ])
+        {
+            continue;
+        }
+
         AttrOutput().StartAbstractNumbering( n + 1 );
 
         const SwNumRule& rRule = *(*m_pUsedNumTable)[ n ];

sw/source/filter/ww8/wrtww8.hxx
@@ -460,6 +460,10 @@ public:
     const SfxItemSet* m_pISet;    // for double attributes
     WW8_WrPct*  m_pPiece;         // Pointer to Piece-Table
     std::unique_ptr<SwNumRuleTable> m_pUsedNumTable;  // all used NumRules
+    /// overriding numdef index -> (existing numdef index, abstractnumdef index)
+    std::map<size_t, std::pair<size_t, size_t>> m_OverridingNums;
+    /// same in reverse
+    std::map<std::pair<size_t, size_t>, size_t> m_OverridingNumsR;
     const SwTextNode *m_pTopNodeOfHdFtPage; ///< Top node of host page when in hd/ft
     std::map< sal_uInt16, sal_uInt16 > m_aRuleDuplicates; //map to Duplicated numrules
     std::stack< sal_Int32 > m_aCurrentCharPropStarts; ///< To remember the position in a run.
@@ -649,6 +653,11 @@ public:
     /// completely new list based on this one and export that instead,
     /// which duplicates words behaviour in this respect.
     sal_uInt16 DuplicateNumRule( const SwNumRule *pRule, sal_uInt8 nLevel, sal_uInt16 nVal );
+
+    /// Create a overriding numbering definition (if it does not yet exist)
+    /// @return index of the overriding numbering definition
+    sal_uInt16 OverrideNumRule(SwNumRule const& rExistingRule,
+                               SwNumRule const& rAbstractRule);
 
     /// Access to the attribute output class.
     virtual AttributeOutputBase& AttrOutput() const = 0;

sw/source/filter/ww8/ww8atr.cxx
@@ -94,6 +94,8 @@
 #include <IDocumentSettingAccess.hxx>
 #include <IDocumentFieldsAccess.hxx>
 #include <IDocumentStylePoolAccess.hxx>
+#include <IDocumentListsAccess.hxx>
+#include <list.hxx>
 #include <docary.hxx>
 #include <pam.hxx>
 #include <paratr.hxx>
@@ -3554,6 +3556,28 @@ void AttributeOutputBase::ParaNumRule( const SwNumRuleItem& rNumRule )
                             nNumId = GetExport().DuplicateNumRule( pRule, nLvl, nStartWith );
                             if ( USHRT_MAX != nNumId )
                                 ++nNumId;
+                        }
+                        else if (GetExport().GetExportFormat() == MSWordExportBase::DOCX) // FIXME
+                        {
+                            // tdf#95848 find the abstract list definition
+                            OUString const listId(pTextNd->GetListId());
+                            if (!listId.isEmpty()
+                                && listId != pRule->GetDefaultListId())
+                            {
+                                SwList const*const pList(
+                                    GetExport().m_pDoc->getIDocumentListsAccess().getListByName(listId));
+                                if (pList)
+                                {
+                                    SwNumRule const*const pAbstractRule(
+                                        GetExport().m_pDoc->FindNumRulePtr(
+                                            pList->GetDefaultListStyleName()));
+                                    assert(pAbstractRule);
+                                    nNumId = GetExport().OverrideNumRule(
+                                            *pRule, *pAbstractRule);
+                                    assert(nNumId != USHRT_MAX);
+                                    ++nNumId;
+                                }
+                            }
                         }
                     }
                     else

sw/inc/list.hxx
@@ -24,6 +24,8 @@
 #include <rtl/ustring.hxx>
 #include <memory>
 
+#include "swdllapi.h"
+
 class SwNumRule;
 class SwNodes;
 class SwNodeNum;
@@ -40,7 +42,7 @@ class SwList
 
         const OUString & GetListId() const;
 
-        const OUString & GetDefaultListStyleName() const;
+        SW_DLLPUBLIC const OUString & GetDefaultListStyleName() const;
         void SetDefaultListStyleName(OUString const&);
 
         void InsertListItem( SwNodeNum& rNodeNum,

sw/qa/extras/ooxmlexport/ooxmlexport13.cxx
@@ -66,8 +66,7 @@ DECLARE_OOXMLEXPORT_TEST(testTdf121374_sectionHF2, "tdf121374_sectionHF2.doc")
     CPPUNIT_ASSERT( xHeaderText->getString().startsWith("virkamatka-anomus") );
 }
 
-// TODO export has the same wrong assumption that abstractNum = SwNumRule
-DECLARE_SW_EXPORT_TEST(testTdf95848, "tdf95848.docx", nullptr, Test)
+DECLARE_OOXMLEXPORT_TEST(testTdf95848, "tdf95848.docx")
 {
     OUString listId;
     OUString listStyle;

sw/source/filter/ww8/attributeoutputbase.hxx
@@ -346,6 +346,10 @@ public:
     /// Definition of a numbering instance.
     virtual void NumberingDefinition( sal_uInt16 nId, const SwNumRule &rRule ) = 0;
 
+    /// Numbering defintion that overrides abstract numbering definition
+    virtual void OverrideNumberingDefinition(sal_uInt16 /*nNum*/, sal_uInt16 /*nAbstractNum*/)
+    { assert(false); } // TODO implement for WW8/RTF
+
     /// Start of the abstract numbering definition instance.
     virtual void StartAbstractNumbering( sal_uInt16 /*nId*/ ) {}
 

sw/source/filter/ww8/docxattributeoutput.cxx
@@ -6558,6 +6558,18 @@ void DocxAttributeOutput::NumberingDefinition( sal_uInt16 nId, const SwNumRule &
     m_pSerializer->endElementNS( XML_w, XML_num );
 }
 
+void DocxAttributeOutput::OverrideNumberingDefinition(
+        sal_uInt16 const nNum, sal_uInt16 const nAbstractNum)
+{
+    m_pSerializer->startElementNS(XML_w, XML_num, FSNS(XML_w, XML_numId), OString::number(nNum));
+
+    m_pSerializer->singleElementNS(XML_w, XML_abstractNumId, FSNS(XML_w, XML_val), OString::number(nAbstractNum));
+
+    // TODO: write SwNumRule into w:lvlOverride
+
+    m_pSerializer->endElementNS( XML_w, XML_num );
+}
+
 void DocxAttributeOutput::StartAbstractNumbering( sal_uInt16 nId )
 {
     const SwNumRule* pRule = (*m_rExport.m_pUsedNumTable)[nId - 1];

sw/source/filter/ww8/docxattributeoutput.hxx
@@ -323,6 +323,9 @@ public:
     /// Definition of a numbering instance.
     virtual void NumberingDefinition( sal_uInt16 nId, const SwNumRule &rRule ) override;
 
+    /// Numbering defintion that overrides abstract numbering definition
+    virtual void OverrideNumberingDefinition(sal_uInt16 nNum, sal_uInt16 nAbstractNum) override;
+
     /// Start of the abstract numbering definition instance.
     virtual void StartAbstractNumbering( sal_uInt16 nId ) override;
 

commit 45f8d39df5555ca305857117972ae3799561fd7d

sw: DOCX export of lists: handle multiple SwList with same SwNumRule
This can be reproduced with ODT documents, e.g. fdo45994-2.fodt.

Multiple lists use the same list style / SwNumRule - one of them will be
the default list style of the SwList - map the others to duplicated
SwNumRules, each of which will be exported to a new w:abstractNum.

(assert is regression from 1f6b7030cbdc81e8e408e3a13bfcd01fcbdd7550)

Change-Id: Ib691c7c6b0b3e6da27204d038a64f094a1bde17d

sw/source/filter/ww8/wrtw8num.cxx
@@ -45,9 +45,8 @@ using namespace ::com::sun::star;
 using namespace sw::types;
 using namespace sw::util;
 
-sal_uInt16 MSWordExportBase::DuplicateNumRule( const SwNumRule *pRule, sal_uInt8 nLevel, sal_uInt16 nVal )
+SwNumRule* MSWordExportBase::DuplicateNumRuleImpl(const SwNumRule *pRule)
 {
-    sal_uInt16 nNumId = USHRT_MAX;
     const OUString sPrefix("WW8TempExport" + OUString::number( m_nUniqueList++ ));
     SwNumRule* pMyNumRule =
             new SwNumRule( m_pDoc->GetUniqueNumRuleName( &sPrefix ),
@@ -59,6 +58,14 @@ sal_uInt16 MSWordExportBase::DuplicateNumRule( const SwNumRule *pRule, sal_uInt8
         const SwNumFormat& rSubRule = pRule->Get(i);
         pMyNumRule->Set( i, rSubRule );
     }
+    return pMyNumRule;
+}
+
+sal_uInt16 MSWordExportBase::DuplicateNumRule( const SwNumRule *pRule, sal_uInt8 nLevel, sal_uInt16 nVal )
+{
+    sal_uInt16 nNumId = USHRT_MAX;
+
+    SwNumRule *const pMyNumRule = DuplicateNumRuleImpl(pRule);
 
     SwNumFormat aNumFormat( pMyNumRule->Get( nLevel ) );
     aNumFormat.SetStart( nVal );
@@ -72,6 +79,25 @@ sal_uInt16 MSWordExportBase::DuplicateNumRule( const SwNumRule *pRule, sal_uInt8
     return nNumId;
 }
 
+// multiple SwList can be based on the same SwNumRule; ensure one w:abstractNum
+// per SwList
+sal_uInt16 MSWordExportBase::DuplicateAbsNum(OUString const& rListId,
+        SwNumRule const& rAbstractRule)
+{
+    auto const it(m_Lists.find(rListId));
+    if (it != m_Lists.end())
+    {
+        return it->second;
+    }
+    else
+    {
+        auto const pNewAbstractRule = DuplicateNumRuleImpl(&rAbstractRule);
+        assert(GetNumberingId(*pNewAbstractRule) == m_pUsedNumTable->size() - 1);
+        m_Lists.insert(std::make_pair(rListId, m_pUsedNumTable->size() - 1));
+        return m_pUsedNumTable->size() - 1;
+    }
+}
+
 // Ideally we want to map SwList to w:abstractNum and SwNumRule to w:num
 // The current approach is to keep exporting every SwNumRule to
 // 1 w:abstractNum and 1 w:num, and then add extra w:num via this function
@@ -79,11 +105,14 @@ sal_uInt16 MSWordExportBase::DuplicateNumRule( const SwNumRule *pRule, sal_uInt8
 // of course this will end up exporting some w:num that aren't actually used.
 sal_uInt16 MSWordExportBase::OverrideNumRule(
         SwNumRule const& rExistingRule,
+        OUString const& rListId,
         SwNumRule const& rAbstractRule)
 {
     assert(&rExistingRule != &rAbstractRule);
     auto const numdef = GetNumberingId(rExistingRule);
-    auto const absnumdef = GetNumberingId(rAbstractRule);
+    auto const absnumdef = rListId == rAbstractRule.GetDefaultListId()
+        ? GetNumberingId(rAbstractRule)
+        : DuplicateAbsNum(rListId, rAbstractRule);
     auto const mapping = std::make_pair(numdef, absnumdef);
 
     auto it = m_OverridingNumsR.find(mapping);
@@ -91,6 +120,7 @@ sal_uInt16 MSWordExportBase::OverrideNumRule(
     {
         it = m_OverridingNumsR.insert(std::make_pair(mapping, m_pUsedNumTable->size())).first;
         m_OverridingNums.insert(std::make_pair(m_pUsedNumTable->size(), mapping));
+
         m_pUsedNumTable->push_back(nullptr); // dummy, it's unique_ptr...
         ++m_nUniqueList; // counter for DuplicateNumRule...
     }

sw/source/filter/ww8/wrtww8.hxx
@@ -464,6 +464,9 @@ public:
     std::map<size_t, std::pair<size_t, size_t>> m_OverridingNums;
     /// same in reverse
     std::map<std::pair<size_t, size_t>, size_t> m_OverridingNumsR;
+    /// list-id -> abstractnumdef index
+    std::map<OUString, size_t> m_Lists;
+
     const SwTextNode *m_pTopNodeOfHdFtPage; ///< Top node of host page when in hd/ft
     std::map< sal_uInt16, sal_uInt16 > m_aRuleDuplicates; //map to Duplicated numrules
     std::stack< sal_Int32 > m_aCurrentCharPropStarts; ///< To remember the position in a run.
@@ -653,10 +656,17 @@ public:
     /// completely new list based on this one and export that instead,
     /// which duplicates words behaviour in this respect.
     sal_uInt16 DuplicateNumRule( const SwNumRule *pRule, sal_uInt8 nLevel, sal_uInt16 nVal );
+    SwNumRule * DuplicateNumRuleImpl(const SwNumRule *pRule);
+
+    /// check if a new abstractNum is needed for this list
+    sal_uInt16 DuplicateAbsNum(OUString const& rListId,
+                               SwNumRule const& rAbstractRule);
+
 
     /// Create a overriding numbering definition (if it does not yet exist)
     /// @return index of the overriding numbering definition
     sal_uInt16 OverrideNumRule(SwNumRule const& rExistingRule,
+                               OUString const& rListId,
                                SwNumRule const& rAbstractRule);
 
     /// Access to the attribute output class.

sw/source/filter/ww8/ww8atr.cxx
@@ -3562,6 +3562,7 @@ void AttributeOutputBase::ParaNumRule( const SwNumRuleItem& rNumRule )
                             // tdf#95848 find the abstract list definition
                             OUString const listId(pTextNd->GetListId());
                             if (!listId.isEmpty()
+                                // default list id uses the 1:1 mapping
                                 && listId != pRule->GetDefaultListId())
                             {
                                 SwList const*const pList(
@@ -3572,8 +3573,16 @@ void AttributeOutputBase::ParaNumRule( const SwNumRuleItem& rNumRule )
                                         GetExport().m_pDoc->FindNumRulePtr(
                                             pList->GetDefaultListStyleName()));
                                     assert(pAbstractRule);
-                                    nNumId = GetExport().OverrideNumRule(
-                                            *pRule, *pAbstractRule);
+                                    if (pAbstractRule == pRule)
+                                    {
+                                        // different list, but no override
+                                        nNumId = GetExport().DuplicateAbsNum(listId, *pAbstractRule);
+                                    }
+                                    else
+                                    {
+                                        nNumId = GetExport().OverrideNumRule(
+                                                *pRule, listId, *pAbstractRule);
+                                    }
                                     assert(nNumId != USHRT_MAX);
                                     ++nNumId;
                                 }