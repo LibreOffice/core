a09f7c76f8740c89b75683cec6ad8ca17090ea57
Szymon Kłos <szymon.klos@collabora.com>
2019-09-23 18:45:34 +0200

    jsdialogs: set frame as a separate type
    Change-Id: I7f56d49dbe3b403c3716798bb52691748974aaae

--- vcl/source/window/layout.cxx
+++ vcl/source/window/layout.cxx
@@ -1617,6 +1617,13 @@ OUString VclFrame::getDefaultAccessibleName() const
     return VclBin::getDefaultAccessibleName();
 }
 
+boost::property_tree::ptree VclFrame::DumpAsPropertyTree()
+{
+    boost::property_tree::ptree aTree(VclBin::DumpAsPropertyTree());
+    aTree.put("type", "frame");
+    return aTree;
+}
+
 Size VclAlignment::calculateRequisition() const
 {
     Size aRet(m_nLeftPadding + m_nRightPadding,
