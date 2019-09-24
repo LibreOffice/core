028696a691ab789c16f1b21ec7b285bddec79900
Caolán McNamara <caolanm@redhat.com>
2019-09-24 13:21:34 +0100

    drop OGeneralPageWizard::GetFocus
    Change-Id: I0ad65590053460f23f1a6dcaace24d47dbbf39b1

--- dbaccess/source/ui/dlg/generalpage.hxx
+++ dbaccess/source/ui/dlg/generalpage.hxx
@@ -162,8 +162,6 @@ namespace dbaui
     private:
         virtual bool FillItemSet( SfxItemSet* _rCoreAttrs ) override;
 
-        virtual void GetFocus() override;
-
         virtual void implInitControls( const SfxItemSet& _rSet, bool _bSaveValue ) override;
         virtual OUString getDatasourceName( const SfxItemSet& _rSet ) override;
         virtual bool approveDatasourceType( ::dbaccess::DATASOURCE_TYPE eType, OUString& _inout_rDisplayName ) override;
