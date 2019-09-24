d478d99014e239530b57e4413b331936ceef745e
Tünde Tóth <tundeth@gmail.com>
2019-09-24 14:29:44 +0200

    tdf#127733 DOCX export: fix internal hyperlinks to chart target
    Hyperlinks to internal chart targets didn't work after export.
    
    Change-Id: I724c6af8fd7f1961260b82331b9f62d8cbd88f25

--- sw/qa/extras/ooxmlexport/ooxmlexport13.cxx
+++ sw/qa/extras/ooxmlexport/ooxmlexport13.cxx
@@ -775,6 +775,17 @@ DECLARE_OOXMLEXPORT_TEST(testTdf127732, "internal_hyperlink_frame.odt")
     CPPUNIT_ASSERT_EQUAL(anchor, bookmarkName);
 }
 
+DECLARE_OOXMLEXPORT_TEST(testTdf127733, "internal_hyperlink_ole.odt")
+{
+    xmlDocPtr pXmlDoc = parseExport("word/document.xml");
+    if (!pXmlDoc)
+        return;
+
+    OUString bookmarkName = getXPath(pXmlDoc, "/w:document/w:body/w:p[3]/w:bookmarkStart", "name");
+    OUString anchor = getXPath(pXmlDoc, "/w:document/w:body/w:p[1]/w:hyperlink", "anchor");
+    CPPUNIT_ASSERT_EQUAL(anchor, bookmarkName);
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
