LibreOffice/core

commit 06f7d1a96eef5aa69d4872ff6d96eb5085296d09

fdo#74775: Preseved Citation after round trip.
Conflicts:
	sw/qa/extras/ooxmlexport/ooxmlexport.cxx
Reviewed on:
	https://gerrit.libreoffice.org/8473

Change-Id: Ie1b0ac3cb4d4b9bf305323599d5e4b63f913fb1b

sw/source/core/fields/fldbas.cxx
@@ -40,6 +40,7 @@
 #include <calc.hxx>
 #include <comcore.hrc>
 #include <docary.hxx>
+#include <authfld.hxx>
 
 #include <math.h>
 
@@ -384,13 +385,19 @@ sal_Bool SwField::IsFixed() const
     return bRet;
 }
 
-OUString SwField::ExpandField(bool const bCached) const
+OUString SwField::ExpandField(bool const bCached, ToxAuthorityField eField) const
 {
     if ( m_bUseFieldValueCache )
     {
         if (!bCached) // #i85766# do not expand fields in clipboard documents
         {
-            m_Cache = Expand();
+            if (GetTypeId() == TYP_AUTHORITY)
+            {
+                const SwAuthorityField* pAuthorityField = static_cast<const SwAuthorityField*>(this);
+                m_Cache = pAuthorityField->ConditionalExpand(eField);
+            }
+            else
+                m_Cache = Expand();
         }
         return m_Cache;
     }

sw/inc/authfld.hxx
@@ -150,6 +150,9 @@ class SwAuthorityField : public SwField
     virtual SwField*    Copy() const;
 
 public:
+    /// For internal use only, in general continue using ExpandField() instead.
+    OUString ConditionalExpand(ToxAuthorityField eField) const;
+
     SwAuthorityField(SwAuthorityFieldType* pType, const OUString& rFieldContents);
     SwAuthorityField(SwAuthorityFieldType* pType, sal_IntPtr nHandle);
     ~SwAuthorityField();

sw/inc/fldbas.hxx
@@ -26,7 +26,7 @@
 #include <com/sun/star/uno/Any.hxx>
 #include <cppuhelper/weakref.hxx>
 #include <vector>
-
+#include <toxe.hxx>
 class SwDoc;
 class SvNumberFormatter;
 
@@ -317,7 +317,7 @@ public:
                     SwTxtFormatter::NewFldPortion() sets things up properly.
         @return     the generated text (suitable for display)
       */
-    OUString            ExpandField(bool const bCached) const;
+    OUString            ExpandField(bool const bCached, ToxAuthorityField eField = AUTH_FIELD_IDENTIFIER) const;
 
     /// @return name or content.
     virtual OUString    GetFieldName() const;

sw/inc/poolfmt.hrc
@@ -221,6 +221,7 @@
 #define STR_POOLCOLL_TOX_USER9          (RC_POOLCOLL_REGISTER_BEGIN+ 34)
 #define STR_POOLCOLL_TOX_USER10         (RC_POOLCOLL_REGISTER_BEGIN+ 35)
 
+#define STR_POOLCOLL_TOX_CITATION       (RC_POOLCOLL_REGISTER_BEGIN+ 36)
 
 // Category Chapter/Document
 #define STR_POOLCOLL_DOC_TITEL          (RC_POOLCOLL_DOC_BEGIN+ 0)

sw/inc/shellres.hxx
@@ -63,6 +63,7 @@ struct SW_DLLPUBLIC ShellResource : public Resource
     OUString        aTOXObjectsName;
     OUString        aTOXTablesName;
     OUString        aTOXAuthoritiesName;
+    OUString        aTOXCitationName;
 
     OUString        aLinkCtrlClick;
     OUString        aLinkClick;

sw/inc/tox.hxx
@@ -56,6 +56,7 @@ class SW_DLLPUBLIC SwTOXMark
     OUString aAltText;    // Text of caption is different.
     OUString aPrimaryKey;
     OUString aSecondaryKey;
+    OUString aCitationKeyReading;
 
     // three more strings for phonetic sorting
     OUString aTextReading;
@@ -113,12 +114,14 @@ public:
     inline void             SetTextReading(const OUString& rStr);
     inline void             SetPrimaryKeyReading(const OUString& rStr );
     inline void             SetSecondaryKeyReading(const OUString& rStr);
+    inline void             SetCitationKeyReading(const OUString& rStr);
 
     inline OUString         GetPrimaryKey() const;
     inline OUString         GetSecondaryKey() const;
     inline OUString         GetTextReading() const;
     inline OUString         GetPrimaryKeyReading() const;
     inline OUString         GetSecondaryKeyReading() const;
+    inline OUString         GetCitationKeyReading() const;
 
     sal_Bool                    IsAutoGenerated() const {return bAutoGenerated;}
     void                    SetAutoGenerated(sal_Bool bSet) {bAutoGenerated = bSet;}
@@ -625,6 +628,12 @@ inline void SwTOXMark::SetPrimaryKeyReading( const OUString& rKey )
     aPrimaryKeyReading = rKey;
 }
 
+inline void SwTOXMark::SetCitationKeyReading( const OUString& rKey )
+{
+    SAL_WARN_IF(GetTOXType()->GetType() != TOX_CITATION, "sw", "Wrong type");
+    aCitationKeyReading = rKey;
+}
+
 inline void SwTOXMark::SetSecondaryKeyReading( const OUString& rKey )
 {
     SAL_WARN_IF(GetTOXType()->GetType() != TOX_INDEX, "sw", "Wrong type");
@@ -665,6 +674,12 @@ inline OUString SwTOXMark::GetSecondaryKeyReading() const
 {
     SAL_WARN_IF(GetTOXType()->GetType() != TOX_INDEX, "sw", "Wrong type");
     return aSecondaryKeyReading;
+}
+
+inline OUString SwTOXMark::GetCitationKeyReading() const
+{
+    SAL_WARN_IF(GetTOXType()->GetType() != TOX_CITATION, "sw", "Wrong type");
+    return aCitationKeyReading;
 }
 
 //SwForm

sw/inc/toxe.hxx
@@ -42,7 +42,8 @@ enum TOXTypes
     TOX_OBJECTS,
     TOX_TABLES,
     TOX_AUTHORITIES,
-    TOX_BIBLIOGRAPHY
+    TOX_BIBLIOGRAPHY,
+    TOX_CITATION
 };
 // this enum contains all types of sources
 enum ToxAuthorityType

commit c47a6136b77fc5e773116acdc80ba3465062b3a3

tdf#107784: DOCX Import: Show citation's title in fields
Regression from 06f7d1a96eef5aa69d4872ff6d96eb5085296d09
The problem is ConditionalExpandAuthIdentifier method uses
AUTH_FIELD_IDENTIFIER while we want to use AUTH_FIELD_TITLE
for citations, as we do in DomainMapper_Impl::SetFieldResult

Change-Id: I30dcceba79dda3532ce10732e43303233d2ee441

sw/source/core/fields/fldbas.cxx
@@ -418,7 +418,7 @@ SwField::ExpandField(bool const bCached, SwRootFrame const*const pLayout) const
             if (GetTypeId() == TYP_AUTHORITY)
             {
                 const SwAuthorityField* pAuthorityField = static_cast<const SwAuthorityField*>(this);
-                m_Cache = pAuthorityField->ConditionalExpandAuthIdentifier(pLayout);
+                m_Cache = pAuthorityField->ExpandCitation(AUTH_FIELD_TITLE, pLayout);
             }
             else
                 m_Cache = ExpandImpl(pLayout);

sw/qa/extras/ooxmlimport/ooxmlimport.cxx
@@ -685,6 +685,23 @@ DECLARE_OOXMLIMPORT_TEST(testTdf105975formula, "tdf105975.docx")
     CPPUNIT_ASSERT_EQUAL(OUString("25"), xEnumerationAccess->getPresentation(false).trim());
 }
 
+DECLARE_OOXMLIMPORT_TEST(testTdf107784, "tdf107784.docx")
+{
+    // Make sure the field displays the citation's title and not the identifier
+    uno::Reference<text::XTextFieldsSupplier> xTextFieldsSupplier(mxComponent, uno::UNO_QUERY);
+    uno::Reference<container::XEnumerationAccess> xFieldsAccess(xTextFieldsSupplier->getTextFields());
+    uno::Reference<container::XEnumeration> xFields(xFieldsAccess->createEnumeration());
+
+    if( !xFields->hasMoreElements() ) {
+        CPPUNIT_ASSERT(false);
+        return;
+    }
+
+    uno::Reference<text::XTextField> xEnumerationAccess(xFields->nextElement(), uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(OUString("Bibliography entry"), xEnumerationAccess->getPresentation(true).trim());
+    CPPUNIT_ASSERT_EQUAL(OUString("(Smith, 1950)"), xEnumerationAccess->getPresentation(false).trim());
+}
+
 DECLARE_OOXMLIMPORT_TEST(testTdf115883, "tdf115883.docx")
 {
     // Import failed due to an unhandled exception when getting the Surround