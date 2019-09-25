860c5b85a2c2655c6919f1ba03969dd2dfe9f238
Caolán McNamara <caolanm@redhat.com>
2019-09-25 14:15:38 +0100

    wip: weld DataEditor
    Change-Id: I8b59140d2e2b0892f17159282a1aeb1a47008ba0

--- chart2/source/controller/dialogs/dlg_DataEditor.cxx
+++ chart2/source/controller/dialogs/dlg_DataEditor.cxx
@@ -25,6 +25,7 @@
 #include <vcl/taskpanelist.hxx>
 #include <svtools/miscopt.hxx>
 
+#include <com/sun/star/awt/XWindow.hpp>
 #include <com/sun/star/frame/XStorable.hpp>
 
 using namespace ::com::sun::star;
@@ -33,100 +34,70 @@ using ::com::sun::star::uno::Reference;
 namespace chart
 {
 
-DataEditor::DataEditor(vcl::Window* pParent,
+DataEditor::DataEditor(weld::Window* pParent,
     const Reference< chart2::XChartDocument > & xChartDoc,
     const Reference< uno::XComponentContext > & xContext)
-    : ModalDialog(pParent, "ChartDataDialog",
-        "modules/schart/ui/chartdatadialog.ui")
+    : GenericDialogController(pParent, "modules/schart/ui/chartdatadialog.ui", "ChartDataDialog")
     , m_bReadOnly(false)
     , m_xChartDoc(xChartDoc)
     , m_xContext(xContext)
+    , m_xTbxData(m_xBuilder->weld_toolbar("toolbar"))
+    , m_xCloseBtn(m_xBuilder->weld_button("close"))
+    , m_xTable(m_xBuilder->weld_container("datawindow"))
+    , m_xColumns(m_xBuilder->weld_container("columns"))
+    , m_xColors(m_xBuilder->weld_container("colorcolumns"))
+    , m_xTableCtrlParent(m_xTable->CreateChildFrame())
+    , m_xBrwData(VclPtr<DataBrowser>::Create(m_xTableCtrlParent, m_xColumns.get(), m_xColors.get()))
 {
-    m_xBrwData.reset(VclPtr<DataBrowser>::Create(get<vcl::Window>("datawindow"), WB_BORDER | WB_TABSTOP, true /* bLiveUpdate */));
-    m_xBrwData->set_hexpand(true);
-    m_xBrwData->set_vexpand(true);
-    m_xBrwData->set_expand(true);
-    Size aSize(m_xBrwData->LogicToPixel(Size(232, 121), MapMode(MapUnit::MapAppFont)));
-    m_xBrwData->set_width_request(aSize.Width());
-    m_xBrwData->set_height_request(aSize.Height());
+    m_xCloseBtn->connect_clicked(LINK(this, DataEditor, CloseHdl));
+
+    Size aSize(m_xTable->get_approximate_digit_width() * 75, m_xTable->get_text_height() * 15);
+    m_xTable->set_size_request(aSize.Width(), aSize.Height());
+
     m_xBrwData->Show();
 
-    get(m_pTbxData, "toolbar");
-
-    TBI_DATA_INSERT_ROW = m_pTbxData->GetItemId("InsertRow");
-    TBI_DATA_INSERT_COL = m_pTbxData->GetItemId("InsertColumn");
-    TBI_DATA_INSERT_TEXT_COL = m_pTbxData->GetItemId("InsertTextColumn");
-    TBI_DATA_DELETE_ROW = m_pTbxData->GetItemId("RemoveRow");
-    TBI_DATA_DELETE_COL = m_pTbxData->GetItemId("RemoveColumn");
-    TBI_DATA_MOVE_LEFT_COL = m_pTbxData->GetItemId("MoveLeftColumn");
-    TBI_DATA_MOVE_RIGHT_COL = m_pTbxData->GetItemId("MoveRightColumn");
-    TBI_DATA_MOVE_UP_ROW = m_pTbxData->GetItemId("MoveUpRow");
-    TBI_DATA_MOVE_DOWN_ROW = m_pTbxData->GetItemId("MoveDownRow");
-
-    m_pTbxData->SetSelectHdl( LINK( this, DataEditor, ToolboxHdl ));
+    m_xTbxData->connect_clicked(LINK(this, DataEditor, ToolboxHdl));
 
     m_xBrwData->SetCursorMovedHdl( LINK( this, DataEditor, BrowserCursorMovedHdl ));
 
     m_xBrwData->SetDataFromModel( m_xChartDoc, m_xContext );
-    GrabFocus();
-    m_xBrwData->GrabFocus();
+//    m_xDialog->grab_focus();
+//    m_xBrwData->GrabFocus();
 
     bool bReadOnly = true;
     Reference< frame::XStorable > xStor( m_xChartDoc, uno::UNO_QUERY );
     if( xStor.is())
         bReadOnly = xStor->isReadonly();
     SetReadOnly( bReadOnly );
-
-    // change buttons to flat-look if set so by user
-    SvtMiscOptions aMiscOptions;
-    const sal_Int16 nStyle( aMiscOptions.GetToolboxStyle() );
-    // react on changes
-    aMiscOptions.AddListenerLink( LINK( this, DataEditor, MiscHdl ) );
-    m_pTbxData->SetOutStyle( nStyle );
-
-    // allow travelling to toolbar with F6
-    notifySystemWindow( this, m_pTbxData, ::comphelper::mem_fun( & TaskPaneList::AddWindow ));
 }
 
 DataEditor::~DataEditor()
 {
-    disposeOnce();
-}
-
-void DataEditor::dispose()
-{
-    notifySystemWindow( this, m_pTbxData, ::comphelper::mem_fun( & TaskPaneList::RemoveWindow ));
-
-    SvtMiscOptions aMiscOptions;
-    aMiscOptions.RemoveListenerLink( LINK( this, DataEditor, MiscHdl ) );
-
-    m_pTbxData.clear();
     m_xBrwData.disposeAndClear();
-    ModalDialog::dispose();
+    m_xTableCtrlParent->dispose();
+    m_xTableCtrlParent.clear();
 }
 
 // react on click (or keypress) on toolbar icon
-IMPL_LINK_NOARG(DataEditor, ToolboxHdl, ToolBox *, void)
+IMPL_LINK(DataEditor, ToolboxHdl, const OString&, rId, void)
 {
-    sal_uInt16 nId = m_pTbxData->GetCurItemId();
-
-    if (nId == TBI_DATA_INSERT_ROW)
+    if (rId == "InsertRow")
         m_xBrwData->InsertRow();
-    else if (nId == TBI_DATA_INSERT_COL)
+    else if (rId == "InsertColumn")
         m_xBrwData->InsertColumn();
-    else if (nId == TBI_DATA_INSERT_TEXT_COL)
+    else if (rId == "InsertTextColumn")
         m_xBrwData->InsertTextColumn();
-    else if (nId == TBI_DATA_DELETE_ROW)
+    else if (rId == "RemoveRow")
         m_xBrwData->RemoveRow();
-    else if (nId == TBI_DATA_DELETE_COL)
+    else if (rId == "RemoveColumn")
         m_xBrwData->RemoveColumn();
-    else if (nId == TBI_DATA_MOVE_LEFT_COL)
+    else if (rId == "MoveLeftColumn")
         m_xBrwData->MoveLeftColumn();
-    else if (nId == TBI_DATA_MOVE_RIGHT_COL)
+    else if (rId == "MoveRightColumn")
         m_xBrwData->MoveRightColumn();
-    else if (nId == TBI_DATA_MOVE_UP_ROW)
+    else if (rId == "MoveUpRow")
         m_xBrwData->MoveUpRow();
-    else if (nId == TBI_DATA_MOVE_DOWN_ROW)
+    else if (rId == "MoveDownRow")
         m_xBrwData->MoveDownRow();
 }
 
@@ -138,16 +109,16 @@ IMPL_LINK_NOARG(DataEditor, BrowserCursorMovedHdl, DataBrowser*, void)
 
     bool bIsDataValid = m_xBrwData->IsEnableItem();
 
-    m_pTbxData->EnableItem( TBI_DATA_INSERT_ROW, bIsDataValid && m_xBrwData->MayInsertRow() );
-    m_pTbxData->EnableItem( TBI_DATA_INSERT_COL, bIsDataValid && m_xBrwData->MayInsertColumn() );
-    m_pTbxData->EnableItem( TBI_DATA_INSERT_TEXT_COL, bIsDataValid && m_xBrwData->MayInsertColumn() );
-    m_pTbxData->EnableItem( TBI_DATA_DELETE_ROW, m_xBrwData->MayDeleteRow() );
-    m_pTbxData->EnableItem( TBI_DATA_DELETE_COL, m_xBrwData->MayDeleteColumn() );
+    m_xTbxData->set_item_sensitive("InsertRow", bIsDataValid && m_xBrwData->MayInsertRow() );
+    m_xTbxData->set_item_sensitive("InsertColumn", bIsDataValid && m_xBrwData->MayInsertColumn() );
+    m_xTbxData->set_item_sensitive("InsertTextColumn", bIsDataValid && m_xBrwData->MayInsertColumn() );
+    m_xTbxData->set_item_sensitive("RemoveRow", m_xBrwData->MayDeleteRow() );
+    m_xTbxData->set_item_sensitive("RemoveColumn", m_xBrwData->MayDeleteColumn() );
 
-    m_pTbxData->EnableItem( TBI_DATA_MOVE_LEFT_COL,   bIsDataValid && m_xBrwData->MayMoveLeftColumns() );
-    m_pTbxData->EnableItem( TBI_DATA_MOVE_RIGHT_COL,   bIsDataValid && m_xBrwData->MayMoveRightColumns() );
-    m_pTbxData->EnableItem( TBI_DATA_MOVE_DOWN_ROW,   bIsDataValid && m_xBrwData->MayMoveDownRows() );
-    m_pTbxData->EnableItem( TBI_DATA_MOVE_UP_ROW,   bIsDataValid && m_xBrwData->MayMoveUpRows() );
+    m_xTbxData->set_item_sensitive("MoveLeftColumn", bIsDataValid && m_xBrwData->MayMoveLeftColumns() );
+    m_xTbxData->set_item_sensitive("MoveRightColumn", bIsDataValid && m_xBrwData->MayMoveRightColumns() );
+    m_xTbxData->set_item_sensitive("MoveUpRow", bIsDataValid && m_xBrwData->MayMoveDownRows() );
+    m_xTbxData->set_item_sensitive("MoveDownRow", bIsDataValid && m_xBrwData->MayMoveUpRows() );
 }
 
 // disable all modifying controls
@@ -156,61 +127,30 @@ void DataEditor::SetReadOnly( bool bReadOnly )
     m_bReadOnly = bReadOnly;
     if( m_bReadOnly )
     {
-        m_pTbxData->EnableItem( TBI_DATA_INSERT_ROW, false );
-        m_pTbxData->EnableItem( TBI_DATA_INSERT_COL, false );
-        m_pTbxData->EnableItem( TBI_DATA_INSERT_TEXT_COL, false );
-        m_pTbxData->EnableItem( TBI_DATA_DELETE_ROW, false );
-        m_pTbxData->EnableItem( TBI_DATA_DELETE_COL, false );
-        m_pTbxData->EnableItem( TBI_DATA_MOVE_LEFT_COL, false );
-        m_pTbxData->EnableItem( TBI_DATA_MOVE_RIGHT_COL, false );
-        m_pTbxData->EnableItem( TBI_DATA_MOVE_UP_ROW, false );
-        m_pTbxData->EnableItem( TBI_DATA_MOVE_DOWN_ROW, false );
+        m_xTbxData->set_item_sensitive("InsertRow", false);
+        m_xTbxData->set_item_sensitive("InsertColumn", false);
+        m_xTbxData->set_item_sensitive("InsertTextColumn", false);
+        m_xTbxData->set_item_sensitive("RemoveRow", false);
+        m_xTbxData->set_item_sensitive("RemoveColumn", false);
+        m_xTbxData->set_item_sensitive("MoveLeftColumn", false);
+        m_xTbxData->set_item_sensitive("MoveRightColumn", false);
+        m_xTbxData->set_item_sensitive("MoveUpRow", false);
+        m_xTbxData->set_item_sensitive("MoveDownRow", false);
     }
 
     m_xBrwData->SetReadOnly( m_bReadOnly );
 }
 
-IMPL_LINK_NOARG(DataEditor, MiscHdl, LinkParamNone*, void)
+IMPL_LINK_NOARG(DataEditor, CloseHdl, weld::Button&, void)
 {
-    SvtMiscOptions aMiscOptions;
-    sal_Int16 nStyle( aMiscOptions.GetToolboxStyle() );
-
-    m_pTbxData->SetOutStyle( nStyle );
-}
-
-bool DataEditor::Close()
-{
-    if( ApplyChangesToModel() )
-        return ModalDialog::Close();
-    else
-        return true;
+    bool bApplied = ApplyChangesToModel();
+    if (bApplied)
+        m_xDialog->response(RET_CLOSE);
 }
 
 bool DataEditor::ApplyChangesToModel()
 {
     return m_xBrwData->EndEditing();
-}
-
-// add/remove a window (the toolbar) to/from the global list, so that F6
-// travels/no longer travels over this window.  _rMemFunc may be
-// TaskPaneList::AddWindow or TaskPaneList::RemoveWindow
-void DataEditor::notifySystemWindow(
-    vcl::Window const * pWindow, vcl::Window* pToRegister,
-    const ::comphelper::mem_fun1_t<TaskPaneList, vcl::Window*>& rMemFunc )
-{
-    OSL_ENSURE( pWindow, "Window must not be null!" );
-    if( !pWindow )
-        return;
-    vcl::Window* pParent = pWindow->GetParent();
-    while( pParent && ! pParent->IsSystemWindow() )
-    {
-        pParent = pParent->GetParent();
-    }
-    if ( pParent && pParent->IsSystemWindow())
-    {
-        SystemWindow* pSystemWindow = static_cast< SystemWindow* >( pParent );
-        rMemFunc( pSystemWindow->GetTaskPaneList(), pToRegister );
-    }
 }
 
 } // namespace chart
