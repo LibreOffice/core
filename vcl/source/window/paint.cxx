bb11efa477b6700744560d3999190c65d016600b
Henry Castro <hcastro@collabora.com>
2019-09-25 07:11:24 -0400

    tdf#126967:vcl: lok: avoid starting idle paint task
    In the Tiled rendering case the invalidation is handled
    by sending logic invalidate notifications to client side.
    Starting an idle paint task, it provokes an infinity loop
    that it does not have the chance to process client side messages
    (mpPollCallback).
    
    Change-Id: I6ef1bbd6270592eebfeef0c8a37945bf5ddd3580
    Reviewed-on: https://gerrit.libreoffice.org/79537
    Tested-by: Jenkins
    Reviewed-by: Tor Lillqvist <tml@collabora.com>
    Tested-by: Tor Lillqvist <tml@collabora.com>

--- vcl/source/window/paint.cxx
+++ vcl/source/window/paint.cxx
@@ -654,6 +654,9 @@ IMPL_LINK_NOARG(Window, ImplHandlePaintHdl, Timer *, void)
     else if ( mpWindowImpl->mbReallyVisible )
     {
         ImplCallOverlapPaint();
+        if (comphelper::LibreOfficeKit::isActive() &&
+            mpWindowImpl->mpFrameData->maPaintIdle.IsActive())
+            mpWindowImpl->mpFrameData->maPaintIdle.Stop();
     }
 }
 
