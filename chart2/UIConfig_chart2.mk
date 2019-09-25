aa7e7747f4296b3b92379c3c7703b80ad8af6a8b
Caolán McNamara <caolanm@redhat.com>
2019-09-25 14:15:38 +0100

    weld DataEditor
    Change-Id: I8b59140d2e2b0892f17159282a1aeb1a47008ba0
    Reviewed-on: https://gerrit.libreoffice.org/79561
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>

--- chart2/UIConfig_chart2.mk
+++ chart2/UIConfig_chart2.mk
@@ -40,9 +40,11 @@ $(eval $(call gb_UIConfig_add_uifiles,modules/schart,\
 	chart2/uiconfig/ui/chardialog \
 	chart2/uiconfig/ui/chartdatadialog \
 	chart2/uiconfig/ui/charttypedialog \
+	chart2/uiconfig/ui/columnfragment \
 	chart2/uiconfig/ui/datarangedialog \
 	chart2/uiconfig/ui/dlg_DataLabel \
 	chart2/uiconfig/ui/dlg_InsertErrorBars \
+	chart2/uiconfig/ui/imagefragment \
 	chart2/uiconfig/ui/insertaxisdlg \
 	chart2/uiconfig/ui/insertgriddlg \
 	chart2/uiconfig/ui/inserttitledlg \
