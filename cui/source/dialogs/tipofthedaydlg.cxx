0e74f12dbeaf21304fa6500c523309fd03a0aa55
Heiko Tietze <tietze.heiko@gmail.com>
2019-09-25 17:31:00 +0200

    Resolves tdf#127756 - Tip of the day not localized
    Introduced %LANGUAGENAME token which is replaced by
    BCP47 value
    
    Change-Id: Iaa7168731e0f6a8e0dd1e906ff0ef646534d9f4b

--- cui/source/dialogs/tipofthedaydlg.cxx
+++ cui/source/dialogs/tipofthedaydlg.cxx
@@ -28,6 +28,8 @@
 #include <vcl/help.hxx>
 #include <vcl/virdev.hxx>
 #include <vcl/svapp.hxx>
+#include <i18nlangtag/languagetag.hxx>
+#include <unotools/configmgr.hxx>
 
 TipOfTheDayDialog::TipOfTheDayDialog(weld::Window* pParent)
     : GenericDialogController(pParent, "cui/ui/tipofthedaydialog.ui", "TipOfTheDayDialog")
@@ -86,7 +88,13 @@ void TipOfTheDayDialog::UpdateTip()
     }
     else if (aLink.startsWith("http"))
     {
-        m_pLink->set_uri(CuiResId(aLink.toUtf8().getStr()));
+        aText = CuiResId(aLink.toUtf8().getStr());
+        sal_Int32 aPos = aText.indexOf("%LANGUAGENAME");
+        if (aPos != -1) 
+            aText = aText.replaceAt(aPos, 13, 
+                                    LanguageTag(utl::ConfigManager::getUILocale()).getBcp47() );
+
+        m_pLink->set_uri(aText);
         m_pLink->set_label(CuiResId(STR_MORE_LINK));
         m_pLink->set_visible(true);
         m_pLink->connect_clicked(Link<weld::LinkButton&, void>());
@@ -99,7 +107,6 @@ void TipOfTheDayDialog::UpdateTip()
         //converts aLink into the proper offline/online hyperlink
         m_pLink->connect_clicked(LINK(this, TipOfTheDayDialog, OnLinkClick));
     }
-
     // image
     OUString aURL("$BRAND_BASE_DIR/$BRAND_SHARE_SUBDIR/tipoftheday/");
     rtl::Bootstrap::expandMacros(aURL);
