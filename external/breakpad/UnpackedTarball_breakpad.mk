224dd1dbb0dc5611314d2312dc8e85f85d390e94
Stephan Bergmann <sbergman@redhat.com>
2019-09-26 22:09:56 +0200

    external/breakpad: glibc 2.30 declares tgkill
    ...so building against glibc-headers-2.30-4.fc31.x86_64 fails with
    
    > src/client/linux/handler/exception_handler.cc:109:12: error: static declaration of 'tgkill' follows non-static declaration
    > static int tgkill(pid_t tgid, pid_t tid, int sig) {
    >            ^
    > /usr/include/bits/signal_ext.h:29:12: note: previous declaration is here
    > extern int tgkill (__pid_t __tgid, __pid_t __tid, int __signal);
    >            ^
    > 1 error generated.
    
    Upstream commit <https://chromium.googlesource.com/breakpad/breakpad/+/
    7e3c165000d44fa153a3270870ed500bc8bbb461%5E%21/> "Fix double declaration of
    tgkill when using Android NDK Headers" looks like the perfect fit.
    
    Change-Id: I1b4805886fb7c770cf9733f34a31296e6b859d92
    Reviewed-on: https://gerrit.libreoffice.org/79661
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>
    (cherry picked from commit 55556a4cebbb35f15e7989bf0a6e276db99944e3)

--- external/breakpad/UnpackedTarball_breakpad.mk
+++ external/breakpad/UnpackedTarball_breakpad.mk
@@ -23,6 +23,7 @@ $(eval $(call gb_UnpackedTarball_add_patches,breakpad,\
 	external/breakpad/breakpad-stackwalk.patch.1 \
 	external/breakpad/ucontext.patch \
 	external/breakpad/0001-Handle-race-between-ExceptionHandler-SignalHandler-a.patch.1 \
+	external/breakpad/0001-Fix-double-declaration-of-tgkill-when-using-Android-.patch.1 \
 ))
 
 ifeq ($(COM_IS_CLANG),TRUE)
