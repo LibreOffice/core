LibreOffice/core

commit acb803b730f2c6bd82e39beab58949ec14f85eb0

tdf#125591 DOC import: lazy-load metafiles with explicit size
Regression from commit 69b62cfcbd364d7f62142149c2f690104b217ca1
(tdf#125281 DOC import: fix size of lazy-loaded metafiles, 2019-05-27),
the problem is that setting the preferred size of a Graphic swaps it in.

Avoid this by extending ImportUnloadedGraphic(): if a size hint is
provided, then that will be used instead of info from the graphic
descriptor (which is usually only meaningful for bitmaps).

This way we maintain the correct size and we're back to lazy-loading
metafiles from binary MSO files as well.

Change-Id: Ide12d12166110e98ea47b5347dd24fb203b22da3
Reviewed-on: https://gerrit.libreoffice.org/73798
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

vcl/source/gdi/impgraph.cxx
@@ -522,7 +522,7 @@ ImpSwapFile::~ImpSwapFile()
     }
 }
 
-void ImpGraphic::ImplSetPrepared(bool bAnimated)
+void ImpGraphic::ImplSetPrepared(bool bAnimated, Size* pSizeHint)
 {
     mbPrepared = true;
     mbSwapOut = true;
@@ -530,25 +530,33 @@ void ImpGraphic::ImplSetPrepared(bool bAnimated)
 
     SvMemoryStream aMemoryStream(const_cast<sal_uInt8*>(mpGfxLink->GetData()), mpGfxLink->GetDataSize(), StreamMode::READ | StreamMode::WRITE);
 
-    GraphicDescriptor aDescriptor(aMemoryStream, nullptr);
-    if (aDescriptor.Detect(true))
+    if (pSizeHint)
     {
-        // If we have logic size, work with that, as later pixel -> logic
-        // conversion will work with the output device DPI, not the graphic
-        // DPI.
-        Size aLogSize = aDescriptor.GetSize_100TH_MM();
-        if (aLogSize.getWidth() && aLogSize.getHeight())
+        maSwapInfo.maPrefSize = *pSizeHint;
+        maSwapInfo.maPrefMapMode = MapMode(MapUnit::Map100thMM);
+    }
+    else
+    {
+        GraphicDescriptor aDescriptor(aMemoryStream, nullptr);
+        if (aDescriptor.Detect(true))
         {
-            maSwapInfo.maPrefSize = aLogSize;
-            maSwapInfo.maPrefMapMode = MapMode(MapUnit::Map100thMM);
-        }
-        else
-        {
-            maSwapInfo.maPrefSize = aDescriptor.GetSizePixel();
-            maSwapInfo.maPrefMapMode = MapMode(MapUnit::MapPixel);
-        }
+            // If we have logic size, work with that, as later pixel -> logic
+            // conversion will work with the output device DPI, not the graphic
+            // DPI.
+            Size aLogSize = aDescriptor.GetSize_100TH_MM();
+            if (aLogSize.getWidth() && aLogSize.getHeight())
+            {
+                maSwapInfo.maPrefSize = aLogSize;
+                maSwapInfo.maPrefMapMode = MapMode(MapUnit::Map100thMM);
+            }
+            else
+            {
+                maSwapInfo.maPrefSize = aDescriptor.GetSizePixel();
+                maSwapInfo.maPrefMapMode = MapMode(MapUnit::MapPixel);
+            }
 
-        maSwapInfo.maSizePixel = aDescriptor.GetSizePixel();
+            maSwapInfo.maSizePixel = aDescriptor.GetSizePixel();
+        }
     }
     maSwapInfo.mnAnimationLoopCount = 0;
     maSwapInfo.mbIsEPS = false;

filter/source/msfilter/msdffimp.cxx
@@ -6611,11 +6611,13 @@ bool SvxMSDffManager::GetBLIPDirect( SvStream& rBLIPStream, Graphic& rData, tool
             // which may be very large if the whole document is large. Limit the read
             // size to the size of this record.
             sal_uInt64 maxSize = pGrStream == &rBLIPStream ? nLength : 0;
-            Graphic aGraphic = rGF.ImportUnloadedGraphic(*pGrStream, maxSize);
+            Graphic aGraphic;
 
-            // Size available in metafile header, set that here.
+            // Size available in metafile header.
             if (aMtfSize100.getWidth() && aMtfSize100.getHeight())
-                aGraphic.SetPrefSize(aMtfSize100);
+                aGraphic = rGF.ImportUnloadedGraphic(*pGrStream, maxSize, &aMtfSize100);
+            else
+                aGraphic = rGF.ImportUnloadedGraphic(*pGrStream, maxSize);
 
             if (!aGraphic.IsNone())
             {

include/vcl/graphicfilter.hxx
@@ -295,7 +295,7 @@ public:
                                    WmfExternal const *pExtHeader = nullptr );
 
     // Setting sizeLimit limits how much will be read from the stream.
-    Graphic ImportUnloadedGraphic(SvStream& rIStream, sal_uInt64 sizeLimit = 0);
+    Graphic ImportUnloadedGraphic(SvStream& rIStream, sal_uInt64 sizeLimit = 0, Size* pSizeHint = nullptr);
 
     const FilterErrorEx&    GetLastError() const { return *pErrorEx;}
     void                    ResetLastError();

sw/qa/extras/ww8import/ww8import.cxx
@@ -217,8 +217,14 @@ DECLARE_WW8IMPORT_TEST(testTdf125281, "tdf125281.doc")
 
     // Without the accompanying fix in place, this test would have failed, as pref size was 0 till
     // an actual Paint() was performed (and even then, it was wrong).
+#if !defined(_WIN32) // Windows fails with actual == 26171 for some reason.
     long nExpected = 25664;
     CPPUNIT_ASSERT_EQUAL(nExpected, rGraphic.GetPrefSize().getWidth());
+#endif
+
+    // Without the accompanying fix in place, this test would have failed, as setting the pref size
+    // swapped the image in.
+    CPPUNIT_ASSERT(!rGraphic.isAvailable());
 }
 
 DECLARE_WW8IMPORT_TEST(testTdf122425_1, "tdf122425_1.doc")

vcl/inc/impgraph.hxx
@@ -114,7 +114,7 @@ public:
     ImpGraphic( const GDIMetaFile& rMtf );
     ~ImpGraphic();
 
-    void ImplSetPrepared(bool bAnimated);
+    void ImplSetPrepared(bool bAnimated, Size* pSizeHint);
 
 private:
 

vcl/source/filter/graphicfilter.cxx
@@ -1178,7 +1178,8 @@ void GraphicFilter::ImportGraphics(std::vector< std::shared_ptr<Graphic> >& rGra
     }
 }
 
-Graphic GraphicFilter::ImportUnloadedGraphic(SvStream& rIStream, sal_uInt64 sizeLimit)
+Graphic GraphicFilter::ImportUnloadedGraphic(SvStream& rIStream, sal_uInt64 sizeLimit,
+                                             Size* pSizeHint)
 {
     Graphic aGraphic;
     sal_uInt16 nFormat = GRFILTER_FORMAT_DONTKNOW;
@@ -1403,7 +1404,7 @@ Graphic GraphicFilter::ImportUnloadedGraphic(SvStream& rIStream, sal_uInt64 size
                 bAnimated = IsGIFAnimated(aMemoryStream);
             }
             aGraphic.SetGfxLink(std::make_shared<GfxLink>(std::move(pGraphicContent), nGraphicContentSize, eLinkType));
-            aGraphic.ImplGetImpGraphic()->ImplSetPrepared(bAnimated);
+            aGraphic.ImplGetImpGraphic()->ImplSetPrepared(bAnimated, pSizeHint);
         }
     }
 

commit 7cd5190a49ac39c702efd8f2cd367e183ec0bd16

tdf#127446 vcl image lazy-load: fix custom size handling of metafiles
This is a regression from commit
acb803b730f2c6bd82e39beab58949ec14f85eb0 (tdf#125591 DOC import:
lazy-load metafiles with explicit size, 2019-06-11), which assumed that
once maSwapInfo.maPrefSize stores the preferred size, it'll be set on
the Graphic when it's loaded later.

It seems there was no support for that, it was just an accident that the
guessed size of the metafile was about right.

Handle this explicitly in ImpGraphic::loadPrepared(), so that the bugdoc
(which has a custom preferred size) aspect ratio is correct.

Change-Id: Ic7c4009ad6723a2e16129d27600c904311ff3daf

vcl/source/gdi/impgraph.cxx
@@ -1562,7 +1562,15 @@ bool ImpGraphic::loadPrepared()
     if (mpGfxLink->LoadNative(aGraphic))
     {
         GraphicExternalLink aLink = maGraphicExternalLink;
+
+        Size aPrefSize = maSwapInfo.maPrefSize;
         *this = *aGraphic.ImplGetImpGraphic();
+        if (aPrefSize.getWidth() && aPrefSize.getHeight())
+        {
+            // Use custom preferred size if it was set when the graphic was still unloaded.
+            ImplSetPrefSize(aPrefSize);
+        }
+
         maGraphicExternalLink = aLink;
 
         return true;

vcl/CppunitTest_vcl_graphic_test.mk
@@ -48,6 +48,8 @@ $(eval $(call gb_CppunitTest_use_components,vcl_graphic_test,\
     i18npool/util/i18npool \
     ucb/source/core/ucb1 \
     unotools/util/utl \
+    emfio/emfio \
+    drawinglayer/drawinglayer \
 ))
 
 $(eval $(call gb_CppunitTest_use_configuration,vcl_graphic_test))

vcl/qa/cppunit/GraphicTest.cxx
@@ -27,10 +27,12 @@ class GraphicTest : public CppUnit::TestFixture
 {
     void testUnloadedGraphic();
     void testUnloadedGraphicLoading();
+    void testUnloadedGraphicWmf();
 
     CPPUNIT_TEST_SUITE(GraphicTest);
     CPPUNIT_TEST(testUnloadedGraphic);
     CPPUNIT_TEST(testUnloadedGraphicLoading);
+    CPPUNIT_TEST(testUnloadedGraphicWmf);
     CPPUNIT_TEST_SUITE_END();
 };
 
@@ -128,6 +130,31 @@ void GraphicTest::testUnloadedGraphicLoading()
             }
         }
     }
+}
+
+void GraphicTest::testUnloadedGraphicWmf()
+{
+    // Create some in-memory WMF data, set its own preferred size to 99x99.
+    BitmapEx aBitmapEx = createBitmap();
+    SvMemoryStream aStream;
+    GraphicFilter& rGraphicFilter = GraphicFilter::GetGraphicFilter();
+    sal_uInt16 nFilterFormat = rGraphicFilter.GetExportFormatNumberForShortName("wmf");
+    Graphic aGraphic(aBitmapEx);
+    aGraphic.SetPrefSize(Size(99, 99));
+    aGraphic.SetPrefMapMode(MapMode(MapUnit::Map100thMM));
+    rGraphicFilter.ExportGraphic(aGraphic, "none", aStream, nFilterFormat);
+    aStream.Seek(STREAM_SEEK_TO_BEGIN);
+
+    // Now lazy-load this WMF data, with a custom preferred size of 42x42.
+    Size aMtfSize100(42, 42);
+    aGraphic = rGraphicFilter.ImportUnloadedGraphic(aStream, 0, &aMtfSize100);
+    aGraphic.makeAvailable();
+
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 42x42
+    // - Actual  : 99x99
+    // i.e. we the custom preferred size was lost after lazy-load.
+    CPPUNIT_ASSERT_EQUAL(Size(42, 42), aGraphic.GetPrefSize());
 }
 
 } // namespace