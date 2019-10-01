LibreOffice/core

commit 78b9dac2ee77bf6efc1298962cbeca284db5b00a

remove internal use of 16-bit packed formats
none of our supported hardware uses these any more

Change-Id: Ic95d6df619a05df0bec1f5870596cb2bb3bcc6cb
Reviewed-on: https://gerrit.libreoffice.org/79476
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/unx/generic/gdi/salbmp.cxx
@@ -120,7 +120,6 @@ std::unique_ptr<BitmapBuffer> X11SalBitmap::ImplCreateDIB(
            nBitCount ==  1
         || nBitCount ==  4
         || nBitCount ==  8
-        || nBitCount == 16
         || nBitCount == 24
         , "Unsupported BitCount!"
     );
@@ -148,37 +147,10 @@ std::unique_ptr<BitmapBuffer> X11SalBitmap::ImplCreateDIB(
         case 1: pDIB->mnFormat |= ScanlineFormat::N1BitMsbPal; break;
         case 4: pDIB->mnFormat |= ScanlineFormat::N4BitMsnPal; break;
         case 8: pDIB->mnFormat |= ScanlineFormat::N8BitPal; break;
-#ifdef OSL_BIGENDIAN
-        case 16:
-        {
-            pDIB->mnFormat|= ScanlineFormat::N16BitTcMsbMask;
-            ColorMaskElement aRedMask(0xf800);
-            aRedMask.CalcMaskShift();
-            ColorMaskElement aGreenMask(0x07e0);
-            aGreenMask.CalcMaskShift();
-            ColorMaskElement aBlueMask(0x001f);
-            aBlueMask.CalcMaskShift();
-            pDIB->maColorMask = ColorMask(aRedMask, aGreenMask, aBlueMask);
-            break;
-        }
-#else
-        case 16:
-        {
-            pDIB->mnFormat|= ScanlineFormat::N16BitTcLsbMask;
-            ColorMaskElement aRedMask(0xf800);
-            aRedMask.CalcMaskShift();
-            ColorMaskElement aGreenMask(0x07e0);
-            aGreenMask.CalcMaskShift();
-            ColorMaskElement aBlueMask(0x001f);
-            aBlueMask.CalcMaskShift();
-            pDIB->maColorMask = ColorMask(aRedMask, aGreenMask, aBlueMask);
-            break;
-        }
-#endif
+        case 24: pDIB->mnFormat |= ScanlineFormat::N24BitTcBgr; break;
         default:
+            assert(false);
             nBitCount = 24;
-            [[fallthrough]];
-        case 24:
             pDIB->mnFormat |= ScanlineFormat::N24BitTcBgr;
         break;
     }
@@ -291,27 +263,6 @@ std::unique_ptr<BitmapBuffer> X11SalBitmap::ImplCreateDIB(
                 }
                 break;
 
-                case 16:
-                {
-                    ColorMaskElement aRedMask(pImage->red_mask);
-                    aRedMask.CalcMaskShift();
-                    ColorMaskElement aGreenMask(pImage->green_mask);
-                    aGreenMask.CalcMaskShift();
-                    ColorMaskElement aBlueMask(pImage->blue_mask);
-                    aBlueMask.CalcMaskShift();
-                    aSrcBuf.maColorMask = ColorMask(aRedMask, aGreenMask, aBlueMask);
-
-                    if( LSBFirst == pImage->byte_order )
-                    {
-                        aSrcBuf.mnFormat |= ScanlineFormat::N16BitTcLsbMask;
-                    }
-                    else
-                    {
-                        aSrcBuf.mnFormat |= ScanlineFormat::N16BitTcMsbMask;
-                    }
-                }
-                break;
-
                 case 24:
                 {
                     if( ( LSBFirst == pImage->byte_order ) && ( pImage->red_mask == 0xFF ) )
@@ -335,6 +286,8 @@ std::unique_ptr<BitmapBuffer> X11SalBitmap::ImplCreateDIB(
                                             );
                 }
                 break;
+
+                default: assert(false);
             }
 
             BitmapPalette& rPal = aSrcBuf.maPalette;
@@ -452,33 +405,6 @@ XImage* X11SalBitmap::ImplCreateXImage(
                     nDstFormat |= ScanlineFormat::N8BitPal;
                 break;
 
-                case 16:
-                {
-                    #ifdef OSL_BIGENDIAN
-
-                    if( MSBFirst == pImage->byte_order )
-                        nDstFormat |= ScanlineFormat::N16BitTcMsbMask;
-                    else
-                        nDstFormat |= ScanlineFormat::N16BitTcLsbMask;
-
-                    #else /* OSL_LITENDIAN */
-
-                    nDstFormat |= ScanlineFormat::N16BitTcLsbMask;
-                    if( MSBFirst == pImage->byte_order )
-                        pImage->byte_order = LSBFirst;
-
-                    #endif
-
-                    ColorMaskElement aRedMask(pImage->red_mask);
-                    aRedMask.CalcMaskShift();
-                    ColorMaskElement aGreenMask(pImage->green_mask);
-                    aGreenMask.CalcMaskShift();
-                    ColorMaskElement aBlueMask(pImage->blue_mask);
-                    aBlueMask.CalcMaskShift();
-                    xMask.reset(new ColorMask(aRedMask, aGreenMask, aBlueMask));
-                }
-                break;
-
                 case 24:
                 {
                     if( ( LSBFirst == pImage->byte_order ) && ( pImage->red_mask == 0xFF ) )
@@ -502,6 +428,8 @@ XImage* X11SalBitmap::ImplCreateXImage(
                                         );
                 }
                 break;
+
+                default: assert(false);
             }
 
             if( pImage->depth == 1 )

vcl/opengl/salbmp.cxx
@@ -56,10 +56,6 @@ bool determineTextureFormat(sal_uInt16 nBits, GLenum& nFormat, GLenum& nType)
         nFormat = GL_LUMINANCE;
         nType = GL_UNSIGNED_BYTE;
         return true;
-    case 16:
-        nFormat = GL_RGB;
-        nType = GL_UNSIGNED_SHORT_5_6_5;
-        return true;
     case 24:
         nFormat = GL_RGB;
         nType = GL_UNSIGNED_BYTE;
@@ -77,7 +73,7 @@ bool determineTextureFormat(sal_uInt16 nBits, GLenum& nFormat, GLenum& nType)
 
 bool isValidBitCount( sal_uInt16 nBitCount )
 {
-    return (nBitCount == 1) || (nBitCount == 4) || (nBitCount == 8) || (nBitCount == 16) || (nBitCount == 24) || (nBitCount == 32);
+    return (nBitCount == 1) || (nBitCount == 4) || (nBitCount == 8) || (nBitCount == 24) || (nBitCount == 32);
 }
 
 sal_uInt32 lclBytesPerRow(sal_uInt16 nBits, int nWidth)
@@ -87,7 +83,6 @@ sal_uInt32 lclBytesPerRow(sal_uInt16 nBits, int nWidth)
     case 1:  return (nWidth + 7) >> 3;
     case 4:  return (nWidth + 1) >> 1;
     case 8:  return  nWidth;
-    case 16: return  nWidth * 2;
     case 24: return  nWidth * 3;
     case 32: return  nWidth * 4;
     default:
@@ -493,7 +488,7 @@ GLuint OpenGLSalBitmap::CreateTexture()
 
     if (mpUserBuffer != nullptr)
     {
-        if( mnBits == 16 || mnBits == 24 || mnBits == 32 )
+        if( mnBits == 24 || mnBits == 32 )
         {
             // no conversion needed for truecolor
             pData = mpUserBuffer.get();
@@ -579,7 +574,7 @@ bool OpenGLSalBitmap::ReadTexture()
     xContext->state().scissor().disable();
     xContext->state().stencil().disable();
 
-    if ((mnBits == 8 && maPalette.IsGreyPalette()) || mnBits == 16 || mnBits == 24 || mnBits == 32)
+    if ((mnBits == 8 && maPalette.IsGreyPalette()) || mnBits == 24 || mnBits == 32)
     {
         determineTextureFormat(mnBits, nFormat, nType);
 
@@ -803,18 +798,6 @@ BitmapBuffer* OpenGLSalBitmap::AcquireBuffer( BitmapAccessMode nMode )
         case 8:
             pBuffer->mnFormat = ScanlineFormat::N8BitPal;
             break;
-        case 16:
-        {
-            pBuffer->mnFormat = ScanlineFormat::N16BitTcMsbMask;
-            ColorMaskElement aRedMask(0x0000f800);
-            aRedMask.CalcMaskShift();
-            ColorMaskElement aGreenMask(0x000007e0);
-            aGreenMask.CalcMaskShift();
-            ColorMaskElement aBlueMask(0x0000001f);
-            aBlueMask.CalcMaskShift();
-            pBuffer->maColorMask  = ColorMask(aRedMask, aGreenMask, aBlueMask);
-            break;
-        }
         case 24:
         {
             pBuffer->mnFormat = ScanlineFormat::N24BitTcRgb;
@@ -832,6 +815,7 @@ BitmapBuffer* OpenGLSalBitmap::AcquireBuffer( BitmapAccessMode nMode )
             pBuffer->maColorMask  = ColorMask(aRedMask, aGreenMask, aBlueMask);
             break;
         }
+        default: assert(false);
     }
 
     return pBuffer;

vcl/quartz/salbmp.cxx
@@ -45,10 +45,6 @@
 #include "saldatabasic.hxx"
 #endif
 
-static const unsigned long k16BitRedColorMask   = 0x00007c00;
-static const unsigned long k16BitGreenColorMask = 0x000003e0;
-static const unsigned long k16BitBlueColorMask  = 0x0000001f;
-
 static const unsigned long k32BitRedColorMask   = 0x00ff0000;
 static const unsigned long k32BitGreenColorMask = 0x0000ff00;
 static const unsigned long k32BitBlueColorMask  = 0x000000ff;
@@ -56,7 +52,7 @@ static const unsigned long k32BitBlueColorMask  = 0x000000ff;
 static bool isValidBitCount( sal_uInt16 nBitCount )
 {
     return (nBitCount == 1) || (nBitCount == 4) || (nBitCount == 8) ||
-        (nBitCount == 16) || (nBitCount == 24) || (nBitCount == 32);
+        (nBitCount == 24) || (nBitCount == 32);
 }
 
 QuartzSalBitmap::QuartzSalBitmap()
@@ -216,9 +212,9 @@ bool QuartzSalBitmap::CreateContext()
     CGBitmapInfo aCGBmpInfo = kCGImageAlphaNoneSkipFirst;
 
     // convert data into something accepted by CGBitmapContextCreate()
-    size_t bitsPerComponent = (mnBits == 16) ? 5 : 8;
+    size_t bitsPerComponent = 8;
     sal_uInt32 nContextBytesPerRow = mnBytesPerRow;
-    if( (mnBits == 16) || (mnBits == 32) )
+    if( mnBits == 32 )
     {
         // no conversion needed for truecolor
         m_pContextBuffer = m_pUserBuffer;
@@ -279,11 +275,10 @@ bool QuartzSalBitmap::AllocateUserData()
         case 1:     mnBytesPerRow = (mnWidth + 7) >> 3; break;
         case 4:     mnBytesPerRow = (mnWidth + 1) >> 1; break;
         case 8:     mnBytesPerRow = mnWidth; break;
-        case 16:    mnBytesPerRow = mnWidth << 1; break;
         case 24:    mnBytesPerRow = (mnWidth << 1) + mnWidth; break;
         case 32:    mnBytesPerRow = mnWidth << 2; break;
         default:
-            OSL_FAIL("vcl::QuartzSalBitmap::AllocateUserData(), illegal bitcount!");
+            assert(false && "vcl::QuartzSalBitmap::AllocateUserData(), illegal bitcount!");
         }
     }
 
@@ -368,34 +363,6 @@ public:
         *pData++ = nColor.GetBlue();
         *pData++ = nColor.GetGreen();
         *pData++ = nColor.GetRed();
-    }
-};
-
-class ImplPixelFormat16 : public ImplPixelFormat
-// currently R5G6B5-format for 16bit depth
-{
-    sal_uInt16* pData;
-public:
-
-    virtual void StartLine( sal_uInt8* pLine ) override
-    {
-        pData = reinterpret_cast<sal_uInt16*>(pLine);
-    }
-    virtual void SkipPixel( sal_uInt32 nPixel ) override
-    {
-        pData += nPixel;
-    }
-    virtual Color ReadPixel() override
-    {
-        const Color c( (*pData & 0xf800) >> 8, (*pData & 0x07e0) >> 3 , (*pData & 0x001f) << 3 );
-        pData++;
-        return c;
-    }
-    virtual void WritePixel( Color nColor ) override
-    {
-        *pData++ =  (( nColor.GetRed() & 0xf8 ) << 8 ) |
-                    (( nColor.GetGreen() & 0xfc ) << 3 ) |
-                    (( nColor.GetBlue() & 0xf8 ) >> 3 );
     }
 };
 
@@ -544,7 +511,6 @@ std::unique_ptr<ImplPixelFormat> ImplPixelFormat::GetFormat( sal_uInt16 nBits, c
     case 1: return std::make_unique<ImplPixelFormat1>( rPalette );
     case 4: return std::make_unique<ImplPixelFormat4>( rPalette );
     case 8: return std::make_unique<ImplPixelFormat8>( rPalette );
-    case 16: return std::make_unique<ImplPixelFormat16>();
     case 24: return std::make_unique<ImplPixelFormat24>();
     case 32: return std::make_unique<ImplPixelFormat32>();
     default:
@@ -745,18 +711,6 @@ BitmapBuffer* QuartzSalBitmap::AcquireBuffer( BitmapAccessMode /*nMode*/ )
         case 8:
             pBuffer->mnFormat = ScanlineFormat::N8BitPal;
             break;
-        case 16:
-        {
-            pBuffer->mnFormat = ScanlineFormat::N16BitTcMsbMask;
-            ColorMaskElement aRedMask(k16BitRedColorMask);
-            aRedMask.CalcMaskShift();
-            ColorMaskElement aGreenMask(k16BitGreenColorMask);
-            aGreenMask.CalcMaskShift();
-            ColorMaskElement aBlueMask(k16BitBlueColorMask);
-            aBlueMask.CalcMaskShift();
-            pBuffer->maColorMask  = ColorMask(aRedMask, aGreenMask, aBlueMask);
-            break;
-        }
         case 24:
             pBuffer->mnFormat = ScanlineFormat::N24BitTcBgr;
             break;
@@ -772,6 +726,7 @@ BitmapBuffer* QuartzSalBitmap::AcquireBuffer( BitmapAccessMode /*nMode*/ )
             pBuffer->maColorMask  = ColorMask(aRedMask, aGreenMask, aBlueMask);
             break;
         }
+        default: assert(false);
     }
 
     // some BitmapBuffer users depend on a complete palette

vcl/win/gdi/salbmp.cxx
@@ -701,7 +701,7 @@ sal_uInt16 WinSalBitmap::ImplGetDIBColorCount( HGLOBAL hDIB )
 
 HGLOBAL WinSalBitmap::ImplCreateDIB( const Size& rSize, sal_uInt16 nBits, const BitmapPalette& rPal )
 {
-    SAL_WARN_IF( nBits != 1 && nBits != 4 && nBits != 8 && nBits != 16 && nBits != 24, "vcl", "Unsupported BitCount!" );
+    SAL_WARN_IF( nBits != 1 && nBits != 4 && nBits != 8 && nBits != 24, "vcl", "Unsupported BitCount!" );
 
     HGLOBAL hDIB = nullptr;
 
@@ -840,7 +840,6 @@ BitmapBuffer* WinSalBitmap::AcquireBuffer( BitmapAccessMode /*nMode*/ )
             pBuffer->mnFormat = pBIH->biBitCount == 1 ? ScanlineFormat::N1BitMsbPal :
                                 pBIH->biBitCount == 4 ? ScanlineFormat::N4BitMsnPal :
                                 pBIH->biBitCount == 8 ? ScanlineFormat::N8BitPal :
-                                pBIH->biBitCount == 16 ? ScanlineFormat::N16BitTcLsbMask :
                                 pBIH->biBitCount == 24 ? ScanlineFormat::N24BitTcBgr :
                                 pBIH->biBitCount == 32 ? ScanlineFormat::N32BitTcMask :
                                 ScanlineFormat::NONE;

canvas/source/directx/dx_vcltools.cxx
@@ -180,7 +180,6 @@ namespace dxcanvas
                     //    ScanlineFormat::N1BitMsbPal
                     //    ScanlineFormat::N4BitMsnPal
                     //    ScanlineFormat::N8BitPal
-                    //    ScanlineFormat::N16BitTcLsbMask
                     //    ScanlineFormat::N24BitTcBgr
                     //    ScanlineFormat::N32BitTcMask
 
@@ -253,7 +252,6 @@ namespace dxcanvas
                             // formats, too.
                             case ScanlineFormat::N1BitMsbPal:
                             case ScanlineFormat::N4BitMsnPal:
-                            case ScanlineFormat::N16BitTcLsbMask:
                             case ScanlineFormat::N32BitTcMask:
                             {
                                 Scanline pAScan = pAlphaReadAccess->GetScanline( y );
@@ -281,7 +279,6 @@ namespace dxcanvas
                             case ScanlineFormat::N4BitLsnPal:
                             case ScanlineFormat::N8BitTcMask:
                             case ScanlineFormat::N24BitTcRgb:
-                            case ScanlineFormat::N16BitTcMsbMask:
                             case ScanlineFormat::N32BitTcAbgr:
                             case ScanlineFormat::N32BitTcArgb:
                             case ScanlineFormat::N32BitTcBgra:
@@ -306,7 +303,6 @@ namespace dxcanvas
                     //    ScanlineFormat::N1BitMsbPal
                     //    ScanlineFormat::N4BitMsnPal
                     //    ScanlineFormat::N8BitPal
-                    //    ScanlineFormat::N16BitTcLsbMask
                     //    ScanlineFormat::N24BitTcBgr
                     //    ScanlineFormat::N32BitTcMask
 
@@ -392,7 +388,6 @@ namespace dxcanvas
                             // formats, too.
                             case ScanlineFormat::N1BitMsbPal:
                             case ScanlineFormat::N4BitMsnPal:
-                            case ScanlineFormat::N16BitTcLsbMask:
                             case ScanlineFormat::N32BitTcMask:
                             {
                                 Scanline pMScan = pMaskReadAccess->GetScanline( y );
@@ -419,7 +414,6 @@ namespace dxcanvas
                             case ScanlineFormat::N4BitLsnPal:
                             case ScanlineFormat::N8BitTcMask:
                             case ScanlineFormat::N24BitTcRgb:
-                            case ScanlineFormat::N16BitTcMsbMask:
                             case ScanlineFormat::N32BitTcAbgr:
                             case ScanlineFormat::N32BitTcArgb:
                             case ScanlineFormat::N32BitTcBgra:

include/vcl/Scanline.hxx
@@ -37,9 +37,6 @@ enum class ScanlineFormat {
     N8BitPal          = 0x00000010,
     N8BitTcMask       = 0x00000020,
 
-    N16BitTcMsbMask   = 0x00000040,
-    N16BitTcLsbMask   = 0x00000080,
-
     N24BitTcBgr       = 0x00000100,
     N24BitTcRgb       = 0x00000200,
 
@@ -54,7 +51,7 @@ enum class ScanlineFormat {
 
 namespace o3tl
 {
-    template<> struct typed_flags<ScanlineFormat> : is_typed_flags<ScanlineFormat, 0x0001fbff> {};
+    template<> struct typed_flags<ScanlineFormat> : is_typed_flags<ScanlineFormat, 0x0001fb3f> {};
 }
 
 inline ScanlineFormat RemoveScanline(ScanlineFormat nFormat)

include/vcl/bitmapaccess.hxx
@@ -247,8 +247,6 @@ public:
     static BitmapColor GetPixelForN4BitLsnPal(ConstScanline pScanline, long nX, const ColorMask& rMask);
     static BitmapColor GetPixelForN8BitPal(ConstScanline pScanline, long nX, const ColorMask& rMask);
     static BitmapColor GetPixelForN8BitTcMask(ConstScanline pScanline, long nX, const ColorMask& rMask);
-    static BitmapColor GetPixelForN16BitTcMsbMask(ConstScanline pScanline, long nX, const ColorMask& rMask);
-    static BitmapColor GetPixelForN16BitTcLsbMask(ConstScanline pScanline, long nX, const ColorMask& rMask);
     static BitmapColor GetPixelForN24BitTcBgr(ConstScanline pScanline, long nX, const ColorMask& rMask);
     static BitmapColor GetPixelForN24BitTcRgb(ConstScanline pScanline, long nX, const ColorMask& rMask);
     static BitmapColor GetPixelForN32BitTcAbgr(ConstScanline pScanline, long nX, const ColorMask& rMask);
@@ -267,8 +265,6 @@ public:
     static void SetPixelForN4BitLsnPal(Scanline pScanline, long nX, const BitmapColor& rBitmapColor, const ColorMask& rMask);
     static void SetPixelForN8BitPal(Scanline pScanline, long nX, const BitmapColor& rBitmapColor, const ColorMask& rMask);
     static void SetPixelForN8BitTcMask(Scanline pScanline, long nX, const BitmapColor& rBitmapColor, const ColorMask& rMask);
-    static void SetPixelForN16BitTcMsbMask(Scanline pScanline, long nX, const BitmapColor& rBitmapColor, const ColorMask& rMask);
-    static void SetPixelForN16BitTcLsbMask(Scanline pScanline, long nX, const BitmapColor& rBitmapColor, const ColorMask& rMask);
     static void SetPixelForN24BitTcBgr(Scanline pScanline, long nX, const BitmapColor& rBitmapColor, const ColorMask& rMask);
     static void SetPixelForN24BitTcRgb(Scanline pScanline, long nX, const BitmapColor& rBitmapColor, const ColorMask& rMask);
     static void SetPixelForN32BitTcAbgr(Scanline pScanline, long nX, const BitmapColor& rBitmapColor, const ColorMask& rMask);

vcl/headless/svpbmp.cxx
@@ -49,7 +49,6 @@ static std::unique_ptr<BitmapBuffer> ImplCreateDIB(
         || nBitCount ==  1
         || nBitCount ==  4
         || nBitCount ==  8
-        || nBitCount == 16
         || nBitCount == 24
         || nBitCount == 32)
         && "Unsupported BitCount!");
@@ -81,22 +80,6 @@ static std::unique_ptr<BitmapBuffer> ImplCreateDIB(
         case 8:
             pDIB->mnFormat = ScanlineFormat::N8BitPal;
             break;
-        case 16:
-        {
-#ifdef OSL_BIGENDIAN
-            pDIB->mnFormat= ScanlineFormat::N16BitTcMsbMask;
-#else
-            pDIB->mnFormat= ScanlineFormat::N16BitTcLsbMask;
-#endif
-            ColorMaskElement aRedMask(0xf800);
-            aRedMask.CalcMaskShift();
-            ColorMaskElement aGreenMask(0x07e0);
-            aGreenMask.CalcMaskShift();
-            ColorMaskElement aBlueMask(0x001f);
-            aBlueMask.CalcMaskShift();
-            pDIB->maColorMask = ColorMask(aRedMask, aGreenMask, aBlueMask);
-            break;
-        }
         case 24:
             pDIB->mnFormat = SVP_24BIT_FORMAT;
             break;

commit a5b991cf517feac17f55848e5df953afd6ec44bc

tdf#127858 Assertion when trying to edit a form (gen backend)
regression from
	commit 78b9dac2ee77bf6efc1298962cbeca284db5b00a
    remove internal use of 16-bit packed formats

we are trying to create a 32-bit image in
drawinglayer::primitive2d::createDefaultCross_3x3

Change-Id: Iaf19326b1e2c11c5d535dd8f317086bebbaaebec

vcl/unx/generic/gdi/salbmp.cxx
@@ -149,7 +149,7 @@ std::unique_ptr<BitmapBuffer> X11SalBitmap::ImplCreateDIB(
         case 8: pDIB->mnFormat |= ScanlineFormat::N8BitPal; break;
         case 24: pDIB->mnFormat |= ScanlineFormat::N24BitTcBgr; break;
         default:
-            assert(false);
+            SAL_WARN("vcl.gdi", "32-bit images not supported, converting to 24-bit");
             nBitCount = 24;
             pDIB->mnFormat |= ScanlineFormat::N24BitTcBgr;
         break;