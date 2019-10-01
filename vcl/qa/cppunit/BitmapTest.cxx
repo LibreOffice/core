72862bf5e995a383a023ba5510c5eeccfb7bd31f
Luboš Luňák <l.lunak@collabora.com>
2019-10-01 17:40:50 +0200

    better handling of bit depths in skia code
    Change-Id: Ifb3103cc3494bc55a562d4b6a16b59a044782416

--- vcl/qa/cppunit/BitmapTest.cxx
+++ vcl/qa/cppunit/BitmapTest.cxx
@@ -384,9 +384,6 @@ void BitmapTest::testConvert()
             CPPUNIT_ASSERT_EQUAL(sal_uInt32(30), pReadAccess->GetScanlineSize());
         else
 #endif
-            if (SkiaHelper::isVCLSkiaEnabled())
-            CPPUNIT_ASSERT_EQUAL(sal_uInt32(40), pReadAccess->GetScanlineSize());
-        else
             CPPUNIT_ASSERT_EQUAL(sal_uInt32(32), pReadAccess->GetScanlineSize());
 #else
 #if defined(_WIN32)
