LibreOffice/core

commit 48480d4f19d2fb92ca4ae0527eec4753cdc439c0

make tools::Rectangle::getBottom return 0 when empty
LayoutConverter::calcAbsRectangle needed to be tweaked because
we now end up with a zero width/height instead of a large
negative number.

Change-Id: I81f04759a1d5bf6f44753a1701596796fad40567
Reviewed-on: https://gerrit.libreoffice.org/75610
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

oox/source/drawingml/chart/converterbase.cxx
@@ -350,18 +350,18 @@ bool LayoutConverter::calcAbsRectangle( awt::Rectangle& orRect ) const
 {
     if( !mrModel.mbAutoLayout )
     {
-        awt::Size rChartSize=getChartSize();
-        if( (rChartSize.Width < 0) || (rChartSize.Height < 0) )
+        awt::Size aChartSize=getChartSize();
+        if( aChartSize.Width <= 0 || aChartSize.Height <= 0 )
         {
-        rChartSize.Width = 16000;
-        rChartSize.Height = 9000;
+            aChartSize.Width = 16000;
+            aChartSize.Height = 9000;
         }
-        orRect.X = lclCalcPosition( rChartSize.Width,  mrModel.mfX, mrModel.mnXMode );
-        orRect.Y = lclCalcPosition( rChartSize.Height, mrModel.mfY, mrModel.mnYMode );
+        orRect.X = lclCalcPosition( aChartSize.Width,  mrModel.mfX, mrModel.mnXMode );
+        orRect.Y = lclCalcPosition( aChartSize.Height, mrModel.mfY, mrModel.mnYMode );
         if( (orRect.X >= 0) && (orRect.Y >= 0) )
         {
-            orRect.Width  = lclCalcSize( orRect.X, rChartSize.Width,  mrModel.mfW, mrModel.mnWMode );
-            orRect.Height = lclCalcSize( orRect.Y, rChartSize.Height, mrModel.mfH, mrModel.mnHMode );
+            orRect.Width  = lclCalcSize( orRect.X, aChartSize.Width,  mrModel.mfW, mrModel.mnWMode );
+            orRect.Height = lclCalcSize( orRect.Y, aChartSize.Height, mrModel.mfH, mrModel.mnHMode );
             return (orRect.Width > 0) && (orRect.Height > 0);
         }
     }

include/tools/gen.hxx
@@ -457,7 +457,7 @@ public:
     /// Returns the difference between right and left, assuming the range includes one end, but not the other.
     long                getWidth() const;
     /// Returns the difference between bottom and top, assuming the range includes one end, but not the other.
-    long                getHeight() const { return nBottom - nTop; }
+    long                getHeight() const;
     /// Set the left edge of the rectangle to x, preserving the width
     void                setX( long x );
     /// Set the top edge of the rectangle to y, preserving the height

tools/source/generic/gen.cxx
@@ -308,4 +308,11 @@ long tools::Rectangle::getWidth() const
 {
     return nRight == RECT_EMPTY ? 0 : nRight - nLeft;
 }
+
+/// Returns the difference between bottom and top, assuming the range includes one end, but not the other.
+long tools::Rectangle::getHeight() const
+{
+    return nBottom == RECT_EMPTY ? 0 : nBottom - nTop;
+}
+
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

commit 695c89cce72d682d987fd697ed8073ae54802c9c

tdf#127907 tdf#126724 Chart DOCX import: fix axis title position
Custom position of axis title reset to default when
the document is opened in LibreOffice.

Also fix tdf#126724 which was a regression from commit:
48480d4f19d2fb92ca4ae0527eec4753cdc439c0
(make tools::Rectangle::getBottom return 0 when empty)

Note: The y coordinate of Y axis title position is still
wrong because of another older bug.

Change-Id: I449ad3b2252e05ab38d6752512d822c355ff8606

oox/inc/drawingml/chart/converterbase.hxx
@@ -85,6 +85,8 @@ protected:
                         getChartDocument() const;
     /** Returns the position and size of the chart shape in 1/100 mm. */
     const css::awt::Size& getChartSize() const;
+    /** Returns the default position and size of the chart shape in 1/100 mm. */
+    static css::awt::Size getDefaultPageSize() { return css::awt::Size(16000, 9000); }
     /** Returns the object formatter. */
     ObjectFormatter&    getFormatter() const;
 

oox/source/drawingml/chart/converterbase.cxx
@@ -350,11 +350,10 @@ bool LayoutConverter::calcAbsRectangle( awt::Rectangle& orRect ) const
 {
     if( !mrModel.mbAutoLayout )
     {
-        awt::Size aChartSize=getChartSize();
+        awt::Size aChartSize = getChartSize();
         if( aChartSize.Width <= 0 || aChartSize.Height <= 0 )
         {
-            aChartSize.Width = 16000;
-            aChartSize.Height = 9000;
+            aChartSize = getDefaultPageSize();
         }
         orRect.X = lclCalcPosition( aChartSize.Width,  mrModel.mfX, mrModel.mnXMode );
         orRect.Y = lclCalcPosition( aChartSize.Height, mrModel.mfY, mrModel.mnYMode );
@@ -396,10 +395,14 @@ void LayoutConverter::convertFromModel( const Reference< XShape >& rxShape, doub
 {
     if( !mrModel.mbAutoLayout )
     {
-        const awt::Size& rChartSize = getChartSize();
+        awt::Size aChartSize = getChartSize();
+        if( aChartSize.Width <= 0 || aChartSize.Height <= 0 )
+        {
+            aChartSize = getDefaultPageSize();
+        }
         awt::Point aShapePos(
-            lclCalcPosition( rChartSize.Width,  mrModel.mfX, mrModel.mnXMode ),
-            lclCalcPosition( rChartSize.Height, mrModel.mfY, mrModel.mnYMode ) );
+            lclCalcPosition( aChartSize.Width,  mrModel.mfX, mrModel.mnXMode ),
+            lclCalcPosition( aChartSize.Height, mrModel.mfY, mrModel.mnYMode ) );
         if( (aShapePos.X >= 0) && (aShapePos.Y >= 0) )
         {
             // the call to XShape.getSize() may recalc the chart view

chart2/qa/extras/chart2import.cxx
@@ -21,6 +21,7 @@
 #include <com/sun/star/drawing/FillStyle.hpp>
 #include <com/sun/star/drawing/LineStyle.hpp>
 #include <com/sun/star/chart/XAxisXSupplier.hpp>
+#include <com/sun/star/chart/XAxisYSupplier.hpp>
 #include <com/sun/star/chart/MissingValueTreatment.hpp>
 #include <com/sun/star/chart2/TickmarkStyle.hpp>
 #include <com/sun/star/chart2/SymbolStyle.hpp>
@@ -108,6 +109,7 @@ public:
     void testAxisTitleDefaultRotationXLSX();
     void testSecondaryAxisTitleDefaultRotationXLSX();
     void testAxisTitleRotationXLSX();
+    void testAxisTitlePositionDOCX();
     void testCombinedChartAttachedAxisXLSX();
 
     void testTdf90510(); // Pie chart label placement settings(XLS)
@@ -200,6 +202,7 @@ public:
     CPPUNIT_TEST(testAxisTitleDefaultRotationXLSX);
     CPPUNIT_TEST(testSecondaryAxisTitleDefaultRotationXLSX);
     CPPUNIT_TEST(testAxisTitleRotationXLSX);
+    CPPUNIT_TEST(testAxisTitlePositionDOCX);
     CPPUNIT_TEST(testCombinedChartAttachedAxisXLSX);
     CPPUNIT_TEST(testTdf90510);
     CPPUNIT_TEST(testTdf109858);
@@ -1503,6 +1506,39 @@ void Chart2ImportTest::testAxisTitleRotationXLSX()
         CPPUNIT_ASSERT_EQUAL(270.0, nRotation);
     }
 
+}
+
+void Chart2ImportTest::testAxisTitlePositionDOCX()
+{
+    load("/chart2/qa/extras/data/docx/", "testAxisTitlePosition.docx");
+    uno::Reference< chart::XDiagram > mxDiagram;
+    uno::Reference< drawing::XShape > xAxisTitle;
+    uno::Reference< chart::XChartDocument > xChartDoc = getChartDocFromWriter(0);
+    CPPUNIT_ASSERT_MESSAGE("failed to load chart", xChartDoc.is());
+    mxDiagram.set(xChartDoc->getDiagram());
+    CPPUNIT_ASSERT(mxDiagram.is());
+    // test X Axis title position
+    uno::Reference< chart::XAxisXSupplier > xAxisXSupp(mxDiagram, uno::UNO_QUERY);
+    CPPUNIT_ASSERT(xAxisXSupp.is());
+
+    xAxisTitle = xAxisXSupp->getXAxisTitle();
+    CPPUNIT_ASSERT(xAxisTitle.is());
+
+    awt::Point aPos = xAxisTitle->getPosition();
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(10640), static_cast<sal_Int32>(aPos.X));
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(7157), static_cast<sal_Int32>(aPos.Y));
+
+    // test Y Axis title position
+    uno::Reference< chart::XAxisYSupplier > xAxisYSupp(mxDiagram, uno::UNO_QUERY);
+    CPPUNIT_ASSERT(xAxisYSupp.is());
+
+    xAxisTitle = xAxisYSupp->getYAxisTitle();
+    CPPUNIT_ASSERT(xAxisTitle.is());
+
+    aPos = xAxisTitle->getPosition();
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(387), static_cast<sal_Int32>(aPos.X));
+    // y coordinate is still wrong because of another older bug!
+    /*CPPUNIT_ASSERT_EQUAL(sal_Int32(1535), static_cast<sal_Int32>(aPos.Y));*/
 }
 
 void Chart2ImportTest::testCombinedChartAttachedAxisXLSX()