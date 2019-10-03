cf573269e5ccf877d227bdee766bb9043aba3c99
Caolán McNamara <caolanm@redhat.com>
2019-10-03 17:04:10 +0100

    tdf#127529 vertical text not drawn in slideshow canvas
    because the canvas text drawing impl falls back to using an OutputDevice view
    of the canvas to use the vcl text drawing apis to achieve vertical text
    
    To get an OutputDevice view of the canvas there is a specific VirtualDevice
    ctor available to create a VirtualDevice that, unlike the normal case, doesn't
    have its own specific backing buffer, but instead draws to the underlying target
    provided via the SystemGraphicsData arg
    
    The svp/gtk impl missed that understanding and provided an ordinary
    VirtualDevice with its own backing buffer, not a VirtualDevice that would draw
    to the expected target surface of the canvas. So the vertical text was drawn to
    a different surface than the intended one, and was just discarded.
    
    The cairo use in the canvas long precedes the use of cairo in vcl itself.
    
    Seeing as text is now rendered with cairo in all cases where the canvas uses
    cairo its probably now pointless for canvas to have its own text rendering
    path.
    
    Change-Id: Ie3b0a43ca2b746cbfe25e2d0415315b3d5403cd2

--- vcl/unx/gtk/gtkinst.cxx
+++ vcl/unx/gtk/gtkinst.cxx
@@ -334,10 +334,12 @@ std::unique_ptr<SalVirtualDevice> GtkInstance::CreateVirtualDevice( SalGraphics 
 {
     EnsureInit();
 #if GTK_CHECK_VERSION(3,0,0)
-    (void) pGd;
     SvpSalGraphics *pSvpSalGraphics = dynamic_cast<SvpSalGraphics*>(pG);
     assert(pSvpSalGraphics);
-    std::unique_ptr<SalVirtualDevice> pNew(new SvpSalVirtualDevice(eFormat, pSvpSalGraphics->getSurface()));
+    // tdf#127529 see SvpSalInstance::CreateVirtualDevice for the rare case of a non-null pPreExistingTarget
+    cairo_surface_t* pPreExistingTarget
+        = pGd ? static_cast<cairo_surface_t*>(pGd->pSurface) : nullptr;
+    std::unique_ptr<SalVirtualDevice> pNew(new SvpSalVirtualDevice(eFormat, pSvpSalGraphics->getSurface(), pPreExistingTarget));
     pNew->SetSize( nDX, nDY );
     return pNew;
 #else
