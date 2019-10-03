a53e56b63367231231b0e5597f0198f407524e5d
Tamas Bunth <tamas.bunth@collabora.co.uk>
2019-10-03 19:49:57 +0200

    tdf#123206 Import/Export chart custom label text
    OOX import supports custom label texts in chart diagrams (produced by
    e.g. double clicking on a data label, and write custom text), but -
    since embedded objects are exported and imported to odf right after migration -
    it is not displayed in case of a Writer document.
    
    In order to make it work, we have to support custom label text in the
    odf structure. This commit only allows the import/export of pure text,
    it should be improved to store and load formatted string.
    
    A new XML token is added, which currently refers to an attribute of the
    chart:data-point tag. If we want to store formatted string, something
    more clever has to be done.
    
    Change-Id: I80c4a3a0dbcf59f1dc732d795fb716da318411cb

--- xmloff/source/chart/SchXMLPlotAreaContext.cxx
+++ xmloff/source/chart/SchXMLPlotAreaContext.cxx
@@ -609,6 +609,7 @@ void SchXMLDataPointContext::StartElement( const uno::Reference< xml::sax::XAttr
     sal_Int16 nAttrCount = xAttrList.is()? xAttrList->getLength(): 0;
     OUString sAutoStyleName;
     sal_Int32 nRepeat = 1;
+    OUString sCustomLabelField;
 
     for( sal_Int16 i = 0; i < nAttrCount; i++ )
     {
@@ -623,6 +624,13 @@ void SchXMLDataPointContext::StartElement( const uno::Reference< xml::sax::XAttr
             else if( IsXMLToken( aLocalName, XML_REPEATED ) )
                 nRepeat = xAttrList->getValueByIndex( i ).toInt32();
         }
+        else if( nPrefix == XML_NAMESPACE_LO_EXT)
+        {
+            if( IsXMLToken( aLocalName, XML_CUSTOM_LABEL_FIELD))
+            {
+                sCustomLabelField = xAttrList->getValueByIndex( i );
+            }
+        }
     }
 
     if( !sAutoStyleName.isEmpty())
@@ -631,6 +639,7 @@ void SchXMLDataPointContext::StartElement( const uno::Reference< xml::sax::XAttr
             DataRowPointStyle::DATA_POINT,
             m_xSeries, mrIndex, nRepeat, sAutoStyleName );
         aStyle.mbSymbolSizeForSeriesIsMissingInFile = mbSymbolSizeForSeriesIsMissingInFile;
+        aStyle.msCustomLabelField = sCustomLabelField;
         mrStyleVector.push_back( aStyle );
     }
     mrIndex += nRepeat;
