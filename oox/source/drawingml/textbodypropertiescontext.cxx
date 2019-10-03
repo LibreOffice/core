96d3c1ad905e4c694df4dccba51079b8f0a54d32
Gülşah Köse <gulsah.kose@collabora.com>
2019-10-03 08:37:00 +0300

    tdf#126060 Handle text camera z rotation while pptx import.
    Reviewed-on: https://gerrit.libreoffice.org/80587
    Tested-by: Jenkins
    Reviewed-by: Gülşah Köse <gulsah.kose@collabora.com>
    
    tdf#128209 Add check for crash handling 3D rotation.
    
    Handling text 3D z rotation works only for custom shapes.
    Prevent to use it for other services.
    
    Reviewed-on: https://gerrit.libreoffice.org/80974
    Reviewed-by: Andras Timar <andras.timar@collabora.com>
    Tested-by: Andras Timar <andras.timar@collabora.com>
    
    49e0127419dbad8a75f1899db02115a2fe54aacd
    
    Change-Id: Ifa4589fb50affc4c5ffb52288db8533c98ec6dd9
    Reviewed-on: https://gerrit.libreoffice.org/80928
    Reviewed-by: Andras Timar <andras.timar@collabora.com>
    Tested-by: Jenkins
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

--- oox/source/drawingml/textbodypropertiescontext.cxx
+++ oox/source/drawingml/textbodypropertiescontext.cxx
@@ -27,6 +27,7 @@
 #include <drawingml/textbodyproperties.hxx>
 #include <drawingml/textbody.hxx>
 #include <drawingml/customshapegeometry.hxx>
+#include <drawingml/scene3dcontext.hxx>
 #include <oox/drawingml/drawingmltypes.hxx>
 #include <oox/helper/attributelist.hxx>
 #include <oox/helper/propertymap.hxx>
@@ -170,6 +171,12 @@ ContextHandlerRef TextBodyPropertiesContext::onCreateContext( sal_Int32 aElement
                 break;
 
             case A_TOKEN( scene3d ):        // CT_Scene3D
+            {
+                if(mpShapePtr && mpShapePtr->getServiceName() == "com.sun.star.drawing.CustomShape")
+                    return new Scene3DPropertiesContext( *this, mpShapePtr->get3DProperties() );
+
+                break;
+            }
 
             // EG_Text3D
             case A_TOKEN( sp3d ):           // CT_Shape3D
