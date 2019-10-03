ed8b405b106e35d31205299622face59928d5276
Miklos Vajna <vmiklos@collabora.com>
2019-10-03 17:24:21 +0200

    DOCX import: fix interaction of table and paragraph style in table cells
    Both table and paragraph styles can define paragraph properties for the
    content of table cells, e.g. line spacing. The intended behavior is that
    direct formatting has priority, then paragraph style, then the table
    styles.
    
    But in practice table style had priority: table style is turned into a
    set of property name-value pairs by writerfilter/ code, then this is
    applied to the content of a cell using SwXCell::setPropertyValue(). To
    make sure that direct-format-from-table-style doesn't override
    direct-format, there was already a check to not replace properties which
    are set directly.
    
    The problem is that in case the property value comes from a paragraph
    style, that's not direct and still should have priority over
    direct-format-from-table-style.
    
    Fix this by checking for custom values not only in the paragraph's item
    set, but also in its parents. Unless the parent would be the default
    style, which doesn't count. Or in case the paragraph is numbered, which
    complicates the situation, so leave that case unchanged.
    
    Reviewed-on: https://gerrit.libreoffice.org/80146
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Tested-by: Jenkins
    (cherry picked from commit c424a1f509205cfbaa3421bddfd6514b340a798a)
    
    Change-Id: Ib554247cdc51fee0d9a6c7a26aecd38442dfa692

--- sw/qa/extras/unowriter/unowriter.cxx
+++ sw/qa/extras/unowriter/unowriter.cxx
@@ -16,6 +16,8 @@
 #include <com/sun/star/rdf/URIs.hpp>
 #include <com/sun/star/awt/XDevice.hpp>
 #include <com/sun/star/awt/XToolkit.hpp>
+#include <com/sun/star/graphic/XGraphic.hpp>
+#include <com/sun/star/style/LineSpacing.hpp>
 #include <comphelper/propertyvalue.hxx>
 #include <toolkit/helper/vclunohelper.hxx>
 #include <wrtsh.hxx>
@@ -121,6 +123,226 @@ DECLARE_UNOAPI_TEST_FILE(testRenderablePagePosition, "renderable-page-position.o
     awt::Point aPosition2 = aRenderer2["PagePos"].get<awt::Point>();
     CPPUNIT_ASSERT_GREATER(static_cast<sal_Int32>(0), aPosition2.X);
     CPPUNIT_ASSERT_GREATER(aPosition1.Y, aPosition2.Y);
+}
+
+CPPUNIT_TEST_FIXTURE(SwUnoWriter, testPasteListener)
+{
+    loadURL("private:factory/swriter", nullptr);
+
+    // Insert initial string.
+    uno::Reference<text::XTextDocument> xTextDocument(mxComponent, uno::UNO_QUERY);
+    uno::Reference<text::XSimpleText> xBodyText = xTextDocument->getText();
+    xBodyText->insertString(xBodyText->getStart(), "ABCDEF", false);
+
+    // Add paste listener.
+    uno::Reference<text::XPasteBroadcaster> xBroadcaster(mxComponent, uno::UNO_QUERY);
+    uno::Reference<text::XPasteListener> xListener(new PasteListener);
+    auto pListener = static_cast<PasteListener*>(xListener.get());
+    xBroadcaster->addPasteEventListener(xListener);
+
+    // Cut "DE" and then paste it.
+    SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument*>(mxComponent.get());
+    CPPUNIT_ASSERT(pTextDoc);
+    SwWrtShell* pWrtShell = pTextDoc->GetDocShell()->GetWrtShell();
+    CPPUNIT_ASSERT(pWrtShell);
+    pWrtShell->Left(CRSR_SKIP_CHARS, /*bSelect=*/false, 3, /*bBasicCall=*/false);
+    pWrtShell->Right(CRSR_SKIP_CHARS, /*bSelect=*/true, 2, /*bBasicCall=*/false);
+    rtl::Reference<SwTransferable> pTransfer = new SwTransferable(*pWrtShell);
+    pTransfer->Cut();
+    TransferableDataHelper aHelper(pTransfer.get());
+    SwTransferable::Paste(*pWrtShell, aHelper);
+    // Without working listener registration in place, this test would have
+    // failed with 'Expected: DE; Actual:', i.e. the paste listener was not
+    // invoked.
+    CPPUNIT_ASSERT_EQUAL(OUString("DE"), pListener->GetString());
+
+    // Make sure that paste did not overwrite anything.
+    CPPUNIT_ASSERT_EQUAL(OUString("ABCDEF"), xBodyText->getString());
+
+    // Paste again, this time overwriting "BC".
+    pWrtShell->Left(CRSR_SKIP_CHARS, /*bSelect=*/false, 4, /*bBasicCall=*/false);
+    pWrtShell->Right(CRSR_SKIP_CHARS, /*bSelect=*/true, 2, /*bBasicCall=*/false);
+    pListener->GetString().clear();
+    SwTransferable::Paste(*pWrtShell, aHelper);
+    CPPUNIT_ASSERT_EQUAL(OUString("DE"), pListener->GetString());
+
+    // Make sure that paste overwrote "BC".
+    CPPUNIT_ASSERT_EQUAL(OUString("ADEDEF"), xBodyText->getString());
+
+    // Test image paste.
+    SwView& rView = pWrtShell->GetView();
+    OUString aGraphicURL = m_directories.getURLFromSrc(DATA_DIRECTORY) + "test.jpg";
+    rView.InsertGraphic(aGraphicURL, OUString(), /*bAsLink=*/false,
+                        &GraphicFilter::GetGraphicFilter());
+
+    // Test that the pasted image is anchored as-char.
+    SwFlyFrame* pFly = pWrtShell->GetSelectedFlyFrame();
+    CPPUNIT_ASSERT(pFly);
+    SwFrameFormat* pFlyFormat = pFly->GetFormat();
+    CPPUNIT_ASSERT(pFlyFormat);
+    RndStdIds eFlyAnchor = pFlyFormat->GetAnchor().GetAnchorId();
+    // Without the working image listener in place, this test would have
+    // failed, eFlyAnchor was FLY_AT_PARA.
+    CPPUNIT_ASSERT_EQUAL(RndStdIds::FLY_AS_CHAR, eFlyAnchor);
+
+    pTransfer->Cut();
+    pListener->GetString().clear();
+    SwTransferable::Paste(*pWrtShell, aHelper);
+    // Without the working image listener in place, this test would have
+    // failed, the listener was not invoked in case of a graphic paste.
+    CPPUNIT_ASSERT(pListener->GetTextGraphicObject().is());
+    CPPUNIT_ASSERT(pListener->GetString().isEmpty());
+
+    // Deregister paste listener, make sure it's not invoked.
+    xBroadcaster->removePasteEventListener(xListener);
+    pListener->GetString().clear();
+    SwTransferable::Paste(*pWrtShell, aHelper);
+    CPPUNIT_ASSERT(pListener->GetString().isEmpty());
+}
+
+CPPUNIT_TEST_FIXTURE(SwUnoWriter, testImageCommentAtChar)
+{
+    // Load a document with an at-char image in it (and a comment on the image).
+    load(mpTestDocumentPath, "image-comment-at-char.odt");
+    SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument*>(mxComponent.get());
+    CPPUNIT_ASSERT(pTextDoc);
+    SwDoc* pDoc = pTextDoc->GetDocShell()->GetDoc();
+
+    // Verify that we have an annotation mark (comment with a text range) in the document.
+    // Without the accompanying fix in place, this test would have failed, as comments lost their
+    // ranges on load when their range only covered the placeholder character of the comment (which
+    // is also the anchor position of the image).
+    IDocumentMarkAccess* pMarks = pDoc->getIDocumentMarkAccess();
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(1), pMarks->getAnnotationMarksCount());
+
+    uno::Reference<text::XTextRange> xPara = getParagraph(1);
+    CPPUNIT_ASSERT_EQUAL(OUString("Text"),
+                         getProperty<OUString>(getRun(xPara, 1), "TextPortionType"));
+    // Without the accompanying fix in place, this test would have failed with 'Expected:
+    // Annotation; Actual: Frame', i.e. the comment-start portion was after the commented image.
+    CPPUNIT_ASSERT_EQUAL(OUString("Annotation"),
+                         getProperty<OUString>(getRun(xPara, 2), "TextPortionType"));
+    CPPUNIT_ASSERT_EQUAL(OUString("Frame"),
+                         getProperty<OUString>(getRun(xPara, 3), "TextPortionType"));
+    CPPUNIT_ASSERT_EQUAL(OUString("AnnotationEnd"),
+                         getProperty<OUString>(getRun(xPara, 4), "TextPortionType"));
+    CPPUNIT_ASSERT_EQUAL(OUString("Text"),
+                         getProperty<OUString>(getRun(xPara, 5), "TextPortionType"));
+
+    // Without the accompanying fix in place, this test would have failed with 'Expected:
+    // 5892; Actual: 1738', i.e. the anchor pos was between the "aaa" and "bbb" portions, not at the
+    // center of the page (horizontally) where the image is.  On macOS, though, with the fix in
+    // place the actual value consistently is even greater with 6283 now instead of 5892, for
+    // whatever reason.
+    SwView* pView = pDoc->GetDocShell()->GetView();
+    SwPostItMgr* pPostItMgr = pView->GetPostItMgr();
+    for (const auto& pItem : *pPostItMgr)
+    {
+        const SwRect& rAnchor = pItem->pPostIt->GetAnchorRect();
+        CPPUNIT_ASSERT_GREATEREQUAL(static_cast<long>(5892), rAnchor.Left());
+    }
+}
+
+CPPUNIT_TEST_FIXTURE(SwUnoWriter, testViewCursorPageStyle)
+{
+    // Load a document with 2 pages, but a single paragraph.
+    load(mpTestDocumentPath, "view-cursor-page-style.fodt");
+    uno::Reference<frame::XModel> xModel(mxComponent, uno::UNO_QUERY);
+    CPPUNIT_ASSERT(xModel.is());
+    uno::Reference<text::XTextViewCursorSupplier> xController(xModel->getCurrentController(),
+                                                              uno::UNO_QUERY);
+    CPPUNIT_ASSERT(xController.is());
+    uno::Reference<text::XPageCursor> xViewCursor(xController->getViewCursor(), uno::UNO_QUERY);
+    CPPUNIT_ASSERT(xViewCursor.is());
+
+    // Go to the first page, which has an explicit page style.
+    xViewCursor->jumpToPage(1);
+    OUString aActualPageStyleName = getProperty<OUString>(xViewCursor, "PageStyleName");
+    CPPUNIT_ASSERT_EQUAL(OUString("First Page"), aActualPageStyleName);
+
+    // Go to the second page, which is still the first paragraph, but the page style is different,
+    // as the explicit 'First Page' page style has a next style defined (Standard).
+    xViewCursor->jumpToPage(2);
+    aActualPageStyleName = getProperty<OUString>(xViewCursor, "PageStyleName");
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: Standard
+    // - Actual  : First Page
+    // i.e. the cursor position was determined only based on the node index, ignoring the content
+    // index.
+    CPPUNIT_ASSERT_EQUAL(OUString("Standard"), aActualPageStyleName);
+}
+
+CPPUNIT_TEST_FIXTURE(SwUnoWriter, testXTextCursor_setPropertyValues)
+{
+    // Create a new document, type a character, pass a set of property/value pairs consisting of one
+    // unknown property and CharStyleName, assert that it threw UnknownPropertyException (actually
+    // wrapped into WrappedTargetException), and assert the style was set, not discarded.
+    loadURL("private:factory/swriter", nullptr);
+
+    uno::Reference<text::XTextDocument> xTextDocument(mxComponent, uno::UNO_QUERY);
+    uno::Reference<text::XSimpleText> xBodyText = xTextDocument->getText();
+    xBodyText->insertString(xBodyText->getStart(), "x", false);
+
+    uno::Reference<text::XTextCursor> xCursor(xBodyText->createTextCursor());
+    xCursor->goLeft(1, true);
+
+    uno::Reference<beans::XMultiPropertySet> xCursorProps(xCursor, uno::UNO_QUERY);
+    uno::Sequence<OUString> aPropNames = { "OneUnknownProperty", "CharStyleName" };
+    uno::Sequence<uno::Any> aPropValues = { uno::Any(), uno::Any(OUString("Emphasis")) };
+    CPPUNIT_ASSERT_THROW(xCursorProps->setPropertyValues(aPropNames, aPropValues),
+                         lang::WrappedTargetException);
+    CPPUNIT_ASSERT_EQUAL(OUString("Emphasis"),
+                         getProperty<OUString>(xCursorProps, "CharStyleName"));
+}
+
+CPPUNIT_TEST_FIXTURE(SwUnoWriter, testShapeAllowOverlap)
+{
+    // Test the AllowOverlap frame/shape property.
+
+    // Create a new document and insert a rectangle.
+    loadURL("private:factory/swriter", nullptr);
+    uno::Reference<lang::XMultiServiceFactory> xDocument(mxComponent, uno::UNO_QUERY);
+    awt::Point aPoint(1000, 1000);
+    awt::Size aSize(10000, 10000);
+    uno::Reference<drawing::XShape> xShape(
+        xDocument->createInstance("com.sun.star.drawing.RectangleShape"), uno::UNO_QUERY);
+    xShape->setPosition(aPoint);
+    xShape->setSize(aSize);
+    uno::Reference<drawing::XDrawPageSupplier> xDrawPageSupplier(xDocument, uno::UNO_QUERY);
+    xDrawPageSupplier->getDrawPage()->add(xShape);
+
+    // The property is on by default, turn it off & verify.
+    uno::Reference<beans::XPropertySet> xShapeProperties(xShape, uno::UNO_QUERY);
+    xShapeProperties->setPropertyValue("AllowOverlap", uno::makeAny(false));
+    CPPUNIT_ASSERT(!getProperty<bool>(xShapeProperties, "AllowOverlap"));
+
+    // Turn it back to on & verify.
+    xShapeProperties->setPropertyValue("AllowOverlap", uno::makeAny(true));
+    CPPUNIT_ASSERT(getProperty<bool>(xShapeProperties, "AllowOverlap"));
+}
+
+CPPUNIT_TEST_FIXTURE(SwUnoWriter, testTextConvertToTableLineSpacing)
+{
+    // Load a document which has a table with a single cell.
+    // The cell has both a table style and a paragraph style, with different line spacing
+    // heights.
+    load(mpTestDocumentPath, "table-line-spacing.docx");
+    uno::Reference<text::XTextTablesSupplier> xTablesSupplier(mxComponent, uno::UNO_QUERY);
+    uno::Reference<container::XIndexAccess> xTables(xTablesSupplier->getTextTables(),
+                                                    uno::UNO_QUERY);
+    uno::Reference<text::XTextTable> xTable(xTables->getByIndex(0), uno::UNO_QUERY);
+    uno::Reference<table::XCell> xCell = xTable->getCellByName("A1");
+    uno::Reference<text::XText> xCellText(xCell, uno::UNO_QUERY);
+    uno::Reference<text::XTextRange> xParagraph = getParagraphOfText(1, xCellText);
+    style::LineSpacing aLineSpacing
+        = getProperty<style::LineSpacing>(xParagraph, "ParaLineSpacing");
+    // Make sure that we take the line spacing from the paragraph style, not from the table style.
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 388
+    // - Actual  : 635
+    // I.e. the 360 twips line spacing was taken from the table style, not the 220 twips one from
+    // the paragraph style.
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int16>(convertTwipToMm100(220)), aLineSpacing.Height);
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();
