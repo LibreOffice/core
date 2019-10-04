8fe484a989a1bea05dd9d1bc032c859c83966b0b
Caolán McNamara <caolanm@redhat.com>
2019-10-04 15:07:02 +0100

    FileControl can be private inside svtools
    Change-Id: I52edd5166edc9aceeafffed898835fe5bc46880c
    Reviewed-on: https://gerrit.libreoffice.org/80232
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>

--- svtools/source/control/filectrl.cxx
+++ svtools/source/control/filectrl.cxx
@@ -17,7 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#include <svtools/filectrl.hxx>
+#include <filectrl.hxx>
 
 #include <com/sun/star/ui/dialogs/FilePicker.hpp>
 #include <com/sun/star/ui/dialogs/TemplateDescription.hpp>
