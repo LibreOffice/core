cb836a7b150d5914d6b7f81d264ac1a1b87c15df
Caolán McNamara <caolanm@redhat.com>
2019-10-04 13:23:55 +0100

    move file picker only code to fpicker
    Change-Id: I47cc2cb7db396a06a2abeffe4a5d40a039f21c58

---
+++ include/svtools/urlfilter.hxx
@@ -0,0 +1,44 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#ifndef INCLUDED_SVL_URLFILTER_HXX
+#define INCLUDED_SVL_URLFILTER_HXX
+
+#include <svtools/svtdllapi.h>
+#include <tools/wldcrd.hxx>
+#include <vector>
+
+struct SVT_DLLPUBLIC FilterMatch
+{
+private:
+    const OUString&   m_rCompareString;
+public:
+    FilterMatch( const OUString& _rCompareString ) : m_rCompareString( _rCompareString ) { }
+
+    bool operator()( const WildCard& _rMatcher )
+    {
+        return _rMatcher.Matches( m_rCompareString );
+    }
+
+    static void createWildCardFilterList(const OUString& _rFilterList,::std::vector< WildCard >& _rFilters);
+};
+
+#endif // INCLUDED_SVL_URLFILTER_HXX
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
