00bef8ad62c4f76848c188bbd148f732c8883332
Justin Luth <justin_luth@sil.org>
2019-10-05 21:23:04 +0300

    tdf#127316 ooxml/ww8export: DFLT_ESC_*_AUTO - use better formulas
    See tdf#99602 and tdf#120412 for many fixes related to escapement.
    The poor formulas for AUTO style came to light in 6.3.0.1
    with 6.4 commit 32262b0a537207832d7d126d8427d8949b9e821d
    
    Looking at sw/source/core/txtnode/swfont.cxx, we can see in
    CalcEsc() that for superscript the DFLT_ESC_SUPER_AUTO
    is calculated as the difference between the two ascents.
    Because the superscript is smaller, it has a smaller ascent,
    so to reach the same ascent line as the original, we can
    raise its baseline by fontAscent(fA) - superscriptAscent(ssA).
    Since the ascent is approximately 80% of the total fontheight(fH),
    we can calculate the Esc number (percentage * 100) as:
    EscHeight = fA - ssA  (formula in CalcEsc)
    ssA = nProp/100 * fA  (superscript ascent/height reduced by nProp)
    .8 = fA/fHeight       (improvement would be to query font.GetAscent)
    Esc% = EscHeight / fHeight
    
    Esc% = (fA - nProp/100*fA) / fHeight
    Esc% = (1 - nProp/100)*fA/fHeight
    Esc = (100 - nProp) * 0.8
    
    DFLT_ESC_SUB_AUTO subscript is similar with the descent(fD)
    being the remainding 20% of the fontHeight.
    EscHeight = fD - ssD (formula in CalcEsc)
    ssD = nProp/100 * fD (by definition of the function of nProp)
    .2 = fD/fHeight      (an approximation - each font will be different)
    
    Esc% = (fD - nProp/100*fD) / fHeight
    Esc% = (1 - nProp/100)*fD/fHeight
    Esc = (100 - nProp) * 0.2 (in the negative direction)
    
    For RTF, I can see no reason for the ++nProp except
    to try and avoid nProp == 0. It is only .01% after all...
    
    Change-Id: Ia7b9f8784572193dd05cb80afa56c90540c3e676

--- sw/source/filter/ww8/ww8atr.cxx
+++ sw/source/filter/ww8/ww8atr.cxx
@@ -1389,18 +1389,27 @@ void WW8AttributeOutput::CharEscapement( const SvxEscapementItem& rEscapement )
         nEsc = 0;
         nProp = 100;
     }
-    else if ( DFLT_ESC_PROP == nProp )
+    else if ( DFLT_ESC_PROP == nProp || nProp < 1 || nProp > 100 )
     {
         if ( DFLT_ESC_SUB == nEsc || DFLT_ESC_AUTO_SUB == nEsc )
             b = 2;
         else if ( DFLT_ESC_SUPER == nEsc || DFLT_ESC_AUTO_SUPER == nEsc )
             b = 1;
     }
-    // FIXME: these need a better formula. See rtfAttributeOutput
     else if ( DFLT_ESC_AUTO_SUPER == nEsc )
-        nEsc = DFLT_ESC_SUPER;
+    {
+        // Raised by the differences between the ascenders (ascent = baseline to top of highest letter).
+        // The ascent is generally about 80% of the total font height.
+        // That is why DFLT_ESC_PROP (58) leads to 33% (DFLT_ESC_SUPER)
+        nEsc = .8 * (100 - nProp);
+    }
     else if ( DFLT_ESC_AUTO_SUB == nEsc )
-        nEsc = DFLT_ESC_SUB;
+    {
+        // Lowered by the differences between the descenders (descent = baseline to bottom of lowest letter).
+        // The descent is generally about 20% of the total font height.
+        // That is why DFLT_ESC_PROP (58) _originally_ lead to 8% (DFLT_ESC_SUB)
+        nEsc = .2 * -(100 - nProp);
+    }
 
     if ( 0xFF != b )
     {
