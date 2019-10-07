b1321ed8d03eff302359ab71c67f6be9bc6d5ad5
Noel Grandin <noel.grandin@collabora.co.uk>
2019-10-07 15:04:36 +0200

    use cppu::WeakImplHelper in CachedContentResultSetFactory
    Change-Id: I1575e051fe963a4bf73453a7e6b89c9c891d1ff9

--- ucb/source/cacher/cachedcontentresultset.cxx
+++ ucb/source/cacher/cachedcontentresultset.cxx
@@ -2066,39 +2066,6 @@ CachedContentResultSetFactory::~CachedContentResultSetFactory()
 {
 }
 
-
-// CachedContentResultSetFactory XInterface methods.
-void SAL_CALL CachedContentResultSetFactory::acquire()
-    throw()
-{
-    OWeakObject::acquire();
-}
-
-void SAL_CALL CachedContentResultSetFactory::release()
-    throw()
-{
-    OWeakObject::release();
-}
-
-css::uno::Any SAL_CALL CachedContentResultSetFactory::queryInterface( const css::uno::Type & rType )
-{
-    css::uno::Any aRet = cppu::queryInterface( rType,
-                                               static_cast< XTypeProvider* >(this),
-                                               static_cast< XServiceInfo* >(this),
-                                               static_cast< XCachedContentResultSetFactory* >(this)
-                                               );
-    return aRet.hasValue() ? aRet : OWeakObject::queryInterface( rType );
-}
-
-// CachedContentResultSetFactory XTypeProvider methods.
-
-
-XTYPEPROVIDER_IMPL_3( CachedContentResultSetFactory,
-                      XTypeProvider,
-                         XServiceInfo,
-                      XCachedContentResultSetFactory );
-
-
 // CachedContentResultSetFactory XServiceInfo methods.
 
 XSERVICEINFO_COMMOM_IMPL( CachedContentResultSetFactory,
