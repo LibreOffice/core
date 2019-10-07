3c2587a152476cbb0ca4a83138a4c34ec8065b32
Dennis Francis <dennis.francis@collabora.com>
2019-10-07 18:02:04 +0530

    move SSE2sum code to separate cxx file...
    and compile it with -arch:SSE2 if Windows.
    
    This code however gets called only if cpuid::hasSSE2()
    is true, so this does not cause problems with machines
    without SSE2 support.
    
    Change-Id: Ice23ac71d4c577b8811b08c74a3ca500a94fdc09
    Reviewed-on: https://gerrit.libreoffice.org/80847
    Tested-by: Jenkins
    Reviewed-by: Luboš Luňák <l.lunak@collabora.com>

--- sc/source/core/inc/arraysumfunctor.hxx
+++ sc/source/core/inc/arraysumfunctor.hxx
@@ -14,7 +14,6 @@
 #include <cstdint>
 #include <rtl/math.hxx>
 
-#include <tools/simdsupport.hxx>
 #include <tools/simd.hxx>
 #include <tools/cpuid.hxx>
 
@@ -87,56 +86,8 @@ public:
     }
 
 private:
-    double executeSSE2(size_t& i, const double* pCurrent) const
-    {
-#if defined(LO_SSE2_AVAILABLE)
-        double fSum = 0.0;
-        size_t nRealSize = mnSize - i;
-        size_t nUnrolledSize = nRealSize - (nRealSize % 8);
 
-        if (nUnrolledSize > 0)
-        {
-            __m128d sum1 = _mm_setzero_pd();
-            __m128d sum2 = _mm_setzero_pd();
-            __m128d sum3 = _mm_setzero_pd();
-            __m128d sum4 = _mm_setzero_pd();
-
-            for (; i < nUnrolledSize; i += 8)
-            {
-                __m128d load1 = _mm_load_pd(pCurrent);
-                sum1 = _mm_add_pd(sum1, load1);
-                pCurrent += 2;
-
-                __m128d load2 = _mm_load_pd(pCurrent);
-                sum2 = _mm_add_pd(sum2, load2);
-                pCurrent += 2;
-
-                __m128d load3 = _mm_load_pd(pCurrent);
-                sum3 = _mm_add_pd(sum3, load3);
-                pCurrent += 2;
-
-                __m128d load4 = _mm_load_pd(pCurrent);
-                sum4 = _mm_add_pd(sum4, load4);
-                pCurrent += 2;
-            }
-            sum1 = _mm_add_pd(_mm_add_pd(sum1, sum2), _mm_add_pd(sum3, sum4));
-
-            double temp;
-
-            _mm_storel_pd(&temp, sum1);
-            fSum += temp;
-
-            _mm_storeh_pd(&temp, sum1);
-            fSum += temp;
-        }
-        return fSum;
-#else
-        (void) i;
-        (void) pCurrent;
-        return 0.0;
-#endif
-    }
-
+    double executeSSE2(size_t& i, const double* pCurrent) const;
     double executeUnrolled(size_t& i, const double* pCurrent) const
     {
         size_t nRealSize = mnSize - i;
