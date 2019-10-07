fd6ace00f4ae0cfabdb56036226f2340853f4a5d
Noel Grandin <noel.grandin@collabora.co.uk>
2019-10-07 14:38:57 +0200

    use cppu::WeakImplHelper in CommandProcessorInfo
    Change-Id: Ifbb2d50b1d88f0bceed38bddd23858a54b8527dc

--- ucbhelper/source/provider/contentinfo.cxx
+++ ucbhelper/source/provider/contentinfo.cxx
@@ -220,38 +220,6 @@ CommandProcessorInfo::~CommandProcessorInfo()
 }
 
 
-// XInterface methods.
-
-
-void SAL_CALL CommandProcessorInfo::acquire()
-    throw()
-{
-    OWeakObject::acquire();
-}
-
-void SAL_CALL CommandProcessorInfo::release()
-    throw()
-{
-    OWeakObject::release();
-}
-
-css::uno::Any SAL_CALL CommandProcessorInfo::queryInterface( const css::uno::Type & rType )
-{
-    css::uno::Any aRet = cppu::queryInterface( rType,
-                                               static_cast< lang::XTypeProvider* >(this),
-                                               static_cast< css::ucb::XCommandInfo* >(this)
-                    );
-    return aRet.hasValue() ? aRet : OWeakObject::queryInterface( rType );
-}
-
-// XTypeProvider methods.
-
-
-XTYPEPROVIDER_IMPL_2( CommandProcessorInfo,
-                         lang::XTypeProvider,
-                         css::ucb::XCommandInfo );
-
-
 // XCommandInfo methods.
 
 
