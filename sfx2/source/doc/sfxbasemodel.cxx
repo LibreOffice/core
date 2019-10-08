9c23d1c2bb804fca1458a68517a82981513ac100
Vasily Melenchuk <vasily.melenchuk@cib.de>
2019-10-08 11:46:05 +0300

    uno: XModel2->setArgs() now able to set "EncryptionData"
    setArgs is able to set only limited media descriptor parameters.
    Extending this list by one more.
    
    Change-Id: I179a1cfc2cdd7b04becba0d7dfe9740d920ae4ee

--- sfx2/source/doc/sfxbasemodel.cxx
+++ sfx2/source/doc/sfxbasemodel.cxx
@@ -1070,6 +1070,10 @@ void SAL_CALL SfxBaseModel::setArgs(const Sequence<beans::PropertyValue>& aArgs)
         {
             pMedium->GetItemSet()->Put(SfxStringItem(SID_SUGGESTEDSAVEASDIR, sValue));
         }
+        else if (rArg.Name == "EncryptionData")
+        {
+            pMedium->GetItemSet()->Put(SfxUnoAnyItem(SID_ENCRYPTIONDATA, rArg.Value));
+        }
         else
         {
             throw lang::IllegalArgumentException("Setting property not supported: " + rArg.Name,
