LibreOffice/core

commit 3c86ffd8ded628e6f2b4187948a1b1056f6a0f56

tdf#118696 impress navigator: zoom slide/slide object on select
Change-Id: I5656c99f72fb94c791282bbf2fcf5044bcdd8a7a
Reviewed-on: https://gerrit.libreoffice.org/57379
Tested-by: Jenkins
Reviewed-by: Katarina Behrens <Katarina.Behrens@cib.de>

sd/source/ui/docshell/docshel4.cxx
@@ -87,6 +87,8 @@
 
 #include <SdUnoDrawView.hxx>
 
+#include <sfx2/zoomitem.hxx>
+
 using namespace ::com::sun::star;
 using namespace ::com::sun::star::uno;
 using ::sd::framework::FrameworkHelper;
@@ -858,11 +860,14 @@ void DrawDocShell::GotoBookmark(const OUString& rBookmark)
                     pDrawViewShell->SwitchPage(nSdPgNum);
                 }
 
+                // show page
+                SvxZoomItem aZoom;
+                aZoom.SetType( SvxZoomType::WHOLEPAGE );
+                pDrawViewShell->GetDispatcher()->ExecuteList(SID_ATTR_ZOOM, SfxCallMode::ASYNCHRON, { &aZoom });
+
                 if (pObj != nullptr)
                 {
-                    // show and select object
-                    pDrawViewShell->MakeVisible(pObj->GetLogicRect(),
-                        *pDrawViewShell->GetActiveWindow());
+                    // select object
                     pDrawViewShell->GetView()->UnmarkAll();
                     pDrawViewShell->GetView()->MarkObj(
                         pObj,

commit b1bc01fbe83622ea361aab4334c505d0c4b03e58

tdf#128013: Do not show page if object is not set
regression from 3c86ffd8ded628e6f2b4187948a1b1056f6a0f56

Unlike Writer, where links can use tables, illustrations, etc...
as target, in Impress they can only point to views and
not to objects, thus, show the page only if we have an object.
E.g. doubleclicking an object in the navigator

Change-Id: I982c990fe0284b08bb1b25cc7e23e524072faf90

sd/source/ui/docshell/docshel4.cxx
@@ -819,13 +819,13 @@ void DrawDocShell::GotoBookmark(const OUString& rBookmark)
                 pDrawViewShell->SwitchPage(nSdPgNum);
             }
 
-            // show page
-            SvxZoomItem aZoom;
-            aZoom.SetType( SvxZoomType::WHOLEPAGE );
-            pDrawViewShell->GetDispatcher()->ExecuteList(SID_ATTR_ZOOM, SfxCallMode::ASYNCHRON, { &aZoom });
-
             if (pObj != nullptr)
             {
+                // show page
+                SvxZoomItem aZoom;
+                aZoom.SetType( SvxZoomType::WHOLEPAGE );
+                pDrawViewShell->GetDispatcher()->ExecuteList(SID_ATTR_ZOOM, SfxCallMode::ASYNCHRON, { &aZoom });
+
                 // select object
                 pDrawViewShell->GetView()->UnmarkAll();
                 pDrawViewShell->GetView()->MarkObj(