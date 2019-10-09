25a3658f514e644ca89294530f1cd5398795dcfb
Julien Nabet <serval2412@yahoo.fr>
2019-10-09 20:30:42 +0200

    Revert "Optimize getNamespaceId (xmlreader)"
    This reverts commit def00961243da0662f394bdf58b1157cfd45dfab.
    
    Finally after other tests, it doesn't improve perfs
    
    Change-Id: Icbaaf751181069dd36dba0c676a4c9a2a6c8a9f3
    Reviewed-on: https://gerrit.libreoffice.org/80565
    Reviewed-by: Julien Nabet <serval2412@yahoo.fr>
    Tested-by: Julien Nabet <serval2412@yahoo.fr>

--- xmlreader/source/span.cxx
+++ xmlreader/source/span.cxx
@@ -46,10 +46,6 @@ OUString Span::convertFromUtf8() const {
     return OUString(s, SAL_NO_ACQUIRE);
 }
 
-std::size_t Span::hashCode() const {
-    return rtl_str_hashCode_WithLength(begin, length);
-}
-
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
