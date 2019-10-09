15955a47ffc8bd0414630ed2a56049d21aaababc
Michael Stahl <Michael.Stahl@cib.de>
2019-10-09 14:31:43 +0200

    sw: WW8: do not create fieldmark with start in frame and end in body
    Actually the start and end must have the same start node; presumably
    CheckNodesRange(..., true) does that.
    
    Change-Id: I04b34b593a4383b565289254bdb756d95c2b7916
    Reviewed-on: https://gerrit.libreoffice.org/80594
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@cib.de>
    (cherry picked from commit 06767a5394f1dfba71c4f0a2a07daa5664bdbd01)
    Reviewed-on: https://gerrit.libreoffice.org/80625
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>

--- sw/inc/pam.hxx
+++ sw/inc/pam.hxx
@@ -277,7 +277,7 @@ public:
 
 SW_DLLPUBLIC std::ostream &operator <<(std::ostream& s, const SwPaM& pam);
 
-bool CheckNodesRange( const SwNodeIndex&, const SwNodeIndex&, bool bChkSection );
+SW_DLLPUBLIC bool CheckNodesRange(const SwNodeIndex&, const SwNodeIndex&, bool bChkSection);
 
 #endif // INCLUDED_SW_INC_PAM_HXX
 
