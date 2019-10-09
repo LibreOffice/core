33476901b2cf2b63df928b60388717eeab69667a
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2019-10-09 09:31:52 +0200

    make impress sidebar work on iOS - missing PanelFactory
    add org_openoffice_comp_Draw_framework_PanelFactory_get_implementation
    make it build
    
    Change-Id: I738fe4c2d08437986976eaa29483c30a5ba42e62

--- sd/source/ui/animations/CustomAnimationDialog.cxx
+++ sd/source/ui/animations/CustomAnimationDialog.cxx
@@ -17,6 +17,8 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
+#include <config_features.h>
+
 #include <com/sun/star/presentation/EffectNodeType.hpp>
 #include <com/sun/star/animations/Timing.hpp>
 #include <com/sun/star/animations/Event.hpp>
@@ -1549,6 +1551,7 @@ void CustomAnimationEffectTabPage::openSoundFileDialog()
 
 void CustomAnimationEffectTabPage::onSoundPreview()
 {
+#if HAVE_FEATURE_AVMEDIA
     const sal_Int32 nPos = mpLBSound->GetSelectedEntryPos();
 
     if( nPos >= 2 ) try
@@ -1561,6 +1564,7 @@ void CustomAnimationEffectTabPage::onSoundPreview()
     {
         OSL_FAIL("CustomAnimationEffectTabPage::onSoundPreview(), exception caught!" );
     }
+#endif
 }
 
 class CustomAnimationDurationTabPage : public TabPage
