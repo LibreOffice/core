de75ee9c081ee932dce2a3ffb5203b31d44e77e8
Caolán McNamara <caolanm@redhat.com>
2019-10-10 12:31:42 +0100

    guesslang needed in calc, impress and math! as well
    Change-Id: I04c48d7d375e42577b5b4c40ce75d5fcbeca21ac

--- vcl/commonfuzzer.mk
+++ vcl/commonfuzzer.mk
@@ -145,6 +145,7 @@ fuzzer_calc_libraries = \
     sc \
     for \
     forui \
+    guesslang \
 
 fuzzer_writer_libraries = \
     msword \
@@ -159,6 +160,8 @@ fuzzer_draw_libraries = \
     sd \
     sdd \
     icg \
+    guesslang \
 
 fuzzer_math_libraries = \
     sm \
+    guesslang \
