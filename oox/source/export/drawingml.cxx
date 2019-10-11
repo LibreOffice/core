d8d0f3a76bbedc0de9ece0e5c1d41e6ad0959fe1
Xisco Fauli <xiscofauli@libreoffice.org>
2019-10-11 17:43:46 +0200

    tdf#128096: pptx: export highlight
    Change-Id: If3084f7ffaceb774dba1b1f195229313a8f1d04a

--- oox/source/export/drawingml.cxx
+++ oox/source/export/drawingml.cxx
@@ -1838,6 +1838,17 @@ void DrawingML::WriteRunProperties( const Reference< XPropertySet >& rRun, bool 
         }
     }
 
+    if (GetProperty(rXPropSet, "CharBackColor"))
+    {
+        ::Color color(*o3tl::doAccess<sal_uInt32>(mAny));
+        if( color != COL_AUTO )
+        {
+            mpFS->startElementNS(XML_a, XML_highlight);
+            WriteColor( color );
+            mpFS->endElementNS( XML_a, XML_highlight );
+        }
+    }
+
     if (underline
         && ((bCheckDirect
              && GetPropertyAndState(rXPropSet, rXPropState, "CharUnderlineColor", eState)
