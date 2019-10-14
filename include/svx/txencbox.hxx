07d042b7685736671a861dd2220f88c1f260402d
Caolán McNamara <caolanm@redhat.com>
2019-10-14 11:08:09 +0100

    SvxTextEncodingBox is now unused
    Change-Id: I0276d79cd713357d36d504e8501cd1b979e86c9c

--- include/svx/txencbox.hxx
+++ include/svx/txencbox.hxx
@@ -19,53 +19,9 @@
 #ifndef INCLUDED_SVX_TXENCBOX_HXX
 #define INCLUDED_SVX_TXENCBOX_HXX
 
-#include <vcl/lstbox.hxx>
 #include <vcl/weld.hxx>
 #include <rtl/textenc.h>
 #include <svx/svxdllapi.h>
-
-class SVX_DLLPUBLIC SvxTextEncodingBox : public ListBox
-{
-private:
-    SVX_DLLPRIVATE sal_Int32                EncodingToPos_Impl( rtl_TextEncoding nEnc ) const;
-
-public:
-    SvxTextEncodingBox( vcl::Window* pParent, WinBits nBits );
-    virtual ~SvxTextEncodingBox() override;
-
-    /** Fill with all known encodings but exclude those matching one or more
-        given flags as defined in rtl/tencinfo.h
-
-         <p> If nButIncludeInfoFlags is given, encodings are included even if they
-         match nExcludeInfoFlags. Thus it is possible to exclude 16/32-bit
-         Unicode with RTL_TEXTENCODING_INFO_UNICODE but to include UTF7 and UTF8
-         with RTL_TEXTENCODING_INFO_MIME </p>
-
-        @param bExcludeImportSubsets
-            If <TRUE/>, some specific encodings are not listed, as they are a
-            subset of another encoding. This is the case for
-            RTL_TEXTENCODING_GB_2312, RTL_TEXTENCODING_GBK,
-            RTL_TEXTENCODING_MS_936, which are covered by
-            RTL_TEXTENCODING_GB_18030. Normally, this flag should be set to
-            <TRUE/> whenever the box is used in import dialogs. */
-    void                FillFromTextEncodingTable(
-                            bool bExcludeImportSubsets,
-                            sal_uInt32 nExcludeInfoFlags = 0,
-                            sal_uInt32 nButIncludeInfoFlags = 0
-                            );
-
-    /** Fill with all known MIME encodings and select the best according to
-        <method>GetBestMimeEncoding</method>
-     */
-    void                FillWithMimeAndSelectBest();
-
-    void                InsertTextEncoding( const rtl_TextEncoding nEnc,
-                            const OUString& rEntry );
-
-    void                SelectTextEncoding( const rtl_TextEncoding nEnc );
-
-    rtl_TextEncoding    GetSelectTextEncoding() const;
-};
 
 class SVX_DLLPUBLIC TextEncodingBox
 {
