0aca9ec56bcb46cb5766cb7b93c5e207fdc50951
Mike Kaganski <mike.kaganski@collabora.com>
2019-10-14 17:42:27 +0200

    tdf#128139: add missing nullptr check
    Regression after ef2ec07b4113fdadf863352c832af657b5ae205c
    
    Change-Id: I5eed95e8136a812813145283e6b533a808324380
    Reviewed-on: https://gerrit.libreoffice.org/80782
    Tested-by: Jenkins
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
    (cherry picked from commit 8a69d0212fa7e3f7f8f2ccf29f9393b9414282a2)

--- sw/source/uibase/docvw/edtwin.cxx
+++ sw/source/uibase/docvw/edtwin.cxx
@@ -2697,7 +2697,7 @@ KEYINPUT_CHECKTABLE_INSDEL:
         if (bNormalChar)
         {
             const bool bAutoTextShown
-                = pACfg->IsAutoTextTip() && ShowAutoText(rSh.GetChunkForAutoText());
+                = pACfg && pACfg->IsAutoTextTip() && ShowAutoText(rSh.GetChunkForAutoText());
             if (!bAutoTextShown && pACorr && pACorr->GetSwFlags().bAutoCompleteWords)
                 ShowAutoCorrectQuickHelp(rSh.GetPrevAutoCorrWord(*pACorr), *pACorr);
         }
