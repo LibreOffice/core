198fc5e09784afe32cf681f65009e5ac43e67ba4
Caolán McNamara <caolanm@redhat.com>
2019-10-14 20:20:45 +0100

    Resolves: tdf#128140 stabilize formula dialog width
    Change-Id: Ifdb80adfd4b399035ed793f8636f31b90cf50094
    Reviewed-on: https://gerrit.libreoffice.org/80801
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@cib.de>

--- formula/source/ui/dlg/formula.cxx
+++ formula/source/ui/dlg/formula.cxx
@@ -253,19 +253,21 @@ FormulaDlg_Impl::FormulaDlg_Impl(weld::Dialog& rDialog,
     , m_xEdRef(new RefEdit(rBuilder.weld_entry("ED_REF")))
     , m_xRefBtn(new RefButton(rBuilder.weld_button("RB_REF")))
 {
+    auto nWidth = m_xMEdit->get_approximate_digit_width() * 62;
+
     //Space for two lines of text
     m_xFtHeadLine->set_label("X\nX\n");
     auto nHeight = m_xFtHeadLine->get_preferred_size().Height();
-    m_xFtHeadLine->set_size_request(-1, nHeight);
+    m_xFtHeadLine->set_size_request(nWidth, nHeight);
     m_xFtHeadLine->set_label("");
 
     m_xFtFuncName->set_label("X\nX\n");
     nHeight = m_xFtFuncName->get_preferred_size().Height();
-    m_xFtFuncName->set_size_request(-1, nHeight);
-    m_xFtFuncDesc->set_size_request(-1, nHeight);
+    m_xFtFuncName->set_size_request(nWidth, nHeight);
+    m_xFtFuncDesc->set_size_request(nWidth, nHeight);
     m_xFtFuncName->set_label("");
 
-    m_xMEdit->set_size_request(m_xMEdit->get_approximate_digit_width() * 62,
+    m_xMEdit->set_size_request(nWidth,
                                m_xMEdit->get_height_rows(5));
 
     m_xEdRef->SetReferences(_pDlg, m_xFtEditName.get());
