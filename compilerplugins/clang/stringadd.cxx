8d0b220b62c3f51a74bfdafd2e7ac660a019c4eb
Noel Grandin <noel.grandin@collabora.co.uk>
2019-10-16 20:20:26 +0200

    loplugin:stringadd fix condition
    Change-Id: I7752c281b1b6dd0d26bd7d6c4a6896c663f4cbc3

--- compilerplugins/clang/stringadd.cxx
+++ compilerplugins/clang/stringadd.cxx
@@ -264,7 +264,7 @@ bool StringAdd::isSideEffectFree(Expr const* expr)
             {
                 auto name = calleeMethodDecl->getName();
                 if (callExpr->getNumArgs() > 0
-                    && (name == "number" || name == "unacquired" || "boolean"))
+                    && (name == "number" || name == "unacquired" || name == "boolean"))
                 {
                     auto tc = loplugin::TypeCheck(calleeMethodDecl->getParent());
                     if (tc.Class("OUString") || tc.Class("OString"))
