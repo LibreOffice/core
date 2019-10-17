8a4f650064edacf4814a9307764f89f9e54f928c
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2019-10-18 00:29:46 +0200

    skia: implement invert operation
    Change-Id: I248518283ee6a4604bc45f36f2af3804a15f5652

--- RepositoryExternal.mk
+++ RepositoryExternal.mk
@@ -115,6 +115,7 @@ ifneq ($(ENABLE_SKIA),)
 define gb_LinkTarget__use_skia
 $(call gb_LinkTarget_set_include,$(1),\
 	-I$(call gb_UnpackedTarball_get_dir,skia)/include/core \
+	-I$(call gb_UnpackedTarball_get_dir,skia)/include/effects \
 	-I$(call gb_UnpackedTarball_get_dir,skia)/include/config \
 	-I$(call gb_UnpackedTarball_get_dir,skia)/include/third_party/vulkan \
 	-I$(call gb_UnpackedTarball_get_dir,skia) \
