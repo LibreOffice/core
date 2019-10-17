e8475b6343af23bc1ac2d722afd8df4d7615a7a8
Noel Grandin <noel.grandin@collabora.co.uk>
2019-10-17 11:05:43 +0200

    loplugin:virtualdead look for virtual methods where a param is unused
    Change-Id: Ibadc2aa79a52082db16eff44c89ab30938838dd8
    Reviewed-on: https://gerrit.libreoffice.org/80935
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

--- compilerplugins/clang/virtualdead.py
+++ compilerplugins/clang/virtualdead.py
@@ -5,6 +5,8 @@ import re
 import io
 
 callDict = dict() # callInfo tuple -> callValue
+definitionToSourceLocationMap = dict()
+paramSet = set() # paraminfo tuple
 
 # clang does not always use exactly the same numbers in the type-parameter vars it generates
 # so I need to substitute them to ensure we can match correctly.
@@ -17,13 +19,22 @@ with io.open("workdir/loplugin.virtualdead.log", "rb", buffering=1024*1024) as t
     for line in txt:
         try:
             tokens = line.strip().split("\t")
-            nameAndParams = normalizeTypeParams(tokens[1])
-            sourceLocation = tokens[2]
-            returnValue = tokens[3]
-            callInfo = (nameAndParams, sourceLocation)
-            if not callInfo in callDict:
-                callDict[callInfo] = set()
-            callDict[callInfo].add(returnValue)
+            if tokens[0] == "virtual:":
+                nameAndParams = normalizeTypeParams(tokens[1])
+                sourceLocation = tokens[2]
+                returnValue = tokens[3]
+                callInfo = (nameAndParams, sourceLocation)
+                if not callInfo in callDict:
+                    callDict[callInfo] = set()
+                callDict[callInfo].add(returnValue)
+                definitionToSourceLocationMap[nameAndParams] = sourceLocation
+            elif tokens[0] == "param:":
+                name = normalizeTypeParams(tokens[1])
+                if len(tokens)>2:
+                    bitfield = tokens[2]
+                    paramSet.add((name,bitfield))
+            else:
+                print( "unknown line: " + line)
         except IndexError:
             print "problem with line " + line.strip()
             raise
@@ -44,22 +55,65 @@ for callInfo, callValues in callDict.iteritems():
         continue
     if "pure" in callValue:
         continue
-    sourceLoc = callInfo[1]
-    if sourceLoc.startswith("workdir/"):
-        continue
+    srcloc = callInfo[1]
+    if srcloc.startswith("workdir/"): continue
+    # ignore Qt stuff
+    if srcloc.startswith("Gui/"): continue
+    if srcloc.startswith("Widgets/"): continue
+    if srcloc.startswith("Core/"): continue
     functionSig = callInfo[0]
-    tmp1list.append((sourceLoc, functionSig, callValue))
+    tmp1list.append((srcloc, functionSig, callValue))
 
+def merge_bitfield(a, b):
+    if len(a) == 0: return b
+    ret = ""
+    for i, c in enumerate(b):
+        if c == "1" or a[i] == "1":
+            ret += "1"
+        else:
+            ret += "0"
+    return ret;
+tmp2dict = dict()
+tmp2list = list()
+for paramInfo in paramSet:
+    name = paramInfo[0]
+    bitfield = paramInfo[1]
+    if re.match( r"\w+ com::", name): continue
+    if re.match( r"\w+ ooo::vba::", name): continue
+    if re.match( r"\w+ orcus::", name): continue
+    if re.match( r"\w+ std::", name): continue
+    if not name in tmp2dict:
+        tmp2dict[name] = bitfield
+    else:
+        tmp2dict[name] = merge_bitfield(tmp2dict[name], bitfield)
+for name, bitfield in tmp2dict.iteritems():
+    srcloc = definitionToSourceLocationMap[name]
+    # ignore Qt stuff
+    if srcloc.startswith("Gui/"): continue
+    if srcloc.startswith("Widgets/"): continue
+    if srcloc.startswith("Core/"): continue
+    # ignore external stuff
+    if srcloc.startswith("workdir/"): continue
+    # referenced by generated code in workdir/
+    if srcloc.startswith("writerfilter/source/ooxml/OOXMLFactory.hxx"): continue
+    if "0" in bitfield:
+        tmp2list.append((srcloc, name, bitfield))
 
 # sort results by filename:lineno
 def natural_sort_key(s, _nsre=re.compile('([0-9]+)')):
     return [int(text) if text.isdigit() else text.lower()
             for text in re.split(_nsre, s)]
 tmp1list.sort(key=lambda v: natural_sort_key(v[0]))
+tmp2list.sort(key=lambda v: natural_sort_key(v[0]))
 
 # print out the results
 with open("compilerplugins/clang/virtualdead.results", "wt") as f:
     for v in tmp1list:
+        f.write(v[0] + "\n")
+        f.write("    " + v[1] + "\n")
+        f.write("    " + v[2] + "\n")
+with open("compilerplugins/clang/virtualdead.unusedparams.results", "wt") as f:
+    for v in tmp2list:
         f.write(v[0] + "\n")
         f.write("    " + v[1] + "\n")
         f.write("    " + v[2] + "\n")
