89ef7ba634eb9d3704227660724af9378a32acb5
Serge Krot <Serge.Krot@cib.de>
2019-10-18 17:40:11 +0200

    Toolbar: Added usage of ImageControl as toolbar control
    Reviewed-on: https://gerrit.libreoffice.org/81064
    Reviewed-by: Serge Krot (CIB) <Serge.Krot@cib.de>
    Tested-by: Serge Krot (CIB) <Serge.Krot@cib.de>
    (cherry picked from commit 48f945a3be16ee0eba0c526e51280dc8deafc316)
    
    Change-Id: I2536a505650b6e4c98e79f749c3b237518cc995d

---
+++ framework/inc/uielement/FixedImageToolbarController.hxx
@@ -0,0 +1,59 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#ifndef INCLUDED_FRAMEWORK_INC_UIELEMENT_FIXEDIMAGETOOLBARCONTROLLER_HXX
+#define INCLUDED_FRAMEWORK_INC_UIELEMENT_FIXEDIMAGETOOLBARCONTROLLER_HXX
+
+#include <com/sun/star/beans/NamedValue.hpp>
+#include <com/sun/star/frame/ControlCommand.hpp>
+
+#include <uielement/complextoolbarcontroller.hxx>
+
+class ToolBox;
+class ImageControl;
+
+namespace framework
+{
+class FixedImageToolbarController : public ComplexToolbarController
+{
+public:
+    FixedImageToolbarController(const css::uno::Reference<css::uno::XComponentContext>& rxContext,
+                                const css::uno::Reference<css::frame::XFrame>& rFrame,
+                                ToolBox* pToolBar, sal_uInt16 nID, const OUString& aCommand);
+
+    // XComponent
+    virtual void SAL_CALL dispose() override;
+
+    void CheckAndUpdateImages();
+
+protected:
+    virtual void executeControlCommand(const css::frame::ControlCommand& rControlCommand) override;
+
+protected:
+    DECL_LINK(MiscOptionsChanged, LinkParamNone*, void);
+
+private:
+    VclPtr<ImageControl> m_pFixedImageControl;
+    sal_Int16 m_eSymbolSize;
+};
+} // namespace framework
+
+#endif // INCLUDED_FRAMEWORK_INC_UIELEMENT_FIXEDIMAGETOOLBARCONTROLLER_HXX
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
