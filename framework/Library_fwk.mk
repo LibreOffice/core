c2682ba52ea0cbb7af95e80f6d67b286061aeae2
Serge Krot <Serge.Krot@cib.de>
2019-10-18 13:57:22 +0200

    Toolbar: Added usage of FixedText as toolbar control
    Change-Id: I26a79c485fa2e168058025e058bcfb793af7515f
    Reviewed-on: https://gerrit.libreoffice.org/81039
    Reviewed-by: Serge Krot (CIB) <Serge.Krot@cib.de>
    Tested-by: Serge Krot (CIB) <Serge.Krot@cib.de>

--- framework/Library_fwk.mk
+++ framework/Library_fwk.mk
@@ -132,6 +132,7 @@ $(eval $(call gb_Library_add_exception_objects,fwk,\
     framework/source/uielement/controlmenucontroller \
     framework/source/uielement/dropdownboxtoolbarcontroller \
     framework/source/uielement/edittoolbarcontroller \
+    framework/source/uielement/FixedTextToolbarController \
     framework/source/uielement/genericstatusbarcontroller \
     framework/source/uielement/generictoolbarcontroller \
     framework/source/uielement/imagebuttontoolbarcontroller \
