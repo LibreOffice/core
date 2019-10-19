c0148bcfbd1c5aa42eef14f0619d94224a516ac1
Chris Sherlock <chris.sherlock79@gmail.com>
2019-10-20 03:53:48 +1100

    vcl: create RAII metafile guard
    Change-Id: I02dfa04f796df86352bf7278b9d176fef1adcd43

--- vcl/source/drawables/B2DPolyPolyLineDrawable.cxx
+++ vcl/source/drawables/B2DPolyPolyLineDrawable.cxx
@@ -3,16 +3,16 @@
  * This file is part of the LibreOffice project.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with pRenderContext
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
  * This file incorporates work covered by the following license notice:
  *
  *   Licensed to the Apache Software Foundation (ASF) under one or more
  *   contributor license agreements. See the NOTICE file distributed
- *   with pRenderContext work for additional information regarding copyright
- *   ownership. The ASF licenses pRenderContext file to you under the Apache
- *   License, Version 2.0 (the "License"); you may not use pRenderContext file
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
@@ -133,6 +133,23 @@ static basegfx::B2DPolyPolygon CreateFillPolyPolygon(basegfx::B2DPolyPolygon& rL
     return aFillPolyPolygon;
 }
 
+class DisableMetafileProcessing
+{
+public:
+    DisableMetafileProcessing(VclPtr<OutputDevice> pRenderContext)
+        : mpMtf(pRenderContext->GetConnectMetaFile())
+        , mpRenderContext(pRenderContext)
+    {
+        pRenderContext->SetConnectMetaFile(nullptr);
+    }
+
+    ~DisableMetafileProcessing() { mpRenderContext->SetConnectMetaFile(mpMtf); }
+
+private:
+    GDIMetaFile* mpMtf;
+    VclPtr<OutputDevice> mpRenderContext;
+};
+
 bool B2DPolyPolyLineDrawable::Draw(OutputDevice* pRenderContext,
                                    basegfx::B2DPolyPolygon const& rLinePolyPolygon,
                                    LineInfo const& rLineInfo) const
@@ -140,8 +157,7 @@ bool B2DPolyPolyLineDrawable::Draw(OutputDevice* pRenderContext,
     basegfx::B2DPolyPolygon aLinePolyPolygon = ApplyLineDashing(rLinePolyPolygon, rLineInfo);
     basegfx::B2DPolyPolygon aFillPolyPolygon = CreateFillPolyPolygon(aLinePolyPolygon, rLineInfo);
 
-    GDIMetaFile* pOldMetaFile = pRenderContext->GetConnectMetaFile();
-    pRenderContext->SetConnectMetaFile(nullptr);
+    DisableMetafileProcessing aDisableMtf(pRenderContext);
 
     const bool bTryAA((pRenderContext->GetAntialiasing() & AntialiasingFlags::EnableB2dDraw)
                       && mpGraphics->supportsOperation(OutDevSupportType::B2DDraw)
@@ -211,8 +227,6 @@ bool B2DPolyPolyLineDrawable::Draw(OutputDevice* pRenderContext,
         pRenderContext->SetFillColor(aOldFillColor);
         pRenderContext->SetLineColor(aOldLineColor);
     }
-
-    pRenderContext->SetConnectMetaFile(pOldMetaFile);
 
     return true;
 }
