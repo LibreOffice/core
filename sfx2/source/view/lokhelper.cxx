dfcfdf47b5da6a11b2c3118a0f63cc48c10c2cd7
Marco Cecchetti <marco.cecchetti@collabora.com>
2019-10-20 13:18:51 +0200

    lok: get spelling context menu on long press
    This patch handles a new flag attached to the invalidate view cursor
    message for informing the client when the text cursor is inside a
    mispelled word.
    This information is used for popping up the spelling context menu on a
    long press event instead of the standard context menu for a selected
    word.
    
    Change-Id: I13fcbe53c83ca6eb56300a601734cdc3211e88a0
    Reviewed-on: https://gerrit.libreoffice.org/85244
    Tested-by: Jenkins
    Reviewed-by: Tamás Zolnai <tamas.zolnai@collabora.com>

--- sfx2/source/view/lokhelper.cxx
+++ sfx2/source/view/lokhelper.cxx
@@ -338,7 +338,7 @@ void SfxLokHelper::notifyDocumentSizeChangedAllViews(vcl::ITiledRenderable* pDoc
     }
 }
 
-void SfxLokHelper::notifyVisCursorInvalidation(OutlinerViewShell const* pThisView, const OString& rRectangle)
+void SfxLokHelper::notifyVisCursorInvalidation(OutlinerViewShell const* pThisView, const OString& rRectangle, bool bMispelledWord)
 {
     if (DisableCallbacks::disabled())
         return;
@@ -347,7 +347,7 @@ void SfxLokHelper::notifyVisCursorInvalidation(OutlinerViewShell const* pThisVie
     if (comphelper::LibreOfficeKit::isViewIdForVisCursorInvalidation())
     {
         sPayload = OStringLiteral("{ \"viewId\": \"") + OString::number(SfxLokHelper::getView()) +
-            "\", \"rectangle\": \"" + rRectangle + "\" }";
+            "\", \"rectangle\": \"" + rRectangle + "\", \"mispelledWord\": \"" +  OString::number(bMispelledWord ? 1 : 0) + "\" }";
     }
     else
     {
