ee78313613605f50795d8ba0fbcd4138c9845a51
Grzegorz Araminowicz <grzegorz.araminowicz@collabora.com>
2019-10-21 12:59:12 +0200

    lok: send hyperlink text and address under cursor
    Change-Id: I827c51ae859b3d3649ec9d293b5ae8eaf4cbd630
    Reviewed-on: https://gerrit.libreoffice.org/81219
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Jan Holesovsky <kendy@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/89691
    Tested-by: Jenkins
    Reviewed-by: Muhammet Kara <muhammet.kara@collabora.com>

--- sfx2/source/view/lokhelper.cxx
+++ sfx2/source/view/lokhelper.cxx
@@ -367,7 +367,7 @@ void SfxLokHelper::notifyDocumentSizeChangedAllViews(vcl::ITiledRenderable* pDoc
     }
 }
 
-void SfxLokHelper::notifyVisCursorInvalidation(OutlinerViewShell const* pThisView, const OString& rRectangle, bool bMispelledWord)
+void SfxLokHelper::notifyVisCursorInvalidation(OutlinerViewShell const* pThisView, const OString& rRectangle, bool bMispelledWord, const OString& rHyperlink)
 {
     if (DisableCallbacks::disabled())
         return;
@@ -375,8 +375,11 @@ void SfxLokHelper::notifyVisCursorInvalidation(OutlinerViewShell const* pThisVie
     OString sPayload;
     if (comphelper::LibreOfficeKit::isViewIdForVisCursorInvalidation())
     {
+        OString sHyperlink = rHyperlink.isEmpty() ? "{}" : rHyperlink;
         sPayload = OStringLiteral("{ \"viewId\": \"") + OString::number(SfxLokHelper::getView()) +
-            "\", \"rectangle\": \"" + rRectangle + "\", \"mispelledWord\": \"" +  OString::number(bMispelledWord ? 1 : 0) + "\" }";
+            "\", \"rectangle\": \"" + rRectangle +
+            "\", \"mispelledWord\": \"" +  OString::number(bMispelledWord ? 1 : 0) +
+            "\", \"hyperlink\": " + sHyperlink + " }";
     }
     else
     {
