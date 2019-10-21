b5a4a3e7aaf22be70e835b051cec9570dfda8471
Jim Raykowski <raykowj@gmail.com>
2019-10-21 02:29:43 -0800

    tdf#128285 Only update Navigator heading tracking if document has focus
    Change-Id: I8e4d9a7b80bdae32d83349db20058f2be62e02c1

--- sw/source/uibase/utlui/content.cxx
+++ sw/source/uibase/utlui/content.cxx
@@ -2121,7 +2121,7 @@ bool SwContentTree::HasContentChanged()
             else
             {
                 SvTreeListEntry* pFirstSel;
-                if(bOutline &&
+                if(bOutline && !HasFocus() &&
                         nullptr != ( pFirstSel = FirstSelected()) &&
                             lcl_IsContent(pFirstSel))
                 {
@@ -2745,12 +2745,14 @@ IMPL_LINK_NOARG(SwContentTree, TimerUpdate, Timer *, void)
     if (IsDisposed())
         return;
 
+    // No update while focus is not in document.
     // No update while drag and drop.
     // Query view because the Navigator is cleared too late.
     SwView* pView = GetParentWindow()->GetCreateView();
-    if( (!HasFocus() || m_bViewHasChanged) &&
-         !bIsInDrag && !m_bIsInternalDrag && pView &&
-         pView->GetWrtShellPtr() && !pView->GetWrtShellPtr()->ActionPend() )
+
+    if(pView && pView->GetWrtShellPtr() &&
+            ((pView->GetWrtShellPtr()->GetWin() == GetFocusedWindow()) || m_bViewHasChanged) &&
+            !bIsInDrag && !m_bIsInternalDrag && !pView->GetWrtShellPtr()->ActionPend())
     {
         m_bViewHasChanged = false;
         m_bIsIdleClear = false;
