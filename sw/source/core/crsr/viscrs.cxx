104e4f0ba685a4b2683185945b820417287ed5e3
Grzegorz Araminowicz <grzegorz.araminowicz@collabora.com>
2019-10-21 12:59:12 +0200

    lok: send hyperlink text and address under cursor
    Change-Id: I827c51ae859b3d3649ec9d293b5ae8eaf4cbd630

--- sw/source/core/crsr/viscrs.cxx
+++ sw/source/core/crsr/viscrs.cxx
@@ -241,18 +241,31 @@ void SwVisibleCursor::SetPosAndShow(SfxViewShell const * pViewShell)
             }
         }
 
+        OString sHyperlink;
+        SwContentAtPos aContentAtPos(IsAttrAtPos::InetAttr);
+        if (const_cast<SwCursorShell*>(m_pCursorShell)->GetContentAtPos(aRect.Pos(), aContentAtPos))
+        {
+            const SwFormatINetFormat* pItem = static_cast<const SwFormatINetFormat*>(aContentAtPos.aFnd.pAttr);
+            boost::property_tree::ptree aTree;
+            aTree.put("text", aContentAtPos.sStr);
+            aTree.put("link", pItem->GetValue());
+            std::stringstream aStream;
+            boost::property_tree::write_json(aStream, aTree, false);
+            sHyperlink = OString(aStream.str().c_str()).trim();
+        }
+
         if (pViewShell)
         {
             if (pViewShell == m_pCursorShell->GetSfxViewShell())
             {
-                SfxLokHelper::notifyVisCursorInvalidation(pViewShell, sRect, bIsWrong);
+                SfxLokHelper::notifyVisCursorInvalidation(pViewShell, sRect, bIsWrong, sHyperlink);
             }
             else
                 SfxLokHelper::notifyOtherView(m_pCursorShell->GetSfxViewShell(), pViewShell, LOK_CALLBACK_INVALIDATE_VIEW_CURSOR, "rectangle", sRect);
         }
         else
         {
-            SfxLokHelper::notifyVisCursorInvalidation(m_pCursorShell->GetSfxViewShell(), sRect, bIsWrong);
+            SfxLokHelper::notifyVisCursorInvalidation(m_pCursorShell->GetSfxViewShell(), sRect, bIsWrong, sHyperlink);
             SfxLokHelper::notifyOtherViews(m_pCursorShell->GetSfxViewShell(), LOK_CALLBACK_INVALIDATE_VIEW_CURSOR, "rectangle", sRect);
         }
     }
