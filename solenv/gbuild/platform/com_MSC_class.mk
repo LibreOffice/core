4a069224c0c38e59271c99313e36395ad141403c
Stephan Bergmann <sbergman@redhat.com>
2019-10-23 17:15:50 +0200

    -W4 must come before -Wno-missing-braces for clang-cl
    ...or else -W4 would re-enable that and cause lots of warnings/errors.
    87608490f205b2fbc2b453ad8ded33050ac29b90 "filter arguments to MSVC to avoid the
    annoying D9025 warning" had changed the relative order of those options on the
    command line.
    
    Change-Id: I2ff9de93cdc6e3dc63961af61169f0adf44f7c0b

--- solenv/gbuild/platform/com_MSC_class.mk
+++ solenv/gbuild/platform/com_MSC_class.mk
@@ -45,8 +45,8 @@ endef
 
 # Avoid annoying warning D9025 about overriding command-line arguments.
 gb_Helper_remove_overriden_flags = \
-    $(filter-out -W4 -w -arch:SSE -arch:SSE2 -arch:AVX -arch:AVX2 -Od -O2,$(1)) \
     $(lastword $(filter -W4 -w,$(1))) \
+    $(filter-out -W4 -w -arch:SSE -arch:SSE2 -arch:AVX -arch:AVX2 -Od -O2,$(1)) \
     $(lastword $(filter -Od -O2,$(1))) \
     $(lastword $(filter -arch:SSE -arch:SSE2 -arch:AVX -arch:AVX2,$(1)))
 
