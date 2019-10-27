b7735d9c705bdec53ddb0077b93ae70ce4c2df48
Jens Carl <j.carl43@gmx.de>
2019-10-26 21:54:16 -0700

    tdf#45904 Move XNamed Java tests to C++
    Move XNamed Java tests to C++ for ScStyleObj.
    
    Change-Id: Idaff7307d15ca2665005e866dff36a033aa91354
    Reviewed-on: https://gerrit.libreoffice.org/81553
    Tested-by: Jenkins
    Reviewed-by: Jens Carl <j.carl43@gmx.de>

---
+++ sc/CppunitTest_sc_styleobj.mk
@@ -0,0 +1,45 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#*************************************************************************
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+#*************************************************************************
+
+$(eval $(call gb_CppunitTest_CppunitTest,sc_styleobj))
+
+$(eval $(call gb_CppunitTest_use_external,sc_styleobj,boost_headers))
+
+$(eval $(call gb_CppunitTest_use_common_precompiled_header,sc_styleobj))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,sc_styleobj, \
+	sc/qa/extras/scstyleobj \
+))
+
+$(eval $(call gb_CppunitTest_use_libraries,sc_styleobj, \
+	cppu \
+	sal \
+	subsequenttest \
+	test \
+	unotest \
+))
+
+$(eval $(call gb_CppunitTest_set_include,sc_styleobj,\
+	$$(INCLUDE) \
+))
+
+$(eval $(call gb_CppunitTest_use_sdk_api,sc_styleobj))
+
+$(eval $(call gb_CppunitTest_use_ure,sc_styleobj))
+$(eval $(call gb_CppunitTest_use_vcl,sc_styleobj))
+
+$(eval $(call gb_CppunitTest_use_components,sc_styleobj,\
+	$(sc_unoapi_common_components) \
+))
+
+$(eval $(call gb_CppunitTest_use_configuration,sc_styleobj))
+
+# vim: set noet sw=4 ts=4:
