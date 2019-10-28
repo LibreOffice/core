LibreOffice/core

commit 7146d8bcd96f844dc0239a5b29a6b36c3cb5a2cc

MSWordExportBase::OutputSectionBreaks: avoid fake section breaks
Regression from ee9f23bb94b4c2c8c4db6466ecca272a092e9492 (docx export:
invalid sectPr added at the beginning of the doc, 2013-01-10), the
problem was that we even tried to generate section breaks at places
where the two page styles are in practice the same.

Change-Id: Iccf91ce542d9b075b93da25bcd42236db667e3d4

sw/source/filter/ww8/ww8atr.cxx
@@ -443,7 +443,7 @@ void MSWordExportBase::OutputSectionBreaks( const SfxItemSet *pSet, const SwNode
             if ( isCellOpen && ( pAktPageDesc->GetName() != pPageDesc->GetName() ) )
                 pSet = NULL;
         }
-        else
+        else if (!sw::util::IsPlausableSingleWordSection(pAktPageDesc->GetFirstMaster(), pPageDesc->GetMaster()))
         {
             bBreakSet = true;
             bNewPageDesc = true;

sw/qa/extras/ooxmlexport/ooxmlexport.cxx
@@ -2532,7 +2532,7 @@ DECLARE_OOXMLEXPORT_TEST(testFdo77718, "fdo77718.docx")
 
 DECLARE_OOXMLEXPORT_TEST(testTableCurruption, "tableCurrupt.docx")
 {
-    xmlDocPtr pXmlDoc = parseExport("word/header4.xml");
+    xmlDocPtr pXmlDoc = parseExport("word/header2.xml");
     if (!pXmlDoc)
         return;
     CPPUNIT_ASSERT(pXmlDoc) ;
@@ -2635,7 +2635,7 @@ DECLARE_OOXMLEXPORT_TEST(testOLEObjectinHeader, "2129393649.docx")
     // Problem was relationship entry for oleobject from header was
     // exported into document.xml.rels file because of this rels file
     // for headers were missing from document/word/rels.
-    xmlDocPtr pXmlDoc = parseExport("word/_rels/header3.xml.rels");
+    xmlDocPtr pXmlDoc = parseExport("word/_rels/header1.xml.rels");
     if(!pXmlDoc)
         return;
 
@@ -2971,12 +2971,12 @@ DECLARE_OOXMLEXPORT_TEST(testContentTypeOLE, "fdo77759.docx")
 
 DECLARE_OOXMLEXPORT_TEST(testfdo78420, "fdo78420.docx")
 {
-    xmlDocPtr pXmlHeader = parseExport("word/header3.xml");
+    xmlDocPtr pXmlHeader = parseExport("word/header2.xml");
 
     if (!pXmlHeader)
        return;
 
-    xmlDocPtr pXmlHeaderRels = parseExport("word/_rels/header3.xml.rels");
+    xmlDocPtr pXmlHeaderRels = parseExport("word/_rels/header2.xml.rels");
     if(!pXmlHeaderRels)
        return;
 
@@ -3407,6 +3407,13 @@ DECLARE_OOXMLEXPORT_TEST(testPlausableBorder, "plausable-border.docx")
     if (xmlDocPtr pXmlDoc = parseExport())
         // Page break was exported as section break, this was 0
         assertXPath(pXmlDoc, "/w:document/w:body/w:p/w:r/w:br", 1);
+}
+
+DECLARE_OOXMLEXPORT_TEST(testUnwantedSectionBreak, "unwanted-section-break.docx")
+{
+    if (xmlDocPtr pXmlDoc = parseExport())
+        // This was 2: an additional sectPr was added to the document.
+        assertXPath(pXmlDoc, "//w:sectPr", 1);
 }
 
 DECLARE_OOXMLEXPORT_TEST(testfdo80897 , "fdo80897.docx")

commit 784f3f20555848d266b7e75350ea329106853c66

tdf#104017 DOC export: be less aggressive with merging page styles
Regression from commit 7146d8bcd96f844dc0239a5b29a6b36c3cb5a2cc
(MSWordExportBase::OutputSectionBreaks: avoid fake section breaks,
2014-07-28), the problem was that we merged two separate page styles
just because they were similar. This resulted in merging two independent
tables into a single one.

Fix the problem by merging the first and follow page style only in case
the first page style is RES_POOLPAGE_FIRST, this resolves the current
problem and keeps the original problem fixed, too.

(Long-term, a better fix would be to not split the page styles in
writerfilter/, similar to how the DOC import doesn't do that anymore:
and then the merging can be avoided unconditionally.)

Change-Id: I357f1337cb0abc7fa1d88aa44ff87b4b464af81c

sw/source/filter/ww8/ww8atr.cxx
@@ -475,7 +475,7 @@ void MSWordExportBase::OutputSectionBreaks( const SfxItemSet *pSet, const SwNode
                 }
             }
         }
-        else if (!sw::util::IsPlausableSingleWordSection(m_pCurrentPageDesc->GetFirstMaster(), pPageDesc->GetMaster()))
+        else if (m_pCurrentPageDesc->GetPoolFormatId() != RES_POOLPAGE_FIRST || !sw::util::IsPlausableSingleWordSection(m_pCurrentPageDesc->GetFirstMaster(), pPageDesc->GetMaster()))
         {
             bBreakSet = true;
             bNewPageDesc = true;

sw/qa/extras/ww8export/ww8export3.cxx
@@ -361,6 +361,15 @@ DECLARE_WW8EXPORT_TEST(testPresetDash, "tdf127166_prstDash_Word97.doc")
     }
 }
 
+DECLARE_WW8EXPORT_TEST(testTdf104017, "tdf104017.doc")
+{
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 2
+    // - Actual  : 1
+    // i.e. the tables on the two pages were merged together to a single one on export.
+    CPPUNIT_ASSERT_EQUAL(2, getPages());
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */