a4757272df03989ae7113e322d05c4f76ae27e8f
Michael Meeks <michael.meeks@collabora.com>
2019-10-28 20:43:46 +0000

    Revert "sc lok: Implement hi-dpi and zoom for spreadsheets."
    This reverts commit 498dceb43f870bf9e380f1f87e99c6ccadf1963c.
    
    Change-Id: Iadb9da47cf8c9a57385530ab888d55169db7639a

--- sc/source/ui/view/gridwin4.cxx
+++ sc/source/ui/view/gridwin4.cxx
@@ -1129,30 +1129,17 @@ void ScGridWindow::PaintTile( VirtualDevice& rDevice,
     // coords only, and avoid all the SetMapMode()'s.
     // Similarly to Writer, we should set the mapmode once on the rDevice, and
     // not care about any zoom settings.
-    //
-    // But until that happens, we actually draw everything at 100%, and only
-    // set cairo's or CoreGraphic's scale factor accordingly, so that everything
-    // is painted  bigger or smaller. This is different to what Calc's internal
-    // scaling would do - because that one is trying to fit the lines between
-    // cells to integer multiples of pixels.
-    //
-    // See also desktop/source/lib/init.cxx for details, where we have to set
-    // the stuff accordingly for the VirtualDevice creation.
 
-    // page break zoom, and aLogicMode in ScViewData - hardcode that to what
-    // we mean as 100% (256px tiles meaning 3840 twips)
-    Fraction aFracX(long(256 * TWIPS_PER_PIXEL), 3840);
-    Fraction aFracY(long(256 * TWIPS_PER_PIXEL), 3840);
+    Fraction aFracX(long(nOutputWidth * TWIPS_PER_PIXEL), nTileWidth);
+    Fraction aFracY(long(nOutputHeight * TWIPS_PER_PIXEL), nTileHeight);
+
+    // page break zoom, and aLogicMode in ScViewData
     pViewData->SetZoom(aFracX, aFracY, true);
 
-    // Cairo or CoreGraphics scales for us, we have to compensate for that,
-    // otherwise we are painting too far away
-    const double fDPIScale = comphelper::LibreOfficeKit::getDPIScale();
-
-    const double fTilePosXPixel = static_cast<double>(nTilePosX) * nOutputWidth / (nTileWidth * fDPIScale);
-    const double fTilePosYPixel = static_cast<double>(nTilePosY) * nOutputHeight / (nTileHeight * fDPIScale);
-    const double fTileBottomPixel = static_cast<double>(nTilePosY + nTileHeight) * nOutputHeight / (nTileHeight * fDPIScale);
-    const double fTileRightPixel = static_cast<double>(nTilePosX + nTileWidth) * nOutputWidth / (nTileWidth * fDPIScale);
+    const double fTilePosXPixel = static_cast<double>(nTilePosX) * nOutputWidth / nTileWidth;
+    const double fTilePosYPixel = static_cast<double>(nTilePosY) * nOutputHeight / nTileHeight;
+    const double fTileBottomPixel = static_cast<double>(nTilePosY + nTileHeight) * nOutputHeight / nTileHeight;
+    const double fTileRightPixel = static_cast<double>(nTilePosX + nTileWidth) * nOutputWidth / nTileWidth;
 
     SCTAB nTab = pViewData->GetTabNo();
     ScDocument* pDoc = pViewData->GetDocument();
