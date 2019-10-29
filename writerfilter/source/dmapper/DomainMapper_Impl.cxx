58a86af36295b4fc1e07c0bd38f74530a2ce0f08
Michael Stahl <Michael.Stahl@cib.de>
2019-10-29 13:44:07 +0100

    writerfilter: use insert position in PushFieldContext()
    Don't hardcode end of text; see mangled nested fields in e.g.
    sw/qa/core/data/ooxml/pass/fdo79838.docx
    
    Change-Id: I1b77e7a0c0d2a7d52b5facbb43a0ed0747d74cea
    Reviewed-on: https://gerrit.libreoffice.org/81668
    Reviewed-by: Michael Stahl <michael.stahl@cib.de>
    Tested-by: Michael Stahl <michael.stahl@cib.de>

--- writerfilter/source/dmapper/DomainMapper_Impl.cxx
+++ writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -3349,7 +3349,10 @@ void DomainMapper_Impl::PushFieldContext()
     {
         uno::Reference<text::XTextAppend> xTextAppend = m_aTextAppendStack.top().xTextAppend;
         if (xTextAppend.is())
-            xCrsr = xTextAppend->createTextCursorByRange(xTextAppend->getEnd());
+            xCrsr = xTextAppend->createTextCursorByRange(
+                        m_aTextAppendStack.top().xInsertPosition.is()
+                            ? m_aTextAppendStack.top().xInsertPosition
+                            : xTextAppend->getEnd());
     }
 
     uno::Reference< text::XTextRange > xStart;
