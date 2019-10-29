8b476e517e66934233c145b6be1192a696f51c38
Stephan Bergmann <sbergman@redhat.com>
2019-10-29 07:47:19 +0100

    Adapt comment
    external/icu/clang-cl.patch has been dropped by
    398e1e6ae83999eea8fb8c845190667695ac115f "Upgrade to ICU 64.2"
    
    Change-Id: Ic1ab2b02820cc15f9b14bdaab4554e1da5234623

--- external/icu/UnpackedTarball_icu.mk
+++ external/icu/UnpackedTarball_icu.mk
@@ -21,9 +21,6 @@ $(eval $(call gb_UnpackedTarball_set_pre_action,icu,\
 
 $(eval $(call gb_UnpackedTarball_set_patchlevel,icu,0))
 
-# * external/icu/clang-cl.patch is covered by upstream
-#   <https://github.com/unicode-org/icu/commit/5a34bfb1516a6719b5f470063c6be2f47446f0b2> "ICU-20209
-#   Fix build failures on Windows with std::atomic not in enclo…":
 $(eval $(call gb_UnpackedTarball_add_patches,icu,\
 	external/icu/icu4c-build.patch.1 \
 	external/icu/icu4c-aix.patch.1 \
