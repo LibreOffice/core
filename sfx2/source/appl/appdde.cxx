LibreOffice/core

commit 2cd6ff8a54b0db76b2f4c5f75988367cfb0feaf0

remove unnecessary includes
Change-Id: Ia4a622b34ff3f71963cff7a1aa8658a4df12f04f
Reviewed-on: https://gerrit.libreoffice.org/81676
Tested-by: Jenkins
Reviewed-by: Caolán McNamara <caolanm@redhat.com>
Tested-by: Caolán McNamara <caolanm@redhat.com>

sfx2/source/appl/appdde.cxx
@@ -23,7 +23,6 @@
 #include <rtl/uri.hxx>
 #include <sal/log.hxx>
 #include <sot/exchange.hxx>
-#include <vcl/wrkwin.hxx>
 #include <svl/rectitem.hxx>
 #include <svl/eitem.hxx>
 #include <svl/intitem.hxx>

commit 17e3cbe266b0dd288135e137f2424bad128a4bbc

Fix build with --disable-pch on Win
Broken after commit 2cd6ff8a54b0db76b2f4c5f75988367cfb0feaf0
- apparently vcl/svapp.hxx is needed

Most of sfx2/source/appl/appdde.cxx is Windows specific so
took the opportunity to clean it up with IWYU and commenting out
ifdefs

Change-Id: Ia0ad54f590cf19033e630ea4df98c244bfdc47a0

sfx2/IwyuFilter_sfx2.yaml
@@ -1,0 +1,26 @@
+---
+assumeFilename: sfx2/source/appl/app.cxx
+blacklist:
+    sfx2/source/appl/appdde.cxx:
+    # Needed on WIN32
+    - config_features.h
+    - rtl/character.hxx
+    - rtl/malformeduriexception.hxx
+    - rtl/uri.hxx
+    - sot/exchange.hxx
+    - svl/eitem.hxx
+    - basic/sbstar.hxx
+    - svl/stritem.hxx
+    - sfx2/lnkbase.hxx
+    - sfx2/linkmgr.hxx
+    - tools/debug.hxx
+    - tools/urlobj.hxx
+    - tools/diagnose_ex.h
+    - unotools/pathoptions.hxx
+    - vcl/svapp.hxx
+    - sfx2/viewfrm.hxx
+    - sfx2/dispatch.hxx
+    - sfx2/sfxsids.hrc
+    - sfx2/docfile.hxx
+    - ucbhelper/content.hxx
+    - comphelper/processfactory.hxx

sfx2/source/appl/appdde.cxx
@@ -21,35 +21,29 @@
 #include <rtl/character.hxx>
 #include <rtl/malformeduriexception.hxx>
 #include <rtl/uri.hxx>
-#include <sal/log.hxx>
 #include <sot/exchange.hxx>
-#include <svl/rectitem.hxx>
 #include <svl/eitem.hxx>
-#include <svl/intitem.hxx>
 #include <basic/sbstar.hxx>
 #include <svl/stritem.hxx>
 #include <svl/svdde.hxx>
 #include <sfx2/lnkbase.hxx>
 #include <sfx2/linkmgr.hxx>
 
+#include <tools/debug.hxx>
 #include <tools/urlobj.hxx>
 #include <tools/diagnose_ex.h>
 #include <unotools/pathoptions.hxx>
+#include <vcl/svapp.hxx>
 
 #include <sfx2/app.hxx>
 #include <appdata.hxx>
 #include <sfx2/objsh.hxx>
 #include <sfx2/viewfrm.hxx>
 #include <sfx2/dispatch.hxx>
-#include <sfxtypes.hxx>
 #include <sfx2/sfxsids.hrc>
-#include <helper.hxx>
 #include <sfx2/docfile.hxx>
 #include <ucbhelper/content.hxx>
 #include <comphelper/processfactory.hxx>
-#include <com/sun/star/ucb/CommandAbortedException.hpp>
-#include <com/sun/star/ucb/IllegalIdentifierException.hpp>
-#include <com/sun/star/ucb/ContentCreationException.hpp>
 
 #if defined(_WIN32)
 