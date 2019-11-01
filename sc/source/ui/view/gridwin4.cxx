05b72d7dc26080b2e97cbe76d08478926b20947e
Michael Meeks <michael.meeks@collabora.com>
2019-11-01 15:40:43 +0000

    sc lok: cope with non 100% zoom better.
    Do the tile rendering and alignment ourselves. More work required
    to get cleaner conversion between view and document twips (view
    twips being rounded to produce nice round pixel sizes when
    re-converted).
    
    Change-Id: I51edb186cfd2dc434005cc074f4ed8de19c85cb3
    Reviewed-on: https://gerrit.libreoffice.org/82092
    Tested-by: Jenkins
    Reviewed-by: Michael Meeks <michael.meeks@collabora.com>

--- sc/source/ui/view/gridwin4.cxx
+++ sc/source/ui/view/gridwin4.cxx
@@ -1086,7 +1086,6 @@ namespace
         const bool bColumnHeader = std::is_same<IndexType, SCCOL>::value;
 
         SCTAB nTab = pViewData->GetTabNo();
-        ScDocument* pDoc = pViewData->GetDocument();
 
         IndexType nStartIndex = -1;
         IndexType nEndIndex = -1;
@@ -1098,7 +1097,7 @@ namespace
         const auto& rStartNearest = rPositionHelper.getNearestByPosition(nTileStartPosPx);
         const auto& rEndNearest = rPositionHelper.getNearestByPosition(nTileEndPosPx);
 
-        ScBoundsProvider aBoundsProvider(pDoc, nTab, bColumnHeader);
+        ScBoundsProvider aBoundsProvider(*pViewData, nTab, bColumnHeader);
         aBoundsProvider.Compute(rStartNearest, rEndNearest, nTileStartPosPx, nTileEndPosPx);
         aBoundsProvider.GetStartIndexAndPosition(nStartIndex, nStartPosPx); ++nStartIndex;
         aBoundsProvider.GetEndIndexAndPosition(nEndIndex, nEndPosPx);
