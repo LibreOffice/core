6fffa2d6ee64fce95fac204eb6d6db0f0cefc75d
Jan Holesovsky <kendy@collabora.com>
2019-11-01 17:56:01 +0100

    android: Add also aarch64 to avoid linking errors.
    Change-Id: Ifdd696dc37de541bc722807054cd4ba7b862c175
    Reviewed-on: https://gerrit.libreoffice.org/81904
    Reviewed-by: Jan Holesovsky <kendy@collabora.com>
    Tested-by: Jan Holesovsky <kendy@collabora.com>
    (cherry picked from commit 620f39b816e3f10a4e047bb4f5ed5f4fdad2b364)
    Reviewed-on: https://gerrit.libreoffice.org/81914

--- external/libpng/StaticLibrary_libpng.mk
+++ external/libpng/StaticLibrary_libpng.mk
@@ -33,7 +33,7 @@ $(eval $(call gb_StaticLibrary_add_generated_cobjects,libpng,\
 	UnpackedTarball/libpng/pngwrite \
 	UnpackedTarball/libpng/pngwtran \
 	UnpackedTarball/libpng/pngwutil \
-	$(if $(filter ARM ARM64,$(CPUNAME)),\
+	$(if $(filter ARM AARCH64 ARM64,$(CPUNAME)),\
 	    UnpackedTarball/libpng/arm/arm_init \
 	    UnpackedTarball/libpng/arm/filter_neon_intrinsics \
 	    UnpackedTarball/libpng/arm/palette_neon_intrinsics \
