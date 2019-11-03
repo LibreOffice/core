3ee197971c91130594052dd1c39546a8efceae69
Mike Kaganski <mike.kaganski@collabora.com>
2019-11-03 20:47:44 +0300

    tdf#120006 New Document converter
    Change-Id: I38b60a56497278f68db68085c32f3f8586b3feed

--- cui/Library_cui.mk
+++ cui/Library_cui.mk
@@ -113,6 +113,7 @@ $(eval $(call gb_Library_add_exception_objects,cui,\
     cui/source/dialogs/cuitbxform \
     cui/source/dialogs/dlgname \
     cui/source/dialogs/DiagramDialog \
+    cui/source/dialogs/DocumentConverterDialog \
     cui/source/dialogs/FontFeaturesDialog \
     cui/source/dialogs/hangulhanjadlg \
     cui/source/dialogs/hldocntp \
