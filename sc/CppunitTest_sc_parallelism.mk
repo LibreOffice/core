770fcec369630f6a6201c035794f2d072ff9f991
Dennis Francis <dennis.francis@collabora.com>
2019-11-04 21:16:50 +0530

    tdf#124270 : unit-tests for spurious Err:522 fixes
    Change-Id: I44818c61611a6cdbcef3b71e151f7899795b3fa2

--- sc/CppunitTest_sc_parallelism.mk
+++ sc/CppunitTest_sc_parallelism.mk
@@ -15,29 +15,41 @@ $(eval $(call gb_CppunitTest_add_exception_objects,sc_parallelism, \
     sc/qa/unit/parallelism \
 ))
 
+$(eval $(call gb_CppunitTest_use_library_objects,sc_parallelism, \
+    sc \
+    scqahelper \
+))
+
 $(eval $(call gb_CppunitTest_use_externals,sc_parallelism, \
 	boost_headers \
     $(call gb_Helper_optional,OPENCL,clew) \
-	mdds_headers \
+    icu_headers \
+    icui18n \
+    icuuc \
+    libxml2 \
+    mdds_headers \
+    orcus \
+    orcus-parser \
 ))
 
 $(eval $(call gb_CppunitTest_use_libraries,sc_parallelism, \
+    $(call gb_Helper_optional,AVMEDIA,avmedia) \
     basegfx \
     comphelper \
     cppu \
     cppuhelper \
+    dbtools \
     drawinglayer \
     editeng \
     for \
     forui \
     i18nlangtag \
-    msfilter \
-    oox \
+    i18nutil \
+    $(call gb_Helper_optional,OPENCL,opencl) \
     sal \
     salhelper \
     sax \
-    sc \
-    scqahelper \
+    sb \
     sfx \
     sot \
     svl \
@@ -50,6 +62,7 @@ $(eval $(call gb_CppunitTest_use_libraries,sc_parallelism, \
     ucbhelper \
 	unotest \
     utl \
+    vbahelper \
     vcl \
     xo \
 ))
@@ -75,43 +88,20 @@ $(eval $(call gb_CppunitTest_use_ure,sc_parallelism))
 $(eval $(call gb_CppunitTest_use_vcl,sc_parallelism))
 
 $(eval $(call gb_CppunitTest_use_components,sc_parallelism,\
-    basic/util/sb \
-    chart2/source/chartcore \
-    chart2/source/controller/chartcontroller \
-    comphelper/util/comphelp \
     configmgr/source/configmgr \
-    dbaccess/util/dba \
-    embeddedobj/util/embobj \
-    eventattacher/source/evtatt \
-    filter/source/config/cache/filterconfig1 \
-    forms/util/frm \
     framework/util/fwk \
+    i18npool/source/search/i18nsearch \
     i18npool/util/i18npool \
-    linguistic/source/lng \
-    oox/util/oox \
-    package/source/xstor/xstor \
-    package/util/package2 \
     sax/source/expatwrap/expwrap \
     scaddins/source/analysis/analysis \
     scaddins/source/datefunc/date \
-    sc/util/sc \
-    sc/util/scfilt \
+    scaddins/source/pricing/pricing \
     sfx2/util/sfx \
-    sot/util/sot \
-    svl/util/svl \
-    svtools/util/svt \
-    svx/util/svx \
-    svx/util/svxcore \
-    toolkit/util/tk \
     ucb/source/core/ucb1 \
     ucb/source/ucp/file/ucpfile1 \
-    ucb/source/ucp/tdoc/ucptdoc1 \
-	uui/util/uui \
-    unotools/util/utl \
-    unoxml/source/rdf/unordf \
     unoxml/source/service/unoxml \
+    uui/util/uui \
     vcl/vcl.common \
-    xmloff/util/xo \
 ))
 
 $(eval $(call gb_CppunitTest_use_configuration,sc_parallelism))
