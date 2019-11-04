2cbfc725ec48e65a23e85853dc1de59355deee8f
Marco Cecchetti <marco.cecchetti@collabora.com>
2019-11-04 17:10:45 +0100

    lok: calc formula bar tunneling
    Change-Id: I92026098222e04a163796225a048c6d7dc1b37c5

--- sc/source/ui/inc/inputwin.hxx
+++ sc/source/ui/inc/inputwin.hxx
@@ -264,6 +264,7 @@ public:
     virtual void    dispose() override;
 
     virtual void    Paint( vcl::RenderContext& rRenderContext, const tools::Rectangle& rRect ) override;
+    virtual void    PixelInvalidate(const tools::Rectangle* pRectangle) override;
     virtual void    Resize() override;
     virtual void    Select() override;
 
@@ -301,6 +302,8 @@ public:
     virtual void    MouseButtonDown( const MouseEvent& rMEvt ) override;
     virtual void    MouseMove( const MouseEvent& rMEvt ) override;
 
+    void            NotifyLOKClient();
+
     DECL_LINK( MenuHdl, Menu *, bool );
     DECL_LINK( DropdownClickHdl, ToolBox*, void );
 
@@ -311,6 +314,7 @@ private:
     VclPtr<ScTextWndBase> pRuntimeWindow;
     ScTextWndBase&  aTextWindow;
     ScInputHandler* pInputHdl;
+    ScTabViewShell* mpViewShell;
     long            mnMaxY;
     bool            bIsOkCancelMode;
     bool            bInResize;
