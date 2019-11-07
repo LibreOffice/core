fb974bc968b3a1a95f8e621776076522ec719dc6
Michael Meeks <michael.meeks@collabora.com>
2019-11-07 14:11:35 +0000

    Drawing calc grid misses pixels on bottom and right; fix.
    Don't attempt to offset empty rectangles causing serious problems.
    
    Change-Id: I6fcac11f5f5e03f7edc0662cf4e06e7f8a109f00
    Reviewed-on: https://gerrit.libreoffice.org/82220
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Michael Meeks <michael.meeks@collabora.com>

--- vcl/source/outdev/rect.cxx
+++ vcl/source/outdev/rect.cxx
@@ -224,10 +224,6 @@ void OutputDevice::DrawGrid( const tools::Rectangle& rRect, const Size& rDist, D
 
     tools::Rectangle aDstRect( PixelToLogic( Point() ), GetOutputSize() );
     aDstRect.Intersection( rRect );
-    // FIXME: seems we have an off-by-one around the border
-    // here with the cairo / svp backend at least.
-    aDstRect.AdjustRight(1);
-    aDstRect.AdjustBottom(1);
 
     if( aDstRect.IsEmpty() || ImplIsRecordLayout() )
         return;
@@ -240,6 +236,11 @@ void OutputDevice::DrawGrid( const tools::Rectangle& rRect, const Size& rDist, D
 
     if( mbOutputClipped )
         return;
+
+    // FIXME: seems we have an off-by-one around the border
+    // here with the cairo / svp backend at least.
+    aDstRect.AdjustRight(1);
+    aDstRect.AdjustBottom(1);
 
     const long nDistX = std::max( rDist.Width(), 1L );
     const long nDistY = std::max( rDist.Height(), 1L );
