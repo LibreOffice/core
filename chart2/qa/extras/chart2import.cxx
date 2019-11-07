LibreOffice/core

commit 830e539547c463b932ce643517f880789185032d

tdf#127393 OOXML chart import: fix X axis position setting "CrossBetween"
Set ShiftedCategoryPosition according to OOXML value CrossBetween.

Change-Id: I6445c94507de83f84b08fd3380cbbf03886b5aa3
Reviewed-on: https://gerrit.libreoffice.org/79142
Reviewed-by: László Németh <nemeth@numbertext.org>
Tested-by: László Németh <nemeth@numbertext.org>

oox/source/drawingml/chart/axisconverter.cxx
@@ -213,6 +213,7 @@ void AxisConverter::convertFromModel(
                     aScaleData.AxisType = (bDateAxis && !mrModel.mbAuto) ? cssc2::AxisType::DATE : cssc2::AxisType::CATEGORY;
                     aScaleData.AutoDateAxis = mrModel.mbAuto;
                     aScaleData.Categories = rTypeGroups.front()->createCategorySequence();
+                    aScaleData.ShiftedCategoryPosition = pCrossingAxis->mnCrossBetween == XML_between;
                 }
                 else
                 {

chart2/qa/extras/chart2import.cxx
@@ -129,6 +129,7 @@ public:
 
     void testTdf114179();
     void testTdf124243();
+    void testTdf127393();
     void testDeletedDataLabel();
     void testDataPointInheritedColorDOCX();
     void testExternalStrRefsXLSX();
@@ -218,6 +219,7 @@ public:
 
     CPPUNIT_TEST(testTdf114179);
     CPPUNIT_TEST(testTdf124243);
+    CPPUNIT_TEST(testTdf127393);
     CPPUNIT_TEST(testDeletedDataLabel);
     CPPUNIT_TEST(testDataPointInheritedColorDOCX);
     CPPUNIT_TEST(testExternalStrRefsXLSX);
@@ -1927,6 +1929,33 @@ void Chart2ImportTest::testTdf124243()
     bool bSuccess = xPS->getPropertyValue("Show") >>= bShow;
     CPPUNIT_ASSERT(bSuccess);
     CPPUNIT_ASSERT(!bShow);
+}
+
+void Chart2ImportTest::testTdf127393()
+{
+    load("/chart2/qa/extras/data/pptx/", "tdf127393.pptx");
+
+    // 1st chart
+    Reference<chart2::XChartDocument> xChartDoc(getChartDocFromDrawImpress(0, 0), uno::UNO_QUERY);
+    CPPUNIT_ASSERT(xChartDoc.is());
+
+    Reference<chart2::XAxis> xAxis = getAxisFromDoc(xChartDoc, 0, 0, 0);
+    CPPUNIT_ASSERT(xAxis.is());
+
+    chart2::ScaleData aScaleData1 = xAxis->getScaleData();
+    CPPUNIT_ASSERT(aScaleData1.Categories.is());
+    CPPUNIT_ASSERT(aScaleData1.ShiftedCategoryPosition);
+
+    // 2nd chart
+    xChartDoc.set(getChartDocFromDrawImpress(1, 0), uno::UNO_QUERY);
+    CPPUNIT_ASSERT(xChartDoc.is());
+
+    xAxis.set(getAxisFromDoc(xChartDoc, 0, 0, 0));
+    CPPUNIT_ASSERT(xAxis.is());
+
+    chart2::ScaleData aScaleData2 = xAxis->getScaleData();
+    CPPUNIT_ASSERT(aScaleData2.Categories.is());
+    CPPUNIT_ASSERT(!aScaleData2.ShiftedCategoryPosition);
 }
 
 namespace {

chart2/source/view/main/ChartView.cxx
@@ -325,7 +325,7 @@ public:
     void AdaptScaleOfYAxisWithoutAttachedSeries( ChartModel& rModel );
 
     bool isCategoryPositionShifted(
-        const chart2::ScaleData& rSourceScale, bool bHasComplexCategories ) const;
+        const chart2::ScaleData& rSourceScale, bool bHasComplexCategories, bool bShiftedCategoryPosition) const;
 
 private:
     /** A vector of series plotters.
@@ -631,12 +631,12 @@ void SeriesPlotterContainer::initializeCooSysAndSeriesPlotter(
 }
 
 bool SeriesPlotterContainer::isCategoryPositionShifted(
-    const chart2::ScaleData& rSourceScale, bool bHasComplexCategories ) const
+    const chart2::ScaleData& rSourceScale, bool bHasComplexCategories, bool bShiftedCategoryPosition) const
 {
-    if (rSourceScale.AxisType == AxisType::CATEGORY && m_bChartTypeUsesShiftedCategoryPositionPerDefault)
+    if (rSourceScale.AxisType == AxisType::CATEGORY && (m_bChartTypeUsesShiftedCategoryPositionPerDefault || bShiftedCategoryPosition))
         return true;
 
-    if (rSourceScale.AxisType==AxisType::CATEGORY && bHasComplexCategories)
+    if (rSourceScale.AxisType == AxisType::CATEGORY && bHasComplexCategories)
         return true;
 
     if (rSourceScale.AxisType == AxisType::DATE)
@@ -683,7 +683,9 @@ void SeriesPlotterContainer::initAxisUsageList(const Date& rNullDate)
                         AxisHelper::checkDateAxis( aSourceScale, pCatProvider, bDateAxisAllowed );
 
                     bool bHasComplexCat = pCatProvider && pCatProvider->hasComplexCategories();
-                    aSourceScale.ShiftedCategoryPosition = isCategoryPositionShifted(aSourceScale, bHasComplexCat);
+                    // Come from CrossBetween OOXML tag
+                    bool bShiftedCategoryPosition = aSourceScale.ShiftedCategoryPosition;
+                    aSourceScale.ShiftedCategoryPosition = isCategoryPositionShifted(aSourceScale, bHasComplexCat, bShiftedCategoryPosition);
 
                     m_aAxisUsageList[xAxis].aAutoScaling = ScaleAutomatism(aSourceScale, rNullDate);
                 }

oox/source/drawingml/chart/axiscontext.cxx
@@ -281,7 +281,7 @@ ContextHandlerRef ValAxisContext::onCreateContext( sal_Int32 nElement, const Att
     if( isRootElement() ) switch( nElement )
     {
         case C_TOKEN( crossBetween ):
-            mrModel.mnCrossBetween = rAttribs.getToken( XML_val, XML_between );
+            mrModel.mnCrossBetween = rAttribs.getToken( XML_val, -1 );
             return nullptr;
         case C_TOKEN( dispUnits ):
             return new AxisDispUnitsContext( *this, mrModel.mxDispUnits.create() );

oox/source/drawingml/chart/axismodel.cxx
@@ -36,7 +36,7 @@ AxisModel::AxisModel( sal_Int32 nTypeId, bool bMSO2007Doc ) :
     mnAxisId( -1 ),
     mnAxisPos( XML_TOKEN_INVALID ),
     mnCrossAxisId( -1 ),
-    mnCrossBetween( XML_between ),
+    mnCrossBetween( -1 ),
     mnCrossMode( XML_autoZero ),
     mnLabelAlign( XML_ctr ),
     mnLabelOffset( 100 ),

commit 0e590013664eab50f2583f41dac8396b440fcc14

tdf#128627 tdf#128634 Fix OOXML import of CrossBetween
Set default 'midCat' value during the import, for Radar charts,
because MSO created them with default 'midCat', but exported a
'between' value.

Set default 'between' value during the import, for 3D Bar and
Stock charts, because sometimes the OOXML file can contain a
wrong CrossBetween value, but MSO import them with default
values.

tdf#128627 regression from commit: 830e539547c463b932ce643517f880789185032d
(tdf#127393 OOXML chart import: fix X axis position setting "CrossBetween")
tdf#128634 regression from commit: 111c260ab2883b7906f1a66e222dbf4dc3c58c4f
(tdf#127777 OOXML chart export: fix "CrossBetween" for not imported charts)

Change-Id: Ie9763197b79c3a661e66043da7b89b4f0f00ba33

oox/source/drawingml/chart/axisconverter.cxx
@@ -213,7 +213,14 @@ void AxisConverter::convertFromModel(
                     aScaleData.AxisType = (bDateAxis && !mrModel.mbAuto) ? cssc2::AxisType::DATE : cssc2::AxisType::CATEGORY;
                     aScaleData.AutoDateAxis = mrModel.mbAuto;
                     aScaleData.Categories = rTypeGroups.front()->createCategorySequence();
-                    aScaleData.ShiftedCategoryPosition = pCrossingAxis->mnCrossBetween == XML_between;
+                    /* set default ShiftedCategoryPosition values for some charttype,
+                       because the XML can contain wrong CrossBetween value, created by MSO */
+                    if( rTypeGroups.front()->is3dChart() && (rTypeInfo.meTypeId == TYPEID_BAR || rTypeInfo.meTypeId == TYPEID_HORBAR || rTypeInfo.meTypeId == TYPEID_STOCK) )
+                        aScaleData.ShiftedCategoryPosition = true;
+                    else if( rTypeInfo.meTypeId == TYPEID_RADARLINE || rTypeInfo.meTypeId == TYPEID_RADARAREA )
+                        aScaleData.ShiftedCategoryPosition = false;
+                    else
+                        aScaleData.ShiftedCategoryPosition = pCrossingAxis->mnCrossBetween == XML_between;
                 }
                 else
                 {

chart2/qa/extras/chart2import.cxx
@@ -133,6 +133,8 @@ public:
     void testTdf124243();
     void testTdf127393();
     void testTdf128432();
+    void testTdf128627();
+    void testTdf128634();
     void testDeletedDataLabel();
     void testDataPointInheritedColorDOCX();
     void testExternalStrRefsXLSX();
@@ -226,6 +228,8 @@ public:
     CPPUNIT_TEST(testTdf124243);
     CPPUNIT_TEST(testTdf127393);
     CPPUNIT_TEST(testTdf128432);
+    CPPUNIT_TEST(testTdf128627);
+    CPPUNIT_TEST(testTdf128634);
     CPPUNIT_TEST(testDeletedDataLabel);
     CPPUNIT_TEST(testDataPointInheritedColorDOCX);
     CPPUNIT_TEST(testExternalStrRefsXLSX);
@@ -2003,6 +2007,38 @@ void Chart2ImportTest::testTdf128432()
 
     uno::Reference< chart2::XChartDocument > xChartDoc = getChartDocFromSheet(0, mxComponent);
     CPPUNIT_ASSERT(xChartDoc.is());
+
+    Reference<chart2::XAxis> xAxis = getAxisFromDoc(xChartDoc, 0, 0, 0);
+    CPPUNIT_ASSERT(xAxis.is());
+
+    chart2::ScaleData aScaleData = xAxis->getScaleData();
+    CPPUNIT_ASSERT(aScaleData.Categories.is());
+    CPPUNIT_ASSERT(aScaleData.ShiftedCategoryPosition);
+}
+
+void Chart2ImportTest::testTdf128627()
+{
+    load("/chart2/qa/extras/data/xlsx/", "tdf128627.xlsx");
+
+    // Test ShiftedCategoryPosition for Radar Chart
+    uno::Reference<chart2::XChartDocument> xChartDoc = getChartDocFromSheet(0, mxComponent);
+    CPPUNIT_ASSERT_MESSAGE("failed to load chart", xChartDoc.is());
+
+    Reference<chart2::XAxis> xAxis = getAxisFromDoc(xChartDoc, 0, 0, 0);
+    CPPUNIT_ASSERT(xAxis.is());
+
+    chart2::ScaleData aScaleData = xAxis->getScaleData();
+    CPPUNIT_ASSERT(aScaleData.Categories.is());
+    CPPUNIT_ASSERT(!aScaleData.ShiftedCategoryPosition);
+}
+
+void Chart2ImportTest::testTdf128634()
+{
+    load("/chart2/qa/extras/data/xlsx/", "tdf128634.xlsx");
+
+    // Test ShiftedCategoryPosition for 3D Charts
+    uno::Reference<chart2::XChartDocument> xChartDoc = getChartDocFromSheet(0, mxComponent);
+    CPPUNIT_ASSERT_MESSAGE("failed to load chart", xChartDoc.is());
 
     Reference<chart2::XAxis> xAxis = getAxisFromDoc(xChartDoc, 0, 0, 0);
     CPPUNIT_ASSERT(xAxis.is());