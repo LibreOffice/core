479f228329830bc4060c1959114d970c8bacb8d5
László Németh <nemeth@numbertext.org>
2019-11-08 16:13:25 +0100

    tdf#128428 RTF: fix longer space sequence regressions
    Workaround for the RTF-only longer space sequences
    of the old or compatible RTF documents.
    
    Regression from commit 24b04db5a63b57a74e58a7616091437ad68548ac
    (tdf#123703 RTF import: fix length of space character sequence)
    
    Change-Id: I308ab06db57a2db5deec1d4c4573da3317cad8e9

--- writerfilter/source/rtftok/rtfdispatchvalue.cxx
+++ writerfilter/source/rtftok/rtfdispatchvalue.cxx
@@ -519,6 +519,12 @@ RTFError RTFDocumentImpl::dispatchValue(RTFKeyword nKeyword, int nParam)
         case RTF_DEFF:
             m_nDefaultFontIndex = nParam;
             break;
+        case RTF_STSHFDBCH:
+            // tdf#123703 switch off longer space sequence except in the case of the fixed compatibility setting font id 31505
+            if (nParam != 31505)
+                m_aSettingsTableSprms.set(NS_ooxml::LN_CT_Settings_longerSpaceSequence,
+                                          new RTFValue(0));
+            break;
         case RTF_DEFLANG:
         case RTF_ADEFLANG:
         {
