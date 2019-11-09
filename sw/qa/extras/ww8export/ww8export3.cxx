LibreOffice/core

commit 3539a1efb41a787237e4333ebc715db96ffacb5b

tdf#116071 ww8import: import to XATTR_FILL instead of RES_BACKGROUND
The new XATTR_FILL* was introduced a few years ago and the UI
changed to that, but .doc import was still importing into
RES_BACKGROUND and although the property showed up OK, it
couldn't be changed or seen by the UI and somehow
hidden/unavailable as UNO properties.

Change-Id: Iea1805a321a306994ab23bb66a354dca8e3a3d40
Reviewed-on: https://gerrit.libreoffice.org/64186
Reviewed-by: Justin Luth <justin_luth@sil.org>
Tested-by: Justin Luth <justin_luth@sil.org>

sw/source/filter/ww8/ww8par6.cxx
@@ -68,6 +68,8 @@
 #include <editeng/frmdiritem.hxx>
 #include <editeng/charhiddenitem.hxx>
 #include <i18nlangtag/mslangid.hxx>
+#include <svx/xfillit0.hxx>
+#include <svx/xflclit.hxx>
 #include "sprmids.hxx"
 #include <fmtpdsc.hxx>
 #include <node.hxx>
@@ -4877,7 +4879,8 @@ void SwWW8ImplReader::Read_Shade( sal_uInt16, const sal_uInt8* pData, short nLen
         aSHD.SetWWValue( *reinterpret_cast<SVBT16 const *>(pData) );
         SwWW8Shade aSh( m_bVer67, aSHD );
 
-        NewAttr(SvxBrushItem(aSh.aColor, RES_BACKGROUND));
+        NewAttr( XFillStyleItem(drawing::FillStyle_SOLID) );
+        NewAttr( XFillColorItem(OUString(), aSh.aColor) );
     }
 }
 
@@ -4893,7 +4896,9 @@ void SwWW8ImplReader::Read_ParaBackColor(sal_uInt16, const sal_uInt8* pData, sho
         OSL_ENSURE(nLen == 10, "Len of para back colour not 10!");
         if (nLen != 10)
             return;
-        NewAttr(SvxBrushItem(ExtractColour(pData, m_bVer67), RES_BACKGROUND));
+
+        NewAttr( XFillStyleItem(drawing::FillStyle_SOLID) );
+        NewAttr( XFillColorItem(OUString(), ExtractColour(pData, m_bVer67)) );
     }
 }
 

sw/qa/extras/ww8export/ww8export3.cxx
@@ -18,6 +18,7 @@
 #include <com/sun/star/text/XTextTablesSupplier.hpp>
 
 #include <drawdoc.hxx>
+#include <svx/xfillit0.hxx>
 
 class Test : public SwModelTestBase
 {
@@ -167,6 +168,18 @@ DECLARE_WW8EXPORT_TEST(testTdf120225_textControlCrossRef, "tdf120225_textControl
 
     // The actual name isn't critical, but if it fails, it is worth asking why.
     CPPUNIT_ASSERT_EQUAL(OUString("Text1"), sTextFieldName);
+}
+
+DECLARE_WW8EXPORT_TEST(testTdf121111_fillStyleNone, "tdf121111_fillStyleNone.docx")
+{
+    uno::Reference<beans::XPropertySet> xStyle(getStyles("ParagraphStyles")->getByName("Numbering - First level"),
+                                                     uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(Color(184,204,228), Color(getProperty<sal_uInt32>(xStyle, "ParaBackColor")));//R:184 G:204 B:228
+    CPPUNIT_ASSERT_EQUAL(drawing::FillStyle_SOLID, getProperty<drawing::FillStyle>(xStyle, "FillStyle"));
+
+    uno::Reference<text::XTextRange> xText(getParagraph(12));
+    CPPUNIT_ASSERT_EQUAL(COL_AUTO, Color(getProperty<sal_uInt32>(xText, "ParaBackColor")));
+    CPPUNIT_ASSERT_EQUAL(drawing::FillStyle_SOLID, getProperty<drawing::FillStyle>(xText, "FillStyle"));
 }
 
 DECLARE_WW8EXPORT_TEST(testTdf94009_zeroPgMargin, "tdf94009_zeroPgMargin.odt")

commit 6072a02b41c1447bafac607d2110c8c14dbb629f

partial revert tdf#116071 import to XATTR_FILL not RES_BACKGROUND
caused by commit 3539a1efb41a787237e4333ebc715db96ffacb5b
which fixes table-related regression bug report tdf#128608.

I was not able to create a unit test for this from scratch.
In fact, if I use MSWord2003 to roundtrip the file, then LO
opens it without problems.

A real fix (to move LO away from RES_BACKGROUND in tables) should
be subject to lots of testing time, so just reverting for now.

Change-Id: I328741947b7ed319ff8df0ba169618f6ad0d2c75

sw/source/filter/ww8/ww8par6.cxx
@@ -4896,8 +4896,13 @@ void SwWW8ImplReader::Read_ParaBackColor(sal_uInt16, const sal_uInt8* pData, sho
     if (nLen <= 0)
     {
         // end of attribute
-        m_xCtrlStck->SetAttr( *m_pPaM->GetPoint(), XATTR_FILLSTYLE );
-        m_xCtrlStck->SetAttr( *m_pPaM->GetPoint(), XATTR_FILLCOLOR );
+        if ( m_nInTable )
+            m_xCtrlStck->SetAttr( *m_pPaM->GetPoint(), RES_BACKGROUND );
+        else
+        {
+            m_xCtrlStck->SetAttr( *m_pPaM->GetPoint(), XATTR_FILLSTYLE );
+            m_xCtrlStck->SetAttr( *m_pPaM->GetPoint(), XATTR_FILLCOLOR );
+        }
     }
     else
     {
@@ -4905,8 +4910,13 @@ void SwWW8ImplReader::Read_ParaBackColor(sal_uInt16, const sal_uInt8* pData, sho
         if (nLen != 10)
             return;
 
-        NewAttr( XFillStyleItem(drawing::FillStyle_SOLID) );
-        NewAttr( XFillColorItem(OUString(), ExtractColour(pData, m_bVer67)) );
+        if ( m_nInTable )
+            NewAttr( SvxBrushItem(ExtractColour(pData, m_bVer67), RES_BACKGROUND) );
+        else
+        {
+            NewAttr( XFillStyleItem(drawing::FillStyle_SOLID) );
+            NewAttr( XFillColorItem(OUString(), ExtractColour(pData, m_bVer67)) );
+        }
     }
 }
 

sw/qa/extras/ww8export/ww8export3.cxx
@@ -226,6 +226,22 @@ DECLARE_WW8EXPORT_TEST(testTdf127862_pageFillStyle, "tdf127862_pageFillStyle.odt
     CPPUNIT_ASSERT(drawing::FillStyle_NONE != getProperty<drawing::FillStyle>(xStyle, "FillStyle"));
 }
 
+DECLARE_WW8EXPORT_TEST(testTdf128608_tableParaBackColor, "tdf128608_tableParaBackColor.doc")
+{
+    uno::Reference<text::XTextTablesSupplier> xTextTablesSupplier(mxComponent, uno::UNO_QUERY);
+    uno::Reference<container::XIndexAccess> xTables(xTextTablesSupplier->getTextTables(), uno::UNO_QUERY);
+    uno::Reference<text::XTextTable> xTable(xTables->getByIndex(0), uno::UNO_QUERY);
+    uno::Reference<text::XTextRange> xCell(xTable->getCellByName("A4"), uno::UNO_QUERY);
+
+    uno::Reference<container::XEnumerationAccess> xParaEnumAccess(xCell->getText(), uno::UNO_QUERY);
+    uno::Reference<container::XEnumeration> xParaEnum = xParaEnumAccess->createEnumeration();
+    uno::Reference<text::XTextRange> xPara(xParaEnum->nextElement(), uno::UNO_QUERY);
+    // ParaBackColor doesn't seem to be used in this case, but keep it here to make sure it stays as AUTO.
+    CPPUNIT_ASSERT_EQUAL(COL_AUTO, Color(getProperty<sal_uInt32>(xPara, "ParaBackColor")));
+    // No paragraph background colour/fill. (The cell background colour should be used.)
+    CPPUNIT_ASSERT_EQUAL_MESSAGE("No fillstyle", drawing::FillStyle_NONE, getProperty<drawing::FillStyle>(xPara, "FillStyle"));
+}
+
 DECLARE_WW8EXPORT_TEST(testTdf94009_zeroPgMargin, "tdf94009_zeroPgMargin.odt")
 {
     uno::Reference<beans::XPropertySet> defaultStyle(getStyles("PageStyles")->getByName("Standard"),