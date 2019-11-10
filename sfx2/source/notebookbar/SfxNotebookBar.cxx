d0043894baf9aeb846d6f44bd44e7d417deb6c4e
Jim Raykowski <raykowj@gmail..com>
2019-11-09 16:07:09 -0900

    tdf#118526 Reload Notebookbar if read mode has switched
    Change-Id: I8d6edd57566c66165690808e853a74777bc8e14a

--- sfx2/source/notebookbar/SfxNotebookBar.cxx
+++ sfx2/source/notebookbar/SfxNotebookBar.cxx
@@ -332,7 +332,7 @@ bool SfxNotebookBar::StateMethod(SfxBindings& rBindings, const OUString& rUIFile
 {
     SfxFrame& rFrame = rBindings.GetDispatcher_Impl()->GetFrame()->GetFrame();
     return StateMethod(rFrame.GetSystemWindow(), rFrame.GetFrameInterface(), rUIFile,
-                       bReloadNotebookbar);
+                       bReloadNotebookbar || rBindings.GetDispatcher_Impl()->IsUpdated_Impl());
 }
 
 bool SfxNotebookBar::StateMethod(SystemWindow* pSysWindow,
