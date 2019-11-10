e7423d0574bae586c37c19764ecc061bd4191eb1
Luboš Luňák <l.lunak@collabora.com>
2019-11-10 20:26:59 +0100

    rename a PCH file to properly match its Makefile
    Change-Id: I61f317f4cc31d329452a08b6cee5f59975fea007
    Reviewed-on: https://gerrit.libreoffice.org/82398
    Tested-by: Jenkins
    Reviewed-by: Luboš Luňák <l.lunak@collabora.com>

--- connectivity/inc/pch/precompiled_mysql_jdbc.cxx
+++ connectivity/inc/pch/precompiled_mysql_jdbc.cxx
@@ -7,6 +7,6 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-#include "precompiled_mysql.hxx"
+#include "precompiled_mysql_jdbc.hxx"
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
