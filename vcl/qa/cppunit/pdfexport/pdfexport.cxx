LibreOffice/core

commit 68549e00d5e23aa22bc974a8151d93cd948444b3

vcl, BitmapEx transformed draw: special-case simple rotations
In case OutputDevice::DrawTransformedBitmapEx() has to do both shearing
and rotation, then recording to a metafile is unchanged.  But if we need
to do rotation, then it's not necessary to go via transformations.

This has the additional benefit that 90/180/270 degree rotations don't
introduce an off-by-one error, where the first row and col of the
transformed bitmap is transparent.

(At the moment it's not clear what introduces the unwanted translation,
but at least the direct Rotate() way resolves the visible end-user
problem, see the test.)

Change-Id: Ie1adbdb2221b086c19cc66f69308b6b7256fe29a
Reviewed-on: https://gerrit.libreoffice.org/80626
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

vcl/source/outdev/bitmap.cxx
@@ -1285,10 +1285,24 @@ void OutputDevice::DrawTransformedBitmapEx(
                 aFullTransform *= aTransform;
             }
 
-            aTransformed = aTransformed.getTransformed(
-                aFullTransform,
-                aVisibleRange,
-                fMaximumArea);
+            if (bSheared)
+            {
+                aTransformed = aTransformed.getTransformed(
+                    aFullTransform,
+                    aVisibleRange,
+                    fMaximumArea);
+            }
+            else
+            {
+                // Just rotation, can do that directly.
+                fFullRotate = fmod(fFullRotate * -1, F_2PI);
+                if (fFullRotate < 0)
+                {
+                    fFullRotate += F_2PI;
+                }
+                long nAngle10 = basegfx::fround(basegfx::rad2deg(fFullRotate) * 10);
+                aTransformed.Rotate(nAngle10, COL_TRANSPARENT);
+            }
             basegfx::B2DRange aTargetRange(0.0, 0.0, 1.0, 1.0);
 
             // get logic object target range

vcl/qa/cppunit/outdev.cxx
@@ -15,6 +15,7 @@
 #include <vcl/bitmapaccess.hxx>
 #include <vcl/gdimtf.hxx>
 #include <vcl/metaact.hxx>
+#include <bitmapwriteaccess.hxx>
 
 #include <basegfx/matrix/b2dhommatrix.hxx>
 
@@ -142,9 +143,22 @@ void VclOutdevTest::testDrawTransformedBitmapEx()
     // Also create a 16x16 bitmap.
     ScopedVclPtrInstance<VirtualDevice> pVDev;
     Bitmap aBitmap(Size(16, 16), 24);
+    {
+        // Fill the top left quarter with black.
+        BitmapScopedWriteAccess pWriteAccess(aBitmap);
+        pWriteAccess->Erase(COL_WHITE);
+        for (int i = 0; i < 8; ++i)
+        {
+            for (int j = 0; j < 8; ++j)
+            {
+                pWriteAccess->SetPixel(j, i, COL_BLACK);
+            }
+        }
+    }
     BitmapEx aBitmapEx(aBitmap);
     basegfx::B2DHomMatrix aMatrix;
     aMatrix.scale(8, 8);
+    // Rotate 90 degrees clockwise, so the black part goes to the top right.
     aMatrix.rotate(M_PI / 2);
     GDIMetaFile aMtf;
     aMtf.Record(pVDev.get());
@@ -162,6 +176,29 @@ void VclOutdevTest::testDrawTransformedBitmapEx()
     // - Actual  : 8x8
     // I.e. the bitmap before scaling was already scaled down, just because it was rotated.
     CPPUNIT_ASSERT_EQUAL(Size(16, 16), aTransformedSize);
+
+    aBitmap = rBitmapEx.GetBitmap();
+    Bitmap::ScopedReadAccess pAccess(aBitmap);
+    for (int i = 0; i < 16; ++i)
+    {
+        for (int j = 0; j < 16; ++j)
+        {
+            BitmapColor aColor = pAccess->GetPixel(j, i);
+            Color aExpected = i >= 8 && j < 8 ? COL_BLACK : COL_WHITE;
+            std::stringstream ss;
+            ss << "Color is expected to be ";
+            ss << ((aExpected == COL_WHITE) ? "white" : "black");
+            ss << ", is " << aColor.AsRGBHexString();
+            ss << " (row " << j << ", col " << i << ")";
+            // Without the accompanying fix in place, this test would have failed with:
+            // - Expected: c[00000000]
+            // - Actual  : c[ffffff00]
+            // - Color is expected to be black, is ffffff (row 0, col 8)
+            // i.e. the top right quarter of the image was not fully black, there was a white first
+            // row.
+            CPPUNIT_ASSERT_EQUAL_MESSAGE(ss.str(), aExpected, Color(aColor));
+        }
+    }
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(VclOutdevTest);

commit cddfb3c218c1321c874d3630a7390c8ad58a2538

tdf#128630 vcl PDF export: fix aspect ratio when scaling rotated image
Regression from commit 68549e00d5e23aa22bc974a8151d93cd948444b3 (vcl,
BitmapEx transformed draw: special-case simple rotations, 2019-10-10),
the intention there was to fix an error on the last col/row of a bitmap,
but that was only tested with input where the aspect ratio doesn't
change on scaling.

Fix the problem by going back to the original way in the "aspect ratio
changes" case.

Change-Id: I52bed503ddaadbbbdf64ac6fec2fe268153866f1

vcl/source/outdev/bitmap.cxx
@@ -1285,8 +1285,20 @@ void OutputDevice::DrawTransformedBitmapEx(
                 aFullTransform *= aTransform;
             }
 
-            if (bSheared)
+            double fSourceRatio = 1.0;
+            if (rOriginalSizePixel.getHeight() != 0)
             {
+                fSourceRatio = rOriginalSizePixel.getWidth() / rOriginalSizePixel.getHeight();
+            }
+            double fTargetRatio = 1.0;
+            if (aFullScale.getY() != 0)
+            {
+                fTargetRatio = aFullScale.getX() / aFullScale.getY();
+            }
+            bool bAspectRatioKept = rtl::math::approxEqual(fSourceRatio, fTargetRatio);
+            if (bSheared || !bAspectRatioKept)
+            {
+                // Not only rotation, or scaling does not keep aspect ratio.
                 aTransformed = aTransformed.getTransformed(
                     aFullTransform,
                     aVisibleRange,

vcl/qa/cppunit/pdfexport/pdfexport.cxx
@@ -128,6 +128,7 @@ public:
     void testTdf66597_3();
     void testTdf109143();
     void testTdf105954();
+    void testTdf128630();
     void testTdf106702();
     void testTdf113143();
     void testTdf115262();
@@ -164,6 +165,7 @@ public:
     CPPUNIT_TEST(testTdf66597_3);
     CPPUNIT_TEST(testTdf109143);
     CPPUNIT_TEST(testTdf105954);
+    CPPUNIT_TEST(testTdf128630);
     CPPUNIT_TEST(testTdf106702);
     CPPUNIT_TEST(testTdf113143);
     CPPUNIT_TEST(testTdf115262);
@@ -1508,6 +1510,40 @@ void PdfExportTest::testTdf105954()
     // This was 2000, i.e. the 'reduce to 300 DPI' request was ignored.
     // This is now around 238 (228 on macOS).
     CPPUNIT_ASSERT_LESS(static_cast<unsigned int>(250), aMeta.width);
+}
+
+void PdfExportTest::testTdf128630()
+{
+    // Import the bugdoc and export as PDF.
+    OUString aURL = m_directories.getURLFromSrc(DATA_DIRECTORY) + "tdf128630.odp";
+    utl::MediaDescriptor aMediaDescriptor;
+    aMediaDescriptor["FilterName"] <<= OUString("impress_pdf_Export");
+    DocumentHolder pPdfDocument = exportAndParse(aURL, aMediaDescriptor);
+
+    // The document has one page.
+    CPPUNIT_ASSERT_EQUAL(1, FPDF_GetPageCount(pPdfDocument.get()));
+
+    // Assert the aspect ratio of the only bitmap on the page.
+    PageHolder pPdfPage(FPDF_LoadPage(pPdfDocument.get(), /*page_index=*/0));
+    CPPUNIT_ASSERT(pPdfPage.get());
+    int nPageObjectCount = FPDFPage_CountObjects(pPdfPage.get());
+    for (int i = 0; i < nPageObjectCount; ++i)
+    {
+        FPDF_PAGEOBJECT pPageObject = FPDFPage_GetObject(pPdfPage.get(), i);
+        if (FPDFPageObj_GetType(pPageObject) != FPDF_PAGEOBJ_IMAGE)
+            continue;
+
+        FPDF_BITMAP pBitmap = FPDFImageObj_GetBitmap(pPageObject);
+        CPPUNIT_ASSERT(pBitmap);
+        int nWidth = FPDFBitmap_GetWidth(pBitmap);
+        int nHeight = FPDFBitmap_GetHeight(pBitmap);
+        FPDFBitmap_Destroy(pBitmap);
+        // Without the accompanying fix in place, this test would have failed with:
+        // assertion failed
+        // - Expression: nWidth != nHeight
+        // i.e. the bitmap lost its custom aspect ratio during export.
+        CPPUNIT_ASSERT(nWidth != nHeight);
+    }
 }
 
 void PdfExportTest::testTdf106702()