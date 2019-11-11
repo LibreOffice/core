783cc8db3253880af0c9d2e4a3988a233e4abd5a
Miklos Vajna <vmiklos@collabora.com>
2019-11-11 14:08:14 +0100

    Related: tdf#117658 PPTX import: ignore math text outside <m:t>
    This way a pretty-printed bug document doesn't assert on import with:
    
    soffice.bin: /home/vmiklos/git/libreoffice/master/oox/source/mathml/importutils.cxx:335: void oox::formulaimport::XmlStreamBuilder::appendCharacters(const rtl::OUString&): Assertion `!tags.empty()' failed.
    
    (cherry picked from commit b280c02d584ad403417db0e8840f8ae6de245883)
    
    Change-Id: Icf8b11f3c56076b1ad2dddad196260ee87540020

---
+++ oox/CppunitTest_oox_mathml.mk
@@ -0,0 +1,44 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#*************************************************************************
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+#*************************************************************************
+
+$(eval $(call gb_CppunitTest_CppunitTest,oox_mathml))
+
+$(eval $(call gb_CppunitTest_use_externals,oox_mathml,\
+	boost_headers \
+))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,oox_mathml, \
+    oox/qa/unit/mathml \
+))
+
+$(eval $(call gb_CppunitTest_use_libraries,oox_mathml, \
+    comphelper \
+    cppu \
+    oox \
+    sal \
+    test \
+    unotest \
+))
+
+$(eval $(call gb_CppunitTest_use_sdk_api,oox_mathml))
+
+$(eval $(call gb_CppunitTest_use_ure,oox_mathml))
+$(eval $(call gb_CppunitTest_use_vcl,oox_mathml))
+
+$(eval $(call gb_CppunitTest_use_rdb,oox_mathml,services))
+
+$(eval $(call gb_CppunitTest_use_custom_headers,oox_mathml,\
+	officecfg/registry \
+))
+
+$(eval $(call gb_CppunitTest_use_configuration,oox_mathml))
+
+# vim: set noet sw=4 ts=4:
