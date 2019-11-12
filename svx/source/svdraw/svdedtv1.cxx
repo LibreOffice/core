f1c051bf82fe5f5bbd270d7125a55d3be8eafd81
Szymon Kłos <szymon.klos@collabora.com>
2019-11-12 10:43:53 +0100

    Avoid nullptr dereference
    Conditions already present on master
    
    Change-Id: I527482521191e51340f19f601f03d94fce50d429

--- svx/source/svdraw/svdedtv1.cxx
+++ svx/source/svdraw/svdedtv1.cxx
@@ -938,26 +938,33 @@ void SdrEditView::MergeAttrFromMarked(SfxItemSet& rAttr, bool bOnlyHardAttr) con
                     case XATTR_LINECOLOR:
                     {
                         const SfxPoolItem* pItem = rSet.GetItem(XATTR_LINECOLOR);
-                        Color aColor = static_cast<const XLineColorItem*>(pItem)->GetColorValue();
-                        sPayload = OUString::number(static_cast<sal_uInt32>(aColor));
+                        if (pItem)
+                        {
+                            Color aColor = static_cast<const XLineColorItem*>(pItem)->GetColorValue();
+                            sPayload = OUString::number(static_cast<sal_uInt32>(aColor));
 
-                        sPayload = ".uno:XLineColor=" + sPayload;
+                            sPayload = ".uno:XLineColor=" + sPayload;
+                        }
                         break;
                     }
 
                     case XATTR_FILLCOLOR:
                     {
                         const SfxPoolItem* pItem = rSet.GetItem(XATTR_FILLCOLOR);
-                        Color aColor = static_cast<const XFillColorItem*>(pItem)->GetColorValue();
-                        sPayload = OUString::number(static_cast<sal_uInt32>(aColor));
+                        if (pItem)
+                        {
+                            Color aColor = static_cast<const XFillColorItem*>(pItem)->GetColorValue();
+                            sPayload = OUString::number(static_cast<sal_uInt32>(aColor));
 
-                        sPayload = ".uno:FillColor=" + sPayload;
+                            sPayload = ".uno:FillColor=" + sPayload;
+                        }
                         break;
                     }
                 }
 
-                GetSfxViewShell()->libreOfficeKitViewCallback(LOK_CALLBACK_STATE_CHANGED,
-                    OUStringToOString(sPayload, RTL_TEXTENCODING_ASCII_US).getStr());
+                if (!sPayload.isEmpty())
+                    GetSfxViewShell()->libreOfficeKitViewCallback(LOK_CALLBACK_STATE_CHANGED,
+                        OUStringToOString(sPayload, RTL_TEXTENCODING_ASCII_US).getStr());
             }
 
             nWhich = aIter.NextWhich();
