cb511910f0dc4db6f739f097de279e20128d1246
Michael Stahl <Michael.Stahl@cib.de>
2019-11-12 18:57:58 +0100

    ofz#18526 sw: WW8 import: don't insert control characters
    Sanitize string before calling InsertString().
    
    This segfaults since:
    
    commit b522fc0646915d4da94df38dd249c88b28f25be7
    Date:   Tue Sep 24 18:11:45 2019 +0200
    
        sw: maintain fieldmarks in DeleteRange()/DeleteAndJoin()/ReplaceRange()
    
    Change-Id: I9ef73d924420686f6838fa21900ec57b4d25c905
    Reviewed-on: https://gerrit.libreoffice.org/81949
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>
    (cherry picked from commit 7ecda38cdaa2361e8510bf3e7206863c4936deab)
    Reviewed-on: https://gerrit.libreoffice.org/82759
    (cherry picked from commit d494a4c0ead7db481757d8d67fbce9e1b02e65df)

--- sw/source/filter/ww8/ww8par5.cxx
+++ sw/source/filter/ww8/ww8par5.cxx
@@ -35,6 +35,7 @@
 #include <svl/urihelper.hxx>
 #include <svl/zforlist.hxx>
 #include <svl/zformat.hxx>
+#include <svl/lngmisc.hxx>
 #include <sfx2/linkmgr.hxx>
 
 #include <ucbhelper/content.hxx>
@@ -1908,7 +1909,8 @@ eF_ResT SwWW8ImplReader::Read_F_Symbol( WW8FieldDesc*, OUString& rStr )
     if( aQ.isEmpty() )
         return eF_ResT::TAGIGN;                      // -> no 0-char in text
 
-    if (sal_Unicode cChar = static_cast<sal_Unicode>(aQ.toInt32()))
+    sal_Unicode const cChar = static_cast<sal_Unicode>(aQ.toInt32());
+    if (!linguistic::IsControlChar(cChar) || cChar == '\r' || cChar == '\n' || cChar == '\t')
     {
         if (!aName.isEmpty())                           // Font Name set ?
         {
@@ -2688,11 +2690,11 @@ void SwWW8ImplReader::Read_SubF_Ruby( WW8ReadFieldParams& rReadParam)
                             if ((nBegin != -1) && (nEnd != -1) && (nBegin < nEnd))
                             {
                                 sText = sPart.copy(nBegin+1,nEnd-nBegin-1);
+                                sText = sw::FilterControlChars(sText);
                             }
                         }
                     }
                 }
-
             }
             break;
         }
