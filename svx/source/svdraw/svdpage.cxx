LibreOffice/core

commit a8b1699ca9c7e8c43eff79467451fd1fcb4fde9b

speed-up shape import if shapes need z-order rearranging
setting z-order individually on each shape and broadcasting is
O(n^2) (remaining shapes also need reordering) and this is bad
bad bad for performance

Change-Id: Ic9c9137a097f6ff524192693910f221885f77cc4
Reviewed-on: https://gerrit.libreoffice.org/75055
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@cib.de>

svx/source/svdraw/svdpage.cxx
@@ -57,6 +57,8 @@
 #include <rtl/strbuf.hxx>
 #include <libxml/xmlwriter.h>
 
+#include <com/sun/star/lang/IllegalArgumentException.hpp>
+
 using namespace ::com::sun::star;
 
 class SdrObjList::WeakSdrObjectContainerType
@@ -554,6 +556,100 @@ SdrObject* SdrObjList::SetObjectOrdNum(size_t nOldObjNum, size_t nNewObjNum)
         pObj->getSdrModelFromSdrObject().SetChanged();
     }
     return pObj;
+}
+
+void SdrObjList::sort( std::vector<sal_Int32>& sortOrder)
+{
+    // no negative indexes and indexes larger than maList size are allowed
+    auto it = std::find_if( sortOrder.begin(), sortOrder.end(), [this](const sal_Int32& rIt)
+         { return ( rIt < 0 || static_cast<size_t>(rIt) >= maList.size() ); } );
+    if ( it != sortOrder.end())
+        throw css::lang::IllegalArgumentException("negative index of shape", nullptr, 1);
+
+    // no duplicates
+    std::vector<bool> aNoDuplicates(sortOrder.size(), false);
+    for (size_t i = 0; i < sortOrder.size(); ++i )
+    {
+        size_t idx =  static_cast<size_t>( sortOrder[i] );
+
+        if ( aNoDuplicates[idx] )
+            throw css::lang::IllegalArgumentException("duplicate index of shape", nullptr, 2);
+
+        aNoDuplicates[idx] = true;
+    }
+
+    // example sortOrder [2 0 1]
+    // example maList [T T S T T] ( T T = shape with textbox, S = just a shape )
+    // (shapes at positions 0 and 2 have a textbox)
+
+    std::vector<SdrObject*> aNewList(maList.size());
+    std::set<sal_Int32> aShapesWithTextbox;
+    std::vector<sal_Int32> aIncrements;
+    std::vector<sal_Int32> aDuplicates;
+
+    if ( maList.size() > 1)
+    {
+        for (size_t i = 1; i< maList.size(); ++i)
+        {
+            // if this shape is a textbox, then look at its left neighbour
+            // (shape this textbox is in)
+            // and insert the number of textboxes to the left of it
+            if (maList[i]->IsTextBox())
+              aShapesWithTextbox.insert( i - 1 - aShapesWithTextbox.size() );
+        }
+        // example aShapesWithTextbox [0 2]
+    }
+
+    aIncrements.push_back(0);
+    aDuplicates.push_back(sortOrder[0]);
+
+    // corner case: 1st shape is a textbox, add it twice
+    // otherwise sortOrder loop below will skip it
+    if (aShapesWithTextbox.count(sortOrder[0]) > 0)
+        aDuplicates.push_back(sortOrder[0]);
+
+    for (size_t i = 1; i< sortOrder.size(); ++i)
+    {
+         aDuplicates.push_back(sortOrder[i]);
+
+         if (aShapesWithTextbox.count(sortOrder[i]) > 0)
+         {
+             aIncrements.push_back(aIncrements[i-1] + 1 );
+             aDuplicates.push_back(sortOrder[i]);
+         }
+         else
+         {
+             aIncrements.push_back(aIncrements[i-1]);
+         }
+
+         // example aDuplicates [2 2 0 0 1]
+         // example aIncrements [0 1 1]
+    }
+
+    std::vector<sal_Int32> aNewSortOrder(maList.size());
+    sal_Int32 nPrev = -1;
+    for (size_t i = 0; i< aDuplicates.size(); ++i)
+    {
+        if (nPrev != aDuplicates[i])
+            aNewSortOrder[i] = aDuplicates[i] + aIncrements[aDuplicates[i]];
+        else
+            aNewSortOrder[i] = aNewSortOrder[i-1] + 1;
+
+        nPrev = aDuplicates[i];
+
+        // example aNewSortOrder [3 4 0 1 2]
+    }
+
+    if ( aNewSortOrder.size() !=  maList.size())
+        throw css::lang::IllegalArgumentException("mismatch of no. of shapes", nullptr, 0);
+
+    for (size_t i = 0; i < aNewSortOrder.size(); ++i)
+    {
+        aNewList[i] = maList[ sortOrder[i] ];
+        aNewList[i]->SetOrdNum(i);
+    }
+
+    std::swap(aNewList, maList);
 }
 
 const tools::Rectangle& SdrObjList::GetAllObjSnapRect() const

include/svx/svdpage.hxx
@@ -108,6 +108,7 @@ public:
     bool           IsObjOrdNumsDirty() const        { return mbObjOrdNumsDirty; }
     virtual void   NbcInsertObject(SdrObject* pObj, size_t nPos=SAL_MAX_SIZE);
     virtual void   InsertObject(SdrObject* pObj, size_t nPos=SAL_MAX_SIZE);
+    virtual void sort( std::vector<sal_Int32>& sortOrder );
 
     /// remove from list without delete
     virtual SdrObject* NbcRemoveObject(size_t nObjNum);

include/svx/unopage.hxx
@@ -24,12 +24,13 @@
 #include <com/sun/star/lang/XServiceInfo.hpp>
 #include <com/sun/star/drawing/XDrawPage.hpp>
 #include <com/sun/star/drawing/XShapes2.hpp>
+#include <com/sun/star/drawing/XShapes3.hpp>
 #include <com/sun/star/drawing/XShapeGrouper.hpp>
 #include <com/sun/star/lang/XUnoTunnel.hpp>
 #include <editeng/mutxhelp.hxx>
 #include <svx/svxdllapi.h>
 
-#include <cppuhelper/implbase6.hxx>
+#include <cppuhelper/implbase7.hxx>
 #include <comphelper/servicehelper.hxx>
 
 #include <memory>
@@ -50,9 +51,10 @@ enum class SdrInventor : sal_uInt32;
 #define TWIPS_TO_MM(val) ((val * 127 + 36) / 72)
 #define MM_TO_TWIPS(val) ((val * 72 + 63) / 127)
 
-class SVX_DLLPUBLIC SvxDrawPage : public ::cppu::WeakAggImplHelper6< css::drawing::XDrawPage,
+class SVX_DLLPUBLIC SvxDrawPage : public ::cppu::WeakAggImplHelper7< css::drawing::XDrawPage,
                                                css::drawing::XShapeGrouper,
                                                css::drawing::XShapes2,
+                                               css::drawing::XShapes3,
                                                css::lang::XServiceInfo,
                                                css::lang::XUnoTunnel,
                                                css::lang::XComponent>,
@@ -108,6 +110,9 @@ class SVX_DLLPUBLIC SvxDrawPage : public ::cppu::WeakAggImplHelper6< css::drawin
     // XShapes2
     virtual void SAL_CALL addTop( const css::uno::Reference< css::drawing::XShape >& xShape ) override;
     virtual void SAL_CALL addBottom( const css::uno::Reference< css::drawing::XShape >& xShape ) override;
+
+    // XShapes3
+    virtual void SAL_CALL sort( const css::uno::Sequence< sal_Int32 >& sortOrder ) override;
 
     // XElementAccess
     virtual css::uno::Type SAL_CALL getElementType() override;

offapi/UnoApi_offapi.mk
@@ -2376,6 +2376,7 @@ $(eval $(call gb_UnoApi_add_idlfiles,offapi,com/sun/star/drawing,\
 	XShapeMirror \
 	XShapes \
 	XShapes2 \
+	XShapes3 \
 	XSlidePreviewCache \
 	XSlidePreviewCacheListener \
 	XSlideRenderer \

offapi/com/sun/star/drawing/XShapes3.idl
@@ -1,0 +1,43 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef __com_sun_star_drawing_XShapes3_idl__
+#define __com_sun_star_drawing_XShapes3_idl__
+
+#include <com/sun/star/drawing/XShapes.idl>
+
+module com {  module sun {  module star { module drawing {
+
+/**
+ * Yet another XShapes interface, enables sorting shapes with
+ * some extra attention paid to shapes with textboxes and overall
+ * performance
+ *
+ * @since LibreOffice 6.4
+ */
+interface XShapes3
+{
+    /**
+     * Sort shapes according to given sort order, for perf reason
+     * just rearrange and don't broadcast
+     *
+     * @param desired order of the shapes
+     *
+     * @since LibreOffice 6.4
+     */
+
+    void sort( [in] sequence< long> sortOrder )
+        raises( com::sun::star::lang::IllegalArgumentException );
+};
+
+}; }; }; };
+
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

svx/source/unodraw/unopage.cxx
@@ -27,6 +27,7 @@
 #include <osl/mutex.hxx>
 #include <sfx2/dispatch.hxx>
 #include <comphelper/classids.hxx>
+#include <comphelper/sequence.hxx>
 #include <cppuhelper/supportsservice.hxx>
 
 #include <sfx2/objsh.hxx>
@@ -328,6 +329,12 @@ void SAL_CALL SvxDrawPage::remove( const Reference< drawing::XShape >& xShape )
     }
 
     mpModel->SetChanged();
+}
+
+void SvxDrawPage::sort( const css::uno::Sequence< sal_Int32 >& sortOrder )
+{
+    auto newOrder = comphelper::sequenceToContainer<std::vector<sal_Int32>>(sortOrder);
+    mpPage->sort(newOrder);
 }
 
 // css::container::XIndexAccess

xmloff/source/draw/shapeimport.cxx
@@ -23,6 +23,7 @@
 
 #include <com/sun/star/beans/XPropertySet.hpp>
 #include <com/sun/star/text/PositionLayoutDir.hpp>
+#include <com/sun/star/drawing/XShapes3.hpp>
 
 #include <utility>
 #include <xmloff/unointerfacetouniqueidentifiermapper.hxx>
@@ -795,8 +796,44 @@ void ShapeSortContext::popGroupAndSort()
         while( nCount );
     }
 
+    bool bSorted = std::is_sorted(maZOrderList.begin(), maZOrderList.end(),
+                       [&](const ZOrderHint& rLeft, const ZOrderHint& rRight)
+                       { return rLeft.nShould < rRight.nShould; } );
+
+    if (bSorted)
+        return; // nothin' to do
+
     // sort z-ordered shapes by nShould field
     std::sort(maZOrderList.begin(), maZOrderList.end());
+
+    uno::Reference<drawing::XShapes3> xShapes3(mxShapes, uno::UNO_QUERY);
+    if( xShapes3.is())
+    {
+        uno::Sequence<sal_Int32> aNewOrder(maZOrderList.size() + maUnsortedList.size());
+        sal_Int32 nIndex = 0;
+
+        for (ZOrderHint& rHint : maZOrderList)
+        {
+            // fill in the gaps from unordered list
+            for (vector<ZOrderHint>::iterator aIt = maUnsortedList.begin(); aIt != maUnsortedList.end() && nIndex < rHint.nShould; )
+            {
+                aNewOrder[nIndex++] = (*aIt).nIs;
+                aIt = maUnsortedList.erase(aIt);
+            }
+
+            aNewOrder[nIndex] = rHint.nIs;
+            nIndex++;
+        }
+
+        try
+        {
+            xShapes3->sort(aNewOrder);
+            maZOrderList.clear();
+            return;
+        }
+        catch (const css::lang::IllegalArgumentException& /*e*/)
+        {}
+    }
 
     // this is the current index, all shapes before that
     // index are finished

commit 623d70e4cf00eec85c10fcdcf5031524934284da

tdf#128737 svx: fix off-by-one in SdrObjList::sort()
Increment if the previous one was a text-box, not the current one.

Also add some consistency checks while at it.

(regression from a8b1699ca9c7e8c43eff79467451fd1fcb4fde9b or its
follow-ups)

Change-Id: Iab79e884f4f9cfa358630681495848f4f894506b

svx/source/svdraw/svdpage.cxx
@@ -627,6 +627,11 @@ void SdrObjList::sort( std::vector<sal_Int32>& sortOrder)
         // example aShapesWithTextbox [0 2]
     }
 
+    if (aShapesWithTextbox.size() != maList.size() - sortOrder.size())
+    {
+        throw lang::IllegalArgumentException("mismatch of no. of shapes", nullptr, 0);
+    }
+
     for (size_t i = 0; i< sortOrder.size(); ++i)
     {
 
@@ -637,17 +642,19 @@ void SdrObjList::sort( std::vector<sal_Int32>& sortOrder)
 
          // example aDuplicates [2 2 0 0 1]
     }
+    assert(aDuplicates.size() == maList.size());
 
     aIncrements.push_back(0);
     for (size_t i = 1; i< sortOrder.size(); ++i)
     {
-         if (aShapesWithTextbox.count(i))
+         if (aShapesWithTextbox.count(i - 1))
              aIncrements.push_back(aIncrements[i-1] + 1 );
          else
              aIncrements.push_back(aIncrements[i-1]);
 
          // example aIncrements [0 1 1]
     }
+    assert(aIncrements.size() == sortOrder.size());
 
     std::vector<sal_Int32> aNewSortOrder(maList.size());
     sal_Int32 nPrev = -1;
@@ -662,9 +669,18 @@ void SdrObjList::sort( std::vector<sal_Int32>& sortOrder)
 
         // example aNewSortOrder [3 4 0 1 2]
     }
+    assert(aNewSortOrder.size() == maList.size());
 
-    if ( aNewSortOrder.size() !=  maList.size())
-        throw css::lang::IllegalArgumentException("mismatch of no. of shapes", nullptr, 0);
+#ifndef NDEBUG
+    {
+        std::vector<sal_Int32> tmp(aNewSortOrder);
+        std::sort(tmp.begin(), tmp.end());
+        for (size_t i = 0; i < tmp.size(); ++i)
+        {
+            assert(size_t(tmp[i]) == i);
+        }
+    }
+#endif
 
     for (size_t i = 0; i < aNewSortOrder.size(); ++i)
     {