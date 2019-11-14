3719babdbf99855e5ac51d9a6dd0108008d94158
Caolán McNamara <caolanm@redhat.com>
2019-11-14 16:01:53 +0000

    tdf#128104 Input Engine events are delivered via Command
    so editengine needs to get those events too.
    
    Change-Id: I528065ef804f0a2dfa98e49034c86e9483bade2d

--- include/svx/weldeditview.hxx
+++ include/svx/weldeditview.hxx
@@ -51,6 +51,7 @@ protected:
     virtual bool MouseButtonDown(const MouseEvent& rMEvt) override;
     virtual bool MouseButtonUp(const MouseEvent& rMEvt) override;
     virtual bool KeyInput(const KeyEvent& rKEvt) override;
+    virtual bool Command(const CommandEvent& rCEvt) override;
     virtual void GetFocus() override;
     virtual void LoseFocus() override;
     virtual void Resize() override;
