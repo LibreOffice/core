8bccf056883b8c178b11f0db0aa0c4843ee7418a
László Németh <nemeth@numbertext.org>
2019-11-15 11:58:01 +0100

    DOCX: clean-up paragraph bottom handling of table style
    Revert of commit 17e904ed66c3caf87e658b9d3a18d7b13f4a0b52
    ("bnc#816593 DOCX filter: import paragraph spacing from table style),
    keeping only the working unit test.
    
    (cherry picked from commit 00eeb7b3f765a51f51f7911a116982fbfb83efb7)
    
    Conflicts:
    	writerfilter/source/dmapper/DomainMapperTableHandler.cxx
    
    Change-Id: I735744aadb071ef2f0d939cb637d83cfc5716fe4

--- writerfilter/source/dmapper/DomainMapperTableHandler.cxx
+++ writerfilter/source/dmapper/DomainMapperTableHandler.cxx
@@ -19,8 +19,6 @@
 #include "DomainMapperTableHandler.hxx"
 #include "DomainMapper_Impl.hxx"
 #include "StyleSheetTable.hxx"
-#include <com/sun/star/beans/XPropertyState.hpp>
-#include <com/sun/star/container/XEnumerationAccess.hpp>
 #include <com/sun/star/table/TableBorderDistances.hpp>
 #include <com/sun/star/table/TableBorder.hpp>
 #include <com/sun/star/table/BorderLine2.hpp>
@@ -1012,23 +1010,6 @@ css::uno::Sequence<css::beans::PropertyValues> DomainMapperTableHandler::endTabl
     return aRowProperties;
 }
 
-// Apply paragraph property to each paragraph within a cell.
-static void lcl_ApplyCellParaProps(uno::Reference<table::XCell> const& xCell,
-        const uno::Any& rBottomMargin)
-{
-    uno::Reference<container::XEnumerationAccess> xEnumerationAccess(xCell, uno::UNO_QUERY);
-    uno::Reference<container::XEnumeration> xEnumeration = xEnumerationAccess->createEnumeration();
-    while (xEnumeration->hasMoreElements())
-    {
-        uno::Reference<beans::XPropertySet> xParagraph(xEnumeration->nextElement(), uno::UNO_QUERY);
-        uno::Reference<beans::XPropertyState> xPropertyState(xParagraph, uno::UNO_QUERY);
-        // Don't apply in case direct formatting is already present.
-        // TODO: probably paragraph style has priority over table style here.
-        if (xPropertyState.is() && xPropertyState->getPropertyState("ParaBottomMargin") == beans::PropertyState_DEFAULT_VALUE)
-            xParagraph->setPropertyValue("ParaBottomMargin", rBottomMargin);
-    }
-}
-
 void DomainMapperTableHandler::endTable(unsigned int nestedTableLevel, bool bTableStartsAtCellStart)
 {
 #ifdef DEBUG_WRITERFILTER
@@ -1132,21 +1113,10 @@ void DomainMapperTableHandler::endTable(unsigned int nestedTableLevel, bool bTab
                 {
                     if (aTableInfo.aTableProperties[i].Name == "ParaBottomMargin")
                     {
-                        uno::Reference<table::XCellRange> xCellRange(xTable, uno::UNO_QUERY);
                         uno::Any aBottomMargin = aTableInfo.aTableProperties[i].Value;
-                        sal_Int32 nRows = aCellProperties.getLength();
 
-                    for (const auto& rParaProp : m_rDMapper_Impl.m_aPendingParaProp )
-                        rParaProp->setPropertyValue("ParaBottomMargin", aBottomMargin );
-
-                        for (sal_Int32 nRow = 0; nRow < nRows; ++nRow)
-                        {
-                            const uno::Sequence< beans::PropertyValues > aCurrentRow = aCellProperties[nRow];
-                            sal_Int32 nCells = aCurrentRow.getLength();
-                            for (sal_Int32 nCell = 0; nCell < nCells; ++nCell)
-                                lcl_ApplyCellParaProps(xCellRange->getCellByPosition(nCell, nRow), aBottomMargin);
-                        }
-                        break;
+                        for (const auto& rParaProp : m_rDMapper_Impl.m_aPendingParaProp )
+                            rParaProp->setPropertyValue("ParaBottomMargin", aBottomMargin );
                     }
                 }
             }
