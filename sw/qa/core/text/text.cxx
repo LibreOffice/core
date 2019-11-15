fb7eb9c94ea9ed9991da269f14327f88a805ff29
Miklos Vajna <vmiklos@collabora.com>
2019-11-15 12:14:09 +0100

    tdf#128736 sw ContinuousEndnotes: fix use-after-free on text frame join
    Regression from commit 61cf196631a2a846e0d3b8b83c0805cf4d1d14b2 (sw
    ContinuousEndnotes: fix moving them to the previous page, 2019-10-25),
    the problem was that the SwFootnoteFrame::mpReference was not updated on
    frame split. This meant that by the time the frame was joined,
    SwTextFrame::JoinFrame() thought that the follow has no footnotes, so
    the footnote reference was not updated, resulting in a dangling pointer.
    
    Fix the problem by going back to using bEnd for endnotes (both the Word
    compat and the normal case), this means that
    SwTextFrame::ConnectFootnote() will invoke
    SwFootnoteBossFrame::ChangeFootnoteRef(), fixing the dangling pointer.
    
    Then fix the original problem differently: similar to the in-section
    endnotes, just remove + append them, this makes sure that the endnotes
    (in the Word compat case) move to a previous page on page delete.
    
    Change-Id: Ia1b8e54b4a0b0f385c703f8e7016011c3ac57a03

---
+++ sw/qa/core/text/text.cxx
@@ -0,0 +1,51 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <swmodeltestbase.hxx>
+#include <wrtsh.hxx>
+
+static char const DATA_DIRECTORY[] = "/sw/qa/core/text/data/";
+
+/// Covers sw/source/core/text/ fixes.
+class SwCoreTextTest : public SwModelTestBase
+{
+public:
+    SwDoc* createDoc(const char* pName = nullptr);
+};
+
+SwDoc* SwCoreTextTest::createDoc(const char* pName)
+{
+    load(DATA_DIRECTORY, pName);
+
+    SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument*>(mxComponent.get());
+    CPPUNIT_ASSERT(pTextDoc);
+    return pTextDoc->GetDocShell()->GetDoc();
+}
+
+CPPUNIT_TEST_FIXTURE(SwCoreTextTest, testFootnoteConnect)
+{
+    SwDoc* pDoc = createDoc("footnote-connect.fodt");
+    SwWrtShell* pWrtShell = pDoc->GetDocShell()->GetWrtShell();
+    // Jump to the start of the next page.
+    pWrtShell->SttNxtPg();
+    // Remove the page break.
+    pWrtShell->DelLeft();
+    // Split the multi-line text frame, containing an endnote.
+    pWrtShell->DelLeft();
+    // Join the split text frame.
+    pWrtShell->DelLeft();
+    // Turn the 3 page document into a 2 page one, so the endnote frame is moved.
+    // Without the accompanying fix in place, this test would have crashed due to a use-after-free
+    // in SwFootnoteFrame::GetRef().
+    pWrtShell->DelLeft();
+}
+
+CPPUNIT_PLUGIN_IMPLEMENT();
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
