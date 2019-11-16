90308c60e85afe129eda92aa91311e38cd7376de
Andrea Gelmini <andrea.gelmini@gelma.net>
2019-11-16 23:00:07 +0100

    Fix typo
    Change-Id: I0f71c9b31456b37130a9b6ef9f47d204b9c21aa1

--- sw/qa/extras/ooxmlexport/ooxmlexport14.cxx
+++ sw/qa/extras/ooxmlexport/ooxmlexport14.cxx
@@ -145,7 +145,7 @@ DECLARE_OOXMLEXPORT_TEST(testTdf124367, "tdf124367.docx")
 
 DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testTdf128820, "tdf128820.fodt")
 {
-    // Import of exported DOCX failed because of wrong namespase used for wsp element
+    // Import of exported DOCX failed because of wrong namespace used for wsp element
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();
