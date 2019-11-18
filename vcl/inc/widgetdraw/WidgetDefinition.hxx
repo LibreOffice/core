f8ca5fb82db18a22f077a01fb1daf8056ab15ef4
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2019-11-18 22:50:46 +0100

    widget theme: add centeredTabs setting to theme definition file
    Change-Id: I714a44d40a99e0bb5ff48e3d36ded73db60af5a0
    Reviewed-on: https://gerrit.libreoffice.org/83133
    Tested-by: Jenkins
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>
    (cherry picked from commit b234743dd26e0be4be272d2d2c82f90c67e192b7)
    Reviewed-on: https://gerrit.libreoffice.org/83155
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

--- vcl/inc/widgetdraw/WidgetDefinition.hxx
+++ vcl/inc/widgetdraw/WidgetDefinition.hxx
@@ -198,6 +198,12 @@ public:
     std::vector<std::shared_ptr<WidgetDefinitionState>> maStates;
 };
 
+class VCL_DLLPUBLIC WidgetDefinitionSettings
+{
+public:
+    OString msCenteredTabs;
+};
+
 class VCL_DLLPUBLIC WidgetDefinitionStyle
 {
 public:
@@ -260,6 +266,7 @@ class VCL_DLLPUBLIC WidgetDefinition
 {
 public:
     std::shared_ptr<WidgetDefinitionStyle> mpStyle;
+    std::shared_ptr<WidgetDefinitionSettings> mpSettings;
     std::unordered_map<ControlTypeAndPart, std::shared_ptr<WidgetDefinitionPart>> maDefinitions;
     std::shared_ptr<WidgetDefinitionPart> getDefinition(ControlType eType, ControlPart ePart);
 };
--- vcl/source/gdi/WidgetDefinition.cxx
+++ vcl/source/gdi/WidgetDefinition.cxx
@@ -64,7 +64,7 @@ WidgetDefinitionPart::getStates(ControlType eType, ControlPart ePart, ControlSta
 
         if (state->msButtonValue != "any"
             && !((state->msButtonValue == "true" && eButtonValue == ButtonValue::On)
-                 || (state->msButtonValue == "false" && eButtonValue != ButtonValue::On)))
+                 || (state->msButtonValue == "false" && eButtonValue == ButtonValue::Off)))
         {
             bAdd = false;
         }
