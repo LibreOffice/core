a3736af3f07c1f83f1ecad4e755a9003ce1481c4
László Németh <nemeth@numbertext.org>
2019-11-19 21:12:28 +0100

    tdf#125300 extend AddParaSpacingToTableCells with line spacing
    Now default compatibility mode AddParaSpacingToTableCells uses
    not only paragraph bottom margin, but also proportional line
    spacing before bottom cell border, as in MSO.
    
    Note: disable testForcepoint76, because it fails again with
    its fixed layout.
    
    Change-Id: I52f6204a5efc63aac4aa332a1563ada0cbeb9618

--- sw/source/core/layout/frmtool.cxx
+++ sw/source/core/layout/frmtool.cxx
@@ -24,6 +24,7 @@
 #include <editeng/ulspitem.hxx>
 #include <editeng/boxitem.hxx>
 #include <editeng/lspcitem.hxx>
+#include <editeng/fhgtitem.hxx>
 #include <sal/log.hxx>
 
 #include <drawdoc.hxx>
@@ -2058,6 +2059,7 @@ SwBorderAttrs::SwBorderAttrs(const SwModify *pMod, const SwFrame *pConstructor)
     , m_nBottom(0)
     , m_nGetTopLine(0)
     , m_nGetBottomLine(0)
+    , m_nLineSpacing(0)
 {
     // #i96772#
     const SwTextFrame* pTextFrame = dynamic_cast<const SwTextFrame*>(pConstructor);
@@ -2069,6 +2071,9 @@ SwBorderAttrs::SwBorderAttrs(const SwModify *pMod, const SwFrame *pConstructor)
     {
         m_rLR = std::make_shared<SvxLRSpaceItem>(RES_LR_SPACE);
     }
+
+    // calculate line spacing before cell border for text frames
+    m_bLineSpacing = pTextFrame ? true : false;
 
     // Caution: The USHORTs for the cached values are not initialized by intention!
 
@@ -2411,6 +2416,18 @@ void SwBorderAttrs::GetBottomLine_( const SwFrame& _rFrame )
     m_bCachedGetBottomLine = m_bCacheGetLine;
 
     m_nGetBottomLine = nRet;
+}
+
+void SwBorderAttrs::CalcLineSpacing_()
+{
+    // tdf#125300 compatibility option AddParaSpacingToTableCells needs also line spacing
+    const SvxLineSpacingItem &rSpace = m_rAttrSet.GetLineSpacing();
+    if ( rSpace.GetInterLineSpaceRule() == SvxInterLineSpaceRule::Prop && rSpace.GetPropLineSpace() > 100 )
+    {
+        sal_Int32 nFontSize = static_cast<const SvxFontHeightItem*>(&m_rAttrSet.Get(RES_CHRATR_FONTSIZE))->GetHeight();
+        m_nLineSpacing = nFontSize * (rSpace.GetPropLineSpace() - 100) * 1.15 / 100;
+    }
+    m_bLineSpacing = false;
 }
 
 static SwModify const* GetCacheOwner(SwFrame const& rFrame)
