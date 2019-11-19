7b1664b398eb5a1fbf44427aab67375e17931651
Szymon Kłos <szymon.klos@collabora.com>
2019-11-19 17:59:17 +0200

    Revert "jsdialogs: don't recreate sidebar on event"
    This reverts commit 0c75ccd84ea64f2c69afe104a4b4ebd55b3cc215.
    
    Change-Id: I127cd0179d24af52f695c9f1ee0792b9573e6e0e
    Reviewed-on: https://gerrit.libreoffice.org/83219
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -3572,6 +3572,10 @@ static void doc_sendDialogEvent(LibreOfficeKitDocument* /*pThis*/, unsigned nWin
                     pUIWindow->execute(sClickAction, aMap);
             }
         } catch(...) {}
+
+        // force resend
+        pWindow->Hide();
+        pWindow->Show();
     }
 }
 
