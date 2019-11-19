c434fc0d181f098a179c6dc9d1506fa806d579f1
Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
2019-11-19 09:18:11 +0100

    tdf#128191 Show infobar when hyphenation info for text language is missing
    During doc loading it's too early to add infobars,
    thus add a mechanism to display infobars once view is ready
    
    Change-Id: Ie963a304d2101a5bbdd59130c354d581ff7d2e9b
    Reviewed-on: https://gerrit.libreoffice.org/84634
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>

--- sw/source/core/text/inftxt.cxx
+++ sw/source/core/text/inftxt.cxx
@@ -23,6 +23,7 @@
 #include <unotools/lingucfg.hxx>
 #include <hintids.hxx>
 #include <svl/ctloptions.hxx>
+#include <sfx2/infobar.hxx>
 #include <sfx2/printer.hxx>
 #include <sal/log.hxx>
 #include <editeng/hyphenzoneitem.hxx>
@@ -70,6 +71,8 @@
 #include <docufld.hxx>
 #include <frmfmt.hxx>
 #include <unomid.h>
+#include <docsh.hxx>
+#include <strings.hrc>
 
 using namespace ::com::sun::star;
 using namespace ::com::sun::star::linguistic2;
@@ -1459,9 +1462,15 @@ bool SwTextFormatInfo::IsHyphenate() const
 
     if (!xHyph->hasLocale(g_pBreakIt->GetLocale(eTmp)))
     {
-        // TODO: Add an infobar for this case, tdf#128191
-        SAL_WARN("sw", "missing hyphenation package for locale: "
-                           << g_pBreakIt->GetLocale(eTmp).Language);
+        SfxObjectShell* pShell = m_pFrame->GetDoc().GetDocShell();
+        if (pShell)
+        {
+            pShell->AppendInfoBarWhenReady(
+                "hyphenationmissing", SwResId(STR_HYPH_MISSING),
+                SwResId(STR_HYPH_MISSING_DETAIL)
+                    .replaceFirst("%1", g_pBreakIt->GetLocale(eTmp).Language),
+                InfobarType::WARNING);
+        }
     }
 
     return xHyph->hasLocale( g_pBreakIt->GetLocale(eTmp) );
