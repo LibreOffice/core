de5eb2092ee213a722e6d99a12b30e99a598ff96
Tor Lillqvist <tml@collabora.com>
2019-11-20 18:14:07 +0200

    We use the system clipboard on iOS and not the "LOK clipboard"
    The use of the system clipboard was implemented for iOS in vcl already
    in cp-6.0. This "LOK clipboard" thing is for different situations in
    web-based Online and not applicable for the iOS app.
    
    Change-Id: I679b5c27d308a563eadaf1e543ce8c45d763f3c6

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -932,6 +932,8 @@ ITiledRenderable* getTiledRenderable(LibreOfficeKitDocument* pThis)
     return dynamic_cast<ITiledRenderable*>(pDocument->mxComponent.get());
 }
 
+#ifndef IOS
+
 /*
  * Unfortunately clipboard creation using UNO is insanely baroque.
  * we also need to ensure that this works for the first view which
@@ -948,6 +950,9 @@ rtl::Reference<LOKClipboard> forceSetClipboardForCurrentView(LibreOfficeKitDocum
 
     return xClip;
 }
+
+#endif
+
 } // anonymous namespace
 
 LibLODocument_Impl::LibLODocument_Impl(const uno::Reference <css::lang::XComponent> &xComponent)
@@ -1033,7 +1038,9 @@ LibLODocument_Impl::LibLODocument_Impl(const uno::Reference <css::lang::XCompone
     }
     pClass = m_pDocumentClass.get();
 
+#ifndef IOS
     forceSetClipboardForCurrentView(this);
+#endif
 }
 
 LibLODocument_Impl::~LibLODocument_Impl()
@@ -1616,10 +1623,12 @@ bool CallbackFlushHandler::processWindowEvent(CallbackData& aCallbackData)
             return false;
         }
 
+#ifndef IOS
         auto xClip = forceSetClipboardForCurrentView(m_pDocument);
 
         uno::Reference<datatransfer::clipboard::XClipboard> xClipboard(xClip.get());
         pWindow->SetClipboard(xClipboard);
+#endif
     }
     else if (aAction == "size_changed")
     {
@@ -3913,6 +3922,13 @@ static int doc_setClipboard(LibreOfficeKitDocument* pThis,
                             const size_t  *pInSizes,
                             const char   **pInStreams)
 {
+#ifdef IOS
+    (void) pThis;
+    (void) nInCount;
+    (void) pInMimeTypes;
+    (void) pInSizes;
+    (void) pInStreams;
+#else
     comphelper::ProfileZone aZone("doc_paste");
 
     SolarMutexGuard aGuard;
@@ -3937,7 +3953,7 @@ static int doc_setClipboard(LibreOfficeKitDocument* pThis,
         SetLastExceptionMsg("Document doesn't support this mime type");
         return false;
     }
-
+#endif
     return true;
 }
 
@@ -4604,7 +4620,11 @@ static int doc_createViewWithOptions(LibreOfficeKitDocument* pThis,
 
     int nId = SfxLokHelper::createView();
 
+#ifdef IOS
+    (void) pThis;
+#else
     forceSetClipboardForCurrentView(pThis);
+#endif
 
     return nId;
 }
