21128d617a1495909d455b1a24895ae21a9d3794
Julien Nabet <serval2412@yahoo.fr>
2019-11-20 20:37:43 +0100

    Remove some obsolete fpicker/win part
    Change-Id: I7c602068e34b904559c0cbe3f869618c9149705a

--- fpicker/source/win32/afxres.h
+++
@@ -1,8 +0,0 @@
-/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
-#if !defined WIN32_LEAN_AND_MEAN
-# define WIN32_LEAN_AND_MEAN
-#endif
-#include <windows.h>
-#include <dlgs.h>
-
-/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
