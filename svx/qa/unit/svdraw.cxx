4074186e38388a4f8ffd3c92a7fa08d8666b0b71
Miklos Vajna <vmiklos@collabora.com>
2019-11-20 16:15:39 +0100

    svx: add rendering for semi-transparent shape text
    The color's alpha is normally lost when we roundtrip SvxColorItem's
    tools Color via TextSimplePortionPrimitive2D's basegfx::BColor.
    
    One way would be to add an extra transparency member to the primitive,
    like BackgroundColorPrimitive2D does that.
    
    However, a much easier way is to go via UnifiedTransparencePrimitive2D,
    that way we handle transparency in
    drawinglayer::impBufferDevice::paint(), rather than platform-specific
    code like CairoTextRender::DrawTextLayout() in the Linux case.
    
    Change-Id: Ie7aebe77ad9ac776dd27fc50538a5045200c8010

---
+++ svx/qa/unit/svdraw.cxx
@@ -0,0 +1,114 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <test/bootstrapfixture.hxx>
+#include <unotest/macros_test.hxx>
+#include <test/xmltesttools.hxx>
+
+#include <com/sun/star/beans/XPropertySet.hpp>
+#include <com/sun/star/drawing/XDrawPagesSupplier.hpp>
+#include <com/sun/star/drawing/XDrawPage.hpp>
+#include <com/sun/star/frame/Desktop.hpp>
+#include <com/sun/star/text/XTextRange.hpp>
+
+#include <drawinglayer/tools/primitive2dxmldump.hxx>
+#include <rtl/ustring.hxx>
+#include <svx/sdr/contact/displayinfo.hxx>
+#include <svx/sdr/contact/viewcontact.hxx>
+#include <svx/sdr/contact/viewobjectcontact.hxx>
+#include <svx/sdrpagewindow.hxx>
+#include <svx/svdpage.hxx>
+#include <svx/svdpagv.hxx>
+#include <svx/svdview.hxx>
+#include <svx/unopage.hxx>
+#include <vcl/virdev.hxx>
+
+using namespace ::com::sun::star;
+
+namespace
+{
+/// Tests for svx/source/svdraw/ code.
+class SvdrawTest : public test::BootstrapFixture, public unotest::MacrosTest, public XmlTestTools
+{
+protected:
+    uno::Reference<lang::XComponent> mxComponent;
+
+public:
+    virtual void setUp() override
+    {
+        test::BootstrapFixture::setUp();
+        mxDesktop.set(frame::Desktop::create(m_xContext));
+    }
+
+    virtual void tearDown() override
+    {
+        if (mxComponent.is())
+        {
+            mxComponent->dispose();
+        }
+        test::BootstrapFixture::tearDown();
+    }
+    uno::Reference<lang::XComponent>& getComponent() { return mxComponent; }
+};
+
+CPPUNIT_TEST_FIXTURE(SvdrawTest, testSemiTransparentText)
+{
+    // Create a new Draw document with a rectangle.
+    getComponent() = loadFromDesktop("private:factory/sdraw");
+    uno::Reference<lang::XMultiServiceFactory> xFactory(getComponent(), uno::UNO_QUERY);
+    uno::Reference<drawing::XShape> xShape(
+        xFactory->createInstance("com.sun.star.drawing.RectangleShape"), uno::UNO_QUERY);
+    xShape->setSize(awt::Size(10000, 10000));
+    xShape->setPosition(awt::Point(1000, 1000));
+
+    uno::Reference<drawing::XDrawPagesSupplier> xDrawPagesSupplier(getComponent(), uno::UNO_QUERY);
+    uno::Reference<drawing::XDrawPage> xDrawPage(xDrawPagesSupplier->getDrawPages()->getByIndex(0),
+                                                 uno::UNO_QUERY);
+    xDrawPage->add(xShape);
+
+    // Add semi-transparent text on the rectangle.
+    uno::Reference<text::XTextRange> xShapeText(xShape, uno::UNO_QUERY);
+    xShapeText->getText()->setString("hello");
+
+    uno::Reference<beans::XPropertySet> xShapeProperties(xShape, uno::UNO_QUERY);
+    xShapeProperties->setPropertyValue("CharColor", uno::makeAny(COL_RED));
+    sal_Int16 nTransparence = 75;
+    xShapeProperties->setPropertyValue("CharTransparence", uno::makeAny(nTransparence));
+
+    // Generates drawinglayer primitives for the page.
+    auto pDrawPage = dynamic_cast<SvxDrawPage*>(xDrawPage.get());
+    CPPUNIT_ASSERT(pDrawPage);
+    SdrPage* pSdrPage = pDrawPage->GetSdrPage();
+    ScopedVclPtrInstance<VirtualDevice> aVirtualDevice;
+    SdrView aSdrView(pSdrPage->getSdrModelFromSdrPage(), aVirtualDevice);
+    SdrPageView aSdrPageView(pSdrPage, aSdrView);
+    SdrPageWindow* pSdrPageWindow = aSdrPageView.GetPageWindow(0);
+    sdr::contact::ObjectContact& rObjectContactOfPageView = pSdrPageWindow->GetObjectContact();
+    const sdr::contact::ViewObjectContact& rDrawPageVOContact
+        = pSdrPage->GetViewContact().GetViewObjectContact(rObjectContactOfPageView);
+    sdr::contact::DisplayInfo aDisplayInfo;
+    drawinglayer::primitive2d::Primitive2DContainer xPrimitiveSequence
+        = rDrawPageVOContact.getPrimitive2DSequenceHierarchy(aDisplayInfo);
+
+    // Make sure the text is semi-transparent.
+    drawinglayer::tools::Primitive2dXmlDump aDumper;
+    xmlDocPtr pDocument = aDumper.dumpAndParse(xPrimitiveSequence);
+
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 1
+    // - Actual  : 0
+    // - XPath '//unifiedtransparence' number of nodes is incorrect
+    // i.e. the text was just plain red, not semi-transparent.
+    double fTransparence = getXPath(pDocument, "//unifiedtransparence", "transparence").toDouble();
+    CPPUNIT_ASSERT_EQUAL(nTransparence,
+                         static_cast<sal_Int16>(basegfx::fround(fTransparence * 100)));
+}
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
