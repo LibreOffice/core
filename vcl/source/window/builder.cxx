7c2591a5effe612ca0679448f3f39cd9b60dd98a
Caolán McNamara <caolanm@redhat.com>
2019-11-20 12:40:10 +0000

    hide help buttons when LibreOfficeKit::isActive and local help unavailable
    Change-Id: Ia263f9f7c78dbf48143678fc2f577402fb1ce998

--- vcl/source/window/builder.cxx
+++ vcl/source/window/builder.cxx
@@ -13,7 +13,9 @@
 #include <unordered_map>
 #include <com/sun/star/accessibility/AccessibleRole.hpp>
 
+#include <comphelper/lok.hxx>
 #include <i18nutil/unicode.hxx>
+#include <officecfg/Office/Common.hxx>
 #include <osl/module.hxx>
 #include <sal/log.hxx>
 #include <unotools/localedatawrapper.hxx>
@@ -795,6 +797,14 @@ VclBuilder::VclBuilder(vcl::Window* pParent, const OUString& sUIDir, const OUStr
         SAL_WARN_IF(nButtons && !bHasDefButton, "vcl.layout", "No default button defined in " << sUIFile);
     }
 #endif
+
+    const bool bHideHelp = comphelper::LibreOfficeKit::isActive() &&
+        officecfg::Office::Common::Help::HelpRootURL::get().isEmpty();
+    if (bHideHelp)
+    {
+        if (vcl::Window *pHelpButton = get<vcl::Window>("help"))
+            pHelpButton->Hide();
+    }
 }
 
 VclBuilder::~VclBuilder()
