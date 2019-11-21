8f7e6f583953c6cdc9736d5dd120fddbde98ce8b
Luboš Luňák <l.lunak@collabora.com>
2019-11-21 12:09:17 +0100

    patch Skia bug with copying data with different alpha type
    https://bugs.chromium.org/p/skia/issues/detail?id=9662
    
    Change-Id: Ic5208c2c817912cddbfae4b86e3b3647306262fb

--- external/skia/UnpackedTarball_skia.mk
+++ external/skia/UnpackedTarball_skia.mk
@@ -19,6 +19,7 @@ skia_patches := \
     make-api-visible.patch.1 \
     fix-shader-locale.patch.1 \
     no-trace-resources-on-exit.patch.1 \
+    fix-alpha-difference-copy.patch.1 \
     share-grcontext.patch.1
 
 $(eval $(call gb_UnpackedTarball_set_patchlevel,skia,1))
