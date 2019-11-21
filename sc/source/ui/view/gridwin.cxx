c407752a5242df50c3aeb1ebc76159ca26db599a
Aditya <adityasahu1511@gmail.com>
2019-11-21 11:08:53 +0530

    tdf#47490: Show overwrite warning when pasting data using "Enter"
    In Calc when a cell is copied and its value is filled/pasted to different
     cell(s) by using the "Enter" key when focus is on selected cells --
     replace warning is not shown even if the selected cells have values of
     their own inside it. The old data is simply lost. Provide a replace
     warning dialog box whenever such a case is encountered.
    
    Change-Id: I13acb424bc449e096512b5a0864ebc47a407f234

--- sc/source/ui/view/gridwin.cxx
+++ sc/source/ui/view/gridwin.cxx
@@ -3211,7 +3211,7 @@ void ScGridWindow::KeyInput(const KeyEvent& rKEvt)
     else if( rKeyCode.GetCode() == KEY_RETURN && pViewData->IsPasteMode() )
     {
         ScTabViewShell* pTabViewShell = pViewData->GetViewShell();
-        ScClipUtil::PasteFromClipboard( pViewData, pTabViewShell, false );
+        ScClipUtil::PasteFromClipboard( pViewData, pTabViewShell, true );
 
         // Clear clipboard content.
         uno::Reference<datatransfer::clipboard::XClipboard> xSystemClipboard =
