d1739c7fc0713190b2dcbcbdda831490759f331a
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2019-11-22 21:07:21 -0500

    sidebar: reset the LOKNotifier when the ViewShell changes
    The ViewShell (which implements the LOKNotifier interface)
    changes during the lifetime of the SidebarDockingWindow.
    This is extremely problematic, because we assign the
    LOKNotifier to a window, and use it to emit notifications
    to the LOKit clients. When the ViewShell changes, a window
    (the SidebarDockingWindow in this case) may end up using
    an old ViewShell instance, which may have just been destroyed.
    
    Now we properly reset the LOKNotifier to make sure we are
    using the current ViewShell at all times. This is still
    not ideal, since we may use the wrong ViewShell to send
    out notifications, but we can't control that as we are at
    the mercy of the mechanism that changes the current ViewShell.
    
    This also removes obsolete flags to control which
    document-types supported sidebar, as well as other misc
    cleanups.
    
    Change-Id: Ic208f64596a39feb7354958cf4d5e1e033d87b5b
    Reviewed-on: https://gerrit.libreoffice.org/83627
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Ashod Nakashian <ashnakash@gmail.com>

--- sfx2/source/sidebar/SidebarDockingWindow.cxx
+++ sfx2/source/sidebar/SidebarDockingWindow.cxx
@@ -91,8 +91,6 @@ SidebarDockingWindow::SidebarDockingWindow(SfxBindings* pSfxBindings, SidebarChi
     : SfxDockingWindow(pSfxBindings, &rChildWindow, pParentWindow, nBits)
     , mpSidebarController()
     , mbIsReadyToDrag(false)
-    , mbSidebarVisibleInLOK(rChildWindow.IsSidebarVisibleInLOK())
-    , mpOldViewShell(SfxViewShell::Current())
     , mpIdleNotify(new SidebarNotifyIdle(*this))
 {
     // Get the XFrame from the bindings.
@@ -115,26 +113,26 @@ SidebarDockingWindow::~SidebarDockingWindow()
 
 void SidebarDockingWindow::dispose()
 {
-    DoDispose();
-    SfxDockingWindow::dispose();
-}
-
-void SidebarDockingWindow::DoDispose()
-{
     if (comphelper::LibreOfficeKit::isActive())
-    {
-        if (const vcl::ILibreOfficeKitNotifier* pNotifier = GetLOKNotifier())
-        {
-            pNotifier->notifyWindow(GetLOKWindowId(), "close");
-            ReleaseLOKNotifier();
-        }
-    }
+        LOKClose();
 
     Reference<lang::XComponent> xComponent (static_cast<XWeak*>(mpSidebarController.get()), UNO_QUERY);
     mpSidebarController.clear();
     if (xComponent.is())
-    {
         xComponent->dispose();
+
+    SfxDockingWindow::dispose();
+}
+
+void SidebarDockingWindow::LOKClose()
+{
+    assert(comphelper::LibreOfficeKit::isActive());
+    if (const vcl::ILibreOfficeKitNotifier* pNotifier = GetLOKNotifier())
+    {
+        mpIdleNotify->Stop();
+
+        pNotifier->notifyWindow(GetLOKWindowId(), "close");
+        ReleaseLOKNotifier();
     }
 }
 
@@ -151,7 +149,9 @@ void SidebarDockingWindow::GetFocus()
 
 bool SidebarDockingWindow::Close()
 {
-    mpSidebarController->SetFloatingDeckClosed( true );
+    if (mpSidebarController.is())
+        mpSidebarController->SetFloatingDeckClosed(true);
+
     return SfxDockingWindow::Close();
 }
 
@@ -164,25 +164,16 @@ void SidebarDockingWindow::Resize()
 
 void SidebarDockingWindow::NotifyResize()
 {
-    if (comphelper::LibreOfficeKit::isActive() && SfxViewShell::Current() && mbSidebarVisibleInLOK)
+    if (comphelper::LibreOfficeKit::isActive() && mpSidebarController.is() && SfxViewShell::Current())
     {
-        // When a new view is attached, and Sidebar is created (SidebarDockingWindow is constructed),
-        // unfortunately we still have the *old* ViewShell (and ViewFrame). This happens because
-        // we get multiple NotifyResize are called while SfxBaseController::ConnectSfxFrame_Impl
-        // goes through the motions of creating and attaching a new frame/view.
-        // Problem is that once we SetLOKNotifier on a window, we can't change it. So we better
-        // set the correct one. Worse, if we set the old one, we will change the sidebar of the
-        // wrong view, messing things up badly for the users.
-        // Knowing the above, we wait until the dust settles, by observing when the ViewShell is
-        // changed from the time we were created.
-        // Note: this means we *cannot* create a sidebar post attaching a new view because the
-        // ViewShell will not change, and therefore we will never SetLOKNotifier. To avoid that
-        // we hide sidebars instead of closing (see OnMenuItemSelected in SidebarController).
-        if (mpSidebarController.is() && !GetLOKNotifier())
-            SetLOKNotifier(SfxViewShell::Current());
-
-        if (GetLOKNotifier())
+        const vcl::ILibreOfficeKitNotifier* pCurrentView = SfxViewShell::Current();
+        if (GetLOKNotifier() != pCurrentView)
         {
+            // ViewShell not yet set, or has changed. Reset it.
+            // Note GetLOKWindowId will return a new value after resetting, so we must notify clients.
+            LOKClose();
+
+            SetLOKNotifier(pCurrentView);
             mpIdleNotify->Start();
         }
     }
