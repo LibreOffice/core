4973f508a29846cbd9b58efb49aea6d2e19a41a5
Stephan Bergmann <sbergman@redhat.com>
2019-11-25 16:52:49 +0100

    Remove some redundantly user-declared copy ctors and assignment ops
    ...that trigger -Werror,-Wdeprecated-copy ("definition of implicit copy
    {constructor, assignment operator} for ... is deprecated beause it has a
    user-declared copy {assignment operator, constructor}") new in recent Clang 10
    trunk (and which apparently warns about more cases then its GCC counterpart, for
    which we already adapted the code in the past, see e.g. the various
    "-Werror=deprecated-copy (GCC trunk towards GCC 9)" commits)
    
    Change-Id: Ie37bd820e6c0c05c74e1a862bb1d4ead5fb7cc9c
    Reviewed-on: https://gerrit.libreoffice.org/83698
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/93694
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

--- sw/source/filter/ww8/ww8par.cxx
+++ sw/source/filter/ww8/ww8par.cxx
@@ -6545,11 +6545,6 @@ namespace sw
         {
         }
 
-        Position::Position(const Position &rPos)
-            : maPtNode(rPos.maPtNode), mnPtContent(rPos.mnPtContent)
-        {
-        }
-
         Position::operator SwPosition() const
         {
             SwPosition aRet(maPtNode);
