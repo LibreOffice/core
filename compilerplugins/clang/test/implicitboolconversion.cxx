28f8a26fa12c2f78696864189356db46c1cae30c
Stephan Bergmann <sbergman@redhat.com>
2019-11-25 11:17:19 +0100

    loplugin:implicitboolconversion: Filter out bool -> std::atomic<bool>
    ...as used since patch set 8 of <https://gerrit.libreoffice.org/#/c/81542/8>
    "WIP: tdf#120006 New Document converter"
    
    Change-Id: I79c2237a2e5839162272c0d49bdb4d87c9e35102
    Reviewed-on: https://gerrit.libreoffice.org/83655
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

---
+++ compilerplugins/clang/test/implicitboolconversion.cxx
@@ -0,0 +1,28 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <sal/config.h>
+
+#include <atomic>
+
+void f()
+{
+    // expected-error@+1 {{implicit conversion (IntegralCast) from 'bool' to 'int' [loplugin:implicitboolconversion]}}
+    int i = false;
+    (void)i;
+    std::atomic<bool> b = false;
+    (void)b;
+    //TODO: Emit only one diagnostic here:
+    // expected-error@+2 {{implicit conversion (ConstructorConversion) from 'bool' to 'std::atomic<int>' [loplugin:implicitboolconversion]}}
+    // expected-error-re@+1 {{implicit conversion (IntegralCast) from 'bool' to {{.+}} [loplugin:implicitboolconversion]}}
+    std::atomic<int> a = false;
+    (void)a;
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
