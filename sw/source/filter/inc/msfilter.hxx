LibreOffice/core

commit 7f2e61f884949ab27bcb7e1a02ece9a5cb4354b9

sw: WW8: fix the separator position
WW8 inserts the fieldmark at the end of the result, so separator should
be at the start; writerfilter inserts fieldmark at the end of command so
separator should be at the end.

Change-Id: I44c9811139a34f529c553dd2fd46fdaccd554732
Reviewed-on: https://gerrit.libreoffice.org/80674
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@cib.de>

sw/source/filter/ww8/ww8par5.cxx
@@ -541,7 +541,8 @@ sal_uInt16 SwWW8ImplReader::End_Field()
             SwPaM aFieldPam( m_aFieldStack.back().GetPtNode(), m_aFieldStack.back().GetPtContent(), aEndPos.nNode, aEndPos.nContent.GetIndex());
             IDocumentMarkAccess* pMarksAccess = m_rDoc.getIDocumentMarkAccess( );
             IFieldmark *pFieldmark = pMarksAccess->makeFieldBookmark(
-                        aFieldPam, m_aFieldStack.back().GetBookmarkName(), ODF_FORMTEXT );
+                        aFieldPam, m_aFieldStack.back().GetBookmarkName(), ODF_FORMTEXT,
+                        aFieldPam.Start() /*same pos as start!*/ );
             OSL_ENSURE(pFieldmark!=nullptr, "hmmm; why was the bookmark not created?");
             if (pFieldmark!=nullptr) {
                 // adapt redline positions to inserted field mark start
@@ -640,7 +641,8 @@ sal_uInt16 SwWW8ImplReader::End_Field()
                     IFieldmark* pFieldmark = pMarksAccess->makeFieldBookmark(
                                 aFieldPam,
                                 m_aFieldStack.back().GetBookmarkName(),
-                                ODF_UNHANDLED );
+                                ODF_UNHANDLED,
+                                aFieldPam.Start() /*same pos as start!*/ );
                     if ( pFieldmark )
                     {
                         // adapt redline positions to inserted field mark start

sw/qa/extras/ww8export/ww8export2.cxx
@@ -312,7 +312,7 @@ DECLARE_WW8EXPORT_TEST(testTdf104334, "tdf104334.doc")
     // This failed with a container::NoSuchElementException: STYLEREF was
     // mapped to SwChapterField, and the field result was "This is a Heading 1"
     // instead of just "1".
-    CPPUNIT_ASSERT_EQUAL(OUString("1"), getRun(getParagraph(2), 3)->getString());
+    CPPUNIT_ASSERT_EQUAL(OUString("1"), getRun(getParagraph(2), 4)->getString());
 }
 
 DECLARE_WW8EXPORT_TEST(testTdf108072, "tdf108072.doc")

commit 490d733d53f4d73244f7237dfcf5c037895ff7e8

sw: WW8 import: fix again asserts on fdo45983-1.doc export to ODT
The problem is that we now insert 2 dummy characters at the start of a
fieldmark instead of 1, so the checks in RedlineStack::MoveAttrs()
were off by 1 and we get the same invalid redline containing the start
but not the end of a fieldmark.

(regression from 7f2e61f884949ab27bcb7e1a02ece9a5cb4354b9)

Change-Id: I9752ca4c3a281539e37ddac4fe811e2f9d7374a6

sw/source/filter/inc/msfilter.hxx
@@ -299,7 +299,8 @@ namespace sw
 
         public:
             explicit RedlineStack(SwDoc &rDoc) : mrDoc(rDoc) {}
-            void MoveAttrs(const SwPosition& rPos);
+            enum class MoveAttrsMode { Default, FieldmarkInserted };
+            void MoveAttrs(const SwPosition& rPos, MoveAttrsMode eMode = MoveAttrsMode::Default);
             void open(const SwPosition& rPos, const SfxPoolItem& rAttr);
             bool close(const SwPosition& rPos, RedlineType eType);
             void close(const SwPosition& rPos, RedlineType eType,

sw/source/filter/ww8/writerhelper.cxx
@@ -738,11 +738,14 @@ namespace sw
             std::for_each(maStack.begin(), maStack.end(), SetEndIfOpen(rPos));
         }
 
-        void RedlineStack::MoveAttrs( const SwPosition& rPos )
+        void RedlineStack::MoveAttrs(const SwPosition& rPos, MoveAttrsMode const eMode)
         {
             size_t nCnt = maStack.size();
+            sal_Int32 const nInserted = eMode == MoveAttrsMode::FieldmarkInserted
+                ? 2 // CH_TXT_ATR_FIELDSTART, CH_TXT_ATR_FIELDSEP
+                : 1;
             sal_uLong nPosNd = rPos.nNode.GetIndex();
-            sal_Int32 nPosCt = rPos.nContent.GetIndex() - 1;
+            sal_Int32 nPosCt = rPos.nContent.GetIndex() - nInserted;
 
             for (size_t i=0; i < nCnt; ++i)
             {
@@ -751,12 +754,12 @@ namespace sw
                 if ((rEntry.m_aMkPos.m_nNode.GetIndex()+1 == nPosNd) &&
                     (nPosCt <= rEntry.m_aMkPos.m_nContent))
                 {
-                    rEntry.m_aMkPos.m_nContent++;
+                    rEntry.m_aMkPos.m_nContent += nInserted;
                     SAL_WARN_IF(rEntry.m_aMkPos.m_nContent > rPos.nNode.GetNodes()[nPosNd]->GetContentNode()->Len(),
                             "sw.ww8", "redline ends after end of line");
                     if (isPoint) // sigh ... important special case...
                     {
-                        rEntry.m_aPtPos.m_nContent++;
+                        rEntry.m_aPtPos.m_nContent += nInserted;
                         continue;
                     }
                 }
@@ -765,7 +768,7 @@ namespace sw
                 if ((rEntry.m_aPtPos.m_nNode.GetIndex()+1 == nPosNd) &&
                     (nPosCt < rEntry.m_aPtPos.m_nContent))
                 {
-                    rEntry.m_aPtPos.m_nContent++;
+                    rEntry.m_aPtPos.m_nContent += nInserted;
                     SAL_WARN_IF(rEntry.m_aPtPos.m_nContent > rPos.nNode.GetNodes()[nPosNd]->GetContentNode()->Len(),
                             "sw.ww8", "redline ends after end of line");
                 }

sw/source/filter/ww8/ww8par5.cxx
@@ -542,7 +542,8 @@ sal_uInt16 SwWW8ImplReader::End_Field()
             if (pFieldmark!=nullptr) {
                 // adapt redline positions to inserted field mark start
                 // dummy char (assume not necessary for end dummy char)
-                m_xRedlineStack->MoveAttrs(*aFieldPam.Start());
+                m_xRedlineStack->MoveAttrs(*aFieldPam.Start(),
+                        RedlineStack::MoveAttrsMode::FieldmarkInserted);
                 const IFieldmark::parameter_map_t& rParametersToAdd = m_aFieldStack.back().getParameters();
                 pFieldmark->GetParameters()->insert(rParametersToAdd.begin(), rParametersToAdd.end());
             }
@@ -642,7 +643,8 @@ sal_uInt16 SwWW8ImplReader::End_Field()
                     {
                         // adapt redline positions to inserted field mark start
                         // dummy char (assume not necessary for end dummy char)
-                        m_xRedlineStack->MoveAttrs(*aFieldPam.Start());
+                        m_xRedlineStack->MoveAttrs(*aFieldPam.Start(),
+                            RedlineStack::MoveAttrsMode::FieldmarkInserted);
                         const IFieldmark::parameter_map_t& rParametersToAdd = m_aFieldStack.back().getParameters();
                         pFieldmark->GetParameters()->insert(rParametersToAdd.begin(), rParametersToAdd.end());
                         OUString sFieldId = OUString::number( m_aFieldStack.back().mnFieldId );