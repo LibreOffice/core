0fc65da2431b1224159a3f92af86c67dd76644ba
Szymon Kłos <szymon.klos@collabora.com>
2019-11-26 10:47:33 +0100

    jsdialogs: share the commands updates sending code
    Change-Id: I34c1e03a8f92efe0c0dd391d16ff907919e65b00

--- sfx2/source/view/viewsh.cxx
+++ sfx2/source/view/viewsh.cxx
@@ -1995,6 +1995,11 @@ Reference< view::XRenderable > SfxViewShell::GetRenderable()
     return xRender;
 }
 
+void SfxViewShell::sendUnoStatus(const SfxItemSet* pSet) const
+{
+    SfxLokHelper::sendUnoStatus(this, pSet);
+}
+
 void SfxViewShell::notifyWindow(vcl::LOKWindowId nDialogId, const OUString& rAction, const std::vector<vcl::LOKPayloadItem>& rPayload) const
 {
     SfxLokHelper::notifyWindow(this, nDialogId, rAction, rPayload);
