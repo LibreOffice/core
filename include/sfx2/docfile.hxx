LibreOffice/core

commit 8f6f9e4217d03d874fb1ce391730a1c893ab6844

tdf#127958 crash adding 2 or more files into writer master document
clean up the memory management here, the SfxMedium wants to own the
passed-in SfxItemSet

Change-Id: I1bafa520ca7b833c4d2df7593649c3720c46e2ce
Reviewed-on: https://gerrit.libreoffice.org/80332
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

include/sfx2/docinsert.hxx
@@ -50,7 +50,7 @@ private:
 
     std::unique_ptr<sfx2::FileDialogHelper>
                             m_pFileDlg;
-    SfxItemSet*             m_pItemSet;
+    std::unique_ptr<SfxItemSet> m_xItemSet;
     std::vector<OUString>   m_pURLList;
 
     DECL_LINK(DialogClosedHdl, sfx2::FileDialogHelper*, void);

sfx2/source/doc/docinsert.cxx
@@ -79,7 +79,6 @@ DocumentInserter::DocumentInserter(weld::Window* pParent, const OUString& rFacto
     , m_sDocFactory             ( rFactory )
     , m_nDlgFlags               ( lcl_map_mode_to_flags(mode) )
     , m_nError                  ( ERRCODE_NONE )
-    , m_pItemSet                ( nullptr )
 {
 }
 
@@ -103,12 +102,15 @@ void DocumentInserter::StartExecuteModal( const Link<sfx2::FileDialogHelper*,voi
 std::unique_ptr<SfxMedium> DocumentInserter::CreateMedium(char const*const pFallbackHack)
 {
     std::unique_ptr<SfxMedium> pMedium;
-    if (!m_nError && m_pItemSet && !m_pURLList.empty())
+    if (!m_nError && m_xItemSet && !m_pURLList.empty())
     {
         DBG_ASSERT( m_pURLList.size() == 1, "DocumentInserter::CreateMedium(): invalid URL list count" );
+        std::unique_ptr<SfxItemSet> xItemSet;
+        if (m_xItemSet)
+            xItemSet = std::make_unique<SfxItemSet>(*m_xItemSet);
         pMedium.reset(new SfxMedium(
                 m_pURLList[0], SFX_STREAM_READONLY,
-                SfxGetpApp()->GetFilterMatcher().GetFilter4FilterName( m_sFilter ), std::unique_ptr<SfxItemSet>(m_pItemSet) ));
+                SfxGetpApp()->GetFilterMatcher().GetFilter4FilterName( m_sFilter ), std::move(xItemSet) ));
         pMedium->UseInteractionHandler( true );
         std::unique_ptr<SfxFilterMatcher> pMatcher;
         if ( !m_sDocFactory.isEmpty() )
@@ -139,13 +141,16 @@ std::unique_ptr<SfxMedium> DocumentInserter::CreateMedium(char const*const pFall
 SfxMediumList DocumentInserter::CreateMediumList()
 {
     SfxMediumList aMediumList;
-    if (!m_nError && m_pItemSet && !m_pURLList.empty())
+    if (!m_nError && m_xItemSet && !m_pURLList.empty())
     {
         for (auto const& url : m_pURLList)
         {
+            std::unique_ptr<SfxItemSet> xItemSet;
+            if (m_xItemSet)
+                xItemSet = std::make_unique<SfxItemSet>(*m_xItemSet);
             std::unique_ptr<SfxMedium> pMedium(new SfxMedium(
                     url, SFX_STREAM_READONLY,
-                    SfxGetpApp()->GetFilterMatcher().GetFilter4FilterName( m_sFilter ), std::unique_ptr<SfxItemSet>(m_pItemSet) ));
+                    SfxGetpApp()->GetFilterMatcher().GetFilter4FilterName( m_sFilter ), std::move(xItemSet) ));
 
             pMedium->UseInteractionHandler( true );
 
@@ -196,7 +201,7 @@ IMPL_LINK_NOARG(DocumentInserter, DialogClosedHdl, sfx2::FileDialogHelper*, void
     if ( xCtrlAccess.is() )
     {
         // always create a new itemset
-        m_pItemSet = new SfxAllItemSet( SfxGetpApp()->GetPool() );
+        m_xItemSet.reset(new SfxAllItemSet( SfxGetpApp()->GetPool() ));
 
         short nDlgType = m_pFileDlg->GetDialogType();
         bool bHasPassword = (
@@ -218,11 +223,11 @@ IMPL_LINK_NOARG(DocumentInserter, DialogClosedHdl, sfx2::FileDialogHelper*, void
                     short nRet = aPasswordDlg.run();
                     if ( RET_OK == nRet )
                     {
-                        m_pItemSet->Put( SfxStringItem( SID_PASSWORD, aPasswordDlg.GetPassword() ) );
+                        m_xItemSet->Put( SfxStringItem( SID_PASSWORD, aPasswordDlg.GetPassword() ) );
                     }
                     else
                     {
-                        DELETEZ( m_pItemSet );
+                        m_xItemSet.reset();
                         return;
                     }
                 }
@@ -237,7 +242,7 @@ IMPL_LINK_NOARG(DocumentInserter, DialogClosedHdl, sfx2::FileDialogHelper*, void
                 Any aValue = xCtrlAccess->getValue( ExtendedFilePickerElementIds::CHECKBOX_SELECTION, 0 );
                 bool bSelection = false;
                 if ( aValue >>= bSelection )
-                    m_pItemSet->Put( SfxBoolItem( SID_SELECTION, bSelection ) );
+                    m_xItemSet->Put( SfxBoolItem( SID_SELECTION, bSelection ) );
             }
             catch( const IllegalArgumentException& )
             {
@@ -248,7 +253,7 @@ IMPL_LINK_NOARG(DocumentInserter, DialogClosedHdl, sfx2::FileDialogHelper*, void
 
         // set the read-only flag. When inserting a file, this flag is always set
         if ( m_nDlgFlags & FileDialogFlags::Insert )
-            m_pItemSet->Put( SfxBoolItem( SID_DOC_READONLY, true ) );
+            m_xItemSet->Put( SfxBoolItem( SID_DOC_READONLY, true ) );
         else
         {
             if ( TemplateDescription::FILEOPEN_READONLY_VERSION == nDlgType )
@@ -258,7 +263,7 @@ IMPL_LINK_NOARG(DocumentInserter, DialogClosedHdl, sfx2::FileDialogHelper*, void
                     Any aValue = xCtrlAccess->getValue( ExtendedFilePickerElementIds::CHECKBOX_READONLY, 0 );
                     bool bReadOnly = false;
                     if ( ( aValue >>= bReadOnly ) && bReadOnly )
-                        m_pItemSet->Put( SfxBoolItem( SID_DOC_READONLY, bReadOnly ) );
+                        m_xItemSet->Put( SfxBoolItem( SID_DOC_READONLY, bReadOnly ) );
                 }
                 catch( const IllegalArgumentException& )
                 {
@@ -276,7 +281,7 @@ IMPL_LINK_NOARG(DocumentInserter, DialogClosedHdl, sfx2::FileDialogHelper*, void
                 sal_Int32 nVersion = 0;
                 if ( ( aValue >>= nVersion ) && nVersion > 0 )
                     // open a special version; 0 == current version
-                    m_pItemSet->Put( SfxInt16Item( SID_VERSION, static_cast<short>(nVersion) ) );
+                    m_xItemSet->Put( SfxInt16Item( SID_VERSION, static_cast<short>(nVersion) ) );
             }
             catch( const IllegalArgumentException& ){}
         }

commit ba98c6fd629fac4d85c9cf727f6939e0223893b6

tdf#128898 import text from file with text-decode failed
regression from

    commit 8f6f9e4217d03d874fb1ce391730a1c893ab6844
    tdf#127958 crash adding 2 or more files into writer master document

just make SfxMedium use shared_ptr, rather than the sometimes yes,
sometimes no, ownership.

Change-Id: I77559e9f766d84aca0f9dc0bd1b856c69f0f62f1

include/sfx2/docfile.hxx
@@ -69,35 +69,22 @@ class SFX2_DLLPUBLIC SfxMedium final : public SvRefBase
 public:
 
                         SfxMedium();
-                        /**
-                         * @param pSet Takes ownership
-                         */
                         SfxMedium( const OUString &rName,
                                    StreamMode nOpenMode,
                                    std::shared_ptr<const SfxFilter> pFilter = nullptr,
-                                   std::unique_ptr<SfxItemSet> pSet = nullptr );
-                        /**
-                         * @param pSet Takes ownership
-                         */
+                                   const std::shared_ptr<SfxItemSet>& pSet = nullptr );
                         SfxMedium( const OUString &rName,
                                    const OUString &rReferer,
                                    StreamMode nOpenMode,
                                    std::shared_ptr<const SfxFilter> pFilter = nullptr,
-                                   std::unique_ptr<SfxItemSet> pSet = nullptr );
-
-                        /**
-                         * @param pSet does NOT take ownership
-                         */
+                                   const std::shared_ptr<SfxItemSet>& pSet = nullptr );
                         SfxMedium( const css::uno::Reference< css::embed::XStorage >& xStorage,
-                                    const OUString& rBaseURL,
-                                    const SfxItemSet* pSet=nullptr );
-                        /**
-                         * @param pSet does NOT take ownership
-                         */
+                                   const OUString& rBaseURL,
+                                   const std::shared_ptr<SfxItemSet>& pSet = nullptr  );
                         SfxMedium( const css::uno::Reference< css::embed::XStorage >& xStorage,
-                                    const OUString& rBaseURL,
-                                    const OUString& rTypeName,
-                                    const SfxItemSet* pSet=nullptr );
+                                   const OUString& rBaseURL,
+                                   const OUString& rTypeName,
+                                   const std::shared_ptr<SfxItemSet>& pSet = nullptr );
                         SfxMedium( const css::uno::Sequence< css::beans::PropertyValue >& aArgs );
 
                         virtual ~SfxMedium() override;

include/sfx2/docinsert.hxx
@@ -50,7 +50,7 @@ private:
 
     std::unique_ptr<sfx2::FileDialogHelper>
                             m_pFileDlg;
-    std::unique_ptr<SfxItemSet> m_xItemSet;
+    std::shared_ptr<SfxItemSet> m_xItemSet;
     std::vector<OUString>   m_pURLList;
 
     DECL_LINK(DialogClosedHdl, sfx2::FileDialogHelper*, void);

sfx2/source/doc/docfile.cxx
@@ -287,7 +287,7 @@ public:
     OUString m_aLogicName;
     OUString m_aLongName;
 
-    mutable std::unique_ptr<SfxItemSet> m_pSet;
+    mutable std::shared_ptr<SfxItemSet> m_pSet;
     mutable std::unique_ptr<INetURLObject> m_pURLObj;
 
     std::shared_ptr<const SfxFilter> m_pFilter;
@@ -3210,20 +3210,20 @@ void SfxMedium::CompleteReOpen()
     pImpl->bUseInteractionHandler = bUseInteractionHandler;
 }
 
-SfxMedium::SfxMedium(const OUString &rName, StreamMode nOpenMode, std::shared_ptr<const SfxFilter> pFilter, std::unique_ptr<SfxItemSet> pInSet) :
+SfxMedium::SfxMedium(const OUString &rName, StreamMode nOpenMode, std::shared_ptr<const SfxFilter> pFilter, const std::shared_ptr<SfxItemSet>& pInSet) :
     pImpl(new SfxMedium_Impl)
 {
-    pImpl->m_pSet = std::move( pInSet );
+    pImpl->m_pSet = pInSet;
     pImpl->m_pFilter = std::move(pFilter);
     pImpl->m_aLogicName = rName;
     pImpl->m_nStorOpenMode = nOpenMode;
     Init_Impl();
 }
 
-SfxMedium::SfxMedium(const OUString &rName, const OUString &rReferer, StreamMode nOpenMode, std::shared_ptr<const SfxFilter> pFilter, std::unique_ptr<SfxItemSet> pInSet) :
+SfxMedium::SfxMedium(const OUString &rName, const OUString &rReferer, StreamMode nOpenMode, std::shared_ptr<const SfxFilter> pFilter, const std::shared_ptr<SfxItemSet>& pInSet) :
     pImpl(new SfxMedium_Impl)
 {
-    pImpl->m_pSet = std::move(pInSet);
+    pImpl->m_pSet = pInSet;
     SfxItemSet * s = GetItemSet();
     if (s->GetItem(SID_REFERER) == nullptr) {
         s->Put(SfxStringItem(SID_REFERER, rReferer));
@@ -3302,7 +3302,7 @@ SfxMedium::SfxMedium( const uno::Sequence<beans::PropertyValue>& aArgs ) :
 }
 
 
-SfxMedium::SfxMedium( const uno::Reference < embed::XStorage >& rStor, const OUString& rBaseURL, const SfxItemSet* p ) :
+SfxMedium::SfxMedium( const uno::Reference < embed::XStorage >& rStor, const OUString& rBaseURL, const std::shared_ptr<SfxItemSet>& p ) :
     pImpl(new SfxMedium_Impl)
 {
     OUString aType = SfxFilter::GetTypeFromStorage(rStor);
@@ -3320,7 +3320,7 @@ SfxMedium::SfxMedium( const uno::Reference < embed::XStorage >& rStor, const OUS
 }
 
 
-SfxMedium::SfxMedium( const uno::Reference < embed::XStorage >& rStor, const OUString& rBaseURL, const OUString &rTypeName, const SfxItemSet* p ) :
+SfxMedium::SfxMedium( const uno::Reference < embed::XStorage >& rStor, const OUString& rBaseURL, const OUString &rTypeName, const std::shared_ptr<SfxItemSet>& p ) :
     pImpl(new SfxMedium_Impl)
 {
     pImpl->m_pFilter = SfxGetpApp()->GetFilterMatcher().GetFilter4EA( rTypeName );

sfx2/source/doc/sfxbasemodel.cxx
@@ -3739,11 +3739,11 @@ void SAL_CALL SfxBaseModel::storeToStorage( const Reference< embed::XStorage >& 
     if ( !m_pData->m_pObjectShell.is() )
         throw io::IOException(); // TODO:
 
-    SfxAllItemSet aSet( m_pData->m_pObjectShell->GetPool() );
-    TransformParameters( SID_SAVEASDOC, aMediaDescriptor, aSet );
+    std::shared_ptr<SfxAllItemSet> xSet( new SfxAllItemSet(m_pData->m_pObjectShell->GetPool()) );
+    TransformParameters( SID_SAVEASDOC, aMediaDescriptor, *xSet );
 
     // TODO/LATER: maybe a special URL "private:storage" should be used
-    const SfxStringItem* pItem = aSet.GetItem<SfxStringItem>(SID_FILTER_NAME, false);
+    const SfxStringItem* pItem = xSet->GetItem<SfxStringItem>(SID_FILTER_NAME, false);
     sal_Int32 nVersion = SOFFICE_FILEFORMAT_CURRENT;
     if( pItem )
     {
@@ -3765,7 +3765,7 @@ void SAL_CALL SfxBaseModel::storeToStorage( const Reference< embed::XStorage >& 
         m_pData->m_pObjectShell->SetupStorage( xStorage, nVersion, false );
 
         // BaseURL is part of the ItemSet
-        SfxMedium aMedium( xStorage, OUString(), &aSet );
+        SfxMedium aMedium( xStorage, OUString(), xSet );
         aMedium.CanDisposeStorage_Impl( false );
         if ( aMedium.GetFilter() )
         {

sw/source/core/unocore/unocrsrhelper.cxx
@@ -1021,8 +1021,8 @@ void InsertFile(SwUnoCursor* pUnoCursor, const OUString& rURL,
         }
         else
             pMed.reset(xReadStorage.is() ?
-                new SfxMedium(xReadStorage, sBaseURL, nullptr ) :
-                new SfxMedium(sFileName, StreamMode::READ, nullptr, nullptr ));
+                new SfxMedium(xReadStorage, sBaseURL ) :
+                new SfxMedium(sFileName, StreamMode::READ ));
         if( !sBaseURL.isEmpty() )
             pMed->GetItemSet()->Put( SfxStringItem( SID_DOC_BASEURL, sBaseURL ) );
 
@@ -1044,7 +1044,7 @@ void InsertFile(SwUnoCursor* pUnoCursor, const OUString& rURL,
         {
             if( xReadStorage.is() )
             {
-                pMed.reset(new SfxMedium(xReadStorage, sBaseURL, nullptr ));
+                pMed.reset(new SfxMedium(xReadStorage, sBaseURL ));
                 pMed->SetFilter( pFilter );
             }
             else