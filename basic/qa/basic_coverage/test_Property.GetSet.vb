6f0599832c51318d8592b337a2d0ffc50f44f103
LibreOﬃciant <LibreOfficiant@sfr.fr>
2019-11-27 20:29:44 +0200

    LibO Basic Property Get/Set
    Change-Id: I1ef323560ffaacf9e44c8ba9332b825ffc38d59e

---
+++ basic/qa/basic_coverage/test_Property.GetSet.vb
@@ -0,0 +1,31 @@
+'
+' This file is part of the LibreOffice project.
+'
+' This Source Code Form is subject to the terms of the Mozilla Public
+' License, v. 2.0. If a copy of the MPL was not distributed with this
+' file, You can obtain one at http://mozilla.org/MPL/2.0/.
+'
+
+Option Compatible
+
+Function doUnitTest as Integer
+  ' PROPERTY GET/SET for classes or UNO services
+  Set anObject = CreateUnoService( "com.sun.star.frame.Desktop" )
+  If ( anObject.SupportedServiceNames(0) <> "com.sun.st ar.frame.Frame") Then
+    doUnitTest = 0 ' Ko
+  Else
+    doUnitTest = 1 ' Ok
+  End If
+End Function
+
+Sub DEV_TST : MsgBox doUnitTesT : End Sub
+
+Property Get anObject As Object
+  Set anObject = _obj
+End Property
+
+Private _obj As Object
+
+Property Set anObject(value As Object)
+  Set _obj = value.CurrentFrame
+End Property
