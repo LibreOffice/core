ede64fa86036f906b9d1f324c2098965700e6efa
Szymon Kłos <szymon.klos@collabora.com>
2019-11-27 11:30:06 +0100

    jsdiloag: export SvxColorBox to JSON
    Change-Id: I1a1296ed2981867af6dac22ba03423a3e5f3d5b5
    Reviewed-on: https://gerrit.libreoffice.org/83898
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>

--- include/svx/colorbox.hxx
+++ include/svx/colorbox.hxx
@@ -71,6 +71,8 @@ public:
     void EnsurePaletteManager();
 
     DECL_LINK(WindowEventListener, VclWindowEvent&, void);
+
+    virtual boost::property_tree::ptree DumpAsPropertyTree() override;
 };
 
 class ListBoxColorWrapper
