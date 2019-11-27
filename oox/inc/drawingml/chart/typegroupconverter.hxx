277cf1b4c3aba328c3f5d5291f4f26ef5d983ca9
Tünde Tóth <tundeth@gmail.com>
2019-11-27 10:24:50 +0100

    tdf#127811 tdf#127813 Introduce compatibility key for the data series
    order of filled net and normal area charts.
    The data series of filled net and normal area charts are drawn in
    reversed order in LibreOffice but not in Microsoft Office.
    
    Default value is true to keep current behavior.
    
    Change-Id: I07adac814597b756878d74610d028f07327f7214
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/83897
    Tested-by: Jenkins
    Reviewed-by: Balazs Varga <balazs.varga991@gmail.com>
    (cherry picked from commit cbe6c4929c9da28ce5553ce31782027d5111ed16)

--- oox/inc/drawingml/chart/typegroupconverter.hxx
+++ oox/inc/drawingml/chart/typegroupconverter.hxx
@@ -86,7 +86,6 @@ struct TypeGroupInfo
     bool                mbCategoryAxis;         /// True = X axis contains categories.
     bool                mbSwappedAxesSet;       /// True = X axis and Y axis are swapped.
     bool                mbSupportsStacking;     /// True = data points can be stacked on each other.
-    bool                mbReverseSeries;        /// True = insert unstacked series in reverse order.
     bool                mbPictureOptions;       /// True = bitmaps support options from c:pictureOptions.
 };
 
