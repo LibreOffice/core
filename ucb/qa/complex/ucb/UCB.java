dd5cea5709874c128216c08471ba57b0eae828be
Noel Grandin <noel.grandin@collabora.co.uk>
2019-11-27 09:08:37 +0200

    more UCB test more robust on my system
    ever since a system update, this test receives an
    
    IllegalIdentifierException thrown by
        ContentProvider::queryContent
    in
        ucb/source/ucp/webdav-neon/webdavprovider.cxx
    
    Given that
    (1) On a system where the test passes, no IllegalArgumentException is
    thrown
    (2) Comment in the test indicates that the test should be run with proxy
    enabled, which is not the case in our build
    (3) I am running with a system proxy enabled
    (4) the IllegalIdentifierException looks quite valid
    
    I suspect that I am the victim of a dodgy test and some new proxy
    behaviour in Fedora31.
    
    Change-Id: Id3fb3499a44b5672e4eed0a2749dc287d6e7a3cc
    Reviewed-on: https://gerrit.libreoffice.org/83853
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
    (cherry picked from commit 649a37f1f4048eb10303b6d1a46050234807f8ce)
    (cherry picked from commit dbe0b3c93fffd5093a606a3f95cde1b344cb34d7)

--- ucb/qa/complex/ucb/UCB.java
+++ ucb/qa/complex/ucb/UCB.java
@@ -117,10 +117,11 @@ public class UCB  {
 
             System.out.println("now executing open");
             executeCommand(content, "open", aArg);
-            fail("Expected 'IllegalArgumentException' was not thrown.");
+            fail("Expected exception 'IllegalArgumentException' or 'IllegalIdentifierException' was not thrown.");
         } catch (com.sun.star.lang.IllegalArgumentException ex) {
-            //TODO error message;
-            System.out.println("Correct exception thrown: " + ex.getClass().toString());
+            // correct
+        } catch (com.sun.star.ucb.IllegalIdentifierException ex) {
+            // correct
         } catch(com.sun.star.ucb.InteractiveNetworkException ex) {
             System.out.println("This Exception is correctly thrown when no Proxy in StarOffice is used.");
             System.out.println("To reproduce the bug behaviour, use a Proxy and try again.");
