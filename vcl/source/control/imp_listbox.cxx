7de9417d5f65d35227c7f80f6d587c2a56bde4e0
Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
2019-11-27 16:43:19 +0100

    tdf#129043 Correctly deliver combo box events when used with keyboard
    Change-Id: I82186f999e74be4aebd59d77666390a7d5e8ad81
    Reviewed-on: https://gerrit.libreoffice.org/83923
    Tested-by: Jenkins
    Reviewed-by: Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>

--- vcl/source/control/imp_listbox.cxx
+++ vcl/source/control/imp_listbox.cxx
@@ -483,6 +483,7 @@ ImplListBoxWindow::ImplListBoxWindow( vcl::Window* pParent, WinBits nWinStyle ) 
     mbCenter            = ( nWinStyle & WB_CENTER );
     mbSimpleMode        = ( nWinStyle & WB_SIMPLEMODE );
     mbSort              = ( nWinStyle & WB_SORT );
+    mbIsComboboxDropdown = ( nWinStyle & WB_DROPDOWN );
     mbEdgeBlending      = false;
 
     // pb: #106948# explicit mirroring for calc
@@ -1637,7 +1638,9 @@ bool ImplListBoxWindow::ProcessKeyInput( const KeyEvent& rKEvt )
         mnCurrentPos = nSelect;
         if(SelectEntries( nSelect, eLET, bShift, bCtrl, bCurPosChange))
         {
-            mbTravelSelect = true;
+            // tdf#129043 Correctly deliver events when changing values with arrow keys in combobox
+            if (mbIsComboboxDropdown && IsReallyVisible())
+                mbTravelSelect = true;
             mnSelectModifier = rKEvt.GetKeyCode().GetModifier();
             ImplCallSelect();
             mbTravelSelect = false;
