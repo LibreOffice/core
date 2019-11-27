960ae8290a23a72adecbbaefb7275afab0510c89
Caolán McNamara <caolanm@redhat.com>
2019-11-27 14:58:00 +0000

    don't put focus into unmapped windows
    defer until the color selectors are activated to grab focus, otherwise
    esc doesn't work to close a dialog under gtk3 until focus is put
    into some visible widget
    
    Change-Id: I297c2738c4103024bbefd70bbcb5d72b8429dd3e
    Reviewed-on: https://gerrit.libreoffice.org/83917
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>
    (cherry picked from commit e55a1dc163165cb79fc9113101d16ee8d3db7298)

--- include/svx/colorwindow.hxx
+++ include/svx/colorwindow.hxx
@@ -155,7 +155,6 @@ private:
     DECL_LINK(SelectPaletteHdl, weld::ComboBox&, void);
     DECL_LINK(AutoColorClickHdl, weld::Button&, void);
     DECL_LINK(OpenPickerClickHdl, weld::Button&, void);
-    DECL_LINK(FocusHdl, weld::Widget&, void);
 
     static bool SelectValueSetEntry(ColorValueSet* pColorSet, const Color& rColor);
     static NamedColor GetSelectEntryColor(SvtValueSet const * pColorSet);
@@ -176,6 +175,8 @@ public:
     void                SelectEntry(const NamedColor& rColor);
     void                SelectEntry(const Color& rColor);
     NamedColor          GetSelectEntryColor() const;
+
+    DECL_LINK(FocusHdl, weld::Widget&, void);
 
     virtual void        statusChanged( const css::frame::FeatureStateEvent& rEvent ) override;
 
