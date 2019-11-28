d402726cef13cba3592121801092fa04431bc137
Szymon Kłos <szymon.klos@collabora.com>
2019-11-28 10:19:39 +0100

    jsdialogs; send graphic updates in Calc and Impress
    Change-Id: Ia69fe214b6961a8f0387e6fc8f97a75687f5ed93

--- svx/source/tbxctrls/grafctrl.cxx
+++ svx/source/tbxctrls/grafctrl.cxx
@@ -61,6 +61,9 @@
 
 #include <com/sun/star/frame/XDispatchProvider.hpp>
 
+#include <comphelper/lok.hxx>
+#include <LibreOfficeKit/LibreOfficeKitEnums.h>
+
 using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star::frame;
 using namespace ::com::sun::star::util;
@@ -1031,6 +1034,10 @@ void SvxGrafAttrHelper::GetGrafAttrState( SfxItemSet& rSet, SdrView const & rVie
 
         nWhich = aIter.NextWhich();
     }
+
+    SfxViewShell* pViewShell = SfxViewShell::Current();
+    if (pViewShell && comphelper::LibreOfficeKit::isActive())
+        pViewShell->sendUnoStatus( &rSet );
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
