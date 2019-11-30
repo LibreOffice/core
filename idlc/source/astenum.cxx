1750552fa4ddb028be4d73ea0e53c28cee592aa3
Stephan Bergmann <sbergman@redhat.com>
2019-11-30 18:55:43 +0100

    missing include
    Change-Id: I7dc79698e642b3b971a769c57d7c7b9cfded249b

--- idlc/source/astenum.cxx
+++ idlc/source/astenum.cxx
@@ -17,6 +17,10 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
+#include <sal/config.h>
+
+#include <algorithm>
+
 #include <astenum.hxx>
 
 #include <registry/version.h>
