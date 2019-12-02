851f4b6869d4971e246103b0452e87f9b59fd2a5
Julien Nabet <serval2412@yahoo.fr>
2019-12-02 21:26:36 +0100

    Remove useless PatternPair (sc/vbainterior)
    Following 3471d83f49dcaab2cbbe18521ab1a405f15a0788
    Use initializer list instead of using several inserts (sc/vba)
    
    + 1a5850e7c4414a06e1ad5603bf1d4565d3ba3a82
    Following 3471d83f49dcaab2cbbe18521ab1a405f15a0788 (sc/vba)
    "Use initializer list instead of using several inserts (sc/vba)"
    I had forgotten to remove the initial commented code
    
    Change-Id: Idd9855e520383c17cf7564ae4646fd613fc45895

--- sc/source/ui/vba/vbainterior.cxx
+++ sc/source/ui/vba/vbainterior.cxx
@@ -31,7 +31,6 @@
 #include <document.hxx>
 
 typedef std::map< sal_Int32, sal_Int32 >  PatternMap;
-typedef std::pair< sal_Int32, sal_Int32 > PatternPair;
 using namespace ::com::sun::star;
 using namespace ::ooo::vba;
 using namespace ::ooo::vba::excel::XlPattern;
