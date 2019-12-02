a1989fc85da8ae15e9ebb227edb845572e679b2b
Miklos Vajna <vmiklos@collabora.com>
2019-12-02 21:01:19 +0100

    tdf#128429 VML import: let mso-layout-flow-alt:bottom-to-top imply vertical
    Normally layout flow is set to vertical to denote TBRL, and then
    optionally there is a layout flow alt to denote BTLR, but the bugdoc
    shows that the first may be missing.
    
    So map to BTLR even in case only the alt layout flow is found in the
    file.
    
    (cherry picked from commit ca7004569b39721c3e7247551a43d544fd3204fe)
    
    Conflicts:
    	oox/Module_oox.mk
    
    Change-Id: I06fce738fca9aedc0de90ccebda3a24e99425326
    Reviewed-on: https://gerrit.libreoffice.org/84317
    Tested-by: Jenkins
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

---
+++ oox/CppunitTest_oox_vml.mk
@@ -0,0 +1,45 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#*************************************************************************
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+#*************************************************************************
+
+$(eval $(call gb_CppunitTest_CppunitTest,oox_vml))
+
+$(eval $(call gb_CppunitTest_use_externals,oox_vml,\
+	boost_headers \
+))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,oox_vml, \
+    oox/qa/unit/vml \
+))
+
+$(eval $(call gb_CppunitTest_use_libraries,oox_vml, \
+    comphelper \
+    cppu \
+    oox \
+    sal \
+    test \
+    unotest \
+    utl \
+))
+
+$(eval $(call gb_CppunitTest_use_sdk_api,oox_vml))
+
+$(eval $(call gb_CppunitTest_use_ure,oox_vml))
+$(eval $(call gb_CppunitTest_use_vcl,oox_vml))
+
+$(eval $(call gb_CppunitTest_use_rdb,oox_vml,services))
+
+$(eval $(call gb_CppunitTest_use_custom_headers,oox_vml,\
+	officecfg/registry \
+))
+
+$(eval $(call gb_CppunitTest_use_configuration,oox_vml))
+
+# vim: set noet sw=4 ts=4:
