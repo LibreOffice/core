786911906f7377576485c7c1d0ef5c31824c3ad8
Jan Holesovsky <kendy@collabora.com>
2019-12-02 12:01:05 +0100

    lok: Fix the condition of setting the transparent background.
    Change-Id: I7b201296efe6541761896e0af1b230aecb534cdc
    Reviewed-on: https://gerrit.libreoffice.org/84211
    Reviewed-by: Jan Holesovsky <kendy@collabora.com>
    Tested-by: Jenkins

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -2995,12 +2995,13 @@ static void doc_paintTile(LibreOfficeKitDocument* pThis,
 #else
     ScopedVclPtrInstance< VirtualDevice > pDevice(DeviceFormat::DEFAULT);
 
-#if HAVE_FEATURE_ANDROID_LOK
-    // Set background to transparent by default.
-    // [Unless it is the 'old' (JNI-based) Android app - no idea why it
-    // needs avoiding this.]
-    pDevice->SetBackground(Wallpaper(COL_TRANSPARENT));
+#if defined(ANDROID) && !HAVE_FEATURE_ANDROID_LOK
+    // Don't set the transparent background in the 'old' (JNI-based) Android
+    // app - no idea why it needs avoiding this.
+    if (false)
 #endif
+    // Set background to transparent by default.
+    pDevice->SetBackground(Wallpaper(COL_TRANSPARENT));
 
     pDevice->SetOutputSizePixelScaleOffsetAndBuffer(
                 Size(nCanvasWidth, nCanvasHeight), Fraction(1.0), Point(),
