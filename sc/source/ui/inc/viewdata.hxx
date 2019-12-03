bf5425a4fecf611043729e041f9cb390d1896cca
Michael Meeks <michael.meeks@collabora.com>
2019-12-03 11:18:33 +0000

    revert optimization - 31b7dc19c32f42197c481cb7d2f44c600b755e2b
    Doing faster lookups across the calc data structrures is far more
    effective than complex caching of this kind.
    
    Change-Id: I43d3ee948ae817ec1d196acc6e5603e6acf1970c
    Reviewed-on: https://gerrit.libreoffice.org/84371
    Reviewed-by: Michael Meeks <michael.meeks@collabora.com>
    Tested-by: Michael Meeks <michael.meeks@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/94391
    Tested-by: Tor Lillqvist <tml@collabora.com>
    Reviewed-by: Tor Lillqvist <tml@collabora.com>

--- sc/source/ui/inc/viewdata.hxx
+++ sc/source/ui/inc/viewdata.hxx
@@ -234,8 +234,6 @@ private:
     SCROW           nCurY;
     SCCOL           nOldCurX;
     SCROW           nOldCurY;
-    SCCOL           nLOKOldCurX;
-    SCROW           nLOKOldCurY;
 
     ScPositionHelper aWidthHelper;
     ScPositionHelper aHeightHelper;
@@ -405,8 +403,6 @@ public:
     SCROW           GetCurYForTab( SCTAB nTabIndex ) const;
     SCCOL           GetOldCurX() const;
     SCROW           GetOldCurY() const;
-    SCCOL           GetLOKOldCurX() const                   { return pThisTab->nLOKOldCurX; }
-    SCROW           GetLOKOldCurY() const                   { return pThisTab->nLOKOldCurY; }
     long            GetLOKDocWidthPixel() const             { return pThisTab->aWidthHelper.getPosition(pThisTab->nMaxTiledCol); }
     long            GetLOKDocHeightPixel() const            { return pThisTab->aHeightHelper.getPosition(pThisTab->nMaxTiledRow); }
 
@@ -437,8 +433,6 @@ public:
     void            SetCurYForTab( SCCOL nNewCurY, SCTAB nTabIndex );
     void            SetOldCursor( SCCOL nNewX, SCROW nNewY );
     void            ResetOldCursor();
-    void            SetLOKOldCurX( SCCOL nCurX )                    { pThisTab->nLOKOldCurX = nCurX; }
-    void            SetLOKOldCurY( SCROW nCurY )                    { pThisTab->nLOKOldCurY = nCurY; }
 
     void            SetHSplitMode( ScSplitMode eMode )              { pThisTab->eHSplitMode = eMode; }
     void            SetVSplitMode( ScSplitMode eMode )              { pThisTab->eVSplitMode = eMode; }
