0b8ae8725083eb0526a262d434cc06fb3f3e7336
Dennis Francis <dennis.francis@collabora.com>
2019-12-04 15:29:06 +0530

    tdf#125662: disable parallel-zip if the memory...
    required to carry it out will cause a breach of addressable memory
    limit. This can happen in 32bit OSes, where the physical memory limit is
    more than the process addressable memory limit. For example in Win32,
    a process can address only upto 2GB.
    
    More specifically, avoid using ZipOutputEntryParallel
    which rougly needs twice the memory compared to its counterparts.
    
    Change-Id: I4d1abbf75e928188bcf806fcf1e5872b1e51b502
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/84394
    Tested-by: Jenkins
    Reviewed-by: Dennis Francis <dennis.francis@collabora.com>

--- package/source/zippackage/ZipPackageStream.cxx
+++ package/source/zippackage/ZipPackageStream.cxx
@@ -48,6 +48,7 @@
 #include <comphelper/seekableinput.hxx>
 #include <comphelper/servicehelper.hxx>
 #include <comphelper/storagehelper.hxx>
+#include <comphelper/meminfo.hxx>
 #include <cppuhelper/exc_hlp.hxx>
 #include <cppuhelper/supportsservice.hxx>
 #include <cppuhelper/typeprovider.hxx>
@@ -768,7 +769,9 @@ bool ZipPackageStream::saveChild(
                 // the full size, but it appears that at this point it usually is.
                 sal_Int64 estimatedSize = xSeek.is() ? xSeek->getLength() : xStream->available();
 
-                if (estimatedSize > 1000000)
+                // ZipOutputEntryParallel/ThreadedDeflater needs to allocate ~twice the estimatedSize.
+                // Check if we can allocate that much without breaching addressable memory limits.
+                if (estimatedSize > 1000000 && comphelper::canAlloc(2*estimatedSize))
                 {
                     // Use ThreadDeflater which will split the stream into blocks and compress
                     // them in threads, but not in background (i.e. writeStream() will block).
