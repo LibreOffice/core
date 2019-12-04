LibreOffice/core

commit d4442ac1ac9aae36dbc08fda8154d71ea0f81708

drop Graphic::GetBitmap
so that we flush out various code using Bitmap, in favour of using
BitmapEx.
This is part of the process of making Bitmap largely an internal detail
of vcl

Change-Id: Iaf2ead5e3d9960838723fb55b812b97108093d74
Reviewed-on: https://gerrit.libreoffice.org/55062
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/source/filter/jpeg/JpegWriter.cxx
@@ -190,12 +190,12 @@ bool JPEGWriter::Write( const Graphic& rGraphic )
         mxStatusIndicator->start( OUString(), 100 );
     }
 
-    Bitmap aGraphicBmp( rGraphic.GetBitmap() );
+    Bitmap aGraphicBmp( rGraphic.GetBitmapEx().GetBitmap() );
 
     if ( mbGreys )
     {
         if ( !aGraphicBmp.Convert( BmpConversion::N8BitGreys ) )
-            aGraphicBmp = rGraphic.GetBitmap();
+            aGraphicBmp = rGraphic.GetBitmapEx().GetBitmap();
     }
 
     mpReadAccess = Bitmap::ScopedReadAccess(aGraphicBmp);

cppcanvas/source/mtfrenderer/emfpimage.cxx
@@ -59,7 +59,7 @@ namespace cppcanvas
                     GraphicFilter filter;
 
                     filter.ImportGraphic(graphic, OUString(), s);
-                    SAL_INFO("cppcanvas.emf", "EMF+\tbitmap width: " << graphic.GetBitmap().GetSizePixel().Width() << " height: " << graphic.GetBitmap().GetSizePixel().Height());
+                    SAL_INFO("cppcanvas.emf", "EMF+\tbitmap width: " << graphic.GetSizePixel().Width() << " height: " << graphic.GetSizePixel().Height());
                 }
 
             }

cui/source/options/personalization.cxx
@@ -436,7 +436,7 @@ void SvxPersonalizationTabPage::LoadDefaultImages()
 
         INetURLObject aURLObj( gallery + aPreviewFile );
         aFilter.ImportGraphic( aGraphic, aURLObj );
-        Bitmap aBmp = aGraphic.GetBitmap();
+        BitmapEx aBmp = aGraphic.GetBitmapEx();
         m_vDefaultPersonaImages[nIndex]->Show();
         m_vDefaultPersonaImages[nIndex++]->SetModeImage( Image( aBmp ) );
     }
@@ -531,7 +531,7 @@ IMPL_LINK_NOARG( SvxPersonalizationTabPage, SelectInstalledPersona, ListBox&, vo
     Graphic aGraphic;
     INetURLObject aURLObj( aPreviewFile );
     aFilter.ImportGraphic( aGraphic, aURLObj );
-    Bitmap aBmp = aGraphic.GetBitmap();
+    BitmapEx aBmp = aGraphic.GetBitmapEx();
     m_pExtensionPersonaPreview->SetModeImage( Image( aBmp ) );
 }
 
@@ -781,7 +781,7 @@ void SearchAndParseThread::execute()
             // for VCL to be able to create bitmaps / do visual changes in the thread
             SolarMutexGuard aGuard;
             aFilter.ImportGraphic( aGraphic, aURLObj );
-            Bitmap aBmp = aGraphic.GetBitmap();
+            BitmapEx aBmp = aGraphic.GetBitmapEx();
 
             if( !m_bExecute )
                 return;

cui/source/tabpages/backgrnd.cxx
@@ -685,7 +685,7 @@ bool SvxBackgroundTabPage::FillItemSet( SfxItemSet* rCoreSet )
                 {
                     const Graphic* pGraphic = rOldItem.GetGraphic();
                     if (pGraphic)
-                        bModifyBrush = pGraphic->GetBitmap() != aBgdGraphic.GetBitmap();
+                        bModifyBrush = pGraphic->GetBitmapEx() != aBgdGraphic.GetBitmapEx();
                 }
                 if (bModifyBrush)
                 {
@@ -1100,7 +1100,7 @@ IMPL_LINK( SvxBackgroundTabPage, FileClickHdl_Impl, Button*, pBox, void )
 
             if ( bIsGraphicValid )
             {
-                Bitmap aBmp = aBgdGraphic.GetBitmap();
+                Bitmap aBmp = aBgdGraphic.GetBitmapEx().GetBitmap();
                 m_pPreviewWin2->NotifyChange( &aBmp );
             }
             else
@@ -1208,7 +1208,7 @@ IMPL_LINK( SvxBackgroundTabPage, LoadIdleHdl_Impl, Timer*, pIdle, void )
 
                 if ( m_pBtnPreview->IsChecked() && bIsGraphicValid )
                 {
-                    Bitmap aBmp = aBgdGraphic.GetBitmap();
+                    Bitmap aBmp = aBgdGraphic.GetBitmapEx().GetBitmap();
                     m_pPreviewWin2->NotifyChange( &aBmp );
                 }
                 else
@@ -1415,7 +1415,7 @@ void SvxBackgroundTabPage::FillControls_Impl( const SvxBrushItem& rBgdAttr,
 
         if ( m_pBtnPreview->IsChecked() && bIsGraphicValid )
         {
-            Bitmap aBmp = aBgdGraphic.GetBitmap();
+            Bitmap aBmp = aBgdGraphic.GetBitmapEx().GetBitmap();
             m_pPreviewWin2->NotifyChange( &aBmp );
         }
         else

cui/source/tabpages/grfpage.cxx
@@ -236,8 +236,7 @@ void SvxGrfCropPage::Reset( const SfxItemSet *rSet )
             aOrigSize = GetGrfOrigSize( *pGrf );
             if (pGrf->GetType() == GraphicType::Bitmap && aOrigSize.Width() && aOrigSize.Height())
             {
-                Bitmap aBitmap = pGrf->GetBitmap();
-                aOrigPixelSize = aBitmap.GetSizePixel();
+                aOrigPixelSize = pGrf->GetSizePixel();
             }
 
             if( aOrigSize.Width() && aOrigSize.Height() )
@@ -386,8 +385,7 @@ void SvxGrfCropPage::ActivatePage(const SfxItemSet& rSet)
             m_pExampleWN->SetGraphic( *pGrf );
             aOrigSize = GetGrfOrigSize( *pGrf );
             if (pGrf->GetType() == GraphicType::Bitmap && aOrigSize.Width() > 1 && aOrigSize.Height() > 1) {
-                Bitmap aBitmap = pGrf->GetBitmap();
-                aOrigPixelSize = aBitmap.GetSizePixel();
+                aOrigPixelSize = pGrf->GetSizePixel();
             }
             m_pExampleWN->SetFrameSize(aOrigSize);
             GraphicHasChanged( aOrigSize.Width() && aOrigSize.Height() );

cui/source/tabpages/numpages.cxx
@@ -2007,7 +2007,7 @@ IMPL_LINK_NOARG(SvxNumOptionsTabPage, PopupActivateHdl_Impl, MenuButton *, void)
 
                 if(GalleryExplorer::GetGraphicObj( GALLERY_THEME_BULLETS, i, &aGraphic))
                 {
-                    Bitmap aBitmap(aGraphic.GetBitmap());
+                    BitmapEx aBitmap(aGraphic.GetBitmapEx());
                     Size aSize(aBitmap.GetSizePixel());
                     if(aSize.Width() > MAX_BMP_WIDTH ||
                         aSize.Height() > MAX_BMP_HEIGHT)

cui/source/tabpages/tpline.cxx
@@ -310,7 +310,7 @@ void SvxLineTabPage::InitSymbols(MenuButton const * pButton)
 
             if(pGraphic)
             {
-                Bitmap aBitmap(pGraphic->GetBitmap());
+                BitmapEx aBitmap(pGraphic->GetBitmapEx());
                 Size aSize(aBitmap.GetSizePixel());
                 if(aSize.Width()  > MAX_BMP_WIDTH || aSize.Height() > MAX_BMP_HEIGHT)
                 {

drawinglayer/source/tools/emfpimage.cxx
@@ -38,7 +38,7 @@ namespace emfplushelper
                 // non native formats
                 GraphicFilter filter;
                 filter.ImportGraphic(graphic, OUString(), s);
-                SAL_INFO("drawinglayer", "EMF+\tbitmap width: " << graphic.GetBitmap().GetSizePixel().Width() << " height: " << graphic.GetBitmap().GetSizePixel().Height());
+                SAL_INFO("drawinglayer", "EMF+\tbitmap width: " << graphic.GetSizePixel().Width() << " height: " << graphic.GetSizePixel().Height());
             }
         }
         else if (ImageDataTypeMetafile == type)

commit e4cbe6f1aa249942a90050e33242a8e5e8da7c00

tdf#128992 Compressing PNG files with alpha channel get a black background
regression from
    commit d4442ac1ac9aae36dbc08fda8154d71ea0f81708
    Date:   Wed May 30 13:02:25 2018 +0200
    drop Graphic::GetBitmap

Change-Id: I937f89255eebc68dffc2987e628a30bc7d2eed7b

vcl/source/filter/jpeg/JpegWriter.cxx
@@ -197,7 +197,8 @@ bool JPEGWriter::Write( const Graphic& rGraphic )
         mxStatusIndicator->start( OUString(), 100 );
     }
 
-    Bitmap aGraphicBmp( rGraphic.GetBitmapEx().GetBitmap() );
+    Color replaceColor = COL_WHITE;
+    Bitmap aGraphicBmp( rGraphic.GetBitmapEx().GetBitmap(&replaceColor) );
 
     if ( mbGreys )
     {