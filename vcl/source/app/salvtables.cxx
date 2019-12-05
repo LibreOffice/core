ff01418fee7bcbda5d7295ef7c12297d4ab388e0
Caolán McNamara <caolanm@redhat.com>
2019-12-05 16:54:21 +0000

    handle unsetting Image
    Change-Id: If4a3ab94c268caeebdc42c664c568aa9004c50ad

--- vcl/source/app/salvtables.cxx
+++ vcl/source/app/salvtables.cxx
@@ -3108,7 +3108,10 @@ public:
 
     virtual void set_image(VirtualDevice* pDevice) override
     {
-        m_xImage->SetImage(createImage(*pDevice));
+        if (pDevice)
+            m_xImage->SetImage(createImage(*pDevice));
+        else
+            m_xImage->SetImage(::Image());
     }
 
     virtual void set_image(const css::uno::Reference<css::graphic::XGraphic>& rImage) override
