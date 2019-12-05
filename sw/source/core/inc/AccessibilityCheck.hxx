32c258887c11e08caadc88f8ede8a5d8ea78c0d8
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2019-12-05 22:04:35 +0100

    accessibility check base functionality
    This adds AccessibilityCheck class to writer, which perfors the
    accessibility check of the current document to make the document
    conform to PDF/UA requirements.
    
    The first check this adds is check for the "alt" (sometimes also
    called "title") to be present for graphics, OLE objects (charts)
    and shapes.
    
    Change-Id: I98dcca787099bdb17cab9291546074cdde2ae5a8

---
+++ sw/source/core/inc/AccessibilityCheck.hxx
@@ -0,0 +1,38 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#ifndef INCLUDED_SW_SOURCE_CORE_ACCESSIBILITYCHECK_HXX
+#define INCLUDED_SW_SOURCE_CORE_ACCESSIBILITYCHECK_HXX
+
+#include <svx/AccessibilityCheckDialog.hxx>
+#include <doc.hxx>
+
+class AccessibilityCheck
+{
+    SwDoc* m_pDoc;
+    std::vector<AccessibilityCheckResult> m_aAccessibilityCheckResultCollection;
+
+public:
+    AccessibilityCheck(SwDoc* pDoc)
+        : m_pDoc(pDoc)
+    {
+    }
+
+    std::vector<AccessibilityCheckResult> const& getResultCollecton()
+    {
+        return m_aAccessibilityCheckResultCollection;
+    }
+
+    void check();
+};
+
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
