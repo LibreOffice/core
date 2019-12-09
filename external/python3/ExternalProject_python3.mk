b10be5d48433076f0b7238d818020f708553e114
Michael Stahl <Michael.Stahl@cib.de>
2019-12-09 14:08:19 +0100

    python3: upgrade to release 3.7.6
    * external/python3/python-3.3.3-aix.patch.1:
      most of it doesn't apply and AIX port isn't maintained anyway so
      remove it for now
    * external/python3/ubsan.patch.0:
      apparently one of the files was removed
    * 0001-3.6-bpo-17239-Disable-external-entities-in-SAX-parse.patch.1:
      fixed upstream
    * python3-osx-avoid-new-10.13.patch.1:
      replace with simply passing ac_cv_func_utimensat=no to configure
    * external/python3/python-3.5.4-ssl.patch.1:
      project files to build OpenSSL removed upstream
    * There have been changes to how python locates OpenSSL; new variables
      OPENSSL_INCLUDES etc; it turns out that you have to pass one directory
      to --with-openssl, as the variables cannot be passed
    * libuuid.so.1 is a new dependency of the _uuid module
    * libffi.so.6 is a new dependency of the _ctypes module (the bundled
      copy of libffi for non-Darwin platforms was removed)
    * python-3.3.0-pythreadstate.patch.1:
      the PyThreadState functions have been changed such that
      CppunitTest_services asserts when there is a PyThreadAttach on top of
      PyThreadDetach on top of PyThreadAttach, i.e., 2 PyThreadState per
      thread (PyGILState_Check() fails).  Instead of patching in additional
      workarounds, change PyThreadAttach so that it re-uses an existing
      PyThreadState if one exists for the thread.
    
    Change-Id: I24c19d79b43a30709261fd9db66312b2e3872fd9
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/84765
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@cib.de>

--- external/python3/ExternalProject_python3.mk
+++ external/python3/ExternalProject_python3.mk
@@ -36,6 +36,9 @@ $(call gb_ExternalProject_get_state_target,python3,build) :
 		MAKEFLAGS= MSBuild.exe pcbuild.sln /t:Build \
 			/p:Configuration=$(if $(MSVC_USE_DEBUG_RUNTIME),Debug,Release) \
 			/p:Platform=$(if $(filter INTEL,$(CPUNAME)),Win32,x64) \
+			/p:opensslIncludeDir=$(call gb_UnpackedTarball_get_dir,openssl)/include \
+			/p:opensslOutDir=$(call gb_UnpackedTarball_get_dir,openssl)/out32dll \
+			/p:zlibDir=$(call gb_UnpackedTarball_get_dir,zlib) \
 			/maxcpucount \
 			$(if $(filter 150,$(VCVER)),/p:PlatformToolset=v141 /p:VisualStudioVersion=15.0 /ToolsVersion:15.0) \
 			$(if $(filter 160,$(VCVER)),/p:PlatformToolset=v142 /p:VisualStudioVersion=16.0 /ToolsVersion:Current) \
@@ -45,6 +48,12 @@ $(call gb_ExternalProject_get_state_target,python3,build) :
 else
 
 # --with-system-expat: this should find the one in the workdir (or system)
+
+# OPENSSL_INCLUDES OPENSSL_LDFLAGS OPENSSL_LIBS cannot be set via commandline!
+# use --with-openssl instead, which requires include/ and lib/ subdirs.
+
+# TODO: libffi has different SONAME on CentOS 6 vs. 7, so we better use
+# --without-system-ffi. However, that doesn't actually do anything on Linux :(
 
 # create a symlink "LO_lib" because the .so are in a directory with platform
 # specific name like build/lib.linux-x86_64-3.3
@@ -77,17 +86,18 @@ $(call gb_ExternalProject_get_state_target,python3,build) :
             ) \
 			--enable-framework=/@__________________________________________________OOO --with-framework-name=LibreOfficePython, \
 			--enable-shared \
+			$(if $(filter 1090 101000 101100 101200,$(MAC_OS_X_VERSION_MIN_REQUIRED)),ac_cv_func_utimensat=no) \
+		) \
+		$(if $(SYSTEM_OPENSSL)$(DISABLE_OPENSSL),,\
+			--with-openssl=$(call gb_UnpackedTarball_get_dir,openssl) \
 		) \
 		CC="$(strip $(CC) \
-			$(if $(SYSTEM_OPENSSL),,-I$(call gb_UnpackedTarball_get_dir,openssl)/include \
-				$(if $(DISABLE_OPENSSL),,-I$(call gb_UnpackedTarball_get_dir,openssl)/include)) \
 			$(if $(SYSTEM_EXPAT),,-I$(call gb_UnpackedTarball_get_dir,expat)/lib) \
 			$(if $(SYSBASE), -I$(SYSBASE)/usr/include) \
 			)" \
 		$(if $(python3_cflags),CFLAGS='$(python3_cflags)') \
 		$(if $(filter -fsanitize=%,$(CC)),LINKCC="$(CXX) -pthread") \
 		LDFLAGS="$(strip $(LDFLAGS) \
-			$(if $(SYSTEM_OPENSSL),,-L$(call gb_UnpackedTarball_get_dir,openssl)) \
 			$(if $(SYSTEM_EXPAT),,-L$(gb_StaticLibrary_WORKDIR)) \
 			$(if $(SYSTEM_ZLIB),,-L$(gb_StaticLibrary_WORKDIR)) \
 			$(if $(SYSBASE), -L$(SYSBASE)/usr/lib) \
