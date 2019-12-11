fb0bb8f2002647f77ec882fba5a86f29d7e1e1d7
Chris Sherlock <chris.sherlock79@gmail.com>
2019-12-11 14:19:10 +1100

    drawinglayer: move EMF+ enums, constants and helpers to enum helper files
    Change-Id: I6026bfbe205aea142848832259a0b8b8693e769d

--- drawinglayer/source/tools/emfphelperdata.hxx
+++ drawinglayer/source/tools/emfphelperdata.hxx
@@ -20,6 +20,7 @@
 #ifndef INCLUDED_DRAWINGLAYER_SOURCE_TOOLS_EMFPHELPERDATA_HXX
 #define INCLUDED_DRAWINGLAYER_SOURCE_TOOLS_EMFPHELPERDATA_HXX
 
+#include "emfpenums.hxx"
 #include <wmfemfhelper.hxx>
 #include <basegfx/matrix/b2dhommatrix.hxx>
 #include <tools/stream.hxx>
@@ -32,141 +33,6 @@ namespace basegfx { class B2DPolyPolygon; }
 
 namespace emfplushelper
 {
-    // EMF+ commands
-    #define EmfPlusRecordTypeHeader 0x4001
-    #define EmfPlusRecordTypeEndOfFile 0x4002
-    #define EmfPlusRecordTypeComment 0x4003
-    #define EmfPlusRecordTypeGetDC 0x4004
-    //TODO EmfPlusRecordTypeMultiFormatStart 0x4005
-    //TODO EmfPlusRecordTypeMultiFormatSection 0x4006
-    //TODO EmfPlusRecordTypeMultiFormatEnd 0x4007
-    #define EmfPlusRecordTypeObject 0x4008
-    //TODO EmfPlusRecordTypeClear 0x4009
-    #define EmfPlusRecordTypeFillRects 0x400A
-    #define EmfPlusRecordTypeDrawRects 0x400B
-    #define EmfPlusRecordTypeFillPolygon 0x400C
-    #define EmfPlusRecordTypeDrawLines 0x400D
-    #define EmfPlusRecordTypeFillEllipse 0x400E
-    #define EmfPlusRecordTypeDrawEllipse 0x400F
-    #define EmfPlusRecordTypeFillPie 0x4010
-    #define EmfPlusRecordTypeDrawPie 0x4011
-    #define EmfPlusRecordTypeDrawArc 0x4012
-    #define EmfPlusRecordTypeFillRegion 0x4013
-    #define EmfPlusRecordTypeFillPath 0x4014
-    #define EmfPlusRecordTypeDrawPath 0x4015
-    //TODO EmfPlusRecordTypeFillClosedCurve 0x4016
-    //TODO EmfPlusRecordTypeDrawClosedCurve 0x4017
-    //TODO EmfPlusRecordTypeDrawCurve 0x4018
-    #define EmfPlusRecordTypeDrawBeziers 0x4019
-    #define EmfPlusRecordTypeDrawImage 0x401A
-    #define EmfPlusRecordTypeDrawImagePoints 0x401B
-    #define EmfPlusRecordTypeDrawString 0x401C
-    #define EmfPlusRecordTypeSetRenderingOrigin 0x401D
-    #define EmfPlusRecordTypeSetAntiAliasMode 0x401E
-    #define EmfPlusRecordTypeSetTextRenderingHint 0x401F
-    #define EmfPlusRecordTypeSetTextContrast 0x4020
-    #define EmfPlusRecordTypeSetInterpolationMode 0x4021
-    #define EmfPlusRecordTypeSetPixelOffsetMode 0x4022
-    //TODO EmfPlusRecordTypeSetCompositingMode 0x4023
-    #define EmfPlusRecordTypeSetCompositingQuality 0x4024
-    #define EmfPlusRecordTypeSave 0x4025
-    #define EmfPlusRecordTypeRestore 0x4026
-    //TODO EmfPlusRecordTypeBeginContainer 0x4027
-    #define EmfPlusRecordTypeBeginContainerNoParams 0x4028
-    #define EmfPlusRecordTypeEndContainer 0x4029
-    #define EmfPlusRecordTypeSetWorldTransform 0x402A
-    #define EmfPlusRecordTypeResetWorldTransform 0x402B
-    #define EmfPlusRecordTypeMultiplyWorldTransform 0x402C
-    #define EmfPlusRecordTypeTranslateWorldTransform 0x402D
-    #define EmfPlusRecordTypeScaleWorldTransform 0x402E
-    #define EmfPlusRecordTypeRotateWorldTransform 0x402F
-    #define EmfPlusRecordTypeSetPageTransform 0x4030
-    #define EmfPlusRecordTypeResetClip 0x4031
-    #define EmfPlusRecordTypeSetClipRect 0x4032
-    #define EmfPlusRecordTypeSetClipPath 0x4033
-    #define EmfPlusRecordTypeSetClipRegion 0x4034
-    #define EmfPlusRecordTypeOffsetClip 0x4035
-    #define EmfPlusRecordTypeDrawDriverString 0x4036
-    //TODO EmfPlusRecordTypeStrokeFillPath 0x4037
-    //TODO EmfPlusRecordTypeSerializableObject 0x4038
-    //TODO EmfPlusRecordTypeSetTSGraphics 0x4039
-    //TODO EmfPlusRecordTypeSetTSClip 0x403A
-
-    // EMF+object types
-    #define EmfPlusObjectTypeBrush 0x100
-    #define EmfPlusObjectTypePen 0x200
-    #define EmfPlusObjectTypePath 0x300
-    #define EmfPlusObjectTypeRegion 0x400
-    #define EmfPlusObjectTypeImage 0x500
-    #define EmfPlusObjectTypeFont 0x600
-    #define EmfPlusObjectTypeStringFormat 0x700
-    #define EmfPlusObjectTypeImageAttributes 0x800
-    #define EmfPlusObjectTypeCustomLineCap 0x900
-
-    OUString UnitTypeToString(sal_uInt16 nType);
-
-    enum PixelOffsetMode
-    {
-        PixelOffsetModeDefault = 0x00,
-        PixelOffsetModeHighSpeed = 0x01,
-        PixelOffsetModeHighQuality = 0x02,
-        PixelOffsetModeNone = 0x03,
-        PixelOffsetModeHalf = 0x04
-    };
-
-    enum SmoothingMode
-    {
-        SmoothingModeDefault = 0x00,
-        SmoothingModeHighSpeed = 0x01,
-        SmoothingModeHighQuality = 0x02,
-        SmoothingModeNone = 0x03,
-        SmoothingModeAntiAlias8x4 = 0x04,
-        SmoothingModeAntiAlias8x8 = 0x05
-    };
-
-    enum InterpolationMode
-    {
-        InterpolationModeDefault = 0x00,
-        InterpolationModeLowQuality = 0x01,
-        InterpolationModeHighQuality = 0x02,
-        InterpolationModeBilinear = 0x03,
-        InterpolationModeBicubic = 0x04,
-        InterpolationModeNearestNeighbor = 0x05,
-        InterpolationModeHighQualityBilinear = 0x06,
-        InterpolationModeHighQualityBicubic = 0x07
-    };
-
-    enum TextRenderingHint
-    {
-        TextRenderingHintSystemDefault = 0x00,
-        TextRenderingHintSingleBitPerPixelGridFit = 0x01,
-        TextRenderingHintSingleBitPerPixel = 0x02,
-        TextRenderingHintAntialiasGridFit = 0x03,
-        TextRenderingHintAntialias = 0x04,
-        TextRenderingHintClearTypeGridFit = 0x05
-    };
-
-    enum UnitType
-    {
-        UnitTypeWorld = 0x00,
-        UnitTypeDisplay = 0x01,
-        UnitTypePixel = 0x02,
-        UnitTypePoint = 0x03,
-        UnitTypeInch = 0x04,
-        UnitTypeDocument = 0x05,
-        UnitTypeMillimeter = 0x06
-    };
-
-    enum EmfPlusCombineMode
-    {
-        EmfPlusCombineModeReplace = 0x00000000,
-        EmfPlusCombineModeIntersect = 0x00000001,
-        EmfPlusCombineModeUnion = 0x00000002,
-        EmfPlusCombineModeXOR = 0x00000003,
-        EmfPlusCombineModeExclude = 0x00000004,
-        EmfPlusCombineModeComplement = 0x00000005
-    };
-
     const char* emfTypeToName(sal_uInt16 type);
     struct EMFPObject
     {
