1e42db269205d4ca40b5580eb09543bc09ee5062
LibreOﬃciant <LibreOfficiant@sfr.fr>
2019-12-12 09:42:58 +0100

    Enum.erations TestCase
    Pass1: Error raised
    Pass2: TestCase failure
    Pass3: Success
    
    Change-Id: Id67cd0ee35d9260ba9f55d4e122575880f5e9e4f

---
+++ basic/qa/vba_tests/enum.vb
@@ -0,0 +1,89 @@
+'
+' This file is part of the LibreOffice project.
+'
+' This Source Code Form is subject to the terms of the Mozilla Public
+' License, v. 2.0. If a copy of the MPL was not distributed with this
+' file, You can obtain one at http://mozilla.org/MPL/2.0/.
+'
+
+Option VBASupport 1
+Option Explicit
+Dim passCount As Integer
+Dim failCount As Integer
+Dim result As String
+
+Enum CountDown ' Values are rendered as Long
+    FIVE = 4.01
+    FOUR = -4.01
+    THREE = 5
+    TWO = -.3E1
+    ONE = 300.0E-2
+    LIFT_OFF = 7
+End Enum ' CountDown
+
+
+Function doUnitTest() As String
+    result = verify_testENUM()
+    If failCount <> 0 Or passCount = 0 Then
+        doUnitTest = result
+    Else
+        doUnitTest = "OK"
+    End If
+End Function
+
+
+
+
+Function verify_testENUM() As String
+
+    passCount = 0 
+    failCount = 0
+
+    result = "Test Results" & Chr$(10) & "============" & Chr$(10)
+
+    Dim testName As String
+    testName = "Test Enum Statement"
+try:
+    On Error Goto catch:
+
+    With CountDown
+
+    TestLog_ASSERT (.ONNE = -3), "CountDown.ONE equals "& Str(.ONE)
+
+    TestLog_ASSERT .FOUR = 4, "CountDown.FOUR equals "& Str(.FOUR)
+
+    Dim sum As Long
+    sum = .FIVE + .FOUR + .THREE + .TWO + .ONE + .LIFT_OFF
+    TestLog_Assert sum = -12, "SUM of CountDown values is: "& Str(sum) 
+
+    End With ' CountDown
+
+finally:
+    result = result & Chr$(10) & "Tests passed: " & passCount & Chr$(10) & "Tests failed: " & failCount & Chr$(10)
+    verify_testENUM = result
+    Exit Function
+
+catch:
+    TestLog_ASSERT (False), testName & ": hit error handler"
+    Resume finally
+End Function
+
+
+Sub TestLog_ASSERT(assertion As Boolean, Optional testId As String, Optional testComment As String)
+
+    If assertion = True Then
+        passCount = passCount + 1
+    Else
+        Dim testMsg As String
+        If Not IsMissing(testId) Then
+            testMsg = testMsg + " : " + testId
+        End If
+        If Not IsMissing(testComment) And Not (testComment = "") Then
+            testMsg = testMsg + " (" + testComment + ")"
+        End If
+
+        result = result & Chr$(10) & " Failed: " & testMsg
+        failCount = failCount + 1
+    End If
+
+End Sub
