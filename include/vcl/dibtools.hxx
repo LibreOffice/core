b4fc7082872347bc9977e568942d038ac4dc2288
Chris Sherlock <chris.sherlock79@gmail.com>
2019-12-15 07:34:58 +1100

    tdf#129342 drawinglayer: read in uncompressed bitmap
    To read in an uncompressed bitmap, I need to read in the DIB body but
    NOT the v5header. Unfortunately, VCL didn't allow for this, so I have
    had to modify a few of the functions to inject my own v5 header.
    
    From here all I need to do is to read in the bitmap data to an SvStream
    and pass it to the VCL DIB creation function.
    
    During the work done on this patch I also added a few SAL_WARN logging
    statements to ImplReadDIBBody() to catch errors.
    
    Change-Id: I463acc255a7eff946dbe67028ab55d1c6b92c1cd

--- include/vcl/dibtools.hxx
+++ include/vcl/dibtools.hxx
@@ -16,18 +16,116 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
+
 #ifndef INCLUDED_VCL_DIBTOOLS_HXX
 #define INCLUDED_VCL_DIBTOOLS_HXX
 
 #include <vcl/dllapi.h>
 #include <vcl/Scanline.hxx>
 
-// predefines
-
 class SvStream;
 class BitmapEx;
 class Bitmap;
 class AlphaMask;
+
+#define DIBCOREHEADERSIZE       ( 12UL )
+#define DIBINFOHEADERSIZE       ( sizeof(DIBInfoHeader) )
+#define DIBV5HEADERSIZE         ( sizeof(DIBV5Header) )
+
+// - DIBInfoHeader and DIBV5Header
+
+typedef sal_Int32 FXPT2DOT30;
+
+struct CIEXYZ
+{
+    FXPT2DOT30      aXyzX;
+    FXPT2DOT30      aXyzY;
+    FXPT2DOT30      aXyzZ;
+
+    CIEXYZ()
+    :   aXyzX(0),
+        aXyzY(0),
+        aXyzZ(0)
+    {}
+};
+
+struct CIEXYZTriple
+{
+    CIEXYZ          aXyzRed;
+    CIEXYZ          aXyzGreen;
+    CIEXYZ          aXyzBlue;
+
+    CIEXYZTriple()
+    :   aXyzRed(),
+        aXyzGreen(),
+        aXyzBlue()
+    {}
+};
+
+struct DIBInfoHeader
+{
+    sal_uInt32      nSize;
+    sal_Int32       nWidth;
+    sal_Int32       nHeight;
+    sal_uInt16      nPlanes;
+    sal_uInt16      nBitCount;
+    sal_uInt32      nCompression;
+    sal_uInt32      nSizeImage;
+    sal_Int32       nXPelsPerMeter;
+    sal_Int32       nYPelsPerMeter;
+    sal_uInt32      nColsUsed;
+    sal_uInt32      nColsImportant;
+
+    DIBInfoHeader()
+    :   nSize(0),
+        nWidth(0),
+        nHeight(0),
+        nPlanes(0),
+        nBitCount(0),
+        nCompression(0),
+        nSizeImage(0),
+        nXPelsPerMeter(0),
+        nYPelsPerMeter(0),
+        nColsUsed(0),
+        nColsImportant(0)
+    {}
+};
+
+struct DIBV5Header : public DIBInfoHeader
+{
+    sal_uInt32      nV5RedMask;
+    sal_uInt32      nV5GreenMask;
+    sal_uInt32      nV5BlueMask;
+    sal_uInt32      nV5AlphaMask;
+    sal_uInt32      nV5CSType;
+    CIEXYZTriple    aV5Endpoints;
+    sal_uInt32      nV5GammaRed;
+    sal_uInt32      nV5GammaGreen;
+    sal_uInt32      nV5GammaBlue;
+    sal_uInt32      nV5Intent;
+    sal_uInt32      nV5ProfileData;
+    sal_uInt32      nV5ProfileSize;
+    sal_uInt32      nV5Reserved;
+
+    DIBV5Header()
+    :   DIBInfoHeader(),
+        nV5RedMask(0),
+        nV5GreenMask(0),
+        nV5BlueMask(0),
+        nV5AlphaMask(0),
+        nV5CSType(0),
+        aV5Endpoints(),
+        nV5GammaRed(0),
+        nV5GammaGreen(0),
+        nV5GammaBlue(0),
+        nV5Intent(0),
+        nV5ProfileData(0),
+        nV5ProfileSize(0),
+        nV5Reserved(0)
+    {}
+
+    DIBV5Header& operator=(const DIBV5Header&) = default;
+};
 
 // - Compression defines
 
@@ -52,7 +150,8 @@ bool VCL_DLLPUBLIC ReadDIBBitmapEx(
 bool VCL_DLLPUBLIC ReadDIBV5(
     Bitmap& rTarget,
     AlphaMask& rTargetAlpha,
-    SvStream& rIStm);
+    SvStream& rIStm,
+    DIBV5Header* pHeader=nullptr);
 
 bool VCL_DLLPUBLIC ReadRawDIB(
     BitmapEx& rTarget,
