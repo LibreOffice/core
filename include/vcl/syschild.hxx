f6e054bd47b84abc2235594bc9677e4b3d3ffa3e
Caolán McNamara <caolanm@redhat.com>
2019-12-17 17:25:57 +0000

    add a SystemChildWindow::GetOptimalSize override
    Change-Id: I812d996e78a6d627fda6612307ca4cb2f111b6a9
    Reviewed-on: https://gerrit.libreoffice.org/85325
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>

--- include/vcl/syschild.hxx
+++ include/vcl/syschild.hxx
@@ -41,6 +41,7 @@ public:
                             // create a SystemChildWindow using the given SystemWindowData
     explicit                SystemChildWindow( vcl::Window* pParent, WinBits nStyle, SystemWindowData *pData, bool bShow = true );
     virtual                 ~SystemChildWindow() override;
+    virtual Size            GetOptimalSize() const override;
     virtual void            dispose() override;
 
     virtual const SystemEnvData* GetSystemData() const override;
