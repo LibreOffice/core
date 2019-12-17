3a50a49465af168e299667e6d8492799e982f591
Andrea Gelmini <andrea.gelmini@gelma.net>
2019-12-17 17:21:09 +0100

    Fix typo
    Change-Id: I64a13c892a5ae8b2c7e9614212be60d9a4300632

--- slideshow/source/engine/effectrewinder.hxx
+++ slideshow/source/engine/effectrewinder.hxx
@@ -97,7 +97,7 @@ public:
     */
     void skipAllMainSequenceEffects();
 
-    //FIXME: That is an opengl issue(it doesn't allow to animate somea animations), remove that function when opengl fixed.
+    //FIXME: That is an opengl issue(it doesn't allow to animate some animations), remove that function when opengl fixed.
     static bool hasBlockedAnimation( const css::uno::Reference<css::animations::XAnimationNode>& xNode);
 
 private:
