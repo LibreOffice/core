e60c4940e4b5274d88cf20f456848d8f92948450
Henry Castro <hcastro@collabora.com>
2019-12-18 16:09:36 -0400

    lok: ensure assign one parent LOK notifier
    The frame window is unique that are parents of different view shells
    (windows)
    
    Change-Id: I0f5e76c0c0d9844c2bd642354ac93d368dc2af38
    Reviewed-on: https://gerrit.libreoffice.org/85434
    Tested-by: Jenkins
    Reviewed-by: Henry Castro <hcastro@collabora.com>

--- sfx2/source/view/viewsh.cxx
+++ sfx2/source/view/viewsh.cxx
@@ -1098,7 +1098,7 @@ SfxViewShell::SfxViewShell
     if (comphelper::LibreOfficeKit::isActive())
     {
         vcl::Window* pFrameWin = pViewFrame->GetWindow().GetFrameWindow();
-        if (pFrameWin)
+        if (pFrameWin && !pFrameWin->GetLOKNotifier())
             pFrameWin->SetLOKNotifier(this, true);
     }
 }
