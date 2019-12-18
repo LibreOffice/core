2b432a522e9aa90226431500e547ee4aad62b695
Kemal Ayhan <kemalayhan013@gmail.com>
2019-12-18 20:00:00 +0300

    tdf#124176: Use pragma once instead of include guards
    Change-Id: I8f8641d41a3c394c494d0e5f6cd5a06949e22fa6

--- cui/source/options/connpooloptions.hxx
+++ cui/source/options/connpooloptions.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_CUI_SOURCE_OPTIONS_CONNPOOLOPTIONS_HXX
-#define INCLUDED_CUI_SOURCE_OPTIONS_CONNPOOLOPTIONS_HXX
+#pragma once
 
 #include <sfx2/tabdlg.hxx>
 
@@ -68,7 +67,5 @@ namespace offapp
     };
 
 } // namespace offapp
-
-#endif // INCLUDED_CUI_SOURCE_OPTIONS_CONNPOOLOPTIONS_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
