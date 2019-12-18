7b1f3135de3211b0b750571ecdcd9dcab752b544
Batuhan Görkem Benzer <batuhan1167@gmail.com>
2019-12-18 12:00:05 -0500

    tdf#124176: Use pragma once instead of header guard
    Change-Id: I720508aa4bfd77f15b5d713e4d948b06af0be4c2
    Reviewed-on: https://gerrit.libreoffice.org/85414
    Tested-by: Jenkins
    Reviewed-by: Muhammet Kara <muhammet.kara@collabora.com>

--- cui/source/options/sdbcdriverenum.hxx
+++ cui/source/options/sdbcdriverenum.hxx
@@ -17,9 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_CUI_SOURCE_OPTIONS_SDBCDRIVERENUM_HXX
-#define INCLUDED_CUI_SOURCE_OPTIONS_SDBCDRIVERENUM_HXX
-
+#pragma once
 
 #include <rtl/ustring.hxx>
 
@@ -52,9 +50,5 @@ namespace offapp
 
 
 }   // namespace offapp
-
-
-#endif // INCLUDED_CUI_SOURCE_OPTIONS_SDBCDRIVERENUM_HXX
-
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
