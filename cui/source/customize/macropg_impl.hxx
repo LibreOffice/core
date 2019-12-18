ecd490b6e3e612df117e82675e63237685e3ca36
Muhammet Kara <muhammet.kara@collabora.com>
2019-12-18 15:57:12 +0300

    tdf#124176: Use pragma once instead of include guards
    Change-Id: I3df73c321efa7fa3e8a87608c3c7189f7d839ac8
    Reviewed-on: https://gerrit.libreoffice.org/85384
    Tested-by: Jenkins
    Reviewed-by: Muhammet Kara <muhammet.kara@collabora.com>

--- cui/source/customize/macropg_impl.hxx
+++ cui/source/customize/macropg_impl.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_CUI_SOURCE_CUSTOMIZE_MACROPG_IMPL_HXX
-#define INCLUDED_CUI_SOURCE_CUSTOMIZE_MACROPG_IMPL_HXX
+#pragma once
 
 #include <svl/itemset.hxx>
 #include <vcl/weld.hxx>
@@ -52,7 +51,5 @@ public:
 
     const OUString& getURL() const { return maURL; }
 };
-
-#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
