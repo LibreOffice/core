3cd2990b4148452e074fd7e58a9f5ff2576363ea
Tolunay Dündar <dndr.dev@protonmail.com>
2019-12-18 20:00:02 +0300

    tdf#124176: Use pragma once instead of include guards
    Change-Id: I47be8a0488b7ff7ce176f10aaf90b8be6913cc8a

--- cui/source/options/optaboutconfig.hxx
+++ cui/source/options/optaboutconfig.hxx
@@ -7,8 +7,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-#ifndef INCLUDED_CUI_SOURCE_OPTIONS_OPTABOUTCONFIG_HXX
-#define INCLUDED_CUI_SOURCE_OPTIONS_OPTABOUTCONFIG_HXX
+#pragma once
 
 #include <com/sun/star/container/XNameAccess.hpp>
 
@@ -92,7 +91,5 @@ public:
         return m_xEDValue->get_text();
     }
 };
-
-#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
