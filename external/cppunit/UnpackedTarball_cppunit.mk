ffe9ff2cf63112ac04a695740a5637f25c16abe8
Markus Mohrhard <markus.mohrhard@googlemail.com>
2019-12-20 01:28:54 +0800

    update cppunit to 1.15.1
    Change-Id: Ic27a77addb409a8d63ea44136a8d2410ee40c4d2
    Reviewed-on: https://gerrit.libreoffice.org/85539
    Reviewed-by: Markus Mohrhard <markus.mohrhard@googlemail.com>
    Tested-by: Markus Mohrhard <markus.mohrhard@googlemail.com>

--- external/cppunit/UnpackedTarball_cppunit.mk
+++ external/cppunit/UnpackedTarball_cppunit.mk
@@ -13,18 +13,12 @@ $(eval $(call gb_UnpackedTarball_set_tarball,cppunit,$(CPPUNIT_TARBALL),,cppunit
 
 $(eval $(call gb_UnpackedTarball_update_autoconf_configs,cppunit))
 
-# gcc9.patch.0 addressed upstream with <https://gerrit.libreoffice.org/58690> "Avoid GCC 9
-#  -Wdeprecated-copy":
-# 0001-Unconditionally-use-C-11-noreturn.patch.1 upstream at <https://gerrit.libreoffice.org/67247>
-#  "Unconditionally use C++11 [[noreturn]]"
 $(eval $(call gb_UnpackedTarball_add_patches,cppunit,\
 	external/cppunit/windows.patch \
 	external/cppunit/unix.patch \
 	external/cppunit/CPPUNIT_PLUGIN_EXPORT.patch.0 \
 	external/cppunit/enable-win32-debug.patch \
 	external/cppunit/rtti.patch.0 \
-	external/cppunit/gcc9.patch.0 \
-	external/cppunit/0001-Unconditionally-use-C-11-noreturn.patch.1 \
 ))
 ifeq ($(DISABLE_DYNLOADING),TRUE)
 $(eval $(call gb_UnpackedTarball_add_patches,cppunit,\
