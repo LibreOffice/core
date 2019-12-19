d016e052ddf30649ad9b729b59134ce1e90a0263
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2019-12-19 20:55:16 +0100

    pdf: extract XMP metadata writing and use XmlWriter
    Instead of writing XMP metadata with a string buffer, change to
    use XmlWriter instead. Extract XMP metadata writing into its own
    class vcl::pdf::XmpMetadata.
    
    This also needs a change to the XmlWriter to not write a classic
    XML header: '<?xml version="1.0" ... ?>'
    
    Change-Id: I95ea0e7ba58e7c43a0c707bf9c676994210ff104
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/85908
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>
    Tested-by: Tomaž Vajngerl <quikee@gmail.com>

---
+++ vcl/inc/pdf/XmpMetadata.hxx
@@ -0,0 +1,47 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#ifndef INCLUDED_VCL_INC_PDF_XMPMETADATA_HXX
+#define INCLUDED_VCL_INC_PDF_XMPMETADATA_HXX
+
+#include <vcl/dllapi.h>
+#include <rtl/string.hxx>
+#include <tools/stream.hxx>
+#include <memory>
+
+namespace vcl::pdf
+{
+class XmpMetadata
+{
+private:
+    bool mbWritten;
+    std::unique_ptr<SvMemoryStream> mpMemoryStream;
+
+public:
+    OString msTitle;
+    OString msAuthor;
+    OString msSubject;
+    OString msProducer;
+    OString msKeywords;
+    sal_Int32 mnPDF_A;
+
+public:
+    XmpMetadata();
+    sal_uInt64 getSize();
+    const void* getData();
+
+private:
+    void write();
+};
+}
+
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
