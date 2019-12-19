102c09ebc8d1fc135d48e695474ea3c07d4d4647
Caolán McNamara <caolanm@redhat.com>
2019-12-19 09:33:31 +0000

    focus-in/out should return a value
    Change-Id: I82df9b24a5f08830eda1e511d6a7d45a1edcc08e

--- vcl/unx/gtk3/gtk3gtkinst.cxx
+++ vcl/unx/gtk3/gtk3gtkinst.cxx
@@ -11465,10 +11465,11 @@ private:
         }
     }
 
-    static void signalEntryFocusIn(GtkWidget*, GdkEvent*, gpointer widget)
+    static gboolean signalEntryFocusIn(GtkWidget*, GdkEvent*, gpointer widget)
     {
         GtkInstanceComboBox* pThis = static_cast<GtkInstanceComboBox*>(widget);
         pThis->signal_entry_focus_in();
+        return false;
     }
 
     void signal_entry_focus_in()
@@ -11476,10 +11477,11 @@ private:
         signal_focus_in();
     }
 
-    static void signalEntryFocusOut(GtkWidget*, GdkEvent*, gpointer widget)
+    static gboolean signalEntryFocusOut(GtkWidget*, GdkEvent*, gpointer widget)
     {
         GtkInstanceComboBox* pThis = static_cast<GtkInstanceComboBox*>(widget);
         pThis->signal_entry_focus_out();
+        return false;
     }
 
     void signal_entry_focus_out()
