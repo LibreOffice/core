7272c67a47d2d036ab20d88505661bb8274800af
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2019-12-19 20:55:16 +0100

    pdf: extract XMP metadata writing and use XmlWriter
    Instead of writing XMP metadata with a string buffer, change to
    use XmlWriter instead. Extract XMP metadata writing into its own
    class vcl::pdf::XmpMetadata.
    
    This also needs a change to the XmlWriter to not write a classic
    XML header: '<?xml version="1.0" ... ?>'
    
    Change-Id: I95ea0e7ba58e7c43a0c707bf9c676994210ff104

--- include/tools/XmlWriter.hxx
+++ include/tools/XmlWriter.hxx
@@ -40,7 +40,7 @@ public:
 
     ~XmlWriter();
 
-    bool startDocument(sal_Int32 nIndent = 2);
+    bool startDocument(sal_Int32 nIndent = 2, bool bWriteXmlHeader = true);
     void endDocument();
 
     void startElement(const OString& sName);
