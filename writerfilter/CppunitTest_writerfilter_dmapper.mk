7aeb13fae1feae82390281e92a2ce2e9d757f39b
Miklos Vajna <vmiklos@collabora.com>
2019-12-19 17:54:08 +0100

    DOCX table import: fix interaction of 1-cell rows and "inside" vertical borders
    The interesting part of the bugdoc was:
    
    - table style wants visible borders
    - table direct formatting clears left and right borders
    - 1st row of the table has 1 cell (2 cells in fact, but they are
      merged)
    
    Fix the "inside" vertical border handling, so that the first cell gets
    these vertical borders as a right border only in case there are multiple
    cells.
    
    (cherry picked from commit fd92740a86ab8e71e77d947d1d7dabc51a8d0794)
    
    Change-Id: Id847109ecfa95d1745abe62ddf36c4936b730855

---
+++ writerfilter/CppunitTest_writerfilter_dmapper.mk
@@ -0,0 +1,48 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#*************************************************************************
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+#*************************************************************************
+
+$(eval $(call gb_CppunitTest_CppunitTest,writerfilter_dmapper))
+
+$(eval $(call gb_CppunitTest_use_externals,writerfilter_dmapper,\
+	boost_headers \
+))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,writerfilter_dmapper, \
+    writerfilter/qa/cppunittests/dmapper/DomainMapperTableHandler \
+))
+
+$(eval $(call gb_CppunitTest_use_libraries,writerfilter_dmapper, \
+    comphelper \
+    cppu \
+    oox \
+    sal \
+    test \
+    unotest \
+))
+
+$(eval $(call gb_CppunitTest_use_sdk_api,writerfilter_dmapper))
+
+$(eval $(call gb_CppunitTest_use_ure,writerfilter_dmapper))
+$(eval $(call gb_CppunitTest_use_vcl,writerfilter_dmapper))
+
+$(eval $(call gb_CppunitTest_use_rdb,writerfilter_dmapper,services))
+
+$(eval $(call gb_CppunitTest_use_custom_headers,writerfilter_dmapper,\
+	officecfg/registry \
+))
+
+$(eval $(call gb_CppunitTest_use_configuration,writerfilter_dmapper))
+
+# we need to explicitly depend on library writerfilter because it is not implied
+# by a link relation
+$(call gb_CppunitTest_get_target,writerfilter_dmapper) : $(call gb_Library_get_target,writerfilter)
+
+# vim: set noet sw=4 ts=4:
