LibreOffice/core

commit f2e3655255db4032738849cd4b77ce67a6e2c984

Avoid -fsanitize=signed-integer-overflow
...later on during loading of (a local copy of)
<https://wiki.documentfoundation.org/images/5/50/BH40-BaseHandbook.odt> at

> sw/inc/swrect.hxx:249:48: runtime error: signed integer overflow: 1080100 + 9223372036853697509 cannot be represented in type 'long'
>  SwRect::Bottom() const sw/inc/swrect.hxx:249:48
>  SwTextFly::CalcMinBottom() const sw/source/core/text/txtfly.cxx:996:31
>  SwTextFly::GetMinBottom() const sw/source/core/inc/txtfly.hxx:339:45
>  SwTextFormatter::CalcBottomLine() const sw/source/core/text/itrform2.cxx:1917:20
>  SwTextFrame::FormatAdjust(SwTextFormatter&, WidowsAndOrphans&, int, bool) sw/source/core/text/frmform.cxx:1092:26
>  SwTextFrame::_Format(SwTextFormatter&, SwTextFormatInfo&, bool) sw/source/core/text/frmform.cxx:1552:13
>  SwTextFrame::_Format(OutputDevice*, SwParaPortion*) sw/source/core/text/frmform.cxx:1662:5
>  SwTextFrame::Format(OutputDevice*, SwBorderAttrs const*) sw/source/core/text/frmform.cxx:1809:17
>  SwContentFrame::MakeAll(OutputDevice*) sw/source/core/layout/calcmove.cxx:1391:17
>  SwFrame::PrepareMake(OutputDevice*) sw/source/core/layout/calcmove.cxx:307:21
>  SwFrame::Calc(OutputDevice*) const sw/source/core/layout/trvlfrm.cxx:1798:9
>  SwObjectFormatterTextFrame::FormatAnchorFrameAndItsPrevs(SwTextFrame&) sw/source/core/layout/objectformattertxtfrm.cxx:753:9
>  SwFlyAtContentFrame::MakeAll(OutputDevice*) sw/source/core/layout/flycnt.cxx:419:21
>  SwFrame::PrepareMake(OutputDevice*) sw/source/core/layout/calcmove.cxx:341:5
>  SwFlyFrame::Calc(OutputDevice*) const sw/source/core/layout/fly.cxx:2665:9
>  SwLayAction::FormatLayoutFly(SwFlyFrame*) sw/source/core/layout/layact.cxx:1428:9
>  SwObjectFormatter::_FormatObj(SwAnchoredObject&) sw/source/core/layout/objectformatter.cxx:331:17
>  SwObjectFormatterTextFrame::DoFormatObj(SwAnchoredObject&, bool) sw/source/core/layout/objectformattertxtfrm.cxx:134:9

sw/source/core/text/frmform.cxx
@@ -808,7 +808,7 @@ bool SwTextFrame::CalcPreps()
                 }
                 else
                 {
-                    SwTwips nTmp  = LONG_MAX - (Frame().Top()+10000);
+                    SwTwips nTmp  = TWIPS_MAX/2 - (Frame().Top()+10000);
                     SwTwips nDiff = nTmp - Frame().Height();
                     Frame().Height( nTmp );
                     Prt().Height( Prt().Height() + nDiff );

commit 51ec03b7b199b4b83778b04de8145cc97fa55bb6

sw: fix widow loop with as-char flys in text formatting
The document has a paragraph with 4 as-character anchored flys depicting
Zirbenholz; due to their size and an additional fly that is anchored at
the paragraph, there are 3 lines that do not fit onto a single page.

This situation causes a loop that proceeds like this:

text frame 80 is the follow of text frame 21.
when formatting 80:
    the 1 line violates the widow rule (>=2) and PREP_WIDOWS is sent to
        21, invalidating its FrameAreaSize
    80 validates its FrameAreaSize
when formatting 21:
    PREP_WIDOWS_ORPHANS sent to 21
    CalcPreps() sees IsPrepWidows() and sets a huge height and calls
        SetWidow(true)
    SwTextFrame::WouldFit() sees IsWidow() true and resets it false
    SwTextFrame::WouldFit() sees IsWidow() false and a huge but
        insufficiently huge height
    21 validates its FrameAreaSize
    CalcPreps() sees IsPrepAdjust()
    FindBreak() calls TruncLines() and because of as-char fly
        invalidates FrameAreaSize of 80

The loop is most easily reproduced by printing via the API; it's
possible to get a loop when loading the document in the UI, but
typically the UI remains responsive even though the layout never
finishes.

As it happens, before commit ee299664940139f6f9543592ece3b3c0210b59f4
"SalInstance::DoYield: Don't drop SolarMutex when accessing user event
queue" the loop on printing via API was broken by releasing SolarMutex;
the result, however, was incorrect, with the last line of Zirbenholz
that should be on the second page missing in the PDF.

This loop is presumably a regression from commit
f2e3655255db4032738849cd4b77ce67a6e2c984 "Avoid
-fsanitize=signed-integer-overflow", which changed a magic number in
SwTextFrame::CalcPreps(), but didn't adapt the poorly documented
corresponding magic numbers in SwTextFrame::WouldFit(); in LO 5.1.6.2
the CPU is idle after loading the document.

Change-Id: Ib6563c21edb68945c14a61b51ba34f0ee3f2544a

sw/source/core/inc/txtfrm.hxx
@@ -1022,6 +1022,8 @@ public:
 };
 
 
+const SwTwips WIDOW_MAGIC = (SAL_MAX_INT32 - 1)/2;
+
 } // namespace sw
 
 #endif

sw/source/core/text/frmform.cxx
@@ -847,7 +847,7 @@ bool SwTextFrame::CalcPreps()
                     // the range of 'long', while the value (SAL_MAX_INT32 - 1)/2 (which matches the
                     // old value on platforms where 'long' is 'sal_Int32') is empirically shown to
                     // be large enough in practice even on platforms where 'long' is 'sal_Int64':
-                    SwTwips nTmp  = (SAL_MAX_INT32 - 1)/2 - (getFrameArea().Top()+10000);
+                    SwTwips const nTmp = sw::WIDOW_MAGIC - (getFrameArea().Top()+10000);
                     SwTwips nDiff = nTmp - getFrameArea().Height();
 
                     {

sw/source/core/text/txtfrm.cxx
@@ -3260,9 +3260,9 @@ bool SwTextFrame::WouldFit( SwTwips &rMaxHeight, bool &bSplit, bool bTst )
     // Because the Orphan flag only exists for a short moment, we also check
     // whether the Framesize is set to very huge by CalcPreps, in order to
     // force a MoveFwd
-    if( IsWidow() || ( aRectFnSet.IsVert() ?
-                       ( 0 == getFrameArea().Left() ) :
-                       ( LONG_MAX - 20000 < getFrameArea().Bottom() ) ) )
+    if (IsWidow() || (aRectFnSet.IsVert()
+                        ? (0 == getFrameArea().Left())
+                        : (sw::WIDOW_MAGIC - 20000 < getFrameArea().Bottom())))
     {
         SetWidow(false);
         if ( GetFollow() )
@@ -3271,7 +3271,7 @@ bool SwTextFrame::WouldFit( SwTwips &rMaxHeight, bool &bSplit, bool bTst )
             // whether there's a Follow with a real height at all.
             // Else (e.g. for newly created SctFrames) we ignore the IsWidow() and
             // still check if we can find enough room
-            if( ( ( ! aRectFnSet.IsVert() && LONG_MAX - 20000 >= getFrameArea().Bottom() ) ||
+            if (((!aRectFnSet.IsVert() && getFrameArea().Bottom() <= sw::WIDOW_MAGIC - 20000) ||
                   (   aRectFnSet.IsVert() && 0 < getFrameArea().Left() ) ) &&
                   ( GetFollow()->IsVertical() ?
                     !GetFollow()->getFrameArea().Width() :