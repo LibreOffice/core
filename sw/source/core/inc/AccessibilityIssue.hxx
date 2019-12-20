5dce2b2eb3e5bfac14327b87c4b7e082d81e00b2
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2019-12-20 09:57:23 +0100

    acc. check: add writer specific accessibility issue
    This adds sw::AccessibilityIssue and refactors the existing
    code to work with it.
    
    In addition adds AccessibilityIssueCollection which holds
    the vector of results.
    
    Change-Id: Ide0fdbdba337bbf8c2fd18770895fb63a22528b6

---
+++ sw/source/core/inc/AccessibilityIssue.hxx
@@ -0,0 +1,31 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#ifndef INCLUDED_SW_SOURCE_CORE_ACCESSIBILITYISSUE_HXX
+#define INCLUDED_SW_SOURCE_CORE_ACCESSIBILITYISSUE_HXX
+
+#include <svx/AccessibilityIssue.hxx>
+
+namespace sw
+{
+class SW_DLLPUBLIC AccessibilityIssue final : public svx::AccessibilityIssue
+{
+public:
+    AccessibilityIssue(svx::AccessibilityIssueID eIssueID = svx::AccessibilityIssueID::UNSPECIFIED)
+        : svx::AccessibilityIssue(eIssueID)
+    {
+    }
+};
+
+} // end sw namespace
+
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
