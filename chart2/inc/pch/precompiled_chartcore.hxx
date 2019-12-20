575ccd69ed285c1597a3fe152880ddd6ea7da54f
Caolán McNamara <caolanm@redhat.com>
2019-12-20 15:52:45 +0000

    update chart pch
    Change-Id: I413ef289e33c66ad15755807fcb7b0467da66a3a

--- chart2/inc/pch/precompiled_chartcore.hxx
+++ chart2/inc/pch/precompiled_chartcore.hxx
@@ -13,7 +13,7 @@
  manual changes will be rewritten by the next run of update_pch.sh (which presumably
  also fixes all possible problems, so it's usually better to use it).
 
- Generated on 2019-10-17 15:15:04 using:
+ Generated on 2019-12-20 15:44:23 using:
  ./bin/update_pch chart2 chartcore --cutoff=3 --exclude:system --exclude:module --include:local
 
  If after updating build fails, use the following command to locate conflicting headers:
@@ -33,7 +33,7 @@
 #include <string_view>
 #include <utility>
 #include <vector>
-#include <o3tl/optional.hxx>
+#include <boost/property_tree/ptree_fwd.hpp>
 #endif // PCH_LEVEL >= 1
 #if PCH_LEVEL >= 2
 #include <osl/diagnose.h>
