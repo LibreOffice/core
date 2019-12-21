da2b9e5cc0d938fa57d3ff11a9b771a5ec7d0da1
Caolán McNamara <caolanm@redhat.com>
2019-12-21 12:46:54 +0000

    SlideTransitionBox is unused
    since...
    
    commit 57ca02a7486090f1dd63977bb8fb351f9bf9a7f3
    Date:   Wed Apr 18 00:04:09 2018 +0200
    
        NB Impress tabbed toolbar big update
    
    Change-Id: I599e84f4bec66eff301ab1a92cc1206a47068e02

--- sd/Library_sd.mk
+++ sd/Library_sd.mk
@@ -189,7 +189,6 @@ $(eval $(call gb_Library_add_exception_objects,sd,\
 	sd/source/ui/animations/CustomAnimationPane \
 	sd/source/ui/animations/STLPropertySet \
 	sd/source/ui/animations/SlideTransitionPane \
-	sd/source/ui/animations/SlideTransitionBox \
 	sd/source/ui/animations/motionpathtag \
 	sd/source/ui/annotations/annotationmanager \
 	sd/source/ui/annotations/annotationtag \
