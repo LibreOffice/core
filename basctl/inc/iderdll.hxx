e413cf669ba14b7632123bd869beab2975d86744
ayhanyalcinsoy <ayhanyalcinsoy@pisilinux.org>
2019-12-22 01:30:32 +0300

    tdf#124176:Use pragma once instead of include guards
    Change-Id: Ie00f05a3661a9fd6159c424e01f11e7bb1ea45fa

--- basctl/inc/iderdll.hxx
+++ basctl/inc/iderdll.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_BASCTL_INC_IDERDLL_HXX
-#define INCLUDED_BASCTL_INC_IDERDLL_HXX
+#pragma once
 
 namespace basctl
 {
@@ -35,7 +34,5 @@ void ShellDestroyed (Shell const * pShell);
 ExtraData* GetExtraData();
 
 } // namespace basctl
-
-#endif // INCLUDED_BASCTL_INC_IDERDLL_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
