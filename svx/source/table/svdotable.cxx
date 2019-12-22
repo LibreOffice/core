99d4556af0104971aa778b804bf062c9a4b74c9d
Stepas Toliautas <stepas.toliautas@gmail.com>
2019-12-22 22:45:55 +0200

    Make SdrTableObj default ctor return 0x0 table
    Current cloning mechanism involves creating object of certain class
    and then copying source object data via operator= function.
    For SdrTableObj, this constructs and layouts 1x1 table object,
    then tears everything down and reconstructs with source object data.
    To avoid this, 'default' SrdTableObj ctor skips most initialization,
    while creation of real new tables is done using another ctor that
    specifies table size and row/ col counts.
    
    Change-Id: Ib1a69fe8c5cd56df3d76d19fa59308d0b2bc46a4

--- svx/source/table/svdotable.cxx
+++ svx/source/table/svdotable.cxx
@@ -372,13 +372,16 @@ void SdrTableObjImpl::CropTableModelToSelection(const CellPos& rStart, const Cel
 void SdrTableObjImpl::init( SdrTableObj* pTable, sal_Int32 nColumns, sal_Int32 nRows )
 {
     mpTableObj = pTable;
-    mxTable = new TableModel( pTable );
-    mxTable->init( nColumns, nRows );
-    Reference< XModifyListener > xListener( static_cast< css::util::XModifyListener* >(this) );
-    mxTable->addModifyListener( xListener );
-    mpLayouter.reset(new TableLayouter( mxTable ));
-    LayoutTable( mpTableObj->maRect, true, true );
-    mpTableObj->maLogicRect = mpTableObj->maRect;
+    if ( nColumns > 0 && nRows > 0 )
+    {
+        mxTable = new TableModel( pTable );
+        mxTable->init( nColumns, nRows );
+        Reference< XModifyListener > xListener( static_cast< css::util::XModifyListener* >(this) );
+        mxTable->addModifyListener( xListener );
+        mpLayouter.reset(new TableLayouter( mxTable ));
+        LayoutTable( mpTableObj->maRect, true, true );
+        mpTableObj->maLogicRect = mpTableObj->maRect;
+    }
 }
 
 
@@ -864,7 +867,7 @@ std::unique_ptr<sdr::contact::ViewContact> SdrTableObj::CreateObjectSpecificView
 SdrTableObj::SdrTableObj(SdrModel& rSdrModel)
 :   SdrTextObj(rSdrModel)
 {
-    init( 1, 1 );
+    init( 0, 0 );
 }
 
 SdrTableObj::SdrTableObj(
