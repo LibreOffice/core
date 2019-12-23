25af62943bea030e507cff5ab13e2c4e78ae79e4
Stephan Bergmann <sbergman@redhat.com>
2019-12-23 22:34:20 +0100

    SvtAccessibilityOptions base SfxListener appears to be unused
    Change-Id: I96fa0c6eb7f0510eb8233b98072fd555307f8329

--- include/svtools/accessibilityoptions.hxx
+++ include/svtools/accessibilityoptions.hxx
@@ -20,13 +20,12 @@
 #define INCLUDED_SVTOOLS_ACCESSIBILITYOPTIONS_HXX
 
 #include <svtools/svtdllapi.h>
-#include <svl/lstner.hxx>
 #include <unotools/options.hxx>
 
 class SvtAccessibilityOptions_Impl;
 
 class SVT_DLLPUBLIC SvtAccessibilityOptions final :
-    public utl::detail::Options, private SfxListener
+    public utl::detail::Options
 {
 private:
     static SvtAccessibilityOptions_Impl* sm_pSingleImplConfig;
@@ -43,8 +42,6 @@ public:
     bool        GetIsAutomaticFontColor() const;
     bool        IsSelectionInReadonly() const;
 
-    //SfxListener:
-    virtual void        Notify( SfxBroadcaster& rBC, const SfxHint& rHint ) override;
     void        SetVCLSettings();
 };
 
