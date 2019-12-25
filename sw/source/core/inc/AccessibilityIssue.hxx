3ed7ad79af88c802ebc478c418cd624b8a3d6e19
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2019-12-25 13:27:32 +0100

    acc. check: implement goto for OLE/Graphic objects
    This implements goto function for Graphic/OLE objects that are
    reported in a AccessibilityIssue. So this enables to go to the
    object inside a document for which we have reported an
    accessibility issue.
    
    Change-Id: I699966b116a76ea032cb1acc9a431687fa204e07

--- sw/source/core/inc/AccessibilityIssue.hxx
+++ sw/source/core/inc/AccessibilityIssue.hxx
@@ -12,17 +12,33 @@
 #define INCLUDED_SW_SOURCE_CORE_ACCESSIBILITYISSUE_HXX
 
 #include <svx/AccessibilityIssue.hxx>
+#include <doc.hxx>
 
 namespace sw
 {
+enum class IssueObject
+{
+    UNKNOWN,
+    GRAPHIC,
+    OLE,
+};
+
 class SW_DLLPUBLIC AccessibilityIssue final : public svx::AccessibilityIssue
 {
+private:
+    IssueObject m_eIssueObject;
+    SwDoc* m_pDoc;
+    OUString m_sObjectID;
+
 public:
-    AccessibilityIssue(svx::AccessibilityIssueID eIssueID = svx::AccessibilityIssueID::UNSPECIFIED)
-        : svx::AccessibilityIssue(eIssueID)
-    {
-    }
-    void gotoIssue() const override {}
+    AccessibilityIssue(svx::AccessibilityIssueID eIssueID = svx::AccessibilityIssueID::UNSPECIFIED);
+
+    void setIssueObject(IssueObject eIssueObject);
+    void setDoc(SwDoc* pDoc);
+    void setObjectID(OUString const& rID);
+
+    bool canGotoIssue() const override;
+    void gotoIssue() const override;
 };
 
 } // end sw namespace
