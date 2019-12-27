33b94b157dde472bb0c19af5e0fb9dac17828bf1
Andrea Gelmini <andrea.gelmini@gelma.net>
2019-12-28 00:56:14 +0100

    tdf#124176: Use "pragma once" on linguistic/*
    It passed "make check" on Linux
    
    Change-Id: I3f2b912827e02613b1e3d27790a4a8bb5c2ac768

--- linguistic/source/lngreg.hxx
+++ linguistic/source/lngreg.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_LINGUISTIC_SOURCE_LNGREG_HXX
-#define INCLUDED_LINGUISTIC_SOURCE_LNGREG_HXX
+#pragma once
 
 #include <sal/config.h>
 
@@ -58,6 +57,5 @@ extern void * GrammarCheckingIterator_getFactory
     css::lang::XMultiServiceFactory * pServiceManager
 );
 
-#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
--- linguistic/source/spelldsp.hxx
+++ linguistic/source/spelldsp.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_LINGUISTIC_SOURCE_SPELLDSP_HXX
-#define INCLUDED_LINGUISTIC_SOURCE_SPELLDSP_HXX
+#pragma once
 
 #include "defs.hxx"
 #include <linguistic/misc.hxx>
@@ -134,6 +133,5 @@ inline css::uno::Reference< css::linguistic2::XSearchableDictionaryList >
 }
 
 
-#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
