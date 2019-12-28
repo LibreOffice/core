772ecf54651778fb7f67ba0a01fed9a7fd3ad555
cagatay <cagatayyigit3@gmail.com>
2019-12-28 20:05:50 +0300

    tdf#124176: Use pragma once instead of include guards
    Change-Id: If73053c1fa0f65ff7296bf8f5a34d6ead64167ab

--- cppcanvas/source/mtfrenderer/cachedprimitivebase.hxx
+++ cppcanvas/source/mtfrenderer/cachedprimitivebase.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_CPPCANVAS_SOURCE_MTFRENDERER_CACHEDPRIMITIVEBASE_HXX
-#define INCLUDED_CPPCANVAS_SOURCE_MTFRENDERER_CACHEDPRIMITIVEBASE_HXX
+#pragma once
 
 #include <com/sun/star/uno/Reference.hxx>
 #include <com/sun/star/rendering/XCachedPrimitive.hpp>
@@ -82,7 +81,5 @@ namespace cppcanvas
         };
     }
 }
-
-#endif // INCLUDED_CPPCANVAS_SOURCE_MTFRENDERER_CACHEDPRIMITIVEBASE_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
