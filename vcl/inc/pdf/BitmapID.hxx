41f9065863cf265d7751cec9b1f875b5ddcbc8af
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2019-12-29 19:13:17 +0100

    pdf: move struct BitmapID to it's own file
    This is done to decrease bloat in pdfwriter_impl.hxx. More will
    follow, but it is safer to do it one by one.
    
    Change-Id: I700f9c729d6521df732d19e3fca866c83e2fb05c

---
+++ vcl/inc/pdf/BitmapID.hxx
@@ -0,0 +1,44 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#ifndef INCLUDED_VCL_INC_PDF_BITMAPID_HXX
+#define INCLUDED_VCL_INC_PDF_BITMAPID_HXX
+
+#include <vcl/dllapi.h>
+#include <vcl/checksum.hxx>
+#include <tools/gen.hxx>
+
+namespace vcl::pdf
+{
+struct BitmapID
+{
+    Size m_aPixelSize;
+    sal_Int32 m_nSize;
+    BitmapChecksum m_nChecksum;
+    BitmapChecksum m_nMaskChecksum;
+
+    BitmapID()
+        : m_nSize(0)
+        , m_nChecksum(0)
+        , m_nMaskChecksum(0)
+    {
+    }
+
+    bool operator==(const BitmapID& rComp) const
+    {
+        return (m_aPixelSize == rComp.m_aPixelSize && m_nSize == rComp.m_nSize
+                && m_nChecksum == rComp.m_nChecksum && m_nMaskChecksum == rComp.m_nMaskChecksum);
+    }
+};
+}
+
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
