caccac87b369988b63b952957d367f4d2ee6791c
Noel Grandin <noelgrandin@gmail.com>
2019-12-29 14:00:14 +0200

    tdf#129228 speedup opening of xlsx file with lots of comments
    de-UNOise the Comment::finalizeImport method, so we can build
    a better custom import code path, that does not touch all the stuff
    that updating a running UI needs.
    
    The primary improvements comes from using setPropertyValues
    to set a bunch of props together.
    
    This takes the opening time from 61s to 53s for me.
    
    Change-Id: I5506a5a37a9b4b84b6930f0563a775a8aa0a9e2c
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/85947
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
    Tested-by: Noel Grandin <noel.grandin@collabora.co.uk>
    (cherry picked from commit 4992d61600536fe14b97b718dbb11f00e936c6a9)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/86343
    Tested-by: Jenkins

--- sc/source/ui/inc/docfunc.hxx
+++ sc/source/ui/inc/docfunc.hxx
@@ -46,6 +46,7 @@ class ScConditionalFormat;
 class ScConditionalFormatList;
 class ScUndoRemoveMerge;
 class ScRangeName;
+class ScPostIt;
 
 enum class TransliterationFlags;
 enum class CreateNameFlags;
@@ -114,10 +115,11 @@ public:
         const ScAddress& rPos, const OUString& rText, bool bInterpret, bool bEnglish, bool bApi,
         const formula::FormulaGrammar::Grammar eGrammar );
 
-    bool            ShowNote( const ScAddress& rPos, bool bShow );
+    SC_DLLPUBLIC bool ShowNote( const ScAddress& rPos, bool bShow );
 
     void            SetNoteText( const ScAddress& rPos, const OUString& rNoteText, bool bApi );
     void            ReplaceNote( const ScAddress& rPos, const OUString& rNoteText, const OUString* pAuthor, const OUString* pDate, bool bApi );
+    SC_DLLPUBLIC ScPostIt* ImportNote( const ScAddress& rPos, const OUString& rNoteText, const OUString* pAuthor, const OUString* pDate );
 
     bool            ApplyAttributes( const ScMarkData& rMark, const ScPatternAttr& rPattern,
                                            bool bApi );
