LibreOffice/core

commit 75ef0e41ea8a9096ac619356d2b837c5333b47e6

tdf#125334 Chart: allow text break in bar chart axis labels
Now vertical category axis labels support text breaking.

Change-Id: I8b1c6eff921ea999bc4f745aa5f85bae278e735b
Reviewed-on: https://gerrit.libreoffice.org/72457
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

chart2/source/view/axes/VCartesianAxis.cxx
@@ -510,7 +510,7 @@ TickInfo* MaxLabelTickIter::nextInfo()
 }
 
 bool VCartesianAxis::isBreakOfLabelsAllowed(
-    const AxisLabelProperties& rAxisLabelProperties, bool bIsHorizontalAxis ) const
+    const AxisLabelProperties& rAxisLabelProperties, bool bIsHorizontalAxis, bool bIsVerticalAxis) const
 {
     if( m_aTextLabels.getLength() > 100 )
         return false;
@@ -525,8 +525,10 @@ bool VCartesianAxis::isBreakOfLabelsAllowed(
            rAxisLabelProperties.fRotationAngleDegree == 90.0 ||
            rAxisLabelProperties.fRotationAngleDegree == 270.0 ) )
         return false;
-    //break only for horizontal axis
-    return bIsHorizontalAxis;
+    if ( !m_aAxisProperties.m_bSwapXAndY )
+        return bIsHorizontalAxis;
+    else
+        return bIsVerticalAxis;
 }
 namespace{
 
@@ -702,7 +704,7 @@ bool VCartesianAxis::createTextShapes(
     const bool bIsHorizontalAxis = pTickFactory->isHorizontalAxis();
     const bool bIsVerticalAxis = pTickFactory->isVerticalAxis();
 
-    if (!isBreakOfLabelsAllowed(rAxisLabelProperties, bIsHorizontalAxis) &&
+    if (!isBreakOfLabelsAllowed(rAxisLabelProperties, bIsHorizontalAxis, bIsVerticalAxis) &&
         !isAutoStaggeringOfLabelsAllowed(rAxisLabelProperties, bIsHorizontalAxis, bIsVerticalAxis) &&
         !rAxisLabelProperties.isStaggered())
         return createTextShapesSimple(xTarget, rTickIter, rAxisLabelProperties, pTickFactory);
@@ -714,7 +716,7 @@ bool VCartesianAxis::createTextShapes(
     B2DVector aTextToTickDistance = pTickFactory->getDistanceAxisTickToText(m_aAxisProperties, true);
     sal_Int32 nLimitedSpaceForText = -1;
 
-    if( isBreakOfLabelsAllowed( rAxisLabelProperties, bIsHorizontalAxis ) )
+    if( isBreakOfLabelsAllowed( rAxisLabelProperties, bIsHorizontalAxis, bIsVerticalAxis ) )
     {
         nLimitedSpaceForText = nScreenDistanceBetweenTicks;
         if( bIsStaggered )
@@ -742,6 +744,12 @@ bool VCartesianAxis::createTextShapes(
                 nLimitedSpaceForText = -1;
             }
         }
+
+        // recalculate the nLimitedSpaceForText in case of vertical category axis if the text break is true
+        if ( m_aAxisProperties.m_bSwapXAndY && bIsVerticalAxis && rAxisLabelProperties.fRotationAngleDegree == 0.0 )
+        {
+            nLimitedSpaceForText = rAxisLabelProperties.m_aMaximumSpaceForLabels.X;
+        }
     }
 
      // Stores an array of text label strings in case of a normal
@@ -750,8 +758,11 @@ bool VCartesianAxis::createTextShapes(
     if( m_bUseTextLabels && !m_aAxisProperties.m_bComplexCategories )
         pCategories = &m_aTextLabels;
 
-    bool bLimitedHeight = fabs(aTextToTickDistance.getX()) > fabs(aTextToTickDistance.getY());
-
+    bool bLimitedHeight;
+    if( !m_aAxisProperties.m_bSwapXAndY )
+        bLimitedHeight = fabs(aTextToTickDistance.getX()) > fabs(aTextToTickDistance.getY());
+    else
+        bLimitedHeight = fabs(aTextToTickDistance.getX()) < fabs(aTextToTickDistance.getY());
     //prepare properties for multipropertyset-interface of shape
     tNameSequence aPropNames;
     tAnySequence aPropValues;

sw/qa/extras/layout/layout.cxx
@@ -2324,7 +2324,25 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf108021)
         pXmlDoc,
         "/metafile/push[1]/push[1]/push[1]/push[3]/push[1]/push[1]/push[1]/textarray[@length='22']",
         8);
-    // This failed, if the textarray length of the first axis label not 17.
+    // This failed, if the textarray length of the first axis label not 22.
+}
+
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf125334)
+{
+    SwDoc* pDoc = createDoc("tdf125334.odt");
+    SwDocShell* pShell = pDoc->GetDocShell();
+
+    // Dump the rendering of the first page as an XML file.
+    std::shared_ptr<GDIMetaFile> xMetaFile = pShell->GetPreviewMetaFile();
+    MetafileXmlDump dumper;
+    xmlDocPtr pXmlDoc = dumpAndParse(dumper, *xMetaFile);
+    CPPUNIT_ASSERT(pXmlDoc);
+
+    assertXPath(
+        pXmlDoc,
+        "/metafile/push[1]/push[1]/push[1]/push[3]/push[1]/push[1]/push[1]/textarray[@length='17']",
+        4);
+    // This failed, if the textarray length of the category axis label not 17.
 }
 
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf122800)

chart2/source/view/axes/VCartesianAxis.hxx
@@ -150,7 +150,7 @@ private: //methods
      * @return true if we can break a single line label text into multiple
      *         lines for better fitting, otherwise false.
      */
-    bool isBreakOfLabelsAllowed( const AxisLabelProperties& rAxisLabelProperties, bool bIsHorizontalAxis ) const;
+    bool isBreakOfLabelsAllowed( const AxisLabelProperties& rAxisLabelProperties, bool bIsHorizontalAxis, bool bIsVerticalAxis ) const;
 
     ::basegfx::B2DVector getScreenPosition( double fLogicX, double fLogicY, double fLogicZ ) const;
     ScreenPosAndLogicPos getScreenPosAndLogicPos( double fLogicX, double fLogicY, double fLogicZ ) const;

commit 3a314841069905091da1ca912a8acf52ceb7c8a2

tdf#128996 Chart: Fix disappeared vertical X axis labels
Regression from commit: 75ef0e41ea8a9096ac619356d2b837c5333b47e6
(tdf#125334 Chart: allow text break in bar chart axis labels)

Change-Id: Ib935de74314b7dec489d94a4aa3c65072e18d9e0

chart2/source/view/axes/Tickmarks.cxx
@@ -204,6 +204,11 @@ sal_Int32 TickFactory2D::getTickScreenDistance( TickIter& rIter )
     return pFirstTickInfo->getScreenDistanceBetweenTicks( *pSecondTickInfo );
 }
 
+B2DVector TickFactory2D::getXaxisStartPos() const
+{
+    return m_aAxisStartScreenPosition2D;
+}
+
 B2DVector TickFactory2D::getTickScreenPosition2D( double fScaledLogicTickValue ) const
 {
     B2DVector aRet(m_aAxisStartScreenPosition2D);

chart2/source/view/axes/Tickmarks.hxx
@@ -135,6 +135,7 @@ public:
 
     bool  isHorizontalAxis() const;
     bool  isVerticalAxis() const;
+    ::basegfx::B2DVector getXaxisStartPos() const;
 
 private:
     ::basegfx::B2DVector     getTickScreenPosition2D( double fScaledLogicTickValue ) const;

chart2/source/view/axes/VCartesianAxis.cxx
@@ -745,22 +745,15 @@ bool VCartesianAxis::createTextShapes(
         // recalculate the nLimitedSpaceForText in case of 90 and 270 degree if the text break is true
         if ( rAxisLabelProperties.fRotationAngleDegree == 90.0 || rAxisLabelProperties.fRotationAngleDegree == 270.0 )
         {
-            if ( rAxisLabelProperties.m_aFontReferenceSize.Height - rAxisLabelProperties.m_aMaximumSpaceForLabels.Height > 2 * rAxisLabelProperties.m_aMaximumSpaceForLabels.Y )
-            {
-                const sal_Int32 nFullHeight = rAxisLabelProperties.m_aFontReferenceSize.Height;
-                sal_Int32 nMaxLabelsHeight = nFullHeight - ( rAxisLabelProperties.m_aMaximumSpaceForLabels.Height + rAxisLabelProperties.m_aMaximumSpaceForLabels.Y );
-                nLimitedSpaceForText = nMaxLabelsHeight;
-            }
-            else
-            {
-                nLimitedSpaceForText = -1;
-            }
+            nLimitedSpaceForText = rAxisLabelProperties.m_aFontReferenceSize.Height - pTickFactory->getXaxisStartPos().getY();
+            m_aAxisProperties.m_bLimitSpaceForLabels = false;
         }
 
         // recalculate the nLimitedSpaceForText in case of vertical category axis if the text break is true
         if ( m_aAxisProperties.m_bSwapXAndY && bIsVerticalAxis && rAxisLabelProperties.fRotationAngleDegree == 0.0 )
         {
-            nLimitedSpaceForText = rAxisLabelProperties.m_aMaximumSpaceForLabels.X;
+            nLimitedSpaceForText = pTickFactory->getXaxisStartPos().getX();
+            m_aAxisProperties.m_bLimitSpaceForLabels = false;
         }
     }
 

sw/qa/extras/layout/layout.cxx
@@ -2365,6 +2365,23 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf122800)
     // This failed, if the textarray length of the first axis label not 22.
 }
 
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf128996)
+{
+    SwDoc* pDoc = createDoc("tdf128996.docx");
+    SwDocShell* pShell = pDoc->GetDocShell();
+
+    // Dump the rendering of the first page as an XML file.
+    std::shared_ptr<GDIMetaFile> xMetaFile = pShell->GetPreviewMetaFile();
+    MetafileXmlDump dumper;
+    xmlDocPtr pXmlDoc = dumpAndParse(dumper, *xMetaFile);
+    CPPUNIT_ASSERT(pXmlDoc);
+
+    assertXPathContent(
+        pXmlDoc,
+        "/metafile/push[1]/push[1]/push[1]/push[4]/push[1]/textarray[1]/text",
+        "A very long category name 1");
+}
+
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf126244)
 {
     SwDoc* pDoc = createDoc("tdf126244.docx");