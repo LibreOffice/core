3d0d5de9275f1da391d36d7f47de3944e04d98d5
Yusuf Keten <ketenyusuf@gmail.com>
2019-12-31 21:19:05 +0300

    tdf#75280 Convert inappropriate use of sal_uIntPtr to better integer types
    Change-Id: I5a1bbbc677f41fec342484b5244a32ce3a624bd8

--- svx/source/gallery2/galbrws1.cxx
+++ svx/source/gallery2/galbrws1.cxx
@@ -135,7 +135,7 @@ GalleryBrowser1::GalleryBrowser1(
     mpThemes->SetSelectHdl( LINK( this, GalleryBrowser1, SelectThemeHdl ) );
     mpThemes->SetAccessibleName(SvxResId(RID_SVXSTR_GALLERYPROPS_GALTHEME));
 
-    for( sal_uIntPtr i = 0, nCount = mpGallery->GetThemeCount(); i < nCount; i++ )
+    for( size_t i = 0, nCount = mpGallery->GetThemeCount(); i < nCount; i++ )
         ImplInsertThemeEntry( mpGallery->GetThemeInfo( i ) );
 
     ImplAdjustControls();
