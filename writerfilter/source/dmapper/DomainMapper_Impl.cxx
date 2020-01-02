LibreOffice/core

commit b2c6d2d961a6113d0f111fab45ae12a40d389a23

fdo#38414 tdf#44986: DOCX table import: handle gridBefore/After
without serious regressions, ie. losing the import of complex
forms with multiple or nested tables.

Complete the fix for tdf#116194 (DOCX import: fix missing
tables with w:gridBefore) with handling gridAfter on
DomainMapper level.

This consists of also rejections (except their unit tests) of

commit cf33af732ed0d3d553bb74636e3b14c55d44c153
(handle w:gridBefore by faking cells (fdo#38414)) and

commit 1d1748d143ab4270a2ca1b5117852b1b1bb4c526 (Related:
tdf#44986 DOCX import: handle w:gridAfter by faking cells)

Change-Id: I31fa1de03bcdf42424fa5507fb5a3e06aa47107d
Reviewed-on: https://gerrit.libreoffice.org/84517
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -279,6 +279,7 @@ DomainMapper_Impl::DomainMapper_Impl(
         m_bIsFirstParaInSection( true ),
         m_bIsFirstParaInSectionAfterRedline( true ),
         m_bDummyParaAddedForTableInSection( false ),
+        m_bDummyCharAddedForTableRowGridAfter( false ),
         m_bTextFrameInserted(false),
         m_bIsPreviousParagraphFramed( false ),
         m_bIsLastParaInSection( false ),
@@ -1634,6 +1635,17 @@ void DomainMapper_Impl::finishParagraph( const PropertyMapPtr& pPropertyMap, con
                                 m_xPreviousParagraph->setPropertyValue("ParaBottomMargin", uno::makeAny(static_cast<sal_Int32>(0)));
                             }
                         }
+                    }
+
+                    // remove dummy character added for gridAfter table cells
+                    if (GetIsDummyCharAddedForTableRowGridAfter())
+                    {
+                        SetIsDummyCharAddedForTableRowGridAfter(false);
+                        uno::Reference<text::XParagraphCursor> xParaCursor(
+                            xTextAppend->createTextCursorByRange(xTextAppend->getEnd()), uno::UNO_QUERY_THROW);
+                        xParaCursor->gotoStartOfParagraph( false);
+                        xParaCursor->goRight(1, true);
+                        xParaCursor->setString("");
                     }
 
                     xTextRange = xTextAppend->finishParagraph( comphelper::containerToSequence(aProperties) );

writerfilter/source/dmapper/DomainMapper_Impl.hxx
@@ -533,6 +533,7 @@ private:
     bool                            m_bIsFirstParaInSectionAfterRedline;
     bool                            m_bIsFirstParaInShape = false;
     bool                            m_bDummyParaAddedForTableInSection;
+    bool                            m_bDummyCharAddedForTableRowGridAfter;
     bool                            m_bTextFrameInserted;
     bool                            m_bIsPreviousParagraphFramed;
     bool                            m_bIsLastParaInSection;
@@ -630,6 +631,8 @@ public:
     bool GetIsFirstParagraphInShape() const { return m_bIsFirstParaInShape; }
     void SetIsDummyParaAddedForTableInSection( bool bIsAdded );
     bool GetIsDummyParaAddedForTableInSection() const { return m_bDummyParaAddedForTableInSection;}
+    void SetIsDummyCharAddedForTableRowGridAfter( bool bIsAdded ) { m_bDummyCharAddedForTableRowGridAfter = bIsAdded; }
+    bool GetIsDummyCharAddedForTableRowGridAfter() const { return m_bDummyCharAddedForTableRowGridAfter;}
 
     /// Track if a textframe has been inserted into this section
     void SetIsTextFrameInserted( bool bIsInserted );

writerfilter/source/dmapper/TableManager.cxx
@@ -408,23 +408,43 @@ void TableManager::endRow()
     TagLogger::getInstance().element("tablemanager.endRow");
 #endif
     TableData::Pointer_t pTableData = mTableDataStack.top();
+    sal_uInt32 nGridBefore = mpTableDataHandler->getDomainMapperImpl().getTableManager().getCurrentGridBefore();
+    sal_uInt32 nGridAfter = mpTableDataHandler->getDomainMapperImpl().getTableManager().getCurrentGridAfter();
 
-    // Add borderless w:gridBefore cell(s) to the row
-    if (pTableData)
+    // Add borderless w:gridBefore and w:gridAfter cell(s) to the row
+    if (pTableData && (nGridBefore > 0 || nGridAfter > 0))
     {
-        sal_uInt32 nGridBefore = mpTableDataHandler->getDomainMapperImpl().getTableManager().getCurrentGridBefore();
-        for (unsigned int i = 0; i < nGridBefore; ++i)
+        css::table::BorderLine2 aBorderLine;
+        aBorderLine.Color = 0;
+        aBorderLine.InnerLineWidth = 0;
+        aBorderLine.OuterLineWidth = 0;
+        TablePropertyMapPtr pCellProperties(new TablePropertyMap);
+        pCellProperties->Insert(PROP_TOP_BORDER, css::uno::makeAny(aBorderLine));
+        pCellProperties->Insert(PROP_LEFT_BORDER, css::uno::makeAny(aBorderLine));
+        pCellProperties->Insert(PROP_BOTTOM_BORDER, css::uno::makeAny(aBorderLine));
+        pCellProperties->Insert(PROP_RIGHT_BORDER, css::uno::makeAny(aBorderLine));
+        if (nGridBefore > 0)
         {
-            css::table::BorderLine2 aBorderLine;
-            aBorderLine.Color = 0;
-            aBorderLine.InnerLineWidth = 0;
-            aBorderLine.OuterLineWidth = 0;
-            TablePropertyMapPtr pCellProperties(new TablePropertyMap);
-            pCellProperties->Insert(PROP_TOP_BORDER, css::uno::makeAny(aBorderLine));
-            pCellProperties->Insert(PROP_LEFT_BORDER, css::uno::makeAny(aBorderLine));
-            pCellProperties->Insert(PROP_BOTTOM_BORDER, css::uno::makeAny(aBorderLine));
-            pCellProperties->Insert(PROP_RIGHT_BORDER, css::uno::makeAny(aBorderLine));
-            pTableData->getCurrentRow()->addCell(pTableData->getCurrentRow()->getCellStart(0), pCellProperties, /*bAddBefore=*/true);
+            const css::uno::Reference<css::text::XTextRange>& rFirstCellStartHandle = pTableData->getCurrentRow()->getCellStart(0);
+            for (unsigned int i = 0; i < nGridBefore; ++i)
+                pTableData->getCurrentRow()->addCell(rFirstCellStartHandle, pCellProperties, /*bAddBefore=*/true);
+        }
+        if (nGridAfter > 0)
+        {
+            const css::uno::Reference<css::text::XTextRange>& rEndCellEndHandle = pTableData->getCurrentRow()->getCellEnd(pTableData->getCurrentRow()->getCellCount()-1);
+            auto xCursor(rEndCellEndHandle->getText()->createTextCursorByRange(rEndCellEndHandle));
+            if (!xCursor->getString().isEmpty())
+            {
+                // add a dummy character to get a right handle for gridAfter cells after the last non-empty cell, removing that later
+                xCursor->gotoEnd(false);
+                xCursor->setString("x");
+                mpTableDataHandler->getDomainMapperImpl().SetIsDummyCharAddedForTableRowGridAfter(true);
+            }
+            for (unsigned int i = 0; i < nGridAfter; ++i)
+            {
+                pTableData->getCurrentRow()->addCell(xCursor, pCellProperties, /*bAddBefore=*/false);
+                pTableData->getCurrentRow()->endCell(xCursor);
+            }
         }
     }
 

sw/qa/extras/ooxmlexport/ooxmlexport.cxx
@@ -936,6 +936,10 @@ DECLARE_OOXMLEXPORT_TEST(testTdf44986, "tdf44986.docx")
     // Check the first row of the table, it should have two cells (one separator).
     // This was 0: the first row had no separators, so it had only one cell, which was too wide.
     CPPUNIT_ASSERT_EQUAL(sal_Int32(1), getProperty< uno::Sequence<text::TableColumnSeparator> >(xTableRows->getByIndex(0), "TableColumnSeparators").getLength());
+    // Check content of cells, including the newly added gridAfter cell
+    CPPUNIT_ASSERT_EQUAL(OUString("A1"), uno::Reference<text::XTextRange>(xTable->getCellByName("A1"), uno::UNO_QUERY_THROW)->getString());
+    CPPUNIT_ASSERT_EQUAL(OUString("A2"), uno::Reference<text::XTextRange>(xTable->getCellByName("A2"), uno::UNO_QUERY_THROW)->getString());
+    CPPUNIT_ASSERT_EQUAL(OUString(""), uno::Reference<text::XTextRange>(xTable->getCellByName("B1"), uno::UNO_QUERY_THROW)->getString());
 }
 
 DECLARE_OOXMLEXPORT_TEST(testTdf106953, "tdf106953.docx")

writerfilter/source/dmapper/DomainMapperTableManager.cxx
@@ -48,7 +48,7 @@ DomainMapperTableManager::DomainMapperTableManager() :
     m_nCell(),
     m_nGridSpan(1),
     m_aGridBefore(),
-    m_nGridAfter(0),
+    m_aGridAfter(),
     m_nHeaderRepeat(0),
     m_nTableWidth(0),
     m_bIsInShape(false),
@@ -357,7 +357,7 @@ bool DomainMapperTableManager::sprm(Sprm & rSprm)
                 m_aGridBefore.back( ) = nIntValue;
                 break;
             case NS_ooxml::LN_CT_TrPrBase_gridAfter:
-                m_nGridAfter = nIntValue;
+                m_aGridAfter.back() = nIntValue;
                 break;
             case NS_ooxml::LN_CT_TblPrBase_tblCaption:
                 // To-Do: Not yet preserved
@@ -397,6 +397,11 @@ DomainMapperTableManager::IntVectorPtr const & DomainMapperTableManager::getCurr
 sal_uInt32 DomainMapperTableManager::getCurrentGridBefore( )
 {
     return m_aGridBefore.back( );
+}
+
+sal_uInt32 DomainMapperTableManager::getCurrentGridAfter( )
+{
+    return m_aGridAfter.back( );
 }
 
 bool DomainMapperTableManager::hasCurrentSpans() const
@@ -462,6 +467,7 @@ void DomainMapperTableManager::startLevel( )
     m_aTmpTableProperties.push_back( pTmpProperties );
     m_nCell.push_back( 0 );
     m_aGridBefore.push_back( 0 );
+    m_aGridAfter.push_back( 0 );
     m_nTableWidth = 0;
     m_nLayoutType = 0;
 
@@ -492,6 +498,7 @@ void DomainMapperTableManager::endLevel( )
 
     m_nCell.pop_back( );
     m_aGridBefore.pop_back( );
+    m_aGridAfter.pop_back( );
     m_nTableWidth = 0;
     m_nLayoutType = 0;
 
@@ -548,6 +555,7 @@ void DomainMapperTableManager::endOfRowAction()
         IntVectorPtr pTmpCellWidths = m_aCellWidths.back();
         sal_uInt32 nTmpCell = m_nCell.back();
         sal_uInt32 nTmpGridBefore = m_aGridBefore.back();
+        sal_uInt32 nTmpGridAfter = m_aGridAfter.back();
 
         // endLevel and startLevel are taking care of the non finished row
         // to carry it over to the next table
@@ -561,11 +569,13 @@ void DomainMapperTableManager::endOfRowAction()
         m_aCellWidths.pop_back();
         m_nCell.pop_back();
         m_aGridBefore.pop_back();
+        m_aGridAfter.pop_back();
         m_aTableGrid.push_back(pTmpTableGrid);
         m_aGridSpans.push_back(pTmpGridSpans);
         m_aCellWidths.push_back(pTmpCellWidths);
         m_nCell.push_back(nTmpCell);
         m_aGridBefore.push_back(nTmpGridBefore);
+        m_aGridAfter.push_back(nTmpGridAfter);
     }
 
     // Push the tmp position now that we compared it
@@ -612,10 +622,10 @@ void DomainMapperTableManager::endOfRowAction()
         //fill missing gridBefore elements with '1'
         pCurrentSpans->insert( pCurrentSpans->begin( ), m_aGridBefore.back(), 1 );
     }
-    if( pCurrentSpans->size() < m_aGridBefore.back() + m_nCell.back( ))
+    if( pCurrentSpans->size() < m_aGridBefore.back() + m_nCell.back( ) + m_aGridAfter.back() )
     {
         //fill missing elements with '1'
-        pCurrentSpans->insert( pCurrentSpans->end( ), m_aGridBefore.back() + m_nCell.back( ) - pCurrentSpans->size(), 1 );
+        pCurrentSpans->insert( pCurrentSpans->end( ), m_aGridBefore.back() + m_nCell.back( ) + m_aGridAfter.back() - pCurrentSpans->size(), 1 );
     }
 
 #ifdef DBG_UTIL
@@ -642,7 +652,7 @@ void DomainMapperTableManager::endOfRowAction()
     for (int i : (*pTableGrid))
         nFullWidthRelative = o3tl::saturating_add(nFullWidthRelative, i);
 
-    if( pTableGrid->size() == ( nGrids + m_nGridAfter ) && m_nCell.back( ) > 0 )
+    if( pTableGrid->size() == nGrids && m_nCell.back( ) > 0 )
     {
         /*
          * If table width property set earlier is smaller than the current table width,
@@ -672,12 +682,12 @@ void DomainMapperTableManager::endOfRowAction()
                 }
             }
         }
-        uno::Sequence< text::TableColumnSeparator > aSeparators( m_aGridBefore.back() + m_nCell.back( ) - 1 );
+        uno::Sequence< text::TableColumnSeparator > aSeparators( m_aGridBefore.back() + m_nCell.back( ) + m_aGridAfter.back() - 1 );
         text::TableColumnSeparator* pSeparators = aSeparators.getArray();
         double nLastRelPos = 0.0;
         sal_uInt32 nBorderGridIndex = 0;
 
-        size_t nWidthsBound = m_aGridBefore.back() + m_nCell.back() - 1;
+        size_t nWidthsBound = m_aGridBefore.back() + m_nCell.back() + m_aGridAfter.back() - 1;
         if (nWidthsBound)
         {
             if (nFullWidthRelative == 0)
@@ -710,7 +720,7 @@ void DomainMapperTableManager::endOfRowAction()
     }
     else if ( !pCellWidths->empty() &&
                ( m_nLayoutType == NS_ooxml::LN_Value_doc_ST_TblLayout_fixed
-                 || pCellWidths->size() == ( nGrids + m_nGridAfter ) )
+                 || pCellWidths->size() == nGrids )
              )
     {
         // If we're here, then the number of cells does not equal to the amount
@@ -765,11 +775,11 @@ void DomainMapperTableManager::endOfRowAction()
     ++m_nRow;
     m_nCell.back( ) = 0;
     m_aGridBefore.back( ) = 0;
+    m_aGridAfter.back( ) = 0;
     getCurrentGrid()->clear();
     pCurrentSpans->clear();
     pCellWidths->clear();
 
-    m_nGridAfter = 0;
     m_bTableSizeTypeInserted = false;
 
 #ifdef DBG_UTIL

writerfilter/source/dmapper/DomainMapperTableManager.hxx
@@ -43,7 +43,7 @@ class DomainMapperTableManager : public TableManager
     ::std::vector< sal_uInt32 > m_nCell;
     sal_uInt32      m_nGridSpan;
     ::std::vector< sal_uInt32 > m_aGridBefore; ///< number of grid columns in the parent table's table grid which must be skipped before the contents of this table row are added to the parent table
-    sal_uInt32      m_nGridAfter; ///< number of grid columns in the parent table's table grid which shall be left after the last cell in the table row
+    ::std::vector< sal_uInt32 > m_aGridAfter; ///< number of grid columns in the parent table's table grid which shall be left after the last cell in the table row
     sal_Int32       m_nHeaderRepeat; //counter of repeated headers - if == -1 then the repeating stops
     sal_Int32       m_nTableWidth; //might be set directly or has to be calculated from the column positions
     /// Are we in a shape (text append stack is not empty) or in the body document?
@@ -94,6 +94,7 @@ public:
     IntVectorPtr const & getCurrentSpans( );
     IntVectorPtr const & getCurrentCellWidths( );
     sal_uInt32 getCurrentGridBefore( );
+    sal_uInt32 getCurrentGridAfter( );
 
     /// Turn the attributes collected so far in m_aTableLook into a property and clear the container.
     void finishTableLook();

writerfilter/source/ooxml/OOXMLFastContextHandler.cxx
@@ -1397,14 +1397,6 @@ void OOXMLFastContextHandlerTextTableRow::startRow()
 
 void OOXMLFastContextHandlerTextTableRow::endRow()
 {
-    if (mpGridAfter)
-    {
-        // Grid after is the same as grid before, the empty cells are just
-        // inserted after the real ones, not before.
-        handleGridBefore(mpGridAfter);
-        mpGridAfter = nullptr;
-    }
-
     startParagraphGroup();
 
     if (isForwardEvents())
@@ -1433,81 +1425,6 @@ void OOXMLFastContextHandlerTextTableRow::endRow()
 
     endCharacterGroup();
     endParagraphGroup();
-}
-
-void OOXMLFastContextHandlerTextTableRow::handleGridAfter(const OOXMLValue::Pointer_t& rValue)
-{
-    if (OOXMLFastContextHandler* pTableRowProperties = getParent())
-    {
-        if (OOXMLFastContextHandler* pTableRow = pTableRowProperties->getParent())
-            // Save the value into the table row context, so it can be handled
-            // right before the end of the row.
-            pTableRow->setGridAfter(rValue);
-    }
-}
-
-namespace {
-OOXMLValue::Pointer_t fakeNoBorder()
-{
-    OOXMLPropertySet::Pointer_t pProps( new OOXMLPropertySet );
-    OOXMLValue::Pointer_t pVal = OOXMLIntegerValue::Create(0);
-    pProps->add(NS_ooxml::LN_CT_Border_val, pVal, OOXMLProperty::ATTRIBUTE);
-    OOXMLValue::Pointer_t pValue( new OOXMLPropertySetValue( pProps ));
-    return pValue;
-}
-}
-
-// Handle w:gridBefore here by faking necessary input that'll fake cells. I'm apparently
-// not insane enough to find out how to add cells in dmapper.
-void OOXMLFastContextHandlerTextTableRow::handleGridBefore( const OOXMLValue::Pointer_t& val )
-{
-    // start removing: disable for w:gridBefore
-    if (!mpGridAfter)
-        return;
-
-    int count = val->getInt();
-    for( int i = 0;
-         i < count;
-         ++i )
-    {
-        endOfParagraph();
-
-        if (isForwardEvents())
-        {
-            // This whole part is OOXMLFastContextHandlerTextTableCell::endCell() .
-            OOXMLPropertySet::Pointer_t pProps(new OOXMLPropertySet);
-            {
-                OOXMLValue::Pointer_t pVal = OOXMLIntegerValue::Create(mnTableDepth);
-                pProps->add(NS_ooxml::LN_tblDepth, pVal, OOXMLProperty::SPRM);
-            }
-            {
-                OOXMLValue::Pointer_t pVal = OOXMLIntegerValue::Create(1);
-                pProps->add(NS_ooxml::LN_inTbl, pVal, OOXMLProperty::SPRM);
-            }
-            {
-                OOXMLValue::Pointer_t pVal = OOXMLBooleanValue::Create(mnTableDepth > 0);
-                pProps->add(NS_ooxml::LN_tblCell, pVal, OOXMLProperty::SPRM);
-            }
-
-            mpStream->props(pProps.get());
-
-            // fake <w:tcBorders> with no border
-            OOXMLPropertySet::Pointer_t pCellProps( new OOXMLPropertySet );
-            {
-                OOXMLPropertySet::Pointer_t pBorderProps( new OOXMLPropertySet );
-                static Id borders[] = { NS_ooxml::LN_CT_TcBorders_top, NS_ooxml::LN_CT_TcBorders_bottom,
-                    NS_ooxml::LN_CT_TcBorders_start, NS_ooxml::LN_CT_TcBorders_end };
-                for(sal_uInt32 border : borders)
-                    pBorderProps->add(border, fakeNoBorder(), OOXMLProperty::SPRM);
-                OOXMLValue::Pointer_t pValue( new OOXMLPropertySetValue( pBorderProps ));
-                pCellProps->add(NS_ooxml::LN_CT_TcPrBase_tcBorders, pValue, OOXMLProperty::SPRM);
-                mpParserState->setCellProperties(pCellProps);
-            }
-        }
-
-        sendCellProperties();
-        endParagraphGroup();
-    }
 }
 
 /*

writerfilter/source/ooxml/OOXMLFastContextHandler.hxx
@@ -186,8 +186,6 @@ public:
     virtual void setDefaultStringValue();
 
     void sendPropertyToParent();
-    OOXMLFastContextHandler* getParent() const { return mpParent; }
-    void setGridAfter(const OOXMLValue::Pointer_t& pGridAfter) { mpGridAfter = pGridAfter; }
 
 protected:
     OOXMLFastContextHandler * mpParent;
@@ -226,7 +224,6 @@ protected:
     const css::uno::Reference< css::uno::XComponentContext >& getComponentContext() const { return m_xContext;}
 
     bool inPositionV;
-    OOXMLValue::Pointer_t mpGridAfter;
 
 private:
     void operator =(OOXMLFastContextHandler const &) = delete;
@@ -407,8 +404,6 @@ public:
 
     static void startRow();
     void endRow();
-    void handleGridBefore( const OOXMLValue::Pointer_t& val );
-    void handleGridAfter(const OOXMLValue::Pointer_t& rValue);
 };
 
 class OOXMLFastContextHandlerTextTable : public OOXMLFastContextHandler

commit b4ef16eb281b08d05524b40abd6a739917ee5be0

tdf#129475 DOCX: fix gridAfter with shape-only cells
regression from commit b2c6d2d961a6113d0f111fab45ae12a40d389a23
"fdo#38414 tdf#44986: DOCX table import: handle gridBefore/After".

Change-Id: Icb2d65b7a0766cf8dd00511cde500af3f94d2a94

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -312,7 +312,8 @@ DomainMapper_Impl::DomainMapper_Impl(
         m_bParaHadField(false),
         m_bParaAutoBefore(false),
         m_bFirstParagraphInCell(true),
-        m_bSaveFirstParagraphInCell(false)
+        m_bSaveFirstParagraphInCell(false),
+        m_bParaAnchoredObject(false)
 
 {
     m_aBaseUrl = rMediaDesc.getUnpackedValueOrDefault(
@@ -6140,6 +6141,9 @@ void  DomainMapper_Impl::ImportGraphic(const writerfilter::Reference< Properties
     if( xTextContent.is())
     {
         appendTextContent( xTextContent, uno::Sequence< beans::PropertyValue >() );
+
+        // Remember this paragraph has an anchored object.
+        m_bParaAnchoredObject = true;
 
         if (eGraphicImportType == IMPORT_AS_DETECTED_ANCHOR && !m_aTextAppendStack.empty())
         {

writerfilter/source/dmapper/DomainMapper_Impl.hxx
@@ -1045,6 +1045,8 @@ public:
     bool m_bIsActualParagraphFramed;
     std::vector<css::uno::Any> aFramedRedlines;
 
+    bool HasAnchoredObjects() const { return m_bParaAnchoredObject; }
+
 private:
     void PushPageHeaderFooter(bool bHeader, SectionPropertyMap::PageType eType);
     std::vector<css::uno::Reference< css::drawing::XShape > > m_vTextFramesForChaining ;
@@ -1056,6 +1058,8 @@ private:
     /// Current paragraph in a table is first paragraph of a cell
     bool m_bFirstParagraphInCell;
     bool m_bSaveFirstParagraphInCell;
+    /// Current paragraph has an anchored object
+    bool m_bParaAnchoredObject;
 };
 
 } //namespace dmapper

writerfilter/source/dmapper/TableManager.cxx
@@ -433,7 +433,7 @@ void TableManager::endRow()
         {
             const css::uno::Reference<css::text::XTextRange>& rEndCellEndHandle = pTableData->getCurrentRow()->getCellEnd(pTableData->getCurrentRow()->getCellCount()-1);
             auto xCursor(rEndCellEndHandle->getText()->createTextCursorByRange(rEndCellEndHandle));
-            if (!xCursor->getString().isEmpty())
+            if (!xCursor->getString().isEmpty() || mpTableDataHandler->getDomainMapperImpl().HasAnchoredObjects())
             {
                 // add a dummy character to get a right handle for gridAfter cells after the last non-empty cell, removing that later
                 xCursor->gotoEnd(false);