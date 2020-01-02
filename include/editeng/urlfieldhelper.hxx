LibreOffice/core

commit 64233bbaa0e91ddf14d59ae7547df6bbfa60adcc

tdf#128666 Only enable hyperlink actions when just the field is selected
Change-Id: I984df967877a47fb9f89c3626737348a87d3ffa5
Reviewed-on: https://gerrit.libreoffice.org/84418
Tested-by: Jenkins
Reviewed-by: Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
(cherry picked from commit f31c3ebb60e4678eb09e377b638b368531df47dc)
Reviewed-on: https://gerrit.libreoffice.org/84571

include/editeng/urlfieldhelper.hxx
@@ -12,12 +12,13 @@
 #include <sal/config.h>
 #include <editeng/editengdllapi.h>
 #include <editeng/outliner.hxx>
+#include <editeng/editview.hxx>
 
 class EDITENG_DLLPUBLIC URLFieldHelper
 {
 public:
-    static void RemoveURLField(Outliner* pOutl, const OutlinerView* pOLV);
-    static bool IsCursorAtURLField(const OutlinerView* pOLV);
+    static void RemoveURLField(EditView& pEditView);
+    static bool IsCursorAtURLField(const EditView& pEditView);
 };
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */

sc/source/ui/drawfunc/drtxtob.cxx
@@ -326,8 +326,7 @@ void ScDrawTextObjectBar::Execute( SfxRequest &rReq )
         case SID_REMOVE_HYPERLINK:
             {
                 // Ensure the field is selected first
-                pOutView->SelectFieldAtCursor();
-                URLFieldHelper::RemoveURLField(pOutliner, pOutView);
+                URLFieldHelper::RemoveURLField(pOutView->GetEditView());
             }
             break;
 
@@ -412,8 +411,7 @@ void ScDrawTextObjectBar::GetState( SfxItemSet& rSet )
         || rSet.GetItemState(SID_REMOVE_HYPERLINK) != SfxItemState::UNKNOWN)
     {
         SdrView* pView = pViewData->GetScDrawView();
-        OutlinerView* pOutView = pView->GetTextEditOutlinerView();
-        if( !URLFieldHelper::IsCursorAtURLField(pOutView) )
+        if( !URLFieldHelper::IsCursorAtURLField(pView->GetTextEditOutlinerView()->GetEditView()) )
         {
             rSet.DisableItem( SID_OPEN_HYPERLINK );
             rSet.DisableItem( SID_EDIT_HYPERLINK );

sd/source/ui/view/drviews7.cxx
@@ -33,6 +33,7 @@
 #include <editeng/flditem.hxx>
 #include <editeng/outlobj.hxx>
 #include <editeng/sizeitem.hxx>
+#include <editeng/urlfieldhelper.hxx>
 #include <officecfg/Office/Impress.hxx>
 #include <svx/svxids.hrc>
 #include <svx/svdpagv.hxx>
@@ -1449,18 +1450,8 @@ void DrawViewShell::GetMenuState( SfxItemSet &rSet )
         if( mpDrawView->IsTextEdit() )
         {
             OutlinerView* pOLV = mpDrawView->GetTextEditOutlinerView();
-            if (pOLV)
-            {
-                const SvxFieldItem* pFieldItem = pOLV->GetFieldUnderMousePointer();
-                if (!pFieldItem)
-                    pFieldItem = pOLV->GetFieldAtSelection();
-                if (pFieldItem)
-                {
-                    const SvxFieldData* pField = pFieldItem->GetField();
-                    if (dynamic_cast<const SvxURLField*>(pField))
-                        bDisableEditHyperlink = false;
-                }
-            }
+            if (pOLV && URLFieldHelper::IsCursorAtURLField(pOLV->GetEditView()))
+                bDisableEditHyperlink = false;
         }
         else
         {

sd/source/ui/view/drviewsf.cxx
@@ -505,7 +505,7 @@ void DrawViewShell::GetAttrState( SfxItemSet& rSet )
 
             case SID_REMOVE_HYPERLINK:
             {
-                if (!URLFieldHelper::IsCursorAtURLField(mpDrawView->GetTextEditOutlinerView()))
+                if (!URLFieldHelper::IsCursorAtURLField(mpDrawView->GetTextEditOutlinerView()->GetEditView()))
                     rSet.DisableItem(nWhich);
             }
             break;

sw/source/uibase/shells/drwtxtex.cxx
@@ -536,10 +536,7 @@ void SwDrawTextShell::Execute( SfxRequest &rReq )
 
         case SID_REMOVE_HYPERLINK:
         {
-            // Ensure the field is selected first
-            pOLV->SelectFieldAtCursor();
-            URLFieldHelper::RemoveURLField(pSdrView->GetTextEditOutliner(),
-                                           pOLV);
+            URLFieldHelper::RemoveURLField(pOLV->GetEditView());
         }
         break;
 
@@ -979,7 +976,7 @@ void SwDrawTextShell::GetState(SfxItemSet& rSet)
             case SID_OPEN_HYPERLINK:
             case SID_COPY_HYPERLINK_LOCATION:
             {
-                if (!URLFieldHelper::IsCursorAtURLField(pOLV))
+                if (!URLFieldHelper::IsCursorAtURLField(pOLV->GetEditView()))
                     rSet.DisableItem(nWhich);
             }
             break;

editeng/source/misc/urlfieldhelper.cxx
@@ -11,27 +11,33 @@
 
 #include <editeng/flditem.hxx>
 #include <editeng/editview.hxx>
+#include <editeng/editeng.hxx>
 
-void URLFieldHelper::RemoveURLField(Outliner* pOutl, const OutlinerView* pOLV)
+void URLFieldHelper::RemoveURLField(EditView& pEditView)
 {
-    if (!pOutl || !pOLV)
-        return;
-
-    const SvxFieldData* pField = pOLV->GetFieldAtCursor();
+    pEditView.SelectFieldAtCursor();
+    const SvxFieldData* pField = pEditView.GetFieldAtCursor();
     if (auto pUrlField = dynamic_cast<const SvxURLField*>(pField))
     {
-        ESelection aSel = pOLV->GetSelection();
-        pOutl->QuickInsertText(pUrlField->GetRepresentation(), aSel);
-        pOLV->GetEditView().Invalidate();
+        ESelection aSel = pEditView.GetSelection();
+        pEditView.GetEditEngine()->QuickInsertText(pUrlField->GetRepresentation(), aSel);
+        pEditView.Invalidate();
     }
 }
 
-bool URLFieldHelper::IsCursorAtURLField(const OutlinerView* pOLV)
+bool URLFieldHelper::IsCursorAtURLField(const EditView& pEditView)
 {
-    if (!pOLV)
+    // tdf#128666 Make sure only URL field (or nothing) is selected
+    ESelection aSel = pEditView.GetSelection();
+    auto nSelectedParas = aSel.nEndPara - aSel.nStartPara;
+    auto nSelectedChars = aSel.nEndPos - aSel.nStartPos;
+    bool bIsValidSelection
+        = nSelectedParas == 0
+          && (nSelectedChars == 0 || nSelectedChars == 1 || nSelectedChars == -1);
+    if (!bIsValidSelection)
         return false;
 
-    const SvxFieldData* pField = pOLV->GetFieldAtCursor();
+    const SvxFieldData* pField = pEditView.GetFieldAtCursor();
     if (dynamic_cast<const SvxURLField*>(pField))
         return true;
 

sc/source/ui/view/editsh.cxx
@@ -33,6 +33,7 @@
 #include <editeng/flditem.hxx>
 #include <editeng/flstitem.hxx>
 #include <editeng/fontitem.hxx>
+#include <editeng/urlfieldhelper.hxx>
 #include <svx/hlnkitem.hxx>
 #include <vcl/EnumContext.hxx>
 #include <editeng/postitem.hxx>
@@ -646,15 +647,7 @@ void ScEditShell::Execute( SfxRequest& rReq )
         break;
         case SID_REMOVE_HYPERLINK:
             {
-                // Ensure the field is selected first
-                pEditView->SelectFieldAtCursor();
-                const SvxURLField* pURLField = GetURLField();
-                if (pURLField)
-                {
-                    ESelection aSel = pEditView->GetSelection();
-                    pEditView->GetEditEngine()->QuickInsertText(pURLField->GetRepresentation(), aSel);
-                }
-
+                URLFieldHelper::RemoveURLField(*pEditView);
             }
         break;
 
@@ -786,8 +779,8 @@ void ScEditShell::GetState( SfxItemSet& rSet )
             case SID_COPY_HYPERLINK_LOCATION:
             case SID_REMOVE_HYPERLINK:
                 {
-                    if ( !GetURLField() )
-                        rSet.DisableItem( nWhich );
+                    if (!URLFieldHelper::IsCursorAtURLField(*pEditView))
+                        rSet.DisableItem (nWhich);
                 }
                 break;
 

sd/source/ui/view/drviews2.cxx
@@ -1200,8 +1200,7 @@ void DrawViewShell::FuTemporary(SfxRequest& rReq)
                 OutlinerView* pOutView = mpDrawView->GetTextEditOutlinerView();
                 if (pOutView)
                     pOutView->SelectFieldAtCursor();
-                URLFieldHelper::RemoveURLField(mpDrawView->GetTextEditOutliner(),
-                                               mpDrawView->GetTextEditOutlinerView());
+                URLFieldHelper::RemoveURLField(pOutView->GetEditView());
             }
         }
         Cancel();

commit d9fc954fc48ce322389e1565e83a2637c7b93634

tdf#129712: check if outliner view pointer is valid to avoid crash
Regression after commit 64233bbaa0e91ddf14d59ae7547df6bbfa60adcc.

editenglo.dll!std::unique_ptr<EditView,std::default_delete<EditView>>::operator*()
	at C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.24.28314\include\memory(1886)
editenglo.dll!OutlinerView::GetEditView()
	at C:\lo\src\core\include\editeng\outliner.hxx(208)
sdlo.dll!sd::DrawViewShell::GetAttrState(SfxItemSet & rSet)
	at C:\lo\src\core\sd\source\ui\view\drviewsf.cxx(514)
sdlo.dll!SfxStubGraphicViewShellGetAttrState(SfxShell * pShell, SfxItemSet & rSet)
	at C:\lo\src\core\workdir\SdiTarget\sd\sdi\sdgslots.hxx(1524)
sfxlo.dll!SfxShell::CallState(void(*)(SfxShell *, SfxItemSet &) pFunc, SfxItemSet & rSet)
	at C:\lo\src\core\include\sfx2\shell.hxx(199)
sfxlo.dll!SfxDispatcher::FillState_(const SfxSlotServer & rSvr, SfxItemSet & rState, const SfxSlot * pRealSlot)
	at C:\lo\src\core\sfx2\source\control\dispatch.cxx(1672)
sfxlo.dll!SfxBindings::Update_Impl(SfxStateCache & rCache)
	at C:\lo\src\core\sfx2\source\control\bindings.cxx(270)
sfxlo.dll!SfxBindings::NextJob_Impl(const Timer * pTimer)
	at C:\lo\src\core\sfx2\source\control\bindings.cxx(1285)
sfxlo.dll!SfxBindings::NextJob(Timer * pTimer)
	at C:\lo\src\core\sfx2\source\control\bindings.cxx(1230)
sfxlo.dll!SfxBindings::LinkStubNextJob(void * instance, Timer * data)
	at C:\lo\src\core\sfx2\source\control\bindings.cxx(1227)
vcllo.dll!Link<Timer *,void>::Call(Timer * data)
	at C:\lo\src\core\include\tools\link.hxx(111)
vcllo.dll!Timer::Invoke()
	at C:\lo\src\core\vcl\source\app\timer.cxx(76)
vcllo.dll!Scheduler::ProcessTaskScheduling()
	at C:\lo\src\core\vcl\source\app\scheduler.cxx(478)
vcllo.dll!Scheduler::CallbackTaskScheduling()
	at C:\lo\src\core\vcl\source\app\scheduler.cxx(288)
vcllo.dll!SalTimer::CallCallback()
	at C:\lo\src\core\vcl\inc\saltimer.hxx(55)
vclplug_winlo.dll!WinSalTimer::ImplHandleElapsedTimer()

include/editeng/urlfieldhelper.hxx
@@ -19,6 +19,10 @@ class EDITENG_DLLPUBLIC URLFieldHelper
 public:
     static void RemoveURLField(EditView& pEditView);
     static bool IsCursorAtURLField(const EditView& pEditView);
+    static bool IsCursorAtURLField(const OutlinerView* pOLV)
+    {
+        return pOLV && IsCursorAtURLField(pOLV->GetEditView());
+    }
 };
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */

sc/source/ui/drawfunc/drtxtob.cxx
@@ -411,7 +411,7 @@ void ScDrawTextObjectBar::GetState( SfxItemSet& rSet )
         || rSet.GetItemState(SID_REMOVE_HYPERLINK) != SfxItemState::UNKNOWN)
     {
         SdrView* pView = pViewData->GetScDrawView();
-        if( !URLFieldHelper::IsCursorAtURLField(pView->GetTextEditOutlinerView()->GetEditView()) )
+        if( !URLFieldHelper::IsCursorAtURLField(pView->GetTextEditOutlinerView()) )
         {
             rSet.DisableItem( SID_OPEN_HYPERLINK );
             rSet.DisableItem( SID_EDIT_HYPERLINK );

sd/source/ui/view/drviews7.cxx
@@ -1449,8 +1449,7 @@ void DrawViewShell::GetMenuState( SfxItemSet &rSet )
     {
         if( mpDrawView->IsTextEdit() )
         {
-            OutlinerView* pOLV = mpDrawView->GetTextEditOutlinerView();
-            if (pOLV && URLFieldHelper::IsCursorAtURLField(pOLV->GetEditView()))
+            if (URLFieldHelper::IsCursorAtURLField(mpDrawView->GetTextEditOutlinerView()))
                 bDisableEditHyperlink = false;
         }
         else

sd/source/ui/view/drviewsf.cxx
@@ -511,7 +511,7 @@ void DrawViewShell::GetAttrState( SfxItemSet& rSet )
 
             case SID_REMOVE_HYPERLINK:
             {
-                if (!URLFieldHelper::IsCursorAtURLField(mpDrawView->GetTextEditOutlinerView()->GetEditView()))
+                if (!URLFieldHelper::IsCursorAtURLField(mpDrawView->GetTextEditOutlinerView()))
                     rSet.DisableItem(nWhich);
             }
             break;

sw/source/uibase/shells/drwtxtex.cxx
@@ -978,7 +978,7 @@ void SwDrawTextShell::GetState(SfxItemSet& rSet)
             case SID_OPEN_HYPERLINK:
             case SID_COPY_HYPERLINK_LOCATION:
             {
-                if (!URLFieldHelper::IsCursorAtURLField(pOLV->GetEditView()))
+                if (!URLFieldHelper::IsCursorAtURLField(pOLV))
                     rSet.DisableItem(nWhich);
             }
             break;