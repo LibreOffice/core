7a22100d618a0cb02d8e0392f7a8e3ec47ff322e
Serge Krot <Serge.Krot@cib.de>
2020-01-03 20:08:45 +0100

    tdf#129708 speed-up: check if we could create outline instead of creation of it
    Change-Id: I0629b4e6ccae4ab9be0142fe39c627f2f9a8f5ea

--- svx/source/accessibility/AccessibleShape.cxx
+++ svx/source/accessibility/AccessibleShape.cxx
@@ -162,7 +162,7 @@ void AccessibleShape::Init()
                 OutlinerParaObject* pOutlinerParaObject = nullptr;
 
                 if( pTextObj )
-                    pOutlinerParaObject = pTextObj->GetEditOutlinerParaObject().release(); // Get the OutlinerParaObject if text edit is active
+                    pOutlinerParaObject = pTextObj->CreateEditOutlinerParaObject().release(); // Get the OutlinerParaObject if text edit is active
 
                 bool bOwnParaObj = pOutlinerParaObject != nullptr;
 
