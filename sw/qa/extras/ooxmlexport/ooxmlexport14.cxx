cc8d2748e97562bca64f071ef856cdfe2e9d4d1e
László Németh <nemeth@numbertext.org>
2020-01-06 19:56:03 +0100

    tdf#77796 DOCX: import table style based cell padding
    Change-Id: Ib86ed45fe816d7e273539798cdebdbb95d575518

--- sw/qa/extras/ooxmlexport/ooxmlexport14.cxx
+++ sw/qa/extras/ooxmlexport/ooxmlexport14.cxx
@@ -259,6 +259,15 @@ DECLARE_OOXMLEXPORT_TEST(testTdf129353, "tdf129353.docx")
                          aIndexString);
 }
 
+DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testTdf77796, "tdf77796_zeropadding.docx")
+{
+    xmlDocPtr pXml = parseExport("word/document.xml");
+    CPPUNIT_ASSERT(pXml);
+    // zero cell padding from table style (it was 108)
+    assertXPath(pXml, "/w:document/w:body/w:tbl/w:tblPr/w:tblCellMar/w:start", "w", "5");
+    assertXPath(pXml, "/w:document/w:body/w:tbl/w:tblPr/w:tblCellMar/w:end", "w", "5");
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
