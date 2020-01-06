LibreOffice/core

commit 39ba5e3bde93358af1b363da8f850bdc96806cfa

tdf#116543: don't forget to commit registrations in DB field editor
This avoids revoking the uncommitted registration when any mailmerge
doc is destroyed.

Also don't forget to pass relevant SwDocShell when registering data
source. This allows to register the data source for current document.

Change-Id: Id89be82b0120661700e9fee6a075e5877d76e3b0
Reviewed-on: https://gerrit.libreoffice.org/56891
Tested-by: Jenkins
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
(cherry picked from commit 54d76f5203aa5f3ef0e5afdca9d131dadeb3794e)
Reviewed-on: https://gerrit.libreoffice.org/57005
Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>

sw/source/ui/fldui/flddb.cxx
@@ -24,6 +24,7 @@
 #include <dbfld.hxx>
 #include <fldtdlg.hxx>
 #include <numrule.hxx>
+#include <doc.hxx>
 
 #include "flddb.hxx"
 #include <dbconfig.hxx>
@@ -78,6 +79,14 @@ SwFieldDBPage::~SwFieldDBPage()
 
 void SwFieldDBPage::dispose()
 {
+    SwWrtShell* pSh = GetWrtShell();
+    if (!pSh)
+        pSh = ::GetActiveWrtShell();
+    // This would cleanup in the case of cancelled dialog
+    SwDBManager* pDbManager = pSh->GetDoc()->GetDBManager();
+    if (pDbManager)
+        pDbManager->RevokeLastRegistrations();
+
     m_pTypeLB.clear();
     m_pDatabaseTLB.clear();
     m_pAddDBPB.clear();
@@ -207,6 +216,10 @@ bool SwFieldDBPage::FillItemSet(SfxItemSet* )
     SwWrtShell *pSh = GetWrtShell();
     if(!pSh)
         pSh = ::GetActiveWrtShell();
+
+    SwDBManager* pDbManager = pSh->GetDoc()->GetDBManager();
+    if (pDbManager)
+        pDbManager->CommitLastRegistrations();
 
     if (aData.sDataSource.isEmpty())
         aData = pSh->GetDBData();
@@ -477,7 +490,12 @@ IMPL_LINK( SwFieldDBPage, TreeSelectHdl, SvTreeListBox *, pBox, void )
 
 IMPL_LINK_NOARG(SwFieldDBPage, AddDBHdl, Button*, void)
 {
-    OUString sNewDB = SwDBManager::LoadAndRegisterDataSource(GetFrameWeld());
+    SwWrtShell* pSh = GetWrtShell();
+    if (!pSh)
+        pSh = ::GetActiveWrtShell();
+
+    OUString sNewDB
+        = SwDBManager::LoadAndRegisterDataSource(GetFrameWeld(), pSh->GetDoc()->GetDocShell());
     if(!sNewDB.isEmpty())
     {
         m_pDatabaseTLB->AddDataSource(sNewDB);

commit f1a9cf223cf67d96178f290a47b474cc8a18b161

tdf#129798: store SwWrtShell when it's available
GetActiveWrtShell may return nullptr when attaching a new component
to current frame, e.g. destroying on print preview. OTOH SwWrtShell
is passed to SwFieldDBPage::SetWrtShell on creation, so store it to
own base class then.

Possibly that would be enough, and we could remove all the checks
in other methods - but I don't know the code well enough to be sure
that SwFieldDlg::PageCreated and SwFieldEditDlg::CreatePage will
always set SwWrtShell; to be on the safe side, I kept the checks
where SwWrtShell is used, and stored it in those places if not yet
stored.

Regression after commit 39ba5e3bde93358af1b363da8f850bdc96806cfa.

Change-Id: I21f3c8ef24764e98b1bc287db3c6485ce0b3ab48

sw/source/ui/fldui/flddb.cxx
@@ -70,13 +70,14 @@ SwFieldDBPage::SwFieldDBPage(weld::Container* pPage, weld::DialogController* pCo
 
 SwFieldDBPage::~SwFieldDBPage()
 {
-    SwWrtShell* pSh = GetWrtShell();
-    if (!pSh)
-        pSh = ::GetActiveWrtShell();
-    // This would cleanup in the case of cancelled dialog
-    SwDBManager* pDbManager = pSh->GetDoc()->GetDBManager();
-    if (pDbManager)
-        pDbManager->RevokeLastRegistrations();
+    // If we have no stored SwWrtShell, it means we didn't do anything useful - no need to revoke.
+    if (SwWrtShell* pSh = GetWrtShell())
+    {
+        // This would cleanup in the case of cancelled dialog
+        SwDBManager* pDbManager = pSh->GetDoc()->GetDBManager();
+        if (pDbManager)
+            pDbManager->RevokeLastRegistrations();
+    }
 }
 
 // initialise TabPage
@@ -137,9 +138,7 @@ void SwFieldDBPage::Reset(const SfxItemSet*)
         }
         else
         {
-            SwWrtShell *pSh = GetWrtShell();
-            if(!pSh)
-                pSh = ::GetActiveWrtShell();
+            SwWrtShell *pSh = CheckAndGetWrtShell();
             if(pSh)
             {
                 SwDBData aTmp(pSh->GetDBData());
@@ -192,9 +191,8 @@ bool SwFieldDBPage::FillItemSet(SfxItemSet* )
     aData.sDataSource = m_xDatabaseTLB->GetDBName(sTableName, sColumnName, &bIsTable);
     aData.sCommand = sTableName;
     aData.nCommandType = bIsTable ? 0 : 1;
-    SwWrtShell *pSh = GetWrtShell();
-    if(!pSh)
-        pSh = ::GetActiveWrtShell();
+    SwWrtShell *pSh = CheckAndGetWrtShell();
+    assert(pSh);
 
     SwDBManager* pDbManager = pSh->GetDoc()->GetDBManager();
     if (pDbManager)
@@ -288,9 +286,8 @@ void SwFieldDBPage::TypeHdl(const weld::TreeView* pBox)
     if (nOld == GetTypeSel())
         return;
 
-    SwWrtShell *pSh = GetWrtShell();
-    if(!pSh)
-        pSh = ::GetActiveWrtShell();
+    SwWrtShell *pSh = CheckAndGetWrtShell();
+    assert(pSh);
     bool bCond = false, bSetNo = false, bFormat = false, bDBFormat = false;
     const SwFieldTypesEnum nTypeId = static_cast<SwFieldTypesEnum>(m_xTypeLB->get_id(GetTypeSel()).toUInt32());
 
@@ -483,15 +480,14 @@ IMPL_LINK(SwFieldDBPage, TreeSelectHdl, weld::TreeView&, rBox, void)
 
 IMPL_LINK_NOARG(SwFieldDBPage, AddDBHdl, weld::Button&, void)
 {
-    SwWrtShell* pSh = GetWrtShell();
-    if (!pSh)
-        pSh = ::GetActiveWrtShell();
-
-    OUString sNewDB
-        = SwDBManager::LoadAndRegisterDataSource(GetFrameWeld(), pSh->GetDoc()->GetDocShell());
-    if(!sNewDB.isEmpty())
+    if (SwWrtShell* pSh = CheckAndGetWrtShell())
     {
-        m_xDatabaseTLB->AddDataSource(sNewDB);
+        OUString sNewDB
+            = SwDBManager::LoadAndRegisterDataSource(GetFrameWeld(), pSh->GetDoc()->GetDocShell());
+        if (!sNewDB.isEmpty())
+        {
+            m_xDatabaseTLB->AddDataSource(sNewDB);
+        }
     }
 }
 
@@ -519,7 +515,21 @@ void SwFieldDBPage::ActivateMailMergeAddress()
 
 void SwFieldDBPage::SetWrtShell(SwWrtShell& rSh)
 {
+    // We need to remember the shell to be able to call correct SwDBManager
+    SwFieldPage::SetWrtShell(&rSh);
     m_xDatabaseTLB->SetWrtShell(rSh);
+}
+
+SwWrtShell* SwFieldDBPage::CheckAndGetWrtShell()
+{
+    SwWrtShell* pSh = GetWrtShell();
+    if (!pSh)
+    {
+        pSh = ::GetActiveWrtShell();
+        if (pSh) // this is not guaranteed: e.g., activating print preview with dialog active
+            SetWrtShell(*pSh);
+    }
+    return pSh;
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sw/source/ui/fldui/flddb.hxx
@@ -56,6 +56,7 @@ class SwFieldDBPage : public SwFieldPage
     void                CheckInsert();
 
     using SwFieldPage::SetWrtShell;
+    SwWrtShell* CheckAndGetWrtShell();
 
 protected:
     virtual sal_uInt16      GetGroup() override;