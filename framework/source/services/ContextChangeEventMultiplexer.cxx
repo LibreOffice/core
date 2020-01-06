LibreOffice/core

commit d05b7b32d9ecb6fcb4a268eb68cdcee09bafa6dd

Notebookbar:Context for printpreview is now working
Also the context stuff is now working well even
after print preview is set

Change-Id: Ia43f512394cf4d162b4019257c039aecb664df22
Reviewed-on: https://gerrit.libreoffice.org/56740
Tested-by: Jenkins
Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>

include/vcl/notebookbar.hxx
@@ -41,10 +41,13 @@ public:
 
     void DataChanged(const DataChangedEvent& rDCEvt) override;
 
+    void ControlListener(bool bListen);
+
 private:
     VclPtr<SystemWindow> m_pSystemWindow;
     css::uno::Reference<css::ui::XContextChangeEventListener> m_pEventListener;
     std::vector<NotebookbarContextControl*> m_pContextContainers;
+    css::uno::Reference<css::frame::XFrame> mxFrame;
 
     AllSettings DefaultSettings;
     AllSettings PersonaSettings;

sw/source/uibase/uiview/pview.cxx
@@ -78,6 +78,8 @@
 #include <svx/dialogs.hrc>
 
 #include <memory>
+#include <vcl/EnumContext.hxx>
+#include <vcl/notebookbar.hxx>
 
 using namespace ::com::sun::star;
 SFX_IMPL_NAMED_VIEWFACTORY(SwPagePreview, "PrintPreview")
@@ -1157,6 +1159,13 @@ SwPagePreview::SwPagePreview(SfxViewFrame *pViewFrame, SfxViewShell* pOldSh):
     CreateScrollbar( true );
     CreateScrollbar( false );
 
+    //notify notebookbar change in context
+    SfxShell::SetContextBroadcasterEnabled(true);
+    SfxShell::SetContextName(vcl::EnumContext::GetContextName(vcl::EnumContext::Context::Printpreview));
+    SfxShell::BroadcastContextForActivation(true);
+    //removelisteners for notebookbar
+    SfxViewFrame::Current()->GetWindow().GetSystemWindow()->GetNotebookBar()->ControlListener(true);
+
     SfxObjectShell* pObjShell = pViewFrame->GetObjectShell();
     if ( !pOldSh )
     {
@@ -1221,7 +1230,7 @@ SwPagePreview::~SwPagePreview()
     delete pVShell;
 
     m_pViewWin.disposeAndClear();
-
+    SfxViewFrame::Current()->GetWindow().GetSystemWindow()->GetNotebookBar()->ControlListener(false);
     m_pScrollFill.disposeAndClear();
     m_pHScrollbar.disposeAndClear();
     m_pVScrollbar.disposeAndClear();

vcl/source/control/notebookbar.cxx
@@ -14,7 +14,7 @@
 #include <cppuhelper/queryinterface.hxx>
 #include <cppuhelper/implbase.hxx>
 #include <vcl/vclevent.hxx>
-
+#include <com/sun/star/ui/ContextChangeEventMultiplexer.hpp>
 /**
  * split from the main class since it needs different ref-counting mana
  */
@@ -37,7 +37,7 @@ NotebookBar::NotebookBar(Window* pParent, const OString& rID, const OUString& rU
 {
     SetStyle(GetStyle() | WB_DIALOGCONTROL);
     m_pUIBuilder.reset( new VclBuilder(this, getUIRootDir(), rUIXMLDescription, rID, rFrame) );
-
+    mxFrame = rFrame;
     // In the Notebookbar's .ui file must exist control handling context
     // - implementing NotebookbarContextControl interface with id "ContextContainer"
     // or "ContextContainerX" where X is a number >= 1
@@ -154,6 +154,23 @@ void SAL_CALL NotebookBarContextChangeEventListener::notifyContextChangeEvent(co
     }
 }
 
+void NotebookBar::ControlListener(bool bListen)
+{
+    if(bListen)
+    {
+        // remove listeners
+        css::uno::Reference<css::ui::XContextChangeEventMultiplexer> xMultiplexer (css::ui::ContextChangeEventMultiplexer::get(
+                ::comphelper::getProcessComponentContext()));
+        xMultiplexer->removeContextChangeEventListener(getContextChangeEventListener(),mxFrame->getController());
+    }
+    else
+    {
+        // add listeners
+        css::uno::Reference<css::ui::XContextChangeEventMultiplexer> xMultiplexer (css::ui::ContextChangeEventMultiplexer::get(
+                ::comphelper::getProcessComponentContext()));
+        xMultiplexer->addContextChangeEventListener(getContextChangeEventListener(),mxFrame->getController());
+    }
+}
 
 void SAL_CALL NotebookBarContextChangeEventListener::disposing(const ::css::lang::EventObject&)
 {

include/vcl/EnumContext.hxx
@@ -96,7 +96,7 @@ public:
         Text,
         TextObject,
         Trendline,
-
+        Printpreview,
         // Default context of an application.  Do we need this?
         Default,
 

vcl/source/window/EnumContext.cxx
@@ -194,6 +194,7 @@ void EnumContext::ProvideContextContainers()
     AddEntry("Text", Context::Text);
     AddEntry("TextObject", Context::TextObject);
     AddEntry("Trendline", Context::Trendline);
+    AddEntry("Printpreview", Context::Printpreview);
 
 }
 

commit 7d971ac62f967417dbbdcf31ca2648ce8da56374

Don't crash on exiting print preview with Notebookbar
Crash caused by this sequence (tested in Writer):

1. Closing print preview, frame is attached to controller;
2. This calls SfxNotebookBar::StateMethod
3. There notebookbar's listener is added to list of the controller's
   context change event listeners
4. Then in SwPagePreview::~SwPagePreview, notebookbar's listener is
   added to that list again
5. ContextChangeEventMultiplexer::addContextChangeEventListener
   detects second addition, and throws an unhandled exception.

I don't know why starting listening is needed in SwPagePreview dtor;
unfortunately commit d05b7b32d9ecb6fcb4a268eb68cdcee09bafa6dd doesn't
say much about context and reasons.

ControlListener is renamed to ControlListenerForCurrentController to
emphasize that it operates on the current controller of notebookbar's
frame; and its bListen parameter meaning was reverted: previously its
true value awkwardly meant "stop listening". All operations with the
listener of notebookbar are replaced with calls to this method.

In ContextChangeEventMultiplexer::addContextChangeEventListener,
uno::UNO_QUERY_THROW was replaced with uno::UNO_QUERY, because not
only chart controller may appear here, and it's not an error:
SfxBaseController doesn't implement lang::XServiceInfo.

Regression after commit d05b7b32d9ecb6fcb4a268eb68cdcee09bafa6dd.

Change-Id: Ief1aed188d8f02a6cfe3ea25f4d082dfdf449f32

framework/source/services/ContextChangeEventMultiplexer.cxx
@@ -166,7 +166,7 @@ void SAL_CALL ContextChangeEventMultiplexer::addContextChangeEventListener (
         if (pFocusDescriptor->msCurrentApplicationName.isEmpty() && pFocusDescriptor->msCurrentContextName.isEmpty()
                 && rxEventFocus.is())
         {
-            Reference< lang::XServiceInfo > xServInfo( rxEventFocus, uno::UNO_QUERY_THROW );
+            Reference< lang::XServiceInfo > xServInfo( rxEventFocus, uno::UNO_QUERY );
             if( xServInfo.is() && xServInfo->getImplementationName() == "com.sun.star.comp.chart2.ChartController")
             {
                 css::ui::ContextChangeEventObject aEvent (

include/vcl/notebookbar.hxx
@@ -39,17 +39,16 @@ public:
 
     void SetSystemWindow(SystemWindow* pSystemWindow);
 
-    const css::uno::Reference<css::ui::XContextChangeEventListener>& getContextChangeEventListener() const { return m_pEventListener; }
-
     void StateChanged(const StateChangedType nStateChange ) override;
 
     void DataChanged(const DataChangedEvent& rDCEvt) override;
 
-    void ControlListener(bool bListen);
+    void ControlListenerForCurrentController(bool bListen);
 
 private:
     VclPtr<SystemWindow> m_pSystemWindow;
     css::uno::Reference<css::ui::XContextChangeEventListener> m_pEventListener;
+    std::set<css::uno::Reference<css::frame::XController>> m_alisteningControllers;
     std::vector<NotebookbarContextControl*> m_pContextContainers;
     css::uno::Reference<css::frame::XFrame> mxFrame;
 

sc/source/ui/view/prevwsh.cxx
@@ -160,7 +160,7 @@ ScPreviewShell::ScPreviewShell( SfxViewFrame* pViewFrame,
 
     auto& pNotebookBar = pViewFrame->GetWindow().GetSystemWindow()->GetNotebookBar();
     if (pNotebookBar)
-        pNotebookBar->ControlListener(true);
+        pNotebookBar->ControlListenerForCurrentController(false); // stop listening
 
     if ( auto pTabViewShell = dynamic_cast<ScTabViewShell*>( pOldSh) )
     {
@@ -189,7 +189,7 @@ ScPreviewShell::~ScPreviewShell()
         mpFrameWindow->SetCloseHdl(Link<SystemWindow&,void>()); // Remove close handler.
 
     if (auto& pBar = GetViewFrame()->GetWindow().GetSystemWindow()->GetNotebookBar())
-        pBar->ControlListener(false);
+        pBar->ControlListenerForCurrentController(true); // let it start listening now
 
     // #108333#; notify Accessibility that Shell is dying and before destroy all
     BroadcastAccessibility( SfxHint( SfxHintId::Dying ) );

sfx2/source/notebookbar/SfxNotebookBar.cxx
@@ -400,15 +400,7 @@ bool SfxNotebookBar::StateMethod(SystemWindow* pSysWindow,
 
             if(pView)
             {
-                Reference<XContextChangeEventMultiplexer> xMultiplexer
-                            = ContextChangeEventMultiplexer::get( xContext );
-
-                if(xFrame.is())
-                {
-                    xMultiplexer->addContextChangeEventListener(
-                                        pNotebookBar->getContextChangeEventListener(),
-                                        xFrame->getController());
-                }
+                pNotebookBar->ControlListenerForCurrentController(true);
             }
         }
 
@@ -430,10 +422,9 @@ void SfxNotebookBar::RemoveListeners(SystemWindow const * pSysWindow)
                         = ContextChangeEventMultiplexer::get(
                                 ::comphelper::getProcessComponentContext());
 
-    if (pSysWindow->GetNotebookBar())
+    if (auto pNotebookBar = pSysWindow->GetNotebookBar())
     {
-        xMultiplexer->removeAllContextChangeEventListeners(
-                           pSysWindow->GetNotebookBar()->getContextChangeEventListener());
+        pNotebookBar->ControlListenerForCurrentController(false);
     }
 }
 

sw/source/uibase/uiview/pview.cxx
@@ -1165,7 +1165,7 @@ SwPagePreview::SwPagePreview(SfxViewFrame *pViewFrame, SfxViewShell* pOldSh):
     SfxShell::BroadcastContextForActivation(true);
     //removelisteners for notebookbar
     if (auto& pBar = SfxViewFrame::Current()->GetWindow().GetSystemWindow()->GetNotebookBar())
-        pBar->ControlListener(true);
+        pBar->ControlListenerForCurrentController(false);
 
     SfxObjectShell* pObjShell = pViewFrame->GetObjectShell();
     if ( !pOldSh )
@@ -1233,7 +1233,7 @@ SwPagePreview::~SwPagePreview()
     m_pViewWin.disposeAndClear();
     if (SfxViewFrame* pCurrent = SfxViewFrame::Current())
         if (auto& pBar = pCurrent->GetWindow().GetSystemWindow()->GetNotebookBar())
-            pBar->ControlListener(false);
+            pBar->ControlListenerForCurrentController(true); // start listening now
     m_pScrollFill.disposeAndClear();
     m_pHScrollbar.disposeAndClear();
     m_pVScrollbar.disposeAndClear();

vcl/source/control/notebookbar.cxx
@@ -97,6 +97,15 @@ void NotebookBar::dispose()
         m_pSystemWindow->GetTaskPaneList()->RemoveWindow(this);
     m_pSystemWindow.clear();
     disposeBuilder();
+    {
+        auto xMultiplexer(
+            css::ui::ContextChangeEventMultiplexer::get(comphelper::getProcessComponentContext()));
+        for (auto& xController : m_alisteningControllers)
+        {
+            xMultiplexer->removeContextChangeEventListener(m_pEventListener, xController);
+        }
+        m_alisteningControllers.clear();
+    }
     m_pEventListener.clear();
     Control::dispose();
 }
@@ -180,21 +189,30 @@ void SAL_CALL NotebookBarContextChangeEventListener::notifyContextChangeEvent(co
     }
 }
 
-void NotebookBar::ControlListener(bool bListen)
+void NotebookBar::ControlListenerForCurrentController(bool bListen)
 {
+    auto xController = mxFrame->getController();
     if(bListen)
     {
-        // remove listeners
-        css::uno::Reference<css::ui::XContextChangeEventMultiplexer> xMultiplexer (css::ui::ContextChangeEventMultiplexer::get(
+        // add listeners
+        if (m_alisteningControllers.count(xController) == 0)
+        {
+            auto xMultiplexer(css::ui::ContextChangeEventMultiplexer::get(
                 ::comphelper::getProcessComponentContext()));
-        xMultiplexer->removeContextChangeEventListener(getContextChangeEventListener(),mxFrame->getController());
+            xMultiplexer->addContextChangeEventListener(m_pEventListener, xController);
+            m_alisteningControllers.insert(xController);
+        }
     }
     else
     {
-        // add listeners
-        css::uno::Reference<css::ui::XContextChangeEventMultiplexer> xMultiplexer (css::ui::ContextChangeEventMultiplexer::get(
+        // remove listeners
+        if (m_alisteningControllers.count(xController))
+        {
+            auto xMultiplexer(css::ui::ContextChangeEventMultiplexer::get(
                 ::comphelper::getProcessComponentContext()));
-        xMultiplexer->addContextChangeEventListener(getContextChangeEventListener(),mxFrame->getController());
+            xMultiplexer->removeContextChangeEventListener(m_pEventListener, xController);
+            m_alisteningControllers.erase(xController);
+        }
     }
 }
 