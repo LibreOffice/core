d4f647f7d150dfbf81da8614a0e8d555b59ea9f4
Yusuf Keten <ketenyusuf@gmail.com>
2020-01-07 00:39:19 +0300

    tdf#74702 Remove OutDevType from OutputDevice::RemoveTransparenciesFromMetaFile
    Extracted SetBackgroundComponentBounds() which removes need for checking OutDevType especially whether it is Printer or not in OutputDevice.
    
    Change-Id: Ib8884b861314f595283af9ae3a2cd5645b334d51

--- vcl/source/gdi/print.cxx
+++ vcl/source/gdi/print.cxx
@@ -316,6 +316,13 @@ void Printer::CopyArea( const Point& /*rDestPt*/,
     SAL_WARN( "vcl.gdi", "Don't use OutputDevice::CopyArea(...) with printer devices!" );
 }
 
+tools::Rectangle Printer::SetBackgroundComponentBounds()
+{
+    Point aPageOffset = Point( 0, 0 ) - this->GetPageOffsetPixel();
+    Size aSize  = this->GetPaperSizePixel();
+    return tools::Rectangle( aPageOffset, aSize );
+}
+
 void Printer::SetPrinterOptions( const PrinterOptions& i_rOptions )
 {
     *mpPrinterOptions = i_rOptions;
