4948dfe4634f86b1f5caefc543f9010b31ab25b9
Stephan Bergmann <sbergman@redhat.com>
2020-01-08 11:42:59 +0100

    Missing include (Windows --disable-pch; for std::min)
    Change-Id: Ie6951257a94ae004e1a51f46e908917a07d137f9

--- sw/source/filter/ww8/WW8Sttbf.cxx
+++ sw/source/filter/ww8/WW8Sttbf.cxx
@@ -17,6 +17,9 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
+#include <sal/config.h>
+
+#include <algorithm>
 #include <iostream>
 #include "WW8Sttbf.hxx"
 #include <osl/endian.h>
