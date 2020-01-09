822bbe6984a642ea3c09ca8a337499655cd54173
Xisco Fauli <xiscofauli@libreoffice.org>
2020-01-09 17:24:41 +0100

    tdf#115026: Add uitest
    Change-Id: Iaac2a1065c0c1c28e56db8a121cb28453e27d529
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/86497
    Tested-by: Jenkins
    Reviewed-by: Xisco Faulí <xiscofauli@libreoffice.org>
    (cherry picked from commit 43d8dc34d74832e928c2cc215e9bf512f4edf3b3)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/86504

---
+++ sw/qa/uitest/table/tdf115026.py
@@ -0,0 +1,38 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+from uitest.framework import UITestCase
+from uitest.path import get_srcdir_url
+from libreoffice.uno.propertyvalue import mkPropertyValues
+from uitest.uihelper.common import get_state_as_dict
+
+class tdf115026(UITestCase):
+
+    def test_pageBreak_and_tableAutoFormat(self):
+        self.ui_test.create_doc_in_start_center("writer")
+        xWriterDoc = self.xUITest.getTopFocusWindow()
+        xWriterEdit = xWriterDoc.getChild("writer_edit")
+
+        self.xUITest.executeCommand(".uno:InsertPagebreak")
+        self.assertEqual(get_state_as_dict(xWriterEdit)["CurrentPage"], "2")
+
+        self.ui_test.execute_dialog_through_command(".uno:InsertTable")
+        xDialog = self.xUITest.getTopFocusWindow()
+
+        xOkBtn = xDialog.getChild("ok")
+        self.ui_test.close_dialog_through_button(xOkBtn)
+
+        self.ui_test.execute_dialog_through_command(".uno:AutoFormat")
+        xDialog = self.xUITest.getTopFocusWindow()
+
+        xOkBtn = xDialog.getChild("ok")
+        self.ui_test.close_dialog_through_button(xOkBtn)
+
+        self.assertEqual(get_state_as_dict(xWriterEdit)["CurrentPage"], "2")
+
+        self.ui_test.close_doc()
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
