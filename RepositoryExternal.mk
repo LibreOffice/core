6b026ae15f58ead0db70accd87357246df442614
Michael Stahl <Michael.Stahl@cib.de>
2020-01-09 15:06:07 +0100

    python3: bundle libffi for GNU/Linux builds
    CPython commit f40d4ddff3c800b3c956a5e8820aabe3aa87cddd "Closes #27979:
    Remove bundled copy of libffi" causes a bit of a problem because it
    turns out that libffi isn't all that stable; there's libffi.so.5 on
    CentOS 6, libffi.so.6 on CentOS 7 and libffi.so.7 on
    lo_daily_update_gandalf tinderbox.
    
    So we have to bundle it in LO; it's only used on GNU/Linux currently.
    
    CPython commit 32119e10b792ad7ee4e5f951a2d89ddbaf111cc5 "bpo-35947:
    Update Windows to the current version of libffi (GH-11797)" also removes
    the libffi for MSVC, so in a future python upgrade we will have to build
    libffi for MSVC too.
    
    The libffi fork for MacOSX is still in CPython git master.
    
    (regression from b10be5d48433076f0b7238d818020f708553e114)
    
    Change-Id: Ibc20cf8cd3614cf9941b6970662bd930496776b2
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/86493
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@cib.de>
    (cherry picked from commit 79084665f0e351a3f83fdee88071919f05ec9cc3)

--- RepositoryExternal.mk
+++ RepositoryExternal.mk
@@ -3169,6 +3169,12 @@ endif
 
 endef
 
+# this is only used by python currently
+define gb_ExternalProject__use_libffi
+$(call gb_ExternalProject_use_external_project,$(1),libffi)
+
+endef
+
 endif # SYSTEM_PYTHON
 
 # ORCUS
