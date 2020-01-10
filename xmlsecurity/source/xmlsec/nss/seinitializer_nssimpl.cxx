4a372929c82e894601ab243a507061e1cfc6a59d
Mesut Çifci <mesutcifci97@gmail.com>
2020-01-10 17:06:33 +0300

    tdf#88205 Adapt uses of css::uno::Sequence to use initializer_list ctor
    Change-Id: I7342275928e0231062f378942c567fe993b2a271
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/86544
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

--- xmlsecurity/source/xmlsec/nss/seinitializer_nssimpl.cxx
+++ xmlsecurity/source/xmlsec/nss/seinitializer_nssimpl.cxx
@@ -100,8 +100,7 @@ sal_Bool SAL_CALL SEInitializer_NssImpl::supportsService( const OUString& rServi
 }
 uno::Sequence< OUString > SAL_CALL SEInitializer_NssImpl::getSupportedServiceNames(  )
 {
-    uno::Sequence<OUString> seqServiceNames{ "com.sun.star.xml.crypto.SEInitializer" };
-    return seqServiceNames;
+    return { "com.sun.star.xml.crypto.SEInitializer" };
 }
 
 namespace {
@@ -128,8 +127,7 @@ OUString NSSInitializer_NssImpl::getImplementationName()
 
 uno::Sequence<OUString> SAL_CALL NSSInitializer_NssImpl::getSupportedServiceNames()
 {
-    uno::Sequence<OUString> seqServiceNames{ "com.sun.star.xml.crypto.NSSInitializer" };
-    return seqServiceNames;
+    return { "com.sun.star.xml.crypto.NSSInitializer" };
 }
 
 extern "C" SAL_DLLPUBLIC_EXPORT uno::XInterface*
