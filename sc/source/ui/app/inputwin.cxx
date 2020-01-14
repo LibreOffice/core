de2a7a66658e283021725eeaeb0961e2962151f9
Marco Cecchetti <marco.cecchetti@collabora.com>
2020-01-14 14:35:11 +0100

    lok: calc: send text selection rectangles for the input bar
    Change-Id: I3caeaddd7a5ddc6e496650539b88e2fd81b37e6e
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/86790
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Jan Holesovsky <kendy@collabora.com>

--- sc/source/ui/app/inputwin.cxx
+++ sc/source/ui/app/inputwin.cxx
@@ -1334,6 +1334,9 @@ void ScTextWnd::InitEditEngine()
     mpEditView = o3tl::make_unique<EditView>(mpEditEngine.get(), this);
     mpEditView->SetInsertMode(bIsInsertMode);
 
+    if (comphelper::LibreOfficeKit::isActive())
+        mpEditView->RegisterViewShell(mpViewShell);
+
     // Text from Clipboard is taken over as ASCII in a single row
     EVControlBits n = mpEditView->GetControlWord();
     mpEditView->SetControlWord( n | EVControlBits::SINGLELINEPASTE );
@@ -1855,6 +1858,8 @@ void ScTextWnd::MakeDialogEditView()
     mpEditEngine->SetUpdateMode( true );
 
     mpEditView = o3tl::make_unique<EditView>(mpEditEngine.get(), this);
+    if (comphelper::LibreOfficeKit::isActive())
+        mpEditView->RegisterViewShell(mpViewShell);
     mpEditEngine->InsertView( mpEditView.get(), EE_APPEND );
 
     Resize();
