4950b639c5c85557441cb8b69548743db2ca530d
Caolán McNamara <caolanm@redhat.com>
2020-01-16 12:34:17 +0000

    drop some unneeded includes
    Change-Id: I312acdd1e893e24c78df76d663e306ba6aa54cd7

--- sw/source/uibase/inc/navipi.hxx
+++ sw/source/uibase/inc/navipi.hxx
@@ -19,7 +19,6 @@
 #ifndef INCLUDED_SW_SOURCE_UIBASE_INC_NAVIPI_HXX
 #define INCLUDED_SW_SOURCE_UIBASE_INC_NAVIPI_HXX
 
-#include <vcl/layout.hxx>
 #include <vcl/lstbox.hxx>
 #include <vcl/toolbox.hxx>
 #include <vcl/idle.hxx>
