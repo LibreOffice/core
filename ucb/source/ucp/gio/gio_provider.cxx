7efd51cef00fbfe12c957ee62bd489eb0cb06e68
Michael Meeks <michael.meeks@collabora.com>
2020-01-20 19:34:34 +0000

    lok: disable ucbgio provider for Online.
    Of course, some lok apps may want the provider, but it creates
    unwanted threads causing problems with forkit in some cases.
    
    Change-Id: I7d19d629b3657780b52c3c753a4c8dcba6ae86b0

--- ucb/source/ucp/gio/gio_provider.cxx
+++ ucb/source/ucp/gio/gio_provider.cxx
@@ -120,10 +120,24 @@ ONE_INSTANCE_SERVICE_FACTORY_IMPL( ContentProvider );
 
 }
 
+// gio creates threads we don't want in online's forkit
+static bool isDisabled()
+{
+    const char *pDisable = getenv("UNODISABLELIBRARY");
+    if (!pDisable)
+        return false;
+    OString aDisable(pDisable, strlen(pDisable));
+    return aDisable.indexOf("ucpgio1") >= 0;
+}
+
 extern "C" SAL_DLLPUBLIC_EXPORT void * ucpgio1_component_getFactory( const sal_Char *pImplName,
     void *pServiceManager, void * )
 {
     void * pRet = nullptr;
+
+    static bool bDisabled = isDisabled();
+    if (bDisabled)
+        return nullptr;
 
     uno::Reference< lang::XMultiServiceFactory > xSMgr
         (static_cast< lang::XMultiServiceFactory * >( pServiceManager ) );
