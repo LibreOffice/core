bee324574e602a2be27ebfdbea188e2fd79b3fd5
Julien Nabet <serval2412@yahoo.fr>
2020-01-20 19:40:52 +0100

    tdf#129412: Crash when registered data source is not available
    See bt here:
    https://bugs.documentfoundation.org/show_bug.cgi?id=129412#c4
    
    Regression from:
    https://cgit.freedesktop.org/libreoffice/core/commit/?id=9009663deb8f0862f419fd99bf0b761c7f923eff
    
    author	Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>	2017-02-26 22:48:06 +0100
    committer	Tomaž Vajngerl <quikee@gmail.com>	2017-04-04 13:39:29 +0000
    commit	9009663deb8f0862f419fd99bf0b761c7f923eff (patch)
    tree	ea25976de0919f9d2161037d83be0eace4c1070b
    parent	1931b5b01c6fdaa204d26ec4b9675dad16373cf2 (diff)
    tdf#83257 [API-CHANGE] Pivot chart implementation
    
    Change-Id: Ib4de665f6e5380baf51589c92473ad88e8c9b84e
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/87096
    Tested-by: Jenkins
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>
    Signed-off-by: xisco <xiscofauli@libreoffice.org>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/87125
    (cherry picked from commit d6f34d4a84ac43889d0bf239e22d18ddcd874ba7)

--- sc/source/ui/unoobj/PivotTableDataProvider.cxx
+++ sc/source/ui/unoobj/PivotTableDataProvider.cxx
@@ -297,6 +297,8 @@ void PivotTableDataProvider::collectPivotTableData()
     m_aFieldOutputDescriptionMap.clear();
 
     uno::Reference<sheet::XDataPilotResults> xDPResults(pDPObject->GetSource(), uno::UNO_QUERY);
+    if (!xDPResults.is())
+        return;
     uno::Sequence<uno::Sequence<sheet::DataResult>> xDataResultsSequence = xDPResults->getResults();
 
     double fNan;
