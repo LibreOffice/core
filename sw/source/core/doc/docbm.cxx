2fb5100662edfa61824350f65f381e9f80c33b13
Michael Stahl <Michael.Stahl@cib.de>
2020-01-21 13:15:50 +0100

    tdf#45589 sw: invalidate on bookmark insertion/deletion
    Invalidate the text frames when a bookmark is inserted or deleted; also
    when MarkManager::repositionMark() changes the positions.
    
    The other calls of SetMarkPos()/SetOtherMarkPos() look like they're all
    from code that corrects positions after text insertions or deletions so
    no additional invalidate should be necessary there.
    
    It turns out that one WW8 document in sw_filters_test wants to insert
    a bookmark on a SwGrfNode; check for that in makeMark().
    
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/87157
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@cib.de>
    (cherry picked from commit ef8427d12a63127a2eb867637699343d630545dd)
    
    crashtesting: null dereference of reexporting abi12570.odt to odt
    
    FLY_AT_FLY shape is anchored on SwStartNode of fly section.
    
    (regression from ef8427d12a63127a2eb867637699343d630545dd)
    
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/91336
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@cib.de>
    (cherry picked from commit 71ed878556422068041025668876fb3300c128df)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/96318
    Tested-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>
    (cherry picked from commit 79bc162d86c62506614a19bf7c92c72237804f5e)
    
    Change-Id: I293e6da9042bea5992cb27091b9cff77e5c7961d
    4fe70237c060cc810af82657bc5791e7024db8f5

--- sw/source/core/doc/docbm.cxx
+++ sw/source/core/doc/docbm.cxx
@@ -561,6 +561,18 @@ namespace sw { namespace mark
                 pPos2->nContent.GetIndex());
         }
 #endif
+        if (   (!rPaM.GetPoint()->nNode.GetNode().IsTextNode()
+                && (eType != MarkType::UNO_BOOKMARK
+                // SwXTextRange can be on table node or plain start node (FLY_AT_FLY)
+                    || !rPaM.GetPoint()->nNode.GetNode().IsStartNode()))
+            || (!rPaM.GetMark()->nNode.GetNode().IsTextNode()
+                && (eType != MarkType::UNO_BOOKMARK
+                    || !rPaM.GetMark()->nNode.GetNode().IsStartNode())))
+        {
+            SAL_WARN("sw.core", "MarkManager::makeMark(..)"
+                " - refusing to create mark on non-textnode");
+            return nullptr;
+        }
         // There should only be one CrossRefBookmark per Textnode per Type
         if ((eType == MarkType::CROSSREF_NUMITEM_BOOKMARK || eType == MarkType::CROSSREF_HEADING_BOOKMARK)
             && (lcl_FindMarkAtPos(m_vBookmarks, *rPaM.Start(), eType) != m_vBookmarks.end()))
@@ -809,6 +821,8 @@ namespace sw { namespace mark
         if (!pMarkBase)
             return;
 
+        pMarkBase->InvalidateFrames();
+
         pMarkBase->SetMarkPos(*(rPaM.GetPoint()));
         if(rPaM.HasMark())
             pMarkBase->SetOtherMarkPos(*(rPaM.GetMark()));
@@ -817,6 +831,8 @@ namespace sw { namespace mark
 
         if(pMarkBase->GetMarkPos() != pMarkBase->GetMarkStart())
             pMarkBase->Swap();
+
+        pMarkBase->InvalidateFrames();
 
         sortMarks();
     }
