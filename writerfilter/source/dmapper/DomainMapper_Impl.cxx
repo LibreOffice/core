LibreOffice/core

commit a290d025339ef92b93ba6d287658ba784a818220

tdf#129805 writerfilter: fix import of nested generic field
The problem was that the end of the outer nested generic field did not
call PopFieldmark(), so the end of the field was at the end of the
document.

(regression from f610f9b611fe9f206b872ed06f7e859d688385fc)

Reviewed-on: https://gerrit.libreoffice.org/c/core/+/86785
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@cib.de>
(cherry picked from commit cf226535f9903a048b1c105b180ae3a50a776e68)
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/86797
Reviewed-by: Xisco Faulí <xiscofauli@libreoffice.org>
Signed-off-by: Xisco Fauli <xiscofauli@libreoffice.org>

Change-Id: If5928b14dd35f7dd509370c2b8eef4c31bd149dc
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/86810
Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Miklos Vajna <vmiklos@collabora.com>

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -254,7 +254,7 @@ DomainMapper_Impl::DomainMapper_Impl(
         m_bStartedTOC(false),
         m_bStartIndex(false),
         m_bStartBibliography(false),
-        m_bStartGenericField(false),
+        m_nStartGenericField(0),
         m_bTextInserted(false),
         m_sCurrentPermId(0),
         m_pLastSectionContext( ),
@@ -1866,7 +1866,7 @@ void DomainMapper_Impl::appendTextPortion( const OUString& rString, const Proper
             }
             else
             {
-                if (m_bStartTOC || m_bStartIndex || m_bStartBibliography || m_bStartGenericField)
+                if (m_bStartTOC || m_bStartIndex || m_bStartBibliography || m_nStartGenericField != 0)
                 {
                     if (IsInHeaderFooter() && !m_bStartTOCHeaderFooter)
                     {
@@ -1878,8 +1878,9 @@ void DomainMapper_Impl::appendTextPortion( const OUString& rString, const Proper
                         uno::Reference< text::XTextCursor > xTOCTextCursor = xTextAppend->getEnd()->getText( )->createTextCursor( );
                         assert(xTOCTextCursor.is());
                         xTOCTextCursor->gotoEnd(false);
-                        if (m_bStartIndex || m_bStartBibliography || m_bStartGenericField)
+                        if (m_bStartIndex || m_bStartBibliography || m_bStartGenericField != 0)
                             xTOCTextCursor->goLeft(1, false);
+                        }
                         xTextRange = xTextAppend->insertTextPortion(rString, aValues, xTOCTextCursor);
                         SAL_WARN_IF(!xTextRange.is(), "writerfilter.dmapper", "insertTextPortion failed");
                         if (!xTextRange.is())
@@ -1887,7 +1888,7 @@ void DomainMapper_Impl::appendTextPortion( const OUString& rString, const Proper
                         m_bTextInserted = true;
                         xTOCTextCursor->gotoRange(xTextRange->getEnd(), true);
                         mxTOCTextCursor = xTOCTextCursor;
-                        if (!m_bStartGenericField)
+                        if (m_nStartGenericField == 0)
                         {
                             m_aTextAppendStack.push(TextAppendContext(xTextAppend, xTOCTextCursor));
                         }
@@ -5266,7 +5267,7 @@ void DomainMapper_Impl::CloseFieldCommand()
                     InsertFieldmark(m_aTextAppendStack, xFormField, pContext->GetStartRange(),
                             pContext->GetFieldId());
                     xFormField->setFieldType(ODF_UNHANDLED);
-                    m_bStartGenericField = true;
+                    ++m_nStartGenericField;
                     pContext->SetFormField( xFormField );
                     uno::Reference<container::XNameContainer> const xNameCont(xFormField->getParameters());
                     // note: setting the code to empty string is *required* in
@@ -5689,9 +5690,9 @@ void DomainMapper_Impl::PopFieldContext()
                                 }
                             }
                         }
-                        else if(m_bStartGenericField)
+                        else if (m_nStartGenericField != 0)
                         {
-                            m_bStartGenericField = false;
+                            --m_nStartGenericField;
                             PopFieldmark(m_aTextAppendStack, xCrsr, pContext->GetFieldId());
                             if(m_bTextInserted)
                             {

writerfilter/source/dmapper/DomainMapper_Impl.hxx
@@ -467,7 +467,7 @@ private:
     bool                                                                            m_bStartedTOC;
     bool                                                                            m_bStartIndex;
     bool                                                                            m_bStartBibliography;
-    bool                                                                            m_bStartGenericField;
+    unsigned int                                                                    m_nStartGenericField;
     bool                                                                            m_bTextInserted;
     LineNumberSettings                                                              m_aLineNumberSettings;
 

commit 232d7bab33ecd089016ce6ba4c0650f4a3610b5a

fix build
m_bStartGenericField became m_nStartGenericField, but more importantly a
closing bracket was wrongly added (well, left over)  during merge.

regression from a290d025339ef92b93ba6d287658ba784a818220

Change-Id: Ic686e9c95fa9e5a95cf3264d4484e917dcbd3c98

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -1878,7 +1878,7 @@ void DomainMapper_Impl::appendTextPortion( const OUString& rString, const Proper
                         uno::Reference< text::XTextCursor > xTOCTextCursor = xTextAppend->getEnd()->getText( )->createTextCursor( );
                         assert(xTOCTextCursor.is());
                         xTOCTextCursor->gotoEnd(false);
-                        if (m_bStartIndex || m_bStartBibliography || m_bStartGenericField != 0)
+                        if (m_bStartIndex || m_bStartBibliography || m_nStartGenericField != 0) {
                             xTOCTextCursor->goLeft(1, false);
                         }
                         xTextRange = xTextAppend->insertTextPortion(rString, aValues, xTOCTextCursor);