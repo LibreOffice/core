LibreOffice/core

commit ac87cfdf263d2abd97b32857534d0f749b7c4479

Replace SystemEnvData pToolkit/pPlatformName strings with enums
(and streamline those data members' names)

Change-Id: Ifdd596c7a54dd507045d412c30b463468c2f798b
Reviewed-on: https://gerrit.libreoffice.org/80313
Tested-by: Jenkins
Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

include/vcl/sysdata.hxx
@@ -61,6 +61,9 @@ struct SystemEnvData
 #elif defined( IOS )
     // Nothing
 #elif defined( UNX )
+    enum class Toolkit { Gtk3, Qt5 };
+    enum class Platform { Wayland, Xcb };
+
     void*               pDisplay;       // the relevant display connection
     unsigned long       aWindow;        // the window of the object
     void*               pSalFrame;      // contains a salframe, if object has one
@@ -70,8 +73,8 @@ struct SystemEnvData
     // note: this is a "long" in Xlib *but* in the protocol it's only 32-bit
     // however, the GTK3 vclplug wants to store pointers in here!
     sal_IntPtr          aShellWindow;   // the window of the frame's shell
-    const char*         pToolkit;       // the toolkit in use (gtk2 vs gtk3)
-    const char*         pPlatformName; // the windowing system in use (xcb vs wayland)
+    Toolkit             toolkit;        // the toolkit in use
+    Platform            platform;       // the windowing system in use
 #endif
 
     SystemEnvData()
@@ -90,8 +93,8 @@ struct SystemEnvData
         , pVisual(nullptr)
         , nScreen(0)
         , aShellWindow(0)
-        , pToolkit(nullptr)
-        , pPlatformName(nullptr)
+        , toolkit(Toolkit())
+        , platform(Platform())
 #endif
     {
     }

avmedia/source/gstreamer/gstplayer.cxx
@@ -850,13 +850,10 @@ uno::Reference< ::media::XPlayerWindow > SAL_CALL Player::createPlayerWindow( co
         if (!pEnvData)
             return nullptr;
 
-        OUString aToolkit = OUString::createFromAscii(pEnvData->pToolkit);
-        OUString aPlatform = OUString::createFromAscii(pEnvData->pPlatformName);
-
         // tdf#124027: the position of embedded window is identical w/ the position
         // of media object in all other vclplugs (kf5, gen), in gtk3 w/o gtksink it
         // needs to be translated
-        if (aToolkit == "gtk3")
+        if (pEnvData->toolkit == SystemEnvData::Toolkit::Gtk3)
         {
             Point aPoint = pParentWindow->GetPosPixel();
             maArea.X = aPoint.getX();
@@ -868,12 +865,12 @@ uno::Reference< ::media::XPlayerWindow > SAL_CALL Player::createPlayerWindow( co
         GstElement *pVideosink = static_cast<GstElement*>(pParentWindow->CreateGStreamerSink());
         if (pVideosink)
         {
-            if (aToolkit == "gtk3")
+            if (pEnvData->toolkit == SystemEnvData::Toolkit::Gtk3)
                 mbUseGtkSink = true;
         }
         else
         {
-            if (aPlatform == "wayland")
+            if (pEnvData->platform == SystemEnvData::Platform::Wayland)
                 pVideosink = gst_element_factory_make("waylandsink", "video-output");
             else
                 pVideosink = gst_element_factory_make("autovideosink", "video-output");

vcl/qt5/Qt5Frame.cxx
@@ -187,11 +187,11 @@ Qt5Frame::Qt5Frame(Qt5Frame* pParent, SalFrameStyleFlags nStyle, bool bUseCairo)
     //m_aSystemData.pSalFrame = this;
     m_aSystemData.pWidget = m_pQWidget;
     //m_aSystemData.nScreen = m_nXScreen.getXScreen();
-    m_aSystemData.pToolkit = "qt5";
+    m_aSystemData.toolkit = SystemEnvData::Toolkit::Qt5;
     if (!bWayland)
-        m_aSystemData.pPlatformName = "xcb";
+        m_aSystemData.platform = SystemEnvData::Platform::Xcb;
     else
-        m_aSystemData.pPlatformName = "wayland";
+        m_aSystemData.platform = SystemEnvData::Platform::Wayland;
 
     SetIcon(SV_ICON_ID_OFFICE);
 }

vcl/qt5/Qt5Instance.cxx
@@ -515,8 +515,7 @@ void* Qt5Instance::CreateGStreamerSink(const SystemChildWindow* pWindow)
     if (!pEnvData)
         return nullptr;
 
-    OUString aPlatform = OUString::createFromAscii(pEnvData->pPlatformName);
-    if (aPlatform != "wayland")
+    if (pEnvData->platform != SystemEnvData::Platform::Wayland)
         return nullptr;
 
     GstElement* pVideosink = pSymbol("qwidget5videosink", "qwidget5videosink");

vcl/qt5/Qt5Object.cxx
@@ -45,17 +45,17 @@ Qt5Object::Qt5Object(Qt5Frame* pParent, bool bShow)
     //m_aSystemData.pSalFrame = this;
     m_aSystemData.pWidget = m_pQWidget;
     //m_aSystemData.nScreen = m_nXScreen.getXScreen();
-    m_aSystemData.pToolkit = "qt5";
-    m_aSystemData.pPlatformName = "xcb";
+    m_aSystemData.toolkit = SystemEnvData::Toolkit::Qt5;
+    m_aSystemData.platform = SystemEnvData::Platform::Xcb;
     const bool bWayland = QGuiApplication::platformName() == "wayland";
     if (!bWayland)
     {
-        m_aSystemData.pPlatformName = "xcb";
+        m_aSystemData.platform = SystemEnvData::Platform::Xcb;
         m_aSystemData.aWindow = m_pQWindow->winId(); // ID of the embedded window
     }
     else
     {
-        m_aSystemData.pPlatformName = "wayland";
+        m_aSystemData.platform = SystemEnvData::Platform::Wayland;
         // TODO implement as needed for Wayland,
         // s.a. commit c0d4f3ad3307c which did this for gtk3
         // QPlatformNativeInterface* native = QGuiApplication::platformNativeInterface();

vcl/unx/gtk3/gtk3gtkframe.cxx
@@ -968,14 +968,14 @@ void GtkSalFrame::InitCommon()
     m_aSystemData.pSalFrame     = this;
     m_aSystemData.pWidget       = m_pWindow;
     m_aSystemData.nScreen       = m_nXScreen.getXScreen();
-    m_aSystemData.pToolkit      = "gtk3";
+    m_aSystemData.toolkit       = SystemEnvData::Toolkit::Gtk3;
 
 #if defined(GDK_WINDOWING_X11)
     GdkDisplay *pDisplay = getGdkDisplay();
     if (DLSYM_GDK_IS_X11_DISPLAY(pDisplay))
     {
         m_aSystemData.pDisplay = gdk_x11_display_get_xdisplay(pDisplay);
-        m_aSystemData.pPlatformName = "xcb";
+        m_aSystemData.platform = SystemEnvData::Platform::Xcb;
         if (GTK_IS_WINDOW(m_pWindow))
         {
             GdkScreen* pScreen = gtk_window_get_screen(GTK_WINDOW(m_pWindow));
@@ -988,7 +988,7 @@ void GtkSalFrame::InitCommon()
     if (DLSYM_GDK_IS_WAYLAND_DISPLAY(pDisplay))
     {
         m_aSystemData.pDisplay = gdk_wayland_display_get_wl_display(pDisplay);
-        m_aSystemData.pPlatformName = "wayland";
+        m_aSystemData.platform = SystemEnvData::Platform::Wayland;
     }
 #endif
 

vcl/unx/gtk3/gtk3gtkobject.cxx
@@ -53,7 +53,7 @@ GtkSalObject::GtkSalObject( GtkSalFrame* pParent, bool bShow )
     m_aSystemData.pSalFrame     = nullptr;
     m_aSystemData.pWidget       = m_pSocket;
     m_aSystemData.nScreen       = pParent->getXScreenNumber().getXScreen();
-    m_aSystemData.pToolkit      = "gtk3";
+    m_aSystemData.toolkit       = SystemEnvData::Toolkit::Gtk3;
     GdkScreen* pScreen = gtk_window_get_screen(GTK_WINDOW(pParent->getWindow()));
     GdkVisual* pVisual = gdk_screen_get_system_visual(pScreen);
 
@@ -63,14 +63,14 @@ GtkSalObject::GtkSalObject( GtkSalFrame* pParent, bool bShow )
     {
         m_aSystemData.pDisplay = gdk_x11_display_get_xdisplay(pDisplay);
         m_aSystemData.pVisual = gdk_x11_visual_get_xvisual(pVisual);
-        m_aSystemData.pPlatformName = "xcb";
+        m_aSystemData.platform = SystemEnvData::Platform::Xcb;
     }
 #endif
 #if defined(GDK_WINDOWING_WAYLAND)
     if (DLSYM_GDK_IS_WAYLAND_DISPLAY(pDisplay))
     {
         m_aSystemData.pDisplay = gdk_wayland_display_get_wl_display(pDisplay);
-        m_aSystemData.pPlatformName = "wayland";
+        m_aSystemData.platform = SystemEnvData::Platform::Wayland;
     }
 #endif
 

commit 0e66d6b48c1891af58e78fd6e7564bce93991a19

Related tdf#126459: fix crash in presenter console using gen vclplug
A slide with video is needed to reproduce

Regression from commit ac87cfdf263d2abd97b32857534d0f749b7c4479,
'toolkit' and 'platform' bits were never set (bc irrelevant) in gen
vclplug but now if not set, values mapping to 0 (Gtk3, Wayland) will
be used and things go down the drain in avmedia/gstplayer then

Just between us girls, nobody in their right mind would use gen vclplug
for presentation, but crash is not nice so let's fix this

Change-Id: If318ab93897f9f4854f7316e630c35aaec2480fd

include/vcl/sysdata.hxx
@@ -61,7 +61,7 @@ struct SystemEnvData
 #elif defined( IOS )
     // Nothing
 #elif defined( UNX )
-    enum class Toolkit { Gtk3, Qt5 };
+    enum class Toolkit { Gtk3, Qt5, Gen };
     enum class Platform { Wayland, Xcb };
 
     void*               pDisplay;       // the relevant display connection

vcl/unx/generic/window/salframe.cxx
@@ -956,6 +956,8 @@ const SystemEnvData* X11SalFrame::GetSystemData() const
     pFrame->maSystemChildData.pVisual       = GetDisplay()->GetVisual( m_nXScreen ).GetVisual();
     pFrame->maSystemChildData.nScreen       = m_nXScreen.getXScreen();
     pFrame->maSystemChildData.aShellWindow  = pFrame->GetShellWindow();
+    pFrame->maSystemChildData.toolkit       = SystemEnvData::Toolkit::Gen;
+    pFrame->maSystemChildData.plaftorm      = SystemEnvData::Platform::Xcb
     return &maSystemChildData;
 }
 

vcl/unx/generic/window/salobj.cxx
@@ -225,6 +225,8 @@ X11SalObject::X11SalObject()
     maSystemChildData.pWidget       = nullptr;
     maSystemChildData.pVisual       = nullptr;
     maSystemChildData.aShellWindow  = 0;
+    maSystemChildData.toolkit = SystemEnvData::Toolkit::Gen;
+    maSystemChildData.plaftorm = SystemEnvData::Platform::Xcb;
 
     std::list< SalObject* >& rObjects = vcl_sal::getSalDisplay(GetGenericUnixSalData())->getSalObjects();
     rObjects.push_back( this );