LibreOffice/core

commit 70944a849dc59fa686e3f316a3983dbc8a47017f

tdf#109858: Override default point label placement setting...
with series label placement setting if available while
importing xlsx chart.

Adds unit test in chart2import.cxx that asserts one of
the point label setting in the bugzilla xlsx document.

Change-Id: I8bc04fd569f94a19633666ffdc51b61ac719e8a9
Reviewed-on: https://gerrit.libreoffice.org/40860
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Dennis Francis <dennis.francis@collabora.co.uk>

oox/inc/drawingml/chart/seriesconverter.hxx
@@ -47,7 +47,7 @@ public:
     /** Converts OOXML data label settings for the passed data point. */
     void                convertFromModel(
                             const css::uno::Reference< css::chart2::XDataSeries >& rxDataSeries,
-                            const TypeGroupConverter& rTypeGroup );
+                            const TypeGroupConverter& rTypeGroup, const PropertySet& rSeriesPropSet );
 };
 
 

oox/source/drawingml/chart/seriesconverter.cxx
@@ -99,7 +99,8 @@ Reference< XLabeledDataSequence > lclCreateLabeledDataSequence(
 }
 
 void lclConvertLabelFormatting( PropertySet& rPropSet, ObjectFormatter& rFormatter,
-        const DataLabelModelBase& rDataLabel, const TypeGroupConverter& rTypeGroup, bool bDataSeriesLabel, bool bMSO2007Doc )
+                                const DataLabelModelBase& rDataLabel, const TypeGroupConverter& rTypeGroup,
+                                bool bDataSeriesLabel, bool bMSO2007Doc, const PropertySet* pSeriesPropSet )
 {
     const TypeGroupInfo& rTypeInfo = rTypeGroup.getTypeInfo();
 
@@ -170,6 +171,12 @@ void lclConvertLabelFormatting( PropertySet& rPropSet, ObjectFormatter& rFormatt
                 case XML_r:         nPlacement = csscd::RIGHT;          break;
                 case XML_bestFit:   nPlacement = csscd::AVOID_OVERLAP;  break;
             }
+
+            sal_Int32 nGlobalPlacement = 0;
+            if ( !bDataSeriesLabel && nPlacement == rTypeInfo.mnDefLabelPos && pSeriesPropSet &&
+                 pSeriesPropSet->getProperty( nGlobalPlacement, PROP_LabelPlacement ) )
+                nPlacement = nGlobalPlacement;
+
             rPropSet.setProperty( PROP_LabelPlacement, nPlacement );
         }
     }
@@ -204,7 +211,8 @@ DataLabelConverter::~DataLabelConverter()
 {
 }
 
-void DataLabelConverter::convertFromModel( const Reference< XDataSeries >& rxDataSeries, const TypeGroupConverter& rTypeGroup )
+void DataLabelConverter::convertFromModel( const Reference< XDataSeries >& rxDataSeries, const TypeGroupConverter& rTypeGroup,
+                                           const PropertySet& rSeriesPropSet )
 {
     if (!rxDataSeries.is())
         return;
@@ -213,7 +221,7 @@ void DataLabelConverter::convertFromModel( const Reference< XDataSeries >& rxDat
     {
         bool bMSO2007Doc = getFilter().isMSO2007Document();
         PropertySet aPropSet( rxDataSeries->getDataPointByIndex( mrModel.mnIndex ) );
-        lclConvertLabelFormatting( aPropSet, getFormatter(), mrModel, rTypeGroup, false, bMSO2007Doc );
+        lclConvertLabelFormatting( aPropSet, getFormatter(), mrModel, rTypeGroup, false, bMSO2007Doc, &rSeriesPropSet );
         const TypeGroupInfo& rTypeInfo = rTypeGroup.getTypeInfo();
         bool bIsPie = rTypeInfo.meTypeCategory == TYPECATEGORY_PIE;
         if( mrModel.mxLayout && !mrModel.mxLayout->mbAutoLayout && !bIsPie )
@@ -256,11 +264,11 @@ DataLabelsConverter::~DataLabelsConverter()
 
 void DataLabelsConverter::convertFromModel( const Reference< XDataSeries >& rxDataSeries, const TypeGroupConverter& rTypeGroup )
 {
+    PropertySet aPropSet( rxDataSeries );
     if( !mrModel.mbDeleted )
     {
         bool bMSO2007Doc = getFilter().isMSO2007Document();
-        PropertySet aPropSet( rxDataSeries );
-        lclConvertLabelFormatting( aPropSet, getFormatter(), mrModel, rTypeGroup, true, bMSO2007Doc );
+        lclConvertLabelFormatting( aPropSet, getFormatter(), mrModel, rTypeGroup, true, bMSO2007Doc, nullptr );
 
         if (mrModel.mxShapeProp)
             // Import baseline border properties for these data labels.
@@ -274,7 +282,7 @@ void DataLabelsConverter::convertFromModel( const Reference< XDataSeries >& rxDa
             (*aIt)->maNumberFormat = mrModel.maNumberFormat;
 
         DataLabelConverter aLabelConv( *this, **aIt );
-        aLabelConv.convertFromModel( rxDataSeries, rTypeGroup );
+        aLabelConv.convertFromModel( rxDataSeries, rTypeGroup, aPropSet );
     }
 }
 

chart2/qa/extras/chart2import.cxx
@@ -94,6 +94,7 @@ public:
     void testAxisTitleRotationXLSX();
 
     void testTdf90510(); // Pie chart label placement settings(XLS)
+    void testTdf109858(); // Pie chart label placement settings(XLSX)
 
     void testInternalDataProvider();
 
@@ -150,6 +151,7 @@ public:
     CPPUNIT_TEST(testSecondaryAxisTitleDefaultRotationXLSX);
     CPPUNIT_TEST(testAxisTitleRotationXLSX);
     CPPUNIT_TEST(testTdf90510);
+    CPPUNIT_TEST(testTdf109858);
 
     CPPUNIT_TEST(testInternalDataProvider);
 
@@ -1252,6 +1254,19 @@ void Chart2ImportTest::testInternalDataProvider() {
 void Chart2ImportTest::testTdf90510()
 {
     load("/chart2/qa/extras/data/xls/", "piechart_outside.xls");
+    uno::Reference< chart::XChartDocument > xChart1Doc( getChartCompFromSheet( 0, mxComponent ), UNO_QUERY_THROW );
+    CPPUNIT_ASSERT_MESSAGE( "failed to load chart", xChart1Doc.is() );
+    Reference<beans::XPropertySet> xPropSet( xChart1Doc->getDiagram()->getDataPointProperties( 0, 0 ), uno::UNO_QUERY_THROW );
+    uno::Any aAny = xPropSet->getPropertyValue( "LabelPlacement" );
+    CPPUNIT_ASSERT( aAny.hasValue() );
+    sal_Int32 nLabelPlacement = 0;
+    CPPUNIT_ASSERT( aAny >>= nLabelPlacement );
+    CPPUNIT_ASSERT_EQUAL_MESSAGE( "Data labels should be placed outside", chart::DataLabelPlacement::OUTSIDE, nLabelPlacement );
+}
+
+void Chart2ImportTest::testTdf109858()
+{
+    load("/chart2/qa/extras/data/xlsx/", "piechart_outside.xlsx");
     uno::Reference< chart::XChartDocument > xChart1Doc( getChartCompFromSheet( 0, mxComponent ), UNO_QUERY_THROW );
     CPPUNIT_ASSERT_MESSAGE( "failed to load chart", xChart1Doc.is() );
     Reference<beans::XPropertySet> xPropSet( xChart1Doc->getDiagram()->getDataPointProperties( 0, 0 ), uno::UNO_QUERY_THROW );

commit 4411537e52d36f05b66af463b1ac9ab9881e72bf

tdf#130105 Chart OOXML Import: Fix data label position
The position of datapoint labels was overwritten with the
position of series placement. Do not overwrite datapoint
labels placement, if we have explicit datapoint label placement.

Regression from commit: 70944a849dc59fa686e3f316a3983dbc8a47017f
(tdf#109858: Override default point label placement setting...)

Change-Id: Ic27c8a7eb93e8c3776389d1b3d285d120568f024

oox/inc/drawingml/chart/seriesconverter.hxx
@@ -47,7 +47,7 @@ public:
     /** Converts OOXML data label settings for the passed data point. */
     void                convertFromModel(
                             const css::uno::Reference< css::chart2::XDataSeries >& rxDataSeries,
-                            const TypeGroupConverter& rTypeGroup, const PropertySet& rSeriesPropSet );
+                            const TypeGroupConverter& rTypeGroup );
 };
 
 

oox/source/drawingml/chart/seriesconverter.cxx
@@ -132,7 +132,7 @@ void convertTextProperty(PropertySet& rPropSet, ObjectFormatter& rFormatter,
 
 void lclConvertLabelFormatting( PropertySet& rPropSet, ObjectFormatter& rFormatter,
                                 const DataLabelModelBase& rDataLabel, const TypeGroupConverter& rTypeGroup,
-                                bool bDataSeriesLabel, bool bMSO2007Doc, const PropertySet* pSeriesPropSet )
+                                bool bDataSeriesLabel, bool bMSO2007Doc )
 {
     const TypeGroupInfo& rTypeInfo = rTypeGroup.getTypeInfo();
 
@@ -191,7 +191,7 @@ void lclConvertLabelFormatting( PropertySet& rPropSet, ObjectFormatter& rFormatt
         if( bDataSeriesLabel || rDataLabel.monLabelPos.has() )
         {
             namespace csscd = ::com::sun::star::chart::DataLabelPlacement;
-            sal_Int32 nPlacement = rTypeInfo.mnDefLabelPos;
+            sal_Int32 nPlacement = -1;
             switch( rDataLabel.monLabelPos.get( XML_TOKEN_INVALID ) )
             {
                 case XML_outEnd:    nPlacement = csscd::OUTSIDE;        break;
@@ -205,10 +205,10 @@ void lclConvertLabelFormatting( PropertySet& rPropSet, ObjectFormatter& rFormatt
                 case XML_bestFit:   nPlacement = csscd::AVOID_OVERLAP;  break;
             }
 
-            sal_Int32 nGlobalPlacement = 0;
-            if ( !bDataSeriesLabel && nPlacement == rTypeInfo.mnDefLabelPos && pSeriesPropSet &&
-                 pSeriesPropSet->getProperty( nGlobalPlacement, PROP_LabelPlacement ) )
-                nPlacement = nGlobalPlacement;
+            if( !bDataSeriesLabel && nPlacement == -1 )
+                return;
+            else if( nPlacement == -1 )
+                nPlacement = rTypeInfo.mnDefLabelPos;
 
             rPropSet.setProperty( PROP_LabelPlacement, nPlacement );
         }
@@ -260,8 +260,7 @@ DataLabelConverter::~DataLabelConverter()
 {
 }
 
-void DataLabelConverter::convertFromModel( const Reference< XDataSeries >& rxDataSeries, const TypeGroupConverter& rTypeGroup,
-                                           const PropertySet& rSeriesPropSet )
+void DataLabelConverter::convertFromModel( const Reference< XDataSeries >& rxDataSeries, const TypeGroupConverter& rTypeGroup )
 {
     if (!rxDataSeries.is())
         return;
@@ -270,7 +269,7 @@ void DataLabelConverter::convertFromModel( const Reference< XDataSeries >& rxDat
     {
         bool bMSO2007Doc = getFilter().isMSO2007Document();
         PropertySet aPropSet( rxDataSeries->getDataPointByIndex( mrModel.mnIndex ) );
-        lclConvertLabelFormatting( aPropSet, getFormatter(), mrModel, rTypeGroup, false, bMSO2007Doc, &rSeriesPropSet );
+        lclConvertLabelFormatting( aPropSet, getFormatter(), mrModel, rTypeGroup, false, bMSO2007Doc );
         const TypeGroupInfo& rTypeInfo = rTypeGroup.getTypeInfo();
         bool bIsPie = rTypeInfo.meTypeCategory == TYPECATEGORY_PIE;
         if( mrModel.mxLayout && !mrModel.mxLayout->mbAutoLayout && !bIsPie )
@@ -370,7 +369,7 @@ void DataLabelsConverter::convertFromModel( const Reference< XDataSeries >& rxDa
     if( !mrModel.mbDeleted )
     {
         bool bMSO2007Doc = getFilter().isMSO2007Document();
-        lclConvertLabelFormatting( aPropSet, getFormatter(), mrModel, rTypeGroup, true, bMSO2007Doc, nullptr );
+        lclConvertLabelFormatting( aPropSet, getFormatter(), mrModel, rTypeGroup, true, bMSO2007Doc );
 
         if (mrModel.mxShapeProp)
             // Import baseline border properties for these data labels.
@@ -384,7 +383,7 @@ void DataLabelsConverter::convertFromModel( const Reference< XDataSeries >& rxDa
             pointLabel->maNumberFormat = mrModel.maNumberFormat;
 
         DataLabelConverter aLabelConv(*this, *pointLabel);
-        aLabelConv.convertFromModel( rxDataSeries, rTypeGroup, aPropSet );
+        aLabelConv.convertFromModel( rxDataSeries, rTypeGroup );
     }
 }
 

chart2/qa/extras/chart2import.cxx
@@ -117,6 +117,7 @@ public:
 
     void testTdf90510(); // Pie chart label placement settings(XLS)
     void testTdf109858(); // Pie chart label placement settings(XLSX)
+    void testTdf130105();
 
     void testTdf111173();
     void testTdf122226();
@@ -217,6 +218,7 @@ public:
     CPPUNIT_TEST(testCombinedChartAttachedAxisXLSX);
     CPPUNIT_TEST(testTdf90510);
     CPPUNIT_TEST(testTdf109858);
+    CPPUNIT_TEST(testTdf130105);
     CPPUNIT_TEST(testTdf111173);
     CPPUNIT_TEST(testTdf122226);
 
@@ -1668,12 +1670,36 @@ void Chart2ImportTest::testTdf109858()
 {
     load("/chart2/qa/extras/data/xlsx/", "piechart_outside.xlsx");
     uno::Reference< chart::XChartDocument > xChart1Doc( getChartCompFromSheet( 0, mxComponent ), UNO_QUERY_THROW );
-    Reference<beans::XPropertySet> xPropSet( xChart1Doc->getDiagram()->getDataPointProperties( 0, 0 ), uno::UNO_SET_THROW );
-    uno::Any aAny = xPropSet->getPropertyValue( "LabelPlacement" );
+
+    // test data point labels position
+    Reference<beans::XPropertySet> xDataPointPropSet( xChart1Doc->getDiagram()->getDataPointProperties( 0, 0 ), uno::UNO_SET_THROW );
+    uno::Any aAny = xDataPointPropSet->getPropertyValue( "LabelPlacement" );
     CPPUNIT_ASSERT( aAny.hasValue() );
     sal_Int32 nLabelPlacement = 0;
     CPPUNIT_ASSERT( aAny >>= nLabelPlacement );
-    CPPUNIT_ASSERT_EQUAL_MESSAGE( "Data labels should be placed outside", chart::DataLabelPlacement::OUTSIDE, nLabelPlacement );
+    CPPUNIT_ASSERT_EQUAL_MESSAGE( "Data point label should be placed bestFit", chart::DataLabelPlacement::AVOID_OVERLAP, nLabelPlacement );
+
+    // test data series label position
+    Reference<beans::XPropertySet> xSeriesPropSet(xChart1Doc->getDiagram()->getDataRowProperties(0), uno::UNO_SET_THROW);
+    aAny = xSeriesPropSet->getPropertyValue( "LabelPlacement" );
+    CPPUNIT_ASSERT( aAny >>= nLabelPlacement );
+    CPPUNIT_ASSERT_EQUAL_MESSAGE( "Data series labels should be placed outside", chart::DataLabelPlacement::OUTSIDE, nLabelPlacement );
+}
+
+void Chart2ImportTest::testTdf130105()
+{
+    load("/chart2/qa/extras/data/xlsx/", "barchart_outend.xlsx");
+    uno::Reference< chart2::XChartDocument > xChartDoc = getChartDocFromSheet(0, mxComponent);
+    CPPUNIT_ASSERT(xChartDoc.is());
+    uno::Reference<chart2::XDataSeries> xDataSeries(getDataSeriesFromDoc(xChartDoc, 0));
+    CPPUNIT_ASSERT(xDataSeries.is());
+
+    uno::Reference<beans::XPropertySet> xPropertySet(xDataSeries->getDataPointByIndex(0), uno::UNO_SET_THROW);
+    uno::Any aAny = xPropertySet->getPropertyValue("LabelPlacement");
+    CPPUNIT_ASSERT(aAny.hasValue());
+    sal_Int32 nLabelPlacement = 0;
+    CPPUNIT_ASSERT(aAny >>= nLabelPlacement);
+    CPPUNIT_ASSERT_EQUAL_MESSAGE("Data label should be placed outend", chart::DataLabelPlacement::OUTSIDE, nLabelPlacement);
 }
 
 void Chart2ImportTest::testTdf111173()