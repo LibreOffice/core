14991a7ba0c95c05062cc319643adb6eaf7c6629
Noel Grandin <noel.grandin@collabora.co.uk>
2020-01-23 15:06:47 +0200

    new loplugin:makeshared
    Change-Id: I4bfcf6e337a6806ab5983a3fa2e2a7b6f2af1c43

---
+++ compilerplugins/clang/test/makeshared.cxx
@@ -0,0 +1,43 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <sal/config.h>
+
+#include <memory>
+#include <o3tl/deleter.hxx>
+#include <o3tl/sorted_vector.hxx>
+
+struct S1
+{
+    friend void test1();
+
+private:
+    S1() {}
+};
+
+void test1()
+{
+    std::shared_ptr<int> x(
+        new int); // expected-error {{rather use make_shared [loplugin:makeshared]}}
+    x.reset(new int); // expected-error {{rather use make_shared [loplugin:makeshared]}}
+    x = std::shared_ptr<int>(
+        new int); // expected-error {{rather use make_shared [loplugin:makeshared]}}
+
+    // no warning expected
+    std::shared_ptr<int> y(new int, o3tl::default_delete<int>());
+    y.reset(new int, o3tl::default_delete<int>());
+    // no warning expected, no public constructor
+    std::shared_ptr<S1> z(new S1);
+    z.reset(new S1);
+
+    // no warning expected - this constructor takes an initializer-list, which maked_shared does not support
+    auto a = std::shared_ptr<o3tl::sorted_vector<int>>(new o3tl::sorted_vector<int>({ 1, 2 }));
+};
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
