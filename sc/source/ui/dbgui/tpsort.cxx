a34e4090b39852b6d44a0107f375ff48253cd077
Caolán McNamara <caolanm@redhat.com>
2020-01-23 11:27:17 +0000

    Resolves: tdf#129158 freeze/thaw around bulk inserts
    Change-Id: I0ba22cd262512b467abd383d2f932d2adf6ca1a5
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/87259
    Tested-by: Jenkins
    Reviewed-by: Adolfo Jayme Barrientos <fitojb@ubuntu.com>

--- sc/source/ui/dbgui/tpsort.cxx
+++ sc/source/ui/dbgui/tpsort.cxx
@@ -326,10 +326,11 @@ void ScTabPageSortFields::FillFieldLists( sal_uInt16 nStartField )
 
         if ( pDoc )
         {
-            for ( sal_uInt16 i=nStartField; i<nSortKeyCount; i++ )
+            for (sal_uInt16 j = nStartField; j < nSortKeyCount; ++j)
             {
-                m_aSortWin.m_aSortKeyItems[i]->m_xLbSort->clear();
-                m_aSortWin.m_aSortKeyItems[i]->m_xLbSort->append_text(aStrUndefined);
+                m_aSortWin.m_aSortKeyItems[j]->m_xLbSort->clear();
+                m_aSortWin.m_aSortKeyItems[j]->m_xLbSort->freeze();
+                m_aSortWin.m_aSortKeyItems[j]->m_xLbSort->append_text(aStrUndefined);
             }
 
             SCCOL   nFirstSortCol   = aSortData.nCol1;
@@ -381,6 +382,12 @@ void ScTabPageSortFields::FillFieldLists( sal_uInt16 nStartField )
                     i++;
                 }
             }
+
+            for (sal_uInt16 j=nStartField; j < nSortKeyCount; ++j)
+            {
+                m_aSortWin.m_aSortKeyItems[j]->m_xLbSort->thaw();
+            }
+
             nFieldCount = i;
         }
     }
