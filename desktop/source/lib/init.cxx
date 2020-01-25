ff8ac5955114eb4cfdbda354fb36a36baf261ec4
Muhammet Kara <muhammet.kara@collabora.com>
2020-01-25 04:10:38 +0300

    lok: Add LOK_CALLBACK_STATE_CHANGED for uno:Orientation
    Change-Id: Iab3aaf742bdb9fc4b9c5875fee7d769fa0581eeb
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/87370
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Aron Budea <aron.budea@collabora.com>
    Tested-by: Aron Budea <aron.budea@collabora.com>

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -2655,7 +2655,8 @@ static void doc_iniUnoCommands ()
         OUString(".uno:OnlineAutoFormat"),
         OUString(".uno:InsertSymbol"),
         OUString(".uno:EditRegion"),
-        OUString(".uno:ThesaurusDialog")
+        OUString(".uno:ThesaurusDialog"),
+        OUString(".uno:Orientation")
     };
 
     util::URL aCommandURL;
