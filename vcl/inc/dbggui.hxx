6f3678f377d532860c0aed4ab97e151927e5b938
Eda Nur Var <var.eda.nur@gmail.com>
2020-01-27 02:26:40 +0300

    tdf#124176 Use pragma once instead of include guards
    Change-Id: Ie78f7a70ce08108b6897dbe46cb33fdbd197c3b0
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/87474
    Tested-by: Jenkins
    Reviewed-by: Muhammet Kara <muhammet.kara@collabora.com>

--- vcl/inc/dbggui.hxx
+++ vcl/inc/dbggui.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_VCL_INC_DBGGUI_HXX
-#define INCLUDED_VCL_INC_DBGGUI_HXX
+#pragma once
 
 #ifndef NDEBUG
 
@@ -26,7 +25,5 @@ void DbgGUIInitSolarMutexCheck();
 void DbgGUIDeInitSolarMutexCheck();
 
 #endif
-
-#endif // INCLUDED_VCL_INC_DBGGUI_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
