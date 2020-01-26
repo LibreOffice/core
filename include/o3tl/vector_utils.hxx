af29884072817481273c03373899624933fd567a
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2020-01-26 09:38:40 +0100

    move "remove duplicates" for a vector to o3tl/vector_utils.hxx
    Change-Id: I9ecf95a2bf975912b7029cdfb459a9ebc159c75d
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/87429
    Tested-by: Jenkins
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

---
+++ include/o3tl/vector_utils.hxx
@@ -0,0 +1,27 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#pragma once
+
+#include <vector>
+
+namespace o3tl
+{
+// removes duplicated elements in a vector
+template <typename T> void remove_duplicates(std::vector<T>& rVector)
+{
+    std::unordered_set<T> aSet;
+    auto aEnd = std::copy_if(rVector.begin(), rVector.end(), rVector.begin(),
+                             [&aSet](T const& rElement) { return aSet.insert(rElement).second; });
+    rVector.erase(aEnd, rVector.end());
+}
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
