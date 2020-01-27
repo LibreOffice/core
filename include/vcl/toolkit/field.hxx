a633f995f0dc4bb101ddb69cd9ae979be54e2ccc
Caolán McNamara <caolanm@redhat.com>
2020-01-27 20:22:05 +0000

    move private TimeBox to toolkit-only headers
    move private DateBox to toolkit-only headers
    move private CurrencyBox to toolkit-only headers
    move private PatternBox to toolkit-only headers
    move private CurrencyField to toolkit-only headers
    move private CurrencyFormatter to toolkit-only headers
    
    Change-Id: If5ba9e07b20cdc69ca554af388ec9d9e362cbf9a

---
+++ include/vcl/toolkit/field.hxx
@@ -0,0 +1,125 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#ifndef INCLUDED_VCL_TOOLKIT_FIELD_HXX
+#define INCLUDED_VCL_TOOLKIT_FIELD_HXX
+
+#if !defined(VCL_DLLIMPLEMENTATION) && !defined(TOOLKIT_DLLIMPLEMENTATION)
+#error "don't use this in new code"
+#endif
+
+#include <vcl/field.hxx>
+
+class VCL_DLLPUBLIC CurrencyFormatter : public NumericFormatter
+{
+protected:
+                            CurrencyFormatter(Edit* pEdit);
+    SAL_DLLPRIVATE void     ImplCurrencyReformat( const OUString& rStr, OUString& rOutStr );
+    virtual sal_Int64       GetValueFromString(const OUString& rStr) const override;
+
+public:
+    virtual                 ~CurrencyFormatter() override;
+
+    virtual void            Reformat() override;
+
+    virtual void            SetValue( sal_Int64 nNewValue ) override;
+    virtual OUString        CreateFieldText( sal_Int64 nValue ) const override;
+};
+
+class VCL_DLLPUBLIC CurrencyField final : public SpinField, public CurrencyFormatter
+{
+public:
+    CurrencyField( vcl::Window* pParent, WinBits nWinStyle );
+
+    virtual bool            PreNotify( NotifyEvent& rNEvt ) override;
+    virtual bool            EventNotify( NotifyEvent& rNEvt ) override;
+    virtual void            DataChanged( const DataChangedEvent& rDCEvt ) override;
+
+    virtual void            Modify() override;
+
+    virtual void            Up() override;
+    virtual void            Down() override;
+    virtual void            First() override;
+    virtual void            Last() override;
+    virtual void            dispose() override;
+};
+
+class VCL_DLLPUBLIC PatternBox final : public ComboBox, public PatternFormatter
+{
+public:
+                            PatternBox( vcl::Window* pParent, WinBits nWinStyle );
+
+    virtual bool            PreNotify( NotifyEvent& rNEvt ) override;
+    virtual bool            EventNotify( NotifyEvent& rNEvt ) override;
+
+    virtual void            Modify() override;
+
+    virtual void            ReformatAll() override;
+    virtual void            dispose() override;
+};
+
+class VCL_DLLPUBLIC CurrencyBox final : public ComboBox, public CurrencyFormatter
+{
+public:
+    explicit                CurrencyBox( vcl::Window* pParent, WinBits nWinStyle );
+
+    virtual bool            PreNotify( NotifyEvent& rNEvt ) override;
+    virtual bool            EventNotify( NotifyEvent& rNEvt ) override;
+    virtual void            DataChanged( const DataChangedEvent& rDCEvt ) override;
+
+    virtual void            Modify() override;
+
+    virtual void            ReformatAll() override;
+
+    virtual void            dispose() override;
+};
+
+class VCL_DLLPUBLIC DateBox final : public ComboBox, public DateFormatter
+{
+public:
+    explicit                DateBox( vcl::Window* pParent, WinBits nWinStyle );
+
+    virtual bool            PreNotify( NotifyEvent& rNEvt ) override;
+    virtual bool            EventNotify( NotifyEvent& rNEvt ) override;
+    virtual void            DataChanged( const DataChangedEvent& rDCEvt ) override;
+
+    virtual void            Modify() override;
+
+    virtual void            ReformatAll() override;
+    virtual void            dispose() override;
+};
+
+class VCL_DLLPUBLIC TimeBox final : public ComboBox, public TimeFormatter
+{
+public:
+    explicit                TimeBox( vcl::Window* pParent, WinBits nWinStyle );
+
+    virtual bool            PreNotify( NotifyEvent& rNEvt ) override;
+    virtual bool            EventNotify( NotifyEvent& rNEvt ) override;
+    virtual void            DataChanged( const DataChangedEvent& rDCEvt ) override;
+
+    virtual void            Modify() override;
+
+    virtual void            ReformatAll() override;
+    virtual void            dispose() override;
+};
+
+#endif // INCLUDED_VCL_FIELD_HXX
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
