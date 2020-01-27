453a6a78c4131395c4e32942d96b9d0cc45263b4
Luboš Luňák <l.lunak@collabora.com>
2020-01-27 22:25:13 +0100

    significantly reduce build time of sharedvisitor.cxx
    In the sharedvisitor.cxx mode all plugins need just one shared
    RecursiveASTVisitor template instance, but as long as they use another
    instance each as the base class, Clang still instantiates those templates
    and then spends a lot of time optimizing each of them, even though they
    should never get used.
    So when compiling using sharedvisitor.cxx simply use dummy base classes
    that do not do anything (and as an additional check they abort() if any
    of the functions get called).
    
    Change-Id: Ic5a350da2c3ba31521f71077b1776b1ee8f06dea

--- compilerplugins/clang/pluginhandler.cxx
+++ compilerplugins/clang/pluginhandler.cxx
@@ -142,7 +142,8 @@ void PluginHandler::createPlugins( std::set< std::string > rewriters )
         const char* name = plugins[i].optionName;
         // When in unit-test mode, ignore plugins whose names don't match the filename of the test,
         // so that we only generate warnings for the plugin that we want to test.
-        if (unitTestMode && mainFileName.find(plugins[ i ].optionName) == StringRef::npos)
+        if (unitTestMode && mainFileName.find(plugins[ i ].optionName) == StringRef::npos
+            && !plugins[ i ].isSharedPlugin)
             continue;
         if( rewriters.erase( name ) != 0 )
             plugins[ i ].object = plugins[ i ].create( InstantiationData { name, *this, compiler, &rewriter } );
