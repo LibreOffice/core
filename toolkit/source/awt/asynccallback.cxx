c6f5e12ee3f91234b064222272d756670f3bb207
Eda Nur Var <var.eda.nur@gmail.com>
2020-01-27 15:38:48 +0300

    tdf#88205 Adapt uses of css::uno::Sequence to use initializer_list ctor
    Change-Id: I9c9a9af78bab721f112c33e485c431aac11bece6
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/87523
    Tested-by: Michael Stahl <michael.stahl@cib.de>
    Reviewed-by: Michael Stahl <michael.stahl@cib.de>

--- toolkit/source/awt/asynccallback.cxx
+++ toolkit/source/awt/asynccallback.cxx
@@ -76,8 +76,7 @@ sal_Bool SAL_CALL AsyncCallback::supportsService(OUString const & serviceName)
 
 css::uno::Sequence< OUString > SAL_CALL AsyncCallback::getSupportedServiceNames()
 {
-    css::uno::Sequence< OUString > s { "com.sun.star.awt.AsyncCallback" };
-    return s;
+    return css::uno::Sequence< OUString >{ "com.sun.star.awt.AsyncCallback" };
 }
 
 // css::awt::XRequestCallback:
