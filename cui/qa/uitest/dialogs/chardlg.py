0e28584876c321f598fcb7e5a86c3e6294bdd857
Miklos Vajna <vmiklos@collabora.com>
2020-01-28 21:21:19 +0100

    sw: add UI for semi-transparent text
    Somewhat surprisingly the test passed without the fix, i.e. the uitest
    operated happily on a hidden widget; so explicitly assert that it's
    visible.
    
    (cherry picked from commit 6d6e80435fd7f71342c429a67759a7b7b280cc55)
    
    Change-Id: I321c7e0e3cb2d67a07724523c885d50577a116a5

--- cui/qa/uitest/dialogs/chardlg.py
+++ cui/qa/uitest/dialogs/chardlg.py
@@ -7,6 +7,7 @@
 from libreoffice.uno.propertyvalue import mkPropertyValues
 from uitest.framework import UITestCase
 from uitest.uihelper.common import select_pos
+from uitest.uihelper.common import get_state_as_dict
 
 # Test for cui/source/tabpages/chardlg.cxx.
 class Test(UITestCase):
@@ -44,6 +45,38 @@ class Test(UITestCase):
         # i.e. the dialog did not set transparency to 5%, instead it left the character color at
         # COL_AUTO.
         self.assertEqual(shape.CharTransparence, 5)
+        self.ui_test.close_doc()
+
+    def testSvxCharEffectsPageWriter(self):
+        # Start Writer.
+        self.ui_test.create_doc_in_start_center("writer")
+        doc = self.xUITest.getTopFocusWindow()
+        editWin = doc.getChild("writer_edit")
+        # Type a character and select it.
+        editWin.executeAction("TYPE", mkPropertyValues({"KEYCODE": "TAB"}))
+        editWin.executeAction("TYPE", mkPropertyValues({"TEXT": "t"}))
+        self.xUITest.executeCommand(".uno:SelectAll")
+
+        # Now use Format -> Character.
+        self.ui_test.execute_dialog_through_command(".uno:FontDialog")
+        xDialog = self.xUITest.getTopFocusWindow()
+        xTabs = xDialog.getChild("tabcontrol")
+        # Select RID_SVXPAGE_CHAR_EFFECTS.
+        select_pos(xTabs, "1")
+        xFontTransparency = xDialog.getChild("fonttransparencymtr")
+        # Without the accompanying fix in place, this test would have failed with:
+        # AssertionError: 'false' != 'true'
+        # i.e. the transparency widget was hidden.
+        self.assertEqual(get_state_as_dict(xFontTransparency)["Visible"], "true")
+        for _ in range(5):
+            xFontTransparency.executeAction("UP", tuple())
+        self.ui_test.close_dialog_through_button(xDialog.getChild("ok"))
+
+        # Verify the result.
+        component = self.ui_test.get_component()
+        paragraph = component.Text.createEnumeration().nextElement()
+
+        self.assertEqual(paragraph.CharTransparence, 5)
         self.ui_test.close_doc()
 
 # vim: set shiftwidth=4 softtabstop=4 expandtab:
