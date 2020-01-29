3c7d27b2d56f98d856476f4b2a1c6aa2f2c83e03
advaitranade <advaitranade4@gmail.com>
2020-01-29 23:50:21 +0530

    tdf#98705 removed GetCaseCorrectPathName
    Change-Id: I4025a8cf88de382a3c0f906ab5c66c9e2f06cb72

--- sal/osl/w32/file_url.hxx
+++ sal/osl/w32/file_url.hxx
@@ -59,11 +59,12 @@ DWORD GetCaseCorrectPathName (
     bool bCheckExistence
 );
 
-oslFileError osl_getSystemPathFromFileURL_ (
-    rtl_uString * strURL,
-    rtl_uString ** pustrPath,
-    bool bAllowRelative
-);
+DWORD GetCaseCorrectPathName (
+    LPCWSTR lpszShortPath,  // file name
+    LPWSTR  lpszLongPath,   // path buffer
+    DWORD   cchBuffer,      // size of path buffer
+    BOOL bCheckExistence
+ );
 
 extern oslMutex g_CurrentDirectoryMutex;
 
