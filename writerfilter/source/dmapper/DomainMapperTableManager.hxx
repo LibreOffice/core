44f91d085b1ee0fbb5789df004a3dd1d9526ede1
László Németh <nemeth@numbertext.org>
2020-02-03 13:54:57 +0100

    tdf#108272 DOCX table-only header: fix SAX parser error
    Floating tables in table-only headers are imported
    as non-floating ones after a SAX parser error. Now
    we import them as non-floating ones from the beginning
    to avoid of the parser error.
    
    Change-Id: I0a816a7af642f402a25ed53d9766b1e8b82db789
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/87874
    Tested-by: Jenkins
    Reviewed-by: László Németh <nemeth@numbertext.org>
    (cherry picked from commit 213d6390a2cc59d174173f4359c161625a9c4bdc)

--- writerfilter/source/dmapper/DomainMapperTableManager.hxx
+++ writerfilter/source/dmapper/DomainMapperTableManager.hxx
@@ -46,8 +46,8 @@ class DomainMapperTableManager : public TableManager
     sal_uInt32      m_nGridAfter; ///< number of grid columns in the parent table's table grid which shall be left after the last cell in the table row
     sal_Int32       m_nHeaderRepeat; //counter of repeated headers - if == -1 then the repeating stops
     sal_Int32       m_nTableWidth; //might be set directly or has to be calculated from the column positions
-    /// Are we in a shape (text append stack is not empty) or in the body document?
-    bool m_bIsInShape;
+    /// Unfloat tables in a shape/table-only header (text append stack is not empty)
+    bool m_bIsUnfloatTable;
     OUString m_sTableStyleName;
     /// Grab-bag of table look attributes for preserving.
     comphelper::SequenceAsHashMap m_aTableLook;
@@ -131,7 +131,7 @@ public:
 
     using TableManager::isInCell;
 
-    void setIsInShape(bool bIsInShape);
+    void setIsUnfloatTable(bool bIsUnfloatTable);
 
 };
 
