LibreOffice/core

commit f8966bb398cf0623be841c618b123866801a063c

tdf#130031 Chart OOXML import: fix area chart data label position
Default data label positioning of area charts in Excel is vertically
centered between the X axes and the data point. In LibreOffice
the data labels positioning was above the data point.

Change-Id: Icff3e2554dee7b5ee264bc6f9579a84852da6f7b
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/86927
Reviewed-by: László Németh <nemeth@numbertext.org>
Tested-by: László Németh <nemeth@numbertext.org>

chart2/source/view/charttypes/AreaChart.cxx
@@ -750,6 +750,7 @@ void AreaChart::createShapes()
                     if( rLogicYForNextSeriesMap.find(nAttachedAxisIndex) == rLogicYForNextSeriesMap.end() )
                         rLogicYForNextSeriesMap[nAttachedAxisIndex] = 0.0;
 
+                    double fPreviousYValue = rLogicYForNextSeriesMap[nAttachedAxisIndex];
                     fLogicY += rLogicYForNextSeriesMap[nAttachedAxisIndex];
                     rLogicYForNextSeriesMap[nAttachedAxisIndex] = fLogicY;
 
@@ -897,12 +898,18 @@ void AreaChart::createShapes()
                         if( pSeries->getDataPointLabelIfLabel(nIndex) )
                         {
                             LabelAlignment eAlignment = LABEL_ALIGN_TOP;
+                            sal_Int32 nLabelPlacement = pSeries->getLabelPlacement(
+                                nIndex, m_xChartTypeModel, rPosHelper.isSwapXAndY());
+
+                            if (m_bArea && nLabelPlacement == css::chart::DataLabelPlacement::CENTER)
+                            {
+                                fLogicY -= (fLogicY - fPreviousYValue) / 2.0;
+                                aScenePosition = rPosHelper.transformLogicToScene(fLogicX, fLogicY, fLogicZ, false);
+                            }
+
                             drawing::Position3D aScenePosition3D( aScenePosition.PositionX
                                     , aScenePosition.PositionY
                                     , aScenePosition.PositionZ+getTransformedDepth() );
-
-                            sal_Int32 nLabelPlacement = pSeries->getLabelPlacement(
-                                nIndex, m_xChartTypeModel, rPosHelper.isSwapXAndY());
 
                             switch(nLabelPlacement)
                             {
@@ -924,7 +931,6 @@ void AreaChart::createShapes()
                                     break;
                                 case css::chart::DataLabelPlacement::CENTER:
                                     eAlignment = LABEL_ALIGN_CENTER;
-                                    //todo implement this different for area charts
                                     break;
                                 default:
                                     OSL_FAIL("this label alignment is not implemented yet");

sw/qa/extras/layout/layout.cxx
@@ -2504,6 +2504,20 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf129173)
         pXmlDoc, "/metafile/push[1]/push[1]/push[1]/push[4]/push[1]/textarray[22]/text", "56");
 }
 
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf130031)
+{
+    SwDoc* pDoc = createDoc("tdf130031.docx");
+    SwDocShell* pShell = pDoc->GetDocShell();
+
+    // Dump the rendering of the first page as an XML file.
+    std::shared_ptr<GDIMetaFile> xMetaFile = pShell->GetPreviewMetaFile();
+    MetafileXmlDump dumper;
+    xmlDocPtr pXmlDoc = dumpAndParse(dumper, *xMetaFile);
+    CPPUNIT_ASSERT(pXmlDoc);
+    sal_Int32 nY = getXPath(pXmlDoc, "//textarray[11]", "y").toInt32();
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(4339, nY, 50);
+}
+
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf116925)
 {
     SwDoc* pDoc = createDoc("tdf116925.docx");

chart2/source/tools/ChartTypeHelper.cxx
@@ -314,9 +314,10 @@ uno::Sequence < sal_Int32 > ChartTypeHelper::getSupportedLabelPlacements( const 
     }
     else if( aChartTypeName.match(CHART2_SERVICE_NAME_CHARTTYPE_AREA) )
     {
-        aRet.realloc(1);
+        aRet.realloc(2);
         sal_Int32* pSeq = aRet.getArray();
         *pSeq++ = css::chart::DataLabelPlacement::TOP;
+        *pSeq++ = css::chart::DataLabelPlacement::CENTER;
     }
     else if( aChartTypeName.match(CHART2_SERVICE_NAME_CHARTTYPE_NET) )
     {

commit 5168e5f5515767941a69a9207e029600f78e402d

tdf#130380 Chart: Fix area chart data labels position
Regression from commit: f8966bb398cf0623be841c618b123866801a063c
(tdf#130031 Chart OOXML import: fix area chart data label position)

Change-Id: Iafcbacb0ecf6f8a175adbf0782ee3e3a1185a726

chart2/source/view/charttypes/AreaChart.cxx
@@ -903,7 +903,10 @@ void AreaChart::createShapes()
 
                             if (m_bArea && nLabelPlacement == css::chart::DataLabelPlacement::CENTER)
                             {
-                                fLogicY -= (fLogicY - fPreviousYValue) / 2.0;
+                                if (fPreviousYValue)
+                                    fLogicY -= (fLogicY - fPreviousYValue) / 2.0;
+                                else
+                                    fLogicY = (fLogicY + rPosHelper.getLogicMinY()) / 2.0;
                                 aScenePosition = rPosHelper.transformLogicToScene(fLogicX, fLogicY, fLogicZ, false);
                             }
 

sw/qa/extras/layout/layout.cxx
@@ -2550,6 +2550,25 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf130242)
     CPPUNIT_ASSERT_DOUBLES_EQUAL(3018, nY, 50);
 }
 
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf130380)
+{
+    SwDoc* pDoc = createDoc("tdf130380.docx");
+    SwDocShell* pShell = pDoc->GetDocShell();
+
+    // Dump the rendering of the first page as an XML file.
+    std::shared_ptr<GDIMetaFile> xMetaFile = pShell->GetPreviewMetaFile();
+    MetafileXmlDump dumper;
+    xmlDocPtr pXmlDoc = dumpAndParse(dumper, *xMetaFile);
+    CPPUNIT_ASSERT(pXmlDoc);
+    sal_Int32 nY = getXPath(pXmlDoc, "/metafile/push[1]/push[1]/push[1]/push[4]/push[1]/push[1]/polypolygon/polygon/point[1]", "y").toInt32();
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 6727
+    // - Actual  : 4411
+    // - Delta   : 50
+    // i.e. the area chart shrank.
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(6727, nY, 50);
+}
+
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf116925)
 {
     SwDoc* pDoc = createDoc("tdf116925.docx");