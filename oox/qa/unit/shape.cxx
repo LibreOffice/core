9e399b3b6077603f1731e0b4503e752386eac3ec
Miklos Vajna <vmiklos@collabora.com>
2020-02-05 21:09:09 +0100

    CppunitTest_oox_shape: remove unused includes
    Change-Id: I00f7b042779aa981a5a6390c02f6f4ede59f3c89

--- oox/qa/unit/shape.cxx
+++ oox/qa/unit/shape.cxx
@@ -10,20 +10,10 @@
 #include <test/bootstrapfixture.hxx>
 #include <unotest/macros_test.hxx>
 
-#include <com/sun/star/beans/XPropertySet.hpp>
 #include <com/sun/star/drawing/XDrawPagesSupplier.hpp>
-#include <com/sun/star/embed/XStorage.hpp>
 #include <com/sun/star/frame/Desktop.hpp>
-#include <com/sun/star/frame/XStorable.hpp>
-#include <com/sun/star/text/WritingMode2.hpp>
 
-#include <comphelper/embeddedobjectcontainer.hxx>
 #include <comphelper/processfactory.hxx>
-#include <comphelper/propertyvalue.hxx>
-#include <comphelper/scopeguard.hxx>
-#include <comphelper/storagehelper.hxx>
-#include <unotools/mediadescriptor.hxx>
-#include <unotools/tempfile.hxx>
 
 using namespace ::com::sun::star;
 
