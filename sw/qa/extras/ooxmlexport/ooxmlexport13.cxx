22f88b10b651ecb07e68b2a4dcf1b3e7f9649a00
Miklos Vajna <vmiklos@collabora.com>
2020-02-06 14:47:08 +0100

    DOCX export: fix handling of section starts that originally had headers
    Both the DOC import (in wwSectionManager::InsertSegments()) and DOCX
    import (in SectionPropertyMap::CloseSectionGroup()) have a mechanism to
    try to attach changed headers/footers from a continuous section break
    somewhere, so they are not lost.
    
    This means that even if the rendering of such documents is OK, explicit
    code is needed to undo the effect of the importer at export time, or
    those headers will be lost.
    
    Start doing this for the DOCX export case when the headers/footers are
    placed at the "previous-in-practice" paragraph, more cases (handled at
    the import side) can be added later.
    
    (cherry picked from commit 26f2a9e1a10a22e864e71ee7c94934821703e021)
    
    Conflicts:
    	sw/qa/extras/ooxmlexport/ooxmlexport14.cxx
    	sw/source/filter/ww8/wrtw8sty.cxx
    
    Change-Id: Ic2304a74919d18da3ba9cb4afe301e0247a50dc2

--- sw/qa/extras/ooxmlexport/ooxmlexport13.cxx
+++ sw/qa/extras/ooxmlexport/ooxmlexport13.cxx
@@ -367,7 +367,7 @@ DECLARE_OOXMLIMPORT_TEST(testTdf128207, "tdf128207.docx")
     CPPUNIT_ASSERT_EQUAL(sal_Int32(12), getProperty<sal_Int32>(getShape(1), "HoriOrientPosition"));
 }
 
-DECLARE_OOXMLIMPORT_TEST(testContSectBreakHeaderFooter, "cont-sect-break-header-footer.docx")
+DECLARE_OOXMLEXPORT_TEST(testContSectBreakHeaderFooter, "cont-sect-break-header-footer.docx")
 {
     // Load a document with a continuous section break on page 2.
     CPPUNIT_ASSERT_EQUAL(OUString("First page header, section 1"),
@@ -391,6 +391,11 @@ DECLARE_OOXMLIMPORT_TEST(testContSectBreakHeaderFooter, "cont-sect-break-header-
     // the own footer text.
     CPPUNIT_ASSERT_EQUAL(OUString("Footer, section 3"),
                          parseDump("/root/page[3]/footer/txt/text()"));
+
+    // Without the export fix in place, the import-export-import test would have failed with:
+    // - Expected: Header, section 2
+    // - Actual  : First page header, section 2
+    // i.e. both the header and the footer on page 3 was wrong.
 }
 
 CPPUNIT_TEST_FIXTURE(SwModelTestBase, testZeroLineSpacing)
