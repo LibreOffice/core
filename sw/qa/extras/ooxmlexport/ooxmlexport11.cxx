4af97fd43ae8761b39a8e7bdb49915177d5c07ea
László Németh <nemeth@numbertext.org>
2020-02-07 15:06:50 +0100

    tdf#130494: DOCX import: limit paragraph-level character property
    expansion for the whole table paragraph based on the
    last character context.
    
    regression from 2ab481b038b62b1ff576ac4d49d03c1798cd7f84
    (tdf#90069 DOCX: fix character style of new table rows)
    
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/88199
    Tested-by: Jenkins
    Reviewed-by: László Németh <nemeth@numbertext.org>
    (cherry picked from commit abb9c7db8bcc06f907d39a7811711882161d5803)
    
    Change-Id: I49da23c268436488ff1537771869c38108113c12
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/103198
    Tested-by: Gabor Kelemen <kelemen.gabor2@nisz.hu>
    Reviewed-by: Gabor Kelemen <kelemen.gabor2@nisz.hu>

--- sw/qa/extras/ooxmlexport/ooxmlexport11.cxx
+++ sw/qa/extras/ooxmlexport/ooxmlexport11.cxx
@@ -1133,6 +1133,14 @@ DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testTdf88496, "tdf88496.docx")
     CPPUNIT_ASSERT_EQUAL(3, getPages());
 }
 
+DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testTdf130494, "tdf130494.docx")
+{
+    xmlDocPtr pXmlDoc = parseExport("word/document.xml");
+    assertXPath(pXmlDoc, "/w:document/w:body/w:tbl/w:tr/w:tc[1]/w:p/w:pPr/w:rPr/w:highlight", "val", "yellow");
+    // keep direct formatting of table cell paragraph with removed highlighting
+    assertXPath(pXmlDoc, "/w:document/w:body/w:tbl/w:tr/w:tc[1]/w:p/w:r/w:rPr/w:highlight", 0);
+}
+
 DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testTdf121597TrackedDeletionOfMultipleParagraphs, "tdf121597.odt")
 {
     xmlDocPtr pXmlDoc = parseExport("word/document.xml");
