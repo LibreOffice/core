bf0c834f9532083ebce737de1a4184433155148f
Kohei Yoshida <kohei@libreoffice.org>
2020-02-06 21:38:19 -0500

    Update mdds to 1.6.0.
    Change-Id: Iff350b47d5d237e0bb3ffcce0e80ce867c74191d

--- external/mdds/UnpackedTarball_mdds.mk
+++ external/mdds/UnpackedTarball_mdds.mk
@@ -11,12 +11,10 @@ $(eval $(call gb_UnpackedTarball_UnpackedTarball,mdds))
 
 $(eval $(call gb_UnpackedTarball_set_tarball,mdds,$(MDDS_TARBALL)))
 
-$(eval $(call gb_UnpackedTarball_set_patchlevel,mdds,0))
+$(eval $(call gb_UnpackedTarball_set_patchlevel,mdds,1))
 
-# external/mdds/Wdeprecated-copy.patch upstream at <https://gitlab.com/mdds/mdds/merge_requests/21>
-# "Remove redundantly user-declared private_data copy constructor":
 $(eval $(call gb_UnpackedTarball_add_patches,mdds,\
-    external/mdds/Wdeprecated-copy.patch \
+    external/mdds/remove-warnings.diff \
 ))
 
 # vim: set noet sw=4 ts=4:
