5b6590a866e65bceeeb982c29118979e79258f5f
David Tardon <dtardon@redhat.com>
2020-02-07 18:45:39 +0100

    upload libcdr 0.1.6
    Change-Id: I569f8ed92358982edbc419a6b687652ffd4fcb14

--- external/libcdr/UnpackedTarball_libcdr.mk
+++ external/libcdr/UnpackedTarball_libcdr.mk
@@ -15,13 +15,10 @@ $(eval $(call gb_UnpackedTarball_set_patchlevel,libcdr,0))
 
 $(eval $(call gb_UnpackedTarball_update_autoconf_configs,libcdr))
 
-# * external/libcdr/0001-Add-missing-include.patch.1 is from upstream master (see content for
-#   details);
 # * external/libcdr/ubsan.patch is upstream at <https://gerrit.libreoffice.org/#/c/73182/> "Avoid UB
 #   converting from double to int via unsigned":
 $(eval $(call gb_UnpackedTarball_add_patches,libcdr, \
     external/libcdr/libcdr-visibility-win.patch \
-    external/libcdr/0001-Add-missing-include.patch.1 \
     external/libcdr/ubsan.patch \
 ))
 
