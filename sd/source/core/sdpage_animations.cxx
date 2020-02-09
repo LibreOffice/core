f5ebd75e4ac0f23be4034a7f15df4c16ac668b5a
Andrea Gelmini <andrea.gelmini@gelma.net>
2020-02-09 16:06:30 +0100

    Fix typo
    Change-Id: Ic0d026ceeedc4118a04f22a9d82a0c85321d131a

--- sd/source/core/sdpage_animations.cxx
+++ sd/source/core/sdpage_animations.cxx
@@ -113,7 +113,7 @@ void SdPage::onParagraphInserted( ::Outliner* pOutliner, Paragraph const * pPara
         ParagraphTarget aTarget;
         aTarget.Shape.set( pObj->getUnoShape(), UNO_QUERY );
         /* FIXME: Paragraph should be sal_Int32, though more than 64k
-         * paragrapsh at a shape are unlikely... */
+         * paragraph at a shape are unlikely... */
         aTarget.Paragraph = static_cast<sal_Int16>(pOutliner->GetAbsPos( pPara ));
 
         getMainSequence()->insertTextRange( makeAny( aTarget ) );
@@ -128,7 +128,7 @@ void SdPage::onParagraphRemoving( ::Outliner* pOutliner, Paragraph const * pPara
         ParagraphTarget aTarget;
         aTarget.Shape.set( pObj->getUnoShape(), UNO_QUERY );
         /* FIXME: Paragraph should be sal_Int32, though more than 64k
-         * paragrapsh at a shape are unlikely... */
+         * paragraph at a shape are unlikely... */
         aTarget.Paragraph = static_cast<sal_Int16>(pOutliner->GetAbsPos( pPara ));
 
         getMainSequence()->disposeTextRange( makeAny( aTarget ) );
