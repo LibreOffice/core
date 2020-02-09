80d3018272f5e5b170536eee2f2254070a9c1883
Caolán McNamara <caolanm@redhat.com>
2020-02-09 20:46:52 +0000

    wip: weld SvxFmAbsRecWin item window
    Change-Id: I6a11d8ba226b28447e2ab04925090491d6953132

--- svx/UIConfig_svx.mk
+++ svx/UIConfig_svx.mk
@@ -10,6 +10,7 @@
 $(eval $(call gb_UIConfig_UIConfig,svx))
 
 $(eval $(call gb_UIConfig_add_uifiles,svx,\
+	svx/uiconfig/ui/absrecbox \
 	svx/uiconfig/ui/acceptrejectchangesdialog \
 	svx/uiconfig/ui/accessibilitycheckdialog \
 	svx/uiconfig/ui/accessibilitycheckentry \
