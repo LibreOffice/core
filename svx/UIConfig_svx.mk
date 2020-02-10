c3e2d2dc85739e839ec261b6fef6fb50c51d8a8d
Caolán McNamara <caolanm@redhat.com>
2020-02-10 11:36:53 +0000

    wip: weld FillControl item window
    Change-Id: I5f943a9cf881494fe8a9fecea6a957a7085a5582

--- svx/UIConfig_svx.mk
+++ svx/UIConfig_svx.mk
@@ -47,6 +47,7 @@ $(eval $(call gb_UIConfig_add_uifiles,svx,\
 	svx/uiconfig/ui/docrecoveryrecoverdialog \
 	svx/uiconfig/ui/docrecoverysavedialog \
 	svx/uiconfig/ui/extrustiondepthdialog \
+	svx/uiconfig/ui/fillctrlbox \
 	svx/uiconfig/ui/filtermenu \
 	svx/uiconfig/ui/findreplacedialog \
 	svx/uiconfig/ui/floatingareastyle \
