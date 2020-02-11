38ef425f04ec518cbe7785f88267e25d94b803dc
Jan-Marek Glogowski <jan-marek.glogowski@extern.cib.de>
2020-02-11 14:19:06 +0100

    neon: drop special whitespace href handling
    Since whitespace (0x20, 0x09) in the URI path is now included in
    the neon_uri_parse_allow_others.patch to accept SharePoint IRIs
    as valid URIs, drop that hunk and document a link to the upstream
    discussion in the Makefile.
    
    Change-Id: I7b776e9bcaa716069b0f83a97499cf49e5ed1644
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/88447
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>
    Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>
    Tested-by: Jenkins

--- external/neon/UnpackedTarball_neon.mk
+++ external/neon/UnpackedTarball_neon.mk
@@ -17,6 +17,10 @@ $(eval $(call gb_UnpackedTarball_add_files,neon,src,\
 
 $(eval $(call gb_UnpackedTarball_set_patchlevel,neon,0))
 
+# For the upstream discussion of neon_uri_parse_allow_others.patch see
+# http://lists.manyfish.co.uk/pipermail/neon/2020-February/001629.html
+# or thread "RfC: Accept SharePoint href IRIs"
+
 $(eval $(call gb_UnpackedTarball_add_patches,neon,\
 	external/neon/neon.patch \
 	external/neon/neon_ne_set_request_flag.patch \
