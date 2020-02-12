4d59f79bd3993c59cd04707db8f95787554b3a63
Caolán McNamara <caolanm@redhat.com>
2020-02-12 20:18:00 +0000

    tdf#130623 owner of FieldControl needs to set its allocation
    which works fine automatically when its in a dialog (copy table)
    but not automatically when hosted inside the design view
    
    this will make the gen case work in master, and the gen and gtk
    case work in 6-4
    
    Change-Id: I530cfa0a7e1f5b62c6812ffde255c1edf478930d
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/88499
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@cib.de>
    (cherry picked from commit 96018929add55cff71a036be3a67df385b023471)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/88694
    Reviewed-by: Xisco Faulí <xiscofauli@libreoffice.org>
    Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>
    Tested-by: Michael Stahl <michael.stahl@cib.de>

--- dbaccess/source/ui/tabledesign/FieldDescGenWin.cxx
+++ dbaccess/source/ui/tabledesign/FieldDescGenWin.cxx
@@ -34,6 +34,10 @@ OFieldDescGenWin::OFieldDescGenWin( vcl::Window* pParent, OTableDesignHelpBar* p
     m_pFieldControl = VclPtr<OTableFieldControl>::Create(this,pHelp);
     m_pFieldControl->SetHelpId(HID_TAB_DESIGN_FIELDCONTROL);
     m_pFieldControl->Show();
+
+    maLayoutIdle.SetPriority(TaskPriority::RESIZE);
+    maLayoutIdle.SetInvokeHandler( LINK( this, OFieldDescGenWin, ImplHandleLayoutTimerHdl ) );
+    maLayoutIdle.SetDebugName( "OFieldDescGenWin maLayoutIdle" );
 }
 
 OFieldDescGenWin::~OFieldDescGenWin()
@@ -43,6 +47,7 @@ OFieldDescGenWin::~OFieldDescGenWin()
 
 void OFieldDescGenWin::dispose()
 {
+    maLayoutIdle.Stop();
     m_pFieldControl.disposeAndClear();
     TabPage::dispose();
 }
@@ -54,10 +59,27 @@ void OFieldDescGenWin::Init()
     m_pFieldControl->Init();
 }
 
-void OFieldDescGenWin::Resize()
+void OFieldDescGenWin::queue_resize(StateChangedType eReason)
 {
+    TabPage::queue_resize(eReason);
+    if (!m_pFieldControl)
+        return;
+    if (maLayoutIdle.IsActive())
+        return;
+    maLayoutIdle.Start();
+}
+
+IMPL_LINK_NOARG(OFieldDescGenWin, ImplHandleLayoutTimerHdl, Timer*, void)
+{
+    if (!m_pFieldControl)
+        return;
     m_pFieldControl->SetPosSizePixel(Point(0,0),GetSizePixel());
     m_pFieldControl->Resize();
+}
+
+void OFieldDescGenWin::Resize()
+{
+    queue_resize();
 }
 
 void OFieldDescGenWin::SetReadOnly( bool bReadOnly )
