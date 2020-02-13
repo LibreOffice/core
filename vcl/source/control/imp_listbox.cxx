f475d0574a2df630b2940f0204bb13199f823706
Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
2020-02-13 07:38:54 +0100

    tdf#130325 Fix listbox used as dropdown
    In this case, the listbox is not used as a static widget,
    also not as a combobox with dropdown.
    Instead the listbox is placed in a popup to choose values from.
    Need to handle this case similiar to the combobox dropdown
    (Cursor movement only travels through items, Return selects one item).
    
    Regression from 7de9417d5f65d35227c7f80f6d587c2a56bde4e0
    
    Change-Id: Idadc3da5847e12e1408203b13ab59eb53fe14eea
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/88581
    Tested-by: Jenkins
    Reviewed-by: Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
    (cherry picked from commit d92463c88a557eea7a439def39659b1409772583)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/88504
    Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>

--- vcl/source/control/imp_listbox.cxx
+++ vcl/source/control/imp_listbox.cxx
@@ -488,7 +488,7 @@ ImplListBoxWindow::ImplListBoxWindow( vcl::Window* pParent, WinBits nWinStyle ) 
     mbCenter            = ( nWinStyle & WB_CENTER );
     mbSimpleMode        = ( nWinStyle & WB_SIMPLEMODE );
     mbSort              = ( nWinStyle & WB_SORT );
-    mbIsComboboxDropdown = ( nWinStyle & WB_DROPDOWN );
+    mbIsDropdown        = ( nWinStyle & WB_DROPDOWN ) ||  ( nWinStyle & WB_LISTBOX_POPUP );
     mbEdgeBlending      = false;
 
     // pb: #106948# explicit mirroring for calc
@@ -1640,7 +1640,7 @@ bool ImplListBoxWindow::ProcessKeyInput( const KeyEvent& rKEvt )
         if(SelectEntries( nSelect, eLET, bShift, bCtrl, bCurPosChange))
         {
             // tdf#129043 Correctly deliver events when changing values with arrow keys in combobox
-            if (mbIsComboboxDropdown && IsReallyVisible())
+            if (mbIsDropdown && IsReallyVisible())
                 mbTravelSelect = true;
             mnSelectModifier = rKEvt.GetKeyCode().GetModifier();
             ImplCallSelect();
