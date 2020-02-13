LibreOffice/core

commit 7de9417d5f65d35227c7f80f6d587c2a56bde4e0

tdf#129043 Correctly deliver combo box events when used with keyboard
Change-Id: I82186f999e74be4aebd59d77666390a7d5e8ad81
Reviewed-on: https://gerrit.libreoffice.org/83923
Tested-by: Jenkins
Reviewed-by: Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>

vcl/inc/listbox.hxx
@@ -220,6 +220,7 @@ private:
     bool mbCenter : 1;           ///< center Text output
     bool mbRight : 1;            ///< right align Text output
     bool mbEdgeBlending : 1;
+    bool mbIsComboboxDropdown : 1;
 
     Link<ImplListBoxWindow*,void>  maScrollHdl;
     Link<LinkParamNone*,void>      maSelectHdl;

vcl/source/control/imp_listbox.cxx
@@ -483,6 +483,7 @@ ImplListBoxWindow::ImplListBoxWindow( vcl::Window* pParent, WinBits nWinStyle ) 
     mbCenter            = ( nWinStyle & WB_CENTER );
     mbSimpleMode        = ( nWinStyle & WB_SIMPLEMODE );
     mbSort              = ( nWinStyle & WB_SORT );
+    mbIsComboboxDropdown = ( nWinStyle & WB_DROPDOWN );
     mbEdgeBlending      = false;
 
     // pb: #106948# explicit mirroring for calc
@@ -1637,7 +1638,9 @@ bool ImplListBoxWindow::ProcessKeyInput( const KeyEvent& rKEvt )
         mnCurrentPos = nSelect;
         if(SelectEntries( nSelect, eLET, bShift, bCtrl, bCurPosChange))
         {
-            mbTravelSelect = true;
+            // tdf#129043 Correctly deliver events when changing values with arrow keys in combobox
+            if (mbIsComboboxDropdown && IsReallyVisible())
+                mbTravelSelect = true;
             mnSelectModifier = rKEvt.GetKeyCode().GetModifier();
             ImplCallSelect();
             mbTravelSelect = false;

vcl/source/control/combobox.cxx
@@ -749,7 +749,7 @@ bool ComboBox::EventNotify( NotifyEvent& rNEvt )
 
             case KEY_RETURN:
             {
-                if ((rNEvt.GetWindow() == m_pImpl->m_pSubEdit) && IsInDropDown())
+                if (rNEvt.GetWindow() == m_pImpl->m_pSubEdit)
                 {
                     m_pImpl->m_pImplLB->ProcessKeyInput( aKeyEvt );
                     bDone = true;

commit c9c4e50ea9e26b969dac6d025c39220827c58682

tdf#130325 Fix listbox used as dropdown
In this case, the listbox is not used as a static widget,
also not as a combobox with dropdown.
Instead the listbox is placed in a popup to choose values from.
Need to handle this case similiar to the combobox dropdown
(Cursor movement only travels through items, Return selects one item).

Regression from 7de9417d5f65d35227c7f80f6d587c2a56bde4e0

Change-Id: Idadc3da5847e12e1408203b13ab59eb53fe14eea

include/tools/wintypes.hxx
@@ -204,6 +204,8 @@ WinBits const WB_IGNORETAB =            0x20000000;
 
 // Window-Bits for ListBox and MultiListBox
 WinBits const WB_SIMPLEMODE =           0x20000000;
+// Special case where the listbox is used as a dropdown popup (not a combobox)
+WinBits const WB_LISTBOX_POPUP =        0x40000000;
 
 // Window-Bits for FixedBitmap
 WinBits const WB_SCALE =                0x08000000;

sc/source/ui/view/gridwin.cxx
@@ -222,7 +222,7 @@ public:
 //  ListBox in a FloatingWindow (pParent)
 ScFilterListBox::ScFilterListBox( vcl::Window* pParent, ScGridWindow* pGrid,
                                   SCCOL nNewCol, SCROW nNewRow, ScFilterBoxMode eNewMode ) :
-    ListBox( pParent, WB_AUTOHSCROLL ),
+    ListBox( pParent, WB_AUTOHSCROLL | WB_LISTBOX_POPUP ),
     pGridWin( pGrid ),
     nCol( nNewCol ),
     nRow( nNewRow ),

vcl/inc/listbox.hxx
@@ -222,7 +222,8 @@ private:
     bool mbCenter : 1;           ///< center Text output
     bool mbRight : 1;            ///< right align Text output
     bool mbEdgeBlending : 1;
-    bool mbIsComboboxDropdown : 1;
+    /// Listbox is actually a dropdown (either combobox, or popup window treated as dropdown)
+    bool mbIsDropdown : 1;
 
     Link<ImplListBoxWindow*,void>  maScrollHdl;
     Link<LinkParamNone*,void>      maSelectHdl;

vcl/source/control/imp_listbox.cxx
@@ -469,7 +469,7 @@ ImplListBoxWindow::ImplListBoxWindow( vcl::Window* pParent, WinBits nWinStyle ) 
     mbCenter            = ( nWinStyle & WB_CENTER );
     mbSimpleMode        = ( nWinStyle & WB_SIMPLEMODE );
     mbSort              = ( nWinStyle & WB_SORT );
-    mbIsComboboxDropdown = ( nWinStyle & WB_DROPDOWN );
+    mbIsDropdown        = ( nWinStyle & WB_DROPDOWN ) ||  ( nWinStyle & WB_LISTBOX_POPUP );
     mbEdgeBlending      = false;
 
     // pb: #106948# explicit mirroring for calc
@@ -1625,7 +1625,7 @@ bool ImplListBoxWindow::ProcessKeyInput( const KeyEvent& rKEvt )
         if(SelectEntries( nSelect, eLET, bShift, bCtrl, bCurPosChange))
         {
             // tdf#129043 Correctly deliver events when changing values with arrow keys in combobox
-            if (mbIsComboboxDropdown && IsReallyVisible())
+            if (mbIsDropdown && IsReallyVisible())
                 mbTravelSelect = true;
             mnSelectModifier = rKEvt.GetKeyCode().GetModifier();
             ImplCallSelect();