LibreOffice/core

commit 7869b3d6e4b24a0567ad2e492038d74c03b06b7d

related tdf#51671, store new "hide legend" feature also in ODF
Change-Id: Ibf55f02eccdcadb2d42f5aff8d72bff20ada3b3a
Reviewed-on: https://gerrit.libreoffice.org/64792
Tested-by: Jenkins
Reviewed-by: Markus Mohrhard <markus.mohrhard@googlemail.com>

xmloff/source/chart/SchXMLExport.cxx
@@ -2669,6 +2669,19 @@ void SchXMLExportHelper_Impl::exportSeries(
                                     // #i75297# allow empty series, export empty range to have all ranges on import
                                     mrExport.AddAttribute( XML_NAMESPACE_CHART, XML_VALUES_CELL_RANGE_ADDRESS, OUString());
 
+                                const SvtSaveOptions::ODFDefaultVersion nCurrentODFVersion( SvtSaveOptions().GetODFDefaultVersion() );
+                                if( nCurrentODFVersion >= SvtSaveOptions::ODFVER_012 )
+                                {
+                                    if (xPropSet.is())
+                                    {
+                                        Any aAny = xPropSet->getPropertyValue("ShowLegendEntry");
+                                        if (!aAny.get<bool>())
+                                        {
+                                            mrExport.AddAttribute(XML_NAMESPACE_LO_EXT, XML_HIDE_LEGEND, OUString::boolean(true));
+                                        }
+                                    }
+                                }
+
                                 if (xLabelSeq.is())
                                 {
                                     // Check if the label is direct string value rather than a reference.

include/xmloff/xmltoken.hxx
@@ -969,6 +969,7 @@ namespace xmloff { namespace token {
         XML_HIDDEN_PARAGRAPH,
         XML_HIDDEN_TEXT,
         XML_HIDE,
+        XML_HIDE_LEGEND,
         XML_HIDE_SHAPE,
         XML_HIDE_TEXT,
         XML_HIGHLIGHTED_RANGE,

xmloff/inc/SchXMLImport.hxx
@@ -136,7 +136,8 @@ enum SchXMLSeriesAttrMap
     XML_TOK_SERIES_LABEL_STRING,
     XML_TOK_SERIES_ATTACHED_AXIS,
     XML_TOK_SERIES_STYLE_NAME,
-    XML_TOK_SERIES_CHART_CLASS
+    XML_TOK_SERIES_CHART_CLASS,
+    XML_TOK_SERIES_HIDE_LEGEND
 };
 
 enum SchXMLRegEquationAttrMap

xmloff/source/chart/SchXMLImport.cxx
@@ -319,10 +319,11 @@ const SvXMLTokenMap& SchXMLImportHelper::GetSeriesAttrTokenMap()
 {
     { XML_NAMESPACE_CHART,  XML_VALUES_CELL_RANGE_ADDRESS,  XML_TOK_SERIES_CELL_RANGE    },
     { XML_NAMESPACE_CHART,  XML_LABEL_CELL_ADDRESS,         XML_TOK_SERIES_LABEL_ADDRESS },
-    { XML_NAMESPACE_LO_EXT,  XML_LABEL_STRING,         XML_TOK_SERIES_LABEL_STRING },
+    { XML_NAMESPACE_LO_EXT, XML_LABEL_STRING,               XML_TOK_SERIES_LABEL_STRING  },
     { XML_NAMESPACE_CHART,  XML_ATTACHED_AXIS,              XML_TOK_SERIES_ATTACHED_AXIS },
     { XML_NAMESPACE_CHART,  XML_STYLE_NAME,                 XML_TOK_SERIES_STYLE_NAME    },
     { XML_NAMESPACE_CHART,  XML_CLASS,                      XML_TOK_SERIES_CHART_CLASS   },
+    { XML_NAMESPACE_LO_EXT, XML_HIDE_LEGEND,                XML_TOK_SERIES_HIDE_LEGEND   },
     XML_TOKEN_MAP_END
 };
 

xmloff/source/chart/SchXMLSeries2Context.cxx
@@ -297,6 +297,7 @@ void SchXMLSeries2Context::StartElement( const uno::Reference< xml::sax::XAttrib
     bool bHasRange = false;
     OUString aSeriesLabelRange;
     OUString aSeriesLabelString;
+    bool bHideLegend = false;
 
     for( sal_Int16 i = 0; i < nAttrCount; i++ )
     {
@@ -346,6 +347,9 @@ void SchXMLSeries2Context::StartElement( const uno::Reference< xml::sax::XAttrib
                         maSeriesChartTypeName = aClassName;
                 }
                 break;
+            case XML_TOK_SERIES_HIDE_LEGEND:
+                bHideLegend = aValue.toBoolean();
+                break;
         }
     }
 
@@ -390,21 +394,25 @@ void SchXMLSeries2Context::StartElement( const uno::Reference< xml::sax::XAttrib
             SchXMLImportHelper::GetNewDataSeries( mxNewDoc, nCoordinateSystemIndex, maSeriesChartTypeName, ! mrGlobalChartTypeUsedBySeries ));
         Reference< chart2::data::XLabeledDataSequence > xLabeledSeq( SchXMLTools::GetNewLabeledDataSequence(), uno::UNO_QUERY_THROW );
 
-        if( bIsCandleStick )
+        Reference< beans::XPropertySet > xSeriesProp( m_xSeries, uno::UNO_QUERY );
+        if (xSeriesProp.is())
         {
-            // set default color for range-line to black (before applying styles)
-            Reference< beans::XPropertySet > xSeriesProp( m_xSeries, uno::UNO_QUERY );
-            if( xSeriesProp.is())
+            if (bHideLegend)
+                xSeriesProp->setPropertyValue("ShowLegendEntry", uno::makeAny(false));
+
+            if( bIsCandleStick )
+            {
+                // set default color for range-line to black (before applying styles)
                 xSeriesProp->setPropertyValue("Color",
-                                               uno::makeAny( sal_Int32( 0x000000 ))); // black
-        }
-        else if ( maSeriesChartTypeName == "com.sun.star.chart2.PieChartType" )
-        {
-            //@todo: this property should be saved
-            Reference< beans::XPropertySet > xSeriesProp( m_xSeries, uno::UNO_QUERY );
-            if( xSeriesProp.is())
+                        uno::makeAny( sal_Int32( 0x000000 ))); // black
+            }
+            else if ( maSeriesChartTypeName == "com.sun.star.chart2.PieChartType" )
+            {
+                //@todo: this property should be saved
                 xSeriesProp->setPropertyValue("VaryColorsByPoint",
-                                               uno::makeAny( true ));
+                        uno::makeAny( true ));
+            }
+
         }
 
         Reference<chart2::data::XDataProvider> xDataProvider(mxNewDoc->getDataProvider());

xmloff/source/core/xmltoken.cxx
@@ -969,6 +969,7 @@ namespace xmloff { namespace token {
         TOKEN( "hidden-paragraph",                XML_HIDDEN_PARAGRAPH ),
         TOKEN( "hidden-text",                     XML_HIDDEN_TEXT ),
         TOKEN( "hide",                            XML_HIDE ),
+        TOKEN( "hide-legend",                     XML_HIDE_LEGEND ),
         TOKEN( "hide-shape",                      XML_HIDE_SHAPE ),
         TOKEN( "hide-text",                       XML_HIDE_TEXT ),
         TOKEN( "highlighted-range",               XML_HIGHLIGHTED_RANGE ),

commit ef7c5159667f7dca1e13b353047308c2ee8c20b1

chart: don't export LO_EXT hide-legend in ODF 1.2
Regressions from

commit 7869b3d6e4b24a0567ad2e492038d74c03b06b7d
(related tdf#51671, store new "hide legend"
feature also in ODF)

and

commit a96ec04a07c35338f5f9a0cb361b9322e5ca9cec
(tdf#130225 implement ODF export of deleted legend
entries of pie charts)

Change-Id: I1d2847ae3e3ab7ccfbdb3841d94a0c1d79ded31f

xmloff/source/chart/SchXMLExport.cxx
@@ -2641,7 +2641,7 @@ void SchXMLExportHelper_Impl::exportSeries(
                                     mrExport.AddAttribute( XML_NAMESPACE_CHART, XML_VALUES_CELL_RANGE_ADDRESS, OUString());
 
                                 const SvtSaveOptions::ODFDefaultVersion nCurrentODFVersion( SvtSaveOptions().GetODFDefaultVersion() );
-                                if( nCurrentODFVersion >= SvtSaveOptions::ODFVER_012 )
+                                if( nCurrentODFVersion > SvtSaveOptions::ODFVER_012 )//do not export to ODF 1.2 or older
                                 {
                                     if (xPropSet.is())
                                     {
@@ -3195,7 +3195,7 @@ void SchXMLExportHelper_Impl::exportDataPoints(
         xSeriesProperties->getPropertyValue("VaryColorsByPoint") >>= bVaryColorsByPoint;
 
         const SvtSaveOptions::ODFDefaultVersion nCurrentODFVersion( SvtSaveOptions().GetODFDefaultVersion() );
-        if( nCurrentODFVersion >= SvtSaveOptions::ODFVER_012 )
+        if( nCurrentODFVersion > SvtSaveOptions::ODFVER_012 )//do not export to ODF 1.2 or older
             xSeriesProperties->getPropertyValue("DeletedLegendEntries") >>= deletedLegendEntriesSeq;
     }
 