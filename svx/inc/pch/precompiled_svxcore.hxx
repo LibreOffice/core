3688235a8d059285ce8dcf6d2fb6c522b0b6501a
Caolán McNamara <caolanm@redhat.com>
2020-02-13 17:02:19 +0000

    weld SearchFormattedControl Item Window
    Change-Id: Ifda9e514ae0329a756ec365a40ec1ccfcfea14ba
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/88621
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>

--- svx/inc/pch/precompiled_svxcore.hxx
+++ svx/inc/pch/precompiled_svxcore.hxx
@@ -13,7 +13,7 @@
  manual changes will be rewritten by the next run of update_pch.sh (which presumably
  also fixes all possible problems, so it's usually better to use it).
 
- Generated on 2020-02-13 15:10:47 using:
+ Generated on 2020-02-13 17:01:33 using:
  ./bin/update_pch svx svxcore --cutoff=7 --exclude:system --include:module --exclude:local
 
  If after updating build fails, use the following command to locate conflicting headers:
@@ -62,6 +62,7 @@
 #include <osl/getglobalmutex.hxx>
 #include <osl/interlck.h>
 #include <osl/mutex.hxx>
+#include <osl/thread.h>
 #include <osl/time.h>
 #include <rtl/alloc.h>
 #include <rtl/character.hxx>
