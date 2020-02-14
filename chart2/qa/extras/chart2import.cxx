8163f4ad81487ae187d00d3ce86214e520cb8b69
Balazs Varga <balazs.varga991@gmail.com>
2020-02-14 14:06:04 +0100

    tdf#130657 OOXML chart import: fix charts without CrossBetween
    by using default values for ShiftedCategoryPosition.
    
    Regression from commit 111c260ab2883b7906f1a66e222dbf4dc3c58c4f
    (tdf#127777 OOXML chart export: fix "CrossBetween" for not imported charts)
    
    Change-Id: Idd48a8343e7d6609b487daf7182e9716f2f5e01f
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/88709
    Tested-by: László Németh <nemeth@numbertext.org>
    Reviewed-by: László Németh <nemeth@numbertext.org>

--- chart2/qa/extras/chart2import.cxx
+++ chart2/qa/extras/chart2import.cxx
@@ -141,6 +141,7 @@ public:
     void testTdf128432();
     void testTdf128627();
     void testTdf128634();
+    void testTdf130657();
     void testDeletedDataLabel();
     void testDataPointInheritedColorDOCX();
     void testExternalStrRefsXLSX();
@@ -244,6 +245,7 @@ public:
     CPPUNIT_TEST(testTdf128432);
     CPPUNIT_TEST(testTdf128627);
     CPPUNIT_TEST(testTdf128634);
+    CPPUNIT_TEST(testTdf130657);
     CPPUNIT_TEST(testDeletedDataLabel);
     CPPUNIT_TEST(testDataPointInheritedColorDOCX);
     CPPUNIT_TEST(testExternalStrRefsXLSX);
@@ -2116,6 +2118,21 @@ void Chart2ImportTest::testTdf128634()
 {
     load("/chart2/qa/extras/data/xlsx/", "tdf128634.xlsx");
     // Test ShiftedCategoryPosition for 3D Charts
+    uno::Reference<chart2::XChartDocument> xChartDoc = getChartDocFromSheet(0, mxComponent);
+    CPPUNIT_ASSERT_MESSAGE("failed to load chart", xChartDoc.is());
+
+    Reference<chart2::XAxis> xAxis = getAxisFromDoc(xChartDoc, 0, 0, 0);
+    CPPUNIT_ASSERT(xAxis.is());
+
+    chart2::ScaleData aScaleData = xAxis->getScaleData();
+    CPPUNIT_ASSERT(aScaleData.Categories.is());
+    CPPUNIT_ASSERT(aScaleData.ShiftedCategoryPosition);
+}
+
+void Chart2ImportTest::testTdf130657()
+{
+    load("/chart2/qa/extras/data/xlsx/", "tdf130657.xlsx");
+    // Test ShiftedCategoryPosition for charts which is not contain a "crossbetween" OOXML tag.
     uno::Reference<chart2::XChartDocument> xChartDoc = getChartDocFromSheet(0, mxComponent);
     CPPUNIT_ASSERT_MESSAGE("failed to load chart", xChartDoc.is());
 
