86841072f0b57ded32ccbe6828ae46fb6ba01096
Mike Kaganski <mike.kaganski@collabora.com>
2020-02-17 13:18:04 +0300

    tdf#130725: use std::from_chars to make sure we get the nearest
    ... representation of given decimal
    
    Change-Id: Ia61b7678e257c4bc1ff193f3f856d611aa5c1a21

--- svl/source/numbers/zforfind.cxx
+++ svl/source/numbers/zforfind.cxx
@@ -151,35 +151,7 @@ static void TransformInput( SvNumberFormatter const * pFormatter, OUString& rStr
  */
 double ImpSvNumberInputScan::StringToDouble( const OUString& rStr, bool bForceFraction )
 {
-    double fNum = 0.0;
-    double fFrac = 0.0;
-    int nExp = 0;
-    sal_Int32 nPos = 0;
-    sal_Int32 nLen = rStr.getLength();
-    bool bPreSep = !bForceFraction;
-
-    while (nPos < nLen)
-    {
-        if (rStr[nPos] == '.')
-        {
-            bPreSep = false;
-        }
-        else if (bPreSep)
-        {
-            fNum = fNum * 10.0 + static_cast<double>(rStr[nPos] - '0');
-        }
-        else
-        {
-            fFrac = fFrac * 10.0 + static_cast<double>(rStr[nPos] - '0');
-            --nExp;
-        }
-        nPos++;
-    }
-    if ( fFrac )
-    {
-        return fNum + ::rtl::math::pow10Exp( fFrac, nExp );
-    }
-    return fNum;
+    return rtl::math::stringToDouble((bForceFraction ? "." : "0") + rStr, '.', 0);
 }
 
 namespace {
