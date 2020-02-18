0c6bf98cd94307ef7b0896e6643be6161691f9c9
Eike Rathke <erack@redhat.com>
2020-02-18 14:10:16 +0100

    Related: tdf#130370 Let cut&paste/move of a sheet-local name reference ...
    ... reference the original sheet-local name instead of a same name
    on the destination sheet.
    
    In bugdoc https://bugs.documentfoundation.org/attachment.cgi?id=102780
    that is the case when cutting Sheet2.B3 and pasting somewhere on Sheet3.
    That yielded formula =value with result 3. (after the fix of commit
    180fef2a54207555c39fb63ead5e66ba3a71a0cf, before it was #NAME?).
    Now it yields the expected formula =Sheet2.value with result 2.
    
    Change-Id: Id92778ea41544583e885eaccfbf82c26762925a5
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/88939
    Reviewed-by: Eike Rathke <erack@redhat.com>
    Tested-by: Jenkins

--- sc/source/core/tool/token.cxx
+++ sc/source/core/tool/token.cxx
@@ -3389,15 +3389,6 @@ sc::RefUpdateResult ScTokenArray::AdjustReferenceOnMove(
                             SCTAB nOldTab = (*pp)->GetSheet();
                             if (isNameModified(rCxt.maUpdatedNames, nOldTab, **pp))
                                 aRes.mbNameModified = true;
-                            if (rCxt.mnTabDelta && aOldRange.In(rOldPos) &&
-                                    aOldRange.aStart.Tab() <= nOldTab && nOldTab <= aOldRange.aEnd.Tab())
-                            {
-                                /* TODO: this may not be needed at all anymore;
-                                 * investigate when we actually hit the
-                                 * condition. */
-                                aRes.mbNameModified = true;
-                                (*pp)->SetSheet( nOldTab + rCxt.mnTabDelta);
-                            }
                         }
                         break;
                     case ocDBArea:
