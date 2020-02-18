1c7ca1e9b9b6af86da5f883a5944aaa463cf760f
Eike Rathke <erack@redhat.com>
2020-02-18 20:57:28 +0100

    Resolves: tdf#130757 Add Minangkabau [min-ID] to language list
    Change-Id: I901e64c223723bf2eac908dda80796a7a2799147

--- include/i18nlangtag/lang.h
+++ include/i18nlangtag/lang.h
@@ -755,6 +755,7 @@ namespace o3tl
 #define LANGUAGE_USER_KABARDIAN             LanguageType(0x06A4)
 #define LANGUAGE_USER_GUADELOUPEAN_CREOLE_FRENCH    LanguageType(0x06A5)
 #define LANGUAGE_USER_LIGURIAN              LanguageType(0x06A6)
+#define LANGUAGE_USER_MINANGKABAU           LanguageType(0x06A7)
 
 
 /* XXX Add new user defined LCIDs ^^^ there.
