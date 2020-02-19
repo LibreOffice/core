6ee038568421ef580a3b9dd401fd8c3bb2741729
Miklos Vajna <vmiklos@collabora.com>
2020-02-19 21:21:19 +0100

    Remove some unused includes
    Change-Id: I28b007cff3a99bc40901ecdeaecacf42b4521574

--- oox/qa/unit/vml.cxx
+++ oox/qa/unit/vml.cxx
@@ -15,8 +15,6 @@
 #include <com/sun/star/frame/Desktop.hpp>
 #include <com/sun/star/text/WritingMode2.hpp>
 
-#include <comphelper/processfactory.hxx>
-
 using namespace ::com::sun::star;
 
 char const DATA_DIRECTORY[] = "/oox/qa/unit/data/";
