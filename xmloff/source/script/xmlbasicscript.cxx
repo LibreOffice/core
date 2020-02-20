c03958401a9ab855270132769fcb911ecd5d1046
Noel Grandin <noel.grandin@collabora.co.uk>
2020-02-20 13:09:32 +0200

    use fast-parser APIs for embedded script parsing
    essentially, this duplicates and converts the service in
        xmlscript/source/xmlflat_impexp/
    and then converts it to use SvXMLImport APIs, stripping about 3 layers
    of unnecessary indirection in the process.
    
    Also improve some exception throwing in sax/ to make the next
    programmers life easier when finding the source of exceptions.
    
    Change-Id: I1ac552ee0565349e8322a55f6f609cf9f4521cab

---
+++ xmloff/source/script/xmlbasicscript.cxx
@@ -0,0 +1,323 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#include <sal/config.h>
+
+#include "xmlbasicscript.hxx"
+#include <sal/log.hxx>
+#include <xmlscript/xmlns.h>
+#include <xmloff/xmlnmspe.hxx>
+#include <com/sun/star/beans/XPropertySet.hpp>
+#include <com/sun/star/lang/XMultiComponentFactory.hpp>
+#include <com/sun/star/document/XEmbeddedScripts.hpp>
+#include <com/sun/star/xml/sax/SAXException.hpp>
+#include <cppuhelper/supportsservice.hxx>
+#include <tools/diagnose_ex.h>
+
+using namespace ::com::sun::star;
+using namespace ::com::sun::star::lang;
+using namespace ::com::sun::star::uno;
+using namespace ::com::sun::star::xml::sax;
+using namespace ::xmloff::token;
+
+namespace xmloff
+{
+// BasicElementBase
+
+BasicElementBase::BasicElementBase(SvXMLImport& rImport, sal_Int32 nExpectedXMLNS)
+    : SvXMLImportContext(rImport)
+    , mnExpectedXMLNS(nExpectedXMLNS)
+{
+}
+
+bool BasicElementBase::getBoolAttr(bool* pRet, sal_Int32 nToken,
+                                   const Reference<XFastAttributeList>& xAttributes)
+{
+    OUString aValue = xAttributes->getOptionalValue(nToken);
+    if (!aValue.isEmpty())
+    {
+        if (aValue == "true")
+        {
+            *pRet = true;
+            return true;
+        }
+        else if (aValue == "false")
+        {
+            *pRet = false;
+            return true;
+        }
+        else
+        {
+            throw xml::sax::SAXException(SvXMLImport::getNameFromToken(nToken)
+                                             + ": no boolean value (true|false)!",
+                                         Reference<XInterface>(), Any());
+        }
+    }
+    return false;
+}
+
+void BasicElementBase::startFastElement(sal_Int32 /*nElement*/,
+                                        const Reference<XFastAttributeList>& /*xAttributes*/)
+{
+}
+
+// BasicLibrariesElement
+
+BasicLibrariesElement::BasicLibrariesElement(SvXMLImport& rImport,
+                                             const css::uno::Reference<css::frame::XModel>& rxModel,
+                                             bool bOasis)
+    : BasicElementBase(rImport, bOasis ? XML_NAMESPACE_OOO : XML_NAMESPACE_SCRIPT_OOO)
+{
+    // try the XEmbeddedScripts interface
+    Reference<document::XEmbeddedScripts> xDocumentScripts(rxModel, UNO_QUERY_THROW);
+    m_xLibContainer.set(xDocumentScripts->getBasicLibraries().get());
+
+    if (!m_xLibContainer.is())
+    {
+        // try the "BasicLibraries" property (old-style, for compatibility)
+        Reference<beans::XPropertySet> xPSet(rxModel, UNO_QUERY);
+        if (xPSet.is())
+            xPSet->getPropertyValue("BasicLibraries") >>= m_xLibContainer;
+    }
+
+    SAL_WARN_IF(!m_xLibContainer.is(), "xmlscript.xmlflat",
+                "BasicImport::startRootElement: nowhere to import to!");
+
+    if (!m_xLibContainer.is())
+    {
+        throw xml::sax::SAXException("nowhere to import to", Reference<XInterface>(), Any());
+    }
+}
+
+// XElement
+
+Reference<XFastContextHandler>
+BasicLibrariesElement::createFastChildContext(sal_Int32 nElement,
+                                              const Reference<XFastAttributeList>& xAttributes)
+{
+    if ((nElement & NMSP_MASK) != NAMESPACE_TOKEN(mnExpectedXMLNS))
+    {
+        throw xml::sax::SAXException("illegal namespace!", Reference<XInterface>(), Any());
+    }
+    else if ((nElement & TOKEN_MASK) == XML_LIBRARY_LINKED)
+    {
+        OUString aName = xAttributes->getValue(NAMESPACE_TOKEN(mnExpectedXMLNS) | XML_NAME);
+
+        OUString aStorageURL = xAttributes->getValue(XML_ELEMENT(XLINK, XML_HREF));
+
+        bool bReadOnly = false;
+        getBoolAttr(&bReadOnly, NAMESPACE_TOKEN(mnExpectedXMLNS) | XML_READONLY, xAttributes);
+
+        if (m_xLibContainer.is())
+        {
+            try
+            {
+                Reference<container::XNameAccess> xLib(
+                    m_xLibContainer->createLibraryLink(aName, aStorageURL, bReadOnly));
+                if (xLib.is())
+                    return new BasicElementBase(GetImport(), mnExpectedXMLNS);
+            }
+            catch (const container::ElementExistException&)
+            {
+                TOOLS_INFO_EXCEPTION("xmlscript.xmlflat",
+                                     "BasicLibrariesElement::startChildElement");
+            }
+            catch (const lang::IllegalArgumentException&)
+            {
+                TOOLS_INFO_EXCEPTION("xmlscript.xmlflat",
+                                     "BasicLibrariesElement::startChildElement");
+            }
+        }
+    }
+    else if ((nElement & TOKEN_MASK) == XML_LIBRARY_EMBEDDED)
+    {
+        // TODO: create password protected libraries
+
+        OUString aName = xAttributes->getValue(NAMESPACE_TOKEN(mnExpectedXMLNS) | XML_NAME);
+
+        bool bReadOnly = false;
+        getBoolAttr(&bReadOnly, NAMESPACE_TOKEN(mnExpectedXMLNS) | XML_READONLY, xAttributes);
+
+        if (m_xLibContainer.is())
+        {
+            try
+            {
+                Reference<container::XNameContainer> xLib;
+                if (m_xLibContainer->hasByName(aName))
+                {
+                    // Standard library
+                    m_xLibContainer->getByName(aName) >>= xLib;
+                }
+                else
+                {
+                    xLib.set(m_xLibContainer->createLibrary(aName));
+                }
+
+                if (xLib.is())
+                    return new BasicEmbeddedLibraryElement(GetImport(), mnExpectedXMLNS,
+                                                           m_xLibContainer, aName, bReadOnly);
+            }
+            catch (const lang::IllegalArgumentException&)
+            {
+                TOOLS_INFO_EXCEPTION("xmlscript.xmlflat",
+                                     "BasicLibrariesElement::startChildElement");
+            }
+        }
+    }
+    else
+    {
+        throw xml::sax::SAXException("expected library-linked or library-embedded element!",
+                                     Reference<XInterface>(), Any());
+    }
+
+    return nullptr;
+}
+
+// BasicEmbeddedLibraryElement
+
+BasicEmbeddedLibraryElement::BasicEmbeddedLibraryElement(
+    SvXMLImport& rImport, sal_Int32 nExpectedXMLNS,
+    const Reference<script::XLibraryContainer2>& rxLibContainer, const OUString& rLibName,
+    bool bReadOnly)
+    : BasicElementBase(rImport, nExpectedXMLNS)
+    , m_xLibContainer(rxLibContainer)
+    , m_aLibName(rLibName)
+    , m_bReadOnly(bReadOnly)
+{
+    try
+    {
+        if (m_xLibContainer.is() && m_xLibContainer->hasByName(m_aLibName))
+            m_xLibContainer->getByName(m_aLibName) >>= m_xLib;
+    }
+    catch (const lang::WrappedTargetException&)
+    {
+        TOOLS_INFO_EXCEPTION("xmlscript.xmlflat", "BasicEmbeddedLibraryElement::CTOR:");
+    }
+}
+
+Reference<XFastContextHandler> BasicEmbeddedLibraryElement::createFastChildContext(
+    sal_Int32 nElement, const Reference<XFastAttributeList>& xAttributes)
+{
+    if ((nElement & NMSP_MASK) != NAMESPACE_TOKEN(mnExpectedXMLNS))
+    {
+        throw xml::sax::SAXException("illegal namespace!", Reference<XInterface>(), Any());
+    }
+    else if ((nElement & TOKEN_MASK) == XML_MODULE)
+    {
+        OUString aName = xAttributes->getValue(NAMESPACE_TOKEN(mnExpectedXMLNS) | XML_NAME);
+
+        if (m_xLib.is() && !aName.isEmpty())
+            return new BasicModuleElement(GetImport(), mnExpectedXMLNS, m_xLib, aName);
+    }
+    else
+    {
+        throw xml::sax::SAXException("expected module element!", Reference<XInterface>(), Any());
+    }
+
+    return nullptr;
+}
+
+void BasicEmbeddedLibraryElement::endFastElement(sal_Int32)
+{
+    if (m_xLibContainer.is() && m_xLibContainer->hasByName(m_aLibName) && m_bReadOnly)
+        m_xLibContainer->setLibraryReadOnly(m_aLibName, m_bReadOnly);
+}
+
+// BasicModuleElement
+
+BasicModuleElement::BasicModuleElement(SvXMLImport& rImport, sal_Int32 nExpectedXMLNS,
+                                       const Reference<container::XNameContainer>& rxLib,
+                                       const OUString& rName)
+    : BasicElementBase(rImport, nExpectedXMLNS)
+    , m_xLib(rxLib)
+    , m_aName(rName)
+{
+}
+
+Reference<XFastContextHandler>
+BasicModuleElement::createFastChildContext(sal_Int32 nElement,
+                                           const Reference<XFastAttributeList>& xAttributes)
+{
+    // TODO: <byte-code>
+
+    if ((nElement & NMSP_MASK) != NAMESPACE_TOKEN(mnExpectedXMLNS))
+    {
+        throw xml::sax::SAXException("illegal namespace!", Reference<XInterface>(), Any());
+    }
+    else if ((nElement & TOKEN_MASK) == XML_SOURCE_CODE)
+    {
+        // TODO: password protected libraries
+
+        if (xAttributes.is())
+        {
+            if (m_xLib.is() && !m_aName.isEmpty())
+                return new BasicSourceCodeElement(GetImport(), mnExpectedXMLNS, m_xLib, m_aName);
+        }
+    }
+    else
+    {
+        throw xml::sax::SAXException("expected source-code element!", Reference<XInterface>(),
+                                     Any());
+    }
+
+    return nullptr;
+}
+
+// BasicSourceCodeElement
+
+BasicSourceCodeElement::BasicSourceCodeElement(SvXMLImport& rImport, sal_Int32 nExpectedXMLNS,
+                                               const Reference<container::XNameContainer>& rxLib,
+                                               const OUString& rName)
+    : BasicElementBase(rImport, nExpectedXMLNS)
+    , m_xLib(rxLib)
+    , m_aName(rName)
+{
+}
+
+// XElement
+
+void BasicSourceCodeElement::characters(const OUString& rChars) { m_aBuffer.append(rChars); }
+
+void BasicSourceCodeElement::endFastElement(sal_Int32)
+{
+    try
+    {
+        if (m_xLib.is() && !m_aName.isEmpty())
+        {
+            Any aElement;
+            aElement <<= m_aBuffer.makeStringAndClear();
+            m_xLib->insertByName(m_aName, aElement);
+        }
+    }
+    catch (const container::ElementExistException&)
+    {
+        TOOLS_INFO_EXCEPTION("xmlscript.xmlflat", "BasicSourceCodeElement::endElement");
+    }
+    catch (const lang::IllegalArgumentException&)
+    {
+        TOOLS_INFO_EXCEPTION("xmlscript.xmlflat", "BasicSourceCodeElement::endElement");
+    }
+    catch (const lang::WrappedTargetException&)
+    {
+        TOOLS_INFO_EXCEPTION("xmlscript.xmlflat", "BasicSourceCodeElement::endElement");
+    }
+}
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
---
+++ xmloff/source/script/xmlbasicscript.hxx
@@ -0,0 +1,116 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#pragma once
+
+#include <com/sun/star/frame/XModel.hpp>
+#include <com/sun/star/script/XLibraryContainer2.hpp>
+#include <cppuhelper/implbase.hxx>
+#include <osl/mutex.hxx>
+#include <rtl/ustrbuf.hxx>
+#include <rtl/ref.hxx>
+#include <xmloff/xmlimp.hxx>
+
+namespace xmloff
+{
+class BasicElementBase : public SvXMLImportContext
+{
+protected:
+    sal_Int32 mnExpectedXMLNS;
+    static bool
+    getBoolAttr(bool* pRet, sal_Int32 nToken,
+                const css::uno::Reference<css::xml::sax::XFastAttributeList>& xAttributes);
+
+public:
+    BasicElementBase(SvXMLImport& rImport, sal_Int32 nExpectedXMLNS);
+
+    virtual void SAL_CALL startFastElement(
+        sal_Int32 nElement,
+        const css::uno::Reference<css::xml::sax::XFastAttributeList>& xAttribs) override;
+};
+
+class BasicLibrariesElement : public BasicElementBase
+{
+private:
+    css::uno::Reference<css::script::XLibraryContainer2> m_xLibContainer;
+
+public:
+    BasicLibrariesElement(SvXMLImport& rImport,
+                          const css::uno::Reference<css::frame::XModel>& rxModel, bool bOasis);
+
+    virtual css::uno::Reference<XFastContextHandler> SAL_CALL createFastChildContext(
+        sal_Int32 Element,
+        const css::uno::Reference<css::xml::sax::XFastAttributeList>& Attribs) override;
+};
+
+class BasicEmbeddedLibraryElement : public BasicElementBase
+{
+private:
+    css::uno::Reference<css::script::XLibraryContainer2> m_xLibContainer;
+    css::uno::Reference<css::container::XNameContainer> m_xLib;
+    OUString const m_aLibName;
+    bool const m_bReadOnly;
+
+public:
+    BasicEmbeddedLibraryElement(
+        SvXMLImport& rImport, sal_Int32 nExpectedXMLNS,
+        const css::uno::Reference<css::script::XLibraryContainer2>& rxLibContainer,
+        const OUString& rLibName, bool bReadOnly);
+
+    virtual css::uno::Reference<XFastContextHandler> SAL_CALL createFastChildContext(
+        sal_Int32 Element,
+        const css::uno::Reference<css::xml::sax::XFastAttributeList>& Attribs) override;
+    virtual void SAL_CALL endFastElement(sal_Int32 nElement) override;
+};
+
+class BasicModuleElement : public BasicElementBase
+{
+private:
+    css::uno::Reference<css::container::XNameContainer> m_xLib;
+    OUString const m_aName;
+
+public:
+    BasicModuleElement(SvXMLImport& rImport, sal_Int32 nExpectedXMLNS,
+                       const css::uno::Reference<css::container::XNameContainer>& rxLib,
+                       const OUString& rName);
+
+    virtual css::uno::Reference<XFastContextHandler> SAL_CALL createFastChildContext(
+        sal_Int32 Element,
+        const css::uno::Reference<css::xml::sax::XFastAttributeList>& Attribs) override;
+};
+
+class BasicSourceCodeElement : public BasicElementBase
+{
+private:
+    css::uno::Reference<css::container::XNameContainer> m_xLib;
+    OUString const m_aName;
+    OUStringBuffer m_aBuffer;
+
+public:
+    BasicSourceCodeElement(SvXMLImport& rImport, sal_Int32 nExpectedXMLNS,
+                           const css::uno::Reference<css::container::XNameContainer>& rxLib,
+                           const OUString& rName);
+
+    virtual void SAL_CALL characters(const OUString& rChars) override;
+    virtual void SAL_CALL endFastElement(sal_Int32 nElement) override;
+};
+
+} // namespace xmloff
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
