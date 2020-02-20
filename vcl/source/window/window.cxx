b55dcb954d364aaf74ca0b260bc47d3eaf4c8199
Jan-Marek Glogowski <jan-marek.glogowski@extern.cib.de>
2020-02-20 16:42:09 +0100

    tdf#130841 resize to client size after SetPosSize
    This fixes the hack of the native menu bar with a smaller client
    size in relation to the frame geometry.
    Eventually this should be replaced by proper mnTopBorder usage,
    but this currently isn't working.
    
    Change-Id: Ib5825d9c8f77e463fcb086e0373228fe91d8705a

--- vcl/source/window/window.cxx
+++ vcl/source/window/window.cxx
@@ -2836,6 +2836,10 @@ void Window::setPosSizePixel( long nX, long nY,
 
         pWindow->mpWindowImpl->mpFrame->SetPosSize( nX, nY, nWidth, nHeight, nSysFlags );
 
+        // Adjust resize with the hack of different client size and frame geometries to fix
+        // native menu bars. Eventually this should be replaced by proper mnTopBorder usage.
+        pWindow->mpWindowImpl->mpFrame->GetClientSize(nWidth, nHeight);
+
         // Resize should be called directly. If we haven't
         // set the correct size, we get a second resize from
         // the system with the correct size. This can be happened
