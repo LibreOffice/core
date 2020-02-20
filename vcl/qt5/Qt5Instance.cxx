c6fb366e05370af553af2130b370909ed4495cff
Jan-Marek Glogowski <jan-marek.glogowski@extern.cib.de>
2020-02-21 00:22:37 +0100

    tdf#130499 Qt5 no qApp == main thread
    If there is no QApplication, we can ignore the GUI thread.
    
    Change-Id: I0d2faf4799d493ad0b6074b40242c443d4bc43dc
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/89241
    Tested-by: Jenkins
    Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>

--- vcl/qt5/Qt5Instance.cxx
+++ vcl/qt5/Qt5Instance.cxx
@@ -400,7 +400,10 @@ void Qt5Instance::AddToRecentDocumentList(const OUString&, const OUString&, cons
 
 OpenGLContext* Qt5Instance::CreateOpenGLContext() { return new Qt5OpenGLContext; }
 
-bool Qt5Instance::IsMainThread() const { return qApp->thread() == QThread::currentThread(); }
+bool Qt5Instance::IsMainThread() const
+{
+    return !qApp || (qApp->thread() == QThread::currentThread());
+}
 
 void Qt5Instance::TriggerUserEventProcessing()
 {
