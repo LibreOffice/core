b7b3bf47a640ef236ea52bab0466cb9c71f29dc8
Stephan Bergmann <sbergman@redhat.com>
2020-02-21 08:59:46 +0100

    Dummy rebuild test w/o --enable-optimized=debug #1; do not submit
    Change-Id: I2b3e6e2abfb37c24aef5d45cd387e7e3b9391954

--- include/sal/config.h
+++ include/sal/config.h
@@ -16,6 +16,7 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
+/* DUMMY REBUILD TRIGGER */
 
 #ifndef INCLUDED_SAL_CONFIG_H
 #define INCLUDED_SAL_CONFIG_H
