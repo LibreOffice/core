5293e95f4d2b20fa4fc9a5e6378f00e7e4329d81
Jussi Pakkanen <jpakkane@gmail.com>
2020-02-22 21:36:13 +0200

    tdf#130911: convert some token generation scripts from Perl to Python.
    Change-Id: Id655b6a0cee7bdfe4804941f20fe358af8f3185e

---
+++ oox/source/token/namespaces.py
@@ -0,0 +1,85 @@
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# This file incorporates work covered by the following license notice:
+#
+#   Licensed to the Apache Software Foundation (ASF) under one or more
+#   contributor license agreements. See the NOTICE file distributed
+#   with this work for additional information regarding copyright
+#   ownership. The ASF licenses this file to you under the Apache
+#   License, Version 2.0 (the "License"); you may not use this file
+#   except in compliance with the License. You may obtain a copy of
+#   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+#
+
+import sys, re
+
+infile_name = sys.argv[1]
+id_out_name = sys.argv[2]
+name_out_name = sys.argv[3]
+txt_out_name = sys.argv[4]
+instrict_name = sys.argv[5]
+strict_out_name = sys.argv[6]
+
+# parse input file
+
+namespaces = {}
+
+with open(infile_name) as infile:
+    for line in infile:
+        line = line.strip()
+        # trim comments
+        line = line.split('#')[0]
+        # skip empty lines
+        if line:
+            # check for valid characters
+            m = re.match(r'([a-zA-Z][a-zA-Z0-9]*)\s+([a-zA-Z0-9-.:\/]+)\s*$', line)
+            if not m:
+                sys.exit('Invalid character in input data: ' + line)
+            namespaces[m.group(1)] = m.group(2)
+
+# OOXML strict namespaces
+
+namespaces_strict = {}
+with open(instrict_name) as infile_strict:
+    for line in infile_strict:
+        line = line.strip()
+        # trim comments
+        line = line.split('#')[0]
+        # skip empty lines
+        if line:
+            # check for valid characters
+            m = re.match(r'([a-zA-Z][a-zA-Z0-9]*)\s+([a-zA-Z0-9-.:\/]+)\s*$', line)
+            if not m:
+                sys.exit("Error: invalid character in input data: " + line)
+            namespaces_strict[m.group(1)] = m.group(2)
+
+# generate output files
+
+idfile = open(id_out_name, 'w')
+namefile = open(name_out_name, 'w')
+txtfile = open(txt_out_name, 'w')
+namefile_strict = open(strict_out_name, 'w')
+
+# number of bits to shift the namespace identifier
+shift = 16
+
+idfile.write("const size_t NMSP_SHIFT = {};\n".format(shift))
+
+i = 1;
+for token in sorted(namespaces.keys()):
+    idfile.write("const sal_Int32 NMSP_{} = {} << NMSP_SHIFT;\n".format(token, i))
+    cur_id = i << shift
+    namefile.write("{{ {}, \"{}\" }},\n".format(cur_id, namespaces[token]))
+    namefile_strict.write("{{ {}, \"{}\" }},\n".format(cur_id, namespaces_strict[token]))
+    txtfile.write("{} {} {}\n".format(cur_id, token, namespaces[token]))
+    txtfile.write("{} {} {}\n".format(cur_id, token, namespaces_strict[token]))
+    i += 1
+
+idfile.close()
+namefile.close()
+namefile_strict.close()
+txtfile.close()
---
+++ solenv/bin/generate-tokens.py
@@ -0,0 +1,80 @@
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# This file incorporates work covered by the following license notice:
+#
+#   Licensed to the Apache Software Foundation (ASF) under one or more
+#   contributor license agreements. See the NOTICE file distributed
+#   with this work for additional information regarding copyright
+#   ownership. The ASF licenses this file to you under the Apache
+#   License, Version 2.0 (the "License"); you may not use this file
+#   except in compliance with the License. You may obtain a copy of
+#   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+#
+
+import sys, re
+
+infile_name = sys.argv[1]
+idfile_out_name = sys.argv[2]
+namefile_out_name = sys.argv[3]
+gperffile_out_name = sys.argv[4]
+
+idfile = open(idfile_out_name, 'w')
+namefile = open(namefile_out_name, 'w')
+gperffile = open(gperffile_out_name, 'w')
+
+gperffile.write("""%language=C++
+%global-table
+%null-strings
+%struct-type
+struct xmltoken {
+    const sal_Char *name;
+    sal_Int32 nToken;
+};
+%%
+""")
+
+token_count = 0;
+tokens = {}
+
+with open(infile_name) as infile:
+    for line in infile:
+        line = line.strip()
+        # check for valid characters
+        if not re.match(r'[a-zA-Z0-9-_]+$', line):
+            sys.exit("Error: invalid character in token '{}'".format(line));
+        cur_id = "XML_" + line;
+        # we have two ids with similar names("cut-offs" and "cut_offs")
+        if cur_id == "XML_cut_offs":
+            cur_id = "cut_offs2";
+        cur_id = cur_id.replace('-', '_')
+        tokens[line] = cur_id
+        idfile.write("const sal_Int32 {} = {};\n".format(cur_id, token_count))
+        namefile.write("\"{}\",\n".format(line));
+        gperffile.write("{},{}\n".format(line, cur_id));
+        token_count += 1
+
+idfile.write("const sal_Int32 XML_TOKEN_COUNT = {};\n".format(token_count))
+gperffile.write("%%\n")
+
+idfile.close()
+namefile.close()
+gperffile.close()
+
+def fix_linefeeds(fname):
+    # Gperf requires LF newlines, not CRLF, even on Windows.
+    # Making this work on both Python 2 and 3 is difficult.
+    # When Python 2 is dropped, delete this and add
+    # newline = '\n' to the open() calls above.
+    with open(fname, 'rb') as ifile:
+        d = ifile.read()
+    d = d.replace(b'\r', b'')
+    with open(fname, 'wb') as ofile:
+        ofile.write(d)
+
+fix_linefeeds(idfile_out_name)
+fix_linefeeds(namefile_out_name)
+fix_linefeeds(gperffile_out_name)
