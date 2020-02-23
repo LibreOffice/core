LibreOffice/core

commit 1bdb2b6f06c556b9af91dba4f29f3ac73190b09e

remove unnecessary use of OUString constructor in CONNECTIVITY module
Change-Id: Ie32d03920d996db2793ead9940fc90668cde03dd

connectivity/source/drivers/postgresql/pq_connection.cxx
@@ -148,7 +148,7 @@ static sal_Int32 readLogLevelFromConfiguration()
     OUString fileName;
     osl_getModuleURLFromAddress(
         (void*) readLogLevelFromConfiguration, (rtl_uString **) &fileName );
-    fileName = OUString( fileName.getStr(), fileName.lastIndexOf( '/' )+1 );
+    fileName = fileName.copy( fileName.lastIndexOf( '/' )+1 );
     fileName += "postgresql-sdbc.ini";
     rtl::Bootstrap bootstrapHandle( fileName );
 
@@ -558,7 +558,7 @@ void Connection::initialize( const Sequence< Any >& aArguments )
                     free(err);
                 }
                 else
-                    errorMessage = OUString("#no error message#");
+                    errorMessage = "#no error message#";
                 OUStringBuffer buf( 128 );
                 buf.appendAscii( "Error in database URL '" );
                 buf.append( url );

commit 9323f0af6a6c0d7e667ba25f395044dbd2774c7c

Postgresql native, fix readLogLevelFromConfiguration
With:
osl_getModuleURLFromFunctionAddress(
    reinterpret_cast<oslGenericFunction>(readLogLevelFromConfiguration), &fileName.pData );

The method retrieves the path to libpostgresql-sdbc-impllo.so, eg:
file:///home/julien/lo/libreoffice/instdir/program/libpostgresql-sdbc-impllo.so

but then instead of retrieving path + "postgresql-sdbc.ini"
we obtained ".so" filename + "postgresql-sdbc.ini"=>
libpostgresql-sdbc-impllo.sopostgresql-sdbc.ini

Regression from 1bdb2b6f06c556b9af91dba4f29f3ac73190b09e
author	Noel Grandin <noel@peralex.com>	2013-11-04 13:51:21 +0200
committer	Noel Grandin <noel@peralex.com>	2013-11-11 11:21:25 +0200
commit	1bdb2b6f06c556b9af91dba4f29f3ac73190b09e (patch)
tree	1d941e17952cd891f5309fd862215dd09a84b001
parent	2f2416ff060c780ad87b4d3979112f52da2ae902 (diff)
remove unnecessary use of OUString constructor in CONNECTIVITY module

Change-Id: Ie7be4b1078760f94d34dd1f6cb9932bd7fa70962

connectivity/source/drivers/postgresql/pq_connection.cxx
@@ -131,7 +131,7 @@ static LogLevel readLogLevelFromConfiguration()
     OUString fileName;
     osl_getModuleURLFromFunctionAddress(
         reinterpret_cast<oslGenericFunction>(readLogLevelFromConfiguration), &fileName.pData );
-    fileName = fileName.copy( fileName.lastIndexOf( '/' )+1 ) +
+    fileName = fileName.copy( 0, fileName.lastIndexOf( '/' )+1 ) +
 #ifdef MACOSX
         "../Resources/"
 #endif