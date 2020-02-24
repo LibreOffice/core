6c4721da4f339d6d1bed6ded0a6ba40d3857b379
Michael Stahl <Michael.Stahl@cib.de>
2020-02-24 09:36:46 +0100

    boost: disable -Wmaybe-uninitialized from GCC 9.2.1 -Og
    This warning is notorious for false positives so let's disable it here.
    
    from sal/cppunittester/cppunittester.cxx:61:
    workdir/UnpackedTarball/boost/boost/function/function_template.hpp: In function ‘SequenceSequenceT& boost::algorithm::iter_split(SequenceSequenceT&, RangeT&&, FinderT) [with SequenceSequenceT = std::__debug::vector<std::__cxx11::basic_string<char> >; RangeT = const char*&; FinderT = boost::algorithm::detail::token_finderF<boost::algorithm::detail::is_any_ofF<char> >]’:
    workdir/UnpackedTarball/boost/boost/function/function_template.hpp:904:22: error: ‘*((void*)&<anonymous> +8)’ may be used uninitialized in this function [-Werror=maybe-uninitialized]
      904 |           std::memcpy(this->functor.data, f.functor.data, sizeof(boost::detail::function::function_buffer));
    
    Change-Id: Id3cf9fae8db190337b6a3eb0c847c019394088ba
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/89349
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@cib.de>

--- external/boost/include/boost/current_function.hpp
+++ external/boost/include/boost/current_function.hpp
@@ -11,6 +11,7 @@
 #pragma GCC diagnostic ignored "-Wignored-qualifiers"
 #pragma GCC diagnostic ignored "-Wimplicit-fallthrough"
 #pragma GCC diagnostic ignored "-Winvalid-constexpr"
+#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
 #pragma GCC diagnostic ignored "-Wmicrosoft-unqualified-friend"
 #pragma GCC diagnostic ignored "-Wnon-virtual-dtor"
 #pragma GCC diagnostic ignored "-Wparentheses"
