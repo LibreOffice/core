78b27dfa7022001de407b23002b202d01d219e1f
Muhammet Kara <muhammet.kara@collabora.com>
2020-02-24 16:04:37 +0300

    lok: Create Text boxes directly
    Change-Id: I281d4648d0e91f74d6f0f5880945d8e7e8659efe
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/89344
    Reviewed-by: Jan Holesovsky <kendy@collabora.com>
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/89709
    Tested-by: Jenkins
    Reviewed-by: Muhammet Kara <muhammet.kara@collabora.com>

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -3640,6 +3640,10 @@ static void doc_postUnoCommand(LibreOfficeKitDocument* pThis, const char* pComma
         ExecuteOrientationChange();
         return;
     }
+    else if (gImpl && aCommand == ".uno:Text")
+    {
+        aCommand = ".uno:Text?CreateDirectly:bool=true";
+    }
 
     // handle potential interaction
     if (gImpl && aCommand == ".uno:Save")
