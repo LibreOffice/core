b1ac0d12163454b0c39ce81083df8cd072caa14f
Szymon Kłos <szymon.klos@collabora.com>
2020-02-24 18:35:18 +0100

    Resend jsdialog on entry change
    Change-Id: Ic255b8ba56f5b355a95ddc9a9587e1747b66702a

--- vcl/inc/jsdialog/jsdialogbuilder.hxx
+++ vcl/inc/jsdialog/jsdialogbuilder.hxx
@@ -8,22 +8,45 @@
 #include <vcl/builder.hxx>
 #include <salvtables.hxx>
 
-class VCL_DLLPUBLIC JSInstanceBuilder : public SalInstanceBuilder
-{
-public:
-    JSInstanceBuilder(weld::Widget* pParent, const OUString& rUIRoot, const OUString& rUIFile);
-    virtual std::unique_ptr<weld::Dialog> weld_dialog(const OString& id, bool bTakeOwnership = true) override;
-    virtual std::unique_ptr<weld::Label> weld_label(const OString &id, bool bTakeOwnership = false) override;
-};
-
-class VCL_DLLPUBLIC JSLabel : public SalInstanceLabel
+class JSDialogSender
 {
     VclPtr<vcl::Window> m_aOwnedToplevel;
 
 public:
-    JSLabel(VclPtr<vcl::Window> aOwnedToplevel, FixedText* pLabel,
-            SalInstanceBuilder* pBuilder, bool bTakeOwnership);
+    JSDialogSender(VclPtr<vcl::Window> aOwnedToplevel)
+        : m_aOwnedToplevel(aOwnedToplevel)
+    {
+    }
+
+    void notifyDialogState();
+};
+
+class VCL_DLLPUBLIC JSInstanceBuilder : public SalInstanceBuilder
+{
+public:
+    JSInstanceBuilder(weld::Widget* pParent, const OUString& rUIRoot, const OUString& rUIFile);
+    virtual std::unique_ptr<weld::Dialog> weld_dialog(const OString& id,
+                                                      bool bTakeOwnership = true) override;
+    virtual std::unique_ptr<weld::Label> weld_label(const OString& id,
+                                                    bool bTakeOwnership = false) override;
+    virtual std::unique_ptr<weld::Entry> weld_entry(const OString& id,
+                                                    bool bTakeOwnership = false) override;
+};
+
+class VCL_DLLPUBLIC JSLabel : public SalInstanceLabel, public JSDialogSender
+{
+public:
+    JSLabel(VclPtr<vcl::Window> aOwnedToplevel, FixedText* pLabel, SalInstanceBuilder* pBuilder,
+            bool bTakeOwnership);
     virtual void set_label(const OUString& rText) override;
+};
+
+class VCL_DLLPUBLIC JSEntry : public SalInstanceEntry, public JSDialogSender
+{
+public:
+    JSEntry(VclPtr<vcl::Window> aOwnedToplevel, ::Edit* pEntry, SalInstanceBuilder* pBuilder,
+            bool bTakeOwnership);
+    virtual void set_text(const OUString& rText) override;
 };
 
 #endif
