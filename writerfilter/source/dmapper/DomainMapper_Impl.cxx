8bcfa7b64a7fbc3f09f8d65c02d92f8e6322d0de
László Németh <nemeth@numbertext.org>
2020-02-24 10:55:38 +0100

    tdf#121045 DOCX import: fix checkbox size in table
    in cell starting position.
    
    Change-Id: Ib99726c03ff3f83177a015721e562ebc5bc516d3
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/89338
    Tested-by: Jenkins
    Tested-by: László Németh <nemeth@numbertext.org>
    Reviewed-by: László Németh <nemeth@numbertext.org>
    (cherry picked from commit 22ad4d69d771708f28a2d9e137cfd43ac846cf3a)

--- writerfilter/source/dmapper/DomainMapper_Impl.cxx
+++ writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -4616,8 +4616,31 @@ void DomainMapper_Impl::CloseFieldCommand()
             OUString const sFirstParam(std::get<1>(field).empty()
                     ? OUString() : std::get<1>(field).front());
 
+<<<<<<< HEAD   (acc1bd tdf#127167 Do not reset Draw/Impress page size)
             FieldConversionMap_t::const_iterator const aIt =
                 aFieldConversionMap.find(std::get<0>(field));
+=======
+            // apply font size to the form control in tables
+            if ( m_nTableDepth > 0 && m_pLastCharacterContext.get() && m_pLastCharacterContext->isSet(PROP_CHAR_HEIGHT) )
+            {
+                uno::Reference< text::XTextAppend >  xTextAppend = m_aTextAppendStack.top().xTextAppend;
+                if (xTextAppend.is())
+                {
+                    uno::Reference< text::XTextCursor > xCrsr = xTextAppend->getText()->createTextCursor();
+                    uno::Reference< text::XText > xText = xTextAppend->getText();
+                    if(xCrsr.is() && xText.is())
+                    {
+                        xCrsr->gotoEnd(false);
+                        uno::Reference< beans::XPropertySet > xProp( xCrsr, uno::UNO_QUERY );
+                        xProp->setPropertyValue(getPropertyName(PROP_CHAR_HEIGHT), m_pLastCharacterContext->getProperty(PROP_CHAR_HEIGHT)->second);
+                        if ( m_pLastCharacterContext->isSet(PROP_CHAR_HEIGHT_COMPLEX) )
+                            xProp->setPropertyValue(getPropertyName(PROP_CHAR_HEIGHT_COMPLEX), m_pLastCharacterContext->getProperty(PROP_CHAR_HEIGHT_COMPLEX)->second);
+                    }
+                }
+            }
+
+            FieldConversionMap_t::const_iterator const aIt = aFieldConversionMap.find(sType);
+>>>>>>> CHANGE (22ad4d tdf#121045 DOCX import: fix checkbox size in table)
             if (aIt != aFieldConversionMap.end()
                 && (!m_bForceGenericFields
                     // these need to convert ffData to properties...
