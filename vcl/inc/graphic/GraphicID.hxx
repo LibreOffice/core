e834c50ca105ecb4fe871e1e2a0eb4c6cb1c63c0
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2020-02-24 23:02:20 +0100

    vcl: move GraphicID out of impgraph.{cxx,hxx}
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/89449
    Tested-by: Jenkins
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>
    (cherry picked from commit b6bc43f354bcbe6a98db9fb372fd47bc5b23f4d8)
    
    Change-Id: I0b3b17736a76be290f6e5b77ee547b7e650d4489

---
+++ vcl/inc/graphic/GraphicID.hxx
@@ -0,0 +1,47 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#pragma once
+
+#include <vcl/dllapi.h>
+#include <rtl/string.hxx>
+#include <vcl/checksum.hxx>
+
+class ImpGraphic;
+
+class GraphicID
+{
+private:
+    sal_uInt32 mnID1;
+    sal_uInt32 mnID2;
+    sal_uInt32 mnID3;
+    BitmapChecksum mnID4;
+
+public:
+    GraphicID(ImpGraphic const& rGraphic);
+
+    bool operator==(const GraphicID& rID) const
+    {
+        return rID.mnID1 == mnID1 && rID.mnID2 == mnID2 && rID.mnID3 == mnID3 && rID.mnID4 == mnID4;
+    }
+
+    OString getIDString() const;
+};
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
