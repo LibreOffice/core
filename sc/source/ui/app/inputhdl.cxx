f16d910a9b43140eb0c98358d7319db80690eac2
Marco Cecchetti <marco.cecchetti@collabora.com>
2020-02-26 14:48:55 +0100

    lok: formula input bar: get a function inserted in a not focused top view
    Change-Id: Ia52ebbcad8d3febab85f19279859e901f8193c49

--- sc/source/ui/app/inputhdl.cxx
+++ sc/source/ui/app/inputhdl.cxx
@@ -1642,11 +1642,21 @@ void ScInputHandler::PasteFunctionData()
 
 void ScInputHandler::LOKPasteFunctionData( sal_uInt32 nIndex )
 {
-    if (pActiveViewSh && (pTopView || pTableView))
+    // in case we have no top view try to create it
+    if (!pTopView && pInputWin)
+    {
+        ScInputMode eCurMode = eMode;
+        SetMode(SC_INPUT_TOP);
+        if (!pTopView)
+            SetMode(eCurMode);
+    }
+
+    EditView* pEditView = pTopView ? pTopView : pTableView;
+
+    if (pActiveViewSh && pEditView)
     {
         bool bEdit = false;
         OUString aFormula;
-        EditView* pEditView = pTopView ? pTopView : pTableView;
         const EditEngine* pEditEngine = pEditView->GetEditEngine();
         if (pEditEngine)
         {
