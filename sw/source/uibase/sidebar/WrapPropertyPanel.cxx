fe154d3f3532a52214d25e5d2ffd6001f23254bd
Caolán McNamara <caolanm@redhat.com>
2020-02-26 20:11:05 +0000

    tdf#130933 wraptools are berserk
    probably a regression from...
    
    commit a22de0aacc9fb4197e9051f572b1a9298b2472e2
    Date:   Thu Jan 23 23:34:59 2020 +0100
    
        tdf#130096 Wrap Sidebar update use common .uno commands and layout
    
    because there were two toolbars with radiobuttons each toolbar has
    to have one active radiobutton so they fight it out toggling on and
    off when attempting to make one toolbar with all inactive radiobuttons
    
    Change-Id: Iafe70dc1328ad80744cb4569365e25bf5eba6b53

--- sw/source/uibase/sidebar/WrapPropertyPanel.cxx
+++ sw/source/uibase/sidebar/WrapPropertyPanel.cxx
@@ -71,10 +71,8 @@ WrapPropertyPanel::WrapPropertyPanel(
     // controller items
     , maSwLRSpacingControl(SID_ATTR_LRSPACE, *pBindings, *this)
     , maSwULSpacingControl(SID_ATTR_ULSPACE, *pBindings, *this)
-    , mxWrapOptions1(m_xBuilder->weld_toolbar("wrapoptions1"))
-    , mxWrapOptions1Dispatch(new ToolbarUnoDispatcher(*mxWrapOptions1, rxFrame))
-    , mxWrapOptions2(m_xBuilder->weld_toolbar("wrapoptions2"))
-    , mxWrapOptions2Dispatch(new ToolbarUnoDispatcher(*mxWrapOptions2, rxFrame))
+    , mxWrapOptions(m_xBuilder->weld_toolbar("wrapoptions"))
+    , mxWrapOptionsDispatch(new ToolbarUnoDispatcher(*mxWrapOptions, rxFrame))
     , mxSpacingLB(m_xBuilder->weld_combo_box("spacingLB"))
 {
     FieldUnit eMetric = ::GetDfltMetric(false);
@@ -92,10 +90,8 @@ void WrapPropertyPanel::dispose()
 {
     mxSpacingLB.reset();
 
-    mxWrapOptions2Dispatch.reset();
-    mxWrapOptions2.reset();
-    mxWrapOptions1Dispatch.reset();
-    mxWrapOptions1.reset();
+    mxWrapOptionsDispatch.reset();
+    mxWrapOptions.reset();
 
     maSwLRSpacingControl.dispose();
     maSwULSpacingControl.dispose();
