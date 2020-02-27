LibreOffice/core

commit 0528012fd0dc4b93645ef7790b0db9d1cecbae66

loplugin:constantparam
Change-Id: If2dee122bf07dc179fd8f6e766442ec1891a5f3d
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/87845
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sw/source/core/doc/doccomp.cxx
@@ -149,8 +149,8 @@ public:
 class CompareMainText : public CompareData
 {
 public:
-    CompareMainText(SwDoc &rD, bool bRecordDiff=true)
-        : CompareData(rD, bRecordDiff)
+    CompareMainText(SwDoc &rD)
+        : CompareData(rD, false)
     {
     }
 
@@ -2082,8 +2082,8 @@ long SwDoc::MergeDoc( const SwDoc& rDoc )
     rSrcDoc.getIDocumentRedlineAccess().SetRedlineFlags( RedlineFlags::ShowDelete );
     getIDocumentRedlineAccess().SetRedlineFlags( RedlineFlags::ShowDelete );
 
-    CompareMainText aD0(rSrcDoc, false);
-    CompareMainText aD1(*this, false);
+    CompareMainText aD0(rSrcDoc);
+    CompareMainText aD1(*this);
     aD1.CompareLines( aD0 );
     if( !aD1.HasDiffs( aD0 ) )
     {

basegfx/source/polygon/b2dlinegeometry.cxx
@@ -848,8 +848,7 @@ namespace basegfx
             css::drawing::LineCap eCap,
             double fMaxAllowedAngle,
             double fMaxPartOfEdge,
-            double fMiterMinimumAngle,
-            basegfx::triangulator::B2DTriangleVector* pTriangles)
+            double fMiterMinimumAngle)
         {
             if(fMaxAllowedAngle > F_PI2)
             {
@@ -959,7 +958,7 @@ namespace basegfx
                                         fHalfLineWidth,
                                         eJoin,
                                         fMiterMinimumAngle,
-                                        pTriangles));
+                                        nullptr));
                             }
                             else if(aOrientation == B2VectorOrientation::Negative)
                             {
@@ -976,7 +975,7 @@ namespace basegfx
                                         fHalfLineWidth,
                                         eJoin,
                                         fMiterMinimumAngle,
-                                        pTriangles));
+                                        nullptr));
                             }
                         }
 
@@ -995,7 +994,7 @@ namespace basegfx
                                     bLast && eCap == css::drawing::LineCap_ROUND,
                                     bFirst && eCap == css::drawing::LineCap_SQUARE,
                                     bLast && eCap == css::drawing::LineCap_SQUARE,
-                                    pTriangles));
+                                    nullptr));
                         }
                         else
                         {
@@ -1007,7 +1006,7 @@ namespace basegfx
                                     false,
                                     false,
                                     false,
-                                    pTriangles));
+                                    nullptr));
                         }
 
                         // prepare next step
@@ -1030,17 +1029,7 @@ namespace basegfx
                             aCandidate.getB2DPoint(0),
                             fHalfLineWidth));
 
-                    if(nullptr != pTriangles)
-                    {
-                        const basegfx::triangulator::B2DTriangleVector aResult(
-                            basegfx::triangulator::triangulate(
-                                aCircle));
-                        pTriangles->insert(pTriangles->end(), aResult.begin(), aResult.end());
-                    }
-                    else
-                    {
-                        aRetval.append(aCircle);
-                    }
+                    aRetval.append(aCircle);
                 }
 
                 return aRetval;

chart2/source/controller/dialogs/ChartResourceGroups.cxx
@@ -122,12 +122,12 @@ StackingResourceGroup::StackingResourceGroup(weld::Builder* pBuilder)
     m_xRB_Stack_Z->connect_toggled(LINK(this, StackingResourceGroup, StackingChangeHdl));
 }
 
-void StackingResourceGroup::showControls(bool bShow, bool bShowDeepStacking)
+void StackingResourceGroup::showControls(bool bShow)
 {
     m_xCB_Stacked->set_visible(bShow);
     m_xRB_Stack_Y->set_visible(bShow);
     m_xRB_Stack_Y_Percent->set_visible(bShow);
-    m_xRB_Stack_Z->set_visible(bShow && bShowDeepStacking);
+    m_xRB_Stack_Z->set_visible(false);
 }
 
 void StackingResourceGroup::fillControls(const ChartTypeParameter& rParameter)

chart2/source/controller/dialogs/tp_ChartType.cxx
@@ -266,7 +266,7 @@ void ChartTypeTabPage::showAllControls( ChartTypeDialogController& rTypeControll
     bool bShow = rTypeController.shouldShow_3DLookControl();
     m_pDim3DLookResourceGroup->showControls( bShow );
     bShow = rTypeController.shouldShow_StackingControl();
-    m_pStackingResourceGroup->showControls( bShow, false );
+    m_pStackingResourceGroup->showControls( bShow );
     bShow = rTypeController.shouldShow_SplineControl();
     m_pSplineResourceGroup->showControls( bShow );
     bShow = rTypeController.shouldShow_GeometryControl();
@@ -344,7 +344,7 @@ void ChartTypeTabPage::initializePage()
     {
         m_xSubTypeList->Hide();
         m_pDim3DLookResourceGroup->showControls( false );
-        m_pStackingResourceGroup->showControls( false, false );
+        m_pStackingResourceGroup->showControls( false );
         m_pSplineResourceGroup->showControls( false );
         m_pGeometryResourceGroup->showControls( false );
         m_pSortByXValuesResourceGroup->showControls( false );

chart2/source/controller/inc/ObjectHierarchy.hxx
@@ -42,15 +42,13 @@ class ObjectHierarchy
 public:
     typedef std::vector< ObjectIdentifier > tChildContainer;
 
-    /** @param bFlattenDiagram
-            If <TRUE/>, the content of the diagram (data series, wall, floor,
-            etc.) is treated as being at the same level as the diagram. (This is
-            used for keyboard navigation).
+    /** The content of the diagram (data series, wall, floor,
+        etc.) is treated as being at the same level as the diagram. (This is
+        used for keyboard navigation).
      */
     explicit ObjectHierarchy(
         const css::uno::Reference< css::chart2::XChartDocument > & xChartDocument,
         ExplicitValueProvider * pExplicitValueProvider,
-        bool bFlattenDiagram = false,
         bool bOrderingForElementSelector = false );
     ~ObjectHierarchy();
 

chart2/source/controller/main/ElementSelector.cxx
@@ -105,7 +105,7 @@ void SelectorListBox::UpdateChartElementsListAndSelection()
         if( xFact.is() )
             xChartView = xFact->createInstance( CHART_VIEW_SERVICE_NAME );
         ExplicitValueProvider* pExplicitValueProvider = nullptr; //ExplicitValueProvider::getExplicitValueProvider(xChartView); this creates all visible data points, that's too much
-        ObjectHierarchy aHierarchy( xChartDoc, pExplicitValueProvider, true /*bFlattenDiagram*/, true /*bOrderingForElementSelector*/ );
+        ObjectHierarchy aHierarchy( xChartDoc, pExplicitValueProvider, true /*bOrderingForElementSelector*/ );
         lcl_addObjectsToList( aHierarchy, ::chart::ObjectHierarchy::getRootNodeOID(), m_aEntries, 0, xChartDoc );
 
         if( bAddSelectionToList )

chart2/source/controller/main/ObjectHierarchy.cxx
@@ -611,9 +611,8 @@ ObjectIdentifier ImplObjectHierarchy::getParent(
 ObjectHierarchy::ObjectHierarchy(
     const Reference< XChartDocument > & xChartDocument,
     ExplicitValueProvider * pExplicitValueProvider /* = 0 */,
-    bool bFlattenDiagram /* = false */,
     bool bOrderingForElementSelector /* = false */) :
-        m_apImpl( new impl::ImplObjectHierarchy( xChartDocument, pExplicitValueProvider, bFlattenDiagram, bOrderingForElementSelector ))
+        m_apImpl( new impl::ImplObjectHierarchy( xChartDocument, pExplicitValueProvider, /*bFlattenDiagram*/false, bOrderingForElementSelector ))
 {}
 
 ObjectHierarchy::~ObjectHierarchy()

chart2/source/controller/sidebar/ChartTypePanel.cxx
@@ -217,7 +217,7 @@ void ChartTypePanel::Initialize()
     {
         m_xSubTypeList->Hide();
         m_pDim3DLookResourceGroup->showControls(false);
-        m_pStackingResourceGroup->showControls(false, false);
+        m_pStackingResourceGroup->showControls(false);
         m_pSplineResourceGroup->showControls(false);
         m_pGeometryResourceGroup->showControls(false);
         m_pSortByXValuesResourceGroup->showControls(false);
@@ -314,7 +314,7 @@ void ChartTypePanel::showAllControls(ChartTypeDialogController& rTypeController)
     bool bShow = rTypeController.shouldShow_3DLookControl();
     m_pDim3DLookResourceGroup->showControls(bShow);
     bShow = rTypeController.shouldShow_StackingControl();
-    m_pStackingResourceGroup->showControls(bShow, false);
+    m_pStackingResourceGroup->showControls(bShow);
     bShow = rTypeController.shouldShow_SplineControl();
     m_pSplineResourceGroup->showControls(bShow);
     bShow = rTypeController.shouldShow_GeometryControl();

commit 4d9cb1b7b5f381953d5b08f23953dfe83d396b2f

tdf#130960 compare documents does not work
regression from
    commit 0528012fd0dc4b93645ef7790b0db9d1cecbae66
    loplugin:constantparam

Change-Id: I7e6d366c60da95f7839755e4ce228cfeff7354af

sw/source/core/doc/doccomp.cxx
@@ -149,8 +149,8 @@ public:
 class CompareMainText : public CompareData
 {
 public:
-    CompareMainText(SwDoc &rD)
-        : CompareData(rD, false)
+    CompareMainText(SwDoc &rD, bool bRecordDiff)
+        : CompareData(rD, bRecordDiff)
     {
     }
 
@@ -1780,8 +1780,8 @@ namespace
     {
         Comparators aComparisons;
         //compare main text
-        aComparisons.emplace_back(std::make_shared<CompareMainText>(rSrcDoc),
-                                  std::make_shared<CompareMainText>(rDestDoc));
+        aComparisons.emplace_back(std::make_shared<CompareMainText>(rSrcDoc, true),
+                                  std::make_shared<CompareMainText>(rDestDoc, true));
 
         //if we have the same number of frames then try to compare within them
         const SwFrameFormats *pSrcFrameFormats = rSrcDoc.GetSpzFrameFormats();
@@ -2082,8 +2082,8 @@ long SwDoc::MergeDoc( const SwDoc& rDoc )
     rSrcDoc.getIDocumentRedlineAccess().SetRedlineFlags( RedlineFlags::ShowDelete );
     getIDocumentRedlineAccess().SetRedlineFlags( RedlineFlags::ShowDelete );
 
-    CompareMainText aD0(rSrcDoc);
-    CompareMainText aD1(*this);
+    CompareMainText aD0(rSrcDoc, false);
+    CompareMainText aD1(*this, false);
     aD1.CompareLines( aD0 );
     if( !aD1.HasDiffs( aD0 ) )
     {