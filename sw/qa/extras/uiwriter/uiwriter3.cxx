fe01d8561339420bdbf8d98a8fd2b0fca78b6707
Xisco Fauli <xiscofauli@libreoffice.org>
2020-02-27 11:41:24 +0100

    tdf#129382: Add unittest
    Change-Id: If22d82d506c365ae956de3d15e64c7065ad6b585
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/89625
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

---
+++ sw/qa/extras/uiwriter/uiwriter3.cxx
@@ -0,0 +1,36 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <swmodeltestbase.hxx>
+
+namespace
+{
+char const DATA_DIRECTORY[] = "/sw/qa/extras/uiwriter/data3/";
+} // namespace
+
+/// Third set of tests asserting the behavior of Writer user interface shells.
+class SwUiWriterTest3 : public SwModelTestBase
+{
+};
+
+CPPUNIT_TEST_FIXTURE(SwUiWriterTest3, testTdf129382)
+{
+    load(DATA_DIRECTORY, "tdf129382.docx");
+
+    SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument*>(mxComponent.get());
+    CPPUNIT_ASSERT(pTextDoc);
+
+    dispatchCommand(mxComponent, ".uno:SelectAll", {});
+    dispatchCommand(mxComponent, ".uno:Cut", {});
+    dispatchCommand(mxComponent, ".uno:Paste", {});
+    dispatchCommand(mxComponent, ".uno:Undo", {});
+    // without the fix, it crashes
+    dispatchCommand(mxComponent, ".uno:Undo", {});
+}
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
