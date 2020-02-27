be7adc4d93576932445bd67ef5dd1773e3e6d72c
Miklos Vajna <vmiklos@collabora.com>
2020-02-27 21:09:52 +0100

    xmloff: create SchXMLImport_Styles instances with an uno constructor
    See tdf#74608 for motivation.
    
    Change-Id: I517ee5a3b22ea90cbbd1eb82f08b69b2964898a9

--- solenv/bin/native-code.py
+++ solenv/bin/native-code.py
@@ -320,6 +320,7 @@ core_constructor_list = [
     "com_sun_star_comp_Impress_XMLClipboardExporter_get_implementation",
     "com_sun_star_comp_Chart_XMLOasisImporter_get_implementation",
     "com_sun_star_comp_Chart_XMLOasisMetaImporter_get_implementation",
+    "com_sun_star_comp_Chart_XMLOasisStylesImporter_get_implementation",
 # xmlscript/util/xmlscript.component
     "com_sun_star_comp_xmlscript_XMLBasicExporter",
     "com_sun_star_comp_xmlscript_XMLOasisBasicExporter",
