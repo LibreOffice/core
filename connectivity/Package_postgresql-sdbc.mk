e57f72d8fc81762860b4579df92db264e8d710ef
Rene Engelhard <rene@debian.org>
2020-02-27 08:24:39 +0100

    remove postgresql-sdbc.ini
    reading of it was removed in b11293297e581b87fba7e995d350ba59ecb820f2
    
    Change-Id: I1900082f8e94737111180a21b25de96afa034fdd
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/89615
    Reviewed-by: Julien Nabet <serval2412@yahoo.fr>
    Tested-by: Jenkins

--- connectivity/Package_postgresql-sdbc.mk
+++ connectivity/Package_postgresql-sdbc.mk
@@ -9,6 +9,4 @@
 
 $(eval $(call gb_Package_Package,connectivity_postgresql-sdbc,$(SRCDIR)/connectivity/source/drivers/postgresql))
 
-$(eval $(call gb_Package_add_file,connectivity_postgresql-sdbc,$(LIBO_ETC_FOLDER)/postgresql-sdbc.ini,postgresql-sdbc.ini))
-
 # vim: set noet sw=4 ts=4:
