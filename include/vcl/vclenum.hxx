LibreOffice/core

commit 65b7b6322b662785bf032e66c76abc36c9a2bb0e

loplugin:unusedenumconstants read-only constants in vcl
Change-Id: I8c81444cbecdc103cc47d9a4133b0b4d9010e0c2
Reviewed-on: https://gerrit.libreoffice.org/34018
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

include/vcl/vclenum.hxx
@@ -26,9 +26,7 @@
 
 enum class ExtTimeFieldFormat
 {
-    Short24H, Long24H,
-    Short12H, Long12H,
-    ShortDuration, LongDuration
+    Short24H, Long24H
 };
 
 enum class ExtDateFieldFormat

vcl/source/control/field2.cxx
@@ -2618,32 +2618,6 @@ void TimeField::SetExtFormat( ExtTimeFieldFormat eFormat )
             SetFormat( TimeFieldFormat::F_SEC );
         }
         break;
-        case ExtTimeFieldFormat::Short12H:
-        {
-            SetTimeFormat( TimeFormat::Hour12 );
-            SetDuration( false );
-            SetFormat( TimeFieldFormat::F_NONE );
-        }
-        break;
-        case ExtTimeFieldFormat::Long12H:
-        {
-            SetTimeFormat( TimeFormat::Hour12 );
-            SetDuration( false );
-            SetFormat( TimeFieldFormat::F_SEC );
-        }
-        break;
-        case ExtTimeFieldFormat::ShortDuration:
-        {
-            SetDuration( true );
-            SetFormat( TimeFieldFormat::F_NONE );
-        }
-        break;
-        case ExtTimeFieldFormat::LongDuration:
-        {
-            SetDuration( true );
-            SetFormat( TimeFieldFormat::F_SEC );
-        }
-        break;
         default:    OSL_FAIL( "ExtTimeFieldFormat unknown!" );
     }
 

commit c105fed82dcc7cca27d1690a74f9e0be84265d4f

tdf#130515 Time field in Base form will not display correct format
regression from
    commit 65b7b6322b662785bf032e66c76abc36c9a2bb0e
    Date:   Wed Feb 8 10:40:28 2017 +0200
    loplugin:unusedenumconstants read-only constants in vcl

Change-Id: Icf2e385763c8ece34521895331d148a5baacf2d1

include/vcl/vclenum.hxx
@@ -147,7 +147,10 @@ enum class TimeFormat
 
 enum class ExtTimeFieldFormat
 {
-    Short24H, Long24H
+    /** the first 4 of these are only used by base/dbaccess */
+    Short24H, Long24H,
+    Short12H, Long12H,
+    ShortDuration, LongDuration
 };
 
 enum class ExtDateFieldFormat

vcl/source/control/field2.cxx
@@ -2601,6 +2601,32 @@ void TimeField::SetExtFormat( ExtTimeFieldFormat eFormat )
             SetFormat( TimeFieldFormat::F_SEC );
         }
         break;
+        case ExtTimeFieldFormat::Short12H:
+        {
+            SetTimeFormat( TimeFormat::Hour12 );
+            SetDuration( false );
+            SetFormat( TimeFieldFormat::F_NONE );
+        }
+        break;
+        case ExtTimeFieldFormat::Long12H:
+        {
+            SetTimeFormat( TimeFormat::Hour12 );
+            SetDuration( false );
+            SetFormat( TimeFieldFormat::F_SEC );
+        }
+        break;
+        case ExtTimeFieldFormat::ShortDuration:
+        {
+            SetDuration( true );
+            SetFormat( TimeFieldFormat::F_NONE );
+        }
+        break;
+        case ExtTimeFieldFormat::LongDuration:
+        {
+            SetDuration( true );
+            SetFormat( TimeFieldFormat::F_SEC );
+        }
+        break;
         default:    OSL_FAIL( "ExtTimeFieldFormat unknown!" );
     }
 