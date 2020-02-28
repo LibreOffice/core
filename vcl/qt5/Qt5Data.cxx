aba618d2a4e1907d03211141de95a765dca357fe
Sarabjot Singh <saini.sarabjotsingh@gmail.com>
2020-02-28 22:47:40 +0300

    tdf#104169 - Change mouse cursor to FatCross in calc spreadsheet
    Added new cursor FatCross to calc. Change mouse cursor to
    FatCross while hovering over the spreadsheet in calc instead
    of earlier Arrow.
    
    Co-authored-by: Mesut Çifci <mesutcifci97@gmail.com>
    Change-Id: I672a058cc702eab0cba6ac953534def915766859

--- vcl/qt5/Qt5Data.cxx
+++ vcl/qt5/Qt5Data.cxx
@@ -150,6 +150,8 @@
 #include <unx/x11_cursors/wshide_mask.h>
 #include <unx/x11_cursors/wsshow_curs.h>
 #include <unx/x11_cursors/wsshow_mask.h>
+#include <unx/x11_cursors/fatcross_curs.h>
+#include <unx/x11_cursors/fatcross_mask.h>
 
 #include <unx/glyphcache.hxx>
 
@@ -181,12 +183,23 @@ static QCursor* getQCursorFromXBM(const unsigned char* pBitmap, const unsigned c
     QBitmap aMask = QBitmap::fromData(QSize(nWidth, nHeight), pMask);
     return new QCursor(aPixmap, aMask, nXHot, nYHot);
 }
-
+/*    static QCursor* getQCursorOnlyFromXBM(const unsigned char* pBitmap, int nWidth, int nHeight,
+ *                                       int nXHot, int nYHot)
+ *   {
+ *       QBitmap aPixmap = QBitmap::fromData(QSize(nWidth, nHeight), pBitmap);
+ *       return new QCursor(aPixmap, nXHot, nYHot);
+ *   } */
 #define MAKE_CURSOR(vcl_name, name)                                                                \
     case vcl_name:                                                                                 \
         pCursor = getQCursorFromXBM(name##curs##_bits, name##mask##_bits, name##curs_width,        \
                                     name##curs_height, name##curs_x_hot, name##curs_y_hot);        \
         break
+
+/*  #define MAKE_CURSOR_ONLY(vcl_name, name)                                                           \
+ *      case vcl_name:                                                                                 \
+ *          pCursor = getQCursorOnlyFromXBM(name##curs##_bits, name##curs_width, name##curs_height,    \
+ *                                          name##curs_x_hot, name##curs_y_hot);                       \
+ *           break */
 
 #define MAP_BUILTIN(vcl_name, qt_enum)                                                             \
     case vcl_name:                                                                                 \
@@ -302,6 +315,8 @@ QCursor& Qt5Data::getCursor(PointerStyle ePointerStyle)
 
             MAKE_CURSOR(PointerStyle::HideWhitespace, hidewhitespace_);
             MAKE_CURSOR(PointerStyle::ShowWhitespace, showwhitespace_);
+
+            MAKE_CURSOR(PointerStyle::FatCross, fatcross_);
             default:
                 break;
         }
