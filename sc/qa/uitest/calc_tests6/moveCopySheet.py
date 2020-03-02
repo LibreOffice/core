5750575771a44d248da597a359fe6abe0c2073ef
Xisco Fauli <xiscofauli@libreoffice.org>
2020-03-02 12:10:49 +0100

    uitest: check copy in .uno:Move
    Change-Id: I916d3853500705b1037aa669374511c184402313
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/89822
    Tested-by: Jenkins
    Reviewed-by: Xisco Faulí <xiscofauli@libreoffice.org>

--- sc/qa/uitest/calc_tests6/moveCopySheet.py
+++ sc/qa/uitest/calc_tests6/moveCopySheet.py
@@ -62,6 +62,23 @@ class moveCopySheet(UITestCase):
         self.assertEqual(document.Sheets[0].Name, "Sheet1")
         self.assertEqual(document.Sheets[1].Name, "moveName")
 
+        #Check copy option
+        self.ui_test.execute_dialog_through_command(".uno:Move")
+        xDialog = self.xUITest.getTopFocusWindow()
+        xCopy = xDialog.getChild("copy")
+        xCopy.executeAction("CLICK", tuple())
+        sheetName = "moveName_2"
+        newName = xDialog.getChild("newName")
+        self.assertEqual(get_state_as_dict(newName)["Text"], sheetName)
+
+        xOKBtn = xDialog.getChild("ok")
+        self.ui_test.close_dialog_through_button(xOKBtn)
+
+        self.assertEqual(document.Sheets.getCount(), 3)
+        self.assertEqual(document.Sheets[0].Name, sheetName)
+        self.assertEqual(document.Sheets[1].Name, "Sheet1")
+        self.assertEqual(document.Sheets[2].Name, "moveName")
+
         self.ui_test.close_doc()
 
 # vim: set shiftwidth=4 softtabstop=4 expandtab:
