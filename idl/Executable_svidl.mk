97f403e4ad1f5c3dff230e73135181d742519113
Caolán McNamara <caolanm@redhat.com>
2020-03-02 20:01:45 +0100

    ofz#20976 build-failure
    Change-Id: I6fc81e6ef13f77dea410f95e159a932089603aeb

--- idl/Executable_svidl.mk
+++ idl/Executable_svidl.mk
@@ -21,6 +21,10 @@ $(eval $(call gb_Executable_Executable,svidl))
 
 $(eval $(call gb_Executable_use_external,svidl,boost_headers))
 
+ifeq ($(DISABLE_DYNLOADING),TRUE)
+$(eval $(call gb_Executable_use_external,svidl,dtoa))
+endif
+
 $(eval $(call gb_Executable_set_include,svidl,\
 	$$(INCLUDE) \
 	-I$(SRCDIR)/idl/inc \
