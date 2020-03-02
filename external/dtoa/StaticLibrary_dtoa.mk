c980680c65e99c5cbe2eaf60b413a7c34cd3fd6c
Mike Kaganski <mike.kaganski@collabora.com>
2020-03-02 23:40:06 +0300

    Use locks in dtoa.c to be thread-safe
    Define MULTIPLE_THREADS and implement ACQUIRE/FREE_DTOA_LOCK.
    This makes the static library depend on sal; but as sal uses
    it, this dependency is not mentioned here to avoid circular
    dependency in makefiles.
    
    Also don't expose dtoa and other symbols - make them static.
    And define IEEE_MC68k, not IEEE_8087 on big endian platforms.
    
    Change-Id: Ie59fe0cf057b043556a5ac15ec6b11a1a5b41f47

--- external/dtoa/StaticLibrary_dtoa.mk
+++ external/dtoa/StaticLibrary_dtoa.mk
@@ -12,12 +12,19 @@ $(eval $(call gb_StaticLibrary_StaticLibrary,dtoa))
 $(eval $(call gb_StaticLibrary_use_unpacked,dtoa,dtoa))
 
 # A place that duplicates these settings is jurt/source/pipe/staticsalhack.cxx:
-$(eval $(call gb_StaticLibrary_add_cflags,dtoa,-DIEEE_8087))
+$(eval $(call gb_StaticLibrary_add_cflags,dtoa,\
+    $(if $(filter little,$(ENDIANNESS)),-DIEEE_8087,-DIEEE_MC68k) \
+    -DMULTIPLE_THREADS \
+))
 
 $(eval $(call gb_StaticLibrary_set_warnings_disabled,dtoa))
 
 $(eval $(call gb_StaticLibrary_add_generated_cobjects,dtoa,\
     UnpackedTarball/dtoa/src/dtoa \
+))
+
+$(eval $(call gb_StaticLibrary_add_exception_objects,dtoa,\
+    external/dtoa/source/dtoa_locking \
 ))
 
 # vim: set noet sw=4 ts=4:
