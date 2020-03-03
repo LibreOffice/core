879e1d2a47b9431c66ebcaaefb669d2988d6cff7
Xisco Fauli <xiscofauli@libreoffice.org>
2020-03-03 17:05:21 +0100

    tdf#130960, tdf#109376: Add uitest
    Change-Id: I2db9729620ab87c8934fa26a63f31c6a95e5a4fa

---
+++ sw/qa/uitest/writer_tests/compareDocuments.py
@@ -0,0 +1,61 @@
+#! /usr/bin/env python
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+from uitest.framework import UITestCase
+from uitest.path import get_srcdir_url
+from uitest.uihelper.common import get_state_as_dict
+from libreoffice.uno.propertyvalue import mkPropertyValues
+import datetime
+
+def get_url_for_data_file(file_name):
+    return get_srcdir_url() + "/sw/qa/uitest/writer_tests/data/" + file_name
+
+
+class compareDocuments(UITestCase):
+
+    def test_tdf130960(self):
+
+        writer_doc = self.ui_test.load_file(get_url_for_data_file("tdf130960.odt"))
+
+        document = self.ui_test.get_component()
+        xWriterDoc = self.xUITest.getTopFocusWindow()
+
+        self.ui_test.execute_dialog_through_command(".uno:CompareDocuments")
+
+        xOpenDialog = self.xUITest.getTopFocusWindow()
+        xFileName = xOpenDialog.getChild("file_name")
+        xFileName.executeAction("TYPE", mkPropertyValues({"TEXT": get_url_for_data_file("tdf130960_2.odt")}))
+
+        xOpenBtn = xOpenDialog.getChild("open")
+        xOpenBtn.executeAction("CLICK", tuple())
+
+        # Close the dialog and open it again so the list of changes is updated
+        xTrackDlg = self.xUITest.getTopFocusWindow()
+        xcloseBtn = xTrackDlg.getChild("close")
+        xcloseBtn.executeAction("CLICK", tuple())
+
+        self.ui_test.execute_modeless_dialog_through_command(".uno:AcceptTrackedChanges")
+        xTrackDlg = self.xUITest.getTopFocusWindow()
+        changesList = xTrackDlg.getChild("writerchanges")
+
+        text = "Unknown Author\t" + datetime.datetime.now().strftime("%m/%d/%Y")
+        self.assertEqual(11, len(changesList.getChildren()))
+        for i in range(11):
+            self.assertTrue(get_state_as_dict(changesList.getChild(str(i)))["Text"].startswith(text))
+
+        # tdf#109376 - it would crash here
+        xAccBtn = xTrackDlg.getChild("acceptall")
+        xAccBtn.executeAction("CLICK", tuple())
+
+        xcloseBtn = xTrackDlg.getChild("close")
+        xcloseBtn.executeAction("CLICK", tuple())
+
+        self.ui_test.close_doc()
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
