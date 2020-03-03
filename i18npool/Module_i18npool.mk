642b7706101a991efc7e312f59dece640d8bfb60
Miklos Vajna <vmiklos@collabora.com>
2020-03-03 09:27:01 +0100

    sw padded numbering: add layout
    lcl_formatArabicZero() looks a bit over-complicated with its hardcoded
    limit of 2. Word supports limits of 3, 4 and 5 as well, so prepare for
    handling them in a generic way.
    
    Change-Id: If6e5634b11616f0ac05e1387016e22f4b171bbfb
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/89864
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Tested-by: Jenkins

--- i18npool/Module_i18npool.mk
+++ i18npool/Module_i18npool.mk
@@ -43,6 +43,7 @@ $(eval $(call gb_Module_add_check_targets,i18npool,\
 	CppunitTest_i18npool_test_characterclassification \
 	CppunitTest_i18npool_test_ordinalsuffix \
 	CppunitTest_i18npool_test_textsearch \
+	CppunitTest_i18npool_defaultnumberingprovider \
 ))
 
 # vim: set noet sw=4 ts=4:
