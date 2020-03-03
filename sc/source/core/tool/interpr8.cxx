LibreOffice/core

commit fb790880eb11552b2e7d1dcf6c09d2663712290f

tdf#94635 follow up; handle linear data properly when samples in period
are to be calculated by function.

Applies to all FORECAST.ETS functions with argument period_length having
value 1.

Change-Id: Icb5a0288670321caf0dd5b70bee4b8811b9ba4a9
Reviewed-on: https://gerrit.libreoffice.org/28923
Reviewed-by: Eike Rathke <erack@redhat.com>
Tested-by: Eike Rathke <erack@redhat.com>

sc/source/core/tool/interpr8.cxx
@@ -195,11 +195,6 @@ bool ScETSForecastCalculation::PreprocessDataRange( const ScMatrixRef& rMatX, co
         }
     }
 
-    if ( rSmplInPrd != 1 )
-        mnSmplInPrd = rSmplInPrd;
-    else
-        mnSmplInPrd = CalcPeriodLen();
-
     // Month intervals don't have exact stepsize, so first
     // detect if month interval is used.
     // Method: assume there is an month interval and verify.
@@ -378,6 +373,16 @@ bool ScETSForecastCalculation::PreprocessDataRange( const ScMatrixRef& rMatX, co
             }
         }
     }
+
+    if ( rSmplInPrd != 1 )
+        mnSmplInPrd = rSmplInPrd;
+    else
+    {
+         mnSmplInPrd = CalcPeriodLen();
+        if ( mnSmplInPrd == 1 )
+            bEDS = true; // period length 1 means no periodic data: EDS suffices
+    }
+
     if ( !initData() )
         return false;  // note: mnErrorValue is set in called function(s)
 
@@ -536,7 +541,7 @@ SCSIZE ScETSForecastCalculation::CalcPeriodLen()
     SCSIZE nBestVal = mnCount;
     double fBestME = ::std::numeric_limits<double>::max();
 
-    for ( SCSIZE nPeriodLen = mnCount / 2; nPeriodLen > 1; nPeriodLen-- )
+    for ( SCSIZE nPeriodLen = mnCount / 2; nPeriodLen >= 1; nPeriodLen-- )
     {
         double fMeanError = 0.0;
         SCSIZE nPeriods = mnCount / nPeriodLen;
@@ -548,7 +553,7 @@ SCSIZE ScETSForecastCalculation::CalcPeriodLen()
         }
         fMeanError /= static_cast< double >( ( nPeriods - 1 ) * nPeriodLen - 1 );
 
-        if ( fMeanError < fBestME || fMeanError == 0.0 )
+        if ( fMeanError <= fBestME || fMeanError == 0.0 )
         {
             nBestVal = nPeriodLen;
             fBestME = fMeanError;

commit 10eae8d5923a8e46bff108b6211eff8a9d3fa814

tdf#131096: Fix crashes when viewing details of 2 Stat func in Func Wizard
See bt here:
https://bugs.documentfoundation.org/attachment.cgi?id=158354

Regression from:
https://cgit.freedesktop.org/libreoffice/core/commit/?id=fb790880eb11552b2e7d1dcf6c09d2663712290f
author	Winfried Donkers <winfrieddonkers@libreoffice.org>	2016-09-15 13:39:35 +0200
committer	Eike Rathke <erack@redhat.com>	2016-10-13 19:04:48 +0000
commit	fb790880eb11552b2e7d1dcf6c09d2663712290f (patch)
tree	a4626267b442f2fee3ef81186d053cb93d900187
parent	7d5d5e6e9b67807560a7db4123e8b41cc0f70a32 (diff)
tdf#94635 follow up; handle linear data properly when samples in period
are to be calculated by function.

Applies to all FORECAST.ETS functions with argument period_length having
value 1.

Change-Id: Ie29df11e481f724c2714e4faf53aa166ae19e957

sc/source/core/tool/interpr8.cxx
@@ -372,6 +372,9 @@ bool ScETSForecastCalculation::PreprocessDataRange( const ScMatrixRef& rMatX, co
         }
     }
 
+    if ( !initData() )
+        return false;  // note: mnErrorValue is set in called function(s)
+
     if ( nSmplInPrd != 1 )
         mnSmplInPrd = nSmplInPrd;
     else
@@ -380,9 +383,6 @@ bool ScETSForecastCalculation::PreprocessDataRange( const ScMatrixRef& rMatX, co
         if ( mnSmplInPrd == 1 )
             bEDS = true; // period length 1 means no periodic data: EDS suffices
     }
-
-    if ( !initData() )
-        return false;  // note: mnErrorValue is set in called function(s)
 
     return true;
 }