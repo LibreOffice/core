LibreOffice/core

commit 52fece12345161911da2a531213b7d5541192aad

tweak GetBitmap methods in BitmapEx
so we return a const& for the normal case, just like other methods,
which reduces copying.

This revealed that CreateDisplayBitmap in Bitmap can be const.

Change-Id: I9f9b9ff0c52d7e95eaae62af152218be8847dd63
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/86836
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svx/source/dialog/_contdlg.cxx
@@ -635,10 +635,10 @@ IMPL_LINK( SvxSuperContourDlg, PipetteClickHdl, ContourWindow&, rWnd, void )
 
         if( aGraphic.GetType() == GraphicType::Bitmap )
         {
-            Bitmap      aBmp( aGraphic.GetBitmapEx().GetBitmap() );
+            const Bitmap & rBmp( aGraphic.GetBitmapEx().GetBitmap() );
             const long  nTol = static_cast<long>(m_xMtfTolerance->get_value(FieldUnit::PERCENT) * 255L / 100L);
 
-            aMask = aBmp.CreateMask( rColor, nTol );
+            aMask = rBmp.CreateMask( rColor, nTol );
 
             if( aGraphic.IsTransparent() )
                 aMask.CombineSimple( aGraphic.GetBitmapEx().GetMask(), BmpCombine::Or );
@@ -652,7 +652,7 @@ IMPL_LINK( SvxSuperContourDlg, PipetteClickHdl, ContourWindow&, rWnd, void )
 
                 aRedoGraphic = Graphic();
                 aUndoGraphic = aGraphic;
-                aGraphic = Graphic( BitmapEx( aBmp, aMask ) );
+                aGraphic = Graphic( BitmapEx( rBmp, aMask ) );
                 mnGrfChanged++;
 
                 bNewContour = (xQBox->run() == RET_YES);

drawinglayer/source/primitive2d/graphicprimitivehelper2d.cxx
@@ -261,22 +261,22 @@ namespace drawinglayer
                             case Disposal::Back:
                             {
                                 // #i70772# react on no mask, for primitives, too.
-                                const Bitmap aMask(rAnimationBitmap.maBitmapEx.GetMask());
-                                const Bitmap aContent(rAnimationBitmap.maBitmapEx.GetBitmap());
+                                const Bitmap & rMask(rAnimationBitmap.maBitmapEx.GetMask());
+                                const Bitmap & rContent(rAnimationBitmap.maBitmapEx.GetBitmap());
 
                                 maVirtualDeviceMask->Erase();
-                                maVirtualDevice->DrawBitmap(rAnimationBitmap.maPositionPixel, aContent);
+                                maVirtualDevice->DrawBitmap(rAnimationBitmap.maPositionPixel, rContent);
 
-                                if (aMask.IsEmpty())
+                                if (rMask.IsEmpty())
                                 {
-                                    const ::tools::Rectangle aRect(rAnimationBitmap.maPositionPixel, aContent.GetSizePixel());
+                                    const ::tools::Rectangle aRect(rAnimationBitmap.maPositionPixel, rContent.GetSizePixel());
                                     maVirtualDeviceMask->SetFillColor(COL_BLACK);
                                     maVirtualDeviceMask->SetLineColor();
                                     maVirtualDeviceMask->DrawRect(aRect);
                                 }
                                 else
                                 {
-                                    BitmapEx aExpandVisibilityMask(aMask, aMask);
+                                    BitmapEx aExpandVisibilityMask(rMask, rMask);
                                     maVirtualDeviceMask->DrawBitmapEx(rAnimationBitmap.maPositionPixel, aExpandVisibilityMask);
                                 }
 

emfio/source/reader/mtftools.cxx
@@ -1720,8 +1720,8 @@ namespace emfio
                                 // is inverted
                                 else if ( ( nWinRop == SRCAND ) && ( pSave2->nWinRop == SRCPAINT ) )
                                 {
-                                    Bitmap aMask( pSave->aBmpEx.GetBitmap() );
-                                    BitmapEx aBmpEx( pSave2->aBmpEx.GetBitmap(), aMask );
+                                    const Bitmap & rMask( pSave->aBmpEx.GetBitmap() );
+                                    BitmapEx aBmpEx( pSave2->aBmpEx.GetBitmap(), rMask );
                                     ImplDrawBitmap( aPos, aSize, aBmpEx );
                                     bDrawn = true;
                                     i++;
@@ -1729,8 +1729,8 @@ namespace emfio
                                 // tdf#90539
                                 else if ( ( nWinRop == SRCAND ) && ( pSave2->nWinRop == SRCINVERT ) )
                                 {
-                                    Bitmap aMask( pSave->aBmpEx.GetBitmap() );
-                                    BitmapEx aBmpEx( pSave2->aBmpEx.GetBitmap(), aMask );
+                                    const Bitmap & rMask( pSave->aBmpEx.GetBitmap() );
+                                    BitmapEx aBmpEx( pSave2->aBmpEx.GetBitmap(), rMask );
                                     ImplDrawBitmap( aPos, aSize, aBmpEx );
                                     bDrawn = true;
                                     i++;

filter/source/msfilter/mstoolbar.cxx
@@ -291,14 +291,14 @@ void TBCData::ImportToolBarControl( CustomToolBarImportHelper& helper, std::vect
                     TBCBitMap* pIconMask = pSpecificInfo->getIconMask();
                     if (pIconMask)
                     {
-                        Bitmap aMaskBase(pIconMask->getBitMap().GetBitmap());
-                        Size aMaskSize = aMaskBase.GetSizePixel();
+                        const Bitmap& rMaskBase(pIconMask->getBitMap().GetBitmap());
+                        Size aMaskSize = rMaskBase.GetSizePixel();
                         if (aMaskSize.Width() && aMaskSize.Height())
                         {
                             // according to the spec:
                             // "the iconMask is white in all the areas in which the icon is
                             // displayed as transparent and is black in all other areas."
-                            aBitEx = BitmapEx(aBitEx.GetBitmap(), aMaskBase.CreateMask(COL_WHITE));
+                            aBitEx = BitmapEx(aBitEx.GetBitmap(), rMaskBase.CreateMask(COL_WHITE));
                         }
                     }
 

include/vcl/bitmap.hxx
@@ -145,7 +145,7 @@ public:
 
     BitmapChecksum          GetChecksum() const;
 
-    Bitmap                  CreateDisplayBitmap( OutputDevice* pDisplay );
+    Bitmap                  CreateDisplayBitmap( OutputDevice* pDisplay ) const;
 
     static const BitmapPalette&
                             GetGreyPalette( int nEntries );

include/vcl/bitmapex.hxx
@@ -20,7 +20,6 @@
 #ifndef INCLUDED_VCL_BITMAPEX_HXX
 #define INCLUDED_VCL_BITMAPEX_HXX
 
-#include <o3tl/optional.hxx>
 #include <vcl/dllapi.h>
 #include <vcl/alpha.hxx>
 #include <vcl/Scanline.hxx>
@@ -73,9 +72,9 @@ public:
     bool                IsTransparent() const;
     TransparentType     GetTransparentType() const { return meTransparent; }
 
-    Bitmap              GetBitmap( o3tl::optional<Color> xTransparentReplaceColor = o3tl::optional<Color>() ) const;
+    Bitmap              GetBitmap( Color aTransparentReplaceColor ) const;
     /// Gives direct access to the contained bitmap.
-    const Bitmap&       GetBitmapRef() const;
+    const Bitmap&       GetBitmap() const;
     Bitmap              GetMask() const;
 
     bool                IsAlpha() const;

sc/source/filter/excel/xiescher.cxx
@@ -749,7 +749,7 @@ void XclImpDrawObjBase::ConvertFillStyle( SdrObject& rSdrObj, const XclObjFillDa
             aXOBitmap.SetPixelColor( aPattColor );
             aXOBitmap.SetBackgroundColor( aBackColor );
             aXOBitmap.Array2Bitmap();
-            aBitmap = aXOBitmap.GetBitmap().GetBitmapRef();
+            aBitmap = aXOBitmap.GetBitmap().GetBitmap();
 
             rSdrObj.SetMergedItem(XFillStyleItem(drawing::FillStyle_BITMAP));
             rSdrObj.SetMergedItem(XFillBitmapItem(EMPTY_OUSTRING, Graphic(aBitmap)));

slideshow/source/engine/shapes/gdimtftools.cxx
@@ -315,14 +315,14 @@ bool getAnimationFromGraphic( VectorOfMtfAnimationFrames&   o_rFrames,
             {
                 // #i70772# react on no mask
                 const Bitmap aMask(rAnimationBitmap.maBitmapEx.GetMask());
-                const Bitmap aContent(rAnimationBitmap.maBitmapEx.GetBitmap());
+                const Bitmap & rContent(rAnimationBitmap.maBitmapEx.GetBitmap());
 
                 pVDevMask->Erase();
-                pVDev->DrawBitmap(rAnimationBitmap.maPositionPixel, aContent);
+                pVDev->DrawBitmap(rAnimationBitmap.maPositionPixel, rContent);
 
                 if(aMask.IsEmpty())
                 {
-                    const tools::Rectangle aRect(rAnimationBitmap.maPositionPixel, aContent.GetSizePixel());
+                    const tools::Rectangle aRect(rAnimationBitmap.maPositionPixel, rContent.GetSizePixel());
                     pVDevMask->SetFillColor( COL_BLACK);
                     pVDevMask->SetLineColor();
                     pVDevMask->DrawRect(aRect);

commit f3afe75905d56c0c3f938f2afc4860d4aad7db19

tdf#131015 crash using the pipette in Edit Contour dialog
regression from
    commit 52fece12345161911da2a531213b7d5541192aad
    Date:   Wed Jan 15 12:43:19 2020 +0200
    tweak GetBitmap methods in BitmapEx
we end up self-assigning, and with a little bit of optimisation, we can
up overwriting parts of the graphic we need to construct the new
graphic.

Change-Id: Ie7e9ce3f9a69f1fe954079873c206985b81db68a

svx/source/dialog/_contdlg.cxx
@@ -623,17 +623,15 @@ IMPL_LINK( SvxSuperContourDlg, PipetteClickHdl, ContourWindow&, rWnd, void )
 {
     if ( rWnd.IsClickValid() )
     {
-        Bitmap          aMask;
         const Color&    rColor = rWnd.GetPipetteColor();
 
         weld::WaitObject aWaitObj(&m_rDialog);
 
         if( aGraphic.GetType() == GraphicType::Bitmap )
         {
-            const Bitmap & rBmp( aGraphic.GetBitmapEx().GetBitmap() );
             const long  nTol = static_cast<long>(m_xMtfTolerance->get_value(FieldUnit::PERCENT) * 255 / 100);
 
-            aMask = rBmp.CreateMask( rColor, nTol );
+            Bitmap aMask = aGraphic.GetBitmapEx().GetBitmap().CreateMask( rColor, nTol );
 
             if( aGraphic.IsTransparent() )
                 aMask.CombineSimple( aGraphic.GetBitmapEx().GetMask(), BmpCombine::Or );
@@ -647,7 +645,8 @@ IMPL_LINK( SvxSuperContourDlg, PipetteClickHdl, ContourWindow&, rWnd, void )
 
                 aRedoGraphic = Graphic();
                 aUndoGraphic = aGraphic;
-                aGraphic = Graphic( BitmapEx( rBmp, aMask ) );
+                Bitmap aBmp = aGraphic.GetBitmapEx().GetBitmap();
+                aGraphic = Graphic( BitmapEx( aBmp, aMask ) );
                 mnGrfChanged++;
 
                 bNewContour = (xQBox->run() == RET_YES);