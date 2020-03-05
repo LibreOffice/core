50f1e87e156ecd559f12917d62488959c7d82f94
Marco Cecchetti <marco.cecchetti@collabora.com>
2020-03-05 11:42:29 +0100

    lok: avoid to grab the focus from the formula bar
    Change-Id: I13a2766e3c5dfd67cc89b5303e4a06f058b43891

--- editeng/source/editeng/impedit.cxx
+++ editeng/source/editeng/impedit.cxx
@@ -1196,8 +1196,13 @@ void ImpEditView::ShowCursor( bool bGotoCursor, bool bForceVisCursor )
                     }
                 }
 
-                SfxLokHelper::notifyVisCursorInvalidation(mpViewShell, sRect, bIsWrong, sHyperlink);
-                mpViewShell->NotifyOtherViews(LOK_CALLBACK_INVALIDATE_VIEW_CURSOR, "rectangle", sRect);
+                // We need to avoid to grab the focus from the formula bar
+                vcl::Window* pWindow = Application::GetFocusWindow();
+                if (pWindow && pWindow == pOutWin)
+                {
+                    SfxLokHelper::notifyVisCursorInvalidation(mpViewShell, sRect, bIsWrong, sHyperlink);
+                    mpViewShell->NotifyOtherViews(LOK_CALLBACK_INVALIDATE_VIEW_CURSOR, "rectangle", sRect);
+                }
             }
         }
 
