bfc61208fd4798e640e27b862a11e32ac39c33e1
Miklos Vajna <vmiklos@collabora.com>
2020-03-06 16:16:56 +0100

    sw chicago numbering: add RTF footnote export
    Chicago numbering is not supported for paragraph numbering (same as
    DOC), so focus on footnote/endnote export only.
    
    There is markup in RTF to store doc-global footnote/endnote numbering
    type and the same for per-section. DOC writes both, then Writer only
    reads the global setting and Word only reads the per-section setting.
    
    This means only export is needed here, import already handled the
    doc-global markup, and that's enough.
    
    (cherry picked from commit 4ba09be7e260ce2a79a23465db7b2837422cde30)
    
    Conflicts:
    	sw/qa/extras/rtfexport/rtfexport5.cxx
    
    Change-Id: I3590560ca913e04078988fe4784e50fa5cbf17bf

--- sw/source/filter/ww8/rtfexport.cxx
+++ sw/source/filter/ww8/rtfexport.cxx
@@ -838,6 +838,7 @@ ErrCode RtfExport::ExportDocument_Impl()
         Strm()
             .WriteCharPtr(OOO_STRING_SVTOOLS_RTF_SECTD)
             .WriteCharPtr(OOO_STRING_SVTOOLS_RTF_SBKNONE);
+        m_pAttrOutput->SectFootnoteEndnotePr();
         // All sections are unlocked by default
         Strm().WriteCharPtr(OOO_STRING_SVTOOLS_RTF_SECTUNLOCKED);
         OutLong(1);
@@ -893,7 +894,7 @@ ErrCode RtfExport::ExportDocument_Impl()
             case SVX_NUM_ROMAN_UPPER:
                 pOut = OOO_STRING_SVTOOLS_RTF_FTNNRUC;
                 break;
-            case SVX_NUM_CHAR_SPECIAL:
+            case SVX_NUM_SYMBOL_CHICAGO:
                 pOut = OOO_STRING_SVTOOLS_RTF_FTNNCHI;
                 break;
             default:
@@ -926,7 +927,7 @@ ErrCode RtfExport::ExportDocument_Impl()
             case SVX_NUM_ROMAN_UPPER:
                 pOut = OOO_STRING_SVTOOLS_RTF_AFTNNRUC;
                 break;
-            case SVX_NUM_CHAR_SPECIAL:
+            case SVX_NUM_SYMBOL_CHICAGO:
                 pOut = OOO_STRING_SVTOOLS_RTF_AFTNNCHI;
                 break;
             default:
