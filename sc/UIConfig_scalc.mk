d9e3b08d83cfadbff12da2fbba6a37830fb6f7cf
Szymon Kłos <szymon.klos@collabora.com>
2020-03-09 15:40:35 +0100

    jsdialog: Add mobile version of data validation dialog
    Change-Id: I8c59892196ae015852fff6004c72e1990b35c1af
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/97762
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>

--- sc/UIConfig_scalc.mk
+++ sc/UIConfig_scalc.mk
@@ -125,6 +125,7 @@ $(eval $(call gb_UIConfig_add_uifiles,modules/scalc,\
 	sc/uiconfig/scalc/ui/dropmenu \
 	sc/uiconfig/scalc/ui/doubledialog \
 	sc/uiconfig/scalc/ui/erroralerttabpage \
+	sc/uiconfig/scalc/ui/erroralerttabpage-mobile \
 	sc/uiconfig/scalc/ui/externaldata \
 	sc/uiconfig/scalc/ui/exponentialsmoothingdialog \
 	sc/uiconfig/scalc/ui/filldlg \
@@ -244,6 +245,7 @@ $(eval $(call gb_UIConfig_add_uifiles,modules/scalc,\
 	sc/uiconfig/scalc/ui/validationdialog \
 	sc/uiconfig/scalc/ui/validationcriteriapage \
 	sc/uiconfig/scalc/ui/validationhelptabpage \
+	sc/uiconfig/scalc/ui/validationhelptabpage-mobile \
 	sc/uiconfig/scalc/ui/xmlsourcedialog \
 	sc/uiconfig/scalc/ui/ztestdialog \
 ))
