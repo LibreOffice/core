70479e1f4cb3c120f46239a648e65f035af2922d
Szymon Kłos <szymon.klos@collabora.com>
2020-03-10 16:24:16 +0100

    jsdialog: avoid crash in watermark dialog
    Change-Id: Iea4a0e6ce005d6a86f9a228026471dd4b4c7b2da
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/94340
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/96830
    Tested-by: Jenkins

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -3610,12 +3610,14 @@ static void doc_sendDialogEvent(LibreOfficeKitDocument* /*pThis*/, unsigned nWin
         static const OUString sDownAction("DOWN");
         static const OUString sValue("VALUE");
 
+        bool bIsWeldedDialog = false;
+
         try
         {
             OString sControlId = OUStringToOString(aMap["id"], RTL_TEXTENCODING_ASCII_US);
             weld::Widget* pWidget = JSInstanceBuilder::FindWeldWidgetsMap(nWindowId, sControlId);
 
-            bool bIsWeldedDialog = pWidget != nullptr;
+            bIsWeldedDialog = pWidget != nullptr;
             bool bContinueWithLOKWindow = false;
 
             if (bIsWeldedDialog)
@@ -3718,7 +3720,8 @@ static void doc_sendDialogEvent(LibreOfficeKitDocument* /*pThis*/, unsigned nWin
         } catch(...) {}
 
         // force resend
-        pWindow->Resize();
+        if (!bIsWeldedDialog)
+            pWindow->Resize();
     }
 }
 
