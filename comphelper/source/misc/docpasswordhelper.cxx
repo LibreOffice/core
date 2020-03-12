0d62c472085a5a0be07d98beb564a0cf21faebfb
Thorsten Behrens <Thorsten.Behrens@CIB.de>
2020-03-12 11:01:55 +0100

    comphelper: fix build against CentOS6 baseline
    Change-Id: Ice8a27dc92c923b3d2c62a6989e13720ff044749
    (cherry picked from commit 25e7d4a47171ec68202269a8f94fae002b7715c9)

--- comphelper/source/misc/docpasswordhelper.cxx
+++ comphelper/source/misc/docpasswordhelper.cxx
@@ -523,11 +523,11 @@ OUString DocPasswordHelper::GetOoxHashAsBase64(
 
     if (eResult == DocPasswordVerifierResult::OK && !aPassword.isEmpty())
     {
-        if (std::find_if(std::cbegin(aEncData), std::cend(aEncData),
+        if (std::find_if(aEncData.begin(), aEncData.end(),
                          [](const css::beans::NamedValue& val) {
                              return val.Name == PACKAGE_ENCRYPTIONDATA_SHA256UTF8;
                          })
-            == std::cend(aEncData))
+            == aEncData.end())
         {
             // tdf#118639: We need ODF encryption data for autorecovery, where password
             // will already be unavailable, so generate and append it here
