cf5b19170ae3e7a69512b22410974ee031feb091
Jussi Pakkanen <jpakkane@gmail.com>
2020-03-12 22:52:46 +0200

    Build smslots.hxx properly from source.
    Change-Id: I647b526f9116ab6a970bfa328fd147a9f2edab9f

---
+++ starmath/smslots.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import sys, os, subprocess
+
+svidl = sys.argv[1]
+source_root = sys.argv[2]
+build_root = sys.argv[3]
+build_dir = sys.argv[4]
+input_file = sys.argv[5]
+output_file = sys.argv[6]
+dep_file = sys.argv[7]
+
+input_abs = os.path.normpath(os.path.join(build_root, input_file))
+output_abs = os.path.join(build_root, output_file)
+dep_abs = os.path.join(build_root, dep_file)
+
+args = ['-quiet',
+        '-I' + os.path.join(source_root, 'starmath/sdi'),
+        '-I' + os.path.join(source_root, 'starmath/inc'),
+        '-I' + os.path.join(source_root, 'svx/sdi'),
+        '-I' + os.path.join(source_root, 'sfx2/sdi'),
+        '-I' + os.path.join(source_root, 'include'),
+        '-fx' + os.path.join(source_root, 'starmath/sdi/smath.sdi'),
+        '-fs' + output_abs,
+        '-fM' + dep_abs,
+        '-fm' + os.path.join(build_root, build_dir),
+        input_abs]
+
+sys.exit(subprocess.call([svidl] + args))
