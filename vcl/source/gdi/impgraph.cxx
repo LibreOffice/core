571755b3653bb06586e7cae232d1ca943719a732
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2020-03-13 20:04:45 +0100

    pdfium: support for pages when using PDF import with pdfium
    Moving PDF to use VectorGraphicData in Graphic has temporary
    removed the support for showing different PDF pages when opening
    the PDF using pdfium (LO_IMPORT_USE_PDFIUM=1).
    This adds the support for back by specifying whcih PDF page to
    render when creating the VectorGraphicData (and can't be changd
    afterwards), which is used to create a Graphic and contains the
    PDF source data array.
    
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/90562
    Tested-by: Jenkins
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>
    (cherry picked from commit 27ee05f860e8225a41e15e0853fcef00a9d7a621)
    
    Change-Id: Ib915216b8d4c0c063d0fead44ff156b1915a35d2
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/90844
    Tested-by: Tomaž Vajngerl <quikee@gmail.com>
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

--- vcl/source/gdi/impgraph.cxx
+++ vcl/source/gdi/impgraph.cxx
@@ -181,8 +181,7 @@ ImpGraphic::ImpGraphic() :
         mbSwapOut       ( false ),
         mbDummyContext  ( false ),
         maLastUsed (std::chrono::high_resolution_clock::now()),
-        mbPrepared      ( false ),
-        mnPageNumber(-1)
+        mbPrepared      ( false )
 {
 }
 
@@ -201,7 +200,6 @@ ImpGraphic::ImpGraphic(const ImpGraphic& rImpGraphic)
     , maGraphicExternalLink(rImpGraphic.maGraphicExternalLink)
     , maLastUsed (std::chrono::high_resolution_clock::now())
     , mbPrepared (rImpGraphic.mbPrepared)
-    , mnPageNumber(rImpGraphic.mnPageNumber)
 {
     if( rImpGraphic.mpAnimation )
     {
@@ -226,7 +224,6 @@ ImpGraphic::ImpGraphic(ImpGraphic&& rImpGraphic)
     , maGraphicExternalLink(rImpGraphic.maGraphicExternalLink)
     , maLastUsed (std::chrono::high_resolution_clock::now())
     , mbPrepared (rImpGraphic.mbPrepared)
-    , mnPageNumber(rImpGraphic.mnPageNumber)
 {
     rImpGraphic.ImplClear();
     rImpGraphic.mbDummyContext = false;
@@ -239,8 +236,7 @@ ImpGraphic::ImpGraphic(GraphicExternalLink const & rGraphicExternalLink) :
         mbDummyContext  ( false ),
         maGraphicExternalLink(rGraphicExternalLink),
         maLastUsed (std::chrono::high_resolution_clock::now()),
-        mbPrepared (false),
-        mnPageNumber(-1)
+        mbPrepared (false)
 {
 }
 
@@ -251,8 +247,7 @@ ImpGraphic::ImpGraphic( const Bitmap& rBitmap ) :
         mbSwapOut       ( false ),
         mbDummyContext  ( false ),
         maLastUsed (std::chrono::high_resolution_clock::now()),
-        mbPrepared (false),
-        mnPageNumber(-1)
+        mbPrepared (false)
 {
 }
 
@@ -263,8 +258,7 @@ ImpGraphic::ImpGraphic( const BitmapEx& rBitmapEx ) :
         mbSwapOut       ( false ),
         mbDummyContext  ( false ),
         maLastUsed (std::chrono::high_resolution_clock::now()),
-        mbPrepared (false),
-        mnPageNumber(-1)
+        mbPrepared (false)
 {
 }
 
@@ -275,8 +269,7 @@ ImpGraphic::ImpGraphic(const VectorGraphicDataPtr& rVectorGraphicDataPtr)
     mbDummyContext  ( false ),
     maVectorGraphicData(rVectorGraphicDataPtr),
     maLastUsed (std::chrono::high_resolution_clock::now()),
-    mbPrepared (false),
-    mnPageNumber(-1)
+    mbPrepared (false)
 {
 }
 
@@ -288,8 +281,7 @@ ImpGraphic::ImpGraphic( const Animation& rAnimation ) :
         mbSwapOut       ( false ),
         mbDummyContext  ( false ),
         maLastUsed (std::chrono::high_resolution_clock::now()),
-        mbPrepared (false),
-        mnPageNumber(-1)
+        mbPrepared (false)
 {
 }
 
@@ -300,8 +292,7 @@ ImpGraphic::ImpGraphic( const GDIMetaFile& rMtf ) :
         mbSwapOut       ( false ),
         mbDummyContext  ( false ),
         maLastUsed (std::chrono::high_resolution_clock::now()),
-        mbPrepared (false),
-        mnPageNumber(-1)
+        mbPrepared (false)
 {
 }
 
@@ -323,7 +314,6 @@ ImpGraphic& ImpGraphic::operator=( const ImpGraphic& rImpGraphic )
         maSwapInfo = rImpGraphic.maSwapInfo;
         mpContext = rImpGraphic.mpContext;
         mbDummyContext = rImpGraphic.mbDummyContext;
-        mnPageNumber = rImpGraphic.mnPageNumber;
         maGraphicExternalLink = rImpGraphic.maGraphicExternalLink;
 
         mpAnimation.reset();
@@ -363,7 +353,6 @@ ImpGraphic& ImpGraphic::operator=(ImpGraphic&& rImpGraphic)
     maSwapInfo = std::move(rImpGraphic.maSwapInfo);
     mpContext = std::move(rImpGraphic.mpContext);
     mbDummyContext = rImpGraphic.mbDummyContext;
-    mnPageNumber = rImpGraphic.mnPageNumber;
     mpAnimation = std::move(rImpGraphic.mpAnimation);
     maEx = std::move(rImpGraphic.maEx);
     mbSwapOut = rImpGraphic.mbSwapOut;
@@ -378,8 +367,6 @@ ImpGraphic& ImpGraphic::operator=(ImpGraphic&& rImpGraphic)
     maLastUsed = std::chrono::high_resolution_clock::now();
 
     vcl::graphic::Manager::get().changeExisting(this, aOldSizeBytes);
-
-    rImpGraphic.mnPageNumber = -1;
 
     return *this;
 }
@@ -1705,6 +1692,13 @@ bool ImpGraphic::ImplExportNative( SvStream& rOStm ) const
     }
 
     return bResult;
+}
+
+sal_Int32 ImpGraphic::getPageNumber() const
+{
+    if (maVectorGraphicData)
+        return maVectorGraphicData->getPageIndex();
+    return -1;
 }
 
 static std::map<BitmapChecksum, std::shared_ptr<std::vector<sal_Int8>>> sPdfDataCache;
--- vcl/source/gdi/vectorgraphicdata.cxx
+++ vcl/source/gdi/vectorgraphicdata.cxx
@@ -32,6 +32,7 @@
 #include <vcl/canvastools.hxx>
 #include <comphelper/seqstream.hxx>
 #include <comphelper/sequence.hxx>
+#include <comphelper/propertysequence.hxx>
 #include <vcl/svapp.hxx>
 #include <vcl/outdev.hxx>
 #include <vcl/wmfexternal.hxx>
@@ -145,7 +146,10 @@ void VectorGraphicData::ensurePdfReplacement()
 
     // use PDFium directly
     std::vector<Bitmap> aBitmaps;
-    vcl::RenderPDFBitmaps(maVectorGraphicDataArray.getConstArray(), maVectorGraphicDataArray.getLength(), aBitmaps, 0, 1/*, fResolutionDPI*/);
+    sal_Int32 nUsePageIndex = 0;
+    if (mnPageIndex >= 0)
+        nUsePageIndex = mnPageIndex;
+    vcl::RenderPDFBitmaps(maVectorGraphicDataArray.getConstArray(), maVectorGraphicDataArray.getLength(), aBitmaps, nUsePageIndex, 1/*, fResolutionDPI*/);
     maReplacement = aBitmaps[0];
 }
 
@@ -200,7 +204,9 @@ void VectorGraphicData::ensureSequenceAndRange()
                 uno::Sequence< ::beans::PropertyValue > aSequence;
 
                 if (mpExternalHeader)
+                {
                     aSequence = mpExternalHeader->getSequence();
+                }
 
                 if (myInputStream.is())
                     maSequence = comphelper::sequenceToContainer<std::deque<css::uno::Reference< css::graphic::XPrimitive2D >>>(xEmfParser->getDecomposition(myInputStream, maPath, aSequence));
@@ -210,7 +216,10 @@ void VectorGraphicData::ensureSequenceAndRange()
             case VectorGraphicDataType::Pdf:
             {
                 const uno::Reference<graphic::XPdfDecomposer> xPdfDecomposer = graphic::PdfTools::create(xContext);
-                auto xPrimitive2D = xPdfDecomposer->getDecomposition(maVectorGraphicDataArray);
+                uno::Sequence<beans::PropertyValue> aDecompositionParameters = comphelper::InitPropertySequence({
+                    {"PageIndex", uno::makeAny<sal_Int32>(mnPageIndex)},
+                });
+                auto xPrimitive2D = xPdfDecomposer->getDecomposition(maVectorGraphicDataArray, aDecompositionParameters);
                 maSequence = comphelper::sequenceToContainer<std::deque<uno::Reference<graphic::XPrimitive2D>>>(xPrimitive2D);
 
                 break;
@@ -261,7 +270,8 @@ auto VectorGraphicData::getSizeBytes() -> std::pair<State, size_t>
 VectorGraphicData::VectorGraphicData(
     const VectorGraphicDataArray& rVectorGraphicDataArray,
     const OUString& rPath,
-    VectorGraphicDataType eVectorDataType)
+    VectorGraphicDataType eVectorDataType,
+    sal_Int32 nPageIndex)
 :   maVectorGraphicDataArray(rVectorGraphicDataArray),
     maPath(rPath),
     mbSequenceCreated(false),
@@ -269,7 +279,8 @@ VectorGraphicData::VectorGraphicData(
     maSequence(),
     maReplacement(),
     mNestedBitmapSize(0),
-    meVectorGraphicDataType(eVectorDataType)
+    meVectorGraphicDataType(eVectorDataType),
+    mnPageIndex(nPageIndex)
 {
 }
 
@@ -283,7 +294,8 @@ VectorGraphicData::VectorGraphicData(
     maSequence(),
     maReplacement(),
     mNestedBitmapSize(0),
-    meVectorGraphicDataType(eVectorDataType)
+    meVectorGraphicDataType(eVectorDataType),
+    mnPageIndex(-1)
 {
     SvFileStream rIStm(rPath, StreamMode::STD_READ);
     if(rIStm.GetError())
