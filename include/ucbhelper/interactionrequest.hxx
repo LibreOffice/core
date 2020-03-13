12a8fa0d656f3f8bad7c9f85e4f760b5201d5f75
Noel Grandin <noel.grandin@collabora.co.uk>
2020-03-13 21:12:38 +0200

    Revert "loplugin:constfields in ucbhelper"
    This reverts commit 64035391ebe8810520a214a3ae0aeb4c1b039819.
    
    Change-Id: Icfde3e984f9ebf93a423d101aee385182f65dea5
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/90518
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

--- include/ucbhelper/interactionrequest.hxx
+++ include/ucbhelper/interactionrequest.hxx
@@ -292,21 +292,21 @@ class UCBHELPER_DLLPUBLIC InteractionSupplyAuthentication final :
                   public css::lang::XTypeProvider,
                   public css::ucb::XInteractionSupplyAuthentication2
 {
-    css::uno::Sequence< css::ucb::RememberAuthentication > const
+    css::uno::Sequence< css::ucb::RememberAuthentication >
                   m_aRememberPasswordModes;
-    css::uno::Sequence< css::ucb::RememberAuthentication > const
+    css::uno::Sequence< css::ucb::RememberAuthentication >
                   m_aRememberAccountModes;
     OUString m_aRealm;
     OUString m_aUserName;
     OUString m_aPassword;
     css::ucb::RememberAuthentication m_eRememberPasswordMode;
-    css::ucb::RememberAuthentication const m_eDefaultRememberPasswordMode;
-    css::ucb::RememberAuthentication const m_eDefaultRememberAccountMode;
-    bool const m_bCanSetRealm    : 1;
-    bool const m_bCanSetUserName : 1;
-    bool const m_bCanSetPassword : 1;
-    bool const m_bCanSetAccount  : 1;
-    bool const m_bCanUseSystemCredentials     : 1;
+    css::ucb::RememberAuthentication m_eDefaultRememberPasswordMode;
+    css::ucb::RememberAuthentication m_eDefaultRememberAccountMode;
+    bool m_bCanSetRealm    : 1;
+    bool m_bCanSetUserName : 1;
+    bool m_bCanSetPassword : 1;
+    bool m_bCanSetAccount  : 1;
+    bool m_bCanUseSystemCredentials     : 1;
     bool m_bUseSystemCredentials        : 1;
 
 public:
