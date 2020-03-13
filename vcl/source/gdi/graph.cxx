04a8d53466e2653a8d54f6261038d65e280adf0c
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2020-03-13 20:04:45 +0100

    pdfium: support for pages when using PDF import with pdfium
    Moving PDF to use VectorGraphicData in Graphic has temporary
    removed the support for showing different PDF pages when opening
    the PDF using pdfium (LO_IMPORT_USE_PDFIUM=1).
    This adds the support for back by specifying whcih PDF page to
    render when creating the VectorGraphicData (and can't be changd
    afterwards), which is used to create a Graphic and contains the
    PDF source data array.
    
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/90562
    Tested-by: Jenkins
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>
    (cherry picked from commit 27ee05f860e8225a41e15e0853fcef00a9d7a621)
    
    Change-Id: Ib915216b8d4c0c063d0fead44ff156b1915a35d2

--- vcl/source/gdi/graph.cxx
+++ vcl/source/gdi/graph.cxx
@@ -570,14 +570,9 @@ const VectorGraphicDataPtr& Graphic::getVectorGraphicData() const
     return mxImpGraphic->getVectorGraphicData();
 }
 
-void Graphic::setPageNumber(sal_Int32 nPageNumber)
-{
-    mxImpGraphic->mnPageNumber = nPageNumber;
-}
-
 sal_Int32 Graphic::getPageNumber() const
 {
-    return mxImpGraphic->mnPageNumber;
+    return mxImpGraphic->getPageNumber();
 }
 
 OUString Graphic::getOriginURL() const
