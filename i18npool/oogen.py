4cbf2aa11503a34bc0b7bb7014116b0994f80c6c
Jussi Pakkanen <jpakkane@gmail.com>
2020-03-14 15:40:11 +0200

    Generate openoffice_dat.c from source.
    Change-Id: I78f00cc0fb38cc5121a9d543197792a9f12e9177

---
+++ i18npool/oogen.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import os, sys, subprocess
+
+output_name = sys.argv[1]
+output_dir = os.path.split(output_name)[0]
+dattmp_name = os.path.splitext(output_name)[0] + '.tmp'
+gncmtmp_name = output_name.replace('_dat.c', '_tmp.c')
+priv_dir = sys.argv[2]
+rsp_name = os.path.join(priv_dir, dattmp_name)
+
+if not os.path.isdir(priv_dir):
+    os.mkdir(priv_dir)
+
+open(dattmp_name, 'w').write('''char_in.brk
+char.brk
+count_word.brk
+dict_word_he.brk
+dict_word_hu.brk
+dict_word_nodash.brk
+dict_word_prepostdash.brk
+dict_word.brk
+edit_word_he.brk
+edit_word_hu.brk
+edit_word.brk
+line.brk
+sent.brk
+''')
+
+rc = subprocess.call(['gencmn',
+                      '-n',
+                      'OpenOffice',
+                      '-t',
+                      'tmp',
+                      '-S',
+                      '-d',
+                      output_dir,
+                      '0',
+                      dattmp_name])
+
+os.unlink(dattmp_name)
+
+if rc != 0:
+    sys.exit(rc)
+
+d = open(gncmtmp_name, 'r').read()
+with open(output_name, 'w') as ofile:
+    ofile.write('''#ifdef _MSC_VER
+#pragma warning(disable : 4229)
+#endif
+''')
+    ofile.write(d)
+
+os.unlink(gncmtmp_name)
