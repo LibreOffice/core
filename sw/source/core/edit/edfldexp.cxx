8008f9c62b73f56bd37f677bd4f3960760047787
Bjoern Michaelsen <bjoern.michaelsen@libreoffice.org>
2020-03-15 01:34:00 +0100

    edfldexp.cxx: SwIterator no more ...
    Change-Id: I3b7aba9c9020e253116940dbe5cd75a047b2e2e0

--- sw/source/core/edit/edfldexp.cxx
+++ sw/source/core/edit/edfldexp.cxx
@@ -34,30 +34,23 @@ bool SwEditShell::IsFieldDataSourceAvailable(OUString& rUsedDataSource) const
     const SwFieldTypes * pFieldTypes = GetDoc()->getIDocumentFieldsAccess().GetFieldTypes();
     uno::Reference<uno::XComponentContext> xContext( ::comphelper::getProcessComponentContext() );
     uno::Reference<sdb::XDatabaseContext> xDBContext = sdb::DatabaseContext::create(xContext);
-    for(const auto & pFieldType : *pFieldTypes)
+    std::vector<SwFormatField*> vFields;
+    for(const auto& pFieldType : *pFieldTypes)
     {
         if(IsUsed(*pFieldType) && pFieldType->Which() == SwFieldIds::Database)
+            pFieldType->GatherFields(vFields);
+    }
+    for(auto pFormatField: vFields)
+    {
+        const SwDBData& rData = static_cast<SwDBFieldType*>(pFormatField->GetField()->GetTyp())->GetDBData();
+        try
         {
-            SwIterator<SwFormatField,SwFieldType> aIter( *pFieldType );
-            SwFormatField* pFormatField = aIter.First();
-            while(pFormatField)
-            {
-                if(pFormatField->IsFieldInDoc())
-                {
-                    const SwDBData& rData =
-                            static_cast<SwDBFieldType*>(pFormatField->GetField()->GetTyp())->GetDBData();
-                    try
-                    {
-                        return xDBContext->getByName(rData.sDataSource).hasValue();
-                    }
-                    catch(uno::Exception const &)
-                    {
-                        rUsedDataSource = rData.sDataSource;
-                        return false;
-                    }
-                }
-                pFormatField = aIter.Next();
-            }
+            return xDBContext->getByName(rData.sDataSource).hasValue();
+        }
+        catch(uno::Exception const &)
+        {
+            rUsedDataSource = rData.sDataSource;
+            return false;
         }
     }
     return true;
