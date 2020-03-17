319c54cac87b3576f762be459d8922cdf091bb2a
Justin Luth <justin_luth@sil.org>
2020-03-17 21:18:41 +0300

    tdf#130746: avoid nullptr dereference
    Change-Id: I32979a23caffea844f1c8eac9f408b18f0768b62
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/90661
    Tested-by: Jenkins
    Reviewed-by: Justin Luth <justin_luth@sil.org>
    (cherry picked from commit 81588ff2f0eb55576a5288778be2dfb5b4bc5e81)

--- sw/source/core/layout/frmtool.cxx
+++ sw/source/core/layout/frmtool.cxx
@@ -3434,9 +3434,9 @@ bool IsFrameInSameContext( const SwFrame *pInnerFrame, const SwFrame *pFrame )
 
 static SwTwips lcl_CalcCellRstHeight( SwLayoutFrame *pCell )
 {
-    if ( pCell->Lower()->IsContentFrame() || pCell->Lower()->IsSctFrame() )
+    SwFrame *pLow = pCell->Lower();
+    if ( pLow && (pLow->IsContentFrame() || pLow->IsSctFrame()) )
     {
-        SwFrame *pLow = pCell->Lower();
         long nHeight = 0, nFlyAdd = 0;
         do
         {
@@ -3463,7 +3463,6 @@ static SwTwips lcl_CalcCellRstHeight( SwLayoutFrame *pCell )
     else
     {
         long nRstHeight = 0;
-        SwFrame *pLow = pCell->Lower();
         while (pLow && pLow->IsLayoutFrame())
         {
             nRstHeight += ::CalcRowRstHeight(static_cast<SwLayoutFrame*>(pLow));
