93998a0e0a70d24e732690e8bb5a062f516eebeb
Szymon Kłos <szymon.klos@collabora.com>
2020-03-18 11:07:02 +0100

    Move SalInstanceCheckButton to header file
    Change-Id: Id6597cd259abefe69a61f0ab414c6bd2114c9749
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/93977
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>

--- vcl/inc/salvtables.hxx
+++ vcl/inc/salvtables.hxx
@@ -924,4 +924,24 @@ public:
     virtual Container* weld_message_area() override;
 };
 
+class SalInstanceCheckButton : public SalInstanceButton, public virtual weld::CheckButton
+{
+private:
+    VclPtr<CheckBox> m_xCheckButton;
+
+    DECL_LINK(ToggleHdl, CheckBox&, void);
+public:
+    SalInstanceCheckButton(CheckBox* pButton, SalInstanceBuilder* pBuilder, bool bTakeOwnership);
+
+    virtual void set_active(bool active) override;
+
+    virtual bool get_active() const override;
+
+    virtual void set_inconsistent(bool inconsistent) override;
+
+    virtual bool get_inconsistent() const override;
+
+    virtual ~SalInstanceCheckButton() override;
+};
+
 #endif
