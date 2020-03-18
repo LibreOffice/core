LibreOffice/core

commit 2ab481b038b62b1ff576ac4d49d03c1798cd7f84

tdf#90069 DOCX: fix character style of new table rows
DOCX table import didn't set paragraph level
character styles on paragraph level, only on
text portions, resulting default character style
in the newly inserted table rows instead of copying
the style of the previous table row.

Change-Id: Idb4438c767bdc7e0026fc6e0f0a795d8efdda3c8
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/86429
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>
Tested-by: László Németh <nemeth@numbertext.org>

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -1771,6 +1771,19 @@ void DomainMapper_Impl::finishParagraph( const PropertyMapPtr& pPropertyMap, con
                         }
                     }
                 }
+
+                // tdf#90069 in tables, apply paragraph level character style also on
+                // paragraph level to support its copy during insertion of new table rows
+                if ( xParaProps && m_nTableDepth > 0 )
+                {
+                    uno::Sequence< beans::PropertyValue > aValues = pParaContext->GetPropertyValues(false);
+
+                    for( const auto& rProp : std::as_const(aValues) )
+                    {
+                        if ( rProp.Name.startsWith("Char") && rProp.Name != "CharStyleName" && rProp.Name != "CharInteropGrabBag" )
+                            xParaProps->setPropertyValue( rProp.Name, rProp.Value );
+                    }
+                }
             }
             if( !bKeepLastParagraphProperties )
                 rAppendContext.pLastParagraphProperties = pToBeSavedProperties;

sw/qa/extras/uiwriter/uiwriter2.cxx
@@ -49,6 +49,7 @@
 #include <postithelper.hxx>
 #include <fmtcntnt.hxx>
 #include <shellio.hxx>
+#include <editeng/fontitem.hxx>
 
 namespace
 {
@@ -2419,6 +2420,35 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest2, testOfz18563)
     OUString sURL(m_directories.getURLFromSrc("/sw/qa/extras/uiwriter/data2/ofz18563.docx"));
     SvFileStream aFileStream(sURL, StreamMode::READ);
     TestImportDOCX(aFileStream);
+}
+
+CPPUNIT_TEST_FIXTURE(SwUiWriterTest2, testTdf90069)
+{
+    SwDoc* pDoc = createDoc("tdf90069.docx");
+
+    SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument*>(mxComponent.get());
+    CPPUNIT_ASSERT(pTextDoc);
+
+    SwDocShell* pDocShell = pTextDoc->GetDocShell();
+    CPPUNIT_ASSERT(pDocShell);
+
+    SwWrtShell* pWrtShell = pDocShell->GetWrtShell();
+    CPPUNIT_ASSERT(pWrtShell);
+
+    sal_uLong nIndex = pWrtShell->GetCursor()->GetNode().GetIndex();
+
+    lcl_dispatchCommand(mxComponent, ".uno:InsertRowsAfter", {});
+    pWrtShell->Down(false);
+    pWrtShell->Insert("foo");
+
+    SwTextNode* pTextNodeA1 = static_cast<SwTextNode*>(pDoc->GetNodes()[nIndex]);
+    CPPUNIT_ASSERT(pTextNodeA1->GetText().startsWith("Insert"));
+    nIndex = pWrtShell->GetCursor()->GetNode().GetIndex();
+    SwTextNode* pTextNodeA2 = static_cast<SwTextNode*>(pDoc->GetNodes()[nIndex]);
+    CPPUNIT_ASSERT_EQUAL(OUString("foo"), pTextNodeA2->GetText());
+    CPPUNIT_ASSERT_EQUAL(true, pTextNodeA2->GetSwAttrSet().HasItem(RES_CHRATR_FONT));
+    OUString sFontName = pTextNodeA2->GetSwAttrSet().GetItem(RES_CHRATR_FONT)->GetFamilyName();
+    CPPUNIT_ASSERT_EQUAL(OUString("Lohit Devanagari"), sFontName);
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

commit 31dd0c9abcee8efc4373f8e75610257b3d9ad3c9

tdf#105215 DOCX import: fix paragraph-length direct formatting
in table cells.

Paragraph-level character settings (w:p/w:pPr/w:rPr) overwrote
run settings (w:r/w:rPr), if the run was applied on the whole
paragraph.

Note: in this case direct character formatting overwrites the
paragraph-level formatting also in a new table row inserted
below the actual row, as in MSO.

Regression from commit 2ab481b038b62b1ff576ac4d49d03c1798cd7f84
(tdf#90069 DOCX: fix character style of new table rows)

See also commit 0045b6f36e5e1445d699f87a0f6597d665b4cfe4
(tdf#130690: DOCX import: fix lost text formatting in cells)

Change-Id: Ice47b93e73aed28b09334eda2f26283c68bd0bd4

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -1866,7 +1866,8 @@ void DomainMapper_Impl::finishParagraph( const PropertyMapPtr& pPropertyMap, con
                             // all text portions contain the same value, so next setPropertyValue() won't overwrite part of them
                             xRunProperties->getPropertyState(rParaProp.Name) == css::beans::PropertyState_DIRECT_VALUE )
                         {
-                            xParaProps->setPropertyValue( rParaProp.Name, rParaProp.Value );
+                            uno::Reference<beans::XPropertySet> xRunPropertySet(xCur, uno::UNO_QUERY);
+                            xParaProps->setPropertyValue( rParaProp.Name, xRunPropertySet->getPropertyValue(rParaProp.Name) );
                         }
                     }
 