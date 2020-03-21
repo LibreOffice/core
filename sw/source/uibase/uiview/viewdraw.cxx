04bea205b63e964f8b3817564e54840bc8912f3e
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2020-03-21 11:48:40 -0400

    sw: check for null SdrObject before accessing
    Change-Id: I30f1cc658450982232feea10dbe9c5bfefe07d91
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/90896
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Ashod Nakashian <ashnakash@gmail.com>
    (cherry picked from commit 8ed4ac6152c96280616a784b47c4f75df147501a)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/90946
    Tested-by: Jenkins

--- sw/source/uibase/uiview/viewdraw.cxx
+++ sw/source/uibase/uiview/viewdraw.cxx
@@ -658,6 +658,8 @@ bool SwView::AreOnlyFormsSelected() const
         {
             // Except controls, are still normal draw objects selected?
             SdrObject *pSdrObj = rMarkList.GetMark(i)->GetMarkedSdrObj();
+            if (!pSdrObj)
+                continue;
 
             if (!HasOnlyObj(pSdrObj, SdrInventor::FmForm))
             {
