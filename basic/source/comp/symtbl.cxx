ce21b3cbfd9684e4b3448c50f9a2a0d1bc2e2328
Andreas Heinisch <andreas.heinisch@yahoo.de>
2020-03-21 14:59:27 +0100

    tdf#131296: get numeric value with its data type in StepLOADNC
    In order to load numeric values, generate SbiOpcode::NUMBER_ opcodes
    including the numeric value and its data type instead of SbiOpcode::CONST_.
    
    The numeric value and its data type will be restored in
    SbiRuntime::StepLOADNC. When the new compiled code is stored in documents,
    e.g. password-protected libraries, leagcy LO versions will just read up to
    non-numeric characters, thus correctly obtaining number value and ignoring
    the type, so the change is backward-compatible.
    
    To interpret legacy compiled code, old treatment of SbiRuntime::StepLOADI
    is restored, reverting commit 0b4f8bf571baf2ccd5a8aafdc4deb41867420be3.
    This change reimplements the fix for tdf#129596.
    
    Change-Id: I46ebfc77f9bea69479968950c0fb7264e4a7ab27

--- basic/source/comp/symtbl.cxx
+++ basic/source/comp/symtbl.cxx
@@ -66,9 +66,11 @@ short SbiStringPool::Add( double n, SbxDataType t )
     char buf[ 40 ];
     switch( t )
     {
-        case SbxINTEGER: snprintf( buf, sizeof(buf), "%d", static_cast<short>(n) ); break;
-        case SbxLONG:    snprintf( buf, sizeof(buf), "%ld", static_cast<long>(n) ); break;
-        case SbxSINGLE:  snprintf( buf, sizeof(buf), "%.6g", static_cast<float>(n) ); break;
+        // tdf#131296 - store numeric value including its type character
+        // See GetSuffixType in basic/source/comp/scanner.cxx for type characters
+        case SbxINTEGER: snprintf( buf, sizeof(buf), "%d%%", static_cast<short>(n) ); break;
+        case SbxLONG:    snprintf( buf, sizeof(buf), "%ld&", static_cast<long>(n) ); break;
+        case SbxSINGLE:  snprintf( buf, sizeof(buf), "%.6g!", static_cast<float>(n) ); break;
         case SbxDOUBLE:  snprintf( buf, sizeof(buf), "%.16g", n ); break;
         default: break;
     }
