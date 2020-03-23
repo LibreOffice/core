LibreOffice/core

commit fd7749fddc5a767461dfced55369af48e5a6d561

sw: fix handling of table vs fly overlaps in the AddVerticalFlyOffsets case
When a table overlaps with a fly frame, Writer creates a fly portion
inside the relevant cell frame (increasing its height), and Word shifts
the table down. Both are valid approaches, but the rendering result is
different in case the table has a border.

So keep the default unchanged, but in case the AddVerticalFlyOffsets
compat flag (set by the Word importers) is set, avoid the overlap the
Word way.

Note that the table frame uses the full width (available in the body)
even for e.g. 50% width tables, so check for the overlap using the print
area, which does not always overlap.

Finally, don't always require a valid frame area definition from the fly
frame:

- the mentioned i#46807 bugdoc currently doesn't need that check

- the fly frame area definition becomes valid only after already
  positioning the table, leading to an overlap

Keep that check for the non-compat case, though.

Change-Id: I9202050befebf2efdbb5395ded6fcb52b378d8e7
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/88724
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

sw/source/core/layout/tabfrm.cxx
@@ -35,6 +35,7 @@
 #include <editeng/ulspitem.hxx>
 #include <editeng/brushitem.hxx>
 #include <editeng/boxitem.hxx>
+#include <basegfx/range/b1drange.hxx>
 #include <fmtlsplt.hxx>
 #include <fmtrowsplt.hxx>
 #include <fmtsrnd.hxx>
@@ -2696,6 +2697,9 @@ bool SwTabFrame::CalcFlyOffsets( SwTwips& rUpper,
         long nYDiff = aRectFnSet.YDiff( aRectFnSet.GetTop(getFramePrintArea()), rUpper );
         if( nYDiff > 0 )
             aRectFnSet.AddBottom( aRect, -nYDiff );
+
+        bool bAddVerticalFlyOffsets = rIDSA.get(DocumentSettingId::ADD_VERTICAL_FLY_OFFSETS);
+
         for ( size_t i = 0; i < pPage->GetSortedObjs()->size(); ++i )
         {
             SwAnchoredObject* pAnchoredObj = (*pPage->GetSortedObjs())[i];
@@ -2723,7 +2727,7 @@ bool SwTabFrame::CalcFlyOffsets( SwTwips& rUpper,
                 bool bConsiderFly =
                     // #i46807# - do not consider invalid
                     // Writer fly frames.
-                    pFly->isFrameAreaDefinitionValid() &&
+                    (pFly->isFrameAreaDefinitionValid() || bAddVerticalFlyOffsets) &&
                     // fly anchored at character
                     pFly->IsFlyAtContentFrame() &&
                     // fly overlaps with corresponding table rectangle
@@ -2767,7 +2771,29 @@ bool SwTabFrame::CalcFlyOffsets( SwTwips& rUpper,
                 {
                     const SwFormatSurround   &rSur = pFly->GetFormat()->GetSurround();
                     const SwFormatHoriOrient &rHori= pFly->GetFormat()->GetHoriOrient();
-                    if ( css::text::WrapTextMode_NONE == rSur.GetSurround() )
+                    bool bShiftDown = css::text::WrapTextMode_NONE == rSur.GetSurround();
+                    if (!bShiftDown && bAddVerticalFlyOffsets)
+                    {
+                        if (rSur.GetSurround() == text::WrapTextMode_PARALLEL
+                            && rHori.GetHoriOrient() == text::HoriOrientation::NONE)
+                        {
+                            // We know that wrapping was requested and the table frame overlaps with
+                            // the fly frame. Check if the print area overlaps with the fly frame as
+                            // well (in case the table does not use all the available width).
+                            basegfx::B1DRange aTabRange(
+                                aRectFnSet.GetLeft(aRect) + aRectFnSet.GetLeft(getFramePrintArea()),
+                                aRectFnSet.GetLeft(aRect) + aRectFnSet.GetLeft(getFramePrintArea())
+                                    + aRectFnSet.GetWidth(getFramePrintArea()));
+                            basegfx::B1DRange aFlyRange(aRectFnSet.GetLeft(aFlyRect),
+                                                        aRectFnSet.GetRight(aFlyRect));
+                            // If it does, shift the table down. Do this only in the compat case,
+                            // normally an SwFlyPortion is created instead that increases the height
+                            // of the first table row.
+                            bShiftDown = aTabRange.overlaps(aFlyRange);
+                        }
+                    }
+
+                    if (bShiftDown)
                     {
                         long nBottom = aRectFnSet.GetBottom(aFlyRect);
                         if( aRectFnSet.YDiff( nPrtPos, nBottom ) < 0 )

sw/qa/core/layout/layout.cxx
@@ -1,0 +1,48 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <swmodeltestbase.hxx>
+
+#include <vcl/gdimtf.hxx>
+#include <comphelper/classids.hxx>
+#include <tools/globname.hxx>
+#include <svtools/embedhlp.hxx>
+
+#include <wrtsh.hxx>
+#include <fmtanchr.hxx>
+
+static char const DATA_DIRECTORY[] = "/sw/qa/core/layout/data/";
+
+/// Covers sw/source/core/layout/ fixes.
+class SwCoreLayoutTest : public SwModelTestBase
+{
+};
+
+CPPUNIT_TEST_FIXTURE(SwCoreLayoutTest, testTableFlyOverlap)
+{
+    // Load a document that has an image anchored in the header.
+    // It also has a table which has the wrap around the image.
+    load(DATA_DIRECTORY, "table-fly-overlap.docx");
+    SwTwips nFlyTop = parseDump("//header/txt/anchored/fly/infos/bounds", "top").toInt32();
+    SwTwips nFlyHeight = parseDump("//header/txt/anchored/fly/infos/bounds", "height").toInt32();
+    SwTwips nFlyBottom = nFlyTop + nFlyHeight;
+    SwTwips nTableFrameTop = parseDump("//tab/infos/bounds", "top").toInt32();
+    SwTwips nTablePrintTop = parseDump("//tab/infos/prtBounds", "top").toInt32();
+    SwTwips nTableTop = nTableFrameTop + nTablePrintTop;
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected greater or equal than: 3579
+    // - Actual  : 2210
+    // i.e. the table's top border overlapped with the image, even if the image's wrap mode was set
+    // to parallel.
+    CPPUNIT_ASSERT_GREATEREQUAL(nFlyBottom, nTableTop);
+}
+
+CPPUNIT_PLUGIN_IMPLEMENT();
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sw/CppunitTest_sw_core_layout.mk
@@ -1,0 +1,72 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#*************************************************************************
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+#*************************************************************************
+
+$(eval $(call gb_CppunitTest_CppunitTest,sw_core_layout))
+
+$(eval $(call gb_CppunitTest_use_common_precompiled_header,sw_core_layout))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,sw_core_layout, \
+    sw/qa/core/layout/layout \
+))
+
+$(eval $(call gb_CppunitTest_use_libraries,sw_core_layout, \
+    comphelper \
+    cppu \
+    cppuhelper \
+    sal \
+    sfx \
+    sw \
+    test \
+    unotest \
+    utl \
+    vcl \
+    svt \
+    tl \
+    svl \
+))
+
+$(eval $(call gb_CppunitTest_use_externals,sw_core_layout,\
+    boost_headers \
+    libxml2 \
+))
+
+$(eval $(call gb_CppunitTest_set_include,sw_core_layout,\
+    -I$(SRCDIR)/sw/inc \
+    -I$(SRCDIR)/sw/source/core/inc \
+    -I$(SRCDIR)/sw/source/uibase/inc \
+    -I$(SRCDIR)/sw/qa/inc \
+    $$(INCLUDE) \
+))
+
+$(eval $(call gb_CppunitTest_use_api,sw_core_layout,\
+	udkapi \
+	offapi \
+	oovbaapi \
+))
+
+$(eval $(call gb_CppunitTest_use_ure,sw_core_layout))
+$(eval $(call gb_CppunitTest_use_vcl,sw_core_layout))
+
+$(eval $(call gb_CppunitTest_use_rdb,sw_core_layout,services))
+
+$(eval $(call gb_CppunitTest_use_custom_headers,sw_core_layout,\
+    officecfg/registry \
+))
+
+$(eval $(call gb_CppunitTest_use_configuration,sw_core_layout))
+
+$(eval $(call gb_CppunitTest_use_uiconfigs,sw_core_layout, \
+    modules/swriter \
+))
+
+$(eval $(call gb_CppunitTest_use_more_fonts,sw_core_layout))
+
+# vim: set noet sw=4 ts=4:

sw/Module_sw.mk
@@ -108,6 +108,7 @@ $(eval $(call gb_Module_add_slowcheck_targets,sw,\
     CppunitTest_sw_core_doc \
     CppunitTest_sw_uibase_shells \
     CppunitTest_sw_core_accessibilitycheck \
+    CppunitTest_sw_core_layout \
 ))
 
 ifneq ($(DISABLE_GUI),TRUE)

commit 87e71ef8b4f8f99295a46b5e69b08674d6f4e79d

tdf#131282 sw layout: fix shifted down table next to a fly frame
Regression from commit fd7749fddc5a767461dfced55369af48e5a6d561 (sw: fix
handling of table vs fly overlaps in the AddVerticalFlyOffsets case,
2020-02-14), the problem was that the rectangle of the fly frame
included spacing when overlapping was checked.

Given that this code was explicitly added for Word compat purposes,
change it to ignore spacing. You can see that Word doesn't care about
spacing: increase the left margin of e.g. the pie chart in the bugdoc to
some large value and the table is still not shifted down.

Writer shifted the table down as the small spacing was already enough to
detect an overlap:

debug:21457:21457: SwTabFrame::CalcFlyOffsets: aTabRange is 896 -> 3698
debug:21457:21457: SwTabFrame::CalcFlyOffsets: aFlyRange is 3650 -> 6290

If we don't include that ~3 px spacing, then we don't overlap, similar
to Word.

Change-Id: I154c211bb0700d132fd168f49c1bbfb29e8caeb7

sw/source/core/layout/tabfrm.cxx
@@ -2806,8 +2806,13 @@ bool SwTabFrame::CalcFlyOffsets( SwTwips& rUpper,
                                 aRectFnSet.GetLeft(aRect) + aRectFnSet.GetLeft(getFramePrintArea()),
                                 aRectFnSet.GetLeft(aRect) + aRectFnSet.GetLeft(getFramePrintArea())
                                     + aRectFnSet.GetWidth(getFramePrintArea()));
-                            basegfx::B1DRange aFlyRange(aRectFnSet.GetLeft(aFlyRect),
-                                                        aRectFnSet.GetRight(aFlyRect));
+
+                            // Ignore spacing when determining the left/right edge of the fly, like
+                            // Word does.
+                            const SwRect aFlyRectWithoutSpaces = pFly->GetObjRect();
+                            basegfx::B1DRange aFlyRange(aRectFnSet.GetLeft(aFlyRectWithoutSpaces),
+                                                        aRectFnSet.GetRight(aFlyRectWithoutSpaces));
+
                             // If it does, shift the table down. Do this only in the compat case,
                             // normally an SwFlyPortion is created instead that increases the height
                             // of the first table row.

sw/qa/core/layout/layout.cxx
@@ -75,6 +75,23 @@ CPPUNIT_TEST_FIXTURE(SwCoreLayoutTest, testBtlrTableRowSpan)
     assertXPathContent(pXmlDoc, "//textarray[1]/text", "USA");
 }
 
+CPPUNIT_TEST_FIXTURE(SwCoreLayoutTest, testTableFlyOverlapSpacing)
+{
+    // Load a document that has an image on the right of a table.  The table wraps around the image.
+    load(DATA_DIRECTORY, "table-fly-overlap-spacing.docx");
+    SwTwips nFlyTop = parseDump("//body/txt/anchored/fly/infos/bounds", "top").toInt32();
+    SwTwips nFlyHeight = parseDump("//body/txt/anchored/fly/infos/bounds", "height").toInt32();
+    SwTwips nFlyBottom = nFlyTop + nFlyHeight;
+    SwTwips nTableFrameTop = parseDump("//tab/infos/bounds", "top").toInt32();
+    SwTwips nTablePrintTop = parseDump("//tab/infos/prtBounds", "top").toInt32();
+    SwTwips nTableTop = nTableFrameTop + nTablePrintTop;
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected greater or equal than: 3993
+    // - Actual  : 3993
+    // i.e. the table was below the image, not on the left of the image.
+    CPPUNIT_ASSERT_LESS(nFlyBottom, nTableTop);
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */