19a58a53bd294530c0f873eb50bb5a3855616367
Tor Lillqvist <tml@collabora.com>
2020-03-24 09:12:07 +0200

    Simplify the LibreOfficeKit mobile phone and tablet API
    There is not need to ever change the kind of device a view is for, so
    why bother with the bool parameter to setMobilePhone() and
    setTablet(). Also, make sure just either of them is called, at most
    once, for a view.
    
    Change-Id: I9ac872f0ab4772e4a7c40c49f62b32fa7b1e47f6
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/90969
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Tor Lillqvist <tml@collabora.com>

--- include/comphelper/lok.hxx
+++ include/comphelper/lok.hxx
@@ -29,16 +29,15 @@ namespace LibreOfficeKit
 
 COMPHELPER_DLLPUBLIC void setActive(bool bActive = true);
 
-// Note that currently it is undefined behaviour to call both setMobilePhone() and setTablet(). This
-// will be remedied in the future. For now, just make sure you call just either for a view, and just
-// once, with the bool parameter as true. In the future, this will probably be changed into using an
-// enum for the kind of the view, that can be DESKTOP, MOBILEPHONE, or TABLET.
+// Call either setMobilePhone() or setTablet() for a view, and at most once. (If neither is called,
+// the view is assumed to be on a desktop browser.) In the future, this will possibly be changed
+// into using an enum for the kind of the view, that can be DESKTOP, MOBILEPHONE, or TABLET.
 
 // Tell that LOK view is on a mobile phone (regardless what its pixel resolution is, whether its form factor is "phablet" or not)
-COMPHELPER_DLLPUBLIC void setMobilePhone(int nViewId, bool bIsMobilePhone);
+COMPHELPER_DLLPUBLIC void setMobilePhone(int nViewId);
 
 // Tell that LOK view is on a tablet
-COMPHELPER_DLLPUBLIC void setTablet(int nViewId, bool bIsTablet);
+COMPHELPER_DLLPUBLIC void setTablet(int nViewId);
 
 enum class statusIndicatorCallbackType { Start, SetValue, Finish };
 
