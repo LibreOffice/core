4af18ebae9d74b43fcd114d5fa5b145586651bc2
Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
2020-03-25 14:08:14 +0100

    tdf#131280 Fix endless invalidation loop on documents with form controls
    Restores a condition which was removed in 8de98e61fbc96bf523b3dec7e1e52eb7e2d7693e
    
    Change-Id: I68a9f8a362d2ded9975e7c73e2a0533aa5ad9e94
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/91053
    Tested-by: Jenkins
    Reviewed-by: Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>

--- vcl/source/window/window2.cxx
+++ vcl/source/window/window2.cxx
@@ -1346,8 +1346,8 @@ void Window::queue_resize(StateChangedType eReason)
     if (VclPtr<vcl::Window> pParent = GetParentWithLOKNotifier())
     {
         Size aSize = GetSizePixel();
-        if (aSize.getWidth() > 0 && aSize.getHeight() > 0 &&
-            !pParent->IsInInitShow())
+        if (aSize.getWidth() > 0 && aSize.getHeight() > 0 && GetParentDialog()
+            && !pParent->IsInInitShow())
             LogicInvalidate(nullptr);
     }
 }
