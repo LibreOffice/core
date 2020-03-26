e0fd3300514e020758930f4fe30652424d0efe2e
Stephan Bergmann <sbergman@redhat.com>
2020-03-26 15:33:36 +0100

    Fix one more get_url_for_data_file
    Change-Id: I07e11cd83443f40fd520088c18647f0992ccae82
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/91139
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

--- sc/qa/uitest/calc_tests/tdf120161.py
+++ sc/qa/uitest/calc_tests/tdf120161.py
@@ -6,13 +6,14 @@
 #
 from uitest.framework import UITestCase
 from libreoffice.uno.propertyvalue import mkPropertyValues
-from uitest.path import get_srcdir_url
+import org.libreoffice.unotest
 import os
+import pathlib
 from tempfile import TemporaryDirectory
 
 
 def get_url_for_data_file(file_name):
-    return get_srcdir_url() + "/sc/qa/uitest/calc_tests/data/" + file_name
+    return pathlib.Path(org.libreoffice.unotest.makeCopyFromTDOC(file_name)).as_uri()
 
 
 # Bug 120161: PRINTING, PDF Export: Problem with selected cells which cross pages
