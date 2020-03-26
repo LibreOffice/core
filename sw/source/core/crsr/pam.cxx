b8a807868cae690e6abd19a8eb0e74daf4bdc548
Michael Stahl <Michael.Stahl@cib.de>
2020-03-26 15:24:37 +0100

    sw: PROTECT_FIELDS should not protect placeholder fields
    The text placeholder field becomes totally nonfunctional, the other
    placeholder fields allow inserting but are then not deleted; just ignore
    protection for placeholder fields.
    
    Change-Id: Ic478a32d616464bfe7f24fc851c1998b5e01a23b
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/91140
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@cib.de>
    (cherry picked from commit 3fc68fdc6f4ef04091db2fd46499a10396c3a4f2)

--- sw/source/core/crsr/pam.cxx
+++ sw/source/core/crsr/pam.cxx
@@ -751,7 +751,9 @@ bool SwPaM::HasReadonlySel( bool bFormView ) const
                         {
                             break; // after selection
                         }
-                        if (pHint->Which() == RES_TXTATR_FIELD)
+                        if (pHint->Which() == RES_TXTATR_FIELD
+                            // placeholders don't work if you can't delete them
+                            && pHint->GetFormatField().GetField()->GetTyp()->Which() != SwFieldIds::JumpEdit)
                         {
                             return true;
                         }
