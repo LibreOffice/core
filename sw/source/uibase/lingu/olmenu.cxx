190e7833777779a00b1ba82d4a48e95e47449ce2
Tamás Zolnai <tamas.zolnai@collabora.com>
2020-03-27 13:58:52 +0100

    sw lok: add uno command for submenus in Spellchecking popup.
    Change-Id: I1c7591379ecbce0099ed7d969e5dc83831e322b6
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/91227
    Tested-by: Jenkins
    Reviewed-by: Tamás Zolnai <tamas.zolnai@collabora.com>

--- sw/source/uibase/lingu/olmenu.cxx
+++ sw/source/uibase/lingu/olmenu.cxx
@@ -595,6 +595,7 @@ void SwSpellPopup::InitItemCommands(const css::uno::Sequence< OUString >& aSugge
         }
 
         PopupMenu *pMenu = m_xPopupMenu->GetPopupMenu(m_nLangSelectionMenuId);
+        m_xPopupMenu->SetItemCommand(m_nLangSelectionMenuId, ".uno:SetSelectionLanguageMenu");
         if(pMenu)
         {
             for (const auto& item : m_aLangTable_Text)
@@ -609,6 +610,7 @@ void SwSpellPopup::InitItemCommands(const css::uno::Sequence< OUString >& aSugge
         }
 
         pMenu = m_xPopupMenu->GetPopupMenu(m_nLangParaMenuId);
+        m_xPopupMenu->SetItemCommand(m_nLangParaMenuId, ".uno:SetParagraphLanguageMenu");
         if(pMenu)
         {
             for (const auto& item : m_aLangTable_Paragraph)
