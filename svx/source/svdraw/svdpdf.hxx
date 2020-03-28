c798963ad3e28dc555edc2d41709b8a067085fff
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2020-03-28 18:00:22 +0100

    svdpdf: move Matrix to basegfx just to get it separated
    Change-Id: I9d887dc7a2836b90151ef352b47a9b9ad3b6f12b

--- svx/source/svdraw/svdpdf.hxx
+++ svx/source/svdraw/svdpdf.hxx
@@ -34,6 +34,8 @@
 #include <svx/svdobj.hxx>
 #include <svx/xdash.hxx>
 
+#include <basegfx/matrix/Matrix.hxx>
+
 // Prevent workdir/UnpackedTarball/pdfium/public/fpdfview.h from including windows.h in a way that
 // it will define e.g. Yield as a macro:
 #include <prewin.h>
@@ -52,110 +54,6 @@ class SvdProgressInfo;
 // Helper Class to import PDF
 class ImpSdrPdfImport final
 {
-    class Matrix
-    {
-    public:
-        Matrix()
-            : Matrix(1, 0, 0, 1, 0, 0)
-        {
-        }
-
-        Matrix(const Matrix& other)
-            : Matrix(other.ma, other.mb, other.mc, other.md, other.me, other.mf)
-        {
-        }
-
-        Matrix(double da, double db, double dc, double dd, double de, double df)
-            : ma(da)
-            , mb(db)
-            , mc(dc)
-            , md(dd)
-            , me(de)
-            , mf(df)
-        {
-        }
-
-        const Matrix& operator=(const Matrix& other)
-        {
-            ma = other.ma;
-            mb = other.mb;
-            mc = other.mc;
-            md = other.md;
-            me = other.me;
-            mf = other.mf;
-            return *this;
-        }
-
-        double a() const { return ma; }
-        double b() const { return mb; }
-        double c() const { return mc; }
-        double d() const { return md; }
-        double e() const { return me; }
-        double f() const { return mf; }
-
-        /// Multiply this * other.
-        void Concatinate(const Matrix& other)
-        {
-            ma = ma * other.ma + mb * other.mc;
-            mb = ma * other.mb + mb * other.md;
-            mc = mc * other.ma + md * other.mc;
-            md = mc * other.mb + md * other.md;
-            me = me * other.ma + mf * other.mc + other.me;
-            mf = me * other.mb + mf * other.md + other.mf;
-        }
-
-        /// Transform the point (x, y) by this Matrix.
-        template <typename T> void Transform(T& x, T& y)
-        {
-            x = ma * x + mc * y + me;
-            y = mb * x + md * y + mf;
-        }
-
-        /// Transform the rectangle (left, right, top, bottom) by this Matrix.
-        template <typename T> void Transform(T& left, T& right, T& top, T& bottom)
-        {
-            T leftTopX = left;
-            T leftTopY = top;
-            Transform(leftTopX, leftTopY);
-
-            T leftBottomX = left;
-            T leftBottomY = bottom;
-            Transform(leftBottomX, leftBottomY);
-
-            T rightTopX = right;
-            T rightTopY = top;
-            Transform(rightTopX, rightTopY);
-
-            T rightBottomX = right;
-            T rightBottomY = bottom;
-            Transform(rightBottomX, rightBottomY);
-
-            left = std::min(leftTopX, leftBottomX);
-            right = std::max(rightTopX, rightBottomX);
-
-            if (top > bottom)
-                top = std::max(leftTopY, rightTopY);
-            else
-                top = std::min(leftTopY, rightTopY);
-
-            if (top > bottom)
-                bottom = std::max(leftBottomY, rightBottomY);
-            else
-                bottom = std::min(leftBottomY, rightBottomY);
-        }
-
-        std::string toString() const
-        {
-            std::ostringstream oss;
-            oss << '(' << ma << ", " << mb << ", " << mc << ", " << md << ", " << me << ", " << mf
-                << ')';
-            return oss.str();
-        }
-
-    private:
-        double ma, mb, mc, md, me, mf;
-    };
-
     Graphic const& mrGraphic;
     std::vector<SdrObject*> maTmpList;
     ScopedVclPtr<VirtualDevice> mpVD;
