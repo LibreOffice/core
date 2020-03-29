LibreOffice/core

commit 5352d45dd4a04f8f02cf7f6ad4169126d3b3586a

convert AnimationImport to fast-parser APIs
And...

(*) space out the namespace constant values so I dont
keep forgetting and making them overlap.

(*) Remove CreateDocumentContext from SvXMLImport since it is
now unused.

Change-Id: I30f54bfc1389e91b18e4fee8b83e1b297419899b
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/88938
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

include/xmloff/xmltoken.hxx
@@ -2497,6 +2497,7 @@ namespace xmloff { namespace token {
         XML_N_DRAW_OOO,
         XML_N_DR3D_OOO,
         XML_N_PRESENTATION_OOO,
+        XML_N_PRESENTATION_OASIS,
         XML_N_CHART_OOO,
         XML_N_CONFIG_OOO,
         XML_N_FORM_OOO,
@@ -2788,6 +2789,7 @@ namespace xmloff { namespace token {
 
         XML_NP_ANIMATION,
         XML_N_ANIMATION,
+        XML_N_ANIMATION_OOO,
 
         XML_PAR,
         XML_SEQ,

xmloff/source/core/xmltoken.cxx
@@ -2499,6 +2499,7 @@ namespace xmloff::token {
         TOKEN( "http://openoffice.org/2000/drawing",    XML_N_DRAW_OOO ),
         TOKEN( "http://openoffice.org/2000/dr3d",       XML_N_DR3D_OOO ),
         TOKEN( "http://openoffice.org/2000/presentation",XML_N_PRESENTATION_OOO ),
+        TOKEN( "urn:oasis:names:tc:openoffice:xmlns:presentation:1.0",XML_N_PRESENTATION_OASIS ),
         TOKEN( "http://openoffice.org/2000/chart",      XML_N_CHART_OOO ),
         TOKEN( "http://openoffice.org/2001/config",     XML_N_CONFIG_OOO ),
         TOKEN( "http://openoffice.org/2000/form",       XML_N_FORM_OOO ),
@@ -2787,6 +2788,7 @@ namespace xmloff::token {
 
         TOKEN( "anim",                      XML_NP_ANIMATION ),
         TOKEN( "urn:oasis:names:tc:opendocument:xmlns:animation:1.0",  XML_N_ANIMATION ),
+        TOKEN( "urn:oasis:names:tc:openoffice:xmlns:animation:1.0",  XML_N_ANIMATION_OOO ),
 
         TOKEN( "par",                           XML_PAR ),
         TOKEN( "seq",                           XML_SEQ ),

include/xmloff/xmlimp.hxx
@@ -244,9 +244,6 @@ protected:
     // This method is called after the namespace map has been updated, but
     // before a context for the current element has been pushed.
     // This base class implementation returns a context that ignores everything.
-    virtual SvXMLImportContext *CreateDocumentContext(sal_uInt16 nPrefix,
-                                               const OUString& rLocalName,
-                                               const css::uno::Reference< css::xml::sax::XAttributeList >& xAttrList );
     virtual SvXMLImportContext *CreateFastContext( sal_Int32 Element,
         const ::css::uno::Reference< ::css::xml::sax::XFastAttributeList >& xAttrList );
 

include/xmloff/xmlnmspe.hxx
@@ -65,7 +65,7 @@ constexpr sal_uInt16 XML_NAMESPACE_GRDDL =           36;
 constexpr sal_uInt16 XML_NAMESPACE_VERSIONS_LIST =   37;
 
 // namespaces for odf extended formats
-constexpr sal_uInt16 XML_NAMESPACE_EXT_BASE   = 38;
+constexpr sal_uInt16 XML_NAMESPACE_EXT_BASE   = 50;
 constexpr sal_uInt16 XML_NAMESPACE_OFFICE_EXT = XML_NAMESPACE_EXT_BASE + 0;
 constexpr sal_uInt16 XML_NAMESPACE_TABLE_EXT  = XML_NAMESPACE_EXT_BASE + 1;
 constexpr sal_uInt16 XML_NAMESPACE_CHART_EXT  = XML_NAMESPACE_EXT_BASE + 2;
@@ -74,7 +74,7 @@ constexpr sal_uInt16 XML_NAMESPACE_CALC_EXT   = XML_NAMESPACE_EXT_BASE + 4;
 constexpr sal_uInt16 XML_NAMESPACE_LO_EXT     = XML_NAMESPACE_EXT_BASE + 5;
 
 // namespaces for OOo formats
-constexpr sal_uInt16 XML_NAMESPACE_OOO_BASE = 44U;
+constexpr sal_uInt16 XML_NAMESPACE_OOO_BASE = 60;
 constexpr sal_uInt16 XML_NAMESPACE_OFFICE_OOO = XML_NAMESPACE_OOO_BASE +         0;
 constexpr sal_uInt16 XML_NAMESPACE_META_OOO = XML_NAMESPACE_OOO_BASE +           1;
 constexpr sal_uInt16 XML_NAMESPACE_STYLE_OOO = XML_NAMESPACE_OOO_BASE +          2;
@@ -84,22 +84,24 @@ constexpr sal_uInt16 XML_NAMESPACE_TABLE_OOO = XML_NAMESPACE_OOO_BASE +         
 constexpr sal_uInt16 XML_NAMESPACE_DRAW_OOO = XML_NAMESPACE_OOO_BASE +           6;
 constexpr sal_uInt16 XML_NAMESPACE_DR3D_OOO = XML_NAMESPACE_OOO_BASE +           7;
 constexpr sal_uInt16 XML_NAMESPACE_PRESENTATION_OOO = XML_NAMESPACE_OOO_BASE +   8;
-constexpr sal_uInt16 XML_NAMESPACE_CHART_OOO = XML_NAMESPACE_OOO_BASE +          9;
-constexpr sal_uInt16 XML_NAMESPACE_CONFIG_OOO = XML_NAMESPACE_OOO_BASE +        10;
-constexpr sal_uInt16 XML_NAMESPACE_FORM_OOO = XML_NAMESPACE_OOO_BASE +          11;
-constexpr sal_uInt16 XML_NAMESPACE_SCRIPT_OOO = XML_NAMESPACE_OOO_BASE +        12;
+constexpr sal_uInt16 XML_NAMESPACE_PRESENTATION_OASIS = XML_NAMESPACE_OOO_BASE + 9; // only used for some config files in sd/
+constexpr sal_uInt16 XML_NAMESPACE_CHART_OOO = XML_NAMESPACE_OOO_BASE +         10;
+constexpr sal_uInt16 XML_NAMESPACE_CONFIG_OOO = XML_NAMESPACE_OOO_BASE +        12;
+constexpr sal_uInt16 XML_NAMESPACE_FORM_OOO = XML_NAMESPACE_OOO_BASE +          13;
+constexpr sal_uInt16 XML_NAMESPACE_SCRIPT_OOO = XML_NAMESPACE_OOO_BASE +        14;
+constexpr sal_uInt16 XML_NAMESPACE_ANIMATION_OOO = XML_NAMESPACE_OOO_BASE +     15;
 
-constexpr sal_uInt16 XML_NAMESPACE_COMPAT_BASE = 57;
+constexpr sal_uInt16 XML_NAMESPACE_COMPAT_BASE = 80;
 constexpr sal_uInt16 XML_NAMESPACE_SVG_COMPAT = XML_NAMESPACE_COMPAT_BASE +      0;
 constexpr sal_uInt16 XML_NAMESPACE_FO_COMPAT = XML_NAMESPACE_COMPAT_BASE +       1;
 constexpr sal_uInt16 XML_NAMESPACE_SMIL_COMPAT = XML_NAMESPACE_COMPAT_BASE +     2;
 
-constexpr sal_uInt16 XML_NAMESPACE_OASIS_BASE = 60;
+constexpr sal_uInt16 XML_NAMESPACE_OASIS_BASE = 90;
 constexpr sal_uInt16 XML_NAMESPACE_DB_OASIS = XML_NAMESPACE_OASIS_BASE +         0;
 constexpr sal_uInt16 XML_NAMESPACE_REPORT_OASIS = XML_NAMESPACE_OASIS_BASE +     1;
 
 // namespaces used in the technical preview (SO 5.2)
-constexpr sal_uInt16 XML_OLD_NAMESPACE_BASE = 62;
+constexpr sal_uInt16 XML_OLD_NAMESPACE_BASE = 100;
 constexpr sal_uInt16 XML_NAMESPACE_FO_SO52 = XML_OLD_NAMESPACE_BASE +             0;
 constexpr sal_uInt16 XML_NAMESPACE_XLINK_SO52 = XML_OLD_NAMESPACE_BASE +          1;
 constexpr sal_uInt16 XML_NAMESPACE_OFFICE_SO52 = XML_OLD_NAMESPACE_BASE +         2;
@@ -114,9 +116,9 @@ constexpr sal_uInt16 XML_NAMESPACE_CHART_SO52 = XML_OLD_NAMESPACE_BASE +        
 constexpr sal_uInt16 XML_NAMESPACE_SMIL_SO52 = XML_OLD_NAMESPACE_BASE +          11;
 
 // experimental namespaces
-constexpr sal_uInt16 XML_NAMESPACE_FIELD =           100;
-constexpr sal_uInt16 XML_NAMESPACE_CSS3TEXT =        103;  // CSS Text Level 3
-constexpr sal_uInt16 XML_NAMESPACE_FORMX =           101;  // form interop extensions
+constexpr sal_uInt16 XML_NAMESPACE_FIELD =           120;
+constexpr sal_uInt16 XML_NAMESPACE_CSS3TEXT =        123;  // CSS Text Level 3
+constexpr sal_uInt16 XML_NAMESPACE_FORMX =           121;  // form interop extensions
 
 
 #endif // INCLUDED_XMLOFF_XMLNMSPE_HXX

sd/qa/unit/import-tests.cxx
@@ -801,7 +801,7 @@ void SdImportTest::testFdo64512()
 
     uno::Reference< drawing::XDrawPage > xPage(
         xDoc->getDrawPages()->getByIndex(0), uno::UNO_QUERY_THROW );
-    CPPUNIT_ASSERT_EQUAL_MESSAGE( "no exactly three shapes", static_cast<sal_Int32>(3), xPage->getCount() );
+    CPPUNIT_ASSERT_EQUAL_MESSAGE( "not exactly three shapes", static_cast<sal_Int32>(3), xPage->getCount() );
 
     uno::Reference< beans::XPropertySet > xConnectorShape(
         xPage->getByIndex(2), uno::UNO_QUERY );

test/source/xmltesttools.cxx
@@ -181,6 +181,8 @@ void XmlTestTools::assertXPath(xmlDocPtr pXmlDoc, const OString& rXPath, int nNu
 {
     xmlXPathObjectPtr pXmlObj = getXPathNode(pXmlDoc, rXPath);
     xmlNodeSetPtr pXmlNodes = pXmlObj->nodesetval;
+    if (nNumberOfNodes != xmlXPathNodeSetGetLength(pXmlNodes))
+        CPPUNIT_ASSERT_EQUAL(1, 2);
     CPPUNIT_ASSERT_EQUAL_MESSAGE(OString(OStringLiteral("In <") + pXmlDoc->name + ">, XPath '" + rXPath + "' number of nodes is incorrect").getStr(),
                                  nNumberOfNodes, xmlXPathNodeSetGetLength(pXmlNodes));
     xmlXPathFreeObject(pXmlObj);

xmloff/inc/animationimport.hxx
@@ -37,22 +37,22 @@ class AnimationNodeContext final : public SvXMLImportContext
     std::shared_ptr<AnimationsImportHelperImpl> mpHelper;
     css::uno::Reference< css::animations::XAnimationNode > mxNode;
 
-    void init_node( const css::uno::Reference< css::xml::sax::XAttributeList >& xAttrList );
+    void init_node( const css::uno::Reference< css::xml::sax::XFastAttributeList >& xAttrList );
 
 public:
 
     AnimationNodeContext(
         const css::uno::Reference< css::animations::XAnimationNode >& xParentNode,
         SvXMLImport& rImport,
-        sal_uInt16 nPrfx,
-        const OUString& rLocalName,
-        const css::uno::Reference< css::xml::sax::XAttributeList>& xAttrList,
+        sal_Int32 nElement,
+        const css::uno::Reference< css::xml::sax::XFastAttributeList>& xAttrList,
         const std::shared_ptr<AnimationsImportHelperImpl>& pImpl = nullptr );
 
-    virtual void StartElement( const css::uno::Reference< css::xml::sax::XAttributeList >& xAttrList ) override;
+    virtual void SAL_CALL startFastElement( sal_Int32 nElement,
+        const css::uno::Reference< css::xml::sax::XFastAttributeList >& xAttrList ) override;
 
-    virtual SvXMLImportContextRef CreateChildContext( sal_uInt16 nPrefix, const OUString& rLocalName,
-        const css::uno::Reference< css::xml::sax::XAttributeList>& xAttrList ) override;
+    virtual css::uno::Reference< XFastContextHandler >  SAL_CALL createFastChildContext(sal_Int32 Element,
+        const css::uno::Reference<css::xml::sax::XFastAttributeList>& Attribs) override;
 
     static void postProcessRootNode( const css::uno::Reference< css::animations::XAnimationNode >& xNode, css::uno::Reference< css::beans::XPropertySet > const & xPageProps );
 };

commit 0329857dbe797703731d5936806b96b30e7011a6

tdf#131632 animation effects are missing features
regression from
    commit 5352d45dd4a04f8f02cf7f6ad4169126d3b3586a
    Date:   Tue Feb 18 15:45:08 2020 +0200
    convert AnimationImport to fast-parser APIs

Change-Id: I7227b122213c877b8ea5db25239eae8151597a8e

include/xmloff/xmltoken.hxx
@@ -1873,6 +1873,7 @@ namespace xmloff { namespace token {
         XML_TEXT_INPUT,
         XML_TEXT_JUSTIFY,
         XML_TEXT_OUTLINE,
+        XML_TEXT_ONLY,
         XML_TEXT_POSITION,
         XML_TEXT_ROTATION_ANGLE,
         XML_TEXT_ROTATION_SCALE,
@@ -2972,6 +2973,7 @@ namespace xmloff { namespace token {
         XML_PRESET_ID,
         XML_PRESET_SUB_TYPE,
         XML_PRESET_CLASS,
+        XML_PRESET_PROPERTY,
         XML_CUSTOM,
         XML_ENTRANCE,
         XML_EXIT,

xmloff/source/core/xmltoken.cxx
@@ -1878,6 +1878,7 @@ namespace xmloff::token {
         TOKEN( "text-input",                      XML_TEXT_INPUT ),
         TOKEN( "text-justify",                    XML_TEXT_JUSTIFY ),
         TOKEN( "text-outline",                    XML_TEXT_OUTLINE ),
+        TOKEN( "text-only",                       XML_TEXT_ONLY ),
         TOKEN( "text-position",                   XML_TEXT_POSITION ),
         TOKEN( "text-rotation-angle",             XML_TEXT_ROTATION_ANGLE ),
         TOKEN( "text-rotation-scale",             XML_TEXT_ROTATION_SCALE ),
@@ -2972,6 +2973,7 @@ namespace xmloff::token {
         TOKEN( "preset-id",         XML_PRESET_ID ),
         TOKEN( "preset-sub-type",       XML_PRESET_SUB_TYPE ),
         TOKEN( "preset-class",      XML_PRESET_CLASS ),
+        TOKEN( "preset-property",      XML_PRESET_PROPERTY ),
         TOKEN( "custom",                XML_CUSTOM ),
         TOKEN( "entrance",          XML_ENTRANCE ),
         TOKEN( "exit",              XML_EXIT ),

xmloff/source/draw/animationimport.cxx
@@ -1142,6 +1142,7 @@ void AnimationNodeContext::init_node(  const css::uno::Reference< css::xml::sax:
                 // push all unknown attributes within the presentation namespace as user data
                 if (nNamespace == NAMESPACE_TOKEN(XML_NAMESPACE_PRESENTATION)
                     || nNamespace == NAMESPACE_TOKEN(XML_NAMESPACE_PRESENTATION_SO52)
+                    || nNamespace == NAMESPACE_TOKEN(XML_NAMESPACE_PRESENTATION_OASIS)
                     || nNamespace == NAMESPACE_TOKEN(XML_NAMESPACE_PRESENTATION_OOO))
                 {
                     aUserData.emplace_back( SvXMLImport::getNameFromToken(aIter.getToken()), makeAny( rValue ) );