e0f0385bd3ccce07a09ee52f8dbb3e563aced870
Michael Meeks <michael.meeks@collabora.com>
2020-03-31 20:27:00 +0100

    listen for and broadcast FrameLineColor state changes.
    Change-Id: I53efdcc4c5a3871761c4feffb079751286d6fbd6
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/91401
    Tested-by: Jenkins
    Reviewed-by: Andras Timar <andras.timar@collabora.com>

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -2417,6 +2417,7 @@ static void doc_iniUnoCommands ()
         OUString(".uno:NumberFormatCurrency"),
         OUString(".uno:NumberFormatPercent"),
         OUString(".uno:NumberFormatDate"),
+        OUString(".uno:FrameLineColor"),
         OUString(".uno:SortAscending"),
         OUString(".uno:SortDescending"),
         OUString(".uno:TrackChanges"),
