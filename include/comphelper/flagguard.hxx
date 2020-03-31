70f9b515404aa90404e203f6d9508ec8046e6d29
Mike Kaganski <mike.kaganski@collabora.com>
2020-03-31 17:15:36 +0300

    Introduce comphelper::ValueRestorationGuard extending FlagRestorationGuard
    ... to any value type.
    
    Change-Id: I808f4729478cb3f90a86ef30be8158ebc40a6b96

--- include/comphelper/flagguard.hxx
+++ include/comphelper/flagguard.hxx
@@ -26,32 +26,34 @@
 namespace comphelper
 {
 
-    //= FlagRestorationGuard
+    //= ValueRestorationGuard
 
-    // note: can't store the originalValue in a FlagRestorationGuard member,
+    // note: can't store the originalValue in a ValueRestorationGuard member,
     // because it will be used from base class dtor
-    struct FlagRestorationGuard_Impl
+    template <typename T> struct ValueRestorationGuard_Impl
     {
-        bool & rFlag;
-        bool const originalValue;
-        FlagRestorationGuard_Impl(bool & i_flagRef)
-            : rFlag(i_flagRef), originalValue(i_flagRef) {}
+        T& rVal;
+        T const originalValue;
+        ValueRestorationGuard_Impl(T& i_valRef)
+            : rVal(i_valRef), originalValue(i_valRef) {}
         void operator()()
         {
-            rFlag = originalValue;
+            rVal = originalValue;
         }
     };
 
-    class FlagRestorationGuard : public ScopeGuard<FlagRestorationGuard_Impl>
+    template <typename T, typename T1>
+    class ValueRestorationGuard : public ScopeGuard<ValueRestorationGuard_Impl<T>>
     {
     public:
-        FlagRestorationGuard( bool& i_flagRef, bool i_temporaryValue )
-            : ScopeGuard(FlagRestorationGuard_Impl(i_flagRef))
+        ValueRestorationGuard(T& i_valRef, T1 i_temporaryValue)
+            : ScopeGuard(ValueRestorationGuard_Impl(i_valRef))
         {
-            i_flagRef = i_temporaryValue;
+            i_valRef = i_temporaryValue;
         }
     };
 
+    typedef ValueRestorationGuard<bool, bool> FlagRestorationGuard;
 
     //= FlagGuard
 
