ddc3cac0a0f9e3837384536cc4a029be3eefe96f
Andrea Gelmini <andrea.gelmini@gelma.net>
2020-04-01 19:21:54 +0200

    Fix typo
    Change-Id: I805ca9fd60b2ad11005bf0eea30f360cffb6166a

--- oox/source/ppt/pptshape.cxx
+++ oox/source/ppt/pptshape.cxx
@@ -420,7 +420,7 @@ namespace
 // 3. ph with nSecondSubType and the same oSubTypeIndex
 // 4. ph with nSecondSubType
 // 5. ph with the same oSubTypeIndex
-// It appears 3 and 1 has the same highest prority.
+// It appears 3 and 1 has the same highest priority.
 
 oox::drawingml::ShapePtr PPTShape::findPlaceholder( sal_Int32 nFirstSubType, sal_Int32 nSecondSubType,
     const OptValue< sal_Int32 >& oSubTypeIndex, std::vector< oox::drawingml::ShapePtr >& rShapes, bool bMasterOnly )
