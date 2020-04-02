262fae7a478dba80caff4d35651965742cb166f9
Stephan Bergmann <sbergman@redhat.com>
2020-04-02 11:10:11 +0200

    Make UITest name match .mk file name
    Change-Id: I38f85fcedfed92a04c2a5538821e94e1c9866ebe
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/91552
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

--- sw/UITest_writer_macro_tests.mk
+++ sw/UITest_writer_macro_tests.mk
@@ -5,12 +5,12 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 # 
 
-$(eval $(call 	gb_UITest_UITest,macro_tests))
+$(eval $(call 	gb_UITest_UITest,writer_macro_tests))
 
-$(eval $(call gb_UITest_add_modules,macro_tests,$(SRCDIR)/sw/qa/uitest,\
+$(eval $(call gb_UITest_add_modules,writer_macro_tests,$(SRCDIR)/sw/qa/uitest,\
 	macro_tests/ \
 ))
 
-$(eval $(call gb_UITest_set_defs,macro_tests, \
+$(eval $(call gb_UITest_set_defs,writer_macro_tests, \
     TDOC="$(SRCDIR)/sw/qa/uitest/macro_tests/data" \
 ))
