92165b8e15696a1f8cdba1773c81bde40542198c
Szabolcs <szabolcs450@gmail.com>
2020-04-03 11:34:07 +0200

    tdf#131841 DOCX DrawingML shape import: Fixed missing HighlightColor
    Implemented highlight color in grouped shapes. It was missing
    completely.
    Co-Author: Balázs Regényi
    
    Change-Id: I51207d01a205fbb24abc51c0d69042d6747570a5

--- oox/source/drawingml/textcharacterpropertiescontext.cxx
+++ oox/source/drawingml/textcharacterpropertiescontext.cxx
@@ -100,10 +100,9 @@ ContextHandlerRef TextCharacterPropertiesContext::onCreateContext( sal_Int32 aEl
         case A_TOKEN( effectDag ):  // CT_EffectContainer 5.1.10.25
         case A_TOKEN( effectLst ):  // CT_EffectList 5.1.10.26
         break;
-
-        case A_TOKEN( highlight ):  // CT_Color
-            return new ColorContext( *this, mrTextCharacterProperties.maHighlightColor );
-
+        case W_TOKEN( highlight ):  // CT_Color
+            mrTextCharacterProperties.maHighlightColor = rAttribs.getColor(W_TOKEN(val));
+            break;
         // EG_TextUnderlineLine
         case A_TOKEN( uLnTx ):      // CT_TextUnderlineLineFollowText
             mrTextCharacterProperties.moUnderlineLineFollowText = true;
