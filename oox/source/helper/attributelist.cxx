e9e757ba6f8c590aead38b8f5c7bbc8299ca201d
Szabolcs <szabolcs450@gmail.com>
2020-04-03 11:34:07 +0200

    tdf#131841 DOCX DrawingML shape import: Fixed missing HighlightColor
    Implemented highlight color in grouped shapes. It was missing
    completely.
    Co-Author: Balázs Regényi
    
    Change-Id: I51207d01a205fbb24abc51c0d69042d6747570a5

--- oox/source/helper/attributelist.cxx
+++ oox/source/helper/attributelist.cxx
@@ -62,6 +62,29 @@ sal_Unicode lclGetXChar( const sal_Unicode*& rpcStr, const sal_Unicode* pcEnd )
 
 } // namespace
 
+#define STRING_TO_TOKEN(color) if (sColorName == #color) return XML_##color
+sal_Int32 getColorTokenFromString(const OUString& sColorName)
+{
+    STRING_TO_TOKEN(black);
+    STRING_TO_TOKEN(blue);
+    STRING_TO_TOKEN(cyan);
+    STRING_TO_TOKEN(darkBlue);
+    STRING_TO_TOKEN(darkCyan);
+    STRING_TO_TOKEN(darkGreen);
+    STRING_TO_TOKEN(darkMagenta);
+    STRING_TO_TOKEN(darkRed);
+    STRING_TO_TOKEN(darkYellow);
+    STRING_TO_TOKEN(darkGray);
+    STRING_TO_TOKEN(green);
+    STRING_TO_TOKEN(lightGray);
+    STRING_TO_TOKEN(magenta);
+    STRING_TO_TOKEN(none);
+    STRING_TO_TOKEN(red);
+    STRING_TO_TOKEN(white);
+    STRING_TO_TOKEN(yellow);
+    return XML_none;
+}
+
 sal_Int32 AttributeConversion::decodeToken( const OUString& rValue )
 {
     return TokenMap::getTokenFromUnicode( rValue );
@@ -123,6 +146,14 @@ sax_fastparser::FastAttributeList *AttributeList::getAttribList() const
 bool AttributeList::hasAttribute( sal_Int32 nAttrToken ) const
 {
     return mxAttribs->hasAttribute( nAttrToken );
+}
+
+oox::drawingml::Color AttributeList::getColor(sal_Int32 nAttrToken) const
+{
+    OUString sColorVal = mxAttribs->getValue(nAttrToken);
+    oox::drawingml::Color aColor;
+    aColor.setPrstClr(getColorTokenFromString(sColorVal));
+    return aColor;
 }
 
 // optional return values -----------------------------------------------------
