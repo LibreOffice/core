962b415edb47187737a5f05c4ff3f6724a19c564
andreas kainz <kainz.a@gmail.com>
2020-04-04 00:14:46 +0200

    tdf#131789 Remove Environment gallery
    Change-Id: I3ed3c2be50457b1ace70f62c23e4495483c80bf1
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/91674
    Tested-by: Jenkins
    Reviewed-by: andreas_kainz <kainz.a@gmail.com>

--- extras/Module_extras.mk
+++ extras/Module_extras.mk
@@ -61,7 +61,6 @@ $(eval $(call gb_Module_add_targets,extras,\
 	Gallery_arrows \
 	Gallery_backgrounds \
 	Gallery_diagrams \
-	Gallery_environment \
 	Gallery_finance \
 	Gallery_people \
 	Gallery_symbols \
