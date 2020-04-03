LibreOffice/core

commit 4c3ba3a413be7339115ea5e6edc825a8434cd345

fdo#75757: Remove inheritance from std::map.
Done for writerfilter::dmapper::PropertyMap.

Added public functions getPropertyValue, bool isSet and Erase. The function
getPropertyValue returns boost::optional<PropertyMap::Property>,
where PropertyMap::Property is an alias for std::pair<PropertyIds,css::uno::Any>.

Fixed all resulting compilation errors. The functions which iterated the map
(ListLevel::GetCharStyleProperties, ListLevel::GetLevelProperties and
SectionPropertyMap::_ApplyProperties) have been written to either used
GetPropertyValues or not iterate over the PropertyMap's attributes.

The properties of the public member functions are now properly used
(Erase and Insert(...,false)).

Change-Id: Ibd90a4ad831c9b7d18b2a50d4aa4eb3f2610cff8
Reviewed-on: https://gerrit.libreoffice.org/10558
Reviewed-by: Michael Stahl <mstahl@redhat.com>
Tested-by: Michael Stahl <mstahl@redhat.com>

writerfilter/source/dmapper/StyleSheetTable.cxx
@@ -99,7 +99,8 @@ TableStyleSheetEntry::~TableStyleSheetEntry( )
 
 void TableStyleSheetEntry::AddTblStylePr( TblStyleType nType, PropertyMapPtr pProps )
 {
-    static const TblStyleType pTypesToFix[] =
+    static const int nTypesProps = 4;
+    static const TblStyleType pTypesToFix[nTypesProps] =
     {
         TBL_STYLE_FIRSTROW,
         TBL_STYLE_LASTROW,
@@ -107,7 +108,7 @@ void TableStyleSheetEntry::AddTblStylePr( TblStyleType nType, PropertyMapPtr pPr
         TBL_STYLE_LASTCOL
     };
 
-    static const PropertyIds pPropsToCheck[] =
+    static const PropertyIds pPropsToCheck[nTypesProps] =
     {
         PROP_BOTTOM_BORDER,
         PROP_TOP_BORDER,
@@ -115,29 +116,24 @@ void TableStyleSheetEntry::AddTblStylePr( TblStyleType nType, PropertyMapPtr pPr
         PROP_LEFT_BORDER
     };
 
-    int i = 0;
-    while ( i < 4 )
+    for (int i=0; i < nTypesProps; ++i )
     {
         if ( nType == pTypesToFix[i] )
         {
             PropertyIds nChecked = pPropsToCheck[i];
-            PropertyMap::iterator pCheckedIt = pProps->find(nChecked);
+            boost::optional<PropertyMap::Property> pChecked = pProps->getProperty(nChecked);
 
             PropertyIds nInsideProp = ( i < 2 ) ? META_PROP_HORIZONTAL_BORDER : META_PROP_VERTICAL_BORDER;
-            PropertyMap::iterator pInsideIt = pProps->find(nInsideProp);
+            boost::optional<PropertyMap::Property> pInside = pProps->getProperty(nInsideProp);
 
-            bool bHasChecked = pCheckedIt != pProps->end( );
-            bool bHasInside = pInsideIt != pProps->end( );
-
-            if ( bHasChecked && bHasInside )
+            if ( pChecked && pProps )
             {
                 // In this case, remove the inside border
-                pProps->erase( pInsideIt );
+                pProps->Erase( nInsideProp );
             }
 
-            i = 4; // Stop looping stupidly
+            break;
         }
-        i++;
     }
 
     // Append the tblStylePr
@@ -162,11 +158,11 @@ PropertyMapPtr TableStyleSheetEntry::GetProperties( sal_Int32 nMask, StyleSheetE
         {
             pStack->push_back(pEntry);
 
-        TableStyleSheetEntry* pParent = static_cast<TableStyleSheetEntry *>( pEntry.get( ) );
+            TableStyleSheetEntry* pParent = static_cast<TableStyleSheetEntry *>( pEntry.get( ) );
             pProps->InsertProps(pParent->GetProperties(nMask));
 
             pStack->pop_back();
-    }
+        }
     }
 
     // And finally get the mask ones
@@ -221,19 +217,15 @@ void lcl_mergeProps( PropertyMapPtr pToFill,  PropertyMapPtr pToAdd, TblStyleTyp
     for ( unsigned i = 0 ; i != sizeof(pPropsToCheck) / sizeof(PropertyIds); i++ )
     {
         PropertyIds nId = pPropsToCheck[i];
-        PropertyMap::iterator pIt = pToAdd->find(nId);
+        boost::optional<PropertyMap::Property> pProp = pToAdd->getProperty(nId);
 
-        if ( pIt != pToAdd->end( ) )
+        if ( pProp )
         {
-            PropertyMap::iterator pDestIt = pToFill->find(nId);
-
             if ( pRemoveInside[i] )
             {
                 // Remove the insideH and insideV depending on the cell pos
                 PropertyIds nInsideProp = ( i < 2 ) ? META_PROP_HORIZONTAL_BORDER : META_PROP_VERTICAL_BORDER;
-                pDestIt = pToFill->find(nInsideProp);
-                if ( pDestIt != pToFill->end( ) )
-                    pToFill->erase( pDestIt );
+                pToFill->Erase(nInsideProp);
             }
         }
     }
@@ -792,12 +784,12 @@ void StyleSheetTable::lcl_sprm(Sprm & rSprm)
                     {
                         // The current style is the default paragraph style.
                         PropertyMapPtr pProperties = m_pImpl->m_pCurrentEntry->pProperties;
-                        if (pProperties->find(PROP_CHAR_HEIGHT) != pProperties->end() && m_pImpl->m_pDefaultParaProps->find(PROP_CHAR_HEIGHT) == m_pImpl->m_pDefaultParaProps->end())
+                        if (pProperties->isSet(PROP_CHAR_HEIGHT) && !m_pImpl->m_pDefaultParaProps->isSet(PROP_CHAR_HEIGHT))
                         {
                             // We provide a character height value, but a document-level default wasn't set.
                             if (m_pImpl->m_xTextDefaults.is())
                             {
-                                m_pImpl->m_xTextDefaults->setPropertyValue("CharHeight", pProperties->operator[](PROP_CHAR_HEIGHT).getValue());
+                                m_pImpl->m_xTextDefaults->setPropertyValue("CharHeight", pProperties->getProperty(PROP_CHAR_HEIGHT)->second);
                             }
                         }
                     }
@@ -1536,7 +1528,7 @@ void StyleSheetTable::applyDefaults(bool bParaProperties)
                 m_pImpl->m_rDMapper.GetTextFactory()->createInstance("com.sun.star.text.Defaults"),
                 uno::UNO_QUERY_THROW );
         }
-        if( bParaProperties && m_pImpl->m_pDefaultParaProps.get() && m_pImpl->m_pDefaultParaProps->size())
+        if( bParaProperties && m_pImpl->m_pDefaultParaProps.get())
         {
             uno::Sequence< beans::PropertyValue > aPropValues = m_pImpl->m_pDefaultParaProps->GetPropertyValues();
             for( sal_Int32 i = 0; i < aPropValues.getLength(); ++i )
@@ -1551,7 +1543,7 @@ void StyleSheetTable::applyDefaults(bool bParaProperties)
                 }
             }
         }
-        if( !bParaProperties && m_pImpl->m_pDefaultCharProps.get() && m_pImpl->m_pDefaultCharProps->size())
+        if( !bParaProperties && m_pImpl->m_pDefaultCharProps.get())
         {
             uno::Sequence< beans::PropertyValue > aPropValues = m_pImpl->m_pDefaultCharProps->GetPropertyValues();
             for( sal_Int32 i = 0; i < aPropValues.getLength(); ++i )

writerfilter/source/dmapper/DomainMapper.cxx
@@ -184,7 +184,6 @@ void DomainMapper::lcl_attribute(Id nName, Value & val)
     OUString sStringValue = val.getString();
 
     SectionPropertyMap * pSectionContext = m_pImpl->GetSectionContext();
-    PropertyMap::iterator oldPropValue;
         switch( nName )
         {
         case NS_ooxml::LN_CT_Lvl_start:
@@ -374,16 +373,10 @@ void DomainMapper::lcl_attribute(Id nName, Value & val)
 #define SINGLE_LINE_SPACING 240
             style::LineSpacing aSpacing;
             PropertyMapPtr pTopContext = m_pImpl->GetTopContext();
-            bool bFound = false;
-            PropertyMap::iterator aLineSpacingIter;
-            if (pTopContext)
+            boost::optional<PropertyMap::Property> aLineSpacingVal;
+            if (pTopContext && (aLineSpacingVal = pTopContext->getProperty(PROP_PARA_LINE_SPACING)) )
             {
-                aLineSpacingIter = pTopContext->find(PROP_PARA_LINE_SPACING);
-                bFound = aLineSpacingIter != pTopContext->end();
-            }
-            if (bFound)
-            {
-                aLineSpacingIter->second.getValue() >>= aSpacing;
+                aLineSpacingVal->second >>= aSpacing;
             }
             else
             {
@@ -404,7 +397,7 @@ void DomainMapper::lcl_attribute(Id nName, Value & val)
                 {
                     // direct formatting is applied for table cell data
                     TablePropertyMapPtr pTblCellWithDirectFormatting(new TablePropertyMap);
-                    pTblCellWithDirectFormatting->insert(std::pair< PropertyIds, PropValue >(PROP_PARA_LINE_SPACING, uno::makeAny( aSpacing )));
+                    pTblCellWithDirectFormatting->Insert(PROP_PARA_LINE_SPACING, uno::makeAny( aSpacing ), false);
                     m_pImpl->getTableManager().cellProps(pTblCellWithDirectFormatting);
                 }
             }
@@ -1035,11 +1028,11 @@ sal_Int32 lcl_getCurrentNumberingProperty(
 static bool ExchangeLeftRight( const PropertyMapPtr rContext, DomainMapper_Impl* m_pImpl )
 {
     bool bExchangeLeftRight = false;
-    PropertyMap::const_iterator aPropParaIte = rContext->find(PROP_WRITING_MODE);
-    if( aPropParaIte != rContext->end())
+    boost::optional<PropertyMap::Property> aPropPara = rContext->getProperty(PROP_WRITING_MODE);
+    if( aPropPara )
     {
         sal_Int32 aAdjust ;
-        if( (aPropParaIte->second.getValue() >>= aAdjust) && aAdjust == text::WritingMode2::RL_TB )
+        if( (aPropPara->second >>= aAdjust) && aAdjust == text::WritingMode2::RL_TB )
             bExchangeLeftRight = true;
     }
     else
@@ -1048,11 +1041,11 @@ static bool ExchangeLeftRight( const PropertyMapPtr rContext, DomainMapper_Impl*
         StyleSheetEntryPtr pTable = m_pImpl->GetStyleSheetTable()->FindDefaultParaStyle();
         if ( pTable )
         {
-            PropertyMap::const_iterator aPropStyle = pTable->pProperties->find(PROP_WRITING_MODE);
-            if( aPropStyle != pTable->pProperties->end())
+            boost::optional<PropertyMap::Property> aPropStyle = pTable->pProperties->getProperty(PROP_WRITING_MODE);
+            if( aPropStyle )
             {
                 sal_Int32 aDirect;
-                if( (aPropStyle->second.getValue() >>= aDirect) && aDirect == text::WritingMode2::RL_TB )
+                if( (aPropStyle->second >>= aDirect) && aDirect == text::WritingMode2::RL_TB )
                     bExchangeLeftRight = true;
             }
         }
@@ -1140,7 +1133,7 @@ void DomainMapper::sprmWithProps( Sprm& rSprm, PropertyMapPtr rContext )
                     uno::Any aRules = uno::makeAny( pList->GetNumberingRules( ) );
                     rContext->Insert( PROP_NUMBERING_RULES, aRules );
                     // erase numbering from pStyle if already set
-                    rContext->erase(PROP_NUMBERING_STYLE_NAME);
+                    rContext->Erase(PROP_NUMBERING_STYLE_NAME);
                 }
             }
             else
@@ -1511,7 +1504,7 @@ void DomainMapper::sprmWithProps( Sprm& rSprm, PropertyMapPtr rContext )
                     break;
                     case NS_ooxml::LN_EG_RPrBase_smallCaps:
                         // If smallcaps would be just disabled and an other casemap is already inserted, don't do anything.
-                        if (nIntValue || rContext->find(ePropertyId) == rContext->end())
+                        if (nIntValue || !rContext->isSet(ePropertyId) )
                             rContext->Insert(ePropertyId, uno::makeAny( nIntValue ? style::CaseMap::SMALLCAPS : style::CaseMap::NONE));
                         m_pImpl->appendGrabBag(m_pImpl->m_aInteropGrabBag, "smallCaps", OUString::number(nIntValue));
                     break;
@@ -1546,7 +1539,7 @@ void DomainMapper::sprmWithProps( Sprm& rSprm, PropertyMapPtr rContext )
                 {
                     // If the default para style contains PROP_CHAR_HEIGHT, that should have priority over the table style.
                     StyleSheetEntryPtr pTable = m_pImpl->GetStyleSheetTable()->FindDefaultParaStyle();
-                    if (pTable && pTable->pProperties->find(PROP_CHAR_HEIGHT) != pTable->pProperties->end())
+                    if (pTable && pTable->pProperties->isSet(PROP_CHAR_HEIGHT) )
                         bIgnore = true;
                 }
                 if (!bIgnore)
@@ -2005,7 +1998,7 @@ void DomainMapper::sprmWithProps( Sprm& rSprm, PropertyMapPtr rContext )
             m_pImpl->GetTopContext()->Insert( PROP_PARA_STYLE_NAME, uno::makeAny( sConvertedStyleName ));
         //apply numbering to paragraph if it was set at the style, but only if the paragraph itself
         //does not specify the numbering
-        if( rContext->find(PROP_NUMBERING_RULES) == rContext->end()) // !contains
+        if( !rContext->isSet(PROP_NUMBERING_RULES) ) // !contains
         {
             const StyleSheetEntryPtr pEntry = pStyleTable->FindStyleSheetByISTD(sStringValue);
             OSL_ENSURE( pEntry.get(), "no style sheet found" );
@@ -2022,8 +2015,9 @@ void DomainMapper::sprmWithProps( Sprm& rSprm, PropertyMapPtr rContext )
                 {
                     const StyleSheetEntryPtr pParent = pStyleTable->FindStyleSheetByISTD(pEntry->sBaseStyleIdentifier);
                     const StyleSheetPropertyMap* pParentProperties = dynamic_cast<const StyleSheetPropertyMap*>(pParent ? pParent->pProperties.get() : 0);
-                    if (pParentProperties && pParentProperties->find(PROP_PARA_RIGHT_MARGIN) != pParentProperties->end())
-                        nParaRightMargin = pParentProperties->find(PROP_PARA_RIGHT_MARGIN)->second.getValue().get<sal_Int32>();
+                    boost::optional<PropertyMap::Property> pPropMargin;
+                    if (pParentProperties && (pPropMargin = pParentProperties->getProperty(PROP_PARA_RIGHT_MARGIN)) )
+                        nParaRightMargin = pPropMargin->second.get<sal_Int32>();
                 }
                 if (nParaRightMargin != 0)
                 {
@@ -2502,7 +2496,7 @@ void DomainMapper::processDeferredCharacterProperties( const std::map< sal_Int32
             {
                 std::map< sal_Int32, uno::Any >::const_iterator font = deferredCharacterProperties.find( NS_ooxml::LN_EG_RPrBase_sz );
                 PropertyMapPtr pDefaultCharProps = m_pImpl->GetStyleSheetTable()->GetDefaultCharProps();
-                PropertyMap::iterator aDefaultFont = pDefaultCharProps->find(PROP_CHAR_HEIGHT);
+                boost::optional<PropertyMap::Property> aDefaultFont = pDefaultCharProps->getProperty(PROP_CHAR_HEIGHT);
                 if( font != deferredCharacterProperties.end())
                 {
                     double fontSize = 0;
@@ -2510,10 +2504,10 @@ void DomainMapper::processDeferredCharacterProperties( const std::map< sal_Int32
                     nEscapement = nIntValue * 100 / fontSize;
                 }
                 // TODO if not direct formatting, check the style first, not directly the default char props.
-                else if (aDefaultFont != pDefaultCharProps->end())
+                else if (aDefaultFont)
                 {
                     double fHeight = 0;
-                    aDefaultFont->second.getValue() >>= fHeight;
+                    aDefaultFont->second >>= fHeight;
                     // fHeight is in points, nIntValue is in half points, nEscapement is in percents.
                     nEscapement = nIntValue * 100 / fHeight / 2;
                 }

commit ddde5ddb836b0427f017705cf194b26e0b1942bd

fix wrong variable checked in AddTblStylePr
regression from
    commit 4c3ba3a413be7339115ea5e6edc825a8434cd345
    Date:   Sat Jul 26 17:17:27 2014 +0100
    fdo#75757: Remove inheritance from std::map.
found by my new unusedvariableplus loplugin

Change-Id: Ica9a3ade207698ac7af76e010fbb9e6bdd0dc227

writerfilter/source/dmapper/StyleSheetTable.cxx
@@ -118,7 +118,7 @@ void TableStyleSheetEntry::AddTblStylePr( TblStyleType nType, const PropertyMapP
             PropertyIds nInsideProp = ( i < 2 ) ? META_PROP_HORIZONTAL_BORDER : META_PROP_VERTICAL_BORDER;
             std::optional<PropertyMap::Property> pInside = pProps->getProperty(nInsideProp);
 
-            if ( pChecked && pProps )
+            if ( pChecked && pInside )
             {
                 // In this case, remove the inside border
                 pProps->Erase( nInsideProp );