ed99bad9ad9a161adfeb65ceb7b0284f3fe883a3
Michael Stahl <Michael.Stahl@cib.de>
2020-04-06 14:33:15 +0200

    lxml: try to remove some quirky PYPATH stuff
    In RepositoryExternal.mk, a system python automatically gets $PYPATH
    added to $PYTHONPATH, but the internal one does not - this doesn't make
    sense.
    
    Try to remove it for system-case by fixing the one case that relies on
    it and for which it was introduced in commit
    84ef6d82546b044990f4efd57e51e29c6c6565c8 to directly extend $PYTHONPATH
    instead, which ought to work as long as it's not evaluated at global
    scope and thereby avoids affecting all python invocations.
    
    Change-Id: I4534f2be92b850dc01193cb1bb2e0a299a5152be

--- external/lxml/ExternalProject_lxml.mk
+++ external/lxml/ExternalProject_lxml.mk
@@ -24,7 +24,7 @@ $(call gb_ExternalProject_get_state_target,lxml,build): \
     $(call gb_ExternalExecutable_get_dependencies,python)
 	$(call gb_Trace_StartRange,lxml,EXTERNAL)
 	$(call gb_ExternalProject_run,build,\
-		PYPATH=$${PYPATH:+$$PYPATH:}$(call gb_UnpackedTarball_get_dir,lxml)/install && \
+		export PYTHONPATH=$${PYTHONPATH:+$$PYTHONPATH:}$(call gb_UnpackedTarball_get_dir,lxml)/install && \
 		$(if $(PYTHON_FOR_BUILD), \
 			unset MACOSX_DEPLOYMENT_TARGET && , \
 			CFLAGS="$$CFLAGS -I$(call gb_UnpackedTarball_get_dir,python3)" && \
