54a8f79f42eaf9a3613a26efcc614e46a42fa7e7
Caolán McNamara <caolanm@redhat.com>
2020-04-09 11:41:00 +0100

    tdf#131120 wip: use a replacement for GtkComboBox
    the problems with GtkComboBox we have are:
    
    1) https://gitlab.gnome.org/GNOME/gtk/issues/1910
       has_entry long menus take forever to appear (tdf#125388)
    
       on measuring each row, the GtkComboBox GtkTreeMenu will call
       its area_apply_attributes_cb function on the row, but that calls
       gtk_tree_menu_get_path_item which then loops through each child of the
       menu looking for the widget of the row, so performance drops to useless.
    
       All area_apply_attributes_cb does it set menu item sensitivity, so block it from running
       with fragile hackery which assumes that the unwanted callback is the only one with a
    
    2) https://gitlab.gnome.org/GNOME/gtk/issues/94
       when a super tall combobox menu is activated, and the selected
       entry is sufficiently far down the list, then the menu doesn't
       appear under wayland
    
    3) https://gitlab.gnome.org/GNOME/gtk/issues/310
       no typeahead support
    
    4) we want to be able to control the width of the button, but have a drop down menu which
       is not limited to the width of the button
    
    5) https://bugs.documentfoundation.org/show_bug.cgi?id=131120
       super tall menu doesn't appear under X sometimes
    
    In general we often pack a lot into the comboboxes and the default ones don't
    like that.
    
    to-do: surface vs pixbuf renderers
         , placement under older gtks
    
    Change-Id: I1b6164020996377341b5992d593a027b76021f65

--- vcl/UIConfig_vcl.mk
+++ vcl/UIConfig_vcl.mk
@@ -11,7 +11,7 @@ $(eval $(call gb_UIConfig_UIConfig,vcl))
 
 $(eval $(call gb_UIConfig_add_uifiles,vcl,\
 	vcl/uiconfig/ui/aboutbox \
-	vcl/uiconfig/ui/wizard \
+	vcl/uiconfig/ui/combobox \
 	vcl/uiconfig/ui/cupspassworddialog \
 	vcl/uiconfig/ui/editmenu \
 	vcl/uiconfig/ui/errornocontentdialog \
@@ -24,6 +24,7 @@ $(eval $(call gb_UIConfig_add_uifiles,vcl,\
 	vcl/uiconfig/ui/printprogressdialog \
 	vcl/uiconfig/ui/querydialog \
 	vcl/uiconfig/ui/screenshotparent \
+	vcl/uiconfig/ui/wizard \
 ))
 
 $(eval $(call gb_UIConfig_add_a11yerrors_uifiles,vcl,\
