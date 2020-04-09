cdadd1a0c5d94a1c92ffd431080c60f65d4abe0d
Heiko Tietze <tietze.heiko@gmail.com>
2020-04-09 09:27:13 +0200

    Resolves build error from patch I7868900d3dceb39497896bcef529fafef3bf268a
    Change-Id: Idfe76b8ca6fac9d93cc16ccaec40191d96092ad1
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/91946
    Tested-by: Jenkins
    Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>

--- Repository.mk
+++ Repository.mk
@@ -878,7 +878,6 @@ $(eval $(call gb_Helper_register_packages_for_install,ooo,\
 	extras_database \
 	extras_databasebiblio \
 	extras_gallbullets \
-	extras_gallhtmlexpo \
 	extras_gallmytheme \
 	extras_gallroot \
 	extras_gallsystem \
