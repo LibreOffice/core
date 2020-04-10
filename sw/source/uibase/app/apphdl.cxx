bb5f7a668d0e4e8df030944bbda57bd804dfa757
Caolán McNamara <caolanm@redhat.com>
2020-04-10 16:21:33 +0100

    null-deref of GetObjectShell
    Change-Id: Ie0c46635d9ad01a17b5d5028ed5f983fd8c5ee74

--- sw/source/uibase/app/apphdl.cxx
+++ sw/source/uibase/app/apphdl.cxx
@@ -158,7 +158,8 @@ void SwModule::StateOther(SfxItemSet &rSet)
             break;
             case FN_MAILMERGE_WIZARD:
             {
-                if (GetObjectShell()->isExportLocked())
+                SfxObjectShell* pObjectShell = GetObjectShell();
+                if (pObjectShell && pObjectShell->isExportLocked())
                     rSet.DisableItem(nWhich);
                 break;
             }
