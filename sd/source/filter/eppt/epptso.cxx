LibreOffice/core

commit d458adeed0c34fae26fe7f3d6ecc9b75b431922f

Simplify Sequence iterations in sd
Use range-based loops, STL and comphelper functions

Change-Id: If4b6d464fc393049dc8d7e5c3faf1cf66b6a369a
Reviewed-on: https://gerrit.libreoffice.org/76480
Tested-by: Jenkins
Reviewed-by: Arkadiy Illarionov <qarkai@gmail.com>

sd/source/filter/eppt/epptso.cxx
@@ -1246,11 +1246,11 @@ void PPTWriter::ImplWriteTextStyleAtom( SvStream& rOut, int nTextInstance, sal_u
             if ( nTextRulerAtomFlags & 4 )
             {
                 pRuleOut->WriteUInt16( nTabCount );
-                for ( i = 0; i < nTabs; i++ )
+                for ( const css::style::TabStop& rTabStop : pPara->maTabStop )
                 {
-                    sal_uInt16 nPosition = static_cast<sal_uInt16>( ( pTabStop[ i ].Position / 4.40972 ) + nTextOfs );
+                    sal_uInt16 nPosition = static_cast<sal_uInt16>( ( rTabStop.Position / 4.40972 ) + nTextOfs );
                     sal_uInt16 nType;
-                    switch ( pTabStop[ i ].Alignment )
+                    switch ( rTabStop.Alignment )
                     {
                         case css::style::TabAlign_DECIMAL :    nType = 3; break;
                         case css::style::TabAlign_RIGHT :      nType = 2; break;
@@ -1258,7 +1258,7 @@ void PPTWriter::ImplWriteTextStyleAtom( SvStream& rOut, int nTextInstance, sal_u
 
                         case css::style::TabAlign_LEFT :
                         default:                               nType = 0;
-                    };
+                    }
                     pRuleOut->WriteUInt16( nPosition )
                              .WriteUInt16( nType );
                 }

sd/source/core/CustomAnimationCloner.cxx
@@ -224,12 +224,8 @@ namespace sd
                 Sequence<Any> aSequence;
                 rValue >>= aSequence;
 
-                const sal_Int32 nLength = aSequence.getLength();
-                sal_Int32 nElement;
-                Any* pAny = aSequence.getArray();
-
-                for( nElement = 0; nElement < nLength; nElement++, pAny++ )
-                    *pAny = transformValue( *pAny );
+                for( Any& rAny : aSequence )
+                    rAny = transformValue( rAny );
 
                 return makeAny( aSequence );
             }

sd/source/core/CustomAnimationEffect.cxx
@@ -156,37 +156,33 @@ void CustomAnimationEffect::setNode( const css::uno::Reference< css::animations:
     mxAudio.clear();
 
     Sequence< NamedValue > aUserData( mxNode->getUserData() );
-    sal_Int32 nLength = aUserData.getLength();
-    const NamedValue* p = aUserData.getConstArray();
 
-    while( nLength-- )
+    for( const NamedValue& rProp : aUserData )
     {
-        if ( p->Name == "node-type" )
+        if ( rProp.Name == "node-type" )
         {
-            p->Value >>= mnNodeType;
+            rProp.Value >>= mnNodeType;
         }
-        else if ( p->Name == "preset-id" )
+        else if ( rProp.Name == "preset-id" )
         {
-            p->Value >>= maPresetId;
+            rProp.Value >>= maPresetId;
         }
-        else if ( p->Name == "preset-sub-type" )
+        else if ( rProp.Name == "preset-sub-type" )
         {
-            p->Value >>= maPresetSubType;
+            rProp.Value >>= maPresetSubType;
         }
-        else if ( p->Name == "preset-class" )
+        else if ( rProp.Name == "preset-class" )
         {
-            p->Value >>= mnPresetClass;
+            rProp.Value >>= mnPresetClass;
         }
-        else if ( p->Name == "preset-property" )
+        else if ( rProp.Name == "preset-property" )
         {
-            p->Value >>= maProperty;
+            rProp.Value >>= maProperty;
         }
-        else if ( p->Name == "group-id" )
+        else if ( rProp.Name == "group-id" )
         {
-            p->Value >>= mnGroupId;
+            rProp.Value >>= mnGroupId;
         }
-
-        p++;
     }
 
     // get effect start time
@@ -389,19 +385,12 @@ sal_Int32 CustomAnimationEffect::get_node_type( const Reference< XAnimationNode 
     if( xNode.is() )
     {
         Sequence< NamedValue > aUserData( xNode->getUserData() );
-        sal_Int32 nLength = aUserData.getLength();
-        if( nLength )
+        if( aUserData.hasElements() )
         {
-            const NamedValue* p = aUserData.getConstArray();
-            while( nLength-- )
-            {
-                if ( p->Name == "node-type" )
-                {
-                    p->Value >>= nNodeType;
-                    break;
-                }
-                p++;
-            }
+            const NamedValue* pProp = std::find_if(aUserData.begin(), aUserData.end(),
+                [](const NamedValue& rProp) { return rProp.Name == "node-type"; });
+            if (pProp != aUserData.end())
+                pProp->Value >>= nNodeType;
         }
     }
 
@@ -424,23 +413,18 @@ void CustomAnimationEffect::setPresetClass( sal_Int16 nPresetClass )
     bool bFound = false;
     if( nLength )
     {
-        NamedValue* p = aUserData.getArray();
-        while( nLength-- )
+        NamedValue* pProp = std::find_if(aUserData.begin(), aUserData.end(),
+            [](const NamedValue& rProp) { return rProp.Name == "preset-class"; });
+        if (pProp != aUserData.end())
         {
-            if ( p->Name == "preset-class" )
-            {
-                p->Value <<= mnPresetClass;
-                bFound = true;
-                break;
-            }
-            p++;
+            pProp->Value <<= mnPresetClass;
+            bFound = true;
         }
     }
 
-    // no "node-type" entry inside user data, so add it
+    // no "preset-class" entry inside user data, so add it
     if( !bFound )
     {
-        nLength = aUserData.getLength();
         aUserData.realloc( nLength + 1);
         aUserData[nLength].Name = "preset-class";
         aUserData[nLength].Value <<= mnPresetClass;
@@ -465,23 +449,18 @@ void CustomAnimationEffect::setNodeType( sal_Int16 nNodeType )
     bool bFound = false;
     if( nLength )
     {
-        NamedValue* p = aUserData.getArray();
-        while( nLength-- )
+        NamedValue* pProp = std::find_if(aUserData.begin(), aUserData.end(),
+            [](const NamedValue& rProp) { return rProp.Name == "node-type"; });
+        if (pProp != aUserData.end())
         {
-            if ( p->Name == "node-type" )
-            {
-                p->Value <<= mnNodeType;
-                bFound = true;
-                break;
-            }
-            p++;
+            pProp->Value <<= mnNodeType;
+            bFound = true;
         }
     }
 
     // no "node-type" entry inside user data, so add it
     if( !bFound )
     {
-        nLength = aUserData.getLength();
         aUserData.realloc( nLength + 1);
         aUserData[nLength].Name = "node-type";
         aUserData[nLength].Value <<= mnNodeType;
@@ -503,23 +482,18 @@ void CustomAnimationEffect::setGroupId( sal_Int32 nGroupId )
     bool bFound = false;
     if( nLength )
     {
-        NamedValue* p = aUserData.getArray();
-        while( nLength-- )
+        NamedValue* pProp = std::find_if(aUserData.begin(), aUserData.end(),
+            [](const NamedValue& rProp) { return rProp.Name == "group-id"; });
+        if (pProp != aUserData.end())
         {
-            if ( p->Name == "group-id" )
-            {
-                p->Value <<= mnGroupId;
-                bFound = true;
-                break;
-            }
-            p++;
+            pProp->Value <<= mnGroupId;
+            bFound = true;
         }
     }
 
-    // no "node-type" entry inside user data, so add it
+    // no "group-id" entry inside user data, so add it
     if( !bFound )
     {
-        nLength = aUserData.getLength();
         aUserData.realloc( nLength + 1);
         aUserData[nLength].Name = "group-id";
         aUserData[nLength].Value <<= mnGroupId;
@@ -1666,21 +1640,11 @@ CustomAnimationEffectPtr EffectSequenceHelper::append( const CustomAnimationPres
             // first, filter all only ui relevant user data
             std::vector< NamedValue > aNewUserData;
             Sequence< NamedValue > aUserData( xNode->getUserData() );
-            sal_Int32 nLength = aUserData.getLength();
-            const NamedValue* p = aUserData.getConstArray();
-            bool bFilter = false;
 
-            while( nLength-- )
-            {
-                if( p->Name != "text-only" && p->Name != "preset-property" )
-                {
-                    aNewUserData.push_back( *p );
-                    bFilter = true;
-                }
-                p++;
-            }
+            std::copy_if(aUserData.begin(), aUserData.end(), std::back_inserter(aNewUserData),
+                [](const NamedValue& rProp) { return rProp.Name != "text-only" && rProp.Name != "preset-property"; });
 
-            if( bFilter )
+            if( !aNewUserData.empty() )
             {
                 aUserData = ::comphelper::containerToSequence( aNewUserData );
                 xNode->setUserData( aUserData );
@@ -2921,18 +2885,11 @@ void EffectSequenceHelper::processAfterEffect( const Reference< XAnimationNode >
         Reference< XAnimationNode > xMaster;
 
         Sequence< NamedValue > aUserData( xNode->getUserData() );
-        sal_Int32 nLength = aUserData.getLength();
-        const NamedValue* p = aUserData.getConstArray();
+        const NamedValue* pProp = std::find_if(aUserData.begin(), aUserData.end(),
+            [](const NamedValue& rProp) { return rProp.Name == "master-element"; });
 
-        while( nLength-- )
-        {
-            if ( p->Name == "master-element" )
-            {
-                p->Value >>= xMaster;
-                break;
-            }
-            p++;
-        }
+        if (pProp != aUserData.end())
+            pProp->Value >>= xMaster;
 
         // only process if this is a valid after effect
         if( xMaster.is() )

sd/source/core/CustomAnimationPreset.cxx
@@ -93,22 +93,18 @@ void implImportLabels( const Reference< XMultiServiceFactory >& xConfigProvider,
         {
             Reference< XNameAccess > xNameAccess;
             Sequence< OUString > aNames( xConfigAccess->getElementNames() );
-            const OUString* p = aNames.getConstArray();
-            sal_Int32 n = aNames.getLength();
-            while(n--)
+            for(const OUString& rName : aNames)
             {
-                xConfigAccess->getByName( *p ) >>= xNameAccess;
+                xConfigAccess->getByName( rName ) >>= xNameAccess;
                 if( xNameAccess.is() )
                 {
                     OUString aUIName;
                     xNameAccess->getByName( "Label" ) >>= aUIName;
                     if( !aUIName.isEmpty() )
                     {
-                        rStringMap[ *p ] = aUIName;
+                        rStringMap[ rName ] = aUIName;
                     }
                 }
-
-                p++;
             }
         }
     }
@@ -132,22 +128,10 @@ CustomAnimationPreset::CustomAnimationPreset( const CustomAnimationEffectPtr& pE
     mfDuration = pEffect->getDuration();
     maDefaultSubTyp = pEffect->getPresetSubType();
 
-    mbIsTextOnly = false;
-
     Sequence< NamedValue > aUserData( pEffect->getNode()->getUserData() );
-    sal_Int32 nLength = aUserData.getLength();
-    const NamedValue* p = aUserData.getConstArray();
 
-    while( nLength-- )
-    {
-        if ( p->Name == "text-only" )
-        {
-            mbIsTextOnly = true;
-            break;
-        }
-        p++;
-    }
-
+    mbIsTextOnly = std::any_of(aUserData.begin(), aUserData.end(),
+        [](const NamedValue& rProp) { return rProp.Name == "text-only"; });
 }
 
 void CustomAnimationPreset::add( const CustomAnimationEffectPtr& pEffect )
@@ -311,9 +295,9 @@ void CustomAnimationPresets::importEffects()
         uno::Sequence< OUString > aFiles;
         xNameAccess->getByName( "EffectFiles" ) >>= aFiles;
 
-        for( sal_Int32 i=0; i<aFiles.getLength(); ++i )
+        for( const auto& rFile : aFiles )
         {
-            OUString aURL = comphelper::getExpandedUri(xContext, aFiles[i]);
+            OUString aURL = comphelper::getExpandedUri(xContext, rFile);
 
             mxRootNode = implImportEffects( xServiceFactory, aURL );
 
@@ -420,11 +404,9 @@ void CustomAnimationPresets::importPresets( const Reference< XMultiServiceFactor
             Reference< XNameAccess > xCategoryAccess;
 
             Sequence< OUString > aNames( xTypeAccess->getElementNames() );
-            const OUString* p = aNames.getConstArray();
-            sal_Int32 n = aNames.getLength();
-            while(n--)
+            for(const OUString& rName : aNames)
             {
-                xTypeAccess->getByName( *p ) >>= xCategoryAccess;
+                xTypeAccess->getByName( rName ) >>= xCategoryAccess;
 
                 if( xCategoryAccess.is() && xCategoryAccess->hasByName( "Label" ) && xCategoryAccess->hasByName( "Effects" ) )
                 {
@@ -436,11 +418,9 @@ void CustomAnimationPresets::importPresets( const Reference< XMultiServiceFactor
 
                     EffectDescriptorList aEffectsList;
 
-                    const OUString* pEffectNames = aEffects.getConstArray();
-                    sal_Int32 nEffectCount = aEffects.getLength();
-                    while( nEffectCount-- )
+                    for( const OUString& rEffectName : aEffects )
                     {
-                        CustomAnimationPresetPtr pEffect = getEffectDescriptor( *pEffectNames );
+                        CustomAnimationPresetPtr pEffect = getEffectDescriptor( rEffectName );
                         if( pEffect.get() )
                         {
                             aEffectsList.push_back( pEffect );
@@ -448,16 +428,13 @@ void CustomAnimationPresets::importPresets( const Reference< XMultiServiceFactor
 #ifdef DEBUG
                         else
                         {
-                            aMissedPresetIds += OUString(*pEffectNames);
+                            aMissedPresetIds += OUString(rEffectName);
                             aMissedPresetIds += "\n";
                         }
 #endif
-                        pEffectNames++;
                     }
                     rPresetMap.push_back( std::make_shared<PresetCategory>( aLabel, aEffectsList ) );
                 }
-
-                p++;
             }
         }
     }

sd/source/core/TransitionPreset.cxx
@@ -59,16 +59,10 @@ TransitionPreset::TransitionPreset( const css::uno::Reference< css::animations::
 {
     // first locate preset id
     Sequence< NamedValue > aUserData( xNode->getUserData() );
-    sal_Int32 nLength = aUserData.getLength();
-    const NamedValue* p = aUserData.getConstArray();
-    while( nLength-- )
-    {
-        if ( p->Name == "preset-id" )
-        {
-            p->Value >>= maPresetId;
-            break;
-        }
-    }
+    const NamedValue* pProp = std::find_if(aUserData.begin(), aUserData.end(),
+        [](const NamedValue& rProp) { return rProp.Name == "preset-id"; });
+    if (pProp != aUserData.end())
+        pProp->Value >>= maPresetId;
 
     // second, locate transition filter element
     Reference< XEnumerationAccess > xEnumerationAccess( xNode, UNO_QUERY_THROW );
@@ -212,9 +206,9 @@ bool TransitionPreset::importTransitionPresetList( TransitionPresetList& rList )
         uno::Sequence< OUString > aFiles;
         xNameAccess->getByName("TransitionFiles") >>= aFiles;
 
-        for( sal_Int32 i=0; i<aFiles.getLength(); ++i )
+        for( const auto& rFile : aFiles )
         {
-            OUString aURL = comphelper::getExpandedUri(xContext, aFiles[i]);
+            OUString aURL = comphelper::getExpandedUri(xContext, rFile);
 
             bRet |= importTransitionsFile( rList,
                                            xServiceFactory,

sd/source/core/drawdoc.cxx
@@ -1110,9 +1110,9 @@ void SdDrawDocument::InitLayoutVector()
         officecfg::Office::Impress::Misc::LayoutListFiles::get(xContext) );
 
     OUString sFilename;
-    for( sal_Int32 i=0; i < aFiles.getLength(); ++i )
+    for( const auto& rFile : aFiles )
     {
-        sFilename = comphelper::getExpandedUri(xContext, aFiles[i]);
+        sFilename = comphelper::getExpandedUri(xContext, rFile);
 
         // load layout file into DOM
         Reference< XMultiServiceFactory > xServiceFactory(
@@ -1149,9 +1149,9 @@ void SdDrawDocument::InitObjectVector()
        officecfg::Office::Impress::Misc::PresObjListFiles::get(xContext) );
 
     OUString sFilename;
-    for( sal_Int32 i=0; i < aFiles.getLength(); ++i )
+    for( const auto& rFile : aFiles )
     {
-        sFilename = comphelper::getExpandedUri(xContext, aFiles[i]);
+        sFilename = comphelper::getExpandedUri(xContext, rFile);
 
         // load presentation object file into DOM
         Reference< XMultiServiceFactory > xServiceFactory(

sd/source/core/stlpool.cxx
@@ -542,14 +542,11 @@ void SdStyleSheetPool::CopyTableStyles(SdStyleSheetPool const & rSourcePool)
                 Reference< XNameAccess> xSourceNames( xSourceTableStyle, UNO_QUERY_THROW );
 
                 Sequence< OUString > aStyleNames( xSourceNames->getElementNames() );
-                OUString* pStyleNames( aStyleNames.getArray() );
 
                 Reference< XNameReplace > xTargetNames( xNewTableStyle, UNO_QUERY );
 
-                sal_Int32 nNames = aStyleNames.getLength();
-                while( nNames-- )
+                for( const OUString& aName : aStyleNames )
                 {
-                    const OUString aName( *pStyleNames++ );
                     Reference< XStyle > xSourceStyle( xSourceNames->getByName( aName ), UNO_QUERY );
                     Reference< XStyle > xTargetStyle;
                     if( xSourceStyle.is() ) try

sd/source/core/stlsheet.cxx
@@ -306,14 +306,11 @@ bool SdStyleSheet::IsUsed() const
         if( pContainer )
         {
             Sequence< Reference< XInterface > > aModifyListeners( pContainer->getElements() );
-            Reference< XInterface > *p = aModifyListeners.getArray();
-            sal_Int32 nCount = aModifyListeners.getLength();
-            while( nCount-- && !bResult )
-            {
-                Reference< XStyle > xStyle( *p++, UNO_QUERY );
-                if( xStyle.is() )
-                    bResult = xStyle->isInUse();
-            }
+            bResult = std::any_of(aModifyListeners.begin(), aModifyListeners.end(),
+                [](const Reference<XInterface>& rListener) {
+                    Reference< XStyle > xStyle( rListener, UNO_QUERY );
+                    return xStyle.is() && xStyle->isInUse();
+                });
         }
     }
     return bResult;
@@ -1248,13 +1245,11 @@ Sequence< PropertyState > SAL_CALL SdStyleSheet::getPropertyStates( const Sequen
     throwIfDisposed();
 
     sal_Int32 nCount = aPropertyName.getLength();
-    const OUString* pNames = aPropertyName.getConstArray();
 
     Sequence< PropertyState > aPropertyStateSequence( nCount );
-    PropertyState* pState = aPropertyStateSequence.getArray();
 
-    while( nCount-- )
-        *pState++ = getPropertyState( *pNames++ );
+    std::transform(aPropertyName.begin(), aPropertyName.end(), aPropertyStateSequence.begin(),
+        [this](const OUString& rName) -> PropertyState { return getPropertyState(rName); });
 
     return aPropertyStateSequence;
 }

commit dca59ca3260e2616b44e4adcdc46c28eb61857b5

tdf#131193: ODP saved as PPT opens in protected mode in MSO PP
pPara was supposed to be the first paragraph, as before the
below commit.

Regression from d458adeed0c34fae26fe7f3d6ecc9b75b431922f

Change-Id: I50b993ff8f37fd5572d60fe5d103c3cf5e636a6b

sd/source/filter/eppt/epptso.cxx
@@ -1236,7 +1236,7 @@ void PPTWriter::ImplWriteTextStyleAtom( SvStream& rOut, int nTextInstance, sal_u
             if ( nTextRulerAtomFlags & 4 )
             {
                 pRuleOut->WriteUInt16( nTabCount );
-                for ( const css::style::TabStop& rTabStop : std::as_const(pPara->maTabStop) )
+                for ( const css::style::TabStop& rTabStop : std::as_const(aTextObj.GetParagraph(0)->maTabStop) )
                 {
                     sal_uInt16 nPosition = static_cast<sal_uInt16>( ( rTabStop.Position / 4.40972 ) + nTextOfs );
                     sal_uInt16 nType;