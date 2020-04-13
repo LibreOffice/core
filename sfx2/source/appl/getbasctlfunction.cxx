d8766d23b0efcc2297c8c5169d7132f79fecb1f2
Stephan Bergmann <sbergman@redhat.com>
2020-04-13 16:47:35 +0200

    Reuse getBasctlFunction code
    Change-Id: I51881c04c071a411855da1629319bfbe735f51ed

---
+++ sfx2/source/appl/getbasctlfunction.cxx
@@ -0,0 +1,58 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#include <sal/config.h>
+
+#include <config_options.h>
+#include <osl/module.h>
+#include <osl/module.hxx>
+#include <sal/log.hxx>
+#include <tools/svlibrary.h>
+
+#include "getbasctlfunction.hxx"
+
+extern "C" { static void thisModule() {} }
+
+oslGenericFunction sfx2::getBasctlFunction(char const* name)
+{
+    osl::Module aMod;
+
+    // load basctl module
+    if (!aMod.loadRelative(
+            &thisModule,
+#if ENABLE_MERGELIBS
+            SVLIBRARY("merged")
+#else
+            SVLIBRARY("basctl")
+#endif
+        ))
+    {
+        SAL_WARN("sfx.appl", "cannot load basctl");
+        return nullptr;
+    }
+
+    // get symbol
+    auto pSymbol = aMod.getFunctionSymbol(name);
+    SAL_WARN_IF(!pSymbol, "sfx.appl", "cannot get basctl function " << name);
+    aMod.release();
+
+    return pSymbol;
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
