LibreOffice/core

commit ed8b5f2debac216243930aba0873e0d75de8d0dd

#99657# handle no fill correctly for export

sw/source/filter/ww8/wrtw8esh.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: wrtw8esh.cxx,v $
  *
- *  $Revision: 1.46 $
+ *  $Revision: 1.47 $
  *
- *  last change: $Author: cmc $ $Date: 2002-09-19 12:33:53 $
+ *  last change: $Author: cmc $ $Date: 2002-09-19 13:54:59 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -1412,7 +1412,11 @@ INT32 SwBasicEscherEx::WriteFlyFrameAttr(const SwFrmFmt& rFmt, MSO_SPT eShapeTyp
         rPropOpt.AddOpt( ESCHER_Prop_dxTextRight, 0 );
     }
 
+#if 0
     if (SFX_ITEM_SET == rFmt.GetItemState(RES_BACKGROUND, true, &pItem))
+#else
+    if (pItem = rWrt.TrueFrameBgBrush(rFmt))
+#endif
     {
         if( ((SvxBrushItem*)pItem)->GetGraphic() )
         {

sw/source/filter/ww8/wrtw8nds.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: wrtw8nds.cxx,v $
  *
- *  $Revision: 1.37 $
+ *  $Revision: 1.38 $
  *
- *  last change: $Author: cmc $ $Date: 2002-09-19 12:33:53 $
+ *  last change: $Author: cmc $ $Date: 2002-09-19 13:54:59 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -1195,11 +1195,11 @@ short SwWW8Writer::TrueFrameDirection(const SwFrmFmt &rFlyFmt) const
         {
             pItem = 0;
             const SwFmtAnchor* pAnchor = &pFlyFmt->GetAnchor();
-            if( FLY_PAGE != pAnchor->GetAnchorId() &&
-                pAnchor->GetCntntAnchor() )
+            if (FLY_PAGE != pAnchor->GetAnchorId() &&
+                pAnchor->GetCntntAnchor())
             {
-                pFlyFmt = pAnchor->GetCntntAnchor()->nNode.
-                                    GetNode().GetFlyFmt();
+                pFlyFmt =
+                    pAnchor->GetCntntAnchor()->nNode.GetNode().GetFlyFmt();
             }
             else
                 pFlyFmt = 0;
@@ -1216,6 +1216,65 @@ short SwWW8Writer::TrueFrameDirection(const SwFrmFmt &rFlyFmt) const
 
     ASSERT(nRet != FRMDIR_ENVIRONMENT, "leaving with environment direction");
     return nRet;
+}
+
+const SvxBrushItem* SwWW8Writer::GetCurrentPageBgBrush() const
+{
+    const SwFrmFmt  &rFmt = pAktPageDesc
+                    ? pAktPageDesc->GetMaster()
+                    : pDoc->GetPageDesc(0).GetMaster();
+
+    const SfxPoolItem* pItem = 0;
+    //If not set, or "no fill", get real bg
+    SfxItemState eState = rFmt.GetItemState(RES_BACKGROUND, true, &pItem);
+
+    const SvxBrushItem* pRet = (const SvxBrushItem*)pItem;
+    if (SFX_ITEM_SET != eState || (!pRet->GetGraphic() &&
+        pRet->GetColor() == COL_TRANSPARENT))
+    {
+        pRet = (const SvxBrushItem*)
+            &pDoc->GetAttrPool().GetDefaultItem(RES_BACKGROUND);
+    }
+    return pRet;
+}
+
+const SvxBrushItem* SwWW8Writer::TrueFrameBgBrush(const SwFrmFmt &rFlyFmt) const
+{
+    const SwFrmFmt *pFlyFmt = &rFlyFmt;
+    const SvxBrushItem* pRet = 0;
+
+    while (pFlyFmt)
+    {
+        //If not set, or "no fill", get real bg
+        const SfxPoolItem* pItem = 0;
+        SfxItemState eState =
+            pFlyFmt->GetItemState(RES_BACKGROUND, true, &pItem);
+        pRet = (const SvxBrushItem*)pItem;
+        if (SFX_ITEM_SET != eState || (!pRet->GetGraphic() &&
+            pRet->GetColor() == COL_TRANSPARENT))
+        {
+            pRet = 0;
+            const SwFmtAnchor* pAnchor = &pFlyFmt->GetAnchor();
+            if (FLY_PAGE != pAnchor->GetAnchorId() &&
+                pAnchor->GetCntntAnchor())
+            {
+                pFlyFmt =
+                    pAnchor->GetCntntAnchor()->nNode.GetNode().GetFlyFmt();
+            }
+            else
+                pFlyFmt = 0;
+        }
+        else
+            pFlyFmt = 0;
+    }
+
+    if (!pRet)
+        pRet = GetCurrentPageBgBrush();
+
+    ASSERT(pRet &&
+        (pRet->GetGraphic() || pRet->GetColor() != COL_TRANSPARENT),
+        "leaving with no real brush");
+    return pRet;
 }
 
 Writer& OutWW8_SwTxtNode( Writer& rWrt, SwCntntNode& rNode )

sw/source/filter/ww8/wrtww8.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: wrtww8.hxx,v $
  *
- *  $Revision: 1.35 $
+ *  $Revision: 1.36 $
  *
- *  last change: $Author: cmc $ $Date: 2002-09-19 12:33:54 $
+ *  last change: $Author: cmc $ $Date: 2002-09-19 13:54:59 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -169,6 +169,7 @@ class SwMSConvertControls;
 class WW8OleMaps;
 class SvStorageRef;
 struct WW8_PdAttrDesc;
+class SvxBrushItem;
 
 #define WWFL_ULSPACE_LIKE_SWG   0x00000001
 #define WWFL_NO_GRAF            0x00000080
@@ -524,6 +525,8 @@ public:
     void WritePostItBegin( WW8Bytes* pO = 0 );
     short TrueFrameDirection(const SwFrmFmt &rFlyFmt) const;
     short GetCurrentPageDirection() const;
+    const SvxBrushItem* GetCurrentPageBgBrush() const;
+    const SvxBrushItem* TrueFrameBgBrush(const SwFrmFmt &rFlyFmt) const;
     void OutWW8FlyFrmsInCntnt( const SwTxtNode& rNd );
     void OutWW8FlyFrm( const SwFrmFmt& rFlyFrmFmt, const Point& rNdTopLeft );
     void OutFlyFrms( const SwCntntNode& rNode );

commit bea955b2bb57ffa64e061df4225cd6bb0dd80519

tdf#132094 doc: fix export of fill in wrap-through fly frames
This builds on commit 2f13dbac060ae6af7e25ad3eff675cc859cfb3ff
by Miklos Vajna on  Fri Jun 15 08:49:46 2012 +0100
    n#325936 fix ww8 export of fly frames with transparent bg
where he wisely and cautiously says
    Regression from commit ed8b5f2d -- to be safe, reverted only
    for fly frames in headers.
because for some unknown reason, way back in 2002, commit
ed8b5f2debac216243930aba0873e0d75de8d0dd forced all
frames to specify a background fill. Typically of
course this is white, and so who notices?

Well, you notice if your frame is transparent, and
now the area fill hides something that it is over top of.
Like for example a transparent image, where the text wraps
through the image.

At first I was going to just try and revert everything.
Then I decided it likely was a difference between how
LO and MSO handled stacking/overlapping things.
After that, I was going to just make an exception for
eShapeType == mso_sptPictureFrame, but that only seems
necessary if there is something underneath. If the something
is just a background, that is handled anyway, so really
it would only be other shapes or (most importantly) text,
so the safest thing is testing wrap through, which there was
already a pre-defined variable to reuse
(and fix the spelling).

Change-Id: I9236579fa692e22205bab5a21c3f9d919f4cf24f

sw/source/filter/ww8/wrtw8esh.cxx
@@ -2026,10 +2026,12 @@ sal_Int32 SwBasicEscherEx::WriteFlyFrameAttr(const SwFrameFormat& rFormat,
 
     // SwWW8ImplReader::Read_GrafLayer() imports these as opaque
     // unconditionally, so if both are true, don't export the property.
-    bool bIsInHeader = sw::IsFlyFrameFormatInHeader(rFormat);
-    bool bIsThrought = rFormat.GetSurround().GetValue() == css::text::WrapTextMode_THROUGH;
+    const bool bIsInHeader = sw::IsFlyFrameFormatInHeader(rFormat);
+    const bool bIsThrough = rFormat.GetSurround().GetValue() == css::text::WrapTextMode_THROUGH;
 
-    if (bIsInHeader)
+    // Anything (like a transparent image) that allows text to wrap through should not force a non-transparent background,
+    // and neither should the commonly seen backgrounds anchored in headers.
+    if (bIsInHeader || bIsThrough)
     {
         std::shared_ptr<SvxBrushItem> aBrush(rFormat.makeBackgroundBrushItem());
 
@@ -2040,6 +2042,7 @@ sal_Int32 SwBasicEscherEx::WriteFlyFrameAttr(const SwFrameFormat& rFormat,
     }
     else
     {
+        // for unknown reasons, force exporting a non-transparent background on fly frames.
         std::shared_ptr<SvxBrushItem> aBrush(rWrt.TrueFrameBgBrush(rFormat));
 
         if(aBrush)
@@ -2051,7 +2054,7 @@ sal_Int32 SwBasicEscherEx::WriteFlyFrameAttr(const SwFrameFormat& rFormat,
     const SdrObject* pObj = rFormat.FindRealSdrObject();
 
     if( pObj && (pObj->GetLayer() == GetHellLayerId() ||
-        pObj->GetLayer() == GetInvisibleHellId() ) && !(bIsInHeader && bIsThrought))
+        pObj->GetLayer() == GetInvisibleHellId() ) && !(bIsInHeader && bIsThrough))
     {
         rPropOpt.AddOpt( ESCHER_Prop_fPrint, 0x200020 );
     }

sw/qa/extras/ww8export/ww8export3.cxx
@@ -300,6 +300,15 @@ DECLARE_WW8EXPORT_TEST(testTdf128608_fillStyleNoneB, "tdf128608_fillStyleNoneB.o
     CPPUNIT_ASSERT_EQUAL_MESSAGE("No fill", drawing::FillStyle_NONE, getProperty<drawing::FillStyle>(xText, "FillStyle"));
 }
 
+DECLARE_WW8EXPORT_TEST(testTdf132094_transparentPageImage, "tdf132094_transparentPageImage.doc")
+{
+    // Don't add fillstyle when none is set.
+    // Well, ok, at least make it transparent if you do uselessly set a solid color...
+    const bool bFillNone = drawing::FillStyle_NONE == getProperty<drawing::FillStyle>(image, "FillStyle");
+    const bool bTransparent = sal_Int16(0) != getProperty<sal_Int16>(image, "FillTransparence");
+    CPPUNIT_ASSERT_MESSAGE("no background fill", bTransparent || bFillNone);
+}
+
 DECLARE_WW8EXPORT_TEST(testTdf112618_textbox_no_bg, "tdf112618_textbox_no_bg.doc")
 {
     sal_uInt16 nTransparence = getProperty<sal_Int16>(getShape(2), "FillTransparence");