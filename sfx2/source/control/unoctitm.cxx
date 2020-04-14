a72463f4e52af39b0c54b0d8afcd48e8bded44ca
Mert Tumer <mert.tumer@collabora.com>
2020-04-14 18:19:09 +0300

    send state changes of .uno:NumberFormatDecimal command to LOK
    Change-Id: I7872c0a0c123865c77d3aa8937775116368f1ffa
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/92203
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Andras Timar <andras.timar@collabora.com>
    (cherry picked from commit 2f6cbb67099edde535a81e7b0a32c1418805d6d6)

--- sfx2/source/control/unoctitm.cxx
+++ sfx2/source/control/unoctitm.cxx
@@ -1153,6 +1153,7 @@ static void InterceptLOKStateChangeEvent(sal_uInt16 nSID, SfxViewFrame* pViewFra
              aEvent.FeatureURL.Path == "WrapText" ||
              aEvent.FeatureURL.Path == "NumberFormatCurrency" ||
              aEvent.FeatureURL.Path == "NumberFormatPercent" ||
+             aEvent.FeatureURL.Path == "NumberFormatDecimal" ||
              aEvent.FeatureURL.Path == "NumberFormatDate")
     {
         bool aBool;
