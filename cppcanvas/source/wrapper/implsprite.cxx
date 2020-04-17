9f3346f9902af9781431a9a0eda9095144aed6c0
Noel Grandin <noel.grandin@collabora.co.uk>
2020-04-17 15:46:19 +0200

    loplugin:flatten in cppcanvas
    Change-Id: I4303c31ad3de1ba71aa366b6dc203504b8fffd77

--- cppcanvas/source/wrapper/implsprite.cxx
+++ cppcanvas/source/wrapper/implsprite.cxx
@@ -90,21 +90,21 @@ namespace cppcanvas::internal
         {
             OSL_ENSURE( mxSprite.is(), "ImplSprite::move(): Invalid sprite");
 
-            if( mxSprite.is() )
-            {
-                rendering::ViewState    aViewState;
-                rendering::RenderState  aRenderState;
+            if( !mxSprite.is() )
+                return;
 
-                ::canvas::tools::initViewState( aViewState );
-                ::canvas::tools::initRenderState( aRenderState );
+            rendering::ViewState    aViewState;
+            rendering::RenderState  aRenderState;
 
-                ::canvas::tools::setViewStateTransform( aViewState,
-                                                        mpTransformArbiter->getTransformation() );
+            ::canvas::tools::initViewState( aViewState );
+            ::canvas::tools::initRenderState( aRenderState );
 
-                mxSprite->move( ::basegfx::unotools::point2DFromB2DPoint( rNewPos ),
-                                aViewState,
-                                aRenderState );
-            }
+            ::canvas::tools::setViewStateTransform( aViewState,
+                                                    mpTransformArbiter->getTransformation() );
+
+            mxSprite->move( ::basegfx::unotools::point2DFromB2DPoint( rNewPos ),
+                            aViewState,
+                            aRenderState );
         }
 
         void ImplSprite::transform( const ::basegfx::B2DHomMatrix& rMatrix )
@@ -135,22 +135,22 @@ namespace cppcanvas::internal
             OSL_ENSURE( mxGraphicDevice.is(), "ImplSprite::setClip(): Invalid canvas");
             OSL_ENSURE( mxSprite.is(), "ImplSprite::transform(): Invalid sprite");
 
-            if( mxSprite.is() && mxGraphicDevice.is() )
-            {
-                ::basegfx::B2DPolyPolygon   aTransformedClipPoly( rClipPoly );
+            if( !(mxSprite.is() && mxGraphicDevice.is()) )
+                return;
 
-                // extract linear part of canvas view transformation (linear means:
-                // without translational components)
-                ::basegfx::B2DHomMatrix     aViewTransform( mpTransformArbiter->getTransformation() );
-                aViewTransform.set( 0, 2, 0.0 );
-                aViewTransform.set( 1, 2, 0.0 );
+            ::basegfx::B2DPolyPolygon   aTransformedClipPoly( rClipPoly );
 
-                // transform polygon from view to device coordinate space
-                aTransformedClipPoly.transform( aViewTransform );
+            // extract linear part of canvas view transformation (linear means:
+            // without translational components)
+            ::basegfx::B2DHomMatrix     aViewTransform( mpTransformArbiter->getTransformation() );
+            aViewTransform.set( 0, 2, 0.0 );
+            aViewTransform.set( 1, 2, 0.0 );
 
-                mxSprite->clip( ::basegfx::unotools::xPolyPolygonFromB2DPolyPolygon( mxGraphicDevice,
-                                                                                     aTransformedClipPoly ) );
-            }
+            // transform polygon from view to device coordinate space
+            aTransformedClipPoly.transform( aViewTransform );
+
+            mxSprite->clip( ::basegfx::unotools::xPolyPolygonFromB2DPolyPolygon( mxGraphicDevice,
+                                                                                 aTransformedClipPoly ) );
         }
 
         void ImplSprite::setClip()
