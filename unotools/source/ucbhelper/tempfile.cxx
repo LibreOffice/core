3855d070f1b05dcc6b7ec197f5111e99ad201d38
Tor Lillqvist <tml@collabora.com>
2020-04-21 19:04:48 +0300

    Fix warning when compiling for iOS: unused parameter 'bCreateParentDirs'
    Change-Id: I3d27837ea49ad964a40f53083d90f2a287bae8a0

--- unotools/source/ucbhelper/tempfile.cxx
+++ unotools/source/ucbhelper/tempfile.cxx
@@ -138,6 +138,7 @@ static OUString ConstructTempDir_Impl( const OUString* pParent, bool bCreatePare
     }
 #else
     (void) pParent;
+    (void) bCreateParentDirs;
 #endif
 
     if ( aName.isEmpty() )
