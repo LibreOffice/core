b6533aa819d9381c62de3d0c19593fa89e182536
Xisco Fauli <xiscofauli@libreoffice.org>
2020-04-21 15:04:19 +0200

    tdf#117039: uitest: check the infobar is actually available
    Change-Id: I436c2659abd40756673634f7cd1f8e4846c03836

--- sw/qa/uitest/writer_tests5/tdf117039.py
+++ sw/qa/uitest/writer_tests5/tdf117039.py
@@ -7,9 +7,10 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 
+import time
 from uitest.framework import UITestCase
-from libreoffice.uno.propertyvalue import mkPropertyValues
-from uitest.uihelper.common import get_state_as_dict
+from com.sun.star.frame import InfobarType
+from com.sun.star.container import NoSuchElementException
 import org.libreoffice.unotest
 import pathlib
 
@@ -21,14 +22,38 @@ def get_url_for_data_file(file_name):
 class tdf117039(UITestCase):
    def test_tdf117039_preview_signed_document(self):
         writer_doc = self.ui_test.load_file(get_url_for_data_file("tdf117039.odt"))
-        document = self.ui_test.get_component()
+        controller = self.ui_test.get_component().getCurrentController()
+
+        #wait until the infobar is available
+        timeout = time.time() + 1
+        while time.time() < timeout:
+            try:
+                controller.updateInfobar("signature", "", "", InfobarType.WARNING)
+                break
+            except NoSuchElementException:
+                time.sleep(0.1)
+
+        #if the infobar is not available it would through a NoSuchElementException exception
+        controller.removeInfobar("signature")
+
         self.xUITest.executeCommand(".uno:PrintPreview")  #open print preview
         self.xUITest.executeCommand(".uno:ClosePreview")  # close print preview
 
         self.xUITest.getTopFocusWindow() #Get focus after closing preview
 
-        #verify
-        self.assertEqual(document.Text.String[0:22], "Test digital signature")
+        controller = self.ui_test.get_component().getCurrentController()
+        #wait until the infobar is available
+        timeout = time.time() + 1
+        while time.time() < timeout:
+            try:
+                controller.updateInfobar("signature", "", "", InfobarType.WARNING)
+                break
+            except NoSuchElementException:
+                time.sleep(0.1)
+
+        #if the infobar is not available it would through a NoSuchElementException exception
+        controller.removeInfobar("signature")
+
         self.ui_test.close_doc()
 
 # vim: set shiftwidth=4 softtabstop=4 expandtab:
