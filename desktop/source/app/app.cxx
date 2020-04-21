61c5685b5f7a8affba336631f605a94a0d0390ab
Thorsten Behrens <Thorsten.Behrens@CIB.de>
2020-04-21 19:06:44 +0200

    Some optimisations around crashreporter status queries
    * make dump enable status query static
    * only offer crash UI if dump is enabled
    
    cherry-pick from commit: f67e4da406f8753fb041705cf197690e7fd9a0aa
    
    Change-Id: I71aff4c6ca5a73e4d1db6163cdf8dec75b9e7538
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/156490
    Reviewed-by: Gabor Kelemen <kelemeng@ubuntu.com>
    Tested-by: Thorsten Behrens <thorsten.behrens@allotropia.de>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/175001
    Reviewed-by: Balazs Varga <balazs.varga.extern@allotropia.de>
    Tested-by: allotropia jenkins <jenkins@allotropia.de>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/190014
    Tested-by: Balazs Varga <balazs.varga@collabora.com>
    Reviewed-by: Balazs Varga <balazs.varga@collabora.com>

--- desktop/source/app/app.cxx
+++ desktop/source/app/app.cxx
@@ -2059,7 +2059,8 @@ void Desktop::OpenClients()
 #endif
 
 #if HAVE_FEATURE_BREAKPAD
-    if (officecfg::Office::Common::Misc::CrashReport::get() && CrashReporter::crashReportInfoExists())
+    if (CrashReporter::IsDumpEnable() &&
+        officecfg::Office::Common::Misc::CrashReport::get() && CrashReporter::crashReportInfoExists())
         handleCrashReport();
 #endif
 
