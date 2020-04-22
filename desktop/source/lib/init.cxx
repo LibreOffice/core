6fb259c45b9568aac6cf7ebc014a7f32c079be75
Henry Castro <hcastro@collabora.com>
2020-04-22 18:22:33 -0400

    lok: add MetricFieldUIObject class
    Add new action "VALUE" to set the value number
    for metric input controls
    
    Change-Id: I5058260c2e1562cfc6d10508d5981d185c5f2212
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/92738
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Henry Castro <hcastro@collabora.com>

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -3559,6 +3559,7 @@ static void doc_sendDialogEvent(LibreOfficeKitDocument* /*pThis*/, unsigned nWin
         const OUString sTypeAction("TYPE");
         const OUString sUpAction("UP");
         const OUString sDownAction("DOWN");
+        const OUString sValue("VALUE");
 
         try
         {
@@ -3589,6 +3590,11 @@ static void doc_sendDialogEvent(LibreOfficeKitDocument* /*pThis*/, unsigned nWin
 
                         pUIWindow->execute(sClearAction, aMap);
                         pUIWindow->execute(sTypeAction, aMap);
+                    }
+                    else if (aMap["cmd"] == "value")
+                    {
+                        aMap["VALUE"] = aMap["data"];
+                        pUIWindow->execute(sValue, aMap);
                     }
                     else
                         bIsClickAction = true;
