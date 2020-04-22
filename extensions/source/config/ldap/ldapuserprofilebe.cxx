a24d3fd715ef3a8eca84f0f6ebd3f3178faf0283
Julien Nabet <serval2412@yahoo.fr>
2020-04-22 09:55:06 +0200

    Typo kServerDefiniton->kServerDefinition (extensions/ldap)
    Change-Id: I7d6ad76de91243e5f5d278bab5df171a5924d3d2

--- extensions/source/config/ldap/ldapuserprofilebe.cxx
+++ extensions/source/config/ldap/ldapuserprofilebe.cxx
@@ -89,7 +89,7 @@ bool LdapUserProfileBe::readLdapConfiguration(
     OSL_ASSERT(context.is() && definition != nullptr && loggedOnUser != nullptr);
     const OUString kReadOnlyViewService("com.sun.star.configuration.ConfigurationAccess") ;
     const OUString kComponent("org.openoffice.LDAP/UserDirectory");
-    const OUString kServerDefiniton("ServerDefinition");
+    const OUString kServerDefinition("ServerDefinition");
     const OUString kServer("Server");
     const OUString kPort("Port");
     const OUString kBaseDN("BaseDN");
@@ -112,7 +112,7 @@ bool LdapUserProfileBe::readLdapConfiguration(
         xIface = xCfgProvider->createInstanceWithArguments(kReadOnlyViewService, aArgs);
 
         uno::Reference<container::XNameAccess > xAccess(xIface, uno::UNO_QUERY_THROW);
-        xAccess->getByName(kServerDefiniton) >>= xIface;
+        xAccess->getByName(kServerDefinition) >>= xIface;
 
         uno::Reference<container::XNameAccess > xChildAccess(xIface, uno::UNO_QUERY_THROW);
 
