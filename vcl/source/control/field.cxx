744fc572fa6a33f8f71fd32298ead145fb97449c
Henry Castro <hcastro@collabora.com>
2020-04-22 18:22:33 -0400

    lok: add MetricFieldUIObject class
    Add new action "VALUE" to set the value number
    for metric input controls
    
    Change-Id: I5058260c2e1562cfc6d10508d5981d185c5f2212
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/92738
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Henry Castro <hcastro@collabora.com>

--- vcl/source/control/field.cxx
+++ vcl/source/control/field.cxx
@@ -32,6 +32,8 @@
 #include <vcl/event.hxx>
 #include <vcl/svapp.hxx>
 #include <vcl/settings.hxx>
+#include <vcl/uitest/uiobject.hxx>
+#include <vcl/uitest/metricfielduiobject.hxx>
 
 #include <svdata.hxx>
 
@@ -621,6 +623,22 @@ sal_Int64 NumericFormatter::GetValueFromString(const OUString& rStr) const
     }
     else
         return mnLastValue;
+}
+
+// currently used by online
+void NumericFormatter::SetValueFromString(const OUString& rStr)
+{
+    sal_Int64 nValue;
+
+    if (ImplNumericGetValue(rStr, nValue, GetDecimalDigits(),
+        Application::GetSettings().GetNeutroLocaleDataWrapper()))
+    {
+        SetValue(nValue);
+    }
+    else
+    {
+        SAL_WARN("vcl", "fail to convert the value: " << rStr );
+    }
 }
 
 sal_Int64 NumericFormatter::GetValue() const
@@ -1695,6 +1713,11 @@ boost::property_tree::ptree MetricField::DumpAsPropertyTree()
     aTree.put("max", GetMax());
     aTree.put("unit", FieldUnitToString(GetUnit()));
     return aTree;
+}
+
+FactoryFunction MetricField::GetUITestFactory() const
+{
+    return MetricFieldUIObject::create;
 }
 
 MetricBox::MetricBox(vcl::Window* pParent, WinBits nWinStyle)
