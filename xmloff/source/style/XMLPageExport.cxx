LibreOffice/core

commit a0dcf961879ab644a52f801f65466756cb144b72

Hidden styles: ODF import/export
Change-Id: I1138314eba33dc8cf8d1f60e77ac419b4550bbcc

xmloff/source/style/XMLPageExport.cxx
@@ -117,6 +117,9 @@ sal_Bool XMLPageExport::exportStyle(
         GetExport().AddAttribute( XML_NAMESPACE_STYLE, XML_NAME,
                           GetExport().EncodeStyleName( sName, &bEncoded ) );
 
+        if ( rStyle->isHidden( ) && GetExport( ).getDefaultVersion( ) == SvtSaveOptions::ODFVER_LATEST )
+            GetExport( ).AddAttribute( XML_NAMESPACE_STYLE, XML_HIDDEN, "true" );
+
         if( bEncoded )
             GetExport().AddAttribute( XML_NAMESPACE_STYLE, XML_DISPLAY_NAME,
                                    sName);

xmloff/source/style/styleexp.cxx
@@ -121,6 +121,9 @@ sal_Bool XMLStyleExport::exportStyle(
     if( !rXMLFamily.isEmpty() )
         GetExport().AddAttribute( XML_NAMESPACE_STYLE, XML_FAMILY, rXMLFamily);
 
+    if ( rStyle->isHidden( ) && GetExport( ).getDefaultVersion( ) == SvtSaveOptions::ODFVER_LATEST )
+        GetExport( ).AddAttribute( XML_NAMESPACE_STYLE, XML_HIDDEN, "true" );
+
     // style:parent-style-name="..."
     OUString sParentString(rStyle->getParentStyle());
     OUString sParent;

xmloff/source/style/xmlnume.cxx
@@ -682,7 +682,7 @@ SvxXMLNumRuleExport::~SvxXMLNumRuleExport()
 }
 
 void SvxXMLNumRuleExport::exportNumberingRule(
-        const OUString& rName,
+        const OUString& rName, sal_Bool bIsHidden,
         const Reference< XIndexReplace >& rNumRule )
 {
     Reference< XPropertySet > xPropSet( rNumRule, UNO_QUERY );
@@ -702,6 +702,10 @@ void SvxXMLNumRuleExport::exportNumberingRule(
             GetExport().AddAttribute( XML_NAMESPACE_STYLE, XML_DISPLAY_NAME,
                                  rName);
     }
+
+    // style:hidden="..."
+    if ( bIsHidden && GetExport( ).getDefaultVersion( ) == SvtSaveOptions::ODFVER_LATEST )
+        GetExport( ).AddAttribute( XML_NAMESPACE_STYLE, XML_HIDDEN, "true" );
 
     // text:consecutive-numbering="..."
     sal_Bool bContNumbering = sal_False;
@@ -747,7 +751,7 @@ sal_Bool SvxXMLNumRuleExport::exportStyle( const Reference< XStyle >& rStyle )
 
     OUString sName = rStyle->getName();
 
-    exportNumberingRule( sName, xNumRule );
+    exportNumberingRule( sName, rStyle->isHidden(), xNumRule );
 
     return sal_True;
 }
@@ -784,7 +788,7 @@ void SvxXMLNumRuleExport::exportOutline()
                    nODFVersion == SvtSaveOptions::ODFVER_011 ) &&
                  GetExport().writeOutlineStyleAsNormalListStyle() )
             {
-                exportNumberingRule( sOutlineStyleName, xNumRule );
+                exportNumberingRule( sOutlineStyleName, sal_False, xNumRule );
             }
             else
             {

xmloff/source/style/xmlstyle.cxx
@@ -138,6 +138,10 @@ void SvXMLStyleContext::SetAttribute( sal_uInt16 nPrefixKey,
                 (nTmp < 0L) ? 0U : ( (nTmp > USHRT_MAX) ? USHRT_MAX
                                                         : (sal_uInt16)nTmp );
         }
+        else if( IsXMLToken( rLocalName, XML_HIDDEN ) )
+        {
+            mbHidden = rValue.toBoolean();
+        }
     }
 }
 
@@ -149,6 +153,7 @@ SvXMLStyleContext::SvXMLStyleContext(
         const uno::Reference< xml::sax::XAttributeList >&,
         sal_uInt16 nFam, sal_Bool bDefault ) :
     SvXMLImportContext( rImp, nPrfx, rLName ),
+    mbHidden( sal_False ),
     mnHelpId( UCHAR_MAX ),
     mnFamily( nFam ),
     mbValid( sal_True ),

sw/source/ui/app/docstyle.cxx
@@ -523,39 +523,36 @@ sal_Bool SwDocStyleSheet::IsHidden( ) const
 {
     sal_Bool bRet = sal_False;
 
-    if(!bPhysical)
+    SwFmt* pFmt = 0;
+    switch(nFamily)
     {
-        SwFmt* pFmt = 0;
-        switch(nFamily)
-        {
-            case SFX_STYLE_FAMILY_CHAR:
-                pFmt = rDoc.FindCharFmtByName( aName );
-                bRet = pFmt && pFmt->IsHidden( );
-                break;
+        case SFX_STYLE_FAMILY_CHAR:
+            pFmt = rDoc.FindCharFmtByName( aName );
+            bRet = pFmt && pFmt->IsHidden( );
+            break;
 
-            case SFX_STYLE_FAMILY_PARA:
-                pFmt = rDoc.FindTxtFmtCollByName( aName );
-                bRet = pFmt && pFmt->IsHidden( );
-                break;
+        case SFX_STYLE_FAMILY_PARA:
+            pFmt = rDoc.FindTxtFmtCollByName( aName );
+            bRet = pFmt && pFmt->IsHidden( );
+            break;
 
-            case SFX_STYLE_FAMILY_FRAME:
-                pFmt = rDoc.FindFrmFmtByName( aName );
-                bRet = pFmt && pFmt->IsHidden( );
-                break;
+        case SFX_STYLE_FAMILY_FRAME:
+            pFmt = rDoc.FindFrmFmtByName( aName );
+            bRet = pFmt && pFmt->IsHidden( );
+            break;
 
-            case SFX_STYLE_FAMILY_PAGE:
-                {
-                    SwPageDesc* pPgDesc = rDoc.FindPageDescByName( aName );
-                    bRet = pPgDesc && pPgDesc->IsHidden( );
-                }
-                break;
-            case SFX_STYLE_FAMILY_PSEUDO:
-                {
-                    SwNumRule* pRule = rDoc.FindNumRulePtr( aName );
-                    bRet = pRule && pRule->IsHidden( );
-                }
-            default:;
-        }
+        case SFX_STYLE_FAMILY_PAGE:
+            {
+                SwPageDesc* pPgDesc = rDoc.FindPageDescByName( aName );
+                bRet = pPgDesc && pPgDesc->IsHidden( );
+            }
+            break;
+        case SFX_STYLE_FAMILY_PSEUDO:
+            {
+                SwNumRule* pRule = rDoc.FindNumRulePtr( aName );
+                bRet = pRule && pRule->IsHidden( );
+            }
+        default:;
     }
 
     return bRet;

xmloff/inc/xmloff/xmlnume.hxx
@@ -86,7 +86,7 @@ public:
                        XMLTextListAutoStylePool *pPool,
                        sal_Bool bExportChapterNumbering = sal_True );
     void exportNumberingRule(
-            const ::rtl::OUString& rName,
+            const ::rtl::OUString& rName, sal_Bool bIsHidden,
             const ::com::sun::star::uno::Reference<
                 ::com::sun::star::container::XIndexReplace > & xNumRule );
 };

xmloff/inc/xmloff/xmlstyle.hxx
@@ -65,6 +65,7 @@ class XMLOFF_DLLPUBLIC SvXMLStyleContext : public SvXMLImportContext
     ::rtl::OUString     maAutoName;
     ::rtl::OUString     maParentName;// Will be moved to XMLPropStyle soon!!!!
     ::rtl::OUString     maFollow;   // Will be moved to XMLPropStyle soon!!!!
+    sal_Bool            mbHidden;
 
     ::rtl::OUString     maHelpFile; // Will be removed very soon!!!!
 
@@ -124,6 +125,8 @@ public:
 
     sal_Bool IsNew() const { return mbNew; }
     void SetNew( sal_Bool b ) { mbNew = b; }
+
+    sal_Bool IsHidden() const { return mbHidden; }
 
     // This method is called for every default style
     virtual void SetDefaults();

xmloff/source/draw/sdpropls.cxx
@@ -1567,7 +1567,7 @@ void XMLShapeExportPropertyMapper::handleElementItem(
                 {
                     uno::Reference< container::XIndexReplace > xNumRule( rProperty.maValue, uno::UNO_QUERY );
                     if( xNumRule.is() )
-                        const_cast<XMLShapeExportPropertyMapper*>(this)->maNumRuleExp.exportNumberingRule( maStyleName, xNumRule );
+                        const_cast<XMLShapeExportPropertyMapper*>(this)->maNumRuleExp.exportNumberingRule( maStyleName, sal_False, xNumRule );
                 }
             }
             break;

commit 2222705b67eb522223f2282e1a128247a982d4ac

xmloff: ODF export: fix style:hidden nonsense, step 1
a0dcf961879ab644a52f801f65466756cb144b72 introduced style:hidden
attribute, fix the obvious problems with this:

* as a non-ODF attribute it should use LO_EXT namespace
* import LO_EXT namespace too
* comparing == ODFVER_LATEST is wrong; it already prevents
  exporting in ODFVER_012_EXT_COMPAT case.

Don't remove export to STYLE namespace yet even though it's invalid,
because existing LO releases expect that.

Change-Id: I258202cc572a7c50f3b2620da3b9970d34fbf80b

xmloff/source/style/XMLPageExport.cxx
@@ -118,8 +118,12 @@ bool XMLPageExport::exportStyle(
         {
             uno::Any aValue = xPropSet->getPropertyValue( "Hidden" );
             bool bHidden = false;
-            if ( ( aValue >>= bHidden ) && bHidden && GetExport( ).getDefaultVersion( ) == SvtSaveOptions::ODFVER_LATEST )
-                GetExport( ).AddAttribute( XML_NAMESPACE_STYLE, XML_HIDDEN, "true" );
+            if ((aValue >>= bHidden) && bHidden
+                && GetExport().getSaneDefaultVersion() & SvtSaveOptions::ODFSVER_EXTENDED)
+            {
+                GetExport().AddAttribute(XML_NAMESPACE_LO_EXT, XML_HIDDEN, "true");
+                GetExport().AddAttribute(XML_NAMESPACE_STYLE, XML_HIDDEN, "true"); // FIXME for compatibility
+            }
         }
 
         if( bEncoded )

xmloff/source/style/styleexp.cxx
@@ -168,8 +168,12 @@ bool XMLStyleExport::exportStyle(
     {
         aAny = xPropSet->getPropertyValue( "Hidden" );
         bool bHidden = false;
-        if ( ( aAny >>= bHidden ) && bHidden && GetExport( ).getDefaultVersion( ) == SvtSaveOptions::ODFVER_LATEST )
-            GetExport( ).AddAttribute( XML_NAMESPACE_STYLE, XML_HIDDEN, "true" );
+        if ((aAny >>= bHidden) && bHidden
+            && GetExport().getSaneDefaultVersion() & SvtSaveOptions::ODFSVER_EXTENDED)
+        {
+            GetExport().AddAttribute(XML_NAMESPACE_LO_EXT, XML_HIDDEN, "true");
+            GetExport().AddAttribute(XML_NAMESPACE_STYLE, XML_HIDDEN, "true"); // FIXME for compatibility
+        }
     }
 
     // style:parent-style-name="..."

xmloff/source/style/xmlnume.cxx
@@ -654,8 +654,12 @@ void SvxXMLNumRuleExport::exportNumberingRule(
     }
 
     // style:hidden="..."
-    if ( bIsHidden && GetExport( ).getDefaultVersion( ) == SvtSaveOptions::ODFVER_LATEST )
-        GetExport( ).AddAttribute( XML_NAMESPACE_STYLE, XML_HIDDEN, "true" );
+    if (bIsHidden
+        && GetExport().getSaneDefaultVersion() & SvtSaveOptions::ODFSVER_EXTENDED)
+    {
+        GetExport().AddAttribute(XML_NAMESPACE_LO_EXT, XML_HIDDEN, "true");
+        GetExport().AddAttribute(XML_NAMESPACE_STYLE, XML_HIDDEN, "true"); // FIXME for compatibility
+    }
 
     // text:consecutive-numbering="..."
     bool bContNumbering = false;

xmloff/source/style/xmlstyle.cxx
@@ -123,6 +123,13 @@ void SvXMLStyleContext::SetAttribute( sal_uInt16 nPrefixKey,
             mbHidden = rValue.toBoolean();
         }
     }
+    else if (XML_NAMESPACE_LO_EXT == nPrefixKey)
+    {
+        if (IsXMLToken(rLocalName, XML_HIDDEN))
+        {
+            mbHidden = rValue.toBoolean();
+        }
+    }
 }
 
 