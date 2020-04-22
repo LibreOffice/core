41c11cc2f9153c5a191484ee5a8b053ba195eb4c
Aditya <adityasahu1511@gmail.com>
2020-04-22 14:33:49 +0530

    unit tests for gallery
    Tests for creation of gallery, theme, renaming, deletion
    
    Change-Id: I2bfb602342f5d6b4ed45ff2f2fb9df7601e0e6b5

---
+++ svx/CppunitTest_svx_gallery_test.mk
@@ -0,0 +1,49 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#*************************************************************************
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+#*************************************************************************
+
+$(eval $(call gb_CppunitTest_CppunitScreenShot,svx_gallery_test))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,svx_gallery_test, \
+    svx/qa/unit/gallery/test_gallery \
+))
+
+$(eval $(call gb_CppunitTest_set_include,svx_gallery_test,\
+    -I$(SRCDIR)/svx/source/inc \
+    -I$(SRCDIR)/svx/inc \
+    $$(INCLUDE) \
+))
+
+$(eval $(call gb_CppunitTest_use_libraries,svx_gallery_test, \
+	sal \
+	utl \
+	svxcore \
+	comphelper \
+))
+
+$(eval $(call gb_CppunitTest_use_vcl,svx_gallery_test))
+
+$(eval $(call gb_CppunitTest_use_sdk_api,svx_gallery_test))
+
+$(eval $(call gb_CppunitTest_use_configuration,svx_gallery_test))
+
+$(eval $(call gb_CppunitTest_use_rdb,svx_gallery_test,services))
+
+$(eval $(call gb_CppunitTest_use_ure,svx_gallery_test))
+
+
+
+
+
+
+
+
+
+# vim: set noet sw=4 ts=4:
