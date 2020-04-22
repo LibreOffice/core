272c0bd23c0b34d66969244b29308dcbe4b87d64
Xisco Fauli <xiscofauli@libreoffice.org>
2020-04-22 10:45:31 +0200

    uitest: fix test for slow executions
    See https://gerrit.libreoffice.org/c/core/+/92616
    
    In 634ce6f2d87a30b8abd2e8c67668e3bb5d87406b
    I removed the first time.sleep(2) because it wasn't needed
    for me, but it fails in slower executions.
    Put it back and use a while loop instead, so
    it can continue as soon as the value has changed.
    Also increase the timeout to 6 in the following asserts,
    the total amount of time the test was sleeping in the original
    commit
    
    Change-Id: Id4f5218856f1a1fccc0e18995261d4dfc54df0b6

--- uitest/writer_tests5/tdf114724.py
+++ uitest/writer_tests5/tdf114724.py
@@ -26,27 +26,32 @@ class tdf114724(UITestCase):
         xNavigatorPanel.executeAction("ROOT", tuple())
 
         xWriterEdit.executeAction("FOCUS", tuple())
+
+        timeout = time.time() + 2
+        while get_state_as_dict(xNavigatorPanel)["selectioncount"] != "1" and time.time() < timeout:
+            time.sleep(0.1)
+
         self.assertEqual(get_state_as_dict(xNavigatorPanel)["selectioncount"], "1")
         self.assertEqual(get_state_as_dict(xNavigatorPanel)["selectedtext"], "Headings")
 
         for _ in range(0,3):
             xWriterEdit.executeAction("TYPE", mkPropertyValues({"KEYCODE": "DOWN"}))
-            self.assertEqual(get_state_as_dict(xNavigatorPanel)["selectioncount"], "1")
 
-        timeout = time.time() + 2
+        timeout = time.time() + 6
         while get_state_as_dict(xNavigatorPanel)["selectedtext"] != "HEADING 4" and time.time() < timeout:
             time.sleep(0.1)
 
+        self.assertEqual(get_state_as_dict(xNavigatorPanel)["selectioncount"], "1")
         self.assertEqual(get_state_as_dict(xNavigatorPanel)["selectedtext"], "HEADING 4")
 
         for _ in range(0,3):
             xWriterEdit.executeAction("TYPE", mkPropertyValues({"KEYCODE": "UP"}))
-            self.assertEqual(get_state_as_dict(xNavigatorPanel)["selectioncount"], "1")
 
-        timeout = time.time() + 2
+        timeout = time.time() + 6
         while get_state_as_dict(xNavigatorPanel)["selectedtext"] != "HEADING 1" and time.time() < timeout:
             time.sleep(0.1)
 
+        self.assertEqual(get_state_as_dict(xNavigatorPanel)["selectioncount"], "1")
         self.assertEqual(get_state_as_dict(xNavigatorPanel)["selectedtext"], "HEADING 1")
 
         self.xUITest.executeCommand(".uno:Sidebar")
