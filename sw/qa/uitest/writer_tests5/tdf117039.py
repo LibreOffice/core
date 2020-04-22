29a346daa5234dcaa19eedd2d7df1898fc2a8634
Xisco Fauli <xiscofauli@libreoffice.org>
2020-04-22 12:47:54 +0200

    uitest: Check the infobar is actually available
    Change-Id: I081d41e6db9203bacd41456e722ea3b219a43ac8

--- sw/qa/uitest/writer_tests5/tdf117039.py
+++ sw/qa/uitest/writer_tests5/tdf117039.py
@@ -8,8 +8,7 @@
 #
 
 from uitest.framework import UITestCase
-from libreoffice.uno.propertyvalue import mkPropertyValues
-from uitest.uihelper.common import get_state_as_dict
+from uitest.uihelper.common import type_text
 import org.libreoffice.unotest
 import pathlib
 
@@ -21,14 +20,35 @@ def get_url_for_data_file(file_name):
 class tdf117039(UITestCase):
    def test_tdf117039_preview_signed_document(self):
         writer_doc = self.ui_test.load_file(get_url_for_data_file("tdf117039.odt"))
-        document = self.ui_test.get_component()
+
+        xWriterDoc = self.xUITest.getTopFocusWindow()
+        xWriterEdit = xWriterDoc.getChild("writer_edit")
+
+        #type some dummy test before checking the infobar
+        type_text(xWriterEdit, "Test ")
+
+        controller = self.ui_test.get_component().getCurrentController()
+        #Would throw a NoSuchElementException exception if not available
+        controller.removeInfobar("signature")
+
         self.xUITest.executeCommand(".uno:PrintPreview")  #open print preview
+
         self.xUITest.executeCommand(".uno:ClosePreview")  # close print preview
 
-        self.xUITest.getTopFocusWindow() #Get focus after closing preview
+        xWriterDoc = self.xUITest.getTopFocusWindow()
+        xWriterEdit = xWriterDoc.getChild("writer_edit")
+
+        #type some dummy test before checking the infobar
+        type_text(xWriterEdit, "Test ")
+
+        controller = self.ui_test.get_component().getCurrentController()
+        #Would throw a NoSuchElementException exception if not available
+        controller.removeInfobar("signature")
 
         #verify
-        self.assertEqual(document.Text.String[0:22], "Test digital signature")
+        document = self.ui_test.get_component()
+        self.assertEqual(document.Text.String.strip(), "Test Test Test digital signature")
+
         self.ui_test.close_doc()
 
 # vim: set shiftwidth=4 softtabstop=4 expandtab:
