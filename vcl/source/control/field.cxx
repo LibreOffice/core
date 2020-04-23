0dbf0a48bc82a60ad9eb90136c5b61e7829939f2
Henry Castro <hcastro@collabora.com>
2020-04-23 11:14:16 -0400

    lok: fix the window type of the metric field control
    Change-Id: I286be638a562e3042c1c91e40cb60a9c98938360

--- vcl/source/control/field.cxx
+++ vcl/source/control/field.cxx
@@ -1570,7 +1570,7 @@ sal_Int64 MetricFormatter::GetCorrectedValue( FieldUnit eOutUnit ) const
 }
 
 MetricField::MetricField(vcl::Window* pParent, WinBits nWinStyle)
-    : SpinField(pParent, nWinStyle)
+    : SpinField(pParent, nWinStyle, WindowType::METRICFIELD)
     , MetricFormatter(this)
 {
     Reformat();
