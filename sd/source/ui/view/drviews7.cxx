8b733f949aeccc9e04aa661d83927872530eb570
Pranam Lashkari <lpranam@collabora.com>
2020-04-24 10:35:45 +0530

    resolved: page gradient reset on reopening doc
    in mobile phone Slide background set to gradient becomes black/white after reopening
    
    Change-Id: Ib579856c3df8b89c74b381f3d79870c467802848
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/92831
    Tested-by: Jenkins
    Reviewed-by: Andras Timar <andras.timar@collabora.com>

--- sd/source/ui/view/drviews7.cxx
+++ sd/source/ui/view/drviews7.cxx
@@ -1807,9 +1807,15 @@ void DrawViewShell::SetPageProperties (SfxRequest& rReq)
                 if (SfxItemState::SET == pArgs->GetItemState(SID_FILL_GRADIENT_JSON, false, &pItem))
                 {
                     const SfxStringItem* pJSON = static_cast<const SfxStringItem*>(pItem);
-                    XFillGradientItem aGradient( XGradient::fromJSON(pJSON->GetValue()) );
+                    XFillGradientItem aGradientItem( XGradient::fromJSON(pJSON->GetValue()) );
+
+                    // MigrateItemSet guarantees unique gradient names
+                    SfxItemSet aMigrateSet( mpDrawView->GetModel()->GetItemPool(), svl::Items<XATTR_FILLGRADIENT, XATTR_FILLGRADIENT>{} );
+                    aMigrateSet.Put( aGradientItem );
+                    SdrModel::MigrateItemSet( &aMigrateSet, pTempSet.get(), mpDrawView->GetModel() );
+
                     rPageProperties.PutItem( XFillStyleItem( drawing::FillStyle_GRADIENT ) );
-                    rPageProperties.PutItem( aGradient );
+                    rPageProperties.PutItemSet( *pTempSet );
                 }
                 else
                 {
@@ -1820,8 +1826,8 @@ void DrawViewShell::SetPageProperties (SfxRequest& rReq)
                     aMigrateSet.Put( aGradientItem );
                     SdrModel::MigrateItemSet( &aMigrateSet, pTempSet.get(), mpDrawView->GetModel() );
 
-                    rPageProperties.PutItemSet( *pTempSet );
                     rPageProperties.PutItem( XFillStyleItem( drawing::FillStyle_GRADIENT ) );
+                    rPageProperties.PutItemSet( *pTempSet );
                 }
             }
             break;
