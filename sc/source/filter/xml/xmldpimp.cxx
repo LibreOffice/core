684418cb58e785773148f760191459bea1200bf4
Michael Stahl <Michael.Stahl@cib.de>
2020-04-24 16:41:08 +0200

    sc: ODF export: table:name extension element, step 1
    3119de050ef5e3561e01badeb6307ae2538d215a introduced an attribute
    table:name on table:source-cell-range, but it should be in loext
    namespace.
    
    Allow import from LO_EXT namespace, but keep export to TABLE for
    now because existing LO releases expect that.
    
    Change-Id: Ic0e8737218473338d18b00150209bcc16678fee9
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/92863
    Reviewed-by: Michael Stahl <michael.stahl@cib.de>
    Tested-by: Jenkins

--- sc/source/filter/xml/xmldpimp.cxx
+++ sc/source/filter/xml/xmldpimp.cxx
@@ -756,6 +756,7 @@ ScXMLSourceCellRangeContext::ScXMLSourceCellRangeContext( ScXMLImport& rImport,
                         pDataPilotTable->SetSourceCellRangeAddress(aSourceRangeAddress);
                 }
                 break;
+                case XML_ELEMENT( LO_EXT, XML_NAME ):
                 case XML_ELEMENT( TABLE, XML_NAME ):
                     pDataPilotTable->SetSourceRangeName(aIter.toString());
                 break;
