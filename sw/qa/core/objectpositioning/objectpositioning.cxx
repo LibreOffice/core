LibreOffice/core

commit d37096f59e7e0286e55008153591a60bab92b9e8

Related: tdf#124600 sw anchored object allow overlap: add layout
If there is an overlap, then a shift towards the bottom or towards the
right would resolve that. Word seems to always push down, do the same
for compatibility.

Otherwise, let's see if some fine-tuning will be needed for the
behavior, sadly "20.4.2.3 anchor (Anchor for Floating DrawingML Object)"
in the OOXML spec (the allowOverlap part) only says the overlapping
object has to be re-positioned, but does no go into details.

Change-Id: Ie1b4626f6d06862d617e28e0cb74838107d993d1
Reviewed-on: https://gerrit.libreoffice.org/79141
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

sw/source/core/objectpositioning/tocntntanchoredobjectposition.cxx
@@ -42,6 +42,8 @@
 #include <frmtool.hxx>
 #include <ndtxt.hxx>
 #include <dflyobj.hxx>
+#include <fmtwrapinfluenceonobjpos.hxx>
+#include <sortedobjs.hxx>
 
 using namespace objectpositioning;
 using namespace ::com::sun::star;
@@ -971,6 +973,34 @@ void SwToContentAnchoredObjectPosition::CalcPosition()
         // keep layout frame vertical position is oriented at.
         mpVertPosOrientFrame = pUpperOfOrientFrame;
 
+        // If it was requested to not overlap with already formatted objects, take care of that
+        // here.
+        bool bAllowOverlap = rFrameFormat.GetWrapInfluenceOnObjPos().GetAllowOverlap();
+        if (!bAllowOverlap)
+        {
+            // Get the list of objects.
+            const SwSortedObjs& rSortedObjs = *pAnchorFrameForVertPos->GetDrawObjs();
+            for (const auto& pAnchoredObj : rSortedObjs)
+            {
+                if (pAnchoredObj == &GetAnchoredObj())
+                {
+                    // We found ourselves, stop iterating.
+                    break;
+                }
+
+                if (!GetAnchoredObj().GetObjRect().IsOver(pAnchoredObj->GetObjRect()))
+                {
+                    // Found an already positioned object, but it doesn't overlap, ignore.
+                    continue;
+                }
+
+                // Already formatted, overlaps: resolve the conflict by shifting ourselves down.
+                SwTwips nYDiff
+                    = pAnchoredObj->GetObjRect().Bottom() - GetAnchoredObj().GetObjRect().Top();
+                aRelPos.setY(aRelPos.getY() + nYDiff + 1);
+                GetAnchoredObj().SetObjTop(nTopOfAnch + aRelPos.Y());
+            }
+        }
     }
 
     // determine 'horizontal' position

sw/qa/extras/layout/layout.cxx
@@ -3096,6 +3096,64 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf127235)
     pDoc->getIDocumentLayoutAccess().GetCurrentViewShell()->CalcLayout();
 }
 
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testShapeAllowOverlap)
+{
+// Need to find out why this fails on macOS.
+#ifndef MACOSX
+    // Create an empty document with two, intentionally overlapping shapes.
+    // Set their AllowOverlap property to false.
+    loadURL("private:factory/swriter", nullptr);
+    uno::Reference<lang::XMultiServiceFactory> xDocument(mxComponent, uno::UNO_QUERY);
+    awt::Point aPoint(1000, 1000);
+    awt::Size aSize(2000, 2000);
+    uno::Reference<drawing::XShape> xShape(
+        xDocument->createInstance("com.sun.star.drawing.RectangleShape"), uno::UNO_QUERY);
+    xShape->setPosition(aPoint);
+    xShape->setSize(aSize);
+    uno::Reference<drawing::XDrawPageSupplier> xDrawPageSupplier(xDocument, uno::UNO_QUERY);
+    uno::Reference<beans::XPropertySet> xShapeProperties(xShape, uno::UNO_QUERY);
+    xShapeProperties->setPropertyValue("AllowOverlap", uno::makeAny(false));
+    xShapeProperties->setPropertyValue("AnchorType",
+                                       uno::makeAny(text::TextContentAnchorType_AT_CHARACTER));
+    xDrawPageSupplier->getDrawPage()->add(xShape);
+
+    aPoint = awt::Point(2000, 2000);
+    xShape.set(xDocument->createInstance("com.sun.star.drawing.RectangleShape"), uno::UNO_QUERY);
+    xShape->setPosition(aPoint);
+    xShape->setSize(aSize);
+    xShapeProperties.set(xShape, uno::UNO_QUERY);
+    xShapeProperties->setPropertyValue("AllowOverlap", uno::makeAny(false));
+    xShapeProperties->setPropertyValue("AnchorType",
+                                       uno::makeAny(text::TextContentAnchorType_AT_CHARACTER));
+    xDrawPageSupplier->getDrawPage()->add(xShape);
+
+    // Now verify that the rectangle of the anchored objects don't overlap.
+    SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument*>(mxComponent.get());
+    CPPUNIT_ASSERT(pTextDoc);
+    SwDoc* pDoc = pTextDoc->GetDocShell()->GetDoc();
+    SwRootFrame* pLayout = pDoc->getIDocumentLayoutAccess().GetCurrentLayout();
+    SwFrame* pPageFrame = pLayout->GetLower();
+    SwFrame* pBodyFrame = pPageFrame->GetLower();
+    SwFrame* pTextFrame = pBodyFrame->GetLower();
+    CPPUNIT_ASSERT(pTextFrame->GetDrawObjs());
+    SwSortedObjs& rObjs = *pTextFrame->GetDrawObjs();
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(2), rObjs.size());
+    SwAnchoredObject* pFirst = rObjs[0];
+    SwAnchoredObject* pSecond = rObjs[1];
+    // Without the accompanying fix in place, this test would have failed: the layout dump was
+    // <bounds left="1984" top="1984" width="1137" height="1137"/>
+    // <bounds left="2551" top="2551" width="1137" height="1137"/>
+    // so there was a clear vertical overlap. (Allow for 1px tolerance.)
+    OString aMessage("Unexpected overlap: first shape's bottom is ");
+    aMessage += OString::number(pFirst->GetObjRect().Bottom());
+    aMessage += ", second shape's top is ";
+    aMessage += OString::number(pSecond->GetObjRect().Top());
+    CPPUNIT_ASSERT_MESSAGE(aMessage.getStr(),
+                           std::abs(pFirst->GetObjRect().Bottom() - pSecond->GetObjRect().Top())
+                               < 15);
+#endif
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

commit a19f2a360da6e8c6b3ae0ad9d90377dd2061b0ce

tdf#131729 sw: handle no drawing objects when calculating overlap of objects
Regression from commit d37096f59e7e0286e55008153591a60bab92b9e8
(Related: tdf#124600 sw anchored object allow overlap: add layout,
2019-09-19), we assumed that the anchor frame always has draw objects,
but that may not be the case.

That happens when dragging a to-character anchored object around, before
the object is added to its anchor.

Change-Id: I1271a6e498553838c3851864b7965a1ba28de663

sw/CppunitTest_sw_core_objectpositioning.mk
@@ -1,0 +1,73 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#*************************************************************************
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+#*************************************************************************
+
+$(eval $(call gb_CppunitTest_CppunitTest,sw_core_objectpositioning))
+
+$(eval $(call gb_CppunitTest_use_common_precompiled_header,sw_core_objectpositioning))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,sw_core_objectpositioning, \
+    sw/qa/core/objectpositioning/objectpositioning \
+))
+
+$(eval $(call gb_CppunitTest_use_libraries,sw_core_objectpositioning, \
+    comphelper \
+    cppu \
+    cppuhelper \
+    sal \
+    sfx \
+    svxcore \
+    sw \
+    test \
+    unotest \
+    utl \
+    vcl \
+    svt \
+    tl \
+    svl \
+))
+
+$(eval $(call gb_CppunitTest_use_externals,sw_core_objectpositioning,\
+    boost_headers \
+    libxml2 \
+))
+
+$(eval $(call gb_CppunitTest_set_include,sw_core_objectpositioning,\
+    -I$(SRCDIR)/sw/inc \
+    -I$(SRCDIR)/sw/source/core/inc \
+    -I$(SRCDIR)/sw/source/uibase/inc \
+    -I$(SRCDIR)/sw/qa/inc \
+    $$(INCLUDE) \
+))
+
+$(eval $(call gb_CppunitTest_use_api,sw_core_objectpositioning,\
+	udkapi \
+	offapi \
+	oovbaapi \
+))
+
+$(eval $(call gb_CppunitTest_use_ure,sw_core_objectpositioning))
+$(eval $(call gb_CppunitTest_use_vcl,sw_core_objectpositioning))
+
+$(eval $(call gb_CppunitTest_use_rdb,sw_core_objectpositioning,services))
+
+$(eval $(call gb_CppunitTest_use_custom_headers,sw_core_objectpositioning,\
+    officecfg/registry \
+))
+
+$(eval $(call gb_CppunitTest_use_configuration,sw_core_objectpositioning))
+
+$(eval $(call gb_CppunitTest_use_uiconfigs,sw_core_objectpositioning, \
+    modules/swriter \
+))
+
+$(eval $(call gb_CppunitTest_use_more_fonts,sw_core_objectpositioning))
+
+# vim: set noet sw=4 ts=4:

sw/Module_sw.mk
@@ -113,6 +113,7 @@ $(eval $(call gb_Module_add_slowcheck_targets,sw,\
     CppunitTest_sw_core_layout \
     CppunitTest_sw_core_frmedt \
     CppunitTest_sw_core_txtnode \
+    CppunitTest_sw_core_objectpositioning \
 ))
 
 ifneq ($(DISABLE_GUI),TRUE)

sw/source/core/objectpositioning/tocntntanchoredobjectposition.cxx
@@ -1112,8 +1112,13 @@ void SwToContentAnchoredObjectPosition::CalcOverlap(const SwTextFrame* pAnchorFr
     }
 
     // Get the list of objects.
-    const SwSortedObjs& rSortedObjs = *pAnchorFrameForVertPos->GetDrawObjs();
-    for (const auto& pAnchoredObj : rSortedObjs)
+    auto pSortedObjs = pAnchorFrameForVertPos->GetDrawObjs();
+    if (!pSortedObjs)
+    {
+        return;
+    }
+
+    for (const auto& pAnchoredObj : *pSortedObjs)
     {
         if (pAnchoredObj == &GetAnchoredObj())
         {

sw/qa/core/objectpositioning/objectpositioning.cxx
@@ -1,0 +1,53 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <swmodeltestbase.hxx>
+
+#include <comphelper/classids.hxx>
+#include <svtools/embedhlp.hxx>
+#include <svx/svdpage.hxx>
+#include <tools/globname.hxx>
+#include <unotest/bootstrapfixturebase.hxx>
+#include <vcl/gdimtf.hxx>
+
+#include <wrtsh.hxx>
+#include <fmtanchr.hxx>
+#include <IDocumentDrawModelAccess.hxx>
+#include <drawdoc.hxx>
+#include <dcontact.hxx>
+
+static char const DATA_DIRECTORY[] = "/sw/qa/core/objectpositioning/data/";
+
+/// Covers sw/source/core/objectpositioning/ fixes.
+class SwCoreObjectpositioningTest : public SwModelTestBase
+{
+};
+
+CPPUNIT_TEST_FIXTURE(SwCoreObjectpositioningTest, testOverlapCrash)
+{
+    // Load a document with 2 images.
+    load(DATA_DIRECTORY, "overlap-crash.odt");
+
+    // Change their anchor type to to-char.
+    uno::Reference<beans::XPropertySet> xShape1(getShape(1), uno::UNO_QUERY);
+    xShape1->setPropertyValue("AnchorType", uno::makeAny(text::TextContentAnchorType_AT_CHARACTER));
+    uno::Reference<beans::XPropertySet> xShape2(getShape(1), uno::UNO_QUERY);
+    xShape2->setPropertyValue("AnchorType", uno::makeAny(text::TextContentAnchorType_AT_CHARACTER));
+
+    // Insert a new paragraph at the start.
+    SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument*>(mxComponent.get());
+    SwDocShell* pDocShell = pTextDoc->GetDocShell();
+    SwWrtShell* pWrtShell = pDocShell->GetWrtShell();
+    // Without the accompanying fix in place, this test would have crashed.
+    pWrtShell->SplitNode();
+}
+
+CPPUNIT_PLUGIN_IMPLEMENT();
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */