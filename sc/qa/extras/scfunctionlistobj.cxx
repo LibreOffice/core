716655820d69a0d6aaa2714cb4f12bae1aa2b862
Eike Rathke <erack@redhat.com>
2020-04-27 23:12:42 +0200

    Resolves: tdf#127831 implement RAND.NV() and RANDBETWEEN.NV() non-volatile
    Same as RAND() and RANDBETWEEN() but not recalculating on every
    change, just the normal expression recalculation.
    
    Change-Id: I8ba7099125e487a78bd3d91db8b666c2f36b22fd
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/92994
    Tested-by: Jenkins
    Reviewed-by: Eike Rathke <erack@redhat.com>

--- sc/qa/extras/scfunctionlistobj.cxx
+++ sc/qa/extras/scfunctionlistobj.cxx
@@ -82,7 +82,7 @@ private:
 ScFunctionListObj::ScFunctionListObj()
     : CalcUnoApiTest("/sc/qa/extras/testdocuments")
     , XElementAccess(cppu::UnoType<uno::Sequence<beans::PropertyValue>>::get())
-    , XIndexAccess(393)
+    , XIndexAccess(395)
     , XNameAccess("IF")
     , XServiceInfo("stardiv.StarCalc.ScFunctionListObj", "com.sun.star.sheet.FunctionDescriptions")
 {
