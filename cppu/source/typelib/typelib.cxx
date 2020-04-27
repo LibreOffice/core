LibreOffice/core

commit ef513fd4b049b214a03fbe6e62a5ea43680a7a9b

remove unnecessary use of OString::getStr
Change-Id: I0490efedf459190521f4339854b3394d57765fdb
Reviewed-on: https://gerrit.libreoffice.org/38058
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

cppu/source/typelib/typelib.cxx
@@ -530,9 +530,7 @@ bool complete(typelib_TypeDescription ** ppTypeDescr, bool initTables) {
         }
         else
         {
-            OString aStr(
-                rtl::OUStringToOString( (*ppTypeDescr)->pTypeName, RTL_TEXTENCODING_ASCII_US ) );
-            SAL_INFO( "cppu.typelib", "type cannot be completed: " <<  aStr.getStr() );
+            SAL_INFO( "cppu.typelib", "type cannot be completed: " <<  (*ppTypeDescr)->pTypeName );
             return false;
         }
     }

commit 7358699f9b5ac4010fc62bb4cc84b2cf721d9395

Fix printing rtl_uString*
...broken with ef513fd4b049b214a03fbe6e62a5ea43680a7a9b "remove unnecessary use
of OString::getStr"

Change-Id: I85f5ccb6c5114ea5e3eab43a3c1821292cf4e994

cppu/source/typelib/typelib.cxx
@@ -497,7 +497,9 @@ bool complete(typelib_TypeDescription ** ppTypeDescr, bool initTables) {
         }
         else
         {
-            SAL_INFO( "cppu.typelib", "type cannot be completed: " <<  (*ppTypeDescr)->pTypeName );
+            SAL_INFO(
+                "cppu.typelib",
+                "type cannot be completed: " << OUString::unacquired(&(*ppTypeDescr)->pTypeName));
             return false;
         }
     }