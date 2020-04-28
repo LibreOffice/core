3000646b2b76d8d6c6c68b43932ba12018131663
Xisco Fauli <xiscofauli@libreoffice.org>
2020-04-28 14:42:29 +0200

    tdf#132282: Revert fix for tdf#131554
    912217285b3058efa54c2336f91fda4efdad6ff0 fixed the
    root cause of tdf#131554 and
    69b83dc2d3014dd9b18402534e15c937dc082464 is no longer needed
    The unittest still passes
    
    Change-Id: I7c723b0c3cc2b56022978bbeb8bf6b3f6f93f1c5
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/93063
    Tested-by: Jenkins
    Reviewed-by: Xisco Faulí <xiscofauli@libreoffice.org>
    Signed-off-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- sd/qa/unit/export-tests-ooxml2.cxx
+++ sd/qa/unit/export-tests-ooxml2.cxx
@@ -183,6 +183,7 @@ public:
     void testTdf127379();
     void testTdf98603();
     void testTdf131554();
+    void testTdf132282();
 
     CPPUNIT_TEST_SUITE(SdOOXMLExportTest2);
 
@@ -284,6 +285,7 @@ public:
     CPPUNIT_TEST(testTdf127379);
     CPPUNIT_TEST(testTdf98603);
     CPPUNIT_TEST(testTdf131554);
+    CPPUNIT_TEST(testTdf132282);
 
     CPPUNIT_TEST_SUITE_END();
 
@@ -2642,6 +2644,18 @@ void SdOOXMLExportTest2::testTdf131554()
     uno::Reference<drawing::XShape> xShape(getShapeFromPage(1, 0, xDocShRef), uno::UNO_QUERY);
     CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(5622), xShape->getPosition().X);
     CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(13251), xShape->getPosition().Y);
+}
+
+void SdOOXMLExportTest2::testTdf132282()
+{
+    ::sd::DrawDocShellRef xDocShRef = loadURL(m_directories.getURLFromSrc("sd/qa/unit/data/pptx/tdf132282.pptx"), PPTX);
+    xDocShRef = saveAndReload( xDocShRef.get(), PPTX );
+    uno::Reference<drawing::XShape> xShape(getShapeFromPage(0, 0, xDocShRef), uno::UNO_QUERY);
+    // Without the fix in place, the position would be 0,0, height = 1 and width = 1
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(1736), xShape->getPosition().X);
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(763), xShape->getPosition().Y);
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(30523), xShape->getSize().Width);
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(2604), xShape->getSize().Height);
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(SdOOXMLExportTest2);
