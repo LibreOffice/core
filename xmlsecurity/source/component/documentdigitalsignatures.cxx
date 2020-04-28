2f16880998e8b396231b8dc6e41206b93aedc65b
Michael Stahl <Michael.Stahl@cib.de>
2020-04-28 18:04:14 +0200

    officecfg,unotools,cui: add ODF 1.2 Extended / ODF 1.3 versions
    ... to configuration and UI.
    
    The new default is ODF 1.3 Extended, which is now ODFVER_LATEST and
    stored as value "3" in configuration.
    
    Adapt a few places related to DocumentDigitalSignatures etc. to new
    default.
    
    Change-Id: I420da4f7787cc864c6bd88470d61b146b9399aa1
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/93177
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@cib.de>

--- xmlsecurity/source/component/documentdigitalsignatures.cxx
+++ xmlsecurity/source/component/documentdigitalsignatures.cxx
@@ -218,7 +218,7 @@ public:
 
 DocumentDigitalSignatures::DocumentDigitalSignatures( const Reference< XComponentContext >& rxCtx ):
     mxCtx(rxCtx),
-    m_sODFVersion(ODFVER_012_TEXT),
+    m_sODFVersion(ODFVER_013_TEXT),
     m_nArgumentsCount(0),
     m_bHasDocumentSignature(false)
 {
