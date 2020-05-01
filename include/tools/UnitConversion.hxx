fab4420f23ae77f8c7266d4b4d44313f2e3129d8
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2020-05-01 17:20:26 +0200

    move unit conversions to UnitConversion, add convertPointToMm100
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/93332
    Tested-by: Tomaž Vajngerl <quikee@gmail.com>
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>
    (cherry picked from commit 09e28008376e2f2a187067409d3076e81eba022e)
    
    Change-Id: I3e5a4632e9809562885c1e0ec5c5262acec145d0
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/95783
    Tested-by: Tomaž Vajngerl <quikee@gmail.com>
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

--- include/tools/UnitConversion.hxx
+++ include/tools/UnitConversion.hxx
@@ -10,6 +10,18 @@
 
 #pragma once
 
+constexpr sal_Int64 convertTwipToMm100(sal_Int64 n)
+{
+    return (n >= 0) ? (n * 127 + 36) / 72 : (n * 127 - 36) / 72;
+}
+
+constexpr sal_Int64 convertPointToMm100(sal_Int64 n) { return convertTwipToMm100(n * 20); }
+
+constexpr sal_Int64 convertMm100ToTwip(sal_Int64 n)
+{
+    return (n >= 0) ? (n * 72 + 63) / 127 : (n * 72 - 63) / 127;
+}
+
 // Convert PPT's "master unit" (1/576 inch) to twips
 constexpr sal_Int64 convertMasterUnitToTwip(sal_Int64 n) { return n * 2540.0 / 576.0; }
 
