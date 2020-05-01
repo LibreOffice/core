LibreOffice/core

commit 65404e2c72ea2d5d3f6c074121005b8b9369e7fb

Move is*Locked methods to object shell
They depend on the model, not the view.
Follow-up to 523922ee9d033fd304d2b50a72b76853ddcfbcdf

Change-Id: I429bc25b8dd4f04ebf62eddd5ef455b5161e925d
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/91867
Tested-by: Jenkins
Reviewed-by: Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>

sw/source/uibase/shells/annotsh.cxx
@@ -963,7 +963,7 @@ void SwAnnotationShell::StateClpbrd(SfxItemSet &rSet)
             }
             case SID_COPY:
             {
-                if (!pOLV->HasSelection() || rView.isContentExtractionLocked())
+                if (!pOLV->HasSelection() || GetObjectShell()->isContentExtractionLocked())
                     rSet.DisableItem( nWhich );
                 break;
             }

include/sfx2/objsh.hxx
@@ -586,6 +586,10 @@ public:
                                     const css::uno::Sequence<OUString>& rExceptions = css::uno::Sequence<OUString>());
 
     bool isEditDocLocked();
+    bool isContentExtractionLocked();
+    bool isExportLocked();
+    bool isPrintLocked();
+    bool isSaveLocked();
 
     // The functions from SvPersist
     void            EnableSetModified( bool bEnable = true );

include/sfx2/viewsh.hxx
@@ -289,10 +289,6 @@ public:
 
     void                        AddRemoveClipboardListener( const css::uno::Reference < css::datatransfer::clipboard::XClipboardListener>&, bool );
     css::uno::Reference< css::datatransfer::clipboard::XClipboardNotifier > GetClipboardNotifier() const;
-    bool isContentExtractionLocked();
-    bool isExportLocked();
-    bool isPrintLocked();
-    bool isSaveLocked();
 
     SAL_DLLPRIVATE SfxInPlaceClient* GetUIActiveIPClient_Impl() const;
     SAL_DLLPRIVATE void AddContextMenuInterceptor_Impl( const css::uno::Reference < css::ui::XContextMenuInterceptor >& xInterceptor );

sc/source/ui/docshell/docsh4.cxx
@@ -2075,7 +2075,7 @@ void ScDocShell::GetState( SfxItemSet &rSet )
 
             case SID_SHARE_DOC:
                 {
-                    if ( IsReadOnly() || GetBestViewShell()->isExportLocked() )
+                    if ( IsReadOnly() || GetObjectShell()->isExportLocked() )
                     {
                         rSet.DisableItem( nWhich );
                     }

sc/source/ui/drawfunc/chartsh.cxx
@@ -73,7 +73,7 @@ void ScChartShell::GetExportAsGraphicState( SfxItemSet& rSet )
             bEnable = true;
     }
 
-    if (GetViewShell()->isExportLocked())
+    if (GetObjectShell()->isExportLocked())
         bEnable = false;
 
     if( !bEnable )

sc/source/ui/drawfunc/drawsh2.cxx
@@ -156,7 +156,8 @@ void ScDrawShell::GetDrawFuncState( SfxItemSet& rSet )      // disable functions
         rSet.DisableItem( SID_FLIP_VERTICAL );
     }
 
-    if (pViewData->GetViewShell()->isContentExtractionLocked())
+
+    if (GetObjectShell()->isContentExtractionLocked())
     {
         rSet.DisableItem(SID_COPY);
         rSet.DisableItem(SID_CUT);

sc/source/ui/drawfunc/drtxtob.cxx
@@ -465,7 +465,7 @@ void ScDrawTextObjectBar::GetState( SfxItemSet& rSet )
             rSet.DisableItem( SID_THESAURUS );
     }
 
-    if (pViewData->GetViewShell()->isContentExtractionLocked())
+    if (GetObjectShell()->isContentExtractionLocked())
     {
         rSet.DisableItem(SID_COPY);
         rSet.DisableItem(SID_CUT);

sc/source/ui/drawfunc/graphsh.cxx
@@ -145,7 +145,7 @@ void ScGraphicShell::GetExternalEditState( SfxItemSet& rSet )
             bEnable = true;
     }
 
-    if (GetViewShell()->isExportLocked())
+    if (GetObjectShell()->isExportLocked())
         bEnable = false;
 
     if( !bEnable )
@@ -307,7 +307,7 @@ void ScGraphicShell::GetSaveGraphicState(SfxItemSet &rSet)
             bEnable = true;
     }
 
-    if (GetViewShell()->isExportLocked())
+    if (GetObjectShell()->isExportLocked())
         bEnable = false;
 
     if( !bEnable )

commit eb6a2661e8f014167810041416e5d41cb287b5ba

tdf#132603: fix crash when selecting part of a comment
See bt here:
https://bugs.documentfoundation.org/attachment.cgi?id=160206

Regression from:
https://cgit.freedesktop.org/libreoffice/core/commit/?id=65404e2c72ea2d5d3f6c074121005b8b9369e7fb
author	Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>	2020-04-08 08:54:21 +0200
committer	Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>	2020-04-09 11:14:15 +0200
commit	65404e2c72ea2d5d3f6c074121005b8b9369e7fb (patch)
tree	15eeeb5b634c461611893dbe3b49d689ef71f5c4
parent	40d83914d43f60a196dfabddea0b52e2046b333a (diff)
Move is*Locked methods to object shell
They depend on the model, not the view.
Follow-up to 523922ee9d033fd304d2b50a72b76853ddcfbcdf

Change-Id: I9267e856a0f0fb22778dcd95974bb4217fd3712f

sw/source/uibase/shells/annotsh.cxx
@@ -963,7 +963,8 @@ void SwAnnotationShell::StateClpbrd(SfxItemSet &rSet)
             }
             case SID_COPY:
             {
-                if (!pOLV->HasSelection() || GetObjectShell()->isContentExtractionLocked())
+                SfxObjectShell* pObjectShell = GetObjectShell();
+                if (!pOLV->HasSelection() || (pObjectShell && pObjectShell->isContentExtractionLocked()) )
                     rSet.DisableItem( nWhich );
                 break;
             }