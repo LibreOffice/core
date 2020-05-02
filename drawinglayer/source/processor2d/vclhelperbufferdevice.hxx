1a86c041cd9eaefd5869df984c780b84f847e6fd
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2020-05-02 19:29:49 +0200

    pragma once in vclhelperbufferdevice.hxx
    Change-Id: I12e53849fa23496799ba5e45fc0077cb9b54048c
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/93361
    Tested-by: Jenkins
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

--- drawinglayer/source/processor2d/vclhelperbufferdevice.hxx
+++ drawinglayer/source/processor2d/vclhelperbufferdevice.hxx
@@ -17,16 +17,11 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_DRAWINGLAYER_SOURCE_PROCESSOR2D_VCLHELPERBUFFERDEVICE_HXX
-#define INCLUDED_DRAWINGLAYER_SOURCE_PROCESSOR2D_VCLHELPERBUFFERDEVICE_HXX
+#pragma once
 
 #include <vcl/virdev.hxx>
 
-
-// predefines
-
 namespace basegfx { class B2DRange; }
-
 
 // support methods for vcl direct gradient rendering
 
@@ -53,8 +48,5 @@ namespace drawinglayer
         VirtualDevice& getTransparence();
     };
 } // end of namespace drawinglayer
-
-
-#endif // INCLUDED_DRAWINGLAYER_SOURCE_PROCESSOR2D_VCLHELPERBUFFERDEVICE_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
