689680094459140f4bc662c4767e98fa0226eb44
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2020-05-02 23:35:27 +0200

    pragma once in helperwrongspellrenderer.hxx
    Change-Id: I25aac069b9212824fee50240a1b6682bf7e6f0f7

--- drawinglayer/source/processor2d/helperwrongspellrenderer.hxx
+++ drawinglayer/source/processor2d/helperwrongspellrenderer.hxx
@@ -17,23 +17,18 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_DRAWINGLAYER_SOURCE_PROCESSOR2D_HELPERWRONGSPELLRENDERER_HXX
-#define INCLUDED_DRAWINGLAYER_SOURCE_PROCESSOR2D_HELPERWRONGSPELLRENDERER_HXX
-
-
-// predefines
+#pragma once
 
 class OutputDevice;
 
-namespace drawinglayer { namespace primitive2d {
+namespace drawinglayer::primitive2d {
     class WrongSpellPrimitive2D;
-}}
+}
 
 namespace basegfx {
     class B2DHomMatrix;
     class BColorModifierStack;
 }
-
 
 // support WrongSpell rendering using VCL from primitives due to VCLs nice
 // and fast solution with wavelines
@@ -47,8 +42,5 @@ namespace drawinglayer
         const basegfx::BColorModifierStack& rBColorModifierStack);
 
 } // end of namespace drawinglayer
-
-
-#endif // INCLUDED_DRAWINGLAYER_SOURCE_PROCESSOR2D_HELPERWRONGSPELLRENDERER_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
