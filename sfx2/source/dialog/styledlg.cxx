99fa8e8aa9c2b256e25004d894b2d14f51b32b5d
Caolán McNamara <caolanm@redhat.com>
2020-05-03 19:46:13 +0100

    use SfxStyleSearchBits::All for GenerateUnusedName
    use in SfxManageStyleSheetPage is definitely with
    SfxStyleSearchBits::All applied. It seems almost certain that
    use in SwDocShell::ExecStyleSheet would prefer that
    
    Change-Id: I245c3f02bcce87f124bf0e507b59738a6dff34d7

--- sfx2/source/dialog/styledlg.cxx
+++ sfx2/source/dialog/styledlg.cxx
@@ -113,7 +113,7 @@ OUString SfxStyleDialogController::GenerateUnusedName(SfxStyleSheetBasePool &rPo
     OUString aNo(SfxResId(STR_NONAME));
     sal_uInt16 i = 1;
     OUString aNoName = aNo + OUString::number(i);
-    while (rPool.Find(aNoName, eFam, rPool.GetSearchMask()))
+    while (rPool.Find(aNoName, eFam))
     {
         ++i;
         aNoName = aNo + OUString::number(i);
