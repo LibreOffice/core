9febd9d179d0a61ca6a28b0fd82b9f86d95eb540
Justin Luth <justin.luth@collabora.com>
2020-05-04 13:16:32 +0300

    tdf#105478 sw layout: treat minHeight as "do not split row"
    Already, if minHeight is greater than the content of the row,
    then the row will "stay intact" and not split just because there
    is space for part of it on the preceeding page. However, if the
    content is greater than minHeight, it can split anywhere and
    moveBwd - since LO 5.3.
    
    At least for MS compatibility, this needs to not be split.
    
    Note: I am just re-using TABLE_ROW_KEEP to see if this is
    MS format. If that is a terrible idea, let me know.
    
    Note2: I could see changing this for ODT as well,
    especially since this matches pre-5.3 behaviour,
    and will help at design-time for MS interoperability.
    
    After all, it seems a bit weird for a "partially empty"
    cell to not split - just because it must be larger than
    its content - while a cell with more content can be
    split. However, we do have a "do not split row" option
    for those who would want that, so it is not essential
    to see-saw back and forth on this implementation.
    
    Change-Id: Id15c44da2e7c38f6d552ffe32f92ab1c6b3c3349

--- sw/source/core/layout/tabfrm.cxx
+++ sw/source/core/layout/tabfrm.cxx
@@ -1091,7 +1091,22 @@ bool SwTabFrame::Split( const SwTwips nCutPos, bool bTryToSplit, bool bTableRowK
         m_pTable->SetRowsToRepeat(0);
         return false;
     }
-    else if ( !GetIndPrev() && nRepeat == nRowCount )
+
+    // Minimum row height has the same force as "do not split row" (as long as it fits on one page)
+    //     [ for Word compatibility only - reusing existing flag TABLE_ROW_KEEP to only apply this logic to MS formats ]
+    if ( bSplitRowAllowed && bTryToSplit && !pRow->IsRowSpanLine()
+         && GetFormat()->GetDoc()->GetDocumentSettingManager().get(DocumentSettingId::TABLE_ROW_KEEP) )
+    {
+        const SwFormatFrameSize &rSz = pRow->GetFormat()->GetFrameSize();
+        const sal_Int32 nMinHeight = rSz.GetHeightSizeType() == SwFrameSize::Minimum ? rSz.GetHeight() : 0;
+        if ( nMinHeight > nRemainingSpaceForLastRow )
+        {
+            const sal_Int32 nFullPageHeight = FindPageFrame()->getFramePrintArea().Height();
+            bSplitRowAllowed = nMinHeight > nFullPageHeight;
+        }
+    }
+
+    if ( !GetIndPrev() && nRepeat == nRowCount )
     {
         // Second case: The first non-headline row does not fit to the page.
         // If it is not allowed to be split, or it contains a sub-row that
