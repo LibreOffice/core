LibreOffice/core

commit 2670e980c7dbadbdc20ff23d57ad892951edc254

tdf#132097: fix Find Previous with direction columns crashes in specific case
See bt here:
https://bugs.documentfoundation.org/attachment.cgi?id=159553

nBlockPos vector is created with nLastCol + 1 elements,
so if we click on a cell in a column which is after last value,
the search will crash

Change-Id: I9de688a8a80a45c3e5ddea6d2f461926e9777052
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/92165
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sc/source/core/data/table6.cxx
@@ -339,8 +339,8 @@ bool ScTable::Search(const SvxSearchItem& rSearchItem, SCCOL& rCol, SCROW& rRow,
     bool bSkipFiltered = !rSearchItem.IsSearchFiltered();
     bool bSearchNotes = (rSearchItem.GetCellType() == SvxSearchCellType::NOTE);
     // We need to cache sc::ColumnBlockConstPosition per each column.
-    std::vector< sc::ColumnBlockConstPosition > blockPos( nLastCol + 1 );
-    for( SCCOL i = 0; i <= nLastCol; ++i )
+    std::vector< sc::ColumnBlockConstPosition > blockPos( nLastCol + 2 );
+    for( SCCOL i = 0; i <= nLastCol+1; ++i )
         aCol[ i ].InitBlockPosition( blockPos[ i ] );
     if (!bAll && rSearchItem.GetBackward())
     {

commit 6820c52330c25cc8a2407312a8b0b25cdc33788a

tdf#132783: fix Calc CRASH when Ctrl+F
See bt here:
https://bugs.documentfoundation.org/attachment.cgi?id=160468

fix inspired by:
    785 void ScTable::GetDataArea( SCCOL& rStartCol, SCROW& rStartRow, SCCOL& rEndCol, SCROW& rEndRow,
    786                            bool bIncludeOld, bool bOnlyDown ) const
    787 {
    788     // return the smallest area containing at least all contiguous cells having data. This area
    789     // is a square containing also empty cells. It may shrink or extend the area given as input
    790     // Flags as modifiers:
    791     //
    792     //     bIncludeOld = true ensure that the returned area contains at least the initial area,
    793     //                   independently of the emptiness of rows / columns (i.e. does not allow shrinking)
    794     //     bOnlyDown = true means extend / shrink the inputted area only down, i.e modify only rEndRow
    795
    796     rStartCol = std::min<SCCOL>( rStartCol, aCol.size()-1 );
    797     rEndCol   = std::min<SCCOL>( rEndCol,   aCol.size()-1 );
See https://opengrok.libreoffice.org/xref/core/sc/source/core/data/table1.cxx?r=9d4c36d7#785

Regression from:
https://cgit.freedesktop.org/libreoffice/core/commit/?id=2670e980c7dbadbdc20ff23d57ad892951edc254
author	Julien Nabet <serval2412@yahoo.fr>	2020-04-14 14:28:55 +0200
committer	Noel Grandin <noel.grandin@collabora.co.uk>	2020-04-16 08:21:40 +0200
commit	2670e980c7dbadbdc20ff23d57ad892951edc254 (patch)
tree	5d14c75f5b4a0d4d76f249430fafc6c0db406a22
parent	f98b3b5b9f1b05259405833e6e528e734ded4512 (diff)
tdf#132097: fix Find Previous with direction columns crashes in specific case
See bt here:
https://bugs.documentfoundation.org/attachment.cgi?id=159553

nBlockPos vector is created with nLastCol + 1 elements,
so if we click on a cell in a column which is after last value,
the search will crash

Change-Id: Id9a9cb4130fad5bdc6e2b6a2ac0c648278860c69

sc/source/core/data/table6.cxx
@@ -330,6 +330,7 @@ bool ScTable::Search(const SvxSearchItem& rSearchItem, SCCOL& rCol, SCROW& rRow,
                      SCCOL nLastCol, SCROW nLastRow,
                      const ScMarkData& rMark, OUString& rUndoStr, ScDocument* pUndoDoc)
 {
+    nLastCol   = std::min<SCCOL>( nLastCol,   aCol.size()-2 );
     bool bFound = false;
     bool bAll =  (rSearchItem.GetCommand() == SvxSearchCmd::FIND_ALL)
                ||(rSearchItem.GetCommand() == SvxSearchCmd::REPLACE_ALL);