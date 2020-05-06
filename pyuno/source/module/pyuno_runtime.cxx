d1786724b8e8e474e1f7e39012c1f19611841dc0
Noel Grandin <noel.grandin@collabora.co.uk>
2020-05-06 18:43:30 +0200

    prevent warnings in pyuno with latest python
    which has marked some members deprecated, but we can't stop initialising
    them or we run the risk of ASAN complaining
    
    Change-Id: I8f4ad0ae083fad9c040613ddde7c40f20d68c7d7
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/93580
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

--- pyuno/source/module/pyuno_runtime.cxx
+++ pyuno/source/module/pyuno_runtime.cxx
@@ -70,6 +70,10 @@ using com::sun::star::beans::theIntrospection;
 namespace pyuno
 {
 
+#ifdef __GNUC__
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#endif
 static PyTypeObject RuntimeImpl_Type =
 {
     PyVarObject_HEAD_INIT (&PyType_Type, 0)
@@ -128,11 +132,14 @@ static PyTypeObject RuntimeImpl_Type =
 #if PY_VERSION_HEX >= 0x03080000
     , nullptr // vectorcallfunc tp_vectorcall
 #if PY_VERSION_HEX >= 0x03080200
-    , 0 //Py_ssize_t tp_print
+    , nullptr //Py_ssize_t tp_print
 #endif
 #endif
 #endif
 };
+#ifdef __GNUC__
+#pragma GCC diagnostic pop
+#endif
 
 /*----------------------------------------------------------------------
   Runtime implementation
