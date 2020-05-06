0f7f3ede6699df09be5b0d9d24818cf1fbbaf6f6
Justin Luth <justin.luth@collabora.com>
2020-05-06 10:45:10 +0300

    tdf#132637 doc import: don't open readonly for lockRev
    Just like with forms, the password should not be
    required to make changes when only enforcement of
    track changes is specified. So just grabbag
    the password and let the document open in read-write mode.
    
    TODO for the overly zealous: actually enforce
    file->properties->security->protect track changes,
    and also export it. (This has been done for DOCX.)
    
    Change-Id: Ib68ec5785aa0f5dbac507a3d50059a443f7ff42b
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/93551
    Tested-by: Jenkins
    Reviewed-by: Justin Luth <justin_luth@sil.org>

--- sw/qa/extras/uiwriter/uiwriter3.cxx
+++ sw/qa/extras/uiwriter/uiwriter3.cxx
@@ -14,6 +14,8 @@
 #include <comphelper/propertysequence.hxx>
 #include <boost/property_tree/json_parser.hpp>
 
+#include <wrtsh.hxx>
+
 namespace
 {
 char const DATA_DIRECTORY[] = "/sw/qa/extras/uiwriter/data3/";
@@ -687,6 +689,16 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest3, testTdf117601)
     CPPUNIT_ASSERT_EQUAL(sal_Int32(3), xTextTable->getColumns()->getCount());
 
     CPPUNIT_ASSERT(xCellB1->getString().endsWith("test1"));
+}
+
+CPPUNIT_TEST_FIXTURE(SwUiWriterTest3, testTdf132637_protectTrackChanges)
+{
+    load(DATA_DIRECTORY, "tdf132637_protectTrackChanges.doc");
+    SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument*>(mxComponent.get());
+    CPPUNIT_ASSERT(pTextDoc);
+
+    // The password should only prevent turning off track changes, not open as read-only
+    CPPUNIT_ASSERT(!pTextDoc->GetDocShell()->IsReadOnly());
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
