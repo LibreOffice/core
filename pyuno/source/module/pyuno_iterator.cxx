42c018949735bdd125f2945e2cd49065a9b1d7c1
Noel Grandin <noel.grandin@collabora.co.uk>
2020-05-06 18:43:30 +0200

    prevent warnings in pyuno with latest python
    which has marked some members deprecated, but we can't stop initialising
    them or we run the risk of ASAN complaining
    
    Change-Id: I8f4ad0ae083fad9c040613ddde7c40f20d68c7d7

--- pyuno/source/module/pyuno_iterator.cxx
+++ pyuno/source/module/pyuno_iterator.cxx
@@ -110,7 +110,10 @@ static PyObject* PyUNO_iterator_next( PyObject *self )
     return nullptr;
 }
 
-
+#ifdef __GNUC__
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#endif
 static PyTypeObject PyUNO_iterator_Type =
 {
     PyVarObject_HEAD_INIT( &PyType_Type, 0 )
@@ -169,11 +172,14 @@ static PyTypeObject PyUNO_iterator_Type =
 #if PY_VERSION_HEX >= 0x03080000
     , nullptr // vectorcallfunc tp_vectorcall
 #if PY_VERSION_HEX >= 0x03080200
-    , 0 //Py_ssize_t tp_print
+    , nullptr //Py_ssize_t tp_print
 #endif
 #endif
 #endif
 };
+#ifdef __GNUC__
+#pragma GCC diagnostic pop
+#endif
 
 PyObject* PyUNO_iterator_new( const Reference< XEnumeration >& xEnumeration )
 {
@@ -250,7 +256,10 @@ static PyObject* PyUNO_list_iterator_next( PyObject *self )
     return nullptr;
 }
 
-
+#ifdef __GNUC__
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#endif
 static PyTypeObject PyUNO_list_iterator_Type =
 {
     PyVarObject_HEAD_INIT( &PyType_Type, 0 )
@@ -309,11 +318,14 @@ static PyTypeObject PyUNO_list_iterator_Type =
 #if PY_VERSION_HEX >= 0x03080000
     , nullptr // vectorcallfunc tp_vectorcall
 #if PY_VERSION_HEX >= 0x03080200
-    , 0 //Py_ssize_t tp_print
+    , nullptr //Py_ssize_t tp_print
 #endif
 #endif
 #endif
 };
+#ifdef __GNUC__
+#pragma GCC diagnostic pop
+#endif
 
 PyObject* PyUNO_list_iterator_new( const Reference<XIndexAccess> &xIndexAccess )
 {
--- pyuno/source/module/pyuno_struct.cxx
+++ pyuno/source/module/pyuno_struct.cxx
@@ -290,6 +290,10 @@ static PyMethodDef PyUNOStructMethods[] =
     {nullptr,         nullptr,                                              0,            nullptr}
 };
 
+#ifdef __GNUC__
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#endif
 static PyTypeObject PyUNOStructType =
 {
     PyVarObject_HEAD_INIT( &PyType_Type, 0 )
@@ -348,11 +352,14 @@ static PyTypeObject PyUNOStructType =
 #if PY_VERSION_HEX >= 0x03080000
     , nullptr // vectorcallfunc tp_vectorcall
 #if PY_VERSION_HEX >= 0x03080200
-    , 0 //Py_ssize_t tp_print
+    , nullptr //Py_ssize_t tp_print
 #endif
 #endif
 #endif
 };
+#ifdef __GNUC__
+#pragma GCC diagnostic pop
+#endif
 
 int PyUNOStruct_initType()
 {
