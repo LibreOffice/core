14320ffa2e716431f3c4a7c73c9a9152df1d014e
Tibor Nagy <nagy.tibor2@nisz.hu>
2020-05-06 11:15:35 +0200

    tdf#112287 DOCX frame import: fix default vAnchor
    Vertical frame alignment was calculated from above
    margin instead of the bottom one, resulting bad
    positions, when vAnchor wasn't defined.
    
    Co-author: Attila Bakos (NISZ)
    
    Change-Id: I81bcf53ec826d5dcc9790c661d784b507d9ababc
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/93556
    Tested-by: László Németh <nemeth@numbertext.org>
    Reviewed-by: László Németh <nemeth@numbertext.org>
    (cherry picked from commit f5636817e7677a3081263df9004940a7d5ac54af)

--- sw/qa/extras/ooxmlexport/ooxmlexport5.cxx
+++ sw/qa/extras/ooxmlexport/ooxmlexport5.cxx
@@ -1225,6 +1225,32 @@ DECLARE_OOXMLEXPORT_TEST(testNonBMPChar, "nonbmpchar.docx")
     CPPUNIT_ASSERT_EQUAL(aExpected, xTextRange1->getString());
 }
 
+<<<<<<< HEAD   (4a30d3 tdf#79998 FILESAVE: XLSX export with long sheet names (lengt)
+=======
+DECLARE_OOXMLEXPORT_TEST(testSpacingGroupShapeText, "tdf131775_SpacingGroupShapeText.docx")
+{
+    // tdf#131775: Check if correct spacing.
+
+    xmlDocUniquePtr pXmlDocument = parseExport("word/document.xml");
+    if (!pXmlDocument)
+        return;
+
+    assertXPath(pXmlDocument, "/w:document/w:body/w:p/w:r/mc:AlternateContent/mc:Choice/w:drawing/wp:anchor"
+        "/a:graphic/a:graphicData/wpg:wgp/wps:wsp[1]/wps:txbx/w:txbxContent/w:p/w:r/w:rPr/w:spacing", "val", "71");
+}
+
+DECLARE_OOXMLEXPORT_TEST(testTdf112287, "tdf112287.docx")
+{
+    // tdf#131775: Check if correct spaceing.
+
+    xmlDocPtr pXmlDocument = parseExport("word/document.xml");
+    if (!pXmlDocument)
+        return;
+
+    assertXPath(pXmlDocument, "/w:document/w:body/w:p[2]/w:pPr/w:framePr","vAnchor","margin");
+}
+
+>>>>>>> CHANGE (f56368 tdf#112287 DOCX frame import: fix default vAnchor)
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
