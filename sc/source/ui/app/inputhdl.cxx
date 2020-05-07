3d19247d9a8dacbe973f295ad078a04c9dd3943f
Jan Holesovsky <kendy@collabora.com>
2020-05-07 15:47:49 +0200

    formula bar: Change completeFunction() to accept string instead of index.
    The 'index' is unsafe, because the set it tries to index can change in
    the meantime.  Instead, use the function name and search for it in the
    set, to get the recent index.
    
    Change-Id: Id2a021c32f421057c87b6f7f4fffcc1c98009acb
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/93666
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Andras Timar <andras.timar@collabora.com>

--- sc/source/ui/app/inputhdl.cxx
+++ sc/source/ui/app/inputhdl.cxx
@@ -1637,7 +1637,7 @@ void ScInputHandler::PasteFunctionData()
         pActiveView->ShowCursor();
 }
 
-void ScInputHandler::LOKPasteFunctionData( sal_uInt32 nIndex )
+void ScInputHandler::LOKPasteFunctionData(const OUString& rFunctionName)
 {
     if (pActiveViewSh && (pTopView || pTableView))
     {
@@ -1660,12 +1660,16 @@ void ScInputHandler::LOKPasteFunctionData( sal_uInt32 nIndex )
             InputReplaceSelection( aNewFormula );
         }
 
-        if (pFormulaData && nIndex < pFormulaData->size())
+        if (pFormulaData)
         {
-            auto aPos = pFormulaData->begin();
-            std::advance(aPos, nIndex);
-            miAutoPosFormula = aPos;
-            PasteFunctionData();
+            OUString aNew;
+            ScTypedCaseStrSet::const_iterator aPos = findText(*pFormulaData, pFormulaData->begin(), rFunctionName, aNew, /* backward = */false);
+
+            if (aPos != pFormulaData->end())
+            {
+                miAutoPosFormula = aPos;
+                PasteFunctionData();
+            }
         }
     }
 }
