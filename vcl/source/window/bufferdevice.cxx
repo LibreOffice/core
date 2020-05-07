LibreOffice/core

commit c04169c586ef1d55b1d0ac469bb4fbd4f50bd08a

tdf#125415 vcl menu floating window: avoid flicker
This is similar to e8d5b8beb5958147235ff955ed38c47b51d860ff (tdf#113714
vcl menu bar window: avoid flicker, 2019-05-20), except that was for the
menu bar window, and this is for the floating window opening from that
one.

Change-Id: Ib24f4999ad3e8cbbecc058368e9d112e106e9178
Reviewed-on: https://gerrit.libreoffice.org/72688
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

vcl/source/window/menufloatingwindow.cxx
@@ -27,6 +27,7 @@
 #include <svdata.hxx>
 #include <vcl/decoview.hxx>
 #include <vcl/settings.hxx>
+#include <vcl/virdev.hxx>
 #include <window.h>
 
 MenuFloatingWindow::MenuFloatingWindow( Menu* pMen, vcl::Window* pParent, WinBits nStyle ) :
@@ -1206,32 +1207,41 @@ void MenuFloatingWindow::Paint(vcl::RenderContext& rRenderContext, const tools::
     if (!pMenu)
         return;
 
-    rRenderContext.Push( PushFlags::CLIPREGION );
-    rRenderContext.SetClipRegion(vcl::Region(rPaintRect));
+    // Make sure that all actual rendering happens in one go to avoid flicker.
+    ScopedVclPtrInstance<VirtualDevice> pBuffer;
+    pBuffer->SetOutputSizePixel(GetOutputSizePixel(), false);
+    pBuffer->DrawOutDev(Point(0, 0), GetOutputSizePixel(), Point(0, 0), GetOutputSizePixel(),
+                        rRenderContext);
+
+    pBuffer->Push(PushFlags::CLIPREGION);
+    pBuffer->SetClipRegion(vcl::Region(rPaintRect));
 
     if (rRenderContext.IsNativeControlSupported(ControlType::MenuPopup, ControlPart::Entire))
     {
-        rRenderContext.SetClipRegion();
+        pBuffer->SetClipRegion();
         long nX = 0;
         Size aPxSize(GetOutputSizePixel());
         aPxSize.AdjustWidth( -nX );
         ImplControlValue aVal(pMenu->nTextPos - GUTTERBORDER);
-        rRenderContext.DrawNativeControl(ControlType::MenuPopup, ControlPart::Entire,
-                                         tools::Rectangle(Point(nX, 0), aPxSize),
-                                         ControlState::ENABLED, aVal, OUString());
-        InitMenuClipRegion(rRenderContext);
+        pBuffer->DrawNativeControl(ControlType::MenuPopup, ControlPart::Entire,
+                                   tools::Rectangle(Point(nX, 0), aPxSize), ControlState::ENABLED,
+                                   aVal, OUString());
+        InitMenuClipRegion(*pBuffer);
     }
     if (IsScrollMenu())
     {
-        ImplDrawScroller(rRenderContext, true);
-        ImplDrawScroller(rRenderContext, false);
+        ImplDrawScroller(*pBuffer, true);
+        ImplDrawScroller(*pBuffer, false);
     }
-    rRenderContext.SetFillColor(rRenderContext.GetSettings().GetStyleSettings().GetMenuColor());
-    pMenu->ImplPaint(rRenderContext, GetOutputSizePixel(), nScrollerHeight, ImplGetStartY());
+    pBuffer->SetFillColor(rRenderContext.GetSettings().GetStyleSettings().GetMenuColor());
+    pMenu->ImplPaint(*pBuffer, GetOutputSizePixel(), nScrollerHeight, ImplGetStartY());
     if (nHighlightedItem != ITEMPOS_INVALID)
-        RenderHighlightItem(rRenderContext, nHighlightedItem);
+        RenderHighlightItem(*pBuffer, nHighlightedItem);
 
-    rRenderContext.Pop();
+    pBuffer->Pop();
+
+    rRenderContext.DrawOutDev(Point(0, 0), GetOutputSizePixel(), Point(0, 0), GetOutputSizePixel(),
+                              *pBuffer);
 }
 
 void MenuFloatingWindow::ImplDrawScroller(vcl::RenderContext& rRenderContext, bool bUp)

commit fe4ed3b67716d4393798d585f5cb1f9e385b39b2

tdf#132267 vcl: fix missing scrollers with non-native rendering
Regression from c04169c586ef1d55b1d0ac469bb4fbd4f50bd08a (tdf#125415 vcl
menu floating window: avoid flicker, 2019-05-21) the problem was that
the clip region was set on the buffer, not on the render context. This
means the original clip was used to determine what gets copied from the
buffer to the screen, so the scroller arrows were not rendered.

Change-Id: Id173e6333721891798da58baf2092f4cd21a62ba

vcl/source/window/bufferdevice.cxx
@@ -23,11 +23,19 @@ BufferDevice::BufferDevice(const VclPtr<vcl::Window>& pWindow, vcl::RenderContex
     m_pBuffer->EnableRTL(rRenderContext.IsRTLEnabled());
 }
 
-BufferDevice::~BufferDevice()
+void BufferDevice::Dispose()
 {
+    if (m_bDisposed)
+    {
+        return;
+    }
+
     m_rRenderContext.DrawOutDev(Point(0, 0), m_pWindow->GetOutputSizePixel(), Point(0, 0),
                                 m_pWindow->GetOutputSizePixel(), *m_pBuffer);
+    m_bDisposed = true;
 }
+
+BufferDevice::~BufferDevice() { Dispose(); }
 
 vcl::RenderContext* BufferDevice::operator->() { return m_pBuffer.get(); }
 

vcl/source/window/bufferdevice.hxx
@@ -21,10 +21,12 @@ class VCL_DLLPUBLIC BufferDevice
     ScopedVclPtr<VirtualDevice> m_pBuffer;
     VclPtr<vcl::Window> m_pWindow;
     vcl::RenderContext& m_rRenderContext;
+    bool m_bDisposed = false;
 
 public:
     BufferDevice(const VclPtr<vcl::Window>& pWindow, vcl::RenderContext& rRenderContext);
     ~BufferDevice();
+    void Dispose();
 
     vcl::RenderContext* operator->();
     vcl::RenderContext& operator*();

vcl/source/window/menufloatingwindow.cxx
@@ -1205,11 +1205,13 @@ void MenuFloatingWindow::Paint(vcl::RenderContext& rRenderContext, const tools::
     if (!pMenu)
         return;
 
+    // Set the clip before the buffering starts: rPaintRect may be larger than the current clip,
+    // this way the buffer -> render context copy happens with this clip.
+    rRenderContext.Push(PushFlags::CLIPREGION);
+    rRenderContext.SetClipRegion(vcl::Region(rPaintRect));
+
     // Make sure that all actual rendering happens in one go to avoid flicker.
     vcl::BufferDevice pBuffer(this, rRenderContext);
-
-    pBuffer->Push(PushFlags::CLIPREGION);
-    pBuffer->SetClipRegion(vcl::Region(rPaintRect));
 
     if (rRenderContext.IsNativeControlSupported(ControlType::MenuPopup, ControlPart::Entire))
     {
@@ -1233,7 +1235,8 @@ void MenuFloatingWindow::Paint(vcl::RenderContext& rRenderContext, const tools::
     if (nHighlightedItem != ITEMPOS_INVALID)
         RenderHighlightItem(*pBuffer, nHighlightedItem);
 
-    pBuffer->Pop();
+    pBuffer.Dispose();
+    rRenderContext.Pop();
 }
 
 void MenuFloatingWindow::ImplDrawScroller(vcl::RenderContext& rRenderContext, bool bUp)