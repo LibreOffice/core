b25b13db47930911217ca0252a60fb3d768422ac
Henry Castro <hcastro@collabora.com>
2020-05-07 18:45:28 -0400

    lok: override get_state of the MetricFieldObject class
    used for unit testing
    
    Change-Id: I7d8ed976837b488bfb22d865583b8d4f1f0978ec

--- include/vcl/uitest/metricfielduiobject.hxx
+++ include/vcl/uitest/metricfielduiobject.hxx
@@ -24,6 +24,8 @@ public:
 
     virtual void execute(const OUString& rAction, const StringMap& rParameters) override;
 
+    virtual StringMap get_state() override;
+
     static std::unique_ptr<UIObject> create(vcl::Window* pWindow);
 
 protected:
