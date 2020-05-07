61eb2a4f6d8506a700a20511f6b443828c50d408
Michael Stahl <Michael.Stahl@cib.de>
2020-05-07 18:18:43 +0200

    tdf#103602 xmloff,sw: ODF 1.3 import: PageStyle with drawing-page style
    Associate a style of family "drawing-page" with a style:master-page.
    
    This fixes the small part of the draw:fill attribute problem that is
    covered by OFFICE-3937 in ODF 1.3.
    
    This is the import part.
    
    Change-Id: I4c86fa24c36407b64ce33f0890e5da8c26c5292a

---
+++ include/xmloff/XMLDrawingPageStyleContext.hxx
@@ -0,0 +1,44 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#pragma once
+
+#include <xmloff/prstylei.hxx>
+#include <xmloff/xmlimppr.hxx>
+#include <xmloff/families.hxx>
+#include <xmloff/dllapi.h>
+
+class XMLOFF_DLLPUBLIC XMLDrawingPageStyleContext : public XMLPropStyleContext
+{
+public:
+    XMLDrawingPageStyleContext(SvXMLImport& rImport, sal_uInt16 nPrefix, OUString const& rLocalName,
+                               css::uno::Reference<css::xml::sax::XAttributeList> const& xAttrList,
+                               SvXMLStylesContext& rStyles,
+                               ContextID_Index_Pair const pContextIDs[],
+                               XmlStyleFamily const pFamilies[]);
+
+    virtual void
+    FillPropertySet(css::uno::Reference<css::beans::XPropertySet> const& rPropSet) override;
+
+private:
+    std::unique_ptr<ContextID_Index_Pair[]> m_pContextIDs;
+    XmlStyleFamily const* const m_pFamilies;
+};
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
