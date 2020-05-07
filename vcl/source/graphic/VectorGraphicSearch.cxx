c0de8e44e92c8d83c98afe8f807deb2633b3d2bd
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2020-05-07 22:01:22 +0200

    vcl: VectorGraphicSearch - for searching text inside PDF
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/95254
    Tested-by: Tomaž Vajngerl <quikee@gmail.com>
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>
    (cherry picked from commit efba780d6155317b592b6f5f73945a7851ec4d3b)
    
    Change-Id: Iee940a3927330c8739774ff3c1af15998f89193b
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/95801
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

---
+++ vcl/source/graphic/VectorGraphicSearch.cxx
@@ -0,0 +1,168 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#include <sal/config.h>
+#include <vcl/VectorGraphicSearch.hxx>
+
+#include <fpdf_text.h>
+
+class SearchContext
+{
+public:
+    bool bInitialized = false;
+
+    FPDF_DOCUMENT mpPdfDocument;
+    sal_Int32 mnPageIndex;
+    FPDF_PAGE mpPage;
+    FPDF_TEXTPAGE mpTextPage;
+    OUString maSearchString;
+    FPDF_SCHHANDLE mpSearchHandle;
+
+    SearchContext(FPDF_DOCUMENT pPdfDocument, sal_Int32 nPageIndex, OUString const& rSearchString)
+        : mpPdfDocument(pPdfDocument)
+        , mnPageIndex(nPageIndex)
+        , mpPage(nullptr)
+        , mpTextPage(nullptr)
+        , maSearchString(rSearchString)
+        , mpSearchHandle(nullptr)
+    {
+    }
+
+    ~SearchContext()
+    {
+        if (mpSearchHandle)
+            FPDFText_FindClose(mpSearchHandle);
+        if (mpTextPage)
+            FPDFText_ClosePage(mpTextPage);
+        if (mpPage)
+            FPDF_ClosePage(mpPage);
+    }
+
+    bool initialize()
+    {
+        if (!mpPdfDocument)
+            return false;
+        mpPage = FPDF_LoadPage(mpPdfDocument, mnPageIndex);
+        if (!mpPage)
+            return false;
+        mpTextPage = FPDFText_LoadPage(mpPage);
+        if (!mpTextPage)
+            return false;
+
+        FPDF_WIDESTRING pString = reinterpret_cast<FPDF_WIDESTRING>(maSearchString.getStr());
+        mpSearchHandle = FPDFText_FindStart(mpTextPage, pString, 0, 0);
+
+        return mpSearchHandle != nullptr;
+    }
+
+    bool next()
+    {
+        if (mpSearchHandle)
+            return FPDFText_FindNext(mpSearchHandle);
+        return false;
+    }
+
+    int index()
+    {
+        if (mpSearchHandle)
+            return FPDFText_GetSchResultIndex(mpSearchHandle);
+        return -1;
+    }
+};
+
+VectorGraphicSearch::VectorGraphicSearch(Graphic const& rGraphic)
+    : maGraphic(rGraphic)
+    , mpPdfDocument(nullptr)
+{
+    FPDF_LIBRARY_CONFIG aConfig;
+    aConfig.version = 2;
+    aConfig.m_pUserFontPaths = nullptr;
+    aConfig.m_pIsolate = nullptr;
+    aConfig.m_v8EmbedderSlot = 0;
+    FPDF_InitLibraryWithConfig(&aConfig);
+}
+
+VectorGraphicSearch::~VectorGraphicSearch()
+{
+    mpSearchContext.reset();
+
+    if (mpPdfDocument)
+        FPDF_CloseDocument(mpPdfDocument);
+    FPDF_DestroyLibrary();
+}
+
+bool VectorGraphicSearch::search(OUString const& rSearchString)
+{
+    auto pData = maGraphic.getVectorGraphicData();
+
+    if (pData && pData->getVectorGraphicDataType() == VectorGraphicDataType::Pdf)
+    {
+        return searchPDF(pData, rSearchString);
+    }
+    return false;
+}
+
+bool VectorGraphicSearch::searchPDF(std::shared_ptr<VectorGraphicData> const& rData,
+                                    OUString const& rSearchString)
+{
+    if (rSearchString.isEmpty())
+        return false;
+
+    mpPdfDocument
+        = FPDF_LoadMemDocument(rData->getVectorGraphicDataArray().getConstArray(),
+                               rData->getVectorGraphicDataArrayLength(), /*password=*/nullptr);
+
+    if (!mpPdfDocument)
+    {
+        //TODO: Handle failure to load.
+        switch (FPDF_GetLastError())
+        {
+            case FPDF_ERR_SUCCESS:
+                break;
+            case FPDF_ERR_UNKNOWN:
+                break;
+            case FPDF_ERR_FILE:
+                break;
+            case FPDF_ERR_FORMAT:
+                break;
+            case FPDF_ERR_PASSWORD:
+                break;
+            case FPDF_ERR_SECURITY:
+                break;
+            case FPDF_ERR_PAGE:
+                break;
+            default:
+                break;
+        }
+        return false;
+    }
+
+    sal_Int32 nPageIndex = std::max(rData->getPageIndex(), 0);
+
+    mpSearchContext.reset(new SearchContext(mpPdfDocument, nPageIndex, rSearchString));
+
+    return mpSearchContext->initialize();
+}
+
+bool VectorGraphicSearch::next()
+{
+    if (mpSearchContext)
+        return mpSearchContext->next();
+    return false;
+}
+
+int VectorGraphicSearch::index()
+{
+    if (mpSearchContext)
+        return mpSearchContext->index();
+    return -1;
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
