32322af9d88b64a2fc1a8d95fa0be0141263a48d
David Ostrovsky <david@ostrovsky.org>
2020-05-08 21:47:25 +0200

    tdf#158698: Fix build with poppler 0.86.0
    Change-Id: I89b4635a6a3e3a5522172d6f4c3f14e6c14994b1

--- sdext/source/pdfimport/xpdfwrapper/pdfioutdev_gpl.cxx
+++ sdext/source/pdfimport/xpdfwrapper/pdfioutdev_gpl.cxx
@@ -563,7 +563,9 @@ void PDFOutDev::processLink(Link* link, Catalog*)
     if (!(pAction && pAction->getKind() == actionURI))
         return;
 
-#if POPPLER_CHECK_VERSION(0, 72, 0)
+#if POPPLER_CHECK_VERSION(0, 86, 0)
+    const char* pURI = static_cast<LinkURI*>(pAction)->getURI().c_str();
+#elif POPPLER_CHECK_VERSION(0, 72, 0)
     const char* pURI = static_cast<LinkURI*>(pAction)->getURI()->c_str();
 #else
     const char* pURI = static_cast<LinkURI*>(pAction)->getURI()->getCString();
