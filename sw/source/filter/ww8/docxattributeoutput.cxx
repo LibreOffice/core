598ca431de96d8bfcf18fa2945e9e30f98387474
Vasily Melenchuk <vasily.melenchuk@cib.de>
2020-05-09 14:35:25 +0300

    tdf#132766: DOCX export: always try to set bullet font for list
    There are some problems with bullet if we use MS Wingdigs bullets
    and do not specify Symbol font for it. It shiuld be either UTF-8
    or Symbol, but not mixture of both.
    
    Change-Id: Ie4a6f7e8fee6cfab21a18fc080f33d1bff455dd9
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/93846
    Tested-by: Jenkins
    Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>

--- sw/source/filter/ww8/docxattributeoutput.cxx
+++ sw/source/filter/ww8/docxattributeoutput.cxx
@@ -6988,7 +6988,10 @@ void DocxAttributeOutput::NumberingLevel( sal_uInt8 nLevel,
                     FSNS( XML_w, XML_cs ), aFamilyName,
                     FSNS( XML_w, XML_hint ), "default" );
         }
-        m_rExport.OutputItemSet( *pOutSet, false, true, i18n::ScriptType::LATIN, m_rExport.m_bExportModeRTF );
+        else
+        {
+            m_rExport.OutputItemSet(*pOutSet, false, true, i18n::ScriptType::LATIN, m_rExport.m_bExportModeRTF);
+        }
 
         WriteCollectedRunProperties();
 
