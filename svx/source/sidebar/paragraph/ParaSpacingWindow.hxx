2266f1d018de92c6f4fdb538a241cfc6a5b10675
Shivam Kumar Singh <shivamhere247@gmail.com>
2020-05-10 01:18:52 +0530

    tdf#124176: Use pragma once instead of header guard
    Change-Id: I6adea171e05fec4a36225cead031214c7d1544e2

--- svx/source/sidebar/paragraph/ParaSpacingWindow.hxx
+++ svx/source/sidebar/paragraph/ParaSpacingWindow.hxx
@@ -16,8 +16,7 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
-#ifndef INCLUDED_SVX_SOURCE_SIDEBAR_PARAGRAPH_PARASPACINGWINDOW_HXX
-#define INCLUDED_SVX_SOURCE_SIDEBAR_PARAGRAPH_PARASPACINGWINDOW_HXX
+#pragma once
 
 #include <editeng/ulspitem.hxx>
 #include <vcl/EnumContext.hxx>
@@ -112,7 +111,5 @@ public:
     virtual void GetFocus() override;
 };
 }
-
-#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
