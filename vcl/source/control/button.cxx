5d74827f088c9bbcca7f41a59696d62395029f68
Michael Weghorn <m.weghorn@posteo.de>
2020-05-11 08:45:19 +0200

    tdf#132832 calcAdjustedSize: Take max width into account
    Actually take into account the preferred width when
    calculating the size of radio button, checkbox and
    hyperlink controls.
    
    This e.g. makes word wrap work properly when the
    multiline property is set for a checkbox, radio or hyperlink
    control and the single line text exceeds the preferred width,
    rather than keeping the whole text in one line that exceeds
    the preferred width.
    
    Change-Id: Id04668e4e1afe7c10a28468eff05cf04c10ae3c0
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/93947
    Tested-by: Jenkins
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>
    (cherry picked from commit 2539f1d142e0077dfeec36ef349a1f5443f1c94b)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/93798
    Reviewed-by: Adolfo Jayme Barrientos <fitojb@ubuntu.com>

--- vcl/source/control/button.cxx
+++ vcl/source/control/button.cxx
@@ -2814,7 +2814,7 @@ void RadioButton::ImplSetMinimumNWFSize()
     Pop();
 }
 
-Size RadioButton::CalcMinimumSize() const
+Size RadioButton::CalcMinimumSize(long nMaxWidth) const
 {
     Size aSize;
     if ( !maImage )
@@ -2838,7 +2838,7 @@ Size RadioButton::CalcMinimumSize() const
     {
         bool bTopImage = (GetStyle() & WB_TOP) != 0;
 
-        Size aTextSize = GetTextRect( tools::Rectangle( Point(), Size( 0x7fffffff, 0x7fffffff ) ),
+        Size aTextSize = GetTextRect( tools::Rectangle( Point(), Size( nMaxWidth > 0 ? nMaxWidth : 0x7fffffff, 0x7fffffff ) ),
                                       aText, FixedText::ImplGetTextStyle( GetStyle() ) ).GetSize();
 
         aSize.AdjustWidth(2 );   // for focus rect
