cbc13ac0624685582ebd4634812681274db803aa
Mike Kaganski <mike.kaganski@collabora.com>
2020-05-12 20:05:54 +0300

    tdf#49247: draw soft edges
    This factors out the common code for blurring used both in glow and
    soft edges into ProcessAndBlurAlphaMask.
    
    Also this reverts commit a98bdbae459ad7341bf7f484c402e77e4062cd16,
    since its use was removed from VclPixelProcessor2D.
    
    Change-Id: Icd7fdb06bef3932ff3b9ce7e283b515b15d246a5
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/94087
    Tested-by: Jenkins
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

---
+++ include/drawinglayer/primitive2d/softedgeprimitive2d.hxx
@@ -0,0 +1,51 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#pragma once
+
+#include <drawinglayer/drawinglayerdllapi.h>
+#include <drawinglayer/primitive2d/groupprimitive2d.hxx>
+
+namespace drawinglayer::primitive2d
+{
+class DRAWINGLAYER_DLLPUBLIC SoftEdgePrimitive2D final : public GroupPrimitive2D
+{
+private:
+    /// Soft edge size, in logical units (100ths of mm)
+    double mfRadius;
+    mutable bool mbInMaskGeneration = false;
+
+public:
+    SoftEdgePrimitive2D(double fRadius, const Primitive2DContainer& rChildren);
+
+    double getRadius() const { return mfRadius; }
+
+    void setMaskGeneration(bool bVal = true) const { mbInMaskGeneration = bVal; }
+
+    virtual bool operator==(const BasePrimitive2D& rPrimitive) const override;
+
+    virtual void
+    get2DDecomposition(Primitive2DDecompositionVisitor& rVisitor,
+                       const geometry::ViewInformation2D& rViewInformation) const override;
+
+    virtual sal_uInt32 getPrimitive2DID() const override;
+};
+} // end of namespace drawinglayer::primitive2d
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
--- vcl/Library_vcl.mk
+++ vcl/Library_vcl.mk
@@ -333,7 +333,6 @@ $(eval $(call gb_Library_add_exception_objects,vcl,\
     vcl/source/bitmap/bitmapfilter \
     vcl/source/bitmap/BitmapAlphaClampFilter \
     vcl/source/bitmap/BitmapBasicMorphologyFilter \
-    vcl/source/bitmap/BitmapColorReplaceFilter \
     vcl/source/bitmap/BitmapMonochromeFilter \
     vcl/source/bitmap/BitmapSmoothenFilter \
     vcl/source/bitmap/BitmapLightenFilter \
