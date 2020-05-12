0507ef963842191d2b34e007831a38800212f602
Caolán McNamara <caolanm@redhat.com>
2020-05-12 11:13:55 +0100

    missing licence header
    I wrote all this so this is the appropiate boilerplate
    
    since...
    
    commit 3832609a238d47375e634cbda4d092b11108fee4
    Date:   Wed Feb 19 13:32:14 2020 +0100
    
        Create header file for SalInstanceBuilder
    
    Change-Id: I7615e15b220cd2e801a33c1418cf1f7cb0d72fc4
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/94046
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>

--- vcl/inc/salvtables.hxx
+++ vcl/inc/salvtables.hxx
@@ -1,5 +1,12 @@
-#ifndef INCLUDED_VCL_INC_SALVTABLES_HXX
-#define INCLUDED_VCL_INC_SALVTABLES_HXX
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+#pragma once
 
 #include <vcl/weld.hxx>
 #include <vcl/svapp.hxx>
@@ -615,4 +622,4 @@ public:
     virtual ~SalInstanceSpinButton() override;
 };
 
-#endif
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
