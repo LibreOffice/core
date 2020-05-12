LibreOffice/core

commit 739ed2c29f49ea5e83bcd1352b2644c2e2d09f7b

tdf#115630 tdf#88922 Chart: fix custom legend position and size
Do not need to set the "AnchorPosition" value, when we move
the legend object. It is enough to set only the "Expansion" as
custom, so we keep the previously set "AnchorPosition" value
and we can use the lcl_calculatePositionAndRemainingSpace function
to calculate the remaining space of the inner chart area.

Change-Id: I7b577bc6acf2de9b6755329b92603ea5ba631d57
Reviewed-on: https://gerrit.libreoffice.org/83399
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

chart2/source/view/main/VLegend.cxx
@@ -701,29 +701,29 @@ awt::Point lcl_calculatePositionAndRemainingSpace(
     switch( ePos )
     {
         case LegendPosition_LINE_START:
-            {
-                sal_Int32 nExtent = aLegendSize.Width;
-                rRemainingSpace.Width -= ( nExtent + nXDistance );
-                rRemainingSpace.X += ( nExtent + nXDistance );
-            }
+        {
+            sal_Int32 nExtent = aLegendSize.Width;
+            rRemainingSpace.Width -= ( nExtent + nXDistance );
+            rRemainingSpace.X += ( nExtent + nXDistance );
+        }
         break;
         case LegendPosition_LINE_END:
-            {
-                rRemainingSpace.Width -= ( aLegendSize.Width + nXDistance );
-            }
-            break;
+        {
+            rRemainingSpace.Width -= ( aLegendSize.Width + nXDistance );
+        }
+        break;
         case LegendPosition_PAGE_START:
-            {
-                sal_Int32 nExtent = aLegendSize.Height;
-                rRemainingSpace.Height -= ( nExtent + nYDistance );
-                rRemainingSpace.Y += ( nExtent + nYDistance );
-            }
+        {
+            sal_Int32 nExtent = aLegendSize.Height;
+            rRemainingSpace.Height -= ( nExtent + nYDistance );
+            rRemainingSpace.Y += ( nExtent + nYDistance );
+        }
         break;
         case LegendPosition_PAGE_END:
-            {
-                rRemainingSpace.Height -= ( aLegendSize.Height + nYDistance );
-            }
-            break;
+        {
+            rRemainingSpace.Height -= ( aLegendSize.Height + nYDistance );
+        }
+        break;
 
         default:
             // nothing

sw/qa/extras/layout/layout.cxx
@@ -2285,6 +2285,31 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf75659)
     // These failed, if the legend names are empty strings.
 }
 
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf115630)
+{
+    SwDoc* pDoc = createDoc("tdf115630.docx");
+    SwDocShell* pShell = pDoc->GetDocShell();
+
+    // Dump the rendering of the first page as an XML file.
+    std::shared_ptr<GDIMetaFile> xMetaFile = pShell->GetPreviewMetaFile();
+    MetafileXmlDump dumper;
+    xmlDocPtr pXmlDoc = dumpAndParse(dumper, *xMetaFile);
+    CPPUNIT_ASSERT(pXmlDoc);
+
+    // Test wide of inner chart area.
+    sal_Int32 nXRight
+        = getXPath(pXmlDoc,
+                   "/metafile/push[1]/push[1]/push[1]/push[4]/push[1]/push[3]/polyline[1]/point[1]",
+                   "x")
+              .toInt32();
+    sal_Int32 nXLeft
+        = getXPath(pXmlDoc,
+                   "/metafile/push[1]/push[1]/push[1]/push[4]/push[1]/push[3]/polyline[1]/point[2]",
+                   "x")
+              .toInt32();
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(2886), nXRight - nXLeft);
+}
+
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf108021)
 {
     SwDoc* pDoc = createDoc("tdf108021.odt");

chart2/source/controller/main/PositionAndSizeHelper.cxx
@@ -72,7 +72,6 @@ bool PositionAndSizeHelper::moveObject( ObjectType eObjectType
     }
     else if(eObjectType==OBJECTTYPE_LEGEND)
     {
-        xObjectProp->setPropertyValue( "AnchorPosition", uno::Any(LegendPosition_CUSTOM));
         xObjectProp->setPropertyValue( "Expansion", uno::Any(css::chart::ChartLegendExpansion_CUSTOM));
         chart2::RelativePosition aRelativePosition;
         chart2::RelativeSize aRelativeSize;

commit ae7f8d9b86150385e6a43c7584d86eca544e18f7

tdf#132956 Chart view: fix missing plot area
Do not reduce the inside area of the chart depending
on the size of the legend. Use the default legend size.

Regression from commit: 739ed2c29f49ea5e83bcd1352b2644c2e2d09f7b
(tdf#115630 tdf#88922 Chart: fix custom legend position and size)

Change-Id: Ic191229d7ceab1f2689ab07424353108f8cb2b95

chart2/source/view/main/ChartView.cxx
@@ -2233,12 +2233,13 @@ bool lcl_createLegend( const uno::Reference< XLegend > & xLegend
     if (!VLegend::isVisible(xLegend))
         return false;
 
+    awt::Size rDefaultLegendSize;
     VLegend aVLegend( xLegend, xContext, rLegendEntryProviderList,
             xPageShapes, xShapeFactory, rModel);
     aVLegend.setDefaultWritingMode( nDefaultWritingMode );
     aVLegend.createShapes( awt::Size( rRemainingSpace.Width, rRemainingSpace.Height ),
-                           rPageSize );
-    aVLegend.changePosition( rRemainingSpace, rPageSize );
+                           rPageSize, rDefaultLegendSize );
+    aVLegend.changePosition( rRemainingSpace, rPageSize, rDefaultLegendSize );
     return true;
 }
 

chart2/source/view/main/VLegend.cxx
@@ -271,7 +271,8 @@ awt::Size lcl_placeLegendEntries(
     const awt::Size& rRemainingSpace,
     sal_Int32 nYStartPosition,
     const awt::Size& rPageSize,
-    bool bIsPivotChart)
+    bool bIsPivotChart,
+    awt::Size& rDefaultLegendSize)
 {
     bool bIsCustomSize = (eExpansion == css::chart::ChartLegendExpansion_CUSTOM);
     awt::Size aResultingLegendSize(0,0);
@@ -309,6 +310,9 @@ awt::Size lcl_placeLegendEntries(
     sal_Int32 nMaxEntryWidth = nXOffset + nSymbolPlusDistanceWidth + aMaxEntryExtent.Width;
     sal_Int32 nMaxEntryHeight = nYOffset + aMaxEntryExtent.Height;
     sal_Int32 nNumberOfEntries = rEntries.size();
+
+    rDefaultLegendSize.Width = nMaxEntryWidth + nXPadding;
+    rDefaultLegendSize.Height = nMaxEntryHeight + nYPadding;
 
     sal_Int32 nNumberOfColumns = 0, nNumberOfRows = 0;
     std::vector< sal_Int32 > aColumnWidths;
@@ -870,7 +874,8 @@ bool VLegend::isVisible( const Reference< XLegend > & xLegend )
 
 void VLegend::createShapes(
     const awt::Size & rAvailableSpace,
-    const awt::Size & rPageSize )
+    const awt::Size & rPageSize,
+    awt::Size & rDefaultLegendSize )
 {
     if(! (m_xLegend.is() &&
           m_xShapeFactory.is() &&
@@ -980,7 +985,7 @@ void VLegend::createShapes(
                 // place the legend entries
                 aLegendSize = lcl_placeLegendEntries(aViewEntries, eExpansion, bSymbolsLeftSide, fViewFontSize,
                                                      aMaxSymbolExtent, aTextProperties, xLegendContainer,
-                                                     m_xShapeFactory, aLegendSize, nUsedButtonHeight, rPageSize, bIsPivotChart);
+                                                     m_xShapeFactory, aLegendSize, nUsedButtonHeight, rPageSize, bIsPivotChart, rDefaultLegendSize);
 
                 uno::Reference<beans::XPropertySet> xModelPage(mrModel.getPageBackground());
 
@@ -1014,7 +1019,8 @@ void VLegend::createShapes(
 
 void VLegend::changePosition(
     awt::Rectangle & rOutAvailableSpace,
-    const awt::Size & rPageSize )
+    const awt::Size & rPageSize,
+    const css::awt::Size & rDefaultLegendSize )
 {
     if(! m_xShape.is())
         return;
@@ -1026,6 +1032,7 @@ void VLegend::changePosition(
         Reference< beans::XPropertySet > xLegendProp( m_xLegend, uno::UNO_QUERY_THROW );
         chart2::RelativePosition aRelativePosition;
 
+        bool bDefaultLegendSize = rDefaultLegendSize.Width != 0 || rDefaultLegendSize.Height != 0;
         bool bAutoPosition =
             ! (xLegendProp->getPropertyValue( "RelativePosition") >>= aRelativePosition);
 
@@ -1048,7 +1055,7 @@ void VLegend::changePosition(
             // manual position: relative to whole page
             awt::Rectangle aAvailableSpace( 0, 0, rPageSize.Width, rPageSize.Height );
             awt::Point aPos = lcl_calculatePositionAndRemainingSpace(
-                aAvailableSpace, rPageSize, aRelativePosition, ePos, aLegendSize, bOverlay );
+                aAvailableSpace, rPageSize, aRelativePosition, ePos, bDefaultLegendSize ? rDefaultLegendSize : aLegendSize, bOverlay );
             m_xShape->setPosition( aPos );
 
             if (!bOverlay)
@@ -1056,7 +1063,7 @@ void VLegend::changePosition(
                 // calculate remaining space as if having autoposition:
                 aRelativePosition = lcl_getDefaultPosition( ePos, rOutAvailableSpace, rPageSize );
                 lcl_calculatePositionAndRemainingSpace(
-                    rOutAvailableSpace, rPageSize, aRelativePosition, ePos, aLegendSize, bOverlay );
+                    rOutAvailableSpace, rPageSize, aRelativePosition, ePos, bDefaultLegendSize ? rDefaultLegendSize : aLegendSize, bOverlay );
             }
         }
     }

chart2/source/view/main/VLegend.hxx
@@ -50,7 +50,8 @@ public:
     void setDefaultWritingMode( sal_Int16 nDefaultWritingMode );
 
     void createShapes( const css::awt::Size & rAvailableSpace,
-                       const css::awt::Size & rPageSize );
+                       const css::awt::Size & rPageSize,
+                       css::awt::Size & rDefaultLegendSize );
 
     /** Sets the position according to its internal anchor.
 
@@ -63,7 +64,8 @@ public:
      */
     void changePosition(
         css::awt::Rectangle & rOutAvailableSpace,
-        const css::awt::Size & rReferenceSize );
+        const css::awt::Size & rReferenceSize,
+        const css::awt::Size & rDefaultLegendSize );
 
     static bool isVisible(
         const css::uno::Reference< css::chart2::XLegend > & xLegend );

sw/qa/extras/layout/layout.cxx
@@ -2369,7 +2369,7 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf115630)
                    "/metafile/push[1]/push[1]/push[1]/push[4]/push[1]/push[3]/polyline[1]/point[2]",
                    "x")
               .toInt32();
-    CPPUNIT_ASSERT_DOUBLES_EQUAL(2886, nXRight - nXLeft, 2);
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(2866, nXRight - nXLeft, 2);
 }
 
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf108021)
@@ -2695,7 +2695,23 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf129095)
     xmlDocPtr pXmlDoc = dumpAndParse(dumper, *xMetaFile);
     CPPUNIT_ASSERT(pXmlDoc);
 
-    // check the inner chart area visibility with testing the X axis label
+    // check the inner chart area (relative size) visibility with testing the X axis label
+    assertXPathContent(pXmlDoc, "/metafile/push[1]/push[1]/push[1]/push[4]/push[1]/textarray/text",
+                       "Category 1");
+}
+
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf132956)
+{
+    SwDoc* pDoc = createDoc("tdf132956.docx");
+    SwDocShell* pShell = pDoc->GetDocShell();
+
+    // Dump the rendering of the first page as an XML file.
+    std::shared_ptr<GDIMetaFile> xMetaFile = pShell->GetPreviewMetaFile();
+    MetafileXmlDump dumper;
+    xmlDocPtr pXmlDoc = dumpAndParse(dumper, *xMetaFile);
+    CPPUNIT_ASSERT(pXmlDoc);
+
+    // check the inner chart area (fix size) visibility with testing the X axis label
     assertXPathContent(pXmlDoc, "/metafile/push[1]/push[1]/push[1]/push[4]/push[1]/textarray/text",
                        "Category 1");
 }