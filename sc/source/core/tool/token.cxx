LibreOffice/core

commit 3f80ac477354f4c6293d983d3ca9eef59a07dce0

Replace remaining getCppuType et al with cppu::UnoType
Change-Id: Iddde148aa783c252d0c864e30855383e996dbf69

sc/source/core/tool/token.cxx
@@ -1172,8 +1172,8 @@ bool ScTokenArray::AddFormulaToken(
                 {
                     if ( eOpCode != ocPush )
                         bError = true;      // not an inline array
-                    else if (!rToken.Data.getValueType().equals( getCppuType(
-                                    (uno::Sequence< uno::Sequence< uno::Any > > *)0)))
+                    else if (!rToken.Data.getValueType().equals( cppu::UnoType<
+                                    uno::Sequence< uno::Any >>::get()))
                         bError = true;      // unexpected sequence type
                     else
                     {

sc/inc/miscuno.hxx
@@ -74,14 +74,14 @@ sal_Bool SAL_CALL ClassName::supportsService( const OUString& ServiceName ) \
     { OSL_FAIL("not implemented"); }
 
 #define SC_QUERYINTERFACE(x)    \
-    if (rType == getCppuType((const uno::Reference<x>*)0))  \
+    if (rType == cppu::UnoType<x>::get())  \
     { return uno::makeAny(uno::Reference<x>(this)); }
 
 // SC_QUERY_MULTIPLE( XElementAccess, XIndexAccess ):
 //  use if interface is used several times in one class
 
 #define SC_QUERY_MULTIPLE(x,y)  \
-    if (rType == getCppuType((const uno::Reference<x>*)0))  \
+    if (rType == cppu::UnoType<x>::get())  \
     { uno::Any aR; aR <<= uno::Reference<x>(static_cast<y*>(this)); return aR; }
 
 class ScIndexEnumeration : public cppu::WeakImplHelper2<

sc/inc/rangeseq.hxx
@@ -80,7 +80,7 @@ public:
 
     /** Convert a sequence of mixed elements to ScMatrix.
 
-        Precondition: rAny.getValueType().equals( getCppuType( (uno::Sequence< uno::Sequence< uno::Any > > *)0))
+        Precondition: rAny.getValueType().equals( cppu::UnoType< uno::Sequence< uno::Sequence< uno::Any > > >::get() )
 
         @returns a new'd ScMatrix as ScMatrixRef, NULL if rAny couldn't be read
         as type Sequence<Sequence<Any>>

sc/source/core/data/dpsave.cxx
@@ -63,7 +63,7 @@ static void lcl_SetBoolProperty( const uno::Reference<beans::XPropertySet>& xPro
 {
     //TODO: move to ScUnoHelpFunctions?
 
-    xProp->setPropertyValue( rName, uno::Any( &bValue, getBooleanCppuType() ) );
+    xProp->setPropertyValue( rName, uno::Any( &bValue, cppu::UnoType<bool>::get() ) );
 }
 
 ScDPSaveMember::ScDPSaveMember(const OUString& rName) :

sc/source/core/data/dptabsrc.cxx
@@ -93,7 +93,7 @@ static bool lcl_GetBoolFromAny( const uno::Any& aAny )
 
 static void lcl_SetBoolInAny( uno::Any& rAny, bool bValue )
 {
-    rAny.setValue( &bValue, getBooleanCppuType() );
+    rAny.setValue( &bValue, cppu::UnoType<bool>::get() );
 }
 
 ScDPSource::ScDPSource( ScDPTableData* pD ) :
@@ -1121,11 +1121,11 @@ uno::Reference<beans::XPropertySetInfo> SAL_CALL ScDPSource::getPropertySetInfo(
 
     static const SfxItemPropertyMapEntry aDPSourceMap_Impl[] =
     {
-        { OUString(SC_UNO_DP_COLGRAND), 0,  getBooleanCppuType(),              0, 0 },
+        { OUString(SC_UNO_DP_COLGRAND), 0,  cppu::UnoType<bool>::get(),              0, 0 },
         { OUString(SC_UNO_DP_DATADESC), 0,  cppu::UnoType<OUString>::get(),    beans::PropertyAttribute::READONLY, 0 },
-        { OUString(SC_UNO_DP_IGNOREEMPTY), 0,  getBooleanCppuType(),              0, 0 },     // for sheet data only
-        { OUString(SC_UNO_DP_REPEATEMPTY), 0,  getBooleanCppuType(),              0, 0 },     // for sheet data only
-        { OUString(SC_UNO_DP_ROWGRAND), 0,  getBooleanCppuType(),              0, 0 },
+        { OUString(SC_UNO_DP_IGNOREEMPTY), 0,  cppu::UnoType<bool>::get(),              0, 0 },     // for sheet data only
+        { OUString(SC_UNO_DP_REPEATEMPTY), 0,  cppu::UnoType<bool>::get(),              0, 0 },     // for sheet data only
+        { OUString(SC_UNO_DP_ROWGRAND), 0,  cppu::UnoType<bool>::get(),              0, 0 },
         { OUString(SC_UNO_DP_ROWFIELDCOUNT),    0, cppu::UnoType<sal_Int32>::get(), READONLY, 0 },
         { OUString(SC_UNO_DP_COLUMNFIELDCOUNT), 0, cppu::UnoType<sal_Int32>::get(), READONLY, 0 },
         { OUString(SC_UNO_DP_DATAFIELDCOUNT),   0, cppu::UnoType<sal_Int32>::get(), READONLY, 0 },
@@ -1493,10 +1493,10 @@ uno::Reference<beans::XPropertySetInfo> SAL_CALL ScDPDimension::getPropertySetIn
 
     static const SfxItemPropertyMapEntry aDPDimensionMap_Impl[] =
     {
-        { OUString(SC_UNO_DP_FILTER),   0,  getCppuType((uno::Sequence<sheet::TableFilterField>*)0), 0, 0 },
+        { OUString(SC_UNO_DP_FILTER),   0,  cppu::UnoType<uno::Sequence<sheet::TableFilterField>>::get(), 0, 0 },
         { OUString(SC_UNO_DP_FLAGS),    0,  cppu::UnoType<sal_Int32>::get(),                beans::PropertyAttribute::READONLY, 0 },
         { OUString(SC_UNO_DP_FUNCTION), 0,  cppu::UnoType<sheet::GeneralFunction>::get(),   0, 0 },
-        { OUString(SC_UNO_DP_ISDATALAYOUT), 0,  getBooleanCppuType(),                      beans::PropertyAttribute::READONLY, 0 },
+        { OUString(SC_UNO_DP_ISDATALAYOUT), 0,  cppu::UnoType<bool>::get(),                      beans::PropertyAttribute::READONLY, 0 },
         { OUString(SC_UNO_DP_NUMBERFO), 0,  cppu::UnoType<sal_Int32>::get(),                beans::PropertyAttribute::READONLY, 0 },
         { OUString(SC_UNO_DP_ORIENTATION), 0,  cppu::UnoType<sheet::DataPilotFieldOrientation>::get(), 0, 0 },
         { OUString(SC_UNO_DP_ORIGINAL), 0,  cppu::UnoType<container::XNamed>::get(), beans::PropertyAttribute::READONLY, 0 },
@@ -1506,7 +1506,7 @@ uno::Reference<beans::XPropertySetInfo> SAL_CALL ScDPDimension::getPropertySetIn
         { OUString(SC_UNO_DP_USEDHIERARCHY), 0,  cppu::UnoType<sal_Int32>::get(),                0, 0 },
         { OUString(SC_UNO_DP_LAYOUTNAME), 0, cppu::UnoType<OUString>::get(), 0, 0 },
         { OUString(SC_UNO_DP_FIELD_SUBTOTALNAME), 0, cppu::UnoType<OUString>::get(), 0, 0 },
-        { OUString(SC_UNO_DP_HAS_HIDDEN_MEMBER), 0, getBooleanCppuType(), 0, 0 },
+        { OUString(SC_UNO_DP_HAS_HIDDEN_MEMBER), 0, cppu::UnoType<bool>::get(), 0, 0 },
         { OUString(), 0, css::uno::Type(), 0, 0 }
     };
     static uno::Reference<beans::XPropertySetInfo> aRef =
@@ -2194,10 +2194,10 @@ uno::Reference<beans::XPropertySetInfo> SAL_CALL ScDPLevel::getPropertySetInfo()
         //TODO: change type of AutoShow/Layout/Sorting to API struct when available
         { OUString(SC_UNO_DP_AUTOSHOW), 0,  cppu::UnoType<sheet::DataPilotFieldAutoShowInfo>::get(),     0, 0 },
         { OUString(SC_UNO_DP_LAYOUT),   0,  cppu::UnoType<sheet::DataPilotFieldLayoutInfo>::get(),       0, 0 },
-        { OUString(SC_UNO_DP_SHOWEMPTY), 0, getBooleanCppuType(),                                   0, 0 },
-        { OUString(SC_UNO_DP_REPEATITEMLABELS), 0, getBooleanCppuType(),                                   0, 0 },
+        { OUString(SC_UNO_DP_SHOWEMPTY), 0, cppu::UnoType<bool>::get(),                                   0, 0 },
+        { OUString(SC_UNO_DP_REPEATITEMLABELS), 0, cppu::UnoType<bool>::get(),                                   0, 0 },
         { OUString(SC_UNO_DP_SORTING),  0,  cppu::UnoType<sheet::DataPilotFieldSortInfo>::get(),         0, 0 },
-        { OUString(SC_UNO_DP_SUBTOTAL), 0,  getCppuType((uno::Sequence<sheet::GeneralFunction>*)0), 0, 0 },
+        { OUString(SC_UNO_DP_SUBTOTAL), 0,  cppu::UnoType<uno::Sequence<sheet::GeneralFunction>>::get(), 0, 0 },
         { OUString(), 0, css::uno::Type(), 0, 0 }
     };
     static uno::Reference<beans::XPropertySetInfo> aRef =
@@ -2642,9 +2642,9 @@ uno::Reference<beans::XPropertySetInfo> SAL_CALL ScDPMember::getPropertySetInfo(
 
     static const SfxItemPropertyMapEntry aDPMemberMap_Impl[] =
     {
-        { OUString(SC_UNO_DP_ISVISIBLE), 0,  getBooleanCppuType(),              0, 0 },
+        { OUString(SC_UNO_DP_ISVISIBLE), 0,  cppu::UnoType<bool>::get(),              0, 0 },
         { OUString(SC_UNO_DP_POSITION), 0,  cppu::UnoType<sal_Int32>::get(),        0, 0 },
-        { OUString(SC_UNO_DP_SHOWDETAILS), 0,  getBooleanCppuType(),              0, 0 },
+        { OUString(SC_UNO_DP_SHOWDETAILS), 0,  cppu::UnoType<bool>::get(),              0, 0 },
         { OUString(SC_UNO_DP_LAYOUTNAME), 0, cppu::UnoType<OUString>::get(), 0, 0 },
         { OUString(), 0, css::uno::Type(), 0, 0 }
     };

sc/source/core/data/validat.cxx
@@ -241,7 +241,7 @@ bool ScValidationData::DoScript( const ScAddress& rPos, const OUString& rInput,
     // The contents of the cell get reset if the script returns false
     bool bTmp = false;
     if ( eRet == ERRCODE_NONE &&
-             aRet.getValueType() == getCppuBooleanType() &&
+             aRet.getValueType() == cppu::UnoType<bool>::get() &&
              ( aRet >>= bTmp ) &&
              !bTmp )
     {

sc/source/core/tool/addincol.cxx
@@ -695,10 +695,10 @@ static bool lcl_ValidReturnType( const uno::Reference<reflection::XIdlClass>& xC
 
                 OUString sName = xClass->getName();
                 return (
-                    IsTypeName( sName, getCppuType((uno::Sequence< uno::Sequence<sal_Int32> >*)0) ) ||
-                    IsTypeName( sName, getCppuType((uno::Sequence< uno::Sequence<double> >*)0) ) ||
-                    IsTypeName( sName, getCppuType((uno::Sequence< uno::Sequence<OUString> >*)0) ) ||
-                    IsTypeName( sName, getCppuType((uno::Sequence< uno::Sequence<uno::Any> >*)0) ) );
+                    IsTypeName( sName, cppu::UnoType<uno::Sequence< uno::Sequence<sal_Int32> >>::get() ) ||
+                    IsTypeName( sName, cppu::UnoType<uno::Sequence< uno::Sequence<double> >>::get() ) ||
+                    IsTypeName( sName, cppu::UnoType<uno::Sequence< uno::Sequence<OUString> >>::get() ) ||
+                    IsTypeName( sName, cppu::UnoType<uno::Sequence< uno::Sequence<uno::Any> >>::get() ) );
             }
     }
 }
@@ -722,16 +722,16 @@ static ScAddInArgumentType lcl_GetArgType( const uno::Reference<reflection::XIdl
     //TODO: XIdlClass needs getType() method!
     OUString sName = xClass->getName();
 
-    if (IsTypeName( sName, getCppuType((uno::Sequence< uno::Sequence<sal_Int32> >*)0) ))
+    if (IsTypeName( sName, cppu::UnoType<uno::Sequence< uno::Sequence<sal_Int32> >>::get() ))
         return SC_ADDINARG_INTEGER_ARRAY;
 
-    if (IsTypeName( sName, getCppuType((uno::Sequence< uno::Sequence<double> >*)0) ))
+    if (IsTypeName( sName, cppu::UnoType<uno::Sequence< uno::Sequence<double> >>::get() ))
         return SC_ADDINARG_DOUBLE_ARRAY;
 
-    if (IsTypeName( sName, getCppuType((uno::Sequence< uno::Sequence<OUString> >*)0) ))
+    if (IsTypeName( sName, cppu::UnoType<uno::Sequence< uno::Sequence<OUString> >>::get() ))
         return SC_ADDINARG_STRING_ARRAY;
 
-    if (IsTypeName( sName, getCppuType((uno::Sequence< uno::Sequence<uno::Any> >*)0) ))
+    if (IsTypeName( sName, cppu::UnoType<uno::Sequence< uno::Sequence<uno::Any> >>::get() ))
         return SC_ADDINARG_MIXED_ARRAY;
 
     if (IsTypeName( sName, cppu::UnoType<uno::Any>::get()))
@@ -743,7 +743,7 @@ static ScAddInArgumentType lcl_GetArgType( const uno::Reference<reflection::XIdl
     if (IsTypeName( sName, cppu::UnoType<beans::XPropertySet>::get()))
         return SC_ADDINARG_CALLER;
 
-    if (IsTypeName( sName, getCppuType((uno::Sequence<uno::Any>*)0) ))
+    if (IsTypeName( sName, cppu::UnoType<uno::Sequence<uno::Any>>::get() ))
         return SC_ADDINARG_VARARGS;
 
     return SC_ADDINARG_NONE;
@@ -1558,7 +1558,7 @@ void ScUnoAddInCall::SetResult( const uno::Any& rNewRes )
             break;
 
         default:
-            if ( aType.equals( getCppuType( (uno::Sequence< uno::Sequence<sal_Int32> > *)0 ) ) )
+            if ( aType.equals( cppu::UnoType<uno::Sequence< uno::Sequence<sal_Int32> >>::get() ) )
             {
                 const uno::Sequence< uno::Sequence<sal_Int32> >* pRowSeq = NULL;
 
@@ -1600,7 +1600,7 @@ void ScUnoAddInCall::SetResult( const uno::Any& rNewRes )
                     }
                 }
             }
-            else if ( aType.equals( getCppuType( (uno::Sequence< uno::Sequence<double> > *)0 ) ) )
+            else if ( aType.equals( cppu::UnoType<uno::Sequence< uno::Sequence<double> >>::get() ) )
             {
                 const uno::Sequence< uno::Sequence<double> >* pRowSeq = NULL;
 
@@ -1642,7 +1642,7 @@ void ScUnoAddInCall::SetResult( const uno::Any& rNewRes )
                     }
                 }
             }
-            else if ( aType.equals( getCppuType( (uno::Sequence< uno::Sequence<OUString> > *)0 ) ) )
+            else if ( aType.equals( cppu::UnoType<uno::Sequence< uno::Sequence<OUString> >>::get() ) )
             {
                 const uno::Sequence< uno::Sequence<OUString> >* pRowSeq = NULL;
 
@@ -1688,7 +1688,7 @@ void ScUnoAddInCall::SetResult( const uno::Any& rNewRes )
                     }
                 }
             }
-            else if ( aType.equals( getCppuType( (uno::Sequence< uno::Sequence<uno::Any> > *)0 ) ) )
+            else if ( aType.equals( cppu::UnoType<uno::Sequence< uno::Sequence<uno::Any> >>::get() ) )
             {
                 xMatrix = ScSequenceToMatrix::CreateMixedMatrix( rNewRes );
             }

sc/source/filter/xml/XMLCodeNameProvider.cxx
@@ -138,7 +138,7 @@ uno::Sequence< OUString > SAL_CALL XMLCodeNameProvider::getElementNames(  )
 uno::Type SAL_CALL XMLCodeNameProvider::getElementType(  )
     throw (uno::RuntimeException, std::exception)
 {
-    return getCppuType(static_cast<uno::Sequence<beans::PropertyValue>*>(0));
+    return cppu::UnoType<uno::Sequence<beans::PropertyValue>>::get();
 }
 
 sal_Bool SAL_CALL XMLCodeNameProvider::hasElements()

commit 4204dbfc147f8a80793be742280b66536a61a00f

Resolves: tdf#132173 handle API sequence to inline array token correctly
The API token of an inline array is always a two dimensional matrix.

Regression from

    commit 3f80ac477354f4c6293d983d3ca9eef59a07dce0
    CommitDate: Wed Apr 1 08:40:39 2015 +0200

        Replace remaining getCppuType et al with cppu::UnoType

Change-Id: I9ab2a807d29847fb843625b898ab8bbd005fab2c

sc/source/core/tool/token.cxx
@@ -1258,7 +1258,7 @@ bool ScTokenArray::AddFormulaToken(
                     if ( eOpCode != ocPush )
                         bError = true;      // not an inline array
                     else if (!rToken.Data.getValueType().equals( cppu::UnoType<
-                                    uno::Sequence< uno::Any >>::get()))
+                                uno::Sequence< uno::Sequence< uno::Any >>>::get()))
                         bError = true;      // unexpected sequence type
                     else
                     {