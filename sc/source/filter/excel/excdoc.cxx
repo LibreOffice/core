fd238380ae7820f12ac1f7c52d0f7180a93f3ba3
Serge Krot <Serge.Krot@cib.de>
2020-05-13 22:52:52 +0200

    tdf#106181 XLSX export: output form controls
    Prepared general algorithm to ouput form controls into XLSX.
    For now only CHECKBOX is supported with a possibility to
    link withem to any worksheet/cell.
    
    Change-Id: Ide8739d81ffb755aeae074c4ebecf24251383e34
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/94161
    Tested-by: Jenkins
    Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>

--- sc/source/filter/excel/excdoc.cxx
+++ sc/source/filter/excel/excdoc.cxx
@@ -689,8 +689,11 @@ void ExcTable::WriteXml( XclExpXmlStream& rStrm )
     rStrm.PushStream( pWorksheet );
 
     pWorksheet->startElement( XML_worksheet,
-            XML_xmlns, rStrm.getNamespaceURL(OOX_NS(xls)).toUtf8(),
-            FSNS(XML_xmlns, XML_r), rStrm.getNamespaceURL(OOX_NS(officeRel)).toUtf8() );
+        XML_xmlns, rStrm.getNamespaceURL(OOX_NS(xls)).toUtf8(),
+        FSNS(XML_xmlns, XML_r), rStrm.getNamespaceURL(OOX_NS(officeRel)).toUtf8(),
+        FSNS(XML_xmlns, XML_xdr), "http://schemas.openxmlformats.org/drawingml/2006/spreadsheetDrawing", // rStrm.getNamespaceURL(OOX_NS(xm)).toUtf8() -> "http://schemas.microsoft.com/office/excel/2006/main",
+        FSNS(XML_xmlns, XML_x14), rStrm.getNamespaceURL(OOX_NS(xls14Lst)).toUtf8(),
+        FSNS(XML_xmlns, XML_mc), rStrm.getNamespaceURL(OOX_NS(mce)).toUtf8());
 
     SetCurrScTab( mnScTab );
     if (mxCellTable)
