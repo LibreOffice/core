d773f11009c5c07b269456761e7406c27db7422e
Attila Bakos <bakos.attilakaroly@nisz.hu>
2020-05-14 15:48:21 +0200

    tdf#133065 :fix wrap export of ole objects
    The wrap setting of the anchored OLE
    object were not preserved. Now it is
    exported as well.
    
    Note: The behind mode not preserved yet
    because that needs z-order fix as well.
    Word renders badly the options, however
    the options dialog shows the real situation.
    There is an import problem with the
    optimal wrap as well.
    
    Co-Dev: Tibor Nagy
    
    Change-Id: I7f8c48019157dda00bcc97a490c5f582eccf1b22

--- sw/qa/extras/ooxmlexport/ooxmlexport14.cxx
+++ sw/qa/extras/ooxmlexport/ooxmlexport14.cxx
@@ -44,6 +44,34 @@ protected:
     }
 };
 
+DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(Tdf133065, "tdf133065.odt")
+{
+    auto pxmldoc = parseExport("word/document.xml");
+    CPPUNIT_ASSERT(pxmldoc);
+    OUString aVal;
+
+    aVal = getXPath(pxmldoc, "/w:document/w:body/w:p[2]/w:r[2]/w:object/v:shape/w10:wrap", "type");
+    CPPUNIT_ASSERT(aVal.indexOf("square") > -1);
+    aVal = getXPath(pxmldoc, "/w:document/w:body/w:p[2]/w:r[2]/w:object/v:shape/w10:wrap", "side");
+    CPPUNIT_ASSERT(aVal.indexOf("left") > -1);
+
+    aVal = getXPath(pxmldoc, "/w:document/w:body/w:p[4]/w:r[2]/w:object/v:shape/w10:wrap", "type");
+    CPPUNIT_ASSERT(aVal.indexOf("square") > -1);
+    aVal = getXPath(pxmldoc, "/w:document/w:body/w:p[4]/w:r[2]/w:object/v:shape/w10:wrap", "side");
+    CPPUNIT_ASSERT(aVal.indexOf("right") > -1);
+
+    aVal = getXPath(pxmldoc, "/w:document/w:body/w:p[6]/w:r[2]/w:object/v:shape/w10:wrap", "type");
+    CPPUNIT_ASSERT(aVal.indexOf("square") > -1);
+    aVal = getXPath(pxmldoc, "/w:document/w:body/w:p[6]/w:r[2]/w:object/v:shape/w10:wrap", "side");
+    CPPUNIT_ASSERT(aVal.indexOf("largest") > -1);
+
+    aVal = getXPath(pxmldoc, "/w:document/w:body/w:p[10]/w:r[2]/w:object/v:shape/w10:wrap", "type");
+    CPPUNIT_ASSERT(aVal.indexOf("topAndBottom") > -1);
+
+    aVal = getXPath(pxmldoc, "/w:document/w:body/w:p[12]/w:r[2]/w:object/v:shape/w10:wrap", "type");
+    CPPUNIT_ASSERT(aVal.indexOf("square") > -1);
+}
+
 DECLARE_OOXMLIMPORT_TEST(Tdf130907,"tdf130907.docx")
 {
     uno::Reference<text::XTextRange> xPara1 = getParagraph(2);
