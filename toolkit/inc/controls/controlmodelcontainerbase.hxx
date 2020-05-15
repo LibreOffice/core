25c692c2a94ab83c2c859ac5ab334b62ac8b825e
Michael Weghorn <m.weghorn@posteo.de>
2020-05-15 15:28:14 +0200

    ControlModelContainerBase: Use property for enabled status
    Use the property BASEPROPERTY_ENABLED for getting and setting
    the status instead of handling it in a custom bool member
    variable.
    
    This way, property changes are also automatically propagated
    and can be handled by the corresponding listeners.
    
    Change-Id: I3a36ac8738a7caae987894ac9dd271d2713c1e09
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/94310
    Tested-by: Jenkins
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>

--- toolkit/inc/controls/controlmodelcontainerbase.hxx
+++ toolkit/inc/controls/controlmodelcontainerbase.hxx
@@ -83,7 +83,6 @@ protected:
     AllGroups                           maGroups;
     bool                            mbGroupsUpToDate;
 
-    bool                         m_bEnabled;
     OUString                         m_sImageURL;
     OUString                         m_sTooltip;
     sal_Int16                        m_nTabPageId;
