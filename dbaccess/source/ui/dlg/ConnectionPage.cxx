ba287b6bbee2ff160360246a16324f0f282e50c9
Julien Nabet <serval2412@yahoo.fr>
2020-05-16 19:39:29 +0200

    tdf#97416: fix Database password properties in ODB file not re-settable via UI
    Just call save_state() of the checkbox
    
    Change-Id: I3fa434b080efdcd284fba8e2f73d8e4846870576
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/94363
    Tested-by: Jenkins
    Reviewed-by: Lionel Elie Mamane <lionel@mamane.lu>
    Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

--- dbaccess/source/ui/dlg/ConnectionPage.cxx
+++ dbaccess/source/ui/dlg/ConnectionPage.cxx
@@ -238,6 +238,7 @@ namespace dbaui
             m_xUserName->save_value();
             m_xConnectionURL->save_value();
             m_xJavaDriver->save_value();
+            m_xPasswordRequired->save_state();
         }
     }
 
