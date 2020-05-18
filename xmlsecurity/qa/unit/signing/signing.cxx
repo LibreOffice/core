0ff75d6653e34b7cbb94f74ff8a71db9f738463e
Michael Stahl <Michael.Stahl@cib.de>
2020-05-18 12:57:23 +0200

    xmlsecurity: actually validate in testODFEncryptedGPG
    Follow-up to 5eda583443cd0ec323e925395ea734c20fdbcf13
    
    Change-Id: Icde98be0a8dccc58ca7c7c260e387bafb7f05b46
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/94412
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@cib.de>

--- xmlsecurity/qa/unit/signing/signing.cxx
+++ xmlsecurity/qa/unit/signing/signing.cxx
@@ -968,6 +968,7 @@ CPPUNIT_TEST_FIXTURE(SigningTest, testODFEncryptedGPG)
         uno::Reference<frame::XStorable> xDocStorable(mxComponent, uno::UNO_QUERY);
         xDocStorable->storeToURL(aTempFile.GetURL(), props);
     }
+    validate(aTempFile.GetFileName(), test::ODF);
 
     createDoc(aTempFile.GetURL());
     pBaseModel = dynamic_cast<SfxBaseModel*>(mxComponent.get());
