74597acc9318177a0266535b5387dba35305171a
Tor Lillqvist <tml@collabora.com>
2020-05-18 16:44:20 +0300

    tdf#129177: Allow extensions to specify also maximum LibreOffice version
    To be used by the potential packaging of language packs as extensions.
    Language packs are by necessity specific to a LibreOffice version as
    they contain message catalogs.
    
    Change-Id: I6037103dfe0949d288f8ad77fec63dd5da221c02
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/94425
    Tested-by: Jenkins
    Reviewed-by: Tor Lillqvist <tml@collabora.com>

--- desktop/source/deployment/misc/dp_dependencies.cxx
+++ desktop/source/deployment/misc/dp_dependencies.cxx
@@ -47,6 +47,7 @@ static char const namespaceOpenOfficeOrg[] =
     "http://openoffice.org/extensions/description/2006";
 
 static char const minimalVersionLibreOffice[] = "LibreOffice-minimal-version";
+static char const maximalVersionLibreOffice[] = "LibreOffice-maximal-version";
 
 static char const minimalVersionOpenOfficeOrg[] =
     "OpenOffice.org-minimal-version";
@@ -134,6 +135,9 @@ check(dp_misc::DescriptionInfoset const & infoset) {
             sat = satisfiesMinimalVersion(
                 getLibreOfficeMajorMinorMicro(),
                 e->getAttribute("value"));
+        } else if (e->getNamespaceURI() == namespaceLibreOffice && e->getTagName() == maximalVersionLibreOffice )
+        {
+            sat = satisfiesMaximalVersion(getLibreOfficeMajorMinorMicro(), e->getAttribute("value"));
         } else if (e->hasAttributeNS(namespaceOpenOfficeOrg,
                        minimalVersionOpenOfficeOrg))
         {
@@ -168,6 +172,11 @@ OUString getErrorText(
     {
         return produceErrorText(
                 DpResId(RID_DEPLOYMENT_DEPENDENCIES_LO_MIN),
+            dependency->getAttribute("value"));
+    } else if (dependency->getNamespaceURI() == namespaceLibreOffice && dependency->getTagName() == maximalVersionLibreOffice )
+    {
+        return produceErrorText(
+                DpResId(RID_DEPLOYMENT_DEPENDENCIES_LO_MAX),
             dependency->getAttribute("value"));
     } else if (dependency->hasAttributeNS(namespaceOpenOfficeOrg,
                    minimalVersionOpenOfficeOrg))
