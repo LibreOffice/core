96baf8c69330d50af7d31ab9838bcd0ec7c8c50b
Miklos Vajna <vmiklos@collabora.com>
2020-05-19 21:03:02 +0200

    tdf#130479 FODG import: fix handling of <draw:fill-image> in styles
    Regression from the image handling rework, the problem was that the old,
    URL-based code had explicit handling of mxBase64Stream, while the new,
    XGraphic-based code assumed that by the time we reach the end of of the
    bitmap style element, maAny already contains the graphic.
    
    Fix the problem by improving XMLBitmapStyleContext::EndElement(), so it
    loads the graphic from the base64 stream if we don't have a graphic
    already.
    
    Change-Id: I6d90f1470a5b797fb51789901081ccd7a6fd29e7
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/94548
    Tested-by: Jenkins
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

---
+++ xmloff/qa/unit/style.cxx
@@ -0,0 +1,76 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <test/bootstrapfixture.hxx>
+#include <unotest/macros_test.hxx>
+
+#include <com/sun/star/frame/Desktop.hpp>
+#include <com/sun/star/embed/XStorage.hpp>
+#include <com/sun/star/container/XNameContainer.hpp>
+
+#include <comphelper/embeddedobjectcontainer.hxx>
+#include <comphelper/propertyvalue.hxx>
+#include <comphelper/scopeguard.hxx>
+#include <comphelper/storagehelper.hxx>
+#include <officecfg/Office/Common.hxx>
+
+using namespace ::com::sun::star;
+
+char const DATA_DIRECTORY[] = "/xmloff/qa/unit/data/";
+
+/// Covers xmloff/source/style/ fixes.
+class XmloffStyleTest : public test::BootstrapFixture, public unotest::MacrosTest
+{
+private:
+    uno::Reference<lang::XComponent> mxComponent;
+
+public:
+    void setUp() override;
+    void tearDown() override;
+    uno::Reference<lang::XComponent>& getComponent() { return mxComponent; }
+    void load(const OUString& rURL);
+};
+
+void XmloffStyleTest::setUp()
+{
+    test::BootstrapFixture::setUp();
+
+    mxDesktop.set(frame::Desktop::create(mxComponentContext));
+}
+
+void XmloffStyleTest::tearDown()
+{
+    if (mxComponent.is())
+        mxComponent->dispose();
+
+    test::BootstrapFixture::tearDown();
+}
+
+void XmloffStyleTest::load(const OUString& rFileName)
+{
+    OUString aURL = m_directories.getURLFromSrc(DATA_DIRECTORY) + rFileName;
+    mxComponent = loadFromDesktop(aURL);
+}
+
+CPPUNIT_TEST_FIXTURE(XmloffStyleTest, testFillImageBase64)
+{
+    // Load a flat ODG that has base64-encoded bitmap as a fill style.
+    load("fill-image-base64.fodg");
+    uno::Reference<lang::XMultiServiceFactory> xFactory(getComponent(), uno::UNO_QUERY);
+    uno::Reference<container::XNameContainer> xBitmaps(
+        xFactory->createInstance("com.sun.star.drawing.BitmapTable"), uno::UNO_QUERY);
+
+    // Without the accompanying fix in place, this test would have failed, as the base64 stream was
+    // not considered when parsing the fill-image style.
+    CPPUNIT_ASSERT(xBitmaps->hasByName("libreoffice_0"));
+}
+
+CPPUNIT_PLUGIN_IMPLEMENT();
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
