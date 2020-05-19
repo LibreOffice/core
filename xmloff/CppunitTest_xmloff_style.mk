1e1f56af7c1c99e23d1265e6bb848ed85a1cbb99
Miklos Vajna <vmiklos@collabora.com>
2020-05-19 21:03:02 +0200

    tdf#130479 FODG import: fix handling of <draw:fill-image> in styles
    Regression from the image handling rework, the problem was that the old,
    URL-based code had explicit handling of mxBase64Stream, while the new,
    XGraphic-based code assumed that by the time we reach the end of of the
    bitmap style element, maAny already contains the graphic.
    
    Fix the problem by improving XMLBitmapStyleContext::EndElement(), so it
    loads the graphic from the base64 stream if we don't have a graphic
    already.
    
    (cherry picked from commit 96baf8c69330d50af7d31ab9838bcd0ec7c8c50b)
    
    Change-Id: I6d90f1470a5b797fb51789901081ccd7a6fd29e7
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/94459
    Tested-by: Jenkins
    Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>
    (cherry picked from commit a97cbe7221c0175a4a2d342e2d1a62b06af4e13a)

---
+++ xmloff/CppunitTest_xmloff_style.mk
@@ -0,0 +1,44 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#*************************************************************************
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+#*************************************************************************
+
+$(eval $(call gb_CppunitTest_CppunitTest,xmloff_style))
+
+$(eval $(call gb_CppunitTest_use_externals,xmloff_style,\
+	boost_headers \
+))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,xmloff_style, \
+    xmloff/qa/unit/style \
+))
+
+$(eval $(call gb_CppunitTest_use_libraries,xmloff_style, \
+    comphelper \
+    cppu \
+    embobj \
+    sal \
+    test \
+    unotest \
+))
+
+$(eval $(call gb_CppunitTest_use_sdk_api,xmloff_style))
+
+$(eval $(call gb_CppunitTest_use_ure,xmloff_style))
+$(eval $(call gb_CppunitTest_use_vcl,xmloff_style))
+
+$(eval $(call gb_CppunitTest_use_rdb,xmloff_style,services))
+
+$(eval $(call gb_CppunitTest_use_custom_headers,xmloff_style,\
+	officecfg/registry \
+))
+
+$(eval $(call gb_CppunitTest_use_configuration,xmloff_style))
+
+# vim: set noet sw=4 ts=4:
