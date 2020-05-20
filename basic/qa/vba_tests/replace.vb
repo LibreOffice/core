3ff159d35770ac3454ee909b348cb4f4ca8b0b9b
Andreas Heinisch <andreas.heinisch@yahoo.de>
2020-05-20 15:49:08 +0200

    tdf#132389 - case-insensitive operation for non-ASCII characters
    Support case-insensitive operation for non-ASCII characters in the
    Replace function in Basic.
    
    Change-Id: I48069ad7be1ae0f012c52f595cc44e6b50580b94
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/94580
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

--- basic/qa/vba_tests/replace.vb
+++ basic/qa/vba_tests/replace.vb
@@ -42,6 +42,13 @@ Function verify_testReplace() as String
     TestLog_ASSERT retStr = "aefefdBc", "count = 2: " & retStr
     retStr = Replace(srcStr, destStr, repStr, 1, 0, vbBinaryCompare)
     TestLog_ASSERT retStr = "abcbcdBc", "start = 1, count = 0, not support in Unix: " & retStr
+
+    ' tdf#132389 - case-insensitive operation for non-ASCII characters
+    retStr = Replace("ABCabc", "b", "*", 1, 2, vbTextCompare)
+    TestLog_ASSERT retStr = "A*Ca*c", "case-insensitive ASCII: " & retStr
+    retStr = Replace("АБВабв", "б", "*", 1, 2, vbTextCompare)
+    TestLog_ASSERT retStr = "А*Ва*в", "case-insensitive non-ASCII: " & retStr
+
     result = result & Chr$(10) & "Tests passed: " & passCount & Chr$(10) & "Tests failed: " & failCount & Chr$(10)
     verify_testReplace = result
 
