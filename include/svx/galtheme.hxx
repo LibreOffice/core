0c3368e9ce45cb686abc5ef2d305a8eecd123534
Aditya <adityasahu1511@gmail.com>
2020-05-21 21:51:23 +0530

    svx:unit tests for adding, acquiring, counting objects into gallery theme
    Change-Id: I256c6658a4257bc5bf8793153021efccef47bad2

--- include/svx/galtheme.hxx
+++ include/svx/galtheme.hxx
@@ -126,7 +126,7 @@ public:
 
     bool                        InsertObject(const SgaObject& rObj, sal_uInt32 nPos = SAL_MAX_UINT32);
     void                        RemoveObject(sal_uInt32 nPos);
-    SAL_DLLPRIVATE bool         ChangeObjectPos(sal_uInt32 nOldPos, sal_uInt32 nNewPos);
+    bool                        ChangeObjectPos(sal_uInt32 nOldPos, sal_uInt32 nNewPos);
 
     const OUString&             GetName() const;
 
