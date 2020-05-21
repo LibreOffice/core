ec17eb370b427f8ffa8078f6860ab4dd4b2968ca
Szymon Kłos <szymon.klos@collabora.com>
2020-05-21 09:24:31 +0200

    Add license header to jsdialogbuilder
    Change-Id: Ia62f225bd95e8c1e40c306fc334cd5700d64f91e

--- vcl/jsdialog/jsdialogbuilder.cxx
+++ vcl/jsdialog/jsdialogbuilder.cxx
@@ -1,3 +1,12 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 #include <vcl/jsdialog/jsdialogbuilder.hxx>
 #include <sal/log.hxx>
 #include <boost/property_tree/json_parser.hpp>
@@ -421,3 +430,5 @@ void JSCheckButton::set_active(bool active)
     SalInstanceCheckButton::set_active(active);
     notifyDialogState();
 }
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
