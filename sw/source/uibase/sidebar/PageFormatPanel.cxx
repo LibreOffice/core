b268d8747992dcb651eff29f8e4a600aedee2da8
Caolán McNamara <caolanm@redhat.com>
2020-05-22 15:19:53 +0100

    Resolves: tdf#133189 values not updated when units changed
    Change-Id: I7028974e9a916a630f5530c65952103ada702212

--- sw/source/uibase/sidebar/PageFormatPanel.cxx
+++ sw/source/uibase/sidebar/PageFormatPanel.cxx
@@ -198,8 +198,12 @@ void PageFormatPanel::NotifyItemUpdate(
             if (eFUnit != meFUnit)
             {
                 meFUnit = eFUnit;
+                auto nHeight = mxPaperHeight->GetCoreValue(meUnit);
                 mxPaperHeight->SetFieldUnit(meFUnit);
+                mxPaperHeight->SetMetricValue(nHeight, meUnit);
+                auto nWidth = mxPaperWidth->GetCoreValue(meUnit);
                 mxPaperWidth->SetFieldUnit(meFUnit);
+                mxPaperWidth->SetMetricValue(nWidth, meUnit);
                 SetMarginFieldUnit();
                 UpdateMarginBox();
             }
