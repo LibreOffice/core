90813c00e290e7a752afe1f0cee6041a820f474b
Pranam Lashkari <lpranam@collabora.com>
2020-05-23 03:26:36 +0530

    LOK: Allow deleting multiple slides from slidesorter
    Change-Id: Ie371e7eb5a96655fd281b4137243aa8628b6283b

--- sd/source/ui/view/drviews4.cxx
+++ sd/source/ui/view/drviews4.cxx
@@ -58,6 +58,13 @@
 #include <drawview.hxx>
 #include <svx/bmpmask.hxx>
 #include <LayerTabBar.hxx>
+#include <SlideSorter.hxx>
+#include <SlideSorterViewShell.hxx>
+#include <controller/SlideSorterController.hxx>
+#include <model/SlideSorterModel.hxx>
+#include <controller/SlsSelectionManager.hxx>
+#include <controller/SlsInsertionIndicatorHandler.hxx>
+#include <svx/svdedtv.hxx>
 
 #include <svx/svditer.hxx>
 
@@ -73,16 +80,18 @@ using namespace ::com::sun::star::drawing;
 
 void DrawViewShell::DeleteActualPage()
 {
-    sal_uInt16          nPage = maTabControl->GetCurPagePos();
-
     mpDrawView->SdrEndTextEdit();
 
     try
     {
-        Reference<XDrawPagesSupplier> xDrawPagesSupplier( GetDoc()->getUnoModel(), UNO_QUERY_THROW );
-        Reference<XDrawPages> xPages( xDrawPagesSupplier->getDrawPages(), UNO_QUERY_THROW );
-        Reference< XDrawPage > xPage( xPages->getByIndex( nPage ), UNO_QUERY_THROW );
-        xPages->remove( xPage );
+        sd::slidesorter::SlideSorter &mrSlideSorter = sd::slidesorter::SlideSorterViewShell::GetSlideSorter(this->GetViewShellBase())->GetSlideSorter();
+
+        // Not Deleting slide if it is only slide to avoid empty document
+        if (mrSlideSorter.GetModel().GetPageCount() > 1)
+        {
+            mrSlideSorter.GetView().EndTextEditAllViews();
+            mrSlideSorter.GetController().GetSelectionManager()->DeleteSelectedPages();
+        }
     }
     catch( Exception& )
     {
