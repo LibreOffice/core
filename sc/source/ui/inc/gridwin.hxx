e3601efee23c6df26867a72ab06648ceff4e3f25
Dennis Francis <dennis.francis@collabora.com>
2020-05-25 17:55:19 +0530

    lokit: scPrintTwipsMsgs: LOK_CALLBACK_*SELECTION*
    Allow print twips coordinates in the below messages:
    
    LOK_CALLBACK_CELL_SELECTION_AREA
    LOK_CALLBACK_TEXT_SELECTION
    LOK_CALLBACK_TEXT_VIEW_SELECTION
    
    Change-Id: I267a636bbeab434b305a45abe3e21cb5afc4c1e9
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/97966
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Dennis Francis <dennis.francis@collabora.com>
    (cherry picked from commit b10cd973445afce7005000d77848a56be0295b23)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/97977
    Tested-by: Jenkins

--- sc/source/ui/inc/gridwin.hxx
+++ sc/source/ui/inc/gridwin.hxx
@@ -300,8 +300,11 @@ class SAL_DLLPUBLIC_RTTI ScGridWindow : public vcl::Window, public DropTargetHel
     void            SelectForContextMenu( const Point& rPosPixel, SCCOL nCellX, SCROW nCellY );
 
     void            GetSelectionRects( ::std::vector< tools::Rectangle >& rPixelRects ) const;
+    void            GetSelectionRectsPrintTwips(::std::vector< tools::Rectangle >& rRects) const;
     void            GetPixelRectsFor( const ScMarkData &rMarkData,
                                       ::std::vector< tools::Rectangle >& rPixelRects ) const;
+    void            GetRectsAnyFor(const ScMarkData &rMarkData,
+                                  ::std::vector< tools::Rectangle >& rRects, bool bInPrintTwips) const;
     void            UpdateKitSelection(const std::vector<tools::Rectangle>& rRectangles,
                                        std::vector<tools::Rectangle>* pLogicRects = nullptr);
     bool            NeedLOKCursorInvalidation(const tools::Rectangle& rCursorRect,
