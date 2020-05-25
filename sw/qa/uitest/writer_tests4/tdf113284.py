68620614831b121cc73c29ad26b01b979d53ea74
Xisco Fauli <xiscofauli@libreoffice.org>
2020-05-25 23:56:43 +0200

    uitest: replace sleep with processEventsToIdle
    Change-Id: I48798e98718cae11b9ef7902cb48fe2d8ab9c2b9

--- sw/qa/uitest/writer_tests4/tdf113284.py
+++ sw/qa/uitest/writer_tests4/tdf113284.py
@@ -8,8 +8,6 @@ from libreoffice.uno.propertyvalue import mkPropertyValues
 from uitest.uihelper.common import get_state_as_dict
 import org.libreoffice.unotest
 import pathlib
-import time
-from uitest.debug import sleep
 
 def get_url_for_data_file(file_name):
     return pathlib.Path(org.libreoffice.unotest.makeCopyFromTDOC(file_name)).as_uri()
@@ -22,7 +20,9 @@ class tdf113284(UITestCase):
     xWriterDoc = self.xUITest.getTopFocusWindow()
     xWriterEdit = xWriterDoc.getChild("writer_edit")
 
-    sleep(2)  #we need this sleep,because without this is pagecount counted without index. Load index takes time probably.
+    xToolkit = self.xContext.ServiceManager.createInstance('com.sun.star.awt.Toolkit')
+    xToolkit.processEventsToIdle()
+
     xPageCount = document.CurrentController.PageCount
     self.ui_test.execute_dialog_through_command(".uno:GotoPage")
     xDialog = self.xUITest.getTopFocusWindow()
@@ -30,7 +30,7 @@ class tdf113284(UITestCase):
     xPageText.executeAction("TYPE", mkPropertyValues({"TEXT":str(xPageCount)})) # goto last page
     xOkBtn = xDialog.getChild("ok")
     self.ui_test.close_dialog_through_button(xOkBtn)
-    xToolkit = self.xContext.ServiceManager.createInstance('com.sun.star.awt.Toolkit')
+
     xToolkit.processEventsToIdle()
 
     self.assertEqual(get_state_as_dict(xWriterEdit)["CurrentPage"], str(xPageCount))
