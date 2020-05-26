LibreOffice/core

commit e0b0502516a10181bbd1737b93b38b2bba4c98e8

tdf#128016 Chart OOXML Import: fix duplicated category labels
Do not create category labels if the category axis is deleted.

Regression from commit: fa0a981af41a2606541eec1cb20a379a739691e0
(tdf#114166 DOCX chart import: fix missing complex categories)

Change-Id: If357b95956fccbbc6142879f22ebe1b6d715e850
Reviewed-on: https://gerrit.libreoffice.org/80443
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

oox/source/drawingml/chart/typegroupmodel.cxx
@@ -56,7 +56,8 @@ TypeGroupModel::TypeGroupModel( sal_Int32 nTypeId, bool bMSO2007Doc ) :
     mbShowNegBubbles( !bMSO2007Doc ),
     mbSmooth( !bMSO2007Doc ),
     mbVaryColors( !bMSO2007Doc ),
-    mbWireframe( !bMSO2007Doc )
+    mbWireframe( !bMSO2007Doc ),
+    mbCatAxisVisible( !bMSO2007Doc )
 {
 }
 

chart2/qa/extras/chart2import.cxx
@@ -136,6 +136,7 @@ public:
     void testDataPointInheritedColorDOCX();
     void testExternalStrRefsXLSX();
     void testSourceNumberFormatComplexCategoriesXLS();
+    void testSimpleCategoryAxis();
     void testMultilevelCategoryAxis();
     void testXaxisValues();
     void testTdf123504();
@@ -227,6 +228,7 @@ public:
     CPPUNIT_TEST(testDataPointInheritedColorDOCX);
     CPPUNIT_TEST(testExternalStrRefsXLSX);
     CPPUNIT_TEST(testSourceNumberFormatComplexCategoriesXLS);
+    CPPUNIT_TEST(testSimpleCategoryAxis);
     CPPUNIT_TEST(testMultilevelCategoryAxis);
     CPPUNIT_TEST(testXaxisValues);
     CPPUNIT_TEST(testTdf123504);
@@ -2060,6 +2062,33 @@ void Chart2ImportTest::testSourceNumberFormatComplexCategoriesXLS()
     chart2::ScaleData aScaleData = xAxis->getScaleData();
     sal_Int32 nNumberFormat =  aScaleData.Categories->getValues()->getNumberFormatKeyByIndex(-1);
     CPPUNIT_ASSERT(nNumberFormat != 0);
+}
+
+void Chart2ImportTest::testSimpleCategoryAxis()
+{
+    load("/chart2/qa/extras/data/docx/", "testSimpleCategoryAxis.docx");
+    uno::Reference< chart2::XChartDocument > xChartDoc(getChartDocFromWriter(0), uno::UNO_QUERY);
+    CPPUNIT_ASSERT(xChartDoc.is());
+
+    // Test the internal data.
+    CPPUNIT_ASSERT(xChartDoc->hasInternalDataProvider());
+
+    Reference<chart2::XInternalDataProvider> xInternalProvider(xChartDoc->getDataProvider(), uno::UNO_QUERY);
+    CPPUNIT_ASSERT(xInternalProvider.is());
+
+    Reference<chart::XComplexDescriptionAccess> xDescAccess(xInternalProvider, uno::UNO_QUERY);
+    CPPUNIT_ASSERT(xDescAccess.is());
+
+    // Get the category labels.
+    Sequence<Sequence<OUString> > aCategories = xDescAccess->getComplexRowDescriptions();
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(1), aCategories[0].getLength());
+    CPPUNIT_ASSERT(aCategories[0][0].endsWith("ria 1"));
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(1), aCategories[1].getLength());
+    CPPUNIT_ASSERT(aCategories[1][0].endsWith("ria 2"));
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(1), aCategories[2].getLength());
+    CPPUNIT_ASSERT(aCategories[2][0].endsWith("ria 3"));
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(1), aCategories[3].getLength());
+    CPPUNIT_ASSERT(aCategories[3][0].endsWith("ria 4"));
 }
 
 void Chart2ImportTest::testMultilevelCategoryAxis()

oox/inc/drawingml/chart/typegroupmodel.hxx
@@ -76,6 +76,7 @@ struct TypeGroupModel
     bool                mbSmooth;           /// True = smooth lines in line charts.
     bool                mbVaryColors;       /// True = different automatic colors for each point.
     bool                mbWireframe;        /// True = wireframe surface chart, false = filled surface chart.
+    bool                mbCatAxisVisible;   /// True = Category axis is visible.
 
     explicit            TypeGroupModel( sal_Int32 nTypeId, bool bMSO2007Doc );
                         ~TypeGroupModel();

oox/source/drawingml/chart/plotareaconverter.cxx
@@ -420,6 +420,8 @@ void PlotAreaConverter::convertFromModel( View3DModel& rView3DModel )
 
     for (auto const& axesSet : aAxesSets)
     {
+        if( !axesSet->maAxes.empty() && mrModel.maTypeGroups.size() > sal::static_int_cast<sal_uInt32>(nAxesSetIdx) )
+            mrModel.maTypeGroups[nAxesSetIdx]->mbCatAxisVisible = !axesSet->maAxes[0]->mbDeleted;
         AxesSetConverter aAxesSetConv(*this, *axesSet);
         aAxesSetConv.convertFromModel( xDiagram, rView3DModel, nAxesSetIdx, bSupportsVaryColorsByPoint );
         if(nAxesSetIdx == nStartAxesSetIdx)

oox/source/drawingml/chart/typegroupconverter.cxx
@@ -272,7 +272,7 @@ Reference< XLabeledDataSequence > TypeGroupConverter::createCategorySequence()
         first series, even if it was empty. */
     for (auto const& elem : mrModel.maSeries)
     {
-        if( elem->maSources.has( SeriesModel::CATEGORIES ) )
+        if( elem->maSources.has( SeriesModel::CATEGORIES ) && mrModel.mbCatAxisVisible)
         {
             SeriesConverter aSeriesConv(*this, *elem);
             xLabeledSeq = aSeriesConv.createCategorySequence( "categories" );
@@ -287,7 +287,7 @@ Reference< XLabeledDataSequence > TypeGroupConverter::createCategorySequence()
         }
     }
     /* n#839727 Create Category Sequence when none are found */
-    if( !xLabeledSeq.is() && !mrModel.maSeries.empty() ) {
+    if( !xLabeledSeq.is() && !mrModel.maSeries.empty() && mrModel.mbCatAxisVisible) {
         if( nMaxValues < 0 )
             nMaxValues = 2;
         SeriesModel &aModel = *mrModel.maSeries.get(0);

commit ddd00e694448163bdf6db00b6cb302804a3cfb4d

tdf#132594 Chart XLSX import: fix legend entries in pie charts
Legend entry text of pie chart wasn't imported correctly
in XLSX documents created with Excel 2007.

Regression from commit: e0b0502516a10181bbd1737b93b38b2bba4c98e8
(tdf#128016 Chart OOXML Import: fix duplicated category labels)

Change-Id: I4567437a41fe66e124dccbd148c0c49196d5c007

oox/source/drawingml/chart/typegroupmodel.cxx
@@ -55,7 +55,7 @@ TypeGroupModel::TypeGroupModel( sal_Int32 nTypeId, bool bMSO2007Doc ) :
     mbSmooth( !bMSO2007Doc ),
     mbVaryColors( !bMSO2007Doc ),
     mbWireframe( !bMSO2007Doc ),
-    mbCatAxisVisible( !bMSO2007Doc )
+    mbCatAxisVisible( true )
 {
 }
 

chart2/qa/extras/chart2export.cxx
@@ -170,6 +170,7 @@ public:
     void testTdf125812();
     void testTdf133190();
     void testTdf133191();
+    void testTdf132594();
 
     CPPUNIT_TEST_SUITE(Chart2ExportTest);
     CPPUNIT_TEST(testErrorBarXLSX);
@@ -303,6 +304,7 @@ public:
     CPPUNIT_TEST(testTdf125812);
     CPPUNIT_TEST(testTdf133190);
     CPPUNIT_TEST(testTdf133191);
+    CPPUNIT_TEST(testTdf132594);
 
     CPPUNIT_TEST_SUITE_END();
 
@@ -2789,6 +2791,15 @@ void Chart2ExportTest::testTdf133191()
 
     // Test rotation of data point label
     assertXPath(pXmlDoc, "/c:chartSpace/c:chart/c:plotArea/c:pieChart/c:ser/c:dLbls/c:dLbl[3]/c:txPr/a:bodyPr", "rot", "-4500000");
+}
+
+void Chart2ExportTest::testTdf132594()
+{
+    load("/chart2/qa/extras/data/xlsx/", "chart_pie2007.xlsx");
+    xmlDocUniquePtr pXmlDoc = parseExport("xl/charts/chart","Calc Office Open XML");
+    CPPUNIT_ASSERT(pXmlDoc);
+
+    assertXPath(pXmlDoc, "/c:chartSpace/c:chart/c:plotArea/c:pieChart/c:ser/c:cat", 1);
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(Chart2ExportTest);