f510bdfa98014ca0ae596dcd0dfd487cfc90f3eb
Mike Kaganski <mike.kaganski@collabora.com>
2020-05-28 17:51:15 +0300

    tdf#101181: use buffer device with alpha in glow effect
    Change-Id: Iddc94a5cfdee03befdf245ee086a872f0bfaf7a3
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/95051
    Tested-by: Jenkins
    Tested-by: Mike Kaganski <mike.kaganski@collabora.com>
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
    (cherry picked from commit c9f4952b98da9adad1b556414c5fcecafedca473)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/95082

--- include/drawinglayer/primitive2d/glowprimitive2d.hxx
+++ include/drawinglayer/primitive2d/glowprimitive2d.hxx
@@ -52,11 +52,6 @@ public:
     virtual basegfx::B2DRange
     getB2DRange(const geometry::ViewInformation2D& rViewInformation) const override;
 
-    ///  create decomposition
-    virtual void
-    get2DDecomposition(Primitive2DDecompositionVisitor& rVisitor,
-                       const geometry::ViewInformation2D& rViewInformation) const override;
-
     /// provide unique ID
     virtual sal_uInt32 getPrimitive2DID() const override;
 };
