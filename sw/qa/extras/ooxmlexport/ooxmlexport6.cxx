c50e7899088280b4cb9d904dfd73bc81c0158d87
Tibor Nagy <nagy.tibor2@nisz.hu>
2020-05-28 13:47:30 +0200

    tdf#133457 DOCX import: fix Frame position
    Fix default vertical frame alignment.
    regression caused by commit f5636817e7677a3081263df9004940a7d5ac54af
    (tdf#112287 DOCX frame import: fix default vAnchor).
    
    Co-author: Attila Bakos (NISZ)
    Change-Id: I6fe16ff274d6a2fa4a335c7790ecd0f01641a6fb

--- sw/qa/extras/ooxmlexport/ooxmlexport6.cxx
+++ sw/qa/extras/ooxmlexport/ooxmlexport6.cxx
@@ -954,6 +954,15 @@ DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testSyncedRelativePercent, "tdf93676-1.odt")
     assertXPath(pXmlDoc, "//wp14:pctHeight", 0);
 }
 
+DECLARE_OOXMLEXPORT_TEST(testTdf133457, "tdf133457.docx")
+{
+    xmlDocUniquePtr pXmlDocument = parseExport("word/document.xml");
+    if (!pXmlDocument)
+        return;
+
+    assertXPath(pXmlDocument, "/w:document/w:body/w:p[4]/w:pPr/w:framePr","vAnchor","text");
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
