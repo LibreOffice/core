LibreOffice/core

commit f5636817e7677a3081263df9004940a7d5ac54af

tdf#112287 DOCX frame import: fix default vAnchor
Vertical frame alignment was calculated from above
margin instead of the bottom one, resulting bad
positions, when vAnchor wasn't defined.

Co-author: Attila Bakos (NISZ)

Change-Id: I81bcf53ec826d5dcc9790c661d784b507d9ababc
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/93556
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -1171,7 +1171,7 @@ void DomainMapper_Impl::CheckUnregisteredFrameConversion( )
             aFrameProperties.push_back(comphelper::makePropertyValue(getPropertyName(PROP_VERT_ORIENT_RELATION), sal_Int16(
                 rAppendContext.pLastParagraphProperties->GetvAnchor() >= 0 ?
                     rAppendContext.pLastParagraphProperties->GetvAnchor() :
-                    pStyleProperties->GetvAnchor() >= 0 ? pStyleProperties->GetvAnchor() : text::RelOrientation::FRAME )));
+                    pStyleProperties->GetvAnchor() >= 0 ? pStyleProperties->GetvAnchor() : text::RelOrientation::PAGE_PRINT_AREA )));
 
             aFrameProperties.push_back(comphelper::makePropertyValue(getPropertyName(PROP_SURROUND),
                 rAppendContext.pLastParagraphProperties->GetWrap() != text::WrapTextMode::WrapTextMode_MAKE_FIXED_SIZE

sw/qa/extras/ooxmlexport/ooxmlexport5.cxx
@@ -1255,6 +1255,17 @@ DECLARE_OOXMLEXPORT_TEST(testSpacingGroupShapeText, "tdf131775_SpacingGroupShape
         "/a:graphic/a:graphicData/wpg:wgp/wps:wsp[1]/wps:txbx/w:txbxContent/w:p/w:r/w:rPr/w:spacing", "val", "71");
 }
 
+DECLARE_OOXMLEXPORT_TEST(testTdf112287, "tdf112287.docx")
+{
+    // tdf#131775: Check if correct spaceing.
+
+    xmlDocPtr pXmlDocument = parseExport("word/document.xml");
+    if (!pXmlDocument)
+        return;
+
+    assertXPath(pXmlDocument, "/w:document/w:body/w:p[2]/w:pPr/w:framePr","vAnchor","margin");
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

commit 211d706126985ae5a239c18364b180764ccd190d

tdf#133457 DOCX import: fix Frame position
Fix default vertical frame alignment.
regression caused by commit f5636817e7677a3081263df9004940a7d5ac54af
(tdf#112287 DOCX frame import: fix default vAnchor).

Co-author: Attila Bakos (NISZ)
Change-Id: I6fe16ff274d6a2fa4a335c7790ecd0f01641a6fb

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -1166,11 +1166,14 @@ void DomainMapper_Impl::CheckUnregisteredFrameConversion( )
                     rAppendContext.pLastParagraphProperties->Gety() :
                     pStyleProperties->IsyValid() ? pStyleProperties->Gety() : DEFAULT_VALUE));
 
+            bool bWrapExist = rAppendContext.pLastParagraphProperties->GetWrap() != text::WrapTextMode::WrapTextMode_DYNAMIC;
+
             //Default the anchor in case FramePr_vAnchor is missing ECMA 17.3.1.11
             aFrameProperties.push_back(comphelper::makePropertyValue(getPropertyName(PROP_VERT_ORIENT_RELATION), sal_Int16(
                 rAppendContext.pLastParagraphProperties->GetvAnchor() >= 0 ?
                     rAppendContext.pLastParagraphProperties->GetvAnchor() :
-                    pStyleProperties->GetvAnchor() >= 0 ? pStyleProperties->GetvAnchor() : text::RelOrientation::PAGE_PRINT_AREA )));
+                    pStyleProperties->GetvAnchor() >= 0 ? pStyleProperties->GetvAnchor() : bWrapExist ?
+                text::RelOrientation::FRAME : text::RelOrientation::PAGE_PRINT_AREA)));
 
             aFrameProperties.push_back(comphelper::makePropertyValue(getPropertyName(PROP_SURROUND),
                 rAppendContext.pLastParagraphProperties->GetWrap() != text::WrapTextMode::WrapTextMode_MAKE_FIXED_SIZE

sw/qa/extras/ooxmlexport/ooxmlexport6.cxx
@@ -954,6 +954,15 @@ DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testSyncedRelativePercent, "tdf93676-1.odt")
     assertXPath(pXmlDoc, "//wp14:pctHeight", 0);
 }
 
+DECLARE_OOXMLEXPORT_TEST(testTdf133457, "tdf133457.docx")
+{
+    xmlDocUniquePtr pXmlDocument = parseExport("word/document.xml");
+    if (!pXmlDocument)
+        return;
+
+    assertXPath(pXmlDocument, "/w:document/w:body/w:p[4]/w:pPr/w:framePr","vAnchor","text");
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */