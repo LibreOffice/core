cd2a289245be2799152b5d7e46c61acf4546c70b
Noel Grandin <noel.grandin@collabora.co.uk>
2020-05-28 13:18:41 +0200

    improve pahole script and pack a few classes
    (*) fix: I was substracting the padding space instead of adding it
    when calculating how much free space we had to improve.
    
    (*) sort input data, so we process structs located in the same DSO
    together, which reduces GDB's memory usage
    
    (*) handle another error condition, where gdbs output is sufficiently
    mixed up that we miss the end of commands terminator
    
    Change-Id: Ic4bb92b736f38a2b3d90e4a14485152b7f869b43

--- include/xmloff/maptype.hxx
+++ include/xmloff/maptype.hxx
@@ -32,9 +32,9 @@ struct XMLPropertyMapEntry
 {
     const char*     msApiName;      /// Property-Name
     sal_Int32       nApiNameLength; /// length of property name
+    enum ::xmloff::token::XMLTokenEnum meXMLName;       /// XML-Name
     sal_uInt16      mnNameSpace;    /** declares the Namespace in which this
                                         property exists */
-    enum ::xmloff::token::XMLTokenEnum meXMLName;       /// XML-Name
 
     /**
      * The lowest 14 bits specify the basic XML type of the property value, of
@@ -98,6 +98,21 @@ struct XMLPropertyMapEntry
         Property-Name exist, all except one must have this flag set.
      */
     bool            mbImportOnly;
+
+    XMLPropertyMapEntry(
+            const char*     sApiName,
+            sal_Int32       nApiNameLength_,
+            sal_uInt16      nNameSpace,
+            enum ::xmloff::token::XMLTokenEnum eXMLName,
+            sal_uInt32 nType,
+            sal_Int16       nContextId,
+            SvtSaveOptions::ODFSaneDefaultVersion nEarliestODFVersionForExport,
+            bool            bImportOnly)
+        : msApiName(sApiName), nApiNameLength(nApiNameLength_),
+        meXMLName(eXMLName), mnNameSpace(nNameSpace), mnType(nType),
+        mnContextId(nContextId), mnEarliestODFVersionForExport(nEarliestODFVersionForExport),
+        mbImportOnly(bImportOnly)
+    {}
 };
 
 
