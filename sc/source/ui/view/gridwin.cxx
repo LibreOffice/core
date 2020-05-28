3389a0422c31ff9678fb538ec4511ec6cee201ca
Tor Lillqvist <tml@iki.fi>
2020-05-28 12:53:16 +0300

    tdf#128502: Remove assertions that fire for gtktiledviewer on two docs
    If you run gtktiledviewer so that it opens a spreadsheet and a text
    document these assertions would fire.
    
    In each case the assertion is followed by code that just returns if
    the asserted condition doesn't hold, so it is unlikely that this
    change will break anything.
    
    There sure will be a lot of further changes needed to make such a use
    case work. (For starters, editing either of the two documents does not
    work.) But let's do it step by step.
    
    Change-Id: I4063dbbcac7c77b57e1f4cb3f05bd5096c08c0fe
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/95037
    Tested-by: Jenkins
    Reviewed-by: Tor Lillqvist <tml@collabora.com>

--- sc/source/ui/view/gridwin.cxx
+++ sc/source/ui/view/gridwin.cxx
@@ -5737,7 +5737,6 @@ void ScGridWindow::notifyKitCellViewCursor(const SfxViewShell* pForShell) const
     if (mpOOCursors) // cf. getCellCursor above
     {
         auto pForTabView = dynamic_cast<const ScTabViewShell *>(pForShell);
-        assert(pForTabView);
         if (!pForTabView)
             return;
         aCursor = pForTabView->GetViewData().describeCellCursorAt(
@@ -5774,7 +5773,6 @@ void ScGridWindow::updateKitOtherCursors() const
          it = SfxViewShell::GetNext(*it))
     {
         auto pOther = dynamic_cast<const ScTabViewShell *>(it);
-        assert(pOther);
         if (!pOther)
             continue;
         const ScGridWindow *pGrid = pOther->GetViewData().GetActiveWin();
@@ -5999,7 +5997,6 @@ void ScGridWindow::updateOtherKitSelections() const
          it = SfxViewShell::GetNext(*it))
     {
         auto pOther = dynamic_cast<const ScTabViewShell *>(it);
-        assert(pOther);
         if (!pOther)
             return;
 
