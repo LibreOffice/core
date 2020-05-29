LibreOffice/core

commit 059f07f9f33460c809a93e0fda1165f5c6f6d805

fixes for code creating reversed Rectangles
ie. where left > right or top > bottom

These are all places where the code is self-evidently doing the wrong
thing.

Found by adding asserts to tools::Rectangle. In theory, this is legit,
and code that wants a proper Rectangle is supposed to be first call
Justify on a Rectangle, but lots of places don't do that, and that seems
very dodgy to me.

So lets work towards Rectangles always being in a valid state.

Change-Id: I03296a624bd9b5b193e6aa8778addfb09708cdc7
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/92310
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

emfio/source/reader/emfreader.cxx
@@ -2088,6 +2088,12 @@ namespace emfio
         mpInputStream->ReadInt32(nBottom);
 
         SAL_INFO("emfio", "\t\tLeft: " << nLeft << ", top: " << nTop << ", right: " << nRight << ", bottom: " << nBottom);
+        if (nLeft > nRight || nTop > nBottom)
+        {
+            SAL_WARN("emfio", "broken rectangle");
+            mpInputStream->SetError( SVSTREAM_FILEFORMAT_ERROR );
+            return tools::Rectangle();
+        }
 
         return tools::Rectangle(nLeft, nTop, nRight, nBottom);
     }

emfio/source/reader/wmfreader.cxx
@@ -165,6 +165,12 @@ namespace emfio
         aTL = ReadYX();
         aBR.AdjustX( -1 );
         aBR.AdjustY( -1 );
+        if (aTL.X() > aBR.X() || aTL.Y() > aBR.Y())
+        {
+            SAL_WARN("vcl.wmf", "broken rectangle");
+            mpInputStream->SetError( SVSTREAM_FILEFORMAT_ERROR );
+            return tools::Rectangle();
+        }
         return tools::Rectangle( aTL, aBR );
     }
 

filter/source/graphicfilter/ipict/ipict.cxx
@@ -454,6 +454,13 @@ void PictReader::ReadRectangle(tools::Rectangle & rRect)
 
     aTopLeft=ReadPoint();
     aBottomRight=ReadPoint();
+    if (aTopLeft.X() > aBottomRight.X() || aTopLeft.Y() > aBottomRight.Y())
+    {
+        SAL_WARN("filter.pict", "broken rectangle");
+        pPict->SetError( SVSTREAM_FILEFORMAT_ERROR );
+        rRect = tools::Rectangle();
+        return;
+    }
     rRect=tools::Rectangle(aTopLeft,aBottomRight);
 
     SAL_INFO("filter.pict", "ReadRectangle: " << rRect);

sc/source/ui/view/tabview3.cxx
@@ -2674,7 +2674,7 @@ void ScTabView::PaintTopArea( SCCOL nStartCol, SCCOL nEndCol )
             long nStartX = aViewData.GetScrPos( nStartCol, 0, eWhich ).X();
             long nEndX;
             if (nEndCol >= pDoc->MaxCol())
-                nEndX = bLayoutRTL ? 0 : ( aWinSize.Width()-1 );
+                nEndX = nStartX + (bLayoutRTL ? 0 : ( aWinSize.Width()-1 ));
             else
                 nEndX = aViewData.GetScrPos( nEndCol+1, 0, eWhich ).X() - nLayoutSign;
             pColBar[eWhich]->Invalidate(
@@ -2727,7 +2727,7 @@ void ScTabView::PaintLeftArea( SCROW nStartRow, SCROW nEndRow )
             long nStartY = aViewData.GetScrPos( 0, nStartRow, eWhich ).Y();
             long nEndY;
             if (nEndRow >= pDoc->MaxRow())
-                nEndY = aWinSize.Height()-1;
+                nEndY = nStartY + aWinSize.Height() - 1;
             else
                 nEndY = aViewData.GetScrPos( 0, nEndRow+1, eWhich ).Y() - 1;
             pRowBar[eWhich]->Invalidate(

sd/source/ui/view/viewshel.cxx
@@ -929,6 +929,8 @@ void ViewShell::ArrangeGUIElements()
 {
     if (mpImpl->mbArrangeActive)
         return;
+    if (maViewSize.IsEmpty())
+        return;
     mpImpl->mbArrangeActive = true;
 
     // Calculate border for in-place editing.

sfx2/source/sidebar/Deck.cxx
@@ -110,6 +110,8 @@ tools::Rectangle Deck::GetContentArea() const
 {
     const Size aWindowSize (GetSizePixel());
     const int nBorderSize (Theme::GetInteger(Theme::Int_DeckBorderSize));
+    if (aWindowSize.IsEmpty())
+        return tools::Rectangle();
 
     return tools::Rectangle(
         Theme::GetInteger(Theme::Int_DeckLeftPadding) + nBorderSize,

svtools/source/control/ruler.cxx
@@ -594,8 +594,8 @@ void Ruler::ImplDrawTicks(vcl::RenderContext& rRenderContext, long nMin, long nM
 
                 if (nMin < nHorizontalLocation && nHorizontalLocation < nMax)
                 {
-                    ImplVDrawRect(rRenderContext, nHorizontalLocation, nBottom, nHorizontalLocation + DPIOffset, nBottom - 1 * nScale);
-                    ImplVDrawRect(rRenderContext, nHorizontalLocation, nTop,    nHorizontalLocation + DPIOffset, nTop + 1 * nScale);
+                    ImplVDrawRect(rRenderContext, nHorizontalLocation, nBottom - 1 * nScale, nHorizontalLocation + DPIOffset, nBottom);
+                    ImplVDrawRect(rRenderContext, nHorizontalLocation, nTop, nHorizontalLocation + DPIOffset, nTop + 1 * nScale);
                 }
 
                 nHorizontalLocation = nStart - n;

sw/source/uibase/uiview/view.cxx
@@ -1290,7 +1290,7 @@ void SwView::ReadUserDataSequence ( const uno::Sequence < beans::PropertyValue >
     const SwViewOption* pVOpt = m_pWrtShell->GetViewOptions();
 
     sal_Int64 nX = rRect.Left(), nY = rRect.Top(), nLeft = rVis.Left(), nTop = rVis.Top();
-    sal_Int64 nRight = LONG_MIN;
+    sal_Int64 nRight = nLeft;
     sal_Int64 nBottom = LONG_MIN;
     sal_Int16 nZoomType = static_cast< sal_Int16 >(pVOpt->GetZoomType());
     sal_Int16 nZoomFactor = static_cast < sal_Int16 > (pVOpt->GetZoom());

commit 604413cb4f1a0933a9848306970942627d233058

tdf#133448 ignore broken rectangles so can we load dodgy EMF/WMF
regression from
    commit 059f07f9f33460c809a93e0fda1165f5c6f6d805
    fixes for code creating reversed Rectangles

Change-Id: Ia4d41ac6845afcae3da1c259d8fbf48aa7db3489

emfio/source/reader/emfreader.cxx
@@ -2091,8 +2091,7 @@ namespace emfio
         if (nLeft > nRight || nTop > nBottom)
         {
             SAL_WARN("emfio", "broken rectangle");
-            mpInputStream->SetError( SVSTREAM_FILEFORMAT_ERROR );
-            return tools::Rectangle();
+            return tools::Rectangle(nLeft, nTop);
         }
 
         return tools::Rectangle(nLeft, nTop, nRight, nBottom);

emfio/source/reader/wmfreader.cxx
@@ -168,8 +168,7 @@ namespace emfio
         if (aTL.X() > aBR.X() || aTL.Y() > aBR.Y())
         {
             SAL_WARN("vcl.wmf", "broken rectangle");
-            mpInputStream->SetError( SVSTREAM_FILEFORMAT_ERROR );
-            return tools::Rectangle();
+            return tools::Rectangle(aTL, Size(0,0));
         }
         return tools::Rectangle( aTL, aBR );
     }