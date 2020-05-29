6938ae59938562063c11a1d90473627baf3cf87a
Shivam Kumar Singh <shivamhere247@gmail.com>
2020-05-29 13:10:52 +0530

    Get the property dump of the text at the cursor
    We need the property dump consisting of
    1) Character Properties
    2) Paragraph Properties
    3) Character Style Properties
    4) Paragraph Style Properties
    
    This patch gives all the properties related to 'Character Styles'
    at the cursor.
    This patch manages inheritance features, which means, it lists
    any property only at that level in which the property has been
    redefined for the last time.
    
    Change-Id: Ice2ae9ac92aec4af9d395885e06602753e636457

--- sw/source/uibase/sidebar/WriterInspectorTextPanel.hxx
+++ sw/source/uibase/sidebar/WriterInspectorTextPanel.hxx
@@ -19,22 +19,39 @@
 #pragma once
 
 #include <sfx2/weldutils.hxx>
-#include <svx/sidebar/InspectorTextPanel.hxx>
+#include <sfx2/sidebar/ControllerItem.hxx>
 
+#include <com/sun/star/beans/XPropertySet.hpp>
+#include <svx/sidebar/InspectorTextPanel.hxx>
 namespace sw
 {
 namespace sidebar
 {
-class WriterInspectorTextPanel final : public svx::sidebar::InspectorTextPanel
+class WriterInspectorTextPanel final
+    : public svx::sidebar::InspectorTextPanel,
+      public sfx2::sidebar::ControllerItem::ItemUpdateReceiverInterface
 {
 public:
     static VclPtr<vcl::Window> Create(vcl::Window* pParent,
-                                      const css::uno::Reference<css::frame::XFrame>& rxFrame);
+                                      const css::uno::Reference<css::frame::XFrame>& rxFrame,
+                                      SfxBindings* pBindings);
 
     WriterInspectorTextPanel(vcl::Window* pParent,
-                             const css::uno::Reference<css::frame::XFrame>& rxFrame);
+                             const css::uno::Reference<css::frame::XFrame>& rxFrame,
+                             SfxBindings* pBindings);
 
     // virtual ~WriterInspectorTextPanel();
+    virtual void NotifyItemUpdate(const sal_uInt16 nSId, const SfxItemState eState,
+                                  const SfxPoolItem* pState) override;
+
+    virtual void GetControlState(const sal_uInt16 /*nSId*/,
+                                 boost::property_tree::ptree& /*rState*/) override{};
+
+private:
+    sfx2::sidebar::ControllerItem maCharStyle;
+
+    static void GetPropertyValues(const css::beans::Property rProperty, const css::uno::Any& rAny,
+                                  OUString& rString);
 };
 }
 } // end of namespace svx::sidebar
