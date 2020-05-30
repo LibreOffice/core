d29468b31e2d9df1b92e7e8f9486b392a1920615
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2020-05-30 15:47:35 +0200

    sd: fix issue when PDF search doesn't send a search result + test
    This fixes the issue when PDF search doesn't send the search
    result, because of premature exit.
    
    Also add test which reproduces this issue and tests the behavior
    of searching in multiple objects.
    
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/95454
    Tested-by: Jenkins
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>
    (cherry picked from commit 18f8340a697be7c076fe111d0bc42faf877b9202)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/95940
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    (cherry picked from commit f5c158d2cdc45df318d56fbc9108f0acb133bb92)
    
    Change-Id: I3a676eeac36bde88c67e90a49583444b8595a346
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/99798
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

--- sd/source/ui/view/Outliner.cxx
+++ sd/source/ui/view/Outliner.cxx
@@ -899,7 +899,7 @@ bool SdOutliner::SearchAndReplaceOnce(std::vector<sd::SearchSelection>* pSelecti
             {
                 ProvideNextTextObject ();
 
-                if (!mbEndOfSearch)
+                if (!mbEndOfSearch && !mpImpl->mbCurrentIsVectorGraphic)
                 {
                     // Remember the current position as the last one with a
                     // text object.
