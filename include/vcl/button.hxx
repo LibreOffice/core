2381e8982b51e7cb4128082a3f672ab83c22bf38
Justin Luth <justin.luth@collabora.com>
2020-05-30 10:47:12 +0300

    Revert "tdf#125609 c10 vcl/button: enforce only one radio selected on init"
    This has already been reverted in 6.4.5
    with commit 64d7f805616fd781d87b49156860d96f081a5a45.
    
    I want to get out of this completely, because everything keeps
    on breaking based on my earlier patches.
    
    This reverts commit d35171456bc230efdaa9426da1398b2db7fa0df8,
    and the accompanying
    LO 6.4 commit a9f4913f283d34c610c4b73c755fdc828857bfce
    "tdf#125609 c14: vcl button: don't modify style directly"
    
    These reverted supporting commits are obsolete.
    (They fixed a situation caused by
    a commit that has since been fully reverted.
    I left these in for the benefit of anyone who
    might attempt that initial fix in the future.
    But now I want to get my finger prints out of
    here completely.)
    
    Change-Id: I0d765ce0cc4ab2b9d282d36a239518d43d6015ee
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/95190
    Tested-by: Jenkins
    Reviewed-by: Justin Luth <justin_luth@sil.org>
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    (cherry picked from commit b9c79a4dd4ad58156b60c2e387c0838ba911ab14)

--- include/vcl/button.hxx
+++ include/vcl/button.hxx
@@ -270,7 +270,7 @@ private:
                               const Size& rImageSize, tools::Rectangle& rStateRect,
                               tools::Rectangle& rMouseRect );
     SAL_DLLPRIVATE void     ImplDrawRadioButton(vcl::RenderContext& rRenderContext );
-    SAL_DLLPRIVATE void     ImplUncheckAllOther( const bool bSetStyle = true);
+    SAL_DLLPRIVATE void     ImplUncheckAllOther();
     SAL_DLLPRIVATE Size     ImplGetRadioImageSize() const;
     SAL_DLLPRIVATE long     ImplGetImageToTextDistance() const;
 
