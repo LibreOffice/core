d9971b98087182a6746d6de899ce089017d40a43
Caolán McNamara <caolanm@redhat.com>
2020-05-31 16:16:37 +0100

    allow sorting buttons via weld::Box
    Change-Id: I315fe2dd2e40c976edd802c1e87b7ccdb0c298fe
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/95221
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>

--- include/vcl/weld.hxx
+++ include/vcl/weld.hxx
@@ -270,6 +270,8 @@ class VCL_DLLPUBLIC Box : virtual public Container
 public:
     // Moves child to a new position in the list of children
     virtual void reorder_child(weld::Widget* pWidget, int position) = 0;
+    // Sort ok/cancel etc buttons in platform order
+    virtual void sort_native_button_order() = 0;
 };
 
 class VCL_DLLPUBLIC ScrolledWindow : virtual public Container
