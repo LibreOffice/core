d414f0513d13df0e893ccc0e43b0df6509526c9e
Noel Grandin <noel.grandin@collabora.co.uk>
2020-06-02 17:15:15 +0200

    tdf#88109 improve autofill perf (2)
    make SvxWeightItem sortable, which shaves off 1%
    
    and turn the tree search off while performing this operation, which
    shaves off about 25% of the time on my machine.
    
    Change-Id: I94358ed565a0d9f1cc0ddb36b8349e83c9deb959
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/95369
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
    (cherry picked from commit 9073a47b99e22585c6d1603dd747cb14f081fca1)

--- include/editeng/wghtitem.hxx
+++ include/editeng/wghtitem.hxx
@@ -59,6 +59,14 @@ public:
     FontWeight              GetWeight() const { return GetValue(); }
 
     void dumpAsXml(xmlTextWriterPtr pWriter) const override;
+
+    virtual bool IsSortable() const override { return true; }
+
+    virtual bool operator<(const SfxPoolItem& rCmp) const override
+    {
+        auto const & other = static_cast<const SvxWeightItem&>(rCmp);
+        return GetValue() < other.GetValue();
+    }
 };
 
 #endif // INCLUDED_EDITENG_WGHTITEM_HXX
