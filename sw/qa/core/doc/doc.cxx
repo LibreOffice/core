ab9793093bf860ac3ac532f685d91dc696b13c52
Miklos Vajna <vmiklos@collabora.com>
2020-06-02 09:07:48 +0200

    tdf#133271 sw textbox: handle TextRotateAngle shape property
    Shape with btlr text direction is imported as TextPreRotateAngle=-270
    from DOCX. Saving this to ODT turns the property name into
    TextRotateAngle and its type into double.
    
    Handle that as well to survive the ODF roundtrip of a shape+textbox
    where the textbox has a btlr text direction.
    
    (Also add a way to make multiple tests in a suite to be more independent
    from each other: depending on ordering, the new test made the old test
    fail. Calling ErrorRegistry::Reset() makes that go away.)
    
    Change-Id: Iea9212f3bbb01059caf3b0f2d809e48debf52953

--- sw/qa/core/doc/doc.cxx
+++ sw/qa/core/doc/doc.cxx
@@ -12,6 +12,8 @@
 #include <comphelper/classids.hxx>
 #include <tools/globname.hxx>
 #include <svtools/embedhlp.hxx>
+#include <editeng/frmdiritem.hxx>
+#include <vcl/errinf.hxx>
 
 #include <wrtsh.hxx>
 #include <fmtanchr.hxx>
@@ -58,6 +60,26 @@ CPPUNIT_TEST_FIXTURE(SwCoreDocTest, testMathInsertAnchorType)
     // - Actual  : 4
     // i.e. the anchor type was at-char, not as-char.
     CPPUNIT_ASSERT_EQUAL(RndStdIds::FLY_AS_CHAR, rAnchor.GetAnchorId());
+    ErrorRegistry::Reset();
+}
+
+CPPUNIT_TEST_FIXTURE(SwCoreDocTest, testTextboxTextRotateAngle)
+{
+    // Check the writing direction of the only TextFrame in the document.
+    SwDoc* pDoc = createDoc("textbox-textrotateangle.odt");
+    SwFrameFormats& rFrameFormats = *pDoc->GetSpzFrameFormats();
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(2), rFrameFormats.size());
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_uInt16>(RES_DRAWFRMFMT), rFrameFormats[0]->Which());
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_uInt16>(RES_FLYFRMFMT), rFrameFormats[1]->Which());
+    SvxFrameDirection eActual = rFrameFormats[1]->GetAttrSet().GetItem(RES_FRAMEDIR)->GetValue();
+
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 5 (btlr)
+    // - Actual  : 0 (lrtb)
+    // i.e. the writing direction was in the ODT file, but it was lost on import in the textbox
+    // case.
+    CPPUNIT_ASSERT_EQUAL(SvxFrameDirection::Vertical_LR_BT, eActual);
+    ErrorRegistry::Reset();
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();
