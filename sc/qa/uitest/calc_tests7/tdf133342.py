646fcdec4c70e7cd2774a9dd3064405505643588
Laurent BP <laurent.balland-poirier@laposte.net>
2020-06-03 19:08:59 +0200

    tdf#133342 UITest Add/Delete decimal of percent
    Space between number and percent sign should be
    preserved while adding/deleting decimals
    
    Change-Id: Ia80faf5eb9ed76bdfb8b26137e2bd4dafd53b062

---
+++ sc/qa/uitest/calc_tests7/tdf133342.py
@@ -0,0 +1,44 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+from uitest.framework import UITestCase
+from libreoffice.calc.document import get_cell_by_position
+from libreoffice.uno.propertyvalue import mkPropertyValues
+import org.libreoffice.unotest
+import pathlib
+
+def get_url_for_data_file(file_name):
+    return pathlib.Path(org.libreoffice.unotest.makeCopyFromTDOC(file_name)).as_uri()
+
+class tdf133342(UITestCase):
+    def test_tdf133342_Add_delete_decimals_of_percent_with_text(self):
+        calc_doc = self.ui_test.load_file(get_url_for_data_file("tdf133342.ods"))
+        xCalcDoc = self.xUITest.getTopFocusWindow()
+        gridwin = xCalcDoc.getChild("grid_window")
+        document = self.ui_test.get_component()
+
+        self.assertEqual(get_cell_by_position(document, 0, 0, 0).getString(), "12,35 %")
+        #Select cell A1
+        gridwin.executeAction("SELECT", mkPropertyValues({"CELL": "A1"}))
+        #Add decimals
+        self.xUITest.executeCommand(".uno:NumberFormatIncDecimals")
+        #Space should preserved before percent sign
+        self.assertEqual(get_cell_by_position(document, 0, 0, 0).getString(), "12,346 %")
+
+        #Delete decimals
+        self.xUITest.executeCommand(".uno:NumberFormatDecDecimals")
+        self.xUITest.executeCommand(".uno:NumberFormatDecDecimals")
+        self.xUITest.executeCommand(".uno:NumberFormatDecDecimals")
+        #Space should preserved before percent sign
+        self.assertEqual(get_cell_by_position(document, 0, 0, 0).getString(), "12 %")
+
+        self.xUITest.executeCommand(".uno:NumberFormatDecDecimals")
+        #Space should preserved before percent sign
+        self.assertEqual(get_cell_by_position(document, 0, 0, 0).getString(), "12 %")
+
+        self.ui_test.close_doc()
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
