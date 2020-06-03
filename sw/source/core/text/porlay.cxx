LibreOffice/core

commit 4ce8120f1e53f7b81e653b01d141643013bc69ab

tdf#45589 sw: create and paint text portions for bookmarks
Add a new SwBookmarkPortion, derived from SwControlCharPortion.

There is no character for the bookmark in the text so the portion has
length of 0, which makes things quite a bit more tricky.

Formatting:

* SwBookmarkPortion is created last in WhichFirstPortion().

* In an empty paragraph the SwTextFrame::FormatEmpty() must be disabled.

* If there's a bookmark at the end of a paragraph,
  SwTextFormatter::CalcAscent() must use the font of the previous
  character, not the paragraph font, because that could grow the line
  if it's higher.

* The SwMultiPortion complicates matters, because it uses a nested
  SwTextFormatInfo and thus we need some extra steps to prevent
  duplicate SwBookmarkPortions; this is particluarly a problem for
  rotated text portions.
  - SwTextFormatter::BuildPortions() must advance the outer
    SwTextFormatInfo's bookmark position because BuildMultiPortion() has
    already created the SwBookmarkPortion.
  - If a SwBookmarkPortion is at the start of a SwMultiPortion, it will
    be created before the SwMultiPortion but must be painted inside the
    SwMultiPortion because its font is going to be initialised as inside
    the SwMultiPortion (e.g. it will be rotated) so its position must
    also be adapted to be inside, and only
    SwTextPainter::PaintMultiPortion() does the setup for that; add a
    hack to move it in SwTextFormatter::BuildMultiPortion().

Painting:

* Using the original font seems rather difficult, hard to predict what
  some character is going to look like, and how it scales if the size is
  increased; use OpenSymbol instead.  Unfortunately OpenSymbol doesn't
  have a good glyph that could be used for both the end of a bookmark
  and start of another bookmark at the same position.

* SwLinePortion::PrePaint() wants to avoid moving the portion
  half-outside the frame but often it looks better that way (previously
  it was misaligned, now it's half-outside).

sw/source/core/text/porlay.cxx
@@ -1686,6 +1686,23 @@ TextFrameIndex SwScriptInfo::NextBookmark(TextFrameIndex const nPos) const
     return TextFrameIndex(COMPLETE_STRING);
 }
 
+auto SwScriptInfo::GetBookmark(TextFrameIndex const nPos) const -> MarkKind
+{
+    MarkKind ret{0};
+    for (auto const& it : m_Bookmarks)
+    {
+        if (nPos == it.first)
+        {
+            ret |= it.second;
+        }
+        else if (nPos < it.first)
+        {
+            break;
+        }
+    }
+    return ret;
+}
+
 // Takes a string and replaced the hidden ranges with cChar.
 sal_Int32 SwScriptInfo::MaskHiddenRanges( const SwTextNode& rNode, OUStringBuffer & rText,
                                        const sal_Int32 nStt, const sal_Int32 nEnd,

sw/qa/extras/ooxmlimport/ooxmlimport.cxx
@@ -318,8 +318,14 @@ DECLARE_OOXMLIMPORT_TEST(testN758883, "n758883.docx")
      * The problem was that direct formatting of the paragraph was not applied
      * to the numbering. This is easier to test using a layout dump.
      */
-    OUString aHeight = parseDump("/root/page/body/txt/Special", "nHeight");
-    CPPUNIT_ASSERT_EQUAL(sal_Int32(220), aHeight.toInt32()); // It was 280
+    xmlDocPtr pXmlDoc = parseLayoutDump();
+    assertXPath(pXmlDoc, "/root/page/body/txt/Special[1]", "nHeight", "220");
+
+    // check the bookmark portions are of the expected height
+    assertXPath(pXmlDoc, "/root/page/body/txt/Special[2]", "nType", "PortionType::Bookmark");
+    assertXPath(pXmlDoc, "/root/page/body/txt/Special[2]", "nHeight", "253");
+    assertXPath(pXmlDoc, "/root/page/body/txt/Special[3]", "nType", "PortionType::Bookmark");
+    assertXPath(pXmlDoc, "/root/page/body/txt/Special[3]", "nHeight", "253");
 
     /*
      * Next problem was that the page margin contained the width of the page border as well.

sw/source/core/access/accportions.cxx
@@ -170,6 +170,9 @@ void SwAccessiblePortionData::Special(
         case PortionType::ControlChar:
             sDisplay = rText + OUStringChar(m_pTextFrame->GetText()[sal_Int32(m_nViewPosition)]);
             break;
+        case PortionType::Bookmark:
+            // TODO
+            break;
         default:
             sDisplay = rText;
             break;

sw/source/core/inc/scriptinfo.hxx
@@ -24,6 +24,7 @@
 #include <deque>
 #include <unordered_set>
 #include <rtl/ustrbuf.hxx>
+#include <o3tl/typed_flags_set.hxx>
 #include <i18nlangtag/lang.h>
 #include "TextFrameIndex.hxx"
 
@@ -42,7 +43,7 @@ class SwScriptInfo
 {
 public:
     enum CompType { KANA, SPECIAL_LEFT, SPECIAL_RIGHT, NONE, SPECIAL_MIDDLE};
-    enum class MarkKind { Start, End, Point };
+    enum class MarkKind { Start = (1<<0), End = (1<<1), Point = (1<<2) };
 
 private:
     //! Records a single change in script type.
@@ -182,6 +183,7 @@ public:
     }
     TextFrameIndex NextHiddenChg(TextFrameIndex nPos) const;
     TextFrameIndex NextBookmark(TextFrameIndex nPos) const;
+    MarkKind GetBookmark(TextFrameIndex nPos) const;
     static void CalcHiddenRanges(const SwTextNode& rNode,
             MultiSelection& rHiddenMulti,
             std::vector<std::pair<sw::mark::IBookmark const*, MarkKind>> * pBookmarks);
@@ -384,6 +386,13 @@ public:
     SwFontScript WhichFont(TextFrameIndex nIdx) const;
     static SwFontScript WhichFont(sal_Int32 nIdx, OUString const & rText);
 };
+
+namespace o3tl
+{
+
+template<> struct typed_flags<SwScriptInfo::MarkKind> : is_typed_flags<SwScriptInfo::MarkKind, 0x07> {};
+
+}
 
 #endif
 

sw/source/core/inc/txttypes.hxx
@@ -33,6 +33,7 @@ enum class PortionType
     Multi       = 0x0085,
     HiddenText  = 0x0086,
     ControlChar = 0x0087,
+    Bookmark    = 0x0088,
 
     Text        = 0x8000,
     Lay         = 0x8001,

sw/source/core/text/inftxt.cxx
@@ -1375,6 +1375,14 @@ void SwTextPaintInfo::DrawViewOpt( const SwLinePortion &rPor,
                 bDraw = true;
             }
             break;
+        case PortionType::Bookmark:
+            if (!GetOpt().IsPagePreview()
+                 && !GetOpt().IsReadonly()
+                 && GetOpt().IsViewMetaChars())
+            {
+                bDraw = true;
+            }
+            break;
         case PortionType::InputField:
             // input field shading also in read-only mode
             if ( !GetOpt().IsPagePreview()
@@ -1582,6 +1590,7 @@ void SwTextFormatInfo::Init()
     m_nForcedLeftMargin = 0;
     m_nSoftHyphPos = TextFrameIndex(0);
     m_nUnderScorePos = TextFrameIndex(COMPLETE_STRING);
+    m_nLastBookmarkPos = TextFrameIndex(-1);
     m_cHookChar = 0;
     SetIdx(TextFrameIndex(0));
     SetLen(TextFrameIndex(GetText().getLength()));
@@ -2005,6 +2014,20 @@ bool SwTextFormatInfo::ChgHyph( const bool bNew )
             m_pFnt->ChgPhysFnt( m_pVsh, *m_pOut );
     }
     return bOld;
+}
+
+
+bool SwTextFormatInfo::CheckCurrentPosBookmark()
+{
+    if (m_nLastBookmarkPos != GetIdx())
+    {
+        m_nLastBookmarkPos = GetIdx();
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sw/source/core/text/inftxt.hxx
@@ -477,6 +477,7 @@ class SwTextFormatInfo : public SwTextPaintInfo
     TextFrameIndex m_nSoftHyphPos;   ///< SoftHyphPos for Hyphenation
     TextFrameIndex m_nLineStart;     ///< Current line start in rText
     TextFrameIndex m_nUnderScorePos; ///< enlarge repaint if underscore has been found
+    TextFrameIndex m_nLastBookmarkPos; ///< need to check for bookmarks at every portion
     // #i34348# Changed type from sal_uInt16 to SwTwips
     SwTwips m_nLeft;              // Left margin
     SwTwips m_nRight;             // Right margin
@@ -635,6 +636,8 @@ public:
     void SetNumDone( const bool bNew ) { m_bNumDone = bNew; }
     bool IsArrowDone() const { return m_bArrowDone; }
     void SetArrowDone( const bool bNew ) { m_bArrowDone = bNew; }
+
+    bool CheckCurrentPosBookmark();
 
     // For SwTextPortion::Hyphenate
     bool ChgHyph( const bool bNew );

sw/source/core/text/itrform2.cxx
@@ -691,6 +691,10 @@ void SwTextFormatter::BuildPortions( SwTextFormatInfo &rInf )
             CalcAscent( rInf, pPor );
 
         InsertPortion( rInf, pPor );
+        if (pPor->IsMultiPortion() && (!m_pMulti || m_pMulti->IsBidi()))
+        {
+            (void) rInf.CheckCurrentPosBookmark(); // bookmark was already created inside MultiPortion!
+        }
         pPor = NewPortion( rInf );
     }
 
@@ -750,6 +754,19 @@ void SwTextFormatter::CalcAscent( SwTextFormatInfo &rInf, SwLinePortion *pPor )
               static_cast<const SwNumberPortion*>(rInf.GetLast())->HasFont() )
     {
         const SwLinePortion* pLast = rInf.GetLast();
+        pPor->Height( pLast->Height() );
+        pPor->SetAscent( pLast->GetAscent() );
+    }
+    else if (pPor->GetWhichPor() == PortionType::Bookmark
+            && rInf.GetIdx() == TextFrameIndex(rInf.GetText().getLength()))
+    {
+        // bookmark at end of paragraph: *don't* advance iterator, use the
+        // current font instead; it's possible that there's a font size on the
+        // paragraph and it's overridden on the last line of the paragraph and
+        // we don't want to apply it via SwBookmarkPortion and grow the line
+        // height (example: n758883.docx)
+        SwLinePortion const*const pLast = rInf.GetLast();
+        assert(pLast);
         pPor->Height( pLast->Height() );
         pPor->SetAscent( pLast->GetAscent() );
     }
@@ -1012,6 +1029,7 @@ SwTextPortion *SwTextFormatter::NewTextPortion( SwTextFormatInfo &rInf )
     return pPor;
 }
 
+// first portions have no length
 SwLinePortion *SwTextFormatter::WhichFirstPortion(SwTextFormatInfo &rInf)
 {
     SwLinePortion *pPor = nullptr;
@@ -1142,6 +1160,39 @@ SwLinePortion *SwTextFormatter::WhichFirstPortion(SwTextFormatInfo &rInf)
     if (!pPor)
     {
         pPor = TryNewNoLengthPortion(rInf);
+    }
+
+    // 12. bookmarks
+    // check this *last* so that BuildMultiPortion() can find it!
+    if (!pPor && rInf.CheckCurrentPosBookmark())
+    {
+        auto const bookmark(m_pScriptInfo->GetBookmark(rInf.GetIdx()));
+        if (static_cast<bool>(bookmark))
+        {
+            sal_Unicode mark;
+            if ((bookmark & (SwScriptInfo::MarkKind::Start|SwScriptInfo::MarkKind::End))
+                        == (SwScriptInfo::MarkKind::Start|SwScriptInfo::MarkKind::End))
+            {
+                //mark = u'\u2336'; // not in OpenSymbol :(
+                mark = '|';
+                // hmm ... paint U+2345 over U+2346 should be same width?
+                // and U+237F // or U+2E20/U+2E21
+            }
+            else if (bookmark & SwScriptInfo::MarkKind::Start)
+            {
+                mark = '[';
+            }
+            else if (bookmark & SwScriptInfo::MarkKind::End)
+            {
+                mark = ']';
+            }
+            else
+            {
+                assert(bookmark & SwScriptInfo::MarkKind::Point);
+                mark = '|';
+            }
+            pPor = new SwBookmarkPortion(rInf.GetLast(), mark);
+        }
     }
 
     return pPor;

commit 97d77b670d1c2e5dd045130427c42a6b74d55889

tdf#130804 sw: fix bookmark portions in line containing only as-char fly
There were 2 problems:

* due to the first bookmark portion, the line got an additional 55 twips
  of its descent added to its height in SwLineLayout::CalcLine().

* when called from SwTextFrame::CalcHeightOfLastLine(),
  SwLineLayout::MaxAscentDescent() is supposed to ignore FlyInCnt
  portions, but it didn't ignore the bookmark portion which had the same
  height, hence the resulting line spacing was 855 instead of 0 and the
  upper margin of the next paragraph increased similarly.

(regression from 4ce8120f1e53f7b81e653b01d141643013bc69ab)

Change-Id: I52b154a48c5d712b7e3e323d2e3e08f37bf1afd1

sw/source/core/text/porlay.cxx
@@ -408,7 +408,9 @@ void SwLineLayout::CalcLine( SwTextFormatter &rLine, SwTextFormatInfo &rInf )
                 }
 
                 // Ignore drop portion height
-                if( pPos->IsDropPortion() && static_cast<SwDropPortion*>(pPos)->GetLines() > 1)
+                // tdf#130804 ... and bookmark portions
+                if ((pPos->IsDropPortion() && static_cast<SwDropPortion*>(pPos)->GetLines() > 1)
+                    || pPos->GetWhichPor() == PortionType::Bookmark)
                 {
                     pLast = pPos;
                     pPos = pPos->GetNextPortion();
@@ -618,6 +620,8 @@ void SwLineLayout::MaxAscentDescent( SwTwips& _orAscent,
     while ( pTmpPortion )
     {
         if ( !pTmpPortion->IsBreakPortion() && !pTmpPortion->IsFlyPortion() &&
+            // tdf#130804 ignore bookmark portions
+             pTmpPortion->GetWhichPor() != PortionType::Bookmark &&
              ( !_bNoFlyCntPorAndLinePor ||
                ( !pTmpPortion->IsFlyCntPortion() &&
                  !(pTmpPortion == this && pTmpPortion->GetNextPortion() ) ) ) )