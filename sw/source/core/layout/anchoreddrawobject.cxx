86fc133348e88b84fe659258409de84c0a975941
Regényi Balázs <regenyi.balazs+gerrit@gmail.com>
2020-06-03 18:15:28 +0200

    tdf#133070 DOCX import: fix shape height relative to bottom page margin
    using UNO API RelativeHeightRelation and the associated
    lo-ext attribute for OpenDocument export.
    
    See commit 43d7f4e3640c5e370fd1204739c2b0c7eb5f40e4
    (offapi: document the 4 new properties which are no longer read-only).
    
    Co-authored-by: Szabolcs Tóth
    
    Change-Id: I31db99fac5ad24971fa427671198b47906e9c057
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/95451
    Tested-by: Jenkins
    Reviewed-by: László Németh <nemeth@numbertext.org>
    (cherry picked from commit b46f4bc9760267ac5e45d43b77b5d2721ee4c386)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/98831
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- sw/source/core/layout/anchoreddrawobject.cxx
+++ sw/source/core/layout/anchoreddrawobject.cxx
@@ -652,13 +652,22 @@ SwRect SwAnchoredDrawObject::GetObjBoundRect() const
         long nTargetHeight = aCurrObjRect.GetHeight( );
         if ( GetDrawObj( )->GetRelativeHeight( ) )
         {
-            tools::Rectangle aPageRect;
+            long nHeight = 0;
             if (GetDrawObj()->GetRelativeHeightRelation() == text::RelOrientation::FRAME)
                 // Exclude margins.
-                aPageRect = GetPageFrame()->getFramePrintArea().SVRect();
+                nHeight = GetPageFrame()->getFramePrintArea().SVRect().GetHeight();
+            else if (GetDrawObj()->GetRelativeHeightRelation() == text::RelOrientation::PAGE_PRINT_AREA_BOTTOM)
+            {
+                // count required height: print area bottom = bottom margin + footer
+                SwRect aFooterRect;
+                auto pFooterFrame = GetPageFrame()->GetFooterFrame();
+                if (pFooterFrame)
+                    aFooterRect = pFooterFrame->GetPaintArea();
+                nHeight = GetPageFrame()->GetBottomMargin() + aFooterRect.Height();
+            }
             else
-                aPageRect = GetPageFrame( )->GetBoundRect( GetPageFrame()->getRootFrame()->GetCurrShell()->GetOut() ).SVRect();
-            nTargetHeight = aPageRect.GetHeight( ) * (*GetDrawObj( )->GetRelativeHeight());
+                nHeight = GetPageFrame( )->GetBoundRect( GetPageFrame()->getRootFrame()->GetCurrShell()->GetOut() ).SVRect().GetHeight();
+            nTargetHeight = nHeight * (*GetDrawObj()->GetRelativeHeight());
         }
 
         if ( nTargetWidth != aCurrObjRect.GetWidth( ) || nTargetHeight != aCurrObjRect.GetHeight( ) )
