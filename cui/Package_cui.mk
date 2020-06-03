ba534e660bfa0bbb2569b17fdd044e95ced4a6cb
Miklos Vajna <vmiklos@collabora.com>
2020-06-03 12:29:49 +0200

    cui: extract signature line SVG template into a file
    It's much easier to inspect/edit the template this way.
    
    (cherry picked from commit 04e534745d7c739d67fd85a0f9cf2c448a344f35)
    
    Conflicts:
    	Repository.mk
    
    Change-Id: I797143871058bcc4715ca13e10b8d16b428dd10e
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/97169
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

---
+++ cui/Package_cui.mk
@@ -0,0 +1,16 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_Package_Package,cui,$(SRCDIR)/cui))
+
+$(eval $(call gb_Package_add_files,cui,$(LIBO_SHARE_FOLDER)/filter,\
+	source/dialogs/signature-line.svg \
+))
+
+# vim: set noet sw=4 ts=4:
