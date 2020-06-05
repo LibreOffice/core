db8b11eb64daea66cfa17d6d58fe26c5dd7066bd
Szabolcs Toth <szabolcs450@gmail.com>
2020-06-05 12:06:22 +0200

    tdf#133670 DOCX shape import: Fix relative width from right margin
    When the width of the text box of a shape was set to "from right
    margin" in MSO Word, we set it incorrectly to "from page" in LO.
    
    Co-authored-by: Balázs Regényi
    Change-Id: Ic5d25701d46cdace6502ec55dbc0e5f0ebd7742b

--- writerfilter/source/dmapper/GraphicImport.cxx
+++ writerfilter/source/dmapper/GraphicImport.cxx
@@ -955,6 +955,14 @@ void GraphicImport::lcl_attribute(Id nName, Value& rValue)
                         xPropertySet->setPropertyValue("RelativeWidthRelation", uno::makeAny(text::RelOrientation::PAGE_LEFT));
                     }
                     break;
+                case NS_ooxml::LN_ST_SizeRelFromH_rightMargin:
+                    if (m_xShape.is())
+                    {
+                        // Same as the left margin above.
+                        uno::Reference<beans::XPropertySet> xPropertySet(m_xShape, uno::UNO_QUERY);
+                        xPropertySet->setPropertyValue("RelativeWidthRelation", uno::makeAny(text::RelOrientation::PAGE_RIGHT));
+                    }
+                    break;
                 case NS_ooxml::LN_ST_SizeRelFromH_page:
                     if (m_xShape.is())
                     {
