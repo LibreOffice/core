0d787b86be1aa90c0e5fd17cb3277208530e7301
Szymon Kłos <szymon.klos@collabora.com>
2020-06-05 06:42:45 +0200

    notebookbar: build using weld::Builder
    and provide welded implementation for writer tabbed
    
    Change-Id: Id6c71234cb1f40fd3889555785fcdbe84b801edd
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/96917
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>

---
+++ include/vcl/WeldedTabbedNotebookbar.hxx
@@ -0,0 +1,34 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef INCLUDED_SFX2_NOTEBOOKBAR_WRITERTABBEDNOTEBOOKBAR_HXX
+#define INCLUDED_SFX2_NOTEBOOKBAR_WRITERTABBEDNOTEBOOKBAR_HXX
+
+#include <vcl/dllapi.h>
+#include <rtl/ustring.hxx>
+#include <vcl/weld.hxx>
+#include <com/sun/star/frame/XFrame.hpp>
+
+/** Tabbed implementation of NotebookBar for Writer
+*/
+class VCL_DLLPUBLIC WeldedTabbedNotebookbar
+{
+    std::unique_ptr<weld::Builder> m_xBuilder;
+
+    std::unique_ptr<weld::Container> m_xContainer;
+    std::unique_ptr<weld::Notebook> m_xNotebook;
+
+public:
+    WeldedTabbedNotebookbar(VclPtr<vcl::Window>& pContainerWindow, const OUString& rUIFilePath,
+                            const css::uno::Reference<css::frame::XFrame>& rFrame);
+};
+
+#endif // INCLUDED_SFX2_NOTEBOOKBAR_SFXNOTEBOOKBAR_HXX
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
