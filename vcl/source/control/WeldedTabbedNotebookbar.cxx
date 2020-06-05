b0460af65c06f8855ab1e56cd02317e785ad5f8c
Szymon Kłos <szymon.klos@collabora.com>
2020-06-05 06:42:45 +0200

    notebookbar: build using weld::Builder
    and provide welded implementation for writer tabbed
    
    Change-Id: Id6c71234cb1f40fd3889555785fcdbe84b801edd
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/96917
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>

---
+++ vcl/source/control/WeldedTabbedNotebookbar.cxx
@@ -0,0 +1,25 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <vcl/WeldedTabbedNotebookbar.hxx>
+#include <vcl/svapp.hxx>
+#include <jsdialog/jsdialogbuilder.hxx>
+
+WeldedTabbedNotebookbar::WeldedTabbedNotebookbar(
+    VclPtr<vcl::Window>& pContainerWindow, const OUString& rUIFilePath,
+    const css::uno::Reference<css::frame::XFrame>& rFrame)
+    : m_xBuilder(new JSInstanceBuilder(pContainerWindow, VclBuilderContainer::getUIRootDir(),
+                                       rUIFilePath, rFrame))
+{
+    m_xContainer = m_xBuilder->weld_container("NotebookBar");
+    m_xNotebook = m_xBuilder->weld_notebook("ContextContainer");
+    m_xNotebook->set_current_page("HomeLabel");
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
