ea1eab85b295e94861fff3bc816f1d15a89fa290
Yusuf Keten <ketenyusuf@gmail.com>
2020-06-05 14:50:39 +0300

    tdf#133026: Tight integration of extensions - Additions Dialog first iteration
    - Add the uno command .uno:AdditionsDialog
    - Add the necessary commands to cui makefiles.
    - Add the dialog AdditionsDialog for only test
    - Implement the basic functionality to show up when clicked on the
      menu/toolbar item.
    
    Change-Id: I89dd74e49f5ff246ca355d2f8d1397621b26dea6

---
+++ cui/source/inc/AdditionsDialog.hxx
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <vcl/weld.hxx>
+
+class AdditionsDialog : public weld::GenericDialogController
+{
+    DECL_LINK(InstallButtonTestHdl, weld::Button&, void);
+
+public:
+    AdditionsDialog(weld::Widget* pParent);
+
+protected:
+private:
+    std::unique_ptr<weld::Button> m_xInstallButton;
+    std::unique_ptr<weld::Label> m_xExtensionName;
+    std::unique_ptr<weld::Label> m_xAuthor;
+    std::unique_ptr<weld::Label> m_xIntro;
+    weld::Widget* mpParent;
+};
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
