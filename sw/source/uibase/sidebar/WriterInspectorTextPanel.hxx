b42c6a355d0574ad4ac67fc3b838363a913ab287
Shivam Kumar Singh <shivamhere247@gmail.com>
2020-06-06 18:57:13 +0530

    Changed the source code location for Inspector
    The Inspector has been instead of defining in svx
    now defined such that the main implementation is
    in sw folder whereas there is a common interface in
    svx.
    
    This change has been done as a progressive step towards
    cleaner code.
    
    Change-Id: I49af862f87b4315385c2e275b1079394c326b119

---
+++ sw/source/uibase/sidebar/WriterInspectorTextPanel.hxx
@@ -0,0 +1,42 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+#pragma once
+
+#include <sfx2/weldutils.hxx>
+#include <svx/sidebar/InspectorTextPanel.hxx>
+
+namespace sw
+{
+namespace sidebar
+{
+class WriterInspectorTextPanel final : public svx::sidebar::InspectorTextPanel
+{
+public:
+    static VclPtr<vcl::Window> Create(vcl::Window* pParent,
+                                      const css::uno::Reference<css::frame::XFrame>& rxFrame);
+
+    WriterInspectorTextPanel(vcl::Window* pParent,
+                             const css::uno::Reference<css::frame::XFrame>& rxFrame);
+
+    // virtual ~WriterInspectorTextPanel();
+};
+}
+} // end of namespace svx::sidebar
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
