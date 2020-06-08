LibreOffice/core

commit 85f28ec44a2c169c91dac9346e9c71feda6a6cab

Clean up C-style casts from pointers to void
Change-Id: I5f4029fc583952ae6392dbf2d478155982ccecef

svx/source/items/customshapeitem.cxx
@@ -56,10 +56,10 @@ SdrCustomShapeGeometryItem::SdrCustomShapeGeometryItem( const uno::Sequence< bea
         }
         if ( rPropVal.Value.getValueType() == ::getCppuType((const ::com::sun::star::uno::Sequence < beans::PropertyValue >*)0) )
         {
-            uno::Sequence< beans::PropertyValue >& rPropSeq = *( uno::Sequence< beans::PropertyValue >*)rPropVal.Value.getValue();
+            uno::Sequence< beans::PropertyValue > const & rPropSeq = *static_cast<uno::Sequence< beans::PropertyValue > const *>(rPropVal.Value.getValue());
             for ( j = 0; j < rPropSeq.getLength(); j++ )
             {
-                beans::PropertyValue& rPropVal2 = rPropSeq[ j ];
+                beans::PropertyValue const & rPropVal2 = rPropSeq[ j ];
                 aPropPairHashMap[ PropertyPair( rPropVal.Name, rPropVal2.Name ) ] = j;
             }
         }
@@ -95,9 +95,9 @@ com::sun::star::uno::Any* SdrCustomShapeGeometryItem::GetPropertyValueByName( co
             PropertyPairHashMap::iterator aHashIter( aPropPairHashMap.find( PropertyPair( rSequenceName, rPropName ) ) );
             if ( aHashIter != aPropPairHashMap.end() )
             {
-                ::com::sun::star::uno::Sequence < beans::PropertyValue >& rSecSequence =
-                    *((::com::sun::star::uno::Sequence < beans::PropertyValue >*)pSeqAny->getValue());
-                pRet = &rSecSequence[ (*aHashIter).second ].Value;
+                ::com::sun::star::uno::Sequence < beans::PropertyValue > const & rSecSequence =
+                    *static_cast<css::uno::Sequence < beans::PropertyValue > const *>(pSeqAny->getValue());
+                pRet = const_cast<css::uno::Any *>(&rSecSequence[ (*aHashIter).second ].Value);
             }
         }
     }
@@ -115,8 +115,8 @@ const com::sun::star::uno::Any* SdrCustomShapeGeometryItem::GetPropertyValueByNa
             PropertyPairHashMap::const_iterator aHashIter( aPropPairHashMap.find( PropertyPair( rSequenceName, rPropName ) ) );
             if ( aHashIter != aPropPairHashMap.end() )
             {
-                ::com::sun::star::uno::Sequence < beans::PropertyValue >& rSecSequence =
-                    *((::com::sun::star::uno::Sequence < beans::PropertyValue >*)pSeqAny->getValue());
+                ::com::sun::star::uno::Sequence < beans::PropertyValue > const & rSecSequence =
+                    *static_cast<css::uno::Sequence < beans::PropertyValue > const *>(pSeqAny->getValue());
                 pRet = &rSecSequence[ (*aHashIter).second ].Value;
             }
         }
@@ -132,8 +132,8 @@ void SdrCustomShapeGeometryItem::SetPropertyValue( const com::sun::star::beans::
         sal_Int32 i;
         if ( pAny->getValueType() == ::getCppuType((const ::com::sun::star::uno::Sequence < beans::PropertyValue >*)0) )
         {   // old property is a sequence->each entry has to be removed from the HashPairMap
-            ::com::sun::star::uno::Sequence < beans::PropertyValue >& rSecSequence =
-                *((::com::sun::star::uno::Sequence < beans::PropertyValue >*)pAny->getValue());
+            ::com::sun::star::uno::Sequence < beans::PropertyValue > const & rSecSequence =
+                *static_cast<css::uno::Sequence < beans::PropertyValue > const *>(pAny->getValue());
             for ( i = 0; i < rSecSequence.getLength(); i++ )
             {
                 PropertyPairHashMap::iterator aHashIter( aPropPairHashMap.find( PropertyPair( rPropVal.Name, rSecSequence[ i ].Name ) ) );
@@ -144,11 +144,11 @@ void SdrCustomShapeGeometryItem::SetPropertyValue( const com::sun::star::beans::
         *pAny = rPropVal.Value;
         if ( rPropVal.Value.getValueType() == ::getCppuType((const ::com::sun::star::uno::Sequence < beans::PropertyValue >*)0) )
         {   // the new property is a sequence->each entry has to be inserted into the HashPairMap
-            ::com::sun::star::uno::Sequence < beans::PropertyValue >& rSecSequence =
-                *((::com::sun::star::uno::Sequence < beans::PropertyValue >*)pAny->getValue());
+            ::com::sun::star::uno::Sequence < beans::PropertyValue > const & rSecSequence =
+                *static_cast<css::uno::Sequence < beans::PropertyValue > const *>(pAny->getValue());
             for ( i = 0; i < rSecSequence.getLength(); i++ )
             {
-                beans::PropertyValue& rPropVal2 = rSecSequence[ i ];
+                beans::PropertyValue const & rPropVal2 = rSecSequence[ i ];
                 aPropPairHashMap[ PropertyPair( rPropVal.Name, rPropVal2.Name ) ] = i;
             }
         }
@@ -201,18 +201,18 @@ void SdrCustomShapeGeometryItem::SetPropertyValue( const OUString& rSequenceName
                 PropertyPairHashMap::iterator aHashIter( aPropPairHashMap.find( PropertyPair( rSequenceName, rPropVal.Name ) ) );
                 if ( aHashIter != aPropPairHashMap.end() )
                 {
-                    ::com::sun::star::uno::Sequence < beans::PropertyValue >& rSecSequence =
-                        *((::com::sun::star::uno::Sequence < beans::PropertyValue >*)pSeqAny->getValue());
-                    rSecSequence[ (*aHashIter).second ].Value = rPropVal.Value;
+                    ::com::sun::star::uno::Sequence < beans::PropertyValue > const & rSecSequence =
+                        *static_cast<css::uno::Sequence < beans::PropertyValue > const *>(pSeqAny->getValue());
+                    const_cast<css::uno::Sequence<css::beans::PropertyValue> &>(rSecSequence)[ (*aHashIter).second ].Value = rPropVal.Value;
                 }
                 else
                 {
-                    ::com::sun::star::uno::Sequence < beans::PropertyValue >& rSecSequence =
-                        *((::com::sun::star::uno::Sequence < beans::PropertyValue >*)pSeqAny->getValue());
+                    ::com::sun::star::uno::Sequence < beans::PropertyValue > const & rSecSequence =
+                        *static_cast<css::uno::Sequence < beans::PropertyValue > const *>(pSeqAny->getValue());
 
                     sal_Int32 nCount = rSecSequence.getLength();
-                    rSecSequence.realloc( nCount + 1 );
-                    rSecSequence[ nCount ] = rPropVal;
+                    const_cast<css::uno::Sequence<css::beans::PropertyValue> &>(rSecSequence).realloc( nCount + 1 );
+                    const_cast<css::uno::Sequence<css::beans::PropertyValue> &>(rSecSequence)[ nCount ] = rPropVal;
 
                     aPropPairHashMap[ PropertyPair( rSequenceName, rPropVal.Name ) ] = nCount;
                 }
@@ -233,8 +233,8 @@ void SdrCustomShapeGeometryItem::ClearPropertyValue( const OUString& rPropName )
             {
                 if ( pSeqAny->getValueType() == ::getCppuType((const ::com::sun::star::uno::Sequence < beans::PropertyValue >*)0) )
                 {
-                    ::com::sun::star::uno::Sequence < beans::PropertyValue >& rSecSequence =
-                        *((::com::sun::star::uno::Sequence < beans::PropertyValue >*)pSeqAny->getValue());
+                    ::com::sun::star::uno::Sequence < beans::PropertyValue > const & rSecSequence =
+                        *static_cast<css::uno::Sequence < beans::PropertyValue > const *>(pSeqAny->getValue());
 
                     sal_Int32 i;
                     for ( i = 0; i < rSecSequence.getLength(); i++ )

include/svx/svdetc.hxx
@@ -248,7 +248,7 @@ inline SdrGlobalData& GetSdrGlobalData()
     if (*ppAppData==NULL) {
         *ppAppData=new SdrGlobalData;
     }
-    return *((SdrGlobalData*)*ppAppData);
+    return *static_cast<SdrGlobalData*>(*ppAppData);
 }
 
 namespace sdr

svx/source/dialog/ctredlin.cxx
@@ -53,7 +53,7 @@ SvxRedlinEntry::SvxRedlinEntry()
 
 SvxRedlinEntry::~SvxRedlinEntry()
 {
-    RedlinData* pRedDat=(RedlinData*) GetUserData();
+    RedlinData* pRedDat=static_cast<RedlinData*>(GetUserData());
     delete pRedDat;
 }
 
@@ -130,8 +130,8 @@ sal_Int32 SvxRedlinTable::ColCompare(SvTreeListEntry* pLeft,SvTreeListEntry* pRi
     {
         if(nDatePos==GetSortedCol())
         {
-            RedlinData *pLeftData=(RedlinData *)(pLeft->GetUserData());
-            RedlinData *pRightData=(RedlinData *)(pRight->GetUserData());
+            RedlinData *pLeftData=static_cast<RedlinData *>(pLeft->GetUserData());
+            RedlinData *pRightData=static_cast<RedlinData *>(pRight->GetUserData());
 
             if(pLeftData!=NULL && pRightData!=NULL)
             {

svx/source/dialog/docrecovery.cxx
@@ -744,7 +744,7 @@ void RecovDocListEntry::Paint(
     const OUString*     pTxt  = 0;
           RecovDocList* pList = static_cast< RecovDocList* >(&aDevice);
 
-    TURLInfo* pInfo  = (TURLInfo*)pEntry->GetUserData();
+    TURLInfo* pInfo  = static_cast<TURLInfo*>(pEntry->GetUserData());
     switch(pInfo->RecoveryState)
     {
         case E_SUCCESSFULLY_RECOVERED :
@@ -1088,7 +1088,7 @@ void RecoveryDialog::updateItems()
         if ( !pEntry )
             continue;
 
-        TURLInfo* pInfo = (TURLInfo*)pEntry->GetUserData();
+        TURLInfo* pInfo = static_cast<TURLInfo*>(pEntry->GetUserData());
         if ( !pInfo )
             continue;
 
@@ -1112,7 +1112,7 @@ void RecoveryDialog::stepNext(TURLInfo* pItem)
         if (!pEntry)
             continue;
 
-        TURLInfo* pInfo = (TURLInfo*)pEntry->GetUserData();
+        TURLInfo* pInfo = static_cast<TURLInfo*>(pEntry->GetUserData());
         if (pInfo->ID != pItem->ID)
             continue;
 

svx/source/dialog/rubydialog.cxx
@@ -273,7 +273,7 @@ void SvxRubyDialog::ClearCharStyleList()
     for(sal_uInt16 i = 0; i < m_pCharStyleLB->GetEntryCount(); i++)
     {
         void* pData = m_pCharStyleLB->GetEntryData(i);
-        delete (OUString*)pData;
+        delete static_cast<OUString*>(pData);
     }
     m_pCharStyleLB->Clear();
 }
@@ -456,7 +456,7 @@ void SvxRubyDialog::Update()
             }
             if(nPosition > -2 && pProps[nProp].Name == cRubyIsAbove )
             {
-                bool bTmp = *(sal_Bool*)pProps[nProp].Value.getValue();
+                bool bTmp = *static_cast<sal_Bool const *>(pProps[nProp].Value.getValue());
                 if(!nRuby)
                     nPosition = bTmp ? 0 : 1;
                 else if( (!nPosition && !bTmp) || (nPosition == 1 && bTmp)  )
@@ -490,7 +490,7 @@ void SvxRubyDialog::Update()
     {
         for(sal_uInt16 i = 0; i < m_pCharStyleLB->GetEntryCount(); i++)
         {
-            const OUString* pCoreName = (const OUString*)m_pCharStyleLB->GetEntryData(i);
+            const OUString* pCoreName = static_cast<const OUString*>(m_pCharStyleLB->GetEntryData(i));
             if(pCoreName && sCharStyleName == *pCoreName)
             {
                 m_pCharStyleLB->SelectEntryPos(i);
@@ -621,7 +621,7 @@ IMPL_LINK_NOARG(SvxRubyDialog, CharStyleHdl_Impl)
     AssertOneEntry();
     OUString sStyleName;
     if(LISTBOX_ENTRY_NOTFOUND != m_pCharStyleLB->GetSelectEntryPos())
-        sStyleName = *(OUString*) m_pCharStyleLB->GetSelectEntryData();
+        sStyleName = *static_cast<OUString*>(m_pCharStyleLB->GetSelectEntryData());
     Sequence<PropertyValues>&  aRubyValues = pImpl->GetRubyValues();
     for(sal_Int32 nRuby = 0; nRuby < aRubyValues.getLength(); nRuby++)
     {

svx/source/fmcomp/gridcols.cxx
@@ -49,7 +49,7 @@ const ::comphelper::StringSequence& getColumnTypes()
 // Vergleichen von PropertyInfo
 extern "C" int SAL_CALL NameCompare(const void* pFirst, const void* pSecond)
 {
-    return ((OUString*)pFirst)->compareTo(*(OUString*)pSecond);
+    return static_cast<OUString const *>(pFirst)->compareTo(*static_cast<OUString const *>(pSecond));
 }
 
 namespace
@@ -58,8 +58,8 @@ namespace
     sal_Int32 lcl_findPos(const OUString& aStr, const Sequence< OUString>& rList)
     {
         const OUString* pStrList = rList.getConstArray();
-        OUString* pResult = (OUString*) bsearch(&aStr, (void*)pStrList, rList.getLength(), sizeof(OUString),
-            &NameCompare);
+        OUString* pResult = static_cast<OUString*>(bsearch(&aStr, (void*)pStrList, rList.getLength(), sizeof(OUString),
+            &NameCompare));
 
         if (pResult)
             return (pResult - pStrList);

svx/source/fmcomp/gridctrl.cxx
@@ -3445,7 +3445,7 @@ void DbGridControl::BeginCursorAction()
 {
     if (m_pFieldListeners)
     {
-        ColumnFieldValueListeners* pListeners = (ColumnFieldValueListeners*)m_pFieldListeners;
+        ColumnFieldValueListeners* pListeners = static_cast<ColumnFieldValueListeners*>(m_pFieldListeners);
         ColumnFieldValueListeners::const_iterator aIter = pListeners->begin();
         while (aIter != pListeners->end())
         {
@@ -3464,7 +3464,7 @@ void DbGridControl::EndCursorAction()
 {
     if (m_pFieldListeners)
     {
-        ColumnFieldValueListeners* pListeners = (ColumnFieldValueListeners*)m_pFieldListeners;
+        ColumnFieldValueListeners* pListeners = static_cast<ColumnFieldValueListeners*>(m_pFieldListeners);
         ColumnFieldValueListeners::const_iterator aIter = pListeners->begin();
         while (aIter != pListeners->end())
         {
@@ -3481,7 +3481,7 @@ void DbGridControl::EndCursorAction()
 
 void DbGridControl::ConnectToFields()
 {
-    ColumnFieldValueListeners* pListeners = (ColumnFieldValueListeners*)m_pFieldListeners;
+    ColumnFieldValueListeners* pListeners = static_cast<ColumnFieldValueListeners*>(m_pFieldListeners);
     DBG_ASSERT(!pListeners || pListeners->empty(), "DbGridControl::ConnectToFields : please call DisconnectFromFields first !");
 
     if (!pListeners)
@@ -3513,7 +3513,7 @@ void DbGridControl::DisconnectFromFields()
     if (!m_pFieldListeners)
         return;
 
-    ColumnFieldValueListeners* pListeners = (ColumnFieldValueListeners*)m_pFieldListeners;
+    ColumnFieldValueListeners* pListeners = static_cast<ColumnFieldValueListeners*>(m_pFieldListeners);
     while (!pListeners->empty())
     {
 #ifdef DBG_UTIL
@@ -3558,7 +3558,7 @@ void DbGridControl::FieldValueChanged(sal_uInt16 _nId, const PropertyChangeEvent
 
 void DbGridControl::FieldListenerDisposing(sal_uInt16 _nId)
 {
-    ColumnFieldValueListeners* pListeners = (ColumnFieldValueListeners*)m_pFieldListeners;
+    ColumnFieldValueListeners* pListeners = static_cast<ColumnFieldValueListeners*>(m_pFieldListeners);
     if (!pListeners)
     {
         OSL_FAIL("DbGridControl::FieldListenerDisposing : invalid call (have no listener array) !");

svx/source/form/filtnav.cxx
@@ -1024,7 +1024,7 @@ const int nxDBmp = 12;
 void FmFilterItemsString::Paint(
     const Point& rPos, SvTreeListBox& rDev, const SvViewDataEntry* /*pView*/, const SvTreeListEntry* pEntry)
 {
-    FmFilterItems* pRow = (FmFilterItems*)pEntry->GetUserData();
+    FmFilterItems* pRow = static_cast<FmFilterItems*>(pEntry->GetUserData());
     FmFormItem* pForm = static_cast<FmFormItem*>(pRow->GetParent());
 
     // current filter is significant painted
@@ -1188,7 +1188,7 @@ bool FmFilterNavigator::EditingEntry( SvTreeListEntry* pEntry, Selection& rSelec
     if (!SvTreeListBox::EditingEntry( pEntry, rSelection ))
         return false;
 
-    return pEntry && ((FmFilterData*)pEntry->GetUserData())->ISA(FmFilterItem);
+    return pEntry && static_cast<FmFilterData*>(pEntry->GetUserData())->ISA(FmFilterItem);
 }
 
 
@@ -1200,7 +1200,7 @@ bool FmFilterNavigator::EditedEntry( SvTreeListEntry* pEntry, const OUString& rN
     if (EditingCanceled())
         return true;
 
-    DBG_ASSERT(((FmFilterData*)pEntry->GetUserData())->ISA(FmFilterItem),
+    DBG_ASSERT(static_cast<FmFilterData*>(pEntry->GetUserData())->ISA(FmFilterItem),
                     "FmFilterNavigator::EditedEntry() wrong entry");
 
     OUString aText(comphelper::string::strip(rNewText, ' '));
@@ -1213,7 +1213,7 @@ bool FmFilterNavigator::EditedEntry( SvTreeListEntry* pEntry, const OUString& rN
     {
         OUString aErrorMsg;
 
-        if (m_pModel->ValidateText((FmFilterItem*)pEntry->GetUserData(), aText, aErrorMsg))
+        if (m_pModel->ValidateText(static_cast<FmFilterItem*>(pEntry->GetUserData()), aText, aErrorMsg))
         {
             GrabFocus();
             // this will set the text at the FmFilterItem, as well as update any filter controls
@@ -1241,7 +1241,7 @@ bool FmFilterNavigator::EditedEntry( SvTreeListEntry* pEntry, const OUString& rN
 IMPL_LINK( FmFilterNavigator, OnRemove, SvTreeListEntry*, pEntry )
 {
     // now remove the entry
-    m_pModel->Remove((FmFilterData*) pEntry->GetUserData());
+    m_pModel->Remove(static_cast<FmFilterData*>(pEntry->GetUserData()));
     return 0L;
 }
 
@@ -1341,7 +1341,7 @@ sal_Int8 FmFilterNavigator::AcceptDrop( const AcceptDropEvent& rEvt )
     if (!pDropTarget)
         return DND_ACTION_NONE;
 
-    FmFilterData* pData = (FmFilterData*)pDropTarget->GetUserData();
+    FmFilterData* pData = static_cast<FmFilterData*>(pDropTarget->GetUserData());
     FmFormItem* pForm = NULL;
     if (pData->ISA(FmFilterItem))
     {
@@ -1409,9 +1409,9 @@ void FmFilterNavigator::InitEntry(SvTreeListEntry* pEntry,
     SvTreeListBox::InitEntry( pEntry, rStr, rImg1, rImg2, eButtonKind );
     SvLBoxString* pString = NULL;
 
-    if (((FmFilterData*)pEntry->GetUserData())->ISA(FmFilterItem))
-        pString = new FmFilterString(pEntry, 0, rStr, ((FmFilterItem*)pEntry->GetUserData())->GetFieldName());
-    else if (((FmFilterData*)pEntry->GetUserData())->ISA(FmFilterItems))
+    if (static_cast<FmFilterData*>(pEntry->GetUserData())->ISA(FmFilterItem))
+        pString = new FmFilterString(pEntry, 0, rStr, static_cast<FmFilterItem*>(pEntry->GetUserData())->GetFieldName());
+    else if (static_cast<FmFilterData*>(pEntry->GetUserData())->ISA(FmFilterItems))
         pString = new FmFilterItemsString(pEntry, 0, rStr );
 
     if (pString)
@@ -1429,12 +1429,12 @@ bool FmFilterNavigator::Select( SvTreeListEntry* pEntry, bool bSelect )
         if (bSelect)
         {
             FmFormItem* pFormItem = NULL;
-            if (((FmFilterData*)pEntry->GetUserData())->ISA(FmFilterItem))
+            if (static_cast<FmFilterData*>(pEntry->GetUserData())->ISA(FmFilterItem))
                 pFormItem = static_cast<FmFormItem*>(static_cast<FmFilterItem*>(pEntry->GetUserData())->GetParent()->GetParent());
-            else if (((FmFilterData*)pEntry->GetUserData())->ISA(FmFilterItems))
+            else if (static_cast<FmFilterData*>(pEntry->GetUserData())->ISA(FmFilterItems))
                 pFormItem = static_cast<FmFormItem*>(static_cast<FmFilterItem*>(pEntry->GetUserData())->GetParent()->GetParent());
-            else if (((FmFilterData*)pEntry->GetUserData())->ISA(FmFormItem))
-                pFormItem = (FmFormItem*)pEntry->GetUserData();
+            else if (static_cast<FmFilterData*>(pEntry->GetUserData())->ISA(FmFormItem))
+                pFormItem = static_cast<FmFormItem*>(pEntry->GetUserData());
 
             if (pFormItem)
             {
@@ -1442,7 +1442,7 @@ bool FmFilterNavigator::Select( SvTreeListEntry* pEntry, bool bSelect )
                 if (static_cast<FmFilterData*>(pEntry->GetUserData())->ISA(FmFilterItem))
                     m_pModel->SetCurrentItems(static_cast<FmFilterItems*>(static_cast<FmFilterItem*>(pEntry->GetUserData())->GetParent()));
                 else if (static_cast<FmFilterData*>(pEntry->GetUserData())->ISA(FmFilterItems))
-                    m_pModel->SetCurrentItems((FmFilterItems*)pEntry->GetUserData());
+                    m_pModel->SetCurrentItems(static_cast<FmFilterItems*>(pEntry->GetUserData()));
                 else if (static_cast<FmFilterData*>(pEntry->GetUserData())->ISA(FmFormItem))
                     m_pModel->SetCurrentController(static_cast<FmFormItem*>(pEntry->GetUserData())->GetController());
             }
@@ -1489,7 +1489,7 @@ SvTreeListEntry* FmFilterNavigator::FindEntry(const FmFilterData* pItem) const
     {
         for (pEntry = First(); pEntry != NULL; pEntry = Next( pEntry ))
         {
-            FmFilterData* pEntryItem = (FmFilterData*)pEntry->GetUserData();
+            FmFilterData* pEntryItem = static_cast<FmFilterData*>(pEntry->GetUserData());
             if (pEntryItem == pItem)
                 break;
         }
@@ -1534,7 +1534,7 @@ FmFormItem* FmFilterNavigator::getSelectedFilterItems(::std::vector<FmFilterItem
          bHandled && pEntry != NULL;
          pEntry = NextSelected(pEntry))
     {
-        FmFilterItem* pFilter = PTR_CAST(FmFilterItem, (FmFilterData*)pEntry->GetUserData());
+        FmFilterItem* pFilter = PTR_CAST(FmFilterItem, static_cast<FmFilterData*>(pEntry->GetUserData()));
         if (pFilter)
         {
             FmFormItem* pForm = PTR_CAST(FmFormItem,pFilter->GetParent()->GetParent());
@@ -1643,9 +1643,9 @@ void FmFilterNavigator::Command( const CommandEvent& rEvt )
                  pEntry = NextSelected(pEntry))
             {
                 // don't delete forms
-                FmFormItem* pForm = PTR_CAST(FmFormItem, (FmFilterData*)pEntry->GetUserData());
+                FmFormItem* pForm = PTR_CAST(FmFormItem, static_cast<FmFilterData*>(pEntry->GetUserData()));
                 if (!pForm)
-                    aSelectList.push_back((FmFilterData*)pEntry->GetUserData());
+                    aSelectList.push_back(static_cast<FmFilterData*>(pEntry->GetUserData()));
             }
             if (aSelectList.size() == 1)
             {
@@ -1662,7 +1662,7 @@ void FmFilterNavigator::Command( const CommandEvent& rEvt )
             aContextMenu.EnableItem( SID_FM_DELETE, !aSelectList.empty() );
 
 
-            bool bEdit = PTR_CAST(FmFilterItem, (FmFilterData*)pClicked->GetUserData()) != NULL &&
+            bool bEdit = PTR_CAST(FmFilterItem, static_cast<FmFilterData*>(pClicked->GetUserData())) != NULL &&
                 IsSelected(pClicked) && GetSelectionCount() == 1;
 
             aContextMenu.EnableItem( SID_FM_FILTER_EDIT,
@@ -1690,9 +1690,9 @@ void FmFilterNavigator::Command( const CommandEvent& rEvt )
                     else
                         aText = "IS NOT NULL";
 
-                    m_pModel->ValidateText((FmFilterItem*)pClicked->GetUserData(),
+                    m_pModel->ValidateText(static_cast<FmFilterItem*>(pClicked->GetUserData()),
                                             aText, aErrorMsg);
-                    m_pModel->SetTextForItem((FmFilterItem*)pClicked->GetUserData(), aText);
+                    m_pModel->SetTextForItem(static_cast<FmFilterItem*>(pClicked->GetUserData()), aText);
                 }   break;
                 case SID_FM_DELETE:
                 {
@@ -1825,11 +1825,11 @@ void FmFilterNavigator::DeleteSelection()
          pEntry != NULL;
          pEntry = NextSelected(pEntry))
     {
-        FmFilterItem* pFilterItem = PTR_CAST(FmFilterItem, (FmFilterData*)pEntry->GetUserData());
+        FmFilterItem* pFilterItem = PTR_CAST(FmFilterItem, static_cast<FmFilterData*>(pEntry->GetUserData()));
         if (pFilterItem && IsSelected(GetParent(pEntry)))
             continue;
 
-        FmFormItem* pForm = PTR_CAST(FmFormItem, (FmFilterData*)pEntry->GetUserData());
+        FmFormItem* pForm = PTR_CAST(FmFormItem, static_cast<FmFilterData*>(pEntry->GetUserData()));
         if (!pForm)
             aEntryList.push_back(pEntry);
     }
@@ -1841,7 +1841,7 @@ void FmFilterNavigator::DeleteSelection()
         // link problems with operator ==
         i.base() != aEntryList.rend().base(); ++i)
     {
-        m_pModel->Remove((FmFilterData*)(*i)->GetUserData());
+        m_pModel->Remove(static_cast<FmFilterData*>((*i)->GetUserData()));
     }
 }
 

commit dd74e75c2692ee50535e2a9c7cf344fafc0e724a

tdf#126025: Non-const GetPropertyValueByName must use non-const Sequence []
...which internally calls getArray to make sure it references a unique sequence.
Regression introduced with 85f28ec44a2c169c91dac9346e9c71feda6a6cab "Clean up C-
style casts from pointers to void".

Change-Id: Id80b75f06322c65fc31040cc0b1892fe3a0aeaab

svx/source/items/customshapeitem.cxx
@@ -90,7 +90,7 @@ css::uno::Any* SdrCustomShapeGeometryItem::GetPropertyValueByName( const OUStrin
             PropertyPairHashMap::iterator aHashIter( aPropPairHashMap.find( PropertyPair( rSequenceName, rPropName ) ) );
             if ( aHashIter != aPropPairHashMap.end() )
             {
-                pRet = const_cast<css::uno::Any *>(&(*rSecSequence)[ (*aHashIter).second ].Value);
+                pRet = &const_cast<css::uno::Sequence<css::beans::PropertyValue> &>(*rSecSequence)[ (*aHashIter).second ].Value;
             }
         }
     }