LibreOffice/core

commit 1c94842e053a20a739a181d38a35c324df3e62a7

tdf#132091 sw: fix fieldmark mess in SwCursor::SelectWordWT()
The problem is that it selects both the field instruction and the field
result; in the typical case where the instruction is empty, the
separator won't be deleted (due to CalcBreaks) but the problem is
that the cursor ends up before the separator, not after it.

Also remove some silly defensive programming.

(regression from ffb26b81e1c7ff1d64959200247bb2edd5a569da)

Change-Id: I80b6cb1790b7102828f12b680631f928734ffa7b
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/92284
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@cib.de>

sw/source/core/crsr/swcrsr.cxx
@@ -28,6 +28,7 @@
 #include <swtblfmt.hxx>
 #include <swcrsr.hxx>
 #include <unocrsr.hxx>
+#include <bookmrk.hxx>
 #include <doc.hxx>
 #include <IDocumentUndoRedo.hxx>
 #include <IDocumentRedlineAccess.hxx>
@@ -1407,23 +1408,20 @@ bool SwCursor::SelectWordWT( SwViewShell const * pViewShell, sal_Int16 nWordType
     {
         // Should we select the whole fieldmark?
         const IDocumentMarkAccess* pMarksAccess = GetDoc()->getIDocumentMarkAccess( );
-        sw::mark::IMark* pMark = GetPoint() ? pMarksAccess->getFieldmarkFor( *GetPoint( ) ) : nullptr;
+        sw::mark::IFieldmark const*const pMark(pMarksAccess->getFieldmarkFor(*GetPoint()));
         if ( pMark )
         {
-            const SwPosition& rStart = pMark->GetMarkStart();
-            GetPoint()->nNode = rStart.nNode;
-            GetPoint()->nContent = rStart.nContent;
-            ++GetPoint()->nContent; // Don't select the start delimiter
+            *GetPoint() = sw::mark::FindFieldSep(*pMark);
+            ++GetPoint()->nContent; // Don't select the separator
 
             const SwPosition& rEnd = pMark->GetMarkEnd();
 
-            if ( rStart != rEnd )
-            {
-                SetMark();
-                GetMark()->nNode = rEnd.nNode;
-                GetMark()->nContent = rEnd.nContent;
-                --GetMark()->nContent; //Don't select the end delimiter
-            }
+            assert(pMark->GetMarkEnd() != *GetPoint());
+            SetMark();
+            GetMark()->nNode = rEnd.nNode;
+            GetMark()->nContent = rEnd.nContent;
+            --GetMark()->nContent; // Don't select the end delimiter
+
             bRet = true;
         }
         else

commit 7a321c462f30e9d1053acbb4e5de02b54927a63c

tdf#133641 sw: fix crash double-clicking CH_TXT_ATR_FORMELEMENT
Only search for separator if there is one.

(regression from 1c94842e053a20a739a181d38a35c324df3e62a7)

Change-Id: I6697faa7cb83cab48084f9710f8c5018b9e738e2

sw/source/core/crsr/swcrsr.cxx
@@ -1422,7 +1422,8 @@ bool SwCursor::SelectWordWT( SwViewShell const * pViewShell, sal_Int16 nWordType
         // Should we select the whole fieldmark?
         const IDocumentMarkAccess* pMarksAccess = GetDoc()->getIDocumentMarkAccess( );
         sw::mark::IFieldmark const*const pMark(pMarksAccess->getFieldmarkFor(*GetPoint()));
-        if ( pMark )
+        if (pMark && (IDocumentMarkAccess::GetType(*pMark) == IDocumentMarkAccess::MarkType::TEXT_FIELDMARK
+                      || IDocumentMarkAccess::GetType(*pMark) == IDocumentMarkAccess::MarkType::DATE_FIELDMARK))
         {
             *GetPoint() = sw::mark::FindFieldSep(*pMark);
             ++GetPoint()->nContent; // Don't select the separator