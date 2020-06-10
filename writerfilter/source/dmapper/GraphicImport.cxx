c30537011443354e9abcdcc9db531e5f3f35433e
Szabolcs Toth <szabolcs450@gmail.com>
2020-06-10 14:27:32 +0200

    InsideOutside
    Change-Id: Id8d93d909986321c38e6bb5be53189cd9ac0cbf9

--- writerfilter/source/dmapper/GraphicImport.cxx
+++ writerfilter/source/dmapper/GraphicImport.cxx
@@ -959,6 +959,20 @@ void GraphicImport::lcl_attribute(Id nName, Value& rValue)
                         xPropertySet->setPropertyValue("RelativeWidthRelation", uno::makeAny(text::RelOrientation::PAGE_RIGHT));
                     }
                     break;
+                case NS_ooxml::LN_ST_SizeRelFromH_insideMargin:
+                    if (m_xShape.is())
+                    {
+                        uno::Reference<beans::XPropertySet> xPropertySet(m_xShape, uno::UNO_QUERY);
+                        xPropertySet->setPropertyValue("RelativeWidthRelation", uno::makeAny(text::RelOrientation::INSIDE_MARGIN));
+                    }
+                    break;
+                case NS_ooxml::LN_ST_SizeRelFromH_outsideMargin:
+                    if (m_xShape.is())
+                    {
+                        uno::Reference<beans::XPropertySet> xPropertySet(m_xShape, uno::UNO_QUERY);
+                        xPropertySet->setPropertyValue("RelativeWidthRelation", uno::makeAny(text::RelOrientation::OUTSIDE_MARGIN));
+                    }
+                    break;
                 case NS_ooxml::LN_ST_SizeRelFromH_page:
                     if (m_xShape.is())
                     {
