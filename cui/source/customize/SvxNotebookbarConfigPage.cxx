8eed608f67404d358e09cbc9fbf5dfcee86ad0c7
Caolán McNamara <caolanm@redhat.com>
2020-06-10 15:53:56 +0100

    Resolves: tdf#133862 crash in customize toolbar in embedded math
    Change-Id: I2fc7f3620145e48516c3ca73c0a5d08dce11d78a

--- cui/source/customize/SvxNotebookbarConfigPage.cxx
+++ cui/source/customize/SvxNotebookbarConfigPage.cxx
@@ -418,6 +418,8 @@ void SvxNotebookbarConfigPage::SelectElement()
     OString sUIFileUIPath = CustomNotebookbarGenerator::getSystemPath(
         CustomNotebookbarGenerator::getCustomizedUIPath());
     xmlDocPtr pDoc = xmlParseFile(sUIFileUIPath.getStr());
+    if (!pDoc)
+        return;
     xmlNodePtr pNodePtr = xmlDocGetRootElement(pDoc);
 
     std::vector<NotebookbarEntries> aEntries;
@@ -479,10 +481,7 @@ void SvxNotebookbarConfigPage::SelectElement()
 
     aEntries.clear();
 
-    if (pDoc != nullptr)
-    {
-        xmlFreeDoc(pDoc);
-    }
+    xmlFreeDoc(pDoc);
 }
 
 SvxNotebookbarEntriesListBox::SvxNotebookbarEntriesListBox(std::unique_ptr<weld::TreeView> xParent,
