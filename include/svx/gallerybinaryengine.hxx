ed8c278ada79396b60f383742d6a80bec53fb439
Aditya <adityasahu1511@gmail.com>
2020-06-10 14:52:20 +0530

    svx: Gallery code refactoring
    Older classes like Gallery, GalleryTheme, GalleryThemeEntry have
     reading and writing data members and member functions included
     in the classes which makes the code unclear and less organized.
    Introduce new class Gallery Binary Engine that would deal with these
     member functions in such a way that older classes will have no
     knowledge about reading and writing.
    Only change the internal structure of the project and no change in
     behaviour of output must be there.
    
    Change-Id: Ia05eb3c692976b775a3d00e7d09fecc861bced2f

---
+++ include/svx/gallerybinaryengine.hxx
@@ -0,0 +1,45 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#pragma once
+
+#include <tools/urlobj.hxx>
+#include <svx/svxdllapi.h>
+
+class SVXCORE_DLLPUBLIC GalleryBinaryEngine
+{
+private:
+    INetURLObject aThmURL;
+    INetURLObject aSdgURL;
+    INetURLObject aSdvURL;
+    INetURLObject aStrURL;
+
+public:
+    void SetThmExtension(INetURLObject aURL);
+    void SetSdgExtension(INetURLObject aURL);
+    void SetSdvExtension(INetURLObject aURL);
+    void SetStrExtension(INetURLObject aURL);
+
+    const INetURLObject& GetThmURL() const { return aThmURL; }
+    const INetURLObject& GetSdgURL() const { return aSdgURL; }
+    const INetURLObject& GetSdvURL() const { return aSdvURL; }
+    const INetURLObject& GetStrURL() const { return aStrURL; }
+};
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
