9be8f3ca4a494fd5c6c9379bd3fcb1a3bdc38f5c
Andreas Heinisch <andreas.heinisch@yahoo.de>
2020-06-11 11:09:02 +0200

    tdf#132563 - Convert parameters of type SbxEMPTY
    During the construction of the parameter list of a method,
    convert parameters of type SbxEMPTY to their requested type,
    since missing parameters are handled differently in StepEMPTY,
    where separate parameter information (SbxMISSING) is added.
    
    Change-Id: Ie1e027cfdd652404ec29bd3d05e7daf533468936

--- basic/source/runtime/runtime.cxx
+++ basic/source/runtime/runtime.cxx
@@ -686,11 +686,10 @@ void SbiRuntime::SetParameters( SbxArray* pParams )
             {
                 bByVal |= ( p->eType & SbxBYREF ) == 0;
                 // tdf#79426, tdf#125180 - don't convert missing arguments to the requested parameter type
-                if ( t != SbxEMPTY && !IsMissing( v, 1 ) )
+                if ( !IsMissing( v, 1 ) )
                 {
                     t = static_cast<SbxDataType>( p->eType & 0x0FFF );
                 }
-
                 if( !bByVal && t != SbxVARIANT &&
                     (!v->IsFixed() || static_cast<SbxDataType>(v->GetType() & 0x0FFF ) != t) )
                 {
