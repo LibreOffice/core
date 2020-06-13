LibreOffice/core

commit a7de363cead5cd0021d2e3df4573d4cbe27df23b

remove unnecessary Window::Update() calls, which immediately follow Invalidate
Update() is effectively "paint immediately". Rather just let the
invalidate do it's thing and have the widget paint on the next paint
loop, along with the rest of the stuff.

This is probably mostly cargo-cult, from the days when our
invalidate/paint timer could take a long time to kick in.

Change-Id: Idff06526e9a2892244cfd8ce6947916032b0d1a9
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/90567
Reviewed-by: Caolán McNamara <caolanm@redhat.com>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Jenkins

vcl/source/control/prgsbar.cxx
@@ -169,7 +169,6 @@ void ProgressBar::SetValue( sal_uInt16 nNewPercent )
         if ( IsReallyVisible() )
         {
             Invalidate();
-            Update();
         }
     }
     else if ( mnPercent != nNewPercent )

basctl/source/dlged/dlgedview.cxx
@@ -110,7 +110,6 @@ void DlgEdView::MakeVisible( const tools::Rectangle& rRect, vcl::Window& rWin )
         rWin.Scroll( -nScrollX, -nScrollY );
         aMap.SetOrigin( Point( aOrg.X() - nScrollX, aOrg.Y() - nScrollY ) );
         rWin.SetMapMode( aMap );
-        rWin.Update();
         rWin.Invalidate();
 
         // update scroll bars

reportdesign/source/ui/report/SectionView.cxx
@@ -110,7 +110,6 @@ void OSectionView::MakeVisible( const tools::Rectangle& rRect, vcl::Window& rWin
         rWin.Scroll( -nScrollX, -nScrollY );
         aMap.SetOrigin( Point( aOrg.X() - nScrollX, aOrg.Y() - nScrollY ) );
         rWin.SetMapMode( aMap );
-        rWin.Update();
         rWin.Invalidate();
 
         if ( m_pReportWindow )

sc/source/ui/view/olinewin.cxx
@@ -123,7 +123,6 @@ void ScOutlineWindow::ScrollPixel( long nDiff )
 
     ScrollRel( nDiff, nStart, nEnd );
     Invalidate( GetRectangle( 0, nInvStart, GetOutputSizeLevel() - 1, nInvEnd ) );
-    Update();
 
     // if focus becomes invisible, move it to next visible button
     ImplMoveFocusToVisible( nDiff < 0 );

sd/source/ui/slidesorter/view/SlideSorterView.cxx
@@ -826,7 +826,6 @@ SlideSorterView::DrawLock::~DrawLock()
         if (mpWindow)
         {
             mpWindow->Invalidate(mrView.maRedrawRegion);
-            mpWindow->Update();
         }
 }
 

sw/source/core/view/vprint.cxx
@@ -114,7 +114,6 @@ void SwPaintQueue::Repaint()
                 {
                     // for previewing, since rows/columns are known in PaintHdl (UI)
                     pSh->GetWin()->Invalidate();
-                    pSh->GetWin()->Update();
                 }
             }
             else

vcl/source/control/button.cxx
@@ -1462,7 +1462,6 @@ bool PushButton::PreNotify( NotifyEvent& rNEvt )
                 else
                 {
                     pBorder->Invalidate( InvalidateFlags::NoErase );
-                    pBorder->Update();
                 }
             }
             else if( (GetStyle() & WB_FLATBUTTON) ||
@@ -2206,7 +2205,6 @@ void RadioButton::ImplCallClick( bool bGrabFocus, GetFocusFlags nFocusFlags )
     mbChecked = true;
     mpWindowImpl->mnStyle |= WB_TABSTOP;
     Invalidate();
-    Update();
     VclPtr<vcl::Window> xWindow = this;
     if ( mbRadioCheck )
         ImplUncheckAllOther();
@@ -2255,7 +2253,6 @@ void RadioButton::MouseButtonDown( const MouseEvent& rMEvt )
     {
         GetButtonState() |= DrawButtonFlags::Pressed;
         Invalidate();
-        Update();
         StartTracking();
         return;
     }
@@ -2280,7 +2277,6 @@ void RadioButton::Tracking( const TrackingEvent& rTEvt )
             else
             {
                 Invalidate();
-                Update();
             }
         }
     }
@@ -2292,7 +2288,6 @@ void RadioButton::Tracking( const TrackingEvent& rTEvt )
             {
                 GetButtonState() |= DrawButtonFlags::Pressed;
                 Invalidate();
-                Update();
             }
         }
         else
@@ -2301,7 +2296,6 @@ void RadioButton::Tracking( const TrackingEvent& rTEvt )
             {
                 GetButtonState() &= ~DrawButtonFlags::Pressed;
                 Invalidate();
-                Update();
             }
         }
     }
@@ -2317,14 +2311,12 @@ void RadioButton::KeyInput( const KeyEvent& rKEvt )
         {
             GetButtonState() |= DrawButtonFlags::Pressed;
             Invalidate();
-            Update();
         }
     }
     else if ( (GetButtonState() & DrawButtonFlags::Pressed) && (aKeyCode.GetCode() == KEY_ESCAPE) )
     {
         GetButtonState() &= ~DrawButtonFlags::Pressed;
         Invalidate();
-        Update();
     }
     else
         Button::KeyInput( rKEvt );
@@ -2447,7 +2439,6 @@ void RadioButton::LoseFocus()
     {
         GetButtonState() &= ~DrawButtonFlags::Pressed;
         Invalidate();
-        Update();
     }
 
     HideFocus();
@@ -3092,7 +3083,6 @@ void CheckBox::ImplCheck()
 
     VclPtr<vcl::Window> xWindow = this;
     Invalidate();
-    Update();
     Toggle();
     if ( xWindow->IsDisposed() )
         return;
@@ -3112,7 +3102,6 @@ void CheckBox::MouseButtonDown( const MouseEvent& rMEvt )
     {
         GetButtonState() |= DrawButtonFlags::Pressed;
         Invalidate();
-        Update();
         StartTracking();
         return;
     }
@@ -3137,7 +3126,6 @@ void CheckBox::Tracking( const TrackingEvent& rTEvt )
             else
             {
                 Invalidate();
-                Update();
             }
         }
     }
@@ -3149,7 +3137,6 @@ void CheckBox::Tracking( const TrackingEvent& rTEvt )
             {
                 GetButtonState() |= DrawButtonFlags::Pressed;
                 Invalidate();
-                Update();
             }
         }
         else
@@ -3158,7 +3145,6 @@ void CheckBox::Tracking( const TrackingEvent& rTEvt )
             {
                 GetButtonState() &= ~DrawButtonFlags::Pressed;
                 Invalidate();
-                Update();
             }
         }
     }
@@ -3174,14 +3160,12 @@ void CheckBox::KeyInput( const KeyEvent& rKEvt )
         {
             GetButtonState() |= DrawButtonFlags::Pressed;
             Invalidate();
-            Update();
         }
     }
     else if ( (GetButtonState() & DrawButtonFlags::Pressed) && (aKeyCode.GetCode() == KEY_ESCAPE) )
     {
         GetButtonState() &= ~DrawButtonFlags::Pressed;
         Invalidate();
-        Update();
     }
     else
         Button::KeyInput( rKEvt );
@@ -3346,7 +3330,6 @@ void CheckBox::LoseFocus()
     {
         GetButtonState() &= ~DrawButtonFlags::Pressed;
         Invalidate();
-        Update();
     }
 
     HideFocus();

vcl/source/control/imp_listbox.cxx
@@ -2638,7 +2638,6 @@ bool ImplWin::PreNotify( NotifyEvent& rNEvt )
             && ! IsNativeControlSupported(ControlType::Listbox, ControlPart::ButtonDown) )
             {
                 GetParent()->GetWindow( GetWindowType::Border )->Invalidate( InvalidateFlags::NoErase );
-                GetParent()->GetWindow( GetWindowType::Border )->Update();
             }
         }
     }

commit 80bec60f7d7cf7b7386058847e9e693c631b3078

tdf#131942 no progress bar when saving in calc
regression from
    commit a7de363cead5cd0021d2e3df4573d4cbe27df23b
    remove unnecessary Window::Update() calls, which immediately follow Invalidate

Change-Id: I2525acc9da786c0908bde5f8553258382f7cd45c

vcl/source/control/prgsbar.cxx
@@ -169,6 +169,7 @@ void ProgressBar::SetValue( sal_uInt16 nNewPercent )
         if ( IsReallyVisible() )
         {
             Invalidate();
+            Update();
         }
     }
     else if ( mnPercent != nNewPercent )