689847251c22ed5b1e46703d33322a5307aef21c
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2020-06-15 13:50:18 +0200

    vcl: wrap PDFium types in c++ classes to ease lifecycle management
    Change-Id: Idd33dfe752a22b6628750c06f02e9467a69802bc

--- include/vcl/filter/PDFiumLibrary.hxx
+++ include/vcl/filter/PDFiumLibrary.hxx
@@ -14,24 +14,79 @@
 
 #if HAVE_FEATURE_PDFIUM
 
+#include <vcl/dllapi.h>
+
 #include <memory>
 #include <rtl/instance.hxx>
-#include <vcl/dllapi.h>
+#include <basegfx/vector/b2dsize.hxx>
+#include <rtl/ustring.hxx>
+
+#include <fpdf_doc.h>
 
 namespace vcl::pdf
 {
+class PDFiumDocument;
+
 class VCL_DLLPUBLIC PDFium final
 {
 private:
     PDFium(const PDFium&) = delete;
     PDFium& operator=(const PDFium&) = delete;
 
+    OUString maLastError;
+
 public:
     PDFium();
     ~PDFium();
+
+    OUString getLastError() { return maLastError; }
+
+    std::unique_ptr<PDFiumDocument> openDocument(const void* pData, int nSize);
 };
 
-struct PDFiumLibrary : public rtl::StaticWithInit<std::shared_ptr<PDFium>, PDFiumLibrary>
+class VCL_DLLPUBLIC PDFiumPage final
+{
+private:
+    FPDF_PAGE mpPage;
+
+private:
+    PDFiumPage(const PDFiumPage&) = delete;
+    PDFiumPage& operator=(const PDFiumPage&) = delete;
+
+public:
+    PDFiumPage(FPDF_PAGE pPage)
+        : mpPage(pPage)
+    {
+    }
+
+    ~PDFiumPage()
+    {
+        if (mpPage)
+            FPDF_ClosePage(mpPage);
+    }
+};
+
+class VCL_DLLPUBLIC PDFiumDocument final
+{
+private:
+    FPDF_DOCUMENT mpPdfDocument;
+
+private:
+    PDFiumDocument(const PDFiumDocument&) = delete;
+    PDFiumDocument& operator=(const PDFiumDocument&) = delete;
+
+public:
+    PDFiumDocument(FPDF_DOCUMENT pPdfDocument);
+    ~PDFiumDocument();
+
+    // Page size in points
+    basegfx::B2DSize getPageSize(int nIndex);
+    int getPageCount();
+
+    std::unique_ptr<PDFiumPage> openPage(int nIndex);
+};
+
+struct PDFiumLibrary final : public rtl::StaticWithInit<std::shared_ptr<PDFium>, PDFiumLibrary>
 {
     std::shared_ptr<PDFium> operator()() { return std::make_shared<PDFium>(); }
 };
