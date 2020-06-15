29a07e4353fd5dda45a1409803d159f9988d1d4a
Andras Timar <andras.timar@collabora.com>
2020-06-15 09:20:24 +0200

    Use  getBcp47() instead of getLanguage()
    getLanguage() only returns the language code which can be
    ambiguous in case of UI localization, when there are more
    than one language variants. Fallback to the first variant
    caused mixed language UI, e.g. en-US vs. en-GB, pt vs. pt-BR,
    zh-CN vs. zh-TW.
    
    Change-Id: I888423cd4b3c65dbdb900ff3b6bc308fa119be4d

--- configmgr/source/access.cxx
+++ configmgr/source/access.cxx
@@ -1382,7 +1382,7 @@ rtl::Reference< ChildAccess > Access::getChild(OUString const & name) {
         // here we override it with the actual current-user's language to
         // support per-view localization in LOK.
         if (comphelper::LibreOfficeKit::isActive())
-            locale = comphelper::LibreOfficeKit::getLanguageTag().getLanguage();
+            locale = comphelper::LibreOfficeKit::getLanguageTag().getBcp47();
 
         if (!locale.isEmpty()) {
             // Find best match using an adaptation of RFC 4647 lookup matching
