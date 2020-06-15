LibreOffice/core

commit 4814e8caa5f06c4fe438dfd7d7315e4a2410ea18

tdf#124601 sw: add ContinuousEndnotes layout compat option
Writer has two cases for laying out endnotes: either they are at the end
of the section or are on a separate endnote page at the end of the
document.

Word always puts endnotes as continuous content at the end of the
document, not on a separate page.

Given that this continuous / separate page behavior difference seems to
be not part of the ODF or OOXML file format and neither UI allows to
configure this, the best way to resolve this looks like a new layout
compat option.

At a layout level, the "endnotes at the end of the section" code is
close to what we need, we just need to make sure that:

1) Endnotes are never moved backwards, even if their reference moves
back.

2) When appending an endnote, they should go to the footnote container
on the last page, not close to their reference.

With this, the page number in Word and Writer now match for the bugdoc.

Change-Id: I6fd0ee191e001d7c3a6df46d5e9fe8d7eb0327dc
Reviewed-on: https://gerrit.libreoffice.org/79857
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

sw/source/core/layout/ftnfrm.cxx
@@ -37,6 +37,7 @@
 #include <pam.hxx>
 #include <ndtxt.hxx>
 #include <sal/log.hxx>
+#include <IDocumentSettingAccess.hxx>
 
 #define ENDNOTE 0x80000000
 
@@ -1431,9 +1432,11 @@ void SwFootnoteBossFrame::AppendFootnote( SwContentFrame *pRef, SwTextFootnote *
     if ( pAttr->GetFootnote().IsEndNote() )
     {
         bEnd = true;
+        const IDocumentSettingAccess& rSettings = *pAttr->GetTextNode().getIDocumentSettingAccess();
         if( GetUpper()->IsSctFrame() &&
             static_cast<SwSectionFrame*>(GetUpper())->IsEndnAtEnd() )
         {
+            // Endnotes at the end of the section.
             SwFrame* pLast =
                 static_cast<SwSectionFrame*>(GetUpper())->FindLastContent( SwFindMode::EndNote );
             if( pLast )
@@ -1442,8 +1445,15 @@ void SwFootnoteBossFrame::AppendFootnote( SwContentFrame *pRef, SwTextFootnote *
                 pPage = pBoss->FindPageFrame();
             }
         }
+        else if (rSettings.get(DocumentSettingId::CONTINUOUS_ENDNOTES))
+        {
+            // Endnotes at the end of the document.
+            pBoss = getRootFrame()->GetLastPage();
+            pPage = pBoss->FindPageFrame();
+        }
         else
         {
+            // Endnotes on a separate page.
             while ( pPage->GetNext() && !pPage->IsEndNotePage() )
             {
                 pPage = static_cast<SwPageFrame*>(pPage->GetNext());

sw/qa/extras/layout/layout.cxx
@@ -3217,6 +3217,23 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf124600)
     assertXPath(pXmlDoc, "/root/page/body/txt[2]/LineBreak", 1);
 }
 
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf124601)
+{
+    // This is a testcase for the ContinuousEndnotes compat flag.
+    // The document has 2 pages, the endnote anchor is on the first page.
+    // The endnote should be on the 2nd page together with the last page content.
+    createDoc("tdf124601.doc");
+    xmlDocPtr pXmlDoc = parseLayoutDump();
+
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 2
+    // - Actual  : 3
+    // i.e. there was a separate endnote page, even when the ContinuousEndnotes compat option was
+    // on.
+    assertXPath(pXmlDoc, "/root/page", 2);
+    assertXPath(pXmlDoc, "/root/page[2]/ftncont", 1);
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sw/inc/IDocumentSettingAccess.hxx
@@ -102,6 +102,7 @@ enum class DocumentSettingId
     EMBED_COMPLEX_SCRIPT_FONTS,
     EMBED_SYSTEM_FONTS,
     APPLY_PARAGRAPH_MARK_FORMAT_TO_NUMBERING,
+    CONTINUOUS_ENDNOTES,
 };
 
  /** Provides access to settings of a document

sw/source/core/doc/DocumentSettingManager.cxx
@@ -216,6 +216,8 @@ bool sw::DocumentSettingManager::get(/*[in]*/ DocumentSettingId id) const
         case DocumentSettingId::APPLY_PARAGRAPH_MARK_FORMAT_TO_NUMBERING: return mApplyParagraphMarkFormatToNumbering;
         case DocumentSettingId::DISABLE_OFF_PAGE_POSITIONING: return mbDisableOffPagePositioning;
         case DocumentSettingId::EMPTY_DB_FIELD_HIDES_PARA: return mbEmptyDbFieldHidesPara;
+        case DocumentSettingId::CONTINUOUS_ENDNOTES:
+            return mbContinuousEndnotes;
         default:
             OSL_FAIL("Invalid setting id");
     }
@@ -447,6 +449,9 @@ void sw::DocumentSettingManager::set(/*[in]*/ DocumentSettingId id, /*[in]*/ boo
             break;
         case DocumentSettingId::EMPTY_DB_FIELD_HIDES_PARA:
             mbEmptyDbFieldHidesPara = value;
+            break;
+        case DocumentSettingId::CONTINUOUS_ENDNOTES:
+            mbContinuousEndnotes = value;
             break;
         default:
             OSL_FAIL("Invalid setting id");

sw/source/core/inc/DocumentSettingManager.hxx
@@ -160,6 +160,7 @@ class DocumentSettingManager :
     bool mbLastBrowseMode                           : 1;
     bool mbDisableOffPagePositioning; // tdf#112443
     bool mbEmptyDbFieldHidesPara;
+    bool mbContinuousEndnotes = false;
 
 public:
 

sw/source/core/layout/flowfrm.cxx
@@ -2087,13 +2087,23 @@ bool SwFlowFrame::MoveBwd( bool &rbReformat )
         // have to have a result != 0
         SwFrame* pRef = nullptr;
         const bool bEndnote = pFootnote->GetAttr()->GetFootnote().IsEndNote();
+        const IDocumentSettingAccess& rSettings
+            = pFootnote->GetAttrSet()->GetDoc()->getIDocumentSettingAccess();
         if( bEndnote && pFootnote->IsInSct() )
         {
             SwSectionFrame* pSect = pFootnote->FindSctFrame();
             if( pSect->IsEndnAtEnd() )
+                // Endnotes at the end of the section.
                 pRef = pSect->FindLastContent( SwFindMode::LastCnt );
         }
+        else if (bEndnote && rSettings.get(DocumentSettingId::CONTINUOUS_ENDNOTES))
+        {
+            // Endnotes at the end of the document.
+            SwPageFrame* pPage = m_rThis.getRootFrame()->GetLastPage();
+            pRef = pPage->FindLastBodyContent();
+        }
         if( !pRef )
+            // Endnotes on a separate page.
             pRef = pFootnote->GetRef();
 
         OSL_ENSURE( pRef, "MoveBwd: Endnote for an empty section?" );

sw/source/filter/ww8/ww8par.cxx
@@ -1907,6 +1907,7 @@ void SwWW8ImplReader::ImportDop()
     m_rDoc.getIDocumentSettingAccess().set(DocumentSettingId::TAB_OVER_MARGIN, true);
     m_rDoc.getIDocumentSettingAccess().set(DocumentSettingId::SURROUND_TEXT_WRAP_SMALL, true);
     m_rDoc.getIDocumentSettingAccess().set(DocumentSettingId::PROP_LINE_SPACING_SHRINKS_FIRST_LINE, true);
+    m_rDoc.getIDocumentSettingAccess().set(DocumentSettingId::CONTINUOUS_ENDNOTES, true);
 
     // COMPATIBILITY FLAGS END
 

commit b3499badc97c0c60ea2a3a2623bb417c069d8006

tdf#133145 sw ContinuousEndnotes: fix moving endnotes to a previous page
Regression from commit 4814e8caa5f06c4fe438dfd7d7315e4a2410ea18
(tdf#124601 sw: add ContinuousEndnotes layout compat option,
2019-09-30), the problem was that SwFrame::GetPrevFootnoteLeaf() did not
take the new compat flag into account when determining the previous
footnote page for endnotes.

Do the same pattern here as the cases already handled in the above
commit, just try to get the "last but one" and not the "last" page,
since we try to move these endnotes to a previous page.

Change-Id: I77841a3a0fb68f054941184ee2a8aca0707d2a9c

sw/source/core/layout/ftnfrm.cxx
@@ -750,13 +750,31 @@ SwLayoutFrame *SwFrame::GetPrevFootnoteLeaf( MakePageType eMakeFootnote )
     {
         bool bEndn = pFootnote->GetAttr()->GetFootnote().IsEndNote();
         SwFrame* pTmpRef = nullptr;
+        const IDocumentSettingAccess& rSettings
+            = pFootnote->GetAttrSet()->GetDoc()->getIDocumentSettingAccess();
         if( bEndn && pFootnote->IsInSct() )
         {
             SwSectionFrame* pSect = pFootnote->FindSctFrame();
             if( pSect->IsEndnAtEnd() )
+                // Endnotes at the end of the section.
                 pTmpRef = pSect->FindLastContent( SwFindMode::LastCnt );
         }
+        else if (bEndn && rSettings.get(DocumentSettingId::CONTINUOUS_ENDNOTES))
+        {
+            // Endnotes at the end of the document.
+            SwPageFrame* pPage = getRootFrame()->GetLastPage();
+            assert(pPage);
+            SwFrame* pPrevPage = pPage->GetPrev();
+            if (pPrevPage)
+            {
+                // Have a last but one page, use that since we try to get a preceding frame.
+                assert(pPrevPage->IsPageFrame());
+                pPage = static_cast<SwPageFrame*>(pPrevPage);
+            }
+            pTmpRef = pPage->FindLastBodyContent();
+        }
         if( !pTmpRef )
+            // Endnotes on a separate page.
             pTmpRef = pFootnote->GetRef();
         SwFootnoteBossFrame* pStop = pTmpRef->FindFootnoteBossFrame( !bEndn );
 

sw/qa/core/layout/layout.cxx
@@ -129,6 +129,23 @@ CPPUNIT_TEST_FIXTURE(SwCoreLayoutTest, testTablesMoveBackwards)
     assertXPath(pLayout, "//page", 1);
 }
 
+CPPUNIT_TEST_FIXTURE(SwCoreLayoutTest, testContinuousEndnotesMoveBackwards)
+{
+    // Load a document with the ContinuousEndnotes flag turned on.
+    load(DATA_DIRECTORY, "continuous-endnotes-move-backwards.doc");
+    xmlDocUniquePtr pLayout = parseLayoutDump();
+    // We have 2 pages.
+    assertXPath(pLayout, "/root/page", 2);
+    // No endnote container on page 1.
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 0
+    // - Actual  : 1
+    // i.e. there were unexpected endnotes on page 1.
+    assertXPath(pLayout, "/root/page[1]/ftncont", 0);
+    // All endnotes are in a container on page 2.
+    assertXPath(pLayout, "/root/page[2]/ftncont", 1);
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */