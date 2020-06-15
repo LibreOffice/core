89dc2b547b4cc307f7d69715a5e1ed4a27691755
Michael Stahl <Michael.Stahl@cib.de>
2020-06-15 13:11:29 +0200

    tdf#133957 sw: SelectAll should select fly in empty section
    Ctrl+A should select everything in the section; if the section is empty,
    there's no text to select, and then the shell will cancel the selection
    because SwCursorShell::HasSelection() returns false - so check if
    there are flys anchored there, which fixes the Copy.
    
    To fix Delete, a few more checks for empty selection need to be changed.
    
    Change-Id: If54160bdca976ad5f153b89cf8492f4da1e774b7
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/96324
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@cib.de>
    (cherry picked from commit 2d89b9929e85bede4c72684a12e7508751875f0e)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/96368
    Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>

--- sw/source/core/doc/DocumentContentOperationsManager.cxx
+++ sw/source/core/doc/DocumentContentOperationsManager.cxx
@@ -4059,8 +4059,11 @@ bool DocumentContentOperationsManager::DeleteRangeImplImpl(SwPaM & rPam)
 {
     SwPosition *pStt = rPam.Start(), *pEnd = rPam.End();
 
-    if( !rPam.HasMark() || *pStt >= *pEnd )
+    if (!rPam.HasMark()
+        || (*pStt == *pEnd && !IsFlySelectedByCursor(m_rDoc, *pStt, *pEnd)))
+    {
         return false;
+    }
 
     if( m_rDoc.GetAutoCorrExceptWord() )
     {
