851a7d9789f5cc36f13cdec56ca65e656c040057
Aditya <adityasahu1511@gmail.com>
2020-06-16 13:00:00 +0530

    svx:Introduce GalleryBinaryEngineEntry
    Change-Id: I42775454cc82629fa74a4890d9e66778fae29c09

---
+++ svx/source/gallery2/gallerybinaryengineentry.cxx
@@ -0,0 +1,89 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#include <svx/gallerybinaryengineentry.hxx>
+#include <svx/galmisc.hxx>
+
+#include <tools/urlobj.hxx>
+
+static bool FileExists(const INetURLObject& rURL, const OUString& rExt)
+{
+    INetURLObject aURL(rURL);
+    aURL.setExtension(rExt);
+    return FileExists(aURL);
+}
+
+INetURLObject GalleryBinaryEngineEntry::ImplGetURLIgnoreCase(const INetURLObject& rURL)
+{
+    INetURLObject aURL(rURL);
+
+    // check original file name
+    if (!FileExists(aURL))
+    {
+        // check upper case file name
+        aURL.setName(aURL.getName().toAsciiUpperCase());
+
+        if (!FileExists(aURL))
+        {
+            // check lower case file name
+            aURL.setName(aURL.getName().toAsciiLowerCase());
+        }
+    }
+
+    return aURL;
+}
+
+void GalleryBinaryEngineEntry::CreateUniqueURL(const INetURLObject& rBaseURL, INetURLObject& aURL)
+{
+    INetURLObject aBaseNoCase(ImplGetURLIgnoreCase(rBaseURL));
+    aURL = aBaseNoCase;
+    static sal_Int32 nIdx = 0;
+    while (FileExists(aURL, "thm"))
+    { // create new URLs
+        nIdx++;
+        aURL = aBaseNoCase;
+        aURL.setName(aURL.getName() + OUString::number(nIdx));
+    }
+}
+
+void GalleryBinaryEngineEntry::SetThmExtension(INetURLObject aURL)
+{
+    aURL.setExtension("thm");
+    aThmURL = ImplGetURLIgnoreCase(aURL);
+}
+
+void GalleryBinaryEngineEntry::SetSdgExtension(INetURLObject aURL)
+{
+    aURL.setExtension("sdg");
+    aSdgURL = ImplGetURLIgnoreCase(aURL);
+}
+
+void GalleryBinaryEngineEntry::SetSdvExtension(INetURLObject aURL)
+{
+    aURL.setExtension("sdv");
+    aSdvURL = ImplGetURLIgnoreCase(aURL);
+}
+
+void GalleryBinaryEngineEntry::SetStrExtension(INetURLObject aURL)
+{
+    aURL.setExtension("str");
+    aStrURL = ImplGetURLIgnoreCase(aURL);
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
