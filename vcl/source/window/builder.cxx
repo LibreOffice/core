67b3bab0d815d5dd07a1686f467585946643f610
Szymon Kłos <szymon.klos@collabora.com>
2020-06-16 08:40:13 +0200

    notebookbar: use JSInstanceBuilder for styles preview in online
    Change-Id: I826ca92aec5e324b935dfc7c52fc95c6ce5c5e05
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/96915
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>

--- vcl/source/window/builder.cxx
+++ vcl/source/window/builder.cxx
@@ -174,6 +174,11 @@ weld::Builder* Application::CreateBuilder(weld::Widget* pParent, const OUString 
 
 weld::Builder* Application::CreateInterimBuilder(vcl::Window* pParent, const OUString &rUIFile)
 {
+    if (comphelper::LibreOfficeKit::isActive() && rUIFile == "svx/ui/stylespreview.ui")
+    {
+        return new JSInstanceBuilder(pParent, VclBuilderContainer::getUIRootDir(), rUIFile, css::uno::Reference<css::frame::XFrame>());
+    }
+
     return ImplGetSVData()->mpDefInst->CreateInterimBuilder(pParent, VclBuilderContainer::getUIRootDir(), rUIFile);
 }
 
