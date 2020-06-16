LibreOffice/core

commit 07dcbed5fb6d88f69d84313ddede81c222a5a7a6

INTEGRATION: CWS fmebugs01 (1.15.104); FILE MERGED 2004/02/09 15:13:57 fme 1.15.104.1: #i13507# Justified alignment for lines containing manual breaks

sw/source/core/text/itradj.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: itradj.cxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: rt $ $Date: 2003-12-01 17:22:20 $
+ *  last change: $Author: obo $ $Date: 2004-03-17 12:50:46 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -192,11 +192,12 @@ void SwTxtAdjuster::CalcNewBlock( SwLineLayout *pCurr,
 
     while( pPos )
     {
-        if ( pPos->IsBreakPortion() && !IsLastBlock() )
+        // disabled for #i13507#
+/*        if ( pPos->IsBreakPortion() && !IsLastBlock() )
         {
-            pCurr->FinishSpaceAdd();
-            break;
-        }
+           pCurr->FinishSpaceAdd();
+           break;
+        } */
         if ( pPos->InTxtGrp() )
             nGluePortion += ((SwTxtPortion*)pPos)->GetSpaceCnt( GetInfo(), nCharCnt );
         else if( pPos->IsMultiPortion() )
@@ -574,8 +575,9 @@ void SwTxtAdjuster::CalcAdjLine( SwLineLayout *pCurr )
         }
         case SVX_ADJUST_BLOCK:
         {
+            // disabled for #i13507#
             // 8311: In Zeilen mit LineBreaks gibt es keinen Blocksatz!
-            if( pCurr->GetLen() &&
+/*          if( pCurr->GetLen() &&
                 CH_BREAK == GetInfo().GetChar( nStart + pCurr->GetLen() - 1 ) &&
                 !IsLastBlock() )
             {
@@ -587,7 +589,7 @@ void SwTxtAdjuster::CalcAdjLine( SwLineLayout *pCurr )
                 }
                 return;
             }
-            FormatBlock();
+*/          FormatBlock();
             break;
         }
         default : return;

commit d5325013c4f71595dc4edd08e3957d27bc5e0d9b

tdf#106234 sw: don't justify after centered tabs
(also after right-aligned and decimal-aligned tabs)
before the page break in a justified paragraph.
Justify only after left-aligned tabs, like MSO does.

Regression from commit 07dcbed5fb6d88f69d84313ddede81c222a5a7a6
  (INTEGRATION: CWS fmebugs01 (1.15.104); FILE MERGED
  2004/02/09 15:13:57 fme 1.15.104.1: #i13507# Justified
  alignment for lines containing manual breaks).

See also commit 5d7b3475278f59a423c2139ae51f9b4849621890
  (INTEGRATION: CWS swqbf34 (1.17.96); FILE MERGED
  2005/06/29 12:12:25 fme 1.17.96.2: #i49277# New
  compatibility option bDoNotJustifyLinesWithManualBreak).

Change-Id: Ib92e301aaf46eec283c0850d23549b1b54106068

sw/source/core/text/itradj.cxx
@@ -287,16 +287,31 @@ void SwTextAdjuster::CalcNewBlock( SwLineLayout *pCurrent,
     // #i49277#
     const bool bDoNotJustifyLinesWithManualBreak =
         GetTextFrame()->GetDoc().getIDocumentSettingAccess().get(DocumentSettingId::DO_NOT_JUSTIFY_LINES_WITH_MANUAL_BREAK);
+    bool bDoNotJustifyTab = false;
 
     SwLinePortion *pPos = pCurrent->GetNextPortion();
 
     while( pPos )
     {
-        if ( bDoNotJustifyLinesWithManualBreak &&
+        if ( ( bDoNotJustifyLinesWithManualBreak || bDoNotJustifyTab ) &&
              pPos->IsBreakPortion() && !IsLastBlock() )
         {
            pCurrent->FinishSpaceAdd();
            break;
+        }
+
+        switch ( pPos->GetWhichPor() )
+        {
+            case PortionType::TabCenter :
+            case PortionType::TabRight :
+            case PortionType::TabDecimal :
+                bDoNotJustifyTab = true;
+                break;
+            case PortionType::TabLeft :
+            case PortionType::Break:
+                bDoNotJustifyTab = false;
+                break;
+            default: break;
         }
 
         if ( pPos->InTextGrp() )

sw/qa/extras/layout/data/tdf106234.fodt
@@ -1,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<office:document xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0" xmlns:style="urn:oasis:names:tc:opendocument:xmlns:style:1.0" xmlns:table="urn:oasis:names:tc:opendocument:xmlns:table:1.0" xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0" xmlns:fo="urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0" xmlns:officeooo="http://openoffice.org/2009/office" xmlns:loext="urn:org:documentfoundation:names:experimental:office:xmlns:loext:1.0" office:version="1.2" office:mimetype="application/vnd.oasis.opendocument.text">
+ <office:styles>
+  <style:style style:name="Standard" style:family="paragraph" style:class="text"/>
+  <style:default-style style:family="paragraph">
+   <style:text-properties fo:language="rn" fo:country="US"/>
+  </style:default-style>
+ </office:styles>
+ <office:automatic-styles>
+  <style:style style:name="P1" style:family="paragraph" style:parent-style-name="Standard">
+   <style:paragraph-properties fo:text-align="justify" style:justify-single-word="false">
+    <style:tab-stops>
+     <style:tab-stop style:position="2.554cm"/>
+    </style:tab-stops>
+   </style:paragraph-properties>
+  </style:style>
+  <style:style style:name="P2" style:family="paragraph" style:parent-style-name="Standard">
+   <style:paragraph-properties fo:text-align="justify" style:justify-single-word="false">
+    <style:tab-stops>
+     <style:tab-stop style:position="2.554cm" style:type="center"/>
+    </style:tab-stops>
+   </style:paragraph-properties>
+  </style:style>
+ </office:automatic-styles>
+ <office:body>
+  <office:text>
+   <text:p text:style-name="P1"><text:tab/>Lorem ipsum<text:line-break/>dolor sit amet, consectetur adipiscing elit. (Justify after left-aligned tab)</text:p>
+   <text:p text:style-name="P2"><text:span text:style-name="T1"><text:tab/>Lorem ipsum<text:line-break/>dolor sit amet, consectetur adipiscing elit. (Don’t justify after centered tab!)</text:span></text:p>
+  </office:text>
+ </office:body>
+</office:document>

sw/qa/extras/layout/layout.cxx
@@ -3069,6 +3069,27 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf120287)
     assertXPath(pXmlDoc, "/root/page/body/txt[1]/LineBreak", 1);
 }
 
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf106234)
+{
+    createDoc("tdf106234.fodt");
+    // Ensure that all text portions are calculated before testing.
+    SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument*>(mxComponent.get());
+    CPPUNIT_ASSERT(pTextDoc);
+    SwViewShell* pViewShell
+        = pTextDoc->GetDocShell()->GetDoc()->getIDocumentLayoutAccess().GetCurrentViewShell();
+    CPPUNIT_ASSERT(pViewShell);
+    pViewShell->Reformat();
+
+    xmlDocUniquePtr pXmlDoc = parseLayoutDump();
+    // In justified paragraphs, there is justification between left tabulators and page breaks
+    assertXPath(pXmlDoc, "/root/page/body/txt[1]/Special", "nType", "PortionType::Margin");
+    assertXPathNoAttribute(pXmlDoc, "/root/page/body/txt[1]/Special", "nWidth");
+    // but not after centered, right and decimal tabulators
+    assertXPath(pXmlDoc, "/root/page/body/txt[2]/Special", "nType", "PortionType::Margin");
+    // This was a justified line, without nWidth
+    assertXPath(pXmlDoc, "/root/page/body/txt[2]/Special", "nWidth", "7881");
+}
+
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf120287b)
 {
     createDoc("tdf120287b.fodt");