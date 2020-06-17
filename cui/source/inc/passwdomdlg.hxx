56e0bbd2e66b7e9f74561ce1a2c293acc04c3e12
Caolán McNamara <caolanm@redhat.com>
2020-06-17 14:09:29 +0100

    tdf#43452 indicate when maximum password length has been reached
    Change-Id: I58ba5887d41a80aa0ef2be547351f2faeccfa3cb

--- cui/source/inc/passwdomdlg.hxx
+++ cui/source/inc/passwdomdlg.hxx
@@ -25,25 +25,32 @@
 class PasswordToOpenModifyDialog : public SfxDialogController
 {
     std::unique_ptr<weld::Entry> m_xPasswdToOpenED;
+    std::unique_ptr<weld::Label> m_xPasswdToOpenInd;
     std::unique_ptr<weld::Entry> m_xReenterPasswdToOpenED;
+    std::unique_ptr<weld::Label> m_xReenterPasswdToOpenInd;
     std::unique_ptr<weld::Expander> m_xOptionsExpander;
     std::unique_ptr<weld::Button> m_xOk;
     std::unique_ptr<weld::CheckButton> m_xOpenReadonlyCB;
     std::unique_ptr<weld::Label> m_xPasswdToModifyFT;
     std::unique_ptr<weld::Entry> m_xPasswdToModifyED;
+    std::unique_ptr<weld::Label> m_xPasswdToModifyInd;
     std::unique_ptr<weld::Label> m_xReenterPasswdToModifyFT;
     std::unique_ptr<weld::Entry> m_xReenterPasswdToModifyED;
+    std::unique_ptr<weld::Label> m_xReenterPasswdToModifyInd;
 
     OUString                    m_aOneMismatch;
     OUString                    m_aTwoMismatch;
     OUString                    m_aInvalidStateForOkButton;
     OUString                    m_aInvalidStateForOkButton_v2;
+    OUString                    m_aIndicatorTemplate;
 
+    int                         m_nMaxPasswdLen;
     bool                        m_bIsPasswordToModify;
 
 
     DECL_LINK(OkBtnClickHdl, weld::Button&, void);
     DECL_LINK(ReadonlyOnOffHdl, weld::Button&, void);
+    DECL_LINK(ChangeHdl, weld::Entry&, void);
 
     PasswordToOpenModifyDialog( const PasswordToOpenModifyDialog & ) = delete;
     PasswordToOpenModifyDialog & operator = ( const PasswordToOpenModifyDialog & ) = delete;
