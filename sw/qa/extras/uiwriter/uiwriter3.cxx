fca8adad96a80cd3594b8d76192ec1eb5c72a5fe
Xisco Fauli <xiscofauli@libreoffice.org>
2020-06-17 22:58:38 +0200

    tdf#132944: sw: Add unittest
    Change-Id: Ia6bdfd9231bc4324ffc4b53bfec89e76d036545c

--- sw/qa/extras/uiwriter/uiwriter3.cxx
+++ sw/qa/extras/uiwriter/uiwriter3.cxx
@@ -492,6 +492,27 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest3, testTdf130685)
     CPPUNIT_ASSERT_EQUAL(2, getPages());
 }
 
+CPPUNIT_TEST_FIXTURE(SwUiWriterTest3, testTdf132944)
+{
+    load(DATA_DIRECTORY, "tdf132944.odt");
+
+    SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument*>(mxComponent.get());
+    CPPUNIT_ASSERT(pTextDoc);
+
+    CPPUNIT_ASSERT_EQUAL(2, getPages());
+
+    dispatchCommand(mxComponent, ".uno:SelectAll", {});
+    dispatchCommand(mxComponent, ".uno:Cut", {});
+    Scheduler::ProcessEventsToIdle();
+    CPPUNIT_ASSERT_EQUAL(1, getPages());
+
+    dispatchCommand(mxComponent, ".uno:Undo", {});
+    Scheduler::ProcessEventsToIdle();
+
+    // Without the fix in place, the document would have had 3 pages
+    CPPUNIT_ASSERT_EQUAL(2, getPages());
+}
+
 CPPUNIT_TEST_FIXTURE(SwUiWriterTest3, testTdf130680)
 {
     load(DATA_DIRECTORY, "tdf130680.odt");
