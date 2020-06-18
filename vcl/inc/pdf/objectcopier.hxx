6e267f362fec03662e36641d275a42119f1798ce
Miklos Vajna <vmiklos@collabora.com>
2020-06-18 12:12:52 +0200

    sd signature line: extract copyExternalResources() from the pdf export code
    Because I would like to reuse this in the "sign existing pdf" code, in
    vcl::filter::PDFDocument::WriteAppearanceObject().
    
    (cherry picked from commit 8277073ce3e33788d93b3df490a8f03d1814863b)
    
    Conflicts:
    	vcl/source/gdi/pdfwriter_impl.cxx
    	vcl/source/gdi/pdfwriter_impl.hxx
    
    Change-Id: Ia5e5c1e452bb0d0486bde2a082375b5131eea8c7
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/97248
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

---
+++ vcl/inc/pdf/objectcopier.hxx
@@ -0,0 +1,48 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#pragma once
+
+#include <map>
+
+#include <rtl/string.hxx>
+#include <sal/types.h>
+#include <vcl/dllapi.h>
+
+class SvMemoryStream;
+
+namespace vcl
+{
+class PDFObjectContainer;
+namespace filter
+{
+class PDFObjectElement;
+}
+
+/// Copies objects from one PDF file into an other one.
+class VCL_DLLPUBLIC PDFObjectCopier
+{
+    PDFObjectContainer& m_rContainer;
+
+public:
+    PDFObjectCopier(PDFObjectContainer& rContainer);
+
+    /// Copies resources of a given kind from an external page to the output,
+    /// returning what has to be included in the new resource dictionary.
+    OString copyExternalResources(filter::PDFObjectElement& rPage, const OString& rKind,
+                                  std::map<sal_Int32, sal_Int32>& rCopiedResources);
+
+    /// Copies a single resource from an external document, returns the new
+    /// object ID in our document.
+    sal_Int32 copyExternalResource(SvMemoryStream& rDocBuffer, filter::PDFObjectElement& rObject,
+                                   std::map<sal_Int32, sal_Int32>& rCopiedResources);
+};
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
