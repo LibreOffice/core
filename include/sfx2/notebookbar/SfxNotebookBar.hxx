237677ba1230b6e800f29065aad9ece067638b45
Szymon Kłos <szymon.klos@collabora.com>
2020-06-18 12:15:13 +0200

    notebookbar: allow to create multiple instances for online
    Change-Id: Ic8a3d07ec6ec5a5d6d56a3958e91d3074ce1493e

--- include/sfx2/notebookbar/SfxNotebookBar.hxx
+++ include/sfx2/notebookbar/SfxNotebookBar.hxx
@@ -18,6 +18,7 @@ namespace com::sun::star::uno { template <typename > class Reference; }
 
 class SfxBindings;
 class SfxViewFrame;
+class SfxViewShell;
 class SystemWindow;
 class WeldedTabbedNotebookbar;
 
@@ -60,9 +61,10 @@ public:
 private:
     static bool m_bLock;
     static bool m_bHide;
-    static std::unique_ptr<WeldedTabbedNotebookbar> m_pNotebookBarWeldedWrapper;
 
-    DECL_STATIC_LINK(SfxNotebookBar, VclDisposeHdl, const void*, void);
+    static std::map<const SfxViewShell*, std::shared_ptr<WeldedTabbedNotebookbar>> m_pNotebookBarWeldedWrapper;
+
+    DECL_STATIC_LINK(SfxNotebookBar, VclDisposeHdl, const SfxViewShell*, void);
 };
 
 } // namespace sfx2
