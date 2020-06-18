f28477b0de576cb89f95a86afaea0118fbc57673
Stephan Bergmann <sbergman@redhat.com>
2020-06-18 10:37:17 +0200

    Mark external/clucene/patches/c++20.patch as sent upstream
    Change-Id: Iaa3e251648afaa4bd3178dcff99594ce609083eb

--- external/clucene/UnpackedTarball_clucene.mk
+++ external/clucene/UnpackedTarball_clucene.mk
@@ -32,6 +32,8 @@ $(eval $(call gb_UnpackedTarball_set_patchlevel,clucene,0))
 #  <https://sourceforge.net/p/clucene/mailman/message/36989348/>
 # heap-buffer-overflow.patch was proposed upstream, see
 #  <https://sourceforge.net/p/clucene/mailman/message/36991067/>
+# c++20.patch was proposed upstream, see
+#  <https://sourceforge.net/p/clucene/mailman/message/37040362/>
 $(eval $(call gb_UnpackedTarball_add_patches,clucene,\
 	external/clucene/patches/clucene-debug.patch \
 	external/clucene/patches/clucene-multimap-put.patch \
