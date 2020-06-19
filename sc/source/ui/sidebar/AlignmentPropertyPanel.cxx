2d789501e7dfdd917be225fe70af4154fe8b229f
Noel Grandin <noel.grandin@collabora.co.uk>
2020-06-19 16:17:25 +0200

    remove JSON node erasing
    as a step towards using tools::JsonWriter for DumpAsPropertyTree
    
    Since this method is only used by the LOK stuff, we can just
    remove the node down at the generation site in vcl::Window
    
    Change-Id: I1b3b9a0732dcdb18267cb56e545a30c8f56b4ae2

--- sc/source/ui/sidebar/AlignmentPropertyPanel.cxx
+++ sc/source/ui/sidebar/AlignmentPropertyPanel.cxx
@@ -140,50 +140,6 @@ void AlignmentPropertyPanel::Initialize()
     mxRefEdgeStd->connect_toggled(aLink2);
 }
 
-namespace {
-
-void eraseNode(boost::property_tree::ptree& pTree, const std::string& aValue)
-{
-    boost::optional<boost::property_tree::ptree&> pId;
-    boost::optional<boost::property_tree::ptree&> pSubTree = pTree.get_child_optional("children");
-
-    if (pSubTree)
-    {
-        boost::property_tree::ptree::iterator itFound = pSubTree.get().end();
-        for (boost::property_tree::ptree::iterator it = pSubTree.get().begin(); it != pSubTree.get().end(); ++it)
-        {
-            pId = it->second.get_child_optional("id");
-            if (pId && pId.get().get_value<std::string>("") == aValue)
-            {
-                itFound = it;
-                break;
-            }
-
-            eraseNode(it->second, aValue);
-        }
-
-        if (itFound != pSubTree.get().end())
-        {
-            pSubTree.get().erase(itFound);
-        }
-    }
-}
-
-}
-
-boost::property_tree::ptree AlignmentPropertyPanel::DumpAsPropertyTree()
-{
-    boost::property_tree::ptree aTree = PanelLayout::DumpAsPropertyTree();
-
-    const SfxViewShell* pViewShell = SfxViewShell::Current();
-    if (pViewShell && pViewShell->isLOKMobilePhone())
-    {
-        eraseNode(aTree, "textorientbox");
-    }
-
-    return aTree;
-}
-
 IMPL_LINK(AlignmentPropertyPanel, ReferenceEdgeHdl, weld::ToggleButton&, rToggle, void)
 {
     if (mbSettingToggles)
