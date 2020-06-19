60a583d34401898a974107b1a3976bcc3f8716fa
Michael Stahl <Michael.Stahl@cib.de>
2020-06-19 15:05:42 +0200

    tdf#132326 sw_redlinehide: no frames allowed in Undo SwNodesArray
    This crashes in SwAccessibleParagraph::GetRealHeadingLevel(); the a11y
    should have been disposed earlier because the node it's connected to is
    in the Undo SwNodesArray.
    
    When SwUndoDelete::SwUndoDelete() calls SwNodes::MoveNodes(), all
    existing frames must be destroyed; the MoveNodes() would reset its flag
    temporarily inside a section, to delay creating the frames inside the
    section when moving to the live SwNodesArray, but this also prevents
    the deletion when moving in the other direction.
    
    (crash is regression from 723728cd358693b8f4bc9d913541aa4479f2bd48)
    
    Change-Id: I13719bf98ea96b8d68db3ee78cf7f4b61a99e7e5
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/96734
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@cib.de>
    (cherry picked from commit a60dd9ef1361c5925803acaa5292e99277d1faf3)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/96701
    Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>
    (cherry picked from commit 088324bbd391c858dd17832708b465eb5b6046e6)

--- sw/source/core/docnode/nodes.cxx
+++ sw/source/core/docnode/nodes.cxx
@@ -663,7 +663,11 @@ bool SwNodes::MoveNodes( const SwNodeRange& aRange, SwNodes & rNodes,
                     {
                         pSctNd->NodesArrChgd();
                         ++nSectNdCnt;
-                        bNewFrames = false;
+                        // tdf#132326 do not let frames survive in undo nodes
+                        if (!GetDoc()->GetIDocumentUndoRedo().IsUndoNodes(rNodes))
+                        {
+                            bNewFrames = false;
+                        }
                     }
                 }
             }
