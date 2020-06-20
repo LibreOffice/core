fd3678043b8913615455483337b6a2b9498cbb51
Caolán McNamara <caolanm@redhat.com>
2020-06-20 16:01:07 +0100

    most of jsdialogbuilder is not used outside vcl
    so split it into the bit that is needed and just include that.
    
    add missing license headers
    
    Change-Id: I875f91176e6881e830fee6a58368d0b28ce9a0f7
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/96774
    Tested-by: Jenkins
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>

--- vcl/source/control/WeldedTabbedNotebookbar.cxx
+++ vcl/source/control/WeldedTabbedNotebookbar.cxx
@@ -7,9 +7,9 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-#include <sfx2/notebookbar/WeldedTabbedNotebookbar.hxx>
+#include <vcl/WeldedTabbedNotebookbar.hxx>
 #include <vcl/svapp.hxx>
-#include <vcl/jsdialog/jsdialogbuilder.hxx>
+#include <jsdialog/jsdialogbuilder.hxx>
 
 WeldedTabbedNotebookbar::WeldedTabbedNotebookbar(
     VclPtr<vcl::Window>& pContainerWindow, const OUString& rUIFilePath,
