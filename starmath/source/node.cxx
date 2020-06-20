048fe99c5619bb801c0f4b90fc35023b456db7fd
dante <dante19031999@gmail.com>
2020-06-20 22:21:01 +0200

    This implements RGB personalized color for math and laplace symbol. #TDF40436 #TDF47914 Change-Id: I7aca3128e20e9dd0aae816a740afb6970be6eba9

--- starmath/source/node.cxx
+++ starmath/source/node.cxx
@@ -1719,6 +1719,7 @@ void SmAttributNode::Arrange(OutputDevice &rDev, const SmFormat &rFormat)
 void SmFontNode::CreateTextFromNode(OUStringBuffer &rText)
 {
     rText.append("{");
+    sal_Int32 nc,r,g,b;
 
     switch (GetToken().eType)
     {
@@ -1819,6 +1820,21 @@ void SmFontNode::CreateTextFromNode(OUStringBuffer &rText)
         case TFUCHSIA:
             rText.append("color fuchsia ");
             break;
+        case TRGB:
+            rText.append("color rgb ");
+            nc = GetToken().aText.toInt32();
+            b = nc % 256;
+            nc /= 256;
+            g = nc % 256;
+            nc /= 256;
+            r = nc % 256;
+            rText.append(OUString::number(r));
+            rText.append(" ");
+            rText.append(OUString::number(g));
+            rText.append(" ");
+            rText.append(OUString::number(b));
+            rText.append(" ");
+            break;
         case TSANS:
             rText.append("font sans ");
             break;
@@ -1866,6 +1882,8 @@ void SmFontNode::Arrange(OutputDevice &rDev, const SmFormat &rFormat)
 {
     SmNode *pNode = GetSubNode(1);
     assert(pNode);
+    sal_Int32 nc;
+    Color COL_PERSO_RGB_COLOR (0);
 
     switch (GetToken().eType)
     {   case TSIZE :
@@ -1902,6 +1920,15 @@ void SmFontNode::Arrange(OutputDevice &rDev, const SmFormat &rFormat)
         case TNAVY :    SetColor(COL_BLUE);  break;
         case TAQUA :    SetColor(COL_LIGHTCYAN);  break;
         case TFUCHSIA : SetColor(COL_LIGHTMAGENTA);  break;
+        case TRGB :
+            nc = GetToken().aText.toInt32();
+            COL_PERSO_RGB_COLOR.SetBlue(nc % 256);
+            nc /= 256;
+            COL_PERSO_RGB_COLOR.SetGreen(nc % 256);
+            nc /= 256;
+            COL_PERSO_RGB_COLOR.SetRed(nc % 256);
+            SetColor(COL_PERSO_RGB_COLOR);
+            break;
 
         default:
             SAL_WARN("starmath", "unknown case");
@@ -2658,7 +2685,7 @@ void SmSpecialNode::Prepare(const SmFormat &rFormat, const SmDocShell &rDocShell
             static const sal_Unicode cUppercaseOmega = 0x03A9;
             sal_Unicode cChar = rTmp[0];
             // uppercase letters should be straight and lowercase letters italic
-            bItalic = (cUppercaseAlpha > cChar || cChar > cUppercaseOmega);
+            bItalic = !(cUppercaseAlpha <= cChar && cChar <= cUppercaseOmega);
         }
     }
 
