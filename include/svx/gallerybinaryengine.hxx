ed90dd5253301e9ce8155320a92696f6e937d656
Aditya <adityasahu1511@gmail.com>
2020-06-20 14:07:28 +0530

    svx:Refactoring of GalleryTheme::WriteData() function
    Change-Id: I7262c77ec8730fbbe516f9fb205c25c4e1f15ff5

--- include/svx/gallerybinaryengine.hxx
+++ include/svx/gallerybinaryengine.hxx
@@ -22,8 +22,10 @@
 #include <tools/urlobj.hxx>
 #include <svx/svxdllapi.h>
 #include <svx/galtheme.hxx>
+#include <svx/gallery1.hxx>
 
 struct GalleryObject;
+class Gallery;
 
 class SVXCORE_DLLPUBLIC GalleryBinaryEngine
 {
@@ -59,6 +61,10 @@ public:
     implCreateUniqueURL(SgaObjKind eObjKind, const INetURLObject& rUserURL,
                         ::std::vector<std::unique_ptr<GalleryObject>>& rObjectList,
                         ConvertDataFormat nFormat = ConvertDataFormat::Unknown);
+    SvStream& writeData(SvStream& rOStm, Gallery* pParent, sal_uInt32 nCount, OUString aThemeName,
+                        const ::std::vector<std::unique_ptr<GalleryObject>>& rObjectList,
+                        const OUString& m_aDestDir, const bool m_bDestDirRelative, sal_uInt32& rId,
+                        bool& bNameFromResource);
 };
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
