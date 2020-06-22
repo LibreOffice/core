70848c7e5ab2035aa7727fa97492e4535fd6937e
Tor Lillqvist <tml@collabora.com>
2020-06-23 01:19:23 +0300

    Add getopt declarations
    Avoids: implicit declaration of function 'getopt' is invalid in C99
    [-Werror,-Wimplicit-function-declaration].
    
    Change-Id: Ic178f53d1002425df52e220b1723fb12edca13df
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/96910
    Tested-by: Jenkins
    Reviewed-by: Tor Lillqvist <tml@collabora.com>

--- external/nss/UnpackedTarball_nss.mk
+++ external/nss/UnpackedTarball_nss.mk
@@ -24,6 +24,7 @@ $(eval $(call gb_UnpackedTarball_add_patches,nss,\
     external/nss/nss.bzmozilla1238154.patch \
     external/nss/macos-dlopen.patch.0 \
     external/nss/nss.nspr-parallel-win-debug_build.patch \
+	external/nss/nss.getopt.patch.0 \
     $(if $(filter iOS,$(OS)), \
         external/nss/nss-ios.patch) \
     $(if $(filter ANDROID,$(OS)), \
