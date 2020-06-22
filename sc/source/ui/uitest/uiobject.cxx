0efd8ed67f8267ab7f7d70a1a3f9875b6b520f22
Ahmed ElShreif <aelshreif7@gmail.com>
2020-06-22 11:15:56 +0200

    uitest : Add support to Calc - comments
    This patch has the implementation of the support for Calc comments .
    
    Now you can use these lines to use comments in calc:
    
    	* Open Comment Window :
    	>>  gridwin.executeAction("COMMENT",mkPropertyValues({"OPEN":""}))
    
    	* Then Type text as normal :
    	>>  gridwin.executeAction("TYPE",mkPropertyValues({"TEXT":"any"}))
    
    	* Close Comment Window :
    	>>  gridwin.executeAction("COMMENT",mkPropertyValues({"CLOSE":""}))
    
    	* Write Comment without opening comment window:
    	>>  gridwin.executeAction("COMMENT",mkPropertyValues({"SETTEXT":"any"}))
    
    You can also check the comment text by this line:
    	>>  get_state_as_dict(gridwind)["CurrentCellCommentText"]
    
    I will provide a test case in another patch.
    
    Change-Id: Ie3fc4efffb07827e742bdaa99dd5147c8b9711d3

--- sc/source/ui/uitest/uiobject.cxx
+++ sc/source/ui/uitest/uiobject.cxx
@@ -14,6 +14,7 @@
 #include <gridwin.hxx>
 
 #include <viewdata.hxx>
+#include <viewfunc.hxx>
 #include <dbfunc.hxx>
 #include <tabvwsh.hxx>
 #include <drwlayer.hxx>
@@ -22,6 +23,8 @@
 #include <sfx2/viewfrm.hxx>
 #include <appoptio.hxx>
 #include <scmod.hxx>
+#include <fudraw.hxx>
+#include <postit.hxx>
 
 #include <svx/svditer.hxx>
 #include <svx/svdobj.hxx>
@@ -75,6 +78,17 @@ StringMap ScGridWinUIObject::get_state()
 
     aMap["MarkedArea"] = aMarkedAreaString;
 
+    ScDocument* pDoc = mxGridWindow->getViewData()->GetDocument();
+    ScAddress aPos( mxGridWindow->getViewData()->GetCurX() , mxGridWindow->getViewData()->GetCurY() , mxGridWindow->getViewData()->GetTabNo() );
+    if ( pDoc->HasNote( aPos ) )
+    {
+        aMap["CurrentCellCommentText"] = pDoc->GetNote( aPos )->GetText();
+    }
+    else
+    {
+        aMap["CurrentCellCommentText"] = "Not exist";
+    }
+
     ScAppOptions aOpt = SC_MOD()->GetAppOptions();
     aMap["Zoom"] = OUString::number( aOpt.GetZoom() );
     return aMap;
@@ -102,6 +116,14 @@ ScTabViewShell* ScGridWinUIObject::getViewShell()
     ScTabViewShell* pViewShell = pViewData->GetViewShell();
 
     return pViewShell;
+}
+
+ScViewFunc* ScGridWinUIObject::getViewFunc()
+{
+    ScViewData* pViewData = mxGridWindow->getViewData();
+    ScViewFunc* pViewFunc = pViewData->GetView();
+
+    return pViewFunc;
 }
 
 void ScGridWinUIObject::execute(const OUString& rAction,
@@ -229,6 +251,27 @@ void ScGridWinUIObject::execute(const OUString& rAction,
             SCROW nRow = itrRow->second.toUInt32();
             SCCOL nCol = itrCol->second.toUInt32();
             mxGridWindow->LaunchDataSelectMenu(nCol, nRow);
+        }
+    }
+    else if (rAction == "COMMENT")
+    {
+        if ( rParameters.find("OPEN") != rParameters.end() )
+        {
+            ScViewFunc* pViewFunc = getViewFunc();
+            pViewFunc->EditNote();
+        }
+        else if ( rParameters.find("CLOSE") != rParameters.end() )
+        {
+            FuDraw* pDraw = dynamic_cast<FuDraw*> (getViewFunc()->GetDrawFuncPtr());
+            pDraw->CloseCommentWindow();
+        }
+        else if ( rParameters.find("SETTEXT") != rParameters.end() )
+        {
+            auto itr = rParameters.find("SETTEXT");
+            const OUString rStr = itr->second;
+            ScDocument* pDoc = mxGridWindow->getViewData()->GetDocument();
+            ScAddress aPos( mxGridWindow->getViewData()->GetCurX() , mxGridWindow->getViewData()->GetCurY() , mxGridWindow->getViewData()->GetTabNo() );
+            pDoc->GetOrCreateNote( aPos )->SetText( aPos , rStr );
         }
     }
     else if (rAction == "SIDEBAR")
