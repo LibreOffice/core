2dbc01575917f0c0019e50f77eb17020c88a9085
nd101 <Fong@nd.com.cn>
2020-06-23 22:15:40 +0800

    tdf#60316 Chart plotarea background issue
    The default shall be of nofill instead of white.
    
    Change-Id: Id29feb7d1f8a24e4c3c768aa69f2775cdbf031a0

--- oox/source/drawingml/chart/plotareacontext.cxx
+++ oox/source/drawingml/chart/plotareacontext.cxx
@@ -191,7 +191,7 @@ ContextHandlerRef PlotAreaContext::onCreateContext( sal_Int32 nElement, const At
                 case C_TOKEN( layout ):
                     return new LayoutContext( *this, mrModel.mxLayout.create() );
                 case C_TOKEN( spPr ):
-                    return new ShapePropertiesContext( *this, mrModel.mxShapeProp.create() );
+                    return new ShapePropertiesContext( *this, mrModel.mxShapeProp.getOrCreate() );
                 case C_TOKEN(dTable):
                     return new DataTableContext( *this, mrModel.mxDataTable.create() );
             }
