9b0a0bcc7ef3ae7a127e7d43b001bdb578184949
Xisco Fauli <xiscofauli@libreoffice.org>
2020-06-24 14:02:13 +0200

    tdf#134243: sw: Add UItest
    Change-Id: I59376419048ddddff3e4fbbd6d3eb7436ee87747
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/97029
    Tested-by: Jenkins
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

---
+++ sw/qa/uitest/writer_tests7/tdf134243.py
@@ -0,0 +1,29 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+from uitest.framework import UITestCase
+from uitest.path import get_srcdir_url
+
+def get_url_for_data_file(file_name):
+    return get_srcdir_url() + "/sw/qa/uitest/writer_tests/data/" + file_name
+
+class tdf134243(UITestCase):
+
+    def test_tdf134243(self):
+        writer_doc = self.ui_test.load_file(get_url_for_data_file("tdf134243.odt"))
+
+        # Without the fix in place, it would hung launching the mailmerge wizard
+        self.ui_test.execute_dialog_through_command(".uno:MailMergeWizard")
+
+        xDialog = self.xUITest.getTopFocusWindow()
+
+        xCancelBtn = xDialog.getChild("cancel")
+        self.ui_test.close_dialog_through_button(xCancelBtn)
+
+        self.ui_test.close_doc()
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
+
