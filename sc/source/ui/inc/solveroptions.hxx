efe623fdc2abd3f1bbd09faa70ae1b7f00edb5b5
Caolán McNamara <caolanm@redhat.com>
2020-06-25 20:44:06 +0100

    Related: tdf#134280 set max limits for solver options that show max values
    just simply clip to the claimed max
    
    Change-Id: Ie64da80204c9c817c85724abd1fee8c6594967b9
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/97185
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>

--- sc/source/ui/inc/solveroptions.hxx
+++ sc/source/ui/inc/solveroptions.hxx
@@ -111,6 +111,7 @@ class ScSolverValueDialog : public weld::GenericDialogController
 {
     std::unique_ptr<weld::Frame> m_xFrame;
     std::unique_ptr<weld::Entry> m_xEdValue;
+    double m_fMaxValue;
 
 public:
     ScSolverValueDialog(weld::Window* pParent);
@@ -118,6 +119,7 @@ public:
 
     void        SetOptionName( const OUString& rName );
     void        SetValue( double fValue );
+    void        SetMax( double fValue );
     double      GetValue() const;
 };
 
