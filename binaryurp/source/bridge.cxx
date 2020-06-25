d939ed03fc25e46e2d4c552fd931a6db24049b04
Stephan Bergmann <sbergman@redhat.com>
2020-06-25 09:50:34 +0200

    Rephrase comment
    760980b0b510acac555f0c252fbaa10a4751bded "Fix typos" had, IMO incorrectly,
    removed one "that" from
    
      ..., so that that asynchronous request would...
    
    Lets rephrase the original comment in a less problematic way.
    
    Change-Id: Ic13ffaad43e2777eb36425363851b53f2f641865

--- binaryurp/source/bridge.cxx
+++ binaryurp/source/bridge.cxx
@@ -977,9 +977,9 @@ void Bridge::makeReleaseCall(
     OUString const & oid, css::uno::TypeDescription const & type)
 {
     //HACK to decouple the processing of release calls from all other threads.  Normally, sending
-    // the release request should use the current thread's TID (via AttachThread), so that
-    // asynchronous request would be processed by a physical thread that is paired with the physical
-    // thread processing the normal synchronous call stack (see ThreadIdHashMap in
+    // the release request should use the current thread's TID (via AttachThread), which would cause
+    // that asynchronous request to be processed by a physical thread that is paired with the
+    // physical thread processing the normal synchronous call stack (see ThreadIdHashMap in
     // cppu/source/threadpool/threadpool.hxx).  However, that can lead to deadlock when a thread
     // illegally makes a synchronous UNO call with the SolarMutex locked (e.g.,
     // SfxBaseModel::postEvent_Impl in sfx2/source/doc/sfxbasemodel.cxx doing documentEventOccurred
