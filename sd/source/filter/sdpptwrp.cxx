d8c80f5f1f0d1bee6fef8ebf978ed12b06f26e5e
Julien Nabet <serval2412@yahoo.fr>
2020-06-27 09:23:09 +0200

    cid#1464975: RESOURCE_LEAK (sd/sdpptwrp)
    Change-Id: Icb8a1add327e7b11b4095c1e3f60cddf2ea0f5c3
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/97296
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>

--- sd/source/filter/sdpptwrp.cxx
+++ sd/source/filter/sdpptwrp.cxx
@@ -332,7 +332,7 @@ bool SdPPTFilter::Export()
                             break;
                         }
 
-                        SotStorageStream* pStream = pStorage->OpenSotStream(sFileName);
+                        tools::SvRef<SotStorageStream> pStream = pStorage->OpenSotStream(sFileName);
                         if (!pStream)
                         {
                             bRet = false;
