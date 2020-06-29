2d8a6f0d55c18354f70b5741477933b836ca0ec8
Tor Lillqvist <tml@collabora.com>
2020-06-29 09:08:52 +0300

    Attempt to fix three more cases of just "hoping" that things work right
    Change-Id: I81c87740b36beeb1288649712f6415cb6074348f
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/97416
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Tor Lillqvist <tml@collabora.com>

--- sc/source/ui/unoobj/docuno.cxx
+++ sc/source/ui/unoobj/docuno.cxx
@@ -583,9 +583,14 @@ VclPtr<vcl::Window> ScModelObj::getDocWindow()
 {
     SolarMutexGuard aGuard;
 
-    // There seems to be no clear way of getting the grid window for this
-    // particular document, hence we need to hope we get the right window.
-    ScViewData* pViewData = ScDocShell::GetViewData();
+    ScTabViewShell* pViewShell = pDocShell->GetBestViewShell(false);
+
+    // FIXME: Can this happen? What should we do?
+    if (!pViewShell)
+        return VclPtr<vcl::Window>();
+
+    ScViewData* pViewData = &pViewShell->GetViewData();
+
     VclPtr<vcl::Window> pWindow;
     if (pViewData)
     {
@@ -652,9 +657,14 @@ void ScModelObj::postMouseEvent(int nType, int nX, int nY, int nCount, int nButt
 {
     SolarMutexGuard aGuard;
 
-    // There seems to be no clear way of getting the grid window for this
-    // particular document, hence we need to hope we get the right window.
-    ScViewData* pViewData = ScDocShell::GetViewData();
+    ScTabViewShell* pViewShell = pDocShell->GetBestViewShell(false);
+
+    // FIXME: Can this happen? What should we do?
+    if (!pViewShell)
+        return;
+
+    ScViewData* pViewData = &pViewShell->GetViewData();
+
     ScGridWindow* pGridWindow = pViewData->GetActiveWin();
 
     if (!pGridWindow)
@@ -800,15 +810,20 @@ void ScModelObj::setGraphicSelection(int nType, int nX, int nY)
 {
     SolarMutexGuard aGuard;
 
-    // There seems to be no clear way of getting the grid window for this
-    // particular document, hence we need to hope we get the right window.
-    ScViewData* pViewData = ScDocShell::GetViewData();
+    ScTabViewShell* pViewShell = pDocShell->GetBestViewShell(false);
+
+    // FIXME: Can this happen? What should we do?
+    if (!pViewShell)
+        return;
+
+    ScViewData* pViewData = &pViewShell->GetViewData();
+
     ScGridWindow* pGridWindow = pViewData->GetActiveWin();
 
     double fPPTX = pViewData->GetPPTX();
     double fPPTY = pViewData->GetPPTY();
 
-    ScTabViewShell* pViewShell = pViewData->GetViewShell();
+    pViewShell = pViewData->GetViewShell();
     LokChartHelper aChartHelper(pViewShell);
     if (aChartHelper.setGraphicSelection(nType, nX, nY, fPPTX, fPPTY))
         return;
