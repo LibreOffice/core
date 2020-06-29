6ec407b4a0ec246105c3f44d404ba32533153617
Michael Stahl <Michael.Stahl@cib.de>
2020-06-29 14:00:56 +0200

    curl: upgrade to release 7.71.0
    Fixes CVE-2020-8169, and about 3 other ones that don't affect LO/libcmis.
    
    Disable new optional dependencies "ngtcp2" and "quiche".
    
    Change-Id: I595ed909c4e792ac3244643511a338b117e7f102
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/97406
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@cib.de>
    (cherry picked from commit cb0c7e26e5708010b1fe941ae2f66134f31ed876)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/97386
    Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>
    (cherry picked from commit 43130216127913d30074d3fe99c5706dcce44dee)

--- external/curl/ExternalPackage_curl.mk
+++ external/curl/ExternalPackage_curl.mk
@@ -20,7 +20,7 @@ $(eval $(call gb_ExternalPackage_add_file,curl,$(LIBO_LIB_FOLDER)/libcurl.4.dyli
 else ifeq ($(OS),AIX)
 $(eval $(call gb_ExternalPackage_add_file,curl,$(LIBO_LIB_FOLDER)/libcurl.so,lib/.libs/libcurl.so.4))
 else
-$(eval $(call gb_ExternalPackage_add_file,curl,$(LIBO_LIB_FOLDER)/libcurl.so.4,lib/.libs/libcurl.so.4.5.0))
+$(eval $(call gb_ExternalPackage_add_file,curl,$(LIBO_LIB_FOLDER)/libcurl.so.4,lib/.libs/libcurl.so.4.6.0))
 endif
 
 endif # $(DISABLE_DYNLOADING)
