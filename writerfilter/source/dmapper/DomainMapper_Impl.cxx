a3200f87f57d426308a921624665bb929d251c01
Bakos Attila <bakos.attilakaroly@nisz.hu>
2020-06-30 15:03:31 +0200

    tdf118701 DOCX Import: fix image position on page break
    Before the image moved to the next page after docx
    import, now this type of page break handled so the
    image is in the right page.
    
    Co-Dev: Attila Banhegyi (NISZ)
    Change-Id: Ic22c7553948eb06739232d7e35fc49ad14f96518

--- writerfilter/source/dmapper/DomainMapper_Impl.cxx
+++ writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -473,6 +473,22 @@ void DomainMapper_Impl::AddDummyParaForTableInSection()
     }
 }
 
+bool DomainMapper_Impl::IsLastParaEmpty()
+{
+    bool bRet = true; //this will return
+    if (!m_aTextAppendStack.empty() && m_aTextAppendStack.top().xTextAppend)//nullptr checking
+    {
+        //creating cursor for finding text content
+        uno::Reference<text::XTextCursor> xCursor = m_aTextAppendStack.top().xTextAppend->createTextCursor();
+        xCursor->gotoEnd(false);
+        //selecting the last 2 characters in the document
+        xCursor->goLeft(2, true);
+        //if the last 2 char are page-breaks, its sure the last para is empty
+        bRet = xCursor->getString() == OUString(SAL_NEWLINE_STRING) + OUString(SAL_NEWLINE_STRING);
+    }
+    return bRet;
+}
+
 void DomainMapper_Impl::RemoveLastParagraph( )
 {
     if (m_bDiscardHeaderFooter)
