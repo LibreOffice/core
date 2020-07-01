abe9453cd14f4a80bff7c8a9a08c6cde857bb5bd
Jan-Marek Glogowski <glogow@fbihome.de>
2020-07-01 18:06:28 +0200

    Allow making SAL_LOG based output fatal
    This introduces the [+-]FATAL marker for SAL_LOG. This way you can
    set part of the matching SAL_LOG string to std::abort on match.
    
    The example "SAL_LOG=+FATAL+WARN.vcl.scheduler-FATAL+INFO" will
    abort for any "+WARN.vcl.scheduler" match, but will just print all
    additional "+INFO" logs.
    
    Change-Id: Ib77f194a78f5165e6c885c82374ae41293815ee9

--- include/sal/log.hxx
+++ include/sal/log.hxx
@@ -26,6 +26,13 @@
 
 /// @cond INTERNAL
 
+enum sal_detail_LogAction
+{
+    SAL_DETAIL_LOG_ACTION_IGNORE = 0,
+    SAL_DETAIL_LOG_ACTION_LOG = 1,
+    SAL_DETAIL_LOG_ACTION_FATAL = 2,
+};
+
 extern "C" SAL_DLLPUBLIC void SAL_CALL sal_detail_log(
     sal_detail_LogLevel level, char const * area, char const * where,
     char const * message, sal_uInt32 backtraceDepth);
@@ -113,22 +120,37 @@ inline char const * unwrapStream(SAL_UNUSED_PARAMETER StreamIgnore const &) {
 
 } }
 
+// to prevent to use a local variable, eventually shadowed...
+#define SAL_DETAIL_LOG_STREAM_PRIVATE_(level, area, where, stream) \
+    if (sizeof ::sal::detail::getResult( \
+            ::sal::detail::StreamStart() << stream) == 1) \
+    { \
+        ::sal_detail_log( \
+            (level), (area), (where), \
+            ::sal::detail::unwrapStream( \
+                ::sal::detail::StreamStart() << stream), \
+            0); \
+    } else { \
+        ::std::ostringstream sal_detail_stream; \
+        sal_detail_stream << stream; \
+        ::sal::detail::log( \
+            (level), (area), (where), sal_detail_stream, 0); \
+    }
+
 #define SAL_DETAIL_LOG_STREAM(condition, level, area, where, stream) \
     do { \
-        if ((condition) && sal_detail_log_report(level, area)) { \
-            if (sizeof ::sal::detail::getResult( \
-                    ::sal::detail::StreamStart() << stream) == 1) \
+        if (condition) \
+        { \
+            switch (sal_detail_log_report(level, area)) \
             { \
-                ::sal_detail_log( \
-                    (level), (area), (where), \
-                    ::sal::detail::unwrapStream( \
-                        ::sal::detail::StreamStart() << stream), \
-                    0); \
-            } else { \
-                ::std::ostringstream sal_detail_stream; \
-                sal_detail_stream << stream; \
-                ::sal::detail::log( \
-                    (level), (area), (where), sal_detail_stream, 0); \
+            case SAL_DETAIL_LOG_ACTION_IGNORE: break; \
+            case SAL_DETAIL_LOG_ACTION_LOG: \
+                SAL_DETAIL_LOG_STREAM_PRIVATE_(level, area, where, stream); \
+                break; \
+            case SAL_DETAIL_LOG_ACTION_FATAL: \
+                SAL_DETAIL_LOG_STREAM_PRIVATE_(level, area, where, stream); \
+                std::abort(); \
+                break; \
             } \
         } \
     } while (false)
@@ -235,7 +257,7 @@ inline char const * unwrapStream(SAL_UNUSED_PARAMETER StreamIgnore const &) {
       <switch> ::= <sense><item>
       <sense> ::= "+"|"-"
       <item> ::= <flag>|<level>("."<area>)?
-      <flag> ::= "TIMESTAMP"|"RELATIVETIMER"
+      <flag> ::= "TIMESTAMP"|"RELATIVETIMER"|"FATAL"
       <level> ::= "INFO"|"WARN"
     @endverbatim
 
@@ -251,6 +273,12 @@ inline char const * unwrapStream(SAL_UNUSED_PARAMETER StreamIgnore const &) {
     The "+RELATIVETIMER" flag causes each output line (as selected by
     the level switch(es)) to be prefixed by a relative timestamp in
     seconds since the first output line like 1.312.
+
+    The "+FATAL" flag will cause later matching rules to log and call
+    std::abort. This can be disabled at some later point by using the
+    "-FATAL" flag before specifying additional rules. The flag will just
+    abort on positive rules, as it doesn't seem to make sense to abort
+    on ignored output.
 
     If both +TIMESTAMP and +RELATIVETIMER are specified, they are
     output in that order.
