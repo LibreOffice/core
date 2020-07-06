125f3c4c930bd28a42c6819417b11b885e4586fc
Noel Grandin <noelgrandin@gmail.com>
2020-07-06 18:46:12 +0200

    drawinglayer: create instances with uno constructors
    See tdf#74608 for motivation
    
    Change-Id: I38de5ec7b9e376ef8868bebbe7f30240de81c241
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/98219
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

--- drawinglayer/Library_drawinglayer.mk
+++ drawinglayer/Library_drawinglayer.mk
@@ -187,7 +187,6 @@ $(eval $(call gb_Library_add_exception_objects,drawinglayer,\
     drawinglayer/source/tools/emfpcustomlinecap \
     drawinglayer/source/tools/wmfemfhelper \
     drawinglayer/source/tools/primitive2dxmldump \
-    drawinglayer/source/drawinglayeruno/drawinglayeruno \
     drawinglayer/source/drawinglayeruno/xprimitive2drenderer \
     drawinglayer/source/texture/texture \
     drawinglayer/source/dumper/XShapeDumper \
