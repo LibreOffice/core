f6fc6ab2a2792c22a3e74cce83ac7d19c820c9b4
Tünde Tóth <tundeth@gmail.com>
2020-07-07 10:50:24 +0200

    tdf#122014 Chart OOXML import: set chart title alignment to center
    Regression from commit: d4190685ac208677bc77f66976287dda0360c42d
    (tdf#114836, only set changed SfxItemSet properties)
    
    Change-Id: If33fa39019bbd36632d15eb3cc0606727e58b111
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/98241
    Tested-by: Jenkins
    Tested-by: László Németh <nemeth@numbertext.org>
    Reviewed-by: László Németh <nemeth@numbertext.org>

--- sw/qa/extras/layout/layout.cxx
+++ sw/qa/extras/layout/layout.cxx
@@ -2823,6 +2823,23 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf132956)
                        "Category 1");
 }
 
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf122014)
+{
+    SwDoc* pDoc = createDoc("tdf122014.docx");
+    SwDocShell* pShell = pDoc->GetDocShell();
+
+    // Dump the rendering of the first page as an XML file.
+    std::shared_ptr<GDIMetaFile> xMetaFile = pShell->GetPreviewMetaFile();
+    MetafileXmlDump dumper;
+    xmlDocUniquePtr pXmlDoc = dumpAndParse(dumper, *xMetaFile);
+    CPPUNIT_ASSERT(pXmlDoc);
+
+    // This failed, if the chart title is aligned to left.
+    sal_Int32 nX1 = getXPath(pXmlDoc, "//textarray[13]", "x").toInt32();
+    sal_Int32 nX2 = getXPath(pXmlDoc, "//textarray[14]", "x").toInt32();
+    CPPUNIT_ASSERT_GREATER(nX1 + 100, nX2);
+}
+
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf134235)
 {
     SwDoc* pDoc = createDoc("tdf134235.docx");
