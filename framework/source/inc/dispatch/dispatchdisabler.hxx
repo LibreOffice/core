d1fe55d4deaec7841f1e950fddae2f413b65776c
Andrea Gelmini <andrea.gelmini@gelma.net>
2020-07-09 17:44:32 +0200

    Removed duplicated include
    Change-Id: I8caa3f1be66ca8cf236e9ada8487ea81f45b1377

--- framework/source/inc/dispatch/dispatchdisabler.hxx
+++ framework/source/inc/dispatch/dispatchdisabler.hxx
@@ -15,7 +15,6 @@
 
 #include <com/sun/star/lang/XServiceInfo.hpp>
 #include <com/sun/star/lang/XInitialization.hpp>
-#include <com/sun/star/lang/XServiceInfo.hpp>
 #include <com/sun/star/uno/XComponentContext.hpp>
 #include <com/sun/star/container/XNameContainer.hpp>
 #include <com/sun/star/frame/XDispatch.hpp>
