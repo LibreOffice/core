7f814b89339811fa29a9e5cc5df5951c8d94c740
Michael Stahl <Michael.Stahl@cib.de>
2020-07-10 19:29:27 +0200

    tdf#134618 sw: DOCX export: fix order of as-char and at-char fly...
    ...at same position.
    
    The problem is that in this case the as-char fly was written before the
    at-char fly but the positioning of the at-char fly can be relative to
    its character position, i.e. before the as-char fly.
    
    Apparently as-char flys are written in
    DocxAttributeOutput::EndRunProperties() via WritePostponedDMLDrawing(),
    wheras at-char flys are written earlier, in SwWW8AttrIter::OutFlys() via
    DocxAttributeOutput::OutputFlyFrame_Impl().
    
    So this undoes the swap that these undergo via the magic of the mark
    stack.
    
    Change-Id: I83a72bb2affbf321fc4dea4e7fb37bdb43cea2e7
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/98543
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@cib.de>
    (cherry picked from commit 7b156d37cfc92292323694ec064fe51ae57b3257)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/98633
    Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>

--- sw/source/filter/ww8/docxattributeoutput.cxx
+++ sw/source/filter/ww8/docxattributeoutput.cxx
@@ -2666,6 +2666,10 @@ void DocxAttributeOutput::EndRunProperties( const SwRedlineData* pRedlineData )
     // write footnotes/endnotes if we have any
     FootnoteEndnoteReference();
 
+    // merge the properties _before_ the run text (strictly speaking, just
+    // after the start of the run)
+    m_pSerializer->mergeTopMarks(Tag_StartRunProperties, sax_fastparser::MergeMarks::PREPEND);
+
     WritePostponedGraphic();
 
     WritePostponedDiagram();
@@ -2678,10 +2682,6 @@ void DocxAttributeOutput::EndRunProperties( const SwRedlineData* pRedlineData )
     WritePostponedOLE();
 
     WritePostponedActiveXControl(true);
-
-    // merge the properties _before_ the run text (strictly speaking, just
-    // after the start of the run)
-    m_pSerializer->mergeTopMarks(Tag_StartRunProperties, sax_fastparser::MergeMarks::PREPEND);
 }
 
 void DocxAttributeOutput::GetSdtEndBefore(const SdrObject* pSdrObj)
@@ -5731,7 +5731,7 @@ void DocxAttributeOutput::OutputFlyFrame_Impl( const ww8::Frame &rFrame, const P
             break;
     }
 
-    m_pSerializer->mergeTopMarks(Tag_OutputFlyFrame, sax_fastparser::MergeMarks::POSTPONE);
+    m_pSerializer->mergeTopMarks(Tag_OutputFlyFrame);
 }
 
 void DocxAttributeOutput::WriteOutliner(const OutlinerParaObject& rParaObj)
