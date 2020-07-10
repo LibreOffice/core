LibreOffice/core

commit 1cd0e02cdf7fc5b5903320ffd24865d11465d14b

cairo/canvas: create instances with uno constructors
See tdf#74608 for motivation

Change-Id: I02411b23136051bfd0cd02d5378297f99127017f
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/98189
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

canvas/source/cairo/cairo_canvas.cxx
@@ -113,7 +113,7 @@ namespace cairocanvas
 
     OUString SAL_CALL Canvas::getServiceName(  )
     {
-        return CANVAS_SERVICE_NAME;
+        return "com.sun.star.rendering.Canvas.Cairo";
     }
 
     bool Canvas::repaint( const SurfaceSharedPtr&       pSurface,
@@ -158,5 +158,16 @@ namespace cairocanvas
         return maDeviceHelper.getOutputDevice();
     }
 }
+
+extern "C" SAL_DLLPUBLIC_EXPORT css::uno::XInterface*
+com_sun_star_comp_rendering_Canvas_Cairo_get_implementation(
+    css::uno::XComponentContext* context, css::uno::Sequence<css::uno::Any> const& args)
+{
+    auto p = new cairocanvas::Canvas(args, context);
+    p->acquire();
+    p->initialize();
+    return static_cast<cppu::OWeakObject*>(p);
+}
+
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

canvas/source/cairo/cairo_canvas.hxx
@@ -40,9 +40,6 @@
 #include "cairo_repainttarget.hxx"
 #include "cairo_surfaceprovider.hxx"
 
-#define CANVAS_SERVICE_NAME        "com.sun.star.rendering.Canvas.Cairo"
-#define CANVAS_IMPLEMENTATION_NAME "com.sun.star.comp.rendering.Canvas.Cairo"
-
 namespace cairocanvas
 {
     typedef ::cppu::WeakComponentImplHelper< css::rendering::XBitmapCanvas,

canvas/source/cairo/cairo_spritecanvas.cxx
@@ -132,7 +132,7 @@ namespace cairocanvas
 
     OUString SAL_CALL SpriteCanvas::getServiceName(  )
     {
-        return SPRITECANVAS_SERVICE_NAME;
+        return "com.sun.star.rendering.SpriteCanvas.Cairo";
     }
 
     SurfaceSharedPtr SpriteCanvas::getSurface()
@@ -202,6 +202,16 @@ namespace cairocanvas
     {
         return maCanvasHelper.repaint( pSurface, viewState, renderState );
     }
+}
+
+extern "C" SAL_DLLPUBLIC_EXPORT css::uno::XInterface*
+com_sun_star_comp_rendering_SpriteCanvas_Cairo_get_implementation(
+    css::uno::XComponentContext* context, css::uno::Sequence<css::uno::Any> const& args)
+{
+    auto p = new cairocanvas::SpriteCanvas(args, context);
+    p->acquire();
+    p->initialize();
+    return static_cast<cppu::OWeakObject*>(p);
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

canvas/source/cairo/cairo_spritecanvas.hxx
@@ -43,9 +43,6 @@
 #include "cairo_surfaceprovider.hxx"
 #include "cairo_spritecanvashelper.hxx"
 
-#define SPRITECANVAS_SERVICE_NAME        "com.sun.star.rendering.SpriteCanvas.Cairo"
-#define SPRITECANVAS_IMPLEMENTATION_NAME "com.sun.star.comp.rendering.SpriteCanvas.Cairo"
-
 namespace cairocanvas
 {
     typedef ::cppu::WeakComponentImplHelper< css::rendering::XSpriteCanvas,

canvas/Library_cairocanvas.mk
@@ -58,7 +58,6 @@ $(eval $(call gb_Library_add_exception_objects,cairocanvas,\
 	canvas/source/cairo/cairo_canvashelper \
 	canvas/source/cairo/cairo_canvashelper_text \
 	canvas/source/cairo/cairo_devicehelper \
-	canvas/source/cairo/cairo_services \
 	canvas/source/cairo/cairo_spritecanvas \
 	canvas/source/cairo/cairo_spritecanvashelper \
 	canvas/source/cairo/cairo_spritedevicehelper \

canvas/source/cairo/cairo_services.cxx
@@ -1,74 +1,0 @@
-/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
-/*
- * This file is part of the LibreOffice project.
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- *
- * This file incorporates work covered by the following license notice:
- *
- *   Licensed to the Apache Software Foundation (ASF) under one or more
- *   contributor license agreements. See the NOTICE file distributed
- *   with this work for additional information regarding copyright
- *   ownership. The ASF licenses this file to you under the Apache
- *   License, Version 2.0 (the "License"); you may not use this file
- *   except in compliance with the License. You may obtain a copy of
- *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
- */
-
-#include <sal/config.h>
-
-#include <comphelper/servicedecl.hxx>
-
-#include "cairo_canvas.hxx"
-#include "cairo_spritecanvas.hxx"
-
-
-using namespace ::com::sun::star;
-
-#if defined(_WIN32) || defined (MACOSX)
-#  error "The cairo canvas should not be enabled on Windows or Mac cf fdo#46901"
-#endif
-
-namespace sdecl = comphelper::service_decl;
-
-namespace cairocanvas
-{
-    static uno::Reference<uno::XInterface> initCanvas( Canvas* pCanvas )
-    {
-        uno::Reference<uno::XInterface> xRet(static_cast<cppu::OWeakObject*>(pCanvas));
-        pCanvas->initialize();
-        return xRet;
-    }
-
-    sdecl::class_< Canvas, sdecl::with_args<true> > const serviceImpl1(&initCanvas);
-    const sdecl::ServiceDecl cairoCanvasDecl(
-        serviceImpl1,
-        CANVAS_IMPLEMENTATION_NAME,
-        CANVAS_SERVICE_NAME );
-
-    static uno::Reference<uno::XInterface> initSpriteCanvas( SpriteCanvas* pCanvas )
-    {
-        uno::Reference<uno::XInterface> xRet(static_cast<cppu::OWeakObject*>(pCanvas));
-        pCanvas->initialize();
-        return xRet;
-    }
-
-    namespace sdecl = comphelper::service_decl;
-    sdecl::class_< SpriteCanvas, sdecl::with_args<true> > const serviceImpl2(&initSpriteCanvas);
-    const sdecl::ServiceDecl cairoSpriteCanvasDecl(
-        serviceImpl2,
-        SPRITECANVAS_IMPLEMENTATION_NAME,
-        SPRITECANVAS_SERVICE_NAME );
-}
-
-// The C shared lib entry points
-extern "C"
-SAL_DLLPUBLIC_EXPORT void* cairocanvas_component_getFactory( char const* pImplName,
-                                         void*, void* )
-{
-    return sdecl::component_getFactoryHelper( pImplName, {&cairocanvas::cairoCanvasDecl, &cairocanvas::cairoSpriteCanvasDecl} );
-}
-
-/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

canvas/source/cairo/cairocanvas.component
@@ -18,11 +18,13 @@
  -->
 
 <component loader="com.sun.star.loader.SharedLibrary" environment="@CPPU_ENV@"
-    prefix="cairocanvas" xmlns="http://openoffice.org/2010/uno-components">
-  <implementation name="com.sun.star.comp.rendering.Canvas.Cairo">
+    xmlns="http://openoffice.org/2010/uno-components">
+  <implementation name="com.sun.star.comp.rendering.Canvas.Cairo"
+    constructor="com_sun_star_comp_rendering_Canvas_Cairo_get_implementation">
     <service name="com.sun.star.rendering.Canvas.Cairo"/>
   </implementation>
-  <implementation name="com.sun.star.comp.rendering.SpriteCanvas.Cairo">
+  <implementation name="com.sun.star.comp.rendering.SpriteCanvas.Cairo"
+    constructor="com_sun_star_comp_rendering_SpriteCanvas_Cairo_get_implementation">
     <service name="com.sun.star.rendering.SpriteCanvas.Cairo"/>
   </implementation>
 </component>

commit 71f03c99d170c162d222cde25a4904b6747bb90b

tdf#134647 crash going to next slide on slideshow
regression from
	commit 1cd0e02cdf7fc5b5903320ffd24865d11465d14b
	cairo/canvas: create instances with uno constructors

Change-Id: I4d09f8d68a24b0efa47d12df1c542f2953efed3d

canvas/source/cairo/cairo_canvas.cxx
@@ -27,6 +27,7 @@
 #include <vcl/sysdata.hxx>
 #include <vcl/opengl/OpenGLWrapper.hxx>
 #include <vcl/skia/SkiaHelper.hxx>
+#include <cppuhelper/supportsservice.hxx>
 
 #include "cairo_canvas.hxx"
 
@@ -114,6 +115,21 @@ namespace cairocanvas
     OUString SAL_CALL Canvas::getServiceName(  )
     {
         return "com.sun.star.rendering.Canvas.Cairo";
+    }
+
+    //  XServiceInfo
+    sal_Bool Canvas::supportsService(const OUString& sServiceName)
+    {
+        return cppu::supportsService(this, sServiceName);
+
+    }
+    OUString Canvas::getImplementationName()
+    {
+        return "com.sun.star.comp.rendering.Canvas.Cairo";
+    }
+    css::uno::Sequence< OUString > Canvas::getSupportedServiceNames()
+    {
+        return { getServiceName() };
     }
 
     bool Canvas::repaint( const SurfaceSharedPtr&       pSurface,

canvas/source/cairo/cairo_canvas.hxx
@@ -22,6 +22,7 @@
 #include <com/sun/star/uno/XComponentContext.hpp>
 #include <com/sun/star/beans/XPropertySet.hpp>
 #include <com/sun/star/lang/XServiceName.hpp>
+#include <com/sun/star/lang/XServiceInfo.hpp>
 #include <com/sun/star/util/XUpdatable.hpp>
 #include <com/sun/star/rendering/XIntegerBitmap.hpp>
 #include <com/sun/star/rendering/XGraphicDevice.hpp>
@@ -48,7 +49,8 @@ namespace cairocanvas
                                              css::lang::XMultiServiceFactory,
                                              css::util::XUpdatable,
                                              css::beans::XPropertySet,
-                                             css::lang::XServiceName >  GraphicDeviceBase_Base;
+                                             css::lang::XServiceName,
+                                             css::lang::XServiceInfo >  GraphicDeviceBase_Base;
     typedef ::canvas::GraphicDeviceBase< ::canvas::BaseMutexHelper< GraphicDeviceBase_Base >,
                                                  DeviceHelper,
                                                  ::osl::MutexGuard,
@@ -114,6 +116,11 @@ namespace cairocanvas
 
         // XServiceName
         virtual OUString SAL_CALL getServiceName(  ) override;
+
+        //  XServiceInfo
+        virtual sal_Bool SAL_CALL supportsService(const OUString& sServiceName) override;
+        virtual OUString SAL_CALL getImplementationName() override;
+        virtual css::uno::Sequence< OUString > SAL_CALL getSupportedServiceNames() override;
 
         // RepaintTarget
         virtual bool repaint( const ::cairo::SurfaceSharedPtr& pSurface,

canvas/source/cairo/cairo_spritecanvas.cxx
@@ -28,6 +28,7 @@
 #include <osl/mutex.hxx>
 #include <toolkit/helper/vclunohelper.hxx>
 #include <tools/diagnose_ex.h>
+#include <cppuhelper/supportsservice.hxx>
 
 #include "cairo_spritecanvas.hxx"
 
@@ -133,6 +134,21 @@ namespace cairocanvas
     OUString SAL_CALL SpriteCanvas::getServiceName(  )
     {
         return "com.sun.star.rendering.SpriteCanvas.Cairo";
+    }
+
+    //  XServiceInfo
+    sal_Bool SpriteCanvas::supportsService(const OUString& sServiceName)
+    {
+        return cppu::supportsService(this, sServiceName);
+
+    }
+    OUString SpriteCanvas::getImplementationName()
+    {
+        return "com.sun.star.comp.rendering.Canvas.Cairo";
+    }
+    css::uno::Sequence< OUString > SpriteCanvas::getSupportedServiceNames()
+    {
+        return { getServiceName() };
     }
 
     SurfaceSharedPtr SpriteCanvas::getSurface()

canvas/source/cairo/cairo_spritecanvas.hxx
@@ -23,6 +23,7 @@
 #include <com/sun/star/uno/XComponentContext.hpp>
 #include <com/sun/star/beans/XPropertySet.hpp>
 #include <com/sun/star/lang/XServiceName.hpp>
+#include <com/sun/star/lang/XServiceInfo.hpp>
 #include <com/sun/star/awt/XWindowListener.hpp>
 #include <com/sun/star/util/XUpdatable.hpp>
 #include <com/sun/star/rendering/XSpriteCanvas.hpp>
@@ -53,7 +54,8 @@ namespace cairocanvas
                                              css::awt::XWindowListener,
                                              css::util::XUpdatable,
                                              css::beans::XPropertySet,
-                                             css::lang::XServiceName >  WindowGraphicDeviceBase_Base;
+                                             css::lang::XServiceName,
+                                             css::lang::XServiceInfo >  WindowGraphicDeviceBase_Base;
     typedef ::canvas::BufferedGraphicDeviceBase< ::canvas::DisambiguationHelper< WindowGraphicDeviceBase_Base >,
                                                  SpriteDeviceHelper,
                                                  ::osl::MutexGuard,
@@ -123,6 +125,11 @@ namespace cairocanvas
 
         // XServiceName
         virtual OUString SAL_CALL getServiceName(  ) override;
+
+        //  XServiceInfo
+        virtual sal_Bool SAL_CALL supportsService(const OUString& sServiceName) override;
+        virtual OUString SAL_CALL getImplementationName() override;
+        virtual css::uno::Sequence< OUString > SAL_CALL getSupportedServiceNames() override;
 
         // SurfaceProvider
         virtual ::cairo::SurfaceSharedPtr getSurface() override;