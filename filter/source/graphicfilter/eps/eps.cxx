LibreOffice/core

commit cf82475c785c47327cdc9d591d63d7a82dd1ac53

tdf#114441 Changing sal_uLong to appropriate integer type in filters module
   changed sal_uLong to one of {size_t, sal_uInt64, sal_uInt32, sal_uInt16, sal_uInt8} in flash/impswfdialog.cxx and graphicsfilter

   changed PS_* in file filter/source/eps/eps.cxx to enum and overloaded bitwise or (|)
   made types of nSize, nParseThis, nComp and nBytesRead to sal_uInt32 as Michael Stahl suggested

   changes suggested by Michael Stahl
   changed x, y, j to sal_uInt32 in filter/source/graphicfilter/etiff/etiff.cxx
   changed nMinPercent, nMaxPercent, nLastPercent, nMainEntitiesCount to sal_uInt16 in filter/source/graphicfilter/idxf/dxf2mtf.hxx
   removed nGcount variable from filter/source/graphicfilter/idxf/dxfgrprd.hxx and filter/source/graphicfilter/idxf/dxfgrprd.cxx
   removed static_cast in filter/source/graphicfilter/idxf/dxf2mtf.cxx
   changed nComp in filter/source/graphicfilter/ieps/ieps.cxx to size_t
   changed mnCol to sal_uInt16 in filter/source/graphicfilter/ipbm/ipbm.cxx

   revised a number of datatypes as suggested by michael stahl in his reviews

   removed redundant static casts which caused test to fail

Change-Id: Id892ee7e9c1ef3cf75c9d768b790ced9c2ce3f3b
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/84726
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@cib.de>

filter/source/graphicfilter/eps/eps.cxx
@@ -52,11 +52,6 @@ using namespace ::com::sun::star::uno;
 
 #define PS_LINESIZE         70      // maximum number of characters a line in the output
 
-#define PS_NONE             0       // formatting mode: action which is inserted behind the output
-#define PS_SPACE            1
-#define PS_RET              2
-#define PS_WRAP             4
-
 // -----------------------------field-types------------------------------
 
 namespace {
@@ -92,6 +87,12 @@ struct PSLZWCTreeNode
     sal_uInt16          nValue;         // the pixel value
 };
 
+enum NMode {PS_NONE = 0x00, PS_SPACE = 0x01, PS_RET = 0x02, PS_WRAP = 0x04}; // formatting mode: action which is inserted behind the output
+inline NMode operator|(NMode a, NMode b)
+{
+    return static_cast<NMode>(static_cast<sal_uInt8>(a) | static_cast<sal_uInt8>(b));
+}
+
 class PSWriter
 {
 private:
@@ -116,7 +117,7 @@ private:
     double              nBoundingY2;
 
     StackMember*        pGDIStack;
-    sal_uLong           mnCursorPos;        // current cursor position in output
+    sal_uInt32           mnCursorPos;        // current cursor position in output
     Color               aColor;             // current color which is used for output
     bool                bLineColor;
     Color               aLineColor;         // current GDIMetafile color settings
@@ -156,33 +157,33 @@ private:
 
                         // this method makes LF's, space inserting and word wrapping as used in all nMode
                         // parameters
-    inline void         ImplExecMode( sal_uLong nMode );
+    inline void         ImplExecMode( NMode nMode );
 
                         // writes char[] + LF to stream
-    inline void         ImplWriteLine( const char*, sal_uLong nMode = PS_RET );
+    inline void         ImplWriteLine( const char*, NMode nMode = PS_RET );
 
                         // writes ( nNumb / 10^nCount ) in ASCII format to stream
-    void                ImplWriteF( sal_Int32 nNumb, sal_uLong nCount = 3, sal_uLong nMode = PS_SPACE );
+    void                ImplWriteF( sal_Int32 nNumb, sal_uInt8 nCount = 3, NMode nMode = PS_SPACE );
 
                         // writes a double in ASCII format to stream
     void                ImplWriteDouble( double );
 
                         // writes a long in ASCII format to stream
-    void                ImplWriteLong( sal_Int32 nNumb, sal_uLong nMode = PS_SPACE );
+    void                ImplWriteLong( sal_Int32 nNumb, NMode nMode = PS_SPACE );
 
                         // writes a byte in ASCII format to stream
-    void                ImplWriteByte( sal_uInt8 nNumb, sal_uLong nMode = PS_SPACE );
+    void                ImplWriteByte( sal_uInt8 nNumb, NMode nMode = PS_SPACE );
 
                         // writes a byte in ASCII (hex) format to stream
-    void                ImplWriteHexByte( sal_uInt8 nNumb, sal_uLong nMode = PS_WRAP );
+    void                ImplWriteHexByte( sal_uInt8 nNumb, NMode nMode = PS_WRAP );
 
                         // writes nNumb as number from 0.000 till 1.000 in ASCII format to stream
     void                ImplWriteB1( sal_uInt8 nNumb );
 
     inline void         ImplWritePoint( const Point& );
     void                ImplMoveTo( const Point& );
-    void                ImplLineTo( const Point&, sal_uInt32 nMode = PS_SPACE );
-    void                ImplCurveTo( const Point& rP1, const Point& rP2, const Point& rP3, sal_uInt32 nMode );
+    void                ImplLineTo( const Point&, NMode nMode = PS_SPACE );
+    void                ImplCurveTo( const Point& rP1, const Point& rP2, const Point& rP3, NMode nMode );
     void                ImplTranslate( const double& fX, const double& fY );
     void                ImplScale( const double& fX, const double& fY );
 
@@ -208,15 +209,15 @@ private:
     void                ImplClosePathDraw();
     void                ImplPathDraw();
 
-    inline void         ImplWriteLineColor( sal_uLong nMode );
-    inline void         ImplWriteFillColor( sal_uLong nMode );
-    inline void         ImplWriteTextColor( sal_uLong nMode );
-    void                ImplWriteColor( sal_uLong nMode );
+    inline void         ImplWriteLineColor( NMode nMode );
+    inline void         ImplWriteFillColor( NMode nMode );
+    inline void         ImplWriteTextColor( NMode nMode );
+    void                ImplWriteColor( NMode nMode );
 
     static double       ImplGetScaling( const MapMode& );
     void                ImplGetMapMode( const MapMode& );
-    static bool         ImplGetBoundingBox( double* nNumb, sal_uInt8* pSource, sal_uLong nSize );
-    static sal_uInt8*   ImplSearchEntry( sal_uInt8* pSource, sal_uInt8 const * pDest, sal_uLong nComp, sal_uLong nSize );
+    static bool         ImplGetBoundingBox( double* nNumb, sal_uInt8* pSource, sal_uInt32 nSize );
+    static sal_uInt8*   ImplSearchEntry( sal_uInt8* pSource, sal_uInt8 const * pDest, sal_uInt32 nComp, sal_uInt32 nSize );
                         // LZW methods
     void                StartCompression();
     void                Compress( sal_uInt8 nSrc );
@@ -1102,8 +1103,8 @@ void PSWriter::ImplWriteActions( const GDIMetaFile& rMtf, VirtualDevice& rVDev )
 
                 bool    bLevelConflict = false;
                 sal_uInt8*  pSource = const_cast<sal_uInt8*>(aGfxLink.GetData());
-                sal_uLong   nSize = aGfxLink.GetDataSize();
-                sal_uLong   nParseThis = POSTSCRIPT_BOUNDINGSEARCH;
+                sal_uInt32   nSize = aGfxLink.GetDataSize();
+                sal_uInt32 nParseThis = POSTSCRIPT_BOUNDINGSEARCH;
                 if ( nSize < 64 )                       // assuming eps is larger than 64 bytes
                     pSource = nullptr;
                 if ( nParseThis > nSize )
@@ -1412,14 +1413,14 @@ void PSWriter::ImplMoveTo( const Point& rPoint )
     ImplExecMode( PS_SPACE );
 }
 
-void PSWriter::ImplLineTo( const Point& rPoint, sal_uInt32 nMode )
+void PSWriter::ImplLineTo( const Point& rPoint, NMode nMode )
 {
     ImplWritePoint( rPoint );
     ImplWriteByte( 'l' );
     ImplExecMode( nMode );
 }
 
-void PSWriter::ImplCurveTo( const Point& rP1, const Point& rP2, const Point& rP3, sal_uInt32 nMode )
+void PSWriter::ImplCurveTo( const Point& rP1, const Point& rP2, const Point& rP3, NMode nMode )
 {
     ImplWritePoint( rP1 );
     ImplWritePoint( rP2 );
@@ -2131,7 +2132,7 @@ void PSWriter::ImplPathDraw()
 }
 
 
-inline void PSWriter::ImplWriteLineColor( sal_uLong nMode )
+inline void PSWriter::ImplWriteLineColor( NMode nMode )
 {
     if ( aColor != aLineColor )
     {
@@ -2140,7 +2141,7 @@ inline void PSWriter::ImplWriteLineColor( sal_uLong nMode )
     }
 }
 
-inline void PSWriter::ImplWriteFillColor( sal_uLong nMode )
+inline void PSWriter::ImplWriteFillColor( NMode nMode )
 {
     if ( aColor != aFillColor )
     {
@@ -2149,7 +2150,7 @@ inline void PSWriter::ImplWriteFillColor( sal_uLong nMode )
     }
 }
 
-inline void PSWriter::ImplWriteTextColor( sal_uLong nMode )
+inline void PSWriter::ImplWriteTextColor( NMode nMode )
 {
     if ( aColor != aTextColor )
     {
@@ -2158,7 +2159,7 @@ inline void PSWriter::ImplWriteTextColor( sal_uLong nMode )
     }
 }
 
-void PSWriter::ImplWriteColor( sal_uLong nMode )
+void PSWriter::ImplWriteColor( NMode nMode )
 {
     if ( mbGrayScale )
     {
@@ -2187,7 +2188,7 @@ void PSWriter::ImplGetMapMode( const MapMode& rMapMode )
     ImplScale( fScaleX, fScaleY );
 }
 
-inline void PSWriter::ImplExecMode( sal_uLong nMode )
+inline void PSWriter::ImplExecMode( NMode nMode )
 {
     if ( nMode & PS_WRAP )
     {
@@ -2210,9 +2211,9 @@ inline void PSWriter::ImplExecMode( sal_uLong nMode )
     }
 }
 
-inline void PSWriter::ImplWriteLine( const char* pString, sal_uLong nMode )
+inline void PSWriter::ImplWriteLine( const char* pString, NMode nMode )
 {
-    sal_uLong i = 0;
+    sal_uInt32 i = 0;
     while ( pString[ i ] )
     {
         mpPS->WriteUChar( pString[ i++ ] );
@@ -2359,7 +2360,7 @@ void PSWriter::ImplWriteLineInfo( const LineInfo& rLineInfo )
     ImplWriteLineInfo( fLWidth, fMiterLimit, aCapType, aJoinType, l_aDashArray );
 }
 
-void PSWriter::ImplWriteLong(sal_Int32 nNumber, sal_uLong nMode)
+void PSWriter::ImplWriteLong(sal_Int32 nNumber, NMode nMode)
 {
     const OString aNumber(OString::number(nNumber));
     mnCursorPos += aNumber.getLength();
@@ -2410,7 +2411,7 @@ void PSWriter::ImplWriteDouble( double fNumber )
 }
 
 /// Writes the number to stream: nNumber / ( 10^nCount )
-void PSWriter::ImplWriteF( sal_Int32 nNumber, sal_uLong nCount, sal_uLong nMode )
+void PSWriter::ImplWriteF( sal_Int32 nNumber, sal_uInt8 nCount, NMode nMode )
 {
     if ( nNumber < 0 )
     {
@@ -2419,7 +2420,7 @@ void PSWriter::ImplWriteF( sal_Int32 nNumber, sal_uLong nCount, sal_uLong nMode 
         mnCursorPos++;
     }
     const OString aScaleFactor(OString::number(nNumber));
-    sal_uLong nLen = aScaleFactor.getLength();
+    sal_uInt32 nLen = aScaleFactor.getLength();
     long nStSize =  ( nCount + 1 ) - nLen;
     if ( nStSize >= 1 )
     {
@@ -2436,7 +2437,7 @@ void PSWriter::ImplWriteF( sal_Int32 nNumber, sal_uLong nCount, sal_uLong nMode 
         }
     }
     mnCursorPos += nLen;
-    for( sal_uLong n = 0; n < nLen; n++  )
+    for( sal_uInt32 n = 0; n < nLen; n++  )
     {
         if ( n == nLen - nCount )
         {
@@ -2448,14 +2449,14 @@ void PSWriter::ImplWriteF( sal_Int32 nNumber, sal_uLong nCount, sal_uLong nMode 
     ImplExecMode( nMode );
 }
 
-void PSWriter::ImplWriteByte( sal_uInt8 nNumb, sal_uLong nMode )
+void PSWriter::ImplWriteByte( sal_uInt8 nNumb, NMode nMode )
 {
     mpPS->WriteUChar( nNumb );
     mnCursorPos++;
     ImplExecMode( nMode );
 }
 
-void PSWriter::ImplWriteHexByte( sal_uInt8 nNumb, sal_uLong nMode )
+void PSWriter::ImplWriteHexByte( sal_uInt8 nNumb, NMode nMode )
 {
     if ( ( nNumb >> 4 ) > 9 )
         mpPS->WriteUChar( ( nNumb >> 4 ) + 'A' - 10 );
@@ -2577,11 +2578,11 @@ void PSWriter::EndCompression()
     pTable.reset();
 }
 
-sal_uInt8* PSWriter::ImplSearchEntry( sal_uInt8* pSource, sal_uInt8 const * pDest, sal_uLong nComp, sal_uLong nSize )
+sal_uInt8* PSWriter::ImplSearchEntry( sal_uInt8* pSource, sal_uInt8 const * pDest, sal_uInt32 nComp, sal_uInt32 nSize )
 {
     while ( nComp-- >= nSize )
     {
-        sal_uLong i;
+        sal_uInt64 i;
         for ( i = 0; i < nSize; i++ )
         {
             if ( ( pSource[i]&~0x20 ) != ( pDest[i]&~0x20 ) )
@@ -2594,10 +2595,10 @@ sal_uInt8* PSWriter::ImplSearchEntry( sal_uInt8* pSource, sal_uInt8 const * pDes
     return nullptr;
 }
 
-bool PSWriter::ImplGetBoundingBox( double* nNumb, sal_uInt8* pSource, sal_uLong nSize )
+bool PSWriter::ImplGetBoundingBox( double* nNumb, sal_uInt8* pSource, sal_uInt32 nSize )
 {
     bool    bRetValue = false;
-    sal_uLong   nBytesRead;
+    sal_uInt32   nBytesRead;
 
     if ( nSize < 256 )      // we assume that the file is greater than 256 bytes
         return false;

filter/source/flash/impswfdialog.cxx
@@ -37,7 +37,7 @@ ImpSWFDialog::ImpSWFDialog(weld::Window* pParent, Sequence< PropertyValue >& rFi
     , mxCheckExportOLEAsJPEG(m_xBuilder->weld_check_button("exportoleasjpeg"))
     , mxCheckExportMultipleFiles(m_xBuilder->weld_check_button("exportmultiplefiles"))
 {
-    const sal_uLong nCompressMode = maConfigItem.ReadInt32( "CompressMode", 75 );
+    const sal_Int32 nCompressMode = maConfigItem.ReadInt32( "CompressMode", 75 );
     mxNumFldQuality->set_value(nCompressMode);
 
     mxCheckExportAll->set_active(true);

filter/source/graphicfilter/etiff/etiff.cxx
@@ -97,7 +97,7 @@ private:
     void                ImplWritePalette();
     void                ImplWriteBody();
     void                ImplWriteTag( sal_uInt16 TagID, sal_uInt16 DataType, sal_uInt32 NumberOfItems, sal_uInt32 Value);
-    void                ImplWriteResolution( sal_uLong nStreamPos, sal_uInt32 nResolutionUnit );
+    void                ImplWriteResolution( sal_uInt64 nStreamPos, sal_uInt32 nResolutionUnit );
     void                StartCompression();
     void                Compress( sal_uInt8 nSrc );
     void                EndCompression();
@@ -313,7 +313,7 @@ bool TIFFWriter::ImplWriteHeader( bool bMultiPage )
 
 void TIFFWriter::ImplWritePalette()
 {
-    sal_uLong nCurrentPos = m_rOStm.Tell();
+    sal_uInt64 nCurrentPos = m_rOStm.Tell();
     m_rOStm.Seek( mnPalPos + 8 );           // the palette tag entry needs the offset
     m_rOStm.WriteUInt32( nCurrentPos - mnStreamOfs );  // to the palette colors
     m_rOStm.Seek( nCurrentPos );
@@ -340,9 +340,9 @@ void TIFFWriter::ImplWriteBody()
 {
     sal_uInt8   nTemp = 0;
     sal_uInt8    nShift;
-    sal_uLong   j, x, y;
+    sal_uInt32   j, x, y;
 
-    sal_uLong nGfxBegin = m_rOStm.Tell();
+    sal_uInt64 nGfxBegin = m_rOStm.Tell();
     m_rOStm.Seek( mnBitmapPos + 8 );                // the strip offset tag entry needs the offset
     m_rOStm.WriteUInt32( nGfxBegin - mnStreamOfs ); // to the bitmap data
     m_rOStm.Seek( nGfxBegin );
@@ -438,7 +438,7 @@ void TIFFWriter::ImplWriteBody()
 
     if ( mnStripByteCountPos && mbStatus )
     {
-        sal_uLong nGfxEnd = m_rOStm.Tell();
+        sal_uInt64 nGfxEnd = m_rOStm.Tell();
         m_rOStm.Seek( mnStripByteCountPos + 8 );
         m_rOStm.WriteUInt32( nGfxEnd - nGfxBegin );      // mnStripByteCountPos needs the size of the compression data
         m_rOStm.Seek( nGfxEnd );
@@ -446,9 +446,9 @@ void TIFFWriter::ImplWriteBody()
 }
 
 
-void TIFFWriter::ImplWriteResolution( sal_uLong nStreamPos, sal_uInt32 nResolutionUnit )
+void TIFFWriter::ImplWriteResolution( sal_uInt64 nStreamPos, sal_uInt32 nResolutionUnit )
 {
-    sal_uLong nCurrentPos = m_rOStm.Tell();
+    sal_uInt64 nCurrentPos = m_rOStm.Tell();
     m_rOStm.Seek( nStreamPos + 8 );
     m_rOStm.WriteUInt32( nCurrentPos - mnStreamOfs );
     m_rOStm.Seek( nCurrentPos );

filter/source/graphicfilter/idxf/dxf2mtf.cxx
@@ -28,10 +28,10 @@
 #include <math.h>
 
 
-sal_uLong DXF2GDIMetaFile::CountEntities(const DXFEntities & rEntities)
+sal_uInt64 DXF2GDIMetaFile::CountEntities(const DXFEntities & rEntities)
 {
     const DXFBasicEntity * pBE;
-    sal_uLong nRes;
+    sal_uInt64 nRes;
 
     nRes=0;
     for (pBE=rEntities.pFirst; pBE!=nullptr; pBE=pBE->pSucc) nRes++;
@@ -784,8 +784,8 @@ bool DXF2GDIMetaFile::Convert(const DXFRepresentation & rDXF, GDIMetaFile & rMTF
 
     OptPointsPerCircle=50;
 
-    nMinPercent=static_cast<sal_uLong>(nminpercent);
-    nMaxPercent=static_cast<sal_uLong>(nmaxpercent);
+    nMinPercent=nminpercent;
+    nMaxPercent=nmaxpercent;
     nLastPercent=nMinPercent;
     nMainEntitiesCount=CountEntities(pDXF->aEntities);
 

filter/source/graphicfilter/idxf/dxf2mtf.hxx
@@ -37,10 +37,10 @@ private:
 
     sal_uInt16 OptPointsPerCircle;
 
-    sal_uLong nMinPercent;
-    sal_uLong nMaxPercent;
-    sal_uLong nLastPercent;
-    sal_uLong nMainEntitiesCount;
+    sal_uInt16 nMinPercent;
+    sal_uInt16 nMaxPercent;
+    sal_uInt16 nLastPercent;
+    sal_uInt16 nMainEntitiesCount;
 
     long        nBlockColor;
     DXFLineInfo aBlockDXFLineInfo;
@@ -51,7 +51,7 @@ private:
     vcl::Font   aActFont;
     const LineInfo aDefaultLineInfo; // to share between lines to reduce memory
 
-    static sal_uLong CountEntities(const DXFEntities & rEntities);
+    static sal_uInt64 CountEntities(const DXFEntities & rEntities);
 
     Color ConvertColor(sal_uInt8 nColor);
 

filter/source/graphicfilter/idxf/dxfgrprd.cxx
@@ -30,7 +30,7 @@ static OString DXFReadLine(SvStream& rIStm)
 {
     char  buf[256 + 1];
     bool  bEnd = false;
-    sal_uLong nOldFilePos = rIStm.Tell();
+    sal_uInt64 nOldFilePos = rIStm.Tell();
     char  c = 0;
 
     OStringBuffer aBuf(512);
@@ -109,7 +109,6 @@ DXFGroupReader::DXFGroupReader(SvStream & rIStream)
   : rIS(rIStream)
   , bStatus(true)
   , nLastG(0)
-  , nGCount(0)
   , S()
   , I(0)
 {
@@ -121,7 +120,6 @@ sal_uInt16 DXFGroupReader::Read()
     sal_uInt16 nG = 0;
     if ( bStatus )
     {
-        nGCount++;
         nG = static_cast<sal_uInt16>(ReadI());
         if ( bStatus )
         {
@@ -152,11 +150,6 @@ sal_uInt16 DXFGroupReader::Read()
     {
         nG = 0;
         S = "EOF";
-        if ( nGCount != 0xffffffff )
-        {
-            // InfoBox(NULL,String("Error in group # ")+String(nGCount)).Execute();
-            nGCount=0xffffffff;
-        }
     }
     nLastG = nG;
     return nG;

filter/source/graphicfilter/idxf/dxfgrprd.hxx
@@ -69,7 +69,6 @@ private:
     SvStream & rIS;
     bool bStatus;
     sal_uInt16 nLastG;
-    sal_uLong nGCount;
 
     OString S;
     union {

filter/source/graphicfilter/ieps/ieps.cxx
@@ -50,11 +50,11 @@ class FilterConfigItem;
 |*
 *************************************************************************/
 
-static sal_uInt8* ImplSearchEntry( sal_uInt8* pSource, sal_uInt8 const * pDest, sal_uLong nComp, sal_uLong nSize )
+static sal_uInt8* ImplSearchEntry( sal_uInt8* pSource, sal_uInt8 const * pDest, size_t nComp, size_t nSize )
 {
     while ( nComp-- >= nSize )
     {
-        sal_uLong i;
+        size_t i;
         for ( i = 0; i < nSize; i++ )
         {
             if ( ( pSource[i]&~0x20 ) != ( pDest[i]&~0x20 ) )

commit 0117dc8c016b274a807bf06bdef23f379f419201

tdf#134667 filter: EPS export: fix integer unsigned->signed SNAFU
In PSWriter::ImplWriteF(), (nCount + 1) - nLen used to be unsigned long
but now it's unsigned 32-bit int so on 64-bit platform values > 2^31
no longer init nStSize to negative values.

(regression from cf82475c785c47327cdc9d591d63d7a82dd1ac53)

Change-Id: Ib17537cf67c80883f10cf2a3b7e38d6a8f8dfcb7

filter/source/graphicfilter/eps/eps.cxx
@@ -2421,7 +2421,8 @@ void PSWriter::ImplWriteF( sal_Int32 nNumber, sal_uInt8 nCount, NMode nMode )
     }
     const OString aScaleFactor(OString::number(nNumber));
     sal_uInt32 nLen = aScaleFactor.getLength();
-    long nStSize =  ( nCount + 1 ) - nLen;
+    sal_Int32 const nStSize = (nCount + 1) - nLen;
+    static_assert(sizeof(nStSize) == sizeof((nCount + 1) - nLen)); // tdf#134667
     if ( nStSize >= 1 )
     {
         mpPS->WriteUChar( '0' );
@@ -2430,7 +2431,7 @@ void PSWriter::ImplWriteF( sal_Int32 nNumber, sal_uInt8 nCount, NMode nMode )
     if ( nStSize >= 2 )
     {
         mpPS->WriteUChar( '.' );
-        for ( long i = 1; i < nStSize; i++ )
+        for (sal_Int32 i = 1; i < nStSize; ++i)
         {
             mpPS->WriteUChar( '0' );
             mnCursorPos++;