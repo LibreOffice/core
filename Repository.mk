244de1fc0f3f201f56e739401ee3cc52f35deab4
Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>
2020-07-14 15:30:42 +0200

    tdf#133788 Missing pieces for Gallery localization
    Change-Id: Id11227b4a271351b50c43f6b5a531b47261c6fc5
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/98544
    Tested-by: Jenkins
    Reviewed-by: Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>

--- Repository.mk
+++ Repository.mk
@@ -878,6 +878,7 @@ $(eval $(call gb_Helper_register_packages_for_install,ooo,\
 	extras_gallmytheme \
 	extras_gallroot \
 	extras_gallsystem \
+	extras_gallsystemstr \
 	extras_glade \
 	extras_labels \
 	$(if $(filter WNT,$(OS)),extras_newfiles) \
