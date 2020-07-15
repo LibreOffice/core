f2937e7edb28b29a143a27cffd05f852a36a2869
Jan-Marek Glogowski <glogow@fbihome.de>
2020-07-15 03:50:44 +0200

    postgresql: drop special internal handling
    I don't see a reason, why all this shouldn't build correctly with
    all the setup from RepositoryExternal.mk.
    
    Change-Id: Id3edd609e4687e324c6ec92ddc0481d9345cdec6

--- external/postgresql/ExternalProject_postgresql.mk
+++ external/postgresql/ExternalProject_postgresql.mk
@@ -10,8 +10,8 @@
 $(eval $(call gb_ExternalProject_ExternalProject,postgresql))
 
 $(eval $(call gb_ExternalProject_use_externals,postgresql,\
-	$(if $(ENABLE_LDAP),openldap) \
-	openssl \
+	$(if $(ENABLE_LDAP),openldap nss3) \
+	$(if $(DISABLE_OPENSSL),,openssl) \
 	zlib \
 ))
 
@@ -32,32 +32,6 @@ $(call gb_ExternalProject_get_state_target,postgresql,build) :
 
 else
 
-postgresql_CPPFLAGS := $(ZLIB_CFLAGS)
-postgresql_LDFLAGS  := $(LDFLAGS)
-
-ifeq ($(SYSTEM_ZLIB),)
-postgresql_LDFLAGS += $(ZLIB_LIBS)
-endif
-
-ifeq ($(DISABLE_OPENSSL),)
-ifeq ($(SYSTEM_OPENSSL),)
-postgresql_CPPFLAGS += -I$(call gb_UnpackedTarball_get_dir,openssl)/include
-postgresql_LDFLAGS  += -L$(call gb_UnpackedTarball_get_dir,openssl)/
-endif
-endif
-
-ifeq ($(SYSTEM_OPENLDAP),)
-postgresql_CPPFLAGS += -I$(call gb_UnpackedTarball_get_dir,openldap)/include
-postgresql_LDFLAGS  += \
-	-L$(call gb_UnpackedTarball_get_dir,openldap)/libraries/libldap_r/.libs \
-	-L$(call gb_UnpackedTarball_get_dir,openldap)/libraries/libldap/.libs \
-	-L$(call gb_UnpackedTarball_get_dir,openldap)/libraries/liblber/.libs \
-	$(if $(SYSTEM_NSS),,\
-		-L$(call gb_UnpackedTarball_get_dir,nss)/dist/out/lib) \
-
-endif
-
-
 $(call gb_ExternalProject_get_state_target,postgresql,build) :
 	$(call gb_Trace_StartRange,postgresql,EXTERNAL)
 	$(call gb_ExternalProject_run,build,\
@@ -68,9 +42,6 @@ $(call gb_ExternalProject_get_state_target,postgresql,build) :
 				$(if $(WITH_KRB5), --with-krb5) \
 				$(if $(WITH_GSSAPI),--with-gssapi)) \
 				$(if $(ENABLE_LDAP),,--with-ldap=no) \
-			CPPFLAGS="$(postgresql_CPPFLAGS)" \
-			LDFLAGS="$(postgresql_LDFLAGS)" \
-			$(if $(ENABLE_LDAP),EXTRA_LDAP_LIBS="-llber -lssl3 -lsmime3 -lnss3 -lnssutil3 -lplds4 -lplc4 -lnspr4") \
 		&& cd src/interfaces/libpq \
 		&& MAKEFLAGS= && $(MAKE) all-static-lib)
 	$(call gb_Trace_EndRange,postgresql,EXTERNAL)
