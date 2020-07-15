5eac13a6d8ccad079e84382422fb2e615408aaaf
Luboš Luňák <l.lunak@collabora.com>
2020-07-15 12:16:47 +0200

    flush all Skia drawing before drawing to the screen
    Otherwise xor drawing might not get applied.
    
    Change-Id: Ic1f9496c76c8f1162951e2ac4acbcd131c714a3b

--- vcl/skia/gdiimpl.cxx
+++ vcl/skia/gdiimpl.cxx
@@ -1080,7 +1080,7 @@ Color SkiaSalGraphicsImpl::getPixel(long nX, long nY)
     SkiaZone zone;
     checkSurface();
     SAL_INFO("vcl.skia.trace", "getpixel(" << this << "): " << Point(nX, nY));
-    mSurface->getCanvas()->flush();
+    flushDrawing();
     // This is presumably slow, but getPixel() should be generally used only by unit tests.
     SkBitmap bitmap;
     if (!bitmap.tryAllocN32Pixels(GetWidth(), GetHeight()))
