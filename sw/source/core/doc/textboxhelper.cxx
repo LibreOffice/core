c937067568ebc54c9d1cbcf9a6550874eede0ca4
Bakos Attila <bakos.attilakaroly@nisz.hu>
2020-07-16 16:56:33 +0200

    Writer Editing: Fix position sync of textboxes
    This commit fixes the following commits:
    -tdf#130805 SwTextBoxHelper::create:
     fix frame position in shape
     (2479ae3ee20fc5f3cbb0c88eb09110a36e86710c)
    -tdf#130802 SwTextBoxHelper::syncFlyFrameAttr:
     fix dragging
     (c1c93987acbb83d8352656d77ee515e98c63d46b)
    
    Also, it fixes copying, anchor dragging, page
    anchored textboxes, and aligning as well.
    ( tdf#124430 tdf#122887 tdf#84691 tdf#107225 )
    
    Change-Id: I31abf364549778a4bbd3c4effc84df7d8b59d9e0

--- sw/source/core/doc/textboxhelper.cxx
+++ sw/source/core/doc/textboxhelper.cxx
@@ -141,6 +141,24 @@ void SwTextBoxHelper::create(SwFrameFormat* pShape)
     text::WritingMode eMode;
     if (xShapePropertySet->getPropertyValue(UNO_NAME_TEXT_WRITINGMODE) >>= eMode)
         syncProperty(pShape, RES_FRAMEDIR, 0, uno::makeAny(sal_Int16(eMode)));
+
+    const SwFormatAnchor& rAnch = pShape->GetAnchor();
+    if ((rAnch.GetAnchorId() == RndStdIds::FLY_AT_PAGE && rAnch.GetPageNum() != 0)
+        || ((rAnch.GetAnchorId() == RndStdIds::FLY_AT_PARA
+            || rAnch.GetAnchorId() == RndStdIds::FLY_AT_CHAR)
+               && rAnch.GetContentAnchor()))
+    {
+        SfxItemSet aTxFrmSet(pFormat->GetDoc()->GetAttrPool(), aFrameFormatSetRange);
+        aTxFrmSet.Put(pShape->GetAnchor());
+        SwFormatVertOrient aVOri(pFormat->GetVertOrient());
+        SwFormatHoriOrient aHOri(pFormat->GetHoriOrient());
+        aVOri.SetRelationOrient(pShape->GetVertOrient().GetRelationOrient());
+        aHOri.SetRelationOrient(pShape->GetHoriOrient().GetRelationOrient());
+        aTxFrmSet.Put(aVOri);
+        aTxFrmSet.Put(aHOri);
+        if (aTxFrmSet.Count())
+            pFormat->SetFormatAttr(aTxFrmSet);
+    }
 }
 
 void SwTextBoxHelper::destroy(SwFrameFormat* pShape)
@@ -695,14 +713,14 @@ void SwTextBoxHelper::syncFlyFrameAttr(SwFrameFormat& rShape, SfxItemSet const& 
 
         SfxItemIter aIter(rSet);
         const SfxPoolItem* pItem = aIter.GetCurItem();
+
+        const RndStdIds aAnchId = rShape.GetAnchor().GetAnchorId();
+        if ((aAnchId == RndStdIds::FLY_AT_PAGE && rShape.GetAnchor().GetPageNum() != 0)
+            || ((aAnchId == RndStdIds::FLY_AT_PARA || aAnchId == RndStdIds::FLY_AT_CHAR)
+                && rShape.GetAnchor().GetContentAnchor()))
+                aTextBoxSet.Put(rShape.GetAnchor());
         do
         {
-            if (rShape.GetAnchor().GetAnchorId() != RndStdIds::FLY_AS_CHAR)
-            {
-                const SwFormatAnchor& rShapeAnch = rShape.GetAnchor();
-                aTextBoxSet.Put(rShapeAnch);
-            }
-
             switch (pItem->Which())
             {
                 case RES_VERT_ORIENT:
@@ -714,7 +732,8 @@ void SwTextBoxHelper::syncFlyFrameAttr(SwFrameFormat& rShape, SfxItemSet const& 
                     if (!aRect.IsEmpty())
                         aOrient.SetPos(aOrient.GetPos() + aRect.getY());
 
-                    if (rShape.GetAnchor().GetAnchorId() == RndStdIds::FLY_AT_PAGE)
+                    if (rShape.GetAnchor().GetAnchorId() == RndStdIds::FLY_AT_PAGE
+                        && rShape.GetAnchor().GetPageNum() != 0)
                     {
                         aOrient.SetRelationOrient(rShape.GetVertOrient().GetRelationOrient());
                     }
@@ -738,7 +757,8 @@ void SwTextBoxHelper::syncFlyFrameAttr(SwFrameFormat& rShape, SfxItemSet const& 
                     if (!aRect.IsEmpty())
                         aOrient.SetPos(aOrient.GetPos() + aRect.getX());
 
-                    if (rShape.GetAnchor().GetAnchorId() == RndStdIds::FLY_AT_PAGE)
+                    if (rShape.GetAnchor().GetAnchorId() == RndStdIds::FLY_AT_PAGE
+                        && rShape.GetAnchor().GetPageNum() != 0)
                     {
                         aOrient.SetRelationOrient(rShape.GetHoriOrient().GetRelationOrient());
                     }
@@ -780,7 +800,8 @@ void SwTextBoxHelper::syncFlyFrameAttr(SwFrameFormat& rShape, SfxItemSet const& 
         } while (pItem && (0 != pItem->Which()));
 
         if (aTextBoxSet.Count())
-            pFormat->GetDoc()->SetFlyFrameAttr(*pFormat, aTextBoxSet);
+            //pFormat->GetDoc()->SetFlyFrameAttr(*pFormat, aTextBoxSet);
+            pFormat->SetFormatAttr(aTextBoxSet);
     }
 }
 
