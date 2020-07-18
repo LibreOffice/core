7dbd90a53655572d1103ce6173ea9d866bb6d374
Shivam Kumar Singh <shivamhere247@gmail.com>
2020-07-18 11:51:33 +0530

    tdf#134564 tdf#134578 Improve hierarchy viewing in Inspector
    Significance of this patch -
    
    * Added TreeView Lines in the ui file for Inspector
      for better hierarchy viewing
    * Properties and there respective values are divided
      into separate columns.
    * Grey out disabled properties
    * bold PS, CS and DF
    
    Change-Id: I825a75dd2c5bc54715fe59f3c639024a5e3ab5cb

--- sw/source/uibase/sidebar/WriterInspectorTextPanel.cxx
+++ sw/source/uibase/sidebar/WriterInspectorTextPanel.cxx
@@ -25,14 +25,12 @@
 #include <wrtsh.hxx>
 #include <com/sun/star/text/XTextRange.hpp>
 #include <com/sun/star/text/XTextCursor.hpp>
-#include <com/sun/star/awt/FontSlant.hpp>
 #include <com/sun/star/beans/XPropertySet.hpp>
 #include <com/sun/star/beans/XPropertyState.hpp>
 #include <com/sun/star/style/XStyleFamiliesSupplier.hpp>
 #include <com/sun/star/lang/IllegalArgumentException.hpp>
 
 #include <unotextrange.hxx>
-#include <svl/languageoptions.hxx>
 
 namespace sw::sidebar
 {
@@ -59,54 +57,6 @@ WriterInspectorTextPanel::WriterInspectorTextPanel(vcl::Window* pParent,
         pShell->SetChgLnk(LINK(this, WriterInspectorTextPanel, AttrChangedNotify));
 }
 
-static bool GetPropertyValues(const beans::Property rProperty, const uno::Any& rAny,
-                              OUString& rString)
-{
-    // Hide Asian and Complex properties
-    if (SvtLanguageOptions().IsCJKFontEnabled() && rProperty.Name.indexOf("Asian") != -1)
-        return false;
-    if (SvtLanguageOptions().IsCTLFontEnabled() && rProperty.Name.indexOf("Complex") != -1)
-        return false;
-
-    rString = rProperty.Name + "     ";
-
-    if (bool bValue; rAny >>= bValue)
-    {
-        rString += OUString::boolean(bValue);
-    }
-    else if (OUString aValue; (rAny >>= aValue) && !(aValue.isEmpty()))
-    {
-        rString += aValue;
-    }
-    else if (awt::FontSlant eValue; rAny >>= eValue)
-    {
-        rString += (eValue == awt::FontSlant_ITALIC) ? OUStringLiteral("italic")
-                                                     : OUStringLiteral("normal");
-    }
-    else if (long nValueLong; rAny >>= nValueLong)
-    {
-        if (rString.indexOf("Color") != -1)
-            rString += "0x" + OUString::number(nValueLong, 16);
-        else
-            rString += OUString::number(nValueLong);
-    }
-    else if (double fValue; rAny >>= fValue)
-    {
-        if (rString.indexOf("Weight") != -1)
-            rString += (fValue > 100) ? OUStringLiteral("bold") : OUStringLiteral("normal");
-        else
-            rString += OUString::number((round(fValue * 100)) / 100.00);
-    }
-    else if (short nValueShort; rAny >>= nValueShort)
-    {
-        rString += OUString::number(nValueShort);
-    }
-    else
-        return false;
-
-    return true;
-}
-
 static void UpdateTree(SwDocShell* pDocSh, std::vector<svx::sidebar::TreeNode>& aStore)
 {
     SwDoc* pDoc = pDocSh->GetDoc();
@@ -118,6 +68,9 @@ static void UpdateTree(SwDocShell* pDocSh, std::vector<svx::sidebar::TreeNode>& 
     aDFNode.sNodeName = "Direct Formatting";
     aCharNode.sNodeName = "Character Styles";
     aParaNode.sNodeName = "Paragraph Styles";
+    aDFNode.NodeType = svx::sidebar::TreeNode::Category;
+    aCharNode.NodeType = svx::sidebar::TreeNode::Category;
+    aParaNode.NodeType = svx::sidebar::TreeNode::Category;
 
     uno::Reference<text::XTextRange> xRange(
         SwXTextRange::CreateXTextRange(*pDoc, *pCursor->GetPoint(), nullptr));
@@ -133,15 +86,12 @@ static void UpdateTree(SwDocShell* pDocSh, std::vector<svx::sidebar::TreeNode>& 
     {
         if (xPropertiesState->getPropertyState(rProperty.Name) == beans::PropertyState_DIRECT_VALUE)
         {
-            OUString aPropertyValuePair;
             const uno::Any aAny = xPropertiesSet->getPropertyValue(rProperty.Name);
-            if (GetPropertyValues(rProperty, aAny, aPropertyValuePair))
-            {
-                aIsDefined[rProperty.Name] = true;
-                svx::sidebar::TreeNode aTemp;
-                aTemp.sNodeName = aPropertyValuePair;
-                aDFNode.children.push_back(aTemp);
-            }
+            aIsDefined[rProperty.Name] = true;
+            svx::sidebar::TreeNode aTemp;
+            aTemp.sNodeName = rProperty.Name;
+            aTemp.aValue = aAny;
+            aDFNode.children.push_back(aTemp);
         }
     }
 
@@ -164,24 +114,21 @@ static void UpdateTree(SwDocShell* pDocSh, std::vector<svx::sidebar::TreeNode>& 
         aProperties = xPropertiesSet->getPropertySetInfo()->getProperties();
         svx::sidebar::TreeNode aCurrentChild;
         aCurrentChild.sNodeName = sDisplayName;
+        aCurrentChild.NodeType = svx::sidebar::TreeNode::ComplexProperty;
 
         for (const beans::Property& rProperty : std::as_const(aProperties))
         {
             OUString sPropName = rProperty.Name;
             if (xPropertiesState->getPropertyState(sPropName) == beans::PropertyState_DIRECT_VALUE)
             {
-                OUString aPropertyValuePair;
                 const uno::Any aAny = xPropertiesSet->getPropertyValue(sPropName);
-                if (GetPropertyValues(rProperty, aAny, aPropertyValuePair))
-                {
-                    if (aIsDefined[sPropName]) // Already defined in "Direct Formatting" ?
-                        aPropertyValuePair = aPropertyValuePair + "  !!<GREY>!!";
-                    else
-                        aIsDefined[sPropName] = true;
-                    svx::sidebar::TreeNode aTemp;
-                    aTemp.sNodeName = aPropertyValuePair;
-                    aCurrentChild.children.push_back(aTemp);
-                }
+                svx::sidebar::TreeNode aTemp;
+                if (aIsDefined[sPropName]) // Already defined in "Direct Formatting" ?
+                    aTemp.isGrey = true;
+                aIsDefined[sPropName] = true;
+                aTemp.sNodeName = sPropName;
+                aTemp.aValue = aAny;
+                aCurrentChild.children.push_back(aTemp);
             }
         }
 
@@ -201,6 +148,7 @@ static void UpdateTree(SwDocShell* pDocSh, std::vector<svx::sidebar::TreeNode>& 
         OUString aParentParaStyle = xPropertiesStyle->getParentStyle();
         svx::sidebar::TreeNode aCurrentChild;
         aCurrentChild.sNodeName = sDisplayName;
+        aCurrentChild.NodeType = svx::sidebar::TreeNode::ComplexProperty;
 
         for (const beans::Property& rProperty : std::as_const(aProperties))
         {
@@ -209,19 +157,15 @@ static void UpdateTree(SwDocShell* pDocSh, std::vector<svx::sidebar::TreeNode>& 
                 || xPropertiesState->getPropertyState(sPropName)
                        == beans::PropertyState_DIRECT_VALUE)
             {
-                OUString aPropertyValuePair;
                 const uno::Any aAny = xPropertiesSet->getPropertyValue(sPropName);
-                if (GetPropertyValues(rProperty, aAny, aPropertyValuePair))
-                {
-                    // Already defined in "Default Formatting" or "Character Styles" or any child Paragraph Style ?
-                    if (aIsDefined[sPropName])
-                        aPropertyValuePair = aPropertyValuePair + "    !!<GREY>!! ";
-                    else
-                        aIsDefined[sPropName] = true;
-                    svx::sidebar::TreeNode aTemp;
-                    aTemp.sNodeName = aPropertyValuePair;
-                    aCurrentChild.children.push_back(aTemp);
-                }
+                svx::sidebar::TreeNode aTemp;
+                // Already defined in "Default Formatting" or "Character Styles" or any child Paragraph Style ?
+                if (aIsDefined[sPropName])
+                    aTemp.isGrey = true;
+                aIsDefined[sPropName] = true;
+                aTemp.sNodeName = sPropName;
+                aTemp.aValue = aAny;
+                aCurrentChild.children.push_back(aTemp);
             }
         }
 
