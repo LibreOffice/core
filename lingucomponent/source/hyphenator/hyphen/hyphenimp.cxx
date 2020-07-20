LibreOffice/core

commit 3fbadfa1ad41a3477804c592e06caec708c05218

lingucomponent: create instances with uno constructors
See tdf#74608 for motivation.

Change-Id: I11770c8873d14cfd0e438fb580d8d0ea9e99f594
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/98709
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

lingucomponent/source/hyphenator/hyphen/hyphenimp.cxx
@@ -692,14 +692,6 @@ OUString Hyphenator::makeInitCap(const OUString& aTerm, CharClass const * pCC)
     return aTerm;
 }
 
-/// @throws Exception
-static Reference< XInterface > Hyphenator_CreateInstance(
-        const Reference< XMultiServiceFactory > & /*rSMgr*/ )
-{
-    Reference< XInterface > xService = static_cast<cppu::OWeakObject*>(new Hyphenator);
-    return xService;
-}
-
 sal_Bool SAL_CALL Hyphenator::addLinguServiceEventListener(
         const Reference< XLinguServiceEventListener >& rxLstnr )
 {
@@ -794,7 +786,7 @@ void SAL_CALL Hyphenator::removeEventListener( const Reference< XEventListener >
 // Service specific part
 OUString SAL_CALL Hyphenator::getImplementationName()
 {
-    return getImplementationName_Static();
+    return "org.openoffice.lingu.LibHnjHyphenator";
 }
 
 sal_Bool SAL_CALL Hyphenator::supportsService( const OUString& ServiceName )
@@ -804,37 +796,15 @@ sal_Bool SAL_CALL Hyphenator::supportsService( const OUString& ServiceName )
 
 Sequence< OUString > SAL_CALL Hyphenator::getSupportedServiceNames()
 {
-    return getSupportedServiceNames_Static();
+    return { SN_HYPHENATOR };
 }
 
-Sequence< OUString > Hyphenator::getSupportedServiceNames_Static()
-        throw()
+extern "C" SAL_DLLPUBLIC_EXPORT css::uno::XInterface*
+lingucomponent_Hyphenator_get_implementation(
+    css::uno::XComponentContext* , css::uno::Sequence<css::uno::Any> const&)
 {
-    Sequence< OUString > aSNS { SN_HYPHENATOR };
-    return aSNS;
+    return cppu::acquire(new Hyphenator());
 }
 
-extern "C"
-{
 
-SAL_DLLPUBLIC_EXPORT void * hyphen_component_getFactory(
-    const char * pImplName, void * pServiceManager, void * /*pRegistryKey*/ )
-{
-    void * pRet = nullptr;
-    if ( Hyphenator::getImplementationName_Static().equalsAscii( pImplName ) )
-    {
-        Reference< XSingleServiceFactory > xFactory =
-            cppu::createOneInstanceFactory(
-                static_cast< XMultiServiceFactory * >( pServiceManager ),
-                Hyphenator::getImplementationName_Static(),
-                Hyphenator_CreateInstance,
-                Hyphenator::getSupportedServiceNames_Static());
-        // acquire, because we return an interface pointer instead of a reference
-        xFactory->acquire();
-        pRet = xFactory.get();
-    }
-    return pRet;
-}
-
-}
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

lingucomponent/source/hyphenator/hyphen/hyphen.component
@@ -18,8 +18,9 @@
  -->
 
 <component loader="com.sun.star.loader.SharedLibrary" environment="@CPPU_ENV@"
-    prefix="hyphen" xmlns="http://openoffice.org/2010/uno-components">
-  <implementation name="org.openoffice.lingu.LibHnjHyphenator">
+    xmlns="http://openoffice.org/2010/uno-components">
+  <implementation name="org.openoffice.lingu.LibHnjHyphenator"
+    constructor="lingucomponent_Hyphenator_get_implementation">
     <service name="com.sun.star.linguistic2.Hyphenator"/>
   </implementation>
 </component>

lingucomponent/source/hyphenator/hyphen/hyphenimp.hxx
@@ -114,19 +114,11 @@ public:
     virtual sal_Bool SAL_CALL supportsService( const OUString& rServiceName ) override;
     virtual Sequence< OUString > SAL_CALL getSupportedServiceNames() override;
 
-    static inline OUString  getImplementationName_Static() throw();
-    static Sequence< OUString > getSupportedServiceNames_Static() throw();
-
 private:
         static OUString makeLowerCase(const OUString&, CharClass const *);
         static OUString makeUpperCase(const OUString&, CharClass const *);
         static OUString makeInitCap(const OUString&, CharClass const *);
 };
-
-inline OUString Hyphenator::getImplementationName_Static() throw()
-{
-    return "org.openoffice.lingu.LibHnjHyphenator";
-}
 
 #endif
 

lingucomponent/source/languageguessing/guesslang.component
@@ -18,8 +18,9 @@
  -->
 
 <component loader="com.sun.star.loader.SharedLibrary" environment="@CPPU_ENV@"
-    prefix="guesslang" xmlns="http://openoffice.org/2010/uno-components">
-  <implementation name="com.sun.star.lingu2.LanguageGuessing">
+    xmlns="http://openoffice.org/2010/uno-components">
+  <implementation name="com.sun.star.lingu2.LanguageGuessing"
+    constructor="lingucomponent_LangGuess_get_implementation">
     <service name="com.sun.star.linguistic2.LanguageGuessing"/>
   </implementation>
 </component>

lingucomponent/source/languageguessing/guesslang.cxx
@@ -52,20 +52,6 @@ using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star::lang;
 using namespace ::com::sun::star::linguistic2;
 
-#define SERVICENAME     "com.sun.star.linguistic2.LanguageGuessing"
-#define IMPLNAME        "com.sun.star.lingu2.LanguageGuessing"
-
-static Sequence< OUString > getSupportedServiceNames_LangGuess_Impl()
-{
-    Sequence<OUString> names { SERVICENAME };
-    return names;
-}
-
-static OUString getImplementationName_LangGuess_Impl()
-{
-    return IMPLNAME;
-}
-
 static osl::Mutex &  GetLangGuessMutex()
 {
     static osl::Mutex aMutex;
@@ -309,7 +295,7 @@ void SAL_CALL LangGuess_Impl::enableLanguages(
 
 OUString SAL_CALL LangGuess_Impl::getImplementationName(  )
 {
-    return IMPLNAME;
+    return "com.sun.star.lingu2.LanguageGuessing";
 }
 
 sal_Bool SAL_CALL LangGuess_Impl::supportsService( const OUString& ServiceName )
@@ -319,42 +305,15 @@ sal_Bool SAL_CALL LangGuess_Impl::supportsService( const OUString& ServiceName )
 
 Sequence<OUString> SAL_CALL LangGuess_Impl::getSupportedServiceNames(  )
 {
-    return { SERVICENAME };
+    return { "com.sun.star.linguistic2.LanguageGuessing" };
 }
 
-/**
- * Function to create a new component instance; is needed by factory helper implementation.
- * @param xMgr service manager to if the components needs other component instances
- */
-static Reference< XInterface > LangGuess_Impl_create(
-    Reference< XComponentContext > const & )
+extern "C" SAL_DLLPUBLIC_EXPORT css::uno::XInterface*
+lingucomponent_LangGuess_get_implementation(
+    css::uno::XComponentContext* , css::uno::Sequence<css::uno::Any> const&)
 {
-    return static_cast< ::cppu::OWeakObject * >( new LangGuess_Impl );
+    return cppu::acquire(new LangGuess_Impl());
 }
 
-//#### EXPORTED ### functions to allow for registration and creation of the UNO component
-const struct ::cppu::ImplementationEntry s_component_entries [] =
-{
-    {
-        LangGuess_Impl_create, getImplementationName_LangGuess_Impl,
-        getSupportedServiceNames_LangGuess_Impl,
-        ::cppu::createSingleComponentFactory,
-        nullptr, 0
-    },
-    { nullptr, nullptr, nullptr, nullptr, nullptr, 0 }
-};
-
-extern "C"
-{
-
-SAL_DLLPUBLIC_EXPORT void * guesslang_component_getFactory(
-    char const * implName, void * xMgr,
-    void * xRegistry )
-{
-    return ::cppu::component_getFactoryHelper(
-        implName, xMgr, xRegistry, s_component_entries );
-}
-
-}
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

lingucomponent/source/numbertext/numbertext.component
@@ -18,8 +18,9 @@
  -->
 
 <component loader="com.sun.star.loader.SharedLibrary" environment="@CPPU_ENV@"
-    prefix="numbertext" xmlns="http://openoffice.org/2010/uno-components">
-  <implementation name="com.sun.star.lingu2.NumberText">
+    xmlns="http://openoffice.org/2010/uno-components">
+  <implementation name="com.sun.star.lingu2.NumberText"
+    constructor="lingucomponent_NumberText_get_implementation">
     <service name="com.sun.star.linguistic2.NumberText"/>
   </implementation>
 </component>

lingucomponent/source/numbertext/numbertext.cxx
@@ -47,17 +47,6 @@ using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star::lang;
 using namespace ::com::sun::star::linguistic2;
 
-#define SERVICENAME "com.sun.star.linguistic2.NumberText"
-#define IMPLNAME "com.sun.star.lingu2.NumberText"
-
-static Sequence<OUString> getSupportedServiceNames_NumberText_Impl()
-{
-    Sequence<OUString> names{ SERVICENAME };
-    return names;
-}
-
-static OUString getImplementationName_NumberText_Impl() { return IMPLNAME; }
-
 static osl::Mutex& GetNumberTextMutex()
 {
     static osl::Mutex aMutex;
@@ -156,38 +145,26 @@ uno::Sequence<Locale> SAL_CALL NumberText_Impl::getAvailableLanguages()
     return aRes;
 }
 
-OUString SAL_CALL NumberText_Impl::getImplementationName() { return IMPLNAME; }
+OUString SAL_CALL NumberText_Impl::getImplementationName()
+{
+    return "com.sun.star.lingu2.NumberText";
+}
 
 sal_Bool SAL_CALL NumberText_Impl::supportsService(const OUString& ServiceName)
 {
     return cppu::supportsService(this, ServiceName);
 }
 
-Sequence<OUString> SAL_CALL NumberText_Impl::getSupportedServiceNames() { return { SERVICENAME }; }
-
-/**
- * Function to create a new component instance; is needed by factory helper implementation.
- * @param xMgr service manager to if the components needs other component instances
- */
-static Reference<XInterface> NumberText_Impl_create(Reference<XComponentContext> const&)
+Sequence<OUString> SAL_CALL NumberText_Impl::getSupportedServiceNames()
 {
-    return static_cast<::cppu::OWeakObject*>(new NumberText_Impl);
+    return { "com.sun.star.linguistic2.NumberText" };
 }
 
-//#### EXPORTED ### functions to allow for registration and creation of the UNO component
-const struct ::cppu::ImplementationEntry s_component_entries[]
-    = { { NumberText_Impl_create, getImplementationName_NumberText_Impl,
-          getSupportedServiceNames_NumberText_Impl, ::cppu::createSingleComponentFactory, nullptr,
-          0 },
-        { nullptr, nullptr, nullptr, nullptr, nullptr, 0 } };
-
-extern "C" {
-
-SAL_DLLPUBLIC_EXPORT void* numbertext_component_getFactory(char const* implName, void* xMgr,
-                                                           void* xRegistry)
+extern "C" SAL_DLLPUBLIC_EXPORT css::uno::XInterface*
+lingucomponent_NumberText_get_implementation(css::uno::XComponentContext*,
+                                             css::uno::Sequence<css::uno::Any> const&)
 {
-    return ::cppu::component_getFactoryHelper(implName, xMgr, xRegistry, s_component_entries);
-}
+    return cppu::acquire(new NumberText_Impl());
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

lingucomponent/source/spellcheck/spell/spell.component
@@ -18,8 +18,9 @@
  -->
 
 <component loader="com.sun.star.loader.SharedLibrary" environment="@CPPU_ENV@"
-    prefix="spell" xmlns="http://openoffice.org/2010/uno-components">
-  <implementation name="org.openoffice.lingu.MySpellSpellChecker">
+    xmlns="http://openoffice.org/2010/uno-components">
+  <implementation name="org.openoffice.lingu.MySpellSpellChecker"
+    constructor="lingucomponent_SpellChecker_get_implementation">
     <service name="com.sun.star.linguistic2.SpellChecker"/>
   </implementation>
 </component>

commit d9b37d55e5e187c8ea56da90a37293243c16ddcf

this should be a single-instance component
regression from
    commit 3fbadfa1ad41a3477804c592e06caec708c05218
    lingucomponent: create instances with uno constructors

Change-Id: Ie53c56a88dc1c15178219b0098803586164837da

lingucomponent/source/hyphenator/hyphen/hyphenimp.cxx
@@ -803,7 +803,9 @@ extern "C" SAL_DLLPUBLIC_EXPORT css::uno::XInterface*
 lingucomponent_Hyphenator_get_implementation(
     css::uno::XComponentContext* , css::uno::Sequence<css::uno::Any> const&)
 {
-    return cppu::acquire(new Hyphenator());
+    static rtl::Reference<Hyphenator> g_Instance(new Hyphenator());
+    g_Instance->acquire();
+    return static_cast<cppu::OWeakObject*>(g_Instance.get());
 }
 
 