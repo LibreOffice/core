LibreOffice/core

commit 9cca15204af9cc44a8a9528ccf2f36616fb70e69

tdf#133052: DOCX import: fix top auto margin of subitems
(nested lists) and other items of lists, when auto
margin is defined by paragraph style, and list items
have got w:numPr.

Follow-up of commit 162d74ae7a53eb1cde738f0a7558f297b8162f7a
(tdf#132807 DOCX import: fix top auto margin in lists).

Change-Id: I4cf470173fa367ac07e15dc901b0e202178c9fc4
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/94588
Tested-by: Jenkins
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -1656,18 +1656,12 @@ void DomainMapper_Impl::finishParagraph( const PropertyMapPtr& pPropertyMap, con
                             uno::Sequence<beans::PropertyValue> aPrevPropertiesSeq;
                             m_xPreviousParagraph->getPropertyValue("ParaInteropGrabBag") >>= aPrevPropertiesSeq;
                             auto aPrevProperties = comphelper::sequenceToContainer< std::vector<beans::PropertyValue> >(aPrevPropertiesSeq);
-                            bool bPrevParaAutoBefore;
-                            if (isNumberingViaRule)
-                                bPrevParaAutoBefore = false;
-                            else
+                            bool bParaAutoBefore = m_bParaAutoBefore || std::any_of(aPrevProperties.begin(), aPrevProperties.end(), [](const beans::PropertyValue& rValue)
                             {
-                                bPrevParaAutoBefore = std::any_of(aPrevProperties.begin(), aPrevProperties.end(), [](const beans::PropertyValue& rValue)
-                                {
                                     return rValue.Name == "ParaTopMarginBeforeAutoSpacing";
-                                });
-                            }
+                            });
                             // There was a previous textnode and it had the same numbering.
-                            if (m_bParaAutoBefore || bPrevParaAutoBefore)
+                            if (bParaAutoBefore)
                             {
                                 // This before spacing is set to auto, set before space to 0.
                                 auto itParaTopMargin = std::find_if(aProperties.begin(), aProperties.end(), [](const beans::PropertyValue& rValue)

sw/qa/extras/ooxmlexport/ooxmlexport9.cxx
@@ -223,6 +223,19 @@ DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testTdf132807, "tdf132807.docx")
     assertXPath(pXmlDoc, "/w:document/w:body/w:p[5]/w:pPr/w:spacing", "before", "280");
 }
 
+DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testTdf133052, "tdf133052.docx")
+{
+    xmlDocUniquePtr pXmlDoc = parseExport("word/document.xml");
+    // These were 240 (top auto spacing of list subitems are zero)
+    assertXPath(pXmlDoc, "/w:document/w:body/w:p[4]/w:pPr/w:spacing", "before", "0");
+    assertXPath(pXmlDoc, "/w:document/w:body/w:p[5]/w:pPr/w:spacing", "before", "0");
+    // in tables, too
+    assertXPath(pXmlDoc, "/w:document/w:body/w:tbl/w:tr[1]/w:tc/w:p[2]/w:pPr/w:spacing", "before", "0");
+    assertXPath(pXmlDoc, "/w:document/w:body/w:tbl/w:tr[1]/w:tc/w:p[3]/w:pPr/w:spacing", "before", "0");
+    assertXPath(pXmlDoc, "/w:document/w:body/w:tbl/w:tr[1]/w:tc/w:p[4]/w:pPr/w:spacing", "before", "0");
+    assertXPath(pXmlDoc, "/w:document/w:body/w:tbl/w:tr[1]/w:tc/w:p[5]/w:pPr/w:spacing", "before", "0");
+}
+
 DECLARE_OOXMLEXPORT_TEST(testTdf129575_directBefore, "tdf129575-directBefore.docx")
 {
     uno::Reference<text::XTextTablesSupplier> xTablesSupplier(mxComponent, uno::UNO_QUERY);

commit 717ecd6640f35f1e1598c8647af674a07914407c

tdf#134648 DOCX import: fix bottom auto margin of subitem
If style based spacing was set to auto in the previous
list item, style of the actual paragraph must be the same
to guarantee the same auto setting.

Regression from commit 9cca15204af9cc44a8a9528ccf2f36616fb70e69
(tdf#133052: DOCX import: fix top auto margin of subitems).

Change-Id: I39d2bad03a659a407b52550768b010cfa716d363

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -1714,6 +1714,16 @@ void DomainMapper_Impl::finishParagraph( const PropertyMapPtr& pPropertyMap, con
                             {
                                     return rValue.Name == "ParaTopMarginBeforeAutoSpacing";
                             });
+                            // if style based spacing was set to auto in the previous paragraph, style of the actual paragraph must be the same
+                            if (bParaAutoBefore && !m_bParaAutoBefore && m_xPreviousParagraph->getPropertySetInfo()->hasPropertyByName("ParaStyleName"))
+                            {
+                               auto itParaStyle = std::find_if(aProperties.begin(), aProperties.end(), [](const beans::PropertyValue& rValue)
+                               {
+                                   return rValue.Name == "ParaStyleName";
+                               });
+                               bParaAutoBefore = itParaStyle != aProperties.end() &&
+                                   m_xPreviousParagraph->getPropertyValue("ParaStyleName") == itParaStyle->Value;
+                            }
                             // There was a previous textnode and it had the same numbering.
                             if (bParaAutoBefore)
                             {

sw/qa/extras/ooxmlexport/ooxmlexport9.cxx
@@ -236,6 +236,18 @@ DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testTdf133052, "tdf133052.docx")
     assertXPath(pXmlDoc, "/w:document/w:body/w:tbl/w:tr[1]/w:tc/w:p[5]/w:pPr/w:spacing", "before", "0");
 }
 
+DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testTdf134648, "tdf134648.docx")
+{
+    xmlDocUniquePtr pXmlDoc = parseExport("word/document.xml");
+
+    // list item with direct top auto spacing
+    assertXPath(pXmlDoc, "/w:document/w:body/w:p[1]/w:pPr/w:spacing", "after", "240");
+    assertXPath(pXmlDoc, "/w:document/w:body/w:p[1]/w:pPr/w:spacing", "beforeAutospacing", "1");
+
+    // This was spacing w:after=200, but bottom auto spacing of first list subitem is zero
+    assertXPath(pXmlDoc, "/w:document/w:body/w:p[2]/w:pPr/w:spacing", 0);
+}
+
 DECLARE_OOXMLEXPORT_TEST(testTdf129575_directBefore, "tdf129575-directBefore.docx")
 {
     uno::Reference<text::XTextTablesSupplier> xTablesSupplier(mxComponent, uno::UNO_QUERY);