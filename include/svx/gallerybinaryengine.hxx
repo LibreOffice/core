14985743b1b211a816e60d36fcbc74a6d368c17b
Aditya <adityasahu1511@gmail.com>
2020-07-20 17:13:13 +0530

    svx: Implement GalleryObjectCollection shared class
    Add GalleryObjectCollection, a shared class primarily owned by GalleryTheme
     which contains object list to be used by GalleryTheme and GalleryBinaryEngine.
    Refactor ImplCreateUniqueURL, InsertGraphic()
    Implement comphelper::GraphicMimeTypeHelper::GetExtensionForConvertDataFormat()
    Refactor Actualize(), implement updateSvDrawObject(), updateTheme()
    
    Change-Id: I6a8c552268628b4e192f45112ac9e8789c6e1609

--- include/svx/gallerybinaryengine.hxx
+++ include/svx/gallerybinaryengine.hxx
@@ -20,19 +20,19 @@
 #pragma once
 
 #include <svx/galtheme.hxx>
-#include <svx/gallerybinaryengine.hxx>
-#include <svx/gallerystoragelocations.hxx>
+#include <svx/galmisc.hxx>
 #include <svx/svxdllapi.h>
 #include <svx/fmmodel.hxx>
+#include <svx/gallerystoragelocations.hxx>
 #include <tools/urlobj.hxx>
 #include <sot/storage.hxx>
 
-#include <cstdio>
 #include <memory>
-#include <vector>
 
 class GalleryStorageLocations;
+class GalleryObjectCollection;
 class SgaObjectSvDraw;
+class SgaObjectBmp;
 class SgaObject;
 class SotStorage;
 struct GalleryObject;
@@ -44,12 +44,17 @@ class SVXCORE_DLLPUBLIC GalleryBinaryEngine
 private:
     tools::SvRef<SotStorage> m_aSvDrawStorageRef;
     const GalleryStorageLocations& maGalleryStorageLocations;
+    GalleryObjectCollection& mrGalleryObjectCollection;
+    bool mbReadOnly;
 
 public:
-    GalleryBinaryEngine(const GalleryStorageLocations& rGalleryStorageLocations);
+    GalleryBinaryEngine(const GalleryStorageLocations& rGalleryStorageLocations,
+                        GalleryObjectCollection& rGalleryObjectCollection, bool bReadOnly);
+    SAL_DLLPRIVATE ~GalleryBinaryEngine();
+
     void clearSotStorage();
 
-    SAL_DLLPRIVATE void ImplCreateSvDrawStorage(bool bReadOnly);
+    SAL_DLLPRIVATE void ImplCreateSvDrawStorage();
     SAL_DLLPRIVATE const tools::SvRef<SotStorage>& GetSvDrawStorage() const;
 
     const INetURLObject& GetThmURL() const { return maGalleryStorageLocations.GetThmURL(); }
@@ -60,21 +65,29 @@ public:
     SAL_DLLPRIVATE bool implWrite(const GalleryTheme& rTheme);
 
     void insertObject(const SgaObject& rObj, GalleryObject* pFoundEntry, OUString& rDestDir,
-                      ::std::vector<std::unique_ptr<GalleryObject>>& rObjectList,
                       sal_uInt32& rInsertPos);
 
     std::unique_ptr<SgaObject> implReadSgaObject(GalleryObject const* pEntry);
     bool implWriteSgaObject(const SgaObject& rObj, sal_uInt32 nPos, GalleryObject* pExistentEntry,
-                            OUString& aDestDir,
-                            ::std::vector<std::unique_ptr<GalleryObject>>& aObjectList);
+                            OUString& aDestDir);
 
     bool readModel(const GalleryObject* pObject, SdrModel& rModel);
-    bool insertModel(const FmFormModel& rModel, INetURLObject& rURL);
+    SgaObjectSvDraw insertModel(const FmFormModel& rModel, const INetURLObject& rUserURL);
 
     bool readModelStream(const GalleryObject* pObject,
                          tools::SvRef<SotStorageStream> const& rxModelStream);
     SgaObjectSvDraw insertModelStream(const tools::SvRef<SotStorageStream>& rxModelStream,
-                                      INetURLObject& rURL);
+                                      const INetURLObject& rUserURL);
+
+    INetURLObject implCreateUniqueURL(SgaObjKind eObjKind, const INetURLObject& rUserURL,
+                                      ConvertDataFormat nFormat = ConvertDataFormat::Unknown);
+
+    SgaObjectBmp insertGraphic(const Graphic& rGraphic, const GfxLink& aGfxLink,
+                               ConvertDataFormat& nExportFormat, const INetURLObject& rUserURL);
+
+    SgaObjectSvDraw updateSvDrawObject(GalleryObject* pEntry);
+
+    void updateTheme();
 };
 
 SvStream& WriteGalleryTheme(SvStream& rOut, const GalleryTheme& rTheme);
