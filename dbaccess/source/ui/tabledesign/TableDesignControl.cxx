529cb199a460ffe5a819e52ec62ec1e0c9e37177
Caolán McNamara <caolanm@redhat.com>
2020-07-21 11:34:19 +0100

    weld PatternControl
    Change-Id: Ie84778c2f127a6b393edbfa08e61b135ec3bcbc0

--- dbaccess/source/ui/tabledesign/TableDesignControl.cxx
+++ dbaccess/source/ui/tabledesign/TableDesignControl.cxx
@@ -24,6 +24,7 @@
 #include <com/sun/star/beans/PropertyValue.hpp>
 #include <vcl/builder.hxx>
 #include <vcl/commandevent.hxx>
+#include <vcl/menu.hxx>
 #include <helpids.h>
 
 using namespace ::dbaui;
