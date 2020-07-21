9cb843e5c61ffd8dcbc7eebdf1c13e2a53299f2c
Xisco Fauli <xiscofauli@libreoffice.org>
2020-07-21 13:14:12 +0200

    fix comment
    Change-Id: Idc6f35fdffed9a466c574f20baba6ec75a797d1c

--- sw/qa/extras/ooxmlexport/ooxmlexport7.cxx
+++ sw/qa/extras/ooxmlexport/ooxmlexport7.cxx
@@ -159,7 +159,7 @@ DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testTdf104539, "tdf104539.odt")
 DECLARE_OOXMLEXPORT_TEST(testTdf129582, "tdf129582.docx")
 {
     CPPUNIT_ASSERT_EQUAL(2, getPages());
-    // Without the fix in place, the image on the second footer would be imported
+    // Without the fix in place, the image on the second footer wouldn't have been imported
     CPPUNIT_ASSERT_EQUAL(2, getShapes());
 }
 
