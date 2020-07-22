45b74c5571389cb2bd2c3836785598909ba8b94a
Xisco Fauli <xiscofauli@libreoffice.org>
2020-07-22 23:03:56 +0200

    tdf#124184: Improve coverage of vbUnicode and vbFromUnicode
    Change-Id: Id66ad718dc9057048f8060b02bb04c79c7dcc608

--- basic/qa/vba_tests/strconv.vb
+++ basic/qa/vba_tests/strconv.vb
@@ -21,21 +21,19 @@ Function verify_testStrConv() as String
 
     Dim testName As String
     Dim srcStr, retStr As String
+    Dim i As Integer
     Dim x() As Byte
     srcStr = "abc EFG hij"
     testName = "Test StrConv function"
     On Error GoTo errorHandler
 
     retStr = StrConv(srcStr, vbUpperCase)
-    'MsgBox retStr
     TestLog_ASSERT retStr = "ABC EFG HIJ", "Converts the string to uppercase characters:" & retStr
 
     retStr = StrConv(srcStr, vbLowerCase)
-    'MsgBox retStr
     TestLog_ASSERT retStr = "abc efg hij", "Converts the string to lowercase characters:" & retStr
 
     retStr = StrConv(srcStr, vbProperCase)
-    'MsgBox retStr
     TestLog_ASSERT retStr = "Abc Efg Hij", "Converts the first letter of every word in string to uppercase:" & retStr
 
     'retStr = StrConv("ABCDEVB¥ì¥¹¥­¥å©`", vbWide)
@@ -61,6 +59,30 @@ Function verify_testStrConv() as String
    ' retStr = StrConv(x, vbUnicode)
     'MsgBox retStr
    ' TestLog_ASSERT retStr = "ÉÏº£ÊÐABC", "Converts the string to Unicode: " & retStr
+
+    x = StrConv(srcStr, vbFromUnicode)
+    TestLog_ASSERT UBound(x) = 10, "Converts the string from Unicode, the length is: " & UBound(x) + 1
+
+    TestLog_ASSERT x(0) = 97, "Converts the string from Unicode, first char is: " & x(0)
+    TestLog_ASSERT x(1) = 98, "Converts the string from Unicode, second char is: " & x(1)
+    TestLog_ASSERT x(2) = 99, "Converts the string from Unicode, third char is: " & x(2)
+    TestLog_ASSERT x(3) = 32, "Converts the string from Unicode, fourth char is: " & x(3)
+    TestLog_ASSERT x(4) = 69, "Converts the string from Unicode, fifth char is: " & x(4)
+    TestLog_ASSERT x(5) = 70, "Converts the string from Unicode, sixth char is: " & x(5)
+    TestLog_ASSERT x(6) = 71, "Converts the string from Unicode, seventh char is: " & x(6)
+    TestLog_ASSERT x(7) = 32, "Converts the string from Unicode, eighth char is: " & x(7)
+    TestLog_ASSERT x(8) = 104, "Converts the string from Unicode, nineth char is: " & x(8)
+    TestLog_ASSERT x(9) = 105, "Converts the string from Unicode, tenth char is: " & x(9)
+    TestLog_ASSERT x(10) = 106, "Converts the string from Unicode, eleventh char is: " & x(10)
+
+    ReDim x(Len(srcStr) - 1)
+    For i = 1 To Len(srcStr)
+        x(i - 1) = Asc(Mid(srcStr, i, 1))
+    Next
+
+    retStr = StrConv(x, vbUnicode)
+
+    TestLog_ASSERT retStr = srcStr, "Converts the string to Unicode: " & retStr
 
     result = result & Chr$(10) & "Tests passed: " & passCount & Chr$(10) & "Tests failed: " & failCount & Chr$(10)
     verify_testStrConv = result
