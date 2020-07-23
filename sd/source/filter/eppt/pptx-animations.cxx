8ab3a48872c19257c177c5a3d161fe5ea3e449a9
Xisco Fauli <xiscofauli@libreoffice.org>
2020-07-23 16:52:14 +0200

    sd: fix access violation
    See https://crashreport.libreoffice.org/stats/signature/%60anonymous%20namespace'::WriteAnimateValues
    
    Change-Id: I66dbb380e8b2d313e58cddf938d952aed4a635b7

--- sd/source/filter/eppt/pptx-animations.cxx
+++ sd/source/filter/eppt/pptx-animations.cxx
@@ -212,7 +212,8 @@ void WriteAnimateValues(const FSHelperPtr& pFS, const Reference<XAnimate>& rXAni
     for (int i = 0; i < aKeyTimes.getLength(); i++)
     {
         SAL_INFO("sd.eppt", "animate value " << i << ": " << aKeyTimes[i]);
-        if (aValues[i].hasValue())
+        assert(i < aValues.getLength());
+        if (i < aValues.getLength() && aValues[i].hasValue())
         {
             pFS->startElementNS(XML_p, XML_tav, XML_fmla,
                                 sax_fastparser::UseIf(sFormula, !sFormula.isEmpty()), XML_tm,
