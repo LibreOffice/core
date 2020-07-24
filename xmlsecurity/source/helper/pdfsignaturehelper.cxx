1cf8bdf218bedde7b898f8f049c79199dbd1bc99
Miklos Vajna <vmiklos@collabora.com>
2020-07-24 11:29:27 +0200

    xmlsecurity: detect unsigned incremental update between signatures
    (cherry picked from commit 7468d5df5ec79783eae84b62bdc5ecf12f0ca255)
    
    Conflicts:
    	vcl/source/filter/ipdf/pdfdocument.cxx
    	xmlsecurity/inc/pdfio/pdfdocument.hxx
    	xmlsecurity/qa/unit/pdfsigning/pdfsigning.cxx
    	xmlsecurity/source/pdfio/pdfdocument.cxx
    
    Change-Id: I269ed858852ee7d1275adf340c8cc1565fc30693

--- xmlsecurity/source/helper/pdfsignaturehelper.cxx
+++ xmlsecurity/source/helper/pdfsignaturehelper.cxx
@@ -52,8 +52,7 @@ bool PDFSignatureHelper::ReadAndVerifySignature(const uno::Reference<io::XInputS
     {
         SignatureInformation aInfo(i);
 
-        bool bLast = i == aSignatures.size() - 1;
-        if (!xmlsecurity::pdfio::ValidateSignature(*pStream, aSignatures[i], aInfo, bLast))
+        if (!xmlsecurity::pdfio::ValidateSignature(*pStream, aSignatures[i], aInfo, aDocument))
             SAL_WARN("xmlsecurity.helper", "failed to determine digest match");
 
         m_aSignatureInfos.push_back(aInfo);
