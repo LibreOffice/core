42aeb9f906ca4e23d118ff8563184f9315ef3b82
Jan-Marek Glogowski <glogow@fbihome.de>
2020-07-29 12:04:03 +0200

    cross-build: fix Java NI linking
    LibreOffice has a JNI component on Windows and Linux, the
    officebean. Therefore we need a host JDK for linkage to the
    jawt, and a build JDK to compile the Java code.
    
    Change-Id: I4138628ab3ea2ef5900a5b4e9281050ae84e4eb5
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/102483
    Tested-by: Jenkins
    Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>

--- external/apache-commons/ExternalProject_apache_commons_logging.mk
+++ external/apache-commons/ExternalProject_apache_commons_logging.mk
@@ -16,6 +16,7 @@ $(eval $(call gb_ExternalProject_register_targets,apache_commons_logging,\
 $(call gb_ExternalProject_get_state_target,apache_commons_logging,build) :
 	$(call gb_Trace_StartRange,apache_commons_logging,EXTERNAL)
 	$(call gb_ExternalProject_run,build,\
+	JAVA_HOME=$(JAVA_HOME_FOR_BUILD) \
 	ANT_OPTS="$$ANT_OPTS -Dfile.encoding=ISO-8859-1" \
 	$(ICECREAM_RUN) "$(ANT)" \
 		$(if $(verbose),-v,-q) \
