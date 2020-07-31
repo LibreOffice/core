LibreOffice/core

commit 05d286e0065ff1c10e75827ac4726b5a74747128

vcl/clipboard: create instances with uno constructors
See tdf#74608 for motivation

Change-Id: I8b50ccb014adeba8fa5084bcb804dfa61fa61159
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/98265
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/source/components/dtranscomp.cxx
@@ -185,64 +185,16 @@ void GenericClipboard::removeClipboardListener( const Reference< datatransfer::c
     m_aListeners.erase(std::remove(m_aListeners.begin(), m_aListeners.end(), listener), m_aListeners.end());
 }
 
-namespace {
 
-class ClipboardFactory : public ::cppu::WeakComponentImplHelper<
-    css::lang::XSingleServiceFactory
->
-{
-    osl::Mutex m_aMutex;
-public:
-    ClipboardFactory();
 
-    /*
-     *  XSingleServiceFactory
-     */
-    virtual Reference< XInterface > SAL_CALL createInstance() override;
-    virtual Reference< XInterface > SAL_CALL createInstanceWithArguments( const Sequence< Any >& rArgs ) override;
-};
-
-}
-
-ClipboardFactory::ClipboardFactory() :
-        cppu::WeakComponentImplHelper<
-    css::lang::XSingleServiceFactory
->( m_aMutex )
-{
-}
-
-Reference< XInterface > ClipboardFactory::createInstance()
-{
-    return createInstanceWithArguments( Sequence< Any >() );
-}
-
-Reference< XInterface > ClipboardFactory::createInstanceWithArguments( const Sequence< Any >& arguments )
+extern "C" SAL_DLLPUBLIC_EXPORT css::uno::XInterface*
+vcl_SystemClipboard_get_implementation(
+    css::uno::XComponentContext* , css::uno::Sequence<css::uno::Any> const& args)
 {
     SolarMutexGuard aGuard;
-    Reference< XInterface > xResult = ImplGetSVData()->mpDefInst->CreateClipboard( arguments );
-    return xResult;
-}
-
-OUString Clipboard_getImplementationName()
-{
-    return
-    #if defined MACOSX
-    "com.sun.star.datatransfer.clipboard.AquaClipboard"
-    #elif defined IOS
-    "com.sun.star.datatransfer.clipboard.iOSClipboard"
-    #elif defined ANDROID
-    "com.sun.star.datatransfer.VCLGenericClipboard"
-    #elif defined UNX
-    "com.sun.star.datatransfer.X11ClipboardSupport"
-    #else
-    "com.sun.star.datatransfer.VCLGenericClipboard"
-    #endif
-     ;
-}
-
-Reference< XSingleServiceFactory > Clipboard_createFactory()
-{
-    return Reference< XSingleServiceFactory >( new ClipboardFactory() );
+    auto xClipboard = ImplGetSVData()->mpDefInst->CreateClipboard( args );
+    xClipboard->acquire();
+    return xClipboard.get();
 }
 
 namespace {

solenv/bin/native-code.py
@@ -304,6 +304,8 @@ core_constructor_list = [
 # uui/util/uui.component
     "com_sun_star_comp_uui_UUIInteractionHandler_get_implementation",
     "com_sun_star_comp_uui_UUIInteractionRequestStringResolver_get_implementation",
+# vcl/*.component
+    "vcl_SystemClipboard_get_implementation",
 # xmloff/source/transform/xof.component
     "xmloff_XMLCalcContentImportOOO_get_implementation",
     "xmloff_XMLCalcImportOOO_get_implementation",

vcl/inc/factory.hxx
@@ -36,11 +36,6 @@ namespace com::sun::star {
 
 namespace vcl {
 
-OUString Clipboard_getImplementationName();
-
-css::uno::Reference<css::lang::XSingleServiceFactory>
-Clipboard_createFactory();
-
 css::uno::Sequence<OUString> DragSource_getSupportedServiceNames();
 
 OUString DragSource_getImplementationName();

vcl/source/components/factory.cxx
@@ -43,11 +43,7 @@ extern "C" {
                 static_cast< css::lang::XMultiServiceFactory* >( pXUnoSMgr )
                 );
             Reference< css::lang::XSingleServiceFactory > xFactory;
-            if( vcl::Clipboard_getImplementationName().equalsAscii( pImplementationName ) )
-            {
-                xFactory = vcl::Clipboard_createFactory();
-            }
-            else if( vcl::DragSource_getImplementationName().equalsAscii( pImplementationName ) )
+            if( vcl::DragSource_getImplementationName().equalsAscii( pImplementationName ) )
             {
                 xFactory = ::cppu::createSingleFactory(
                     xMgr, vcl::DragSource_getImplementationName(), vcl::DragSource_createInstance,

vcl/vcl.android.component
@@ -18,8 +18,9 @@
  -->
 
 <component loader="com.sun.star.loader.SharedLibrary" environment="@CPPU_ENV@"
-    prefix="vcl" xmlns="http://openoffice.org/2010/uno-components">
-  <implementation name="com.sun.star.datatransfer.VCLGenericClipboard">
+    xmlns="http://openoffice.org/2010/uno-components">
+  <implementation name="com.sun.star.datatransfer.VCLGenericClipboard"
+    constructor="vcl_SystemClipboard_get_implementation">
     <service name="com.sun.star.datatransfer.clipboard.SystemClipboard"/>
   </implementation>
   <implementation name="com.sun.star.frame.VCLSessionManagerClient"

vcl/vcl.ios.component
@@ -18,8 +18,9 @@
  -->
 
 <component loader="com.sun.star.loader.SharedLibrary" environment="@CPPU_ENV@"
-    prefix="vcl" xmlns="http://openoffice.org/2010/uno-components">
-  <implementation name="com.sun.star.datatransfer.clipboard.iOSClipboard">
+    xmlns="http://openoffice.org/2010/uno-components">
+  <implementation name="com.sun.star.datatransfer.clipboard.iOSClipboard"
+    constructor="vcl_SystemClipboard_get_implementation">
     <service name="com.sun.star.datatransfer.clipboard.SystemClipboard"/>
   </implementation>
   <implementation name="com.sun.star.frame.VCLSessionManagerClient"

vcl/vcl.macosx.component
@@ -25,7 +25,8 @@
   <implementation name="com.sun.star.comp.datatransfer.dnd.OleDropTarget_V1">
     <service name="com.sun.star.datatransfer.dnd.OleDropTarget"/>
   </implementation>
-  <implementation name="com.sun.star.datatransfer.clipboard.AquaClipboard">
+  <implementation name="com.sun.star.datatransfer.clipboard.AquaClipboard"
+    constructor="vcl_SystemClipboard_get_implementation">
     <service name="com.sun.star.datatransfer.clipboard.SystemClipboard"/>
   </implementation>
   <implementation name="com.sun.star.frame.VCLSessionManagerClient"

vcl/vcl.unx.component
@@ -19,7 +19,8 @@
 
 <component loader="com.sun.star.loader.SharedLibrary" environment="@CPPU_ENV@"
     prefix="vcl" xmlns="http://openoffice.org/2010/uno-components">
-  <implementation name="com.sun.star.datatransfer.X11ClipboardSupport">
+  <implementation name="com.sun.star.datatransfer.X11ClipboardSupport"
+    constructor="vcl_SystemClipboard_get_implementation">
     <service name="com.sun.star.datatransfer.clipboard.SystemClipboard"/>
   </implementation>
   <implementation name="com.sun.star.datatransfer.dnd.XdndDropTarget">

commit a429e36fd26f66fa791ebfbce8b70b69e7dc5a7f

tdf#135212 Don't crash when unable to create clipboard
Gracefully handle the case where the SalInstance returns an
empty Reference when trying to create clipboard.

Primary selection support on Wayland has only recently been
added to the relevant KDE components ([1], [2]), and an empty
Reference would be returned for the qt5/kf5 VCL plugin when
trying to create create a clipboard for primary selection
in a Plasma Wayland session with that functionality not yet being
available.

Regression from commit 05d286e0065ff1c10e75827ac4726b5a74747128
("vcl/clipboard: create instances with uno constructors", 2020-07-07).

[1] https://invent.kde.org/plasma/kwayland-server/-/merge_requests/15
[2] https://invent.kde.org/plasma/kwin/-/merge_requests/27

Change-Id: Ia9016f56b775003b2ffe81a6f3bf44f411ad39a2

vcl/source/components/dtranscomp.cxx
@@ -193,7 +193,8 @@ vcl_SystemClipboard_get_implementation(
 {
     SolarMutexGuard aGuard;
     auto xClipboard = ImplGetSVData()->mpDefInst->CreateClipboard( args );
-    xClipboard->acquire();
+    if (xClipboard.is())
+        xClipboard->acquire();
     return xClipboard.get();
 }
 