afaed0b1141d67c36ff0714be5d7eb2825a79bdd
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2020-08-01 16:09:00 -0400

    lok: minor cleanup of ViewCallback in tiledrendering tests
    Change-Id: I39b964a7aa7d181083a153283f38c1a5bc64de58
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/99973
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Ashod Nakashian <ash@collabora.com>

--- sd/qa/unit/tiledrendering/tiledrendering.cxx
+++ sd/qa/unit/tiledrendering/tiledrendering.cxx
@@ -838,7 +838,7 @@ void SdTiledRenderingTest::testResizeTableColumn()
 }
 
 /// A view callback tracks callbacks invoked on one specific view.
-class ViewCallback
+class ViewCallback final
 {
     SfxViewShell* mpViewShell;
     int mnView;
