d9c07f4699326c8e441e8236b7b0adf7ad4eba26
Heiko Tietze <tietze.heiko@gmail.com>
2020-08-02 17:10:00 +0200

    Resolves tdf#117463 - Dialog to show UI options
    * New dialog to pick and apply one of the UI variants
    * Tip-of-the-Day enhanced to execute UNO commands in order
    to advertise the UI chooser on first start
    
    Change-Id: I5e5c4a8e0fb11d11bafc99cc37b7ec244a993781

--- cui/UIConfig_cui.mk
+++ cui/UIConfig_cui.mk
@@ -203,6 +203,7 @@ $(eval $(call gb_UIConfig_add_uifiles,cui,\
 	cui/uiconfig/ui/textdialog \
 	cui/uiconfig/ui/textflowpage \
 	cui/uiconfig/ui/thesaurus \
+	cui/uiconfig/ui/toolbarmodedialog \
 	cui/uiconfig/ui/transparencytabpage \
 	cui/uiconfig/ui/tsaurldialog \
 	cui/uiconfig/ui/twolinespage \
