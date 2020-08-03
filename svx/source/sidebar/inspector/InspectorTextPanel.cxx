ff4d1d5f65ac5441e2fe35aefec4f800125a3a57
Shivam Kumar Singh <shivamhere247@gmail.com>
2020-08-03 11:52:21 +0530

    tdf#134560 make inspector contents human readable
    This patch adds a dedicated hrc file for RID's
    of all the properties that can be displayed in
    the Inspector
    
    Change-Id: I258ca060508e28b7b66e96393fcef4fd104bb781

--- svx/source/sidebar/inspector/InspectorTextPanel.cxx
+++ svx/source/sidebar/inspector/InspectorTextPanel.cxx
@@ -23,6 +23,7 @@
 #include <com/sun/star/awt/FontSlant.hpp>
 #include <com/sun/star/beans/XPropertySet.hpp>
 #include <com/sun/star/lang/IllegalArgumentException.hpp>
+#include <svx/inspectorproperties.hrc>
 
 using namespace css;
 
