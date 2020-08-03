b124d6c12fc70d353bae07e04a2d6683471f0f2f
Caolán McNamara <caolanm@redhat.com>
2020-08-03 20:29:30 +0100

    ofz#24648 fix slkfuzzer
    Change-Id: I3dce445038a29a622ceea2415e7736768a95206d

--- vcl/workben/slkfuzzer.cxx
+++ vcl/workben/slkfuzzer.cxx
@@ -27,6 +27,7 @@ void * com_sun_star_i18n_NativeNumberSupplier_get_implementation( void *, void *
 void * com_sun_star_i18n_NumberFormatCodeMapper_get_implementation( void *, void * );
 void * com_sun_star_i18n_Transliteration_get_implementation( void *, void * );
 void * emfio_emfreader_XEmfParser_get_implementation( void *, void * );
+void * i18npool_CalendarImpl_get_implementation( void *, void * );
 }
 
 const lib_to_factory_mapping *
@@ -55,6 +56,7 @@ lo_get_constructor_map(void)
         { "com_sun_star_i18n_NumberFormatCodeMapper_get_implementation", com_sun_star_i18n_NumberFormatCodeMapper_get_implementation },
         { "com_sun_star_i18n_Transliteration_get_implementation", com_sun_star_i18n_Transliteration_get_implementation },
         { "emfio_emfreader_XEmfParser_get_implementation", emfio_emfreader_XEmfParser_get_implementation},
+        { "i18npool_CalendarImpl_get_implementation", i18npool_CalendarImpl_get_implementation},
         { 0, 0 }
     };
 
