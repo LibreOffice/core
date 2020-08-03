d89c9917847919b84edd80eaf9d53989125f5ee5
Aditya <adityasahu1511@gmail.com>
2020-08-03 22:50:53 +0530

    svx: Introduce GalleryFileStorage
    Abstract class for GalleryBinaryEngine and upcoming GalleryXMLEngine. This
     interface will be used by GalleryTheme.
    
    Change-Id: I2193e3ea9860c41a1580d5ca59da68bf41b70a3d

--- include/svx/gallerybinaryengine.hxx
+++ include/svx/gallerybinaryengine.hxx
@@ -24,6 +24,7 @@
 #include <svx/svxdllapi.h>
 #include <svx/fmmodel.hxx>
 #include <svx/gallerystoragelocations.hxx>
+#include <svx/galleryfilestorage.hxx>
 #include <tools/urlobj.hxx>
 #include <sot/storage.hxx>
 
@@ -40,7 +41,7 @@ class FmFormModel;
 class GalleryTheme;
 class GalleryThemeEntry;
 
-class SVXCORE_DLLPUBLIC GalleryBinaryEngine
+class SVXCORE_DLLPUBLIC GalleryBinaryEngine : public GalleryFileStorage
 {
 private:
     tools::SvRef<SotStorage> m_aSvDrawStorageRef;
