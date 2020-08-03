f77876631af26a945d92c84f97c17a27e6bd68b2
Szymon Kłos <szymon.klos@collabora.com>
2020-08-03 12:15:17 +0200

    lok: state updates for commands in Writer
    Change-Id: Id3d429ff4788abce03e9d7c5522655f362d6c249
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/99996
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -2742,7 +2742,26 @@ static void doc_iniUnoCommands ()
         OUString(".uno:ParaspaceIncrease"),
         OUString(".uno:ParaspaceDecrease"),
         OUString(".uno:AcceptTrackedChange"),
-        OUString(".uno:RejectTrackedChange")
+        OUString(".uno:RejectTrackedChange"),
+        OUString(".uno:ShowResolvedAnnotations"),
+        OUString(".uno:FreezePanesColumn"),
+        OUString(".uno:InsertBreak"),
+        OUString(".uno:InsertEndnote"),
+        OUString(".uno:InsertFootnote"),
+        OUString(".uno:InsertReferenceField"),
+        OUString(".uno:InsertBookmark"),
+        OUString(".uno:InsertAuthoritiesEntry"),
+        OUString(".uno:InsertMultiIndex"),
+        OUString(".uno:InsertField"),
+        OUString(".uno:InsertPageNumberField"),
+        OUString(".uno:InsertPageCountField"),
+        OUString(".uno:InsertDateField"),
+        OUString(".uno:InsertTitleField"),
+        OUString(".uno:InsertFieldCtrl"),
+        OUString(".uno:CharmapControl"),
+        OUString(".uno:EnterGroup"),
+        OUString(".uno:LeaveGroup"),
+        OUString(".uno:FreezePanesRow")
     };
 
     util::URL aCommandURL;
