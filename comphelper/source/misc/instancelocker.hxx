88da6b7b62dc0c391e28a920bb2d355bce89f8bf
George Bateman <george.bateman16@gmail.com>
2020-08-05 15:41:27 +0000

    tdf#124176 Use #pragma once in comphelper
    This commit was carried out by a Python script, source of which
    is at https://bugs.documentfoundation.org/show_bug.cgi?id=124176#c97.
    
    Change-Id: I72229dc1ef743f0394f778aec7c49c11fe7e5176
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/100183
    Tested-by: Jenkins
    Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

--- comphelper/source/misc/instancelocker.hxx
+++ comphelper/source/misc/instancelocker.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_COMPHELPER_SOURCE_MISC_INSTANCELOCKER_HXX
-#define INCLUDED_COMPHELPER_SOURCE_MISC_INSTANCELOCKER_HXX
+#pragma once
 
 #include <com/sun/star/lang/XComponent.hpp>
 #include <com/sun/star/util/XCloseListener.hpp>
@@ -109,7 +108,5 @@ public:
     virtual void SAL_CALL notifyTermination( const css::lang::EventObject& Event ) override;
 
 };
-
-#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
