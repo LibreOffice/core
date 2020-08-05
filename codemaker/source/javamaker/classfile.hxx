ce73b526c459bf1ab15220fe78afae44cb436184
George Bateman <george.bateman16@gmail.com>
2020-08-05 15:39:00 +0000

    tdf#124176 Use #pragma once in codemaker
    This commit was carried out by a Python script, source of which
    is at https://bugs.documentfoundation.org/show_bug.cgi?id=124176#c97.
    
    Change-Id: I421f0594d5f4389f7800a5bd0e339edde5627bb3
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/100182
    Tested-by: Jenkins
    Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

--- codemaker/source/javamaker/classfile.hxx
+++ codemaker/source/javamaker/classfile.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_CODEMAKER_SOURCE_JAVAMAKER_CLASSFILE_HXX
-#define INCLUDED_CODEMAKER_SOURCE_JAVAMAKER_CLASSFILE_HXX
+#pragma once
 
 #include <codemaker/unotype.hxx>
 #include <sal/types.h>
@@ -236,7 +235,5 @@ private:
 };
 
 }
-
-#endif // INCLUDED_CODEMAKER_SOURCE_JAVAMAKER_CLASSFILE_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
