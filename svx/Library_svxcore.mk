a5b2ddb8c7ed27f8a1c365013ba7bf737050a3d3
Aditya <adityasahu1511@gmail.com>
2020-08-05 15:22:02 +0530

    svx:Implement GalleryXMLEngine
    Change-Id: I94775464bc0a9c29a4812d2a7ef55019fe94067e

--- svx/Library_svxcore.mk
+++ svx/Library_svxcore.mk
@@ -157,6 +157,7 @@ $(eval $(call gb_Library_add_exception_objects,svxcore,\
     svx/source/gallery2/gallerystoragelocations \
     svx/source/gallery2/galleryobjectcollection \
     svx/source/gallery2/galleryfilestorage \
+    svx/source/gallery2/galleryxmlengine \
     svx/source/items/chrtitem \
     svx/source/items/clipfmtitem \
     svx/source/items/customshapeitem \
