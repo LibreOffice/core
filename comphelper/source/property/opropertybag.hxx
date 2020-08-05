081e4c53c651d6bef658f0bfad18a440dc64d1bb
George Bateman <george.bateman16@gmail.com>
2020-08-05 15:41:27 +0000

    tdf#124176 Use #pragma once in comphelper
    This commit was carried out by a Python script, source of which
    is at https://bugs.documentfoundation.org/show_bug.cgi?id=124176#c97.
    
    Change-Id: I72229dc1ef743f0394f778aec7c49c11fe7e5176

--- comphelper/source/property/opropertybag.hxx
+++ comphelper/source/property/opropertybag.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_COMPHELPER_SOURCE_PROPERTY_OPROPERTYBAG_HXX
-#define INCLUDED_COMPHELPER_SOURCE_PROPERTY_OPROPERTYBAG_HXX
+#pragma once
 
 #include <com/sun/star/lang/XInitialization.hpp>
 #include <com/sun/star/lang/XServiceInfo.hpp>
@@ -215,7 +214,5 @@ namespace comphelper
 
 } // namespace comphelper
 
-
-#endif // INCLUDED_COMPHELPER_SOURCE_PROPERTY_OPROPERTYBAG_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
