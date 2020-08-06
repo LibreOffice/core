96852049dc8e1d7036ec407a7fb7077a6b49ee8a
Mike Kaganski <mike.kaganski@collabora.com>
2020-08-06 22:49:41 +0300

    Fix Skia build using VS 2019 v.16.7.0 with --disable-pch
    No idea if updated VS makes a difference, or some recent Skia update...
    
    In file included from C:/lo/src/build/workdir/UnpackedTarball/skia/src/core/SkBitmapDevice.cpp:11:
    In file included from C:/lo/src/build/workdir/UnpackedTarball/skia\include/core/SkPath.h:13:
    C:/lo/src/build/workdir/UnpackedTarball/skia\include/private/SkPathRef.h(113,41): error: implicit instantiation of undefined template
          'std::tuple<SkPoint *, float *>'
            std::tuple<SkPoint*, SkScalar*> growForVerbsInPath(const SkPathRef& path) {
                                            ^
    C:/PROGRA~2/MICROS~1/2019/COMMUN~1/VC/Tools/MSVC/1427~1.291/Include\utility(126,7): note: template is declared here
    class tuple;
          ^
    In file included from C:/lo/src/build/workdir/UnpackedTarball/skia/src/core/SkBitmapDevice.cpp:11:
    In file included from C:/lo/src/build/workdir/UnpackedTarball/skia\include/core/SkPath.h:13:
    C:/lo/src/build/workdir/UnpackedTarball/skia\include/private/SkPathRef.h(114,30): error: implicit instantiation of undefined template
          'std::tuple<SkPoint *, float *>'
                return fPathRef->growForVerbsInPath(path);
                                 ^
    C:/PROGRA~2/MICROS~1/2019/COMMUN~1/VC/Tools/MSVC/1427~1.291/Include\utility(126,7): note: template is declared here
    class tuple;
          ^
    In file included from C:/lo/src/build/workdir/UnpackedTarball/skia/src/core/SkBitmapDevice.cpp:11:
    C:/lo/src/build/workdir/UnpackedTarball/skia\include/core/SkPath.h(1543,65): error: implicit instantiation of undefined template 'std::tuple<SkPathVerb, const
          SkPoint *, const float *>'
            std::tuple<SkPathVerb, const SkPoint*, const SkScalar*> operator*() const {
                                                                    ^
    C:/PROGRA~2/MICROS~1/2019/COMMUN~1/VC/Tools/MSVC/1427~1.291/Include\utility(126,7): note: template is declared here
    class tuple;
          ^
    In file included from C:/lo/src/build/workdir/UnpackedTarball/skia/src/core/SkBitmapDevice.cpp:11:
    C:/lo/src/build/workdir/UnpackedTarball/skia\include/core/SkPath.h(1550,20): error: implicit instantiation of undefined template 'std::tuple<SkPathVerb, const
          SkPoint *, const float *>'
                return {verb, fPoints + backset, fWeights};
                       ^
    C:/PROGRA~2/MICROS~1/2019/COMMUN~1/VC/Tools/MSVC/1427~1.291/Include\utility(126,7): note: template is declared here
    class tuple;
          ^
    In file included from C:/lo/src/build/workdir/UnpackedTarball/skia/src/core/SkBitmapDevice.cpp:11:
    C:/lo/src/build/workdir/UnpackedTarball/skia\include/core/SkPath.h(1625,68): error: implicit instantiation of undefined template 'std::tuple<SkPathVerb, const
          SkPoint *, const float *>'
                return (fIter != fEnd) ? static_cast<Verb>(std::get<0>(*fIter)) : kDone_Verb;
                                                                       ^
    C:/PROGRA~2/MICROS~1/2019/COMMUN~1/VC/Tools/MSVC/1427~1.291/Include\utility(126,7): note: template is declared here
    class tuple;
          ^
    5 errors generated.
    make[1]: *** [C:/lo/src/core/solenv/gbuild/LinkTarget.mk:351: C:/lo/src/build/workdir/GenCxxObject/UnpackedTarball/skia/src/core/SkBitmapDevice.o] Error 1
    
    Change-Id: Ica85829cf61d86e486cf4b2a730f50e06e3fb337

--- external/skia/UnpackedTarball_skia.mk
+++ external/skia/UnpackedTarball_skia.mk
@@ -39,6 +39,7 @@ skia_patches := \
     fix-windows-dwrite.patch.1 \
     public-make-from-backend-texture.patch.1 \
     c++20.patch.0 \
+    missing-include.patch.1 \
 
 $(eval $(call gb_UnpackedTarball_set_patchlevel,skia,1))
 
