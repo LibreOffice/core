a94fb9d9f0a6f9deadc14a0a5cd00f614e2e9275
George Bateman <george.bateman16@gmail.com>
2020-08-06 14:38:33 +0000

    tdf#124176 Use #pragma once in cppcanvas
    This commit was carried out by a Python script, source of which
    is at https://bugs.documentfoundation.org/show_bug.cgi?id=124176#c97.
    
    Change-Id: I984bb1056da13d163f7fe654d780502a6d34231f

--- cppcanvas/source/wrapper/implsprite.hxx
+++ cppcanvas/source/wrapper/implsprite.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_CPPCANVAS_SOURCE_WRAPPER_IMPLSPRITE_HXX
-#define INCLUDED_CPPCANVAS_SOURCE_WRAPPER_IMPLSPRITE_HXX
+#pragma once
 
 #include <com/sun/star/uno/Reference.hxx>
 #include <com/sun/star/rendering/XSpriteCanvas.hpp>
@@ -60,7 +59,5 @@ namespace cppcanvas::internal
             ImplSpriteCanvas::TransformationArbiterSharedPtr              mpTransformArbiter;
         };
 }
-
-#endif // INCLUDED_CPPCANVAS_SOURCE_WRAPPER_IMPLSPRITE_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
