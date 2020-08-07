edd916b47e01ea53f0989bdbb6a703e081e25aea
Ahmed ElShreif <aelshreif7@gmail.com>
2020-08-08 01:04:28 +0200

    uitest : Add support for Normal ValueSet
    This patch will add support for all the normal valuesets inside any dialog .
    
    for example it will help in "Select Presets and Shadow Styles in borders tab ( format table or format cell )" to test tdf#133641 .
    
    You can use the support by this line:
    	>> obj_name.executeAction("SELECT", mkPropertyValues({"POS": "4"}))
    
    Change-Id: I35d0608318f1bca4f4e702ebcc258957835ce0db
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/100373
    Tested-by: Jenkins
    Reviewed-by: Markus Mohrhard <markus.mohrhard@googlemail.com>

---
+++ svtools/source/uitest/uiobject.cxx
@@ -0,0 +1,57 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <memory>
+#include <uiobject.hxx>
+#include <svtools/valueset.hxx>
+#include <vcl/layout.hxx>
+
+ValueSetUIObject::ValueSetUIObject(const VclPtr<vcl::Window>& xSetWin, ValueSet* pSet)
+    : WindowUIObject(xSetWin)
+    , mpSet(pSet)
+{
+}
+
+void ValueSetUIObject::execute(const OUString& rAction, const StringMap& rParameters)
+{
+    if (rAction == "SELECT")
+    {
+        if (rParameters.find("POS") != rParameters.end())
+        {
+            OUString aIndexStr = rParameters.find("POS")->second;
+
+            sal_Int32 nIndex = aIndexStr.toInt32();
+            mpSet->SelectItem(nIndex);
+            mpSet->Select();
+        }
+    }
+    else
+        WindowUIObject::execute(rAction, rParameters);
+}
+
+std::unique_ptr<UIObject> ValueSetUIObject::create(vcl::Window* pWindow)
+{
+    VclDrawingArea* pSetWin = dynamic_cast<VclDrawingArea*>(pWindow);
+    assert(pSetWin);
+    return std::unique_ptr<UIObject>(
+        new ValueSetUIObject(pSetWin, static_cast<ValueSet*>(pSetWin->GetUserData())));
+}
+
+OUString ValueSetUIObject::get_name() const { return "ValueSetUIObject"; }
+
+StringMap ValueSetUIObject::get_state()
+{
+    StringMap aMap = WindowUIObject::get_state();
+    aMap["SelectedItemId"] = OUString::number(mpSet->GetSelectedItemId());
+    aMap["SelectedItemPos"] = OUString::number(mpSet->GetSelectItemPos());
+    aMap["ItemsCount"] = OUString::number(mpSet->GetItemCount());
+    return aMap;
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
