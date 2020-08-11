8ab544945bf7495ab91020a6f7284acb5c0a324d
László Németh <nemeth@numbertext.org>
2020-08-11 19:32:52 +0200

    tdf#118682 DOCX: export formula fields
    Convert also cell references by removing
    parenthesization:
    
    =<A1>+<B1> -> =A1*B1
    =SUM(<A1:ZZ99> -> =SUM(A1:ZZ99)
    
    See tdf#133647 for fixing import of cell references.
    
    (cherry picked from commit d42776e01b87f12fddbcf78101bca1e10a6e4f97)
    
    Change-Id: I5082198aaf8230989f99984f8129b54867b77859
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/105520
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

--- sw/source/filter/ww8/ww8atr.cxx
+++ sw/source/filter/ww8/ww8atr.cxx
@@ -3279,6 +3279,13 @@ void AttributeOutputBase::TextField( const SwFormatField& rField )
             GetExport().OutputField( pField, ww::eMACROBUTTON, sStr );
         }
         break;
+    case SwFieldIds::Table:
+        {
+            ww::eField eField = ww::eEquals;
+            OUString aExpand = FieldString(eField) + pField->GetFieldName();
+            GetExport().OutputField(pField, eField, aExpand);
+        }
+        break;
     case SwFieldIds::User:
     {
         ww::eField eField = ww::eDOCVARIABLE;
