09111f124be4f3c34b4d7cebcc238dcd29ceaede
Szymon Kłos <szymon.klos@collabora.com>
2020-08-11 10:28:52 +0200

    Instant update shadow fields in sidebar
    This allows mobile LOK client to receive updated
    state.
    
    Change-Id: I07fb397c1566546e6e2eb8071eaf6d848f6effc0
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/100466
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Jan Holesovsky <kendy@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/101387
    Tested-by: Jenkins
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>

--- svx/source/sidebar/shadow/ShadowPropertyPanel.cxx
+++ svx/source/sidebar/shadow/ShadowPropertyPanel.cxx
@@ -21,6 +21,7 @@
 #include <svx/sdprcitm.hxx>
 #include <svx/sdsxyitm.hxx>
 #include <svx/sdshcitm.hxx>
+#include <comphelper/lok.hxx>
 
 using namespace css;
 using namespace css::uno;
@@ -109,12 +110,27 @@ IMPL_LINK_NOARG(ShadowPropertyPanel, ClickShadowHdl, weld::ToggleButton&, void)
         SdrOnOffItem aItem(makeSdrShadowItem(false));
         GetBindings()->GetDispatcher()->ExecuteList(SID_ATTR_FILL_SHADOW,
                 SfxCallMode::RECORD, { &aItem });
+
+        if (comphelper::LibreOfficeKit::isActive())
+        {
+            mxShowShadow->set_state( TRISTATE_FALSE );
+            UpdateControls();
+        }
     }
     else
     {
         SdrOnOffItem aItem(makeSdrShadowItem(true));
         GetBindings()->GetDispatcher()->ExecuteList(SID_ATTR_FILL_SHADOW,
                 SfxCallMode::RECORD, { &aItem });
+
+        if (mxShadowDistance->get_value(FieldUnit::POINT) == 0)
+            mxShadowDistance->set_value( 8, FieldUnit::POINT );
+
+        if (comphelper::LibreOfficeKit::isActive())
+        {
+            mxShowShadow->set_state( TRISTATE_TRUE );
+            UpdateControls();
+        }
     }
 }
 
