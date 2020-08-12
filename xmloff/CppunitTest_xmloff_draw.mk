b2d06342a7610bc3c6ca347f634b0e1b2b859571
Miklos Vajna <vmiklos@collabora.com>
2020-08-12 09:06:14 +0200

    tdf#135144 xmloff textbox: fix export style name to be consitent with import
    Regression from commit 28d67b792724a23015dec32fb0278b729f676736
    (tdf#107776 sw ODF shape import: make is-textbox check more strict,
    2019-08-26), the problem was that in case the import side in
    SdXMLCustomShapeContext expects a fixed "Frame" parent style name, then
    the export side should go with that name as well.
    
    Fix the problem by simplifying XMLShapeExport::collectShapeAutoStyles(),
    which initially assumed that the string may be localized, so try to look
    it up: instead just use the fixed string that the import side will look
    for.
    
    This solves all problem which may stem from a parent style name which is
    non-empty, but other than Frame.
    
    (cherry picked from commit 347d05edd8910907ae185c61c1e56eee139b3c09)
    
    Change-Id: I8146440c591d3dd5a904d243d85741d704711e9c

---
+++ xmloff/CppunitTest_xmloff_draw.mk
@@ -0,0 +1,45 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#*************************************************************************
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+#*************************************************************************
+
+$(eval $(call gb_CppunitTest_CppunitTest,xmloff_draw))
+
+$(eval $(call gb_CppunitTest_use_externals,xmloff_draw,\
+	boost_headers \
+))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,xmloff_draw, \
+    xmloff/qa/unit/draw \
+))
+
+$(eval $(call gb_CppunitTest_use_libraries,xmloff_draw, \
+    comphelper \
+    cppu \
+    embobj \
+    sal \
+    test \
+    unotest \
+    utl \
+))
+
+$(eval $(call gb_CppunitTest_use_sdk_api,xmloff_draw))
+
+$(eval $(call gb_CppunitTest_use_ure,xmloff_draw))
+$(eval $(call gb_CppunitTest_use_vcl,xmloff_draw))
+
+$(eval $(call gb_CppunitTest_use_rdb,xmloff_draw,services))
+
+$(eval $(call gb_CppunitTest_use_custom_headers,xmloff_draw,\
+	officecfg/registry \
+))
+
+$(eval $(call gb_CppunitTest_use_configuration,xmloff_draw))
+
+# vim: set noet sw=4 ts=4:
