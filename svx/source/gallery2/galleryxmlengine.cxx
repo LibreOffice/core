8b4cca74ce2256ea5abf1b0bc59d1a56d44f5368
Aditya <adityasahu1511@gmail.com>
2020-08-12 21:09:14 +0530

    svx:Implement writeGalleryTheme in GalleryXMLEngine
    This function will perform all the tasks that were done by
     GalleryBinaryEngine::writeGalleryTheme except that the data will be written in
     XML instead of binary.
    
    Change-Id: I6338a0eba0b9c88ff8ae1852d64ae7ba1ad53743

--- svx/source/gallery2/galleryxmlengine.cxx
+++ svx/source/gallery2/galleryxmlengine.cxx
@@ -18,12 +18,33 @@
  */
 
 #include <svx/galleryxmlengine.hxx>
+#include <svx/galleryobjectcollection.hxx>
+#include <svx/galtheme.hxx>
+#include <svx/gallery1.hxx>
+#include <galobj.hxx>
 
-#include <tools/urlobj.hxx>
 #include <com/sun/star/packages/zip/ZipFileAccess.hpp>
 #include <com/sun/star/beans/NamedValue.hpp>
 #include <comphelper/processfactory.hxx>
 #include <unotools/ucbstreamhelper.hxx>
+
+#include <tools/XmlWalker.hxx>
+#include <tools/XmlWriter.hxx>
+#include <tools/urlobj.hxx>
+#include <tools/vcompat.hxx>
+
+#include <osl/thread.hxx>
+#include <rtl/ustring.hxx>
+
+#include <sal/log.hxx>
+
+#include <tools/urlobj.hxx>
+#include <com/sun/star/packages/zip/ZipFileAccess.hpp>
+#include <com/sun/star/embed/XTransactedObject.hpp>
+#include <com/sun/star/beans/NamedValue.hpp>
+#include <comphelper/processfactory.hxx>
+#include <unotools/ucbstreamhelper.hxx>
+#include <unotools/streamwrap.hxx>
 
 #include <tools/XmlWalker.hxx>
 #include <tools/XmlWriter.hxx>
@@ -32,86 +53,211 @@
 
 using namespace ::com::sun::star;
 
-/*void GalleryXMLEngine::readZIP()
+GalleryXMLEngine::GalleryXMLEngine(GalleryObjectCollection& rGalleryObjectCollection)
+    : mrGalleryObjectCollection(rGalleryObjectCollection)
 {
-    SAL_WARN("svx", "call to XMLEngine function createZipFile() triggered");
-    //OUString aURL = "C:/cygwin/home/adith/lode/dev/core/package/qa/cppunit/data/a2z.zip";
-    OUString aURL = "file:///C:/Users/adith/Desktop/zip testing/sendEmails.zip";
-    uno::Reference<uno::XComponentContext> xContext = comphelper::getProcessComponentContext();
-    uno::Reference<packages::zip::XZipFileAccess2> const xZip(
-        packages::zip::ZipFileAccess::createWithURL(xContext, aURL));
-    SAL_WARN_IF(!xZip.is(), "svx", "Failed to read zip file");
-
-    uno::Sequence<OUString> elementList = xZip->getElementNames();
-    sal_uInt32 len = elementList.getLength();
-    len;
-    // to read meta.xml
-    uno::Reference<io::XInputStream> const xInputStream(xZip->getByName("meta.xml"),
-                                                        uno::UNO_QUERY);
-    std::unique_ptr<SvStream> const pStream(utl::UcbStreamHelper::CreateStream(xInputStream, true));
-    pStream.get();
 }
 
-void GalleryXMLEngine::readXML()
+void GalleryXMLEngine::writeMimeType(uno::Reference<embed::XStorage>& rxWriteableZipStore)
 {
-    //OUString sFilePath = "file:///C:/Users/adith/Desktop/zip testing/test.xml";
-    OUString sFilePath = "file:///C:/cygwin/home/adith/lode/dev/core/tools/qa/data/test.xml";
-    SvFileStream aFileStream(sFilePath, StreamMode::READ);
-    tools::XmlWalker aWalker;
-    if (!aWalker.open(&aFileStream))
-        return;
-    if ((aWalker.name() != "root") || (aWalker.attribute("root-attr") != "Hello World"))
-        return;
-    OString tmpAttr, tmpAttr2, tmpContent, tmpName;
-    aWalker.children();
-    while (aWalker.isValid())
+    // Write mimetype to an existing ZIP file
+    css::uno::Reference<css::io::XOutputStream> xOutputStream;
+    xOutputStream.set(
+        rxWriteableZipStore->openStreamElement("mimetype", embed::ElementModes::READWRITE),
+        uno::UNO_QUERY);
+    const OString aMimeType("application/gallerytheme+zip");
+    uno::Sequence<sal_Int8> aData(reinterpret_cast<const sal_Int8*>(aMimeType.getStr()),
+                                  aMimeType.getLength());
+    xOutputStream->writeBytes(aData);
+
+    uno::Reference<embed::XTransactedObject> xTransact(rxWriteableZipStore, uno::UNO_QUERY_THROW);
+    xTransact->commit();
+    xTransact.set(rxWriteableZipStore, uno::UNO_QUERY_THROW);
+    xTransact->commit();
+}
+
+uno::Reference<embed::XStorage> GalleryXMLEngine::writeZipFile(const OUString& rZipFile)
+{
+    // Write ZipFile
+    std::unique_ptr<SvStream> pStream(
+        utl::UcbStreamHelper::CreateStream(rZipFile, StreamMode::READ | StreamMode::WRITE));
+    uno::Reference<io::XStream> xStream(new utl::OStreamWrapper(std::move(pStream)));
+    uno::Reference<embed::XStorage> xWriteableZipStore;
+    xWriteableZipStore = comphelper::OStorageHelper::GetStorageOfFormatFromStream(
+        ZIP_STORAGE_FORMAT_STRING, xStream);
+
+    uno::Reference<embed::XTransactedObject> xTransact(xWriteableZipStore, uno::UNO_QUERY_THROW);
+    xTransact->commit();
+    xTransact.set(xWriteableZipStore, uno::UNO_QUERY_THROW);
+    xTransact->commit();
+
+    return xWriteableZipStore;
+}
+
+SvStream& GalleryXMLEngine::writeGalleryTheme(SvStream& rOStrm, const GalleryTheme& rTheme,
+                                              const GalleryThemeEntry* pThm)
+{
+    const OUString sThemeName = pThm->GetThemeName();
+
+    OUString aZipFile = "file:///C:/Users/adith/Desktop/zip testing/" + sThemeName + ".zip";
+    uno::Reference<embed::XStorage> xWriteableZipStore = writeZipFile(aZipFile);
+    writeMimeType(xWriteableZipStore);
+
+    // Currently, Stream is unknown so for test purposes custom rOStm was used, will replace rOStrm with rOStm as soon as I know the correct rOStm
+    rOStrm.GetVersion();
+
+    const INetURLObject rRelURL1 = rTheme.GetParent()->GetRelativeURL();
+    const INetURLObject rRelURL2 = rTheme.GetParent()->GetUserURL();
+    const sal_uInt32 rId = rTheme.GetId();
+    bool bRel;
+
+    sal_uInt32 nCount = mrGalleryObjectCollection.size();
+
+    std::unique_ptr<SvMemoryStream> pMemoryStream = std::make_unique<SvMemoryStream>();
+    tools::XmlWriter aXmlWriter(pMemoryStream.get());
+
+    if (aXmlWriter.startDocument())
     {
-        if (aWalker.name() == "text")
+        aXmlWriter.startElement("Theme-Information");
+
+        //  write_uInt16_lenPrefixed_uInt8s_FromOUString(rOStm, pThm->GetThemeName(), RTL_TEXTENCODING_UTF8);
+        aXmlWriter.startElement("Name");
+        aXmlWriter.content(OUStringToOString(sThemeName, RTL_TEXTENCODING_UTF8));
+        aXmlWriter.endElement();
+
+        // rOStm.WriteUInt32(nCount).WriteUInt16(osl_getThreadTextEncoding());
+        aXmlWriter.startElement("Object-Count");
+        aXmlWriter.content(OString::number(nCount));
+        aXmlWriter.endElement();
+
+        aXmlWriter.startElement("Text-Encoding");
+        aXmlWriter.content(OString(osl_getThreadTextEncoding()));
+        aXmlWriter.endElement();
+
+        for (sal_uInt32 i = 0; i < nCount; i++)
         {
-            aWalker.next();
-            if (aWalker.name() == "child")
+            OString CurrentObject("Object-" + OString::number(nCount));
+            aXmlWriter.startElement(CurrentObject);
+
+            const GalleryObject* pObj = mrGalleryObjectCollection.getForPosition(i);
+            OUString aPath;
+
+            if (SgaObjKind::SvDraw == pObj->eObjKind)
             {
-                if (aWalker.attribute("attribute") == "123")
+                aPath = GetSvDrawStreamNameFromURL(pObj->getURL());
+                bRel = false;
+            }
+            else
+            {
+                aPath = pObj->getURL().GetMainURL(INetURLObject::DecodeMechanism::NONE);
+                aPath = aPath.copy(
+                    0,
+                    std::min(rRelURL1.GetMainURL(INetURLObject::DecodeMechanism::NONE).getLength(),
+                             aPath.getLength()));
+                bRel = aPath == rRelURL1.GetMainURL(INetURLObject::DecodeMechanism::NONE);
+
+                if (bRel
+                    && (pObj->getURL().GetMainURL(INetURLObject::DecodeMechanism::NONE).getLength()
+                        > (rRelURL1.GetMainURL(INetURLObject::DecodeMechanism::NONE).getLength()
+                           + 1)))
                 {
-                    aWalker.children();
-                    if (aWalker.name() == "text")
+                    aPath = pObj->getURL().GetMainURL(INetURLObject::DecodeMechanism::NONE);
+                    aPath = aPath.copy(std::min(
+                        rRelURL1.GetMainURL(INetURLObject::DecodeMechanism::NONE).getLength(),
+                        aPath.getLength()));
+                }
+                else
+                {
+                    aPath = pObj->getURL().GetMainURL(INetURLObject::DecodeMechanism::NONE);
+                    aPath = aPath.copy(
+                        0,
+                        std::min(
+                            rRelURL2.GetMainURL(INetURLObject::DecodeMechanism::NONE).getLength(),
+                            aPath.getLength()));
+                    bRel = aPath == rRelURL2.GetMainURL(INetURLObject::DecodeMechanism::NONE);
+
+                    if (bRel
+                        && (pObj->getURL()
+                                .GetMainURL(INetURLObject::DecodeMechanism::NONE)
+                                .getLength()
+                            > (rRelURL2.GetMainURL(INetURLObject::DecodeMechanism::NONE).getLength()
+                               + 1)))
                     {
-                        aWalker.next();
-                        if (aWalker.isValid())
-                        {
-                            //tmpContent = aWalker.content();
-                            tmpName = aWalker.name();
-                            tmpAttr = aWalker.attribute("attribute1");
-                            tmpAttr2 = aWalker.attribute("attribute2");
-                            tmpContent = aWalker.content();
-                            break;
-                        }
+                        aPath = pObj->getURL().GetMainURL(INetURLObject::DecodeMechanism::NONE);
+                        aPath = aPath.copy(std::min(
+                            rRelURL2.GetMainURL(INetURLObject::DecodeMechanism::NONE).getLength(),
+                            aPath.getLength()));
                     }
+                    else
+                        aPath = pObj->getURL().GetMainURL(INetURLObject::DecodeMechanism::NONE);
                 }
             }
+
+            /*if (!m_aDestDir.isEmpty())
+            {
+                bool aFound = aPath.indexOf(m_aDestDir) != -1;
+                aPath = aPath.replaceFirst(m_aDestDir, "");
+                if (aFound)
+                    bRel = m_bDestDirRelative;
+                else
+                    SAL_WARN("svx", "failed to replace destdir of '" << m_aDestDir << "' in '" << aPath
+                        << "'");
+            }*/
+
+            //rOStm.WriteBool(bRel);
+            aXmlWriter.startElement("bRel");
+            aXmlWriter.content(OString::boolean(bRel));
+            aXmlWriter.endElement();
+
+            //write_uInt16_lenPrefixed_uInt8s_FromOUString(rOStm, aPath, RTL_TEXTENCODING_UTF8);
+            aXmlWriter.startElement("Path");
+            aXmlWriter.content(OUStringToOString(aPath, RTL_TEXTENCODING_UTF8));
+            aXmlWriter.endElement();
+
+            //rOStm.WriteUInt32(pObj->nOffset).WriteUInt16(static_cast<sal_uInt16>(pObj->eObjKind));
+            aXmlWriter.startElement("Offset");
+            aXmlWriter.content(OString::number(pObj->nOffset));
+            aXmlWriter.endElement();
+
+            aXmlWriter.startElement("Object-Kind");
+            aXmlWriter.content(OString::number(static_cast<sal_uInt16>(pObj->eObjKind)));
+            aXmlWriter.endElement();
+
+            aXmlWriter.endElement();
         }
+
+        //rOStm.WriteUInt32(rId).WriteBool(pThm->IsNameFromResource()); // From version 2 and up
+        aXmlWriter.startElement("Id");
+        aXmlWriter.content(OString::number(rId));
+        aXmlWriter.endElement();
+
+        aXmlWriter.startElement("IsNameFromResource");
+        aXmlWriter.content(OString::boolean(pThm->IsNameFromResource()));
+        aXmlWriter.endElement();
+
+        aXmlWriter.endElement();
+
+        aXmlWriter.endDocument();
     }
-}
-
-void GalleryXMLEngine::writeXML()
-{
-    OUString rStreamName = "file:///C:/Users/adith/Desktop/zip testing/writexmltest.xml";
-    std::unique_ptr<SvStream> pStream;
-    pStream.reset(new SvFileStream(rStreamName, StreamMode::STD_READWRITE | StreamMode::TRUNC));
-    tools::XmlWriter aXmlWriter(pStream.get());
-
-    if (!aXmlWriter.startDocument())
-    {
+    else
         SAL_WARN("svx", "failed to start document");
-        return;
-    }
 
-    aXmlWriter.startElement("theme");
-    aXmlWriter.startElement("name");
-    aXmlWriter.content(OString("Arrows"));
-    aXmlWriter.endElement();
-    aXmlWriter.endElement();
-    aXmlWriter.endDocument();
-}*/
+    pMemoryStream->Seek(0);
+
+    css::uno::Reference<css::io::XOutputStream> xOutputStream;
+    xOutputStream.set(
+        xWriteableZipStore->openStreamElement(sThemeName + ".xml", embed::ElementModes::READWRITE),
+        uno::UNO_QUERY);
+    uno::Reference<io::XInputStream> xInputStream
+        = new utl::OInputStreamWrapper(pMemoryStream.get());
+    comphelper::OStorageHelper::CopyInputToOutput(xInputStream, xOutputStream);
+
+    uno::Reference<embed::XTransactedObject> xTransact(xWriteableZipStore, uno::UNO_QUERY_THROW);
+    xTransact->commit();
+    xTransact.set(xWriteableZipStore, uno::UNO_QUERY_THROW);
+    xTransact->commit();
+
+    return *pMemoryStream;
+}
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
