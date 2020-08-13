b37653fdc74e05ff2f86dad39cc1878d9047580b
Noel Grandin <noel.grandin@collabora.co.uk>
2020-08-13 15:03:20 +0200

    improve comments around blocks which abort on UI thread event exceptions
    Change-Id: If3336b8a39e41d958de2e998e3d9467888f2df6b
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/100659
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

--- vcl/source/app/salusereventlist.cxx
+++ vcl/source/app/salusereventlist.cxx
@@ -103,6 +103,13 @@ bool SalUserEventList::DispatchUserEvents( bool bHandleAllCurrentEvents )
                 continue;
             }
 
+            /*
+            * Current policy is that scheduler tasks aren't allowed to throw an exception.
+            * Because otherwise the exception is caught somewhere totally unrelated.
+            * TODO Ideally we could capture a proper backtrace and feed this into breakpad,
+            *   which is do-able, but requires writing some assembly.
+            * See also Scheduler::ProcessTaskScheduling
+            */
 #ifndef IOS
             try
 #endif
