LibreOffice/core

commit c73b5e969b2f9abdb2b9191938ca30bec5af725d

SwFormatField::Modify no more (folded into SwClientNotify)
- refactor out field formatting
  * make direct calls explicit with new UpdateTextNode() call
  * try to bring at least some sanity to the event handler

Change-Id: I40a39ddf70476b7fa0bf5e157fb263e863bb1d20
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/87927
Tested-by: Jenkins
Reviewed-by: Björn Michaelsen <bjoern.michaelsen@libreoffice.org>

sw/source/core/txtnode/atrfld.cxx
@@ -226,25 +226,27 @@ void SwFormatField::SwClientNotify( const SwModify& rModify, const SfxHint& rHin
     if( !mpTextField )
         return;
 
-    const SwFieldHint* pHint = dynamic_cast<const SwFieldHint*>( &rHint );
-    if ( pHint )
+    if (const SwFieldHint* pFieldHint = dynamic_cast<const SwFieldHint*>( &rHint ))
     {
         // replace field content by text
-        SwPaM* pPaM = pHint->m_pPaM;
+        SwPaM* pPaM = pFieldHint->m_pPaM;
         SwDoc* pDoc = pPaM->GetDoc();
         const SwTextNode& rTextNode = mpTextField->GetTextNode();
         pPaM->GetPoint()->nNode = rTextNode;
         pPaM->GetPoint()->nContent.Assign( const_cast<SwTextNode*>(&rTextNode), mpTextField->GetStart() );
 
-        OUString const aEntry(mpField->ExpandField(pDoc->IsClipBoard(), pHint->m_pLayout));
+        OUString const aEntry(mpField->ExpandField(pDoc->IsClipBoard(), pFieldHint->m_pLayout));
         pPaM->SetMark();
         pPaM->Move( fnMoveForward );
         pDoc->getIDocumentContentOperations().DeleteRange( *pPaM );
         pDoc->getIDocumentContentOperations().InsertString( *pPaM, aEntry );
+    } else if (const sw::LegacyModifyHint* pLegacyHint = dynamic_cast<const sw::LegacyModifyHint*>( &rHint ))
+    {
+        UpdateTextNode(pLegacyHint->m_pOld, pLegacyHint->m_pNew);
     }
 }
 
-void SwFormatField::Modify( const SfxPoolItem* pOld, const SfxPoolItem* pNew )
+void SwFormatField::UpdateTextNode(const SfxPoolItem* pOld, const SfxPoolItem* pNew)
 {
     if (pOld && (RES_REMOVE_UNO_OBJECT == pOld->Which()))
     {   // invalidate cached UNO object
@@ -263,59 +265,80 @@ void SwFormatField::Modify( const SfxPoolItem* pOld, const SfxPoolItem* pNew )
 
     SwTextNode* pTextNd = &mpTextField->GetTextNode();
     OSL_ENSURE( pTextNd, "Where is my Node?" );
-    if( pNew )
+
+    bool bTriggerNode = false;
+    bool bExpand = false;
+    const SfxPoolItem* pNodeOld = nullptr;
+    const SfxPoolItem* pNodeNew = nullptr;
+    if(pNew)
     {
-        switch( pNew->Which() )
+        switch(pNew->Which())
         {
         case RES_REFMARKFLD_UPDATE:
-                // update GetRef fields
-                if( SwFieldIds::GetRef == mpField->GetTyp()->Which() )
-                {
-                    // #i81002#
-                    static_cast<SwGetRefField*>(mpField.get())->UpdateField( mpTextField );
-                }
-                break;
+            // update GetRef fields
+            if( SwFieldIds::GetRef == mpField->GetTyp()->Which() )
+            {
+                // #i81002#
+                static_cast<SwGetRefField*>(mpField.get())->UpdateField( mpTextField );
+            }
+            break;
         case RES_DOCPOS_UPDATE:
-                // handled in SwTextFrame::Modify()
-                pTextNd->ModifyNotification( pNew, this );
-                return;
-
+            // handled in SwTextFrame::Modify()
+            bTriggerNode = true;
+            pNodeOld = pNew;
+            pNodeNew = this;
+            break;
         case RES_ATTRSET_CHG:
         case RES_FMT_CHG:
-                pTextNd->ModifyNotification( pOld, pNew );
-                return;
+            bTriggerNode = true;
+            pNodeOld = pOld;
+            pNodeNew = pNew;
+            break;
         default:
-                break;
+            break;
         }
     }
-
-    switch (mpField->GetTyp()->Which())
+    if(!bTriggerNode)
     {
-        case SwFieldIds::HiddenPara:
-            if( !pOld || RES_HIDDENPARA_PRINT != pOld->Which() )
-                break;
-            [[fallthrough]];
-        case SwFieldIds::DbSetNumber:
-        case SwFieldIds::DbNumSet:
-        case SwFieldIds::DbNextSet:
-        case SwFieldIds::DatabaseName:
-            pTextNd->ModifyNotification( nullptr, pNew);
-            return;
-        default: break;
-    }
-
-    if( SwFieldIds::User == mpField->GetTyp()->Which() )
-    {
-        SwUserFieldType* pType = static_cast<SwUserFieldType*>(mpField->GetTyp());
-        if(!pType->IsValid())
+        switch (mpField->GetTyp()->Which())
         {
-            SwCalc aCalc( *pTextNd->GetDoc() );
-            pType->GetValue( aCalc );
+            case SwFieldIds::HiddenPara:
+                if( !pOld || pOld->Which() != RES_HIDDENPARA_PRINT ) {
+                    bExpand =true;
+                    break;
+                }
+                [[fallthrough]];
+            case SwFieldIds::DbSetNumber:
+            case SwFieldIds::DbNumSet:
+            case SwFieldIds::DbNextSet:
+            case SwFieldIds::DatabaseName:
+                bTriggerNode = true;
+                pNodeNew = pNew;
+                break;
+            case SwFieldIds::User:
+            {
+                SwUserFieldType* pType = static_cast<SwUserFieldType*>(mpField->GetTyp());
+                if(!pType->IsValid())
+                {
+                    SwCalc aCalc( *pTextNd->GetDoc() );
+                    pType->GetValue( aCalc );
+                }
+                bExpand = true;
+            }
+            break;
+            default:
+                bExpand = true;
+                break;
         }
     }
-
-    const bool bForceNotify = (pOld == nullptr) && (pNew == nullptr);
-    mpTextField->ExpandTextField( bForceNotify );
+    if(bTriggerNode)
+    {
+        pTextNd->ModifyNotification(pNodeOld, pNodeNew);
+    }
+    if(bExpand)
+    {
+        mpTextField->ExpandTextField( pOld == nullptr && pNew == nullptr );
+    }
 }
 
 bool SwFormatField::GetInfo( SfxPoolItem& rInfo ) const

sw/inc/fmtfld.hxx
@@ -47,7 +47,6 @@ class SW_DLLPUBLIC SwFormatField final
     std::unique_ptr<SwField> mpField;
     SwTextField* mpTextField; // the TextAttribute
 
-    virtual void Modify( const SfxPoolItem* pOld, const SfxPoolItem *pNew) override;
     virtual void SwClientNotify( const SwModify& rModify, const SfxHint& rHint ) override;
 
 public:
@@ -105,6 +104,8 @@ public:
     SAL_DLLPRIVATE void SetXTextField(css::uno::Reference<css::text::XTextField> const& xTextField)
             { m_wXTextField = xTextField; }
     void dumpAsXml(xmlTextWriterPtr pWriter) const override;
+
+    void UpdateTextNode(const SfxPoolItem* pOld, const SfxPoolItem* pNew);
 };
 
 enum class SwFormatFieldHintWhich

sw/source/core/doc/DocumentFieldsManager.cxx
@@ -601,7 +601,7 @@ bool DocumentFieldsManager::UpdateField(SwTextField * pDstTextField, SwField & r
             [[fallthrough]];
 
         default:
-            pDstFormatField->ModifyNotification( nullptr, pMsgHint );
+            pDstFormatField->UpdateTextNode(nullptr, pMsgHint);
         }
 
         // The fields we can calculate here are being triggered for an update
@@ -798,7 +798,7 @@ void DocumentFieldsManager::UpdateTableFields( SfxPoolItem* pHt )
                     }
                     pCalc->SetCalcError( SwCalcError::NONE );
                 }
-                pFormatField->ModifyNotification( nullptr, pHt );
+                pFormatField->UpdateTextNode(nullptr, pHt);
         }
     }
 
@@ -1282,7 +1282,7 @@ void DocumentFieldsManager::UpdateExpFieldsImpl(
                         pInputField->UnlockNotifyContentChange();
                     }
                 });
-            pFormatField->ModifyNotification(nullptr, nullptr); // trigger formatting
+            pFormatField->UpdateTextNode(nullptr, nullptr); // trigger formatting
         }
 
         if (pUpdateField == pTextField) // if only this one is updated
@@ -1537,7 +1537,7 @@ void DocumentFieldsManager::SetFixFields( const DateTime* pNewDateTime )
 
                 // Trigger formatting
                 if( bChgd )
-                    pFormatField->ModifyNotification( nullptr, nullptr );
+                    pFormatField->UpdateTextNode(nullptr, nullptr);
             }
         }
     }

sw/source/core/doc/docfld.cxx
@@ -940,7 +940,7 @@ void SwDocUpdateField::MakeFieldList_( SwDoc& rDoc, int eGetMode )
                         // evaluate field
                         const_cast<SwHiddenTextField*>(static_cast<const SwHiddenTextField*>(pField))->Evaluate(&rDoc);
                         // trigger formatting
-                        const_cast<SwFormatField*>(pFormatField)->ModifyNotification( nullptr, nullptr );
+                        const_cast<SwFormatField*>(pFormatField)->UpdateTextNode(nullptr, nullptr);
                     }
                     break;
 

sw/source/core/fields/ddefld.cxx
@@ -124,7 +124,7 @@ public:
                     else if( pSh )
                         pSh->StartAction();
                 }
-                pLast->ModifyNotification( nullptr, &aUpdateDDE );
+                static_cast<SwFormatField*>(pLast)->UpdateTextNode( nullptr, &aUpdateDDE );
                 bCallModify = true;
             }
         }

commit 5db47f51b49c5eb080183c58aba0594c10a177f3

tdf#135457 sw: fix invalidation of SwXTextField
This was supposed to happen from SwNodes::ChgNode() when moving to the
undo nodes array.

There are fields with a range that contains other fields, when the outer
field's range is deleted, the contained field moves to the undo array
and its position is no longer valid to be given out by
SwXTextField::getAnchor().

(regression from c73b5e969b2f9abdb2b9191938ca30bec5af725d)

Change-Id: I38c8b3393a777ca8dc8739b5ca4233a7f124961e

sw/source/core/txtnode/atrfld.cxx
@@ -217,7 +217,7 @@ void SwFormatField::InvalidateField()
 {
     SwPtrMsgPoolItem const item(RES_REMOVE_UNO_OBJECT,
             &static_cast<SwModify&>(*this)); // cast to base class (void*)
-    NotifyClients(&item, &item);
+    CallSwClientNotify(sw::LegacyModifyHint{ &item, &item });
 }
 
 void SwFormatField::SwClientNotify( const SwModify& rModify, const SfxHint& rHint )

sw/source/core/undo/undobj.cxx
@@ -868,6 +868,10 @@ void SwUndoSaveContent::DelContentIndex( const SwPosition& rMark,
 
     SwDoc* pDoc = rMark.nNode.GetNode().GetDoc();
 
+    // if it's not in the doc array, probably missing some invalidation somewhere
+    assert(&rPoint.nNode.GetNodes() == &pDoc->GetNodes());
+    assert(&rMark.nNode.GetNodes() == &pDoc->GetNodes());
+
     ::sw::UndoGuard const undoGuard(pDoc->GetIDocumentUndoRedo());
 
     // 1. Footnotes