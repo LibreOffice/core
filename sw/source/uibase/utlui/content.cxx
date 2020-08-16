b8fb1b7c6065bc28f8db544fb1f8e8d551446f3e
Jim Raykowski <raykowj@gmail.com>
2020-08-16 11:23:57 -0800

    Improve location of unfold fold code use in outline move
    Change-Id: I299f871c4d6e06f63efaf80e04cb668c52ae8543

--- sw/source/uibase/utlui/content.cxx
+++ sw/source/uibase/utlui/content.cxx
@@ -2772,7 +2772,6 @@ void SwContentTree::ExecCommand(const OString& rCmd, bool bOutlineWithChildren)
 
     SwOutlineNodes::difference_type nDirLast = bUp ? -1 : 1;
     bool bStartedAction = false;
-    std::vector<SwNode*> aFoldedOutlineNdsArray;
     for (auto const& pCurrentEntry : selected)
     {
         assert(pCurrentEntry && lcl_IsContent(*pCurrentEntry, *m_xTreeView));
@@ -2794,28 +2793,6 @@ void SwContentTree::ExecCommand(const OString& rCmd, bool bOutlineWithChildren)
         if (!bStartedAction)
         {
             pShell->StartAllAction();
-            if (bUpDown)
-            {
-                if (pShell->GetViewOptions()->IsShowOutlineContentVisibilityButton())
-                {
-                    // unfold all folded outline content
-                    SwOutlineNodes rOutlineNds = pShell->GetDoc()->GetNodes().GetOutLineNds();
-                    for (SwOutlineNodes::size_type nPos = 0; nPos < rOutlineNds.size(); ++nPos)
-                    {
-                        SwNode* pNd = rOutlineNds[nPos];
-                        if (pNd->IsTextNode()) // should always be true
-                        {
-                            bool bOutlineContentVisibleAttr = true;
-                            pNd->GetTextNode()->GetAttrOutlineContentVisible(bOutlineContentVisibleAttr);
-                            if (!bOutlineContentVisibleAttr)
-                            {
-                                aFoldedOutlineNdsArray.push_back(pNd);
-                                pShell->ToggleOutlineContentVisibility(nPos);
-                            }
-                        }
-                    }
-                }
-            }
             pShell->StartUndo(bLeftRight ? SwUndoId::OUTLINE_LR : SwUndoId::OUTLINE_UD);
             bStartedAction = true;
         }
@@ -2987,15 +2964,6 @@ void SwContentTree::ExecCommand(const OString& rCmd, bool bOutlineWithChildren)
     if (bStartedAction)
     {
         pShell->EndUndo();
-        if (bUpDown)
-        {
-            if (pShell->GetViewOptions()->IsShowOutlineContentVisibilityButton())
-            {
-                // fold all outlines that were folded before chapter promote/demote
-                for (SwNode* pNd : aFoldedOutlineNdsArray)
-                    pShell->ToggleOutlineContentVisibility(pNd, true);
-            }
-        }
         pShell->EndAllAction();
         if (m_aActiveContentArr[ContentTypeId::OUTLINE])
             m_aActiveContentArr[ContentTypeId::OUTLINE]->Invalidate();
@@ -3171,27 +3139,6 @@ void SwContentTree::MoveOutline(SwOutlineNodes::size_type nTargetPos)
 {
     SwWrtShell *const pShell = GetWrtShell();
     pShell->StartAllAction();
-    std::vector<SwNode*> aFoldedOutlineNdsArray;
-
-    if (pShell->GetViewOptions()->IsShowOutlineContentVisibilityButton())
-    {
-        // unfold all folded outline content
-        SwOutlineNodes rOutlineNds = pShell->GetDoc()->GetNodes().GetOutLineNds();
-        for (SwOutlineNodes::size_type nPos = 0; nPos < rOutlineNds.size(); ++nPos)
-        {
-            SwNode* pNd = rOutlineNds[nPos];
-            if (pNd->IsTextNode()) // should always be true
-            {
-                bool bOutlineContentVisibleAttr = true;
-                pNd->GetTextNode()->GetAttrOutlineContentVisible(bOutlineContentVisibleAttr);
-                if (!bOutlineContentVisibleAttr)
-                {
-                    aFoldedOutlineNdsArray.push_back(pNd);
-                    pShell->ToggleOutlineContentVisibility(nPos);
-                }
-            }
-        }
-    }
     pShell->StartUndo(SwUndoId::OUTLINE_UD);
 
     SwOutlineNodes::size_type nPrevSourcePos = SwOutlineNodes::npos;
@@ -3242,12 +3189,6 @@ void SwContentTree::MoveOutline(SwOutlineNodes::size_type nTargetPos)
     }
 
     pShell->EndUndo();
-    if (pShell->GetViewOptions()->IsShowOutlineContentVisibilityButton())
-    {
-        // fold all outlines that were folded before chapter promote/demote
-        for (SwNode* pNd : aFoldedOutlineNdsArray)
-            pShell->ToggleOutlineContentVisibility(pNd, true);
-    }
     pShell->EndAllAction();
     m_aActiveContentArr[ContentTypeId::OUTLINE]->Invalidate();
     Display(true);
