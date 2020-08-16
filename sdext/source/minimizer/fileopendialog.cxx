LibreOffice/core

commit a0dcc8f78061d22d925f31ef8d03ca751ad6dc19

Resolves: #i121857# Presentation Minimizer: fix filter naming
(cherry picked from commit 75833fdb6ede664fdcef38a16ecf1f8d3f3adc7d)

Conflicts:
	sdext/source/minimizer/fileopendialog.cxx

Change-Id: I164cc6d9916ddf9dba9da273b5181e5728127292

sdext/source/minimizer/fileopendialog.cxx
@@ -46,6 +46,7 @@
 #include <com/sun/star/view/XControlAccess.hpp>
 #include <com/sun/star/ucb/InteractiveAugmentedIOException.hpp>
 
+#include <rtl/ustrbuf.hxx>
 
 using namespace ::rtl;
 using namespace ::com::sun::star::uno;
@@ -140,7 +141,27 @@ FileOpenDialog::FileOpenDialog( const Reference< XComponentContext >& rxContext 
                 }
                 if ( aExtensions.getLength() )
                 {
-                    mxFilePicker->appendFilter( aIter->maUIName, aExtensions[ 0 ] );
+                    OUString aExtension = aExtensions[0];
+
+                    const char filter[] = "*.";
+                    // the filter title must be formed in the same it is currently done
+                    // in the internal implementation: "UIName (.<extension>)"
+                    OUStringBuffer aUIName;
+                    // the filter must be in the form "*.<extension>"
+                    OUStringBuffer aFilter;
+
+                    // form the title: "<UIName> (.<extension)"
+                    aUIName.append( aIter->maUIName );
+                    aUIName.appendAscii( RTL_CONSTASCII_STRINGPARAM( " (." ));
+                    aUIName.append( aExtension );
+                    aUIName.append( sal_Unicode( ')' ) );
+                    // form the filter: "(*.<extension>)"
+                    aFilter.appendAscii( RTL_CONSTASCII_STRINGPARAM( filter ) );
+                    aFilter.append( aExtensions[0] );
+
+                    mxFilePicker->appendFilter( aUIName.makeStringAndClear(),
+                                                  aFilter.makeStringAndClear() );
+
                     if ( aIter->maFlags & 0x100 )
                         mxFilePicker->setCurrentFilter( aIter->maUIName );
                 }

commit 381d0155fc51a41c91c9e3b178f0b59f0c65a357

tdf#135342: filter list must include extensions
Regression after commit a0dcc8f78061d22d925f31ef8d03ca751ad6dc19.
The commit had added extensions to the filter names sent to file
dialog, but kept internal list of filter names aFilterEntryList
without extensions. Then dialog returned the chosen filter's name
in the form it was sent there, i.e. with extension; and that name
couldn't be found in aFilterEntryList.

So let's keep aFilterEntryList synchronized with the UI names in
the dialog.

Change-Id: Ic21d19d1a8d0b7f64ccfb517a7d0ee8ab36fc6a8

sdext/source/minimizer/fileopendialog.cxx
@@ -106,7 +106,7 @@ FileOpenDialog::FileOpenDialog( const Reference< XComponentContext >& rxContext 
     Reference< XNameAccess > xTypes( rxContext->getServiceManager()->createInstanceWithContext(
         "com.sun.star.document.TypeDetection", rxContext ), UNO_QUERY_THROW );
 
-    for( const auto& rFilterEntry : aFilterEntryList )
+    for (auto& rFilterEntry : aFilterEntryList)
     {
         Sequence< PropertyValue > aTypeProperties;
         try
@@ -120,10 +120,12 @@ FileOpenDialog::FileOpenDialog( const Reference< XComponentContext >& rxContext 
                     pProp->Value >>= aExtensions;
                 if ( aExtensions.hasElements() )
                 {
-                    // The filter title must be formed in the same way it is
-                    // currently done in the internal implementation:
+                    // The filter title must be formed in the same way it is currently done in the
+                    // internal implementation (see sfx2::appendFiltersForSave). And we will look
+                    // for the same string returned from the dialog, so save it to maUIName:
                     OUString aTitle(
                         rFilterEntry.maUIName + " (." + aExtensions[0] + ")");
+                    rFilterEntry.maUIName = aTitle;
                     OUString aFilter("*." + aExtensions[0]);
                     mxFilePicker->appendFilter(aTitle, aFilter);
                     if ( rFilterEntry.maFlags & 0x100 )