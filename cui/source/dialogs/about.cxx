37f452bc2beeae9b4648013a3bf4f2e8b56c0c85
Mike Kaganski <mike.kaganski@collabora.com>
2020-08-18 22:23:30 +0300

    tdf#135133: Don't try to read BuildVersion
    The option to set it was removed in 5fdf2009d21fa220dfee70ea755bd698c16257a7,
    and now the check is redundant - and even harmful until the remnants of the
    setting is dropped from bootstrap.ini. Currently the dummy value found there
    makes the About dialog miss the build id, and copying info to clipboard gives
    <buildversion> instead of proper build id. Previously the string was always
    empty in most builds (including TDF), which made the code proceed to buildid.
    
    Bootstrap::getBuildVersion got unused now, and is removed.
    
    Change-Id: If59e456655da29bb9421edc0e15d421829d0a02b
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/100957
    Tested-by: Mike Kaganski <mike.kaganski@collabora.com>
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/100975
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    (cherry picked from commit 0a796797ddfe2d1913ac91908074c397cf7c9c82)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/101056
    Reviewed-by: Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>
    Tested-by: Stephan Bergmann <sbergman@redhat.com>

--- cui/source/dialogs/about.cxx
+++ cui/source/dialogs/about.cxx
@@ -19,7 +19,6 @@
 
 #include <about.hxx>
 
-#include <osl/diagnose.h>    //OSL_ENSURE
 #include <osl/process.h>     //osl_getProcessLocale
 #include <rtl/character.hxx> //rtl::isAsciiHexDigit
 #include <sal/log.hxx>       //SAL_WARN
@@ -34,7 +33,7 @@
 #include <sfx2/app.hxx> //SfxApplication::loadBrandSvg
 #include <strings.hrc>
 #include <svtools/langhelp.hxx>
-#include <unotools/bootstrap.hxx> //utl::Bootstrap::getBuildVersion
+#include <unotools/bootstrap.hxx> //utl::Bootstrap::getBuildIdData
 #include <unotools/configmgr.hxx> //ConfigManager::
 
 #include <com/sun/star/datatransfer/clipboard/SystemClipboard.hpp>
@@ -155,16 +154,10 @@ OUString AboutDialog::GetVersionString() {
   return sVersion;
 }
 
-OUString AboutDialog::GetBuildString() {
-
-  OUString sDefault;
-  OUString sBuildId(utl::Bootstrap::getBuildVersion(sDefault));
-  if (sBuildId.isEmpty())
-    sBuildId = utl::Bootstrap::getBuildIdData(sDefault);
-  if (sBuildId.isEmpty()) {
-    sBuildId = sBuildId.getToken(0, '-');
-  }
-  OSL_ENSURE(!sBuildId.isEmpty(), "No BUILDID in bootstrap file");
+OUString AboutDialog::GetBuildString()
+{
+  OUString sBuildId(utl::Bootstrap::getBuildIdData(""));
+  SAL_WARN_IF(!sBuildId.isEmpty(), "cui.dialogs", "No BUILDID in bootstrap file");
 
   return sBuildId;
 }
