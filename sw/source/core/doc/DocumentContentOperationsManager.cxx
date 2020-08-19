ec6a8f9594a8c2cfeee6ded9a611593487c1c8d4
Michael Stahl <Michael.Stahl@cib.de>
2020-08-19 16:44:09 +0200

    tdf#135412 tdf#135888 sw: fix copying of linked text-boxes
    The resetLink()/restoreLinks() were added in commit
    00a007be5ad88bac9905b373bc5e02d02acab11a because testMissingPath
    missing-path.docx was crashing.
    
    But then 0bcc5b3daebeb2a7d2b5ba132af4745cc6c78cd0 refactored how linking
    works and introduced the isTextBox function, which is called in the
    middle of DocumentContentOperationsManager::CopyFlyInFlyImpl() after
    resetLink(), and this now always returns false, the same for another
    call inside CopyLayoutFormat() (when called from CopyFlyInFlyImpl()),
    which causes text-boxes to be copied to 2 separate flys (tdf#135888).
    
    The problem in tdf#135412 is that somehow when called from
    SwFEShell::Paste() the content-index from the clipboard document ends up
    in the SwDrawFrameFormat that is created in the target document,
    and this causes crash in Undo because the node index is out of bounds.
    
     10 SwUndoInsLayFormat::UndoImpl (this=0x7c2a760, rContext=...) at sw/source/core/undo/undobj1.cxx:310
    (rr) p rContent.GetContentIdx()->GetNode().GetDoc()->IsClipBoard()
    (rr) $29 = true
    
    It turns out that missing-path.docx doesn't crash any more without
    resetLink(), and removing it fixes the 2 bugs.
    
    Change-Id: I0c6c91a42e00b9f3b79b774c814e7323f2bb3e05
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/101004
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@cib.de>
    (cherry picked from commit 10ae7ba661dff57a7d08174792565ec5e33fae9b)

--- sw/source/core/doc/DocumentContentOperationsManager.cxx
+++ sw/source/core/doc/DocumentContentOperationsManager.cxx
@@ -3595,7 +3595,6 @@ void DocumentContentOperationsManager::CopyFlyInFlyImpl(
 
     SwTextBoxHelper::SavedLink aOldTextBoxes;
     SwTextBoxHelper::saveLinks(*m_rDoc.GetSpzFrameFormats(), aOldTextBoxes);
-    SwTextBoxHelper::SavedContent aOldContent;
 
     for ( size_t n = 0; n < nArrLen; ++n )
     {
@@ -3662,9 +3661,6 @@ void DocumentContentOperationsManager::CopyFlyInFlyImpl(
         }
         if( bAdd )
         {
-            // Make sure draw formats don't refer to content, so that such
-            // content can be removed without problems.
-            SwTextBoxHelper::resetLink(pFormat, aOldContent);
             aSet.insert( ZSortFly( pFormat, pAnchor, nArrLen + aSet.size() ));
         }
     }
@@ -3834,7 +3830,7 @@ void DocumentContentOperationsManager::CopyFlyInFlyImpl(
         // Re-create content property of draw formats, knowing how old shapes
         // were paired with old fly formats (aOldTextBoxes) and that aSet is
         // parallel with aVecSwFrameFormat.
-        SwTextBoxHelper::restoreLinks(aSet, aVecSwFrameFormat, aOldTextBoxes, aOldContent);
+        SwTextBoxHelper::restoreLinks(aSet, aVecSwFrameFormat, aOldTextBoxes);
     }
 }
 
