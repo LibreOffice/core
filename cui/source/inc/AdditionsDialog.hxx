48a796c5bbdf897228eb4125512e4f80527f4b55
Yusuf Keten <ketenyusuf@gmail.com>
2020-08-19 19:36:58 +0300

    tdf#133026: Additions: Rating image implementation
    Change-Id: Id250f0e70490941743e0f3ca2b94897f69a9208f
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/101013
    Tested-by: Jenkins
    Reviewed-by: Muhammet Kara <muhammet.kara@collabora.com>

--- cui/source/inc/AdditionsDialog.hxx
+++ cui/source/inc/AdditionsDialog.hxx
@@ -126,7 +126,12 @@ public:
     std::unique_ptr<weld::Label> m_xLabelVersion;
     std::unique_ptr<weld::Label> m_xLabelComments;
     std::unique_ptr<weld::LinkButton> m_xLinkButtonComments;
-    std::unique_ptr<weld::Image> m_xImageVoting;
+    std::unique_ptr<weld::Image> m_xImageVoting1;
+    std::unique_ptr<weld::Image> m_xImageVoting2;
+    std::unique_ptr<weld::Image> m_xImageVoting3;
+    std::unique_ptr<weld::Image> m_xImageVoting4;
+    std::unique_ptr<weld::Image> m_xImageVoting5;
+    std::unique_ptr<weld::Label> m_xLabelNoVoting;
     std::unique_ptr<weld::Image> m_xImageDownloadNumber;
     std::unique_ptr<weld::Label> m_xLabelDownloadNumber;
     std::unique_ptr<weld::Button> m_xButtonShowMore;
