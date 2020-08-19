3e37f3db69c3c7e4b824fba7751cf6769d594984
Stephan Bergmann <sbergman@redhat.com>
2020-08-19 10:13:17 +0200

    Remove unused include
    Change-Id: I019148e0823e68ccbb1cf60f6eac9e69b634515b
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/100973
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

--- compilerplugins/clang/test/stringstatic.cxx
+++ compilerplugins/clang/test/stringstatic.cxx
@@ -7,7 +7,6 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-#include <rtl/string.hxx>
 #include <rtl/ustring.hxx>
 
 // expected-error@+1 {{rather declare this using OUStringLiteral/OStringLiteral/char[] [loplugin:stringstatic]}}
