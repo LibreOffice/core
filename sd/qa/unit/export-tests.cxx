LibreOffice/core

commit 450cd772aa734cfcb989c8cedd3c0a454db74a34

Fix fdo#64512 Handle xml:id correctly on multi-image draw:frames
Fixes a regression from the pick-best-image from draw:frame in ODF,
where before sometimes the XShape got deleted that the
UnoInterfaceToUniqueIdentifierMapper::registerReference stored.

For that, added a
UnoInterfaceToUniqueIdentifierMapper::registerReferenceAlways
function, which overwrites potentially existing earlier entries
with the same identifier string.

This fix was originally much more messy, but then dtardon committed
30b248dfe5bfb8a0649e36f22c943b3feb2f1385 which also fixes this here
bug. Now only sneaking in slightly less involved interface map
handling and a safeguard in ximpshap.cxx.

Change-Id: I87501e43518a5fc2fee166c45a4e2f01718f5228

include/xmloff/unointerfacetouniqueidentifiermapper.hxx
@@ -24,7 +24,6 @@
 #include "xmloff/dllapi.h"
 #include "sal/types.h"
 
-#include <deque>
 #include <map>
 #include <rtl/ustring.hxx>
 #include <com/sun/star/uno/XInterface.hpp>
@@ -32,12 +31,10 @@
 namespace comphelper
 {
 
-typedef ::std::map< OUString, const ::com::sun::star::uno::Reference< ::com::sun::star::uno::XInterface > > IdMap_t;
+typedef ::std::map< OUString, ::com::sun::star::uno::Reference< ::com::sun::star::uno::XInterface > > IdMap_t;
 
 class XMLOFF_DLLPUBLIC UnoInterfaceToUniqueIdentifierMapper
 {
-    typedef std::deque< rtl::OUString > Reserved_t;
-
 public:
     UnoInterfaceToUniqueIdentifierMapper();
     ~UnoInterfaceToUniqueIdentifierMapper();
@@ -54,16 +51,12 @@ public:
     */
     bool registerReference( const OUString& rIdentifier, const ::com::sun::star::uno::Reference< ::com::sun::star::uno::XInterface >& rInterface );
 
-    /** reserves an identifier for later registration.
+    /** always registers the given uno object with the given identifier.
 
-        @returns
-            false, if the identifier already exists
-      */
-    bool reserveIdentifier( const rtl::OUString& rIdentifier );
-
-    /** registers the given uno object with reserved identifier.
-      */
-    bool registerReservedReference( const rtl::OUString& rIdentifier, const com::sun::star::uno::Reference< com::sun::star::uno::XInterface >& rInterface );
+        In contrast to registerReference(), this here overwrites any
+        earlier registration of the same identifier
+    */
+    void registerReferenceAlways( const rtl::OUString& rIdentifier, const ::com::sun::star::uno::Reference< ::com::sun::star::uno::XInterface >& rInterface );
 
     /** @returns
             the identifier for the given uno object. If this uno object is not already
@@ -80,12 +73,10 @@ public:
 private:
     bool findReference( const ::com::sun::star::uno::Reference< ::com::sun::star::uno::XInterface >& rInterface, IdMap_t::const_iterator& rIter ) const;
     bool findIdentifier( const OUString& rIdentifier, IdMap_t::const_iterator& rIter ) const;
-    bool findReserved( const OUString& rIdentifier ) const;
-    bool findReserved( const OUString& rIdentifier, Reserved_t::const_iterator& rIter ) const;
+    void insertReference( const OUString& rIdentifier, const ::com::sun::star::uno::Reference< ::com::sun::star::uno::XInterface >& rInterface );
 
     IdMap_t	maEntries;
     sal_Int32 mnNextId;
-    Reserved_t maReserved;
 };
 
 }

xmloff/source/core/unointerfacetouniqueidentifiermapper.cxx
@@ -17,8 +17,6 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#include <algorithm>
-
 #include <xmloff/unointerfacetouniqueidentifiermapper.hxx>
 
 using namespace ::com::sun::star;
@@ -72,42 +70,25 @@ bool UnoInterfaceToUniqueIdentifierMapper::registerReference( const OUString& rI
     {
         return rIdentifier != (*aIter).first;
     }
-    else if( findIdentifier( rIdentifier, aIter ) || findReserved( rIdentifier ) )
+    else if( findIdentifier( rIdentifier, aIter ) )
     {
         return false;
     }
     else
     {
-        maEntries.insert( IdMap_t::value_type( rIdentifier, xRef ) );
-
-        // see if this is a reference like something we would generate in the future
-        const sal_Unicode *p = rIdentifier.getStr();
-        sal_Int32 nLength = rIdentifier.getLength();
-
-        // see if the identifier is 'id' followed by a pure integer value
-        if( nLength < 2 || p[0] != 'i' || p[1] != 'd' )
-            return true;
-
-        nLength -= 2;
-        p += 2;
-
-        while(nLength--)
-        {
-            if( (*p < '0') || (*p > '9') )
-                return true; // a custom id, that will never conflict with genereated id's
-
-            p++;
-        }
-
-        // the identifier is a pure integer value
-        // so we make sure we will never generate
-        // an integer value like this one
-        sal_Int32 nId = rIdentifier.copy(2).toInt32();
-        if( mnNextId <= nId )
-            mnNextId = nId + 1;
-
-        return true;
+        insertReference( rIdentifier, xRef );
     }
+
+    return true;
+}
+
+void UnoInterfaceToUniqueIdentifierMapper::registerReferenceAlways( const OUString& rIdentifier, const Reference< XInterface >& rInterface )
+{
+    // Be certain that the references we store in our table are to the
+    // leading / primary XInterface - cf. findReference
+    uno::Reference< uno::XInterface > xRef( rInterface, uno::UNO_QUERY );
+
+    insertReference( rIdentifier, xRef );
 }
 
 const OUString& UnoInterfaceToUniqueIdentifierMapper::getIdentifier( const Reference< XInterface >& rInterface ) const
@@ -166,42 +147,35 @@ bool UnoInterfaceToUniqueIdentifierMapper::findIdentifier( const OUString& rIden
     return rIter != maEntries.end();
 }
 
-bool UnoInterfaceToUniqueIdentifierMapper::reserveIdentifier( const rtl::OUString& rIdentifier )
+void UnoInterfaceToUniqueIdentifierMapper::insertReference( const OUString& rIdentifier, const Reference< XInterface >& rInterface )
 {
-    if ( findReserved( rIdentifier ) )
-        return false;
+    maEntries[rIdentifier] = rInterface;
 
-    maReserved.push_back( rIdentifier );
-    return true;
-}
+    // see if this is a reference like something we would generate in the future
+    const sal_Unicode *p = rIdentifier.getStr();
+    sal_Int32 nLength = rIdentifier.getLength();
 
-bool UnoInterfaceToUniqueIdentifierMapper::registerReservedReference(
-        const rtl::OUString& rIdentifier,
-        const com::sun::star::uno::Reference< com::sun::star::uno::XInterface >& rInterface )
-{
-    Reserved_t::const_iterator aIt;
-    if ( !findReserved( rIdentifier, aIt ) )
-        return false;
+    // see if the identifier is 'id' followed by a pure integer value
+    if( nLength < 2 || p[0] != 'i' || p[1] != 'd' )
+        return;
 
-    Reserved_t::iterator aRemoveIt( maReserved.begin() + ( aIt - maReserved.begin() ) );
-    maReserved.erase( aRemoveIt );
-    registerReference( rIdentifier, rInterface );
+    nLength -= 2;
+    p += 2;
 
-    return true;
-}
+    while(nLength--)
+    {
+        if( (*p < '0') || (*p > '9') )
+            return; // a custom id, that will never conflict with genereated id's
 
-bool UnoInterfaceToUniqueIdentifierMapper::findReserved( const OUString& rIdentifier ) const
-{
-    Reserved_t::const_iterator aDummy;
-    return findReserved( rIdentifier, aDummy );
-}
+        p++;
+    }
 
-bool UnoInterfaceToUniqueIdentifierMapper::findReserved(
-        const OUString& rIdentifier,
-        Reserved_t::const_iterator& rIter ) const
-{
-    rIter = std::find( maReserved.begin(), maReserved.end(), rIdentifier );
-    return rIter != maReserved.end();
+    // the identifier is a pure integer value
+    // so we make sure we will never generate
+    // an integer value like this one
+    sal_Int32 nId = rIdentifier.copy(2).toInt32();
+    if( mnNextId <= nId )
+        mnNextId = nId + 1;
 }
 
 }

xmloff/source/draw/ximpshap.cxx
@@ -3455,9 +3455,6 @@ SvXMLImportContext *SdXMLFrameShapeContext::CreateChildContext( sal_uInt16 nPref
 
         if(getSupportsMultipleContents() && dynamic_cast< SdXMLGraphicObjectShapeContext* >(pContext))
         {
-            if ( !maShapeId.isEmpty() )
-                GetImport().getInterfaceToIdentifierMapper().reserveIdentifier( maShapeId );
-
             addContent(*mxImplContext);
         }
     }
@@ -3534,11 +3531,13 @@ void SdXMLFrameShapeContext::EndElement()
     // solve if multiple image child contexts were imported
     const SvXMLImportContext* const pSelectedContext(solveMultipleImages());
     const SdXMLGraphicObjectShapeContext* pShapeContext( dynamic_cast<const SdXMLGraphicObjectShapeContext*>( pSelectedContext ) );
-    if ( pShapeContext )
+    if ( pShapeContext && !maShapeId.isEmpty() )
     {
+        // fdo#64512 and fdo#60075 - make sure *this* shape is
+        // registered for given ID
         assert( mxImplContext.Is() );
         const uno::Reference< uno::XInterface > xShape( pShapeContext->getShape() );
-        GetImport().getInterfaceToIdentifierMapper().registerReservedReference( maShapeId, xShape );
+        GetImport().getInterfaceToIdentifierMapper().registerReferenceAlways( maShapeId, xShape );
     }
 
     if( !mxImplContext.Is() )

commit f91edbd4e81d34a6f850f23bd9005dfc9859cc18

rhbz#1870501 crash on reexport of odg
where SdrObjects in a list have no navigation position set

a regression from

450cd772aa734cfcb989c8cedd3c0a454db74a34

    Fix fdo#64512 Handle xml:id correctly on multi-image draw:frames

this just reverts that and adds the test case that crashed

Change-Id: I1a49dab9578699c42fe845e8ec42de40159dec3d

include/xmloff/unointerfacetouniqueidentifiermapper.hxx
@@ -24,6 +24,7 @@
 #include <xmloff/dllapi.h>
 #include <sal/types.h>
 
+#include <deque>
 #include <map>
 #include <rtl/ustring.hxx>
 #include <com/sun/star/uno/XInterface.hpp>
@@ -35,6 +36,8 @@ typedef ::std::map< OUString, css::uno::Reference< css::uno::XInterface > > IdMa
 
 class XMLOFF_DLLPUBLIC UnoInterfaceToUniqueIdentifierMapper
 {
+    typedef std::deque< OUString > Reserved_t;
+
 public:
     UnoInterfaceToUniqueIdentifierMapper();
 
@@ -50,12 +53,16 @@ public:
     */
     bool registerReference( const OUString& rIdentifier, const css::uno::Reference< css::uno::XInterface >& rInterface );
 
-    /** always registers the given uno object with the given identifier.
+    /** reserves an identifier for later registration.
 
-        In contrast to registerReference(), this here overwrites any
-        earlier registration of the same identifier
-    */
-    void registerReferenceAlways( const OUString& rIdentifier, const css::uno::Reference< css::uno::XInterface >& rInterface );
+        @returns
+            false, if the identifier already exists
+      */
+    bool reserveIdentifier( const OUString& rIdentifier );
+
+    /** registers the given uno object with reserved identifier.
+      */
+    bool registerReservedReference( const OUString& rIdentifier, const css::uno::Reference< css::uno::XInterface >& rInterface );
 
     /** @returns
             the identifier for the given uno object. If this uno object is not already
@@ -72,10 +79,12 @@ public:
 private:
     bool findReference( const css::uno::Reference< css::uno::XInterface >& rInterface, IdMap_t::const_iterator& rIter ) const;
     bool findIdentifier( const OUString& rIdentifier, IdMap_t::const_iterator& rIter ) const;
-    void insertReference( const OUString& rIdentifier, const css::uno::Reference< css::uno::XInterface >& rInterface );
+    bool findReserved( const OUString& rIdentifier ) const;
+    bool findReserved( const OUString& rIdentifier, Reserved_t::const_iterator& rIter ) const;
 
     IdMap_t maEntries;
     sal_uInt32 mnNextId;
+    Reserved_t maReserved;
 };
 
 }

xmloff/source/core/unointerfacetouniqueidentifiermapper.cxx
@@ -21,6 +21,7 @@
 
 #include <o3tl/safeint.hxx>
 #include <xmloff/unointerfacetouniqueidentifiermapper.hxx>
+#include <algorithm>
 
 using namespace ::com::sun::star;
 using css::uno::Reference;
@@ -64,25 +65,41 @@ bool UnoInterfaceToUniqueIdentifierMapper::registerReference( const OUString& rI
     {
         return rIdentifier != (*aIter).first;
     }
-    else if( findIdentifier( rIdentifier, aIter ) )
+    else if( findIdentifier( rIdentifier, aIter ) || findReserved( rIdentifier ) )
     {
         return false;
     }
     else
     {
-        insertReference( rIdentifier, xRef );
+        maEntries.insert( IdMap_t::value_type( rIdentifier, xRef ) );
+
+        // see if this is a reference like something we would generate in the future
+        const sal_Unicode *p = rIdentifier.getStr();
+        sal_Int32 nLength = rIdentifier.getLength();
+
+        // see if the identifier is 'id' followed by a pure integer value
+        if( nLength < 2 || p[0] != 'i' || p[1] != 'd' )
+            return true;
+
+        nLength -= 2;
+        p += 2;
+
+        while(nLength--)
+        {
+            if( (*p < '0') || (*p > '9') )
+                return true; // a custom id, that will never conflict with genereated id's
+            p++;
+        }
+
+        // the identifier is a pure integer value
+        // so we make sure we will never generate
+        // an integer value like this one
+        sal_Int32 nId = rIdentifier.copy(2).toInt32();
+        if (nId > 0 && mnNextId <= o3tl::make_unsigned(nId))
+            mnNextId = nId + 1;
+
+        return true;
     }
-
-    return true;
-}
-
-void UnoInterfaceToUniqueIdentifierMapper::registerReferenceAlways( const OUString& rIdentifier, const Reference< XInterface >& rInterface )
-{
-    // Be certain that the references we store in our table are to the
-    // leading / primary XInterface - cf. findReference
-    uno::Reference< uno::XInterface > xRef( rInterface, uno::UNO_QUERY );
-
-    insertReference( rIdentifier, xRef );
 }
 
 const OUString& UnoInterfaceToUniqueIdentifierMapper::getIdentifier( const Reference< XInterface >& rInterface ) const
@@ -135,38 +152,42 @@ bool UnoInterfaceToUniqueIdentifierMapper::findIdentifier( const OUString& rIden
     return rIter != maEntries.end();
 }
 
-void UnoInterfaceToUniqueIdentifierMapper::insertReference( const OUString& rIdentifier, const Reference< XInterface >& rInterface )
+bool UnoInterfaceToUniqueIdentifierMapper::reserveIdentifier( const OUString& rIdentifier )
 {
-    maEntries[rIdentifier] = rInterface;
+    if ( findReserved( rIdentifier ) )
+        return false;
 
-    // see if this is a reference like something we would generate in the future
-    const sal_Unicode *p = rIdentifier.getStr();
-    sal_Int32 nLength = rIdentifier.getLength();
+    maReserved.push_back( rIdentifier );
+    return true;
+}
 
-    // see if the identifier is 'id' followed by a pure integer value
-    if( nLength < 2 || p[0] != 'i' || p[1] != 'd' )
-        return;
+bool UnoInterfaceToUniqueIdentifierMapper::registerReservedReference(
+        const OUString& rIdentifier,
+        const css::uno::Reference< css::uno::XInterface >& rInterface )
+{
+    Reserved_t::const_iterator aIt;
+    if ( !findReserved( rIdentifier, aIt ) )
+        return false;
 
-    nLength -= 2;
-    p += 2;
+    Reserved_t::iterator aRemoveIt( maReserved.begin() + ( aIt - maReserved.begin() ) );
+    maReserved.erase( aRemoveIt );
+    registerReference( rIdentifier, rInterface );
 
-    while(nLength--)
-    {
-        if( (*p < '0') || (*p > '9') )
-            return; // a custom id, that will never conflict with generated id's
+    return true;
+}
 
-        p++;
-    }
+bool UnoInterfaceToUniqueIdentifierMapper::findReserved( const OUString& rIdentifier ) const
+{
+    Reserved_t::const_iterator aDummy;
+    return findReserved( rIdentifier, aDummy );
+}
 
-    // the identifier is a pure integer value
-    // so we make sure we will never generate
-    // an integer value like this one
-    sal_Int32 nId = rIdentifier.copy(2).toInt32();
-    if (nId > 0 && mnNextId <= o3tl::make_unsigned(nId))
-    {
-        mnNextId = nId;
-        ++mnNextId;
-    }
+bool UnoInterfaceToUniqueIdentifierMapper::findReserved(
+        const OUString& rIdentifier,
+        Reserved_t::const_iterator& rIter ) const
+{
+    rIter = std::find( maReserved.begin(), maReserved.end(), rIdentifier );
+    return rIter != maReserved.end();
 }
 
 }

xmloff/source/draw/ximpshap.cxx
@@ -3459,6 +3459,9 @@ SvXMLImportContextRef SdXMLFrameShapeContext::CreateChildContext( sal_uInt16 nPr
 
         if(getSupportsMultipleContents() && dynamic_cast< SdXMLGraphicObjectShapeContext* >(xContext.get()))
         {
+            if ( !maShapeId.isEmpty() )
+                GetImport().getInterfaceToIdentifierMapper().reserveIdentifier( maShapeId );
+
             addContent(*mxImplContext);
         }
     }
@@ -3568,13 +3571,11 @@ void SdXMLFrameShapeContext::EndElement()
     SvXMLImportContextRef const pSelectedContext(solveMultipleImages());
     const SdXMLGraphicObjectShapeContext* pShapeContext(
         dynamic_cast<const SdXMLGraphicObjectShapeContext*>(pSelectedContext.get()));
-    if ( pShapeContext && !maShapeId.isEmpty() )
+    if ( pShapeContext )
     {
-        // fdo#64512 and fdo#60075 - make sure *this* shape is
-        // registered for given ID
         assert( mxImplContext.is() );
         const uno::Reference< uno::XInterface > xShape( pShapeContext->getShape() );
-        GetImport().getInterfaceToIdentifierMapper().registerReferenceAlways( maShapeId, xShape );
+        GetImport().getInterfaceToIdentifierMapper().registerReservedReference( maShapeId, xShape );
     }
 
     if( !mxImplContext.is() )

sd/qa/unit/export-tests.cxx
@@ -81,6 +81,7 @@ public:
     void testSoftEdges();
     void testShadowBlur();
     void testTdf115753();
+    void testRhbz1870501();
 
     CPPUNIT_TEST_SUITE(SdExportTest);
 
@@ -119,6 +120,7 @@ public:
     CPPUNIT_TEST(testSoftEdges);
     CPPUNIT_TEST(testShadowBlur);
     CPPUNIT_TEST(testTdf115753);
+    CPPUNIT_TEST(testRhbz1870501);
 
     CPPUNIT_TEST_SUITE_END();
 
@@ -1375,6 +1377,13 @@ void SdExportTest::testShadowBlur()
             "shadow-blur", "0.388cm");
 
     xDocShRef->DoClose();
+}
+
+void SdExportTest::testRhbz1870501()
+{
+    //Without the fix in place, it would crash at export time
+    ::sd::DrawDocShellRef xDocShRef = loadURL( m_directories.getURLFromSrc("/sd/qa/unit/data/odg/rhbz1870501.odg"), ODG);
+    xDocShRef = saveAndReload( xDocShRef.get(), ODG );
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(SdExportTest);