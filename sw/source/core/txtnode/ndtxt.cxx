5887f2b45b05ea2abe15ab931fdf53cb3c8d7187
Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
2020-08-20 15:33:49 +0200

    Related tdf#100492 Detect click into empty field
    Change-Id: Ic2937d619a8361b9d17b7dfa16698a5005f34ec6
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/101076
    Tested-by: Jenkins
    Reviewed-by: Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
    (cherry picked from commit 2c7bf3543ab798d1c117d9f3258467e4aef9a8db)

--- sw/source/core/txtnode/ndtxt.cxx
+++ sw/source/core/txtnode/ndtxt.cxx
@@ -1780,7 +1780,7 @@ SwTextField* SwTextNode::GetFieldTextAttrAt(
             dynamic_cast<SwTextField*>( GetTextAttrAt(
                 nIndex,
                 RES_TXTATR_INPUTFIELD,
-                bIncludeInputFieldAtStart ? DEFAULT : PARENT ));
+                bIncludeInputFieldAtStart ? DEFAULT : EXPAND ));
     }
 
     return pTextField;
