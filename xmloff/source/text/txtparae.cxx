ef27927b9d953aca6043f09074df956401b32a43
Mike Kaganski <mike.kaganski@collabora.com>
2020-08-21 00:15:29 +0300

    tdf#135942: avoid collecting autostyles during writing them
    This modifies the container over which iteration is performed.
    Additionally, make sure that all nested table autostyles are
    collected on the first phase.
    
    Change-Id: I74c0bb1aaacad095226c21e6bf51cc8668133bb3
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/101096
    Tested-by: Jenkins
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
    (cherry picked from commit f0286ad82465152b29bba01ab2edeb97291397fa)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/101069
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    (cherry picked from commit 0273675e7dde577077ccca17571846a0942f2630)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/102700
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

--- xmloff/source/text/txtparae.cxx
+++ xmloff/source/text/txtparae.cxx
@@ -3675,6 +3675,8 @@ void XMLTextParagraphExport::exportTableAutoStyles() {}
 
 void XMLTextParagraphExport::exportTextAutoStyles()
 {
+    // tdf#135942: do not collect styles during their export: this may modify iterated containers
+    mbCollected = true;
     exportTableAutoStyles();
 
     GetAutoStylePool().exportXML( XML_STYLE_FAMILY_TEXT_PARAGRAPH );
