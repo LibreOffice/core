2aef33220d450194172612c27852422c64cb7a4e
Noel Grandin <noel.grandin@collabora.co.uk>
2020-08-21 09:55:25 +0200

    use fast-parser in XMLFootnoteConfigurationImportContext
    Change-Id: Ic139ef9956f59178582a344f65926f95a6423f14

--- xmloff/inc/XMLFootnoteConfigurationImportContext.hxx
+++ xmloff/inc/XMLFootnoteConfigurationImportContext.hxx
@@ -60,17 +60,14 @@ public:
 
     XMLFootnoteConfigurationImportContext(
         SvXMLImport& rImport,
-        sal_uInt16 nPrfx,
-        const OUString& rLName,
-        const css::uno::Reference< css::xml::sax::XAttributeList> & xAttrList);
+        sal_Int32 nElement,
+        const css::uno::Reference< css::xml::sax::XFastAttributeList> & xAttrList);
 
     virtual ~XMLFootnoteConfigurationImportContext() override;
 
     /// for footnotes, also parse begin and end notices
-    virtual SvXMLImportContextRef CreateChildContext(
-        sal_uInt16 nPrefix,
-        const OUString& rLocalName,
-        const css::uno::Reference< css::xml::sax::XAttributeList> & xAttrList ) override;
+    virtual css::uno::Reference< css::xml::sax::XFastContextHandler > SAL_CALL createFastChildContext(
+        sal_Int32 nElement, const css::uno::Reference< css::xml::sax::XFastAttributeList >& AttrList ) override;
 
     /// get token map for attributes
     const SvXMLTokenMap& GetFtnConfigAttrTokenMap();
