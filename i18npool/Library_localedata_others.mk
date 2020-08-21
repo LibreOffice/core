6fc27ef398fdf7003f4390be8ef80454ff32b081
Eike Rathke <erack@redhat.com>
2020-08-21 11:51:44 +0200

    Resolves: tdf#135518 Add English (Hong Kong) [en-HK] locale data
    Inherited from en-GB, zh-HK
    
    Change-Id: I966d19cfa2da26d3d882af35afe79a2f77eaffa8
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/101139
    Reviewed-by: Eike Rathke <erack@redhat.com>
    Tested-by: Jenkins
    (cherry picked from commit 5dffb7ba361ad76aa249d6c93fd87fff67362726)

--- i18npool/Library_localedata_others.mk
+++ i18npool/Library_localedata_others.mk
@@ -50,6 +50,7 @@ $(eval $(call gb_Library_add_generated_exception_objects,localedata_others,\
 	CustomTarget/i18npool/localedata/localedata_dz_BT \
 	CustomTarget/i18npool/localedata/localedata_ebo_CG \
 	CustomTarget/i18npool/localedata/localedata_ee_GH \
+	CustomTarget/i18npool/localedata/localedata_en_HK \
 	CustomTarget/i18npool/localedata/localedata_en_IN \
 	CustomTarget/i18npool/localedata/localedata_en_MY \
 	CustomTarget/i18npool/localedata/localedata_eo \
