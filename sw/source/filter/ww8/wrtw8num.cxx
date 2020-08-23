fcbe04b7625524b0b16a664444009bf5a1119243
Vasily Melenchuk <vasily.melenchuk@cib.de>
2020-08-23 14:02:40 +0300

    WIP: tdf#135973
    Change-Id: I1f94a09b7d51fcc3300b056d6d9e8ea6367a4446

--- sw/source/filter/ww8/wrtw8num.cxx
+++ sw/source/filter/ww8/wrtw8num.cxx
@@ -86,22 +86,22 @@ sal_uInt16 MSWordExportBase::OverrideNumRule(
         OUString const& rListId,
         SwNumRule const& rAbstractRule)
 {
-    auto const numdef = GetNumberingId(rExistingRule);
+    SwNumRule* p = const_cast<SwNumRule*>(&rExistingRule);
+    sal_uInt16 numdef = static_cast<sal_uInt16>(m_pUsedNumTable->GetPos(p));
+
+    p = const_cast<SwNumRule*>(&rAbstractRule);
+
     auto const absnumdef = rListId == rAbstractRule.GetDefaultListId()
-        ? GetNumberingId(rAbstractRule)
+        ? static_cast<sal_uInt16>(m_pUsedNumTable->GetPos(p))
         : DuplicateAbsNum(rListId, rAbstractRule);
     auto const mapping = std::make_pair(numdef, absnumdef);
 
-    auto it = m_OverridingNumsR.find(mapping);
-    if (it == m_OverridingNumsR.end())
-    {
-        it = m_OverridingNumsR.insert(std::make_pair(mapping, m_pUsedNumTable->size())).first;
-        m_OverridingNums.insert(std::make_pair(m_pUsedNumTable->size(), mapping));
+    auto it = m_OverridingNums.insert(std::make_pair(m_pUsedNumTable->size(), mapping));
 
-        m_pUsedNumTable->push_back(nullptr); // dummy, it's unique_ptr...
-        ++m_nUniqueList; // counter for DuplicateNumRule...
-    }
-    return it->second;
+    m_pUsedNumTable->push_back(nullptr); // dummy, it's unique_ptr...
+    ++m_nUniqueList; // counter for DuplicateNumRule...
+
+    return it.first->first;
 }
 
 void MSWordExportBase::AddListLevelOverride(sal_uInt16 nListId,
@@ -143,16 +143,19 @@ sal_uInt16 MSWordExportBase::GetNumberingId( const SwNumRule& rNumRule )
     SwNumRule* p = const_cast<SwNumRule*>(&rNumRule);
     sal_uInt16 nRet = static_cast<sal_uInt16>(m_pUsedNumTable->GetPos(p));
 
-    // Is this list now duplicated into a new list which we should use
-    // #i77812# - perform 'deep' search in duplication map
-    std::map<sal_uInt16,sal_uInt16>::const_iterator aResult = m_aRuleDuplicates.end();
-    do {
-        aResult = m_aRuleDuplicates.find(nRet);
-        if ( aResult != m_aRuleDuplicates.end() )
+    // Find last override
+    sal_uInt16 nOverrideNumId = USHRT_MAX;
+    for (const auto& it : m_OverridingNums)
+    {
+        if (it.second.second == nRet)
         {
-            nRet = (*aResult).second;
+            nOverrideNumId = it.first;
         }
-    } while ( aResult != m_aRuleDuplicates.end() );
+    }
+    if (nOverrideNumId != USHRT_MAX)
+    {
+        nRet = nOverrideNumId;
+    }
 
     return nRet;
 }
