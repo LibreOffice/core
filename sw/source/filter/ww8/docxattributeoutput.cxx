c31b436c86c0d4d287725708732c14357ed5583a
László Németh <nemeth@numbertext.org>
2020-08-25 17:22:43 +0200

    tdf#123401 DOCX: fix table formula AVERAGE
    by adding AVERAGE as an alternative name of MEAN,
    and by converting MEAN to AVERAGE during DOCX export.
    
    Change-Id: I691618141a67f19f5823d8a214169c15f2ac6cce
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/101336
    Tested-by: László Németh <nemeth@numbertext.org>
    Reviewed-by: László Németh <nemeth@numbertext.org>

--- sw/source/filter/ww8/docxattributeoutput.cxx
+++ sw/source/filter/ww8/docxattributeoutput.cxx
@@ -2178,7 +2178,10 @@ void DocxAttributeOutput::CmdField_Impl( const SwTextNode* pNode, sal_Int32 nPos
                     icu::UnicodeString sInput(sToken.getStr());
                     // remove < and > around cell references, e.g. <A1> to A1, <A1:B2> to A1:B2
                     icu::RegexMatcher aMatcher("<([A-Z]{1,3}[0-9]+(:[A-Z]{1,3}[0-9]+)?)>", sInput, 0, nErr);
-                    sToken = aMatcher.replaceAll(icu::UnicodeString("$1"), nErr).getTerminatedBuffer();
+                    sInput = aMatcher.replaceAll(icu::UnicodeString("$1"), nErr);
+                    // convert MEAN to AVERAGE
+                    icu::RegexMatcher aMatcher2("\\bMEAN\\b", sInput, UREGEX_CASE_INSENSITIVE, nErr);
+                    sToken = aMatcher2.replaceAll(icu::UnicodeString("AVERAGE"), nErr).getTerminatedBuffer();
                 }
             }
 
