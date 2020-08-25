437350ba6ee66f4ef988e1c33f09b5d2bb32f429
Caolán McNamara <caolanm@redhat.com>
2020-08-25 13:53:46 +0100

    tdf#127802 support tristate for hidden/unhidden text
    Change-Id: I9a8ad72ae3ee45ac2521e662b17d3d22d98aa055

--- cui/source/inc/chardlg.hxx
+++ cui/source/inc/chardlg.hxx
@@ -167,6 +167,11 @@ private:
     Color                      m_aOrigFontColor;
     sal_uInt16                 m_nHtmlMode;
 
+    weld::TriStateEnabled m_aOutlineState;
+    weld::TriStateEnabled m_aShadowState;
+    weld::TriStateEnabled m_aHiddenState;
+    weld::TriStateEnabled m_aIndividualWordsState;
+
     std::unique_ptr<weld::Label> m_xFontColorFT;
     std::unique_ptr<ColorListBox> m_xFontColorLB;
     std::unique_ptr<weld::Label> m_xFontTransparencyFT;
@@ -202,8 +207,10 @@ private:
 
     void SelectHdl_Impl(const weld::ComboBox*);
     DECL_LINK(SelectListBoxHdl_Impl, weld::ComboBox&, void);
+    DECL_LINK(OutlineBtnClickHdl, weld::ToggleButton&, void);
+    DECL_LINK(ShadowBtnClickHdl, weld::ToggleButton&, void);
+    DECL_LINK(HiddenBtnClickHdl, weld::ToggleButton&, void);
     DECL_LINK(CbClickHdl_Impl, weld::ToggleButton&, void);
-    DECL_LINK(TristClickHdl_Impl, weld::ToggleButton&, void);
     DECL_LINK(ColorBoxSelectHdl_Impl, ColorListBox&, void);
     DECL_LINK(ModifyFontTransparencyHdl_Impl, weld::MetricSpinButton&, void);
 
