b3fada2f64d1ddbc3d481a409e01333c732c5fe0
Marco Cecchetti <marco.cecchetti@collabora.com>
2020-08-26 09:06:59 +0200

    Online: Calculator highlight should follow font size changes / core part
    When user changes font size of selected text, markers and highlight rectangle should be updated.
    
    Change-Id: I9b7e3224ad162bfb7d8f0853b6cb17b4feafa0cf

--- sd/qa/unit/tiledrendering/tiledrendering.cxx
+++ sd/qa/unit/tiledrendering/tiledrendering.cxx
@@ -303,10 +303,6 @@ void SdTiledRenderingTest::callbackImpl(int nType, const char* pPayload)
             lcl_convertRectangle(rString, aRectangle);
             m_aSelection.push_back(aRectangle);
         }
-        if (m_aSearchResultSelection.empty())
-            ++m_nSelectionBeforeSearchResult;
-        else
-            ++m_nSelectionAfterSearchResult;
     }
     break;
     case LOK_CALLBACK_SEARCH_NOT_FOUND:
@@ -2328,6 +2324,10 @@ void SdTiledRenderingTest::testCutSelectionChange()
     // Select first text object
     pXImpressDocument->postKeyEvent(LOK_KEYEVENT_KEYINPUT, 0, awt::Key::TAB);
     pXImpressDocument->postKeyEvent(LOK_KEYEVENT_KEYUP, 0, awt::Key::TAB);
+    Scheduler::ProcessEventsToIdle();
+
+    pXImpressDocument->postKeyEvent(LOK_KEYEVENT_KEYINPUT, 0, KEY_F2);
+    pXImpressDocument->postKeyEvent(LOK_KEYEVENT_KEYUP, 0, KEY_F2);
     Scheduler::ProcessEventsToIdle();
 
     // step into text editing
