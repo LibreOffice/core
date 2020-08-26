b0b92b2da2fcb60e80c50684f131632ab4fa21f3
Regényi Balázs <regenyi.balazsmiklos@nisz.hu>
2020-08-26 17:38:21 +0200

    tdf#135047 DOCX export: fix spacing around objects
    defined by frame style instead of direct formatting.
    
    Co-authored-by: Szabolcs Tóth
    
    Change-Id: I376c6ff09d15fe8e2db699b4cfc157ce08016cfa
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/101416
    Tested-by: Jenkins
    Tested-by: László Németh <nemeth@numbertext.org>
    Reviewed-by: László Németh <nemeth@numbertext.org>
    (cherry picked from commit fe72feef80a4336be9a57a8137839de1d52b2601)

--- sw/qa/extras/ooxmlexport/ooxmlexport15.cxx
+++ sw/qa/extras/ooxmlexport/ooxmlexport15.cxx
@@ -257,6 +257,18 @@ DECLARE_OOXMLEXPORT_TEST(testRelativeAnchorHeightFromBottomMarginNoFooter,
     CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(1147), nAnchoredHeight);
 }
 
+DECLARE_OOXMLEXPORT_TEST(testImageSpaceSettings, "tdf135047_ImageSpaceSettings.fodt")
+{
+    // tdf#135047 The spaces of image were not saved.
+    xmlDocUniquePtr pXmlDoc = parseExport();
+    if (!pXmlDoc)
+        return;
+    assertXPath(pXmlDoc, "/w:document/w:body/w:p[1]/w:r[1]/w:drawing/wp:anchor", "distT", "90170");
+    assertXPath(pXmlDoc, "/w:document/w:body/w:p[1]/w:r[1]/w:drawing/wp:anchor", "distB", "90170");
+    assertXPath(pXmlDoc, "/w:document/w:body/w:p[1]/w:r[1]/w:drawing/wp:anchor", "distL", "90170");
+    assertXPath(pXmlDoc, "/w:document/w:body/w:p[1]/w:r[1]/w:drawing/wp:anchor", "distR", "90170");
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
