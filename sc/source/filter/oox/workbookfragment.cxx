LibreOffice/core

commit edf13fe1247e7ef411a9ff5435385573fad01f56

tdf#93831 xlsx file full of pictures of numbers slow to open
Change-Id: I6c984c84c409519a986eacb265c943307a7b57af
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/90245
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sc/source/filter/oox/workbookfragment.cxx
@@ -50,6 +50,7 @@
 #include <biffhelper.hxx>
 
 #include <document.hxx>
+#include <drwlayer.hxx>
 #include <docsh.hxx>
 #include <calcconfig.hxx>
 #include <globstr.hrc>
@@ -343,6 +344,12 @@ void importSheetFragments( WorkbookFragment& rWorkbookHandler, SheetFragmentVect
 
 void WorkbookFragment::finalizeImport()
 {
+    // lock the model to prevent broadcasting, speeds up load a lot
+    getScDocument().InitDrawLayer();
+    auto pModel = getScDocument().GetDrawLayer();
+    bool bWasLocked = pModel->isLocked();
+    pModel->setLock(true);
+
     ISegmentProgressBarRef xGlobalSegment = getProgressBar().createSegment( PROGRESS_LENGTH_GLOBALS );
 
     // read the theme substream
@@ -504,6 +511,8 @@ void WorkbookFragment::finalizeImport()
 
     // attach macros to registered objects now that all objects have been created.
     getBaseFilter().getVbaProject().attachMacros();
+
+    pModel->setLock(bWasLocked);
 }
 
 namespace {

commit 3b9fe687d6e5af6f9f501d2d9bfb307ffd388483

tdf#136113 shape editing does not change modified status
regression from
    commit edf13fe1247e7ef411a9ff5435385573fad01f56
    tdf#93831 xlsx file full of pictures of numbers slow to open

Change-Id: I1d8fdc5134a1b70bf214df642a9e7a386b853895

sc/source/filter/oox/workbookfragment.cxx
@@ -344,12 +344,6 @@ void importSheetFragments( WorkbookFragment& rWorkbookHandler, SheetFragmentVect
 
 void WorkbookFragment::finalizeImport()
 {
-    // lock the model to prevent broadcasting, speeds up load a lot
-    getScDocument().InitDrawLayer();
-    auto pModel = getScDocument().GetDrawLayer();
-    bool bWasLocked = pModel->isLocked();
-    pModel->setLock(true);
-
     ISegmentProgressBarRef xGlobalSegment = getProgressBar().createSegment( PROGRESS_LENGTH_GLOBALS );
 
     // read the theme substream
@@ -472,6 +466,12 @@ void WorkbookFragment::finalizeImport()
             getBaseFilter().getVbaProject().readVbaModules( *xPrjStrg );
         }
     }
+
+    // lock the model to prevent broadcasting, speeds up load a lot
+    getScDocument().InitDrawLayer();
+    auto pModel = getScDocument().GetDrawLayer();
+    bool bWasLocked = pModel->isLocked();
+    pModel->setLock(true);
 
     // load all worksheets
     importSheetFragments(*this, aSheetFragments);