973d019ad00dedc26f176ae1ef415952447a078a
Noel Grandin <noel.grandin@collabora.co.uk>
2020-08-28 10:54:02 +0200

    use fastparser in SvXMLNumFormatContext
    Change-Id: I310fe566ba3df511e4499afe0c9f7206f3451ff9
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/101528
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

--- xmloff/inc/XMLNumberStylesImport.hxx
+++ xmloff/inc/XMLNumberStylesImport.hxx
@@ -53,8 +53,8 @@ public:
 
     virtual void SAL_CALL endFastElement(sal_Int32 nElement) override;
 
-    virtual SvXMLImportContextRef CreateChildContext( sal_uInt16 nPrefix, const OUString& rLocalName,
-        const css::uno::Reference< css::xml::sax::XAttributeList>& xAttrList ) override;
+    virtual css::uno::Reference< css::xml::sax::XFastContextHandler > SAL_CALL createFastChildContext(
+        sal_Int32 nElement, const css::uno::Reference< css::xml::sax::XFastAttributeList >& AttrList ) override;
 
     sal_Int32 GetDrawKey() const { return mnKey; }
 };
