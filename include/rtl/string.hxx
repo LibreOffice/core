LibreOffice/core

commit 2e21240f23ac2191a3535d697a7308b29303c67c

Goodbye O[U]StringView, welcome O[U]String::Concat
O[U]StringView had an odd mixture of uses.  For one, it was used like
std::[u16]string_view, for which directly using the latter std types is clearly
the better alternative.  For another, it was used in concatenation sequences,
when neither of the two leading terms were of our rtl string-related types.

For that second use case introduce O[U]String::Concat (as std::[u16]string_view
can obviously not be used, those not being one of our rtl string-related types).
Also, O[U]StringLiteral is occasionally used for this, but the planned changes
outlined in the 33ecd0d5c4fff9511a8436513936a3f7044a775a "Change OUStringLiteral
from char[] to char16_t[]" commit message will make that no longer work, so
O[U]String::Concat will be the preferred solution in such use cases going
forward, too.

O[U]StringView was also occasionally used to include O[U]StringBuffer values in
concatenation sequences, for which a more obvious alternative is to make
O[U]StringBuffer participate directly in the ToStringHelper/O[U]StringConcat
machinery.

Change-Id: I1f0e8d836796c9ae01c45f32c518be5f52976622
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/101586
Tested-by: Jenkins
Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

include/rtl/string.hxx
@@ -1900,6 +1900,31 @@ public:
 #if defined LIBO_INTERNAL_ONLY
     operator std::string_view() const { return {getStr(), sal_uInt32(getLength())}; }
 #endif
+
+#if defined LIBO_INTERNAL_ONLY
+    // A wrapper for the first expression in an
+    //
+    //   OString::Concat(e1) + e2 + ...
+    //
+    // concatenation chain, when neither of the first two e1, e2 is one of our rtl string-related
+    // classes (so something like
+    //
+    //   OString s = "a" + (b ? std::string_view("c" : "dd");
+    //
+    // would not compile):
+    template<typename T> [[nodiscard]] static
+    typename std::enable_if_t<
+        ToStringHelper<T>::allowOStringConcat, OStringConcat<OStringConcatMarker, T>>
+    Concat(T const & value) { return OStringConcat<OStringConcatMarker, T>({}, value); }
+
+    // This overload is needed so that an argument of type 'char const[N]' ends up as
+    // 'OStringConcat<rtl::OStringConcatMarker, char const[N]>' rather than as
+    // 'OStringConcat<rtl::OStringConcatMarker, char[N]>':
+    template<typename T, std::size_t N> [[nodiscard]] static
+    typename std::enable_if_t<
+        ToStringHelper<T[N]>::allowOStringConcat, OStringConcat<OStringConcatMarker, T[N]>>
+    Concat(T (& value)[N]) { return OStringConcat<OStringConcatMarker, T[N]>({}, value); }
+#endif
 };
 
 /* ======================================================================= */

include/rtl/ustring.hxx
@@ -3693,6 +3693,31 @@ public:
     operator std::u16string_view() const { return {getStr(), sal_uInt32(getLength())}; }
 #endif
 
+#if defined LIBO_INTERNAL_ONLY
+    // A wrapper for the first expression in an
+    //
+    //   OUString::Concat(e1) + e2 + ...
+    //
+    // concatenation chain, when neither of the first two e1, e2 is one of our rtl string-related
+    // classes (so something like
+    //
+    //   OUString s = "a" + (b ? std::u16string_view(u"c" : u"dd");
+    //
+    // would not compile):
+    template<typename T> [[nodiscard]] static
+    typename std::enable_if_t<
+        ToStringHelper<T>::allowOUStringConcat, OUStringConcat<OUStringConcatMarker, T>>
+    Concat(T const & value) { return OUStringConcat<OUStringConcatMarker, T>({}, value); }
+
+    // This overload is needed so that an argument of type 'char const[N]' ends up as
+    // 'OUStringConcat<rtl::OUStringConcatMarker, char const[N]>' rather than as
+    // 'OUStringConcat<rtl::OUStringConcatMarker, char[N]>':
+    template<typename T, std::size_t N> [[nodiscard]] static
+    typename std::enable_if_t<
+        ToStringHelper<T[N]>::allowOUStringConcat, OUStringConcat<OUStringConcatMarker, T[N]>>
+    Concat(T (& value)[N]) { return OUStringConcat<OUStringConcatMarker, T[N]>({}, value); }
+#endif
+
 private:
     OUString & internalAppend( rtl_uString* pOtherData )
     {

codemaker/source/javamaker/javatype.cxx
@@ -379,7 +379,7 @@ void MethodDescriptor::addTypeParameter(OUString const & name) {
 }
 
 OString MethodDescriptor::getDescriptor() const {
-    return rtl::OStringView(m_descriptorStart) + m_descriptorEnd;
+    return m_descriptorStart + m_descriptorEnd;
 }
 
 

connectivity/source/drivers/postgresql/pq_preparedstatement.cxx
@@ -489,7 +489,7 @@ void PreparedStatement::setBytes(
             *this, OUString(), 1, Any() );
     }
     m_vars[parameterIndex-1]
-        = "'" + rtl::OStringView(reinterpret_cast<char *>(escapedString.get()), len -1) + "'";
+        = OString::Concat("'") + std::string_view(reinterpret_cast<char *>(escapedString.get()), len -1) + "'";
 }
 
 

i18npool/source/breakiterator/breakiterator_unicode.cxx
@@ -199,7 +199,7 @@ void BreakIterator_Unicode::loadICUBreakIterator(const css::lang::Locale& rLocal
                 }
 
                 status = U_ZERO_ERROR;
-                OString aUDName = rtl::OStringView(rule) + "_" + aLanguage;
+                OString aUDName = OString::Concat(rule) + "_" + aLanguage;
                 UDataMemory* pUData = udata_open("OpenOffice", "brk", aUDName.getStr(), &status);
                 if( U_SUCCESS(status) )
                     rbi = std::make_shared<OOoRuleBasedBreakIterator>( pUData, status);

i18npool/source/breakiterator/xdictionary.cxx
@@ -166,11 +166,11 @@ void xdictionary::initDictionaryData(const char *pLang)
 
 #ifdef SAL_DLLPREFIX
     OString sModuleName = // mostly "lib*.so" (with * == dict_zh)
-        SAL_DLLPREFIX
+        OString::Concat(SAL_DLLPREFIX "dict_") + pLang + SAL_DLLEXTENSION;
 #else
     OString sModuleName = // mostly "*.dll" (with * == dict_zh)
+        OString::Concat("dict_") + pLang + SAL_DLLEXTENSION;
 #endif
-        "dict_" + rtl::OStringView(pLang) + SAL_DLLEXTENSION;
     aEntry.mhModule = osl_loadModuleRelativeAscii( &thisModule, sModuleName.getStr(), SAL_LOADMODULE_DEFAULT );
     if( aEntry.mhModule ) {
         oslGenericFunction func;

i18npool/source/localedata/localedata.cxx
@@ -550,7 +550,7 @@ oslGenericFunction lcl_LookupTableHelper::getFunctionSymbolByName(
                         {
                             (*pOutCachedItem).reset(new LocaleDataLookupTableItem( rCurrent ));
                             (*pOutCachedItem)->localeName = i.pLocale;
-                            OString sSymbolName = rtl::OStringView(pFunction) + "_" +
+                            OString sSymbolName = OString::Concat(pFunction) + "_" +
                                     (*pOutCachedItem)->localeName;
                             return (*pOutCachedItem)->module->getFunctionSymbol(
                                     sSymbolName.getStr());
@@ -563,10 +563,10 @@ oslGenericFunction lcl_LookupTableHelper::getFunctionSymbolByName(
             // Library not loaded, load it and add it to the list.
 #ifdef SAL_DLLPREFIX
             OString sModuleName =    // mostly "lib*.so"
-                SAL_DLLPREFIX + rtl::OStringView(i.pLib) + SAL_DLLEXTENSION;
+                OString::Concat(SAL_DLLPREFIX) + i.pLib + SAL_DLLEXTENSION;
 #else
             OString sModuleName =    // mostly "*.dll"
-                rtl::OStringView(i.pLib) + SAL_DLLEXTENSION;
+                OString::Concat(i.pLib) + SAL_DLLEXTENSION;
 #endif
             std::unique_ptr<osl::Module> module(new osl::Module());
             if ( module->loadRelative(&thisModule, sModuleName.getStr()) )
@@ -578,7 +578,7 @@ oslGenericFunction lcl_LookupTableHelper::getFunctionSymbolByName(
                 if( pOutCachedItem )
                 {
                     pOutCachedItem->reset(new LocaleDataLookupTableItem( maLookupTable.back() ));
-                    OString sSymbolName = rtl::OStringView(pFunction) + "_" + (*pOutCachedItem)->localeName;
+                    OString sSymbolName = OString::Concat(pFunction) + "_" + (*pOutCachedItem)->localeName;
                     return pTmpModule->getFunctionSymbol(sSymbolName.getStr());
                 }
                 else
@@ -1436,7 +1436,7 @@ oslGenericFunction LocaleDataImpl::getFunctionSymbol( const Locale& rLocale, con
 
     if (cachedItem && cachedItem->equals(rLocale))
     {
-        OString sSymbolName = rtl::OStringView(pFunction) + "_" +
+        OString sSymbolName = OString::Concat(pFunction) + "_" +
                 cachedItem->localeName;
         return cachedItem->module->getFunctionSymbol(sSymbolName.getStr());
     }

idlc/source/options.cxx
@@ -17,6 +17,9 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
+#include <sal/config.h>
+
+#include <string_view>
 
 #include <options.hxx>
 
@@ -285,7 +288,7 @@ bool Options::initOptions(std::vector< std::string > & rArgs)
         {
           return badOption("invalid", option);
         }
-        OString param = "-D" + rtl::OStringView((*first).c_str(), (*first).size());
+        OString param = OString::Concat("-D") + std::string_view((*first).c_str(), (*first).size());
         if (m_options.count("-D") > 0)
         {
           param = m_options["-D"] + " " + param;

commit 98889262fd52dea6dd16c7471ddddc7dffbf3f35

Fix typos in comments
...introduced with 2e21240f23ac2191a3535d697a7308b29303c67c "Goodbye
O[U]StringView, welcome O[U]String::Concat".  Thanks to Mike for spotting them!

Change-Id: Ic401c564eac03a34184204f09cb8d0cb6be4e6e5

include/rtl/string.hxx
@@ -1909,7 +1909,7 @@ public:
     // concatenation chain, when neither of the first two e1, e2 is one of our rtl string-related
     // classes (so something like
     //
-    //   OString s = "a" + (b ? std::string_view("c" : "dd");
+    //   OString s = "a" + (b ? std::string_view("c") : std::string_view("dd"));
     //
     // would not compile):
     template<typename T> [[nodiscard]] static

include/rtl/ustring.hxx
@@ -3701,7 +3701,7 @@ public:
     // concatenation chain, when neither of the first two e1, e2 is one of our rtl string-related
     // classes (so something like
     //
-    //   OUString s = "a" + (b ? std::u16string_view(u"c" : u"dd");
+    //   OUString s = "a" + (b ? std::u16string_view(u"c") : std::u16string_view(u"dd"));
     //
     // would not compile):
     template<typename T> [[nodiscard]] static