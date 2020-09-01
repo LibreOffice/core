9e123fa8e85e732c696805041faf0434f7bf5f99
Szabolcs Toth <szabolcs450@gmail.com>
2020-09-01 13:46:38 +0200

    tdf#133688 tdf#125414 XLSX: don't export tracer arrows
    (see Tools->Detective) as plain shapes.
    
    Co-authored-by: Balázs Regényi
    
    Change-Id: I920445637a6be12169ae7d70295e4460d7f9b26b
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/101845
    Tested-by: László Németh <nemeth@numbertext.org>
    Reviewed-by: László Németh <nemeth@numbertext.org>
    (cherry picked from commit 14b40ec7be7ca8315848034591e3c3a246d5a8dd)

--- sc/source/core/tool/detfunc.cxx
+++ sc/source/core/tool/detfunc.cxx
@@ -1597,14 +1597,22 @@ ScDetectiveObjType ScDetectiveFunc::GetDetectiveObjectType( SdrObject* pObject, 
                 if ( nObjColor == GetErrorColor() && nObjColor != GetArrowColor() )
                     rRedLine = true;
             }
-            else if ( dynamic_cast<const SdrCircObj*>( pObject) !=  nullptr )
+            else if (dynamic_cast<const SdrCircObj*>(pObject) != nullptr)
             {
-                if ( bValidStart )
+                if (bValidStart)
                 {
                     // cell position is returned in rPosition
-
                     rPosition = pData->maStart;
                     eType = SC_DETOBJ_CIRCLE;
+                }
+            }
+            else if (dynamic_cast<const SdrRectObj*>(pObject) != nullptr)
+            {
+                if (bValidStart)
+                {
+                    // cell position is returned in rPosition
+                    rPosition = pData->maStart;
+                    eType = SC_DETOBJ_RECTANGLE;
                 }
             }
         }
