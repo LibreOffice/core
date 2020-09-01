20abda9d49284a4302222090f699759336f66283
László Németh <nemeth@numbertext.org>
2020-09-01 08:48:53 +0200

    tdf#134649 DOCX export: fix page break between tables
    Section break between tables wasn't exported,
    resulting missing page break and table content.
    
    For example, a table on a new landscape page was
    merged to the previous table, removing not only the
    landscape page, but hiding half of the table because
    of the narrower width of the previous page with
    portrait orientation.
    
    Change-Id: Id02529d023a6306fb5be42486d25f252d8821f2f

--- sw/source/filter/ww8/docxattributeoutput.cxx
+++ sw/source/filter/ww8/docxattributeoutput.cxx
@@ -1025,6 +1025,13 @@ void DocxAttributeOutput::SectionBreaks(const SwNode& rNode)
             if (rNode.StartOfSectionNode()->IsTableNode() || rNode.StartOfSectionNode()->IsSectionNode())
                 m_rExport.OutputSectionBreaks(pTextNode->GetpSwAttrSet(), *pTextNode, m_tableReference->m_bTableCellOpen);
         }
+        else if (aNextIndex.GetNode().IsTableNode())
+        {
+            // Handle section break between tables.
+            const SwTableNode* pTableNode = static_cast<SwTableNode*>(&aNextIndex.GetNode());
+            const SwFrameFormat *pFormat = pTableNode->GetTable().GetFrameFormat();
+            m_rExport.OutputSectionBreaks(&(pFormat->GetAttrSet()), *pTableNode);
+        }
     }
 }
 
