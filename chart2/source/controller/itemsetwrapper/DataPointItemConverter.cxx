16babc553294b676d7cdbbca9bb2e90cb46e484d
Balazs Varga <balazs.varga991@gmail.com>
2020-09-02 15:09:01 +0200

    tdf#136402 Chart View: remove custom label text
    Now Delete Data Labels and Insert Data Labels local
    menu options remove custom label text instead of keeping it.
    See with unit test document custom_data_label.xlsx of
    commit bcda268cde1f8408c2066ce985acedfce3e3bc2e
    (tdf#134977 Chart OOXML: fix import of custom data label).
    
    Change-Id: Ifc7afd0f2ece9da528e6206a6bd393b394b36f9e
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/101929
    Tested-by: László Németh <nemeth@numbertext.org>
    Reviewed-by: László Németh <nemeth@numbertext.org>
    (cherry picked from commit 0dce53516011f5d2d9408501714e2b1865517119)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/111142
    Tested-by: Gabor Kelemen <kelemen.gabor2@nisz.hu>
    Reviewed-by: Gabor Kelemen <kelemen.gabor2@nisz.hu>

--- chart2/source/controller/itemsetwrapper/DataPointItemConverter.cxx
+++ chart2/source/controller/itemsetwrapper/DataPointItemConverter.cxx
@@ -338,6 +338,7 @@ bool DataPointItemConverter::ApplySpecialItem(
                         DataSeriesHelper::hasAttributedDataPointDifferentValue( xSeries, CHART_UNONAME_LABEL , aOldValue ) )
                     {
                         DataSeriesHelper::setPropertyAlsoToAllAttributedDataPoints( xSeries, CHART_UNONAME_LABEL , uno::Any( aLabel ) );
+                        DataSeriesHelper::setPropertyAlsoToAllAttributedDataPoints( xSeries, CHART_UNONAME_CUSTOM_LABEL_FIELDS, uno::Any() );
                         bChanged = true;
                     }
                 }
