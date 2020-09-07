ecb06c0c5af4563d2feae5a927ab23542f63001b
Regényi Balázs <regenyi.balazs@nisz.hu>
2020-09-07 19:54:19 +0200

    tdf#77236 DOCX shape export: fix missing solidFill
    (resulting for example, invisible shapes).
    
    Co-authored-by: Szabolcs Tóth
    
    Change-Id: I328514022f4a594c374d5a97db39d1cf3a954e02
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/102195
    Tested-by: László Németh <nemeth@numbertext.org>
    Reviewed-by: László Németh <nemeth@numbertext.org>
    Signed-off-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/103615
    Tested-by: Jenkins

--- sw/qa/extras/ooxmlexport/ooxmlexport6.cxx
+++ sw/qa/extras/ooxmlexport/ooxmlexport6.cxx
@@ -793,7 +793,7 @@ DECLARE_OOXMLEXPORT_TEST(testShapeThemePreservation, "shape-theme-preservation.d
             0);
     assertXPath(pXmlDocument,
             "/w:document/w:body/w:p[1]/w:r/mc:AlternateContent/mc:Choice/w:drawing/wp:anchor/a:graphic/a:graphicData/wps:wsp/wps:spPr/a:ln/a:solidFill",
-            0);
+            1);
 
     // check direct theme assignments have been preserved
     assertXPath(pXmlDocument,
