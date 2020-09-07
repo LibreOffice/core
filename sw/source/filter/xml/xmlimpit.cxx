9208eab706bfffec306d3611080b86b7bdd27567
Noel Grandin <noel.grandin@collabora.co.uk>
2020-09-07 22:14:55 +0200

    fix crashtest ooo53770-1.odt
    Change-Id: I0a3a63e8c4146c2f8a9ac6593774e6e4d9f5b8ba
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/102201
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

--- sw/source/filter/xml/xmlimpit.cxx
+++ sw/source/filter/xml/xmlimpit.cxx
@@ -19,6 +19,7 @@
 
 #include "xmlimpit.hxx"
 
+#include <sal/log.hxx>
 #include <sax/tools/converter.hxx>
 #include <utility>
 #include <xmloff/xmluconv.hxx>
@@ -226,8 +227,12 @@ void SvXMLImportItemMapper::importXMLUnknownAttributes( SfxItemSet& rSet,
                     sPrefix = sName.copy(0, i-1);
                     sName = sName.copy(i+1);
                 }
-                pUnknownItem->AddAttr( sPrefix, rAttribute.NamespaceURL, sName,
-                                       rAttribute.Value );
+                // the sax parser doesn't reject these, strangely
+                if (sName.indexOf(':') == -1)
+                    pUnknownItem->AddAttr( sPrefix, rAttribute.NamespaceURL, sName,
+                                           rAttribute.Value );
+                else
+                    SAL_WARN("sw", "ignoring dodgy attribute: " + rAttribute.Name);
             }
         }
     }
