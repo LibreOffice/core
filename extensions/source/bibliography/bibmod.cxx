c4c7bfb006dbaa6502130dc62cb34ca78f33f791
Stephan Bergmann <sbergman@redhat.com>
2020-09-07 22:16:54 +0200

    Remove some unused const variables
    Change-Id: Ib27249e9d4c9c7669a2766ec501e02652199c670
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/102202
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

--- extensions/source/bibliography/bibmod.cxx
+++ extensions/source/bibliography/bibmod.cxx
@@ -92,12 +92,5 @@ BibConfig*  BibModul::GetConfig()
 const OUStringLiteral FM_PROP_LABEL = u"Label";
 const OUStringLiteral FM_PROP_CONTROLSOURCE = u"DataField";
 const OUStringLiteral FM_PROP_NAME = u"Name";
-const OUStringLiteral FM_PROP_FORMATKEY = u"FormatKey";
-const OUStringLiteral FM_PROP_EDITMODE = u"RecordMode";
-const OUStringLiteral FM_PROP_CURSORSOURCETYPE = u"DataSelectionType";
-const OUStringLiteral FM_PROP_CURSORSOURCE = u"DataSelection";
-const OUStringLiteral FM_PROP_DATASOURCE = u"DataSource";
-const OUStringLiteral FM_PROP_VALUE = u"Value";
-const OUStringLiteral FM_PROP_TEXT = u"Text";
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
