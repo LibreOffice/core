LibreOffice/core

commit a6032ff5418ad66cc8fec10c636e32b124ee7864

Resolves: tdf#90698 catch list (1;2) of non-references as error
Change-Id: Icc6f93bbf85df245ba332ce89791a1c8d266b1c6
Reviewed-on: https://gerrit.libreoffice.org/61639
Reviewed-by: Eike Rathke <erack@redhat.com>
Tested-by: Jenkins

formula/source/core/api/FormulaCompiler.cxx
@@ -1401,18 +1401,37 @@ void FormulaCompiler::Factor()
             NextToken();
             CheckSetForceArrayParameter( mpToken, 0);
             eOp = Expression();
-            // Do not ignore error here, regardless of bIgnoreErrors, otherwise
-            // errors like =(1;) would also result in display of =(1~)
+            // Do not ignore error here, regardless of mbStopOnError, to not
+            // change the formula expression in case of an unexpected state.
             if (pArr->GetCodeError() == FormulaError::NONE)
             {
-                pFacToken->NewOpCode( ocUnion, FormulaToken::PrivateAccess());
-                PutCode( pFacToken);
+                // Left and right operands must be reference or function
+                // returning reference to form a range list.
+                const FormulaToken* p;
+                if (pc >= 2
+                        && ((p = pCode[-2]) != nullptr) && isPotentialRangeType( p, true, false)
+                        && ((p = pCode[-1]) != nullptr) && isPotentialRangeType( p, true, true))
+                {
+                    pFacToken->NewOpCode( ocUnion, FormulaToken::PrivateAccess());
+                    PutCode( pFacToken);
+                }
             }
         }
         if (eOp != ocClose)
             SetError( FormulaError::PairExpected);
         else
             NextToken();
+
+        /* TODO: if no conversion to ocUnion is involved this could collect
+         * such expression as a list or (matrix) vector to be passed as
+         * argument for one parameter (which in fact the ocUnion svRefList is a
+         * special case of), which would require a new StackVar type and needed
+         * to be handled by the interpreter for functions that could support it
+         * (i.e. already handle VAR_ARGS or svRefList parameters). This is also
+         * not defined by ODF.
+         * Does Excel handle =SUM((1;2))?
+         * As is, the interpreter catches extraneous uncalculated
+         * subexpressions like 1 of (1;2) as error. */
     }
     else
     {

sc/source/core/tool/interpr4.cxx
@@ -4507,7 +4507,7 @@ StackVar ScInterpreter::Interpret()
             bForcedResultType = false;
     }
 
-    if( sp )
+    if (sp == 1)
     {
         pCur = pStack[ sp-1 ];
         if( pCur->GetOpCode() == ocPush )
@@ -4634,6 +4634,8 @@ StackVar ScInterpreter::Interpret()
         else
             SetError( FormulaError::UnknownStackVariable);
     }
+    else if (sp > 1)
+        SetError( FormulaError::OperatorExpected);
     else
         SetError( FormulaError::NoCode);
 

commit a9b13b02d90297b8337e11c35610a3f504b9c0a7

Resolves: tdf#136364 treat svSep type with ocUnion as range type
So for RPN
sv...Ref(ocPush), sv...Ref(ocPush), svSep(ocUnion), sv...Ref(ocPush)
another svSep(ocUnion) instead of svSep(ocSep) is appended.

Regression from

    commit a6032ff5418ad66cc8fec10c636e32b124ee7864
    CommitDate: Thu Oct 11 11:26:37 2018 +0200

        Resolves: tdf#90698 catch list (1;2) of non-references as error

that introduced a conditional check on operands and operators.

Change-Id: If5da3a781f24f891ff12c4a8f32a2ec92c6b4c8a

formula/source/core/api/FormulaCompiler.cxx
@@ -305,7 +305,7 @@ bool isRangeResultOpCode( OpCode eOp )
     @param  bRight
             If bRPN==false, bRight==false means opcodes for left side are
             checked, bRight==true means opcodes for right side. If bRPN==true
-            it doesn't matter.
+            it doesn't matter except for the ocSep converted to ocUnion case.
  */
 bool isPotentialRangeType( FormulaToken const * pToken, bool bRPN, bool bRight )
 {
@@ -326,6 +326,10 @@ bool isPotentialRangeType( FormulaToken const * pToken, bool bRPN, bool bRight )
         case svExternalDoubleRef:
         case svExternalName:        // could be range
             return true;
+        case svSep:
+            // A special case if a previous ocSep was converted to ocUnion it
+            // stays svSep instead of svByte.
+            return bRPN && !bRight && pToken->GetOpCode() == ocUnion;
         default:
             // Separators are not part of RPN and right opcodes need to be
             // other StackVar types or functions and thus svByte.
@@ -1407,6 +1411,7 @@ void FormulaCompiler::Factor()
                     if (p && isPotentialRangeType( p, true, true))
                     {
                         pFacToken->NewOpCode( ocUnion, FormulaToken::PrivateAccess());
+                        // XXX NOTE: the token's eType is still svSep here!
                         PutCode( pFacToken);
                     }
                 }