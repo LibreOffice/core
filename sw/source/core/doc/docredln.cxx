d8f8cb7a75b8cd054174ca270e1195a3560a6ddc
Julien Nabet <serval2412@yahoo.fr>
2020-09-09 12:22:14 +0200

    micro-optim for SwRedlineTable::DeleteAndDestroyAll
    Change-Id: I3eaafcfc2809223500a3aa907fcd015ad32d14e3

--- sw/source/core/doc/docredln.cxx
+++ sw/source/core/doc/docredln.cxx
@@ -622,6 +622,13 @@ void SwRedlineTable::Remove( size_type nP )
 
 void SwRedlineTable::DeleteAndDestroyAll()
 {
+    // Disable since usability is very low beyond some small number of changes.
+    static bool bDisableRedlineComments = getenv("DISABLE_REDLINE") != nullptr;
+    if (!comphelper::LibreOfficeKit::isActive() || bDisableRedlineComments)
+    {
+        maVector.clear();
+        return;
+    }
     while (!maVector.empty())
     {
         auto const pRedline = maVector.back();
