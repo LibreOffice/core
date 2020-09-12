853d4f33fa7f37295f9207823d713337ec8cda55
Noel Grandin <noelgrandin@gmail.com>
2020-09-12 18:21:44 +0200

    tdf#133327 fix calc loading background color with many cols
    regression from
        commit 7282014e362a1529a36c88eb308df8ed359c2cfa
        tdf#50916 Makes numbers of columns dynamic.
    
    Change-Id: Ic9e1b31d74b11367a5ad1a82480ffe9467c37ad8
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/102535
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
    (cherry picked from commit 4176beb7ef831152ce92ac3fa31314438635ec2c)

--- sc/source/core/data/table2.cxx
+++ sc/source/core/data/table2.cxx
@@ -2599,10 +2599,9 @@ void ScTable::MergeSelectionPattern( ScMergePatternState& rState, const ScMarkDa
 
     for (const sc::ColRowSpan & rSpan : aSpans)
     {
-        SCCOL nEnd = ClampToAllocatedColumns(rSpan.mnEnd);
-        for (SCCOLROW i = rSpan.mnStart; i <= nEnd; ++i)
+        for (SCCOLROW i = rSpan.mnStart; i <= rSpan.mnEnd; ++i)
         {
-            aCol[i].MergeSelectionPattern( rState, rMark, bDeep );
+            CreateColumnIfNotExists(i).MergeSelectionPattern( rState, rMark, bDeep );
         }
     }
 }
