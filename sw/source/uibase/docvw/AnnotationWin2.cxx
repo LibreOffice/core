fc0493e59be95f101d26144e622998c38ac3fd45
Pranam Lashkari <lpranam@collabora.com>
2020-09-13 17:30:21 +0530

    changed FN_RESOLVE_NOTE behaviour to resolve single note
    Earlier it used to resolve the entire thread
    and there was no way to resolve a single comment in thread
    
    Change-Id: I471c5b575dd365d52653835be5b0d40009bb5cfa
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/102580
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Andras Timar <andras.timar@collabora.com>

--- sw/source/uibase/docvw/AnnotationWin2.cxx
+++ sw/source/uibase/docvw/AnnotationWin2.cxx
@@ -179,7 +179,7 @@ void SwAnnotationWin::Paint(vcl::RenderContext& rRenderContext, const tools::Rec
         }
 
         sal_uInt32 boxHeight = mpMetadataAuthor->GetSizePixel().Height() + mpMetadataDate->GetSizePixel().Height();
-        boxHeight += IsThreadResolved() ? mpMetadataResolved->GetSizePixel().Height() : 0;
+        boxHeight += IsResolved() ? mpMetadataResolved->GetSizePixel().Height() : 0;
 
         rRenderContext.SetLineColor();
         tools::Rectangle aRectangle(Point(mpMetadataAuthor->GetPosPixel().X() + mpMetadataAuthor->GetSizePixel().Width(),
@@ -583,7 +583,7 @@ void SwAnnotationWin::InitControls()
     mpSidebarTextControl->Show();
     mpMetadataAuthor->Show();
     mpMetadataDate->Show();
-    if(IsThreadResolved()) { mpMetadataResolved->Show(); }
+    if(IsResolved()) { mpMetadataResolved->Show(); }
     mpVScrollbar->Show();
 }
 
@@ -905,7 +905,7 @@ void SwAnnotationWin::DoResize()
 
     aHeight -= GetMetaHeight();
     mpMetadataAuthor->Show();
-    if(IsThreadResolved()) { mpMetadataResolved->Show(); }
+    if(IsResolved()) { mpMetadataResolved->Show(); }
     mpMetadataDate->Show();
     mpSidebarTextControl->SetQuickHelpText(OUString());
     unsigned int numFields = GetNumFields();
@@ -930,7 +930,7 @@ void SwAnnotationWin::DoResize()
                                          aHeight + aSizeOfMetadataControls.Height(),
                                          aSizeOfMetadataControls.Width(),
                                          aSizeOfMetadataControls.Height() );
-        if(IsThreadResolved()) {
+        if(IsResolved()) {
             mpMetadataResolved->setPosSizePixel( 0,
                                                  aHeight + aSizeOfMetadataControls.Height()*2,
                                                  aSizeOfMetadataControls.Width(),
@@ -1267,8 +1267,7 @@ void SwAnnotationWin::ExecuteCommand(sal_uInt16 nSlot)
             mnEventId = Application::PostUserEvent( LINK( this, SwAnnotationWin, DeleteHdl), nullptr, true );
             break;
         case FN_RESOLVE_NOTE:
-            GetTopReplyNote()->ToggleResolved();
-            mrMgr.UpdateResolvedStatus(GetTopReplyNote());
+            ToggleResolved();
             DoResize();
             Invalidate();
             mrMgr.LayoutPostIts();
@@ -1392,7 +1391,7 @@ sal_Int32 SwAnnotationWin::GetMetaHeight()
 
 sal_Int32 SwAnnotationWin::GetNumFields()
 {
-    return IsThreadResolved() ? 3 : 2;
+    return IsResolved() ? 3 : 2;
 }
 
 sal_Int32 SwAnnotationWin::GetMinimumSizeWithMeta() const
