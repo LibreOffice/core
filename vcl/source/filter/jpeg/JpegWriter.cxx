LibreOffice/core

commit 61e8387fa8ab55a04b95839c7c385c163d6de1cd

tdf#128992 Compressing PNG files with alpha channel get a black background
regression from
    commit d4442ac1ac9aae36dbc08fda8154d71ea0f81708
    Date:   Wed May 30 13:02:25 2018 +0200
    drop Graphic::GetBitmap

Change-Id: I937f89255eebc68dffc2987e628a30bc7d2eed7b
Reviewed-on: https://gerrit.libreoffice.org/84412
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
(cherry picked from commit 1187e2b35bb864808d9d158f0b10d83ce75dbaa7)
Reviewed-on: https://gerrit.libreoffice.org/84428
(cherry picked from commit 54a4c75bd35ba684927862f4e859623e91ebcfe8)
Reviewed-on: https://gerrit.libreoffice.org/84542
Reviewed-by: Xisco Faulí <xiscofauli@libreoffice.org>

vcl/source/filter/jpeg/JpegWriter.cxx
@@ -197,7 +197,8 @@ bool JPEGWriter::Write( const Graphic& rGraphic )
         mxStatusIndicator->start( OUString(), 100 );
     }
 
-    Bitmap aGraphicBmp( rGraphic.GetBitmapEx().GetBitmap() );
+    Color replaceColor = COL_WHITE;
+    Bitmap aGraphicBmp( rGraphic.GetBitmapEx().GetBitmap(&replaceColor) );
 
     if ( mbGreys )
     {

commit 2d2711d78b26c9c19af25a3e34e6e8acd58a0281

tdf#132561 fix png export from Draw
fixes a regression from
    commit 61e8387fa8ab55a04b95839c7c385c163d6de1cd
    tdf#128992 Compressing PNG files with alpha channel get a black background

Change-Id: I8a225f76072c49f64393bb4b28c4d92c6e85587e

vcl/source/filter/jpeg/JpegWriter.cxx
@@ -197,7 +197,14 @@ bool JPEGWriter::Write( const Graphic& rGraphic )
         mxStatusIndicator->start( OUString(), 100 );
     }
 
-    Bitmap aGraphicBmp( rGraphic.GetBitmapEx().GetBitmap(COL_WHITE) );
+    // This slightly weird logic is here to match the behaviour in ImpGraphic::ImplGetBitmap
+    // and is necessary to match pre-existing behaviour. We should probably pass down the expected
+    // background color for alpha from the higher layers.
+    Bitmap aGraphicBmp;
+    if (rGraphic.GetType() == GraphicType::Bitmap)
+        rGraphic.GetBitmapEx().GetBitmap(COL_WHITE);
+    else
+        rGraphic.GetBitmapEx().GetBitmap();
 
     if ( mbGreys )
     {