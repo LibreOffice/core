794736b4eaa928eeab200508ee617b3ba752010a
Szabolcs Toth <toth.szabolcs@nisz.hu>
2020-09-14 17:16:51 +0200

    tdf#123610 DOCX shape import: keep text upright
    Grab-bag attribute upright to keep text upright
    regardless of shape rotation, fixing the text
    direction in MSO after a round-trip.
    
    Co-authored-by: Balázs Regényi
    
    Change-Id: If18a12c7728317a7af093048b07bc392d2964389

--- oox/source/export/drawingml.cxx
+++ oox/source/export/drawingml.cxx
@@ -2891,6 +2891,24 @@ void DrawingML::WriteText(const Reference<XInterface>& rXIface, bool bBodyPr, bo
     else if( bVertical && eHorizontalAlignment == TextHorizontalAdjust_LEFT )
         sVerticalAlignment = "b";
 
+    bool isUpright = false;
+    if (GetProperty(rXPropSet, "InteropGrabBag"))
+    {
+        if (rXPropSet->getPropertySetInfo()->hasPropertyByName("InteropGrabBag"))
+        {
+            uno::Sequence<beans::PropertyValue> aGrabBag;
+            rXPropSet->getPropertyValue("InteropGrabBag") >>= aGrabBag;
+            for (auto& aProp : aGrabBag)
+            {
+                if (aProp.Name == "Upright")
+                {
+                    aProp.Value >>= isUpright;
+                    break;
+                }
+            }
+        }
+    }
+
     bool bHasWrap = false;
     bool bWrap = false;
     // Only custom shapes obey the TextWordWrap option, normal text always wraps.
@@ -2922,6 +2940,7 @@ void DrawingML::WriteText(const Reference<XInterface>& rXIface, bool bBodyPr, bo
                                XML_anchor, sVerticalAlignment,
                                XML_anchorCtr, sax_fastparser::UseIf("1", bHorizontalCenter),
                                XML_vert, sWritingMode,
+                               XML_upright, isUpright ? "1" : "0",
                                XML_rot, sax_fastparser::UseIf(oox::drawingml::calcRotationValue((nTextPreRotateAngle + nTextRotateAngle) * 100), (nTextPreRotateAngle + nTextRotateAngle) != 0));
         if (bIsFontworkShape)
         {
