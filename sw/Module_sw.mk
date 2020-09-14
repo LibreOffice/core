d3a874cedc6129bace47fbee4b781e7a5e30b65f
Miklos Vajna <vmiklos@collabora.com>
2020-09-14 21:02:31 +0200

    tdf#135682 sw: fix lost selection-all when doc starts with table
    Regression from commit c56bf1479cc71d1a2b0639f6383e90c1f7e3655b
    (tdf#105330 sw: fix lost cursor on undoing nested table insert,
    2019-09-16), the problem was that the change reverted lcl_notifyRow()
    back to its original state, because it seemed the conditional
    notification is no longer needed. However, this broke the fix for
    tdf#37606 (ability to select-all when the doc starts with a table).
    
    Fix the problem by handling the starts-with-table case similar to a
    normal table selection, so there is still no need to restore the nested
    table visitor code but select-all works nicely with starts-with-table
    documents again.
    
    (cherry picked from commit 6f1e02c96b887750f974c187a82ecd6236e6a435)
    
     Conflicts:
    	sw/qa/core/crsr/crsr.cxx
    
    Change-Id: Icb823a39432d1774a63a0c633c172bba827ac76d
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/102706
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>

--- sw/Module_sw.mk
+++ sw/Module_sw.mk
@@ -113,6 +113,7 @@ $(eval $(call gb_Module_add_slowcheck_targets,sw,\
     CppunitTest_sw_core_objectpositioning \
     CppunitTest_sw_core_layout \
     CppunitTest_sw_core_unocore \
+    CppunitTest_sw_core_crsr \
 ))
 
 ifneq ($(DISABLE_GUI),TRUE)
