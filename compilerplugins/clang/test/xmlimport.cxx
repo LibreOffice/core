LibreOffice/core

commit 998308c363dfad03143591aa18256d2669b4da11

use more FastParser in SvXMLStylesContext
Change-Id: I05c7314739246a864b16723c13bd8fbb4ef725e7
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/87146
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sw/source/filter/xml/xmlfmt.cxx
@@ -704,8 +704,6 @@ public:
 
     SwXMLStylesContext_Impl(
             SwXMLImport& rImport,
-            const OUString& rLName ,
-            const uno::Reference< xml::sax::XAttributeList > & xAttrList,
             bool bAuto );
 
     virtual bool InsertStyleFamily( sal_uInt16 nFamily ) const override;
@@ -805,10 +803,9 @@ SvXMLStyleContext *SwXMLStylesContext_Impl::CreateDefaultStyleStyleChildContext(
 }
 
 SwXMLStylesContext_Impl::SwXMLStylesContext_Impl(
-        SwXMLImport& rImport, const OUString& rLName,
-        const uno::Reference< xml::sax::XAttributeList > & xAttrList,
+        SwXMLImport& rImport,
         bool bAuto ) :
-    SvXMLStylesContext( rImport, XML_NAMESPACE_OFFICE, rLName, xAttrList, bAuto )
+    SvXMLStylesContext( rImport, bAuto )
 {
 }
 
@@ -910,10 +907,7 @@ protected:
 public:
 
 
-    SwXMLMasterStylesContext_Impl(
-            SwXMLImport& rImport,
-            const OUString& rLName ,
-            const uno::Reference< xml::sax::XAttributeList > & xAttrList );
+    SwXMLMasterStylesContext_Impl( SwXMLImport& rImport );
 
     virtual void EndElement() override;
 };
@@ -921,10 +915,8 @@ public:
 }
 
 SwXMLMasterStylesContext_Impl::SwXMLMasterStylesContext_Impl(
-        SwXMLImport& rImport,
-        const OUString& rLName ,
-        const uno::Reference< xml::sax::XAttributeList > & xAttrList ) :
-    XMLTextMasterStylesContext( rImport, XML_NAMESPACE_OFFICE, rLName, xAttrList )
+        SwXMLImport& rImport ) :
+    XMLTextMasterStylesContext( rImport )
 {
 }
 
@@ -949,13 +941,9 @@ void SwXMLMasterStylesContext_Impl::EndElement()
 }
 
 SvXMLImportContext *SwXMLImport::CreateStylesContext(
-        const OUString& rLocalName,
-        const uno::Reference< xml::sax::XAttributeList > & xAttrList,
         bool bAuto )
 {
-    SvXMLStylesContext *pContext =
-        new SwXMLStylesContext_Impl( *this, rLocalName,
-                                       xAttrList, bAuto );
+    SvXMLStylesContext *pContext = new SwXMLStylesContext_Impl( *this, bAuto );
     if( bAuto )
         SetAutoStyles( pContext );
     else
@@ -964,12 +952,10 @@ SvXMLImportContext *SwXMLImport::CreateStylesContext(
     return pContext;
 }
 
-SvXMLImportContext *SwXMLImport::CreateMasterStylesContext(
-        const OUString& rLocalName,
-        const uno::Reference< xml::sax::XAttributeList > & xAttrList )
+SvXMLImportContext *SwXMLImport::CreateMasterStylesContext()
 {
     SvXMLStylesContext *pContext =
-        new SwXMLMasterStylesContext_Impl( *this, rLocalName, xAttrList );
+        new SwXMLMasterStylesContext_Impl( *this );
     SetMasterStyles( pContext );
 
     return pContext;

dbaccess/source/filter/xml/xmlfilter.cxx
@@ -426,33 +426,23 @@ public:
     {
     }
 
-    virtual SvXMLImportContextRef CreateChildContext(sal_uInt16 const nPrefix,
-        const OUString& rLocalName,
-        const uno::Reference<xml::sax::XAttributeList> & xAttrList) override
+    virtual uno::Reference< xml::sax::XFastContextHandler > SAL_CALL createFastChildContext(
+        sal_Int32 nElement, const uno::Reference< xml::sax::XFastAttributeList >& /*xAttrList*/ ) override
     {
-        SvXMLImportContext *pContext = nullptr;
-
         ODBFilter & rImport(static_cast<ODBFilter&>(GetImport()));
-        const SvXMLTokenMap& rTokenMap = rImport.GetDocContentElemTokenMap();
-        switch (rTokenMap.Get(nPrefix, rLocalName))
+        switch (nElement)
         {
-            case XML_TOK_CONTENT_STYLES:
+            case XML_ELEMENT(OFFICE, XML_STYLES):
+            case XML_ELEMENT(OOO, XML_STYLES):
                 rImport.GetProgressBarHelper()->Increment( PROGRESS_BAR_STEP );
-                pContext = rImport.CreateStylesContext(nPrefix, rLocalName, xAttrList, false);
+                return rImport.CreateStylesContext(false);
                 break;
-            case XML_TOK_CONTENT_AUTOSTYLES:
+            case XML_ELEMENT(OFFICE, XML_AUTOMATIC_STYLES):
+            case XML_ELEMENT(OOO, XML_AUTOMATIC_STYLES):
                 rImport.GetProgressBarHelper()->Increment( PROGRESS_BAR_STEP );
-                pContext = rImport.CreateStylesContext(nPrefix, rLocalName, xAttrList, true);
-                break;
-            default:
+                return rImport.CreateStylesContext(true);
                 break;
         }
-
-        return pContext;
-    }
-    virtual uno::Reference< xml::sax::XFastContextHandler > SAL_CALL createFastChildContext(
-        sal_Int32 /*nElement*/, const uno::Reference< xml::sax::XFastAttributeList >& /*xAttrList*/ ) override
-    {
         return nullptr;
     }
 };
@@ -492,28 +482,9 @@ public:
     {
     }
 
-    virtual SvXMLImportContextRef CreateChildContext(sal_uInt16 const nPrefix,
-        const OUString& rLocalName,
-        const uno::Reference<xml::sax::XAttributeList> & xAttrList) override
-    {
-        SvXMLImportContext *pContext = nullptr;
-
-        ODBFilter & rImport(static_cast<ODBFilter&>(GetImport()));
-        const SvXMLTokenMap& rTokenMap = rImport.GetDocContentElemTokenMap();
-        switch (rTokenMap.Get(nPrefix, rLocalName))
-        {
-            case XML_TOK_CONTENT_AUTOSTYLES:
-                rImport.GetProgressBarHelper()->Increment( PROGRESS_BAR_STEP );
-                pContext = rImport.CreateStylesContext(nPrefix, rLocalName, xAttrList, true);
-                break;
-            default:
-                break;
-        }
-
-        return pContext;
-    }
     virtual void SAL_CALL startFastElement( sal_Int32 /*nElement*/,
             const css::uno::Reference< css::xml::sax::XFastAttributeList >& ) override {}
+
     virtual uno::Reference< xml::sax::XFastContextHandler > SAL_CALL createFastChildContext(
         sal_Int32 nElement, const uno::Reference< xml::sax::XFastAttributeList >& /*xAttrList*/ ) override
     {
@@ -526,6 +497,11 @@ public:
                 break;
             case XML_ELEMENT(OFFICE, XML_SCRIPTS):
                 return new XMLScriptContext(GetImport(), rImport.GetModel());
+                break;
+            case XML_ELEMENT(OFFICE, XML_AUTOMATIC_STYLES):
+            case XML_ELEMENT(OOO, XML_AUTOMATIC_STYLES):
+                rImport.GetProgressBarHelper()->Increment( PROGRESS_BAR_STEP );
+                return rImport.CreateStylesContext(true);
                 break;
             default: break;
         }
@@ -634,10 +610,9 @@ const SvXMLTokenMap& ODBFilter::GetDocContentElemTokenMap() const
 
 
 
-SvXMLImportContext* ODBFilter::CreateStylesContext(sal_uInt16 _nPrefix,const OUString& rLocalName,
-                                     const uno::Reference< XAttributeList>& xAttrList, bool bIsAutoStyle )
+SvXMLImportContext* ODBFilter::CreateStylesContext( bool bIsAutoStyle )
 {
-    SvXMLImportContext *pContext = new OTableStylesContext(*this, _nPrefix, rLocalName, xAttrList, bIsAutoStyle);
+    SvXMLImportContext *pContext = new OTableStylesContext(*this, bIsAutoStyle);
     if (bIsAutoStyle)
         SetAutoStyles(static_cast<SvXMLStylesContext*>(pContext));
     else

dbaccess/source/filter/xml/xmlStyleImport.cxx
@@ -134,11 +134,8 @@ ODBFilter& OTableStyleContext::GetOwnImport()
 
 
 OTableStylesContext::OTableStylesContext( SvXMLImport& rImport,
-                                          sal_uInt16 nPrfx ,
-                                          const OUString& rLName ,
-                                          const Reference< XAttributeList > & xAttrList,
-                                          const bool bTempAutoStyles )
-    : SvXMLStylesContext( rImport, nPrfx, rLName, xAttrList )
+                                          bool bTempAutoStyles )
+    : SvXMLStylesContext( rImport )
     , m_nNumberFormatIndex(-1)
     , m_nMasterPageNameIndex(-1)
     , bAutoStyles(bTempAutoStyles)

dbaccess/source/filter/xml/xmlStyleImport.hxx
@@ -93,10 +93,7 @@ namespace dbaxml
     public:
 
 
-        OTableStylesContext( SvXMLImport& rImport, sal_uInt16 nPrfx ,
-                const OUString& rLName ,
-                const css::uno::Reference< css::xml::sax::XAttributeList > & xAttrList,
-                const bool bAutoStyles );
+        OTableStylesContext( SvXMLImport& rImport, bool bAutoStyles );
         virtual ~OTableStylesContext() override;
 
         virtual void EndElement() override;

dbaccess/source/filter/xml/xmlfilter.hxx
@@ -83,8 +83,7 @@ private:
     static void fillPropertyMap(const Any& _rValue,TPropertyNameMap& _rMap);
 
 public:
-    SvXMLImportContext* CreateStylesContext(sal_uInt16 nPrefix,const OUString& rLocalName,
-                                     const Reference< XAttributeList>& xAttrList, bool bIsAutoStyle );
+    SvXMLImportContext* CreateStylesContext( bool bIsAutoStyle );
 
 protected:
     // SvXMLImport

include/xmloff/XMLTextMasterStylesContext.hxx
@@ -43,6 +43,7 @@ public:
     XMLTextMasterStylesContext( SvXMLImport& rImport, sal_uInt16 nPrfx,
         const OUString& rLName,
         const css::uno::Reference< css::xml::sax::XAttributeList > & xAttrList);
+    XMLTextMasterStylesContext( SvXMLImport& rImport );
 
     virtual ~XMLTextMasterStylesContext() override;
 };

include/xmloff/xmlstyle.hxx
@@ -198,8 +198,17 @@ public:
         const OUString& rLName,
         const css::uno::Reference< css::xml::sax::XAttributeList > & xAttrList,
         bool bAutomatic = false );
+    SvXMLStylesContext( SvXMLImport& rImport,
+        bool bAutomatic = false );
 
     virtual ~SvXMLStylesContext() override;
+
+    virtual void SAL_CALL startFastElement( sal_Int32 /*nElement*/,
+        const css::uno::Reference< css::xml::sax::XFastAttributeList >& ) override {}
+
+    // Create child element.
+    virtual css::uno::Reference< css::xml::sax::XFastContextHandler > SAL_CALL createFastChildContext(
+        sal_Int32 nElement, const css::uno::Reference< css::xml::sax::XFastAttributeList >& AttrList ) override;
 
     // Create child element.
     virtual SvXMLImportContextRef CreateChildContext( sal_uInt16 nPrefix,

sc/source/filter/xml/xmlimprt.cxx
@@ -357,14 +357,6 @@ SvXMLImportContextRef ScXMLDocContext_Impl::CreateChildContext( sal_uInt16 nPref
         if (GetScImport().getImportFlags() & SvXMLImportFlags::FONTDECLS)
             pContext = GetScImport().CreateFontDeclsContext(nPrefix, rLocalName, xAttrList);
         break;
-    case XML_TOK_DOC_STYLES:
-        if (GetScImport().getImportFlags() & SvXMLImportFlags::STYLES)
-            pContext = GetScImport().CreateStylesContext( rLocalName, xAttrList, false);
-        break;
-    case XML_TOK_DOC_AUTOSTYLES:
-        if (GetScImport().getImportFlags() & SvXMLImportFlags::AUTOSTYLES)
-            pContext = GetScImport().CreateStylesContext( rLocalName, xAttrList, true);
-        break;
     case XML_TOK_DOC_MASTERSTYLES:
         if (GetScImport().getImportFlags() & SvXMLImportFlags::MASTERSTYLES)
             pContext = new ScXMLMasterStylesContext( GetImport(), nPrefix, rLocalName,
@@ -398,6 +390,14 @@ uno::Reference< xml::sax::XFastContextHandler > SAL_CALL
         if (GetScImport().getImportFlags() & SvXMLImportFlags::SETTINGS)
             pContext = new XMLDocumentSettingsContext(GetScImport());
         break;
+        case XML_ELEMENT(OFFICE, XML_STYLES):
+            if (GetScImport().getImportFlags() & SvXMLImportFlags::STYLES)
+                pContext = GetScImport().CreateStylesContext( false);
+            break;
+        case XML_ELEMENT(OFFICE, XML_AUTOMATIC_STYLES):
+            if (GetScImport().getImportFlags() & SvXMLImportFlags::AUTOSTYLES)
+                pContext = GetScImport().CreateStylesContext( true);
+            break;
 
         //TODO: handle all other cases
     }
@@ -752,11 +752,10 @@ SvXMLImportContext *ScXMLImport::CreateFontDeclsContext(const sal_uInt16 nPrefix
     return pContext;
 }
 
-SvXMLImportContext *ScXMLImport::CreateStylesContext(const OUString& rLocalName,
-                                                     const uno::Reference<xml::sax::XAttributeList>& xAttrList, bool bIsAutoStyle )
+SvXMLImportContext *ScXMLImport::CreateStylesContext( bool bIsAutoStyle )
 {
     SvXMLImportContext* pContext = new XMLTableStylesContext(
-        *this, XML_NAMESPACE_OFFICE, rLocalName, xAttrList, bIsAutoStyle);
+        *this, bIsAutoStyle);
 
     if (bIsAutoStyle)
         SetAutoStyles(static_cast<SvXMLStylesContext*>(pContext));

commit fc851558a6f51171fb29cac4723fb5c258dd603b

loplugin:xmlimport check for bad conversions to fastparser
add a check for classes which have been partly converted to fastparser,
but not completedly.
This is to help me when I convert stuff.

and it uncovers a bug introduced with
    commit 998308c363dfad03143591aa18256d2669b4da11
    use more FastParser in SvXMLStylesContext

Change-Id: Ib50e7136da10a1a7a346102aa47efef2f543e2ac

compilerplugins/clang/xmlimport.cxx
@@ -52,81 +52,139 @@ public:
     bool VisitCXXMethodDecl(const CXXMethodDecl*);
 };
 
-static bool containsStartFastElementMethod(const CXXRecordDecl* cxxRecordDecl,
-                                           bool& rbFoundImportContext)
+static bool containsStartFastElementMethod(const CXXRecordDecl* cxxRecordDecl)
 {
     auto dc = loplugin::DeclCheck(cxxRecordDecl);
-    if (dc.Class("SvXMLImportContext"))
-    {
-        rbFoundImportContext = true;
-        return false;
-    }
     if (dc.Class("XFastContextHandler"))
         return false;
     for (auto it = cxxRecordDecl->method_begin(); it != cxxRecordDecl->method_end(); ++it)
     {
         auto i = *it;
         if (i->getIdentifier() && i->getName() == "startFastElement")
-        {
-            //            i->dump();
             return true;
-        }
     }
     return false;
 }
+
+
 bool XmlImport::VisitCXXMethodDecl(const CXXMethodDecl* methodDecl)
 {
     auto beginLoc = compat::getBeginLoc(methodDecl);
     if (!beginLoc.isValid() || ignoreLocation(beginLoc))
         return true;
 
+    if (!methodDecl->getIdentifier())
+        return true;
+
     auto cxxRecordDecl = methodDecl->getParent();
     if (!cxxRecordDecl || !cxxRecordDecl->getIdentifier())
         return true;
-    auto className = cxxRecordDecl->getName();
-    if (className == "OOXMLFactory") // writerfilter
-        return true;
-    if (className == "SvXMLLegacyToFastDocHandler" || className == "ImportDocumentHandler"
-        || className == "ExportDocumentHandler") // reportdesign
-        return true;
-    if (className == "XMLEmbeddedObjectExportFilter" || className == "XMLBasicExportFilter"
-        || className == "XMLTransformerBase" || className == "SvXMLMetaExport") // xmloff
-        return true;
-    if (!methodDecl->getIdentifier())
-        return true;
-    if (!(methodDecl->getName() == "createFastChildContext" || methodDecl->getName() == "characters"
-          || methodDecl->getName() == "endFastElement"))
-        return true;
+
     if (loplugin::DeclCheck(cxxRecordDecl).Class("SvXMLImportContext"))
         return true;
 
-    bool foundImportContext = false;
-    if (containsStartFastElementMethod(cxxRecordDecl, foundImportContext))
-        return true;
+    if (methodDecl->getName() == "createFastChildContext" || methodDecl->getName() == "characters"
+          || methodDecl->getName() == "endFastElement")
+    {
+        auto className = cxxRecordDecl->getName();
+        if (className == "OOXMLFactory") // writerfilter
+            return true;
+        if (className == "SvXMLLegacyToFastDocHandler" || className == "ImportDocumentHandler"
+            || className == "ExportDocumentHandler") // reportdesign
+            return true;
+        if (className == "XMLEmbeddedObjectExportFilter" || className == "XMLBasicExportFilter"
+            || className == "XMLTransformerBase" || className == "SvXMLMetaExport") // xmloff
+            return true;
 
-    bool foundStartFastElement = false;
-    CXXBasePaths aPaths;
-    cxxRecordDecl->lookupInBases(
-        [&](const CXXBaseSpecifier* Specifier, CXXBasePath & /*Path*/) -> bool {
-            if (!Specifier->getType().getTypePtr())
+        if (containsStartFastElementMethod(cxxRecordDecl))
+            return true;
+
+        bool foundStartFastElement = false;
+        bool foundImportContext = false;
+
+        CXXBasePaths aPaths;
+        cxxRecordDecl->lookupInBases(
+            [&](const CXXBaseSpecifier* Specifier, CXXBasePath & /*Path*/) -> bool {
+                if (!Specifier->getType().getTypePtr())
+                    return false;
+                const CXXRecordDecl* baseCXXRecordDecl = Specifier->getType()->getAsCXXRecordDecl();
+                if (!baseCXXRecordDecl)
+                    return false;
+                if (baseCXXRecordDecl->isInvalidDecl())
+                    return false;
+                if (loplugin::DeclCheck(baseCXXRecordDecl).Class("SvXMLImportContext"))
+                    foundImportContext |= true;
+                else
+                    foundStartFastElement |= containsStartFastElementMethod(baseCXXRecordDecl);
                 return false;
-            const CXXRecordDecl* baseCXXRecordDecl = Specifier->getType()->getAsCXXRecordDecl();
-            if (!baseCXXRecordDecl)
-                return false;
-            if (baseCXXRecordDecl->isInvalidDecl())
-                return false;
-            foundStartFastElement
-                |= containsStartFastElementMethod(baseCXXRecordDecl, foundImportContext);
-            return false;
-        },
+            },
         aPaths);
 
-    if (foundStartFastElement || !foundImportContext)
-        return true;
+        if (foundImportContext && !foundStartFastElement)
+            report(DiagnosticsEngine::Warning, "must override startFastElement too",
+                   compat::getBeginLoc(methodDecl))
+                << methodDecl->getSourceRange();
+    }
+    else if (methodDecl->getName() == "StartElement" || methodDecl->getName() == "EndElement"
+          || methodDecl->getName() == "Characters")
+    {
+        if (loplugin::DeclCheck(cxxRecordDecl).Class("SchXMLAxisContext"))
+            return true;
+        if (loplugin::DeclCheck(cxxRecordDecl).Class("SchXMLChartContext"))
+            return true;
+        if (loplugin::DeclCheck(cxxRecordDecl).Class("SchXMLParagraphContext"))
+            return true;
+        if (loplugin::DeclCheck(cxxRecordDecl).Class("SchXMLLegendContext"))
+            return true;
+        if (loplugin::DeclCheck(cxxRecordDecl).Class("SchXMLPropertyMappingContext"))
+            return true;
 
-    report(DiagnosticsEngine::Warning, "must override startFastElement too",
-           compat::getBeginLoc(methodDecl))
-        << methodDecl->getSourceRange();
+        bool foundImportContext = false;
+        CXXBasePaths aPaths;
+        cxxRecordDecl->lookupInBases(
+            [&](const CXXBaseSpecifier* Specifier, CXXBasePath & /*Path*/) -> bool {
+                if (!Specifier->getType().getTypePtr())
+                    return false;
+                const CXXRecordDecl* baseCXXRecordDecl = Specifier->getType()->getAsCXXRecordDecl();
+                if (!baseCXXRecordDecl)
+                    return false;
+                if (baseCXXRecordDecl->isInvalidDecl())
+                    return false;
+                if (loplugin::DeclCheck(baseCXXRecordDecl).Class("SvXMLImportContext"))
+                    foundImportContext |= true;
+                return false;
+            },
+        aPaths);
+
+        if (!foundImportContext)
+            return true;
+
+        bool foundConstructor = false;
+        for (auto it = cxxRecordDecl->ctor_begin(); it != cxxRecordDecl->ctor_end(); ++it)
+        {
+            const CXXConstructorDecl* ctor = *it;
+            bool foundInt16 = false;
+            for (auto paramIt = ctor->param_begin(); paramIt != ctor->param_end(); ++paramIt)
+            {
+                const ParmVarDecl* pvd = *paramIt;
+                auto tc = loplugin::TypeCheck(pvd->getType());
+                if (tc.Typedef("sal_uInt16"))
+                    foundInt16 = true;
+                else if (tc.LvalueReference().Const().Class("OUString") && foundInt16)
+                    foundConstructor = true;
+                else
+                    foundInt16 = false;
+                if (tc.LvalueReference().Const().Class("OUString") && pvd->getName() == "rLocalName")
+                    foundConstructor = true;
+            }
+        }
+
+        if (!foundConstructor)
+            report(DiagnosticsEngine::Warning, "overrides startElement, but looks like a fastparser context class, no constructor that takes slowparser args",
+                   compat::getBeginLoc(methodDecl))
+                << methodDecl->getSourceRange();
+    }
+
     return true;
 }
 

reportdesign/source/filter/xml/xmlfilter.cxx
@@ -93,7 +93,7 @@ public:
 
     RptMLMasterStylesContext_Impl(const RptMLMasterStylesContext_Impl&) = delete;
     RptMLMasterStylesContext_Impl& operator=(const RptMLMasterStylesContext_Impl&) = delete;
-    virtual void EndElement() override;
+    virtual void SAL_CALL endFastElement(sal_Int32 nElement) override;
 };
 
 }
@@ -103,7 +103,7 @@ RptMLMasterStylesContext_Impl::RptMLMasterStylesContext_Impl( ORptFilter& rImpor
 {
 }
 
-void RptMLMasterStylesContext_Impl::EndElement()
+void RptMLMasterStylesContext_Impl::endFastElement(sal_Int32 )
 {
     FinishStyles( true );
     GetImport().FinishStyles();

sw/source/filter/xml/xmlfmt.cxx
@@ -738,14 +738,13 @@ protected:
 
 public:
 
-
     SwXMLStylesContext_Impl(
             SwXMLImport& rImport,
             bool bAuto );
 
     virtual bool InsertStyleFamily( XmlStyleFamily nFamily ) const override;
 
-    virtual void EndElement() override;
+    virtual void SAL_CALL endFastElement(sal_Int32 nElement) override;
 };
 
 }
@@ -929,7 +928,7 @@ OUString SwXMLStylesContext_Impl::GetServiceName( XmlStyleFamily nFamily ) const
     return SvXMLStylesContext::GetServiceName( nFamily );
 }
 
-void SwXMLStylesContext_Impl::EndElement()
+void SwXMLStylesContext_Impl::endFastElement(sal_Int32 )
 {
     GetSwImport().InsertStyles( IsAutomaticStyle() );
 }

compilerplugins/clang/test/xmlimport.cxx
@@ -1,4 +1,4 @@
-/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
 /*
  * This file is part of the LibreOffice project.
  *
@@ -12,6 +12,10 @@
 // Cannot include this, makes clang crash
 //#include "xmloff/xmlimp.hxx"
 
+#include <com/sun/star/uno/Reference.hxx>
+
+namespace com::sun::star::xml::sax { class XAttributeList; }
+
 class SvXMLImportContext
 {
 public:
@@ -20,6 +24,12 @@ public:
     virtual void createFastChildContext() {}
     virtual void startFastElement() {}
     virtual void endFastElement() {}
+
+
+    virtual void StartElement( const css::uno::Reference< css::xml::sax::XAttributeList >& ) {}
+    virtual void EndElement() {}
+    virtual void Characters(const OUString&) {}
+
 };
 
 class Test1 : public SvXMLImportContext
@@ -49,6 +59,27 @@ class Test4 : public SvXMLImportContext
 public:
     // expected-error@+1 {{must override startFastElement too [loplugin:xmlimport]}}
     virtual void endFastElement() override;
+};
+
+class Test5 : public SvXMLImportContext
+{
+public:
+    // expected-error@+1 {{overrides startElement, but looks like a fastparser context class, no constructor that takes slowparser args [loplugin:xmlimport]}}
+    virtual void StartElement( const css::uno::Reference< css::xml::sax::XAttributeList >& xAttrList ) override;
+    // expected-error@+1 {{overrides startElement, but looks like a fastparser context class, no constructor that takes slowparser args [loplugin:xmlimport]}}
+    virtual void EndElement() override;
+    // expected-error@+1 {{overrides startElement, but looks like a fastparser context class, no constructor that takes slowparser args [loplugin:xmlimport]}}
+    virtual void Characters(const OUString&) override;
+};
+
+// no warning expected
+class Test6 : public SvXMLImportContext
+{
+public:
+    Test6(sal_uInt16, const OUString&);
+    virtual void StartElement( const css::uno::Reference< css::xml::sax::XAttributeList >& xAttrList ) override;
+    virtual void EndElement() override;
+    virtual void Characters(const OUString&) override;
 };
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */