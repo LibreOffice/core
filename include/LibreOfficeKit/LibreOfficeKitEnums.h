08ea7923f40295ab54ef150f224fbcb0c281b990
gokaysatir <gokaysatir@collabora.com>
2020-09-15 11:35:16 +0300

    Online: Show input help on Online / Core part.
    Change-Id: I9d10179f266a725b770fdae50045fdb5d77178ab

--- include/LibreOfficeKit/LibreOfficeKitEnums.h
+++ include/LibreOfficeKit/LibreOfficeKitEnums.h
@@ -766,6 +766,13 @@ typedef enum
      * and row-groups data have changed.
      */
     LOK_CALLBACK_INVALIDATE_SHEET_GEOMETRY = 50,
+
+    /**
+     * When for the current cell is defined an input help text.
+     *
+     * The payload format is JSON: { "title": "title text", "content": "content text" }
+     */
+    LOK_CALLBACK_VALIDITY_INPUT_HELP = 51,
 }
 LibreOfficeKitCallbackType;
 
@@ -870,6 +877,8 @@ static inline const char* lokCallbackTypeToString(int nType)
         return "LOK_CALLBACK_WINDOW";
     case LOK_CALLBACK_VALIDITY_LIST_BUTTON:
         return "LOK_CALLBACK_VALIDITY_LIST_BUTTON";
+    case LOK_CALLBACK_VALIDITY_INPUT_HELP:
+        return "LOK_CALLBACK_VALIDITY_INPUT_HELP";
     case LOK_CALLBACK_CLIPBOARD_CHANGED:
         return "LOK_CALLBACK_CLIPBOARD_CHANGED";
     case LOK_CALLBACK_CONTEXT_CHANGED:
