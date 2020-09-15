c575d481cc92509bcc11269bd4d8d2434e61b328
Ezinne Nnamani <nnamani.ezinne@collabora.com>
2020-09-16 00:30:21 +0100

    A fix to display the writer page number
    Change-Id: I3b89f176c455b830c48d3c53db75e8ab81c7d9f9

--- sw/source/uibase/uiview/view2.cxx
+++ sw/source/uibase/uiview/view2.cxx
@@ -1379,14 +1379,16 @@ void SwView::StateStatusLine(SfxItemSet &rSet)
                 sal_uInt16 nPage, nLogPage;
                 OUString sDisplay;
                 rShell.GetPageNumber( -1, rShell.IsCursorVisible(), nPage, nLogPage, sDisplay );
-                OUString sTemp( GetPageStr( nPage, nLogPage, sDisplay ) );
-                const SfxStringItem aTmp( FN_STAT_PAGE, sTemp );
-                GetViewFrame()->GetBindings().SetState( aTmp );
+                rSet.Put( SfxStringItem( FN_STAT_PAGE,
+-                            GetPageStr( nPage, nLogPage, sDisplay) ));
                 // Used to distinguish which tooltip to show
-                const SfxBoolItem bExtendedTooltip( FN_STAT_PAGE, !sDisplay.isEmpty() &&
+                if (!comphelper::LibreOfficeKit::isActive())
+                {
+                    const SfxBoolItem bExtendedTooltip( FN_STAT_PAGE, !sDisplay.isEmpty() &&
                                                     OUString::number( nPage ) != sDisplay &&
                                                     nPage != nLogPage );
                 GetViewFrame()->GetBindings().SetState( bExtendedTooltip );
+                }
                 //if existing page number is not equal to old page number, send out this event.
                 if (m_nOldPageNum != nLogPage )
                 {
