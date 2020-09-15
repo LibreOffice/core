727c920d9866718e7b9eb7e25ac8f3ae517fdf0d
Caolán McNamara <caolanm@redhat.com>
2020-09-15 12:15:43 +0100

    IsNameValid never passed a null ScDocument*
    Change-Id: I2bef21f5c6356823b3c0470e7640896740e2ee9c

--- sc/source/ui/app/inputwin.cxx
+++ sc/source/ui/app/inputwin.cxx
@@ -2339,7 +2339,7 @@ static ScNameInputType lcl_GetInputType( const OUString& rText )
             eRet = SC_NAME_INPUT_ROW;
         else if ( rDoc.GetTable( rText, nNameTab ) )
             eRet = SC_NAME_INPUT_SHEET;
-        else if ( ScRangeData::IsNameValid( rText, &rDoc ) == ScRangeData::NAME_VALID )     // nothing found, create new range?
+        else if ( ScRangeData::IsNameValid( rText, rDoc ) == ScRangeData::NAME_VALID )     // nothing found, create new range?
         {
             if ( rViewData.GetSimpleArea( aRange ) == SC_MARK_SIMPLE )
                 eRet = SC_NAME_INPUT_DEFINE;
