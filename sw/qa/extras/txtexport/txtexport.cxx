626088cdb6c9c1eea8debf8c97cbe860b8140eb3
Tomofumi Yagi <yagitmknada@gmail.com>
2020-09-16 18:11:22 +0900

    tdf#60145 sw: fix UTF-8 encoding without BOM is not detected
    Writer can now detect Unicode type even if importing text file does not
    have a BOM.
    
    Change-Id: I70fa4145672d855f9ef9df040a930ff5d1564905

--- sw/qa/extras/txtexport/txtexport.cxx
+++ sw/qa/extras/txtexport/txtexport.cxx
@@ -15,36 +15,6 @@
 #include <unotxdoc.hxx>
 #include <docsh.hxx>
 
-class TxtImportTest : public SwModelTestBase
-{
-public:
-    TxtImportTest() :
-        SwModelTestBase("/sw/qa/extras/txtexport/data/", "Text")
-    {}
-
-    // Export & assert part of the document (defined by SwPaM).
-    void assertExportedRange(const OString& aExpected, SwPaM& rPaM)
-    {
-        WriterRef rAsciiWriter;
-        SwReaderWriter::GetWriter(FILTER_TEXT, OUString(), rAsciiWriter);
-        CPPUNIT_ASSERT(rAsciiWriter.is());
-
-        // no start char
-        rAsciiWriter->m_bUCS2_WithStartChar = false;
-
-        SvMemoryStream aMemoryStream;
-
-        SwWriter aWriter(aMemoryStream, rPaM);
-        ErrCode nError = aWriter.Write(rAsciiWriter);
-        CPPUNIT_ASSERT_EQUAL(ERRCODE_NONE, nError);
-
-        const char* pData = static_cast<const char*>(aMemoryStream.GetData());
-        OString aResult(pData, aMemoryStream.GetSize());
-
-        CPPUNIT_ASSERT_EQUAL(aExpected, aResult);
-    }
-};
-
 class TxtExportTest : public SwModelTestBase
 {
 public:
@@ -68,7 +38,6 @@ protected:
     }
 };
 
-#define DECLARE_TXTIMPORT_TEST(TestName, filename) DECLARE_SW_EXPORT_TEST(TestName, filename, nullptr, TxtImportTest)
 #define DECLARE_TXTEXPORT_TEST(TestName, filename) DECLARE_SW_EXPORT_TEST(TestName, filename, nullptr, TxtExportTest)
 
 DECLARE_TXTEXPORT_TEST(testBullets, "bullets.odt")
@@ -102,35 +71,6 @@ DECLARE_TXTEXPORT_TEST(testBullets, "bullets.odt")
     OString aExpected(OUStringToOString(aString, osl_getThreadTextEncoding()));
 
     CPPUNIT_ASSERT_EQUAL(aExpected, aData);
-}
-
-DECLARE_TXTIMPORT_TEST(testTdf112191, "bullets.odt")
-{
-    SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument *>(mxComponent.get());
-    CPPUNIT_ASSERT(pTextDoc);
-    SwDoc* pDoc = pTextDoc->GetDocShell()->GetDoc();
-    CPPUNIT_ASSERT(pDoc);
-
-    // just the 5th paragraph - no bullet
-    uno::Reference<text::XTextRange> xPara(getParagraph(5));
-    SwUnoInternalPaM aPaM(*pDoc);
-    bool bSuccess = sw::XTextRangeToSwPaM(aPaM, xPara);
-    CPPUNIT_ASSERT(bSuccess);
-
-    assertExportedRange("First bullet", aPaM);
-
-    // but when we extend to the next paragraph - now there are bullets
-    xPara = getParagraph(6);
-    SwUnoInternalPaM aPaM2(*pDoc);
-    bSuccess = sw::XTextRangeToSwPaM(aPaM2, xPara);
-    CPPUNIT_ASSERT(bSuccess);
-
-    OUString aString = OStringToOUString(
-        "    \xe2\x80\xa2 First bullet" SAL_NEWLINE_STRING
-        "    \xe2\x80\xa2 Second bullet", RTL_TEXTENCODING_UTF8);
-
-    SwPaM aPaM3(*aPaM2.GetMark(), *aPaM.GetPoint());
-    assertExportedRange(OUStringToOString(aString, osl_getThreadTextEncoding()), aPaM3);
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();
