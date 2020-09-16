3a7087f62e68728359665cb2da0c31b569d75afe
Jim Raykowski <raykowj@gmail.com>
2020-09-16 11:17:50 -0800

    tdf#89707 Make statusbar page number/bookmarks control tool tip show
    This is a replacement hack for the initial hack that I did to make the
    tool tip show.
    
    Change-Id: I3a84ae29d4758799e1ee7e2ab1349ca3b30fb5f1

--- sw/source/uibase/uiview/view2.cxx
+++ sw/source/uibase/uiview/view2.cxx
@@ -1418,14 +1418,8 @@ void SwView::StateStatusLine(SfxItemSet &rSet)
                 sal_uInt16 nPage, nLogPage;
                 OUString sDisplay;
                 rShell.GetPageNumber( -1, rShell.IsCursorVisible(), nPage, nLogPage, sDisplay );
-                OUString sTemp( GetPageStr( nPage, nLogPage, sDisplay ) );
-                const SfxStringItem aTmp( FN_STAT_PAGE, sTemp );
-                GetViewFrame()->GetBindings().SetState( aTmp );
-                // Used to distinguish which tooltip to show
-                const SfxBoolItem bExtendedTooltip( FN_STAT_PAGE, !sDisplay.isEmpty() &&
-                                                    OUString::number( nPage ) != sDisplay &&
-                                                    nPage != nLogPage );
-                GetViewFrame()->GetBindings().SetState( bExtendedTooltip );
+                rSet.Put( SfxStringItem( FN_STAT_PAGE,
+                            GetPageStr( nPage, nLogPage, sDisplay) ));
                 //if existing page number is not equal to old page number, send out this event.
                 if (m_nOldPageNum != nLogPage )
                 {
