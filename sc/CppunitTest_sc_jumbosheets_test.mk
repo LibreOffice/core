5dda76618585176fe9df7970d99e5c0b4d5949de
Xisco Fauli <xiscofauli@libreoffice.org>
2020-09-17 20:01:38 +0200

    tdf#134392: sc_jumbosheets_test: Add unittest
    Change-Id: Id1a63d6b7fde3f911625573ffce1bed5d2b322df

---
+++ sc/CppunitTest_sc_jumbosheets_test.mk
@@ -0,0 +1,129 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_CppunitTest_CppunitTest,sc_jumbosheets_test))
+
+$(eval $(call gb_CppunitTest_use_common_precompiled_header,sc_jumbosheets_test))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,sc_jumbosheets_test, \
+    sc/qa/unit/jumbosheets-test \
+))
+
+$(eval $(call gb_CppunitTest_use_externals,sc_jumbosheets_test, \
+	boost_headers \
+	mdds_headers \
+))
+
+$(eval $(call gb_CppunitTest_use_libraries,sc_jumbosheets_test, \
+    basegfx \
+    comphelper \
+    cppu \
+    cppuhelper \
+    drawinglayer \
+    editeng \
+    for \
+    forui \
+    i18nlangtag \
+    msfilter \
+    oox \
+    sal \
+    salhelper \
+    sax \
+    sb \
+    sc \
+    scqahelper \
+    sfx \
+    sot \
+    subsequenttest \
+    svl \
+    svt \
+    svx \
+    svxcore \
+	test \
+    tk \
+    tl \
+    ucbhelper \
+	unotest \
+    utl \
+    vbahelper \
+    vcl \
+    xo \
+))
+
+$(eval $(call gb_CppunitTest_set_include,sc_jumbosheets_test,\
+    -I$(SRCDIR)/sc/source/ui/inc \
+    -I$(SRCDIR)/sc/inc \
+	-I$(SRCDIR)/sc/source/filter/inc \
+    $$(INCLUDE) \
+))
+
+$(eval $(call gb_CppunitTest_use_api,sc_jumbosheets_test,\
+	udkapi \
+	offapi \
+	oovbaapi \
+))
+
+$(eval $(call gb_CppunitTest_use_ure,sc_jumbosheets_test))
+$(eval $(call gb_CppunitTest_use_vcl,sc_jumbosheets_test))
+
+$(eval $(call gb_CppunitTest_use_components,sc_jumbosheets_test,\
+	basic/util/sb \
+	basctl/util/basctl \
+    comphelper/util/comphelp \
+    configmgr/source/configmgr \
+    dbaccess/util/dba \
+    embeddedobj/util/embobj \
+    emfio/emfio \
+    eventattacher/source/evtatt \
+    filter/source/config/cache/filterconfig1 \
+    forms/util/frm \
+    framework/util/fwk \
+    i18npool/source/search/i18nsearch \
+    i18npool/util/i18npool \
+    linguistic/source/lng \
+    oox/util/oox \
+    package/source/xstor/xstor \
+    package/util/package2 \
+    sax/source/expatwrap/expwrap \
+    scaddins/source/analysis/analysis \
+    scaddins/source/datefunc/date \
+    sc/util/sc \
+    sc/util/scfilt \
+    sc/util/vbaobj \
+    sfx2/util/sfx \
+    sot/util/sot \
+    svl/util/svl \
+    svl/source/fsstor/fsstorage \
+    svtools/util/svt \
+    toolkit/util/tk \
+    ucb/source/core/ucb1 \
+    ucb/source/ucp/file/ucpfile1 \
+    ucb/source/ucp/tdoc/ucptdoc1 \
+    unotools/util/utl \
+    unoxml/source/rdf/unordf \
+    unoxml/source/service/unoxml \
+    uui/util/uui \
+    vcl/vcl.common \
+    xmloff/util/xo \
+))
+
+$(eval $(call gb_CppunitTest_use_components,sc_jumbosheets_test,))
+
+$(eval $(call gb_CppunitTest_use_externals,sc_jumbosheets_test,\
+	boost_filesystem \
+	boost_system \
+	boost_iostreams \
+	zlib \
+))
+
+$(eval $(call gb_CppunitTest_use_configuration,sc_jumbosheets_test))
+
+$(eval $(call gb_CppunitTest_use_more_fonts,sc_jumbosheets_test))
+
+# vim: set noet sw=4 ts=4:
