f700f91b5de63464a31c127e81356f8ef5a4b4d9
Xisco Fauli <xiscofauli@libreoffice.org>
2020-09-17 20:01:38 +0200

    tdf#134392: sc_jumbosheets_test: Add unittest
    Change-Id: Id1a63d6b7fde3f911625573ffce1bed5d2b322df

---
+++ sc/qa/unit/jumbosheets-test.cxx
@@ -0,0 +1,108 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <memory>
+#include <sal/config.h>
+#include <unotest/filters-test.hxx>
+#include <test/bootstrapfixture.hxx>
+
+#include <defaultsoptions.hxx>
+#include <scmod.hxx>
+
+#include "helper/qahelper.hxx"
+#include "helper/shared_test_impl.hxx"
+
+using namespace ::com::sun::star;
+using namespace ::com::sun::star::uno;
+
+/* Implementation of Filters test */
+
+class ScFiltersTest : public test::FiltersTest, public ScBootstrapFixture
+{
+public:
+    ScFiltersTest();
+
+    virtual bool load(const OUString& rFilter, const OUString& rURL, const OUString& rUserData,
+                      SfxFilterFlags nFilterFlags, SotClipboardFormatId nClipboardID,
+                      unsigned int nFilterVersion) override;
+
+    virtual void setUp() override;
+    virtual void tearDown() override;
+
+    void testTdf134392();
+
+    CPPUNIT_TEST_SUITE(ScFiltersTest);
+
+    CPPUNIT_TEST(testTdf134392);
+
+    CPPUNIT_TEST_SUITE_END();
+
+private:
+    uno::Reference<uno::XInterface> m_xCalcComponent;
+};
+
+bool ScFiltersTest::load(const OUString& rFilter, const OUString& rURL, const OUString& rUserData,
+                         SfxFilterFlags nFilterFlags, SotClipboardFormatId nClipboardID,
+                         unsigned int nFilterVersion)
+{
+    ScDocShellRef xDocShRef = ScBootstrapFixture::load(rURL, rFilter, rUserData, OUString(),
+                                                       nFilterFlags, nClipboardID, nFilterVersion);
+    bool bLoaded = xDocShRef.is();
+    //reference counting of ScDocShellRef is very confused.
+    if (bLoaded)
+        xDocShRef->DoClose();
+    return bLoaded;
+}
+
+void ScFiltersTest::testTdf134392()
+{
+    // Without the fix in place, the file would have crashed
+    ScDocShellRef xDocSh = loadDoc("tdf134392.", FORMAT_XLSX);
+    CPPUNIT_ASSERT(xDocSh.is());
+
+    ScDocument& rDoc = xDocSh->GetDocument();
+    rDoc.CalcAll(); // perform hard re-calculation.
+    xDocSh->DoClose();
+}
+
+ScFiltersTest::ScFiltersTest()
+    : ScBootstrapFixture("sc/qa/unit/data")
+{
+}
+
+void ScFiltersTest::setUp()
+{
+    test::BootstrapFixture::setUp();
+
+    // This is a bit of a fudge, we do this to ensure that ScGlobals::ensure,
+    // which is a private symbol to us, gets called
+    m_xCalcComponent
+        = getMultiServiceFactory()->createInstance("com.sun.star.comp.Calc.SpreadsheetDocument");
+    CPPUNIT_ASSERT_MESSAGE("no calc component!", m_xCalcComponent.is());
+
+    ScDefaultsOptions aDefaultsOption = SC_MOD()->GetDefaultsOptions();
+    aDefaultsOption.SetInitJumboSheets(true);
+    SC_MOD()->SetDefaultsOptions(aDefaultsOption);
+}
+
+void ScFiltersTest::tearDown()
+{
+    uno::Reference<lang::XComponent>(m_xCalcComponent, UNO_QUERY_THROW)->dispose();
+    test::BootstrapFixture::tearDown();
+
+    ScDefaultsOptions aDefaultsOption = SC_MOD()->GetDefaultsOptions();
+    aDefaultsOption.SetInitJumboSheets(false);
+    SC_MOD()->SetDefaultsOptions(aDefaultsOption);
+}
+
+CPPUNIT_TEST_SUITE_REGISTRATION(ScFiltersTest);
+
+CPPUNIT_PLUGIN_IMPLEMENT();
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
