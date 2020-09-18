a49952d5677b5531dc00db7a3b3f8d5c9731cf69
Gülşah Köse <gulsah.kose@collabora.com>
2020-09-18 15:45:17 +0300

    tdf#136247 Change element order of data labels
    Reference OOXML (Appendix B.5.1, line 248)
    
    Change-Id: Idf5c2546b4ad65c8e78ca03e18ecfa575ef17fe8

--- oox/source/export/chartexport.cxx
+++ oox/source/export/chartexport.cxx
@@ -3351,6 +3351,8 @@ void writeLabelProperties( const FSHelperPtr& pFS, ChartExport* pChartExport,
         pFS->endElement(FSNS(XML_c, XML_spPr));
     }
 
+    pChartExport->exportTextProps(xPropSet);
+
     if (aCustomLabelFields.hasElements())
         writeCustomLabel(pFS, pChartExport, aCustomLabelFields);
 
@@ -3517,15 +3519,12 @@ void ChartExport::exportDataLabels(
         }
 
         // Individual label property that overwrites the baseline.
-        exportTextProps( xLabelPropSet );
         writeLabelProperties(pFS, this, xLabelPropSet, aParam);
         pFS->endElement(FSNS(XML_c, XML_dLbl));
     }
 
-    exportTextProps( xPropSet );
     // Baseline label properties for all labels.
     writeLabelProperties(pFS, this, xPropSet, aParam);
-
     pFS->singleElement(FSNS(XML_c, XML_showLeaderLines), XML_val, "0");
 
     // Export leader line
