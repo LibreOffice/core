5a76580bb332ffb4a944c4075a34084977f621ed
Tor Lillqvist <tml@collabora.com>
2020-09-18 12:23:44 +0300

    tdf#128136: Build curl, nss, and xmlsec for iOS, too
    We need to link nss statically, including the three dylibs that
    normally are loaded at run-time, because including bare dylibs in an
    iOS appp on the App Store is not OK. See
    https://developer.apple.com/forums/thread/125796 .
    
    For linking the softokn3 library statically, NSS already had code,
    behind NSS_STATIC_SOFTOKEN ifdefs. Introduce two more macros:
    NSS_STATIC_FREEBL for the freebl library and NSS_STATIC_PKCS11 for the
    nssckbi library.
    
    Turn off parallelism for the sub-make building nss. There seems to be
    race conditions or something in both the nsinstall.py script and the
    nsinstall program in nss (used when building nss for the build
    platform).
    
    Always use python3 to run the nsinstall.py script (when
    cross-compiling), as it is Python 3.
    
    Change-Id: Idd427b5ebf21f802b3feb0d5a3d259317ba8fc67

--- external/curl/UnpackedTarball_curl.mk
+++ external/curl/UnpackedTarball_curl.mk
@@ -24,6 +24,7 @@ $(eval $(call gb_UnpackedTarball_add_patches,curl,\
 	external/curl/curl-msvc-disable-protocols.patch.1 \
 	external/curl/curl-7.26.0_win-proxy.patch \
 	external/curl/zlib.patch.0 \
+    external/curl/curl-ios.patch.1 \
 ))
 
 ifeq ($(SYSTEM_NSS),)
