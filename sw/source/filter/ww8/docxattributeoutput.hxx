8679bf3ec608aec277fd677082aa5c38e63a65ce
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2020-09-22 07:29:34 +0200

    docx export: Use checksum as key to cache graphic, not Graphic*
    The problem is when we have multiple identical images in the
    document and try to export. Without this change, the images will
    be duplicated because the cache is using Graphic*, for which the
    instance changes all the time (is not unique). Using the checksum
    will make sure that all the images with the same content will be
    saved as one image into the document.
    
    Change-Id: I980af2dba51060ce4320571aca14d21e26ed8976
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/103132
    Tested-by: Jenkins
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

--- sw/source/filter/ww8/docxattributeoutput.hxx
+++ sw/source/filter/ww8/docxattributeoutput.hxx
@@ -958,8 +958,12 @@ private:
     // store hardcoded value which was set during import.
     sal_Int32 m_nParaBeforeSpacing,m_nParaAfterSpacing;
 
+    OString getExistingGraphicRelId(BitmapChecksum aChecksum);
+    void cacheGraphicRelId(BitmapChecksum nChecksum, OString const & rRelId);
+
     /// RelId <-> Graphic* cache, so that in case of alternate content, the same graphic only gets written once.
-    std::stack< std::map<const Graphic*, OString> > m_aRelIdCache;
+    std::stack< std::map<BitmapChecksum, OString> > m_aRelIdCache;
+
     /// RelId <-> BitmapChecksum cache, similar to m_aRelIdCache, but used for non-Writer graphics, handled in oox.
     std::stack< std::map<BitmapChecksum, OUString> > m_aSdrRelIdCache;
 
