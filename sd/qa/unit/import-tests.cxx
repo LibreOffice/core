1b7aeb082d20326b8b761ddf8eb0764f1ade28ce
Xisco Fauli <xiscofauli@libreoffice.org>
2020-09-22 18:31:51 +0200

    tdf#136949: sd_import_tests: Add unittest
    Change-Id: I3d3f7719aa211dfba1656730803efdc2b05143c0

--- sd/qa/unit/import-tests.cxx
+++ sd/qa/unit/import-tests.cxx
@@ -173,6 +173,7 @@ public:
     void testTdf93097();
     void testTdf62255();
     void testTdf93124();
+    void testTdf136949();
     void testTdf99729();
     void testTdf89927();
     void testTdf93868();
@@ -285,6 +286,7 @@ public:
     CPPUNIT_TEST(testTdf93097);
     CPPUNIT_TEST(testTdf62255);
     CPPUNIT_TEST(testTdf93124);
+    CPPUNIT_TEST(testTdf136949);
     CPPUNIT_TEST(testTdf99729);
     CPPUNIT_TEST(testTdf89927);
     CPPUNIT_TEST(testTdf93868);
@@ -1612,6 +1614,66 @@ void SdImportTest::testTdf93124()
         }
         CPPUNIT_ASSERT_MESSAGE("Tdf93124: vertical alignment of text is incorrect!", nNonWhiteCount>50);
     }
+    xDocShRef->DoClose();
+}
+
+void SdImportTest::testTdf136949()
+{
+    sd::DrawDocShellRef xDocShRef = loadURL(m_directories.getURLFromSrc("/sd/qa/unit/data/odp/td136949.odp"), ODP);
+
+    uno::Reference<drawing::XShape> xTableShape(getShapeFromPage(0, 0, xDocShRef), uno::UNO_QUERY);
+
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(-718), xTableShape->getPosition().X);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(-1061), xTableShape->getPosition().Y);
+
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(17858), xTableShape->getSize().Height);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(32907), xTableShape->getSize().Width);
+
+    uno::Reference < uno::XComponentContext > xContext = ::comphelper::getProcessComponentContext();
+    uno::Reference< drawing::XGraphicExportFilter > xGraphicExporter = drawing::GraphicExportFilter::create(xContext);
+
+    uno::Sequence< beans::PropertyValue > aFilterData(2);
+    aFilterData[0].Name = "PixelWidth";
+    aFilterData[0].Value <<= sal_Int32(100);
+    aFilterData[1].Name = "PixelHeight";
+    aFilterData[1].Value <<= sal_Int32(100);
+
+    utl::TempFile aTempFile;
+    aTempFile.EnableKillingFile();
+
+    uno::Sequence< beans::PropertyValue > aDescriptor(3);
+    aDescriptor[0].Name = "URL";
+    aDescriptor[0].Value <<= aTempFile.GetURL();
+    aDescriptor[1].Name = "FilterName";
+    aDescriptor[1].Value <<= OUString("PNG");
+    aDescriptor[2].Name = "FilterData";
+    aDescriptor[2].Value <<= aFilterData;
+
+    uno::Reference< lang::XComponent > xPage(getPage(0, xDocShRef), uno::UNO_QUERY);
+    xGraphicExporter->setSourceDocument(xPage);
+    xGraphicExporter->filter(aDescriptor);
+
+    SvFileStream aFileStream(aTempFile.GetURL(), StreamMode::READ);
+    vcl::PNGReader aPNGReader(aFileStream);
+    BitmapEx aBMPEx = aPNGReader.Read();
+
+    // make sure the bitmap is not empty and correct size (PNG export->import was successful)
+    CPPUNIT_ASSERT_EQUAL(Size(100, 100), aBMPEx.GetSizePixel());
+    Bitmap aBMP = aBMPEx.GetBitmap();
+    Bitmap::ScopedReadAccess pReadAccess(aBMP);
+    int nNonWhiteCount = 0;
+
+    for (long nY = 0; nY < 100; ++nY)
+    {
+        for (long nX = 0; nX < 100; ++nX)
+        {
+            const Color aColor = pReadAccess->GetColor(nY, nX);
+            if ((aColor.GetRed() != 0xff) || (aColor.GetGreen() != 0xff) || (aColor.GetBlue() != 0xff))
+                ++nNonWhiteCount;
+        }
+    }
+    // Check the table borders are not displayed on the slide
+    CPPUNIT_ASSERT_MESSAGE("Tdf136949: the whole slide should be white", nNonWhiteCount == 0);
     xDocShRef->DoClose();
 }
 
