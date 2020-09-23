LibreOffice/core

commit 86ea64f216819696cd86d1926aff0a138ace2baf

Support for native 32bit Bitmap in VCL and SVP (cairo) backend
This adds basic support for 32bit bitmaps for the SVP backend. For 
other backends the support is disabled for now as we need to add it for 
each backend separately and enable support.

When this patch is applied it is possible to a Bitmap with bit count 
32, but currently no input filter uses this with the exception of the 
new PngImageReader(libpng based), which is used only for the icons.

For a general support more things need to be implemented and tested:
- conversion back and fourth between 32-bit and 24-bit + 8bit alpha (or 
other supported pairs) 
- 'raw' export of the bitmap needs to be handeled properly (like in
SVM import/export) so it creates the correct image.
- input filters need to be checked and converted if this is necessary
- look for possible bugs when drawing transparent bitmaps
- check of UNO API

Change-Id: I7a7be0e6134dfdd9a7aeaef897131bb6e710ae7e
Reviewed-on: https://gerrit.libreoffice.org/69289
Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>
Tested-by: Tomaž Vajngerl <quikee@gmail.com>

vcl/source/filter/png/PngImageReader.cxx
@@ -14,6 +14,9 @@
 #include <bitmapwriteaccess.hxx>
 #include <vcl/bitmap.hxx>
 #include <vcl/alpha.hxx>
+#include <vcl/BitmapTools.hxx>
+#include <svdata.hxx>
+#include <salinst.hxx>
 
 namespace
 {
@@ -32,13 +35,14 @@ void lclReadStream(png_structp pPng, png_bytep pOutBytes, png_size_t nBytesToRea
         png_error(pPng, "Error reading");
 }
 
-bool reader(SvStream& rStream, BitmapEx& rBitmapEx)
+bool reader(SvStream& rStream, BitmapEx& rBitmapEx, bool bUseBitmap32)
 {
     enum
     {
         PNG_SIGNATURE_SIZE = 8
     };
 
+    // Check signature bytes
     sal_uInt8 aHeader[PNG_SIGNATURE_SIZE];
     rStream.ReadBytes(aHeader, PNG_SIGNATURE_SIZE);
 
@@ -62,9 +66,11 @@ bool reader(SvStream& rStream, BitmapEx& rBitmapEx)
         return false;
     }
 
+    png_set_option(pPng, PNG_MAXIMUM_INFLATE_WINDOW, PNG_OPTION_ON);
+
     png_set_read_fn(pPng, &rStream, lclReadStream);
 
-    png_set_crc_action(pPng, PNG_CRC_WARN_USE, PNG_CRC_WARN_DISCARD);
+    png_set_crc_action(pPng, PNG_CRC_ERROR_QUIT, PNG_CRC_WARN_DISCARD);
 
     png_set_sig_bytes(pPng, PNG_SIGNATURE_SIZE);
 
@@ -84,9 +90,6 @@ bool reader(SvStream& rStream, BitmapEx& rBitmapEx)
         png_destroy_read_struct(&pPng, &pInfo, nullptr);
         return false;
     }
-
-    Bitmap aBitmap(Size(width, height), 24);
-    AlphaMask aBitmapAlpha(Size(width, height), nullptr);
 
     if (colorType == PNG_COLOR_TYPE_PALETTE)
         png_set_palette_to_rgb(pPng);
@@ -135,61 +138,114 @@ bool reader(SvStream& rStream, BitmapEx& rBitmapEx)
         {
             size_t aRowSizeBytes = png_get_rowbytes(pPng, pInfo);
 
-            BitmapScopedWriteAccess pWriteAccess(aBitmap);
-            ScanlineFormat eFormat = pWriteAccess->GetScanlineFormat();
-            if (eFormat == ScanlineFormat::N24BitTcBgr)
-                png_set_bgr(pPng);
-
-            std::vector<png_byte> aRow(aRowSizeBytes, 0);
-
-            for (int pass = 0; pass < nNumberOfPasses; pass++)
+            Bitmap aBitmap(Size(width, height), 24);
             {
-                for (png_uint_32 y = 0; y < height; y++)
+                BitmapScopedWriteAccess pWriteAccess(aBitmap);
+                ScanlineFormat eFormat = pWriteAccess->GetScanlineFormat();
+                if (eFormat == ScanlineFormat::N24BitTcBgr)
+                    png_set_bgr(pPng);
+
+                std::vector<std::vector<png_byte>> aRows(height);
+                for (auto& rRow : aRows)
+                    rRow.resize(aRowSizeBytes, 0);
+
+                for (int pass = 0; pass < nNumberOfPasses; pass++)
                 {
-                    Scanline pScanline = pWriteAccess->GetScanline(y);
-                    png_bytep pRow = aRow.data();
-                    png_read_rows(pPng, &pRow, nullptr, 1);
-                    size_t iColor = 0;
-                    for (size_t i = 0; i < aRowSizeBytes; i += 3)
+                    for (png_uint_32 y = 0; y < height; y++)
                     {
-                        pScanline[iColor++] = pRow[i + 0];
-                        pScanline[iColor++] = pRow[i + 1];
-                        pScanline[iColor++] = pRow[i + 2];
+                        Scanline pScanline = pWriteAccess->GetScanline(y);
+                        png_bytep pRow = aRows[y].data();
+                        png_read_row(pPng, pRow, nullptr);
+                        size_t iColor = 0;
+                        for (size_t i = 0; i < aRowSizeBytes; i += 3)
+                        {
+                            pScanline[iColor++] = pRow[i + 0];
+                            pScanline[iColor++] = pRow[i + 1];
+                            pScanline[iColor++] = pRow[i + 2];
+                        }
                     }
                 }
             }
+            rBitmapEx = BitmapEx(aBitmap);
         }
         else if (colorType == PNG_COLOR_TYPE_RGB_ALPHA)
         {
             size_t aRowSizeBytes = png_get_rowbytes(pPng, pInfo);
 
-            BitmapScopedWriteAccess pWriteAccess(aBitmap);
-            AlphaScopedWriteAccess pWriteAccessAlpha(aBitmapAlpha);
-
-            ScanlineFormat eFormat = pWriteAccess->GetScanlineFormat();
-            if (eFormat == ScanlineFormat::N24BitTcBgr)
-                png_set_bgr(pPng);
-
-            std::vector<png_byte> aRow(aRowSizeBytes, 0);
-
-            for (int pass = 0; pass < nNumberOfPasses; pass++)
+            if (bUseBitmap32)
             {
-                for (png_uint_32 y = 0; y < height; y++)
+                Bitmap aBitmap(Size(width, height), 32);
                 {
-                    Scanline pScanAlpha = pWriteAccessAlpha->GetScanline(y);
-                    Scanline pScanline = pWriteAccess->GetScanline(y);
-                    png_bytep pRow = aRow.data();
-                    png_read_rows(pPng, &pRow, nullptr, 1);
-                    size_t iAlpha = 0;
-                    size_t iColor = 0;
-                    for (size_t i = 0; i < aRowSizeBytes; i += 4)
+                    BitmapScopedWriteAccess pWriteAccess(aBitmap);
+                    ScanlineFormat eFormat = pWriteAccess->GetScanlineFormat();
+                    if (eFormat == ScanlineFormat::N32BitTcAbgr
+                        || eFormat == ScanlineFormat::N32BitTcBgra)
                     {
-                        pScanline[iColor++] = pRow[i + 0];
-                        pScanline[iColor++] = pRow[i + 1];
-                        pScanline[iColor++] = pRow[i + 2];
-                        pScanAlpha[iAlpha++] = 0xFF - pRow[i + 3];
+                        png_set_bgr(pPng);
+                    }
+
+                    std::vector<std::vector<png_byte>> aRows(height);
+                    for (auto& rRow : aRows)
+                        rRow.resize(aRowSizeBytes, 0);
+
+                    for (int pass = 0; pass < nNumberOfPasses; pass++)
+                    {
+                        for (png_uint_32 y = 0; y < height; y++)
+                        {
+                            Scanline pScanline = pWriteAccess->GetScanline(y);
+                            png_bytep pRow = aRows[y].data();
+                            png_read_row(pPng, pRow, nullptr);
+                            size_t iColor = 0;
+                            for (size_t i = 0; i < aRowSizeBytes; i += 4)
+                            {
+                                sal_Int8 alpha = pRow[i + 3];
+                                pScanline[iColor++] = vcl::bitmap::premultiply(pRow[i + 0], alpha);
+                                pScanline[iColor++] = vcl::bitmap::premultiply(pRow[i + 1], alpha);
+                                pScanline[iColor++] = vcl::bitmap::premultiply(pRow[i + 2], alpha);
+                                pScanline[iColor++] = alpha;
+                            }
+                        }
                     }
                 }
+                rBitmapEx = BitmapEx(aBitmap);
+            }
+            else
+            {
+                Bitmap aBitmap(Size(width, height), 24);
+                AlphaMask aBitmapAlpha(Size(width, height), nullptr);
+                {
+                    BitmapScopedWriteAccess pWriteAccess(aBitmap);
+                    ScanlineFormat eFormat = pWriteAccess->GetScanlineFormat();
+                    if (eFormat == ScanlineFormat::N24BitTcBgr)
+                        png_set_bgr(pPng);
+
+                    AlphaScopedWriteAccess pWriteAccessAlpha(aBitmapAlpha);
+
+                    std::vector<std::vector<png_byte>> aRows(height);
+                    for (auto& rRow : aRows)
+                        rRow.resize(aRowSizeBytes, 0);
+
+                    for (int pass = 0; pass < nNumberOfPasses; pass++)
+                    {
+                        for (png_uint_32 y = 0; y < height; y++)
+                        {
+                            Scanline pScanline = pWriteAccess->GetScanline(y);
+                            Scanline pScanAlpha = pWriteAccessAlpha->GetScanline(y);
+                            png_bytep pRow = aRows[y].data();
+                            png_read_row(pPng, pRow, nullptr);
+                            size_t iAlpha = 0;
+                            size_t iColor = 0;
+                            for (size_t i = 0; i < aRowSizeBytes; i += 4)
+                            {
+                                pScanline[iColor++] = pRow[i + 0];
+                                pScanline[iColor++] = pRow[i + 1];
+                                pScanline[iColor++] = pRow[i + 2];
+                                pScanAlpha[iAlpha++] = 0xFF - pRow[i + 3];
+                            }
+                        }
+                    }
+                }
+                rBitmapEx = BitmapEx(aBitmap, aBitmapAlpha);
             }
         }
     }
@@ -197,8 +253,6 @@ bool reader(SvStream& rStream, BitmapEx& rBitmapEx)
     png_read_end(pPng, pInfo);
 
     png_destroy_read_struct(&pPng, &pInfo, nullptr);
-
-    rBitmapEx = BitmapEx(aBitmap, aBitmapAlpha);
 
     return true;
 }
@@ -212,7 +266,13 @@ PngImageReader::PngImageReader(SvStream& rStream)
 {
 }
 
-bool PngImageReader::read(BitmapEx& rBitmapEx) { return reader(mrStream, rBitmapEx); }
+bool PngImageReader::read(BitmapEx& rBitmapEx)
+{
+    auto pBackendCapabilities = ImplGetSVData()->mpDefInst->GetBackendCapabilities();
+    bool bSupportsBitmap32 = pBackendCapabilities->mbSupportsBitmap32;
+
+    return reader(mrStream, rBitmapEx, bSupportsBitmap32);
+}
 
 } // namespace vcl
 

include/vcl/BitmapTools.hxx
@@ -33,6 +33,9 @@ typedef sal_uInt8 (*lookup_table)[256];
 lookup_table VCL_DLLPUBLIC get_premultiply_table();
 lookup_table VCL_DLLPUBLIC get_unpremultiply_table();
 
+VCL_DLLPUBLIC sal_uInt8 unpremultiply(sal_uInt8 c, sal_uInt8 a);
+VCL_DLLPUBLIC sal_uInt8 premultiply(sal_uInt8 c, sal_uInt8 a);
+
 /**
  * Intended to be used to feed into CreateFromData to create a BitmapEx. RGB data format.
  */
@@ -123,6 +126,8 @@ VCL_DLLPUBLIC css::uno::Sequence< sal_Int8 > CanvasExtractBitmapData(BitmapEx co
 
 BitmapEx VCL_DLLPUBLIC createHistorical8x8FromArray(std::array<sal_uInt8,64> const & pArray, Color aColorPix, Color aColorBack);
 bool VCL_DLLPUBLIC isHistorical8x8(const BitmapEx& rBitmapEx, BitmapColor& o_rBack, BitmapColor& o_rFront);
+
+VCL_DLLPUBLIC bool convertBitmap32To24Plus8(BitmapEx const & rInput, BitmapEx & rResult);
 
 }} // end vcl::bitmap
 

vcl/headless/svpgdi.cxx
@@ -1624,7 +1624,7 @@ void SvpSalGraphics::drawBitmap(const SalTwoRect& rTR, const SalBitmap& rSourceB
 {
     SourceHelper aSurface(rSourceBitmap);
     cairo_surface_t* source = aSurface.getSurface();
-    copySource(rTR, source);
+    copyWithOperator(rTR, source, CAIRO_OPERATOR_OVER);
 }
 
 void SvpSalGraphics::drawBitmap(const SalTwoRect& rTR, const BitmapBuffer* pBuffer, cairo_operator_t eOp)
@@ -1749,11 +1749,10 @@ std::shared_ptr<SalBitmap> SvpSalGraphics::getBitmap( long nX, long nY, long nWi
 Color SvpSalGraphics::getPixel( long nX, long nY )
 {
 #if CAIRO_VERSION >= CAIRO_VERSION_ENCODE(1, 12, 0)
-    cairo_surface_t *target = cairo_surface_create_similar_image(m_pSurface,
+    cairo_surface_t *target = cairo_surface_create_similar_image(m_pSurface, CAIRO_FORMAT_ARGB32, 1, 1);
 #else
-    cairo_surface_t *target = cairo_image_surface_create(
+    cairo_surface_t *target = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, 1, 1);
 #endif
-            CAIRO_FORMAT_ARGB32, 1, 1);
 
     cairo_t* cr = cairo_create(target);
 
@@ -1769,11 +1768,10 @@ Color SvpSalGraphics::getPixel( long nX, long nY )
     sal_uInt8 b = unpremultiply_table[a][data[SVP_CAIRO_BLUE]];
     sal_uInt8 g = unpremultiply_table[a][data[SVP_CAIRO_GREEN]];
     sal_uInt8 r = unpremultiply_table[a][data[SVP_CAIRO_RED]];
-    Color nRet = Color(r, g, b);
-
+    Color aColor(0xFF - a, r, g, b);
     cairo_surface_destroy(target);
 
-    return nRet;
+    return aColor;
 }
 
 namespace

vcl/headless/svpinst.cxx
@@ -537,6 +537,13 @@ void SvpSalInstance::AddToRecentDocumentList(const OUString&, const OUString&, c
 {
 }
 
+std::shared_ptr<vcl::BackendCapabilities> SvpSalInstance::GetBackendCapabilities()
+{
+    auto pBackendCapabilities = SalInstance::GetBackendCapabilities();
+    pBackendCapabilities->mbSupportsBitmap32 = true;
+    return pBackendCapabilities;
+}
+
 //obviously doesn't actually do anything, it's just a nonfunctional stub
 
 #ifdef LIBO_HEADLESS

vcl/inc/backend/BackendCapabilities.hxx
@@ -1,0 +1,28 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#ifndef INCLUDED_VCL_INC_BACKENDCAPABILITIES_HXX
+#define INCLUDED_VCL_INC_BACKENDCAPABILITIES_HXX
+
+namespace vcl
+{
+struct BackendCapabilities
+{
+    bool mbSupportsBitmap32;
+    BackendCapabilities()
+        : mbSupportsBitmap32(false)
+    {
+    }
+};
+}
+
+#endif // INCLUDED_VCL_INC_BACKENDCAPABILITIES_HXX
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

vcl/inc/headless/svpinst.hxx
@@ -167,6 +167,8 @@ public:
     // SalBitmap
     virtual std::shared_ptr<SalBitmap> CreateSalBitmap() override;
 
+    std::shared_ptr<vcl::BackendCapabilities> GetBackendCapabilities() override;
+
     // wait next event and dispatch
     // must returned by UserEvent (SalFrame::PostEvent)
     // and timer

vcl/inc/salinst.hxx
@@ -28,6 +28,8 @@
 #include <osl/thread.hxx>
 #include <vcl/vclenum.hxx>
 
+#include "backend/BackendCapabilities.hxx"
+
 #include "displayconnectiondispatch.hxx"
 
 #include <com/sun/star/uno/XComponentContext.hpp>
@@ -134,6 +136,11 @@ public:
     virtual SalSystem*      CreateSalSystem() = 0;
     // SalBitmap
     virtual std::shared_ptr<SalBitmap> CreateSalBitmap() = 0;
+    // BackendCapabilities
+    virtual std::shared_ptr<vcl::BackendCapabilities> GetBackendCapabilities()
+    {
+        return std::make_shared<vcl::BackendCapabilities>();
+    }
 
     // YieldMutex
     comphelper::SolarMutex* GetYieldMutex();

vcl/qa/cppunit/BitmapTest.cxx
@@ -30,6 +30,9 @@
 #include <BitmapSymmetryCheck.hxx>
 #include <bitmapwriteaccess.hxx>
 
+#include <svdata.hxx>
+#include <salinst.hxx>
+
 namespace
 {
 class BitmapTest : public CppUnit::TestFixture
@@ -44,6 +47,8 @@ class BitmapTest : public CppUnit::TestFixture
     void testCRC();
     void testGreyPalette();
     void testCustom8BitPalette();
+    void testErase();
+    void testBitmap32();
 
     CPPUNIT_TEST_SUITE(BitmapTest);
     CPPUNIT_TEST(testCreation);
@@ -56,6 +61,8 @@ class BitmapTest : public CppUnit::TestFixture
     CPPUNIT_TEST(testCRC);
     CPPUNIT_TEST(testGreyPalette);
     CPPUNIT_TEST(testCustom8BitPalette);
+    CPPUNIT_TEST(testErase);
+    CPPUNIT_TEST(testBitmap32);
     CPPUNIT_TEST_SUITE_END();
 };
 
@@ -151,6 +158,10 @@ void BitmapTest::testCreation()
                                      aBmp.GetSizeBytes());
     }
 
+    // Check backend capabilities and return from the test successfully
+    // if the backend doesn't support 32-bit bitmap
+    auto pBackendCapabilities = ImplGetSVData()->mpDefInst->GetBackendCapabilities();
+    if (pBackendCapabilities->mbSupportsBitmap32)
     {
         Bitmap aBmp(Size(10, 10), 32);
         Size aSize = aBmp.GetSizePixel();
@@ -158,12 +169,11 @@ void BitmapTest::testCreation()
         CPPUNIT_ASSERT_EQUAL_MESSAGE("Wrong height", static_cast<long>(10), aSize.Height());
         CPPUNIT_ASSERT_EQUAL_MESSAGE("Wrong pref size", Size(), aBmp.GetPrefSize());
         CPPUNIT_ASSERT_MESSAGE("Empty bitmap", !aBmp.IsEmpty());
-        CPPUNIT_ASSERT_EQUAL_MESSAGE("Wrong bit count", static_cast<sal_uInt16>(24),
-                                     aBmp.GetBitCount());
-        CPPUNIT_ASSERT_EQUAL_MESSAGE("Wrong color count", static_cast<sal_uLong>(16777216),
-                                     aBmp.GetColorCount());
-        CPPUNIT_ASSERT_EQUAL_MESSAGE("Wrong byte size", static_cast<sal_uLong>(300),
-                                     aBmp.GetSizeBytes());
+
+        CPPUNIT_ASSERT_EQUAL_MESSAGE("Wrong bit count", sal_uInt16(32), aBmp.GetBitCount());
+        CPPUNIT_ASSERT_EQUAL_MESSAGE("Wrong color count", sal_Int64(4294967296U),
+                                     sal_Int64(aBmp.GetColorCount()));
+        CPPUNIT_ASSERT_EQUAL_MESSAGE("Wrong byte size", sal_uLong(400), aBmp.GetSizeBytes());
     }
 }
 
@@ -596,6 +606,48 @@ void BitmapTest::testCustom8BitPalette()
 
         CPPUNIT_ASSERT_EQUAL(255, int(pAccess->GetPixelIndex(1, 2)));
         CPPUNIT_ASSERT_EQUAL(BitmapColor(0xFF, 0xCC, 0x22), pAccess->GetColor(1, 2));
+    }
+}
+
+void BitmapTest::testErase()
+{
+    Bitmap aBitmap(Size(3, 3), 24);
+    {
+        BitmapScopedWriteAccess pWriteAccess(aBitmap);
+        pWriteAccess->Erase(Color(0x11, 0x22, 0x33));
+    }
+    {
+        Bitmap::ScopedReadAccess pReadAccess(aBitmap);
+        BitmapColor aColor(pReadAccess->GetPixel(0, 0));
+        CPPUNIT_ASSERT_EQUAL(BitmapColor(0x11, 0x22, 0x33, 0x00), aColor);
+    }
+}
+
+void BitmapTest::testBitmap32()
+{
+    // Check backend capabilities and return from the test successfully
+    // if the backend doesn't support 32-bit bitmap
+    auto pBackendCapabilities = ImplGetSVData()->mpDefInst->GetBackendCapabilities();
+    if (!pBackendCapabilities->mbSupportsBitmap32)
+        return;
+
+    Bitmap aBitmap(Size(3, 3), 32);
+    {
+        BitmapScopedWriteAccess pWriteAccess(aBitmap);
+        pWriteAccess->Erase(Color(0xFF, 0x11, 0x22, 0x33));
+        pWriteAccess->SetPixel(1, 1, BitmapColor(0x44, 0xFF, 0xBB, 0x00));
+        pWriteAccess->SetPixel(2, 2, BitmapColor(0x99, 0x77, 0x66, 0x55));
+    }
+    {
+        Bitmap::ScopedReadAccess pReadAccess(aBitmap);
+        BitmapColor aColor = pReadAccess->GetPixel(0, 0);
+        CPPUNIT_ASSERT_EQUAL(BitmapColor(0x00, 0x00, 0x00, 0xFF), aColor);
+
+        aColor = pReadAccess->GetPixel(1, 1);
+        CPPUNIT_ASSERT_EQUAL(BitmapColor(0x44, 0xFF, 0xBB, 0x00), aColor);
+
+        aColor = pReadAccess->GetPixel(2, 2);
+        CPPUNIT_ASSERT_EQUAL(BitmapColor(0x99, 0x77, 0x66, 0x55), aColor);
     }
 }
 

commit 06a02d1800206c75545cd9a3a42aa38252dd24cb

Correctly read PNG into bitmaps N32BitTcA... formats (where alpha comes first)
This appears to be a regression introduced with
86ea64f216819696cd86d1926aff0a138ace2baf "Support for native 32bit Bitmap in VCL
and SVP (cairo) backend".  It caused CppunitTest_vcl_png_test to fail on
(big-endian) Linux s390x with

> vcl/qa/cppunit/png/PngFilterTest.cxx:176:PngFilterTest::testPng
> equality assertion failed
> - Expected: c[ff000040]
> - Actual  : c[0000ff40]

where eFormat happens to be ScanlineFormat::N32BitTcArgb, vs.
ScanlineFormat::N32BitTcBgra on e.g. Linux x86-64 (and which thus didn't notice
the lack of support for N32BitTcA... formats where alpha goes first instead of
last).

Change-Id: Id6030468718f6ef831b42f2b5ad7ba2c4c46a805

vcl/source/filter/png/PngImageReader.cxx
@@ -187,6 +187,8 @@ bool reader(SvStream& rStream, BitmapEx& rBitmapEx, bool bUseBitmap32)
                     for (auto& rRow : aRows)
                         rRow.resize(aRowSizeBytes, 0);
 
+                    auto const alphaFirst = (eFormat == ScanlineFormat::N32BitTcAbgr
+                                             || eFormat == ScanlineFormat::N32BitTcArgb);
                     for (int pass = 0; pass < nNumberOfPasses; pass++)
                     {
                         for (png_uint_32 y = 0; y < height; y++)
@@ -198,10 +200,17 @@ bool reader(SvStream& rStream, BitmapEx& rBitmapEx, bool bUseBitmap32)
                             for (size_t i = 0; i < aRowSizeBytes; i += 4)
                             {
                                 sal_Int8 alpha = pRow[i + 3];
+                                if (alphaFirst)
+                                {
+                                    pScanline[iColor++] = alpha;
+                                }
                                 pScanline[iColor++] = vcl::bitmap::premultiply(pRow[i + 0], alpha);
                                 pScanline[iColor++] = vcl::bitmap::premultiply(pRow[i + 1], alpha);
                                 pScanline[iColor++] = vcl::bitmap::premultiply(pRow[i + 2], alpha);
-                                pScanline[iColor++] = alpha;
+                                if (!alphaFirst)
+                                {
+                                    pScanline[iColor++] = alpha;
+                                }
                             }
                         }
                     }