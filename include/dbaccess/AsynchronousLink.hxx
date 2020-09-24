20047f366f653f388d3486a4f879e3e9150b461b
Andrea Gelmini <andrea.gelmini@gelma.net>
2020-09-24 12:55:55 +0200

    Fix typos in code
    Fixing:
    _ACESSIBLE -> _ACCESSIBLE
    _ASYNCRONOUS -> _ASYNCHRONOUS
    _XARRAU -> _XARRAY
    
    Then switched to #pragma
    
    It passed "make check" on Linux
    
    Change-Id: Idb01cc7d4ccf404df82016b1b3356400320eb317

--- include/dbaccess/AsynchronousLink.hxx
+++ include/dbaccess/AsynchronousLink.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_DBACCESS_ASYNCRONOUSLINK_HXX
-#define INCLUDED_DBACCESS_ASYNCRONOUSLINK_HXX
+#pragma once
 
 #include <osl/mutex.hxx>
 #include <tools/link.hxx>
@@ -57,6 +56,5 @@ namespace dbaui
         void CancelCall();
     };
 }
-#endif // INCLUDED_DBACCESS_ASYNCRONOUSLINK_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
