ae8995694da9ca29a62d13e6a320e5d39f4727e8
Eike Rathke <erack@redhat.com>
2020-09-24 11:54:27 +0200

    Resolves: tdf#115436 Add English Kenya {en-KE} locale data
    Change-Id: I06b9479e48bfcc3421ae1ec634064f9f3209cffd
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/103301
    Tested-by: Jenkins
    Reviewed-by: Eike Rathke <erack@redhat.com>

--- i18npool/source/localedata/localedata.cxx
+++ i18npool/source/localedata/localedata.cxx
@@ -331,7 +331,8 @@ const struct {
     { "ilo_PH", lcl_DATA_OTHERS },
     { "ha_Latn_NG",  lcl_DATA_OTHERS },
     { "min_ID", lcl_DATA_OTHERS },
-    { "sun_ID", lcl_DATA_OTHERS }
+    { "sun_ID", lcl_DATA_OTHERS },
+    { "en_KE",  lcl_DATA_OTHERS }
 };
 
 #else
