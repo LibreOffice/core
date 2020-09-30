54d84e00dff12c693b0fc07facb2d93abcc98579
Luboš Luňák <l.lunak@collabora.com>
2020-09-30 19:34:36 +0200

    forward drawBitmap calls from vclcanvas to Skia backend
    This is primarily to test the vclcanvas->Skia backend forwarding,
    but this still may improve performance in some cases.
    
    Change-Id: I312d4b386e4d88eee4afdd155fc6cfe105705150

--- include/vcl/skia/interface.hxx
+++ include/vcl/skia/interface.hxx
@@ -16,9 +16,13 @@
 
 #if HAVE_FEATURE_SKIA
 
+class SkPaint;
 class SkMatrix;
-class SkPath;
 class BitmapEx;
+namespace vcl
+{
+class Region;
+}
 
 /**
 Interface class giving direct access to Skia features of an output device.
@@ -26,7 +30,24 @@ Interface class giving direct access to Skia features of an output device.
 class VCL_DLLPUBLIC SkiaOutDevInterface
 {
 public:
+    // Calls will abort if FailIfSlow is passed and they cannot be fast.
+    // This allows e.g. vclcanvas to handle call locally with caching.
+    enum SlowHandling
+    {
+        FailIfSlow,
+        ProceedIfSlow
+    };
     virtual ~SkiaOutDevInterface();
+
+    // Sets the given clip region. If IsNull(), resets clipping.
+    virtual bool setClipRegion(const vcl::Region&) = 0;
+    // Return the current clip region.
+    virtual const vcl::Region& getClipRegion() const = 0;
+
+    // Draw the bitmap with the given paint, matrix, clip region and alpha modulation.
+    virtual bool drawBitmap(const BitmapEx& bitmap, const SkPaint& paint, const SkMatrix& matrix,
+                            double alphaModulation, SlowHandling slowHandling = ProceedIfSlow)
+        = 0;
 };
 
 #endif // HAVE_FEATURE_SKIA
