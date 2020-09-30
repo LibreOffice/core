LibreOffice/core

commit 61a35560cb412d7ab0e3d0574eec4a790e3b9dfd

tdf#131280 Fix endless invalidation loop on documents with form controls
Restores a condition which was removed in 8de98e61fbc96bf523b3dec7e1e52eb7e2d7693e

Change-Id: I68a9f8a362d2ded9975e7c73e2a0533aa5ad9e94
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/91053
Tested-by: Jenkins
Reviewed-by: Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/93867
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Reviewed-by: Andras Timar <andras.timar@collabora.com>

vcl/source/window/window2.cxx
@@ -1354,8 +1354,8 @@ void Window::queue_resize(StateChangedType eReason)
     if (VclPtr<vcl::Window> pParent = GetParentWithLOKNotifier())
     {
         Size aSize = GetSizePixel();
-        if (aSize.getWidth() > 0 && aSize.getHeight() > 0 &&
-            !pParent->IsInInitShow())
+        if (aSize.getWidth() > 0 && aSize.getHeight() > 0 && GetParentDialog()
+            && !pParent->IsInInitShow())
             LogicInvalidate(nullptr);
     }
 }

commit f96d8e60c72adcb791b26e0b87cc8870aefd260a

Allow invalidate after queue_resize for lok in sidebar
This is a fix for regression introduced by
61a35560cb412d7ab0e3d0574eec4a790e3b9dfd

Sidebar wasn't properly refreshed in Online eg.
in Impress change 'Background' in sidebar 'Slide' deck
to 'Color' -> resulted in overlapping content

Change-Id: Id64f5d8694908d28cf5fa9787b65e555fb317e35

vcl/source/window/window2.cxx
@@ -1320,6 +1320,19 @@ void Window::InvalidateSizeCache()
     pWindowImpl->mnOptimalHeightCache = -1;
 }
 
+static bool HasParentDockingWindow(const vcl::Window* pWindow)
+{
+    while( pWindow )
+    {
+        if( pWindow->IsDockingWindow() )
+            return true;
+
+        pWindow = pWindow->GetParent();
+    }
+
+    return pWindow && pWindow->IsDockingWindow();
+}
+
 void Window::queue_resize(StateChangedType eReason)
 {
     if (IsDisposed())
@@ -1355,7 +1368,12 @@ void Window::queue_resize(StateChangedType eReason)
     if (VclPtr<vcl::Window> pParent = GetParentWithLOKNotifier())
     {
         Size aSize = GetSizePixel();
-        if (aSize.getWidth() > 0 && aSize.getHeight() > 0 && GetParentDialog()
+
+        // Form controls (VCL controls inside document window) was causing
+        // infinite loop of calls, so call it only for widgets having as a parent
+        // dialog or docking window (eg. sidebar)
+        if (aSize.getWidth() > 0 && aSize.getHeight() > 0
+            && (GetParentDialog() || HasParentDockingWindow(this))
             && !pParent->IsInInitShow())
             LogicInvalidate(nullptr);
     }