60e62dbe91c123c4a67834d901c385d04cfdb97a
Xisco Fauli <xiscofauli@libreoffice.org>
2020-10-01 12:40:04 +0200

    uitest: sc: factor out common duplicated code
    Change-Id: Ib6d4edaf3bd1b0a4078c277d1139d7b0db479e2b

--- uitest/uitest/uihelper/calc.py
+++ uitest/uitest/uihelper/calc.py
@@ -13,4 +13,21 @@ def enter_text_to_cell(gridwin, cell, text):
     type_text(gridwin, text)
     gridwin.executeAction("TYPE", mkPropertyValues({"KEYCODE": "RETURN"}))
 
+def change_measurement_units(UITestCase, unit):
+    UITestCase.ui_test.execute_dialog_through_command(".uno:OptionsTreeDialog")
+    xDialogOpt = UITestCase.xUITest.getTopFocusWindow()
+
+    xPages = xDialogOpt.getChild("pages")
+    xEntry = xPages.getChild('3')
+    xEntry.executeAction("EXPAND", tuple())
+    xGeneralEntry = xEntry.getChild('0')
+    xGeneralEntry.executeAction("SELECT", tuple())
+    xUnit = xDialogOpt.getChild("unitlb")
+    props = {"TEXT": unit}
+    actionProps = mkPropertyValues(props)
+    xUnit.executeAction("SELECT", actionProps)
+
+    xOKBtn = xDialogOpt.getChild("ok")
+    UITestCase.ui_test.close_dialog_through_button(xOKBtn)
+
 # vim: set shiftwidth=4 softtabstop=4 expandtab:
