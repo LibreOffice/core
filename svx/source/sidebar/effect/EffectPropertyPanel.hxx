af6e2ab68f3c00e8ab1d7602b452325c3c81bb53
andreas kainz <kainz.a@gmail.com>
2020-10-02 13:24:17 +0200

    tdf#136073 combine glow and softedge sidebar section
    Change-Id: I19381009f5b286b051ae0cb90a794ed51a413a7a

--- svx/source/sidebar/effect/EffectPropertyPanel.hxx
+++ svx/source/sidebar/effect/EffectPropertyPanel.hxx
@@ -6,8 +6,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-#ifndef INCLUDED_SVX_SOURCE_SIDEBAR_GLOW_GLOWPROPERTYPANEL_HXX
-#define INCLUDED_SVX_SOURCE_SIDEBAR_GLOW_GLOWPROPERTYPANEL_HXX
+#ifndef INCLUDED_SVX_SOURCE_SIDEBAR_EFFECT_EFFECTPROPERTYPANEL_HXX
+#define INCLUDED_SVX_SOURCE_SIDEBAR_EFFECT_EFFECTPROPERTYPANEL_HXX
 
 #include <vcl/vclptr.hxx>
 #include <sfx2/sidebar/ControllerItem.hxx>
@@ -17,38 +17,48 @@ class ColorListBox;
 
 namespace svx::sidebar
 {
-class GlowPropertyPanel : public PanelLayout,
-                          public ::sfx2::sidebar::ControllerItem::ItemUpdateReceiverInterface
+class EffectPropertyPanel
+    : public PanelLayout,
+    public ::sfx2::sidebar::ControllerItem::ItemUpdateReceiverInterface
 {
 public:
-    GlowPropertyPanel(vcl::Window* pParent, const css::uno::Reference<css::frame::XFrame>& rxFrame,
-                      SfxBindings* pBindings);
-    virtual ~GlowPropertyPanel() override;
+    EffectPropertyPanel(
+        vcl::Window* pParent,
+        const css::uno::Reference<css::frame::XFrame>& rxFrame,
+        SfxBindings* pBindings);
+    virtual ~EffectPropertyPanel() override;
     virtual void dispose() override;
 
-    static VclPtr<vcl::Window> Create(vcl::Window* pParent,
-                                      const css::uno::Reference<css::frame::XFrame>& rxFrame,
-                                      SfxBindings* pBindings);
+    static VclPtr<vcl::Window> Create(
+        vcl::Window* pParent,
+        const css::uno::Reference<css::frame::XFrame>& rxFrame,
+        SfxBindings* pBindings);
 
-    virtual void NotifyItemUpdate(const sal_uInt16 nSId, const SfxItemState eState,
-                                  const SfxPoolItem* pState) override;
+    virtual void NotifyItemUpdate(
+        const sal_uInt16 nSId,
+        const SfxItemState eState,
+        const SfxPoolItem* pState) override;
 
-    virtual void GetControlState(const sal_uInt16 /*nSId*/,
-                                 boost::property_tree::ptree& /*rState*/) override{};
+    virtual void GetControlState(
+        const sal_uInt16 /*nSId*/,
+        boost::property_tree::ptree& /*rState*/) override{};
 
 private:
     sfx2::sidebar::ControllerItem maGlowColorController;
     sfx2::sidebar::ControllerItem maGlowRadiusController;
     sfx2::sidebar::ControllerItem maGlowTransparencyController;
+    std::unique_ptr<weld::Label> mxFTTransparency;
+    sfx2::sidebar::ControllerItem maSoftEdgeRadiusController;
 
     SfxBindings* mpBindings;
 
     std::unique_ptr<weld::MetricSpinButton> mxGlowRadius;
     std::unique_ptr<ColorListBox> mxLBGlowColor;
     std::unique_ptr<weld::MetricSpinButton> mxGlowTransparency;
-    std::unique_ptr<weld::Label> mxFTRadius;
+    std::unique_ptr<weld::Label> mxFTRadiusSoftEdge;
+    std::unique_ptr<weld::Label> mxFTRadiusGlow;
     std::unique_ptr<weld::Label> mxFTColor;
-    std::unique_ptr<weld::Label> mxFTTransparency;
+    std::unique_ptr<weld::MetricSpinButton> mxSoftEdgeRadius;
 
     void Initialize();
     void UpdateControls();
@@ -56,6 +66,7 @@ private:
     DECL_LINK(ModifyGlowColorHdl, ColorListBox&, void);
     DECL_LINK(ModifyGlowRadiusHdl, weld::MetricSpinButton&, void);
     DECL_LINK(ModifyGlowTransparencyHdl, weld::MetricSpinButton&, void);
+    DECL_LINK(ModifySoftEdgeRadiusHdl, weld::MetricSpinButton&, void);
 };
 }
 
