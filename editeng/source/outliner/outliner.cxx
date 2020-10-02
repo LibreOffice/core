fa59bfdf46731d7186d8469105d85c6339e696f4
Noel <noelgrandin@gmail.com>
2020-10-02 09:36:35 +0200

    loplugin:reducevarscope in desktop..emfio
    Change-Id: I25ca760ae15114ada621d928997a7117401c75d1

--- editeng/source/outliner/outliner.cxx
+++ editeng/source/outliner/outliner.cxx
@@ -496,8 +496,6 @@ bool Outliner::ImpConvertEdtToOut( sal_Int32 nPara )
     ESelection aDelSel;
 
     OUString aName;
-    OUString aHeading_US( "heading" );
-    OUString aNumber_US( "Numbering" );
 
     OUString aStr( pEditEngine->GetText( nPara ) );
     const sal_Unicode* pPtr = aStr.getStr();
@@ -507,6 +505,8 @@ bool Outliner::ImpConvertEdtToOut( sal_Int32 nPara )
     SfxStyleSheet* pStyle= pEditEngine->GetStyleSheet( nPara );
     if( pStyle )
     {
+        OUString aHeading_US( "heading" );
+        OUString aNumber_US( "Numbering" );
         aName = pStyle->GetName();
         sal_Int32 nSearch;
         if ( ( nSearch = aName.indexOf( aHeading_US ) ) != -1 )
