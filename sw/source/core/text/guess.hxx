1b6dead4dc7d11fd3d2e1b88da941857cb1be046
Nikhil <nikhil.varghese23@gmail.com>
2020-10-04 14:43:23 +0530

    tdf#124176 Use #pragma once in text
    Change-Id: I4fd4fb8d232a2b250f1da3b9414d80a284367ec8
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/103919
    Tested-by: Jenkins
    Reviewed-by: Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>

--- sw/source/core/text/guess.hxx
+++ sw/source/core/text/guess.hxx
@@ -16,8 +16,8 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
-#ifndef INCLUDED_SW_SOURCE_CORE_TEXT_GUESS_HXX
-#define INCLUDED_SW_SOURCE_CORE_TEXT_GUESS_HXX
+
+#pragma once
 #include <memory>
 
 #include "porrst.hxx"
@@ -59,6 +59,5 @@ public:
         { return xHyphWord; }
 };
 
-#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
