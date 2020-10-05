698e5d54182d96a1fd0c3b864ba0e618f82dd1f1
Andreas Heinisch <andreas.heinisch@yahoo.de>
2020-10-05 12:20:50 +0200

    tdf#123025 - fixed broken tests for cverr method
    Change-Id: I44eb44107a24207764da24b4e01b407570342013
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/103956
    Tested-by: Jenkins
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

--- basic/qa/basic_coverage/test_cverr_method.vb
+++ basic/qa/basic_coverage/test_cverr_method.vb
@@ -7,26 +7,18 @@
 '
 
 Function doUnitTest as Integer
+
+    doUnitTest = 0
+
     ' CVERR
-    If (CVerr(100) <> 100) Then
-        doUnitTest = 0
-    Else
-        doUnitTest = 1
-    End If
-
+    If (CVerr(100) <> 100) Then Exit Function
     ' tdf#79426 - passing an error object to a function
-    if ( TestCVErr( CVErr( 2 ) ) <> 2 ) Then
-        doUnitTest = 0
-    Else
-        doUnitTest = 1
-    End If
-
+    If (TestCVErr(CVErr(2)) <> 2) Then Exit Function
     ' tdf#79426 - test with Error-Code 448 ( ERRCODE_BASIC_NAMED_NOT_FOUND )
-    if ( TestCVErr( CVErr( 448 ) ) <> 448 ) Then
-        doUnitTest = 0
-    Else
-        doUnitTest = 1
-    End If
+    If (TestCVErr(CVErr(448)) <> 448) Then Exit Function
+
+    doUnitTest = 1
+
 End Function
 
 Function TestCVErr(vErr As Variant)
