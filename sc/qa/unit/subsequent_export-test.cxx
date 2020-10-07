d90f244e7190580775a99a98100bcabcce43c100
Attila Szűcs <szucs.attila3@nisz.hu>
2020-10-07 09:46:27 +0200

    tdf#76047 sc import: fix externalLinks in array
    Wrote a SetExternalLinks into applyArrayFormulas.
    Without it, the id of the linked files was not replaced to its filenames.
    
    Co-authored-by: Tibor Nagy (NISZ)
    Change-Id: I57c304a46899f4171db9ad12e7e187bd6e4c1c15

--- sc/qa/unit/subsequent_export-test.cxx
+++ sc/qa/unit/subsequent_export-test.cxx
@@ -271,7 +271,11 @@ public:
     void testTdf133688_precedents();
     void testTdf91251_missingOverflowRoundtrip();
     void testTdf137000_handle_upright();
+<<<<<<< HEAD
     void testTdf126305_DataValidatyErrorAlert();
+=======
+    void testTdf76047_externalLink();
+>>>>>>> d69e04543fa3... tdf#76047 sc import: fix externalLinks in array
 
     CPPUNIT_TEST_SUITE(ScExportTest);
     CPPUNIT_TEST(test);
@@ -437,7 +441,11 @@ public:
     CPPUNIT_TEST(testTdf133688_precedents);
     CPPUNIT_TEST(testTdf91251_missingOverflowRoundtrip);
     CPPUNIT_TEST(testTdf137000_handle_upright);
+<<<<<<< HEAD
     CPPUNIT_TEST(testTdf126305_DataValidatyErrorAlert);
+=======
+    CPPUNIT_TEST(testTdf76047_externalLink);
+>>>>>>> d69e04543fa3... tdf#76047 sc import: fix externalLinks in array
 
     CPPUNIT_TEST_SUITE_END();
 
@@ -5523,6 +5531,29 @@ void ScExportTest::testTdf126305_DataValidatyErrorAlert()
     assertXPath(pDoc, "/x:worksheet/x:dataValidations/x:dataValidation[3]", "errorStyle", "information");
 
     xDocSh->DoClose();
+}
+
+void ScExportTest::testTdf76047_externalLink()
+{
+    ScDocShellRef pShell = loadDoc("tdf76047_externalLink.", FORMAT_XLSX);
+    CPPUNIT_ASSERT(pShell.is());
+
+    // load data from external links. (tdf76047_externalLinkSource.ods)
+    // that file have to be the same directory as tdf76047_externalLink.xlsx
+    pShell->ReloadAllLinks();
+    ScDocument& rDoc = pShell->GetDocument();
+
+    // compare the loaded data (from external links) to the data copied manually to the testfile
+    for (int nCol = 1; nCol <= 5; nCol++) {
+        for (int nRow = 3; nRow <= 5; nRow++) {
+            OUString aStr1 = rDoc.GetString(ScAddress(nCol, nRow, 0));
+            OUString aStr2 = rDoc.GetString(ScAddress(nCol, nRow + 5, 0));
+            OUString aStr3 = rDoc.GetString(ScAddress(nCol, nRow + 11, 0));
+
+            CPPUNIT_ASSERT_EQUAL(aStr1, aStr3);
+            CPPUNIT_ASSERT_EQUAL(aStr2, aStr3);
+        }
+    }
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(ScExportTest);
