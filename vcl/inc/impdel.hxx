65203ca170eb775f63d3759b64778eccf312cfa5
Julien Nabet <serval2412@yahoo.fr>
2020-10-11 13:15:38 +0200

    Replace list by vector in vcl/impdel
    Change-Id: I91f5859413ec3265647b8d55225b29ee95a06857

--- vcl/inc/impdel.hxx
+++ vcl/inc/impdel.hxx
@@ -20,7 +20,8 @@
 #ifndef INCLUDED_VCL_IMPDEL_HXX
 #define INCLUDED_VCL_IMPDEL_HXX
 
-#include <list>
+#include <algorithm>
+#include <vector>
 
 namespace vcl
 {
@@ -29,7 +30,7 @@ class DeletionListener;
 
 class DeletionNotifier
 {
-    std::list< DeletionListener* > m_aListeners;
+    std::vector< DeletionListener* > m_aListeners;
     protected:
     DeletionNotifier() {}
 
@@ -43,7 +44,7 @@ class DeletionNotifier
     { m_aListeners.push_back( pListener ); }
 
     void removeDel( DeletionListener* pListener )
-    { m_aListeners.remove( pListener ); }
+    { m_aListeners.erase(std::remove(m_aListeners.begin(), m_aListeners.end(), pListener), m_aListeners.end()); }
 };
 
 class DeletionListener
