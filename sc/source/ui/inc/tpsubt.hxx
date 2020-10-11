b14317ea84fe782f4ceac5801b05c9adeff33c27
Caolán McNamara <caolanm@redhat.com>
2020-10-11 19:56:24 +0100

    Revert "tdf#133886 - Libreoffice Calc Subtotals check all columns at once"
    This reverts commit e0c1fdcab23dbd2b7a0a7227557d7afc41b49bd7.
    
    it works locally for me, but builds before it passed and after are failing
    
    Change-Id: I0f55ec65c348c8d8a221225d56a4d1995bb0879d

--- sc/source/ui/inc/tpsubt.hxx
+++ sc/source/ui/inc/tpsubt.hxx
@@ -57,7 +57,6 @@ protected:
     std::unique_ptr<weld::ComboBox> mxLbGroup;
     std::unique_ptr<weld::TreeView> mxLbColumns;
     std::unique_ptr<weld::TreeView> mxLbFunctions;
-    std::unique_ptr<weld::CheckButton> mxLbSelectAllColumns;
 
 private:
     void            Init            ();
@@ -70,7 +69,6 @@ private:
     DECL_LINK( SelectListBoxHdl, weld::ComboBox&, void );
     DECL_LINK( SelectTreeListBoxHdl, weld::TreeView&, void );
     DECL_LINK(CheckHdl, const weld::TreeView::iter_col&, void);
-    DECL_LINK(CheckBoxHdl, weld::Button&, void);
     void SelectHdl(const weld::Widget*);
 };
 
