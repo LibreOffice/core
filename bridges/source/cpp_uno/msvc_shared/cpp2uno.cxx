c9cd9c0a6fe9539fe59eceee84c5d88544861f4a
Stephan Bergmann <sbergman@redhat.com>
2020-10-13 17:05:17 +0200

    loplugin:includeform (clang-cl)
    Change-Id: If1cffd3896d87695ba189728d8c1f1f04d55b6b4

--- bridges/source/cpp_uno/msvc_shared/cpp2uno.cxx
+++ bridges/source/cpp_uno/msvc_shared/cpp2uno.cxx
@@ -24,10 +24,10 @@
 #include <uno/data.h>
 #include <typelib/typedescription.hxx>
 
-#include "bridge.hxx"
-#include "cppinterfaceproxy.hxx"
-#include "types.hxx"
-#include "vtablefactory.hxx"
+#include <bridge.hxx>
+#include <cppinterfaceproxy.hxx>
+#include <types.hxx>
+#include <vtablefactory.hxx>
 
 #include <msvc/except.hxx>
 #include <msvc/cpp2uno.hxx>
