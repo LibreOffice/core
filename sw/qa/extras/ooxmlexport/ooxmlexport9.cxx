c690d49770d0c93a95bd8c3d11e95c1d17a17a60
Szabolcs Toth <toth.szabolcs@nisz.hu>
2020-10-14 16:32:05 +0200

    tdf#137641 DOCX export: fix relative from top page border
    alignment.
    
    Follow-up of commit 1c593e1916c9164c7db71da2017cfc26972f8e9f
    (tdf#133045 sw: add shape alignment to the top page border).
    
    Change-Id: Ie5bc536556b72030d17829a917f91a15ad5157a0
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/104660
    Tested-by: László Németh <nemeth@numbertext.org>
    Reviewed-by: László Németh <nemeth@numbertext.org>

--- sw/qa/extras/ooxmlexport/ooxmlexport9.cxx
+++ sw/qa/extras/ooxmlexport/ooxmlexport9.cxx
@@ -1560,6 +1560,36 @@ DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testTdf115557, "tdf115557.docx")
     assertXPath(pXmlDoc, "//w:footnote/w:p/w:r/w:drawing", 1);
 }
 
+DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testAlignmentRelativeFromTopMargin, "tdf137641_RelativeFromTopMargin.docx")
+{
+    xmlDocUniquePtr pXmlDoc = parseExport("word/document.xml");
+
+    assertXPath(pXmlDoc,
+                "/w:document/w:body/w:p[2]/w:r/mc:AlternateContent[1]/mc:Choice/w:drawing/"
+                "wp:anchor/wp:positionV",
+                "relativeFrom", "topMargin");
+    assertXPathContent(pXmlDoc,
+                       "/w:document/w:body/w:p[2]/w:r/mc:AlternateContent[1]/mc:Choice/w:drawing/"
+                       "wp:anchor/wp:positionV/wp:align",
+                       "top");
+    assertXPath(pXmlDoc,
+        "/w:document/w:body/w:p[2]/w:r/mc:AlternateContent[2]/mc:Choice/w:drawing/"
+        "wp:anchor/wp:positionV",
+        "relativeFrom", "topMargin");
+    assertXPathContent(pXmlDoc,
+                       "/w:document/w:body/w:p[2]/w:r/mc:AlternateContent[2]/mc:Choice/w:drawing/"
+                       "wp:anchor/wp:positionV/wp:align",
+                       "bottom");
+    assertXPath(pXmlDoc,
+        "/w:document/w:body/w:p[2]/w:r/mc:AlternateContent[3]/mc:Choice/w:drawing/"
+        "wp:anchor/wp:positionV",
+        "relativeFrom", "topMargin");
+    assertXPathContent(pXmlDoc,
+                       "/w:document/w:body/w:p[2]/w:r/mc:AlternateContent[3]/mc:Choice/w:drawing/"
+                       "wp:anchor/wp:positionV/wp:align",
+                       "center");
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
