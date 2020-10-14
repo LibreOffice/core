LibreOffice/core

commit 398ba26077f9029bdf6f7378bfc9ce8376b6f02d

tdf#127635 sw_redlinehide: put point at the end of deletion
SwWrtShell::Insert() is called with a selection, so it first calls
DeleteAndJoin() to get rid of the selection.

With redlining enabled, this leaves the cursor as it was before, so if
the point was at the start it still is at the start, so the following
Insert will insert at the start of the selection.

But then AutoCorrect wants to do things and it uses TextFrameIndex and
ViewToModelPos and gets a result at the end of the delete redline,
while the cursor remains at the start of the delete redline.

Avoid this by swapping the selection after DeleteAndJoin() (and also
DeleteRange() for consistency) so that insertion happens at end of
redline (swap "above" DoWithBreaks because it creates temporary PaMs).

(regression from sw_redlinehide)

Change-Id: Ib5b0475f610c2ce426e9202e9d325dd0cc451d5c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/96743
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@cib.de>

sw/source/core/doc/DocumentContentOperationsManager.cxx
@@ -2010,6 +2010,11 @@ void DocumentContentOperationsManager::DeleteRange( SwPaM & rPam )
 {
     lcl_DoWithBreaks( *this, rPam, &DocumentContentOperationsManager::DeleteRangeImpl );
 
+    if (m_rDoc.getIDocumentRedlineAccess().IsRedlineOn())
+    {
+        rPam.Normalize(false); // tdf#127635 put point at the end of deletion
+    }
+
     if (!m_rDoc.getIDocumentRedlineAccess().IsIgnoreRedline()
         && !m_rDoc.getIDocumentRedlineAccess().GetRedlineTable().empty())
     {
@@ -2181,10 +2186,17 @@ bool DocumentContentOperationsManager::DeleteAndJoin( SwPaM & rPam,
     if ( lcl_StrLenOverflow( rPam ) )
         return false;
 
-    return lcl_DoWithBreaks( *this, rPam, (m_rDoc.getIDocumentRedlineAccess().IsRedlineOn())
+    bool const ret = lcl_DoWithBreaks( *this, rPam, (m_rDoc.getIDocumentRedlineAccess().IsRedlineOn())
                 ? &DocumentContentOperationsManager::DeleteAndJoinWithRedlineImpl
                 : &DocumentContentOperationsManager::DeleteAndJoinImpl,
                 bForceJoinNext );
+
+    if (m_rDoc.getIDocumentRedlineAccess().IsRedlineOn())
+    {
+        rPam.Normalize(false); // tdf#127635 put point at the end of deletion
+    }
+
+    return ret;
 }
 
 // It seems that this is mostly used by SwDoc internals; the only

commit 30828623abf989a640e5b37a808ccac3abda8872

tdf#135260 sw_redlinehide: fix insert-with-delete differently
The problem with the fix for tdf#127635 is that now the cursor doesn't
move left on backspace if change tracking is on.

(regression from 398ba26077f9029bdf6f7378bfc9ce8376b6f02d)

Revert that and fix the autocorrect position in SwWrtShell::Insert()
instead.

Change-Id: I5989a589b654fc6e5ba3dd66922af15eff758ecc

sw/source/core/doc/DocumentContentOperationsManager.cxx
@@ -2013,11 +2013,6 @@ void DocumentContentOperationsManager::DeleteRange( SwPaM & rPam )
 {
     lcl_DoWithBreaks( *this, rPam, &DocumentContentOperationsManager::DeleteRangeImpl );
 
-    if (m_rDoc.getIDocumentRedlineAccess().IsRedlineOn())
-    {
-        rPam.Normalize(false); // tdf#127635 put point at the end of deletion
-    }
-
     if (!m_rDoc.getIDocumentRedlineAccess().IsIgnoreRedline()
         && !m_rDoc.getIDocumentRedlineAccess().GetRedlineTable().empty())
     {
@@ -2193,11 +2188,6 @@ bool DocumentContentOperationsManager::DeleteAndJoin( SwPaM & rPam,
                 ? &DocumentContentOperationsManager::DeleteAndJoinWithRedlineImpl
                 : &DocumentContentOperationsManager::DeleteAndJoinImpl,
                 bForceJoinNext );
-
-    if (m_rDoc.getIDocumentRedlineAccess().IsRedlineOn())
-    {
-        rPam.Normalize(false); // tdf#127635 put point at the end of deletion
-    }
 
     return ret;
 }

sw/source/uibase/wrtsh/wrtsh1.cxx
@@ -228,7 +228,11 @@ void SwWrtShell::Insert( const OUString &rStr )
 
         StartUndo(SwUndoId::REPLACE, &aRewriter);
         bStarted = true;
+        Push();
         bDeleted = DelRight();
+        Pop(SwCursorShell::PopMode::DeleteCurrent); // Restore selection (if tracking changes)
+        NormalizePam(false); // tdf#127635 put point at the end of deletion
+        ClearMark();
     }
 
     bCallIns ?