d3eca4177a78c3db17b4699ea6e071e52488c46f
Michael Stahl <Michael.Stahl@cib.de>
2020-10-14 16:10:29 +0200

    tdf#136577 sw: fix Undo of Replace with nothing
    The UI doesn't allow to replace an empty selection anyway, but it now
    happens on Undo.
    
    (regression from e1629c210ad78310e3d48c0756723134a27b89df)
    
    Change-Id: I468f28335beaeb8c42df8ed4cfc90f2c03129239
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/104308
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@cib.de>

--- sw/source/core/doc/DocumentContentOperationsManager.cxx
+++ sw/source/core/doc/DocumentContentOperationsManager.cxx
@@ -4291,7 +4291,7 @@ bool DocumentContentOperationsManager::DeleteRangeImplImpl(SwPaM & rPam)
 bool DocumentContentOperationsManager::ReplaceRangeImpl( SwPaM& rPam, const OUString& rStr,
         const bool bRegExReplace )
 {
-    if( !rPam.HasMark() || *rPam.GetPoint() == *rPam.GetMark() )
+    if (!rPam.HasMark())
         return false;
 
     bool bJoinText, bJoinPrev;
