606d9450953dcb98dac9e02d46252303125cd3ac
Jan Holesovsky <kendy@collabora.com>
2020-10-15 18:05:35 +0200

    lok: Simplify the check for command in sendDialogEvent.
    Change-Id: I1d2c967b68113d2528b80e91c32170f749ed9335

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -3656,11 +3656,11 @@ static void doc_sendDialogEvent(LibreOfficeKitDocument* /*pThis*/, unsigned nWin
 
             bIsWeldedDialog = pWidget != nullptr;
             bool bContinueWithLOKWindow = false;
+            OUString sAction = aMap["cmd"];
 
             if (bIsWeldedDialog)
             {
                 OUString sControlType = aMap["type"];
-                OUString sAction = aMap["cmd"];
 
                 if (sControlType == "tabcontrol")
                 {
@@ -3746,36 +3746,32 @@ static void doc_sendDialogEvent(LibreOfficeKitDocument* /*pThis*/, unsigned nWin
                 if (pUIWindow) {
                     bool bIsClickAction = false;
 
-                    if (aMap.find("cmd") != aMap.end()) {
-                        if (aMap["cmd"] == "selected")
-                        {
-                            aMap["POS"] = aMap["data"];
-                            aMap["TEXT"] = aMap["data"];
+                    if (sAction == "selected")
+                    {
+                        aMap["POS"] = aMap["data"];
+                        aMap["TEXT"] = aMap["data"];
 
-                            pUIWindow->execute(sSelectAction, aMap);
-                        }
-                        else if (aMap["cmd"] == "plus")
-                        {
-                            pUIWindow->execute(sUpAction, aMap);
-                        }
-                        else if (aMap["cmd"] == "minus")
-                        {
-                            pUIWindow->execute(sDownAction, aMap);
-                        }
-                        else if (aMap["cmd"] == "set")
-                        {
-                            aMap["TEXT"] = aMap["data"];
+                        pUIWindow->execute(sSelectAction, aMap);
+                    }
+                    else if (sAction == "plus")
+                    {
+                        pUIWindow->execute(sUpAction, aMap);
+                    }
+                    else if (sAction == "minus")
+                    {
+                        pUIWindow->execute(sDownAction, aMap);
+                    }
+                    else if (sAction == "set")
+                    {
+                        aMap["TEXT"] = aMap["data"];
 
-                            pUIWindow->execute(sClearAction, aMap);
-                            pUIWindow->execute(sTypeAction, aMap);
-                        }
-                        else if (aMap["cmd"] == "value")
-                        {
-                            aMap["VALUE"] = aMap["data"];
-                            pUIWindow->execute(sValue, aMap);
-                        }
-                        else
-                            bIsClickAction = true;
+                        pUIWindow->execute(sClearAction, aMap);
+                        pUIWindow->execute(sTypeAction, aMap);
+                    }
+                    else if (sAction == "value")
+                    {
+                        aMap["VALUE"] = aMap["data"];
+                        pUIWindow->execute(sValue, aMap);
                     }
                     else
                         bIsClickAction = true;
