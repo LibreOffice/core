714558d6a7cf4e61093918e75190238b75dbcc02
Jan Holesovsky <kendy@collabora.com>
2020-10-16 14:27:27 +0200

    uitest: Rename the "SET" to "TYPE" for Edit boxes + implement the real "SET".
    To be consistent with the other controls: "TYPE" actually enters the
    characters one by one, while "SET" sets it as a whole.
    
    Also I believe Modify() should be called, not UpdateData()...
    
    Change-Id: I967dc270b1d92fe76107732a511cc3e70d3d65c0
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/104435
    Tested-by: Andras Timar <andras.timar@collabora.com>
    Reviewed-by: Andras Timar <andras.timar@collabora.com>

--- vcl/source/uitest/uiobject.cxx
+++ vcl/source/uitest/uiobject.cxx
@@ -720,7 +720,7 @@ void EditUIObject::execute(const OUString& rAction,
         const StringMap& rParameters)
 {
     bool bHandled = true;
-    if (rAction == "SET")
+    if (rAction == "TYPE")
     {
         if (rParameters.find("TEXT") != rParameters.end())
         {
@@ -742,6 +742,17 @@ void EditUIObject::execute(const OUString& rAction,
         {
             bHandled = false;
         }
+    }
+    else if (rAction == "SET")
+    {
+        auto it = rParameters.find("TEXT");
+        if (it != rParameters.end())
+        {
+            mxEdit->SetText(it->second);
+            mxEdit->Modify();
+        }
+        else
+            bHandled = false;
     }
     else if (rAction == "SELECT")
     {
