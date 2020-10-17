00d92d8fa2d621853da46f5a8005730e97675125
mariamfahmy <mariamfahmy66@gmail.com>
2020-10-17 13:49:04 +0200

    tdf#124176: Using pragma once
    Change-Id: I634d2eebfa71ebdbe6dc334f3bc29e70a06a94ec

--- unoidl/source/sourcefileprovider.hxx
+++ unoidl/source/sourcefileprovider.hxx
@@ -7,8 +7,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-#ifndef INCLUDED_UNOIDL_SOURCE_SOURCEFILEPROVIDER_HXX
-#define INCLUDED_UNOIDL_SOURCE_SOURCEFILEPROVIDER_HXX
+#pragma once
 
 #include <sal/config.h>
 
@@ -38,7 +37,5 @@ private:
 };
 
 }
-
-#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
