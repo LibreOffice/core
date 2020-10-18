a79cfdde55e736de61aefd57321f8d8106ccb101
mariamfahmy <mariamfahmy66@gmail.com>
2020-10-18 20:11:26 +0200

    tdf#124176: Use pragma once instead of include guards
    Change-Id: Ib9fd7e3ffbe8760edf4a108342aa5ed03c453b01

--- writerfilter/source/dmapper/DomainMapper_Impl.hxx
+++ writerfilter/source/dmapper/DomainMapper_Impl.hxx
@@ -16,8 +16,7 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
-#ifndef INCLUDED_WRITERFILTER_SOURCE_DMAPPER_DOMAINMAPPER_IMPL_HXX
-#define INCLUDED_WRITERFILTER_SOURCE_DMAPPER_DOMAINMAPPER_IMPL_HXX
+#pragma once
 
 #include <com/sun/star/text/XParagraphCursor.hpp>
 #include <com/sun/star/text/XTextDocument.hpp>
@@ -1107,6 +1106,5 @@ private:
 };
 
 } //namespace writerfilter::dmapper
-#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
