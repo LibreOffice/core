7ca89d9b5e30f89edd21ff797acb1a13e53a8b54
Muhammet Kara <muhammet.kara@collabora.com>
2020-10-18 16:21:59 +0300

    clang-format CommandCategoryListBox
    Change-Id: If7ae64a0b9be01b3fa73310a4d9834ae3c151c10

--- cui/source/inc/CommandCategoryListBox.hxx
+++ cui/source/inc/CommandCategoryListBox.hxx
@@ -26,11 +26,11 @@ class CommandCategoryListBox
 {
     SfxGroupInfoArr_Impl m_aGroupInfo;
     OUString m_sModuleLongName;
-    css::uno::Reference< css::uno::XComponentContext > m_xContext;
-    css::uno::Reference< css::frame::XFrame > m_xFrame;
-    css::uno::Reference< css::container::XNameAccess > m_xGlobalCategoryInfo;
-    css::uno::Reference< css::container::XNameAccess > m_xModuleCategoryInfo;
-    css::uno::Reference< css::container::XNameAccess > m_xUICmdDescription;
+    css::uno::Reference<css::uno::XComponentContext> m_xContext;
+    css::uno::Reference<css::frame::XFrame> m_xFrame;
+    css::uno::Reference<css::container::XNameAccess> m_xGlobalCategoryInfo;
+    css::uno::Reference<css::container::XNameAccess> m_xModuleCategoryInfo;
+    css::uno::Reference<css::container::XNameAccess> m_xUICmdDescription;
 
     // For search
     i18nutil::SearchOptions2 m_searchOptions;
@@ -45,36 +45,38 @@ public:
     ~CommandCategoryListBox();
     void ClearAll();
 
-    void        Init(
-                    const css::uno::Reference< css::uno::XComponentContext >& xContext,
-                    const css::uno::Reference< css::frame::XFrame >& xFrame,
-                    const OUString& sModuleLongName);
-    void        FillFunctionsList(
-                    const css::uno::Sequence< css::frame::DispatchInformation >& xCommands,
-                    CuiConfigFunctionListBox*  pFunctionListBox,
-                    const OUString& filterTerm,
-                    SaveInData *pCurrentSaveInData );
-    OUString    getCommandName(const OUString& sCommand);
+    void Init(const css::uno::Reference<css::uno::XComponentContext>& xContext,
+              const css::uno::Reference<css::frame::XFrame>& xFrame,
+              const OUString& sModuleLongName);
+    void FillFunctionsList(const css::uno::Sequence<css::frame::DispatchInformation>& xCommands,
+                           CuiConfigFunctionListBox* pFunctionListBox, const OUString& filterTerm,
+                           SaveInData* pCurrentSaveInData);
+    OUString getCommandName(const OUString& sCommand);
 
-    void connect_changed(const Link<weld::ComboBox&, void>& rLink) { m_xControl->connect_changed(rLink); }
+    void connect_changed(const Link<weld::ComboBox&, void>& rLink)
+    {
+        m_xControl->connect_changed(rLink);
+    }
 
     /**
         Signals that a command category has been selected.
         And updates the functions list box to include
         the commands in the selected category.
     */
-    void categorySelected(CuiConfigFunctionListBox* pFunctionListBox,
-                          const OUString& filterTerm, SaveInData* pCurrentSaveInData = nullptr);
+    void categorySelected(CuiConfigFunctionListBox* pFunctionListBox, const OUString& filterTerm,
+                          SaveInData* pCurrentSaveInData = nullptr);
 
-    void                SetStylesInfo(SfxStylesInfo_Impl* pStyles);
+    void SetStylesInfo(SfxStylesInfo_Impl* pStyles);
 
     // Adds children of the given macro group to the functions list
-    void addChildren(
-        const weld::TreeIter* parentEntry, const css::uno::Reference<com::sun::star::script::browse::XBrowseNode> &parentNode,
-        CuiConfigFunctionListBox* pFunctionListBox, const OUString &filterTerm , SaveInData *pCurrentSaveInData,
-        std::vector<std::unique_ptr<weld::TreeIter>> &rNodesToExpand);
+    void
+    addChildren(const weld::TreeIter* parentEntry,
+                const css::uno::Reference<com::sun::star::script::browse::XBrowseNode>& parentNode,
+                CuiConfigFunctionListBox* pFunctionListBox, const OUString& filterTerm,
+                SaveInData* pCurrentSaveInData,
+                std::vector<std::unique_ptr<weld::TreeIter>>& rNodesToExpand);
 
-    void set_visible(bool bVisible) {m_xControl->set_visible(bVisible);}
+    void set_visible(bool bVisible) { m_xControl->set_visible(bVisible); }
 };
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
