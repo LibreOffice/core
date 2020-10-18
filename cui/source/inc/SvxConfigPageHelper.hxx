c85383837e413426e9437528a5d3d7e62aab971e
Muhammet Kara <muhammet.kara@collabora.com>
2020-10-18 17:15:45 +0300

    clang-format customize/SvxConfigPageHelper
    Change-Id: Ia802bfc4a6cd8d970fbb3c37a55959729ea19257

--- cui/source/inc/SvxConfigPageHelper.hxx
+++ cui/source/inc/SvxConfigPageHelper.hxx
@@ -26,64 +26,51 @@
 class SvxConfigPageHelper
 {
 public:
-    static void RemoveEntry( SvxEntries* pEntries, SvxConfigEntry const * pChildEntry );
+    static void RemoveEntry(SvxEntries* pEntries, SvxConfigEntry const* pChildEntry);
 
-    static OUString replaceSaveInName( const OUString& rMessage, const OUString& rSaveInName );
-    static OUString stripHotKey( const OUString& str );
-    static OUString replaceSixteen( const OUString& str, sal_Int32 nReplacement );
+    static OUString replaceSaveInName(const OUString& rMessage, const OUString& rSaveInName);
+    static OUString stripHotKey(const OUString& str);
+    static OUString replaceSixteen(const OUString& str, sal_Int32 nReplacement);
 
-    static sal_Int16    GetImageType();
-    static void         InitImageType();
-    static css::uno::Reference< css::graphic::XGraphic > GetGraphic(
-        const css::uno::Reference< css::ui::XImageManager >& xImageManager,
-        const OUString& rCommandURL );
+    static sal_Int16 GetImageType();
+    static void InitImageType();
+    static css::uno::Reference<css::graphic::XGraphic>
+    GetGraphic(const css::uno::Reference<css::ui::XImageManager>& xImageManager,
+               const OUString& rCommandURL);
 
-    static OUString     generateCustomName(
-        const OUString& prefix,
-        SvxEntries* entries,
-        sal_Int32 suffix = 1 );
-    static OUString     generateCustomMenuURL(
-        SvxEntries* entries,
-        sal_Int32 suffix = 1 );
-    static sal_uInt32   generateRandomValue();
+    static OUString generateCustomName(const OUString& prefix, SvxEntries* entries,
+                                       sal_Int32 suffix = 1);
+    static OUString generateCustomMenuURL(SvxEntries* entries, sal_Int32 suffix = 1);
+    static sal_uInt32 generateRandomValue();
     /**
         Generates a custom resource URL for a new toolbar.
         Typically something like: private:resource/toolbar/custom_toolbar_########
         The last 8 letters are randomly generated alphanumeric characters.
     */
-    static OUString     generateCustomURL( SvxEntries* entries );
+    static OUString generateCustomURL(SvxEntries* entries);
 
-    static OUString GetModuleName( const OUString& aModuleId );
-    static OUString GetUIModuleName(
-        const OUString& aModuleId,
-        const css::uno::Reference< css::frame::XModuleManager2 >& rModuleManager );
+    static OUString GetModuleName(const OUString& aModuleId);
+    static OUString
+    GetUIModuleName(const OUString& aModuleId,
+                    const css::uno::Reference<css::frame::XModuleManager2>& rModuleManager);
 
-    static bool GetMenuItemData(
-        const css::uno::Reference< css::container::XIndexAccess >& rItemContainer,
-        sal_Int32 nIndex,
-        OUString& rCommandURL,
-        OUString& rLabel,
-        sal_uInt16& rType,
-        sal_Int32& rStyle,
-        css::uno::Reference< css::container::XIndexAccess >& rSubMenu );
-    static bool GetToolbarItemData(
-        const css::uno::Reference< css::container::XIndexAccess >& rItemContainer,
-        sal_Int32 nIndex,
-        OUString& rCommandURL,
-        OUString& rLabel,
-        sal_uInt16& rType,
-        bool& rIsVisible,
-        sal_Int32& rStyle );
+    static bool
+    GetMenuItemData(const css::uno::Reference<css::container::XIndexAccess>& rItemContainer,
+                    sal_Int32 nIndex, OUString& rCommandURL, OUString& rLabel, sal_uInt16& rType,
+                    sal_Int32& rStyle, css::uno::Reference<css::container::XIndexAccess>& rSubMenu);
+    static bool
+    GetToolbarItemData(const css::uno::Reference<css::container::XIndexAccess>& rItemContainer,
+                       sal_Int32 nIndex, OUString& rCommandURL, OUString& rLabel, sal_uInt16& rType,
+                       bool& rIsVisible, sal_Int32& rStyle);
 
-    static css::uno::Sequence< css::beans::PropertyValue > ConvertSvxConfigEntry(
-        const SvxConfigEntry* pEntry );
-    static css::uno::Sequence< css::beans::PropertyValue > ConvertToolbarEntry(
-        const SvxConfigEntry* pEntry );
+    static css::uno::Sequence<css::beans::PropertyValue>
+    ConvertSvxConfigEntry(const SvxConfigEntry* pEntry);
+    static css::uno::Sequence<css::beans::PropertyValue>
+    ConvertToolbarEntry(const SvxConfigEntry* pEntry);
 
-    static bool EntrySort( SvxConfigEntry const * a, SvxConfigEntry const * b );
+    static bool EntrySort(SvxConfigEntry const* a, SvxConfigEntry const* b);
 
-    static bool SvxConfigEntryModified( SvxConfigEntry const * pEntry );
-
+    static bool SvxConfigEntryModified(SvxConfigEntry const* pEntry);
 };
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
