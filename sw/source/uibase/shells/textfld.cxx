b4bcee02229fbb382c6b2d774caba54d43b58697
Seth Chaiklin <sdc.blanco@youmail.dk>
2020-10-20 21:52:06 +0200

    tdf#137568  grey-out Format - Comments in menu,  when no comments
    Change-Id: Ieaec67ee057235d1aa9a81106ca75042e190e0e8
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/104507
    Tested-by: Jenkins
    Reviewed-by: Seth Chaiklin <sdc.blanco@youmail.dk>
    Reviewed-by: Heiko Tietze <heiko.tietze@documentfoundation.org>

--- sw/source/uibase/shells/textfld.cxx
+++ sw/source/uibase/shells/textfld.cxx
@@ -803,6 +803,9 @@ void SwTextShell::StateField( SfxItemSet &rSet )
                     rSet.InvalidateItem( FN_DELETE_COMMENT );
                     rSet.InvalidateItem( FN_HIDE_NOTE );
                 }
+                // tdf#137568 do not offer comment formating, if no comments are present
+                if ( !pPostItMgr->HasNotes() )
+                    rSet.DisableItem( FN_FORMAT_ALL_NOTES );
             }
             break;
 
