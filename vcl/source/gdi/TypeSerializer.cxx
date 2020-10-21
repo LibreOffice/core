LibreOffice/core

commit 0fb58a1ff168ae122e9c8993a3136658e3b0e3f0

new tools::Degree10 strong typedef
partly to flush some use of "long" out the codebase,
but also to make it obvious which units are being used
for angle values.

Change-Id: I1dc22494ca42c4677a63f685d5903f2b89886dc2
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/104548
Tested-by: Noel Grandin <noel.grandin@collabora.co.uk>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/source/gdi/TypeSerializer.cxx
@@ -58,7 +58,7 @@ void TypeSerializer::readGradient(Gradient& rGradient)
     rGradient.SetStyle(static_cast<GradientStyle>(nStyle));
     rGradient.SetStartColor(aStartColor);
     rGradient.SetEndColor(aEndColor);
-    rGradient.SetAngle(nAngle);
+    rGradient.SetAngle(Degree10(nAngle));
     rGradient.SetBorder(nBorder);
     rGradient.SetOfsX(nOffsetX);
     rGradient.SetOfsY(nOffsetY);
@@ -74,7 +74,7 @@ void TypeSerializer::writeGradient(const Gradient& rGradient)
     mrStream.WriteUInt16(static_cast<sal_uInt16>(rGradient.GetStyle()));
     writeColor(rGradient.GetStartColor());
     writeColor(rGradient.GetEndColor());
-    mrStream.WriteUInt16(rGradient.GetAngle());
+    mrStream.WriteUInt16(rGradient.GetAngle().get());
     mrStream.WriteUInt16(rGradient.GetBorder());
     mrStream.WriteUInt16(rGradient.GetOfsX());
     mrStream.WriteUInt16(rGradient.GetOfsY());

commit 13abce628e5fd18f4ff53354f8cf8b674da47ca3

clamp angle to valid value
fallout from
    commit 0fb58a1ff168ae122e9c8993a3136658e3b0e3f0
    new tools::Degree10 strong typedef

which caused failures in CppunitTest_vcl_filters_test

Change-Id: Ief60aae62935137330118b1a8d8dd0c428430430

vcl/source/gdi/TypeSerializer.cxx
@@ -58,6 +58,11 @@ void TypeSerializer::readGradient(Gradient& rGradient)
     rGradient.SetStyle(static_cast<GradientStyle>(nStyle));
     rGradient.SetStartColor(aStartColor);
     rGradient.SetEndColor(aEndColor);
+    if (nAngle > SAL_MAX_INT16)
+    {
+        SAL_WARN("vcl", "angle out of range " << nAngle);
+        nAngle = 0;
+    }
     rGradient.SetAngle(Degree10(nAngle));
     rGradient.SetBorder(nBorder);
     rGradient.SetOfsX(nOffsetX);