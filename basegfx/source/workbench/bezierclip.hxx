5854801873496a4b5aeae656a021e3bdd5378d15
gizemozgun <gizemozguun@gmail.com>
2020-10-23 01:43:37 +0300

    tdf#124176 Using pragma once instead of include guards
    Change-Id: I2f2470f509ff4a6cef5962755c6de86ebf1a89a5

--- basegfx/source/workbench/bezierclip.hxx
+++ basegfx/source/workbench/bezierclip.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_BASEGFX_SOURCE_WORKBENCH_BEZIERCLIP_HXX
-#define INCLUDED_BASEGFX_SOURCE_WORKBENCH_BEZIERCLIP_HXX
+#pragma once
 
 #include <vector>
 
