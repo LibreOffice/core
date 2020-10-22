d72a80ab52a1b912ffa73e509a6ff903b300c43a
Andreas Heinisch <andreas.heinisch@yahoo.de>
2020-10-22 20:03:44 +0200

    tdf#85371 - grant write access to the method used as a variable
    During the creation of the parameter list of a method, explicitly grant
    write access to the method which will used as a variable. Otherwise, the
    name of the method can't be used in certain statements, i.e., index in a
    for loop or as a dimension in ReDim.
    
    Change-Id: I3e4c49c21fd3345d5ddd69bc31a5823b5de5b8e7

---
+++ basic/qa/basic_coverage/test_method_name_variable.vb
@@ -0,0 +1,30 @@
+' This file is part of the LibreOffice project.
+'
+' This Source Code Form is subject to the terms of the Mozilla Public
+' License, v. 2.0. If a copy of the MPL was not distributed with this
+' file, You can obtain one at http://mozilla.org/MPL/2.0/.
+'
+
+Function assignVarToMethod() As Integer
+
+    ' method name used as dimension specifier
+    Dim fieldOfLongs() As Long
+    ReDim fieldOfLongs(assignVarToMethod) As Long
+
+    ' method name used as loop index
+    Dim sum As Integer
+    For assignVarToMethod = 1 To 3
+        sum = sum + assignVarToMethod
+    Next assignVarToMethod
+    assignVarToMethod = sum
+
+End Function
+
+Function doUnitTest() As Integer
+    ' tdf#85371 - check if the name of the method can be used as a variable in certain statements
+    If (assignVarToMethod() = 6) Then
+        doUnitTest = 1
+    Else
+        doUnitTest = 0
+    End If
+End Function
