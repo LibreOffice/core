42d4a1e85a4e4e847714b3016f4b4786e3fc0940
Jan-Marek Glogowski <glogow@fbihome.de>
2020-10-23 18:43:25 +0200

    tdf#136555 apply dialog bg color for button boxes
    The default background color of the frame is APPBACKGROUND, which
    is matched to GetWorkspaceColor(). That color is visible in Writer
    as the default document workspace background.
    
    Originally I just saw the dark background of the button box in the
    start center, and thought the workspace color in Qt is wrong and
    changed it in the reverted commit, missing the change of the color
    around the Writer document.
    
    Now that the button boxes's background color isn't fixed via the
    "branding" setting, we also must react to changed style settings,
    so we override the DataChanged function.
    
    This also reverts commit e0c72e31c1d455c26110c35e8780d420e17cdea6.
    
    Regressed-by: a927e0964ba0442d53fffb22c577e54bcf183ed7
    Change-Id: I62396355054523baef1197a8a8af61c2f0d29ef3

--- vcl/qt5/Qt5Frame.cxx
+++ vcl/qt5/Qt5Frame.cxx
@@ -1023,6 +1023,7 @@ void Qt5Frame::UpdateSettings(AllSettings& rSettings)
     Color aText = toColor(pal.color(QPalette::Active, QPalette::Text));
     Color aBase = toColor(pal.color(QPalette::Active, QPalette::Base));
     Color aButn = toColor(pal.color(QPalette::Active, QPalette::ButtonText));
+    Color aMid = toColor(pal.color(QPalette::Active, QPalette::Mid));
     Color aHigh = toColor(pal.color(QPalette::Active, QPalette::Highlight));
     Color aHighText = toColor(pal.color(QPalette::Active, QPalette::HighlightedText));
     Color aLink = toColor(pal.color(QPalette::Active, QPalette::Link));
@@ -1046,7 +1047,6 @@ void Qt5Frame::UpdateSettings(AllSettings& rSettings)
     style.SetFieldColor(aBase);
     style.SetWindowColor(aBase);
     style.SetActiveTabColor(aBase);
-    style.SetWorkspaceColor(aBase);
     style.SetAlternatingRowColor(toColor(pal.color(QPalette::Active, QPalette::AlternateBase)));
 
     // Buttons
@@ -1078,9 +1078,14 @@ void Qt5Frame::UpdateSettings(AllSettings& rSettings)
     style.BatchSetBackgrounds(aBack);
     style.SetInactiveTabColor(aBack);
 
+    // Workspace
+    style.SetWorkspaceColor(aMid);
+
     // Selection
     style.SetHighlightColor(aHigh);
     style.SetHighlightTextColor(aHighText);
+    style.SetActiveColor(aHigh);
+    style.SetActiveTextColor(aHighText);
 
     // Links
     style.SetLinkColor(aLink);
