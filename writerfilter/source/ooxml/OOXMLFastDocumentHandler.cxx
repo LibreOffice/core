3c09c306a15e8c40deedb08d6bcf38b9ee190db4
Noel Grandin <noelgrandin@gmail.com>
2020-10-23 19:00:50 +0200

    clog -> SAL_INFO
    Change-Id: I6cf543bd7052f7664b4d674e70c0d8a64cd1f734
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/104739
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

--- writerfilter/source/ooxml/OOXMLFastDocumentHandler.cxx
+++ writerfilter/source/ooxml/OOXMLFastDocumentHandler.cxx
@@ -17,10 +17,10 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#include <iostream>
 #include "OOXMLFastDocumentHandler.hxx"
 #include "OOXMLFastContextHandler.hxx"
 #include "OOXMLFactory.hxx"
+#include <sal/log.hxx>
 
 namespace writerfilter::ooxml
 {
@@ -44,67 +44,30 @@ OOXMLFastDocumentHandler::OOXMLFastDocumentHandler(
 OOXMLFastDocumentHandler::~OOXMLFastDocumentHandler() {}
 
 // css::xml::sax::XFastContextHandler:
-void SAL_CALL OOXMLFastDocumentHandler::startFastElement
-(::sal_Int32
-#ifdef DBG_UTIL
-Element
-#endif
+void SAL_CALL OOXMLFastDocumentHandler::startFastElement(sal_Int32 Element
 , const uno::Reference< xml::sax::XFastAttributeList > & /*Attribs*/)
 {
-#ifdef DBG_UTIL
-    clog << this << ":start element:"
-         << fastTokenToId(Element)
-         << endl;
-#endif
+    SAL_INFO("writerfilter", "start element:" << fastTokenToId(Element));
 }
 
 void SAL_CALL OOXMLFastDocumentHandler::startUnknownElement
-(const OUString &
-#ifdef DBG_UTIL
-Namespace
-#endif
-, const OUString &
-#ifdef DBG_UTIL
-Name
-#endif
-,
- const uno::Reference< xml::sax::XFastAttributeList > & /*Attribs*/)
+(const OUString & Namespace
+, const OUString & Name
+, const uno::Reference< xml::sax::XFastAttributeList > & /*Attribs*/)
 {
-#ifdef DBG_UTIL
-    clog << this << ":start unknown element:"
-         << Namespace  << ":" << Name << endl;
-#endif
+    SAL_INFO("writerfilter", "start unknown element:" << Namespace  << ":" << Name);
 }
 
-void SAL_CALL OOXMLFastDocumentHandler::endFastElement(::sal_Int32
-#ifdef DBG_UTIL
-Element
-#endif
-)
+void SAL_CALL OOXMLFastDocumentHandler::endFastElement(sal_Int32 Element)
 {
-#ifdef DBG_UTIL
-    clog << this << ":end element:"
-         << fastTokenToId(Element)
-         << endl;
-#endif
+    SAL_INFO("writerfilter", "end element:" << fastTokenToId(Element));
 }
 
 void SAL_CALL OOXMLFastDocumentHandler::endUnknownElement
-(const OUString &
-#ifdef DBG_UTIL
-Namespace
-#endif
-, const OUString &
-#ifdef DBG_UTIL
-Name
-#endif
-)
+(const OUString & Namespace
+, const OUString & Name)
 {
-#ifdef DBG_UTIL
-    clog << this << ":end unknown element:"
-         << Namespace << ":" << Name
-         << endl;
-#endif
+    SAL_INFO("writerfilter", "end unknown element:" << Namespace << ":" << Name);
 }
 
 rtl::Reference< OOXMLFastContextHandler > const &
@@ -139,22 +102,11 @@ uno::Reference< xml::sax::XFastContextHandler > SAL_CALL
 
 uno::Reference< xml::sax::XFastContextHandler > SAL_CALL
 OOXMLFastDocumentHandler::createUnknownChildContext
-(const OUString &
-#ifdef DBG_UTIL
-Namespace
-#endif
-,
- const OUString &
-#ifdef DBG_UTIL
-Name
-#endif
+(const OUString & Namespace
+, const OUString & Name
 , const uno::Reference< xml::sax::XFastAttributeList > & /*Attribs*/)
 {
-#ifdef DBG_UTIL
-    clog << this << ":createUnknownChildContext:"
-         << Namespace << ":"<< Name
-         << endl;
-#endif
+    SAL_INFO("writerfilter", "createUnknownChildContext:" << Namespace << ":"<< Name);
 
     return uno::Reference< xml::sax::XFastContextHandler >
         ( new OOXMLFastDocumentHandler( m_xContext, nullptr, nullptr, 0 ) );
