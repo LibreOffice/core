0aa1303f1efdd5323a4bd7c464781387c989ffe2
Balazs Varga <balazs.varga991@gmail.com>
2020-10-27 21:19:36 +0100

    tdf#138307 Chart import: fix lost custom shape text
    Do not set empty text to SdrOutliner, if it is already set.
    
    Note: Also fix the missing custom shape problem in
    tdf#72776 and tdf#93641. The wrong position (X:0, Y:0) of
    these shapes is another problem.
    
    Regression from commit 6f62a5c4ee2c1b7654c7fcaa618fb495e0d32f0a
    (tdf#128985: ODP: Style text directions revert to RTL on save and re-open)
    
    Change-Id: I07d8cb12836daf15db347f6bb19c17ed9373189e
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/104909
    Tested-by: László Németh <nemeth@numbertext.org>
    Reviewed-by: László Németh <nemeth@numbertext.org>
    Signed-off-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- sw/qa/extras/layout/layout2.cxx
+++ sw/qa/extras/layout/layout2.cxx
@@ -21,6 +21,8 @@
 #include <editeng/fhgtitem.hxx>
 #include <editeng/postitem.hxx>
 #include <editeng/unolingu.hxx>
+#include <editeng/outlobj.hxx>
+#include <editeng/editobj.hxx>
 #include <comphelper/sequence.hxx>
 
 #include <fmtanchr.hxx>
@@ -47,6 +49,7 @@
 #include <unoframe.hxx>
 #include <drawdoc.hxx>
 #include <svx/svdpage.hxx>
+#include <svx/svdotext.hxx>
 #include <dcontact.hxx>
 
 char const DATA_DIRECTORY[] = "/sw/qa/extras/layout/data/";
@@ -2561,8 +2564,12 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter2, testTdf137185)
     auto xTextFrame = SwXTextFrame::CreateXTextFrame(*pFormat->GetDoc(), pFormat);
 
     CPPUNIT_ASSERT_EQUAL(OUString("Align me!"), xTextFrame->getText()->getString());
-    CPPUNIT_ASSERT_EQUAL(OUString(), xTxt->getText()->getString());
-    // Before the patch it failled, because the text appeared 2 times on each other.
+    SdrTextObj* pTextObj = dynamic_cast<SdrTextObj*>(pObj);
+    CPPUNIT_ASSERT(pTextObj);
+    auto aOutStr = pTextObj->GetOutlinerParaObject()->GetTextObject();
+
+    CPPUNIT_ASSERT(aOutStr.GetText(0).isEmpty());
+    // Before the patch it failed, because the text appeared 2 times on each other.
 }
 
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter2, testTdf135035)
