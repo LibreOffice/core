5e585b1852e46f0625565ef1ef9f129349581b0e
Michael Stahl <Michael.Stahl@cib.de>
2020-10-28 15:55:42 +0100

    Revert "tdf#135001 sw_redlinehide: fix IsShown() for AT_PARA flys"
    This reverts commit 4018dba80e1fe37b24582e803d360998e1a73b2c.
    
    The code there changed substantially in 6.3, and this fix doesn't
    actually fix the bug in 6.1 branch.
    
    Change-Id: I441c47844afeafd084f7e0126bf89d74288e16d4

--- sw/source/core/layout/frmtool.cxx
+++ sw/source/core/layout/frmtool.cxx
@@ -1045,16 +1045,10 @@ static bool IsShown(sal_uLong const nIndex,
     {
         return false;
     }
-    if (rAnch.GetAnchorId() == RndStdIds::FLY_AT_PARA
+    if (pIter && rAnch.GetAnchorId() != RndStdIds::FLY_AT_PARA
         // sw_redlinehide: we want to hide AT_CHAR, but currently can't
         // because Delete and Accept Redline don't delete them!
-        || rAnch.GetAnchorId() == RndStdIds::FLY_AT_CHAR)
-    {
-        return pIter == nullptr // not merged
-            || pIter != pEnd    // at least one char visible in node
-            || rAnchor.nNode.GetNode().GetRedlineMergeFlag() != SwNode::Merge::Hidden;
-    }
-    if (pIter)
+              && rAnch.GetAnchorId() != RndStdIds::FLY_AT_CHAR)
     {
         // note: frames are not sorted by anchor position.
         assert(pEnd);
