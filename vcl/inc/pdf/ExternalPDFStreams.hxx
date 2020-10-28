ec11ab8c5c76d15b247e225048013468a9e8b4f6
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2020-10-28 13:55:23 +0100

    pdf: deduplicate resources when copying from external PDF stream
    When using external PDF stream/data (from PDF graphic objects),
    make sure to copy the content of external PDF resources (fonts,
    bitmaps, forms) only one time (by sharing the map between calls)
    and every other use, just use the reference to the objects.
    
    Change-Id: Ibaa632c8f74806eb195e69404551db6fd077a986
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/104935
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

---
+++ vcl/inc/pdf/ExternalPDFStreams.hxx
@@ -0,0 +1,70 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#pragma once
+
+#include <sal/types.h>
+#include <sal/log.hxx>
+#include <vcl/dllapi.h>
+
+#include <map>
+#include <vector>
+#include <memory>
+
+#include <vcl/filter/pdfdocument.hxx>
+
+namespace vcl
+{
+// A external PDF stream, which stores the PDF stream data as byte array.
+// This struct is also responsible to parsing the stream as a PDFDocument,
+// and store its instance for the life-cycle of the struct, so that it
+// reused to avoid unneccesary parsing.
+struct VCL_DLLPUBLIC ExternalPDFStream
+{
+    std::vector<sal_uInt8> maData;
+    std::shared_ptr<filter::PDFDocument> mpPDFDocument;
+    std::map<sal_Int32, sal_Int32> maCopiedResources;
+
+    std::map<sal_Int32, sal_Int32>& getCopiedResources() { return maCopiedResources; }
+
+    filter::PDFDocument& getPDFDocument()
+    {
+        if (!mpPDFDocument)
+        {
+            SvMemoryStream aPDFStream;
+            aPDFStream.WriteBytes(maData.data(), maData.size());
+            aPDFStream.Seek(0);
+            mpPDFDocument = std::make_unique<filter::PDFDocument>();
+            if (!mpPDFDocument->Read(aPDFStream))
+            {
+                SAL_WARN("vcl.pdfwriter",
+                         "PDFWriterImpl::writeReferenceXObject: reading the PDF document failed");
+            }
+        }
+        return *mpPDFDocument;
+    }
+};
+
+// Class to manage external PDF streams, for the de-duplication purpuse.
+class VCL_DLLPUBLIC ExternalPDFStreams
+{
+private:
+    std::map<std::vector<sal_uInt8>, sal_Int32> maStreamIndexMap;
+    std::vector<ExternalPDFStream> maStreamList;
+
+public:
+    ExternalPDFStreams() {}
+
+    sal_Int32 store(const sal_uInt8* pData, sal_uInt32 nLength);
+
+    ExternalPDFStream& get(sal_uInt32 nIndex);
+};
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
