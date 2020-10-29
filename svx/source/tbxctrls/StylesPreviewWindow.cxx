4d2e2d4a69e56a17746e46d06afe444e24a6772a
andreas kainz <kainz.a@gmail.com>
2020-10-29 14:31:35 +0100

    StylesPreview in Writer NB arrangement in two rows
    Change-Id: I05e0b903e7d51cb2487f18f5bbeb6228ba7c945f

--- svx/source/tbxctrls/StylesPreviewWindow.cxx
+++ svx/source/tbxctrls/StylesPreviewWindow.cxx
@@ -369,7 +369,7 @@ StylesPreviewWindow_Base::StylesPreviewWindow_Base(
 
         m_xStyleControllersWeld[i].reset(
             new weld::CustomWeld(xBuilder, sId, *m_xStyleControllers[i]));
-        m_xStyleControllersWeld[i]->set_size_request(100, 60);
+        m_xStyleControllersWeld[i]->set_size_request(128, 28);
     }
 
     m_xUp->connect_clicked(LINK(this, StylesPreviewWindow_Base, GoUp));
