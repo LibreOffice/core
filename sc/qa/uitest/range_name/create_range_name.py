dda3592f6a1028bd9d433eb2f88e561959212cf6
Xisco Fauli <xiscofauli@libreoffice.org>
2020-11-02 17:21:46 +0100

    tdf#67007: sc: Add UItest
    Change-Id: Id8e52f10943f5c28b3c28f757bd38b55f18f3384
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/105189
    Tested-by: Jenkins
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- sc/qa/uitest/range_name/create_range_name.py
+++ sc/qa/uitest/range_name/create_range_name.py
@@ -9,6 +9,7 @@ from libreoffice.uno.propertyvalue import mkPropertyValues
 
 from uitest.framework import UITestCase
 from uitest.uihelper.common import type_text, select_pos
+from uitest.uihelper.common import get_state_as_dict
 
 class CreateRangeNameTest(UITestCase):
 
@@ -16,15 +17,21 @@ class CreateRangeNameTest(UITestCase):
 
         self.ui_test.create_doc_in_start_center("calc")
 
+        calcDoc = self.xUITest.getTopFocusWindow()
+        xPosWindow = calcDoc.getChild('pos_window')
+        self.assertEqual('A1', get_state_as_dict(xPosWindow)['Text'])
+
         self.ui_test.execute_modeless_dialog_through_command(".uno:AddName")
 
         xAddNameDlg = self.xUITest.getTopFocusWindow()
 
         xEdit = xAddNameDlg.getChild("edit")
-        type_text(xEdit, "simpleRangeName")
+        type_text(xEdit, "globalRangeName")
 
         xAddBtn = xAddNameDlg.getChild("add")
         self.ui_test.close_dialog_through_button(xAddBtn)
+
+        self.assertEqual('globalRangeName', get_state_as_dict(xPosWindow)['Text'])
 
         self.ui_test.close_doc()
 
@@ -32,18 +39,26 @@ class CreateRangeNameTest(UITestCase):
 
         self.ui_test.create_doc_in_start_center("calc")
 
+        calcDoc = self.xUITest.getTopFocusWindow()
+        xPosWindow = calcDoc.getChild('pos_window')
+        self.assertEqual('A1', get_state_as_dict(xPosWindow)['Text'])
+
         self.ui_test.execute_modeless_dialog_through_command(".uno:AddName")
 
         xAddNameDlg = self.xUITest.getTopFocusWindow()
 
         xEdit = xAddNameDlg.getChild("edit")
-        type_text(xEdit, "simpleRangeName")
+        type_text(xEdit, "localRangeName")
 
         xScope = xAddNameDlg.getChild("scope")
         select_pos(xScope, "1")
 
         xAddBtn = xAddNameDlg.getChild("add")
         self.ui_test.close_dialog_through_button(xAddBtn)
+
+        # tdf#67007: Without the fix in place, this test would have failed with
+        # AssertionError: 'localRangeName' != 'A1'
+        self.assertEqual('localRangeName', get_state_as_dict(xPosWindow)['Text'])
 
         self.ui_test.close_doc()
 
