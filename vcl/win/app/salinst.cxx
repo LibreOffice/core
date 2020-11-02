e198640b9499d8b85543be8b1e6773f2375aacc0
Mike Kaganski <mike.kaganski@collabora.com>
2020-11-02 09:55:47 +0200

    Use branding here
    Change-Id: I2ed1fffc0ccca34d87ffc39d009eed466b5fb937

--- vcl/win/app/salinst.cxx
+++ vcl/win/app/salinst.cxx
@@ -943,7 +943,7 @@ void WinSalInstance::AddToRecentDocumentList(const OUString& rFileUrl, const OUS
 
             if ( !sApplicationName.isEmpty() )
             {
-                OUString sApplicationID("TheDocumentFoundation.LibreOffice." + sApplicationName);
+                OUString sApplicationID("Collabora.CollaboraOffice." + sApplicationName);
 
                 SHARDAPPIDINFO info;
                 info.psi = pShellItem;
