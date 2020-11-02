708a8b7f1d869580741787e71a9248757bb1541e
Szymon Kłos <szymon.klos@collabora.com>
2020-11-02 12:24:49 +0100

    jsdialog: execute checkbox action
    Change-Id: Ib19997f600404cc9555acbfaf87acac32f8aa5fc
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/106562
    Tested-by: Jenkins
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>

--- include/vcl/weld.hxx
+++ include/vcl/weld.hxx
@@ -1301,6 +1301,8 @@ public:
 
 class VCL_DLLPUBLIC ToggleButton : virtual public Button
 {
+    friend class ::LOKTrigger;
+
 protected:
     Link<ToggleButton&, void> m_aToggleHdl;
     TriState m_eSavedValue = TRISTATE_FALSE;
