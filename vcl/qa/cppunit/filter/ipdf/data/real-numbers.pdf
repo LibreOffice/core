LibreOffice/core

commit 0d68738d67eacdfebdca3c9183dc11f953b38174

Correctly parse real numbers in PDFs
The current way of parsing real numbers was not conforming to the PDF
standard ([1]), failing to recognize real numbers without a leading
zero, such as .6, or numbers with a leading +.

[1] PDF 1.7 standard, p. 14

Change-Id: I68eac4796b182f2632aa1152e58d63c054871581
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/103369
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/source/filter/ipdf/pdfdocument.cxx
@@ -1215,7 +1215,8 @@ bool PDFDocument::Tokenize(SvStream& rStream, TokenizeMode eMode,
             }
             default:
             {
-                if (rtl::isAsciiDigit(static_cast<unsigned char>(ch)) || ch == '-')
+                if (rtl::isAsciiDigit(static_cast<unsigned char>(ch)) || ch == '-' || ch == '+'
+                    || ch == '.')
                 {
                     // Numbering object: an integer or a real.
                     auto pNumberElement = new PDFNumberElement();
@@ -2189,14 +2190,15 @@ bool PDFNumberElement::Read(SvStream& rStream)
     {
         return false;
     }
-    if (!rtl::isAsciiDigit(static_cast<unsigned char>(ch)) && ch != '-' && ch != '.')
+    if (!rtl::isAsciiDigit(static_cast<unsigned char>(ch)) && ch != '-' && ch != '+' && ch != '.')
     {
         rStream.SeekRel(-1);
         return false;
     }
     while (!rStream.eof())
     {
-        if (!rtl::isAsciiDigit(static_cast<unsigned char>(ch)) && ch != '-' && ch != '.')
+        if (!rtl::isAsciiDigit(static_cast<unsigned char>(ch)) && ch != '-' && ch != '+'
+            && ch != '.')
         {
             rStream.SeekRel(-1);
             m_nLength = rStream.Tell() - m_nOffset;

commit 381357e3fa6ca7bf44a8785857ba98d97a6fab30

vcl pdf tokenizer: add real numbers testcase
Fails with commit 0d68738d67eacdfebdca3c9183dc11f953b38174 (Correctly
parse real numbers in PDFs, 2020-09-25) reverted.

Change-Id: I449ed781a3dd4b78e27a3fd2e135495e7d6c35ae

vcl/qa/cppunit/filter/ipdf/data/real-numbers.pdf
@@ -1,0 +1,55 @@
+%PDF-1.7
+% ò¤ô
+1 0 obj <<
+  /Type /Catalog
+  /Pages 2 0 R
+>>
+endobj
+2 0 obj <<
+  /Type /Pages
+  /MediaBox [0 0 200 300]
+  /Count 1
+  /Kids [3 0 R]
+>>
+endobj
+3 0 obj <<
+  /Type /Page
+  /Parent 2 0 R
+  /Contents 4 0 R
+>>
+endobj
+4 0 obj <<
+  /Length 188
+  /Test [.00 1.00 .00 1.00 .00 1.00]
+>>
+stream
+q
+0 0 0 rg
+0 290 10 10 re B*
+10 150 50 30 re B*
+0 0 1 rg
+190 290 10 10 re B*
+70 232 50 30 re B*
+0 1 0 rg
+190 0 10 10 re B*
+130 150 50 30 re B*
+1 0 0 rg
+0 0 10 10 re B*
+70 67 50 30 re B*
+Q
+endstream
+endobj
+xref
+0 5
+0000000000 65535 f 
+0000000015 00000 n 
+0000000068 00000 n 
+0000000157 00000 n 
+0000000226 00000 n 
+trailer <<
+  /Root 1 0 R
+  /Size 5
+>>
+startxref
+503
+%%EOF

vcl/qa/cppunit/filter/ipdf/ipdf.cxx
@@ -161,6 +161,23 @@ CPPUNIT_TEST_FIXTURE(VclFilterIpdfTest, testDictArrayDict)
     CPPUNIT_ASSERT(pKey);
 }
 
+CPPUNIT_TEST_FIXTURE(VclFilterIpdfTest, testRealNumbers)
+{
+    // Load a file that has markup like this:
+    // 4 0 obj <<
+    //   /Test [.00 1.00 .00 1.00 .00 1.00]
+    // >>
+    OUString aSourceURL = m_directories.getURLFromSrc(DATA_DIRECTORY) + "real-numbers.pdf";
+    SvFileStream aFile(aSourceURL, StreamMode::READ);
+    vcl::filter::PDFDocument aDocument;
+
+    // Without the accompanying fix in place, this test would have failed, because the parser
+    // stopped when it saw an unexpected "." character.
+    CPPUNIT_ASSERT(aDocument.Read(aFile));
+    std::vector<vcl::filter::PDFObjectElement*> aPages = aDocument.GetPages();
+    CPPUNIT_ASSERT(!aPages.empty());
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */