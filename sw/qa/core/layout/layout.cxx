LibreOffice/core

commit c9eb53f200225f2ee6ca695e1326843a487aee51

tdf#135198 sw editing: text box fell out of its shape
Without this patch if a shape anchored to a paragraph and containing
a text box was moved upwards beyond the vertical position of its anchor,
the text box failed to keep its position in sync with the shape.

This is fixed by telling the text box the absolute vertical position of
the corresponding shape after the latter is moved.

Note: Other fix is associated to the shortcut keys
Shift-Arrow Up/Shift-Arrow Down for moving text frames with
bigger steps vertically. Now it's not possible to use these
keys, but that was bad with text boxes, because it moved only
the text content of the text box.

Note: this patch fixes the vertical position of the text box
of textbox-wps-only.docx of the unit test textboxWpsOnly.

Change-Id: Ib66b13cae455462c616fed6bbd088433c83e61a0
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/104520
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/source/core/layout/objectformattertxtfrm.cxx
@@ -30,6 +30,10 @@
 #include <fmtfollowtextflow.hxx>
 #include <layact.hxx>
 #include <ftnfrm.hxx>
+#include <fmtornt.hxx>
+#include <textboxhelper.hxx>
+#include <svx/svdobj.hxx>
+#include <svx/svdpage.hxx>
 
 using namespace ::com::sun::star;
 
@@ -340,6 +344,32 @@ bool SwObjectFormatterTextFrame::DoFormatObjs()
            ( !mrAnchorTextFrame.IsFollow() &&
              AtLeastOneObjIsTmpConsiderWrapInfluence() ) ) )
     {
+        // tdf#135198: force text box to stay inside shape after layout changes
+        if (SwSortedObjs* pDrawObjs = mrAnchorTextFrame.GetDrawObjs())
+        {
+            // N.B.: avoid using ranged for because the iterator might get invalidated
+            for (size_t i = 0; i < pDrawObjs->size(); ++i)
+            {
+                SwAnchoredObject* const pObj = (*pDrawObjs)[i];
+                SwFrameFormat& rFormat = pObj->GetFrameFormat();
+                if (SwTextBoxHelper::isTextBox(&rFormat, RES_DRAWFRMFMT))
+                {
+                    SfxItemSet aSet(rFormat.GetDoc()->GetAttrPool(),
+                                    svl::Items<RES_VERT_ORIENT, RES_ANCHOR>{});
+                    if (const SwPageFrame* pPageFrame = pObj->GetPageFrame())
+                    {
+                        const SwRect& rPageFrameArea = pPageFrame->getFrameArea();
+                        aSet.Put(SwFormatVertOrient(pObj->GetObjRect().Top() - rPageFrameArea.Top(),
+                                                    text::VertOrientation::NONE,
+                                                    text::RelOrientation::PAGE_FRAME));
+                        aSet.Put(SwFormatAnchor(RndStdIds::FLY_AT_PAGE, pObj->GetPageFrame()->GetPhyPageNum()));
+
+                        SwTextBoxHelper::syncFlyFrameAttr(rFormat, aSet);
+                    }
+                }
+            }
+        }
+
         const bool bDoesAnchorHadPrev = ( mrAnchorTextFrame.GetIndPrev() != nullptr );
 
         // Format anchor text frame after its objects are formatted.

sw/qa/core/layout/layout.cxx
@@ -14,6 +14,11 @@
 #include <wrtsh.hxx>
 #include <docsh.hxx>
 #include <unotxdoc.hxx>
+#include <flyfrm.hxx>
+#include <fmtornt.hxx>
+//#include <frameformats.hxx>
+#include <frmtool.hxx>
+#include <textboxhelper.hxx>
 
 char const DATA_DIRECTORY[] = "/sw/qa/core/layout/data/";
 
@@ -161,6 +166,32 @@ CPPUNIT_TEST_FIXTURE(SwCoreLayoutTest, testAnchorPositionBasedOnParagraph)
     assertXPath(pXmlDoc, "(//SwAnchoredDrawObject)[2]/bounds", "bottom", "2008");
     assertXPath(pXmlDoc, "(//SwAnchoredDrawObject)[3]/bounds", "top", "3783");
     assertXPath(pXmlDoc, "(//SwAnchoredDrawObject)[3]/bounds", "bottom", "3844");
+}
+
+CPPUNIT_TEST_FIXTURE(SwCoreLayoutTest, testTextBoxStaysInsideShape)
+{
+    // tdf#135198: check whether text box stays inside shape after moving it upwards
+    load(DATA_DIRECTORY, "shape-textbox.odt");
+    SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument*>(mxComponent.get());
+    SwDocShell* pDocShell = pTextDoc->GetDocShell();
+    SwWrtShell* pWrtShell = pDocShell->GetWrtShell();
+    SdrObject* pTextBoxObj = pWrtShell->GetObjAt({ 8000, 3000 });
+
+    xmlDocUniquePtr pLayoutBefore = parseLayoutDump();
+    CPPUNIT_ASSERT(pLayoutBefore);
+    const int nTextBoxTopBefore = getXPath(pLayoutBefore, "//fly/infos/bounds", "top").toInt32();
+
+    uno::Reference<drawing::XShape> xShape(pTextBoxObj->getUnoShape(), uno::UNO_QUERY_THROW);
+    auto aPosition = xShape->getPosition();
+    aPosition.Y -= 500;
+    xShape->setPosition(aPosition);
+
+    discardDumpedLayout();
+    xmlDocUniquePtr pLayoutAfter = parseLayoutDump();
+    CPPUNIT_ASSERT(pLayoutAfter);
+    const int nTextBoxTopAfter = getXPath(pLayoutAfter, "//fly/infos/bounds", "top").toInt32();
+    CPPUNIT_ASSERT_MESSAGE("text box was supposed to stay inside its shape",
+                           nTextBoxTopAfter < nTextBoxTopBefore);
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();

sw/qa/extras/ooxmlimport/ooxmlimport.cxx
@@ -1065,7 +1065,8 @@ DECLARE_OOXMLIMPORT_TEST(textboxWpsOnly, "textbox-wps-only.docx")
     if ( nsScreen.frame.size.width * scaleFactor > 4000 )
         return;
 #endif
-    CPPUNIT_ASSERT_EQUAL(sal_Int32(2805), getProperty<sal_Int32>(xFrame, "VertOrientPosition"));
+    // Vertically oriented to page due to tdf#135198
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(5304), getProperty<sal_Int32>(xFrame, "VertOrientPosition"));
 }
 
 DECLARE_OOXMLIMPORT_TEST(testGroupshapeRelsize, "groupshape-relsize.docx")

sw/qa/extras/ooxmlexport/ooxmlexport10.cxx
@@ -661,9 +661,11 @@ DECLARE_OOXMLEXPORT_TEST(testChartSize, "chart-size.docx")
     // This was 10954.
     CPPUNIT_ASSERT_EQUAL(sal_Int32(6008), getProperty<sal_Int32>(xEmbeddedObjects->getByIndex(0), "Width"));
 
+    // The following assert no longer applies due to tdf#135198
+
     // Layout modified the document when it had this chart.
-    uno::Reference<util::XModifiable> xModifiable(mxComponent, uno::UNO_QUERY);
-    CPPUNIT_ASSERT_EQUAL(false, bool(xModifiable->isModified()));
+    //uno::Reference<util::XModifiable> xModifiable(mxComponent, uno::UNO_QUERY);
+    //CPPUNIT_ASSERT_EQUAL(false, bool(xModifiable->isModified()));
 }
 
 DECLARE_OOXMLEXPORT_TEST(testInlineGroupshape, "inline-groupshape.docx")

commit deba5acf14705742afa8e96c8387a45a441d5ce3

tdf#137881 sw: fix text box size regression
from commit c9eb53f200225f2ee6ca695e1326843a487aee51
(tdf#135198 sw editing: text box fell out of its shape).

The height of the bottom text box frame from the tdf#117921 sample
file grew unreasonably large. This is countered by only activating
the previous fix if it is strictly needed, i.e. when a shape and
its text box drift apart from each other.

The test file is derived from the one used for tdf#117921.

Change-Id: Ied01c6089efe586b73de559f89f08b958f4b62d7

sw/source/core/layout/objectformattertxtfrm.cxx
@@ -355,23 +355,22 @@ bool SwObjectFormatterTextFrame::DoFormatObjs()
                 SwFrameFormat& rFormat = pObj->GetFrameFormat();
                 if (SwTextBoxHelper::isTextBox(&rFormat, RES_DRAWFRMFMT))
                 {
-                    if (const SwPageFrame* pPageFrame = pObj->GetPageFrame())
+                    const SwFrameFormat* pOtherFormat = SwTextBoxHelper::getOtherTextBoxFormat(&rFormat, RES_DRAWFRMFMT);
+                    if (rFormat.GetVertOrient().GetPos() != pOtherFormat->GetVertOrient().GetPos() ||
+                        rFormat.GetAnchor().GetAnchorId() != pOtherFormat->GetAnchor().GetAnchorId())
                     {
-                        SwDoc* pDoc = rFormat.GetDoc();
+                        if (const SwPageFrame* pPageFrame = pObj->GetPageFrame())
+                        {
+                            const SwRect& rPageFrameArea = pPageFrame->getFrameArea();
+                            SfxItemSet aSet(rFormat.GetDoc()->GetAttrPool(),
+                                            svl::Items<RES_VERT_ORIENT, RES_ANCHOR>{});
+                            aSet.Put(SwFormatVertOrient(pObj->GetObjRect().Top() - rPageFrameArea.Top(),
+                                                        text::VertOrientation::NONE,
+                                                        text::RelOrientation::PAGE_FRAME));
+                            aSet.Put(SwFormatAnchor(RndStdIds::FLY_AT_PAGE, pObj->GetPageFrame()->GetPhyPageNum()));
 
-                        // avoid Undo creation,
-                        ::sw::UndoGuard const ug(pDoc->GetIDocumentUndoRedo());
-
-                        SfxItemSet aSet(pDoc->GetAttrPool(),
-                                        svl::Items<RES_VERT_ORIENT, RES_ANCHOR>{});
-
-                        const SwRect& rPageFrameArea = pPageFrame->getFrameArea();
-                        aSet.Put(SwFormatVertOrient(pObj->GetObjRect().Top() - rPageFrameArea.Top(),
-                                                    text::VertOrientation::NONE,
-                                                    text::RelOrientation::PAGE_FRAME));
-                        aSet.Put(SwFormatAnchor(RndStdIds::FLY_AT_PAGE, pObj->GetPageFrame()->GetPhyPageNum()));
-
-                        SwTextBoxHelper::syncFlyFrameAttr(rFormat, aSet);
+                            SwTextBoxHelper::syncFlyFrameAttr(rFormat, aSet);
+                        }
                     }
                 }
             }

sw/qa/core/layout/layout.cxx
@@ -196,6 +196,21 @@ CPPUNIT_TEST_FIXTURE(SwCoreLayoutTest, testTextBoxStaysInsideShape)
                            nTextBoxTopAfter < nTextBoxTopBefore);
 }
 
+CPPUNIT_TEST_FIXTURE(SwCoreLayoutTest, testTextBoxSizeAtBottomOfPage)
+{
+    // tdf#137881: check whether text box at the bottom of the page stays the right size
+    load(DATA_DIRECTORY, "bottom-textbox-size.docx");
+    SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument*>(mxComponent.get());
+    SwDoc* pDoc = pTextDoc->GetDocShell()->GetDoc();
+    SdrPage* pPage = pDoc->getIDocumentDrawModelAccess().GetDrawModel()->GetPage(0);
+    SdrObject* pTextBoxObj = pPage->GetObj(0);
+
+    uno::Reference<drawing::XShape> xShape(pTextBoxObj->getUnoShape(), uno::UNO_QUERY_THROW);
+    const sal_Int32 nHeight = xShape->getSize().Height;
+    CPPUNIT_ASSERT_EQUAL_MESSAGE("text box was not supposed to be resized on fileopen",
+                                 static_cast<sal_Int32>(2200), nHeight);
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sw/qa/extras/ooxmlimport/ooxmlimport.cxx
@@ -1065,8 +1065,7 @@ DECLARE_OOXMLIMPORT_TEST(textboxWpsOnly, "textbox-wps-only.docx")
     if ( nsScreen.frame.size.width * scaleFactor > 4000 )
         return;
 #endif
-    // Vertically oriented to page due to tdf#135198
-    CPPUNIT_ASSERT_EQUAL(sal_Int32(5304), getProperty<sal_Int32>(xFrame, "VertOrientPosition"));
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(2805), getProperty<sal_Int32>(xFrame, "VertOrientPosition"));
 }
 
 DECLARE_OOXMLIMPORT_TEST(testGroupshapeRelsize, "groupshape-relsize.docx")