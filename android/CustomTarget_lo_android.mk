be2a4891bc8c52a7712a9bdefbfa7b10226e9701
Thorsten Behrens <Thorsten.Behrens@CIB.de>
2020-11-07 18:00:36 +0100

    Don't clobber SRCDIR with APK result
    Odd - a6ecd8b2a9f0f3eaa66388861a6dcc6260ec72b9 had this copied
    under instsetoo_native/foo for the tinderbox to pick it up.
    Can perhaps be solved differently?
    
    Change-Id: I65ea2fcd513f272d3cc4c04a4347ef3f018f8f61

--- android/CustomTarget_lo_android.mk
+++ android/CustomTarget_lo_android.mk
@@ -17,14 +17,6 @@ $(loandroid3_DIR)/done : $(call gb_Postprocess_get_target,AllModulesButInstsetNa
 	$(call gb_Output_announce,$(subst $(WORKDIR)/,,$@),$(true),MAK,2)
 	$(call gb_Trace_StartRange,$(subst $(WORKDIR)/,,$@),MAK)
 	cd $(SRCDIR)/android/source && $(MAKE) all
-ifeq ($(ENABLE_JAVA),TRUE)
-# Copy to $(BUILDDIR)/instsetoo_native as that is where the tinderbox build script
-# still looks for the .apk, and we want fresh daily builds to be uploaded.
-# Us "foo" instead of the old INPATH
-	mkdir -p $(BUILDDIR)/instsetoo_native/foo/bin; \
-	if test "$$ENABLE_ANDROID_LOK" != "TRUE" ; then cp $(WORKDIR)/gradle-build/outputs/apk/strippedUI/$(if $(ENABLE_ANDROID_EDITING),Editing)/$(if $(ENABLE_RELEASE_BUILD),release/*-release-unsigned.apk,debug/*-debug.apk) $(BUILDDIR)/instsetoo_native/foo/bin ; fi
-	$(call gb_Trace_EndRange,$(subst $(WORKDIR)/,,$@),MAK)
-endif
 
 $(call gb_CustomTarget_get_clean_target,android/loandroid3) :
 	$(call gb_Output_announce,$(subst $(WORKDIR)/Clean/,,$@),$(false),MAK,2)
