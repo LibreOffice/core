4e1813389e63f590bf2559153658d42370868a22
Noel Grandin <noelgrandin@gmail.com>
2020-11-08 15:26:37 +0200

    dont call StartElement from startUnknownElement
    to avoid having the StartElement/endFastElement call pairs
    end up unbalanced.
    
    Change-Id: I331aa48e3fd928a093d1f6b3248d9f9668ef83ed

--- xmloff/source/core/xmlictxt.cxx
+++ xmloff/source/core/xmlictxt.cxx
@@ -70,37 +70,34 @@ void SAL_CALL SvXMLImportContext::startFastElement(sal_Int32 nElement, const uno
     const OUString& rPrefix = SvXMLImport::getNamespacePrefixFromToken(nElement, &GetImport().GetNamespaceMap());
     const OUString& rLocalName = SvXMLImport::getNameFromToken( nElement );
     startUnknownElement( SvXMLImport::aDefaultNamespace, (rPrefix.isEmpty())? rLocalName : rPrefix + SvXMLImport::aNamespaceSeparator + rLocalName, Attribs );
-}
-
-void SAL_CALL SvXMLImportContext::startUnknownElement(const OUString & /*rNamespace*/, const OUString & /*rElementName*/,
-    const uno::Reference< xml::sax::XFastAttributeList > & Attribs)
-{
     mrImport.maAttrList->Clear();
     mrImport.maNamespaceHandler->addNSDeclAttributes( mrImport.maAttrList );
 
-    if ( Attribs.is() )
+    for( auto &it : sax_fastparser::castToFastAttributeList( Attribs ) )
     {
-        for( auto &it : sax_fastparser::castToFastAttributeList( Attribs ) )
-        {
-            sal_Int32 nToken = it.getToken();
-            const OUString& rAttrNamespacePrefix = SvXMLImport::getNamespacePrefixFromToken(nToken, &GetImport().GetNamespaceMap());
-            OUString sAttrName = SvXMLImport::getNameFromToken( nToken );
-            if ( !rAttrNamespacePrefix.isEmpty() )
-                sAttrName = rAttrNamespacePrefix + SvXMLImport::aNamespaceSeparator + sAttrName;
+        sal_Int32 nToken = it.getToken();
+        const OUString& rAttrNamespacePrefix = SvXMLImport::getNamespacePrefixFromToken(nToken, &GetImport().GetNamespaceMap());
+        OUString sAttrName = SvXMLImport::getNameFromToken( nToken );
+        if ( !rAttrNamespacePrefix.isEmpty() )
+            sAttrName = rAttrNamespacePrefix + SvXMLImport::aNamespaceSeparator + sAttrName;
 
-            mrImport.maAttrList->AddAttribute( sAttrName, "CDATA", it.toString() );
-        }
+        mrImport.maAttrList->AddAttribute( sAttrName, "CDATA", it.toString() );
+    }
 
-        const uno::Sequence< xml::Attribute > unknownAttribs = Attribs->getUnknownAttributes();
-        for ( const auto& rUnknownAttrib : unknownAttribs )
-        {
-            const OUString& rAttrValue = rUnknownAttrib.Value;
-            const OUString& rAttrName = rUnknownAttrib.Name;
-            // note: rAttrName is expected to be namespace-prefixed here
-            mrImport.maAttrList->AddAttribute( rAttrName, "CDATA", rAttrValue );
-        }
+    const uno::Sequence< xml::Attribute > unknownAttribs = Attribs->getUnknownAttributes();
+    for ( const auto& rUnknownAttrib : unknownAttribs )
+    {
+        const OUString& rAttrValue = rUnknownAttrib.Value;
+        const OUString& rAttrName = rUnknownAttrib.Name;
+        // note: rAttrName is expected to be namespace-prefixed here
+        mrImport.maAttrList->AddAttribute( rAttrName, "CDATA", rAttrValue );
     }
     StartElement( mrImport.maAttrList.get() );
+}
+
+void SAL_CALL SvXMLImportContext::startUnknownElement(const OUString & /*rNamespace*/, const OUString & /*rElementName*/,
+    const uno::Reference< xml::sax::XFastAttributeList > & /*Attribs*/)
+{
 }
 
 void SAL_CALL SvXMLImportContext::endUnknownElement (const OUString & /*rNamespace*/, const OUString & /*rElementName*/)
