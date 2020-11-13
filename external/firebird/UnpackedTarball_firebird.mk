45061b32a1d50da221a9dac1c46252349e68343d
Jan-Marek Glogowski <glogow@fbihome.de>
2020-11-13 18:45:29 +0100

    Upgrade Firebird to 3.0.7
    This tries to get rid of a lot of cruft from older builds and it
    also aims to build as much as possible on Windows and do parallel
    builds on all archs (by dropping the "unset MAKEFLAGS").
    
    AFAI can tell, all the "CPU" stuff handling in UnpackedTarball is
    now / then pointless. Without implicit make rules, it seems only
    one rule in btyacc needs to be added to pass the build..
    
    The firebird-cygwin-msvc-warnings.patch should be optional. It
    gets rid of various compiler warnings on Windows, either by
    disableing or fixing them:
    - fix: ignoring unknown option '-fno-rtti'
    - fix: unrecognized source file type 'warshall.o', object file
      assumed
    - ign: C4291 - no matching operator delete found...
    - ign: C4477 - format string type mismatches for long values and
      some others
    
    And I explicitly got rid of the "win32" handling and simply use
    arch depending patches on Windows, which strips additional stuff.
    
    Change-Id: Id34bb88900d15f89adda03e34af2ac3d4f6aa085

--- external/firebird/UnpackedTarball_firebird.mk
+++ external/firebird/UnpackedTarball_firebird.mk
@@ -25,22 +25,20 @@ $(eval $(call gb_UnpackedTarball_add_patches,firebird,\
         external/firebird/firebird.disable-ib-util-not-found.patch.1 \
 		external/firebird/firebird-Engine12.patch \
 		external/firebird/firebird-rpath.patch.0 \
-		external/firebird/firebird-cloop-compiler.patch.1 \
-		external/firebird/firebird-gcc6.patch.1 \
 		external/firebird/wnt-dbgutil.patch \
-		external/firebird/libc++.patch \
-		external/firebird/0001-Avoid-hangup-in-SS-when-error-happens-at-system-atta.patch.1 \
-		external/firebird/0002-Backported-fix-for-CORE-5452-Segfault-when-engine-s-.patch.1 \
 		external/firebird/c++17.patch \
 		external/firebird/ubsan.patch \
 		external/firebird/asan.patch \
 		external/firebird/firebird-tdf125284.patch.1 \
 		external/firebird/0001-Make-comparison-operator-member-functions-const.patch.1 \
+		external/firebird/firebird-307.patch.1 \
+		external/firebird/firebird-btyacc-add-explicit-rule.patch \
 ))
 
 ifeq ($(OS),WNT)
 $(eval $(call gb_UnpackedTarball_add_patches,firebird,\
 	external/firebird/firebird-cygwin-msvc.patch \
+	external/firebird/firebird-cygwin-msvc-warnings.patch \
 	external/firebird/firebird-vs2017.patch.1 \
 ))
 endif
