d379b8ac4234d23e82d19b801c807987ab105968
Bjoern Michaelsen <bjoern.michaelsen@libreoffice.org>
2020-11-15 11:28:05 +0100

    Replace needless SwClientNotifyCall with CallSwClientNotify
    Change-Id: If1286c3271038be3ad69739b54d5ed5ce1167818
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/105894
    Tested-by: Jenkins
    Reviewed-by: Bjoern Michaelsen <bjoern.michaelsen@libreoffice.org>

--- sw/source/core/doc/DocumentTimerManager.cxx
+++ sw/source/core/doc/DocumentTimerManager.cxx
@@ -195,7 +195,7 @@ IMPL_LINK_NOARG( DocumentTimerManager, DoIdleJobs, Timer*, void )
         pShell->LockView( true );
 
         auto pChapterFieldType = m_rDoc.getIDocumentFieldsAccess().GetSysFieldType( SwFieldIds::Chapter );
-        pChapterFieldType->SwClientNotifyCall(*pChapterFieldType, sw::LegacyModifyHint( nullptr, nullptr ));  // ChapterField
+        pChapterFieldType->CallSwClientNotify(sw::LegacyModifyHint( nullptr, nullptr ));  // ChapterField
         m_rDoc.getIDocumentFieldsAccess().UpdateExpFields( nullptr, false );  // Updates ExpressionFields
         m_rDoc.getIDocumentFieldsAccess().UpdateTableFields(nullptr);  // Tables
         m_rDoc.getIDocumentFieldsAccess().UpdateRefFields();  // References
