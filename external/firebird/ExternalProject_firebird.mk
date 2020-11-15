22b6cfa35c4cec8462785376b8543ce9cd4e422a
Jan-Marek Glogowski <glogow@fbihome.de>
2020-11-15 19:57:48 +0100

    firebird: build fixes (incl. parallel build)
    The main idea is to get rid of the "unset MAKEFLAGS". AFAI can
    see, the whole CPU stuff isn't used anymore. So we can drop the
    whole FB_CPU_ARG handling. Since LO doesn't use any of make's
    implicit rules, the build breaks, but luckily it just requires a
    single rule for the btyacc build - just a Firebuild build tool.
    
    Then there is the whole broken handling of LIBTOMMATH and
    LIBATOMIC_OPS already in LO's configure.ac. I don't know if any
    internal build of Firebird with these as system libs would work.
    I guess people either have a system Firebird or also build with
    internal libtommath and libatomic_ops. This fixes just the
    obvious errors. I didn't try to build it, so there might still
    be typos (TBH I thought hard about just dropping the system
    build of these libraries, after seeing the broken configure.ac).
    
    And I'm not sure our / the system boost preprocessor library is
    ever used over the Firebird-internal copy. It also looks like
    it's also just used in an other build tool and nothing of the
    Firebird DB itself depends on it.
    
    Then there is the movement of the install_name_tool / otool
    patching on MacOS from the patch into the ExternalProject to
    further shrink the patches, as the build doesn't depend on it.
    
    This also introduces a different debug build mode for the
    gcc-/g++-wrapper: MSVC_USE_INDIVIDUAL_PDBS.
    It uses -Fd to write a separate PDB per output file instead of
    using -FS to use sync writes to a single PDB, which might work
    around the PDB access failures seen by Jenkins for linking
    executables. In theory it's also faster and should work with all
    the other wrapper users, but I don't want to open that can of
    additional build errors (yet), for eventually marginal gains.
    
    Change-Id: I8d4c5d2f17def9e840a67ef1004787e8baaffa83
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/105902
    Tested-by: Jenkins
    Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>

--- external/firebird/ExternalProject_firebird.mk
+++ external/firebird/ExternalProject_firebird.mk
@@ -22,34 +22,20 @@ $(eval $(call gb_ExternalProject_register_targets,firebird,\
 	build \
 ))
 
-ifneq ($(OS),WNT)
-INVOKE_FPA:="CPU=\$$(EMPTY) $${FB_CPU_ARG}"
-endif
-
-ifeq ($(COM_IS_CLANG),TRUE)
-firebird_NO_CXX11_NARROWING := -Wno-c++11-narrowing
-endif
-
-MAKE_PRE=LC_ALL=C
-
-MAKE_POST=$(if $(filter MACOSX,$(OS)),&& $(PERL) \
-			$(SRCDIR)/solenv/bin/macosx-change-install-names.pl shl OOO \
-			$(EXTERNAL_WORKDIR)/gen/$(if $(ENABLE_DEBUG),Debug,Release)/firebird/plugins/libEngine12.dylib \
-			$(EXTERNAL_WORKDIR)/gen/$(if $(ENABLE_DEBUG),Debug,Release)/firebird/lib/libfbclient.dylib.3.0.0)
+firebird_BUILDDIR = $(EXTERNAL_WORKDIR)/gen/$(if $(ENABLE_DEBUG),Debug,Release)/firebird
+firebird_VERSION := 3.0.0
 
 $(call gb_ExternalProject_get_state_target,firebird,build):
 	$(call gb_Trace_StartRange,firebird,EXTERNAL)
 	$(call gb_ExternalProject_run,build,\
-		unset MAKEFLAGS \
-		&& FB_CPU_ARG='$(filter --jobserver-fds=%,$(MAKEFLAGS))' \
-		&& export PKG_CONFIG="" \
+		export PKG_CONFIG="" \
 		&& export CPPFLAGS=" \
+			$(BOOST_CPPFLAGS) \
 			$(if $(SYSTEM_LIBATOMIC_OPS),$(LIBATOMIC_OPS_CFLAGS), \
 				-I$(call gb_UnpackedTarball_get_dir,libatomic_ops)/src \
 			) \
 			$(if $(SYSTEM_LIBTOMMATH),$(LIBTOMMATH_CFLAGS), \
 				-I$(call gb_UnpackedTarball_get_dir,libtommath) \
-				-L$(call gb_UnpackedTarball_get_dir,libtommath) \
 			) \
 			$(if $(SYSTEM_ICU),$(ICU_CPPFLAGS), \
 				-I$(call gb_UnpackedTarball_get_dir,icu)/source \
@@ -59,30 +45,26 @@ $(call gb_ExternalProject_get_state_target,firebird,build):
 			$(if $(filter GCC-INTEL,$(COM)-$(CPUNAME)),-Di386=1) \
 			" \
 		&& export CXXFLAGS=" \
+			$(BOOST_CXXFLAGS) \
 			$(if $(filter MSC,$(COM)),$(if $(MSVC_USE_DEBUG_RUNTIME),-DMSVC_USE_DEBUG_RUNTIME)) \
 			$(if $(HAVE_GCC_FNO_SIZED_DEALLOCATION),-fno-sized-deallocation -fno-delete-null-pointer-checks) \
-			$(if $(SYSTEM_BOOST),$(BOOST_CPPFLAGS), \
-				$(BOOST_CPPFLAGS) \
-				-L$(call gb_UnpackedTarball_get_dir,boost)/source/lib \
-			) \
-			$(if $(SYSTEM_ICU),$(ICU_CPPFLAGS), \
-				-I$(call gb_UnpackedTarball_get_dir,icu)/source \
-				-I$(call gb_UnpackedTarball_get_dir,icu)/source/i18n \
-				-I$(call gb_UnpackedTarball_get_dir,icu)/source/common \
-			) \
-			$(if $(SYSTEM_LIBTOMMATH),$(LIBTOMMATH_CFLAGS), \
-				-L$(call gb_UnpackedTarball_get_dir,libtommath) \
-			) \
 			$(CXXFLAGS_CXX11) \
-			$(firebird_NO_CXX11_NARROWING) \
+			$(if $(filter TRUE,$(COM_IS_CLANG)), -Wno-c++11-narrowing) \
 			$(if $(call gb_Module__symbols_enabled,firebird),$(gb_DEBUGINFO_FLAGS)) \
 		" \
 		&& export LDFLAGS=" \
+			$(if $(SYSTEM_LIBATOMIC_OPS),$(LIBATOMIC_OPS_LIBS), \
+				-L$(call gb_UnpackedTarball_get_dir,libatomic_ops)/src \
+			) \
+			$(if $(SYSTEM_LIBTOMMATH),$(LIBTOMMATH_LIBS), \
+				-L$(call gb_UnpackedTarball_get_dir,libtommath) \
+			) \
 			$(if $(SYSTEM_ICU),$(ICU_LIBS), \
 				-L$(call gb_UnpackedTarball_get_dir,icu)/source/lib \
 			) \
 		" \
 		&& export LIBREOFFICE_ICU_LIB="$(call gb_UnpackedTarball_get_dir,icu)/source/lib" \
+		&& export MSVC_USE_INDIVIDUAL_PDBS=TRUE \
 		&& MAKE=$(MAKE) ./configure \
 			--without-editline \
 			--with-wire-compress=no \
@@ -97,19 +79,21 @@ $(call gb_ExternalProject_get_state_target,firebird,build):
 						$(shell expr '$(MAC_OS_X_VERSION_MIN_REQUIRED)' \
 							'<' 101200)), \
 					ac_cv_func_clock_gettime=no)) \
-		&& if [ -n "$${FB_CPU_ARG}" ]; then \
-				$(MAKE_PRE) $(MAKE) \
-					$(if $(filter LINUX,$(OS)),CXXFLAGS="$$CXXFLAGS -std=gnu++11") \
-					$(if $(ENABLE_DEBUG),Debug) $(INVOKE_FPA) SHELL='$(SHELL)' \
-					LIBO_TUNNEL_LIBRARY_PATH='$(subst ','\'',$(subst $$,$$$$,$(call gb_Helper_extend_ld_path,$(call gb_UnpackedTarball_get_dir,icu)/source/lib)))' \
-				$(MAKE_POST); \
-			else \
-				$(MAKE_PRE) $(MAKE) \
-					$(if $(filter LINUX,$(OS)),CXXFLAGS="$$CXXFLAGS -std=gnu++11") \
-					$(if $(ENABLE_DEBUG),Debug) SHELL='$(SHELL)' \
-					LIBO_TUNNEL_LIBRARY_PATH='$(subst ','\'',$(subst $$,$$$$,$(call gb_Helper_extend_ld_path,$(call gb_UnpackedTarball_get_dir,icu)/source/lib)))' \
-				$(MAKE_POST); \
-			fi \
+		&& LC_ALL=C $(MAKE) \
+			$(if $(ENABLE_DEBUG),Debug) SHELL='$(SHELL)' $(if $(filter LINUX,$(OS)),CXXFLAGS="$$CXXFLAGS -std=gnu++11") \
+			MATHLIB="$(if $(SYSTEM_LIBTOMMATH),$(LIBTOMMATH_LIBS),-L$(call gb_UnpackedTarball_get_dir,libtommath) -ltommath)" \
+			LIBO_TUNNEL_LIBRARY_PATH='$(subst ','\'',$(subst $$,$$$$,$(call gb_Helper_extend_ld_path,$(call gb_UnpackedTarball_get_dir,icu)/source/lib)))' \
+		$(if $(filter MACOSX,$(OS)), \
+			&& install_name_tool -id @__________________________________________________OOO/libfbclient.dylib.$(firebird_VERSION) \
+				$(firebird_BUILDDIR)/lib/libfbclient.dylib.$(firebird_VERSION) \
+			&& install_name_tool -id @__________________________________________________OOO/libEngine12.dylib \
+				$(firebird_BUILDDIR)/plugins/libEngine12.dylib \
+			&& install_name_tool -change $(firebird_BUILDDIR)/lib/libfbclient.dylib.$(firebird_VERSION) \
+				@loader_path/libfbclient.dylib.$(firebird_VERSION) $(firebird_BUILDDIR)/plugins/libEngine12.dylib \
+			&& $(PERL) $(SRCDIR)/solenv/bin/macosx-change-install-names.pl shl OOO \
+				$(firebird_BUILDDIR)/lib/libfbclient.dylib.$(firebird_VERSION) \
+				$(firebird_BUILDDIR)/plugins/libEngine12.dylib \
+			) \
 	)
 	$(call gb_Trace_EndRange,firebird,EXTERNAL)
 
