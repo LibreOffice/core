LibreOffice/core

commit 0d2340998415fb4b2f794054c62ef61c83e32155

tdf#136061 Chart ODF/OOXML: fix missing custom labels
by UNO extensions ShowCustomLabel in DataPointLabel.idl and
CUSTOM in ChartDataCaption.idl, fixing OOXML/ODF import/export.

We should display custom data label even if DataPointLabel is
disabled (e.g. category name and/or value fields are not displayed).

Note: import of the embedded chart of the DOCX unit test
document uses also ODF format in the background, testing
also the extension of the native file format.

Change-Id: I73e21f1e69fddec9f3b4163c46b6582cd1c74b5d
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/101640
Tested-by: Jenkins
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

oox/source/drawingml/chart/seriesconverter.cxx
@@ -109,7 +109,7 @@ void convertTextProperty(PropertySet& rPropSet, ObjectFormatter& rFormatter,
 
 void lclConvertLabelFormatting( PropertySet& rPropSet, ObjectFormatter& rFormatter,
                                 const DataLabelModelBase& rDataLabel, const TypeGroupConverter& rTypeGroup,
-                                bool bDataSeriesLabel, bool bMSO2007Doc )
+                                bool bDataSeriesLabel, bool bCustomLabelField, bool bMSO2007Doc )
 {
     const TypeGroupInfo& rTypeInfo = rTypeGroup.getTypeInfo();
 
@@ -137,7 +137,7 @@ void lclConvertLabelFormatting( PropertySet& rPropSet, ObjectFormatter& rFormatt
     // type of attached label
     if( bHasAnyElement || rDataLabel.mbDeleted )
     {
-        DataPointLabel aPointLabel( bShowValue, bShowPercent, bShowCateg, bShowSymbol );
+        DataPointLabel aPointLabel( bShowValue, bShowPercent, bShowCateg, bShowSymbol, bCustomLabelField );
         rPropSet.setProperty( PROP_Label, aPointLabel );
     }
 
@@ -271,7 +271,8 @@ void DataLabelConverter::convertFromModel( const Reference< XDataSeries >& rxDat
     {
         bool bMSO2007Doc = getFilter().isMSO2007Document();
         PropertySet aPropSet( rxDataSeries->getDataPointByIndex( mrModel.mnIndex ) );
-        lclConvertLabelFormatting( aPropSet, getFormatter(), mrModel, rTypeGroup, false, bMSO2007Doc );
+        bool bCustomLabelField = mrModel.mxText && mrModel.mxText->mxTextBody && !mrModel.mxText->mxTextBody->getParagraphs().empty();
+        lclConvertLabelFormatting( aPropSet, getFormatter(), mrModel, rTypeGroup, false, bCustomLabelField, bMSO2007Doc );
         const TypeGroupInfo& rTypeInfo = rTypeGroup.getTypeInfo();
         bool bIsPie = rTypeInfo.meTypeCategory == TYPECATEGORY_PIE;
 
@@ -289,7 +290,7 @@ void DataLabelConverter::convertFromModel( const Reference< XDataSeries >& rxDat
             importFillProperties(aPropSet, *mrModel.mxShapeProp, getFilter().getGraphicHelper(),
                                  rHelper);
         }
-        if( mrModel.mxText && mrModel.mxText->mxTextBody && !mrModel.mxText->mxTextBody->getParagraphs().empty() )
+        if( bCustomLabelField )
         {
             css::uno::Reference< XComponentContext > xContext = getComponentContext();
             uno::Sequence< css::uno::Reference< XDataPointCustomLabelField > > aSequence;
@@ -405,7 +406,7 @@ void DataLabelsConverter::convertFromModel( const Reference< XDataSeries >& rxDa
         // tdf#132174: the inner data table has no own cell number format.
         if( getChartDocument()->hasInternalDataProvider() && mrModel.mobShowVal.get(!bMSO2007Doc) )
             mrModel.maNumberFormat.mbSourceLinked = false;
-        lclConvertLabelFormatting( aPropSet, getFormatter(), mrModel, rTypeGroup, true, bMSO2007Doc );
+        lclConvertLabelFormatting( aPropSet, getFormatter(), mrModel, rTypeGroup, true, false, bMSO2007Doc );
 
         if (mrModel.mxShapeProp)
         {

chart2/source/controller/chartapiwrapper/WrappedDataCaptionProperties.cxx
@@ -71,7 +71,7 @@ sal_Int32 lcl_LabelToCaption( const chart2::DataPointLabel& rLabel )
 
 chart2::DataPointLabel lcl_CaptionToLabel( sal_Int32 nCaption )
 {
-    chart2::DataPointLabel aLabel(false,false,false,false);
+    chart2::DataPointLabel aLabel(false,false,false,false,false);
 
     if( nCaption & css::chart::ChartDataCaption::VALUE )
         aLabel.ShowNumber = true;
@@ -81,6 +81,8 @@ chart2::DataPointLabel lcl_CaptionToLabel( sal_Int32 nCaption )
         aLabel.ShowCategoryName = true;
     if( nCaption & css::chart::ChartDataCaption::SYMBOL )
         aLabel.ShowLegendSymbol = true;
+    if( nCaption & css::chart::ChartDataCaption::CUSTOM )
+        aLabel.ShowCustomLabel = true;
 
     return aLabel;
 }

chart2/source/controller/main/ChartController_Tools.cxx
@@ -761,6 +761,7 @@ bool ChartController::executeDispatch_Delete()
                     aLabel.ShowNumberInPercent = false;
                     aLabel.ShowCategoryName = false;
                     aLabel.ShowLegendSymbol = false;
+                    aLabel.ShowCustomLabel = false;
                     if( aObjectType == OBJECTTYPE_DATA_LABELS )
                     {
                         uno::Reference< chart2::XDataSeries > xSeries( ObjectIdentifier::getDataSeriesForCID( aCID, getModel() ));

chart2/source/model/main/DataPointProperties.cxx
@@ -515,7 +515,8 @@ void DataPointProperties::AddDefaultsToMap(
             false, // ShowNumber
             false, // ShowNumberInPercent
             false, // ShowCategoryName
-            false  // ShowLegendSymbol
+            false, // ShowLegendSymbol
+            false  // ShowCustomLabel
             ));
 
     PropertyHelper::setPropertyValueDefault( rOutMap, PROP_DATAPOINT_TEXT_WORD_WRAP, false );

chart2/source/tools/DataSeriesHelper.cxx
@@ -170,6 +170,7 @@ void lcl_insertOrDeleteDataLabelsToSeriesAndAllPoints( const Reference< chart2::
                         {
                             aLabel.ShowNumberInPercent = false;
                             aLabel.ShowCategoryName = false;
+                            aLabel.ShowCustomLabel = false;
                         }
                         xPointProp->setPropertyValue(CHART_UNONAME_LABEL, uno::Any(aLabel));
                     }
@@ -746,7 +747,7 @@ bool hasDataLabelsAtPoints( const Reference< chart2::XDataSeries >& xSeries )
                     {
                         DataPointLabel aLabel;
                         if( xPointProp->getPropertyValue(CHART_UNONAME_LABEL) >>= aLabel )
-                            bRet = aLabel.ShowNumber || aLabel.ShowNumberInPercent || aLabel.ShowCategoryName;
+                            bRet = aLabel.ShowNumber || aLabel.ShowNumberInPercent || aLabel.ShowCategoryName || aLabel.ShowCustomLabel;
                         if( bRet )
                             break;
                     }
@@ -784,7 +785,7 @@ bool hasDataLabelAtPoint( const Reference< chart2::XDataSeries >& xSeries, sal_I
             {
                 DataPointLabel aLabel;
                 if( xProp->getPropertyValue(CHART_UNONAME_LABEL) >>= aLabel )
-                    bRet = aLabel.ShowNumber || aLabel.ShowNumberInPercent || aLabel.ShowCategoryName;
+                    bRet = aLabel.ShowNumber || aLabel.ShowNumberInPercent || aLabel.ShowCategoryName || aLabel.ShowCustomLabel;
             }
         }
     }
@@ -834,6 +835,7 @@ void deleteDataLabelsFromPoint( const Reference< beans::XPropertySet >& xPointPr
             aLabel.ShowNumber = false;
             aLabel.ShowNumberInPercent = false;
             aLabel.ShowCategoryName = false;
+            aLabel.ShowCustomLabel = false;
             xPointProp->setPropertyValue(CHART_UNONAME_LABEL, uno::Any(aLabel));
         }
     }

chart2/source/view/main/VDataSeries.cxx
@@ -996,7 +996,7 @@ DataPointLabel* VDataSeries::getDataPointLabelIfLabel( sal_Int32 index ) const
 {
     DataPointLabel* pLabel = getDataPointLabel( index );
     if( !pLabel || (!pLabel->ShowNumber && !pLabel->ShowNumberInPercent
-        && !pLabel->ShowCategoryName ) )
+        && !pLabel->ShowCategoryName && !pLabel->ShowCustomLabel ) )
         return nullptr;
     return pLabel;
 }

offapi/com/sun/star/chart/ChartDataCaption.idl
@@ -68,6 +68,14 @@ published constants ChartDataCaption
     /** The symbol of data column/row is additionally displayed in the caption.
      */
     const long SYMBOL = 16;
+
+
+    /** The caption contains a custom text, which belongs
+        to a data point label.
+
+        @since LibreOffice 7.1
+     */
+    const long CUSTOM = 32;
 };
 
 

offapi/com/sun/star/chart2/DataPointLabel.idl
@@ -57,6 +57,13 @@ struct DataPointLabel
         caption.
      */
     boolean                     ShowLegendSymbol;
+
+    /** The caption contains a custom label text, which belongs
+        to a data point label.
+
+        @since LibreOffice 7.1
+     */
+    boolean                     ShowCustomLabel;
 };
 
 

commit 5ae33bc6fe85e5f4cf9d9a2fd84aea378a34cb32

tdf#138204 Chart OOXML Import: doesn't show placeholder
for CELLRANGE data label field type, like we do at CELLREF,
while both of them are not implemented correctly.

Regression from commit: 0d2340998415fb4b2f794054c62ef61c83e32155
(tdf#136061 Chart ODF/OOXML: fix missing custom labels)

Change-Id: I8e0f4ca9db6e613a190cbda31504a7542fc13a86

chart2/source/view/charttypes/VSeriesPlotter.cxx
@@ -542,6 +542,7 @@ uno::Reference< drawing::XShape > VSeriesPlotter::createDataLabel( const uno::Re
                         break;
                     }
                     case DataPointCustomLabelFieldType_CELLREF:
+                    case DataPointCustomLabelFieldType_CELLRANGE:
                     {
                         // TODO: for now doesn't show placeholder
                         aTextList[i] = OUString();

offapi/com/sun/star/chart2/DataPointCustomLabelFieldType.idl
@@ -24,7 +24,8 @@ enum DataPointCustomLabelFieldType
     CATEGORYNAME,
     CELLREF,
     NEWLINE,
-    PERCENTAGE
+    PERCENTAGE,
+    CELLRANGE
 };
 
 }; }; }; };

oox/source/drawingml/chart/seriesconverter.cxx
@@ -249,6 +249,8 @@ DataPointCustomLabelFieldType lcl_ConvertFieldNameToFieldEnum( const OUString& r
         return DataPointCustomLabelFieldType::DataPointCustomLabelFieldType_CATEGORYNAME;
     else if (rField == "CELLREF")
         return DataPointCustomLabelFieldType::DataPointCustomLabelFieldType_CELLREF;
+    else if (rField == "CELLRANGE")
+        return DataPointCustomLabelFieldType::DataPointCustomLabelFieldType_CELLRANGE;
     else if (rField == "PERCENTAGE")
         return DataPointCustomLabelFieldType::DataPointCustomLabelFieldType_PERCENTAGE;
     else

oox/source/export/chartexport.cxx
@@ -3397,6 +3397,9 @@ OUString getFieldTypeString( const chart2::DataPointCustomLabelFieldType aType )
     case chart2::DataPointCustomLabelFieldType_CELLREF:
         return "CELLREF";
 
+    case chart2::DataPointCustomLabelFieldType_CELLRANGE:
+        return "CELLRANGE";
+
     default:
         break;
     }

chart2/qa/extras/chart2export.cxx
@@ -182,6 +182,7 @@ public:
     void testTdf136267();
     void testDataLabelPlacementPieChart();
     void testTdf137917();
+    void testTdf138204();
 
     CPPUNIT_TEST_SUITE(Chart2ExportTest);
     CPPUNIT_TEST(testErrorBarXLSX);
@@ -326,6 +327,7 @@ public:
     CPPUNIT_TEST(testTdf136267);
     CPPUNIT_TEST(testDataLabelPlacementPieChart);
     CPPUNIT_TEST(testTdf137917);
+    CPPUNIT_TEST(testTdf138204);
 
     CPPUNIT_TEST_SUITE_END();
 
@@ -2982,6 +2984,31 @@ void Chart2ExportTest::testTdf137917()
     assertXPath(pXmlDoc, "/c:chartSpace/c:chart/c:plotArea/c:dateAx/c:majorTimeUnit", "val", "months");
     assertXPath(pXmlDoc, "/c:chartSpace/c:chart/c:plotArea/c:dateAx/c:minorUnit", "val", "7");
     assertXPath(pXmlDoc, "/c:chartSpace/c:chart/c:plotArea/c:dateAx/c:minorTimeUnit", "val", "days");
+}
+
+void Chart2ExportTest::testTdf138204()
+{
+    load("/chart2/qa/extras/data/xlsx/", "tdf138204.xlsx");
+    xmlDocUniquePtr pXmlDoc = parseExport("xl/charts/chart", "Calc Office Open XML");
+    CPPUNIT_ASSERT(pXmlDoc);
+
+    // Check the first data label field type
+    assertXPath(pXmlDoc, "/c:chartSpace/c:chart/c:plotArea/c:barChart/c:ser[1]/c:dLbls/c:dLbl/c:tx/c:rich/a:p/a:fld", "type", "CELLRANGE");
+
+    Reference< chart2::XChartDocument> xChartDoc = getChartDocFromSheet(0, mxComponent);
+    CPPUNIT_ASSERT(xChartDoc.is());
+
+    uno::Reference<chart2::XDataSeries> xDataSeries(getDataSeriesFromDoc(xChartDoc, 1));
+    CPPUNIT_ASSERT(xDataSeries.is());
+
+    uno::Reference<beans::XPropertySet> xPropertySet;
+    uno::Sequence<uno::Reference<chart2::XDataPointCustomLabelField>> aFields;
+    xPropertySet.set(xDataSeries->getDataPointByIndex(0), uno::UNO_SET_THROW);
+    xPropertySet->getPropertyValue("CustomLabelFields") >>= aFields;
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(1), aFields.getLength());
+
+    CPPUNIT_ASSERT_EQUAL(chart2::DataPointCustomLabelFieldType::DataPointCustomLabelFieldType_CELLRANGE, aFields[0]->getFieldType());
+    //CPPUNIT_ASSERT_EQUAL(OUString("67.5%"), aFields[0]->getString()); TODO: Not implemented yet
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(Chart2ExportTest);