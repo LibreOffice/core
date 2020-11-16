b8e2731f7cd6eb83367c9c25886418f7c746d0fd
Tor Lillqvist <tml@collabora.com>
2020-11-16 19:29:04 +0000

    Make firebird build for macOS on arm64
    No idea whether it works.
    
    Change-Id: I008cf9fab56bedb2e1f33ad6a99c9cd95d7483a7

--- external/firebird/UnpackedTarball_firebird.mk
+++ external/firebird/UnpackedTarball_firebird.mk
@@ -36,6 +36,7 @@ $(eval $(call gb_UnpackedTarball_add_patches,firebird,\
 		external/firebird/asan.patch \
 		external/firebird/firebird-tdf125284.patch.1 \
 		external/firebird/0001-Make-comparison-operator-member-functions-const.patch.1 \
+		external/firebird/macos-arm64.patch.0 \
 ))
 
 ifeq ($(OS),WNT)
