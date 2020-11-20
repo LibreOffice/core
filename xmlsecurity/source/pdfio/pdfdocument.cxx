dd673692d698abaaa350ace500a12ea959d5936c
Tor Lillqvist <tml@collabora.com>
2020-11-20 17:47:49 +0200

    Avoid unused parameter warning in the !HAVE_FEATURE_PDFIUM case
    Change-Id: I218265f0bda7cdfcae523ea386edaec1a9143cef
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/106283
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Tor Lillqvist <tml@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/106458
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>

--- xmlsecurity/source/pdfio/pdfdocument.cxx
+++ xmlsecurity/source/pdfio/pdfdocument.cxx
@@ -252,6 +252,7 @@ bool IsValidSignature(SvStream& rStream, vcl::filter::PDFObjectElement* pSignatu
     return aSignedPages == aAllPages;
 #else
     (void)rStream;
+    (void)nMDPPerm;
     return true;
 #endif
 }
