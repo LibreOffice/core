bac71fd104a19e481ed688fba0b0f427e855ce91
Umut Emre Bayramoglu <umut265@gmail.com>
2020-11-21 15:43:02 +0000

    tdf#124176: Use pragma once instead of include guards
    Change-Id: I5f4f6ccdc8ce2ccfeb5046b7d477c29849696e39
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/106320
    Tested-by: Jenkins
    Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

--- sw/source/uibase/docvw/SidebarScrollBar.hxx
+++ sw/source/uibase/docvw/SidebarScrollBar.hxx
@@ -7,8 +7,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-#ifndef INCLUDED_SW_SOURCE_UIBASE_DOCVW_SIDEBARSCROLLBAR_HXX
-#define INCLUDED_SW_SOURCE_UIBASE_DOCVW_SIDEBARSCROLLBAR_HXX
+#pragma once
 
 #include <vcl/scrbar.hxx>
 
@@ -37,7 +36,5 @@ public:
     ~SidebarScrollBar() override;
 };
 }
-
-#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
