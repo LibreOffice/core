d767d0c968d243a891999df5685e177d8777fe7c
dante <dante19031999@gmail.com>
2020-11-21 01:14:00 +0100

    clang-formated rest of the mathml files on starmath
    Change-Id: I1aedf909f7371f259b122364368c157fd615ae06
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/106298
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

--- starmath/source/mathmlexport.hxx
+++ starmath/source/mathmlexport.hxx
@@ -26,90 +26,93 @@
 class SfxMedium;
 class SmNode;
 class SmVerticalBraceNode;
-namespace com::sun::star {
-    namespace io {
-        class XOutputStream; }
-    namespace beans {
-        class XPropertySet; }
+namespace com::sun::star
+{
+namespace io
+{
+class XOutputStream;
 }
-
+namespace beans
+{
+class XPropertySet;
+}
+}
 
 class SmXMLExportWrapper
 {
     css::uno::Reference<css::frame::XModel> xModel;
-    bool bFlat;     //set true for export to flat .mml, set false for
-                        //export to a .sxm (or whatever) package
+    bool bFlat; //set true for export to flat .mml, set false for
+        //export to a .sxm (or whatever) package
 public:
-    explicit SmXMLExportWrapper(css::uno::Reference<css::frame::XModel> const &rRef)
-        : xModel(rRef), bFlat(true) {}
+    explicit SmXMLExportWrapper(css::uno::Reference<css::frame::XModel> const& rRef)
+        : xModel(rRef)
+        , bFlat(true)
+    {
+    }
 
-    bool Export(SfxMedium &rMedium);
-    void SetFlat(bool bIn) {bFlat = bIn;}
+    bool Export(SfxMedium& rMedium);
+    void SetFlat(bool bIn) { bFlat = bIn; }
+
+    static bool
+    WriteThroughComponent(const css::uno::Reference<css::io::XOutputStream>& xOutputStream,
+                          const css::uno::Reference<css::lang::XComponent>& xComponent,
+                          css::uno::Reference<css::uno::XComponentContext> const& rxContext,
+                          css::uno::Reference<css::beans::XPropertySet> const& rPropSet,
+                          const char* pComponentName);
 
     static bool WriteThroughComponent(
-        const css::uno::Reference< css::io::XOutputStream >&   xOutputStream,
-        const css::uno::Reference< css::lang::XComponent >&    xComponent,
-        css::uno::Reference< css::uno::XComponentContext > const & rxContext,
-        css::uno::Reference< css::beans::XPropertySet > const & rPropSet,
-        const char* pComponentName );
-
-    static bool WriteThroughComponent(
-        const css::uno::Reference< css::embed::XStorage >& xStor,
-        const css::uno::Reference< css::lang::XComponent >& xComponent,
-        const char* pStreamName,
-        css::uno::Reference< css::uno::XComponentContext > const & rxContext,
-        css::uno::Reference< css::beans::XPropertySet > const & rPropSet,
-        const char* pComponentName );
+        const css::uno::Reference<css::embed::XStorage>& xStor,
+        const css::uno::Reference<css::lang::XComponent>& xComponent, const char* pStreamName,
+        css::uno::Reference<css::uno::XComponentContext> const& rxContext,
+        css::uno::Reference<css::beans::XPropertySet> const& rPropSet, const char* pComponentName);
 };
-
 
 class SmXMLExport final : public SvXMLExport
 {
-    const SmNode *  pTree;
-    OUString        aText;
-    bool        bSuccess;
+    const SmNode* pTree;
+    OUString aText;
+    bool bSuccess;
 
-    void ExportNodes(const SmNode *pNode, int nLevel);
-    void ExportTable(const SmNode *pNode, int nLevel);
-    void ExportLine(const SmNode *pNode, int nLevel);
-    void ExportExpression(const SmNode *pNode, int nLevel,
-                          bool bNoMrowContainer = false);
-    void ExportText(const SmNode *pNode);
-    void ExportMath(const SmNode *pNode);
-    void ExportBinaryHorizontal(const SmNode *pNode, int nLevel);
-    void ExportUnaryHorizontal(const SmNode *pNode, int nLevel);
-    void ExportBrace(const SmNode *pNode, int nLevel);
-    void ExportBinaryVertical(const SmNode *pNode, int nLevel);
-    void ExportBinaryDiagonal(const SmNode *pNode, int nLevel);
-    void ExportSubSupScript(const SmNode *pNode, int nLevel);
-    void ExportRoot(const SmNode *pNode, int nLevel);
-    void ExportOperator(const SmNode *pNode, int nLevel);
-    void ExportAttributes(const SmNode *pNode, int nLevel);
-    void ExportFont(const SmNode *pNode, int nLevel);
-    void ExportVerticalBrace(const SmVerticalBraceNode *pNode, int nLevel);
-    void ExportMatrix(const SmNode *pNode, int nLevel);
-    void ExportBlank(const SmNode *pNode);
+    void ExportNodes(const SmNode* pNode, int nLevel);
+    void ExportTable(const SmNode* pNode, int nLevel);
+    void ExportLine(const SmNode* pNode, int nLevel);
+    void ExportExpression(const SmNode* pNode, int nLevel, bool bNoMrowContainer = false);
+    void ExportText(const SmNode* pNode);
+    void ExportMath(const SmNode* pNode);
+    void ExportBinaryHorizontal(const SmNode* pNode, int nLevel);
+    void ExportUnaryHorizontal(const SmNode* pNode, int nLevel);
+    void ExportBrace(const SmNode* pNode, int nLevel);
+    void ExportBinaryVertical(const SmNode* pNode, int nLevel);
+    void ExportBinaryDiagonal(const SmNode* pNode, int nLevel);
+    void ExportSubSupScript(const SmNode* pNode, int nLevel);
+    void ExportRoot(const SmNode* pNode, int nLevel);
+    void ExportOperator(const SmNode* pNode, int nLevel);
+    void ExportAttributes(const SmNode* pNode, int nLevel);
+    void ExportFont(const SmNode* pNode, int nLevel);
+    void ExportVerticalBrace(const SmVerticalBraceNode* pNode, int nLevel);
+    void ExportMatrix(const SmNode* pNode, int nLevel);
+    void ExportBlank(const SmNode* pNode);
 
 public:
-    SmXMLExport(
-        const css::uno::Reference< css::uno::XComponentContext >& rContext,
-        OUString const & implementationName, SvXMLExportFlags nExportFlags);
+    SmXMLExport(const css::uno::Reference<css::uno::XComponentContext>& rContext,
+                OUString const& implementationName, SvXMLExportFlags nExportFlags);
 
     // XUnoTunnel
-    sal_Int64 SAL_CALL getSomething( const css::uno::Sequence< sal_Int8 >& rId ) override;
-    static const css::uno::Sequence< sal_Int8 > & getUnoTunnelId() throw();
+    sal_Int64 SAL_CALL getSomething(const css::uno::Sequence<sal_Int8>& rId) override;
+    static const css::uno::Sequence<sal_Int8>& getUnoTunnelId() throw();
 
     void ExportAutoStyles_() override {}
     void ExportMasterStyles_() override {}
     void ExportContent_() override;
-    ErrCode exportDoc(enum ::xmloff::token::XMLTokenEnum eClass = ::xmloff::token::XML_TOKEN_INVALID) override;
+    ErrCode exportDoc(enum ::xmloff::token::XMLTokenEnum eClass
+                      = ::xmloff::token::XML_TOKEN_INVALID) override;
 
     virtual void GetViewSettings(css::uno::Sequence<css::beans::PropertyValue>& aProps) override;
-    virtual void GetConfigurationSettings(css::uno::Sequence<css::beans::PropertyValue>& aProps) override;
+    virtual void
+    GetConfigurationSettings(css::uno::Sequence<css::beans::PropertyValue>& aProps) override;
 
-    bool GetSuccess() const {return bSuccess;}
+    bool GetSuccess() const { return bSuccess; }
 };
-
 
 #endif
 
