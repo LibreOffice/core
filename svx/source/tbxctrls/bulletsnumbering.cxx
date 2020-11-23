e2ff7176cbad3a406c9572201973e644bb770018
Caolán McNamara <caolanm@redhat.com>
2020-11-23 16:40:23 +0000

    tdf#138430 toolbar bullet dropdown should have a scrolledwindow
    as should the matching dialog pages
    
    Change-Id: Ib327d3c02d1bc5ae11a6a76d52c9e17803f05534

--- svx/source/tbxctrls/bulletsnumbering.cxx
+++ svx/source/tbxctrls/bulletsnumbering.cxx
@@ -68,7 +68,7 @@ NumberingPopup::NumberingPopup(NumberingToolBoxControl& rController,
     : WeldToolbarPopup(rController.getFrameInterface(), pParent, "svx/ui/numberingwindow.ui", "NumberingWindow")
     , mePageType(ePageType)
     , mrController(rController)
-    , mxValueSet(new SvxNumValueSet(nullptr))
+    , mxValueSet(new SvxNumValueSet(m_xBuilder->weld_scrolled_window("valuesetwin")))
     , mxValueSetWin(new weld::CustomWeld(*m_xBuilder, "valueset", *mxValueSet))
     , mxMoreButton(m_xBuilder->weld_button("more"))
 {
