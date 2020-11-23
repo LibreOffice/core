6a905223b6caefeace9c5037974462e0b94546f4
Bayram Çiçek <mail@bayramcicek.com.tr>
2020-11-23 19:17:44 +0000

    tdf#124176: Use pragma once instead of include guards
    Change-Id: I49acd246a9675a0749e056178dec43d0d73c488e
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/106450
    Tested-by: Jenkins
    Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

--- sw/source/core/inc/unobookmark.hxx
+++ sw/source/core/inc/unobookmark.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_SW_SOURCE_CORE_INC_UNOBOOKMARK_HXX
-#define INCLUDED_SW_SOURCE_CORE_INC_UNOBOOKMARK_HXX
+#pragma once
 
 #include <com/sun/star/lang/XUnoTunnel.hpp>
 #include <com/sun/star/lang/XServiceInfo.hpp>
@@ -240,7 +239,5 @@ public:
     virtual css::uno::Reference<css::container::XNameContainer> SAL_CALL getParameters() override;
 
 };
-
-#endif // INCLUDED_SW_SOURCE_CORE_INC_UNOBOOKMARK_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
