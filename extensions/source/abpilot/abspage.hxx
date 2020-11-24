622a2631932a05b9231fffa813562cf2b69e5e8f
ShyamPraveenSingh <shyampraveensingh@gmail.com>
2020-11-24 09:26:32 +0530

    tdf#124176 Added #pragma once to 13 files
    Change-Id: Ia370a447a23d6d78c380667fd07ba5f230c927dc

--- extensions/source/abpilot/abspage.hxx
+++ extensions/source/abpilot/abspage.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_EXTENSIONS_SOURCE_ABPILOT_ABSPAGE_HXX
-#define INCLUDED_EXTENSIONS_SOURCE_ABPILOT_ABSPAGE_HXX
+#pragma once
 
 #include <vcl/wizardmachine.hxx>
 #include <com/sun/star/lang/XMultiServiceFactory.hpp>
