843aedb772223189aa2ba3b1e9ffb7dba86db1ac
Gül Toksöz <meryemgltksz@gmail.com>
2020-11-25 18:33:08 +0000

    tdf#124176: Use pragma once instead of include guards
    Change-Id: I334fee9257c83aa940b256b049d4408ab2e53a72
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/106609
    Reviewed-by: Julien Nabet <serval2412@yahoo.fr>
    Tested-by: Jenkins

--- sw/source/uibase/inc/optpage.hxx
+++ sw/source/uibase/inc/optpage.hxx
@@ -16,8 +16,9 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
-#ifndef INCLUDED_SW_SOURCE_UIBASE_INC_OPTPAGE_HXX
-#define INCLUDED_SW_SOURCE_UIBASE_INC_OPTPAGE_HXX
+
+#pragma once
+
 #include <sfx2/tabdlg.hxx>
 
 #include <vcl/weld.hxx>
@@ -382,5 +383,4 @@ public:
     virtual void Reset( const SfxItemSet* rSet ) override;
 };
 
-#endif
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
--- sw/source/uibase/inc/unomod.hxx
+++ sw/source/uibase/inc/unomod.hxx
@@ -16,8 +16,8 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
-#ifndef INCLUDED_SW_SOURCE_UIBASE_INC_UNOMOD_HXX
-#define INCLUDED_SW_SOURCE_UIBASE_INC_UNOMOD_HXX
+
+#pragma once
 
 #include <com/sun/star/beans/XPropertySet.hpp>
 #include <com/sun/star/view/XPrintSettingsSupplier.hpp>
@@ -132,7 +132,5 @@ public:
     bool    IsValid() const {return bObjectValid;}
     void    Invalidate() {bObjectValid = false;}
 };
-
-#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
