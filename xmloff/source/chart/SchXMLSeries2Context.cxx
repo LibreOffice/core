LibreOffice/core

commit cfddbf52ea6aed12b0c69a151d3caf4b5732a263

Fix typo
Change-Id: I9bc856d4d7370f2a5fa09001d5b8b933bc72e96f
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/102095
Tested-by: Jenkins
Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

xmloff/source/chart/SchXMLSeries2Context.cxx
@@ -828,7 +828,7 @@ const std::pair<OUString, OUString> aApiToLabelFooPairs[]
     = { { "LineStyle", "LabelBorderStyle" },
         { "LineWidth", "LabelBorderWidth" },
         { "LineColor", "LabelBorderColor" },
-        // The name "LaberBorderDash" ist defined, but the associated API name "LineDash" belongs to
+        // TThe name "LabelBorderDash" is defined, but the associated API name "LineDash" belongs to
         // the <draw:stroke-dash> element and is not used directly as line property.
         //{"LineDash", "LabelBorderDash"},
         { "LineDashName", "LabelBorderDashName" },

commit d99f248e3ea716f48e37543796040e924e98c7b8

Fix typo in comment
...introduced with cfddbf52ea6aed12b0c69a151d3caf4b5732a263 "Fix typo"

Change-Id: Id3888f0216a7702d7f09d25d069d50f64859eff9

xmloff/source/chart/SchXMLSeries2Context.cxx
@@ -828,7 +828,7 @@ const std::pair<OUString, OUString> aApiToLabelFooPairs[]
     = { { "LineStyle", "LabelBorderStyle" },
         { "LineWidth", "LabelBorderWidth" },
         { "LineColor", "LabelBorderColor" },
-        // TThe name "LabelBorderDash" is defined, but the associated API name "LineDash" belongs to
+        // The name "LabelBorderDash" is defined, but the associated API name "LineDash" belongs to
         // the <draw:stroke-dash> element and is not used directly as line property.
         //{"LineDash", "LabelBorderDash"},
         { "LineDashName", "LabelBorderDashName" },