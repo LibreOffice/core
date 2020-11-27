a99897ad210b04e2039a3aafb3edc7a733f43ce4
Miklos Vajna <vmiklos@collabora.com>
2020-11-27 16:58:38 +0100

    oox: make effectpropertiescontext available to everyone in oox/
    I plan to use this from oox/source/drawingml/table/tablecontext.cxx for
    tdf#129961.
    
    (cherry picked from commit f2e1685e489c054ea10afc0df52451c74da10cc0)
    
    Conflicts:
    	solenv/clang-format/excludelist
    
    Change-Id: Id5177467341f9ddf73c6043796ad35e9cc655d37
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/107592
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

--- oox/source/drawingml/effectpropertiescontext.cxx
+++ oox/source/drawingml/effectpropertiescontext.cxx
@@ -7,7 +7,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-#include "effectpropertiescontext.hxx"
+#include <drawingml/effectpropertiescontext.hxx>
 #include "effectproperties.hxx"
 #include <oox/drawingml/drawingmltypes.hxx>
 #include <drawingml/misccontexts.hxx>
