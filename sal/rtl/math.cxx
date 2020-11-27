cfd486110dce62f08742fdf707bcfc6a34ed704f
Eike Rathke <erack@redhat.com>
2020-11-27 13:26:26 +0100

    Consistently use RTL_CONSTASCII_LENGTH(), tdf#136272 follow-up
    The mix with SAL_N_ELEMENTS() was confusing and even wrong in one
    case.
    
    Change-Id: Ife73342b0efc01ed4e76e217d372fc32500c9b2c

--- sal/rtl/math.cxx
+++ sal/rtl/math.cxx
@@ -292,7 +292,7 @@ void doubleToString(typename T::String ** pResult,
         constexpr char pDig[] = "7976931348623157";
         constexpr char pRou[] = "8087931459623267";     // the only up-carry is 80
         static_assert(SAL_N_ELEMENTS(pDig) == SAL_N_ELEMENTS(pRou), "digit count mismatch");
-        constexpr sal_Int32 nDig2 = SAL_N_ELEMENTS(pRou) - 2;
+        constexpr sal_Int32 nDig2 = RTL_CONSTASCII_LENGTH(pRou) - 2;
         sal_Int32 nCapacity = RTL_CONSTASCII_LENGTH(pRou) + 8;  // + "-1.E+308"
         const char pSlot[5][2][3] =
         { // rounded, not
@@ -314,7 +314,7 @@ void doubleToString(typename T::String ** pResult,
             T::appendAscii(pResult, pResultCapacity, &nResultOffset,
                            RTL_CONSTASCII_STRINGPARAM("-"));
 
-        nDecPlaces = std::clamp<sal_Int32>( nDecPlaces, 0, SAL_N_ELEMENTS(pRou));
+        nDecPlaces = std::clamp<sal_Int32>( nDecPlaces, 0, RTL_CONSTASCII_LENGTH(pRou));
         if (nDecPlaces == 0)
         {
             T::appendAscii(pResult, pResultCapacity, &nResultOffset,
@@ -342,7 +342,7 @@ void doubleToString(typename T::String ** pResult,
                 const size_t nSlot = ((fValue < fB3) ? 4 : ((fValue < fB2) ? 3
                             : ((fValue < fB1) ? 2 : ((fValue < DBL_MAX) ? 1 : 0))));
 
-                T::appendAscii(pResult, pResultCapacity, &nResultOffset, pDig, nDecPlaces - 1);
+                T::appendAscii(pResult, pResultCapacity, &nResultOffset, pDig, nDecPlaces);
                 T::appendAscii(pResult, pResultCapacity, &nResultOffset, pSlot[nSlot][nDec-1], nDec);
             }
         }
