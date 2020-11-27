a84da4df9f38fb0b45f15c7d18730867a9342d57
Bartu Bayazıt <bartubayazit81@gmail.com>
2020-11-27 20:41:49 +0000

    tdf#124176: Use pragma once instead of include guards
    Change-Id: I2f583dd1c0a6b8a546d83bbf852eb0ef5492c374

--- sw/source/core/text/txtcache.hxx
+++ sw/source/core/text/txtcache.hxx
@@ -16,8 +16,8 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
-#ifndef INCLUDED_SW_SOURCE_CORE_TEXT_TXTCACHE_HXX
-#define INCLUDED_SW_SOURCE_CORE_TEXT_TXTCACHE_HXX
+
+#pragma once
 
 #include <swcache.hxx>
 #include "porlay.hxx"
@@ -58,7 +58,5 @@ public:
 
     bool IsAvailable() const;
 };
-
-#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
