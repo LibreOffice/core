8658ba1db1cd3fd52991ce149c80dc7db7a8f4c2
zeynepsninkaya <zeynsemn@gmail.com>
2020-11-28 09:42:13 +0000

    tdf#124176: Use pragma once instead of include guards
    Change-Id: I0531950697f96c208eadc64732a200e27fd94741

--- sw/source/uibase/table/tablepg.hxx
+++ sw/source/uibase/table/tablepg.hxx
@@ -16,7 +16,6 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
-
 #pragma once
 
 #include <sfx2/tabdlg.hxx>
@@ -182,7 +181,5 @@ public:
 
     void                DisablePageBreak();
 };
-
-
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
