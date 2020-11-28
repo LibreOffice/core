ed96a8339c59d09905c58481fae3e6558968b03d
Ahmet Bera Kansu <abkansu@gmail.com>
2020-11-28 01:47:00 +0000

    tdf#124176: Use pragma once instead of include guards
    Change-Id: Iaa72b6daf92f3a7f5b862a425a2724cfa99f5549

--- sw/source/uibase/inc/workctrl.hxx
+++ sw/source/uibase/inc/workctrl.hxx
@@ -16,8 +16,8 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
-#ifndef INCLUDED_SW_SOURCE_UIBASE_INC_WORKCTRL_HXX
-#define INCLUDED_SW_SOURCE_UIBASE_INC_WORKCTRL_HXX
+
+#pragma once
 
 #include <sfx2/tbxctrl.hxx>
 #include <vcl/toolbox.hxx>
@@ -89,6 +89,5 @@ public:
 
     virtual VclPtr<InterimItemWindow> CreateItemWindow( vcl::Window *pParent ) override;
 };
-#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
