3aaff1083a60010ad31342e029ce6e9b8ec62f9c
zeynepsninkaya <zeynsemn@gmail.com>
2020-11-28 09:42:13 +0000

    tdf#124176: Use pragma once instead of include guards
    Change-Id: I0531950697f96c208eadc64732a200e27fd94741
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/106801
    Reviewed-by: Julien Nabet <serval2412@yahoo.fr>
    Tested-by: Jenkins

--- sw/source/uibase/inc/selglos.hxx
+++ sw/source/uibase/inc/selglos.hxx
@@ -16,8 +16,8 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
-#ifndef INCLUDED_SW_SOURCE_UIBASE_INC_SELGLOS_HXX
-#define INCLUDED_SW_SOURCE_UIBASE_INC_SELGLOS_HXX
+
+#pragma once
 
 #include <vcl/weld.hxx>
 
@@ -45,7 +45,5 @@ public:
         m_xGlosBox->select(nIdx);
     }
 };
-
-#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
