e3e68283a0a3457df3501428f8c1310077a7d9b8
László Németh <nemeth@numbertext.org>
2020-12-01 19:45:22 +0100

    tdf#107870 sw: don't jump to cursor setting Record changes
    Change-Id: I890e845ff94b636dd6edae19969d913e8d7a7b65

--- include/sfx2/objsh.hxx
+++ include/sfx2/objsh.hxx
@@ -671,7 +671,7 @@ public:
     // slots used for Calc:         FID_CHG_RECORD, SID_CHG_PROTECT
     virtual bool    IsChangeRecording() const;
     virtual bool    HasChangeRecordProtection() const;
-    virtual void    SetChangeRecording( bool bActivate );
+    virtual void    SetChangeRecording( bool bActivate, bool bLockAllViews = false );
     virtual void    SetProtectionPassword( const OUString &rPassword );
     virtual bool    GetProtectionHash( /*out*/ css::uno::Sequence< sal_Int8 > &rPasswordHash );
 
