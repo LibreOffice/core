071e5a422b6a795fd396b378bebc9cd8cc02819c
Caolán McNamara <caolanm@redhat.com>
2020-12-01 10:17:18 +0000

    cid#1202935 Uninitialized scalar field
    Change-Id: Ie95426e6188253dd5f2bd77782f62647d9ddba50
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/106956
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    (cherry picked from commit ad53629186343f8a111b4bee21721e8b446bd8a2)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/118582
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

--- vcl/source/gdi/print.cxx
+++ vcl/source/gdi/print.cxx
@@ -459,6 +459,7 @@ void Printer::ImplInitData()
     mbPrintFile         = false;
     mbInPrintPage       = false;
     mbNewJobSetup       = false;
+    mbSinglePrintJobs   = false;
     mpInfoPrinter       = nullptr;
     mpPrinter           = nullptr;
     mpDisplayDev        = nullptr;
