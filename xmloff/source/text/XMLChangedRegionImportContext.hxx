380e3fdc8a0ca86429ab44669820f09dad1a6fb8
Noel <noelgrandin@gmail.com>
2020-12-01 10:24:14 +0200

    fastparser in XMLChangedRegionImportContext
    Change-Id: I71d31fc2f21d1f261b1e5d8d923269ae0531ccb9
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/106966
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

--- xmloff/source/text/XMLChangedRegionImportContext.hxx
+++ xmloff/source/text/XMLChangedRegionImportContext.hxx
@@ -57,8 +57,9 @@ public:
 
     virtual ~XMLChangedRegionImportContext() override;
 
-    virtual void StartElement(
-        const css::uno::Reference<css::xml::sax::XAttributeList> & xAttrList) override;
+    virtual void SAL_CALL startFastElement(
+        sal_Int32 nElement,
+        const css::uno::Reference< css::xml::sax::XFastAttributeList >& xAttrList ) override;
 
     virtual css::uno::Reference< css::xml::sax::XFastContextHandler > SAL_CALL createFastChildContext(
         sal_Int32 nElement, const css::uno::Reference< css::xml::sax::XFastAttributeList >& AttrList ) override;
