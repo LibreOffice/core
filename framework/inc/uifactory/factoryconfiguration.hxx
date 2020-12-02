1d3d958e0089429e05265cfd13dc540fce5887d1
cu-16bcs1798 <cu.16bcs1798@gmail.com>
2020-12-03 01:34:39 +0530

    tdf#124176 Use #pragma once in framework/inc/uifactory
    Change-Id: Ibb2d7c8a8fac7fabace2d751f0082533ce2c1f50
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/107132
    Tested-by: Jenkins
    Reviewed-by: Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>

--- framework/inc/uifactory/factoryconfiguration.hxx
+++ framework/inc/uifactory/factoryconfiguration.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_FRAMEWORK_INC_UIFACTORY_FACTORYCONFIGURATION_HXX
-#define INCLUDED_FRAMEWORK_INC_UIFACTORY_FACTORYCONFIGURATION_HXX
+#pragma once
 
 #include <com/sun/star/container/XContainerListener.hpp>
 #include <com/sun/star/container/XNameAccess.hpp>
@@ -88,6 +87,5 @@ private:
 };
 
 } // namespace framework
-#endif // INCLUDED_FRAMEWORK_INC_UIFACTORY_FACTORYCONFIGURATION_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
