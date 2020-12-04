33bace0defd5528a78d31abad58d90af7f026681
Attila Bakos (NISZ) <bakos.attilakaroly@nisz.hu>
2020-12-04 13:42:09 +0100

    tdf#137819 SW: Fix As-Char Anchor of TextBoxes
    This is a so quick patch, like fire extinguising,
    without unitest. Steps to check:
    1 Open Witer 7.2
    2 Add a shape (a rectangle for exmaple) to the doc
    3 Add a textbox to the shape
    4 Change the anchor to as-char
    
    Before: Separation
    Now: It stays together
    Change-Id: Iea0abe6f12a293fb4270970fde53ea903b347fb2

--- sw/source/core/doc/docfly.cxx
+++ sw/source/core/doc/docfly.cxx
@@ -61,6 +61,7 @@
 #include <svx/xlnstit.hxx>
 #include <svx/xlnedit.hxx>
 #include <svx/xflhtit.hxx>
+#include <unomid.h>
 
 using namespace ::com::sun::star;
 
@@ -918,6 +919,14 @@ bool SwDoc::ChgAnchor( const SdrMarkList& _rMrkList,
 
                     SwFormatFlyCnt aFormat( pContact->GetFormat() );
                     pNd->InsertItem( aFormat, aPos.nContent.GetIndex(), 0 );
+
+                    // Has a textbox attached to the format? Sync it as well!
+                    if (SwTextBoxHelper::getOtherTextBoxFormat(pContact->GetFormat(),
+                                                               RES_DRAWFRMFMT))
+                    {
+                        SwTextBoxHelper::syncFlyFrameAttr(*pContact->GetFormat(),
+                                                          pContact->GetFormat()->GetAttrSet());
+                    }
                 }
                 break;
             default:
