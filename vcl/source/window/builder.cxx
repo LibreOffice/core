06a736d3bce110bb3f980ef2d29fc8b66e4bc203
Henry Castro <hcastro@collabora.com>
2020-12-04 16:32:30 -0400

    lok: use JSDialog Builder to create a Macro Selector Dialog
    The Macro Selector Dialog should be created for desktop too.
    The JS Builder has an implementation to send "jsdialog: "
    data to the client side.
    
    Change-Id: If29660467e494323ac26605e704ee9bf15725fd5
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/107243
    Tested-by: Jenkins
    Reviewed-by: Henry Castro <hcastro@collabora.com>

--- vcl/source/window/builder.cxx
+++ vcl/source/window/builder.cxx
@@ -190,7 +190,8 @@ weld::Builder* Application::CreateBuilder(weld::Widget* pParent, const OUString 
             || rUIFile == "modules/scalc/ui/datafielddialog.ui"
             || rUIFile == "modules/scalc/ui/pivotfielddialog.ui"
             || rUIFile == "modules/scalc/ui/datafieldoptionsdialog.ui"
-            || rUIFile == "svx/ui/fontworkgallerydialog.ui")
+            || rUIFile == "svx/ui/fontworkgallerydialog.ui"
+            || rUIFile == "cui/ui/macroselectordialog.ui")
         {
             bUseJSBuilder = true;
         }
