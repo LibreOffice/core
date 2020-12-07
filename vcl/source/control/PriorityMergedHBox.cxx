2b7a38bbf689e9872c0e739936e43953f105c01d
Caolán McNamara <caolanm@redhat.com>
2020-12-07 11:35:32 +0000

    move Button to vcl/toolkit
    Change-Id: I11e4fbab2e70342683febacf444fbe08dabf2417
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/107346
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>

--- vcl/source/control/PriorityMergedHBox.cxx
+++ vcl/source/control/PriorityMergedHBox.cxx
@@ -17,7 +17,7 @@
 *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
 */
 
-#include <vcl/button.hxx>
+#include <vcl/toolkit/button.hxx>
 #include <vcl/layout.hxx>
 #include <bitmaps.hlst>
 #include <PriorityHBox.hxx>
