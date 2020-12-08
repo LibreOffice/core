2e1e8566d48e805d4b85e349655da9d6aa5ccd33
Bjoern Michaelsen <bjoern.michaelsen@libreoffice.org>
2020-12-08 23:02:25 +0100

    WIP: Modify no more
    Change-Id: I7ebb18c8e034d33fad6f5e18cd91b8a3cb9b259a

--- sw/source/core/inc/flyfrms.hxx
+++ sw/source/core/inc/flyfrms.hxx
@@ -152,7 +152,7 @@ public:
     SwFlyLayFrame( SwFlyFrameFormat*, SwFrame*, SwFrame *pAnchor );
 
 protected:
-    virtual void Modify( const SfxPoolItem*, const SfxPoolItem* ) override;
+    virtual void SwClientNotify(const SwModify&, const SfxHint&) override;
 };
 
 // Flys that are bound to Content but not in Content
