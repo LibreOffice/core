LibreOffice/core

commit b5fdb148fe87a90f3e586efcea7aa6ef865fa42a

tdf#124654 Don't reopen closed sidebar decks
NotifyResize was called after a deck was closed,
so this is the wrong place to ensure minimum sidebar width.

Change-Id: I5a03dc1ee24257d49673db58bffbda1a7358cf3a
Reviewed-on: https://gerrit.libreoffice.org/70597
Tested-by: Jenkins
Reviewed-by: Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>

sfx2/source/sidebar/SidebarController.cxx
@@ -553,6 +553,13 @@ void SidebarController::OpenThenToggleDeck (
     }
     RequestOpenDeck();
     SwitchToDeck(rsDeckId);
+
+    // Make sure the sidebar is wide enough to fit the requested content
+    sal_Int32 nRequestedWidth = (mpCurrentDeck->GetMinimalWidth() + TabBar::GetDefaultWidth())
+                                * mpTabBar->GetDPIScaleFactor();
+    if (mnSavedSidebarWidth < nRequestedWidth)
+        SetChildWindowWidth(nRequestedWidth);
+
     mpTabBar->Invalidate();
     mpTabBar->HighlightDeck(rsDeckId);
     collectUIInformation(rsDeckId);
@@ -1222,10 +1229,6 @@ void SidebarController::RestrictWidth (sal_Int32 nWidth)
         const sal_uInt16 nSetId (pSplitWindow->GetSet(nId));
         const sal_Int32 nRequestedWidth
             = (TabBar::GetDefaultWidth() + nWidth) * mpTabBar->GetDPIScaleFactor();
-
-        // Make sure the sidebar is wide enough to fit the requested content
-        if (pSplitWindow->GetSizePixel().Width() < nRequestedWidth)
-            SetChildWindowWidth(nRequestedWidth);
 
         pSplitWindow->SetItemSizeRange(
             nSetId,

commit 51c2dc351730319546756c9456f0de2ca29a54d4

tdf#138762: crash when trying to open sidebar w chart in Writer
In Writer when in chart edit mode, and the chart is deselected,
the object selection remains. Opening Properties sidebar at
this point crashed. Calc and Impress were unaffected.

Regression from b5fdb148fe87a90f3e586efcea7aa6ef865fa42a

Change-Id: I356c3a42c41dd38e4739df27c72c6d67722b1dbc

sfx2/source/sidebar/SidebarController.cxx
@@ -628,10 +628,13 @@ void SidebarController::OpenThenToggleDeck (
     SwitchToDeck(rsDeckId);
 
     // Make sure the sidebar is wide enough to fit the requested content
-    sal_Int32 nRequestedWidth = (mpCurrentDeck->GetMinimalWidth() + TabBar::GetDefaultWidth())
-                                * mpTabBar->GetDPIScaleFactor();
-    if (mnSavedSidebarWidth < nRequestedWidth)
-        SetChildWindowWidth(nRequestedWidth);
+    if (mpCurrentDeck && mpTabBar)
+    {
+        sal_Int32 nRequestedWidth = (mpCurrentDeck->GetMinimalWidth() + TabBar::GetDefaultWidth())
+                                    * mpTabBar->GetDPIScaleFactor();
+        if (mnSavedSidebarWidth < nRequestedWidth)
+            SetChildWindowWidth(nRequestedWidth);
+    }
 
     collectUIInformation(rsDeckId);
 }