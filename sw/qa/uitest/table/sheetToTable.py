LibreOffice/core

commit 0c3ac02d8a3c7ea50ae262daf134c28df5c8b343

tdf#124646 Don't paste hidden rows of Calc sheets into Writer tables
The fix for tdf#37223 creates native text tables
from the RTF clipboard data to insert them cell
by cell in Writer tables instead of OLE embedding.
Unfortunately, using Calc RTF clipboard data inserts
also the cells of the hidden rows (tdf#118676).
This improvement uses the HTML clipboard data
to paste only the visible rows.

Note: first the original patch 1) converted the clipboard
data to a temporary nested text table at the cursor position,
and 2) copied its already native table content to the
clipboard, and inserts it into the original target table.
This doesn't work with the HTML format (that inserts plain
text data within a text table cell instead of a nested
table). As a workaround, the patch removes the target text
table with the insertion point temporarily, creating the
native text table from the HTML clipboard data at the place
of the removed text table. After copying the converted
native text table data to the clipboard, the removed
table is restored, and the native clipboard data is inserted
at the original cursor position.

Change-Id: Iee5bb4027729ca1eab373029f176eeadc59388a8
Reviewed-on: https://gerrit.libreoffice.org/70425
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/source/uibase/dochdl/swdtflvr.cxx
@@ -1280,12 +1280,14 @@ bool SwTransferable::Paste(SwWrtShell& rSh, TransferableDataHelper& rData, RndSt
                                     &nActionFlags );
     }
 
-    bool bInsertOleTable = ( EXCHG_OUT_ACTION_INSERT_OLE == nAction && ( rData.HasFormat( SotClipboardFormatId::SYLK ) ||
-                  rData.HasFormat( SotClipboardFormatId::SYLK_BIGCAPS ) ) );
-
-    // content of 1-cell tables is inserted as simple text
-    if (bInsertOleTable)
+    // tdf#37223 avoid OLE insertion of worksheets in the following cases:
+    // content of 1-cell worksheets are inserted as simple text using RTF format,
+    // bigger worksheets within native (Writer) table cells are inserted as native tables,
+    // ie. cell by cell instead of embedding the worksheet in a single cell of the Writer table
+    if ( EXCHG_OUT_ACTION_INSERT_OLE == nAction && ( rData.HasFormat( SotClipboardFormatId::SYLK ) ||
+                  rData.HasFormat( SotClipboardFormatId::SYLK_BIGCAPS ) ) )
     {
+        // is it a 1-cell worksheet?
         OUString aExpand;
         if( rData.GetString( SotClipboardFormatId::STRING, aExpand ))
         {
@@ -1298,13 +1300,42 @@ bool SwTransferable::Paste(SwWrtShell& rSh, TransferableDataHelper& rData, RndSt
                     bSingleCellTable = true;
             }
         }
+
+        // convert the worksheet to a temporary native table using HTML format, and copy that into the original native table
+        if (!bSingleCellTable && rData.HasFormat( SotClipboardFormatId::HTML ) &&
+                        rSh.GetDoc()->IsIdxInTable(rSh.GetCursor()->GetNode()) != nullptr && rSh.DoesUndo())
+        {
+            SfxDispatcher* pDispatch = rSh.GetView().GetViewFrame()->GetDispatcher();
+            sal_uInt32 nLevel = 0;
+            // within Writer table cells, inserting worksheets using HTML format results only plain text, not a native table,
+            // so remove all outer nested tables temporary to get a working insertion point
+            // (RTF format has no such problem, but that inserts the hidden rows of the original Calc worksheet, too)
+            do
+            {
+                // insert a random character to redo the place of the insertion at the end
+                pDispatch->Execute(FN_INSERT_NNBSP, SfxCallMode::SYNCHRON);
+                pDispatch->Execute(FN_TABLE_DELETE_TABLE, SfxCallMode::SYNCHRON);
+                nLevel++;
+            } while (rSh.GetDoc()->IsIdxInTable(rSh.GetCursor()->GetNode()) != nullptr);
+            if ( SwTransferable::PasteData( rData, rSh, EXCHG_OUT_ACTION_INSERT_STRING, nActionFlags, SotClipboardFormatId::HTML,
+                                        nDestination, false, false, nullptr, 0, false, nAnchorType, &aPasteContext ))
+            {
+                pDispatch->Execute(FN_CHAR_LEFT, SfxCallMode::SYNCHRON);
+                pDispatch->Execute(FN_TABLE_SELECT_ALL, SfxCallMode::SYNCHRON);
+                pDispatch->Execute(SID_COPY, SfxCallMode::SYNCHRON);
+                for(sal_uInt32 a = 0; a < 1 + (nLevel * 2); a++)
+                    pDispatch->Execute(SID_UNDO, SfxCallMode::SYNCHRON);
+                pDispatch->Execute(SID_PASTE, SfxCallMode::SYNCHRON);
+                return true;
+            } else {
+                for(sal_uInt32 a = 0; a < (nLevel * 2); a++)
+                    pDispatch->Execute(SID_UNDO, SfxCallMode::SYNCHRON);
+            }
+        }
     }
 
-    bool bInsertOleTableInTable = (bInsertOleTable && !bSingleCellTable &&
-            (rSh.GetDoc()->IsIdxInTable(rSh.GetCursor()->GetNode()) != nullptr));
-
     // special case for tables from draw application or 1-cell tables
-    if( EXCHG_OUT_ACTION_INSERT_DRAWOBJ == nAction || bSingleCellTable || bInsertOleTableInTable )
+    if( EXCHG_OUT_ACTION_INSERT_DRAWOBJ == nAction || bSingleCellTable )
     {
         if( rData.HasFormat( SotClipboardFormatId::RTF ) )
         {
@@ -1316,26 +1347,6 @@ bool SwTransferable::Paste(SwWrtShell& rSh, TransferableDataHelper& rData, RndSt
             nAction = EXCHG_OUT_ACTION_INSERT_STRING;
             nFormat = SotClipboardFormatId::RICHTEXT;
         }
-    }
-
-    // tdf#37223 insert OLE table in text tables as a native text table
-    // (first as an RTF nested table, and cut and paste that to get a
-    // native table insertion, removing also the temporary nested table)
-    // TODO set a working view lock to avoid of showing the temporary nested table for a moment
-    if (bInsertOleTableInTable && EXCHG_OUT_ACTION_INSERT_STRING == nAction)
-    {
-        bool bPasted = SwTransferable::PasteData( rData, rSh, nAction, nActionFlags, nFormat,
-                                        nDestination, false, false, nullptr, 0, false, nAnchorType, &aPasteContext );
-        if (bPasted && rSh.DoesUndo())
-        {
-            SfxDispatcher* pDispatch = rSh.GetView().GetViewFrame()->GetDispatcher();
-            pDispatch->Execute(FN_PREV_TABLE, SfxCallMode::SYNCHRON);
-            pDispatch->Execute(FN_TABLE_SELECT_ALL, SfxCallMode::SYNCHRON);
-            pDispatch->Execute(SID_COPY, SfxCallMode::SYNCHRON);
-            pDispatch->Execute(SID_UNDO, SfxCallMode::SYNCHRON);
-            pDispatch->Execute(SID_PASTE, SfxCallMode::SYNCHRON);
-        }
-        return bPasted;
     }
 
     return EXCHG_INOUT_ACTION_NONE != nAction &&

commit b0d73d141ee9fe3766cab7a3a549385711023c3a

tdf#138688 tdf#124646 sw: fix crash at pasting Calc data
.. in a table before a numbered paragraph. Dispatcher
calls of the workaround for tdf#124646 missed the temporary
table in this case, selecting nothing and after that
trying to paste the same non-native Calc data again,
resulting infinite recursion.

Replacing FN_CHAR_LEFT with FN_LINE_UP solved the
problem (FN_CHAR_LEFT selected the numbers of the
numbered list instead of the temporary table).
Fixing the fragile dispatcher calls, now we check
the selection of the temporary table to avoid similar
crashes.

Unit tests are added for the fix, also for the
original problem of tdf#124646 (avoid copying
hidden rows from Calc, e.g. copying only visible
result of a filtering).

Regression from commit 0c3ac02d8a3c7ea50ae262daf134c28df5c8b343
(tdf#124646 Don't paste hidden rows of Calc sheets into Writer tables).

(Note: to check/show the fix of the crash manually, run the test with

$ (cd sw && make -srj8 UITest_sw_table UITEST_TEST_NAME="sheetToTable.sheetToTable.test_tdf138688" SAL_USE_VCLPLUGIN=gen)

adding

import time
time.sleep(5)

to the called function of sheetToTable.py)

Change-Id: I7b90af8219d6fd00b75d91f7c92fff5744373cc6

sw/source/uibase/dochdl/swdtflvr.cxx
@@ -1525,11 +1525,20 @@ bool SwTransferable::Paste(SwWrtShell& rSh, TransferableDataHelper& rData, RndSt
             if ( SwTransferable::PasteData( rData, rSh, EXCHG_OUT_ACTION_INSERT_STRING, nActionFlags, SotClipboardFormatId::HTML,
                                         nDestination, false, false, nullptr, 0, false, nAnchorType, bIgnoreComments, &aPasteContext, ePasteTable) )
             {
-                pDispatch->Execute(FN_CHAR_LEFT, SfxCallMode::SYNCHRON);
-                pDispatch->Execute(FN_TABLE_SELECT_ALL, SfxCallMode::SYNCHRON);
-                pDispatch->Execute(SID_COPY, SfxCallMode::SYNCHRON);
+                bool bFoundTemporaryTable = false;
+                pDispatch->Execute(FN_LINE_UP, SfxCallMode::SYNCHRON);
+                if (rSh.GetDoc()->IsIdxInTable(rSh.GetCursor()->GetNode()) != nullptr)
+                {
+                    bFoundTemporaryTable = true;
+                    pDispatch->Execute(FN_TABLE_SELECT_ALL, SfxCallMode::SYNCHRON);
+                    pDispatch->Execute(SID_COPY, SfxCallMode::SYNCHRON);
+                }
                 for(sal_uInt32 a = 0; a < 1 + (nLevel * 2); a++)
                     pDispatch->Execute(SID_UNDO, SfxCallMode::SYNCHRON);
+                // clipboard content hasn't changed (limit potential infinite
+                // recursion with the same non-native table, as was in tdf#138688)
+                if (!bFoundTemporaryTable)
+                    return false;
                 if (ePasteTable == PasteTableType::PASTE_TABLE)
                     pDispatch->Execute(FN_PASTE_NESTED_TABLE, SfxCallMode::SYNCHRON);
                 else if (ePasteTable == PasteTableType::PASTE_ROW)

sw/qa/uitest/table/sheetToTable.py
@@ -1,0 +1,75 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+from uitest.framework import UITestCase
+from uitest.uihelper.common import get_state_as_dict
+from uitest.uihelper.common import select_pos
+from libreoffice.uno.propertyvalue import mkPropertyValues
+from uitest.uihelper.common import get_state_as_dict, type_text
+import org.libreoffice.unotest
+import pathlib
+
+def get_url_for_data_file(file_name):
+    return pathlib.Path(org.libreoffice.unotest.makeCopyFromTDOC(file_name)).as_uri()
+
+#Calc sheet to Writer table
+
+class sheetToTable(UITestCase):
+    def test_sheet_to_table_without_hidden_rows(self):
+        print(get_url_for_data_file("hiddenRow.ods"))
+        calc_doc = self.ui_test.load_file(get_url_for_data_file("hiddenRow.ods"))
+        xCalcDoc = self.xUITest.getTopFocusWindow()
+        self.xUITest.executeCommand(".uno:SelectAll")
+        self.xUITest.executeCommand(".uno:Copy")
+        gridwin = xCalcDoc.getChild("grid_window")
+        document = self.ui_test.get_component()
+        self.ui_test.close_doc()
+        writer_doc = self.ui_test.load_file(get_url_for_data_file("tableToText.odt"))
+        document = self.ui_test.get_component()
+        xWriterDoc = self.xUITest.getTopFocusWindow()
+        self.xUITest.executeCommand(".uno:Paste")
+        #verify (don't copy hidden cells)
+        self.assertEqual(document.TextTables.getCount(), 1)
+        table = document.getTextTables()[0]
+        # This was 3 (copied hidden row)
+        self.assertEqual(len(table.getRows()), 2)
+        self.assertEqual(table.getCellByName("A1").getString(), "1")
+        # This was "2 (hidden)" (copied hidden row)
+        self.assertEqual(table.getCellByName("A2").getString(), "3")
+        self.ui_test.close_doc()
+
+    def test_tdf138688(self):
+        print(get_url_for_data_file("hiddenRow.ods"))
+        calc_doc = self.ui_test.load_file(get_url_for_data_file("hiddenRow.ods"))
+        xCalcDoc = self.xUITest.getTopFocusWindow()
+        self.xUITest.executeCommand(".uno:SelectAll")
+        self.xUITest.executeCommand(".uno:Copy")
+        gridwin = xCalcDoc.getChild("grid_window")
+        document = self.ui_test.get_component()
+        self.ui_test.close_doc()
+        writer_doc = self.ui_test.load_file(get_url_for_data_file("tableToText.odt"))
+        document = self.ui_test.get_component()
+        xWriterDoc = self.xUITest.getTopFocusWindow()
+
+        # set numbering in the paragraph after the table
+        self.xUITest.executeCommand(".uno:GoDown")
+        self.xUITest.executeCommand(".uno:GoDown")
+        self.xUITest.executeCommand(".uno:DefaultNumbering")
+        self.xUITest.executeCommand(".uno:GoUp")
+        self.xUITest.executeCommand(".uno:GoUp")
+
+        #verify (this was a freezing/crash)
+        self.xUITest.executeCommand(".uno:Paste")
+
+        #verify also tdf#124646 (don't copy hidden cells)
+        self.assertEqual(document.TextTables.getCount(), 1)
+        table = document.getTextTables()[0]
+        # This was 3 (copied hidden row)
+        self.assertEqual(len(table.getRows()), 2)
+        self.assertEqual(table.getCellByName("A1").getString(), "1")
+        # This was "2 (hidden)" (copied hidden row)
+        self.assertEqual(table.getCellByName("A2").getString(), "3")
+        self.ui_test.close_doc()