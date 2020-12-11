a9800bd7a3c8f7186a8950da981a414e7803a0a0
dante <dante19031999@gmail.com>
2020-12-11 20:37:27 +0100

    Add typedef sal_Unicode32
    This allows to have a type for unicode 32 characters.
    Will allow easier handle of unicode 32 from oustring in near future.
    Will be very usefull for starmath since there are mathematical symbols sections outside UTF-16 ( when implemented opensymbol2  ).
    
    Change-Id: I783ef521821aa15aa6acd24178abc3d4856fd33f

--- include/sal/types.h
+++ include/sal/types.h
@@ -112,11 +112,14 @@ typedef unsigned char       sal_uInt8;
 #if defined LIBO_INTERNAL_ONLY && defined __cplusplus
     #define SAL_UNICODE_NOTEQUAL_WCHAR_T
     typedef char16_t sal_Unicode;
+    typedef char32_t sal_Unicode32;
 #elif defined(_WIN32)
     typedef wchar_t sal_Unicode;
+    typedef sal_uInt32 sal_Unicode32;
 #else
     #define SAL_UNICODE_NOTEQUAL_WCHAR_T
     typedef sal_uInt16 sal_Unicode;
+    typedef sal_uInt32 sal_Unicode32;
 #endif
 
 typedef void *                   sal_Handle;
