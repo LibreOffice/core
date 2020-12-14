acf4c5bc649624823d84e01d57a9af78ff3084ae
Caolán McNamara <caolanm@redhat.com>
2020-12-14 20:57:38 +0000

    ofz#28630 Fuzzing build failure
    Step #4: vcl/workben/commonfuzzer.hxx:70:28: error: no matching function for call to 'OUStringToOString'
    Step #4:             aBuffer.append(OUStringToOString(sExecDir + getExecutableName(), osl_getThreadTextEncoding())).append(".fonts");
    Step #4:                            ^~~~~~~~~~~~~~~~~
    Step #4: include/rtl/ustring.hxx:3587:16: note: candidate function not viable: no known conversion from 'typename std::enable_if_t<ToStringHelper<OUString>::allowOUStringConcat && ToStringHelper<OUString>::allowOUStringConcat, OUStringConcat<OUString, OUString>>' (aka 'rtl::OUStringConcat<rtl::OUString, rtl::OUString>') to 'std::u16string_view' (aka 'basic_string_view<char16_t>') for 1st argument
    Step #4: inline OString OUStringToOString( std::u16string_view rUnicode,
    
    dunno what the preferred concatenation scheme is these days
    
    Change-Id: I7390d5b4a23a76d7cfbc75c90ea75df4ee39457f
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/107731
    Tested-by: Caolán McNamara <caolanm@redhat.com>
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>

--- vcl/workben/commonfuzzer.hxx
+++ vcl/workben/commonfuzzer.hxx
@@ -67,7 +67,8 @@ namespace
             osl::FileBase::getSystemPathFromFileURL(execdir, sExecDir);
 
             OStringBuffer aBuffer("<?xml version=\"1.0\"?>\n<fontconfig><dir>");
-            aBuffer.append(OUStringToOString(sExecDir + getExecutableName(), osl_getThreadTextEncoding())).append(".fonts");
+            aBuffer.append(OUStringToOString(sExecDir, osl_getThreadTextEncoding()))
+                   .append(OUStringToOString(getExecutableName(), osl_getThreadTextEncoding())).append(".fonts");
             aBuffer.append("</dir><cachedir>/tmp/cache/fontconfig</cachedir></fontconfig>");
             OString aConf = aBuffer.makeStringAndClear();
             sal_uInt64 aBytesWritten;
