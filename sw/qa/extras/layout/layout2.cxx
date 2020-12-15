LibreOffice/core

commit 300e65cc47f3d6ae1563350757dbfadc080d7452

tdf#123268 fix lost chart if all legend entries are hidden
See also commit 19b2ed4dee1ec33ad55473d43bfcd0bfa194cbee
(related tdf#51671, add UI options for new "hide legend entry" feature)

Change-Id: If20ab167d867d2845fa414ba0bbc490bed09f61c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/89123
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

chart2/source/view/main/VLegend.cxx
@@ -950,6 +950,8 @@ void VLegend::createShapes(
                     std::vector<ViewLegendEntry> aNewEntries = pLegendEntryProvider->createLegendEntries(
                                                                     aMaxSymbolExtent, eExpansion, xLegendProp,
                                                                     xLegendContainer, m_xShapeFactory, m_xContext, mrModel);
+                    if (aNewEntries.size() == 0)
+                        return;
                     aViewEntries.insert( aViewEntries.end(), aNewEntries.begin(), aNewEntries.end() );
                 }
             }

sw/qa/extras/layout/layout.cxx
@@ -2319,6 +2319,24 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf75659)
     // These failed, if the legend names are empty strings.
 }
 
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf123268)
+{
+    SwDoc* pDoc = createDoc("tdf123268.odt");
+    SwDocShell* pShell = pDoc->GetDocShell();
+
+    // Dump the rendering of the first page as an XML file.
+    std::shared_ptr<GDIMetaFile> xMetaFile = pShell->GetPreviewMetaFile();
+    MetafileXmlDump dumper;
+    xmlDocPtr pXmlDoc = dumpAndParse(dumper, *xMetaFile);
+    CPPUNIT_ASSERT(pXmlDoc);
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 41
+    // - Actual  : 0
+    // i.e. the chart lost.
+    assertXPath(pXmlDoc, "/metafile/push[1]/push[1]/push[1]/push[3]/push[1]/push[1]/push[1]/push",
+                41);
+}
+
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf115630)
 {
     SwDoc* pDoc = createDoc("tdf115630.docx");

commit 51dddd832976d7b43f5f81ef4a655e88ed117ebf

tdf#136816: fix hidden legend entries in "Column and Line" charts
Regression from commit: 300e65cc47f3d6ae1563350757dbfadc080d7452
(tdf#123268 fix lost chart if all legend entries are hidden)

Change-Id: Id59cd8d681dada123feadbe7910be7fbc7ec37f6

chart2/source/view/main/VLegend.cxx
@@ -1000,8 +1000,6 @@ void VLegend::createShapes(
                     std::vector<ViewLegendEntry> aNewEntries = pLegendEntryProvider->createLegendEntries(
                                                                     aMaxSymbolExtent, eLegendPosition, xLegendProp,
                                                                     xLegendContainer, m_xShapeFactory, m_xContext, mrModel);
-                    if (aNewEntries.size() == 0)
-                        return;
                     aViewEntries.insert( aViewEntries.end(), aNewEntries.begin(), aNewEntries.end() );
                 }
             }
@@ -1042,17 +1040,14 @@ void VLegend::createShapes(
                     // create the buttons
                     pButton->createShapes(xModelPage);
                 }
+
+                Reference<drawing::XShape> xBorder = pShapeFactory->createRectangle(
+                    xLegendContainer, aLegendSize, awt::Point(0, 0), aLineFillProperties.first,
+                    aLineFillProperties.second, ShapeFactory::StackPosition::Bottom);
+
+                //because of this name this border will be used for marking the legend
+                ShapeFactory::setShapeName(xBorder, "MarkHandles");
             }
-
-            Reference< drawing::XShape > xBorder =
-                pShapeFactory->createRectangle( xLegendContainer,
-                        aLegendSize,
-                        awt::Point(0,0),
-                        aLineFillProperties.first,
-                        aLineFillProperties.second, ShapeFactory::StackPosition::Bottom );
-
-            //because of this name this border will be used for marking the legend
-            ShapeFactory::setShapeName( xBorder, "MarkHandles" );
         }
     }
     catch( const uno::Exception & )

sw/qa/extras/layout/layout2.cxx
@@ -454,6 +454,21 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter2, testTdf75659)
     // These failed, if the legend names are empty strings.
 }
 
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter2, testTdf136816)
+{
+    SwDoc* pDoc = createDoc("tdf136816.odt");
+    SwDocShell* pShell = pDoc->GetDocShell();
+
+    // Dump the rendering of the first page as an XML file.
+    std::shared_ptr<GDIMetaFile> xMetaFile = pShell->GetPreviewMetaFile();
+    MetafileXmlDump dumper;
+    xmlDocUniquePtr pXmlDoc = dumpAndParse(dumper, *xMetaFile);
+    CPPUNIT_ASSERT(pXmlDoc);
+
+    // Check number of legend entries
+    assertXPath(pXmlDoc, "//text[contains(text(),\"Column\")]", 2);
+}
+
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter2, testTdf126425)
 {
     SwDoc* pDoc = createDoc("long_legendentry.docx");