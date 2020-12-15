67a3bfeb43df3bfd2bf56e0206f27b1092f190ff
Seth Chaiklin <sdc.blanco@youmail.dk>
2020-12-15 15:37:31 +0100

    change "Cancel" button to "Close" in Template Manager dialog
    Change-Id: I3e8cbdb70f70002f1fe2b873e899f25a72463358

--- sd/qa/uitest/impress_tests/masterElements.py
+++ sd/qa/uitest/impress_tests/masterElements.py
@@ -16,7 +16,7 @@ class masterElements(UITestCase):
    def test_master_elements(self):
         self.ui_test.create_doc_in_start_center("impress")
         xTemplateDlg = self.xUITest.getTopFocusWindow()
-        xCancelBtn = xTemplateDlg.getChild("cancel")
+        xCancelBtn = xTemplateDlg.getChild("close")
         self.ui_test.close_dialog_through_button(xCancelBtn)
         document = self.ui_test.get_component()
 
