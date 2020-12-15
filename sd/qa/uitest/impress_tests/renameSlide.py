04b045cd8520bba1a27d3c458d310eb3ce375376
Seth Chaiklin <sdc.blanco@youmail.dk>
2020-12-15 15:37:31 +0100

    change "Cancel" button to "Close" in Template Manager dialog
    Change-Id: I3e8cbdb70f70002f1fe2b873e899f25a72463358

--- sd/qa/uitest/impress_tests/renameSlide.py
+++ sd/qa/uitest/impress_tests/renameSlide.py
@@ -16,7 +16,7 @@ class renameSlide(UITestCase):
    def test_rename_slide(self):
         self.ui_test.create_doc_in_start_center("impress")
         xTemplateDlg = self.xUITest.getTopFocusWindow()
-        xCancelBtn = xTemplateDlg.getChild("cancel")
+        xCancelBtn = xTemplateDlg.getChild("close")
         self.ui_test.close_dialog_through_button(xCancelBtn)
 
         self.ui_test.execute_dialog_through_command(".uno:RenamePage")
