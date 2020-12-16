bb573e63e154d605024f8df4ac2a13e21a43e50a
Jan-Marek Glogowski <glogow@fbihome.de>
2020-12-16 10:51:33 +0100

    Scheduler assert mbInScheduler state
    Since the introduction of the scheduler stack, this became merely
    a debugging and assertion hint. So actually handle it like that
    and don't use it for any decisions, even in debug code.
    
    Change-Id: Ibeb247279d9e94ea4ff3a7b4652e98e179d6c76e

--- vcl/inc/schedulerimpl.hxx
+++ vcl/inc/schedulerimpl.hxx
@@ -32,6 +32,18 @@ struct ImplSchedulerData final
     Task*              mpTask;        ///< Pointer to VCL Task instance
     sal_uInt64         mnUpdateTime;  ///< Last Update Time
     TaskPriority       mePriority;    ///< Task priority
+    /**
+     * Is the Task currently processed / on the stack?
+     *
+     * Since the introduction of the scheduler stack, this became merely a
+     * debugging and assertion hint. No decisions are anymore made based on
+     * this, because invoked Tasks are removed from the scheduler lists and
+     * placed on the stack, so no code should actually ever find one, where
+     * mbInScheduler is true (I don't see a reason to walk the stack for
+     * normal Scheduler usage, that is).
+     *
+     * This was originally used to prevent invoking Tasks recursively.
+     **/
     bool               mbInScheduler; ///< Task currently processed?
 
     const char *GetDebugName() const;
