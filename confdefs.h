64a9447ac6328e93626f26e61633ce9f5789a3d9
dante <dante19031999@gmail.com>
2020-12-16 15:06:07 +0100

    Fix dupplicated and triplicated asserts.
    Change-Id: I5af45922925c403c7b2379b2b5113f5ef1bbb984

---
+++ confdefs.h
@@ -0,0 +1,12 @@
+/* confdefs.h */
+#define PACKAGE_NAME "LibreOffice"
+#define PACKAGE_TARNAME "libreoffice"
+#define PACKAGE_VERSION "7.2.0.0.alpha0+"
+#define PACKAGE_STRING "LibreOffice 7.2.0.0.alpha0+"
+#define PACKAGE_BUGREPORT ""
+#define PACKAGE_URL "http://documentfoundation.org/"
+#define LIBO_VERSION_MAJOR 7
+#define LIBO_VERSION_MINOR 2
+#define LIBO_VERSION_MICRO 0
+#define LIBO_VERSION_PATCH 0
+#define LIBO_THIS_YEAR 2020
