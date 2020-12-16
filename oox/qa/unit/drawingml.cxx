fe902e9d1dd01801aedc45c2361e2992d4c19d2d
Miklos Vajna <vmiklos@collabora.com>
2020-12-16 14:19:16 +0100

    tdf#129961 oox: add PPTX export for table shadow as direct format
    Custom shapes export shadow as part of WriteShapeEffects(), so use the
    same for table shapes as well.
    
    This needs fixing the effect export up a bit, because table shapes have
    no interop grab-bag, glow or soft edge properties, but the rest of the
    code can be shared.
    
    Change-Id: Icf0b90c5b44e3d9c4115c9f3b0d56ba0852ab640

--- oox/qa/unit/drawingml.cxx
+++ oox/qa/unit/drawingml.cxx
@@ -293,21 +293,37 @@ CPPUNIT_TEST_FIXTURE(OoxDrawingmlTest, testCameraRotationRevolution)
 
 CPPUNIT_TEST_FIXTURE(OoxDrawingmlTest, testTableShadow)
 {
+    auto verify = [](const uno::Reference<lang::XComponent>& xComponent) {
+        uno::Reference<drawing::XDrawPagesSupplier> xDrawPagesSupplier(xComponent, uno::UNO_QUERY);
+        uno::Reference<drawing::XDrawPage> xDrawPage(
+            xDrawPagesSupplier->getDrawPages()->getByIndex(0), uno::UNO_QUERY);
+        uno::Reference<beans::XPropertySet> xShape(xDrawPage->getByIndex(0), uno::UNO_QUERY);
+        bool bShadow = false;
+        CPPUNIT_ASSERT(xShape->getPropertyValue("Shadow") >>= bShadow);
+
+        CPPUNIT_ASSERT(bShadow);
+        sal_Int32 nColor = 0;
+        CPPUNIT_ASSERT(xShape->getPropertyValue("ShadowColor") >>= nColor);
+        CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(0xff0000), nColor);
+    };
     OUString aURL = m_directories.getURLFromSrc(DATA_DIRECTORY) + "table-shadow.pptx";
     load(aURL);
-    uno::Reference<drawing::XDrawPagesSupplier> xDrawPagesSupplier(getComponent(), uno::UNO_QUERY);
-    uno::Reference<drawing::XDrawPage> xDrawPage(xDrawPagesSupplier->getDrawPages()->getByIndex(0),
-                                                 uno::UNO_QUERY);
-    uno::Reference<beans::XPropertySet> xShape(xDrawPage->getByIndex(0), uno::UNO_QUERY);
-    bool bShadow = false;
-    CPPUNIT_ASSERT(xShape->getPropertyValue("Shadow") >>= bShadow);
-
     // Without the accompanying fix in place, this test would have failed, because shadow on a table
     // was lost on import.
-    CPPUNIT_ASSERT(bShadow);
-    sal_Int32 nColor = 0;
-    CPPUNIT_ASSERT(xShape->getPropertyValue("ShadowColor") >>= nColor);
-    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(0xff0000), nColor);
+    verify(getComponent());
+
+    uno::Reference<frame::XStorable> xStorable(getComponent(), uno::UNO_QUERY);
+    utl::MediaDescriptor aMediaDescriptor;
+    aMediaDescriptor["FilterName"] <<= OUString("Impress Office Open XML");
+    utl::TempFile aTempFile;
+    aTempFile.EnableKillingFile();
+    xStorable->storeToURL(aTempFile.GetURL(), aMediaDescriptor.getAsConstPropertyValueList());
+    getComponent()->dispose();
+    validate(aTempFile.GetFileName(), test::OOXML);
+    getComponent() = loadFromDesktop(aTempFile.GetURL());
+    // Without the accompanying fix in place, this test would have failed, because shadow on a table
+    // was lost on export.
+    verify(getComponent());
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();
