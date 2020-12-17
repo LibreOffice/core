ed3ddcdfc7831f67706688359ca0fe7cab06035f
Henry Castro <hcastro@collabora.com>
2020-12-17 10:45:13 -0400

    jsdialog: disable idle notify
    After testing on client side, when showing
    the Macro Selector Dialog its receive 5 times
    the same data.
    
    So add an option to not use the idle notifier
    instead it will use the event 'WindowShow'.
    
    Change-Id: Ib5657f3c823fc6e998581b99052aaf578d0913e8

--- vcl/inc/window.h
+++ vcl/inc/window.h
@@ -117,6 +117,8 @@ struct ImplWinData
     sal_uInt16          mnIsTopWindow;
     bool                mbMouseOver;            //< tracks mouse over for native widget paint effect
     bool                mbEnableNativeWidget;   //< toggle native widget rendering
+    bool                mbDisableIdleNotify;
+
     ::std::list< VclPtr<vcl::Window> >
                         maTopWindowChildren;
 
