LibreOffice/core

commit b226383a83e41bbced9fc2a02dc09a449401ec97

tdf#116371: export rotation of SwGrfNode
Change-Id: I42620da798a35dfada67d9a9fb23d554cc20b16f
Reviewed-on: https://gerrit.libreoffice.org/66963
Tested-by: Jenkins
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

sw/source/filter/ww8/docxattributeoutput.cxx
@@ -4761,7 +4761,27 @@ void DocxAttributeOutput::FlyFrameGraphic( const SwGrfNode* pGrfNode, const Size
         CharGrabBag(*pGrabBag);
     }
 
-    m_rExport.SdrExporter().startDMLAnchorInline(pFrameFormat, rSize);
+    rtl::Reference<sax_fastparser::FastAttributeList> xFrameAttributes(
+        FastSerializerHelper::createAttrList());
+    Size aSize = rSize;
+    if (pGrfNode)
+    {
+        const SwAttrSet& rSet = pGrfNode->GetSwAttrSet();
+        MirrorGraph eMirror = rSet.Get(RES_GRFATR_MIRRORGRF).GetValue();
+        if (eMirror == MirrorGraph::Vertical || eMirror == MirrorGraph::Both)
+            // Mirror on the vertical axis is a horizontal flip.
+            xFrameAttributes->add(XML_flipH, "1");
+        // RES_GRFATR_ROTATION is sal_uInt16; use sal_uInt32 for multiplication later
+        if (sal_uInt32 nRot = rSet.Get(RES_GRFATR_ROTATION).GetValue())
+        {
+            // RES_GRFATR_ROTATION is in 10ths of degree; convert to 100ths for macro
+            sal_uInt32 mOOXMLRot = OOX_DRAWINGML_EXPORT_ROTATE_CLOCKWISIFY(nRot*10);
+            xFrameAttributes->add(XML_rot, OString::number(mOOXMLRot));
+            aSize = pGrfNode->GetTwipSize();
+        }
+    }
+
+    m_rExport.SdrExporter().startDMLAnchorInline(pFrameFormat, aSize);
 
     // picture description (used for pic:cNvPr later too)
     ::sax_fastparser::FastAttributeList* docPrattrList = FastSerializerHelper::createAttrList();
@@ -4868,25 +4888,14 @@ void DocxAttributeOutput::FlyFrameGraphic( const SwGrfNode* pGrfNode, const Size
             XML_bwMode, "auto",
             FSEND );
 
-    rtl::Reference<sax_fastparser::FastAttributeList> xFrameAttributes(
-        FastSerializerHelper::createAttrList());
-
-    if (pGrfNode)
-    {
-        MirrorGraph eMirror = pGrfNode->GetSwAttrSet().Get(RES_GRFATR_MIRRORGRF).GetValue();
-        if (eMirror == MirrorGraph::Vertical || eMirror == MirrorGraph::Both)
-            // Mirror on the vertical axis is a horizontal flip.
-            xFrameAttributes->add(XML_flipH, "1");
-    }
-
     m_pSerializer->startElementNS(
         XML_a, XML_xfrm, uno::Reference<xml::sax::XFastAttributeList>(xFrameAttributes.get()));
 
     m_pSerializer->singleElementNS( XML_a, XML_off,
             XML_x, "0", XML_y, "0",
             FSEND );
-    OString aWidth( OString::number( TwipsToEMU( rSize.Width() ) ) );
-    OString aHeight( OString::number( TwipsToEMU( rSize.Height() ) ) );
+    OString aWidth( OString::number( TwipsToEMU( aSize.Width() ) ) );
+    OString aHeight( OString::number( TwipsToEMU( aSize.Height() ) ) );
     m_pSerializer->singleElementNS( XML_a, XML_ext,
             XML_cx, aWidth.getStr(),
             XML_cy, aHeight.getStr(),

sw/qa/extras/ooxmlexport/ooxmlexport13.cxx
@@ -97,6 +97,16 @@ DECLARE_OOXMLEXPORT_TEST(testInputListExport, "tdf122186_input_list.odt")
     assertXPath(pXmlDoc, "/w:document/w:body/w:p/w:r[4]/w:t", 0);
 }
 
+DECLARE_OOXMLEXPORT_TEST(testTdf116371, "tdf116371.odt")
+{
+    // Make sure the rotation is exported correctly, and size not distorted
+    auto xShape(getShape(1));
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(4700.0, getProperty<double>(xShape, "RotateAngle"), 10);
+    auto frameRect = getProperty<awt::Rectangle>(xShape, "FrameRect");
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(24070), frameRect.Height);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(24188), frameRect.Width);
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

commit 15254b8466b22b4016908166ca7a352590aa76cd

tdf#138953: use original (cropped, but unrotated) object size in spPr
This not only fixes the regression from b226383a83e41bbced9fc2a02dc09a449401ec97,
but also makes the written size more correct than before, when it was
slightly larger compared to original object size.

Corrected unit test for tdf#116371 reflect that: the object in ODT is
241.78 mm x 240.61 mm. It previously was exported as 241.88 x 240.70;
now the exported size is closer: 241.79 x 240.63.

Change-Id: Ibfe85c7cd98c089e58af8d7f3848990af8e1100f

sw/source/filter/ww8/docxattributeoutput.cxx
@@ -4834,13 +4834,12 @@ void DocxAttributeOutput::DefaultStyle()
 /* Writes <a:srcRect> tag back to document.xml if a file contains a cropped image.
 *  NOTE : Tested on images of type JPEG,EMF/WMF,BMP, PNG and GIF.
 */
-void DocxAttributeOutput::WriteSrcRect(const SdrObject* pSdrObj, const SwFrameFormat* pFrameFormat )
+void DocxAttributeOutput::WriteSrcRect(
+    const css::uno::Reference<css::beans::XPropertySet>& xShapePropSet,
+    const SwFrameFormat* pFrameFormat)
 {
-    uno::Reference< drawing::XShape > xShape( const_cast<SdrObject*>(pSdrObj)->getUnoShape(), uno::UNO_QUERY );
-    uno::Reference< beans::XPropertySet > xPropSet( xShape, uno::UNO_QUERY );
-
     uno::Reference<graphic::XGraphic> xGraphic;
-    xPropSet->getPropertyValue("Graphic") >>= xGraphic;
+    xShapePropSet->getPropertyValue("Graphic") >>= xGraphic;
     const Graphic aGraphic(xGraphic);
 
     Size aOriginalSize(aGraphic.GetPrefSize());
@@ -4853,7 +4852,7 @@ void DocxAttributeOutput::WriteSrcRect(const SdrObject* pSdrObj, const SwFrameFo
     }
 
     css::text::GraphicCrop aGraphicCropStruct;
-    xPropSet->getPropertyValue( "GraphicCrop" ) >>= aGraphicCropStruct;
+    xShapePropSet->getPropertyValue("GraphicCrop") >>= aGraphicCropStruct;
     sal_Int32 nCropL = aGraphicCropStruct.Left;
     sal_Int32 nCropR = aGraphicCropStruct.Right;
     sal_Int32 nCropT = aGraphicCropStruct.Top;
@@ -5020,7 +5019,6 @@ void DocxAttributeOutput::FlyFrameGraphic( const SwGrfNode* pGrfNode, const Size
 
     rtl::Reference<sax_fastparser::FastAttributeList> xFrameAttributes(
         FastSerializerHelper::createAttrList());
-    Size aSize = rSize;
     if (pGrfNode)
     {
         const SwAttrSet& rSet = pGrfNode->GetSwAttrSet();
@@ -5034,8 +5032,25 @@ void DocxAttributeOutput::FlyFrameGraphic( const SwGrfNode* pGrfNode, const Size
             // RES_GRFATR_ROTATION is in 10ths of degree; convert to 100ths for macro
             sal_uInt32 mOOXMLRot = oox::drawingml::ExportRotateClockwisify(nRot*10);
             xFrameAttributes->add(XML_rot, OString::number(mOOXMLRot));
-            aSize = pGrfNode->GetTwipSize();
         }
+    }
+
+    css::uno::Reference<css::beans::XPropertySet> xShapePropSet;
+    if (pSdrObj)
+    {
+        css::uno::Reference<css::drawing::XShape> xShape(
+            const_cast<SdrObject*>(pSdrObj)->getUnoShape(), css::uno::UNO_QUERY);
+        xShapePropSet.set(xShape, css::uno::UNO_QUERY);
+        assert(xShapePropSet);
+    }
+
+    Size aSize = rSize;
+    // We need the original (cropped, but unrotated) size of object. So prefer the object data,
+    // and only use passed frame size as fallback.
+    if (xShapePropSet)
+    {
+        if (css::awt::Size val; xShapePropSet->getPropertyValue("Size") >>= val)
+            aSize = Size(convertMm100ToTwip(val.Width), convertMm100ToTwip(val.Height));
     }
 
     m_rExport.SdrExporter().startDMLAnchorInline(pFrameFormat, aSize);
@@ -5051,11 +5066,9 @@ void DocxAttributeOutput::FlyFrameGraphic( const SwGrfNode* pGrfNode, const Size
     m_pSerializer->startElementNS( XML_wp, XML_docPr, docPrAttrListRef );
 
     OUString sURL, sRelId;
-    if(pSdrObj)
+    if (xShapePropSet)
     {
-        uno::Reference< drawing::XShape > xShape( const_cast<SdrObject*>(pSdrObj)->getUnoShape(), uno::UNO_QUERY );
-        uno::Reference< beans::XPropertySet > xPropSet( xShape, uno::UNO_QUERY );
-        xPropSet->getPropertyValue("HyperLinkURL") >>= sURL;
+        xShapePropSet->getPropertyValue("HyperLinkURL") >>= sURL;
         if(!sURL.isEmpty())
         {
             if (sURL.startsWith("#") && sURL.indexOf(' ') != -1 && !sURL.endsWith("|outline") && !sURL.endsWith("|table") &&
@@ -5134,9 +5147,8 @@ void DocxAttributeOutput::FlyFrameGraphic( const SwGrfNode* pGrfNode, const Size
     }
     m_pSerializer->endElementNS( XML_a, XML_blip );
 
-    if (pSdrObj){
-        WriteSrcRect(pSdrObj, pFrameFormat);
-    }
+    if (xShapePropSet)
+        WriteSrcRect(xShapePropSet, pFrameFormat);
 
     m_pSerializer->startElementNS(XML_a, XML_stretch);
     m_pSerializer->singleElementNS(XML_a, XML_fillRect);

sw/source/filter/ww8/docxattributeoutput.hxx
@@ -403,7 +403,8 @@ private:
     ///
     /// @see WriteOLE2Obj()
     void FlyFrameGraphic( const SwGrfNode* pGrfNode, const Size& rSize, const SwFlyFrameFormat* pOLEFrameFormat, SwOLENode* pOLENode, const SdrObject* pSdrObj = nullptr);
-    void WriteSrcRect( const SdrObject* pSdrObj, const SwFrameFormat* pFrameFormat );
+    void WriteSrcRect(const css::uno::Reference<css::beans::XPropertySet>& xShapePropSet,
+                      const SwFrameFormat* pFrameFormat);
     void WriteOLE2Obj( const SdrObject* pSdrObj, SwOLENode& rNode, const Size& rSize, const SwFlyFrameFormat* pFlyFrameFormat, const sal_Int8 nFormulaAlignment);
     bool WriteOLEChart( const SdrObject* pSdrObj, const Size& rSize, const SwFlyFrameFormat* pFlyFrameFormat);
     bool WriteOLEMath( const SwOLENode& rNode, const sal_Int8 nAlign );

sw/qa/extras/ooxmlexport/ooxmlexport13.cxx
@@ -610,8 +610,8 @@ DECLARE_OOXMLEXPORT_TEST(testTdf116371, "tdf116371.odt")
     auto xShape(getShape(1));
     CPPUNIT_ASSERT_DOUBLES_EQUAL(4700.0, getProperty<double>(xShape, "RotateAngle"), 10);
     auto frameRect = getProperty<awt::Rectangle>(xShape, "FrameRect");
-    CPPUNIT_ASSERT_EQUAL(sal_Int32(24070), frameRect.Height);
-    CPPUNIT_ASSERT_EQUAL(sal_Int32(24188), frameRect.Width);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(24063), frameRect.Height);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(24179), frameRect.Width);
 }
 
 DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testFrameSizeExport, "floating-tables-anchor.docx")

sw/qa/extras/ooxmlexport/ooxmlexport16.cxx
@@ -38,6 +38,20 @@ DECLARE_OOXMLEXPORT_TEST(testTdf138892_noNumbering, "tdf138892_noNumbering.docx"
     CPPUNIT_ASSERT_MESSAGE("Para3: <blank line>", getProperty<OUString>(getParagraph(3), "NumberingStyleName").isEmpty());
 }
 
+DECLARE_OOXMLEXPORT_TEST(testTdf138953, "croppedAndRotated.odt")
+{
+    CPPUNIT_ASSERT_EQUAL(1, getShapes());
+    CPPUNIT_ASSERT_EQUAL(1, getPages());
+    // Make sure the rotation is exported correctly, and size not distorted
+    auto xShape(getShape(1));
+    CPPUNIT_ASSERT_EQUAL(27000.0, getProperty<double>(xShape, "RotateAngle"));
+    auto frameRect = getProperty<css::awt::Rectangle>(xShape, "FrameRect");
+    // Before the fix, original object size (i.e., before cropping) was written to spPr in OOXML,
+    // and the resulting object size was much larger than should be.
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(12961), frameRect.Height);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(8664), frameRect.Width);
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */