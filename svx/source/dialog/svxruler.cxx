5d8951ecff932216b34bf44cb23dc5b87f315b6f
Caolán McNamara <caolanm@redhat.com>
2020-12-18 20:46:34 +0000

    missing modelines
    Change-Id: I33e07218b1d3ae06395d7b5a4ca852e887093669
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/107982
    Tested-by: Caolán McNamara <caolanm@redhat.com>
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>

--- svx/source/dialog/svxruler.cxx
+++ svx/source/dialog/svxruler.cxx
@@ -1,4 +1,4 @@
-/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
 /*
  * This file is part of the LibreOffice project.
  *
@@ -3558,3 +3558,5 @@ void SvxRuler::SetValues(RulerChangeType type, tools::Long diffValue)
         SetMargin2( GetMargin2() - diffValue);
     ApplyMargins();
 }
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
