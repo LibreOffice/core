2f81b06e345c64a8e4ca04b44afea54a3a9e12a7
Jan-Marek Glogowski <glogow@fbihome.de>
2020-12-21 17:31:23 +0100

    Catchall again
    As described, I think I found the origin of the problem with an
    emcc.py patch I wrote in the beginning to fix configure with cpp
    tests. This compiles LO up to some point in Calc with a template
    failure. I've made an extra patch from the galeries changes.
    
    The binary is essentially broken. While the alignment is checked
    by LO configure, the result instantly crashes with memory
    alignement errors somewhere. There seems to be a problem with
    inline Exceptions, so I just converted SvStreamEOFException to
    non-inline as a workaround.
    
    Change-Id: I3333a04cdb765da3efc4aa511e265a3863d5c9cd

--- RepositoryExternal.mk
+++ RepositoryExternal.mk
@@ -1383,6 +1383,8 @@ $(call gb_LinkTarget_add_libs,$(1),$(FONTCONFIG_LIBS))
 
 endef
 
+gb_ExternalProject__use_fontconfig :=
+
 else
 
 define gb_LinkTarget__use_fontconfig
@@ -1395,6 +1397,11 @@ $(call gb_LinkTarget_set_include,$(1),\
 $(call gb_LinkTarget_add_libs,$(1),\
     -L$(call gb_UnpackedTarball_get_dir,fontconfig)/src/.libs -lfontconfig \
 )
+
+endef
+
+define gb_ExternalProject__use_fontconfig
+$(call gb_ExternalProject_use_external_project,$(1),fontconfig)
 
 endef
 
