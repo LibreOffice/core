d466affd2872c36c0c7a829319102b342767654a
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2020-12-21 21:47:39 +0900

    vcl: add BinaryDataContainer and change GfxLink to use it
    Add a wrapper for shared_ptr<sal_uInt8> called BinaryDataContainer,
    which is used to easily manage the binary data - mainly for a
    better reuse, control and to prevent duplication.
    
    Change-Id: I68140ec379dba4a5ab1b624a334129bba2401998

---
+++ vcl/source/graphic/BinaryDataContainer.cxx
@@ -0,0 +1,36 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#include <vcl/BinaryDataContainer.hxx>
+#include <boost/functional/hash.hpp>
+
+BinaryDataContainer::BinaryDataContainer() = default;
+
+BinaryDataContainer::BinaryDataContainer(size_t nSize)
+    : mpData(std::make_shared<std::vector<sal_uInt8>>(nSize))
+{
+}
+
+BinaryDataContainer::BinaryDataContainer(const sal_uInt8* pData, size_t nSize)
+    : mpData(std::make_shared<std::vector<sal_uInt8>>(nSize))
+{
+    std::copy(pData, pData + nSize, mpData->data());
+}
+
+size_t BinaryDataContainer::calculateHash() const
+{
+    size_t nSeed = 0;
+    boost::hash_combine(nSeed, getSize());
+    for (sal_uInt8 const& rByte : *mpData)
+        boost::hash_combine(nSeed, rByte);
+    return nSeed;
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
