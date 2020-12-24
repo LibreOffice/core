LibreOffice/core

commit 4491cee27c80bc83493136b323af5bdd128352e7

replace margin-left with margin-start and margin-right with margin-end
Change-Id: Iee3cc8c22b393ca420d0ed68673c61fe7ef240ba
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/108113
Tested-by: Caolán McNamara <caolanm@redhat.com>
Reviewed-by: Caolán McNamara <caolanm@redhat.com>

vcl/source/window/window2.cxx
@@ -1531,9 +1531,15 @@ bool Window::set_property(const OString &rKey, const OUString &rValue)
     else if (rKey == "border-width")
         set_border_width(rValue.toInt32());
     else if (rKey == "margin-start" || rKey == "margin-left")
-        set_margin_left(rValue.toInt32());
+    {
+        assert(rKey == "margin-start" && "margin-left deprecated in favor of margin-start");
+        set_margin_start(rValue.toInt32());
+    }
     else if (rKey == "margin-end" || rKey == "margin-right")
-        set_margin_right(rValue.toInt32());
+    {
+        assert(rKey == "margin-end" && "margin-right deprecated in favor of margin-end");
+        set_margin_end(rValue.toInt32());
+    }
     else if (rKey == "margin-top")
         set_margin_top(rValue.toInt32());
     else if (rKey == "margin-bottom")
@@ -1837,7 +1843,7 @@ sal_Int32 Window::get_border_width() const
     return pWindowImpl->mnBorderWidth;
 }
 
-void Window::set_margin_left(sal_Int32 nWidth)
+void Window::set_margin_start(sal_Int32 nWidth)
 {
     WindowImpl *pWindowImpl = mpWindowImpl->mpBorderWindow ? mpWindowImpl->mpBorderWindow->mpWindowImpl.get() : mpWindowImpl.get();
     if (pWindowImpl->mnMarginLeft != nWidth)
@@ -1847,13 +1853,13 @@ void Window::set_margin_left(sal_Int32 nWidth)
     }
 }
 
-sal_Int32 Window::get_margin_left() const
+sal_Int32 Window::get_margin_start() const
 {
     WindowImpl *pWindowImpl = mpWindowImpl->mpBorderWindow ? mpWindowImpl->mpBorderWindow->mpWindowImpl.get() : mpWindowImpl.get();
     return pWindowImpl->mnMarginLeft;
 }
 
-void Window::set_margin_right(sal_Int32 nWidth)
+void Window::set_margin_end(sal_Int32 nWidth)
 {
     WindowImpl *pWindowImpl = mpWindowImpl->mpBorderWindow ? mpWindowImpl->mpBorderWindow->mpWindowImpl.get() : mpWindowImpl.get();
     if (pWindowImpl->mnMarginRight != nWidth)
@@ -1863,7 +1869,7 @@ void Window::set_margin_right(sal_Int32 nWidth)
     }
 }
 
-sal_Int32 Window::get_margin_right() const
+sal_Int32 Window::get_margin_end() const
 {
     WindowImpl *pWindowImpl = mpWindowImpl->mpBorderWindow ? mpWindowImpl->mpBorderWindow->mpWindowImpl.get() : mpWindowImpl.get();
     return pWindowImpl->mnMarginRight;

commit aaf96ba14d9ab3d589cca91dedeec48f00169a06

Temporarily disable assert that kills Jenkins
It was introduced by 4491cee27c80bc83493136b323af5bdd128352e7 "replace
margin-left with margin-start and margin-right with margin-end", but keeps
breaking CppunitTest_sw_dialogs_test and CppunitTest_sw_dialogs_test2 of
<https://ci.libreoffice.org/job/gerrit_linux_clang_dbgutil/> now.

Change-Id: I91a7e4bdb6f39f3d6c094efe68a4accb1d6a99b2

vcl/source/window/window2.cxx
@@ -1532,7 +1532,7 @@ bool Window::set_property(const OString &rKey, const OUString &rValue)
         set_border_width(rValue.toInt32());
     else if (rKey == "margin-start" || rKey == "margin-left")
     {
-        assert(rKey == "margin-start" && "margin-left deprecated in favor of margin-start");
+        //TODO: assert(rKey == "margin-start" && "margin-left deprecated in favor of margin-start");
         set_margin_start(rValue.toInt32());
     }
     else if (rKey == "margin-end" || rKey == "margin-right")