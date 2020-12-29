64f30cf4212b80541e99a299b2b994ead507d19a
Tibor Nagy <nagy.tibor2@nisz.hu>
2020-12-29 08:36:49 +0100

    tdf#139394 XLSX export: remove extra quotation marks
    in "beginsWith" and "endsWith" conditional formatting
    
    Change-Id: Idae3190b5f87dac551251da19c511f501bdaaa21

--- sc/source/filter/excel/xeextlst.cxx
+++ sc/source/filter/excel/xeextlst.cxx
@@ -175,16 +175,16 @@ OString GetFixedFormula(ScConditionMode eMode, const ScAddress& rAddress, std::s
     OString aPos = aBuffer.makeStringAndClear();
     switch (eMode)
     {
-    case ScConditionMode::BeginsWith:
-        return OString("LEFT(" + aPos + ",LEN(" + rText + "))=\"" + rText + "\"");
-    case ScConditionMode::EndsWith:
-        return OString("RIGHT(" + aPos + ",LEN(" + rText + "))=\"" + rText + "\"");
-    case ScConditionMode::ContainsText:
-        return OString(OString::Concat("NOT(ISERROR(SEARCH(") + rText + "," + aPos + ")))");
-    case ScConditionMode::NotContainsText:
-        return OString(OString::Concat("ISERROR(SEARCH(") + rText + "," + aPos + "))");
-    default:
-        break;
+        case ScConditionMode::BeginsWith:
+            return OString("LEFT(" + aPos + ",LEN(" + rText + "))=" + rText);
+        case ScConditionMode::EndsWith:
+            return OString("RIGHT(" + aPos + ",LEN(" + rText + "))=" + rText);
+        case ScConditionMode::ContainsText:
+            return OString(OString::Concat("NOT(ISERROR(SEARCH(") + rText + "," + aPos + ")))");
+        case ScConditionMode::NotContainsText:
+            return OString(OString::Concat("ISERROR(SEARCH(") + rText + "," + aPos + "))");
+        default:
+            break;
     }
 
     return "";
