9165d5f834a6c831e8683a430bf5911eb4316b68
Tibor Nagy <nagy.tibor2@nisz.hu>
2020-12-29 08:36:49 +0100

    tdf#139394 XLSX export: remove extra quotation marks
    This fixes commit 583e2bfba2d72ac8afe7261c23f380daf5486889
    (tdf#139021 XLSX export: fix "contains" conditional formatting).
    
    Change-Id: Idae3190b5f87dac551251da19c511f501bdaaa21
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/108452
    Tested-by: László Németh <nemeth@numbertext.org>
    Reviewed-by: László Németh <nemeth@numbertext.org>
    Reviewed-by: Eike Rathke <erack@redhat.com>

--- sc/qa/unit/subsequent_export-test.cxx
+++ sc/qa/unit/subsequent_export-test.cxx
@@ -89,6 +89,7 @@ public:
     virtual void tearDown() override;
 
     void test();
+    void testTdf139394();
     void testExtCondFormatXLSX();
     void testTdf90104();
     void testTdf111876();
@@ -280,6 +281,7 @@ public:
 
     CPPUNIT_TEST_SUITE(ScExportTest);
     CPPUNIT_TEST(test);
+    CPPUNIT_TEST(testTdf139394);
     CPPUNIT_TEST(testExtCondFormatXLSX);
     CPPUNIT_TEST(testTdf90104);
     CPPUNIT_TEST(testTdf111876);
@@ -516,6 +518,31 @@ void ScExportTest::test()
     ScDocument& rLoadedDoc = xDocSh->GetDocument();
     double aVal = rLoadedDoc.GetValue(0,0,0);
     ASSERT_DOUBLES_EQUAL(aVal, 1.0);
+    xDocSh->DoClose();
+}
+
+void ScExportTest::testTdf139394()
+{
+    ScDocShellRef xShell = loadDoc(u"tdf139394.", FORMAT_XLSX);
+    CPPUNIT_ASSERT(xShell.is());
+
+    ScDocShellRef xDocSh = saveAndReload(&(*xShell), FORMAT_XLSX);
+    CPPUNIT_ASSERT(xDocSh.is());
+
+    std::shared_ptr<utl::TempFile> pXPathFile = ScBootstrapFixture::exportTo(&(*xDocSh), FORMAT_XLSX);
+    xmlDocUniquePtr pDoc = XPathHelper::parseExport(pXPathFile, m_xSFactory, "xl/worksheets/sheet1.xml");
+    CPPUNIT_ASSERT(pDoc);
+
+    assertXPathContent(pDoc,
+                "/x:worksheet/x:extLst/x:ext/x14:conditionalFormattings/x14:conditionalFormatting[1]/"
+                "x14:cfRule/xm:f", "LEFT(A1,LEN(\"+\"))=\"+\"");
+    assertXPathContent(pDoc,
+                "/x:worksheet/x:extLst/x:ext/x14:conditionalFormattings/x14:conditionalFormatting[2]/"
+                "x14:cfRule/xm:f", "RIGHT(A2,LEN(\"-\"))=\"-\"");
+    assertXPathContent(pDoc,
+                "/x:worksheet/x:extLst/x:ext/x14:conditionalFormattings/x14:conditionalFormatting[3]/"
+                "x14:cfRule/xm:f", "LEFT(A3,LEN($B$3))=$B$3");
+
     xDocSh->DoClose();
 }
 
