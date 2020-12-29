7bffe46aa7e42d9e323bd1c08c43fa4ab7382824
Stephan Bergmann <sbergman@redhat.com>
2020-12-29 10:56:48 +0100

    external/cairo: Fix mask usage in image-compositor
    Change-Id: Id3d8e4715e295290e07146ef06898b313ead57a5
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/108449
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>
    (cherry picked from commit c767e91c725b504db08e84b1fd25b11bfc33a4cc)

--- external/cairo/UnpackedTarball_cairo.mk
+++ external/cairo/UnpackedTarball_cairo.mk
@@ -14,6 +14,7 @@ $(eval $(call gb_UnpackedTarball_set_tarball,cairo,$(CAIRO_TARBALL),,cairo))
 $(eval $(call gb_UnpackedTarball_add_patches,cairo,\
 	external/cairo/cairo/cairo-1.10.2.patch \
 	external/cairo/cairo/cairo-libtool-rpath.patch.1 \
+	external/cairo/cairo/0001-Fix-mask-usage-in-image-compositor.patch.1 \
 ))
 
 ifeq ($(OS),iOS)
