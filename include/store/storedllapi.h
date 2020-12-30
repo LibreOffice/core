d732a0f2a559088ecba5679a2504b6efe3dfff4b
princesinghtomar <singhtomarprince470@gmail.com>
2020-12-30 18:05:55 +0530

    tdf#124176 Use pragma once instead of include guards
    Change-Id: I44a269b5c01f044880b07aaa6d7e9ee369d98f54

--- include/store/storedllapi.h
+++ include/store/storedllapi.h
@@ -16,8 +16,7 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
-#ifndef INCLUDED_STORE_STOREDLLAPI_H
-#define INCLUDED_STORE_STOREDLLAPI_H
+#pragma once
 
 #include <sal/types.h>
 
@@ -26,7 +25,5 @@
 #else
 #define STORE_DLLPUBLIC SAL_DLLPUBLIC_IMPORT
 #endif
-
-#endif // INCLUDED_STORE_STOREDLLAPI_H
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
