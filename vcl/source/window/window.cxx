69edaac2eefb04e9d144aaae123a18f4948d9e44
Szymon Kłos <szymon.klos@collabora.com>
2020-12-30 10:05:26 +0100

    Revert "jsdialog: disable idle notify"
    This reverts commit 71ec214d7583f637fefcb5eca13c637cc6b38029.
    and partially 54b5e6b4e083ce7a71e16ef622753898f38dc8ab
    
    freeze/thaw functionality was added so no longer needed.
    
    Change-Id: Iee29fa79bcebfd409fd3cea4f11cb1d48053daa6

--- vcl/source/window/window.cxx
+++ vcl/source/window/window.cxx
@@ -755,8 +755,7 @@ ImplWinData::ImplWinData() :
     mnTrackFlags(ShowTrackFlags::NONE),
     mnIsTopWindow(sal_uInt16(~0)), // not initialized yet, 0/1 will indicate TopWindow (see IsTopWindow())
     mbMouseOver(false),
-    mbEnableNativeWidget(false),
-    mbDisableIdleNotify(false)
+    mbEnableNativeWidget(false)
 {
 }
 
@@ -3200,17 +3199,6 @@ VclPtr<Window> Window::FindLOKWindow(vcl::LOKWindowId nWindowId)
         return it->second;
 
     return VclPtr<Window>();
-}
-
-bool Window::IsDisableIdleNotify()
-{
-    return ImplGetWinData()->mbDisableIdleNotify;
-}
-
-void Window::SetDisableIdleNotify(bool bValue)
-{
-    if (ImplGetWinData()->mbDisableIdleNotify != bValue)
-        ImplGetWinData()->mbDisableIdleNotify = bValue;
 }
 
 bool Window::IsLOKWindowsEmpty()
