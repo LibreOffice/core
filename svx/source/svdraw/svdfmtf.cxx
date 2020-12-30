cc0c5338904bd51f6cf0e1b8c2fa56677e5028ee
Noel Grandin <noelgrandin@gmail.com>
2020-12-30 09:30:07 +0200

    use more NbcRotate utility method
    Change-Id: I74330033c1540c4ed8633dd176a88ed5cacb7c6b

--- svx/source/svdraw/svdfmtf.cxx
+++ svx/source/svdraw/svdfmtf.cxx
@@ -1057,10 +1057,7 @@ void ImpSdrGDIMetaFileImport::ImportText( const Point& rPos, const OUString& rSt
     if ( nAngle )
     {
         nAngle*=10;
-        double a = nAngle * F_PI18000;
-        double nSin=sin(a);
-        double nCos=cos(a);
-        pText->NbcRotate(aPos,nAngle,nSin,nCos);
+        pText->SdrAttrObj::NbcRotate(aPos,nAngle);
     }
     InsertObj( pText, false );
 }
