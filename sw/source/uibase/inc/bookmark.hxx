6d4d6e0dadc18b03d0770741553477836d09fb16
Vishwas <19ucc001@lnmiit.ac.in>
2020-12-31 18:32:23 +0530

    tdf#124176: Use pragma once instead of include guards
    Change-Id: I346da72b15d21f09440f65f2c4f832115267c5e5

--- sw/source/uibase/inc/bookmark.hxx
+++ sw/source/uibase/inc/bookmark.hxx
@@ -16,8 +16,7 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
-#ifndef INCLUDED_SW_SOURCE_UIBASE_INC_BOOKMARK_HXX
-#define INCLUDED_SW_SOURCE_UIBASE_INC_BOOKMARK_HXX
+#pragma once
 
 #include <sfx2/basedlgs.hxx>
 #include <vcl/weld.hxx>
@@ -104,7 +103,5 @@ public:
     SwInsertBookmarkDlg(weld::Window* pParent, SwWrtShell& rSh);
     virtual ~SwInsertBookmarkDlg() override;
 };
-
-#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
