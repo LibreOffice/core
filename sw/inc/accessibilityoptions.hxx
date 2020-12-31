9610e33bdf593ec356e1a4611adeee03dc38c6a6
Alchemist <rhithick02@gmail.com>
2020-12-31 18:04:12 +0530

    tdf#124176 Use pragma once instead of include guards
    Change-Id: I491742c51ddc7c97f02b148dfb81d9ba820a759e

--- sw/inc/accessibilityoptions.hxx
+++ sw/inc/accessibilityoptions.hxx
@@ -16,8 +16,7 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
-#ifndef INCLUDED_SW_INC_ACCESSIBILITYOPTIONS_HXX
-#define INCLUDED_SW_INC_ACCESSIBILITYOPTIONS_HXX
+#pragma once
 
 struct SwAccessibilityOptions
 {
@@ -34,6 +33,5 @@ struct SwAccessibilityOptions
     bool IsStopAnimatedGraphics() const       { return bIsStopAnimatedGraphics;}
     void SetStopAnimatedGraphics( bool b )    { bIsStopAnimatedGraphics = b; }
 };
-#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
