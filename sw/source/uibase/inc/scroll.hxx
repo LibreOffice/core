e245217bf4abd70b7395061190557c2b7e6ec952
Vishwas <19ucc001@lnmiit.ac.in>
2020-12-31 18:32:23 +0530

    tdf#124176: Use pragma once instead of include guards
    Change-Id: I346da72b15d21f09440f65f2c4f832115267c5e5
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/108552
    Reviewed-by: Julien Nabet <serval2412@yahoo.fr>
    Tested-by: Julien Nabet <serval2412@yahoo.fr>

--- sw/source/uibase/inc/scroll.hxx
+++ sw/source/uibase/inc/scroll.hxx
@@ -16,8 +16,8 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
-#ifndef INCLUDED_SW_SOURCE_UIBASE_INC_SCROLL_HXX
-#define INCLUDED_SW_SOURCE_UIBASE_INC_SCROLL_HXX
+#pragma once
+
 #include <vcl/scrbar.hxx>
 
 class SwScrollbar: public ScrollBar
@@ -51,7 +51,5 @@ public:
     SwScrollbar(vcl::Window *pParent, bool bHori );
     virtual ~SwScrollbar() override;
 };
-
-#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
