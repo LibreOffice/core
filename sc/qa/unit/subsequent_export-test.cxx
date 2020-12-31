aa23da6b9e935fbfcb0b432d43c72f7a6db50657
Szabolcs Toth <toth.szabolcs@nisz.hu>
2020-12-31 10:31:30 +0100

    tdf#139258 XLSX export: fix position of rotated images
    Follow-up of commit a4eec60c388cc65ae0b4c8ea0fd7235f520a749d
    (tdf#123613 XLSX export: fix position of rotated shapes) and
    commit 368c56144aab5794c39d5bc2082d9b3d6d7cebdb (refactor
    for xlsx shape export).
    
    Change-Id: If5a79685efa1a80ea82eac19aed12bb426d10987
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/108533
    Tested-by: László Németh <nemeth@numbertext.org>
    Reviewed-by: László Németh <nemeth@numbertext.org>
    (cherry picked from commit fddad2ed797f1773ed5be979a0b05d3f976b744e)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/109517
    Tested-by: Jenkins
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- sc/qa/unit/subsequent_export-test.cxx
+++ sc/qa/unit/subsequent_export-test.cxx
@@ -278,6 +278,7 @@ public:
     void testTdf129969();
     void testTdf84874();
     void testTdf136721_paper_size();
+    void testTdf139258_rotated_image();
 
     CPPUNIT_TEST_SUITE(ScExportTest);
     CPPUNIT_TEST(test);
@@ -454,6 +455,7 @@ public:
     CPPUNIT_TEST(testTdf129969);
     CPPUNIT_TEST(testTdf84874);
     CPPUNIT_TEST(testTdf136721_paper_size);
+    CPPUNIT_TEST(testTdf139258_rotated_image);
 
     CPPUNIT_TEST_SUITE_END();
 
@@ -5776,6 +5778,25 @@ void ScExportTest::testTdf136721_paper_size()
     CPPUNIT_ASSERT(pDoc);
 
     assertXPath(pDoc, "/x:worksheet/x:pageSetup", "paperSize", "70");
+}
+
+void ScExportTest::testTdf139258_rotated_image()
+{
+    // Check that the topleft position of the image is correct.
+    ScDocShellRef xShell = loadDoc(u"tdf139258_rotated_image.", FORMAT_ODS);
+    CPPUNIT_ASSERT(xShell.is());
+
+    std::shared_ptr<utl::TempFile> pXPathFile
+        = ScBootstrapFixture::exportTo(&(*xShell), FORMAT_XLSX);
+
+    xmlDocUniquePtr pDrawing
+        = XPathHelper::parseExport(pXPathFile, m_xSFactory, "xl/drawings/drawing1.xml");
+    CPPUNIT_ASSERT(pDrawing);
+
+    assertXPathContent(pDrawing, "/xdr:wsDr/xdr:twoCellAnchor/xdr:from/xdr:col", "1");
+    assertXPathContent(pDrawing, "/xdr:wsDr/xdr:twoCellAnchor/xdr:from/xdr:row", "12");
+    assertXPathContent(pDrawing, "/xdr:wsDr/xdr:twoCellAnchor/xdr:to/xdr:col", "6");
+    assertXPathContent(pDrawing, "/xdr:wsDr/xdr:twoCellAnchor/xdr:to/xdr:row", "25");
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(ScExportTest);
