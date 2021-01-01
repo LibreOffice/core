1a943a2380cd905b085774a1bb88cc2d4f45b17f
Vishwas <19ucc001@lnmiit.ac.in>
2021-01-01 12:30:02 +0530

    tdf#124176: Use pragma once instead of include guards
    Change-Id: I346da72b15d21f09440f65f2c4f832115267c5e5

--- sw/source/uibase/inc/wtextsh.hxx
+++ sw/source/uibase/inc/wtextsh.hxx
@@ -16,9 +16,7 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
-
-#ifndef INCLUDED_SW_SOURCE_UIBASE_INC_WTEXTSH_HXX
-#define INCLUDED_SW_SOURCE_UIBASE_INC_WTEXTSH_HXX
+#pragma once
 
 #include "textsh.hxx"
 
@@ -35,7 +33,5 @@ public:
     SwWebTextShell(SwView& rView);
     virtual ~SwWebTextShell() override;
 };
-
-#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
