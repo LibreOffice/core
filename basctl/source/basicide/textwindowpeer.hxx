ffa0db687474e631c9db3e998b8dc5ab0f990dbc
csegura <carlossegurag@gmail.com>
2021-01-03 13:36:12 -0600

    tdf#124176: Use pragma once instead of include guards
    pragma once instead of include guards in textwindowpeer.hxx
    
    Change-Id: Ie0be2626863dc6a570c2146cb86bb0f1c548e91a

--- basctl/source/basicide/textwindowpeer.hxx
+++ basctl/source/basicide/textwindowpeer.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_SVTOOLS_TEXTWINDOWPEER_HXX
-#define INCLUDED_SVTOOLS_TEXTWINDOWPEER_HXX
+#pragma once
 
 #include <sal/config.h>
 
@@ -34,7 +33,5 @@ namespace basctl
 {
 css::uno::Reference<css::awt::XWindowPeer> createTextWindowPeer(TextView& view);
 }
-
-#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
