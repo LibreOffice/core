12dbd2fb11a17324c06eb5038c4c6b44bebb0f08
dante <dante19031999@gmail.com>
2021-01-04 17:56:09 +0100

    Mathml mo operator list
    This is a preparation for further improvements on starmayh.
    It is related with tdf#66279.
    
    Change-Id: Ife2a9c3efb5b82cf840739e9669f74f92d39cb93

--- starmath/mathml/inc/mathmlMo.hxx
+++ starmath/mathml/inc/mathmlMo.hxx
@@ -27,39 +27,51 @@
 #ifndef MATH_MO_HXX
 #define MATH_MO_HXX
 
+#include <sal/types.h>
+#include <rtl/ustring.hxx>
+#include <vector>
+
 // https://www.w3.org/TR/MathML3/appendixc.html
 
 enum moOpDP
 { // moOperatorDataProperty
-    sal_uInt16 none = 0x0000; sal_uInt16 accent = 0x0001; sal_uInt16 fence = 0x0002;
-    sal_uInt16 stretchy = 0x0004;
-    sal_uInt16 symmetric = 0x0008;
-    sal_uInt16 separator = 0x0010;
-    sal_uInt16 linebreakstyleAfter = 0x0020;
-    sal_uInt16 largeop = 0x0080;
-    sal_uInt16 movablelimits = 0x0100;
-    sal_uInt16 starmathCustom = 0x0200;
+    nonedp = 0x0000,
+    accent = 0x0001,
+    fence = 0x0002,
+    stretchy = 0x0004,
+    symmetric = 0x0008,
+    separator = 0x0010,
+    linebreakstyleAfter = 0x0020,
+    largeop = 0x0080,
+    movablelimits = 0x0100,
+    starmathCustom = 0x0200
 };
 
 enum moOpDF
 { // moOperatorDataForm
-    sal_uInt8 none = 0x00; sal_uInt8 prefix = 0x01; sal_uInt8 infix = 0x02;
-    sal_uInt8 postfix = 0x04;
-}
+    nonedf = 0x0000,
+    prefix = 0x0001,
+    infix = 0x0002,
+    postfix = 0x0004
+};
 
 struct moOperatorData
 {
     OUString m_motxt;
     sal_uInt16 m_pripority;
-    sal_uInt8 m_form;
+    sal_uInt16 m_form;
     sal_uInt16 m_lspace;
     sal_uInt16 m_rspace;
-    sal_uInt8 m_properties;
+    sal_uInt16 m_properties;
 
-    moOperatorData(OUString motxt, sal_uInt16 pripority, sal_uInt8 form, sal_uInt16 lspace,
-                   sal_uInt16 rspace, sal_uInt8 properties = moOpDP::none),
-        m_motxt(motxt), m_pripority(pripority), m_form(form), m_lspace(lspace), m_rspace(rspace),
-        m_properties(properties)
+    moOperatorData(OUString motxt, sal_uInt16 pripority, sal_uInt16 form, sal_uInt16 lspace,
+                   sal_uInt16 rspace, sal_uInt16 properties = moOpDP::nonedp)
+        : m_motxt(motxt)
+        , m_pripority(pripority)
+        , m_form(form)
+        , m_lspace(lspace)
+        , m_rspace(rspace)
+        , m_properties(properties)
     {
     }
 };
