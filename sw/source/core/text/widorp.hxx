0803ea6024eaa9199c3890a464f63c44be0dafaf
Suhaas Joshi <joshiesuhaas@gmail.com>
2021-01-04 22:23:50 +0530

    tdf#124176 : changed include guard to pragma once
    changed include guard to pragma once in 2 header files
    
    Change-Id: I9eb44c16fef1da8a1931e60857912887282bff59

--- sw/source/core/text/widorp.hxx
+++ sw/source/core/text/widorp.hxx
@@ -16,8 +16,9 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
-#ifndef INCLUDED_SW_SOURCE_CORE_TEXT_WIDORP_HXX
-#define INCLUDED_SW_SOURCE_CORE_TEXT_WIDORP_HXX
+
+#pragma once
+
 class SwTextFrame;
 
 #include <swtypes.hxx>
@@ -26,54 +27,56 @@ class SwTextFrame;
 class SwTextFrameBreak
 {
 private:
-    SwTwips  m_nRstHeight;
-    SwTwips  m_nOrigin;
+    SwTwips m_nRstHeight;
+    SwTwips m_nOrigin;
+
 protected:
-    SwTextFrame *m_pFrame;
-    bool     m_bBreak;
-    bool     m_bKeep;
+    SwTextFrame* m_pFrame;
+    bool m_bBreak;
+    bool m_bKeep;
+
 public:
-    SwTextFrameBreak( SwTextFrame *pFrame, const SwTwips nRst = 0  );
-    bool IsBreakNow( SwTextMargin &rLine );
+    SwTextFrameBreak(SwTextFrame* pFrame, const SwTwips nRst = 0);
+    bool IsBreakNow(SwTextMargin& rLine);
     bool IsKeepAlways() const { return m_bKeep; }
 
-    void SetKeep( const bool bNew ) { m_bKeep = bNew; }
+    void SetKeep(const bool bNew) { m_bKeep = bNew; }
 
-    bool IsInside( SwTextMargin const &rLine ) const;
+    bool IsInside(SwTextMargin const& rLine) const;
 
     // In order to be able to handle special cases with Footnote.
     // SetRstHeight sets the rest height for SwTextFrameBreak. This is needed
     // to call TruncLines() without IsBreakNow() returning another value.
     // We assume that rLine is pointing to the last non-fitting line.
 
-    void SetRstHeight( const SwTextMargin &rLine );
+    void SetRstHeight(const SwTextMargin& rLine);
 };
 
 class WidowsAndOrphans : public SwTextFrameBreak
 {
 private:
-    sal_uInt16   m_nWidLines, m_nOrphLines;
+    sal_uInt16 m_nWidLines, m_nOrphLines;
 
 public:
-    WidowsAndOrphans( SwTextFrame *pFrame, const SwTwips nRst = 0,
-        bool bCheckKeep = true );
-    bool FindWidows( SwTextFrame *pFrame, SwTextMargin &rLine );
-    sal_uInt16 GetOrphansLines() const
-    { return m_nOrphLines; }
-    void ClrOrphLines(){ m_nOrphLines = 0; }
+    WidowsAndOrphans(SwTextFrame* pFrame, const SwTwips nRst = 0, bool bCheckKeep = true);
+    bool FindWidows(SwTextFrame* pFrame, SwTextMargin& rLine);
+    sal_uInt16 GetOrphansLines() const { return m_nOrphLines; }
+    void ClrOrphLines() { m_nOrphLines = 0; }
 
-    bool FindBreak( SwTextFrame *pFrame, SwTextMargin &rLine, bool bHasToFit );
-    bool WouldFit( SwTextMargin &rLine, SwTwips &rMaxHeight, bool bTest );
+    bool FindBreak(SwTextFrame* pFrame, SwTextMargin& rLine, bool bHasToFit);
+    bool WouldFit(SwTextMargin& rLine, SwTwips& rMaxHeight, bool bTest);
     // i#16128 - This method is named this way to avoid confusion with
     // base class method <SwTextFrameBreak::IsBreakNow>, which isn't virtual.
-    bool IsBreakNowWidAndOrp( SwTextMargin &rLine )
+    bool IsBreakNowWidAndOrp(SwTextMargin& rLine)
     {
         bool isOnFirstLine = (rLine.GetLineNr() == 1 && !rLine.GetPrev());
-        if ( isOnFirstLine && rLine.GetCurr()->IsDummy()) {
-            return IsBreakNow( rLine );
+        if (isOnFirstLine && rLine.GetCurr()->IsDummy())
+        {
+            return IsBreakNow(rLine);
         }
-        if ( rLine.GetLineNr() > m_nOrphLines ) {
-            return IsBreakNow( rLine );
+        if (rLine.GetLineNr() > m_nOrphLines)
+        {
+            return IsBreakNow(rLine);
         }
         return false;
     }
