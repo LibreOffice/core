b868a8dcbe686365490b00247e2dc2361228d259
Suhaas Joshi <joshisuhaas22@gmail.com>
2021-01-04 22:23:50 +0530

    tdf#124176 : changed include guard to pragma once
    changed include guard to pragma once in two header files
    
    Change-Id: I9eb44c16fef1da8a1931e60857912887282bff59

--- sw/source/core/text/porref.hxx
+++ sw/source/core/text/porref.hxx
@@ -16,8 +16,8 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
-#ifndef INCLUDED_SW_SOURCE_CORE_TEXT_PORREF_HXX
-#define INCLUDED_SW_SOURCE_CORE_TEXT_PORREF_HXX
+
+#pragma once
 
 #include "portxt.hxx"
 
