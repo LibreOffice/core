c20ac51dfb4a74a8951b4207fbf9b2ed9c6ef5dc
Suhaas Joshi <joshiesuhaas@gmail.com>
2021-01-05 16:10:54 +0530

    tdf#124176 Use pragma once in sw/source/core/text
    Change-Id: I2104cad267e6f704f9389b03ff3f116fca875517
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/108796
    Tested-by: Jenkins
    Reviewed-by: Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>

--- sw/source/core/text/itratr.hxx
+++ sw/source/core/text/itratr.hxx
@@ -16,8 +16,8 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
-#ifndef INCLUDED_SW_SOURCE_CORE_TEXT_ITRATR_HXX
-#define INCLUDED_SW_SOURCE_CORE_TEXT_ITRATR_HXX
+#pragma once
+
 #include <o3tl/deleter.hxx>
 #include "atrhndl.hxx"
 #include <swfont.hxx>
@@ -108,7 +108,5 @@ public:
 
     SwAttrHandler& GetAttrHandler() { return m_aAttrHandler; }
 };
-
-#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
