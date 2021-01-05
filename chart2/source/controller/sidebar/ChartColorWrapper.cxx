8c4563e7fb4671febf0e85b913020b78b23511da
Szymon Kłos <szymon.klos@collabora.com>
2021-01-05 10:36:31 +0100

    lok: send chart color updates
    Change-Id: I037049fb19ddf4c1450d7d9327c574cb4e37977c

--- chart2/source/controller/sidebar/ChartColorWrapper.cxx
+++ chart2/source/controller/sidebar/ChartColorWrapper.cxx
@@ -16,6 +16,10 @@
 
 #include <svx/tbcontrl.hxx>
 
+#include <comphelper/lok.hxx>
+#include <sfx2/viewsh.hxx>
+#include <LibreOfficeKit/LibreOfficeKitEnums.h>
+
 namespace chart { namespace sidebar {
 
 namespace {
@@ -111,6 +115,16 @@ void ChartColorWrapper::updateData()
     aEvent.IsEnabled = true;
     aEvent.State = xPropSet->getPropertyValue(maPropertyName);
     mpControl->statusChanged(aEvent);
+
+    SfxViewShell* pViewShell = SfxViewShell::Current();
+    if (comphelper::LibreOfficeKit::isActive() && pViewShell && (maPropertyName == aLineColor))
+    {
+        std::string sCommand = OUStringToOString(aUrl.Complete, RTL_TEXTENCODING_ASCII_US).getStr();
+        sal_uInt32 nColor = -1;
+        aEvent.State >>= nColor;
+        pViewShell->libreOfficeKitViewCallback(LOK_CALLBACK_STATE_CHANGED,
+                                               (sCommand + "=" + std::to_string(nColor)).c_str());
+    }
 }
 
 } }
