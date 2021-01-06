7d39eb1cb3cc34812ccefc239e1a177892a3edd8
Dennis Francis <dennis.francis@collabora.com>
2021-01-06 17:44:00 +0530

    tdf#133858 reduce the double-ref range to data content
    in certain matrix formulas like SUM(IF(A1=G:G, H:H)*B1/B2) where whole
    columns are used for comparison in the condition of IF ultimately
    followed by a reducer like SUM. In such cases we can safely reduce the
    double-refs involved in the comparison to the sheet area where there is
    data before converting the data to ScMatrix.
    
    This is a more restricted version of Noel's fix in
    37ffe509ef011357123642577c04ff296d59ce68
    
    Change-Id: I1c2e8985adedb3f4c4648f541fb0e8e7d0fae033
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/109050
    Reviewed-by: Luboš Luňák <l.lunak@collabora.com>
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

--- include/formula/FormulaCompiler.hxx
+++ include/formula/FormulaCompiler.hxx
@@ -337,6 +337,8 @@ protected:
     // Called from CompileTokenArray() after RPN code generation is done.
     virtual void PostProcessCode() {}
 
+    virtual void AnnotateOperands() {}
+
     OUString            aCorrectedFormula;      // autocorrected Formula
     OUString            aCorrectedSymbol;       // autocorrected Symbol
 
