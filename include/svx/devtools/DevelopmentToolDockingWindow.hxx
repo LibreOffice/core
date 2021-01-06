736c6ce923098736120646cc5ae4821df94a3bdc
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2021-01-06 15:58:13 +0900

    devtools: Add treeview with content of current object
    Change-Id: I8d6dd035e60a7521c404e23045fade7eae47fc6c

--- include/svx/devtools/DevelopmentToolDockingWindow.hxx
+++ include/svx/devtools/DevelopmentToolDockingWindow.hxx
@@ -29,11 +29,17 @@ class SAL_WARN_UNUSED SVX_DLLPUBLIC DevelopmentToolChildWindow final : public Sf
 
 class SAL_WARN_UNUSED SVX_DLLPUBLIC DevelopmentToolDockingWindow final : public SfxDockingWindow
 {
+private:
+    std::unique_ptr<weld::Label> mpClassNameLabel;
+    std::unique_ptr<weld::TreeView> mpClassListBox;
+
 public:
     DevelopmentToolDockingWindow(SfxBindings* pBindings, SfxChildWindow* pChildWindow,
                                  vcl::Window* pParent);
 
     virtual void ToggleFloatingMode() override;
+
+    void introspect(css::uno::Reference<css::uno::XInterface> const& xInterface);
 };
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
