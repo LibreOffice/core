414490ddba424b38ef1c627decdb4af8bbdbd717
Suhaas Joshi <joshiesuhaas@gmail.com>
2021-01-07 08:45:14 +0530

    tdf#124176 Use pragma once in sc/source/core/opencl
    Change-Id: I90253fdf9528116379cf06cb00a3342944e3351a
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/108918
    Tested-by: Jenkins
    Reviewed-by: Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>

--- sc/source/core/opencl/op_array.hxx
+++ sc/source/core/opencl/op_array.hxx
@@ -6,9 +6,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-
-#ifndef INCLUDED_SC_SOURCE_CORE_OPENCL_OP_ARRAY_HXX
-#define INCLUDED_SC_SOURCE_CORE_OPENCL_OP_ARRAY_HXX
+#pragma once
 
 #include "opbase.hxx"
 
@@ -38,7 +36,5 @@ public:
     virtual std::string BinFuncName() const override { return "SumXMY2"; }
 };
 }
-
-#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
