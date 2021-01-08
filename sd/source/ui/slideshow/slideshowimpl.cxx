LibreOffice/core

commit 3e0092031b73bad107e3122d5d4be2f5bd487744

tdf#112318 sd opengl: fix lack of initial animation
Commit 881043d1f01fad663c0a17f11cbe2dab79e31217 (#i98792# Allow frequent
calls to slide show update()., 2009-04-27) added a comment in
sd::SlideshowImpl::updateSlideShow() talking about an "above" case, when
the slideshow update suggests to not wait till the next update,
but later commit 12dcf5e6e770b1933252a1f919663ba45ded4cdf (slideshow:
cleanup main-loop usage, post-yield listeners, etc., 2015-11-13) removed
that handling.

That was fine till opengl rendering was introduced, which swaps its
backbuffer in an idle handler. Given that OpenGLFlushIdle's priority is
TaskPriority::POST_PAINT and SlideshowImpl::maUpdateTimer's priority is
TaskPriority::REPAINT, the GL backbuffer is not always painted while an
animation is ongoing.

This is more visible when tweaking the bugdoc to have an e.g. 5 sec
animation, then starting the slideshow results in waiting for 5 seconds
and only then the slide shows up, without an animation.

Fix the problem by re-introducing the handling of the fUpdate == 0, and
processing idle events there, which means the GL backbuffer will be
always painted, regardless if fUpdate is a small number or 0.

Change-Id: I25b2dc0aa41af62d9bd89617178eb2e9997f5b17
Reviewed-on: https://gerrit.libreoffice.org/70287
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

sd/source/ui/slideshow/slideshowimpl.cxx
@@ -74,6 +74,7 @@
 #include <vcl/canvastools.hxx>
 #include <vcl/commandinfoprovider.hxx>
 #include <vcl/settings.hxx>
+#include <vcl/scheduler.hxx>
 
 #include <comphelper/anytostring.hxx>
 #include <comphelper/processfactory.hxx>
@@ -1695,7 +1696,12 @@ void SlideshowImpl::updateSlideShow()
 
         if (mxShow.is() && (fUpdate >= 0.0))
         {
-            if (!::basegfx::fTools::equalZero(fUpdate))
+            if (::basegfx::fTools::equalZero(fUpdate))
+            {
+                // Make sure idle tasks don't starve when we don't have to wait.
+                Scheduler::ProcessEventsToIdle();
+            }
+            else
             {
                 // Avoid busy loop when the previous call to update()
                 // returns a small positive number but not 0 (which is

commit 0f160caa1aca9f339ec1622ab558ee1180d92f8f

tdf#128353 sd slideshow: fix handling of animgifs on slower machines
Regression from commit 3e0092031b73bad107e3122d5d4be2f5bd487744
(tdf#112318 sd opengl: fix lack of initial animation, 2019-04-05), the
problem was that in case a timer adds itself to the end of the scheduler
queue during Scheduler::ProcessEventsToIdle(), then the idle trigger in
SlideshowImpl::updateSlideShow() will never finish.

The intention of invoking idles is to help vcl backends which do the
actual painting in an idle, so actually it's enough to only process
tasks which are already scheduled. That is enough for skia/gtk3/etc and
avoids the problem with the animgifs.

This is only a problem in debug builds or on slower machines: the
animgif from the bugdoc wants to play a frame every 40ms, so in case
processing the current tasks finishes faster than that, then the problem
was not visible.

Change-Id: I7487cb9a79e7e5acc0b816eb4744a04559ae26ce

sd/source/ui/slideshow/slideshowimpl.cxx
@@ -1678,7 +1678,8 @@ void SlideshowImpl::updateSlideShow()
             if (::basegfx::fTools::equalZero(fUpdate))
             {
                 // Make sure idle tasks don't starve when we don't have to wait.
-                Scheduler::ProcessEventsToIdle();
+                // Don't process any events generated after invoking the function.
+                Application::Reschedule(/*bHandleAllCurrentEvents=*/true);
             }
             else
             {