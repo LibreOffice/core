48c8ce94fa706469b7aad45eba52c94f1e1f07d1
Jan-Marek Glogowski <glogow@fbihome.de>
2021-01-08 12:37:06 +0100

    WASM use Qt qtloader.js and html template
    The emscripten generated one doesn't work, probably because of
    qtloader.js.
    
    Change-Id: I402d20d0175130ce3b26a9d998661fd66f5806a1

--- solenv/gbuild/platform/unxgcc.mk
+++ solenv/gbuild/platform/unxgcc.mk
@@ -156,6 +156,9 @@ $(call gb_Helper_abbreviate_dirs,\
 			>> $(WORKDIR)/LinkTarget/$(2).exports.tmp && \
 		$(call gb_Helper_replace_if_different_and_touch,$(WORKDIR)/LinkTarget/$(2).exports.tmp, \
 			$(WORKDIR)/LinkTarget/$(2).exports,$(1))))
+	$(if $(and $(filter CppunitTest Executable,$(TARGETTYPE)),$(filter EMSCRIPTEN,$(OS)),$(filter TRUE,$(ENABLE_QT5))), \
+		cp $(QT5_PLATFORMS_SRCDIR)/qtlogo.svg $(QT5_PLATFORMS_SRCDIR)/qtloader.js $(dir $(1)) ; \
+		sed -e 's/@APPNAME@/$(subst $(gb_Executable_EXT),,$(notdir $(1)))/' $(QT5_PLATFORMS_SRCDIR)/wasm_shell.html > $(1))
 endef
 
 define gb_LinkTarget__command_staticlink
