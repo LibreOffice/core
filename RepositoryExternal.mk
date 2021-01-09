4f2a76662f638f80b2199e62beed20496045d386
Rene Engelhard <rene@rene-engelhard.de>
2021-01-09 12:38:21 +0100

    define BOOST_BIND_GLOBAL_PLACEHOLDERS to silence warnings with newer boosts
    Change-Id: I2bb9d883c22e97b3112797f10ccb5e6b9c112dfd

--- RepositoryExternal.mk
+++ RepositoryExternal.mk
@@ -593,6 +593,10 @@ $(call gb_LinkTarget_add_ldflags,$(1),\
 
 $(call gb_LinkTarget_add_libs,$(1),$(2))
 
+$(call gb_LinkTarget_add_defs,$(1),\
+	-DBOOST_BIND_GLOBAL_PLACEHOLDERS \
+)
+
 endef
 
 define gb_LinkTarget__use_boost_locale
@@ -641,6 +645,7 @@ else # !SYSTEM_BOOST
 define gb_LinkTarget__use_boost_lib
 $(call gb_LinkTarget_add_defs,$(1),\
 	-DBOOST_ALL_NO_LIB \
+	-DBOOST_BIND_GLOBAL_PLACEHOLDERS \
 )
 
 $(call gb_LinkTarget_use_static_libraries,$(1),$(2))
