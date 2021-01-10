f2254f11e5e381b959dcaab53a7ddb101a83b561
anirudhS <iit2019068@iiita.ac.in>
2021-01-10 21:04:03 +0530

    tdf#124176 Use pragma once instead of include guards
    Change-Id: I609925d09086c764ae0360e08e0d74c8d4b7d484
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/109053
    Tested-by: Jenkins
    Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

--- connectivity/source/drivers/macab/MacabResultSetMetaData.hxx
+++ connectivity/source/drivers/macab/MacabResultSetMetaData.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_CONNECTIVITY_SOURCE_DRIVERS_MACAB_MACABRESULTSETMETADATA_HXX
-#define INCLUDED_CONNECTIVITY_SOURCE_DRIVERS_MACAB_MACABRESULTSETMETADATA_HXX
+#pragma once
 
 #include "MacabConnection.hxx"
 #include <connectivity/CommonTools.hxx>
@@ -78,7 +77,5 @@ namespace connectivity::macab
             virtual OUString SAL_CALL getColumnServiceName( sal_Int32 column ) override;
         };
 }
-
-#endif // INCLUDED_CONNECTIVITY_SOURCE_DRIVERS_MACAB_MACABRESULTSETMETADATA_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
