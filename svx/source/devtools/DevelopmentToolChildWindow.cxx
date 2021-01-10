998b4458752b3c44458d3c109703359d71c6a4ca
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2021-01-10 10:51:52 +0900

    devtools: move DevelopmentToolChildWindow to its own file
    Change-Id: I12326479afb5756729b62ff3676f995d16f64aa0

---
+++ svx/source/devtools/DevelopmentToolChildWindow.cxx
@@ -0,0 +1,30 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#include <svx/devtools/DevelopmentToolChildWindow.hxx>
+#include <svx/devtools/DevelopmentToolDockingWindow.hxx>
+
+#include <svx/svxids.hrc>
+
+SFX_IMPL_DOCKINGWINDOW_WITHID(DevelopmentToolChildWindow, SID_DEVELOPMENT_TOOLS_DOCKING_WINDOW);
+
+DevelopmentToolChildWindow::DevelopmentToolChildWindow(vcl::Window* pParentWindow, sal_uInt16 nId,
+                                                       SfxBindings* pBindings,
+                                                       SfxChildWinInfo* pInfo)
+    : SfxChildWindow(pParentWindow, nId)
+{
+    VclPtr<DevelopmentToolDockingWindow> pWin
+        = VclPtr<DevelopmentToolDockingWindow>::Create(pBindings, this, pParentWindow);
+    SetWindow(pWin);
+    SetAlignment(SfxChildAlignment::BOTTOM);
+    pWin->Initialize(pInfo);
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
