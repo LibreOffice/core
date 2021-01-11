1f252d20a80a514b07c8d0a4e6c394a547cf6806
ViKrAm-Baisclear <vikrambais02@gmail.com>
2021-01-11 15:05:50 +0530

    tdf#124176 used pragma once instead of include guards
    Change-Id: Idf57df70ac895915959e8d35e493c0e8b8db137f

--- starmath/inc/action.hxx
+++ starmath/inc/action.hxx
@@ -16,8 +16,7 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
-#ifndef INCLUDED_STARMATH_INC_ACTION_HXX
-#define INCLUDED_STARMATH_INC_ACTION_HXX
+#pragma once
 
 #include <svl/undo.hxx>
 #include "format.hxx"
@@ -38,7 +37,5 @@ public:
     virtual void Repeat(SfxRepeatTarget& rDocSh) override;
     virtual OUString GetComment() const override;
 };
-
-#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
