66562987ba163889128d2c9ed08f5e7d22f30092
Mike Kaganski <mike.kaganski@collabora.com>
2021-01-12 13:16:32 +0100

    Use comphelper::ContainerUniquePtrEquals here
    This restores correct comparison of pointed values, that was
    broken since commit df3e716b3c4313d9c495c64aa7d84082fb9596e7.
    Thanks Noel for investigation :-)
    
    Change-Id: I5614be004c0a98f5dc6f22febc3a51de08e75664

--- sfx2/source/dialog/dinfdlg.cxx
+++ sfx2/source/dialog/dinfdlg.cxx
@@ -28,6 +28,7 @@
 #include <unotools/localedatawrapper.hxx>
 #include <unotools/cmdoptions.hxx>
 #include <comphelper/processfactory.hxx>
+#include <comphelper/stl_types.hxx>
 #include <comphelper/xmlsechelper.hxx>
 #include <unotools/useroptions.hxx>
 #include <svtools/ctrlbox.hxx>
@@ -311,9 +312,7 @@ bool SfxDocumentInfoItem::operator==( const SfxPoolItem& rItem) const
          m_Keywords             == rInfoItem.m_Keywords          &&
          m_Subject              == rInfoItem.m_Subject           &&
          m_Title                == rInfoItem.m_Title             &&
-         m_aCustomProperties.size() == rInfoItem.m_aCustomProperties.size() &&
-         std::equal(m_aCustomProperties.begin(), m_aCustomProperties.end(),
-            rInfoItem.m_aCustomProperties.begin()) &&
+         comphelper::ContainerUniquePtrEquals(m_aCustomProperties, rInfoItem.m_aCustomProperties) &&
          m_aCmisProperties.getLength() == rInfoItem.m_aCmisProperties.getLength();
 }
 
