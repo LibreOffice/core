LibreOffice/core

commit 14a56533ff2c9c859d22cd3039ada75b99e94bc0

SmartArt Pyramid: Now lays out shapes
This algorithm was initially not implemented, because of that it was
not able to show shapes which relates to pyramid, but with this patch,
It rendered correctly except some minor adjustement values issues which
can be fixed in upcoming patches.

Change-Id: I298c812615956d67eb00e1b7544d7b171a4ac14a
Reviewed-on: https://gerrit.libreoffice.org/57241
Tested-by: Jenkins
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

oox/source/drawingml/diagram/diagramlayoutatoms.cxx
@@ -531,7 +531,38 @@ void AlgAtom::layoutShape( const ShapePtr& rShape,
         }
 
         case XML_pyra:
+        {
+            if (rShape->getChildren().empty() || rShape->getSize().Width == 0 || rShape->getSize().Height == 0)
+                break;
+
+            // const sal_Int32 nDir = maMap.count(XML_linDir) ? maMap.find(XML_linDir)->second : XML_fromT;
+            // const sal_Int32 npyraAcctPos = maMap.count(XML_pyraAcctPos) ? maMap.find(XML_pyraAcctPos)->second : XML_bef;
+            // const sal_Int32 ntxDir = maMap.count(XML_txDir) ? maMap.find(XML_txDir)->second : XML_fromT;
+            // const sal_Int32 npyraLvlNode = maMap.count(XML_pyraLvlNode) ? maMap.find(XML_pyraLvlNode)->second : XML_level;
+            // uncomment when use in code.
+
+            sal_Int32 nCount = rShape->getChildren().size();
+            double fAspectRatio = 0.32;
+
+            awt::Size aChildSize = rShape->getSize();
+                aChildSize.Width /= nCount;
+                aChildSize.Height /= nCount;
+
+            awt::Point aCurrPos(0, 0);
+                aCurrPos.X = fAspectRatio*aChildSize.Width*(nCount-1);
+                aCurrPos.Y = fAspectRatio*aChildSize.Height;
+
+            for (auto & aCurrShape : rShape->getChildren())
+            {
+                aCurrShape->setPosition(aCurrPos);
+                aCurrPos.X -=  aChildSize.Height/(nCount-1);
+                aChildSize.Width += aChildSize.Height;
+                aCurrShape->setSize(aChildSize);
+                aCurrShape->setChildSize(aChildSize);
+                aCurrPos.Y += (aChildSize.Height);
+            }
             break;
+        }
 
         case XML_snake:
         {

commit 405f0dad392fe6b5c6ade392a1c5e21f069f87df

oox smartart: fix crash in pyra algorithm with a single child shape
Regression from commit 14a56533ff2c9c859d22cd3039ada75b99e94bc0
(SmartArt Pyramid: Now lays out shapes, 2018-07-10), the added pyramid
algorithm by first centering the topmost children, then decrementing the
horizontal postion of each additional shape, with the end goal of having
0 horizontal position of the last children.

This means that simply avoiding the division in the 1-child case leads
to correct results, because in this case the only child is also the last
child at the sane time.

Change-Id: Ifd0027e9616b0909dbfde43e1555427b50de4dad

oox/source/drawingml/diagram/diagramlayoutatoms.cxx
@@ -1617,7 +1617,10 @@ void AlgAtom::layoutShape(const ShapePtr& rShape, const std::vector<Constraint>&
             for (auto & aCurrShape : rShape->getChildren())
             {
                 aCurrShape->setPosition(aCurrPos);
-                aCurrPos.X -=  aChildSize.Height/(nCount-1);
+                if (nCount > 1)
+                {
+                    aCurrPos.X -= aChildSize.Height / (nCount - 1);
+                }
                 aChildSize.Width += aChildSize.Height;
                 aCurrShape->setSize(aChildSize);
                 aCurrShape->setChildSize(aChildSize);

sd/qa/unit/import-tests-smartart.cxx
@@ -76,6 +76,7 @@ public:
     void testRotation();
     void testTextAutoRotation();
     void testPyramid();
+    void testPyramidOneChild();
     void testChevron();
     void testCycle();
     void testvenndiagram();
@@ -127,6 +128,7 @@ public:
     CPPUNIT_TEST(testRotation);
     CPPUNIT_TEST(testTextAutoRotation);
     CPPUNIT_TEST(testPyramid);
+    CPPUNIT_TEST(testPyramidOneChild);
     CPPUNIT_TEST(testChevron);
     CPPUNIT_TEST(testCycle);
     CPPUNIT_TEST(testHierarchy);
@@ -449,6 +451,19 @@ void SdImportTestSmartArt::testBasicProcess()
 void SdImportTestSmartArt::testPyramid()
 {
     //FIXME : so far this only introduce the test document, but the actual importer was not fixed yet.
+}
+
+void SdImportTestSmartArt::testPyramidOneChild()
+{
+    // Load a document with a pyra algorithm in it.
+    // Without the accompanying fix in place, this test would have crashed.
+    sd::DrawDocShellRef xDocShRef = loadURL(
+        m_directories.getURLFromSrc(u"sd/qa/unit/data/pptx/smartart-pyramid-1child.pptx"), PPTX);
+    uno::Reference<drawing::XShape> xGroup(getShapeFromPage(0, 0, xDocShRef), uno::UNO_QUERY);
+    uno::Reference<text::XTextRange> xText(getChildShape(getChildShape(xGroup, 1), 1),
+                                           uno::UNO_QUERY);
+    // Verify that the text of the only child is imported correctly.
+    CPPUNIT_ASSERT_EQUAL(OUString("A"), xText->getString());
 }
 
 void SdImportTestSmartArt::testChevron()