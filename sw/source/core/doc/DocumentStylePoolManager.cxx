6e4049dfe2a4ae8fe90b87c0c20371840011a219
Seth Chaiklin <sdc.blanco@youmail.dk>
2021-01-12 14:43:48 +0100

    tdf#128531 make Heading 3 font size 100%
    Change-Id: I39bec73d756c39fded6f47b45e9c0133da793b22

--- sw/source/core/doc/DocumentStylePoolManager.cxx
+++ sw/source/core/doc/DocumentStylePoolManager.cxx
@@ -100,7 +100,7 @@ namespace
 
     const sal_uInt16 aHeadlineSizes[ 2 * MAXLEVEL ] = {
         // we do everything percentual now:
-        130, 115, 101, 95, 85,
+        130, 115, 100, 95, 85,
         85,   80,  80, 75, 75,  // normal
         PT_24, PT_18, PT_14, PT_12, PT_10,
         PT_7, PT_7, PT_7, PT_7, PT_7            // HTML mode
