627d30ec436800c4454bd0005841feb3ff41824f
Michael Stahl <michael.stahl@allotropia.de>
2021-01-13 11:03:17 +0100

    tdf#135014 sw: fix crash exporting numbering to DOCX
    It can happen that the default SwNumRule of a SwList isn't used by
    anything directly, but there are other SwNumRule associated with that
    SwList and then the DOCX export needs to export it as an abstract
    numbering definition.
    
    (regression from 632ee9aae6d5f3cf08b6d6b2789310c20db713b7)
    
    Change-Id: I6b1851980464aaa95bf731a60b7d11ab91cec7b6

--- sw/inc/SwNumberTree.hxx
+++ sw/inc/SwNumberTree.hxx
@@ -327,6 +327,13 @@ public:
     void IsSane(bool bRecursive) const;
 #endif // DBG_UTIL
 
+    /**
+       Returns how many children this node has got.
+
+       @return number of children
+     */
+    tSwNumberTreeChildren::size_type GetChildCount() const;
+
 protected:
     /** the children */
     tSwNumberTreeChildren mChildren;
@@ -347,13 +354,6 @@ protected:
        @retval false  else
      */
     virtual bool IsNotificationEnabled() const = 0;
-
-    /**
-       Returns how many children this node has got.
-
-       @return number of children
-     */
-    tSwNumberTreeChildren::size_type GetChildCount() const;
 
     // #i64010# - made pure virtual
     virtual bool HasCountedChildren() const = 0;
