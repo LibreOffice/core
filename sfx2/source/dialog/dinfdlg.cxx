36c32807b4f8361e3410127d77ca95ee168cd740
Mike Kaganski <mike.kaganski@collabora.com>
2021-01-14 12:51:57 +0100

    Drop some unused "using" directives
    Change-Id: Ibc071f7b3d21c24e119fdc4e0db5351f80813287

--- sfx2/source/dialog/dinfdlg.cxx
+++ sfx2/source/dialog/dinfdlg.cxx
@@ -75,8 +75,6 @@
 #include <algorithm>
 
 using namespace ::com::sun::star;
-using namespace ::com::sun::star::lang;
-using namespace ::com::sun::star::ui::dialogs;
 using namespace ::com::sun::star::uno;
 
 struct CustomProperty
