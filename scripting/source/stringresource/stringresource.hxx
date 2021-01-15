d3e81d063dff023564dbbfd9c53582dbda813aa1
ViKrAm-Baisclear <vikrambais02@gmail.com>
2021-01-15 14:03:08 +0530

    Used pragma once instead of include guards
    Change-Id: I235462f3ce81816f3ce36c7423dd3e50fc03778a

--- scripting/source/stringresource/stringresource.hxx
+++ scripting/source/stringresource/stringresource.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_SCRIPTING_SOURCE_STRINGRESOURCE_STRINGRESOURCE_HXX
-#define INCLUDED_SCRIPTING_SOURCE_STRINGRESOURCE_STRINGRESOURCE_HXX
+#pragma once
 
 #include <com/sun/star/resource/XStringResourceWithStorage.hpp>
 #include <com/sun/star/resource/XStringResourceWithLocation.hpp>
@@ -492,6 +491,5 @@ public:
 }   // namespace stringtable
 
 
-#endif // INCLUDED_SCRIPTING_SOURCE_STRINGRESOURCE_STRINGRESOURCE_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
