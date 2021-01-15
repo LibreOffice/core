256b29e6b4d2c565074c44d67ffa7be1d1087eef
Caolán McNamara <caolanm@redhat.com>
2021-01-15 14:00:50 +0000

    tdf#135641 update DataBrowser has-focus when child widget gains focus
    Change-Id: Ic5e618b50c31ebeffa1b2cc59857582804874747

--- svtools/source/brwbox/ebbcontrols.cxx
+++ svtools/source/brwbox/ebbcontrols.cxx
@@ -30,6 +30,7 @@ namespace svt
         m_xWidget->set_entry_width_chars(1); // so a smaller than default width can be used
         m_xWidget->connect_changed(LINK(this, ComboBoxControl, SelectHdl));
         m_xWidget->connect_key_press(LINK(this, ControlBase, KeyInputHdl));
+        m_xWidget->connect_focus_in(LINK(this, ControlBase, FocusInHdl));
     }
 
     void ComboBoxControl::dispose()
@@ -115,6 +116,7 @@ namespace svt
         m_xWidget->set_size_request(42, -1); // so a later narrow size request can stick
         m_xWidget->connect_changed(LINK(this, ListBoxControl, SelectHdl));
         m_xWidget->connect_key_press(LINK(this, ControlBase, KeyInputHdl));
+        m_xWidget->connect_focus_in(LINK(this, ControlBase, FocusInHdl));
     }
 
     void ListBoxControl::dispose()
@@ -183,6 +185,7 @@ namespace svt
         m_aModeState.bTriStateEnabled = true;
         InitControlBase(m_xBox.get());
         m_xBox->connect_key_press(LINK(this, ControlBase, KeyInputHdl));
+        m_xBox->connect_focus_in(LINK(this, ControlBase, FocusInHdl));
         m_xBox->connect_toggled(LINK(this, CheckBoxControl, OnToggle));
     }
 
@@ -334,6 +337,7 @@ namespace svt
         m_pEntry->show();
         m_pEntry->set_width_chars(1); // so a smaller than default width can be used
         m_pEntry->connect_key_press(LINK(this, ControlBase, KeyInputHdl));
+        m_pEntry->connect_focus_in(LINK(this, ControlBase, FocusInHdl));
     }
 
     bool ControlBase::ProcessKey(const KeyEvent& rKEvt)
@@ -344,6 +348,11 @@ namespace svt
     IMPL_LINK(ControlBase, KeyInputHdl, const KeyEvent&, rKEvt, bool)
     {
         return ProcessKey(rKEvt);
+    }
+
+    IMPL_LINK_NOARG(ControlBase, FocusInHdl, weld::Widget&, void)
+    {
+        return static_cast<BrowserDataWin*>(GetParent())->GetParent()->ChildFocusIn();
     }
 
     void EditControlBase::dispose()
@@ -608,6 +617,7 @@ namespace svt
     {
         InitControlBase(m_xWidget.get());
         m_xWidget->connect_key_press(LINK(this, ControlBase, KeyInputHdl));
+        m_xWidget->connect_focus_in(LINK(this, ControlBase, FocusInHdl));
         // so any the natural size doesn't have an effect
         m_xWidget->set_size_request(1, 1);
     }
