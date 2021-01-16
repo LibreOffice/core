5f23c4db05cd26ef9fef3c3a93b3b5e81d00d905
Justin Luth <justin_luth@sil.org>
2021-01-16 12:12:04 +0300

    tdf#116394 writerfilter: append '=' if not a formula marker
    Oops - a silly mistake to throw away the character if
    it didn't match a special purpose.
    
    There is no point in adding a unit test for this.
    
    Change-Id: I3b48af578ae96744405ec0919ff341d1c9b43f65

--- writerfilter/source/dmapper/DomainMapper_Impl.cxx
+++ writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -3534,6 +3534,8 @@ static OUString lcl_ExtractToken(OUString const& rCommand,
                     ++rIndex;
                     return "FORMULA";
                 }
+                else
+                    token.append('=');
             break;
             default:
                 token.append(currentChar);
