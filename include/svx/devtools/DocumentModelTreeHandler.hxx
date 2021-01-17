f0ecca1a0cf0426fb0bc6b167ee5b410c548ed85
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2021-01-18 08:17:01 +0900

    devtools: handle the doc. model tree with attached obects
    Instead of filling the document model tree with fill* methods,
    refactor that to use objects that are attached (via get_id) to the
    tree nodes directly. For this introduce DocumentModelTreeEntry
    and subclasses, which implement what the current UNO object is
    that is being shown for a tree node and a "fill" virtual method,
    which is used to fill the child nodes of the tree when expanding
    a tree node.
    
    This makes the code much easier to work with and in addition it
    makes it possible to have all the tree nodes to fill the content
    on demand when expanded.
    
    Change-Id: Id5a027e060af90e483181439568f17d0172d1b35

--- include/svx/devtools/DocumentModelTreeHandler.hxx
+++ include/svx/devtools/DocumentModelTreeHandler.hxx
@@ -24,24 +24,7 @@ private:
     std::unique_ptr<weld::TreeView>& mpDocumentModelTree;
     css::uno::Reference<css::uno::XInterface> mxDocument;
 
-    std::unordered_map<OUString, css::uno::Reference<css::uno::XInterface>> maUnoObjectMap;
-
     void clearChildren(weld::TreeIter const& rParent);
-
-    void fillSheets(weld::TreeIter const& rParent);
-    void fillPages(weld::TreeIter const& rParent);
-    void fillSlides(weld::TreeIter const& rParent);
-    void fillMasterSlides(weld::TreeIter const& rParent);
-    void fillParagraphs(weld::TreeIter const& rParent);
-    void fillShapes(weld::TreeIter const& rParent);
-    void fillTables(weld::TreeIter const& rParent);
-    void fillFrames(weld::TreeIter const& rParent);
-    void fillGraphicObjects(weld::TreeIter const& rParent);
-    void fillOLEObjects(weld::TreeIter const& rParent);
-    void fillStyleFamilies(weld::TreeIter const& rParent);
-
-    void insertDocModelToParent(weld::TreeIter const& rParent, OUString const& rName,
-                                css::uno::Reference<css::uno::XInterface> const& rInterface);
 
 public:
     DocumentModelTreeHandler(std::unique_ptr<weld::TreeView>& pDocumentModelTree,
@@ -56,7 +39,9 @@ public:
     DECL_LINK(ExpandingHandler, const weld::TreeIter&, bool);
 
     void inspectDocument();
-    css::uno::Reference<css::uno::XInterface> getObjectByID(OUString const& rID);
+    static css::uno::Reference<css::uno::XInterface> getObjectByID(OUString const& rID);
+
+    void dispose();
 };
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
