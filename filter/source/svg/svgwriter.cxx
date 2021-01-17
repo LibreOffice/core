8a8773f7216678142d41ca0f39ffcd5155b63086
Marco Cecchetti <marco.cecchetti@collabora.com>
2021-01-17 23:38:57 +0100

    filter: svg: js engine: further improving text fields handling
    Added support for slide name text field on master page
    
    Change-Id: I969bd3b2d030cf117f4c7022716b55152538f846
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/109497
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Marco Cecchetti <marco.cecchetti@collabora.com>

--- filter/source/svg/svgwriter.cxx
+++ filter/source/svg/svgwriter.cxx
@@ -1183,7 +1183,8 @@ bool SVGTextWriter::nextTextPortion()
                             }
                         }
                         if( sFieldName == "DateTime" || sFieldName == "Header"
-                                || sFieldName == "Footer" || sFieldName == "PageNumber" )
+                                || sFieldName == "Footer" || sFieldName == "PageNumber"
+                                || sFieldName == "PageName" )
                         {
                             mbIsPlaceholderShape = true;
                         }
