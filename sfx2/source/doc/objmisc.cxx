7df8b437b721b25561995346abfb55dca0d500b5
Samuel Mehrbrodt <samuel.mehrbrodt@allotropia.de>
2021-01-18 15:24:48 +0100

    Improve macro checks
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/109552
    Tested-by: Jenkins
    Reviewed-by: Thorsten Behrens <thorsten.behrens@allotropia.de>
    (cherry picked from commit 1dc71daf7fa7204a98c75dac680af664ab9c8edb)
    
    Change-Id: Ie40801df8866b52c1458e020ffa9cba120720af7

--- sfx2/source/doc/objmisc.cxx
+++ sfx2/source/doc/objmisc.cxx
@@ -937,7 +937,8 @@ void SfxObjectShell::CheckSecurityOnLoading_Impl()
     CheckEncryption_Impl( xInteraction );
 
     // check macro security
-    pImpl->aMacroMode.checkMacrosOnLoading( xInteraction );
+    const bool bHasValidContentSignature = HasValidSignatures();
+    pImpl->aMacroMode.checkMacrosOnLoading( xInteraction, bHasValidContentSignature );
 }
 
 
