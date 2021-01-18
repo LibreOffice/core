37b3df889430415029873e275efc9285000111f9
Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>
2021-01-18 11:01:48 +0100

    mac: don't put script files into Contents/MacOS or framework-bin directory
    Signing them as executable code would require external attributes, and
    those in turn break packaging into hfs+ dmg when building on apfs with
    Big Sur.
    It is not a new thing - the old Code Signing in Depth technote
    https://developer.apple.com/library/archive/technotes/tn2206/_index.html
    already reads:
    "Store Python, Perl, shell, and other script files and other non-Mach-O
    executables in your app's Contents/Resources directory. While it's
    possible to sign such executables and store them in Contents/MacOS, this
    is not recommended.
    […]
    Put another way, a properly-signed app that has all of its files in the
    correct places will not contain any signatures stored as extended
    attributes."
    The patch does exactly that for LO and the shipped python framework and
    adds symlinks for the moved files.
    Same applies for the Language pack applescript and the tarball - those
    are also moved into Contents/Resources
    
    Change-Id: Iab21e77b73f941248ca89c6e80703fdf67a1057c

--- shell/Package_senddoc.mk
+++ shell/Package_senddoc.mk
@@ -9,6 +9,14 @@
 
 $(eval $(call gb_Package_Package,shell_senddoc,$(SRCDIR)/shell/source/unix/misc))
 
+ifeq ($(OS), MACOSX)
+# only mach-o binaries allowed in bin folder (signing scripts would require extended attributes)
+# so install it into Resources folder and use a symlink instead
+# see https://developer.apple.com/library/archive/technotes/tn2206/_index.html
+$(eval $(call gb_Package_add_file,shell_senddoc,$(LIBO_SHARE_FOLDER)/senddoc,senddoc.sh))
+$(eval $(call gb_Package_add_symbolic_link,shell_senddoc,$(LIBO_BIN_FOLDER)/senddoc,../$(LIBO_SHARE_FOLDER)/senddoc))
+else
 $(eval $(call gb_Package_add_file,shell_senddoc,$(LIBO_BIN_FOLDER)/senddoc,senddoc.sh))
+endif
 
 # vim: set shiftwidth=4 tabstop=4 noexpandtab:
