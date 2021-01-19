9372483e8148ee38e69714539bc2e67cdb6a1d15
Michael Stahl <michael.stahl@allotropia.de>
2021-01-19 15:38:05 +0100

    postgres: upgrade to release 13.1
    Fixes CVE-2020-25694, plus a bunch more CVE that don't look relevant.
    
    * --with-krb5 no longer exists, neither does --disable-shared
    * remove internal-zlib.patch.1:
      zlib is only used by pg_* tools / contrib/pgcrypto
    * remove postgresql-libs-leak.patch:
      some relic from pre-gbuild times, not clear what the point is for
      static libs
    * remove postgresql-9.2.1-libreoffice.patch:
      another dmake .mk file relic, and the win32 nmake build system was
      removed
    * TODO: windows
    
    Change-Id: Ic0232a28801b2f604d9f4e33d5621ae3362defaa

--- external/postgresql/UnpackedTarball_postgresql.mk
+++ external/postgresql/UnpackedTarball_postgresql.mk
@@ -13,19 +13,9 @@ $(eval $(call gb_UnpackedTarball_set_tarball,postgresql,$(POSTGRESQL_TARBALL),,p
 
 $(eval $(call gb_UnpackedTarball_update_autoconf_configs,postgresql,config))
 
-$(eval $(call gb_UnpackedTarball_set_patchlevel,postgresql,3))
-
 $(eval $(call gb_UnpackedTarball_add_patches,postgresql, \
-	external/postgresql/postgresql-libs-leak.patch \
-	external/postgresql/postgresql-9.2.1-libreoffice.patch \
 	external/postgresql/windows.patch.0 \
 	external/postgresql/postgresql.exit.patch.0 \
 ))
-
-ifeq ($(SYSTEM_ZLIB),)
-$(eval $(call gb_UnpackedTarball_add_patches,postgresql, \
-	external/postgresql/internal-zlib.patch.1 \
-))
-endif
 
 # vim: set noet sw=4 ts=4:
