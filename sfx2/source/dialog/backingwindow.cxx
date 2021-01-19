1479d718ee3970a9040428c3402a0af47d413608
Heiko Tietze <tietze.heiko@gmail.com>
2021-01-19 14:37:19 +0100

    Make brand image clickable
    and link to LibreOffice volunteer page
    
    Change-Id: Ie422983d0e23faa16c2e2364b25798b938712a43

--- sfx2/source/dialog/backingwindow.cxx
+++ sfx2/source/dialog/backingwindow.cxx
@@ -22,6 +22,7 @@
 #include <vcl/event.hxx>
 #include <vcl/help.hxx>
 #include <vcl/menu.hxx>
+#include <vcl/ptrstyle.hxx>
 #include <vcl/settings.hxx>
 #include <vcl/svapp.hxx>
 #include <vcl/syswin.hxx>
@@ -37,6 +38,7 @@
 #include <comphelper/processfactory.hxx>
 #include <comphelper/propertysequence.hxx>
 #include <sfx2/app.hxx>
+#include <officecfg/Office/Common.hxx>
 
 #include <tools/diagnose_ex.h>
 
@@ -60,6 +62,55 @@ using namespace ::com::sun::star::document;
 
 constexpr OUStringLiteral SERVICENAME_CFGREADACCESS = u"com.sun.star.configuration.ConfigurationAccess";
 
+class BrandImage : public weld::CustomWidgetController
+{
+private:
+    BitmapEx maBrandImage;
+
+public:
+    virtual void SetDrawingArea(weld::DrawingArea* pDrawingArea) override
+    {
+        weld::CustomWidgetController::SetDrawingArea(pDrawingArea);
+        SetPointer(PointerStyle::RefHand);
+    }
+
+    virtual void StyleUpdated() override
+    {
+        if (!maBrandImage)
+        {
+            const bool bIsDark = Application::GetSettings().GetStyleSettings().GetDialogColor().IsDark();
+            SfxApplication::loadBrandSvg(bIsDark ? "shell/logo-sc_inverted" : "shell/logo-sc",
+                                        maBrandImage, GetOutputSizePixel().Width());
+
+            const Size aBmpSize(maBrandImage.GetSizePixel());
+            set_size_request(aBmpSize.Width(), aBmpSize.Height());
+
+            weld::CustomWidgetController::StyleUpdated();
+        }
+    }
+
+    virtual bool MouseButtonUp(const MouseEvent& rMEvt) override
+    {
+        if (rMEvt.IsLeft())
+        {
+            OUString sURL = officecfg::Office::Common::Menus::VolunteerURL::get();
+            localizeWebserviceURI(sURL);
+
+            Reference<css::system::XSystemShellExecute> const xSystemShellExecute(
+                css::system::SystemShellExecute::create(
+                    ::comphelper::getProcessComponentContext()));
+            xSystemShellExecute->execute(sURL, OUString(),
+                                         css::system::SystemShellExecuteFlags::URIS_ONLY);
+        }
+        return true;
+    }
+
+    virtual void Paint(vcl::RenderContext& rRenderContext, const tools::Rectangle&) override
+    {
+        rRenderContext.DrawBitmapEx(Point(0, 0), maBrandImage);
+    }
+};
+
 // increase size of the text in the buttons on the left fMultiplier-times
 float const g_fMultiplier = 1.4f;
 
@@ -77,7 +128,8 @@ BackingWindow::BackingWindow(vcl::Window* i_pParent)
     , mxDrawAllButton(m_xBuilder->weld_button("draw_all"))
     , mxDBAllButton(m_xBuilder->weld_button("database_all"))
     , mxMathAllButton(m_xBuilder->weld_button("math_all"))
-    , mxBrandImage(m_xBuilder->weld_image("imBrand"))
+    , mxBrandImage(new BrandImage)
+    , mxBrandImageWeld(new weld::CustomWeld(*m_xBuilder, "daBrand", *mxBrandImage))
     , mxHelpButton(m_xBuilder->weld_button("help"))
     , mxExtensionsButton(m_xBuilder->weld_button("extensions"))
     , mxAllButtonsBox(m_xBuilder->weld_container("all_buttons_box"))
@@ -95,19 +147,6 @@ BackingWindow::BackingWindow(vcl::Window* i_pParent)
     // init background
     SetPaintTransparent(false);
     SetBackground(svtools::ColorConfig().GetColorValue(::svtools::APPBACKGROUND).nColor);
-
-    //brand image
-    BitmapEx aBackgroundBitmap;
-    bool bIsDark = Application::GetSettings().GetStyleSettings().GetDialogColor().IsDark();
-    if (SfxApplication::loadBrandSvg(bIsDark ? "shell/logo-sc_inverted" : "shell/logo-sc",
-                                     aBackgroundBitmap, mxButtonsBox->get_preferred_size().Width()))
-    {
-        ScopedVclPtr<VirtualDevice> m_pVirDev = mxBrandImage->create_virtual_device();
-        m_pVirDev->SetOutputSizePixel(aBackgroundBitmap.GetSizePixel());
-        m_pVirDev->DrawBitmapEx(Point(0, 0), aBackgroundBitmap);
-        mxBrandImage->set_image(m_pVirDev.get());
-        m_pVirDev.disposeAndClear();
-    }
 
     //set an alternative help label that doesn't hotkey the H of the Help menu
     mxHelpButton->set_label(mxAltHelpLabel->get_label());
