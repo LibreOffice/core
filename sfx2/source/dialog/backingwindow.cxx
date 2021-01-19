71fc90bef30d582c0bb4d63e98d161605b3b61f1
Caolán McNamara <caolanm@redhat.com>
2021-01-19 10:47:34 +0000

    silence "gsignal.c:2731: instance X has no handler with id Y" warning
    Change-Id: Iafc0aa085338fb41758fa845252f584c2a8aea76

--- sfx2/source/dialog/backingwindow.cxx
+++ sfx2/source/dialog/backingwindow.cxx
@@ -170,6 +170,7 @@ void BackingWindow::dispose()
     mxDrawAllButton.reset();
     mxDBAllButton.reset();
     mxMathAllButton.reset();
+    mxBrandImage.reset();
     mxHelpButton.reset();
     mxExtensionsButton.reset();
     mxAllButtonsBox.reset();
