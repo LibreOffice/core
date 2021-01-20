LibreOffice/core

commit 3347d71b23595919e167bd49932675e77cbbdbbb

tdf#137546 sw: fix text position at textbox alignment
Follow-up of commit 06fd06597796d9e92117602245f3968c93707708
(tdf#124430 Writer Editing: Fix textbox aligning).

Testing:

1. Insert a shape. Choose menu item "Add Text Box" of
   its local menu, and write a short text in it.

2. Choose menu item "Align->Right" of the local menu of
   the shape. The textbox is aligned to the right margin
   and before the fix, text position had changed slightly
   within the shape. (Only moving the textbox fixed it.)

Change-Id: I758e0f65c8b51e09ac15a96e19819cb3ad3beacb
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/105253
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/source/uibase/shells/drwbassh.cxx
@@ -443,7 +443,8 @@ void SwDrawBaseShell::Execute(SfxRequest const &rReq)
             if ( bAlignPossible )
             {
                 const SdrMarkList& rMarkList = pSdrView->GetMarkedObjectList();
-                if ( rMarkList.GetMarkCount() == 1 )
+                if (rMarkList.GetMarkCount() == 1
+                    && !SwTextBoxHelper::hasTextFrame(rMarkList.GetMark(0)->GetMarkedSdrObj()))
                 {
                     sal_Int16 nHorizOrient = -1, nVertOrient = -1;
 
@@ -479,8 +480,6 @@ void SwDrawBaseShell::Execute(SfxRequest const &rReq)
                         SwFormatHoriOrient aHOrient(pFrameFormat->GetFormatAttr(RES_HORI_ORIENT));
                         aHOrient.SetHoriOrient( nHorizOrient );
                         pFrameFormat->SetFormatAttr(aHOrient);
-                        if (auto pTxFrm = SwTextBoxHelper::getOtherTextBoxFormat(pFrameFormat, RES_DRAWFRMFMT))
-                            pTxFrm->SetFormatAttr(aHOrient);
                         pSh->EndAction();
                     }
 
@@ -492,8 +491,6 @@ void SwDrawBaseShell::Execute(SfxRequest const &rReq)
                         SwFormatVertOrient aVOrient(pFrameFormat->GetFormatAttr(RES_VERT_ORIENT));
                         aVOrient.SetVertOrient( nVertOrient );
                         pFrameFormat->SetFormatAttr(aVOrient);
-                        if (auto pTxFrm = SwTextBoxHelper::getOtherTextBoxFormat(pFrameFormat, RES_DRAWFRMFMT))
-                            pTxFrm->SetFormatAttr(aVOrient);
                         pSh->EndAction();
                     }
 

commit 6d9d1b75359f6b09aa11e410ff52bf4a7d9af0f7

tdf#138512 SW: Fix crash on textbox aligning
Regression from 3347d71b23595919e167bd49932675e77cbbdbbb
(sw: fix text position at textbox alignment) has been
fixed by this commit. Hopefully there will not be newer
regressions. Please note the unitest under dev so not merge.

Change-Id: I3280547a6195e240e5b33e0d0931fb60d4d19fff

sw/source/uibase/shells/drwbassh.cxx
@@ -443,8 +443,7 @@ void SwDrawBaseShell::Execute(SfxRequest const &rReq)
             if ( bAlignPossible )
             {
                 const SdrMarkList& rMarkList = pSdrView->GetMarkedObjectList();
-                if (rMarkList.GetMarkCount() == 1
-                    && !SwTextBoxHelper::hasTextFrame(rMarkList.GetMark(0)->GetMarkedSdrObj()))
+                if (rMarkList.GetMarkCount() == 1)
                 {
                     sal_Int16 nHorizOrient = -1, nVertOrient = -1;
 
@@ -472,13 +471,16 @@ void SwDrawBaseShell::Execute(SfxRequest const &rReq)
                             break;
                     }
 
+                    SdrObject* pObj = rMarkList.GetMark(0)->GetMarkedSdrObj();
+                    SwFrameFormat* pFrameFormat = FindFrameFormat(pObj);
+                    SwFrameFormat* pTextBox
+                        = SwTextBoxHelper::getOtherTextBoxFormat(pFrameFormat, RES_DRAWFRMFMT);
+
                     if (nHorizOrient != -1)
                     {
                         pSh->StartAction();
-                        SdrObject* pObj = rMarkList.GetMark(0)->GetMarkedSdrObj();
-                        SwFrameFormat* pFrameFormat = FindFrameFormat( pObj );
                         SwFormatHoriOrient aHOrient(pFrameFormat->GetFormatAttr(RES_HORI_ORIENT));
-                        aHOrient.SetHoriOrient( nHorizOrient );
+                        aHOrient.SetHoriOrient(nHorizOrient);
                         pFrameFormat->SetFormatAttr(aHOrient);
                         pSh->EndAction();
                     }
@@ -486,12 +488,32 @@ void SwDrawBaseShell::Execute(SfxRequest const &rReq)
                     if (nVertOrient != -1)
                     {
                         pSh->StartAction();
-                        SdrObject* pObj = rMarkList.GetMark(0)->GetMarkedSdrObj();
-                        SwFrameFormat* pFrameFormat = FindFrameFormat( pObj );
                         SwFormatVertOrient aVOrient(pFrameFormat->GetFormatAttr(RES_VERT_ORIENT));
-                        aVOrient.SetVertOrient( nVertOrient );
+                        aVOrient.SetVertOrient(nVertOrient);
                         pFrameFormat->SetFormatAttr(aVOrient);
                         pSh->EndAction();
+                    }
+
+                    if (pTextBox)
+                    {
+                        Point nShapePos(pFrameFormat->GetHoriOrient().GetPos(),
+                                        pFrameFormat->GetVertOrient().GetPos());
+                        Point nOffset(
+                            SwTextBoxHelper::getTextRectangle(pFrameFormat, false).TopLeft());
+
+                        if (nHorizOrient != -1)
+                        {
+                            SwFormatHoriOrient aNewHOri(pTextBox->GetHoriOrient());
+                            aNewHOri.SetPos(nShapePos.X() + nOffset.X());
+                            pTextBox->SetFormatAttr(aNewHOri);
+                        }
+
+                        if (nVertOrient != -1)
+                        {
+                            SwFormatVertOrient aNewVOri(pTextBox->GetVertOrient());
+                            aNewVOri.SetPos(nShapePos.Y() + nOffset.Y());
+                            pTextBox->SetFormatAttr(aNewVOri);
+                        }
                     }
 
                     break;