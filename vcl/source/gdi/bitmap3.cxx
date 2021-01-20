8b8a49e14c1010c5ab325b64fc3b65524a6a6049
Caolán McNamara <caolanm@redhat.com>
2021-01-20 09:19:25 +0000

    rhbz#1918152 fix FTBFS
    Change-Id: I5c03c810d3b2572b0e58c62293add49bd7025e70

--- vcl/source/gdi/bitmap3.cxx
+++ vcl/source/gdi/bitmap3.cxx
@@ -17,8 +17,6 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#include <math.h>
-
 #include <vcl/bitmapaccess.hxx>
 #include <vcl/bitmapex.hxx>
 #include <vcl/bitmap.hxx>
