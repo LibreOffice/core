d20688a3a6447388eb074a2403c4ad5bb091f5ca
Pranam Lashkari <lpranam@collabora.com>
2021-01-22 01:27:28 +0530

    prevent reference range resetting on panning in mobile
    Change-Id: Ic6cabe1d4e5a1e7c4d42294550d654e38e27db22

--- sc/source/ui/view/gridwin.cxx
+++ sc/source/ui/view/gridwin.cxx
@@ -1429,6 +1429,19 @@ void ScGridWindow::LogicMouseMove(const MouseEvent& rMouseEvent)
 
 void ScGridWindow::MouseButtonDown( const MouseEvent& rMEvt )
 {
+    ScViewFunc* pView = pViewData->GetView();
+    ScTabViewShell* pViewShell = pViewData->GetViewShell();
+    bool bRefMode = pViewShell && pViewShell->IsRefInputMode();
+
+    Point aPos(rMEvt.GetPosPixel());
+    SCCOL nPosX;
+    SCROW nPosY;
+    pViewData->GetPosFromPixel(aPos.X(), aPos.Y(), eWhich, nPosX, nPosY);
+
+    if (bRefMode && pView->GetFunctionSet().CheckRefBounds(nPosX, nPosY) &&
+        SfxLokHelper::getDeviceFormFactor() == LOKDeviceFormFactor::MOBILE)
+        return;
+
     nNestedButtonState = ScNestedButtonState::Down;
 
     MouseEventState aState;
