013d6da8737ec074f9e398ec27cb91926572b2b4
Bjoern Michaelsen <bjoern.michaelsen@libreoffice.org>
2021-01-22 22:08:36 +0100

    Dont double clear caches
    - NumRuleChg calls sw::BroadcastMixin::CallSwClientNotify
    - which always calls SwModify::CallSwClientNotify
    - which always clears caches
    
    Change-Id: Iaf00e90bb7db9bd20ec08718043ce5b4025abecc
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/109153
    Tested-by: Jenkins
    Reviewed-by: Bjoern Michaelsen <bjoern.michaelsen@libreoffice.org>

--- sw/source/core/txtnode/ndtxt.cxx
+++ sw/source/core/txtnode/ndtxt.cxx
@@ -2827,12 +2827,6 @@ void SwTextNode::NumRuleChgd()
         }
     }
 
-    if( IsInCache() )
-    {
-        SwFrame::GetCache().Delete( this );
-        SetInCache( false );
-    }
-
     // Sending "noop" modify in order to cause invalidations of registered
     // <SwTextFrame> instances to get the list style change respectively the change
     // in the list tree reflected in the layout.
