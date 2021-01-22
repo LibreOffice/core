6e30139f09bfd29406e8d7068c42c09ea7861456
Pranam Lashkari <lpranam@collabora.com>
2021-01-22 17:21:05 +0530

    fixed range text not being updated on dragging reference marker
    dragging reference stat marker in online did not change the text of range selection
    
    Change-Id: I0f9ee9df2418bc4dcf519ca3462b510758b9766d

--- sc/source/ui/view/cellsh1.cxx
+++ sc/source/ui/view/cellsh1.cxx
@@ -2736,6 +2736,11 @@ void ScCellShell::ExecuteEdit( SfxRequest& rReq )
                     pTabView->UpdateRef( colEnd, rowEnd, table ); // setup the end & refresh formula
 
                     pInputHdl->UpdateLokReferenceMarks();
+
+                    ScRange aRef(
+                        rViewData.GetRefStartX(), rViewData.GetRefStartY(), rViewData.GetRefStartZ(),
+                        rViewData.GetRefEndX(), rViewData.GetRefEndY(), rViewData.GetRefEndZ() );
+                    SC_MOD()->SetReference( aRef, rViewData.GetDocument(), &rViewData.GetMarkData() );
                 }
             }
             break;
