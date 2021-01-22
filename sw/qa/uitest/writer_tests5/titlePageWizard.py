794584bf3d2ab0ac220d7ecffbf1e67aaeb9c738
Justin Luth <justin.luth@collabora.com>
2021-01-22 21:55:52 +0300

    tdf#138907 sw TitlePageDlg: Goto correct (inserted) page
    Especially with inserted-in-the-middle pages, until the layout
    was re-calculated, the new pages were not recognized.
    
    This patch should bring this format option into pretty good shape.
    
    I made a couple of decisions:
    1.) Insert an additional content page when adding to end of document
        (otherwise the last index page turned into content).
    2.) Ensure that index pages don't inherit page-renumbering
    
    This CalcLayout could take a fair amount of time on a huge document.
    I tested on a document that took over a minute to load.
    It inserted pages in about 10 seconds. The experience seemed
    reasonable to me given the known slowness of the document.
    
    make UITest_writer_tests5 UITEST_TEST_NAME=\
    titlePageWizard2.tdf138907.test_tdf138907
    
    Change-Id: I6d7763ab10b46b5a459e9b62ed96b0194b2258b4

--- sw/qa/uitest/writer_tests5/titlePageWizard.py
+++ sw/qa/uitest/writer_tests5/titlePageWizard.py
@@ -10,6 +10,7 @@ from libreoffice.uno.propertyvalue import mkPropertyValues
 
 # This tests the Format->Title Page wizard, specifically the reset page number portion,
 # replacing some pages with title pages,
+# inserting pages in the middle of the document,
 # and inserting at the very end of the document.
 class tdf138907(UITestCase):
     def test_tdf138907(self):
@@ -45,8 +46,6 @@ class tdf138907(UITestCase):
         xOKBtn = xDialog.getChild("ok")
         self.ui_test.close_dialog_through_button(xOKBtn)
 
-        # This correctly reset the starting page number for page 2 as "1".
-        # It wasn't persistent across round-trips though
         Paragraphs = document.Text.createEnumeration()
         Para1 = Paragraphs.nextElement()
         self.assertEqual(Para1.String, "6")
@@ -81,7 +80,7 @@ class tdf138907(UITestCase):
         self.ui_test.execute_dialog_through_command(".uno:TitlePageDialog")
         xDialog = self.xUITest.getTopFocusWindow()
         print(xDialog.getChildren())
-        #Insert the title/index pages at page two.
+        #Convert three pages to title/index pages starting at page two.
         xPageCount = xDialog.getChild("NF_PAGE_COUNT")
         for _ in range(0,2):
             xPageCount.executeAction("UP", tuple())
@@ -117,7 +116,7 @@ class tdf138907(UITestCase):
         self.ui_test.execute_dialog_through_command(".uno:TitlePageDialog")
         xDialog = self.xUITest.getTopFocusWindow()
         print(xDialog.getChildren())
-        #Insert title/index pages at the end of the document.
+        #Insert three title/index pages at the end of the document (plus a content page).
         newPages = xDialog.getChild("RB_INSERT_NEW_PAGES")
         newPages.executeAction("CLICK", tuple())
         xPageCount = xDialog.getChild("NF_PAGE_COUNT")
@@ -133,7 +132,6 @@ class tdf138907(UITestCase):
         self.ui_test.close_dialog_through_button(xOKBtn)
 
         # Without the fix, the pages were being inserted before the last page.
-        # NOTE: there are still LOTS of problems here that still need fixing.
         text = document.Text.String.replace('\r\n', '\n')
         self.assertEqual(text[0:1], "6")
         self.assertEqual(text[2:3], "6")
@@ -141,6 +139,76 @@ class tdf138907(UITestCase):
         self.assertEqual(text[6:7], "8")
         # Without the fix, the new pages were inserted before the last page.
         self.assertFalse("\n" in text[8:9])
+        #Note: 13 total virtual pages, including four blanks, as seen in book view
+        self.assertEqual(document.CurrentController.PageCount, 13)
+
+        #Now test inserting in the middle of the document
+
+        #dialog Title Page
+        self.ui_test.execute_dialog_through_command(".uno:TitlePageDialog")
+        xDialog = self.xUITest.getTopFocusWindow()
+        print(xDialog.getChildren())
+        #Insert three title/index pages starting at page 2.
+        newPages = xDialog.getChild("RB_INSERT_NEW_PAGES")
+        newPages.executeAction("CLICK", tuple())
+        xPageCount = xDialog.getChild("NF_PAGE_COUNT")
+        for _ in range(0,2):
+            xPageCount.executeAction("UP", tuple())
+        xUseStartingPage = xDialog.getChild("RB_PAGE_START")
+        xUseStartingPage.executeAction("CLICK", tuple())
+        xStartingPage = xDialog.getChild("NF_PAGE_START")
+        for _ in range(0,10):
+            xStartingPage.executeAction("DOWN", tuple()) #Reset to page 1
+        xStartingPage.executeAction("UP", tuple()) #Start at page 2.
+
+        xOKBtn = xDialog.getChild("ok")
+        self.ui_test.close_dialog_through_button(xOKBtn)
+
+        # Without first re-calculating the layout, the styles were applied to the wrong pages.
+        Paragraphs = document.Text.createEnumeration()
+        Para1 = Paragraphs.nextElement()
+        self.assertEqual(Para1.String, "6")
+        self.assertEqual(Para1.PageDescName, "First Page")
+        # The next three pages are the ones that were just inserted.
+        Para2 = Paragraphs.nextElement()
+        self.assertEqual(Para2.String, "")
+        self.assertEqual(Para2.PageDescName, "First Page")
+        Para3 = Paragraphs.nextElement()
+        self.assertEqual(Para3.String, "")
+        self.assertEqual(Para3.PageDescName, "Index")
+        Para4 = Paragraphs.nextElement()
+        self.assertEqual(Para4.String, "")
+        self.assertEqual(Para4.PageDescName, "Index")
+        # A bit of a quirk is that the style of the first page after the
+        # title page is still First Page - so that is used as the Normal page style.
+        # OK - this is a bit of a strange workflow, so just accept that.
+        Para5 = Paragraphs.nextElement()
+        self.assertEqual(Para5.String, "6")
+        self.assertEqual(Para5.PageDescName, "First Page")
+        Para6 = Paragraphs.nextElement()
+        self.assertEqual(Para6.String, "7")
+        self.assertEqual(Para6.PageDescName, "Index")
+        Para7 = Paragraphs.nextElement()
+        self.assertEqual(Para7.String, "8")
+        self.assertEqual(Para7.PageDescName, "Index")
+        Para8 = Paragraphs.nextElement()
+        self.assertEqual(Para8.String, "2")
+        self.assertEqual(Para8.PageDescName, "Landscape")
+        Para9 = Paragraphs.nextElement()
+        self.assertEqual(Para9.String, "")
+        self.assertEqual(Para9.PageDescName, "First Page")
+        Para10 = Paragraphs.nextElement()
+        self.assertEqual(Para10.String, "")
+        self.assertEqual(Para10.PageDescName, "Index")
+        Para11 = Paragraphs.nextElement()
+        self.assertEqual(Para11.String, "")
+        self.assertEqual(Para11.PageDescName, "Index")
+        # The quirk resets this extra content page to the "style after First page == First Page"
+        Para12 = Paragraphs.nextElement()
+        self.assertEqual(Para12.String, "")
+        self.assertEqual(Para12.PageDescName, "First Page")
+        #Note: 17 total virtual pages, including five blanks, as seen in book view
+        self.assertEqual(document.CurrentController.PageCount, 17)
 
         self.ui_test.close_doc()
 
