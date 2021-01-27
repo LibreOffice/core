2ba2bf1248492675a3a0029f071ea63017cf3f63
Attila Szűcs <szucs.attila3@nisz.hu>
2021-01-27 17:43:43 +0100

    tdf#104502 sc: skip hidden columns at printing pages
    Page calculation counted the hidden columns, resulted
    printing blank pages by accident.
    
    Extend GetPrintArea() and GetTableArea() to count pages
    without the hidden columns, too.
    
    Co-authored-by: Tibor Nagy (NISZ)
    
    Change-Id: I4817965a675e059cdc8f81ca3bb6e128af874f2f
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/110028
    Tested-by: László Németh <nemeth@numbertext.org>
    Reviewed-by: László Németh <nemeth@numbertext.org>
    (cherry picked from commit 2bf3e0d00e3bccb5b250642ee0d3fdbe6cae8ecc)

--- sc/inc/table.hxx
+++ sc/inc/table.hxx
@@ -198,6 +198,8 @@ private:
 
     mutable SCCOL nTableAreaX;
     mutable SCROW nTableAreaY;
+    mutable SCCOL nTableAreaVisibleX;
+    mutable SCROW nTableAreaVisibleY;
 
     SCTAB           nTab;
     ScDocument&     rDocument;
@@ -232,6 +234,7 @@ private:
     bool            bLoadingRTL:1;
     bool            bPageSizeValid:1;
     mutable bool    bTableAreaValid:1;
+    mutable bool    bTableAreaVisibleValid:1;
     bool            bVisible:1;
     bool            bStreamValid:1;
     bool            bPendingRowHeights:1;
@@ -567,8 +570,8 @@ public:
     void        InvalidatePageBreaks();
 
     bool        GetCellArea( SCCOL& rEndCol, SCROW& rEndRow ) const;            // FALSE = empty
-    bool        GetTableArea( SCCOL& rEndCol, SCROW& rEndRow ) const;
-    bool        GetPrintArea( SCCOL& rEndCol, SCROW& rEndRow, bool bNotes ) const;
+    bool        GetTableArea( SCCOL& rEndCol, SCROW& rEndRow, bool bCalcHiddens = false) const;
+    bool        GetPrintArea( SCCOL& rEndCol, SCROW& rEndRow, bool bNotes, bool bCalcHiddens = false) const;
     bool        GetPrintAreaHor( SCROW nStartRow, SCROW nEndRow,
                                 SCCOL& rEndCol ) const;
     bool        GetPrintAreaVer( SCCOL nStartCol, SCCOL nEndCol,
