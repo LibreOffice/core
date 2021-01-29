5a32df721e6e839ad592cac8f78510c412e55b22
Mike Kaganski <mike.kaganski@collabora.com>
2021-01-29 09:57:41 +0300

    Simplify osl_getGlobalMutex to use function-local static
    Remove some reinterpret_cast, and use more C++
    
    Change-Id: I36df8be817d0f0cb75efa00b0b523f379f67d1d1

--- cppu/source/typelib/typelib.cxx
+++ cppu/source/typelib/typelib.cxx
@@ -175,8 +175,6 @@ struct TypeDescriptor_Init_Impl
     std::unique_ptr<CallbackSet_Impl> pCallbacks;
     // A cache to hold descriptions
     std::unique_ptr<TypeDescriptionList_Impl> pCache;
-    // The mutex to guard all type library accesses
-    std::unique_ptr<Mutex>      pMutex;
 
     inline Mutex & getMutex();
 
@@ -212,13 +210,9 @@ struct TypeDescriptor_Init_Impl
 
 inline Mutex & TypeDescriptor_Init_Impl::getMutex()
 {
-    if( !pMutex )
-    {
-        MutexGuard aGuard( Mutex::getGlobalMutex() );
-        if( !pMutex )
-            pMutex.reset(new Mutex());
-    }
-    return * pMutex;
+    // The mutex to guard all type library accesses
+    static osl::Mutex s_Mutex;
+    return s_Mutex;
 }
 
 inline void TypeDescriptor_Init_Impl::callChain(
