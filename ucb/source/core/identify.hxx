7f1cd5c19d3e4b9708c7a40d3883ac4bd8746526
Shobhit <000shobhitchaurasia@gmail.com>
2021-01-30 22:17:13 +0530

    tdf#124176: Use pragma once instead of include guards
    Applied clang format
    
    Change-Id: Idb3f4dc8726115f8d53721bea28cbe3e53adef3d

--- ucb/source/core/identify.hxx
+++ ucb/source/core/identify.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_UCB_SOURCE_CORE_IDENTIFY_HXX
-#define INCLUDED_UCB_SOURCE_CORE_IDENTIFY_HXX
+#pragma once
 
 #include <com/sun/star/ucb/XContentIdentifier.hpp>
 #include <cppuhelper/implbase.hxx>
@@ -37,7 +36,5 @@ private:
     OUString m_aContentId;
     OUString m_aProviderScheme;
 };
-
-#endif // INCLUDED_UCB_SOURCE_CORE_IDENTIFY_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
