e28d97bce1a705648350f97b4ecce627f9339cb5
László Németh <nemeth@numbertext.org>
2021-02-03 16:56:49 +0100

    tdf#139301 fix OOXML-compatible preset dash styles
    Despite the purpose of the new interoperability styles
    added in commit b2d8093b19642038631dfb8f1ab6745a380a652c
    (tdf#127499 added 9 new line styles all are (var) and in line with MSO),
    collision with commit 57c9bdab377a00649299d1a4c9ed2f9e5e03b84e
    (tdf#127166, tdf#123903 improve import/export of line styles)
    resulted still missing interoperability on the user interface:
    
    1) opening an OOXML document, preset styles weren't recognized
       on the UI, also the preview was a solid line on the Drawing
       Object Properties toolbar.
    
    2) using the new compatibility preset styles, OOXML export
       resulted custom styles instead of the intended preset styles.
    
    On the UI, now the layout of the first 8 preset dash styles
    follows the OOXML standard, see 20.1.10.49 ST_PresetLineDashVal
    (Preset Line Dash Value) in ISO/IEC 29500-1, 2016:
    
        dash pattern    name (OOXML name)
    10                  "Dot"  (sysDot)
    1000                "Long Dot" (dot)
    1110                "Dash" (sysDash)
    1111000             "Long Dash" (dash)
    11111111000         "Double Dash" (lgDash)
    11110001000         "Long Dash Dot (dashDot)
    111111110001000     "Double Dash Dot" (lgDashDot)
    1111111100010001000 "Double Dash Dot Dot" (lgDashDotDot)
    
    (Note: missing "Dash Dot" (sysDashDot) and "Dash Dot Dot"
    (sysDashDotDot) were added only in the master, but not in this
    back-port, yet, because the commit would contain new translatable
    strings:
    
    111010              "Dash Dot" (sysDashDot)
    11101010            "Dash Dot Dot" (sysDashDotDot))
    
    Remove "Double Dot": it really wasn't an OOXML-compatible preset
    style, and it has a good replacement among the older styles,
    the similarly loosely dotted "Fine dotted", which is still
    supported for back-compatibility.
    
    Note: to start the new UI test, use
    
    $ (cd sw; make -srj8 UITest_writer_tests7 UITEST_TEST_NAME="tdf139301.tdf139301.test_tdf139301" SAL_USE_VCLPLUGIN=gen)
    
    (cherry picked from commit 183c06fc02a50fb117bb6162e4d6e56cdd34fad1)
    Change-Id: I7f7cf6ea954aad4cb43ef7730b204ed7635f8fb9

---
+++ sw/qa/uitest/writer_tests7/tdf139301.py
@@ -0,0 +1,53 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+from uitest.framework import UITestCase
+from uitest.uihelper.common import get_state_as_dict, get_url_for_data_file
+import time
+
+class tdf139301(UITestCase):
+
+    def test_tdf139301(self):
+        writer_doc = self.ui_test.load_file(get_url_for_data_file("tdf127166_prstDash_Word97.docx"))
+
+        styles = ('Long Dash', 'Long Dash Dot', 'Long Dot', 'Double Dash', 'Double Dash Dot', 'Double Dash Dot Dot', 'Dash', 'Dash Dot', 'Dash Dot Dot', 'Dot')
+
+        xWriterDoc = self.xUITest.getTopFocusWindow()
+        xWriterEdit = xWriterDoc.getChild("writer_edit")
+
+        for i in range(len(styles)):
+            # don't check not back-ported styles
+            if styles[i] in ('Dash Dot', 'Dash Dot Dot'):
+                continue
+
+            # select next line shape
+            writer_doc.getCurrentController().select(writer_doc.getDrawPage()[i])
+
+            # wait for available line style setting
+            self.ui_test.wait_until_child_is_available(xWriterEdit, 'metricfield')
+            time.sleep(1)
+
+            # line setting dialog window
+            self.ui_test.execute_dialog_through_command(".uno:FormatLine")
+            time.sleep(1)
+            xFormatLineDlg = self.xUITest.getTopFocusWindow()
+            time.sleep(1)
+            # get line style combo box
+            xLineStyle = xFormatLineDlg.getChild("LB_LINE_STYLE")
+            time.sleep(1)
+
+            # check preset line style
+            style = get_state_as_dict(xLineStyle)['SelectEntryText']
+
+            xOKBtn = xFormatLineDlg.getChild("ok")
+            self.ui_test.close_dialog_through_button(xOKBtn)
+
+            self.assertEqual(style, styles[i])
+
+        self.ui_test.close_doc()
+
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
