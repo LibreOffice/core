36b58cc7855a5b8e63e064f9315034fe2fc1e175
Michael Stahl <michael.stahl@allotropia.de>
2021-02-04 19:19:30 +0100

    avoid -Werror=unused-variable
    Change-Id: I5002ca7411d0d9f85b95af864d5cf3dcd201c34c

--- sw/qa/extras/ooxmlexport/ooxmlexport.cxx
+++ sw/qa/extras/ooxmlexport/ooxmlexport.cxx
@@ -441,15 +441,15 @@ DECLARE_OOXMLEXPORT_TEST(testDrawingmlFlipv, "drawingml-flipv.docx")
 
 DECLARE_OOXMLEXPORT_TEST(testRot90Fliph, "rot90-fliph.docx")
 {
+#if 0
     // The problem was that a shape rotation of 90° got turned into 270° after roundtrip.
     if (xmlDocPtr pXmlDoc = parseExport("word/document.xml"))
     {
-#if 0
         assertXPath(pXmlDoc, "//a:xfrm", "flipH", "1");
         // This was 16200000 (270 * 60000).
         assertXPath(pXmlDoc, "//a:xfrm", "rot", "5400000");
-#endif
     }
+#endif
 }
 
 DECLARE_OOXMLEXPORT_TEST(testRot180Flipv, "rot180-flipv.docx")
