e1d217b086e8c47efc5ced4c490062956d564e3f
Miklos Vajna <vmiklos@collabora.com>
2021-02-05 17:03:08 +0100

    tdf#91920 sw page gutter margin, from top: add DOC filter
    But leave the fRTLGutter case alone for now, I can't find the Word UI
    for that.
    
    Change-Id: I1d97e85308aa13892f50c0fcd3680cec514ef566
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/110471
    Tested-by: Jenkins
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

--- sw/source/filter/ww8/ww8par.cxx
+++ sw/source/filter/ww8/ww8par.cxx
@@ -1957,6 +1957,11 @@ void SwWW8ImplReader::ImportDop()
     const SvtFilterOptions& rOpt = SvtFilterOptions::Get();
     if (rOpt.IsUseEnhancedFields())
         m_rDoc.getIDocumentSettingAccess().set(DocumentSettingId::PROTECT_FORM, m_xWDop->fProtEnabled );
+
+    if (m_xWDop->iGutterPos)
+    {
+        m_rDoc.getIDocumentSettingAccess().set(DocumentSettingId::GUTTER_AT_TOP, true);
+    }
 }
 
 void SwWW8ImplReader::ImportDopTypography(const WW8DopTypography &rTypo)
