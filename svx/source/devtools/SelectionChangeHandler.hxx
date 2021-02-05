65d087cb0309e5fb03206b22fc2c27d27f84d4d4
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2021-02-05 17:39:05 +0900

    devtools: move SelectionChangeHandler to its own file
    Change-Id: I78d3712caedeb2ad4741e3706beb43051a978801

---
+++ svx/source/devtools/SelectionChangeHandler.hxx
@@ -0,0 +1,74 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#pragma once
+
+#include <memory>
+
+#include <svx/devtools/DevelopmentToolDockingWindow.hxx>
+
+#include <com/sun/star/frame/XController.hpp>
+#include <com/sun/star/view/XSelectionSupplier.hpp>
+
+#include <cppuhelper/compbase.hxx>
+#include <cppuhelper/basemutex.hxx>
+
+typedef cppu::WeakComponentImplHelper<css::view::XSelectionChangeListener>
+    SelectionChangeHandlerInterfaceBase;
+
+class SelectionChangeHandler final : private ::cppu::BaseMutex,
+                                     public SelectionChangeHandlerInterfaceBase
+{
+private:
+    css::uno::Reference<css::frame::XController> mxController;
+    VclPtr<DevelopmentToolDockingWindow> mpDockingWindow;
+
+public:
+    SelectionChangeHandler(const css::uno::Reference<css::frame::XController>& rxController,
+                           DevelopmentToolDockingWindow* pDockingWindow)
+        : SelectionChangeHandlerInterfaceBase(m_aMutex)
+        , mxController(rxController)
+        , mpDockingWindow(pDockingWindow)
+    {
+        css::uno::Reference<css::view::XSelectionSupplier> xSupplier(mxController,
+                                                                     css::uno::UNO_QUERY);
+        xSupplier->addSelectionChangeListener(this);
+    }
+
+    ~SelectionChangeHandler() { mpDockingWindow.disposeAndClear(); }
+
+    virtual void SAL_CALL selectionChanged(const css::lang::EventObject& /*rEvent*/) override
+    {
+        css::uno::Reference<css::view::XSelectionSupplier> xSupplier(mxController,
+                                                                     css::uno::UNO_QUERY);
+        if (xSupplier.is())
+        {
+            css::uno::Any aAny = xSupplier->getSelection();
+            auto xInterface = aAny.get<css::uno::Reference<css::uno::XInterface>>();
+            mpDockingWindow->selectionChanged(xInterface);
+        }
+    }
+
+    void stopListening()
+    {
+        css::uno::Reference<css::view::XSelectionSupplier> xSupplier(mxController,
+                                                                     css::uno::UNO_QUERY);
+        xSupplier->removeSelectionChangeListener(this);
+    }
+
+    virtual void SAL_CALL disposing(const css::lang::EventObject& /*rEvent*/) override {}
+    virtual void SAL_CALL disposing() override {}
+
+private:
+    SelectionChangeHandler(const SelectionChangeHandler&) = delete;
+    SelectionChangeHandler& operator=(const SelectionChangeHandler&) = delete;
+};
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
