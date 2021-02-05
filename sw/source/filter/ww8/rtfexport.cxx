d613a6d72d64ef01368c198b348870de11b7e416
Miklos Vajna <vmiklos@collabora.com>
2021-02-05 17:27:26 +0100

    tdf#91920 sw page gutter margin, from top: add RTF export
    The documentation for \gutterprl is "Parallel gutter.", but it seems
    that's what Word use to convert between RTF and DOCX's <w:gutterAtTop/>.
    
    (cherry picked from commit faf0635def4ef603563f7288cb29d24e3f2ce1e3)
    
    Change-Id: I06d80f234c6f52950db8a047bfc88910b808977d

--- sw/source/filter/ww8/rtfexport.cxx
+++ sw/source/filter/ww8/rtfexport.cxx
@@ -728,6 +728,13 @@ ErrCode RtfExport::ExportDocument_Impl()
     // Mirror margins?
     if ((UseOnPage::Mirror & m_pDoc->GetPageDesc(0).ReadUseOn()) == UseOnPage::Mirror)
         Strm().WriteCharPtr(OOO_STRING_SVTOOLS_RTF_MARGMIRROR);
+
+    // Gutter at top?
+    if (m_pDoc->getIDocumentSettingAccess().get(DocumentSettingId::GUTTER_AT_TOP))
+    {
+        Strm().WriteCharPtr(LO_STRING_SVTOOLS_RTF_GUTTERPRL);
+    }
+
     // Init sections
     m_pSections = new MSWordSections(*this);
 
