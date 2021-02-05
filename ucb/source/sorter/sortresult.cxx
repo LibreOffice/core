21e18699899e75a21d01e5e1df52291dcd6f35aa
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-02-05 23:12:32 +0530

    tdf#75280 Edited some occurences of sal_uIntPtr
    Edited some sal_uIntPtr in ucb to sal_Int32
    
    Change-Id: Icb4ae99fd49a4b080ad8f3df6f15dddcdcb09e42

--- ucb/source/sorter/sortresult.cxx
+++ ucb/source/sorter/sortresult.cxx
@@ -1305,7 +1305,7 @@ void SortedResultSet::Initialize(
 }
 
 
-void SortedResultSet::CheckProperties( sal_IntPtr nOldCount, bool bWasFinal )
+void SortedResultSet::CheckProperties( size_t nOldCount, bool bWasFinal )
 {
     osl::Guard< osl::Mutex > aGuard( maMutex );
 
