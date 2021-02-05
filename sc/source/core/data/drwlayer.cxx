f21595cb5d9f978e11b23471671f11736c42e99d
Regina Henschel <rb.henschel@t-online.de>
2021-02-05 14:15:30 +0100

    cid#1472783 avoid null pointer dereference
    The target was created, but the pointer was not updated.
    
    Change-Id: I2e19ae3c84769de493f712f3189a2a506f173c0f
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/110476
    Tested-by: Jenkins
    Reviewed-by: Regina Henschel <rb.henschel@t-online.de>
    Signed-off-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/110658
    Reviewed-by: Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>

--- sc/source/core/data/drwlayer.cxx
+++ sc/source/core/data/drwlayer.cxx
@@ -2026,6 +2026,8 @@ void ScDrawLayer::MirrorRTL( SdrObject* pObj )
                               *pDoc, pData->maStart.Tab());
                 aNoRotateAnchor.mbResizeWithCell = pData->mbResizeWithCell;
                 SetNonRotatedAnchor(*pObj, aNoRotateAnchor);
+                pNoRotatedAnchor = GetNonRotatedObjData(pObj);
+                assert(pNoRotatedAnchor);
             }
             // Mirror object at vertical axis
             Point aRef1( 0, 0 );
