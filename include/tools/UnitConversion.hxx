f36df64512f966f8a92f919458790e3dde78e352
Mike Kaganski <mike.kaganski@collabora.com>
2021-02-05 19:35:14 +0300

    Use more conversion functions from <tools/UnitConversion.hxx>
    This unifies conversion functions to provide both floating-point
    and integral overloads, with correct rounding for the latter.
    
    TODO: also unify sanitizing variants or maybe move into the main
    function.
    
    Change-Id: Ibe1c9fe4d5baa226c600445779dbaf7dc41a02cb

--- include/tools/UnitConversion.hxx
+++ include/tools/UnitConversion.hxx
@@ -13,20 +13,34 @@
 #include <sal/types.h>
 #include <cassert>
 #include <limits>
+#include <type_traits>
 
-constexpr sal_Int64 convertTwipToMm100(sal_Int64 n)
+// Ensure correct rounding for both positive and negative integers
+template <int mul, int div, typename I, std::enable_if_t<std::is_integral_v<I>, int> = 0>
+constexpr sal_Int64 MulDiv(I n)
 {
-    assert(n < std::numeric_limits<sal_Int64>::max() / 127
-           && n > std::numeric_limits<sal_Int64>::min() / 127);
-    return (n >= 0) ? (n * 127 + 36) / 72 : (n * 127 - 36) / 72;
+    static_assert(mul > 0 && div > 0);
+    // This excludes sal_uInt64 as not representable in sal_Int64
+    static_assert(sizeof(sal_Int64) >= sizeof(I)
+                  && std::is_signed_v<std::common_type_t<sal_Int64, I>>);
+    assert(sal_Int64(n) <= (std::numeric_limits<sal_Int64>::max() - div / 2) / mul
+           && sal_Int64(n) >= (std::numeric_limits<sal_Int64>::min() + div / 2) / mul);
+    if constexpr (mul % div == 0)
+        return sal_Int64(n) * (mul / div);
+    else if constexpr (div % mul == 0)
+        return sal_Int64(n) / (div / mul);
+    else
+        return (n >= 0 ? (sal_Int64(n) * mul + div / 2) : (sal_Int64(n) * mul - div / 2)) / div;
+}
+template <int mul, int div, typename F, std::enable_if_t<std::is_floating_point_v<F>, int> = 0>
+constexpr double MulDiv(F f)
+{
+    static_assert(mul > 0 && div > 0);
+    return f * (double(mul) / div);
 }
 
-constexpr sal_Int64 convertMm100ToTwip(sal_Int64 n)
-{
-    assert(n < std::numeric_limits<sal_Int64>::max() / 72
-           && n > std::numeric_limits<sal_Int64>::min() / 72);
-    return (n >= 0) ? (n * 72 + 63) / 127 : (n * 72 - 63) / 127;
-}
+template <typename N> constexpr auto convertTwipToMm100(N n) { return MulDiv<127, 72>(n); }
+template <typename N> constexpr auto convertMm100ToTwip(N n) { return MulDiv<72, 127>(n); }
 
 constexpr sal_Int64 sanitiseMm100ToTwip(sal_Int64 n)
 {
@@ -38,20 +52,13 @@ constexpr sal_Int64 sanitiseMm100ToTwip(sal_Int64 n)
 }
 
 constexpr sal_Int64 convertPointToTwip(sal_Int64 nNumber) { return nNumber * 20; }
-
-constexpr sal_Int64 convertPointToMm100(sal_Int64 nNumber)
-{
-    return convertTwipToMm100(convertPointToTwip(nNumber));
-}
-
 constexpr double convertPointToTwip(double fNumber) { return fNumber * 20.0; }
 
-constexpr double convertPointToMm100(double fNumber) { return fNumber * (2540.0 / 72.0); }
+template <typename N> constexpr auto convertPointToMm100(N n) { return MulDiv<2540, 72>(n); }
+template <typename N> constexpr auto convertMm100ToPoint(N n) { return MulDiv<72, 2540>(n); }
 
-// Convert PPT's "master unit" (1/576 inch) to mm/100
-constexpr sal_Int64 convertMasterUnitToMm100(sal_Int64 n) { return n * (2540.0 / 576.0); }
-
-// Convert mm/100 to PPT's "master unit"
-constexpr sal_Int64 convertMm100ToMasterUnit(sal_Int64 n) { return n / (2540.0 / 576.0); }
+// PPT's "master unit" (1/576 inch) <=> mm/100
+template <typename N> constexpr auto convertMasterUnitToMm100(N n) { return MulDiv<2540, 576>(n); }
+template <typename N> constexpr auto convertMm100ToMasterUnit(N n) { return MulDiv<576, 2540>(n); }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
