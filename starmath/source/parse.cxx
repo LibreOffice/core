5c92acb9f05287efd2d59153b43fae31c4a1b16d
dante <dante19031999@gmail.com>
2021-02-07 13:27:35 +0100

    Use priority tree in starmath for loading binary operators
    This isn't yet the mathml priority import,
    however implements some required mechanics.
    
    Change-Id: I75d4d163ce422c7943b3492a4e9d37f81389daad

--- starmath/source/parse.cxx
+++ starmath/source/parse.cxx
@@ -1268,9 +1268,9 @@ std::unique_ptr<SmNode> SmParser::DoExpression(bool bUseExtraSpaces)
     DepthProtect aDepthGuard(m_nParseDepth);
 
     std::vector<std::unique_ptr<SmNode>> RelationArray;
-    RelationArray.push_back(DoRelation());
+    RelationArray.push_back(DoBinMo(0));
     while (m_aCurToken.nLevel >= 4)
-        RelationArray.push_back(DoRelation());
+        RelationArray.push_back(DoBinMo(0));
 
     if (RelationArray.size() > 1)
     {
@@ -1284,6 +1284,106 @@ std::unique_ptr<SmNode> SmParser::DoExpression(bool bUseExtraSpaces)
         // This expression has only one node so just push this node.
         return std::move(RelationArray[0]);
     }
+}
+
+
+/* How do we merge trees:
+ *      1                               1
+ *    /   \                       /           \
+ *   2     3    <-   8    =      2             8
+ *  / \   / \         \        /   \         /   \
+ * 4   5 6   7         A      4     5       3     A <- New bottom, left branches can never be reached
+ *                                         / \
+ *                                        6   7
+ */
+std::unique_ptr<SmNode> SmParser::DoBinMo(sal_uInt32 priority)
+{
+    DepthProtect aDepthGuard(m_nParseDepth);
+
+    // Data we need
+    // Head of the tree, also returned data
+    SmBinHorNode* xTree = nullptr;
+    // Bottom of the tree, where we are actually working
+    SmBinHorNode* xBottom = nullptr;
+    // The node we are parsing
+    SmBinHorNode* xTerm;
+    // The operator node
+    SmNode* xOper;
+    // First node code
+    SmNode* xFirst;
+    // Right opperand
+    SmNode* xRight;
+
+    // We are here, so now what ?
+    // There are two possible scenarios:
+    //  - The priority is highter and steals right term to parent
+    //  - The priority is less or equal and englobes parent
+
+    // Load something
+    xFirst = DoTerm(true).release();
+    // That should call next token and deliver the operator
+
+    // Now, we have a tree diagram, wich place corresponds ?
+    // We setup a loop
+    while (TokenInGroup(TG::BinMo))
+    {
+
+        // So, first of all, wich priority has?
+        sal_uInt32 mpriority = m_aCurToken.priority();
+        xOper = new SmNode(m_aCurToken);
+
+        // Very important, if priority drops under the minimum, break
+        if(mpriority < priority)
+            break;
+
+        // Now we're good.
+        // Prepare stuff
+        xOper = new SmNode(m_aCurToken);
+        xTerm = new SmBinHorNode(m_aCurToken);
+
+        // No tree yet.
+        if(xTree = nullptr)
+        {
+            // Generate a tree
+            xRight = DoTerm(true).release();
+            xTerm.setSubnodesBinMo(xBottom, xOper, xRight);
+            xTree = xTerm;
+            xBottom = xTree;
+            continue;
+        }
+
+        // We start by the bottom
+        // Is the bottom the new parent ?
+        if( xBottom.getSubnodeBinMo(1).priority() < mpriority)
+        {
+            // Bottom has less priority
+            xRight = DoTerm(true).release();
+            xTerm.setSubnodesBinMo(xBottom.getSubnodeBinMo(2),xOper, xRight);
+            xBottom.setSubnodesBinMo(xBottom.getSubnodeBinMo(0),xBottom.getSubnodeBinMo(1),xTerm);
+            // Where is the new bottom?
+            xBottom = xTerm;
+        }
+        // Is there more room to search ?
+        else if(xBottom != xTree)
+        {
+            // We go up one step
+            xBottom = xBottom->GetParent();
+        }
+        // There is no more room to search
+        else if(xBottom == xTree)
+        {
+            // xTerm is the new tree
+            xRight = DoTerm(true).release();
+            xTerm.setSubnodesBinMo(xBottom, xOper, xRight);
+            xTree = xTerm;
+            // Left branches can never be reached
+            // So the bottom now is the tree head ;)
+            xBottom = xTree;
+        }
+    }
+
+    // In case there where no operators
+    return xFirst;
 }
 
 std::unique_ptr<SmNode> SmParser::DoRelation()
