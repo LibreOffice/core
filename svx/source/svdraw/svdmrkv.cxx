fad183821dd957d5eea4fc7c159ebbd1b25c384d
mert <mert.tumer@collabora.com>
2021-02-08 10:31:38 +0300

    Exclude table selection from handle message
    Change-Id: Ic079c2d59040c8b6d714af3932d5294c2060edc0
    Signed-off-by: mert <mert.tumer@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/110548
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

--- svx/source/svdraw/svdmrkv.cxx
+++ svx/source/svdraw/svdmrkv.cxx
@@ -883,7 +883,7 @@ void SdrMarkView::SetMarkHandlesForLOKit(tools::Rectangle const & rRect, const S
                     }
                 }
             }
-            if (!pOtherShell && maHdlList.GetHdlCount())
+            if (!bTableSelection && !pOtherShell && maHdlList.GetHdlCount())
             {
                 boost::property_tree::ptree responseJSON;
                 boost::property_tree::ptree others;
