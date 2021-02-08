cac63972b6796b799d1797f18193694ae34037cc
László Németh <nemeth@numbertext.org>
2021-02-08 12:29:17 +0100

    tdf#139301 line dash interoperability: fix asynchron UI test
    to avoid false alarms during slow testing. Also the test
    doesn't open the same file n times for testing the n line dash
    styles now.
    
    The problem reported by Stephan Bergmann.
    
    Change-Id: I4b072f334a59abda373ab3a92f27afafda981145

--- sw/qa/uitest/writer_tests7/tdf139301.py
+++ sw/qa/uitest/writer_tests7/tdf139301.py
@@ -10,15 +10,16 @@ from uitest.uihelper.common import get_state_as_dict, get_url_for_data_file
 class tdf139301(UITestCase):
 
     def test_tdf139301(self):
+        writer_doc = self.ui_test.load_file(get_url_for_data_file("tdf127166_prstDash_Word97.docx"))
+
         styles = ('Long Dash', 'Long Dash Dot', 'Long Dot', 'Double Dash', 'Double Dash Dot', 'Double Dash Dot Dot', 'Dash', 'Dash Dot', 'Dash Dot Dot', 'Dot')
 
-        for i in range(len(styles)):
-            writer_doc = self.ui_test.load_file(get_url_for_data_file("tdf127166_prstDash_Word97.docx"))
-            xWriterDoc = self.xUITest.getTopFocusWindow()
-            xWriterEdit = xWriterDoc.getChild("writer_edit")
+        xWriterDoc = self.xUITest.getTopFocusWindow()
+        xWriterEdit = xWriterDoc.getChild("writer_edit")
 
-            for i in range(i+1):
-                self.xUITest.executeCommand(".uno:JumpToNextFrame")
+        for i in range(len(styles)):
+            # select next line shape
+            writer_doc.getCurrentController().select(writer_doc.getDrawPage()[i])
 
             # wait for available line style setting
             self.ui_test.wait_until_child_is_available(xWriterEdit, 'metricfield')
@@ -35,7 +36,7 @@ class tdf139301(UITestCase):
             xOKBtn = xFormatLineDlg.getChild("ok")
             self.ui_test.close_dialog_through_button(xOKBtn)
 
-            self.ui_test.close_doc()
+        self.ui_test.close_doc()
 
 # vim: set shiftwidth=4 softtabstop=4 expandtab:
 
