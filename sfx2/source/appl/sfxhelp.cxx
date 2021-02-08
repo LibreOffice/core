6b51f618e3ddfc7350a25a1f2bde689fed735f36
Szymon Kłos <szymon.klos@collabora.com>
2021-02-08 19:01:00 +0100

    lok: send help even when document not ready
    Change-Id: I44855ef00a6c54a774965b3c951a4476ed5c8b01
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/110593
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Henry Castro <hcastro@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/112159
    Tested-by: Jenkins
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>

--- sfx2/source/appl/sfxhelp.cxx
+++ sfx2/source/appl/sfxhelp.cxx
@@ -719,6 +719,13 @@ static bool impl_showOnlineHelp( const OUString& rURL )
                                                    aHelpLink.toUtf8().getStr());
             return true;
         }
+        else if (GetpApp())
+        {
+            GetpApp()->libreOfficeKitViewCallback(LOK_CALLBACK_HYPERLINK_CLICKED,
+                                                   aHelpLink.toUtf8().getStr());
+            return true;
+        }
+
         return false;
     }
 
