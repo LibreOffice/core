64cfe7f616f7fb5d9bf16f8642c9da27a7590bb2
Gülşah Köse <gulsah.kose@collabora.com>
2021-02-10 20:14:22 +0300

    tdf#134210 Better testing for cropping bitmap of custom shape.
    Change-Id: I592e2e99306a4aa27dce17e8b8d75e4bb2262cab

--- sd/qa/unit/import-tests.cxx
+++ sd/qa/unit/import-tests.cxx
@@ -213,7 +213,7 @@ public:
     void testShapeGlowEffectPPTXImpoer();
     void testShapeBlurPPTXImport();
     void testMirroredGraphic();
-    void testCropPositionGraphic();
+    void testTdf134210CropPosition();
 
     bool checkPattern(sd::DrawDocShellRef const & rDocRef, int nShapeNumber, std::vector<sal_uInt8>& rExpected);
     void testPatternImport();
@@ -343,7 +343,7 @@ public:
     CPPUNIT_TEST(testShapeGlowEffectPPTXImpoer);
     CPPUNIT_TEST(testShapeBlurPPTXImport);
     CPPUNIT_TEST(testMirroredGraphic);
-    CPPUNIT_TEST(testCropPositionGraphic);
+    CPPUNIT_TEST(testTdf134210CropPosition);
 
     CPPUNIT_TEST_SUITE_END();
 };
@@ -3323,8 +3323,9 @@ void SdImportTest::testMirroredGraphic()
     xDocShRef->DoClose();
 }
 
-void SdImportTest::testCropPositionGraphic()
+void SdImportTest::testTdf134210CropPosition()
 {
+    // We are testing crop position of bitmap in custom shapes. We should see only green with proper fix.
     sd::DrawDocShellRef xDocShRef = loadURL(m_directories.getURLFromSrc(u"sd/qa/unit/data/pptx/crop-position.pptx"), PPTX);
     uno::Reference<beans::XPropertySet> xShape(getShapeFromPage(0, 0, xDocShRef), uno::UNO_SET_THROW);
     CPPUNIT_ASSERT(xShape.is());
@@ -3333,7 +3334,7 @@ void SdImportTest::testCropPositionGraphic()
     CPPUNIT_ASSERT(xGraphic.is());
     Graphic aGraphic(xGraphic);
     BitmapEx aBitmap(aGraphic.GetBitmapEx());
-    CPPUNIT_ASSERT_EQUAL( Color(8682893), aBitmap.GetPixelColor( 0, 0 ));
+    CPPUNIT_ASSERT_EQUAL( Color(8508442), aBitmap.GetPixelColor( 0, 0 ));
     xDocShRef->DoClose();
 }
 
