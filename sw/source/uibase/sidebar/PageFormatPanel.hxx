b57091fd2c74d42146f0e9d60d1d7c491b242c5e
buseorak <buseorak@hotmail.com>
2021-02-13 18:24:29 +0000

    remove blank line
    Change-Id: I8f0a6654be335053c6cc1d398765060275feb092

--- sw/source/uibase/sidebar/PageFormatPanel.hxx
+++ sw/source/uibase/sidebar/PageFormatPanel.hxx
@@ -16,7 +16,6 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
-
 #pragma once
 
 #include <com/sun/star/frame/XFrame.hpp>
