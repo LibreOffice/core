9d161857f1d4afcb772b477455797a2da0e47a9b
Armin Le Grand (Allotropia) <armin.le.grand@me.com>
2021-02-16 18:20:32 +0100

    tdf#127471 correct EMF/WMF im/export for scaled font
    If FontScaling is used, system-dependent data is held at
    vcl::Font Width(). Already if not scaled, we have three
    definitions: Width is zero, Width is equal to Height (unx)
    or - on Windows - Width equals avgFontWidth.
    If used it is W!=H where on unx Width equals Height multiplied
    with the scale factor. On Windows, this is Width multiplied
    with the only there existing avgFontWidth.
    Unfortunately that is ex/imported (since ever) undetected
    to EMF/WMF thus making EMF/WMF files containing FontScaling
    system-dependent - on which system was LO running when
    creating the file? The error can be seen when loading such
    a EMF/WMF on the vice-versa system, the FontScale is very
    ugly and wrong.
    Since EMF/WMF *are* Windows-specific formats the Windows-like
    definition is the correct one. This change makes all other
    systems export that now, and adapt on import to their system-
    specific definition (assuming coming from Windows).
    As can be seen, the difficulty is that these adaptions are
    necessary on non-Windows plattforms, but these do not have
    that avgFontWidth available. Thus I made a deep-dive
    investigation and multiple experiments to create a as
    similar as possible value to apply the needed calculations.
    For details and discussion refer to the bug description.
    
    Change-Id: I983fb6d882e2e8fccf9c8460f01509201d8157f9
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/111000
    Tested-by: Jenkins
    Reviewed-by: Armin Le Grand <Armin.Le.Grand@me.com>

--- vcl/source/filter/wmf/emfwr.cxx
+++ vcl/source/filter/wmf/emfwr.cxx
@@ -459,10 +459,33 @@ void EMFWriter::ImplCheckTextAttr()
     sal_uInt16       i;
     sal_uInt8        nPitchAndFamily;
 
+    // tdf#127471 adapt nFontWidth from NormedFontScaling to
+    // Windows-like notation if used for text scaling
+    const tools::Long nFontHeight(rFont.GetFontSize().Height());
+    tools::Long nFontWidth(rFont.GetFontSize().Width());
+
+#ifndef _WIN32
+    const bool bFontScaledHorizontally(nFontWidth != 0 && nFontWidth != nFontHeight);
+
+    if(bFontScaledHorizontally)
+    {
+        // tdf#127471 nFontWidth is the non-Windows NormedFontScaling, need to convert to
+        // Windows-like notation with pre-multiplied AvgFontWidth since EMF/WMF are Windows
+        // specific formats.
+        const tools::Long nAverageFontWidth(rFont.GetOrCalculateAverageFontWidth());
+
+        if(nAverageFontWidth > 0)
+        {
+            const double fScaleFactor(static_cast<double>(nAverageFontWidth) / static_cast<double>(nFontHeight));
+            nFontWidth = static_cast<tools::Long>(static_cast<double>(nFontWidth) * fScaleFactor);
+        }
+    }
+#endif
+
     ImplBeginRecord( WIN_EMR_EXTCREATEFONTINDIRECTW );
     m_rStm.WriteUInt32( mnTextHandle );
-    ImplWriteExtent( -rFont.GetFontSize().Height() );
-    ImplWriteExtent( rFont.GetFontSize().Width() );
+    ImplWriteExtent( -nFontHeight );
+    ImplWriteExtent( nFontWidth );
     m_rStm.WriteInt32( rFont.GetOrientation().get() ).WriteInt32( rFont.GetOrientation().get() );
 
     switch( rFont.GetWeight() )
