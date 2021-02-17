5e4fe3e8acc84412a856ea42be8c5dee9732ab72
dante <dante19031999@gmail.com>
2021-02-17 13:55:33 +0100

    Starmath now allows multiple parsers
    It is achieved using multiple syntax versions.
    Using as defualt smsyntax5 in order to be able to code parser6 without
    provoking any backguards compatibility problem.
    It can be accessed via expert configuration (but don't expect it to do anything on this patch level).
    
    This is an intermediate step towards making a parser wich supports
    mathml standards.
    
    Change-Id: Iccf73e5ba96870450b98907f78a2a0c28abf6093

--- starmath/inc/mathml/mathmlimport.hxx
+++ starmath/inc/mathml/mathmlimport.hxx
@@ -78,6 +78,7 @@ class SmXMLImport : public SvXMLImport
     bool bSuccess;
     int nParseDepth;
     OUString aText;
+    sal_uInt16 mnSmSyntaxVersion;
 
 public:
     SmXMLImport(const css::uno::Reference<css::uno::XComponentContext>& rContext,
@@ -108,6 +109,8 @@ public:
     void IncParseDepth() { ++nParseDepth; }
     bool TooDeep() const { return nParseDepth >= 2048; }
     void DecParseDepth() { --nParseDepth; }
+    void SetSmSyntaxVersion(sal_uInt16 nSmSyntaxVersion) { mnSmSyntaxVersion = nSmSyntaxVersion; }
+    sal_uInt16 GetSmSyntaxVersion() const { return mnSmSyntaxVersion; }
 };
 
 #endif
