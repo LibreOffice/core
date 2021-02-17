7c2c4256d7268bc6e820b13c9d2f140ff968b726
Alain Romedenne <alain.romedenne@libreoffice.org>
2021-02-17 15:50:50 +0100

    VarType, TypeName Basic QA test cases
    Change-Id: I51bac3caf555d94e91de7c6d624b3fca9e73a673

--- basic/qa/basic_coverage/test_typename_method.vb
+++ basic/qa/basic_coverage/test_typename_method.vb
@@ -6,11 +6,52 @@
 ' file, You can obtain one at http://mozilla.org/MPL/2.0/.
 '
 
+Type UDF ' User defined type
+    l As Objec
+    s as String
+End Type
+Dim myType as UDF
+
+Dim int16 As Integer, int32 As Long,   float As Single, flaot As Double, _
+    curr  As Currency,  dat As Date,     str As String,   obj As Object, _
+    myUDF As UDF,       var As Variant, byt3 As Byte,    bool As Boolean
+    
+Dim int_%, long_&, single_!, double_#, currency_@, string_$, array_
+
+Dim intArray() As Integer,  lngArray(5) As Long, sngArray!() As Single, dblArray#(8) As Double, _
+    curArray@() As Currency, datArray() As Date, strArray$() As String,   objArray() As Object, _
+    varArray() As Variant,  byteArray() As Byte, boolArray() As Boolean 
+
 Function doUnitTest as Integer
-    ' TYPENAME
-    If ( TypeName("Hello") <> "String" ) Then
-        doUnitTest = 0
-    Else
-        doUnitTest = 1
-    End If
+    ' TypeName()
+    doUnitTest = 0 ' Assume failure by default
+    
+    If TypeName(int16) <> "Integer"  Or TypeName(int32) <> "Long"     Then Exit Function
+    If TypeName(float) <> "Single"   Or TypeName(flaot) <> "Double"   Then Exit Function
+    If  TypeName(curr) <> "Currency" Or   TypeName(dat) <> "Date"     Then Exit Function   
+    If  TypeName(byt3) <> "Byte"     Or  TypeName(bool) <> "Boolean"  Then Exit Function
+    If   TypeName(str) <> "String"   Or   TypeName(obj) <> "Object"   Then Exit Function 
+    If TypeName(myUDF) <> "Object"  Or    TypeName(var) <> "Empty"    Then Exit Function 
+    
+    If      TypeName(int_) <> "Integer"  Or   TypeName(long_) <> "Long"   Then Exit Function 
+    If   TypeName(single_) <> "Single"   Or TypeName(double_) <> "Double" Then Exit Function 
+    If TypeName(currency_) <> "Currency" Or TypeName(string_) <> "String" Then Exit Function 
+
+    If  TypeName(intArray) <> "Integer()"  Then Exit Function
+    If  TypeName(lngArray) <> "Long()"     Then Exit Function
+    If  TypeName(sngArray) <> "Single()"   Then Exit Function
+    If  TypeName(dblArray) <> "Double()"   Then Exit Function
+    If  TypeName(curArray) <> "Currency()" Then Exit Function
+    If  TypeName(datArray) <> "Date()"     Then Exit Function
+    If  TypeName(strArray) <> "String()"   Then Exit Function
+    If  TypeName(objArray) <> "Object()"   Then Exit Function
+    If TypeName(boolArray) <> "Boolean()"  Then Exit Function
+    If  TypeName(varArray) <> "Variant()"  Then Exit Function
+    If TypeName(byteArray) <> "Byte()"     Then Exit Function
+
+    doUnitTest = 1 ' All checks passed
 End Function
+
+Sub DEV_TEST
+    Print doUnitTest
+End Sub
