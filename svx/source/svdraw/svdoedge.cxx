3e12d347fa723bc4c7d0e9aa6d252f75188ea888
mert <mert.tumer@collabora.com>
2021-02-17 09:12:25 +0300

    Fix connectors cannot select a glue point
    Change-Id: I6b6d2dad27910ac6d5de43e9d0992e3565f5be5e
    Signed-off-by: mert <mert.tumer@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/111024

--- svx/source/svdraw/svdoedge.cxx
+++ svx/source/svdraw/svdoedge.cxx
@@ -43,6 +43,7 @@
 #include <svx/sxenditm.hxx>
 #include <svx/xpoly.hxx>
 #include <vcl/ptrstyle.hxx>
+#include <comphelper/lok.hxx>
 
 void SdrObjConnection::ResetVars()
 {
@@ -2140,7 +2141,10 @@ bool SdrEdgeObj::ImpFindConnector(const Point& rPt, const SdrPageView& rPV, SdrO
     // sensitive area of connectors is twice as large as the one of the handles
     sal_uInt16 nMarkHdSiz=rPV.GetView().GetMarkHdlSizePixel();
     Size aHalfConSiz(nMarkHdSiz,nMarkHdSiz);
-    aHalfConSiz=pOut->PixelToLogic(aHalfConSiz);
+    if (comphelper::LibreOfficeKit::isActive())
+        aHalfConSiz=pOut->PixelToLogic(aHalfConSiz, MapMode(MapUnit::Map100thMM));
+    else
+        aHalfConSiz=pOut->PixelToLogic(aHalfConSiz);
     tools::Rectangle aMouseRect(rPt,rPt);
     aMouseRect.AdjustLeft( -(aHalfConSiz.Width()) );
     aMouseRect.AdjustTop( -(aHalfConSiz.Height()) );
