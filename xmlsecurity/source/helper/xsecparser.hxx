549e485791b9390dcbd78b900eb042f11000f24c
Michael Stahl <michael.stahl@allotropia.de>
2021-02-19 17:56:21 +0100

    xmlsecurity: ignore elements in ds:Object that aren't signed
    Change-Id: I2e4411f0907b89e7ad6e0185cee8f12b600515e8
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/111253
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
    (cherry picked from commit 2bfa00e6bf4b2a310a8b8f5060acec85b5f7a3ce)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/111909
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    (cherry picked from commit 94ce59dd02fcfcaa1eb4f195b45a9a2edbd58242)

--- xmlsecurity/source/helper/xsecparser.hxx
+++ xmlsecurity/source/helper/xsecparser.hxx
@@ -53,6 +53,7 @@ public:
     class Context;
 private:
     class UnknownContext;
+    class ReferencedContextImpl;
     class LoPGPOwnerContext;
     class DsPGPKeyPacketContext;
     class DsPGPKeyIDContext;
