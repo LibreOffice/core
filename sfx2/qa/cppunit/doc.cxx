LibreOffice/core

commit 5259ab8104cfba60c40748ed0cd59d93df038c5b

sfx2 store: create temp files next to local files
This way it's more likely that we can do a cheap rename instead of a
copy in SfxMedium::Commit().

Change-Id: I45c80cd19c3ab3bc70ecbf9793dbe1bb55994ee9
Reviewed-on: https://gerrit.libreoffice.org/47611
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>

sfx2/source/doc/docfile.cxx
@@ -3385,7 +3385,18 @@ void SfxMedium::CreateTempFile( bool bReplace )
         pImpl->m_aName.clear();
     }
 
-    pImpl->pTempFile = new ::utl::TempFile();
+    OUString aLogicBase;
+    if (comphelper::isFileUrl(pImpl->m_aLogicName))
+    {
+        // Try to create the temp file in the same directory.
+        sal_Int32 nOffset = pImpl->m_aLogicName.lastIndexOf("/");
+        if (nOffset != -1)
+            aLogicBase = pImpl->m_aLogicName.copy(0, nOffset);
+        if (aLogicBase == "file://")
+            // Doesn't make sense.
+            aLogicBase.clear();
+    }
+    pImpl->pTempFile = new ::utl::TempFile(aLogicBase.isEmpty() ? nullptr : &aLogicBase);
     pImpl->pTempFile->EnableKillingFile();
     pImpl->m_aName = pImpl->pTempFile->GetFileName();
     OUString aTmpURL = pImpl->pTempFile->GetURL();

commit 4e78044783fdf0725cc2a076ff0d3d0f14ecf72e

sfx2 store: fix export to PDF when dir name is URL-encoded
Regression from commit 5259ab8104cfba60c40748ed0cd59d93df038c5b (sfx2
store: create temp files next to local files, 2018-01-08), the problem
was that the directory hosting the input ODT file was called
"Bugzilla%C3%BF" (URL-encoded from "ÿ"), we got this as
"Bugzilla%25C3%25BF" (encoded from directory name), but we forgot to
decode it before passing this URL to the utl::TempFile ctor as a parent
directory, which resulted in failing to save as PDF, and crashing on
DOCX export.

Note that in practice this only caused a problem on Windows.

Change-Id: I5fc8a356b63efc2f16405a14ee59a061b52c5205

sfx2/CppunitTest_sfx2_doc.mk
@@ -27,6 +27,7 @@ $(eval $(call gb_CppunitTest_use_libraries,sfx2_doc, \
     unotest \
     sfx \
     svl \
+    utl \
 ))
 
 $(eval $(call gb_CppunitTest_use_sdk_api,sfx2_doc))

sfx2/source/doc/docfile.cxx
@@ -3626,7 +3626,7 @@ bool SfxMedium::SetWritableForUserOnly( const OUString& aURL )
 namespace
 {
 /// Get the parent directory of a temporary file for output purposes.
-OUString GetLogicBase(std::unique_ptr<SfxMedium_Impl> const & pImpl)
+OUString GetLogicBase(const INetURLObject& rURL, std::unique_ptr<SfxMedium_Impl> const & pImpl)
 {
     OUString aLogicBase;
 
@@ -3637,15 +3637,12 @@ OUString GetLogicBase(std::unique_ptr<SfxMedium_Impl> const & pImpl)
     (void) pImpl;
 #else
 
-    if (comphelper::isFileUrl(pImpl->m_aLogicName) && !pImpl->m_pInStream)
+    if (rURL.GetProtocol() == INetProtocol::File && !pImpl->m_pInStream)
     {
         // Try to create the temp file in the same directory when storing.
-        sal_Int32 nOffset = pImpl->m_aLogicName.lastIndexOf("/");
-        if (nOffset != -1)
-            aLogicBase = pImpl->m_aLogicName.copy(0, nOffset);
-        if (aLogicBase == "file://")
-            // Doesn't make sense.
-            aLogicBase.clear();
+        INetURLObject aURL(rURL);
+        aURL.removeSegment();
+        aLogicBase = aURL.GetMainURL(INetURLObject::DecodeMechanism::WithCharset);
     }
 
     if (pImpl->m_bHasEmbeddedObjects)
@@ -3669,7 +3666,7 @@ void SfxMedium::CreateTempFile( bool bReplace )
         pImpl->m_aName.clear();
     }
 
-    OUString aLogicBase = GetLogicBase(pImpl);
+    OUString aLogicBase = GetLogicBase(GetURLObject(), pImpl);
     pImpl->pTempFile.reset( new ::utl::TempFile(aLogicBase.isEmpty() ? nullptr : &aLogicBase) );
     pImpl->pTempFile->EnableKillingFile();
     pImpl->m_aName = pImpl->pTempFile->GetFileName();
@@ -3765,7 +3762,7 @@ void SfxMedium::CreateTempFileNoCopy()
     // this call always replaces the existing temporary file
     pImpl->pTempFile.reset();
 
-    OUString aLogicBase = GetLogicBase(pImpl);
+    OUString aLogicBase = GetLogicBase(GetURLObject(), pImpl);
     pImpl->pTempFile.reset( new ::utl::TempFile(aLogicBase.isEmpty() ? nullptr : &aLogicBase) );
     pImpl->pTempFile->EnableKillingFile();
     pImpl->m_aName = pImpl->pTempFile->GetFileName();

sfx2/qa/cppunit/doc.cxx
@@ -17,6 +17,9 @@
 #include <comphelper/propertyvalue.hxx>
 #include <sfx2/objsh.hxx>
 #include <sfx2/sfxbasemodel.hxx>
+#include <unotools/tempfile.hxx>
+#include <sfx2/docfile.hxx>
+#include <osl/file.hxx>
 
 using namespace com::sun::star;
 
@@ -73,6 +76,34 @@ CPPUNIT_TEST_FIXTURE(Test, testNoGrabBagShape)
     // An uncaught exception of type com.sun.star.beans.UnknownPropertyException
     // which was not caught later, resulting in a crash.
     pObjectShell->GetSignPDFCertificate();
+}
+
+CPPUNIT_TEST_FIXTURE(Test, testTempFilePath)
+{
+    // Create a test file in a directory that contains the URL-encoded "testÿ" string.
+    getComponent() = loadFromDesktop("private:factory/swriter");
+    auto pBaseModel = dynamic_cast<SfxBaseModel*>(getComponent().get());
+    CPPUNIT_ASSERT(pBaseModel);
+    OUString aTargetDir
+        = m_directories.getURLFromWorkdir(u"CppunitTest/sfx2_doc.test.user/test%25C3%25Bf");
+    osl::Directory::create(aTargetDir);
+    OUString aTargetFile = aTargetDir + "/test.odt";
+    css::uno::Sequence<css::beans::PropertyValue> aArgs{ comphelper::makePropertyValue(
+        "FilterName", OUString("writer8")) };
+    pBaseModel->storeAsURL(aTargetFile, aArgs);
+    getComponent()->dispose();
+
+    // Load it and export to PDF.
+    getComponent() = loadFromDesktop(aTargetFile);
+    pBaseModel = dynamic_cast<SfxBaseModel*>(getComponent().get());
+    OUString aPdfTarget = aTargetDir + "/test.pdf";
+    css::uno::Sequence<css::beans::PropertyValue> aPdfArgs{ comphelper::makePropertyValue(
+        "FilterName", OUString("writer_pdf_Export")) };
+    // Without the accompanying fix in place, this test would have failed on Windows with:
+    // An uncaught exception of type com.sun.star.io.IOException
+    // because we first tried to create a temp file next to test.odt in a directory named
+    // "test%25C3%25Bf" instead of a directory named "test%C3%Bf".
+    pBaseModel->storeToURL(aPdfTarget, aPdfArgs);
 }
 }
 