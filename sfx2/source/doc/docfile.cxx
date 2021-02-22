55b7490decf9a22e609a4bcad717e20664da278c
Miklos Vajna <vmiklos@collabora.com>
2021-02-22 21:07:34 +0100

    tdf#139039 sfx2 store: fix export to PDF when dir name is URL-encoded
    Regression from commit 5259ab8104cfba60c40748ed0cd59d93df038c5b (sfx2
    store: create temp files next to local files, 2018-01-08), the problem
    was that the directory hosting the input ODT file was called
    "Bugzilla%C3%BF" (URL-encoded from "ÿ"), we got this as
    "Bugzilla%25C3%25BF" (encoded from directory name), but we forgot to
    decode it before passing this URL to the utl::TempFile ctor as a parent
    directory, which resulted in failing to save as PDF, and crashing on
    DOCX export.
    
    Note that in practice this only caused a problem on Windows.
    
    (cherry picked from commit aaf7fe0326b3be088a5440525363317fb3d0f158)
    
    Change-Id: I5fc8a356b63efc2f16405a14ee59a061b52c5205
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/111372
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
    Signed-off-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/111747
    (cherry picked from commit 55a6b24e0809246ec5700dcd76856df9d8348e36)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/111775
    Reviewed-by: Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>
    Tested-by: Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>

--- sfx2/source/doc/docfile.cxx
+++ sfx2/source/doc/docfile.cxx
@@ -3616,7 +3616,7 @@ bool SfxMedium::SetWritableForUserOnly( const OUString& aURL )
 namespace
 {
 /// Get the parent directory of a temporary file for output purposes.
-OUString GetLogicBase(std::unique_ptr<SfxMedium_Impl> const & pImpl)
+OUString GetLogicBase(const INetURLObject& rURL, std::unique_ptr<SfxMedium_Impl> const & pImpl)
 {
     OUString aLogicBase;
 
@@ -3627,15 +3627,12 @@ OUString GetLogicBase(std::unique_ptr<SfxMedium_Impl> const & pImpl)
     (void) pImpl;
 #else
 
-    if (comphelper::isFileUrl(pImpl->m_aLogicName) && !pImpl->m_pInStream)
+    if (rURL.GetProtocol() == INetProtocol::File && !pImpl->m_pInStream)
     {
         // Try to create the temp file in the same directory when storing.
-        sal_Int32 nOffset = pImpl->m_aLogicName.lastIndexOf("/");
-        if (nOffset != -1)
-            aLogicBase = pImpl->m_aLogicName.copy(0, nOffset);
-        if (aLogicBase == "file://")
-            // Doesn't make sense.
-            aLogicBase.clear();
+        INetURLObject aURL(rURL);
+        aURL.removeSegment();
+        aLogicBase = aURL.GetMainURL(INetURLObject::DecodeMechanism::WithCharset);
     }
 
     if (pImpl->m_bHasEmbeddedObjects)
@@ -3659,7 +3656,7 @@ void SfxMedium::CreateTempFile( bool bReplace )
         pImpl->m_aName.clear();
     }
 
-    OUString aLogicBase = GetLogicBase(pImpl);
+    OUString aLogicBase = GetLogicBase(GetURLObject(), pImpl);
     pImpl->pTempFile.reset( new ::utl::TempFile(aLogicBase.isEmpty() ? nullptr : &aLogicBase) );
     pImpl->pTempFile->EnableKillingFile();
     pImpl->m_aName = pImpl->pTempFile->GetFileName();
@@ -3755,7 +3752,7 @@ void SfxMedium::CreateTempFileNoCopy()
     // this call always replaces the existing temporary file
     pImpl->pTempFile.reset();
 
-    OUString aLogicBase = GetLogicBase(pImpl);
+    OUString aLogicBase = GetLogicBase(GetURLObject(), pImpl);
     pImpl->pTempFile.reset( new ::utl::TempFile(aLogicBase.isEmpty() ? nullptr : &aLogicBase) );
     pImpl->pTempFile->EnableKillingFile();
     pImpl->m_aName = pImpl->pTempFile->GetFileName();
