36538b33413b24241920d8c4d873beb5c671e13f
Gülşah Köse <gulsah.kose@collabora.com>
2021-02-22 14:44:03 +0300

    tdf#133473 Change SDRATTR_SHADOWSIZEX flag.
    SDRATTR_SHADOWSIZEX holds a percentage value. So we
    shouldn't use METRIC_ITEM.
    
    Change-Id: I9f1a20d5871f60d2899ef7d525f4cf837b0a9088
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/111311
    Tested-by: Jenkins
    Reviewed-by: Gülşah Köse <gulsah.kose@collabora.com>
    Signed-off-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/112241
    Signed-off-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/112262

--- sw/qa/extras/ooxmlexport/ooxmlexport16.cxx
+++ sw/qa/extras/ooxmlexport/ooxmlexport16.cxx
@@ -9,6 +9,11 @@
 
 #include <swmodeltestbase.hxx>
 
+#include <svx/svddef.hxx>
+#include <svx/unoapi.hxx>
+#include <svx/sdmetitm.hxx>
+#include <svx/svdobj.hxx>
+
 #include <com/sun/star/beans/XPropertySet.hpp>
 #include <com/sun/star/text/XTextViewCursorSupplier.hpp>
 #include <com/sun/star/text/XTextTable.hpp>
@@ -84,6 +89,22 @@ DECLARE_OOXMLEXPORT_TEST(testTdf138953, "croppedAndRotated.odt")
 DECLARE_OOXMLEXPORT_TEST(testTdf140137, "tdf140137.docx")
 {
     // Don't throw exception during load
+}
+
+DECLARE_OOXMLEXPORT_TEST(testTdf133473_shadowSize, "tdf133473.docx")
+{
+    uno::Reference<drawing::XShape> xShape = getShape(1);
+
+    SdrObject* pObj(GetSdrObjectFromXShape(xShape));
+    const SfxItemSet& rSet = pObj->GetMergedItemSet();
+    sal_Int32 nSize1 = rSet.Get(SDRATTR_SHADOWSIZEX).GetValue();
+
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 200000
+    // - Actual  : 113386
+    // I.e. Shadow size will be smaller than actual.
+
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(200000), nSize1);
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();
