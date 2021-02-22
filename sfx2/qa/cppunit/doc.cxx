55a6b24e0809246ec5700dcd76856df9d8348e36
Miklos Vajna <vmiklos@collabora.com>
2021-02-22 21:07:34 +0100

    tdf#139039 sfx2 store: fix export to PDF when dir name is URL-encoded
    Regression from commit 5259ab8104cfba60c40748ed0cd59d93df038c5b (sfx2
    store: create temp files next to local files, 2018-01-08), the problem
    was that the directory hosting the input ODT file was called
    "Bugzilla%C3%BF" (URL-encoded from "ÿ"), we got this as
    "Bugzilla%25C3%25BF" (encoded from directory name), but we forgot to
    decode it before passing this URL to the utl::TempFile ctor as a parent
    directory, which resulted in failing to save as PDF, and crashing on
    DOCX export.
    
    Note that in practice this only caused a problem on Windows.
    
    (cherry picked from commit aaf7fe0326b3be088a5440525363317fb3d0f158)
    
    Change-Id: I5fc8a356b63efc2f16405a14ee59a061b52c5205
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/111372
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
    Signed-off-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/111747

---
+++ sfx2/qa/cppunit/doc.cxx
@@ -0,0 +1,86 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <test/bootstrapfixture.hxx>
+#include <unotest/macros_test.hxx>
+
+#include <com/sun/star/frame/Desktop.hpp>
+#include <com/sun/star/view/XSelectionSupplier.hpp>
+#include <com/sun/star/drawing/XDrawPagesSupplier.hpp>
+
+#include <comphelper/propertyvalue.hxx>
+#include <sfx2/objsh.hxx>
+#include <sfx2/sfxbasemodel.hxx>
+#include <unotools/tempfile.hxx>
+#include <sfx2/docfile.hxx>
+#include <osl/file.hxx>
+
+using namespace com::sun::star;
+
+namespace
+{
+/// Covers sfx2/source/doc/ fixes.
+class Test : public test::BootstrapFixture, public unotest::MacrosTest
+{
+private:
+    uno::Reference<lang::XComponent> mxComponent;
+
+public:
+    void setUp() override;
+    void tearDown() override;
+    uno::Reference<lang::XComponent>& getComponent() { return mxComponent; }
+};
+
+void Test::setUp()
+{
+    test::BootstrapFixture::setUp();
+
+    mxDesktop.set(frame::Desktop::create(mxComponentContext));
+}
+
+void Test::tearDown()
+{
+    if (mxComponent.is())
+        mxComponent->dispose();
+
+    test::BootstrapFixture::tearDown();
+}
+
+CPPUNIT_TEST_FIXTURE(Test, testTempFilePath)
+{
+    // Create a test file in a directory that contains the URL-encoded "testÿ" string.
+    getComponent() = loadFromDesktop("private:factory/swriter");
+    auto pBaseModel = dynamic_cast<SfxBaseModel*>(getComponent().get());
+    CPPUNIT_ASSERT(pBaseModel);
+    OUString aTargetDir
+        = m_directories.getURLFromWorkdir(u"CppunitTest/sfx2_doc.test.user/test%25C3%25Bf");
+    osl::Directory::create(aTargetDir);
+    OUString aTargetFile = aTargetDir + "/test.odt";
+    css::uno::Sequence<css::beans::PropertyValue> aArgs{ comphelper::makePropertyValue(
+        "FilterName", OUString("writer8")) };
+    pBaseModel->storeAsURL(aTargetFile, aArgs);
+    getComponent()->dispose();
+
+    // Load it and export to PDF.
+    getComponent() = loadFromDesktop(aTargetFile);
+    pBaseModel = dynamic_cast<SfxBaseModel*>(getComponent().get());
+    OUString aPdfTarget = aTargetDir + "/test.pdf";
+    css::uno::Sequence<css::beans::PropertyValue> aPdfArgs{ comphelper::makePropertyValue(
+        "FilterName", OUString("writer_pdf_Export")) };
+    // Without the accompanying fix in place, this test would have failed on Windows with:
+    // An uncaught exception of type com.sun.star.io.IOException
+    // because we first tried to create a temp file next to test.odt in a directory named
+    // "test%25C3%25Bf" instead of a directory named "test%C3%Bf".
+    pBaseModel->storeToURL(aPdfTarget, aPdfArgs);
+}
+}
+
+CPPUNIT_PLUGIN_IMPLEMENT();
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
