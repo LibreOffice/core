6ae10100f1aa32329f91dd9eb4c062ee66255d2f
Armin Le Grand (Allotropia) <armin.le.grand@me.com>
2021-02-23 14:24:24 +0100

    tdf#127471 Detect&Correct EMF/WMF with wrong FontScale
    Before correcting our EMF/WMF export to write the Windows-
    specific data in the case of FontScaling, we wrote these
    files with wrong FontScaling.
    This change tries to detect and correct this at import, so
    that newer versions of the office on all plattforms can
    again load old, from us but not on Windows written EMF/WMF
    files.
    With this change we can read again all new and old EMF/WMF
    files (see table in task, comment 80).
    
    Change-Id: I1a0b0ab5f57c7cd40520401568af05cab4ecb4c8
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/111399
    Tested-by: Jenkins
    Reviewed-by: Armin Le Grand <Armin.Le.Grand@me.com>
    (cherry picked from commit 3d33e4ce3987ea17e73a72e84f7f0df7af8101a6)

--- emfio/inc/mtftools.hxx
+++ emfio/inc/mtftools.hxx
@@ -27,6 +27,8 @@
 #include <vcl/lineinfo.hxx>
 #include <vcl/outdevstate.hxx>
 #include <vcl/FilterConfigItem.hxx>
+#include <vcl/metaact.hxx>
+#include <rtl/ref.hxx>
 
 #include "emfiodllapi.h"
 
@@ -455,6 +457,24 @@ namespace emfio
         {}
     };
 
+    // tdf#127471 implement detection and correction of wrongly scaled
+    // fonts in own-written, old (before this fix) EMF/WMF files
+    class ScaledFontDetectCorrectHelper
+    {
+    private:
+        rtl::Reference<MetaFontAction>                                  maCurrentMetaFontAction;
+        std::vector<double>                                             maAlternativeFontScales;
+        std::vector<std::pair<rtl::Reference<MetaFontAction>, double>>  maPositiveIdentifiedCases;
+        std::vector<std::pair<rtl::Reference<MetaFontAction>, double>>  maNegativeIdentifiedCases;
+
+    public:
+        ScaledFontDetectCorrectHelper();
+        void endCurrentMetaFontAction();
+        void newCurrentMetaFontAction(rtl::Reference<MetaFontAction>& rNewMetaFontAction);
+        void evaluateAlternativeFontScale(OUString const & rText, long nImportedTextLength);
+        void applyAlternativeFontScale();
+    };
+
     class EMFIO_DLLPUBLIC MtfTools
     {
         MtfTools(MtfTools const &) = delete;
@@ -517,6 +537,9 @@ namespace emfio
         sal_uInt32          mnStartPos;
         sal_uInt32          mnEndPos;
         std::vector<std::unique_ptr<BSaveStruct>>    maBmpSaveList;
+
+        // tdf#127471 always try to detect - only used with ScaledText
+        ScaledFontDetectCorrectHelper maScaledFontHelper;
 
         bool                mbNopMode : 1;
         bool                mbFillStyleSelected : 1;
--- vcl/source/font/font.cxx
+++ vcl/source/font/font.cxx
@@ -33,6 +33,7 @@
 
 #include <rtl/instance.hxx>
 #include <TypeSerializer.hxx>
+#include <vcl/svapp.hxx>
 
 #ifdef _WIN32
 #include <vcl/metric.hxx>
@@ -366,34 +367,49 @@ void Font::GetFontAttributes( FontAttributes& rAttrs ) const
 // tdf#127471 for corrections on EMF/WMF we need the AvgFontWidth in Windows-specific notation
 long Font::GetOrCalculateAverageFontWidth() const
 {
-#ifdef _WIN32
-    // on windows we just have it available
-    return GetAverageFontWidth();
-#else
-    // On non-Windows systems we need to calculate AvgFontWidth
-    // as close as possible (discussion see documentation in task)
     if(0 == mpImplFont->GetCalculatedAverageFontWidth())
     {
-        // calculate it. For discussion of method used, see task
-        const std::size_t nSize(127 - 32);
-        std::array<sal_Unicode, nSize> aArray;
+        // VirtualDevice is not thread safe
+        SolarMutexGuard aGuard;
 
-        for(sal_Unicode a(0); a < nSize; a++)
+        // create unscaled copy of font (this), a VirtualDevice and set it there
+        vcl::Font aUnscaledFont(*this);
+        ScopedVclPtr<VirtualDevice> pTempVirtualDevice(VclPtr<VirtualDevice>::Create());
+        aUnscaledFont.SetAverageFontWidth(0);
+        pTempVirtualDevice->SetFont(aUnscaledFont);
+
+#ifdef _WIN32
+        // on Windows systems use FontMetric to get/create AverageFontWidth from system
+        const FontMetric aMetric(pTempVirtualDevice->GetFontMetric());
+        const_cast<Font*>(this)->mpImplFont->SetCalculatedAverageFontWidth(aMetric.GetAverageFontWidth());
+#else
+        // On non-Windows systems we need to calculate AvgFontWidth
+        // as close as possible (discussion see documentation in task),
+        // so calculate it. For discussion of method used, see task
+        // buffer measure string creation, will always use the same
+        static OUString aMeasureString;
+
+        if(!aMeasureString.getLength())
         {
-            aArray[a] = a + 32;
+            const std::size_t nSize(127 - 32);
+            std::array<sal_Unicode, nSize> aArray;
+
+            for(sal_Unicode a(0); a < nSize; a++)
+            {
+                aArray[a] = a + 32;
+            }
+
+            aMeasureString = OUString(aArray.data());
         }
 
-        vcl::Font aUnscaledFont(*this);
-        ScopedVclPtr<VirtualDevice> pVirDev(VclPtr<VirtualDevice>::Create());
-        aUnscaledFont.SetAverageFontWidth(0);
-        pVirDev->SetFont(aUnscaledFont);
         const double fAverageFontWidth(
-            pVirDev->GetTextWidth(OUString(aArray.data())) / static_cast<double>(nSize));
+            pTempVirtualDevice->GetTextWidth(aMeasureString, aMeasureString.getLength()) /
+            static_cast<double>(aMeasureString.getLength()));
         const_cast<Font*>(this)->mpImplFont->SetCalculatedAverageFontWidth(basegfx::fround(fAverageFontWidth));
+#endif
     }
 
     return mpImplFont->GetCalculatedAverageFontWidth();
-#endif
 }
 
 SvStream& ReadImplFont( SvStream& rIStm, ImplFont& rImplFont, long& rnNormedFontScaling )
@@ -517,7 +533,7 @@ SvStream& ReadFont( SvStream& rIStm, vcl::Font& rFont )
                 aUnscaledFont.SetAverageFontWidth(0);
                 const FontMetric aUnscaledFontMetric(Application::GetDefaultDevice()->GetFontMetric(aUnscaledFont));
 
-                if (aUnscaledFontMetric.GetAverageFontWidth() > 0)
+                if (nHeight > 0)
                 {
                     const double fScaleFactor(static_cast<double>(nNormedFontScaling) / static_cast<double>(nHeight));
                     nScaledWidth = basegfx::fround(static_cast<double>(aUnscaledFontMetric.GetAverageFontWidth()) * fScaleFactor);
@@ -881,10 +897,8 @@ ImplFont::ImplFont() :
     maFillColor( COL_TRANSPARENT ),
     mbWordLine( false ),
     mnOrientation( 0 ),
-    mnQuality( 0 )
-#ifndef _WIN32
-    , mnCalculatedAverageFontWidth(0)
-#endif
+    mnQuality( 0 ),
+    mnCalculatedAverageFontWidth( 0 )
 {}
 
 ImplFont::ImplFont( const ImplFont& rImplFont ) :
@@ -916,10 +930,8 @@ ImplFont::ImplFont( const ImplFont& rImplFont ) :
     maFillColor( rImplFont.maFillColor ),
     mbWordLine( rImplFont.mbWordLine ),
     mnOrientation( rImplFont.mnOrientation ),
-    mnQuality( rImplFont.mnQuality )
-#ifndef _WIN32
-    , mnCalculatedAverageFontWidth(rImplFont.mnCalculatedAverageFontWidth)
-#endif
+    mnQuality( rImplFont.mnQuality ),
+    mnCalculatedAverageFontWidth( rImplFont.mnCalculatedAverageFontWidth )
 {}
 
 bool ImplFont::operator==( const ImplFont& rOther ) const
