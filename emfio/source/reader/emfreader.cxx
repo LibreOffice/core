8d132ed9fb89667d70da5736320372afd2d42ef1
Armin Le Grand (Allotropia) <armin.le.grand@me.com>
2021-02-23 14:24:24 +0100

    tdf#127471 Detect&Correct EMF/WMF with wrong FontScale
    Before correcting our EMF/WMF export to write the Windows-
    specific data in the case of FontScaling, we wrote these
    files with wrong FontScaling.
    This change tries to detect and correct this at import, so
    that newer versions of the office on all plattforms can
    again load old, from us but not on Windows written EMF/WMF
    files.
    With this change we can read again all new and old EMF/WMF
    files (see table in task, comment 80).
    
    Change-Id: I1a0b0ab5f57c7cd40520401568af05cab4ecb4c8
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/111399
    Tested-by: Jenkins
    Reviewed-by: Armin Le Grand <Armin.Le.Grand@me.com>
    (cherry picked from commit 3d33e4ce3987ea17e73a72e84f7f0df7af8101a6)

--- emfio/source/reader/emfreader.cxx
+++ emfio/source/reader/emfreader.cxx
@@ -2034,6 +2034,10 @@ namespace emfio
             }
             mpInputStream->Seek( nNextPos );
         }
+
+        // tdf#127471
+        maScaledFontHelper.applyAlternativeFontScale();
+
         if( !maBmpSaveList.empty() )
             ResolveBitmapActions( maBmpSaveList );
 
