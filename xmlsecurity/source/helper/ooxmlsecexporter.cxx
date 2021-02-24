6324c8be13ff26abe632fd232c213bbab5d3b1e3
Michael Stahl <michael.stahl@allotropia.de>
2021-02-24 19:18:51 +0100

    xmlsecurity: multiple certificates inside X509Data
    Change-Id: I9633a980b0c18d58dfce24fc59396a833498a77d

--- xmlsecurity/source/helper/ooxmlsecexporter.cxx
+++ xmlsecurity/source/helper/ooxmlsecexporter.cxx
@@ -201,15 +201,19 @@ void OOXMLSecExporter::Impl::writeKeyInfo()
 {
     m_xDocumentHandler->startElement(
         "KeyInfo", uno::Reference<xml::sax::XAttributeList>(new SvXMLAttributeList()));
-    assert(!m_rInformation.X509Datas.empty());
-    for (auto const& it : m_rInformation.X509Datas)
+    assert(m_rInformation.GetSigningCertificate());
+    for (auto const& rData : m_rInformation.X509Datas)
     {
         m_xDocumentHandler->startElement(
             "X509Data", uno::Reference<xml::sax::XAttributeList>(new SvXMLAttributeList()));
-        m_xDocumentHandler->startElement(
-            "X509Certificate", uno::Reference<xml::sax::XAttributeList>(new SvXMLAttributeList()));
-        m_xDocumentHandler->characters(it.X509Certificate);
-        m_xDocumentHandler->endElement("X509Certificate");
+        for (auto const& it : rData)
+        {
+            m_xDocumentHandler->startElement(
+                "X509Certificate",
+                uno::Reference<xml::sax::XAttributeList>(new SvXMLAttributeList()));
+            m_xDocumentHandler->characters(it.X509Certificate);
+            m_xDocumentHandler->endElement("X509Certificate");
+        }
         m_xDocumentHandler->endElement("X509Data");
     }
     m_xDocumentHandler->endElement("KeyInfo");
