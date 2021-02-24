3e885c29ff1a295a20fdb8688d2875dd2a5fccd8
mert <mert.tumer@collabora.com>
2021-02-24 06:22:49 +0300

    Fix chart sub element rectangle and handle positions
    Change-Id: I486b542ae0bb4a987c98d92c1866895eafcf620c
    Signed-off-by: mert <mert.tumer@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/111440
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

--- svx/source/svdraw/svdmrkv.cxx
+++ svx/source/svdraw/svdmrkv.cxx
@@ -709,7 +709,7 @@ void SdrMarkView::SetMarkHandlesForLOKit(tools::Rectangle const & rRect, SfxView
 
     tools::Rectangle aSelection(rRect);
     bool bIsChart = false;
-
+    Point addLogicOffset = Point(0,0);
     if (!rRect.IsEmpty())
     {
         sal_uInt32 nTotalPaintWindows = this->PaintWindowCount();
@@ -724,6 +724,7 @@ void SdrMarkView::SetMarkHandlesForLOKit(tools::Rectangle const & rRect, SfxView
                 {
                     Point aOffsetPx = pWin->GetOffsetPixelFrom(*pViewShellWindow);
                     Point aLogicOffset = pWin->PixelToLogic(aOffsetPx);
+                    addLogicOffset = aLogicOffset;
                     aSelection.Move(aLogicOffset.getX(), aLogicOffset.getY());
                 }
             }
@@ -883,8 +884,6 @@ void SdrMarkView::SetMarkHandlesForLOKit(tools::Rectangle const & rRect, SfxView
                                                 }
                                                 sPolygonElem += R"elem(\" style=\"stroke: none; fill: rgb(114,159,207); fill-opacity: 0.8\"/>)elem";
 
-                                                aSelection = OutputDevice::LogicToLogic(aSelection, MapMode(MapUnit::MapTwip), MapMode(MapUnit::Map100thMM));
-
                                                 OString sSVGElem = R"elem(<svg version=\"1.2\" width=\")elem" +
                                                     OString::number(aSelection.GetWidth() / 100.0) +
                                                     R"elem(mm\" height=\")elem" +
@@ -928,6 +927,7 @@ void SdrMarkView::SetMarkHandlesForLOKit(tools::Rectangle const & rRect, SfxView
                     child.put("kind", kind);
                     child.put("pointer", static_cast<sal_Int32>(pHdl->GetPointer()));
                     Point pHdlPos = pHdl->GetPos();
+                    pHdlPos.Move(addLogicOffset.getX(), addLogicOffset.getY());
                     if (convertMapMode)
                         pHdlPos = OutputDevice::LogicToLogic(pHdlPos, MapMode(MapUnit::Map100thMM), MapMode(MapUnit::MapTwip));
                     point.put("x", pHdlPos.getX());
