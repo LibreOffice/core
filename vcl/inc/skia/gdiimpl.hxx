ca74826ae3454002e65cd43ff4d54b1f3c628a80
Luboš Luňák <l.lunak@centrum.cz>
2021-02-25 11:37:54 +0000

    generic Skia workaround for VCL sending empty size (tdf#140288)
    There have already been commits to handle this, but the case
    of getting the wrong size the first was still broken (or was
    fixed in the past and got broken again). Try to be generic
    by forcing these to be always considered to be offscreen
    and force non-zero size there.
    
    Change-Id: Ie366a296f7f6645333630fa31e9fe18d54c7fba8
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/111528
    Tested-by: Jenkins
    Reviewed-by: Luboš Luňák <l.lunak@collabora.com>
    (cherry picked from commit 9d8c04d2d5529626d649af3fcebb6d4b65193b28)

--- vcl/inc/skia/gdiimpl.hxx
+++ vcl/inc/skia/gdiimpl.hxx
@@ -239,7 +239,7 @@ protected:
     void checkSurface();
     void destroySurface();
     // Reimplemented for X11.
-    virtual bool avoidRecreateByResize() const { return false; }
+    virtual bool avoidRecreateByResize() const;
     void createWindowSurface(bool forceRaster = false);
     virtual void createWindowContext(bool forceRaster = false) = 0;
     void createOffscreenSurface();
@@ -249,7 +249,7 @@ protected:
 
     void setProvider(SalGeometryProvider* provider) { mProvider = provider; }
 
-    bool isOffscreen() const { return mProvider == nullptr || mProvider->IsOffScreen(); }
+    bool isOffscreen() const;
     bool isGPU() const { return mIsGPU; }
 
     void invert(basegfx::B2DPolygon const& rPoly, SalInvert eFlags);
