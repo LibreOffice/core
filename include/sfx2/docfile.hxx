8fcef114de43a4db25e7b11cd487ee1e1001c76b
Matt K <mattkse@gmail.com>
2021-02-26 10:24:38 -0600

    tdf#47065 Implement a new thread to check document access
    Thread checks for read/write access to opened file and whether lock file is available, every 60 seconds.  If access is allowed, then show UI dialog to the user asking to Reload. Thread is spawned after the user chooses to open a document as read-only.  If Reload is selected by the user then the document is reloaded with read/write access, then the thread terminates.
    
    Change-Id: I7555ce6f5df79c2c87216e0129ef3b2883c7d921

--- include/sfx2/docfile.hxx
+++ include/sfx2/docfile.hxx
@@ -30,6 +30,7 @@
 #include <svl/itemset.hxx>
 #include <tools/link.hxx>
 #include <tools/stream.hxx>
+#include <mutex>
 
 namespace com::sun::star::beans { struct PropertyValue; }
 namespace com::sun::star::embed { class XStorage; }
@@ -53,6 +54,7 @@ class SfxMedium_Impl;
 class INetURLObject;
 class SfxFrame;
 class DateTime;
+struct ImplSVEvent;
 
 namespace weld
 {
@@ -92,6 +94,18 @@ public:
                         SfxMedium( const css::uno::Sequence< css::beans::PropertyValue >& aArgs );
 
                         virtual ~SfxMedium() override;
+
+    DECL_STATIC_LINK(SfxMedium, ShowReloadEditableDialog, void*, void);
+    bool CheckCanGetLockfile() const;
+    void SetOriginallyReadOnly(bool val);
+    void LaunchCheckEditableWorkerThread();
+    void SetCheckEditableWorkerDone();
+    void SetCheckEditableWorkerRunning(bool bIsRunning);
+    void SetWorkerReloadEvent(ImplSVEvent* pEvent);
+    ImplSVEvent* GetWorkerReloadEvent() const;
+    bool GetCheckEditableWorkerRunning() const;
+    std::shared_ptr<std::recursive_mutex> GetCheckEditableMutex() const;
+    void CancelCheckEditableThread();
 
     void                UseInteractionHandler( bool );
     css::uno::Reference< css::task::XInteractionHandler >
---
+++ sfx2/source/doc/checkreadonlytask.cxx
@@ -0,0 +1,88 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <sfx2/docfile.hxx>
+#include <osl/file.hxx>
+#include <vcl/svapp.hxx>
+#include "checkreadonlytask.hxx"
+
+CheckReadOnlyTask::CheckReadOnlyTask(SfxMedium* pMed,
+                                     const std::shared_ptr<std::recursive_mutex>& pMutex,
+                                     const std::shared_ptr<std::condition_variable_any>& pCond,
+                                     const std::shared_ptr<bool>& pIsDestructed,
+                                     const std::shared_ptr<comphelper::ThreadTaskTag>& pTag)
+    : ThreadTask(pTag)
+    , _pMed(pMed)
+    , _pMutex(pMutex)
+    , _pMyCond(pCond)
+    , _pIsDestructed(pIsDestructed)
+{
+}
+
+// worker thread method
+void CheckReadOnlyTask::doWork()
+{
+    if (_pMed == nullptr || _pMyCond == nullptr || _pMutex == nullptr || _pIsDestructed == nullptr)
+        return;
+
+    std::unique_lock<std::recursive_mutex> lock(*_pMutex);
+
+    if (*_pIsDestructed || _pMed->GetWorkerReloadEvent() != nullptr
+        || _pMed->GetCheckEditableWorkerRunning())
+        return;
+
+    _pMed->SetCheckEditableWorkerRunning(true);
+
+    OUString aDocumentURL
+        = _pMed->GetURLObject().GetMainURL(INetURLObject::DecodeMechanism::WithCharset);
+
+    while (true)
+    {
+        switch (_pMyCond->wait_for(lock, std::chrono::seconds(60)))
+        {
+            case std::cv_status::timeout:
+                break;
+            default: // signalled or error
+                _pMed->SetCheckEditableWorkerRunning(false);
+                return;
+        }
+
+        // prevent multiple reload events from occurring
+        if (*_pIsDestructed || _pMed->GetWorkerReloadEvent() != nullptr
+            || !_pMed->GetCheckEditableWorkerRunning())
+        {
+            _pMed->SetCheckEditableWorkerRunning(false);
+            return;
+        }
+
+        // close the file handle so we can open it below and check for write access
+        _pMed->UnlockFile(true);
+
+        osl::File aFile(aDocumentURL);
+        if (aFile.open(osl_File_OpenFlag_Write) != osl::FileBase::E_None)
+            continue;
+
+        if (!_pMed->CheckCanGetLockfile())
+            continue;
+
+        _pMed->SetCheckEditableWorkerRunning(false);
+
+        if (aFile.close() != osl::FileBase::E_None)
+            return;
+
+        // we can load, ask user
+        ImplSVEvent* pEvent
+            = Application::PostUserEvent(LINK(nullptr, SfxMedium, ShowReloadEditableDialog), _pMed);
+        _pMed->SetWorkerReloadEvent(pEvent);
+
+        return;
+    }
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
