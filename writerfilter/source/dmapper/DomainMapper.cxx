b7712c7e8bf2eec084ee01a5eaad73dcf9ac3cd4
Attila Szűcs <szucs.attila3@nisz.hu>
2021-02-26 11:19:01 +0100

    tdf#140182 docx import: fix extra page break
    Altered the hack with m_bDummyParaAddedForTableInSection, to not cancel
    the handling of the special case: "page break + section break".
    
    2 breaks would normally result in 3 pages,
    but page break + section break is a special case
    that is handled so to break only 1 page that result only 2 pages.
    
    Table + page break + section break should be only 2 pages, but
    Because of the table, a hack set m_bDummyParaAddedForTableInSection
    for the entire section, that canceled the
    page break + section break special case handling, resulting 3 pages.
    
    Co-authored-by: Tibor Nagy (NISZ)
    Change-Id: Ibd8ab5444becdfd09d0d05eb246e6853914e1f05

--- writerfilter/source/dmapper/DomainMapper.cxx
+++ writerfilter/source/dmapper/DomainMapper.cxx
@@ -3253,7 +3253,10 @@ void DomainMapper::lcl_text(const sal_uInt8 * data_, size_t len)
                 case 0x0c: //page break
                     // page breaks aren't supported in footnotes and endnotes
                     if (!m_pImpl->IsInFootOrEndnote())
+                    {
                         m_pImpl->deferBreak(PAGE_BREAK);
+                        m_pImpl->SetIsDummyParaAddedForTableInSectionPage(false);
+                    }
                     return;
                 case 0x0e: //column break
                     m_pImpl->deferBreak(COLUMN_BREAK);
@@ -3589,7 +3592,7 @@ void DomainMapper::lcl_utext(const sal_uInt8 * data_, size_t len)
                            (!m_pImpl->GetParaChanged() && m_pImpl->GetParaSectpr()
                             && !bSingleParagraphAfterRedline
                             && !m_pImpl->GetParaHadField()
-                            && !m_pImpl->GetIsDummyParaAddedForTableInSection()
+                            && (!m_pImpl->GetIsDummyParaAddedForTableInSectionPage())
                             && !( pSectionContext && pSectionContext->GetBreakType() != -1 && pContext && pContext->isSet(PROP_BREAK_TYPE) )
                             && !m_pImpl->GetIsPreviousParagraphFramed()
                             && !m_pImpl->HasTopAnchoredObjects()
