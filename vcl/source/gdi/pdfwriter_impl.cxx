LibreOffice/core

commit 76b5dca9dc0ff60f8f62cbecdee68f8f3b287ceb

tdf#113448 don't export any font for radio buttons
Change-Id: Ie84b19a3dfaec32184bb825b7593ec33a5c4145c
Reviewed-on: https://gerrit.libreoffice.org/74994
Tested-by: Jenkins
Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>
(cherry picked from commit 6ec26ba3aa195eac62fb8803137070d23a69491c)
Reviewed-on: https://gerrit.libreoffice.org/75012
Reviewed-by: Katarina Behrens <Katarina.Behrens@cib.de>

vcl/source/gdi/pdfwriter_impl.cxx
@@ -4061,12 +4061,7 @@ void PDFWriterImpl::createDefaultRadioButtonAppearance( PDFWidget& rBox, const P
     pop();
 
     OStringBuffer aDA( 256 );
-    const pdf::BuildinFont& rBestFont = pdf::BuildinFontFace::Get(
-        getBestBuildinFont(Font("ZapfDingbats", aFont.GetFontSize())));
     appendNonStrokingColor( replaceColor( rWidget.TextColor, rSettings.GetRadioCheckTextColor() ), aDA );
-    aDA.append( ' ' );
-    aDA.append(rBestFont.getNameObject());
-    aDA.append( " 0 Tf" );
     rBox.m_aDAString = aDA.makeStringAndClear();
     //to encrypt this (el)
     rBox.m_aMKDict = "/CA";
@@ -4083,10 +4078,8 @@ void PDFWriterImpl::createDefaultRadioButtonAppearance( PDFWidget& rBox, const P
     aDA.append( "/Tx BMC\nq BT\n" );
     appendNonStrokingColor( replaceColor( rWidget.TextColor, rSettings.GetRadioCheckTextColor() ), aDA );
     aDA.append( ' ' );
-    aDA.append(rBestFont.getNameObject());
-    aDA.append( ' ' );
     m_aPages[m_nCurrentPage].appendMappedLength( sal_Int32( aCheckRect.GetHeight() ), aDA );
-    aDA.append( " Tf\n0 0 Td\nET\nQ\n" );
+    aDA.append( " 0 0 Td\nET\nQ\n" );
     writeBuffer( aDA.getStr(), aDA.getLength() );
     setFillColor( replaceColor( rWidget.TextColor, rSettings.GetRadioCheckTextColor() ) );
     setLineColor( COL_TRANSPARENT );

commit c2c41c18164e298e726e077b0f2d83986abd0f60

tdf#127217: Fix buttons in forms not working in Adobe Reader
Regression from 76b5dca9dc0ff60f8f62cbecdee68f8f3b287ceb:
author	Thorsten Behrens <Thorsten.Behrens@CIB.de>	2019-04-09 02:19:14 +0200
committer	Katarina Behrens <Katarina.Behrens@cib.de>	2019-07-03 14:48:45 +0200
commit	76b5dca9dc0ff60f8f62cbecdee68f8f3b287ceb (patch)
tree	e778dad33073a228eb61ab62ce5f38e2a26d07ca
parent	1e8a9c19a6f7b82c9e0c481aee0c04c0780f4433 (diff)
tdf#113448 don't export any font for radio buttons

Don't let only color value in m_aDAString since it'll go here:
        if( !rWidget.m_aDAString.isEmpty() )
        {
            if( !rWidget.m_aDRDict.isEmpty() )
            {
                aLine.append( "/DR<<" );
                aLine.append( rWidget.m_aDRDict );
                aLine.append( ">>\n" );
            }
            else
            {
                aLine.append( "/DR<</Font<<" );
                appendBuildinFontsToDict( aLine );
                aLine.append( ">>>>\n" );
            }
            aLine.append( "/DA" );
            appendLiteralStringEncrypt( rWidget.m_aDAString, rWidget.m_nObject, aLine );
            aLine.append( "\n" );
            if( rWidget.m_nTextStyle & DrawTextFlags::Center )
                aLine.append( "/Q 1\n" );
            else if( rWidget.m_nTextStyle & DrawTextFlags::Right )
                aLine.append( "/Q 2\n" );
        }

But if we shouldn't have /DR at all since we don't want fonts for radiobutton, it also means
we don't want /DA too.
Indeed at the end of 12.7.3.3 https://www.adobe.com/content/dam/acom/en/devnet/pdf/pdfs/PDF32000_2008.pdf:
The default appearance string (DA) contains any graphics state or text state operators needed to establish the
graphics state parameters, such as text size and colour, for displaying the field’s variable text. Only operators
that are allowed within text objects shall occur in this string (see Figure 9). At a minimum, the string shall

vcl/source/gdi/pdfwriter_impl.cxx
@@ -4022,9 +4022,6 @@ void PDFWriterImpl::createDefaultRadioButtonAppearance( PDFWidget& rBox, const P
 
     pop();
 
-    OStringBuffer aDA( 256 );
-    appendNonStrokingColor( replaceColor( rWidget.TextColor, rSettings.GetRadioCheckTextColor() ), aDA );
-    rBox.m_aDAString = aDA.makeStringAndClear();
     //to encrypt this (el)
     rBox.m_aMKDict = "/CA";
     //after this assignment, to m_aMKDic cannot be added anything
@@ -4037,6 +4034,7 @@ void PDFWriterImpl::createDefaultRadioButtonAppearance( PDFWidget& rBox, const P
     SvMemoryStream* pCheckStream = new SvMemoryStream( 256, 256 );
 
     beginRedirect( pCheckStream, aCheckRect );
+    OStringBuffer aDA( 256 );
     aDA.append( "/Tx BMC\nq BT\n" );
     appendNonStrokingColor( replaceColor( rWidget.TextColor, rSettings.GetRadioCheckTextColor() ), aDA );
     aDA.append( ' ' );