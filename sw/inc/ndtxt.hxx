e779b52045ec8ea4dbe11a75500013c51539a478
heet-2312 <surana.heet1811@gmail.com>
2021-03-01 11:36:46 +0530

    tdf#124176 Use pragma once in sw/inc/ndtxt.hxx
    Change-Id: I75b6788e21ac010daea1cac5661887a0812d1773

--- sw/inc/ndtxt.hxx
+++ sw/inc/ndtxt.hxx
@@ -16,8 +16,7 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
-#ifndef INCLUDED_SW_INC_NDTXT_HXX
-#define INCLUDED_SW_INC_NDTXT_HXX
+#pragma once
 
 #include <cppuhelper/weakref.hxx>
 
@@ -867,7 +866,5 @@ inline sal_Int32 SwTextNode::GetSpaceLeft() const
     const sal_Int32 TXTNODE_MAX = SAL_MAX_INT32 - 2;
     return TXTNODE_MAX-m_Text.getLength();
 }
-
-#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
