LibreOffice/core

commit 7d42346ba77c9c4df241ea40eaf550993ca18783

tdf#90421 RTF export: ignore hyperlinks without an URL
Commit fe444d1f74abe417962be0bcd3340f40f2446b58 (fdo#62536: sw: fix
AutoCorrect bold/underline on existing AUTOFMT, 2013-06-20) adds an
empty inet format hint on the text node during autocorrection, on export
we can safely ignore that to provide well-formed output.

Change-Id: Iafae941a13e29dcc7d8b4bade5ce94a486b59638

sw/source/filter/ww8/rtfattributeoutput.cxx
@@ -423,52 +423,58 @@ void RtfAttributeOutput::EndRuby()
 
 bool RtfAttributeOutput::StartURL(const OUString& rUrl, const OUString& rTarget)
 {
-    m_aStyles.append('{');
-    m_aStyles.append(OOO_STRING_SVTOOLS_RTF_FIELD);
-    m_aStyles.append('{');
-    m_aStyles.append(OOO_STRING_SVTOOLS_RTF_IGNORE);
-    m_aStyles.append(OOO_STRING_SVTOOLS_RTF_FLDINST);
-    m_aStyles.append(" HYPERLINK ");
-
-    OUString sURL(rUrl);
-    if (!sURL.isEmpty())
+    m_sURL = rUrl;
+    // Ignore hyperlink without an URL.
+    if (!rUrl.isEmpty())
     {
+        m_aStyles.append('{');
+        m_aStyles.append(OOO_STRING_SVTOOLS_RTF_FIELD);
+        m_aStyles.append('{');
+        m_aStyles.append(OOO_STRING_SVTOOLS_RTF_IGNORE);
+        m_aStyles.append(OOO_STRING_SVTOOLS_RTF_FLDINST);
+        m_aStyles.append(" HYPERLINK ");
+
+        OUString sURL(rUrl);
         m_aStyles.append("\"");
         m_aStyles.append(msfilter::rtfutil::OutString(sURL, m_rExport.eCurrentEncoding));
         m_aStyles.append("\" ");
-    }
 
-    if (!rTarget.isEmpty())
-    {
-        m_aStyles.append("\\\\t \"");
-        m_aStyles.append(msfilter::rtfutil::OutString(rTarget, m_rExport.eCurrentEncoding));
-        m_aStyles.append("\" ");
-    }
+        if (!rTarget.isEmpty())
+        {
+            m_aStyles.append("\\\\t \"");
+            m_aStyles.append(msfilter::rtfutil::OutString(rTarget, m_rExport.eCurrentEncoding));
+            m_aStyles.append("\" ");
+        }
 
-    m_aStyles.append("}");
-    m_aStyles.append("{" OOO_STRING_SVTOOLS_RTF_FLDRSLT " {");
+        m_aStyles.append("}");
+        m_aStyles.append("{" OOO_STRING_SVTOOLS_RTF_FLDRSLT " {");
+    }
     return true;
 }
 
 bool RtfAttributeOutput::EndURL(bool const isAtEndOfParagraph)
 {
-    // UGLY: usually EndRun is called earlier, but there is an extra
-    // call to OutAttrWithRange() when at the end of the paragraph,
-    // so in that special case the output needs to be appended to the
-    // new run's text instead of the previous run
-    if (isAtEndOfParagraph)
+    if (!m_sURL.isEmpty())
     {
-        // close the fldrslt group
-        m_aRunText->append("}}");
-        // close the field group
-        m_aRunText->append('}');
-    }
-    else
-    {
-        // close the fldrslt group
-        m_aRun->append("}}");
-        // close the field group
-        m_aRun->append('}');
+        // UGLY: usually EndRun is called earlier, but there is an extra
+        // call to OutAttrWithRange() when at the end of the paragraph,
+        // so in that special case the output needs to be appended to the
+        // new run's text instead of the previous run
+        if (isAtEndOfParagraph)
+        {
+            // close the fldrslt group
+            m_aRunText->append("}}");
+            // close the field group
+            m_aRunText->append('}');
+        }
+        else
+        {
+            // close the fldrslt group
+            m_aRun->append("}}");
+            // close the field group
+            m_aRun->append('}');
+        }
+        m_sURL.clear();
     }
     return true;
 }

sw/source/filter/ww8/rtfattributeoutput.hxx
@@ -596,6 +596,9 @@ private:
 
     boost::optional<css::drawing::FillStyle> m_oFillStyle;
 
+    /// If we're in the process of exporting a hyperlink, then its URL.
+    OUString m_sURL;
+
 public:
     RtfAttributeOutput(RtfExport& rExport);
 

sw/qa/extras/inc/swmodeltestbase.hxx
@@ -240,6 +240,7 @@ protected:
         header();
         preTest(filename);
         load(mpTestDocumentPath, filename);
+        postLoad(filename);
         reload(mpFilter, filename);
         postTest(filename);
         verify();
@@ -286,6 +287,11 @@ protected:
      * Override this function if some special filename-specific setup is needed
      */
     virtual void preTest(const char* /*filename*/)
+    {
+    }
+
+    /// Override this function if some special file-specific setup is needed during export test: after load, but before save.
+    virtual void postLoad(const char* /*pFilename*/)
     {
     }
 

sw/qa/extras/rtfexport/data/tdf90421.fodt
@@ -1,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<office:document xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0" xmlns:style="urn:oasis:names:tc:opendocument:xmlns:style:1.0" xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0" xmlns:table="urn:oasis:names:tc:opendocument:xmlns:table:1.0" xmlns:draw="urn:oasis:names:tc:opendocument:xmlns:drawing:1.0" xmlns:fo="urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:meta="urn:oasis:names:tc:opendocument:xmlns:meta:1.0" xmlns:number="urn:oasis:names:tc:opendocument:xmlns:datastyle:1.0" xmlns:svg="urn:oasis:names:tc:opendocument:xmlns:svg-compatible:1.0" xmlns:chart="urn:oasis:names:tc:opendocument:xmlns:chart:1.0" xmlns:dr3d="urn:oasis:names:tc:opendocument:xmlns:dr3d:1.0" xmlns:math="http://www.w3.org/1998/Math/MathML" xmlns:form="urn:oasis:names:tc:opendocument:xmlns:form:1.0" xmlns:script="urn:oasis:names:tc:opendocument:xmlns:script:1.0" xmlns:config="urn:oasis:names:tc:opendocument:xmlns:config:1.0" xmlns:ooo="http://openoffice.org/2004/office" xmlns:ooow="http://openoffice.org/2004/writer" xmlns:oooc="http://openoffice.org/2004/calc" xmlns:dom="http://www.w3.org/2001/xml-events" xmlns:xforms="http://www.w3.org/2002/xforms" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:rpt="http://openoffice.org/2005/report" xmlns:of="urn:oasis:names:tc:opendocument:xmlns:of:1.2" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:grddl="http://www.w3.org/2003/g/data-view#" xmlns:officeooo="http://openoffice.org/2009/office" xmlns:tableooo="http://openoffice.org/2009/table" xmlns:drawooo="http://openoffice.org/2010/draw" xmlns:calcext="urn:org:documentfoundation:names:experimental:calc:xmlns:calcext:1.0" xmlns:loext="urn:org:documentfoundation:names:experimental:office:xmlns:loext:1.0" xmlns:field="urn:openoffice:names:experimental:ooo-ms-interop:xmlns:field:1.0" xmlns:formx="urn:openoffice:names:experimental:ooxml-odf-interop:xmlns:form:1.0" xmlns:css3t="http://www.w3.org/TR/css3-text/" office:version="1.2" office:mimetype="application/vnd.oasis.opendocument.text">
+ <office:body>
+  <office:text>
+   <text:p text:style-name="P1">A <text:a xlink:type="simple" xlink:href="http://s/" text:style-name="Internet_20_link" text:visited-style-name="Visited_20_Internet_20_Link">http://s/</text:a> B</text:p>
+  </office:text>
+ </office:body>
+</office:document>

sw/qa/extras/rtfexport/rtfexport.cxx
@@ -78,6 +78,16 @@ public:
         return false;
 
     }
+
+    virtual void postLoad(const char* pFilename) SAL_OVERRIDE
+    {
+        if (OString(pFilename) == "tdf90421.fodt")
+        {
+            // Change the hyperlink, so its URL is empty.
+            uno::Reference<beans::XPropertySet> xRun(getRun(getParagraph(1), 2), uno::UNO_QUERY);
+            xRun->setPropertyValue("HyperLinkURL", uno::makeAny(OUString()));
+        }
+    }
 };
 
 DECLARE_RTFEXPORT_TEST(testZoom, "zoom.rtf")
@@ -887,6 +897,19 @@ DECLARE_RTFEXPORT_TEST(testTdf80708, "tdf80708.rtf")
     uno::Reference<table::XTableRows> xTableRows(xTable->getRows(), uno::UNO_QUERY);
     // This was 2, i.e. the second table had 3 cols, now 2 as expected.
     CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(1), getProperty< uno::Sequence<text::TableColumnSeparator> >(xTableRows->getByIndex(0), "TableColumnSeparators").getLength());
+}
+
+DECLARE_RTFEXPORT_TEST(testTdf90421, "tdf90421.fodt")
+{
+    if (mbExported)
+    {
+        SvMemoryStream aMemoryStream;
+        SvFileStream aStream(maTempFile.GetURL(), StreamMode::READ);
+        aStream.ReadStream(aMemoryStream);
+        OString aData(static_cast<const char*>(aMemoryStream.GetData()), aMemoryStream.GetSize());
+        // This was some positive number, i.e. we exported a hyperlink with an empty URL.
+        CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(-1), aData.indexOf("HYPERLINK"));
+    }
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();

commit d576bda63b5da3cefba3cc1d87879a90a49b4d24

tdf#140552 RTF export: fix hyperlink, in footnote, in hyperlink
Regression from commit 7d42346ba77c9c4df241ea40eaf550993ca18783
(tdf#90421 RTF export: ignore hyperlinks without an URL, 2015-04-21),
URLs can be nested in the footnote case, which requires a stack.
Otherwise the inner URL clears "the" URL and we don't close the outer
field as we believe it's empty, so it was not started.

Change-Id: I9f87ddbb7e597c413bf836eb9b58beb76722361f

sw/source/filter/ww8/rtfattributeoutput.cxx
@@ -538,7 +538,7 @@ void RtfAttributeOutput::EndRuby(const SwTextNode& /*rNode*/, sal_Int32 /*nPos*/
 
 bool RtfAttributeOutput::StartURL(const OUString& rUrl, const OUString& rTarget)
 {
-    m_sURL = rUrl;
+    m_aURLs.push(rUrl);
     // Ignore hyperlink without a URL.
     if (!rUrl.isEmpty())
     {
@@ -568,7 +568,13 @@ bool RtfAttributeOutput::StartURL(const OUString& rUrl, const OUString& rTarget)
 
 bool RtfAttributeOutput::EndURL(bool const isAtEndOfParagraph)
 {
-    if (!m_sURL.isEmpty())
+    if (m_aURLs.empty())
+    {
+        return true;
+    }
+
+    const OUString& rURL = m_aURLs.top();
+    if (!rURL.isEmpty())
     {
         // UGLY: usually EndRun is called earlier, but there is an extra
         // call to OutAttrWithRange() when at the end of the paragraph,
@@ -588,8 +594,8 @@ bool RtfAttributeOutput::EndURL(bool const isAtEndOfParagraph)
             // close the field group
             m_aRun->append('}');
         }
-        m_sURL.clear();
     }
+    m_aURLs.pop();
     return true;
 }
 

sw/source/filter/ww8/rtfattributeoutput.hxx
@@ -623,7 +623,7 @@ private:
     std::optional<css::drawing::FillStyle> m_oFillStyle;
 
     /// If we're in the process of exporting a hyperlink, then its URL.
-    OUString m_sURL;
+    std::stack<OUString> m_aURLs;
 
     /// If original file had \sbauto.
     bool m_bParaBeforeAutoSpacing;

sw/qa/extras/rtfexport/rtfexport3.cxx
@@ -16,6 +16,7 @@
 #include <com/sun/star/text/XEndnotesSupplier.hpp>
 #include <com/sun/star/text/XTextTablesSupplier.hpp>
 #include <com/sun/star/text/XTextTable.hpp>
+#include <com/sun/star/text/XTextDocument.hpp>
 #include <com/sun/star/style/TabStop.hpp>
 
 #include <comphelper/sequenceashashmap.hxx>
@@ -253,6 +254,52 @@ DECLARE_RTFEXPORT_TEST(testTdf112520, "tdf112520.docx")
     // instead of the good 0/at-page/white, 1/at-char/yellow, 2/at-char/white.
     CPPUNIT_ASSERT_EQUAL(text::TextContentAnchorType_AT_CHARACTER,
                          getProperty<text::TextContentAnchorType>(getShape(3), "AnchorType"));
+}
+
+CPPUNIT_TEST_FIXTURE(Test, testNestedHyperlink)
+{
+    // Given a hyperlink contains a footnote which contains a hyperlink:
+    {
+        createSwDoc();
+        uno::Reference<lang::XMultiServiceFactory> xFactory(mxComponent, uno::UNO_QUERY);
+        uno::Reference<text::XTextContent> xFootnote(
+            xFactory->createInstance("com.sun.star.text.Footnote"), uno::UNO_QUERY);
+        uno::Reference<text::XTextDocument> xTextDocument(mxComponent, uno::UNO_QUERY);
+        uno::Reference<text::XText> xText = xTextDocument->getText();
+        uno::Reference<text::XTextCursor> xCursor = xText->createTextCursor();
+        xText->insertString(xCursor, "a", /*bAbsorb=*/false);
+        xText->insertTextContent(xCursor, xFootnote, /*bAbsorb=*/false);
+        xText->insertString(xCursor, "b", /*bAbsorb=*/false);
+        xCursor->gotoStart(/*bExpand=*/false);
+        xCursor->gotoEnd(/*bExpand=*/true);
+        uno::Reference<beans::XPropertySet> xCursorProps(xCursor, uno::UNO_QUERY);
+        xCursorProps->setPropertyValue("HyperLinkURL", uno::makeAny(OUString("http://body.com/")));
+        uno::Reference<text::XText> xFootnoteText(xFootnote, uno::UNO_QUERY);
+        xCursor = xFootnoteText->createTextCursor();
+        xFootnoteText->insertString(xCursor, "x", /*bAbsorb=*/false);
+        xCursor->gotoStart(/*bExpand=*/false);
+        xCursor->gotoEnd(/*bExpand=*/true);
+        xCursorProps.set(xCursor, uno::UNO_QUERY);
+        xCursorProps->setPropertyValue("HyperLinkURL",
+                                       uno::makeAny(OUString("http://footnote.com/")));
+    }
+
+    // When exporting to RTF:
+    // Without the accompanying fix in place, this test would have failed with:
+    // assertion failed
+    // - Expression: xComponent.is()
+    // i.e. the RTF output was not well-formed, loading failed.
+    reload(mpFilter, "nested-hyperlink.rtf");
+
+    // Then make sure both hyperlinks are have the correct URLs.
+    uno::Reference<text::XTextRange> xParagraph = getParagraph(1);
+    uno::Reference<text::XTextRange> xPortion = getRun(xParagraph, 1);
+    CPPUNIT_ASSERT_EQUAL(OUString("http://body.com/"),
+                         getProperty<OUString>(xPortion, "HyperLinkURL"));
+    auto xFootnote = getProperty<uno::Reference<text::XText>>(getRun(xParagraph, 2), "Footnote");
+    uno::Reference<text::XTextRange> xFootnotePortion = getRun(getParagraphOfText(1, xFootnote), 1);
+    CPPUNIT_ASSERT_EQUAL(OUString("http://footnote.com/"),
+                         getProperty<OUString>(xFootnotePortion, "HyperLinkURL"));
 }
 
 DECLARE_RTFEXPORT_TEST(testTdf121623, "tdf121623.rtf")