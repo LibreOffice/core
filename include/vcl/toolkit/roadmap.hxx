62462f7b464bfe00b93961ac8d3f1aaaf6c3e06a
Caolán McNamara <caolanm@redhat.com>
2021-03-02 09:57:59 +0000

    make all these headers pragma once
    Change-Id: Ic7968abf807c080636ef37a690990a962f702d35

--- include/vcl/toolkit/roadmap.hxx
+++ include/vcl/toolkit/roadmap.hxx
@@ -16,8 +16,8 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
-#ifndef INCLUDED_VCL_INC_ROADMAP_HXX
-#define INCLUDED_VCL_INC_ROADMAP_HXX
+
+#pragma once
 
 #if !defined(VCL_DLLIMPLEMENTATION) && !defined(TOOLKIT_DLLIMPLEMENTATION) && !defined(VCL_INTERNALS)
 #error "don't use this in new code"
@@ -112,7 +112,5 @@ private:
 };
 
 }   // namespace vcl
-
-#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
