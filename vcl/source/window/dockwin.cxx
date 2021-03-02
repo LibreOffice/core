e0c39bb24a0945bc2509ce2794479bf2acd53aa7
Pranam Lashkari <lpranam@collabora.com>
2021-03-02 06:04:40 +0530

    LOK: added missing PixelInvalidate method for currency list
    when docking window is displayed in popup mode,
    there are two windows created docking window and floating window,
    to make the this window work correctly with LOK,
    we have to invalidate the floating window to update floating window,
    as well as docking window.
    
    Change-Id: Ia1b4220646664aa0666a24e34fc14bf41421f9e2

--- vcl/source/window/dockwin.cxx
+++ vcl/source/window/dockwin.cxx
@@ -300,6 +300,7 @@ void DockingWindow::ImplInitDockingWindowData()
     mbIsDeferredInit = false;
     mbIsCalculatingInitialLayoutSize = false;
     mpDialogParent = nullptr;
+    mnLOKPopUpWinId = 0;
 
     //To-Do, reuse maResizeTimer
     maLayoutIdle.SetPriority(TaskPriority::RESIZE);
