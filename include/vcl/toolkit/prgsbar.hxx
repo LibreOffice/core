e0c002cb2a4dc248a78864204c6ad1324bfd9d2c
Caolán McNamara <caolanm@redhat.com>
2021-03-02 09:57:59 +0000

    make all these headers pragma once
    Change-Id: Ic7968abf807c080636ef37a690990a962f702d35

--- include/vcl/toolkit/prgsbar.hxx
+++ include/vcl/toolkit/prgsbar.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_VCL_PRGSBAR_HXX
-#define INCLUDED_VCL_PRGSBAR_HXX
+#pragma once
 
 #if !defined(VCL_DLLIMPLEMENTATION) && !defined(TOOLKIT_DLLIMPLEMENTATION) && !defined(VCL_INTERNALS)
 #error "don't use this in new code"
@@ -77,7 +76,5 @@ public:
 
     void                SetValue( sal_uInt16 nNewPercent );
 };
-
-#endif // INCLUDED_VCL_PRGSBAR_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
