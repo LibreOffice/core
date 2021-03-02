bef11ae221706d18bd2a8a515326c10b53b6d186
Mike Kaganski <mike.kaganski@collabora.com>
2021-03-02 15:00:56 +0300

    tdf#116983 tdf#136175: retry if failed
    Debugging the test case from the latter bug report shows that indeed
    the call to OleGetClipboard may fail first time, as jasonkres had
    suspected in the former bug. So follow the suggestion in tdf#116983,
    and retry the failing calls several times in case of failure.
    
    Many thanks to Telesto for preparing a clear bug report with reliable
    test case.
    
    Co-authored-by: jasonkres
    
    Change-Id: Ib3c497da830bc5faac586bcfe1eededa54bfa117

---
+++ include/systools/win32/retry_if_failed.hxx
@@ -0,0 +1,42 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#pragma once
+
+#include <type_traits>
+#include <systools/win32/uwinapi.h>
+
+#pragma comment(lib, "Kernel32.lib") // for Sleep
+
+namespace sal::systools
+{
+// Some system calls (e.g., clipboard access functions) may fail first time, because the resource
+// may only be accessed by one process at a time. This function allows to retry failed call up to
+// specified number of times with a specified timeout (in ms), until the call succeeds or the limit
+// of attempts is exceeded.
+// Usage:
+//     HRESULT hr = sal::systools::RetryIfFailed(10, 100, []{ return OleFlushClipboard(); });
+template <typename Func>
+std::enable_if_t<std::is_same_v<std::invoke_result_t<Func>, HRESULT>, HRESULT>
+RetryIfFailed(unsigned times, unsigned msTimeout, Func func)
+{
+    HRESULT hr = E_FAIL;
+    for (unsigned i = 0; i < times; ++i)
+    {
+        hr = func();
+        if (SUCCEEDED(hr))
+            break;
+        if (i < times - 1)
+            Sleep(msTimeout);
+    }
+    return hr;
+}
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
