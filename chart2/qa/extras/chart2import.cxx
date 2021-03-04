LibreOffice/core

commit 73993fdb5d4b507694cd0edf80887d19f7e2bf9a

tdf#134183 PPTX: improve import of transparency in multi-step gradients
Impress core only support a single step, improve the conversion from
multi-step to single step to take transparency into account explicitly.

Once we select the widest segment, look backwards and forward if there
are other next segments which have the same RGB color, just different
transparency and include them. This helps in general, because in case a
0% or 100% transparency is mishandled, that's very visible.

Change-Id: I11d593c01a6a4b16149ce74c1408c2a39895e941
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/100231
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

oox/source/drawingml/fillproperties.cxx
@@ -512,21 +512,51 @@ void FillProperties::pushToPropMap( ShapePropertyMap& rPropMap,
                     // convert DrawingML angle (in 1/60000 degrees) to API angle (in 1/10 degrees)
                     aGradient.Angle = static_cast< sal_Int16 >( (8100 - (nDmlAngle / (PER_DEGREE / 10))) % 3600 );
                     Color aStartColor, aEndColor;
+
+                    // Try to grow the widest segment backwards: if a previous segment has the same
+                    // color, just different transparency, include it.
+                    while (aWidestSegmentStart != aGradientStops.begin())
+                    {
+                        auto it = std::prev(aWidestSegmentStart);
+                        if (it->second.getColor(rGraphicHelper, nPhClr)
+                            != aWidestSegmentStart->second.getColor(rGraphicHelper, nPhClr))
+                        {
+                            break;
+                        }
+
+                        aWidestSegmentStart = it;
+                    }
+
+                    auto aWidestSegmentEnd = std::next(aWidestSegmentStart);
+                    // Try to grow the widest segment forward: if a neext segment has the same
+                    // color, just different transparency, include it.
+                    while (aWidestSegmentEnd != std::prev(aGradientStops.end()))
+                    {
+                        auto it = std::next(aWidestSegmentEnd);
+                        if (it->second.getColor(rGraphicHelper, nPhClr)
+                            != aWidestSegmentEnd->second.getColor(rGraphicHelper, nPhClr))
+                        {
+                            break;
+                        }
+
+                        aWidestSegmentEnd = it;
+                    }
+
                     if( bSymmetric )
                     {
-                        aStartColor = std::next(aWidestSegmentStart)->second;
+                        aStartColor = aWidestSegmentEnd->second;
                         aEndColor = aWidestSegmentStart->second;
                         nBorder *= 2;
                     }
                     else if( bSwap )
                     {
-                        aStartColor = std::next(aWidestSegmentStart)->second;
+                        aStartColor = aWidestSegmentEnd->second;
                         aEndColor = aWidestSegmentStart->second;
                     }
                     else
                     {
                         aStartColor = aWidestSegmentStart->second;
-                        aEndColor = std::next(aWidestSegmentStart)->second;
+                        aEndColor = aWidestSegmentEnd->second;
                     }
 
                     SAL_INFO("oox.drawingml.gradient", "start color: " << std::hex << sal_Int32(aStartColor.getColor( rGraphicHelper, nPhClr )) << std::dec <<

oox/qa/unit/drawingml.cxx
@@ -10,7 +10,9 @@
 #include <test/bootstrapfixture.hxx>
 #include <unotest/macros_test.hxx>
 
+#include <com/sun/star/awt/Gradient.hpp>
 #include <com/sun/star/beans/XPropertySet.hpp>
+#include <com/sun/star/container/XNamed.hpp>
 #include <com/sun/star/drawing/XDrawPagesSupplier.hpp>
 #include <com/sun/star/drawing/FillStyle.hpp>
 #include <com/sun/star/drawing/XShape.hpp>
@@ -197,6 +199,31 @@ CPPUNIT_TEST_FIXTURE(OoxDrawingmlTest, testChartDataLabelCharColor)
     // - Actual  : -1
     // i.e. the data label had no explicit (white) color.
     CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(0xffffff), nCharColor);
+}
+
+CPPUNIT_TEST_FIXTURE(OoxDrawingmlTest, testGradientMultiStepTransparency)
+{
+    // Load a document with a multi-step gradient.
+    OUString aURL
+        = m_directories.getURLFromSrc(DATA_DIRECTORY) + "gradient-multistep-transparency.pptx";
+    load(aURL);
+
+    // Check the end transparency of the gradient.
+    uno::Reference<drawing::XDrawPagesSupplier> xDrawPagesSupplier(getComponent(), uno::UNO_QUERY);
+    uno::Reference<drawing::XDrawPage> xDrawPage(xDrawPagesSupplier->getDrawPages()->getByIndex(0),
+                                                 uno::UNO_QUERY);
+    uno::Reference<container::XNamed> xShape(xDrawPage->getByIndex(1), uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(OUString("Rectangle 4"), xShape->getName());
+    uno::Reference<beans::XPropertySet> xShapeProps(xShape, uno::UNO_QUERY);
+    awt::Gradient aTransparence;
+    xShapeProps->getPropertyValue("FillTransparenceGradient") >>= aTransparence;
+
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 16777215 (0xffffff)
+    // - Actual  : 3487029 (0x353535)
+    // i.e. the end transparency was not 100%, but was 21%, leading to an unexpected visible line on
+    // the right of this shape.
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(0xffffff), aTransparence.EndColor);
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();

commit 7c2bd277d3d3f5e3471078a5931bab0dba92296d

tdf#139940 oox: mark end before growing largest segment backwards
This fixes a regression in 7.1 (backported to 7.0.1) caused by
commit 73993fdb5d4b507694cd0edf80887d19f7e2bf9a for tdf#134183.

The largest segment has already been found, so we know where the
next segment starts. Now if we grow it backwards, that shouldn't
affect where it ends. So flag the end before growing the start,
otherwise we would have to recalculate the end again.

Change-Id: Iaae401d0438e533c7498be56f48fa153f7d3ff0a

oox/source/drawingml/fillproperties.cxx
@@ -630,6 +630,9 @@ void FillProperties::pushToPropMap( ShapePropertyMap& rPropMap,
                     aGradient.Angle = static_cast< sal_Int16 >( (8100 - (nDmlAngle / (PER_DEGREE / 10))) % 3600 );
                     Color aStartColor, aEndColor;
 
+                    // Make a note where the widest segment stops, because we will try to grow it next.
+                    auto aWidestSegmentEnd = std::next(aWidestSegmentStart);
+
                     // Try to grow the widest segment backwards: if a previous segment has the same
                     // color, just different transparency, include it.
                     while (aWidestSegmentStart != aGradientStops.begin())
@@ -644,7 +647,6 @@ void FillProperties::pushToPropMap( ShapePropertyMap& rPropMap,
                         aWidestSegmentStart = it;
                     }
 
-                    auto aWidestSegmentEnd = std::next(aWidestSegmentStart);
                     // Try to grow the widest segment forward: if a next segment has the same
                     // color, just different transparency, include it.
                     while (aWidestSegmentEnd != std::prev(aGradientStops.end()))

chart2/qa/extras/chart2import.cxx
@@ -841,6 +841,16 @@ void Chart2ImportTest::testBnc889755()
     CPPUNIT_ASSERT_EQUAL(OUString("Nov-13"), aRowLabels[13]);
     CPPUNIT_ASSERT_EQUAL(OUString("Dec-13"), aRowLabels[14]);
     CPPUNIT_ASSERT_EQUAL(OUString("Jan-14"), aRowLabels[15]);
+
+    //tdf#139940 - the title's gradient was lost and was filled with solid blue, instead of a "blue underline".
+    uno::Reference<drawing::XDrawPagesSupplier> xDoc(mxComponent, uno::UNO_QUERY_THROW);
+    uno::Reference<drawing::XDrawPage> xPage(xDoc->getDrawPages()->getByIndex(0), uno::UNO_QUERY_THROW);
+    // Shape "Title 3"
+    uno::Reference<beans::XPropertySet> xShapeProps(xPage->getByIndex(5), uno::UNO_QUERY_THROW);
+    awt::Gradient aTransparence;
+    xShapeProps->getPropertyValue("FillTransparenceGradient") >>= aTransparence;
+    CPPUNIT_ASSERT(aTransparence.StartColor != aTransparence.EndColor);
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(0xffffff), aTransparence.StartColor);
 }
 
 void Chart2ImportTest::testBnc882383()