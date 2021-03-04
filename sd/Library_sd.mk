0c6305defb55653f87a04b9c64e454026847ca4c
Caolán McNamara <caolanm@redhat.com>
2021-03-04 16:01:36 +0000

    remove intermediate TableDesignPanel
    Change-Id: Icdc1aef7a0d53239ee41e818a56d137aa959249c
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/111973
    Tested-by: Caolán McNamara <caolanm@redhat.com>
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>

--- sd/Library_sd.mk
+++ sd/Library_sd.mk
@@ -341,7 +341,6 @@ $(eval $(call gb_Library_add_exception_objects,sd,\
 	sd/source/ui/sidebar/RecentlyUsedMasterPages \
 	sd/source/ui/sidebar/RecentMasterPagesSelector \
 	sd/source/ui/sidebar/SlideBackground \
-	sd/source/ui/sidebar/TableDesignPanel \
 	sd/source/ui/slideshow/PaneHider \
 	sd/source/ui/slideshow/SlideShowRestarter \
 	sd/source/ui/slideshow/showwin \
