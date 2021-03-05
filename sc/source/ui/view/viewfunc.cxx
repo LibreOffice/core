ce0fc9b59726f09e4ecbec540c3cf646d20f949e
Tünde Tóth <toth.tunde@nisz.hu>
2021-03-05 10:20:03 +0100

    tdf#36383 sc AutoFilter: fix changing row height
    Changing row height showed the rows hidden
    by AutoFilter, removing the result of the filtering.
    
    Change-Id: Ie2cba567c1fa4b479bd351693ae6dd3fd604ffc8
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/112006
    Tested-by: László Németh <nemeth@numbertext.org>
    Reviewed-by: László Németh <nemeth@numbertext.org>
    (cherry picked from commit 3678e0efcb8bedc58dd329a430da0ac3b1572df8)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/114863
    Tested-by: Gabor Kelemen <kelemen.gabor2@nisz.hu>
    Reviewed-by: Gabor Kelemen <kelemen.gabor2@nisz.hu>

--- sc/source/ui/view/viewfunc.cxx
+++ sc/source/ui/view/viewfunc.cxx
@@ -2158,7 +2158,17 @@ void ScViewFunc::SetWidthOrHeight(
                     aCxt.setExtraHeight(nSizeTwips);
                     rDoc.SetOptimalHeight(aCxt, nStartNo, nEndNo, nTab);
                     if (bAll)
-                        rDoc.ShowRows( nStartNo, nEndNo, nTab, true );
+                    {
+                        for (SCROW nRow = nStartNo; nRow <= nEndNo; ++nRow)
+                        {
+                            SCROW nLastRow = nRow;
+                            // tdf#36383 skip consecutive rows hidden by AutoFilter
+                            if (rDoc.RowFiltered(nRow, nTab, nullptr, &nLastRow))
+                                nRow = nLastRow;
+                            else
+                                rDoc.ShowRow(nRow, nTab, true);
+                        }
+                    }
 
                     //  Manual-Flag already (re)set in SetOptimalHeight in case of bAll=sal_True
                     //  (set for Extra-Height, else reset).
@@ -2171,7 +2181,15 @@ void ScViewFunc::SetWidthOrHeight(
                         rDoc.SetManualHeight( nStartNo, nEndNo, nTab, true );          // height was set manually
                     }
 
-                    rDoc.ShowRows( nStartNo, nEndNo, nTab, nSizeTwips != 0 );
+                    for (SCROW nRow = nStartNo; nRow <= nEndNo; ++nRow)
+                    {
+                        SCROW nLastRow = nRow;
+                        // tdf#36383 skip consecutive rows hidden by AutoFilter
+                        if (rDoc.RowFiltered(nRow, nTab, nullptr, &nLastRow))
+                            nRow = nLastRow;
+                        else
+                            rDoc.ShowRow(nRow, nTab, nSizeTwips != 0);
+                    }
 
                     if (!bShow && nStartNo <= nCurY && nCurY <= nEndNo && nTab == nCurTab)
                     {
