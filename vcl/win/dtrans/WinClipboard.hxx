76ddbf0baf9079c62ae62a4083309b9c3a3cbf9a
Mike Kaganski <mike.kaganski@collabora.com>
2021-03-05 20:45:08 +0300

    tdf#140813: Use GetUpdatedClipboardFormats to enumerate clipboard formats
    We really don't have to provide plain text formats other than Unicode, so
    we may avoid checking CF_LOCALE data when initializing flavor list. Let's
    pretend that any textual format in the clipboard is Unicode, and ensure
    that we only actually access system clipboard when we paste.
    
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/112044
    Tested-by: Jenkins
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
    (cherry picked from commit 7b6c0e63e64eb2ad1e83bd744a0d20f78c7a6b84)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/112971
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
    Reviewed-by: Adolfo Jayme Barrientos <fitojb@ubuntu.com>
    
    Change-Id: Ife30f57605a42d59233bfcb97f8bc297b3ace463
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/143176
    Tested-by: Gabor Kelemen <kelemeng@ubuntu.com>
    Reviewed-by: Balazs Varga <balazs.varga.extern@allotropia.de>

--- vcl/win/dtrans/WinClipboard.hxx
+++ vcl/win/dtrans/WinClipboard.hxx
@@ -33,10 +33,10 @@
 #include <com/sun/star/lang/XServiceInfo.hpp>
 #include <com/sun/star/uno/XComponentContext.hpp>
 #include <osl/conditn.hxx>
+#include <systools/win32/comtools.hxx>
 
 #include "MtaOleClipb.hxx"
-
-class CXNotifyingDataObject;
+#include "XNotifyingDataObject.hxx"
 
 // implements the XClipboard[Ex] ... interfaces
 // for the clipboard viewer mechanism we need a static callback function
@@ -114,6 +114,8 @@ public:
     virtual OUString SAL_CALL getImplementationName() override;
     virtual sal_Bool SAL_CALL supportsService(const OUString& ServiceName) override;
     virtual css::uno::Sequence<OUString> SAL_CALL getSupportedServiceNames() override;
+
+    IDataObjectPtr getIDataObject();
 };
 
 #endif
