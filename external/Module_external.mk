25570cfcc795787fb5efbbc0a624d601b803b067
homeboy445 <akshitsan13@gmail.com>
2021-03-06 14:21:25 +0530

    tdf#139778 bundle external:zxing lib
    Change-Id: I0023f6ce8315427b1a3deaf755e78ae06475b08c

--- external/Module_external.mk
+++ external/Module_external.mk
@@ -91,6 +91,7 @@ $(eval $(call gb_Module_add_moduledirs,external,\
 	$(call gb_Helper_optional,PYTHON,python3) \
 	$(call gb_Helper_optional,QXP,libqxp) \
 	$(call gb_Helper_optional,QRCODEGEN,qrcodegen) \
+	$(call gb_Helper_optional,ZXING,zxing) \
 	$(call gb_Helper_optional,REDLAND,redland) \
 	$(call gb_Helper_optional,REVENGE,librevenge) \
 	$(call gb_Helper_optional,RHINO,rhino) \
---
+++ external/zxing/UnpackedTarball_zxing.mk
@@ -0,0 +1,19 @@
+#-*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_UnpackedTarball_UnpackedTarball,zxing))
+
+$(eval $(call gb_UnpackedTarball_set_tarball,zxing,$(ZXING_TARBALL)))
+
+$(eval $(call gb_UnpackedTarball_set_patchlevel,zxing,1))
+
+$(eval $(call gb_UnpackedTarball_add_patches,zxing, \
+))
+
+# vim: set noet sw=4 ts=4:
