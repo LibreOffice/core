99eba11281af6dfa4c5157457451b090308ba8df
Chris Sherlock <chris.sherlock79@gmail.com>
2021-03-06 12:09:07 +1100

    vcl: move OutDevState functions to own file
    Change-Id: I9561b311d8752fa12802b5ae8e11123aedabe8be

---
+++ vcl/source/rendercontext/OutDevState.cxx
@@ -0,0 +1,50 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#include <vcl/font.hxx>
+#include <vcl/outdevstate.hxx>
+#include <vcl/region.hxx>
+
+OutDevState::OutDevState()
+    : mbMapActive(false)
+    , meTextAlign(ALIGN_TOP)
+    , meRasterOp(RasterOp::OverPaint)
+    , mnTextLayoutMode(ComplexTextLayoutFlags::Default)
+    , meTextLanguage(0)
+    , mnFlags(PushFlags::NONE)
+{
+}
+
+OutDevState::OutDevState(OutDevState&&) = default;
+
+OutDevState::~OutDevState()
+{
+    mpLineColor.reset();
+    mpFillColor.reset();
+    mpFont.reset();
+    mpTextColor.reset();
+    mpTextFillColor.reset();
+    mpTextLineColor.reset();
+    mpOverlineColor.reset();
+    mpMapMode.reset();
+    mpClipRegion.reset();
+    mpRefPoint.reset();
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
