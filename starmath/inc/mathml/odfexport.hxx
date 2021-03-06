a056b36a439440e9db344eda5ce1db38b97ed42a
dante <dante19031999@gmail.com>
2021-03-06 13:07:31 +0100

    Fast odf export
    Change-Id: I417c3b2ed86f330a27676b00266712a47d81d1e9

--- starmath/inc/mathml/odfexport.hxx
+++ starmath/inc/mathml/odfexport.hxx
@@ -46,36 +46,33 @@ class SmXMLExportWrapper
 private:
     // Use customized entities
     bool m_bUseHTMLMLEntities;
-    bool m_bExportODF;
 
 public:
     explicit SmXMLExportWrapper(css::uno::Reference<css::frame::XModel> const& rRef)
         : xModel(rRef)
         , bFlat(true)
         , m_bUseHTMLMLEntities(false)
-        , m_bExportODF(false)
     {
     }
-
-    bool IsExportODF() const { return m_bExportODF; }
-    void SetExportODF(bool bExportODF) { m_bExportODF = bExportODF; }
 
     bool Export(SfxMedium& rMedium);
     void SetFlat(bool bIn) { bFlat = bIn; }
     void useHTMLMLEntities(bool bUseHTMLMLEntities) { m_bUseHTMLMLEntities = bUseHTMLMLEntities; }
 
-    bool WriteThroughComponent(const css::uno::Reference<css::io::XOutputStream>& xOutputStream,
-                               const css::uno::Reference<css::lang::XComponent>& xComponent,
-                               css::uno::Reference<css::uno::XComponentContext> const& rxContext,
-                               css::uno::Reference<css::beans::XPropertySet> const& rPropSet,
-                               const char* pComponentName, bool bUseHTMLMLEntities);
+    static bool
+    WriteThroughComponent(const css::uno::Reference<css::io::XOutputStream>& xOutputStream,
+                          const css::uno::Reference<css::lang::XComponent>& xComponent,
+                          css::uno::Reference<css::uno::XComponentContext> const& rxContext,
+                          css::uno::Reference<css::beans::XPropertySet> const& rPropSet,
+                          const char* pComponentName, bool bUseHTMLMLEntities);
 
-    bool WriteThroughComponent(const css::uno::Reference<css::embed::XStorage>& xStor,
-                               const css::uno::Reference<css::lang::XComponent>& xComponent,
-                               const char* pStreamName,
-                               css::uno::Reference<css::uno::XComponentContext> const& rxContext,
-                               css::uno::Reference<css::beans::XPropertySet> const& rPropSet,
-                               const char* pComponentName, bool bUseHTMLMLEntities);
+    static bool
+    WriteThroughComponent(const css::uno::Reference<css::embed::XStorage>& xStor,
+                          const css::uno::Reference<css::lang::XComponent>& xComponent,
+                          const char* pStreamName,
+                          css::uno::Reference<css::uno::XComponentContext> const& rxContext,
+                          css::uno::Reference<css::beans::XPropertySet> const& rPropSet,
+                          const char* pComponentName, bool bUseHTMLMLEntities);
 };
 
 class SmXMLExport final : public SvXMLExport
@@ -83,27 +80,6 @@ class SmXMLExport final : public SvXMLExport
     const SmNode* pTree;
     OUString aText;
     bool bSuccess;
-    bool m_bExportODF;
-
-    void ExportNodes(const SmNode* pNode, int nLevel);
-    void ExportTable(const SmNode* pNode, int nLevel);
-    void ExportLine(const SmNode* pNode, int nLevel);
-    void ExportExpression(const SmNode* pNode, int nLevel, bool bNoMrowContainer = false);
-    void ExportText(const SmNode* pNode);
-    void ExportMath(const SmNode* pNode);
-    void ExportBinaryHorizontal(const SmNode* pNode, int nLevel);
-    void ExportUnaryHorizontal(const SmNode* pNode, int nLevel);
-    void ExportBrace(const SmNode* pNode, int nLevel);
-    void ExportBinaryVertical(const SmNode* pNode, int nLevel);
-    void ExportBinaryDiagonal(const SmNode* pNode, int nLevel);
-    void ExportSubSupScript(const SmNode* pNode, int nLevel);
-    void ExportRoot(const SmNode* pNode, int nLevel);
-    void ExportOperator(const SmNode* pNode, int nLevel);
-    void ExportAttributes(const SmNode* pNode, int nLevel);
-    void ExportFont(const SmNode* pNode, int nLevel);
-    void ExportVerticalBrace(const SmVerticalBraceNode* pNode, int nLevel);
-    void ExportMatrix(const SmNode* pNode, int nLevel);
-    void ExportBlank(const SmNode* pNode);
 
 public:
     SmXMLExport(const css::uno::Reference<css::uno::XComponentContext>& rContext,
@@ -122,9 +98,6 @@ public:
     virtual void GetViewSettings(css::uno::Sequence<css::beans::PropertyValue>& aProps) override;
     virtual void
     GetConfigurationSettings(css::uno::Sequence<css::beans::PropertyValue>& aProps) override;
-
-    bool IsExportODF() const { return m_bExportODF; }
-    void SetExportODF(bool bExportODF) { m_bExportODF = bExportODF; }
 
     bool GetSuccess() const { return bSuccess; }
 };
