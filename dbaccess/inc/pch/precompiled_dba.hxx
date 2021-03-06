2736628bcf88fda8f4448175c443d3acb49b0388
Julien Nabet <serval2412@yahoo.fr>
2021-03-06 20:42:57 +0100

    update pches
    Change-Id: Id892e426343746df2f01f4cdc5f470351bdb6a12

--- dbaccess/inc/pch/precompiled_dba.hxx
+++ dbaccess/inc/pch/precompiled_dba.hxx
@@ -13,7 +13,7 @@
  manual changes will be rewritten by the next run of update_pch.sh (which presumably
  also fixes all possible problems, so it's usually better to use it).
 
- Generated on 2021-03-06 18:49:22 using:
+ Generated on 2021-03-06 20:41:35 using:
  ./bin/update_pch dbaccess dba --cutoff=6 --exclude:system --include:module --include:local
 
  If after updating build fails, use the following command to locate conflicting headers:
@@ -70,6 +70,7 @@
 #include <rtl/math.h>
 #include <rtl/math.hxx>
 #include <rtl/ref.hxx>
+#include <rtl/strbuf.h>
 #include <rtl/string.h>
 #include <rtl/string.hxx>
 #include <rtl/stringconcat.hxx>
