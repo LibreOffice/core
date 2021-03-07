01ca3adb02c75eb6d8f905e42f49c9ce0f18a3c5
Chris Sherlock <chris.sherlock79@gmail.com>
2021-03-07 11:19:29 +1100

    tdf#74702 vcl: remove GetOutDevType() from ImplNewFont()
    Change-Id: I59a14b0c392098761f9304708f4859f7e2381c14

--- include/vcl/outdev.hxx
+++ include/vcl/outdev.hxx
@@ -176,8 +176,6 @@ private:
     mutable VclPtr<OutputDevice>    mpNextGraphics;     ///< Next output device in list
     GDIMetaFile*                    mpMetaFile;
     mutable rtl::Reference<LogicalFontInstance> mpFontInstance;
-    mutable std::shared_ptr<ImplFontCache> mxFontCache;
-    mutable std::shared_ptr<PhysicalFontCollection> mxFontCollection;
     mutable std::unique_ptr<ImplDeviceFontList>     mpDeviceFontList;
     mutable std::unique_ptr<ImplDeviceFontSizeList> mpDeviceFontSizeList;
     std::vector<OutDevState>        maOutDevStateStack;
@@ -251,6 +249,10 @@ private:
     mutable bool                    mbTextSpecial : 1;
     mutable bool                    mbRefPoint : 1;
     mutable bool                    mbEnableRTL : 1;
+
+protected:
+    mutable std::shared_ptr<PhysicalFontCollection> mxFontCollection;
+    mutable std::shared_ptr<ImplFontCache> mxFontCache;
 
     /** @name Initialization and accessor functions
      */
@@ -1175,11 +1177,11 @@ protected:
     void SetFontCollectionFromSVData();
     void ResetNewFontCache();
 
+    SAL_DLLPRIVATE bool         ImplNewFont() const;
+
 private:
 
     typedef void ( OutputDevice::* FontUpdateHandler_t )( bool );
-
-    SAL_DLLPRIVATE bool         ImplNewFont() const;
 
     SAL_DLLPRIVATE static void  ImplUpdateFontDataForAllFrames( FontUpdateHandler_t pHdl, bool bNewFontLists );
 
