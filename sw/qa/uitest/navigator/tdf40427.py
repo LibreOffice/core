57e73d26147f400b085d64b16cb93bcf43db4755
Mike Kaganski <mike.kaganski@collabora.com>
2021-03-09 12:59:41 +0300

    tdf#40427: use node index as position, not Y position on screen
    As mentioned in comment to SwContent::nYPosition in
    sw/source/uibase/inc/swcont.hxx:
    
      some subclasses appear to use this for a tools/gen.hxx-style
      geometric Y position, while e.g. SwOutlineContent wants to store
      the index in its subtree
    
    Abusing the nYPosition to store vertical position *on screen* gives
    wrong results when a following section is positioned on screen higher
    than a previous section - e.g., when multiple-page view is active.
    
    So just use the section's node as Y position of the Navigator entry.
    When the section is inside a fly frame, use the frame's anchor node.
    
    Change-Id: I6caf26aeb19d845129dc837138c37f42bbc18655

---
+++ sw/qa/uitest/navigator/tdf40427.py
@@ -0,0 +1,79 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+from uitest.framework import UITestCase
+from libreoffice.uno.propertyvalue import mkPropertyValues
+from uitest.uihelper.common import get_state_as_dict, get_url_for_data_file
+
+class tdf40427(UITestCase):
+
+  def test_tdf40427(self):
+    self.ui_test.load_file(get_url_for_data_file("tdf40427_SectionPositions.odt"))
+    xMainWindow = self.xUITest.getTopFocusWindow()
+    xWriterEdit = xMainWindow.getChild("writer_edit")
+
+    self.assertEqual(2, self.ui_test.get_component().CurrentController.PageCount)
+
+    # Make sure that the view is 2 pages side-by-side - look at dialog View-Zoom-Zoom
+    self.ui_test.execute_dialog_through_command(".uno:Zoom")
+    xDialog = self.xUITest.getTopFocusWindow()
+
+    columnssb = xDialog.getChild("columnssb")
+    columns = xDialog.getChild("columns")
+    bookmode = xDialog.getChild("bookmode")
+    self.assertEqual("true", get_state_as_dict(columns)["Checked"])
+    self.assertEqual("2", get_state_as_dict(columnssb)["Text"])
+    self.assertEqual("false", get_state_as_dict(bookmode)["Selected"])
+
+    xOKBtn = xDialog.getChild("ok")
+    self.ui_test.close_dialog_through_button(xOKBtn)
+
+    # In this view, the sections "SectionB" and "SectionC" on second page are positioned on screen
+    # higher than "SectionY" and "SectionA" respectively; there are nested and anchored sections.
+    # Make sure that order in Navigator follows their relative position in document, not vertical
+    # position on screen, nor sorted alphabetically. Sections in flying frames are sorted by their
+    # anchor position in the document.
+
+    self.xUITest.executeCommand(".uno:Sidebar")
+    xWriterEdit.executeAction("SIDEBAR", mkPropertyValues({"PANEL": "SwNavigatorPanel"}))
+
+    # wait until the navigator panel is available
+    xNavigatorPanel = self.ui_test.wait_until_child_is_available('NavigatorPanelParent')
+
+    xContentTree = xNavigatorPanel.getChild("contenttree")
+    xSections = xContentTree.getChild('6')
+    self.assertEqual('Sections', get_state_as_dict(xSections)['Text'])
+
+    xSections.executeAction("EXPAND", tuple())
+
+    nSections = 12
+    self.assertEqual(nSections, len(xSections.getChildren()))
+
+    sectionNames = []
+    for i in range(nSections):
+      sectionNames.append(get_state_as_dict(xSections.getChild(str(i)))['Text'])
+    # Without the fix in place, this would fail with
+    #   AssertionError: Lists differ: ['SectionZ', 'SectionY', 'SectionT2', 'SectionT1', 'SectionT0'[87 chars]onC'] != ['SectionZ', 'SectionB', 'SectionF3', 'SectionFinF3', 'Section[87 chars]onA']
+    self.assertEqual(['SectionZ',
+                      'SectionY',
+                      'SectionT2',
+                      'SectionT1',
+                      'SectionT0',
+                      'SectionF2',
+                      'SectionF3',
+                      'SectionFinF3',
+                      'SectionA',
+                      'SectionF1',
+                      'SectionB',
+                      'SectionC'], sectionNames)
+
+    self.xUITest.executeCommand(".uno:Sidebar")
+    self.ui_test.close_doc()
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
