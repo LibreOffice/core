816870b8e5a92a9b1ac948c06e25df689950b901
Mike Kaganski <mike.kaganski@collabora.com>
2021-03-09 16:57:40 +0100

    Add missing comments as per TEMPLATE.SOURCECODE.HEADER
    Change-Id: I6a8ed8598690795d5e23fb461db50f7f7852912a

--- sw/qa/uitest/navigator/tdf114724.py
+++ sw/qa/uitest/navigator/tdf114724.py
@@ -1,3 +1,6 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This file is part of the LibreOffice project.
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -44,3 +47,5 @@ class tdf114724(UITestCase):
 
         self.xUITest.executeCommand(".uno:Sidebar")
         self.ui_test.close_doc()
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
