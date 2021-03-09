8e7dc248f2787df0e658c4ece33220944fca7f16
Miklos Vajna <vmiklos@collabora.com>
2021-03-09 11:49:06 +0100

    sw: add UNO API to find the closest doc model position based on pixel position
    The use-case is drag&drop: if an UNO API client registers its drag&drop
    listener, then it gets a DropTargetDropEvent and has to decide if it
    wants to handle that event or allow Writer to handle it. In case it
    decides to handle the event, it would be good to able to e.g. insert a
    string at the point where the Writer UI indicates it to the user. But
    DropTargetDropEvent only exposes a pixel position and Writer requires
    you to have an XTextRange when inserting content.
    
    Fix the problem by introducing a new createTextRangeByPixelPosition()
    which first does a pixel -> logic coordinate conversion (this is
    window-specific, in case you have multiple windows), then picks the doc
    model position which is the closest to a logic coordinate.
    
    Change-Id: I6a8e69e3c39b9d99209342469653c0e0bd99bf53
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/112201
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Tested-by: Jenkins

--- sw/qa/uibase/uno/uno.cxx
+++ sw/qa/uibase/uno/uno.cxx
@@ -9,9 +9,17 @@
 
 #include <swmodeltestbase.hxx>
 
+#include <com/sun/star/frame/XModel2.hpp>
+#include <com/sun/star/text/XTextViewTextRangeSupplier.hpp>
 #include <com/sun/star/util/XCloseable.hpp>
 
 #include <vcl/scheduler.hxx>
+
+#include <docsh.hxx>
+#include <edtwin.hxx>
+#include <unotextrange.hxx>
+#include <view.hxx>
+#include <wrtsh.hxx>
 
 constexpr OUStringLiteral DATA_DIRECTORY = u"/sw/qa/uibase/uno/data/";
 
@@ -45,6 +53,41 @@ CPPUNIT_TEST_FIXTURE(SwUibaseUnoTest, testCondFieldCachedValue)
     // - Actual  :
     // i.e. the conditional field lost its cached content.
     getParagraph(2, "1");
+}
+
+CPPUNIT_TEST_FIXTURE(SwUibaseUnoTest, testCreateTextRangeByPixelPosition)
+{
+    // Given a document with 2 characters, and the pixel position of the point between them:
+    SwDoc* pDoc = createSwDoc();
+    SwDocShell* pDocShell = pDoc->GetDocShell();
+    SwWrtShell* pWrtShell = pDocShell->GetWrtShell();
+    pWrtShell->Insert2("AZ");
+    pWrtShell->Left(CRSR_SKIP_CHARS, /*bSelect=*/false, 1, /*bBasicCall=*/false);
+    Point aLogic = pWrtShell->GetCharRect().Center();
+    SwView* pView = pDocShell->GetView();
+    SwEditWin& rEditWin = pView->GetEditWin();
+    Point aPixel = rEditWin.LogicToPixel(aLogic);
+
+    // When converting that pixel position to a document model position (text range):
+    uno::Reference<frame::XModel2> xModel(mxComponent, uno::UNO_QUERY);
+    uno::Reference<container::XEnumeration> xControllers = xModel->getControllers();
+    uno::Reference<text::XTextViewTextRangeSupplier> xController(xControllers->nextElement(),
+                                                                 uno::UNO_QUERY);
+    awt::Point aPoint(aPixel.getX(), aPixel.getY());
+    uno::Reference<text::XTextRange> xTextRange
+        = xController->createTextRangeByPixelPosition(aPoint);
+
+    // Then make sure that text range points after the first character:
+    auto pTextRange = dynamic_cast<SwXTextRange*>(xTextRange.get());
+    SwPaM aPaM(pDoc->GetNodes());
+    pTextRange->GetPositions(aPaM);
+    sal_Int32 nActual = aPaM.GetPoint()->nContent.GetIndex();
+    // Without the needed PixelToLogic() call in place, this test would have failed with:
+    // - Expected: 1
+    // - Actual  : 0
+    // i.e. the returned text range pointed before the first character, not between the first and
+    // the second character.
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(1), nActual);
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();
