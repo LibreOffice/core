LibreOffice/core

commit e4e80ed313882f9ea1b309054e5aa3e839586516

loplugin:staticdynamic now with extra salt
because it wasn't quite there yet - now checks for casts with and
without const, because const doesn't really matter here.

Change-Id: I319025b2095a803fcaad2a7a696d2730b7fd2f81
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/112098
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/source/edit/textundo.cxx
@@ -273,10 +273,9 @@ void TextUndoInsertChars::Redo()
 
 bool TextUndoInsertChars::Merge( SfxUndoAction* pNextAction )
 {
-    if ( nullptr == dynamic_cast< const TextUndoInsertChars*>( pNextAction ) )
-        return false;
-
     TextUndoInsertChars* pNext = static_cast<TextUndoInsertChars*>(pNextAction);
+    if ( !pNext )
+        return false;
 
     if ( maTextPaM.GetPara() != pNext->maTextPaM.GetPara() )
         return false;

compilerplugins/clang/staticdynamic.cxx
@@ -80,6 +80,18 @@ bool StaticDynamic::TraverseCompoundStmt(CompoundStmt* compoundStmt)
     return ret;
 }
 
+const clang::Type* strip(QualType qt)
+{
+    const clang::Type* varType = qt->getUnqualifiedDesugaredType();
+    if (varType->isPointerType())
+        varType = varType->getPointeeType()->getUnqualifiedDesugaredType();
+    if (varType->isReferenceType())
+        varType = varType->getAs<clang::ReferenceType>()
+                      ->getPointeeType()
+                      ->getUnqualifiedDesugaredType();
+    return varType;
+}
+
 bool StaticDynamic::VisitCXXStaticCastExpr(CXXStaticCastExpr const* staticCastExpr)
 {
     if (ignoreLocation(staticCastExpr))
@@ -90,8 +102,8 @@ bool StaticDynamic::VisitCXXStaticCastExpr(CXXStaticCastExpr const* staticCastEx
     auto varDecl = dyn_cast_or_null<VarDecl>(subExprDecl->getDecl());
     if (!varDecl)
         return true;
-    auto it = blockState.dynamicCastVars.find(
-        { varDecl, staticCastExpr->getTypeAsWritten().getTypePtr() });
+    auto varType = strip(staticCastExpr->getType());
+    auto it = blockState.dynamicCastVars.find({ varDecl, varType });
     if (it != blockState.dynamicCastVars.end())
     {
         StringRef fn = getFilenameOfLocation(
@@ -110,8 +122,7 @@ bool StaticDynamic::VisitCXXStaticCastExpr(CXXStaticCastExpr const* staticCastEx
         report(DiagnosticsEngine::Note, "dynamic_cast here", it->second);
         return true;
     }
-    blockState.staticCastVars.insert({ { varDecl, staticCastExpr->getTypeAsWritten().getTypePtr() },
-                                       compat::getBeginLoc(staticCastExpr) });
+    blockState.staticCastVars.insert({ { varDecl, varType }, compat::getBeginLoc(staticCastExpr) });
     return true;
 }
 
@@ -126,8 +137,8 @@ bool StaticDynamic::VisitCXXDynamicCastExpr(CXXDynamicCastExpr const* dynamicCas
     auto varDecl = dyn_cast_or_null<VarDecl>(subExprDecl->getDecl());
     if (!varDecl)
         return true;
-    auto it = blockState.staticCastVars.find(
-        { varDecl, dynamicCastExpr->getTypeAsWritten().getTypePtr() });
+    auto varType = strip(dynamicCastExpr->getTypeAsWritten());
+    auto it = blockState.staticCastVars.find({ varDecl, varType });
     if (it != blockState.staticCastVars.end())
     {
         report(DiagnosticsEngine::Warning, "dynamic_cast after static_cast",
@@ -145,8 +156,7 @@ bool StaticDynamic::VisitCXXDynamicCastExpr(CXXDynamicCastExpr const* dynamicCas
         return true;
     }
     blockState.dynamicCastVars.insert(
-        { { varDecl, dynamicCastExpr->getTypeAsWritten().getTypePtr() },
-          compat::getBeginLoc(dynamicCastExpr) });
+        { { varDecl, varType }, compat::getBeginLoc(dynamicCastExpr) });
     return true;
 }
 

compilerplugins/clang/test/staticdynamic.cxx
@@ -14,7 +14,7 @@ struct ClassA
 
 struct ClassB : public ClassA
 {
-    void foo() {}
+    void foo() const {}
 };
 
 void f1(ClassA* p1)
@@ -29,6 +29,14 @@ void f2(ClassA* p1)
 {
     // expected-note@+1 {{dynamic_cast here [loplugin:staticdynamic]}}
     dynamic_cast<ClassB*>(p1)->foo();
+    // expected-error@+1 {{static_cast after dynamic_cast [loplugin:staticdynamic]}}
+    static_cast<ClassB*>(p1)->foo();
+};
+
+void f3(ClassA* p1)
+{
+    // expected-note@+1 {{dynamic_cast here [loplugin:staticdynamic]}}
+    dynamic_cast<const ClassB*>(p1)->foo();
     // expected-error@+1 {{static_cast after dynamic_cast [loplugin:staticdynamic]}}
     static_cast<ClassB*>(p1)->foo();
 };

sc/source/core/tool/interpr4.cxx
@@ -3225,14 +3225,19 @@ void ScInterpreter::ScMacro()
     }
 
     SbxVariable* pVar = pRoot ? pRoot->Find(aMacro, SbxClassType::Method) : nullptr;
-    if( !pVar || pVar->GetType() == SbxVOID || dynamic_cast<const SbMethod*>( pVar) ==  nullptr )
+    if( !pVar || pVar->GetType() == SbxVOID )
+    {
+        PushError( FormulaError::NoMacro );
+        return;
+    }
+    SbMethod* pMethod = dynamic_cast<SbMethod*>(pVar);
+    if( !pMethod )
     {
         PushError( FormulaError::NoMacro );
         return;
     }
 
     bool bVolatileMacro = false;
-    SbMethod* pMethod = static_cast<SbMethod*>(pVar);
 
     SbModule* pModule = pMethod->GetModule();
     bool bUseVBAObjects = pModule->IsVBACompat();

starmath/source/edit.cxx
@@ -467,9 +467,8 @@ void SmEditWindow::KeyInput(const KeyEvent& rKEvt)
             {
                 // SFX has maybe called a slot of the view and thus (because of a hack in SFX)
                 // set the focus to the view
-                SfxViewShell* pVShell = GetView();
-                if ( dynamic_cast<const SmViewShell *>(pVShell) &&
-                     static_cast<SmViewShell*>(pVShell)->GetGraphicWindow().HasFocus() )
+                SmViewShell* pVShell = GetView();
+                if ( pVShell && pVShell->GetGraphicWindow().HasFocus() )
                 {
                     GrabFocus();
                 }

svx/source/engine3d/view3d.cxx
@@ -662,7 +662,8 @@ void E3dView::ImpChangeSomeAttributesFor3DConversion(SdrObject* pObj)
 
 void E3dView::ImpChangeSomeAttributesFor3DConversion2(SdrObject* pObj)
 {
-    if(dynamic_cast<const SdrPathObj*>( pObj) ==  nullptr)
+    auto pPathObj = dynamic_cast<const SdrPathObj*>( pObj);
+    if(!pPathObj)
         return;
 
     const SfxItemSet& rSet = pObj->GetMergedItemSet();
@@ -670,7 +671,7 @@ void E3dView::ImpChangeSomeAttributesFor3DConversion2(SdrObject* pObj)
     drawing::LineStyle eLineStyle = rSet.Get(XATTR_LINESTYLE).GetValue();
     drawing::FillStyle eFillStyle = rSet.Get(XATTR_FILLSTYLE).GetValue();
 
-    if(static_cast<SdrPathObj*>(pObj)->IsClosed()
+    if(pPathObj->IsClosed()
         && eLineStyle == drawing::LineStyle_SOLID
         && !nLineWidth
         && eFillStyle != drawing::FillStyle_NONE)

svx/source/form/fmview.cxx
@@ -76,9 +76,9 @@ void FmFormView::Init()
     SdrModel* pModel = GetModel();
 
     DBG_ASSERT( dynamic_cast<const FmFormModel*>( pModel) !=  nullptr, "Wrong model" );
-    if( dynamic_cast<const FmFormModel*>( pModel) ==  nullptr ) return;
-    FmFormModel* pFormModel = static_cast<FmFormModel*>(pModel);
-
+    FmFormModel* pFormModel = dynamic_cast<FmFormModel*>(pModel);
+    if( !pFormModel )
+        return;
 
     // get DesignMode from model
     bool bInitDesignMode = pFormModel->GetOpenInDesignMode();

svx/source/form/navigatortree.cxx
@@ -1602,19 +1602,19 @@ namespace svxform
             FmEntryData* pCurrent = reinterpret_cast<FmEntryData*>(m_xTreeView->get_id(*rIter).toInt64());
 
             // a form ?
-            bool bIsForm = dynamic_cast<const FmFormData*>( pCurrent) !=  nullptr;
+            auto pFormData = dynamic_cast<FmFormData*>(pCurrent);
 
             // because deletion is done by the view, and i build on its MarkList,
             // but normally only direct controls, no indirect ones, are marked in a marked form,
             // I have to do it later
-            if (bIsForm)
-                MarkViewObj(static_cast<FmFormData*>(pCurrent), true/*deep*/);
+            if (pFormData)
+                MarkViewObj(pFormData, true/*deep*/);
 
             // a hidden control ?
             bool bIsHidden = IsHiddenControl(pCurrent);
 
             // keep forms and hidden controls, the rest not
-            if (!bIsForm && !bIsHidden)
+            if (!pFormData && !bIsHidden)
             {
                 // well, no form and no hidden control -> we can remove it from m_arrCurrentSelection, as it will
                 // be deleted automatically. This is because for every model (except forms and hidden control models)

commit 60d4fa743cd1863719ccb2dc1ba9aa892314bc29

this should be dynamic_cast
accidentally broken with
    commit e4e80ed313882f9ea1b309054e5aa3e839586516
    Date:   Sat Mar 6 20:40:58 2021 +0200
    loplugin:staticdynamic now with extra salt

Change-Id: If5946d7f65eb51c9785e7ad2e6def0ebd56fd9cc

vcl/source/edit/textundo.cxx
@@ -273,7 +273,7 @@ void TextUndoInsertChars::Redo()
 
 bool TextUndoInsertChars::Merge( SfxUndoAction* pNextAction )
 {
-    TextUndoInsertChars* pNext = static_cast<TextUndoInsertChars*>(pNextAction);
+    TextUndoInsertChars* pNext = dynamic_cast<TextUndoInsertChars*>(pNextAction);
     if ( !pNext )
         return false;
 