48983209a155071e738d2dc0baf5a7bc5b001e93
Mike Kaganski <mike.kaganski@collabora.com>
2021-03-10 10:03:36 +0300

    Drop 'static_cast<cppu::OWeakObject*>' syntactic noise
    ... a leftover from 96388e5e809a48573970df9b6b2649517a08447f.
    
    Change-Id: I909a470612c421472d8bf94f80e3e94ecb51d6e3

--- stoc/source/proxy_factory/proxyfac.cxx
+++ stoc/source/proxy_factory/proxyfac.cxx
@@ -401,7 +401,7 @@ extern "C" SAL_DLLPUBLIC_EXPORT css::uno::XInterface*
 stoc_FactoryImpl_get_implementation(
     css::uno::XComponentContext* , css::uno::Sequence<css::uno::Any> const&)
 {
-    return cppu::acquire(static_cast< ::cppu::OWeakObject * >(new FactoryImpl));
+    return cppu::acquire(new FactoryImpl);
 }
 
 
