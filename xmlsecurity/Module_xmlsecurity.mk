149df1fec6472e30582162e17e04c75aee91d26a
Miklos Vajna <vmiklos@collabora.com>
2021-03-11 17:45:58 +0100

    Turn CppunitTest_xmlsecurity_signing into a subsequentcheck
    This is one of the most unreliable tests on Windows, probably due to
    reading the real system cert store, unlike macOS/Linux (which have a
    per-test NSS template).
    
    Give up on running this on Windows / macOS for pre-submit CI to get more
    reliability instead. Local 'make check' on Windows keeps running this,
    since it does pass most of the time.
    
    The change can be reverted once somebody can figure out how to reliably
    reproduce this failure on Windows.
    
    Change-Id: I50df09bda9c353ce08038dab8d307bbf7738bb5b
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/112358
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Tested-by: Jenkins

--- xmlsecurity/Module_xmlsecurity.mk
+++ xmlsecurity/Module_xmlsecurity.mk
@@ -18,8 +18,11 @@ $(eval $(call gb_Module_add_targets,xmlsecurity,\
 ))
 
 $(eval $(call gb_Module_add_slowcheck_targets,xmlsecurity,\
-    CppunitTest_xmlsecurity_signing \
     CppunitTest_xmlsecurity_pdfsigning \
+))
+
+$(eval $(call gb_Module_add_subsequentcheck_targets,xmlsecurity,\
+    CppunitTest_xmlsecurity_signing \
 ))
 
 $(eval $(call gb_Module_add_l10n_targets,xmlsecurity,\
