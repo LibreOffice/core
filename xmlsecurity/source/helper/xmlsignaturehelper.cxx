0537731048d79f77b5557a1829836fb2bd0031b5
Caolán McNamara <caolanm@redhat.com>
2021-03-11 20:23:31 +0000

    do same set error state as ReadAndVerifySignature does
    this function is nearly exactly the same as ReadAndVerifySignature
    except it doesn't set error-state on exception during parse
    
    Change-Id: Ife881f639a11d3185920ca62cc2cd22812fae36d
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/112366
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/112412
    (cherry picked from commit 52a23d595b820cab27d76d0e7c129f2757c762d6)
    (cherry picked from commit ed6db8f42484f135137939cf38dc40fc3f0913b1)

--- xmlsecurity/source/helper/xmlsignaturehelper.cxx
+++ xmlsecurity/source/helper/xmlsignaturehelper.cxx
@@ -408,6 +408,7 @@ bool XMLSignatureHelper::ReadAndVerifySignatureStorageStream(const css::uno::Ref
     catch(const uno::Exception&)
     {
         DBG_UNHANDLED_EXCEPTION("xmlsecurity.helper");
+        mbError = true;
     }
 
     mpXSecController->releaseSignatureReader();
