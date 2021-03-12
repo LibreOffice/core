17cb52467cc74ffd5769a5d8640070ef6217674c
ViswaasLP <viswaas98@gmail.com>
2021-03-11 22:12:51 -0800

    Changed header guards to pragma once
    Change-Id: I74c85b0327005ba7b2d07169f052a903381eb1f1

--- sw/source/uibase/inc/abstract.hxx
+++ sw/source/uibase/inc/abstract.hxx
@@ -16,8 +16,7 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
-#ifndef INCLUDED_SW_SOURCE_UIBASE_INC_ABSTRACT_HXX
-#define INCLUDED_SW_SOURCE_UIBASE_INC_ABSTRACT_HXX
+#pragma once
 
 #include <sfx2/basedlgs.hxx>
 
@@ -33,7 +32,5 @@ public:
     sal_uInt8 GetLevel() const;
     sal_uInt8 GetPara() const;
 };
-
-#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
