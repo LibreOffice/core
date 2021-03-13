5b39249da86402bff83166a57235c38f51e62f80
Arnaud Versini <arnaud.versini@libreoffice.org>
2021-03-13 18:10:46 +0100

    cppu : use std::mutex in threadpool
    Change-Id: I559a2c533accfe95740c29d726833d0bbab210fa

--- cppu/source/threadpool/threadpool.hxx
+++ cppu/source/threadpool/threadpool.hxx
@@ -19,6 +19,7 @@
 
 #pragma once
 
+#include <mutex>
 #include <vector>
 #include <unordered_map>
 
@@ -103,7 +104,7 @@ namespace cppu_threadpool {
         void join();
 
         void remove_locked( rtl::Reference< ORequestThread > const & );
-        ::osl::Mutex m_mutex;
+        ::std::mutex m_mutex;
 
     private:
         std::deque< rtl::Reference< ORequestThread > > m_deque;
@@ -147,9 +148,9 @@ namespace cppu_threadpool {
 
 
         ThreadIdHashMap m_mapQueue;
-        ::osl::Mutex m_mutex;
+        ::std::mutex m_mutex;
 
-        ::osl::Mutex m_mutexWaitingThreadList;
+        ::std::mutex m_mutexWaitingThreadList;
         WaitingThreadDeque m_dequeThreads;
 
         DisposedCallerAdminHolder m_DisposedCallerAdmin;
