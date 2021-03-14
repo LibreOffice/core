1b34aaeff54f1df11bff56d090a3d7b75ff08769
Thorsten Behrens <thorsten.behrens@allotropia.de>
2021-03-14 02:24:59 +0100

    Fix ARM64 build after postgres upgrade to 13.1
    Change-Id: I12bdda6aedd9b7b15423f997fe8c6910d9c9e9d5
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/112464
    Tested-by: Jenkins
    Reviewed-by: Thorsten Behrens <thorsten.behrens@allotropia.de>
    (cherry picked from commit db8fb941ecc4c6e0279b47e4560b4b285339a17f)

--- external/postgresql/UnpackedTarball_postgresql.mk
+++ external/postgresql/UnpackedTarball_postgresql.mk
@@ -17,8 +17,11 @@ $(eval $(call gb_UnpackedTarball_add_patches,postgresql, \
 	external/postgresql/windows.patch.0 \
 	external/postgresql/postgresql.exit.patch.0 \
 	external/postgresql/postgres-msvc-build.patch.1 \
+	$(if $(filter WNT_ARM64,$(OS)_$(CPUNAME)), external/postgresql/arm64.patch.1) \
 ))
 
+ifeq ($(CROSS_COMPILING),)
 $(eval $(call gb_UnpackedTarball_add_file,postgresql,src/tools/msvc/config.pl,external/postgresql/config.pl))
+endif
 
 # vim: set noet sw=4 ts=4:
