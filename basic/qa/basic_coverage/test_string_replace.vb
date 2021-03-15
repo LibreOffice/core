1052c7db143cae6cbb51b0dadcb777667c881fcf
Andreas Heinisch <andreas.heinisch@yahoo.de>
2021-03-15 13:41:58 +0100

    tdf#141045 - fixed copy paste error in the replace function
    Change-Id: Id68670fed89e4cc700c5eea395139914bebdb657

--- basic/qa/basic_coverage/test_string_replace.vb
+++ basic/qa/basic_coverage/test_string_replace.vb
@@ -23,6 +23,10 @@ Function verify_stringReplace() As String
     retStr = Replace("АБВабв", "б", "*")
     TestLog_ASSERT retStr, "А*Ва*в", "case-insensitive non-ASCII: " & retStr
 
+    ' tdf#141045 - different length of search and replace string
+    retStr = Replace("a", "abc", "ab")
+    TestLog_ASSERT retStr, "a", "different length of search and replace string: " & retStr
+
     result = result & Chr$(10) & "Tests passed: " & passCount & Chr$(10) & "Tests failed: " & failCount & Chr$(10)
     verify_stringReplace = result
 End Function
