LibreOffice/core

commit a5a836d8c43dc9cebbbf8af39bf0142de603a7c6

DOCX filter: effect extent should be part of the margin
OOXML has an object size (extent) a size including effects like shadow (effect
extent) and the margin (top/bottom/left/right distance). Our doc model doesn't
have an explicit "effect extent", but to have the intended layout, we at least
need the spacing of the effect around flys (even if a number of effects are not
rendered). Fix this by adding the effect extent to the margin; if we still have
the effect extent grab-bag around, then this can be undone during export.

Change-Id: I9609ce347c54588395e09ad91a84c872b9007f85

sw/source/filter/ww8/docxsdrexport.cxx
@@ -319,6 +319,56 @@ void DocxSdrExport::startDMLAnchorInline(const SwFrmFmt* pFrmFmt, const Size& rS
     {
         isAnchor = pFrmFmt->GetAnchor().GetAnchorId() != FLY_AS_CHAR;
     }
+
+    // Count effectExtent values, their value is needed before dist{T,B,L,R} is written.
+    SvxShadowItem aShadowItem = pFrmFmt->GetShadow();
+    sal_Int32 nLeftExt = 0, nRightExt = 0, nTopExt = 0, nBottomExt = 0;
+    if (aShadowItem.GetLocation() != SVX_SHADOW_NONE)
+    {
+        sal_Int32 nShadowWidth(TwipsToEMU(aShadowItem.GetWidth()));
+        switch (aShadowItem.GetLocation())
+        {
+        case SVX_SHADOW_TOPLEFT:
+            nTopExt = nLeftExt = nShadowWidth;
+            break;
+        case SVX_SHADOW_TOPRIGHT:
+            nTopExt = nRightExt = nShadowWidth;
+            break;
+        case SVX_SHADOW_BOTTOMLEFT:
+            nBottomExt = nLeftExt = nShadowWidth;
+            break;
+        case SVX_SHADOW_BOTTOMRIGHT:
+            nBottomExt = nRightExt = nShadowWidth;
+            break;
+        case SVX_SHADOW_NONE:
+        case SVX_SHADOW_END:
+            break;
+        }
+    }
+    else if (const SdrObject* pObject = pFrmFmt->FindRealSdrObject())
+    {
+        // No shadow, but we have an idea what was the original effectExtent.
+        uno::Any aAny;
+        pObject->GetGrabBagItem(aAny);
+        comphelper::SequenceAsHashMap aGrabBag(aAny);
+        comphelper::SequenceAsHashMap::iterator it = aGrabBag.find("CT_EffectExtent");
+        if (it != aGrabBag.end())
+        {
+            comphelper::SequenceAsHashMap aEffectExtent(it->second);
+            for (std::pair<const OUString, uno::Any>& rDirection : aEffectExtent)
+            {
+                if (rDirection.first == "l" && rDirection.second.has<sal_Int32>())
+                    nLeftExt = rDirection.second.get<sal_Int32>();
+                else if (rDirection.first == "t" && rDirection.second.has<sal_Int32>())
+                    nTopExt = rDirection.second.get<sal_Int32>();
+                else if (rDirection.first == "r" && rDirection.second.has<sal_Int32>())
+                    nRightExt = rDirection.second.get<sal_Int32>();
+                else if (rDirection.first == "b" && rDirection.second.has<sal_Int32>())
+                    nBottomExt = rDirection.second.get<sal_Int32>();
+            }
+        }
+    }
+
     if (isAnchor)
     {
         sax_fastparser::FastAttributeList* attrList = m_pImpl->m_pSerializer->createAttrList();
@@ -333,10 +383,10 @@ void DocxSdrExport::startDMLAnchorInline(const SwFrmFmt* pFrmFmt, const Size& rS
             lclMovePositionWithRotation(aPos, rSize, pObj->GetRotateAngle());
         }
         attrList->add(XML_behindDoc, bOpaque ? "0" : "1");
-        attrList->add(XML_distT, OString::number(TwipsToEMU(pULSpaceItem.GetUpper())).getStr());
-        attrList->add(XML_distB, OString::number(TwipsToEMU(pULSpaceItem.GetLower())).getStr());
-        attrList->add(XML_distL, OString::number(TwipsToEMU(pLRSpaceItem.GetLeft())).getStr());
-        attrList->add(XML_distR, OString::number(TwipsToEMU(pLRSpaceItem.GetRight())).getStr());
+        attrList->add(XML_distT, OString::number(TwipsToEMU(pULSpaceItem.GetUpper()) - nTopExt).getStr());
+        attrList->add(XML_distB, OString::number(TwipsToEMU(pULSpaceItem.GetLower()) - nBottomExt).getStr());
+        attrList->add(XML_distL, OString::number(TwipsToEMU(pLRSpaceItem.GetLeft()) - nLeftExt).getStr());
+        attrList->add(XML_distR, OString::number(TwipsToEMU(pLRSpaceItem.GetRight()) - nRightExt).getStr());
         attrList->add(XML_simplePos, "0");
         attrList->add(XML_locked, "0");
         attrList->add(XML_layoutInCell, "1");
@@ -587,59 +637,11 @@ void DocxSdrExport::startDMLAnchorInline(const SwFrmFmt* pFrmFmt, const Size& rS
                                             FSEND);
 
     // effectExtent, extent including the effect (shadow only for now)
-    SvxShadowItem aShadowItem = pFrmFmt->GetShadow();
-    OString aLeftExt("0"), aRightExt("0"), aTopExt("0"), aBottomExt("0");
-    if (aShadowItem.GetLocation() != SVX_SHADOW_NONE)
-    {
-        OString aShadowWidth(OString::number(TwipsToEMU(aShadowItem.GetWidth())));
-        switch (aShadowItem.GetLocation())
-        {
-        case SVX_SHADOW_TOPLEFT:
-            aTopExt = aLeftExt = aShadowWidth;
-            break;
-        case SVX_SHADOW_TOPRIGHT:
-            aTopExt = aRightExt = aShadowWidth;
-            break;
-        case SVX_SHADOW_BOTTOMLEFT:
-            aBottomExt = aLeftExt = aShadowWidth;
-            break;
-        case SVX_SHADOW_BOTTOMRIGHT:
-            aBottomExt = aRightExt = aShadowWidth;
-            break;
-        case SVX_SHADOW_NONE:
-        case SVX_SHADOW_END:
-            break;
-        }
-    }
-    else if (const SdrObject* pObject = pFrmFmt->FindRealSdrObject())
-    {
-        // No shadow, but we have an idea what was the original effectExtent.
-        uno::Any aAny;
-        pObject->GetGrabBagItem(aAny);
-        comphelper::SequenceAsHashMap aGrabBag(aAny);
-        comphelper::SequenceAsHashMap::iterator it = aGrabBag.find("CT_EffectExtent");
-        if (it != aGrabBag.end())
-        {
-            comphelper::SequenceAsHashMap aEffectExtent(it->second);
-            for (std::pair<const OUString, uno::Any>& rDirection : aEffectExtent)
-            {
-                if (rDirection.first == "l" && rDirection.second.has<sal_Int32>())
-                    aLeftExt = OString::number(rDirection.second.get<sal_Int32>());
-                else if (rDirection.first == "t" && rDirection.second.has<sal_Int32>())
-                    aTopExt = OString::number(rDirection.second.get<sal_Int32>());
-                else if (rDirection.first == "r" && rDirection.second.has<sal_Int32>())
-                    aRightExt = OString::number(rDirection.second.get<sal_Int32>());
-                else if (rDirection.first == "b" && rDirection.second.has<sal_Int32>())
-                    aBottomExt = OString::number(rDirection.second.get<sal_Int32>());
-            }
-        }
-    }
-
     m_pImpl->m_pSerializer->singleElementNS(XML_wp, XML_effectExtent,
-                                            XML_l, aLeftExt,
-                                            XML_t, aTopExt,
-                                            XML_r, aRightExt,
-                                            XML_b, aBottomExt,
+                                            XML_l, OString::number(nLeftExt),
+                                            XML_t, OString::number(nTopExt),
+                                            XML_r, OString::number(nRightExt),
+                                            XML_b, OString::number(nBottomExt),
                                             FSEND);
 
     // See if we know the exact wrap type from grab-bag.

writerfilter/source/dmapper/GraphicImport.cxx
@@ -557,15 +557,19 @@ void GraphicImport::lcl_attribute(Id nName, Value& rValue)
         break;
         case NS_ooxml::LN_CT_EffectExtent_l:
             m_pImpl->m_oEffectExtentLeft = nIntValue;
+            m_pImpl->nLeftMargin += oox::drawingml::convertEmuToHmm(nIntValue);
             break;
         case NS_ooxml::LN_CT_EffectExtent_t:
             m_pImpl->m_oEffectExtentTop = nIntValue;
+            m_pImpl->nTopMargin += oox::drawingml::convertEmuToHmm(nIntValue);
             break;
         case NS_ooxml::LN_CT_EffectExtent_r:
             m_pImpl->m_oEffectExtentRight = nIntValue;
+            m_pImpl->nRightMargin += oox::drawingml::convertEmuToHmm(nIntValue);
             break;
         case NS_ooxml::LN_CT_EffectExtent_b:
             m_pImpl->m_oEffectExtentBottom = nIntValue;
+            m_pImpl->nBottomMargin += oox::drawingml::convertEmuToHmm(nIntValue);
             break;
         case NS_ooxml::LN_CT_NonVisualDrawingProps_id:// 90650;
             //id of the object - ignored

sw/qa/extras/ooxmlexport/ooxmlexport.cxx
@@ -16,6 +16,7 @@
 #include <com/sun/star/text/HoriOrientation.hpp>
 #include <com/sun/star/text/XTextRangeCompare.hpp>
 #include <com/sun/star/text/WritingMode2.hpp>
+#include <oox/drawingml/drawingmltypes.hxx>
 
 #include <string>
 
@@ -619,6 +620,15 @@ DECLARE_OOXMLEXPORT_TEST(testTextboxRightEdge, "textbox-right-edge.docx")
     // inside the draw shape.
     CPPUNIT_ASSERT(nShapeLeft + nShapeWidth >= nTextboxLeft + nTextboxWidth);
 }
+
+DECLARE_OOXMLEXPORT_TEST(testEffectExtentMargin, "effectextent-margin.docx")
+{
+    // This was 318, i.e. oox::drawingml::convertEmuToHmm(114300), effectExtent
+    // wasn't part of the margin, leading to the fly not taking enough space
+    // around itself.
+    CPPUNIT_ASSERT_EQUAL(oox::drawingml::convertEmuToHmm(114300+95250), getProperty<sal_Int32>(getShape(1), "LeftMargin"));
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

commit f58e1d8abda9a7e38a4b192123bc4189d2d365e3

tdf#138895 DOCX filter: fix handling for effect extent vs line width
Regression from commit a5a836d8c43dc9cebbbf8af39bf0142de603a7c6 (DOCX
filter: effect extent should be part of the margin, 2014-12-04), the
problem was that effect extent is OK to be added as an extra margin, but
line width is part of that effect extent in Word, so Writer margin
should not be increased with the line width.

The Word behavior seems to be that half of the line width is part of
e.g. the top effect extent, then the other half is part of the bottom
one (and so on).

The bugdoc's case was that a too large margin shifted the last line
below the shape, and this tiny half-line-width extra margin handled
correctly puts the line back to its correct place.

Change-Id: Ic897926f3d79f979ea84aef3dbda49c46b18a3ac

sw/source/filter/ww8/docxsdrexport.cxx
@@ -37,6 +37,7 @@
 #include <IDocumentDrawModelAccess.hxx>
 
 #include <tools/diagnose_ex.h>
+#include <svx/xlnwtit.hxx>
 
 using namespace com::sun::star;
 using namespace oox;
@@ -452,22 +453,32 @@ void DocxSdrExport::startDMLAnchorInline(const SwFrameFormat* pFrameFormat, cons
             }
         }
         attrList->add(XML_behindDoc, bOpaque ? "0" : "1");
+        sal_Int32 nLineWidth = 0;
+        if (const SdrObject* pObject = pFrameFormat->FindRealSdrObject())
+        {
+            nLineWidth = pObject->GetMergedItem(XATTR_LINEWIDTH).GetValue();
+        }
+
         // Extend distance with the effect extent if the shape is not rotated, which is the opposite
         // of the mapping done at import time.
         // The type of dist* attributes is unsigned, so make sure no negative value is written.
         sal_Int64 nTopExtDist = nRotation ? 0 : nTopExt;
+        nTopExtDist -= TwipsToEMU(nLineWidth / 2);
         sal_Int64 nDistT = std::max(static_cast<sal_Int64>(0),
                                     TwipsToEMU(aULSpaceItem.GetUpper()) - nTopExtDist);
         attrList->add(XML_distT, OString::number(nDistT).getStr());
         sal_Int64 nBottomExtDist = nRotation ? 0 : nBottomExt;
+        nBottomExtDist -= TwipsToEMU(nLineWidth / 2);
         sal_Int64 nDistB = std::max(static_cast<sal_Int64>(0),
                                     TwipsToEMU(aULSpaceItem.GetLower()) - nBottomExtDist);
         attrList->add(XML_distB, OString::number(nDistB).getStr());
         sal_Int64 nLeftExtDist = nRotation ? 0 : nLeftExt;
+        nLeftExtDist -= TwipsToEMU(nLineWidth / 2);
         sal_Int64 nDistL = std::max(static_cast<sal_Int64>(0),
                                     TwipsToEMU(aLRSpaceItem.GetLeft()) - nLeftExtDist);
         attrList->add(XML_distL, OString::number(nDistL).getStr());
         sal_Int64 nRightExtDist = nRotation ? 0 : nRightExt;
+        nRightExtDist -= TwipsToEMU(nLineWidth / 2);
         sal_Int64 nDistR = std::max(static_cast<sal_Int64>(0),
                                     TwipsToEMU(aLRSpaceItem.GetRight()) - nRightExtDist);
         attrList->add(XML_distR, OString::number(nDistR).getStr());

writerfilter/source/dmapper/GraphicImport.cxx
@@ -810,25 +810,51 @@ void GraphicImport::lcl_attribute(Id nName, Value& rValue)
                                 // Include effect extent in the margin to bring Writer layout closer
                                 // to Word. But do this for non-rotated shapes only, where effect
                                 // extents map to increased margins as-is.
+
+                                sal_Int32 nLineWidth{};
+                                if (xShapeProps->getPropertySetInfo()->hasPropertyByName("LineWidth"))
+                                {
+                                    xShapeProps->getPropertyValue("LineWidth") >>= nLineWidth;
+                                }
+
                                 if (m_pImpl->m_oEffectExtentLeft)
                                 {
-                                    m_pImpl->nLeftMargin += oox::drawingml::convertEmuToHmm(
+                                    sal_Int32 nLeft = oox::drawingml::convertEmuToHmm(
                                         *m_pImpl->m_oEffectExtentLeft);
+                                    if (nLeft >= nLineWidth / 2)
+                                    {
+                                        nLeft -= nLineWidth / 2;
+                                    }
+                                    m_pImpl->nLeftMargin += nLeft;
                                 }
                                 if (m_pImpl->m_oEffectExtentTop)
                                 {
-                                    m_pImpl->nTopMargin += oox::drawingml::convertEmuToHmm(
-                                        *m_pImpl->m_oEffectExtentTop);
+                                    sal_Int32 nTop = oox::drawingml::convertEmuToHmm(*m_pImpl->m_oEffectExtentTop);
+                                    if (nTop >= nLineWidth / 2)
+                                    {
+                                        nTop -= nLineWidth / 2;
+                                    }
+                                    m_pImpl->nTopMargin += nTop;
                                 }
                                 if (m_pImpl->m_oEffectExtentRight)
                                 {
-                                    m_pImpl->nRightMargin += oox::drawingml::convertEmuToHmm(
+                                    sal_Int32 nRight = oox::drawingml::convertEmuToHmm(
                                         *m_pImpl->m_oEffectExtentRight);
+                                    if (nRight >= nLineWidth / 2)
+                                    {
+                                        nRight -= nLineWidth / 2;
+                                    }
+                                    m_pImpl->nRightMargin += nRight;
                                 }
                                 if (m_pImpl->m_oEffectExtentBottom)
                                 {
-                                    m_pImpl->nBottomMargin += oox::drawingml::convertEmuToHmm(
+                                    sal_Int32 nBottom = oox::drawingml::convertEmuToHmm(
                                         *m_pImpl->m_oEffectExtentBottom);
+                                    if (nBottom >= nLineWidth / 2)
+                                    {
+                                        nBottom -= nLineWidth / 2;
+                                    }
+                                    m_pImpl->nBottomMargin += nBottom;
                                 }
                             }
                         }

sw/qa/extras/ooxmlexport/ooxmlexport16.cxx
@@ -149,6 +149,27 @@ DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testFooterMarginLost, "footer-margin-lost.do
     assertXPath(pXmlDoc, "/w:document/w:body/w:sectPr/w:pgMar", "footer", "709");
 }
 
+CPPUNIT_TEST_FIXTURE(Test, testEffectExtentLineWidth)
+{
+    auto verify = [this]() {
+        CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(508),
+                             getProperty<sal_Int32>(getShape(1), "TopMargin"));
+    };
+
+    // Given a document with a shape that has a non-zero line width and effect extent:
+    // When loading the document:
+    load(mpTestDocumentPath, "effect-extent-line-width.docx");
+    // Then make sure that the line width is not taken twice (once as part of the margin, and then
+    // also as the line width):
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 508
+    // - Actual  : 561
+    // i.e. the upper spacing was too large, the last line of the text moved below the shape.
+    verify();
+    reload(mpFilter, "effect-extent-line-width.docx");
+    verify();
+}
+
 DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testTdf140572_docDefault_superscript, "tdf140572_docDefault_superscript.docx")
 {
     // A round-trip was crashing.