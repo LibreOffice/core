2e57abb9c75fa6e31cb79086bc98d87f7da4d608
Samuel Mehrbrodt <samuel.mehrbrodt@allotropia.de>
2021-03-16 16:27:44 +0100

    Add mechanism to selectively enable macros for document events
    Change-Id: I56703b2c0ee009a645458c78c026c546b2e7e321

--- sfx2/source/inc/eventsupplier.hxx
+++ sfx2/source/inc/eventsupplier.hxx
@@ -78,6 +78,10 @@ public:
                                     ::comphelper::NamedValueCollection& o_normalizedDescriptor,
                                     SfxObjectShell* i_document );
     static void Execute( css::uno::Any const & aEventData, const css::document::DocumentEvent& aTrigger, SfxObjectShell* pDoc );
+
+private:
+    /// Check if script URL whitelist exists, and if so, if current script url is part of it
+    static bool isScriptURLAllowed(const OUString& aScriptURL);
 };
 
 #endif
