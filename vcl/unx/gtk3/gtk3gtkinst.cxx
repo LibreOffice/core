8c60505ba0dd0ac9334f625bb0005c3a41dd2118
Caolán McNamara <caolanm@redhat.com>
2021-03-16 11:30:07 +0000

    add access to DumpAsPropertyTree for sidebar::Deck
    Change-Id: I9df780ba3a6701275256fff585f4a969178aaff2
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/112574
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>

--- vcl/unx/gtk3/gtk3gtkinst.cxx
+++ vcl/unx/gtk3/gtk3gtkinst.cxx
@@ -3047,6 +3047,11 @@ public:
         //not implemented for the gtk variant
     }
 
+    virtual void get_property_tree(tools::JsonWriter& /*rJsonWriter*/) override
+    {
+        //not implemented for the gtk variant
+    }
+
     virtual void set_stack_background() override
     {
         do_set_background(Application::GetSettings().GetStyleSettings().GetWindowColor());
