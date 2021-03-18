31e5617bbbe98ad0618e6681e4b7d88257d8148d
Noel <noel.grandin@collabora.co.uk>
2021-03-18 11:17:57 +0200

    WindowContentFactoryManager is already a singleton
    in the .component file, no need to do it twice
    
    Change-Id: I877c541c11d5c667b0d94cb3875f4263a9323dcb

--- framework/source/uifactory/windowcontentfactorymanager.cxx
+++ framework/source/uifactory/windowcontentfactorymanager.cxx
@@ -193,22 +193,6 @@ uno::Reference< uno::XInterface > SAL_CALL WindowContentFactoryManager::createIn
     return xWindow;
 }
 
-struct Instance {
-    explicit Instance(
-        css::uno::Reference<css::uno::XComponentContext> const & context):
-        instance(static_cast<cppu::OWeakObject *>(
-                    new WindowContentFactoryManager(context)))
-    {
-    }
-
-    css::uno::Reference<css::uno::XInterface> instance;
-};
-
-struct Singleton:
-    public rtl::StaticWithArg<
-        Instance, css::uno::Reference<css::uno::XComponentContext>, Singleton>
-{};
-
 }
 
 extern "C" SAL_DLLPUBLIC_EXPORT css::uno::XInterface *
@@ -216,8 +200,7 @@ com_sun_star_comp_framework_WindowContentFactoryManager_get_implementation(
     css::uno::XComponentContext *context,
     css::uno::Sequence<css::uno::Any> const &)
 {
-    return cppu::acquire(static_cast<cppu::OWeakObject *>(
-                Singleton::get(context).instance.get()));
+    return cppu::acquire(new WindowContentFactoryManager(context));
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
