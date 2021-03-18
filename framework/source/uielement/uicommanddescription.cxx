6b7f7904ebd21f25615cc4b269de29467377cb7f
Noel <noel.grandin@collabora.co.uk>
2021-03-18 11:26:03 +0200

    use single-use attribute for UICommandDescription
    instead of rtl::Instance, which means it will get
    cleaned up when UNO shuts down
    
    Change-Id: Ia26871e1a2cf2cc6150d681f3a40b42269fb65c0

--- framework/source/uielement/uicommanddescription.cxx
+++ framework/source/uielement/uicommanddescription.cxx
@@ -710,33 +710,12 @@ sal_Bool SAL_CALL UICommandDescription::hasElements()
 
 } // namespace framework
 
-namespace {
-
-struct Instance {
-    explicit Instance(
-        css::uno::Reference<css::uno::XComponentContext> const & context):
-        instance(static_cast<cppu::OWeakObject *>(
-                    new framework::UICommandDescription(context)))
-    {
-    }
-
-    css::uno::Reference<css::uno::XInterface> instance;
-};
-
-struct Singleton:
-    public rtl::StaticWithArg<
-        Instance, css::uno::Reference<css::uno::XComponentContext>, Singleton>
-{};
-
-}
-
 extern "C" SAL_DLLPUBLIC_EXPORT css::uno::XInterface *
 com_sun_star_comp_framework_UICommandDescription_get_implementation(
     css::uno::XComponentContext *context,
     css::uno::Sequence<css::uno::Any> const &)
 {
-    return cppu::acquire(static_cast<cppu::OWeakObject *>(
-                Singleton::get(context).instance.get()));
+    return cppu::acquire(new framework::UICommandDescription(context));
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
