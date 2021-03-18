604798905bdad39a25096d521a24565906d9f751
Noel <noel.grandin@collabora.co.uk>
2021-03-18 11:27:23 +0200

    use single-use attribute for WindowStateConfiguration
    instead of rtl::Instance, which means it will get
    cleaned up when UNO shuts down
    
    Change-Id: I2a6a3b1e989d48f4eebf19e9922df22124d25d2a

--- framework/source/uiconfiguration/windowstateconfiguration.cxx
+++ framework/source/uiconfiguration/windowstateconfiguration.cxx
@@ -1383,22 +1383,6 @@ sal_Bool SAL_CALL WindowStateConfiguration::hasElements()
     return true;
 }
 
-struct Instance {
-    explicit Instance(
-        css::uno::Reference<css::uno::XComponentContext> const & context):
-        instance(static_cast<cppu::OWeakObject *>(
-                    new WindowStateConfiguration(context)))
-    {
-    }
-
-    css::uno::Reference<css::uno::XInterface> instance;
-};
-
-struct Singleton:
-    public rtl::StaticWithArg<
-        Instance, css::uno::Reference<css::uno::XComponentContext>, Singleton>
-{};
-
 }
 
 extern "C" SAL_DLLPUBLIC_EXPORT css::uno::XInterface *
@@ -1406,8 +1390,7 @@ com_sun_star_comp_framework_WindowStateConfiguration_get_implementation(
     css::uno::XComponentContext *context,
     css::uno::Sequence<css::uno::Any> const &)
 {
-    return cppu::acquire(static_cast<cppu::OWeakObject *>(
-                Singleton::get(context).instance.get()));
+    return cppu::acquire(new WindowStateConfiguration(context));
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
