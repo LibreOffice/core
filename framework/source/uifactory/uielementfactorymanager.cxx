f5784794f4cecd4fd57a803191b544f80f7ba02e
Noel <noel.grandin@collabora.co.uk>
2021-03-18 11:19:21 +0200

    use single-use attribute for UIElementFactoryManager
    instead of rtl::Instance, which means it will get
    cleaned up when UNO shuts down
    
    Change-Id: I52193861c4b2e07c3655773c8d185b2036336da2
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/112682
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

--- framework/source/uifactory/uielementfactorymanager.cxx
+++ framework/source/uifactory/uielementfactorymanager.cxx
@@ -542,22 +542,6 @@ void SAL_CALL UIElementFactoryManager::deregisterFactory( const OUString& aType,
     // SAFE
 }
 
-struct Instance {
-    explicit Instance(
-        css::uno::Reference<css::uno::XComponentContext> const & context):
-        instance(static_cast<cppu::OWeakObject *>(
-                    new UIElementFactoryManager(context)))
-    {
-    }
-
-    css::uno::Reference<css::uno::XInterface> instance;
-};
-
-struct Singleton:
-    public rtl::StaticWithArg<
-        Instance, css::uno::Reference<css::uno::XComponentContext>, Singleton>
-{};
-
 }
 
 extern "C" SAL_DLLPUBLIC_EXPORT css::uno::XInterface *
@@ -565,8 +549,7 @@ com_sun_star_comp_framework_UIElementFactoryManager_get_implementation(
     css::uno::XComponentContext *context,
     css::uno::Sequence<css::uno::Any> const &)
 {
-    return cppu::acquire(static_cast<cppu::OWeakObject *>(
-            Singleton::get(context).instance.get()));
+    return cppu::acquire(new UIElementFactoryManager(context));
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
