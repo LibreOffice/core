c191fb06954f990b9bad71302252e7b565b9bcda
Noel <noel.grandin@collabora.co.uk>
2021-03-18 11:16:27 +0200

    use single-use attribute for ShutdownIcon
    instead of rtl::Instance, which means it will get
    cleaned up when UNO shuts down
    
    Change-Id: I40de6f9d9c86f1056860d6a9e412b0c1571703bb

--- sfx2/source/appl/shutdownicon.cxx
+++ sfx2/source/appl/shutdownicon.cxx
@@ -706,31 +706,12 @@ css::uno::Any SAL_CALL ShutdownIcon::getFastPropertyValue( ::sal_Int32 nHandle )
     return aValue;
 }
 
-namespace {
-
-struct Instance {
-    explicit Instance(
-        css::uno::Reference<css::uno::XComponentContext> const & context):
-        instance(static_cast<cppu::OWeakObject *>(new ShutdownIcon(context)))
-    {}
-
-    rtl::Reference<css::uno::XInterface> instance;
-};
-
-struct Singleton:
-    public rtl::StaticWithArg<
-        Instance, css::uno::Reference<css::uno::XComponentContext>, Singleton>
-{};
-
-}
-
 extern "C" SAL_DLLPUBLIC_EXPORT css::uno::XInterface *
 com_sun_star_comp_desktop_QuickstartWrapper_get_implementation(
     css::uno::XComponentContext *context,
     css::uno::Sequence<css::uno::Any> const &)
 {
-    return cppu::acquire(static_cast<cppu::OWeakObject *>(
-                Singleton::get(context).instance.get()));
+    return cppu::acquire(new ShutdownIcon(context));
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
