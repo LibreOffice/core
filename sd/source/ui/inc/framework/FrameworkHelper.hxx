a841b03b14776d6ebb5e3c72ab5b366fef04cc2e
pekka-dev <prashant.garg.1901@gmail.com>
2021-03-21 07:40:28 +0530

    tdf#124176 add pragma once in sd/ui/inc/framework\FrameworkHelper.hxx
    Change-Id: I8ab895a7b8e430d13e4db39a270af8d20949815c

--- sd/source/ui/inc/framework/FrameworkHelper.hxx
+++ sd/source/ui/inc/framework/FrameworkHelper.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_SD_SOURCE_UI_INC_FRAMEWORK_FRAMEWORKHELPER_HXX
-#define INCLUDED_SD_SOURCE_UI_INC_FRAMEWORK_FRAMEWORKHELPER_HXX
+#pragma once
 
 #include <ViewShell.hxx>
 
@@ -340,6 +339,5 @@ private:
 
 } // end of namespace sd::framework
 
-#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
