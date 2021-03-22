59b0dd6e94c876dd322503f326fedf5fa350d25e
Stephan Bergmann <sbergman@redhat.com>
2021-03-22 13:25:51 +0100

    Forgotten dp_misc::isPlatformSupported support for PLATFORMID=macosx_aarch64
    Change-Id: Ie3dfa624e3e4b8bad0c845175c2bff30feadbc23
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/112890
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

--- desktop/source/deployment/misc/dp_platform.cxx
+++ desktop/source/deployment/misc/dp_platform.cxx
@@ -85,6 +85,8 @@ namespace
             ret = checkOSandCPU(u"Solaris", u"x86");
         else if (token == u"aix_powerpc")
             ret = checkOSandCPU(u"AIX", u"PowerPC");
+        else if (token == u"macosx_aarch64")
+            ret = checkOSandCPU(u"MacOSX", u"AARCH64");
         else if (token == u"macosx_x86_64")
             ret = checkOSandCPU(u"MacOSX", u"X86_64");
         else if (token == u"linux_x86")
