8047e61b3d41a66124dc2865bc223837833be1ff
Tor Lillqvist <tml@collabora.com>
2021-03-24 18:58:58 +0200

    tdf#124173: Enable thesauruses in the iOS app
    Build our lnth library and the external mythes library. Install
    thesauruses for the app's Xcode project to pick up and include in the
    app bundle. Look for them in the place where they will end up.
    
    To get thesauruses you need to configure with --with-myspell-dicts.
    
    Change-Id: I2d850ca3c821c5c764cb061340a265440d04e41b
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/113066
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Tor Lillqvist <tml@collabora.com>

--- postprocess/Rdb_services.mk
+++ postprocess/Rdb_services.mk
@@ -94,9 +94,7 @@ $(eval $(call gb_Rdb_add_components,services,\
 	lingucomponent/source/hyphenator/hyphen/hyphen \
 	lingucomponent/source/languageguessing/guesslang \
 	lingucomponent/source/spellcheck/spell/spell \
-	$(if $(filter-out iOS,$(OS)), \
-		lingucomponent/source/thesaurus/libnth/lnth \
-	) \
+	lingucomponent/source/thesaurus/libnth/lnth \
 	lingucomponent/source/numbertext/numbertext \
 	linguistic/source/lng \
 	$(if $(ENABLE_LWP), \
