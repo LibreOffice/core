73282582fbe0e4d7244887b3ac57437bd60444b0
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-03-24 14:32:18 +0530

    tdf#60684 Add unit test
    Change-Id: I9fc633a9a7c977d869297237cdd8547ca0cd9d47

--- svx/qa/unit/xoutdev.cxx
+++ svx/qa/unit/xoutdev.cxx
@@ -68,4 +68,32 @@ CPPUNIT_TEST_FIXTURE(XOutdevTest, testPdfGraphicExport)
     CPPUNIT_ASSERT_EQUAL(static_cast<sal_uInt8>('-'), sFirstBytes[4]);
 }
 
+CPPUNIT_TEST_FIXTURE(XOutdevTest, testTdf60684)
+{
+    Graphic aGraphic;
+    test::Directories aDirectories;
+    OUString aURL = aDirectories.getURLFromSrc(u"svx/qa/unit/data/tdf60684.jpg");
+    SvFileStream aStream(aURL, StreamMode::READ);
+    CPPUNIT_ASSERT_EQUAL(ERRCODE_NONE,
+                         GraphicFilter::GetGraphicFilter().ImportGraphic(aGraphic, aURL, aStream));
+
+    // Export it.
+    utl::TempFile aTempFile;
+    aTempFile.EnableKillingFile();
+    XOutFlags const eFlags = XOutFlags::DontExpandFilename | XOutFlags::DontAddExtension
+                             | XOutFlags::UseNativeIfPossible;
+    OUString aTempURL = aTempFile.GetURL();
+    XOutBitmap::WriteGraphic(aGraphic, aTempURL, "png", eFlags);
+
+    SvStream* pStream = aTempFile.GetStream(StreamMode::READ);
+    CPPUNIT_ASSERT(pStream->TellEnd() > 4);
+    sal_uInt8 sFirstBytes[4];
+    pStream->ReadBytes(sFirstBytes, 4);
+
+    //Checks if the file's header matches a PNG's expected header
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_uInt8>('P'), sFirstBytes[1]);
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_uInt8>('N'), sFirstBytes[2]);
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_uInt8>('G'), sFirstBytes[3]);
+}
+
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
