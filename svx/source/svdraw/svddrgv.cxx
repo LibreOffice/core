40bfa5c961edb2784d0bf4db1a581096e0533cfe
merttumer <mert.tumer@collabora.com>
2021-03-24 12:06:08 +0300

    LOK: Fix Moving drag handles calculates wrong offset
    When dragging the shape handles for resizing,
    the handle's grid offset must be the shapes grid
    offset. In small numbered row&column orders, it is
    not visible much since the difference is very little
    but as the number gets greater, the difference becomes more visible.
    
    Change-Id: I44d03cc67a239c8b7758206930def331ed8e5e42
    Signed-off-by: merttumer <mert.tumer@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/113028
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>

--- svx/source/svdraw/svddrgv.cxx
+++ svx/source/svdraw/svddrgv.cxx
@@ -36,6 +36,7 @@
 #include <svx/sdr/overlay/overlaymanager.hxx>
 #include <svx/sdrpagewindow.hxx>
 #include <osl/diagnose.h>
+#include <comphelper/lok.hxx>
 
 using namespace sdr;
 
@@ -206,10 +207,12 @@ bool SdrDragView::BegDragObj(const Point& rPnt, OutputDevice* pOut, SdrHdl* pHdl
 
         // Coordinate maybe affected by GridOffset, so we may need to
         // adapt to Model-coordinates here
-        if(getPossibleGridOffsetForPosition(
+        if((comphelper::LibreOfficeKit::isActive() && mpMarkedObj
+            && getPossibleGridOffsetForSdrObject(aGridOffset, GetMarkedObjectByIndex(0), GetSdrPageView()))
+            || (getPossibleGridOffsetForPosition(
             aGridOffset,
             basegfx::B2DPoint(aPnt.X(), aPnt.Y()),
-            GetSdrPageView()))
+            GetSdrPageView())))
         {
             aPnt.AdjustX(basegfx::fround(-aGridOffset.getX()));
             aPnt.AdjustY(basegfx::fround(-aGridOffset.getY()));
@@ -508,10 +511,12 @@ void SdrDragView::MovDragObj(const Point& rPnt)
 
     // Coordinate maybe affected by GridOffset, so we may need to
     // adapt to Model-coordinates here
-    if(getPossibleGridOffsetForPosition(
+    if((comphelper::LibreOfficeKit::isActive() && mpMarkedObj
+        && getPossibleGridOffsetForSdrObject(aGridOffset, GetMarkedObjectByIndex(0), GetSdrPageView()))
+        || (getPossibleGridOffsetForPosition(
         aGridOffset,
         basegfx::B2DPoint(aPnt.X(), aPnt.Y()),
-        GetSdrPageView()))
+        GetSdrPageView())))
     {
         aPnt.AdjustX(basegfx::fround(-aGridOffset.getX()));
         aPnt.AdjustY(basegfx::fround(-aGridOffset.getY()));
