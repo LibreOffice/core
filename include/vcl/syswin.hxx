3e501c5b4bf334f70b7a0b167387d83cdf8d03db
anirudh4583 <anirudhmitra210@gmail.com>
2021-03-27 02:07:12 +0530

    tdf#124176 Use pragma once
    Change-Id: I58e9a50587d2f815ca09939a650debaa9a0a4969

--- include/vcl/syswin.hxx
+++ include/vcl/syswin.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_VCL_SYSWIN_HXX
-#define INCLUDED_VCL_SYSWIN_HXX
+#pragma once
 
 #include <vcl/dllapi.h>
 #include <vcl/idle.hxx>
@@ -253,6 +252,5 @@ inline void SystemWindow::SetIdleDebugName( const char *pDebugName )
     maLayoutIdle.SetDebugName( pDebugName );
 }
 
-#endif // INCLUDED_VCL_SYSWIN_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
