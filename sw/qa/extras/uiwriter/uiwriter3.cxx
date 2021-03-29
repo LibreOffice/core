b48bf9384c40fc61078db9242a467afb07b93e6e
Xisco Fauli <xiscofauli@libreoffice.org>
2021-03-29 15:49:19 +0200

    sw_uiwriter: unify data folder
    Change-Id: I2506c9eb077ca8caa36010a06056b23f83b444a4

--- sw/qa/extras/uiwriter/uiwriter3.cxx
+++ sw/qa/extras/uiwriter/uiwriter3.cxx
@@ -41,7 +41,7 @@
 
 namespace
 {
-constexpr OUStringLiteral DATA_DIRECTORY = u"/sw/qa/extras/uiwriter/data3/";
+constexpr OUStringLiteral DATA_DIRECTORY = u"/sw/qa/extras/uiwriter/data/";
 } // namespace
 
 /// Third set of tests asserting the behavior of Writer user interface shells.
