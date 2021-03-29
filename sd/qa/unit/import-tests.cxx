LibreOffice/core

commit 92f74f6ccb5a55807724db85815f7ea0c49370e0

bnc#887230: always use theme color for hyperlinks in Impress
Change-Id: I888f107c61037162439ad2d1ba99ad8185532f71

oox/source/drawingml/textrun.cxx
@@ -144,8 +144,7 @@ sal_Int32 TextRun::insertAt(
 
                 xTextFieldCursor->gotoEnd( sal_True );
 
-                if ( !maTextCharacterProperties.maCharColor.isUsed() )
-                    aTextCharacterProps.maCharColor.setSchemeClr( XML_hlink );
+                aTextCharacterProps.maCharColor.setSchemeClr( XML_hlink );
                 if ( !maTextCharacterProperties.moUnderline.has() )
                     aTextCharacterProps.moUnderline.set( XML_sng );
 

commit ad7388f546556cfd4dd9eac6af00fb79874bafe8

tdf#137367 PPTX import: fix lost direct hyperlink colors
Regression from commit 92f74f6ccb5a55807724db85815f7ea0c49370e0
(bnc#887230: always use theme color for hyperlinks in Impress)

Testing: direct color of the first text line is theme based
(a:rPr/a:solidFill/a:schemeClr), the second line contains
a direct color (a:rPr/a:solidFill/a:srgbClr), the third one
contains a theme based direct color darkened by 25.000%
(val=75000 of a:rPr/a:solidFill/a:schemeClr/a:lumMod).

Note: overwriting default theme based hyperlink colors is
also supported by Google Docs, Office 365 and MS Office 2019.

Change-Id: I1e3e78fac729a97f42c8dddcffd877a16383adbf

oox/source/drawingml/hyperlinkcontext.cxx
@@ -150,7 +150,8 @@ ContextHandlerRef HyperLinkContext::onCreateContext(
     switch( aElement )
     {
     case A_TOKEN( extLst ):
-        return nullptr;
+        maProperties.setProperty(PROP_CharColor, XML_fillcolor);
+        break;
     case A_TOKEN( snd ):
         // TODO use getEmbeddedWAVAudioFile() here
         break;

oox/source/drawingml/textrun.cxx
@@ -148,7 +148,9 @@ sal_Int32 TextRun::insertAt(
 
                 xTextFieldCursor->gotoEnd( true );
 
-                aTextCharacterProps.maFillProperties.maFillColor.setSchemeClr( XML_hlink );
+                if (!maTextCharacterProperties.maHyperlinkPropertyMap.hasProperty(PROP_CharColor))
+                    aTextCharacterProps.maFillProperties.maFillColor.setSchemeClr(XML_hlink);
+
                 aTextCharacterProps.maFillProperties.moFillType.set(XML_solidFill);
                 if ( !maTextCharacterProperties.moUnderline.has() )
                     aTextCharacterProps.moUnderline.set( XML_sng );

sd/qa/unit/import-tests.cxx
@@ -118,6 +118,7 @@ public:
     virtual void setUp() override;
 
     void testDocumentLayout();
+    void testHyperlinkColor();
     void testSmoketest();
     void testN759180();
     void testN778859();
@@ -235,6 +236,7 @@ public:
     CPPUNIT_TEST_SUITE(SdImportTest);
 
     CPPUNIT_TEST(testDocumentLayout);
+    CPPUNIT_TEST(testHyperlinkColor);
     CPPUNIT_TEST(testSmoketest);
     CPPUNIT_TEST(testN759180);
     CPPUNIT_TEST(testN778859);
@@ -426,6 +428,45 @@ void SdImportTest::testDocumentLayout()
                 OUString(m_directories.getPathFromSrc( u"/sd/qa/unit/data/" ) + OUString::createFromAscii( aFilesToCompare[i].pDump )),
                 i == nUpdateMe );
     }
+}
+
+void SdImportTest::testHyperlinkColor()
+{
+    ::sd::DrawDocShellRef xDocShRef = loadURL(m_directories.getURLFromSrc(u"/sd/qa/unit/data/pptx/tdf137367.pptx"), PPTX);
+
+    uno::Reference< beans::XPropertySet > xShape( getShapeFromPage( 0, 0, xDocShRef ) );
+
+    // Get first paragraph of the text
+    uno::Reference<text::XTextRange> const xParagraph1( getParagraphFromShape( 0, xShape ) );
+    // Get second paragraph of the text
+    uno::Reference<text::XTextRange> const xParagraph2( getParagraphFromShape( 1, xShape ) );
+    // Get third paragraph of the text
+    uno::Reference<text::XTextRange> const xParagraph3( getParagraphFromShape( 2, xShape ) );
+
+    // Get run of the first paragraph
+    uno::Reference<text::XTextRange> xRun1( getRunFromParagraph (0, xParagraph1 ) );
+    uno::Reference< beans::XPropertySet > xPropSet1( xRun1, uno::UNO_QUERY_THROW );
+    sal_Int32 nCharColorBlue;
+    xPropSet1->getPropertyValue( "CharColor" ) >>= nCharColorBlue;
+
+    // Get run of the second paragraph
+    uno::Reference<text::XTextRange> xRun2( getRunFromParagraph (1, xParagraph2 ) );
+    uno::Reference< beans::XPropertySet > xPropSet2( xRun2, uno::UNO_QUERY_THROW );
+    sal_Int32 nCharColorRed;
+    xPropSet2->getPropertyValue( "CharColor" ) >>= nCharColorRed;
+
+    // Get run of the third paragraph
+    uno::Reference<text::XTextRange> xRun3( getRunFromParagraph (2, xParagraph3 ) );
+    uno::Reference< beans::XPropertySet > xPropSet3( xRun3, uno::UNO_QUERY_THROW );
+    sal_Int32 nCharColorGreen;
+    xPropSet3->getPropertyValue( "CharColor" ) >>= nCharColorGreen;
+
+    // Hyperlink colors should be blue, red, green.
+    CPPUNIT_ASSERT_EQUAL( sal_Int32(4485828), nCharColorBlue );
+    CPPUNIT_ASSERT_EQUAL( sal_Int32(16711680), nCharColorRed );
+    CPPUNIT_ASSERT_EQUAL( sal_Int32(5538357), nCharColorGreen );
+
+    xDocShRef->DoClose();
 }
 
 void SdImportTest::testSmoketest()