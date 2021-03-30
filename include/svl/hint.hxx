6b3f0866e9eddd5553857741862ca0d7d5b2ec2b
Eike Rathke <erack@redhat.com>
2021-03-31 00:00:31 +0200

    Related: tdf#137577 Update Name Box range name list on sheet rename
    ... for sheet-local names that include the sheet name.
    
    Introduce SfxHintId::ScTablesRenamed for this as the existing
    ScTablesChanged is fired quite too often.
    
    Change-Id: I94c3479bba4d4cc8b75905e3fa4f35d9a8737870
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/113391
    Reviewed-by: Eike Rathke <erack@redhat.com>
    Tested-by: Jenkins

--- include/svl/hint.hxx
+++ include/svl/hint.hxx
@@ -73,6 +73,7 @@ enum class SfxHintId {
     ScDbAreasChanged,
     ScAreasChanged,
     ScTablesChanged,
+    ScTablesRenamed,
     ScDrawChanged,
     ScDocNameChanged,
     ScAreaLinksChanged,
@@ -164,6 +165,7 @@ inline std::basic_ostream<charT, traits> & operator <<(
     case SfxHintId::ScDbAreasChanged: return stream << "ScDbAreasChanged";
     case SfxHintId::ScAreasChanged: return stream << "ScAreasChanged";
     case SfxHintId::ScTablesChanged: return stream << "ScTablesChanged";
+    case SfxHintId::ScTablesRenamed: return stream << "ScTablesRenamed";
     case SfxHintId::ScDrawChanged: return stream << "ScDrawChanged";
     case SfxHintId::ScDocNameChanged: return stream << "ScDocNameChanged";
     case SfxHintId::ScAreaLinksChanged: return stream << "ScAreaLinksChanged";
