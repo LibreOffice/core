85c34e71e169241cd01c7db943917c7795d71e06
Bartosz Kosiorek <gang65@poczta.onet.pl>
2021-04-01 16:42:34 +0200

    tdf#55058 EMF: Implement PAINTRGN record
    After implement support for PAINTRGN record, the reference image
    is displayed correctly:
    https://sourceforge.net/projects/libuemf/
    
    Change-Id: I761779713d1200e6079ff798e9c3c9aaba57ad4f

--- emfio/source/reader/emfreader.cxx
+++ emfio/source/reader/emfreader.cxx
@@ -1934,6 +1934,17 @@ namespace emfio
                     }
                     break;
 
+                    case EMR_PAINTRGN :
+                    {
+                        sal_uInt32 nRgnDataSize;
+                        tools::PolyPolygon aPolyPoly;
+                        mpInputStream->SeekRel( 0x10 );  // RectL bounds
+                        mpInputStream->ReadUInt32( nRgnDataSize );
+
+                        if ( ImplReadRegion( aPolyPoly, *mpInputStream, nRecSize ) )
+                            DrawPolyPolygon( aPolyPoly );
+                    }
+
                     case EMR_CREATEDIBPATTERNBRUSHPT :
                     {
                         sal_uInt32  nStart = mpInputStream->Tell() - 8;
@@ -1987,7 +1998,6 @@ namespace emfio
                     case EMR_SETDIBITSTODEVICE :
                     case EMR_FRAMERGN :
                     case EMR_INVERTRGN :
-                    case EMR_PAINTRGN :
                     case EMR_FLATTENPATH :
                     case EMR_WIDENPATH :
                     case EMR_POLYDRAW :
