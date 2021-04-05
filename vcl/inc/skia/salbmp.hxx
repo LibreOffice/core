ad736c7fff070d0e5507e31e3e7237e1121cc5c9
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2021-04-05 22:34:32 +0900

    vcl: use PixelFormat enum in SalBitmap interface and backends
    Change-Id: I8d2b6adfcb8fe3dd78010538411f338c9a1c3996

--- vcl/inc/skia/salbmp.hxx
+++ vcl/inc/skia/salbmp.hxx
@@ -36,11 +36,11 @@ public:
     virtual ~SkiaSalBitmap() override;
 
     // SalBitmap methods
-    virtual bool Create(const Size& rSize, sal_uInt16 nBitCount,
+    virtual bool Create(const Size& rSize, vcl::PixelFormat ePixelFormat,
                         const BitmapPalette& rPal) override;
     virtual bool Create(const SalBitmap& rSalBmp) override;
     virtual bool Create(const SalBitmap& rSalBmp, SalGraphics* pGraphics) override;
-    virtual bool Create(const SalBitmap& rSalBmp, sal_uInt16 nNewBitCount) override;
+    virtual bool Create(const SalBitmap& rSalBmp, vcl::PixelFormat eNewPixelFormat) override;
     virtual bool Create(const css::uno::Reference<css::rendering::XBitmapCanvas>& rBitmapCanvas,
                         Size& rSize, bool bMask = false) override;
 
