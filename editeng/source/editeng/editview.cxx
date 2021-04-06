97e830bd7c92aff126595409e1e4a9e676ab5db8
Henry Castro <hcastro@collabora.com>
2021-04-06 10:41:40 -0400

    lok: remove popup auto correct item
    The auto correct options and auto correct
    menu item are no longer need it.
    
    Change-Id: I0a5e624c3393151cf80a06961f96d6a5fb8891ab
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/113680
    Tested-by: Jenkins
    Reviewed-by: Henry Castro <hcastro@collabora.com>

--- editeng/source/editeng/editview.cxx
+++ editeng/source/editeng/editview.cxx
@@ -1093,6 +1093,9 @@ void EditView::ExecuteSpellPopup( const Point& rPosPixel, Link<SpellCallbackInfo
 
         if (comphelper::LibreOfficeKit::isActive())
         {
+            aPopupMenu->RemoveItem(nAutoCorrId);
+            aPopupMenu->RemoveItem(nAutoCorrectDlgId);
+
             // For mobile phones, send the context menu structure
             const SfxViewShell* pViewShell = SfxViewShell::Current();
             if (pViewShell && pViewShell->isLOKMobilePhone())
