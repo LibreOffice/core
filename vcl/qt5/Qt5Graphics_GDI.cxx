LibreOffice/core

commit c181e510c5f5e74f1f6824b64637849aace9ae63

convert internal bitmap formats transparency->alpha
Image formats and graphics APIs use alpha, not transparency,
so change our internal formats and data structures to work directly
with alpha, so we don't need to modify data before we push it to
graphics APIs.

Change-Id: I537f77f79e83876be11d787901df42710d190842
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/108919
Tested-by: Jenkins
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
Reviewed-by: Luboš Luňák <l.lunak@collabora.com>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/qt5/Qt5Graphics_GDI.cxx
@@ -592,7 +592,7 @@ static bool getAlphaImage(const SalBitmap& rSourceBitmap, const SalBitmap& rAlph
             uchar* image_line = rAlphaImage.scanLine(y);
             const uchar* alpha_line = pAlpha->scanLine(y);
             for (int x = 0; x < rAlphaImage.width(); ++x, image_line += 4)
-                image_line[3] = 255 - alpha_line[x];
+                image_line[3] = alpha_line[x];
         }
     }
     else

canvas/source/directx/dx_canvasbitmap.cxx
@@ -218,7 +218,7 @@ namespace dxcanvas
                         sal_uInt8* pOutBits=pAlphaBits.get()+y*nScanWidth;
                         for( sal_Int32 x=0; x<aSize.getX(); ++x )
                         {
-                            *pOutBits++ = 255-*pInBits;
+                            *pOutBits++ = *pInBits;
                             pInBits += 4;
                         }
                     }

canvas/source/directx/dx_vcltools.cxx
@@ -215,11 +215,7 @@ namespace dxcanvas::tools
                                     *pCurrOutput++ = aCol.GetBlue();
                                     *pCurrOutput++ = aCol.GetGreen();
                                     *pCurrOutput++ = aCol.GetRed();
-
-                                    // out notion of alpha is
-                                    // different from the rest
-                                    // of the world's
-                                    *pCurrOutput++ = 255 - static_cast<BYTE>(*pAScan++);
+                                    *pCurrOutput++ = static_cast<BYTE>(*pAScan++);
                                 }
                             }
                             break;
@@ -235,11 +231,7 @@ namespace dxcanvas::tools
                                     *pCurrOutput++ = *pScan++;
                                     *pCurrOutput++ = *pScan++;
                                     *pCurrOutput++ = *pScan++;
-
-                                    // out notion of alpha is
-                                    // different from the rest
-                                    // of the world's
-                                    *pCurrOutput++ = 255 - static_cast<BYTE>(*pAScan++);
+                                    *pCurrOutput++ = static_cast<BYTE>(*pAScan++);
                                 }
                             }
                             break;
@@ -263,11 +255,7 @@ namespace dxcanvas::tools
                                     *pCurrOutput++ = aCol.GetBlue();
                                     *pCurrOutput++ = aCol.GetGreen();
                                     *pCurrOutput++ = aCol.GetRed();
-
-                                    // out notion of alpha is
-                                    // different from the rest
-                                    // of the world's
-                                    *pCurrOutput++ = 255 - static_cast<BYTE>(*pAScan++);
+                                    *pCurrOutput++ = static_cast<BYTE>(*pAScan++);
                                 }
                             }
                             break;

canvas/source/vcl/canvasbitmaphelper.cxx
@@ -148,7 +148,7 @@ namespace vclcanvas
         pRes[ 0 ] = aColor.GetRed();
         pRes[ 1 ] = aColor.GetGreen();
         pRes[ 2 ] = aColor.GetBlue();
-        pRes[ 3 ] = 255 - aColor.GetAlpha();
+        pRes[ 3 ] = aColor.GetAlpha();
 
         return aRes;
     }

vcl/inc/bitmap/ScanlineTools.hxx
@@ -46,7 +46,7 @@ public:
 
     virtual void writePixel(Color nColor) override
     {
-        *pData++ = 255 - nColor.GetAlpha();
+        *pData++ = nColor.GetAlpha();
         *pData++ = nColor.GetRed();
         *pData++ = nColor.GetGreen();
         *pData++ = nColor.GetBlue();

vcl/qa/cppunit/ScanlineToolsTest.cxx
@@ -42,9 +42,9 @@ void ScanlineToolsTest::ScanlineTransformer_32_ARGB()
     pScanlineTransformer->startLine(aScanLine.data());
 
     std::vector<Color> aColors{
-        Color(ColorTransparency, 0, 10, 250, 120),   Color(ColorTransparency, 50, 30, 230, 110),
-        Color(ColorTransparency, 100, 50, 210, 100), Color(ColorTransparency, 150, 70, 190, 90),
-        Color(ColorTransparency, 200, 90, 170, 80),
+        Color(ColorAlpha, 255, 10, 250, 120), Color(ColorAlpha, 205, 30, 230, 110),
+        Color(ColorAlpha, 155, 50, 210, 100), Color(ColorAlpha, 105, 70, 190, 90),
+        Color(ColorAlpha, 55, 90, 170, 80),
     };
 
     for (Color const& aColor : aColors)
@@ -52,8 +52,8 @@ void ScanlineToolsTest::ScanlineTransformer_32_ARGB()
         pScanlineTransformer->writePixel(aColor);
     }
 
-    std::vector<sal_uInt8> aExpectedBytes{ 0,   10,  250, 120, 50,  30, 230, 110, 100, 50,
-                                           210, 100, 150, 70,  190, 90, 200, 90,  170, 80 };
+    std::vector<sal_uInt8> aExpectedBytes{ 255, 10,  250, 120, 205, 30, 230, 110, 155, 50,
+                                           210, 100, 105, 70,  190, 90, 55,  90,  170, 80 };
 
     for (size_t i = 0; i < aScanLine.size(); ++i)
     {

vcl/skia/salbmp.cxx
@@ -1243,7 +1243,7 @@ OString SkiaSalBitmap::GetAlphaImageKey() const
     {
         std::stringstream ss;
         ss << std::hex << std::setfill('0') << std::setw(2)
-           << static_cast<int>(255 - SkColorGetA(fromEraseColorToAlphaImageColor(mEraseColor)));
+           << static_cast<int>(SkColorGetA(fromEraseColorToAlphaImageColor(mEraseColor)));
         return OStringLiteral("E") + ss.str().c_str();
     }
     return OStringLiteral("I") + OString::number(GetAlphaSkImage()->uniqueID());

vcl/source/bitmap/BitmapEx.cxx
@@ -1588,7 +1588,7 @@ void BitmapEx::AdjustTransparency(sal_uInt8 cTrans)
                 for( tools::Long nX = 0; nX < nWidth; nX++ )
                 {
                     nNewTrans = nTrans + *pAScan;
-                    *pAScan++ = static_cast<sal_uInt8>( ( nNewTrans & 0xffffff00 ) ? 255 : nNewTrans );
+                    *pAScan++ = static_cast<sal_uInt8>( ( nNewTrans & 0xffffff00 ) ? 0 : (255 - nNewTrans) );
                 }
             }
         }

commit f4cf17c4d35069af1e257f82fe619a82629338da

tdf#141504 qt5: ugly images
regression from
    commit c181e510c5f5e74f1f6824b64637849aace9ae63
    Date:   Thu Jan 7 09:46:07 2021 +0200
    convert internal bitmap formats transparency->alpha

Change-Id: I858ba2c986bdac6f7e26145efde253485e8b2281

vcl/qt5/Qt5Graphics_GDI.cxx
@@ -583,7 +583,7 @@ static bool getAlphaImage(const SalBitmap& rSourceBitmap, const SalBitmap& rAlph
             uchar* image_line = rAlphaImage.scanLine(y);
             const uchar* alpha_line = pAlpha->scanLine(y);
             for (int x = 0; x < rAlphaImage.width(); ++x, image_line += 4)
-                image_line[3] = alpha_line[x];
+                image_line[3] = 255 - alpha_line[x];
         }
     }
     else