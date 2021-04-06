18d138ab756492d1f1b86726c21b493de2c4bebe
Szymon Kłos <szymon.klos@collabora.com>
2021-04-06 07:08:30 +0200

    notebookbar: minify for online
    Notebookbar load takes some time. We don't use most of
    the items in online so minify .ui file to contain
    only needed widgets.
    
    Change-Id: I4796caae14bb63e3e04d318093209adfb87a77df
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/113623
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Jan Holesovsky <kendy@collabora.com>

--- sd/UIConfig_simpress.mk
+++ sd/UIConfig_simpress.mk
@@ -144,6 +144,7 @@ $(eval $(call gb_UIConfig_add_uifiles,modules/simpress,\
 	sd/uiconfig/simpress/ui/notebookbar_groups \
 	sd/uiconfig/simpress/ui/notebookbar_groupedbar_full \
 	sd/uiconfig/simpress/ui/notebookbar_groupedbar_compact \
+	sd/uiconfig/simpress/ui/notebookbar_online \
 	sd/uiconfig/simpress/ui/optimpressgeneralpage \
 	sd/uiconfig/simpress/ui/pagesfieldbox \
 	sd/uiconfig/simpress/ui/photoalbum \
