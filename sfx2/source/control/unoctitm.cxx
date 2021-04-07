b0b5fd970ce8acc4c0953b750adb15dbd464f80f
Gabriel Masei <gabriel.masei@1and1.ro>
2021-04-07 12:46:27 +0300

    lok: intercept indentation and decimals state change events
    This solves some toolbar buttons enabling issues in online
    when dynamically changing UI mode to classic.
    
    Change-Id: I3301b92c35effce905f1283ae645d8ad2b168ece
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/113723
    Tested-by: Jenkins
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>
    (cherry picked from commit 8859e62d7dfaa71c35f80b5eff99b0788b49c9fa)

--- sfx2/source/control/unoctitm.cxx
+++ sfx2/source/control/unoctitm.cxx
@@ -1169,7 +1169,11 @@ static void InterceptLOKStateChangeEvent(sal_uInt16 nSID, SfxViewFrame* pViewFra
              aEvent.FeatureURL.Path == "Substract" ||
              aEvent.FeatureURL.Path == "DistributeSelection" ||
              aEvent.FeatureURL.Path == "Intersect" ||
-             aEvent.FeatureURL.Path == "ResetAttributes")
+             aEvent.FeatureURL.Path == "ResetAttributes" ||
+             aEvent.FeatureURL.Path == "IncrementIndent" ||
+             aEvent.FeatureURL.Path == "DecrementIndent" ||
+             aEvent.FeatureURL.Path == "NumberFormatDecDecimals" ||
+             aEvent.FeatureURL.Path == "NumberFormatIncDecimals")
     {
         aBuffer.append(aEvent.IsEnabled ? OUStringLiteral("enabled") : OUStringLiteral("disabled"));
     }
