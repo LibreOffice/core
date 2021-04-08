c04be7a6c9d8302bbe8544cde1f2a7921d9fe849
Miklos Vajna <vmiklos@collabora.com>
2021-04-08 17:08:32 +0200

    xmloff: build-time test that XMLRtlGutterPropertyHandler.hxx is self-contained
    By making it the first include in XMLRtlGutterPropertyHandler.cxx.
    
    Change-Id: Id2e273ac60ad0ca9811b8581b32fc40776f0877e
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/113816
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Tested-by: Jenkins

--- xmloff/source/style/XMLRtlGutterPropertyHandler.cxx
+++ xmloff/source/style/XMLRtlGutterPropertyHandler.cxx
@@ -17,13 +17,13 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
+#include <XMLRtlGutterPropertyHandler.hxx>
+
 #include <set>
 
 #include <com/sun/star/uno/Any.hxx>
 
 #include <xmloff/xmluconv.hxx>
-
-#include <XMLRtlGutterPropertyHandler.hxx>
 
 using namespace com::sun::star;
 
