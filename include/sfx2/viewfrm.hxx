LibreOffice/core

commit afbbfb3b55beb937555a972d9edbb47ede91001a

tdf#83958: sw: enable Track Changes toolbar automatically
Switch on the disabled Tracked Changes toolbar if the document hasn't
modified, yet (i.e. on load), only if recording of changes is enabled
or the document contains tracked changes.

Note: the toolbar stays there, but there is no need for it to manage
changes only with the menus Edit/View, keyboard shortcuts or the
experimental sidebar, so it may worth to add a View option for this
behaviour.

Change-Id: Ia46b552b3669e147cdcbf0cc0be164883e9fb2cc
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/105519
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/source/uibase/uiview/viewstat.cxx
@@ -52,6 +52,7 @@
 
 #include <cmdid.h>
 #include <IDocumentRedlineAccess.hxx>
+#include <IDocumentState.hxx>
 
 #include <doc.hxx>
 
@@ -311,6 +312,17 @@ void SwView::GetState(SfxItemSet &rSet)
             break;
             case FN_REDLINE_ON:
                 rSet.Put( SfxBoolItem( nWhich, GetDocShell()->IsChangeRecording() ) );
+                // switch on the disabled Tracked Changes toolbar if the document hasn't
+                // modified, yet (i.e. on load), and if recording of changes is enabled
+                // or if it contains tracked changes. TODO: the toolbar stays there, but
+                // there is no need for it to manage changes only with the menus Edit/View,
+                // keyboard shortcuts or the experimental sidebar, so it's worth to add
+                // a View option for this behaviour.
+                if ( !GetDocShell()->IsModified() && ( GetDocShell()->IsChangeRecording() ||
+                    m_pWrtShell->GetDoc()->getIDocumentRedlineAccess().GetRedlineTable().size() ) )
+                {
+                    ShowUIElement("private:resource/toolbar/changes");
+                }
             break;
             case FN_REDLINE_PROTECT :
                 rSet.Put( SfxBoolItem( nWhich, GetDocShell()->HasChangeRecordProtection() ) );

commit fc7e417eed0745c76028e5d212f499978a412c26

tdf#141298 sw: show Track Changes infobar
instead of enabling Track Changes toolbar automatically,
when Track Changes toolbar is disabled, but the document
contains recorded changes or Record Track Changes is enabled.

Button of the Track Changes infobar allows to show/hide the
Track Changes toolbar. Hiding the Track Changes toolbar with
button of the Track Changes infobar closes the infobar, too.

Regression from commit afbbfb3b55beb937555a972d9edbb47ede91001a
"tdf#83958: sw: enable Track Changes toolbar automatically" and
commit 1989201c56c03b1ef13a282cfd09af69620040ea
"tdf#138870 sw: fix Track Changes toolbar reappearing".

Change-Id: I9162102d63d671b412fa0228e6bbfb5c356ee03e

include/sfx2/strings.hrc
@@ -292,6 +292,7 @@
 #define STR_SIGNATURE_NOTVALIDATED_PARTIAL_OK   NC_("STR_SIGNATURE_NOTVALIDATED_PARTIAL_OK", "The certificate could not be validated and the document is only partially signed.")
 #define STR_SIGNATURE_OK                        NC_("STR_SIGNATURE_OK", "This document is digitally signed and the signature is valid.")
 #define STR_SIGNATURE_SHOW                      NC_("STR_SIGNATURE_SHOW", "Show Signatures")
+#define STR_TRACK_CHANGES_BUTTON                NC_("STR_TRACK_CHANGES_BUTTON", "Show/Hide Track Changes Functions")
 
 #define STR_CLOSE_PANE                          NC_("STR_CLOSE_PANE", "Close Pane")
 

include/sfx2/viewfrm.hxx
@@ -64,6 +64,7 @@ protected:
     DECL_LINK(WhatsNewHandler, weld::Button&, void);
     DECL_LINK(SwitchReadOnlyHandler, weld::Button&, void);
     DECL_LINK(SignDocumentHandler, weld::Button&, void);
+    DECL_LINK(TrackChangesHandler, weld::Button&, void);
     SAL_DLLPRIVATE void KillDispatcher_Impl();
 
     virtual                 ~SfxViewFrame() override;

officecfg/registry/schema/org/openoffice/Office/UI/Infobar.xcs
@@ -54,6 +54,12 @@
         </info>
         <value>true</value>
       </prop>
+      <prop oor:name="TrackChanges" oor:type="xs:boolean" oor:nillable="false">
+        <info>
+          <desc>Whether an Infobar is shown when Track Changes settings or data are there in a document</desc>
+        </info>
+        <value>true</value>
+      </prop>
     </group>
   </component>
 </oor:component-schema>

sfx2/source/dialog/infobar.cxx
@@ -434,6 +434,8 @@ bool SfxInfoBarContainerWindow::isInfobarEnabled(std::u16string_view sId)
         return officecfg::Office::UI::Infobar::Enabled::HyphenationMissing::get();
     if (sId == u"whatsnew")
         return officecfg::Office::UI::Infobar::Enabled::WhatsNew::get();
+    if (sId == u"trackchanges")
+        return officecfg::Office::UI::Infobar::Enabled::TrackChanges::get();
 
     return true;
 }

sfx2/source/view/viewfrm.cxx
@@ -133,6 +133,8 @@ using ::com::sun::star::container::XIndexContainer;
 #define ShellClass_SfxViewFrame
 #include <sfxslots.hxx>
 
+#define CHANGES_STR "private:resource/toolbar/changes"
+
 SFX_IMPL_SUPERCLASS_INTERFACE(SfxViewFrame,SfxShell)
 
 void SfxViewFrame::InitInterface_Impl()
@@ -1280,6 +1282,28 @@ void SfxViewFrame::AppendReadOnlyInfobar()
     }
 }
 
+namespace
+{
+css::uno::Reference<css::frame::XLayoutManager> getLayoutManager(const SfxFrame& rFrame)
+{
+    css::uno::Reference<css::frame::XLayoutManager> xLayoutManager;
+    css::uno::Reference<css::beans::XPropertySet> xPropSet(rFrame.GetFrameInterface(),
+                                                 uno::UNO_QUERY);
+    if (xPropSet.is())
+    {
+        try
+        {
+            xLayoutManager.set(xPropSet->getPropertyValue("LayoutManager"), uno::UNO_QUERY);
+        }
+        catch (const Exception& e)
+        {
+            SAL_WARN("sfx.view", "Failure getting layout manager: " + e.Message);
+        }
+    }
+    return xLayoutManager;
+}
+}
+
 void SfxViewFrame::Notify( SfxBroadcaster& /*rBC*/, const SfxHint& rHint )
 {
     if(m_pImpl->bIsDowning)
@@ -1437,9 +1461,35 @@ void SfxViewFrame::Notify( SfxBroadcaster& /*rBC*/, const SfxHint& rHint )
                 while (!aPendingInfobars.empty())
                 {
                     InfobarData& aInfobarData = aPendingInfobars.back();
-                    AppendInfoBar(aInfobarData.msId, aInfobarData.msPrimaryMessage,
+                    bool bSkipTrackChanges = false;
+
+                    if (aInfobarData.msId == "trackchanges")
+                    {
+                        // only hidden, if Track Changes toolbar is not visible
+                        if (auto xLayoutManager = getLayoutManager(GetFrame()))
+                        {
+                            if ( xLayoutManager->getElement(CHANGES_STR).is() )
+                                bSkipTrackChanges = true;
+                        }
+                    }
+
+                    if ( !bSkipTrackChanges )
+                    {
+                        VclPtr<SfxInfoBarWindow> pInfoBar =
+                            AppendInfoBar(aInfobarData.msId, aInfobarData.msPrimaryMessage,
                                   aInfobarData.msSecondaryMessage, aInfobarData.maInfobarType,
                                   aInfobarData.mbShowCloseButton);
+
+                        // add a button to show/hide Track Changes functions
+                        if ( pInfoBar && aInfobarData.msId == "trackchanges" )
+                        {
+                            weld::Button& rWhatsNewButton = pInfoBar->addButton();
+                            rWhatsNewButton.set_label(SfxResId(STR_TRACK_CHANGES_BUTTON));
+                            rWhatsNewButton.connect_clicked(LINK(this,
+                                                    SfxViewFrame, TrackChangesHandler));
+                        }
+                    }
+
                     aPendingInfobars.pop_back();
                 }
 
@@ -1557,6 +1607,26 @@ IMPL_LINK(SfxViewFrame, SwitchReadOnlyHandler, weld::Button&, rButton, void)
 IMPL_LINK_NOARG(SfxViewFrame, SignDocumentHandler, weld::Button&, void)
 {
     GetDispatcher()->Execute(SID_SIGNATURE);
+}
+
+IMPL_LINK_NOARG(SfxViewFrame, TrackChangesHandler, weld::Button&, void)
+{
+    // enable Track Changes toolbar, if it is disabled.
+    // Otherwise disable the toolbar, and close the infobar
+    if (auto xLayoutManager = getLayoutManager(GetFrame()))
+    {
+        if (!xLayoutManager->getElement(CHANGES_STR).is())
+        {
+            xLayoutManager->createElement(CHANGES_STR);
+            xLayoutManager->showElement(CHANGES_STR);
+        }
+        else
+        {
+            xLayoutManager->hideElement(CHANGES_STR);
+            xLayoutManager->destroyElement(CHANGES_STR);
+            RemoveInfoBar(u"trackchanges");
+        }
+    }
 }
 
 void SfxViewFrame::Construct_Impl( SfxObjectShell *pObjSh )
@@ -2871,24 +2941,7 @@ void SfxViewFrame::MiscExec_Impl( SfxRequest& rReq )
 
         case SID_TOGGLESTATUSBAR:
         {
-            css::uno::Reference< css::frame::XFrame > xFrame =
-                    GetFrame().GetFrameInterface();
-
-            Reference< css::beans::XPropertySet > xPropSet( xFrame, UNO_QUERY );
-            Reference< css::frame::XLayoutManager > xLayoutManager;
-            if ( xPropSet.is() )
-            {
-                try
-                {
-                    Any aValue = xPropSet->getPropertyValue("LayoutManager");
-                    aValue >>= xLayoutManager;
-                }
-                catch ( Exception& )
-                {
-                }
-            }
-
-            if ( xLayoutManager.is() )
+            if ( auto xLayoutManager = getLayoutManager(GetFrame()) )
             {
                 static const OUStringLiteral aStatusbarResString( u"private:resource/statusbar/statusbar" );
                 // Evaluate parameter.
@@ -2924,23 +2977,7 @@ void SfxViewFrame::MiscExec_Impl( SfxRequest& rReq )
                 WorkWindow* pWork = static_cast<WorkWindow*>( pTop->GetFrame().GetTopWindow_Impl() );
                 if ( pWork )
                 {
-                    css::uno::Reference< css::frame::XFrame > xFrame =
-                            GetFrame().GetFrameInterface();
-
-                    Reference< css::beans::XPropertySet > xPropSet( xFrame, UNO_QUERY );
-                    Reference< css::frame::XLayoutManager > xLayoutManager;
-                    if ( xPropSet.is() )
-                    {
-                        try
-                        {
-                            Any aValue = xPropSet->getPropertyValue("LayoutManager");
-                            aValue >>= xLayoutManager;
-                        }
-                        catch ( Exception& )
-                        {
-                        }
-                    }
-
+                    Reference< css::frame::XLayoutManager > xLayoutManager = getLayoutManager(GetFrame());
                     bool bNewFullScreenMode = pItem ? pItem->GetValue() : !pWork->IsFullScreenMode();
                     if ( bNewFullScreenMode != pWork->IsFullScreenMode() )
                     {

sw/inc/strings.hrc
@@ -404,6 +404,10 @@
 #define STR_HYPH_TITLE                          NC_("STR_HYPH_TITLE", "Hyphenation")
 #define STR_HYPH_MISSING                        NC_("STR_HYPH_MISSING", "Missing hyphenation data")
 #define STR_HYPH_MISSING_DETAIL                 NC_("STR_HYPH_MISSING", "Please install the hyphenation package for locale “%1”.")
+#define STR_HIDDEN_CHANGES                      NC_("STR_HIDDEN_CHANGES", "Track Changes")
+#define STR_HIDDEN_CHANGES_DETAIL               NC_("STR_HIDDEN_CHANGES_DETAIL", "Recorded changes in document, and Record Track Changes is enabled.")
+#define STR_HIDDEN_CHANGES_DETAIL2              NC_("STR_HIDDEN_CHANGES_DETAIL2", "Record Track Changes is enabled.")
+#define STR_HIDDEN_CHANGES_DETAIL3              NC_("STR_HIDDEN_CHANGES_DETAIL3", "Recorded changes in document.")
 
 // Undo
 #define STR_CANT_UNDO                           NC_("STR_CANT_UNDO", "not possible")

sw/source/uibase/uiview/viewstat.cxx
@@ -49,6 +49,9 @@
 #include <redline.hxx>
 #include <rootfrm.hxx>
 #include <docary.hxx>
+#include <sfx2/infobar.hxx>
+#include <docsh.hxx>
+#include <strings.hrc>
 
 #include <cmdid.h>
 #include <IDocumentRedlineAccess.hxx>
@@ -324,13 +327,26 @@ void SwView::GetState(SfxItemSet &rSet)
             break;
             case FN_REDLINE_ON:
                 rSet.Put( SfxBoolItem( nWhich, GetDocShell()->IsChangeRecording() ) );
-                // switch on the disabled Tracked Changes toolbar if the view is
-                // new (e.g. on load), and if recording of changes is enabled
-                // or if it contains tracked changes.
-                if ( m_bForceChangesToolbar && ( GetDocShell()->IsChangeRecording() ||
-                    m_pWrtShell->GetDoc()->getIDocumentRedlineAccess().GetRedlineTable().size() ) )
+                // show the Track Changes infobar if the view is new (e.g. on load), and
+                // if recording of changes is enabled or if there are tracked changes in
+                // the document. (The infobar won't be shown, if the Track Changes toolbar
+                // is already enabled, see in sfx2.)
+                if ( m_bForceChangesToolbar )
                 {
-                    ShowUIElement("private:resource/toolbar/changes");
+                    bool isRecording = GetDocShell()->IsChangeRecording();
+                    bool hasRecorded =
+                        m_pWrtShell->GetDoc()->getIDocumentRedlineAccess().GetRedlineTable().size();
+                    if ( isRecording || hasRecorded )
+                    {
+                        GetDocShell()->AppendInfoBarWhenReady(
+                            "trackchanges", SwResId(STR_HIDDEN_CHANGES),
+                            SwResId( (isRecording && hasRecorded)
+                                    ? STR_HIDDEN_CHANGES_DETAIL
+                                    : isRecording
+                                        ? STR_HIDDEN_CHANGES_DETAIL2
+                                        : STR_HIDDEN_CHANGES_DETAIL3 ),
+                            InfobarType::INFO);
+                    }
                 }
                 m_bForceChangesToolbar = false;
             break;