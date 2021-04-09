bbb741fbb150f52bb9720a3b56681cbda356f493
Stephan Bergmann <sbergman@redhat.com>
2021-04-09 15:15:33 +0200

    external/jfreereport: Get rid of apache-commons-logging
    ...using Java 1.4 java.util.logging.Logger instead.  The two patch files are
    taken from
    <https://src.fedoraproject.org/rpms/pentaho-reporting-flow-engine/c/7db2dc0aaa9219edc794b535084a17fb408f284a>
    "Related: rhbz#1895921 replace apache-commons-logging with java.util.logging"
    and
    <https://src.fedoraproject.org/rpms/liblayout/c/675bcd0243d31593d7b047017108b71adfe748c3>
    "Related: rhbz#1895921 replace apache-commons-logging with java.util.logging",
    respectively
    
    Change-Id: Ie03319568f2cefc8a982c4f9ee9241f8dc0c923e

--- external/jfreereport/ExternalProject_jfreereport_liblayout.mk
+++ external/jfreereport/ExternalProject_jfreereport_liblayout.mk
@@ -9,10 +9,6 @@
 
 $(eval $(call gb_ExternalProject_ExternalProject,jfreereport_liblayout))
 
-$(eval $(call gb_ExternalProject_use_externals,jfreereport_liblayout,\
-	commons-logging \
-))
-
 $(eval $(call gb_ExternalProject_use_external_projects,jfreereport_liblayout,\
 	jfreereport_sac \
 	jfreereport_libbase \
@@ -37,9 +33,6 @@ $(call gb_ExternalProject_get_state_target,jfreereport_liblayout,build) :
 			$(if $(verbose),-v,-q) \
 			-f build.xml \
 			-Dbuild.label="build-$(LIBO_VERSION_MAJOR).$(LIBO_VERSION_MINOR).$(LIBO_VERSION_MICRO).$(LIBO_VERSION_PATCH)" \
-			$(if $(SYSTEM_APACHE_COMMONS),\
-				-Dcommons-logging.jar=$(COMMONS_LOGGING_JAR), \
-				-Dcommons-logging.jar="$(call gb_UnpackedTarball_get_dir,apache_commons_logging)/target/commons-logging-$(COMMONS_LOGGING_VERSION).jar") \
 			-Dflute.jar=$(call gb_UnpackedTarball_get_dir,jfreereport_flute)/dist/flute-$(FLUTE_VERSION).jar \
 			-Dlibbase.jar=$(call gb_UnpackedTarball_get_dir,jfreereport_libbase)/dist/libbase-$(LIBBASE_VERSION).jar \
 			-Dlibformula.jar=$(call gb_UnpackedTarball_get_dir,jfreereport_libformula)/dist/libformula-$(LIBFORMULA_VERSION).jar \
