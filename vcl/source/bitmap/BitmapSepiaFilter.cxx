LibreOffice/core

commit c38485279cd36da96ac81107d567ea4e779b2b96

vcl: ImplSolarize() to BitmapSolarizeFilter
Change-Id: I3d615bcce851cb0f0140e2a1542a4073727a51be
Reviewed-on: https://gerrit.libreoffice.org/53201
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

vcl/source/bitmap/BitmapSolarizeFilter.cxx
@@ -1,0 +1,68 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#include <vcl/bitmap.hxx>
+#include <vcl/bitmapex.hxx>
+#include <vcl/bitmapaccess.hxx>
+#include <vcl/BitmapSolarizeFilter.hxx>
+
+#include <bitmapwriteaccess.hxx>
+
+BitmapEx BitmapSolarizeFilter::execute(BitmapEx const& rBitmapEx)
+{
+    Bitmap aBitmap(rBitmapEx.GetBitmap());
+    bool bRet = false;
+    BitmapScopedWriteAccess pWriteAcc(aBitmap);
+
+    if (pWriteAcc)
+    {
+        if (pWriteAcc->HasPalette())
+        {
+            const BitmapPalette& rPal = pWriteAcc->GetPalette();
+
+            for (sal_uInt16 i = 0, nCount = rPal.GetEntryCount(); i < nCount; i++)
+            {
+                if (rPal[i].GetLuminance() >= mcSolarGreyThreshold)
+                {
+                    BitmapColor aCol(rPal[i]);
+                    pWriteAcc->SetPaletteColor(i, aCol.Invert());
+                }
+            }
+        }
+        else
+        {
+            BitmapColor aCol;
+            const long nWidth = pWriteAcc->Width();
+            const long nHeight = pWriteAcc->Height();
+
+            for (long nY = 0; nY < nHeight; nY++)
+            {
+                Scanline pScanline = pWriteAcc->GetScanline(nY);
+                for (long nX = 0; nX < nWidth; nX++)
+                {
+                    aCol = pWriteAcc->GetPixelFromData(pScanline, nX);
+
+                    if (aCol.GetLuminance() >= mcSolarGreyThreshold)
+                        pWriteAcc->SetPixelOnData(pScanline, nX, aCol.Invert());
+                }
+            }
+        }
+
+        pWriteAcc.reset();
+        bRet = true;
+    }
+
+    if (bRet)
+        return BitmapEx(rBitmapEx);
+
+    return BitmapEx();
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

include/vcl/BitmapSolarizeFilter.hxx
@@ -1,0 +1,34 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#ifndef INCLUDED_VCL_BITMAPSOLARIZEFILTER_HXX
+#define INCLUDED_VCL_BITMAPSOLARIZEFILTER_HXX
+
+#include <vcl/BitmapFilter.hxx>
+
+class BitmapEx;
+
+class VCL_DLLPUBLIC BitmapSolarizeFilter : public BitmapFilter
+{
+public:
+    BitmapSolarizeFilter(double cSolarGreyThreshold)
+        : mcSolarGreyThreshold(cSolarGreyThreshold)
+    {
+    }
+
+    virtual BitmapEx execute(BitmapEx const& rBitmapEx) override;
+
+private:
+    sal_uInt8 mcSolarGreyThreshold;
+};
+
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

include/vcl/bitmap.hxx
@@ -659,7 +659,6 @@ public:
     SAL_DLLPRIVATE bool     ImplDitherFloyd16();
 
     SAL_DLLPRIVATE bool     ImplEmbossGrey( const BmpFilterParam* pFilterParam );
-    SAL_DLLPRIVATE bool     ImplSolarize( const BmpFilterParam* pFilterParam );
     SAL_DLLPRIVATE bool     ImplSepia( const BmpFilterParam* pFilterParam );
     SAL_DLLPRIVATE bool     ImplMosaic( const BmpFilterParam* pFilterParam );
     SAL_DLLPRIVATE bool     ImplDuotoneFilter( const sal_uLong nColorOne,  sal_uLong nColorTwo );

vcl/Library_vcl.mk
@@ -313,6 +313,7 @@ $(eval $(call gb_Library_add_exception_objects,vcl,\
     vcl/source/bitmap/bitmap \
     vcl/source/bitmap/bitmapfilter \
     vcl/source/bitmap/BitmapSobelGreyFilter \
+    vcl/source/bitmap/BitmapSolarizeFilter \
     vcl/source/bitmap/BitmapPopArtFilter \
     vcl/source/bitmap/BitmapConvolutionMatrixFilter \
     vcl/source/bitmap/BitmapMedianFilter \

vcl/source/gdi/bitmap4.cxx
@@ -24,6 +24,7 @@
 #include <vcl/BitmapSharpenFilter.hxx>
 #include <vcl/BitmapMedianFilter.hxx>
 #include <vcl/BitmapSobelGreyFilter.hxx>
+#include <vcl/BitmapSolarizeFilter.hxx>
 #include <vcl/BitmapPopArtFilter.hxx>
 
 #include <bitmapwriteaccess.hxx>
@@ -78,7 +79,11 @@ bool Bitmap::Filter( BmpFilter eFilter, const BmpFilterParam* pFilterParam )
         break;
 
         case BmpFilter::Solarize:
-            bRet = ImplSolarize( pFilterParam );
+        {
+            BitmapEx aBmpEx(*this);
+            bRet = BitmapFilter::Filter(aBmpEx, BitmapSolarizeFilter(pFilterParam->mcSolarGreyThreshold));
+            *this = aBmpEx.GetBitmap();
+        }
         break;
 
         case BmpFilter::Sepia:
@@ -221,55 +226,6 @@ bool Bitmap::ImplEmbossGrey( const BmpFilterParam* pFilterParam )
                 maPrefSize = aSize;
             }
         }
-    }
-
-    return bRet;
-}
-
-bool Bitmap::ImplSolarize( const BmpFilterParam* pFilterParam )
-{
-    bool                bRet = false;
-    BitmapScopedWriteAccess pWriteAcc(*this);
-
-    if( pWriteAcc )
-    {
-        const sal_uInt8 cThreshold = ( pFilterParam && pFilterParam->meFilter == BmpFilter::Solarize ) ?
-                                pFilterParam->mcSolarGreyThreshold : 128;
-
-        if( pWriteAcc->HasPalette() )
-        {
-            const BitmapPalette& rPal = pWriteAcc->GetPalette();
-
-            for( sal_uInt16 i = 0, nCount = rPal.GetEntryCount(); i < nCount; i++ )
-            {
-                if( rPal[ i ].GetLuminance() >= cThreshold )
-                {
-                    BitmapColor aCol( rPal[ i ] );
-                    pWriteAcc->SetPaletteColor( i, aCol.Invert() );
-                }
-            }
-        }
-        else
-        {
-            BitmapColor aCol;
-            const long  nWidth = pWriteAcc->Width();
-            const long  nHeight = pWriteAcc->Height();
-
-            for( long nY = 0; nY < nHeight ; nY++ )
-            {
-                Scanline pScanline = pWriteAcc->GetScanline(nY);
-                for( long nX = 0; nX < nWidth; nX++ )
-                {
-                    aCol = pWriteAcc->GetPixelFromData( pScanline, nX );
-
-                    if( aCol.GetLuminance() >= cThreshold )
-                        pWriteAcc->SetPixelOnData( pScanline, nX, aCol.Invert() );
-                }
-            }
-        }
-
-        pWriteAcc.reset();
-        bRet = true;
     }
 
     return bRet;

commit 65697fe445767f321b5f8d83ce755cf44b6b5448

Related tdf#117750: fix "Solarization" and "Aging"
For solarization, regression from:
https://cgit.freedesktop.org/libreoffice/core/commit/?id=c38485279cd36da96ac81107d567ea4e779b2b96
author	Chris Sherlock <chris.sherlock79@gmail.com>	2018-04-20 20:27:41 +1000
committer	Tomaž Vajngerl <quikee@gmail.com>	2018-04-22 05:33:11 +0200
commit	c38485279cd36da96ac81107d567ea4e779b2b96 (patch)
tree	ad9843a17f87271f9e0cf89643e2e6c3ee8a8de1
parent	bcbf767bcfc024e2be839e0c0886f942dd068e4f (diff)
vcl: ImplSolarize() to BitmapSolarizeFilter

For aging, regression from:
https://cgit.freedesktop.org/libreoffice/core/commit/?id=1ab12471f3a69c4d502e6271e84ddf8a981f507f
author	Chris Sherlock <chris.sherlock79@gmail.com>	2018-04-20 20:32:23 +1000
committer	Tomaž Vajngerl <quikee@gmail.com>	2018-04-22 05:33:39 +0200
commit	1ab12471f3a69c4d502e6271e84ddf8a981f507f (patch)
tree	6652276229ad1c3ffca48f7a31e1b77d2ea04b07
parent	c38485279cd36da96ac81107d567ea4e779b2b96 (diff)
vcl: ImplSepia -> BitmapSepiaFilter

Change-Id: I4f350aa8b494caf55c9c810ac7fa9ed8db349725

vcl/source/bitmap/BitmapSepiaFilter.cxx
@@ -102,7 +102,7 @@ BitmapEx BitmapSepiaFilter::execute(BitmapEx const& rBitmapEx) const
     }
 
     if (bRet)
-        return rBitmapEx;
+        return BitmapEx(aBitmap);
 
     return BitmapEx();
 }

vcl/source/bitmap/BitmapSolarizeFilter.cxx
@@ -63,7 +63,7 @@ BitmapEx BitmapSolarizeFilter::execute(BitmapEx const& rBitmapEx) const
     }
 
     if (bRet)
-        return rBitmapEx;
+        return BitmapEx(aBitmap);
 
     return BitmapEx();
 }