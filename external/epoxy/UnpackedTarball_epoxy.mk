4ad8eb78dd0f7ffb0c64a2f97a92dd592fb87f66
Jan-Marek Glogowski <glogow@fbihome.de>
2021-04-11 14:16:42 +0200

    epoxy: fix gen_dispatch.py fildescriptor leak
    Change-Id: I3f7bd1befffb45b12bc178f9addf1445a6c33921

--- external/epoxy/UnpackedTarball_epoxy.mk
+++ external/epoxy/UnpackedTarball_epoxy.mk
@@ -30,6 +30,7 @@ $(eval $(call gb_UnpackedTarball_add_patches,epoxy, \
     external/epoxy/epoxy.windows.api.patch \
     external/epoxy/epoxy.noegl.by.default.patch \
     external/epoxy/clang-cl.patch \
+    external/epoxy/epoxy.pyresource-leak.patch \
 ))
 
 # vim: set noet sw=4 ts=4:
