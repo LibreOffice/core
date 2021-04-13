a19cbc4ed1c5f17cf0aec965f3d05fb510633d90
Noel Grandin <noel@peralex.com>
2021-04-13 14:56:11 +0200

    flatten PropertyChangeNotifier
    which removes an allocation hotspot when loading lots of shapes
    
    Change-Id: I0121f5bc86c6dfeddcad13e81203a514151d61bf

--- include/svx/shapepropertynotifier.hxx
+++ include/svx/shapepropertynotifier.hxx
@@ -25,6 +25,7 @@
 #include <rtl/ustring.hxx>
 
 #include <memory>
+#include <unordered_map>
 
 namespace com::sun::star::beans { class XPropertyChangeListener; }
 namespace com::sun::star::uno { class Any; }
@@ -89,10 +90,6 @@ namespace svx
     };
 
 
-    //= PropertyChangeNotifier
-
-    struct PropertyChangeNotifier_Data;
-
     /** helper class for notifying XPropertyChangeListeners
 
         The class is intended to be held as member of the class which does the property change broadcasting.
@@ -132,7 +129,18 @@ namespace svx
         PropertyChangeNotifier(const PropertyChangeNotifier&) = delete;
         PropertyChangeNotifier& operator=(const PropertyChangeNotifier&) = delete;
 
-        std::unique_ptr< PropertyChangeNotifier_Data >  m_xData;
+        struct ShapePropertyHash
+        {
+            size_t operator()( svx::ShapeProperty x ) const
+            {
+                return size_t( x );
+            }
+        };
+        typedef std::unordered_map< ShapeProperty, std::shared_ptr<IPropertyValueProvider>, ShapePropertyHash  >
+            PropertyProviders;
+        ::cppu::OWeakObject&            m_rContext;
+        PropertyProviders               m_aProviders;
+        cppu::OMultiTypeInterfaceContainerHelperVar<OUString> m_aPropertyChangeListeners;
     };
 
 
