7dd92127c270d385275090f481697e32ff81b9b6
Michael Stahl <michael.stahl@allotropia.de>
2021-04-13 20:04:27 +0200

    sw: layout dump: sorted_objs of SwPageFrame
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/114065
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
    (cherry picked from commit 3d64587c53eea47674d51e84622a167db1635909)
    
    Change-Id: I2bf8493744d054c513ab9b65136b6e29e9aa07f9

--- sw/source/core/text/xmldump.cxx
+++ sw/source/core/text/xmldump.cxx
@@ -15,6 +15,7 @@
 #include <pagefrm.hxx>
 #include <txtfrm.hxx>
 #include <cellfrm.hxx>
+#include <flyfrm.hxx>
 #include <hffrm.hxx>
 #include <rootfrm.hxx>
 #include <editsh.hxx>
@@ -350,6 +351,27 @@ void SwFrame::dumpAsXml( xmlTextWriterPtr writer ) const
             OUString aFormatName = pPageFrame->GetPageDesc()->GetName();
             xmlTextWriterWriteFormatAttribute( writer, BAD_CAST("pageDesc"), "%s", BAD_CAST(OUStringToOString(aFormatName, RTL_TEXTENCODING_UTF8).getStr()));
             xmlTextWriterEndElement(writer);
+            // disable for tests to avoid resolving loads of merge conflicts
+            if (!getenv("LO_TESTNAME")) if (auto const* pObjs = pPageFrame->GetSortedObjs())
+            {
+                (void)xmlTextWriterStartElement(writer, BAD_CAST("sorted_objs"));
+                for (SwAnchoredObject const*const pObj : *pObjs)
+                {   // just print pointer, full details will be printed on its anchor frame
+                    // this nonsense is needed because of multiple inheritance
+                    if (SwFlyFrame const*const pFly = dynamic_cast<SwFlyFrame const*>(pObj))
+                    {
+                        (void)xmlTextWriterStartElement(writer, BAD_CAST("fly"));
+                        (void)xmlTextWriterWriteFormatAttribute(writer, BAD_CAST("ptr"), "%p", pFly);
+                    }
+                    else
+                    {
+                        (void)xmlTextWriterStartElement(writer, BAD_CAST(pObj->getElementName()));
+                        (void)xmlTextWriterWriteFormatAttribute(writer, BAD_CAST("ptr"), "%p", pObj);
+                    }
+                    (void)xmlTextWriterEndElement(writer);
+                }
+                (void)xmlTextWriterEndElement(writer);
+            }
         }
 
         if (IsTextFrame())
