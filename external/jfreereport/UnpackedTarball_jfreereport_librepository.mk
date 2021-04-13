351382edb60a630ba232bd39403f97460edcf5bc
Stephan Bergmann <sbergman@redhat.com>
2021-04-13 15:24:48 +0200

    external/jfreereport: Get rid of remaining apache-commons-logging
    ...similar to b721a1cecb6ea0013be8402350110822e50b4415 "external/jfreereport:
    Get rid of apache-commons-logging", but where the external tarballs contain
    copies of commons-logging-api-1.0.4.jar to compile against, so
    6de0b1710adfba82c96b75a5da6f52633a54c692 "Get rid of apache-commons-logging" did
    not affect them at build- but only at run-time.
    
    The sources of the seven new patch files (and which have further been adapted as
    necessary) are taken from
    <https://src.fedoraproject.org/rpms/libbase/blob/4a8cd85e49a0a00d0e0f865a3c841e3f1858a04b>
    "Related: rhbz#1895921 replace apache-commons-logging with java.util.logging",
    <https://src.fedoraproject.org/rpms/libfonts/c/c7f7d4ed67b9ca701152732cbdac547db3ada5f4>
    "Related: rhbz#1895921 replace apache-commons-logging with java.util.logging",
    <https://src.fedoraproject.org/rpms/libformula/c/feb5be4b393f13b131623339a5868c6b381b2507>
    "Related: rhbz#1895921 replace apache-commons-logging with java.util.logging",
    <https://src.fedoraproject.org/rpms/libloader/c/53eeb19460f2ab934cdf4c7c7c73fd681141216c>
    "Related: rhbz#1895921 replace apache-commons-logging with java.util.logging",
    <https://src.fedoraproject.org/rpms/librepository/c/9fa7021dfe7dcc76e32c40f99f74c3745013c501>
    "Related: rhbz#1895921 replace apache-commons-logging with java.util.logging",
    <https://src.fedoraproject.org/rpms/libserializer/c/027e1a16f7171f7ac5f9da4683d1e1b63794bb06>
    "Related: rhbz#1895921 replace apache-commons-logging with java.util.logging",
    and
    <https://src.fedoraproject.org/rpms/pentaho-libxml/c/74538ea7cdd0845f8e6dff748f8d4abfe080dbe4>
    "add missing patch".
    
    Change-Id: I98bbe0e24398d3652cda09b0664b4775661cd939
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/114053
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/127199
    Tested-by: Gabor Kelemen <kelemeng@ubuntu.com>
    Reviewed-by: Gabor Kelemen <kelemeng@ubuntu.com>

--- external/jfreereport/UnpackedTarball_jfreereport_librepository.mk
+++ external/jfreereport/UnpackedTarball_jfreereport_librepository.mk
@@ -21,6 +21,7 @@ $(eval $(call gb_UnpackedTarball_fix_end_of_line,jfreereport_librepository,\
 $(eval $(call gb_UnpackedTarball_add_patches,jfreereport_librepository,\
 	external/jfreereport/patches/common_build.patch \
 	external/jfreereport/patches/librepository-$(LIBREPOSITORY_VERSION)-deprecated.patch \
+	external/jfreereport/patches/librepository-1.1.3-remove-commons-logging.patch.1 \
 ))
 
 # vim: set noet sw=4 ts=4:
