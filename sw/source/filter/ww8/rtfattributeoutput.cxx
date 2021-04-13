LibreOffice/core

commit a072b3533f44730565f42b45cfd9f77f44f506a9

fdo#59886 export fixed date and time fields to docx.
Fixed date and time fields are supported in OOXML by adding the
attribute fldLock="true" to the fldChar element. This applies only
to the first instance of fldChar in a field (i.e. when fldCharType
is "begin").

Change-Id: Ibb84503b942ca20b53fe476e5006d64b2f3112e5
Reviewed-on: https://gerrit.libreoffice.org/13429
Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
Tested-by: Miklos Vajna <vmiklos@collabora.co.uk>

sw/source/filter/ww8/ww8atr.cxx
@@ -2861,7 +2861,7 @@ void AttributeOutputBase::TextField( const SwFmtFld& rField )
     case RES_DATETIMEFLD:
         {
             OUString sStr;
-            if (FIXEDFLD & nSubType || !GetExport().GetNumberFmt(*pFld, sStr))
+            if (!GetExport().GetNumberFmt(*pFld, sStr))
                 bWriteExpand = true;
             else
             {

sw/source/filter/ww8/docxattributeoutput.cxx
@@ -1443,9 +1443,19 @@ void DocxAttributeOutput::StartField_Impl( FieldInfos& rInfos, bool bWriteRun )
         else
         {
             // Write the field start
-            m_pSerializer->startElementNS( XML_w, XML_fldChar,
-                FSNS( XML_w, XML_fldCharType ), "begin",
-                FSEND );
+            if ( rInfos.pField && rInfos.pField->GetSubType() & FIXEDFLD )
+            {
+                m_pSerializer->startElementNS( XML_w, XML_fldChar,
+                    FSNS( XML_w, XML_fldCharType ), "begin",
+                    FSNS( XML_w, XML_fldLock ), "true",
+                    FSEND );
+            }
+            else
+            {
+                m_pSerializer->startElementNS( XML_w, XML_fldChar,
+                    FSNS( XML_w, XML_fldCharType ), "begin",
+                    FSEND );
+            }
 
             if ( rInfos.pFieldmark )
                 WriteFFData(  rInfos );

commit 7933f367d7e51497826c945f1e9aa0b0fd763bfd

tdf#100961 doc/rtf export: save FIXEDFLD as fldlock
This fixes the 5.0 regression by
commit a072b3533f44730565f42b45cfd9f77f44f506a9
    fdo#59886 export fixed date and time fields to docx.
which fixed it for DOCX, but broke it (a plain text dump)
for DOC and RTF formats.

We still can't READ fixed fields properly,
(there is no impact/regression on import at all)
but this at least works for MS Word,
and gets us setup for properly importing.

Change-Id: I8835fce06233a52f1433f02879196234f1355280

sw/source/filter/ww8/rtfattributeoutput.cxx
@@ -1679,6 +1679,8 @@ void RtfAttributeOutput::WriteField_Impl(const SwField* const pField, ww::eField
         if (bHasInstructions)
         {
             m_aRunText->append("{" OOO_STRING_SVTOOLS_RTF_FIELD);
+            if (pField && (pField->GetSubType() & FIXEDFLD))
+                m_aRunText->append(OOO_STRING_SVTOOLS_RTF_FLDLOCK);
             m_aRunText->append("{" OOO_STRING_SVTOOLS_RTF_IGNORE OOO_STRING_SVTOOLS_RTF_FLDINST
                                " ");
             m_aRunText->append(

sw/source/filter/ww8/ww8atr.cxx
@@ -1916,6 +1916,13 @@ void WW8Export::OutputField( const SwField* pField, ww::eField eFieldType,
             if (nSubType == REF_SEQUENCEFLD)
                 aField15[0] |= (0x4 << 5);
         }
+        // This ought to apply to any field, but just to be safe, start off with DATE/TIME only.
+        if (pField->GetTyp()->Which() == SwFieldIds::DateTime
+            && (pField->GetSubType() & FIXEDFLD))
+        {
+            //bit 5 - Locked: do not recalculate field
+            aField15[1] |= 0x10;
+        }
     }
 
     pFieldP->Append( Fc2Cp( Strm().Tell() ), aField15 );