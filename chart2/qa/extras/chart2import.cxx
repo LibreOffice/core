LibreOffice/core

commit cbe6c4929c9da28ce5553ce31782027d5111ed16

tdf#127811 tdf#127813 Introduce compatibility key for the data series
order of filled net and normal area charts.
The data series of filled net and normal area charts are drawn in
reversed order in LibreOffice but not in Microsoft Office.

Default value is true to keep current behavior.

Change-Id: I07adac814597b756878d74610d028f07327f7214
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/83897
Tested-by: Jenkins
Reviewed-by: Balazs Varga <balazs.varga991@gmail.com>

chart2/source/view/charttypes/AreaChart.cxx
@@ -43,6 +43,7 @@
 #include <com/sun/star/drawing/DoubleSequence.hpp>
 #include <com/sun/star/drawing/XShapes.hpp>
 #include <com/sun/star/beans/XPropertySet.hpp>
+#include <officecfg/Office/Compatibility.hxx>
 
 namespace chart
 {
@@ -606,7 +607,8 @@ void AreaChart::createShapes()
     if( m_aZSlots.empty() ) //no series
         return;
 
-    if( m_nDimension == 2 && ( m_bArea || !m_bCategoryXAxis ) )
+    //tdf#127813 Don't reverse the series in OOXML-heavy environments
+    if( officecfg::Office::Compatibility::View::ReverseSeriesOrderAreaAndNetChart::get() && m_nDimension == 2 && ( m_bArea || !m_bCategoryXAxis ) )
         lcl_reorderSeries( m_aZSlots );
 
     OSL_ENSURE(m_pShapeFactory&&m_xLogicTarget.is()&&m_xFinalTarget.is(),"AreaChart is not proper initialized");

oox/source/drawingml/chart/typegroupconverter.cxx
@@ -69,24 +69,24 @@ namespace csscd = ::com::sun::star::chart::DataLabelPlacement;
 
 static const TypeGroupInfo spTypeInfos[] =
 {
-    // type-id          type-category         service                   varied-point-color   default label pos     polar  area2d 1stvis xcateg swap   stack  revers picopt
-    { TYPEID_BAR,       TYPECATEGORY_BAR,     SERVICE_CHART2_COLUMN,    VARPOINTMODE_SINGLE, csscd::OUTSIDE,       false, true,  false, true,  false, true,  false, true  },
-    { TYPEID_HORBAR,    TYPECATEGORY_BAR,     SERVICE_CHART2_COLUMN,    VARPOINTMODE_SINGLE, csscd::OUTSIDE,       false, true,  false, true,  true,  true,  false, true  },
-    { TYPEID_LINE,      TYPECATEGORY_LINE,    SERVICE_CHART2_LINE,      VARPOINTMODE_SINGLE, csscd::RIGHT,         false, false, false, true,  false, true,  false, false },
-    { TYPEID_AREA,      TYPECATEGORY_LINE,    SERVICE_CHART2_AREA,      VARPOINTMODE_NONE,   csscd::CENTER,        false, true,  false, true,  false, true,  true,  false },
-    { TYPEID_STOCK,     TYPECATEGORY_LINE,    SERVICE_CHART2_CANDLE,    VARPOINTMODE_NONE,   csscd::RIGHT,         false, false, false, true,  false, true,  false, false },
-    { TYPEID_RADARLINE, TYPECATEGORY_RADAR,   SERVICE_CHART2_NET,       VARPOINTMODE_SINGLE, csscd::OUTSIDE,       true,  false, false, true,  false, false, false, false },
-    { TYPEID_RADARAREA, TYPECATEGORY_RADAR,   SERVICE_CHART2_FILLEDNET, VARPOINTMODE_NONE,   csscd::OUTSIDE,       true,  true,  false, true,  false, false, true,  false },
-    { TYPEID_PIE,       TYPECATEGORY_PIE,     SERVICE_CHART2_PIE,       VARPOINTMODE_MULTI,  csscd::AVOID_OVERLAP, true,  true,  true,  true,  false, false, false, false },
-    { TYPEID_DOUGHNUT,  TYPECATEGORY_PIE,     SERVICE_CHART2_PIE,       VARPOINTMODE_MULTI,  csscd::AVOID_OVERLAP, true,  true,  false, true,  false, false, false, false },
-    { TYPEID_OFPIE,     TYPECATEGORY_PIE,     SERVICE_CHART2_PIE,       VARPOINTMODE_MULTI,  csscd::AVOID_OVERLAP, true,  true,  true,  true,  false, false, false, false },
-    { TYPEID_SCATTER,   TYPECATEGORY_SCATTER, SERVICE_CHART2_SCATTER,   VARPOINTMODE_SINGLE, csscd::RIGHT,         false, false, false, false, false, false, false, false },
-    { TYPEID_BUBBLE,    TYPECATEGORY_SCATTER, SERVICE_CHART2_BUBBLE,    VARPOINTMODE_SINGLE, csscd::RIGHT,         false, true,  false, false, false, false, false, false },
-    { TYPEID_SURFACE,   TYPECATEGORY_SURFACE, SERVICE_CHART2_SURFACE,   VARPOINTMODE_NONE,   csscd::RIGHT,         false, true,  false, true,  false, false, false, false }
+    // type-id          type-category         service                   varied-point-color   default label pos     polar  area2d 1stvis xcateg swap   stack  picopt
+    { TYPEID_BAR,       TYPECATEGORY_BAR,     SERVICE_CHART2_COLUMN,    VARPOINTMODE_SINGLE, csscd::OUTSIDE,       false, true,  false, true,  false, true,  true  },
+    { TYPEID_HORBAR,    TYPECATEGORY_BAR,     SERVICE_CHART2_COLUMN,    VARPOINTMODE_SINGLE, csscd::OUTSIDE,       false, true,  false, true,  true,  true,  true  },
+    { TYPEID_LINE,      TYPECATEGORY_LINE,    SERVICE_CHART2_LINE,      VARPOINTMODE_SINGLE, csscd::RIGHT,         false, false, false, true,  false, true,  false },
+    { TYPEID_AREA,      TYPECATEGORY_LINE,    SERVICE_CHART2_AREA,      VARPOINTMODE_NONE,   csscd::CENTER,        false, true,  false, true,  false, true,  false },
+    { TYPEID_STOCK,     TYPECATEGORY_LINE,    SERVICE_CHART2_CANDLE,    VARPOINTMODE_NONE,   csscd::RIGHT,         false, false, false, true,  false, true,  false },
+    { TYPEID_RADARLINE, TYPECATEGORY_RADAR,   SERVICE_CHART2_NET,       VARPOINTMODE_SINGLE, csscd::OUTSIDE,       true,  false, false, true,  false, false, false },
+    { TYPEID_RADARAREA, TYPECATEGORY_RADAR,   SERVICE_CHART2_FILLEDNET, VARPOINTMODE_NONE,   csscd::OUTSIDE,       true,  true,  false, true,  false, false, false },
+    { TYPEID_PIE,       TYPECATEGORY_PIE,     SERVICE_CHART2_PIE,       VARPOINTMODE_MULTI,  csscd::AVOID_OVERLAP, true,  true,  true,  true,  false, false, false },
+    { TYPEID_DOUGHNUT,  TYPECATEGORY_PIE,     SERVICE_CHART2_PIE,       VARPOINTMODE_MULTI,  csscd::AVOID_OVERLAP, true,  true,  false, true,  false, false, false },
+    { TYPEID_OFPIE,     TYPECATEGORY_PIE,     SERVICE_CHART2_PIE,       VARPOINTMODE_MULTI,  csscd::AVOID_OVERLAP, true,  true,  true,  true,  false, false, false },
+    { TYPEID_SCATTER,   TYPECATEGORY_SCATTER, SERVICE_CHART2_SCATTER,   VARPOINTMODE_SINGLE, csscd::RIGHT,         false, false, false, false, false, false, false },
+    { TYPEID_BUBBLE,    TYPECATEGORY_SCATTER, SERVICE_CHART2_BUBBLE,    VARPOINTMODE_SINGLE, csscd::RIGHT,         false, true,  false, false, false, false, false },
+    { TYPEID_SURFACE,   TYPECATEGORY_SURFACE, SERVICE_CHART2_SURFACE,   VARPOINTMODE_NONE,   csscd::RIGHT,         false, true,  false, true,  false, false, false }
 };
 
 static const TypeGroupInfo saUnknownTypeInfo =
-    { TYPEID_UNKNOWN,   TYPECATEGORY_BAR,     SERVICE_CHART2_COLUMN,  VARPOINTMODE_SINGLE, csscd::OUTSIDE,       false, true,  false, true,  false, true,  false, true  };
+    { TYPEID_UNKNOWN,   TYPECATEGORY_BAR,     SERVICE_CHART2_COLUMN,  VARPOINTMODE_SINGLE, csscd::OUTSIDE,       false, true,  false, true,  false, true, true  };
 
 const TypeGroupInfo& lclGetTypeInfoFromTypeId( TypeId eTypeId )
 {
@@ -347,10 +347,6 @@ void TypeGroupConverter::convertFromModel( const Reference< XDiagram >& rxDiagra
         SeriesConvVector aSeries;
         for (auto const& elemSeries : mrModel.maSeries)
             aSeries.push_back( std::make_shared<SeriesConverter>(*this, *elemSeries) );
-
-        // reverse series order for some unstacked 2D chart types
-        if( maTypeInfo.mbReverseSeries && !mb3dChart && !isStacked() && !isPercent() )
-            ::std::reverse( aSeries.begin(), aSeries.end() );
 
         // decide whether to use varying colors for each data point
         bool bVaryColorsByPoint = bSupportsVaryColorsByPoint && mrModel.mbVaryColors;

chart2/source/view/charttypes/NetChart.cxx
@@ -36,6 +36,7 @@
 #include <osl/diagnose.h>
 
 #include <com/sun/star/drawing/XShapes.hpp>
+#include <officecfg/Office/Compatibility.hxx>
 
 namespace chart
 {
@@ -318,7 +319,8 @@ void NetChart::createShapes()
     if( m_aZSlots.empty() ) //no series
         return;
 
-    if( m_bArea )
+    //tdf#127813 Don't reverse the series in OOXML-heavy environments
+    if (officecfg::Office::Compatibility::View::ReverseSeriesOrderAreaAndNetChart::get() && m_bArea)
         lcl_reorderSeries( m_aZSlots );
 
     OSL_ENSURE(m_pShapeFactory&&m_xLogicTarget.is()&&m_xFinalTarget.is(),"NetChart is not proper initialized");

officecfg/registry/schema/org/openoffice/Office/Compatibility.xcs
@@ -171,6 +171,14 @@
         </info>
         <value>true</value>
       </prop>
+      <prop oor:name="ReverseSeriesOrderAreaAndNetChart" oor:type="xs:boolean" oor:nillable="false">
+        <info>
+          <!-- See tdf#127813 for rationale -->
+          <desc>Specifies the data series order of filled net and normal area charts.</desc>
+          <label>Reverse the data series order of filled net and normal area charts. Disable for better OOXML interoperability.</label>
+        </info>
+        <value>true</value>
+      </prop>
     </group>
   </component>
 </oor:component-schema>

oox/inc/drawingml/chart/typegroupconverter.hxx
@@ -86,7 +86,6 @@ struct TypeGroupInfo
     bool                mbCategoryAxis;         /// True = X axis contains categories.
     bool                mbSwappedAxesSet;       /// True = X axis and Y axis are swapped.
     bool                mbSupportsStacking;     /// True = data points can be stacked on each other.
-    bool                mbReverseSeries;        /// True = insert unstacked series in reverse order.
     bool                mbPictureOptions;       /// True = bitmaps support options from c:pictureOptions.
 };
 

commit bc691b820b280bf4b56d4c632496f457afd547bb

tdf#141657 don't reverse series order in area chart for oox
This is followup for tdf#140489 fixing regression introduced by:
https://cgit.freedesktop.org/libreoffice/core/commit/?id=cbe6c4929c9da28ce5553ce31782027d5111ed16

Change-Id: Ia2c39f1cd08d1405de888c4cf7f0986ca38769d2

chart2/inc/unonames.hxx
@@ -32,5 +32,6 @@
 #define CHART_UNONAME_LABEL_FILL_HATCH_NAME "LabelFillHatchName"
 #define CHART_UNONAME_LABEL_FILL_COLOR      "LabelFillColor"
 #define CHART_UNONAME_CUSTOM_LABEL_FIELDS   "CustomLabelFields"
+#define CHART_UNONAME_REVERSE_Z_ORDER       "ReverseZOrder"
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

chart2/source/model/template/ChartType.cxx
@@ -23,7 +23,9 @@
 #include <CloneHelper.hxx>
 #include <AxisIndexDefines.hxx>
 #include <ModifyListenerHelper.hxx>
+#include <PropertyHelper.hxx>
 #include <vcl/svapp.hxx>
+#include <com/sun/star/beans/PropertyAttribute.hpp>
 #include <com/sun/star/chart2/AxisType.hpp>
 #include <com/sun/star/container/NoSuchElementException.hpp>
 #include <tools/diagnose_ex.h>
@@ -33,6 +35,37 @@ using namespace ::com::sun::star;
 using ::com::sun::star::beans::Property;
 using ::com::sun::star::uno::Sequence;
 using ::com::sun::star::uno::Reference;
+
+namespace {
+    enum
+    {
+        PROP_CHART_TYPE_REVERSEZORDER
+    };
+
+    void lcl_AddPropertiesToVector(
+        std::vector< Property > & rOutProperties )
+    {
+        rOutProperties.emplace_back( "ReverseZOrder",
+                    PROP_CHART_TYPE_REVERSEZORDER,
+                    cppu::UnoType<bool>::get(),
+                    css::beans::PropertyAttribute::BOUND
+                    | css::beans::PropertyAttribute::MAYBEDEFAULT );
+    }
+
+    struct StaticChartTypeDefaults_Initializer
+    {
+        ::chart::tPropertyValueMap* operator()()
+        {
+            static ::chart::tPropertyValueMap aStaticDefaults;
+            ::chart::PropertyHelper::setPropertyValueDefault< bool >( aStaticDefaults, PROP_CHART_TYPE_REVERSEZORDER, true );
+            return &aStaticDefaults;
+        }
+    };
+
+    struct StaticChartTypeDefaults : public rtl::StaticAggregate< ::chart::tPropertyValueMap, StaticChartTypeDefaults_Initializer >
+    {
+    };
+}
 
 namespace chart
 {
@@ -191,9 +224,13 @@ void SAL_CALL ChartType::setDataSeries( const Sequence< Reference< chart2::XData
 }
 
 // ____ OPropertySet ____
-uno::Any ChartType::GetDefaultValue( sal_Int32 /* nHandle */ ) const
+uno::Any ChartType::GetDefaultValue( sal_Int32 nHandle ) const
 {
-    return uno::Any();
+    const tPropertyValueMap& rStaticDefaults = *StaticChartTypeDefaults::get();
+    tPropertyValueMap::const_iterator aFound( rStaticDefaults.find( nHandle ) );
+    if( aFound == rStaticDefaults.end() )
+        return uno::Any();
+    return (*aFound).second;
 }
 
 namespace
@@ -203,8 +240,20 @@ struct StaticChartTypeInfoHelper_Initializer
 {
     ::cppu::OPropertyArrayHelper* operator()()
     {
-        static ::cppu::OPropertyArrayHelper aPropHelper( Sequence< beans::Property >{} );
+        static ::cppu::OPropertyArrayHelper aPropHelper( lcl_GetPropertySequence() );
         return &aPropHelper;
+    }
+
+private:
+    static uno::Sequence< Property > lcl_GetPropertySequence()
+    {
+        std::vector< css::beans::Property > aProperties;
+        lcl_AddPropertiesToVector( aProperties );
+
+        std::sort( aProperties.begin(), aProperties.end(),
+                     ::chart::PropertyNameLess() );
+
+        return comphelper::containerToSequence( aProperties );
     }
 };
 

chart2/source/view/charttypes/AreaChart.cxx
@@ -607,8 +607,16 @@ void AreaChart::createShapes()
     if( m_aZSlots.empty() ) //no series
         return;
 
+    bool bReverseZOrder = true;
+    if( m_xChartTypeModelProps.is() )
+    {
+        const uno::Reference<css::beans::XPropertySetInfo>& rPropertySetInfo = m_xChartTypeModelProps->getPropertySetInfo();
+        if( rPropertySetInfo.is() && rPropertySetInfo->hasPropertyByName(CHART_UNONAME_REVERSE_Z_ORDER) )
+            m_xChartTypeModelProps->getPropertyValue(CHART_UNONAME_REVERSE_Z_ORDER) >>= bReverseZOrder;
+    }
+
     //tdf#127813 Don't reverse the series in OOXML-heavy environments
-    if( officecfg::Office::Compatibility::View::ReverseSeriesOrderAreaAndNetChart::get() && m_nDimension == 2 && ( m_bArea || !m_bCategoryXAxis ) )
+    if( bReverseZOrder && officecfg::Office::Compatibility::View::ReverseSeriesOrderAreaAndNetChart::get() && m_nDimension == 2 && ( m_bArea || !m_bCategoryXAxis ) )
         lcl_reorderSeries( m_aZSlots );
 
     OSL_ENSURE(m_pShapeFactory&&m_xLogicTarget.is()&&m_xFinalTarget.is(),"AreaChart is not proper initialized");

oox/source/drawingml/chart/typegroupconverter.cxx
@@ -352,6 +352,11 @@ void TypeGroupConverter::convertFromModel( const Reference< XDiagram >& rxDiagra
         for (auto const& elemSeries : mrModel.maSeries)
             aSeries.push_back( std::make_shared<SeriesConverter>(*this, *elemSeries) );
 
+        // some unstacked 2D chart types shouldn't have reversed z-order
+        if( (maTypeInfo.meTypeId == TYPEID_AREA || maTypeInfo.meTypeId == TYPEID_RADARAREA)
+            && !mb3dChart && !isStacked() && !isPercent() )
+            aTypeProp.setProperty( PROP_ReverseZOrder, false );
+
         // decide whether to use varying colors for each data point
         bool bVaryColorsByPoint = bSupportsVaryColorsByPoint && mrModel.mbVaryColors;
         switch( maTypeInfo.meVarPointMode )

chart2/qa/extras/chart2import.cxx
@@ -1769,9 +1769,18 @@ void Chart2ImportTest::testTdf140489MultiSeriesChartAxisXLSX()
     Reference<chart2::XDataSeries> xSeries = getDataSeriesFromDoc(xChartDoc, 0);
     CPPUNIT_ASSERT(xSeries.is());
 
+    Reference< chart2::XChartType > xChartType = getChartTypeFromDoc(xChartDoc, 0, 0);
+    CPPUNIT_ASSERT(xChartType.is());
+
+    Reference<beans::XPropertySet> xTypePropSet(xChartType, uno::UNO_QUERY_THROW);
+    bool bReverseZOrder = true;
+    uno::Any aAny = xTypePropSet->getPropertyValue("ReverseZOrder");
+    CPPUNIT_ASSERT(aAny >>= bReverseZOrder);
+    CPPUNIT_ASSERT_EQUAL(false, bReverseZOrder);
+
     Reference<beans::XPropertySet> xPropSet(xSeries, uno::UNO_QUERY_THROW);
     sal_Int32 nAxisIndex = -1;
-    uno::Any aAny = xPropSet->getPropertyValue("AttachedAxisIndex");
+    aAny = xPropSet->getPropertyValue("AttachedAxisIndex");
     CPPUNIT_ASSERT(aAny >>= nAxisIndex);
     CPPUNIT_ASSERT_EQUAL(sal_Int32(0), nAxisIndex);
 