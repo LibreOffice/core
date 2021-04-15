4dabd26be520510b9614130d9e90157813543ced
Thorsten Behrens <thorsten.behrens@allotropia.de>
2021-04-16 00:27:57 +0200

    Remove .uno:QuestionAnswers, direct safeMode help to landing page
    Also point to Allotropia contact page.
    
    Change-Id: I41a456b838508a7904a81ff858de6dada0ed6824
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/156496
    Reviewed-by: Gabor Kelemen <kelemeng@ubuntu.com>
    Tested-by: Thorsten Behrens <thorsten.behrens@allotropia.de>

--- svx/source/dialog/SafeModeDialog.cxx
+++ svx/source/dialog/SafeModeDialog.cxx
@@ -13,6 +13,8 @@
 #include <sfx2/safemode.hxx>
 #include <vcl/svapp.hxx>
 #include <vcl/weld.hxx>
+#include <vcl/settings.hxx>
+#include <i18nlangtag/languagetag.hxx>
 #include <comphelper/processfactory.hxx>
 #include <unotools/ZipPackageHelper.hxx>
 #include <unotools/configmgr.hxx>
@@ -83,9 +85,7 @@ SafeModeDialog::SafeModeDialog(weld::Window* pParent)
     RadioBtnHdl(*mxRadioRestore);
 
     // Set URL for help button (module=safemode)
-    OUString sURL("http://hub.libreoffice.org/send-feedback/?LOversion=" + utl::ConfigManager::getAboutBoxProductVersion() +
-        "&LOlocale=" + utl::ConfigManager::getUILocale() + "&LOmodule=safemode");
-    mxBugLink->set_uri(sURL);
+    mxBugLink->set_uri("https://www.allotropia.de/#contact");
 
     mxUserProfileLink->set_uri(comphelper::BackupFileHelper::getUserProfileURL());
 }
