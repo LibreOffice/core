9f2991874f707aa994a49ace3e3c9a0a33543665
Noel Grandin <noelgrandin@gmail.com>
2021-04-17 14:49:43 +0200

    WIP improve the look of the macOS combobox
    Change-Id: I4484fabcf6d883deeb3e417e46ce3973f2332f94

--- vcl/source/control/imp_listbox.cxx
+++ vcl/source/control/imp_listbox.cxx
@@ -496,12 +496,12 @@ void ImplListBoxWindow::ApplySettings(vcl::RenderContext& rRenderContext)
     const StyleSettings& rStyleSettings = rRenderContext.GetSettings().GetStyleSettings();
 
     ApplyControlFont(rRenderContext, rStyleSettings.GetFieldFont());
-    ApplyControlForeground(rRenderContext, rStyleSettings.GetFieldTextColor());
+    ApplyControlForeground(rRenderContext, rStyleSettings.GetListBoxWindowTextColor());
 
     if (IsControlBackground())
         rRenderContext.SetBackground(GetControlBackground());
     else
-        rRenderContext.SetBackground(rStyleSettings.GetFieldColor());
+        rRenderContext.SetBackground(rStyleSettings.GetListBoxWindowBackgroundColor());
 }
 
 void ImplListBoxWindow::ImplCalcMetrics()
@@ -1612,8 +1612,8 @@ void ImplListBoxWindow::ImplPaint(vcl::RenderContext& rRenderContext, sal_Int32 
     bool bSelected = mpEntryList->IsEntryPosSelected(nPos);
     if (bSelected)
     {
-        rRenderContext.SetTextColor(!IsEnabled() ? rStyleSettings.GetDisableColor() : rStyleSettings.GetHighlightTextColor());
-        rRenderContext.SetFillColor(rStyleSettings.GetHighlightColor());
+        rRenderContext.SetTextColor(!IsEnabled() ? rStyleSettings.GetDisableColor() : rStyleSettings.GetListBoxWindowHighlightTextColor());
+        rRenderContext.SetFillColor(rStyleSettings.GetListBoxWindowHighlightColor());
         rRenderContext.SetLineColor();
         rRenderContext.DrawRect(aRect);
     }
