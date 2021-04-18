LibreOffice/core

commit c63148ba139bd6b9ae7a0f9e24e51f29e5370963

Revert "Revert "add some libs to libmerged""
This reverts commit b8077713987253f6db0887ddad56fd6871374a8b.

Reason for revert: 

Change-Id: I09b7d548a674fc8b2991be00616a7e95ec7d1444
sberg:You can probably reverts this again, as <https://ci.libreoffice.org/job/lo_daily_tb_win/162/> now succeeded with <https://gerrit.libreoffice.org/plugins/gitiles/core/+/9ed75e2c65544b4f71c73e1c51a68d74e31d544b%5E!/> "Properly use createOneInstanceComponentFactory for javaloader".
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/90275
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

solenv/gbuild/extensions/pre_MergedLibsList.mk
@@ -83,13 +83,24 @@ MERGE_LIBRARY_LIST := \
 	svx \
 	svxcore \
 	syssh \
+	textfd \
 	tk \
 	tl \
 	ucb1 \
 	ucbhelper \
 	ucpexpand1 \
+	ucpext \
+	ucphier1 \
+	ucpimage \
 	ucpfile1 \
+	ucptdoc1 \
+	unordf \
 	unoxml \
+	$(if $(ENABLE_ONLINE_UPDATE), \
+		updatecheckui \
+        updchk \
+	) \
+	updatefeed \
 	utl \
 	uui \
 	vcl \

commit a6ca747e7e7145c3fe8de9e556c40de836ebe691

tdf#132901: do not add updatecheck to libmerged
LibreOffice crashes if "online update"
feature is not installed

Regression from c63148ba139bd6b9ae7a0f9e24e51f29e5370963
< Revert "Revert "add some libs to libmerged"" >

Change-Id: Ia18f50bc6baf830b2d3e0814645ef04283bfaa04

solenv/gbuild/extensions/pre_MergedLibsList.mk
@@ -92,10 +92,6 @@ MERGE_LIBRARY_LIST := \
 	ucptdoc1 \
 	unordf \
 	unoxml \
-	$(if $(ENABLE_ONLINE_UPDATE), \
-		updatecheckui \
-        updchk \
-	) \
 	updatefeed \
 	utl \
 	uui \