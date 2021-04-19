LibreOffice/core

commit db1d2af02861b49e4f53d726d59cd71c20cee9b1

Resolves: #i123539# some optimizations for 3D chart...
geometry creation using UNO API

(cherry picked from commit bbe35be767d76d08891c4f3d3600d80e0e6805f2)

Change-Id: Ic9b6ab8fc31cfe585f7c75d85bafe4613910c57a

svx/source/sdr/contact/viewcontactofsdrole2obj.cxx
@@ -51,7 +51,8 @@ namespace sdr
         }
 
         ViewContactOfSdrOle2Obj::ViewContactOfSdrOle2Obj(SdrOle2Obj& rOle2Obj)
-        :   ViewContactOfSdrRectObj(rOle2Obj)
+        :   ViewContactOfSdrRectObj(rOle2Obj),
+            mxChartContent()
         {
         }
 
@@ -100,30 +101,45 @@ namespace sdr
 
             if(GetOle2Obj().IsChart())
             {
-                // try to get chart primitives and chart range directly from xChartModel
-                basegfx::B2DRange aChartContentRange;
-                const drawinglayer::primitive2d::Primitive2DSequence aChartSequence(
-                    ChartHelper::tryToGetChartContentAsPrimitive2DSequence(
-                        GetOle2Obj().getXModel(),
-                        aChartContentRange));
-                const double fWidth(aChartContentRange.getWidth());
-                const double fHeight(aChartContentRange.getHeight());
-
-                if(aChartSequence.hasElements()
-                    && basegfx::fTools::more(fWidth, 0.0)
-                    && basegfx::fTools::more(fHeight, 0.0))
+                // #i123539# allow buffering and reuse of local chart data to not need to rebuild it
+                // on every ViewObjectContact::getPrimitive2DSequence call. TTTT: No tneeded for
+                // aw080, there this mechanism alraedy works differently
+                if(mxChartContent.is())
                 {
-                    // create embedding transformation
-                    basegfx::B2DHomMatrix aEmbed(
-                        basegfx::tools::createTranslateB2DHomMatrix(
-                            -aChartContentRange.getMinX(),
-                            -aChartContentRange.getMinY()));
+                    xContent = mxChartContent;
+                }
+                else
+                {
+                    // try to get chart primitives and chart range directly from xChartModel
+                    basegfx::B2DRange aChartContentRange;
+                    const drawinglayer::primitive2d::Primitive2DSequence aChartSequence(
+                        ChartHelper::tryToGetChartContentAsPrimitive2DSequence(
+                            GetOle2Obj().getXModel(),
+                            aChartContentRange));
+                    const double fWidth(aChartContentRange.getWidth());
+                    const double fHeight(aChartContentRange.getHeight());
 
-                    aEmbed.scale(1.0 / fWidth, 1.0 / fHeight);
-                    aEmbed = aObjectMatrix * aEmbed;
-                    xContent = new drawinglayer::primitive2d::TransformPrimitive2D(
-                        aEmbed,
-                        aChartSequence);
+                    if(aChartSequence.hasElements()
+                        && basegfx::fTools::more(fWidth, 0.0)
+                        && basegfx::fTools::more(fHeight, 0.0))
+                    {
+                        // create embedding transformation
+                        basegfx::B2DHomMatrix aEmbed(
+                            basegfx::tools::createTranslateB2DHomMatrix(
+                                -aChartContentRange.getMinX(),
+                                -aChartContentRange.getMinY()));
+
+                        aEmbed.scale(1.0 / fWidth, 1.0 / fHeight);
+                        aEmbed = aObjectMatrix * aEmbed;
+                        xContent = new drawinglayer::primitive2d::TransformPrimitive2D(
+                            aEmbed,
+                            aChartSequence);
+                    }
+
+                    if(xContent.is())
+                    {
+                        const_cast< ViewContactOfSdrOle2Obj* >(this)->mxChartContent = xContent;
+                    }
                 }
             }
 
@@ -152,6 +168,18 @@ namespace sdr
                     aAttribute));
 
             return drawinglayer::primitive2d::Primitive2DSequence(&xReference, 1);
+        }
+
+        void ViewContactOfSdrOle2Obj::ActionChanged()
+        {
+            // call parent
+            ViewContactOfSdrRectObj::ActionChanged();
+
+            // #i123539# if we have buffered chart data, reset it
+            if(mxChartContent.is())
+            {
+                mxChartContent.clear();
+            }
         }
 
         drawinglayer::primitive2d::Primitive2DSequence ViewContactOfSdrOle2Obj::createViewIndependentPrimitive2DSequence() const

svx/source/unodraw/unoshape.cxx
@@ -1253,7 +1253,15 @@ void SAL_CALL SvxShape::setSize( const awt::Size& rSize )
 
     if( mpObj.is() && mpModel)
     {
-        Rectangle aRect( svx_getLogicRectHack(mpObj.get()) );
+        // #i123539# optimization for 3D chart object generation: do not use UNO
+        // API commmands to get the range, this is too expensive since for 3D
+        // scenes it may recalculate the whole scene since in AOO this depends
+        // on the contained geometry (layouted to show all content)
+        const bool b3DConstruction(dynamic_cast< E3dObject* >(mpObj.get()) && mpModel->isLocked());
+        Rectangle aRect(
+            b3DConstruction ?
+                Rectangle(maPosition.X, maPosition.Y, maSize.Width, maSize.Height) :
+                svx_getLogicRectHack(mpObj.get()) );
         Size aLocalSize( rSize.Width, rSize.Height );
         ForceMetricToItemPoolMetric(aLocalSize);
 

include/svx/sdr/contact/viewcontactofsdrole2obj.hxx
@@ -37,6 +37,10 @@ namespace sdr
     {
         class ViewContactOfSdrOle2Obj : public ViewContactOfSdrRectObj
         {
+        private:
+            // #i123539# allow local buffering of chart data (if chart)
+            drawinglayer::primitive2d::Primitive2DReference mxChartContent;
+
         protected:
             // Create a Object-Specific ViewObjectContact, set ViewContact and
             // ObjectContact. Always needs to return something.
@@ -61,6 +65,9 @@ namespace sdr
             // createViewIndependentPrimitive2DSequence with false, and with evtl. HighContrast true
             // from the VOC which knows that
             drawinglayer::primitive2d::Primitive2DSequence createPrimitive2DSequenceWithParameters() const;
+
+            // #i123539# get rid of buffered chart content (if there) on change
+            virtual void ActionChanged();
 
         protected:
             // This method is responsible for creating the graphical visualisation data

svx/source/engine3d/scene3d.cxx
@@ -45,6 +45,7 @@
 #include <drawinglayer/geometry/viewinformation3d.hxx>
 #include <basegfx/polygon/b2dpolypolygontools.hxx>
 #include <svx/e3dsceneupdater.hxx>
+#include <svx/svdmodel.hxx>
 
 //////////////////////////////////////////////////////////////////////////////
 
@@ -379,7 +380,14 @@ void E3dScene::NewObjectInserted(const E3dObject* p3DObj)
 void E3dScene::StructureChanged()
 {
     E3dObject::StructureChanged();
-    SetRectsDirty();
+
+    if(!GetModel() || !GetModel()->isLocked())
+    {
+        // #i123539# optimization for 3D chart object generation: do not reset
+        // already calculated scene projection data every time an object gets
+        // initialized
+        SetRectsDirty();
+    }
 
     ImpCleanup3DDepthMapper();
 }

commit 1b057549ebfefacdd35a59716b5e76299bf33556

tdf#86321: Revert "Resolves: #i123539# some optimizations for 3D chart..."
This reverts commit db1d2af02861b49e4f53d726d59cd71c20cee9b1

The commit was already partically reverted in
bca8d8985666d3fd22b91eb280f9baaeb933c2d0 < Resolves: tdf#90583
3D pie-chart missing guide handles >.
This partial revert broke the optimization introduced in
db1d2af02861b49e4f53d726d59cd71c20cee9b1 <Resolves: #i123539#
some optimizations for 3D chart...> for 3D charts like
https://bz.apache.org/ooo/attachment.cgi?id=81810

Later, 3D charts were optimized again in
55a7e836a2db662a53adc4f8b98d08b06790c758 < chart2: When
creating objects prevent setting object rects dirty > so
the remaining code originally introduced can go.

Import time of https://bz.apache.org/ooo/attachment.cgi?id=81810
is the same with and without this patch

Change-Id: I25338d0dfb4b41651dfe05e7bfbd74c86091dacb

svx/inc/sdr/contact/viewcontactofsdrole2obj.hxx
@@ -31,9 +31,6 @@ namespace sdr::contact
 class ViewContactOfSdrOle2Obj final : public ViewContactOfSdrRectObj
 {
 private:
-    // #i123539# allow local buffering of chart data (if chart)
-    drawinglayer::primitive2d::Primitive2DReference mxChartContent;
-
     // Create an Object-Specific ViewObjectContact, set ViewContact and
     // ObjectContact. Always needs to return something.
     virtual ViewObjectContact&
@@ -55,9 +52,6 @@ public:
     // createViewIndependentPrimitive2DSequence with false, and with evtl. HighContrast true
     // from the VOC which knows that
     drawinglayer::primitive2d::Primitive2DContainer createPrimitive2DSequenceWithParameters() const;
-
-    // #i123539# get rid of buffered chart content (if there) on change
-    virtual void ActionChanged() override;
 
     virtual basegfx::B2DRange
     getRange(const drawinglayer::geometry::ViewInformation2D& rViewInfo2D) const override;

svx/source/sdr/contact/viewcontactofsdrole2obj.cxx
@@ -45,8 +45,7 @@ ViewObjectContact& ViewContactOfSdrOle2Obj::CreateObjectSpecificViewObjectContac
 }
 
 ViewContactOfSdrOle2Obj::ViewContactOfSdrOle2Obj(SdrOle2Obj& rOle2Obj)
-:   ViewContactOfSdrRectObj(rOle2Obj),
-    mxChartContent()
+:   ViewContactOfSdrRectObj(rOle2Obj)
 {
 }
 
@@ -90,45 +89,30 @@ drawinglayer::primitive2d::Primitive2DContainer ViewContactOfSdrOle2Obj::createP
 
     if(GetOle2Obj().IsChart())
     {
-        // #i123539# allow buffering and reuse of local chart data to not need to rebuild it
-        // on every ViewObjectContact::getPrimitive2DSequence call. TTTT: Not needed for
-        // aw080, there this mechanism already works differently
-        if(mxChartContent.is())
+        // try to get chart primitives and chart range directly from xChartModel
+        basegfx::B2DRange aChartContentRange;
+        const drawinglayer::primitive2d::Primitive2DContainer aChartSequence(
+            ChartHelper::tryToGetChartContentAsPrimitive2DSequence(
+                GetOle2Obj().getXModel(),
+                aChartContentRange));
+        const double fWidth(aChartContentRange.getWidth());
+        const double fHeight(aChartContentRange.getHeight());
+
+        if(!aChartSequence.empty()
+            && basegfx::fTools::more(fWidth, 0.0)
+            && basegfx::fTools::more(fHeight, 0.0))
         {
-            xContent = mxChartContent;
-        }
-        else
-        {
-            // try to get chart primitives and chart range directly from xChartModel
-            basegfx::B2DRange aChartContentRange;
-            const drawinglayer::primitive2d::Primitive2DContainer aChartSequence(
-                ChartHelper::tryToGetChartContentAsPrimitive2DSequence(
-                    GetOle2Obj().getXModel(),
-                    aChartContentRange));
-            const double fWidth(aChartContentRange.getWidth());
-            const double fHeight(aChartContentRange.getHeight());
+            // create embedding transformation
+            basegfx::B2DHomMatrix aEmbed(
+                basegfx::utils::createTranslateB2DHomMatrix(
+                    -aChartContentRange.getMinX(),
+                    -aChartContentRange.getMinY()));
 
-            if(!aChartSequence.empty()
-                && basegfx::fTools::more(fWidth, 0.0)
-                && basegfx::fTools::more(fHeight, 0.0))
-            {
-                // create embedding transformation
-                basegfx::B2DHomMatrix aEmbed(
-                    basegfx::utils::createTranslateB2DHomMatrix(
-                        -aChartContentRange.getMinX(),
-                        -aChartContentRange.getMinY()));
-
-                aEmbed.scale(1.0 / fWidth, 1.0 / fHeight);
-                aEmbed = aObjectMatrix * aEmbed;
-                xContent = new drawinglayer::primitive2d::TransformPrimitive2D(
-                    aEmbed,
-                    aChartSequence);
-            }
-
-            if(xContent.is())
-            {
-                const_cast< ViewContactOfSdrOle2Obj* >(this)->mxChartContent = xContent;
-            }
+            aEmbed.scale(1.0 / fWidth, 1.0 / fHeight);
+            aEmbed = aObjectMatrix * aEmbed;
+            xContent = new drawinglayer::primitive2d::TransformPrimitive2D(
+                aEmbed,
+                aChartSequence);
         }
     }
 
@@ -183,18 +167,6 @@ basegfx::B2DRange ViewContactOfSdrOle2Obj::getRange( const drawinglayer::geometr
             aAttribute));
 
     return drawinglayer::primitive2d::getB2DRangeFromPrimitive2DReference(xReference, rViewInfo2D);
-}
-
-void ViewContactOfSdrOle2Obj::ActionChanged()
-{
-    // call parent
-    ViewContactOfSdrRectObj::ActionChanged();
-
-    // #i123539# if we have buffered chart data, reset it
-    if(mxChartContent.is())
-    {
-        mxChartContent.clear();
-    }
 }
 
 drawinglayer::primitive2d::Primitive2DContainer ViewContactOfSdrOle2Obj::createViewIndependentPrimitive2DSequence() const

svx/source/unodraw/unoshape.cxx
@@ -1197,15 +1197,7 @@ void SAL_CALL SvxShape::setSize( const awt::Size& rSize )
 
     if(HasSdrObject())
     {
-        // #i123539# optimization for 3D chart object generation: do not use UNO
-        // API commands to get the range, this is too expensive since for 3D
-        // scenes it may recalculate the whole scene since in AOO this depends
-        // on the contained geometry (layouted to show all content)
-        const bool b3DConstruction(dynamic_cast< E3dObject* >(GetSdrObject()) && GetSdrObject()->getSdrModelFromSdrObject().isLocked());
-        tools::Rectangle aRect(
-            b3DConstruction ?
-                tools::Rectangle(maPosition.X, maPosition.Y, maSize.Width, maSize.Height) :
-                svx_getLogicRectHack(GetSdrObject()) );
+        tools::Rectangle aRect( svx_getLogicRectHack(GetSdrObject()) );
         Size aLocalSize( rSize.Width, rSize.Height );
         ForceMetricToItemPoolMetric(aLocalSize);
 