53f43ae41de7315c58c9d329d7549bf7f28c6dfb
László Németh <nemeth@numbertext.org>
2021-04-21 15:42:37 +0200

    add preset dash styles with round cap for interoperability
    New OOXML-compatible preset styles weren't recognized,
    if the preset styles use round cap, e.g. line styles
    with dot-like dots (i.e. not tiny squares as dots).
    
    As a workaround, add "Rounded" version for every
    OOXML-compatible preset styles with round cap. This
    allows to apply "real" dotted lines to new shapes, too.
    
    Background: round cap modifies the DotLen and DashLen
    values of the LineDash struct during the OOXML import,
    using ~zero values to get dot-like dots. For the details,
    see commit 3f3b50015e4fd9efc3459612a70409fca49cf390
    "tdf#134053 tweak dash and space length for ooxml".
    
    Follow of commit 183c06fc02a50fb117bb6162e4d6e56cdd34fad1
    "tdf#139301 fix OOXML-compatible preset dash styles".
    
    Change-Id: I4f3173579964b2c00618ada475b012c85320f758

--- sw/qa/uitest/writer_tests7/tdf139301.py
+++ sw/qa/uitest/writer_tests7/tdf139301.py
@@ -9,12 +9,12 @@ from uitest.uihelper.common import get_state_as_dict, get_url_for_data_file
 
 class tdf139301(UITestCase):
 
+    styles = ('Long Dash', 'Long Dash Dot', 'Long Dot', 'Double Dash', 'Double Dash Dot', 'Double Dash Dot Dot', 'Dash', 'Dash Dot', 'Dash Dot Dot', 'Dot')
+
     def test_tdf139301(self):
         writer_doc = self.ui_test.load_file(get_url_for_data_file("tdf127166_prstDash_Word97.docx"))
 
-        styles = ('Long Dash', 'Long Dash Dot', 'Long Dot', 'Double Dash', 'Double Dash Dot', 'Double Dash Dot Dot', 'Dash', 'Dash Dot', 'Dash Dot Dot', 'Dot')
-
-        for i in range(len(styles)):
+        for i in range(len(self.styles)):
             # select next line shape
             writer_doc.getCurrentController().select(writer_doc.getDrawPage()[i])
 
@@ -33,7 +33,35 @@ class tdf139301(UITestCase):
             xOKBtn = xFormatLineDlg.getChild("ok")
             self.ui_test.close_dialog_through_button(xOKBtn)
 
-            self.assertEqual(style, styles[i])
+            self.assertEqual(style, self.styles[i])
+
+        self.ui_test.close_doc()
+
+    def test_round_cap(self):
+        writer_doc = self.ui_test.load_file(get_url_for_data_file("tdf127166_prstDash_round_cap.docx"))
+
+        style_name_extension = ' (Rounded)'
+
+        for i in range(len(self.styles)):
+            # select next line shape
+            writer_doc.getCurrentController().select(writer_doc.getDrawPage()[i])
+
+            # wait for available line style setting
+            self.ui_test.wait_until_child_is_available('metricfield')
+
+            # line setting dialog window
+            self.ui_test.execute_dialog_through_command(".uno:FormatLine")
+            xFormatLineDlg = self.xUITest.getTopFocusWindow()
+            # get line style combo box
+            xLineStyle = xFormatLineDlg.getChild("LB_LINE_STYLE")
+
+            # check preset line style
+            style = get_state_as_dict(xLineStyle)['SelectEntryText']
+
+            xOKBtn = xFormatLineDlg.getChild("ok")
+            self.ui_test.close_dialog_through_button(xOKBtn)
+
+            self.assertEqual(style, self.styles[i] + style_name_extension)
 
         self.ui_test.close_doc()
 
