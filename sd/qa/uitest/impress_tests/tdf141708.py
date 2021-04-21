df423060abf34002e28a758f92336540fa7492e8
Xisco Fauli <xiscofauli@libreoffice.org>
2021-04-21 11:07:09 +0200

    tdf#141708: sd: Add UItest
    Change-Id: I80c7ad4fe2934c9cb19b4b31eb8ca73635c5fdd5

---
+++ sd/qa/uitest/impress_tests/tdf141708.py
@@ -0,0 +1,32 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+from uitest.framework import UITestCase
+
+class tdf141708(UITestCase):
+
+    def test_tdf141708(self):
+
+        self.ui_test.create_doc_in_start_center("impress")
+
+        xTemplateDlg = self.xUITest.getTopFocusWindow()
+        xCancelBtn = xTemplateDlg.getChild("close")
+        self.ui_test.close_dialog_through_button(xCancelBtn)
+
+        self.xUITest.executeCommand(".uno:Navigator")
+
+        self.xUITest.executeCommand(".uno:CloseDoc")
+
+        self.ui_test.create_doc_in_start_center("impress")
+
+        xTemplateDlg = self.xUITest.getTopFocusWindow()
+        xCancelBtn = xTemplateDlg.getChild("close")
+        self.ui_test.close_dialog_through_button(xCancelBtn)
+
+        # Without the fix in place, this test would have crashed here
+        self.ui_test.close_doc()
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
