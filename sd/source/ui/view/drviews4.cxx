LibreOffice/core

commit 67ad205404211a2ae17c430a17ede6e9d04d0b7e

tdf#139804 Focus form controls inside document with Alt-<Mnemonic>
Change-Id: I710a23a53ca0eb256a477a78ce1b7ae01129f717
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/111865
Tested-by: Jenkins
Reviewed-by: Samuel Mehrbrodt <samuel.mehrbrodt@allotropia.de>

sd/source/ui/view/drviews4.cxx
@@ -148,9 +148,9 @@ void DrawViewShell::DeleteActualLayer()
 
 bool DrawViewShell::KeyInput (const KeyEvent& rKEvt, ::sd::Window* pWin)
 {
-    bool bRet = false;
+    bool bRet = GetView()->KeyInput(rKEvt, pWin);
 
-    if ( !IsInputLocked() || ( rKEvt.GetKeyCode().GetCode() == KEY_ESCAPE ) )
+    if (!bRet && (!IsInputLocked() || (rKEvt.GetKeyCode().GetCode() == KEY_ESCAPE)))
     {
         if(KEY_RETURN == rKEvt.GetKeyCode().GetCode()
             && rKEvt.GetKeyCode().IsMod1()

sc/source/ui/inc/gridwin.hxx
@@ -260,7 +260,7 @@ class SAL_DLLPUBLIC_RTTI ScGridWindow : public vcl::Window, public DropTargetHel
     bool            DrawMouseButtonDown(const MouseEvent& rMEvt);
     bool            DrawMouseButtonUp(const MouseEvent& rMEvt);
     bool            DrawMouseMove(const MouseEvent& rMEvt);
-    bool            DrawKeyInput(const KeyEvent& rKEvt);
+    bool            DrawKeyInput(const KeyEvent& rKEvt, vcl::Window* pWin);
     bool            DrawCommand(const CommandEvent& rCEvt);
     bool            DrawHasMarkedObj();
     void            DrawEndAction();

sc/source/ui/view/gridwin.cxx
@@ -3250,7 +3250,7 @@ void ScGridWindow::KeyInput(const KeyEvent& rKEvt)
         if (mrViewData.GetDocShell()->GetProgress())
             return;
 
-        if (DrawKeyInput(rKEvt))
+        if (DrawKeyInput(rKEvt, this))
         {
             const vcl::KeyCode& rLclKeyCode = rKEvt.GetKeyCode();
             if (rLclKeyCode.GetCode() == KEY_DOWN

sc/source/ui/view/gridwin3.cxx
@@ -167,10 +167,15 @@ bool ScGridWindow::DrawCommand(const CommandEvent& rCEvt)
     return false;
 }
 
-bool ScGridWindow::DrawKeyInput(const KeyEvent& rKEvt)
+bool ScGridWindow::DrawKeyInput(const KeyEvent& rKEvt, vcl::Window* pWin)
 {
     ScDrawView* pDrView = mrViewData.GetScDrawView();
     FuPoor* pDraw = mrViewData.GetView()->GetDrawFuncPtr();
+
+
+    if (pDrView && pDrView->KeyInput(rKEvt, pWin))
+        return true;
+
     if (pDrView && pDraw && !mrViewData.IsRefMode())
     {
         pDraw->SetWindow( this );

svx/source/form/fmview.cxx
@@ -41,11 +41,14 @@
 #include <fmundo.hxx>
 #include <svx/dataaccessdescriptor.hxx>
 #include <comphelper/namedvaluecollection.hxx>
+#include <toolkit/helper/vclunohelper.hxx>
 #include <com/sun/star/lang/XServiceInfo.hpp>
+#include <com/sun/star/awt/XControl.hpp>
 #include <tools/debug.hxx>
 #include <svx/sdrpagewindow.hxx>
 #include <svx/sdrpaintwindow.hxx>
 #include <svx/svxids.hrc>
+#include <vcl/i18nhelp.hxx>
 
 using namespace ::com::sun::star;
 using namespace ::com::sun::star::uno;
@@ -491,6 +494,31 @@ bool FmFormView::KeyInput(const KeyEvent& rKEvt, vcl::Window* pWin)
             pFormShell->GetImpl()->handleShowPropertiesRequest_Lock();
         }
 
+    }
+
+    // tdf#139804 Allow selecting form controls with Alt-<Mnemonic>
+    if (rKeyCode.IsMod2() && rKeyCode.GetCode())
+    {
+        FmFormPage* pCurPage = GetCurPage();
+        for (size_t a = 0; a < pCurPage->GetObjCount(); ++a)
+        {
+            SdrObject* pObj = pCurPage->GetObj(a);
+            FmFormObj* pFormObject = FmFormObj::GetFormObject(pObj);
+            if (!pFormObject)
+                continue;
+
+            Reference<awt::XControl> xControl = pFormObject->GetUnoControl(*this, *pWin);
+            if (!xControl.is())
+                continue;
+            const vcl::I18nHelper& rI18nHelper = Application::GetSettings().GetUILocaleI18nHelper();
+            VclPtr<vcl::Window> pWindow = VCLUnoHelper::GetWindow(xControl->getPeer());
+            if (rI18nHelper.MatchMnemonic(pWindow->GetText(), rKEvt.GetCharCode()))
+            {
+                pWindow->GrabFocus();
+                bDone = true;
+                break;
+            }
+        }
     }
 
     if ( !bDone )

commit 6e15e0a06772d238cd74ffc3620b56295cdf6073

tdf#141703 Restore tab function in sd tables
Regression from 67ad205404211a2ae17c430a17ede6e9d04d0b7e

Change-Id: Iadbdaf1b59d2481264e45b85aed248c84404c26b

sd/source/ui/view/drviews4.cxx
@@ -177,9 +177,9 @@ void DrawViewShell::DeleteActualLayer()
 
 bool DrawViewShell::KeyInput (const KeyEvent& rKEvt, ::sd::Window* pWin)
 {
-    bool bRet = GetView()->KeyInput(rKEvt, pWin);
+    bool bRet = false;
 
-    if (!bRet && (!IsInputLocked() || (rKEvt.GetKeyCode().GetCode() == KEY_ESCAPE)))
+    if (!IsInputLocked() || (rKEvt.GetKeyCode().GetCode() == KEY_ESCAPE))
     {
         if(KEY_RETURN == rKEvt.GetKeyCode().GetCode()
             && rKEvt.GetKeyCode().IsMod1()
@@ -255,6 +255,9 @@ bool DrawViewShell::KeyInput (const KeyEvent& rKEvt, ::sd::Window* pWin)
             }
         }
     }
+
+    if (!bRet)
+        bRet = GetView()->KeyInput(rKEvt, pWin);
 
     return bRet;
 }