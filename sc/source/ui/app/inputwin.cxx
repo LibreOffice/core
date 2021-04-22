13b2dcbaf0e37e1e6c8f99b11fc35e385a7ee925
Szymon Kłos <szymon.klos@collabora.com>
2021-04-22 10:16:55 +0200

    Revert ScInputWindow::setPosSizePixel() early returns
    Revert changes that were introduced to fix CollaboraOnline/online#1988
    because it should be fixed in online
    
    The root cause was that online was sending "resizewindow"
    even when size was not changed what was triggering many times
    ScInputWindow::setPosSizePixel(). (before online fix "resizewindow"
    message was sent on every sidebar refresh)
    
    "Fix early return in ScInputWindow::setPosSizePixel"
    reverts commit a9e0bbadb151d6cfe652fad1d49147f559a6d83c.
    
    "Early return from ScInputWindow::setPosSizePixel() if no-op"
    reverts commit 0ad467d9abd27197bc5f7f22db5a25b3246e6808.
    
    Change-Id: Iad1c3d427a5feefdbb2bac912710847addf4ee02

--- sc/source/ui/app/inputwin.cxx
+++ sc/source/ui/app/inputwin.cxx
@@ -170,8 +170,7 @@ ScInputWindow::ScInputWindow( vcl::Window* pParent, const SfxBindings* pBind ) :
         mpViewShell     ( nullptr ),
         mnMaxY          (0),
         bIsOkCancelMode ( false ),
-        bInResize       ( false ),
-        nOldOutOffYPixel( GetOutOffYPixel() )
+        bInResize       ( false )
 {
     // #i73615# don't rely on SfxViewShell::Current while constructing the input line
     // (also for GetInputHdl below)
@@ -491,11 +490,6 @@ void ScInputWindow::SetSizePixel( const Size& rNewSize )
 void ScInputWindow::setPosSizePixel(long nX, long nY, long nWidth, long nHeight, PosSizeFlags nFlags)
 {
     ToolBox::setPosSizePixel(nX, nY, nWidth, nHeight, nFlags);
-
-    // send update only when position changed eg. when notebookbar was opened
-    if (nOldOutOffYPixel == GetOutOffYPixel())
-        return;
-
     if (const vcl::ILibreOfficeKitNotifier* pNotifier = GetLOKNotifier())
     {
         std::vector<vcl::LOKPayloadItem> aItems;
@@ -504,8 +498,6 @@ void ScInputWindow::setPosSizePixel(long nX, long nY, long nWidth, long nHeight,
         aItems.emplace_back("lines", OString::number(aTextWindow.GetNumLines()));
         pNotifier->notifyWindow(GetLOKWindowId(), "size_changed", aItems);
     }
-
-    nOldOutOffYPixel = GetOutOffYPixel();
 }
 
 void ScInputWindow::Resize()
