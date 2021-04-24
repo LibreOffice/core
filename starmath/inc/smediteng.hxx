ffe71943f0f0564304cc3e6780d51da20c369734
dante <dante19031999@gmail.com>
2021-04-25 00:26:38 +0200

    tdf#130654 editable smeditwindow zoom
    Setting only accessible from advanced configuration.
    Access: org.openoffice.Office.Math.smeditwindowzoom
    
    Note that the change won't take immediate effect.
    LibreOffice has to be restarted to the change to take effect.
    Math configuration entries are not synced correctly.
    More detailed explanation can be fond in tdf#132145.
    
    Change-Id: I0017ddda73ab2916c6ad1fa238265d57a2a3020e

---
+++ starmath/inc/smediteng.hxx
@@ -0,0 +1,60 @@
+/* This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#pragma once
+
+#include <editeng/editeng.hxx>
+#include <editeng/fontitem.hxx>
+#include <unotools/lingucfg.hxx>
+
+class SmEditEngine : public EditEngine
+{
+public:
+    SmEditEngine(SfxItemPool* pItemPool);
+    SmEditEngine(const SmEditEngine&) = delete;
+
+public:
+    /**
+      * Runs checkZoom and if true runs updateZoom
+      */
+    void executeZoom(EditView* pEditView = nullptr);
+
+    /**
+      * Sets up default font parameters for the item pool.
+      */
+    static void setSmItemPool(SfxItemPool* mpItemPool, const SvtLinguOptions& maLangOptions);
+
+    // Deal with text scaling
+private:
+    sal_Int32 m_nOldZoom;
+    sal_Int32 m_nNewZoom;
+    sal_Int32 m_nDefaultFontSize;
+
+    /**
+      * Checks if the zoom of smeditwindow has changed.
+      * m_nNewZoom is updated.
+      * @return zoom has changed
+      */
+    bool checkZoom();
+
+    /**
+      * Updates the zoom of smeditwindow.
+      * m_nOldZoom is set to m_nNewZoom.
+      */
+
+    void updateZoom();
+
+    // Gather information for more complex tasks
+private:
+    ESelection m_aAllSelection;
+
+    /**
+      * Finds the ESelection wich contains all the text.
+      */
+    void updateAllESelection();
+};
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
