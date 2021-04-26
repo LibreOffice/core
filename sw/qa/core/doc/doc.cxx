LibreOffice/core

commit 682e0488df819c191c13a03758fad0690706e508

tdf#134099 sw: fix textbox anchors on copy-paste and undo
Regression from commit c7307c77254557646f33017af915f6808a861e29
(fdo#82191 sw::DocumentLayoutManager: copy textbox content of draw
formats, 2014-08-15), without which this problem gets hidden, as copy
breaks the textbox into 2 pieces, so the textbox codepaths are no longer
hit.

The direct problem is that SwHistorySetFormat::SetInDoc() uses a raw
node index into the nodes array, which is past the end of the nodes
array.

Root cause is that we have this invariant that actions and their undo
has to be in sync, otherwise raw node indexes no longer work. In this
case, SwUndoSaveContent::DelContentIndex() did not delete a fly frame
format, because it was out of range, as it had a wrong anchor.

Fix this in SwTextFlyCnt::SetAnchor(), so that whenever the anchor of a
draw format is set via that function, we update its textbox as well.

Also fix a related problem when fly formats were copied twice.

Change-Id: I0d6c9069544c405eb20c5fed65fb40423b0adc84
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/97457
Tested-by: Jenkins
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

sw/source/core/txtnode/atrflyin.cxx
@@ -33,6 +33,7 @@
 #include <objectformatter.hxx>
 #include <calbck.hxx>
 #include <dcontact.hxx>
+#include <textboxhelper.hxx>
 
 SwFormatFlyCnt::SwFormatFlyCnt( SwFrameFormat *pFrameFormat )
     : SfxPoolItem( RES_TXTATR_FLYCNT ),
@@ -200,6 +201,15 @@ void SwTextFlyCnt::SetAnchor( const SwTextNode *pNode )
             }
         }
         pFormat->SetFormatAttr( aAnchor );  // only set the anchor
+
+        // If the draw format has a TextBox, then set its anchor as well.
+        if (SwFrameFormat* pTextBox
+            = SwTextBoxHelper::getOtherTextBoxFormat(pFormat, RES_DRAWFRMFMT))
+        {
+            SwFormatAnchor aTextBoxAnchor(pTextBox->GetAnchor());
+            aTextBoxAnchor.SetAnchor(aAnchor.GetContentAnchor());
+            pTextBox->SetFormatAttr(aTextBoxAnchor);
+        }
     }
 
     // The node may have several SwTextFrames - for every SwTextFrame a

sw/qa/core/txtnode/txtnode.cxx
@@ -11,6 +11,10 @@
 
 #include <vcl/gdimtf.hxx>
 
+#include <fmtanchr.hxx>
+#include <frameformats.hxx>
+#include <wrtsh.hxx>
+
 static char const DATA_DIRECTORY[] = "/sw/qa/core/txtnode/data/";
 
 /// Covers sw/source/core/txtnode/ fixes.
@@ -36,6 +40,34 @@ CPPUNIT_TEST_FIXTURE(SwCoreTxtnodeTest, testBtlrCellChinese)
     // - Actual  : true
     // i.e. the glyph was rotated further, so it was upside down.
     assertXPath(pXmlDoc, "//font[1]", "vertical", "false");
+}
+
+CPPUNIT_TEST_FIXTURE(SwCoreTxtnodeTest, testTextBoxCopyAnchor)
+{
+    load(DATA_DIRECTORY, "textbox-copy-anchor.docx");
+    SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument*>(mxComponent.get());
+    SwDocShell* pShell = pTextDoc->GetDocShell();
+    SwWrtShell* pWrtShell = pShell->GetWrtShell();
+    SwDoc aClipboard;
+    pWrtShell->SelAll();
+    pWrtShell->Copy(&aClipboard);
+    pWrtShell->SttEndDoc(/*bStart=*/false);
+    pWrtShell->Paste(&aClipboard);
+
+    const SwFrameFormats& rFormats = *pShell->GetDoc()->GetSpzFrameFormats();
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 4
+    // - Actual  : 6
+    // i.e. 2 fly frames were copied twice.
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(4), rFormats.size());
+
+    SwPosition aDrawAnchor1 = *rFormats[0]->GetAnchor().GetContentAnchor();
+    SwPosition aFlyAnchor1 = *rFormats[1]->GetAnchor().GetContentAnchor();
+    CPPUNIT_ASSERT_EQUAL(aFlyAnchor1.nNode, aDrawAnchor1.nNode);
+    SwPosition aDrawAnchor2 = *rFormats[2]->GetAnchor().GetContentAnchor();
+    SwPosition aFlyAnchor2 = *rFormats[3]->GetAnchor().GetContentAnchor();
+    // This also failed, aFlyAnchor2 was wrong, as it got out of sync with aDrawAnchor2.
+    CPPUNIT_ASSERT_EQUAL(aFlyAnchor2.nNode, aDrawAnchor2.nNode);
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();

sw/source/core/doc/DocumentContentOperationsManager.cxx
@@ -3788,6 +3788,14 @@ void DocumentContentOperationsManager::CopyFlyInFlyImpl(
             }
         }
 
+        // Ignore TextBoxes, they are already handled in
+        // sw::DocumentLayoutManager::CopyLayoutFormat().
+        if (SwTextBoxHelper::isTextBox(it->GetFormat(), RES_FLYFRMFMT))
+        {
+            it = aSet.erase(it);
+            continue;
+        }
+
         // Copy the format and set the new anchor
         aVecSwFrameFormat.push_back( pDest->getIDocumentLayoutAccess().CopyLayoutFormat( *(*it).GetFormat(),
                 aAnchor, false, true ) );

commit 641a529f8ca267ed59f9a0dff6d70499aba21b48

tdf#138604 sw textbox copy: fix missing fly frames of as-char draw frame
Regression from commit 682e0488df819c191c13a03758fad0690706e508
(tdf#134099 sw: fix textbox anchors on copy-paste and undo, 2020-06-29),
now that we don't copy draw+fly format pairs more than once, the fly frame
of a fly format was missing.

This was broken because
sw::DocumentContentOperationsManager::CopyFlyInFlyImpl() calls
CopyLayoutFormat() with bMakeFrames=true, but the frame format list
doesn't contain as-char frames.

For as-char frames CopyLayoutFormat() is called by
SwTextFlyCnt::CopyFlyFormat() instead, but with bMakeFrames=false, so
the fly frame's layout frame were never made.

Fix the problem by explicitly making frames in
DocumentLayoutManager::CopyLayoutFormat(), which also requires
delete+make in SwTextFlyCnt::SetAnchor(). [ This is now consistent with
the start of SwTextFlyCnt::SetAnchor(), which also deletes frames for
fly frame formats. Otherwise CppunitTest_sw_core_txtnode would crash in
testTextBoxCopyAnchor. ]

Change-Id: I87003ee09ca75b9fecd70a1aa5c42f762f715be8

sw/source/core/doc/DocumentLayoutManager.cxx
@@ -474,6 +474,14 @@ SwFrameFormat *DocumentLayoutManager::CopyLayoutFormat(
         // presumably these anchors are supported though not sure
         assert(RndStdIds::FLY_AT_CHAR == boxAnchor.GetAnchorId() || RndStdIds::FLY_AT_PARA == boxAnchor.GetAnchorId()
         || boxAnchor.GetAnchorId() == RndStdIds::FLY_AT_PAGE);
+
+        if (!bMakeFrames && rNewAnchor.GetAnchorId() == RndStdIds::FLY_AS_CHAR)
+        {
+            // If the draw format is as-char, then it will be copied with bMakeFrames=false, but
+            // doing the same for the fly format would result in not making fly frames at all.
+            bMakeFrames = true;
+        }
+
         SwFrameFormat* pDestTextBox = CopyLayoutFormat(*pSourceTextBox,
                 boxAnchor, bSetTextFlyAtt, bMakeFrames);
         SwAttrSet aSet(pDest->GetAttrSet());

sw/source/core/txtnode/atrflyin.cxx
@@ -218,6 +218,11 @@ void SwTextFlyCnt::SetAnchor( const SwTextNode *pNode )
             {
                 pTextBox->LockModify();
             }
+            else
+            {
+                // Otherwise delete fly frames on anchor change.
+                pTextBox->DelFrames();
+            }
 
             pTextBox->SetFormatAttr(aTextBoxAnchor);
 
@@ -226,6 +231,10 @@ void SwTextFlyCnt::SetAnchor( const SwTextNode *pNode )
                 pOldNode->RemoveAnchoredFly(pTextBox);
                 aPos.nNode.GetNode().AddAnchoredFly(pTextBox);
                 pTextBox->UnlockModify();
+            }
+            else
+            {
+                pTextBox->MakeFrames();
             }
         }
     }

sw/qa/core/doc/doc.cxx
@@ -16,6 +16,8 @@
 #include <vcl/errinf.hxx>
 #include <vcl/event.hxx>
 #include <editeng/langitem.hxx>
+#include <sfx2/viewfrm.hxx>
+#include <sfx2/dispatch.hxx>
 
 #include <wrtsh.hxx>
 #include <fmtanchr.hxx>
@@ -24,6 +26,9 @@
 #include <edtwin.hxx>
 #include <view.hxx>
 #include <ndtxt.hxx>
+#include <swdtflvr.hxx>
+#include <cmdid.h>
+#include <unotxdoc.hxx>
 
 constexpr OUStringLiteral DATA_DIRECTORY = u"/sw/qa/core/doc/data/";
 
@@ -127,6 +132,37 @@ CPPUNIT_TEST_FIXTURE(SwCoreDocTest, testTextBoxZOrder)
     // - Actual  : 1
     // i.e. the ellipse was under the frame of the shape-frame pair, not on top of it.
     CPPUNIT_ASSERT_EQUAL(static_cast<sal_uInt32>(2), pEllipseShape->GetOrdNum());
+}
+
+CPPUNIT_TEST_FIXTURE(SwCoreDocTest, testTextBoxMakeFlyFrame)
+{
+    // Given a document with an as-char textbox (as-char draw format + at-char fly format):
+    SwDoc* pDoc = createSwDoc(DATA_DIRECTORY, "textbox-makeflyframe.docx");
+
+    // When cutting the textbox and pasting it to a new document:
+    SwView* pView = pDoc->GetDocShell()->GetView();
+    pView->GetViewFrame()->GetDispatcher()->Execute(FN_CNTNT_TO_NEXT_FRAME, SfxCallMode::SYNCHRON);
+    pView->StopShellTimer();
+    SwDocShell* pDocShell = pDoc->GetDocShell();
+    SwWrtShell* pWrtShell = pDocShell->GetWrtShell();
+    rtl::Reference<SwTransferable> pTransfer = new SwTransferable(*pWrtShell);
+    pTransfer->Cut();
+    TransferableDataHelper aHelper(pTransfer);
+    uno::Reference<lang::XComponent> xDoc2
+        = loadFromDesktop("private:factory/swriter", "com.sun.star.text.TextDocument", {});
+    SwXTextDocument* pTextDoc2 = dynamic_cast<SwXTextDocument*>(xDoc2.get());
+    SwDocShell* pDocShell2 = pTextDoc2->GetDocShell();
+    SwWrtShell* pWrtShell2 = pDocShell2->GetWrtShell();
+    SwTransferable::Paste(*pWrtShell2, aHelper);
+
+    // Then make sure its fly frame is created.
+    mxComponent->dispose();
+    mxComponent = xDoc2;
+    xmlDocUniquePtr pLayout = parseLayoutDump();
+    // Without the accompanying fix in place, this test would have failed, because the first text
+    // frame in the body frame had an SwAnchoredDrawObject anchored to it, but not a fly frame, so
+    // a blank square was painted, not the image.
+    assertXPath(pLayout, "/root/page/body/txt/anchored/fly", 1);
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();