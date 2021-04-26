e2f771ffe8e82ba5a5001a71601abf3c45c56192
Dennis Francis <dennis.francis@collabora.com>
2021-04-26 14:40:11 +0530

    lok: Enable the "AutoInput" again.
    This partly reverts commit 65990058f041c3f1d280a69d411eb4ceacf5a721.
    
    Change-Id: I494e3ffa3c9824fd6668b1c7322b9cf08accac21
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/115456
    Tested-by: Jenkins
    Reviewed-by: Dennis Francis <dennis.francis@collabora.com>

--- sc/source/ui/app/inputhdl.cxx
+++ sc/source/ui/app/inputhdl.cxx
@@ -801,6 +801,7 @@ ScInputHandler::ScInputHandler()
     if (comphelper::LibreOfficeKit::isActive())
     {
         ScInputHandler::bOptLoaded = true;            // Evaluate App options
+        ScInputHandler::bAutoComplete = true;         // Is set in KeyInput
     }
 }
 
