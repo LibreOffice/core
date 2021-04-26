ac9176712160cab5dc3c89dd7156f6ce7a7ba76d
Luboš Luňák <l.lunak@collabora.com>
2021-04-26 15:16:49 +0200

    Revert "tdf#136957 Use bigger dots for better handling in presentation mode."
    This change was a workaround and a follow-up commit will handle the problem
    in a better way.
    
    This reverts commit 5d80f679e1891f98ef964efa1166c90d001c5806.
    
    Change-Id: I24a98a0828d3b6fbd014d58f37bf4da40d7dfdfd

--- sd/qa/unit/uiimpress.cxx
+++ sd/qa/unit/uiimpress.cxx
@@ -650,19 +650,9 @@ CPPUNIT_TEST_FIXTURE(SdUiImpressTest, testTdf134053)
     // Because 0% is not possible as dash length (as of June 2020) 1% is used in the fix.
     // For that a larger delta is here allowed to the ideal value than needed for
     // rounding errors.
+    CPPUNIT_ASSERT_DOUBLES_EQUAL_MESSAGE("Distance", 2117, fDistance, 12);
     CPPUNIT_ASSERT_DOUBLES_EQUAL_MESSAGE("Dot length", 706, fDotLength, 12);
-
-    // tdf#136957 (dotted lines are  disappearing in presentation mode)
-    // Test value used as 2089 instead of 2117 for tdf#136957 workaround.
-    // If this test fails as Expected: 2089 Actual:2117
-    // plaese test tdf#136957 manually and use 2117 as test value again.
-    CPPUNIT_ASSERT_DOUBLES_EQUAL_MESSAGE("Distance", 2089, fDistance, 12);
-
-    // tdf#136957 (dotted lines are  disappearing in presentation mode)
-    // Test value used as 2854 instead of 2822 for tdf#136957 workaround.
-    // If this test fails as Expected: 2854 Actual:2822
-    // plaese test tdf#136957 manually and use 2822 as test value again.
-    CPPUNIT_ASSERT_DOUBLES_EQUAL_MESSAGE("Dash length", 2854, fDashLength, 12);
+    CPPUNIT_ASSERT_DOUBLES_EQUAL_MESSAGE("Dash length", 2822, fDashLength, 12);
 }
 
 CPPUNIT_TEST_FIXTURE(SdUiImpressTest, testSpellOnlineParameter)
