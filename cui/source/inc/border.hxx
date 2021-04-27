089c7d05fde13251eb8cd8daaf7627b6bb0072f9
Vasily Melenchuk <vasily.melenchuk@cib.de>
2021-04-27 13:34:23 +0300

    tdf#48622 add border line thickness predefined values in UI
    According to multiple reports border line width selection will be
    more intuitive with some predefined values. Here is an implementation
    of this proposal: line width can be selected from combobox from
    predefined values (thin, medium, thick and custom). Classical spinner
    is right now hidden unless custom line width is selected.
    
    Change-Id: I87a6237335b79a5f5b63e109360e1ea8f12ae071
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/114709
    Tested-by: Jenkins
    Reviewed-by: Samuel Mehrbrodt <samuel.mehrbrodt@allotropia.de>

--- cui/source/inc/border.hxx
+++ cui/source/inc/border.hxx
@@ -77,6 +77,7 @@ private:
 class SvxBorderTabPage : public SfxTabPage
 {
     static const WhichRangesContainer pRanges;
+    static const std::vector<int> m_aLineWidths;
 
 public:
     SvxBorderTabPage(weld::Container* pPage, weld::DialogController* pController, const SfxItemSet& rCoreAttrs);
@@ -128,6 +129,7 @@ private:
 
     std::unique_ptr<SvtLineListBox> m_xLbLineStyle;
     std::unique_ptr<ColorListBox> m_xLbLineColor;
+    std::unique_ptr<weld::ComboBox> m_xLineWidthLB;
     std::unique_ptr<weld::MetricSpinButton> m_xLineWidthMF;
 
     std::unique_ptr<weld::Container> m_xSpacingFrame;
@@ -165,7 +167,8 @@ private:
     DECL_LINK(SelSdwHdl_Impl, ValueSet*, void);
     DECL_LINK(LinesChanged_Impl, LinkParamNone*, void);
     DECL_LINK(ModifyDistanceHdl_Impl, weld::MetricSpinButton&, void);
-    DECL_LINK(ModifyWidthHdl_Impl, weld::MetricSpinButton&, void);
+    DECL_LINK(ModifyWidthLBHdl_Impl, weld::ComboBox&, void);
+    DECL_LINK(ModifyWidthMFHdl_Impl, weld::MetricSpinButton&, void);
     DECL_LINK(SyncHdl_Impl, weld::Toggleable&, void);
     DECL_LINK(RemoveAdjacentCellBorderHdl_Impl, weld::Toggleable&, void);
 
@@ -175,6 +178,7 @@ private:
     void                FillPresetVS();
     void                FillShadowVS();
     void                FillValueSets();
+    void                SetLineWidth(sal_Int64 nWidth);
 
     // Filler
     void                FillLineListBox_Impl();
