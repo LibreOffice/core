7394bef4c51f9ad87e3ba1b368c8b5ed1b34589e
Szymon Kłos <szymon.klos@collabora.com>
2021-04-27 15:49:32 +0200

    Revert ScInputWindow::setPosSizePixel() early returns
    Revert changes that were introduced to fix CollaboraOnline/online#1988
    because it should be fixed in online
    
    The root cause was that online was sending "resizewindow"
    even when size was not changed what was triggering many times
    ScInputWindow::setPosSizePixel(). (before online fix "resizewindow"
    message was sent on every sidebar refresh)
    
    "Fix early return in ScInputWindow::setPosSizePixel"
    reverts commit a9e0bbadb151d6cfe652fad1d49147f559a6d83c.
    
    "Early return from ScInputWindow::setPosSizePixel() if no-op"
    reverts commit 0ad467d9abd27197bc5f7f22db5a25b3246e6808.
    
    Change-Id: Iad1c3d427a5feefdbb2bac912710847addf4ee02

--- sc/source/ui/app/inputwin.cxx
+++ sc/source/ui/app/inputwin.cxx
@@ -478,10 +478,6 @@ void ScInputWindow::SetSizePixel( const Size& rNewSize )
 
 void ScInputWindow::setPosSizePixel(tools::Long nX, tools::Long nY, tools::Long nWidth, tools::Long nHeight, PosSizeFlags nFlags)
 {
-    if ((!(nFlags & PosSizeFlags::Size) ||  GetSizePixel() == Size(nWidth, nHeight)) &&
-        (!(nFlags & PosSizeFlags::Pos) || GetPosPixel() == Point(nX, nY)))
-        return;
-
     ToolBox::setPosSizePixel(nX, nY, nWidth, nHeight, nFlags);
     if (const vcl::ILibreOfficeKitNotifier* pNotifier = GetLOKNotifier())
     {
