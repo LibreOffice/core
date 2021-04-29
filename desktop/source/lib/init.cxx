857dee030f0a45cb3e28a0f3870b02a97fd8e280
Tor Lillqvist <tml@collabora.com>
2021-04-29 12:06:24 +0300

    Let's call it "traceeventrecording" and not "profilezonerecording"
    It is not just ProfileZone events any longer that are recorded.
    
    Change-Id: I9c2b5817fca8f8a23ebd571cbaccb9ac063ec73b
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/114848
    Tested-by: Tor Lillqvist <tml@collabora.com>
    Reviewed-by: Tor Lillqvist <tml@collabora.com>

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -3849,7 +3849,7 @@ static void lo_setOption(LibreOfficeKit* /*pThis*/, const char *pOption, const c
 {
     static char* pCurrentSalLogOverride = nullptr;
 
-    if (strcmp(pOption, "profilezonerecording") == 0)
+    if (strcmp(pOption, "traceeventrecording") == 0)
     {
         if (strcmp(pValue, "start") == 0)
             comphelper::TraceEvent::startRecording();
