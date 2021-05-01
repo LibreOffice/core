e61cbca254fb0c060d06abbea669822f0a8582ad
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2021-05-01 12:16:27 +0900

    svgio: add test for svgtools starting with SvgNumber
    Change-Id: I1f903a947fb1c338cf62980256c5369fefb7740e
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/114956
    Tested-by: Jenkins
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

---
+++ svgio/CppunitTest_svgio_tools.mk
@@ -0,0 +1,35 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_CppunitTest_CppunitTest,svgio_tools))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,svgio_tools,\
+    svgio/qa/cppunit/SvgNumberTest \
+))
+
+$(eval $(call gb_CppunitTest_set_include,svgio_tools,\
+    $$(INCLUDE) \
+    -I$(SRCDIR)/svgio/inc \
+))
+
+$(eval $(call gb_CppunitTest_use_externals,svgio_tools,\
+    boost_headers \
+))
+
+$(eval $(call gb_CppunitTest_use_libraries,svgio_tools,\
+    basegfx \
+    drawinglayer \
+    sal \
+    sax \
+    svt \
+    vcl \
+    svgio \
+))
+
+# vim: set noet sw=4 ts=4:
