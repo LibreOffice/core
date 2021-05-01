20c6cb67a3450358d393a48770317434d36bfa28
Michael Stahl <michael.stahl@allotropia.de>
2021-05-01 18:19:27 +0200

    liborcus: newline problem with GCC 11 -E -fdirectives-only
    ooxml_tokens.inl:3524:32: error: stray '#' in program
    
    Change-Id: Ib7f50e1798e8c985f31a0338944ae1e9b439e98f
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/114975
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
    (cherry picked from commit aa38eede9d340cbb11a792ab1ebbe681521a4489)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/114921
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>

--- external/liborcus/UnpackedTarball_liborcus.mk
+++ external/liborcus/UnpackedTarball_liborcus.mk
@@ -21,6 +21,7 @@ $(eval $(call gb_UnpackedTarball_add_patches,liborcus,\
 	external/liborcus/libtool.patch.0 \
 	external/liborcus/fix-pch.patch.0 \
 	external/liborcus/include.patch.0 \
+	external/liborcus/liborcus_newline.patch.1 \
 ))
 
 $(eval $(call gb_UnpackedTarball_add_patches,liborcus,\
