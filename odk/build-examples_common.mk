8a2fa36eb2c0a69884d4a3188ec542b89861e57d
Michael Stahl <michael.stahl@allotropia.de>
2021-05-03 12:48:14 +0200

    odk: build examples with GCC with explicit -std=c++11
    GCC 11 defaults to -std=c++17, which doesn't support exception
    specifications any more.
    
    ddcc98fa50dd9d86a60dada4daa00f4d95ffe005 seems a bit large to backport.
    
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/115505
    Tested-by: Thorsten Behrens <thorsten.behrens@allotropia.de>
    Reviewed-by: Thorsten Behrens <thorsten.behrens@allotropia.de>
    (cherry picked from commit dff559024d271a69186b608615f4a7095cbee2a4)
    
    Change-Id: I74a182435b268be8fd7a9ff0be9f404122108b51
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/115978
    Tested-by: Michael Stahl <michael.stahl@allotropia.de>
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

--- odk/build-examples_common.mk
+++ odk/build-examples_common.mk
@@ -31,7 +31,7 @@ endif
 	$(foreach my_dir,$(2), \
 	    && (cd $(INSTDIR)/$(SDKDIRNAME)/examples/$(my_dir) \
 		&& printf 'yes\n' | LC_ALL=C make \
-			CC="$(CXX)" LINK="$(CXX)" LIB="$(CXX)" \
+			CC="$(CXX) $(if $(filter GCC,$(COM)),-std=c++11)" LINK="$(CXX)" LIB="$(CXX)" \
 		    $(if $(filter MACOSX,$(OS)), SHELL=$(ODK_BUILD_SHELL), )))) \
 	    >$(call gb_CustomTarget_get_workdir,$(1))/log 2>&1 \
 	|| (RET=$$$$? \
