406f3a5dac181aa90938b70f22ccf7a706be38fa
baltasarq <baltasarq@gmail.com>
2021-05-03 15:48:18 +0200

    tdf#112369 allow to disable the add command button in menu customize
    Instead of showing an alert dialog reporting that a command is already present
    in a given menu, enable or disable the add command button when needed.
    
    Change-Id: I2e209a0293b0a6e5c89139212373f56c8f2cef91

--- cui/source/customize/SvxMenuConfigPage.cxx
+++ cui/source/customize/SvxMenuConfigPage.cxx
@@ -170,10 +170,19 @@ void SvxMenuConfigPage::UpdateButtonStates()
     // which contains the functions of the selected menu/toolbar needs to be disabled
     SvxConfigEntry* pMenuData = GetTopLevelSelection();
 
-    m_xInsertBtn->set_sensitive(pMenuData != nullptr);
+    m_xInsertBtn->set_sensitive( pMenuData != nullptr);
 
-    m_xAddCommandButton->set_sensitive(pMenuData != nullptr);
-    m_xRemoveCommandButton->set_sensitive(pMenuData != nullptr);
+    SvxConfigEntry * selectedCmd = CreateCommandFromSelection( GetScriptURL() );
+
+    m_xAddCommandButton->set_sensitive(pMenuData != nullptr
+                              && !IsCommandInMenuList(
+                                    selectedCmd, pMenuData->GetEntries() ) );
+
+    delete selectedCmd;
+
+    if ( bIsValidSelection ) {
+        m_xRemoveCommandButton->set_sensitive(pMenuData != nullptr);
+    }
 
     //Handle the gear button
     if (pMenuData && m_bIsMenuBar)
