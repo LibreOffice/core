bb7b74591db0b3856e44b2a7335489e3fda4296d
Stephan Bergmann <sbergman@redhat.com>
2021-05-03 17:13:31 +0200

    loplugin:noexcept (macOS)
    Change-Id: Ib8951e9590d70ffecf8fd6af3070ef9c6d4ae151

--- bridges/source/cpp_uno/gcc3_linux_aarch64/abi.hxx
+++ bridges/source/cpp_uno/gcc3_linux_aarch64/abi.hxx
@@ -115,7 +115,7 @@ struct __cxa_eh_globals {
 
 #if !HAVE_CXXABI_H_CXA_GET_GLOBALS
 namespace __cxxabiv1 {
-extern "C" __cxa_eh_globals * __cxa_get_globals() throw();
+extern "C" __cxa_eh_globals * __cxa_get_globals() noexcept;
 }
 #endif
 
