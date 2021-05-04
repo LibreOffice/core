80913217dc26b66a72d07689cdf19b3d5c5a3208
Chris Sherlock <chris.sherlock79@gmail.com>
2021-05-05 04:06:41 +1000

    vcl: outdevmap.hxx -> rendercontext/ImplMapRes.hxx
    Change-Id: I96e191999e43e1a4203d548eeacd386ed883cd8b

--- vcl/Library_vcl.mk
+++ vcl/Library_vcl.mk
@@ -115,6 +115,7 @@ $(eval $(call gb_Library_use_externals,vcl,\
 endif
 
 $(eval $(call gb_Library_add_exception_objects,vcl,\
+    vcl/source/rendercontext/ImplMapRes \
     vcl/source/animate/Animation \
     vcl/source/animate/AnimationBitmap \
     vcl/source/cnttype/mcnttfactory \
