e699632c5aac8b61512447123b70c88dc59e27da
Noel Grandin <noelgrandin@gmail.com>
2021-05-04 20:11:21 +0200

    convert sal_uLong and tools::Long in vcl/settings
    Change-Id: I69711b791884218f4a14c85fe69072b9ea06dc79

--- vcl/source/app/help.cxx
+++ vcl/source/app/help.cxx
@@ -530,7 +530,7 @@ void ImplShowHelpWindow( vcl::Window* pParent, sal_uInt16 nHelpWinStyle, QuickHe
         return;
 
     sal_uInt64 nCurTime = tools::Time::GetSystemTicks();
-    if ( ( nCurTime - aHelpData.mnLastHelpHideTime ) < HelpSettings::GetTipDelay() )
+    if ( ( nCurTime - aHelpData.mnLastHelpHideTime ) < static_cast<sal_uInt64>(HelpSettings::GetTipDelay()) )
         bNoDelay = true;
 
     pHelpWin = VclPtr<HelpTextWindow>::Create( pParent, rHelpText, nHelpWinStyle, nStyle );
