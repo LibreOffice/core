538bc642937d9f2e0d11f0451681f2569ff8137a
Szymon Kłos <szymon.klos@collabora.com>
2021-05-04 13:32:51 +0200

    Fix style previews widget with multiple languages
    Broadcast also universal name (English identifier) for styles
    on change. This allows to select correct style without knowledge
    about all languages that other users use in other views.
    Fixes style previews widget in online with multiple sessions in
    different languages
    
    Change-Id: I9b9bcc92d96b5a5482a97a5947f148a638f257d7
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/115093
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Jan Holesovsky <kendy@collabora.com>

--- sw/source/uibase/app/docst.cxx
+++ sw/source/uibase/app/docst.cxx
@@ -163,11 +163,16 @@ void  SwDocShell::StateStyleSheet(SfxItemSet& rSet, SwWrtShell* pSh)
             case SID_STYLE_FAMILY2:
                 if(!pShell->IsFrameSelected())
                 {
+                    OUString aProgName;
                     SwTextFormatColl* pColl = pShell->GetCurTextFormatColl();
                     if(pColl)
+                    {
                         aName = pColl->GetName();
+                        sal_uInt16 nId = pColl->GetPoolFormatId();
+                        SwStyleNameMapper::FillProgName(nId, aProgName);
+                    }
 
-                    SfxTemplateItem aItem(nWhich, aName);
+                    SfxTemplateItem aItem(nWhich, aName, aProgName);
 
                     SfxStyleSearchBits nMask = SfxStyleSearchBits::Auto;
                     if (m_xDoc->getIDocumentSettingAccess().get(DocumentSettingId::HTML_MODE))
