LibreOffice/core

commit eb85de8e6b61fb3fcb6c03ae0145f7fe5478bccf

sw: layout: if fly's anchor moves forward, move fly off SwPageFrame
The problem is that on Show Changes->Hide Changes->Show Changes
in a 311 page document, the fly "Grafik1" was initially on page 203
but ends up on page 204, with a fly-sized gap on page 194.

In a 25 page cut down version of the bugdoc, on layout action 659
the fly's anchor SwTextFrame moves from page 21 to page 22, but
the fly remains in the SwPageFrame's m_pSortedObjs, because it's
not the anchor frame itself that moves but a distant previous frame,
and page 21 goes valid.

0  SwFlowFrame::PasteTree(SwFrame*, SwLayoutFrame*, SwFrame*, SwFrame*) (pStart=0x57c9e30, pParent=0xba15c50, pSibling=0x5a0f920, pOldParent=0xb057690) at sw/source/core/layout/flowfrm.cxx:586
1  SwFlowFrame::MoveSubTree(SwLayoutFrame*, SwFrame*) (this=0x57c9f48, pParent=0xba15c50, pSibling=0x5a0f920) at sw/source/core/layout/flowfrm.cxx:677
2  SwFlowFrame::MoveFwd(bool, bool, bool) (this=0x57c9f48, bMakePage=true, bPageBreak=false, bMoveAlways=false) at sw/source/core/layout/flowfrm.cxx:2061
3  SwContentFrame::MakeAll(OutputDevice*) (this=0x57c9e30) at sw/source/core/layout/calcmove.cxx:1831
4  SwFrame::OptPrepareMake() (this=0x57c9e30) at sw/source/core/layout/calcmove.cxx:399
5  SwFrame::OptCalc() const (this=0x57c9e30) at sw/source/core/inc/frame.hxx:1065
6  SwLayAction::FormatContent_(SwContentFrame const*, SwPageFrame const*) (this=0x7ffec0191b30, pContent=0x57c9e30, pPage=0xb9a1fd0) at sw/source/core/layout/layact.cxx:1833

In subsequent layout actions the anchor frame moves forward one
page at a time, until in action 665, when things get really interesting.

On page 24, the anchor text frame 582 is formatted for the first time,
and it moves the fly to page 24, after positioning it on the page.

2  SwPageFrame::MoveFly(SwFlyFrame*, SwPageFrame*) (this=0xb9a1fd0, pToMove=0x641d310, pDest=0x9125bb0) at sw/source/core/layout/flylay.cxx:972

sw/inc/anchoredobject.hxx
@@ -295,6 +295,8 @@ class SW_DLLPUBLIC SwAnchoredObject
         /** method to invalidate position of the anchored object */
         virtual void InvalidateObjPos() = 0;
 
+        virtual void RegisterAtPage(SwPageFrame &) = 0;
+
         /** method to perform necessary invalidations for the positioning of
             objects, for whose the wrapping style influence has to be considered
             on the object positioning.

sw/source/core/layout/layact.cxx
@@ -62,6 +62,8 @@
 #include <sortedobjs.hxx>
 #include <objectformatter.hxx>
 #include <fntcache.hxx>
+#include <fmtanchr.hxx>
+#include <comphelper/scopeguard.hxx>
 #include <vector>
 #include <tools/diagnose_ex.h>
 
@@ -1624,8 +1626,45 @@ bool SwLayAction::FormatLayoutTab( SwTabFrame *pTab, bool bAddRect )
     return bChanged;
 }
 
-bool SwLayAction::FormatContent( const SwPageFrame *pPage )
+bool SwLayAction::FormatContent(SwPageFrame *const pPage)
 {
+    ::comphelper::ScopeGuard g([this, pPage]() {
+        if (IsAgain())
+        {
+            return; // pPage probably deleted
+        }
+        if (auto const* pObjs = pPage->GetSortedObjs())
+        {
+            std::vector<std::pair<SwAnchoredObject*, SwPageFrame*>> moved;
+            for (auto const pObj : *pObjs)
+            {
+                SwPageFrame *const pAnchorPage(pObj->FindPageFrameOfAnchor());
+                assert(pAnchorPage);
+                if (pAnchorPage != pPage
+                    && pPage->GetPhyPageNum() < pAnchorPage->GetPhyPageNum()
+                    && pObj->GetFrameFormat().GetAnchor().GetAnchorId()
+                        != RndStdIds::FLY_AS_CHAR)
+                {
+                    moved.emplace_back(pObj, pAnchorPage);
+                }
+            }
+            for (auto const& [pObj, pAnchorPage] : moved)
+            {
+                pObj->RegisterAtPage(*pAnchorPage);
+                ::Notify_Background(pObj->GetDrawObj(), pPage,
+                    pObj->GetObjRect(), PrepareHint::FlyFrameLeave, false);
+            }
+            if (!moved.empty())
+            {
+                pPage->InvalidateFlyLayout();
+                if (auto *const pContent = pPage->FindLastBodyContent())
+                {
+                    pContent->InvalidateSize();
+                }
+            }
+        }
+    });
+
     const SwContentFrame *pContent = pPage->ContainsContent();
     const SwViewShell *pSh = m_pRoot->GetCurrShell();
     const bool bBrowse = pSh && pSh->GetViewOptions()->getBrowseMode();

sw/inc/anchoreddrawobject.hxx
@@ -94,6 +94,7 @@ class SwAnchoredDrawObject final : public SwAnchoredObject
             page frame
         */
         virtual void RegisterAtCorrectPage() override;
+        virtual void RegisterAtPage(SwPageFrame &) override;
 
         virtual bool SetObjTop_( const SwTwips _nTop) override;
         virtual bool SetObjLeft_( const SwTwips _nLeft) override;

sw/source/core/inc/flyfrm.hxx
@@ -136,6 +136,7 @@ protected:
         page frame
     */
     virtual void RegisterAtCorrectPage() override;
+    virtual void RegisterAtPage(SwPageFrame &) override;
 
     virtual bool SetObjTop_( const SwTwips _nTop ) override;
     virtual bool SetObjLeft_( const SwTwips _nLeft ) override;

sw/source/core/inc/flyfrms.hxx
@@ -167,6 +167,7 @@ class SwFlyAtContentFrame final: public SwFlyFreeFrame
         #i28701#
     */
     virtual void RegisterAtCorrectPage() override;
+    virtual void RegisterAtPage(SwPageFrame &) override;
     virtual void SwClientNotify(const SwModify&, const SfxHint&) override;
 
 public:

sw/source/core/inc/layact.hxx
@@ -111,7 +111,7 @@ class SwLayAction
 
     bool FormatLayout( OutputDevice* pRenderContext, SwLayoutFrame *, bool bAddRect = true );
     bool FormatLayoutTab( SwTabFrame *, bool bAddRect );
-    bool FormatContent( const SwPageFrame* pPage );
+    bool FormatContent(SwPageFrame * pPage);
     void FormatContent_( const SwContentFrame* pContent,
                        const SwPageFrame* pPage );
     bool IsShortCut( SwPageFrame *& );

sw/source/core/layout/anchoreddrawobject.cxx
@@ -919,10 +919,18 @@ void SwAnchoredDrawObject::RegisterAtCorrectPage()
     }
     if ( pPageFrame && GetPageFrame() != pPageFrame )
     {
-        if ( GetPageFrame() )
-            GetPageFrame()->RemoveDrawObjFromPage( *this );
-        pPageFrame->AppendDrawObjToPage( *this );
+        RegisterAtPage(*pPageFrame);
     }
+}
+
+void SwAnchoredDrawObject::RegisterAtPage(SwPageFrame & rPageFrame)
+{
+    assert(GetPageFrame() != &rPageFrame);
+    if (GetPageFrame())
+    {
+        GetPageFrame()->RemoveDrawObjFromPage( *this );
+    }
+    rPageFrame.AppendDrawObjToPage( *this );
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sw/source/core/layout/fly.cxx
@@ -2840,6 +2840,11 @@ void SwFlyFrame::RegisterAtCorrectPage()
     // default behaviour is to do nothing.
 }
 
+void SwFlyFrame::RegisterAtPage(SwPageFrame &)
+{
+    // default behaviour is to do nothing.
+}
+
 /** method to determine, if a <MakeAll()> on the Writer fly frame is possible
 
     OD 2004-05-11 #i28701#

commit e7b2babbd98bce4944b9f2eb8162a1e2aa54bc58

tdf#141945 sw: layout: check master frame when moving fly forward
The problem is that in the finished layout the fly frames are
positioned on the first page but are in SwPageFrame::m_pSortedObjs
of the second page.

Don't use FindPageFrameOfAnchor() because that looks up the follow-frame
that contains the anchor position.  This was unintentional; the idea
was to get flys anchored in subsequent paragraphs out of the way.

This situation where it's on a follow-frame of the same paragraph is
more complicated and less obvious, so don't try to solve it now.

(regression from eb85de8e6b61fb3fcb6c03ae0145f7fe5478bccf)

Change-Id: Ic6809a43c467955368807ccc82a7873c29fbfc2e

sw/inc/anchoredobject.hxx
@@ -46,7 +46,8 @@ class SW_DLLPUBLIC SwAnchoredObject
     private:
         // drawing object representing the anchored object in the drawing layer
         SdrObject* mpDrawObj;
-        // frame the object is anchored at
+        /// Frame the object is anchored at.
+        /// For at-char/at-para anchor, this is always the master SwTextFrame.
         SwFrame* mpAnchorFrame;
         // #i28701 - page frame the object is registered at
         // note: no page frame for as-character anchored objects

sw/source/core/layout/layact.cxx
@@ -1606,7 +1606,9 @@ bool SwLayAction::FormatContent(SwPageFrame *const pPage)
             std::vector<std::pair<SwAnchoredObject*, SwPageFrame*>> moved;
             for (auto const pObj : *pObjs)
             {
-                SwPageFrame *const pAnchorPage(pObj->FindPageFrameOfAnchor());
+                assert(!pObj->AnchorFrame()->IsTextFrame()
+                    || !static_cast<SwTextFrame const*>(pObj->AnchorFrame())->IsFollow());
+                SwPageFrame *const pAnchorPage(pObj->AnchorFrame()->FindPageFrame());
                 assert(pAnchorPage);
                 if (pAnchorPage != pPage
                     && pPage->GetPhyPageNum() < pAnchorPage->GetPhyPageNum()
@@ -1618,6 +1620,7 @@ bool SwLayAction::FormatContent(SwPageFrame *const pPage)
             }
             for (auto const& [pObj, pAnchorPage] : moved)
             {
+                SAL_INFO("sw.layout", "SwLayAction::FormatContent: move anchored " << pObj << " from " << pPage->GetPhyPageNum() << " to " << pAnchorPage->GetPhyPageNum());
                 pObj->RegisterAtPage(*pAnchorPage);
                 ::Notify_Background(pObj->GetDrawObj(), pPage,
                     pObj->GetObjRect(), PrepareHint::FlyFrameLeave, false);