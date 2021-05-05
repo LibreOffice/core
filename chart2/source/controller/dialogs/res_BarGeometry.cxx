e24a8d1484befe9e2bbb1937ace5493ea7b5f80d
Caolán McNamara <caolanm@redhat.com>
2021-05-05 10:51:16 +0100

    Resolves: tdf#142049 expand widget to use unused extra space
    also includes...
    
    Related: tdf#142049 restore some elements default visibility in the ui
    
    these defaulted to not visible before...
    
    commit ec548bce309d965a25e807ae79432114926e06f3
    Author: Caolán McNamara <caolanm@redhat.com>
    Date:   Mon Oct 15 11:14:02 2018 +0100
    
        weld ChartTypeDialog and SchLayoutTabPage
    
        Change-Id: I101c56b855d0bdc43559ca4561d298fa4ad92572
        Reviewed-on: https://gerrit.libreoffice.org/61797
    
    Change-Id: I7ff6aa4feeccd790b6176cb60b6adb9b21db0f63
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/115136
    Tested-by: Jenkins
    Reviewed-by: Adolfo Jayme Barrientos <fitojb@ubuntu.com>

--- chart2/source/controller/dialogs/res_BarGeometry.cxx
+++ chart2/source/controller/dialogs/res_BarGeometry.cxx
@@ -29,7 +29,8 @@ BarGeometryResources::BarGeometryResources(weld::Builder* pBuilder)
 {
     for (size_t i = 0; i < SAL_N_ELEMENTS(CHART_TYPE); ++i)
         m_xLB_Geometry->append_text(SchResId(CHART_TYPE[i]));
-    m_xLB_Geometry->set_size_request(-1, m_xLB_Geometry->get_height_rows(4));
+    m_xLB_Geometry->set_size_request(-1,
+                                     m_xLB_Geometry->get_height_rows(SAL_N_ELEMENTS(CHART_TYPE)));
 }
 
 void BarGeometryResources::connect_changed(const Link<weld::TreeView&, void>& rLink)
