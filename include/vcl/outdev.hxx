52899623f0a921d3eaea3fd14a3d4f3de84372a6
Chris Sherlock <chris.sherlock79@gmail.com>
2021-05-05 18:01:43 +1000

    vcl: create drawmode functions
    Change-Id: I3cb0325dc16db84ad58de28aa9110e9c7a0b9697

--- include/vcl/outdev.hxx
+++ include/vcl/outdev.hxx
@@ -478,7 +478,7 @@ public:
     AntialiasingFlags           GetAntialiasing() const { return mnAntialiasing; }
 
     void                        SetDrawMode( DrawModeFlags nDrawMode );
-    DrawModeFlags               GetDrawMode() const { return mnDrawMode; }
+    DrawModeFlags               GetDrawMode() const;
 
     void                        SetLayoutMode( ComplexTextLayoutFlags nTextLayoutMode );
     ComplexTextLayoutFlags       GetLayoutMode() const { return mnTextLayoutMode; }
@@ -1490,9 +1490,6 @@ public:
                                     bool bTransparencyAutoMode,
                                     bool bDownsampleBitmaps,
                                     const Color& rBackground = COL_TRANSPARENT );
-
-    SAL_DLLPRIVATE Color        ImplDrawModeToColor  ( const Color& rColor ) const;
-
 
     void                        DrawTransparent( const tools::PolyPolygon& rPolyPoly, sal_uInt16 nTransparencePercent );
 
