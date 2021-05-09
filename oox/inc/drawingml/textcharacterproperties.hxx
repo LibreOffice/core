4a3b9d2092ff5ce8336cc87cacbdc421f3d26ea7
Sarper Akdemir <sarper.akdemir@collabora.com>
2021-05-09 20:17:20 +0300

    tdf#59323: ooxml import: hasVisualRunProperties
    Introduces helper functions to determine whether a shape has non inherited run
    properties that change it visually.
    
    mbHasVisualRunProperties is set on import if there was a run property that
    alters visual appearance.
    
    Change-Id: Ie1e8e22d2757dc8594e7c6c3b8fc1dd7973c92af
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/117004
    Tested-by: Jenkins
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

--- oox/inc/drawingml/textcharacterproperties.hxx
+++ oox/inc/drawingml/textcharacterproperties.hxx
@@ -58,6 +58,8 @@ struct TextCharacterProperties
     OptValue< bool >    moUnderlineLineFollowText;
     OptValue< bool >    moUnderlineFillFollowText;
     FillProperties      maFillProperties;
+    /// Set if there was a property set that alters run visually during import
+    bool mbHasVisualRunProperties;
 
     std::vector<css::beans::PropertyValue> maTextEffectsProperties;
 
@@ -78,6 +80,8 @@ struct TextCharacterProperties
     void                pushToPropSet(
                             PropertySet& rPropSet,
                             const ::oox::core::XmlFilterBase& rFilter ) const;
+
+    TextCharacterProperties() : mbHasVisualRunProperties(false) {}
 };
 
 
