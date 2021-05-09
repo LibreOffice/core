93d91c7cc1dee08fdc28d3210d7475438777ec86
Sarper Akdemir <sarper.akdemir@collabora.com>
2021-05-09 19:46:21 +0300

    tdf#59323: ooxml import: add OOXML to LO datetime helper
    Added static helper functions getLOTimeFormat and getLODateFormat to
    TextField class for mapping datetime field types to SvxDateFomat and
    SvxTimeFormat.
    
    Change-Id: I9c1553cc89d47855dc7af06a8ea995de01692ded
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/117001
    Tested-by: Jenkins
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

--- oox/inc/drawingml/textfield.hxx
+++ oox/inc/drawingml/textfield.hxx
@@ -24,6 +24,9 @@
 #include <drawingml/textparagraphproperties.hxx>
 #include <memory>
 
+enum class SvxTimeFormat;
+enum class SvxDateFormat;
+
 namespace oox { namespace drawingml {
 
 struct TextCharacterProperties;
@@ -49,6 +52,16 @@ public:
                         const TextCharacterProperties& rTextCharacterStyle,
                         float nDefaultCharHeight) const override;
 
+    /** Gets the corresponding LO Date format for given OOXML datetime field type
+     *
+     * @param rDateTimeType PPTX datetime field type e.g. datetime3
+     */
+    static SvxDateFormat getLODateFormat( std::u16string_view rDateTimeType );
+    /** Gets the corresponding LO Time format for given OOXML datetime field type
+     *
+     * @param rDateTimeType PPTX datetime field type e.g. datetime3
+     */
+    static SvxTimeFormat getLOTimeFormat( std::u16string_view rDateTimeType );
 private:
     TextParagraphProperties  maTextParagraphProperties;
     OUString msType;
