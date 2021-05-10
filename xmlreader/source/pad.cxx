e28614a40c7d1b1066d1e52eb6ed4206967726a6
Noel Grandin <noel@peralex.com>
2021-05-10 15:38:29 +0200

    improve perf of xmlreader::Pad
    Change-Id: I83cc88977e7c287b0f428a3e01f289179b6dbad9

--- xmlreader/source/pad.cxx
+++ xmlreader/source/pad.cxx
@@ -29,46 +29,49 @@ namespace xmlreader {
 
 void Pad::add(char const * begin, sal_Int32 length) {
     assert(
-        begin != nullptr && length >= 0 && !(span_.is() && buffer_.getLength() != 0));
+        begin != nullptr && length >= 0 && !(span_.is() && buflength_ != 0));
     if (length != 0) {
         flushSpan();
-        if (buffer_.isEmpty()) {
+        if (buflength_ == 0) {
             span_ = Span(begin, length);
         } else {
-            buffer_.append(begin, length);
+            memcpy(buffer_ + buflength_, begin, length);
+            buflength_ += length;
         }
     }
 }
 
 void Pad::addEphemeral(char const * begin, sal_Int32 length) {
     assert(
-        begin != nullptr && length >= 0 && !(span_.is() && buffer_.getLength() != 0));
+        begin != nullptr && length >= 0 && !(span_.is() && buflength_ != 0));
     if (length != 0) {
         flushSpan();
-        buffer_.append(begin, length);
+        memcpy(buffer_ + buflength_, begin, length);
+        buflength_ += length;
     }
 }
 
 void Pad::clear() {
-    assert(!(span_.is() && buffer_.getLength() != 0));
+    assert(!(span_.is() && buflength_ != 0));
     span_.clear();
-    buffer_.setLength(0);
+    buflength_ = 0;
 }
 
 Span Pad::get() const {
-    assert(!(span_.is() && buffer_.getLength() != 0));
+    assert(!(span_.is() && buflength_ != 0));
     if (span_.is()) {
         return span_;
-    } else if (buffer_.isEmpty()) {
+    } else if (buflength_ == 0) {
         return Span("");
     } else {
-        return Span(buffer_.getStr(), buffer_.getLength());
+        return Span(buffer_, buflength_);
     }
 }
 
 void Pad::flushSpan() {
     if (span_.is()) {
-        buffer_.append(span_.begin, span_.length);
+        memcpy(buffer_ + buflength_, span_.begin, span_.length);
+        buflength_ += span_.length;
         span_.clear();
     }
 }
