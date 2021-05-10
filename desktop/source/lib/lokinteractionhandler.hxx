e49133a6fbe424ca7a5c635bbf089ec333baf3f5
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2021-05-10 14:59:29 +0900

    lok: move handleMacroConfirmationRequest to its own method
    Just refactor to make it similar to other requests.
    
    Change-Id: I1a76d3d4ecc02b0a223ab8e44b11047455280142
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/115306
    Tested-by: Jenkins
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/115533
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

--- desktop/source/lib/lokinteractionhandler.hxx
+++ desktop/source/lib/lokinteractionhandler.hxx
@@ -74,6 +74,7 @@ private:
     bool handleIOException(const css::uno::Sequence<css::uno::Reference<css::task::XInteractionContinuation>> &rContinuations, const css::uno::Any& rRequest);
     bool handleNetworkException(const css::uno::Sequence<css::uno::Reference<css::task::XInteractionContinuation>> &rContinuations, const css::uno::Any& rRequest);
     bool handlePasswordRequest(const css::uno::Sequence<css::uno::Reference<css::task::XInteractionContinuation>> &rContinuations, const css::uno::Any& rRequest);
+    static bool handleMacroConfirmationRequest(const css::uno::Reference<css::task::XInteractionRequest>& xRequest);
 
 public:
     void SetPassword(char const* pPassword);
