2009af1c0fb4fead653dd7688347c15d74eaa1b0
Mike Kaganski <mike.kaganski@collabora.com>
2021-05-11 20:58:50 +0200

    Revert "tdf#141015: Show Mail Merge toolbar when DBData has data source"
    This reverts commit 346153f08c907739ca310934ad45814379971027.
    It seems to create more problems than expected, and there's no
    way to remove DB association other than editing document XML.
    
    Change-Id: I93171f17c01e231abbcfa9a816fdadce83c983ec
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/115283
    Tested-by: Jenkins
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

--- sw/source/uibase/uiview/view.cxx
+++ sw/source/uibase/uiview/view.cxx
@@ -453,9 +453,7 @@ void SwView::SelectShell()
         }
 
         // Show Mail Merge toolbar initially for documents with Database fields
-        if (!m_bInitOnceCompleted
-            && (GetWrtShell().IsAnyDatabaseFieldInDoc()
-                || !GetWrtShell().GetDBData().sDataSource.isEmpty()))
+        if (!m_bInitOnceCompleted && GetWrtShell().IsAnyDatabaseFieldInDoc())
             ShowUIElement("private:resource/toolbar/mailmerge");
 
         // Activate the toolbar to the new selection which also was active last time.
