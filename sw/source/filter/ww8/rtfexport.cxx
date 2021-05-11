a82011e4c3bb888ca9bdbf56bb88bc9c677794be
Noel Grandin <noel.grandin@collabora.co.uk>
2021-05-11 10:04:47 +0200

    fix leak in RtfExport
    Change-Id: I6e1059cef9032de36cf8e1ac56eefecc4e417337

--- sw/source/filter/ww8/rtfexport.cxx
+++ sw/source/filter/ww8/rtfexport.cxx
@@ -731,7 +731,7 @@ ErrCode RtfExport::ExportDocument_Impl()
     }
 
     // Init sections
-    m_pSections = new MSWordSections(*this);
+    m_pSections.reset(new MSWordSections(*this));
 
     // Page description
     WritePageDescTable();
