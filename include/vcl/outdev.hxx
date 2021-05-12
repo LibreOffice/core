7e0c47b9a1a1061cca49b6b633d93e9f7984f915
Chris Sherlock <chris.sherlock79@gmail.com>
2021-05-12 20:16:24 +1000

    tdf#74702 vcl: make helper funcs for ImplDrawWaveLine() and ImplDrawWavePixel()
    Change-Id: Ic8e6e02ce0df349fc6fb6a3334105c1e6dfa3f36

--- include/vcl/outdev.hxx
+++ include/vcl/outdev.hxx
@@ -1078,8 +1078,11 @@ private:
     SAL_DLLPRIVATE void         ImplDrawSpecialText( SalLayout& );
     SAL_DLLPRIVATE void         ImplDrawTextRect( tools::Long nBaseX, tools::Long nBaseY, tools::Long nX, tools::Long nY, tools::Long nWidth, tools::Long nHeight );
 
-    SAL_DLLPRIVATE static void  ImplDrawWavePixel( tools::Long nOriginX, tools::Long nOriginY, tools::Long nCurX, tools::Long nCurY, Degree10 nOrientation, SalGraphics* pGraphics, const OutputDevice& rOutDev,
-                                                   bool bDrawPixAsRect, tools::Long nPixWidth, tools::Long nPixHeight );
+    SAL_DLLPRIVATE void  ImplDrawWavePixel( tools::Long nOriginX, tools::Long nOriginY, tools::Long nCurX, tools::Long nCurY, tools::Long nWidth, Degree10 nOrientation, SalGraphics* pGraphics, const OutputDevice& rOutDev, tools::Long nPixWidth, tools::Long nPixHeight );
+    virtual bool DrawWavePixelAsRect(tools::Long nLineWidth) const;
+    virtual void SetWaveLineColors(Color const& rColor, tools::Long nLineWidth);
+    virtual Size GetWaveLineSize(tools::Long nLineWidth) const;
+
     SAL_DLLPRIVATE void         ImplDrawWaveLine( tools::Long nBaseX, tools::Long nBaseY, tools::Long nStartX, tools::Long nStartY, tools::Long nWidth, tools::Long nHeight, tools::Long nLineWidth, Degree10 nOrientation, const Color& rColor );
     SAL_DLLPRIVATE void         ImplDrawWaveTextLine( tools::Long nBaseX, tools::Long nBaseY, tools::Long nX, tools::Long nY, tools::Long nWidth, FontLineStyle eTextLine, Color aColor, bool bIsAbove );
     SAL_DLLPRIVATE void         ImplDrawStraightTextLine( tools::Long nBaseX, tools::Long nBaseY, tools::Long nX, tools::Long nY, tools::Long nWidth, FontLineStyle eTextLine, Color aColor, bool bIsAbove );
