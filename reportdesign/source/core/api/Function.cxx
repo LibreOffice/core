7b71c679393bcbe550c2794805361ed2319f02b0
Noel Grandin <noel.grandin@collabora.co.uk>
2021-05-13 18:47:07 +0200

    tdf#141571 Base crashed on creation of user function
    Revert "reportdesign/core: create instances with uno constructors"
    This reverts commit 79c1d1234bdac0d41109930529f42d39c70b0f7b.
    
    I have no idea what is causing this. Something is stomping
    on memory belonging to something else, but why?
    
    Change-Id: Ieb758e5788f07e3f477063274ccd0721e3bf7726

--- reportdesign/source/core/api/Function.cxx
+++ reportdesign/source/core/api/Function.cxx
@@ -25,6 +25,11 @@ namespace reportdesign
 
     using namespace com::sun::star;
 
+uno::Reference< uno::XInterface > OFunction::create(uno::Reference< uno::XComponentContext > const & xContext)
+{
+    return *(new OFunction(xContext));
+}
+
 
 OFunction::OFunction(uno::Reference< uno::XComponentContext > const & _xContext)
 :FunctionBase(m_aMutex)
@@ -47,14 +52,27 @@ void SAL_CALL OFunction::dispose()
     cppu::WeakComponentImplHelperBase::dispose();
 }
 
-OUString SAL_CALL OFunction::getImplementationName(  )
+OUString OFunction::getImplementationName_Static(  )
 {
     return "com.sun.star.comp.report.OFunction";
 }
 
+
+OUString SAL_CALL OFunction::getImplementationName(  )
+{
+    return getImplementationName_Static();
+}
+
+uno::Sequence< OUString > OFunction::getSupportedServiceNames_Static(  )
+{
+    uno::Sequence< OUString > aServices { SERVICE_FUNCTION };
+
+    return aServices;
+}
+
 uno::Sequence< OUString > SAL_CALL OFunction::getSupportedServiceNames(  )
 {
-    return { SERVICE_FUNCTION };
+    return getSupportedServiceNames_Static();
 }
 
 sal_Bool SAL_CALL OFunction::supportsService(const OUString& ServiceName)
@@ -178,13 +196,6 @@ void SAL_CALL OFunction::setParent( const uno::Reference< uno::XInterface >& Par
 
 
 } // namespace reportdesign
-
-extern "C" SAL_DLLPUBLIC_EXPORT css::uno::XInterface*
-reportdesign_OFunction_get_implementation(
-    css::uno::XComponentContext* context, css::uno::Sequence<css::uno::Any> const&)
-{
-    return cppu::acquire(new reportdesign::OFunction(context));
-}
 
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
