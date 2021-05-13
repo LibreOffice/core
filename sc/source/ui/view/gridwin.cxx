63a9f20a0e432c58b1d2a1007a53e8467de59642
Balazs Varga <balazs.varga991@gmail.com>
2021-05-13 17:49:59 +0200

    tdf#68113 sc autofilter: fix not empty button unchecks all entries
    Instead unchecks all entries, unchecks only empty entries.
    
    Change-Id: I6f0d6432151ad2c65871dc5f1dd206883994217a
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/115562
    Tested-by: Jenkins
    Tested-by: László Németh <nemeth@numbertext.org>
    Reviewed-by: László Németh <nemeth@numbertext.org>
    (cherry picked from commit 00e13be1c3dd45597237f805a06b3e8a74187125)

--- sc/source/ui/view/gridwin.cxx
+++ sc/source/ui/view/gridwin.cxx
@@ -684,6 +684,7 @@ void ScGridWindow::LaunchAutoFilterMenu(SCCOL nCol, SCROW nRow)
 
     ScQueryParam aParam;
     pDBData->GetQueryParam(aParam);
+    bool bQueryByNonEmpty = false;
     std::vector<ScQueryEntry*> aEntries = aParam.FindAllEntriesByField(nCol);
     std::unordered_set<OUString> aSelectedString;
     std::unordered_set<double> aSelectedValue;
@@ -691,8 +692,13 @@ void ScGridWindow::LaunchAutoFilterMenu(SCCOL nCol, SCROW nRow)
     {
         if (pEntry && pEntry->bDoQuery && pEntry->eOp == SC_EQUAL)
         {
-            ScQueryEntry::QueryItemsType& rItems = pEntry->GetQueryItems();
-            std::for_each(rItems.begin(), rItems.end(), AddSelectedItemString(aSelectedString, aSelectedValue));
+            if (!pEntry->IsQueryByNonEmpty())
+            {
+                ScQueryEntry::QueryItemsType& rItems = pEntry->GetQueryItems();
+                std::for_each(rItems.begin(), rItems.end(), AddSelectedItemString(aSelectedString, aSelectedValue));
+            }
+            else
+                bQueryByNonEmpty = true;
         }
     }
 
@@ -711,6 +717,8 @@ void ScGridWindow::LaunchAutoFilterMenu(SCCOL nCol, SCROW nRow)
                 bSelected
                     = aSelectedValue.count(aDoubleVal) > 0 || aSelectedString.count(aStringVal) > 0;
         }
+        else if (bQueryByNonEmpty)
+            bSelected = !aStringVal.isEmpty();
 
         if ( rEntry.IsDate() )
             rControl.addDateMember( aStringVal, rEntry.GetValue(), bSelected );
