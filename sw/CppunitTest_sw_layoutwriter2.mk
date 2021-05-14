91be1f37e680de9b009b669e06f8cf257212dd9a
Xisco Fauli <xiscofauli@libreoffice.org>
2021-05-14 17:32:51 +0200

    sw: split CppunitTest_sw_layoutwriter into 2
    It already has 2 cxx files
    
    Change-Id: I766b07292a505ad30bca506c18470993ece5ac78

--- sw/CppunitTest_sw_layoutwriter2.mk
+++ sw/CppunitTest_sw_layoutwriter2.mk
@@ -9,7 +9,6 @@
 #
 #*************************************************************************
 
-# empty second argument (i.e. no 1)
-$(eval $(call sw_layoutwriter_test,))
+$(eval $(call sw_layoutwriter_test,2))
 
 # vim: set noet sw=4 ts=4:
