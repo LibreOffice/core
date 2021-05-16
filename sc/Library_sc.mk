573b6ca6af2ce3ca4a48024eedaefb322ddcd2ae
dante <dante19031999@gmail.com>
2021-05-16 16:04:20 +0200

    tdf#142307 - Upgrade SSE2 sum to AVX512 sum with Neumaier 1
    This part focuses on allowing it on replacing arrayfunctor
    By thefault it will try AVX512F (1,17%)
    If not available will use AVX (94,77%)
    Use of AVX2 (82,28%) has been avoided even if the code could been more compact
    Source of hardware statistics: https://store.steampowered.com/hwsurvey
    
    Change-Id: Iae737a565379e82c5f84f3fdee6321ac74f59d40

--- sc/Library_sc.mk
+++ sc/Library_sc.mk
@@ -99,7 +99,7 @@ $(eval $(call gb_Library_use_libraries,sc,\
 ))
 
 $(eval $(call gb_Library_add_exception_objects,sc,\
-    sc/source/core/tool/arraysumSSE2, $(CXXFLAGS_INTRINSICS_SSE2) \
+    sc/source/core/tool/arraysum, -mavx -mavx512f\
 ))
 
 $(eval $(call gb_Library_add_exception_objects,sc,\
