5af1494f9c4f11803bf74e23c05962634aaa228b
dante <dante19031999@gmail.com>
2021-05-16 16:04:20 +0200

    tdf#142307 - Upgrade SSE2 sum to AVX512 sum with Neumaier 1
    This part focuses on allowing it on replacing arrayfunctor
    By thefault it will try AVX512F (1,17%)
    If not available will use AVX (94,77%)
    Use of AVX2 (82,28%) has been avoided even if the code could been more compact
    Source of hardware statistics: https://store.steampowered.com/hwsurvey
    
    Change-Id: Iae737a565379e82c5f84f3fdee6321ac74f59d40

---
+++ sc/inc/arraysumfunctor.hxx
@@ -0,0 +1,161 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#pragma once
+
+#include <cmath>
+#include "kahan.hxx"
+#include "scdllapi.h"
+#include <tools/cpuid.hxx>
+#include <formula/errorcodes.hxx>
+
+/* TODO Remove this once GCC updated and AVX512 can work. */
+#ifdef __GNUC__
+#if __GNUC__ < 9
+#ifdef LO_AVX512F_AVAILABLE
+#define HAS_LO_AVX512F_AVAILABLE
+#undef LO_AVX512F_AVAILABLE
+#endif
+#endif
+#endif
+
+namespace sc::op
+{
+/* Checkout available optimization options */
+#ifndef LO_AVX512F_AVAILABLE
+constexpr bool hasAVX512F = false;
+#else
+const bool hasAVX512F = cpuid::hasAVX512F();
+#endif
+const bool hasAVX = cpuid::hasAVX();
+const bool hasSSE2 = cpuid::hasSSE2();
+
+/**
+  * Performs one step of the Neumanier sum between doubles
+  * Overwrites the summand and error
+  * @parma sum
+  * @param err
+  * @param value
+  */
+inline void sumNeumanierNormal(double& sum, double& err, const double& value)
+{
+    double t = sum + value;
+    if (std::abs(sum) >= std::abs(value))
+        err += (sum - t) + value;
+    else
+        err += (value - t) + sum;
+    sum = t;
+}
+
+/**
+  * If no boosts available, Unrolled KahanSum.
+  * Most likely to use on android.
+  */
+static inline KahanSum executeUnrolled(size_t& i, size_t nSize, const double* pCurrent)
+{
+    size_t nRealSize = nSize - i;
+    size_t nUnrolledSize = nRealSize - (nRealSize % 4);
+
+    if (nUnrolledSize > 0)
+    {
+        KahanSum sum0 = 0.0;
+        KahanSum sum1 = 0.0;
+        KahanSum sum2 = 0.0;
+        KahanSum sum3 = 0.0;
+
+        for (; i + 3 < nUnrolledSize; i += 4)
+        {
+            sum0 += *pCurrent++;
+            sum1 += *pCurrent++;
+            sum2 += *pCurrent++;
+            sum3 += *pCurrent++;
+        }
+        // We are using pairwise summation alongside Kahan
+        return (sum0 + sum1) + (sum2 + sum3);
+    }
+    return 0.0;
+}
+
+/* Available methos */
+SC_DLLPUBLIC KahanSum executeAVX512F(size_t& i, size_t nSize, const double* pCurrent);
+SC_DLLPUBLIC KahanSum executeAVX(size_t& i, size_t nSize, const double* pCurrent);
+SC_DLLPUBLIC KahanSum executeSSE2(size_t& i, size_t nSize, const double* pCurrent);
+
+/**
+  * This function task is to choose the fastest method available to perform the sum.
+  * @param i
+  * @param nSize
+  * @param pCurrent
+  */
+static inline KahanSum executeFast(size_t& i, size_t nSize, const double* pCurrent)
+{
+    if (hasAVX512F)
+        return executeAVX512F(i, nSize, pCurrent);
+    if (hasAVX)
+        return executeAVX(i, nSize, pCurrent);
+    if (hasSSE2)
+        return executeSSE2(i, nSize, pCurrent);
+    return executeUnrolled(i, nSize, pCurrent);
+}
+
+/**
+  * Performs the sum of an array.
+  * Note that align 16 will speed up the process.
+  * @param pArray
+  * @param nSize
+  */
+inline KahanSum sumArray(const double* pArray, size_t nSize)
+{
+    size_t i = 0;
+    const double* pCurrent = pArray;
+    KahanSum fSum = executeFast(i, nSize, pCurrent);
+
+    // sum rest of the array
+    for (; i < nSize; ++i)
+        fSum += pArray[i];
+
+    // If the sum is a NaN, some of the terms were empty cells, probably.
+    // Re-calculate, carefully
+    double fVal = fSum.get();
+    if (!std::isfinite(fVal))
+    {
+        FormulaError nErr = GetDoubleErrorValue(fVal);
+        if (nErr == FormulaError::NoValue)
+        {
+            fSum = 0;
+            for (i = 0; i < nSize; i++)
+            {
+                if (!std::isfinite(pArray[i]))
+                {
+                    nErr = GetDoubleErrorValue(pArray[i]);
+                    if (nErr != FormulaError::NoValue)
+                        fSum += pArray[i]; // Let errors encoded as NaNs propagate ???
+                }
+                else
+                    fSum += pArray[i];
+            }
+        }
+    }
+    return fSum;
+}
+
+} // end namespace sc::op
+
+/* TODO Remove this once GCC updated and AVX512 can work. */
+#ifdef __GNUC__
+#if __GNUC__ < 9
+#ifdef HAS_LO_AVX512F_AVAILABLE
+#define LO_AVX512F_AVAILABLE
+#undef HAS_LO_AVX512F_AVAILABLE
+#endif
+#endif
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
---
+++ sc/source/core/tool/arraysumAVX512.cxx
@@ -0,0 +1,143 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#include <arraysumfunctor.hxx>
+#include <sal/log.hxx>
+#include <tools/simd.hxx>
+#include <tools/simdsupport.hxx>
+
+#ifdef LO_AVX512F_AVAILABLE
+#pragma info available
+#else
+#pragma info unavailable
+#endif
+
+/* TODO Remove this once GCC updated and AVX512 can work. */
+#ifdef __GNUC__
+#if __GNUC__ < 9
+#ifdef LO_AVX512F_AVAILABLE
+#define HAS_LO_AVX512F_AVAILABLE
+#undef LO_AVX512F_AVAILABLE
+#endif
+#endif
+#endif
+
+namespace sc::op
+{
+#ifdef LO_AVX512F_AVAILABLE // New processors
+
+/** Kahan sum with AVX512.
+  */
+static inline void sumAVX512(__m512d& sum, __m512d& err, const __m512d& value)
+{
+    // Temporal parameter
+    __m512d t = _mm512_add_pd(sum, value);
+    // Absolute value of the total sum
+    __m512d asum = _mm512_abs_pd(sum);
+    // Absolute value of the value to add
+    __m512d avalue = _mm512_abs_pd(value);
+    // Comaprate the absolute values sum >= value
+    __mmask8 mask = _mm512_cmp_pd_mask(avalue, asum, _CMP_GE_OQ);
+    // The following code has this form ( a - t + b)
+    // Case 1: a = sum b = value
+    // Case 2: a = value b = sum
+    __m512d a = _mm512_mask_blend_pd(mask, sum, value);
+    __m512d b = _mm512_mask_blend_pd(mask, value, sum);
+    err = _mm512_add_pd(err, _mm512_add_pd(_mm512_sub_pd(a, t), b));
+    // Store result
+    sum = t;
+}
+
+#endif
+
+/** Execute Kahan sum with AVX512.
+  */
+KahanSum executeAVX512F(size_t& i, size_t nSize, const double* pCurrent)
+{
+#ifdef LO_AVX512F_AVAILABLE // New processors
+    // Make sure we don't fall out of bounds.
+    // This works by sums of 8 terms.
+    // So the 8'th term is i+7
+    // If we iterate untill nSize won't fall out of bounds
+    if (nSize > i + 7)
+    {
+        // Setup sums and errors as 0
+        __m512d sum = _mm512_setzero_pd();
+        __m512d err = _mm512_setzero_pd();
+
+        // Sum the stuff
+        for (; i + 7 < nSize; i += 8)
+        {
+            // Kahan sum
+            __m512d load = _mm512_loadu_pd(pCurrent);
+            sumAVX512(sum, err, load);
+            pCurrent += 8;
+
+            printf("AVX512\n\n");
+        }
+
+        // Store result
+        static_assert(sizeof(double) == 8);
+        double sums[8];
+        double errs[8];
+        _mm512_storeu_pd(static_cast<void*>(&sums[0]), sum);
+        _mm512_storeu_pd(static_cast<void*>(&errs[0]), err);
+
+        // First Kahan & pairwise summation
+        // 0+1 1+2 3+4 4+5 6+7 -> 0, 2, 4, 6
+        sumNeumanierNormal(sums[0], errs[0], sums[1]);
+        sumNeumanierNormal(sums[2], errs[2], sums[3]);
+        sumNeumanierNormal(sums[4], errs[4], sums[5]);
+        sumNeumanierNormal(sums[6], errs[6], sums[7]);
+        sumNeumanierNormal(sums[0], errs[0], errs[1]);
+        sumNeumanierNormal(sums[2], errs[2], errs[3]);
+        sumNeumanierNormal(sums[4], errs[4], errs[5]);
+        sumNeumanierNormal(sums[6], errs[6], errs[7]);
+
+        // Second Kahan & pairwise summation
+        // 0+2 4+6 -> 0, 4
+        sumNeumanierNormal(sums[0], errs[0], sums[2]);
+        sumNeumanierNormal(sums[4], errs[4], sums[6]);
+        sumNeumanierNormal(sums[0], errs[0], errs[2]);
+        sumNeumanierNormal(sums[4], errs[4], errs[6]);
+
+        // Third Kahan & pairwise summation
+        // 0+4 -> 0
+        sumNeumanierNormal(sums[0], errs[0], sums[4]);
+        sumNeumanierNormal(sums[0], errs[0], errs[4]);
+
+        // Return final result
+        return KahanSum(sums[0], errs[0]);
+    }
+    else
+        return 0.0;
+#else
+    printf("Failed to enable AVX 512 %i\n\n", __GNUC__);
+    SAL_WARN("sc", "Failed to use AVX 512");
+    (void)i;
+    (void)nSize;
+    (void)pCurrent;
+    return 0.0;
+#endif
+}
+
+} // end namespace sc::op
+
+/* TODO Remove this once GCC updated and AVX512 can work. */
+#ifdef __GNUC__
+#if __GNUC__ < 9
+#ifdef HAS_LO_AVX512F_AVAILABLE
+#define LO_AVX512F_AVAILABLE
+#undef HAS_LO_AVX512F_AVAILABLE
+#endif
+#endif
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
