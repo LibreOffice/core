712de920dec661333b9da508bce176eed3bdc952
dante <dante19031999@gmail.com>
2021-05-16 16:04:20 +0200

    tdf#142307 - Upgrade SSE2 sum to AVX512 sum with Neumaier 1
    This part focuses on allowing it on replacing arrayfunctor
    By thefault it will try AVX512F (1,17%)
    If not available will use AVX (94,77%)
    Use of AVX2 (82,28%) has been avoided even if the code could been more compact
    Source of hardware statistics: https://store.steampowered.com/hwsurvey
    
    Change-Id: Iae737a565379e82c5f84f3fdee6321ac74f59d40

--- sc/source/core/tool/arraysumSSE2.cxx
+++ sc/source/core/tool/arraysumSSE2.cxx
@@ -9,97 +9,121 @@
  */
 
 #include <arraysumfunctor.hxx>
+#include <sal/log.hxx>
+#include <tools/simd.hxx>
 #include <tools/simdsupport.hxx>
 
-namespace sc
-{
-double ArraySumFunctor::executeSSE2(size_t& i, const double* pCurrent) const
-{
-#if defined(LO_SSE2_AVAILABLE)
-    double fSum = 0.0;
-    size_t nRealSize = mnSize - i;
-    size_t nUnrolledSize = nRealSize - (nRealSize % 8);
+//AVX512VL + AVX512F + KNCNI
 
-    if (nUnrolledSize > 0)
+namespace sc::op
+{
+#ifdef LO_SSE2_AVAILABLE // Old processors
+
+const __m128d ANNULATE_SIGN_BIT = _mm_castsi128_pd(_mm_set1_epi64x(0x7FFF'FFFF'FFFF'FFFF));
+
+/** Kahan sum with SSE4.2.
+  */
+static inline void sumSSE2(__m128d& sum, __m128d& err, const __m128d& value)
+{
+    // Temporal parameter
+    __m128d t = _mm_add_pd(sum, value);
+    // Absolute value of the total sum
+    __m128d asum = _mm_and_pd(sum, ANNULATE_SIGN_BIT);
+    // Absolute value of the value to add
+    __m128d avalue = _mm_and_pd(value, ANNULATE_SIGN_BIT);
+    // Comaprate the absolute values sum >= value
+    __m128d mask = _mm_cmpge_pd(asum, avalue);
+    // The following code has this form ( a - t + b)
+    // Case 1: a = sum b = value
+    // Case 2: a = value b = sum
+    __m128d a = _mm_add_pd(_mm_and_pd(mask, sum), _mm_andnot_pd(mask, value));
+    __m128d b = _mm_add_pd(_mm_and_pd(mask, value), _mm_andnot_pd(mask, sum));
+    err = _mm_add_pd(err, _mm_add_pd(_mm_sub_pd(a, t), b));
+    // Store result
+    sum = t;
+}
+
+#endif
+
+/** Execute Kahan sum with SSE2.
+  */
+KahanSum executeSSE2(size_t& i, size_t nSize, const double* pCurrent)
+{
+#ifdef LO_SSE2_AVAILABLE
+    // Make sure we don't fall out of bounds.
+    // This works by sums of 8 terms.
+    // So the 8'th term is i+7
+    // If we iterate untill nSize won't fall out of bounds
+    if (nSize > i + 7)
     {
+        // Setup sums and errors as 0
         __m128d sum1 = _mm_setzero_pd();
-        __m128d sum2 = _mm_setzero_pd();
-        __m128d sum3 = _mm_setzero_pd();
-        __m128d sum4 = _mm_setzero_pd();
-
         __m128d err1 = _mm_setzero_pd();
+        __m128d sum2 = _mm_setzero_pd();
         __m128d err2 = _mm_setzero_pd();
+        __m128d sum3 = _mm_setzero_pd();
         __m128d err3 = _mm_setzero_pd();
+        __m128d sum4 = _mm_setzero_pd();
         __m128d err4 = _mm_setzero_pd();
 
-        __m128d y, t;
-
-        for (; i < nUnrolledSize; i += 8)
+        for (; i + 7 < nSize; i += 8)
         {
             // Kahan sum 1
-            __m128d load1 = _mm_load_pd(pCurrent);
-            y = _mm_sub_pd(load1, err1);
-            t = _mm_add_pd(sum1, y);
-            err1 = _mm_sub_pd(_mm_sub_pd(t, sum1), y);
-            sum1 = t;
+            __m128d load1 = _mm_loadu_pd(pCurrent);
+            sumSSE2(sum1, err1, load1);
             pCurrent += 2;
 
             // Kahan sum 2
-            __m128d load2 = _mm_load_pd(pCurrent);
-            y = _mm_sub_pd(load2, err2);
-            t = _mm_add_pd(sum2, y);
-            err2 = _mm_sub_pd(_mm_sub_pd(t, sum2), y);
-            sum2 = t;
+            __m128d load2 = _mm_loadu_pd(pCurrent);
+            sumSSE2(sum2, err2, load2);
             pCurrent += 2;
 
             // Kahan sum 3
-            __m128d load3 = _mm_load_pd(pCurrent);
-            y = _mm_sub_pd(load3, err3);
-            t = _mm_add_pd(sum3, y);
-            err3 = _mm_sub_pd(_mm_sub_pd(t, sum3), y);
-            sum3 = t;
+            __m128d load3 = _mm_loadu_pd(pCurrent);
+            sumSSE2(sum3, err3, load3);
             pCurrent += 2;
 
             // Kahan sum 4
-            __m128d load4 = _mm_load_pd(pCurrent);
-            y = _mm_sub_pd(load4, err4);
-            t = _mm_add_pd(sum4, y);
-            err4 = _mm_sub_pd(_mm_sub_pd(t, sum4), y);
-            sum4 = t;
+            __m128d load4 = _mm_loadu_pd(pCurrent);
+            sumSSE2(sum4, err4, load4);
             pCurrent += 2;
         }
 
         // Now we combine pairwise summation with Kahan summation
 
-        // sum 1 + sum 2
-        y = _mm_sub_pd(sum2, err1);
-        t = _mm_add_pd(sum1, y);
-        err1 = _mm_sub_pd(_mm_sub_pd(t, sum1), y);
-        sum1 = t;
+        // 1+2 3+4 -> 1, 3
+        sumSSE2(sum1, err1, sum2);
+        sumSSE2(sum1, err1, err2);
+        sumSSE2(sum3, err3, sum4);
+        sumSSE2(sum3, err3, err4);
 
-        // sum 3 + sum 4
-        y = _mm_sub_pd(sum4, err3);
-        t = _mm_add_pd(sum3, y);
-        sum3 = t;
+        // 1+3 -> 1
+        sumSSE2(sum1, err1, sum3);
+        sumSSE2(sum1, err1, err3);
 
-        // sum 1 + sum 3
-        y = _mm_sub_pd(sum3, err1);
-        t = _mm_add_pd(sum1, y);
-        sum1 = t;
+        // Store results
+        double sums[2];
+        double errs[2];
+        _mm_storeu_pd(&sums[0], sum1);
+        _mm_storeu_pd(&errs[0], err1);
 
-        double temp;
+        // First Kahan & pairwise summation
+        // 0+1 -> 0
+        sumNeumanierNormal(sums[0], errs[0], sums[1]);
+        sumNeumanierNormal(sums[0], errs[0], errs[1]);
 
-        _mm_storel_pd(&temp, sum1);
-        fSum += temp;
-
-        _mm_storeh_pd(&temp, sum1);
-        fSum += temp;
+        // Store result
+        return KahanSum(sums[0], errs[0]);
     }
-    return fSum;
+    return 0.0;
 #else
+    SAL_WARN("sc", "Failed to use SSE2");
     (void)i;
+    (void)nSize;
     (void)pCurrent;
     return 0.0;
 #endif
 }
 }
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
