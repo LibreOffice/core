LibreOffice/core

commit 9090dc1f3b27195f5defd35586ac79357992be21

split OutputDevice from Window
as part of a longer-term goal of doing our
widget rendering only inside a top-level render-
context.

I moved all of the OutputDevice-related code that existed in vcl::Window
into a new subclass of OutputDevice called WindowOutputDevice.

Notes for further work

(*) not sure why we are getting an 1x1 surface in
    SvpSalGraphics::releaseCairoContext, but to fix it I clamp
    the size there

(*) might have to dump VCLXDevice, and move it's code down into VCLXWindow and VCLXVirtualDevice

(*) can we remove use of VCLXDevice in other places, in favour of just talking to the VCL code?

Change-Id: I105946377f5322677d6f7d0c1c23847178a720b6
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/113204
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

canvas/source/vcl/outdevholder.hxx
@@ -35,6 +35,7 @@ public:
     explicit OutDevHolder(OutputDevice& rOutDev)
         : mrOutDev(rOutDev)
     {
+        assert(&rOutDev);
     }
 
 private:

commit 9f082817bdd0ff92b413a95e9fca0cebcbcdd7f2

Drop assert checking address of reference is not nullptr
It was introduced in

    commit 9090dc1f3b27195f5defd35586ac79357992be21
    Date:   Mon Jan 18 18:27:19 2021 +0200

        split OutputDevice from Window

and caused my GCC 10 '--enable-werror' build to fail with

    In file included from /usr/include/c++/10/cassert:44,
                     from .../include/tools/ref.hxx:23,
                     from .../include/vcl/outdev.hxx:24,
                     from .../canvas/source/vcl/impltools.hxx:25,
                     from .../canvas/source/vcl/canvas.hxx:39,
                     from .../canvas/source/vcl/canvas.cxx:22:
    .../canvas/source/vcl/outdevholder.hxx: In constructor ‘vclcanvas::OutDevHolder::OutDevHolder(OutputDevice&)’:
    .../canvas/source/vcl/outdevholder.hxx:38:16: error: the compiler can assume that the address of ‘rOutDev’ will never be NULL [-Werror=address]
       38 |         assert(&rOutDev);
          |                ^~~~~~~~
    cc1plus: all warnings being treated as errors
    make[1]: *** [.../solenv/gbuild/LinkTarget.mk:301: /home/michi/development/git/libreoffice/workdir/CxxObject/canvas/source/vcl/canvas.o] Error 1
    make[1]: *** Waiting for unfinished jobs....
    In file included from .../include/comphelper/solarmutex.hxx:25,
                     from .../include/vcl/svapp.hxx:29,
                     from .../canvas/source/vcl/impltools.hxx:24,
                     from .../canvas/source/vcl/spritecanvas.hxx:44,
                     from .../canvas/source/vcl/spritecanvas.cxx:28:
    .../canvas/source/vcl/outdevholder.hxx: In constructor ‘vclcanvas::OutDevHolder::OutDevHolder(OutputDevice&)’:
    .../canvas/source/vcl/outdevholder.hxx:38:16: error: the compiler can assume that the address of ‘rOutDev’ will never be NULL [-Werror=address]
       38 |         assert(&rOutDev);

Change-Id: I9e8ef7e91dba847f4894124348f13755fc620fac

canvas/source/vcl/outdevholder.hxx
@@ -35,7 +35,6 @@ public:
     explicit OutDevHolder(OutputDevice& rOutDev)
         : mrOutDev(rOutDev)
     {
-        assert(&rOutDev);
     }
 
 private: