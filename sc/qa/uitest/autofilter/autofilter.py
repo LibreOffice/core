0a49492147bdd9544c576a6495030a248f257c69
Tünde Tóth <toth.tunde@nisz.hu>
2021-05-18 15:41:51 +0200

    tdf#142350 sc AutoFilter: fix (empty) entry checking
    When a column was filtered for values that included 0,
    but not the (empty) entry, the (empty) entry also
    was checked in the Autofilter dropdown.
    
    Regression from commit 4fd1333ba4bb4f2311e9098291154772bd310429
    "tdf#140968 tdf#140978 XLSX import: fix lost rounded filters".
    
    Change-Id: Ic2595c707bd43f1a19c86d2ee796f06d9b1af1e3
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/115755
    Tested-by: László Németh <nemeth@numbertext.org>
    Reviewed-by: László Németh <nemeth@numbertext.org>
    (cherry picked from commit 45a49b7a1b552f204e7b417cfbe2d86e4b169993)

--- sc/qa/uitest/autofilter/autofilter.py
+++ sc/qa/uitest/autofilter/autofilter.py
@@ -313,4 +313,83 @@ class AutofilterTest(UITestCase):
         xOkBtn.executeAction("CLICK", tuple())
 
         self.ui_test.close_doc()
+<<<<<<< HEAD   (eab109 tdf#57351 sc UI: fix disabled Insert Columns options)
+=======
+
+    def test_tdf140968(self):
+        doc = self.ui_test.load_file(get_url_for_data_file("tdf140968.xlsx"))
+
+        xGridWin = self.xUITest.getTopFocusWindow().getChild("grid_window")
+
+        xGridWin.executeAction("LAUNCH", mkPropertyValues({"AUTOFILTER": "", "COL": "0", "ROW": "0"}))
+        xFloatWindow = self.xUITest.getFloatWindow()
+        xCheckListMenu = xFloatWindow.getChild("check_list_menu")
+        xTreeList = xCheckListMenu.getChild("check_list_box")
+        self.assertEqual(4, len(xTreeList.getChildren()))
+        self.assertEqual("0.000", get_state_as_dict(xTreeList.getChild('0'))['Text'])
+        self.assertEqual("0.046", get_state_as_dict(xTreeList.getChild('1'))['Text'])
+        self.assertEqual("0.365", get_state_as_dict(xTreeList.getChild('2'))['Text'])
+        self.assertEqual("0.516", get_state_as_dict(xTreeList.getChild('3'))['Text'])
+
+        xFirstEntry = xTreeList.getChild("0")
+        xFirstEntry.executeAction("CLICK", tuple())
+        xFirstEntry = xTreeList.getChild("1")
+        xFirstEntry.executeAction("CLICK", tuple())
+
+        xOkBtn = xFloatWindow.getChild("ok")
+        xOkBtn.executeAction("CLICK", tuple())
+
+        self.assertFalse(is_row_hidden(doc, 0))
+        self.assertTrue(is_row_hidden(doc, 1))
+        self.assertTrue(is_row_hidden(doc, 2))
+        self.assertTrue(is_row_hidden(doc, 3))
+        self.assertTrue(is_row_hidden(doc, 4))
+        self.assertFalse(is_row_hidden(doc, 5))
+        self.assertFalse(is_row_hidden(doc, 6))
+
+        self.ui_test.close_doc()
+
+    def test_tdf142350(self):
+        self.ui_test.create_doc_in_start_center("calc")
+        document = self.ui_test.get_component()
+        calcDoc = self.xUITest.getTopFocusWindow()
+        gridwin = calcDoc.getChild("grid_window")
+        document = self.ui_test.get_component()
+
+        enter_text_to_cell(gridwin, "A1", "A")
+        enter_text_to_cell(gridwin, "A2", "0")
+        enter_text_to_cell(gridwin, "A3", "")
+        enter_text_to_cell(gridwin, "A4", "1")
+
+        gridwin.executeAction("SELECT", mkPropertyValues({"RANGE": "A1:A4"}))
+
+        self.xUITest.executeCommand(".uno:DataFilterAutoFilter")
+
+        gridwin.executeAction("LAUNCH", mkPropertyValues({"AUTOFILTER": "", "COL": "0", "ROW": "0"}))
+        xFloatWindow = self.xUITest.getFloatWindow()
+        xCheckListMenu = xFloatWindow.getChild("check_list_menu")
+        xList = xCheckListMenu.getChild("check_list_box")
+        xEntry = xList.getChild("2")
+        xEntry.executeAction("CLICK", tuple())
+
+        xOkButton = xFloatWindow.getChild("ok")
+        xOkButton.executeAction("CLICK", tuple())
+
+        self.assertFalse(is_row_hidden(document, 1))
+        self.assertTrue(is_row_hidden(document, 2))
+        self.assertFalse(is_row_hidden(document, 3))
+
+        gridwin.executeAction("LAUNCH", mkPropertyValues({"AUTOFILTER": "", "COL": "0", "ROW": "0"}))
+        xFloatWindow = self.xUITest.getFloatWindow()
+        xCheckListMenu = xFloatWindow.getChild("check_list_menu")
+        xList = xCheckListMenu.getChild("check_list_box")
+        self.assertEqual(3, len(xList.getChildren()))
+        self.assertEqual('true', get_state_as_dict(xList.getChild('0'))['IsChecked'])
+        self.assertEqual('true', get_state_as_dict(xList.getChild('1'))['IsChecked'])
+        self.assertEqual('false', get_state_as_dict(xList.getChild('2'))['IsChecked'])
+        xCloseButton = xFloatWindow.getChild("cancel")
+        xCloseButton.executeAction("CLICK", tuple())
+
+        self.ui_test.close_doc()
+>>>>>>> CHANGE (45a49b tdf#142350 sc AutoFilter: fix (empty) entry checking)
 # vim: set shiftwidth=4 softtabstop=4 expandtab:
