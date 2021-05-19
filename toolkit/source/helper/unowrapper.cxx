LibreOffice/core

commit 883b7a34d288410ac6f820e91d9eaa65ba2b9cb1

DBG_ASSERT->assert in UnoWrapper
Change-Id: Ibc6b489f12c2734869bd45f93f946a53e3015226
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/115564
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

toolkit/source/helper/unowrapper.cxx
@@ -35,7 +35,7 @@
 
 using namespace ::com::sun::star;
 
-static css::uno::Reference< css::awt::XWindowPeer > CreateXWindow( vcl::Window const * pWindow )
+static rtl::Reference<VCLXWindow> CreateXWindow( vcl::Window const * pWindow )
 {
     switch ( pWindow->GetType() )
     {
@@ -148,12 +148,13 @@ css::uno::Reference< css::awt::XToolkit> UnoWrapper::GetVCLToolkit()
 css::uno::Reference< css::awt::XWindowPeer> UnoWrapper::GetWindowInterface( vcl::Window* pWindow )
 {
     css::uno::Reference< css::awt::XWindowPeer> xPeer = pWindow->GetWindowPeer();
-    if ( !xPeer.is() )
-    {
-        xPeer = CreateXWindow( pWindow );
-        SetWindowInterface( pWindow, xPeer );
-    }
-    return xPeer;
+    if ( xPeer )
+        return xPeer;
+
+    rtl::Reference<VCLXWindow> xVCLXWindow = CreateXWindow( pWindow );
+    xVCLXWindow->SetWindow( pWindow );
+    pWindow->SetWindowPeer( xVCLXWindow, xVCLXWindow.get() );
+    return xVCLXWindow;
 }
 
 VclPtr<vcl::Window> UnoWrapper::GetWindow(const css::uno::Reference<css::awt::XWindow>& rWindow)
@@ -165,7 +166,7 @@ void UnoWrapper::SetWindowInterface( vcl::Window* pWindow, const css::uno::Refer
 {
     VCLXWindow* pVCLXWindow = comphelper::getUnoTunnelImplementation<VCLXWindow>( xIFace );
 
-    DBG_ASSERT( pVCLXWindow, "SetComponentInterface - unsupported type" );
+    assert( pVCLXWindow && "must be a VCLXWindow subclass" );
     if ( !pVCLXWindow )
         return;
 
@@ -173,7 +174,7 @@ void UnoWrapper::SetWindowInterface( vcl::Window* pWindow, const css::uno::Refer
     if( xPeer.is() )
     {
         bool bSameInstance( pVCLXWindow == dynamic_cast< VCLXWindow* >( xPeer.get() ));
-        DBG_ASSERT( bSameInstance, "UnoWrapper::SetWindowInterface: there already *is* a WindowInterface for this window!" );
+        assert( bSameInstance && "UnoWrapper::SetWindowInterface: there is already a WindowPeer/ComponentInterface for this VCL window" );
         if ( bSameInstance )
             return;
     }

commit 917aa77a4363316a29b1c2acec7e4e9af144b239

fix crash in X-Ray extension
caused by
    commit 883b7a34d288410ac6f820e91d9eaa65ba2b9cb1
    DBG_ASSERT->assert in UnoWrapper

No idea why X-ray is triggering this, but it seems to work when I dial
it back to a SAL_WARN again

And also caused by
    commit 5aa60be574ece81b27c8f63e6e809871c694dba0
    fix leak in VCLXWindow

So disconnect the toolkit-window-peer from the vcl::Window before
disposing the peer.

Change-Id: I3310b76c1affc13c53c1e23810e383a8d2152ec7

toolkit/source/helper/unowrapper.cxx
@@ -170,16 +170,24 @@ void UnoWrapper::SetWindowInterface( vcl::Window* pWindow, const css::uno::Refer
     if ( !pVCLXWindow )
         return;
 
-    css::uno::Reference< css::awt::XWindowPeer> xPeer = pWindow->GetWindowPeer();
-    if( xPeer.is() )
+    if (!pWindow)
     {
-        bool bSameInstance( pVCLXWindow == dynamic_cast< VCLXWindow* >( xPeer.get() ));
-        assert( bSameInstance && "UnoWrapper::SetWindowInterface: there is already a WindowPeer/ComponentInterface for this VCL window" );
-        if ( bSameInstance )
-            return;
+        // we are disconnecting a peer from a window
+        pVCLXWindow->SetWindow( nullptr );
     }
-    pVCLXWindow->SetWindow( pWindow );
-    pWindow->SetWindowPeer( xIFace, pVCLXWindow );
+    else
+    {
+        css::uno::Reference< css::awt::XWindowPeer> xPeer = pWindow->GetWindowPeer();
+        if( xPeer.is() )
+        {
+            bool bSameInstance( pVCLXWindow == dynamic_cast< VCLXWindow* >( xPeer.get() ));
+            SAL_WARN_IF( !bSameInstance, "toolkit.helper", "UnoWrapper::SetWindowInterface: there is already a WindowPeer/ComponentInterface for this VCL window" );
+            if ( bSameInstance )
+                return;
+        }
+        pVCLXWindow->SetWindow( pWindow );
+        pWindow->SetWindowPeer( xIFace, pVCLXWindow );
+    }
 }
 
 css::uno::Reference<css::awt::XPopupMenu> UnoWrapper::CreateMenuInterface( PopupMenu* pPopupMenu )

vcl/source/window/window.cxx
@@ -3129,6 +3129,11 @@ void Window::SetWindowPeer( Reference< css::awt::XWindowPeer > const & xPeer, VC
     // be safe against re-entrance: first clear the old ref, then assign the new one
     if (mpWindowImpl->mxWindowPeer)
     {
+        // first, disconnect the peer from ourself, otherwise disposing it, will dispose us
+        UnoWrapperBase* pWrapper = UnoWrapperBase::GetUnoWrapper();
+        SAL_WARN_IF( !pWrapper, "vcl.window", "SetComponentInterface: No Wrapper!" );
+        if ( pWrapper )
+            pWrapper->SetWindowInterface( nullptr, mpWindowImpl->mxWindowPeer );
         mpWindowImpl->mxWindowPeer->dispose();
         mpWindowImpl->mxWindowPeer.clear();
     }