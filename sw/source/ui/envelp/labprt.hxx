157fb80b9c147c2ab18cb81ee48eab0c8e54b36a
Caolán McNamara <caolanm@redhat.com>
2021-05-20 11:12:38 +0100

    split handlers into button click and radiobutton toggle
    Change-Id: I375d75518b09a14dd3a5033f2be394fb3e891838
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/115866
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>

--- sw/source/ui/envelp/labprt.hxx
+++ sw/source/ui/envelp/labprt.hxx
@@ -40,7 +40,8 @@ class SwLabPrtPage : public SfxTabPage
     std::unique_ptr<weld::Label> m_xPrinterInfo;
     std::unique_ptr<weld::Button> m_xPrtSetup;
 
-    DECL_LINK(CountHdl, weld::Button&, void);
+    DECL_LINK(CountHdl, weld::ToggleButton&, void);
+    DECL_LINK(PrtSetupHdl, weld::Button&, void);
 
     SwLabDlg* GetParentSwLabDlg() { return static_cast<SwLabDlg*>(GetDialogController()); }
 
