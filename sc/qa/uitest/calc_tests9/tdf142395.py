d287dc6a94efb6e2149c870f80af89795352d1cf
Xisco Fauli <xiscofauli@libreoffice.org>
2021-05-21 17:23:06 +0200

    tdf#142395: sc: Add UItest
    Change-Id: I428b77283a9828bd3902702fb23b3acc560dea94

--- sc/qa/uitest/calc_tests9/tdf142395.py
+++ sc/qa/uitest/calc_tests9/tdf142395.py
@@ -9,48 +9,44 @@ from uitest.uihelper.common import get_state_as_dict, get_url_for_data_file
 from libreoffice.uno.propertyvalue import mkPropertyValues
 from libreoffice.calc.document import get_cell_by_position
 
-class Tdf60468(UITestCase):
+class Tdf142395(UITestCase):
 
-    def test_tdf60468(self):
+    def test_tdf142395(self):
 
         # Load file from Open dialog
         self.ui_test.execute_dialog_through_command(".uno:Open")
 
         xOpenDialog = self.xUITest.getTopFocusWindow()
         xFileName = xOpenDialog.getChild("file_name")
-        xFileName.executeAction("TYPE", mkPropertyValues({"TEXT": get_url_for_data_file("tdf60468.csv")}))
+        xFileName.executeAction("TYPE", mkPropertyValues({"TEXT": get_url_for_data_file("tdf142395.csv")}))
 
         xOpenBtn = xOpenDialog.getChild("open")
         xOpenBtn.executeAction("CLICK", tuple())
 
         xDialog = self.xUITest.getTopFocusWindow()
-        self.assertEqual('true', get_state_as_dict(xDialog.getChild("tab"))['Selected'])
-        self.assertEqual('true', get_state_as_dict(xDialog.getChild("comma"))['Selected'])
-        self.assertEqual('true', get_state_as_dict(xDialog.getChild("semicolon"))['Selected'])
-        self.assertEqual('1', get_state_as_dict(xDialog.getChild("fromrow"))['Text'])
 
-        # Set text delimiter in case it's changed by another test
+        # Remove the text delimiter
         xTextDelimiter = xDialog.getChild("textdelimiter")
         xTextDelimiter.executeAction("TYPE", mkPropertyValues({"KEYCODE":"CTRL+A"}))
         xTextDelimiter.executeAction("TYPE", mkPropertyValues({"KEYCODE":"BACKSPACE"}))
-        xTextDelimiter.executeAction("TYPE", mkPropertyValues({"TEXT": "\""}))
 
         xOK = xDialog.getChild('ok')
         self.ui_test.close_dialog_through_button(xOK)
 
         document = self.ui_test.get_component()
 
-        # tdf#142040: Without the fix in place, this test would have failed with
-        # AssertionError: 'head1' != ''
-        for i in range(3):
-            self.assertEqual("head" + str(i + 1), get_cell_by_position(document, 0, i, 0).getString())
-
-        self.assertEqual("value1.1\nvalue1.2", get_cell_by_position(document, 0, 0, 1).getString())
+        self.assertEqual("a", get_cell_by_position(document, 0, 0, 0).getString())
+        self.assertEqual(" b", get_cell_by_position(document, 0, 1, 0).getString())
+        self.assertEqual(" c", get_cell_by_position(document, 0, 2, 0).getString())
 
         # Without the fix in place, this test would have failed with
-        # AssertionError: 'value2.1\n\tvalue2.2\nvalue2.3' != 'value2.1\n\tvalue2.2'
-        self.assertEqual("value2.1\n\tvalue2.2\nvalue2.3", get_cell_by_position(document, 0, 1, 1).getString())
-        self.assertEqual("value3", get_cell_by_position(document, 0, 2, 1).getString())
+        # AssertionError: ' ' != '晦餪'
+        self.assertEqual(" ", get_cell_by_position(document, 0, 3, 0).getString())
+
+        self.assertEqual("e", get_cell_by_position(document, 0, 0, 1).getString())
+        self.assertEqual(" f", get_cell_by_position(document, 0, 1, 1).getString())
+        self.assertEqual(" g", get_cell_by_position(document, 0, 2, 1).getString())
+        self.assertEqual(" ", get_cell_by_position(document, 0, 3, 1).getString())
 
         self.ui_test.close_doc()
 
