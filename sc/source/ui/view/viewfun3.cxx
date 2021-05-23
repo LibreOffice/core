3afecfd35e90893776b0666d81557f31812d64d1
Eike Rathke <erack@redhat.com>
2021-05-24 01:29:19 +0200

    Resolves: tdf#134675 Allow unrestricted pastes of same size in one dimension
    So copy-paste of for example one entire column onto more than 23
    columns is possible as that does not create multiple repetitions.
    
    Change-Id: I2b035afa1c04522db55569396a36b1bac57c590c
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/116031
    Reviewed-by: Eike Rathke <erack@redhat.com>
    Tested-by: Jenkins
    Signed-off-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- sc/source/ui/view/viewfun3.cxx
+++ sc/source/ui/view/viewfun3.cxx
@@ -884,7 +884,7 @@ bool ScViewFunc::PasteFromClip( InsertDeleteFlags nFlags, ScDocument* pClipDoc,
         return false;
     }
 
-    if (GetViewData().SelectionForbidsCellFill())
+    if (GetViewData().SelectionForbidsPaste(pClipDoc))
         return false;
 
     //  undo: save all or no content
