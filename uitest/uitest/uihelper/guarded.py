5f041ebd1c6a1321c3ea92c8ade14375eb85a6a8
Mike Kaganski <mike.kaganski@collabora.com>
2021-05-23 15:40:08 +0300

    editengine-columns: Create document model and dialog page
    Change-Id: I056aad9474ca18134d1f1686a53618cc9ab3d525

--- uitest/uitest/uihelper/guarded.py
+++ uitest/uitest/uihelper/guarded.py
@@ -18,6 +18,16 @@ def load_file(testCase, url):
     finally:
         testCase.ui_test.close_doc()
 
+# Calls UITest.close_doc at exit
+@contextmanager
+def create_doc_in_start_center(testCase, app):
+    testCase.ui_test.create_doc_in_start_center(app)
+    component = testCase.ui_test.get_component()
+    try:
+        yield component
+    finally:
+        testCase.ui_test.close_doc()
+
 # Calls UITest.close_dialog_through_button at exit
 @contextmanager
 def execute_dialog_through_action(testCase, ui_object, action, parameters = None, event_name = "DialogExecute", close_button = "ok"):
@@ -30,8 +40,8 @@ def execute_dialog_through_action(testCase, ui_object, action, parameters = None
 
 # Calls UITest.close_dialog_through_button at exit
 @contextmanager
-def execute_dialog_through_command(testCase, unoCommand, close_button = "ok"):
-    testCase.ui_test.execute_dialog_through_command(unoCommand)
+def execute_dialog_through_command(testCase, command, printNames=False, close_button = "ok"):
+    testCase.ui_test.execute_dialog_through_command(command, printNames)
     xDialog = testCase.xUITest.getTopFocusWindow()
     try:
         yield xDialog
