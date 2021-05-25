8e9f7edb39c66214532b4852397a0807f7cca518
Dennis Francis <dennis.francis@collabora.com>
2021-05-25 10:53:22 +0530

    sc: lok: apply the previous fix for selections too
    Conflicts:
    	editeng/source/editeng/impedit.cxx
    
    Change-Id: Ic1f6c1642da71e0ef8c23831786ae405dda21133
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/116427
    Reviewed-by: Luboš Luňák <l.lunak@collabora.com>
    Reviewed-by: Dennis Francis <dennis.francis@collabora.com>
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    (cherry picked from commit c5e249877b93e9d11788ec04fffee1dcb142a0e5)

--- editeng/source/editeng/impedit.cxx
+++ editeng/source/editeng/impedit.cxx
@@ -658,11 +658,14 @@ void ImpEditView::ImplDrawHighlightRect( OutputDevice& rTarget, const Point& rDo
 
     if (mpLOKSpecialPositioning && pPolyPoly)
     {
-        MapUnit eDevUnit = rTarget.GetMapMode().GetMapUnit();
+        MapUnit eDevUnit = _pTarget->GetMapMode().GetMapUnit();
         tools::Rectangle aSelRect(rDocPosTopLeft, rDocPosBottomRight);
-        aSelRect = mpLOKSpecialPositioning->GetWindowPos(aSelRect, eDevUnit);
-        const Point aRefPoint = mpLOKSpecialPositioning->GetRefPoint();
-        aSelRect.Move(-aRefPoint.X(), -aRefPoint.Y());
+        aSelRect = GetWindowPos(aSelRect);
+        Point aRefPointLogical = GetOutputArea().TopLeft();
+        // Get the relative coordinates w.r.t refpoint in display units.
+        aSelRect.Move(-aRefPointLogical.X(), -aRefPointLogical.Y());
+        // Convert from display unit to twips.
+        aSelRect = OutputDevice::LogicToLogic(aSelRect, MapMode(eDevUnit), MapMode(MapUnit::MapTwip));
 
         tools::Polygon aTmpPoly(4);
         aTmpPoly[0] = aSelRect.TopLeft();
