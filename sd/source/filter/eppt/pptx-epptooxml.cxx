LibreOffice/core

commit b6b02e0b4c9d739836e1f61a886ea45b01e6696e

tdf#111903 tdf#137152 PPTX export: fix placeholders
Empty placeholders were exported as white empty
custom shapes, losing their visibility and usability.

Note: export of properties hasn't been implemented, yet.

Change-Id: Ie8bd6a611f5fb43bcaa55f6b2f5b07daf731b163
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/114331
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

sd/source/filter/eppt/pptx-epptooxml.cxx
@@ -1439,13 +1439,22 @@ ShapeExport& PowerPointShapeExport::WritePageShape(const Reference< XShape >& xS
 bool PowerPointShapeExport::WritePlaceholder(const Reference< XShape >& xShape, PlaceholderType ePlaceholder, bool bMaster)
 {
     SAL_INFO("sd.eppt", "WritePlaceholder " << bMaster << " " << ShapeExport::NonEmptyText(xShape));
-    if (bMaster && ShapeExport::NonEmptyText(xShape))
+    if (!xShape)
+        return false;
+    try
     {
-        WritePlaceholderShape(xShape, ePlaceholder);
+        Reference<XPropertySet> xShapeProps(xShape, UNO_QUERY);
+        if (xShapeProps->getPropertyValue("IsPresentationObject").get<bool>())
+        {
+            WritePlaceholderShape(xShape, ePlaceholder);
 
-        return true;
+            return true;
+        }
     }
-
+    catch (Exception&)
+    {
+        return false;
+    }
     return false;
 }
 

sd/qa/unit/export-tests-ooxml1.cxx
@@ -69,6 +69,7 @@ public:
     void testN828390_4();
     void testN828390_5();
     void testFdo71961();
+    void testLostPlaceholders();
     void testN828390();
     void testBnc880763();
     void testBnc862510_5();
@@ -122,6 +123,7 @@ public:
     CPPUNIT_TEST(testN828390_4);
     CPPUNIT_TEST(testN828390_5);
     CPPUNIT_TEST(testFdo71961);
+    CPPUNIT_TEST(testLostPlaceholders);
     CPPUNIT_TEST(testN828390);
     CPPUNIT_TEST(testBnc880763);
     CPPUNIT_TEST(testBnc862510_5);
@@ -370,6 +372,37 @@ void SdOOXMLExportTest1::testN828390_5()
     xDocShRef->DoClose();
 }
 
+void SdOOXMLExportTest1::testLostPlaceholders()
+{
+    ::sd::DrawDocShellRef xDocShRef = loadURL(m_directories.getURLFromSrc(u"/sd/qa/unit/data/pptx/LostPlaceholder.odp"), ODP);
+    CPPUNIT_ASSERT(xDocShRef.is());
+
+    xDocShRef = saveAndReload( xDocShRef.get(), PPTX );
+    CPPUNIT_ASSERT(xDocShRef.is());
+
+    auto pDoc = xDocShRef->GetDoc();
+    CPPUNIT_ASSERT(pDoc);
+    auto pPage = pDoc->GetPage(1);
+    CPPUNIT_ASSERT(pPage);
+    auto pObj = pPage->GetObj(1);
+    CPPUNIT_ASSERT(pObj);
+    uno::Reference<drawing::XShape> xShp (pObj->getUnoShape(), uno::UNO_QUERY);
+    CPPUNIT_ASSERT(xShp);
+
+    CPPUNIT_ASSERT_EQUAL_MESSAGE("Wrong ShapeType!", OUString(u"com.sun.star.presentation.OutlinerShape"), xShp->getShapeType());
+    uno::Reference<beans::XPropertySet> xShpProps(xShp, uno::UNO_QUERY);
+    // Without the fix in place there will be the following error:
+    // Expected: com.sun.star.presentation.OutlinerShape
+    // Actual: com.sun.star.drawing.CustomShape
+
+    CPPUNIT_ASSERT_EQUAL_MESSAGE("It must be a placeholder!", true, xShpProps->getPropertyValue("IsPresentationObject").get<bool>());
+    // Without the fix in place this will the following:
+    // Expected: true
+    // Actual: false
+
+    xDocShRef->DoClose();
+}
+
 void SdOOXMLExportTest1::testFdo71961()
 {
     ::sd::DrawDocShellRef xDocShRef = loadURL(m_directories.getURLFromSrc(u"/sd/qa/unit/data/fdo71961.odp"), ODP);
@@ -379,7 +412,7 @@ void SdOOXMLExportTest1::testFdo71961()
 
     // Export to .pptx changes all text frames to custom shape objects, which obey TextWordWrap property
     // (which is false for text frames otherwise and is ignored). Check that frames that should wrap still do.
-    SdrObjCustomShape *pTxtObj = dynamic_cast<SdrObjCustomShape *>( pPage->GetObj( 1 ));
+    auto  pTxtObj = pPage->GetObj( 1 );
     CPPUNIT_ASSERT_MESSAGE( "no text object", pTxtObj != nullptr);
     CPPUNIT_ASSERT_EQUAL( OUString( "Text to be always wrapped" ), pTxtObj->GetOutlinerParaObject()->GetTextObject().GetText(0));
     CPPUNIT_ASSERT_EQUAL( true, pTxtObj->GetMergedItem(SDRATTR_TEXT_WORDWRAP).GetValue());

commit bae0aad9457f905c2bbaa6bbd98d0d2b7a6259f5

[WIP] PPTX export: fix placeholder style export
Regression from: b6b02e0b4c9d739836e1f61a886ea45b01e6696e
(tdf#111903 tdf#137152 PPTX export: fix placeholders)

Placeholders lost the formatting, which has been fixed.

Note: test under creation.
Change-Id: I2b9300b85cf3641d89d748a03377f950e668a2af

sd/source/filter/eppt/pptx-epptooxml.cxx
@@ -1468,9 +1468,33 @@ ShapeExport& PowerPointShapeExport::WritePlaceholderShape(const Reference< XShap
     WritePresetShape("rect");
     Reference< XPropertySet > xProps(xShape, UNO_QUERY);
     if (xProps.is())
+    {
         WriteBlipFill(xProps, "Graphic");
-    mpFS->endElementNS(XML_p, XML_spPr);
+        // Do not forget to export the visible properties.
+        WriteFill( xProps );
+        WriteOutline( xProps );
+        WriteShapeEffects( xProps );
 
+        bool bHas3DEffectinShape = false;
+        uno::Sequence<beans::PropertyValue> grabBag;
+        if (xProps->getPropertySetInfo()->hasPropertyByName("InteropGrabBag"))
+            xProps->getPropertyValue("InteropGrabBag") >>= grabBag;
+
+        for (auto const& it : std::as_const(grabBag))
+            if (it.Name == "3DEffectProperties")
+                bHas3DEffectinShape = true;
+
+        if( bHas3DEffectinShape)
+            WriteShape3DEffects( xProps );
+    }
+    mpFS->endElementNS(XML_p, XML_spPr);
+    if (xProps.is() && 0)
+    {
+        // Dont forget to export the styles too.
+        mpFS->startElementNS(XML_p, XML_style);
+        WriteShapeStyle(xProps);
+        mpFS->endElementNS(XML_p, XML_style);
+    }
     WriteTextBox(xShape, XML_p);
 
     mpFS->endElementNS(XML_p, XML_sp);