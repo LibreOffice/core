a0edcc68f94915a78fcc08e70d2cdd752abd9ebb
Luboš Luňák <l.lunak@collabora.com>
2021-05-25 12:13:24 +0200

    update Skia to chrome/m94
    Updating to m91 or newer required handling the problem with Vulkan
    crashing, but that's been finally fixed by the previous commit.
    
    This Skia version also requires at least freetype-2.8.1, so disable
    Skia if that's not available (LO's minimum is lower). Additionally
    patch out Skia's use of TT_SUPPORT_COLRV1, which seems to be
    an unstable freetype API from the git version and it doesn't even
    compile with the latest stable 2.9.11 release
    
    Change-Id: I4a17effb912468c43fc7cdb69fe453062e4d2447
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/120967
    Tested-by: Jenkins
    Reviewed-by: Luboš Luňák <l.lunak@collabora.com>

--- external/skia/UnpackedTarball_skia.mk
+++ external/skia/UnpackedTarball_skia.mk
@@ -11,8 +11,6 @@ $(eval $(call gb_UnpackedTarball_UnpackedTarball,skia))
 
 $(eval $(call gb_UnpackedTarball_set_tarball,skia,$(SKIA_TARBALL)))
 
-# * c++20.patch.0 has been reported upstream at
-#   <https://groups.google.com/forum/#!topic/skia-discuss/pYZQq_sLnv8> "C++20 operator== issue":
 skia_patches := \
     fix-pch.patch.1 \
     fix-ddi.patch \
@@ -21,7 +19,6 @@ skia_patches := \
     fix-alpha-difference-copy.patch.1 \
     libvulkan-name.patch.1 \
     share-grcontext.patch.1 \
-    fix-gcc-x86.patch.1	\
     clang11-flax-vector-conversion.patch.0 \
     clang-attributes-warning.patch.1 \
     fontconfig-get-typeface.patch.0 \
@@ -32,13 +29,13 @@ skia_patches := \
     windows-typeface-directwrite.patch.0 \
     windows-raster-surface-no-copies.patch.1 \
     fix-windows-dwrite.patch.1 \
-    c++20.patch.0 \
     constexpr-debug-std-max.patch.1 \
     swap-buffers-rect.patch.1 \
     ubsan.patch.0 \
     fast-png-write.patch.1 \
     skia_sk_cpu_sse_level_0_by_default.patch.1 \
     fix-warnings.patch.1 \
+    disable-freetype-colrv1.1 \
 
 $(eval $(call gb_UnpackedTarball_set_patchlevel,skia,1))
 
