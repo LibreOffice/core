d86889978d7caa4ac82eb4ede226c7f2561c8607
Noel Grandin <noel.grandin@collabora.co.uk>
2021-05-26 09:28:54 +0200

    fix leak in ScAcceptChgDlg
    Change-Id: I51636d04389c8493377fe906314ef5d8f5c372d2
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/116140
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

--- sc/source/ui/miscdlgs/acredlin.cxx
+++ sc/source/ui/miscdlgs/acredlin.cxx
@@ -278,6 +278,13 @@ void ScAcceptChgDlg::ClearView()
     nAcceptCount=0;
     nRejectCount=0;
     weld::TreeView& rTreeView = pTheView->GetWidget();
+    rTreeView.selected_foreach(
+        [&rTreeView](weld::TreeIter& rEntry)
+        {
+            ScRedlinData *pEntryData = reinterpret_cast<ScRedlinData*>(rTreeView.get_id(rEntry).toInt64());
+            delete pEntryData;
+            return false;
+        });
     rTreeView.freeze();
     rTreeView.clear();
     rTreeView.thaw();
