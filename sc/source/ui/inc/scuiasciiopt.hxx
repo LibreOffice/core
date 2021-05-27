dba4f1031ef5b308f60f354c160187321cba6bbe
Pedro Pinto Silva <pedro.silva@collabora.com>
2021-05-27 16:01:49 +0200

    Dialog: Import CSV: Improve layout
    Remove extra padding from headers, they are already in bold no need
    to add extra padding to emphasis hierarchy.
    
    Do not use the first header to repeat what the dialog title already says
    "Import", use instead "Character encoding" as the first header of the
    first group (advantage: 1st dropdown does not need extra labeling)
    
    The 2 radio buttons for Use/not use separators appear to be
    grouping the elements underneath them. This is wrong, all those options
    are actually related only to the user chooses to use separators.
     - Convert those two radio buttons in one check button labeled "Use separators"
       - Fixes the confusion above mentioned
    	 - Reduced the amount of items
    	 - It does force the user to do trial and error in order to understand
    	 what is what
    
    Change-Id: Iee63a6dd336d7c5a4e6f5ce8c845b32902e49a73
    Signed-off-by: Pedro Pinto Silva <pedro.silva@collabora.com>
    Signed-off-by: Mert Tumer <mert.tumer@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/116265
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/118091

--- sc/source/ui/inc/scuiasciiopt.hxx
+++ sc/source/ui/inc/scuiasciiopt.hxx
@@ -48,7 +48,6 @@ class ScImportAsciiDlg : public weld::GenericDialogController
     ScImportAsciiCall           meCall;             /// How the dialog is called (see asciiopt.hxx)
     bool                        mbDetectSpaceSep;   /// Whether to detect a possible space separator.
 
-    std::unique_ptr<weld::Label> mxFtCharSet;
     std::unique_ptr<SvxTextEncodingBox> mxLbCharSet;
     std::unique_ptr<weld::Label> mxFtCustomLang;
     std::unique_ptr<SvxLanguageBox> mxLbCustomLang;
@@ -56,8 +55,7 @@ class ScImportAsciiDlg : public weld::GenericDialogController
     std::unique_ptr<weld::Label> mxFtRow;
     std::unique_ptr<weld::SpinButton> mxNfRow;
 
-    std::unique_ptr<weld::RadioButton> mxRbFixed;
-    std::unique_ptr<weld::RadioButton> mxRbSeparated;
+    std::unique_ptr<weld::CheckButton> mxRbSeparated;
 
     std::unique_ptr<weld::CheckButton> mxCkbTab;
     std::unique_ptr<weld::CheckButton> mxCkbSemicolon;
