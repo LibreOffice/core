883acaed5b5e600f1fee94a85bd9c4b57831d007
Justin Luth <justin_luth@sil.org>
2021-05-28 16:03:52 +0200

    tdf#138302 partial revert tdf#108496: DOCX: redesign of override in lists
    LO 6.4.5 commit	cf13fe3e6f6a40f6db064d65d4514d13a23a31f0
    was only concerned about DOCX, but just elinated shared code.
    So restore the shared code and just don't execute it for DOCX.
    
    It doesn't look like RTF was working before this,
    but this does fix DOC format.
    I looked for existing unit tests in rtf and doc,
    but none were found that hit this code.
    
    Change-Id: Id624f73181384f38e1ef9f27575e0fb82eea19c7
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/116349
    Tested-by: Jenkins
    Reviewed-by: Vasily Melenchuk <vasily.melenchuk@cib.de>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/116433
    Tested-by: Thorsten Behrens <thorsten.behrens@allotropia.de>
    Reviewed-by: Thorsten Behrens <thorsten.behrens@allotropia.de>

--- sw/source/filter/ww8/ww8atr.cxx
+++ sw/source/filter/ww8/ww8atr.cxx
@@ -3649,6 +3649,13 @@ void AttributeOutputBase::ParaNumRule( const SwNumRuleItem& rNumRule )
                     }
                 }
             }
+            else if (pTextNd->IsListRestart())
+            {
+                sal_uInt16 nStartWith = static_cast<sal_uInt16>(pTextNd->GetActualListStartValue());
+                nNumId = GetExport().DuplicateNumRule(pRule, nLvl, nStartWith);
+                if (USHRT_MAX != nNumId)
+                    ++nNumId;
+            }
         }
         else
         {
