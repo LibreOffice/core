aab0de255fd34d4aa719b5189f83e827840a2632
Anshu <anshukhare50@gmail.com>
2021-05-28 18:36:51 +0530

    Making a treeview for character styles
    Change-Id: I864db4d3d16c506b2adc470d57341635dc696c7f

--- sfx2/source/inc/templdgi.hxx
+++ sfx2/source/inc/templdgi.hxx
@@ -100,8 +100,11 @@ protected:
 
     std::unique_ptr<weld::TreeView> mxFmtLb;
     std::unique_ptr<weld::TreeView> mxTreeBox;
+    std::unique_ptr<weld::TreeView> mxCharTreeBox;
     std::unique_ptr<weld::CheckButton> mxPreviewCheckbox;
+    std::unique_ptr<weld::CheckButton> mxCharPreviewCheckbox;
     std::unique_ptr<weld::ComboBox> mxFilterLb;
+    std::unique_ptr<weld::ComboBox> mxCharFilterLb;
     std::unique_ptr<TreeViewDropTarget> m_xTreeView1DropTargetHelper;
     std::unique_ptr<TreeViewDropTarget> m_xTreeView2DropTargetHelper;
 
