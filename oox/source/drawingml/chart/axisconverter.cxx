d862d1f246a8ba7cfb6705103f3b493bdd3e9a24
Tünde Tóth <toth.tunde@nisz.hu>
2021-06-02 11:51:13 +0200

    tdf#140623 Chart OOXML import: set text overlap to false
    of category axis label unless the rotation is 0 in xml.
    
    Regression from commit: 21620f9d2f50e66dffc45a5afb539edb8d54434c
    (tdf#138194 Chart OOXML import: set text break to true)
    
    Change-Id: I18db7483f49c84a83760200037f8858a3b471994
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/116575
    Tested-by: László Németh <nemeth@numbertext.org>
    Reviewed-by: László Németh <nemeth@numbertext.org>
    (cherry picked from commit 6185d1ff0130b3d178d5e50eeb6944ab70db41f9)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/116905
    Tested-by: Jenkins
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- oox/source/drawingml/chart/axisconverter.cxx
+++ oox/source/drawingml/chart/axisconverter.cxx
@@ -271,10 +271,12 @@ void AxisConverter::convertFromModel(const Reference<XCoordinateSystem>& rxCoord
                 }
                 else
                 {
-                    // do not overlap text when the rotation is undefined in xml
-                    bool bTextOverlap
-                        = mrModel.mxTextProp.is()
-                          && mrModel.mxTextProp->getTextProperties().moRotation.has();
+                    // do not overlap text unless the rotation is 0 in xml
+                    bool bTextOverlap = false;
+                    if (mrModel.mxTextProp.is()
+                        && mrModel.mxTextProp->getTextProperties().moRotation.has())
+                        bTextOverlap
+                            = mrModel.mxTextProp->getTextProperties().moRotation.get() == 0;
                     aAxisProp.setProperty(PROP_TextOverlap, bTextOverlap);
                     /* do not break text into several lines unless the rotation is 0 degree,
                        or the rotation is 90 degree and the inner size of the chart is not fixed,
