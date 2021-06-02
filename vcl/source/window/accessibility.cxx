6dc3db49265ba087f45f06d8baa85251ff0348e0
Noel Grandin <noel.grandin@collabora.co.uk>
2021-06-02 12:03:23 +0200

    make this warning fire less often
    Change-Id: Ie7eb634ce75b8156fae02f48429deca6d63c0251
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/116579
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

--- vcl/source/window/accessibility.cxx
+++ vcl/source/window/accessibility.cxx
@@ -445,7 +445,7 @@ void Window::SetAccessibleDescription( const OUString& rDescription )
     if ( ! mpWindowImpl->mpAccessibleInfos )
         mpWindowImpl->mpAccessibleInfos.reset( new ImplAccessibleInfos );
 
-    SAL_WARN_IF( mpWindowImpl->mpAccessibleInfos->pAccessibleDescription, "vcl", "AccessibleDescription already set!" );
+    SAL_WARN_IF( mpWindowImpl->mpAccessibleInfos->pAccessibleDescription != rDescription, "vcl", "AccessibleDescription already set!" );
     mpWindowImpl->mpAccessibleInfos->pAccessibleDescription = rDescription;
 }
 
