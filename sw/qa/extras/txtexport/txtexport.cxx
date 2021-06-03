7ef571cbc8d7ddf7ab129c43a43dbf1346ed5ab0
tobias <tobias.schulz@hotmail.com>
2021-06-03 16:37:54 +0200

    tdf#120574 Store Ascii Options for Later Saving
    store auto detected/configured ascii options in medium.
    use stored ascii settings for saving and text encoding
    settings dialog.
    
    Change-Id: I88e2d54923bfcf5d7c460ef3b89c36fc002c5097

--- sw/qa/extras/txtexport/txtexport.cxx
+++ sw/qa/extras/txtexport/txtexport.cxx
@@ -64,6 +64,43 @@ DECLARE_TXTEXPORT_TEST(testBullets, "bullets.odt")
     CPPUNIT_ASSERT_EQUAL(aExpected, aData);
 }
 
+DECLARE_TXTEXPORT_TEST(testTdf120574_utf8, "UTF8BOMCRLF.txt")
+{
+    SvMemoryStream aMemoryStream;
+    SvFileStream aStream(maTempFile.GetURL(), StreamMode::READ);
+    aStream.ReadStream(aMemoryStream);
+    const char* pData = static_cast<const char*>(aMemoryStream.GetData());
+
+    CPPUNIT_ASSERT_EQUAL('\xEF', pData[0]);
+    CPPUNIT_ASSERT_EQUAL('\xBB', pData[1]);
+    CPPUNIT_ASSERT_EQUAL('\xBF', pData[2]);
+
+    OUString aData
+        = OStringToOUString(OString(pData + 3, aMemoryStream.GetSize() - 3), RTL_TEXTENCODING_UTF8);
+
+    OUString aExpected(u"フー\r\nバー\r\n");
+
+    CPPUNIT_ASSERT_EQUAL(aExpected, aData);
+}
+
+DECLARE_TXTEXPORT_TEST(testTdf120574_utf16le, "UTF16LECRLF.txt")
+{
+    SvMemoryStream aMemoryStream;
+    SvFileStream aStream(maTempFile.GetURL(), StreamMode::READ);
+    aStream.ReadStream(aMemoryStream);
+
+    const char16_t* pData = static_cast<const char16_t*>(aMemoryStream.GetData());
+    const int16_t* pBom = static_cast<const int16_t*>(aMemoryStream.GetData());
+
+    CPPUNIT_ASSERT_EQUAL(static_cast<int16_t>(0xFEFF), *pBom);
+
+    OUString aData(static_cast<const char16_t*>(pData + 1),
+                   aMemoryStream.GetSize() / sizeof(char16_t) - 1);
+
+    OUString aExpected(u"フー\r\nバー\r\n");
+
+    CPPUNIT_ASSERT_EQUAL(aExpected, aData);
+}
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
