b1d135484940db34bc84940c06a2df1740d19469
Heiko Tietze <tietze.heiko@gmail.com>
2021-06-04 14:42:58 +0200

    Resolves tdf#140360 - Provide option to turn off sidebar's minimum width
    Boolean option Sidebar:General:MinimumWidth introduced defaulting
    to true; if false the sidebar width is under full user control
    
    Change-Id: Iab1413a83adf90461feb5d6416aad174439392ca
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/116715
    Tested-by: Jenkins
    Reviewed-by: Jim Raykowski <raykowj@gmail.com>
    (cherry picked from commit f43fdcad400b6ff51f0810d923fe75d5a2cfde0c)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/117414
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- sfx2/source/sidebar/SidebarController.cxx
+++ sfx2/source/sidebar/SidebarController.cxx
@@ -136,6 +136,7 @@ SidebarController::SidebarController (
       maRequestedContext(),
       mnRequestedForceFlags(SwitchFlag_NoForce),
       mnMaximumSidebarWidth(officecfg::Office::UI::Sidebar::General::MaximumWidth::get()),
+      mbMinimumSidebarWidth(officecfg::Office::UI::Sidebar::General::MinimumWidth::get()),
       msCurrentDeckId(gsDefaultDeckId),
       maPropertyChangeForwarder([this](){ return this->BroadcastPropertyChange(); }),
       maContextChangeUpdate([this](){ return this->UpdateConfigurations(); }),
@@ -376,7 +377,7 @@ void SAL_CALL SidebarController::requestLayout()
     if (mpCurrentDeck && !mpCurrentDeck->isDisposed())
     {
         mpCurrentDeck->RequestLayout();
-        nMinimalWidth = mpCurrentDeck->GetMinimalWidth();
+        nMinimalWidth = mbMinimumSidebarWidth ? mpCurrentDeck->GetMinimalWidth() : 0;
     }
     RestrictWidth(nMinimalWidth);
 }
@@ -471,7 +472,7 @@ void SidebarController::NotifyResize()
         VclPtr<DeckTitleBar> pTitleBar = mpCurrentDeck->GetTitleBar();
         if (pTitleBar && pTitleBar->IsVisible())
             pTitleBar->SetCloserVisible(CanModifyChildWindowWidth());
-        nMinimalWidth = mpCurrentDeck->GetMinimalWidth();
+        nMinimalWidth = mbMinimumSidebarWidth ? mpCurrentDeck->GetMinimalWidth() : 0;
     }
 
     RestrictWidth(nMinimalWidth);
