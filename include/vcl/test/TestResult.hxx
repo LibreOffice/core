84b18b2dcca970449e38ea683ddab88ed7ea100d
homeboy445 <akshitsan13@gmail.com>
2021-06-07 15:52:34 +0530

    Added the VCL tests for first run/upgrades
    Change-Id: I59c94b57a1f3f3a9cba55af1067a9702b6da98d8

---
+++ include/vcl/test/TestResult.hxx
@@ -0,0 +1,26 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#pragma once
+
+/** Rendering test result.
+ *
+ * Test either "Passed", "Failed" or "PassedWithQuirks" which means
+ * the test passed but at least one rendering quirk was detected.
+ */
+namespace vcl::test
+{
+enum class TestResult
+{
+    Failed,
+    PassedWithQuirks,
+    Passed
+};
+}
