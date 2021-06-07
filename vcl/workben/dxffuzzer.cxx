01c20a75b388ded73a4f220970cf3c2e95f83dff
Caolán McNamara <caolanm@redhat.com>
2021-06-07 20:32:26 +0100

    ofz#35033 missing components
    Change-Id: I5b0322d026f819dde0a1a2473087e56ebb75008a
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/116801
    Tested-by: Caolán McNamara <caolanm@redhat.com>
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>

--- vcl/workben/dxffuzzer.cxx
+++ vcl/workben/dxffuzzer.cxx
@@ -19,6 +19,8 @@ extern "C" {
 void * com_sun_star_i18n_LocaleDataImpl_get_implementation( void *, void * );
 void * com_sun_star_i18n_BreakIterator_Unicode_get_implementation( void *, void * );
 void * com_sun_star_i18n_BreakIterator_get_implementation( void *, void * );
+void * com_sun_star_i18n_NativeNumberSupplier_get_implementation( void *, void * );
+void * com_sun_star_i18n_NumberFormatCodeMapper_get_implementation( void *, void * );
 }
 
 const lib_to_factory_mapping *
@@ -38,6 +40,8 @@ lo_get_constructor_map(void)
         { "com_sun_star_i18n_LocaleDataImpl_get_implementation", com_sun_star_i18n_LocaleDataImpl_get_implementation },
         { "com_sun_star_i18n_BreakIterator_Unicode_get_implementation", com_sun_star_i18n_BreakIterator_Unicode_get_implementation },
         { "com_sun_star_i18n_BreakIterator_get_implementation", com_sun_star_i18n_BreakIterator_get_implementation },
+        { "com_sun_star_i18n_NativeNumberSupplier_get_implementation", com_sun_star_i18n_NativeNumberSupplier_get_implementation },
+        { "com_sun_star_i18n_NumberFormatCodeMapper_get_implementation", com_sun_star_i18n_NumberFormatCodeMapper_get_implementation },
         { 0, 0 }
     };
 
