96f8ed6619a73655f9c3b94e6a4a3979cc67f551
Andrea Gelmini <andrea.gelmini@gelma.net>
2021-06-08 22:31:21 +0200

    Fix typos
    Change-Id: I3539edf26a793f89d38f3df376002f4ed4295343
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/116869
    Tested-by: Jenkins
    Reviewed-by: Adolfo Jayme Barrientos <fitojb@ubuntu.com>

--- writerfilter/source/filter/WriterFilter.cxx
+++ writerfilter/source/filter/WriterFilter.cxx
@@ -319,7 +319,7 @@ void WriterFilter::setTargetDocument(const uno::Reference<lang::XComponent>& xDo
     xSettings->setPropertyValue("InvertBorderSpacing", uno::makeAny(true));
     xSettings->setPropertyValue("CollapseEmptyCellPara", uno::makeAny(true));
     xSettings->setPropertyValue("TabOverflow", uno::makeAny(true));
-    // tdf#142404 TabOverSpacing (new for compatiblityMode15/Word2013+) is a subset of TabOverMargin
+    // tdf#142404 TabOverSpacing (new for compatibilityMode15/Word2013+) is a subset of TabOverMargin
     // (which applied to DOCX <= compatibilityMode14).
     // TabOverMargin looks at tabs beyond the normal text area,
     // while TabOverSpacing only refers to a tab beyond the paragraph margin.
