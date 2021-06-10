0bf160165a538be3db479871e8f958809c56ea1a
Noel Grandin <noel.grandin@collabora.co.uk>
2021-06-10 12:17:15 +0200

    loplugin:unnecessaryreturn in SvxColorValueSet::layoutToGivenHeight
    Change-Id: Ia42158597588fe802a2f06a6e8e59f372c62c022

--- include/svx/SvxColorValueSet.hxx
+++ include/svx/SvxColorValueSet.hxx
@@ -41,7 +41,7 @@ public:
     void addEntriesForXColorList(const XColorList& rXColorList, sal_uInt32 nStartIndex = 1);
     void addEntriesForColorSet(const std::set<Color>& rColorSet, const OUString& rNamePrefix);
     Size layoutAllVisible(sal_uInt32 nEntryCount);
-    Size layoutToGivenHeight(sal_uInt32 nHeight, sal_uInt32 nEntryCount);
+    void layoutToGivenHeight(sal_uInt32 nHeight, sal_uInt32 nEntryCount);
 
     virtual FactoryFunction GetUITestFactory() const override;
 };
