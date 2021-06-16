13cdfeff471b7d01fa2032eecd8f64ffc86fc8d4
Xisco Fauli <xiscofauli@libreoffice.org>
2021-06-17 00:42:25 +0200

    uitest: guard a couple of tests as an example
    Change-Id: I8d2ef520a7075781028a3a22cf12747beaad1050

--- uitest/uitest/uihelper/guarded.py
+++ uitest/uitest/uihelper/guarded.py
@@ -8,6 +8,11 @@
 #
 
 from contextlib import contextmanager
+import threading
+from uitest.config import MAX_WAIT
+from uitest.test import DialogNotExecutedException
+
+from libreoffice.uno.eventlistener import EventListener
 
 # Calls UITest.close_doc at exit
 @contextmanager
@@ -47,5 +52,34 @@ def execute_dialog_through_command(testCase, command, printNames=False, close_bu
         yield xDialog
     finally:
         testCase.ui_test.close_dialog_through_button(xDialog.getChild(close_button))
+
+# Executes an action which blocks while a dialog is shown.
+#
+# Args:
+#     action(callable): Will be called to show a dialog, and is expected
+#         to block while the dialog is shown.
+#     args(tuple, optional): The arguments to be passed to `action`
+#     close_button(str, optional): The name of a button on the dialog
+#         which will be clicked to close the dialog.
+#     printNames(boolean, optional): print all received event names
+def execute_blocking_action(testCase, action, args=(), close_button="ok", printNames=False):
+    thread = threading.Thread(target=action, args=args)
+    with EventListener(testCase._xContext, ["DialogExecute", "ModelessDialogExecute", "ModelessDialogVisible"], printNames=printNames) as event:
+        thread.start()
+        time_ = 0
+        # we are not necessarily opening a dialog, so wait much longer
+        while time_ < 10 * MAX_WAIT:
+            if event.executed:
+                xDialog = testCase.xUITest.getTopFocusWindow()
+                try:
+                    yield xDialog
+                finally:
+                    testCase.ui_test.close_dialog_through_button(xDialog.getChild(close_button))
+                    thread.join()
+                    return
+
+            time_ += DEFAULT_SLEEP
+            time.sleep(DEFAULT_SLEEP)
+    raise DialogNotExecutedException("did not execute a dialog for a blocking action")
 
 # vim: set shiftwidth=4 softtabstop=4 expandtab:
