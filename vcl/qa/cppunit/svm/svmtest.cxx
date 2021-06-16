d072786e06421658ec26d987c47bf194e853d54a
panoskorovesis <panoskorovesis@outlook.com>
2021-06-16 10:25:43 +0300

    Add Font cppunit test to vcl.
    The test creates a Font, sets custom attributes for it and then
    tests them.
    
    Change-Id: Ib1427392674c7728180e1ad00621e56950d495ca

--- vcl/qa/cppunit/svm/svmtest.cxx
+++ vcl/qa/cppunit/svm/svmtest.cxx
@@ -166,7 +166,7 @@ class SvmTest : public test::BootstrapFixture, public XmlTestTools
     void checkMapMode(const GDIMetaFile& rMetaFile);
     void testMapMode();
 
-    //void checkFont(const GDIMetaFile& rMetaFile);
+    void checkFont(const GDIMetaFile& rMetaFile);
     void testFont();
 
     void checkPushPop(const GDIMetaFile& rMetaFile);
@@ -1890,8 +1890,45 @@ void SvmTest::testMapMode()
     checkMapMode(readFile(u"mapmode.svm"));
 }
 
+void SvmTest::checkFont(const GDIMetaFile& rMetafile)
+{
+    xmlDocUniquePtr pDoc = dumpMeta(rMetafile);
+
+    assertXPathAttrs(pDoc, "/metafile/font[1]", {
+        {"color", "#ffffff"},
+        {"fillcolor", "#ffffff"},
+        {"name", "Liberation Sans"},
+        {"stylename", "Liberation Serif"},
+        {"width", "12"},
+        {"height", "12"},
+        {"orientation", "50"},
+        {"weight", "thin"},
+        {"vertical", "true"},
+    });
+}
+
 void SvmTest::testFont()
-{}
+{
+#if HAVE_MORE_FONTS
+    GDIMetaFile aGDIMetaFile;
+    ScopedVclPtrInstance<VirtualDevice> pVirtualDev;
+    setupBaseVirtualDevice(*pVirtualDev, aGDIMetaFile);
+
+    vcl::Font aFont(FontFamily::FAMILY_SCRIPT, Size(15, 15));
+    aFont.SetWeight(FontWeight::WEIGHT_THIN);
+    aFont.SetFamilyName("Liberation Sans");
+    aFont.SetStyleName("Liberation Serif");
+    aFont.SetFontHeight(12);
+    aFont.SetAverageFontWidth(12);
+    aFont.SetVertical(true);
+    aFont.SetOrientation(Degree10(50));
+
+    pVirtualDev->SetFont(aFont);
+
+    checkFont(writeAndReadStream(aGDIMetaFile));
+    checkFont(readFile(u"font.svm"));
+#endif
+}
 
 void SvmTest::checkPushPop(const GDIMetaFile& rMetaFile)
 {
