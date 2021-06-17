57835096d0a91620764972dff94313542748f884
Noel Grandin <noel.grandin@collabora.co.uk>
2021-06-17 15:48:38 +0200

    avoid maybe-uninitialised in FontConfig
    triggers inside the std::optional<PrintFont> with gcc 10.2.0
    
    Change-Id: Id34e46ddfff447cb48b886003f04203979c30732

--- vcl/unx/generic/fontmanager/fontconfig.cxx
+++ vcl/unx/generic/fontmanager/fontconfig.cxx
@@ -17,6 +17,12 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
+#ifdef __GNUC__
+// gcc 10.2.0 gets unhappy about one of the OString inside PrintFont at line 656
+// I have to turn it off here because the warning actually occurs inside rtl::OString
+#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
+#endif
+
 #include <memory>
 #include <string_view>
 
