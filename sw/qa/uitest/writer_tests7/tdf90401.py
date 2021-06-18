20b9e6a20d6ed3c1cda45a3f0c06f194a470a7a2
László Németh <nemeth@numbertext.org>
2021-06-18 13:03:17 +0200

    tdf#142902 DOCX export: remove personal info of comments and changes
    If Options → LibreOffice → Security → Security Options
    and Warnings → Options... → Security Options → Remove personal
    information on saving" is enabled.
    
    Use the same time (Unix epoch) for mandatory time stamps, and
    replace authors with "Author1", "Author2", ... and creator-initials
    with "1", "2", "3" etc., also to avoid of joining adjacent redline
    ranges.
    
    Note: to see the work of the unit test in Linux command line:
    
    (cd sw && make UITest_writer_tests7 UITEST_TEST_NAME="tdf90401.tdf90401.test_tdf142902_remove_personal_info_in_DOCX" SAL_USE_VCLPLUGIN=gen)
    
    Follow-up to commit 12da70f88517bf3c053afe1c504bb70bd27573f2
    "tdf#90401 xmloff: remove personal info of comments and changes".
    
    Change-Id: Ice996f171f5d82d13ce0ea2e4833696af0aab90c

--- sw/qa/uitest/writer_tests7/tdf90401.py
+++ sw/qa/uitest/writer_tests7/tdf90401.py
@@ -42,7 +42,10 @@ class tdf90401(UITestCase):
         xOptions = xDialog.getChild('options')
         def handle_options_dlg(dialog):
             xRemovePersonal = dialog.getChild('removepersonal')
-            xRemovePersonal.executeAction('CLICK', tuple())
+            if get_state_as_dict(xRemovePersonal)['Selected'] == "false":
+                xRemovePersonal.executeAction('CLICK', tuple())
+            self.ui_test.wait_until_property_is_updated(xRemovePersonal, "Selected", "true")
+            self.assertEqual(get_state_as_dict(xRemovePersonal)["Selected"], "true")
             #Click on its button Close
             xOkBtn = dialog.getChild('ok')
             xOkBtn.executeAction('CLICK', tuple())
@@ -75,6 +78,106 @@ class tdf90401(UITestCase):
 
             self.ui_test.load_file(systemPathToFileUrl(xFilePath))
             document = self.ui_test.get_component()
+
+            # Wait for async events to be processed
+            xToolkit = self.xContext.ServiceManager.createInstance('com.sun.star.awt.Toolkit')
+            xToolkit.processEventsToIdle()
+
+            # check removed personal info on comments
+
+            textfields = document.getTextFields()
+            author = ""
+            year = -1
+            for textfield in textfields:
+                if textfield.supportsService("com.sun.star.text.TextField.Annotation"):
+                    author = textfield.Author
+                    year = textfield.Date.Year
+            # This was 'Unknown Author'
+            self.assertEqual(author, 'Author2')
+            # This was 2021
+            self.assertEqual(year, 0)
+
+            # check removed personal info on tracked changes
+
+            self.ui_test.execute_modeless_dialog_through_command('.uno:AcceptTrackedChanges')
+            xTrackDlg = self.xUITest.getTopFocusWindow()
+            xTreeList = xTrackDlg.getChild('writerchanges')
+            state = get_state_as_dict(xTreeList)
+            # This was 'NL\t11/03/2020 19:19:05\t', containing personal info
+            self.assertEqual(state['SelectEntryText'], 'Author1\t01/01/1970 00:00:00\t')
+
+        self.ui_test.close_doc()
+
+   def test_tdf142902_remove_personal_info_in_DOCX(self):
+
+        # load a test document with a tracked change, and add a comment
+
+        writer_doc = self.ui_test.load_file(get_url_for_data_file('redline-para-join.docx'))
+        xWriterDoc = self.xUITest.getTopFocusWindow()
+        xWriterEdit = xWriterDoc.getChild('writer_edit')
+
+        document = self.ui_test.get_component()
+        selection = self.xUITest.executeCommand('.uno:SelectAll')
+        self.xUITest.executeCommand('.uno:InsertAnnotation')
+
+        # enable remove personal info security option
+
+        self.ui_test.execute_dialog_through_command('.uno:OptionsTreeDialog')  #optionsdialog
+        xDialog = self.xUITest.getTopFocusWindow()
+
+        xPages = xDialog.getChild('pages')
+        xGenEntry = xPages.getChild('0')
+        xSecurityPage = xGenEntry.getChild('6')
+        xSecurityPage.executeAction('SELECT', tuple())
+        # Click Button Options...
+        xOptions = xDialog.getChild('options')
+        def handle_options_dlg(dialog):
+            xRemovePersonal = dialog.getChild('removepersonal')
+            if get_state_as_dict(xRemovePersonal)['Selected'] == "false":
+                xRemovePersonal.executeAction('CLICK', tuple())
+            self.ui_test.wait_until_property_is_updated(xRemovePersonal, "Selected", "true")
+            self.assertEqual(get_state_as_dict(xRemovePersonal)["Selected"], "true")
+            #Click on its button Close
+            xOkBtn = dialog.getChild('ok')
+            xOkBtn.executeAction('CLICK', tuple())
+
+        self.ui_test.execute_blocking_action(xOptions.executeAction, args=('CLICK', ()),
+                dialog_handler=handle_options_dlg)
+
+        xOKBtn = xDialog.getChild('ok')
+        self.ui_test.close_dialog_through_button(xOKBtn)
+
+        # save and reload the document to remove personal info
+
+        with TemporaryDirectory() as tempdir:
+            xFilePath = os.path.join(tempdir, 'redline-para-join-tmp.docx')
+
+            # Save Copy as
+            self.ui_test.execute_dialog_through_command('.uno:SaveAs')
+            xDialog = self.xUITest.getTopFocusWindow()
+
+            xFileName = xDialog.getChild('file_name')
+            xFileName.executeAction('TYPE', mkPropertyValues({'KEYCODE':'CTRL+A'}))
+            xFileName.executeAction('TYPE', mkPropertyValues({'KEYCODE':'BACKSPACE'}))
+            xFileName.executeAction('TYPE', mkPropertyValues({'TEXT': xFilePath}))
+
+            xOpenBtn = xDialog.getChild('open')
+            self.ui_test.close_dialog_through_button(xOpenBtn)
+
+            # DOCX confirmation dialog is displayed
+            xWarnDialog = self.xUITest.getTopFocusWindow()
+            xOK = xWarnDialog.getChild("save")
+            self.ui_test.close_dialog_through_button(xOK)
+
+            # Close the Writer document
+            self.ui_test.close_doc()
+
+            self.ui_test.load_file(systemPathToFileUrl(xFilePath))
+            document = self.ui_test.get_component()
+
+            # Wait for async events to be processed
+            xToolkit = self.xContext.ServiceManager.createInstance('com.sun.star.awt.Toolkit')
+            xToolkit.processEventsToIdle()
 
             # check removed personal info on comments
 
