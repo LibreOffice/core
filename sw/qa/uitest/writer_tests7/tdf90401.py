e016e8cf63df2ebcd2f9cac0c7663f7d386f6b1c
László Németh <nemeth@numbertext.org>
2021-06-18 13:03:17 +0200

    tdf#142902 DOCX export: remove personal info of comments and changes
    If Options → LibreOffice → Security → Security Options
    and Warnings → Options... → Security Options → Remove personal
    information on saving" is enabled.
    
    Use the same time (Unix epoch) for mandatory time stamps, and
    replace authors with "Author1", "Author2", ... and creator-initials
    with "1", "2", "3" etc., also to avoid of joining adjacent redline
    ranges.
    
    Note: to see the work of the unit test in Linux command line:
    
    (cd sw && make UITest_writer_tests7 UITEST_TEST_NAME="tdf90401.tdf90401.test_tdf142902_remove_personal_info_in_DOCX" SAL_USE_VCLPLUGIN=gen)
    
    Follow-up to commit 12da70f88517bf3c053afe1c504bb70bd27573f2
    "tdf#90401 xmloff: remove personal info of comments and changes".
    
    Change-Id: Ice996f171f5d82d13ce0ea2e4833696af0aab90c

--- sw/qa/uitest/writer_tests7/tdf90401.py
+++ sw/qa/uitest/writer_tests7/tdf90401.py
@@ -40,7 +40,11 @@ class tdf90401(UITestCase):
 
                 with self.ui_test.execute_blocking_action(xOptions.executeAction, args=('CLICK', ()), close_button="") as dialog:
                     xRemovePersonal = dialog.getChild('removepersonal')
-                    xRemovePersonal.executeAction('CLICK', tuple())
+                    if get_state_as_dict(xRemovePersonal)['Selected'] == "false":
+                        xRemovePersonal.executeAction('CLICK', tuple())
+                    self.ui_test.wait_until_property_is_updated(xRemovePersonal, "Selected", "true")
+                    self.assertEqual(get_state_as_dict(xRemovePersonal)["Selected"], "true")
+
                     xOkBtn = dialog.getChild('ok')
                     # FIXME: we can't use close_dialog_through_button here, the dialog doesn't emit the
                     # event DialogClosed after closing
@@ -86,5 +90,86 @@ class tdf90401(UITestCase):
                     # This was 'NL\t11/03/2020 19:19:05\t', containing personal info
                     self.assertEqual(state['SelectEntryText'], 'Author1\t01/01/1970 00:00:00\t')
 
+
+   def test_tdf142902_remove_personal_info_in_DOCX(self):
+
+        # load a test document with a tracked change, and add a comment
+
+        with self.ui_test.load_file(get_url_for_data_file('redline-para-join.docx')) as writer_doc:
+
+            xWriterDoc = self.xUITest.getTopFocusWindow()
+            xWriterEdit = xWriterDoc.getChild('writer_edit')
+
+            selection = self.xUITest.executeCommand('.uno:SelectAll')
+            self.xUITest.executeCommand('.uno:InsertAnnotation')
+
+            # enable remove personal info security option
+
+            with self.ui_test.execute_dialog_through_command('.uno:OptionsTreeDialog') as xDialog:
+                xPages = xDialog.getChild('pages')
+                xGenEntry = xPages.getChild('0')
+                xSecurityPage = xGenEntry.getChild('6')
+                xSecurityPage.executeAction('SELECT', tuple())
+                # Click Button Options...
+                xOptions = xDialog.getChild('options')
+
+                with self.ui_test.execute_blocking_action(xOptions.executeAction, args=('CLICK', ()), close_button="") as dialog:
+                    xRemovePersonal = dialog.getChild('removepersonal')
+                    if get_state_as_dict(xRemovePersonal)['Selected'] == "false":
+                        xRemovePersonal.executeAction('CLICK', tuple())
+                    self.ui_test.wait_until_property_is_updated(xRemovePersonal, "Selected", "true")
+                    self.assertEqual(get_state_as_dict(xRemovePersonal)["Selected"], "true")
+
+                    xOkBtn = dialog.getChild('ok')
+                    # FIXME: we can't use close_dialog_through_button here, the dialog doesn't emit the
+                    # event DialogClosed after closing
+                    xOkBtn.executeAction('CLICK', tuple())
+
+            # save and reload the document to remove personal info
+
+            with TemporaryDirectory() as tempdir:
+                xFilePath = os.path.join(tempdir, 'redline-para-join-tmp.docx')
+
+                # Save Copy as
+                with self.ui_test.execute_dialog_through_command('.uno:SaveAs', close_button="open") as xDialog:
+                    xFileName = xDialog.getChild('file_name')
+                    xFileName.executeAction('TYPE', mkPropertyValues({'KEYCODE':'CTRL+A'}))
+                    xFileName.executeAction('TYPE', mkPropertyValues({'KEYCODE':'BACKSPACE'}))
+                    xFileName.executeAction('TYPE', mkPropertyValues({'TEXT': xFilePath}))
+
+                # DOCX confirmation dialog is displayed
+                xWarnDialog = self.xUITest.getTopFocusWindow()
+                xOK = xWarnDialog.getChild("save")
+                self.ui_test.close_dialog_through_button(xOK)
+
+                # Close the Writer document
+                self.ui_test.close_doc()
+
+                with self.ui_test.load_file(systemPathToFileUrl(xFilePath)) as writer_doc2:
+
+                    # check removed personal info on comments
+
+                    textfields = writer_doc2.getTextFields()
+                    author = ""
+                    year = -1
+                    for textfield in textfields:
+                        if textfield.supportsService("com.sun.star.text.TextField.Annotation"):
+                            author = textfield.Author
+                            year = textfield.Date.Year
+                    # This was 'Unknown Author'
+                    self.assertEqual(author, 'Author2')
+                    # This was 2021
+                    self.assertEqual(year, 0)
+
+                    # check removed personal info on tracked changes
+
+                    self.ui_test.execute_modeless_dialog_through_command('.uno:AcceptTrackedChanges')
+                    xTrackDlg = self.xUITest.getTopFocusWindow()
+                    xTreeList = xTrackDlg.getChild('writerchanges')
+                    state = get_state_as_dict(xTreeList)
+                    # This was 'NL\t11/03/2020 19:19:05\t', containing personal info
+                    self.assertEqual(state['SelectEntryText'], 'Author1\t01/01/1970 00:00:00\t')
+
+                    self.ui_test.close_doc()
 
 # vim: set shiftwidth=4 softtabstop=4 expandtab:
