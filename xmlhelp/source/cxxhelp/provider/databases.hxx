ff74a6eb3b4656e396368a41552699c6b642cfa4
Caolán McNamara <caolanm@redhat.com>
2021-06-23 20:47:38 +0100

    error: unknown type name 'OStringBuffer'
    Change-Id: I28a4a2581e6afb02aeb137932fbf884b109e4b5c
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/117748
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>

--- xmlhelp/source/cxxhelp/provider/databases.hxx
+++ xmlhelp/source/cxxhelp/provider/databases.hxx
@@ -28,7 +28,7 @@
 #include <vector>
 #include <osl/mutex.hxx>
 #include <rtl/ustring.hxx>
-#include <rtl/string.hxx>
+#include <rtl/strbuf.hxx>
 #include <com/sun/star/uno/Sequence.hxx>
 #include <com/sun/star/container/XHierarchicalNameAccess.hpp>
 #include <com/sun/star/lang/XMultiComponentFactory.hpp>
