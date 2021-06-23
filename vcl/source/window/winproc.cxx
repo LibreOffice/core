68e5c773d4c14778075f78279b07ba6ca1d19932
Henry Castro <hcastro@collabora.com>
2021-06-23 08:37:34 -0400

    lok: introduce Application::LOKHandleMouseEvent
    Entry point to call the ImplLOKHandleMouseEvent function
    mainly to process mouse events.
    
    Change-Id: I17513643733bf5990d41ab8cf47cdc322ed526ea
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/118846
    Tested-by: Szymon Kłos <szymon.klos@collabora.com>
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>

--- vcl/source/window/winproc.cxx
+++ vcl/source/window/winproc.cxx
@@ -2154,7 +2154,7 @@ static void ImplHandleUserEvent( ImplSVEvent* pSVEvent )
     }
 }
 
-static MouseEventModifiers ImplGetMouseMoveMode( SalMouseEvent const * pEvent )
+MouseEventModifiers ImplGetMouseMoveMode( SalMouseEvent const * pEvent )
 {
     MouseEventModifiers nMode = MouseEventModifiers::NONE;
     if ( !pEvent->mnCode )
@@ -2166,7 +2166,7 @@ static MouseEventModifiers ImplGetMouseMoveMode( SalMouseEvent const * pEvent )
     return nMode;
 }
 
-static MouseEventModifiers ImplGetMouseButtonMode( SalMouseEvent const * pEvent )
+MouseEventModifiers ImplGetMouseButtonMode( SalMouseEvent const * pEvent )
 {
     MouseEventModifiers nMode = MouseEventModifiers::NONE;
     if ( pEvent->mnButton == MOUSE_LEFT )
