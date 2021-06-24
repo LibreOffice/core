a9223a71a5dc68c305c36b078accdea077a6d2f0
Henry Castro <hcastro@collabora.com>
2021-06-24 07:48:19 -0400

    lok: sc: handle all local mouse tracking for Calc
    Change-Id: I11f3477c0f966d403e076fc73b7e5507ad6597f7
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/118877
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>

--- sc/source/ui/view/gridwin.cxx
+++ sc/source/ui/view/gridwin.cxx
@@ -2995,16 +2995,13 @@ void ScGridWindow::Tracking( const TrackingEvent& rTEvt )
     }
     else if ( rTEvt.IsTrackingEnded() )
     {
-        if ( !comphelper::LibreOfficeKit::isActive() )
-        {
-            // MouseButtonUp always with matching buttons (eg for test tool, # 63148 #)
-            // The tracking event will indicate if it was completed and not canceled.
-            MouseEvent aUpEvt( rMEvt.GetPosPixel(), rMEvt.GetClicks(),
-                                rMEvt.GetMode(), nButtonDown, rMEvt.GetModifier() );
-            MouseButtonUp( aUpEvt );
-        }
+        // MouseButtonUp always with matching buttons (eg for test tool, # 63148 #)
+        // The tracking event will indicate if it was completed and not canceled.
+        MouseEvent aUpEvt( rMEvt.GetPosPixel(), rMEvt.GetClicks(),
+                            rMEvt.GetMode(), nButtonDown, rMEvt.GetModifier() );
+        MouseButtonUp( aUpEvt );
     }
-    else if ( !comphelper::LibreOfficeKit::isActive() )
+    else
         MouseMove( rMEvt );
 }
 
