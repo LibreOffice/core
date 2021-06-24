LibreOffice/core

commit 14069d84174ca7a4e60db4d75912903e9679b643

configure: Improve help-options handling
This patch includes:

* A README.help.md as a general documentation for the different
  help types, the LO code and help-related build options.
* Adds --disable-xmlhelp for removing the xmlhelp support from the
  build. Disable for iOS, Android and Emscripten. This was partly
  included in HAVE_FEATURE_DESKTOP before.
* Rename WITH_HELP define to WITH_HELPPACKS, which reflects the
  actual usage AFAIK.
* Depend --with-omindex on --with-help=online and don't override
  the --with-help setting. Error out on conflicting options.
* Depend --with-helppack-integration on build help variants, which
  actually result in help packs.

Kind of reverts commit 2c38ea6d16b910294220cefaf8ae6a0683e6405a
("Building without --with-help is not supposed to disable help
functionality").

Change-Id: Ie4cb73905b3ed94e991d9f1bd75cfbd6de9da385
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/116222
Tested-by: Jenkins
Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>

configure.ac
@@ -981,6 +981,7 @@ ios*) # iOS
     enable_coinmp=no
     enable_lpsolve=no
     enable_extension_integration=no
+    enable_xmlhelp=no
     with_ppds=no
     if test "$enable_ios_simulator" = "yes"; then
         host=x86_64-apple-darwin
@@ -1080,6 +1081,7 @@ linux-android*)
     enable_lpsolve=no
     enable_odk=no
     enable_python=no
+    enable_xmlhelp=no
     _os=Android
 
     AC_DEFINE(HAVE_FT_FACE_GETCHARVARIANTINDEX)
@@ -1108,6 +1110,7 @@ emscripten)
     test_webdav=no
     enable_database_connectivity=no
     enable_lpsolve=no
+    enable_xmlhelp=no
     with_system_zlib=no
     with_theme="breeze"
     _os=Emscripten
@@ -1940,6 +1943,11 @@ AC_ARG_ENABLE(wasm-strip,
     AS_HELP_STRING([--enable-wasm-strip],
         [Strip the static build like for WASM/emscripten platform.]),
 ,enable_wasm_strip=yes)
+
+AC_ARG_ENABLE(xmlhelp,
+    AS_HELP_STRING([--disable-xmlhelp],
+        [Disable XML help support]),
+,enable_xmlhelp=yes)
 
 
 dnl ===================================================================
@@ -2977,6 +2985,9 @@ fi
 if test "$enable_extensions" = yes; then
     BUILD_TYPE="$BUILD_TYPE EXTENSIONS"
     AC_DEFINE(HAVE_FEATURE_EXTENSIONS)
+else
+    enable_extension_integration=no
+    enable_extension_update=no
 fi
 
 if test -z "$enable_scripting"; then
@@ -5296,6 +5307,97 @@ fi
 AC_SUBST(PKGFORMAT)
 
 dnl ===================================================================
+dnl handle help related options
+dnl
+dnl If you change help related options, please update README.help
+dnl ===================================================================
+
+ENABLE_HTMLHELP=
+HELP_OMINDEX_PAGE=
+HELP_ONLINE=
+WITH_HELPPACKS=
+
+AC_MSG_CHECKING([which help to build])
+if test -n "$with_help" -a "$with_help" != "no"; then
+    GIT_NEEDED_SUBMODULES="helpcontent2 $GIT_NEEDED_SUBMODULES"
+    BUILD_TYPE="$BUILD_TYPE HELP"
+    case "$with_help" in
+    "html")
+        ENABLE_HTMLHELP=TRUE
+        WITH_HELPPACKS=TRUE
+        SCPDEFS="$SCPDEFS -DWITH_HELPPACKS"
+        AC_MSG_RESULT([HTML (local)])
+        ;;
+    "online")
+        ENABLE_HTMLHELP=TRUE
+        HELP_ONLINE=TRUE
+        AC_MSG_RESULT([HTML (online)])
+        ;;
+    yes)
+        WITH_HELPPACKS=TRUE
+        SCPDEFS="$SCPDEFS -DWITH_HELPPACKS"
+        AC_MSG_RESULT([XML (local)])
+        ;;
+    *)
+        AC_MSG_ERROR([Unknown --with-help=$with_help])
+        ;;
+    esac
+else
+    AC_MSG_RESULT([no])
+fi
+
+if test "$with_help" = yes -o "$enable_extension_integration" != no; then
+    BUILD_TYPE="$BUILD_TYPE HELPTOOLS"
+fi
+
+AC_MSG_CHECKING([whether to enable xapian-omega support for online help])
+if test -n "$with_omindex" -a "$with_omindex" != "no"; then
+    if test "$HELP_ONLINE" != TRUE; then
+        AC_MSG_ERROR([Can't build xapian-omega index without --help=online])
+    fi
+    case "$with_omindex" in
+    "server")
+        HELP_OMINDEX_PAGE=TRUE
+        AC_MSG_RESULT([SERVER])
+        ;;
+    "noxap")
+        AC_MSG_RESULT([NOXAP])
+        ;;
+    *)
+        AC_MSG_ERROR([Unknown --with-omindex=$with_omindex])
+        ;;
+    esac
+else
+    AC_MSG_RESULT([no])
+fi
+
+AC_MSG_CHECKING([whether to include the XML-help support])
+if test "$enable_xmlhelp" = yes; then
+    BUILD_TYPE="$BUILD_TYPE XMLHELP"
+    AC_DEFINE(HAVE_FEATURE_XMLHELP)
+    AC_MSG_RESULT([yes])
+else
+    if test "$with_help" = yes; then
+        add_warning "Building the XML help, but LO with disabled xmlhelp support. Generated help can't be accesssed from this LO build!"
+    fi
+    AC_MSG_RESULT([no])
+fi
+
+dnl Test whether to integrate helppacks into the product's installer
+AC_MSG_CHECKING([for helppack integration])
+if test -z "$WITH_HELPPACKS" -o "$with_helppack_integration" = no; then
+    AC_MSG_RESULT([no integration])
+else
+    SCPDEFS="$SCPDEFS -DWITH_HELPPACK_INTEGRATION"
+    AC_MSG_RESULT([integration])
+fi
+
+AC_SUBST([ENABLE_HTMLHELP])
+AC_SUBST([HELP_OMINDEX_PAGE])
+AC_SUBST([HELP_ONLINE])
+# WITH_HELPPACKS is used only in configure
+
+dnl ===================================================================
 dnl Set up a different compiler to produce tools to run on the build
 dnl machine when doing cross-compilation
 dnl ===================================================================
@@ -5381,6 +5483,7 @@ if test "$cross_compiling" = "yes"; then
         --build="$build_alias" \
         --disable-cairo-canvas \
         --disable-cups \
+        --disable-extensions \
         --disable-firebird-sdbc \
         --disable-gpgmepp \
         --disable-gstreamer-1-0 \
@@ -5711,65 +5814,6 @@ else
     DO_FETCH_TARBALLS=
 fi
 AC_SUBST(DO_FETCH_TARBALLS)
-
-AC_MSG_CHECKING([whether to build help])
-if test -n "$with_help" -a "$with_help" != "no" -a $_os != iOS -a $_os != Android; then
-    BUILD_TYPE="$BUILD_TYPE HELP"
-    GIT_NEEDED_SUBMODULES="helpcontent2 $GIT_NEEDED_SUBMODULES"
-    case "$with_help" in
-    "html")
-        ENABLE_HTMLHELP=TRUE
-        SCPDEFS="$SCPDEFS -DWITH_HELP"
-        AC_MSG_RESULT([HTML])
-        ;;
-    "online")
-        ENABLE_HTMLHELP=TRUE
-        HELP_ONLINE=TRUE
-        AC_MSG_RESULT([HTML])
-        ;;
-    yes)
-        SCPDEFS="$SCPDEFS -DWITH_HELP"
-        AC_MSG_RESULT([yes])
-        ;;
-    *)
-        AC_MSG_ERROR([Unknown --with-help=$with_help])
-        ;;
-    esac
-else
-    AC_MSG_RESULT([no])
-fi
-AC_SUBST([ENABLE_HTMLHELP])
-AC_SUBST([HELP_ONLINE])
-
-AC_MSG_CHECKING([whether to enable xapian-omega support for help])
-if test -n "$with_omindex" -a "$with_omindex" != "no" -a $_os != iOS -a $_os != Android; then
-    BUILD_TYPE="$BUILD_TYPE HELP"
-    GIT_NEEDED_SUBMODULES="helpcontent2 $GIT_NEEDED_SUBMODULES"
-    case "$with_omindex" in
-    "server")
-        ENABLE_HTMLHELP=TRUE
-        HELP_ONLINE=TRUE
-        HELP_OMINDEX_PAGE=TRUE
-        AC_MSG_RESULT([SERVER])
-        ;;
-    "noxap")
-        ENABLE_HTMLHELP=TRUE
-        HELP_ONLINE=TRUE
-        HELP_OMINDEX_PAGE=FALSE
-        AC_MSG_RESULT([NOXAP])
-        ;;
-    *)
-        AC_MSG_ERROR([Unknown --with-omindex=$with_omindex])
-        ;;
-    esac
-else
-    HELP_OMINDEX_PAGE=FALSE
-    AC_MSG_RESULT([no])
-fi
-AC_SUBST([ENABLE_HTMLHELP])
-AC_SUBST([HELP_OMINDEX_PAGE])
-AC_SUBST([HELP_ONLINE])
-
 
 dnl Test whether to include MySpell dictionaries
 dnl ===================================================================
@@ -9184,6 +9228,7 @@ dnl ===================================================================
 dnl we should rather be using
 dnl libo_CHECK_SYSTEM_MODULE([clucence],[CLUCENE],[liblucence-core]) here
 dnl but the contribs-lib check seems tricky
+if test "$enable_xmlhelp" = yes -o "$enable_extension_integration" = yes; then
 AC_MSG_CHECKING([which clucene to use])
 if test "$with_system_clucene" = "yes"; then
     AC_MSG_RESULT([external])
@@ -9210,6 +9255,7 @@ else
     AC_MSG_RESULT([internal])
     SYSTEM_CLUCENE=
     BUILD_TYPE="$BUILD_TYPE CLUCENE"
+fi
 fi
 AC_SUBST(SYSTEM_CLUCENE)
 AC_SUBST(CLUCENE_CFLAGS)
@@ -12873,17 +12919,6 @@ for theme in $with_theme; do
     *) ;;
     esac
 done
-
-dnl ===================================================================
-dnl Test whether to integrate helppacks into the product's installer
-dnl ===================================================================
-AC_MSG_CHECKING([for helppack integration])
-if test "$with_helppack_integration" = "no"; then
-    AC_MSG_RESULT([no integration])
-else
-    SCPDEFS="$SCPDEFS -DWITH_HELPPACK_INTEGRATION"
-    AC_MSG_RESULT([integration])
-fi
 
 ###############################################################################
 # Extensions checking

helpcompiler/Module_helpcompiler.mk
@@ -10,8 +10,10 @@
 $(eval $(call gb_Module_Module,helpcompiler))
 
 $(eval $(call gb_Module_add_targets,helpcompiler,\
-    Executable_HelpIndexer \
-    Executable_HelpLinker \
+    $(call gb_Helper_optional,HELPTOOLS, \
+        Executable_HelpIndexer \
+        Executable_HelpLinker \
+    ) \
     Library_helplinker \
 ))
 

solenv/gbuild/Helper.mk
@@ -301,8 +301,21 @@ $(1) : $(2) | $(dir $(1)).dir
 $(call gb_Deliver_add_deliverable,$(1),$(2),$(1))
 endef
 
+# call gb_Helper_optional,build_type,if-true,if-false
 define gb_Helper_optional
-$(if $(filter $(1),$(BUILD_TYPE)),$(2))
+$(if $(filter $(1),$(BUILD_TYPE)),$(2),$(3))
+endef
+
+# call gb_Helper_optionals_or,build_types,if-true,if-false
+define gb_Helper_optionals_or
+$(call gb_Helper_optional,$(1),$(2),$(3))
+endef
+
+gb_Helper_optionals_and_token = $(subst $(gb_SPACE),_,gb $(sort $(1)))
+
+# call gb_Helper_optionals_and,build_types,if-true,if-false
+define gb_Helper_optionals_and
+$(if $(filter $(call gb_Helper_optionals_and_token,$(1)),$(call gb_Helper_optionals_and_token,$(filter $(1),$(BUILD_TYPE)))),$(2),$(3))
 endef
 
 ifeq ($(WITH_LOCALES),)

solenv/gbuild/extensions/pre_BuildTools.mk
@@ -33,11 +33,11 @@ gb_BUILD_TOOLS_executables = \
 		unoidl-check \
 		unoidl-write \
 		xrmex \
-		$(call gb_Helper_optional_for_host,DESKTOP, \
-			HelpIndexer \
-			HelpLinker \
-			lngconvex \
-		) \
+    $(call gb_Helper_optional_for_host,HELPTOOLS, \
+        HelpIndexer \
+        HelpLinker \
+    ) \
+    $(if $(filter WNT,$(OS)),$(call gb_Helper_optional_for_host,DESKTOP,lngconvex)) \
 
 gb_BUILD_TOOLS_executables_extern = \
     python \

Repository.mk
@@ -18,8 +18,10 @@
 #
 
 $(eval $(call gb_Helper_register_executables,NONE, \
+    $(call gb_Helper_optional,HELPTOOLS, \
 	HelpIndexer \
 	HelpLinker \
+    ) \
 	bestreversemap \
 	canvasdemo \
 	cfgex \
@@ -368,7 +370,7 @@ $(eval $(call gb_Helper_register_libraries_for_install,OOOLIBS,ooo, \
 	fwk \
 	$(if $(filter WNT,$(OS)),gdipluscanvas) \
 	guesslang \
-	$(if $(filter DESKTOP,$(BUILD_TYPE)),helplinker) \
+    $(call gb_Helper_optionals_or,HELPTOOLS XMLHELP,helplinker) \
 	i18npool \
 	i18nsearch \
 	hyphen \
@@ -622,7 +624,7 @@ $(eval $(call gb_Helper_register_libraries_for_install,PLAINLIBS_OOO,ooo, \
 	$(if $(WITH_WEBDAV),ucpdav1) \
 	ucpfile1 \
 	ucpftp1 \
-	ucpchelp1 \
+    $(call gb_Helper_optional,XMLHELP,ucpchelp1) \
 	ucphier1 \
 	ucppkg1 \
 	unopkgapp \
@@ -914,7 +916,7 @@ $(eval $(call gb_Helper_register_packages_for_install,ooo,\
 	$(if $(filter $(OS),MACOSX),vcl_osxres) \
 	xmloff_dtd \
 	xmlscript_dtd \
-	xmlhelp_helpxsl \
+    $(call gb_Helper_optional,XMLHELP,xmlhelp_helpxsl) \
 	$(if $(ENABLE_JAVA),\
 		scripting_java \
 		scripting_java_jars \

RepositoryModule_build.mk
@@ -87,10 +87,8 @@ $(eval $(call gb_Module_add_moduledirs,cross_toolset,\
 		xmloff \
 		xmlscript \
 	) \
-	$(call gb_Helper_optional_for_host,DESKTOP, \
-		helpcompiler \
-		xmlhelp \
-	) \
+    $(call gb_Helper_optionals_or,HELPTOOLS XMLHELP,helpcompiler) \
+    $(call gb_Helper_optional,XMLHELP,xmlhelp) \
 	$(call gb_Helper_optional,QADEVOOO,qadevOOo) \
 ))
 

RepositoryModule_host.mk
@@ -60,7 +60,7 @@ $(eval $(call gb_Module_add_moduledirs,libreoffice,\
 	formula \
 	$(call gb_Helper_optional,DESKTOP,fpicker) \
 	framework \
-	$(call gb_Helper_optional,DESKTOP,helpcompiler) \
+    $(call gb_Helper_optionals_or,HELPTOOLS XMLHELP,helpcompiler) \
 	$(call gb_Helper_optional,HELP,helpcontent2) \
 	hwpfilter \
 	i18nlangtag \
@@ -153,7 +153,7 @@ $(eval $(call gb_Module_add_moduledirs,libreoffice,\
 	writerfilter \
 	writerperfect \
 	xmerge \
-	$(call gb_Helper_optional,DESKTOP,xmlhelp) \
+    $(call gb_Helper_optional,XMLHELP,xmlhelp) \
 	xmloff \
 	xmlreader \
 	xmlscript \

config_host/config_features.h.in
@@ -145,4 +145,11 @@
  */
 #define HAVE_FEATURE_COMMUNITY_FLAVOR 0
 
+/* XMLHELP - whether we include the XML help mechanisms
+ *
+ * Can be turned off with --without-xmlhelp
+ */
+
+#define HAVE_FEATURE_XMLHELP 0
+
 #endif

commit cae2bdae895d650b27afc6f3aba18dfdf1923170

cross-toolset: fix real cross help building
Regression from commit 14069d84174ca7a4e60db4d75912903e9679b643
("configure: Improve help-options handling").

We now must forward the help and extension configure options, as
this is needed by the cross toolset to know, if it needs to build
the HelpIndexer and HelpLinker executables.

Also get rid of gb_Helper_optional_for_host. While it's in theory
more correct, it makes gb_Helper_optional more complex, so we now
rely on the fact, that the config_build.mk BUILD_TYPE is a subset
of the config_host.mk BUILD_TYPE.

This should fix the Windows Arm64 build, which currently seem to
be the only cross-build including help.

Change-Id: I8b90923c891b43cd2655b2578015c9bd579aae7d

configure.ac
@@ -5346,8 +5346,12 @@ else
     AC_MSG_RESULT([no])
 fi
 
+AC_MSG_CHECKING([if we need to build the help index tooling])
 if test "$with_help" = yes -o "$enable_extension_integration" != no; then
     BUILD_TYPE="$BUILD_TYPE HELPTOOLS"
+    AC_MSG_RESULT([yes])
+else
+    AC_MSG_RESULT([no])
 fi
 
 AC_MSG_CHECKING([whether to enable xapian-omega support for online help])
@@ -5475,6 +5479,8 @@ if test "$cross_compiling" = "yes"; then
     test -n "$TARFILE_LOCATION" && sub_conf_opts="$sub_conf_opts --with-external-tar=$TARFILE_LOCATION"
     test "$with_system_icu_for_build" = "yes" -o "$with_system_icu_for_build" = "force" && sub_conf_opts="$sub_conf_opts --with-system-icu"
     test "$with_galleries" = "no" -o -z "$WITH_GALLERY_BUILD" && sub_conf_opts="$sub_conf_opts --with-galleries=no --disable-database-connectivity"
+    test -n "$with_help" -a "$with_help" != "no" && sub_conf_opts="$sub_conf_opts --with-help=$with_help"
+    test "$enable_extensions" = yes || sub_conf_opts="$sub_conf_opts --disable-extensions"
     test "$enable_wasm_strip" = "yes" && sub_conf_opts="$sub_conf_opts --enable-wasm-strip"
     sub_conf_opts="$sub_conf_opts $with_build_platform_configure_options"
 
@@ -5483,7 +5489,6 @@ if test "$cross_compiling" = "yes"; then
         --build="$build_alias" \
         --disable-cairo-canvas \
         --disable-cups \
-        --disable-extensions \
         --disable-firebird-sdbc \
         --disable-gpgmepp \
         --disable-gstreamer-1-0 \
@@ -5524,6 +5529,7 @@ if test "$cross_compiling" = "yes"; then
         GLM
         GRAPHITE
         HARFBUZZ
+        HELPTOOLS
         ICU
         LCMS2
         LIBJPEG_TURBO

helpcompiler/Module_helpcompiler.mk
@@ -10,11 +10,14 @@
 $(eval $(call gb_Module_Module,helpcompiler))
 
 $(eval $(call gb_Module_add_targets,helpcompiler,\
+    Library_helplinker \
+))
+
+$(eval $(call gb_Module_add_targets_for_build,helpcompiler,\
     $(call gb_Helper_optional,HELPTOOLS, \
         Executable_HelpIndexer \
         Executable_HelpLinker \
     ) \
-    Library_helplinker \
 ))
 
 # vim:set noet sw=4 ts=4:

solenv/gbuild/Helper.mk
@@ -328,12 +328,6 @@ $(if $(filter $(1) $(1)_%,$(WITH_LOCALES)),$(2))
 endef
 endif
 
-define gb_Helper_optional_for_host
-$(if $(filter build,$(gb_Side)), \
-	$(if $(filter $(1),$(BUILD_TYPE_FOR_HOST)),$(2)), \
-	$(call gb_Output_error,gb_Helper_optional_for_host: Use only when gb_Side=build))
-endef
-
 define gb_Helper_print_on_error
 $(if $(gb_QUIET_EXTERNAL), \
     $(if $(2), \

solenv/gbuild/extensions/pre_BuildTools.mk
@@ -33,11 +33,11 @@ gb_BUILD_TOOLS_executables = \
 		unoidl-check \
 		unoidl-write \
 		xrmex \
-    $(call gb_Helper_optional_for_host,HELPTOOLS, \
+    $(call gb_Helper_optional,HELPTOOLS, \
         HelpIndexer \
         HelpLinker \
     ) \
-    $(if $(filter WNT,$(OS)),$(call gb_Helper_optional_for_host,DESKTOP,lngconvex)) \
+    $(if $(filter WNT,$(OS)),$(call gb_Helper_optional,DESKTOP,lngconvex)) \
 
 gb_BUILD_TOOLS_executables_extern = \
     python \