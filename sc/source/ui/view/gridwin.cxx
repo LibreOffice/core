44448816cb1d391732205d313b73ff420905da9b
Henry Castro <hcastro@collabora.com>
2021-06-24 07:48:19 -0400

    lok: sc: handle all local mouse tracking for Calc
    Change-Id: I11f3477c0f966d403e076fc73b7e5507ad6597f7

--- sc/source/ui/view/gridwin.cxx
+++ sc/source/ui/view/gridwin.cxx
@@ -2728,7 +2728,6 @@ void ScGridWindow::Tracking( const TrackingEvent& rTEvt )
     }
     else if ( rTEvt.IsTrackingEnded() )
     {
-        if ( !comphelper::LibreOfficeKit::isActive() )
         {
             // MouseButtonUp always with matching buttons (eg for test tool, # 63148 #)
             // The tracking event will indicate if it was completed and not canceled.
@@ -2737,7 +2736,7 @@ void ScGridWindow::Tracking( const TrackingEvent& rTEvt )
             MouseButtonUp( aUpEvt );
         }
     }
-    else if ( !comphelper::LibreOfficeKit::isActive() )
+    else
         MouseMove( rMEvt );
 }
 
