34ade20a09a1c3fe08daec2b276a06a1f895bd5a
Dennis Francis <dennis.francis@collabora.com>
2021-06-25 14:52:22 +0530

    lok: sw: avoid duplicate LOK_CALLBACK_TABLE_SELECTED msgs
    Alternate approach is to track changes in the active table in each view
    but it needs a bigger change and run the risk of overlooking cases
    where table dimensions and position changes, which is possibly why this
    message is created from UpdateCursor() in the first place.
    
    Change-Id: I06643e2a7416d6c0fe2b0dc5c9b4adcd45fa50c5
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/118001
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -1427,6 +1427,7 @@ CallbackFlushHandler::CallbackFlushHandler(LibreOfficeKitDocument* pDocument, Li
     m_states.emplace(LOK_CALLBACK_CELL_ADDRESS, "NIL");
     m_states.emplace(LOK_CALLBACK_CURSOR_VISIBLE, "NIL");
     m_states.emplace(LOK_CALLBACK_SET_PART, "NIL");
+    m_states.emplace(LOK_CALLBACK_TABLE_SELECTED, "NIL");
 
     Start();
 }
