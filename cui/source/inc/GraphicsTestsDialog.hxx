894f7069d191499b110830d5869b510d109af74f
homeboy445 <akshitsan13@gmail.com>
2021-06-26 20:46:25 +0530

    Added the feature to store VCL test results as a zip file
    The results can now be downloaded as a zip file, which would
    consist of the test log and all the resultant bitmap images produced
    by the tests compressed as png stored in the user directory folder.
    
    Change-Id: I8a6098a7454a621bbb9cafa7b6f2cafaa5503522

--- cui/source/inc/GraphicsTestsDialog.hxx
+++ cui/source/inc/GraphicsTestsDialog.hxx
@@ -12,6 +12,7 @@
 #include <vcl/svapp.hxx>
 #include <vcl/weld.hxx>
 #include <tools/link.hxx>
+#include <svx/FileExportedDialog.hxx>
 
 #include "ImageViewerDialog.hxx"
 
@@ -44,9 +45,14 @@ class GraphicsTestsDialog : public weld::GenericDialogController
 
     std::vector<std::unique_ptr<GraphicTestEntry>> m_xGraphicTestEntries;
 
-    DECL_STATIC_LINK(GraphicsTestsDialog, HandleDownloadRequest, weld::Button&, void);
+    OUString m_xZipFileUrl;
+    OUString m_xCreateFolderUrl;
+
+    DECL_LINK(HandleDownloadRequest, weld::Button&, void);
+    DECL_LINK(HandleResultViewRequest, weld::Button&, void);
 
 public:
     GraphicsTestsDialog(weld::Window* pParent);
+    ~GraphicsTestsDialog();
     virtual short run() override;
 };
