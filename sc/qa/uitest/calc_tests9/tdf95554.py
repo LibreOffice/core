32f31a96b16ccca2e51a986e625c56e87f50b540
shubham656 <shubham656jain@gmail.com>
2021-06-28 02:13:03 +0530

    tdf#95554 Add UnitTest
    Change-Id: I38a9768fc94f26a593f51ce566da4111f5f3b78b

---
+++ sc/qa/uitest/calc_tests9/tdf95554.py
@@ -0,0 +1,33 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from uitest.framework import UITestCase
+from uitest.uihelper.common import get_url_for_data_file
+from libreoffice.uno.propertyvalue import mkPropertyValues
+
+class Tdf95554(UITestCase):
+
+   def test_tdf95554(self):
+        with self.ui_test.load_file(get_url_for_data_file("tdf95554.ods")):
+
+            xCalcDoc = self.xUITest.getTopFocusWindow()
+            gridwin = xCalcDoc.getChild("grid_window")
+
+            gridwin.executeAction("SELECT", mkPropertyValues({"CELL": "A1"}))
+            gridwin.executeAction("SELECT", mkPropertyValues({"CELL": "A3"}))
+            gridwin.executeAction("SELECT", mkPropertyValues({"CELL": "A5"}))
+            gridwin.executeAction("SELECT", mkPropertyValues({"CELL": "A6"}))
+            gridwin.executeAction("TYPE", mkPropertyValues({"KEYCODE": "CTRL+c"}))
+
+            with self.ui_test.load_empty_file("writer") as writer_document:
+
+                self.xUITest.getTopFocusWindow()
+
+                self.xUITest.executeCommand(".uno:PasteUnformatted")
+
+                self.assertEqual('1\n3\n5\n6',
+                        writer_document.Text.String)
+
