54bc159a1cf01dd366fa11701c2e08ebe07617bd
Stephan Bergmann <sbergman@redhat.com>
2021-06-28 10:01:45 +0200

    Mark external/harfbuzz/Wunused-but-set-variable.patch as fixed upstream
    Change-Id: Iefc8f4f6ff93ec1a11256724b1881689d5eb56d9

--- external/harfbuzz/UnpackedTarball_harfbuzz.mk
+++ external/harfbuzz/UnpackedTarball_harfbuzz.mk
@@ -17,6 +17,10 @@ $(eval $(call gb_UnpackedTarball_set_patchlevel,harfbuzz,0))
 
 # icu-65-api-macros-with-semicolon.patch.1
 #   See http://site.icu-project.org/download/65  Migration Issues
+# Wunused-but-set-variable.patch
+#   Independently fixed upstream in a similar way with
+#   <https://github.com/harfbuzz/harfbuzz/commit/d6a83abd6a0c684bd9a27f593d5be388819022b7> "Define
+#   HB_UNUSED for clang"
 $(eval $(call gb_UnpackedTarball_add_patches,harfbuzz, \
     external/harfbuzz/ubsan.patch \
     external/harfbuzz/icu-65-api-macros-with-semicolon.patch.1 \
