c50b130c4fe5b5c6cad84d31886b8de15bf4b1cb
Rafael Lima <rafael.palma.lima@gmail.com>
2021-06-29 13:15:01 +0200

    Add Portuguese support to ScriptForge library (l10n)
    Change-Id: I04341059becb36467795a2aa1d5c2f573fe1bc6f
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/118092
    Tested-by: Jenkins
    Reviewed-by: Jean-Pierre Ledure <jp@ledure.be>
    (cherry picked from commit 1b7d564ee8c75d35dc2b8cb99afcad499c9e6fb7)

--- wizards/Package_scriptforge.mk
+++ wizards/Package_scriptforge.mk
@@ -51,6 +51,7 @@ $(eval $(call gb_Package_add_files,wizards_basicsrvscriptforge,$(LIBO_SHARE_FOLD
 $(eval $(call gb_Package_add_files,wizards_basicsrvscriptforge,$(LIBO_SHARE_FOLDER)/basic/ScriptForge/po,\
 	po/ScriptForge.pot \
 	po/en.po \
+	po/pt.po \
 ))
 
 $(eval $(call gb_Package_add_files,wizards_basicsrvscriptforge,$(LIBO_LIB_PYUNO_FOLDER),\
