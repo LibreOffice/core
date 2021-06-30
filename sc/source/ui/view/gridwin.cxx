e01f889e95698221a95e0c70a48380a940775020
Henry Castro <hcastro@collabora.com>
2021-06-29 21:39:45 -0400

    lok: sc: set mouse pointer when drag over & drag drop
    Change-Id: I7040f45af8bc125823bbfc92531596a3f45d752b

--- sc/source/ui/view/gridwin.cxx
+++ sc/source/ui/view/gridwin.cxx
@@ -3927,6 +3927,9 @@ sal_Int8 ScGridWindow::AcceptDrop( const AcceptDropEvent& rEvt )
             DropScroll( rEvt.maPosPixel );
     }
 
+    if (comphelper::LibreOfficeKit::isActive() && nRet != DND_ACTION_NONE)
+        SetPointer(PointerStyle::Hand);
+
     return nRet;
 }
 
@@ -4419,6 +4422,9 @@ sal_Int8 ScGridWindow::DropTransferObj( ScTransferObj* pTransObj, SCCOL nDestPos
 
 sal_Int8 ScGridWindow::ExecuteDrop( const ExecuteDropEvent& rEvt )
 {
+    if (comphelper::LibreOfficeKit::isActive())
+        SetPointer(PointerStyle::Arrow);
+
     DrawMarkDropObj( nullptr );    // drawing layer
 
     ScModule* pScMod = SC_MOD();
