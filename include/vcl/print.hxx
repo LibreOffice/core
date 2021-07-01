125500fed7ee9929ae9f94bcdc113801813dbf73
Caolán McNamara <caolanm@redhat.com>
2021-07-01 21:12:26 +0100

    tdf#143149 fix crash if app exits while print progress is running
    dismiss the dialog if its parent goes away, but leave the print
    job running
    
    Change-Id: Ic29ee0300a7d9476ad27a2594a555312e30c74f2
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/118244
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>

--- include/vcl/print.hxx
+++ include/vcl/print.hxx
@@ -387,7 +387,7 @@ class VCL_DLLPUBLIC PrinterController
     std::unique_ptr<ImplPrinterControllerData>
                                         mpImplData;
 protected:
-    PrinterController(const VclPtr<Printer>&, weld::Window*);
+    PrinterController(const VclPtr<Printer>&, weld::Window* pDialogParent);
 public:
     struct MultiPageSetup
     {
@@ -515,6 +515,8 @@ public:
 
     bool                                isShowDialogs() const;
     bool                                isDirectPrint() const;
+
+    void                                dialogsParentClosing();
 
     // implementation details, not usable outside vcl
     // don't use outside vcl. Some of these are exported for
