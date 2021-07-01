0d3398e5d141b79706653c04f1ab6dc9381a5cc1
Tünde Tóth <toth.tunde@nisz.hu>
2021-07-01 13:40:03 +0200

    tdf#142929 XLSX: fix import of "Less than" filter condition
    "Less than" standard filter condition became <> instead of <.
    
    Change-Id: I53435590de3a03d9fcaf0359e8421ae3ef941316
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/118227
    Tested-by: Jenkins
    Tested-by: László Németh <nemeth@numbertext.org>
    Reviewed-by: László Németh <nemeth@numbertext.org>

--- sc/qa/unit/subsequent_export-test2.cxx
+++ sc/qa/unit/subsequent_export-test2.cxx
@@ -192,6 +192,7 @@ public:
     void testTdf140431();
     void testCheckboxFormControlXlsxExport();
     void testButtonFormControlXlsxExport();
+    void testTdf142929_filterLessThanXLSX();
     void testInvalidNamedRange();
 
     CPPUNIT_TEST_SUITE(ScExportTest2);
@@ -293,6 +294,7 @@ public:
     CPPUNIT_TEST(testTdf140431);
     CPPUNIT_TEST(testCheckboxFormControlXlsxExport);
     CPPUNIT_TEST(testButtonFormControlXlsxExport);
+    CPPUNIT_TEST(testTdf142929_filterLessThanXLSX);
     CPPUNIT_TEST(testInvalidNamedRange);
 
     CPPUNIT_TEST_SUITE_END();
@@ -2422,6 +2424,21 @@ void ScExportTest2::testButtonFormControlXlsxExport()
     // - XPath '//x:controlPr' unexpected 'macro' attribute
     // i.e. macro in an xlsx file was not omitted, which is considered invalid by Excel.
     assertXPathNoAttribute(pDoc, "//x:controlPr", "macro");
+}
+
+void ScExportTest2::testTdf142929_filterLessThanXLSX()
+{
+    // Document contains a standard filter with '<' condition.
+    ScDocShellRef xDocSh = loadDoc(u"tdf142929.", FORMAT_XLSX);
+    CPPUNIT_ASSERT(xDocSh.is());
+
+    xmlDocUniquePtr pDoc = XPathHelper::parseExport2(*this, *xDocSh, m_xSFactory,
+                                                     "xl/worksheets/sheet1.xml", FORMAT_XLSX);
+    CPPUNIT_ASSERT(pDoc);
+    assertXPath(pDoc, "//x:customFilters/x:customFilter", "val", "2");
+    assertXPath(pDoc, "//x:customFilters/x:customFilter", "operator", "lessThan");
+
+    xDocSh->DoClose();
 }
 
 void ScExportTest2::testInvalidNamedRange()
