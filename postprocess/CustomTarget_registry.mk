70bc57f52b416b00ca4ea44d062d8cdc2d49c87d
Stéphane Guillou <stephane.guillou@member.fsf.org>
2021-07-02 22:57:00 +1000

    tdf#133368: update extensions website URLs and use https
    Change-Id: Idcdb5dbb4f0754425eeca4b137fa1626bba9ef6a

--- postprocess/CustomTarget_registry.mk
+++ postprocess/CustomTarget_registry.mk
@@ -573,9 +573,9 @@ postprocess_main_SED := \
 	-e 's,$${PRODUCTNAME},$(PRODUCTNAME),g' \
 	-e 's,$${PRODUCTVERSION},$(PRODUCTVERSION),g' \
 	-e 's,$${PRODUCTEXTENSION},.$(LIBO_VERSION_MICRO).$(LIBO_VERSION_PATCH)$(LIBO_VERSION_SUFFIX),g' \
-	-e 's,$${STARTCENTER_ADDFEATURE_URL},http://extensions.libreoffice.org/,g' \
+	-e 's,$${STARTCENTER_ADDFEATURE_URL},https://extensions.libreoffice.org/,g' \
 	-e 's,$${STARTCENTER_INFO_URL},https://www.libreoffice.org/,g' \
-	-e 's,$${STARTCENTER_TEMPLREP_URL},http://templates.libreoffice.org/,g' \
+	-e 's,$${STARTCENTER_TEMPLREP_URL},https://extensions.libreoffice.org/?Tags%5B%5D=118,g' \
 	-e 's,$${SYSTEM_LIBEXTTEXTCAT_DATA},$(SYSTEM_LIBEXTTEXTCAT_DATA),g' \
 	-e 's,$${SYSTEM_LIBNUMBERTEXT_DATA},$(SYSTEM_LIBNUMBERTEXT_DATA),g' \
 	-e 's,$${PRIVACY_POLICY_URL},$(PRIVACY_POLICY_URL),g' \
