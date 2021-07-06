9a7109d98151860c1d5aadd37827eb64c3e88a67
Gülşah Köse <gulsah.kose@collabora.com>
2021-07-07 00:27:58 +0300

    tdf143222 Handle alternate content of graphicData element.
    Handle alternate content and make true choice.
    
    According to ooxml spec ole object requires exactly one pic
    element. (ECMA-376 Part 1, Annex A, CT_OleObject). In the
    current case first choice has not pic element and we should
    allow fallback processing.
    
    Change-Id: I30b7de703b8c2f00d6bf286e05eea505ac3627f2
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/118539
    Tested-by: Jenkins
    Reviewed-by: Gülşah Köse <gulsah.kose@collabora.com>

--- include/oox/ole/oleobjecthelper.hxx
+++ include/oox/ole/oleobjecthelper.hxx
@@ -47,6 +47,7 @@ struct OleObjectInfo
     bool                mbLinked;           ///< True = linked OLE object, false = embedded OLE object.
     bool                mbShowAsIcon;       ///< True = show as icon, false = show contents.
     bool                mbAutoUpdate;
+    bool                mbHasPicture;       ///<Ole object requires a picture element according to spec.>
 
     explicit            OleObjectInfo();
 };
