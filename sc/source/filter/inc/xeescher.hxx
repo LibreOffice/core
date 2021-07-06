9ef217144d6452fead389ec031eb6f78ba5067e7
Miklos Vajna <vmiklos@collabora.com>
2021-07-06 14:31:07 +0200

    XLSX export: handle macros on button form controls
    This builds on top of commit 1e3263a677b61c718d0fd1be15c066b933f7de18
    (XLSX export: handle button form controls, 2021-07-01).
    
    The binary XLS export already had code to turn Calc macro names into
    Excel ones, reuse that for XLSX purposes.
    
    Also fix the unwanted named range on export,
    oox::xls::FormulaParser::importMacroName() mentions how XLSX doesn't
    have matching named ranges for vba macros (while XLS has), mirror this
    on the export side as well.
    
    (cherry picked from commit 7e7a871bcd4f923b015a7e040969335696b434c6)
    
    Conflicts:
    	sc/source/filter/excel/xeescher.cxx
    
    Change-Id: I877b6ba2c2e834a2327482da5cadcddf1b4672bb
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/118563
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Luboš Luňák <l.lunak@collabora.com>

--- sc/source/filter/inc/xeescher.hxx
+++ sc/source/filter/inc/xeescher.hxx
@@ -195,6 +195,7 @@ protected:
 class XclMacroHelper : public XclExpControlHelper
 {
     XclTokenArrayRef    mxMacroLink;    /// Token array containing a link to an attached macro.
+    OUString maMacroName;
 
 public:
     explicit            XclMacroHelper( const XclExpRoot& rRoot );
@@ -208,6 +209,7 @@ public:
     /** Sets the name of a macro
         @return  true = The passed macro name has been found. */
     bool                SetMacroLink( const OUString& rMacro );
+    OUString GetMacroName() const;
 };
 
 class XclExpShapeObj : public XclObjAny, public XclMacroHelper
