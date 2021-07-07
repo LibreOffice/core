135f96029d3fa601f1c4cc5f29b125995d0e511d
Szymon Kłos <szymon.klos@collabora.com>
2021-07-07 12:19:12 +0200

    jsdialog: send type for popup messages
    Change-Id: Ic0f189645dc9a1164cbbc528b3a5d30d72ee3333

--- vcl/jsdialog/jsdialogbuilder.cxx
+++ vcl/jsdialog/jsdialogbuilder.cxx
@@ -253,6 +253,7 @@ JSDialogNotifyIdle::generateClosePopupMessage(OUString sWindowId) const
         return aJsonWriter;
 
     aJsonWriter->put("jsontype", "dialog");
+    aJsonWriter->put("type", "modalpopup");
     aJsonWriter->put("action", "close");
     aJsonWriter->put("id", sWindowId);
 
