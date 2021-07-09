93bcb3dbc4319ab2d50058a22adcf70081bf3c91
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2021-07-09 19:36:58 +0900

    indexing: add support for SdrObjects in SearchResultLocator
    Also add (node) "type" parameter because we need to differentiate
    between Writer nodes and SdrObject nodes.
    
    Change-Id: I590695ae71781f64c22bdd7e1df01d69e3376e67
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/118671
    Tested-by: Jenkins
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>
    (cherry picked from commit ea1818b8ba34378b777b8706069d28fade2cc924)

--- sw/qa/extras/indexing/SearchResultLocatorTest.cxx
+++ sw/qa/extras/indexing/SearchResultLocatorTest.cxx
@@ -27,9 +27,11 @@ private:
 
 public:
     void testSearchResultLocator();
+    void testSearchResultLocatorForSdrObjects();
 
     CPPUNIT_TEST_SUITE(SearchResultLocatorTest);
     CPPUNIT_TEST(testSearchResultLocator);
+    CPPUNIT_TEST(testSearchResultLocatorForSdrObjects);
     CPPUNIT_TEST_SUITE_END();
 };
 
@@ -47,26 +49,58 @@ SwDoc* SearchResultLocatorTest::createDoc(const char* pName)
 
 void SearchResultLocatorTest::testSearchResultLocator()
 {
-#if !defined(_WIN32) && !defined(MACOSX)
     if (!IsDefaultDPI())
         return;
 
     SwDoc* pDoc = createDoc("IndexingExport_VariousParagraphs.odt");
     CPPUNIT_ASSERT(pDoc);
 
-    sw::SearchResultLocator aLocator(pDoc);
-    sw::SearchIndexData aData;
+    sw::search::SearchResultLocator aLocator(pDoc);
+    sw::search::SearchIndexData aData;
+    aData.eType = sw::search::NodeType::WriterNode;
     aData.nNodeIndex = 14;
 
-    sw::LocationResult aResult = aLocator.find(aData);
+    sw::search::LocationResult aResult = aLocator.find(aData);
     CPPUNIT_ASSERT_EQUAL(size_t(1), aResult.maRectangles.size());
-    auto aRectangle = aResult.maRectangles[0];
 
+    // skip asserting exact values for macOS and Windows because of
+    // inconsistent results
+#if !defined(_WIN32) && !defined(MACOSX)
+    auto aRectangle = aResult.maRectangles[0];
     CPPUNIT_ASSERT_DOUBLES_EQUAL(1418.0, aRectangle.getMinX(), 1e-4);
     CPPUNIT_ASSERT_DOUBLES_EQUAL(4444.0, aRectangle.getMinY(), 1e-4);
 
     CPPUNIT_ASSERT_DOUBLES_EQUAL(9638.0, aRectangle.getWidth(), 1e-4);
     CPPUNIT_ASSERT_DOUBLES_EQUAL(276.0, aRectangle.getHeight(), 1e-4);
+#endif
+}
+
+void SearchResultLocatorTest::testSearchResultLocatorForSdrObjects()
+{
+    if (!IsDefaultDPI())
+        return;
+
+    SwDoc* pDoc = createDoc("IndexingExport_Shapes.odt");
+    CPPUNIT_ASSERT(pDoc);
+
+    sw::search::SearchResultLocator aLocator(pDoc);
+    sw::search::SearchIndexData aData;
+    aData.eType = sw::search::NodeType::SdrObject;
+    aData.aObjectName = u"Circle";
+    aData.nNodeIndex = 1;
+
+    sw::search::LocationResult aResult = aLocator.find(aData);
+    CPPUNIT_ASSERT_EQUAL(size_t(1), aResult.maRectangles.size());
+
+    // skip asserting exact values for macOS and Windows because of
+    // inconsistent results
+#if !defined(_WIN32) && !defined(MACOSX)
+    auto aRectangle = aResult.maRectangles[0];
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(1478.0, aRectangle.getMinX(), 1e-4);
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(3223.0, aRectangle.getMinY(), 1e-4);
+
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(2059.0, aRectangle.getWidth(), 1e-4);
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(2059.0, aRectangle.getHeight(), 1e-4);
 #endif
 }
 
