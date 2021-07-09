5bd0018312ad3a7c23bbe89948883afd9cae5eb4
Michael Meeks <michael.meeks@collabora.com>
2021-07-09 15:14:50 +0100

    vcl: make get_id safe for disposed windows.
    crash from:
    	findChild(vcl::Window*, rtl::OUString const&, bool)      include/rtl/ustring.hxx:1744
    	WindowUIObject::get_visible_child(rtl::OUString const&)  /vcl/source/uitest/uiobject.cxx:463
    	lcl_sendDialogEvent ...
    
    Change-Id: Ic1756c7b03f2dee9780fb1a735917f1d0fc11876
    Signed-off-by: Michael Meeks <michael.meeks@collabora.com>

--- vcl/source/window/window.cxx
+++ vcl/source/window/window.cxx
@@ -3939,7 +3939,8 @@ void Window::set_id(const OUString& rID)
 
 const OUString& Window::get_id() const
 {
-    return mpWindowImpl->maID;
+    static OUString empty;
+    return mpWindowImpl ? mpWindowImpl->maID : empty;
 }
 
 FactoryFunction Window::GetUITestFactory() const
