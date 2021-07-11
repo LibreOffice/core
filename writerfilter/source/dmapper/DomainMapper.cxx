eec71229fbe708303d9a54602314f4d344fb7958
Noel Grandin <noel.grandin@collabora.co.uk>
2021-07-11 10:37:20 +0200

    pProperties in StyleSheetEntry is never nullptr
    so we can elide some null checks
    
    Change-Id: I6fb07daf96b76dfa9223dad64bc9690585c00b2d

--- writerfilter/source/dmapper/DomainMapper.cxx
+++ writerfilter/source/dmapper/DomainMapper.cxx
@@ -2732,7 +2732,7 @@ void DomainMapper::sprmWithProps( Sprm& rSprm, const PropertyMapPtr& rContext )
                 if( !sStyleName.isEmpty() && GetStyleSheetTable() )
                     pStyle = GetStyleSheetTable()->FindStyleSheetByConvertedStyleName( sStyleName );
 
-                if( pStyle && pStyle->pProperties
+                if( pStyle
                     && pStyle->pProperties->isSet(PROP_BREAK_TYPE)
                     && pStyle->pProperties->getProperty(PROP_BREAK_TYPE)->second == aBreakType )
                 {
