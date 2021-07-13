e935a98fd40aecb5b24da2eeb3c61ba19a3c6a87
Samuel Mehrbrodt <samuel.mehrbrodt@allotropia.de>
2021-07-13 14:03:44 +0200

    Related tdf#tdf#126665 Fix image select dialog
    Use proper default (work dir, not internal gallery folder)
    
    Change-Id: I3a7a3558274ea066219e2c9f362890d2ab12212e

--- sfx2/source/dialog/filedlghelper.cxx
+++ sfx2/source/dialog/filedlghelper.cxx
@@ -2204,7 +2204,7 @@ void FileDialogHelper_Impl::loadConfig()
         }
 
         if ( maPath.isEmpty() )
-            displayFolder( SvtPathOptions().GetGraphicPath() );
+            displayFolder( SvtPathOptions().GetWorkPath() );
     }
     else
     {
