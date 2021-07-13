52c01abf49342770158047874e7e6fb815620075
Balazs Varga <balazs.varga991@gmail.com>
2021-07-13 18:03:28 +0200

    tdf#64086 tdf#118108 sc ooxml: import/export correctly named ranges in charts
    Do not create inner data table in Calc for charts, if the data source
    are named ranges.
    Use the named ranges (local sheet names and global names) to find the
    data source of charts, so it will be updated correctly if we modify the
    values in cells.
    
    TODO: add unit test (WIP)
    
    TODO (tdf#66250): allow on UI to write named ranges as data source in chart dialogue.
    TODO: odf import/export
    
    Change-Id: I10d8563fb436092e833682f331c25b0c0829ef86

--- sc/source/core/tool/compiler.cxx
+++ sc/source/core/tool/compiler.cxx
@@ -3532,18 +3532,26 @@ bool ScCompiler::IsNamedRange( const OUString& rUpperName )
     }
 
     // Sheet-local name with sheet specified.
-    if (mnCurrentSheetEndPos > 0 && mnCurrentSheetTab >= 0)
+    if (mnCurrentSheetEndPos > 0 && mnCurrentSheetTab >= -1)
     {
+        // if mnCurrentSheetTab == -1 and mnCurrentSheetEndPos > 1,
+        // it means we have a global range name which came from an ooxml,
+        // where the original string looks like this: [0]!Global_range_name
         OUString aName( rUpperName.copy( mnCurrentSheetEndPos));
         pRangeName = rDoc.GetRangeName( mnCurrentSheetTab);
         if (pRangeName)
-        {
             pData = pRangeName->findByUpperName(aName);
-            if (pData)
-            {
-                maRawToken.SetName( mnCurrentSheetTab, pData->GetIndex());
-                return true;
-            }
+        if (!pData)
+        {
+            pRangeName = rDoc.GetRangeName();
+            if (pRangeName)
+                pData = pRangeName->findByUpperName(aName);
+        }
+
+        if (pData)
+        {
+            maRawToken.SetName(mnCurrentSheetTab, pData->GetIndex());
+            return true;
         }
     }
 
@@ -5275,7 +5283,8 @@ void ScCompiler::CreateStringFromIndex( OUStringBuffer& rBuffer, const FormulaTo
             if (pData)
             {
                 SCTAB nTab = _pTokenP->GetSheet();
-                if (nTab >= 0 && nTab != aPos.Tab())
+                bool bExcelConvention = FormulaGrammar::isExcelConvention(pConv->meConv);
+                if (nTab >= 0 && (nTab != aPos.Tab() || bExcelConvention))
                 {
                     // Sheet-local on other sheet.
                     OUString aName;
@@ -5288,6 +5297,13 @@ void ScCompiler::CreateStringFromIndex( OUStringBuffer& rBuffer, const FormulaTo
                         aBuffer.append(ScCompiler::GetNativeSymbol(ocErrName));
                     aBuffer.append( pConv->getSpecialSymbol( ScCompiler::Convention::SHEET_SEPARATOR));
                 }
+                else if (bExcelConvention)
+                {
+                    // For global names require an extra 'external file' ooxml prefix, which links to themselves.
+                    aBuffer.append("[0]");
+                    aBuffer.append(pConv->getSpecialSymbol(ScCompiler::Convention::SHEET_SEPARATOR));
+                }
+
                 aBuffer.append(pData->GetName());
             }
         }
