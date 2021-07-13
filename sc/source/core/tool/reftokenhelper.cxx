e16aa94a665062115f257eea376dbfc18eae6850
Balazs Varga <balazs.varga991@gmail.com>
2021-07-13 18:03:28 +0200

    tdf#64086 tdf#118108 sc ooxml: import/export correctly named ranges in charts
    Do not create inner data table in Calc for charts, if the data source
    are named ranges.
    Use the named ranges (local sheet names and global names) to find the
    data source of charts, so it will be updated correctly if we modify the
    values in cells.
    
    TODO: add unit test (WIP)
    
    TODO (tdf#66250): allow on UI to write named ranges as data source in chart dialogue.
    TODO: odf import/export
    
    Change-Id: I10d8563fb436092e833682f331c25b0c0829ef86

--- sc/source/core/tool/reftokenhelper.cxx
+++ sc/source/core/tool/reftokenhelper.cxx
@@ -150,6 +150,19 @@ bool ScRefTokenHelper::getRangeFromToken(
             rRange = rRefData.toAbs(*pDoc, rPos);
             return true;
         }
+        case svIndex:
+        {
+            ScRangeData* pNameRange = pDoc->FindRangeNameBySheetAndIndex(pToken->GetSheet(), pToken->GetIndex());
+            if (pNameRange)
+            {
+                ScTokenArray* pRangeNameToken = pNameRange->GetCode();
+                if (pRangeNameToken->GetLen() == 1)
+                {
+                    ::boost::intrusive_ptr<formula::FormulaToken> pToken = pRangeNameToken->FirstToken();
+                    return getRangeFromToken(pDoc, rRange, pToken, rPos, bExternal);
+                }
+            }
+        }
         default:
             ; // do nothing
     }
