07c7609a30cc8b718a777e95b609ecb394447f27
Caolán McNamara <caolanm@redhat.com>
2021-07-14 20:25:06 +0100

    cid#1487034 Untrusted value as argument
    Change-Id: I2c0edac58b92b9d828c62ff3b8859f23ed1d3c85

--- vcl/source/filter/svm/SvmReader.cxx
+++ vcl/source/filter/svm/SvmReader.cxx
@@ -663,6 +663,14 @@ rtl::Reference<MetaAction> SvmReader::TextHandler(ImplMetaReadData* pData)
     if (aCompat.GetVersion() >= 2) // Version 2
         aStr = read_uInt16_lenPrefixed_uInt16s_ToOUString(mrStream);
 
+    if (nTmpIndex + nTmpLen > aStr.getLength())
+    {
+        SAL_WARN("vcl.gdi", "inconsistent offset and len");
+        pAction->SetIndex(0);
+        pAction->SetLen(aStr.getLength());
+        aArray.reset();
+    }
+
     pAction->SetText(aStr);
 
     return pAction;
@@ -696,8 +704,9 @@ rtl::Reference<MetaAction> SvmReader::TextArrayHandler(ImplMetaReadData* pData)
     sal_Int32 nAryLen(0);
     mrStream.ReadInt32(nAryLen);
 
-    if (nTmpLen > aStr.getLength() - nTmpIndex)
+    if (nTmpIndex + nTmpLen > aStr.getLength())
     {
+        SAL_WARN("vcl.gdi", "inconsistent offset and len");
         pAction->SetIndex(0);
         pAction->SetLen(aStr.getLength());
         return pAction;
@@ -735,6 +744,7 @@ rtl::Reference<MetaAction> SvmReader::TextArrayHandler(ImplMetaReadData* pData)
 
         if (nTmpIndex + nTmpLen > aStr.getLength())
         {
+            SAL_WARN("vcl.gdi", "inconsistent offset and len");
             pAction->SetIndex(0);
             pAction->SetLen(aStr.getLength());
             aArray.reset();
