169392a4b9a968844040a516d1e576c4ab751abb
Henry Castro <hcastro@collabora.com>
2021-07-15 11:31:21 -0400

    lok: sc: simplify drag & drop functions
    The ScDragData is a global use for private drag & drop
    in a desktop case, it will be used by tiled rendering
    case to pass its own local ScDragData per user.
    
    Change-Id: I0bca8e6d5c0eab82b69374a67425e2a8d746ef4e
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/118990
    Tested-by: Szymon Kłos <szymon.klos@collabora.com>
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>

--- sc/source/ui/view/gridwin.cxx
+++ sc/source/ui/view/gridwin.cxx
@@ -3845,7 +3845,7 @@ static ScRange lcl_MakeDropRange( const ScDocument& rDoc, SCCOL nPosX, SCROW nPo
     return ScRange( nCol1, nRow1, nTab, nCol2, nRow2, nTab );
 }
 
-sal_Int8 ScGridWindow::AcceptPrivateDrop( const AcceptDropEvent& rEvt )
+sal_Int8 ScGridWindow::AcceptPrivateDrop( const AcceptDropEvent& rEvt, const ScDragData& rData )
 {
     if ( rEvt.mbLeaving )
     {
@@ -3854,7 +3854,6 @@ sal_Int8 ScGridWindow::AcceptPrivateDrop( const AcceptDropEvent& rEvt )
         return rEvt.mnAction;
     }
 
-    const ScDragData& rData = SC_MOD()->GetDragData();
     if ( rData.pCellTransfer )
     {
         // Don't move source that would include filtered rows.
@@ -4067,7 +4066,7 @@ sal_Int8 ScGridWindow::AcceptDrop( const AcceptDropEvent& rEvt )
     {
         DrawMarkDropObj( nullptr );
         if ( rData.pCellTransfer )
-            return AcceptPrivateDrop( rEvt );   // hide drop marker for internal D&D
+            return AcceptPrivateDrop( rEvt, rData );   // hide drop marker for internal D&D
         else
             return rEvt.mnAction;
     }
@@ -4085,7 +4084,7 @@ sal_Int8 ScGridWindow::AcceptDrop( const AcceptDropEvent& rEvt )
              aSource.aStart.Row() != 0 || aSource.aEnd.Row() != rThisDoc.MaxRow() )
             DropScroll( rEvt.maPosPixel );
 
-        nRet = AcceptPrivateDrop( rEvt );
+        nRet = AcceptPrivateDrop( rEvt, rData );
     }
     else
     {
@@ -4344,14 +4343,11 @@ static SotClipboardFormatId lcl_GetDropLinkId( const uno::Reference<datatransfer
     return nFormatId;
 }
 
-sal_Int8 ScGridWindow::ExecutePrivateDrop( const ExecuteDropEvent& rEvt )
+sal_Int8 ScGridWindow::ExecutePrivateDrop( const ExecuteDropEvent& rEvt, const ScDragData& rData )
 {
     // hide drop marker
     bDragRect = false;
     UpdateDragRectOverlay();
-
-    ScModule* pScMod = SC_MOD();
-    const ScDragData& rData = pScMod->GetDragData();
 
     return DropTransferObj( rData.pCellTransfer, nDragStartX, nDragStartY,
                                 PixelToLogic(rEvt.maPosPixel), rEvt.mnAction );
@@ -4722,7 +4718,7 @@ sal_Int8 ScGridWindow::ExecuteDrop( const ExecuteDropEvent& rEvt )
     ScModule* pScMod = SC_MOD();
     const ScDragData& rData = pScMod->GetDragData();
     if (rData.pCellTransfer)
-        return ExecutePrivateDrop( rEvt );
+        return ExecutePrivateDrop( rEvt, rData );
 
     Point aPos = rEvt.maPosPixel;
 
