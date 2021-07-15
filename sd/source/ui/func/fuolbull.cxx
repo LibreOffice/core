e7b632bc727413f399a4d0cbc2e92d90b04a3bf7
Noel Grandin <noel.grandin@collabora.co.uk>
2021-07-15 21:49:50 +0200

    use WhichRangesContainer in sd
    Change-Id: I528905c14f40bee85689f24ebaef8e8fde1b107b
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/118999
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

--- sd/source/ui/func/fuolbull.cxx
+++ sd/source/ui/func/fuolbull.cxx
@@ -79,14 +79,7 @@ void FuBulletAndPosition::DoExecute( SfxRequest& rReq )
     SfxItemSet aEditAttr( mpDoc->GetPool() );
     mpView->GetAttributes( aEditAttr );
 
-    static const sal_uInt16 aAttrMap[] =
-    {
-        EE_PARA_NUMBULLET,
-        EE_PARA_BULLET,
-        0
-    };
-
-    SfxItemSet aNewAttr( mpViewShell->GetPool(), aAttrMap );
+    SfxItemSet aNewAttr( mpViewShell->GetPool(), svl::Items<EE_PARA_NUMBULLET, EE_PARA_BULLET>{} );
     aNewAttr.Put( aEditAttr, false );
 
     auto pView = mpView;
