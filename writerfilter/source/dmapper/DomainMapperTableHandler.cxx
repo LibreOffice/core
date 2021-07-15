LibreOffice/core

commit 2ab481b038b62b1ff576ac4d49d03c1798cd7f84

tdf#90069 DOCX: fix character style of new table rows
DOCX table import didn't set paragraph level
character styles on paragraph level, only on
text portions, resulting default character style
in the newly inserted table rows instead of copying
the style of the previous table row.

Change-Id: Idb4438c767bdc7e0026fc6e0f0a795d8efdda3c8
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/86429
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>
Tested-by: László Németh <nemeth@numbertext.org>

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -1771,6 +1771,19 @@ void DomainMapper_Impl::finishParagraph( const PropertyMapPtr& pPropertyMap, con
                         }
                     }
                 }
+
+                // tdf#90069 in tables, apply paragraph level character style also on
+                // paragraph level to support its copy during insertion of new table rows
+                if ( xParaProps && m_nTableDepth > 0 )
+                {
+                    uno::Sequence< beans::PropertyValue > aValues = pParaContext->GetPropertyValues(false);
+
+                    for( const auto& rProp : std::as_const(aValues) )
+                    {
+                        if ( rProp.Name.startsWith("Char") && rProp.Name != "CharStyleName" && rProp.Name != "CharInteropGrabBag" )
+                            xParaProps->setPropertyValue( rProp.Name, rProp.Value );
+                    }
+                }
             }
             if( !bKeepLastParagraphProperties )
                 rAppendContext.pLastParagraphProperties = pToBeSavedProperties;

sw/qa/extras/uiwriter/uiwriter2.cxx
@@ -49,6 +49,7 @@
 #include <postithelper.hxx>
 #include <fmtcntnt.hxx>
 #include <shellio.hxx>
+#include <editeng/fontitem.hxx>
 
 namespace
 {
@@ -2419,6 +2420,35 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest2, testOfz18563)
     OUString sURL(m_directories.getURLFromSrc("/sw/qa/extras/uiwriter/data2/ofz18563.docx"));
     SvFileStream aFileStream(sURL, StreamMode::READ);
     TestImportDOCX(aFileStream);
+}
+
+CPPUNIT_TEST_FIXTURE(SwUiWriterTest2, testTdf90069)
+{
+    SwDoc* pDoc = createDoc("tdf90069.docx");
+
+    SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument*>(mxComponent.get());
+    CPPUNIT_ASSERT(pTextDoc);
+
+    SwDocShell* pDocShell = pTextDoc->GetDocShell();
+    CPPUNIT_ASSERT(pDocShell);
+
+    SwWrtShell* pWrtShell = pDocShell->GetWrtShell();
+    CPPUNIT_ASSERT(pWrtShell);
+
+    sal_uLong nIndex = pWrtShell->GetCursor()->GetNode().GetIndex();
+
+    lcl_dispatchCommand(mxComponent, ".uno:InsertRowsAfter", {});
+    pWrtShell->Down(false);
+    pWrtShell->Insert("foo");
+
+    SwTextNode* pTextNodeA1 = static_cast<SwTextNode*>(pDoc->GetNodes()[nIndex]);
+    CPPUNIT_ASSERT(pTextNodeA1->GetText().startsWith("Insert"));
+    nIndex = pWrtShell->GetCursor()->GetNode().GetIndex();
+    SwTextNode* pTextNodeA2 = static_cast<SwTextNode*>(pDoc->GetNodes()[nIndex]);
+    CPPUNIT_ASSERT_EQUAL(OUString("foo"), pTextNodeA2->GetText());
+    CPPUNIT_ASSERT_EQUAL(true, pTextNodeA2->GetSwAttrSet().HasItem(RES_CHRATR_FONT));
+    OUString sFontName = pTextNodeA2->GetSwAttrSet().GetItem(RES_CHRATR_FONT)->GetFamilyName();
+    CPPUNIT_ASSERT_EQUAL(OUString("Lohit Devanagari"), sFontName);
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

commit 86ad6b4327f2ef26c08d9ce428693e10221de6df

tdf#131546 writerfilter: optimization of table import
This improves the opening time (via 15-20%) with the reported document. This bug is a regression from 2ab481b038b62b1ff576ac4d49d03c1798cd7f84.
Recently in tables for every paragraph, the paragraph level character styles were applied too on paragraph level, to support its copy during insertion of new table rows.
This was done for every paragraphs of the table, but as https://bugs.documentfoundation.org/show_bug.cgi?id=131546#c4 mentions, it is enough to do this only for the last row's paragraphs.

Change-Id: Ic24d0d1830e9ec43323e5e213d6481ec28e9abe3

writerfilter/source/dmapper/DomainMapperTableHandler.cxx
@@ -1413,6 +1413,53 @@ void DomainMapperTableHandler::endTable(unsigned int nestedTableLevel, bool bTab
         if (bConvertToFloating)
             DomainMapper_Impl::fillEmptyFrameProperties(aFrameProperties, true);
 
+        //copy run props to para props only to the last rows' paragraphs, to support copying rows when inserting new rows
+        TableParagraphVectorPtr pTableParagraphs = m_rDMapper_Impl.getTableManager().getCurrentParagraphs();
+        for(size_t nRow = 0; nRow < m_aTableRanges.size(); ++nRow)
+        {
+            for(size_t nCell = 0; nCell < m_aTableRanges[nRow].size(); ++nCell)
+            {
+                std::vector<TableParagraph>::iterator aIt2 = pTableParagraphs->begin();
+                if(nRow >= m_aTableRanges.size()-1)
+                {
+                //we are in the last row
+                auto rStartPara = m_aTableRanges[nRow][nCell][0];
+                if (!rStartPara.is())
+                    continue;
+                uno::Reference<text::XTextRangeCompare> xTextRangeCompare(rStartPara->getText(), uno::UNO_QUERY);
+                while ( aIt2 != pTableParagraphs->end() ) try
+                {
+                        if (xTextRangeCompare->compareRegionStarts(rStartPara, aIt2->m_rStartParagraph) == 0)
+                        {
+                            //copying run props to the para
+                            css::uno::Reference<css::beans::XPropertySet> xParaProps(rStartPara, uno::UNO_QUERY);
+                            uno::Sequence< beans::PropertyValue > aParaProps = aIt2->m_pPropertyMap->GetPropertyValues(false);
+                            uno::Reference<text::XTextCursor> xCur =  rStartPara->getText()->createTextCursorByRange(rStartPara);
+                            uno::Reference< beans::XPropertyState > xRunProperties( xCur, uno::UNO_QUERY_THROW );
+                            for( const auto& rParaProp : std::as_const(aParaProps) )
+                            {
+                                if ( rParaProp.Name.startsWith("Char") && rParaProp.Name != "CharStyleName" && rParaProp.Name != "CharInteropGrabBag" &&
+                                // all text portions contain the same value, so next setPropertyValue() won't overwrite part of them
+                                xRunProperties->getPropertyState(rParaProp.Name) == css::beans::PropertyState_DIRECT_VALUE )
+                                {
+                                    uno::Reference<beans::XPropertySet> xRunPropertySet(xCur, uno::UNO_QUERY);
+                                    xParaProps->setPropertyValue( rParaProp.Name, xRunPropertySet->getPropertyValue(rParaProp.Name) );
+                                    // remember this for table style handling
+                                    m_rDMapper_Impl.getTableManager().getCurrentParagraphs()->back().m_aParaOverrideApplied.insert(rParaProp.Name);
+                                }
+                            }
+                        }
+                        ++aIt2;
+                    }
+                    catch( const lang::IllegalArgumentException & )
+                    {
+                        // skip compareRegion with nested tables
+                        ++aIt2;
+                    }
+                }
+            }
+        }
+
         // OOXML table style may contain paragraph properties, apply these on cell paragraphs
         if ( m_aTableRanges[0].hasElements() && m_aTableRanges[0][0].hasElements() )
         {
@@ -1430,7 +1477,6 @@ void DomainMapperTableHandler::endTable(unsigned int nestedTableLevel, bool bTab
 
             if ( !aAllTableParaProperties.empty() )
             {
-                TableParagraphVectorPtr pTableParagraphs = m_rDMapper_Impl.getTableManager().getCurrentParagraphs();
                 for (size_t nRow = 0; nRow < m_aTableRanges.size(); ++nRow)
                 {
                     // Note that this is "cell" since you must not treat it as "column".

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -1199,7 +1199,6 @@ void DomainMapper_Impl::CheckUnregisteredFrameConversion( )
     {
         StyleSheetEntryPtr pParaStyle =
             GetStyleSheetTable()->FindStyleSheetByConvertedStyleName(rAppendContext.pLastParagraphProperties->GetParaStyleName());
-
         std::vector<beans::PropertyValue> aFrameProperties;
 
         if ( pParaStyle )
@@ -2198,27 +2197,8 @@ void DomainMapper_Impl::finishParagraph( const PropertyMapPtr& pPropertyMap, con
                     }
                 }
 
-                // fix table paragraph properties
                 if ( xTextRange.is() && xParaProps && m_nTableDepth > 0 )
                 {
-                    uno::Sequence< beans::PropertyValue > aParaProps = pParaContext->GetPropertyValues(false);
-                    uno::Reference<text::XTextCursor> xCur =  xTextRange->getText()->createTextCursorByRange(xTextRange);
-                    uno::Reference< beans::XPropertyState > xRunProperties( xCur, uno::UNO_QUERY_THROW );
-                    // tdf#90069 in tables, apply paragraph level character style also on
-                    // paragraph level to support its copy during insertion of new table rows
-                    for( const auto& rParaProp : std::as_const(aParaProps) )
-                    {
-                        if ( rParaProp.Name.startsWith("Char") && rParaProp.Name != "CharStyleName" && rParaProp.Name != "CharInteropGrabBag" &&
-                            // all text portions contain the same value, so next setPropertyValue() won't overwrite part of them
-                            xRunProperties->getPropertyState(rParaProp.Name) == css::beans::PropertyState_DIRECT_VALUE )
-                        {
-                            uno::Reference<beans::XPropertySet> xRunPropertySet(xCur, uno::UNO_QUERY);
-                            xParaProps->setPropertyValue( rParaProp.Name, xRunPropertySet->getPropertyValue(rParaProp.Name) );
-                            // remember this for table style handling
-                            getTableManager().getCurrentParagraphs()->back().m_aParaOverrideApplied.insert(rParaProp.Name);
-                        }
-                    }
-
                     // tdf#128959 table paragraphs haven't got window and orphan controls
                     uno::Any aAny = uno::makeAny(static_cast<sal_Int8>(0));
                     xParaProps->setPropertyValue("ParaOrphans", aAny);