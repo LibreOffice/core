LibreOffice/core

commit 04aafba860f613c20e7078d038cc83eb02de0b54

loplugin:stringadd simplify some *StringBuffer operations
pulled from a larger patch which I created with a more permissive
variant of this plugin

Change-Id: I7abf1f3f09e84703b6e0e52fe9587dff691b2187
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/114875
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

dbaccess/source/ui/querydesign/QueryDesignView.cxx
@@ -658,11 +658,10 @@ namespace
                     if  ( field->isAggregateFunction() )
                     {
                         OSL_ENSURE(!field->GetFunction().isEmpty(),"Function name must not be empty! ;-(");
-                        OUStringBuffer aTmpStr2( field->GetFunction());
-                        aTmpStr2.append("(");
-                        aTmpStr2.append(aTmpStr.makeStringAndClear());
-                        aTmpStr2.append(")");
-                        aTmpStr = aTmpStr2;
+                        aTmpStr = field->GetFunction() +
+                            "(" +
+                            aTmpStr +
+                            ")";
                     }
 
                     if (!rFieldAlias.isEmpty()                         &&

commit 001157f73d0a73d97bd417d26ef476e507d7deec

tdf#143400: fix query with most functions could not be created in GUI
Regression from:
https://cgit.freedesktop.org/libreoffice/core/commit/?id=04aafba860f613c20e7078d038cc83eb02de0b54
loplugin:stringadd simplify some *StringBuffer operations
pulled from a larger patch which I created with a more permissive
variant of this plugin

Specifically here:
-------------- dbaccess/source/ui/querydesign/QueryDesignView.cxx --------------
index 22408bbc58aa..63c5d07998ec 100644
@@ -658,11 +658,10 @@ namespace
                     if  ( field->isAggregateFunction() )
                     {
                         OSL_ENSURE(!field->GetFunction().isEmpty(),"Function name must not be empty! ;-(");
-                        OUStringBuffer aTmpStr2( field->GetFunction());
-                        aTmpStr2.append("(");
-                        aTmpStr2.append(aTmpStr.makeStringAndClear());
-                        aTmpStr2.append(")");
-                        aTmpStr = aTmpStr2;
+                        aTmpStr = field->GetFunction() +
+                            "(" +
+                            aTmpStr +
+                            ")";
                     }

                     if (!rFieldAlias.isEmpty()                         &&

Change-Id: Ib0b57e6c44a58a6a5f9c98aebc6a1213b35108d1

dbaccess/source/ui/querydesign/QueryDesignView.cxx
@@ -658,10 +658,8 @@ namespace
                     if  ( field->isAggregateFunction() )
                     {
                         OSL_ENSURE(!field->GetFunction().isEmpty(),"Function name must not be empty! ;-(");
-                        aTmpStr = field->GetFunction() +
-                            "(" +
-                            aTmpStr +
-                            ")";
+                        OUStringBuffer aTmpStr2( field->GetFunction() + "(" + aTmpStr.makeStringAndClear() + ")");
+                        aTmpStr = aTmpStr2;
                     }
 
                     if (!rFieldAlias.isEmpty()                         &&