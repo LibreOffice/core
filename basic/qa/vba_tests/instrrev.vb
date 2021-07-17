69ba67a125ed60d5a94648e8978439a888ece0cf
Andreas Heinisch <andreas.heinisch@yahoo.de>
2021-07-17 10:42:58 +0200

    tdf#143332 - Use utl::TextSearch to implement the InStrRev function
    Change-Id: I2dae038d061f036b3b7c0fbbcb6a821305d4839e
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/119094
    Tested-by: Jenkins
    Reviewed-by: Andreas Heinisch <andreas.heinisch@yahoo.de>
    (cherry picked from commit f5e030a343bec9f0f2c7b30e2dfb21ba5c94e5e8)

--- basic/qa/vba_tests/instrrev.vb
+++ basic/qa/vba_tests/instrrev.vb
@@ -28,7 +28,14 @@ Sub verify_testInStrRev()
     SearchChar = "P"    ' Search for "P".
     TestUtil.AssertEqual(InStrRev(SearchString, SearchChar, 4, 1),   3, "InStrRev(SearchString, SearchChar, 4, 1)")
     TestUtil.AssertEqual(InStrRev(SearchString, SearchChar, -1, 0), 12, "InStrRev(SearchString, SearchChar, -1, 0)")
-    TestUtil.AssertEqual(InStrRev(SearchString, "W", 1),             0, "InStrRev(SearchString, ""W"", 1)")
+    TestUtil.AssertEqual(InStrRev(SearchString, "W", 1), 0, "InStrRev(SearchString, ""W"", 1)")
+
+    ' tdf#143332 - case-insensitive operation for non-ASCII characters
+    TestUtil.AssertEqual(InStrRev("α", "Α", -1, 1),      1, "InStrRev(""α"", ""Α"", -1, 1)")
+    TestUtil.AssertEqual(InStrRev("abc", "d", -1, 1),    0, "InStrRev(""abc"", ""d"", -1, 1)")
+    ' tdf#143332 - German Eszett is uppercased to a two-character 'SS'.
+    ' This test should fail after tdf#110003 has been fixed.
+    TestUtil.AssertEqual(InStrRev("Straße", "s", -1, 1), 5, "InStrRev(""Straße"", ""s"", -1, 1)")
 
     Exit Sub
 errorHandler:
