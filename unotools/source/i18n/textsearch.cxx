90bb4a92ed86e476f78f922397a21b8173ecf0ae
Noel Grandin <noelgrandin@gmail.com>
2021-07-17 19:02:30 +0200

    osl::Mutex->std::mutex in CachedTextSearch
    and drop an rtl::Static
    
    Change-Id: I17bd6ad0b81ecb947e4306819ddc7d83f8a51dba

--- unotools/source/i18n/textsearch.cxx
+++ unotools/source/i18n/textsearch.cxx
@@ -93,30 +93,27 @@ namespace
 {
     struct CachedTextSearch
     {
-        ::osl::Mutex mutex;
+        std::mutex mutex;
         i18nutil::SearchOptions2 Options;
         css::uno::Reference< css::util::XTextSearch2 > xTextSearch;
     };
-
-    struct theCachedTextSearch
-        : public rtl::Static< CachedTextSearch, theCachedTextSearch > {};
 }
 
 Reference<XTextSearch2> TextSearch::getXTextSearch( const i18nutil::SearchOptions2& rPara )
 {
-    CachedTextSearch &rCache = theCachedTextSearch::get();
+    static CachedTextSearch theCachedTextSearch;
 
-    osl::MutexGuard aGuard(rCache.mutex);
+    std::lock_guard aGuard(theCachedTextSearch.mutex);
 
-    if ( lcl_Equals(rCache.Options, rPara) )
-        return rCache.xTextSearch;
+    if ( lcl_Equals(theCachedTextSearch.Options, rPara) )
+        return theCachedTextSearch.xTextSearch;
 
     Reference< XComponentContext > xContext = ::comphelper::getProcessComponentContext();
-    rCache.xTextSearch.set( ::TextSearch2::create(xContext) );
-    rCache.xTextSearch->setOptions2( rPara.toUnoSearchOptions2() );
-    rCache.Options = rPara;
+    theCachedTextSearch.xTextSearch.set( ::TextSearch2::create(xContext) );
+    theCachedTextSearch.xTextSearch->setOptions2( rPara.toUnoSearchOptions2() );
+    theCachedTextSearch.Options = rPara;
 
-    return rCache.xTextSearch;
+    return theCachedTextSearch.xTextSearch;
 }
 
 TextSearch::TextSearch(const SearchParam & rParam, LanguageType eLang )
