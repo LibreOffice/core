LibreOffice/core

commit 0771ac00acc8730f77db76b901724f1513a32723

use string_view in the Translate API
Change-Id: I0bb0ea9d39ed623928060ffd3f2e2bc36ba33209
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/117272
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

unotools/source/i18n/resmgr.cxx
@@ -116,11 +116,11 @@ static int IgnoringCrtReportHook(int reportType, wchar_t *message, int * /* retu
 
 namespace Translate
 {
-    std::locale Create(const char* pPrefixName, const LanguageTag& rLocale)
+    std::locale Create(std::string_view aPrefixName, const LanguageTag& rLocale)
     {
         static std::unordered_map<OString, std::locale> aCache;
         OString sIdentifier = rLocale.getGlibcLocaleString(u".UTF-8").toUtf8();
-        OString sUnique = sIdentifier + pPrefixName;
+        OString sUnique = sIdentifier + aPrefixName;
         auto aFind = aCache.find(sUnique);
         if (aFind != aCache.end())
             return aFind->second;
@@ -146,11 +146,11 @@ namespace Translate
         gen.add_messages_path(sPath.getStr());
 #if defined UNX && !defined MACOSX && !defined IOS && !defined ANDROID
         // allow gettext to find these .mo files e.g. so gtk dialogs can use them
-        bindtextdomain(pPrefixName, sPath.getStr());
+        bindtextdomain(aPrefixName.data(), sPath.getStr());
         // tdf#131069 gtk, and anything sane, always wants utf-8 strings as output
-        bind_textdomain_codeset(pPrefixName, "UTF-8");
+        bind_textdomain_codeset(aPrefixName.data(), "UTF-8");
 #endif
-        gen.add_messages_domain(pPrefixName);
+        gen.add_messages_domain(aPrefixName.data());
 
 #if defined(_WIN32) && defined(DBG_UTIL)
         // With a newer C++ debug runtime (in an --enable-dbgutil build), passing an invalid locale
@@ -197,28 +197,29 @@ namespace Translate
         return aRet;
     }
 
-    OUString get(const char* pContextAndId, const std::locale &loc)
+    OUString get(std::string_view sContextAndId, const std::locale &loc)
     {
-        OString sContext;
-        const char *pId = strchr(pContextAndId, '\004');
-        if (!pId)
-            pId = pContextAndId;
+        std::string_view sContext;
+        std::string_view sId;
+        const char *p = strchr(sContextAndId.data(), '\004');
+        if (!p)
+            sId = sContextAndId;
         else
         {
-            sContext = OString(pContextAndId, pId - pContextAndId);
-            ++pId;
-            assert(!strchr(pId, '\004') && "should be using nget, not get");
+            sContext = std::string_view(sContextAndId.data(), p - sContextAndId.data());
+            sId = sContextAndId.substr(p - sContextAndId.data() + 1);
+            assert(!strchr(sId.data(), '\004') && "should be using nget, not get");
         }
 
         //if it's a key id locale, generate it here
         if (std::use_facet<boost::locale::info>(loc).language() == "qtz")
         {
-            OString sKeyId(genKeyId(OString(pContextAndId).replace('\004', '|')));
-            return OUString::fromUtf8(sKeyId) + u"\u2016" + createFromUtf8(pId, strlen(pId));
+            OString sKeyId(genKeyId(OString(sContextAndId).replace('\004', '|')));
+            return OUString::fromUtf8(sKeyId) + u"\u2016" + createFromUtf8(sId.data(), sId.size());
         }
 
         //otherwise translate it
-        const std::string ret = boost::locale::pgettext(sContext.getStr(), pId, loc);
+        const std::string ret = boost::locale::pgettext(sContext.data(), sId.data(), loc);
         OUString result(ExpandVariables(createFromUtf8(ret.data(), ret.size())));
 
         if (comphelper::LibreOfficeKit::isActive())
@@ -231,9 +232,9 @@ namespace Translate
         return result;
     }
 
-    OUString nget(const char* pContextAndIds, int n, const std::locale &loc)
+    OUString nget(std::string_view aContextAndIds, int n, const std::locale &loc)
     {
-        OString sContextIdId(pContextAndIds);
+        OString sContextIdId(aContextAndIds);
         std::vector<OString> aContextIdId;
         sal_Int32 nIndex = 0;
         do

commit a40716a3fa80a2e77a81289bf4fedfb0a3f040c3

tdf#143409: fix Translation is not applied in some UI such as 'Option dialog'
After some debug, I noticed this:
4  0x00007f4a5b33ac32 in Translate::get (sContextAndId="STR_POOLCOLL_GREETING\004Complimentary Close", loc=...) at unotools/source/i18n/resmgr.cxx:223
223	        const std::string ret = boost::locale::pgettext(sContext.data(), sId.data(), loc);
(gdb) p sContext
$22 = "STR_POOLCOLL_GREETING"
(gdb) p sContext.data()
$23 = (const std::basic_string_view<char, std::char_traits<char> >::value_type *) 0x7f4a463ff8ff "STR_POOLCOLL_GREETING\004Complimentary Close"

Here's the code:
    200     OUString get(std::string_view sContextAndId, const std::locale &loc)
    201     {
    202         std::string_view sContext;
    203         std::string_view sId;
    204         const char *p = strchr(sContextAndId.data(), '\004');
    205         if (!p)
    206             sId = sContextAndId;
    207         else
    208         {
    209             sContext = std::string_view(sContextAndId.data(), p - sContextAndId.data());
    210             sId = sContextAndId.substr(p - sContextAndId.data() + 1);
    211             assert(!strchr(sId.data(), '\004') && "should be using nget, not get");
    212         }
    213
    214         //if it's a key id locale, generate it here
    215         if (std::use_facet<boost::locale::info>(loc).language() == "qtz")
    216         {
    217             OString sKeyId(genKeyId(OString(sContextAndId).replace('\004', '|')));
    218             return OUString::fromUtf8(sKeyId) + u"\u2016" + createFromUtf8(sId.data(), sId.size());
    219         }
    220
    221         //otherwise translate it
    222         const std::string ret = boost::locale::pgettext(sContext.data(), sId.data(), loc);
    223         OUString result(ExpandVariables(createFromUtf8(ret.data(), ret.size())));

Let's take the example I took above "STR_POOLCOLL_GREETING\004Complimentary Close".
- sContextAndId contains indeed "STR_POOLCOLL_GREETING\004Complimentary Close" and it's ok
- p is a pointer on first character of "\004Complimentary Close", quite expected
- sContextAndId.data() is a pointer on the first character of the view,
so if we display it, we've got too "STR_POOLCOLL_GREETING\004Complimentary Close"
- p - sContextAndId.data() = 21, ok too

At line 209, sContext by calling std::string_view with the data from sContextAndId and size 21
=> sContext value is "STR_POOLCOLL_GREETING", ok but its data is "STR_POOLCOLL_GREETING\004Complimentary Close"
and it seems to correspond to https://en.cppreference.com/w/cpp/string/basic_string_view/basic_string_view, ctr 3:
"3) Constructs a view of the first count characters of the character array starting with the element pointed by s.
s can contain null characters. The behavior is undefined if [s, s+count) is not a valid range
(even though the constructor may not access any of the elements of this range).
After construction, data() is equal to s, and size() is equal to count."

So "data() is equal to s"!, rather unexpected!

Regression from 0771ac00acc8730f77db76b901724f1513a32723

unotools/source/i18n/resmgr.cxx
@@ -199,14 +199,12 @@ namespace Translate
 
     OUString get(std::string_view sContextAndId, const std::locale &loc)
     {
-        std::string_view sContext;
         std::string_view sId;
         const char *p = strchr(sContextAndId.data(), '\004');
         if (!p)
             sId = sContextAndId;
         else
         {
-            sContext = std::string_view(sContextAndId.data(), p - sContextAndId.data());
             sId = sContextAndId.substr(p - sContextAndId.data() + 1);
             assert(!strchr(sId.data(), '\004') && "should be using nget, not get");
         }
@@ -219,7 +217,7 @@ namespace Translate
         }
 
         //otherwise translate it
-        const std::string ret = boost::locale::pgettext(sContext.data(), sId.data(), loc);
+        const std::string ret = boost::locale::pgettext(std::string(sContextAndId.data(), p - sContextAndId.data()).c_str(), sId.data(), loc);
         OUString result(ExpandVariables(createFromUtf8(ret.data(), ret.size())));
 
         if (comphelper::LibreOfficeKit::isActive())