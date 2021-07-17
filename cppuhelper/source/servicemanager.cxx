05736f18190bf7d3175733be1b6979b6727530d0
Noel Grandin <noelgrandin@gmail.com>
2021-07-17 18:46:51 +0200

    osl::Mutex->std::mutex in ContentEnumeration
    Change-Id: I79457a4ffc957f6de47880fdc888648ab69d1349

--- cppuhelper/source/servicemanager.cxx
+++ cppuhelper/source/servicemanager.cxx
@@ -12,6 +12,7 @@
 #include <algorithm>
 #include <cassert>
 #include <iostream>
+#include <mutex>
 #include <vector>
 
 #include <com/sun/star/beans/NamedValue.hpp>
@@ -462,20 +463,20 @@ private:
 
     virtual css::uno::Any SAL_CALL nextElement() override;
 
-    osl::Mutex mutex_;
+    std::mutex mutex_;
     std::vector< css::uno::Any > factories_;
     std::vector< css::uno::Any >::const_iterator iterator_;
 };
 
 sal_Bool ContentEnumeration::hasMoreElements()
 {
-    osl::MutexGuard g(mutex_);
+    std::lock_guard g(mutex_);
     return iterator_ != factories_.end();
 }
 
 css::uno::Any ContentEnumeration::nextElement()
 {
-    osl::MutexGuard g(mutex_);
+    std::lock_guard g(mutex_);
     if (iterator_ == factories_.end()) {
         throw css::container::NoSuchElementException(
             "Bootstrap service manager service enumerator has no more elements",
