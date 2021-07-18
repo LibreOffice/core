371ae1cf2307e5585f4f66f001c97d464cf44429
Julien Nabet <serval2412@yahoo.fr>
2021-07-18 10:01:45 +0200

    nlpsolver: DEPSAgent doesn't implement ILibEngine
    Fix this error:
    /home/julien/lo/libreoffice/nlpsolver/ThirdParty/EvolutionarySolver/src/net/adaptivebox/deps/DEPSAgent.java:45: error: DEPSAgent is not abstract and does not override abstract method setLibrary(Library) in ILibEngine
    public class DEPSAgent implements ILibEngine {
           ^
    1 error
    
    (on pc Debian testing x86-64 with master sources updated today
    openjdk 11.0.11 2021-04-20
    OpenJDK Runtime Environment (build 11.0.11+9-post-Debian-1)
    OpenJDK 64-Bit Server VM (build 11.0.11+9-post-Debian-1, mixed mode, sharing)
    )
    
    Change-Id: I1ce2d1a9ddee1020889f1a7a8fcd3387980b241f
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/119119
    Tested-by: Jenkins
    Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

--- nlpsolver/ThirdParty/EvolutionarySolver/src/net/adaptivebox/deps/DEPSAgent.java
+++ nlpsolver/ThirdParty/EvolutionarySolver/src/net/adaptivebox/deps/DEPSAgent.java
@@ -43,7 +43,7 @@ import net.adaptivebox.knowledge.SearchPoint;
 import net.adaptivebox.problem.ProblemEncoder;
 import net.adaptivebox.space.BasicPoint;
 
-public class DEPSAgent implements ILibEngine {
+public class DEPSAgent {
 
   // Describes the problem to be solved
   private ProblemEncoder problemEncoder;
