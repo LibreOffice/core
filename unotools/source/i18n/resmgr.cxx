LibreOffice/core

commit 0771ac00acc8730f77db76b901724f1513a32723

use string_view in the Translate API
Change-Id: I0bb0ea9d39ed623928060ffd3f2e2bc36ba33209
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/117272
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

unotools/source/i18n/resmgr.cxx
@@ -116,11 +116,11 @@ static int IgnoringCrtReportHook(int reportType, wchar_t *message, int * /* retu
 
 namespace Translate
 {
-    std::locale Create(const char* pPrefixName, const LanguageTag& rLocale)
+    std::locale Create(std::string_view aPrefixName, const LanguageTag& rLocale)
     {
         static std::unordered_map<OString, std::locale> aCache;
         OString sIdentifier = rLocale.getGlibcLocaleString(u".UTF-8").toUtf8();
-        OString sUnique = sIdentifier + pPrefixName;
+        OString sUnique = sIdentifier + aPrefixName;
         auto aFind = aCache.find(sUnique);
         if (aFind != aCache.end())
             return aFind->second;
@@ -146,11 +146,11 @@ namespace Translate
         gen.add_messages_path(sPath.getStr());
 #if defined UNX && !defined MACOSX && !defined IOS && !defined ANDROID
         // allow gettext to find these .mo files e.g. so gtk dialogs can use them
-        bindtextdomain(pPrefixName, sPath.getStr());
+        bindtextdomain(aPrefixName.data(), sPath.getStr());
         // tdf#131069 gtk, and anything sane, always wants utf-8 strings as output
-        bind_textdomain_codeset(pPrefixName, "UTF-8");
+        bind_textdomain_codeset(aPrefixName.data(), "UTF-8");
 #endif
-        gen.add_messages_domain(pPrefixName);
+        gen.add_messages_domain(aPrefixName.data());
 
 #if defined(_WIN32) && defined(DBG_UTIL)
         // With a newer C++ debug runtime (in an --enable-dbgutil build), passing an invalid locale
@@ -197,28 +197,29 @@ namespace Translate
         return aRet;
     }
 
-    OUString get(const char* pContextAndId, const std::locale &loc)
+    OUString get(std::string_view sContextAndId, const std::locale &loc)
     {
-        OString sContext;
-        const char *pId = strchr(pContextAndId, '\004');
-        if (!pId)
-            pId = pContextAndId;
+        std::string_view sContext;
+        std::string_view sId;
+        const char *p = strchr(sContextAndId.data(), '\004');
+        if (!p)
+            sId = sContextAndId;
         else
         {
-            sContext = OString(pContextAndId, pId - pContextAndId);
-            ++pId;
-            assert(!strchr(pId, '\004') && "should be using nget, not get");
+            sContext = std::string_view(sContextAndId.data(), p - sContextAndId.data());
+            sId = sContextAndId.substr(p - sContextAndId.data() + 1);
+            assert(!strchr(sId.data(), '\004') && "should be using nget, not get");
         }
 
         //if it's a key id locale, generate it here
         if (std::use_facet<boost::locale::info>(loc).language() == "qtz")
         {
-            OString sKeyId(genKeyId(OString(pContextAndId).replace('\004', '|')));
-            return OUString::fromUtf8(sKeyId) + u"\u2016" + createFromUtf8(pId, strlen(pId));
+            OString sKeyId(genKeyId(OString(sContextAndId).replace('\004', '|')));
+            return OUString::fromUtf8(sKeyId) + u"\u2016" + createFromUtf8(sId.data(), sId.size());
         }
 
         //otherwise translate it
-        const std::string ret = boost::locale::pgettext(sContext.getStr(), pId, loc);
+        const std::string ret = boost::locale::pgettext(sContext.data(), sId.data(), loc);
         OUString result(ExpandVariables(createFromUtf8(ret.data(), ret.size())));
 
         if (comphelper::LibreOfficeKit::isActive())
@@ -231,9 +232,9 @@ namespace Translate
         return result;
     }
 
-    OUString nget(const char* pContextAndIds, int n, const std::locale &loc)
+    OUString nget(std::string_view aContextAndIds, int n, const std::locale &loc)
     {
-        OString sContextIdId(pContextAndIds);
+        OString sContextIdId(aContextAndIds);
         std::vector<OString> aContextIdId;
         sal_Int32 nIndex = 0;
         do

commit 8e7c88ab77e7624a3e4031776f615580d9fa01a3

tdf#143409: fix Translation is not applied in some UI
regression from
    commit 0771ac00acc8730f77db76b901724f1513a32723
    Author: Noel Grandin <noel@peralex.com>
    Date:   Tue Jun 15 21:12:25 2021 +0200
    use string_view in the Translate API
Because I mixed string_view with C-style null-terminated
strings.

We are using string_view.data() to pass stuff to boost,
but of course, doing that loses the fact that the string data
has associated length, as it finds the length by looking for
a terminating NULL.

Change-Id: Ia0eb7be5621506dfe0a40a21f360da1b446f5591

unotools/source/i18n/resmgr.cxx
@@ -199,27 +199,37 @@ namespace Translate
 
     OUString get(std::string_view sContextAndId, const std::locale &loc)
     {
-        std::string_view sContext;
-        std::string_view sId;
-        const char *p = strchr(sContextAndId.data(), '\004');
-        if (!p)
-            sId = sContextAndId;
+        // this function is performance-sensitive, so we allocate string data on stack
+        std::array<char, 128> sContextBuffer;
+        std::array<char, 128> sIdBuffer;
+        const char* pContext;
+        const char* pId;
+        auto p = sContextAndId.find('\004');
+        if (p == std::string_view::npos)
+        {
+            sContextBuffer[0] = 0;
+            pContext = sContextBuffer.data();
+            strncpy(sIdBuffer.data(), sContextAndId.data(), sContextAndId.size());
+            pId = sIdBuffer.data();
+        }
         else
         {
-            sContext = std::string_view(sContextAndId.data(), p - sContextAndId.data());
-            sId = sContextAndId.substr(p - sContextAndId.data() + 1);
-            assert(!strchr(sId.data(), '\004') && "should be using nget, not get");
+            strncpy(sContextBuffer.data(), sContextAndId.data(), p);
+            pContext = sContextBuffer.data();
+            strncpy(sIdBuffer.data(), sContextAndId.data() + p + 1, sContextAndId.size() - p - 1);
+            pId = sIdBuffer.data();
+            assert(!strchr(pId, '\004') && "should be using nget, not get");
         }
 
         //if it's a key id locale, generate it here
         if (std::use_facet<boost::locale::info>(loc).language() == "qtz")
         {
             OString sKeyId(genKeyId(OString(sContextAndId).replace('\004', '|')));
-            return OUString::fromUtf8(sKeyId) + u"\u2016" + createFromUtf8(sId.data(), sId.size());
+            return OUString::fromUtf8(sKeyId) + u"\u2016" + createFromUtf8(pId, strlen(pId));
         }
 
         //otherwise translate it
-        const std::string ret = boost::locale::pgettext(sContext.data(), sId.data(), loc);
+        const std::string ret = boost::locale::pgettext(pContext, pId, loc);
         OUString result(ExpandVariables(createFromUtf8(ret.data(), ret.size())));
 
         if (comphelper::LibreOfficeKit::isActive())