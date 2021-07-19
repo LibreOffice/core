17667cb10b0de878be666a1a746b8956be11733b
Jeff Huang <jeff@mail.ossii.com.tw>
2021-07-19 17:00:17 +0800

    tdf#140107 Remove all collect usage data code.
    Remove all collect usage data code because we
    don't really use them.
    
    Change-Id: I8d83d57220bed8f8c15bd012584943cd0913500d

--- cui/source/options/optgdlg.hxx
+++ cui/source/options/optgdlg.hxx
@@ -44,9 +44,9 @@ private:
     std::unique_ptr<weld::Widget> m_xYearFrame;
     std::unique_ptr<weld::SpinButton> m_xYearValueField;
     std::unique_ptr<weld::Label> m_xToYearFT;
-    std::unique_ptr<weld::CheckButton> m_xCollectUsageInfo;
     std::unique_ptr<weld::CheckButton> m_xCrashReport;
     std::unique_ptr<weld::Widget> m_xQuickStarterFrame;
+    std::unique_ptr<weld::Label> m_xHelpImproveLabel;
     std::unique_ptr<weld::CheckButton> m_xQuickLaunchCB;
 #if defined(_WIN32)
     std::unique_ptr<weld::Widget> m_xFileAssocFrame;
