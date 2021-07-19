LibreOffice/core

commit d72e0cadceb0b43928a9b4f18d75c9d5d30afdda

Watermark: tdf#91687 correct size in the .doc
Export:
* Watermarks saved using Writer were very small in the MSO.
  Export fUsegtextFStretch property in the Geometry Text
  Boolean Properties.
* tdf#91687: SnapRect contains size of Watermark after rotation.
  We have to export size without rotation.

Import:
* When import set height depending on used font and width.
  Text will keep the ratio. Remember the padding for export.

* added unit test
* introduced enum to avoid magic numbers for stretch and best fit
  properties.

Change-Id: I3427afe78488d499f13c543ca401c096161aaf34
Reviewed-on: https://gerrit.libreoffice.org/38979
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Andras Timar <andras.timar@collabora.com>

sw/qa/extras/ww8export/ww8export2.cxx
@@ -133,6 +133,16 @@ DECLARE_WW8EXPORT_TEST(testTdf108072, "tdf108072.doc")
     CPPUNIT_ASSERT_EQUAL(true, getProperty<bool>(xTableRows->getByIndex(0), "IsSplitAllowed"));
 }
 
+DECLARE_WW8EXPORT_TEST(testTdf91687, "tdf91687.doc")
+{
+    // Exported Watermarks were resized
+    uno::Reference<drawing::XShape> xWatermark = getShape(1);
+    uno::Reference<beans::XPropertySet> xWatermarkProperties(xWatermark, uno::UNO_QUERY);
+
+    CPPUNIT_ASSERT_EQUAL((sal_Int32)5172, xWatermark->getSize().Height);
+    CPPUNIT_ASSERT_EQUAL((sal_Int32)18105, xWatermark->getSize().Width);
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

filter/source/msfilter/escherex.cxx
@@ -3482,6 +3482,10 @@ void EscherPropertyContainer::CreateCustomShapeProperties( const MSO_SPT eShapeT
                             if ( pOutlinerParaObject && pOutlinerParaObject->IsVertical() )
                                 nTextPathFlags |= 0x2000;
                         }
+
+                        // Use gtextFStretch for Watermark like MSO does
+                        nTextPathFlags |= use_gtextFStretch | gtextFStretch;
+
                         if ( nTextPathFlags != nTextPathFlagsOrg )
                             AddOpt( DFF_Prop_gtextFStrikethrough, nTextPathFlags );
                     }

filter/source/msfilter/msdffimp.cxx
@@ -4334,7 +4334,8 @@ SdrObject* SvxMSDffManager::ImportShape( const DffRecordHeader& rHd, SvStream& r
                     pRet = new SdrObjCustomShape();
                     pRet->SetModel( pSdrModel );
 
-                    bool bIsFontwork = ( GetPropertyValue( DFF_Prop_gtextFStrikethrough, 0 ) & 0x4000 ) != 0;
+                    sal_uInt32 ngtextFStrikethrough = GetPropertyValue( DFF_Prop_gtextFStrikethrough, 0 );
+                    bool bIsFontwork = ( ngtextFStrikethrough & 0x4000 ) != 0;
 
                     // in case of a FontWork, the text is set by the escher import
                     if ( bIsFontwork )
@@ -4359,16 +4360,16 @@ SdrObject* SvxMSDffManager::ImportShape( const DffRecordHeader& rHd, SvStream& r
 
                         // SJ: applying fontattributes for Fontwork :
                         if ( IsHardAttribute( DFF_Prop_gtextFItalic ) )
-                            aSet.Put( SvxPostureItem( ( GetPropertyValue( DFF_Prop_gtextFStrikethrough, 0 ) & 0x0010 ) != 0 ? ITALIC_NORMAL : ITALIC_NONE, EE_CHAR_ITALIC ) );
+                            aSet.Put( SvxPostureItem( ( ngtextFStrikethrough & 0x0010 ) != 0 ? ITALIC_NORMAL : ITALIC_NONE, EE_CHAR_ITALIC ) );
 
                         if ( IsHardAttribute( DFF_Prop_gtextFBold ) )
-                            aSet.Put( SvxWeightItem( ( GetPropertyValue( DFF_Prop_gtextFStrikethrough, 0 ) & 0x0020 ) != 0 ? WEIGHT_BOLD : WEIGHT_NORMAL, EE_CHAR_WEIGHT ) );
+                            aSet.Put( SvxWeightItem( ( ngtextFStrikethrough & 0x0020 ) != 0 ? WEIGHT_BOLD : WEIGHT_NORMAL, EE_CHAR_WEIGHT ) );
 
                         // SJ TODO: Vertical Writing is not correct, instead
                         // this should be replaced through "CharacterRotation"
                         // by 90 degrees, therefore a new Item has to be
                         // supported by svx core, api and xml file format
-                        static_cast<SdrObjCustomShape*>(pRet)->SetVerticalWriting( ( GetPropertyValue( DFF_Prop_gtextFStrikethrough, 0 ) & 0x2000 ) != 0 );
+                        static_cast<SdrObjCustomShape*>(pRet)->SetVerticalWriting( ( ngtextFStrikethrough & 0x2000 ) != 0 );
 
                         if ( SeekToContent( DFF_Prop_gtextUNICODE, rSt ) )
                         {
@@ -4403,12 +4404,35 @@ SdrObject* SvxMSDffManager::ImportShape( const DffRecordHeader& rHd, SvStream& r
                             if ( nTextWidth != 100 )
                                 aSet.Put( SvxCharScaleWidthItem( (sal_uInt16)nTextWidth, EE_CHAR_FONTWIDTH ) );
                         }
-                        if ( GetPropertyValue( DFF_Prop_gtextFStrikethrough, 0 ) & 0x1000 ) // SJ: Font Kerning On ?
+                        if ( ngtextFStrikethrough & 0x1000 ) // SJ: Font Kerning On ?
                             aSet.Put( SvxKerningItem( 1, EE_CHAR_KERNING ) );
 
                         // #i119496# the resize autoshape to fit text attr of word art in MS PPT is always false
                         aSet.Put(makeSdrTextAutoGrowHeightItem(false));
                         aSet.Put(makeSdrTextAutoGrowWidthItem(false));
+
+                        double fRatio = 0;
+                        OutputDevice* pOut = Application::GetDefaultDevice();
+                        vcl::Font aFont( pOut->GetFont() );
+                        aFont.SetFamilyName( aFontName );
+                        auto nTextWidth = pOut->GetTextWidth( aObjectText );
+
+                        if ( nTextWidth )
+                        {
+                            fRatio = aFont.GetFontSize().Height();
+                            fRatio /= nTextWidth;
+                            sal_Int32 nNewHeight = fRatio * aObjData.aBoundRect.getWidth();
+                            sal_Int32 nPaddingY = aObjData.aBoundRect.getHeight() - nNewHeight;
+
+                            if ( nPaddingY > 0 )
+                            {
+                                // Remember that value because original size have to be exported
+                                aSet.Put( SdrMetricItem( SDRATTR_TEXT_UPPERDIST, nPaddingY ) );
+                                aObjData.aBoundRect.setHeight( nNewHeight );
+                            }
+                            else
+                                aSet.Put( SdrMetricItem( SDRATTR_TEXT_UPPERDIST, 0 ) );
+                        }
                     }
                     pRet->SetMergedItemSet( aSet );
 

include/svx/msdffdef.hxx
@@ -846,6 +846,13 @@ enum MSO_SYSCOLORINDEX {
     mso_colorBParamShift = 16           // To extract the parameter value
 };
 
+enum MSO_TextGeometryProperties {
+    use_gtextFBestFit   = 0x00000100,
+    use_gtextFStretch   = 0x00000400,
+    gtextFBestFit       = 0x01000000,
+    gtextFStretch       = 0x04000000
+};
+
 //ALT_TXT_MSINTEROP
 #define MSPROP_DESCRIPTION_MAX_LEN  4096
 

sw/source/filter/ww8/wrtw8esh.cxx
@@ -756,7 +756,12 @@ void PlcDrawObj::WritePlc( WW8Export& rWrt ) const
                 OSL_ENSURE(pObj, "Where is the SDR-Object?");
                 if (pObj)
                 {
-                    aRect = pObj->GetSnapRect();
+                    aRect = pObj->GetLogicRect();
+
+                    // We have to export original size with padding
+                    const SfxItemSet& rSet = pObj->GetMergedItemSet();
+                    const SdrMetricItem* pItem = static_cast<const SdrMetricItem*>(rSet.GetItem(SDRATTR_TEXT_UPPERDIST));
+                    aRect.SetSize(Size(aRect.GetWidth(), aRect.GetHeight() + pItem->GetValue()));
                 }
             }
 

commit c077eabc3fe8f17ce44af492ce828ee71f0800bd

tdf#123321 Fix DOC/DOCX export bug which made some lines dissappear
* Fix the wrong calculation of SdrPathObj (line) extent while exporting
  to .doc and .docx formats. This resolves tdf#123321 which cause some
  lines to disappear after save and reload. This fix does not break
  tdf#91687
* added tests to make sure this does not happen again, based on the
  .doc file provided in BugZilla
* Loading the bad exported file in MSWord is OK, but it does not work
  in LibreOffice, and some of the lines will be hidden. This needs
  additional work.

The regression came from d72e0cadceb0b43928a9b4f18d75c9d5d30afdda

Change-Id: Ic5a7af4d29df741204c50590728542e674ee9f91

include/svx/svdopath.hxx
@@ -87,6 +87,7 @@ public:
     virtual sal_uInt32 GetHdlCount() const override;
     virtual void AddToHdlList(SdrHdlList& rHdlList) const override;
     virtual void AddToPlusHdlList(SdrHdlList& rHdlList, SdrHdl& rHdl) const override;
+    virtual const tools::Rectangle& GetLogicRect() const override;
 
     // special drag methods
     virtual bool hasSpecialDrag() const override;

svx/source/svdraw/svdopath.cxx
@@ -2059,6 +2059,19 @@ void SdrPathObj::AddToPlusHdlList(SdrHdlList& rHdlList, SdrHdl& rHdl) const
     }
 }
 
+// tdf#123321: Make sure that SdrPathObj (e.g. line) has big enough extent for
+// visibility. This is realised by ensuring GetLogicRect() is the same as
+// GetSnapRect() for the SdrPathObj. Other SdrTextObj objects like
+// SdrObjCustomShape will still use a different version of this method that
+// does not consider the rotation. Otherwise, the rotated SdrObjCustomShape
+// would become mistakenly larger after save and reload (tdf#91687).
+// The invokation of the GetLogicRect() method that caused tdf#123321 was in
+// PlcDrawObj::WritePlc().
+const tools::Rectangle &SdrPathObj::GetLogicRect() const
+{
+    return GetSnapRect();
+}
+
 // dragging
 
 bool SdrPathObj::hasSpecialDrag() const

sw/qa/extras/ww8export/ww8export2.cxx
@@ -414,6 +414,32 @@ DECLARE_WW8EXPORT_TEST(testTdf108072, "tdf108072.doc")
     CPPUNIT_ASSERT_EQUAL(true, getProperty<bool>(xTableRows->getByIndex(0), "IsSplitAllowed"));
 }
 
+DECLARE_WW8EXPORT_TEST(testTdf123321, "shapes-line-ellipse.doc")
+{
+    // These are the 3 lines in which some of them were disappearing before
+    uno::Reference<drawing::XShape> l1 = getShape(7);
+    uno::Reference<drawing::XShape> l2 = getShape(8);
+    uno::Reference<drawing::XShape> l3 = getShape(9);
+
+    // first line (smallest)
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(423), l1->getSize().Height);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(0), l1->getSize().Width);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(7908), l1->getPosition().X);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(37), l1->getPosition().Y);
+
+    // second line (larger)
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(2542), l2->getSize().Height);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(2), l2->getSize().Width);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(7916), l2->getPosition().X);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(289), l2->getPosition().Y);
+
+    // third line (largest)
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(7027), l3->getSize().Height);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(0), l3->getSize().Width);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(7911), l3->getPosition().X);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(231), l3->getPosition().Y);
+}
+
 DECLARE_WW8EXPORT_TEST(testTdf91687, "tdf91687.doc")
 {
     // Exported Watermarks were resized