8d85b8c9670e34c0fa3f76272594a3267ff8da77
Julien Nabet <serval2412@yahoo.fr>
2021-07-21 22:36:54 +0200

    tdf#143464: fix SmartTag management
    by putting back missing information in lcl_FillRecognizerData
    
    Regression from e98711c14db9348f4d3f7d0f5bbde9276a3e73bc
    loplugin:unusedfields in sw (2018-12-12)
    
    Change-Id: I2e99df1a712915851c30c018a1f7279756da72de
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/119346
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
    (cherry picked from commit 717ec99667f5a9ab570f1c8581e2d7a0241c32f6)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/119263
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
    (cherry picked from commit 6d799778945e5481a50ffdbb908a188f6ec77aae)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/119264

--- sw/source/core/crsr/crsrsh.cxx
+++ sw/source/core/crsr/crsrsh.cxx
@@ -3670,6 +3670,7 @@ static void lcl_FillRecognizerData( std::vector< OUString >& rSmartTagTypes,
             if ( pArea )
             {
                 rSmartTagTypes.push_back( pArea->maType );
+                aStringKeyMaps.push_back( pArea->mxPropertyBag );
             }
         }
     }
