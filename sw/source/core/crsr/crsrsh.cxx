LibreOffice/core

commit e98711c14db9348f4d3f7d0f5bbde9276a3e73bc

loplugin:unusedfields in sw
Change-Id: I523146ae16ed87ef042b9d19217d70ae02e411d3
Reviewed-on: https://gerrit.libreoffice.org/65051
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sw/source/core/crsr/crsrsh.cxx
@@ -3542,7 +3542,6 @@ static void lcl_FillRecognizerData( std::vector< OUString >& rSmartTagTypes,
             if ( pArea )
             {
                 rSmartTagTypes.push_back( pArea->maType );
-                aStringKeyMaps.push_back( pArea->mxPropertyBag );
             }
         }
     }

sw/source/core/inc/wrong.hxx
@@ -65,7 +65,6 @@ class SwWrongArea
 {
 public:
     OUString maType;
-    css::uno::Reference< css::container::XStringKeyMap > mxPropertyBag;
     sal_Int32 mnPos;
     sal_Int32 mnLen;
     SwWrongList* mpSubList;

sw/source/core/text/wrong.cxx
@@ -29,7 +29,7 @@ SwWrongArea::SwWrongArea( const OUString& rType, WrongListType listType,
         css::uno::Reference< css::container::XStringKeyMap > const & xPropertyBag,
         sal_Int32 nPos,
         sal_Int32 nLen)
-: maType(rType), mxPropertyBag(xPropertyBag), mnPos(nPos), mnLen(nLen), mpSubList(nullptr)
+: maType(rType), mnPos(nPos), mnLen(nLen), mpSubList(nullptr)
 {
     mColor =  getWrongAreaColor(listType, xPropertyBag);
     mLineType = getWrongAreaLineType(listType, xPropertyBag);
@@ -40,7 +40,7 @@ SwWrongArea::SwWrongArea( const OUString& rType,
         sal_Int32 nPos,
         sal_Int32 nLen,
         SwWrongList* pSubList)
-: maType(rType), mxPropertyBag(xPropertyBag), mnPos(nPos), mnLen(nLen), mpSubList(pSubList), mLineType(WRONGAREA_NONE)
+: maType(rType), mnPos(nPos), mnLen(nLen), mpSubList(pSubList), mLineType(WRONGAREA_NONE)
 {
     if (pSubList != nullptr)
     {

sw/source/core/doc/docbm.cxx
@@ -453,7 +453,6 @@ namespace sw { namespace mark
             pMarkBase->SetName( getUniqueMarkName( pMarkBase->GetName() ) );
 
         // register mark
-        m_aMarkNamesSet.insert(pMarkBase->GetName());
         lcl_InsertMarkSorted(m_vAllMarks, pMark);
         switch(eType)
         {
@@ -575,8 +574,6 @@ namespace sw { namespace mark
         if (::sw::mark::MarkBase* pMarkBase = dynamic_cast< ::sw::mark::MarkBase* >(io_pMark))
         {
             const OUString sOldName(pMarkBase->GetName());
-            m_aMarkNamesSet.erase(sOldName);
-            m_aMarkNamesSet.insert(rNewName);
             pMarkBase->SetName(rNewName);
 
             if (dynamic_cast< ::sw::mark::Bookmark* >(io_pMark))
@@ -962,7 +959,6 @@ namespace sw { namespace mark
         //vector, and on xHoldPastErase release findMark won't find
         //it anymore.
         pMark_t xHoldPastErase = *aI;
-        m_aMarkNamesSet.erase(ppMark->get()->GetName());
         m_vAllMarks.erase(aI);
         return ret;
     }
@@ -994,7 +990,6 @@ namespace sw { namespace mark
     {
         m_vFieldmarks.clear();
         m_vBookmarks.clear();
-        m_aMarkNamesSet.clear();
 
         m_vAnnotationMarks.clear();
 

sw/source/core/inc/MarkManager.hxx
@@ -116,7 +116,6 @@ namespace sw {
             // additional container for fieldmarks
             container_t m_vFieldmarks;
 
-            std::unordered_set<OUString> m_aMarkNamesSet;
             mutable MarkBasenameMapUniqueOffset_t m_aMarkBasenameMapUniqueOffset;
 
             // container for annotation marks

commit 64bc80893b912f48c1d5d0d2a328f24dfc937ef4

tdf#143464: fix SmartTag management
by putting back missing information in lcl_FillRecognizerData

Regression from e98711c14db9348f4d3f7d0f5bbde9276a3e73bc
loplugin:unusedfields in sw (2018-12-12)

Change-Id: I2e99df1a712915851c30c018a1f7279756da72de

sw/source/core/crsr/crsrsh.cxx
@@ -3710,6 +3710,7 @@ static void lcl_FillRecognizerData( std::vector< OUString >& rSmartTagTypes,
             if ( pArea )
             {
                 rSmartTagTypes.push_back( pArea->maType );
+                aStringKeyMaps.push_back( pArea->mxPropertyBag );
             }
         }
     }

sw/source/core/inc/wrong.hxx
@@ -67,6 +67,7 @@ class SwWrongArea
 {
 public:
     OUString maType;
+    css::uno::Reference< css::container::XStringKeyMap > mxPropertyBag;
     sal_Int32 mnPos;
     sal_Int32 mnLen;
     SwWrongList* mpSubList;

sw/source/core/text/wrong.cxx
@@ -29,7 +29,7 @@ SwWrongArea::SwWrongArea( const OUString& rType, WrongListType listType,
         css::uno::Reference< css::container::XStringKeyMap > const & xPropertyBag,
         sal_Int32 nPos,
         sal_Int32 nLen)
-: maType(rType), mnPos(nPos), mnLen(nLen), mpSubList(nullptr)
+: maType(rType), mxPropertyBag(xPropertyBag), mnPos(nPos), mnLen(nLen), mpSubList(nullptr)
 {
     mColor =  getWrongAreaColor(listType, xPropertyBag);
     mLineType = getWrongAreaLineType(listType, xPropertyBag);
@@ -40,7 +40,7 @@ SwWrongArea::SwWrongArea( const OUString& rType,
         sal_Int32 nPos,
         sal_Int32 nLen,
         SwWrongList* pSubList)
-: maType(rType), mnPos(nPos), mnLen(nLen), mpSubList(pSubList), mLineType(WRONGAREA_NONE)
+: maType(rType), mxPropertyBag(xPropertyBag), mnPos(nPos), mnLen(nLen), mpSubList(pSubList), mLineType(WRONGAREA_NONE)
 {
     if (pSubList != nullptr)
     {