65162bbf7c6a70d67a3975c5d415a75932134e89
Huilin <lynntang12138@gmail.com>
2021-07-20 22:44:50 -0700

    use pragma once instead of include guards
    Change-Id: Ie3181b9b9ded514a6155faa6b26a04e8266fd4ad

---
+++ workdir/CustomTarget/officecfg/registry/officecfg/TypeDetection/Types.hxx
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "sal/config.h"
+
+#include "rtl/ustring.h"
+#include "rtl/ustring.hxx"
+#include "comphelper/configuration.hxx"
+
+namespace officecfg { namespace TypeDetection { namespace Types {
+
+struct Types: public comphelper::ConfigurationSet< Types> {
+    static OUString path() { return OUString("/org.openoffice.TypeDetection.Types/Types"); }
+private:
+    Types(); // not defined
+    ~Types(); // not defined
+};
+
+} } }
+
+
