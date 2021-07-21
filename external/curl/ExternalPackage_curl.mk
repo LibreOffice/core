fbdd241e64bbd651a7d0bbcac6a9f63403795ed3
Michael Stahl <michael.stahl@allotropia.de>
2021-07-21 11:57:51 +0200

    curl: upgrade to release 7.78.0
    * Fixes CVE-2020-8284 CVE-2021-22924
    * Also fixes these which don't look relevant to LO:
      CVE-2020-8231
      CVE-2020-8285 CVE-2020-8286
      CVE-2021-22876 CVE-2021-22890
      CVE-2021-22897 CVE-2021-22898 CVE-2021-22901
      CVE-2021-22922 CVE-2021-22923 CVE-2021-22925 CVE-2021-22926
    * disable some new protocols and dependencies
    * remove curl-ios.patch.1 as the code no longer exists upstream
    
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/119313
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
    (cherry picked from commit 946f457c885bd10ff1a7281c351f3981f035f5a7)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/119262
    Reviewed-by: Thorsten Behrens <thorsten.behrens@allotropia.de>
    (cherry picked from commit 020eb3b363a5c9444c97075a2e15b63ccbe7bf2d)
    
    Change-Id: I12d5f87f4d503a5f9859226a05cfe2a07e46d993
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/119423
    Tested-by: Thorsten Behrens <thorsten.behrens@allotropia.de>
    Reviewed-by: Thorsten Behrens <thorsten.behrens@allotropia.de>

--- external/curl/ExternalPackage_curl.mk
+++ external/curl/ExternalPackage_curl.mk
@@ -14,13 +14,13 @@ $(eval $(call gb_ExternalPackage_use_external_project,curl,curl))
 ifneq ($(DISABLE_DYNLOADING),TRUE)
 
 ifeq ($(COM),MSC)
-$(eval $(call gb_ExternalPackage_add_file,curl,$(LIBO_LIB_FOLDER)/libcurl$(if $(MSVC_USE_DEBUG_RUNTIME),_debug).dll,builds/libcurl-vc12-$(if $(filter X86_64,$(CPUNAME)),x64,x86)-$(if $(MSVC_USE_DEBUG_RUNTIME),debug,release)-dll-ipv6-sspi-winssl/bin/libcurl$(if $(MSVC_USE_DEBUG_RUNTIME),_debug).dll))
+$(eval $(call gb_ExternalPackage_add_file,curl,$(LIBO_LIB_FOLDER)/libcurl$(if $(MSVC_USE_DEBUG_RUNTIME),_debug).dll,builds/libcurl-vc12-$(if $(filter X86_64,$(CPUNAME)),x64,x86)-$(if $(MSVC_USE_DEBUG_RUNTIME),debug,release)-dll-ipv6-sspi-schannel/bin/libcurl$(if $(MSVC_USE_DEBUG_RUNTIME),_debug).dll))
 else ifeq ($(OS),MACOSX)
 $(eval $(call gb_ExternalPackage_add_file,curl,$(LIBO_LIB_FOLDER)/libcurl.4.dylib,lib/.libs/libcurl.4.dylib))
 else ifeq ($(OS),AIX)
 $(eval $(call gb_ExternalPackage_add_file,curl,$(LIBO_LIB_FOLDER)/libcurl.so,lib/.libs/libcurl.so.4))
 else
-$(eval $(call gb_ExternalPackage_add_file,curl,$(LIBO_LIB_FOLDER)/libcurl.so.4,lib/.libs/libcurl.so.4.6.0))
+$(eval $(call gb_ExternalPackage_add_file,curl,$(LIBO_LIB_FOLDER)/libcurl.so.4,lib/.libs/libcurl.so.4.7.0))
 endif
 
 endif # $(DISABLE_DYNLOADING)
