LibreOffice/core

commit dd5bf12193471f064bf7f581dd1b21783390e735

editengine-columns: add sidebar property panel
Change-Id: I90aefc10f9ddddeb64a65799480777bc4287abae
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/117107
Tested-by: Jenkins
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

sc/source/ui/drawfunc/drawsh2.cxx
@@ -389,7 +389,20 @@ void ScDrawShell::GetDrawAttrState( SfxItemSet& rSet )
 
     if( bHasMarked )
     {
-        rSet.Put( pDrView->GetAttrFromMarked(false), false );
+        SfxAllItemSet aSet(pDrView->GetAttrFromMarked(false));
+        if (const SfxPoolItem * pItem;
+            aSet.GetItemState(SDRATTR_TEXTCOLUMNS_NUMBER, false, &pItem) >= SfxItemState::DEFAULT
+            && pItem)
+        {
+            aSet.Put(pItem->CloneSetWhich(SID_ATTR_TEXTCOLUMNS_NUMBER));
+        }
+        if (const SfxPoolItem * pItem;
+            aSet.GetItemState(SDRATTR_TEXTCOLUMNS_SPACING, false, &pItem) >= SfxItemState::DEFAULT
+            && pItem)
+        {
+            aSet.Put(pItem->CloneSetWhich(SID_ATTR_TEXTCOLUMNS_SPACING));
+        }
+        rSet.Put(aSet, false);
     }
     else
     {

sw/source/uibase/shells/drawdlg.cxx
@@ -368,7 +368,24 @@ void SwDrawShell::GetDrawAttrState(SfxItemSet& rSet)
 
         if( !bDisable )
         {
-            pSdrView->GetAttributes( rSet );
+            SfxItemSet aSet(rSet);
+            aSet.MergeRange(SDRATTR_TEXTCOLUMNS_NUMBER, SDRATTR_TEXTCOLUMNS_SPACING);
+            pSdrView->GetAttributes(aSet);
+            if (const SfxPoolItem * pItem;
+                aSet.GetItemState(SDRATTR_TEXTCOLUMNS_NUMBER, false, &pItem)
+                    >= SfxItemState::DEFAULT
+                && pItem)
+            {
+                aSet.Put(pItem->CloneSetWhich(SID_ATTR_TEXTCOLUMNS_NUMBER));
+            }
+            if (const SfxPoolItem * pItem;
+                aSet.GetItemState(SDRATTR_TEXTCOLUMNS_SPACING, false, &pItem)
+                    >= SfxItemState::DEFAULT
+                && pItem)
+            {
+                aSet.Put(pItem->CloneSetWhich(SID_ATTR_TEXTCOLUMNS_SPACING));
+            }
+            rSet.Put(aSet, false);
             if (comphelper::LibreOfficeKit::isActive())
                 lcl_unifyFillTransparencyItems(rSet);
         }

include/svx/svxids.hrc
@@ -438,6 +438,9 @@ class SdrAngleItem;
 
 //#define SID_SIDEBAR                                   ( SID_SVX_START + 336 )   -> sfxsids.hrc
 
+#define SID_ATTR_TEXTCOLUMNS_NUMBER                     ( SID_SVX_START + 337 )
+#define SID_ATTR_TEXTCOLUMNS_SPACING                    ( SID_SVX_START + 338 )
+
 #define SID_SB_CONNECTIONPOOLING                        ( SID_SVX_START + 348 )
 #define SID_SB_DBREGISTEROPTIONS                        ( SID_SVX_START + 349 )
 #define SID_BMPMASK                                     ( SID_SVX_START + 350 )

officecfg/registry/data/org/openoffice/Office/UI/Sidebar.xcu
@@ -1843,6 +1843,39 @@
         </prop>
       </node>
 
+      <node oor:name="TextColumnsPropertyPanel" oor:op="replace">
+        <prop oor:name="Title" oor:type="xs:string">
+          <value xml:lang="en-US">Columns</value>
+        </prop>
+        <prop oor:name="Id" oor:type="xs:string">
+          <value>TextColumnsPropertyPanel</value>
+        </prop>
+        <prop oor:name="DeckId" oor:type="xs:string">
+          <value>PropertyDeck</value>
+        </prop>
+        <prop oor:name="ContextList">
+          <value oor:separator=";">
+            Calc,           Draw,       hidden ;
+            Calc,           Graphic,    hidden ;
+            Calc,           Media,      hidden ;
+            DrawImpress,    Draw,       hidden ;
+            DrawImpress,    Graphic,    hidden ;
+            DrawImpress,    TextObject, hidden ;
+            WriterVariants, Draw,       hidden ;
+            WriterVariants, Graphic,    hidden ;
+          </value>
+        </prop>
+        <prop oor:name="ImplementationURL" oor:type="xs:string">
+          <value>private:resource/toolpanel/SvxPanelFactory/TextColumnsPropertyPanel</value>
+        </prop>
+        <prop oor:name="OrderIndex" oor:type="xs:int">
+          <value>800</value>
+        </prop>
+        <prop oor:name="WantsAWT" oor:type="xs:boolean">
+          <value>false</value>
+        </prop>
+      </node>
+
     </node>
   </node>
 </oor:component-data>

sc/sdi/drawsh.sdi
@@ -231,6 +231,8 @@ interface TableDraw
 
     SID_ATTR_SOFTEDGE_RADIUS        [ ExecMethod = ExecDrawAttr; StateMethod = GetDrawAttrState; ]
     SID_MOVE_SHAPE_HANDLE           [ ExecMethod = ExecDrawAttr; ]
+    SID_ATTR_TEXTCOLUMNS_NUMBER     [ ExecMethod = ExecDrawAttr; StateMethod = GetDrawAttrState; ]
+    SID_ATTR_TEXTCOLUMNS_SPACING    [ ExecMethod = ExecDrawAttr; StateMethod = GetDrawAttrState; ]
 }
 
 

sc/source/ui/drawfunc/drawsh.cxx
@@ -417,6 +417,8 @@ void ScDrawShell::ExecDrawAttr( SfxRequest& rReq )
         case SID_ATTR_GLOW_RADIUS:
         case SID_ATTR_GLOW_TRANSPARENCY:
         case SID_ATTR_SOFTEDGE_RADIUS:
+        case SID_ATTR_TEXTCOLUMNS_NUMBER:
+        case SID_ATTR_TEXTCOLUMNS_SPACING:
             if (const SfxItemSet* pNewArgs = rReq.GetArgs())
                 pView->SetAttrToMarked(*pNewArgs, false);
             rReq.Done();

sd/sdi/_drvwsh.sdi
@@ -2927,4 +2927,15 @@ interface DrawView
     [
         ExecMethod = FuTemporary ;
     ]
+    SID_ATTR_TEXTCOLUMNS_NUMBER
+    [
+        ExecMethod = FuTemporary;
+        StateMethod = GetAttrState;
+    ]
+    SID_ATTR_TEXTCOLUMNS_SPACING
+    [
+        ExecMethod = FuTemporary;
+        StateMethod = GetAttrState;
+    ]
+
 }

sd/source/ui/view/drviews2.cxx
@@ -3538,6 +3538,8 @@ void DrawViewShell::FuTemporary(SfxRequest& rReq)
         case SID_ATTR_GLOW_RADIUS:
         case SID_ATTR_GLOW_TRANSPARENCY:
         case SID_ATTR_SOFTEDGE_RADIUS:
+        case SID_ATTR_TEXTCOLUMNS_NUMBER:
+        case SID_ATTR_TEXTCOLUMNS_SPACING:
             if (const SfxItemSet* pNewArgs = rReq.GetArgs())
                 mpDrawView->SetAttributes(*pNewArgs);
             rReq.Done();

commit 56b0ef57b9f98d19ec335cbe36f3db1655695fc2

Dereferencing uninitialized pointer
SfxItemSet::GetItemState returns SfxItemState::DEFAULT without
resetting the passed pointer.

Regression after commit dd5bf12193471f064bf7f581dd1b21783390e735.

Change-Id: I1e427a05f8e5b375f50d81b51d32e78fca83eb42

sc/source/ui/drawfunc/drawsh2.cxx
@@ -390,13 +390,13 @@ void ScDrawShell::GetDrawAttrState( SfxItemSet& rSet )
     if( bHasMarked )
     {
         SfxAllItemSet aSet(pDrView->GetAttrFromMarked(false));
-        if (const SfxPoolItem * pItem;
+        if (const SfxPoolItem* pItem = nullptr;
             aSet.GetItemState(SDRATTR_TEXTCOLUMNS_NUMBER, false, &pItem) >= SfxItemState::DEFAULT
             && pItem)
         {
             aSet.Put(pItem->CloneSetWhich(SID_ATTR_TEXTCOLUMNS_NUMBER));
         }
-        if (const SfxPoolItem * pItem;
+        if (const SfxPoolItem* pItem = nullptr;
             aSet.GetItemState(SDRATTR_TEXTCOLUMNS_SPACING, false, &pItem) >= SfxItemState::DEFAULT
             && pItem)
         {

sw/source/uibase/shells/drawdlg.cxx
@@ -371,14 +371,14 @@ void SwDrawShell::GetDrawAttrState(SfxItemSet& rSet)
             SfxItemSet aSet(rSet);
             aSet.MergeRange(SDRATTR_TEXTCOLUMNS_NUMBER, SDRATTR_TEXTCOLUMNS_SPACING);
             pSdrView->GetAttributes(aSet);
-            if (const SfxPoolItem * pItem;
+            if (const SfxPoolItem* pItem = nullptr;
                 aSet.GetItemState(SDRATTR_TEXTCOLUMNS_NUMBER, false, &pItem)
                     >= SfxItemState::DEFAULT
                 && pItem)
             {
                 aSet.Put(pItem->CloneSetWhich(SID_ATTR_TEXTCOLUMNS_NUMBER));
             }
-            if (const SfxPoolItem * pItem;
+            if (const SfxPoolItem* pItem = nullptr;
                 aSet.GetItemState(SDRATTR_TEXTCOLUMNS_SPACING, false, &pItem)
                     >= SfxItemState::DEFAULT
                 && pItem)