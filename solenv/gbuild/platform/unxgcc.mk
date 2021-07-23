88b94aafc23348238b2d112682bc55719d1978a3
Jan-Marek Glogowski <glogow@fbihome.de>
2021-07-23 14:17:26 +0200

    WASM create and link the Emscripten FS image
    There are three posibilites to add files to the image:
    - gb_emscripten_fs_image_files to add individual files
    - gb_emscripten_fs_image_autoinstall to add files from AutoInstall
    - gb_emscripten_fs_image_filelists to add files from a .filelist
    
    Change-Id: If6804e282a37a37ffae1d291d2af8430e60b59d1
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/128598
    Tested-by: Jenkins
    Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>

--- solenv/gbuild/platform/unxgcc.mk
+++ solenv/gbuild/platform/unxgcc.mk
@@ -174,7 +174,10 @@ $(if $(filter Library,$(TARGETTYPE)), $(call gb_Helper_abbreviate_dirs,\
         $(WORKDIR)/LinkTarget/$(2).exports,$(1))))
 $(if $(and $(filter CppunitTest Executable,$(TARGETTYPE)),$(filter EMSCRIPTEN,$(OS)),$(filter TRUE,$(ENABLE_QT5))), \
     cp $(QT5_PLATFORMS_SRCDIR)/qtlogo.svg $(QT5_PLATFORMS_SRCDIR)/qtloader.js $(dir $(1)) ; \
-    sed -e 's/@APPNAME@/$(subst $(gb_Executable_EXT),,$(notdir $(1)))/' $(QT5_PLATFORMS_SRCDIR)/wasm_shell.html > $(dir $(1))qt_$(notdir $(1)))
+    sed -e 's/@APPNAME@/$(subst $(gb_Executable_EXT),,$(notdir $(1)))/' $(QT5_PLATFORMS_SRCDIR)/wasm_shell.html > $(dir $(1))qt_$(notdir $(1)) ; \
+    ln -sf $(call gb_CustomTarget_get_workdir,static/emscripten_fs_image)/soffice.data $(dir $(1))/soffice.data ; \
+    ln -sf $(call gb_CustomTarget_get_workdir,static/emscripten_fs_image)/soffice.data.js.metadata $(dir $(1))/soffice.data.js.metadata ; \
+)
 endef
 
 define gb_LinkTarget__command_staticlink
