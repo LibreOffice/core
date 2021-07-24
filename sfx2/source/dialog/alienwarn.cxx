2edd37b0645868fea51d57e30193bcd780e0fa2e
Noel Grandin <noel.grandin@collabora.co.uk>
2021-07-24 09:21:23 +0200

    use officecfg to retrieve WarnAlienFormat
    Change-Id: Id7ca2ffa237cb8fda58af60ee5d296f492c5a47e

--- sfx2/source/dialog/alienwarn.cxx
+++ sfx2/source/dialog/alienwarn.cxx
@@ -19,6 +19,7 @@
 
 #include <unotools/saveopt.hxx>
 #include <alienwarn.hxx>
+#include <officecfg/Office/Common.hxx>
 
 SfxAlienWarningDialog::SfxAlienWarningDialog(weld::Window* pParent,
                                              std::u16string_view _rFormatName,
@@ -55,7 +56,7 @@ SfxAlienWarningDialog::SfxAlienWarningDialog(weld::Window* pParent,
     m_xUseDefaultFormatBtn->set_label(sInfoText);
 
     // load value of "warning on" checkbox from save options
-    m_xWarningOnBox->set_active(SvtSaveOptions().IsWarnAlienFormat());
+    m_xWarningOnBox->set_active(officecfg::Office::Common::Save::Document::WarnAlienFormat::get());
 }
 
 SfxAlienWarningDialog::~SfxAlienWarningDialog()
@@ -65,8 +66,12 @@ SfxAlienWarningDialog::~SfxAlienWarningDialog()
         // save value of "warning off" checkbox, if necessary
         SvtSaveOptions aSaveOpt;
         bool bChecked = m_xWarningOnBox->get_active();
-        if (aSaveOpt.IsWarnAlienFormat() != bChecked)
-            aSaveOpt.SetWarnAlienFormat(bChecked);
+        if (officecfg::Office::Common::Save::Document::WarnAlienFormat::get() != bChecked)
+        {
+            auto xChanges = comphelper::ConfigurationChanges::create();
+            officecfg::Office::Common::Save::Document::WarnAlienFormat::set(bChecked, xChanges);
+            xChanges->commit();
+        }
     }
     catch (...)
     {
