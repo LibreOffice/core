c82634d5a222120b3a95cff1b950a7f7a1f5ded1
Ahmet Hakan Çelik <ahmethcelik@gmail.com>
2021-07-25 12:31:28 +0300

    tdf#64345: EDITING: Selected area looses selection when switching sheet
    Change-Id: Idf59c3968319053431741394e2ad218d7be6463d
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/119478
    Tested-by: Jenkins
    Reviewed-by: Heiko Tietze <heiko.tietze@documentfoundation.org>

--- sc/inc/markdata.hxx
+++ sc/inc/markdata.hxx
@@ -46,6 +46,8 @@ public:
 private:
     MarkedTabsType  maTabMarked;
 
+    std::vector<std::vector<std::pair<SCTAB, ScRange>>>  maSheetsMark;
+
     ScRange         aMarkRange;             // area
     ScRange         aMultiRange;            // maximum area altogether
     ScMultiSel      aMultiSel;              // multi selection
@@ -127,6 +129,8 @@ public:
 
     std::vector<sc::ColRowSpan> GetMarkedRowSpans() const;
     std::vector<sc::ColRowSpan> GetMarkedColSpans() const;
+
+    std::vector<std::vector<std::pair<SCTAB, ScRange>>>& GetSheetsMark() { return maSheetsMark; }
 
     bool        IsColumnMarked( SCCOL nCol ) const;
     bool        IsRowMarked( SCROW nRow ) const;
