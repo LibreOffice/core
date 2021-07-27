c650c8f2882ec8ace49648a115e905e6a17bb2b4
panoskorovesis <panoskorovesis@outlook.com>
2021-07-27 10:40:58 +0300

    Create SvmWriter class
    This class will separate Write functionality from metaact.hxx with
    the use of handlers for each metaact subclass
    
    Change-Id: I7c74ec183f08b62d713457e4e971ea5b0b67cde2

---
+++ vcl/source/filter/svm/SvmWriter.cxx
@@ -0,0 +1,87 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#include <vcl/filter/SvmWriter.hxx>
+#include <vcl/TypeSerializer.hxx>
+
+#include <tools/vcompat.hxx>
+
+SvmWriter::SvmWriter(SvStream& rIStm)
+    : mrStream(rIStm)
+{
+}
+
+SvStream& SvmWriter::Write(GDIMetaFile& rMetaFile)
+{
+    const SvStreamCompressFlags nStmCompressMode = mrStream.GetCompressMode();
+    SvStreamEndian nOldFormat = mrStream.GetEndian();
+
+    mrStream.SetEndian(SvStreamEndian::LITTLE);
+    mrStream.WriteBytes("VCLMTF", 6);
+
+    {
+        VersionCompatWrite aCompat(mrStream, 1);
+
+        mrStream.WriteUInt32(static_cast<sal_uInt32>(nStmCompressMode));
+        TypeSerializer aSerializer(mrStream);
+        aSerializer.writeMapMode(rMetaFile.GetPrefMapMode());
+        aSerializer.writeSize(rMetaFile.GetPrefSize());
+        mrStream.WriteUInt32(rMetaFile.GetActionSize());
+    } // VersionCompatWrite dtor writes stuff into the header
+
+    ImplMetaWriteData aWriteData;
+
+    aWriteData.meActualCharSet = mrStream.GetStreamCharSet();
+
+    MetaAction* pAct = rMetaFile.FirstAction();
+    while (pAct)
+    {
+        MetaActionHandler(pAct, &aWriteData);
+        pAct = rMetaFile.NextAction();
+    }
+
+    mrStream.SetEndian(nOldFormat);
+
+    return mrStream;
+}
+
+void SvmWriter::MetaActionHandler(MetaAction* pAction, ImplMetaWriteData* pData)
+{
+    MetaActionType nType = pAction->GetType();
+
+    switch (nType)
+    {
+        case MetaActionType::NONE:
+        {
+            ActionHandler(pAction);
+        }
+        break;
+
+        /* default case prevents test failure and will be
+        removed once all the handlers are completed */
+        default:
+            pAction->Write(mrStream, pData);
+    }
+}
+
+void SvmWriter::ActionHandler(MetaAction* pAction)
+{
+    mrStream.WriteUInt16(static_cast<sal_uInt16>(pAction->GetType()));
+}
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
