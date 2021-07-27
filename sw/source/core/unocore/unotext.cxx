7ec91bcb7d248c38522375c31f2b6f640b1b84e2
Attila Szűcs <szucs.attila3@nisz.hu>
2021-07-27 11:45:58 +0200

    tdf#143384 DOCX import: fix SAXException at header with table
    Regression from commit d656191ec308d4280b93c7169372e543a255d108
    "tdf#119952 DOCX import: fix negative page margins".
    
    Add SwXHeadFootText::CreateTextCursor(bool bIgnoreTables = false)
    (modeled after SwXBodyText::CreateTextCursor) to create text cursor
    for copying the header/footer also started with table during
    fly frame creation in convertoToTextFrame().
    
    Note: add hidden property PROP_CURSOR_NOT_IGNORE_TABLES_IN_HF
    to use the new feature in domainmapper (followed
    commit af4e5ee0f93c1ff442d08caed5c875f2b2c1fd43
    "tdf#97128 DOCX import: fix frame direction").
    
    Co-authored-by: Tibor Nagy (NISZ)
    
    Change-Id: I96e2cf2dddcecd146c53c12d7fdb44fc4d90fa0d
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/119549
    Tested-by: László Németh <nemeth@numbertext.org>
    Reviewed-by: László Németh <nemeth@numbertext.org>
    (cherry picked from commit c1ad429d925855c1baacbdeca1ef42f4486eb9c2)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/120985
    Tested-by: Jenkins
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- sw/source/core/unocore/unotext.cxx
+++ sw/source/core/unocore/unotext.cxx
@@ -1537,14 +1537,31 @@ SwXText::convertToTextFrame(
     {
         throw  uno::RuntimeException();
     }
+    // tdf#143384 recognize dummy property, that was set to make createTextCursor
+    // to not ignore tables.
+    // It is enough to use this hack only for the range start,
+    // because as far as I know, the range cannot end with table when this property is set.
+    ::sw::TextRangeMode eMode = ::sw::TextRangeMode::RequireTextNode;
+    for (const auto& rCellProperty : rFrameProperties)
+    {
+        if (rCellProperty.Name == "CursorNotIgnoreTables")
+        {
+            bool bAllowNonTextNode = false;
+            rCellProperty.Value >>= bAllowNonTextNode;
+            if (bAllowNonTextNode)
+                eMode = ::sw::TextRangeMode::AllowTableNode;
+            break;
+        }
+    }
     uno::Reference< text::XTextContent > xRet;
     std::optional<SwUnoInternalPaM> pTempStartPam(*GetDoc());
     std::optional<SwUnoInternalPaM> pEndPam(*GetDoc());
-    if (!::sw::XTextRangeToSwPaM(*pTempStartPam, xStart) ||
-        !::sw::XTextRangeToSwPaM(*pEndPam, xEnd))
+    if (!::sw::XTextRangeToSwPaM(*pTempStartPam, xStart, eMode)
+        || !::sw::XTextRangeToSwPaM(*pEndPam, xEnd))
     {
         throw lang::IllegalArgumentException();
     }
+
     auto pStartPam(GetDoc()->CreateUnoCursor(*pTempStartPam->GetPoint()));
     if (pTempStartPam->HasMark())
     {
@@ -2613,8 +2630,7 @@ uno::Any SAL_CALL SwXHeadFootText::queryInterface(const uno::Type& rType)
         : ret;
 }
 
-uno::Reference<text::XTextCursor> SAL_CALL
-SwXHeadFootText::createTextCursor()
+uno::Reference<text::XTextCursor> SwXHeadFootText::CreateTextCursor(const bool bIgnoreTables)
 {
     SolarMutexGuard aGuard;
 
@@ -2632,18 +2648,22 @@ SwXHeadFootText::createTextCursor()
     // after the table - otherwise the cursor would be in the body text!
     SwStartNode const*const pOwnStartNode = rNode.FindSttNodeByType(
             (m_pImpl->m_bIsHeader) ? SwHeaderStartNode : SwFooterStartNode);
-    // is there a table here?
-    SwTableNode* pTableNode = rUnoCursor.GetNode().FindTableNode();
-    SwContentNode* pCont = nullptr;
-    while (pTableNode)
+
+    if (!bIgnoreTables)
     {
-        rUnoCursor.GetPoint()->nNode = *pTableNode->EndOfSectionNode();
-        pCont = GetDoc()->GetNodes().GoNext(&rUnoCursor.GetPoint()->nNode);
-        pTableNode = pCont->FindTableNode();
-    }
-    if (pCont)
-    {
-        rUnoCursor.GetPoint()->nContent.Assign(pCont, 0);
+        // is there a table here?
+        SwTableNode* pTableNode = rUnoCursor.GetNode().FindTableNode();
+        SwContentNode* pCont = nullptr;
+        while (pTableNode)
+        {
+            rUnoCursor.GetPoint()->nNode = *pTableNode->EndOfSectionNode();
+            pCont = GetDoc()->GetNodes().GoNext(&rUnoCursor.GetPoint()->nNode);
+            pTableNode = pCont->FindTableNode();
+        }
+        if (pCont)
+        {
+            rUnoCursor.GetPoint()->nContent.Assign(pCont, 0);
+        }
     }
     SwStartNode const*const pNewStartNode = rUnoCursor.GetNode().FindSttNodeByType(
             (m_pImpl->m_bIsHeader) ? SwHeaderStartNode : SwFooterStartNode);
@@ -2654,6 +2674,12 @@ SwXHeadFootText::createTextCursor()
         throw aExcept;
     }
     return static_cast<text::XWordCursor*>(pXCursor.get());
+}
+
+uno::Reference<text::XTextCursor> SAL_CALL
+SwXHeadFootText::createTextCursor()
+{
+    return CreateTextCursor(false);
 }
 
 uno::Reference<text::XTextCursor> SAL_CALL SwXHeadFootText::createTextCursorByRange(
