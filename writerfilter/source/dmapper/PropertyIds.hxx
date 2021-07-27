9fa9d13e66d0b24b9345dc4232fd5f2c77d6d70c
Attila Szűcs <szucs.attila3@nisz.hu>
2021-07-27 11:45:58 +0200

    tdf#143384 DOCX import: fix SAXException at header with table
    Regression from commit d656191ec308d4280b93c7169372e543a255d108
    "tdf#119952 DOCX import: fix negative page margins".
    
    Add SwXHeadFootText::CreateTextCursor(bool bIgnoreTables = false)
    (modeled after SwXBodyText::CreateTextCursor) to create text cursor
    for copying the header/footer also started with table during
    fly frame creation in convertoToTextFrame().
    
    Note: add hidden property PROP_CURSOR_NOT_IGNORE_TABLES_IN_HF
    to use the new feature in domainmapper (followed
    commit af4e5ee0f93c1ff442d08caed5c875f2b2c1fd43
    "tdf#97128 DOCX import: fix frame direction").
    
    Co-authored-by: Tibor Nagy (NISZ)
    
    Change-Id: I96e2cf2dddcecd146c53c12d7fdb44fc4d90fa0d
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/119549
    Tested-by: László Németh <nemeth@numbertext.org>
    Reviewed-by: László Németh <nemeth@numbertext.org>
    (cherry picked from commit c1ad429d925855c1baacbdeca1ef42f4486eb9c2)

--- writerfilter/source/dmapper/PropertyIds.hxx
+++ writerfilter/source/dmapper/PropertyIds.hxx
@@ -360,6 +360,7 @@ enum PropertyIds
         ,PROP_CELL_FORMULA_CONVERTED
         ,PROP_GUTTER_MARGIN
         ,PROP_RTL_GUTTER
+        ,PROP_CURSOR_NOT_IGNORE_TABLES_IN_HF
     };
 
 //Returns the UNO string equivalent to eId.
