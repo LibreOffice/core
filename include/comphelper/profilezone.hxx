b708a1a04bf4e7bc6bea7388498ec5f89f672846
Gopi Krishna Menon <gopi.menon@collabora.com>
2021-07-29 15:21:13 +0530

    Fix Nesting Level Bug in ProfileZone
    Moves the profile zone global nesting variable into the source from header and makes it threadlocal
    
    Change-Id: I97751f5c532d8e0e36adb7d9d383bd88f752953f

--- include/comphelper/profilezone.hxx
+++ include/comphelper/profilezone.hxx
@@ -22,12 +22,13 @@ namespace comphelper
 {
 class COMPHELPER_DLLPUBLIC ProfileZone : public NamedEvent
 {
-    static int s_nNesting; // level of nested zones.
-
     long long m_nCreateTime;
     int m_nNesting;
 
     void addRecording();
+
+    static void setNestingLevel(int nNestingLevel);
+    static int getNestingLevel();
 
     ProfileZone(const char* sName, const OUString& sArgs)
         : NamedEvent(sName, sArgs)
@@ -37,7 +38,8 @@ class COMPHELPER_DLLPUBLIC ProfileZone : public NamedEvent
         {
             m_nCreateTime = getNow();
 
-            m_nNesting = s_nNesting++;
+            m_nNesting = getNestingLevel();
+            setNestingLevel(getNestingLevel() + 1);
         }
         else
             m_nCreateTime = 0;
@@ -65,9 +67,9 @@ public:
     {
         if (m_nCreateTime > 0)
         {
-            s_nNesting--;
+            setNestingLevel(getNestingLevel() - 1);
 
-            if (m_nNesting != s_nNesting)
+            if (m_nNesting != getNestingLevel())
             {
                 SAL_WARN("comphelper.traceevent", "Incorrect ProfileZone nesting for " << m_sName);
             }
