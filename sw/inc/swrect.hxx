151237558c3da083a45b29a98523366a8d438aeb
Huilin <lynntang12138@gmail.com>
2021-07-28 23:23:05 -0700

    tdf#143148: Use pragma once instead of include guards
    Change-Id: I769206b581dd056d2b8cd2c0b20bab0e48af68cc
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/119578
    Tested-by: Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>
    Reviewed-by: Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>

--- sw/inc/swrect.hxx
+++ sw/inc/swrect.hxx
@@ -16,8 +16,7 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
-#ifndef INCLUDED_SW_INC_SWRECT_HXX
-#define INCLUDED_SW_INC_SWRECT_HXX
+#pragma once
 
 #include <ostream>
 
@@ -337,7 +336,5 @@ inline std::basic_ostream<charT, traits> & operator <<(
     return stream << rectangle.SSize()
                   << "@(" << rectangle.Pos() << ")";
 }
-
-#endif // INCLUDED_SW_INC_SWRECT_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
