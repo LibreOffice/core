LibreOffice/core

commit 7e6fee830116823b9cd8e46d6962df4ea2bc1ea6

Qt5 fix Qt::Popup window handling
I guess to get popup windows somehow working, they were matched
to Qt::ToolTip. Fixing that by removing SalFrameStyleFlags::FLOAT
from the tooltip check, resulted in rather bizarre bahviour. The
Qt::Popup window is modal, as documented, but wouldn't close when
clicked outside, like normal popups. And I neither got any mouse
button or focus events, just some leave or enter. The only way
to interact with anything again, was by clicking somewhere on the
popup where it can't take focus... and it has nothing to do with
the transition setting either.

In the end it turned out that Qt::StrongFocus prevented the mouse
events to happen. Even without it the widget accepts mouse and
keyboard focus, so nothing seems lost by omitting that for the
Qt::Popup case; hopefully...

Change-Id: Icda7bbe87268293a400fe0da551d2804675d4c2d
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/119236
Tested-by: Jenkins
Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>
(cherry picked from commit 63f92f185b78db5a575da58efac3f94a8cb5a5f6)
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/119252
Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>

vcl/qt5/Qt5Frame.cxx
@@ -144,7 +144,7 @@ Qt5Frame::Qt5Frame(Qt5Frame* pParent, SalFrameStyleFlags nStyle, bool bUseCairo)
         else if ((nStyle & SalFrameStyleFlags::FLOAT)
                  && (nStyle & SalFrameStyleFlags::OWNERDRAWDECORATION))
             aWinFlags |= Qt::Tool | Qt::FramelessWindowHint;
-        else if (nStyle & (SalFrameStyleFlags::FLOAT | SalFrameStyleFlags::TOOLTIP))
+        else if (nStyle & SalFrameStyleFlags::TOOLTIP)
             aWinFlags |= Qt::ToolTip;
         else if ((nStyle & SalFrameStyleFlags::FLOAT)
                  && !(nStyle & SalFrameStyleFlags::OWNERDRAWDECORATION))

vcl/qt5/Qt5Widget.cxx
@@ -177,7 +177,14 @@ void Qt5Widget::handleMouseButtonEvent(const Qt5Frame& rFrame, const QMouseEvent
     rFrame.CallCallback(nEventType, &aEvent);
 }
 
-void Qt5Widget::mousePressEvent(QMouseEvent* pEvent) { handleMousePressEvent(m_rFrame, pEvent); }
+void Qt5Widget::mousePressEvent(QMouseEvent* pEvent)
+{
+    if ((windowFlags() & Qt::Popup)
+        && !geometry().translated(geometry().topLeft() * -1).contains(pEvent->pos()))
+        close();
+    else
+        handleMousePressEvent(m_rFrame, pEvent);
+}
 
 void Qt5Widget::mouseReleaseEvent(QMouseEvent* pEvent)
 {
@@ -601,7 +608,8 @@ Qt5Widget::Qt5Widget(Qt5Frame& rFrame, Qt::WindowFlags f)
 {
     create();
     setMouseTracking(true);
-    setFocusPolicy(Qt::StrongFocus);
+    if (!(f & Qt::Popup))
+        setFocusPolicy(Qt::StrongFocus);
 }
 
 static ExtTextInputAttr lcl_MapUndrelineStyle(QTextCharFormat::UnderlineStyle us)

commit 6969bf78ebe570c6c47a3a3b4dab81dd95483861

tdf#143580 Qt5 don't use Qt::Popup for FLOAT wins
Main problem is, that Qt::Popup grabs the focus and therefore
generates a focus-out event for the combobox, which then closes
the just shown popup window. The grab happens inside QWidget
private code and there is no way around it. But instead of
"faking" Qt::Tooltip, this uses Qt::Widget with additional flags.

Regression from commit 7e6fee830116823b9cd8e46d6962df4ea2bc1ea6
("Qt5 fix Qt::Popup window handling").

Change-Id: Ia1f8e33d98f7ec36cf1ebc350886121dfaadd658

vcl/inc/qt5/Qt5Frame.hxx
@@ -210,12 +210,19 @@ public:
     inline bool CallCallback(SalEvent nEvent, const void* pEvent) const;
 
     void setInputLanguage(LanguageType);
+    inline bool isPopup() const;
 };
 
 inline bool Qt5Frame::CallCallback(SalEvent nEvent, const void* pEvent) const
 {
     SolarMutexGuard aGuard;
     return SalFrame::CallCallback(nEvent, pEvent);
+}
+
+inline bool Qt5Frame::isPopup() const
+{
+    return ((m_nStyle & SalFrameStyleFlags::FLOAT)
+            && !(m_nStyle & SalFrameStyleFlags::OWNERDRAWDECORATION));
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

vcl/inc/qt5/Qt5Widget.hxx
@@ -75,6 +75,7 @@ class Qt5Widget : public QWidget
 
     void inputMethodEvent(QInputMethodEvent*) override;
     QVariant inputMethodQuery(Qt::InputMethodQuery) const override;
+    void closePopup();
 
 public:
     Qt5Widget(Qt5Frame& rFrame, Qt::WindowFlags f = Qt::WindowFlags());

vcl/qt5/Qt5Frame.cxx
@@ -146,9 +146,11 @@ Qt5Frame::Qt5Frame(Qt5Frame* pParent, SalFrameStyleFlags nStyle, bool bUseCairo)
             aWinFlags |= Qt::Tool | Qt::FramelessWindowHint;
         else if (nStyle & SalFrameStyleFlags::TOOLTIP)
             aWinFlags |= Qt::ToolTip;
-        else if ((nStyle & SalFrameStyleFlags::FLOAT)
-                 && !(nStyle & SalFrameStyleFlags::OWNERDRAWDECORATION))
-            aWinFlags |= Qt::Popup;
+        // Can't use Qt::Popup, because it grabs the input focus and generates
+        // a focus-out event, reaking the compbo box. This used to map to
+        // Qt::ToolTip, which doesn't feel that correct...
+        else if (isPopup())
+            aWinFlags = Qt::Widget | Qt::FramelessWindowHint | Qt::BypassWindowManagerHint;
         else if (nStyle & SalFrameStyleFlags::TOOLWINDOW)
             aWinFlags |= Qt::Tool;
         // top level windows can't be transient in Qt, so make them dialogs, if they have a parent. At least
@@ -426,7 +428,7 @@ void Qt5Frame::Show(bool bVisible, bool bNoActivate)
     pSalInst->RunInMainThread([this, bVisible, bNoActivate]() {
         asChild()->setVisible(bVisible);
         asChild()->raise();
-        if (!bNoActivate)
+        if (!bNoActivate && !isPopup())
         {
             asChild()->activateWindow();
             asChild()->setFocus();

vcl/qt5/Qt5Widget.cxx
@@ -46,6 +46,7 @@
 #include <cairo.h>
 #include <vcl/commandevent.hxx>
 #include <vcl/event.hxx>
+#include <vcl/toolkit/floatwin.hxx>
 #include <window.h>
 #include <tools/diagnose_ex.h>
 
@@ -179,11 +180,10 @@ void Qt5Widget::handleMouseButtonEvent(const Qt5Frame& rFrame, const QMouseEvent
 
 void Qt5Widget::mousePressEvent(QMouseEvent* pEvent)
 {
-    if ((windowFlags() & Qt::Popup)
-        && !geometry().translated(geometry().topLeft() * -1).contains(pEvent->pos()))
-        close();
-    else
-        handleMousePressEvent(m_rFrame, pEvent);
+    handleMousePressEvent(m_rFrame, pEvent);
+    if (m_rFrame.isPopup()
+        || !geometry().translated(geometry().topLeft() * -1).contains(pEvent->pos()))
+        closePopup();
 }
 
 void Qt5Widget::mouseReleaseEvent(QMouseEvent* pEvent)
@@ -593,10 +593,21 @@ void Qt5Widget::keyReleaseEvent(QKeyEvent* pEvent)
 
 void Qt5Widget::focusInEvent(QFocusEvent*) { m_rFrame.CallCallback(SalEvent::GetFocus, nullptr); }
 
+void Qt5Widget::closePopup()
+{
+    VclPtr<FloatingWindow> pFirstFloat = ImplGetSVData()->mpWinData->mpFirstFloat;
+    if (pFirstFloat && !(pFirstFloat->GetPopupModeFlags() & FloatWinPopupFlags::NoAppFocusClose))
+    {
+        SolarMutexGuard aGuard;
+        pFirstFloat->EndPopupMode(FloatWinPopupEndFlags::Cancel | FloatWinPopupEndFlags::CloseAll);
+    }
+}
+
 void Qt5Widget::focusOutEvent(QFocusEvent*)
 {
     endExtTextInput();
     m_rFrame.CallCallback(SalEvent::LoseFocus, nullptr);
+    closePopup();
 }
 
 Qt5Widget::Qt5Widget(Qt5Frame& rFrame, Qt::WindowFlags f)
@@ -608,8 +619,7 @@ Qt5Widget::Qt5Widget(Qt5Frame& rFrame, Qt::WindowFlags f)
 {
     create();
     setMouseTracking(true);
-    if (!(f & Qt::Popup))
-        setFocusPolicy(Qt::StrongFocus);
+    setFocusPolicy(Qt::StrongFocus);
 }
 
 static ExtTextInputAttr lcl_MapUndrelineStyle(QTextCharFormat::UnderlineStyle us)