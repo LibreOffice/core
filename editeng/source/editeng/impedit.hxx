ff1fdf19c44835071916d20e879c383d1513265e
Mike Kaganski <mike.kaganski@collabora.com>
2021-07-31 09:51:42 +0300

    editengine-columns: number of columns is sal_Int16
    Change-Id: If0e46614a398a1b2fe93eb1bfa25b5670be3511b
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/119725
    Tested-by: Jenkins
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

--- editeng/source/editeng/impedit.hxx
+++ editeng/source/editeng/impedit.hxx
@@ -1137,7 +1137,7 @@ public:
     };
     struct LineAreaInfo
     {
-        sal_Int32 nColumn; // Column number; when overflowing, equal to total number of columns
+        sal_Int16 nColumn; // Column number; when overflowing, equal to total number of columns
         ParaPortion& rPortion; // Current ParaPortion
         sal_Int32 nPortion;
         EditLine* pLine; // Current line, or nullptr for paragraph start
@@ -1156,7 +1156,7 @@ public:
     void IterateLineAreas(const IterateLinesAreasFunc& f, IterFlag eOptions);
 
     tools::Long GetColumnWidth(const Size& rPaperSize) const;
-    Point MoveToNextLine(Point& rMovePos, tools::Long nLineHeight, sal_Int32& nColumn,
+    Point MoveToNextLine(Point& rMovePos, tools::Long nLineHeight, sal_Int16& nColumn,
                          Point aOrigin, tools::Long* pnHeightNeededToNotWrap = nullptr) const;
 
     tools::Long getWidthDirectionAware(const Size& sz) const;
