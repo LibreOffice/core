d96885abd8ffd3a89b13d03d3e735e14e3843ec3
Zeynep Yavuz <zeynepyavuz2001@hotmail.com>
2021-08-02 20:05:38 +0300

    tdf#143148: Use pragma once instead of include guards (Episode 2: Endgame)
    Change-Id: I049bd94eaaa771584d18859903ef75e4319494b3

--- vcl/inc/osx/salinst.h
+++ vcl/inc/osx/salinst.h
@@ -18,8 +18,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_VCL_INC_OSX_SALINST_H
-#define INCLUDED_VCL_INC_OSX_SALINST_H
+#pragma once
 
 #include <sal/config.h>
 
@@ -158,6 +157,5 @@ public:
 CGImageRef CreateCGImage( const Image& );
 NSImage*   CreateNSImage( const Image& );
 
-#endif // INCLUDED_VCL_INC_OSX_SALINST_H
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
