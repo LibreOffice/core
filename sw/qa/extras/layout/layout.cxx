c5abee7aefb088a11fc1618bc1512413e9bde682
Justin Luth <justin_luth@sil.org>
2021-08-02 14:39:27 +0200

    Revert "tdf#104254 sw DOCX import: fix text wrapping in headers"
    This reverts 7.2 commit 798b69087119c01a3b51e0bb3240ef35cfededeb.
    The change affected not only DOCX compatibilityMode 15,
    but also DOC and ODT and older versions of DOCX
    and everything else which should not have been affected.
    
    This reverted change of course affected layout.
    Some bug documents didn't loop after this layout breakage.
    That doesn't mean that those bugs were fixed.
    So just revert the tests:
    
    Revert "tdf#96840: sw_ww8export3: Add unittest"
    This reverts commit 975488594fc88aaba7298448e0ff727ebca7fe85.
    
    Revert "tdf#64997: sw_ww8export3: Add unittest"
    This reverts commit 02af9b8ef1f87bb6bc5fee22ad184681b20f201a.
    
    Change-Id: I54f7b801199bba45d8ca6f82f77e7060c7b3fcf7
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/119887
    Tested-by: Jenkins
    Tested-by: László Németh <nemeth@numbertext.org>
    Reviewed-by: Justin Luth <justin_luth@sil.org>
    Reviewed-by: László Németh <nemeth@numbertext.org>
    (cherry picked from commit d03aaef3dcb5d142955457ae9b8ca4469cfafa38)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/119973
    Tested-by: Justin Luth <justin_luth@sil.org>
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
    (cherry picked from commit ff91ddf0a3992b58112f3ffc3e76735b9d66e514)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/120144
    Reviewed-by: Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
    Tested-by: Michael Stahl <michael.stahl@allotropia.de>

--- sw/qa/extras/layout/layout.cxx
+++ sw/qa/extras/layout/layout.cxx
@@ -3253,24 +3253,6 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf124770)
     assertXPath(pXmlDoc, "/root/page/body/txt[1]/LineBreak", 1);
 }
 
-CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTextWrappingInHeader)
-{
-    SwDoc* pDoc = createSwDoc(DATA_DIRECTORY, "tdf104254.docx");
-
-    std::shared_ptr<GDIMetaFile> xMetaFile = pDoc->GetDocShell()->GetPreviewMetaFile();
-    MetafileXmlDump dumper;
-    xmlDocUniquePtr pXmlDoc = dumpAndParse(dumper, *xMetaFile);
-    CPPUNIT_ASSERT(pXmlDoc);
-    assertXPath(pXmlDoc, "/metafile/push[1]/push[1]/push[1]/textarray", 2);
-
-    // Make sure the header image does not block any of the header text.
-    // Without the accompanying fix in place, this test would have failed with:
-    // - Expected: > 3000
-    // - Actual  : 2009
-    OUString sTextArrayX = getXPath(pXmlDoc, "/metafile/push[1]/push[1]/push[1]/textarray[1]", "x");
-    CPPUNIT_ASSERT(sTextArrayX.toUInt32() > 3000);
-}
-
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testContinuousEndnotesInsertPageAtStart)
 {
     // Create a new document with CONTINUOUS_ENDNOTES enabled.
