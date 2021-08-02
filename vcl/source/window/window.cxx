3a666da00022bd26820b47928c685af36c95956b
Michael Meeks <michael.meeks@collabora.com>
2021-08-02 13:38:31 +0100

    vcl: avoid native widget pieces for disposed BorderWindow.
    Fixes crash around help windows:
    	vcl::Window::IsCompoundControl() const
    		vcl/source/window/window2.cxx:1200
    	vcl::Window::Show(bool, ShowFlags)
    		vcl/source/window/window.cxx:2243 (discriminator 3)
    	vcl::Window::Show(bool, ShowFlags)
    		vcl/source/window/window.cxx:2298
    	HelpTextWindow::ImplShow()
    		vcl/source/app/help.cxx:371
    	Scheduler::ProcessTaskScheduling()
    		vcl/source/app/scheduler.cxx:495
    
    Change-Id: Ia0205813f3e9d306314577d59d6cdd1bdfa0ee71
    Signed-off-by: Michael Meeks <michael.meeks@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/119854
    Tested-by: Jenkins
    (cherry picked from commit ab281c561f7fa7a3512875a22423693dca84316a)

--- vcl/source/window/window.cxx
+++ vcl/source/window/window.cxx
@@ -2272,7 +2272,8 @@ void Window::Show(bool bVisible, ShowFlags nFlags)
         // inherit native widget flag for form controls
         // required here, because frames never show up in the child hierarchy - which should be fixed...
         // eg, the drop down of a combobox which is a system floating window
-        if( mpWindowImpl->mbFrame && GetParent() && GetParent()->IsCompoundControl() &&
+        if( mpWindowImpl->mbFrame && GetParent() && !GetParent()->isDisposed() &&
+            GetParent()->IsCompoundControl() &&
             GetParent()->IsNativeWidgetEnabled() != IsNativeWidgetEnabled() &&
             !(GetStyle() & WB_TOOLTIPWIN) )
         {
@@ -3692,7 +3693,7 @@ void Window::EnableNativeWidget( bool bEnable )
 
 bool Window::IsNativeWidgetEnabled() const
 {
-    return ImplGetWinData()->mbEnableNativeWidget;
+    return mpWindowImpl && ImplGetWinData()->mbEnableNativeWidget;
 }
 
 Reference< css::rendering::XCanvas > WindowOutputDevice::ImplGetCanvas( bool bSpriteCanvas ) const
