2882d0c7dc3403bd9bec24cd7d1dc267565bf0bf
Thorsten Behrens <thorsten.behrens@allotropia.de>
2021-08-03 18:32:16 +0200

    Always provision PATH the cygwin way under Windows
    With PATH essentially serving the role of LD_LIBRARY_PATH under
    Windows, there was the notion that this needs to be provided in
    Windows notation, for win32 gnumake.
    
    That was perhaps once true; currently we're always evaluating PATH
    inside a shell, not the Makefile. So this since a while only worked
    accidentally, due to cygwin transparently converting between DOS and
    UNIX PATH vars. It did break though for corner-cases, e.g.
    SRCDIR!=BUILDDIR, and BUILDDIR e.g. D:\FOO.
    
    With that simplification, also GNUMAKE_WIN_NATIVE can go.
    
    Conflicts:
          solenv/gbuild/platform/com_MSC_defs.mk
    
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/119941
    Tested-by: Jenkins
    Reviewed-by: Thorsten Behrens <thorsten.behrens@allotropia.de>
    (cherry picked from commit da36d655608c3da39fd79d95974e1f7404a27aa0)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/119977
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
    
    Change-Id: Ied5a0443dc70e7dc629c0c0620e6ce911d9a73d0

--- solenv/gbuild/platform/com_MSC_defs.mk
+++ solenv/gbuild/platform/com_MSC_defs.mk
@@ -304,19 +304,6 @@ endif
 
 # Helper class
 
-ifeq ($(GNUMAKE_WIN_NATIVE),TRUE)
-gb_Helper_set_ld_path := PATH="$(shell cygpath -w $(INSTDIR)/$(LIBO_URE_LIB_FOLDER));$(shell cygpath -w $(INSTDIR)/$(LIBO_BIN_FOLDER));$$PATH"
-
-define gb_Helper_prepend_ld_path
-PATH="$(shell cygpath -w $(INSTDIR)/$(LIBO_URE_LIB_FOLDER));$(shell cygpath -w $(INSTDIR)/$(LIBO_BIN_FOLDER));$(1);$$PATH"
-endef
-
-# $(1): one directory pathname to append to the ld path
-define gb_Helper_extend_ld_path
-$(gb_Helper_set_ld_path)';$(shell cygpath -w $(1))'
-endef
-
-else
 gb_Helper_set_ld_path := PATH="$(shell cygpath -u $(INSTDIR)/$(LIBO_URE_LIB_FOLDER)):$(shell cygpath -u $(INSTDIR)/$(LIBO_BIN_FOLDER)):$$PATH"
 
 define gb_Helper_prepend_ld_path
@@ -325,9 +312,7 @@ endef
 
 # $(1): one directory pathname to append to the ld path
 define gb_Helper_extend_ld_path
-$(gb_Helper_set_ld_path):$(shell cygpath -u $(1))
+$(gb_Helper_set_ld_path)':$(shell cygpath -u $(1))'
 endef
-
-endif
 
 # vim: set noet sw=4:
