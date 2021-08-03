967a7125f720863146d047bbb0235985e77ed5a8
oguzbalkaya <oguz.balkaya@gmail.com>
2021-08-04 00:05:26 +0300

    tdf#143148: Use pragma once instead of include guards
    Change-Id: I7882fe4892ce7fb22507db53c8c8615257362afc

--- vcl/inc/osx/printview.h
+++ vcl/inc/osx/printview.h
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_VCL_INC_OSX_PRINTVIEW_H
-#define INCLUDED_VCL_INC_OSX_PRINTVIEW_H
+#pragma once
 
 #include <premac.h>
 #include <Cocoa/Cocoa.h>
@@ -58,6 +57,6 @@ struct PrintAccessoryViewState
                     withState: (PrintAccessoryViewState*)pState;
 @end
 
-#endif // INCLUDED_VCL_INC_OSX_PRINTVIEW_H
+
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
