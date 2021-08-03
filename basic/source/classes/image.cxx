a0ea68a8448915a16ee00d663de80b9c60d8e9b9
Andreas Heinisch <andreas.heinisch@yahoo.de>
2021-08-03 20:56:22 +0200

    tdf#143707 - Don't add a suffix type character for Variants
    If an optional parameter is of type Variant, don't add a suffix type
    character, because it won't be removed during the type conversion to the
    requested type in SbiRuntime::StepPARAM.
    
    Change-Id: I86c8192c6dc28457053fa7b23a073420e45407b2

--- basic/source/classes/image.cxx
+++ basic/source/classes/image.cxx
@@ -646,26 +646,43 @@ void SbiImage::AddEnum(SbxObject* pObject) // Register enum type
 }
 
 // Note: IDs start with 1
-OUString SbiImage::GetString( short nId ) const
+OUString SbiImage::GetString( short nId, SbxDataType *eType ) const
 {
     if( nId && nId <= short(mvStringOffsets.size()) )
     {
         sal_uInt32 nOff = mvStringOffsets[ nId - 1 ];
         sal_Unicode* pStr = pStrings.get() + nOff;
 
+        // ADD AND CHANGE COMMENT
+        // TODO - Change initialization order
         // #i42467: Special treatment for vbNullChar
-        if( *pStr == 0 )
+        sal_uInt32 nNextOff
+            = (nId < short(mvStringOffsets.size())) ? mvStringOffsets[nId] : nStringOff;
+        sal_uInt32 nLen = nNextOff - nOff - 1;
+        if (*pStr == 0 && nLen == 1)
         {
-            sal_uInt32 nNextOff = (nId < short(mvStringOffsets.size())) ? mvStringOffsets[ nId ] : nStringOff;
-            sal_uInt32 nLen = nNextOff - nOff - 1;
-            if( nLen == 1 )
-            {
-                return OUString( u'\0');
-            }
+            return OUString(u'\0');
         }
         else
         {
-            return OUString(pStr);
+            OUString aOUStr(pStr);
+            if (eType != nullptr && size_t(aOUStr.getLength()) < nLen)
+            {
+                sal_Unicode pStr = *(pStrings.get() + nOff + aOUStr.getLength() + 1);
+                switch (pStr)
+                {
+                    // See GetSuffixType in basic/source/comp/scanner.cxx for type characters
+                    case '%': *eType = SbxINTEGER; break;
+                    case '&': *eType = SbxLONG; break;
+                    case '!': *eType = SbxSINGLE; break;
+                    case '#': *eType = SbxDOUBLE; break;
+                    case '@': *eType = SbxCURRENCY; break;
+                    // tdf#142460 - properly handle boolean values in string pool
+                    case 'b': *eType = SbxBOOL; break;
+                    default:  *eType = SbxSTRING;
+                }
+            }
+            return aOUStr;
         }
     }
     return OUString();
