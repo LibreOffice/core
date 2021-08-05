39392ee94c78692a9179f7face15af0c9e74e492
Miklos Vajna <vmiklos@collabora.com>
2021-08-05 13:43:50 +0200

    sw: fix assert fail with ShowRedlineChanges=false and para format
    It fails like this:
    
    	loolforkit: sw/source/core/layout/wsfrm.cxx:4551: void UnHide(SwRootFrame&): Assertion `rLayout.GetCurrShell()->ActionPend()' failed.
    	#4  0x00007f98e9f8c198 in UnHide(SwRootFrame&) (rLayout=...) at sw/source/core/layout/wsfrm.cxx:4551
    	#5  0x00007f98e9f8c839 in SwRootFrame::SetHideRedlines(bool) (this=0x7f98c40f7bd0, bHideRedlines=false) at sw/source/core/layout/wsfrm.cxx:4664
    	#6  0x00007f98e9b18afd in sw::DocumentRedlineManager::SetRedlineFlags(RedlineFlags) (this=0x80a4120, eMode=49) at sw/source/core/doc/DocumentRedlineManager.cxx:1097
    	#7  0x00007f98e9cfa503 in SwEditShell::SetAttrSet(SfxItemSet const&, SetAttrMode, SwPaM*, bool) (this=0x8885840, rSet=
    	    SfxItemSet of pool 0x808fd10 with parent 0x0 and Which ranges: [(63, 64), (120, 120)] = {...}, nFlags=SetAttrMode::DEFAULT, pPaM=0x0, bParagraphSetting=true)
    	    at sw/source/core/edit/edatmisc.cxx:187
    	#8  0x00007f98ea995f07 in SwTextShell::ExecParaAttr(SfxRequest&) (this=0x8bd0820, rReq=...) at sw/source/uibase/shells/txtattr.cxx:451
    
    Fix it the same way commit dd489bc01adc22fc5015ea56b61d66104af184a8
    (tdf#125754 sw_redlinehide: avoid recursive layout in SetHideRedlines(),
    2019-06-19) did: make sure that SetHideRedlines() is called before
    EndAllAction().
    
    Change-Id: I6304abec2e2e2967a8369b0219492bebcd606d03
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/120069
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Tested-by: Jenkins

---
+++ sw/qa/core/edit/edit.cxx
@@ -0,0 +1,43 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <swmodeltestbase.hxx>
+
+#include <docsh.hxx>
+#include <view.hxx>
+#include <wrtsh.hxx>
+
+constexpr OUStringLiteral DATA_DIRECTORY = u"/sw/qa/core/edit/data/";
+
+namespace
+{
+/// Covers sw/source/core/edit/ fixes.
+class Test : public SwModelTestBase
+{
+};
+}
+
+CPPUNIT_TEST_FIXTURE(Test, testRedlineHidden)
+{
+    // Given a document with ShowRedlineChanges=false:
+    SwDoc* pDoc = createSwDoc(DATA_DIRECTORY, "redline-hidden.fodt");
+
+    // When formatting a paragraph by setting the para adjust to center, then make sure setting the
+    // new item set on the paragraph doesn't crash:
+    SwView* pView = pDoc->GetDocShell()->GetView();
+    SfxItemSet aSet(pView->GetPool(), svl::Items<RES_PARATR_ADJUST, RES_PARATR_ADJUST>);
+    SvxAdjustItem aItem(SvxAdjust::Center, RES_PARATR_ADJUST);
+    aSet.Put(aItem);
+    SwWrtShell* pWrtShell = pDoc->GetDocShell()->GetWrtShell();
+    pWrtShell->SetAttrSet(aSet, SetAttrMode::DEFAULT, nullptr, true);
+}
+
+CPPUNIT_PLUGIN_IMPLEMENT();
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
