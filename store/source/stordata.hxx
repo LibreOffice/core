285925f6523b40b0d410d5b44bc1dcce199e6e67
Emircan Agac <thesadson@gmail.com>
2021-08-05 17:25:03 +0300

    tdf#143148:Use pragma once instead of include guards (Episode 2: Endgame)#2
    Change-Id: Ia12dd3d7b69a8fdfbdfa6503e1ce2ee46c9446d4

--- store/source/stordata.hxx
+++ store/source/stordata.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_STORE_SOURCE_STORDATA_HXX
-#define INCLUDED_STORE_SOURCE_STORDATA_HXX
+#pragma once
 
 #include <sal/config.h>
 
@@ -745,7 +744,5 @@ private:
 };
 
 } // namespace store
-
-#endif // INCLUDED_STORE_SOURCE_STORDATA_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
