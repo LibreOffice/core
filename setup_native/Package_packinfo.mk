f267340d300da6318435ed1b2ae48b9f4275e2c4
Stephan Bergmann <sbergman@redhat.com>
2021-08-05 18:18:26 +0200

    Remove unused $(LIBO_SHARE_FOLDER)/extensions/package.txt
    There had originally been some back-and-forth among
    134ae5da1cc6a5dfa4432063f930235c1a872bc0 "jl154 #162868# remove
    basis/presets/bundled/bundled.ini, and create always share/extensions folder",
    4c333e8ac7f6135fb4e62e4960c826b278e8cc63 "jl154 #162868# removing the
    package.txt from the extensions folder. Extensions folder will be created in a
    different way than installing a dummy file", and
    8f4cf9a16eb9936ef1e862ecdec7d30c9e240987 "jl154 #162868# guarantee existence of
    extensions folder" whether or not that dummy file was necessary to ensure
    creation of the (otherwise potentially empty) $(LIBO_SHARE_FOLDER)/extensions/
    directory.  That is no longer necessary with gb_Package_add_empty_directory
    (though the logic that had to be added to setup_native/Package_packinfo.mk now
    is a bit unfortunate, and will need to be updated whenever the list of potential
    bundled extensions changes).
    
    (And that dummy package.txt file is apparently not used to track whether per-
    user extension information needs to be updated when the contents of
    $(LIBO_SHARE_FOLDER)/extensions/ changes.  That is rather done by
    comparing the status of the $(LIBO_SHARE_FOLDER)/extensions/ directory itself
    against user/extensions/bundled/lastsynchronized in the UserInstallation, see
    needToSyncRepository in desktop/source/deployment/misc/dp_misc.cxx.)
    
    Change-Id: I5b9956c508288945f32a88afaef57ac0b63dc27f
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/120081
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

--- setup_native/Package_packinfo.mk
+++ setup_native/Package_packinfo.mk
@@ -7,8 +7,15 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 
-$(eval $(call gb_Package_Package,setup_native_packinfo,$(SRCDIR)/setup_native/source/packinfo))
+$(eval $(call gb_Package_Package,setup_native_packinfo))
 
-$(eval $(call gb_Package_add_file,setup_native_packinfo,$(LIBO_SHARE_FOLDER)/extensions/package.txt,package.txt))
+# Check if any bundled extensions are installed (where ENABLE_JAVA covers Extension_nlpsolver, cf.
+# nlpsolver/Module_nlpsolver.mk); if none are installed, then gb_Package_add_empty_directory is
+# needed to create the empty extensions directory (but if any are installed, then
+# gb_Package_add_empty_directory would cause errors during CustomTarget_instsetoo_native/install, as
+# the directory is not empty in that case):
+ifeq ($(filter CT2N DICTIONARIES LANGUAGETOOL,$(BUILD_TYPE))$(ENABLE_MEDIAWIKI)$(NUMBERTEXT_EXTENSION_PACK)$(ENABLE_JAVA),)
+$(eval $(call gb_Package_add_empty_directory,setup_native_packinfo,$(LIBO_SHARE_FOLDER)/extensions))
+endif
 
 # vim: set noet sw=4 ts=4:
