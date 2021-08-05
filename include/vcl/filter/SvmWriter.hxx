526646cb631bf56fc0fae25499a867399db81b02
panoskorovesis <panoskorovesis@outlook.com>
2021-08-05 10:18:36 +0300

    Add Handler for ISectRectClipRegion Write
    The handler separates MetaISectRectClipRegionAction::Write from metaact.hxx
    Write implementation is now in SvmWriter.hxx
    
    Change-Id: I999fa1baa4e3c34f3f83dc1d3973f961857f89d6
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/120308
    Tested-by: Jenkins
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

--- include/vcl/filter/SvmWriter.hxx
+++ include/vcl/filter/SvmWriter.hxx
@@ -70,6 +70,7 @@ public:
     void HatchHandler(MetaHatchAction* pAction);
     void WallpaperHandler(MetaWallpaperAction* pAction);
     void ClipRegionHandler(MetaClipRegionAction* pAction);
+    void ISectRectClipRegionHandler(MetaISectRectClipRegionAction* pAction);
     void OverlineColorHandler(MetaOverlineColorAction* pAction);
     void TextAlignHandler(MetaTextAlignAction* pAction);
     void MapModeHandler(MetaMapModeAction* pAction);
