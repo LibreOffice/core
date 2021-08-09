7876dce17fed23e85a222ec4fda00fdfbd204847
Noel Grandin <noelgrandin@gmail.com>
2021-08-09 18:56:13 +0200

    flatten Accelerator a little
    Change-Id: I188a9ce69a89958f776f696f1da2f96b82a7f6da

--- vcl/inc/accel.hxx
+++ vcl/inc/accel.hxx
@@ -20,12 +20,14 @@
 #pragma once
 
 #include <config_options.h>
+#include <tools/solar.h>
 #include <tools/link.hxx>
 #include <vcl/keycod.hxx>
 #include <vcl/dllapi.h>
 #include <memory>
+#include <map>
+#include <vector>
 
-class ImplAccelData;
 class CommandEvent;
 
 class Accelerator;
@@ -45,7 +47,10 @@ class Accelerator
     friend class ImplAccelManager;
 
 private:
-    std::unique_ptr<ImplAccelData> mpData;
+    typedef ::std::map< sal_uLong, ImplAccelEntry* > ImplAccelMap;
+    typedef ::std::vector< std::unique_ptr<ImplAccelEntry> > ImplAccelList;
+    ImplAccelMap  maKeyMap; // for keycodes, generated with a code
+    ImplAccelList maIdList; // Id-List
     Link<Accelerator&,void> maActivateHdl;
     Link<Accelerator&,void> maSelectHdl;
 
@@ -54,7 +59,7 @@ private:
     bool*                   mpDel;
 
     void    ImplInit();
-    void    ImplCopyData( ImplAccelData& rAccelData );
+    void    ImplCopyData( const Accelerator& rAccelData );
     void    ImplDeleteData();
     void    ImplInsertAccel(sal_uInt16 nItemId, const vcl::KeyCode& rKeyCode,
                             bool bEnable, Accelerator* pAutoAccel);
