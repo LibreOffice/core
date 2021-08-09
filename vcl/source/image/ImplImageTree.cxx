794ba366be25dc58986b262d8cfd521a803bbd2f
Noel Grandin <noelgrandin@gmail.com>
2021-08-09 19:00:14 +0200

    flatten ImplImageTree a little
    Change-Id: Ibf67c85f4af80e5420b017a1a1f89e3eac8a3118

--- vcl/source/image/ImplImageTree.cxx
+++ vcl/source/image/ImplImageTree.cxx
@@ -517,9 +517,9 @@ ImplImageTree::IconCache &ImplImageTree::getIconCache(const ImageRequestParamete
     IconSet &rSet = getCurrentIconSet();
     auto it = rSet.maScaledIconCaches.find(rParameters.mnScalePercentage);
     if ( it != rSet.maScaledIconCaches.end() )
-        return *it->second;
-    rSet.maScaledIconCaches[rParameters.mnScalePercentage] = std::make_unique<IconCache>();
-    return *rSet.maScaledIconCaches[rParameters.mnScalePercentage];
+        return it->second;
+    rSet.maScaledIconCaches.emplace(rParameters.mnScalePercentage, IconCache());
+    return rSet.maScaledIconCaches[rParameters.mnScalePercentage];
 }
 
 bool ImplImageTree::iconCacheLookup(ImageRequestParameters& rParameters)
