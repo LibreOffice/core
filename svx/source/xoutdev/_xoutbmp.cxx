d40de32d637926bf324d8bcce1fe8715d4534338
Miklos Vajna <vmiklos@collabora.com>
2021-08-09 16:16:35 +0200

    sw HTML export: avoid writing EMF data with JPG extension
    Regression from commit f3b2fc2276ee8a7f64e73d9975d0143d1696362c (sw
    XHTML import, improved <object> handling for images: support more
    formats, 2021-07-20), the problem was that now an EMF image was written
    with the JPG extension in the HTML export.
    
    Solve the problem by making the "use emf as-is" check more strict in
    XOutBitmap::WriteGraphic(): that will be still true in the XHTML case
    when the source image is EMF, but the filter name will be JPG in the
    HTML case, so that will avoid the unwanted EMF data in the HTML case.
    
    Change-Id: I7bced482d677cf121be1ed494a3aaeb319e1c5b4

--- svx/source/xoutdev/_xoutbmp.cxx
+++ svx/source/xoutdev/_xoutbmp.cxx
@@ -132,12 +132,13 @@ ErrCode XOutBitmap::WriteGraphic( const Graphic& rGraphic, OUString& rFileName,
 
     if (rVectorGraphicDataPtr && rVectorGraphicDataPtr->getBinaryDataContainer().getSize())
     {
+        // Does the filter name match the original format?
         const bool bIsSvg(rFilterName.equalsIgnoreAsciiCase("svg") && VectorGraphicDataType::Svg == rVectorGraphicDataPtr->getType());
         const bool bIsWmf(rFilterName.equalsIgnoreAsciiCase("wmf") && VectorGraphicDataType::Wmf == rVectorGraphicDataPtr->getType());
         bool bIsEmf(rFilterName.equalsIgnoreAsciiCase("emf") && VectorGraphicDataType::Emf == rVectorGraphicDataPtr->getType());
         if (!bIsEmf)
         {
-            bIsEmf = rGraphic.GetGfxLink().IsEMF();
+            bIsEmf = rFilterName.equalsIgnoreAsciiCase("emf") && rGraphic.GetGfxLink().IsEMF();
         }
         const bool bIsPdf(rFilterName.equalsIgnoreAsciiCase("pdf") && VectorGraphicDataType::Pdf == rVectorGraphicDataPtr->getType());
 
