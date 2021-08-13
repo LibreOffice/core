LibreOffice/core

commit 429e6e6efbfbc65d36452cbf8b3a25740a1dc8bc

osl::Mutex->std::mutex in OInputStreamHelper
Change-Id: Ia6aa6541474e0e4c190ab805a931d74c8a598baf
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/119723
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

include/unotools/streamhelper.hxx
@@ -23,9 +23,9 @@
 #include <unotools/unotoolsdllapi.h>
 #include <com/sun/star/io/XInputStream.hpp>
 #include <com/sun/star/io/XSeekable.hpp>
-#include <osl/mutex.hxx>
 #include <cppuhelper/implbase.hxx>
 #include <tools/stream.hxx>
+#include <mutex>
 
 namespace utl
 {
@@ -35,7 +35,7 @@ namespace utl
  */
 class UNOTOOLS_DLLPUBLIC OInputStreamHelper final : public cppu::WeakImplHelper<css::io::XInputStream, css::io::XSeekable>
 {
-    ::osl::Mutex    m_aMutex;
+    std::mutex      m_aMutex;
     SvLockBytesRef  m_xLockBytes;
     sal_uInt64      m_nActPos;
     sal_Int32       m_nAvailable;   // this is typically the chunk(buffer) size

unotools/source/streaming/streamhelper.cxx
@@ -36,7 +36,7 @@ sal_Int32 SAL_CALL OInputStreamHelper::readBytes(css::uno::Sequence< sal_Int8 >&
     if (nBytesToRead < 0)
         throw css::io::BufferSizeExceededException(OUString(), static_cast<css::uno::XWeak*>(this));
 
-    ::osl::MutexGuard aGuard( m_aMutex );
+    std::lock_guard aGuard( m_aMutex );
     if (aData.getLength() < nBytesToRead)
         aData.realloc(nBytesToRead);
 
@@ -56,7 +56,7 @@ sal_Int32 SAL_CALL OInputStreamHelper::readBytes(css::uno::Sequence< sal_Int8 >&
 
 void SAL_CALL OInputStreamHelper::seek( sal_Int64 location )
 {
-    ::osl::MutexGuard aGuard( m_aMutex );
+    std::lock_guard aGuard( m_aMutex );
     m_nActPos = location;
 }
 
@@ -70,7 +70,7 @@ sal_Int64 SAL_CALL OInputStreamHelper::getLength(  )
     if (!m_xLockBytes.is())
         return 0;
 
-    ::osl::MutexGuard aGuard( m_aMutex );
+    std::lock_guard aGuard( m_aMutex );
     SvLockBytesStat aStat;
     m_xLockBytes->Stat( &aStat );
     return aStat.nSize;
@@ -85,7 +85,7 @@ sal_Int32 SAL_CALL OInputStreamHelper::readSomeBytes(css::uno::Sequence< sal_Int
 
 void SAL_CALL OInputStreamHelper::skipBytes(sal_Int32 nBytesToSkip)
 {
-    ::osl::MutexGuard aGuard( m_aMutex );
+    std::lock_guard aGuard( m_aMutex );
     if (!m_xLockBytes.is())
         throw css::io::NotConnectedException(OUString(), static_cast<css::uno::XWeak*>(this));
 
@@ -97,7 +97,7 @@ void SAL_CALL OInputStreamHelper::skipBytes(sal_Int32 nBytesToSkip)
 
 sal_Int32 SAL_CALL OInputStreamHelper::available()
 {
-    ::osl::MutexGuard aGuard( m_aMutex );
+    std::lock_guard aGuard( m_aMutex );
     if (!m_xLockBytes.is())
         throw css::io::NotConnectedException(OUString(), static_cast<css::uno::XWeak*>(this));
 
@@ -106,7 +106,7 @@ sal_Int32 SAL_CALL OInputStreamHelper::available()
 
 void SAL_CALL OInputStreamHelper::closeInput()
 {
-    ::osl::MutexGuard aGuard( m_aMutex );
+    std::lock_guard aGuard( m_aMutex );
     if (!m_xLockBytes.is())
         throw css::io::NotConnectedException(OUString(), static_cast<css::uno::XWeak*>(this));
 

commit 72efbf41217ac097d4aef58aea9bbc2e984b9021

Help older linkers (Ubuntu 18.04, Fedora 31)
Fixes a regression from commit 429e6e6efbfbc65d36452cbf8b3a25740a1dc8bc
("osl::Mutex->std::mutex in OInputStreamHelper"), which results in
failed linking with some older toolchains:

/usr/bin/ld.gold: error: workdir/CxxObject/xmlsecurity/source/helper/UriBindingHelper.o:
  requires dynamic R_X86_64_PC32 reloc against '_ZThn40_N3utl18OInputStreamHelper7acquireEv'
  which may overflow at runtime; recompile with -fPIC
include/com/sun/star/uno/Reference.hxx:240: error: undefined reference to
  'non-virtual thunk to utl::OInputStreamHelper::acquire()'
collect2: error: ld returned 1 exit status
xmlsecurity/Library_xmlsecurity.mk:10: recipe for target
  'symbols/instdir/program/libxmlsecurity.so' failed

Known problematic setups are my Ubuntu 18.04 build schroot and
Caolán's Fedora 31 VM to run Coverity.

Change-Id: I40f0734a0cb9131193abf21ba95dc63816b5c1d8

compilerplugins/clang/unnecessaryoverride.cxx
@@ -95,6 +95,8 @@ public:
         // HAVE_ODBC_ADMINISTRATION
         if (loplugin::isSamePathname(fn, SRCDIR "/dbaccess/source/ui/dlg/dsselect.cxx"))
             return false;
+        if (loplugin::isSamePathname(fn, SRCDIR "/unotools/source/streaming/streamhelper.cxx"))
+            return false;
         return true;
     }
 

include/unotools/streamhelper.hxx
@@ -57,6 +57,9 @@ public:
     virtual void SAL_CALL seek( sal_Int64 location ) override;
     virtual sal_Int64 SAL_CALL getPosition(  ) override;
     virtual sal_Int64 SAL_CALL getLength(  ) override;
+
+    // workaround for Ubuntu 18.04 / Fedora 31 (Coverity VM) build with std::mutex; must not be inline!
+    virtual void SAL_CALL acquire() SAL_NOEXCEPT override;
 };
 
 }   // namespace utl

unotools/source/streaming/streamhelper.cxx
@@ -113,6 +113,11 @@ void SAL_CALL OInputStreamHelper::closeInput()
     m_xLockBytes = nullptr;
 }
 
+void SAL_CALL OInputStreamHelper::acquire() SAL_NOEXCEPT
+{
+    cppu::WeakImplHelper<css::io::XInputStream, css::io::XSeekable>::acquire();
+}
+
 } // namespace utl
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */