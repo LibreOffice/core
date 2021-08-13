59f3db1763e1ffb85e7b5d01b051a27a388281e2
Miklos Vajna <vmiklos@collabora.com>
2021-08-13 10:21:49 +0200

    sw bibiliography: limit browse button / page number widgets to the local URL
    The normal URL is typically a HTTP resource, so the browse button is not
    helpful there after all. And if the URL is copied in manually anyway,
    then the dedicated page number widgets are not that helpful, either. So
    remove all this, and only keep it for local URLs only.
    
    Change-Id: I25733810cf38981c50014e06201555d98fd9f77f
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/120420
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Tested-by: Jenkins

--- sw/qa/uitest/ui/index/index.py
+++ sw/qa/uitest/ui/index/index.py
@@ -14,44 +14,6 @@ from uitest.uihelper.common import type_text
 
 class TestSwuiidxmrk(UITestCase):
 
-    def test_bibliography_page_number_insert(self):
-
-        # Given an empty Writer document:
-        with self.ui_test.create_doc_in_start_center("writer") as component:
-
-            self.ui_test.execute_modeless_dialog_through_command(".uno:InsertAuthoritiesEntry")
-            insert_entry = self.xUITest.getTopFocusWindow()
-            from_document = insert_entry.getChild("fromdocument")
-            from_document.executeAction("CLICK", tuple())
-            new = insert_entry.getChild("new")
-
-            # When inserting a biblio entry field with a page number:
-            with self.ui_test.execute_blocking_action(new.executeAction, args=('CLICK', ())) as define_entry:
-                entry = define_entry.getChild("entry")
-                type_text(entry, "aaa")
-                listbox = define_entry.getChild("listbox")
-                select_pos(listbox, "16")  # WWW document
-                pagecb = define_entry.getChild("pagecb-visible")
-                pagecb.executeAction("CLICK", tuple())
-
-            insert = insert_entry.getChild("insert")
-            insert.executeAction("CLICK", tuple())
-            close = insert_entry.getChild("close")
-            self.ui_test.close_dialog_through_button(close)
-
-            # Then make sure the URL contains that page number:
-            paragraphs = component.Text.createEnumeration()
-            paragraph = paragraphs.nextElement()
-            portions = paragraph.createEnumeration()
-            portion = portions.nextElement()
-            for field in portion.TextField.Fields:
-                if field.Name != "URL":
-                    continue
-                # Without the accompanying fix in place, this test would have failed with:
-                # AssertionError: '' != '#page=1'
-                # i.e. the page number was not part of the URL.
-                self.assertEqual(field.Value, "#page=1")
-
     def test_bibliography_local_page_number_insert(self):
 
         # Given an empty Writer document:
