92bfb54720eabea8ccbca068792508d256067493
dante <dante19031999@gmail.com>
2021-08-14 20:39:56 +0200

    Some corrections to the mathml exporter for SmMlElement
    Change-Id: I03db8cb8023d55b53e5361577820635449106823

---
+++ confdefs.h
@@ -0,0 +1,15 @@
+/* confdefs.h */
+#define PACKAGE_NAME "LibreOffice"
+#define PACKAGE_TARNAME "libreoffice"
+#define PACKAGE_VERSION "7.2.0.0.alpha1+"
+#define PACKAGE_STRING "LibreOffice 7.2.0.0.alpha1+"
+#define PACKAGE_BUGREPORT ""
+#define PACKAGE_URL "http://documentfoundation.org/"
+#define LIBO_VERSION_MAJOR 7
+#define LIBO_VERSION_MINOR 2
+#define LIBO_VERSION_MICRO 0
+#define LIBO_VERSION_PATCH 0
+#define LIBO_THIS_YEAR 2021
+#define SRCDIR "/home/uv/Downloads/libreoffice"
+#define SRC_ROOT "/home/uv/Downloads/libreoffice"
+#define BUILDDIR "/home/uv/Downloads/libreofficeMerge"
