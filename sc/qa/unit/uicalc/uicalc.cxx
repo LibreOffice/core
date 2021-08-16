6ede12a41c927713edb68554730ef500c45defbe
Xisco Fauli <xiscofauli@libreoffice.org>
2021-08-16 18:56:14 +0200

    tdf#143896: sc_uicalc: Add unittest
    Change-Id: Idb8aa411a9e9d2c83346f21705ff4ae5aef74aab

--- sc/qa/unit/uicalc/uicalc.cxx
+++ sc/qa/unit/uicalc/uicalc.cxx
@@ -210,6 +210,28 @@ CPPUNIT_TEST_FIXTURE(ScUiCalcTest, testTdf138432)
     CPPUNIT_ASSERT_EQUAL(OUString("12345,67"), pDoc->GetString(ScAddress(0, 1, 0)));
 }
 
+CPPUNIT_TEST_FIXTURE(ScUiCalcTest, testTdf143896)
+{
+    mxComponent = loadFromDesktop("private:factory/scalc");
+    ScModelObj* pModelObj = dynamic_cast<ScModelObj*>(mxComponent.get());
+    CPPUNIT_ASSERT(pModelObj);
+    ScDocument* pDoc = pModelObj->GetDocument();
+    CPPUNIT_ASSERT(pDoc);
+
+    insertStringToCell(*pModelObj, "A2000", "Test");
+
+    CPPUNIT_ASSERT_EQUAL(OUString("Test"), pDoc->GetString(ScAddress(0, 1999, 0)));
+
+    pModelObj = saveAndReload(mxComponent, "Calc Office Open XML");
+    pDoc = pModelObj->GetDocument();
+    CPPUNIT_ASSERT(pDoc);
+
+    // Without the fix in place, this test would have failed with
+    // - Expected: Test
+    // - Actual  :
+    CPPUNIT_ASSERT_EQUAL(OUString("Test"), pDoc->GetString(ScAddress(0, 1999, 0)));
+}
+
 CPPUNIT_TEST_FIXTURE(ScUiCalcTest, testTdf100582)
 {
     ScModelObj* pModelObj = createDoc("tdf100582.xls");
