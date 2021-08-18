99d84b2aeb52cd3b9d035b61f74dd55c542bbcb6
Caolán McNamara <caolanm@redhat.com>
2021-08-18 14:03:47 +0100

    tdf#143353 defer tip of the day until impress template dialog is gone
    wait until that dialog is dismissed before showing the tip dialog
    
    Change-Id: Id0e7e28f09c5a9727e10eda55e468adb56bfda70

--- include/sfx2/app.hxx
+++ include/sfx2/app.hxx
@@ -218,6 +218,12 @@ public:
     /** loads the application logo as used in the impress slideshow pause screen */
     static BitmapEx GetApplicationLogo(tools::Long nWidth);
 
+    /** if true then dialog/infobar notifications like the tip of the day or
+        version change infobar should be suppressed */
+    static bool IsHeadlessOrUITest();
+
+    static bool IsTipOfTheDayDue();
+
     /** this Theme contains Images so must be deleted before DeInitVCL */
     sfx2::sidebar::Theme & GetSidebarTheme();
 };
