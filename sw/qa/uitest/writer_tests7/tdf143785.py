637d1e4519d0dfc40210cc5e4ec04c8b27176b6b
Xisco Fauli <xiscofauli@libreoffice.org>
2021-08-18 11:45:24 +0200

    tdf#143778, tdf#143785: sw: add UItest
    Change-Id: I3a917feb4a3ea6f14e7236e1bcdbc8b9e2bcf93b

---
+++ sw/qa/uitest/writer_tests7/tdf143785.py
@@ -0,0 +1,41 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+from uitest.framework import UITestCase
+from uitest.uihelper.common import get_url_for_data_file
+from libreoffice.uno.propertyvalue import mkPropertyValues
+from com.sun.star.text.TextContentAnchorType import AT_PAGE, AT_PARAGRAPH
+
+class tdf143785(UITestCase):
+
+    def test_tdf143785(self):
+
+        with self.ui_test.load_file(get_url_for_data_file("tdf137802.odt")) as document:
+
+            xWriterDoc = self.xUITest.getTopFocusWindow()
+            xWriterEdit = xWriterDoc.getChild("writer_edit")
+
+            self.xUITest.executeCommand(".uno:JumpToNextFrame")
+
+            self.ui_test.wait_until_child_is_available('metricfield')
+
+            self.assertEqual(False, document.isModified())
+
+            with self.ui_test.execute_dialog_through_command(".uno:TransformDialog", close_button="cancel") as xDialog:
+                pass
+
+            # Without the fix in place, this test would have failed with
+            # AssertionError: False != True
+            self.assertEqual(False, document.isModified())
+
+            with self.ui_test.execute_dialog_through_command(".uno:FormatArea", close_button="cancel") as xDialog:
+                pass
+
+            # tdf#143778: Without the fix in place, this test would have failed with
+            # AssertionError: False != True
+            self.assertEqual(False, document.isModified())
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
