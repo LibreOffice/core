b4d1bed49e0c262f90e9e3f720dd9816d57d3b99
Szymon Kłos <szymon.klos@collabora.com>
2021-08-19 15:34:46 +0200

    Rename AsyncColorPicker -> AsynchronousColorPicker
    Change-Id: If26865ea063df3052e6ba61642fe36f5a1a47929
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/120735
    Tested-by: Jenkins
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>

--- offapi/UnoApi_offapi.mk
+++ offapi/UnoApi_offapi.mk
@@ -100,7 +100,7 @@ $(eval $(call gb_UnoApi_add_idlfiles_nohdl,offapi,com/sun/star/configuration,\
     theDefaultProvider \
 ))
 $(eval $(call gb_UnoApi_add_idlfiles_nohdl,offapi,com/sun/star/cui,\
-    AsyncColorPicker \
+    AsynchronousColorPicker \
     ColorPicker \
 ))
 $(eval $(call gb_UnoApi_add_idlfiles_nohdl,offapi,com/sun/star/datatransfer,\
