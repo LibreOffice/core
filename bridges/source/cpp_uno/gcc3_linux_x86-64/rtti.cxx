1ddb23e8fd254675066df6a19e6d95a453b0b16f
Emircan Agac <thesadson@gmail.com>
2021-08-19 23:58:53 +0300

    tdf#130924 : replace debugging printf calls with SAL_INFO/SAL_WARN
    Change-Id: Ifede38f3b19d1e5226e18dba1aa21d478a430d9a

--- bridges/source/cpp_uno/gcc3_linux_x86-64/rtti.cxx
+++ bridges/source/cpp_uno/gcc3_linux_x86-64/rtti.cxx
@@ -158,9 +158,9 @@ std::type_info * RTTI::getRTTI(typelib_TypeDescription const & pTypeDescr)
     // symbol and rtti-name is nearly identical,
     // the symbol is prefixed with _ZTI
     char const * rttiName = symName.getStr() +4;
-#if OSL_DEBUG_LEVEL > 1
-    fprintf( stderr,"generated rtti for %s\n", rttiName );
-#endif
+
+    SAL_INFO("bridges", "Generated rtti for" << rttiName);
+
     std::unique_ptr<Generated> newRtti;
     switch (pTypeDescr.eTypeClass) {
     case typelib_TypeClass_EXCEPTION:
