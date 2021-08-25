LibreOffice/core

commit 75cd2b0fa71c73ae815b80d8e42328024ea63555

tdf#143736 Load hyperlink from more shapes
And limit the shape types which can have a hyperlink to those known to work.

Change-Id: I3d3522bea1e756dad8ddc2041e6588a367f42a7c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/120861
Tested-by: Jenkins
Reviewed-by: Samuel Mehrbrodt <samuel.mehrbrodt@allotropia.de>

sw/source/uibase/shells/drwbassh.cxx
@@ -856,11 +856,35 @@ void SwDrawBaseShell::GetState(SfxItemSet& rSet)
             {
                 if (pSdrView->GetMarkedObjectCount() != 1)
                     rSet.DisableItem(nWhich);
-                else if (nWhich == SID_OPEN_HYPERLINK || nWhich == SID_REMOVE_HYPERLINK
-                         || nWhich == SID_EDIT_HYPERLINK || nWhich == SID_COPY_HYPERLINK_LOCATION)
+
+                const SdrMarkList& rMarkList = pSdrView->GetMarkedObjectList();
+                SdrObject* pObj = rMarkList.GetMark(0)->GetMarkedSdrObj();
+                sal_uInt16 nObjType = pObj->GetObjIdentifier();
+
+                // Only enable hyperlink for the following types
+                switch (nObjType)
                 {
-                    const SdrMarkList& rMarkList = pSdrView->GetMarkedObjectList();
-                    SdrObject* pObj = rMarkList.GetMark(0)->GetMarkedSdrObj();
+                    case OBJ_PATHFILL:
+                    case OBJ_SECT:
+                    case OBJ_LINE:
+                    case OBJ_CUSTOMSHAPE:
+                    case OBJ_TEXT:
+                    case OBJ_RECT:
+                    case OBJ_CAPTION:
+                    case OBJ_POLY:
+                    case OBJ_PLIN:
+                    case E3D_SCENE_ID:
+                    case OBJ_MEASURE:
+                    case OBJ_EDGE:
+                        break;
+                    default:
+                        rSet.DisableItem(nWhich);
+                        break;
+                }
+
+                if (nWhich == SID_OPEN_HYPERLINK || nWhich == SID_REMOVE_HYPERLINK
+                    || nWhich == SID_EDIT_HYPERLINK || nWhich == SID_COPY_HYPERLINK_LOCATION)
+                {
                     if (pObj->getHyperlink().isEmpty())
                         rSet.DisableItem(nWhich);
                 }

xmloff/source/text/XMLTextFrameHyperlinkContext.cxx
@@ -115,6 +115,17 @@ css::uno::Reference< css::xml::sax::XFastContextHandler > XMLTextFrameHyperlinkC
         case XML_ELEMENT(DRAW, XML_PATH):
         case XML_ELEMENT(DRAW, XML_ELLIPSE):
         case XML_ELEMENT(DRAW, XML_LINE):
+        case XML_ELEMENT(DRAW, XML_RECT):
+        case XML_ELEMENT(DRAW, XML_CAPTION):
+        case XML_ELEMENT(DRAW, XML_POLYGON):
+        case XML_ELEMENT(DRAW, XML_POLYLINE):
+        case XML_ELEMENT(DRAW, XML_MEASURE):
+        case XML_ELEMENT(DRAW, XML_CIRCLE):
+        case XML_ELEMENT(DRAW, XML_CONNECTOR):
+        case XML_ELEMENT(DRAW, XML_CONTROL):
+        case XML_ELEMENT(DRAW, XML_PAGE_THUMBNAIL):
+        case XML_ELEMENT(DRAW, XML_G):
+        case XML_ELEMENT(DR3D, XML_SCENE):
         {
             Reference<XShapes> xShapes;
             SvXMLShapeContext* pShapeContext = XMLShapeImportHelper::CreateGroupChildContext(

commit 568643902036c0fdffa3cebfe122e4c2221fdc8c

Fix crash when drawing shape
Fallout from 75cd2b0fa71c73ae815b80d8e42328024ea63555

Change-Id: I455519126d11bee1b809f9fd9476b2fdd0f1a524

sw/source/uibase/shells/drwbassh.cxx
@@ -855,7 +855,10 @@ void SwDrawBaseShell::GetState(SfxItemSet& rSet)
             case SID_COPY_HYPERLINK_LOCATION:
             {
                 if (pSdrView->GetMarkedObjectCount() != 1)
+                {
                     rSet.DisableItem(nWhich);
+                    break;
+                }
 
                 const SdrMarkList& rMarkList = pSdrView->GetMarkedObjectList();
                 SdrObject* pObj = rMarkList.GetMark(0)->GetMarkedSdrObj();