36bf67ba03273a5b8b56b381794824a4cc389f12
Chris Sherlock <chris.sherlock79@gmail.com>
2021-08-25 17:07:07 +1000

    vcl: move OutputDevice rasterop functions to rasterop.cxx
    Split off RasterOp enum into own header, and also add unit tests for
    SetRasterOp() and GetRasterOp().
    
    Change-Id: I1c97e87ef2d0684cb15b6ac544597eace5adb48a

---
+++ vcl/source/outdev/rasterop.cxx
@@ -0,0 +1,50 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#include <vcl/metaact.hxx>
+#include <vcl/virdev.hxx>
+
+#include <salgdi.hxx>
+
+RasterOp OutputDevice::GetRasterOp() const { return meRasterOp; }
+
+void OutputDevice::SetRasterOp(RasterOp eRasterOp)
+{
+    if (mpMetaFile)
+        mpMetaFile->AddAction(new MetaRasterOpAction(eRasterOp));
+
+    if (meRasterOp != eRasterOp)
+    {
+        meRasterOp = eRasterOp;
+        mbInitLineColor = mbInitFillColor = true;
+
+        if (mpGraphics || AcquireGraphics())
+        {
+            assert(mpGraphics);
+            mpGraphics->SetXORMode((RasterOp::Invert == meRasterOp)
+                                       || (RasterOp::Xor == meRasterOp),
+                                   RasterOp::Invert == meRasterOp);
+        }
+    }
+
+    if (mpAlphaVDev)
+        mpAlphaVDev->SetRasterOp(eRasterOp);
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
