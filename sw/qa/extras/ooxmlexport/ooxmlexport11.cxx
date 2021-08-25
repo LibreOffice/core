LibreOffice/core

commit d656191ec308d4280b93c7169372e543a255d108

tdf#119952 DOCX import: fix negative page margins
DOCX body text can overlap with header/footer, if top/bottom
page margin is negative. To support this, convert header/footer
text content to textbox anchored to header/footer, if needed.

Note: possible improvements:

1) Skip this hack, if the header is small enough to not overlap with
the body, calculate only the height of the header at the import time.

2) This hack does not fix the case when the top of the header
is under the top of the body. (A problem in DOC import, too.)
This could be achieved by repositioning the dummy header to the top,
and lower the textbox by the same amount. (This would still not
resolve the extreme situation, when the body start from 0 mm
(in LibreOffice, header must be at least 1 mm).

3) Import of VertOrientation::BOTTOM property seems to be bad,
or at least the footer loses this property after a DOCX round-trip,
resulting bad footer position.

4) after a round-trip, the 1 mm height of the dummy header
increases to 1 line height. Also the "Autofit height" and
"Use dynamic spacing" settings are changed, likely related
to their missing DOCX export.

Co-authored-by: Tibor Nagy (NISZ)

Change-Id: I8319c93c6c5a980878ee9956c8ab2953da60409e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/117842
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -2600,6 +2600,64 @@ void DomainMapper_Impl::appendGlossaryEntry()
     appendTextSectionAfter(m_xGlossaryEntryStart);
 }
 
+void DomainMapper_Impl::fillEmptyFrameProperties(std::vector<beans::PropertyValue>& rFrameProperties, bool bSetAnchorToChar)
+{
+    if (bSetAnchorToChar)
+        rFrameProperties.push_back(comphelper::makePropertyValue(getPropertyName(PROP_ANCHOR_TYPE), text::TextContentAnchorType_AS_CHARACTER));
+
+    uno::Any aEmptyBorder = uno::makeAny(table::BorderLine2());
+    static const std::vector<PropertyIds> aBorderIds
+        = { PROP_BOTTOM_BORDER, PROP_LEFT_BORDER, PROP_RIGHT_BORDER, PROP_TOP_BORDER };
+    for (size_t i = 0; i < aBorderIds.size(); ++i)
+        rFrameProperties.push_back(comphelper::makePropertyValue(getPropertyName(aBorderIds[i]), aEmptyBorder));
+
+    static const std::vector<PropertyIds> aMarginIds
+        = { PROP_BOTTOM_MARGIN, PROP_BOTTOM_BORDER_DISTANCE,
+            PROP_LEFT_MARGIN,   PROP_LEFT_BORDER_DISTANCE,
+            PROP_RIGHT_MARGIN,  PROP_RIGHT_BORDER_DISTANCE,
+            PROP_TOP_MARGIN,    PROP_TOP_BORDER_DISTANCE };
+    for (size_t i = 0; i < aMarginIds.size(); ++i)
+        rFrameProperties.push_back(comphelper::makePropertyValue(getPropertyName(aMarginIds[i]), static_cast<sal_Int32>(0)));
+}
+
+void DomainMapper_Impl::ConvertHeaderFooterToTextFrame(bool bDynamicHeightTop, bool bDynamicHeightBottom)
+{
+    while (!m_aHeaderFooterTextAppendStack.empty())
+    {
+        auto aFooterHeader = m_aHeaderFooterTextAppendStack.top();
+        if ((aFooterHeader.second && !bDynamicHeightTop) || (!aFooterHeader.second && !bDynamicHeightBottom))
+        {
+            uno::Reference< text::XTextAppend > xTextAppend = aFooterHeader.first.xTextAppend;
+            uno::Reference< text::XTextCursor > xCursor = xTextAppend->createTextCursor();
+            uno::Reference< text::XTextRange > xRangeStart, xRangeEnd;
+
+            xRangeStart = xCursor->getStart();
+            xCursor->gotoEnd(false);
+            xRangeEnd = xCursor->getStart();
+
+            std::vector<beans::PropertyValue> aFrameProperties;
+
+            aFrameProperties.push_back(comphelper::makePropertyValue("TextWrap", css::text::WrapTextMode_THROUGH));
+            aFrameProperties.push_back(comphelper::makePropertyValue(getPropertyName(PROP_HORI_ORIENT), text::HoriOrientation::LEFT));
+            aFrameProperties.push_back(comphelper::makePropertyValue(getPropertyName(PROP_OPAQUE), false));
+            aFrameProperties.push_back(comphelper::makePropertyValue(getPropertyName(PROP_WIDTH_TYPE), text::SizeType::MIN));
+            aFrameProperties.push_back(comphelper::makePropertyValue(getPropertyName(PROP_SIZE_TYPE), text::SizeType::MIN));
+
+            fillEmptyFrameProperties(aFrameProperties, false);
+
+            // If it is a footer, then orient the frame to the bottom
+            if (!aFooterHeader.second)
+                aFrameProperties.push_back(comphelper::makePropertyValue(getPropertyName(PROP_VERT_ORIENT), text::VertOrientation::BOTTOM));
+
+            uno::Reference<text::XTextAppendAndConvert> xBodyText(
+                xRangeStart->getText(), uno::UNO_QUERY);
+            xBodyText->convertToTextFrame(xRangeStart, xRangeEnd,
+                comphelper::containerToSequence(aFrameProperties));
+        }
+        m_aHeaderFooterTextAppendStack.pop();
+    }
+}
+
 void DomainMapper_Impl::PushPageHeaderFooter(bool bHeader, SectionPropertyMap::PageType eType)
 {
     m_bSaveParaHadField = m_bParaHadField;
@@ -2662,6 +2720,11 @@ void DomainMapper_Impl::PushPageHeaderFooter(bool bHeader, SectionPropertyMap::P
                 m_bIsNewDoc
                     ? uno::Reference<text::XTextCursor>()
                     : xText->createTextCursorByRange(xText->getStart())));
+            m_aHeaderFooterTextAppendStack.push(std::make_pair(TextAppendContext(uno::Reference< text::XTextAppend >(xText, uno::UNO_QUERY_THROW),
+                m_bIsNewDoc
+                    ? uno::Reference<text::XTextCursor>()
+                    : xText->createTextCursorByRange(xText->getStart())),
+                bHeader));
             m_bDiscardHeaderFooter = false; // set only on success!
         }
         // If we have *hidden* header footer

sw/qa/extras/ooxmlexport/ooxmlexport11.cxx
@@ -1458,6 +1458,89 @@ DECLARE_OOXMLEXPORT_TEST(testTdf124678_case2, "tdf124678_with_leading_paragraph.
     CPPUNIT_ASSERT_EQUAL_MESSAGE("Second page header text", OUString("HEADER"), parseDump("/root/page[2]/header/txt"));
 }
 
+static bool lcl_nearEqual(const sal_Int32 nNumber1, const sal_Int32 nNumber2, sal_Int32 nMaxDiff = 5)
+{
+    return std::abs(nNumber1 - nNumber2) < nMaxDiff;
+}
+
+DECLARE_OOXMLEXPORT_TEST(testTdf119952_negativeMargins, "tdf119952_negativeMargins.docx")
+{
+    // With negative margins (in MS Word) one can set up header (or footer) that overlaps with the body.
+    // LibreOffice unable to display that, so when importing negative margins,
+    // the header (or footer) converted to a flyframe, anchored to the header..
+    // that can overlap with the body, and will appear like in Word.
+    // This conversion modifies the document [i.e. replacing header text with a textbox...]
+    // but its DOCX export looks the same, as the original document in Word, too.
+    xmlDocUniquePtr pDump = parseLayoutDump();
+
+    //Check layout positions / sizes
+    sal_Int32 nLeftHead = getXPath(pDump, "//page[1]/header/infos/bounds", "left").toInt32();
+    sal_Int32 nLeftBody = getXPath(pDump, "//page[1]/body/infos/bounds", "left").toInt32();
+    sal_Int32 nLeftFoot = getXPath(pDump, "//page[1]/footer/infos/bounds", "left").toInt32();
+    sal_Int32 nLeftHFly = getXPath(pDump, "//page[1]/header/txt/anchored/fly/infos/bounds", "left").toInt32();
+    sal_Int32 nLeftFFly = getXPath(pDump, "//page[1]/footer/txt/anchored/fly/infos/bounds", "left").toInt32();
+
+    sal_Int32 nTopHead = getXPath(pDump, "//page[1]/header/infos/bounds", "top").toInt32();
+    sal_Int32 nTopBody = getXPath(pDump, "//page[1]/body/infos/bounds", "top").toInt32();
+    sal_Int32 nTopFoot = getXPath(pDump, "//page[1]/footer/infos/bounds", "top").toInt32();
+    sal_Int32 nTopHFly = getXPath(pDump, "//page[1]/header/txt/anchored/fly/infos/bounds", "top").toInt32();
+    sal_Int32 nTopFFly = getXPath(pDump, "//page[1]/footer/txt/anchored/fly/infos/bounds", "top").toInt32();
+
+    sal_Int32 nHeightHead = getXPath(pDump, "//page[1]/header/infos/bounds", "height").toInt32();
+    sal_Int32 nHeightBody = getXPath(pDump, "//page[1]/body/infos/bounds", "height").toInt32();
+    sal_Int32 nHeightFoot = getXPath(pDump, "//page[1]/footer/infos/bounds", "height").toInt32();
+    sal_Int32 nHeightHFly = getXPath(pDump, "//page[1]/header/txt/anchored/fly/infos/bounds", "height").toInt32();
+    sal_Int32 nHeightFFly = getXPath(pDump, "//page[1]/footer/txt/anchored/fly/infos/bounds", "height").toInt32();
+    sal_Int32 nHeightHFlyBound = getXPath(pDump, "//page[1]/header/infos/prtBounds", "height").toInt32();
+    sal_Int32 nHeightFFlyBound = getXPath(pDump, "//page[1]/footer/infos/prtBounds", "height").toInt32();
+
+    CPPUNIT_ASSERT(lcl_nearEqual(nLeftHead, nLeftBody));
+    CPPUNIT_ASSERT(lcl_nearEqual(nLeftHead, nLeftFoot));
+    CPPUNIT_ASSERT(lcl_nearEqual(nLeftHead, nLeftHFly));
+    CPPUNIT_ASSERT(lcl_nearEqual(nLeftHead, nLeftFFly));
+
+    CPPUNIT_ASSERT(lcl_nearEqual(nTopHead, 851));
+    CPPUNIT_ASSERT(lcl_nearEqual(nTopBody, 1418));
+    CPPUNIT_ASSERT(lcl_nearEqual(nTopFoot, 15875));
+    CPPUNIT_ASSERT(lcl_nearEqual(nTopHFly, 851));
+
+    // this seems to be an import bug
+    if (!mbExported)
+        CPPUNIT_ASSERT(lcl_nearEqual(nTopFFly, 14403));
+
+    CPPUNIT_ASSERT(lcl_nearEqual(nHeightHead, 567));
+    CPPUNIT_ASSERT(lcl_nearEqual(nHeightBody, 14457));
+    CPPUNIT_ASSERT(lcl_nearEqual(nHeightFoot, 680));
+    CPPUNIT_ASSERT(lcl_nearEqual(nHeightHFly, 2152));
+    CPPUNIT_ASSERT(lcl_nearEqual(nHeightFFly, 2152));
+
+    // after export these heights increase to like 567..
+    // not sure if it is an other import, or export bug... or just the result of the modified document
+    if (!mbExported)
+    {
+        CPPUNIT_ASSERT(lcl_nearEqual(nHeightHFlyBound, 57));
+        CPPUNIT_ASSERT(lcl_nearEqual(nHeightFFlyBound, 57));
+    }
+
+    //Check text of header/ footer
+    CPPUNIT_ASSERT_EQUAL(OUString("f1"), getXPath(pDump, "//page[1]/header/txt/anchored/fly/txt[1]/Text", "Portion"));
+    CPPUNIT_ASSERT_EQUAL(OUString("                f8"), getXPath(pDump, "//page[1]/header/txt/anchored/fly/txt[8]/Text", "Portion"));
+    CPPUNIT_ASSERT_EQUAL(OUString("                f8"), getXPath(pDump, "//page[1]/footer/txt/anchored/fly/txt[1]/Text", "Portion"));
+    CPPUNIT_ASSERT_EQUAL(OUString("f1"), getXPath(pDump, "//page[1]/footer/txt/anchored/fly/txt[8]/Text", "Portion"));
+
+    CPPUNIT_ASSERT_EQUAL(OUString("p1"), getXPath(pDump, "//page[2]/header/txt/anchored/fly/txt[1]/Text", "Portion"));
+    CPPUNIT_ASSERT_EQUAL(OUString("p1"), getXPath(pDump, "//page[2]/footer/txt/anchored/fly/txt[1]/Text", "Portion"));
+
+    CPPUNIT_ASSERT_EQUAL(OUString("  aaaa"), getXPath(pDump, "//page[3]/header/txt/anchored/fly/txt[1]/Text", "Portion"));
+    CPPUNIT_ASSERT_EQUAL(OUString("      eeee"), getXPath(pDump, "//page[3]/header/txt/anchored/fly/txt[5]/Text", "Portion"));
+
+    CPPUNIT_ASSERT_EQUAL(OUString("f1    f2      f3        f4          f5            f6              f7                f8"), parseDump("/root/page[1]/header/txt/anchored/fly"));
+    CPPUNIT_ASSERT_EQUAL(OUString("                f8              f7            f6          f5        f4      f3    f2f1"), parseDump("/root/page[1]/footer/txt/anchored/fly"));
+    CPPUNIT_ASSERT_EQUAL(OUString("p1"), parseDump("/root/page[2]/header/txt/anchored/fly"));
+    CPPUNIT_ASSERT_EQUAL(OUString("p1"), parseDump("/root/page[2]/footer/txt/anchored/fly"));
+    CPPUNIT_ASSERT_EQUAL(OUString("  aaaa   bbbb    cccc     dddd      eeee"), parseDump("/root/page[3]/header/txt/anchored/fly"));
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

writerfilter/source/dmapper/DomainMapperTableHandler.cxx
@@ -340,38 +340,6 @@ void lcl_adjustBorderDistance(TableInfo& rInfo, const table::BorderLine2& rLeftB
     rInfo.nRightBorderDistance = nActualR;
 }
 
-void lcl_fillEmptyFrameProperties(std::vector<beans::PropertyValue>& rFrameProperties)
-{
-    // fill empty frame properties to create an invisible frame around the table:
-    // hide frame borders and zero inner and outer frame margins
-    beans::PropertyValue aValue;
-    aValue.Name = getPropertyName( PROP_ANCHOR_TYPE );
-    aValue.Value <<= text::TextContentAnchorType_AS_CHARACTER;
-    rFrameProperties.push_back(aValue);
-
-    table::BorderLine2 aEmptyBorder;
-    static const std::vector<std::u16string_view> aBorderNames
-        = { u"TopBorder", u"LeftBorder", u"BottomBorder", u"RightBorder" };
-    for (size_t i = 0; i < aBorderNames.size(); ++i)
-    {
-        beans::PropertyValue aBorderValue;
-        aBorderValue.Name = aBorderNames[i];
-        aBorderValue.Value <<= aEmptyBorder;
-        rFrameProperties.push_back(aBorderValue);
-    }
-    static const std::vector<std::u16string_view> aMarginNames
-        = { u"TopBorderDistance", u"LeftBorderDistance",
-            u"BottomBorderDistance", u"RightBorderDistance",
-            u"TopMargin", u"LeftMargin", u"BottomMargin", u"RightMargin" };
-    for (size_t i = 0; i < aMarginNames.size(); ++i)
-    {
-        beans::PropertyValue aMarginValue;
-        aMarginValue.Name = aMarginNames[i];
-        aMarginValue.Value <<= sal_Int32(10);
-        rFrameProperties.push_back(aMarginValue);
-    }
-}
-
 }
 
 TableStyleSheetEntry * DomainMapperTableHandler::endTableGetTableStyle(TableInfo & rInfo,
@@ -1440,8 +1408,10 @@ void DomainMapperTableHandler::endTable(unsigned int nestedTableLevel, bool bTab
         uno::Reference<text::XTextRange> xStart;
         uno::Reference<text::XTextRange> xEnd;
 
-        if ( bConvertToFloating )
-            lcl_fillEmptyFrameProperties(aFrameProperties);
+        // fill empty frame properties to create an invisible frame around the table:
+        // hide frame borders and zero inner and outer frame margins
+        if (bConvertToFloating)
+            DomainMapper_Impl::fillEmptyFrameProperties(aFrameProperties, true);
 
         // OOXML table style may contain paragraph properties, apply these on cell paragraphs
         if ( m_aTableRanges[0].hasElements() && m_aTableRanges[0][0].hasElements() )

writerfilter/source/dmapper/DomainMapper_Impl.hxx
@@ -467,6 +467,7 @@ private:
     std::stack<TextAppendContext>                                                   m_aTextAppendStack;
     std::stack<AnchoredContext>                                                     m_aAnchoredStack;
     std::stack<HeaderFooterContext>                                                 m_aHeaderFooterStack;
+    std::stack<std::pair<TextAppendContext, bool>>                                  m_aHeaderFooterTextAppendStack;
     std::deque<FieldContextPtr> m_aFieldStack;
     bool m_bForceGenericFields;
     bool                                                                            m_bSetUserFieldContent;
@@ -820,6 +821,8 @@ public:
 
     void PopPageHeaderFooter();
     bool IsInHeaderFooter() const { return m_eInHeaderFooterImport != HeaderFooterImportState::none; }
+    void ConvertHeaderFooterToTextFrame(bool, bool);
+    static void fillEmptyFrameProperties(std::vector<css::beans::PropertyValue>& rFrameProperties, bool bSetAnchorToChar);
 
     bool IsInTOC() const { return m_bStartTOC; }
 

commit 1c57913bb372848d3b9deefb142a1f7371ac2aa4

tdf#143384 DOCX import: fix SAXException at header with table
Regression from commit d656191ec308d4280b93c7169372e543a255d108
"tdf#119952 DOCX import: fix negative page margins".

Add SwXHeadFootText::CreateTextCursor(bool bIgnoreTables = false)
(modeled after SwXBodyText::CreateTextCursor) to create text cursor
for copying the header/footer also started with table during
fly frame creation in convertoToTextFrame().

Note: add hidden property PROP_CURSOR_NOT_IGNORE_TABLES_IN_HF
to use the new feature in domainmapper (followed
commit af4e5ee0f93c1ff442d08caed5c875f2b2c1fd43
"tdf#97128 DOCX import: fix frame direction").

Co-authored-by: Tibor Nagy (NISZ)

Change-Id: I96e2cf2dddcecd146c53c12d7fdb44fc4d90fa0d

sw/inc/unotextbodyhf.hxx
@@ -115,6 +115,8 @@ public:
     static css::uno::Reference< css::text::XText >
         CreateXHeadFootText(SwFrameFormat & rHeadFootFormat, const bool bIsHeader);
 
+    css::uno::Reference<css::text::XTextCursor> CreateTextCursor(const bool bIgnoreTables = false);
+
     // XInterface
     virtual css::uno::Any SAL_CALL queryInterface(
             const css::uno::Type& rType) override;

sw/inc/unotextrange.hxx
@@ -57,7 +57,8 @@ namespace sw {
 
     enum class TextRangeMode {
         RequireTextNode,
-        AllowNonTextNode
+        AllowNonTextNode,
+        AllowTableNode
     };
 
     void DeepCopyPaM(SwPaM const & rSource, SwPaM & rTarget);

sw/source/core/unocore/unoframe.cxx
@@ -1418,7 +1418,13 @@ void SwXFrame::setPropertyValue(const OUString& rPropertyName, const ::uno::Any&
     const ::SfxItemPropertyMapEntry* pEntry = m_pPropSet->getPropertyMap().getByName(rPropertyName);
 
     if (!pEntry)
-        throw beans::UnknownPropertyException( "Unknown property: " + rPropertyName, static_cast < cppu::OWeakObject * > ( this ) );
+    {
+        // Hack to skip the dummy CursorNotIgnoreTables property
+        if (rPropertyName == "CursorNotIgnoreTables")
+            return;
+        else
+            throw beans::UnknownPropertyException("Unknown property: " + rPropertyName, static_cast <cppu::OWeakObject*> (this));
+    }
 
     const sal_uInt8 nMemberId(pEntry->nMemberId);
     uno::Any aValue(_rValue);

sw/source/core/unocore/unoobj2.cxx
@@ -1116,6 +1116,7 @@ bool XTextRangeToSwPaM( SwUnoInternalPaM & rToFill,
     SwXTextPortion* pPortion = nullptr;
     SwXText* pText = nullptr;
     SwXParagraph* pPara = nullptr;
+    SwXHeadFootText* pHeadText = nullptr;
     if(xRangeTunnel.is())
     {
         pRange  = ::sw::UnoTunnelGetImplementation<SwXTextRange>(xRangeTunnel);
@@ -1125,12 +1126,26 @@ bool XTextRangeToSwPaM( SwUnoInternalPaM & rToFill,
             ::sw::UnoTunnelGetImplementation<SwXTextPortion>(xRangeTunnel);
         pText   = ::sw::UnoTunnelGetImplementation<SwXText>(xRangeTunnel);
         pPara   = ::sw::UnoTunnelGetImplementation<SwXParagraph>(xRangeTunnel);
+        if (eMode == TextRangeMode::AllowTableNode)
+            pHeadText = dynamic_cast<SwXHeadFootText*>(pText);
     }
 
     // if it's a text then create a temporary cursor there and re-use
     // the pCursor variable
     // #i108489#: Reference in outside scope to keep cursor alive
     uno::Reference< text::XTextCursor > xTextCursor;
+    if (pHeadText)
+    {
+        // if it is a header / footer text, and eMode == TextRangeMode::AllowTableNode
+        // then set the cursor to the beginning of the text
+        // if it is started with a table then set into the table
+        xTextCursor.set(pHeadText->CreateTextCursor(true));
+        xTextCursor->gotoEnd(true);
+        pCursor =
+            comphelper::getUnoTunnelImplementation<OTextCursorHelper>(xTextCursor);
+        pCursor->GetPaM()->Normalize();
+    }
+    else
     if (pText)
     {
         xTextCursor.set( pText->CreateCursor() );

sw/source/core/unocore/unotext.cxx
@@ -1537,14 +1537,31 @@ SwXText::convertToTextFrame(
     {
         throw  uno::RuntimeException();
     }
+    // tdf#143384 recognize dummy property, that was set to make createTextCursor
+    // to not ignore tables.
+    // It is enough to use this hack only for the range start,
+    // because as far as I know, the range cannot end with table when this property is set.
+    ::sw::TextRangeMode eMode = ::sw::TextRangeMode::RequireTextNode;
+    for (const auto& rCellProperty : rFrameProperties)
+    {
+        if (rCellProperty.Name == "CursorNotIgnoreTables")
+        {
+            bool bAllowNonTextNode = false;
+            rCellProperty.Value >>= bAllowNonTextNode;
+            if (bAllowNonTextNode)
+                eMode = ::sw::TextRangeMode::AllowTableNode;
+            break;
+        }
+    }
     uno::Reference< text::XTextContent > xRet;
     std::optional<SwUnoInternalPaM> pTempStartPam(*GetDoc());
     std::optional<SwUnoInternalPaM> pEndPam(*GetDoc());
-    if (!::sw::XTextRangeToSwPaM(*pTempStartPam, xStart) ||
-        !::sw::XTextRangeToSwPaM(*pEndPam, xEnd))
+    if (!::sw::XTextRangeToSwPaM(*pTempStartPam, xStart, eMode)
+        || !::sw::XTextRangeToSwPaM(*pEndPam, xEnd))
     {
         throw lang::IllegalArgumentException();
     }
+
     auto pStartPam(GetDoc()->CreateUnoCursor(*pTempStartPam->GetPoint()));
     if (pTempStartPam->HasMark())
     {
@@ -2613,8 +2630,7 @@ uno::Any SAL_CALL SwXHeadFootText::queryInterface(const uno::Type& rType)
         : ret;
 }
 
-uno::Reference<text::XTextCursor> SAL_CALL
-SwXHeadFootText::createTextCursor()
+uno::Reference<text::XTextCursor> SwXHeadFootText::CreateTextCursor(const bool bIgnoreTables)
 {
     SolarMutexGuard aGuard;
 
@@ -2632,18 +2648,22 @@ SwXHeadFootText::createTextCursor()
     // after the table - otherwise the cursor would be in the body text!
     SwStartNode const*const pOwnStartNode = rNode.FindSttNodeByType(
             (m_pImpl->m_bIsHeader) ? SwHeaderStartNode : SwFooterStartNode);
-    // is there a table here?
-    SwTableNode* pTableNode = rUnoCursor.GetNode().FindTableNode();
-    SwContentNode* pCont = nullptr;
-    while (pTableNode)
+
+    if (!bIgnoreTables)
     {
-        rUnoCursor.GetPoint()->nNode = *pTableNode->EndOfSectionNode();
-        pCont = GetDoc()->GetNodes().GoNext(&rUnoCursor.GetPoint()->nNode);
-        pTableNode = pCont->FindTableNode();
-    }
-    if (pCont)
-    {
-        rUnoCursor.GetPoint()->nContent.Assign(pCont, 0);
+        // is there a table here?
+        SwTableNode* pTableNode = rUnoCursor.GetNode().FindTableNode();
+        SwContentNode* pCont = nullptr;
+        while (pTableNode)
+        {
+            rUnoCursor.GetPoint()->nNode = *pTableNode->EndOfSectionNode();
+            pCont = GetDoc()->GetNodes().GoNext(&rUnoCursor.GetPoint()->nNode);
+            pTableNode = pCont->FindTableNode();
+        }
+        if (pCont)
+        {
+            rUnoCursor.GetPoint()->nContent.Assign(pCont, 0);
+        }
     }
     SwStartNode const*const pNewStartNode = rUnoCursor.GetNode().FindSttNodeByType(
             (m_pImpl->m_bIsHeader) ? SwHeaderStartNode : SwFooterStartNode);
@@ -2654,6 +2674,12 @@ SwXHeadFootText::createTextCursor()
         throw aExcept;
     }
     return static_cast<text::XWordCursor*>(pXCursor.get());
+}
+
+uno::Reference<text::XTextCursor> SAL_CALL
+SwXHeadFootText::createTextCursor()
+{
+    return CreateTextCursor(false);
 }
 
 uno::Reference<text::XTextCursor> SAL_CALL SwXHeadFootText::createTextCursorByRange(

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -2742,6 +2742,12 @@ void DomainMapper_Impl::ConvertHeaderFooterToTextFrame(bool bDynamicHeightTop, b
             aFrameProperties.push_back(comphelper::makePropertyValue(getPropertyName(PROP_OPAQUE), false));
             aFrameProperties.push_back(comphelper::makePropertyValue(getPropertyName(PROP_WIDTH_TYPE), text::SizeType::MIN));
             aFrameProperties.push_back(comphelper::makePropertyValue(getPropertyName(PROP_SIZE_TYPE), text::SizeType::MIN));
+            // tdf#143384 If the header/footer started with a table, convertToTextFrame could not
+            // convert the table, because it used createTextCursor() -which ignore tables-
+            // to set the conversion range.
+            // This dummy property is set to make convertToTextFrame to use an other CreateTextCursor
+            // method that can be parameterized to not ignore tables.
+            aFrameProperties.push_back(comphelper::makePropertyValue(getPropertyName(PROP_CURSOR_NOT_IGNORE_TABLES_IN_HF), true));
 
             fillEmptyFrameProperties(aFrameProperties, false);
 
@@ -2751,7 +2757,7 @@ void DomainMapper_Impl::ConvertHeaderFooterToTextFrame(bool bDynamicHeightTop, b
 
             uno::Reference<text::XTextAppendAndConvert> xBodyText(
                 xRangeStart->getText(), uno::UNO_QUERY);
-            xBodyText->convertToTextFrame(xRangeStart, xRangeEnd,
+            xBodyText->convertToTextFrame(xTextAppend, xRangeEnd,
                 comphelper::containerToSequence(aFrameProperties));
         }
         m_aHeaderFooterTextAppendStack.pop();

writerfilter/source/dmapper/PropertyIds.cxx
@@ -364,6 +364,7 @@ OUString getPropertyName( PropertyIds eId )
         case PROP_RTL_GUTTER:
             sName = "RtlGutter";
             break;
+        case PROP_CURSOR_NOT_IGNORE_TABLES_IN_HF: sName = "CursorNotIgnoreTables"; break;
     }
     assert(sName.getLength()>0);
     return sName;

writerfilter/source/dmapper/PropertyIds.hxx
@@ -361,6 +361,7 @@ enum PropertyIds
         ,PROP_CELL_FORMULA_CONVERTED
         ,PROP_GUTTER_MARGIN
         ,PROP_RTL_GUTTER
+        ,PROP_CURSOR_NOT_IGNORE_TABLES_IN_HF
     };
 
 //Returns the UNO string equivalent to eId.

sw/qa/extras/ooxmlexport/ooxmlexport11.cxx
@@ -1618,6 +1618,13 @@ DECLARE_OOXMLEXPORT_TEST(testTdf119952_negativeMargins, "tdf119952_negativeMargi
     CPPUNIT_ASSERT_EQUAL(OUString("  aaaa   bbbb    cccc     dddd      eeee"), parseDump("/root/page[3]/header/txt/anchored/fly"));
 }
 
+DECLARE_OOXMLEXPORT_TEST(testTdf143384_tableInFoot_negativeMargins, "tdf143384_tableInFoot_negativeMargins.docx")
+{
+    // There should be no crash during loading of the document
+    // so, let's check just how much pages we have
+    CPPUNIT_ASSERT_EQUAL(1, getPages());
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */