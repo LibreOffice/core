5a30031d321729e6f517fa7519deb226f4fb9f23
Stephan Bergmann <sbergman@redhat.com>
2021-08-27 13:48:58 +0200

    loplugin:nullptr (macOS)
    Change-Id: I5fc54c9005fbada4184c520d447276be2df16f0f

--- comphelper/source/misc/debuggerinfo.cxx
+++ comphelper/source/misc/debuggerinfo.cxx
@@ -55,7 +55,7 @@ bool isDebuggerAttached()
     // Call sysctl.
 
     size = sizeof(info);
-    junk = sysctl(mib, sizeof(mib) / sizeof(*mib), &info, &size, NULL, 0);
+    junk = sysctl(mib, sizeof(mib) / sizeof(*mib), &info, &size, nullptr, 0);
     assert(junk == 0);
 
     // We're being debugged if the P_TRACED flag is set.
