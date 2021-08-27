886e23d49924d416c000da25abd3e405f0ea32bf
Vasily Melenchuk <vasily.melenchuk@cib.de>
2021-08-27 18:10:15 +0300

    tdf#126426: WIP
    Change-Id: Ie954f53696bd872cb1f59cb586fb55f6cd7c73bc

--- oox/source/drawingml/textbodycontext.cxx
+++ oox/source/drawingml/textbodycontext.cxx
@@ -29,6 +29,7 @@
 #include <oox/token/namespaces.hxx>
 #include <oox/helper/attributelist.hxx>
 #include <sax/fastattribs.hxx>
+#include "hyperlinkcontext.hxx"
 
 #include <oox/mathml/import.hxx>
 
@@ -104,6 +105,16 @@ ContextHandlerRef TextParagraphContext::onCreateContext( sal_Int32 aElementToken
             return this;
         case OOX_TOKEN(a14, m):
             return CreateLazyMathBufferingContext(*this, mrParagraph);
+        case W_TOKEN( hyperlink ):
+        {
+            TextRunPtr pRun = std::make_shared<TextRun>();
+            mrParagraph.addRun(pRun);
+            // parse hyperlink attributes: use HyperLinkContext for that
+            rtl::Reference<HyperLinkContext> pContext(new HyperLinkContext(
+                *this, rAttribs, pRun->getTextCharacterProperties().maHyperlinkPropertyMap));
+            // but create text run context because HyperLinkContext can't process internal w:r, w:t, etc
+            return new RegularTextRunContext(*this, pRun);
+        }
         default:
             SAL_WARN("oox", "TextParagraphContext::onCreateContext: unhandled element: " << getBaseToken(aElementToken));
     }
