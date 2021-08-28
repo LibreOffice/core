chrissherlock/libreoffice-experimental

vcl: move ImplLayoutArgs to own files
Change-Id: Ic1da4e4edcb55125b30d475a6c337570d6fbc0cf

vcl/Library_vcl.mk
@@ -292,6 +292,7 @@ $(eval $(call gb_Library_add_exception_objects,vcl,\
     vcl/source/treelist/svimpbox \
     vcl/source/treelist/svlbitm \
     vcl/source/treelist/uiobject \
+    vcl/source/gdi/ImplLayoutArgs \
     vcl/source/gdi/configsettings \
     vcl/source/gdi/cvtgrf \
     vcl/source/gdi/svmconverter \

LibreOffice/core

vcl/Library_vcl.mk
@@ -235,6 +235,7 @@ $(eval $(call gb_Library_add_exception_objects,vcl,\
     vcl/source/outdev/vclreferencebase \
     vcl/source/outdev/nativecontrols \
     vcl/source/outdev/map \
+    vcl/source/text/ImplLayoutArgs \
     vcl/source/text/TextLayoutCache \
     vcl/source/treelist/headbar \
     vcl/source/treelist/iconview \