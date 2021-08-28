1cd652383f960a836993ffd37e74c5f6c8b3cc9d
Chris Sherlock <chris.sherlock79@gmail.com>
2021-08-29 00:59:08 +1000

    vcl: move TextLayoutCache into vcl::text namespace
    Also place TextLayoutCache function into own source file.
    
    Change-Id: Ia38c2d73715676a924cdbb0de6308a72a40ec3b3

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -4543,7 +4543,7 @@ static bool doc_paste(LibreOfficeKitDocument* pThis, const char* pMimeType, cons
 
     uno::Sequence<beans::PropertyValue> aPropertyValues(comphelper::InitPropertySequence(
     {
-        {"AnchorType", uno::makeAny(static_cast<sal_uInt16>(text::TextContentAnchorType_AS_CHARACTER))},
+        {"AnchorType", uno::makeAny(static_cast<sal_uInt16>(css::text::TextContentAnchorType_AS_CHARACTER))},
         {"IgnoreComments", uno::makeAny(true)},
     }));
     if (!comphelper::dispatchCommand(".uno:Paste", aPropertyValues))
