7f7d891b324249e23e4a5b4455cf64fea82b4e41
Noel Grandin <noel@peralex.com>
2021-08-28 18:53:07 +0200

    tdf#135683 speed up writer layout cache access
    can be hot, so use a std::unordered_map
    
    Change-Id: I70e34e80cad67536414c71facbe0222dd88c65a7

--- include/o3tl/hash_combine.hxx
+++ include/o3tl/hash_combine.hxx
@@ -12,10 +12,32 @@
 namespace o3tl
 {
 template <typename T, typename N>
+inline std::enable_if_t<(sizeof(N) == 4)> hash_combine(N& nSeed, T const* pValue, size_t nCount)
+{
+    static_assert(sizeof(nSeed) == 4);
+    for (size_t i = 0; i < nCount; ++i)
+    {
+        nSeed ^= std::hash<T>{}(*pValue) + 0x9E3779B9u + (nSeed << 6) + (nSeed >> 2);
+        ++pValue;
+    }
+}
+
+template <typename T, typename N>
 inline std::enable_if_t<(sizeof(N) == 4)> hash_combine(N& nSeed, T const& nValue)
 {
     static_assert(sizeof(nSeed) == 4);
     nSeed ^= std::hash<T>{}(nValue) + 0x9E3779B9u + (nSeed << 6) + (nSeed >> 2);
+}
+
+template <typename T, typename N>
+inline std::enable_if_t<(sizeof(N) == 8)> hash_combine(N& nSeed, T const* pValue, size_t nCount)
+{
+    static_assert(sizeof(nSeed) == 8);
+    for (size_t i = 0; i < nCount; ++i)
+    {
+        nSeed ^= std::hash<T>{}(*pValue) + 0x9E3779B97F4A7C15llu + (nSeed << 12) + (nSeed >> 4);
+        ++pValue;
+    }
 }
 
 template <typename T, typename N>
