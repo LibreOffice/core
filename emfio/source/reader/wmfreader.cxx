e41857da843d28eb53a037061a63794dffe23abd
Hossein <hossein@libreoffice.org>
2021-08-30 10:41:46 +0200

    tdf#88163 Fix WMF font size
    The problems in tdf#88163 can be categorized into two parts:
    
    1. A regression which is caused by the commit
    a9020e461803964a206d5551884b70717eed165c. The font size for text is
    wrongly calculated for wmf files WITHOUT the placeable header.
    
    2. A long-lasting bug that has existed from LO 3.5 (tested with
    various LibreOffice versions from 3.5 to master). The font size for
    text is wrongly calculated for wmf files WITH the placeable header.
    
    This patch solves the first part. In this patch the way wmf is scaled
    is changed using mnUnitsPerInch as a variable that controls the scale.
    The previous method was dividing the left/right/top/bottom fields of
    aPlaceableBound which caused the bad scaling of text.
    
    The second problem still remains, and possibly can be solved by usign
    the old scaling method that was previously used here (dividing pos
    values of aPlaceableBound), but the scaling factor should be
    calculated, which varies in different wmf files. More study should be
    done to solve this part.
    
    Values used for the example "visio_import_source.wmf" used in the test
    WmfTest::testNonPlaceableWmf() are slightly changed, but the rendering
    should not noticeably change, except the fix for the text font size.
    
    Change-Id: I55bf38ba4345a0aa48c3e522976a2a5c32f7ec8c

--- emfio/source/reader/wmfreader.cxx
+++ emfio/source/reader/wmfreader.cxx
@@ -1462,7 +1462,7 @@ namespace emfio
             {
                 mpInputStream->Seek(nStrmPos + 18);    // set the streampos to the start of the metaactions
                 GetPlaceableBound(aPlaceableBound, mpInputStream);
-
+              
                 // The image size is not known so normalize the calculated bounds so that the
                 // resulting image is not too big
                 if (aPlaceableBound.GetWidth() > aMaxWidth)
@@ -1470,16 +1470,13 @@ namespace emfio
                     const double fMaxWidth = static_cast<double>(aMaxWidth);
                     double fRatio = aPlaceableBound.GetWidth() / fMaxWidth;
 
-                    aPlaceableBound = tools::Rectangle(
-                        aPlaceableBound.Left() / fRatio,
-                        aPlaceableBound.Top() / fRatio,
-                        aPlaceableBound.Right() / fRatio,
-                        aPlaceableBound.Bottom() / fRatio);
+                    // changing mnUnitsPerInch as a tool to scale wmf
+                    mnUnitsPerInch *= fRatio;
 
                     SAL_INFO("emfio", "Placeable bounds "
                         " left: " << aPlaceableBound.Left() << " top: " << aPlaceableBound.Top()
                         << " right: " << aPlaceableBound.Right() << " bottom: " << aPlaceableBound.Bottom());
-                }
+                }              
             }
 
             mpInputStream->Seek( nStrmPos );
