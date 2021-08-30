d913ad0aa9b69b957703e90fc355bfd77d6d3515
Hossein <hossein@libreoffice.org>
2021-08-30 10:41:46 +0200

    tdf#88163 Fix WMF font size
    Fixed the scale, mnUnitsPerInch, and aPlaceableBound
    
    Change-Id: I55bf38ba4345a0aa48c3e522976a2a5c32f7ec8c

--- emfio/source/reader/wmfreader.cxx
+++ emfio/source/reader/wmfreader.cxx
@@ -1431,15 +1431,21 @@ namespace emfio
             // inch
             mpInputStream->ReadUInt16( mnUnitsPerInch );
 
-            // reserved
-            mpInputStream->SeekRel( 4 );
+           int scale = mpInputStream->SeekRel( 4 );
+
+           double fRatio = static_cast<double>(scale);
+           aPlaceableBound = tools::Rectangle(
+               aPlaceableBound.Left() * ( fRatio / mnUnitsPerInch * 1440 / 96 ) ,
+               aPlaceableBound.Top() * ( fRatio / mnUnitsPerInch * 1440 / 96 ) ,
+               aPlaceableBound.Right() * ( fRatio / mnUnitsPerInch * 1440 / 96 ) ,
+               aPlaceableBound.Bottom() * ( fRatio / mnUnitsPerInch * 1440 / 96 ) );
 
             // Skip and don't check the checksum
             mpInputStream->SeekRel( 2 );
         }
         else
         {
-            mnUnitsPerInch = 96;
+            mnUnitsPerInch = 1440;
 
             if (mpExternalHeader != nullptr
                 && mpExternalHeader->xExt > 0
@@ -1462,24 +1468,6 @@ namespace emfio
             {
                 mpInputStream->Seek(nStrmPos + 18);    // set the streampos to the start of the metaactions
                 GetPlaceableBound(aPlaceableBound, mpInputStream);
-
-                // The image size is not known so normalize the calculated bounds so that the
-                // resulting image is not too big
-                if (aPlaceableBound.GetWidth() > aMaxWidth)
-                {
-                    const double fMaxWidth = static_cast<double>(aMaxWidth);
-                    double fRatio = aPlaceableBound.GetWidth() / fMaxWidth;
-
-                    aPlaceableBound = tools::Rectangle(
-                        aPlaceableBound.Left() / fRatio,
-                        aPlaceableBound.Top() / fRatio,
-                        aPlaceableBound.Right() / fRatio,
-                        aPlaceableBound.Bottom() / fRatio);
-
-                    SAL_INFO("emfio", "Placeable bounds "
-                        " left: " << aPlaceableBound.Left() << " top: " << aPlaceableBound.Top()
-                        << " right: " << aPlaceableBound.Right() << " bottom: " << aPlaceableBound.Bottom());
-                }
             }
 
             mpInputStream->Seek( nStrmPos );
