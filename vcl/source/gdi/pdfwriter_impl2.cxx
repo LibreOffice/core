88ec0521a74b31c3166c27d0161b2d943395e2fc
Chris Sherlock <chris.sherlock79@gmail.com>
2021-08-31 03:51:23 +1000

    vcl: move RemoveTransparenciesFromMetaFile() to outdev/transparent.cxx
    Don't know why an OutputDevice function was used in print2.cxx, but
    definitely needs to be in the outdev source directory, and 
    transparent.cxx is the most appropriate place to add this.
    
    To be honest, that function only operates on metafiles, so perhaps we
    should make this part of GDIMetafile, so added a TODO.
    
    Also, a number of local header files (all to do with PDFs) needed to 
    become module local headers, so I have moved these to the vcl/inc/pdf 
    directory.
    
    Change-Id: I1feb8e5e8aedc7399740a035beacd2dad3de317f

--- vcl/source/gdi/pdfwriter_impl2.cxx
+++ vcl/source/gdi/pdfwriter_impl2.cxx
@@ -17,7 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#include "pdfwriter_impl.hxx"
+#include <pdf/pdfwriter_impl.hxx>
 
 #include <vcl/pdfextoutdevdata.hxx>
 #include <vcl/virdev.hxx>
