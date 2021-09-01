81b7c565a45ec538f04db32d218fcfd6af78711a
Tünde Tóth <toth.tunde@nisz.hu>
2021-09-01 15:47:40 +0200

    tdf#115933 XLSX import: fix permission for editing
    The passwords for editing in XLSX documents
    created with Excel weren't asked and verified.
    
    Note: LibreOffice supports only a subset of the hashing
    algorithms specified in MS-OE376, according to
    DocPasswordHelper::GetOoxHashAsVector() and
    https://docs.microsoft.com/en-us/openspecs/office_standards/ms-oe376/f70a4140-340b-4e94-a604-dff25b9846b1.
    Also the documents encrypted with unsupported algorithms
    got edit protection now, but it's not possible to add
    permission to edit them (copy of these documents are still
    editable).
    
    Change-Id: Iabc90f6bba4ed071dd2c60e9dea905481816964b

---
+++ sc/qa/uitest/calc_tests9/tdf115933.py
@@ -0,0 +1,32 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+from uitest.framework import UITestCase
+from uitest.uihelper.common import get_url_for_data_file
+from libreoffice.uno.propertyvalue import mkPropertyValues
+
+#Bug 115933 - XLSX <fileSharing> password protected with algorithmName, hashValue, saltValue and spinCount
+
+class tdf115933(UITestCase):
+
+    def test_tdf115933(self):
+        with self.ui_test.load_file(get_url_for_data_file("tdf115933.xlsx")):
+            #The document was created in Excel.
+            calcDoc = self.xUITest.getTopFocusWindow()
+            gridwin = calcDoc.getChild("grid_window")
+
+            document = self.ui_test.get_component()
+
+            self.assertTrue(document.isReadonly())
+
+            #Without the fix in place, this dialog wouldn't have been displayed
+            with self.ui_test.execute_dialog_through_action(gridwin, "TYPE", mkPropertyValues({"KEYCODE": "CTRL+SHIFT+M"})) as xDialog:
+                xPassword = xDialog.getChild("newpassEntry")
+                xPassword.executeAction("TYPE", mkPropertyValues({"TEXT": "a"}))
+
+            self.assertFalse(document.isReadonly())
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
