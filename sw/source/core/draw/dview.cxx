64e83199f3cc7217b0a58ddc07200b7d3cba58e1
Attila Bakos (NISZ) <bakos.attilakaroly@nisz.hu>
2021-09-01 16:38:07 +0200

    tdf#143574 sw: textboxes in group shapes - part 2
    Introduce sync of textboxes with group shapes and
    make possible to add a textbox to a group shape.
    This patch also fixes a memory leak, what has been
    introduced by:
    
    504d78acb866495fd954fcd6db22ea68f174a5ab
    tdf#143574 sw: textboxes in group shapes - part 1
    
    Known issues:
    *As_Char anchoring is far not the best
    *Import/export missing, follow up commit will intruduce
    *Group/Ungroup not handled yet, follow up will
    introduce
    
    Change-Id: I7dc3b8d36c4a04f792ae4742fe4a45af9227a17e

--- sw/source/core/draw/dview.cxx
+++ sw/source/core/draw/dview.cxx
@@ -967,8 +967,16 @@ void SwDrawView::DeleteMarked()
         SdrObject *pObject = rMarkList.GetMark(i)->GetMarkedSdrObj();
         SwContact* pContact = GetUserCall(pObject);
         SwFrameFormat* pFormat = pContact->GetFormat();
-        if (SwFrameFormat* pTextBox = SwTextBoxHelper::getOtherTextBoxFormat(pFormat, RES_DRAWFRMFMT))
-            aTextBoxesToDelete.push_back(pTextBox);
+        if (auto pChildren = pObject->getChildrenOfSdrObject())
+        {
+            for (size_t i = 0; i < pChildren->GetObjCount(); ++i)
+                if (SwFrameFormat* pTextBox = SwTextBoxHelper::getOtherTextBoxFormat(
+                        pFormat, RES_DRAWFRMFMT, pChildren->GetObj(i)))
+                    aTextBoxesToDelete.push_back(pTextBox);
+        }
+        else
+            if (SwFrameFormat* pTextBox = SwTextBoxHelper::getOtherTextBoxFormat(pFormat, RES_DRAWFRMFMT))
+                aTextBoxesToDelete.push_back(pTextBox);
     }
 
     if ( pDoc->DeleteSelection( *this ) )
