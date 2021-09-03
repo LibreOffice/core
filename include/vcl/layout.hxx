99141aea4d81d89ff5d14087e6647b25f7018e8b
Caolán McNamara <caolanm@redhat.com>
2021-09-03 21:16:13 +0100

    Related: tdf#142458 don't change border size during Paint
    so GetOptimalSize/get_preferred_size before/after paint has the same result
    
    Change-Id: I25abe31c069561e30a9af7c4b9b81582298e0f9d
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/121616
    Tested-by: Caolán McNamara <caolanm@redhat.com>
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>

--- include/vcl/layout.hxx
+++ include/vcl/layout.hxx
@@ -500,8 +500,6 @@ public:
 private:
     virtual Size calculateRequisition() const override;
     virtual void setAllocation(const Size &rAllocation) override;
-    // sets new border size and adapts scrollbar and child widget position/size as needed
-    void updateBorderWidth(tools::Long nBorderWidth);
     int CalcBorderWidth() const;
     DECL_LINK(ScrollBarHdl, ScrollBar*, void);
     void InitScrollBars(const Size &rRequest);
