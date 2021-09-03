bdc1eaf5fba789adee31e96b8ebbdef774c9dc28
Caolán McNamara <caolanm@redhat.com>
2021-09-03 17:27:54 +0100

    tdf#142458 Set correct ScrolledWindow BorderWidth in the ctor
    and keep it updated when the relevant properties that affect it change.
    This avoids the problem that the borderwidth can change during "Paint"
    (which is something we strongly want to avoid) since...
    
    since:
    
    commit f39f21d92ec83c3a5062f29dd26214fc83012c06
    Date:   Thu Nov 19 11:51:13 2020 +0100
    
        tdf#138010 (IV) VclScrolledWindow: Use actual border width
    
    hopefully after this we can then double-check to see if we can remove
    the potential change of BorderWidth during paint.
    
    Change-Id: I2ec317d86687fdb75e6323905f6d1c3b8fc655e1

--- include/vcl/layout.hxx
+++ include/vcl/layout.hxx
@@ -502,6 +502,7 @@ private:
     virtual void setAllocation(const Size &rAllocation) override;
     // sets new border size and adapts scrollbar and child widget position/size as needed
     void updateBorderWidth(tools::Long nBorderWidth);
+    int CalcBorderWidth() const;
     DECL_LINK(ScrollBarHdl, ScrollBar*, void);
     void InitScrollBars(const Size &rRequest);
     virtual bool EventNotify(NotifyEvent& rNEvt) override;
