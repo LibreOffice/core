fba98ebfac9ff485717e58fe930a595858b24692
Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>
2021-09-04 20:29:28 +0300

    Revert "tdf#142214: autocomplete: do not search across empty blocks"
    This reverts commit ca2ec443893731093970914feb750b31ea13e47f.
    
    Breaks user experience.
    
    Removed ScTiledRenderingTest::testAutoInputStringBlock as well.
    
    Change-Id: Id03bc645d78c29c0aa588b2133fc1cb96f3cceb3
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/121640
    Tested-by: Jenkins
    Reviewed-by: Heiko Tietze <heiko.tietze@documentfoundation.org>
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Reviewed-by: Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>
    (cherry picked from commit 223f3a6fac43580114bca86abb34d7cf3219f4bc)

--- sc/source/core/data/column3.cxx
+++ sc/source/core/data/column3.cxx
@@ -2562,11 +2562,6 @@ public:
         return (maPos.first->type == sc::element_type_string || maPos.first->type == sc::element_type_edittext);
     }
 
-    bool isEmpty() const
-    {
-        return maPos.first->type == sc::element_type_empty;
-    }
-
     bool prev()
     {
         if (!has())
@@ -2574,7 +2569,7 @@ public:
             // Not in a string block. Move back until we hit a string block.
             while (!has())
             {
-                if (isEmpty() || maPos.first == miBeg)
+                if (maPos.first == miBeg)
                     return false;
 
                 --maPos.first; // move to the preceding block.
@@ -2600,10 +2595,6 @@ public:
                 // Move to the last cell of the previous block.
                 --maPos.first;
                 maPos.second = maPos.first->size - 1;
-
-                if (isEmpty())
-                    return false;
-
                 if (has())
                     break;
             }
@@ -2618,9 +2609,6 @@ public:
             // Not in a string block. Move forward until we hit a string block.
             while (!has())
             {
-                if (isEmpty())
-                    return false;
-
                 ++maPos.first;
                 if (maPos.first == miEnd)
                     return false;
@@ -2642,10 +2630,6 @@ public:
                     return false;
 
                 maPos.second = 0;
-
-                if (isEmpty())
-                    return false;
-
                 if (has())
                     break;
             }
@@ -2681,12 +2665,16 @@ bool ScColumn::GetDataEntries(
     // going upward and downward directions in parallel. The start position
     // cell must be skipped.
 
-    StrCellIterator aItrUp(maCells, nStartRow-1, &GetDoc());
+    StrCellIterator aItrUp(maCells, nStartRow, &GetDoc());
     StrCellIterator aItrDown(maCells, nStartRow+1, &GetDoc());
 
     bool bMoveUp = aItrUp.valid();
-    if (bMoveUp && !aItrUp.has())
-        bMoveUp = aItrUp.prev(); // Find the previous string cell position.
+    if (!bMoveUp)
+        // Current cell is invalid.
+        return false;
+
+    // Skip the start position cell.
+    bMoveUp = aItrUp.prev(); // Find the previous string cell position.
 
     bool bMoveDown = aItrDown.valid();
     if (bMoveDown && !aItrDown.has())
