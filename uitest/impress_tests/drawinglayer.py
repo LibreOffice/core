443f7040b07671b36cfe1b70adb0717307cfb8df
Rene Engelhard <rene@debian.org>
2021-09-04 19:58:13 +0200

    work around off-by-ones on i386 in uicheck uitests...
    <@x1sc0> _rene__, I guess we could use assertAlmostEqual with delta=1 there,
             but maybe once we add it, the next assert also fails for you?
    
    Change-Id: Ic64641ccdf9ca721997b4a842b646cf4b9e30f16

--- uitest/impress_tests/drawinglayer.py
+++ uitest/impress_tests/drawinglayer.py
@@ -69,7 +69,7 @@ class ImpressDrawinglayerTest(UITestCase):
             self.assertEqual(25199, document.DrawPages[0].getByIndex(0).Size.Width)
             self.assertEqual(2629, document.DrawPages[0].getByIndex(0).Size.Height)
             self.assertEqual(25199, document.DrawPages[0].getByIndex(1).Size.Width)
-            self.assertEqual(9134, document.DrawPages[0].getByIndex(1).Size.Height)
+            self.assertAlmostEqual(9134, document.DrawPages[0].getByIndex(1).Size.Height,delta=1)
 
             xEditWin = xImpressDoc.getChild("impress_win")
 
@@ -79,7 +79,7 @@ class ImpressDrawinglayerTest(UITestCase):
             self.assertEqual(25199, document.DrawPages[0].getByIndex(0).Size.Width)
             self.assertEqual(2629, document.DrawPages[0].getByIndex(0).Size.Height)
             self.assertEqual(12600, document.DrawPages[0].getByIndex(1).Size.Width)
-            self.assertEqual(4568, document.DrawPages[0].getByIndex(1).Size.Height)
+            self.assertAlmostEqual(4568, document.DrawPages[0].getByIndex(1).Size.Height,delta=1)
 
             self.assertIsNone(document.CurrentSelection)
 
@@ -110,7 +110,7 @@ class ImpressDrawinglayerTest(UITestCase):
             self.assertEqual(25199, document.DrawPages[0].getByIndex(0).Size.Width)
             self.assertEqual(2629, document.DrawPages[0].getByIndex(0).Size.Height)
             self.assertEqual(25199, document.DrawPages[0].getByIndex(1).Size.Width)
-            self.assertEqual(9134, document.DrawPages[0].getByIndex(1).Size.Height)
+            self.assertAlmostEqual(9134, document.DrawPages[0].getByIndex(1).Size.Height,delta=1)
             self.assertEqual(0, document.DrawPages[0].getByIndex(1).RotateAngle)
 
             xEditWin = xImpressDoc.getChild("impress_win")
@@ -121,7 +121,7 @@ class ImpressDrawinglayerTest(UITestCase):
             self.assertEqual(25199, document.DrawPages[0].getByIndex(0).Size.Width)
             self.assertEqual(2629, document.DrawPages[0].getByIndex(0).Size.Height)
             self.assertEqual(25199, document.DrawPages[0].getByIndex(1).Size.Width)
-            self.assertEqual(9134, document.DrawPages[0].getByIndex(1).Size.Height)
+            self.assertAlmostEqual(9134, document.DrawPages[0].getByIndex(1).Size.Height,delta=1)
             self.assertEqual(3000, document.DrawPages[0].getByIndex(1).RotateAngle)
 
             self.assertIsNone(document.CurrentSelection)
