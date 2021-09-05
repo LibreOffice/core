LibreOffice/core

commit 426930d0c4bd6f782a04a92e8a36e92cd65e186f

speedup dynamic_cast to SwTextFrame
which is a hotspot in loading some documents

Change-Id: I7f1c48305cd40bcd9bee4baf219228f10cc297b3
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/121184
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sw/source/core/docnode/ndsect.cxx
@@ -1122,8 +1122,8 @@ void SwSectionNode::MakeFramesForAdjacentContentNode(const SwNodeIndex & rIdx)
                          pViewShell->GetLayout()->IsAnyShellAccessible() )
                     {
                         pViewShell->InvalidateAccessibleParaFlowRelation(
-                            dynamic_cast<SwTextFrame*>(pNew->FindNextCnt( true )),
-                            dynamic_cast<SwTextFrame*>(pNew->FindPrevCnt()) );
+                            pNew->FindNextCnt( true )->DynCastTextFrame(),
+                            pNew->FindPrevCnt()->DynCastTextFrame() );
                     }
                 }
                 pNew = pSct;
@@ -1148,8 +1148,8 @@ void SwSectionNode::MakeFramesForAdjacentContentNode(const SwNodeIndex & rIdx)
                      pViewShell->GetLayout()->IsAnyShellAccessible() )
                 {
                     pViewShell->InvalidateAccessibleParaFlowRelation(
-                        dynamic_cast<SwTextFrame*>(pNew->FindNextCnt( true )),
-                        dynamic_cast<SwTextFrame*>(pNew->FindPrevCnt()) );
+                        pNew->FindNextCnt( true )->DynCastTextFrame(),
+                        pNew->FindPrevCnt()->DynCastTextFrame() );
                 }
             }
             if ( bInitNewSect )

sw/source/core/docnode/ndtbl.cxx
@@ -2433,8 +2433,8 @@ void SwTableNode::MakeOwnFrames(SwNodeIndex* pIdxBehind)
                  pViewShell->GetLayout()->IsAnyShellAccessible() )
             {
                 pViewShell->InvalidateAccessibleParaFlowRelation(
-                            dynamic_cast<SwTextFrame*>(pNew->FindNextCnt( true )),
-                            dynamic_cast<SwTextFrame*>(pNew->FindPrevCnt()) );
+                            pNew->FindNextCnt( true )->DynCastTextFrame(),
+                            pNew->FindPrevCnt()->DynCastTextFrame() );
             }
         }
         pNew->RegistFlys();
@@ -2468,8 +2468,8 @@ void SwTableNode::DelFrames(SwRootFrame const*const pLayout)
                          pViewShell->GetLayout()->IsAnyShellAccessible() )
                     {
                         pViewShell->InvalidateAccessibleParaFlowRelation(
-                            dynamic_cast<SwTextFrame*>(pFrame->FindNextCnt( true )),
-                            dynamic_cast<SwTextFrame*>(pFrame->FindPrevCnt()) );
+                            pFrame->FindNextCnt( true )->DynCastTextFrame(),
+                            pFrame->FindPrevCnt()->DynCastTextFrame() );
                     }
                 }
                 pFrame->Cut();

sw/source/core/docnode/node.cxx
@@ -1393,8 +1393,8 @@ void SwContentNode::MakeFramesForAdjacentContentNode(SwContentNode& rNode)
                  pViewShell->GetLayout()->IsAnyShellAccessible() )
             {
                 pViewShell->InvalidateAccessibleParaFlowRelation(
-                            dynamic_cast<SwTextFrame*>(pNew->FindNextCnt( true )),
-                            dynamic_cast<SwTextFrame*>(pNew->FindPrevCnt()) );
+                            pNew->FindNextCnt( true )->DynCastTextFrame(),
+                            pNew->FindPrevCnt()->DynCastTextFrame() );
             }
         }
     }
@@ -1486,8 +1486,8 @@ void SwContentNode::DelFrames(SwRootFrame const*const pLayout)
                  pViewShell->GetLayout()->IsAnyShellAccessible() )
             {
                 pViewShell->InvalidateAccessibleParaFlowRelation(
-                            dynamic_cast<SwTextFrame*>(pFrame->FindNextCnt( true )),
-                            dynamic_cast<SwTextFrame*>(pFrame->FindPrevCnt()) );
+                            pFrame->FindNextCnt( true )->DynCastTextFrame(),
+                            pFrame->FindPrevCnt()->DynCastTextFrame() );
             }
         }
 

sw/source/core/layout/frmtool.cxx
@@ -1589,8 +1589,8 @@ void InsertCnt_( SwLayoutFrame *pLay, SwDoc *pDoc,
                      pFrame->FindPageFrame() != nullptr)
                 {
                     pViewShell->InvalidateAccessibleParaFlowRelation(
-                        dynamic_cast<SwTextFrame*>(pFrame->FindNextCnt( true )),
-                        dynamic_cast<SwTextFrame*>(pFrame->FindPrevCnt()) );
+                        pFrame->FindNextCnt( true )->DynCastTextFrame(),
+                        pFrame->FindPrevCnt()->DynCastTextFrame() );
                     // #i68958#
                     // The information flags of the text frame are validated
                     // in methods <FindNextCnt(..)> and <FindPrevCnt(..)>.
@@ -1675,8 +1675,8 @@ void InsertCnt_( SwLayoutFrame *pLay, SwDoc *pDoc,
                      pFrame->FindPageFrame() != nullptr)
                 {
                     pViewShell->InvalidateAccessibleParaFlowRelation(
-                            dynamic_cast<SwTextFrame*>(pFrame->FindNextCnt( true )),
-                            dynamic_cast<SwTextFrame*>(pFrame->FindPrevCnt()) );
+                            pFrame->FindNextCnt( true )->DynCastTextFrame(),
+                            pFrame->FindPrevCnt()->DynCastTextFrame() );
                 }
             }
             if ( bObjsDirect && !pTable->empty() )
@@ -1787,8 +1787,8 @@ void InsertCnt_( SwLayoutFrame *pLay, SwDoc *pDoc,
                          pFrame->FindPageFrame() != nullptr)
                     {
                         pViewShell->InvalidateAccessibleParaFlowRelation(
-                            dynamic_cast<SwTextFrame*>(pFrame->FindNextCnt( true )),
-                            dynamic_cast<SwTextFrame*>(pFrame->FindPrevCnt()) );
+                            pFrame->FindNextCnt( true )->DynCastTextFrame(),
+                            pFrame->FindPrevCnt()->DynCastTextFrame() );
                     }
                 }
                 pFrame->CheckDirChange();
@@ -2195,7 +2195,7 @@ SwBorderAttrs::SwBorderAttrs(const sw::BorderCacheOwner* pOwner, const SwFrame* 
     , m_nLineSpacing(0)
 {
     // #i96772#
-    const SwTextFrame* pTextFrame = dynamic_cast<const SwTextFrame*>(pConstructor);
+    const SwTextFrame* pTextFrame = pConstructor->DynCastTextFrame();
     if ( pTextFrame )
     {
         pTextFrame->GetTextNodeForParaProps()->ClearLRSpaceItemDueToListLevelIndents( m_rLR );

sw/source/core/text/frmform.cxx
@@ -669,7 +669,7 @@ SwContentFrame *SwTextFrame::JoinFrame()
              pViewShell->GetLayout()->IsAnyShellAccessible() )
         {
             pViewShell->InvalidateAccessibleParaFlowRelation(
-                            dynamic_cast<SwTextFrame*>(pFoll->FindNextCnt( true )),
+                            pFoll->FindNextCnt( true )->DynCastTextFrame(),
                             this );
         }
     }
@@ -703,7 +703,7 @@ void SwTextFrame::SplitFrame(TextFrameIndex const nTextPos)
              pViewShell->GetLayout()->IsAnyShellAccessible() )
         {
             pViewShell->InvalidateAccessibleParaFlowRelation(
-                            dynamic_cast<SwTextFrame*>(pNew->FindNextCnt( true )),
+                            pNew->FindNextCnt( true )->DynCastTextFrame(),
                             this );
         }
     }

sw/source/core/access/accmap.cxx
@@ -3419,8 +3419,7 @@ void SwAccessibleMap::InvalidateTextSelectionOfAllParas()
                                 static_cast<SwAccessibleContext*>( xAcc.get() ) );
                     if ( xAccImpl.is() && xAccImpl->GetFrame() )
                     {
-                        const SwTextFrame* pTextFrame(
-                            dynamic_cast<const SwTextFrame*>(xAccImpl->GetFrame()) );
+                        const SwTextFrame* pTextFrame = xAccImpl->GetFrame()->DynCastTextFrame();
                         OSL_ENSURE( pTextFrame,
                                 "<SwAccessibleMap::_SubmitTextSelectionChangedEvents()> - unexpected type of frame" );
                         if ( pTextFrame )
@@ -3448,8 +3447,7 @@ void SwAccessibleMap::InvalidateTextSelectionOfAllParas()
                         static_cast<SwAccessibleContext*>( xAcc.get() ) );
             if ( xAccImpl.is() && xAccImpl->GetFrame() )
             {
-                const SwTextFrame* pTextFrame(
-                        dynamic_cast<const SwTextFrame*>(xAccImpl->GetFrame()) );
+                const SwTextFrame* pTextFrame = xAccImpl->GetFrame()->DynCastTextFrame();
                 OSL_ENSURE( pTextFrame,
                         "<SwAccessibleMap::_SubmitTextSelectionChangedEvents()> - unexpected type of frame" );
                 if ( pTextFrame )

sw/source/core/access/accpara.cxx
@@ -718,7 +718,7 @@ lang::Locale SAL_CALL SwAccessibleParagraph::getLocale()
 {
     SolarMutexGuard aGuard;
 
-    const SwTextFrame *pTextFrame = dynamic_cast<const SwTextFrame*>( GetFrame()  );
+    const SwTextFrame *pTextFrame = GetFrame()->DynCastTextFrame();
     if( !pTextFrame )
     {
         throw uno::RuntimeException("no SwTextFrame", static_cast<cppu::OWeakObject*>(this));
@@ -738,7 +738,7 @@ uno::Reference<XAccessibleRelationSet> SAL_CALL SwAccessibleParagraph::getAccess
 
     rtl::Reference<utl::AccessibleRelationSetHelper> pHelper = new utl::AccessibleRelationSetHelper();
 
-    const SwTextFrame* pTextFrame = dynamic_cast<const SwTextFrame*>(GetFrame());
+    const SwTextFrame* pTextFrame = GetFrame()->DynCastTextFrame();
     OSL_ENSURE( pTextFrame,
             "<SwAccessibleParagraph::getAccessibleRelationSet()> - missing text frame");
     if ( pTextFrame )

sw/source/core/frmedt/feshview.cxx
@@ -1450,8 +1450,7 @@ bool SwFEShell::ShouldObjectBeSelected(const Point& rPt)
                     {
                         if ( pContentFrame->UnionFrame().IsInside( rPt ) )
                         {
-                            const SwTextFrame* pTextFrame =
-                                    dynamic_cast<const SwTextFrame*>(pContentFrame);
+                            const SwTextFrame* pTextFrame = pContentFrame->DynCastTextFrame();
                             if ( pTextFrame )
                             {
                                 SwPosition aPos(GetDoc()->GetNodes());

commit c627e82a870e0d7bc7f865631685e42f6c133903

tdf#144304 Fix crashes from "speedup dynamic_cast to SwTextFrame"
Also preemptively adjust cases where FindNextCnt(true) or
FindPrevCnt() can be null.

Regressions from 426930d0c4bd6f782a04a92e8a36e92cd65e186f.

Change-Id: Idb91464ecd285f6f5cf2e399c00faa2dfbbd5601

sw/source/core/docnode/ndsect.cxx
@@ -1121,9 +1121,11 @@ void SwSectionNode::MakeFramesForAdjacentContentNode(const SwNodeIndex & rIdx)
                     if ( pViewShell && pViewShell->GetLayout() &&
                          pViewShell->GetLayout()->IsAnyShellAccessible() )
                     {
+                        auto pNext = pNew->FindNextCnt( true );
+                        auto pPrev = pNew->FindPrevCnt();
                         pViewShell->InvalidateAccessibleParaFlowRelation(
-                            pNew->FindNextCnt( true )->DynCastTextFrame(),
-                            pNew->FindPrevCnt()->DynCastTextFrame() );
+                            pNext ? pNext->DynCastTextFrame() : nullptr,
+                            pPrev ? pPrev->DynCastTextFrame() : nullptr );
                     }
                 }
                 pNew = pSct;
@@ -1147,9 +1149,11 @@ void SwSectionNode::MakeFramesForAdjacentContentNode(const SwNodeIndex & rIdx)
                 if ( pViewShell && pViewShell->GetLayout() &&
                      pViewShell->GetLayout()->IsAnyShellAccessible() )
                 {
+                    auto pNext = pNew->FindNextCnt( true );
+                    auto pPrev = pNew->FindPrevCnt();
                     pViewShell->InvalidateAccessibleParaFlowRelation(
-                        pNew->FindNextCnt( true )->DynCastTextFrame(),
-                        pNew->FindPrevCnt()->DynCastTextFrame() );
+                        pNext ? pNext->DynCastTextFrame() : nullptr,
+                        pPrev ? pPrev->DynCastTextFrame() : nullptr );
                 }
             }
             if ( bInitNewSect )

sw/source/core/docnode/ndtbl.cxx
@@ -2432,9 +2432,11 @@ void SwTableNode::MakeOwnFrames(SwNodeIndex* pIdxBehind)
             if ( pViewShell && pViewShell->GetLayout() &&
                  pViewShell->GetLayout()->IsAnyShellAccessible() )
             {
+                auto pNext = pNew->FindNextCnt( true );
+                auto pPrev = pNew->FindPrevCnt();
                 pViewShell->InvalidateAccessibleParaFlowRelation(
-                            pNew->FindNextCnt( true )->DynCastTextFrame(),
-                            pNew->FindPrevCnt()->DynCastTextFrame() );
+                            pNext ? pNext->DynCastTextFrame() : nullptr,
+                            pPrev ? pPrev->DynCastTextFrame() : nullptr );
             }
         }
         pNew->RegistFlys();
@@ -2467,9 +2469,11 @@ void SwTableNode::DelFrames(SwRootFrame const*const pLayout)
                     if ( pViewShell && pViewShell->GetLayout() &&
                          pViewShell->GetLayout()->IsAnyShellAccessible() )
                     {
+                        auto pNext = pFrame->FindNextCnt( true );
+                        auto pPrev = pFrame->FindPrevCnt();
                         pViewShell->InvalidateAccessibleParaFlowRelation(
-                            pFrame->FindNextCnt( true )->DynCastTextFrame(),
-                            pFrame->FindPrevCnt()->DynCastTextFrame() );
+                            pNext ? pNext->DynCastTextFrame() : nullptr,
+                            pPrev ? pPrev->DynCastTextFrame() : nullptr );
                     }
                 }
                 pFrame->Cut();

sw/source/core/docnode/node.cxx
@@ -1487,9 +1487,11 @@ void SwContentNode::DelFrames(SwRootFrame const*const pLayout)
             if ( pViewShell && pViewShell->GetLayout() &&
                  pViewShell->GetLayout()->IsAnyShellAccessible() )
             {
+                auto pNext = pFrame->FindNextCnt( true );
+                auto pPrev = pFrame->FindPrevCnt();
                 pViewShell->InvalidateAccessibleParaFlowRelation(
-                            pFrame->FindNextCnt( true )->DynCastTextFrame(),
-                            pFrame->FindPrevCnt()->DynCastTextFrame() );
+                            pNext ? pNext->DynCastTextFrame() : nullptr,
+                            pPrev ? pPrev->DynCastTextFrame() : nullptr );
             }
         }
 

sw/source/core/layout/frmtool.cxx
@@ -1588,9 +1588,11 @@ void InsertCnt_( SwLayoutFrame *pLay, SwDoc *pDoc,
                      pViewShell->GetLayout()->IsAnyShellAccessible() &&
                      pFrame->FindPageFrame() != nullptr)
                 {
+                    auto pNext = pFrame->FindNextCnt( true );
+                    auto pPrev = pFrame->FindPrevCnt();
                     pViewShell->InvalidateAccessibleParaFlowRelation(
-                        pFrame->FindNextCnt( true )->DynCastTextFrame(),
-                        pFrame->FindPrevCnt()->DynCastTextFrame() );
+                        pNext ? pNext->DynCastTextFrame() : nullptr,
+                        pPrev ? pPrev->DynCastTextFrame() : nullptr );
                     // #i68958#
                     // The information flags of the text frame are validated
                     // in methods <FindNextCnt(..)> and <FindPrevCnt(..)>.
@@ -1674,9 +1676,11 @@ void InsertCnt_( SwLayoutFrame *pLay, SwDoc *pDoc,
                      pViewShell->GetLayout()->IsAnyShellAccessible() &&
                      pFrame->FindPageFrame() != nullptr)
                 {
+                    auto pNext = pFrame->FindNextCnt( true );
+                    auto pPrev = pFrame->FindPrevCnt();
                     pViewShell->InvalidateAccessibleParaFlowRelation(
-                            pFrame->FindNextCnt( true )->DynCastTextFrame(),
-                            pFrame->FindPrevCnt()->DynCastTextFrame() );
+                            pNext ? pNext->DynCastTextFrame() : nullptr,
+                            pPrev ? pPrev->DynCastTextFrame() : nullptr );
                 }
             }
             if ( bObjsDirect && !pTable->empty() )
@@ -1786,9 +1790,11 @@ void InsertCnt_( SwLayoutFrame *pLay, SwDoc *pDoc,
                          pViewShell->GetLayout()->IsAnyShellAccessible() &&
                          pFrame->FindPageFrame() != nullptr)
                     {
+                        auto pNext = pFrame->FindNextCnt( true );
+                        auto pPrev = pFrame->FindPrevCnt();
                         pViewShell->InvalidateAccessibleParaFlowRelation(
-                            pFrame->FindNextCnt( true )->DynCastTextFrame(),
-                            pFrame->FindPrevCnt()->DynCastTextFrame() );
+                            pNext ? pNext->DynCastTextFrame() : nullptr,
+                            pPrev ? pPrev->DynCastTextFrame() : nullptr );
                     }
                 }
                 pFrame->CheckDirChange();

sw/source/core/text/frmform.cxx
@@ -668,8 +668,9 @@ SwContentFrame *SwTextFrame::JoinFrame()
         if ( pViewShell && pViewShell->GetLayout() &&
              pViewShell->GetLayout()->IsAnyShellAccessible() )
         {
+            auto pNext = pFoll->FindNextCnt( true );
             pViewShell->InvalidateAccessibleParaFlowRelation(
-                            pFoll->FindNextCnt( true )->DynCastTextFrame(),
+                            pNext ? pNext->DynCastTextFrame() : nullptr,
                             this );
         }
     }
@@ -702,8 +703,9 @@ void SwTextFrame::SplitFrame(TextFrameIndex const nTextPos)
         if ( pViewShell && pViewShell->GetLayout() &&
              pViewShell->GetLayout()->IsAnyShellAccessible() )
         {
+            auto pNext = pNew->FindNextCnt( true );
             pViewShell->InvalidateAccessibleParaFlowRelation(
-                            pNew->FindNextCnt( true )->DynCastTextFrame(),
+                            pNext ? pNext->DynCastTextFrame() : nullptr,
                             this );
         }
     }