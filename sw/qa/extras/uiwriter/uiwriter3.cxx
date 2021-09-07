36aeb72f4a47304c9e44e66c5cd7418f14c8989e
László Németh <nemeth@numbertext.org>
2021-09-07 15:57:35 +0200

    tdf#143359 sw: track deletion of empty table rows
    Empty table rows were deleted immediately during
    change tracking, or in the case of a deleted table
    also with non-empty rows, accepting table deletion
    kept empty rows.
    
    Note: as a workaround for tracking of the empty rows,
    i.e. rows without text content, add a redline with
    invisible text ZWJ in the first cell of the empty row.
    
    See also commit a483a44ca00f43a64ae51d62b8fbb4129a413f6d
    "tdf#143215 DOCX import: fix tracked empty row insertion/deletion",
    commit b50d386dfa70f7c1d4eb1a49091ec9dd782b767b
    "tdf#142701 track changes: fix layout regression of image deletion"
    and commit 05366b8e6683363688de8708a3d88cf144c7a2bf
    "tdf#60382 sw offapi: add change tracking of table/row deletion".
    
    Note: switch off unnecessary redlining of tdf#132744 unit test
    to keep the sake of that test (i.e. cut of the selected table
    with empty rows).
    
    Change-Id: Ief59008e8714fb88afdfce867598e3dda21bfba5

--- sw/qa/extras/uiwriter/uiwriter3.cxx
+++ sw/qa/extras/uiwriter/uiwriter3.cxx
@@ -1615,6 +1615,13 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest3, testTdf132744)
     SwDoc* pDoc = createSwDoc(DATA_DIRECTORY, "tdf132744.odt");
     SwWrtShell* pWrtShell = pDoc->GetDocShell()->GetWrtShell();
 
+    // disable change tracking to cut the table
+    pDoc->getIDocumentRedlineAccess().SetRedlineFlags(RedlineFlags::ShowDelete
+                                                      | RedlineFlags::ShowInsert);
+
+    CPPUNIT_ASSERT_MESSAGE("redlining should be off",
+                           !pDoc->getIDocumentRedlineAccess().IsRedlineOn());
+
     CPPUNIT_ASSERT_EQUAL(1, getShapes());
 
     dispatchCommand(mxComponent, ".uno:SelectAll", {});
