2c0e5d8b2e53921f09b2e5036cc25f8b0a177dd6
Xisco Fauli <xiscofauli@libreoffice.org>
2021-09-08 12:13:32 +0200

    uitest: sc: factor out common code
    Change-Id: Iaba3b9633eac26516bbb4bb0b6baadb4c6772fa6
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/121808
    Tested-by: Jenkins
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- sc/qa/uitest/csv_dialog/tdf117868.py
+++ sc/qa/uitest/csv_dialog/tdf117868.py
@@ -5,41 +5,30 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 from uitest.framework import UITestCase
-from uitest.uihelper.common import get_state_as_dict, get_url_for_data_file
-from libreoffice.uno.propertyvalue import mkPropertyValues
+from uitest.uihelper.common import get_state_as_dict
 from libreoffice.calc.document import get_cell_by_position
+from libreoffice.calc.csv_dialog import load_csv_file
 
 class Td117868(UITestCase):
 
     def test_tdf117868(self):
 
-        # Load file from Open dialog
-        with self.ui_test.execute_dialog_through_command(".uno:Open", close_button="open") as xOpenDialog:
+        with load_csv_file(self, "tdf117868.csv", False) as xDialog:
+            # Set text delimiter in case it's changed by another test
+            xSeparatedBy = xDialog.getChild("toseparatedby")
+            xSeparatedBy.executeAction("CLICK", tuple())
 
-            xFileName = xOpenDialog.getChild("file_name")
-            xFileName.executeAction("TYPE", mkPropertyValues({"TEXT": get_url_for_data_file("tdf117868.csv")}))
+            # Without the fix in place, this test would have failed with
+            # AssertionError: 'true' != 'false'
+            self.assertEqual('true', get_state_as_dict(xDialog.getChild("other"))['Selected'])
+            self.assertEqual('false', get_state_as_dict(xDialog.getChild("tab"))['Selected'])
+            self.assertEqual('false', get_state_as_dict(xDialog.getChild("comma"))['Selected'])
+            self.assertEqual('false', get_state_as_dict(xDialog.getChild("semicolon"))['Selected'])
 
-        xDialog = self.ui_test.wait_for_top_focus_window('TextImportCsvDialog')
+            self.assertEqual('1', get_state_as_dict(xDialog.getChild("fromrow"))['Text'])
 
-        # Set text delimiter in case it's changed by another test
-        xSeparatedBy = xDialog.getChild("toseparatedby")
-        xSeparatedBy.executeAction("CLICK", tuple())
-
-        # Without the fix in place, this test would have failed with
-        # AssertionError: 'true' != 'false'
-        self.assertEqual('true', get_state_as_dict(xDialog.getChild("other"))['Selected'])
-        self.assertEqual('false', get_state_as_dict(xDialog.getChild("tab"))['Selected'])
-        self.assertEqual('false', get_state_as_dict(xDialog.getChild("comma"))['Selected'])
-        self.assertEqual('false', get_state_as_dict(xDialog.getChild("semicolon"))['Selected'])
-
-        self.assertEqual('1', get_state_as_dict(xDialog.getChild("fromrow"))['Text'])
-
-        xInputOther = xDialog.getChild("inputother")
-        self.assertEqual("|", get_state_as_dict(xInputOther)['Text'])
-
-        xOK = xDialog.getChild('ok')
-        with self.ui_test.wait_until_component_loaded():
-            self.ui_test.close_dialog_through_button(xOK)
+            xInputOther = xDialog.getChild("inputother")
+            self.assertEqual("|", get_state_as_dict(xInputOther)['Text'])
 
         document = self.ui_test.get_component()
 
@@ -51,5 +40,7 @@ class Td117868(UITestCase):
         self.assertEqual('bear', get_cell_by_position(document, 0, 1, 3).getString())
         self.assertEqual('c', get_cell_by_position(document, 0, 0, 4).getString())
         self.assertEqual('cow', get_cell_by_position(document, 0, 1, 4).getString())
+
+        self.ui_test.close_doc()
 
 # vim: set shiftwidth=4 softtabstop=4 expandtab:
