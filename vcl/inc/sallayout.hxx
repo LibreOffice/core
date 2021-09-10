7a95cec216aee04050ac67af0463f95b0d719c7e
Chris Sherlock <chris.sherlock79@gmail.com>
2021-09-10 22:22:07 +1000

    vcl: test PhysicalFontFamily and move to vcl::font namespace
    - Adds a series of unit tests for PhysicalFontFamily
    - Moves PhysicalFontFamily into vcl::font
    - Move PhysicalFontFamily.hxx into vcl/inc/font
    
    This is part of a series of patches where I also move font files into
    the relevant font directories, and into the vcl::font namespace.
    
    Change-Id: I9e8d7ceb5ec3494bf3ab6560645e52e88223ee69

--- vcl/inc/sallayout.hxx
+++ vcl/inc/sallayout.hxx
@@ -44,10 +44,9 @@
 
 #define MAX_FALLBACK 16
 
-
-class SalGraphics;
 class PhysicalFontFace;
 class GenericSalLayout;
+class SalGraphics;
 enum class SalLayoutFlags;
 namespace vcl::text {
     class TextLayoutCache;
