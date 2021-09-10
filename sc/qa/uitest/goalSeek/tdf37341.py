be852138fff150cb1fb4f536cf2a80a413303e1c
Xisco Fauli <xiscofauli@libreoffice.org>
2021-09-10 17:45:55 +0200

    uitest: guard execute_modeless_dialog_through_command in sc
    Change-Id: Ifd03ae26659ff56d36bfe8d4938d690ada761d5f

--- sc/qa/uitest/goalSeek/tdf37341.py
+++ sc/qa/uitest/goalSeek/tdf37341.py
@@ -19,17 +19,16 @@ class tdf37341(UITestCase):
             xCalcDoc = self.xUITest.getTopFocusWindow()
             gridwin = xCalcDoc.getChild("grid_window")
             gridwin.executeAction("SELECT", mkPropertyValues({"CELL": "F111"}))
-            self.ui_test.execute_modeless_dialog_through_command(".uno:GoalSeekDialog")
-            xDialog = self.xUITest.getTopFocusWindow()
-            xformulaedit = xDialog.getChild("formulaedit")
-            xtarget = xDialog.getChild("target")
-            xvaredit = xDialog.getChild("varedit")
-            xtarget.executeAction("TYPE", mkPropertyValues({"TEXT":"0"}))
-            xvaredit.executeAction("TYPE", mkPropertyValues({"TEXT":"E7"}))
-            xOKBtn = xDialog.getChild("ok")
+            with self.ui_test.execute_modeless_dialog_through_command_guarded(".uno:GoalSeekDialog", close_button="") as xDialog:
+                xformulaedit = xDialog.getChild("formulaedit")
+                xtarget = xDialog.getChild("target")
+                xvaredit = xDialog.getChild("varedit")
+                xtarget.executeAction("TYPE", mkPropertyValues({"TEXT":"0"}))
+                xvaredit.executeAction("TYPE", mkPropertyValues({"TEXT":"E7"}))
+                xOKBtn = xDialog.getChild("ok")
 
-            with self.ui_test.execute_blocking_action(xOKBtn.executeAction, args=('CLICK', ()), close_button="yes"):
-                pass
+                with self.ui_test.execute_blocking_action(xOKBtn.executeAction, args=('CLICK', ()), close_button="yes"):
+                    pass
 
             #verify
             self.assertEqual(get_cell_by_position(calc_doc, 0, 4, 6).getValue() > 0, True)
