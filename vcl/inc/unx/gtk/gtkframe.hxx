ea86f6ae9399b2306b20ad64ec02825fcff35661
Caolán McNamara <caolanm@redhat.com>
2021-09-10 12:31:31 +0100

    gtk4: use gtk_event_controller_key_forward to see if toplevel would use key
    Change-Id: Ia0074daab6528f561169ecaf5544fa1c969a7103

--- vcl/inc/unx/gtk/gtkframe.hxx
+++ vcl/inc/unx/gtk/gtkframe.hxx
@@ -311,7 +311,7 @@ class GtkSalFrame final : public SalFrame
     bool WindowCloseRequest();
     void DrawingAreaMotion(int nEventX, int nEventY, guint32 nTime, guint nState);
     void DrawingAreaCrossing(SalEvent nEventType, int nEventX, int nEventY, guint32 nTime, guint nState);
-    bool DrawingAreaKey(SalEvent nEventType, guint keyval, guint keycode, guint32 nTime, guint nState);
+    bool DrawingAreaKey(GtkEventControllerKey* pController, SalEvent nEventType, guint keyval, guint keycode, guint nState);
     void DrawingAreaScroll(double delta_x, double delta_y, int nEventX, int nEventY, guint32 nTime, guint nState);
 #if GTK_CHECK_VERSION(4, 0, 0)
     static void         signalMap(GtkWidget*, gpointer);
