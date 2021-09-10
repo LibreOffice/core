0fa0e42749897cf2517c75b9c2637de1b562503e
Chris Sherlock <chris.sherlock79@gmail.com>
2021-09-10 22:22:07 +1000

    vcl: test PhysicalFontFamily and move to vcl::font namespace
    - Adds a series of unit tests for PhysicalFontFamily
    - Moves PhysicalFontFamily into vcl::font
    - Move PhysicalFontFamily.hxx into vcl/inc/font
    
    This is part of a series of patches where I also move font files into
    the relevant font directories, and into the vcl::font namespace.
    
    Change-Id: I9e8d7ceb5ec3494bf3ab6560645e52e88223ee69

--- vcl/source/outdev/font.cxx
+++ vcl/source/outdev/font.cxx
@@ -128,7 +128,7 @@ int OutputDevice::GetFontFaceCollectionCount() const
 bool OutputDevice::IsFontAvailable( const OUString& rFontName ) const
 {
     ImplInitFontList();
-    PhysicalFontFamily* pFound = mxFontCollection->FindFontFamily( rFontName );
+    vcl::font::PhysicalFontFamily* pFound = mxFontCollection->FindFontFamily( rFontName );
     return (pFound != nullptr);
 }
 
@@ -792,7 +792,7 @@ vcl::Font OutputDevice::GetDefaultFont( DefaultFontType nType, LanguageType eLan
             sal_Int32     nIndex = 0;
             do
             {
-                PhysicalFontFamily* pFontFamily = pOutDev->mxFontCollection->FindFontFamily( GetNextFontToken( aSearch, nIndex ) );
+                vcl::font::PhysicalFontFamily* pFontFamily = pOutDev->mxFontCollection->FindFontFamily( GetNextFontToken( aSearch, nIndex ) );
                 if( pFontFamily )
                 {
                     AddTokenFontName( aName, pFontFamily->GetFamilyName() );
