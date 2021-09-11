a701559c6beb1df3ae3d4c477c4d7f386b8826c2
Chris Sherlock <chris.sherlock79@gmail.com>
2021-09-11 16:47:50 +1000

    tdf#103514 vcl: remove GetDevFontSizeCount() and GetDevFontSizeList()
    The OutputDevice::GetDevFontSizeList() function is only used for
    non-scalable (bitmap) fonts. We have stopped supporting bitmap fonts
    since LO 5.3, see tdf#103514: Support for bitmap-only fonts on Windows
    has been removed (Khaled Hosny)
    
    I found this when removing PhysicalFontFace::SetBitmapSize(), as this
    also removed GetHeight() and GetWidth() I chased the caller back and
    discovered the unnecessary functions.
    
    Change-Id: If840e240155c36ed351c63e3136b5b44bb058697

--- svtools/source/control/ctrltool.cxx
+++ svtools/source/control/ctrltool.cxx
@@ -754,37 +754,14 @@ const int* FontList::GetSizeAry( const FontMetric& rInfo ) const
         return aStdSizeAry;
 
     // first search fontname in order to use device from the matching font
-    OutputDevice*           pDevice = mpDev;
     ImplFontListNameInfo*   pData = ImplFindByName( rInfo.GetFamilyName() );
     if ( pData )
-        pDevice = pData->mpFirst->GetDevice();
-
-    int nDevSizeCount = pDevice->GetDevFontSizeCount( rInfo );
-    if ( !nDevSizeCount ||
-         (pDevice->GetDevFontSize( rInfo, 0 ).Height() == 0) )
-        return aStdSizeAry;
-
-    MapMode aOldMapMode = pDevice->GetMapMode();
-    MapMode aMap( MapUnit::Map10thInch, Point(), Fraction( 1, 72 ), Fraction( 1, 72 ) );
-    pDevice->SetMapMode( aMap );
-
-    int nRealCount = 0;
-    tools::Long    nOldHeight = 0;
-    mpSizeAry.reset(new int[nDevSizeCount+1] );
-    for (int i = 0; i < nDevSizeCount; ++i)
     {
-        Size aSize = pDevice->GetDevFontSize( rInfo, i );
-        if ( aSize.Height() != nOldHeight )
-        {
-            nOldHeight = aSize.Height();
-            mpSizeAry[nRealCount] = nOldHeight;
-            nRealCount++;
-        }
+        OutputDevice* pDevice = mpDev;
+        pDevice = pData->mpFirst->GetDevice();
     }
-    mpSizeAry[nRealCount] = 0;
 
-    pDevice->SetMapMode( aOldMapMode );
-    return mpSizeAry.get();
+    return aStdSizeAry;
 }
 
 struct ImplFSNameItem
