fbbab93f2694eeee26a42da6f98a241aabb2f2d3
Chris Sherlock <chris.sherlock79@gmail.com>
2021-09-12 14:41:19 +1000

    vcl: test PhysicalFontFace and move to vcl::font namespace
    - moved PhysicalFontFace.hxx to vcl/inc/font
    - moved and renamed fontselect to vcl/inc/font/FontSelectPattern
    - added PhysicalFontFace to vcl::font namespace
    - added FontSelectPattern to vcl::font::namespace
    - had to regenerate precompiled_vcl.hxx
    - refactored PhysicalFontFace::IsBetterMatch(), which works on a
      match weighting system to figure out if the font face is, as
      the functions implies, a better "match" than the one already in
      the collection.
      To refactor this I created a number of static functions that
      return a match value for specific attributes to be matched upon
    - tested PhysicalFontFace, with some extensive tests for
      IsBetterMatch()
    
    Change-Id: I860022ac244f8a827f6f9cb7ed9018c5d9c328cb

--- vcl/inc/font/PhysicalFontFace.hxx
+++ vcl/inc/font/PhysicalFontFace.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_VCL_INC_PHYSICALFONTFACE_HXX
-#define INCLUDED_VCL_INC_PHYSICALFONTFACE_HXX
+#pragma once
 
 #include <sal/config.h>
 
@@ -28,7 +27,7 @@
 #include <vcl/dllapi.h>
 #include <vcl/fontcharmap.hxx>
 
-#include "fontattributes.hxx"
+#include <fontattributes.hxx>
 
 class LogicalFontInstance;
 struct FontMatchStatus;
@@ -41,6 +40,10 @@ struct FontCapabilities;
 class PhysicalFontFamily;
 }
 
+namespace vcl::font
+{
+class FontSelectPattern;
+
 struct FontMatchStatus
 {
 public:
@@ -49,7 +52,6 @@ public:
     int                 mnWidthMatch;
     const OUString*     mpTargetStyleName;
 };
-
 
 // TODO: no more direct access to members
 // TODO: get rid of height/width for scalable fonts
@@ -77,7 +79,7 @@ protected:
     explicit PhysicalFontFace(const FontAttributes&);
 };
 
-#endif // INCLUDED_VCL_INC_PHYSICALFONTFACE_HXX
+}
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
 
