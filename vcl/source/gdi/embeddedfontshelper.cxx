4b5260a368e49f09ec2fc6ad5738ce62a4e15378
Chris Sherlock <chris.sherlock79@gmail.com>
2021-09-12 14:41:19 +1000

    vcl: test PhysicalFontFace and move to vcl::font namespace
    - tested PhysicalFontFace
    - moved PhysicalFontFace.hxx to vcl/inc/font
    - moved and renamed fontselect to vcl/inc/font/FontSelectPattern
    - added PhysicalFontFace to vcl::font namespace
    - added FontSelectPattern to vcl::font::namespace
    - had to regenerate precompiled_vcl.hxx
    
    Change-Id: I860022ac244f8a827f6f9cb7ed9018c5d9c328cb

--- vcl/source/gdi/embeddedfontshelper.cxx
+++ vcl/source/gdi/embeddedfontshelper.cxx
@@ -260,12 +260,12 @@ OUString EmbeddedFontsHelper::fontFileUrl( std::u16string_view familyName, FontF
     PhysicalFontCollection fonts;
     graphics->GetDevFontList( &fonts );
     std::unique_ptr< vcl::font::PhysicalFontFaceCollection > fontInfo( fonts.GetFontFaceCollection());
-    PhysicalFontFace* selected = nullptr;
+    vcl::font::PhysicalFontFace* selected = nullptr;
     for( int i = 0;
          i < fontInfo->Count();
          ++i )
     {
-        PhysicalFontFace* f = fontInfo->Get( i );
+        vcl::font::PhysicalFontFace* f = fontInfo->Get( i );
         if( f->GetFamilyName() == familyName )
         {
             // Ignore comparing text encodings, at least for now. They cannot be trivially compared
