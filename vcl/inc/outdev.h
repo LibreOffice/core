6fd9acbe0cfbd8396aec4647fcd7a647c1b91750
Chris Sherlock <chris.sherlock79@gmail.com>
2021-09-12 14:41:19 +1000

    vcl: test PhysicalFontFace and move to vcl::font namespace
    - tested PhysicalFontFace
    - moved PhysicalFontFace.hxx to vcl/inc/font
    - moved and renamed fontselect to vcl/inc/font/FontSelectPattern
    - added PhysicalFontFace to vcl::font namespace
    - added FontSelectPattern to vcl::font::namespace
    - had to regenerate precompiled_vcl.hxx
    
    Change-Id: I860022ac244f8a827f6f9cb7ed9018c5d9c328cb

--- vcl/inc/outdev.h
+++ vcl/inc/outdev.h
@@ -25,8 +25,8 @@
 #include <tools/gen.hxx>
 #include <vcl/vclptr.hxx>
 
+#include "font/PhysicalFontFace.hxx"
 #include "fontinstance.hxx"
-#include "PhysicalFontFace.hxx"
 #include "impfontcache.hxx"
 
 class Size;
@@ -95,7 +95,7 @@ class ImplPreMatchFontSubstitution
 :   public ImplFontSubstitution
 {
 public:
-    virtual bool FindFontSubstitute(FontSelectPattern&)  const = 0;
+    virtual bool FindFontSubstitute(vcl::font::FontSelectPattern&)  const = 0;
 };
 
 // ImplGlyphFallbackFontSubstitution
@@ -104,7 +104,7 @@ class ImplGlyphFallbackFontSubstitution
 :   public ImplFontSubstitution
 {
 public:
-    virtual bool FindFontSubstitute(FontSelectPattern&, LogicalFontInstance* pLogicalFont, OUString& rMissingCodes) const = 0;
+    virtual bool FindFontSubstitute(vcl::font::FontSelectPattern&, LogicalFontInstance* pLogicalFont, OUString& rMissingCodes) const = 0;
 };
 
 namespace vcl { struct ControlLayoutData; }
