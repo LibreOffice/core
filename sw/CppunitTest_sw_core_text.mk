2ff59573d3ac9dc6327aba5453b60a25e9006437
Miklos Vajna <vmiklos@collabora.com>
2021-09-13 19:27:48 +0200

    tdf#144305 sw: fix rendering of ruby portions with non-default ruby alignment
    Regression from 301278b656e76b6f42af5cf8a6f5c6c02acfffeb (sw: allow the
    height of a line to be larger than 65536 twips, 2021-05-20), the problem
    was that changing from sal_uInt16 to sal_uInt32 broke
    SwRubyPortion::Adjust_(), which relied on integer promotion rules to
    have a negative diff.
    
    Old storage size was smaller than int, so got promoted to signed int, so
    the result could be a small negative number.
    
    New storage size is an unsigned int, so no promotion happens, so the new
    result was a large positive number.
    
    Fix this by casting to signed int explicitly.
    
    Change-Id: I8778c1bd0d62e27c99d4ceb1bb7bc6107a179803
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/122048
    Tested-by: Jenkins
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    (cherry picked from commit 0c3e47cc2f0570a7cd8ff4889763991a86b29f26)

--- sw/CppunitTest_sw_core_text.mk
+++ sw/CppunitTest_sw_core_text.mk
@@ -40,6 +40,7 @@ $(eval $(call gb_CppunitTest_use_externals,sw_core_text,\
 $(eval $(call gb_CppunitTest_set_include,sw_core_text,\
     -I$(SRCDIR)/sw/inc \
     -I$(SRCDIR)/sw/source/core/inc \
+    -I$(SRCDIR)/sw/source/core/text \
     -I$(SRCDIR)/sw/source/uibase/inc \
     -I$(SRCDIR)/sw/qa/inc \
     $$(INCLUDE) \
