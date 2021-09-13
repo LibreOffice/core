f123b9a7b20996260010e8d52b2243b4d3fe7021
Noel Grandin <noel.grandin@collabora.co.uk>
2021-09-13 13:02:14 +0200

    loplugin:cow_wrapper
    check the classes that use o3tl::cow_wrapper so that they
    don't unnecessarity trigger copies
    
    Change-Id: I545e627598217f3e61ba2c384adb8d4f8b404829

---
+++ compilerplugins/clang/test/cow_wrapper.cxx
@@ -0,0 +1,33 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "config_clang.h"
+#include "o3tl/cow_wrapper.hxx"
+
+struct ImplBitmapPalette
+{
+    void foo() const;
+};
+
+struct BitmapPalette
+{
+    void foo1()
+    {
+        // expected-error@+1 {{calling const method on o3tl::cow_wrapper impl class via non-const pointer, rather use std::as_const to prevent triggering an unnecessary copy [loplugin:cow_wrapper]}}
+        mpImpl->foo();
+    }
+    void foo2() const
+    {
+        // no error expected
+        mpImpl->foo();
+    }
+    o3tl::cow_wrapper<ImplBitmapPalette> mpImpl;
+};
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
