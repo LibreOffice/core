d63421a6ec03739b33399286935956fab0e56f19
Chris Sherlock <chris.sherlock79@gmail.com>
2021-09-14 00:17:06 +1000

    vcl: test PhysicalFontCollection and move to vcl::font namespace
    - tested PhysicalFontCollection, noted odd behaviour with search names
      and normalization
    - moved PhysicalFontCollection.hxx to vcl/inc/font
    - moved PhysicalFontCollection into vcl::font namespace
    
    Change-Id: Ifa0c7b871c40687bd15002565d2f7a3e408218f8

--- vcl/inc/font/PhysicalFontCollection.hxx
+++ vcl/inc/font/PhysicalFontCollection.hxx
@@ -23,8 +23,9 @@
 
 #include <vcl/dllapi.h>
 
+#include "PhysicalFontFamily.hxx"
 #include "fontinstance.hxx"
-#include "font/PhysicalFontFamily.hxx"
+
 #include <array>
 
 #define MAX_GLYPHFALLBACK 16
@@ -37,6 +38,9 @@ class PreMatchFontSubstitution;
 
 // TODO: merge with ImplFontCache
 // TODO: rename to LogicalFontManager
+
+namespace vcl::font
+{
 
 class VCL_PLUGIN_PUBLIC PhysicalFontCollection final
 {
@@ -91,5 +95,7 @@ private:
     vcl::font::PhysicalFontFamily* ImplFindFontFamilyOfDefaultFont() const;
 
 };
+
+}
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
