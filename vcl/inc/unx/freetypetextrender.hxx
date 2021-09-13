12563b74a4c0d0d97cd18503c01cb1f071ff1fa1
Chris Sherlock <chris.sherlock79@gmail.com>
2021-09-14 00:17:06 +1000

    vcl: test PhysicalFontCollection and move to vcl::font namespace
    - tested PhysicalFontCollection, noted odd behaviour with search names
      and normalization
    - moved PhysicalFontCollection.hxx to vcl/inc/font
    - moved PhysicalFontCollection into vcl::font namespace
    
    Change-Id: Ifa0c7b871c40687bd15002565d2f7a3e408218f8

--- vcl/inc/unx/freetypetextrender.hxx
+++ vcl/inc/unx/freetypetextrender.hxx
@@ -44,9 +44,9 @@ public:
     virtual void                GetFontMetric( ImplFontMetricDataRef&, int nFallbackLevel ) override;
     virtual FontCharMapRef      GetFontCharMap() const override;
     virtual bool                GetFontCapabilities(vcl::FontCapabilities &rFontCapabilities) const override;
-    virtual void                GetDevFontList( PhysicalFontCollection* ) override;
+    virtual void                GetDevFontList( vcl::font::PhysicalFontCollection* ) override;
     virtual void                ClearDevFontCache() override;
-    virtual bool                AddTempDevFont( PhysicalFontCollection*, const OUString& rFileURL, const OUString& rFontName ) override;
+    virtual bool                AddTempDevFont( vcl::font::PhysicalFontCollection*, const OUString& rFileURL, const OUString& rFontName ) override;
     virtual bool                CreateFontSubset(
                                     const OUString& rToFile,
                                     const vcl::font::PhysicalFontFace*,
