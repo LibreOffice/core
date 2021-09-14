cf32c34b00878d024655bdc3ce09131120d5d670
Chris Sherlock <chris.sherlock79@gmail.com>
2021-09-14 23:18:05 +1000

    vcl: extract and fix functions from FindFontFamilyByAttributes()
    I have extracted functions to make it easier to debug font searching
    functionality:
    
    - warn that searching for "none latin" symbol families is not
      implemented
    - warn that searching on "special" scripts is not implemented
    - add a SAL_INFO to show the matching weight, needed when debugging
      matching
    
    Fixes:
    
    - Whilst testing CJK matching, I uncovered an issue. The matching
      algorithm detects that the font is a CJK font if there are any CJK
      characters in the font name. However, if you searched for a CJK font
      against a font that was not a CJK font (i.e. the family name had no
      CJK characters) then it would still match against this font family.
    
      We now check if the font being matched against was a CJK font family
      or not, if not then I reduced the testMatch value by CJK_MATCH_VALUE.
    
    - The rounded, typewriter, gothic, schoolbook and titling/captilization
      checks would always succeed, regardless of what was matched on, so
      fixed this also.
    
    - When checking if a font family is fixed, we also check if it has a
      typewriter attribute - this code was never reachable when using a font
      like typewriter or lineprinter which used by Typewriter and Fixed
      attributes, so this is fixed also.
    
    - fixed issue where when using the search parameter in
      FindFontFamilyByAttributes() would aways return the font.
    
    Change-Id: I7603f5b2996761c956c07b9b7fa21aab25abb336

--- unotools/source/config/fontcfg.cxx
+++ unotools/source/config/fontcfg.cxx
@@ -734,6 +734,7 @@ void FontSubstConfiguration::getMapName( const OUString& rOrgName, OUString& rSh
     {
         if ( ImplFindAndErase( rFamilyName, pTypeList->mpStr ) )
             rType |= pTypeList->mnType;
+
         pTypeList++;
     }
 
