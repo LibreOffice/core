37406b596e3e046cfe689ffb64c9857e59ee06bb
Henry Castro <hcastro@collabora.com>
2021-09-15 14:56:19 -0400

    lok: fix the "mbStartDragCalled" status
    The drag starts it is only called when mouse down +
    mouse move.
    
    Change-Id: Ifadfd904c2be28bd123c17a47a82f49dd6d5a04a
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/118867
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>
    Tested-by: Szymon Kłos <szymon.klos@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/117895
    Tested-by: Jenkins
    Reviewed-by: Henry Castro <hcastro@collabora.com>

--- vcl/source/window/winproc.cxx
+++ vcl/source/window/winproc.cxx
@@ -873,7 +873,6 @@ bool ImplLOKHandleMouseEvent(const VclPtr<vcl::Window>& xWindow, MouseNotifyEven
                     }
                 }
             }
-            else pFrameData->mbStartDragCalled = true;
         }
     }
 
