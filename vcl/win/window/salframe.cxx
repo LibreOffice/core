d0e734297046eb0470fdf58f9ad3c001214a797f
Jan-Marek Glogowski <glogow@fbihome.de>
2021-09-15 17:24:48 +0200

    WIN always (de-)init WinSalGraphics
    Originally I thought the whole (de-)initialization was some kind
    of lazy SalGraphics init stuff. But reading the existing code
    proved me wrong and every InitGraphics came a few lines after a
    setHDC call and DeInitGraphics before deletion or setHDC(nullptr).
    
    $ git grep -n "delete.*pGraph\|setHDC\|InitGraphics" vcl/win/
    
    So just make (De-)Init part of setHDC and drop all the other calls
    to (De-)InitGraphics, adding a setHDC(nullptr) to the destructor.
    
    I've also added some questionable asserts, like
    
    assert(pPrinter->mpGraphics->getHDC() == pPrinter->mhDC);
    
    As I read the code, you can have a printer object initialized with
    a DC but without graphics, which will be initialized on demand.
    
    But AFAIK it's invalid to have a printer DC != graphics DC.
    
    Then there was the hDC check in WinSalPrinter::EndPage, which is
    IMHO not needed. AFAIK there is no way the graphics DC might be
    "magically" invalidated, so restoring the old DC values should
    always work.
    
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/122157
    Tested-by: Jenkins
    Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>
    (cherry picked from commit 42f9d4335bfa4b7299224801fd7ccdd97ae92fbf)
    
     Conflicts:
    	vcl/inc/win/salgdi.h
    	vcl/win/gdi/salprn.cxx
    
    Change-Id: I1b961cfa733263ce773575a728bcce5c7d3e97ad
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/134514
    Tested-by: Thorsten Behrens <thorsten.behrens@allotropia.de>
    Reviewed-by: Thorsten Behrens <thorsten.behrens@allotropia.de>

--- vcl/win/window/salframe.cxx
+++ vcl/win/window/salframe.cxx
@@ -913,12 +913,11 @@ bool WinSalFrame::ReleaseFrameGraphicsDC( WinSalGraphics* pGraphics )
         return false;
     if ( pGraphics->getDefPal() )
         SelectPalette( hDC, pGraphics->getDefPal(), TRUE );
-    pGraphics->DeInitGraphics();
+    pGraphics->setHDC(nullptr);
     SendMessageW( pSalData->mpInstance->mhComWnd, SAL_MSG_RELEASEDC,
         reinterpret_cast<WPARAM>(mhWnd), reinterpret_cast<LPARAM>(hDC) );
     if ( pGraphics == mpThreadGraphics )
         pSalData->mnCacheDCInUse--;
-    pGraphics->setHDC(nullptr);
     return true;
 }
 
@@ -997,7 +996,6 @@ bool WinSalFrame::InitFrameGraphicsDC( WinSalGraphics *pGraphics, HDC hDC, HWND 
         pGraphics->setDefPal(SelectPalette( hDC, pSalData->mhDitherPal, TRUE ));
         RealizePalette( hDC );
     }
-    pGraphics->InitGraphics();
 
     if ( pGraphics == mpThreadGraphics )
         pSalData->mnCacheDCInUse++;
