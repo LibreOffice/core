c3f042f87e9125d2ce3849eedde3573f088cbc98
Chris Sherlock <chris.sherlock79@gmail.com>
2021-09-16 16:15:17 +1000

    Add sal/config.h in preparation for patch
    The convention is that we need to add sal/config.h to the start of
    files.
    
    I have a patch queued to be able to create a FontMetric from
    a PhysicalFontFace. Submitting this patch so I can hopefully one day
    land this patch.
    
    Change-Id: I5817acd6e194da576f2c8a13539d38954a4f4e15

--- toolkit/source/awt/vclxdevice.cxx
+++ toolkit/source/awt/vclxdevice.cxx
@@ -17,6 +17,8 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
+#include <sal/config.h>
+
 #include <com/sun/star/util/MeasureUnit.hpp>
 #include <com/sun/star/lang/IllegalArgumentException.hpp>
 
