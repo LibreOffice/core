22d208de2bdae5aa94462bb3ee848eba6b1c79c9
Chris Sherlock <chris.sherlock79@gmail.com>
2021-09-16 15:58:41 +1000

    Update vim modelines in preparation for new change
    I have a patch queued to be able to create a FontMetric from
    a PhysicalFontFace. Submitting this patch so I can hopefully one day
    land this patch.
    
    Change-Id: Id818efb1ee03adbad5d551db12cfede1705c1df2

--- vcl/source/outdev/font.cxx
+++ vcl/source/outdev/font.cxx
@@ -1492,4 +1492,4 @@ void OutputDevice::ImplReleaseFonts()
     mpDeviceFontSizeList.reset();
 }
 
-/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
