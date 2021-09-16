a36872637f2d52cc84934633fce5fbbe1fd18f37
Chris Sherlock <chris.sherlock79@gmail.com>
2021-09-16 17:32:41 +1000

    vcl: update vim modelines in preparation for new change
    I have a patch queued to test PhysicalFontFamily and move it to
    vcl::font. Submitting patch to update the modelines in the files
    that I will change in that patch.
    
    Change-Id: Ibc477f1926e833d361031ca5238f4d22c5ae677d

--- vcl/source/font/PhysicalFontFamily.cxx
+++ vcl/source/font/PhysicalFontFamily.cxx
@@ -273,4 +273,4 @@ void PhysicalFontFamily::UpdateCloneFontList(PhysicalFontCollection& rFontCollec
     }
 }
 
-/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
