1e005d2f3e6885d485bc01cf2dbc454de2491b89
Chris Sherlock <chris.sherlock79@gmail.com>
2021-09-16 16:15:17 +1000

    Add sal/config.h in preparation for patch
    The convention is that we need to add sal/config.h to the start of
    files.
    
    I have a patch queued to be able to create a FontMetric from
    a PhysicalFontFace. Submitting this patch so I can hopefully one day
    land this patch.
    
    Change-Id: I5817acd6e194da576f2c8a13539d38954a4f4e15

--- include/vcl/metric.hxx
+++ include/vcl/metric.hxx
@@ -19,6 +19,8 @@
 
 #pragma once
 
+#include <sal/config.h>
+
 #include <vcl/dllapi.h>
 #include <vcl/font.hxx>
 #include <tools/ref.hxx>
--- vcl/workben/svptest.cxx
+++ vcl/workben/svptest.cxx
@@ -17,6 +17,8 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
+#include <sal/config.h>
+
 #include <sal/main.h>
 #include <sal/log.hxx>
 #include <tools/diagnose_ex.h>
