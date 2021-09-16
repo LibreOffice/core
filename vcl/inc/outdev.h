69703ee23d934c4ae41ae4f0f30b166a4487a4a2
Chris Sherlock <chris.sherlock79@gmail.com>
2021-09-16 12:46:23 +1000

    tdf#124176 - Use pragma once instead of include guards
    I have a patch queued to rename ImplDeviceFontList to
    PhysicalFontFaceCollection, which I am adding a test to. Submitting
    patch to update the include guards to pragma once
    
    Change-Id: I091817905599089edec1138eeee06ba3dacd6964

--- vcl/inc/outdev.h
+++ vcl/inc/outdev.h
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_VCL_INC_OUTDEV_H
-#define INCLUDED_VCL_INC_OUTDEV_H
+#pragma once
 
 #include <set>
 #include <vector>
@@ -138,7 +137,5 @@ struct ImplOutDevData
 };
 
 void ImplFontSubstitute( OUString& rFontName );
-
-#endif // INCLUDED_VCL_INC_OUTDEV_H
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
