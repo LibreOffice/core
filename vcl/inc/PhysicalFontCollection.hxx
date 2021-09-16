5785ddfb7d6642751aebdcfb8f7aab3de3aeef8a
Chris Sherlock <chris.sherlock79@gmail.com>
2021-09-16 12:41:19 +1000

    vcl: update vim modelines in preparation for new change
    I have a patch queued to rename ImplDeviceFontList to
    PhysicalFontFaceCollection, which I am adding a test to. Submitting
    patch to update the modelines in the files that I will change in that
    patch.
    
    Change-Id: I3b0b022cbfa7b05dbfbb5c2d03f6d68128c0598f

--- vcl/inc/PhysicalFontCollection.hxx
+++ vcl/inc/PhysicalFontCollection.hxx
@@ -93,4 +93,4 @@ private:
 
 #endif // INCLUDED_VCL_INC_PHYSICALFONTCOLLECTION_HXX
 
-/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
