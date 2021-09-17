c7be2c020dfe0126726c97a4e697535f3cf341fd
Caolán McNamara <caolanm@redhat.com>
2021-09-17 14:46:23 +0100

    ofz: MemorySanitizer: use-of-uninitialized-value
    Change-Id: I65338d31b3b556b2ba96f592c2b12084dab87e4d

--- sw/source/filter/ww8/ww8graf2.cxx
+++ sw/source/filter/ww8/ww8graf2.cxx
@@ -444,7 +444,7 @@ bool SwWW8ImplReader::PicRead(SvStream *pDataStream, WW8_PIC *pPic,
     bool bVer67)
 {
     //Only the first 0x2e bytes are the same between version 6/7 and 8+
-    WW8_PIC_SHADOW aPicS;
+    WW8_PIC_SHADOW aPicS{};
     pDataStream->ReadBytes( &aPicS, sizeof( aPicS ) );
     WW8PicShadowToReal( &aPicS, pPic );
     for (WW8_BRC & i : pPic->rgbrc)
