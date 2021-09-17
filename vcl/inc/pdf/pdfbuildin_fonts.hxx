e74b5e006f5579586e5492e513149696ec3e67a1
Chris Sherlock <chris.sherlock79@gmail.com>
2021-09-17 14:54:33 +1000

    tdf#124176 - Use pragma once instead of include guards
    This patch is created in preparation of a patch I have queued to test
    and move PhysicalFontFace to vcl::font namespace.
    
    Change-Id: I805a8bd1fa881fc4bc6d2f26f1051b9247587701

--- vcl/inc/pdf/pdfbuildin_fonts.hxx
+++ vcl/inc/pdf/pdfbuildin_fonts.hxx
@@ -16,8 +16,8 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
-#ifndef INCLUDED_VCL_SOURCE_PDF_BUILDIN_FONTS_HXX
-#define INCLUDED_VCL_SOURCE_PDF_BUILDIN_FONTS_HXX
+
+#pragma once
 
 #include <PhysicalFontFace.hxx>
 #include <fontinstance.hxx>
@@ -75,7 +75,5 @@ public:
 };
 
 } // namespace vcl::pdf
-
-#endif // INCLUDED_VCL_SOURCE_PDF_BUILDIN_FONTS_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
