b964de6daacd583a1ae10760fe76a6cb49bd2e4d
Chris Sherlock <chris.sherlock79@gmail.com>
2021-09-22 05:21:28 +1000

    tdf#143148 - Use pragma once instead of include guards
    Done in preparation of split of outdev.h into seperate headers
    
    Change-Id: If9c52a7cf8d401cdcf84043d27dc826cca27fea8
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/122405
    Tested-by: Jenkins
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

--- vcl/inc/svdata.hxx
+++ vcl/inc/svdata.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_VCL_INC_SVDATA_HXX
-#define INCLUDED_VCL_INC_SVDATA_HXX
+#pragma once
 
 #include <config_version.h>
 
@@ -460,7 +459,5 @@ struct ImplSVEvent
 };
 
 extern int nImplSysDialog;
-
-#endif // INCLUDED_VCL_INC_SVDATA_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
