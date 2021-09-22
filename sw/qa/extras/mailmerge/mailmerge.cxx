25794c1cfe37e4bffc14aa70d918726626ea68b9
Justin Luth <justin.luth@collabora.com>
2021-09-22 19:28:21 +0200

    tdf#144215 mailmerge: start individual document at 1, not 0
    Humans deal with numbers starting with 1, so record one
    in a mailmerge should match with file name1.
    
    The request was to separate with an underscore
    as well, but lets do that separately so it can
    be reverted if others dislike that (although it would
    make it consistent with the new toolbox naming convention).
    
    Change-Id: I3b44bc5c6e7619c8013618add585b2bd33870181

--- sw/qa/extras/mailmerge/mailmerge.cxx
+++ sw/qa/extras/mailmerge/mailmerge.cxx
@@ -234,7 +234,7 @@ public:
         if (mnCurOutputType != text::MailMergeType::FILE)
             return nullptr;
 
-        OUString name = msMailMergeOutputPrefix + OUString::number( 0 ) + ".odt";
+        OUString name = msMailMergeOutputPrefix + OUString::number(1) + ".odt";
         return parseExportInternal( msMailMergeOutputURL + "/" + name, rStreamName );
     }
 
@@ -268,7 +268,7 @@ public:
             aURLObj.SetSmartURL( msMailMergeDocumentURL );
             name = aURLObj.GetBase();
         }
-        name += OUString::number( number ) + ".odt";
+        name += OUString::number(number+1) + ".odt";
         loadMailMergeDocument( name );
     }
 
