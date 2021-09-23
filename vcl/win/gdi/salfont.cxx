a2728335db45966dbcb3282d56c9af7f156b73f6
Chris Sherlock <chris.sherlock79@gmail.com>
2021-09-23 18:20:55 +1000

    vcl: rename ImplFontMetricData to FontInstanceData and move to vcl::font
    Change-Id: I56bf6e12ec3f7b16977443ad4f4b3c7fc0db7965

--- vcl/win/gdi/salfont.cxx
+++ vcl/win/gdi/salfont.cxx
@@ -65,7 +65,7 @@
 #include <win/winlayout.hxx>
 #include <win/wingdiimpl.hxx>
 #include <impfontcharmap.hxx>
-#include <impfontmetricdata.hxx>
+#include <font/FontInstanceData.hxx>
 #include <impglyphitem.hxx>
 
 #if HAVE_FEATURE_SKIA
@@ -909,7 +909,7 @@ void WinSalGraphics::SetFont(LogicalFontInstance* pFont, int nFallbackLevel)
     pFontFace->UpdateFromHDC(getHDC());
 }
 
-void WinSalGraphics::GetFontMetric( ImplFontMetricDataRef& rxFontMetric, int nFallbackLevel )
+void WinSalGraphics::GetFontMetric( vcl::font::FontInstanceDataRef& rxFontMetric, int nFallbackLevel )
 {
     // temporarily change the HDC to the font in the fallback level
     rtl::Reference<WinFontInstance> pFontInstance = mpWinFontEntry[nFallbackLevel];
