991a1a368365c506c4ced168f8479ab3cfaba464
Szymon Kłos <szymon.klos@collabora.com>
2021-09-23 16:33:27 +0200

    jsdialog: send full update for autofilter
    Change-Id: I64baa5fea03f96350fe4db86ea0e42e02d41e8be

--- vcl/jsdialog/jsdialogbuilder.cxx
+++ vcl/jsdialog/jsdialogbuilder.cxx
@@ -606,6 +606,7 @@ JSInstanceBuilder::JSInstanceBuilder(vcl::Window* pParent, const OUString& rUIRo
     }
 
     initializeSender(GetNotifierWindow(), GetContentWindow(), GetTypeOfJSON());
+    sendFullUpdate();
 }
 
 JSInstanceBuilder* JSInstanceBuilder::CreateDialogBuilder(weld::Widget* pParent,
