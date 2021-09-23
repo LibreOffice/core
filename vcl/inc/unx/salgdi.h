6b1758adc8fe9e22cf1085dcc1bce4b0965a7f60
Chris Sherlock <chris.sherlock79@gmail.com>
2021-09-23 18:20:55 +1000

    vcl: rename ImplFontMetricData to FontInstanceData and move to vcl::font
    Change-Id: I56bf6e12ec3f7b16977443ad4f4b3c7fc0db7965

--- vcl/inc/unx/salgdi.h
+++ vcl/inc/unx/salgdi.h
@@ -116,7 +116,7 @@ public:
 
     virtual void                    SetTextColor( Color nColor ) override;
     virtual void                    SetFont(LogicalFontInstance*, int nFallbackLevel) override;
-    virtual void                    GetFontMetric( ImplFontMetricDataRef&, int nFallbackLevel ) override;
+    virtual void                    GetFontMetric( vcl::font::FontInstanceDataRef&, int nFallbackLevel ) override;
     virtual FontCharMapRef          GetFontCharMap() const override;
     virtual bool                    GetFontCapabilities(vcl::FontCapabilities &rFontCapabilities) const override;
     virtual void                    GetDevFontList( vcl::font::PhysicalFontCollection* ) override;
