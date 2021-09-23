LibreOffice/core

commit ec01d43e0a8fa560d7cd8c76c0d609b18a60cddb

pass SfxChildWinFactory around by value
no need to allocate on heap

Change-Id: I7410a6c9cb434b18ed6f7ae1dc0ccea34151921c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/119331
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sfx2/source/appl/childwin.cxx
@@ -48,10 +48,6 @@ SfxChildWinFactory::SfxChildWinFactory( SfxChildWinCtor pTheCtor, sal_uInt16 nID
     , nPos(n)
 {}
 
-SfxChildWinFactory::~SfxChildWinFactory()
-{
-}
-
 struct SfxChildWindow_Impl
 {
     css::uno::Reference< css::frame::XFrame >             xFrame;
@@ -621,9 +617,9 @@ void SfxChildWindow::SetFrame( const css::uno::Reference< css::frame::XFrame > &
         pImpl->xFrame->addEventListener( pImpl->xListener );
 }
 
-void SfxChildWindow::RegisterChildWindow(SfxModule* pMod, std::unique_ptr<SfxChildWinFactory> pFact)
+void SfxChildWindow::RegisterChildWindow(SfxModule* pMod, const SfxChildWinFactory& rFact)
 {
-    SfxGetpApp()->RegisterChildWindow_Impl( pMod, std::move(pFact) );
+    SfxGetpApp()->RegisterChildWindow_Impl( pMod, rFact );
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

include/sfx2/app.hxx
@@ -171,7 +171,7 @@ public:
     SAL_DLLPRIVATE SfxAppData_Impl* Get_Impl() const { return pImpl.get(); }
 
     // Object-Factories/global arrays
-    SAL_DLLPRIVATE void         RegisterChildWindow_Impl(SfxModule*, std::unique_ptr<SfxChildWinFactory>);
+    SAL_DLLPRIVATE void         RegisterChildWindow_Impl(SfxModule*, const SfxChildWinFactory&);
     SAL_DLLPRIVATE void         RegisterStatusBarControl_Impl(SfxModule*, const SfxStbCtrlFactory&);
     SAL_DLLPRIVATE void         RegisterToolBoxControl_Impl( SfxModule*, const SfxTbxCtrlFactory&);
     SAL_DLLPRIVATE SfxTbxCtrlFactArr_Impl& GetTbxCtrlFactories_Impl() const;

include/sfx2/childwin.hxx
@@ -90,7 +90,6 @@ struct SFX2_DLLPUBLIC SfxChildWinFactory
     sal_uInt16                  nPos;   // Position in UI
 
     SfxChildWinFactory( SfxChildWinCtor pTheCtor, sal_uInt16 nID, sal_uInt16 n );
-    ~SfxChildWinFactory();
 };
 
 struct SfxChildWindow_Impl;
@@ -135,7 +134,7 @@ public:
     virtual SfxChildWinInfo GetInfo() const;
     void                SaveStatus(const SfxChildWinInfo& rInfo);
 
-    static void         RegisterChildWindow(SfxModule*, std::unique_ptr<SfxChildWinFactory>);
+    static void         RegisterChildWindow(SfxModule*, const SfxChildWinFactory&);
 
     static std::unique_ptr<SfxChildWindow> CreateChildWindow( sal_uInt16, vcl::Window*, SfxBindings*, SfxChildWinInfo const &);
     void                SetHideNotDelete( bool bOn );
@@ -182,11 +181,11 @@ public:
                 } \
         void    Class::RegisterChildWindow (bool bVis, SfxModule *pMod, SfxChildWindowFlags nFlags)   \
                 {   \
-                    auto pFact = std::make_unique<SfxChildWinFactory>( \
+                    SfxChildWinFactory aFact( \
                         Class::CreateImpl, MyID, Pos );   \
-                    pFact->aInfo.nFlags |= nFlags;  \
-                    pFact->aInfo.bVisible = bVis;         \
-                    SfxChildWindow::RegisterChildWindow(pMod, std::move(pFact)); \
+                    aFact.aInfo.nFlags |= nFlags;  \
+                    aFact.aInfo.bVisible = bVis;         \
+                    SfxChildWindow::RegisterChildWindow(pMod, aFact); \
                 }
 
 #define SFX_IMPL_POS_CHILDWINDOW_WITHID(Class, MyID, Pos) \

include/sfx2/module.hxx
@@ -74,7 +74,7 @@ public:
     SfxSlotPool*                GetSlotPool() const;
 
     void                        RegisterToolBoxControl(const SfxTbxCtrlFactory&);
-    void                        RegisterChildWindow(std::unique_ptr<SfxChildWinFactory>);
+    void                        RegisterChildWindow(const SfxChildWinFactory&);
     void                        RegisterStatusBarControl(const SfxStbCtrlFactory&);
 
     virtual std::unique_ptr<SfxTabPage>  CreateTabPage( sal_uInt16 nId,

sc/source/ui/inc/ChildWindowWrapper.hxx
@@ -49,10 +49,10 @@ public:
                     SfxModule* pModule  = nullptr,
                     SfxChildWindowFlags nFlags = SfxChildWindowFlags::NONE)
     {
-        auto pFactory = std::make_unique<SfxChildWinFactory>(ChildControllerWrapper::CreateImpl, WindowID, CHILDWIN_NOPOS );
-        pFactory->aInfo.nFlags |= nFlags;
-        pFactory->aInfo.bVisible = bVisible;
-        SfxChildWindow::RegisterChildWindow(pModule, std::move(pFactory));
+        SfxChildWinFactory aFactory(ChildControllerWrapper::CreateImpl, WindowID, CHILDWIN_NOPOS );
+        aFactory.aInfo.nFlags |= nFlags;
+        aFactory.aInfo.bVisible = bVisible;
+        SfxChildWindow::RegisterChildWindow(pModule, aFactory);
     }
 
     static sal_uInt16 GetChildWindowId()

sfx2/source/appl/appchild.cxx
@@ -30,11 +30,11 @@
 #include <sfx2/viewfrm.hxx>
 
 
-void SfxApplication::RegisterChildWindow_Impl( SfxModule *pMod, std::unique_ptr<SfxChildWinFactory> pFact )
+void SfxApplication::RegisterChildWindow_Impl( SfxModule *pMod, const SfxChildWinFactory& rFact )
 {
     if ( pMod )
     {
-        pMod->RegisterChildWindow( std::move(pFact) );
+        pMod->RegisterChildWindow( rFact );
         return;
     }
 
@@ -43,13 +43,13 @@ void SfxApplication::RegisterChildWindow_Impl( SfxModule *pMod, std::unique_ptr<
 
     for (size_t nFactory=0; nFactory<pImpl->pFactArr->size(); ++nFactory)
     {
-        if (pFact->nId ==  (*pImpl->pFactArr)[nFactory].nId)
+        if (rFact.nId == (*pImpl->pFactArr)[nFactory].nId)
         {
             pImpl->pFactArr->erase( pImpl->pFactArr->begin() + nFactory );
         }
     }
 
-    pImpl->pFactArr->push_back( std::move(pFact) );
+    pImpl->pFactArr->push_back( rFact );
 }
 
 SfxChildWinFactArr_Impl& SfxApplication::GetChildWinFactories_Impl() const

sfx2/source/appl/childwinimpl.cxx
@@ -27,17 +27,17 @@ size_t SfxChildWinFactArr_Impl::size() const
 
 const SfxChildWinFactory& SfxChildWinFactArr_Impl::operator []( size_t i ) const
 {
-    return *maData[i];
+    return maData[i];
 }
 
 SfxChildWinFactory& SfxChildWinFactArr_Impl::operator []( size_t i )
 {
-    return *maData[i];
+    return maData[i];
 }
 
-void SfxChildWinFactArr_Impl::push_back( std::unique_ptr<SfxChildWinFactory> p )
+void SfxChildWinFactArr_Impl::push_back( const SfxChildWinFactory& p )
 {
-    maData.push_back(std::move(p));
+    maData.push_back(p);
 }
 
 void SfxChildWinFactArr_Impl::erase( const iterator& it )

sfx2/source/appl/module.cxx
@@ -107,7 +107,7 @@ SfxSlotPool* SfxModule::GetSlotPool() const
 }
 
 
-void SfxModule::RegisterChildWindow(std::unique_ptr<SfxChildWinFactory> pFact)
+void SfxModule::RegisterChildWindow(const SfxChildWinFactory& rFact)
 {
     DBG_ASSERT( pImpl, "No real Module!" );
 
@@ -116,7 +116,7 @@ void SfxModule::RegisterChildWindow(std::unique_ptr<SfxChildWinFactory> pFact)
 
     for (size_t nFactory=0; nFactory<pImpl->pFactArr->size(); ++nFactory)
     {
-        if (pFact->nId ==  (*pImpl->pFactArr)[nFactory].nId)
+        if (rFact.nId == (*pImpl->pFactArr)[nFactory].nId)
         {
             pImpl->pFactArr->erase( pImpl->pFactArr->begin() + nFactory );
             SAL_WARN("sfx.appl", "ChildWindow registered multiple times!");
@@ -124,7 +124,7 @@ void SfxModule::RegisterChildWindow(std::unique_ptr<SfxChildWinFactory> pFact)
         }
     }
 
-    pImpl->pFactArr->push_back( std::move(pFact) );
+    pImpl->pFactArr->push_back( rFact );
 }
 
 

commit 234aee7fd21a8c85dd8752c3f1e20290c6167a7f

tdf#144132 Crash when insert formula object in Writer
regression from
    commit ec01d43e0a8fa560d7cd8c76c0d609b18a60cddb
    Date:   Wed Jul 21 14:24:28 2021 +0200
    pass SfxChildWinFactory around by value

Change-Id: I88bc6c8bfd35d0a653512b064c9ee0d59b1e8b38

sfx2/source/appl/childwin.cxx
@@ -51,7 +51,7 @@ struct SfxChildWindow_Impl
 {
     css::uno::Reference< css::frame::XFrame >             xFrame;
     css::uno::Reference< css::lang::XEventListener >      xListener;
-    SfxChildWinFactory* pFact;
+    SfxChildWinFactory aFact = { nullptr, 0, 0 };
     bool                bHideNotDelete;
     bool                bVisible;
     bool                bWantsFocus;
@@ -147,7 +147,6 @@ SfxChildWindow::SfxChildWindow(vcl::Window *pParentWindow, sal_uInt16 nId)
     , eChildAlignment(SfxChildAlignment::NOALIGNMENT)
     , nType(nId)
 {
-    pImpl->pFact = nullptr;
     pImpl->bHideNotDelete = false;
     pImpl->bWantsFocus = true;
     pImpl->bVisible = true;
@@ -291,7 +290,7 @@ void SfxChildWindow::SaveStatus(const SfxChildWinInfo& rInfo)
     aWinOpt.SetUserData( aSeq );
 
     // ... but save status at runtime!
-    pImpl->pFact->aInfo = rInfo;
+    pImpl->aFact.aInfo = rInfo;
 }
 
 void SfxChildWindow::SetAlignment(SfxChildAlignment eAlign)
@@ -301,7 +300,7 @@ void SfxChildWindow::SetAlignment(SfxChildAlignment eAlign)
 
 SfxChildWinInfo SfxChildWindow::GetInfo() const
 {
-    SfxChildWinInfo aInfo(pImpl->pFact->aInfo);
+    SfxChildWinInfo aInfo(pImpl->aFact.aInfo);
     if (xController)
     {
         weld::Dialog* pDialog = xController->getDialog();
@@ -343,7 +342,7 @@ SfxChildWinInfo SfxChildWindow::GetInfo() const
 
 sal_uInt16 SfxChildWindow::GetPosition() const
 {
-    return pImpl->pFact->nPos;
+    return pImpl->aFact.nPos;
 }
 
 void SfxChildWindow::InitializeChildWinFactory_Impl(sal_uInt16 nId, SfxChildWinInfo& rInfo)
@@ -423,7 +422,7 @@ bool ParentIsFloatingWindow(const vcl::Window *pParent)
 
 void SfxChildWindow::SetFactory_Impl( SfxChildWinFactory *pF )
 {
-    pImpl->pFact = pF;
+    pImpl->aFact = *pF;
 }
 
 void SfxChildWindow::SetHideNotDelete( bool bOn )