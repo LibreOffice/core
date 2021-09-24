2c41a74024dcc41528d1f8eed1e2936ba761138a
Noel Grandin <noelgrandin@gmail.com>
2021-09-24 14:20:49 +0200

    use SfxItemSetFixed in svx
    Change-Id: I8123b34c73a16579b59989ae31329dec378d8b98

--- svx/source/svdraw/svdedtv2.cxx
+++ svx/source/svdraw/svdedtv2.cxx
@@ -558,10 +558,9 @@ void SdrEditView::ImpCopyAttributes(const SdrObject* pSource, SdrObject* pDest) 
     if(!(pSource && pDest))
         return;
 
-    SfxItemSet aSet(mpModel->GetItemPool(),
-        svl::Items<SDRATTR_START,              SDRATTR_NOTPERSIST_FIRST-1,
+    SfxItemSetFixed<SDRATTR_START,              SDRATTR_NOTPERSIST_FIRST-1,
         SDRATTR_NOTPERSIST_LAST+1,  SDRATTR_END,
-        EE_ITEMS_START,             EE_ITEMS_END>);
+        EE_ITEMS_START,             EE_ITEMS_END> aSet(mpModel->GetItemPool());
 
     aSet.Put(pSource->GetMergedItemSet());
 
