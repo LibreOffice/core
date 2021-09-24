6f908b48373b71d45c8119b296b0504fb586f6f8
Vasily Melenchuk <vasily.melenchuk@cib.de>
2021-09-24 15:18:13 +0200

    tdf#143104 Fix xlsx import/export of color filter colors
    1. In XLSX filter colors are always stored in dxf as foreground
    colors, so Calc should keep them, if possible. So practically
    use only one color during import.
    
    3. On export we need to distinguish type of filter, this is done
    with cellColor=0 or cellColor=1 attribute of <colorFilter>.
    
    4. Since p.1 there is no need to keep on export separate dxf
    structures for fg and bg colors: we always use only foreground
    color for color filters.
    
    Co-authored-by: Samuel Mehrbrodt <samuel.mehrbrodt@allotropia.de>
    
    Change-Id: Iacd352ae46bf84859dc15ee695b6dc63240afe7d
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/122593
    Tested-by: Jenkins
    Reviewed-by: Thorsten Behrens <thorsten.behrens@allotropia.de>

--- sc/source/filter/oox/stylesbuffer.cxx
+++ sc/source/filter/oox/stylesbuffer.cxx
@@ -1825,11 +1825,14 @@ void Fill::finalizeImport()
         {
             if( rModel.mbFillColorUsed && (!rModel.mbPatternUsed || (rModel.mnPattern == XML_solid)) )
             {
-                rModel.maPatternColor = rModel.maFillColor;
+                if (!rModel.mbPatternUsed)
+                    rModel.maPatternColor = rModel.maFillColor;
                 rModel.mnPattern = XML_solid;
                 rModel.mbPattColorUsed = rModel.mbPatternUsed = true;
             }
-            else if( !rModel.mbFillColorUsed && rModel.mbPatternUsed && (rModel.mnPattern == XML_solid) )
+            else if(
+                !rModel.mbFillColorUsed && !rModel.mbPattColorUsed &&
+                rModel.mbPatternUsed && rModel.mnPattern == XML_solid )
             {
                 rModel.mbPatternUsed = false;
             }
