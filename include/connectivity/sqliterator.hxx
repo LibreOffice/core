2f85449bb9a16c56466aec296c2170956bc2fa07
Caolán McNamara <caolanm@redhat.com>
2021-09-26 20:57:29 +0100

    setSelectColumnName is always called with m_aSelectColumns as 1st arg
    Change-Id: If86c4f48ed7eb6860f461fe905c0628cb9b73043

--- include/connectivity/sqliterator.hxx
+++ include/connectivity/sqliterator.hxx
@@ -141,7 +141,7 @@ namespace connectivity
         css::uno::Reference< css::beans::XPropertySet > findSelectColumn(
             std::u16string_view rColumnName );
 
-        void setSelectColumnName(::rtl::Reference<OSQLColumns> const & _rColumns,const OUString & rColumnName,const OUString & rColumnAlias, const OUString & rTableRange, bool bFkt=false, sal_Int32 _nType = css::sdbc::DataType::VARCHAR, bool bAggFkt=false);
+        void setSelectColumnName(const OUString& rColumnName, const OUString& rColumnAlias, const OUString& rTableRange, bool bFkt = false, sal_Int32 _nType = css::sdbc::DataType::VARCHAR, bool bAggFkt = false);
         void appendColumns(::rtl::Reference<OSQLColumns> const & _rColumns,const OUString& _rTableAlias,const OSQLTable& _rTable);
         // Other member variables that should be available in the "set" functions
         // can be defined in the derived class. They can be initialized
