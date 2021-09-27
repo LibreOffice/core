LibreOffice/core

commit 0a4d77f35e96e4dfdf69cc5ceb788ddaa849084c

SwTxtNode::Update: don't move anchors at insert position
Use cases:

1) User of the XTextAppend API's uses appendTextPortion(), then
appendTextContent(), then appendTextPortion() again. It's expected that
the text content will be in the middle of the paragraph, but before, it
was at the end of it.

It's possible to work this around by inserting the text contents only at
the end of the paragraph (like it's done for ODF import in
XMLParaContext::~XMLParaContext()), but probably it's not a good idea to
copy&paste this workaround in all the import filters.

2) User types some characters in a new document, then inserts a picture,
and sets anchor type to at-char. Now the anchor point is set to the end
of the paragraph, but it's impossible to type anything after that
character.

To be fair, now it's not possible to type something *before* the anchor
point, if it's at the first character, but hopefully that's less of an
issue, since setting the anchor type to at-char sets it to the end of
the paragraph, not at the start of it.

Change-Id: Ia8b33e8880aae86f62fae09cd5ac1cf82c83320f

sw/source/core/txtnode/ndtxt.cxx
@@ -1097,6 +1097,22 @@ void SwTxtNode::Update(
 
             bSortMarks = bAtLeastOneBookmarkMoved && bAtLeastOneExpandedBookmarkAtInsertionPosition;
         }
+
+        // at-char anchored flys shouldn't be moved, either.
+        const SwFrmFmts& rFmts = *GetDoc()->GetSpzFrmFmts();
+        for (SwFrmFmts::const_iterator pFmt = rFmts.begin(); pFmt != rFmts.end(); ++pFmt)
+        {
+            const SwFmtAnchor& rAnchor = (*pFmt)->GetAnchor();
+            const SwPosition* pCntntAnchor = rAnchor.GetCntntAnchor();
+            if (rAnchor.GetAnchorId() == FLY_AT_CHAR && pCntntAnchor)
+            {
+                // The fly is at-char anchored and has an anchor position.
+                SwIndex& rEndIdx = const_cast<SwIndex&>(pCntntAnchor->nContent);
+                if (&pCntntAnchor->nNode.GetNode() == this && rEndIdx.GetIndex() == rPos.GetIndex())
+                    // The anchor position is exactly our insert position.
+                    rEndIdx.Assign(&aTmpIdxReg, rEndIdx.GetIndex());
+            }
+        }
     }
 
     // base class

sw/qa/extras/ooxmlexport/ooxmlexport.cxx
@@ -403,6 +403,15 @@ DECLARE_OOXMLEXPORT_TEST(testTableStyleBorderExport, "table-style-border-export.
     CPPUNIT_ASSERT_EQUAL(sal_Int32(0x8064A2), getProperty<table::BorderLine2>(xCell, "BottomBorder").Color);
 }
 
+DECLARE_OOXMLEXPORT_TEST(testAnchorPosition, "anchor-position.docx")
+{
+    // The problem was that the at-char anchored picture was at the end of the
+    // paragraph, so there were only two postions: a Text, then a Frame one.
+    CPPUNIT_ASSERT_EQUAL(OUString("Text"), getProperty<OUString>(getRun(getParagraph(1), 1), "TextPortionType"));
+    CPPUNIT_ASSERT_EQUAL(OUString("Frame"), getProperty<OUString>(getRun(getParagraph(1), 2), "TextPortionType"));
+    CPPUNIT_ASSERT_EQUAL(OUString("Text"), getProperty<OUString>(getRun(getParagraph(1), 3), "TextPortionType"));
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sw/qa/extras/ooxmlexport/ooxmlexport5.cxx
@@ -466,7 +466,7 @@ DECLARE_OOXMLEXPORT_TEST(testFDO79915, "fdo79915.docx")
     if (!pXmlDoc)
         return;
 
-    assertXPathContent(pXmlDoc, "/w:document/w:body/w:p[2]/w:r[8]/w:t", "How much buoyancy does the water provide?");
+    assertXPathContent(pXmlDoc, "/w:document/w:body/w:p[2]/w:r[10]/w:t", "How much buoyancy does the water provide?");
 }
 
 DECLARE_OOXMLEXPORT_TEST(testfdo79817, "fdo79817.docx")

sw/qa/extras/ooxmlexport/ooxmlsdrexport.cxx
@@ -902,7 +902,7 @@ DECLARE_OOXMLEXPORT_TEST(testOuterShdw,"testOuterShdw.docx")
     xmlDocPtr pXmlDoc = parseExport("word/document.xml");
     if (!pXmlDoc)
         return;
-    assertXPath(pXmlDoc, "/w:document[1]/w:body[1]/w:p[1]/w:r[3]/mc:AlternateContent[1]/mc:Choice[1]/w:drawing[1]/wp:anchor[1]/a:graphic[1]/a:graphicData[1]/wps:wsp[1]/wps:spPr[1]/a:effectLst[1]/a:outerShdw[1]", "dist", "57811035");
+    assertXPath(pXmlDoc, "//mc:AlternateContent[1]/mc:Choice[1]/w:drawing[1]/wp:anchor[1]/a:graphic[1]/a:graphicData[1]/wps:wsp[1]/wps:spPr[1]/a:effectLst[1]/a:outerShdw[1]", "dist", "57811035");
 }
 
 DECLARE_OOXMLEXPORT_TEST(testExtentValue, "fdo74605.docx")

commit e509dd6249231634050c2b4be43b948064132616

tdf#143819 sw: fix undo not restoring adjusted anchor
Regression from commit 0a4d77f35e96e4dfdf69cc5ceb788ddaa849084c
(SwTxtNode::Update: don't move anchors at insert position, 2014-10-06),
the problem is that in case backspace adjusts the anchor point of a fly
and undo does an insert (which now doesn't adjust the anchor point),
then the user action and its undo is not in sync.

Fix this by informing SwTextNode::Update() if an undo is in progress and
doing the old behavior in that case.

Change-Id: I0b3f3954be11420846f84287e486b993b2dc39e8

sw/inc/ndtxt.hxx
@@ -117,6 +117,9 @@ class SW_DLLPUBLIC SwTextNode final
        Needed to avoid duplicate handling of attribute change actions. */
     bool mbInSetOrResetAttr;
 
+    /// Is an undo operation in progress?
+    bool m_bInUndo;
+
     std::optional< OUString > m_oNumStringCache;
 
     css::uno::WeakReference<css::text::XTextContent> m_wXParagraph;
@@ -790,6 +793,7 @@ public:
     /// sfx2::Metadatable
     virtual ::sfx2::IXmlIdRegistry& GetRegistry() override;
     virtual bool IsInClipboard() const override;
+    /// Is this node in the undo array?
     virtual bool IsInUndo() const override;
     virtual bool IsInContent() const override;
     virtual css::uno::Reference< css::rdf::XMetadatable > MakeUnoObject() override;
@@ -811,6 +815,8 @@ public:
     static bool IsIgnoredCharFormatForNumbering(const sal_uInt16 nWhich, bool bIsCharStyle = false);
     void FormatDropNotify(const SwFormatDrop& rDrop) override
             { TriggerNodeUpdate(sw::LegacyModifyHint(&rDrop, &rDrop)); };
+
+    void SetInSwUndo(bool bInUndo);
 };
 
 inline SwpHints & SwTextNode::GetSwpHints()

sw/source/core/txtnode/ndtxt.cxx
@@ -194,7 +194,8 @@ SwTextNode::SwTextNode( const SwNodeIndex &rWhere, SwTextFormatColl *pTextColl, 
     m_bLastOutlineState( false ),
     m_bNotifiable( false ),
     mbEmptyListStyleSetDueToSetOutlineLevelAttr( false ),
-    mbInSetOrResetAttr( false )
+    mbInSetOrResetAttr( false ),
+    m_bInUndo(false)
 {
     InitSwParaStatistics( true );
 
@@ -1426,20 +1427,23 @@ void SwTextNode::Update(
         }
 
         // at-char anchored flys shouldn't be moved, either.
-        std::vector<SwFrameFormat*> const& rFlys(GetAnchoredFlys());
-        for (size_t i = 0; i != rFlys.size(); ++i)
+        if (!m_bInUndo)
         {
-            SwFrameFormat const*const pFormat = rFlys[i];
-            const SwFormatAnchor& rAnchor = pFormat->GetAnchor();
-            const SwPosition* pContentAnchor = rAnchor.GetContentAnchor();
-            if (rAnchor.GetAnchorId() == RndStdIds::FLY_AT_CHAR && pContentAnchor)
+            std::vector<SwFrameFormat*> const& rFlys(GetAnchoredFlys());
+            for (size_t i = 0; i != rFlys.size(); ++i)
             {
-                // The fly is at-char anchored and has an anchor position.
-                SwIndex& rEndIdx = const_cast<SwIndex&>(pContentAnchor->nContent);
-                if (&pContentAnchor->nNode.GetNode() == this && rEndIdx.GetIndex() == rPos.GetIndex())
+                SwFrameFormat const*const pFormat = rFlys[i];
+                const SwFormatAnchor& rAnchor = pFormat->GetAnchor();
+                const SwPosition* pContentAnchor = rAnchor.GetContentAnchor();
+                if (rAnchor.GetAnchorId() == RndStdIds::FLY_AT_CHAR && pContentAnchor)
                 {
-                    // The anchor position is exactly our insert position.
-                    rEndIdx.Assign(&aTmpIdxReg, rEndIdx.GetIndex());
+                    // The fly is at-char anchored and has an anchor position.
+                    SwIndex& rEndIdx = const_cast<SwIndex&>(pContentAnchor->nContent);
+                    if (&pContentAnchor->nNode.GetNode() == this && rEndIdx.GetIndex() == rPos.GetIndex())
+                    {
+                        // The anchor position is exactly our insert position.
+                        rEndIdx.Assign(&aTmpIdxReg, rEndIdx.GetIndex());
+                    }
                 }
             }
         }
@@ -4857,6 +4861,11 @@ bool SwTextNode::SetAttr( const SfxItemSet& rSet )
     mbInSetOrResetAttr = bOldIsSetOrResetAttr;
 
     return bRet;
+}
+
+void SwTextNode::SetInSwUndo(bool bInUndo)
+{
+    m_bInUndo = bInUndo;
 }
 
 namespace {

sw/source/core/undo/undel.cxx
@@ -1015,8 +1015,10 @@ void SwUndoDelete::UndoImpl(::sw::UndoRedoContext & rContext)
                 // SectionNode mode and selection from top to bottom:
                 //  -> in StartNode is still the rest of the Join => delete
                 aPos.nContent.Assign( pTextNd, m_nSttContent );
+                pTextNd->SetInSwUndo(true);
                 OUString const ins( pTextNd->InsertText(*m_aSttStr, aPos.nContent,
                         SwInsertFlags::NOHINTEXPAND) );
+                pTextNd->SetInSwUndo(false);
                 assert(ins.getLength() == m_aSttStr->getLength()); // must succeed
                 (void) ins;
                 // METADATA: restore

sw/qa/core/txtnode/txtnode.cxx
@@ -154,6 +154,31 @@ CPPUNIT_TEST_FIXTURE(SwCoreTxtnodeTest, testTitleFieldInvalidate)
     comphelper::LibreOfficeKit::setActive(false);
 }
 
+CPPUNIT_TEST_FIXTURE(SwCoreTxtnodeTest, testFlyAnchorUndo)
+{
+    // Given a document with a fly frame, anchored after the last char of the document:
+    load(DATA_DIRECTORY, "fly-anchor-undo.odt");
+    SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument*>(mxComponent.get());
+    SwDocShell* pShell = pTextDoc->GetDocShell();
+    SwDoc* pDoc = pShell->GetDoc();
+    const SwFrameFormats& rSpz = *pDoc->GetSpzFrameFormats();
+    sal_Int32 nExpected = rSpz[0]->GetAnchor().GetContentAnchor()->nContent.GetIndex();
+
+    // When deleting that last character and undoing it:
+    SwWrtShell* pWrtShell = pShell->GetWrtShell();
+    pWrtShell->SttEndDoc(/*bStt=*/false);
+    pWrtShell->DelLeft();
+    pWrtShell->Undo();
+
+    // Then make sure the anchor position after the undo is the same as the original:
+    sal_Int32 nActual = rSpz[0]->GetAnchor().GetContentAnchor()->nContent.GetIndex();
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 3
+    // - Actual  : 2
+    // i.e. the anchor position was left unchanged by the undo.
+    CPPUNIT_ASSERT_EQUAL(nExpected, nActual);
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */