627304604842bd55a71c9a5de3d280a4903016b3
Andreas Heinisch <andreas.heinisch@yahoo.de>
2021-09-29 18:19:35 +0200

    tdf#143575, tdf#143974 - Use rtl::math::doubleToUString to convert numbers to strings
    Change-Id: I1427dbd49af680a1bf386410977c9cb2b0a3961e
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/122831
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
    Reviewed-by: Andreas Heinisch <andreas.heinisch@yahoo.de>
    Tested-by: Andreas Heinisch <andreas.heinisch@yahoo.de>
    Signed-off-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- basic/qa/basic_coverage/test_cstr_method.vb
+++ basic/qa/basic_coverage/test_cstr_method.vb
@@ -6,11 +6,29 @@
 ' file, You can obtain one at http://mozilla.org/MPL/2.0/.
 '
 
-Function doUnitTest as Integer
-    ' CSTR
-    If (CStr(100) <> "100") Then
-        doUnitTest = 0
-    Else
-        doUnitTest = 1
-    End If
+Function doUnitTest() As String
+    TestUtil.TestInit
+    verify_testCStr
+    doUnitTest = TestUtil.GetResult()
 End Function
+
+Sub verify_testCStr
+    On Error GoTo errorHandler
+
+    ' CSTR
+    TestUtil.AssertEqual(CStr(100), "100", "CStr(100)")
+
+    ' tdf#143575 - round string to their nearest double representation
+    ' Without the fix in place, this test would have failed with:
+    ' - Expected: 691.2
+    ' - Actual  : 691.2000000000001
+    TestUtil.AssertEqual(CStr(691.2), "691.2", "CStr(691.2)")
+    ' Without the fix in place, this test would have failed with:
+    ' - Expected: 691.2
+    ' - Actual  : 691.1999999999999
+    TestUtil.AssertEqual(CStr(123.4 + 567.8), "691.2", "CStr(123.4 + 567.8)")
+
+    Exit Sub
+errorHandler:
+    TestUtil.ReportErrorHandler("verify_testCStr", Err, Error$, Erl)
+End Sub
