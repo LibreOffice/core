154f98055a1af4fd738f760bd4df47d81aef931c
Vasily Melenchuk <vasily.melenchuk@cib.de>
2021-09-30 14:10:12 +0200

    sc: colorfilter dangling pointer fixed
    Upper call to aParam.RemoveAllEntriesByField can invalidate
    pEntry, making further calls to pEntry->SetQueryBy* illegal.
    
    Anyway at this branch we are disabling color filter, so this
    is not required.
    
    Change-Id: Ia595649975b659d28056ba3f3658c306e63088f5

--- sc/source/ui/view/gridwin.cxx
+++ sc/source/ui/view/gridwin.cxx
@@ -969,6 +969,7 @@ void ScGridWindow::UpdateAutoFilterFromMenu(AutoFilterMode eMode)
                 if (nSelected == nActive)
                 {
                     aParam.RemoveAllEntriesByField(rPos.Col());
+                    pEntry = nullptr;   // invalidated by RemoveAllEntriesByField call
 
                     // tdf#46184 reset filter options to default values
                     aParam.eSearchType = utl::SearchParam::SearchType::Normal;
@@ -976,16 +977,18 @@ void ScGridWindow::UpdateAutoFilterFromMenu(AutoFilterMode eMode)
                     aParam.bDuplicate = true;
                     aParam.bInplace = true;
                 }
-
-                // Get selected color from set
-                std::set<Color>::iterator it = aColors.begin();
-                std::advance(it, nSelected - 1);
-                Color selectedColor = *it;
-
-                if (eMode == AutoFilterMode::TextColor)
-                    pEntry->SetQueryByTextColor(selectedColor);
                 else
-                    pEntry->SetQueryByBackgroundColor(selectedColor);
+                {
+                    // Get selected color from set
+                    std::set<Color>::iterator it = aColors.begin();
+                    std::advance(it, nSelected - 1);
+                    Color selectedColor = *it;
+
+                    if (eMode == AutoFilterMode::TextColor)
+                        pEntry->SetQueryByTextColor(selectedColor);
+                    else
+                        pEntry->SetQueryByBackgroundColor(selectedColor);
+                }
             }
 
             break;
