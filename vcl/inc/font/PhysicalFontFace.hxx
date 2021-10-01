cc81b6353ec1983b652d1c370ee80d77b5f8d1e8
Chris Sherlock <chris.sherlock79@gmail.com>
2021-10-01 20:36:17 +1000

    tdf#124176 vcl: Use pragma once instead of include guards
    Change-Id: I0bd1384df1f022bcd03e586bd9e380e432a46313
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/122909
    Tested-by: Mike Kaganski <mike.kaganski@collabora.com>
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

--- vcl/inc/font/PhysicalFontFace.hxx
+++ vcl/inc/font/PhysicalFontFace.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_VCL_INC_PHYSICALFONTFACE_HXX
-#define INCLUDED_VCL_INC_PHYSICALFONTFACE_HXX
+#pragma once
 
 #include <sal/config.h>
 
@@ -84,8 +83,6 @@ protected:
 };
 
 }
-
-#endif // INCLUDED_VCL_INC_PHYSICALFONTFACE_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
 
