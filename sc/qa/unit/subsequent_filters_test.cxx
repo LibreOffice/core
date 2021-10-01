LibreOffice/core

commit ed88b144ce24b9a733d4a9ab6614307c96537baa

add all imported properties to ScDBData, fdo#40426

sc/source/filter/xml/xmldrani.cxx
@@ -148,6 +148,8 @@ ScXMLDatabaseRangeContext::ScXMLDatabaseRangeContext( ScXMLImport& rImport,
     bSubTotalsEnabledUserList(false),
     bSubTotalsAscending(true),
     bFilterConditionSourceRange(false),
+    bHasHeader(true),
+    bByRow(false),
     meRangeType(ScDBCollection::GlobalNamed)
 {
     nSourceType = sheet::DataImportMode_NONE;
@@ -190,12 +192,14 @@ ScXMLDatabaseRangeContext::ScXMLDatabaseRangeContext( ScXMLImport& rImport,
             break;
             case XML_TOK_DATABASE_RANGE_ATTR_ORIENTATION :
             {
-                mpQueryParam->bByRow = !IsXMLToken(sValue, XML_COLUMN);
+                bByRow = !IsXMLToken(sValue, XML_COLUMN);
+                mpQueryParam->bByRow = bByRow;
             }
             break;
             case XML_TOK_DATABASE_RANGE_ATTR_CONTAINS_HEADER :
             {
-                mpQueryParam->bHasHeader = IsXMLToken(sValue, XML_TRUE);
+                bHasHeader = IsXMLToken(sValue, XML_TRUE);
+                mpQueryParam->bHasHeader = bHasHeader;
             }
             break;
             case XML_TOK_DATABASE_RANGE_ATTR_DISPLAY_FILTER_BUTTONS :
@@ -303,7 +307,7 @@ ScDBData* ScXMLDatabaseRangeContext::ConvertToDBData(const OUString& rName)
 
     SAL_WNODEPRECATED_DECLARATIONS_PUSH
     ::std::auto_ptr<ScDBData> pData(
-        new ScDBData(rName, maRange.aStart.Tab(), maRange.aStart.Col(), maRange.aStart.Row(), maRange.aEnd.Col(), maRange.aEnd.Row()));
+        new ScDBData(rName, maRange.aStart.Tab(), maRange.aStart.Col(), maRange.aStart.Row(), maRange.aEnd.Col(), maRange.aEnd.Row(), bByRow, bHasHeader));
     SAL_WNODEPRECATED_DECLARATIONS_POP
 
     pData->SetAutoFilter(bAutoFilter);

sc/source/filter/xml/xmldrani.hxx
@@ -106,6 +106,8 @@ class ScXMLDatabaseRangeContext : public SvXMLImportContext
     bool            bSubTotalsEnabledUserList;
     bool            bSubTotalsAscending;
     bool            bFilterConditionSourceRange;
+    bool            bHasHeader;
+    bool            bByRow;
     ScDBCollection::RangeType meRangeType;
 
     const ScXMLImport& GetScImport() const { return (const ScXMLImport&)GetImport(); }

commit d716f9d2e8752a375247e18931df549ad7b15b2f

tdf#144758: default value for 'table:orientation' attribute is 'row'
See [OpenDocument-v1.3-part3] section 19.690.3 <table:database-range>.

Regression after commit ed88b144ce24b9a733d4a9ab6614307c96537baa.

Change-Id: I6a12a0ad13405bc388efb7c2c3bbf31e89905620

sc/source/filter/xml/xmldrani.cxx
@@ -103,7 +103,7 @@ ScXMLDatabaseRangeContext::ScXMLDatabaseRangeContext( ScXMLImport& rImport,
     bSubTotalsAscending(true),
     bFilterConditionSourceRange(false),
     bHasHeader(true),
-    bByRow(false),
+    bByRow(true),
     meRangeType(ScDBCollection::GlobalNamed)
 {
     if( rAttrList.is() )

sc/qa/unit/data/fods/tdf144758-dbdata-no-orientation.fods
@@ -1,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8"?>
+
+<office:document xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0" xmlns:table="urn:oasis:names:tc:opendocument:xmlns:table:1.0" office:version="1.3" office:mimetype="application/vnd.oasis.opendocument.spreadsheet">
+ <office:body>
+  <office:spreadsheet>
+   <table:table table:name="Sheet1">
+    <table:table-column/>
+    <table:table-row>
+     <table:table-cell/>
+    </table:table-row>
+   </table:table>
+   <table:database-ranges>
+    <table:database-range table:name="__Anonymous_Sheet_DB__0" table:target-range-address="Sheet1.A1:Sheet1.A1" table:contains-header="false"/>
+   </table:database-ranges>
+  </office:spreadsheet>
+ </office:body>
+</office:document>

sc/qa/unit/subsequent_filters_test.cxx
@@ -71,6 +71,7 @@
 #include <detfunc.hxx>
 #include <cellmergeoption.hxx>
 #include <undoblk.hxx>
+#include <sortparam.hxx>
 
 #include <orcusfilters.hxx>
 #include <filter.hxx>
@@ -217,6 +218,7 @@ public:
     void testTdf129681();
     void testTdf111974XLSM();
     void testEscapedUnicodeXLSX();
+    void testTdf144758_DBDataDefaultOrientation();
 
     CPPUNIT_TEST_SUITE(ScFiltersTest);
     CPPUNIT_TEST(testCondFormatOperatorsSameRangeXLSX);
@@ -320,6 +322,7 @@ public:
     CPPUNIT_TEST(testTdf129681);
     CPPUNIT_TEST(testTdf111974XLSM);
     CPPUNIT_TEST(testEscapedUnicodeXLSX);
+    CPPUNIT_TEST(testTdf144758_DBDataDefaultOrientation);
 
     CPPUNIT_TEST_SUITE_END();
 
@@ -3119,6 +3122,24 @@ void ScFiltersTest::testEscapedUnicodeXLSX()
 
     // Without the fix, there would be "_x000D_" after every new-line char.
     CPPUNIT_ASSERT_EQUAL(OUString("Line 1\nLine 2\nLine 3\nLine 4"), rDoc.GetString(1, 1, 0));
+
+    xDocSh->DoClose();
+}
+
+void ScFiltersTest::testTdf144758_DBDataDefaultOrientation()
+{
+    ScDocShellRef xDocSh = loadDoc(u"tdf144758-dbdata-no-orientation.", FORMAT_FODS);
+    CPPUNIT_ASSERT(xDocSh);
+    ScDocument& rDoc = xDocSh->GetDocument();
+    ScDBData* pAnonDBData = rDoc.GetAnonymousDBData(0);
+    CPPUNIT_ASSERT(pAnonDBData);
+
+    ScSortParam aSortParam;
+    pAnonDBData->GetSortParam(aSortParam);
+
+    // Without the fix, the default value for bByRow (in absence of 'table:orientation' attribute
+    // in 'table:database-range' element) was false
+    CPPUNIT_ASSERT(aSortParam.bByRow);
 
     xDocSh->DoClose();
 }