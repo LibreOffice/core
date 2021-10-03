8287e56e8487ace7b54f9eee0eace8052f93d079
Chris Sherlock <chris.sherlock79@gmail.com>
2021-10-03 13:35:41 +1100

    vcl: rename GetGradientStepCount() to GetGradientStepIncrement()
    Change-Id: Icc43d7b81f152a57ba5a5774f7609000da6db646

--- include/vcl/outdev.hxx
+++ include/vcl/outdev.hxx
@@ -796,7 +796,7 @@ protected:
 
     virtual bool                UsePolyPolygonForComplexGradient() = 0;
 
-    virtual tools::Long                GetGradientStepCount( tools::Long nMinRect );
+    virtual tools::Long GetGradientStepIncrement(tools::Long nMinRect);
 
 private:
 
