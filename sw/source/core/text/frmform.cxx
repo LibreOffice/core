c0553f63239f258b7e58176d2d44b96f2b7b3b72
Aron Budea <aron.budea@collabora.com>
2021-10-04 23:35:31 +0200

    Revert "sw: eliminate recently added static_cast<SwTwips>() calls"
    Needed to revert 301278b656e76b6f42af5cf8a6f5c6c02acfffeb
    in libreoffice-7-2.
    
    This reverts commit 9e075acf2bf1ce6c43fdf5b601507ee0663bd691.
    
    Change-Id: I79460e19dadc5a9def5b0ee89cd1cbb5c3ad699c
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/123094
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
    Reviewed-by: Andras Timar <andras.timar@collabora.com>
    Tested-by: Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>

--- sw/source/core/text/frmform.cxx
+++ sw/source/core/text/frmform.cxx
@@ -1205,7 +1205,7 @@ bool SwTextFrame::FormatLine( SwTextFormatter &rLine, const bool bPrev )
                   bOldHyph == pNew->IsEndHyph();
     if ( bUnChg && !bPrev )
     {
-        const tools::Long nWidthDiff = nOldWidth > o3tl::narrowing<SwTwips>(pNew->Width())
+        const tools::Long nWidthDiff = nOldWidth > static_cast<SwTwips>(pNew->Width())
                                 ? nOldWidth - pNew->Width()
                                 : pNew->Width() - nOldWidth;
 
@@ -1259,7 +1259,7 @@ bool SwTextFrame::FormatLine( SwTextFormatter &rLine, const bool bPrev )
                 rLine.SetUnclipped( false );
             }
         }
-        SwTwips nRght = std::max( nOldWidth, o3tl::narrowing<SwTwips>(pNew->Width()) +
+        SwTwips nRght = std::max( nOldWidth, static_cast<SwTwips>(pNew->Width()) +
                              pNew->GetHangingMargin() );
         SwViewShell *pSh = getRootFrame()->GetCurrShell();
         const SwViewOption *pOpt = pSh ? pSh->GetViewOptions() : nullptr;
