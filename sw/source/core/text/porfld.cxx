15bdf9d77cef4a898e4d64d2d56cd83df2c3a06a
Aron Budea <aron.budea@collabora.com>
2021-10-04 23:35:31 +0200

    Revert "sw: eliminate recently added static_cast<SwTwips>() calls"
    Needed to revert 301278b656e76b6f42af5cf8a6f5c6c02acfffeb
    in libreoffice-7-2.
    
    This reverts commit 9e075acf2bf1ce6c43fdf5b601507ee0663bd691.
    
    Change-Id: I79460e19dadc5a9def5b0ee89cd1cbb5c3ad699c
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/123089
    Tested-by: Jenkins
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

--- sw/source/core/text/porfld.cxx
+++ sw/source/core/text/porfld.cxx
@@ -594,10 +594,10 @@ bool SwNumberPortion::Format( SwTextFormatInfo &rInf )
         // Height has to be changed
         if ( rInf.IsMulti() )
         {
-            if ( o3tl::narrowing<SwTwips>(Height()) < nDiff )
+            if ( static_cast<SwTwips>(Height()) < nDiff )
                 Height( sal_uInt16( nDiff ) );
         }
-        else if( o3tl::narrowing<SwTwips>(Width()) < nDiff )
+        else if( static_cast<SwTwips>(Width()) < nDiff )
             Width( sal_uInt16(nDiff) );
     }
     return bFull;
@@ -868,7 +868,7 @@ bool SwGrfNumPortion::Format( SwTextFormatInfo &rInf )
             SetHide( true );
     }
 
-    if( o3tl::narrowing<SwTwips>(Width()) < nDiff )
+    if( static_cast<SwTwips>(Width()) < nDiff )
         Width( sal_uInt16(nDiff) );
     return bFull;
 }
@@ -1257,7 +1257,7 @@ bool SwCombinedPortion::Format( SwTextFormatInfo &rInf )
         Height( Height() + nMainAscent - GetAscent() );
         SetAscent( nMainAscent );
     }
-    if( o3tl::narrowing<SwTwips>(Height()) < nMainAscent + nMainDescent )
+    if( static_cast<SwTwips>(Height()) < nMainAscent + nMainDescent )
         Height( nMainAscent + nMainDescent );
 
     // We calculate the x positions of the characters in both lines...
