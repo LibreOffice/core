e7e3277cf3a865e27d17fb19e58c85dd62f0c3c1
Chris Sherlock <chris.sherlock79@gmail.com>
2021-10-05 18:09:03 +1100

    vcl: introduce MetaGradientContainerAction
    Introduce a metaaction that acts as a container for both linear and
    complex actions.
    
    Note that the clipping mechanism differs depending on whether you are
    rendering to a screen or a printer, so to cater for this I have
    made DrawGradientToMetafile() a virtual function and passed a printing
    parameter to the container action.
    
    Refactored tests.
    
    Change-Id: I4621a453bd3ff213af2a87c1d2704d0028a425e5

--- vcl/source/outdev/gradient.cxx
+++ vcl/source/outdev/gradient.cxx
@@ -159,94 +159,18 @@ void OutputDevice::DrawGradient( const tools::PolyPolygon& rPolyPoly,
     }
 }
 
-void OutputDevice::ClipAndDrawGradientMetafile ( const Gradient &rGradient, const tools::PolyPolygon &rPolyPoly )
+void OutputDevice::DrawGradientToMetafile(tools::PolyPolygon const& rPolyPoly, Gradient const& rGradient)
 {
-    const tools::Rectangle aBoundRect( rPolyPoly.GetBoundRect() );
-    const bool  bOldOutput = IsOutputEnabled();
-
-    EnableOutput( false );
-    Push( vcl::PushFlags::RASTEROP );
-    SetRasterOp( RasterOp::Xor );
-    DrawGradient( aBoundRect, rGradient );
-    SetFillColor( COL_BLACK );
-    SetRasterOp( RasterOp::N0 );
-    DrawPolyPolygon( rPolyPoly );
-    SetRasterOp( RasterOp::Xor );
-    DrawGradient( aBoundRect, rGradient );
-    Pop();
-    EnableOutput( bOldOutput );
-}
-
-void OutputDevice::DrawGradientToMetafile ( const tools::PolyPolygon& rPolyPoly,
-                                            const Gradient& rGradient )
-{
-    assert(!is_double_buffered_window());
-
-    if ( !mpMetaFile )
-        return;
-
-    if ( !(rPolyPoly.Count() && rPolyPoly[ 0 ].GetSize()) )
-        return;
-
-    Gradient aGradient( rGradient );
-
-    if (mnDrawMode & DrawModeFlags::GrayGradient)
-        aGradient.MakeGrayscale();
-
-    const tools::Rectangle aBoundRect( rPolyPoly.GetBoundRect() );
-
-    if ( rPolyPoly.IsRect() )
-    {
-        mpMetaFile->AddAction( new MetaGradientAction( aBoundRect, aGradient ) );
-    }
-    else
-    {
-        mpMetaFile->AddAction( new MetaCommentAction( "XGRAD_SEQ_BEGIN" ) );
-        mpMetaFile->AddAction( new MetaGradientExAction( rPolyPoly, rGradient ) );
-
-        ClipAndDrawGradientMetafile ( rGradient, rPolyPoly );
-
-        mpMetaFile->AddAction( new MetaCommentAction( "XGRAD_SEQ_END" ) );
-    }
-
-    if( !IsDeviceOutputNecessary() || ImplIsRecordLayout() )
-        return;
-
-    // Clip and then draw the gradient
-    if( tools::Rectangle( PixelToLogic( Point() ), GetOutputSize() ).IsEmpty() )
-        return;
-
-    // convert rectangle to pixels
-    tools::Rectangle aRect( ImplLogicToDevicePixel( aBoundRect ) );
-    aRect.Justify();
-
-    // do nothing if the rectangle is empty
-    if ( aRect.IsEmpty() )
-        return;
-
-    if( mbOutputClipped )
-        return;
-
-    // calculate step count if necessary
-    if ( !aGradient.GetSteps() )
-        aGradient.SetSteps( GRADIENT_DEFAULT_STEPCOUNT );
-
-    if ( rPolyPoly.IsRect() )
-    {
-        // because we draw with no border line, we have to expand gradient
-        // rect to avoid missing lines on the right and bottom edge
-        aRect.AdjustLeft( -1 );
-        aRect.AdjustTop( -1 );
-        aRect.AdjustRight( 1 );
-        aRect.AdjustBottom( 1 );
-    }
+    tools::Rectangle aBoundRect(ImplLogicToDevicePixel(rPolyPoly.GetBoundRect()));
 
     // if the clipping polypolygon is a rectangle, then it's the same size as the bounding of the
     // polypolygon, so pass in a NULL for the clipping parameter
-    if( aGradient.GetStyle() == GradientStyle::Linear || rGradient.GetStyle() == GradientStyle::Axial )
-        mpMetaFile->AddAction(new MetaLinearGradientAction(aRect, aGradient, GetLinearGradientSteps(rGradient, aRect, true/*bMtf*/)));
+    if (rGradient.GetStyle() == GradientStyle::Linear || rGradient.GetStyle() == GradientStyle::Axial)
+        mpMetaFile->AddAction(new MetaGradientContainerAction(rPolyPoly,
+                    aBoundRect, rGradient, GetLinearGradientSteps(rGradient, aBoundRect, true/*bMtf*/), mnDrawMode));
     else
-        mpMetaFile->AddAction(new MetaComplexGradientAction(aRect, aGradient, GetComplexGradientSteps(rGradient, aRect, true)));
+        mpMetaFile->AddAction(new MetaGradientContainerAction(rPolyPoly,
+                    aBoundRect, rGradient, GetComplexGradientSteps(rGradient, aBoundRect, true), mnDrawMode));
 }
 
 namespace
