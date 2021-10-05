LibreOffice/core

commit 9994120c8d0fe8c5a029390ad7411b99c18ff5c9

tdf#144347 sw: fix tracked deletion of table with tracked content
When a table contained tracked content already, its tracked
deletion (or cut) didn't work correctly, if the deletion
contained also the text before or after the table: accepting
the deletion left an empty table in the text, also Hide Changes
showed an empty table before that in the place of the deleted
text.

Follow-up of commit f481c2c8e74bded11fac754e493560391229dbcd
"df#144057 sw track changes: hide deleted table rows".

Change-Id: If3294fe0316df7c065e7ff63290c1723386ec7ba
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/122382
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/source/core/docnode/ndtbl1.cxx
@@ -538,14 +538,21 @@ bool SwDoc::GetRowBackground( const SwCursor& rCursor, std::unique_ptr<SvxBrushI
     return bRet;
 }
 
-void SwDoc::SetRowNotTracked( const SwCursor& rCursor, const SvxPrintItem &rNew )
+void SwDoc::SetRowNotTracked( const SwCursor& rCursor, const SvxPrintItem &rNew, bool bAll )
 {
     SwTableNode* pTableNd = rCursor.GetPoint()->nNode.GetNode().FindTableNode();
     if( !pTableNd )
         return;
 
     std::vector<SwTableLine*> aRowArr; // For Lines collecting
-    ::lcl_CollectLines( aRowArr, rCursor, true );
+    if ( bAll )
+    {
+        const SwTableLines &rLines = pTableNd->GetTable().GetTabLines();
+        for ( auto pLine : rLines )
+            aRowArr.push_back(pLine);
+    }
+    else
+        ::lcl_CollectLines( aRowArr, rCursor, true );
 
     if( aRowArr.empty() )
         return;

sw/inc/doc.hxx
@@ -1489,7 +1489,8 @@ public:
     void SetRowBackground( const SwCursor& rCursor, const SvxBrushItem &rNew );
     static bool GetRowBackground( const SwCursor& rCursor, std::unique_ptr<SvxBrushItem>& rToFill );
     /// rNotTracked = false means that the row was deleted or inserted with its tracked cell content
-    void SetRowNotTracked( const SwCursor& rCursor, const SvxPrintItem &rNotTracked );
+    /// bAll: delete all table rows without selection
+    void SetRowNotTracked( const SwCursor& rCursor, const SvxPrintItem &rNotTracked, bool bAll = false );
     void SetTabBorders( const SwCursor& rCursor, const SfxItemSet& rSet );
     void SetTabLineStyle( const SwCursor& rCursor,
                           const Color* pColor, bool bSetLine,

sw/qa/extras/layout/layout.cxx
@@ -43,6 +43,7 @@
 #include <docsh.hxx>
 #include <IDocumentLayoutAccess.hxx>
 #include <IDocumentDrawModelAccess.hxx>
+#include <IDocumentRedlineAccess.hxx>
 #include <textboxhelper.hxx>
 #include <unoframe.hxx>
 #include <drawdoc.hxx>
@@ -2334,6 +2335,60 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf144057)
     assertXPath(pXmlDoc, "/root/page[2]/body/tab/row[6]/cell/txt/Text", "Portion", "A12");
     assertXPath(pXmlDoc, "/root/page[3]/body/tab/row[6]/cell/txt/Text", "Portion", "B6");
     assertXPath(pXmlDoc, "/root/page[4]/body/tab/row[6]/cell/txt/Text", "Portion", "B12");
+}
+
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf144347)
+{
+    createSwDoc(DATA_DIRECTORY, "tdf144057.fodt");
+    SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument*>(mxComponent.get());
+    CPPUNIT_ASSERT(pTextDoc);
+    SwDoc* pDoc(pTextDoc->GetDocShell()->GetDoc());
+    SwRootFrame* pLayout(pDoc->getIDocumentLayoutAccess().GetCurrentLayout());
+
+    // enable redlining
+    dispatchCommand(mxComponent, ".uno:TrackChanges", {});
+    CPPUNIT_ASSERT_MESSAGE("redlining should be on",
+                           pDoc->getIDocumentRedlineAccess().IsRedlineOn());
+    CPPUNIT_ASSERT(!pLayout->IsHideRedlines());
+
+    // remove first table
+    SwEditShell* const pEditShell(pDoc->GetEditShell());
+    for (int i = 0; i < 12; ++i)
+        pEditShell->AcceptRedline(0);
+
+    pDoc->getIDocumentLayoutAccess().GetCurrentViewShell()->CalcLayout();
+    discardDumpedLayout();
+    xmlDocUniquePtr pXmlDoc = parseLayoutDump();
+    // show tracked row deletions
+    assertXPath(pXmlDoc, "/root/page", 2);
+    assertXPath(pXmlDoc, "/root/page[1]/body/tab", 1);
+
+    // select all the text, including the texts before and after the table
+    // Note: this table contains tracked changes, which was a
+    // problem for the original OOo implementation of track changes,
+    // resulting empty tables after accepting the deletion of these tables.
+    dispatchCommand(mxComponent, ".uno:SelectAll", {});
+    dispatchCommand(mxComponent, ".uno:SelectAll", {});
+    dispatchCommand(mxComponent, ".uno:Delete", {});
+    pDoc->getIDocumentLayoutAccess().GetCurrentViewShell()->CalcLayout();
+    discardDumpedLayout();
+    pXmlDoc = parseLayoutDump();
+
+    // table is deleted with change tracking: it still exists
+    assertXPath(pXmlDoc, "/root/page", 2);
+    assertXPath(pXmlDoc, "/root/page[1]/body/tab", 1);
+
+    // accept all deletions, removing the table completely
+    while (pEditShell->GetRedlineCount() > 0)
+        pEditShell->AcceptRedline(0);
+
+    pDoc->getIDocumentLayoutAccess().GetCurrentViewShell()->CalcLayout();
+    discardDumpedLayout();
+    pXmlDoc = parseLayoutDump();
+
+    assertXPath(pXmlDoc, "/root/page", 1);
+    // This was 1 (bad empty table)
+    assertXPath(pXmlDoc, "/root/page[1]/body/tab", 0);
 }
 
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf109137)

sw/source/core/doc/DocumentRedlineManager.cxx
@@ -2265,6 +2265,29 @@ DocumentRedlineManager::AppendRedline(SwRangeRedline* pNewRedl, bool const bCall
                             pTextNd = aIdx.GetNode().GetContentNode();
                     }
                 }
+
+                // delete tables of the deletion explicitly, to avoid
+                // remaining empty tables after accepting the rejection
+                // and visible empty tables in Hide Changes mode
+                // (this was the case, if tables have already contained
+                // other tracked changes)
+                // FIXME: because of recursive nature of AppendRedline,
+                // this doesn't work for selections with multiple tables
+                if ( m_rDoc.GetIDocumentUndoRedo().DoesUndo() )
+                {
+                    SwNodeIndex aSttIdx( pStt->nNode.GetNode() );
+                    SwNodeIndex aEndIdx( pEnd->nNode.GetNode() );
+                    while ( aSttIdx < aEndIdx )
+                    {
+                        if ( aSttIdx.GetNode().IsTableNode() )
+                        {
+                            SvxPrintItem aNotTracked(RES_PRINT, false);
+                            SwCursor aCursor( SwPosition(aSttIdx), nullptr );
+                            m_rDoc.SetRowNotTracked( aCursor, aNotTracked, /*bAll=*/true );
+                        }
+                        ++aSttIdx;
+                    }
+                }
             }
             bool const ret = maRedlineTable.Insert( pNewRedl );
             assert(ret || !pNewRedl);

commit 2658110af24029a7936caa89fa444751c988eba3

tdf#144748 sw: fix crash at Undo of deletion with empty table rows
When a table with empty rows is part of a bigger deletion,
Undo of the deletion resulted a crash, because the deletion
created extra redlines for the empty rows within the bigger
redline.

Regression from commit 9994120c8d0fe8c5a029390ad7411b99c18ff5c9
"tdf#144347 sw: fix tracked deletion of table with tracked content".
See also commit 99059a1ececa3621c2fe46fabdd79eed9d626c42
"tdf#143359 sw: track deletion of empty table rows".

Change-Id: I0e33e169c1ffe60022a6b66a6d70fdd9c0a9332a

sw/source/core/docnode/ndtbl1.cxx
@@ -572,7 +572,9 @@ void SwDoc::SetRowNotTracked( const SwCursor& rCursor, const SvxPrintItem &rNew,
         ::lcl_ProcessRowAttr( aFormatCmp, pLn, rNew );
         // as a workaround for the rows without text content,
         // add a redline with invisible text CH_TXT_TRACKED_DUMMY_CHAR
-        if (pLn->IsEmpty())
+        // (unless the table is part of a bigger deletion, where the
+        // new redline can cause a problem)
+        if (!bAll && pLn->IsEmpty())
         {
             SwNodeIndex aInsPos( *(pLn->GetTabBoxes()[0]->GetSttNd()), 1 );
             RedlineFlags eOld = getIDocumentRedlineAccess().GetRedlineFlags();

sw/qa/extras/uiwriter/uiwriter2.cxx
@@ -4551,6 +4551,50 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest2, testTdf143215)
     CPPUNIT_ASSERT_EQUAL(sal_Int32(3), xTable->getRows()->getCount());
 }
 
+CPPUNIT_TEST_FIXTURE(SwUiWriterTest2, testTdf144748)
+{
+    // load a table with an empty row, and an empty line before the table
+    // (to allow the easy selection of the full text with the table)
+    SwDoc* pDoc = createSwDoc(DATA_DIRECTORY, "tdf144748.fodt");
+
+    // turn on red-lining and show changes
+    pDoc->getIDocumentRedlineAccess().SetRedlineFlags(RedlineFlags::On | RedlineFlags::ShowDelete
+                                                      | RedlineFlags::ShowInsert);
+    CPPUNIT_ASSERT_MESSAGE("redlining should be on",
+                           pDoc->getIDocumentRedlineAccess().IsRedlineOn());
+    CPPUNIT_ASSERT_MESSAGE(
+        "redlines should be visible",
+        IDocumentRedlineAccess::IsShowChanges(pDoc->getIDocumentRedlineAccess().GetRedlineFlags()));
+
+    uno::Reference<text::XTextTablesSupplier> xTextTablesSupplier(mxComponent, uno::UNO_QUERY);
+    uno::Reference<container::XIndexAccess> xTables(xTextTablesSupplier->getTextTables(),
+                                                    uno::UNO_QUERY);
+    // there is a table in the text
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(1), xTables->getCount());
+
+    // delete full text with the table and check Undo
+
+    dispatchCommand(mxComponent, ".uno:SelectAll", {});
+    dispatchCommand(mxComponent, ".uno:Delete", {});
+    // this crashed LibreOffice
+    dispatchCommand(mxComponent, ".uno:Undo", {});
+
+    // redo and check redline usage
+
+    dispatchCommand(mxComponent, ".uno:Redo", {});
+    SwEditShell* const pEditShell(pDoc->GetEditShell());
+    // This was 2 (bad extra redline for the empty row of the deleted table)
+    CPPUNIT_ASSERT_EQUAL(static_cast<SwRedlineTable::size_type>(1), pEditShell->GetRedlineCount());
+
+    // accept deletion of the text, including the table with the empty row
+
+    IDocumentRedlineAccess& rIDRA(pDoc->getIDocumentRedlineAccess());
+    rIDRA.AcceptAllRedline(true);
+
+    // no table left in the text
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(0), xTables->getCount());
+}
+
 CPPUNIT_TEST_FIXTURE(SwUiWriterTest2, testTdf128335)
 {
     // Load the bugdoc, which has 3 textboxes.