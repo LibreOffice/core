95be1f8f85e51911e12c075c545ce9ceb9866a9d
Luboš Luňák <l.lunak@collabora.com>
2021-10-06 12:57:15 +0200

    ScRangeList::UpdateReference() join all ranges properly (tdf#140901)
    This is basically a revert of 6eb8634a9f62bfe486ecd2f46, which
    made this Join() just the last range, probably under the assumption
    that the function is always called with just one range to update,
    or to avoid the possibility that Join() removes several items
    from the list, breaking the next step of the loop. But DeleteArea()
    may split several ranges, so we need to make sure to Join()
    all of them.
    
    Change-Id: Iea124142335ccdc8fa578344cddce8670c27573d

--- sc/source/ui/unoobj/chart2uno.cxx
+++ sc/source/ui/unoobj/chart2uno.cxx
@@ -2778,7 +2778,7 @@ void ScChart2DataSequence::Notify( SfxBroadcaster& /*rBC*/, const SfxHint& rHint
             }
         }
 
-        OSL_ENSURE(m_pRangeIndices->size() == aRanges.size(),
+        assert(m_pRangeIndices->size() == aRanges.size() &&
                    "range list and range index list have different sizes.");
 
         unique_ptr<ScRangeList> pUndoRanges;
@@ -2791,7 +2791,7 @@ void ScChart2DataSequence::Notify( SfxBroadcaster& /*rBC*/, const SfxHint& rHint
 
         if (bChanged)
         {
-            OSL_ENSURE(m_pRangeIndices->size() == aRanges.size(),
+            assert(m_pRangeIndices->size() == aRanges.size() &&
                        "range list and range index list have different sizes after the reference update.");
 
             // Bring the change back from the range list to the token list.
@@ -2813,7 +2813,7 @@ void ScChart2DataSequence::Notify( SfxBroadcaster& /*rBC*/, const SfxHint& rHint
 
             if (!m_pRangeIndices || m_pRangeIndices->empty())
             {
-                OSL_FAIL(" faulty range indices");
+                assert(false && " faulty range indices");
                 break;
             }
 
@@ -2822,7 +2822,7 @@ void ScChart2DataSequence::Notify( SfxBroadcaster& /*rBC*/, const SfxHint& rHint
             size_t nCount = rRanges.size();
             if (nCount != m_pRangeIndices->size())
             {
-                OSL_FAIL("range count and range index count differ.");
+                assert(false && "range count and range index count differ.");
                 break;
             }
 
