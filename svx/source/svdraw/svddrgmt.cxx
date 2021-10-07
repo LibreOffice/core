38ac858f91bc6374894d511a9f543bed93281686
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2021-10-07 16:48:46 +0200

    svx: Change text edit to use a view-local undo manager
    This is needed so we can still change objects in another view
    (for example add them or move them around the page), while we text
    edit in the current view.
    
    It also makes sure the undo stack doesn't get jumbled when two or
    more views are editing at the same time.
    
    Change-Id: I04edb4f91d7e111a490c946f7121cbca75f818d7

--- svx/source/svdraw/svddrgmt.cxx
+++ svx/source/svdraw/svddrgmt.cxx
@@ -1345,7 +1345,7 @@ bool SdrDragObjOwn::EndSdrDrag(bool /*bCopy*/)
 
         if( bUndo )
         {
-            getSdrDragView().EndTextEditAllViews();
+            getSdrDragView().EndTextEditCurrentView();
             if(!getSdrDragView().IsInsObjPoint() && pObj->IsInserted() )
             {
                 if (DragStat().IsEndDragChangesAttributes())
