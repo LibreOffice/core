LibreOffice/core

commit a19dc0d9cc35ae1e200f51d86950595f6771e9c3

sw_redlinehide_3: update frames in MoveParagraph()
... if redlining is disabled and MoveNodeRange is called.

Change-Id: I8c4e35b1b783446ab9bd888599bcce44222857e8

sw/source/core/doc/DocumentContentOperationsManager.cxx
@@ -822,6 +822,10 @@ namespace
         {
             rSvRedLine.SetPos( nInsPos );
             pDoc->getIDocumentRedlineAccess().AppendRedline( rSvRedLine.pRedl, true );
+            if (rSvRedLine.pRedl->GetType() == nsRedlineType_t::REDLINE_DELETE)
+            {
+                UpdateFramesForAddDeleteRedline(*pDoc, *rSvRedLine.pRedl);
+            }
         }
 
         pDoc->getIDocumentRedlineAccess().SetRedlineFlags_intern( eOld );

commit 88cd5ddcb416a3e6b2d198a34b0d3aeabb782ab1

tdf#145093 sw track changes: fix crash at moving list items
over a tracked paragraph insertion, caused by an invalid pointer.

Note: we needed a Python UI test, because uiwriter doesn't crash
on Linux.

Likely regression from commit a19dc0d9cc35ae1e200f51d86950595f6771e9c3
"sw_redlinehide_3: update frames in MoveParagraph()".

Change-Id: I3026c9136e2fa6dcc1f6b5bad1677846a6cfe614

sw/source/core/doc/DocumentContentOperationsManager.cxx
@@ -949,8 +949,10 @@ namespace
         for(SaveRedline & rSvRedLine : rArr)
         {
             rSvRedLine.SetPos( nInsPos );
-            rDoc.getIDocumentRedlineAccess().AppendRedline( rSvRedLine.pRedl, true );
-            if (rSvRedLine.pRedl->GetType() == RedlineType::Delete)
+            IDocumentRedlineAccess::AppendResult const result(
+                rDoc.getIDocumentRedlineAccess().AppendRedline( rSvRedLine.pRedl, true ));
+            if ( IDocumentRedlineAccess::AppendResult::APPENDED == result &&
+                rSvRedLine.pRedl->GetType() == RedlineType::Delete )
             {
                 UpdateFramesForAddDeleteRedline(rDoc, *rSvRedLine.pRedl);
             }

sw/qa/uitest/writer_tests7/tdf145093.py
@@ -1,0 +1,34 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+from uitest.framework import UITestCase
+from libreoffice.uno.propertyvalue import mkPropertyValues
+from uitest.uihelper.common import select_pos
+from uitest.uihelper.common import type_text
+from uitest.uihelper.common import get_url_for_data_file
+
+class tdf145093(UITestCase):
+    def test_tdf145093(self):
+        # load the sample file
+        with self.ui_test.load_file(get_url_for_data_file("tdf39721.fodt")) as document:
+
+            # redlining should be on
+            self.xUITest.executeCommand(".uno:TrackChanges")
+
+            # Move Up/Down a list item over a tracked paragraph insertion resulted a crash
+            # (Note: not only at the end of the document, but check the original
+            # bug report temporarily, where the problem is triggered by an other problem).
+
+            self.xUITest.executeCommand(".uno:MoveDown")
+            self.xUITest.executeCommand(".uno:MoveDown")
+            # Note: Move list item from the end of the document creates an extra insertion.
+            # TODO: fix this other problem, and improve the test with an extra paragraph insertion
+            self.xUITest.executeCommand(".uno:MoveUp")
+
+            # This was a crash (using invalid pointer) both in Show Changes and Hide Changes modes
+            self.xUITest.executeCommand(".uno:MoveDown")
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab: