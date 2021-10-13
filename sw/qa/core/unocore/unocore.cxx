0fb7a4324aac356856e215e6c19521cb6871bc77
Miklos Vajna <vmiklos@collabora.com>
2021-10-13 17:00:30 +0200

    sw: fix corrupted proxy object for SwGrfNode via the view cursor UNO API
    Creating an SwXTextFrame for a graphic node will result in a wrapper
    that has no underlying SwTextNode, as the downcast from SwNode fails in
    SwXParagraphEnumerationImpl::NextElement_Impl(). This is certainly not
    intended, similar code in SwFmDrawPage::CreateShape() handles the text
    node, graphic node and OLE node cases separately.
    
    To make this more problematic, this corrupted wrapper can be still alive
    by the time we try to save to ODT, but the wrapper without an underlying
    SwTextNode will be unable to enumerat the paragraphs of the text frame,
    so it throws, making it impossible to save the document.
    
    Fix this by limiting the TextFrame property of the cursor to actual text
    frames any returning an empty reference in other cases.
    
    If there is a need to access graphic or OLE frames via that API, then
    dedicated properties can be added to expose those different nodes.
    
    (cherry picked from commit a399b05401fc35ebba4dbd07504fae4a609b3614)
    
    Conflicts:
    	sw/qa/core/unocore/unocore.cxx
    
    Change-Id: I5e97a826271b0d8a1bf262e43cd8516656b37c3a

--- sw/qa/core/unocore/unocore.cxx
+++ sw/qa/core/unocore/unocore.cxx
@@ -27,6 +27,7 @@
 #include <com/sun/star/graphic/XGraphic.hpp>
 #include <com/sun/star/style/LineSpacing.hpp>
 #include <com/sun/star/view/XSelectionSupplier.hpp>
+#include <com/sun/star/text/XTextViewCursorSupplier.hpp>
 
 #include <comphelper/propertyvalue.hxx>
 #include <toolkit/helper/vclunohelper.hxx>
@@ -42,6 +43,7 @@
 #include <flyfrm.hxx>
 #include <fmtanchr.hxx>
 #include <unotextrange.hxx>
+#include <view.hxx>
 
 using namespace ::com::sun::star;
 
@@ -106,6 +108,30 @@ CPPUNIT_TEST_FIXTURE(SwCoreUnocoreTest, flyAtParaAnchor)
         xMSF->createInstance("com.sun.star.text.Fieldmark"), uno::UNO_QUERY_THROW);
     // this crashed because the anchor didn't have SwIndex
     xText->insertTextContent(xAnchor, xFieldmark, false);
+}
+
+CPPUNIT_TEST_FIXTURE(SwCoreUnocoreTest, testViewCursorTextFrame)
+{
+    // Given a document with a graphic and holding a reference to that graphic frame:
+    mxComponent = loadFromDesktop("private:factory/swriter", "com.sun.star.text.TextDocument");
+    uno::Sequence<beans::PropertyValue> aInsertArgs = { comphelper::makePropertyValue(
+        "FileName", m_directories.getURLFromSrc(DATA_DIRECTORY) + "graphic.png") };
+    dispatchCommand(mxComponent, ".uno:InsertGraphic", aInsertArgs);
+    uno::Reference<frame::XModel> xModel(mxComponent, uno::UNO_QUERY);
+    uno::Reference<text::XTextViewCursorSupplier> xTextViewCursorSupplier(
+        xModel->getCurrentController(), uno::UNO_QUERY);
+    uno::Reference<beans::XPropertySet> xViewCursor(xTextViewCursorSupplier->getViewCursor(),
+                                                    uno::UNO_QUERY);
+    uno::Reference<beans::XPropertySet> xFrame;
+    xViewCursor->getPropertyValue("TextFrame") >>= xFrame;
+
+    // When saving to ODT, then make sure the store doesn't fail:
+    uno::Reference<frame::XStorable> xStorable(xModel, uno::UNO_QUERY);
+    uno::Sequence<beans::PropertyValue> aStoreArgs
+        = { comphelper::makePropertyValue("FilterName", OUString("writer8")) };
+    // Without the accompanying fix in place, this test would have failed with:
+    // uno.RuntimeException: "SwXParagraph: disposed or invalid ..."
+    xStorable->storeToURL(maTempFile.GetURL(), aStoreArgs);
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();
