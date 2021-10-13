f0431a7d65f9e50a2f7b1bf1a5812726b2ddbdf6
Daniel Arato (NISZ) <arato.daniel@nisz.hu>
2021-10-13 13:21:11 +0200

    tdf#144940 Calc: header/footer disabled on reload
    If you checked the "Same content on first page" checkbox in the
    Page Style dialog, on either the Header or Footer tab, then saved
    the spreadsheet in ODF and reloaded it, you found the whole header
    or footer disabled. This was due to flawed logic when assigning
    the SC_UNO_PAGE_HDRON/FTRON value: "leftness" was checked for
    but not "first pageness".
    
    Change-Id: Id20cf226039e2e80cc7ec02c2f02d6e6c81b865e

---
+++ sc/qa/uitest/calc_tests8/tdf144940.py
@@ -0,0 +1,61 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+from uitest.framework import UITestCase
+from uitest.uihelper.common import get_state_as_dict
+from uitest.uihelper.common import select_pos
+from libreoffice.uno.propertyvalue import mkPropertyValues
+
+class tdf144940(UITestCase):
+
+    def test_tdf144940_header(self):
+
+        with self.ui_test.create_doc_in_start_center("calc"):
+            xCalcDoc = self.xUITest.getTopFocusWindow()
+            gridwin = xCalcDoc.getChild("grid_window")
+            gridwin.executeAction("SELECT", mkPropertyValues({"CELL": "A1"}))
+
+            with self.ui_test.execute_dialog_through_command(".uno:PageFormatDialog") as xDialog:
+                xTabs = xDialog.getChild("tabcontrol")
+                select_pos(xTabs, "4")
+                xFirstHeaderSameBtn = xDialog.getChild("checkSameFP")
+                xFirstHeaderSameBtn.executeAction("CLICK", tuple())
+
+            self.xUITest.executeCommand(".uno:Reload")
+
+            with self.ui_test.execute_dialog_through_command(".uno:PageFormatDialog") as xDialog2:
+                xTabs = xDialog2.getChild("tabcontrol")
+                select_pos(xTabs, "4")
+                xHeaderOnBtn = xDialog2.getChild("checkHeaderOn")
+                self.assertEqual(get_state_as_dict(xHeaderOnBtn)["Selected"], "true")
+                xFirstHeaderSameBtn = xDialog2.getChild("checkSameFP")
+                self.assertEqual(get_state_as_dict(xFirstHeaderSameBtn)["Selected"], "true")
+
+    def test_tdf144940_footer(self):
+
+        with self.ui_test.create_doc_in_start_center("calc"):
+            xCalcDoc = self.xUITest.getTopFocusWindow()
+            gridwin = xCalcDoc.getChild("grid_window")
+            gridwin.executeAction("SELECT", mkPropertyValues({"CELL": "A1"}))
+
+            with self.ui_test.execute_dialog_through_command(".uno:PageFormatDialog") as xDialog:
+                xTabs = xDialog.getChild("tabcontrol")
+                select_pos(xTabs, "5")
+                xFirstFooterSameBtn = xDialog.getChild("checkSameFP")
+                xFirstFooterSameBtn.executeAction("CLICK", tuple())
+
+            self.xUITest.executeCommand(".uno:Reload")
+
+            with self.ui_test.execute_dialog_through_command(".uno:PageFormatDialog") as xDialog2:
+                xTabs = xDialog2.getChild("tabcontrol")
+                select_pos(xTabs, "5")
+                xFooterOnBtn = xDialog2.getChild("checkFooterOn")
+                self.assertEqual(get_state_as_dict(xFooterOnBtn)["Selected"], "true")
+                xFirstFooterSameBtn = xDialog2.getChild("checkSameFP")
+                self.assertEqual(get_state_as_dict(xFirstFooterSameBtn)["Selected"], "true")
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
