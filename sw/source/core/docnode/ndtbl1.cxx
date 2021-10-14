3ab6a73271344e7716ea7ea222907056fad64d1a
Armin Le Grand (Allotropia) <Armin.Le.Grand@me.com>
2021-10-14 17:56:22 +0200

    tdf#144843 call to GetBoxBackground requires incarnated item
    Change-Id: Id47f26a762804cd8c1b5742966ec54b71a26ab09
    
    Added assert/warning to SwDoc::GetBoxAttr - that is the
    method used by GetBoxBackground && GetBoxDirection
    
    Change-Id: I59e8dfb790e3872810885a8f888cea57be27e80c

--- sw/source/core/docnode/ndtbl1.cxx
+++ sw/source/core/docnode/ndtbl1.cxx
@@ -1250,6 +1250,8 @@ void SwDoc::SetBoxAttr( const SwCursor& rCursor, const SfxPoolItem &rNew )
 
 bool SwDoc::GetBoxAttr( const SwCursor& rCursor, std::unique_ptr<SfxPoolItem>& rToFill )
 {
+    // tdf#144843 calling GetBoxAttr *requires* object
+    assert(rToFill && "requires object here");
     bool bRet = false;
     SwTableNode* pTableNd = rCursor.GetPoint()->nNode.GetNode().FindTableNode();
     SwSelBoxes aBoxes;
