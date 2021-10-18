a7df405e846a98c628385e16fdf6026c155f2333
Henrik Palomäki <henrik.palomaki@yandex.com>
2021-10-18 14:17:36 +0300

    tdf#143148 - Use pragma once instead of include guards
    This patch replaces include guards with the "pragma once" directive.
    
    Change-Id: I1325488a22cca27d1f33e0191a0674a662630f66

--- sw/source/ui/inc/swuiexp.hxx
+++ sw/source/ui/inc/swuiexp.hxx
@@ -16,8 +16,8 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
-#ifndef INCLUDED_SW_SOURCE_UI_INC_SWUIEXP_HXX
-#define INCLUDED_SW_SOURCE_UI_INC_SWUIEXP_HXX
+
+#pragma once
 
 #include <swabstdlg.hxx>
 
@@ -25,7 +25,5 @@ namespace swui
 {
 SwAbstractDialogFactory& GetFactory();
 }
-
-#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
