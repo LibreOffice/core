LibreOffice/core

commit 10efab2b9a3cf7fc49655c90ba29db4512680c38

tdf#82824 DOCX import: fix at-char embedded object handling
The anchor type of embedded object was simply not handled, we always
assumed that it's as-char.

When it's at-char set the anchor type accordingly, and also set the
usual 6 properties determining the position of the anchored object.

Change-Id: I3f8bede33c6f1a0bdc4f4d4ea59c4fc805802291
Reviewed-on: https://gerrit.libreoffice.org/30860
Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
Tested-by: Jenkins <ci@libreoffice.org>

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -4787,7 +4787,21 @@ void  DomainMapper_Impl::ImportGraphic(const writerfilter::Reference< Properties
 
     // Update the shape properties if it is embedded object.
     if(m_xEmbedded.is()){
-        UpdateEmbeddedShapeProps(m_pGraphicImport->GetXShapeObject());
+        uno::Reference<drawing::XShape> xShape = m_pGraphicImport->GetXShapeObject();
+        UpdateEmbeddedShapeProps(xShape);
+        if (eGraphicImportType == IMPORT_AS_DETECTED_ANCHOR)
+        {
+            uno::Reference<beans::XPropertySet> xEmbeddedProps(m_xEmbedded, uno::UNO_QUERY);
+            text::TextContentAnchorType eAnchorType = text::TextContentAnchorType_AT_CHARACTER;
+            xEmbeddedProps->setPropertyValue("AnchorType", uno::makeAny(eAnchorType));
+            uno::Reference<beans::XPropertySet> xShapeProps(xShape, uno::UNO_QUERY);
+            xEmbeddedProps->setPropertyValue("HoriOrient", xShapeProps->getPropertyValue("HoriOrient"));
+            xEmbeddedProps->setPropertyValue("HoriOrientPosition", xShapeProps->getPropertyValue("HoriOrientPosition"));
+            xEmbeddedProps->setPropertyValue("HoriOrientRelation", xShapeProps->getPropertyValue("HoriOrientRelation"));
+            xEmbeddedProps->setPropertyValue("VertOrient", xShapeProps->getPropertyValue("VertOrient"));
+            xEmbeddedProps->setPropertyValue("VertOrientPosition", xShapeProps->getPropertyValue("VertOrientPosition"));
+            xEmbeddedProps->setPropertyValue("VertOrientRelation", xShapeProps->getPropertyValue("VertOrientRelation"));
+        }
     }
     //insert it into the document at the current cursor position
     OSL_ENSURE( xTextContent.is(), "DomainMapper_Impl::ImportGraphic");

sw/qa/extras/ooxmlimport/ooxmlimport.cxx
@@ -1266,6 +1266,11 @@ DECLARE_OOXMLIMPORT_TEST(testTdf103664, "tdf103664.docx")
     CPPUNIT_ASSERT_EQUAL(awt::CharSet::SYMBOL, getProperty<sal_Int16>(xRun, "CharFontCharSet"));
 }
 
+DECLARE_OOXMLIMPORT_TEST(testTdf82824, "tdf82824.docx")
+{
+    // This was text::TextContentAnchorType_AS_CHARACTER, <wp:anchor> wasn't handled on import for the chart.
+    CPPUNIT_ASSERT_EQUAL(text::TextContentAnchorType_AT_CHARACTER, getProperty<text::TextContentAnchorType>(getShape(1), "AnchorType"));
+}
 
 // tests should only be added to ooxmlIMPORT *if* they fail round-tripping in ooxmlEXPORT
 

commit d1b67fc01dfda313479e0e72602c04e1b7d38fae

tdf#144798 DOCX import: handle ZOrder of chart objects
Regression from commit 10efab2b9a3cf7fc49655c90ba29db4512680c38
(tdf#82824 DOCX import: fix at-char embedded object handling,
2016-11-15), the problem was that if we start supporting anchor types
other than as-char, then handling ZOrder is no longer optional.

Change-Id: I605cee33180490817055978b22f3271b3c08a1c0

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -7364,6 +7364,9 @@ void  DomainMapper_Impl::ImportGraphic(const writerfilter::Reference< Properties
             xEmbeddedProps->setPropertyValue("VertOrientRelation", xShapeProps->getPropertyValue("VertOrientRelation"));
             //tdf123873 fix missing textwrap import
             xEmbeddedProps->setPropertyValue("TextWrap", xShapeProps->getPropertyValue("TextWrap"));
+
+            // GraphicZOrderHelper::findZOrder() was called already, so can just copy it over.
+            xEmbeddedProps->setPropertyValue("ZOrder", xShapeProps->getPropertyValue("ZOrder"));
         }
     }
     //insert it into the document at the current cursor position

writerfilter/qa/cppunittests/dmapper/DomainMapper_Impl.cxx
@@ -18,6 +18,7 @@
 #include <com/sun/star/text/WritingMode2.hpp>
 #include <com/sun/star/text/XTextTablesSupplier.hpp>
 #include <com/sun/star/text/XTextTable.hpp>
+#include <com/sun/star/lang/XServiceInfo.hpp>
 
 #include <vcl/scheduler.hxx>
 
@@ -211,6 +212,23 @@ CPPUNIT_TEST_FIXTURE(Test, testCreateDatePreserve)
     // - Actual  : 07/07/2020
     // i.e. the formatting of the create date field was lost.
     CPPUNIT_ASSERT_EQUAL(OUString("7/7/2020 10:11:00 AM"), xPortion->getString());
+}
+
+CPPUNIT_TEST_FIXTURE(Test, testChartZOrder)
+{
+    // Given a document with a chart and a shape on it:
+    OUString aURL = m_directories.getURLFromSrc(DATA_DIRECTORY) + "chart-zorder.docx";
+
+    // When loading the document:
+    getComponent() = loadFromDesktop(aURL);
+
+    // Then make sure the shape is on top of the chart:
+    uno::Reference<drawing::XDrawPageSupplier> xDrawPageSupplier(getComponent(), uno::UNO_QUERY);
+    uno::Reference<container::XIndexAccess> xDrawPage = xDrawPageSupplier->getDrawPage();
+    uno::Reference<lang::XServiceInfo> xChart(xDrawPage->getByIndex(0), uno::UNO_QUERY);
+    // Without the accompanying fix in place, this test would have failed, as the chart was on top
+    // of the shape.
+    CPPUNIT_ASSERT(xChart->supportsService("com.sun.star.text.TextEmbeddedObject"));
 }
 }
 