LibreOffice/core

commit 1cf57765dd9a66982ecf9cd33687005a70c70c3f

loplugin:moveparam in connectivity
Change-Id: Ie5c2f6ec1573ab2c47218dd4da8c9150b23bcd2e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/123247
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

connectivity/source/drivers/firebird/DatabaseMetaData.cxx
@@ -801,7 +801,7 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getTableTypes(  )
     aRow[1] = new ORowSetValueDecorator(OUString("SYSTEM TABLE"));
     aResults.push_back(aRow);
 
-    pResultSet->setRows(aResults);
+    pResultSet->setRows(std::move(aResults));
     return pResultSet;
 }
 
@@ -1026,7 +1026,7 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getTypeInfo()
         tmp.push_back(aRow);
         return tmp;
     }();
-    pResultSet->setRows(aResults);
+    pResultSet->setRows(std::move(aResults));
     return pResultSet;
 }
 
@@ -1099,7 +1099,7 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getColumnPrivileges(
         aResults.push_back(aCurrentRow);
     }
 
-    pResultSet->setRows( aResults );
+    pResultSet->setRows( std::move(aResults) );
 
     return pResultSet;
 }
@@ -1307,7 +1307,7 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getColumns(
     }
     rtl::Reference<ODatabaseMetaDataResultSet> pResultSet = new
             ODatabaseMetaDataResultSet(ODatabaseMetaDataResultSet::eColumns);
-    pResultSet->setRows( aResults );
+    pResultSet->setRows( std::move(aResults) );
 
     return pResultSet;
 }
@@ -1424,7 +1424,7 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getTables(
         aResults.push_back(aCurrentRow);
     }
 
-    pResultSet->setRows( aResults );
+    pResultSet->setRows( std::move(aResults) );
 
     return pResultSet;
 }
@@ -1552,7 +1552,7 @@ uno::Reference< XResultSet > ODatabaseMetaData::lcl_getKeys(const bool bIsImport
         aResults.push_back(aCurrentRow);
     }
 
-    pResultSet->setRows( aResults );
+    pResultSet->setRows( std::move(aResults) );
     return pResultSet;
 }
 
@@ -1606,7 +1606,7 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getPrimaryKeys(
     }
     rtl::Reference<ODatabaseMetaDataResultSet> pResultSet = new
             ODatabaseMetaDataResultSet(ODatabaseMetaDataResultSet::ePrimaryKeys);
-    pResultSet->setRows( aResults );
+    pResultSet->setRows( std::move(aResults) );
 
     return pResultSet;
 }
@@ -1699,7 +1699,7 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getIndexInfo(
     }
     rtl::Reference<ODatabaseMetaDataResultSet> pResultSet = new
             ODatabaseMetaDataResultSet(ODatabaseMetaDataResultSet::ePrimaryKeys);
-    pResultSet->setRows( aResults );
+    pResultSet->setRows( std::move(aResults) );
 
     return pResultSet;
 }
@@ -1774,7 +1774,7 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getTablePrivileges(
         aResults.push_back(aRow);
     }
 
-    pResultSet->setRows( aResults );
+    pResultSet->setRows( std::move(aResults) );
 
     return pResultSet;
 }

connectivity/source/drivers/jdbc/DatabaseMetaData.cxx
@@ -481,7 +481,7 @@ Reference< XResultSet > SAL_CALL java_sql_DatabaseMetaData::getTablePrivileges(
 
                     aRows.push_back(aRow);
                 }
-                pNewPrivRes->setRows(aRows);
+                pNewPrivRes->setRows(std::move(aRows));
             }
         }
     }

connectivity/source/commontools/FDatabaseMetaDataResultSet.cxx
@@ -143,9 +143,9 @@ Sequence< Type > SAL_CALL ODatabaseMetaDataResultSet::getTypes(  )
     return ::comphelper::concatSequences(aTypes.getTypes(),ODatabaseMetaDataResultSet_BASE::getTypes());
 }
 
-void ODatabaseMetaDataResultSet::setRows(const ORows& _rRows)
+void ODatabaseMetaDataResultSet::setRows(ORows&& _rRows)
 {
-    m_aRows = _rRows;
+    m_aRows = std::move(_rRows);
     m_bBOF = true;
     m_bEOF = m_aRows.empty();
 }
@@ -822,7 +822,7 @@ void SAL_CALL ODatabaseMetaDataResultSet::initialize( const Sequence< Any >& _aA
         }
         aRowsToSet.push_back(aRowToSet);
     } // for (; pRowsIter != pRowsEnd;++pRowsIter
-    setRows(aRowsToSet);
+    setRows(std::move(aRowsToSet));
 }
 // XServiceInfo
 

connectivity/source/commontools/TDatabaseMetaDataBase.cxx
@@ -164,7 +164,7 @@ Reference< XResultSet > SAL_CALL ODatabaseMetaDataBase::getTypeInfo(  )
         }
     }
     rtl::Reference<::connectivity::ODatabaseMetaDataResultSet> pResult = new ::connectivity::ODatabaseMetaDataResultSet(::connectivity::ODatabaseMetaDataResultSet::eTypeInfo);
-    pResult->setRows(m_aTypeInfoRows);
+    pResult->setRows(std::vector(m_aTypeInfoRows));
     return pResult;
 }
 

connectivity/source/commontools/TPrivilegesResultSet.cxx
@@ -74,7 +74,7 @@ OResultSetPrivileges::OResultSetPrivileges( const Reference< XDatabaseMetaData>&
         aRow[6] = new ORowSetValueDecorator(OUString("REFERENCE"));
         aRows.push_back(aRow);
 
-        setRows(aRows);
+        setRows(std::move(aRows));
     }
     osl_atomic_decrement( &m_refCount );
 }

connectivity/source/drivers/ado/ADatabaseMetaData.cxx
@@ -347,7 +347,7 @@ Reference< XResultSet > SAL_CALL ODatabaseMetaData::getTablePrivileges(
         aRows.push_back(aRow);
         aRow[6] = ::connectivity::ODatabaseMetaDataResultSet::getDropValue();
         aRows.push_back(aRow);
-        pResult->setRows(aRows);
+        pResult->setRows(std::move(aRows));
     }
 
     return xRef;

connectivity/source/drivers/calc/CDatabaseMetaData.cxx
@@ -209,7 +209,7 @@ Reference< XResultSet > SAL_CALL OCalcDatabaseMetaData::getTables(
         }
     }
 
-    pResult->setRows(aRows);
+    pResult->setRows(std::move(aRows));
 
     return pResult;
 }

connectivity/source/drivers/component/CDatabaseMetaData.cxx
@@ -115,7 +115,7 @@ Reference< XResultSet > OComponentDatabaseMetaData::impl_getTypeInfo_throw(  )
         aRows.push_back(aRow);
     }
 
-    pResult->setRows(aRows);
+    pResult->setRows(std::move(aRows));
     return pResult;
 }
 
@@ -207,7 +207,7 @@ Reference< XResultSet > SAL_CALL OComponentDatabaseMetaData::getColumns(
     }
 
     rtl::Reference<ODatabaseMetaDataResultSet> pResult = new ODatabaseMetaDataResultSet(::connectivity::ODatabaseMetaDataResultSet::eColumns);
-    pResult->setRows(aRows);
+    pResult->setRows(std::move(aRows));
 
     return pResult;
 }

commit 6a061dc8ff910154329c47d4d6675f5e68742086

tdf#145203: FIREBIRD cannot create a table
Regression from:
https://cgit.freedesktop.org/libreoffice/core/commit/?id=1cf57765dd9a66982ecf9cd33687005a70c70c3f
2021-10-08
loplugin:moveparam in connectivity

Change-Id: Iec0fa1597e63d42ee3a905bf55eff798b3f46177

connectivity/source/drivers/firebird/DatabaseMetaData.cxx
@@ -813,9 +813,8 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getTypeInfo()
     // in special the metadata of the resultset already returns the right columns
     rtl::Reference<ODatabaseMetaDataResultSet> pResultSet =
             new ODatabaseMetaDataResultSet(ODatabaseMetaDataResultSet::eTypeInfo);
-    static ODatabaseMetaDataResultSet::ORows aResults = []()
+    ODatabaseMetaDataResultSet::ORows aRows;
     {
-        ODatabaseMetaDataResultSet::ORows tmp;
         ODatabaseMetaDataResultSet::ORow aRow(19);
 
         // Common data
@@ -845,7 +844,7 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getTypeInfo()
         aRow[12] = new ORowSetValueDecorator(false); // Autoincrement
         aRow[14] = ODatabaseMetaDataResultSet::get0Value(); // Minimum scale
         aRow[15] = ODatabaseMetaDataResultSet::get0Value(); // Max scale
-        tmp.push_back(aRow);
+        aRows.push_back(aRow);
 
         // Varchar
         aRow[1] = new ORowSetValueDecorator(OUString("VARCHAR"));
@@ -857,7 +856,7 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getTypeInfo()
         aRow[12] = new ORowSetValueDecorator(false); // Autoincrement
         aRow[14] = ODatabaseMetaDataResultSet::get0Value(); // Minimum scale
         aRow[15] = ODatabaseMetaDataResultSet::get0Value(); // Max scale
-        tmp.push_back(aRow);
+        aRows.push_back(aRow);
 
         // Binary (CHAR); we use the Firebird synonym CHARACTER
         // to fool LO into seeing it as different types.
@@ -871,7 +870,7 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getTypeInfo()
                 sal_Int16(ColumnSearch::NONE)); // Searchable
         aRow[14] = ODatabaseMetaDataResultSet::get0Value(); // Minimum scale
         aRow[15] = ODatabaseMetaDataResultSet::get0Value(); // Max scale
-        tmp.push_back(aRow);
+        aRows.push_back(aRow);
 
         // Varbinary (VARCHAR); see comment above about BINARY
         aRow[1] = new ORowSetValueDecorator(OUString("CHARACTER VARYING"));
@@ -891,13 +890,13 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getTypeInfo()
         aRow[12] = new ORowSetValueDecorator(false); // Autoincrement
         aRow[14] = ODatabaseMetaDataResultSet::get0Value(); // Minimum scale
         aRow[15] = ODatabaseMetaDataResultSet::get0Value(); // Max scale
-        tmp.push_back(aRow);
+        aRows.push_back(aRow);
 
         // Longvarbinary (SQL_BLOB)
         // Distinguished from simple blob with a user-defined subtype.
         aRow[1] = new ORowSetValueDecorator(OUString("BLOB SUB_TYPE " + OUString::number(static_cast<short>(BlobSubtype::Image))) ); // BLOB, with subtype 0
         aRow[2] = new ORowSetValueDecorator(DataType::LONGVARBINARY);
-        tmp.push_back(aRow);
+        aRows.push_back(aRow);
 
         // Integer Types common
         {
@@ -912,17 +911,17 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getTypeInfo()
         aRow[1] = new ORowSetValueDecorator(OUString("SMALLINT"));
         aRow[2] = new ORowSetValueDecorator(DataType::SMALLINT);
         aRow[3] = new ORowSetValueDecorator(sal_Int16(5)); // Prevision
-        tmp.push_back(aRow);
+        aRows.push_back(aRow);
         // Integer (SQL_LONG)
         aRow[1] = new ORowSetValueDecorator(OUString("INTEGER"));
         aRow[2] = new ORowSetValueDecorator(DataType::INTEGER);
         aRow[3] = new ORowSetValueDecorator(sal_Int16(10)); // Precision
-        tmp.push_back(aRow);
+        aRows.push_back(aRow);
         // Bigint (SQL_INT64)
         aRow[1] = new ORowSetValueDecorator(OUString("BIGINT"));
         aRow[2] = new ORowSetValueDecorator(DataType::BIGINT);
         aRow[3] = new ORowSetValueDecorator(sal_Int16(20)); // Precision
-        tmp.push_back(aRow);
+        aRows.push_back(aRow);
 
         // Decimal Types common
         {
@@ -938,14 +937,14 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getTypeInfo()
         aRow[3] = new ORowSetValueDecorator(sal_Int16(18)); // Precision
         aRow[14] = new ORowSetValueDecorator(sal_Int16(0)); // Minimum scale
         aRow[15] = new ORowSetValueDecorator(sal_Int16(18)); // Max scale
-        tmp.push_back(aRow);
+        aRows.push_back(aRow);
         // Decimal
         aRow[1] = new ORowSetValueDecorator(OUString("DECIMAL"));
         aRow[2] = new ORowSetValueDecorator(DataType::DECIMAL);
         aRow[3] = new ORowSetValueDecorator(sal_Int16(18)); // Precision
         aRow[14] = new ORowSetValueDecorator(sal_Int16(0)); // Minimum scale
         aRow[15] = new ORowSetValueDecorator(sal_Int16(18)); // Max scale
-        tmp.push_back(aRow);
+        aRows.push_back(aRow);
 
         aRow[6] = new ORowSetValueDecorator(); // Create Params
         // Float (SQL_FLOAT)
@@ -954,14 +953,14 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getTypeInfo()
         aRow[3] = new ORowSetValueDecorator(sal_Int16(7)); // Precision
         aRow[14] = new ORowSetValueDecorator(sal_Int16(1)); // Minimum scale
         aRow[15] = new ORowSetValueDecorator(sal_Int16(7)); // Max scale
-        tmp.push_back(aRow);
+        aRows.push_back(aRow);
         // Double (SQL_DOUBLE)
         aRow[1] = new ORowSetValueDecorator(OUString("DOUBLE PRECISION"));
         aRow[2] = new ORowSetValueDecorator(DataType::DOUBLE);
         aRow[3] = new ORowSetValueDecorator(sal_Int16(15)); // Precision
         aRow[14] = new ORowSetValueDecorator(sal_Int16(1)); // Minimum scale
         aRow[15] = new ORowSetValueDecorator(sal_Int16(15)); // Max scale
-        tmp.push_back(aRow);
+        aRows.push_back(aRow);
 
         // TODO: no idea whether D_FLOAT corresponds to an sql type
 
@@ -975,7 +974,7 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getTypeInfo()
         aRow[12] = new ORowSetValueDecorator(false); // Autoincrement
         aRow[14] = ODatabaseMetaDataResultSet::get0Value(); // Minimum scale
         aRow[15] = ODatabaseMetaDataResultSet::get0Value(); // Max scale
-        tmp.push_back(aRow);
+        aRows.push_back(aRow);
 
         // SQL_TYPE_TIME
         aRow[1] = new ORowSetValueDecorator(OUString("TIME"));
@@ -987,7 +986,7 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getTypeInfo()
         aRow[12] = new ORowSetValueDecorator(false); // Autoincrement
         aRow[14] = ODatabaseMetaDataResultSet::get0Value(); // Minimum scale
         aRow[15] = ODatabaseMetaDataResultSet::get0Value(); // Max scale
-        tmp.push_back(aRow);
+        aRows.push_back(aRow);
 
         // SQL_TYPE_DATE
         aRow[1] = new ORowSetValueDecorator(OUString("DATE"));
@@ -999,7 +998,7 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getTypeInfo()
         aRow[12] = new ORowSetValueDecorator(false); // Autoincrement
         aRow[14] = ODatabaseMetaDataResultSet::get0Value(); // Minimum scale
         aRow[15] = ODatabaseMetaDataResultSet::get0Value(); // Max scale
-        tmp.push_back(aRow);
+        aRows.push_back(aRow);
 
         // SQL_BLOB
         aRow[1] = new ORowSetValueDecorator(OUString("BLOB SUB_TYPE BINARY"));
@@ -1011,7 +1010,7 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getTypeInfo()
         aRow[12] = new ORowSetValueDecorator(false); // Autoincrement
         aRow[14] = ODatabaseMetaDataResultSet::get0Value(); // Minimum scale
         aRow[15] = ODatabaseMetaDataResultSet::get0Value(); // Max scale
-        tmp.push_back(aRow);
+        aRows.push_back(aRow);
 
         // SQL_BOOLEAN
         aRow[1] = new ORowSetValueDecorator(OUString("BOOLEAN"));
@@ -1023,10 +1022,9 @@ uno::Reference< XResultSet > SAL_CALL ODatabaseMetaData::getTypeInfo()
         aRow[12] = new ORowSetValueDecorator(false); // Autoincrement
         aRow[14] = ODatabaseMetaDataResultSet::get0Value(); // Minimum scale
         aRow[15] = ODatabaseMetaDataResultSet::get0Value(); // Max scale
-        tmp.push_back(aRow);
-        return tmp;
-    }();
-    pResultSet->setRows(std::move(aResults));
+        aRows.push_back(aRow);
+    };
+    pResultSet->setRows(std::move(aRows));
     return pResultSet;
 }
 