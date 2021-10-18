123edad21081ab45c8d83be4788f3c47c3b9a2dc
Henrik Palomäki <henrik.palomaki@yandex.com>
2021-10-18 14:17:36 +0300

    tdf#143148 - Use pragma once instead of include guards
    This patch replaces include guards with the "pragma once" directive.
    
    Change-Id: I1325488a22cca27d1f33e0191a0674a662630f66
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/123743
    Tested-by: Jenkins
    Reviewed-by: Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>

--- sw/source/ui/inc/mmresultdialogs.hxx
+++ sw/source/ui/inc/mmresultdialogs.hxx
@@ -16,8 +16,8 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
-#ifndef INCLUDED_SW_SOURCE_UI_DBUI_MMOUTPUTPAGE_HXX
-#define INCLUDED_SW_SOURCE_UI_DBUI_MMOUTPUTPAGE_HXX
+
+#pragma once
 
 #include <vcl/weld.hxx>
 #include <vcl/print.hxx>
@@ -187,6 +187,5 @@ public:
     void                AllMailsSent();
 
 };
-#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
