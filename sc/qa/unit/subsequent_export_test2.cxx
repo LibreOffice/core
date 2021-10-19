LibreOffice/core

commit 3c3b9ad8886da916027f0fb940a2df822d63d4d7

tdf#143220 XLSX export: fix hyperlink to sheet target
Hyperlink to another sheet didn't work in Excel.

Change-Id: Ic63e581d5de8f7ff1eac50a5e56a51d7ba2e72ce
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/118935
Tested-by: Jenkins
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

sc/source/filter/excel/xecontent.cxx
@@ -428,15 +428,17 @@ XclExpHyperlink::XclExpHyperlink( const XclExpRoot& rRoot, const SvxURLField& rU
            aTextMark = aTextMark.replaceAt( nSepPos, 1, "!" );
         }
 
-        if(nSepPos != -1)
+        if (nSepPos != -1)
         {
-            OUString aSheetName( aTextMark.copy(0, nSepPos));
+            OUString aSheetName(aTextMark.copy(0, nSepPos));
 
-            if ( aSheetName.indexOf(' ') != -1 && aSheetName[0] != '\'')
+            if (aSheetName.indexOf(' ') != -1 && aSheetName[0] != '\'')
             {
                 aTextMark = "'" + aTextMark.replaceAt(nSepPos, 0, "'");
             }
         }
+        else
+            aTextMark += "!A1";    // tdf#143220 link to sheet not valid without cell reference
 
         mxTextMark.reset( new XclExpString( aTextMark, XclStrFlags::ForceUnicode, 255 ) );
     }

sc/qa/unit/subsequent_export-test2.cxx
@@ -194,6 +194,7 @@ public:
     void testButtonFormControlXlsxExport();
     void testTdf142929_filterLessThanXLSX();
     void testInvalidNamedRange();
+    void testTdf143220XLSX();
 
     CPPUNIT_TEST_SUITE(ScExportTest2);
 
@@ -296,6 +297,7 @@ public:
     CPPUNIT_TEST(testButtonFormControlXlsxExport);
     CPPUNIT_TEST(testTdf142929_filterLessThanXLSX);
     CPPUNIT_TEST(testInvalidNamedRange);
+    CPPUNIT_TEST(testTdf143220XLSX);
 
     CPPUNIT_TEST_SUITE_END();
 
@@ -2458,6 +2460,19 @@ void ScExportTest2::testInvalidNamedRange()
     // Without the fix in place, this test would have failed, we didn't ignore the problematic named
     // range on import.
     CPPUNIT_ASSERT(!xNamedRanges->hasByName("myname"));
+}
+
+void ScExportTest2::testTdf143220XLSX()
+{
+    ScDocShellRef xDocSh = loadDoc(u"tdf143220.", FORMAT_ODS);
+    CPPUNIT_ASSERT(xDocSh.is());
+
+    xmlDocUniquePtr pDoc = XPathHelper::parseExport2(*this, *xDocSh, m_xSFactory,
+                                                     "xl/worksheets/sheet1.xml", FORMAT_XLSX);
+    CPPUNIT_ASSERT(pDoc);
+    assertXPath(pDoc, "/x:worksheet/x:hyperlinks/x:hyperlink", "location", "Sheet2!A1");
+
+    xDocSh->DoClose();
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(ScExportTest2);

commit 571666b76ad184709c654b172a344b5fc10e2af6

tdf#145079 XLSX export: fix internal hyperlinks
Hyperlinks with internal named range targets didn't work.

Regression from commit: 3c3b9ad8886da916027f0fb940a2df822d63d4d7
(tdf#143220 XLSX export: fix hyperlink to sheet target)

Change-Id: I8128ebb977dba7304bc9c69e45f6c55c71f800cf

sc/source/filter/excel/xecontent.cxx
@@ -438,7 +438,11 @@ XclExpHyperlink::XclExpHyperlink( const XclExpRoot& rRoot, const SvxURLField& rU
             }
         }
         else
-            aTextMark += "!A1";    // tdf#143220 link to sheet not valid without cell reference
+        {
+            SCTAB nTab;
+            if (rRoot.GetDoc().GetTable(aTextMark, nTab))
+                aTextMark += "!A1"; // tdf#143220 link to sheet not valid without cell reference
+        }
 
         mxTextMark.reset( new XclExpString( aTextMark, XclStrFlags::ForceUnicode, 255 ) );
     }

sc/qa/unit/subsequent_export_test2.cxx
@@ -206,7 +206,7 @@ public:
     void testTdf142929_filterLessThanXLSX();
     void testInvalidNamedRange();
     void testExternalDefinedNameXLSX();
-    void testTdf143220XLSX();
+    void testHyperlinkLocationXLSX();
     void testTdf142264ManyChartsToXLSX();
     void testTdf143929MultiColumnToODS();
     void testTdf142578();
@@ -317,7 +317,7 @@ public:
     CPPUNIT_TEST(testTdf142929_filterLessThanXLSX);
     CPPUNIT_TEST(testInvalidNamedRange);
     CPPUNIT_TEST(testExternalDefinedNameXLSX);
-    CPPUNIT_TEST(testTdf143220XLSX);
+    CPPUNIT_TEST(testHyperlinkLocationXLSX);
     CPPUNIT_TEST(testTdf142264ManyChartsToXLSX);
     CPPUNIT_TEST(testTdf143929MultiColumnToODS);
     CPPUNIT_TEST(testTdf142578);
@@ -2730,7 +2730,7 @@ void ScExportTest2::testExternalDefinedNameXLSX()
     xDocSh->DoClose();
 }
 
-void ScExportTest2::testTdf143220XLSX()
+void ScExportTest2::testHyperlinkLocationXLSX()
 {
     ScDocShellRef xDocSh = loadDoc(u"tdf143220.", FORMAT_ODS);
     CPPUNIT_ASSERT(xDocSh.is());
@@ -2738,7 +2738,13 @@ void ScExportTest2::testTdf143220XLSX()
     xmlDocUniquePtr pDoc = XPathHelper::parseExport2(*this, *xDocSh, m_xSFactory,
                                                      "xl/worksheets/sheet1.xml", FORMAT_XLSX);
     CPPUNIT_ASSERT(pDoc);
-    assertXPath(pDoc, "/x:worksheet/x:hyperlinks/x:hyperlink", "location", "Sheet2!A1");
+
+    // tdf#143220 link to sheet not valid without cell reference
+    assertXPath(pDoc, "/x:worksheet/x:hyperlinks/x:hyperlink[@ref='A1']", "location", "Sheet2!A1");
+
+    // tdf#145079 link with defined name target didn't work because Calc added "A1" at the end
+    assertXPath(pDoc, "/x:worksheet/x:hyperlinks/x:hyperlink[@ref='A2']", "location", "name");
+    assertXPath(pDoc, "/x:worksheet/x:hyperlinks/x:hyperlink[@ref='A3']", "location", "db");
 
     xDocSh->DoClose();
 }