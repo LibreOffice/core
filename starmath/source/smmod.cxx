733623d6d4de6ed3441269e3ecf3ed86066735c2
Caolán McNamara <caolanm@redhat.com>
2021-10-20 10:23:58 +0100

    combine these contiguous ranges
    Change-Id: I5f2b4ae72599fb16d567b3500fd1a77811e708a0
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/123881
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>

--- starmath/source/smmod.cxx
+++ starmath/source/smmod.cxx
@@ -210,8 +210,7 @@ std::optional<SfxItemSet> SmModule::CreateItemSet( sal_uInt16 nId )
             svl::Items< //TP_SMPRINT
                 SID_PRINTTITLE, SID_PRINTZOOM,
                 SID_NO_RIGHT_SPACES, SID_SAVE_ONLY_USED_SYMBOLS,
-                SID_AUTO_CLOSE_BRACKETS, SID_AUTO_CLOSE_BRACKETS,
-                SID_SMEDITWINDOWZOOM, SID_SMEDITWINDOWZOOM>);
+                SID_AUTO_CLOSE_BRACKETS, SID_SMEDITWINDOWZOOM>);
 
         GetConfig()->ConfigToItemSet(*pRet);
     }
