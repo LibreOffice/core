bd345238ddaeee39c9653e83435479f50e08426c
Luboš Luňák <l.lunak@collabora.com>
2021-10-20 11:32:17 +0200

    properly flush LOK invalidations in tests
    This should be done always before doing something with a flag
    related to the invalidations. Use an idle timer for simplicity,
    tests already usually process to idle.
    
    Change-Id: I979900da28061cc690ecbcce023dbb769239f205

--- test/source/lokcallback.cxx
+++ test/source/lokcallback.cxx
@@ -13,16 +13,23 @@
 #include <rtl/strbuf.hxx>
 #include <tools/gen.hxx>
 #include <comphelper/lok.hxx>
+#include <sfx2/viewsh.hxx>
 
 TestLokCallbackWrapper::TestLokCallbackWrapper(LibreOfficeKitCallback callback, void* data)
-    : m_callback(callback)
+    : Idle("TestLokCallbackWrapper flush timer")
+    , m_callback(callback)
     , m_data(data)
 {
+    // Flushing timer needs to run with the lowest priority, so that all pending tasks
+    // such as invalidations are processed before it.
+    SetPriority(TaskPriority::LOWEST);
 }
 
 inline void TestLokCallbackWrapper::callCallback(int nType, const char* pPayload)
 {
     m_callback(nType, pPayload, m_data);
+    if (!IsActive())
+        Start();
 }
 
 void TestLokCallbackWrapper::libreOfficeKitViewCallback(int nType, const char* pPayload)
@@ -50,6 +57,16 @@ void TestLokCallbackWrapper::libreOfficeKitViewInvalidateTilesCallback(
         buf.append(nPart);
     }
     callCallback(LOK_CALLBACK_INVALIDATE_TILES, buf.makeStringAndClear().getStr());
+}
+
+void TestLokCallbackWrapper::Invoke()
+{
+    // Timer timeout, flush any possibly pending data.
+    for (SfxViewShell* viewShell = SfxViewShell::GetFirst(false); viewShell != nullptr;
+         viewShell = SfxViewShell::GetNext(*viewShell, false))
+    {
+        viewShell->flushPendingLOKInvalidateTiles();
+    }
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
