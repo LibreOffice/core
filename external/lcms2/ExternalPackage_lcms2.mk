d15a10f8d72c7a01df7219627f494d370be86729
Caolán McNamara <caolanm@redhat.com>
2021-10-20 10:27:24 +0100

    upgrade lcms2 to 2.12
    Change-Id: I40c3239495c6050add3ce2343453241f8c825d62
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/123886
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    (cherry picked from commit 9574b70e5430b89d49e01e92e8fda0d674144706)
    Reviewed-on: https://gerrit.collaboraoffice.com/c/core/+/2517
    Tested-by: Michael Stahl <michael.stahl@collabora.com>
    Reviewed-by: Michael Stahl <michael.stahl@collabora.com>

--- external/lcms2/ExternalPackage_lcms2.mk
+++ external/lcms2/ExternalPackage_lcms2.mk
@@ -21,7 +21,7 @@ else ifeq ($(COM),MSC)
 $(eval $(call gb_ExternalPackage_add_file,lcms2,$(LIBO_LIB_FOLDER)/lcms2.dll,bin/lcms2.dll))
 endif # $(COM)
 else  # $(OS) != WNT/MACOSX
-$(eval $(call gb_ExternalPackage_add_file,lcms2,$(LIBO_LIB_FOLDER)/liblcms2.so.2,src/.libs/liblcms2.so.2.0.10))
+$(eval $(call gb_ExternalPackage_add_file,lcms2,$(LIBO_LIB_FOLDER)/liblcms2.so.2,src/.libs/liblcms2.so.2.0.12))
 endif # $(OS)
 endif # $(DISABLE_DYNLOADING)
 
