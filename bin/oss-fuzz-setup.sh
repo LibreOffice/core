02d799a6c16f1ec815c77b3c00bde68b59350ee2
Caolán McNamara <caolanm@redhat.com>
2021-10-20 16:07:29 +0100

    wip: upgrade libjpeg-turbo to 2.1.1
    only simd for x86_64
    
    Change-Id: Ic2726ee8c6b6e59ca983b977ee2731f5b78b97d1

--- bin/oss-fuzz-setup.sh
+++ bin/oss-fuzz-setup.sh
@@ -21,7 +21,7 @@ curl --no-progress-meter -S \
     -C - -O https://dev-www.libreoffice.org/src/dtoa-20180411.tgz \
     -C - -O https://dev-www.libreoffice.org/src/expat-2.4.1.tar.bz2 \
     -C - -O https://dev-www.libreoffice.org/src/libcuckoo-93217f8d391718380c508a722ab9acd5e9081233.tar.gz \
-    -C - -O https://dev-www.libreoffice.org/src/libjpeg-turbo-1.5.3.tar.gz \
+    -C - -O https://dev-www.libreoffice.org/src/libjpeg-turbo-2.1.1.tar.gz \
     -C - -O https://dev-www.libreoffice.org/src/lcms2-2.12.tar.gz \
     -C - -O https://dev-www.libreoffice.org/src/libexttextcat-3.4.5.tar.xz \
     -C - -O https://dev-www.libreoffice.org/src/cppunit-1.15.1.tar.gz \
