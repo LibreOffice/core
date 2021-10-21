d9e63412bf81715b388e971cf6a397b9fd7446c6
Miklos Vajna <vmiklos@collabora.com>
2021-10-21 19:53:46 +0200

    sw: remove useless debug code in SwTextShell
    A debugger can be used to examine that temporary instead.
    
    Change-Id: I8c04144f5174564e232c5d5effe466ccf5e52468

--- sw/source/uibase/shells/txtattr.cxx
+++ sw/source/uibase/shells/txtattr.cxx
@@ -797,12 +797,7 @@ void SwTextShell::GetAttrState(SfxItemSet &rSet)
             case SID_ATTR_CHAR_KERNING:
             case RES_PARATR_DROP:
             {
-#if OSL_DEBUG_LEVEL > 1
-                const SfxPoolItem& rItem = aCoreSet.Get(GetPool().GetWhich(nSlot), true);
-                rSet.Put(rItem);
-#else
                 rSet.Put(aCoreSet.Get( GetPool().GetWhich(nSlot)));
-#endif
                 nSlot = 0;
             }
             break;
