8b932667b45f316dbd195e20ae91abc17f9626af
Szymon Kłos <szymon.klos@collabora.com>
2021-10-27 13:04:17 +0200

    Make Certificate not found dialog async
    Change-Id: I8da2a2dc763cffd13659b61966a954a6e1ef06a4
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/124269
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/128841
    Tested-by: Jenkins

--- xmlsecurity/source/dialogs/digitalsignaturesdialog.cxx
+++ xmlsecurity/source/dialogs/digitalsignaturesdialog.cxx
@@ -206,6 +206,9 @@ DigitalSignaturesDialog::~DigitalSignaturesDialog()
 {
     if (m_xViewer)
         m_xViewer->response(RET_OK);
+
+    if (m_xInfoBox)
+        m_xInfoBox->response(RET_OK);
 }
 
 bool DigitalSignaturesDialog::Init()
@@ -785,10 +788,13 @@ void DigitalSignaturesDialog::ImplShowSignaturesDetails()
     }
     else
     {
-        std::unique_ptr<weld::MessageDialog> xInfoBox(Application::CreateMessageDialog(m_xDialog.get(),
-                                                      VclMessageType::Info, VclButtonsType::Ok,
-                                                      XsResId(STR_XMLSECDLG_NO_CERT_FOUND)));
-        xInfoBox->run();
+        if (m_xInfoBox)
+            m_xInfoBox->response(RET_OK);
+
+        m_xInfoBox = std::shared_ptr<weld::MessageDialog>(Application::CreateMessageDialog(m_xDialog.get(),
+                                                          VclMessageType::Info, VclButtonsType::Ok,
+                                                          XsResId(STR_XMLSECDLG_NO_CERT_FOUND)));
+        m_xInfoBox->runAsync(m_xInfoBox, [this] (sal_Int32) { m_xInfoBox = nullptr; });
     }
 }
 
