b8bc237b0a412a7ecd744b9eba2ea916790fdd63
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2021-10-30 10:03:33 +0200

    sw: Wrong ZOrder after import for images anchored to non-1nd page
    Importing a document that has images anchored to the non-1st
    page, can result in images having the wrong the wrong Z index
    (ZOrder). If there are overlapping objects in the document,
    this can become a problem.
    
    The issue is with the call to SwRootFrame::RemoveMasterObjs,
    which removed the "master objects" - SwFlyDrawObj* castable
    objects. Normally the SwFlyDrawObj* objects on the page are wrapped
    and replaced by SwVirtFlyDrawObj*, but when the call to the
    RemoveMasterObjs was made, this hasn't happen yet to all the
    objects (objects that were not on the 1st page).
    
    The fix is to move the RemoveMasterObjs call to a different place,
    where we processed all the pages, so we can safely remove the
    remaining master objects on the page.
    
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/124597
    Tested-by: Jenkins
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>
    (cherry picked from commit 8b7dbd220dbdfd286de2d770503dae8bf6d2e53e)
    
    Change-Id: I7d2f31614d269c22a82e7e5dea960f3d0844a5d5

--- sw/qa/extras/odfimport/odfimport.cxx
+++ sw/qa/extras/odfimport/odfimport.cxx
@@ -1000,5 +1000,33 @@ DECLARE_ODFIMPORT_TEST(testTdf133459, "tdf133459.odt")
     CPPUNIT_ASSERT_EQUAL(OUString("QQ YYYY"), getProperty<OUString>(xFormat, "FormatString"));
 }
 
+CPPUNIT_TEST_FIXTURE(Test, testPageAnchorZIndexFirstPage)
+{
+    load(mpTestDocumentPath, "PageAnchorZIndexFirstPage.fodt");
+
+    CPPUNIT_ASSERT_EQUAL_MESSAGE("Image1 index not 0",
+        sal_Int32(0), getProperty<sal_Int32>(getShapeByName(u"Image1"), "ZOrder"));
+    CPPUNIT_ASSERT_EQUAL_MESSAGE("Image2 index not 1",
+        sal_Int32(1), getProperty<sal_Int32>(getShapeByName(u"Image2"), "ZOrder"));
+    CPPUNIT_ASSERT_EQUAL_MESSAGE("Shape1 index not 2",
+        sal_Int32(2), getProperty<sal_Int32>(getShapeByName(u"Shape1"), "ZOrder"));
+    CPPUNIT_ASSERT_EQUAL_MESSAGE("Shape2 index not 3",
+        sal_Int32(3), getProperty<sal_Int32>(getShapeByName(u"Shape2"), "ZOrder"));
+}
+
+CPPUNIT_TEST_FIXTURE(Test, testPageAnchorZIndexSecondPage)
+{
+    load(mpTestDocumentPath, "PageAnchorZIndexSecondPage.fodt");
+
+    CPPUNIT_ASSERT_EQUAL_MESSAGE("Image1 index not 0",
+        sal_Int32(0), getProperty<sal_Int32>(getShapeByName(u"Image1"), "ZOrder"));
+    CPPUNIT_ASSERT_EQUAL_MESSAGE("Image2 index not 1",
+        sal_Int32(1), getProperty<sal_Int32>(getShapeByName(u"Image2"), "ZOrder"));
+    CPPUNIT_ASSERT_EQUAL_MESSAGE("Shape1 index not 2",
+        sal_Int32(2), getProperty<sal_Int32>(getShapeByName(u"Shape1"), "ZOrder"));
+    CPPUNIT_ASSERT_EQUAL_MESSAGE("Shape2 index not 3",
+        sal_Int32(3), getProperty<sal_Int32>(getShapeByName(u"Shape2"), "ZOrder"));
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
