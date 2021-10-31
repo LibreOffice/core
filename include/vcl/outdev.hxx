59580c84a25573f613f234cfa4f0a7afcea3d6e7
Chris Sherlock <chris.sherlock79@gmail.com>
2021-10-31 21:19:35 +1100

    vcl: rename DrawTransparent() metafile version to DrawTransparentMetaFile()
    Change-Id: I80ededdf2ca5f99141e71a0e7962e9084a14e68b

--- include/vcl/outdev.hxx
+++ include/vcl/outdev.hxx
@@ -1478,7 +1478,7 @@ public:
                                     const basegfx::B2DPolyPolygon& rB2DPolyPoly,
                                     double fTransparency);
 
-    void                        DrawTransparent(
+    void                        DrawTransparentMetaFile(
                                         const GDIMetaFile& rMtf, const Point& rPos, const Size& rSize,
                                         const Gradient& rTransparenceGradient );
 
