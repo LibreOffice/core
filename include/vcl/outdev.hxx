af6c2cc6ca0b2a97ae606f5fc640f67316d963ff
Chris Sherlock <chris.sherlock79@gmail.com>
2021-10-31 21:34:49 +1100

    vcl: split GetGradientSteps() into Get{Complex|Linear}GradientSteps()
    Change-Id: Ieb0d5d6f1a7c9fff3671ef74ffac9eaf37dcb0e1

--- include/vcl/outdev.hxx
+++ include/vcl/outdev.hxx
@@ -815,7 +815,8 @@ private:
     SAL_DLLPRIVATE void         DrawLinearGradientToMetafile( const tools::Rectangle& rRect, const Gradient& rGradient );
     SAL_DLLPRIVATE void         DrawComplexGradientToMetafile( const tools::Rectangle& rRect, const Gradient& rGradient );
 
-    SAL_DLLPRIVATE tools::Long         GetGradientSteps( const Gradient& rGradient, const tools::Rectangle& rRect, bool bMtf, bool bComplex=false );
+    SAL_DLLPRIVATE tools::Long  GetLinearGradientSteps( const Gradient& rGradient, const tools::Rectangle& rRect, bool bMtf);
+    SAL_DLLPRIVATE tools::Long  GetComplexGradientSteps( const Gradient& rGradient, const tools::Rectangle& rRect, bool bMtf);
 
     SAL_DLLPRIVATE Color        GetSingleColorGradientFill();
     ///@}
