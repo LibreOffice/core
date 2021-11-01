4f5b21e3a4dc74d26eb6fc6d0de45c5ab16a53a9
Kevin Suo <suokunlong@126.com>
2021-11-01 23:18:00 +0800

    tdf#139205: Keep hierarchical structure of localized default styles in Calc
    The default cell styles as returned by orcus parsing are English names, as defined in
    sc/res/xml/styles.xml. These names can be localized. On non-English UI, the hierarchical
    structure is lost if we use the English names as the parent (i.e. all the styles will
    be forced to use the "Default" as their parent).
    
    In this patch, I moved the constexpr into the header file (so that they can be reused),
    added SC_DLLPUBLIC to the two member functions of ScStyleNameConversion, then use the
    ProgrammaticToDisplayName function to convert the parent style name to the (localized)
    UI name.
    
    Change-Id: I3689211a10c60a5e7b2b273b6c15a4da92f0d633

--- sc/inc/stylehelper.hxx
+++ sc/inc/stylehelper.hxx
@@ -20,14 +20,29 @@
 #pragma once
 
 #include <rtl/ustring.hxx>
+#include "scdllapi.h"
+
+constexpr OUStringLiteral SC_STYLE_PROG_STANDARD = u"Default";
+constexpr OUStringLiteral SC_STYLE_PROG_RESULT = u"Result";
+constexpr OUStringLiteral SC_STYLE_PROG_RESULT1 = u"Result2";
+constexpr OUStringLiteral SC_STYLE_PROG_HEADING = u"Heading";
+constexpr OUStringLiteral SC_STYLE_PROG_HEADING1 = u"Heading1";
+constexpr OUStringLiteral SC_STYLE_PROG_REPORT = u"Report";
+
+constexpr OUStringLiteral SC_PIVOT_STYLE_PROG_INNER = u"Pivot Table Value";
+constexpr OUStringLiteral SC_PIVOT_STYLE_PROG_RESULT = u"Pivot Table Result";
+constexpr OUStringLiteral SC_PIVOT_STYLE_PROG_CATEGORY = u"Pivot Table Category";
+constexpr OUStringLiteral SC_PIVOT_STYLE_PROG_TITLE = u"Pivot Table Title";
+constexpr OUStringLiteral SC_PIVOT_STYLE_PROG_FIELDNAME = u"Pivot Table Field";
+constexpr OUStringLiteral SC_PIVOT_STYLE_PROG_TOP = u"Pivot Table Corner";
 
 enum class SfxStyleFamily;
 
 class ScStyleNameConversion
 {
 public:
-    static OUString DisplayToProgrammaticName(const OUString& rDispName, SfxStyleFamily nType);
-    static OUString ProgrammaticToDisplayName(const OUString& rProgName, SfxStyleFamily nType);
+    static SC_DLLPUBLIC OUString DisplayToProgrammaticName(const OUString& rDispName, SfxStyleFamily nType);
+    static SC_DLLPUBLIC OUString ProgrammaticToDisplayName(const OUString& rProgName, SfxStyleFamily nType);
 };
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
