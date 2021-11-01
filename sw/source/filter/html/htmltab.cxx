5d9416d06f5d16faab2dc56f8ee571dbbebb925d
Caolán McNamara <caolanm@redhat.com>
2021-11-01 17:34:23 +0000

    ofz#40593 remove Objects from m_xResizeDrawObjects if deleted during parse
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/124563
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    (cherry picked from commit 2f01faaf88b6d172d7293f0c9e2a061d99b8ceb5)
    
    fix misplaced line
    
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/124630
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    (cherry picked from commit 4ed359093c991291216c39cffe14a60e607ec551)
    
    Change-Id: I11fa665175ef067a36f4822676c02d4df1e1e250
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/125332
    Tested-by: Michael Stahl <michael.stahl@allotropia.de>
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

--- sw/source/filter/html/htmltab.cxx
+++ sw/source/filter/html/htmltab.cxx
@@ -34,6 +34,7 @@
 #include <svtools/htmlkywd.hxx>
 #include <svl/urihelper.hxx>
 #include <svl/listener.hxx>
+#include <svx/sdrobjectuser.hxx>
 #include <sal/log.hxx>
 
 #include <dcontact.hxx>
@@ -372,7 +373,7 @@ typedef std::vector<HTMLTableColumn> HTMLTableColumns;
 
 typedef std::vector<SdrObject *> SdrObjects;
 
-class HTMLTable
+class HTMLTable : public sdr::ObjectUser
 {
     OUString m_aId;
     OUString m_aStyle;
@@ -520,6 +521,8 @@ private:
     sal_uInt16 GetBorderWidth( const SvxBorderLine& rBLine,
                            bool bWithDistance=false ) const;
 
+    virtual void ObjectInDestruction(const SdrObject& rObject) override;
+
 public:
 
     bool m_bFirstCell;                // is there a cell created already?
@@ -529,7 +532,7 @@ public:
               bool bHasToFly,
               const HTMLTableOptions& rOptions);
 
-    ~HTMLTable();
+    virtual ~HTMLTable();
 
     // Identifying of a cell
     const HTMLTableCell& GetCell(sal_uInt16 nRow, sal_uInt16 nCell) const;
@@ -1065,11 +1068,37 @@ bool SwHTMLParser::IsReqIF() const
     return m_bReqIF;
 }
 
+// if any m_pResizeDrawObjects members are deleted during parse, remove them
+// from m_pResizeDrawObjects and m_xDrawObjectPercentWidths
+void HTMLTable::ObjectInDestruction(const SdrObject& rObject)
+{
+    auto it = std::find(m_pResizeDrawObjects->begin(), m_pResizeDrawObjects->end(), &rObject);
+    assert(it != m_pResizeDrawObjects->end());
+#if 0
+    auto nIndex = std::distance(m_pResizeDrawObjects->begin(), it);
+#endif
+    m_pResizeDrawObjects->erase(it);
+#if 0
+    auto otherit = m_xDrawObjectPercentWidths->begin() + nIndex * 3;
+    m_xDrawObjectPercentWidths->erase(otherit, otherit + 3);
+#endif
+}
+
 HTMLTable::~HTMLTable()
 {
     m_pParser->DeregisterHTMLTable(this);
 
-    m_pResizeDrawObjects.reset();
+    if (m_pResizeDrawObjects)
+    {
+        size_t nCount = m_pResizeDrawObjects->size();
+        for (size_t i = 0; i < nCount; ++i)
+        {
+            SdrObject *pObj = (*m_pResizeDrawObjects)[i];
+            pObj->RemoveObjectUser(*this);
+        }
+        m_pResizeDrawObjects.reset();
+    }
+
     m_pDrawObjectPrcWidths.reset();
 
     m_pContext.reset();
@@ -2483,6 +2512,7 @@ void HTMLTable::RegisterDrawObject( SdrObject *pObj, sal_uInt8 nPrcWidth )
     if( !m_pResizeDrawObjects )
         m_pResizeDrawObjects.reset(new SdrObjects);
     m_pResizeDrawObjects->push_back( pObj );
+    pObj->AddObjectUser(*this);
 
     if( !m_pDrawObjectPrcWidths )
         m_pDrawObjectPrcWidths.reset(new std::vector<sal_uInt16>);
