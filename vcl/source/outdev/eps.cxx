3e237c278e199f4c39e89840b553cc254e2eab24
Chris Sherlock <chris.sherlock79@gmail.com>
2021-11-03 01:35:12 +1100

    vcl: move eps functions to eps.cxx
    Change-Id: I0d61835d0964e18b578ec42b7002671310a8a36d

---
+++ vcl/source/outdev/eps.cxx
@@ -0,0 +1,84 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#include <vcl/gfxlink.hxx>
+#include <vcl/graph.hxx>
+#include <vcl/metaact.hxx>
+#include <vcl/virdev.hxx>
+
+#include <salgdi.hxx>
+
+bool OutputDevice::DrawEPS( const Point& rPoint, const Size& rSize,
+                            const GfxLink& rGfxLink, GDIMetaFile* pSubst )
+{
+    bool bDrawn(true);
+
+    if ( mpMetaFile )
+    {
+        GDIMetaFile aSubst;
+
+        if( pSubst )
+            aSubst = *pSubst;
+
+        mpMetaFile->AddAction( new MetaEPSAction( rPoint, rSize, rGfxLink, aSubst ) );
+    }
+
+    if ( !IsDeviceOutputNecessary() || ImplIsRecordLayout() )
+        return bDrawn;
+
+    if( mbOutputClipped )
+        return bDrawn;
+
+    tools::Rectangle aRect( ImplLogicToDevicePixel( tools::Rectangle( rPoint, rSize ) ) );
+
+    if( !aRect.IsEmpty() )
+    {
+        // draw the real EPS graphics
+        if( rGfxLink.GetData() && rGfxLink.GetDataSize() )
+        {
+            if( !mpGraphics && !AcquireGraphics() )
+                return bDrawn;
+            assert(mpGraphics);
+
+            if( mbInitClipRegion )
+                InitClipRegion();
+
+            aRect.Justify();
+            bDrawn = mpGraphics->DrawEPS( aRect.Left(), aRect.Top(), aRect.GetWidth(), aRect.GetHeight(),
+                         const_cast<sal_uInt8*>(rGfxLink.GetData()), rGfxLink.GetDataSize(), *this );
+        }
+
+        // else draw the substitution graphics
+        if( !bDrawn && pSubst )
+        {
+            GDIMetaFile* pOldMetaFile = mpMetaFile;
+
+            mpMetaFile = nullptr;
+            Graphic(*pSubst).Draw(*this, rPoint, rSize);
+            mpMetaFile = pOldMetaFile;
+        }
+    }
+
+    if( mpAlphaVDev )
+        mpAlphaVDev->DrawEPS( rPoint, rSize, rGfxLink, pSubst );
+
+    return bDrawn;
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
