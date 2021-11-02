97d14c7765ce397202d4e018173a68f9bb49c151
Stephan Bergmann <sbergman@redhat.com>
2021-11-02 08:41:42 +0100

    Tweak loplugin:implicitboolconversion to allow some more bool -> sal_Bool
    ...in templated code, to cater for the needs of
    <https://gerrit.libreoffice.org/c/core/+/124400> "Prepare for removal of
    non-const operator[] from Sequence in testtools".
    
    For one, by defining ImplicitBoolConversion::TraverseInitListExpr, make sure
    that Clang versions before and after
    <https://github.com/llvm/llvm-project/commit/0a42fe70a566f22599e04a6f1344ca2dc5565e17>
    "[AST] Treat semantic form of InitListExpr as implicit code in traversals"
    behave the same.  Old versions of Clang would have erroneously reported
    
      Sequence<Sequence<sal_Bool>> s2{ { false } };
    
    (and reported
    
      Sequence<Sequence<sal_Int32>> s4{ { false } };
    
    twice) in compilerplugins/clang/test/implicitboolconversion.cxx when one of the
    four combinations of syntactic/semantic visit of the outer/inner InitListExpr
    defeated the intended suppression logic in
    ImplicitBoolConversion::TraverseCXXStdInitializerListExpr.
    
    And for another, ImplicitBoolConversion::TraverseInitListExpr can subsume the
    exising ImplicitBoolConversion::TraverseCXXStdInitializerListExpr.
    
    But for a third, that would still make
    
      Sequence<Wrap2<sal_Bool>> s6{ { false } };
    
    in compilerplugins/clang/test/implicitboolconversion.cxx emit a false warning,
    so add a cheesy "TODO" chicken-out special case to
    ImplicitBoolConversion::checkCXXConstructExpr for now.
    
    Change-Id: Ib9a1b78a7812feb98c673b75a357af7737168342

--- compilerplugins/clang/test/implicitboolconversion.cxx
+++ compilerplugins/clang/test/implicitboolconversion.cxx
@@ -10,8 +10,28 @@
 #include <sal/config.h>
 
 #include <atomic>
+#include <initializer_list>
 
 #include <sal/types.h>
+
+template <typename T> struct Sequence
+{
+    Sequence(std::initializer_list<T>);
+};
+
+template <typename T> struct Wrap1
+{
+    T element;
+};
+
+template <typename T> struct Wrap2
+{
+    Wrap2(T const& e)
+        : element(e)
+    {
+    }
+    T element;
+};
 
 bool g();
 
@@ -32,6 +52,24 @@ void f()
     bool b2 = true;
     b2 &= g();
     (void)b2;
+    Sequence<sal_Bool> s1{ false };
+    (void)s1;
+    Sequence<Sequence<sal_Bool>> s2{ { false } };
+    (void)s2;
+    // expected-error@+1 {{implicit conversion (IntegralCast) from 'bool' to 'const int' [loplugin:implicitboolconversion]}}
+    Sequence<sal_Int32> s3{ false };
+    (void)s3;
+    // expected-error@+1 {{implicit conversion (IntegralCast) from 'bool' to 'const int' [loplugin:implicitboolconversion]}}
+    Sequence<Sequence<sal_Int32>> s4{ { false } };
+    (void)s4;
+    Wrap1<sal_Bool> w1{ false };
+    (void)w1;
+    Sequence<Wrap1<sal_Bool>> s5{ { false } };
+    (void)s5;
+    Wrap2<sal_Bool> w2{ false };
+    (void)w2;
+    Sequence<Wrap2<sal_Bool>> s6{ { false } };
+    (void)s6;
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
