f8b10b76bc3000eab3269de4472d6c28bd308059
Stephan Bergmann <sbergman@redhat.com>
2021-11-04 14:40:25 +0100

    The std::auto_ptr workarounds should no longer be necessary
    ...with the bundled Boost 1.76.0, as boost/locale no longer uses it since
    <https://github.com/boostorg/locale/commit/322437a485c63d9fce4dc620597b6c75b6396daf>
    "Adding dual auto_ptr/unique_ptr support" in Boost 1.67.0 in combination with
    BOOST_NO_AUTO_PTR being set automatically when building for C++17 and beyond for
    both libc++ (via
    <https://github.com/boostorg/config/commit/0df7552f38cc059defa4189d7a36101925559eb8>
    "define BOOST_NO_AUTO_PTR when building with libc++ and C++17" in Boost 1.65.0)
    and MSVC (via
    <https://github.com/boostorg/config/commit/776bc8ac107e864fc4c51d6aee0e532026a50281>
    "Update for MSVC14's _HAS_AUTO_PTR_ETC" in Boost 1.60.0; if we didn't globally
    set _HAS_AUTO_PTR_ETC in gb_COMPILERDEFS in
    solenv/gbuild/platform/com_MSC_defs.mk since
    61c88ae6945c241f5f2aeb844eeca0776b487132 "gbuild: always compile as C++17 with
    MSVC 2017" anyway)
    
    Change-Id: Idd9d44c8350217f19ad2fa6749b90a9ffce38511
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/124712
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

--- external/boost/StaticLibrary_boost_locale.mk
+++ external/boost/StaticLibrary_boost_locale.mk
@@ -18,13 +18,6 @@ $(eval $(call gb_StaticLibrary_add_defs,boost_locale,\
 	-DBOOST_ALL_NO_LIB -DBOOST_LOCALE_NO_WINAPI_BACKEND -DBOOST_LOCALE_NO_POSIX_BACKEND -DBOOST_USE_WINDOWS_H \
 ))
 
-# Needed when building against MSVC resp. libc++ in C++17 mode, as Boost 1.65.1
-# workdir/UnpackedTarball/boost/boost/locale/generator.hpp contains "std::auto_ptr<data> d;":
-$(eval $(call gb_StaticLibrary_add_defs,boost_locale, \
-    -D_HAS_AUTO_PTR_ETC=1 \
-    -D_LIBCPP_ENABLE_CXX17_REMOVED_AUTO_PTR \
-))
-
 $(eval $(call gb_StaticLibrary_use_external,boost_locale,boost_headers))
 
 $(eval $(call gb_StaticLibrary_set_generated_cxx_suffix,boost_locale,cpp))
