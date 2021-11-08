LibreOffice/core

commit 1597cc5b3e07dd24cb4cb10b35b1e93545e4b929

convert #defines to OUStringLiteral
mostly by doing

$ git grep -l '#define.*\"' -- *.cxx
    | xargs perl -pi -e
's/^#define\s+(\w+)\s+(\".*\")/constexpr OUStringLiteral \1 =
u\2;/g'

Change-Id: Idface893449b0ef2a3c5254865a300585d752fbb
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/119669
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

libreofficekit/source/gtk/lokdocview.cxx
@@ -35,7 +35,7 @@
 #endif
 
 // Cursor bitmaps from the installation set.
-#define CURSOR_HANDLE_DIR "/../share/libreofficekit/"
+constexpr OUStringLiteral CURSOR_HANDLE_DIR = u"/../share/libreofficekit/";
 // Number of handles around a graphic selection.
 #define GRAPHIC_HANDLE_COUNT 8
 // Maximum Zoom allowed

commit cf71714d2e401131e095d99da2944761c611ac0d

libreofficekit: fix lokdocview crash on startup
Regression from commit 1597cc5b3e07dd24cb4cb10b35b1e93545e4b929
(convert #defines to OUStringLiteral, 2021-07-29), the problem is that
this the new path was

instdir/programhandle_image_middle.png

instead of the old:

instdir/program/../share/libreofficekit/handle_image_middle.png

This is external code, just revert back to the old code.

Change-Id: I816e67091a2cd6bf07138a5166363803022bab8c

libreofficekit/source/gtk/lokdocview.cxx
@@ -35,7 +35,7 @@
 #endif
 
 // Cursor bitmaps from the installation set.
-constexpr OUStringLiteral CURSOR_HANDLE_DIR = u"/../share/libreofficekit/";
+#define CURSOR_HANDLE_DIR "/../share/libreofficekit/"
 // Number of handles around a graphic selection.
 #define GRAPHIC_HANDLE_COUNT 8
 // Maximum Zoom allowed