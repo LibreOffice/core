168dfbb12605ac5068132ed4dc2272dd5e0d3b60
Xisco Fauli <xiscofauli@libreoffice.org>
2021-11-09 13:06:49 +0100

    tdf#145158: sw: Add UItest
    Change-Id: I942be9bc342a8a2a1b2f4c526b101e6d446c70fb

---
+++ sw/qa/uitest/writer_tests7/tdf145158.py
@@ -0,0 +1,36 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+from uitest.framework import UITestCase
+from libreoffice.uno.propertyvalue import mkPropertyValues
+from uitest.uihelper.common import get_state_as_dict
+from uitest.uihelper.common import select_by_text
+
+class tdf145158(UITestCase):
+
+    def test_tdf145158(self):
+
+        with self.ui_test.create_doc_in_start_center("writer"):
+
+            xMainWindow = self.xUITest.getTopFocusWindow()
+            xWriterEdit = xMainWindow.getChild("writer_edit")
+
+            xFontsize = xMainWindow.getChild("fontsizecombobox")
+
+            self.assertEqual("12 pt", get_state_as_dict(xFontsize)['Text'])
+
+            select_by_text(xFontsize, "10.5 pt")
+
+            xWriterEdit.executeAction("TYPE", mkPropertyValues({"TEXT": "Test"}))
+
+            with self.ui_test.execute_dialog_through_command(".uno:FontDialog") as xDialog:
+                xSizeFont = xDialog.getChild("westsizelb-cjk")
+
+                # Without the fix in place, this test would have failed with
+                # AssertionError: '10.5 pt' != '11 pt'
+                self.assertEqual("10.5 pt", get_state_as_dict(xSizeFont)['Text'])
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
