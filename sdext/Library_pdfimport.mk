ee01ef86a67cd15dfe8092e3b2b8a971bceb434e
Kevin Suo <suokunlong@126.com>
2021-11-09 16:59:37 +0800

    tdf#114840 tdf#106057 tdf#55425 tdf#143472 sdext.pdfimport: use poppler to...
    ...check encryption.
    
    The checkEncryption function in our sdext.pdfimport wrapper.cxx code is quite
    old and weak. It does not support many new PDF features and contains many bugs.
    It does more than check encryption - it tries to read the PDF xref tables and
    tries to find the encryption information. For most of the bug with un-encrypted
    pdf, it just failed at the xref table reading stage.
    
    We use poppler as an out-of-the-process to generate the xpdfimport tokens, so
    why not use poppler to check encryption? Poppler's PDFDocFactory can be used
    for this purpose. It reads the PDF, and if it is encrypted then the isOk() is
    false and the checkErrorCode() returns the 4.
    
    Also in this patch, I have pushed the compiler to ignore -Wunused-parameter
    in poppler. Poppler has several places which include unused parameters.
    
    Also, the enumerators in parseKey has been changed to add a prefix to avoid
    compiling errors in Windows (because of the constants defined in wingdi.h).
    
    Change-Id: Ic60bf9c9264ebc3193513260c9bbd1d6b964ef4a

--- sdext/Library_pdfimport.mk
+++ sdext/Library_pdfimport.mk
@@ -35,6 +35,7 @@ $(eval $(call gb_Library_use_externals,pdfimport,\
 	boost_headers \
     zlib \
     $(if $(filter-out WNT MACOSX,$(OS)),fontconfig) \
+	$(if $(ENABLE_POPPLER), poppler) \
 ))
 
 ifeq ($(COM),MSC)
