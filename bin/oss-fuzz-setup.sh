48ae3cc1af37271e3ad6ef15a603b1d1add6a69b
Caolán McNamara <caolanm@redhat.com>
2021-11-10 16:38:39 +0000

    use tarball names from download.lst for oss-fuzz-setup.sh
    Change-Id: I6534e8c81a6598e49de99b08bae80657e1355965
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/125004
    Tested-by: Caolán McNamara <caolanm@redhat.com>
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>

--- bin/oss-fuzz-setup.sh
+++ bin/oss-fuzz-setup.sh
@@ -4,69 +4,72 @@ set -e
 
 echo start downloading dependencies at `date -u`
 
+# convert FOO := BAR$(MICRO) to FOO=BAR$MICRO
+source <(sed -e's# := #=#g' download.lst | sed -e 's#[)(]##g')
+
 cd $SRC
 
 #cache build dependencies
 curl --no-progress-meter -S \
-    -C - -O https://dev-www.libreoffice.org/src/liberation-fonts-ttf-2.1.4.tar.gz \
-    -C - -O https://dev-www.libreoffice.org/extern/f543e6e2d7275557a839a164941c0a86e5f2c3f2a0042bfc434c88c6dde9e140-opens___.ttf \
-    -C - -O https://dev-www.libreoffice.org/src/1f5def51ca0026cd192958ef07228b52-rasqal-0.9.33.tar.gz \
-    -C - -O https://dev-www.libreoffice.org/src/48d647fbd8ef8889e5a7f422c1bfda94-clucene-core-2.3.3.4.tar.gz \
-    -C - -O https://dev-www.libreoffice.org/src/5ade6ae2a99bc1e9e57031ca88d36dad-hyphen-2.8.8.tar.gz \
-    -C - -O https://dev-www.libreoffice.org/src/a39f6c07ddb20d7dd2ff1f95fa21e2cd-raptor2-2.0.15.tar.gz \
-    -C - -O https://dev-www.libreoffice.org/src/a8c2c5b8f09e7ede322d5c602ff6a4b6-mythes-1.2.4.tar.gz \
-    -C - -O https://dev-www.libreoffice.org/src/e5be03eda13ef68aabab6e42aa67715e-redland-1.0.17.tar.gz \
-    -C - -O https://dev-www.libreoffice.org/src/boost_1_77_0.tar.xz \
-    -C - -O https://dev-www.libreoffice.org/src/box2d-2.3.1.tar.gz \
-    -C - -O https://dev-www.libreoffice.org/src/dtoa-20180411.tgz \
-    -C - -O https://dev-www.libreoffice.org/src/expat-2.4.1.tar.bz2 \
-    -C - -O https://dev-www.libreoffice.org/src/libcuckoo-93217f8d391718380c508a722ab9acd5e9081233.tar.gz \
-    -C - -O https://dev-www.libreoffice.org/src/libjpeg-turbo-2.1.1.tar.gz \
-    -C - -O https://dev-www.libreoffice.org/src/lcms2-2.12.tar.gz \
-    -C - -O https://dev-www.libreoffice.org/src/libexttextcat-3.4.6.tar.xz \
-    -C - -O https://dev-www.libreoffice.org/src/cppunit-1.15.1.tar.gz \
-    -C - -O https://dev-www.libreoffice.org/src/cairo-1.17.4.tar.xz \
-    -C - -O https://dev-www.libreoffice.org/src/curl-7.79.1.tar.xz \
-    -C - -O https://dev-www.libreoffice.org/src/xmlsec1-1.2.32.tar.gz \
-    -C - -O https://dev-www.libreoffice.org/src/liblangtag-0.6.3.tar.bz2 \
-    -C - -O https://dev-www.libreoffice.org/src/libabw-0.1.3.tar.xz \
-    -C - -O https://dev-www.libreoffice.org/src/libcdr-0.1.7.tar.xz \
-    -C - -O https://dev-www.libreoffice.org/src/libcmis-0.5.2.tar.xz \
-    -C - -O https://dev-www.libreoffice.org/src/libe-book-0.1.3.tar.xz \
-    -C - -O https://dev-www.libreoffice.org/src/libetonyek-0.1.10.tar.xz \
-    -C - -O https://dev-www.libreoffice.org/src/libfreehand-0.1.2.tar.xz \
-    -C - -O https://dev-www.libreoffice.org/src/libmspub-0.1.4.tar.xz \
-    -C - -O https://dev-www.libreoffice.org/src/libmwaw-0.3.20.tar.xz \
-    -C - -O https://dev-www.libreoffice.org/src/libodfgen-0.1.8.tar.xz \
-    -C - -O https://dev-www.libreoffice.org/src/liborcus-0.17.0.tar.bz2 \
-    -C - -O https://dev-www.libreoffice.org/src/libpagemaker-0.0.4.tar.xz \
-    -C - -O https://dev-www.libreoffice.org/src/libpng-1.6.37.tar.xz \
-    -C - -O https://dev-www.libreoffice.org/src/librevenge-0.0.4.tar.bz2 \
-    -C - -O https://dev-www.libreoffice.org/src/libstaroffice-0.0.7.tar.xz \
-    -C - -O https://dev-www.libreoffice.org/src/libvisio-0.1.7.tar.xz \
-    -C - -O https://dev-www.libreoffice.org/src/libwpd-0.10.3.tar.xz \
-    -C - -O https://dev-www.libreoffice.org/src/libwpg-0.3.3.tar.xz \
-    -C - -O https://dev-www.libreoffice.org/src/libwps-0.4.12.tar.xz \
-    -C - -O https://dev-www.libreoffice.org/src/libzmf-0.0.2.tar.xz \
-    -C - -O https://dev-www.libreoffice.org/src/pixman-0.40.0.tar.gz \
-    -C - -O https://dev-www.libreoffice.org/src/zlib-1.2.11.tar.xz \
-    -C - -O https://dev-www.libreoffice.org/src/mdds-2.0.0.tar.bz2 \
-    -C - -O https://dev-www.libreoffice.org/src/openssl-1.1.1l.tar.gz \
-    -C - -O https://dev-www.libreoffice.org/src/language-subtag-registry-2021-08-06.tar.bz2 \
-    -C - -O https://dev-www.libreoffice.org/src/graphite2-minimal-1.3.14.tgz \
-    -C - -O https://dev-www.libreoffice.org/src/harfbuzz-2.8.2.tar.xz \
-    -C - -O https://dev-www.libreoffice.org/src/glm-0.9.9.8.zip \
-    -C - -O https://dev-www.libreoffice.org/src/icu4c-69_1-src.tgz \
-    -C - -O https://dev-www.libreoffice.org/src/icu4c-69_1-data.zip \
-    -C - -O https://dev-www.libreoffice.org/src/libxml2-2.9.12.tar.gz \
-    -C - -O https://dev-www.libreoffice.org/src/libxslt-1.1.34.tar.gz \
-    -C - -O https://dev-www.libreoffice.org/src/hunspell-1.7.0.tar.gz \
-    -C - -O https://dev-www.libreoffice.org/src/freetype-2.11.0.tar.xz \
-    -C - -O https://dev-www.libreoffice.org/src/fontconfig-2.13.94.tar.xz \
-    -C - -O https://dev-www.libreoffice.org/src/libepoxy-1.5.9.tar.xz \
-    -C - -O https://dev-www.libreoffice.org/src/libepubgen-0.1.1.tar.xz \
-    -C - -O https://dev-www.libreoffice.org/src/libnumbertext-1.0.7.tar.xz \
-    -C - -O https://dev-www.libreoffice.org/src/libqxp-0.0.2.tar.xz
+    -C - -O https://dev-www.libreoffice.org/src/$FONT_LIBERATION_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/extern/$OPENSYMBOL_TTF \
+    -C - -O https://dev-www.libreoffice.org/src/$RASQAL_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$CLUCENE_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$HYPHEN_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$RAPTOR_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$MYTHES_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$REDLAND_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$BOOST_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$BOX2D_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$DTOA_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$EXPAT_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$CUCKOO_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$LIBJPEG_TURBO_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$LCMS2_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$LIBEXTTEXTCAT_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$CPPUNIT_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$CAIRO_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$CURL_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$XMLSEC_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$LIBLANGTAG_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$ABW_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$CDR_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$LIBCMIS_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$EBOOK_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$ETONYEK_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$FREEHAND_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$MSPUB_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$MWAW_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$ODFGEN_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$ORCUS_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$PAGEMAKER_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$LIBPNG_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$REVENGE_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$STAROFFICE_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$VISIO_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$WPD_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$WPG_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$WPS_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$ZMF_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$PIXMAN_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$ZLIB_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$MDDS_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$OPENSSL_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$LANGTAGREG_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$GRAPHITE_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$HARFBUZZ_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$GLM_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$ICU_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$ICU_DATA_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$LIBXML_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$LIBXSLT_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$HUNSPELL_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$FREETYPE_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$FONTCONFIG_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$EPOXY_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$EPUBGEN_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$LIBNUMBERTEXT_TARBALL \
+    -C - -O https://dev-www.libreoffice.org/src/$QXP_TARBALL
 #fuzzing dictionaries
 curl --no-progress-meter -S \
     -C - -O https://raw.githubusercontent.com/rc0r/afl-fuzz/master/dictionaries/gif.dict \
