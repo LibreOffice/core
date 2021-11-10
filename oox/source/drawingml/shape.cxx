5ed4033fee2b7871c87175f6feae9bd37ed5c6a4
Attila Bakos (NISZ) <bakos.attilakaroly@nisz.hu>
2021-11-10 14:10:11 +0100

    oox: textboxes in group shapes
    In this part, oox has been modified, in
    order to prepare for the WPG handling in
    import time:
    1) Added new parameter for WpgContext, to
    support nested Wpg shapes, and WPS enabled
    for the WPG member shapes.
    2) A bug has fixed, where group member line
    shape and connector shapes have
    wrong positions before in the group.
    3) Unit tests had to be modified, and 3
    of them temporaly disabled due to missing
    Writerfilter implementation (what will be
    the next commit)
    
    To summarise, now group shapes have textboxes
    and the text is imported to that, but complex
    content still missing. (this will be fixed in
    the writerfilter module, in the next commit)
    
    Known issue: WPG shapes with textboxes in
    floating table have issues during import
    at floating table conversion, so until this
    is not fixed this function is disabled for
    shapes in tables (will be fixed a follow up
    commit later).
    
    Do not forget in the previous 3 commits the
    function have been implemented in sw, so
    enabling WPS in the WPG shapes will not lead
    to assert and crash like it could before.
    
    Change-Id: I71032187697807087bd8f27f7c3a7b052e174bd7

--- oox/source/drawingml/shape.cxx
+++ oox/source/drawingml/shape.cxx
@@ -1531,7 +1531,7 @@ Reference< XShape > const & Shape::createAndInsert(
         // These can have a custom geometry, so position should be set here,
         // after creation but before custom shape handling, using the position
         // we got from the caller.
-        if (mbWps && aServiceName == "com.sun.star.drawing.LineShape")
+        if (mbWps && aServiceName == "com.sun.star.drawing.LineShape" && !pParentGroupShape)
             mxShape->setPosition(maPosition);
 
         if( bIsCustomShape )
