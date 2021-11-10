fd1570de17f3f586531587ee6c900fa90495a42c
Attila Bakos (NISZ) <bakos.attilakaroly@nisz.hu>
2021-11-10 14:10:11 +0100

    oox: textboxes in groupshapes -- part 4
    In this part, oox has been modified, in
    order to prepare for the WPG handling in
    import time:
    1) Added new parameter for WpgContext, to
    support nested Wpg shapes, and WPS enabled
    for the WPG member shapes.
    2) A bug has fixed, where group member
    line shapes and connector shapes have
    wrong positions before int he group.
    3) Unit test have to be modified, and 2
    of them temporaly disabled due to missing
    Writerfilter implementation (what will be
    the next commit)
    
    To summarise, now group shapes have textboxes
    and the text is imported to that, but complex
    content still missing. (this will be fixed in
    the writerfilter module, in the next commit)
    
    Do not forget in the previous 3 commits the
    function have been implemented in sw, so
    enabling WPS in the WPG shapes will not lead
    to assert and crash like it could before.
    
    Change-Id: I71032187697807087bd8f27f7c3a7b052e174bd7

--- oox/source/shape/WpgContext.hxx
+++ oox/source/shape/WpgContext.hxx
@@ -19,7 +19,8 @@ namespace oox::shape
 class WpgContext final : public oox::core::FragmentHandler2
 {
 public:
-    explicit WpgContext(oox::core::FragmentHandler2 const& rParent);
+    explicit WpgContext(oox::core::FragmentHandler2 const& rParent,
+                        oox::drawingml::ShapePtr pMaster);
     ~WpgContext() override;
 
     oox::core::ContextHandlerRef onCreateContext(sal_Int32 nElementToken,
