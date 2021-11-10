LibreOffice/core

commit 20ddabc88b6b45ea3efcc44ede5244ea526b09c4

tdf#144989 sw: disable idle jobs during printing or PDF export
The problem is that (with the kde5 and WNT vclplugs) the idle jobs
update fields and un-hides every section, which destroys the layout
that SwXTextDocument::getRendererCount() finalised with great effort.

This doesn't seem to happen with gtk3 or gen vclplugs.

 6  SwSectionNode::MakeOwnFrames(SwNodeIndex*, SwNodeIndex*)
 7  SwSectionFormat::MakeFrames()
 8  SwSection::ImplSetHiddenFlag(bool, bool)
 9  SwSection::SetCondHidden(bool)
 10 SwDocUpdateField::MakeFieldList_(SwDoc&, int)
 11 SwDocUpdateField::MakeFieldList(SwDoc&, bool, int)
 12 sw::DocumentFieldsManager::UpdateExpFields(SwTextField*, bool)
 13 sw::DocumentTimerManager::DoIdleJobs(Timer*)
 14 sw::DocumentTimerManager::LinkStubDoIdleJobs(void*, Timer*)
 15 Link<Timer*, void>::Call(Timer*) const
 16 Timer::Invoke()
 17 Scheduler::CallbackTaskScheduling() ()
 18 SalTimer::CallCallback()
 19 QtTimer::timeoutActivated()
 20 QtTimer::qt_static_metacall(QObject*, QMetaObject::Call, int, void**)
 21 void doActivate<false>(QObject*, int, void**) ()
 22 QTimer::timeout(QTimer::QPrivateSignal) ()
 23 QObject::event(QEvent*) ()
 24 QApplicationPrivate::notify_helper(QObject*, QEvent*) ()
 25 QCoreApplication::notifyInternal2(QObject*, QEvent*) ()
 26 QTimerInfoList::activateTimers() ()
 27 timerSourceDispatch()
 28 g_main_context_dispatch ()
 29 g_main_context_iterate.constprop ()
 30 g_main_context_iteration ()
 31 QEventDispatcherGlib::processEvents(QFlags<QEventLoop::ProcessEventsFlag>) ()
 32 QtInstance::ImplYield(bool, bool)
 33 QtInstance::DoYield(bool, bool)
 34 ImplYield(bool, bool)
 35 Application::Reschedule(bool)
 36 framework::StatusIndicatorFactory::impl_reschedule(bool)
 37 framework::StatusIndicatorFactory::setValue()
 38 framework::StatusIndicator::setValue(int)
 39 PDFExport::ExportSelection()
 40 PDFExport::Export()

Reportedly this started to happen with commit
4184569b963877c2a488ff05766654b9db194798 - presumably before that the

sw/source/uibase/uno/unotxdoc.cxx
@@ -2580,6 +2580,10 @@ sal_Int32 SAL_CALL SwXTextDocument::getRendererCount(
         }
     }
     OSL_ENSURE( nRet >= 0, "negative number of pages???" );
+    // tdf#144989 the layout is complete now - prevent DoIdleJobs() from
+    // messing it up, particulary SwDocUpdateField::MakeFieldList_() unhiding
+    // sections
+    pDoc->getIDocumentTimerAccess().BlockIdling();
 
     return nRet;
 }
@@ -3004,6 +3008,8 @@ void SAL_CALL SwXTextDocument::render(
     {
         m_pRenderData.reset();
         m_pPrintUIOptions.reset();
+        // tdf#144989 enable DoIdleJobs() again after last page
+        pDoc->getIDocumentTimerAccess().UnblockIdling();
     }
 }
 

commit a188d3ee5c47087791c43a0974883f7ab7a70554

tdf#145584: fix crash on exporting as PDF with Selection rRange
Regression from 20ddabc88b6b45ea3efcc44ede5244ea526b09c4
tdf#144989 sw: disable idle jobs during printing or PDF export
The problem is that (with the kde5 and WNT vclplugs) the idle jobs
update fields and un-hides every section, which destroys the layout
that SwXTextDocument::getRendererCount() finalised with great effort

bt:
0  0x00007f773af65dfa in SwXTextDocument::render(int, com::sun::star::uno::Any const&, com::sun::star::uno::Sequence<com::sun::star::beans::PropertyValue> const&) (this=0x66e4980, nRenderer=0, rSelection=
  uno::Any("com.sun.star.uno.XInterface": {_vptr$XInterface = 0x9579020}), rxOptions=uno::Sequence of length 8 = {...}) at sw/source/uibase/uno/unotxdoc.cxx:3012
1  0x00007f76ff5f2832 in PDFExport::ExportSelection(vcl::PDFWriter&, com::sun::star::uno::Reference<com::sun::star::view::XRenderable> const&, com::sun::star::uno::Any const&, StringRangeEnumerator const&, com::sun::star::uno::Sequence<com::sun::star::beans::PropertyValue>&, int) (this=0x7ffc7ee0e990, rPDFWriter=..., rRenderable=uno::Reference to (SwXTextDocument *) 0x66e4c38, rSelection=
  uno::Any("com.sun.star.uno.XInterface": {_vptr$XInterface = 0x9579020}), rRangeEnum=..., rRenderOptions=uno::Sequence of length 8 = {...}, nPageCount=1) at filter/source/pdf/pdfexport.cxx:218
2  0x00007f76ff5f787e in PDFExport::Export(rtl::OUString const&, com::sun::star::uno::Sequence<com::sun::star::beans::PropertyValue> const&)
    (this=0x7ffc7ee0e990, rFile="file:///tmp/lu4uimg.tmp/lu4uimk.tmp", rFilterData=uno::Sequence of length 54 = {...}) at filter/source/pdf/pdfexport.cxx:949
3  0x00007f76ff60a4a8 in PDFFilter::implExport(com::sun::star::uno::Sequence<com::sun::star::beans::PropertyValue> const&) (this=0x71798a0, rDescriptor=uno::Sequence of length 14 = {...})
    at filter/source/pdf/pdffilter.cxx:161

sw/source/uibase/uno/unotxdoc.cxx
@@ -3006,10 +3006,10 @@ void SAL_CALL SwXTextDocument::render(
     }
     if( bLastPage )
     {
-        m_pRenderData.reset();
-        m_pPrintUIOptions.reset();
         // tdf#144989 enable DoIdleJobs() again after last page
         pDoc->getIDocumentTimerAccess().UnblockIdling();
+        m_pRenderData.reset();
+        m_pPrintUIOptions.reset();
     }
 }
 