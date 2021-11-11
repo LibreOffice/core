27e0bf9d5ec73d01dc9b10e1cdc8fd9aa7b00e1c
Noel Grandin <noelgrandin@gmail.com>
2021-11-11 19:08:17 +0200

    modernise IdPropArrayHelper a little
    Change-Id: I73bbb59d41ba752e5bbb798ae590e60fdb550d8b

--- include/comphelper/IdPropArrayHelper.hxx
+++ include/comphelper/IdPropArrayHelper.hxx
@@ -16,40 +16,32 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
-#ifndef INCLUDED_COMPHELPER_IDPROPARRAYHELPER_HXX
-#define INCLUDED_COMPHELPER_IDPROPARRAYHELPER_HXX
+#pragma once
 
 #include <sal/config.h>
 
-#include <map>
-
 #include <osl/mutex.hxx>
-#include <osl/diagnose.h>
 #include <rtl/instance.hxx>
 #include <cppuhelper/propshlp.hxx>
+#include <cassert>
+#include <unordered_map>
 
 namespace comphelper
 {
 
-    //  OIdPropertyArrayUsageHelper
-
     template <typename TYPE> struct OIdPropertyArrayUsageHelperMutex
             : public rtl::Static< ::osl::Mutex, OIdPropertyArrayUsageHelperMutex<TYPE> > {};
 
-    typedef std::map< sal_Int32, ::cppu::IPropertyArrayHelper* > OIdPropertyArrayMap;
+    typedef std::unordered_map< sal_Int32, ::cppu::IPropertyArrayHelper* > OIdPropertyArrayMap;
     template <class TYPE>
     class OIdPropertyArrayUsageHelper
     {
-    protected:
-        static sal_Int32                        s_nRefCount;
-        static OIdPropertyArrayMap*             s_pMap;
-
     public:
         OIdPropertyArrayUsageHelper();
         virtual ~OIdPropertyArrayUsageHelper()
         {
             ::osl::MutexGuard aGuard(OIdPropertyArrayUsageHelperMutex<TYPE>::get());
-            OSL_ENSURE(s_nRefCount > 0, "OIdPropertyArrayUsageHelper::~OIdPropertyArrayUsageHelper : suspicious call : have a refcount of 0 !");
+            assert(s_nRefCount > 0 && "OIdPropertyArrayUsageHelper::~OIdPropertyArrayUsageHelper : suspicious call : have a refcount of 0 !");
             if (!--s_nRefCount)
             {
                 // delete the element
@@ -73,8 +65,10 @@ namespace comphelper
             @return                         a pointer to the newly created array helper. Must not be NULL.
         */
         virtual ::cppu::IPropertyArrayHelper* createArrayHelper(sal_Int32 nId) const = 0;
+    private:
+        static sal_Int32                        s_nRefCount;
+        static OIdPropertyArrayMap*             s_pMap;
     };
-
 
     template<class TYPE>
     sal_Int32                       OIdPropertyArrayUsageHelper< TYPE >::s_nRefCount    = 0;
@@ -82,33 +76,30 @@ namespace comphelper
     template<class TYPE>
     OIdPropertyArrayMap*            OIdPropertyArrayUsageHelper< TYPE >::s_pMap = nullptr;
 
-
     template <class TYPE>
     OIdPropertyArrayUsageHelper<TYPE>::OIdPropertyArrayUsageHelper()
     {
         ::osl::MutexGuard aGuard(OIdPropertyArrayUsageHelperMutex<TYPE>::get());
         // create the map if necessary
-        if (s_pMap == nullptr)
+        if (!s_pMap)
             s_pMap = new OIdPropertyArrayMap;
         ++s_nRefCount;
     }
 
-
     template <class TYPE>
     ::cppu::IPropertyArrayHelper* OIdPropertyArrayUsageHelper<TYPE>::getArrayHelper(sal_Int32 nId)
     {
-        OSL_ENSURE(s_nRefCount, "OIdPropertyArrayUsageHelper::getArrayHelper : suspicious call : have a refcount of 0 !");
+        assert(s_nRefCount && "OIdPropertyArrayUsageHelper::getArrayHelper : suspicious call : have a refcount of 0 !");
         ::osl::MutexGuard aGuard(OIdPropertyArrayUsageHelperMutex<TYPE>::get());
         // do we have the array already?
         auto& rEntry = (*s_pMap)[nId];
         if (!rEntry)
         {
             rEntry = createArrayHelper(nId);
-            OSL_ENSURE((*s_pMap)[nId], "OIdPropertyArrayUsageHelper::getArrayHelper : createArrayHelper returned nonsense !");
+            assert(rEntry && "OIdPropertyArrayUsageHelper::getArrayHelper : createArrayHelper returned nonsense !");
         }
         return (*s_pMap)[nId];
     }
 }
-#endif // INCLUDED_COMPHELPER_IDPROPARRAYHELPER_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
