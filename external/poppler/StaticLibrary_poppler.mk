f19d1957192929a26edca6afd767316f677c99d5
Michael Stahl <michael.stahl@allotropia.de>
2021-11-11 11:01:43 +0100

    poppler: upgrade to release 21.11.0
    The changelogs tend to mention "crash in malformed files" a lot.
    
    Change-Id: Iadc1d9cc23abd09a8fff58ba0cb7a7803236a542

--- external/poppler/StaticLibrary_poppler.mk
+++ external/poppler/StaticLibrary_poppler.mk
@@ -74,6 +74,7 @@ $(eval $(call gb_StaticLibrary_add_generated_exception_objects,poppler,\
 	UnpackedTarball/poppler/fofi/FoFiType1C \
 	UnpackedTarball/poppler/fofi/FoFiIdentifier \
 	UnpackedTarball/poppler/poppler/Annot \
+	UnpackedTarball/poppler/poppler/AnnotStampImageHelper \
 	UnpackedTarball/poppler/poppler/Array \
 	UnpackedTarball/poppler/poppler/BBoxOutputDev \
 	UnpackedTarball/poppler/poppler/CachedFile \
@@ -138,6 +139,7 @@ $(eval $(call gb_StaticLibrary_add_generated_exception_objects,poppler,\
 	UnpackedTarball/poppler/poppler/Movie \
 	UnpackedTarball/poppler/poppler/Rendition \
 	UnpackedTarball/poppler/poppler/DCTStream \
+	UnpackedTarball/poppler/splash/SplashBitmap \
 ))
 
 # vim: set noet sw=4 ts=4:
