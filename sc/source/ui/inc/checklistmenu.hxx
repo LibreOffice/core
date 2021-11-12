3e69e6a2eb8f011b90c211d2e7070cec41ba21ca
Caolán McNamara <caolanm@redhat.com>
2021-11-12 15:39:49 +0000

    Resolves: tdf#145645 pivot table popups don't dismiss when item activated
    Change-Id: I5a68a08600b7792d924ec77694a60651df5d9c83

--- sc/source/ui/inc/checklistmenu.hxx
+++ sc/source/ui/inc/checklistmenu.hxx
@@ -62,7 +62,8 @@ public:
     {
     public:
         virtual ~Action() {}
-        virtual void execute() = 0;
+        // return true to dismiss the popup
+        virtual bool execute() = 0;
     };
 
     struct ResultEntry
