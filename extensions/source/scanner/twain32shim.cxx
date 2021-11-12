901115b99120fd85b31ea185a79f363a2f940720
Hossein <hossein@libreoffice.org>
2021-11-12 15:32:13 +0100

    Use o3tl::convert
    Use o3tl::convert to convert nXRes and nYRes from meters to pixels
    and store them in pBIH->biXPelsPerMeter and pBIH->biYPelsPerMeter
    
    Change-Id: Ia40f17b7d1c27c094158af89bf15ed2f11bdfc05

--- extensions/source/scanner/twain32shim.cxx
+++ extensions/source/scanner/twain32shim.cxx
@@ -30,6 +30,7 @@
 #include "twain32shim.hxx"
 #include <tools/helpers.hxx>
 #include <twain/twain.h>
+#include <o3tl/unit_conversion.hxx>
 
 #define WM_TWAIN_FALLBACK (WM_SHIM_INTERNAL + 0)
 
@@ -421,10 +422,11 @@ void ImpTwain::ImplXfer()
                             {
                                 // set resolution of bitmap
                                 BITMAPINFOHEADER* pBIH = static_cast<BITMAPINFOHEADER*>(pBmpMem);
-                                static const double fFactor = 100.0 / 2.54;
 
-                                pBIH->biXPelsPerMeter = FRound(fFactor * nXRes);
-                                pBIH->biYPelsPerMeter = FRound(fFactor * nYRes);
+                                pBIH->biXPelsPerMeter
+                                    = o3tl::convert(nXRes, o3tl::Length::m, o3tl::Length::px);
+                                pBIH->biYPelsPerMeter
+                                    = o3tl::convert(nYRes, o3tl::Length::m, o3tl::Length::px);
                             }
 
                             HANDLE hMap = CreateFileMappingW(INVALID_HANDLE_VALUE, nullptr,
