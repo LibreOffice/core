da7b06425155938fb0d02c6c178d63fd95ad0cd4
Dr. David Alan Gilbert <dave@treblig.org>
2021-11-13 01:33:06 +0000

    tdf#143216: pdfwriter: Don't treat semi-valid URIs as local paths
    Currently the PDF writer treats URIs that are rejected by INetURIObject,
    as local files, and prepends a path to them.  For URIs that are valid
    according to the basic URI syntax, but unhandled by INetURIObject
    (such as http://user:password@domain) this produces a confusing result
    with a ./uri in the PDF.
    
    Avoid the prefixing where the URI follows the basic URI syntax, even
    if INetURIObject didn't like it.
    
    Change-Id: I87c599885a40fd7101c678ae79f83f594d0f23ee
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/125202
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>
    Tested-by: Jenkins
    Signed-off-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/131259

--- vcl/qa/cppunit/pdfexport/pdfexport.cxx
+++ vcl/qa/cppunit/pdfexport/pdfexport.cxx
@@ -2963,6 +2963,12 @@ CPPUNIT_TEST_FIXTURE(PdfExportTest, testURIs)
                 true,
             },
             {
+                // tdf 143216
+                "http://username:password@example.com",
+                "http://username:password@example.com",
+                true,
+            },
+            {
                 "git://git.example.org/project/example",
                 "git://git.example.org/project/example",
                 true,
