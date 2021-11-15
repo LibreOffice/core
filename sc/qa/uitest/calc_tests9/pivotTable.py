35d9d085436e159bdeb6cba9c94865d6346af530
Xisco Fauli <xiscofauli@libreoffice.org>
2021-11-15 21:07:01 +0100

    uitest: sc: add support for pivot table popup
    Change-Id: Id990178051e81a81bf6f6a0fb920473d3ee21fff
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/125259
    Tested-by: Jenkins
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- sc/qa/uitest/calc_tests9/pivotTable.py
+++ sc/qa/uitest/calc_tests9/pivotTable.py
@@ -7,10 +7,11 @@
 from uitest.framework import UITestCase
 from libreoffice.uno.propertyvalue import mkPropertyValues
 from uitest.uihelper.common import get_state_as_dict, get_url_for_data_file
+from libreoffice.calc.document import get_cell_by_position
 
 class pivotTable(UITestCase):
 
-   def test_cancelButton(self):
+    def test_cancelButton(self):
 
         # This is basically a test for cf93998eb5abc193d95ae5433bf4dfd11a9d62d8
         # Without the fix in place, this test would have crashed
@@ -59,5 +60,37 @@ class pivotTable(UITestCase):
                         xEmptyLine = dialog2.getChild("emptyline")
 
                         self.assertEqual('true', get_state_as_dict(xEmptyLine)['Selected'])
+
+    def test_popup(self):
+        with self.ui_test.load_file(get_url_for_data_file("pivotTable.ods")) as calc_doc:
+
+            xCalcDoc = self.xUITest.getTopFocusWindow()
+            gridwin = xCalcDoc.getChild("grid_window")
+
+            self.assertEqual("a", get_cell_by_position(calc_doc, 0, 3, 1).getString())
+            self.assertEqual("b", get_cell_by_position(calc_doc, 0, 3, 2).getString())
+            self.assertEqual("m", get_cell_by_position(calc_doc, 0, 4, 1).getString())
+            self.assertEqual("n", get_cell_by_position(calc_doc, 0, 4, 2).getString())
+            self.assertEqual("1", get_cell_by_position(calc_doc, 0, 5, 1).getString())
+            self.assertEqual("1", get_cell_by_position(calc_doc, 0, 5, 2).getString())
+
+            gridwin.executeAction("LAUNCH", mkPropertyValues({"PIVOTTABLE": "", "COL": "3", "ROW": "0"}))
+            xFloatWindow = self.xUITest.getFloatWindow()
+            xCheckListMenu = xFloatWindow.getChild("check_list_menu")
+
+            xTreeList = xCheckListMenu.getChild("check_list_box")
+            xFirstEntry = xTreeList.getChild("0")
+
+            xFirstEntry.executeAction("CLICK", tuple())
+
+            xOkBtn = xFloatWindow.getChild("ok")
+            xOkBtn.executeAction("CLICK", tuple())
+
+            self.assertEqual("b", get_cell_by_position(calc_doc, 0, 3, 1).getString())
+            self.assertEqual("Total Result", get_cell_by_position(calc_doc, 0, 3, 2).getString())
+            self.assertEqual("n", get_cell_by_position(calc_doc, 0, 4, 1).getString())
+            self.assertEqual("", get_cell_by_position(calc_doc, 0, 4, 2).getString())
+            self.assertEqual("1", get_cell_by_position(calc_doc, 0, 5, 1).getString())
+            self.assertEqual("1", get_cell_by_position(calc_doc, 0, 5, 2).getString())
 
 # vim: set shiftwidth=4 softtabstop=4 expandtab:
