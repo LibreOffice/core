b89f2b5d711a733cfd9ecfa9efe6c6849832fdf4
Harjot <harjotsingh20022001@gmail.com>
2021-11-17 19:48:56 +0530

    Replaced include guards with pragma once in all files of /i18npool/inc
    Change-Id: Id67d8926fdc1616add1bd9de051911b2aae030e4

--- i18npool/inc/numberformatcode.hxx
+++ i18npool/inc/numberformatcode.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_I18NPOOL_INC_NUMBERFORMATCODE_HXX
-#define INCLUDED_I18NPOOL_INC_NUMBERFORMATCODE_HXX
+#pragma once;
 
 #include <cppuhelper/implbase.hxx>
 
@@ -69,6 +68,6 @@ private:
 };
 
 
-#endif // INCLUDED_I18NPOOL_INC_NUMBERFORMATCODE_HXX
+
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
