db47e955264a27b5f7167c8e0ce5ea0dd388cd71
Noel Grandin <noel.grandin@collabora.co.uk>
2021-11-17 12:52:24 +0200

    loplugin:stringliteraldefine in chart2
    and drop namespace declarations that were surrounding the #define
    - namespace decls have no affect on #defines
    
    Change-Id: Ic66ba7837ba3a5c48a0cf414af30985c480d73af

--- chart2/inc/unonames.hxx
+++ chart2/inc/unonames.hxx
@@ -9,28 +9,28 @@
 
 #pragma once
 
-#define CHART_UNONAME_SORT_BY_XVALUES       "SortByXValues"
-#define CHART_UNONAME_SPLINE_TYPE           "SplineType"
-#define CHART_UNONAME_SPLINE_ORDER          "SplineOrder"
-#define CHART_UNONAME_SPLINE_RESOLUTION     "SplineResolution"
-#define CHART_UNONAME_CURVE_STYLE           "CurveStyle"
-#define CHART_UNONAME_CURVE_RESOLUTION      "CurveResolution"
-#define CHART_UNONAME_NUMFMT                "NumberFormat"
-#define CHART_UNONAME_LINK_TO_SRC_NUMFMT    "LinkNumberFormatToSource"
-#define CHART_UNONAME_ERRORBAR_X            "ErrorBarX"
-#define CHART_UNONAME_ERRORBAR_Y            "ErrorBarY"
-#define CHART_UNONAME_LABEL                 "Label"
-#define CHART_UNONAME_LABEL_SEP             "LabelSeparator"
-#define CHART_UNONAME_LABEL_BORDER_STYLE    "LabelBorderStyle"
-#define CHART_UNONAME_LABEL_BORDER_WIDTH    "LabelBorderWidth"
-#define CHART_UNONAME_LABEL_BORDER_COLOR    "LabelBorderColor"
-#define CHART_UNONAME_LABEL_BORDER_DASH     "LabelBorderDash"
-#define CHART_UNONAME_LABEL_BORDER_DASHNAME "LabelBorderDashName"
-#define CHART_UNONAME_LABEL_BORDER_TRANS    "LabelBorderTransparency"
-#define CHART_UNONAME_LABEL_FILL_STYLE      "LabelFillStyle"
-#define CHART_UNONAME_LABEL_FILL_BACKGROUND "LabelFillBackground"
-#define CHART_UNONAME_LABEL_FILL_HATCH_NAME "LabelFillHatchName"
-#define CHART_UNONAME_LABEL_FILL_COLOR      "LabelFillColor"
-#define CHART_UNONAME_CUSTOM_LABEL_FIELDS   "CustomLabelFields"
+constexpr OUStringLiteral CHART_UNONAME_SORT_BY_XVALUES = u"SortByXValues";
+constexpr OUStringLiteral CHART_UNONAME_SPLINE_TYPE = u"SplineType";
+constexpr OUStringLiteral CHART_UNONAME_SPLINE_ORDER = u"SplineOrder";
+constexpr OUStringLiteral CHART_UNONAME_SPLINE_RESOLUTION = u"SplineResolution";
+constexpr OUStringLiteral CHART_UNONAME_CURVE_STYLE = u"CurveStyle";
+constexpr OUStringLiteral CHART_UNONAME_CURVE_RESOLUTION = u"CurveResolution";
+constexpr OUStringLiteral CHART_UNONAME_NUMFMT = u"NumberFormat";
+constexpr OUStringLiteral CHART_UNONAME_LINK_TO_SRC_NUMFMT = u"LinkNumberFormatToSource";
+constexpr OUStringLiteral CHART_UNONAME_ERRORBAR_X = u"ErrorBarX";
+constexpr OUStringLiteral CHART_UNONAME_ERRORBAR_Y = u"ErrorBarY";
+constexpr OUStringLiteral CHART_UNONAME_LABEL = u"Label";
+constexpr OUStringLiteral CHART_UNONAME_LABEL_SEP = u"LabelSeparator";
+constexpr OUStringLiteral CHART_UNONAME_LABEL_BORDER_STYLE = u"LabelBorderStyle";
+constexpr OUStringLiteral CHART_UNONAME_LABEL_BORDER_WIDTH = u"LabelBorderWidth";
+constexpr OUStringLiteral CHART_UNONAME_LABEL_BORDER_COLOR = u"LabelBorderColor";
+constexpr OUStringLiteral CHART_UNONAME_LABEL_BORDER_DASH = u"LabelBorderDash";
+constexpr OUStringLiteral CHART_UNONAME_LABEL_BORDER_DASHNAME = u"LabelBorderDashName";
+constexpr OUStringLiteral CHART_UNONAME_LABEL_BORDER_TRANS = u"LabelBorderTransparency";
+constexpr OUStringLiteral CHART_UNONAME_LABEL_FILL_STYLE = u"LabelFillStyle";
+constexpr OUStringLiteral CHART_UNONAME_LABEL_FILL_BACKGROUND = u"LabelFillBackground";
+constexpr OUStringLiteral CHART_UNONAME_LABEL_FILL_HATCH_NAME = u"LabelFillHatchName";
+constexpr OUStringLiteral CHART_UNONAME_LABEL_FILL_COLOR = u"LabelFillColor";
+constexpr OUStringLiteral CHART_UNONAME_CUSTOM_LABEL_FIELDS = u"CustomLabelFields";
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
