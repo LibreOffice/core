LibreOffice/core

commit 73334560b2dd2d60ac58d2cc2b1a5295490b03e1

Replace some macros in forms part 16
Remove DEFAULT_DECLARE_SUBTYPE + EXCEPT + EXPAND_PROP_INIT
+ DECLARE_DEFAULT_CLONE_CTOR + DECLARE_DEFAULT_LEAF_XTOR + FORWARD_TO_PEER_1
+ IMPLEMENT_DEFAULT_CLONING
+ ADD_ENTRY (include removing:
/home/julien/lo/libreoffice/forms/source/xforms/convert.cxx:56:14: error: unused function 'lcl_toXSD_bool' [-Werror,-Wunused-function]
    OUString lcl_toXSD_bool( const Any& rAny )
             ^
/home/julien/lo/libreoffice/forms/source/xforms/convert.cxx:60:9: error: unused function 'lcl_toAny_bool' [-Werror,-Wunused-function]
    Any lcl_toAny_bool( const OUString& rStr )
        ^
/home/julien/lo/libreoffice/forms/source/xforms/convert.cxx:67:14: error: unused function 'lcl_toXSD_double' [-Werror,-Wunused-function]
    OUString lcl_toXSD_double( const Any& rAny )
             ^
/home/julien/lo/libreoffice/forms/source/xforms/convert.cxx:80:9: error: unused function 'lcl_toAny_double' [-Werror,-Wunused-function]
    Any lcl_toAny_double( const OUString& rString )
        ^
)

Change-Id: I1e6923922b7037e8d1e9d009b05a27111c94b6fa
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/124827
Tested-by: Jenkins
Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

forms/source/solar/component/navbarcontrol.cxx
@@ -48,13 +48,6 @@ namespace frm
     using namespace ::com::sun::star::graphic;
     namespace FormFeature = ::com::sun::star::form::runtime::FormFeature;
 
-#define FORWARD_TO_PEER_1( unoInterface, method, param1 )   \
-    Reference< unoInterface > xTypedPeer( getPeer(), UNO_QUERY );   \
-    if ( xTypedPeer.is() )  \
-    {   \
-        xTypedPeer->method( param1 );  \
-    }
-
     ONavigationBarControl::ONavigationBarControl( const Reference< XComponentContext >& _rxORB)
         : m_xContext(_rxORB)
     {
@@ -174,20 +167,32 @@ namespace frm
 
     void SAL_CALL ONavigationBarControl::registerDispatchProviderInterceptor( const Reference< XDispatchProviderInterceptor >& _rxInterceptor )
     {
-        FORWARD_TO_PEER_1( XDispatchProviderInterception, registerDispatchProviderInterceptor, _rxInterceptor );
+        Reference< XDispatchProviderInterception > xTypedPeer(getPeer(), UNO_QUERY);
+        if (xTypedPeer.is())
+        {
+            xTypedPeer->registerDispatchProviderInterceptor(_rxInterceptor);
+        }
     }
 
 
     void SAL_CALL ONavigationBarControl::releaseDispatchProviderInterceptor( const Reference< XDispatchProviderInterceptor >& _rxInterceptor )
     {
-        FORWARD_TO_PEER_1( XDispatchProviderInterception, releaseDispatchProviderInterceptor, _rxInterceptor );
+        Reference< XDispatchProviderInterception > xTypedPeer(getPeer(), UNO_QUERY);
+        if (xTypedPeer.is())
+        {
+            xTypedPeer->registerDispatchProviderInterceptor(_rxInterceptor);
+        }
     }
 
 
     void SAL_CALL ONavigationBarControl::setDesignMode( sal_Bool _bOn )
     {
         UnoControl::setDesignMode( _bOn );
-        FORWARD_TO_PEER_1( XVclWindowPeer, setDesignMode, _bOn );
+        Reference< XVclWindowPeer > xTypedPeer(getPeer(), UNO_QUERY);
+        if (xTypedPeer.is())
+        {
+            xTypedPeer->setDesignMode(_bOn);
+        }
     }
 
 

forms/source/component/Button.cxx
@@ -115,7 +115,13 @@ void OButtonModel::describeFixedProperties( Sequence< Property >& _rProps ) cons
 }
 
 
-IMPLEMENT_DEFAULT_CLONING( OButtonModel )
+css::uno::Reference< css::util::XCloneable > SAL_CALL OButtonModel::createClone()
+{
+    rtl::Reference<OButtonModel> pClone = new OButtonModel(this, getContext());
+    pClone->clonedFrom(this);
+    return pClone;
+}
+
 
 // XServiceInfo
 

forms/source/component/Button.hxx
@@ -45,7 +45,14 @@ class OButtonModel  :public OClickableImageBaseModel
                     ,public OButtonModel_Base
 {
 public:
-    DECLARE_DEFAULT_LEAF_XTOR( OButtonModel );
+    OButtonModel(
+        const css::uno::Reference< css::uno::XComponentContext>& _rxFactory
+    );
+    OButtonModel(
+        const OButtonModel* _pOriginal,
+        const css::uno::Reference< css::uno::XComponentContext>& _rxFactory
+    );
+    virtual ~OButtonModel() override;
 
     // UNO
     DECLARE_UNO3_AGG_DEFAULTS( OButtonModel, OClickableImageBaseModel )

forms/source/component/CheckBox.cxx
@@ -78,7 +78,13 @@ OCheckBoxModel::~OCheckBoxModel()
 }
 
 
-IMPLEMENT_DEFAULT_CLONING( OCheckBoxModel )
+css::uno::Reference< css::util::XCloneable > SAL_CALL OCheckBoxModel::createClone()
+{
+    rtl::Reference<OCheckBoxModel> pClone = new OCheckBoxModel(this, getContext());
+    pClone->clonedFrom(this);
+    return pClone;
+}
+
 
 // XServiceInfo
 

forms/source/component/CheckBox.hxx
@@ -30,7 +30,14 @@ class OCheckBoxModel final : public OReferenceValueComponent
     bool        DbUseBool();
 
 public:
-    DECLARE_DEFAULT_LEAF_XTOR( OCheckBoxModel );
+    OCheckBoxModel(
+        const css::uno::Reference< css::uno::XComponentContext>& _rxFactory
+    );
+    OCheckBoxModel(
+        const OCheckBoxModel* _pOriginal,
+        const css::uno::Reference< css::uno::XComponentContext>& _rxFactory
+    );
+    virtual ~OCheckBoxModel() override;
 
     // XServiceInfo
     OUString SAL_CALL getImplementationName() override

forms/source/component/ComboBox.cxx
@@ -144,7 +144,12 @@ OComboBoxModel::~OComboBoxModel()
 
 // XCloneable
 
-IMPLEMENT_DEFAULT_CLONING( OComboBoxModel )
+css::uno::Reference< css::util::XCloneable > SAL_CALL OComboBoxModel::createClone()
+{
+    rtl::Reference<OComboBoxModel> pClone = new OComboBoxModel(this, getContext());
+    pClone->clonedFrom(this);
+    return pClone;
+}
 
 
 void OComboBoxModel::disposing()

forms/source/component/ComboBox.hxx
@@ -54,7 +54,14 @@ class OComboBoxModel final
     virtual css::uno::Sequence< css::uno::Type>   _getTypes() override;
 
 public:
-    DECLARE_DEFAULT_LEAF_XTOR( OComboBoxModel );
+    OComboBoxModel(
+        const css::uno::Reference< css::uno::XComponentContext>& _rxFactory
+    );
+    OComboBoxModel(
+        const OComboBoxModel* _pOriginal,
+        const css::uno::Reference< css::uno::XComponentContext>& _rxFactory
+    );
+    virtual ~OComboBoxModel() override;
 
     virtual void SAL_CALL disposing() override;
 

forms/source/component/Currency.cxx
@@ -130,7 +130,13 @@ OCurrencyModel::~OCurrencyModel()
 
 // XCloneable
 
-IMPLEMENT_DEFAULT_CLONING( OCurrencyModel )
+css::uno::Reference< css::util::XCloneable > SAL_CALL OCurrencyModel::createClone()
+{
+    rtl::Reference<OCurrencyModel> pClone = new OCurrencyModel(this, getContext());
+    pClone->clonedFrom(this);
+    return pClone;
+}
+
 
 // XServiceInfo
 

commit 3ab39b3fa2def1ec35fa2c48a6f7adb367016695

tdf#145731: fix crash closing document
Regression from:
https://cgit.freedesktop.org/libreoffice/core/commit/?id=73334560b2dd2d60ac58d2cc2b1a5295490b03e1
author	Julien Nabet <serval2412@yahoo.fr>	2021-11-07 15:40:37 +0100
committer	Julien Nabet <serval2412@yahoo.fr>	2021-11-07 21:58:53 +0100
commit	73334560b2dd2d60ac58d2cc2b1a5295490b03e1 (patch)
tree	b5bc4f69dd8ed455c78ea05ab1c5e2f3c25b909e
parent	6be03ac71e0d4927612b4a57ead3d0b245c29c77 (diff)
Replace some macros in forms part 16

Thank you Xisco for having spotted it!

Change-Id: Ie0eb2a7e0e9f4c4fc12b83a233553a8713d6a6f2

forms/source/solar/component/navbarcontrol.cxx
@@ -180,7 +180,7 @@ namespace frm
         Reference< XDispatchProviderInterception > xTypedPeer(getPeer(), UNO_QUERY);
         if (xTypedPeer.is())
         {
-            xTypedPeer->registerDispatchProviderInterceptor(_rxInterceptor);
+            xTypedPeer->releaseDispatchProviderInterceptor(_rxInterceptor);
         }
     }
 