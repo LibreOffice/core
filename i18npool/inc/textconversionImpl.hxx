51b1864c033675f9a5801951ef0fba983bc74402
Harjot <harjotsingh20022001@gmail.com>
2021-11-17 19:48:56 +0530

    Replaced include guards with pragma once in all files of /i18npool/inc
    Change-Id: Id67d8926fdc1616add1bd9de051911b2aae030e4

--- i18npool/inc/textconversionImpl.hxx
+++ i18npool/inc/textconversionImpl.hxx
@@ -16,8 +16,7 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
-#ifndef INCLUDED_I18NPOOL_INC_TEXTCONVERSIONIMPL_HXX
-#define INCLUDED_I18NPOOL_INC_TEXTCONVERSIONIMPL_HXX
+#pragma once
 
 #include <com/sun/star/lang/XServiceInfo.hpp>
 #include <com/sun/star/i18n/XExtendedTextConversion.hpp>
@@ -72,8 +71,5 @@ private:
 };
 
 } // i18npool
-
-
-#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
