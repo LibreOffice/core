339405818c9c31a3ba29f406afca36568c378fb2
Miklos Vajna <vmiklos@collabora.com>
2021-11-18 20:16:46 +0100

    sw: enable warning in XMLRedlineImportHelper::InsertIntoDocument()
    This one looks useful, so why hide it behind dbglevel=2.
    
    Change-Id: I424808ae8ccd2ab7edcd1a75cd7c4479a0ec3b13
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/125501
    Tested-by: Jenkins
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

--- sw/source/filter/xml/XMLRedlineImportHelper.cxx
+++ sw/source/filter/xml/XMLRedlineImportHelper.cxx
@@ -667,10 +667,8 @@ void XMLRedlineImportHelper::InsertIntoDocument(RedlineInfo* pRedlineInfo)
             if( nPoint < pRedlineInfo->pContentIndex->GetIndex() ||
                 nPoint > pRedlineInfo->pContentIndex->GetNode().EndOfSectionIndex() )
                 pRedline->SetContentIdx(pRedlineInfo->pContentIndex);
-#if OSL_DEBUG_LEVEL > 1
             else
-                OSL_FAIL( "Recursive change tracking" );
-#endif
+                SAL_WARN("sw", "Recursive change tracking");
         }
 
         // set redline mode (without doing the associated book-keeping)
