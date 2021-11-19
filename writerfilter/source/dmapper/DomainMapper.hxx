20c7e0355ed83bfb235b1b188171d7cb4c6664c9
Vasily Melenchuk <vasily.melenchuk@cib.de>
2021-11-19 15:09:31 +0300

    tdf#104823: basic support for reading field data from databinding
    Change-Id: Ie45eb18205c1c54a631303b45887e54e456b6d5d
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/125550
    Tested-by: Jenkins
    Reviewed-by: Vasily Melenchuk <vasily.melenchuk@cib.de>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/127129
    Tested-by: Thorsten Behrens <thorsten.behrens@allotropia.de>
    Reviewed-by: Thorsten Behrens <thorsten.behrens@allotropia.de>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/130042
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

--- writerfilter/source/dmapper/DomainMapper.hxx
+++ writerfilter/source/dmapper/DomainMapper.hxx
@@ -81,6 +81,8 @@ public:
                  utl::MediaDescriptor const & rMediaDesc);
     virtual ~DomainMapper() override;
 
+    virtual void setDocumentReference(void* pDocument) override;
+
     // Stream
     virtual void markLastParagraphInSection() override;
     virtual void markLastSectionGroup() override;
--- writerfilter/source/dmapper/DomainMapper_Impl.hxx
+++ writerfilter/source/dmapper/DomainMapper_Impl.hxx
@@ -75,6 +75,11 @@ namespace com{ namespace sun{ namespace star{
 }}}
 
 namespace writerfilter {
+
+namespace ooxml {
+    class OOXMLDocument;
+}
+
 namespace dmapper {
 
 class SdtHelper;
@@ -436,6 +441,7 @@ public:
 private:
     SourceDocumentType const                                                        m_eDocumentType;
     DomainMapper&                                                                   m_rDMapper;
+    void* m_pOOXMLDocument;
     OUString m_aBaseUrl;
     css::uno::Reference<css::text::XTextDocument> m_xTextDocument;
     css::uno::Reference<css::beans::XPropertySet> m_xDocumentSettings;
@@ -601,6 +607,9 @@ public:
             SourceDocumentType eDocumentType,
             utl::MediaDescriptor const & rMediaDesc);
     ~DomainMapper_Impl();
+
+    void setDocumentReference(void* pDocument) { m_pOOXMLDocument = pDocument; };
+    writerfilter::ooxml::OOXMLDocument* getDocumentReference() const;
 
     SectionPropertyMap* GetLastSectionContext( )
     {
