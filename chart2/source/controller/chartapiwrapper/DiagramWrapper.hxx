2958109df0d2901b59748c3028e5df7a2c5b2481
Noel Grandin <noelgrandin@gmail.com>
2021-11-22 21:01:01 +0200

    use more OInterfaceContainerHelper3 in chart2
    Change-Id: Icff667a11f1798e29c732c13b5739a4f7b581999
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/125921
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

--- chart2/source/controller/chartapiwrapper/DiagramWrapper.hxx
+++ chart2/source/controller/chartapiwrapper/DiagramWrapper.hxx
@@ -20,7 +20,7 @@
 
 #include <WrappedPropertySet.hxx>
 
-#include <comphelper/interfacecontainer2.hxx>
+#include <comphelper/interfacecontainer3.hxx>
 #include <com/sun/star/chart/XDiagramPositioning.hpp>
 #include <com/sun/star/chart2/XDiagramProvider.hpp>
 #include <com/sun/star/chart/XDiagram.hpp>
@@ -186,7 +186,7 @@ protected:
 
 private:
     std::shared_ptr< Chart2ModelContact >   m_spChart2ModelContact;
-    ::comphelper::OInterfaceContainerHelper2           m_aEventListenerContainer;
+    ::comphelper::OInterfaceContainerHelper3<css::lang::XEventListener> m_aEventListenerContainer;
 
     css::uno::Reference<
         css::chart::XAxis >        m_xXAxis;
