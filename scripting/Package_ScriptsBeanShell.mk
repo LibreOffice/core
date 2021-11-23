43f32f05cbefbfa6a157798e40a24502bdb3e038
Samuel Mehrbrodt <samuel.mehrbrodt@allotropia.de>
2021-11-23 09:43:35 +0100

    Add more beanshell samples
    For Writer & Calc
    
    Change-Id: I85815acf11b750ec76d138ff8fe72cc4791e9038

--- scripting/Package_ScriptsBeanShell.mk
+++ scripting/Package_ScriptsBeanShell.mk
@@ -10,6 +10,12 @@
 $(eval $(call gb_Package_Package,scripting_ScriptsBeanShell,$(SRCDIR)/scripting/examples))
 
 $(eval $(call gb_Package_add_files_with_dir,scripting_ScriptsBeanShell,$(LIBO_SHARE_FOLDER)/Scripts,\
+	beanshell/Calc/CopyRange.bsh \
+	beanshell/Calc/FixView.bsh \
+	beanshell/Calc/InsertSheet.bsh \
+	beanshell/Calc/parcel-descriptor.xml \
+	beanshell/Calc/ProtectSheet.bsh \
+	beanshell/Calc/SelectCell.bsh \
 	beanshell/Capitalise/capitalise.bsh \
 	beanshell/Capitalise/parcel-descriptor.xml \
 	beanshell/HelloWorld/helloworld.bsh \
@@ -22,6 +28,12 @@ $(eval $(call gb_Package_add_files_with_dir,scripting_ScriptsBeanShell,$(LIBO_SH
 	beanshell/MemoryUsage/parcel-descriptor.xml \
 	beanshell/WordCount/parcel-descriptor.xml \
 	beanshell/WordCount/wordcount.bsh \
+	beanshell/Writer/ChangeFont.bsh \
+	beanshell/Writer/ChangeParaAdjust.bsh \
+	beanshell/Writer/InsertTable.bsh \
+	beanshell/Writer/InsertText.bsh \
+	beanshell/Writer/parcel-descriptor.xml \
+	beanshell/Writer/SetText.bsh \
 ))
 
 # vim: set noet sw=4 ts=4:
