a884f0596cf4d519f2e8e75737a108d134185f9b
Stephan Bergmann <sbergman@redhat.com>
2021-11-23 12:18:23 +0100

    Use proper type for OUString length variables
    Change-Id: Id116ed42c553bb6524876288ca0066be4a4a1820
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/125694
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>
    Tested-by: Jenkins

--- tools/source/fsys/urlobj.cxx
+++ tools/source/fsys/urlobj.cxx
@@ -1476,7 +1476,7 @@ bool INetURLObject::setAbsURIRef(OUString const & rTheAbsURIRef,
 }
 
 void INetURLObject::changeScheme(INetProtocol eTargetScheme) {
-    int oldSchemeLen = 0;
+    sal_Int32 oldSchemeLen = 0;
     const OUString& rOldSchemeName = getSchemeInfo().m_sScheme;
     if (m_eScheme == INetProtocol::Generic)
         oldSchemeLen = m_aScheme.getLength();
@@ -1484,10 +1484,10 @@ void INetURLObject::changeScheme(INetProtocol eTargetScheme) {
         oldSchemeLen = rOldSchemeName.getLength();
     m_eScheme=eTargetScheme;
     const OUString& rNewSchemeName = getSchemeInfo().m_sScheme;
-    int newSchemeLen = rNewSchemeName.getLength();
+    sal_Int32 newSchemeLen = rNewSchemeName.getLength();
     m_aAbsURIRef.remove(0, oldSchemeLen);
     m_aAbsURIRef.insert(0, rNewSchemeName);
-    int delta=newSchemeLen-oldSchemeLen;
+    sal_Int32 delta=newSchemeLen-oldSchemeLen;
     m_aUser+=delta;
     m_aAuth+=delta;
     m_aHost+=delta;
