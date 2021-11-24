LibreOffice/core

commit 4bf04dea9afb30a9395e80b07a81d1908937ee8b

tdf#128106 sw: copy bookmarks at start if whole node is copied
The CrossRefHeadingBookmarks are always at index 0 so copy them if the
selection also starts at index 0.

This happens in SwDoc::SplitDoc() for the 2nd etc. chapter.

Change-Id: I4765d497e65d3c8257f7eab59b8ba2088688967e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/121147
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/core/doc/DocumentContentOperationsManager.cxx
@@ -251,10 +251,21 @@ namespace sw
             const SwPosition& rMarkEnd = pMark->GetMarkEnd();
             // only include marks that are in the range and not touching both start and end
             // - not for annotation or checkbox marks.
+            bool const isIncludeStart(
+                   (rStt.nContent.GetIndex() == 0 // paragraph start selected?
+                    // also: only if inserting at the start - cross reference
+                    // marks require index to be 0, and there could be one
+                    // on the target node already
+                    && rCpyPam.nContent.GetIndex() == 0)
+                || rMarkStart != rStt);
+            bool const isIncludeEnd(
+                   (rEnd.nNode.GetNode().IsTextNode() // paragraph end selected?
+                    && rEnd.nContent.GetIndex() == rEnd.nNode.GetNode().GetTextNode()->Len())
+                || rMarkEnd != rEnd);
             const bool bIsNotOnBoundary =
                 pMark->IsExpanded()
-                ? (rMarkStart != rStt || rMarkEnd != rEnd)  // rMarkStart != rMarkEnd
-                : (rMarkStart != rStt && rMarkEnd != rEnd); // rMarkStart == rMarkEnd
+                ? (isIncludeStart || isIncludeEnd)  // rMarkStart != rMarkEnd
+                : (isIncludeStart && isIncludeEnd); // rMarkStart == rMarkEnd
             const IDocumentMarkAccess::MarkType aMarkType = IDocumentMarkAccess::GetType(*pMark);
             if ( rMarkStart >= rStt && rMarkEnd <= rEnd
                  && ( bIsNotOnBoundary

sw/qa/extras/uiwriter/uiwriter3.cxx
@@ -2664,7 +2664,8 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest3, testTdf128106)
     CPPUNIT_ASSERT_EQUAL(sal_uInt16(REF_BOOKMARK), fields[1]->GetField()->GetSubType());
     CPPUNIT_ASSERT(
         static_cast<SwGetRefField const*>(fields[1]->GetField())->IsRefToHeadingCrossRefBookmark());
-    //    CPPUNIT_ASSERT_EQUAL(OUString("Chapter 2"), static_cast<SwGetRefField const*>(fields[1]->GetField())->GetPar2());
+    CPPUNIT_ASSERT_EQUAL(OUString("Chapter 2"),
+                         static_cast<SwGetRefField const*>(fields[1]->GetField())->GetPar2());
     CPPUNIT_ASSERT_EQUAL(sal_uInt16(REF_BOOKMARK), fields[2]->GetField()->GetSubType());
     CPPUNIT_ASSERT_EQUAL(OUString("Bookmarkchapter1"),
                          static_cast<SwGetRefField const*>(fields[2]->GetField())->GetSetRefName());
@@ -2683,7 +2684,8 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest3, testTdf128106)
     CPPUNIT_ASSERT_EQUAL(sal_uInt16(REF_BOOKMARK), fields[5]->GetField()->GetSubType());
     CPPUNIT_ASSERT(
         static_cast<SwGetRefField const*>(fields[5]->GetField())->IsRefToHeadingCrossRefBookmark());
-    //    CPPUNIT_ASSERT_EQUAL(OUString("Chapter 2"), static_cast<SwGetRefField const*>(fields[5]->GetField())->GetPar2());
+    CPPUNIT_ASSERT_EQUAL(OUString("Chapter 2"),
+                         static_cast<SwGetRefField const*>(fields[5]->GetField())->GetPar2());
 
     tempDir.EnableKillingFile();
 }

commit e33df0c61bcf120982098c7656ba637fe016ad9d

tdf#145621 sw: fix crash pasting paragraph with cross-reference mark
At last, a case where copying a mark fails for legitimate reasons.

(regression from 4bf04dea9afb30a9395e80b07a81d1908937ee8b)

Change-Id: I7a318e03c0b0d30e204eadb5012f190edbbba486

sw/source/core/doc/DocumentContentOperationsManager.cxx
@@ -300,6 +300,12 @@ namespace sw
                 ::sw::mark::InsertMode::CopyText);
             // Explicitly try to get exactly the same name as in the source
             // because NavigatorReminders, DdeBookmarks etc. ignore the proposed name
+            if (pNewMark == nullptr)
+            {
+                assert(IDocumentMarkAccess::GetType(*pMark) == IDocumentMarkAccess::MarkType::CROSSREF_NUMITEM_BOOKMARK
+                    || IDocumentMarkAccess::GetType(*pMark) == IDocumentMarkAccess::MarkType::CROSSREF_HEADING_BOOKMARK);
+                continue; // can't insert duplicate cross reference mark
+            }
             rDestDoc.getIDocumentMarkAccess()->renameMark(pNewMark, pMark->GetName());
 
             // copying additional attributes for bookmarks or fieldmarks