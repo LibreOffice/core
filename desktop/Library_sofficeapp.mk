bd9d83216d8a8478cc6084c63c7c9fd157219d87
Szymon Kłos <szymon.klos@collabora.com>
2021-11-26 17:05:41 +0100

    Make LOKClipboard available to other modules
    LOKClipboard has higher level dependencies (sfx2) so it cannot be
    moved to the vcl where SystemClibpoard instance is created.
    
    - introduce new interface LokClipboard to differentiate
      from SystemClipboard so we can have vcl's and lok's implementations
      at the same time
    - publish LOKClipboard using new interface for other modules by adding
      component file in desktop module
    
    Thanks to that when code calls GetClipboard and we cannot get clipboard
    assigned to the vcl::Window* (for welded widgets) in GetSystemClipboard
    correct instance is returned (shared clipboard for current view) so we
    can access content which was copied before. Previously always a new
    instance was created (with empty content).
    
    test ScTiledRenderingTest::testPasteIntoWrapTextCell was broken
    add some content to clipboard to simulate more real case
    and test the content copied
    
    Change-Id: I23c0298960a710c498646493f33122b908864cba
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/126310
    Reviewed-by: Luboš Luňák <l.lunak@collabora.com>
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/131644
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>
    Tested-by: Szymon Kłos <szymon.klos@collabora.com>

--- desktop/Library_sofficeapp.mk
+++ desktop/Library_sofficeapp.mk
@@ -136,6 +136,8 @@ $(eval $(call gb_Library_add_exception_objects,sofficeapp,\
 	$(if $(filter $(OS),ANDROID), \
 		desktop/source/lib/lokandroid) \
 ))
+$(if $(filter-out $(OS),IOS), \
+    $(eval $(call gb_Library_set_componentfile,sofficeapp,desktop/lokclipboard,services)))
 else
 ifeq ($(USING_X11),TRUE)
 $(eval $(call gb_Library_add_exception_objects,sofficeapp,\
@@ -143,6 +145,7 @@ $(eval $(call gb_Library_add_exception_objects,sofficeapp,\
 	desktop/source/lib/lokinteractionhandler \
 	desktop/source/lib/lokclipboard \
 ))
+$(eval $(call gb_Library_set_componentfile,sofficeapp,desktop/lokclipboard,services))
 endif
 ifeq ($(DISABLE_GUI),TRUE)
 $(eval $(call gb_Library_add_exception_objects,sofficeapp,\
@@ -150,6 +153,7 @@ $(eval $(call gb_Library_add_exception_objects,sofficeapp,\
     desktop/source/lib/lokinteractionhandler \
     desktop/source/lib/lokclipboard \
 ))
+$(eval $(call gb_Library_set_componentfile,sofficeapp,desktop/lokclipboard,services))
 endif
 endif
 
