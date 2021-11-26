LibreOffice/core

commit b9993369b4011cd8f367e439f5248096827f1efc

tdf#121715 XLSX: support custom first page header/footer
Add XLSX import/export support for a different header or
footer (activated separately) on the first page.
Print preview is also extended to support this.

Note: only ODS export is supported, yet. Follow-up commits
are going to add ODS import and UI support.

Change-Id: Icd3a40131bdbcd5d5a42f98c86a71345a5745051
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/116842
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>
(cherry picked from commit 19fa853ce12136b5c14e0c5a0aa906c296b75388)
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/118067
Tested-by: Jenkins

sc/source/filter/xml/XMLTableHeaderFooterContext.cxx
@@ -41,7 +41,7 @@ XMLTableHeaderFooterContext::XMLTableHeaderFooterContext( SvXMLImport& rImport, 
                        const uno::Reference<
                             xml::sax::XFastAttributeList > & xAttrList,
                        const Reference < XPropertySet > & rPageStylePropSet,
-                       bool bFooter, bool bLeft ) :
+                       bool bFooter, bool bLeft, bool bFirst ) :
     SvXMLImportContext( rImport ),
     xPropSet( rPageStylePropSet ),
     bContainsLeft(false),
@@ -51,6 +51,7 @@ XMLTableHeaderFooterContext::XMLTableHeaderFooterContext( SvXMLImport& rImport, 
     OUString sOn( bFooter ? OUString(SC_UNO_PAGE_FTRON) : OUString(SC_UNO_PAGE_HDRON) );
     OUString sContent( bFooter ? OUString(SC_UNO_PAGE_RIGHTFTRCON) : OUString(SC_UNO_PAGE_RIGHTHDRCON) );
     OUString sContentLeft( bFooter ? OUString(SC_UNO_PAGE_LEFTFTRCONT) : OUString(SC_UNO_PAGE_LEFTHDRCONT) );
+    OUString sContentFirst( bFooter ? OUString(SC_UNO_PAGE_FIRSTFTRCONT) : OUString(SC_UNO_PAGE_FIRSTHDRCONT) );
     OUString sShareContent( bFooter ? OUString(SC_UNO_PAGE_FTRSHARED) : OUString(SC_UNO_PAGE_HDRSHARED) );
     bool bDisplay( true );
     for( auto &aIter : sax_fastparser::castToFastAttributeList( xAttrList ) )
@@ -85,6 +86,8 @@ XMLTableHeaderFooterContext::XMLTableHeaderFooterContext( SvXMLImport& rImport, 
     }
     if (bLeft)
         sCont = sContentLeft;
+    else if (bFirst)
+        sCont = sContentFirst;
     else
         sCont = sContent;
     xPropSet->getPropertyValue( sCont ) >>= xHeaderFooterContent;

reportdesign/source/core/api/ReportDefinition.cxx
@@ -149,6 +149,7 @@
 #define SC_UNO_PAGE_HDRON           "HeaderIsOn"
 #define SC_UNO_PAGE_HDRDYNAMIC      "HeaderIsDynamicHeight"
 #define SC_UNO_PAGE_HDRSHARED       "HeaderIsShared"
+#define SC_UNO_PAGE_FIRSTHDRSHARED  "FirstPageHeaderIsShared"
 #define SC_UNO_PAGE_FTRBACKCOL      "FooterBackColor"
 #define SC_UNO_PAGE_FTRBACKTRAN     "FooterBackTransparent"
 #define SC_UNO_PAGE_FTRGRFFILT      "FooterBackGraphicFilter"
@@ -171,6 +172,7 @@
 #define SC_UNO_PAGE_FTRON           "FooterIsOn"
 #define SC_UNO_PAGE_FTRDYNAMIC      "FooterIsDynamicHeight"
 #define SC_UNO_PAGE_FTRSHARED       "FooterIsShared"
+#define SC_UNO_PAGE_FIRSTFTRSHARED  "FirstPageFooterIsShared"
 
 namespace reportdesign
 {
@@ -309,6 +311,7 @@ OStyle::OStyle()
     registerPropertyNoMember(SC_UNO_PAGE_FTRDYNAMIC,  ++i,nBound,cppu::UnoType<bool>::get(), css::uno::Any(false));
     registerPropertyNoMember(SC_UNO_PAGE_FTRON,       ++i,nBound,cppu::UnoType<bool>::get(), css::uno::Any(false));
     registerPropertyNoMember(SC_UNO_PAGE_FTRSHARED,   ++i,nBound,cppu::UnoType<bool>::get(), css::uno::Any(false));
+    registerPropertyNoMember(SC_UNO_PAGE_FIRSTFTRSHARED, ++i,nBound,cppu::UnoType<bool>::get(), css::uno::Any(false));
     registerPropertyNoMember(SC_UNO_PAGE_FTRLEFTBOR,  ++i,nBound, cppu::UnoType<table::BorderLine2>::get(), css::uno::Any(table::BorderLine2()));
     registerPropertyNoMember(SC_UNO_PAGE_FTRLEFTBDIS, ++i,nBound, cppu::UnoType<sal_Int32>::get(), css::uno::makeAny<sal_Int32>(0));
     registerPropertyNoMember(SC_UNO_PAGE_FTRLEFTMAR,  ++i,nBound, cppu::UnoType<sal_Int32>::get(), css::uno::makeAny<sal_Int32>(0));
@@ -332,6 +335,7 @@ OStyle::OStyle()
     registerPropertyNoMember(SC_UNO_PAGE_HDRDYNAMIC,  ++i,nBound|nMayBeVoid,cppu::UnoType<bool>::get(), css::uno::Any(false));
     registerPropertyNoMember(SC_UNO_PAGE_HDRON,       ++i,nBound|nMayBeVoid,cppu::UnoType<bool>::get(), css::uno::Any(false));
     registerPropertyNoMember(SC_UNO_PAGE_HDRSHARED,   ++i,nBound|nMayBeVoid,cppu::UnoType<bool>::get(), css::uno::Any(false));
+    registerPropertyNoMember(SC_UNO_PAGE_FIRSTHDRSHARED, ++i,nBound|nMayBeVoid,cppu::UnoType<bool>::get(), css::uno::Any(false));
     registerPropertyNoMember(SC_UNO_PAGE_HDRLEFTBOR,  ++i,nBound|nMayBeVoid, cppu::UnoType<table::BorderLine2>::get(), css::uno::Any(table::BorderLine2()));
     registerPropertyNoMember(SC_UNO_PAGE_HDRLEFTBDIS, ++i,nBound|nMayBeVoid, cppu::UnoType<sal_Int32>::get(), css::uno::makeAny<sal_Int32>(0));
     registerPropertyNoMember(SC_UNO_PAGE_HDRLEFTMAR,  ++i,nBound|nMayBeVoid, cppu::UnoType<sal_Int32>::get(), css::uno::makeAny<sal_Int32>(0));

sc/inc/scitems.hxx
@@ -175,31 +175,34 @@ constexpr TypedWhichId<SfxBoolItem> ATTR_PAGE_VERCENTER (163);
 constexpr TypedWhichId<SfxBoolItem> ATTR_PAGE_ON (164);     // editor: header/footer-page
 constexpr TypedWhichId<SfxBoolItem> ATTR_PAGE_DYNAMIC (165);
 constexpr TypedWhichId<SfxBoolItem> ATTR_PAGE_SHARED (166);
+constexpr TypedWhichId<SfxBoolItem> ATTR_PAGE_SHARED_FIRST (167);
 
-constexpr TypedWhichId<SfxBoolItem> ATTR_PAGE_NOTES (167);     // editor: table
-constexpr TypedWhichId<SfxBoolItem> ATTR_PAGE_GRID (168);
-constexpr TypedWhichId<SfxBoolItem> ATTR_PAGE_HEADERS (169);
-constexpr TypedWhichId<ScViewObjectModeItem> ATTR_PAGE_CHARTS (170);
-constexpr TypedWhichId<ScViewObjectModeItem> ATTR_PAGE_OBJECTS (171);
-constexpr TypedWhichId<ScViewObjectModeItem> ATTR_PAGE_DRAWINGS (172);
-constexpr TypedWhichId<SfxBoolItem> ATTR_PAGE_TOPDOWN (173);
-constexpr TypedWhichId<SfxUInt16Item> ATTR_PAGE_SCALE (174);
-constexpr TypedWhichId<SfxUInt16Item> ATTR_PAGE_SCALETOPAGES (175);
-constexpr TypedWhichId<SfxUInt16Item> ATTR_PAGE_FIRSTPAGENO (176);
+constexpr TypedWhichId<SfxBoolItem> ATTR_PAGE_NOTES (168);     // editor: table
+constexpr TypedWhichId<SfxBoolItem> ATTR_PAGE_GRID (169);
+constexpr TypedWhichId<SfxBoolItem> ATTR_PAGE_HEADERS (170);
+constexpr TypedWhichId<ScViewObjectModeItem> ATTR_PAGE_CHARTS (171);
+constexpr TypedWhichId<ScViewObjectModeItem> ATTR_PAGE_OBJECTS (172);
+constexpr TypedWhichId<ScViewObjectModeItem> ATTR_PAGE_DRAWINGS (173);
+constexpr TypedWhichId<SfxBoolItem> ATTR_PAGE_TOPDOWN (174);
+constexpr TypedWhichId<SfxUInt16Item> ATTR_PAGE_SCALE (175);
+constexpr TypedWhichId<SfxUInt16Item> ATTR_PAGE_SCALETOPAGES (176);
+constexpr TypedWhichId<SfxUInt16Item> ATTR_PAGE_FIRSTPAGENO (177);
 
-constexpr TypedWhichId<ScPageHFItem> ATTR_PAGE_HEADERLEFT (177);     // contents of header/
-constexpr TypedWhichId<ScPageHFItem> ATTR_PAGE_FOOTERLEFT (178);     // footer (left)
-constexpr TypedWhichId<ScPageHFItem> ATTR_PAGE_HEADERRIGHT (179);     // contents of header/
-constexpr TypedWhichId<ScPageHFItem> ATTR_PAGE_FOOTERRIGHT (180);     // footer (right)
-constexpr TypedWhichId<SvxSetItem> ATTR_PAGE_HEADERSET (181);     // the corresponding sets
-constexpr TypedWhichId<SvxSetItem> ATTR_PAGE_FOOTERSET (182);
+constexpr TypedWhichId<ScPageHFItem> ATTR_PAGE_HEADERLEFT (178);     // contents of header/
+constexpr TypedWhichId<ScPageHFItem> ATTR_PAGE_FOOTERLEFT (179);     // footer (left)
+constexpr TypedWhichId<ScPageHFItem> ATTR_PAGE_HEADERRIGHT (180);    // contents of header/
+constexpr TypedWhichId<ScPageHFItem> ATTR_PAGE_FOOTERRIGHT (181);    // footer (right)
+constexpr TypedWhichId<ScPageHFItem> ATTR_PAGE_HEADERFIRST (182);    // contents of header/
+constexpr TypedWhichId<ScPageHFItem> ATTR_PAGE_FOOTERFIRST (183);    // footer (first page)
+constexpr TypedWhichId<SvxSetItem> ATTR_PAGE_HEADERSET (184);     // the corresponding sets
+constexpr TypedWhichId<SvxSetItem> ATTR_PAGE_FOOTERSET (185);
 
-constexpr TypedWhichId<SfxBoolItem> ATTR_PAGE_FORMULAS (183);
-constexpr TypedWhichId<SfxBoolItem> ATTR_PAGE_NULLVALS (184);
+constexpr TypedWhichId<SfxBoolItem> ATTR_PAGE_FORMULAS (186);
+constexpr TypedWhichId<SfxBoolItem> ATTR_PAGE_NULLVALS (187);
 
-constexpr TypedWhichId<ScPageScaleToItem> ATTR_PAGE_SCALETO (185);     // #i8868# scale printout to width/height
+constexpr TypedWhichId<ScPageScaleToItem> ATTR_PAGE_SCALETO (188);     // #i8868# scale printout to width/height
 
-constexpr TypedWhichId<SfxBoolItem> ATTR_HIDDEN (186);
+constexpr TypedWhichId<SfxBoolItem> ATTR_HIDDEN (189);
 
 constexpr sal_uInt16 ATTR_ENDINDEX(ATTR_HIDDEN);        // end of pool-range
 
@@ -219,6 +222,8 @@ constexpr sal_uInt16 ATTR_ENDINDEX(ATTR_HIDDEN);        // end of pool-range
 #define SID_SCATTR_PAGE_FOOTERLEFT      ATTR_PAGE_FOOTERLEFT
 #define SID_SCATTR_PAGE_HEADERRIGHT     ATTR_PAGE_HEADERRIGHT
 #define SID_SCATTR_PAGE_FOOTERRIGHT     ATTR_PAGE_FOOTERRIGHT
+#define SID_SCATTR_PAGE_HEADERFIRST     ATTR_PAGE_HEADERFIRST
+#define SID_SCATTR_PAGE_FOOTERFIRST     ATTR_PAGE_FOOTERFIRST
 #define SID_SCATTR_PAGE_FORMULAS        ATTR_PAGE_FORMULAS
 #define SID_SCATTR_PAGE_NULLVALS        ATTR_PAGE_NULLVALS
 #define SID_SCATTR_PAGE_SCALETO         ATTR_PAGE_SCALETO

sc/inc/unonames.hxx
@@ -441,10 +441,14 @@
 #define SC_UNO_PAGE_SCALETOX        "ScaleToPagesX"
 #define SC_UNO_PAGE_SCALETOY        "ScaleToPagesY"
 #define SC_UNO_PAGE_FIRSTPAGE       "FirstPageNumber"
+#define SC_UNO_PAGE_FIRSTHDRSHARED  "FirstPageHeaderIsShared"
+#define SC_UNO_PAGE_FIRSTFTRSHARED  "FirstPageFooterIsShared"
 #define SC_UNO_PAGE_LEFTHDRCONT     "LeftPageHeaderContent"
 #define SC_UNO_PAGE_LEFTFTRCONT     "LeftPageFooterContent"
 #define SC_UNO_PAGE_RIGHTHDRCON     "RightPageHeaderContent"
 #define SC_UNO_PAGE_RIGHTFTRCON     "RightPageFooterContent"
+#define SC_UNO_PAGE_FIRSTHDRCONT    "FirstPageHeaderContent"
+#define SC_UNO_PAGE_FIRSTFTRCONT    "FirstPageFooterContent"
 #define SC_UNO_PAGE_PRINTFORMUL     "PrintFormulas"
 #define SC_UNO_PAGE_PRINTZERO       "PrintZeroValues"
 #define SC_UNO_PAGE_HDRBACKCOL      "HeaderBackColor"

sc/qa/unit/subsequent_export-test2.cxx
@@ -163,6 +163,7 @@ public:
     void testTdf81470();
     void testTdf122331();
     void testTdf83779();
+    void testTdf121715_FirstPageHeaderFooterXLSX();
     void testTdf121716_ExportEvenHeaderFooterXLSX();
     void testTdf134459_HeaderFooterColorXLSX();
     void testTdf134817_HeaderFooterTextWith2SectionXLSX();
@@ -256,6 +257,7 @@ public:
     CPPUNIT_TEST(testTdf81470);
     CPPUNIT_TEST(testTdf122331);
     CPPUNIT_TEST(testTdf83779);
+    CPPUNIT_TEST(testTdf121715_FirstPageHeaderFooterXLSX);
     CPPUNIT_TEST(testTdf121716_ExportEvenHeaderFooterXLSX);
     CPPUNIT_TEST(testTdf134459_HeaderFooterColorXLSX);
     CPPUNIT_TEST(testTdf134817_HeaderFooterTextWith2SectionXLSX);
@@ -1675,6 +1677,25 @@ void ScExportTest2::testTdf83779()
 
     assertXPathContent(pVmlDrawing, "/x:worksheet/x:sheetData/x:row[1]/x:c/x:f", "FALSE()");
     assertXPathContent(pVmlDrawing, "/x:worksheet/x:sheetData/x:row[2]/x:c/x:f", "TRUE()");
+
+    xShell->DoClose();
+}
+
+void ScExportTest2::testTdf121715_FirstPageHeaderFooterXLSX()
+{
+    // Check if first page header and footer are exported properly
+    ScDocShellRef xShell = loadDoc(u"tdf121715.", FORMAT_XLSX);
+    CPPUNIT_ASSERT(xShell.is());
+
+    std::shared_ptr<utl::TempFile> pXPathFile
+        = ScBootstrapFixture::exportTo(&(*xShell), FORMAT_XLSX);
+    xmlDocUniquePtr pDoc
+        = XPathHelper::parseExport(pXPathFile, m_xSFactory, "xl/worksheets/sheet1.xml");
+    CPPUNIT_ASSERT(pDoc);
+
+    assertXPath(pDoc, "/x:worksheet/x:headerFooter", "differentFirst", "true");
+    assertXPathContent(pDoc, "/x:worksheet/x:headerFooter/x:firstHeader", "&CFirst Page Header");
+    assertXPathContent(pDoc, "/x:worksheet/x:headerFooter/x:firstFooter", "&CFirst Page Footer");
 
     xShell->DoClose();
 }

sc/source/core/data/docpool.cxx
@@ -157,6 +157,7 @@ SfxItemInfo const  aItemInfos[] =
     { SID_ATTR_PAGE_ON,             true },    // ATTR_PAGE_ON
     { SID_ATTR_PAGE_DYNAMIC,        true },    // ATTR_PAGE_DYNAMIC
     { SID_ATTR_PAGE_SHARED,         true },    // ATTR_PAGE_SHARED
+    { SID_ATTR_PAGE_SHARED_FIRST,   true },    // ATTR_PAGE_SHARED_FIRST
     { SID_SCATTR_PAGE_NOTES,        true },    // ATTR_PAGE_NOTES
     { SID_SCATTR_PAGE_GRID,         true },    // ATTR_PAGE_GRID
     { SID_SCATTR_PAGE_HEADERS,      true },    // ATTR_PAGE_HEADERS
@@ -171,6 +172,8 @@ SfxItemInfo const  aItemInfos[] =
     { SID_SCATTR_PAGE_FOOTERLEFT,   true },    // ATTR_PAGE_FOOTERLEFT
     { SID_SCATTR_PAGE_HEADERRIGHT,  true },    // ATTR_PAGE_HEADERRIGHT
     { SID_SCATTR_PAGE_FOOTERRIGHT,  true },    // ATTR_PAGE_FOOTERRIGHT
+    { SID_SCATTR_PAGE_HEADERFIRST,  true },    // ATTR_PAGE_HEADERFIRST
+    { SID_SCATTR_PAGE_FOOTERFIRST,  true },    // ATTR_PAGE_FOOTERFIRST
     { SID_ATTR_PAGE_HEADERSET,      true },    // ATTR_PAGE_HEADERSET
     { SID_ATTR_PAGE_FOOTERSET,      true },    // ATTR_PAGE_FOOTERSET
     { SID_SCATTR_PAGE_FORMULAS,     true },    // ATTR_PAGE_FORMULAS
@@ -206,7 +209,7 @@ ScDocumentPool::ScDocumentPool()
                                      ATTR_BORDER,     ATTR_SHADOW,
                                      ATTR_LRSPACE,    ATTR_ULSPACE,
                                      ATTR_PAGE_SIZE,  ATTR_PAGE_SIZE,
-                                     ATTR_PAGE_ON,    ATTR_PAGE_SHARED>{} );
+                                     ATTR_PAGE_ON,    ATTR_PAGE_SHARED_FIRST>{} );
 
     pGlobalBorderInnerAttr->SetLine(nullptr, SvxBoxInfoItemLine::HORI);
     pGlobalBorderInnerAttr->SetLine(nullptr, SvxBoxInfoItemLine::VERT);
@@ -292,6 +295,7 @@ ScDocumentPool::ScDocumentPool()
     mvPoolDefaults[ ATTR_PAGE_ON         - ATTR_STARTINDEX ] = new SfxBoolItem( ATTR_PAGE_ON, true );
     mvPoolDefaults[ ATTR_PAGE_DYNAMIC    - ATTR_STARTINDEX ] = new SfxBoolItem( ATTR_PAGE_DYNAMIC, true );
     mvPoolDefaults[ ATTR_PAGE_SHARED     - ATTR_STARTINDEX ] = new SfxBoolItem( ATTR_PAGE_SHARED, true );
+    mvPoolDefaults[ ATTR_PAGE_SHARED_FIRST- ATTR_STARTINDEX ] = new SfxBoolItem( ATTR_PAGE_SHARED_FIRST, true );
     mvPoolDefaults[ ATTR_PAGE_NOTES      - ATTR_STARTINDEX ] = new SfxBoolItem( ATTR_PAGE_NOTES, false );
     mvPoolDefaults[ ATTR_PAGE_GRID       - ATTR_STARTINDEX ] = new SfxBoolItem( ATTR_PAGE_GRID, false );
     mvPoolDefaults[ ATTR_PAGE_HEADERS    - ATTR_STARTINDEX ] = new SfxBoolItem( ATTR_PAGE_HEADERS, false );
@@ -306,6 +310,8 @@ ScDocumentPool::ScDocumentPool()
     mvPoolDefaults[ ATTR_PAGE_FOOTERLEFT - ATTR_STARTINDEX ] = new ScPageHFItem( ATTR_PAGE_FOOTERLEFT );
     mvPoolDefaults[ ATTR_PAGE_HEADERRIGHT- ATTR_STARTINDEX ] = new ScPageHFItem( ATTR_PAGE_HEADERRIGHT );
     mvPoolDefaults[ ATTR_PAGE_FOOTERRIGHT- ATTR_STARTINDEX ] = new ScPageHFItem( ATTR_PAGE_FOOTERRIGHT );
+    mvPoolDefaults[ ATTR_PAGE_HEADERFIRST- ATTR_STARTINDEX ] = new ScPageHFItem( ATTR_PAGE_HEADERFIRST );
+    mvPoolDefaults[ ATTR_PAGE_FOOTERFIRST- ATTR_STARTINDEX ] = new ScPageHFItem( ATTR_PAGE_FOOTERFIRST );
     mvPoolDefaults[ ATTR_PAGE_HEADERSET  - ATTR_STARTINDEX ] = new SvxSetItem( ATTR_PAGE_HEADERSET, aSetItemItemSet );
     mvPoolDefaults[ ATTR_PAGE_FOOTERSET  - ATTR_STARTINDEX ] = new SvxSetItem( ATTR_PAGE_FOOTERSET, aSetItemItemSet );
     mvPoolDefaults[ ATTR_PAGE_FORMULAS   - ATTR_STARTINDEX ] = new SfxBoolItem( ATTR_PAGE_FORMULAS, false );
@@ -412,6 +418,7 @@ static bool lcl_HFPresentation
             case ATTR_PAGE_ON:
             case ATTR_PAGE_DYNAMIC:
             case ATTR_PAGE_SHARED:
+            case ATTR_PAGE_SHARED_FIRST:
             break;
 
             case ATTR_LRSPACE:

sc/source/filter/excel/xepage.cxx
@@ -60,11 +60,13 @@ void XclExpHeaderFooter::SaveXml( XclExpXmlStream& rStrm )
     sax_fastparser::FSHelperPtr& rWorksheet = rStrm.GetCurrentStream();
     sal_Int32 nElement;
     switch(GetRecId()) {
-        case EXC_ID_HEADER_EVEN: nElement = XML_evenHeader; break;
-        case EXC_ID_FOOTER_EVEN: nElement = XML_evenFooter; break;
-        case EXC_ID_HEADER:      nElement = XML_oddHeader; break;
+        case EXC_ID_HEADER_FIRST: nElement = XML_firstHeader; break;
+        case EXC_ID_FOOTER_FIRST: nElement = XML_firstFooter; break;
+        case EXC_ID_HEADER_EVEN:  nElement = XML_evenHeader; break;
+        case EXC_ID_FOOTER_EVEN:  nElement = XML_evenFooter; break;
+        case EXC_ID_HEADER:       nElement = XML_oddHeader; break;
         case EXC_ID_FOOTER:
-        default:                 nElement = XML_oddFooter;
+        default:                  nElement = XML_oddFooter;
     }
     rWorksheet->startElement(nElement);
     rWorksheet->writeEscaped( maHdrString );
@@ -277,6 +279,7 @@ XclExpPageSettings::XclExpPageSettings( const XclExpRoot& rRoot ) :
 
         maData.mxBrushItem.reset( new SvxBrushItem( rItemSet.Get( ATTR_BACKGROUND ) ) );
         maData.mbUseEvenHF = false;
+        maData.mbUseFirstHF = false;
 
         // *** header and footer ***
 
@@ -300,6 +303,17 @@ XclExpPageSettings::XclExpPageSettings( const XclExpRoot& rRoot ) :
             {
                 // If maData.mbUseEvenHF become true, then we will need a copy of maHeader in maHeaderEven.
                 maData.maHeaderEven = maData.maHeader;
+            }
+            if (rHdrItemSet.HasItem(ATTR_PAGE_SHARED_FIRST) && !rHdrItemSet.Get(ATTR_PAGE_SHARED_FIRST).GetValue())
+            {
+                const ScPageHFItem& rHFItemFirst = rItemSet.Get( ATTR_PAGE_HEADERFIRST );
+                aHFConv.GenerateString( rHFItemFirst.GetLeftArea(), rHFItemFirst.GetCenterArea(), rHFItemFirst.GetRightArea() );
+                maData.maHeaderFirst = aHFConv.GetHFString();
+                maData.mbUseFirstHF = true;
+            }
+            else
+            {
+                maData.maHeaderFirst = maData.maHeader;
             }
             // header height (Excel excludes header from top margin)
             sal_Int32 nHdrHeight = rHdrItemSet.Get( ATTR_PAGE_DYNAMIC ).GetValue() ?
@@ -328,6 +342,17 @@ XclExpPageSettings::XclExpPageSettings( const XclExpRoot& rRoot ) :
             else
             {
                 maData.maFooterEven = maData.maFooter;
+            }
+            if (rFtrItemSet.HasItem(ATTR_PAGE_SHARED_FIRST) && !rFtrItemSet.Get(ATTR_PAGE_SHARED_FIRST).GetValue())
+            {
+                const ScPageHFItem& rHFItemFirst = rItemSet.Get( ATTR_PAGE_FOOTERFIRST );
+                aHFConv.GenerateString( rHFItemFirst.GetLeftArea(), rHFItemFirst.GetCenterArea(), rHFItemFirst.GetRightArea() );
+                maData.maFooterFirst = aHFConv.GetHFString();
+                maData.mbUseFirstHF = true;
+            }
+            else
+            {
+                maData.maFooterFirst = maData.maFooter;
             }
             // footer height (Excel excludes footer from bottom margin)
             sal_Int32 nFtrHeight = rFtrItemSet.Get( ATTR_PAGE_DYNAMIC ).GetValue() ?
@@ -373,12 +398,13 @@ namespace {
 class XclExpXmlStartHeaderFooterElementRecord : public XclExpXmlElementRecord
 {
 public:
-    explicit XclExpXmlStartHeaderFooterElementRecord(sal_Int32 const nElement, bool const bDifferentOddEven = false)
-         : XclExpXmlElementRecord(nElement), mbDifferentOddEven(bDifferentOddEven) {}
+    explicit XclExpXmlStartHeaderFooterElementRecord(sal_Int32 const nElement, bool const bDifferentOddEven = false, bool const bDifferentFirst = false)
+         : XclExpXmlElementRecord(nElement), mbDifferentOddEven(bDifferentOddEven), mbDifferentFirst(bDifferentFirst) {}
 
     virtual void        SaveXml( XclExpXmlStream& rStrm ) override;
 private:
     bool            mbDifferentOddEven;
+    bool            mbDifferentFirst;
 };
 
 }
@@ -390,7 +416,7 @@ void XclExpXmlStartHeaderFooterElementRecord::SaveXml(XclExpXmlStream& rStrm)
     sax_fastparser::FSHelperPtr& rStream = rStrm.GetCurrentStream();
     rStream->startElement( mnElement,
             // OOXTODO: XML_alignWithMargins,
-            XML_differentFirst,     "false",    // OOXTODO
+            XML_differentFirst,     mbDifferentFirst   ? "true" : "false",
             XML_differentOddEven,   mbDifferentOddEven ? "true" : "false"
             // OOXTODO: XML_scaleWithDoc
     );
@@ -439,13 +465,18 @@ void XclExpPageSettings::SaveXml( XclExpXmlStream& rStrm )
 
     XclExpSetup( maData ).SaveXml( rStrm );
 
-    XclExpXmlStartHeaderFooterElementRecord(XML_headerFooter, maData.mbUseEvenHF).SaveXml(rStrm);
+    XclExpXmlStartHeaderFooterElementRecord(XML_headerFooter, maData.mbUseEvenHF, maData.mbUseFirstHF).SaveXml(rStrm);
     XclExpHeaderFooter( EXC_ID_HEADER, maData.maHeader ).SaveXml( rStrm );
     XclExpHeaderFooter( EXC_ID_FOOTER, maData.maFooter ).SaveXml( rStrm );
     if (maData.mbUseEvenHF)
     {
         XclExpHeaderFooter( EXC_ID_HEADER_EVEN, maData.maHeaderEven ).SaveXml( rStrm );
         XclExpHeaderFooter( EXC_ID_FOOTER_EVEN, maData.maFooterEven ).SaveXml( rStrm );
+    }
+    if (maData.mbUseFirstHF)
+    {
+        XclExpHeaderFooter( EXC_ID_HEADER_FIRST, maData.maHeaderFirst ).SaveXml( rStrm );
+        XclExpHeaderFooter( EXC_ID_FOOTER_FIRST, maData.maFooterFirst ).SaveXml( rStrm );
     }
     XclExpXmlEndElementRecord( XML_headerFooter ).SaveXml( rStrm );
 

commit 3a8ca8e9f9f04a156055b1b364629b9d0741801c

tdf#144940 sc: fix disabled header/footer regression
Setting "Same content on first page" in the Page Style
dialog on either the Header or Footer tab resulted
disabled header/footer after reload. This was due to
flawed logic when assigning SC_UNO_PAGE_HDRON/FTRON
value: "leftness" was checked for but not "first pageness".

Regression from commit b9993369b4011cd8f367e439f5248096827f1efc
"tdf#121715 XLSX: support custom first page header/footer".

Co-authored-by: Daniel Arato (NISZ)

Change-Id: I8ee4c9f8145913d9dc5f4b66cdbc2352f2e405f0

sc/source/filter/xml/XMLTableHeaderFooterContext.cxx
@@ -63,19 +63,20 @@ XMLTableHeaderFooterContext::XMLTableHeaderFooterContext( SvXMLImport& rImport, 
             XMLOFF_WARN_UNKNOWN("sc", aIter);
     }
     bool bOn(::cppu::any2bool(xPropSet->getPropertyValue( sOn )));
-    if( bLeft )
+    if( bLeft || bFirst )
     {
+        const OUString sShare = bLeft ? sShareContent : sShareFirstContent;
         if( bOn && bDisplay )
         {
-            if( ::cppu::any2bool(xPropSet->getPropertyValue( sShareContent )) )
+            if( ::cppu::any2bool(xPropSet->getPropertyValue( sShare )) )
                 // Don't share headers any longer
-                xPropSet->setPropertyValue( sShareContent, uno::makeAny(false) );
+                xPropSet->setPropertyValue( sShare, uno::makeAny(false) );
         }
         else
         {
-            if( !::cppu::any2bool(xPropSet->getPropertyValue( sShareContent )) )
+            if( !::cppu::any2bool(xPropSet->getPropertyValue( sShare )) )
                 // share headers
-                xPropSet->setPropertyValue( sShareContent, uno::makeAny(true) );
+                xPropSet->setPropertyValue( sShare, uno::makeAny(true) );
         }
     }
     else

sc/qa/uitest/calc_tests8/tdf144940.py
@@ -1,0 +1,59 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+from uitest.framework import UITestCase
+from uitest.uihelper.common import get_state_as_dict
+from uitest.uihelper.common import select_pos
+from libreoffice.uno.propertyvalue import mkPropertyValues
+from tempfile import TemporaryDirectory
+from org.libreoffice.unotest import systemPathToFileUrl
+import os.path
+
+class tdf144940(UITestCase):
+
+    def test_tdf144940(self):
+
+        with TemporaryDirectory() as tempdir:
+            xFilePath = os.path.join(tempdir, "tdf144940-tmp.ods")
+
+            with self.ui_test.create_doc_in_start_center("calc"):
+
+                with self.ui_test.execute_dialog_through_command(".uno:PageFormatDialog") as xDialog:
+                    xTabs = xDialog.getChild("tabcontrol")
+                    select_pos(xTabs, "4")
+
+                    xCheckHeaderOn = xDialog.getChild("checkHeaderOn")
+                    xCheckSameFP = xDialog.getChild("checkSameFP")
+                    xCheckSameLR = xDialog.getChild("checkSameLR")
+
+                    self.assertEqual("true", get_state_as_dict(xCheckHeaderOn)["Selected"])
+                    self.assertEqual("true", get_state_as_dict(xCheckSameLR)["Selected"])
+                    self.assertEqual("false", get_state_as_dict(xCheckSameFP)["Selected"])
+
+                    xCheckSameFP.executeAction("CLICK", tuple())
+
+                    self.assertEqual("true", get_state_as_dict(xCheckSameFP)["Selected"])
+
+                with self.ui_test.execute_dialog_through_command(".uno:Save", close_button="open") as xSaveDialog:
+                    xFileName = xSaveDialog.getChild("file_name")
+                    xFileName.executeAction("TYPE", mkPropertyValues({"KEYCODE":"CTRL+A"}))
+                    xFileName.executeAction("TYPE", mkPropertyValues({"KEYCODE":"BACKSPACE"}))
+                    xFileName.executeAction("TYPE", mkPropertyValues({"TEXT": xFilePath}))
+
+            with self.ui_test.load_file(systemPathToFileUrl(xFilePath)) as document:
+
+                xPageStyles = document.StyleFamilies.getByIndex(1)
+                xDefaultPageStyle = xPageStyles.getByIndex(0)
+
+                # Without the fix in place, this test would have failed with
+                # AssertionError: False is not true
+                self.assertTrue(xDefaultPageStyle.HeaderOn)
+                self.assertTrue(xDefaultPageStyle.FooterOn)
+                self.assertTrue(xDefaultPageStyle.FirstPageHeaderIsShared)
+                self.assertTrue(xDefaultPageStyle.FirstPageFooterIsShared)
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab: