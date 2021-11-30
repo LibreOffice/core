05bdbcb8dd35e8bff76b596eb11f2f9ad8ae5658
Luboš Luňák <l.lunak@collabora.com>
2021-11-30 09:04:00 +0100

    workaround gcc bug #55776 about enum class shadowing a global
    GCC warns that 'enum class XorMode { None }' shadows 'constexpr int
    None = 0' for a workaround for X11 headers, even though there's
    no case for confusion here. So instead of a workaround for X11
    headers work this around by placing the enum first, and then GCC
    does not complain.
    
    Change-Id: I3e7cfaced957d47dee8cc4f10ee74e8dd97a5cc5

--- vcl/unx/generic/app/salinst.cxx
+++ vcl/unx/generic/app/salinst.cxx
@@ -19,6 +19,14 @@
 
 #include <stdlib.h>
 
+#include <config_features.h>
+#include <vcl/skia/SkiaHelper.hxx>
+#include <config_skia.h>
+#if HAVE_FEATURE_SKIA
+#include <skia/x11/gdiimpl.hxx>
+#include <skia/salbmp.hxx>
+#endif
+
 #include <unx/saldata.hxx>
 #include <unx/saldisp.hxx>
 #include <unx/salinst.h>
@@ -32,14 +40,6 @@
 #include <vcl/inputtypes.hxx>
 
 #include <salwtype.hxx>
-
-#include <config_features.h>
-#include <vcl/skia/SkiaHelper.hxx>
-#include <config_skia.h>
-#if HAVE_FEATURE_SKIA
-#include <skia/x11/gdiimpl.hxx>
-#include <skia/salbmp.hxx>
-#endif
 
 // plugin factory function
 extern "C"
