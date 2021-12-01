5b253b2197e957fb5e42e3d0e233c10ac83afc10
Miklos Vajna <vmiklos@collabora.com>
2021-12-01 11:48:17 +0100

    PPTX export: write the theme for the master pages from the doc model
    The instant benefit is that now the name of the theme and the color
    scheme are preserved, but this will also take changes done on the UI
    into account (which is not true for the grab-bag).
    
    Change-Id: I162eb7275d5a69d66db71fc5cd6e2e3ec94725bb
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/126164
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Tested-by: Jenkins

--- sd/source/filter/eppt/epptooxml.hxx
+++ sd/source/filter/eppt/epptooxml.hxx
@@ -26,6 +26,11 @@
 
 using ::sax_fastparser::FSHelperPtr;
 
+namespace svx
+{
+class Theme;
+}
+
 namespace oox::core {
 
 struct LayoutInfo
@@ -85,9 +90,15 @@ private:
     virtual void ImplWriteNotes( sal_uInt32 nPageNum ) override;
     virtual void ImplWriteSlideMaster( sal_uInt32 nPageNum, css::uno::Reference< css::beans::XPropertySet > const & aXBackgroundPropSet ) override;
     void ImplWritePPTXLayout( sal_Int32 nOffset, sal_uInt32 nMasterNum );
+
+    /// Export the color set part of a theme.
+    static bool WriteColorSets(const FSHelperPtr& pFS, svx::Theme* pTheme);
+
+    /// Same as WriteColorSets(), but works from a grab-bag.
     bool WriteColorSchemes(const FSHelperPtr& pFS, const OUString& rThemePath);
+
     static void WriteDefaultColorSchemes(const FSHelperPtr& pFS);
-    void WriteTheme( sal_Int32 nThemeNum );
+    void WriteTheme( sal_Int32 nThemeNum, svx::Theme* pTheme );
 
     virtual bool ImplCreateDocument() override;
     virtual bool ImplCreateMainNotes() override;
