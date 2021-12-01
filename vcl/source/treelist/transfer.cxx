e48cffa85c7dfe66394238dd23d25591a65d9242
Mike Kaganski <mike.kaganski@collabora.com>
2021-12-01 12:29:37 +0300

    tdf#145964 related: also drop special-casing in TransferableHelper::SetString
    Follow-up to commit 5fb9f4ffa9284c7248e2e82210506babaad4044d
    
    Change-Id: I46d5ea404f77ac5ff67b6ee6a42afee13274a481

--- vcl/source/treelist/transfer.cxx
+++ vcl/source/treelist/transfer.cxx
@@ -645,25 +645,9 @@ bool TransferableHelper::SetAny( const Any& rAny )
 }
 
 
-bool TransferableHelper::SetString( const OUString& rString, const DataFlavor& rFlavor )
+bool TransferableHelper::SetString( const OUString& rString )
 {
-    DataFlavor aFileFlavor;
-
-    if( !rString.isEmpty() &&
-        SotExchange::GetFormatDataFlavor( SotClipboardFormatId::SIMPLE_FILE, aFileFlavor ) &&
-        TransferableDataHelper::IsEqual( aFileFlavor, rFlavor ) )
-    {
-        const OString aByteStr(OUStringToOString(rString, osl_getThreadTextEncoding()));
-        Sequence< sal_Int8 >    aSeq( aByteStr.getLength() + 1 );
-        auto pSeq = aSeq.getArray();
-
-        memcpy( pSeq, aByteStr.getStr(), aByteStr.getLength() );
-        pSeq[ aByteStr.getLength() ] = 0;
-        maAny <<= aSeq;
-    }
-    else
-        maAny <<= rString;
-
+    maAny <<= rString;
     return maAny.hasValue();
 }
 
