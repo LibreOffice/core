8b71bc7ec704301325752ba9d0efd0b75663349b
Xisco Fauli <xiscofauli@libreoffice.org>
2021-12-01 17:07:18 +0100

    uitest: fix license headers
    Change-Id: I6b1a2808af9e6bb4d3240a83798c3c5958ef38e4
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/126183
    Tested-by: Jenkins
    Reviewed-by: Michael Meeks <michael.meeks@collabora.com>

--- sw/qa/uitest/writer_tests/trackedChanges.py
+++ sw/qa/uitest/writer_tests/trackedChanges.py
@@ -1,10 +1,13 @@
 # -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
 #
+# This file is part of the LibreOffice project.
+#
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-# tests for tracked changes ; tdf912270
+#
 
+# tests for tracked changes ; tdf912270
 from uitest.framework import UITestCase
 from uitest.uihelper.common import get_state_as_dict, get_url_for_data_file, type_text
 
