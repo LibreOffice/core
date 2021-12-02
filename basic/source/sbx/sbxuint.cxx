de00af1353376a27537380ab6ee5c4bc33d70b87
Tor Lillqvist <tml@iki.fi>
2021-12-02 12:37:07 +0200

    Make the tdf#97983 changes to BASIC optional based on an environment variable
    If you want to keep the string to floating point conversion semantics
    in BASIC as they were in LibreOffice 6, set the
    LIBREOFFICE6FLOATINGPOINTMODE environment variable to some non-empty
    value.
    
    Change-Id: I13d6d5d834e1bb81ef8df489db2b1da79f01dfc5

--- basic/source/sbx/sbxuint.cxx
+++ basic/source/sbx/sbxuint.cxx
@@ -23,6 +23,7 @@
 #include <vcl/errcode.hxx>
 #include <basic/sberrors.hxx>
 #include "sbxconv.hxx"
+#include <rtlproto.hxx>
 
 sal_uInt16 ImpGetUShort( const SbxValues* p )
 {
@@ -136,7 +137,7 @@ start:
             {
                 double d;
                 SbxDataType t;
-                if( ImpScan( *p->pOUString, d, t, nullptr, true ) != ERRCODE_NONE )
+                if( ImpScan( *p->pOUString, d, t, nullptr, !LibreOffice6FloatingPointMode() ) != ERRCODE_NONE )
                     nRes = 0;
                 else
                     nRes = ImpDoubleToUShort(d);
