86b6af5761fcf1e60e0090e153c766766abea20c
Tor Lillqvist <tml@iki.fi>
2021-12-02 12:37:07 +0200

    Make the tdf#97983 changes to BASIC optional based on an environment variable
    If you want to keep the string to floating point conversion semantics
    in BASIC as they were in LibreOffice 6, set the
    LIBREOFFICE6FLOATINGPOINTMODE environment variable to some non-empty
    value.
    
    Change-Id: I13d6d5d834e1bb81ef8df489db2b1da79f01dfc5

--- basic/source/runtime/methods1.cxx
+++ basic/source/runtime/methods1.cxx
@@ -2968,6 +2968,21 @@ void SbRtl_CompatibilityMode(StarBASIC *, SbxArray & rPar, bool)
     rPar.Get32(0)->PutBool( bEnabled );
 }
 
+bool LibreOffice6FloatingPointMode()
+{
+    static bool bMode = std::getenv("LIBREOFFICE6FLOATINGPOINTMODE") != nullptr;
+
+    return bMode;
+}
+
+void SbRtl_LibreOffice6FloatingPointMode(StarBASIC *, SbxArray & rPar, bool)
+{
+    if ( rPar.Count32() != 1 )
+        StarBASIC::Error( ERRCODE_BASIC_BAD_ARGUMENT );
+
+    rPar.Get32(0)->PutBool( LibreOffice6FloatingPointMode() );
+}
+
 void SbRtl_Input(StarBASIC *, SbxArray & rPar, bool)
 {
     // 2 parameters needed
--- basic/source/sbx/sbxint.cxx
+++ basic/source/sbx/sbxint.cxx
@@ -24,6 +24,7 @@
 #include <vcl/errcode.hxx>
 #include <basic/sberrors.hxx>
 #include "sbxconv.hxx"
+#include "rtlproto.hxx"
 
 #include <rtl/math.hxx>
 
@@ -158,7 +159,7 @@ start:
             {
                 double d;
                 SbxDataType t;
-                if( ImpScan( *p->pOUString, d, t, nullptr, true ) != ERRCODE_NONE )
+                if( ImpScan( *p->pOUString, d, t, nullptr, !LibreOffice6FloatingPointMode() ) != ERRCODE_NONE )
                     nRes = 0;
                 else if( !o3tl::convertsToAtMost(o3tl::roundAway(d), SbxMAXINT) )
                 {
@@ -443,7 +444,7 @@ start:
                     // Check if really 0 or invalid conversion
                     double d;
                     SbxDataType t;
-                    if( ImpScan( *p->pOUString, d, t, nullptr, true ) != ERRCODE_NONE )
+                    if( ImpScan( *p->pOUString, d, t, nullptr, !LibreOffice6FloatingPointMode() ) != ERRCODE_NONE )
                         nRes = 0;
                     else
                         nRes = static_cast<sal_Int64>(d);
@@ -704,7 +705,7 @@ start:
                     // Check if really 0 or invalid conversion
                     double d;
                     SbxDataType t;
-                    if( ImpScan( *p->pOUString, d, t, nullptr, true ) != ERRCODE_NONE )
+                    if( ImpScan( *p->pOUString, d, t, nullptr, !LibreOffice6FloatingPointMode() ) != ERRCODE_NONE )
                         nRes = 0;
                     else if( !o3tl::convertsToAtMost(o3tl::roundAway(d), SAL_MAX_UINT64) )
                     {
