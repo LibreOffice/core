826c2e13e1ef7d91e0b78d8e7001b2c4fef2b2c1
Stephan Bergmann <sbergman@redhat.com>
2021-12-03 10:08:05 +0100

    CppunitTest_oox_export needs the ASan LD_LIBRARY_PATH hack now too
    ...for CPPUNIT_TEST_NAME=testReferToTheme::TestBody introduced in
    f36767fde87191258ea21f3faac0be6ad79328e0 "PPTX export: handle theme colors from
    the doc model for shape text",
    
    > /home/sbergman/lo/core/test/source/bootstrapfixture.cxx:248:(anonymous namespace)::testReferToTheme::TestBody
    > equality assertion failed
    > - Expected: 0
    > - Actual  : 256
    > - failed to execute: sh /home/sbergman/lo/core/bin/officeotron.sh /home/sbergman/lo/tmp/libtest_oox_export.so1a9v6q.tmp > /home/sbergman/lo/tmp/_anonymous_namespace___testReferToTheme__TestBody_1a9v6u.tmp 2>&1
    > Exception in thread "main" java.lang.UnsatisfiedLinkError: /usr/lib/jvm/java-11-openjdk-11.0.13.0.8-2.fc35.x86_64/lib/libsystemconf.so: /home/sbergman/lo/core/instdir/program/libnspr4.so: undefined symbol: __asan_option_detect_stack_use_after_return
    
    Change-Id: If241bc3aa727c56ae7fa69744957690c722fbb1e

--- oox/CppunitTest_oox_export.mk
+++ oox/CppunitTest_oox_export.mk
@@ -43,4 +43,8 @@ $(eval $(call gb_CppunitTest_use_custom_headers,oox_export,\
 
 $(eval $(call gb_CppunitTest_use_configuration,oox_export))
 
+$(eval $(call gb_CppunitTest_add_arguments,oox_export, \
+    -env:arg-env=$(gb_Helper_LIBRARY_PATH_VAR)"$$$${$(gb_Helper_LIBRARY_PATH_VAR)+=$$$$$(gb_Helper_LIBRARY_PATH_VAR)}" \
+))
+
 # vim: set noet sw=4 ts=4:
