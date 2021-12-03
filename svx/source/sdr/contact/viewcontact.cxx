257e67dca7230ad0ee14f37a50a59df3902ac424
Miklos Vajna <vmiklos@collabora.com>
2021-12-03 09:53:41 +0100

    svx: fix crash in ViewContact::ActionChanged()
    Crashreport:
    
            sdr::contact::ViewContact::ActionChanged()
                    svx/source/sdr/contact/viewcontact.cxx:197 (discriminator 2)
            SdrTextObj::NbcSetOutlinerParaObjectForText(std::unique_ptr<OutlinerParaObject, std::default_delete<OutlinerParaObject> >, SdrText*)
                    svx/source/svdraw/svdotext.cxx:1379
            sdr::properties::TextProperties::ItemSetChanged(SfxItemSet const&)
                    /usr/include/c++/10/bits/unique_ptr.h:360
            sdr::properties::RectangleProperties::ItemSetChanged(SfxItemSet const&)
                    svx/source/sdr/properties/rectangleproperties.cxx:54
    
    Change-Id: I82de3e849cef33d207c8fd5e1c225de78d5f8705

--- svx/source/sdr/contact/viewcontact.cxx
+++ svx/source/sdr/contact/viewcontact.cxx
@@ -193,7 +193,10 @@ void ViewContact::ActionChanged()
         DBG_ASSERT(pCandidate,
                    "ViewContact::GetViewObjectContact() invalid ViewObjectContactList (!)");
 
-        pCandidate->ActionChanged();
+        if (pCandidate)
+        {
+            pCandidate->ActionChanged();
+        }
     }
 }
 
