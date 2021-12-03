62c5ddd86c3448e93e044171a53f973c9a40cf88
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2021-12-03 20:52:12 +0100

    Add image preffered DPI document setting, use it in Writer, Impress
    This adds a "image preferred DPI" document setting, which is used
    as a suggestion of the DPI that an image should have in the
    document. This is currently used when the image is inserted into
    the document (Writer, Impress/Draw) to resize it to the preferred
    DPI value.
    
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/126334
    Tested-by: Jenkins
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>
    (cherry picked from commit 4c00e8fb10437fcaefe8635ef390b78376938d15)
    
    Change-Id: I3ee9d409257e3c6aa2ead05144ecbba7b3b916f6

--- sd/source/ui/view/sdview4.cxx
+++ sd/source/ui/view/sdview4.cxx
@@ -170,6 +170,8 @@ SdrGrafObj* View::InsertGraphic( const Graphic& rGraphic, sal_Int8& rAction,
 
     else if ( pPV )
     {
+        Size aSizePixel = rGraphic.GetSizePixel();
+
         // create  new object
         Size aSize;
 
@@ -192,15 +194,34 @@ SdrGrafObj* View::InsertGraphic( const Graphic& rGraphic, sal_Int8& rAction,
                                                 MapMode( MapUnit::Map100thMM ) );
         }
 
-        pNewGrafObj = new SdrGrafObj(
-            getSdrModelFromSdrView(),
-            rGraphic,
-            ::tools::Rectangle(rPos, aSize));
-        SdrPage* pPage = pPV->GetPage();
-        Size aPageSize( pPage->GetSize() );
-        aPageSize.AdjustWidth( -(pPage->GetLeftBorder() + pPage->GetRightBorder()) );
-        aPageSize.AdjustHeight( -(pPage->GetUpperBorder() + pPage->GetLowerBorder()) );
-        pNewGrafObj->AdjustToMaxRect( ::tools::Rectangle( Point(), aPageSize ), true );
+        sal_Int32 nPreferredDPI = mrDoc.getImagePreferredDPI();
+        if (nPreferredDPI > 0)
+        {
+            constexpr double fTwipsInAnInch = 1444.0;
+            auto nWidth = (aSizePixel.Width() / double(nPreferredDPI)) * fTwipsInAnInch;
+            auto nHeight = (aSizePixel.Height() / double(nPreferredDPI)) * fTwipsInAnInch;
+            nWidth = convertTwipToMm100(nWidth);
+            nHeight = convertTwipToMm100(nHeight);
+
+            if (nWidth > 0 && nHeight > 0)
+                aSize = Size(nWidth, nHeight);
+        }
+
+        pNewGrafObj = new SdrGrafObj(getSdrModelFromSdrView(), rGraphic, ::tools::Rectangle(rPos, aSize));
+
+        if (nPreferredDPI > 0)
+        {
+            // move to the center of insertion point
+            pNewGrafObj->NbcMove(Size(-aSize.Width() / 2, -aSize.Height() / 2));
+        }
+        else
+        {
+            SdrPage* pPage = pPV->GetPage();
+            Size aPageSize( pPage->GetSize() );
+            aPageSize.AdjustWidth( -(pPage->GetLeftBorder() + pPage->GetRightBorder()) );
+            aPageSize.AdjustHeight( -(pPage->GetUpperBorder() + pPage->GetLowerBorder()) );
+            pNewGrafObj->AdjustToMaxRect( ::tools::Rectangle( Point(), aPageSize ), true );
+        }
 
         SdrInsertFlags nOptions = SdrInsertFlags::SETDEFLAYER;
         bool    bIsPresTarget = false;
