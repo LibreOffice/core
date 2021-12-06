8579b6e39ea30048ae7763f27d77e093b26e76dc
Luboš Luňák <l.lunak@collabora.com>
2021-12-06 02:20:46 +0100

    process broadcasts for adjacent cells together (tdf#119083)
    The problem in tdf#119083 is that it sets up listening for the range
    of cells used by VLOOKUP, and when adding a new column changes all
    those cells, it results in repeated SfxHintId::ScDataChanged
    broadcasts to the cells with the VLOOKUP formula. This commit
    makes ScHint include a row count, making it possible to group
    adjacent rows for SfxHintId::ScDataChanged together and send
    notifications just once for the range.
    
    Change-Id: Ib3439de58a2b1e5e8f01b037a62608e38b8e9125
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/126395
    Tested-by: Jenkins
    Reviewed-by: Luboš Luňák <l.lunak@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/126448

--- sc/source/core/data/documen7.cxx
+++ sc/source/core/data/documen7.cxx
@@ -121,23 +121,38 @@ void ScDocument::Broadcast( const ScHint& rHint )
     if ( eHardRecalcState == HardRecalcState::OFF )
     {
         ScBulkBroadcast aBulkBroadcast( pBASM.get(), rHint.GetId());     // scoped bulk broadcast
-        bool bIsBroadcasted = false;
-        SvtBroadcaster* pBC = GetBroadcaster(rHint.GetAddress());
-        if ( pBC )
-        {
-            pBC->Broadcast( rHint );
-            bIsBroadcasted = true;
-        }
+        bool bIsBroadcasted = BroadcastHintInternal(rHint);
         if ( pBASM->AreaBroadcast( rHint ) || bIsBroadcasted )
             TrackFormulas( rHint.GetId() );
     }
 
-    if ( rHint.GetAddress() != BCA_BRDCST_ALWAYS )
+    if ( rHint.GetStartAddress() != BCA_BRDCST_ALWAYS )
     {
-        SCTAB nTab = rHint.GetAddress().Tab();
+        SCTAB nTab = rHint.GetStartAddress().Tab();
         if (nTab < static_cast<SCTAB>(maTabs.size()) && maTabs[nTab])
             maTabs[nTab]->SetStreamValid(false);
     }
+}
+
+bool ScDocument::BroadcastHintInternal( const ScHint& rHint )
+{
+    bool bIsBroadcasted = false;
+    const ScAddress address(rHint.GetStartAddress());
+    SvtBroadcaster* pLastBC = nullptr;
+    // Process all broadcasters for the given row range.
+    for( SCROW nRow = address.Row(); nRow < address.Row() + rHint.GetRowCount(); ++nRow )
+    {
+        ScAddress a(address);
+        a.SetRow(nRow);
+        SvtBroadcaster* pBC = GetBroadcaster(a);
+        if ( pBC && pBC != pLastBC )
+        {
+            pBC->Broadcast( rHint );
+            bIsBroadcasted = true;
+            pLastBC = pBC;
+        }
+    }
+    return bIsBroadcasted;
 }
 
 void ScDocument::BroadcastCells( const ScRange& rRange, SfxHintId nHint, bool bBroadcastSingleBroadcasters )
@@ -528,32 +543,35 @@ void ScDocument::TrackFormulas( SfxHintId nHintId )
     {
         // outside the loop, check if any sheet has a "calculate" event script
         bool bCalcEvent = HasAnySheetEventScript( ScSheetEventId::CALCULATE, true );
-        ScFormulaCell* pTrack;
-        ScFormulaCell* pNext;
-        pTrack = pFormulaTrack;
-        do
+        for( ScFormulaCell* pTrack = pFormulaTrack; pTrack != nullptr; pTrack = pTrack->GetNextTrack())
         {
-            SvtBroadcaster* pBC = GetBroadcaster(pTrack->aPos);
-            ScHint aHint(nHintId, pTrack->aPos);
-            if (pBC)
-                pBC->Broadcast( aHint );
+            SCROW rowCount = 1;
+            ScAddress address = pTrack->aPos;
+            // Compress to include all adjacent cells in the same column.
+            for(ScFormulaCell* pNext = pTrack->GetNextTrack(); pNext != nullptr; pNext = pNext->GetNextTrack())
+            {
+                if(pNext->aPos != ScAddress(address.Col(), address.Row() + rowCount, address.Tab()))
+                    break;
+                ++rowCount;
+                pTrack = pNext;
+            }
+            ScHint aHint( nHintId, address, rowCount );
+            BroadcastHintInternal( aHint );
             pBASM->AreaBroadcast( aHint );
             // for "calculate" event, keep track of which sheets are affected by tracked formulas
             if ( bCalcEvent )
-                SetCalcNotification( pTrack->aPos.Tab() );
-            pTrack = pTrack->GetNextTrack();
-        } while ( pTrack );
-        pTrack = pFormulaTrack;
+                SetCalcNotification( address.Tab() );
+        }
         bool bHaveForced = false;
-        do
+        for( ScFormulaCell* pTrack = pFormulaTrack; pTrack != nullptr;)
         {
-            pNext = pTrack->GetNextTrack();
+            ScFormulaCell* pNext = pTrack->GetNextTrack();
             RemoveFromFormulaTrack( pTrack );
             PutInFormulaTree( pTrack );
             if ( pTrack->GetCode()->IsRecalcModeForced() )
                 bHaveForced = true;
             pTrack = pNext;
-        } while ( pTrack );
+        }
         if ( bHaveForced )
         {
             SetForcedFormulas( true );
