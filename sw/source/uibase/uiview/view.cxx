3d81621f77dde06c4c096b1ab5373b69bb7e06c0
Dhiraj Holden <dhiraj.holden@gmail.com>
2021-12-07 08:16:59 -0500

    tdf#142450 add code to store showing whitespace
    I have put in code to store the option to show whitespace.
    This option is stored at the document level like the other
    layout options.
    
    Change-Id: I26989da2714f884a5a4d5ced3329ff669771fe7a

--- sw/source/uibase/uiview/view.cxx
+++ sw/source/uibase/uiview/view.cxx
@@ -1318,6 +1318,7 @@ void SwView::ReadUserDataSequence ( const uno::Sequence < beans::PropertyValue >
     sal_Int16 nZoomFactor = static_cast < sal_Int16 > (pVOpt->GetZoom());
     bool bViewLayoutBookMode = pVOpt->IsViewLayoutBookMode();
     sal_Int16 nViewLayoutColumns = pVOpt->GetViewLayoutColumns();
+    bool bHideWhitespace = pVOpt->IsHideWhitespaceMode();
 
     bool bSelectedFrame = ( m_pWrtShell->GetSelFrameType() != FrameTypeFlags::NONE ),
              bGotVisibleLeft = false,
@@ -1325,6 +1326,7 @@ void SwView::ReadUserDataSequence ( const uno::Sequence < beans::PropertyValue >
              bGotVisibleBottom = false, bGotZoomType = false,
              bGotZoomFactor = false, bGotIsSelectedFrame = false,
              bGotViewLayoutColumns = false, bGotViewLayoutBookMode = false,
+             bGotHideWhitespace = false,
              bBrowseMode = false, bGotBrowseMode = false;
     bool bKeepRatio = pVOpt->IsKeepRatio();
     bool bGotKeepRatio = false;
@@ -1399,6 +1401,11 @@ void SwView::ReadUserDataSequence ( const uno::Sequence < beans::PropertyValue >
         {
             rValue.Value >>= bKeepRatio;
             bGotKeepRatio = true;
+        }
+        else if (rValue.Name == "HideWhitespace")
+        {
+            rValue.Value >>= bHideWhitespace;
+            bGotHideWhitespace = true;
         }
         // Fallback to common SdrModel processing
         else
@@ -1476,11 +1483,21 @@ void SwView::ReadUserDataSequence ( const uno::Sequence < beans::PropertyValue >
         m_pWrtShell->SetMacroExecAllowed( bSavedFlagValue );
     }
 
+    SwViewOption aUsrPref(*pVOpt);
+    bool bUsrPrefModified = false;
     if (bGotKeepRatio && bKeepRatio != pVOpt->IsKeepRatio())
     {
         // Got a custom value, then it makes sense to trigger notifications.
-        SwViewOption aUsrPref(*pVOpt);
         aUsrPref.SetKeepRatio(bKeepRatio);
+        bUsrPrefModified = true;
+    }
+    if (bGotHideWhitespace && bHideWhitespace != pVOpt->IsHideWhitespaceMode())
+    {
+        aUsrPref.SetHideWhitespaceMode(bHideWhitespace);
+        bUsrPrefModified = true;
+    }
+    if (bUsrPrefModified)
+    {
         SW_MOD()->ApplyUsrPref(aUsrPref, this);
     }
 
@@ -1584,6 +1601,9 @@ void SwView::WriteUserDataSequence ( uno::Sequence < beans::PropertyValue >& rSe
 
     aVector.push_back(
         comphelper::makePropertyValue("KeepRatio", m_pWrtShell->GetViewOptions()->IsKeepRatio()));
+
+    aVector.push_back(comphelper::makePropertyValue(
+        "HideWhitespace", m_pWrtShell->GetViewOptions()->IsHideWhitespaceMode()));
 
     rSequence = comphelper::containerToSequence(aVector);
 
