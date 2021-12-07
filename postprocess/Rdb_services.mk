LibreOffice/core

commit 1c9a40299d328c78c035ca63ccdf22c5c669a03b

gbuild: create services.rdb from built components
... and introduce the --enable-services-rdb-from-build option.

Currently the build handles global build options redundant
in a few places:

* in Repository*mk + modules - the "real" build dependencies
* the full services.rdb generation
* the static UNO constructor map generation

Also the component files don't reflect the really built components
and so the RDB services generation must handle the whole options
to select the correct components.

So this optionally replaces the latter two by generating the list
of components and it's constructors from the build itself. As a
consequence, component files must now be split, so they reflect
the real components in the libraries, otherwise the static
constructor list will have missing symbols. IMHO this is more
natural, as it happens in the same place already handling these
build options for the sourcecode.

This also adds a convenience helper to add multiple component
files: gb_Library_set_componentfiles

This is WIP and currently just works for the stripped WASM build,
which introduces many more split component files in later patches.

It also explicitly filters the gb_Rdb__URECOMPONENTS and the
CppunitTest related components from the services.rdb. Maybe there
is a good way to do this properly.

Change-Id: I1b38a6f2c1e5221f18d7e5e756c30263b555d962
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/126185
Tested-by: Jenkins
Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>

postprocess/Rdb_services.mk
@@ -9,6 +9,9 @@
 
 $(eval $(call gb_Rdb_Rdb_install,services))
 
+# see Makefile.gbuild for the gb_Rdb_add_components,services calls
+ifeq (,$(ENABLE_SERVICES_RDB_FROM_BUILD))
+
 #
 # "minimal" list of services for cross-compiling build tools
 #
@@ -115,6 +118,7 @@ $(eval $(call gb_Rdb_add_components,services,\
 	slideshow/util/slideshow \
 	starmath/util/sm \
 	starmath/util/smd \
+    $(call gb_Helper_optional,BREAKPAD,svx/util/crashreport) \
 	svx/util/svx \
 	svx/util/textconversiondlgs \
 	sw/util/msword \
@@ -271,6 +275,7 @@ $(eval $(call gb_Rdb_add_components,services,\
 	reportdesign/util/rptui \
 	reportdesign/util/rptxml \
 	shell/source/backends/localebe/localebe1 \
+    sw/util/sw_mm \
 	$(if $(filter-out ANDROID iOS,$(OS)),\
 		connectivity/source/drivers/odbc/odbc \
 	) \
@@ -337,5 +342,7 @@ $(eval $(call gb_Rdb_add_components,services,\
 endif
 
 endif # $(gb_Side),host
+
+endif # !ENABLE_SERVICES_RDB_FROM_BUILD
 
 # vim: set noet sw=4 ts=4:

Makefile.gbuild
@@ -24,6 +24,11 @@ ifneq (,$(DISABLE_DYNLOADING))
 $(if $(gb_LinkTarget__Lock),$(shell rm -f $(gb_LinkTarget__Lock)))
 endif
 
+ifeq ($(ENABLE_SERVICES_RDB_FROM_BUILD),TRUE)
+$(foreach comp,$(filter-out $(gb_Rdb__URECOMPONENTS),$(gb_ComponentTarget__ALLCOMPONENTS)), \
+    $(eval $(call gb_Rdb_add_components,services,$(comp))))
+endif
+
 upload-symbols:
 	bin/upload_symbols.py $(WORKDIR)/symbols.zip $(BREAKPAD_SYMBOL_CONFIG) "$(LIBO_VERSION_MAJOR).$(LIBO_VERSION_MINOR).$(LIBO_VERSION_MICRO).$(LIBO_VERSION_PATCH)$(LIBO_VERSION_SUFFIX)$(LIBO_VERSION_SUFFIX_SUFFIX)"
 

basic/Library_sb.mk
@@ -19,8 +19,6 @@
 
 $(eval $(call gb_Library_Library,sb))
 
-$(eval $(call gb_Library_set_componentfile,sb,basic/util/sb))
-
 $(eval $(call gb_Library_set_include,sb,\
 	$$(INCLUDE) \
 	-I$(SRCDIR)/basic/inc \
@@ -59,6 +57,8 @@ $(eval $(call gb_Library_use_libraries,sb,\
 ))
 
 ifneq ($(filter SCRIPTING,$(BUILD_TYPE)),)
+
+$(eval $(call gb_Library_set_componentfile,sb,basic/util/sb))
 
 $(eval $(call gb_Library_add_exception_objects,sb,\
 	basic/source/basmgr/basicmanagerrepository \

config_host.mk.in
@@ -201,6 +201,7 @@ export ENABLE_SCRIPTING_BEANSHELL=@ENABLE_SCRIPTING_BEANSHELL@
 export ENABLE_SCRIPTING_JAVASCRIPT=@ENABLE_SCRIPTING_JAVASCRIPT@
 export ENABLE_SDREMOTE=@ENABLE_SDREMOTE@
 export ENABLE_SDREMOTE_BLUETOOTH=@ENABLE_SDREMOTE_BLUETOOTH@
+export ENABLE_SERVICES_RDB_FROM_BUILD=@ENABLE_SERVICES_RDB_FROM_BUILD@
 export ENABLE_SILENT_MSI=@ENABLE_SILENT_MSI@
 export ENABLE_SKIA=@ENABLE_SKIA@
 export ENABLE_SKIA_DEBUG=@ENABLE_SKIA_DEBUG@

configure.ac
@@ -1967,6 +1967,12 @@ AC_ARG_ENABLE(xmlhelp,
         [Disable XML help support]),
 ,enable_xmlhelp=yes)
 
+AC_ARG_ENABLE(services-rdb-from-build,
+    AS_HELP_STRING([--enable-services-rdb-from-build],
+        [Automatically creates the services.rdb from all built components.
+         Splitting the component files and adapting the Library_*.mk files
+         is WIP and probably just works together with --enable-wasm-strip.]))
+
 
 dnl ===================================================================
 dnl Optional Packages (--with/without-)
@@ -2564,6 +2570,7 @@ AC_ARG_WITH(locales,
          Very experimental and might well break stuff.
          Just a desperate measure to shrink code and data size.
          By default all the locales available is included.
+         Just works with --disable-dynloading. Defaults to "ALL".
          This option is completely unrelated to --with-lang.])
     [
                           Affects also our character encoding conversion
@@ -2825,7 +2832,6 @@ AC_ARG_WITH(system-icu-for-build,
     AS_HELP_STRING([--with-system-icu-for-build=yes/no/force],
         [Use icu already on system for build tools (cross-compilation only).]))
 
-
 dnl ===================================================================
 dnl Check for incompatible options set by fuzzing, and reset those
 dnl automatically to working combinations
@@ -2943,6 +2949,15 @@ if test $_os != iOS -a $_os != Android -a $_os != Emscripten -a "$enable_fuzzers
     AC_DEFINE(HAVE_FEATURE_DESKTOP)
     AC_DEFINE(HAVE_FEATURE_MULTIUSER_ENVIRONMENT)
 fi
+
+ENABLE_SERVICES_RDB_FROM_BUILD=
+if test "$enable_services_rdb_from_build" = yes; then
+    ENABLE_SERVICES_RDB_FROM_BUILD=TRUE
+    if test -n "$with_locales" -a "$with_locales" != en -a "$with_locales" != ALL; then
+        AC_MSG_ERROR([Currently just --with-locales=all or en is supported with --enable-services-rdb-from-build])
+    fi
+fi
+AC_SUBST(ENABLE_SERVICES_RDB_FROM_BUILD)
 
 # Whether to build "avmedia" functionality or not.
 
@@ -5516,6 +5531,7 @@ if test "$cross_compiling" = "yes"; then
         --disable-opencl \
         --disable-pdfimport \
         --disable-postgresql-sdbc \
+        --disable-services-rdb-from-build \
         --disable-skia \
         --enable-icecream="$enable_icecream" \
         --without-doxygen \
@@ -13883,7 +13899,7 @@ if test -d "$SRC_ROOT/translations/source"; then
 fi
 AC_SUBST(WITH_POOR_HELP_LOCALIZATIONS)
 
-if test -n "$with_locales"; then
+if test -n "$with_locales" -a "$with_locales" != ALL; then
     WITH_LOCALES="$with_locales"
 
     just_langs="`echo $WITH_LOCALES | sed -e 's/_[A-Z]*//g'`"

i18npool/Library_i18npool.mk
@@ -9,7 +9,11 @@
 
 $(eval $(call gb_Library_Library,i18npool))
 
+ifeq ($(WITH_LOCALES),en)
+$(eval $(call gb_Library_set_componentfile,i18npool,i18npool/util/i18npool.en))
+else
 $(eval $(call gb_Library_set_componentfile,i18npool,i18npool/util/i18npool))
+endif
 
 $(eval $(call gb_Library_set_include,i18npool,\
 	$$(INCLUDE) \

i18npool/util/i18npool.en.component
@@ -1,0 +1,162 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!--
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ -->
+
+<component loader="com.sun.star.loader.SharedLibrary" environment="@CPPU_ENV@"
+    prefix="i18npool" xmlns="http://openoffice.org/2010/uno-components">
+  <implementation name="com.sun.star.i18n.BreakIterator"
+      constructor="com_sun_star_i18n_BreakIterator_get_implementation">
+    <service name="com.sun.star.i18n.BreakIterator"/>
+  </implementation>
+  <implementation name="com.sun.star.i18n.BreakIterator_Unicode"
+      constructor="com_sun_star_i18n_BreakIterator_Unicode_get_implementation">
+    <service name="com.sun.star.i18n.BreakIterator_Unicode"/>
+  </implementation>
+  <implementation name="com.sun.star.i18n.CalendarImpl"
+    constructor="i18npool_CalendarImpl_get_implementation">
+    <service name="com.sun.star.i18n.LocaleCalendar"/>
+    <service name="com.sun.star.i18n.LocaleCalendar2"/>
+  </implementation>
+  <implementation name="com.sun.star.i18n.Calendar_ROC"
+    constructor="i18npool_Calendar_ROC_get_implementation">
+    <service name="com.sun.star.i18n.Calendar_ROC"/>
+  </implementation>
+  <implementation name="com.sun.star.i18n.Calendar_dangi"
+    constructor="i18npool_Calendar_dangi_get_implementation">
+    <service name="com.sun.star.i18n.Calendar_dangi"/>
+  </implementation>
+  <implementation name="com.sun.star.i18n.Calendar_buddhist"
+    constructor="i18npool_Calendar_buddhist_get_implementation">
+    <service name="com.sun.star.i18n.Calendar_buddhist"/>
+  </implementation>
+  <implementation name="com.sun.star.i18n.Calendar_gengou"
+    constructor="i18npool_Calendar_gengou_get_implementation">
+    <service name="com.sun.star.i18n.Calendar_gengou"/>
+  </implementation>
+  <implementation name="com.sun.star.i18n.Calendar_gregorian"
+    constructor="i18npool_Calendar_gregorian_get_implementation">
+    <service name="com.sun.star.i18n.Calendar_gregorian"/>
+  </implementation>
+  <implementation name="com.sun.star.i18n.Calendar_hanja"
+    constructor="i18npool_Calendar_hanja_get_implementation">
+    <service name="com.sun.star.i18n.Calendar_hanja"/>
+  </implementation>
+  <implementation name="com.sun.star.i18n.Calendar_hanja_yoil"
+    constructor="i18npool_Calendar_hanja_yoil_get_implementation">
+    <service name="com.sun.star.i18n.Calendar_hanja_yoil"/>
+  </implementation>
+  <implementation name="com.sun.star.i18n.Calendar_hijri"
+    constructor="i18npool_Calendar_hijri_get_implementation">
+    <service name="com.sun.star.i18n.Calendar_hijri"/>
+  </implementation>
+  <implementation name="com.sun.star.i18n.Calendar_jewish"
+    constructor="i18npool_Calendar_jewish_get_implementation">
+    <service name="com.sun.star.i18n.Calendar_jewish"/>
+  </implementation>
+  <implementation name="com.sun.star.i18n.ChapterCollator"
+    constructor="i18npool_ChapterCollator_get_implementation">
+    <service name="com.sun.star.i18n.ChapterCollator"/>
+  </implementation>
+  <implementation name="com.sun.star.i18n.CharacterClassification"
+      constructor="com_sun_star_i18n_CharacterClassification_get_implementation">
+    <service name="com.sun.star.i18n.CharacterClassification"/>
+  </implementation>
+  <implementation name="com.sun.star.i18n.CharacterClassification_Unicode"
+      constructor="com_sun_star_i18n_CharacterClassification_Unicode_get_implementation">
+    <service name="com.sun.star.i18n.CharacterClassification_Unicode"/>
+  </implementation>
+  <implementation name="com.sun.star.i18n.Collator"
+      constructor="com_sun_star_i18n_Collator_get_implementation">
+    <service name="com.sun.star.i18n.Collator"/>
+  </implementation>
+  <implementation name="com.sun.star.i18n.Collator_Unicode"
+    constructor="i18npool_Collator_Unicode_get_implementation">
+    <service name="com.sun.star.i18n.Collator_Unicode"/>
+  </implementation>
+  <implementation name="com.sun.star.i18n.IndexEntrySupplier"
+    constructor="i18npool_IndexEntrySupplier_get_implementation">
+    <service name="com.sun.star.i18n.IndexEntrySupplier"/>
+  </implementation>
+  <implementation name="com.sun.star.i18n.IndexEntrySupplier_Unicode"
+    constructor="i18npool_IndexEntrySupplier_Unicode_get_implementation">
+    <service name="com.sun.star.i18n.IndexEntrySupplier_Unicode"/>
+  </implementation>
+  <implementation name="com.sun.star.i18n.IndexEntrySupplier_asian"
+    constructor="i18npool_IndexEntrySupplier_asian_get_implementation">
+    <service name="com.sun.star.i18n.IndexEntrySupplier_asian"/>
+  </implementation>
+  <implementation name="com.sun.star.i18n.InputSequenceChecker"
+      constructor="com_sun_star_i18n_InputSequenceChecker_get_implementation">
+    <service name="com.sun.star.i18n.InputSequenceChecker"/>
+  </implementation>
+  <implementation name="com.sun.star.i18n.LocaleDataImpl"
+      constructor="com_sun_star_i18n_LocaleDataImpl_get_implementation">
+    <service name="com.sun.star.i18n.LocaleData"/>
+    <service name="com.sun.star.i18n.LocaleData2"/>
+  </implementation>
+  <implementation name="com.sun.star.i18n.NativeNumberSupplier"
+      constructor="com_sun_star_i18n_NativeNumberSupplier_get_implementation">
+    <service name="com.sun.star.i18n.NativeNumberSupplier"/>
+    <service name="com.sun.star.i18n.NativeNumberSupplier2"/>
+  </implementation>
+  <implementation name="com.sun.star.i18n.NumberFormatCodeMapper"
+      constructor="com_sun_star_i18n_NumberFormatCodeMapper_get_implementation">
+    <service name="com.sun.star.i18n.NumberFormatMapper"/>
+  </implementation>
+  <implementation name="com.sun.star.i18n.OrdinalSuffix"
+      constructor="com_sun_star_i18n_OrdinalSuffix_get_implementation">
+    <service name="com.sun.star.i18n.OrdinalSuffix"/>
+  </implementation>
+  <implementation name="com.sun.star.i18n.ScriptTypeDetector"
+      constructor="com_sun_star_i18n_ScriptTypeDetector_get_implementation">
+    <service name="com.sun.star.i18n.ScriptTypeDetector"/>
+  </implementation>
+  <implementation name="com.sun.star.i18n.TextConversion"
+      constructor="com_sun_star_i18n_TextConversion_get_implementation">
+    <service name="com.sun.star.i18n.TextConversion"/>
+  </implementation>
+  <implementation name="com.sun.star.i18n.Transliteration"
+      constructor="com_sun_star_i18n_Transliteration_get_implementation">
+    <service name="com.sun.star.i18n.Transliteration"/>
+  </implementation>
+  <implementation name="com.sun.star.i18n.Transliteration.NumToCharFullwidth">
+    <service name="com.sun.star.i18n.Transliteration.l10n"/>
+  </implementation>
+  <implementation name="com.sun.star.i18n.Transliteration.NumToCharHalfwidth">
+    <service name="com.sun.star.i18n.Transliteration.l10n"/>
+  </implementation>
+  <implementation name="com.sun.star.i18n.Transliteration.NumToTextCircledNumber">
+    <service name="com.sun.star.i18n.Transliteration.l10n"/>
+  </implementation>
+  <implementation name="com.sun.star.i18n.Transliteration.SENTENCE_CASE">
+    <service name="com.sun.star.i18n.Transliteration.l10n"/>
+  </implementation>
+  <implementation name="com.sun.star.i18n.Transliteration.TITLE_CASE">
+    <service name="com.sun.star.i18n.Transliteration.l10n"/>
+  </implementation>
+  <implementation name="com.sun.star.i18n.Transliteration.TOGGLE_CASE">
+    <service name="com.sun.star.i18n.Transliteration.l10n"/>
+  </implementation>
+  <implementation name="com.sun.star.i18n.Transliteration.UPPERCASE_LOWERCASE">
+    <service name="com.sun.star.i18n.Transliteration.l10n"/>
+  </implementation>
+  <implementation name="com.sun.star.text.DefaultNumberingProvider"
+      constructor="com_sun_star_text_DefaultNumberingProvider_get_implementation">
+    <service name="com.sun.star.text.DefaultNumberingProvider"/>
+  </implementation>
+</component>

postprocess/CustomTarget_components.mk
@@ -1,0 +1,49 @@
+# vim: set noet sw=4 ts=4:
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_CustomTarget_CustomTarget,postprocess/components))
+
+ifeq (,$(gb_PARTIAL_BUILD))
+
+postprocess_WORKDIR := $(call gb_CustomTarget_get_workdir,postprocess)
+
+$(call gb_CustomTarget_get_target,postprocess/components): \
+    $(postprocess_WORKDIR)/services_constructors.list \
+
+gb_EMPTY :=
+define gb_NEWLINE
+
+$(gb_EMPTY)
+endef
+
+$(postprocess_WORKDIR)/services_componentfiles.list: \
+    $(call gb_Rdb_get_target,services) \
+    $(call gb_Rdb_get_target,ure/services) \
+    $(foreach comp,$(gb_ComponentTarget__ALLCOMPONENTS),$(call gb_ComponentTarget_get_source,$(comp))) \
+    | $(postprocess_WORKDIR)/.dir
+	$(call gb_Output_announce,$(subst $(BUILDDIR)/,,$@),$(true),GEN,2)
+	TEMPFILE=$(call gb_var2file,$(shell $(gb_MKTEMP)),1, \
+            $(foreach comp,$(sort $(gb_ComponentTarget__ALLCOMPONENTS)),$(call gb_ComponentTarget_get_source,$(comp))$(gb_NEWLINE))) && \
+            mv $$TEMPFILE $@
+
+$(postprocess_WORKDIR)/services_constructors.list: \
+    $(SRCDIR)/solenv/bin/constructors.py \
+    $(postprocess_WORKDIR)/services_componentfiles.list \
+    | $(postprocess_WORKDIR)/.dir
+	$(call gb_Output_announce,$(subst $(BUILDDIR)/,,$@),$(true),GEN,2)
+	TEMPFILE=$(shell $(gb_MKTEMP)) && \
+	    $(call gb_Helper_abbreviate_dirs,$(call gb_ExternalExecutable_get_command,python) $^) > $$TEMPFILE && \
+	    $(call gb_Helper_replace_if_different_and_touch,$${TEMPFILE},$@)
+
+.PHONY: $(postprocess_WORKDIR)/services_componentfiles.list
+
+endif # gb_PARTIAL_BUILD
+
+# vim: set noet sw=4:

commit 2847150e6624bf7fb60f1b2c454b391c72edb19e

services.rdb: use i18npool/util/i18npool.en
oss-fuzz failed with .../solenv/gbuild/ComponentTarget.mk:53:
*** No LIBFILENAME set at component target:
/work/workdir/ComponentTarget/i18npool/util/i18npool.component.
Stop.

oss-fuzz uses --with-locales=en, so actually include the
i18npool.en.component file in the services.rdb generation.

Regression from commit 1c9a40299d328c78c035ca63ccdf22c5c669a03b
("gbuild: create services.rdb from built components").

Change-Id: Ica250403b687f636880bbc066e83b9ffe9689c5f

postprocess/Rdb_services.mk
@@ -41,7 +41,7 @@ $(eval $(call gb_Rdb_add_components,services,\
 	filter/source/svg/svgfilter \
 	framework/util/fwk \
 	i18npool/source/search/i18nsearch \
-	i18npool/util/i18npool \
+	$(if $(filter en,$(WITH_LOCALES)),i18npool/util/i18npool.en,i18npool/util/i18npool) \
 	package/source/xstor/xstor \
 	package/util/package2 \
 	sax/source/expatwrap/expwrap \