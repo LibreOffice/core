69df902aee3ed8704c225844d13776c2b279b8a6
Stephan Bergmann <sbergman@redhat.com>
2021-12-07 15:50:47 +0100

    CppunitTest_xmloff_draw needs the ASan LD_LIBRARY_PATH hack now too
    ...for CPPUNIT_TEST_NAME=testThemeExport::TestBody introduced in
    c95288aec4eb4d92a5ccfb9d8fc02a0185d8e8d0 "ODP export: write the theme of a
    master page",
    
    > warn:test:3875202:3875202:test/source/bootstrapfixture.cxx:240: /home/sbergman/lo/core/schema/libreoffice/OpenDocument-v1.3+libreoffice-manifest-schema.rng[-1,-1]:  Error: Can't find bundle for base name com.sun.msv.reader.Messages, locale en_US
    > /home/sbergman/lo/core/schema/libreoffice/OpenDocument-v1.3+libreoffice-manifest-schema.rng[-1,-1]:  Fatal: Can't find bundle for base name com.sun.msv.reader.Messages, locale en_US
    > /home/sbergman/lo/core/schema/libreoffice/OpenDocument-v1.3+libreoffice-manifest-schema.rng:Schema has validation errors.
    > Validation aborted.
    >
    > /home/sbergman/lo/core/test/source/bootstrapfixture.cxx:241:testThemeExport::TestBody
    > forced failure
    
    Change-Id: Ib028d1449a9b878aea3b073de6388fb3bb6d590c

--- xmloff/CppunitTest_xmloff_draw.mk
+++ xmloff/CppunitTest_xmloff_draw.mk
@@ -43,4 +43,8 @@ $(eval $(call gb_CppunitTest_use_custom_headers,xmloff_draw,\
 
 $(eval $(call gb_CppunitTest_use_configuration,xmloff_draw))
 
+$(eval $(call gb_CppunitTest_add_arguments,xmloff_draw, \
+    -env:arg-env=$(gb_Helper_LIBRARY_PATH_VAR)"$$$${$(gb_Helper_LIBRARY_PATH_VAR)+=$$$$$(gb_Helper_LIBRARY_PATH_VAR)}" \
+))
+
 # vim: set noet sw=4 ts=4:
