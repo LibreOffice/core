046e6cfa544d2ffd67fd29ba7dde41b495744618
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2021-12-07 23:25:54 +0100

    Add graphic size checker for the preferred document DPI
    This change adds a graphic size checker, which checks all the
    images in the document, if they largely differ (outside of 50% and
    110% of the image size) from the set preferred image DPI document
    setting. For all images that don't fall under this bounds, list
    them in the dialog and offer the posibility to select/goto the
    image and pop-up the properties dialog for the image to change
    its size.
    
    Change-Id: I06efce77c291fdb6ec3864d72c2f4d15dba9c42b
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/127094
    Tested-by: Jenkins
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

---
+++ sd/source/ui/tools/GraphicSizeCheck.cxx
@@ -0,0 +1,213 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#include <memory>
+#include <tools/GraphicSizeCheck.hxx>
+#include <svx/strings.hrc>
+#include <svx/svdobj.hxx>
+#include <svx/svdpage.hxx>
+#include <svx/svxids.hrc>
+#include <sfx2/dispatch.hxx>
+
+#include <sdresid.hxx>
+#include <DrawViewShell.hxx>
+#include <DrawDocShell.hxx>
+
+namespace sd
+{
+namespace
+{
+class ModelTraverseHandler
+{
+public:
+    virtual ~ModelTraverseHandler() {}
+
+    virtual void handleSdrObject(SdrObject* pObject) = 0;
+};
+
+class ModelTraverser
+{
+private:
+    std::vector<std::shared_ptr<ModelTraverseHandler>> m_pNodeHandler;
+    SdDrawDocument* m_pDocument;
+
+public:
+    ModelTraverser(SdDrawDocument* pDocument)
+        : m_pDocument(pDocument)
+    {
+    }
+
+    void traverse()
+    {
+        if (!m_pDocument)
+            return;
+
+        for (sal_uInt16 nPage = 0; nPage < m_pDocument->GetPageCount(); ++nPage)
+        {
+            SdrPage* pPage = m_pDocument->GetPage(nPage);
+            if (pPage)
+            {
+                for (size_t nObject = 0; nObject < pPage->GetObjCount(); ++nObject)
+                {
+                    SdrObject* pObject = pPage->GetObj(nObject);
+                    if (pObject)
+                    {
+                        for (auto& pNodeHandler : m_pNodeHandler)
+                        {
+                            pNodeHandler->handleSdrObject(pObject);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    void addNodeHandler(std::shared_ptr<ModelTraverseHandler> pHandler)
+    {
+        m_pNodeHandler.push_back(pHandler);
+    }
+};
+}
+
+GraphicSizeViolation::GraphicSizeViolation(sal_Int32 nDPI, SdrGrafObj* pGraphicObject)
+    : m_pGraphicObject(pGraphicObject)
+{
+    constexpr double fLowPercentage = 110;
+    constexpr double fHighPercentage = 50;
+
+    m_nLowDPILimit = sal_Int32(100.0 / fLowPercentage * nDPI);
+    m_nHighDPILimit = sal_Int32(100.0 / fHighPercentage * nDPI);
+}
+
+bool GraphicSizeViolation::check()
+{
+    Graphic aGraphic = m_pGraphicObject->GetGraphic();
+    Size aSizePixel = aGraphic.GetSizePixel();
+    Size aGraphicSize = m_pGraphicObject->GetLogicRect().GetSize();
+
+    double nSizeXInch
+        = o3tl::convert(double(aGraphicSize.Width()), o3tl::Length::mm100, o3tl::Length::in);
+    double nSizeYInch
+        = o3tl::convert(double(aGraphicSize.Height()), o3tl::Length::mm100, o3tl::Length::in);
+
+    m_nDPIX = sal_Int32(aSizePixel.Width() / nSizeXInch);
+    m_nDPIY = sal_Int32(aSizePixel.Height() / nSizeYInch);
+
+    return isDPITooLow() || isDPITooHigh();
+}
+
+OUString GraphicSizeViolation::getGraphicName() { return m_pGraphicObject->GetName(); }
+
+namespace
+{
+class GraphicSizeCheckHandler : public ModelTraverseHandler
+{
+    sal_Int32 m_nDPI;
+    std::vector<std::unique_ptr<GraphicSizeViolation>>& m_rGraphicSizeViolationList;
+
+public:
+    GraphicSizeCheckHandler(
+        sal_Int32 nDPI,
+        std::vector<std::unique_ptr<GraphicSizeViolation>>& rGraphicSizeViolationList)
+        : m_nDPI(nDPI)
+        , m_rGraphicSizeViolationList(rGraphicSizeViolationList)
+    {
+    }
+
+    void handleSdrObject(SdrObject* pObject) override
+    {
+        auto* pGraphicObject = dynamic_cast<SdrGrafObj*>(pObject);
+        if (!pGraphicObject)
+            return;
+
+        auto pEntry = std::make_unique<GraphicSizeViolation>(m_nDPI, pGraphicObject);
+        if (pEntry->check())
+        {
+            m_rGraphicSizeViolationList.push_back(std::move(pEntry));
+        }
+    }
+};
+
+} // end anonymous namespace
+
+void GraphicSizeCheck::check()
+{
+    if (!m_pDocument)
+        return;
+
+    sal_Int32 nDPI = m_pDocument->getImagePreferredDPI();
+    if (nDPI == 0)
+        return;
+
+    auto pHandler = std::make_shared<GraphicSizeCheckHandler>(nDPI, m_aGraphicSizeViolationList);
+
+    ModelTraverser aModelTraverser(m_pDocument);
+    aModelTraverser.addNodeHandler(pHandler);
+    aModelTraverser.traverse();
+}
+
+OUString GraphicSizeCheckGUIEntry::getText()
+{
+    OUString sText;
+
+    if (m_pViolation->isDPITooLow())
+    {
+        sText = SdResId(STR_WARNING_GRAPHIC_PIXEL_COUNT_LOW);
+    }
+    else if (m_pViolation->isDPITooHigh())
+    {
+        sText = SdResId(STR_WARNING_GRAPHIC_PIXEL_COUNT_HIGH);
+    }
+
+    sText = sText.replaceAll("%NAME%", m_pViolation->getGraphicName());
+    sText = sText.replaceAll("%DPIX%", OUString::number(m_pViolation->getDPIX()));
+    sText = sText.replaceAll("%DPIY%", OUString::number(m_pViolation->getDPIY()));
+
+    return sText;
+}
+
+void GraphicSizeCheckGUIEntry::markObject()
+{
+    sd::ViewShell* pViewShell = m_pDocument->GetDocSh()->GetViewShell();
+    SdrView* pView = pViewShell->GetView();
+    pView->ShowSdrPage(m_pViolation->getObject()->getSdrPageFromSdrObject());
+    pView->UnmarkAll();
+    pView->MarkObj(m_pViolation->getObject(), pView->GetSdrPageView());
+}
+
+void GraphicSizeCheckGUIEntry::runProperties()
+{
+    markObject();
+    sd::ViewShell* pViewShell = m_pDocument->GetDocSh()->GetViewShell();
+    pViewShell->GetDispatcher()->Execute(SID_ATTR_GRAF_CROP, SfxCallMode::SYNCHRON);
+}
+
+GraphicSizeCheckGUIResult::GraphicSizeCheckGUIResult(SdDrawDocument* pDocument)
+{
+    GraphicSizeCheck aCheck(pDocument);
+    aCheck.check();
+
+    auto& rCollection = getCollection();
+    for (auto& rpViolation : aCheck.getViolationList())
+    {
+        auto rGUIEntry
+            = std::make_unique<GraphicSizeCheckGUIEntry>(pDocument, std::move(rpViolation));
+        rCollection.push_back(std::move(rGUIEntry));
+    }
+}
+
+OUString GraphicSizeCheckGUIResult::getTitle()
+{
+    return SdResId(STR_GRAPHIC_SIZE_CHECK_DIALOG_TITLE);
+}
+
+} // end of namespace sd
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
