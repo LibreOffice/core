edb5188a35358fa1c22bde02f1abceb3cbe8f848
Szymon Kłos <szymon.klos@collabora.com>
2021-12-09 09:41:50 +0100

    jsdialog: send info about initial focus in dialog
    Change-Id: I9daef497cb083e863d45a2acfd7eee3ee92bdc38
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/126565
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Mert Tumer <mert.tumer@collabora.com>

--- vcl/source/window/dialog.cxx
+++ vcl/source/window/dialog.cxx
@@ -1296,7 +1296,7 @@ void Dialog::ImplSetModalInputMode( bool bModal )
     }
 }
 
-void Dialog::GrabFocusToFirstControl()
+vcl::Window* Dialog::GetFirstControlForFocus()
 {
     vcl::Window* pFocusControl = nullptr;
     vcl::Window* pFirstOverlapWindow = ImplGetFirstOverlapWindow();
@@ -1320,6 +1320,13 @@ void Dialog::GrabFocusToFirstControl()
     {
         pFocusControl = ImplGetDlgWindow( 0, GetDlgWindowType::First );
     }
+
+    return pFocusControl;
+}
+
+void Dialog::GrabFocusToFirstControl()
+{
+    vcl::Window* pFocusControl = GetFirstControlForFocus();
     if ( pFocusControl )
         pFocusControl->ImplControlFocus( GetFocusFlags::Init );
 }
@@ -1695,6 +1702,10 @@ void Dialog::DumpAsPropertyTree(tools::JsonWriter& rJsonWriter)
             rJsonWriter.put("response", rResponse.second);
         }
     }
+
+    vcl::Window* pFocusControl = GetFirstControlForFocus();
+    if (pFocusControl)
+        rJsonWriter.put("init_focus_id", pFocusControl->get_id());
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
