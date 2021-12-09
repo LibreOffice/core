LibreOffice/core

commit fe6a140a537eda1b6703c44ff5ee49d2ba875b81

used cache value in ViewObjectContact::getPrimitive2DSequence
I'm not sure why we don't rely on this, it seems pretty obvious, we even
has an explicit flush method to invalidate it.

This takes the load time of a spreadsheet with a large chart from 40s to
20s.

Change-Id: I731246abb5e0c118116b70a0386b652798555503
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/126477
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svx/source/sdr/contact/viewobjectcontact.cxx
@@ -330,6 +330,9 @@ void ViewObjectContact::createPrimitive2DSequence(const DisplayInfo& rDisplayInf
 
 drawinglayer::primitive2d::Primitive2DContainer const & ViewObjectContact::getPrimitive2DSequence(const DisplayInfo& rDisplayInfo) const
 {
+    if (!mxPrimitive2DSequence.empty())
+        return mxPrimitive2DSequence;
+
     /**
     This method is weird because
     (1) we have to re-walk the primitive tree because the flushing is unreliable

commit 10108738f1278700f52b1fc971fa84f46235c171

tdf#146137 tdf#146132 image redrawing
regression from
    commit fe6a140a537eda1b6703c44ff5ee49d2ba875b81
    Author: Noel Grandin <noel.grandin@collabora.co.uk>
    Date:   Tue Dec 7 14:55:13 2021 +0200
    used cache value in ViewObjectContact::getPrimitive2DSequence

we need to blow away the cached data when the object changes

Change-Id: I3be8d57b166b6f7e3a04bd8fc34c83eed5ff8857

svx/source/sdr/contact/viewobjectcontact.cxx
@@ -208,6 +208,9 @@ const basegfx::B2DRange& ViewObjectContact::getObjectRange() const
 
 void ViewObjectContact::ActionChanged()
 {
+    // clear cached primitives
+    mxPrimitive2DSequence.clear();
+
     if(mbLazyInvalidate)
         return;
 