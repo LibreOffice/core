0ab9615391527119d87e4e27592f7a9d2c2c4098
Noel Grandin <noel.grandin@collabora.co.uk>
2021-12-09 11:45:36 +0200

    use background XShapeGroup for bar-chart area
    which means we don't have to set the Z-order, which is expensive for
    large charts.
    Shaves 5% off the load time
    
    Change-Id: I8c73df6ddc17a3cfedaa18dbe8ec3aeb768e3e42

--- chart2/source/view/inc/ShapeFactory.hxx
+++ chart2/source/view/inc/ShapeFactory.hxx
@@ -157,7 +157,8 @@ public:
 
     static SdrPathObj*
         createArea2D( const rtl::Reference<SvxShapeGroupAnyD>& xTarget
-                    , const css::drawing::PolyPolygonShape3D& rPolyPolygon);
+                    , const css::drawing::PolyPolygonShape3D& rPolyPolygon
+                    , bool bSetZOrderToZero = true);
 
     static rtl::Reference<SvxShapePolyPolygon>
         createSymbol2D( const rtl::Reference<SvxShapeGroupAnyD>& xTarget
