3ef0fa50aea77bbc5531e25bde9b5a4bc01fb6b9
Tor Lillqvist <tml@iki.fi>
2021-12-09 13:11:53 +0200

    Make the LibreOffice6FloatingPointMode thing work like in the master branch
    Add an Expert Configurtion setting to turn it on in addition to
    checking an environment variable.
    
    Don't expose the check as a Basic function.
    
    Change-Id: I6c7f3e85d63ce79f83a0747a00d05e14e559792f

--- basic/qa/basic_coverage/test_types_conversion.vb
+++ basic/qa/basic_coverage/test_types_conversion.vb
@@ -11,6 +11,21 @@ Dim nTotalCount As Integer
 Dim nPassCount As Integer
 Dim nFailCount As Integer
 
+' See LibreOffice6FloatingPointMode in basic/source/runtime/methods1.cxx
+Function LibreOffice6FloatingPointMode() As Boolean
+    Dim bMode As Boolean
+    bMode = Environ("LIBREOFFICE6FLOATINGPOINTMODE") <> ""
+    If (Not bMode) Then
+	Dim oConfigProvider As Object, aNodePath(0) As New com.sun.star.beans.PropertyValue, oRegistryKey As Object
+	oConfigProvider = createUnoService("com.sun.star.configuration.ConfigurationProvider")
+	aNodePath(0).Name = "nodepath"
+	aNodePath(0).Value = "org.openoffice.Office.Scripting/Basic/Compatibility"
+	oRegistryKey = oConfigProvider.createInstanceWithArguments("com.sun.star.configuration.ConfigurationAccess", aNodePath)
+	bMode = oRegistryKey.getPropertyValue("UseLibreOffice6FloatingPointConversion")
+    End If
+    LibreOffice6FloatingPointMode = bMode
+End Function
+
 ' For the following tests the en-US (English - United States) locale is required
 Function doUnitTest() As Integer
     nTotalCount = 0
