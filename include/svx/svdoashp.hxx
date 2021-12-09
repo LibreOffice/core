f79aa435eb3f3830612f88644e2e642d0d2fce5b
Noel Grandin <noel.grandin@collabora.co.uk>
2021-12-09 11:53:47 +0200

    merge setUnoShape and impl_setUnoShape
    since the one purely forwards to the other
    
    Change-Id: I5f614cc1eb819572a55da57d9d6c9ae56b9139c2

--- include/svx/svdoashp.hxx
+++ include/svx/svdoashp.hxx
@@ -80,7 +80,7 @@ private:
 
 protected:
     virtual std::unique_ptr<sdr::contact::ViewContact> CreateObjectSpecificViewContact() override;
-    virtual void impl_setUnoShape(const css::uno::Reference<css::drawing::XShape>& rxUnoShape) override;
+    virtual void setUnoShape(const css::uno::Reference<css::drawing::XShape>& rxUnoShape) override;
 
 public:
     virtual std::unique_ptr<sdr::properties::BaseProperties> CreateObjectSpecificProperties() override;
