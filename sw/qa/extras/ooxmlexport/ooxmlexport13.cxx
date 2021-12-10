LibreOffice/core

commit f51fa7534421a195a58b4a737a2e836d8c25ba81

tdf#145718 sw, DOCX import: complete tracked text moving
Add IsMoved bit to SwRangeRedline, and keep it in both
parts of a split Delete/Insert redline. Set this bit
during DOCX import, fixing incomplete import of
moveFrom/moveTo elements.

Details:

- Search text moving only at redline Insert() and AppendRedline()
  instead in the layout code (which was much slower, because
  triggered by also mouse hovering):

- detect text moving in Hide Changes mode, too;

- Insertion inside or directly after tracked text moving keeps
  "moved text" layout of the original moved text parts (before
  and after the insertion).

- at detection of text moving, invalidate (update) layout of the
  redline pair, too.

- fix DOCX import: extend makeRedline() with property RedlineMoved
  to keep all moveFrom/moveTo stored in DOCX instead of
  losing them (joining them with normal redlines) in the case
  of missing Delete/Insert pair (see unit test document);

Follow-up to commit ec577f566fa3e6d2666069180f8ec8474054aea9
"tdf#145233 sw track changes: show moved text in green color",
commit bcdebc832b272662d28035007a4796e42d1305ae
"tdf#104797 DOCX change tracking: handle moveFrom and moveTo"
and commit d32d9a2b3c5e3963f4a18f6c7bbf50fab2e9b2be
"tdf#123460 DOCX track changes: moveFrom completely".

Change-Id: Iaca80e5e326a172bc7ba5fec64b63668b9378e2d
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/125317
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/source/core/doc/docredln.cxx
@@ -435,6 +435,10 @@ bool SwRedlineTable::Insert(SwRangeRedline*& p)
         std::pair<vector_type::const_iterator, bool> rv = maVector.insert( p );
         size_type nP = rv.first - begin();
         LOKRedlineNotification(RedlineNotification::Add, p);
+
+        // set IsMoved checking nearby redlines
+        isMoved(nP);
+
         p->CallDisplayFunc(nP);
         if (rv.second)
             CheckOverlapping(rv.first);
@@ -769,7 +773,7 @@ const SwRangeRedline* SwRedlineTable::FindAtPosition( const SwPosition& rSttPos,
 bool SwRedlineTable::isMoved( size_type rPos ) const
 {
     auto constexpr nLookahead = 20;
-    const SwRangeRedline* pRedline = (*this)[ rPos ];
+    SwRangeRedline* pRedline = (*this)[ rPos ];
 
     // set redline type of the searched pair
     RedlineType nPairType = pRedline->GetType();
@@ -800,7 +804,7 @@ bool SwRedlineTable::isMoved( size_type rPos ) const
     rPos = rPos > nLookahead ? rPos - nLookahead : 0;
     for ( ; rPos < nEnd ; ++rPos )
     {
-        const SwRangeRedline* pPair = (*this)[ rPos ];
+        SwRangeRedline* pPair = (*this)[ rPos ];
         // TODO handle also Show Changes in Margin mode
         if ( pPair->HasMark() && pPair->IsVisible() )
         {
@@ -810,6 +814,9 @@ bool SwRedlineTable::isMoved( size_type rPos ) const
                 abs(pRedline->GetText().getLength() - pPair->GetText().getLength()) <= 2 &&
                 sTrimmed == pPair->GetText().trim() )
             {
+                pRedline->SetMoved();
+                pPair->SetMoved();
+                pPair->InvalidateRange(SwRangeRedline::Invalidation::Remove);
                 return true;
             }
         }
@@ -1073,6 +1080,7 @@ SwRangeRedline::SwRangeRedline(RedlineType eTyp, const SwPaM& rPam )
 {
     m_bDelLastPara = false;
     m_bIsVisible = true;
+    m_bIsMoved = false;
     if( !rPam.HasMark() )
         DeleteMark();
 }
@@ -1085,6 +1093,7 @@ SwRangeRedline::SwRangeRedline( const SwRedlineData& rData, const SwPaM& rPam )
 {
     m_bDelLastPara = false;
     m_bIsVisible = true;
+    m_bIsMoved = false;
     if( !rPam.HasMark() )
         DeleteMark();
 }
@@ -1097,6 +1106,7 @@ SwRangeRedline::SwRangeRedline( const SwRedlineData& rData, const SwPosition& rP
 {
     m_bDelLastPara = false;
     m_bIsVisible = true;
+    m_bIsMoved = false;
 }
 
 SwRangeRedline::SwRangeRedline( const SwRangeRedline& rCpy )
@@ -1107,6 +1117,9 @@ SwRangeRedline::SwRangeRedline( const SwRangeRedline& rCpy )
 {
     m_bDelLastPara = false;
     m_bIsVisible = true;
+    // inserting characters within a moved text must keep
+    // IsMoved bit in the second part of the split redline
+    m_bIsMoved = rCpy.IsMoved();
     if( !rCpy.HasMark() )
         DeleteMark();
 }
@@ -1827,7 +1840,8 @@ void SwRangeRedline::SetContentIdx( const SwNodeIndex* pIdx )
 bool SwRangeRedline::CanCombine( const SwRangeRedline& rRedl ) const
 {
     return  IsVisible() && rRedl.IsVisible() &&
-            m_pRedlineData->CanCombine( *rRedl.m_pRedlineData );
+            m_pRedlineData->CanCombine( *rRedl.m_pRedlineData ) &&
+            !IsMoved() && !rRedl.IsMoved();
 }
 
 void SwRangeRedline::PushData( const SwRangeRedline& rRedl, bool bOwnAsNext )

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -3039,6 +3039,7 @@ void DomainMapper_Impl::CreateRedline(uno::Reference<text::XTextRange> const& xR
     if ( !pRedline )
         return;
 
+    bool bRedlineMoved = false;
     try
     {
         OUString sType;
@@ -3048,10 +3049,13 @@ void DomainMapper_Impl::CreateRedline(uno::Reference<text::XTextRange> const& xR
             sType = getPropertyName( PROP_FORMAT );
             break;
         case XML_moveTo:
+            bRedlineMoved = true;
+            [[fallthrough]];
         case XML_ins:
             sType = getPropertyName( PROP_INSERT );
             break;
         case XML_moveFrom:
+            bRedlineMoved = true;
             m_pParaMarkerRedlineMoveFrom = pRedline.get();
             [[fallthrough]];
         case XML_del:
@@ -3063,7 +3067,7 @@ void DomainMapper_Impl::CreateRedline(uno::Reference<text::XTextRange> const& xR
         default:
             throw lang::IllegalArgumentException("illegal redline token type", nullptr, 0);
         }
-        beans::PropertyValues aRedlineProperties( 3 );
+        beans::PropertyValues aRedlineProperties( 4 );
         beans::PropertyValue * pRedlineProperties = aRedlineProperties.getArray(  );
         pRedlineProperties[0].Name = getPropertyName( PROP_REDLINE_AUTHOR );
         pRedlineProperties[0].Value <<= pRedline->m_sAuthor;
@@ -3071,6 +3075,9 @@ void DomainMapper_Impl::CreateRedline(uno::Reference<text::XTextRange> const& xR
         pRedlineProperties[1].Value <<= ConversionHelper::ConvertDateStringToDateTime( pRedline->m_sDate );
         pRedlineProperties[2].Name = getPropertyName( PROP_REDLINE_REVERT_PROPERTIES );
         pRedlineProperties[2].Value <<= pRedline->m_aRevertProperties;
+        pRedlineProperties[3].Name = "RedlineMoved";
+        pRedlineProperties[3].Value <<= bRedlineMoved;
+
         if (!m_bIsActualParagraphFramed)
         {
             uno::Reference < text::XRedline > xRedline( xRange, uno::UNO_QUERY_THROW );

sw/qa/extras/ooxmlexport/ooxmlexport13.cxx
@@ -767,16 +767,20 @@ DECLARE_OOXMLEXPORT_TEST(testTdf123460, "tdf123460.docx")
     CPPUNIT_ASSERT_EQUAL(OUString("Delete"),getProperty<OUString>(getRun(getParagraph(2), 1), "RedlineType"));
     CPPUNIT_ASSERT_EQUAL(true, getRun( getParagraph( 2 ), 2 )->getString().endsWith("tellus."));
     CPPUNIT_ASSERT_EQUAL( OUString( "" ), getRun( getParagraph( 2 ), 3 )->getString());
-    bool bCaught = false;
-    try
+    if (mbExported)
     {
-        getRun( getParagraph( 2 ), 4 );
+        // TODO fix export of moved text
+        bool bCaught = false;
+        try
+        {
+            getRun( getParagraph( 2 ), 4 );
+        }
+        catch (container::NoSuchElementException&)
+        {
+            bCaught = true;
+        }
+        CPPUNIT_ASSERT_EQUAL(true, bCaught);
     }
-    catch (container::NoSuchElementException&)
-    {
-        bCaught = true;
-    }
-    CPPUNIT_ASSERT_EQUAL(true, bCaught);
 }
 
 //tdf#125298: fix charlimit restrictions in bookmarknames and field references if they contain non-ascii characters

offapi/com/sun/star/text/XRedline.idl
@@ -46,6 +46,7 @@ published interface XRedline
                 [readonly, property] string RedlineAuthor;
                 [readonly, property] com::sun::star::util::DateTime RedlineDateTime;
                 [readonly, property] string RedlineComment;
+                [readonly, optional, property] boolean RedlineMoved;
         */
         void makeRedline( [in]string RedlineType, [in] com::sun::star::beans::PropertyValues  RedlineProperties)
                         raises( com::sun::star::lang::IllegalArgumentException );

sw/inc/redline.hxx
@@ -155,6 +155,7 @@ class SW_DLLPUBLIC SwRangeRedline final : public SwPaM
     SwNodeIndex* m_pContentSect;
     bool m_bDelLastPara : 1;
     bool m_bIsVisible : 1;
+    bool m_bIsMoved : 1;
     sal_uInt32 m_nId;
 
     std::optional<tools::Long> m_oLOKLastNodeTop;
@@ -174,7 +175,7 @@ public:
     SwRangeRedline(SwRedlineData* pData, const SwPosition& rPos,
                bool bDelLP) :
         SwPaM( rPos ), m_pRedlineData( pData ), m_pContentSect( nullptr ),
-        m_bDelLastPara( bDelLP ), m_bIsVisible( true ), m_nId( s_nLastId++ )
+        m_bDelLastPara( bDelLP ), m_bIsVisible( true ), m_bIsMoved( false ), m_nId( s_nLastId++ )
     {}
     SwRangeRedline( const SwRangeRedline& );
     virtual ~SwRangeRedline() override;
@@ -261,6 +262,9 @@ public:
     void dumpAsXml(xmlTextWriterPtr pWriter) const;
 
     void MaybeNotifyRedlinePositionModification(tools::Long nTop);
+
+    void SetMoved() { m_bIsMoved = true; }
+    bool IsMoved() const { return m_bIsMoved; }
 };
 
 void MaybeNotifyRedlineModification(SwRangeRedline& rRedline, SwDoc& rDoc);

sw/qa/extras/layout/layout2.cxx
@@ -330,6 +330,26 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter2, testRedlineMoving)
     assertXPath(pXmlDoc, "/metafile/push/push/push/font[@color='#008000']", 11);
 }
 
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter2, testRedlineMovingDOCX)
+{
+    SwDoc* pDoc = createSwDoc(DATA_DIRECTORY, "tdf104797.docx");
+    SwDocShell* pShell = pDoc->GetDocShell();
+
+    SwEditShell* const pEditShell(pDoc->GetEditShell());
+    // This was 2 (moveFrom and moveTo joined other redlines)
+    CPPUNIT_ASSERT_EQUAL(static_cast<SwRedlineTable::size_type>(6), pEditShell->GetRedlineCount());
+
+    // Dump the rendering of the first page as an XML file.
+    std::shared_ptr<GDIMetaFile> xMetaFile = pShell->GetPreviewMetaFile();
+    MetafileXmlDump dumper;
+    xmlDocUniquePtr pXmlDoc = dumpAndParse(dumper, *xMetaFile);
+    CPPUNIT_ASSERT(pXmlDoc);
+
+    // text colors show moved text
+    // These were 0 (other color, not COL_GREEN, color of the tracked text movement)
+    assertXPath(pXmlDoc, "/metafile/push/push/push/textcolor[@color='#008000']", 6);
+}
+
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter2, testTdf145225_RedlineMovingWithBadInsertion)
 {
     SwDoc* pDoc = createSwDoc(DATA_DIRECTORY, "tdf42748.fodt");

sw/qa/extras/ooxmlexport/ooxmlexport11.cxx
@@ -896,7 +896,25 @@ DECLARE_OOXMLEXPORT_TEST(testTdf104797, "tdf104797.docx")
     CPPUNIT_ASSERT(hasProperty(getRun(getParagraph(2), 3), "RedlineType"));
     CPPUNIT_ASSERT_EQUAL(OUString("Insert"),getProperty<OUString>(getRun(getParagraph(2), 3), "RedlineType"));
     CPPUNIT_ASSERT_EQUAL(true,getProperty<bool>(getRun(getParagraph(2), 3), "IsStart"));
-    CPPUNIT_ASSERT_EQUAL( OUString( " Will this sentence be duplicated ADDED STUFF?" ), getRun( getParagraph( 2 ), 4 )->getString());
+
+    if (mbExported)
+    {
+        // TODO fix export of moved text
+        CPPUNIT_ASSERT_EQUAL( OUString( " Will this sentence be duplicated ADDED STUFF?" ), getRun( getParagraph( 2 ), 4 )->getString());
+    }
+    else
+    {
+        CPPUNIT_ASSERT_EQUAL( OUString( " " ), getRun( getParagraph( 2 ), 4 )->getString());
+        CPPUNIT_ASSERT_EQUAL( OUString( "" ), getRun( getParagraph( 2 ), 5 )->getString());
+        CPPUNIT_ASSERT(hasProperty(getRun(getParagraph(2), 5), "RedlineType"));
+        CPPUNIT_ASSERT_EQUAL(OUString("Insert"),getProperty<OUString>(getRun(getParagraph(2), 5), "RedlineType"));
+        CPPUNIT_ASSERT_EQUAL( OUString( "" ), getRun( getParagraph( 2 ), 6 )->getString());
+        CPPUNIT_ASSERT(hasProperty(getRun(getParagraph(2), 6), "RedlineType"));
+        CPPUNIT_ASSERT_EQUAL(true,getProperty<bool>(getRun(getParagraph(2), 6), "IsStart"));
+        CPPUNIT_ASSERT_EQUAL( OUString( "Will this sentence be duplicated" ), getRun( getParagraph( 2 ), 7 )->getString());
+        CPPUNIT_ASSERT_EQUAL( OUString( " ADDED STUFF" ), getRun( getParagraph( 2 ), 10 )->getString());
+        CPPUNIT_ASSERT_EQUAL( OUString( "?" ), getRun( getParagraph( 2 ), 13 )->getString());
+    }
 }
 
 DECLARE_OOXMLEXPORT_TEST(testTdf143510, "TC-table-DnD-move.docx")

commit 512aa3767b34c5d03953ebc2bbd21cb6149156a8

tdf#146140 sw DOCX import: fix moveFrom regression with broken text content
(Also a small clean-up: increase the character limit for tracked text
moving detection: Only 2 or more (non-whitespace) character deletions
are checked for it, because single characters are often typos or some
control-like characters, e.g. soft hyphen, not real text movings.)

Details of the regression: commit d32d9a2b3c5e3963f4a18f6c7bbf50fab2e9b2be
"tdf#123460 DOCX track changes: moveFrom completely" fixed
the missing redline import of the end of the moved paragraphs,
but paragraph end was imported as w:del, not w:moveFrom explicitly.
From commit f51fa7534421a195a58b4a737a2e836d8c25ba81
"tdf#145718 sw, DOCX import: complete tracked text moving"
this resulted two deletions (a moved one and a plain one) instead of
the previous single one.

Moreover, exporting these double deletions at the same position to
ODT, raised a back-compatibility issue with broken text content, see
tdf#107292 (solved recently, but not in older LibreOffice versions).

Removing the explicit w:del code path in writerfilter, it solved
the regression from commit f51fa7534421a195a58b4a737a2e836d8c25ba81
"tdf#145718 sw, DOCX import: complete tracked text moving".

See also commit 9e1e88ad5cf2dc0e9b188c60930445652a6c7519
"tdf#145720 DOCX export: fix loss of tracked moving".

Change-Id: I15bfc83b87dd42a762ff84edf5bae765fe02a5ae

sw/source/core/doc/docredln.cxx
@@ -802,7 +802,7 @@ bool SwRedlineTable::isMoved( size_type rPos ) const
     }
 
     const OUString sTrimmed = pPaM->GetText().trim();
-    if ( sTrimmed.isEmpty() )
+    if ( sTrimmed.getLength() < 2 )
     {
         if ( bDeletePaM )
             delete pPaM;

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -3119,7 +3119,6 @@ void DomainMapper_Impl::CheckParaMarkerRedline( uno::Reference< text::XTextRange
     else if ( m_pParaMarkerRedlineMoveFrom )
     {
         // terminating moveFrom redline removes also the paragraph mark
-        m_pParaMarkerRedlineMoveFrom->m_nToken = XML_del;
         CreateRedline( xRange, m_pParaMarkerRedlineMoveFrom );
     }
     if ( m_pParaMarkerRedlineMoveFrom )

sw/qa/extras/ooxmlexport/ooxmlexport13.cxx
@@ -766,10 +766,37 @@ DECLARE_OOXMLEXPORT_TEST(testTdf123460, "tdf123460.docx")
     CPPUNIT_ASSERT(hasProperty(getRun(getParagraph(2), 2), "RedlineType"));
     CPPUNIT_ASSERT_EQUAL(OUString("Delete"),getProperty<OUString>(getRun(getParagraph(2), 2), "RedlineType"));
     CPPUNIT_ASSERT_EQUAL(true, getRun( getParagraph( 2 ), 3 )->getString().endsWith("tellus."));
+    CPPUNIT_ASSERT(hasProperty(getRun(getParagraph(2), 4), "Bookmark"));
     // deleted paragraph mark at the end of the second paragraph
-    CPPUNIT_ASSERT(hasProperty(getRun(getParagraph(2), 5), "RedlineType"));
-    CPPUNIT_ASSERT_EQUAL(OUString("Delete"),getProperty<OUString>(getRun(getParagraph(2), 5), "RedlineType"));
-    CPPUNIT_ASSERT_EQUAL( OUString( "" ), getRun( getParagraph( 2 ), 6 )->getString());
+    if (mbExported)
+    {
+        // there is no run after the MoveBookmark
+        bool bCaught = false;
+        try
+        {
+            getRun( getParagraph( 2 ), 5 );
+        }
+        catch (container::NoSuchElementException&)
+        {
+            bCaught = true;
+        }
+        CPPUNIT_ASSERT_EQUAL(true, bCaught);
+    }
+}
+
+CPPUNIT_TEST_FIXTURE(Test, testTdf146140)
+{
+    loadAndSave("tdf123460.docx");
+    CPPUNIT_ASSERT_EQUAL(1, getPages());
+    xmlDocUniquePtr pXmlDoc = parseExport("word/document.xml");
+    CPPUNIT_ASSERT(pXmlDoc);
+
+    // This was 1 (put end of paragraph of the previous moveFrom into a w:del,
+    // resulting double deletions at the same position, which is an
+    // ODT back-compatibility issue described in tdf#107292)
+    assertXPath(pXmlDoc, "/w:document/w:body/w:p[2]/w:pPr/w:rPr/w:del", 0);
+    // This was 0
+    assertXPath(pXmlDoc, "/w:document/w:body/w:p[2]/w:pPr/w:rPr/w:moveFrom", 1);
 }
 
 //tdf#125298: fix charlimit restrictions in bookmarknames and field references if they contain non-ascii characters