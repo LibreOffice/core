87df02a938ef0b8ad668de6028fa9ee3f7bc7b0f
Caolán McNamara <caolanm@redhat.com>
2021-12-14 14:06:28 +0000

    tdf#145999 -1 was the representation of unlimited cell width
    when using UniString -1 -> 0xFFFF i.e. old-time EDIT_NOLIMIT
    
    Change-Id: Ie40fa65a88a2b8961d70635ca5e4ec851567e089

--- svx/source/fmcomp/gridcell.cxx
+++ svx/source/fmcomp/gridcell.cxx
@@ -1175,12 +1175,11 @@ void DbTextField::updateFromModel( Reference< XPropertySet > _rxModel )
     _rxModel->getPropertyValue( FM_PROP_TEXT ) >>= sText;
 
     sal_Int32 nMaxTextLen = m_pEdit->GetMaxTextLen();
-    if (nMaxTextLen != 0 && sText.getLength() > nMaxTextLen)
+    if (nMaxTextLen > 0 && sText.getLength() > nMaxTextLen)
     {
         sal_Int32 nDiff = sText.getLength() - nMaxTextLen;
         sText = sText.replaceAt(sText.getLength() - nDiff,nDiff, OUString());
     }
-
 
     m_pEdit->SetText( sText );
     m_pEdit->SetSelection( Selection( SELECTION_MAX, SELECTION_MIN ) );
@@ -1191,7 +1190,7 @@ bool DbTextField::commitControl()
     OUString aText( m_pEdit->GetText( getModelLineEndSetting( m_rColumn.getModel() ) ) );
     // we have to check if the length before we can decide if the value was modified
     sal_Int32 nMaxTextLen = m_pEdit->GetMaxTextLen();
-    if (nMaxTextLen != 0)
+    if (nMaxTextLen > 0)
     {
         OUString sOldValue;
         m_rColumn.getModel()->getPropertyValue( FM_PROP_TEXT ) >>= sOldValue;
