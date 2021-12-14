LibreOffice/core

commit 27aa4b16bf704d0246595750daf57b57ff2577b3

(related: tdf#130685) sw: fix ~SwIndexReg asserts from ...
... lcl_DeleteRedlines(), if there are redlines immediately consecutive
that get deleted; merge the SwPaM ring in that case which should help.

Change-Id: I07b67413ad6ee52bc81b89facc40202d0ca75e2a
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/91595
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@cib.de>

sw/source/core/doc/DocumentContentOperationsManager.cxx
@@ -384,6 +384,12 @@ namespace
                                 lcl_SetCpyPos( *pREnd, *pStt, *pCpyStt,
                                                 *pDelPam->GetPoint(), nDelCount );
                             }
+
+                            if (pDelPam->GetNext() && *pDelPam->GetNext()->End() == *pDelPam->Start())
+                            {
+                                *pDelPam->GetNext()->End() = *pDelPam->End();
+                                pDelPam.reset(pDelPam->GetNext());
+                            }
                         }
                     }
                 }

commit df3f4e2b847e5cc7c1c544be67f7eac48fcc8431

tdf#141175 sw_redlinehide: fix crash in lcl_DeleteRedlines()
The GetNext() returns itself if it's alone in the Ring.

(regression from 27aa4b16bf704d0246595750daf57b57ff2577b3)

Change-Id: Idf92ce5362c4a4acf1582f463fe7bafc8553b400

sw/source/core/doc/DocumentContentOperationsManager.cxx
@@ -408,7 +408,8 @@ namespace
                                             *pDelPam->GetPoint(), nDelCount );
                         }
 
-                        if (pDelPam->GetNext() && *pDelPam->GetNext()->End() == *pDelPam->Start())
+                        if (pDelPam->GetNext() != pDelPam.get()
+                            && *pDelPam->GetNext()->End() == *pDelPam->Start())
                         {
                             *pDelPam->GetNext()->End() = *pDelPam->End();
                             pDelPam.reset(pDelPam->GetNext());