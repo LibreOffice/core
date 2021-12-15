02b8bcf06c431ea4694d9fe13d6aa59100c79c05
László Németh <nemeth@numbertext.org>
2021-12-15 12:31:27 +0100

    tdf#146244 sw: update HasTextChangesOnly in modified rows
    It's not forbidden to write in rows deleted or
    inserted with change tracking, also Accept/Reject
    only part of the text changes here. Improve to
    handle these in SwTableLine::UpdateTextChangesOnly()
    by keeping property HasTextChangesOnly = false
    (tracked row change) only if
    
    1) there is an insert redline, which is the oldest redline in
       the row (tracked row insertion) or
    
    2) there is a delete redline, which is the newest redline in
       the row, and no text outside of redlines, and no insert
       redline in the row, i.e. whole text content is deleted
       (tracked row deletion).
    
    Also update HasTextChangesOnly table row property at accepting
    SwRangeRedlines in changed table rows.
    
    And follow-up bug fix commits of this:
    commit bc206f7fd3b4a9e843c81f39dcd2faeeff9c785a
    "cid#1495785 silence Unchecked return value" and
    commit 213173edc5e47bb6da11f9a301f4dca71854a48c
    "sw: fix crash with commit 8c028b7e41e3d350d0e67005b16faf0159cc5c12".
    
    (cherry-picked from commit 8c028b7e41e3d350d0e67005b16faf0159cc5c12,
    commit bc206f7fd3b4a9e843c81f39dcd2faeeff9c785a and
    commit 213173edc5e47bb6da11f9a301f4dca71854a48c)
    
    Change-Id: I2bc556c732e1d0fdca24678c79351aafbfc156b2
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/127195
    Tested-by: László Németh <nemeth@numbertext.org>
    Reviewed-by: László Németh <nemeth@numbertext.org>

--- sw/source/core/docnode/ndtbl1.cxx
+++ sw/source/core/docnode/ndtbl1.cxx
@@ -573,7 +573,9 @@ void SwDoc::SetRowNotTracked( const SwCursor& rCursor, const SvxPrintItem &rNew,
         // add a redline with invisible text CH_TXT_TRACKED_DUMMY_CHAR
         // (unless the table is part of a bigger deletion, where the
         // new redline can cause a problem)
-        if (!bAll && pLn->IsEmpty())
+        if ( !bAll &&
+            // HasTextChangesOnly == false, i.e. a tracked row insertion or deletion
+            !rNew.GetValue() && pLn->IsEmpty() )
         {
             SwNodeIndex aInsPos( *(pLn->GetTabBoxes()[0]->GetSttNd()), 1 );
             RedlineFlags eOld = getIDocumentRedlineAccess().GetRedlineFlags();
