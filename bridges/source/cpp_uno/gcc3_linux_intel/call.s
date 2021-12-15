0dd8cf11fe79e116fb3a8758cecf1438c6b45f1c
Caolán McNamara <caolanm@redhat.com>
2021-12-15 12:46:24 +0000

    annocheck warning about missing .note.gnu.property section
    copy and paste recommendation from:
    https: //sourceware.org/annobin/annobin.html/Test-cf-protection.html
    https://bugs.ruby-lang.org/attachments/8962 is similar
    Change-Id: I637e9efbbb861916ffdeafba58670a446a65fd1c
    
    Change-Id: Ie867c263a888763db4478720ba189c9ec6cc974d

--- bridges/source/cpp_uno/gcc3_linux_intel/call.s
+++ bridges/source/cpp_uno/gcc3_linux_intel/call.s
@@ -290,3 +290,19 @@ privateSnippetExecutorClass:
     .align 4
 .LEFDEc:
     .section .note.GNU-stack,"",@progbits
+    .section .note.gnu.property,"a"
+    .align 8
+    .long     1f - 0f
+    .long     4f - 1f
+    .long     5
+0:
+    .string     "GNU"
+1:
+    .align 8
+    .long     0xc0000002
+    .long     3f - 2f
+2:
+    .long     0x3
+3:
+    .align 8
+4:
--- bridges/source/cpp_uno/gcc3_linux_x86-64/call.s
+++ bridges/source/cpp_uno/gcc3_linux_x86-64/call.s
@@ -121,3 +121,19 @@ privateSnippetExecutor:
 	.align 8
 .LEFDE1:
 	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
