1a038e6ee1c29eafb267e035104b2a2a05714f15
Xisco Fauli <xiscofauli@libreoffice.org>
2021-12-15 13:51:53 +0100

    uitest: sc: fix problematic test
    Change-Id: I3a445f48930e1d6c6f8d59d392bbf87cc9cb2cf6
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/126858
    Tested-by: Jenkins
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- sc/qa/uitest/conditional_format/tdf105466.py
+++ sc/qa/uitest/conditional_format/tdf105466.py
@@ -11,20 +11,22 @@ from uitest.framework import UITestCase
 from libreoffice.uno.propertyvalue import mkPropertyValues
 from uitest.uihelper.common import select_pos
 
-import unittest
-
 class tdf105466(UITestCase):
 
-    @unittest.skip("issue with floating windows")
     def test_changing_conditional_format(self):
 
         with self.ui_test.create_doc_in_start_center("calc"):
 
-            with self.ui_test.execute_modeless_dialog_through_command(".uno:ConditionalFormatDialog") as xCondFormatDlg:
+            with self.ui_test.execute_modeless_dialog_through_command(".uno:ConditionalFormatDialog", close_button="") as xCondFormatDlg:
 
                 for i in range(0,4):
-                    with self.subTest(i = i):
-                        xTypeLstBox = xCondFormatDlg.getChild("type")
-                        select_pos(xTypeLstBox, str(i))
+                    xTypeLstBox = xCondFormatDlg.getChild("type")
+                    select_pos(xTypeLstBox, str(i))
+
+                    # After changing the type, the dialog is recalculated
+                    xCondFormatDlg = self.xUITest.getTopFocusWindow()
+
+                xOkBtn = xCondFormatDlg.getChild("ok")
+                self.ui_test.close_dialog_through_button(xOkBtn)
 
 # vim: set shiftwidth=4 softtabstop=4 expandtab:
