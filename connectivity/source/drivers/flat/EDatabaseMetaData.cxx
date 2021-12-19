LibreOffice/core

commit e01786898406130aa81eadc32f7bc2fad65c5344

Directly initialize vector in connectivity (part 3)
Change-Id: Ie7f74a3f0fa865994057575b3dfac2bc209c6793
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/115631
Tested-by: Jenkins
Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

connectivity/source/drivers/flat/EDatabaseMetaData.cxx
@@ -53,28 +53,29 @@ Reference< XResultSet > OFlatDatabaseMetaData::impl_getTypeInfo_throw(  )
 
     static ODatabaseMetaDataResultSet::ORows aRows = [&]()
     {
-        ODatabaseMetaDataResultSet::ORows tmp;
-        ODatabaseMetaDataResultSet::ORow aRow;
-
-        aRow.push_back(ODatabaseMetaDataResultSet::getEmptyValue());
-        aRow.push_back(new ORowSetValueDecorator(OUString("CHAR")));
-        aRow.push_back(new ORowSetValueDecorator(DataType::CHAR));
-        aRow.push_back(new ORowSetValueDecorator(sal_Int32(254)));
-        aRow.push_back(ODatabaseMetaDataResultSet::getQuoteValue());
-        aRow.push_back(ODatabaseMetaDataResultSet::getQuoteValue());
-        aRow.push_back(ODatabaseMetaDataResultSet::getEmptyValue());
-        aRow.push_back(new ORowSetValueDecorator(sal_Int32(ColumnValue::NULLABLE)));
-        aRow.push_back(ODatabaseMetaDataResultSet::get1Value());
-        aRow.push_back(new ORowSetValueDecorator(sal_Int32(ColumnSearch::CHAR)));
-        aRow.push_back(ODatabaseMetaDataResultSet::get1Value());
-        aRow.push_back(ODatabaseMetaDataResultSet::get0Value());
-        aRow.push_back(ODatabaseMetaDataResultSet::get0Value());
-        aRow.push_back(ODatabaseMetaDataResultSet::getEmptyValue());
-        aRow.push_back(ODatabaseMetaDataResultSet::get0Value());
-        aRow.push_back(ODatabaseMetaDataResultSet::get0Value());
-        aRow.push_back(ODatabaseMetaDataResultSet::getEmptyValue());
-        aRow.push_back(ODatabaseMetaDataResultSet::getEmptyValue());
-        aRow.push_back(new ORowSetValueDecorator(sal_Int32(10)));
+        ODatabaseMetaDataResultSet::ORows tmp(10);
+        ODatabaseMetaDataResultSet::ORow aRow
+        {
+            { ODatabaseMetaDataResultSet::getEmptyValue() },
+            { new ORowSetValueDecorator(OUString("CHAR")) },
+            { new ORowSetValueDecorator(DataType::CHAR) },
+            { new ORowSetValueDecorator(sal_Int32(254)) },
+            { ODatabaseMetaDataResultSet::getQuoteValue() },
+            { ODatabaseMetaDataResultSet::getQuoteValue() },
+            { ODatabaseMetaDataResultSet::getEmptyValue() },
+            { new ORowSetValueDecorator(sal_Int32(ColumnValue::NULLABLE)) },
+            { ODatabaseMetaDataResultSet::get1Value() },
+            { new ORowSetValueDecorator(sal_Int32(ColumnSearch::CHAR)) },
+            { ODatabaseMetaDataResultSet::get1Value() },
+            { ODatabaseMetaDataResultSet::get0Value() },
+            { ODatabaseMetaDataResultSet::get0Value() },
+            { ODatabaseMetaDataResultSet::getEmptyValue() },
+            { ODatabaseMetaDataResultSet::get0Value() },
+            { ODatabaseMetaDataResultSet::get0Value() },
+            { ODatabaseMetaDataResultSet::getEmptyValue() },
+            { ODatabaseMetaDataResultSet::getEmptyValue() },
+            { new ORowSetValueDecorator(sal_Int32(10)) }
+        };
 
         tmp.push_back(aRow);
 

connectivity/source/drivers/flat/EDriver.cxx
@@ -82,47 +82,48 @@ Sequence< DriverPropertyInfo > SAL_CALL ODriver::getPropertyInfo( const OUString
 {
     if ( acceptsURL(url) )
     {
-        std::vector< DriverPropertyInfo > aDriverInfo;
-
         Sequence< OUString > aBoolean(2);
         aBoolean[0] = "0";
         aBoolean[1] = "1";
 
-        aDriverInfo.push_back(DriverPropertyInfo(
+        std::vector< DriverPropertyInfo > aDriverInfo
+        {
+            { DriverPropertyInfo(
                 "FieldDelimiter"
                 ,"Field separator."
                 ,false
                 ,OUString()
                 ,Sequence< OUString >())
-                );
-        aDriverInfo.push_back(DriverPropertyInfo(
+            },
+            { DriverPropertyInfo(
                 "HeaderLine"
                 ,"Text contains headers."
                 ,false
                 ,"0"
                 ,aBoolean)
-                );
-        aDriverInfo.push_back(DriverPropertyInfo(
+            },
+            { DriverPropertyInfo(
                 "StringDelimiter"
                 ,"Text separator."
                 ,false
                 ,"0"
                 ,aBoolean)
-                );
-        aDriverInfo.push_back(DriverPropertyInfo(
+            },
+            { DriverPropertyInfo(
                 "DecimalDelimiter"
                 ,"Decimal separator."
                 ,false
                 ,"0"
                 ,aBoolean)
-                );
-        aDriverInfo.push_back(DriverPropertyInfo(
+            },
+            { DriverPropertyInfo(
                 "ThousandDelimiter"
                 ,"Thousands separator."
                 ,false
                 ,"0"
                 ,aBoolean)
-                );
+            }
+        };
         return ::comphelper::concatSequences(OFileDriver::getPropertyInfo(url,info ),
                                              Sequence< DriverPropertyInfo >(aDriverInfo.data(),aDriverInfo.size()));
     }

connectivity/source/drivers/jdbc/JDriver.cxx
@@ -108,103 +108,104 @@ Sequence< DriverPropertyInfo > SAL_CALL java_sql_Driver::getPropertyInfo( const 
 {
     if ( acceptsURL(url) )
     {
-        std::vector< DriverPropertyInfo > aDriverInfo;
-
         Sequence< OUString > aBooleanValues(2);
         aBooleanValues[0] = "false";
         aBooleanValues[1] = "true";
 
-        aDriverInfo.push_back(DriverPropertyInfo(
+        std::vector< DriverPropertyInfo > aDriverInfo
+        {
+            { DriverPropertyInfo(
                 "JavaDriverClass"
                 ,"The JDBC driver class name."
                 ,true
                 ,OUString()
                 ,Sequence< OUString >())
-        );
-        aDriverInfo.push_back(DriverPropertyInfo(
+            },
+            { DriverPropertyInfo(
                 "JavaDriverClassPath"
                 ,"The class path where to look for the JDBC driver."
                 ,true
                 , ""
                 ,Sequence< OUString >())
-        );
-        aDriverInfo.push_back(DriverPropertyInfo(
+            },
+            { DriverPropertyInfo(
                 "SystemProperties"
                 ,"Additional properties to set at java.lang.System before loading the driver."
                 ,true
                 , ""
                 ,Sequence< OUString >())
-        );
-        aDriverInfo.push_back(DriverPropertyInfo(
+            },
+            { DriverPropertyInfo(
                 "ParameterNameSubstitution"
                 ,"Change named parameters with '?'."
                 ,false
                 ,"false"
                 ,aBooleanValues)
-        );
-        aDriverInfo.push_back(DriverPropertyInfo(
+            },
+            { DriverPropertyInfo(
                 "IgnoreDriverPrivileges"
                 ,"Ignore the privileges from the database driver."
                 ,false
                 , "false"
                 ,aBooleanValues)
-        );
-        aDriverInfo.push_back(DriverPropertyInfo(
+            },
+            { DriverPropertyInfo(
                 "IsAutoRetrievingEnabled"
                 ,"Retrieve generated values."
                 ,false
                 ,"false"
                 ,aBooleanValues)
-        );
-        aDriverInfo.push_back(DriverPropertyInfo(
+            },
+            { DriverPropertyInfo(
                 "AutoRetrievingStatement"
                 ,"Auto-increment statement."
                 ,false
                 ,OUString()
                 ,Sequence< OUString >())
-        );
-        aDriverInfo.push_back(DriverPropertyInfo(
+            },
+            { DriverPropertyInfo(
                 "GenerateASBeforeCorrelationName"
                 ,"Generate AS before table correlation names."
                 ,false
                 ,"false"
                 ,aBooleanValues)
-        );
-        aDriverInfo.push_back(DriverPropertyInfo(
+            },
+            { DriverPropertyInfo(
                 "IgnoreCurrency"
                 ,"Ignore the currency field from the ResultsetMetaData."
                 ,false
                 ,"false"
                 ,aBooleanValues)
-        );
-        aDriverInfo.push_back(DriverPropertyInfo(
+            },
+            { DriverPropertyInfo(
                 "EscapeDateTime"
                 ,"Escape date time format."
                 ,false
                 ,"true"
                 ,aBooleanValues)
-        );
-        aDriverInfo.push_back(DriverPropertyInfo(
+            },
+            { DriverPropertyInfo(
                 "TypeInfoSettings"
                 ,"Defines how the type info of the database metadata should be manipulated."
                 ,false
-                ,OUString( )
+                ,OUString()
                 ,Sequence< OUString > ())
-        );
-        aDriverInfo.push_back(DriverPropertyInfo(
+            },
+            { DriverPropertyInfo(
                 "ImplicitCatalogRestriction"
                 ,"The catalog which should be used in getTables calls, when the caller passed NULL."
                 ,false
                 ,OUString( )
                 ,Sequence< OUString > ())
-        );
-        aDriverInfo.push_back(DriverPropertyInfo(
+            },
+            { DriverPropertyInfo(
                 "ImplicitSchemaRestriction"
                 ,"The schema which should be used in getTables calls, when the caller passed NULL."
                 ,false
                 ,OUString( )
                 ,Sequence< OUString > ())
-        );
+            }
+        };
         return Sequence< DriverPropertyInfo >(aDriverInfo.data(),aDriverInfo.size());
     }
     ::connectivity::SharedResources aResources;

commit a146e5c56a2b53ddf8ebb565a7ec8764a6b5a870

Regression from e01786898406130aa81eadc32f7bc2fad65c5344 author	Julien Nabet <serval2412@yahoo.fr>	2021-05-14 18:46:57 +0200 committer	Julien Nabet <serval2412@yahoo.fr>	2021-05-14 23:09:11 +0200 commit	e01786898406130aa81eadc32f7bc2fad65c5344 (patch) tree	6852a41ac21a081114f51f5c0272249eb5f962d6 parent	be96aa21aed3069775609780566541b3631cbbe1 (diff) Directly initialize vector in connectivity (part 3)
Change-Id: Ib4097b3425e07de73abbc47ffefebb06b1458308

connectivity/source/drivers/flat/EDatabaseMetaData.cxx
@@ -53,7 +53,7 @@ Reference< XResultSet > OFlatDatabaseMetaData::impl_getTypeInfo_throw(  )
 
     static ODatabaseMetaDataResultSet::ORows aRows = []()
     {
-        ODatabaseMetaDataResultSet::ORows tmp(10);
+        ODatabaseMetaDataResultSet::ORows tmp;
         ODatabaseMetaDataResultSet::ORow aRow
         {
              ODatabaseMetaDataResultSet::getEmptyValue() ,