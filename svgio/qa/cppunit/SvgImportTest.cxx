LibreOffice/core

commit 701324a1e1f7e0c181ff1a50956ced686785ea53

SVGIO: tdf#45771: Fix issue when text size is twice its size...
... when using relative units (em, ex)

This is because for nodes of type textnode, getFontSizeNumber
is called from SvgCharacterNode::createSimpleTextPrimitive
and from SvgNode::getCurrentFontSize(), so avoid the second call.

Change-Id: Ibd418708a572e3c1643164a900fac7e7481afe81
Reviewed-on: https://gerrit.libreoffice.org/21801
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noelgrandin@gmail.com>

svgio/source/svgreader/svgtextnode.cxx
@@ -260,6 +260,11 @@ namespace svgio
                 }
             }
         }
+
+        double SvgTextNode::getCurrentFontSize() const
+        {
+            return getCurrentFontSizeInherited();
+        }
     } // end of namespace svgreader
 } // end of namespace svgio
 

svgio/inc/svgio/svgreader/svgnode.hxx
@@ -160,7 +160,7 @@ namespace svgio
             virtual double getCurrentFontSizeInherited() const override;
             virtual double getCurrentXHeightInherited() const override;
 
-            double getCurrentFontSize() const;
+            virtual double getCurrentFontSize() const;
             double getCurrentXHeight() const;
 
             /// Id access

svgio/inc/svgio/svgreader/svgtextnode.hxx
@@ -58,6 +58,8 @@ namespace svgio
             virtual void parseAttribute(const OUString& rTokenName, SVGToken aSVGToken, const OUString& aContent) override;
             virtual void decomposeSvgNode(drawinglayer::primitive2d::Primitive2DContainer& rTarget, bool bReferenced) const override;
 
+            virtual double getCurrentFontSize() const override;
+
             /// transform content, set if found in current context
             const basegfx::B2DHomMatrix* getTransform() const { return mpaTransform; }
             void setTransform(const basegfx::B2DHomMatrix* pMatrix = nullptr) { if(mpaTransform) delete mpaTransform; mpaTransform = nullptr; if(pMatrix) mpaTransform = new basegfx::B2DHomMatrix(*pMatrix); }

commit d189772bbe6b3bbff574e04449cf8bc3810eb753

tdf#97663 SVGIO: Fix line spacing for <tspan>
tdf#97663 is a regression caused by the commit
701324a1e1f7e0c181ff1a50956ced686785ea53. The previous patch caused
LO to forget the size of the font which was needed to calculate line
height based on em units.

em, px, pt, cm, in...
https://www.w3.org/Style/Examples/007/units.en.html

Accompanied with this fix is a unit test provided to avoid this
issue in the future.

The fix can be tested with:

    make CPPUNIT_TEST_NAME="testTdf97663" -sr \
        CppunitTest_svgio

The em_units.svg is opened with Firefox, Chrome and Inkscape and the
rendering in LibreOffice is compatible with the rendering in these
applications.

Change-Id: Idaecd9fb18101f7925fe2a917f7fc3fe7257ebc5

svgio/inc/svgnode.hxx
@@ -159,7 +159,7 @@ namespace svgio::svgreader
             virtual double getCurrentFontSizeInherited() const override;
             virtual double getCurrentXHeightInherited() const override;
 
-            virtual double getCurrentFontSize() const;
+            double getCurrentFontSize() const;
             double getCurrentXHeight() const;
 
             /// Id access

svgio/inc/svgtextnode.hxx
@@ -58,8 +58,6 @@ namespace svgio::svgreader
             virtual void parseAttribute(const OUString& rTokenName, SVGToken aSVGToken, const OUString& aContent) override;
             virtual void decomposeSvgNode(drawinglayer::primitive2d::Primitive2DContainer& rTarget, bool bReferenced) const override;
 
-            virtual double getCurrentFontSize() const override;
-
             /// transform content, set if found in current context
             const std::optional<basegfx::B2DHomMatrix>& getTransform() const { return mpaTransform; }
             void setTransform(const std::optional<basegfx::B2DHomMatrix>& pMatrix) { mpaTransform = pMatrix; }

svgio/inc/svgtspannode.hxx
@@ -42,7 +42,7 @@ namespace svgio::svgreader
             virtual const SvgStyleAttributes* getSvgStyleAttributes() const override;
             virtual void parseAttribute(const OUString& rTokenName, SVGToken aSVGToken, const OUString& aContent) override;
 
-            virtual double getCurrentFontSize() const override;
+            double getCurrentFontSize() const;
 
             /// access to SvgTextPositions
             const SvgTextPositions& getSvgTextPositions() const { return maSvgTextPositions; }

svgio/source/svgreader/svgtextnode.cxx
@@ -255,10 +255,6 @@ namespace svgio::svgreader
             }
         }
 
-        double SvgTextNode::getCurrentFontSize() const
-        {
-            return getCurrentFontSizeInherited();
-        }
 } // end of namespace svgio::svgreader
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

svgio/qa/cppunit/SvgImportTest.cxx
@@ -69,6 +69,7 @@ class Test : public test::BootstrapFixture, public XmlTestTools
     void testTdf101237();
     void testTdf94765();
     void testBehaviourWhenWidthAndHeightIsOrIsNotSet();
+    void testTdf97663();
 
     Primitive2DSequence parseSvg(std::u16string_view aSource);
 
@@ -104,6 +105,7 @@ public:
     CPPUNIT_TEST(testTdf101237);
     CPPUNIT_TEST(testTdf94765);
     CPPUNIT_TEST(testBehaviourWhenWidthAndHeightIsOrIsNotSet);
+    CPPUNIT_TEST(testTdf97663);
     CPPUNIT_TEST_SUITE_END();
 };
 
@@ -802,6 +804,24 @@ void Test::testBehaviourWhenWidthAndHeightIsOrIsNotSet()
         CPPUNIT_ASSERT_DOUBLES_EQUAL(11.0, fWidth, 1E-12);
         CPPUNIT_ASSERT_DOUBLES_EQUAL(11.0, fHeight, 1E-12);
     }
+}
+
+void Test::testTdf97663()
+{
+    Primitive2DSequence aSequence = parseSvg(u"/svgio/qa/cppunit/data/em_units.svg");
+    CPPUNIT_ASSERT_EQUAL(1, static_cast<int>(aSequence.getLength()));
+
+    drawinglayer::Primitive2dXmlDump dumper;
+    // This can be dumped to a file using dumper.dump(container, file_url)
+    Primitive2DContainer container = comphelper::sequenceToContainer<Primitive2DContainer>(aSequence);
+    xmlDocUniquePtr pDocument = dumper.dumpAndParse(container);
+
+    CPPUNIT_ASSERT (pDocument);
+
+    // tdf#97663: Without the fix in place, this test would have failed with
+    // - Expected: 236
+    // - Actual  : 204
+    assertXPath(pDocument, "/primitive2D/transform/textsimpleportion[2]", "y", "236");
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(Test);

svgio/qa/cppunit/data/em_units.svg
@@ -1,0 +1,14 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="10cm" height="10cm">
+
+  <style>
+    text {font-family: sans-serif; font-size: 36pt;}
+    new {font-family: sans-serif; font-size: 1em;}
+  </style>
+
+  <line x1="5cm" y1="5cm" x2="8cm" y2="5cm" stroke="black" />
+  <!-- 0.5in = 1.27cm = 36pt !-->
+  <line x1="5cm" y1="6.27cm" x2="8cm" y2="6.27cm" stroke="black" />
+  <text x="5cm" y="5cm" class="new">AWlll<tspan x="5cm" dy="1em">AWlll</tspan>
+  </text>
+</svg>
+