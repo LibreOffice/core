bf00bbe4193083aa6aa4ff68130d2831395c032e
Noel Grandin <noelgrandin@gmail.com>
2021-12-21 17:40:11 +0200

    osl::Mutex->std::mutex in SwRetrievedInputStreamDataManager
    drop the locking call in
    
        IMPL_LINK( SwRetrievedInputStreamDataManager, LinkedInputStreamReady
    
    because it doesn't actually touch any related data and the lock
    is taken in the call to PopData.
    
    Change-Id: I2e679b97a1ce36e7d39a9a2818a9d62fd53009a0

--- sw/source/core/docnode/retrievedinputstreamdata.cxx
+++ sw/source/core/docnode/retrievedinputstreamdata.cxx
@@ -34,7 +34,7 @@ SwRetrievedInputStreamDataManager& SwRetrievedInputStreamDataManager::GetManager
 SwRetrievedInputStreamDataManager::tDataKey SwRetrievedInputStreamDataManager::ReserveData(
                         std::weak_ptr< SwAsyncRetrieveInputStreamThreadConsumer > const & pThreadConsumer )
 {
-    osl::MutexGuard aGuard(maMutex);
+    std::unique_lock aGuard(maMutex);
 
     // create empty data container for given thread Consumer
     tDataKey nDataKey( snNextKeyValue );
@@ -59,7 +59,7 @@ void SwRetrievedInputStreamDataManager::PushData(
         css::uno::Reference<css::io::XInputStream> const & xInputStream,
         const bool bIsStreamReadOnly )
 {
-    osl::MutexGuard aGuard(maMutex);
+    std::unique_lock aGuard(maMutex);
 
     std::map< tDataKey, tData >::iterator aIter = maInputStreamData.find( nDataKey );
 
@@ -88,7 +88,7 @@ void SwRetrievedInputStreamDataManager::PushData(
 bool SwRetrievedInputStreamDataManager::PopData( const tDataKey nDataKey,
                                                  tData& rData )
 {
-    osl::MutexGuard aGuard(maMutex);
+    std::unique_lock aGuard(maMutex);
 
     bool bDataProvided( false );
 
@@ -124,8 +124,6 @@ IMPL_LINK( SwRetrievedInputStreamDataManager,
     {
         return;
     }
-
-    osl::MutexGuard aGuard(maMutex);
 
     SwRetrievedInputStreamDataManager& rDataManager =
                             SwRetrievedInputStreamDataManager::GetManager();
