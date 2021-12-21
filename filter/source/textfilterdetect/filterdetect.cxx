13ceaa008e25873bbcc0cf28487ec9e2f18ea399
Miklos Vajna <vmiklos@collabora.com>
2021-12-21 14:31:34 +0100

    filter: try to detect 0-byte DOC files based on extension
    Commit ae1f51b4888a3aa14837ac6e4083f33b2176ca45 (tdf#123476 filter: try
    to detect 0-byte files based on extension, 2020-10-28), already
    implemented this UNO-based import filters, do the same for built-in
    filters as well.
    
    Another problem in filter/ was to pick the WW6 filter for .doc --
    require export+preferred support in the filter to get WW8 instead.
    
    An additional filter that may kick in is MS Word 2003 XML: this is
    avoided by requiring "preferred".
    
    (cherry picked from commit e519d7465f38367205b37bc80f0244029e352306)
    
    Conflicts:
    	filter/qa/unit/textfilterdetect.cxx
    
    Change-Id: I46e280beb5341213b0fe7a09a549b52c0c1ea3f6
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/127296
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

--- filter/source/textfilterdetect/filterdetect.cxx
+++ filter/source/textfilterdetect/filterdetect.cxx
@@ -148,7 +148,11 @@ bool HandleEmptyFileUrlByExtension(MediaDescriptor& rMediaDesc, const OUString& 
         return false;
     }
 
-    std::shared_ptr<const SfxFilter> pFilter(SfxFilterMatcher().GetFilter4Extension(rExt));
+    // Requiring the export+preferred flags helps to find the relevant filter, e.g. .doc -> WW8 (and
+    // not WW6 or Mac_Word).
+    SfxFilterFlags nMust
+        = SfxFilterFlags::IMPORT | SfxFilterFlags::EXPORT | SfxFilterFlags::PREFERED;
+    std::shared_ptr<const SfxFilter> pFilter(SfxFilterMatcher().GetFilter4Extension(rExt, nMust));
     if (!pFilter)
     {
         return false;
