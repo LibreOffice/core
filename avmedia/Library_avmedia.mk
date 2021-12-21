LibreOffice/core

commit f9e79a91f15e10ededad0f103c4ffe0ca38fcb31

Fix --disable-avmedia for DESKTOP build
Probably additional bits can be removed, but this now passes.

Change-Id: I366bec43cfd6fb02e914c85c711b3b19586d534d
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/126161
Tested-by: Jenkins
Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>

avmedia/Library_avmedia.mk
@@ -9,15 +9,16 @@
 
 $(eval $(call gb_Library_Library,avmedia))
 
-$(eval $(call gb_Library_set_componentfile,avmedia,avmedia/util/avmedia))
-
 $(eval $(call gb_Library_set_include,avmedia,\
 	$$(INCLUDE) \
 	-I$(SRCDIR)/avmedia/inc \
 	-I$(SRCDIR)/avmedia/source/inc \
 ))
 
-$(eval $(call gb_Library_set_componentfile,avmedia,avmedia/util/avmedia))
+$(eval $(call gb_Library_use_libraries,avmedia,\
+    sal \
+    svl \
+))
 
 $(eval $(call gb_Library_use_sdk_api,avmedia,))
 
@@ -25,35 +26,30 @@ $(eval $(call gb_Library_use_externals,avmedia,\
 	boost_headers \
 ))
 
-ifeq ($(USE_AVMEDIA_DUMMY),TRUE)
-$(eval $(call gb_Library_add_exception_objects,avmedia,\
-	avmedia/source/avmediadummy \
-))
-
-
-else
-
-
 $(eval $(call gb_Library_add_defs,avmedia,\
-	-DAVMEDIA_DLLIMPLEMENTATION \
+    -DAVMEDIA_DLLIMPLEMENTATION \
 ))
+
+$(eval $(call gb_Library_add_exception_objects,avmedia,\
+    avmedia/source/framework/mediaitem \
+))
+
+ifneq (,$(filter AVMEDIA,$(BUILD_TYPE)))
 
 ifeq ($(DISABLE_GUI),)
 $(eval $(call gb_Library_use_externals,avmedia,\
     epoxy \
 ))
-endif
 
+$(eval $(call gb_Library_set_componentfile,avmedia,avmedia/util/avmedia))
 
 $(eval $(call gb_Library_use_libraries,avmedia,\
 	comphelper \
 	ucbhelper \
 	cppu \
 	cppuhelper \
-	sal \
 	i18nlangtag \
 	sfx \
-	svl \
 	svt \
 	tl \
 	utl \
@@ -63,7 +59,6 @@ $(eval $(call gb_Library_use_libraries,avmedia,\
 $(eval $(call gb_Library_add_exception_objects,avmedia,\
 	avmedia/source/framework/mediacontrol \
 	avmedia/source/framework/MediaControlBase \
-	avmedia/source/framework/mediaitem \
 	avmedia/source/framework/mediaplayer \
 	avmedia/source/framework/mediatoolbox \
 	avmedia/source/framework/soundhandler \
@@ -71,6 +66,8 @@ $(eval $(call gb_Library_add_exception_objects,avmedia,\
 	avmedia/source/viewer/mediawindow \
 	avmedia/source/viewer/mediawindow_impl \
 ))
-endif
+
+endif # !DISABLE_GUI
+endif # AVMEDIA
 
 # vim: set noet sw=4 ts=4:

Repository.mk
@@ -324,10 +324,12 @@ $(eval $(call gb_Helper_register_libraries_for_install,OOOLIBS,ogltrans, \
 
 $(eval $(call gb_Helper_register_libraries_for_install,OOOLIBS,ooo, \
 	acc \
-	$(call gb_Helper_optional,AVMEDIA,avmedia) \
+    avmedia \
+    $(call gb_Helper_optional,AVMEDIA, \
 	$(if $(filter MACOSX,$(OS)),\
 		avmediaMacAVF \
 	) \
+    ) \
 	$(call gb_Helper_optional,SCRIPTING, \
 		basctl \
 		basprov \
@@ -593,8 +595,10 @@ $(eval $(call gb_Helper_register_libraries_for_install,PRIVATELIBS_URE,ure, \
 ))
 
 $(eval $(call gb_Helper_register_libraries_for_install,PLAINLIBS_OOO,ooo, \
-	$(if $(ENABLE_GSTREAMER_1_0),avmediagst) \
-	$(if $(filter WNT,$(OS)),avmediawin) \
+    $(call gb_Helper_optional,AVMEDIA, \
+        $(if $(ENABLE_GSTREAMER_1_0),avmediagst) \
+        $(if $(filter WNT,$(OS)),avmediawin) \
+    ) \
 	cached1 \
 	collator_data \
 	comphelper \
@@ -1108,7 +1112,7 @@ $(eval $(call gb_ExternalExecutable_register_executables,\
 # Resources
 $(eval $(call gb_Helper_register_mos,\
 	acc \
-	avmedia \
+    $(call gb_Helper_optional,AVMEDIA,avmedia) \
 	$(call gb_Helper_optional,SCRIPTING,basctl) \
 	chart \
 	cnr \

RepositoryModule_host.mk
@@ -25,7 +25,7 @@ $(eval $(call gb_Module_add_moduledirs,libreoffice,\
 	android \
 	animations \
 	apple_remote \
-	$(call gb_Helper_optional,AVMEDIA,avmedia) \
+	avmedia \
 	basctl \
 	basegfx \
 	basic \

avmedia/Module_avmedia.mk
@@ -13,7 +13,8 @@ $(eval $(call gb_Module_add_targets,avmedia,\
 	Library_avmedia \
 ))
 
-ifneq ($(USE_AVMEDIA_DUMMY),TRUE)
+ifneq (,$(filter AVMEDIA,$(BUILD_TYPE)))
+
 $(eval $(call gb_Module_add_l10n_targets,avmedia,\
     AllLangMoTarget_avmedia \
 ))
@@ -36,6 +37,6 @@ $(eval $(call gb_Module_add_targets,avmedia,\
 ))
 endif
 
-endif
+endif # AVMEDIA
 
 # vim: set noet sw=4 ts=4:

avmedia/source/avmediadummy.cxx
@@ -1,93 +1,0 @@
-/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
-/*
- * This file is part of the LibreOffice project.
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- *
- * This file incorporates work covered by the following license notice:
- *
- *   Licensed to the Apache Software Foundation (ASF) under one or more
- *   contributor license agreements. See the NOTICE file distributed
- *   with this work for additional information regarding copyright
- *   ownership. The ASF licenses this file to you under the Apache
- *   License, Version 2.0 (the "License"); you may not use this file
- *   except in compliance with the License. You may obtain a copy of
- *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
- */
-
-#include <avmedia/mediaitem.hxx>
-
-#include <com/sun/star/uno/Sequence.hxx>
-#include <com/sun/star/uno/XComponentContext.hpp>
-
-using namespace ::com::sun::star;
-
-namespace avmedia
-{
-MediaItem::MediaItem( sal_uInt16 i_nWhich, AVMediaSetMask )
-    : SfxPoolItem( i_nWhich )
-{
-}
-
-MediaItem::MediaItem( const MediaItem& rItem )
-    : SfxPoolItem( rItem )
-{
-}
-
-MediaItem::~MediaItem()
-{
-}
-
-struct MediaItem::Impl
-{
-};
-
-bool MediaItem::QueryValue( css::uno::Any&, sal_uInt8 ) const
-{
-    return false;
-}
-
-bool MediaItem::GetPresentation( SfxItemPresentation, MapUnit, MapUnit, OUString&, const IntlWrapper& ) const
-{
-    return false;
-}
-
-bool MediaItem::PutValue( const css::uno::Any&, sal_uInt8 )
-{
-  return false;
-}
-
-AVMediaSetMask MediaItem::getMaskSet() const
-{
-    (void) this;                // Silence loplugin:staticmethods
-    return AVMediaSetMask::NONE;
-}
-
-SfxPoolItem* MediaItem::CreateDefault()
-{
-    return new MediaItem;
-}
-
-
-MediaItem* MediaItem::Clone( SfxItemPool*) const
-{
-  return nullptr;
-}
-
-bool MediaItem::operator==( const SfxPoolItem& rItem ) const
-{
-    assert(SfxPoolItem::operator==(rItem)); (void)rItem;
-    return false;
-}
-
-} // namespace avmedia
-
-extern "C" SAL_DLLPUBLIC_EXPORT css::uno::XInterface*
-com_sun_star_comp_framework_SoundHandler_get_implementation(css::uno::XComponentContext*,
-                                                            css::uno::Sequence<css::uno::Any> const &)
-{
-    return nullptr;
-}
-/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

config_host.mk.in
@@ -730,7 +730,6 @@ export ZLIB_CFLAGS=$(gb_SPACE)@ZLIB_CFLAGS@
 export ZLIB_LIBS=$(gb_SPACE)@ZLIB_LIBS@
 export ZMF_CFLAGS=$(gb_SPACE)@ZMF_CFLAGS@
 export ZMF_LIBS=$(gb_SPACE)@ZMF_LIBS@
-export USE_AVMEDIA_DUMMY=@USE_AVMEDIA_DUMMY@
 
 # lang-related stuff
 include $(BUILDDIR)/config_$(gb_Side)_lang.mk

configure.ac
@@ -1381,8 +1381,8 @@ libo_FUZZ_ARG_ENABLE(extension-integration,
 
 AC_ARG_ENABLE(avmedia,
     AS_HELP_STRING([--disable-avmedia],
-        [Disable displaying and inserting AV media in documents. Work in progress, use only if you are hacking on it.])
-)
+        [Disable displaying and inserting AV media in documents. Work in progress, use only if you are hacking on it.]),
+,test "${enable_avmedia+set}" = set || enable_avmedia=yes)
 
 AC_ARG_ENABLE(database-connectivity,
     AS_HELP_STRING([--disable-database-connectivity],
@@ -2944,17 +2944,12 @@ fi
 
 # Whether to build "avmedia" functionality or not.
 
-if test -z "$enable_avmedia"; then
-    enable_avmedia=yes
-fi
-
-BUILD_TYPE="$BUILD_TYPE AVMEDIA"
 if test "$enable_avmedia" = yes; then
+    BUILD_TYPE="$BUILD_TYPE AVMEDIA"
     AC_DEFINE(HAVE_FEATURE_AVMEDIA)
 else
-    USE_AVMEDIA_DUMMY='TRUE'
+    test_gstreamer_1_0=no
 fi
-AC_SUBST(USE_AVMEDIA_DUMMY)
 
 # Decide whether to build database connectivity stuff (including Base) or not.
 if test "$enable_database_connectivity" != no; then

oox/CppunitTest_oox_tokenmap.mk
@@ -35,7 +35,7 @@ endif
 endif
 
 $(eval $(call gb_CppunitTest_use_libraries,oox_tokenmap,\
-    avmedia \
+    $(call gb_Helper_optional,AVMEDIA,avmedia) \
     basegfx \
     comphelper \
     cppu \

commit 7fc7f5a5bee39644afd8b8925932cc66a368ab27

Keep GUI with --enable-avmedia and --disable-gui
So I thought I could come up with a rich MediaItem and can get
rid of all the GUI elements. But all the still needed features
require a MediaWindow. Probably not for real, but there are
multiple levels of createPlayer (some static) in MediaWindow
and priv::MediaWindowImpl.

Untangling this will require more time, so just return to the old
--disable-gui state with actually keeping all the GUI elements.

Regression from commit f9e79a91f15e10ededad0f103c4ffe0ca38fcb31
("Fix --disable-avmedia for DESKTOP build")...

Change-Id: I2b04459003d474cd3abf8b086f2f20f17274a7ee

avmedia/Library_avmedia.mk
@@ -45,6 +45,7 @@ ifeq ($(DISABLE_GUI),)
 $(eval $(call gb_Library_use_externals,avmedia,\
     epoxy \
 ))
+endif
 
 $(eval $(call gb_Library_set_componentfile,avmedia,avmedia/util/avmedia,services))
 
@@ -67,7 +68,6 @@ $(eval $(call gb_Library_add_exception_objects,avmedia,\
 	avmedia/source/viewer/mediawindow_impl \
 ))
 
-endif # !DISABLE_GUI
 endif # AVMEDIA
 
 # vim: set noet sw=4 ts=4:

distro-configs/LibreOfficeOssFuzz.conf
@@ -2,7 +2,6 @@
 --enable-ld=gold
 --disable-dynamic-loading
 --disable-runtime-optimizations
---disable-avmedia
 --disable-scripting
 --disable-cairo-canvas
 --disable-coinmp