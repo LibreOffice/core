b85e99950dc4584160512cffec303827c02f2d15
Samuel Mehrbrodt <samuel.mehrbrodt@allotropia.de>
2021-12-22 16:44:08 +0100

    tdf#145057 Fix saving color filter when multiple data ranges in sheet
    When mutliple data ranges existed in one sheet, only the first one was
    considered when exporting color filters.
    Consider all of them, as any could hold a color filter.
    
    Change-Id: I13ae2018057eef7ef24fc8298c814a93df24f74b
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/127328
    Tested-by: Jenkins
    Reviewed-by: Samuel Mehrbrodt <samuel.mehrbrodt@allotropia.de>

--- sc/source/core/tool/dbdata.cxx
+++ sc/source/core/tool/dbdata.cxx
@@ -1509,6 +1509,20 @@ ScDBData* ScDBCollection::GetDBNearCursor(SCCOL nCol, SCROW nRow, SCTAB nTab )
     return rDoc.GetAnonymousDBData(nTab);  // "unbenannt"/"unnamed" only if nothing else
 }
 
+std::vector<ScDBData*> ScDBCollection::GetAllDBsFromTab(SCTAB nTab)
+{
+    std::vector<ScDBData*> pTabData;
+    for (const auto& rxNamedDB : maNamedDBs)
+    {
+        if (rxNamedDB->GetTab() == nTab)
+            pTabData.emplace_back(rxNamedDB.get());
+    }
+    auto pAnonDBData = rDoc.GetAnonymousDBData(nTab);
+    if (pAnonDBData)
+        pTabData.emplace_back(pAnonDBData);
+    return pTabData;
+}
+
 bool ScDBCollection::empty() const
 {
     return maNamedDBs.empty() && maAnonDBs.empty();
