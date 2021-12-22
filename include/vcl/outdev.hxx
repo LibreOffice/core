176260a8987a43e8476be14535cab4c5a3a589a4
Caolán McNamara <caolanm@redhat.com>
2021-12-22 15:49:52 +0000

    allow selecting text rendering mode suitable for natural glyph positions
    Change-Id: I6b8c815fda3a48917467719432071c0716e3e9ab

--- include/vcl/outdev.hxx
+++ include/vcl/outdev.hxx
@@ -242,6 +242,7 @@ private:
     Point                           maRefPoint;
     AntialiasingFlags               mnAntialiasing;
     LanguageType                    meTextLanguage;
+    bool mbTextRenderModeForResolutionIndependentLayout;
 
     mutable bool                    mbMap : 1;
     mutable bool                    mbClipRegion : 1;
@@ -485,6 +486,10 @@ public:
 
     void                        SetAntialiasing( AntialiasingFlags nMode );
     AntialiasingFlags           GetAntialiasing() const { return mnAntialiasing; }
+
+    // Render glyphs with a mode suitable for rendering of resolution-independent layout positions.
+    void                        SetTextRenderModeForResolutionIndependentLayout(bool bMode);
+    bool                        GetTextRenderModeForResolutionIndependentLayout() const { return mbTextRenderModeForResolutionIndependentLayout; }
 
     void                        SetDrawMode( DrawModeFlags nDrawMode );
     DrawModeFlags               GetDrawMode() const { return mnDrawMode; }
