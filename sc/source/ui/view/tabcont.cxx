0c3697e83d27a50fc4092ae5438fe420659a0537
Noel Grandin <noelgrandin@gmail.com>
2021-12-22 20:06:20 +0200

    WIP
    Change-Id: I1b7873a0c2d9cda21f529e43a4ac2fa7574c91a5

--- sc/source/ui/view/tabcont.cxx
+++ sc/source/ui/view/tabcont.cxx
@@ -17,6 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
+#include <osl/diagnose.h>
 #include <sfx2/viewfrm.hxx>
 #include <sfx2/dispatch.hxx>
 #include <sfx2/docfile.hxx>
