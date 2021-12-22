752052cb1ff82949183cea59ca18683266c7b4ff
Jan-Marek Glogowski <glogow@fbihome.de>
2021-12-22 15:59:49 +0100

    Convert i18npool_LD_RULE template to pattern rule
    No need for various, evaled custom rules. A normal pattern rule is
    sufficent for this task.
    
    Change-Id: I4f7ad0cafb7fe78fc161eab54bb3170003c83ade
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/127325
    Tested-by: Jenkins
    Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>

--- i18npool/CustomTarget_localedata.mk
+++ i18npool/CustomTarget_localedata.mk
@@ -15,24 +15,19 @@ $(eval $(call gb_CustomTarget_register_targets,i18npool/localedata,\
 	$(foreach name,$(i18npool_LD_NAMES),localedata_$(name).cxx) \
 ))
 
-define i18npool_LD_RULE
-$(call gb_CustomTarget_get_workdir,i18npool/localedata)/localedata_$(1).cxx : \
-		$(SRCDIR)/i18npool/source/localedata/data/$(1).xml \
+$(call gb_CustomTarget_get_workdir,i18npool/localedata)/localedata_%.cxx : \
+		$(SRCDIR)/i18npool/source/localedata/data/%.xml \
 		$(call gb_Executable_get_runtime_dependencies,saxparser)
-	$$(call gb_Output_announce,$$(subst $(WORKDIR)/,,$$@),$(true),SAX,1)
-	$$(call gb_Trace_StartRange,$$(subst $(WORKDIR)/,,$$@),SAX)
-	TEMPSAX=$$(shell $$(gb_MKTEMP)) && TEMPSED=$$(shell $$(gb_MKTEMP)) && \
-	$$(call gb_Helper_abbreviate_dirs, \
-		$$(call gb_Helper_print_on_error, \
-			$$(call gb_Helper_execute,saxparser) $(1) $$< $$$${TEMPSAX} \
+	$(call gb_Output_announce,$(subst $(WORKDIR)/,,$@),$(true),SAX,1)
+	$(call gb_Trace_StartRange,$(subst $(WORKDIR)/,,$@),SAX)
+	TEMPSAX=$(shell $(gb_MKTEMP)) && TEMPSED=$(shell $(gb_MKTEMP)) && \
+	$(call gb_Helper_abbreviate_dirs, \
+		$(call gb_Helper_print_on_error, \
+			$(call gb_Helper_execute,saxparser) $* $< $${TEMPSAX} \
 				-env:LO_LIB_DIR=$(call gb_Helper_make_url,$(INSTROOT_FOR_BUILD)/$(LIBO_LIB_FOLDER)) \
 				-env:URE_MORE_SERVICES=$(call gb_Helper_make_url,$(call gb_Rdb_get_target_for_build,saxparser))) && \
-		sed 's/\(^.*get[^;]*$$$$\)/SAL_DLLPUBLIC_EXPORT \1/' $$$${TEMPSAX} > $$$${TEMPSED} && \
-		rm $$$${TEMPSAX} && $(call gb_Helper_replace_if_different_and_touch,$$$${TEMPSED},$$@))
-	$$(call gb_Trace_EndRange,$$(subst $(WORKDIR)/,,$$@),SAX)
-
-endef
-
-$(foreach name,$(i18npool_LD_NAMES),$(eval $(call i18npool_LD_RULE,$(name))))
+		sed 's/\(^.*get[^;]*$$\)/SAL_DLLPUBLIC_EXPORT \1/' $${TEMPSAX} > $${TEMPSED} && \
+		rm $${TEMPSAX} && $(call gb_Helper_replace_if_different_and_touch,$${TEMPSED},$@))
+	$(call gb_Trace_EndRange,$(subst $(WORKDIR)/,,$@),SAX)
 
 # vim: set noet sw=4 ts=4:
