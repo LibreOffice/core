3ac8e2c907b90838ae46b26f968804cf9de3f915
Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>
2021-12-22 17:48:11 +0100

    janitorial: the lang definitions aren't really an optional part of gbuild
    furthermore there's no need to keep them separate to fit dmake anymore,
    so lump together what goes together...
    
    Change-Id: Ic0377a322100c20352e211e53a670a9b0b227ab4

--- solenv/inc/langlist.mk
+++ solenv/inc/langlist.mk
@@ -136,3 +136,36 @@ xh \
 zh-CN \
 zh-TW \
 zu
+
+ifneq ($(WITH_LANG),ALL)
+gb_WITH_LANG=$(WITH_LANG)
+else
+# expand ALL based on language list)
+gb_WITH_LANG=$(completelangiso)
+endif
+gb_HELP_LANGS := en-US
+
+ifneq ($(ENABLE_RELEASE_BUILD),TRUE)
+ifneq ($(WITH_LANG),)
+gb_WITH_LANG += qtz
+gb_HELP_LANGS += qtz
+endif
+endif
+
+gb_TRANS_LANGS = $(filter-out en-US,$(filter-out qtz,$(gb_WITH_LANG)))
+
+gb_HELP_LANGS += \
+	$(foreach lang,$(filter-out $(WITH_POOR_HELP_LOCALIZATIONS),$(gb_TRANS_LANGS)),\
+		$(if \
+			$(and \
+				$(wildcard $(SRCDIR)/helpcontent2/source/auxiliary/$(lang)),\
+				$(wildcard $(SRCDIR)/translations/source/$(lang)/helpcontent2)),\
+			$(lang)))
+
+# Langs that need special handling for registry files. This is done by
+# MAKE_LANG_SPECIFIC flag in scp.
+gb_CJK_LANGS := $(filter $(gb_WITH_LANG),ja ko zh-CN zh-TW)
+gb_CTL_LANGS := $(filter $(gb_WITH_LANG),ar bo dz fa gu he hi km ky-CN lo my ne or pa-IN ta th)
+gb_CTLSEQCHECK_LANGS := $(filter $(gb_CTL_LANGS),km lo th)
+
+# vim: set noet ts=4 sw=4:
