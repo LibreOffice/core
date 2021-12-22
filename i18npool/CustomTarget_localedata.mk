LibreOffice/core

commit 14eeed686c5490ddbd356c1ac807b16231e4cb88

Just recompile changed generated localedata
Also use random temporary files.

Change-Id: If59a9f3ac8e736272ad51747c6a032351a6bec39
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/127138
Tested-by: Jenkins
Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>

i18npool/CustomTarget_localedata.mk
@@ -21,13 +21,14 @@ $(call gb_CustomTarget_get_workdir,i18npool/localedata)/localedata_$(1).cxx : \
 		$(call gb_Executable_get_runtime_dependencies,saxparser)
 	$$(call gb_Output_announce,$$(subst $(WORKDIR)/,,$$@),$(true),SAX,1)
 	$$(call gb_Trace_StartRange,$$(subst $(WORKDIR)/,,$$@),SAX)
+	TEMPSAX=$$(shell $$(gb_MKTEMP)) && TEMPSED=$$(shell $$(gb_MKTEMP)) && \
 	$$(call gb_Helper_abbreviate_dirs, \
 		$$(call gb_Helper_print_on_error, \
-			$$(call gb_Helper_execute,saxparser) $(1) $$< $$@.tmp \
+			$$(call gb_Helper_execute,saxparser) $(1) $$< $$$${TEMPSAX} \
 				-env:LO_LIB_DIR=$(call gb_Helper_make_url,$(INSTROOT_FOR_BUILD)/$(LIBO_LIB_FOLDER)) \
 				-env:URE_MORE_SERVICES=$(call gb_Helper_make_url,$(call gb_Rdb_get_target_for_build,saxparser))) && \
-		sed 's/\(^.*get[^;]*$$$$\)/SAL_DLLPUBLIC_EXPORT \1/' $$@.tmp > $$@ && \
-		rm $$@.tmp)
+		sed 's/\(^.*get[^;]*$$$$\)/SAL_DLLPUBLIC_EXPORT \1/' $$$${TEMPSAX} > $$$${TEMPSED} && \
+		rm $$$${TEMPSAX} && $(call gb_Helper_replace_if_different_and_touch,$$$${TEMPSED},$$@))
 	$$(call gb_Trace_EndRange,$$(subst $(WORKDIR)/,,$$@),SAX)
 
 endef

commit 5aa193f86bfd695585b740fbb177ad6ebcb48a8f

Fix i18npool_LD_RULE escaping
Regression from commit 14eeed686c5490ddbd356c1ac807b16231e4cb88
("Just recompile changed generated localedata").

Change-Id: I4f7ad0cafb7fe78fc161eab54bb3170003c83ade

i18npool/CustomTarget_localedata.mk
@@ -28,7 +28,7 @@ $(call gb_CustomTarget_get_workdir,i18npool/localedata)/localedata_$(1).cxx : \
 				-env:LO_LIB_DIR=$(call gb_Helper_make_url,$(INSTROOT_FOR_BUILD)/$(LIBO_LIB_FOLDER)) \
 				-env:URE_MORE_SERVICES=$(call gb_Helper_make_url,$(call gb_Rdb_get_target_for_build,saxparser))) && \
 		sed 's/\(^.*get[^;]*$$$$\)/SAL_DLLPUBLIC_EXPORT \1/' $$$${TEMPSAX} > $$$${TEMPSED} && \
-		rm $$$${TEMPSAX} && $(call gb_Helper_replace_if_different_and_touch,$$$${TEMPSED},$$@))
+		rm $$$${TEMPSAX} && $$(call gb_Helper_replace_if_different_and_touch,$$$${TEMPSED},$$@))
 	$$(call gb_Trace_EndRange,$$(subst $(WORKDIR)/,,$$@),SAX)
 
 endef