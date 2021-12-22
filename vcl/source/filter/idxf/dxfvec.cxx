e7e729f73501ad9af2578308e8031ab49dc34157
Ramreiso Kashung <ramreisog24@gmail.com>
2021-12-22 14:35:06 +0530

    tdf#145759 :  Use symbolic constants instead of magic numerical constants
    Change-Id: I0436aae81d6f89f44a8da91324ac1ccaa2f107d5

--- vcl/source/filter/idxf/dxfvec.cxx
+++ vcl/source/filter/idxf/dxfvec.cxx
@@ -72,8 +72,8 @@ DXFTransform::DXFTransform(double fScaleX, double fScaleY, double fScaleZ,
     aMZ(0.0, 0.0, fScaleZ),
     aMP(rShift)
 {
-    aMX.fx=cos(3.14159265359/180.0*fRotAngle);
-    aMX.fy=sin(3.14159265359/180.0*fRotAngle);
+    aMX.fx=cos(basegfx::deg2rad(fRotAngle));
+    aMX.fy=sin(basegfx::deg2rad(fRotAngle));
     aMY.fx=-aMX.fy;
     aMY.fy=aMX.fx;
     aMX*=fScaleX;
@@ -221,7 +221,7 @@ LineInfo DXFTransform::Transform(const DXFLineInfo& aDXFLineInfo) const
 
 double DXFTransform::CalcRotAngle() const
 {
-    return atan2(aMX.fy,aMX.fx)/3.14159265359*180.0;
+    return basegfx::rad2deg(atan2(aMX.fy,aMX.fx));
 }
 
 bool DXFTransform::Mirror() const
