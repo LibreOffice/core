d0a05369fabb5bfcc858713543515084c2e48455
Noel Grandin <noelgrandin@gmail.com>
2021-12-24 10:06:17 +0200

    use comphelper::WeakComponentImplHelper in SvXMLGraphicHelper
    and remove unused disposing() override
    
    Change-Id: I6423d9c3c418bb07f84e1622c8df6eac8ad762c4

--- svx/source/xml/xmlgrhlp.cxx
+++ svx/source/xml/xmlgrhlp.cxx
@@ -367,26 +367,16 @@ const GraphicObject& SvXMLGraphicOutputStream::GetGraphicObject()
 }
 
 SvXMLGraphicHelper::SvXMLGraphicHelper(SvXMLGraphicHelperMode eCreateMode)
-    : cppu::WeakComponentImplHelper<document::XGraphicObjectResolver,
-                                    document::XGraphicStorageHandler,
-                                    document::XBinaryStreamResolver>(m_aMutex)
 {
     Init( nullptr, eCreateMode );
 }
 
 SvXMLGraphicHelper::SvXMLGraphicHelper()
-    : cppu::WeakComponentImplHelper<document::XGraphicObjectResolver,
-                                    document::XGraphicStorageHandler,
-                                    document::XBinaryStreamResolver>(m_aMutex)
-    , meCreateMode(SvXMLGraphicHelperMode::Read)
+    : meCreateMode(SvXMLGraphicHelperMode::Read)
 {
 }
 
 SvXMLGraphicHelper::~SvXMLGraphicHelper()
-{
-}
-
-void SAL_CALL SvXMLGraphicHelper::disposing()
 {
 }
 
@@ -580,7 +570,7 @@ OUString SAL_CALL SvXMLGraphicHelper::resolveGraphicObjectURL( const OUString& /
 // XGraphicStorageHandler
 uno::Reference<graphic::XGraphic> SAL_CALL SvXMLGraphicHelper::loadGraphic(OUString const & rURL)
 {
-    osl::MutexGuard aGuard(m_aMutex);
+    std::unique_lock aGuard(m_aMutex);
 
     uno::Reference<graphic::XGraphic> xGraphic;
 
@@ -612,7 +602,7 @@ uno::Reference<graphic::XGraphic> SAL_CALL SvXMLGraphicHelper::loadGraphic(OUStr
 
 uno::Reference<graphic::XGraphic> SAL_CALL SvXMLGraphicHelper::loadGraphicFromOutputStream(uno::Reference<io::XOutputStream> const & rxOutputStream)
 {
-    osl::MutexGuard aGuard(m_aMutex);
+    std::unique_lock aGuard(m_aMutex);
 
     uno::Reference<graphic::XGraphic> xGraphic;
 
