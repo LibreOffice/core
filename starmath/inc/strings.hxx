b404a6fbbd254c19599b81a3b2f9ee85f0ac8c04
Jeff Huang <jeff@mail.ossii.com.tw>
2021-12-24 10:44:58 +0800

    tdf#140726 Change func func(<?>) to func <?>(<?>) in Math
    Change-Id: I9b28097c0043c7c6daffb047683b6af1f0202223
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/127393
    Tested-by: Jenkins
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
    (cherry picked from commit 54115790926a5964534472dff7e4661ea34acb28)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/127377
    Reviewed-by: Po-Yen Huang <jeff@mail.ossii.com.tw>
    Reviewed-by: Adolfo Jayme Barrientos <fitojb@ubuntu.com>

--- starmath/inc/strings.hxx
+++ starmath/inc/strings.hxx
@@ -75,7 +75,7 @@
 #define RID_XNSUBSETEQY         "<?> nsubseteq <?> "
 #define RID_XNSUPSETY           "<?> nsupset <?> "
 #define RID_XNSUPSETEQY         "<?> nsupseteq <?> "
-#define RID_FUNCX               "func func(<?>) "
+#define RID_FUNCX               "func <?>(<?>) "
 #define RID_ABSX                "abs{<?>} "
 #define RID_FACTX               "fact{<?>} "
 #define RID_SQRTX               "sqrt{<?>} "
