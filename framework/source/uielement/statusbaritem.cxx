5ae6cb1967597f4de385dd882f0422f873979b1d
Noel Grandin <noelgrandin@gmail.com>
2021-12-24 20:42:20 +0200

    use comphelper::WeakComponentImplHelper in StatusbarItem
    Change-Id: I2c278547d16c5ad0530188f1e465cb784d345746

--- framework/source/uielement/statusbaritem.cxx
+++ framework/source/uielement/statusbaritem.cxx
@@ -62,8 +62,7 @@ StatusbarItem::StatusbarItem(
     StatusBar              *pStatusBar,
     sal_uInt16              nId,
     const OUString&    aCommand )
-    : StatusbarItem_Base( m_aMutex )
-    , m_pStatusBar( pStatusBar )
+    : m_pStatusBar( pStatusBar )
     , m_nId( nId )
     , m_nStyle( 0 )
     , m_aCommand( aCommand )
@@ -77,21 +76,20 @@ StatusbarItem::~StatusbarItem()
 {
 }
 
-void SAL_CALL StatusbarItem::disposing()
+void StatusbarItem::disposing(std::unique_lock<std::mutex>&)
 {
-    osl::MutexGuard aGuard( m_aMutex );
     m_pStatusBar = nullptr;
 }
 
 OUString SAL_CALL StatusbarItem::getCommand()
 {
-    osl::MutexGuard aGuard( m_aMutex );
+    std::unique_lock aGuard( m_aMutex );
     return m_aCommand;
 }
 
 ::sal_uInt16 SAL_CALL StatusbarItem::getItemId()
 {
-    osl::MutexGuard aGuard( m_aMutex );
+    std::unique_lock aGuard( m_aMutex );
     return m_nId;
 }
 
@@ -106,7 +104,7 @@ OUString SAL_CALL StatusbarItem::getCommand()
 
 ::sal_uInt16 SAL_CALL StatusbarItem::getStyle()
 {
-    osl::MutexGuard aGuard( m_aMutex );
+    std::unique_lock aGuard( m_aMutex );
     return m_nStyle;
 }
 
