LibreOffice/core

commit 252dd254b5b29298457b889623783152e9bed534

new comphelper::WeakImplComponentHelper
to replace the cppu:: equivalent with a lightweight version
that uses std::mutex instead of osl::Mutex

Change-Id: I1b7873a0c2d9cda21f529e43a4ac2fa7574c91a5
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/127335
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

comphelper/source/misc/compbase.cxx
@@ -1,0 +1,48 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <comphelper/compbase.hxx>
+
+namespace comphelper
+{
+WeakComponentImplHelperBase::~WeakComponentImplHelperBase() {}
+
+// css::lang::XComponent
+void SAL_CALL WeakComponentImplHelperBase::dispose()
+{
+    std::unique_lock aGuard(m_aMutex);
+    if (m_bDisposed)
+        return;
+    m_bDisposed = true;
+    disposing();
+    css::lang::EventObject aEvt(static_cast<OWeakObject*>(this));
+    maEventListeners.disposeAndClear(aGuard, aEvt);
+}
+
+void WeakComponentImplHelperBase::disposing() {}
+
+void SAL_CALL WeakComponentImplHelperBase::addEventListener(
+    css::uno::Reference<css::lang::XEventListener> const& rxListener)
+{
+    std::unique_lock aGuard(m_aMutex);
+    if (m_bDisposed)
+        return;
+    maEventListeners.addInterface(rxListener);
+}
+
+void SAL_CALL WeakComponentImplHelperBase::removeEventListener(
+    css::uno::Reference<css::lang::XEventListener> const& rxListener)
+{
+    std::unique_lock aGuard(m_aMutex);
+    maEventListeners.removeInterface(rxListener);
+}
+
+} // namespace comphelper
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */

include/comphelper/compbase.hxx
@@ -1,0 +1,106 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#pragma once
+
+#include <sal/config.h>
+
+#include <comphelper/comphelperdllapi.h>
+#include <comphelper/interfacecontainer4.hxx>
+#include <cppuhelper/weak.hxx>
+#include <cppuhelper/queryinterface.hxx>
+#include <com/sun/star/lang/XComponent.hpp>
+#include <com/sun/star/lang/XTypeProvider.hpp>
+#include <mutex>
+
+namespace comphelper
+{
+/**
+    Serves two purposes
+    (1) extracts code that doesn't need to be templated
+    (2) helps to handle the custom where we have conflicting interfaces
+        e.g. multiple UNO interfaces that extend css::lang::XComponent
+*/
+class COMPHELPER_DLLPUBLIC WeakComponentImplHelperBase : public cppu::OWeakObject,
+                                                         public css::lang::XComponent
+{
+public:
+    virtual ~WeakComponentImplHelperBase() override;
+
+    // css::lang::XComponent
+    virtual void SAL_CALL dispose() override;
+    virtual void SAL_CALL
+    addEventListener(css::uno::Reference<css::lang::XEventListener> const& rxListener) override;
+    virtual void SAL_CALL
+    removeEventListener(css::uno::Reference<css::lang::XEventListener> const& rxListener) override;
+
+    /** called by dispose for subclasses to do dispose() work */
+    virtual void disposing();
+
+protected:
+    comphelper::OInterfaceContainerHelper4<css::lang::XEventListener> maEventListeners;
+    mutable std::mutex m_aMutex;
+    bool m_bDisposed;
+};
+
+template <typename... Ifc>
+class SAL_DLLPUBLIC_TEMPLATE WeakComponentImplHelper : public WeakComponentImplHelperBase,
+                                                       public css::lang::XTypeProvider,
+                                                       public Ifc...
+{
+public:
+    virtual void SAL_CALL acquire() noexcept final override { OWeakObject::acquire(); }
+
+    virtual void SAL_CALL release() noexcept final override { OWeakObject::release(); }
+
+    // css::lang::XComponent
+    virtual void SAL_CALL dispose() noexcept final override
+    {
+        WeakComponentImplHelperBase::dispose();
+    }
+    virtual void SAL_CALL addEventListener(
+        css::uno::Reference<css::lang::XEventListener> const& rxListener) final override
+    {
+        WeakComponentImplHelperBase::addEventListener(rxListener);
+    }
+    virtual void SAL_CALL removeEventListener(
+        css::uno::Reference<css::lang::XEventListener> const& rxListener) final override
+    {
+        WeakComponentImplHelperBase::removeEventListener(rxListener);
+    }
+
+    virtual css::uno::Any SAL_CALL queryInterface(css::uno::Type const& rType) override
+    {
+        css::uno::Any aReturn = ::cppu::queryInterface(
+            rType, static_cast<css::uno::XWeak*>(this),
+            static_cast<css::lang::XComponent*>(static_cast<WeakComponentImplHelperBase*>(this)),
+            static_cast<css::lang::XTypeProvider*>(this), static_cast<Ifc*>(this)...);
+        if (aReturn.hasValue())
+            return aReturn;
+        return OWeakObject::queryInterface(rType);
+    }
+
+    // css::lang::XTypeProvider
+    virtual css::uno::Sequence<css::uno::Type> SAL_CALL getTypes() override
+    {
+        static const css::uno::Sequence<css::uno::Type> aTypeList{
+            cppu::UnoType<css::uno::XWeak>::get(), cppu::UnoType<css::lang::XComponent>::get(),
+            cppu::UnoType<css::lang::XTypeProvider>::get(), cppu::UnoType<Ifc>::get()...
+        };
+        return aTypeList;
+    }
+    virtual css::uno::Sequence<sal_Int8> SAL_CALL getImplementationId() override
+    {
+        return css::uno::Sequence<sal_Int8>();
+    }
+};
+
+} //  namespace comphelper
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */

comphelper/Library_comphelper.mk
@@ -91,6 +91,7 @@ $(eval $(call gb_Library_add_exception_objects,comphelper,\
     comphelper/source/misc/automationinvokedzone \
     comphelper/source/misc/backupfilehelper \
     comphelper/source/misc/base64 \
+    comphelper/source/misc/compbase \
     comphelper/source/misc/componentbase \
     comphelper/source/misc/configuration \
     comphelper/source/misc/configurationhelper \

dbaccess/source/ui/inc/unodatbr.hxx
@@ -31,6 +31,7 @@
 #include <com/sun/star/sdb/application/DatabaseObject.hpp>
 #include <com/sun/star/sdb/application/DatabaseObjectContainer.hpp>
 #include <com/sun/star/sdb/XDatabaseRegistrationsListener.hpp>
+#include <comphelper/interfacecontainer2.hxx>
 #include <cppuhelper/implbase5.hxx>
 #include "callbacks.hxx"
 #include <vcl/transfer.hxx>

drawinglayer/source/primitive2d/baseprimitive2d.cxx
@@ -29,66 +29,6 @@
 
 using namespace css;
 
-BasePrimitive2DImplBase::~BasePrimitive2DImplBase() {}
-
-css::uno::Any BasePrimitive2DImplBase::queryInterface(css::uno::Type const& rType)
-{
-    css::uno::Any aReturn = ::cppu::queryInterface(
-        rType, static_cast<uno::XWeak*>(this), static_cast<lang::XComponent*>(this),
-        static_cast<lang::XTypeProvider*>(this), static_cast<graphic::XPrimitive2D*>(this),
-        static_cast<util::XAccounting*>(this));
-    if (aReturn.hasValue())
-        return aReturn;
-    return OWeakObject::queryInterface(rType);
-}
-
-void BasePrimitive2DImplBase::acquire() noexcept { OWeakObject::acquire(); }
-
-void BasePrimitive2DImplBase::release() noexcept
-{
-    if (osl_atomic_decrement(&m_refCount) != 0)
-        return;
-
-    // ensure no other references are created, via the weak connection point, from now on
-    disposeWeakConnectionPoint();
-    // restore reference count:
-    osl_atomic_increment(&m_refCount);
-    //    if (! rBHelper.bDisposed) {
-    //        try {
-    //            dispose();
-    //        }
-    //        catch (RuntimeException const& exc) { // don't break throw ()
-    //            SAL_WARN( "cppuhelper", exc );
-    //        }
-    //        OSL_ASSERT( rBHelper.bDisposed );
-    //    }
-    OWeakObject::release();
-}
-
-void BasePrimitive2DImplBase::dispose() {}
-
-void BasePrimitive2DImplBase::addEventListener(
-    css::uno::Reference<css::lang::XEventListener> const&)
-{
-    assert(false);
-}
-void BasePrimitive2DImplBase::removeEventListener(
-    css::uno::Reference<css::lang::XEventListener> const&)
-{
-    assert(false);
-}
-
-css::uno::Sequence<css::uno::Type> BasePrimitive2DImplBase::getTypes()
-{
-    static const css::uno::Sequence<uno::Type> aTypeList{
-        cppu::UnoType<uno::XWeak>::get(), cppu::UnoType<lang::XComponent>::get(),
-        cppu::UnoType<lang::XTypeProvider>::get(), cppu::UnoType<graphic::XPrimitive2D>::get(),
-        cppu::UnoType<util::XAccounting>::get()
-    };
-
-    return aTypeList;
-}
-
 namespace drawinglayer::primitive2d
 {
 BasePrimitive2D::BasePrimitive2D() {}

editeng/source/accessibility/AccessibleEditableTextPara.cxx
@@ -104,8 +104,7 @@ namespace accessibility
     AccessibleEditableTextPara::AccessibleEditableTextPara(
                                 const uno::Reference< XAccessible >& rParent,
                                 const AccessibleParaManager* _pParaManager )
-        : AccessibleTextParaInterfaceBase( m_aMutex ),
-          mnParagraphIndex( 0 ),
+        : mnParagraphIndex( 0 ),
           mnIndexInParent( 0 ),
           mpEditSource( nullptr ),
           maEEOffset( 0, 0 ),

extensions/source/propctrlr/standardcontrol.hxx
@@ -26,6 +26,7 @@
 #include <com/sun/star/inspection/XStringListControl.hpp>
 #include <com/sun/star/inspection/XHyperlinkControl.hpp>
 #include <com/sun/star/uno/Sequence.hxx>
+#include <comphelper/interfacecontainer2.hxx>
 #include <svtools/ctrlbox.hxx>
 #include <svx/colorbox.hxx>
 

include/drawinglayer/primitive2d/baseprimitive2d.hxx
@@ -25,55 +25,21 @@
 #include <drawinglayer/primitive2d/Primitive2DVisitor.hxx>
 #include <drawinglayer/geometry/viewinformation2d.hxx>
 
-#include <cppuhelper/weak.hxx>
-#include <com/sun/star/lang/XComponent.hpp>
-#include <com/sun/star/lang/XTypeProvider.hpp>
 #include <com/sun/star/util/XAccounting.hpp>
 #include <basegfx/range/b2drange.hxx>
 #include <com/sun/star/graphic/XPrimitive2D.hpp>
+#include <comphelper/compbase.hxx>
 #include <salhelper/simplereferenceobject.hxx>
 #include <rtl/ref.hxx>
 #include <deque>
-#include <mutex>
 
 namespace drawinglayer::geometry
 {
 class ViewInformation2D;
 }
 
-/** This is a custom re-implementation of cppu::WeakComponentImplHelper which uses
-   std::mutex and skips parts of the XComponent stuff.
-*/
-class DRAWINGLAYERCORE_DLLPUBLIC BasePrimitive2DImplBase : public cppu::OWeakObject,
-                                                           public css::lang::XComponent,
-                                                           public css::lang::XTypeProvider,
-                                                           public css::graphic::XPrimitive2D,
-                                                           public css::util::XAccounting
-{
-public:
-    virtual ~BasePrimitive2DImplBase() override;
-
-    virtual void SAL_CALL acquire() noexcept override;
-    virtual void SAL_CALL release() noexcept override;
-    virtual css::uno::Any SAL_CALL queryInterface(css::uno::Type const& aType) override;
-
-    // css::lang::XComponent
-    virtual void SAL_CALL dispose() override;
-    virtual void SAL_CALL
-    addEventListener(css::uno::Reference<css::lang::XEventListener> const& xListener) override;
-    virtual void SAL_CALL
-    removeEventListener(css::uno::Reference<css::lang::XEventListener> const& xListener) override;
-
-    // css::lang::XTypeProvider
-    virtual css::uno::Sequence<css::uno::Type> SAL_CALL getTypes() override;
-    virtual css::uno::Sequence<sal_Int8> SAL_CALL getImplementationId() override
-    {
-        return css::uno::Sequence<sal_Int8>();
-    }
-
-protected:
-    mutable std::mutex m_aMutex;
-};
+typedef comphelper::WeakComponentImplHelper<css::graphic::XPrimitive2D, css::util::XAccounting>
+    BasePrimitive2DImplBase;
 
 namespace drawinglayer::primitive2d
 {

commit 55cf79ae7529f226abc13a80ee44bfdc8f89af74

Init WeakComponentImplHelperBase::m_bDisposed
From the Jenkins_Linux_Ubsan TB:

/comphelper/source/misc/compbase.cxx:20:9: runtime error: load \
    of value 190, which is not a valid value for type ´bool´
... in comphelper::WeakComponentImplHelperBase::dispose() \
    /comphelper/source/misc/compbase.cxx:20:9

Maybe also the problem breaking Java for the Jenkins_Linux_Dbg
since the comphelper::WeakComponentImplHelperBase patches
went in.

Regression from commit 252dd254b5b29298457b889623783152e9bed534
("new comphelper::WeakImplComponentHelper").

Change-Id: Idebf6dfcc051f8fcb4cde1fb66d40b38e6f9dddc

comphelper/source/misc/compbase.cxx
@@ -11,6 +11,11 @@
 
 namespace comphelper
 {
+WeakComponentImplHelperBase::~WeakComponentImplHelperBase()
+    : m_bDisposed(false)
+{
+}
+
 WeakComponentImplHelperBase::~WeakComponentImplHelperBase() {}
 
 // css::lang::XComponent

include/comphelper/compbase.hxx
@@ -31,6 +31,7 @@ class COMPHELPER_DLLPUBLIC WeakComponentImplHelperBase : public cppu::OWeakObjec
                                                          public css::lang::XComponent
 {
 public:
+    WeakComponentImplHelperBase();
     virtual ~WeakComponentImplHelperBase() override;
 
     // css::lang::XComponent