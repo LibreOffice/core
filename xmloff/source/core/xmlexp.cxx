5b5c35329544cc6c6df83e8feae8603e524b5d44
Noel Grandin <noelgrandin@gmail.com>
2021-12-25 08:52:18 +0200

    can use OUStringLiteral here
    Change-Id: I1e71a35c2ce6bc7ef3081054e51494411d9a29ae

--- xmloff/source/core/xmlexp.cxx
+++ xmloff/source/core/xmlexp.cxx
@@ -1084,8 +1084,8 @@ void SvXMLExport::ImplExportStyles()
     if( ( mnExportFlags & SvXMLExportFlags::CONTENT ) || !mxExportInfo.is() )
         return;
 
-    static OUString sStyleNames( "StyleNames" );
-    static OUString sStyleFamilies( "StyleFamilies" );
+    constexpr OUStringLiteral sStyleNames( u"StyleNames" );
+    constexpr OUStringLiteral sStyleFamilies( u"StyleFamilies" );
     uno::Reference< beans::XPropertySetInfo > xPropertySetInfo = mxExportInfo->getPropertySetInfo();
     if ( xPropertySetInfo->hasPropertyByName( sStyleNames ) && xPropertySetInfo->hasPropertyByName( sStyleFamilies ) )
     {
