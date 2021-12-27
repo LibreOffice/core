5c754f4cd64b6921e49f24b1057b67eebda31261
Xisco Fauli <xiscofauli@libreoffice.org>
2021-12-27 16:50:20 +0100

    tdf#95217, tdf#142600: sc: Add UItest
    for that, launch an http server in a separate thread
    
    Change-Id: I9d78c9a4349620a9de31b3963f2a31ff1d354f86

---
+++ sc/qa/uitest/calc_tests3/tdf95217.py
@@ -0,0 +1,39 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+from uitest.framework import UITestCase
+from uitest.uihelper.common import get_state_as_dict
+from uitest.uihelper.common import select_pos
+from uitest.uihelper.httpserver import launchHTTPServer
+from libreoffice.uno.propertyvalue import mkPropertyValues
+from uitest.uihelper.common import get_url_for_data_file
+from libreoffice.calc.document import get_cell_by_position
+
+class tdf95217(UITestCase):
+
+    def test_tdf95217(self):
+
+        with launchHTTPServer() as httpd:
+            with self.ui_test.create_doc_in_start_center("calc") as calc_doc:
+
+                with self.ui_test.execute_dialog_through_command(".uno:InsertExternalDataSource", close_button="") as xDialog:
+                    # FIXME: close_dialog_through_button fails here
+                    xOkBtn = xDialog.getChild("cancel")
+                    xOkBtn.executeAction("CLICK", tuple())
+
+                # Without the fix in place, this test would have failed with
+                # AssertionError: 'وضعیت بازار' != 'ÙˆØ¶Ø¹ÛŒØª Ø¨Ø§Ø²Ø§Ø±'
+                self.assertEqual("وضعیت بازار", get_cell_by_position(calc_doc, 0, 0, 0).getString())
+                self.assertEqual("شاخص کل", get_cell_by_position(calc_doc, 0, 0, 1).getString())
+                self.assertEqual("شاخص كل (هم وزن)", get_cell_by_position(calc_doc, 0, 0, 2).getString())
+                self.assertEqual("بسته", get_cell_by_position(calc_doc, 0, 1, 0).getString())
+                self.assertEqual("63175", get_cell_by_position(calc_doc, 0, 1, 1).getString())
+                self.assertEqual("9,690.30 (22.80)", get_cell_by_position(calc_doc, 0, 1, 2).getString())
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
