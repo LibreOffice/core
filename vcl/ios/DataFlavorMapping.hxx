663959031852800f11b3c0ffcf6aa45f963f61be
Ramreiso Kashung <ramreisog24@gmail.com>
2021-12-28 19:18:00 +0530

    tdf#143148 : Use pragma once instead of include guards
    Change-Id: Ic9318847b387fb84af6124ad94d4340cbd57c797

--- vcl/ios/DataFlavorMapping.hxx
+++ vcl/ios/DataFlavorMapping.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_VCL_IOS_DATAFLAVORMAPPING_HXX
-#define INCLUDED_VCL_IOS_DATAFLAVORMAPPING_HXX
+#pragma once
 
 #include <com/sun/star/datatransfer/DataFlavor.hpp>
 #include <com/sun/star/datatransfer/XMimeContentTypeFactory.hpp>
@@ -122,6 +121,6 @@ private:
     mutable OfficeOnlyTypes maOfficeOnlyTypes;
 };
 
-#endif // INCLUDED_VCL_IOS_DATAFLAVORMAPPING_HXX
+// INCLUDED_VCL_IOS_DATAFLAVORMAPPING_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
