0d8936d26960cb5ae5d7421f4cba1910e06be464
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2021-12-28 14:12:19 +0900

    vcl: move Toggle1BitTransparency to CairoCommon
    Intermediate step beore moving bitmap related members.
    
    Change-Id: Iaa30fd53d3b14c08fd502b33d370950569994139

--- vcl/headless/CairoCommon.cxx
+++ vcl/headless/CairoCommon.cxx
@@ -1160,4 +1160,17 @@ std::unique_ptr<BitmapBuffer> FastConvert24BitRgbTo32BitCairo(const BitmapBuffer
     return pDst;
 }
 
+void Toggle1BitTransparency(const BitmapBuffer& rBuf)
+{
+    assert(rBuf.maPalette.GetBestIndex(BitmapColor(COL_BLACK)) == 0);
+    // TODO: make upper layers use standard alpha
+    if (getCairoFormat(rBuf) == CAIRO_FORMAT_A1)
+    {
+        const int nImageSize = rBuf.mnHeight * rBuf.mnScanlineSize;
+        unsigned char* pDst = rBuf.mpBits;
+        for (int i = nImageSize; --i >= 0; ++pDst)
+            *pDst = ~*pDst;
+    }
+}
+
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
