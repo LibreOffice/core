4a81902ecf8a71827fe045d9f047e565f19d7341
Jan-Marek Glogowski <glogow@fbihome.de>
2021-12-29 17:16:16 +0100

    oss-fuzz: chain-depend all *fuzzer binaries
    Change-Id: Id92ae26db0c4321f1670f7bd6d8c03c521e18d93

---
+++ solenv/gbuild/extensions/post_Fuzzers.mk
@@ -0,0 +1,17 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+# chain-link all fuzzer binaries
+$(foreach fuzzer,$(gb_Fuzzers_KNOWN), \
+    $(eval gb_Fuzzers_KNOWN = $(filter-out $(fuzzer),$(gb_Fuzzers_KNOWN))) \
+    $(if $(firstword $(gb_Fuzzers_KNOWN)), \
+        $(eval $(call gb_LinkTarget_get_target,$(call gb_Executable_get_linktarget,$(firstword $(gb_Fuzzers_KNOWN)))) : \
+            $(call gb_LinkTarget_get_target,$(call gb_Executable_get_linktarget,$(fuzzer))))))
+
+# vim: set noet sw=4 ts=4:
