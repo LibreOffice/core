LibreOffice/core

commit c859b4aa48956cbf5ff41280b0008456e8f5189c

gbuild: introduce gb_%_linktarget_target
Just some refactoring.

Change-Id: I47adb93f8a413d289f6abb2a48ed3f049f582a46
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/127799
Tested-by: Jenkins
Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>

bridges/Library_cpp_uno.mk
@@ -15,9 +15,9 @@ ifneq ($(filter ANDROID DRAGONFLY FREEBSD LINUX NETBSD OPENBSD,$(OS)),)
 bridges_SELECTED_BRIDGE := gcc3_linux_arm
 bridge_noopt_objects := cpp2uno except uno2cpp
 # HACK
-$(call gb_LinkTarget_get_target,$(call gb_Library_get_linktarget,gcc3_uno)) : \
+$(call gb_Library_get_linktarget_target,gcc3_uno) : \
 	$(call gb_CustomTarget_get_workdir,bridges/source/cpp_uno/gcc3_linux_arm)/armhelper.objectlist
-$(call gb_LinkTarget_get_target,$(call gb_Library_get_linktarget,gcc3_uno)) : \
+$(call gb_Library_get_linktarget_target,gcc3_uno)) : \
 	EXTRAOBJECTLISTS += $(call gb_CustomTarget_get_workdir,bridges/source/cpp_uno/gcc3_linux_arm)/armhelper.objectlist
 endif
 

solenv/gbuild/extensions/post_Fuzzers.mk
@@ -11,7 +11,7 @@
 $(foreach fuzzer,$(gb_Fuzzers_KNOWN), \
     $(eval gb_Fuzzers_KNOWN = $(filter-out $(fuzzer),$(gb_Fuzzers_KNOWN))) \
     $(if $(firstword $(gb_Fuzzers_KNOWN)), \
-        $(eval $(call gb_LinkTarget_get_target,$(call gb_Executable_get_linktarget,$(firstword $(gb_Fuzzers_KNOWN)))) : \
-            $(call gb_LinkTarget_get_target,$(call gb_Executable_get_linktarget,$(fuzzer))))))
+        $(eval $(call gb_Executable_get_linktarget_target,$(firstword $(gb_Fuzzers_KNOWN)))) : \
+            $(call gb_Executable_linktarget_target,$(fuzzer))))
 
 # vim: set noet sw=4 ts=4:

RepositoryModule_host.mk
@@ -165,8 +165,7 @@ $(eval $(call gb_Module_add_moduledirs,libreoffice,\
 # etc., try to prevent linking these in parallel by adding artificial build
 # order dependencies here.
 define repositorymodule_serialize1
-$(call gb_LinkTarget_get_target,$(call gb_Library_get_linktarget,$(1))) \
-	:| $(foreach lib,$(2),$(call gb_Library_get_target,$(lib)))
+$(call gb_Library_get_linktarget_target,$(1)) :| $(foreach lib,$(2),$(call gb_Library_get_target,$(lib)))
 endef
 
 define repositorymodule_serialize

jurt/Library_jpipe.mk
@@ -81,12 +81,9 @@ $(eval $(call gb_Library_use_system_darwin_frameworks,jpipe, \
 ))
 endif
 
-$(call gb_LinkTarget_get_target,$(call gb_Library_get_linktarget,jpipe)): \
-    $(call gb_UnpackedTarball_get_target,dtoa)
-$(call gb_LinkTarget_get_target,$(call gb_Library_get_linktarget,jpipe)): \
-    gb_CC := $(filter-out -fsanitize%,$(gb_CC))
-$(call gb_LinkTarget_get_target,$(call gb_Library_get_linktarget,jpipe)): \
-    gb_CXX := $(filter-out -fsanitize%,$(gb_CXX))
+$(call gb_Library_get_linktarget_target,jpipe): $(call gb_UnpackedTarball_get_target,dtoa)
+$(call gb_Library_get_linktarget_target,jpipe): gb_CC := $(filter-out -fsanitize%,$(gb_CC))
+$(call gb_Library_get_linktarget_target,jpipe): gb_CXX := $(filter-out -fsanitize%,$(gb_CXX))
 
 endif
 endif

sal/CppunitTest_sal_osl.mk
@@ -32,8 +32,7 @@ $(eval $(call gb_CppunitTest_use_libraries,sal_osl,\
 ))
 
 # the test uses the library created by Module_DLL
-$(call gb_CppunitTest_get_target,sal_osl) : \
-	$(call gb_LinkTarget_get_target,$(call gb_CppunitTest_get_linktarget,Module_DLL))
+$(call gb_CppunitTest_get_target,sal_osl) : $(call gb_CppunitTest_get_linktarget_target,Module_DLL)
 
 $(eval $(call gb_CppunitTest_use_executable,sal_osl,osl_process_child))
 

solenv/gbuild/CppunitTest.mk
@@ -139,7 +139,7 @@ else
 			PYTHONDONTWRITEBYTECODE=1) \
 		$(ICECREAM_RUN) $(gb_CppunitTest_GDBTRACE) $(gb_CppunitTest_VALGRINDTOOL) $(gb_CppunitTest_RR) \
 			$(gb_CppunitTest_CPPTESTCOMMAND) \
-		$(call gb_LinkTarget_get_target,$(call gb_CppunitTest_get_linktarget,$*)) \
+		$(call gb_CppunitTest_get_linktarget_target,$*) \
 		$(call gb_CppunitTest__make_args) "-env:CPPUNITTESTTARGET=$@" \
 		$(if $(gb_CppunitTest_localized),|| exit $$?; done) \
 		) \

solenv/gbuild/Executable.mk
@@ -82,7 +82,7 @@ $(call gb_Helper_make_userfriendly_targets,$(1),Executable)
 endef
 
 define gb_Executable_set_targettype_gui
-$(call gb_LinkTarget_get_target,$(call gb_Executable_get_linktarget,$(1))) : TARGETGUI := $(2)
+$(call gb_Executable_get_linktarget_target,$(1)) : TARGETGUI := $(2)
 endef
 
 # forward the call to the gb_LinkTarget implementation

solenv/gbuild/Extension.mk
@@ -189,7 +189,7 @@ endef
 # add a library from workdir; DO NOT use gb_Library_get_target
 define gb_Extension_add_library
 $(call gb_Extension_add_file,$(1),$(call gb_Library_get_runtime_filename,$(2)),\
-	$(call gb_LinkTarget_get_target,$(call gb_Library_get_linktarget,$(2))))
+	$(call gb_Library_get_linktarget_target,$(2)))
 
 endef
 

commit 904b66776f529690260fc01018c82ac6b5162452

Fix wrong brackets after refactor
How did that even pass in Jenkins on Android Arm?

Regression from commit c859b4aa48956cbf5ff41280b0008456e8f5189c
("gbuild: introduce gb_%_linktarget_target").

Change-Id: Id06cc151e92e221da856814d69eb8cced167b427

bridges/Library_cpp_uno.mk
@@ -17,7 +17,7 @@ bridge_noopt_objects := cpp2uno except uno2cpp
 # HACK
 $(call gb_Library_get_linktarget_target,gcc3_uno) : \
 	$(call gb_CustomTarget_get_workdir,bridges/source/cpp_uno/gcc3_linux_arm)/armhelper.objectlist
-$(call gb_Library_get_linktarget_target,gcc3_uno)) : \
+$(call gb_Library_get_linktarget_target,gcc3_uno) : \
 	EXTRAOBJECTLISTS += $(call gb_CustomTarget_get_workdir,bridges/source/cpp_uno/gcc3_linux_arm)/armhelper.objectlist
 endif
 

solenv/gbuild/extensions/post_Fuzzers.mk
@@ -11,7 +11,7 @@
 $(foreach fuzzer,$(gb_Fuzzers_KNOWN), \
     $(eval gb_Fuzzers_KNOWN = $(filter-out $(fuzzer),$(gb_Fuzzers_KNOWN))) \
     $(if $(firstword $(gb_Fuzzers_KNOWN)), \
-        $(eval $(call gb_Executable_get_linktarget_target,$(firstword $(gb_Fuzzers_KNOWN)))) : \
-            $(call gb_Executable_linktarget_target,$(fuzzer))))
+        $(eval $(call gb_Executable_get_linktarget_target,$(firstword $(gb_Fuzzers_KNOWN))) : \
+            $(call gb_Executable_linktarget_target,$(fuzzer)))))
 
 # vim: set noet sw=4 ts=4: