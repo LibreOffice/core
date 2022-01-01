a4e15140b33633301c387a4d95b18de24231cbdb
Jan-Marek Glogowski <glogow@fbihome.de>
2022-01-01 13:27:50 +0100

    Further separate saxparser run from cmp'n'touch
    At least on Windows the calls to saxparser almost feel as slow
    as the compilation. So this adds an intermediate file just for
    the cmp calls, costing just a bit more filesystem space.
    
    This also reverts the gb_MKTEMP usage introduced in commit
    14eeed686c5490ddbd356c1ac807b16231e4cb88. Using a fixed name in
    WORKDIR is easier to debug eventually, then some random mktemp
    generated file, and is faster anyway.
    
    Change-Id: Id4979b75b881e916c209ff0ff8e123536294e93b
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/127841
    Tested-by: Jenkins
    Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>

--- i18npool/CustomTarget_localedata.mk
+++ i18npool/CustomTarget_localedata.mk
@@ -15,19 +15,22 @@ $(eval $(call gb_CustomTarget_register_targets,i18npool/localedata,\
 	$(foreach name,$(i18npool_LD_NAMES),localedata_$(name).cxx) \
 ))
 
-$(call gb_CustomTarget_get_workdir,i18npool/localedata)/localedata_%.cxx : \
+$(call gb_CustomTarget_get_workdir,i18npool/localedata)/localedata_%_new.cxx : \
 		$(SRCDIR)/i18npool/source/localedata/data/%.xml \
 		$(call gb_Executable_get_runtime_dependencies,saxparser)
 	$(call gb_Output_announce,$(subst $(WORKDIR)/,,$@),$(true),SAX,1)
 	$(call gb_Trace_StartRange,$(subst $(WORKDIR)/,,$@),SAX)
-	TEMPSAX=$(shell $(gb_MKTEMP)) && TEMPSED=$(shell $(gb_MKTEMP)) && \
 	$(call gb_Helper_abbreviate_dirs, \
 		$(call gb_Helper_print_on_error, \
-			$(call gb_Helper_execute,saxparser) $* $< $${TEMPSAX} \
+			$(call gb_Helper_execute,saxparser) $* $< $@.sax \
 				-env:LO_LIB_DIR=$(call gb_Helper_make_url,$(INSTROOT_FOR_BUILD)/$(LIBO_LIB_FOLDER)) \
 				-env:URE_MORE_SERVICES=$(call gb_Helper_make_url,$(call gb_Rdb_get_target_for_build,saxparser))) && \
-		sed 's/\(^.*get[^;]*$$\)/SAL_DLLPUBLIC_EXPORT \1/' $${TEMPSAX} > $${TEMPSED} && \
-		rm $${TEMPSAX} && $(call gb_Helper_replace_if_different_and_touch,$${TEMPSED},$@))
+		sed 's/\(^.*get[^;]*$$\)/SAL_DLLPUBLIC_EXPORT \1/' $@.sax > $@)
+	rm $@.sax
 	$(call gb_Trace_EndRange,$(subst $(WORKDIR)/,,$@),SAX)
+
+$(call gb_CustomTarget_get_workdir,i18npool/localedata)/localedata_%.cxx : \
+		$(call gb_CustomTarget_get_workdir,i18npool/localedata)/localedata_%_new.cxx
+	$(call gb_Helper_copy_if_different_and_touch,$<,$@)
 
 # vim: set noet sw=4 ts=4:
