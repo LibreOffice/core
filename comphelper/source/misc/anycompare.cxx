df0e28bc34bea03a770c70827685f8a0d939a114
Luboš Luňák <l.lunak@collabora.com>
2022-01-06 14:11:30 +0100

    use C++ class handling ownership for anyToString()
    This matches changes I did for anyLess() in my previous commit.
    
    Change-Id: Id38c49c10223340b7359765e1071d8a9f72ba92e

--- comphelper/source/misc/anycompare.cxx
+++ comphelper/source/misc/anycompare.cxx
@@ -26,10 +26,13 @@
 #include <com/sun/star/util/Time.hpp>
 #include <com/sun/star/util/DateTime.hpp>
 
+#include "typedescriptionref.hxx"
+
 namespace comphelper
 {
     using ::com::sun::star::uno::Reference;
     using ::com::sun::star::uno::Type;
+    using ::com::sun::star::uno::TypeDescription;
     using ::com::sun::star::uno::TypeClass_CHAR;
     using ::com::sun::star::uno::TypeClass_BOOLEAN;
     using ::com::sun::star::uno::TypeClass_BYTE;
@@ -50,6 +53,7 @@ namespace comphelper
     using ::com::sun::star::util::Date;
     using ::com::sun::star::util::Time;
     using ::com::sun::star::util::DateTime;
+    using ::comphelper::detail::TypeDescriptionRef;
 
     namespace {
 
@@ -177,41 +181,6 @@ namespace comphelper
             return std::optional( false );
         return std::nullopt; // equal, so can't yet tell if anyLess() should return
     }
-
-    using com::sun::star::uno::TypeDescription;
-
-    // This is like com::sun::star::uno::TypeDescription, but it uses TYPELIB_DANGER_GET
-    // (which the code used originally, but it's easier to have a class to handle ownership).
-    class TypeDescriptionRef
-    {
-    public:
-        TypeDescriptionRef( typelib_TypeDescriptionReference* typeDef )
-        {
-            TYPELIB_DANGER_GET( &typeDescr, typeDef );
-        }
-        ~TypeDescriptionRef()
-        {
-            TYPELIB_DANGER_RELEASE( typeDescr );
-        }
-        typelib_TypeDescription* get()
-        {
-            return typeDescr;
-        }
-        typelib_TypeDescription* operator->()
-        {
-            return typeDescr;
-        }
-        bool is()
-        {
-            return typeDescr != nullptr;
-        }
-        bool equals( const TypeDescriptionRef& other ) const
-        {
-            return typeDescr && other.typeDescr && typelib_typedescription_equals( typeDescr, other.typeDescr );
-        }
-    private:
-        typelib_TypeDescription* typeDescr = nullptr;
-    };
 
     // This is typelib_typedescription_equals(), but returns -1/0/1 values like strcmp().
     int compareTypes( const typelib_TypeDescription * lhsType,
