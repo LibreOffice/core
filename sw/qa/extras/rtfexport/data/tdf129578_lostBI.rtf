LibreOffice/core

commit fd95fb975b754d71d3750e85431a4e596a40e659

tdf#125719 sw: rtf: refactor associated character properties
1. \rtlch, \ltrch should be placed before their properties.
2. Do not mix associated and normal character properties in output.
3. Do not output empty "\rtlch \ltrch", "\ltrch \rtlch" pairs.
4. Handle associated character properties runs instead of
handling separately their parts without order of them.

Change-Id: Ibbf7365d04708682a5f1eb664a579c60a47465d2
Reviewed-on: https://gerrit.libreoffice.org/72578
Tested-by: Jenkins
Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>

writerfilter/source/rtftok/rtfdocumentimpl.cxx
@@ -1850,17 +1850,41 @@ RTFError RTFDocumentImpl::dispatchToggle(RTFKeyword nKeyword, bool bParam, int n
     {
         case RTF_B:
         case RTF_AB:
-            nSprm = (m_aStates.top().getIsRightToLeft()
-                     || m_aStates.top().getRunType() == RTFParserState::RunType::HICH)
-                        ? NS_ooxml::LN_EG_RPrBase_bCs
-                        : NS_ooxml::LN_EG_RPrBase_b;
+            switch (m_aStates.top().getRunType())
+            {
+                case RTFParserState::RunType::HICH:
+                case RTFParserState::RunType::RTLCH_LTRCH_1:
+                case RTFParserState::RunType::LTRCH_RTLCH_2:
+                case RTFParserState::RunType::DBCH:
+                    nSprm = NS_ooxml::LN_EG_RPrBase_bCs;
+                    break;
+                case RTFParserState::RunType::NONE:
+                case RTFParserState::RunType::LOCH:
+                case RTFParserState::RunType::LTRCH_RTLCH_1:
+                case RTFParserState::RunType::RTLCH_LTRCH_2:
+                default:
+                    nSprm = NS_ooxml::LN_EG_RPrBase_b;
+                    break;
+            }
             break;
         case RTF_I:
         case RTF_AI:
-            nSprm = (m_aStates.top().getIsRightToLeft()
-                     || m_aStates.top().getRunType() == RTFParserState::RunType::HICH)
-                        ? NS_ooxml::LN_EG_RPrBase_iCs
-                        : NS_ooxml::LN_EG_RPrBase_i;
+            switch (m_aStates.top().getRunType())
+            {
+                case RTFParserState::RunType::HICH:
+                case RTFParserState::RunType::RTLCH_LTRCH_1:
+                case RTFParserState::RunType::LTRCH_RTLCH_2:
+                case RTFParserState::RunType::DBCH:
+                    nSprm = NS_ooxml::LN_EG_RPrBase_iCs;
+                    break;
+                case RTFParserState::RunType::NONE:
+                case RTFParserState::RunType::LOCH:
+                case RTFParserState::RunType::LTRCH_RTLCH_1:
+                case RTFParserState::RunType::RTLCH_LTRCH_2:
+                default:
+                    nSprm = NS_ooxml::LN_EG_RPrBase_i;
+                    break;
+            }
             break;
         case RTF_OUTL:
             nSprm = NS_ooxml::LN_EG_RPrBase_outline;
@@ -1950,7 +1974,11 @@ RTFError RTFDocumentImpl::pushState()
     else
     {
         // fdo#85812 group resets run type of _current_ and new state (but not RTL)
-        m_aStates.top().setRunType(RTFParserState::RunType::LOCH);
+        if (m_aStates.top().getRunType() != RTFParserState::RunType::LTRCH_RTLCH_2
+            && m_aStates.top().getRunType() != RTFParserState::RunType::RTLCH_LTRCH_2)
+        {
+            m_aStates.top().setRunType(RTFParserState::RunType::NONE);
+        }
 
         if (m_aStates.top().getDestination() == Destination::MR)
             lcl_DestinationToMath(m_aStates.top().getCurrentDestinationText(), m_aMathBuffer,
@@ -3567,8 +3595,7 @@ RTFParserState::RTFParserState(RTFDocumentImpl* pDocumentImpl)
     , m_nListLevelNum(0)
     , m_bLevelNumbersValid(true)
     , m_aFrame(this)
-    , m_eRunType(RunType::LOCH)
-    , m_bIsRightToLeft(false)
+    , m_eRunType(RunType::NONE)
     , m_nYear(0)
     , m_nMonth(0)
     , m_nDay(0)

sw/qa/extras/rtfexport/rtfexport3.cxx
@@ -12,6 +12,7 @@
 #include <com/sun/star/text/XFootnote.hpp>
 #include <com/sun/star/text/XFootnotesSupplier.hpp>
 #include <com/sun/star/text/TextContentAnchorType.hpp>
+#include <com/sun/star/awt/FontWeight.hpp>
 
 class Test : public SwModelTestBase
 {
@@ -226,6 +227,24 @@ DECLARE_RTFEXPORT_TEST(testTdf122455, "tdf122455.rtf")
     // 'Expected: 16; Actual  : 32', the font size from a list definition
     // leaked into the first run's character properties.
     CPPUNIT_ASSERT_EQUAL(16.0, getProperty<double>(getRun(getParagraph(1), 1), "CharHeight"));
+}
+
+DECLARE_RTFEXPORT_TEST(testTdf125719_case_1, "tdf125719_case_1.rtf")
+{
+    CPPUNIT_ASSERT_EQUAL(awt::FontWeight::NORMAL,
+                         getProperty<float>(getRun(getParagraph(1), 1), "CharWeight"));
+    CPPUNIT_ASSERT_EQUAL(awt::FontWeight::NORMAL,
+                         getProperty<float>(getRun(getParagraph(3), 1), "CharWeight"));
+}
+
+DECLARE_RTFEXPORT_TEST(testTdf125719_case_2, "tdf125719_case_2.rtf")
+{
+    CPPUNIT_ASSERT_EQUAL(awt::FontWeight::BOLD,
+                         getProperty<float>(getRun(getParagraph(1), 1), "CharWeight"));
+    CPPUNIT_ASSERT_EQUAL(awt::FontWeight::BOLD,
+                         getProperty<float>(getRun(getParagraph(3), 1), "CharWeight"));
+    CPPUNIT_ASSERT_EQUAL(awt::FontWeight::NORMAL,
+                         getProperty<float>(getRun(getParagraph(5), 1), "CharWeight"));
 }
 
 DECLARE_RTFEXPORT_TEST(testTabs, "tabs.rtf")

sw/source/filter/ww8/rtfattributeoutput.cxx
@@ -202,43 +202,9 @@ static OString OutBorderLine(RtfExport const& rExport, const editeng::SvxBorderL
 
 void RtfAttributeOutput::RTLAndCJKState(bool bIsRTL, sal_uInt16 nScript)
 {
-    /*
-       You would have thought that
-       m_rExport.Strm() << (bIsRTL ? OOO_STRING_SVTOOLS_RTF_RTLCH : OOO_STRING_SVTOOLS_RTF_LTRCH); would be sufficient here ,
-       but looks like word needs to see the other directional token to be
-       satisfied that all is kosher, otherwise it seems in ver 2003 to go and
-       semi-randomly stick strike through about the place. Perhaps
-       strikethrough is some ms developers "something is wrong signal" debugging
-       code that we're triggering ?
-       */
-    if (bIsRTL)
-    {
-        m_aStylesEnd.append(OOO_STRING_SVTOOLS_RTF_LTRCH);
-        m_aStylesEnd.append(' ');
-        m_aStylesEnd.append(OOO_STRING_SVTOOLS_RTF_RTLCH);
-    }
-    else
-    {
-        m_aStylesEnd.append(OOO_STRING_SVTOOLS_RTF_RTLCH);
-        m_aStylesEnd.append(' ');
-        m_aStylesEnd.append(OOO_STRING_SVTOOLS_RTF_LTRCH);
-    }
-
-    switch (nScript)
-    {
-        case i18n::ScriptType::LATIN:
-            m_aStylesEnd.append(OOO_STRING_SVTOOLS_RTF_LOCH);
-            break;
-        case i18n::ScriptType::ASIAN:
-            m_aStylesEnd.append(OOO_STRING_SVTOOLS_RTF_DBCH);
-            break;
-        case i18n::ScriptType::COMPLEX:
-            /* noop */
-            break;
-        default:
-            /* should not happen? */
-            break;
-    }
+    m_bIsRTL = bIsRTL;
+    m_nScript = nScript;
+    m_bControlLtrRtl = true;
 }
 
 void RtfAttributeOutput::StartParagraph(ww8::WW8TableNodeInfo::Pointer_t pTextNodeInfo)
@@ -412,8 +378,8 @@ void RtfAttributeOutput::EndParagraphProperties(
     const SwRedlineData* /*pRedlineParagraphMarkerDeleted*/,
     const SwRedlineData* /*pRedlineParagraphMarkerInserted*/)
 {
-    m_aStyles.append(m_aStylesEnd.makeStringAndClear());
-    m_rExport.Strm().WriteCharPtr(m_aStyles.makeStringAndClear().getStr());
+    const OString aProperties = MoveCharacterProperties(true);
+    m_rExport.Strm().WriteCharPtr(aProperties.getStr());
 }
 
 void RtfAttributeOutput::StartRun(const SwRedlineData* pRedlineData, sal_Int32 /*nPos*/,
@@ -448,8 +414,78 @@ void RtfAttributeOutput::StartRunProperties()
 
 void RtfAttributeOutput::EndRunProperties(const SwRedlineData* /*pRedlineData*/)
 {
-    m_aStyles.append(m_aStylesEnd.makeStringAndClear());
-    m_aRun->append(m_aStyles.makeStringAndClear());
+    const OString aProperties = MoveCharacterProperties(true);
+    m_aRun->append(aProperties.getStr());
+}
+
+OString RtfAttributeOutput::MoveCharacterProperties(bool aAutoWriteRtlLtr)
+{
+    const OString aAssoc = m_aStylesAssoc.makeStringAndClear();
+    const OString aNormal = m_aStyles.makeStringAndClear();
+    OStringBuffer aBuf;
+
+    if (aAutoWriteRtlLtr && !m_bControlLtrRtl)
+    {
+        m_bControlLtrRtl = !aAssoc.isEmpty();
+        m_bIsRTL = false;
+        m_nScript = i18n::ScriptType::LATIN;
+    }
+
+    if (m_bControlLtrRtl)
+    {
+        m_bControlLtrRtl = false;
+
+        /*
+           You would have thought that
+           m_rExport.Strm() << (bIsRTL ? OOO_STRING_SVTOOLS_RTF_RTLCH : OOO_STRING_SVTOOLS_RTF_LTRCH); would be sufficient here ,
+           but looks like word needs to see the other directional token to be
+           satisfied that all is kosher, otherwise it seems in ver 2003 to go and
+           semi-randomly stick strike through about the place. Perhaps
+           strikethrough is some ms developers "something is wrong signal" debugging
+           code that we're triggering ?
+           */
+        if (!aAssoc.isEmpty() || !aNormal.isEmpty())
+        {
+            if (m_bIsRTL)
+            {
+                aBuf.append(OOO_STRING_SVTOOLS_RTF_LTRCH)
+                    .append(aAssoc)
+                    .append(' ')
+                    .append(OOO_STRING_SVTOOLS_RTF_RTLCH)
+                    .append(aNormal);
+            }
+            else
+            {
+                aBuf.append(OOO_STRING_SVTOOLS_RTF_RTLCH)
+                    .append(aAssoc)
+                    .append(' ')
+                    .append(OOO_STRING_SVTOOLS_RTF_LTRCH)
+                    .append(aNormal);
+            }
+        }
+
+        switch (m_nScript)
+        {
+            case i18n::ScriptType::LATIN:
+                aBuf.append(OOO_STRING_SVTOOLS_RTF_LOCH);
+                break;
+            case i18n::ScriptType::ASIAN:
+                aBuf.append(OOO_STRING_SVTOOLS_RTF_DBCH);
+                break;
+            case i18n::ScriptType::COMPLEX:
+                /* noop */
+                break;
+            default:
+                /* should not happen? */
+                break;
+        }
+    }
+    else
+    {
+        aBuf.append(aAssoc).append(aNormal);
+    }
+
+    return aBuf.makeStringAndClear();
 }
 
 void RtfAttributeOutput::RunText(const OUString& rText, rtl_TextEncoding /*eCharSet*/)
@@ -459,8 +495,6 @@ void RtfAttributeOutput::RunText(const OUString& rText, rtl_TextEncoding /*eChar
 }
 
 OStringBuffer& RtfAttributeOutput::RunText() { return m_aRunText.getLastBuffer(); }
-
-OStringBuffer& RtfAttributeOutput::StylesEnd() { return m_aStylesEnd; }
 
 void RtfAttributeOutput::RawText(const OUString& rText, rtl_TextEncoding eCharSet)
 {
@@ -1138,8 +1172,7 @@ void RtfAttributeOutput::StartStyle(const OUString& rName, StyleType eType, sal_
 
 void RtfAttributeOutput::EndStyle()
 {
-    m_aStyles.append(m_aStylesEnd.makeStringAndClear());
-    OString aStyles = m_aStyles.makeStringAndClear();
+    OString aStyles = MoveCharacterProperties();
     m_rExport.InsStyle(m_nStyleId, aStyles);
     m_aStylesheet.append(aStyles);
     m_aStylesheet.append(' ');
@@ -1539,8 +1572,8 @@ void RtfAttributeOutput::NumberingLevel(sal_uInt8 nLevel, sal_uInt16 nStart,
         }
         m_rExport.OutputItemSet(*pOutSet, false, true, i18n::ScriptType::LATIN,
                                 m_rExport.m_bExportModeRTF);
-        m_aStyles.append(m_aStylesEnd.makeStringAndClear());
-        m_rExport.Strm().WriteCharPtr(m_aStyles.makeStringAndClear().getStr());
+        const OString aProperties = MoveCharacterProperties(true);
+        m_rExport.Strm().WriteCharPtr(aProperties.getStr());
     }
 
     m_rExport.Strm().WriteCharPtr(OOO_STRING_SVTOOLS_RTF_FI);
@@ -2315,17 +2348,17 @@ void RtfAttributeOutput::CharEscapement(const SvxEscapementItem& rEscapement)
 
 void RtfAttributeOutput::CharFont(const SvxFontItem& rFont)
 {
-    m_aStylesEnd.append(OOO_STRING_SVTOOLS_RTF_LOCH);
-    m_aStylesEnd.append(OOO_STRING_SVTOOLS_RTF_F);
-    m_aStylesEnd.append(static_cast<sal_Int32>(m_rExport.m_aFontHelper.GetId(rFont)));
+    m_aStyles.append(OOO_STRING_SVTOOLS_RTF_LOCH);
+    m_aStyles.append(OOO_STRING_SVTOOLS_RTF_F);
+    m_aStyles.append(static_cast<sal_Int32>(m_rExport.m_aFontHelper.GetId(rFont)));
 
     if (!m_rExport.HasItem(RES_CHRATR_CJK_FONT) && !m_rExport.HasItem(RES_CHRATR_CTL_FONT))
     {
         // Be explicit about that the given font should be used everywhere, not
         // just for the loch range.
-        m_aStylesEnd.append(OOO_STRING_SVTOOLS_RTF_HICH);
-        m_aStylesEnd.append(OOO_STRING_SVTOOLS_RTF_AF);
-        m_aStylesEnd.append(static_cast<sal_Int32>(m_rExport.m_aFontHelper.GetId(rFont)));
+        m_aStylesAssoc.append(OOO_STRING_SVTOOLS_RTF_HICH);
+        m_aStylesAssoc.append(OOO_STRING_SVTOOLS_RTF_AF);
+        m_aStylesAssoc.append(static_cast<sal_Int32>(m_rExport.m_aFontHelper.GetId(rFont)));
     }
 
     // FIXME: this may be a tad expensive... but the charset needs to be
@@ -2343,16 +2376,16 @@ void RtfAttributeOutput::CharFontSize(const SvxFontHeightItem& rFontSize)
     switch (rFontSize.Which())
     {
         case RES_CHRATR_FONTSIZE:
-            m_aStylesEnd.append(OOO_STRING_SVTOOLS_RTF_FS);
-            m_aStylesEnd.append(static_cast<sal_Int32>(rFontSize.GetHeight() / 10));
-            break;
-        case RES_CHRATR_CJK_FONTSIZE:
             m_aStyles.append(OOO_STRING_SVTOOLS_RTF_FS);
             m_aStyles.append(static_cast<sal_Int32>(rFontSize.GetHeight() / 10));
             break;
+        case RES_CHRATR_CJK_FONTSIZE:
+            m_aStylesAssoc.append(OOO_STRING_SVTOOLS_RTF_FS);
+            m_aStylesAssoc.append(static_cast<sal_Int32>(rFontSize.GetHeight() / 10));
+            break;
         case RES_CHRATR_CTL_FONTSIZE:
-            m_aStyles.append(OOO_STRING_SVTOOLS_RTF_AFS);
-            m_aStyles.append(static_cast<sal_Int32>(rFontSize.GetHeight() / 10));
+            m_aStylesAssoc.append(OOO_STRING_SVTOOLS_RTF_AFS);
+            m_aStylesAssoc.append(static_cast<sal_Int32>(rFontSize.GetHeight() / 10));
             break;
     }
 }
@@ -2371,18 +2404,18 @@ void RtfAttributeOutput::CharLanguage(const SvxLanguageItem& rLanguage)
     switch (rLanguage.Which())
     {
         case RES_CHRATR_LANGUAGE:
-            m_aStylesEnd.append(OOO_STRING_SVTOOLS_RTF_LANG);
-            m_aStylesEnd.append(
+            m_aStyles.append(OOO_STRING_SVTOOLS_RTF_LANG);
+            m_aStyles.append(
                 static_cast<sal_Int32>(static_cast<sal_uInt16>(rLanguage.GetLanguage())));
             break;
         case RES_CHRATR_CJK_LANGUAGE:
-            m_aStyles.append(OOO_STRING_SVTOOLS_RTF_LANGFE);
-            m_aStyles.append(
+            m_aStylesAssoc.append(OOO_STRING_SVTOOLS_RTF_LANGFE);
+            m_aStylesAssoc.append(
                 static_cast<sal_Int32>(static_cast<sal_uInt16>(rLanguage.GetLanguage())));
             break;
         case RES_CHRATR_CTL_LANGUAGE:
-            m_aStyles.append(OOO_STRING_SVTOOLS_RTF_ALANG);
-            m_aStyles.append(
+            m_aStylesAssoc.append(OOO_STRING_SVTOOLS_RTF_ALANG);
+            m_aStylesAssoc.append(
                 static_cast<sal_Int32>(static_cast<sal_uInt16>(rLanguage.GetLanguage())));
             break;
     }
@@ -2506,9 +2539,9 @@ void RtfAttributeOutput::CharBackground(const SvxBrushItem& rBrush)
 
 void RtfAttributeOutput::CharFontCJK(const SvxFontItem& rFont)
 {
-    m_aStyles.append(OOO_STRING_SVTOOLS_RTF_DBCH);
-    m_aStyles.append(OOO_STRING_SVTOOLS_RTF_AF);
-    m_aStyles.append(static_cast<sal_Int32>(m_rExport.m_aFontHelper.GetId(rFont)));
+    m_aStylesAssoc.append(OOO_STRING_SVTOOLS_RTF_DBCH);
+    m_aStylesAssoc.append(OOO_STRING_SVTOOLS_RTF_AF);
+    m_aStylesAssoc.append(static_cast<sal_Int32>(m_rExport.m_aFontHelper.GetId(rFont)));
 }
 
 void RtfAttributeOutput::CharFontSizeCJK(const SvxFontHeightItem& rFontSize)
@@ -2523,23 +2556,23 @@ void RtfAttributeOutput::CharLanguageCJK(const SvxLanguageItem& rLanguageItem)
 
 void RtfAttributeOutput::CharPostureCJK(const SvxPostureItem& rPosture)
 {
-    m_aStyles.append(OOO_STRING_SVTOOLS_RTF_I);
+    m_aStylesAssoc.append(OOO_STRING_SVTOOLS_RTF_I);
     if (rPosture.GetPosture() == ITALIC_NONE)
-        m_aStyles.append(sal_Int32(0));
+        m_aStylesAssoc.append(sal_Int32(0));
 }
 
 void RtfAttributeOutput::CharWeightCJK(const SvxWeightItem& rWeight)
 {
-    m_aStyles.append(OOO_STRING_SVTOOLS_RTF_B);
+    m_aStylesAssoc.append(OOO_STRING_SVTOOLS_RTF_B);
     if (rWeight.GetWeight() != WEIGHT_BOLD)
-        m_aStyles.append(sal_Int32(0));
+        m_aStylesAssoc.append(sal_Int32(0));
 }
 
 void RtfAttributeOutput::CharFontCTL(const SvxFontItem& rFont)
 {
-    m_aStyles.append(OOO_STRING_SVTOOLS_RTF_DBCH);
-    m_aStyles.append(OOO_STRING_SVTOOLS_RTF_AF);
-    m_aStyles.append(static_cast<sal_Int32>(m_rExport.m_aFontHelper.GetId(rFont)));
+    m_aStylesAssoc.append(OOO_STRING_SVTOOLS_RTF_DBCH);
+    m_aStylesAssoc.append(OOO_STRING_SVTOOLS_RTF_AF);
+    m_aStylesAssoc.append(static_cast<sal_Int32>(m_rExport.m_aFontHelper.GetId(rFont)));
 }
 
 void RtfAttributeOutput::CharFontSizeCTL(const SvxFontHeightItem& rFontSize)
@@ -2554,16 +2587,16 @@ void RtfAttributeOutput::CharLanguageCTL(const SvxLanguageItem& rLanguageItem)
 
 void RtfAttributeOutput::CharPostureCTL(const SvxPostureItem& rPosture)
 {
-    m_aStyles.append(OOO_STRING_SVTOOLS_RTF_AI);
+    m_aStylesAssoc.append(OOO_STRING_SVTOOLS_RTF_AI);
     if (rPosture.GetPosture() == ITALIC_NONE)
-        m_aStyles.append(sal_Int32(0));
+        m_aStylesAssoc.append(sal_Int32(0));
 }
 
 void RtfAttributeOutput::CharWeightCTL(const SvxWeightItem& rWeight)
 {
-    m_aStyles.append(OOO_STRING_SVTOOLS_RTF_AB);
+    m_aStylesAssoc.append(OOO_STRING_SVTOOLS_RTF_AB);
     if (rWeight.GetWeight() != WEIGHT_BOLD)
-        m_aStyles.append(sal_Int32(0));
+        m_aStylesAssoc.append(sal_Int32(0));
 }
 
 void RtfAttributeOutput::CharBidiRTL(const SfxPoolItem& /*rItem*/) {}
@@ -3624,6 +3657,9 @@ RtfAttributeOutput::RtfAttributeOutput(RtfExport& rExport)
     , m_pPrevPageDesc(nullptr)
     , m_nStyleId(0)
     , m_nListId(0)
+    , m_bIsRTL(false)
+    , m_nScript(i18n::ScriptType::LATIN)
+    , m_bControlLtrRtl(false)
     , m_nNextAnnotationMarkId(0)
     , m_nCurrentAnnotationMarkId(-1)
     , m_bTableCellOpen(false)

sw/source/filter/ww8/rtfattributeoutput.hxx
@@ -84,7 +84,7 @@ public:
     // Access to (anyway) private buffers, used by the sdr exporter
     OStringBuffer& RunText();
     OStringBuffer& Styles() { return m_aStyles; }
-    OStringBuffer& StylesEnd();
+    OString MoveCharacterProperties(bool aAutoWriteRtlLtr = false);
 
     /// Output text (without markup).
     void RawText(const OUString& rText, rtl_TextEncoding eCharSet) override;
@@ -516,9 +516,12 @@ private:
      */
     OStringBuffer m_aStyles;
     /*
-     * This is the same as m_aStyles but the contents of it is written last.
+     * This is the same as m_aStyles but the contents of it is Assoc.
      */
-    OStringBuffer m_aStylesEnd;
+    OStringBuffer m_aStylesAssoc;
+    bool m_bIsRTL;
+    sal_uInt16 m_nScript;
+    bool m_bControlLtrRtl;
 
     sal_Int32 m_nNextAnnotationMarkId;
     sal_Int32 m_nCurrentAnnotationMarkId;

sw/source/filter/ww8/rtfsdrexport.cxx
@@ -706,8 +706,7 @@ void RtfSdrExport::WriteOutliner(const OutlinerParaObject& rParaObj, TextTypes e
         const sal_Int32 nEnd = aStr.getLength();
 
         aAttrIter.OutParaAttr(false);
-        m_rAttrOutput.RunText().append(m_rAttrOutput.Styles().makeStringAndClear());
-        m_rAttrOutput.RunText().append(m_rAttrOutput.StylesEnd().makeStringAndClear());
+        m_rAttrOutput.RunText().append(m_rAttrOutput.MoveCharacterProperties(true));
 
         do
         {
@@ -716,8 +715,7 @@ void RtfSdrExport::WriteOutliner(const OutlinerParaObject& rParaObj, TextTypes e
 
             aAttrIter.OutAttr(nCurrentPos);
             m_rAttrOutput.RunText().append('{');
-            m_rAttrOutput.RunText().append(m_rAttrOutput.Styles().makeStringAndClear());
-            m_rAttrOutput.RunText().append(m_rAttrOutput.StylesEnd().makeStringAndClear());
+            m_rAttrOutput.RunText().append(m_rAttrOutput.MoveCharacterProperties(true));
             m_rAttrOutput.RunText().append(SAL_NEWLINE_STRING);
             bool bTextAtr = aAttrIter.IsTextAttr(nCurrentPos);
             if (!bTextAtr)

commit 61246cd1c0efcf684a2354bf29c8e573355e919f

tdf#129578 rtfimport: CJK char properties are not CS
This fixes a LO 6.4/6.3.0.1 regression caused by
commit fd95fb975b754d71d3750e85431a4e596a40e659
which was focused on export, and not import.

The code originally didn't have any
mention of DBCH, so it used the default.
However, in the 6.4 change, it was wrongly tied together
with the Complex Script indicator.

So revert that part of the change.
Mark fixed the same problem in 7.1 for font size.

Change-Id: I1c87ae856de93ebc693135585574f2eb144b214b

writerfilter/source/rtftok/rtfdocumentimpl.cxx
@@ -1862,13 +1862,13 @@ RTFError RTFDocumentImpl::dispatchToggle(RTFKeyword nKeyword, bool bParam, int n
                 case RTFParserState::RunType::HICH:
                 case RTFParserState::RunType::RTLCH_LTRCH_1:
                 case RTFParserState::RunType::LTRCH_RTLCH_2:
-                case RTFParserState::RunType::DBCH:
                     nSprm = NS_ooxml::LN_EG_RPrBase_bCs;
                     break;
                 case RTFParserState::RunType::NONE:
                 case RTFParserState::RunType::LOCH:
                 case RTFParserState::RunType::LTRCH_RTLCH_1:
                 case RTFParserState::RunType::RTLCH_LTRCH_2:
+                case RTFParserState::RunType::DBCH:
                 default:
                     nSprm = NS_ooxml::LN_EG_RPrBase_b;
                     break;
@@ -1881,13 +1881,13 @@ RTFError RTFDocumentImpl::dispatchToggle(RTFKeyword nKeyword, bool bParam, int n
                 case RTFParserState::RunType::HICH:
                 case RTFParserState::RunType::RTLCH_LTRCH_1:
                 case RTFParserState::RunType::LTRCH_RTLCH_2:
-                case RTFParserState::RunType::DBCH:
                     nSprm = NS_ooxml::LN_EG_RPrBase_iCs;
                     break;
                 case RTFParserState::RunType::NONE:
                 case RTFParserState::RunType::LOCH:
                 case RTFParserState::RunType::LTRCH_RTLCH_1:
                 case RTFParserState::RunType::RTLCH_LTRCH_2:
+                case RTFParserState::RunType::DBCH:
                 default:
                     nSprm = NS_ooxml::LN_EG_RPrBase_i;
                     break;

sw/qa/extras/rtfexport/data/tdf129578_lostBI.rtf
@@ -1,0 +1,31 @@
+{\rtf1\ansi\deff4\adeflang1025
+{\fonttbl{\f0\froman\fprq2\fcharset0 Times New Roman;}{\f1\froman\fprq2\fcharset2 Symbol;}{\f2\fswiss\fprq2\fcharset0 Arial;}{\f3\froman\fprq2\fcharset0 Liberation Serif{\*\falt Times New Roman};}{\f4\froman\fprq0\fcharset128 Calibri;}{\f5\froman\fprq0\fcharset128 Times New Roman;}{\f6\froman\fprq0\fcharset128 Cambria;}{\f7\froman\fprq0\fcharset128 Tahoma;}{\f8\fnil\fprq2\fcharset0 MS Mincho;}{\f9\fnil\fprq2\fcharset128 \'91\'76\'91\'cc;}{\f10\fnil\fprq2\fcharset0 DejaVu Sans;}{\f11\froman\fprq0\fcharset128 Arial;}}
+{\colortbl;\red0\green0\blue0;\red0\green0\blue255;\red0\green255\blue255;\red0\green255\blue0;\red255\green0\blue255;\red255\green0\blue0;\red255\green255\blue0;\red255\green255\blue255;\red0\green0\blue128;\red0\green128\blue128;\red0\green128\blue0;\red128\green0\blue128;\red128\green0\blue0;\red128\green128\blue0;\red128\green128\blue128;\red192\green192\blue192;\red82\green125\blue85;}
+{\stylesheet{\s0\snext0\rtlch\af11\afs22\alang1037 \ltrch\lang1033\langfe2052\hich\af4\loch\sl276\slmult1\ql\widctlpar\hyphpar0\sb0\sa200\ltrpar\cf0\f4\fs22\lang1033\kerning0\dbch\af9\langfe2052 Normal;}
+{\s1\sbasedon0\snext0\rtlch\af5\afs28\alang1037\ab \ltrch\lang1033\langfe2052\hich\af6\loch\sl276\slmult1\ql\keep\widctlpar\hyphpar0\sb480\sa0\keepn\ltrpar\cf17\f6\fs28\lang1033\b\kerning0\dbch\af9\langfe2052 Heading 1;}
+{\*\cs15\snext15 Default Paragraph Font;}
+{\*\cs16\sbasedon15\snext16 Header Char;}
+{\*\cs17\sbasedon15\snext17 Footer Char;}
+{\*\cs18\sbasedon15\snext18\rtlch\af5\afs28\ab \ltrch\hich\af6\loch\cf17\f6\fs28\b\dbch\af9 Heading 1 Char;}
+{\*\cs19\sbasedon15\snext19\rtlch\af7\afs16 \ltrch\hich\af7\loch\f7\fs16 Balloon Text Char;}
+{\s20\sbasedon0\snext21\rtlch\af5\afs28\alang1037 \ltrch\lang1033\langfe2052\hich\af5\loch\sl276\slmult1\ql\widctlpar\hyphpar0\sb240\sa120\keepn\ltrpar\cf0\f5\fs28\lang1033\kerning0\dbch\af10\langfe2052 Heading;}
+{\s21\sbasedon0\snext21\rtlch\af11\afs22\alang1037 \ltrch\lang1033\langfe2052\hich\af4\loch\sl276\slmult1\ql\widctlpar\hyphpar0\sb0\sa140\ltrpar\cf0\f4\fs22\lang1033\kerning0\dbch\af9\langfe2052 Text Body;}
+{\s22\sbasedon21\snext22\rtlch\af5\afs22\alang1037 \ltrch\lang1033\langfe2052\hich\af5\loch\sl276\slmult1\ql\widctlpar\hyphpar0\sb0\sa140\ltrpar\cf0\f5\fs22\lang1033\kerning0\dbch\af9\langfe2052 List;}
+{\s23\sbasedon0\snext23\rtlch\af5\afs24\alang1037\ai \ltrch\lang1033\langfe2052\hich\af5\loch\sl276\slmult1\ql\widctlpar\hyphpar0\sb120\sa120\noline\ltrpar\cf0\f5\fs24\lang1033\i\kerning0\dbch\af9\langfe2052 Caption;}
+{\s24\sbasedon0\snext24\rtlch\af5\afs22\alang1037 \ltrch\lang1033\langfe2052\hich\af5\loch\sl276\slmult1\ql\widctlpar\hyphpar0\sb0\sa200\noline\ltrpar\cf0\f5\fs22\lang1033\kerning0\dbch\af9\langfe2052 Index;}
+{\s25\sbasedon0\snext25\rtlch\af11\afs22\alang1037 \ltrch\lang1033\langfe2052\hich\af4\loch\sl276\slmult1\ql\widctlpar\hyphpar0\sb0\sa200\ltrpar\cf0\f4\fs22\lang1033\kerning0\dbch\af9\langfe2052 Header and Footer;}
+{\s26\sbasedon0\snext26\rtlch\af11\afs22\alang1037 \ltrch\lang1033\langfe2052\hich\af4\loch\sl240\slmult1\ql\widctlpar\tqc\tx4680\tqr\tx9360\hyphpar0\sb0\sa0\ltrpar\cf0\f4\fs22\lang1033\kerning0\dbch\af9\langfe2052 Header;}
+{\s27\sbasedon0\snext27\rtlch\af11\afs22\alang1037 \ltrch\lang1033\langfe2052\hich\af4\loch\sl240\slmult1\ql\widctlpar\tqc\tx4680\tqr\tx9360\hyphpar0\sb0\sa0\ltrpar\cf0\f4\fs22\lang1033\kerning0\dbch\af9\langfe2052 Footer;}
+{\s28\sbasedon1\snext0\rtlch\af5\afs28\alang1025\ab \ltrch\lang1033\langfe1041\hich\af6\loch\sl276\slmult1\ql\keep\widctlpar\hyphpar0\sb480\sa0\keepn\ltrpar\cf17\f6\fs28\lang1033\b\kerning0\dbch\af9\langfe1041 TOC Heading;}
+{\s29\sbasedon0\snext29\rtlch\af7\afs16\alang1037 \ltrch\lang1033\langfe2052\hich\af7\loch\sl240\slmult1\ql\widctlpar\hyphpar0\sb0\sa0\ltrpar\cf0\f7\fs16\lang1033\kerning0\dbch\af9\langfe2052 Balloon Text;}
+{\s30\snext30\rtlch\af11\afs22\alang1037 \ltrch\lang1033\langfe2052\hich\af4\loch\sl240\slmult1\ql\widctlpar\hyphpar0\sb0\sa0\ltrpar\cf0\f4\fs22\lang1033\kerning0\dbch\af9\langfe2052 Revision;}
+}{\*\generator LibreOffice/7.1.7.2$Linux_X86_64 LibreOffice_project/10$Build-2}{\info{\*\company Amazon.com}{\author EC2}{\creatim\yr2013\mo1\dy21\hr9\min3}{\revtim\yr2022\mo1\dy7\hr11\min5}{\printim\yr0\mo0\dy0\hr0\min0}}{\*\userprops{\propname AppVersion}\proptype30{\staticval 15.0000}}\deftab720\deftab720
+\hyphauto1\viewscale50
+{\*\pgdsctbl
+{\pgdsc0\pgdscuse451\pgwsxn12240\pghsxn15840\marglsxn1440\margrsxn1440\margtsxn1440\margbsxn1440\pgdscnxt0 Default Page Style;}}
+\formshade\paperh15840\paperw12240\margl1440\margr1440\margt1440\margb1440\sectd\sbknone\pgndec\sftnnar\saftnnrlc\sectunlocked1\pgwsxn12240\pghsxn15840\marglsxn1440\margrsxn1440\margtsxn1440\margbsxn1440\ftnbj\ftnstart1\ftnrstcont\ftnnar\aenddoc\aftnrstcont\aftnstart1\aftnnrlc\htmautsp
+{\*\ftnsep\chftnsep}\pgndec\pard\plain \s0\rtlch\af11\afs22\alang1037 \ltrch\lang1033\langfe2052\hich\af4\loch\sl276\slmult1\ql\widctlpar\hyphpar0\sb0\sa200\ltrpar\cf0\f4\fs22\lang1033\kerning0\dbch\af9\langfe2052{\rtlch\afs48\ab \ltrch\langfe1041\dbch\afs48\langfe1041\ab\af8\dbch
+\uc2 \u36020\'8b\'4d\u31038\'8e\'d0\u12414\'82\'dc\u12377\'82\'b7\u12414\'82\'dc\u12377\'82\'b7\u24481\'8c\'e4\u38534\'97\'b2\u26124\'8f\'b9\u12395\'82\'c9\u12390\'82\'c4\u12362\'82\'a8\u24950\'8c\'63\u12403\'82\'d1\u30003\'90\'5c\u12375\'82\'b5\u19978\'8f\'e3\u12370\'82\'b0\u12414\'82\'dc\u12377\'82\'b7\uc1 }
+\par \pard\plain \s0\rtlch\af11\afs22\alang1037 \ltrch\lang1033\langfe2052\hich\af4\loch\sl276\slmult1\ql\widctlpar\hyphpar0\sb0\sa200\ltrpar\cf0\f4\fs22\lang1033\kerning0\dbch\af9\langfe2052\loch\sb0\sa200{\rtlch\afs48\ai\ab \ltrch\langfe1041\dbch\afs48\langfe1041\i\ab\af8\dbch
+\uc2 \u24179\'95\'bd\u32032\'91\'66\u12399\'82\'cd\u26684\'8a\'69\u21029\'95\'ca\u12398\'82\'cc\u12372\'82\'b2\u39640\'8d\'82\u37197\'94\'7a\u12434\'82\'f0\u36060\'8e\'92\u12426\'82\'e8\u12289\'81\'41\u21402\'8c\'fa\u12367\'82\'ad\u24481\'8c\'e4\u31036\'97\'e7\u30003\'90\'5c\u12375\'82\'b5\u19978\'8f\'e3\u12370\'82\'b0\u12414\'82\'dc\u12377\'82\'b7\u12290\'81\'42\uc1 }
+\par }

sw/qa/extras/rtfexport/rtfexport4.cxx
@@ -9,6 +9,7 @@
 
 #include <swmodeltestbase.hxx>
 
+#include <com/sun/star/awt/FontSlant.hpp>
 #include <com/sun/star/table/ShadowFormat.hpp>
 #include <com/sun/star/text/WritingMode2.hpp>
 #include <com/sun/star/text/XDocumentIndex.hpp>
@@ -266,6 +267,14 @@ DECLARE_RTFEXPORT_TEST(testBtlrFrame, "btlr-frame.odt")
     // - Actual  : 0
     // i.e. custom writing mode was lost.
     CPPUNIT_ASSERT_EQUAL(text::WritingMode2::BT_LR, nActual);
+}
+
+DECLARE_RTFEXPORT_TEST(testTdf129578_lostBI, "tdf129578_lostBI.rtf")
+{
+    CPPUNIT_ASSERT_EQUAL(150.f, getProperty<float>(getRun(getParagraph(1), 1), "CharWeightAsian"));
+    CPPUNIT_ASSERT_EQUAL(
+        awt::FontSlant_ITALIC,
+        getProperty<awt::FontSlant>(getRun(getParagraph(2), 1), "CharPostureAsian"));
 }
 
 DECLARE_RTFEXPORT_TEST(testTdf129631_lostBorders, "tdf129631_lostBorders.rtf")