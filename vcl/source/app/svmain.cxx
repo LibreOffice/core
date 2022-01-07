7cdae23d690921349737c5da30ea7bc125ad2d2e
Mike Kaganski <mike.kaganski@collabora.com>
2022-01-07 13:22:05 +0300

    Use CreateThread instead of _beginthreadex
    Change-Id: I5633674ba7dcb03d9ad3049fa5eb380d385983b4
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/128119
    Tested-by: Jenkins
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

--- vcl/source/app/svmain.cxx
+++ vcl/source/app/svmain.cxx
@@ -628,7 +628,7 @@ struct WorkerThreadData
 
 #ifdef _WIN32
 static HANDLE hThreadID = nullptr;
-static unsigned __stdcall threadmain( void *pArgs )
+static DWORD WINAPI threadmain( _In_ LPVOID pArgs )
 {
     OleInitialize( nullptr );
     static_cast<WorkerThreadData*>(pArgs)->pWorker( static_cast<WorkerThreadData*>(pArgs)->pThreadData );
@@ -655,14 +655,14 @@ void CreateMainLoopThread( oslWorkerFunction pWorker, void * pThreadData )
 #ifdef _WIN32
     // sal thread always call CoInitializeEx, so a system dependent implementation is necessary
 
-    unsigned uThreadID;
-    hThreadID = reinterpret_cast<HANDLE>(_beginthreadex(
+    DWORD uThreadID;
+    hThreadID = CreateThread(
         nullptr,       // no security handle
         0,          // stacksize 0 means default
         threadmain,    // thread worker function
         new WorkerThreadData( pWorker, pThreadData ),       // arguments for worker function
         0,          // 0 means: create immediately otherwise use CREATE_SUSPENDED
-        &uThreadID ));   // thread id to fill
+        &uThreadID );   // thread id to fill
 #else
     hThreadID = osl_createThread( MainWorkerFunction, new WorkerThreadData( pWorker, pThreadData ) );
 #endif
