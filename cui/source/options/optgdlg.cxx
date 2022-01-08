LibreOffice/core

commit 131759a3db51140c21594308ed99c71aa7aba43a

use officecfg for menu options
Change-Id: I94c9cf310ed6975f158548913439445faf6d178f
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/119483
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

cui/source/options/optgdlg.cxx
@@ -30,7 +30,6 @@
 #include <i18nlangtag/mslangid.hxx>
 #include <i18nlangtag/languagetag.hxx>
 #include <unotools/compatibility.hxx>
-#include <svtools/menuoptions.hxx>
 #include <svl/languageoptions.hxx>
 #include <svtools/miscopt.hxx>
 #include <unotools/syslocaleoptions.hxx>
@@ -661,11 +660,10 @@ std::unique_ptr<SfxTabPage> OfaViewTabPage::Create( weld::Container* pPage, weld
 
 bool OfaViewTabPage::FillItemSet( SfxItemSet* )
 {
-    SvtMenuOptions aMenuOpt;
-
     bool bModified = false;
     bool bMenuOptModified = false;
     bool bRepaintWindows(false);
+    std::shared_ptr<comphelper::ConfigurationChanges> xChanges(comphelper::ConfigurationChanges::create());
 
     SvtMiscOptions aMiscOptions;
     const sal_Int32 nSizeLB_NewSelection = m_xIconSizeLB->get_active();
@@ -698,9 +696,7 @@ bool OfaViewTabPage::FillItemSet( SfxItemSet* )
             default:
                 OSL_FAIL( "OfaViewTabPage::FillItemSet(): This state of m_xSidebarIconSizeLB should not be possible!" );
         }
-        auto xChanges = comphelper::ConfigurationChanges::create();
         officecfg::Office::Common::Misc::SidebarIconSize::set(static_cast<sal_Int16>(eSet), xChanges);
-        xChanges->commit();
     }
 
     const sal_Int32 nNotebookbarSizeLB_NewSelection = m_xNotebookbarIconSizeLB->get_active();
@@ -716,9 +712,7 @@ bool OfaViewTabPage::FillItemSet( SfxItemSet* )
             default:
                 OSL_FAIL( "OfaViewTabPage::FillItemSet(): This state of m_xNotebookbarIconSizeLB should not be possible!" );
         }
-        auto xChanges = comphelper::ConfigurationChanges::create();
         officecfg::Office::Common::Misc::NotebookbarIconSize::set(static_cast<sal_Int16>(eSet), xChanges);
-        xChanges->commit();
     }
 
     const sal_Int32 nStyleLB_NewSelection = m_xIconStyleLB->get_active();
@@ -768,17 +762,19 @@ bool OfaViewTabPage::FillItemSet( SfxItemSet* )
 
     if (m_xFontShowCB->get_state_changed_from_saved())
     {
-        std::shared_ptr<comphelper::ConfigurationChanges> batch(comphelper::ConfigurationChanges::create());
-        officecfg::Office::Common::Font::View::ShowFontBoxWYSIWYG::set(m_xFontShowCB->get_active(), batch);
-        batch->commit();
+        officecfg::Office::Common::Font::View::ShowFontBoxWYSIWYG::set(m_xFontShowCB->get_active(), xChanges);
         bModified = true;
     }
 
     if (m_xMenuIconsLB->get_value_changed_from_saved())
     {
-        aMenuOpt.SetMenuIconsState(m_xMenuIconsLB->get_active() == 0 ?
+        TriState eMenuIcons = m_xMenuIconsLB->get_active() == 0 ?
             TRISTATE_INDET :
-            static_cast<TriState>(m_xMenuIconsLB->get_active() - 1));
+            static_cast<TriState>(m_xMenuIconsLB->get_active() - 1);
+        // Output cache of current setting as possibly modified by System Theme for older version
+        bool bMenuIcons = Application::GetSettings().GetStyleSettings().GetUseImagesInMenus();
+        officecfg::Office::Common::View::Menu::IsSystemIconsInMenus::set(eMenuIcons == TRISTATE_INDET, xChanges);
+        officecfg::Office::Common::View::Menu::ShowIconsInMenues::set(bMenuIcons, xChanges);
         bModified = true;
         bMenuOptModified = true;
         bAppearanceChanged = true;
@@ -786,9 +782,11 @@ bool OfaViewTabPage::FillItemSet( SfxItemSet* )
 
     if (m_xContextMenuShortcutsLB->get_value_changed_from_saved())
     {
-        aMenuOpt.SetContextMenuShortcuts(m_xContextMenuShortcutsLB->get_active() == 0 ?
+        officecfg::Office::Common::View::Menu::ShortcutsInContextMenus::set(
+            m_xContextMenuShortcutsLB->get_active() == 0 ?
             TRISTATE_INDET :
-            static_cast<TriState>(m_xContextMenuShortcutsLB->get_active() - 1));
+            static_cast<TriState>(m_xContextMenuShortcutsLB->get_active() - 1),
+            xChanges);
         bModified = true;
         bMenuOptModified = true;
         bAppearanceChanged = true;
@@ -818,12 +816,12 @@ bool OfaViewTabPage::FillItemSet( SfxItemSet* )
     if (m_xUseSkia->get_state_changed_from_saved() ||
         m_xForceSkiaRaster->get_state_changed_from_saved())
     {
-        std::shared_ptr<comphelper::ConfigurationChanges> batch(comphelper::ConfigurationChanges::create());
-        officecfg::Office::Common::VCL::UseSkia::set(m_xUseSkia->get_active(), batch);
-        officecfg::Office::Common::VCL::ForceSkiaRaster::set(m_xForceSkiaRaster->get_active(), batch);
-        batch->commit();
+        officecfg::Office::Common::VCL::UseSkia::set(m_xUseSkia->get_active(), xChanges);
+        officecfg::Office::Common::VCL::ForceSkiaRaster::set(m_xForceSkiaRaster->get_active(), xChanges);
         bModified = true;
     }
+
+    xChanges->commit();
 
     if( bMenuOptModified )
     {
@@ -926,11 +924,13 @@ void OfaViewTabPage::Reset( const SfxItemSet* )
 
     // WorkingSet
     m_xFontShowCB->set_active(officecfg::Office::Common::Font::View::ShowFontBoxWYSIWYG::get());
-    SvtMenuOptions aMenuOpt;
-    m_xMenuIconsLB->set_active(aMenuOpt.GetMenuIconsState() == 2 ? 0 : aMenuOpt.GetMenuIconsState() + 1);
+    bool bMenuIcons = officecfg::Office::Common::View::Menu::ShowIconsInMenues::get();
+    bool bSystemMenuIcons = officecfg::Office::Common::View::Menu::IsSystemIconsInMenus::get();
+    TriState eMenuIcons = bSystemMenuIcons ? TRISTATE_INDET : static_cast<TriState>(bMenuIcons);
+    m_xMenuIconsLB->set_active(eMenuIcons == 2 ? 0 : eMenuIcons + 1);
     m_xMenuIconsLB->save_value();
 
-    TriState eContextMenuShortcuts = aMenuOpt.GetContextMenuShortcuts();
+    TriState eContextMenuShortcuts = static_cast<TriState>(officecfg::Office::Common::View::Menu::ShortcutsInContextMenus::get());
     bool bContextMenuShortcutsNonDefault = eContextMenuShortcuts == TRISTATE_FALSE || eContextMenuShortcuts == TRISTATE_TRUE;
     m_xContextMenuShortcutsLB->set_active(bContextMenuShortcutsNonDefault ? eContextMenuShortcuts + 1 : 0);
     m_xContextMenuShortcutsLB->save_value();

desktop/source/app/app.cxx
@@ -100,7 +100,6 @@
 #include <rtl/byteseq.hxx>
 #include <unotools/pathoptions.hxx>
 #include <unotools/VersionConfig.hxx>
-#include <svtools/menuoptions.hxx>
 #include <rtl/bootstrap.hxx>
 #include <vcl/test/GraphicsRenderTests.hxx>
 #include <vcl/glxtestprocess.hxx>
@@ -1863,9 +1862,11 @@ void Desktop::OverrideSystemSettings( AllSettings& rSettings )
     hMouseSettings.SetFollow( aAppearanceCfg.IsMenuMouseFollow() ? (nFollow|MouseFollowFlags::Menu) : (nFollow&~MouseFollowFlags::Menu));
     rSettings.SetMouseSettings(hMouseSettings);
 
-    SvtMenuOptions aMenuOpt;
-    hStyleSettings.SetUseImagesInMenus(aMenuOpt.GetMenuIconsState());
-    hStyleSettings.SetContextMenuShortcuts(aMenuOpt.GetContextMenuShortcuts());
+    bool bMenuIcons = officecfg::Office::Common::View::Menu::ShowIconsInMenues::get();
+    bool bSystemMenuIcons = officecfg::Office::Common::View::Menu::IsSystemIconsInMenus::get();
+    TriState eMenuIcons = bSystemMenuIcons ? TRISTATE_INDET : static_cast<TriState>(bMenuIcons);
+    hStyleSettings.SetUseImagesInMenus(eMenuIcons);
+    hStyleSettings.SetContextMenuShortcuts(static_cast<TriState>(officecfg::Office::Common::View::Menu::ShortcutsInContextMenus::get()));
     hStyleSettings.SetDragFullOptions( nDragFullOptions );
     rSettings.SetStyleSettings ( hStyleSettings );
 }

framework/source/uielement/menubarmanager.cxx
@@ -43,7 +43,6 @@
 
 #include <comphelper/propertysequence.hxx>
 #include <officecfg/Office/Common.hxx>
-#include <svtools/menuoptions.hxx>
 #include <svtools/javainteractionhandler.hxx>
 #include <uno/current_context.hxx>
 #include <unotools/cmdoptions.hxx>
@@ -571,7 +570,7 @@ IMPL_LINK( MenuBarManager, Activate, Menu *, pMenu, bool )
                 css::uno::getCurrentContext()));
 
         // set/unset hiding disabled menu entries
-        bool bDontHide           = SvtMenuOptions().IsEntryHidingEnabled();
+        bool bDontHide           = officecfg::Office::Common::View::Menu::DontHideDisabledEntry::get();
         const StyleSettings& rSettings = Application::GetSettings().GetStyleSettings();
         bool bShowMenuImages     = rSettings.GetUseImagesInMenus();
         bool bShowShortcuts      = m_bHasMenuBar || rSettings.GetContextMenuShortcuts();

framework/source/uielement/toolbarmanager.cxx
@@ -73,8 +73,6 @@
 #include <vcl/weldutils.hxx>
 #include <tools/debug.hxx>
 
-#include <svtools/menuoptions.hxx>
-
 //  namespaces
 
 using namespace ::com::sun::star::awt;
@@ -1887,7 +1885,7 @@ void ToolBarManager::AddCustomizeMenuItems(ToolBox const * pToolBar)
     }
 
     // popup menu for quick customization
-    bool bHideDisabledEntries = !SvtMenuOptions().IsEntryHidingEnabled();
+    bool bHideDisabledEntries = !officecfg::Office::Common::View::Menu::DontHideDisabledEntry::get();
 
     ::PopupMenu *pMenu = pToolBar->GetMenu();
 

include/svtools/menuoptions.hxx
@@ -1,90 +1,0 @@
-/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
-/*
- * This file is part of the LibreOffice project.
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- *
- * This file incorporates work covered by the following license notice:
- *
- *   Licensed to the Apache Software Foundation (ASF) under one or more
- *   contributor license agreements. See the NOTICE file distributed
- *   with this work for additional information regarding copyright
- *   ownership. The ASF licenses this file to you under the Apache
- *   License, Version 2.0 (the "License"); you may not use this file
- *   except in compliance with the License. You may obtain a copy of
- *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
- */
-#pragma once
-
-#include <svtools/svtdllapi.h>
-#include <sal/types.h>
-#include <tools/gen.hxx>
-#include <unotools/options.hxx>
-#include <memory>
-
-namespace osl
-{
-class Mutex;
-}
-
-/*-************************************************************************************************************
-    @short          forward declaration to our private date container implementation
-    @descr          We use these class as internal member to support small memory requirements.
-                    You can create the container if it is necessary. The class which use these mechanism
-                    is faster and smaller then a complete implementation!
-**-***********************************************************************************************************/
-
-class SvtMenuOptions_Impl;
-
-/*-************************************************************************************************************
-    @short          collect information about menu features
-    @devstatus      ready to use
-**-***********************************************************************************************************/
-
-class SAL_WARN_UNUSED SVT_DLLPUBLIC SvtMenuOptions final : public utl::detail::Options
-{
-public:
-    SvtMenuOptions();
-    virtual ~SvtMenuOptions() override;
-
-    /*-****************************************************************************************************
-        @short      interface methods to get and set value of config key "org.openoffice.Office.Common/View/Menu/..."
-        @descr      These options describe internal states to enable/disable features of installed office.
-
-                    IsEntryHidingEnabled()
-                    SetEntryHidingState()   =>  Activate this field for viewing all deactivated menu entries.
-                                                Menu commands that are normally not necessary are hidden by default.
-                                                Default=false
-
-                    IsFollowMouseEnabled()
-                    SetFollowMouseState()   =>  Automatic selection while moving the mouse on a menu.
-                                                Default=true
-
-        @seealso    configuration package "org.openoffice.Office.Common/View/Menu"
-    **-***************************************************************************************************/
-
-    bool IsEntryHidingEnabled() const;
-    TriState GetMenuIconsState() const;
-    void SetMenuIconsState(TriState eState);
-    TriState GetContextMenuShortcuts() const;
-    void SetContextMenuShortcuts(TriState eState);
-
-private:
-    /*-****************************************************************************************************
-        @short      return a reference to a static mutex
-        @descr      These class is partially threadsafe (for de-/initialization only).
-                    All access methods aren't safe!
-                    We create a static mutex only for one ime and use at different times.
-        @return     A reference to a static mutex member.
-    **-***************************************************************************************************/
-
-    SVT_DLLPRIVATE static ::osl::Mutex& GetOwnStaticMutex();
-
-private:
-    std::shared_ptr<SvtMenuOptions_Impl> m_pImpl;
-
-}; // class SvtMenuOptions
-
-/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

include/unotools/itemholderbase.hxx
@@ -48,7 +48,6 @@ enum class EItem
 
     LinguConfig                   ,
 
-    MenuOptions                   ,
     MiscOptions                   ,
     ModuleOptions                 ,
 

svtools/Library_svt.mk
@@ -82,7 +82,6 @@ $(eval $(call gb_Library_add_exception_objects,svt,\
     svtools/source/config/fontsubstconfig \
     svtools/source/config/htmlcfg \
     svtools/source/config/itemholder2 \
-    svtools/source/config/menuoptions \
     svtools/source/config/miscopt \
     svtools/source/config/slidesorterbaropt \
     svtools/source/config/optionsdrawinglayer \

svtools/source/config/itemholder2.cxx
@@ -26,7 +26,6 @@
 #include <com/sun/star/configuration/theDefaultProvider.hpp>
 
 #include <svtools/accessibilityoptions.hxx>
-#include <svtools/menuoptions.hxx>
 #include <svtools/colorcfg.hxx>
 #include <svtools/printoptions.hxx>
 #include <unotools/options.hxx>
@@ -126,10 +125,6 @@ void ItemHolder2::impl_newItem(TItemInfo& rItem)
 
         case EItem::ColorConfig :
             rItem.pItem.reset( new ::svtools::ColorConfig() );
-            break;
-
-        case EItem::MenuOptions :
-            rItem.pItem.reset( new SvtMenuOptions() );
             break;
 
         case EItem::MiscOptions :

commit 5896798f49332c818f3ab6d9c7d96945652a0420

fix Options->LibreOffice->View->Visibility->Menu on Windows,
impossible to set to Hide, regression from
    commit 131759a3db51140c21594308ed99c71aa7aba43a
    Date:   Sun Jul 25 16:21:39 2021 +0200
    use officecfg for menu options

Change-Id: I5312be2963d15ab85c267ece073e52984b1a34de

cui/source/options/optgdlg.cxx
@@ -775,13 +775,8 @@ bool OfaViewTabPage::FillItemSet( SfxItemSet* )
 
     if (m_xMenuIconsLB->get_value_changed_from_saved())
     {
-        TriState eMenuIcons = m_xMenuIconsLB->get_active() == 0 ?
-            TRISTATE_INDET :
-            static_cast<TriState>(m_xMenuIconsLB->get_active() - 1);
-        // Output cache of current setting as possibly modified by System Theme for older version
-        bool bMenuIcons = Application::GetSettings().GetStyleSettings().GetUseImagesInMenus();
-        officecfg::Office::Common::View::Menu::IsSystemIconsInMenus::set(eMenuIcons == TRISTATE_INDET, xChanges);
-        officecfg::Office::Common::View::Menu::ShowIconsInMenues::set(bMenuIcons, xChanges);
+        officecfg::Office::Common::View::Menu::IsSystemIconsInMenus::set(m_xMenuIconsLB->get_active() == 0, xChanges);
+        officecfg::Office::Common::View::Menu::ShowIconsInMenues::set(m_xMenuIconsLB->get_active() == 2, xChanges);
         bModified = true;
         bMenuOptModified = true;
         bAppearanceChanged = true;
@@ -931,10 +926,9 @@ void OfaViewTabPage::Reset( const SfxItemSet* )
 
     // WorkingSet
     m_xFontShowCB->set_active(officecfg::Office::Common::Font::View::ShowFontBoxWYSIWYG::get());
-    bool bMenuIcons = officecfg::Office::Common::View::Menu::ShowIconsInMenues::get();
     bool bSystemMenuIcons = officecfg::Office::Common::View::Menu::IsSystemIconsInMenus::get();
-    TriState eMenuIcons = bSystemMenuIcons ? TRISTATE_INDET : static_cast<TriState>(bMenuIcons);
-    m_xMenuIconsLB->set_active(eMenuIcons == 2 ? 0 : eMenuIcons + 1);
+    bool bMenuIcons = officecfg::Office::Common::View::Menu::ShowIconsInMenues::get();
+    m_xMenuIconsLB->set_active(bSystemMenuIcons ? 0 : (bMenuIcons ? 2 : 1));
     m_xMenuIconsLB->save_value();
 
     TriState eContextMenuShortcuts = static_cast<TriState>(officecfg::Office::Common::View::Menu::ShortcutsInContextMenus::get());