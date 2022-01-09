328b801a34840fab9f94080dd6a9e5de2d49a0b2
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2022-01-09 13:04:18 +0900

    sc: make icon size of cond. formatting same as font size
    This makes the behavior of the Calc te same as other spreadsheet
    programs, where the icon size of the conditional formatting is
    the same as the font size and not the actual cell height (as it
    is the behaviour now).
    
    (cherry picked from commit 80956822bff4a0105ca53fc3fb99cfa1128b4f13)
    
    Change-Id: I00c96ad83458cd31f67c0b6f566e3d01dd8cd47b

--- sc/source/core/data/colorscale.cxx
+++ sc/source/core/data/colorscale.cxx
@@ -16,6 +16,8 @@
 #include <tokenarray.hxx>
 #include <refupdatecontext.hxx>
 #include <refdata.hxx>
+#include <editeng/fhgtitem.hxx>
+#include <scitems.hxx>
 
 #include <formula/token.hxx>
 #include <vcl/bitmapex.hxx>
@@ -1055,6 +1057,10 @@ std::unique_ptr<ScIconSetInfo> ScIconSetFormat::GetIconSetInfo(const ScAddress& 
         ++nIndex;
 
     std::unique_ptr<ScIconSetInfo> pInfo(new ScIconSetInfo);
+
+    const SfxPoolItem& rPoolItem = mpDoc->GetPattern(rAddr)->GetItem(ATTR_FONT_HEIGHT);
+    long aFontHeight = static_cast<const SvxFontHeightItem&>(rPoolItem).GetHeight();
+    pInfo->mnHeight = aFontHeight;
 
     if(mpFormatData->mbReverse)
     {
