fd5f07fcf2c662e290d5056a3148f6110a9a8ef8
Mert Tumer <mert.tumer@collabora.com>
2022-01-10 13:23:03 +0300

    lok: Fix freshly added comments are not displayed
    Signed-off-by: Mert Tumer <mert.tumer@collabora.com>
    Change-Id: Ice772eff8b9f8ae8c23ec1e66675e33a80492028

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -1585,13 +1585,18 @@ void CallbackFlushHandler::queue(const int type, CallbackData& aCallbackData)
     SAL_INFO("lok", "Queue: [" << type << "]: [" << aCallbackData.getPayload() << "] on " << m_queue1.size() << " entries.");
 
     bool bIsChartActive = false;
+    bool bIsComment = false;
     if (type == LOK_CALLBACK_GRAPHIC_SELECTION)
     {
         LokChartHelper aChartHelper(SfxViewShell::Current());
         bIsChartActive = aChartHelper.GetWindow() != nullptr;
     }
+    else if (type == LOK_CALLBACK_COMMENT)
+    {
+        bIsComment = true;
+    }
 
-    if (callbacksDisabled() && !bIsChartActive)
+    if (callbacksDisabled() && !bIsChartActive && !bIsComment)
     {
         // We drop notifications when this is set, except for important ones.
         // When we issue a complex command (such as .uno:InsertAnnotation)
--- sfx2/source/view/viewsh.cxx
+++ sfx2/source/view/viewsh.cxx
@@ -1455,6 +1455,7 @@ static bool ignoreLibreOfficeKitViewCallback(int nType, const SfxViewShell_Impl*
         {
         case LOK_CALLBACK_FORM_FIELD_BUTTON:
         case LOK_CALLBACK_TEXT_SELECTION:
+        case LOK_CALLBACK_COMMENT:
             break;
         default:
             // Reject e.g. invalidate during paint.
