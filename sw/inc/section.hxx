LibreOffice/core

commit 2e32f4ed5af16a68c97a50806a42ffa2d10f1d7a

get rid of long deprecated SwModify::BroadcastModify
Change-Id: I9c65de16bf1f1a604c0045444203a16f30f2833e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/105318
Tested-by: Jenkins
Reviewed-by: Bjoern Michaelsen <bjoern.michaelsen@libreoffice.org>

sw/source/core/docnode/section.cxx
@@ -743,7 +743,6 @@ void SwSectionFormat::SwClientNotify(const SwModify&, const SfxHint& rHint)
     auto pLegacy = dynamic_cast<const sw::LegacyModifyHint*>(&rHint);
     if(!pLegacy)
         return;
-    bool bClients = false;
     sal_uInt16 nWhich = pLegacy->GetWhich();
     auto pOld = pLegacy->m_pOld;
     auto pNew = pLegacy->m_pNew;
@@ -758,7 +757,7 @@ void SwSectionFormat::SwClientNotify(const SwModify&, const SfxHint& rHint)
             if( SfxItemState::SET == pNewSet->GetItemState(
                                         RES_PROTECT, false, &pItem ))
             {
-                ModifyBroadcast( pItem, pItem );
+                GetNotifier().Broadcast(sw::LegacyModifyHint(pItem, pItem));
                 pNewSet->ClearItem( RES_PROTECT );
                 pOldSet->ClearItem( RES_PROTECT );
             }
@@ -767,7 +766,7 @@ void SwSectionFormat::SwClientNotify(const SwModify&, const SfxHint& rHint)
             if( SfxItemState::SET == pNewSet->GetItemState(
                         RES_EDIT_IN_READONLY, false, &pItem ) )
             {
-                ModifyBroadcast( pItem, pItem );
+                GetNotifier().Broadcast(sw::LegacyModifyHint(pItem, pItem));
                 pNewSet->ClearItem( RES_EDIT_IN_READONLY );
                 pOldSet->ClearItem( RES_EDIT_IN_READONLY );
             }
@@ -775,14 +774,14 @@ void SwSectionFormat::SwClientNotify(const SwModify&, const SfxHint& rHint)
             if( SfxItemState::SET == pNewSet->GetItemState(
                                     RES_FTN_AT_TXTEND, false, &pItem ))
             {
-                ModifyBroadcast( &pOldSet->Get( RES_FTN_AT_TXTEND ), pItem );
+                GetNotifier().Broadcast(sw::LegacyModifyHint(pItem, pItem));
                 pNewSet->ClearItem( RES_FTN_AT_TXTEND );
                 pOldSet->ClearItem( RES_FTN_AT_TXTEND );
             }
             if( SfxItemState::SET == pNewSet->GetItemState(
                                     RES_END_AT_TXTEND, false, &pItem ))
             {
-                ModifyBroadcast( &pOldSet->Get( RES_END_AT_TXTEND ), pItem );
+                GetNotifier().Broadcast(sw::LegacyModifyHint(pItem, pItem));
                 pNewSet->ClearItem( RES_END_AT_TXTEND );
                 pOldSet->ClearItem( RES_END_AT_TXTEND );
             }
@@ -791,28 +790,22 @@ void SwSectionFormat::SwClientNotify(const SwModify&, const SfxHint& rHint)
         }
         break;
 
-    case RES_FTN_AT_TXTEND:
-    case RES_END_AT_TXTEND : bClients = true;
-        [[fallthrough]];
     case RES_SECTION_HIDDEN:
     case RES_SECTION_NOT_HIDDEN:
         {
-            SwSection* pSect = GetSection();
-            if( pSect && ( bClients || ( RES_SECTION_HIDDEN == nWhich ?
-                            !pSect->IsHiddenFlag() : pSect->IsHiddenFlag() ) ) )
-            {
-                ModifyBroadcast( pOld, pNew );
-            }
+            auto pSect = GetSection();
+            if(!pSect || (RES_SECTION_HIDDEN == nWhich) == pSect->IsHiddenFlag()) // already at target state, skipping.
+                return;
         }
-        return ;
-
+        [[fallthrough]];
+    case RES_FTN_AT_TXTEND:
+    case RES_END_AT_TXTEND:
+        GetNotifier().Broadcast(sw::LegacyModifyHint(pOld, pNew));
+        return;
     case RES_PROTECT:
     case RES_EDIT_IN_READONLY: // edit in readonly sections
         // Pass through these Messages until the End of the tree!
-        if( HasWriterListeners() )
-        {
-            ModifyBroadcast( pOld, pNew );
-        }
+        GetNotifier().Broadcast(sw::LegacyModifyHint(pOld, pNew));
         return; // That's it!
 
     case RES_OBJECTDYING:

sw/inc/calbck.hxx
@@ -192,10 +192,6 @@ public:
     // broadcasting: send notifications to all clients
     // DO NOT USE IN NEW CODE! use CallSwClientNotify instead.
     void NotifyClients( const SfxPoolItem *pOldValue, const SfxPoolItem *pNewValue );
-    // the same, but without setting m_bModifyLocked or checking for any of the flags
-    // DO NOT USE IN NEW CODE! use CallSwClientNotify instead.
-    void ModifyBroadcast( const SfxPoolItem *pOldValue, const SfxPoolItem *pNewValue)
-        { CallSwClientNotify( sw::LegacyModifyHint{ pOldValue, pNewValue } ); };
 
     // a more universal broadcasting mechanism
     virtual void CallSwClientNotify( const SfxHint& rHint ) const;

sw/qa/core/uwriter.cxx
@@ -1817,7 +1817,7 @@ void SwDocTest::testIntrusiveRing()
 namespace
 {
     struct TestHint final : SfxHint {};
-    struct TestModify : SwModify
+    struct TestModify : sw::BroadcastingModify
     {
     };
     struct TestClient : SwClient
@@ -1880,12 +1880,12 @@ void SwDocTest::testClientModify()
     CPPUNIT_ASSERT(aMod.HasWriterListeners());
     CPPUNIT_ASSERT(!aMod.HasOnlyOneListener());
     // test broadcast
-    aMod.ModifyBroadcast(nullptr, nullptr);
+    aMod.CallSwClientNotify(sw::LegacyModifyHint(nullptr, nullptr));
     CPPUNIT_ASSERT_EQUAL(1,aClient1.m_nModifyCount);
     CPPUNIT_ASSERT_EQUAL(1,aClient2.m_nModifyCount);
     CPPUNIT_ASSERT_EQUAL(0,aClient1.m_nNotifyCount);
     CPPUNIT_ASSERT_EQUAL(0,aClient2.m_nNotifyCount);
-    aMod.ModifyBroadcast(nullptr, nullptr);
+    aMod.CallSwClientNotify(sw::LegacyModifyHint(nullptr, nullptr));
     CPPUNIT_ASSERT_EQUAL(2,aClient1.m_nModifyCount);
     CPPUNIT_ASSERT_EQUAL(2,aClient2.m_nModifyCount);
     CPPUNIT_ASSERT_EQUAL(0,aClient1.m_nNotifyCount);
@@ -1953,7 +1953,7 @@ void SwDocTest::testClientModify()
             CPPUNIT_ASSERT(false);
         }
     }
-    aMod.ModifyBroadcast(nullptr, nullptr);
+    aMod.CallSwClientNotify(sw::LegacyModifyHint(nullptr, nullptr));
     CPPUNIT_ASSERT_EQUAL(2,aClient1.m_nModifyCount);
     CPPUNIT_ASSERT_EQUAL(2,aClient2.m_nModifyCount);
     CPPUNIT_ASSERT_EQUAL(1,aClient1.m_nNotifyCount);
@@ -1968,7 +1968,7 @@ void SwDocTest::testBroadcastingModify()
     aMod.Add(&aClient);
     aListener.StartListening(aMod.GetNotifier());
 
-    aMod.ModifyBroadcast(nullptr, nullptr);
+    aMod.CallSwClientNotify(sw::LegacyModifyHint(nullptr, nullptr));
     CPPUNIT_ASSERT_EQUAL(1,aClient.m_nModifyCount);
     CPPUNIT_ASSERT_EQUAL(1,aClient.m_nModifyCount);
     CPPUNIT_ASSERT_EQUAL(1,aListener.m_nNotifyCount);

sw/source/core/attr/calbck.cxx
@@ -181,7 +181,7 @@ void SwModify::NotifyClients( const SfxPoolItem* pOldValue, const SfxPoolItem* p
         return;
 
     LockModify();
-    ModifyBroadcast( pOldValue, pNewValue );
+    CallSwClientNotify( sw::LegacyModifyHint{ pOldValue, pNewValue } );
     UnlockModify();
 }
 

commit 337b60c9e987dcfefb719ce538b10a407bb548f4

tdf#146605 sw: try to fix SwSectionFormat notifications
SwSectionFormat no longer notifies via its SwModify to SwClient
listeners, but only via SvtBroadcaster, so SwSection and SwSectionFrame
need to be SvtListeners.

This can be seen when changing the footnote-at-end setting in the
section dialog, if the SwSection doesn't get the event the footnote
numbers aren't adapted to a new restart value, if the SwSectionFrame
doesn't get the event the footnote container isn't moved.

This is of course not pretty but hopefully it will be refactored further
in the future.

(regression from commit 2e32f4ed5af16a68c97a50806a42ffa2d10f1d7a)

Change-Id: I93c616e209a01fc5d4c8eba701a6ddce5d5b432e

sw/inc/section.hxx
@@ -24,6 +24,7 @@
 
 #include <tools/ref.hxx>
 #include <svl/hint.hxx>
+#include <svl/listener.hxx>
 #include <sfx2/lnkbase.hxx>
 #include <sfx2/Metadatable.hxx>
 
@@ -137,6 +138,7 @@ public:
 
 class SW_DLLPUBLIC SwSection
     : public SwClient
+    , public SvtListener // needed for SwClientNotify to be called from SwSectionFormat
 {
     // In order to correctly maintain the flag when creating/deleting frames.
     friend class SwSectionNode;
@@ -153,6 +155,7 @@ private:
             bool const bHidden, bool const bCondition);
 
 protected:
+    virtual void Notify(SfxHint const& rHint) override;
     virtual void SwClientNotify(const SwModify&, const SfxHint&) override;
 
 public:

sw/source/core/docnode/section.cxx
@@ -198,6 +198,8 @@ SwSection::SwSection(
     : SwClient(& rFormat)
     , m_Data(eType, rName)
 {
+    StartListening(rFormat.GetNotifier());
+
     SwSection *const pParentSect = GetParent();
     if( pParentSect )
     {
@@ -230,6 +232,7 @@ SwSection::~SwSection()
     else
     {
         pFormat->Remove( this ); // remove
+        SvtListener::EndListeningAll();
 
         if (SectionType::Content != m_Data.GetType())
         {
@@ -400,6 +403,11 @@ void SwSection::SetEditInReadonly(bool const bFlag)
 }
 
 void SwSection::SwClientNotify(const SwModify&, const SfxHint& rHint)
+{
+    Notify(rHint);
+}
+
+void SwSection::Notify(SfxHint const& rHint)
 {
     if (rHint.GetId() != SfxHintId::SwLegacyModify)
         return;

sw/source/core/inc/sectfrm.hxx
@@ -22,6 +22,8 @@
 #include "layfrm.hxx"
 #include "flowfrm.hxx"
 
+#include <svl/listener.hxx>
+
 class SwSection;
 class SwSectionFormat;
 class SwAttrSetChg;
@@ -45,6 +47,7 @@ namespace o3tl {
 }
 
 class SwSectionFrame final: public SwLayoutFrame, public SwFlowFrame
+    , public SvtListener // TODO?
 {
     SwSection* m_pSection;
     bool m_bFootnoteAtEnd; // footnotes at the end of section
@@ -70,6 +73,7 @@ class SwSectionFrame final: public SwLayoutFrame, public SwFlowFrame
     virtual void MakeAll(vcl::RenderContext* pRenderContext) override;
     virtual bool ShouldBwdMoved( SwLayoutFrame *pNewUpper, bool &rReformat ) override;
     virtual void Format( vcl::RenderContext* pRenderContext, const SwBorderAttrs *pAttrs = nullptr ) override;
+    virtual void Notify(SfxHint const& rHint) override;
     virtual void SwClientNotify( const SwModify&, const SfxHint& ) override;
 
 public:

sw/source/core/layout/sectfrm.cxx
@@ -74,6 +74,8 @@ SwSectionFrame::SwSectionFrame( SwSection &rSect, SwFrame* pSib )
     , m_bOwnFootnoteNum(false)
     , m_bFootnoteLock(false)
 {
+    StartListening(rSect.GetFormat()->GetNotifier());
+
     mnFrameType = SwFrameType::Section;
 
     CalcFootnoteAtEndFlag();
@@ -90,6 +92,8 @@ SwSectionFrame::SwSectionFrame( SwSectionFrame &rSect, bool bMaster ) :
     m_bOwnFootnoteNum( false ),
     m_bFootnoteLock( false )
 {
+    StartListening(rSect.GetFormat()->GetNotifier());
+
     mnFrameType = SwFrameType::Section;
 
     PROTOCOL( this, PROT::Section, bMaster ? DbgAction::CreateMaster : DbgAction::CreateFollow, &rSect )
@@ -2565,6 +2569,13 @@ void SwSectionFrame::CalcEndAtEndFlag()
             break;
         m_bEndnAtEnd = pFormat->GetEndAtTextEnd( false ).IsAtEnd();
     }
+}
+
+void SwSectionFrame::Notify(SfxHint const& rHint)
+{
+    SwSectionFormat *const pFormat(GetSection()->GetFormat());
+    assert(pFormat);
+    SwClientNotify(*pFormat, rHint);
 }
 
 void SwSectionFrame::SwClientNotify(const SwModify& rMod, const SfxHint& rHint)