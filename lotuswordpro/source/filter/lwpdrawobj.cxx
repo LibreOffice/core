fb34972b945b73ab86caf8dd3f4d7abb701f2299
Caolán McNamara <caolanm@redhat.com>
2022-01-12 09:51:30 +0000

    ofz#43551 Use-of-uninitialized-value
    Change-Id: I2eb139f3bff296cff7d8f0e9a05f19df56b27ecd
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/128328
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
    Tested-by: Jenkins

--- lotuswordpro/source/filter/lwpdrawobj.cxx
+++ lotuswordpro/source/filter/lwpdrawobj.cxx
@@ -1023,6 +1023,7 @@ void LwpDrawTextBox::Read()
 
     m_pStream->ReadInt16( m_aTextRec.nTextHeight );
     m_pStream->ReadBytes(m_aTextRec.tmpTextFaceName, DRAW_FACESIZE);
+    m_aTextRec.tmpTextFaceName[DRAW_FACESIZE - 1] = 0;
     m_pStream->SeekRel(1);// PitchAndFamily
 
     m_pStream->ReadInt16( m_aTextRec.nTextSize );
@@ -1222,6 +1223,7 @@ void LwpDrawTextArt::Read()
     m_pStream->SeekRel(1);
 
     m_pStream->ReadBytes(m_aTextArtRec.tmpTextFaceName, DRAW_FACESIZE);
+    m_aTextArtRec.tmpTextFaceName[DRAW_FACESIZE - 1] = 0;
     m_pStream->SeekRel(1);// PitchAndFamily
 
     m_pStream->ReadInt16( m_aTextArtRec.nTextSize );
