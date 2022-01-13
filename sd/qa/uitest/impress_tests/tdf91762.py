fadffba749a503da4f0c6dac05bb17a6e059f6ea
Justin Luth <justin_luth@sil.org>
2022-01-13 12:43:56 +0200

    tdf#144092 revert tdf#80340, tdf#91762, tdf#137949, tdf#139511
    All of these commits are completely wrong.
    An empty cell is not irrelevant in making height decisions.
    
    The main problem is that PPTX import doesn't know how to
    pass the EndParaRPr sz character fontsize to an empty cell,
    and thus svx uses the default style (probably 18pt)
    instead of the proper size.
    
    There is probably also a separate problem of not passing
    the row size during import as well. tdf#139511 should be a
    good example of what goes wrong when you get that part wrong,
    and the reverted implementation didn't address it properly.
    
    This undoubtedly will raise lots of "regression" reports.
    Likely they are related to empty cells. If the fontsize in the LO cell
    matches the default style, it probably doesn't match MSO's fontsize.
    In that case, it isn't a regression - it just happened to look better.
    
    If the row specifies a larger size than minimum, that was "fixed"
    by bug 137949, but not in the correct way (see bug 144092 and bug 139511),
    and so it should just be reverted until a proper way
    to handle that is developed.
    So this case also wouldn't really be a true regression.
    
    Revert "tdf#139511 Correct calculation of minimum row height during resize."
    
    This reverts LO7.2-7.0.5 commit b6d53ce5359d4f41b9fece475339cdf63c5817e9.
    
    Revert "tdf#137949 Fix table row heigths."
    
    This reverts LO7.1/7.0.4 commit b7b05dd36403af50b20fe06cbf8a10d8defb28a9.
    
    Revert "tdf#91762: Check wether the row and the cell have text..."
    
    This reverts LO5.2-5.0.6 commit 13d4398820ded5914f635757865e258db2db2b57.
    
    Revert "tdf#80340: Table changes format in PPTX format"
    
    This reverts LO5.0 commit 4f2c8194f485b1527fb4f4dfe23ce804937f1f9c.
    
    Change-Id: I935e733aca5640479254bba9ac79eb865fe4aa4a

--- sd/qa/uitest/impress_tests/tdf91762.py
+++ sd/qa/uitest/impress_tests/tdf91762.py
@@ -40,7 +40,7 @@ class tdf91762(UITestCase):
 
             # tdf#138011: Without the fix in place, this test would have failed with
             # AssertionError: 5494 != 3559
-            self.assertEqual(5494, document.DrawPages[0].getByIndex(1).BoundRect.Height)
+            self.assertEqual(5504, document.DrawPages[0].getByIndex(1).BoundRect.Height)
 
 
 # vim: set shiftwidth=4 softtabstop=4 expandtab:
