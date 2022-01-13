2af65a7aa9054ebfd24d64e66e012b64daa8d3c7
Szymon Kłos <szymon.klos@collabora.com>
2022-01-13 19:58:34 +0100

    RTL: lok: mirror cursor position when needed
    Change-Id: I3e574e1b1d5e26528794c39cb631d8f5cf60e927

--- vcl/source/window/cursor.cxx
+++ vcl/source/window/cursor.cxx
@@ -257,7 +257,19 @@ void vcl::Cursor::LOKNotify( vcl::Window* pWindow, const OUString& rAction )
         if (!aSize.Width())
             aSize.setWidth( pWindow->GetSettings().GetStyleSettings().GetCursorSize() );
 
-        const tools::Rectangle aRect(Point(nX, nY), aSize);
+        Point aPos(nX, nY);
+
+        if (pWindow->IsRTLEnabled() && !pWindow->ImplIsAntiparallel() && pParent->GetOutDev())
+            pParent->GetOutDev()->ReMirror(aPos);
+
+        if (!pWindow->IsRTLEnabled() && pWindow->ImplIsAntiparallel()
+            && pWindow->GetOutDev() && pParent->GetOutDev())
+        {
+            pWindow->GetOutDev()->ReMirror(aPos);
+            pParent->GetOutDev()->ReMirror(aPos);
+        }
+
+        const tools::Rectangle aRect(aPos, aSize);
         aItems.emplace_back("rectangle", aRect.toString());
     }
 
