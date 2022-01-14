LibreOffice/core

commit ad94bb5d23d9c4ecbb2ca84807f01623862de43e

VCL drop m_pInstance from *nix SalData
AKA the "*nix SalData untangling" commit.

The original plan was to get rid of vcl/inc/saldatabasic.hxx and
even SalData for all the *nix backends. But after many backs and
forths, reinspecting the code and imagining the resulting code, I
decided against that plan. All these variants would have resulted
in reinterpret_cast calls, I wanted to prevent. And they would
have required larger renames for no benefit.

An other, related idea was to include all SalData implementations
in the vcl/inc/svdata.hxx header, but that seemed like an include
explosion, so was also dropped.

I tried to untangling iOS from using GenericUnixSalData, as it
doesn't use any of it's features. The new, minimal SalData should
be sufficient.

I'm leaving the easier drop of mpInstance from the Windows and
MacOSX backend as a minimal interesting EasyHack.

Change-Id: I5be01c1f42131a7e31cb30899392308e1e2de53b

vcl/headless/headlessinst.cxx
@@ -66,20 +66,11 @@ const OUString& SalGetDesktopEnvironment()
     return aEnv;
 }
 
-SalData::SalData()
-    : m_pInstance(nullptr)
-{
-}
-
-SalData::~SalData()
-{
-}
-
 // This is our main entry point:
 SalInstance *CreateSalInstance()
 {
     HeadlessSalInstance* pInstance = new HeadlessSalInstance(std::make_unique<SvpSalYieldMutex>());
-    new SvpSalData(pInstance);
+    new SvpSalData();
     pInstance->AcquireYieldMutex();
     return pInstance;
 }

vcl/ios/iosinst.cxx
@@ -22,7 +22,6 @@
 #include <postmac.h>
 
 #include "ios/iosinst.hxx"
-#include <headless/svpdata.hxx>
 #include "headless/svpdummies.hxx"
 #include "unx/gendata.hxx"
 #include "quartz/utils.h"
@@ -31,6 +30,17 @@
 
 // Totally wrong of course but doesn't seem to harm much in the iOS app.
 static int viewWidth = 1, viewHeight = 1;
+
+class SalData
+{
+    SystemFontList* mpFontList;
+    CGColorSpaceRef mxRGBSpace;
+    CGColorSpaceRef mxGraySpace;
+
+    static void ensureThreadAutoreleasePool() {};
+
+    explicit SalData();
+};
 
 void IosSalInstance::GetWorkArea( tools::Rectangle& rRect )
 {
@@ -42,10 +52,7 @@ IosSalInstance *IosSalInstance::getInstance()
 {
     if (!ImplGetSVData())
         return NULL;
-    SvpSalData *pData = static_cast<SvpSalData *>(ImplGetSVData()->mpSalData);
-    if (!pData)
-        return NULL;
-    return static_cast<IosSalInstance *>(pData->m_pInstance);
+    return static_cast<IosSalInstance *>(GetSalInstance());
 }
 
 IosSalInstance::IosSalInstance( std::unique_ptr<SalYieldMutex> pMutex )
@@ -147,16 +154,12 @@ const OUString& SalGetDesktopEnvironment()
 }
 
 SalData::SalData() :
-    m_pInstance( 0 ),
     mpFontList( 0 ),
     mxRGBSpace( CGColorSpaceCreateDeviceRGB() ),
     mxGraySpace( CGColorSpaceCreateDeviceGray() )
 {
 }
 
-SalData::~SalData()
-{
-}
 
 // This is our main entry point:
 SalInstance *CreateSalInstance()

vcl/source/app/salplug.cxx
@@ -35,7 +35,7 @@
 
 #include <unistd.h>
 #else
-#include <saldatabasic.hxx>
+#include <svdata.hxx>
 #include <o3tl/char16_t2wchar_t.hxx>
 #include <Windows.h>
 #endif
@@ -339,8 +339,8 @@ void DestroySalInstance( SalInstance *pInst )
 
 void SalAbort( const OUString& rErrorText, bool bDumpCore )
 {
-    if (GetSalData()->m_pInstance)
-        GetSalData()->m_pInstance->BeforeAbort(rErrorText, bDumpCore);
+    if (GetSalInstance())
+        GetSalInstance()->BeforeAbort(rErrorText, bDumpCore);
 
 #if defined _WIN32
     if( rErrorText.isEmpty() )
@@ -401,15 +401,6 @@ const OUString& SalGetDesktopEnvironment()
     static OUString aDesktopEnvironment("unknown");
 #endif
     return aDesktopEnvironment;
-}
-
-SalData::SalData() :
-    m_pInstance(nullptr)
-{
-}
-
-SalData::~SalData() COVERITY_NOEXCEPT_FALSE
-{
 }
 
 #ifdef _WIN32

vcl/source/app/svmain.cxx
@@ -52,7 +52,7 @@
 #include <debugevent.hxx>
 #include <scrwnd.hxx>
 #include <windowdev.hxx>
-#include <saldatabasic.hxx>
+#include <svdata.hxx>
 
 #ifdef _WIN32
 #include <svsys.h>
@@ -105,6 +105,10 @@
 #if OSL_DEBUG_LEVEL > 0
 #include <typeinfo>
 #include <rtl/strbuf.hxx>
+#endif
+
+#ifdef LINUX
+#include <unx/gendata.hxx>
 #endif
 
 using namespace ::com::sun::star;

solenv/clang-format/excludelist
@@ -14412,7 +14412,6 @@ vcl/inc/quartz/salvd.h
 vcl/inc/quartz/utils.h
 vcl/inc/regband.hxx
 vcl/inc/salbmp.hxx
-vcl/inc/saldatabasic.hxx
 vcl/inc/salframe.hxx
 vcl/inc/salgdi.hxx
 vcl/inc/salgdiimpl.hxx

vcl/android/androidinst.cxx
@@ -36,10 +36,7 @@ AndroidSalInstance *AndroidSalInstance::getInstance()
 {
     if (!ImplGetSVData())
         return NULL;
-    SvpSalData *pData = static_cast<SvpSalData *>(ImplGetSVData()->mpSalData);
-    if (!pData)
-        return NULL;
-    return static_cast<AndroidSalInstance *>(pData->m_pInstance);
+    return static_cast<AndroidSalInstance *>(GetSalInstance());
 }
 
 AndroidSalInstance::AndroidSalInstance( std::unique_ptr<SalYieldMutex> pMutex )
@@ -161,7 +158,7 @@ extern "C" SalInstance *create_SalInstance()
 {
     LOGI("Android: CreateSalInstance!");
     AndroidSalInstance* pInstance = new AndroidSalInstance( std::make_unique<SvpSalYieldMutex>() );
-    new SvpSalData(pInstance);
+    new SvpSalData();
     return pInstance;
 }
 

vcl/headless/svpdata.cxx
@@ -14,7 +14,7 @@
 SalInstance* svp_create_SalInstance()
 {
     SvpSalInstance* pInstance = new SvpSalInstance( std::make_unique<SvpSalYieldMutex>() );
-    new SvpSalData( pInstance );
+    new SvpSalData();
     return pInstance;
 }
 

vcl/headless/svpinst.cxx
@@ -52,7 +52,6 @@
 
 #include <salframe.hxx>
 #include <svdata.hxx>
-#include <unx/gendata.hxx>
 // FIXME: remove when we re-work the svp mainloop
 #include <unx/salunxtime.h>
 #include <comphelper/lok.hxx>
@@ -339,7 +338,7 @@ SvpSalYieldMutex::~SvpSalYieldMutex()
 
 void SvpSalYieldMutex::doAcquire(sal_uInt32 const nLockCount)
 {
-    SvpSalInstance *const pInst = static_cast<SvpSalInstance *>(GetSalData()->m_pInstance);
+    auto *const pInst = static_cast<SvpSalInstance*>(GetSalInstance());
     if (pInst && pInst->IsMainThread())
     {
         if (m_bNoYieldLock)
@@ -386,7 +385,7 @@ void SvpSalYieldMutex::doAcquire(sal_uInt32 const nLockCount)
 
 sal_uInt32 SvpSalYieldMutex::doRelease(bool const bUnlockAll)
 {
-    SvpSalInstance *const pInst = static_cast<SvpSalInstance *>(GetSalData()->m_pInstance);
+    auto *const pInst = static_cast<SvpSalInstance*>(GetSalInstance());
     if (pInst && pInst->IsMainThread())
     {
         if (m_bNoYieldLock)
@@ -420,7 +419,7 @@ sal_uInt32 SvpSalYieldMutex::doRelease(bool const bUnlockAll)
 
 bool SvpSalYieldMutex::IsCurrentThread() const
 {
-    if (GetSalData()->m_pInstance->IsMainThread() && m_bNoYieldLock)
+    if (GetSalInstance()->IsMainThread() && m_bNoYieldLock)
         return true;
     else
         return SalYieldMutex::IsCurrentThread();

commit 8380955a7963e25e3313569bf72d77cd7d8b3665

VCL move SolarMutex (un)locking into (De)InitVCL
Fixes all the backends, where SolarMutex access needs SalInstance.
The ImplSVData::SalInstance is just set fron the return value of
CreateSalInstance, so it can't be accessed earlier, globally.

Regression from commit ad94bb5d23d9c4ecbb2ca84807f01623862de43e
("VCL drop m_pInstance from *nix SalData).

Change-Id: I13259009571a9973ea7e3ded20c0ea4c660ee214

vcl/headless/headlessinst.cxx
@@ -71,13 +71,11 @@ SalInstance *CreateSalInstance()
 {
     HeadlessSalInstance* pInstance = new HeadlessSalInstance(std::make_unique<SvpSalYieldMutex>());
     new SvpSalData();
-    pInstance->AcquireYieldMutex();
     return pInstance;
 }
 
 void DestroySalInstance( SalInstance *pInst )
 {
-    pInst->ReleaseYieldMutexAll();
     delete pInst;
 }
 

vcl/ios/iosinst.cxx
@@ -166,13 +166,11 @@ SalInstance *CreateSalInstance()
 {
     IosSalInstance* pInstance = new IosSalInstance( std::make_unique<SvpSalYieldMutex>() );
     new SvpSalData(pInstance);
-    pInstance->AcquireYieldMutex();
     return pInstance;
 }
 
 void DestroySalInstance( SalInstance *pInst )
 {
-    pInst->ReleaseYieldMutexAll();
     delete pInst;
 }
 

vcl/source/app/salplug.cxx
@@ -319,17 +319,11 @@ SalInstance *CreateSalInstance()
         _exit( 1 );
     }
 
-    // acquire SolarMutex
-    pInst->AcquireYieldMutex();
-
     return pInst;
 }
 
 void DestroySalInstance( SalInstance *pInst )
 {
-    // release SolarMutex
-    pInst->ReleaseYieldMutexAll();
-
     delete pInst;
 #ifndef DISABLE_DYNLOADING
     if( pCloseModule )

vcl/source/app/svmain.cxx
@@ -321,6 +321,7 @@ bool InitVCL()
     pSVData->mpDefInst = CreateSalInstance();
     if ( !pSVData->mpDefInst )
         return false;
+    pSVData->mpDefInst->AcquireYieldMutex();
 
     // Desktop Environment context (to be able to get value of "system.desktop-environment" as soon as possible)
     css::uno::setCurrentContext(
@@ -598,6 +599,7 @@ void DeInitVCL()
     // Deinit Sal
     if (pSVData->mpDefInst)
     {
+        pSVData->mpDefInst->ReleaseYieldMutexAll();
         DestroySalInstance( pSVData->mpDefInst );
         pSVData->mpDefInst = nullptr;
     }