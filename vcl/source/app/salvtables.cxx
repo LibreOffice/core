ca65da1974364069706d8f57e69eeaf018065516
Szymon Kłos <szymon.klos@collabora.com>
2022-01-17 13:11:19 +0100

    RTL: lok: render correctly interim windows
    Change-Id: Iec23fba24464c6d29ecb63d3d2018ded8d9937f1

--- vcl/source/app/salvtables.cxx
+++ vcl/source/app/salvtables.cxx
@@ -1233,6 +1233,8 @@ void DoRecursivePaint(vcl::Window* pWindow, const Point& rPos, OutputDevice& rOu
 
     rOutput.DrawOutDev(rPos, aSize, Point(), aSize, *xOutput);
 
+    bool bHasMirroredGraphics = pWindow->GetOutDev()->HasMirroredGraphics();
+
     xOutput.disposeAndClear();
 
     for (vcl::Window* pChild = pWindow->GetWindow(GetWindowType::FirstChild); pChild;
@@ -1240,7 +1242,17 @@ void DoRecursivePaint(vcl::Window* pWindow, const Point& rPos, OutputDevice& rOu
     {
         if (!pChild->IsVisible())
             continue;
-        DoRecursivePaint(pChild, rPos + pChild->GetPosPixel(), rOutput);
+
+        tools::Long nDeltaX = pChild->GetOutOffXPixel() - pWindow->GetOutOffXPixel();
+        if (bHasMirroredGraphics)
+            nDeltaX = pWindow->GetOutputWidthPixel() - nDeltaX - pChild->GetOutputWidthPixel();
+
+        tools::Long nDeltaY = pChild->GetOutOffYPixel() - pWindow->GetOutOffYPixel();
+
+        Point aPos(rPos);
+        aPos += Point(nDeltaX, nDeltaY);
+
+        DoRecursivePaint(pChild, aPos, rOutput);
     }
 }
 }
