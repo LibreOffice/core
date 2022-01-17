0bc1739210e5b7d462400ef194a495ec95588366
Xisco Fauli <xiscofauli@libreoffice.org>
2022-01-17 14:56:20 +0100

    tdf#145321: sw: Add UItest
    This test has been implemented as a UItest
    because it doesn't crash as CppUnittest
    
    Change-Id: I1d3ae34ad6c394b6bbc6e5d60675b85700aa0da5

---
+++ sw/qa/uitest/writer_tests5/tdf145321.py
@@ -0,0 +1,34 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+from uitest.framework import UITestCase
+from uitest.uihelper.common import get_url_for_data_file, get_state_as_dict
+
+class tdf145321(UITestCase):
+
+    # This test has been implemented as a UItest because it doesn't crash as CppUnittest
+    def test_tdf145321(self):
+
+        with self.ui_test.load_file(get_url_for_data_file("tdf145321.odt")) as writer_doc:
+            xWriterDoc = self.xUITest.getTopFocusWindow()
+            xWriterEdit = xWriterDoc.getChild("writer_edit")
+
+            for j in range(5):
+                for i in range(1,7):
+                    self.assertEqual(str(i), get_state_as_dict(xWriterEdit)["CurrentPage"])
+
+                    self.xUITest.executeCommand(".uno:PageDown")
+
+                # Without the fix in place, this test would have crashed here
+                for i in range(6, 0, -1):
+                    self.assertEqual(str(i), get_state_as_dict(xWriterEdit)["CurrentPage"])
+
+                    self.xUITest.executeCommand(".uno:PageUp")
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
