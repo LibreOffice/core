LibreOffice/core

commit b76628acb1ae4fc06f8c1b70ec2e0cf39356deef

text export support for bullets and hyperlinks
SVGFilter:
- new:
implExportTextShapeIndex, implEmbedBulletGlyphs, implEmbedBulletGlyph,
implExportTextEmbeddedBitmaps
- modified:
implExportShape, implCreateObjectsFromShapes, implCreateObjectsFromShape

SVGActionWriter:
- new:
GetChecksum
- modified:
ImplWriteActions, WriteMetaFile
 
SVGTextWriter:
-new:
implExportHyperlinkIds, implWriteBulletChars, writeBitmapPlaceholder, 
implWriteEmbeddedBitmaps, setTextShape, implRegisterInterface, 
implGetValidIDFromInterface
-modified:
nextParagraph, nextTextPortion, startTextShape, endTextShape, startTextParagraph, 
endTextParagraph, writeTextPortion, implWriteTextPortion

filter/source/svg/svgexport.cxx
@@ -89,6 +89,7 @@ static const char    aOOOAttrDateTimeField[] = NSPREFIX "date-time-field";
 static const char    aOOOAttrFooterField[] = NSPREFIX "footer-field";
 static const char    aOOOAttrHeaderField[] = NSPREFIX "header-field";
 static const char    aOOOAttrHasTransition[] = NSPREFIX "has-transition";
+static const char    aOOOAttrIdList[] = NSPREFIX "id-list";
 
 // ooo xml attributes for pages and shapes
 static const char    aOOOAttrName[] = NSPREFIX "name";
@@ -800,12 +801,16 @@ sal_Bool SVGFilter::implExportDocument()
             mpSVGFontExport = new SVGFontExport( *mpSVGExport, aObjects );
             mpSVGWriter = new SVGActionWriter( *mpSVGExport, *mpSVGFontExport );
 
-
             if( mpSVGExport->IsEmbedFonts() )
             {
                 mpSVGFontExport->EmbedFonts();
             }
-
+            if( !mpSVGExport->IsUsePositionedCharacters() )
+            {
+                implExportTextShapeIndex();
+                implEmbedBulletGlyphs();
+                implExportTextEmbeddedBitmaps();
+            }
             implExportMasterPages( mMasterPageTargets, 0, mMasterPageTargets.getLength() - 1 );
             implExportDrawPages( mSelectedPages, 0, nLastPage );
 
@@ -1138,6 +1143,154 @@ sal_Bool SVGFilter::implExportAnimations()
     return bRet;
 }
 
+// -----------------------------------------------------------------------------
+
+void SVGFilter::implExportTextShapeIndex()
+{
+    mpSVGExport->AddAttribute( XML_NAMESPACE_NONE, "class", B2UCONST( "TextShapeIndex" )  );
+    SvXMLElementExport aDefsContainerElem( *mpSVGExport, XML_NAMESPACE_NONE, "defs", sal_True, sal_True );
+
+    sal_Int32 nCount = mSelectedPages.getLength();
+    for( sal_Int32 i = 0; i < nCount; ++i )
+    {
+        const Reference< XDrawPage > & xDrawPage = mSelectedPages[i];
+        if( mTextShapeIdListMap.find( xDrawPage ) != mTextShapeIdListMap.end() )
+        {
+            OUString sTextShapeIdList = mTextShapeIdListMap[xDrawPage].trim();
+
+            Reference< XInterface > xRef( xDrawPage, UNO_QUERY );
+            const OUString& rPageId = implGetValidIDFromInterface( xRef );
+            if( !rPageId.isEmpty() && !sTextShapeIdList.isEmpty() )
+            {
+                mpSVGExport->AddAttribute( XML_NAMESPACE_NONE, aOOOAttrSlide, rPageId  );
+                mpSVGExport->AddAttribute( XML_NAMESPACE_NONE, aOOOAttrIdList, sTextShapeIdList );
+                SvXMLElementExport aGElem( *mpSVGExport, XML_NAMESPACE_NONE, "g", sal_True, sal_True );
+            }
+        }
+    }
+}
+
+// -----------------------------------------------------------------------------
+
+void SVGFilter::implEmbedBulletGlyphs()
+{
+    mpSVGExport->AddAttribute( XML_NAMESPACE_NONE, "class", B2UCONST( "EmbeddedBulletChars" )  );
+    SvXMLElementExport aDefsContainerElem( *mpSVGExport, XML_NAMESPACE_NONE, "defs", sal_True, sal_True );
+
+    OUString sPathData =  B2UCONST( "M 580,1141 L 1163,571 580,0 -4,571 580,1141 Z" );
+    implEmbedBulletGlyph( 57356, sPathData );
+    sPathData =  B2UCONST( "M 8,1128 L 1137,1128 1137,0 8,0 8,1128 Z" );
+    implEmbedBulletGlyph( 57354, sPathData );
+    sPathData =  B2UCONST( "M 174,0 L 602,739 174,1481 1456,739 174,0 Z M 1358,739 L 309,1346 659,739 1358,739 Z" );
+    implEmbedBulletGlyph( 10146, sPathData );
+    sPathData =  B2UCONST( "M 2015,739 L 1276,0 717,0 1260,543 174,543 174,936 1260,936 717,1481 1274,1481 2015,739 Z" );
+    implEmbedBulletGlyph( 10132, sPathData );
+    sPathData =  B2UCONST( "M 0,-2 C -7,14 -16,27 -25,37 L 356,567 C 262,823 215,952 215,954 215,979 228,992 255,992 264,992 276,990 289,987 310,991 331,999 354,1012 L 381,999 492,748 772,1049 836,1024 860,1049 C 881,1039 901,1025 922,1006 886,937 835,863 770,784 769,783 710,716 594,584 L 774,223 C 774,196 753,168 711,139 L 727,119 C 717,90 699,76 672,76 641,76 570,178 457,381 L 164,-76 C 142,-110 111,-127 72,-127 30,-127 9,-110 8,-76 1,-67 -2,-52 -2,-32 -2,-23 -1,-13 0,-2 Z" );
+    implEmbedBulletGlyph( 10007, sPathData );
+    sPathData =  B2UCONST( "M 285,-33 C 182,-33 111,30 74,156 52,228 41,333 41,471 41,549 55,616 82,672 116,743 169,778 240,778 293,778 328,747 346,684 L 369,508 C 377,444 397,411 428,410 L 1163,1116 C 1174,1127 1196,1133 1229,1133 1271,1133 1292,1118 1292,1087 L 1292,965 C 1292,929 1282,901 1262,881 L 442,47 C 390,-6 338,-33 285,-33 Z" );
+    implEmbedBulletGlyph( 10004, sPathData );
+    sPathData =  B2UCONST( "M 813,0 C 632,0 489,54 383,161 276,268 223,411 223,592 223,773 276,916 383,1023 489,1130 632,1184 813,1184 992,1184 1136,1130 1245,1023 1353,916 1407,772 1407,592 1407,412 1353,268 1245,161 1136,54 992,0 813,0 Z" );
+    implEmbedBulletGlyph( 9679, sPathData );
+    sPathData =  B2UCONST( "M 346,457 C 273,457 209,483 155,535 101,586 74,649 74,723 74,796 101,859 155,911 209,963 273,989 346,989 419,989 480,963 531,910 582,859 608,796 608,723 608,648 583,586 532,535 482,483 420,457 346,457 Z" );
+    implEmbedBulletGlyph( 8226, sPathData );
+    sPathData =  B2UCONST( "M -4,459 L 1135,459 1135,606 -4,606 -4,459 Z" );
+    implEmbedBulletGlyph( 8211, sPathData );
+}
+
+// -----------------------------------------------------------------------------
+
+void SVGFilter::implEmbedBulletGlyph( sal_Unicode cBullet, const ::rtl::OUString & sPathData )
+{
+    OUString sId = B2UCONST( "bullet-char-template(" );
+    sId += OUString::valueOf( (sal_Int32)cBullet );
+    sId += B2UCONST( ")" );
+    mpSVGExport->AddAttribute( XML_NAMESPACE_NONE, "id", sId );
+
+    double fFactor = 1.0 / 2048;
+    OUString sFactor = OUString::valueOf( fFactor );
+    OUString sTransform = B2UCONST( "scale(" );
+    sTransform += sFactor; sTransform += B2UCONST( ",-" ); sTransform += sFactor;
+    sTransform += B2UCONST( ")" );
+    mpSVGExport->AddAttribute( XML_NAMESPACE_NONE, "transform", sTransform );
+
+    SvXMLElementExport aGElem( *mpSVGExport, XML_NAMESPACE_NONE, "g", sal_True, sal_True );
+
+    mpSVGExport->AddAttribute( XML_NAMESPACE_NONE, "d", sPathData );
+    SvXMLElementExport aPathElem( *mpSVGExport, XML_NAMESPACE_NONE, "path", sal_True, sal_True );
+
+}
+
+// -----------------------------------------------------------------------------
+
+/** SVGFilter::implExportTextEmbeddedBitmaps
+ *  We export bitmaps embedded into text shapes, such as those used by list
+ *  items with image style, only once in a specic <defs> element.
+ */
+sal_Bool SVGFilter::implExportTextEmbeddedBitmaps()
+{
+    mpSVGExport->AddAttribute( XML_NAMESPACE_NONE, "class", B2UCONST( "TextEmbeddedBitmaps" )  );
+    SvXMLElementExport aDefsContainerElem( *mpSVGExport, XML_NAMESPACE_NONE, "defs", sal_True, sal_True );
+
+    OUString sId;
+
+    MetaBitmapActionSet::const_iterator it = mEmbeddedBitmapActionSet.begin();
+    MetaBitmapActionSet::const_iterator end = mEmbeddedBitmapActionSet.end();
+    for( ; it != end; ++it)
+    {
+        const GDIMetaFile& aMtf = it->GetRepresentation();
+
+        if( aMtf.GetActionSize() == 1 )
+        {
+            MetaBmpExScaleAction* pAction = (MetaBmpExScaleAction*) aMtf.GetAction( 0 );
+            if( pAction )
+            {
+                sal_uLong nId = pAction->GetBitmapEx().GetChecksum();
+                sId = B2UCONST( "bitmap(" );
+                sId += OUString::valueOf( (sal_Int64)nId );
+                sId += B2UCONST( ")" );
+                mpSVGExport->AddAttribute( XML_NAMESPACE_NONE, "id", sId );
+
+                const Reference< XShape >& rxShape = (const Reference< XShape >&)( it->GetObject() );
+                Reference< XPropertySet > xShapePropSet( rxShape, UNO_QUERY );
+                ::com::sun::star::awt::Rectangle    aBoundRect;
+                if( xShapePropSet.is() && ( xShapePropSet->getPropertyValue( B2UCONST( "BoundRect" ) ) >>= aBoundRect ) )
+                {
+                    // Origin of the coordinate device must be (0,0).
+                    const Point aTopLeft;
+                    const Size  aSize( aBoundRect.Width, aBoundRect.Height );
+
+                    const Point aPt = pAction->GetPoint();
+                    // The image must be exported with x, y attribute set to 0,
+                    // on the contrary when referenced by a <use> element,
+                    // specifying the wanted position, they will result
+                    // misplaced.
+                    pAction->Move( -aPt.X(), -aPt.Y() );
+                    mpSVGWriter->WriteMetaFile( aTopLeft, aSize, aMtf, SVGWRITER_WRITE_ALL, NULL );
+                    // We reset to the original values so that when the <use>
+                    // element is created the x, y attributes are correct.
+                    pAction->Move( aPt.X(), aPt.Y() );
+                }
+                else
+                {
+                    OSL_FAIL( "implExportTextEmbeddedBitmaps: no shape bounding box." );
+                    return sal_False;
+                }
+            }
+            else
+            {
+                OSL_FAIL( "implExportTextEmbeddedBitmaps: metafile should have MetaBmpExScaleAction only." );
+                return sal_False;
+            }
+        }
+        else
+        {
+            OSL_FAIL( "implExportTextEmbeddedBitmaps: metafile should have a single action." );
+            return sal_False;
+        }
+
+    }
+    return sal_True;
+}
 
 // -----------------------------------------------------------------------------
 
@@ -1572,11 +1725,21 @@ sal_Bool SVGFilter::implExportShape( const Reference< XShape >& rxShape )
                         //mpSVGExport->AddAttributeIdLegacy( XML_NAMESPACE_DRAW, rShapeId );
                     }
 
+                    const GDIMetaFile* pEmbeddedBitmapsMtf = NULL;
+                    if( mEmbeddedBitmapActionMap.find( rxShape ) !=  mEmbeddedBitmapActionMap.end() )
                     {
-                        Reference< XText > xText( rxShape, UNO_QUERY );
-                        mpSVGWriter->bIsTextShape = xText.is();
+                        pEmbeddedBitmapsMtf = &( mEmbeddedBitmapActionMap[ rxShape ].GetRepresentation() );
+                    }
+
+                    {
+//                        Reference< XText > xText( rxShape, UNO_QUERY );
+//                        mpSVGWriter->bIsTextShape = xText.is();
                         SvXMLElementExport aExp2( *mpSVGExport, XML_NAMESPACE_NONE, "g", sal_True, sal_True );
-                        mpSVGWriter->WriteMetaFile( aTopLeft, aSize, rMtf, SVGWRITER_WRITE_ALL, pElementId );
+                        mpSVGWriter->WriteMetaFile( aTopLeft, aSize, rMtf,
+                                                    SVGWRITER_WRITE_ALL,
+                                                    pElementId,
+                                                    &rxShape,
+                                                    pEmbeddedBitmapsMtf );
                     }
                 }
 
@@ -1606,7 +1769,7 @@ sal_Bool SVGFilter::implCreateObjects()
             Reference< XShapes > xShapes( xMasterPage, UNO_QUERY );
 
             if( xShapes.is() )
-                implCreateObjectsFromShapes( xShapes );
+                implCreateObjectsFromShapes( xMasterPage, xShapes );
         }
     }
 
@@ -1641,7 +1804,7 @@ sal_Bool SVGFilter::implCreateObjects()
             Reference< XShapes > xShapes( xDrawPage, UNO_QUERY );
 
             if( xShapes.is() )
-                implCreateObjectsFromShapes( xShapes );
+                implCreateObjectsFromShapes( xDrawPage, xShapes );
         }
     }
     return sal_True;
@@ -1649,7 +1812,7 @@ sal_Bool SVGFilter::implCreateObjects()
 
 // -----------------------------------------------------------------------------
 
-sal_Bool SVGFilter::implCreateObjectsFromShapes( const Reference< XShapes >& rxShapes )
+sal_Bool SVGFilter::implCreateObjectsFromShapes( const Reference< XDrawPage > & rxPage, const Reference< XShapes >& rxShapes )
 {
     Reference< XShape > xShape;
     sal_Bool            bRet = sal_False;
@@ -1657,7 +1820,7 @@ sal_Bool SVGFilter::implCreateObjectsFromShapes( const Reference< XShapes >& rxS
     for( sal_Int32 i = 0, nCount = rxShapes->getCount(); i < nCount; ++i )
     {
         if( ( rxShapes->getByIndex( i ) >>= xShape ) && xShape.is() )
-            bRet = implCreateObjectsFromShape( xShape ) || bRet;
+            bRet = implCreateObjectsFromShape( rxPage, xShape ) || bRet;
 
         xShape = NULL;
     }
@@ -1667,15 +1830,16 @@ sal_Bool SVGFilter::implCreateObjectsFromShapes( const Reference< XShapes >& rxS
 
 // -----------------------------------------------------------------------------
 
-sal_Bool SVGFilter::implCreateObjectsFromShape( const Reference< XShape >& rxShape )
+sal_Bool SVGFilter::implCreateObjectsFromShape( const Reference< XDrawPage > & rxPage, const Reference< XShape >& rxShape )
 {
     sal_Bool bRet = sal_False;
+
     if( rxShape->getShapeType().lastIndexOf( B2UCONST( "drawing.GroupShape" ) ) != -1 )
     {
         Reference< XShapes > xShapes( rxShape, UNO_QUERY );
 
         if( xShapes.is() )
-            bRet = implCreateObjectsFromShapes( xShapes );
+            bRet = implCreateObjectsFromShapes( rxPage, xShapes );
     }
     else
     {
@@ -1700,8 +1864,54 @@ sal_Bool SVGFilter::implCreateObjectsFromShape( const Reference< XShape >& rxSha
                     (*mpObjects)[ rxShape ] = ObjectRepresentation( rxShape, aMtf );
                 }
                 else
-                    (*mpObjects)[ rxShape ] = ObjectRepresentation( rxShape, aGraphic.GetGDIMetaFile() );
+                {
+                    Reference< XText > xText( rxShape, UNO_QUERY );
+                    sal_Bool bIsTextShape = xText.is();
 
+                    if( !mpSVGExport->IsUsePositionedCharacters() && bIsTextShape )
+                    {
+
+                        // We create a map of text shape ids.
+                        implRegisterInterface( rxShape );
+                        Reference< XInterface > xRef( rxShape, UNO_QUERY );
+                        const OUString& rShapeId = implGetValidIDFromInterface( xRef );
+                        if( !rShapeId.isEmpty() )
+                        {
+                            mTextShapeIdListMap[rxPage] += rShapeId;
+                            mTextShapeIdListMap[rxPage] += B2UCONST( " " );
+                        }
+
+                        // We create a set of bitmaps embedded into text shape.
+                        GDIMetaFile aMtf;
+                        const Point    aNullPt;
+                        const Size    aSize( pObj->GetCurrentBoundRect().GetSize() );
+                        MetaAction*   pAction;
+                        const GDIMetaFile& rMtf = aGraphic.GetGDIMetaFile();
+                        sal_uLong nCount = rMtf.GetActionSize();
+                        for( sal_uLong nCurAction = 0; nCurAction < nCount; ++nCurAction )
+                        {
+                            pAction = rMtf.GetAction( nCurAction );
+                            const sal_uInt16    nType = pAction->GetType();
+
+                            if( nType == META_BMPEXSCALE_ACTION )
+                            {
+                                GDIMetaFile aEmbeddedBitmapMtf;
+                                pAction->Duplicate();
+                                aEmbeddedBitmapMtf.AddAction( pAction );
+                                aEmbeddedBitmapMtf.SetPrefSize( aSize );
+                                aEmbeddedBitmapMtf.SetPrefMapMode( MAP_100TH_MM );
+                                mEmbeddedBitmapActionSet.insert( ObjectRepresentation( rxShape, aEmbeddedBitmapMtf ) );
+                                pAction->Duplicate();
+                                aMtf.AddAction( pAction );
+                            }
+                        }
+                        aMtf.SetPrefSize( aSize );
+                        aMtf.SetPrefMapMode( MAP_100TH_MM );
+                        mEmbeddedBitmapActionMap[ rxShape ] = ObjectRepresentation( rxShape, aMtf );
+                    }
+
+                    (*mpObjects)[ rxShape ] = ObjectRepresentation( rxShape, aGraphic.GetGDIMetaFile() );
+                }
                 bRet = sal_True;
             }
         }

commit 0d6ddbbd3c52bbd9cc75d9e5acfa7a995f656a99

tdf#115566 SVG export: fix missing custom bullets
It seems this was broken since b76628acb1ae4fc06f8c1b70ec2e0cf39356deef
(text export support for bullets and hyperlinks, 2012-08-11), the
problem is that SVGFilter::implEmbedBulletGlyphs() has a fixed list of
characters that are typically used as bullets, but e.g. "-" is missing
from that list.

Fix this by improving SVGTextWriter::implWriteBulletChars() to continue
working from those shared glyph paths when the glyph is in the fixed
list, but otherwise call GetTextOutline() to look up the path for the
custom bullet.

Change-Id: I3de8fab8dc6c78e273629d13566d1f9f289eb752
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/128495
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins
(cherry picked from commit bbc4360b5beb012adf1e2652328d3e18d66224aa)

filter/source/svg/svgexport.cxx
@@ -1527,6 +1527,7 @@ void SVGFilter::implEmbedBulletGlyph( sal_Unicode cBullet, const OUString & sPat
     mpSVGExport->AddAttribute( XML_NAMESPACE_NONE, "d", sPathData );
     SvXMLElementExport aPathElem( *mpSVGExport, XML_NAMESPACE_NONE, "path", true, true );
 
+    mpSVGExport->SetEmbeddedBulletGlyph(cBullet);
 }
 
 void SVGFilter::implExportBackgroundBitmaps()
@@ -2859,6 +2860,17 @@ void SVGExport::writeMtf( const GDIMetaFile& rMtf )
         aWriter.WriteMetaFile( aPoint100thmm, aSize100thmm, rMtf,
             SVGWRITER_WRITE_FILL | SVGWRITER_WRITE_TEXT );
     }
+}
+
+void SVGExport::SetEmbeddedBulletGlyph(sal_Unicode cBullet)
+{
+    maEmbeddedBulletGlyphs.insert(cBullet);
+}
+
+bool SVGExport::IsEmbeddedBulletGlyph(sal_Unicode cBullet) const
+{
+    auto it = maEmbeddedBulletGlyphs.find(cBullet);
+    return it != maEmbeddedBulletGlyphs.end();
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

filter/source/svg/svgfilter.hxx
@@ -66,6 +66,7 @@ class SVGExport : public SvXMLExport
     bool    mbIsUseOpacity;
     bool    mbIsUseNativeTextDecoration;
     bool    mbIsUsePositionedCharacters;
+    std::set<sal_Unicode> maEmbeddedBulletGlyphs;
 
 public:
 
@@ -83,6 +84,9 @@ public:
     bool IsUsePositionedCharacters() const { return mbIsUsePositionedCharacters; };
 
     void writeMtf( const GDIMetaFile& rMtf );
+
+    void SetEmbeddedBulletGlyph(sal_Unicode cBullet);
+    bool IsEmbeddedBulletGlyph(sal_Unicode cBullet) const;
 
 protected:
 

filter/source/svg/svgwriter.cxx
@@ -1420,11 +1420,12 @@ void SVGTextWriter::implWriteBulletChars()
 
             SvXMLElementExport aPositioningElem( mrExport, XML_NAMESPACE_NONE, aXMLElemG, true, true );
 
-            // <use transform="scale(font-size)" xlink:ref="/" >
+            if (mrExport.IsEmbeddedBulletGlyph(rInfo.cBulletChar))
             {
+                // <use transform="scale(font-size)" xlink:ref="/" >
                 // Add size attribute through a scaling
-                sScaling = "scale(" + OUString::number( rInfo.nFontSize ) +
-                           "," + OUString::number( rInfo.nFontSize )+ ")";
+                sScaling = "scale(" + OUString::number( rInfo.aFont.GetFontHeight() ) +
+                           "," + OUString::number( rInfo.aFont.GetFontHeight() )+ ")";
                 mrExport.AddAttribute( XML_NAMESPACE_NONE, "transform", sScaling );
 
                 // Add ref attribute
@@ -1433,6 +1434,21 @@ void SVGTextWriter::implWriteBulletChars()
                 mrExport.AddAttribute( XML_NAMESPACE_NONE, aXMLAttrXLinkHRef, sRefId );
 
                 SvXMLElementExport aRefElem( mrExport, XML_NAMESPACE_NONE, "use", true, true );
+            }
+            else
+            {
+                // <path d="...">
+                tools::PolyPolygon aPolyPolygon;
+                OUString aStr(rInfo.cBulletChar);
+                mpVDev->Push(vcl::PushFlags::FONT);
+                mpVDev->SetFont(rInfo.aFont);
+                if (mpVDev->GetTextOutline(aPolyPolygon, aStr))
+                {
+                    OUString aPathString(SVGActionWriter::GetPathString(aPolyPolygon, false));
+                    mrExport.AddAttribute(XML_NAMESPACE_NONE, "d", aPathString);
+                    SvXMLElementExport aPath(mrExport, XML_NAMESPACE_NONE, "path", true, true);
+                }
+                mpVDev->Pop();
             }
         } // close aPositioningElem
     }
@@ -1688,7 +1704,7 @@ void SVGTextWriter::implWriteTextPortion( const Point& rPos,
             {
                 sId += ".bp";
                 BulletListItemInfo& aBulletListItemInfo = maBulletListItemMap[ sId ];
-                aBulletListItemInfo.nFontSize = rFont.GetFontHeight();
+                aBulletListItemInfo.aFont = rFont;
                 aBulletListItemInfo.aColor = aTextColor;
                 aBulletListItemInfo.aPos = maTextPos;
                 aBulletListItemInfo.cBulletChar = mcBulletChar;

filter/source/svg/svgwriter.hxx
@@ -188,7 +188,7 @@ class GDIMetaFile;
 
 struct BulletListItemInfo
 {
-    tools::Long nFontSize;
+    vcl::Font aFont;
     Color aColor;
     Point aPos;
     sal_Unicode cBulletChar;

filter/qa/unit/data/custom-bullet.fodp
@@ -1,0 +1,36 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<office:document xmlns:presentation="urn:oasis:names:tc:opendocument:xmlns:presentation:1.0" xmlns:svg="urn:oasis:names:tc:opendocument:xmlns:svg-compatible:1.0" xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0" xmlns:style="urn:oasis:names:tc:opendocument:xmlns:style:1.0" xmlns:draw="urn:oasis:names:tc:opendocument:xmlns:drawing:1.0" xmlns:fo="urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0" xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0" office:version="1.3" office:mimetype="application/vnd.oasis.opendocument.presentation">
+  <office:automatic-styles>
+    <style:page-layout style:name="PM1">
+      <style:page-layout-properties fo:margin-top="0cm" fo:margin-bottom="0cm" fo:margin-left="0cm" fo:margin-right="0cm" fo:page-width="28cm" fo:page-height="15.75cm" style:print-orientation="landscape"/>
+    </style:page-layout>
+    <style:style style:name="gr1" style:family="graphic" style:parent-style-name="standard">
+      <style:graphic-properties draw:stroke="none" svg:stroke-color="#000000" draw:fill="none" draw:fill-color="#ffffff" draw:auto-grow-height="true" draw:auto-grow-width="false" fo:max-height="0cm" fo:min-height="0cm"/>
+    </style:style>
+    <text:list-style style:name="L1">
+      <text:list-level-style-bullet text:level="1" text:bullet-char="-">
+        <style:list-level-properties text:min-label-width="0.6cm"/>
+        <style:text-properties fo:font-family="OpenSymbol" style:font-style-name="Regular" style:font-charset="x-symbol" style:use-window-font-color="true" fo:font-size="45%"/>
+      </text:list-level-style-bullet>
+    </text:list-style>
+  </office:automatic-styles>
+  <office:master-styles>
+    <style:master-page style:name="Default" style:page-layout-name="PM1">
+    </style:master-page>
+  </office:master-styles>
+  <office:body>
+    <office:presentation>
+      <draw:page draw:name="page1" draw:master-page-name="Default">
+        <draw:frame draw:style-name="gr1" draw:text-style-name="P8" draw:layer="layout" svg:width="9.525cm" svg:height="0.962cm" svg:x="3.175cm" svg:y="2.54cm">
+          <draw:text-box>
+            <text:list text:style-name="L1">
+              <text:list-item>
+                <text:p>hello</text:p>
+              </text:list-item>
+            </text:list>
+          </draw:text-box>
+        </draw:frame>
+      </draw:page>
+    </office:presentation>
+  </office:body>
+</office:document>

filter/qa/unit/svg.cxx
@@ -182,6 +182,33 @@ CPPUNIT_TEST_FIXTURE(SvgFilterTest, testShapeNographic)
     xStorable->storeToURL("private:stream", aMediaDescriptor.getAsConstPropertyValueList());
 }
 
+CPPUNIT_TEST_FIXTURE(SvgFilterTest, testCustomBullet)
+{
+    // Given a presentation with a custom bullet:
+    load(u"custom-bullet.fodp");
+
+    // When exporting that to SVG:
+    uno::Reference<frame::XStorable> xStorable(getComponent(), uno::UNO_QUERY_THROW);
+    SvMemoryStream aStream;
+    uno::Reference<io::XOutputStream> xOut = new utl::OOutputStreamWrapper(aStream);
+    utl::MediaDescriptor aMediaDescriptor;
+    aMediaDescriptor["FilterName"] <<= OUString("impress_svg_Export");
+    aMediaDescriptor["OutputStream"] <<= xOut;
+    xStorable->storeToURL("private:stream", aMediaDescriptor.getAsConstPropertyValueList());
+
+    // Then make sure the bullet glyph is not lost:
+    aStream.Seek(STREAM_SEEK_TO_BEGIN);
+    xmlDocUniquePtr pXmlDoc = parseXmlStream(&aStream);
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 1
+    // - Actual  : 0
+    // - XPath '//svg:g[@class='BulletChars']//svg:path' number of nodes is incorrect
+    // i.e. the custom bullet used '<use transform="scale(285,285)"
+    // xlink:href="#bullet-char-template-45"/>', but nobody produced a bullet-char-template-45,
+    // instead we need the path of the glyph inline.
+    CPPUNIT_ASSERT(!getXPath(pXmlDoc, "//svg:g[@class='BulletChars']//svg:path", "d").isEmpty());
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */