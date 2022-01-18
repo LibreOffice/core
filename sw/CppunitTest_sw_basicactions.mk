178971d0252cef1c90cfcdaba6257bed39a1b78b
Xisco Fauli <xiscofauli@libreoffice.org>
2022-01-18 23:51:30 +0100

    [DO NOT SUBMIT] Test basic actions with existing files
    Change-Id: I6b9ada91d3bf7f2764ca92a30159253b25c5d894

---
+++ sw/CppunitTest_sw_basicactions.mk
@@ -0,0 +1,68 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#*************************************************************************
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+#*************************************************************************
+
+$(eval $(call gb_CppunitTest_CppunitTest,sw_basicactions))
+
+$(eval $(call gb_CppunitTest_use_common_precompiled_header,sw_basicactions))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,sw_basicactions, \
+    sw/qa/extras/basicactions \
+))
+
+$(eval $(call gb_CppunitTest_use_libraries,sw_basicactions, \
+    comphelper \
+    editeng \
+    cppu \
+    cppuhelper \
+    sal \
+	svt \
+	sfx \
+	svl \
+	sw \
+	swqahelper \
+    test \
+    unotest \
+    vcl \
+    tl \
+	utl \
+))
+
+$(eval $(call gb_CppunitTest_use_externals,sw_basicactions,\
+	boost_headers \
+    libxml2 \
+))
+
+$(eval $(call gb_CppunitTest_set_include,sw_basicactions,\
+    -I$(SRCDIR)/sw/inc \
+    -I$(SRCDIR)/sw/source/core/inc \
+    -I$(SRCDIR)/sw/source/uibase/inc \
+	-I$(SRCDIR)/sw/qa/inc \
+    $$(INCLUDE) \
+))
+
+$(eval $(call gb_CppunitTest_use_api,sw_basicactions,\
+	udkapi \
+	offapi \
+	oovbaapi \
+))
+
+$(eval $(call gb_CppunitTest_use_ure,sw_basicactions))
+$(eval $(call gb_CppunitTest_use_vcl,sw_basicactions))
+
+$(eval $(call gb_CppunitTest_use_rdb,sw_basicactions,services))
+
+$(eval $(call gb_CppunitTest_use_configuration,sw_basicactions))
+
+$(eval $(call gb_CppunitTest_add_arguments,sw_basicactions, \
+    -env:arg-env=$(gb_Helper_LIBRARY_PATH_VAR)"$$$${$(gb_Helper_LIBRARY_PATH_VAR)+=$$$$$(gb_Helper_LIBRARY_PATH_VAR)}" \
+))
+
+# vim: set noet sw=4 ts=4:
