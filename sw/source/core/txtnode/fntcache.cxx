e3a1c2672af2f8fdce38b02b24f5c964e9a2706d
Caolán McNamara <caolanm@redhat.com>
2022-01-18 14:49:40 +0000

    set portion width using the last position in the dxarray
    so both branches are consistent in measuring mechanism
    
    Change-Id: Ie9b671ac69d00550d42328ac28c707c850238312

--- sw/source/core/txtnode/fntcache.cxx
+++ sw/source/core/txtnode/fntcache.cxx
@@ -2091,13 +2091,15 @@ Size SwFntObj::GetTextSize( SwDrawTextInfo& rInf )
     {
         if( !m_pPrtFont->IsSameInstance( rInf.GetOut().GetFont() ) )
             rInf.GetOut().SetFont( *m_pPrtFont );
+
+        std::vector<sal_Int32> aKernArray;
         SwTextGlyphsKey aGlyphsKey{ &rInf.GetOut(), rInf.GetText(), sal_Int32(rInf.GetIdx()), sal_Int32(nLn) };
+        SalLayoutGlyphs* pGlyphs = GetCachedSalLayoutGlyphs(aGlyphsKey);
+        rInf.GetOut().GetTextArray( rInf.GetText(), &aKernArray,
+                            sal_Int32(rInf.GetIdx()), sal_Int32(nLn), nullptr, pGlyphs);
+
         if( bCompress )
         {
-            std::vector<sal_Int32> aKernArray;
-            SalLayoutGlyphs* pGlyphs = GetCachedSalLayoutGlyphs(aGlyphsKey);
-            rInf.GetOut().GetTextArray( rInf.GetText(), &aKernArray,
-                                sal_Int32(rInf.GetIdx()), sal_Int32(nLn), nullptr, pGlyphs);
             rInf.SetKanaDiff( rInf.GetScriptInfo()->Compress( aKernArray.data(),
                 rInf.GetIdx(), nLn, rInf.GetKanaComp(),
                 o3tl::narrowing<sal_uInt16>(m_aFont.GetFontSize().Height()) ,lcl_IsFullstopCentered( rInf.GetOut() ) ) );
@@ -2105,7 +2107,7 @@ Size SwFntObj::GetTextSize( SwDrawTextInfo& rInf )
         }
         else
         {
-            aTextSize.setWidth( GetCachedTextWidth(aGlyphsKey, rInf.GetVclCache()));
+            aTextSize.setWidth(aKernArray[sal_Int32(nLn) - 1]);
             rInf.SetKanaDiff( 0 );
         }
 
