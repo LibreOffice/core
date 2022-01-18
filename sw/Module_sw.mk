559bea0465c39dcca8ef8e7551bbec22a9293cbb
Xisco Fauli <xiscofauli@libreoffice.org>
2022-01-18 23:51:30 +0100

    [DO NOT SUBMIT] Test basic actions with existing files
    Change-Id: I6b9ada91d3bf7f2764ca92a30159253b25c5d894

--- sw/Module_sw.mk
+++ sw/Module_sw.mk
@@ -60,6 +60,7 @@ $(eval $(call gb_Module_add_check_targets,sw,\
 $(eval $(call gb_Module_add_slowcheck_targets,sw,\
 	$(if $(and $(filter $(COM),MSC),$(MERGELIBS)),, \
 		CppunitTest_sw_uwriter) \
+    CppunitTest_sw_basicactions \
     CppunitTest_sw_rtfexport \
     CppunitTest_sw_rtfexport2 \
     CppunitTest_sw_rtfexport3 \
