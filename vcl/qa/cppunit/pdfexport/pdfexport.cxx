LibreOffice/core

commit 01dbb38680aa39a4d3bc7afd05d44a4b2c9bc6ab

tdf#61274 sd PDF export: fix links ending up on wrong pages with hidden slides
SdPage::IsExcluded() decides if a slide is hidden,
SdXImpressDocument::render() checks for this and returns early if
needed. In that case PDFExport::ExportSelection() detects that the
produced metafile has no actions and avoids creating a PDF page.

Then Impress links are created using the
vcl::PDFExtOutDevData::CreateLink() call in
drawinglayer::processor2d::VclMetafileProcessor2D::processTextHierarchyFieldPrimitive2D(),
not specifying the PDF page number explicitly. This means the link is
created on the "current" page number, set in
vcl::PDFExtOutDevData::SetCurrentPageNumber(), called by
PDFExport::ExportSelection(), but that filter/ code can't know about
hidden slides in sd/.

Fix the problem by setting the page number again in
SdXImpressDocument::render(), that way the link created by drawinglayer
will end on the correct page.

Change-Id: Ic29e345d45bc7c944d65e6e450f1d742dd0e9f8c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/90299
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

sd/source/ui/unoidl/unomodel.cxx
@@ -1893,6 +1893,14 @@ void SAL_CALL SdXImpressDocument::render( sal_Int32 nRenderer, const uno::Any& r
         (pPDFExtOutDevData && pPDFExtOutDevData->GetIsExportHiddenSlides())) )
         return;
 
+    if (pPDFExtOutDevData)
+    {
+        // Calculate the page number in the PDF output, which may be smaller than the page number in
+        // case of hidden slides.
+        sal_Int32 nOutputPageNum = CalcOutputPageNum(pPDFExtOutDevData, mpDoc, nPageNumber);
+        pPDFExtOutDevData->SetCurrentPageNumber(nOutputPageNum);
+    }
+
     std::unique_ptr<::sd::ClientView> pView( new ::sd::ClientView( mpDocShell, pOut ) );
     ::tools::Rectangle aVisArea( Point(), mpDoc->GetSdPage( static_cast<sal_uInt16>(nPageNumber) - 1, ePageKind )->GetSize() );
     vcl::Region                       aRegion( aVisArea );

vcl/qa/cppunit/pdfexport/pdfexport.cxx
@@ -142,6 +142,7 @@ public:
     void testPdfImageResourceInlineXObjectRef();
     void testReduceSmallImage();
     void testReduceImage();
+    void testLinkWrongPage();
 
     CPPUNIT_TEST_SUITE(PdfExportTest);
     CPPUNIT_TEST(testTdf106059);
@@ -181,6 +182,7 @@ public:
     CPPUNIT_TEST(testPdfImageResourceInlineXObjectRef);
     CPPUNIT_TEST(testReduceSmallImage);
     CPPUNIT_TEST(testReduceImage);
+    CPPUNIT_TEST(testLinkWrongPage);
     CPPUNIT_TEST_SUITE_END();
 };
 
@@ -1960,6 +1962,38 @@ void PdfExportTest::testReduceImage()
     // i.e. the image was scaled down even with ReduceImageResolution=false.
     CPPUNIT_ASSERT_EQUAL(160, nWidth);
     CPPUNIT_ASSERT_EQUAL(160, nHeight);
+}
+
+bool HasLinksOnPage(PageHolder& pPdfPage)
+{
+    int nStartPos = 0;
+    FPDF_LINK pLinkAnnot = nullptr;
+    return FPDFLink_Enumerate(pPdfPage.get(), &nStartPos, &pLinkAnnot);
+}
+
+void PdfExportTest::testLinkWrongPage()
+{
+    // Import the bugdoc and export as PDF.
+    OUString aURL = m_directories.getURLFromSrc(DATA_DIRECTORY) + "link-wrong-page.odp";
+    utl::MediaDescriptor aMediaDescriptor;
+    aMediaDescriptor["FilterName"] <<= OUString("impress_pdf_Export");
+    DocumentHolder pPdfDocument = exportAndParse(aURL, aMediaDescriptor);
+
+    // The document has 2 pages.
+    CPPUNIT_ASSERT_EQUAL(2, FPDF_GetPageCount(pPdfDocument.get()));
+
+    // First page should have 1 link (2nd slide, 1st was hidden).
+    PageHolder pPdfPage(FPDF_LoadPage(pPdfDocument.get(), /*page_index=*/0));
+    CPPUNIT_ASSERT(pPdfPage.get());
+
+    // Without the accompanying fix in place, this test would have failed, as the link of the first
+    // page went to the second page due to the hidden first slide.
+    CPPUNIT_ASSERT(HasLinksOnPage(pPdfPage));
+
+    // Second page should have no links (3rd slide).
+    PageHolder pPdfPage2(FPDF_LoadPage(pPdfDocument.get(), /*page_index=*/1));
+    CPPUNIT_ASSERT(pPdfPage2.get());
+    CPPUNIT_ASSERT(!HasLinksOnPage(pPdfPage2));
 }
 
 void PdfExportTest::testPdfImageResourceInlineXObjectRef()

commit 1e43098eaa99c2c62879ed6232b893976e11a00e

tdf#141340 PDF export: fix hyperlinks on the wrong page with page num range
Regression from commit 01dbb38680aa39a4d3bc7afd05d44a4b2c9bc6ab
(tdf#61274 sd PDF export: fix links ending up on wrong pages with hidden
slides, 2020-03-10), the problem was that the sd/ code that mapped page
numbers between the model and the PDF output only handled hidden slides,
but not partial exports.

Fix this by revisiting the decision to handle hidden slides in sd/, the
filter/ code at the end does have enough information to correctly do
this mapping at the end, and this way both tdf#61274 and tdf#141340 can
work at the same time.

Change-Id: I5679743ca67fab562e14c73e32f1a06ead8e7a31

filter/source/pdf/pdfexport.cxx
@@ -220,6 +220,7 @@ bool PDFExport::ExportSelection( vcl::PDFWriter& rPDFWriter,
                     aMtf.Stop();
                     aMtf.WindStart();
 
+                    bool bEmptyPage = false;
                     if( aMtf.GetActionSize() &&
                              ( !mbSkipEmptyPages || aPageSize.Width || aPageSize.Height ) )
                     {
@@ -244,6 +245,10 @@ bool PDFExport::ExportSelection( vcl::PDFWriter& rPDFWriter,
                         ImplExportPage(rPDFWriter, rPDFExtOutDevData, aMtf);
                         bRet = true;
                     }
+                    else
+                    {
+                        bEmptyPage = true;
+                    }
 
                     pOut->Pop();
 
@@ -253,7 +258,12 @@ bool PDFExport::ExportSelection( vcl::PDFWriter& rPDFWriter,
                         *pFirstPage <<= false;
 
                     ++mnProgressValue;
-                    ++nCurrentPage;
+                    if (!bEmptyPage)
+                    {
+                        // Calculate the page number in the PDF output, which may be smaller than the page number in
+                        // case of hidden slides or a partial export.
+                        ++nCurrentPage;
+                    }
                 }
             }
             else

sd/source/ui/unoidl/unomodel.cxx
@@ -1893,14 +1893,6 @@ void SAL_CALL SdXImpressDocument::render( sal_Int32 nRenderer, const uno::Any& r
         !(pPDFExtOutDevData && pPDFExtOutDevData->GetIsExportHiddenSlides()) )
         return;
 
-    if (pPDFExtOutDevData)
-    {
-        // Calculate the page number in the PDF output, which may be smaller than the page number in
-        // case of hidden slides.
-        sal_Int32 nOutputPageNum = CalcOutputPageNum(pPDFExtOutDevData, mpDoc, nPageNumber);
-        pPDFExtOutDevData->SetCurrentPageNumber(nOutputPageNum);
-    }
-
     ::sd::ClientView aView( mpDocShell, pOut );
     ::tools::Rectangle aVisArea( Point(), mpDoc->GetSdPage( static_cast<sal_uInt16>(nPageNumber) - 1, ePageKind )->GetSize() );
     vcl::Region                       aRegion( aVisArea );

vcl/qa/cppunit/pdfexport/pdfexport.cxx
@@ -43,6 +43,7 @@
 #include <unotools/streamwrap.hxx>
 
 #include <vcl/filter/PDFiumLibrary.hxx>
+#include <comphelper/propertyvalue.hxx>
 
 using namespace ::com::sun::star;
 
@@ -1975,6 +1976,31 @@ CPPUNIT_TEST_FIXTURE(PdfExportTest, testLinkWrongPage)
     CPPUNIT_ASSERT(pPdfPage->hasLinks());
 
     // Second page should have no links (3rd slide).
+    std::unique_ptr<vcl::pdf::PDFiumPage> pPdfPage2 = pPdfDocument->openPage(/*nIndex=*/1);
+    CPPUNIT_ASSERT(pPdfPage2);
+    CPPUNIT_ASSERT(!pPdfPage2->hasLinks());
+}
+
+CPPUNIT_TEST_FIXTURE(PdfExportTest, testLinkWrongPagePartial)
+{
+    // Given a Draw document with 3 pages, a link on the 2nd page:
+    // When exporting that the 2nd and 3rd page to pdf:
+    uno::Sequence<beans::PropertyValue> aFilterData = {
+        comphelper::makePropertyValue("PageRange", OUString("2-3")),
+    };
+    aMediaDescriptor["FilterName"] <<= OUString("draw_pdf_Export");
+    aMediaDescriptor["FilterData"] <<= aFilterData;
+    saveAsPDF(u"link-wrong-page-partial.odg");
+
+    // Then make sure the we have a link on the 1st page, but not on the 2nd one:
+    std::unique_ptr<vcl::pdf::PDFiumDocument> pPdfDocument = parseExport();
+    CPPUNIT_ASSERT(pPdfDocument);
+    CPPUNIT_ASSERT_EQUAL(2, pPdfDocument->getPageCount());
+    std::unique_ptr<vcl::pdf::PDFiumPage> pPdfPage = pPdfDocument->openPage(/*nIndex=*/0);
+    CPPUNIT_ASSERT(pPdfPage);
+    // Without the accompanying fix in place, this test would have failed, as the link was on the
+    // 2nd page instead.
+    CPPUNIT_ASSERT(pPdfPage->hasLinks());
     std::unique_ptr<vcl::pdf::PDFiumPage> pPdfPage2 = pPdfDocument->openPage(/*nIndex=*/1);
     CPPUNIT_ASSERT(pPdfPage2);
     CPPUNIT_ASSERT(!pPdfPage2->hasLinks());