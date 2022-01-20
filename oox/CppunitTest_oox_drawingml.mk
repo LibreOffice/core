0fd55759d713a5327a781c9a2385c7bcd40c54f7
Stephan Bergmann <sbergman@redhat.com>
2022-01-20 17:25:55 +0100

    Add missing test dependencies
    Change-Id: Icd74795dfe912cb180ff2e0d86996915d6d5b013

--- oox/CppunitTest_oox_drawingml.mk
+++ oox/CppunitTest_oox_drawingml.mk
@@ -42,6 +42,12 @@ $(eval $(call gb_CppunitTest_use_custom_headers,oox_drawingml,\
 
 $(eval $(call gb_CppunitTest_use_configuration,oox_drawingml))
 
+$(eval $(call gb_CppunitTest_use_uiconfigs,oox_drawingml, \
+    modules/simpress \
+    sfx \
+    svt \
+))
+
 $(eval $(call gb_CppunitTest_add_arguments,oox_drawingml, \
     -env:arg-env=$(gb_Helper_LIBRARY_PATH_VAR)"$$$${$(gb_Helper_LIBRARY_PATH_VAR)+=$$$$$(gb_Helper_LIBRARY_PATH_VAR)}" \
 ))
