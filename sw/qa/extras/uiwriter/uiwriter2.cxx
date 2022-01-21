LibreOffice/core

commit dbc82c02eb24ec1c97c6ee32069771d8deb394f9

tdf#143358 sw: track insertion of empty table rows
Insertion of empty table rows wasn't tracked, and
inserting also text in the new rows and rejecting
everything didn't reject the row insertion.

Follow-up to commit 99059a1ececa3621c2fe46fabdd79eed9d626c42
"tdf#143359 sw: track deletion of empty table rows".

Note: as a workaround for tracking of the empty rows,
i.e. rows without text content, add a redline with
invisible text CH_TXT_TRACKED_DUMMY_CHAR in the first
cell of the empty row.

See also commit a483a44ca00f43a64ae51d62b8fbb4129a413f6d
"tdf#143215 DOCX import: fix tracked empty row insertion/deletion",
commit b50d386dfa70f7c1d4eb1a49091ec9dd782b767b
"tdf#142701 track changes: fix layout regression of image deletion"
and commit 05366b8e6683363688de8708a3d88cf144c7a2bf
"tdf#60382 sw offapi: add change tracking of table/row deletion".

Change-Id: I237d566a468f61f31fa19dd1d6d9bb559be99158
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/122460
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/qa/extras/uiwriter/uiwriter2.cxx
@@ -4611,6 +4611,51 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest2, testRedlineTableRowDeletionWithReject)
     assertXPath(pXmlDoc, "//page[1]//body/tab", 0);
 }
 
+CPPUNIT_TEST_FIXTURE(SwUiWriterTest2, testRedlineTableRowInsertionWithReject)
+{
+    // load a 1-row table, and insert a row with enabled change tracking
+    SwDoc* pDoc = createSwDoc(DATA_DIRECTORY, "tdf118311.fodt");
+
+    // turn on red-lining and show changes
+    pDoc->getIDocumentRedlineAccess().SetRedlineFlags(RedlineFlags::On | RedlineFlags::ShowDelete
+                                                      | RedlineFlags::ShowInsert);
+    CPPUNIT_ASSERT_MESSAGE("redlining should be on",
+                           pDoc->getIDocumentRedlineAccess().IsRedlineOn());
+    CPPUNIT_ASSERT_MESSAGE(
+        "redlines should be visible",
+        IDocumentRedlineAccess::IsShowChanges(pDoc->getIDocumentRedlineAccess().GetRedlineFlags()));
+
+    // check table and its single row
+    xmlDocUniquePtr pXmlDoc = parseLayoutDump();
+    assertXPath(pXmlDoc, "//page[1]//body/tab");
+    assertXPath(pXmlDoc, "//page[1]//body/tab/row", 1);
+
+    // insert rows before and after with enabled change tracking
+    // (HasTextChangesOnly property of the row will be false, and
+    // add dummy characters CH_TXT_TRACKED_DUMMY_CHAR)
+    dispatchCommand(mxComponent, ".uno:InsertRowsBefore", {});
+    dispatchCommand(mxComponent, ".uno:InsertRowsAfter", {});
+
+    SwEditShell* const pEditShell(pDoc->GetEditShell());
+    // This was 0 (not tracked row insertion)
+    CPPUNIT_ASSERT_EQUAL(static_cast<SwRedlineTable::size_type>(2), pEditShell->GetRedlineCount());
+
+    discardDumpedLayout();
+    pXmlDoc = parseLayoutDump();
+    assertXPath(pXmlDoc, "//page[1]//body/tab");
+    assertXPath(pXmlDoc, "//page[1]//body/tab/row", 3);
+
+    // reject redlines
+    pEditShell->RejectRedline(0);
+    pEditShell->RejectRedline(0);
+
+    discardDumpedLayout();
+    pXmlDoc = parseLayoutDump();
+    assertXPath(pXmlDoc, "//page[1]//body/tab");
+    // This was 3 (not rejected row insertion)
+    assertXPath(pXmlDoc, "//page[1]//body/tab/row", 1);
+}
+
 CPPUNIT_TEST_FIXTURE(SwUiWriterTest2, testTdf128603)
 {
     // Load the bugdoc, which has 3 textboxes.

sw/source/core/doc/tblrwcl.cxx
@@ -244,7 +244,7 @@ struct CpyPara
 
 }
 
-static void lcl_CopyRow(FndLine_ & rFndLine, CpyPara *const pCpyPara);
+static SwTableLine* lcl_CopyRow(FndLine_ & rFndLine, CpyPara *const pCpyPara);
 
 static void lcl_CopyCol( FndBox_ & rFndBox, CpyPara *const pCpyPara)
 {
@@ -379,7 +379,7 @@ static void lcl_CopyCol( FndBox_ & rFndBox, CpyPara *const pCpyPara)
     }
 }
 
-static void lcl_CopyRow(FndLine_& rFndLine, CpyPara *const pCpyPara)
+static SwTableLine* lcl_CopyRow(FndLine_& rFndLine, CpyPara *const pCpyPara)
 {
     SwTableLine* pNewLine = new SwTableLine(
                             static_cast<SwTableLineFormat*>(rFndLine.GetLine()->GetFrameFormat()),
@@ -402,6 +402,8 @@ static void lcl_CopyRow(FndLine_& rFndLine, CpyPara *const pCpyPara)
     }
 
     pCpyPara->nDelBorderFlag &= 0xf8;
+
+    return pNewLine;
 }
 
 static void lcl_InsCol( FndLine_* pFndLn, CpyPara& rCpyPara, sal_uInt16 nCpyCnt,
@@ -584,7 +586,22 @@ bool SwTable::InsertRow_( SwDoc* pDoc, const SwSelBoxes& rBoxes,
         if( bBehind )
             aCpyPara.nDelBorderFlag = 1;
         for (auto & rpFndLine : pFndBox->GetLines())
-            lcl_CopyRow( *rpFndLine, &aCpyPara );
+        {
+            SwTableLine* pNewTableLine = lcl_CopyRow( *rpFndLine, &aCpyPara );
+
+            // tracked insertion of empty table line
+            if ( pDoc->getIDocumentRedlineAccess().IsRedlineOn() )
+            {
+                SvxPrintItem aSetTracking(RES_PRINT, false);
+                SwPosition aPos(*pNewTableLine->GetTabBoxes()[0]->GetSttNd());
+                SwCursor aCursor( aPos, nullptr );
+                SwNodeIndex aInsPos(*pNewTableLine->GetTabBoxes()[0]->GetSttNd(), 1 );
+                SwPaM aPaM(aInsPos);
+                pDoc->getIDocumentContentOperations().InsertString( aPaM,
+                        OUStringChar(CH_TXT_TRACKED_DUMMY_CHAR) );
+                pDoc->SetRowNotTracked( aCursor, aSetTracking );
+            }
+        }
     }
 
     // clean up this Line's structure once again, generally all of them

commit c6b79a3caa1611c84a0dc966d241599b01471e9a

sw track changes: fix tracked table row moving and copying
During track changes, drag & drop table rows or moving
and copying by Paste Special->Rows Above put a nested table
in the first cell of the newly inserted empty table row before.

Regression from commit dbc82c02eb24ec1c97c6ee32069771d8deb394f9
"tdf#143358 sw: track insertion of empty table rows".

This fixes crashing at Redo of tracked table row insertion, too.

Change-Id: I9a31cae2c0e6e5e05450336a1e5b8d792035df35

sw/source/uibase/dochdl/swdtflvr.cxx
@@ -109,6 +109,7 @@
 #include <svx/svditer.hxx>
 #include <editeng/eeitem.hxx>
 #include <editeng/fhgtitem.hxx>
+#include <editeng/prntitem.hxx>
 #include <svx/svdpage.hxx>
 #include <avmedia/mediawindow.hxx>
 #include <swcrsr.hxx>
@@ -1591,6 +1592,12 @@ bool SwTransferable::Paste(SwWrtShell& rSh, TransferableDataHelper& rData, RndSt
                                     IDocumentMarkAccess::MarkType::UNO_BOOKMARK );
 
             // add a new empty row/column before the actual table row/column and go there
+            // (without setting the rows to tracked table row insertion here, do that at the end
+            // to avoid layout problems and unnecessary insertion of dummy characters for empty rows)
+            RedlineFlags eOld = rSh.GetDoc()->getIDocumentRedlineAccess().GetRedlineFlags();
+            if ( eOld & RedlineFlags::On )
+                rSh.GetDoc()->getIDocumentRedlineAccess().SetRedlineFlags( eOld | RedlineFlags::Ignore );
+
             const sal_uInt16 nDispatchSlot = bRowMode ? FN_TABLE_INSERT_ROW_BEFORE : FN_TABLE_INSERT_COL_BEFORE;
             pDispatch->Execute(nDispatchSlot, SfxCallMode::SYNCHRON);
             pDispatch->Execute(bRowMode ? FN_LINE_UP : FN_CHAR_LEFT, SfxCallMode::SYNCHRON);
@@ -1605,9 +1612,19 @@ bool SwTransferable::Paste(SwWrtShell& rSh, TransferableDataHelper& rData, RndSt
                     { &aCountItem, &aAfter });
             }
 
+            if ( eOld & RedlineFlags::On )
+                rSh.GetDoc()->getIDocumentRedlineAccess().SetRedlineFlags( eOld );
+
             // paste rows
             bool bResult = SwTransferable::PasteData( rData, rSh, nAction, nActionFlags, nFormat,
                                         nDestination, false, false, nullptr, 0, false, nAnchorType, bIgnoreComments, &aPasteContext );
+
+            // set tracked insertion
+            if ( eOld & RedlineFlags::On )
+            {
+                SvxPrintItem aTracked(RES_PRINT, false);
+                rSh.GetDoc()->SetRowNotTracked( *rSh.GetCursor(), aTracked );
+            }
 
             // restore cursor position
             if (pMark != nullptr)

sw/qa/extras/uiwriter/uiwriter2.cxx
@@ -5063,6 +5063,53 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest2, testTdf145089_RedlineTableRowInsertionDOCX
     assertXPath(pXmlDoc, "//page[1]//body/tab/row", 1);
 }
 
+CPPUNIT_TEST_FIXTURE(SwUiWriterTest2, testPasteTrackedTableRow)
+{
+    // load a 1-row table
+    SwDoc* pDoc = createSwDoc(DATA_DIRECTORY, "tdf118311.fodt");
+
+    // turn on red-lining and show changes
+    pDoc->getIDocumentRedlineAccess().SetRedlineFlags(RedlineFlags::On | RedlineFlags::ShowDelete
+                                                      | RedlineFlags::ShowInsert);
+    CPPUNIT_ASSERT_MESSAGE("redlining should be on",
+                           pDoc->getIDocumentRedlineAccess().IsRedlineOn());
+    CPPUNIT_ASSERT_MESSAGE(
+        "redlines should be visible",
+        IDocumentRedlineAccess::IsShowChanges(pDoc->getIDocumentRedlineAccess().GetRedlineFlags()));
+
+    // check table count
+    uno::Reference<text::XTextTablesSupplier> xTextTablesSupplier(mxComponent, uno::UNO_QUERY);
+    uno::Reference<container::XIndexAccess> xTables(xTextTablesSupplier->getTextTables(),
+                                                    uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(1), xTables->getCount());
+
+    // check table row count
+    uno::Reference<text::XTextTable> xTable(xTables->getByIndex(0), uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(1), xTable->getRows()->getCount());
+
+    // copy table row and paste it by Paste Special->Rows Above
+    dispatchCommand(mxComponent, ".uno:SelectTable", {});
+    dispatchCommand(mxComponent, ".uno:Copy", {});
+    dispatchCommand(mxComponent, ".uno:Escape", {});
+    dispatchCommand(mxComponent, ".uno:PasteRowsBefore", {});
+
+    // 2-row table
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(2), xTable->getRows()->getCount());
+
+    // This was 2 (inserted as a nested table in the first cell of the new row)
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(1), xTables->getCount());
+
+    // Is it a tracked row insertion? Its rejection results the original 1-row table
+    dispatchCommand(mxComponent, ".uno:RejectAllTrackedChanges", {});
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(1), xTable->getRows()->getCount());
+
+    dispatchCommand(mxComponent, ".uno:Undo", {});
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(2), xTable->getRows()->getCount());
+
+    dispatchCommand(mxComponent, ".uno:Redo", {});
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(1), xTable->getRows()->getCount());
+}
+
 CPPUNIT_TEST_FIXTURE(SwUiWriterTest2, testTdf145091)
 {
     // load a deleted table, reject them, and delete only its text and export to DOCX