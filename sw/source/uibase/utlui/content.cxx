e7eb5f7e10899cf106b922439454b5d2f65d8f5e
Andrea Gelmini <andrea.gelmini@gelma.net>
2022-01-22 22:18:07 +0100

    Fix typos
    Change-Id: Ia34d2b2f704da96ff3182d25a1cfa05a98fbb60b
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/128795
    Tested-by: Jenkins
    Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

--- sw/source/uibase/utlui/content.cxx
+++ sw/source/uibase/utlui/content.cxx
@@ -2912,7 +2912,7 @@ bool SwContentTree::HasContentChanged()
             if (bContentChanged)
                 continue;
 
-            // does entry have childern?
+            // does entry have children?
             if (m_xTreeView->get_row_expanded(*xEntry))
             {
                 const size_t nChildCount = GetChildCount(*xEntry);
