ce78340ec64629b091ac4327c39a3bd87df0a957
Noel Grandin <noel.grandin@collabora.co.uk>
2022-01-23 13:46:32 +0200

    use more concrete types in chart2, ChartModel
    Change-Id: Icee63a8a9d2e63f05064506df6c2d36314fe05c0

--- chart2/source/inc/chartview/ExplicitValueProvider.hxx
+++ chart2/source/inc/chartview/ExplicitValueProvider.hxx
@@ -20,6 +20,7 @@
 
 #include <com/sun/star/awt/Rectangle.hpp>
 #include <chartview/chartviewdllapi.hxx>
+#include <rtl/ref.hxx>
 
 #include <memory>
 
@@ -79,6 +80,10 @@ public:
               const css::uno::Reference< css::chart2::XAxis >& xAxis
             , const css::uno::Reference< css::chart2::XCoordinateSystem > & xCorrespondingCoordinateSystem
             , const css::uno::Reference< css::chart2::XChartDocument>& xChartDoc);
+    static sal_Int32 getExplicitNumberFormatKeyForAxis(
+              const css::uno::Reference< css::chart2::XAxis >& xAxis
+            , const css::uno::Reference< css::chart2::XCoordinateSystem > & xCorrespondingCoordinateSystem
+            , const rtl::Reference< ::chart::ChartModel>& xChartDoc);
 
     static sal_Int32 getExplicitNumberFormatKeyForDataLabel(
             const css::uno::Reference< css::beans::XPropertySet >& xSeriesOrPointProp );
