a6747a7b27b0a3049c533e68dafa33187ab3dc83
Szymon Kłos <szymon.klos@collabora.com>
2022-01-24 14:16:31 +0100

    lok: use focused tab page help
    When we open dialog with tab pages, help should open
    website for corresponding tab.
    
    Use focused window - but not global which can be modified
    by other view in lok case.
    
    followup for:
    commit 70f2fc7a10f41204e353bd3487377983a1018b53
    lok: don't use focus window to request help
    
    Change-Id: Ib7544a91696ce2f2c8ab01e62a47d279d2c5c655
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/128863
    Reviewed-by: Mert Tumer <mert.tumer@collabora.com>
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

--- vcl/source/app/salvtables.cxx
+++ vcl/source/app/salvtables.cxx
@@ -7181,8 +7181,10 @@ void SalInstanceWindow::help()
 {
     //show help for widget with keyboard focus
     vcl::Window* pWidget = ImplGetSVData()->mpWinData->mpFocusWin;
-    if (!pWidget || comphelper::LibreOfficeKit::isActive())
+    if (!pWidget)
         pWidget = m_xWindow;
+    if (comphelper::LibreOfficeKit::isActive() && m_xWindow->GetFocusedWindow())
+        pWidget = m_xWindow->GetFocusedWindow();
     OString sHelpId = pWidget->GetHelpId();
     while (sHelpId.isEmpty())
     {
