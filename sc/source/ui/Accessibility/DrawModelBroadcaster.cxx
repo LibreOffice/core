LibreOffice/core

commit a2eaf99e46f370ffb3b73828c2bdc53dc193b9a4

make comphelper::OInterfaceContainerHelper4 more threadsafe
(*) make all the methods that require an external mutex take a
std::unique_lock as a parameter, so that call sites cannot forget

(*) make the forEach method drop the lock when firing listener methods,
to reduce the odds of deadlock

Change-Id: I0a80e3b3d1c1c03b7de4a658d31fcc2847690903
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/128415
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sc/source/ui/Accessibility/DrawModelBroadcaster.cxx
@@ -40,14 +40,14 @@ ScDrawModelBroadcaster::~ScDrawModelBroadcaster()
 
 void SAL_CALL ScDrawModelBroadcaster::addEventListener( const uno::Reference< document::XEventListener >& xListener )
 {
-    std::scoped_lock aGuard(maListenerMutex);
-    maEventListeners.addInterface( xListener );
+    std::unique_lock aGuard(maListenerMutex);
+    maEventListeners.addInterface( aGuard, xListener );
 }
 
 void SAL_CALL ScDrawModelBroadcaster::removeEventListener( const uno::Reference< document::XEventListener >& xListener )
 {
-    std::scoped_lock aGuard(maListenerMutex);
-    maEventListeners.removeInterface( xListener );
+    std::unique_lock aGuard(maListenerMutex);
+    maEventListeners.removeInterface( aGuard, xListener );
 }
 
 void SAL_CALL ScDrawModelBroadcaster::addShapeEventListener(
@@ -87,20 +87,12 @@ void ScDrawModelBroadcaster::Notify( SfxBroadcaster&,
         return;
 
     std::unique_lock aGuard(maListenerMutex);
-    ::comphelper::OInterfaceIteratorHelper4 aIter( maEventListeners );
-    aGuard.unlock();
-    while( aIter.hasMoreElements() )
-    {
-        const uno::Reference < document::XEventListener >& xListener = aIter.next();
-        try
+    maEventListeners.forEach(aGuard,
+        [&aEvent](const css::uno::Reference<document::XEventListener>& xListener)
         {
-            xListener->notifyEvent( aEvent );
+            xListener->notifyEvent(aEvent);
         }
-        catch( const uno::RuntimeException& )
-        {
-            TOOLS_WARN_EXCEPTION("sc.ui", "Runtime exception caught while notifying shape");
-        }
-    }
+    );
 
     // right now, we're only handling the specific event necessary to fix this performance problem
     if (pSdrHint->GetKind() == SdrHintKind::ObjectChange)

basic/source/classes/sbunoobj.cxx
@@ -4364,13 +4364,13 @@ void SAL_CALL ModuleInvocationProxy::dispose()
 void SAL_CALL ModuleInvocationProxy::addEventListener( const Reference< XEventListener >& xListener )
 {
     std::unique_lock aGuard( m_aMutex );
-    m_aListeners.addInterface( xListener );
+    m_aListeners.addInterface( aGuard, xListener );
 }
 
 void SAL_CALL ModuleInvocationProxy::removeEventListener( const Reference< XEventListener >& xListener )
 {
     std::unique_lock aGuard( m_aMutex );
-    m_aListeners.removeInterface( xListener );
+    m_aListeners.removeInterface( aGuard, xListener );
 }
 
 

chart2/source/tools/ModifyListenerHelper.cxx
@@ -38,23 +38,25 @@ void SAL_CALL ModifyEventForwarder::addModifyListener( const Reference< util::XM
 {
     std::unique_lock aGuard(m_aMutex);
 
-    m_aModifyListeners.addInterface( aListener );
+    m_aModifyListeners.addInterface( aGuard, aListener );
 }
 
 void SAL_CALL ModifyEventForwarder::removeModifyListener( const Reference< util::XModifyListener >& aListener )
 {
     std::unique_lock aGuard(m_aMutex);
 
-    m_aModifyListeners.removeInterface( aListener );
+    m_aModifyListeners.removeInterface( aGuard, aListener );
 }
 
 // ____ XModifyListener ____
 void SAL_CALL ModifyEventForwarder::modified( const lang::EventObject& aEvent )
 {
-    if( m_aModifyListeners.getLength() == 0 )
+    std::unique_lock aGuard(m_aMutex);
+
+    if( m_aModifyListeners.getLength(aGuard) == 0 )
         return;
 
-    m_aModifyListeners.notifyEach( &util::XModifyListener::modified, aEvent );
+    m_aModifyListeners.notifyEach( aGuard, &util::XModifyListener::modified, aEvent );
 }
 
 // ____ XEventListener (base of XModifyListener) ____

chart2/source/tools/RangeHighlighter.cxx
@@ -306,7 +306,8 @@ void SAL_CALL RangeHighlighter::addSelectionChangeListener( const Reference< vie
 
     if( m_nAddedListenerCount == 0 )
         startListening();
-    maSelectionChangeListeners.addInterface( xListener);
+    std::unique_lock g(m_aMutex);
+    maSelectionChangeListeners.addInterface( g, xListener);
     ++m_nAddedListenerCount;
 
     //bring the new listener up to the current state
@@ -316,7 +317,8 @@ void SAL_CALL RangeHighlighter::addSelectionChangeListener( const Reference< vie
 
 void SAL_CALL RangeHighlighter::removeSelectionChangeListener( const Reference< view::XSelectionChangeListener >& xListener )
 {
-    maSelectionChangeListeners.removeInterface( xListener );
+    std::unique_lock g(m_aMutex);
+    maSelectionChangeListeners.removeInterface( g, xListener );
     --m_nAddedListenerCount;
     if( m_nAddedListenerCount == 0 )
         stopListening();
@@ -334,14 +336,16 @@ void SAL_CALL RangeHighlighter::selectionChanged( const lang::EventObject& /*aEv
 
 void RangeHighlighter::fireSelectionEvent()
 {
-    if( maSelectionChangeListeners.getLength() )
+    std::unique_lock g(m_aMutex);
+    if( maSelectionChangeListeners.getLength(g) )
     {
         lang::EventObject aEvent( static_cast< lang::XComponent* >( this ) );
-        comphelper::OInterfaceIteratorHelper4 aIt( maSelectionChangeListeners );
-        while( aIt.hasMoreElements() )
-        {
-            aIt.next()->selectionChanged( aEvent );
-        }
+        maSelectionChangeListeners.forEach(g,
+            [&aEvent](const css::uno::Reference<view::XSelectionChangeListener>& xListener)
+            {
+                xListener->selectionChanged(aEvent);
+            }
+        );
     }
 }
 

comphelper/source/misc/accessibleeventnotifier.cxx
@@ -209,7 +209,7 @@ void AccessibleEventNotifier::revokeClientNotifyDisposing(
 sal_Int32 AccessibleEventNotifier::addEventListener(
     const TClientId _nClient, const Reference< XAccessibleEventListener >& _rxListener )
 {
-    std::scoped_lock aGuard( GetLocalMutex() );
+    std::unique_lock aGuard( GetLocalMutex() );
 
     ClientMap::iterator aClientPos;
     if ( !implLookupClient( _nClient, aClientPos ) )
@@ -217,15 +217,15 @@ sal_Int32 AccessibleEventNotifier::addEventListener(
         return 0;
 
     if ( _rxListener.is() )
-        aClientPos->second->addInterface( _rxListener );
+        aClientPos->second->addInterface( aGuard, _rxListener );
 
-    return aClientPos->second->getLength();
+    return aClientPos->second->getLength(aGuard);
 }
 
 sal_Int32 AccessibleEventNotifier::removeEventListener(
     const TClientId _nClient, const Reference< XAccessibleEventListener >& _rxListener )
 {
-    std::scoped_lock aGuard( GetLocalMutex() );
+    std::unique_lock aGuard( GetLocalMutex() );
 
     ClientMap::iterator aClientPos;
     if ( !implLookupClient( _nClient, aClientPos ) )
@@ -233,9 +233,9 @@ sal_Int32 AccessibleEventNotifier::removeEventListener(
         return 0;
 
     if ( _rxListener.is() )
-        aClientPos->second->removeInterface( _rxListener );
+        aClientPos->second->removeInterface( aGuard,  _rxListener );
 
-    return aClientPos->second->getLength();
+    return aClientPos->second->getLength(aGuard);
 }
 
 void AccessibleEventNotifier::addEvent( const TClientId _nClient, const AccessibleEventObject& _rEvent )
@@ -243,7 +243,7 @@ void AccessibleEventNotifier::addEvent( const TClientId _nClient, const Accessib
     std::vector< Reference< XAccessibleEventListener > > aListeners;
 
     {
-        std::scoped_lock aGuard( GetLocalMutex() );
+        std::unique_lock aGuard( GetLocalMutex() );
 
         ClientMap::iterator aClientPos;
         if ( !implLookupClient( _nClient, aClientPos ) )
@@ -251,7 +251,7 @@ void AccessibleEventNotifier::addEvent( const TClientId _nClient, const Accessib
             return;
 
         // since we're synchronous, again, we want to notify immediately
-        aListeners = aClientPos->second->getElements();
+        aListeners = aClientPos->second->getElements(aGuard);
     }
 
     // default handling: loop through all listeners, and notify them

comphelper/source/misc/compbase.cxx
@@ -37,14 +37,14 @@ void SAL_CALL WeakComponentImplHelperBase::addEventListener(
     std::unique_lock aGuard(m_aMutex);
     if (m_bDisposed)
         return;
-    maEventListeners.addInterface(rxListener);
+    maEventListeners.addInterface(aGuard, rxListener);
 }
 
 void SAL_CALL WeakComponentImplHelperBase::removeEventListener(
     css::uno::Reference<css::lang::XEventListener> const& rxListener)
 {
     std::unique_lock aGuard(m_aMutex);
-    maEventListeners.removeInterface(rxListener);
+    maEventListeners.removeInterface(aGuard, rxListener);
 }
 
 css::uno::Any SAL_CALL WeakComponentImplHelperBase::queryInterface(css::uno::Type const& rType)

comphelper/source/misc/instancelocker.cxx
@@ -87,18 +87,18 @@ void SAL_CALL OInstanceLocker::dispose()
 
 void SAL_CALL OInstanceLocker::addEventListener( const uno::Reference< lang::XEventListener >& xListener )
 {
-    std::scoped_lock aGuard( m_aMutex );
+    std::unique_lock aGuard( m_aMutex );
     if ( m_bDisposed )
         throw lang::DisposedException(); // TODO
 
-    m_aListenersContainer.addInterface( xListener );
+    m_aListenersContainer.addInterface( aGuard, xListener );
 }
 
 
 void SAL_CALL OInstanceLocker::removeEventListener( const uno::Reference< lang::XEventListener >& xListener )
 {
-    std::scoped_lock aGuard( m_aMutex );
-    m_aListenersContainer.removeInterface( xListener );
+    std::unique_lock aGuard( m_aMutex );
+    m_aListenersContainer.removeInterface( aGuard, xListener );
 }
 
 // XInitialization

editeng/source/uno/unotext2.cxx
@@ -242,13 +242,13 @@ void SAL_CALL SvxUnoTextContent::dispose()
 void SAL_CALL SvxUnoTextContent::addEventListener( const uno::Reference< lang::XEventListener >& xListener )
 {
     std::unique_lock aGuard(maDisposeContainerMutex);
-    maDisposeListeners.addInterface(xListener);
+    maDisposeListeners.addInterface(aGuard, xListener);
 }
 
 void SAL_CALL SvxUnoTextContent::removeEventListener( const uno::Reference< lang::XEventListener >& aListener )
 {
    std::unique_lock aGuard(maDisposeContainerMutex);
-   maDisposeListeners.removeInterface(aListener);
+   maDisposeListeners.removeInterface(aGuard, aListener);
 }
 
 // XEnumerationAccess

commit 04803d37dfbe1a5a03d58f6fc521569e985c0af7

tdf#146991 Insert chart -> crash
regression from
    commit a2eaf99e46f370ffb3b73828c2bdc53dc193b9a4
    Author: Noel Grandin <noel.grandin@collabora.co.uk>
    Date:   Fri Jan 14 10:56:50 2022 +0200
    make comphelper::OInterfaceContainerHelper4 more threadsafe

Change-Id: I259d24a3f0c710222544797728c5f40ff299e50b

sc/source/ui/Accessibility/DrawModelBroadcaster.cxx
@@ -99,7 +99,6 @@ void ScDrawModelBroadcaster::Notify( SfxBroadcaster&,
     {
         auto pSdrObject = const_cast<SdrObject*>(pSdrHint->GetObject());
         uno::Reference<drawing::XShape> xShape(pSdrObject->getUnoShape(), uno::UNO_QUERY);
-        aGuard.lock();
         auto it = maShapeListeners.find(xShape);
         if (it != maShapeListeners.end())
             it->second->notifyShapeEvent(aEvent);