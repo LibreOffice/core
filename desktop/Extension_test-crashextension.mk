63f3bac396c40ba8f4b8d868733704ebe8210660
Samuel Mehrbrodt <samuel.mehrbrodt@allotropia.de>
2022-01-27 10:55:58 +0100

    Add LibreOffice Crash extension
    This extension crashes LibreOffice.
    It is to be used to test the crashreporter setup.
    
    Change-Id: I494cdcd746dd49a4f566507712c601494a0bbfac

---
+++ desktop/Extension_test-crashextension.mk
@@ -0,0 +1,25 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t; fill-column: 100 -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_Extension_Extension,test-crashextension,desktop/test/deployment/crashextension,nodeliver))
+
+$(eval $(call gb_Extension_add_file,test-crashextension,platform.components,$(call gb_Rdb_get_target,crashextension)))
+
+$(eval $(call gb_Extension_add_files,test-crashextension,, \
+    $(SRCDIR)/desktop/test/deployment/crashextension/Addons.xcu \
+    $(SRCDIR)/desktop/test/deployment/crashextension/ProtocolHandler.xcu \
+    $(SRCDIR)/desktop/test/deployment/crashextension/ToolbarMerge.xcu \
+    $(SRCDIR)/desktop/test/deployment/crashextension/crash.png \
+))
+
+$(eval $(call gb_Extension_add_libraries,test-crashextension, \
+    crashextension \
+))
+
+# vim: set noet sw=4 ts=4:
