LibreOffice/core

commit ecccc429183b999f2e488e991ef41bbee17604b6

INTEGRATION: CWS fwk36 (1.113.44); FILE MERGED 2006/03/16 15:44:13 cd 1.113.44.2: #i46895# Fixed typos 2006/03/16 14:55:24 mav 1.113.44.1: #i46895# allow to create a copy of the document if possible

sfx2/source/appl/appuno.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: appuno.cxx,v $
  *
- *  $Revision: 1.113 $
+ *  $Revision: 1.114 $
  *
- *  last change: $Author: rt $ $Date: 2006-02-07 10:28:25 $
+ *  last change: $Author: obo $ $Date: 2006-03-27 09:33:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -270,6 +270,7 @@ static const String sComponentData  = String::CreateFromAscii( "ComponentData" )
 static const String sComponentContext = String::CreateFromAscii( "ComponentContext" );
 static const String sDocumentBaseURL = String::CreateFromAscii( "DocumentBaseURL" );
 static const String sHierarchicalDocumentName = String::CreateFromAscii( "HierarchicalDocumentName" );
+static const String sCopyStreamIfPossible = String::CreateFromAscii( "CopyStreamIfPossible" );
 
 void TransformParameters( sal_uInt16 nSlotId, const ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue>& rArgs, SfxAllItemSet& rSet, const SfxSlot* pSlot )
 {
@@ -869,6 +870,14 @@ void TransformParameters( sal_uInt16 nSlotId, const ::com::sun::star::uno::Seque
                     if (bOK)
                         rSet.Put( SfxStringItem( SID_DOC_HIERARCHICALNAME, sVal ) );
                 }
+                else if ( aName == sCopyStreamIfPossible )
+                {
+                    sal_Bool bVal = sal_False;
+                    sal_Bool bOK = (rProp.Value >>= bVal);
+                    DBG_ASSERT( bOK, "invalid type for CopyStreamIfPossible" )
+                    if (bOK)
+                        rSet.Put( SfxBoolItem( SID_COPY_STREAM_IF_POSSIBLE, bVal ) );
+                }
 #ifdef DBG_UTIL
                 else
                     --nFoundArgs;
@@ -1048,6 +1057,8 @@ void TransformItems( sal_uInt16 nSlotId, const SfxItemSet& rSet, ::com::sun::sta
                 nAdditional++;
             if ( rSet.GetItemState( SID_DOC_HIERARCHICALNAME ) == SFX_ITEM_SET )
                 nAdditional++;
+            if ( rSet.GetItemState( SID_COPY_STREAM_IF_POSSIBLE ) == SFX_ITEM_SET )
+                nAdditional++;
 
             // consider additional arguments
             nProps += nAdditional;
@@ -1170,6 +1181,8 @@ void TransformItems( sal_uInt16 nSlotId, const SfxItemSet& rSet, ::com::sun::sta
                     if ( nId == SID_DOC_BASEURL )
                         continue;
                     if ( nId == SID_DOC_HIERARCHICALNAME )
+                        continue;
+                    if ( nId == SID_COPY_STREAM_IF_POSSIBLE )
                         continue;
 
                     // used only internally
@@ -1497,6 +1510,11 @@ void TransformItems( sal_uInt16 nSlotId, const SfxItemSet& rSet, ::com::sun::sta
             {
                 pValue[nActProp].Name = sHierarchicalDocumentName;
                 pValue[nActProp++].Value <<= ( ::rtl::OUString(((SfxStringItem*)pItem)->GetValue()) );
+            }
+            if ( rSet.GetItemState( SID_COPY_STREAM_IF_POSSIBLE, sal_False, &pItem ) == SFX_ITEM_SET )
+            {
+                pValue[nActProp].Name = sCopyStreamIfPossible;
+                pValue[nActProp++].Value = ( ((SfxUnoAnyItem*)pItem)->GetValue() );
             }
         }
     }

commit 83cdb2a66d6fe78f729f19b13e5e1b5d5ed40f6d

SID_COPY_STREAM_IF_POSSIBLE is a SfxBoolItem
The cast in appuno.cxx has been wrong since this constant was introduced
in
    commit ecccc429183b999f2e488e991ef41bbee17604b6
    Author: Oliver Bolte <obo@openoffice.org>
    Date:   Mon Mar 27 08:33:39 2006 +0000
    INTEGRATION: CWS fwk36 (1.113.44); FILE MERGED
    2006/03/16 15:44:13 cd 1.113.44.2: #i46895# Fixed typos
    2006/03/16 14:55:24 mav 1.113.44.1: #i46895# allow to create a copy of the document if possible

and I looked that cast when I added the TypedWhichId annotation.

Change-Id: I1d541094e08633e5aced2423c30904506ca68368

include/sfx2/sfxsids.hrc
@@ -248,7 +248,7 @@ class SvxZoomItem;
 #define SID_APPLY_STYLE                     (SID_SFX_START + 1703)
 #define SID_MACRO_SIGNATURE                 (SID_SFX_START + 1704)
 #define SID_ATTR_WARNALIENFORMAT            (SID_SFX_START + 1705)
-#define SID_COPY_STREAM_IF_POSSIBLE         TypedWhichId<SfxUnoAnyItem>(SID_SFX_START + 1706)
+#define SID_COPY_STREAM_IF_POSSIBLE         TypedWhichId<SfxBoolItem>(SID_SFX_START + 1706)
 #define SID_MAIL_SENDDOCASFORMAT            (SID_SFX_START + 1707)
 #define SID_MAIL_SENDDOCASMS                (SID_SFX_START + 1708)
 #define SID_MAIL_SENDDOCASOOO               (SID_SFX_START + 1709)

sfx2/source/appl/appuno.cxx
@@ -1621,7 +1621,7 @@ void TransformItems( sal_uInt16 nSlotId, const SfxItemSet& rSet, uno::Sequence<b
         if ( rSet.GetItemState( SID_COPY_STREAM_IF_POSSIBLE, false, &pItem ) == SfxItemState::SET )
         {
             pValue[nActProp].Name = sCopyStreamIfPossible;
-            pValue[nActProp++].Value = static_cast<const SfxUnoAnyItem*>(pItem)->GetValue();
+            pValue[nActProp++].Value <<= static_cast<const SfxBoolItem*>(pItem)->GetValue();
         }
         if ( rSet.GetItemState( SID_NOAUTOSAVE, false, &pItem ) == SfxItemState::SET )
         {

sfx2/source/doc/objstor.cxx
@@ -2712,7 +2712,7 @@ bool SfxObjectShell::CommonSaveAs_Impl(const INetURLObject& aURL, const OUString
     }
 
 
-    const SfxBoolItem* pCopyStreamItem = rItemSet.GetItem<SfxBoolItem>(SID_COPY_STREAM_IF_POSSIBLE, false);
+    const SfxBoolItem* pCopyStreamItem = rItemSet.GetItem(SID_COPY_STREAM_IF_POSSIBLE, false);
     if ( bSaveTo && pCopyStreamItem && pCopyStreamItem->GetValue() && !IsModified() )
     {
         if (pMedium->TryDirectTransfer(aURL.GetMainURL(INetURLObject::DecodeMechanism::NONE), rItemSet))