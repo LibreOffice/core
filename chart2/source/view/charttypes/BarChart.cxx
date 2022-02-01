458b4e2d7a3fd9d214bb21f57b0de99dc37333a1
Noel Grandin <noel.grandin@collabora.co.uk>
2022-02-01 14:16:33 +0200

    tdf#147027 Cannot display pattern, gradient or hatch fill in bar or area chart
    this reverts
        commit 0b0934056b47485527442b6366d7fd3274a4601f
        Author: Noel Grandin <noelgrandin@gmail.com>
        Date:   Tue Jan 4 18:50:01 2022 +0200
        bypass SvxShape when creating rectangles in chart2
    
    there is just too much logic to re-implement in bypassing SvxShape. And
    the logic is spread out over several different classes/methods and
    overrides, so getting it perfect is pretty much impossible.
    
    Change-Id: I7dbae95dda780e7fa40c4e0798e6c199e4eaf41c

--- chart2/source/view/charttypes/BarChart.cxx
+++ chart2/source/view/charttypes/BarChart.cxx
@@ -859,6 +859,7 @@ void BarChart::doXSlot(
             //create partial point
             if( !approxEqual(fLowerYValue,fUpperYValue) )
             {
+                rtl::Reference< SvxShape >  xShape;
                 if( m_nDimension==3 )
                 {
                     drawing::Position3D aLogicBottom            (fLogicX,fLogicYStart,fLogicZ);
@@ -893,7 +894,7 @@ void BarChart::doXSlot(
                     if( fTopHeight < 0 )
                         fTopHeight *= -1.0;
 
-                    rtl::Reference< SvxShape > xShape = createDataPoint3D_Bar(
+                    xShape = createDataPoint3D_Bar(
                         xSeriesGroupShape_Shapes, aTransformedBottom, aSize, fTopHeight, nRotateZAngleHundredthDegree
                         , xDataPointProperties, nGeometry3D );
 
@@ -925,20 +926,9 @@ void BarChart::doXSlot(
                     AddPointToPoly( aPoly, aLeftUpperPoint );
                     AddPointToPoly( aPoly, drawing::Position3D( fLogicX-fLogicBarWidth/2.0,fLowerYValue,fLogicZ) );
                     pPosHelper->transformScaledLogicToScene( aPoly );
-                    std::optional<sal_Int32> xFillColor;
-                    if(bHasFillColorMapping)
-                    {
-                        double nPropVal = pSeries->getValueByProperty(nPointIndex, "FillColor");
-                        if(!std::isnan(nPropVal))
-                            xFillColor = static_cast<sal_Int32>(nPropVal);
-                    }
-                    SdrPathObj* pShape = ShapeFactory::createArea2D( xSeriesGroupShape_Shapes, aPoly, /*bSetZOrderToZero*/false );
-                    PropertyMapper::setPropertyNameMapForFilledSeriesProperties(pShape, xDataPointProperties, xFillColor);
+                    xShape = ShapeFactory::createArea2D( xSeriesGroupShape_Shapes, aPoly );
+                    PropertyMapper::setMappedProperties( *xShape, xDataPointProperties, PropertyMapper::getPropertyNameMapForFilledSeriesProperties() );
 
-                    //set name/classified ObjectID (CID)
-                    ShapeFactory::setShapeName(pShape
-                        , ObjectIdentifier::createPointCID(
-                            pSeries->getPointCID_Stub(),nPointIndex) );
                 }
 
             }
