0cfc87a65eb65cb2fd447b560199075c7546731a
Miklos Vajna <vmiklos@collabora.com>
2022-02-02 16:02:19 +0100

    tdf#137920 sw: avoid layout loop when inserting at-char anchored large image
    Regression from commit d250ca91c79f457102daf4da81446b7f130fb0ee (sw:
    insert image: set anchor to at-char by default, 2019-11-18), the problem
    was that the document has 2 pages, the first page would host the anchor
    of an inserted image and looped while creating/deleting a next page
    frame when updating the layout on inserting of an image.
    
    This problem was less visible before, as the at-para anchor is a
    different codepath, which was the previous default.
    
    The primary problem is that the loop in SwLayAction::Action() assumes
    that we make progress, so in case m_bAgain is set, then doing the layout
    again won't re-set m_bAgain, but it happened here.
    
    That happens because each iteration calls SwFrame::InsertPage(),
    followed by a SwPageFrame::DestroyImpl(). Examining the backtrace of the
    SwPageFrame ctor leads to SwTextFrame::FormatAdjust() which tries to
    split the text frame only in case it has either a number portion or a
    text portion, but the check there was incomplete: a "none" number format
    results in no number portion, leading to this loop.
    
    Fix the problem by checking for HasVisibleNumberingOrBullet() before
    assuming that we can split an empty text frame into two pieces where at
    least one of them is not empty.
    
    Change-Id: I454e1ec275ad6c00202e65b97adb79647d11a0b7

--- sw/qa/core/text/text.cxx
+++ sw/qa/core/text/text.cxx
@@ -241,6 +241,22 @@ CPPUNIT_TEST_FIXTURE(SwCoreTextTest, testRuby)
     }
 }
 
+CPPUNIT_TEST_FIXTURE(SwCoreTextTest, testEmptyNumberingPageSplit)
+{
+    // Given a document with 2 pages: the only para on page 1 is a numbering without a number
+    // portion:
+    createSwDoc(DATA_DIRECTORY, "empty-numbering-page-split.fodt");
+
+    // When inserting an image that doesn't fit the body frame:
+    // Then make sure that the layout update after insertion finishes:
+    uno::Sequence<beans::PropertyValue> aArgs = {
+        comphelper::makePropertyValue("FileName",
+                                      m_directories.getURLFromSrc(DATA_DIRECTORY) + "image.png"),
+    };
+    // Without the accompanying fix in place, this never finished.
+    dispatchCommand(mxComponent, ".uno:InsertGraphic", aArgs);
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
