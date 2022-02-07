3467b8b63f100e5474673727f137778fed7ccad5
Tünde Tóth <toth.tunde@nisz.hu>
2022-02-07 11:58:16 +0100

    tdf#147086 OOXML export: don't save the empty editing password
    Saving with opening password, but without editing password
    resulted read-only documents (only on their original path: saving,
    i.e. copying the file in a different path was a workaround for
    the lost permission).
    
    Regression from commit 1b53c1dfc76f08ca7df40a0673aa50eca700d072
    (tdf#144374 DOCX: export the password for editing).
    
    Change-Id: I2759dc876ee7668d00eee0aa81490c9d1e0c272b
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/129593
    Tested-by: László Németh <nemeth@numbertext.org>
    Reviewed-by: László Németh <nemeth@numbertext.org>
    Signed-off-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/130060
    Tested-by: Jenkins
    (cherry picked from commit f7e03352c18e13161effcfe08c7591d3be11b3e0)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/130088
    Reviewed-by: Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>

---
+++ sc/qa/uitest/calc_tests8/tdf147086.py
@@ -0,0 +1,55 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+from uitest.framework import UITestCase
+from libreoffice.uno.propertyvalue import mkPropertyValues
+from org.libreoffice.unotest import systemPathToFileUrl
+from uitest.uihelper.common import select_by_text
+from tempfile import TemporaryDirectory
+import os.path
+
+#Bug 147086 - Password in .xlsx creates readonly sheet
+
+class tdf147086(UITestCase):
+
+    def test_tdf147086(self):
+
+        with TemporaryDirectory() as tempdir:
+            xFilePath = os.path.join(tempdir, "tdf147086_tmp.xlsx")
+
+            with self.ui_test.create_doc_in_start_center("calc"):
+                # Save the document
+                with self.ui_test.execute_dialog_through_command(".uno:Save", close_button="") as xSaveDialog:
+                    xFileName = xSaveDialog.getChild("file_name")
+                    xFileName.executeAction("TYPE", mkPropertyValues({"KEYCODE":"CTRL+A"}))
+                    xFileName.executeAction("TYPE", mkPropertyValues({"KEYCODE":"BACKSPACE"}))
+                    xFileName.executeAction("TYPE", mkPropertyValues({"TEXT": xFilePath}))
+                    xFileTypeCombo = xSaveDialog.getChild("file_type")
+                    select_by_text(xFileTypeCombo, "Excel 2007–365 (.xlsx)")
+                    xPasswordCheckButton = xSaveDialog.getChild("password")
+                    xPasswordCheckButton.executeAction("CLICK", tuple())
+                    xOpen = xSaveDialog.getChild("open")
+
+                    with self.ui_test.execute_dialog_through_action(xOpen, "CLICK", close_button="") as xPasswordDialog:
+                        xReadonly = xPasswordDialog.getChild("readonly")
+                        xReadonly.executeAction("CLICK", tuple())
+
+                        xOk = xPasswordDialog.getChild("ok")
+                        # XLSX confirmation dialog is displayed
+                        with self.ui_test.execute_dialog_through_action(xOk, "CLICK", close_button="save"):
+                            pass
+
+            with self.ui_test.load_file(systemPathToFileUrl(xFilePath)) as document:
+
+                self.assertTrue(document.isReadonly())
+
+                self.xUITest.executeCommand(".uno:EditDoc")
+
+                self.assertFalse(document.isReadonly())
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
