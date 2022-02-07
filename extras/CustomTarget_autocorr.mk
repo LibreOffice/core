ecd9026d088c1ca224607d8ad12a20006446294e
Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>
2022-02-07 17:12:49 +0100

    simplify autocorrect makefile
    It used way too complex nested constructs for no reason, and also had
    a dependency error (creation of individual languages zip files did
    depend on the xml for all languages to be done)
    
    also remove the empty mimetype files and just touch them instead of
    copying them.
    
    The xml and mimetype files are now intermediates and are removed by make
    once the .dat is built. (if that's not desired, they can be flagged as
    .SECONDARY)
    
    Further simplification is possible when using the same language code
    for both the intermediate directory as well as the source directory
    layout and the .dat files
    
    Change-Id: I0e0c28abcad2b404a88d1f32b9b9cdb53b4125c6

--- extras/CustomTarget_autocorr.mk
+++ extras/CustomTarget_autocorr.mk
@@ -59,6 +59,8 @@ extras_AUTOCORR_LANGS := \
 	zh-CN:zh-CN \
 	zh-TW:zh-TW \
 
+extras_AUTOCORR_SHORTLANG  = $(firstword $(subst :, ,$(filter %:$(1),$(extras_AUTOCORR_LANGS))))
+
 extras_AUTOCORR_XMLFILES := \
 	af-ZA/DocumentList.xml \
 	af-ZA/META-INF/manifest.xml \
@@ -234,62 +236,42 @@ extras_AUTOCORR_XMLFILES := \
 	zh-TW/META-INF/manifest.xml \
 	zh-TW/SentenceExceptList.xml \
 
+# param: shortlang
+extras_AUTOCORR_XMLFILES_LANG = $(subst $(1)/,,$(filter $(1)/%,$(extras_AUTOCORR_XMLFILES)))
 
-extras_AUTOCORR_MIMETYPEFILES := $(foreach lang,$(extras_AUTOCORR_LANGS),$(firstword $(subst :, ,$(lang)))/mimetype)
-
-
-$(call gb_CustomTarget_get_target,extras/source/autocorr) : \
-	$(foreach lang,$(extras_AUTOCORR_LANGS),$(call gb_CustomTarget_get_workdir,extras/source/autocorr)/acor_$(lastword $(subst :, ,$(lang))).dat)
-
-$(call gb_CustomTarget_get_workdir,extras/source/autocorr)/%/mimetype : $(SRCDIR)/extras/source/autocorr/lang/%/mimetype
-	$(call gb_Output_announce,autocorr/$*/mimetype,$(true),CPY,1)
-	$(call gb_Trace_StartRange,autocorr/$*/mimetype,CPY)
-	cp $< $@
-	$(call gb_Trace_EndRange,autocorr/$*/mimetype,CPY)
+$(call gb_CustomTarget_get_workdir,extras/source/autocorr)/%/mimetype : \
+        | $(dir $(call gb_CustomTarget_get_workdir,extras/source/autocorr)/%/mimetype).dir
+	$(call gb_Output_announce,autocorr/$*/mimetype,$(true),TCH,1)
+	$(call gb_Trace_StartRange,autocorr/$*/mimetype,TCH)
+	touch $@
+	$(call gb_Trace_EndRange,autocorr/$*/mimetype,TCH)
 
 $(call gb_CustomTarget_get_workdir,extras/source/autocorr)/%.xml : $(SRCDIR)/extras/source/autocorr/lang/%.xml \
-		| $(call gb_ExternalExecutable_get_dependencies,xsltproc)
+        | $(call gb_ExternalExecutable_get_dependencies,xsltproc) \
+          $(dir $(call gb_CustomTarget_get_workdir,extras/source/autocorr)/%.xml).dir
 	$(call gb_Output_announce,autocorr/$*.xml,$(true),XSL,1)
 	$(call gb_Trace_StartRange,autocorr/$*.xml,XSL)
 	$(call gb_ExternalExecutable_get_command,xsltproc) --nonet -o $@ $(SRCDIR)/extras/util/compact.xsl $<
 	$(call gb_Trace_EndRange,autocorr/$*.xml,XSL)
 
-$(call gb_CustomTarget_get_workdir,extras/source/autocorr)/%.dat :
-	$(call gb_Output_announce,autocorr/$*.dat,$(true),ZIP,2)
-	$(call gb_Trace_StartRange,autocorr/$*.dat,ZIP)
+# secondexpansion necessry since the .dat are created with longlang form, while the other files
+# are created in the shortlang directory, and thus % cannot be used verbatim, the second expansion
+# allows the actual stem from the target to be used in the macro calls
+# there's no reason for the different directories thouch, so a janitorial cleanup would be
+# TODO: get rid of the shortform:longform language names when assembling the autocorr files
+.SECONDEXPANSION:
+$(call gb_CustomTarget_get_workdir,extras/source/autocorr)/acor_%.dat : \
+        $$(addprefix \
+            $(call gb_CustomTarget_get_workdir,extras/source/autocorr)/$$(call extras_AUTOCORR_SHORTLANG,$$*)/,\
+                mimetype \
+                $$(call extras_AUTOCORR_XMLFILES_LANG,$$(call extras_AUTOCORR_SHORTLANG,$$*)))
+	$(call gb_Output_announce,autocorr/acor_$*.dat,$(true),ZIP,2)
+	$(call gb_Trace_StartRange,autocorr/acor_$*.dat,ZIP)
 	$(call gb_Helper_abbreviate_dirs,\
-		cd $(EXTRAS_AUTOCORR_DIR) && \
-		zip -q0X --filesync --must-match $@ $(EXTRAS_AUTOCORR_MIMEFILES_FILTER) && \
-		zip -qrX --must-match $@ $(EXTRAS_AUTOCORR_XMLFILES_FILTER) \
+		cd $(dir $<) && \
+		zip -q0X --filesync --must-match $@ mimetype && \
+		zip -qrX --must-match $@ $(call extras_AUTOCORR_XMLFILES_LANG,$(call extras_AUTOCORR_SHORTLANG,$*)) \
 	)
-	$(call gb_Trace_EndRange,autocorr/$*.dat,ZIP)
-
-define extras_Autocorr_make_file_deps
-$(call gb_CustomTarget_get_workdir,$(1))/$(2) : $(SRCDIR)/$(1)/lang/$(2) \
-	| $(dir $(call gb_CustomTarget_get_workdir,$(1))/$(2)).dir
-
-endef
-
-define extras_Autocorr_make_zip_deps
-$(call gb_CustomTarget_get_workdir,$(1))/$(2) : \
-	$(addprefix $(call gb_CustomTarget_get_workdir,$(1))/,$(filter $(3)/%,$(extras_AUTOCORR_MIMETYPEFILES) $(extras_AUTOCORR_XMLFILES))) \
-	| $(dir $(call gb_CustomTarget_get_workdir,$(1))/$(2)).dir
-
-$(call gb_CustomTarget_get_workdir,$(1))/$(2) : \
-	EXTRAS_AUTOCORR_MIMEFILES_FILTER := $(foreach file,$(filter $(3)/%,$(extras_AUTOCORR_MIMETYPEFILES)),$(subst $(3)/,,$(file)))
-$(call gb_CustomTarget_get_workdir,$(1))/$(2) : \
-	EXTRAS_AUTOCORR_XMLFILES_FILTER := $(foreach file,$(filter $(3)/%,$(extras_AUTOCORR_XMLFILES)),$(subst $(3)/,,$(file)))
-$(call gb_CustomTarget_get_workdir,$(1))/$(2) : \
-	EXTRAS_AUTOCORR_DIR := $(call gb_CustomTarget_get_workdir,$(1))/$(3)
-
-endef
-
-$(eval $(foreach file,$(extras_AUTOCORR_MIMETYPEFILES) $(extras_AUTOCORR_XMLFILES),\
-	$(call extras_Autocorr_make_file_deps,extras/source/autocorr,$(file)) \
-))
-
-$(eval $(foreach lang,$(extras_AUTOCORR_LANGS),\
-	$(call extras_Autocorr_make_zip_deps,extras/source/autocorr,acor_$(lastword $(subst :, ,$(lang))).dat,$(firstword $(subst :, ,$(lang)))) \
-))
+	$(call gb_Trace_EndRange,autocorr/acor_$*.dat,ZIP)
 
 # vim: set noet sw=4 ts=4:
