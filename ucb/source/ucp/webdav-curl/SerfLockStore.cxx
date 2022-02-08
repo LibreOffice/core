LibreOffice/core

commit 03c7cd9bbe3d46bb13a5cf1ad72ba0eaf702747e

osl::Mutex->std::mutex in SerfLockStore
Change-Id: I931e20b4b604ebd6fac83006deeabaa5939e8c7e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/127587
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

ucb/source/ucp/webdav-curl/SerfLockStore.cxx
@@ -89,9 +89,9 @@ SerfLockStore::SerfLockStore()
 
 SerfLockStore::~SerfLockStore()
 {
-    osl::ResettableMutexGuard aGuard(m_aMutex);
+    std::unique_lock aGuard(m_aMutex);
     stopTicker(aGuard);
-    aGuard.reset(); // actually no threads should even try to access members now
+    aGuard.lock(); // actually no threads should even try to access members now
     m_bFinishing = true;
 
     // release active locks, if any.
@@ -106,7 +106,7 @@ SerfLockStore::~SerfLockStore()
 
 void SerfLockStore::startTicker()
 {
-    osl::MutexGuard aGuard( m_aMutex );
+    std::unique_lock aGuard( m_aMutex );
 
     if ( !m_pTickerThread.is() )
     {
@@ -116,7 +116,7 @@ void SerfLockStore::startTicker()
 }
 
 
-void SerfLockStore::stopTicker(osl::ClearableMutexGuard & rGuard)
+void SerfLockStore::stopTicker(std::unique_lock<std::mutex> & rGuard)
 {
     rtl::Reference<TickerThread> pTickerThread;
 
@@ -128,7 +128,7 @@ void SerfLockStore::stopTicker(osl::ClearableMutexGuard & rGuard)
         m_pTickerThread.clear();
     }
 
-    rGuard.clear();
+    rGuard.unlock();
 
     if (pTickerThread.is() && pTickerThread->getIdentifier() != osl::Thread::getCurrentIdentifier())
     {
@@ -141,7 +141,7 @@ SerfLockStore::getLockTokenForURI(OUString const& rURI, css::ucb::Lock const*con
 {
     assert(rURI.startsWith("http://") || rURI.startsWith("https://"));
 
-    osl::MutexGuard aGuard( m_aMutex );
+    std::unique_lock aGuard( m_aMutex );
 
     auto const it(m_aLockInfoMap.find(rURI));
 
@@ -179,7 +179,7 @@ void SerfLockStore::addLock( const OUString& rURI,
 {
     assert(rURI.startsWith("http://") || rURI.startsWith("https://"));
 
-    osl::MutexGuard aGuard( m_aMutex );
+    std::unique_lock aGuard( m_aMutex );
 
     m_aLockInfoMap[ rURI ]
         = LockInfo(sToken, rLock, xSession, nLastChanceToSendRefreshRequest);
@@ -190,9 +190,16 @@ void SerfLockStore::addLock( const OUString& rURI,
 
 void SerfLockStore::removeLock(const OUString& rURI)
 {
+    std::unique_lock aGuard( m_aMutex );
+
+    removeLockImpl(rURI);
+}
+
+void SerfLockStore::removeLockImpl(const OUString& rURI)
+{
     assert(rURI.startsWith("http://") || rURI.startsWith("https://"));
 
-    osl::ClearableMutexGuard aGuard( m_aMutex );
+    std::unique_lock aGuard( m_aMutex );
 
     m_aLockInfoMap.erase(rURI);
 
@@ -200,10 +207,9 @@ void SerfLockStore::removeLock(const OUString& rURI)
         stopTicker(aGuard);
 }
 
-
 void SerfLockStore::refreshLocks()
 {
-    osl::MutexGuard aGuard( m_aMutex );
+    std::unique_lock aGuard( m_aMutex );
 
     ::std::vector<OUString> authFailedLocks;
 
@@ -244,7 +250,7 @@ void SerfLockStore::refreshLocks()
 
     for (auto const& rLock : authFailedLocks)
     {
-        removeLock(rLock);
+        removeLockImpl(rLock);
     }
 }
 

ucb/source/ucp/webdav-curl/SerfLockStore.hxx
@@ -21,7 +21,7 @@
 #pragma once
 
 #include <map>
-#include <osl/mutex.hxx>
+#include <mutex>
 #include <rtl/ref.hxx>
 #include <rtl/ustring.hxx>
 #include <com/sun/star/ucb/Lock.hpp>
@@ -58,7 +58,7 @@ typedef std::map< OUString, LockInfo > LockInfoMap;
 
 class SerfLockStore
 {
-    osl::Mutex         m_aMutex;
+    std::mutex         m_aMutex;
     rtl::Reference< TickerThread > m_pTickerThread;
     bool               m_bFinishing;
     LockInfoMap        m_aLockInfoMap;
@@ -82,8 +82,9 @@ public:
     void refreshLocks();
 
 private:
+    void removeLockImpl(const OUString& rURI);
     void startTicker();
-    void stopTicker(osl::ClearableMutexGuard & rGuard);
+    void stopTicker(std::unique_lock<std::mutex> & rGuard);
 };
 
 } // namespace http_dav_ucp

commit 2b27816eb2d94990da700f87512da54523b69f30

ucb: webdav-curl: fix deadlock in SerfLockStore::refreshLocks()
... and removeLock()

(regression from commit 03c7cd9bbe3d46bb13a5cf1ad72ba0eaf702747e)

Change-Id: I4029a7079c7331a7a00c11e2bec5639086bcc928

ucb/source/ucp/webdav-curl/SerfLockStore.cxx
@@ -190,19 +190,19 @@ void SerfLockStore::removeLock(const OUString& rURI)
 {
     std::unique_lock aGuard( m_aMutex );
 
-    removeLockImpl(rURI);
+    removeLockImpl(aGuard, rURI);
 }
 
-void SerfLockStore::removeLockImpl(const OUString& rURI)
+void SerfLockStore::removeLockImpl(std::unique_lock<std::mutex> & rGuard, const OUString& rURI)
 {
     assert(rURI.startsWith("http://") || rURI.startsWith("https://"));
-
-    std::unique_lock aGuard( m_aMutex );
 
     m_aLockInfoMap.erase(rURI);
 
     if ( m_aLockInfoMap.empty() )
-        stopTicker(aGuard);
+    {
+        stopTicker(rGuard);
+    }
 }
 
 void SerfLockStore::refreshLocks()
@@ -248,7 +248,7 @@ void SerfLockStore::refreshLocks()
 
     for (auto const& rLock : authFailedLocks)
     {
-        removeLockImpl(rLock);
+        removeLockImpl(aGuard, rLock);
     }
 }
 

ucb/source/ucp/webdav-curl/SerfLockStore.hxx
@@ -81,7 +81,7 @@ public:
     void refreshLocks();
 
 private:
-    void removeLockImpl(const OUString& rURI);
+    void removeLockImpl(std::unique_lock<std::mutex> & rGuard, const OUString& rURI);
     void startTicker();
     void stopTicker(std::unique_lock<std::mutex> & rGuard);
 };