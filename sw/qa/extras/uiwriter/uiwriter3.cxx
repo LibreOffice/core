LibreOffice/core

commit 58a86af36295b4fc1e07c0bd38f74530a2ce0f08

writerfilter: use insert position in PushFieldContext()
Don't hardcode end of text; see mangled nested fields in e.g.
sw/qa/core/data/ooxml/pass/fdo79838.docx

Change-Id: I1b77e7a0c0d2a7d52b5facbb43a0ed0747d74cea
Reviewed-on: https://gerrit.libreoffice.org/81668
Reviewed-by: Michael Stahl <michael.stahl@cib.de>
Tested-by: Michael Stahl <michael.stahl@cib.de>

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -3349,7 +3349,10 @@ void DomainMapper_Impl::PushFieldContext()
     {
         uno::Reference<text::XTextAppend> xTextAppend = m_aTextAppendStack.top().xTextAppend;
         if (xTextAppend.is())
-            xCrsr = xTextAppend->createTextCursorByRange(xTextAppend->getEnd());
+            xCrsr = xTextAppend->createTextCursorByRange(
+                        m_aTextAppendStack.top().xInsertPosition.is()
+                            ? m_aTextAppendStack.top().xInsertPosition
+                            : xTextAppend->getEnd());
     }
 
     uno::Reference< text::XTextRange > xStart;

commit ba0cca72f59d0c312195128566cbe6770235244e

tdf#147206 writerfilter: fix end position of hyperlink in ToX
(regression from commit 58a86af36295b4fc1e07c0bd38f74530a2ce0f08)

Change-Id: Ied4c26255b60f4320f38432ef5e2e27882b7a135

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -7021,7 +7021,14 @@ void DomainMapper_Impl::PopFieldContext()
                         }
                         else if (!pContext->GetHyperlinkURL().isEmpty() && xCrsr.is())
                         {
-                            xCrsr->gotoEnd( true );
+                            if (m_aTextAppendStack.top().xInsertPosition.is())
+                            {
+                                xCrsr->gotoRange(m_aTextAppendStack.top().xInsertPosition, true);
+                            }
+                            else
+                            {
+                                xCrsr->gotoEnd(true);
+                            }
 
                             // Draw components (like comments) need hyperlinks set differently
                             SvxUnoTextRangeBase* pDrawText = dynamic_cast<SvxUnoTextRangeBase*>(xCrsr.get());

sw/qa/extras/uiwriter/uiwriter3.cxx
@@ -34,6 +34,7 @@
 #include <unotools/streamwrap.hxx>
 #include <sfx2/linkmgr.hxx>
 
+#include <toxmgr.hxx>
 #include <fmtinfmt.hxx>
 #include <view.hxx>
 #include <wrtsh.hxx>
@@ -533,6 +534,70 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest3, testTdf139737)
     // Without the fix in place, this test would have crashed here
     dispatchCommand(mxComponent, ".uno:Undo", {});
     Scheduler::ProcessEventsToIdle();
+}
+
+CPPUNIT_TEST_FIXTURE(SwUiWriterTest3, testTdf147206)
+{
+    SwDoc* pDoc = createSwDoc();
+    SwWrtShell* pWrtShell = pDoc->GetDocShell()->GetWrtShell();
+
+    // insert empty paragraph and heading text
+    pWrtShell->SplitNode();
+    pWrtShell->Insert("abc");
+    pWrtShell->SplitNode();
+
+    // set one to heading so there will be an entry in the tox
+    pWrtShell->Up(false, 1);
+    uno::Sequence<beans::PropertyValue> aPropertyValues = comphelper::InitPropertySequence({
+        { "Style", uno::makeAny(OUString("Heading 1")) },
+        { "FamilyName", uno::makeAny(OUString("ParagraphStyles")) },
+    });
+    dispatchCommand(mxComponent, ".uno:StyleApply", aPropertyValues);
+
+    pWrtShell->EndOfSection(false);
+
+    // insert table of contents
+    SwTOXMgr mgr(pWrtShell);
+    SwTOXDescription desc{ TOX_CONTENT };
+    mgr.UpdateOrInsertTOX(desc, nullptr, nullptr);
+
+    // get url of heading cross reference mark
+    IDocumentMarkAccess& rIDMA(*pDoc->getIDocumentMarkAccess());
+    auto const headingMark
+        = std::find_if(rIDMA.getAllMarksBegin(), rIDMA.getAllMarksEnd(), [](auto const* const it) {
+              return it->GetName().startsWith(
+                  IDocumentMarkAccess::GetCrossRefHeadingBookmarkNamePrefix());
+          });
+    CPPUNIT_ASSERT(headingMark != rIDMA.getAllMarksEnd());
+    OUString const headingLink("#" + (*headingMark)->GetName());
+
+    // select tox entry
+    pWrtShell->SttEndDoc(false);
+    pWrtShell->Up(false, 1);
+    pWrtShell->EndPara(true);
+
+    rtl::Reference<SwTransferable> xTransfer = new SwTransferable(*pWrtShell);
+    xTransfer->Copy();
+
+    pWrtShell->SttEndDoc(true);
+
+    // Paste special as RTF
+    TransferableDataHelper helper(xTransfer);
+    SwTransferable::PasteFormat(*pWrtShell, helper, SotClipboardFormatId::RTF);
+    Scheduler::ProcessEventsToIdle();
+
+    // check hyperlinkering
+    CPPUNIT_ASSERT_EQUAL(
+        headingLink, getProperty<OUString>(getRun(getParagraph(1), 1, "abc\t1"), "HyperLinkURL"));
+    CPPUNIT_ASSERT_EQUAL(
+        OUString(), getProperty<OUString>(getRun(getParagraph(2), 1, OUString()), "HyperLinkURL"));
+    CPPUNIT_ASSERT_EQUAL(
+        OUString(),
+        getProperty<OUString>(getRun(getParagraph(3), 1, "Table of Contents"), "HyperLinkURL"));
+    CPPUNIT_ASSERT_EQUAL(
+        headingLink, getProperty<OUString>(getRun(getParagraph(4), 1, "abc\t1"), "HyperLinkURL"));
+    CPPUNIT_ASSERT_EQUAL(
+        OUString(), getProperty<OUString>(getRun(getParagraph(5), 1, OUString()), "HyperLinkURL"));
 }
 
 CPPUNIT_TEST_FIXTURE(SwUiWriterTest3, testTdf144840)