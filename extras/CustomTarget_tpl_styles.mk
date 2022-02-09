967900354d36de50eff76e9622b0b4655d450f41
Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>
2022-02-09 14:04:06 +0100

    simplify the template makefiles
    can later be combined further, the process is the same/no real need for
    separate makefile rules
    
    Change-Id: Ic04acc4084605382147fa43a905fe559f9197d7f
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/129726
    Tested-by: Jenkins
    Reviewed-by: Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>

--- extras/CustomTarget_tpl_styles.mk
+++ extras/CustomTarget_tpl_styles.mk
@@ -9,12 +9,6 @@
 
 $(eval $(call gb_CustomTarget_CustomTarget,extras/source/templates/styles))
 
-extras_TEMPLATES_STYLES := \
-	Default \
-	Modern \
-	Simple \
-
-
 extras_STYLES_XMLFILES := \
 	Default/META-INF/manifest.xml \
 	Default/meta.xml \
@@ -29,92 +23,38 @@ extras_STYLES_XMLFILES := \
 	Simple/styles.xml \
 	Simple/Thumbnails/thumbnail.png \
 
+# param: style-base (e.g. Modern)
+extras_STYLES_XMLFILES_RELATIVE = $(subst $(1)/,,$(filter $(1)/%,$(extras_STYLES_XMLFILES)))
 
-extras_STYLES_MIMETYPEFILES := $(foreach atexts,$(extras_TEMPLATES_STYLES),$(atexts)/mimetype)
-
-
-$(call gb_CustomTarget_get_target,extras/source/templates/styles) : \
-	$(foreach atexts,$(extras_TEMPLATES_STYLES),$(call gb_CustomTarget_get_workdir,extras/source/templates/styles)/$(atexts).ott)
-
-$(call gb_CustomTarget_get_workdir,extras/source/templates/styles)/%/mimetype : $(SRCDIR)/extras/source/templates/styles/%/mimetype
-	$(call gb_Output_announce,templates/styles/$*/mimetype,$(true),CPY,1)
-	$(call gb_Trace_StartRange,templates/styles/$*/mimetype,CPY)
+.SECONDEXPANSION:
+# secondexpansion since the patterns not just cover a filename portion, but also include a
+# directory portion withdifferent number of elements
+# copy regular files (mimetype, *.jpg, *.png, *.rdf, *.svg, *.svm, …)
+$(call gb_CustomTarget_get_workdir,extras/source/templates/styles)/% : $(SRCDIR)/extras/source/templates/styles/% \
+        | $$(dir $(call gb_CustomTarget_get_workdir,extras/source/templates/styles)/$$*).dir
+	$(call gb_Output_announce,templates/styles/$*,$(true),CPY,1)
+	$(call gb_Trace_StartRange,templates/styles/$*,CPY)
 	cp $< $@
-	$(call gb_Trace_EndRange,templates/styles/$*/mimetype,CPY)
-
-$(call gb_CustomTarget_get_workdir,extras/source/templates/styles)/%.jpg : $(SRCDIR)/extras/source/templates/styles/%.jpg
-	$(call gb_Output_announce,templates/styles/$*.jpg,$(true),CPY,1)
-	$(call gb_Trace_StartRange,templates/styles/$*.jpg,CPY)
-	cp $< $@
-	$(call gb_Trace_EndRange,templates/styles/$*.jpg,CPY)
-
-$(call gb_CustomTarget_get_workdir,extras/source/templates/styles)/%.png : $(SRCDIR)/extras/source/templates/styles/%.png
-	$(call gb_Output_announce,templates/styles/$*.png,$(true),CPY,1)
-	$(call gb_Trace_StartRange,templates/styles/$*.png,CPY)
-	cp $< $@
-	$(call gb_Trace_EndRange,templates/styles/$*.png,CPY)
-
-$(call gb_CustomTarget_get_workdir,extras/source/templates/styles)/%.rdf : $(SRCDIR)/extras/source/templates/styles/%.rdf
-	$(call gb_Output_announce,templates/styles/$*.rdf,$(true),CPY,1)
-	$(call gb_Trace_StartRange,templates/styles/$*.rdf,CPY)
-	cp $< $@
-	$(call gb_Trace_EndRange,templates/styles/$*.rdf,CPY)
-
-$(call gb_CustomTarget_get_workdir,extras/source/templates/styles)/%.svg : $(SRCDIR)/extras/source/templates/styles/%.svg
-	$(call gb_Output_announce,templates/styles/$*.svg,$(true),CPY,1)
-	$(call gb_Trace_StartRange,templates/styles/$*.svg,CPY)
-	cp $< $@
-	$(call gb_Trace_EndRange,templates/styles/$*.svg,CPY)
-
-$(call gb_CustomTarget_get_workdir,extras/source/templates/styles)/%.svm : $(SRCDIR)/extras/source/templates/styles/%.svm
-	$(call gb_Output_announce,templates/styles/$*.svm,$(true),CPY,1)
-	$(call gb_Trace_StartRange,templates/styles/$*.svm,CPY)
-	cp $< $@
-	$(call gb_Trace_EndRange,templates/styles/$*.svm,CPY)
+	$(call gb_Trace_EndRange,templates/styles/$*,CPY)
 
 $(call gb_CustomTarget_get_workdir,extras/source/templates/styles)/%.xml : $(SRCDIR)/extras/source/templates/styles/%.xml \
-		| $(call gb_ExternalExecutable_get_dependencies,xsltproc)
+        | $(call gb_ExternalExecutable_get_dependencies,xsltproc) \
+          $$(dir $(call gb_CustomTarget_get_workdir,extras/source/templates/styles)/$$*.xml).dir
 	$(call gb_Output_announce,templates/styles/$*.xml,$(true),XSL,1)
 	$(call gb_Trace_StartRange,templates/styles/$*.xml,XSL)
 	$(call gb_ExternalExecutable_get_command,xsltproc) --nonet -o $@ $(SRCDIR)/extras/util/compact.xsl $<
 	$(call gb_Trace_EndRange,templates/styles/$*.xml,XSL)
 
-$(call gb_CustomTarget_get_workdir,extras/source/templates/styles)/%.ott :
+$(call gb_CustomTarget_get_workdir,extras/source/templates/styles)/%.ott : \
+        $$(addprefix $(call gb_CustomTarget_get_workdir,extras/source/templates/styles)/$$*/,\
+            mimetype $$(call extras_STYLES_XMLFILES_RELATIVE,$$*))
 	$(call gb_Output_announce,templates/styles/$*.ott,$(true),ZIP,2)
 	$(call gb_Trace_StartRange,templates/styles/$*.ott,ZIP)
 	$(call gb_Helper_abbreviate_dirs,\
-		cd $(EXTRAS_STYLES_DIR) && \
-		zip -q0X --filesync --must-match $@ $(EXTRAS_STYLES_MIMEFILES_FILTER) && \
-		zip -qrX --must-match $@ $(EXTRAS_STYLES_XMLFILES_FILTER) \
+		cd $(dir $<) && \
+		zip -q0X --filesync --must-match $@ mimetype && \
+		zip -qrX --must-match $@ $(call extras_STYLES_XMLFILES_RELATIVE,$*) \
 	)
 	$(call gb_Trace_EndRange,templates/styles/$*.ott,ZIP)
-
-define extras_Tplstyles_make_file_deps
-$(call gb_CustomTarget_get_workdir,$(1))/$(2) : $(SRCDIR)/$(1)/$(2) \
-	| $(dir $(call gb_CustomTarget_get_workdir,$(1))/$(2)).dir
-
-endef
-
-define extras_Tplstyles_make_zip_deps
-$(call gb_CustomTarget_get_workdir,$(1))/$(2) : \
-	$(addprefix $(call gb_CustomTarget_get_workdir,$(1))/,$(filter $(3)/%,$(extras_STYLES_MIMETYPEFILES) $(extras_STYLES_XMLFILES))) \
-	| $(dir $(call gb_CustomTarget_get_workdir,$(1))/$(2)).dir
-
-$(call gb_CustomTarget_get_workdir,$(1))/$(2) : \
-	EXTRAS_STYLES_MIMEFILES_FILTER := $(foreach file,$(filter $(3)/%,$(extras_STYLES_MIMETYPEFILES)),$(subst $(3)/,,$(file)))
-$(call gb_CustomTarget_get_workdir,$(1))/$(2) : \
-	EXTRAS_STYLES_XMLFILES_FILTER := $(foreach file,$(filter $(3)/%,$(extras_STYLES_XMLFILES)),$(subst $(3)/,,$(file)))
-$(call gb_CustomTarget_get_workdir,$(1))/$(2) : \
-	EXTRAS_STYLES_DIR := $(call gb_CustomTarget_get_workdir,$(1))/$(3)
-
-endef
-
-$(eval $(foreach file,$(extras_STYLES_MIMETYPEFILES) $(extras_STYLES_XMLFILES),\
-	$(call extras_Tplstyles_make_file_deps,extras/source/templates/styles,$(file)) \
-))
-
-$(eval $(foreach atexts,$(extras_TEMPLATES_STYLES),\
-	$(call extras_Tplstyles_make_zip_deps,extras/source/templates/styles,$(atexts).ott,$(atexts)) \
-))
 
 # vim: set noet sw=4 ts=4:
--- extras/CustomTarget_tplpersonal.mk
+++ extras/CustomTarget_tplpersonal.mk
@@ -9,11 +9,6 @@
 
 $(eval $(call gb_CustomTarget_CustomTarget,extras/source/templates/personal))
 
-extras_TEMPLATES_PERSONAL := \
-	CV \
-	Resume1page \
-
-
 extras_PERSONAL_XMLFILES := \
 	CV/content.xml \
 	CV/manifest.rdf \
@@ -28,92 +23,38 @@ extras_PERSONAL_XMLFILES := \
 	Resume1page/styles.xml \
 	Resume1page/Thumbnails/thumbnail.png \
 
+# param: style-base (e.g. Modern)
+extras_PERSONAL_XMLFILES_RELATIVE = $(subst $(1)/,,$(filter $(1)/%,$(extras_PERSONAL_XMLFILES)))
 
-extras_PERSONAL_MIMETYPEFILES := $(foreach atexts,$(extras_TEMPLATES_PERSONAL),$(atexts)/mimetype)
-
-
-$(call gb_CustomTarget_get_target,extras/source/templates/personal) : \
-	$(foreach atexts,$(extras_TEMPLATES_PERSONAL),$(call gb_CustomTarget_get_workdir,extras/source/templates/personal)/$(atexts).ott)
-
-$(call gb_CustomTarget_get_workdir,extras/source/templates/personal)/%/mimetype : $(SRCDIR)/extras/source/templates/personal/%/mimetype
-	$(call gb_Output_announce,templates/personal/$*/mimetype,$(true),CPY,1)
-	$(call gb_Trace_StartRange,templates/personal/$*/mimetype,CPY)
+.SECONDEXPANSION:
+# secondexpansion since the patterns not just cover a filename portion, but also include a
+# directory portion withdifferent number of elements
+# copy regular files (mimetype, *.jpg, *.png, *.rdf, *.svg, *.svm, …)
+$(call gb_CustomTarget_get_workdir,extras/source/templates/personal)/% : $(SRCDIR)/extras/source/templates/personal/% \
+        | $$(dir $(call gb_CustomTarget_get_workdir,extras/source/templates/personal)/$$*).dir
+	$(call gb_Output_announce,templates/personal/$*,$(true),CPY,1)
+	$(call gb_Trace_StartRange,templates/personal/$*,CPY)
 	cp $< $@
-	$(call gb_Trace_EndRange,templates/personal/$*/mimetype,CPY)
-
-$(call gb_CustomTarget_get_workdir,extras/source/templates/personal)/%.jpg : $(SRCDIR)/extras/source/templates/personal/%.jpg
-	$(call gb_Output_announce,templates/personal/$*.jpg,$(true),CPY,1)
-	$(call gb_Trace_StartRange,templates/personal/$*.jpg,CPY)
-	cp $< $@
-	$(call gb_Trace_EndRange,templates/personal/$*.jpg,CPY)
-
-$(call gb_CustomTarget_get_workdir,extras/source/templates/personal)/%.png : $(SRCDIR)/extras/source/templates/personal/%.png
-	$(call gb_Output_announce,templates/personal/$*.png,$(true),CPY,1)
-	$(call gb_Trace_StartRange,templates/personal/$*.png,CPY)
-	cp $< $@
-	$(call gb_Trace_EndRange,templates/personal/$*.png,CPY)
-
-$(call gb_CustomTarget_get_workdir,extras/source/templates/personal)/%.rdf : $(SRCDIR)/extras/source/templates/personal/%.rdf
-	$(call gb_Output_announce,templates/personal/$*.rdf,$(true),CPY,1)
-	$(call gb_Trace_StartRange,templates/personal/$*.rdf,CPY)
-	cp $< $@
-	$(call gb_Trace_EndRange,templates/personal/$*.rdf,CPY)
-
-$(call gb_CustomTarget_get_workdir,extras/source/templates/personal)/%.svg : $(SRCDIR)/extras/source/templates/personal/%.svg
-	$(call gb_Output_announce,templates/personal/$*.svg,$(true),CPY,1)
-	$(call gb_Trace_StartRange,templates/personal/$*.svg,CPY)
-	cp $< $@
-	$(call gb_Trace_EndRange,templates/personal/$*.svg,CPY)
-
-$(call gb_CustomTarget_get_workdir,extras/source/templates/personal)/%.svm : $(SRCDIR)/extras/source/templates/personal/%.svm
-	$(call gb_Output_announce,templates/personal/$*.svm,$(true),CPY,1)
-	$(call gb_Trace_StartRange,templates/personal/$*.svm,CPY)
-	cp $< $@
-	$(call gb_Trace_EndRange,templates/personal/$*.svm,CPY)
+	$(call gb_Trace_EndRange,templates/personal/$*,CPY)
 
 $(call gb_CustomTarget_get_workdir,extras/source/templates/personal)/%.xml : $(SRCDIR)/extras/source/templates/personal/%.xml \
-		| $(call gb_ExternalExecutable_get_dependencies,xsltproc)
+        | $(call gb_ExternalExecutable_get_dependencies,xsltproc) \
+          $$(dir $(call gb_CustomTarget_get_workdir,extras/source/templates/personal)/$$*.xml).dir
 	$(call gb_Output_announce,templates/personal/$*.xml,$(true),XSL,1)
 	$(call gb_Trace_StartRange,templates/personal/$*.xml,XSL)
 	$(call gb_ExternalExecutable_get_command,xsltproc) --nonet -o $@ $(SRCDIR)/extras/util/compact.xsl $<
 	$(call gb_Trace_EndRange,templates/personal/$*.xml,XSL)
 
-$(call gb_CustomTarget_get_workdir,extras/source/templates/personal)/%.ott :
+$(call gb_CustomTarget_get_workdir,extras/source/templates/personal)/%.ott : \
+        $$(addprefix $(call gb_CustomTarget_get_workdir,extras/source/templates/personal)/$$*/,\
+            mimetype $$(call extras_PERSONAL_XMLFILES_RELATIVE,$$*))
 	$(call gb_Output_announce,templates/personal/$*.ott,$(true),ZIP,2)
 	$(call gb_Trace_StartRange,templates/personal/$*.ott,ZIP)
 	$(call gb_Helper_abbreviate_dirs,\
-		cd $(EXTRAS_PERSONAL_DIR) && \
-		zip -q0X --filesync --must-match $@ $(EXTRAS_PERSONAL_MIMEFILES_FILTER) && \
-		zip -qrX --must-match $@ $(EXTRAS_PERSONAL_XMLFILES_FILTER) \
+		cd $(dir $<) && \
+		zip -q0X --filesync --must-match $@ mimetype && \
+		zip -qrX --must-match $@ $(call extras_PERSONAL_XMLFILES_RELATIVE,$*) \
 	)
 	$(call gb_Trace_EndRange,templates/personal/$*.ott,ZIP)
-
-define extras_Tplpersonal_make_file_deps
-$(call gb_CustomTarget_get_workdir,$(1))/$(2) : $(SRCDIR)/$(1)/$(2) \
-	| $(dir $(call gb_CustomTarget_get_workdir,$(1))/$(2)).dir
-
-endef
-
-define extras_Tplpersonal_make_zip_deps
-$(call gb_CustomTarget_get_workdir,$(1))/$(2) : \
-	$(addprefix $(call gb_CustomTarget_get_workdir,$(1))/,$(filter $(3)/%,$(extras_PERSONAL_MIMETYPEFILES) $(extras_PERSONAL_XMLFILES))) \
-	| $(dir $(call gb_CustomTarget_get_workdir,$(1))/$(2)).dir
-
-$(call gb_CustomTarget_get_workdir,$(1))/$(2) : \
-	EXTRAS_PERSONAL_MIMEFILES_FILTER := $(foreach file,$(filter $(3)/%,$(extras_PERSONAL_MIMETYPEFILES)),$(subst $(3)/,,$(file)))
-$(call gb_CustomTarget_get_workdir,$(1))/$(2) : \
-	EXTRAS_PERSONAL_XMLFILES_FILTER := $(foreach file,$(filter $(3)/%,$(extras_PERSONAL_XMLFILES)),$(subst $(3)/,,$(file)))
-$(call gb_CustomTarget_get_workdir,$(1))/$(2) : \
-	EXTRAS_PERSONAL_DIR := $(call gb_CustomTarget_get_workdir,$(1))/$(3)
-
-endef
-
-$(eval $(foreach file,$(extras_PERSONAL_MIMETYPEFILES) $(extras_PERSONAL_XMLFILES),\
-	$(call extras_Tplpersonal_make_file_deps,extras/source/templates/personal,$(file)) \
-))
-
-$(eval $(foreach atexts,$(extras_TEMPLATES_PERSONAL),\
-	$(call extras_Tplpersonal_make_zip_deps,extras/source/templates/personal,$(atexts).ott,$(atexts)) \
-))
 
 # vim: set noet sw=4 ts=4:
