LibreOffice/core

commit a995462e6855061816c6529c366f20ace2b45868

Take advantage of segment-based hidden flag storage.
To avoid querying this states for every single column and row.

Change-Id: I3e570ae5309c826e809c170ea4635dd37c9b0d43

sc/source/core/data/table4.cxx
@@ -1270,10 +1270,19 @@ void ScTable::FillAutoSimple(
     CellType eCellType = CELLTYPE_NONE;
     bool bIsOrdinalSuffix = false;
 
+    bool bColHidden = false, bRowHidden = false;
+    SCCOL nColHiddenLast = -1;
+    SCROW nRowHiddenLast = -1;
+
     rInner = nIStart;
     while (true)        // #i53728# with "for (;;)" old solaris/x86 compiler mis-optimizes
     {
-        if(!ColHidden(rCol) && !RowHidden(rRow))
+        if (rCol > nColHiddenLast)
+            bColHidden = ColHidden(rCol, NULL, &nColHiddenLast);
+        if (rRow > nRowHiddenLast)
+            bRowHidden = RowHidden(rRow, NULL, &nRowHiddenLast);
+
+        if (!bColHidden && !bRowHidden)
         {
             if ( bGetCell )
             {

commit fb83ce2611b6d2b86f40c67ea80dabab4b8bb394

tdf#113785 sc FillAutoSimple: also optimize in negative direction
By only coding the optimization for the downward autofill,
it totally broke an upward autofill.
So this patch simply fixes the LO 4.3ish regression
from commit a995462e6855061816c6529c366f20ace2b45868.

This fix means that the non-hidden cells get the auto-value
instead of being erased.

However, the hidden cells are still erased
(which is the topic of tdf#119957).

I'm not going to bother with a unit test for this
portion. If I can solve bug 119957 as well,
then the unit test for that can cover both situations.

Change-Id: If6320ccf87fa8893ca6766c265b7760cc46ed7d8

sc/source/core/data/table4.cxx
@@ -1782,16 +1782,28 @@ void ScTable::FillAutoSimple(
     bool bIsOrdinalSuffix = false;
 
     bool bColHidden = false, bRowHidden = false;
+    SCCOL nColHiddenFirst = rDocument.MaxCol();
     SCCOL nColHiddenLast = -1;
+    SCROW nRowHiddenFirst = rDocument.MaxRow();
     SCROW nRowHiddenLast = -1;
 
     rInner = nIStart;
     while (true)        // #i53728# with "for (;;)" old solaris/x86 compiler mis-optimizes
     {
-        if (rCol > nColHiddenLast)
-            bColHidden = ColHidden(rCol, nullptr, &nColHiddenLast);
-        if (rRow > nRowHiddenLast)
-            bRowHidden = RowHidden(rRow, nullptr, &nRowHiddenLast);
+        if (bPositive)
+        {
+            if (rCol > nColHiddenLast)
+                bColHidden = ColHidden(rCol, nullptr, &nColHiddenLast);
+            if (rRow > nRowHiddenLast)
+                bRowHidden = RowHidden(rRow, nullptr, &nRowHiddenLast);
+        }
+        else
+        {
+            if (rCol < nColHiddenFirst)
+                bColHidden = ColHidden(rCol, &nColHiddenFirst);
+            if (rRow < nRowHiddenFirst)
+                bRowHidden = RowHidden(rRow, &nRowHiddenFirst);
+        }
 
         if (!bColHidden && !bRowHidden)
         {