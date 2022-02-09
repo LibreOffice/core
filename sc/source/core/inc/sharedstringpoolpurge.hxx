2fa6bedd4521b2e529a520ec76a44bb81d4d7ae9
Luboš Luňák <l.lunak@collabora.com>
2022-02-09 10:39:10 +0100

    do not call purge() on string pool too often (tdf#125428)
    It turns out svl::SharedStringPool::purge() can be somewhat
    expensive with larger documents. Profiling suggests it's primarily
    the cost of the CPU trying to access the rtl_uString instances
    scattered all over the memory, so it can't be easily optimized.
    So instead delay and compress purge() calls if they come from
    temporary ScDocument instances from undo or clipboard.
    
    Change-Id: Ie26cce113025ff45ee2c473c6b06f684f453b27b

---
+++ sc/source/core/inc/sharedstringpoolpurge.hxx
@@ -0,0 +1,46 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#pragma once
+
+#include <memory>
+
+#include <vcl/timer.hxx>
+
+namespace svl
+{
+class SharedStringPool;
+}
+
+namespace sc
+{
+/*
+Calls svl::SharedStringPool::purge() after a delay when idle. Can be
+used to compress repeated calls, as purge() may be somewhat expensive
+with large documents. And since vcl links to svl, it's not possible
+to use VCL timers in svl, so a separate class is needed.
+*/
+class SharedStringPoolPurge
+{
+public:
+    SharedStringPoolPurge();
+    ~SharedStringPoolPurge();
+    void delayedPurge(const std::shared_ptr<svl::SharedStringPool>& pool);
+
+private:
+    void cleanup();
+    std::vector<std::shared_ptr<svl::SharedStringPool>> mPoolsToPurge;
+    Timer mTimer;
+    static SharedStringPoolPurge* self;
+    DECL_DLLPRIVATE_LINK(timerHandler, Timer*, void);
+};
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
