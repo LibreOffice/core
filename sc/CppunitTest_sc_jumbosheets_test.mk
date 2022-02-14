678b4e2bd247ee3f4f2a0cc44f10747781f30e21
Stephan Bergmann <sbergman@redhat.com>
2022-02-14 15:29:16 +0100

    Fix CppunitTest_sc_jumbosheets_test for ASan
    ...cf. test::BootstrapFixture::validate (test/source/bootstrapfixture.cxx),
    which it uses
    
    Change-Id: I77ebb83634ca8a4ae33a370222ac327a9b391874

--- sc/CppunitTest_sc_jumbosheets_test.mk
+++ sc/CppunitTest_sc_jumbosheets_test.mk
@@ -56,4 +56,8 @@ $(eval $(call gb_CppunitTest_use_rdb,sc_jumbosheets_test,services))
 
 $(eval $(call gb_CppunitTest_use_configuration,sc_jumbosheets_test))
 
+$(eval $(call gb_CppunitTest_add_arguments,sc_jumbosheets_test, \
+    -env:arg-env=$(gb_Helper_LIBRARY_PATH_VAR)"$$$${$(gb_Helper_LIBRARY_PATH_VAR)+=$$$$$(gb_Helper_LIBRARY_PATH_VAR)}" \
+))
+
 # vim: set noet sw=4 ts=4:
