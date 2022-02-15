LibreOffice/core

commit 99059a1ececa3621c2fe46fabdd79eed9d626c42

tdf#143359 sw: track deletion of empty table rows
Empty table rows were deleted immediately during
change tracking, or in the case of a deleted table
also with non-empty rows, accepting table deletion
kept empty rows.

Note: as a workaround for tracking of the empty rows,
i.e. rows without text content, add a redline with
invisible text ZWJ in the first cell of the empty row.

See also commit a483a44ca00f43a64ae51d62b8fbb4129a413f6d
"tdf#143215 DOCX import: fix tracked empty row insertion/deletion",
commit b50d386dfa70f7c1d4eb1a49091ec9dd782b767b
"tdf#142701 track changes: fix layout regression of image deletion"
and commit 05366b8e6683363688de8708a3d88cf144c7a2bf
"tdf#60382 sw offapi: add change tracking of table/row deletion".

Note: switch off unnecessary redlining of tdf#132744 unit test
to keep the sake of that test (i.e. cut of the selected table
with empty rows).

Change-Id: Ief59008e8714fb88afdfce867598e3dda21bfba5
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/121784
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/source/core/docnode/ndtbl1.cxx
@@ -35,6 +35,8 @@
 #include <doc.hxx>
 #include <IDocumentUndoRedo.hxx>
 #include <IDocumentState.hxx>
+#include <IDocumentContentOperations.hxx>
+#include <IDocumentRedlineAccess.hxx>
 #include <IDocumentLayoutAccess.hxx>
 #include <pam.hxx>
 #include <swcrsr.hxx>
@@ -557,7 +559,24 @@ void SwDoc::SetRowNotTracked( const SwCursor& rCursor, const SvxPrintItem &rNew 
     aFormatCmp.reserve( std::max( 255, static_cast<int>(aRowArr.size()) ) );
 
     for( auto pLn : aRowArr )
+    {
         ::lcl_ProcessRowAttr( aFormatCmp, pLn, rNew );
+        // as a workaround for the rows without text content,
+        // add a redline with invisible text CH_TXT_TRACKED_DUMMY_CHAR
+        if (pLn->IsEmpty())
+        {
+            SwNodeIndex aInsPos( *(pLn->GetTabBoxes()[0]->GetSttNd()), 1 );
+            RedlineFlags eOld = getIDocumentRedlineAccess().GetRedlineFlags();
+            getIDocumentRedlineAccess().SetRedlineFlags_intern(RedlineFlags::NONE);
+            SwPaM aPaM(aInsPos);
+            getIDocumentContentOperations().InsertString( aPaM,
+                    OUStringChar(CH_TXT_TRACKED_DUMMY_CHAR) );
+            aPaM.SetMark();
+            aPaM.GetMark()->nContent.Assign(aPaM.GetContentNode(), 0);
+            getIDocumentRedlineAccess().SetRedlineFlags_intern( eOld );
+            getIDocumentContentOperations().DeleteAndJoin( aPaM );
+        }
+    }
 
     getIDocumentState().SetModified();
 }

sw/source/core/frmedt/fetab.cxx
@@ -332,6 +332,8 @@ bool SwFEShell::DeleteRow(bool bCompleteTable)
     // and set IsNoTracked table line property to false
     if ( GetDoc()->GetDocShell()->IsChangeRecording() )
     {
+        SwEditShell* pEditShell = GetDoc()->GetEditShell();
+        SwRedlineTable::size_type nOldRedlineCount = pEditShell->GetRedlineCount();
         StartUndo(bCompleteTable ? SwUndoId::UI_TABLE_DELETE : SwUndoId::ROW_DELETE);
         StartAllAction();
 
@@ -341,8 +343,6 @@ bool SwFEShell::DeleteRow(bool bCompleteTable)
         if ( SwWrtShell* pWrtShell = dynamic_cast<SwWrtShell*>(this) )
             pWrtShell->SelectTableRow();
 
-        SwEditShell* pEditShell = GetDoc()->GetEditShell();
-        SwRedlineTable::size_type nPrev = pEditShell->GetRedlineCount();
         pEditShell->Delete();
 
         EndAllActionAndCall();
@@ -351,7 +351,7 @@ bool SwFEShell::DeleteRow(bool bCompleteTable)
         // track row deletion only if there were tracked text changes
         // FIXME redline count can be the same in special cases, e.g. adding a
         // new tracked deletion with removing an own tracked insertion...
-        if ( nPrev != pEditShell->GetRedlineCount() )
+        if ( nOldRedlineCount != pEditShell->GetRedlineCount() )
             return true;
     }
 

sw/qa/extras/uiwriter/uiwriter3.cxx
@@ -1615,6 +1615,13 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest3, testTdf132744)
     SwDoc* pDoc = createSwDoc(DATA_DIRECTORY, "tdf132744.odt");
     SwWrtShell* pWrtShell = pDoc->GetDocShell()->GetWrtShell();
 
+    // disable change tracking to cut the table
+    pDoc->getIDocumentRedlineAccess().SetRedlineFlags(RedlineFlags::ShowDelete
+                                                      | RedlineFlags::ShowInsert);
+
+    CPPUNIT_ASSERT_MESSAGE("redlining should be off",
+                           !pDoc->getIDocumentRedlineAccess().IsRedlineOn());
+
     CPPUNIT_ASSERT_EQUAL(1, getShapes());
 
     dispatchCommand(mxComponent, ".uno:SelectAll", {});

sw/inc/hintids.hxx
@@ -182,6 +182,10 @@ class SfxVoidItem;
 #define CH_TXT_ATR_SUBST_FIELDSTART ("[")
 #define CH_TXT_ATR_SUBST_FIELDEND ("]")
 
+// a non-visible dummy character to track deleted tables,
+// table rows, and images anchored to characters
+#define CH_TXT_TRACKED_DUMMY_CHAR u'\x200D'
+
 /*
  * Enums for the hints
  */

sw/source/core/unocore/unocrsrhelper.cxx
@@ -35,6 +35,7 @@
 #include <svx/unoshape.hxx>
 
 #include <cmdid.h>
+#include <hintids.hxx>
 #include <unotextrange.hxx>
 #include <unodraw.hxx>
 #include <unofootnote.hxx>
@@ -1411,11 +1412,12 @@ void makeTableRowRedline( SwTableLine& rTableLine,
         SvxPrintItem aSetTracking(RES_PRINT, false);
         SwNodeIndex aInsPos( *(rTableLine.GetTabBoxes()[0]->GetSttNd()), 1 );
         // as a workaround for the rows without text content,
-        // add a redline with invisible text ZWJ
+        // add a redline with invisible text CH_TXT_TRACKED_DUMMY_CHAR
         if ( rTableLine.IsEmpty() )
         {
             SwPaM aPaM(aInsPos);
-            pDoc->getIDocumentContentOperations().InsertString( aPaM, u"‍" );
+            pDoc->getIDocumentContentOperations().InsertString( aPaM,
+                    OUStringChar(CH_TXT_TRACKED_DUMMY_CHAR) );
             aPaM.SetMark();
             aPaM.GetMark()->nContent.Assign(aPaM.GetContentNode(), 0);
             makeRedline(aPaM, RedlineType::TableRowInsert == eType

sw/source/uibase/wrtsh/delete.cxx
@@ -18,6 +18,7 @@
  */
 
 #include <cmdid.h>
+#include <hintids.hxx>
 #include <wrtsh.hxx>
 #include <swcrsr.hxx>
 #include <editeng/lrspitem.hxx>
@@ -463,8 +464,9 @@ bool SwWrtShell::DelRight()
                     ( eAnchorId == RndStdIds::FLY_AT_CHAR || eAnchorId == RndStdIds::FLY_AS_CHAR ) )
             {
                 sal_Int32 nRedlineLength = 1;
-                // create a double ZWJ anchor point of the image to record the deletion, if needed
-                // (otherwise use the existing anchor point of the image anchored *as* character)
+                // create a double CH_TXT_TRACKED_DUMMY_CHAR anchor point of the image to record the
+                // deletion, if needed (otherwise use the existing anchor point of the image anchored
+                // *as* character)
                 if ( eAnchorId == RndStdIds::FLY_AT_CHAR )
                 {
                     nRedlineLength = 2;
@@ -472,7 +474,8 @@ bool SwWrtShell::DelRight()
                     UnSelectFrame();
                     RedlineFlags eOld = GetRedlineFlags();
                     SetRedlineFlags( eOld | RedlineFlags::Ignore );
-                    Insert( OUString( u"‍‍" ) );
+                    Insert( OUStringChar(CH_TXT_TRACKED_DUMMY_CHAR) +
+                            OUStringChar(CH_TXT_TRACKED_DUMMY_CHAR) );
                     SwFormatAnchor anchor(RndStdIds::FLY_AT_CHAR);
                     SwCursorShell::Left(1, CRSR_SKIP_CHARS);
                     anchor.SetAnchor(GetCursor()->GetPoint());

commit 31273d657526236575b759fc740eeda520aa2f30

tdf#146622 sw crash fix: don't delete already deleted rows
Delete tables and rows removed also tables and table rows
with tracked row deletion/insertion. This resulted not only
lost change tracking, but a crashing Undo.

Crash regression from commit 99059a1ececa3621c2fe46fabdd79eed9d626c42
"tdf#143359 sw: track deletion of empty table rows".

Non-tracked deletion of the tracked row changes was a
temporary solution for the missing UX support of tracked
row changes, implemented in commit 05366b8e6683363688de8708a3d88cf144c7a2bf
"tdf#60382 sw offapi: add change tracking of table/row deletion".

Note: UX support was added in commit f348440e17debacbcba9153e238e010e8c020bdc
"tdf#146120 sw: show tracked table changes with different color",
commit 95c003d75e0f8b255344715a35358072b5eba99d
"tdf#146145 sw: 1-click Accept/Reject of table row changes",
commit 84fbb3398f7486f00e7b7dea415e1ea2510a9535
"tdf#146144 sw: add tooltip to table rows with change tracking",
commit eebe4747d2d13545004937bb0267ccfc8ab9d63f
"tdf#144270 sw: manage tracked table (row) deletion/insertion" and
commit 23846867ea32667ccf328c36142394dd6aaee8ba
"tdf#147182 sw: accept/reject all changes of a table selection".

Change-Id: I384b750b0d3626fa8b3f256c7eaf5b93f382e4e5

sw/inc/doc.hxx
@@ -1491,6 +1491,8 @@ public:
     /// rNotTracked = false means that the row was deleted or inserted with its tracked cell content
     /// bAll: delete all table rows without selection
     void SetRowNotTracked( const SwCursor& rCursor, const SvxPrintItem &rNotTracked, bool bAll = false );
+    /// don't call SetRowNotTracked() for rows with tracked row change
+    bool HasRowNotTracked( const SwCursor& rCursor );
     void SetTabBorders( const SwCursor& rCursor, const SfxItemSet& rSet );
     void SetTabLineStyle( const SwCursor& rCursor,
                           const Color* pColor, bool bSetLine,

sw/source/core/docnode/ndtbl1.cxx
@@ -540,6 +540,28 @@ bool SwDoc::GetRowBackground( const SwCursor& rCursor, std::unique_ptr<SvxBrushI
     return bRet;
 }
 
+bool SwDoc::HasRowNotTracked( const SwCursor& rCursor )
+{
+    SwTableNode* pTableNd = rCursor.GetPoint()->nNode.GetNode().FindTableNode();
+    if( !pTableNd )
+        return false;
+
+    std::vector<SwTableLine*> aRowArr; // For Lines collecting
+    ::lcl_CollectLines( aRowArr, rCursor, true );
+
+    if( aRowArr.empty() )
+        return false;
+
+    for( auto pLn : aRowArr )
+    {
+        auto pHasTextChangesOnlyProp = pLn->GetFrameFormat()->GetAttrSet().GetItem<SvxPrintItem>(RES_PRINT);
+        if ( !pHasTextChangesOnlyProp || pHasTextChangesOnlyProp->GetValue() )
+            // there is a not deleted row in the table selection
+            return true;
+    }
+    return false;
+}
+
 void SwDoc::SetRowNotTracked( const SwCursor& rCursor, const SvxPrintItem &rNew, bool bAll )
 {
     SwTableNode* pTableNd = rCursor.GetPoint()->nNode.GetNode().FindTableNode();

sw/source/core/frmedt/fetab.cxx
@@ -332,8 +332,11 @@ bool SwFEShell::DeleteRow(bool bCompleteTable)
     // and set IsNoTracked table line property to false
     if ( GetDoc()->GetDocShell()->IsChangeRecording() )
     {
+        // all rows have already had tracked row change in the table selection
+        if ( !GetDoc()->HasRowNotTracked( *getShellCursor( false ) ) )
+            return false;
+
         SwEditShell* pEditShell = GetDoc()->GetEditShell();
-        SwRedlineTable::size_type nOldRedlineCount = pEditShell->GetRedlineCount();
         StartUndo(bCompleteTable ? SwUndoId::UI_TABLE_DELETE : SwUndoId::ROW_DELETE);
         StartAllAction();
 
@@ -347,12 +350,7 @@ bool SwFEShell::DeleteRow(bool bCompleteTable)
 
         EndAllActionAndCall();
         EndUndo(bCompleteTable ? SwUndoId::UI_TABLE_DELETE : SwUndoId::ROW_DELETE);
-
-        // track row deletion only if there were tracked text changes
-        // FIXME redline count can be the same in special cases, e.g. adding a
-        // new tracked deletion with removing an own tracked insertion...
-        if ( nOldRedlineCount != pEditShell->GetRedlineCount() )
-            return true;
+        return true;
     }
 
     StartAllAction();

sw/qa/extras/uiwriter/uiwriter3.cxx
@@ -2066,6 +2066,59 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest3, testTdf132744)
     CPPUNIT_ASSERT_EQUAL(1, getShapes());
 }
 
+CPPUNIT_TEST_FIXTURE(SwUiWriterTest3, testTdf146622)
+{
+    SwDoc* pDoc = createSwDoc(DATA_DIRECTORY, "TC-table-del-add.docx");
+    CPPUNIT_ASSERT(pDoc);
+    SwWrtShell* pWrtShell = pDoc->GetDocShell()->GetWrtShell();
+    CPPUNIT_ASSERT(pWrtShell);
+
+    CPPUNIT_ASSERT_MESSAGE("redlining should be on",
+                           pDoc->getIDocumentRedlineAccess().IsRedlineOn());
+
+    uno::Reference<text::XTextTablesSupplier> xTablesSupplier(mxComponent, uno::UNO_QUERY);
+    uno::Reference<container::XIndexAccess> xTables(xTablesSupplier->getTextTables(),
+                                                    uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(3), xTables->getCount());
+    uno::Reference<container::XNameAccess> xTableNames = xTablesSupplier->getTextTables();
+    CPPUNIT_ASSERT(xTableNames->hasByName("Table1"));
+    uno::Reference<text::XTextTable> xTable1(xTableNames->getByName("Table1"), uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(4), xTable1->getRows()->getCount());
+
+    dispatchCommand(mxComponent, ".uno:DeleteRows", {});
+
+    // This was 3 (deleting the already deleted row with change tracking)
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(4), xTable1->getRows()->getCount());
+
+    dispatchCommand(mxComponent, ".uno:SelectAll", {});
+    dispatchCommand(mxComponent, ".uno:SelectAll", {});
+    Scheduler::ProcessEventsToIdle();
+
+    dispatchCommand(mxComponent, ".uno:DeleteRows", {});
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(4), xTable1->getRows()->getCount());
+
+    dispatchCommand(mxComponent, ".uno:SelectAll", {});
+    dispatchCommand(mxComponent, ".uno:SelectAll", {});
+    Scheduler::ProcessEventsToIdle();
+
+    dispatchCommand(mxComponent, ".uno:DeleteRows", {});
+    // This was 2 (deleting the already deleted table with change tracking)
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(3), xTables->getCount());
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(4), xTable1->getRows()->getCount());
+
+    // check that the first table was deleted with change tracking
+    dispatchCommand(mxComponent, ".uno:AcceptAllTrackedChanges", {});
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(1), xTables->getCount());
+
+    // Undo AcceptAllTrackedChanges and DeleteRows
+    dispatchCommand(mxComponent, ".uno:Undo", {});
+    dispatchCommand(mxComponent, ".uno:Undo", {});
+
+    // now only the second table deleted by AcceptAllTrackedChanges
+    dispatchCommand(mxComponent, ".uno:AcceptAllTrackedChanges", {});
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(2), xTables->getCount());
+}
+
 CPPUNIT_TEST_FIXTURE(SwUiWriterTest3, testTdf135014)
 {
     createSwDoc();