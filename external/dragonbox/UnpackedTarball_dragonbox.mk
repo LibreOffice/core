59043423de671242bf5373c6d76b580953b23940
Mike Kaganski <mike.kaganski@collabora.com>
2022-02-15 09:20:52 +0300

    Use Dragonbox to implement doubleTo*String*
    This header-only library is accurate in decimal representation of
    doubles; provides API that allows to create custom representation
    - so it's possible to use custom decimal separators and grouping.
    
    This allows to unify all corner cases: integers, numbers close to
    DBL_MAX, up-rounding to the next decade.
    
    Note that Dragonbox creates the shortest decimal representation
    of the number, that is unambiguously convertible back to the same
    number; thus it may hide trailing digits that are unneeded for
    such conversion.
    
    The functional changes are minimal, and beneficial:
    1. Rounding numbers close to DBL_MAX now takes into account the
    bEraseTrailingDecZeros argument, as it should, allowing to have
    "1.8E+308" for rounding DBL_MAX to 2 decimals without trailing
    zeroes, instead of previous "1.80E+308".
    2. Incorrect rounding is fixed in some cases, e.g. 9.9999999999999929
    rounded to 10 previously using rtl_math_DecimalPlaces_Max.
    3. Representing the number in the shortest way may change display
    of some printed numbers. E.g., 5th greatest double is represented
    as "1.797693134862315E+308" instead of a bit longer, but giving
    the same double on roundtrip, "1.7976931348623149E+308". This would
    generally look better for some numbers similar to the famous 0.1,
    where users would likely expect more "round" representation where
    it's unambiguous (but we still truncate to 15 significant decimals
    anyway - so there's no point in pretending to provide exact digits
    for actual binary representation).
    
    These are reflected in the unit tests affected by the change.
    
    Change-Id: I05e20274a30eec499593ee3e9ec070e1269232a2

---
+++ external/dragonbox/UnpackedTarball_dragonbox.mk
@@ -0,0 +1,20 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t; fill-column: 100 -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_UnpackedTarball_UnpackedTarball,dragonbox))
+
+$(eval $(call gb_UnpackedTarball_set_tarball,dragonbox,$(DRAGONBOX_TARBALL)))
+
+$(eval $(call gb_UnpackedTarball_set_patchlevel,dragonbox,1))
+
+$(eval $(call gb_UnpackedTarball_add_patches,dragonbox, \
+    external/dragonbox/inline_static.patch \
+))
+
+# vim: set noet sw=4 ts=4:
