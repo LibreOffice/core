5b477b78cf14238b14b874cd63e0e1f436d7ea0b
Mike Kaganski <mike.kaganski@collabora.com>
2022-02-16 09:45:40 +0100

    Related: tdf#147421: optimize O(U)String's replaceAll*
    ... and underlying low-level C functions.
    
    Change-Id: I78c2dd75e272e6d3cdd2cfae007ca0a0ec1635e1

--- sal/rtl/strimp.hxx
+++ sal/rtl/strimp.hxx
@@ -25,8 +25,14 @@
 #include <sys/sdt.h>
 #endif
 
+#include <algorithm>
+#include <cassert>
+#include <cstdlib>
+
 #include <sal/types.h>
+#include <rtl/strbuf.h>
 #include <rtl/string.hxx>
+#include <rtl/ustrbuf.h>
 #include <rtl/ustring.hxx>
 
 /* ======================================================================= */
@@ -90,6 +96,91 @@ extern rtl_freeStringFn rtl_freeString;
 #  define RTL_LOG_STRING_INTERN_NEW(s,o)
 #  define RTL_LOG_STRING_INTERN_DELETE(s)
 #endif /* USE_SDT_PROBES */
+
+namespace
+{
+namespace detail
+{
+void acquire(rtl_uString* s) { return rtl_uString_acquire(s); }
+void acquire(rtl_String* s) { return rtl_string_acquire(s); }
+
+void release(rtl_uString* s) { return rtl_uString_release(s); }
+void release(rtl_String* s) { return rtl_string_release(s); }
+
+void assign(rtl_uString** s, rtl_uString* s1) { return rtl_uString_assign(s, s1); }
+void assign(rtl_String** s, rtl_String* s1) { return rtl_string_assign(s, s1); }
+
+void new_WithLength(rtl_uString** s, sal_Int32 len) { return rtl_uString_new_WithLength(s, len); }
+void new_WithLength(rtl_String** s, sal_Int32 len) { return rtl_string_new_WithLength(s, len); }
+
+template <typename CharType1, typename CharType2>
+sal_Int32 indexOf(const CharType1* s, sal_Int32 len, const CharType2* subStr, sal_Int32 subLen)
+{
+    if constexpr (std::is_same_v<CharType1, sal_Unicode> && std::is_same_v<CharType2, sal_Unicode>)
+        return rtl_ustr_indexOfStr_WithLength(s, len, subStr, subLen);
+    else if constexpr (std::is_same_v<CharType1, sal_Unicode> && std::is_same_v<CharType2, char>)
+        return rtl_ustr_indexOfAscii_WithLength(s, len, subStr, subLen);
+    else if constexpr (std::is_same_v<CharType1, char> && std::is_same_v<CharType2, char>)
+        return rtl_str_indexOfStr_WithLength(s, len, subStr, subLen);
+}
+
+template <class S, typename CharType2>
+void append(S** s, sal_Int32* capacity, const CharType2* s1, sal_Int32 len)
+{
+    if constexpr (std::is_same_v<S, rtl_uString> && std::is_same_v<CharType2, sal_Unicode>)
+        return rtl_uStringbuffer_insert(s, capacity, (*s)->length, s1, len);
+    else if constexpr (std::is_same_v<S, rtl_uString> && std::is_same_v<CharType2, char>)
+        return rtl_uStringbuffer_insert_ascii(s, capacity, (*s)->length, s1, len);
+    else if constexpr (std::is_same_v<S, rtl_String> && std::is_same_v<CharType2, char>)
+        return rtl_stringbuffer_insert(s, capacity, (*s)->length, s1, len);
+}
+}
+
+template <class S, typename CharType1, typename CharType2>
+void string_newReplaceAllFromIndex_impl(S** s, S* s1, CharType1 const* from, sal_Int32 fromLength,
+                                        CharType2 const* to, sal_Int32 toLength,
+                                        sal_Int32 fromIndex)
+{
+    assert(s1 != nullptr);
+    assert(fromLength >= 0);
+    assert(from != nullptr || fromLength == 0);
+    assert(toLength >= 0);
+    assert(to != nullptr || toLength == 0);
+    assert(fromIndex >= 0 && fromIndex <= s1->length);
+    sal_Int32 i = detail::indexOf(s1->buffer + fromIndex, s1->length - fromIndex, from, fromLength);
+    if (i >= 0)
+    {
+        if (s1->length - fromLength > SAL_MAX_INT32 - toLength)
+            std::abort();
+        detail::acquire(s1); // in case *s == s1
+        sal_Int32 nCapacity = s1->length + (toLength - fromLength);
+        if (fromLength < toLength)
+        {
+            // Pre-allocate up to 16 replacements more
+            const sal_Int32 nMaxMoreFinds = (s1->length - fromIndex - i - fromLength) / fromLength;
+            const sal_Int32 nIncrease = toLength - fromLength;
+            const sal_Int32 nMoreReplacements = std::min(
+                { nMaxMoreFinds, (SAL_MAX_INT32 - nCapacity) / nIncrease, sal_Int32(16) });
+            nCapacity += nMoreReplacements * nIncrease;
+        }
+        detail::new_WithLength(s, nCapacity);
+        i += fromIndex;
+        fromIndex = 0;
+        do
+        {
+            detail::append(s, &nCapacity, s1->buffer + fromIndex, i);
+            detail::append(s, &nCapacity, to, toLength);
+            fromIndex += i + fromLength;
+            i = detail::indexOf(s1->buffer + fromIndex, s1->length - fromIndex, from, fromLength);
+        } while (i >= 0);
+        // the rest
+        detail::append(s, &nCapacity, s1->buffer + fromIndex, s1->length - fromIndex);
+        detail::release(s1);
+    }
+    else
+        detail::assign(s, s1);
+}
+}
 
 #endif // INCLUDED_SAL_RTL_STRIMP_HXX
 
