75ea0b4ce1d2ca5008d43a68103c4319ed385ffa
Luboš Luňák <l.lunak@collabora.com>
2022-02-16 12:59:31 +0100

    clamp ColumnSpanSet::executeAction() to allocated columns
    It's already done by executeColumnAction() and currently existing
    actions do not need unallocated columns. This prevents allocating
    all columns e.g. when selecting a full row just because
    ScDocument::GetRangeScriptType() gets called.
    
    Change-Id: I517ce14a756be83a385968549b32d430274b8002

--- sc/source/core/data/columnspanset.cxx
+++ sc/source/core/data/columnspanset.cxx
@@ -142,15 +142,20 @@ void ColumnSpanSet::scan(
     }
 }
 
-void ColumnSpanSet::executeAction(Action& ac) const
+void ColumnSpanSet::executeAction(ScDocument& rDoc, Action& ac) const
 {
     for (size_t nTab = 0; nTab < maTables.size(); ++nTab)
     {
         if (maTables[nTab].empty())
             continue;
 
+        ScTable* pTab = rDoc.FetchTable(nTab);
+        if (!pTab)
+            continue;
+
         const TableType& rTab = maTables[nTab];
-        for (size_t nCol = 0; nCol < rTab.size(); ++nCol)
+        const SCCOL nEndCol = pTab->ClampToAllocatedColumns(rTab.size());
+        for (size_t nCol = 0; nCol <= nEndCol; ++nCol)
         {
             if (!rTab[nCol])
                 continue;
@@ -180,22 +185,16 @@ void ColumnSpanSet::executeColumnAction(ScDocument& rDoc, ColumnAction& ac) cons
         if (maTables[nTab].empty())
             continue;
 
+        ScTable* pTab = rDoc.FetchTable(nTab);
+        if (!pTab)
+            continue;
+
         const TableType& rTab = maTables[nTab];
-        for (SCCOL nCol = 0; nCol < static_cast<SCCOL>(rTab.size()); ++nCol)
+        const SCCOL nEndCol = pTab->ClampToAllocatedColumns(rTab.size());
+        for (SCCOL nCol = 0; nCol <= nEndCol; ++nCol)
         {
             if (!rTab[nCol])
                 continue;
-
-            ScTable* pTab = rDoc.FetchTable(nTab);
-            if (!pTab)
-                continue;
-
-            if (!rDoc.ValidCol(nCol) || nCol >= pTab->GetAllocatedColumnsCount())
-            {
-                // End the loop.
-                nCol = rTab.size();
-                continue;
-            }
 
             ScColumn& rColumn = pTab->aCol[nCol];
             ac.startColumn(&rColumn);
