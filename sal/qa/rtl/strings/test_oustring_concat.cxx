5510548a923d1f15c49b500f598f90ad47cc8c3b
Stephan Bergmann <sbergman@redhat.com>
2022-02-16 17:16:55 +0100

    Bump Linux Clang baseline to 8.0.1
    ...as discussed in the mail sub-thread starting at
    <https://lists.freedesktop.org/archives/libreoffice/2022-February/088476.html>
    "Also bump Linux Clang baseline to 12.0.1 (was: Bump --enable-compiler-plugins
    Clang baseline?)", and clean up newly-obsolete __clang_major__ checks
    
    Change-Id: Idacb9148b019c07e138277df3a085ba71c64a8e1

--- sal/qa/rtl/strings/test_oustring_concat.cxx
+++ sal/qa/rtl/strings/test_oustring_concat.cxx
@@ -69,12 +69,11 @@ void test::oustring::StringConcat::checkConcat()
     CPPUNIT_ASSERT_EQUAL( OUString( "foobar" ), OUString( OUStringBuffer( "foo" ) + "bar" ));
     CPPUNIT_ASSERT_EQUAL(( typeid( OUStringConcat< OUStringBuffer, const char[ 4 ] > )), typeid( OUStringBuffer( "foo" ) + "bar" ));
     CPPUNIT_ASSERT_EQUAL( OUString( "foobar" ), OUString( OUStringLiteral( u"foo" ) + "bar" ));
-#if (defined __GNUC__ && __GNUC__ <= 11 && !defined __clang__) || (defined __clang__ && __clang_major__ <= 6)
+#if defined __GNUC__ && __GNUC__ <= 11 && !defined __clang__
     CPPUNIT_ASSERT_EQUAL(( typeid( OUStringConcat< OUStringLiteral<4>, const char[ 4 ] > )), typeid( OUStringLiteral<4>( u"foo" ) + "bar" ));
         // the explicit OUStringLiteral<4> template argument in the unevaluated typeid context
         // is needed by some GCC versions, see <https://gcc.gnu.org/bugzilla/show_bug.cgi?id=96878>
-        // "Failed class template argument deduction in unevaluated, parenthesized context", and by
-        // some old Clang versions
+        // "Failed class template argument deduction in unevaluated, parenthesized context"
 #else
     CPPUNIT_ASSERT_EQUAL(( typeid( OUStringConcat< OUStringLiteral<4>, const char[ 4 ] > )), typeid( OUStringLiteral( u"foo" ) + "bar" ));
 #endif
