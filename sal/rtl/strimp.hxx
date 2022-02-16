a5e65173fcd90998764208b6a2241a3ae968fb81
Mike Kaganski <mike.kaganski@collabora.com>
2022-02-16 09:45:40 +0100

    Related: tdf#147421: optimize O(U)String's replaceAll*
    ... and underlying low-level C functions.
    
    Change-Id: I78c2dd75e272e6d3cdd2cfae007ca0a0ec1635e1

--- sal/rtl/strimp.hxx
+++ sal/rtl/strimp.hxx
@@ -25,8 +25,13 @@
 #include <sys/sdt.h>
 #endif
 
+#include <algorithm>
+#include <cassert>
+
 #include <sal/types.h>
+#include <rtl/strbuf.h>
 #include <rtl/string.hxx>
+#include <rtl/ustrbuf.h>
 #include <rtl/ustring.hxx>
 
 /* ======================================================================= */
@@ -90,6 +95,98 @@ extern rtl_freeStringFn rtl_freeString;
 #  define RTL_LOG_STRING_INTERN_NEW(s,o)
 #  define RTL_LOG_STRING_INTERN_DELETE(s)
 #endif /* USE_SDT_PROBES */
+
+namespace
+{
+namespace detail
+{
+void acquire(rtl_uString* s) { return rtl_uString_acquire(s); }
+void acquire(rtl_String* s) { return rtl_string_acquire(s); }
+
+void release(rtl_uString* s) { return rtl_uString_release(s); }
+void release(rtl_String* s) { return rtl_string_release(s); }
+
+void assign(rtl_uString** s, rtl_uString* s1) { return rtl_uString_assign(s, s1); }
+void assign(rtl_String** s, rtl_String* s1) { return rtl_string_assign(s, s1); }
+
+void new_WithLength(rtl_uString** s, sal_Int32 len) { return rtl_uString_new_WithLength(s, len); }
+void new_WithLength(rtl_String** s, sal_Int32 len) { return rtl_string_new_WithLength(s, len); }
+
+sal_Int32 indexOf(const sal_Unicode* s, sal_Int32 len, const sal_Unicode* subStr, sal_Int32 subLen)
+{
+    return rtl_ustr_indexOfStr_WithLength(s, len, subStr, subLen);
+}
+sal_Int32 indexOf(const sal_Unicode* s, sal_Int32 len, const char* subStr, sal_Int32 subLen)
+{
+    return rtl_ustr_indexOfAscii_WithLength(s, len, subStr, subLen);
+}
+sal_Int32 indexOf(const char* s, sal_Int32 nLen, const char* subStr, sal_Int32 subLen)
+{
+    return rtl_str_indexOfStr_WithLength(s, nLen, subStr, subLen);
+}
+
+void insert(rtl_uString** s, sal_Int32* capacity, sal_Int32 offset, const sal_Unicode* s1,
+            sal_Int32 len)
+{
+    return rtl_uStringbuffer_insert(s, capacity, offset, s1, len);
+}
+void insert(rtl_uString** s, sal_Int32* capacity, sal_Int32 offset, const char* s1, sal_Int32 len)
+{
+    return rtl_uStringbuffer_insert_ascii(s, capacity, offset, s1, len);
+}
+void insert(rtl_String** s, sal_Int32* capacity, sal_Int32 offset, const char* s1, sal_Int32 len)
+{
+    return rtl_stringbuffer_insert(s, capacity, offset, s1, len);
+}
+}
+
+template <class S, typename CharTypeFrom, typename CharTypeTo>
+void string_newReplaceAllFromIndex_impl(S** newStr, S* str, CharTypeFrom const* from,
+                                        sal_Int32 fromLength, CharTypeTo const* to,
+                                        sal_Int32 toLength, sal_Int32 fromIndex)
+{
+    assert(str != nullptr);
+    assert(fromLength >= 0);
+    assert(from != nullptr || fromLength == 0);
+    assert(toLength >= 0);
+    assert(to != nullptr || toLength == 0);
+    assert(fromIndex >= 0 && fromIndex <= str->length);
+    sal_Int32 i
+        = detail::indexOf(str->buffer + fromIndex, str->length - fromIndex, from, fromLength);
+    if (i >= 0)
+    {
+        if (str->length - fromLength > SAL_MAX_INT32 - toLength)
+            std::abort();
+        detail::acquire(str); // in case *newStr == str
+        sal_Int32 nCapacity = str->length + (toLength - fromLength);
+        if (fromLength < toLength)
+        {
+            // Pre-allocate up to 16 replacements more
+            const sal_Int32 nMaxMoreFinds = (str->length - fromIndex - i - fromLength) / fromLength;
+            const sal_Int32 nIncrease = toLength - fromLength;
+            const sal_Int32 nMoreReplacements = std::min(
+                { nMaxMoreFinds, (SAL_MAX_INT32 - nCapacity) / nIncrease, sal_Int32(16) });
+            nCapacity += nMoreReplacements * nIncrease;
+        }
+        detail::new_WithLength(newStr, nCapacity);
+        i += fromIndex;
+        fromIndex = 0;
+        do
+        {
+            detail::insert(newStr, &nCapacity, (*newStr)->length, str->buffer + fromIndex, i);
+            detail::insert(newStr, &nCapacity, (*newStr)->length, to, toLength);
+            fromIndex += i + fromLength;
+            i = detail::indexOf(str->buffer + fromIndex, str->length - fromIndex, from, fromLength);
+        } while (i >= 0);
+        // the rest
+        detail::insert(newStr, &nCapacity, (*newStr)->length, str->buffer + fromIndex,
+                       str->length - fromIndex);
+        detail::release(str);
+    }
+    else
+        detail::assign(newStr, str);
+}
+}
 
 #endif // INCLUDED_SAL_RTL_STRIMP_HXX
 
