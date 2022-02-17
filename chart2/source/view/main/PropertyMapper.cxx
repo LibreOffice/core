e5aea51818fad489ad79c90dbfb4acf2dae38355
Noel Grandin <noel.grandin@collabora.co.uk>
2022-02-17 14:30:50 +0200

    micro-optimisation: use std::move
    Change-Id: I3acee160130185e65a154bf205fcdcf52b9f8797

--- chart2/source/view/main/PropertyMapper.cxx
+++ chart2/source/view/main/PropertyMapper.cxx
@@ -59,7 +59,7 @@ void PropertyMapper::setMappedProperties(
             {
                 //do not set empty anys because of performance (otherwise SdrAttrObj::ItemChange will take much longer)
                 pNames[nN]  = rTarget;
-                pValues[nN] = aAny;
+                pValues[nN] = std::move(aAny);
                 ++nN;
             }
         }
