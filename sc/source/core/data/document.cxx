eacd4664815f427953ae35da074e8834bce16bee
Luboš Luňák <l.lunak@collabora.com>
2022-02-17 15:27:59 +0100

    fix range checking in calls like ScDocument::GetNote()
    Change-Id: I5612e765b3484b0515f4a16030ee19133ae3126a
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/130076
    Tested-by: Jenkins
    Reviewed-by: Luboš Luňák <l.lunak@collabora.com>
    (cherry picked from commit 957d99a539df6e21fd40370938ca5dab1613cf8c)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/130149
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
    Tested-by: Michael Stahl <michael.stahl@allotropia.de>

--- sc/source/core/data/document.cxx
+++ sc/source/core/data/document.cxx
@@ -6520,9 +6520,8 @@ ScPostIt* ScDocument::GetNote(const ScAddress& rPos)
 
 ScPostIt* ScDocument::GetNote(SCCOL nCol, SCROW nRow, SCTAB nTab)
 {
-    if (ValidTab(nTab) && nTab < static_cast<SCTAB>(maTabs.size()) &&
-        nCol < maTabs[nTab]->GetAllocatedColumnsCount())
-        return maTabs[nTab]->aCol[nCol].GetCellNote(nRow);
+    if (ValidTab(nTab) && nTab < static_cast<SCTAB>(maTabs.size()))
+        return maTabs[nTab]->GetNote(nCol, nRow);
     else
         return nullptr;
 
@@ -6535,7 +6534,8 @@ void ScDocument::SetNote(const ScAddress& rPos, std::unique_ptr<ScPostIt> pNote)
 
 void ScDocument::SetNote(SCCOL nCol, SCROW nRow, SCTAB nTab, std::unique_ptr<ScPostIt> pNote)
 {
-    return maTabs[nTab]->CreateColumnIfNotExists(nCol).SetCellNote(nRow, std::move(pNote));
+    if (ValidTab(nTab) && nTab < static_cast<SCTAB>(maTabs.size()))
+        maTabs[nTab]->SetNote(nCol, nRow, std::move(pNote));
 }
 
 bool ScDocument::HasNote(const ScAddress& rPos) const
@@ -6582,6 +6582,9 @@ bool ScDocument::HasColNotes(SCCOL nCol, SCTAB nTab) const
     if (!pTab)
         return false;
 
+    if (nCol >= pTab->GetAllocatedColumnsCount())
+        return false;
+
     return pTab->aCol[nCol].HasCellNotes();
 }
 
@@ -6625,6 +6628,7 @@ ScPostIt* ScDocument::GetOrCreateNote(const ScAddress& rPos)
     else
         return CreateNote(rPos);
 }
+
 ScPostIt* ScDocument::CreateNote(const ScAddress& rPos)
 {
     ScPostIt* pPostIt = new ScPostIt(*this, rPos);
