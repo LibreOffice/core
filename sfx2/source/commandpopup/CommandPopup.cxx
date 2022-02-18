5778fb7f9e51d24ce1a52eb1b931751cef3792dd
Rafael Lima <rafael.palma.lima@gmail.com>
2022-02-18 02:27:11 +0100

    tdf#143817 Fix HUD deleting text contents in Writer
    Change-Id: Ie28388016c475907c5d8fbe51906bc4a01d6b0d5
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/130091
    Tested-by: Jenkins
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>
    (cherry picked from commit 3c94a0f7729f1638ab3b273a186c48de859cfb49)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/130092
    (cherry picked from commit 4886374fca8f17de78ae558033aa677bcefc0a4e)

--- sfx2/source/commandpopup/CommandPopup.cxx
+++ sfx2/source/commandpopup/CommandPopup.cxx
@@ -248,6 +248,7 @@ IMPL_LINK(CommandListBox, TreeViewKeyPress, const KeyEvent&, rKeyEvent, bool)
     else if (rKeyEvent.GetKeyCode().GetCode() == KEY_RETURN)
     {
         RowActivated(*mpCommandTreeView);
+        return true;
     }
 
     return false;
