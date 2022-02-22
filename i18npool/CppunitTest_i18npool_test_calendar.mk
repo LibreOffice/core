2dfc95518328fc07720ca5de398a2b3c404286b8
Hossein <hossein@libreoffice.org>
2022-02-22 13:12:08 +0100

    tdf#145759 30.6001 -> monthDaysWithoutJanFeb
    30.6001 shows month days without Jan and Feb.
    According to the below link, it is calcuated as (365-31-28)/10 = 30.6
    but because of a floating point bug in hp-85, it was used as 30.6001
    as a workaround.
    
    "30.6001, 25 year old hack?"
    https://www.hpmuseum.org/cgi-sys/cgiwrap/hpmuseum/archv011.cgi?read=31650
    
    The value 30.6 is used as i18nutil::monthDaysWithoutJanFeb here
    instead of 30.6001
    
    Change-Id: I8cc7e046514dc3de652a1c37399e351cb2b614dc

---
+++ i18npool/CppunitTest_i18npool_test_calendar.mk
@@ -0,0 +1,37 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t; fill-column: 100 -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_CppunitTest_CppunitTest,i18npool_test_calendar))
+
+$(eval $(call gb_CppunitTest_set_include,i18npool_test_calendar,\
+    $$(INCLUDE) \
+    -I$(SRCDIR)/i18npool/inc \
+))
+
+$(eval $(call gb_CppunitTest_use_sdk_api,i18npool_test_calendar))
+
+$(eval $(call gb_CppunitTest_use_libraries,i18npool_test_calendar,\
+	cppu \
+	cppuhelper \
+	sal \
+	unotest \
+    i18npool \
+))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,i18npool_test_calendar,\
+    i18npool/qa/cppunit/test_calendar \
+))
+
+$(eval $(call gb_CppunitTest_use_ure,i18npool_test_calendar))
+
+$(eval $(call gb_CppunitTest_use_components,i18npool_test_calendar,\
+	i18npool/util/i18npool \
+))
+
+# vim: set noet sw=4 ts=4:
