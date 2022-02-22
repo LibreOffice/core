3e67678af2f6f8f7d25c5ad67b3828e96bc293b8
Caolán McNamara <caolanm@redhat.com>
2022-02-22 20:38:55 +0000

    add to suffix denylist
    add the macOS specific suffixes from
    https: //support.google.com/mail/answer/6590?hl=en#zippy=%2Cmessages-that-have-attachments
    
    Change-Id: I3a834694e3aefb757974cd00ade009874c68c8b4
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/130397
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

--- shell/source/unix/exec/shellexec.cxx
+++ shell/source/unix/exec/shellexec.cxx
@@ -144,8 +144,10 @@ void SAL_CALL ShellExec::execute( const OUString& aCommand, const OUString& aPar
                 throw css::lang::IllegalArgumentException(
                     "XSystemShellExecute.execute, cannot process <" + aCommand + ">", {}, 0);
             } else if (pathname.endsWithIgnoreAsciiCase(".class")
+                       || pathname.endsWithIgnoreAsciiCase(".dmg")
                        || pathname.endsWithIgnoreAsciiCase(".fileloc")
                        || pathname.endsWithIgnoreAsciiCase(".inetloc")
+                       || pathname.endsWithIgnoreAsciiCase(".ipa")
                        || pathname.endsWithIgnoreAsciiCase(".jar")
                        || pathname.endsWithIgnoreAsciiCase(".terminal"))
             {
