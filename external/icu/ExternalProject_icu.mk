LibreOffice/core

commit 4537886ec1de8beed02c7aea34a50727bc058bbd

externals: always provide platform configure flags
No idea why we just provided the platform flags when cross-
compiling. In the curious case, where the host platform is
detected as x86_64-pc-mingw32 per default and we actually
want to override it with x86_64-pc-cygwin, we don't do a
cross compile, but must override the host platform.

But there is additional special handling needed for the omitted
cross-platform build in the special case of --host=i686-pc-cygwin
and --build=x86_64-pc-cygwin, where we deliberatly ignore cross
building; Windows is already a slow build, so try to keep this
optimization (AMD64 can execute x86 binaries).

There is the theoretical case, where the externals config.guess
would have detected something else and that "magically" even
worked, while the LO detected triplet would fail, but this
should be fixed in the external in any way.

Change-Id: Ib7a9719e0e406fe90334b7611dc3f01b51692bfa
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/129153
Tested-by: Jenkins
Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>

external/icu/ExternalProject_icu.mk
@@ -27,7 +27,8 @@ $(call gb_ExternalProject_get_state_target,icu,build) :
 			INSTALL=`cygpath -m /usr/bin/install` $(if $(MSVC_USE_DEBUG_RUNTIME),LDFLAGS="-DEBUG") \
 			$(gb_RUN_CONFIGURE) ./configure \
 				$(if $(MSVC_USE_DEBUG_RUNTIME),--enable-debug --disable-release) \
-				$(if $(CROSS_COMPILING),--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM) \
+				$(gb_CONFIGURE_PLATFORMS) \
+				$(if $(CROSS_COMPILING), \
 					--with-cross-build=$(WORKDIR_FOR_BUILD)/UnpackedTarball/icu/source \
 					--disable-tools --disable-extras) \
 		&& $(MAKE) $(if $(CROSS_COMPILING),DATASUBDIR=data) $(if $(verbose),VERBOSE=1) \
@@ -78,7 +79,8 @@ $(call gb_ExternalProject_get_state_target,icu,build) :
 			$(if $(filter TRUE,$(DISABLE_DYNLOADING)),\
 				--with-data-packaging=static --enable-static --disable-shared --disable-dyload,\
 				--disable-static --enable-shared $(if $(filter ANDROID,$(OS)),--with-library-suffix=lo)) \
-			$(if $(CROSS_COMPILING),--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM)\
+			$(gb_CONFIGURE_PLATFORMS) \
+			$(if $(CROSS_COMPILING), \
 				--with-cross-build=$(WORKDIR_FOR_BUILD)/UnpackedTarball/icu/source \
 				--disable-tools --disable-extras) \
 			AR="$(AR)" RANLIB="$(RANLIB)" \

external/cairo/ExternalProject_cairo.mk
@@ -71,8 +71,8 @@ $(call gb_ExternalProject_get_state_target,cairo,build) :
 		--disable-valgrind \
 		$(if $(filter iOS,$(OS)),--disable-ft,--enable-ft --enable-fc) \
 		--disable-svg --enable-gtk-doc=no --enable-test-surfaces=no \
-		$(if $(CROSS_COMPILING),--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM) \
-		$(if $(filter INTEL ARM,$(CPUNAME)),ac_cv_c_bigendian=no ax_cv_c_float_words_bigendian=no)) \
+		$(gb_CONFIGURE_PLATFORMS) \
+		$(if $(CROSS_COMPILING),$(if $(filter INTEL ARM,$(CPUNAME)),ac_cv_c_bigendian=no ax_cv_c_float_words_bigendian=no)) \
 		$(if $(filter MACOSX,$(OS)),--prefix=/@.__________________________________________________OOO) \
 	&& cd src && $(MAKE) \
 	)

external/cairo/ExternalProject_pixman.mk
@@ -26,8 +26,8 @@ $(call gb_ExternalProject_get_state_target,pixman,build) :
 		$(gb_RUN_CONFIGURE) ./configure \
 		$(if $(filter TRUE,$(DISABLE_DYNLOADING)),--disable-shared,--disable-static) \
 		$(if $(filter ANDROID,$(OS)),--disable-arm-simd --disable-arm-neon --disable-arm-iwmmxt) \
-		$(if $(CROSS_COMPILING),--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM) \
-		$(if $(filter INTEL ARM,$(CPUNAME)),ac_cv_c_bigendian=no)) \
+		$(gb_CONFIGURE_PLATFORMS) \
+		$(if $(CROSS_COMPILING),$(if $(filter INTEL ARM,$(CPUNAME)),ac_cv_c_bigendian=no)) \
 		$(if $(filter EMSCRIPTEN,$(OS)),CFLAGS="-pthread") \
 		&& $(MAKE) \
 	)

external/coinmp/ExternalProject_coinmp.mk
@@ -31,7 +31,7 @@ $(call gb_ExternalProject_get_state_target,coinmp,build) :
 	+$(call gb_ExternalProject_run,build,\
 		$(gb_RUN_CONFIGURE) ./configure COIN_SKIP_PROJECTS="Data/Sample" \
 			$(if $(verbose),--disable-silent-rules,--enable-silent-rules) \
-			$(if $(CROSS_COMPILING),--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM)) \
+			$(gb_CONFIGURE_PLATFORMS) \
 			$(if $(DISABLE_DYNLOADING),--disable-shared) \
 			--disable-bzlib \
 			$(if $(filter MACOSX,$(OS)),--prefix=/@.__________________________________________________OOO) \

external/cppunit/ExternalProject_cppunit.mk
@@ -46,7 +46,7 @@ $(call gb_ExternalProject_get_state_target,cppunit,build) :
 			--disable-html-docs \
 			--disable-latex-docs \
 			--disable-werror \
-			$(if $(CROSS_COMPILING),--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM)) \
+			$(gb_CONFIGURE_PLATFORMS) \
 			$(if $(filter MACOSX,$(OS)),--prefix=/@.__________________________________________________NONE) \
 			$(if $(filter WNT,$(OS)),LDFLAGS="-Wl$(COMMA)--enable-runtime-pseudo-reloc-v2") \
 			$(if $(filter SOLARIS,$(OS)),LIBS="-lm") \

external/curl/ExternalProject_curl.mk
@@ -57,7 +57,7 @@ $(call gb_ExternalProject_get_state_target,curl,build):
 			--disable-rtsp --disable-smb --disable-smtp --disable-telnet  \
 			--disable-tftp  \
 			$(if $(filter LINUX,$(OS)),--without-ca-bundle --without-ca-path) \
-			$(if $(CROSS_COMPILING),--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM)) \
+			$(gb_CONFIGURE_PLATFORMS) \
 			$(if $(filter TRUE,$(DISABLE_DYNLOADING)),--disable-shared,--disable-static) \
 			$(if $(ENABLE_DEBUG),--enable-debug) \
 			$(if $(verbose),--disable-silent-rules,--enable-silent-rules) \

external/expat/ExternalProject_expat.mk
@@ -17,8 +17,8 @@ $(call gb_ExternalProject_get_state_target,expat,configure) :
 	$(call gb_Trace_StartRange,expat,EXTERNAL)
 	$(call gb_ExternalProject_run,configure,\
 		$(gb_RUN_CONFIGURE) ./configure --without-docbook \
-			$(if $(CROSS_COMPILING),--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM) \
-			$(if $(filter INTEL ARM,$(CPUNAME)),ac_cv_c_bigendian=no)) \
+			$(gb_CONFIGURE_PLATFORMS) \
+			$(if $(CROSS_COMPILING),$(if $(filter INTEL ARM,$(CPUNAME)),ac_cv_c_bigendian=no)) \
 	,,expat_configure.log)
 	$(call gb_Trace_EndRange,expat,EXTERNAL)
 

external/firebird/ExternalProject_firebird.mk
@@ -70,7 +70,7 @@ $(call gb_ExternalProject_get_state_target,firebird,build):
 		&& MAKE=$(MAKE) $(gb_RUN_CONFIGURE) ./configure \
 			--without-editline \
 			--with-wire-compress=no \
-			$(if $(CROSS_COMPILING),--build=$(BUILD_PLATFORM) --host=$(HOST_PLATFORM)) \
+			$(gb_CONFIGURE_PLATFORMS) \
 			$(if $(DISABLE_DYNLOADING), \
 				--enable-static --disable-shared \
 			, \

commit d51982ae2ebca7fde7b8437ae89f60490fc30b85

icu: override platform flags for MacOS build
So we actually have some external configure failing with my
"theoretical case" from the regressing commit message. But the
error in the --build/--host handling just happens when cross-
compiling...

This patch tries to be at least consistent and not just a cross-
compiling artifact, so it's not in the $(CROSS_COMPILING) block.

This fix is just blindly copied from the Firebird and NSS builds.

Regression from commit 4537886ec1de8beed02c7aea34a50727bc058bbd
("externals: always provide platform configure flags").

Change-Id: I8212dcdb50f41bb953994fbc51c37724267d6655

external/icu/ExternalProject_icu.mk
@@ -80,6 +80,8 @@ $(call gb_ExternalProject_get_state_target,icu,build) :
 				--with-data-packaging=static --enable-static --disable-shared --disable-dyload,\
 				--disable-static --enable-shared $(if $(filter ANDROID,$(OS)),--with-library-suffix=lo)) \
 			$(gb_CONFIGURE_PLATFORMS) \
+			$(if $(filter MACOSX,$(OS)), \
+				--build=$(subst macos,darwin,$(BUILD_PLATFORM)) --host=$(subst macos,darwin,$(HOST_PLATFORM))) \
 			$(if $(CROSS_COMPILING), \
 				--with-cross-build=$(WORKDIR_FOR_BUILD)/UnpackedTarball/icu/source \
 				--disable-tools --disable-extras) \