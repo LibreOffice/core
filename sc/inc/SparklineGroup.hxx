28a2748f189491cbd1e1ee218cf6b0b961448f9d
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2022-02-23 10:06:20 +0900

    sc: write the OOXML Sparkline props. to the model
    This moves the properties read during the OOXML import into the
    SparlineGroup class (the document model), so a sparkline can
    be connected with the properties.
    
    Change-Id: I09ecf560f1870624cb984722bdb8ee306d839dfe
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/131163
    Tested-by: Jenkins
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>
    (cherry picked from commit c4e0fd8dd682820c7daed1b7043e0612696ed513)

--- sc/inc/SparklineGroup.hxx
+++ sc/inc/SparklineGroup.hxx
@@ -11,6 +11,7 @@
 #pragma once
 
 #include "scdllapi.h"
+#include <tools/color.hxx>
 #include <optional>
 
 namespace sc
@@ -19,6 +20,40 @@ namespace sc
 class SC_DLLPUBLIC SparklineGroup
 {
 public:
+    Color m_aColorSeries;
+    Color m_aColorNegative;
+    Color m_aColorAxis;
+    Color m_aColorMarkers;
+    Color m_aColorFirst;
+    Color m_aColorLast;
+    Color m_aColorHigh;
+    Color m_aColorLow;
+
+    OUString m_sMinAxisType; // individual, group, custom
+    OUString m_sMaxAxisType; // individual, group, custom
+
+    double m_fLineWeight; // In pt
+
+    OUString m_sType; // line, column, stacked
+
+    bool m_bDateAxis;
+
+    OUString m_sDisplayEmptyCellsAs; // span, gap, zero
+
+    bool m_bMarkers;
+    bool m_bHigh;
+    bool m_bLow;
+    bool m_bFirst;
+    bool m_bLast;
+    bool m_bNegative;
+    bool m_bDisplayXAxis;
+    bool m_bDisplayHidden;
+    bool m_bRightToLeft;
+
+    std::optional<double> m_aManualMax; // if m_sMinAxisType is "custom"
+    std::optional<double> m_aManualMin; // if m_sMaxAxisType is "custom"
+    OUString m_sUID;
+
     SparklineGroup() {}
 
     SparklineGroup(const SparklineGroup&) = delete;
