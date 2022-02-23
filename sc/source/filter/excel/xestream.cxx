LibreOffice/core

commit 07b66cd3ac1a9f6c7b61a1d7da6e9d266e6de92d

Insert matrix formula vis ScDocumentImport, and more formula imorts.
Now SetGroupFormulaCell() is no longer used.

Change-Id: I10a387da04724794974eaf491a8efa4cda09d82a

sc/source/filter/oox/formulabuffer.cxx
@@ -16,6 +16,7 @@
 #include <com/sun/star/table/XCell2.hpp>
 #include "formulacell.hxx"
 #include "document.hxx"
+#include "documentimport.hxx"
 #include "convuno.hxx"
 
 #include "rangelst.hxx"
@@ -92,9 +93,7 @@ void FormulaBuffer::applyCellFormula( ScDocument& rDoc, const ApiTokenSequence& 
     ScUnoConversion::FillScAddress( aCellPos, rAddress );
     ScTokenConversion::ConvertToTokenArray( rDoc, aTokenArray, rTokens );
     ScFormulaCell* pNewCell = new ScFormulaCell( &rDoc, aCellPos, &aTokenArray );
-    pNewCell->StartListeningTo( &rDoc );
-    rDoc.EnsureTable(aCellPos.Tab());
-    rDoc.SetGroupFormulaCell(aCellPos, pNewCell);
+    getDocImport().setFormulaCell(aCellPos, pNewCell);
 }
 
 void FormulaBuffer::applyCellFormulas( const std::vector< TokenAddressItem >& rVector )
@@ -140,7 +139,7 @@ void FormulaBuffer::applySharedFormulas( sal_Int32 nTab )
     const std::vector<SharedFormulaEntry>& rSharedFormulas = itShared->second;
     const std::vector<SharedFormulaDesc>& rCells = itCells->second;
 
-    ScDocument& rDoc = getScDocument();
+    ScDocumentImport& rDoc = getDocImport();
 
     sc::SharedFormulaGroups aGroups;
     {
@@ -154,7 +153,7 @@ void FormulaBuffer::applySharedFormulas( sal_Int32 nTab )
 
             ScAddress aPos;
             ScUnoConversion::FillScAddress(aPos, rAddr);
-            ScCompiler aComp(&rDoc, aPos);
+            ScCompiler aComp(&rDoc.getDoc(), aPos);
             aComp.SetGrammar(formula::FormulaGrammar::GRAM_ENGLISH_XL_OOX);
             ScTokenArray* pArray = aComp.CompileString(rTokenStr);
             if (pArray)
@@ -174,17 +173,8 @@ void FormulaBuffer::applySharedFormulas( sal_Int32 nTab )
 
             ScAddress aPos;
             ScUnoConversion::FillScAddress(aPos, rAddr);
-            ScFormulaCell* pCell = new ScFormulaCell(&rDoc, aPos, pArray);
-            bool bInserted = rDoc.SetGroupFormulaCell(aPos, pCell);
-            if (!bInserted)
-            {
-                // Insertion failed.
-                delete pCell;
-                continue;
-            }
-
-            pCell->StartListeningTo(&rDoc);
-
+            ScFormulaCell* pCell = new ScFormulaCell(&rDoc.getDoc(), aPos, pArray);
+            rDoc.setFormulaCell(aPos, pCell);
             if (it->maCellValue.isEmpty())
             {
                 // No cached cell value. Mark it for re-calculation.
@@ -210,7 +200,7 @@ void FormulaBuffer::applySharedFormulas( sal_Int32 nTab )
 
 void FormulaBuffer::applyArrayFormulas( const std::vector< TokenRangeAddressItem >& rVector )
 {
-    ScDocument& rDoc = getScDocument();
+    ScDocumentImport& rDocImport = getDocImport();
     std::vector<TokenRangeAddressItem>::const_iterator it = rVector.begin(), itEnd = rVector.end();
     for (; it != itEnd; ++it)
     {
@@ -219,21 +209,11 @@ void FormulaBuffer::applyArrayFormulas( const std::vector< TokenRangeAddressItem
         ScRange aRange;
         ScUnoConversion::FillScRange(aRange, it->maCellRangeAddress);
 
-        ScCompiler aComp(&rDoc, aPos);
+        ScCompiler aComp(&rDocImport.getDoc(), aPos);
         aComp.SetGrammar(formula::FormulaGrammar::GRAM_ENGLISH_XL_OOX);
         ScTokenArray* pArray = aComp.CompileString(it->maTokenAndAddress.maTokenStr);
         if (pArray)
-        {
-            ScMarkData aMark;
-            aMark.SelectOneTable(aPos.Tab());
-            rDoc.InsertMatrixFormula(
-                aRange.aStart.Col(), aRange.aStart.Row(), aRange.aEnd.Col(), aRange.aEnd.Row(),
-                aMark, it->maTokenAndAddress.maTokenStr, pArray, formula::FormulaGrammar::GRAM_ENGLISH_XL_OOX);
-
-            ScFormulaCell* pFC = rDoc.GetFormulaCell(aPos);
-            if (pFC)
-                pFC->StartListeningTo(&rDoc);
-        }
+            rDocImport.setMatrixCells(aRange, *pArray, formula::FormulaGrammar::GRAM_ENGLISH_XL_OOX);
     }
 }
 

sc/inc/column.hxx
@@ -284,7 +284,6 @@ public:
      */
     ScFormulaCell* SetFormulaCell( SCROW nRow, ScFormulaCell* pCell );
     ScFormulaCell* SetFormulaCell( sc::ColumnBlockPosition& rBlockPos, SCROW nRow, ScFormulaCell* pCell );
-    bool SetGroupFormulaCell( SCROW nRow, ScFormulaCell* pCell );
 
     svl::SharedString GetSharedString( SCROW nRow ) const;
 

sc/inc/document.hxx
@@ -825,16 +825,6 @@ public:
      */
     SC_DLLPUBLIC ScFormulaCell* SetFormulaCell( const ScAddress& rPos, ScFormulaCell* pCell );
 
-    /**
-     * Set formula cell, and transfer its ownership to the document.  Unlike
-     * SetFormulaCell(), this call will <i>not</i> attempt to group the passed
-     * formula cell with the adjacent cells or cell groups.
-     *
-     * @return true if the cell is inserted, false otherwise. The caller
-     *         should delete the cell instance if the method returns false.
-     */
-    SC_DLLPUBLIC bool SetGroupFormulaCell( const ScAddress& rPos, ScFormulaCell* pCell );
-
     SC_DLLPUBLIC void InsertMatrixFormula(SCCOL nCol1, SCROW nRow1,
                                         SCCOL nCol2, SCROW nRow2,
                                         const ScMarkData& rMark,

sc/inc/table.hxx
@@ -339,7 +339,6 @@ public:
      *         is deleted automatically on failure to insert.
      */
     ScFormulaCell* SetFormulaCell( SCCOL nCol, SCROW nRow, ScFormulaCell* pCell );
-    bool SetGroupFormulaCell( SCCOL nCol, SCROW nRow, ScFormulaCell* pCell );
 
     svl::SharedString GetSharedString( SCCOL nCol, SCROW nRow ) const;
 

sc/source/core/data/column3.cxx
@@ -1769,21 +1769,6 @@ ScFormulaCell* ScColumn::SetFormulaCell( sc::ColumnBlockPosition& rBlockPos, SCR
     return pCell;
 }
 
-bool ScColumn::SetGroupFormulaCell( SCROW nRow, ScFormulaCell* pCell )
-{
-    sc::CellStoreType::iterator it = GetPositionToInsert(nRow);
-    sal_uInt32 nCellFormat = GetNumberFormat(nRow);
-    if( (nCellFormat % SV_COUNTRY_LANGUAGE_OFFSET) == 0)
-        pCell->SetNeedNumberFormat(true);
-    it = maCells.set(it, nRow, pCell);
-    maCellTextAttrs.set(nRow, sc::CellTextAttr());
-
-    CellStorageModified();
-
-    ActivateNewFormulaCell(it, nRow, *pCell, false);
-    return true;
-}
-
 svl::SharedString ScColumn::GetSharedString( SCROW nRow ) const
 {
     sc::CellStoreType::const_position_type aPos = maCells.position(nRow);

sc/source/core/data/documen2.cxx
@@ -1093,14 +1093,6 @@ ScFormulaCell* ScDocument::SetFormulaCell( const ScAddress& rPos, ScFormulaCell*
     return maTabs[rPos.Tab()]->SetFormulaCell(rPos.Col(), rPos.Row(), pCell);
 }
 
-bool ScDocument::SetGroupFormulaCell( const ScAddress& rPos, ScFormulaCell* pCell )
-{
-    if (!TableExists(rPos.Tab()))
-        return false;
-
-    return maTabs[rPos.Tab()]->SetGroupFormulaCell(rPos.Col(), rPos.Row(), pCell);
-}
-
 void ScDocument::SetConsolidateDlgData( const ScConsolidateParam* pData )
 {
     delete pConsolidateDlgData;

sc/source/core/data/table2.cxx
@@ -1383,14 +1383,6 @@ ScFormulaCell* ScTable::SetFormulaCell( SCCOL nCol, SCROW nRow, ScFormulaCell* p
     return aCol[nCol].SetFormulaCell(nRow, pCell);
 }
 
-bool ScTable::SetGroupFormulaCell( SCCOL nCol, SCROW nRow, ScFormulaCell* pCell )
-{
-    if (!ValidColRow(nCol, nRow))
-        return false;
-
-    return aCol[nCol].SetGroupFormulaCell(nRow, pCell);
-}
-
 svl::SharedString ScTable::GetSharedString( SCCOL nCol, SCROW nRow ) const
 {
     if (!ValidColRow(nCol, nRow))

sc/source/filter/oox/worksheethelper.cxx
@@ -1578,12 +1578,12 @@ void WorksheetHelper::putRichString( const CellAddress& rAddress, const RichStri
 
 void WorksheetHelper::putFormulaTokens( const CellAddress& rAddress, const ApiTokenSequence& rTokens )
 {
-    ScDocument& rDoc = getScDocument();
+    ScDocumentImport& rDoc = getDocImport();
     ScTokenArray aTokenArray;
     ScAddress aCellPos;
     ScUnoConversion::FillScAddress( aCellPos, rAddress );
-    ScTokenConversion::ConvertToTokenArray( rDoc, aTokenArray, rTokens );
-    getDocImport().setFormulaCell(aCellPos, aTokenArray);
+    ScTokenConversion::ConvertToTokenArray(rDoc.getDoc(), aTokenArray, rTokens);
+    rDoc.setFormulaCell(aCellPos, aTokenArray);
 }
 
 void WorksheetHelper::initializeWorksheetImport()

commit a699187a97b56e736d9dee7a6b5ebd640b2935e8

tdf#122098 xlsx im/export: let formulas guess their number format
This fixes the import side of a LO 4.2 regression
from commit 07b66cd3ac1a9f6c7b61a1d7da6e9d266e6de92d
and the export side of a LO 4.3 regression
from commit 69ecdad805281b2cb6ec2437da18daa19576deae

make CppunitTest_sc_pivottable_filters_test \
  CPPUNIT_TEST_NAME=testPivotTableBoolFieldFilterXLSX

Change-Id: I4c680dbff844cf1eca52de641856daafa032eeb4

sc/source/filter/excel/xestream.cxx
@@ -673,8 +673,16 @@ void XclXmlUtils::GetFormulaTypeAndValue( ScFormulaCell& rCell, const char*& rsT
             rsValue = ToOUString(lcl_GetErrorString(aResValue.mnError));
         break;
         case sc::FormulaResultValue::Value:
-            rsType = "n";
-            rsValue = OUString::number(aResValue.mfValue);
+            if (rCell.GetFormatType() == SvNumFormatType::LOGICAL)
+            {
+                rsType = "b";
+                rsValue = rCell.GetValue() == 0.0 ? "0" : "1";
+            }
+            else
+            {
+                rsType = "n";
+                rsValue = OUString::number(aResValue.mfValue);
+            }
         break;
         case sc::FormulaResultValue::String:
             rsType = "str";

sc/source/filter/oox/formulabuffer.cxx
@@ -153,6 +153,9 @@ void applySharedFormulas(
                 pCell = new ScFormulaCell(rDoc.getDoc(), aPos, *pArray);
 
             rDoc.setFormulaCell(aPos, pCell);
+            if (rDoc.getDoc().GetNumberFormat(aPos.Col(), aPos.Row(), aPos.Tab()) % SV_COUNTRY_LANGUAGE_OFFSET == 0)
+                pCell->SetNeedNumberFormat(true);
+
             if (rDesc.maCellValue.isEmpty())
             {
                 // No cached cell value. Mark it for re-calculation.
@@ -227,6 +230,8 @@ void applyCellFormulas(
                 pCell = new ScFormulaCell(rDoc.getDoc(), aPos, p->mpCell->GetCode()->Clone());
 
             rDoc.setFormulaCell(aPos, pCell);
+            if (rDoc.getDoc().GetNumberFormat(aPos.Col(), aPos.Row(), aPos.Tab()) % SV_COUNTRY_LANGUAGE_OFFSET == 0)
+                pCell->SetNeedNumberFormat(true);
 
             // Update the cache.
             p->mnRow = aPos.Row();
@@ -245,6 +250,8 @@ void applyCellFormulas(
 
         ScFormulaCell* pCell = new ScFormulaCell(rDoc.getDoc(), aPos, std::move(pCode));
         rDoc.setFormulaCell(aPos, pCell);
+        if (rDoc.getDoc().GetNumberFormat(aPos.Col(), aPos.Row(), aPos.Tab()) % SV_COUNTRY_LANGUAGE_OFFSET == 0)
+            pCell->SetNeedNumberFormat(true);
         rCache.store(aPos, pCell);
     }
 }

sc/qa/unit/pivottable_filters_test.cxx
@@ -2196,6 +2196,7 @@ void ScPivotTableFiltersTest::testPivotTableBoolFieldFilterXLSX()
     ScDocument& rDoc = xDocSh->GetDocument();
     ScDPCollection* pDPs = rDoc.GetDPCollection();
     CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(1), pDPs->GetCount());
+    CPPUNIT_ASSERT_EQUAL(OUString("TRUE"), rDoc.GetString(ScAddress(0, 1, 0))); //A2
 
     // Reload and check filtering of row dimensions
     xDocSh = saveAndReload(*xDocSh, FORMAT_XLSX);
@@ -2213,11 +2214,11 @@ void ScPivotTableFiltersTest::testPivotTableBoolFieldFilterXLSX()
 
     const ScDPSaveDimension::MemberList& rMembers = pSaveDim->GetMembers();
     CPPUNIT_ASSERT_EQUAL(size_t(2), rMembers.size());
-    ScDPSaveMember* pMember = pSaveDim->GetExistingMemberByName("0");
+    ScDPSaveMember* pMember = pSaveDim->GetExistingMemberByName("FALSE");
     CPPUNIT_ASSERT(pMember);
     CPPUNIT_ASSERT(pMember->HasIsVisible());
     CPPUNIT_ASSERT(!pMember->GetIsVisible());
-    pMember = pSaveDim->GetExistingMemberByName("1");
+    pMember = pSaveDim->GetExistingMemberByName("TRUE");
     CPPUNIT_ASSERT(pMember);
     CPPUNIT_ASSERT(pMember->HasIsVisible());
     CPPUNIT_ASSERT(pMember->GetIsVisible());