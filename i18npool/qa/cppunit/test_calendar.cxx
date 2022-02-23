9b9c50b8d6d4ea91be46a061f52303e68ec0eb05
Hossein <hossein@libreoffice.org>
2022-02-23 23:00:36 +0100

    tdf#145759 30.6001 -> monthDaysWithoutJanFeb
    30.6001 shows month days without Jan and Feb.
    According to the below link, it is calcuated as (365-31-28)/10 = 30.6
    but because of a floating point bug, it was used as 30.6001 as a
    workaround.
    
    "30.6001, 25 year old hack?"
    https://www.hpmuseum.org/cgi-sys/cgiwrap/hpmuseum/archv011.cgi?read=31650
    
    The value 30.6 is used as i18nutil::monthDaysWithoutJanFeb here
    instead of 30.6001. The new value is ~30.60000038 which is > 30.6, so
    the calculations should be correct. In order to make sure, a unit test
    is added, and part of the values are checked against the values
    calculated by this website:
    
    Julian Day and Civil Date Calculator
    https://core2.gsfc.nasa.gov/time/julian.html
    
    Change-Id: I8cc7e046514dc3de652a1c37399e351cb2b614dc

---
+++ i18npool/qa/cppunit/test_calendar.cxx
@@ -0,0 +1,88 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+#include <calendar_hijri.hxx>
+#include <com/sun/star/i18n/XOrdinalSuffix.hpp>
+#include <unotest/bootstrapfixturebase.hxx>
+
+using namespace com::sun::star;
+
+class TestCalendar : public test::BootstrapFixtureBase
+{
+private:
+    uno::Reference<i18n::XOrdinalSuffix> m_xOrdinal;
+
+public:
+    virtual void setUp() override;
+    virtual void tearDown() override;
+
+    void testGetHijri();
+    void testToGregorian();
+    void testGetGregorianDay();
+    void testGetJulianDay();
+
+    CPPUNIT_TEST_SUITE(TestCalendar);
+    CPPUNIT_TEST(testGetHijri);
+    CPPUNIT_TEST(testToGregorian);
+    CPPUNIT_TEST(testGetGregorianDay);
+    CPPUNIT_TEST(testGetJulianDay);
+    CPPUNIT_TEST_SUITE_END();
+};
+
+void TestCalendar::setUp()
+{
+    BootstrapFixtureBase::setUp();
+    m_xOrdinal.set(m_xSFactory->createInstance("com.sun.star.i18n.OrdinalSuffix"),
+                   uno::UNO_QUERY_THROW);
+}
+
+void TestCalendar::tearDown()
+{
+    m_xOrdinal.clear();
+    BootstrapFixtureBase::tearDown();
+}
+
+void TestCalendar::testGetHijri()
+{
+    sal_Int32 day = 22, month = 2, year = 2022;
+    i18npool::Calendar_hijri::getHijri(&day, &month, &year);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(21), day);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(7), month);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(1443), year);
+}
+
+void TestCalendar::testToGregorian()
+{
+    sal_Int32 day = 21, month = 7, year = 1443;
+    i18npool::Calendar_hijri::ToGregorian(&day, &month, &year);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(22), day);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(2), month);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(2022), year);
+}
+
+void TestCalendar::testGetGregorianDay()
+{
+    sal_Int32 lJulianDay = 2459633, day, month, year;
+    i18npool::Calendar_hijri::getGregorianDay(lJulianDay, &day, &month, &year);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(22), day);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(2), month);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(2022), year);
+}
+
+void TestCalendar::testGetJulianDay()
+{
+    sal_Int32 lJulianDay, day = 22, month = 2, year = 2022;
+    lJulianDay = i18npool::Calendar_hijri::getJulianDay(day, month, year);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(2459633), lJulianDay);
+}
+
+CPPUNIT_TEST_SUITE_REGISTRATION(TestCalendar);
+
+CPPUNIT_PLUGIN_IMPLEMENT();
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
