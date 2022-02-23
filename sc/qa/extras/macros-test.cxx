LibreOffice/core

commit a02b445c39d969fedc554fc2c500b88a27a13906

fdo#45747 remove the limitation to 3 sort entries in calc part1

sc/source/ui/vba/vbarange.cxx
@@ -3539,9 +3539,9 @@ ScVbaRange::Sort( const uno::Any& Key1, const uno::Any& Order1, const uno::Any& 
 
     // set up defaults
 
-    sal_Int16 nOrder1 = aSortParam.bAscending[0] ? excel::XlSortOrder::xlAscending : excel::XlSortOrder::xlDescending;
-    sal_Int16 nOrder2 = aSortParam.bAscending[1] ? excel::XlSortOrder::xlAscending : excel::XlSortOrder::xlDescending;
-    sal_Int16 nOrder3 = aSortParam.bAscending[2] ? excel::XlSortOrder::xlAscending : excel::XlSortOrder::xlDescending;
+    sal_Int16 nOrder1 = aSortParam.maKeyState[1].bAscending ? excel::XlSortOrder::xlAscending : excel::XlSortOrder::xlDescending;
+    sal_Int16 nOrder2 = aSortParam.maKeyState[2].bAscending ? excel::XlSortOrder::xlAscending : excel::XlSortOrder::xlDescending;
+    sal_Int16 nOrder3 = aSortParam.maKeyState[3].bAscending ? excel::XlSortOrder::xlAscending : excel::XlSortOrder::xlDescending;
 
     sal_Int16 nCustom = aSortParam.nUserIndex;
     sal_Int16 nSortMethod = excel::XlSortMethod::xlPinYin;
@@ -3620,26 +3620,26 @@ ScVbaRange::Sort( const uno::Any& Key1, const uno::Any& Order1, const uno::Any& 
     {
         nOrder1 = ::comphelper::getINT16(Order1);
         if (  nOrder1 == excel::XlSortOrder::xlAscending )
-            aSortParam.bAscending[0]  = true;
+            aSortParam.maKeyState[0].bAscending  = true;
         else
-            aSortParam.bAscending[0]  = false;
+            aSortParam.maKeyState[0].bAscending  = false;
 
     }
     if ( Order2.hasValue() )
     {
         nOrder2 = ::comphelper::getINT16(Order2);
         if ( nOrder2 == excel::XlSortOrder::xlAscending )
-            aSortParam.bAscending[1]  = true;
+            aSortParam.maKeyState[1].bAscending  = true;
         else
-            aSortParam.bAscending[1]  = false;
+            aSortParam.maKeyState[1].bAscending  = false;
     }
     if ( Order3.hasValue() )
     {
         nOrder3 = ::comphelper::getINT16(Order3);
         if ( nOrder3 == excel::XlSortOrder::xlAscending )
-            aSortParam.bAscending[2]  = true;
+            aSortParam.maKeyState[2].bAscending  = true;
         else
-            aSortParam.bAscending[2]  = false;
+            aSortParam.maKeyState[2].bAscending  = false;
     }
 
     uno::Reference< table::XCellRange > xKey1;

sc/inc/sortparam.hxx
@@ -29,15 +29,26 @@
 #ifndef SC_SORTPARAM_HXX
 #define SC_SORTPARAM_HXX
 
+#define DEFSORT 3
+
+#include <vector>
+
 #include "address.hxx"
 #include <tools/solar.h>
 #include <com/sun/star/lang/Locale.hpp>
 #include "scdllapi.h"
 
-#define MAXSORT 3
-
 struct ScSubTotalParam;
 struct ScQueryParam;
+
+struct ScSortKeyState
+{
+    bool     bDoSort;
+    SCCOLROW nField;
+    bool     bAscending;
+};
+
+typedef ::std::vector<ScSortKeyState> ScSortKeyStateVec;
 
 struct SC_DLLPUBLIC ScSortParam
 {
@@ -56,12 +67,11 @@ struct SC_DLLPUBLIC ScSortParam
     SCTAB       nDestTab;
     SCCOL       nDestCol;
     SCROW       nDestRow;
-    bool        bDoSort[MAXSORT];
-    SCCOLROW    nField[MAXSORT];
-    bool        bAscending[MAXSORT];
+    ScSortKeyStateVec maKeyState;
     ::com::sun::star::lang::Locale aCollatorLocale;
     ::rtl::OUString aCollatorAlgorithm;
     sal_uInt16  nCompatHeader;
+
 
     ScSortParam();
     ScSortParam( const ScSortParam& r );
@@ -73,8 +83,9 @@ struct SC_DLLPUBLIC ScSortParam
     ScSortParam&    operator=  ( const ScSortParam& r );
     bool            operator== ( const ScSortParam& rOther ) const;
     void            Clear       ();
-
     void            MoveToDest();
+
+    inline sal_uInt16 GetSortKeyCount() const { return maKeyState.size(); }
 };
 
 

sc/source/core/data/sortparam.cxx
@@ -51,15 +51,10 @@ ScSortParam::ScSortParam( const ScSortParam& r ) :
         bNaturalSort(r.bNaturalSort),bUserDef(r.bUserDef),
         bIncludePattern(r.bIncludePattern),bInplace(r.bInplace),
         nDestTab(r.nDestTab),nDestCol(r.nDestCol),nDestRow(r.nDestRow),
+        maKeyState( r.maKeyState ),
         aCollatorLocale( r.aCollatorLocale ), aCollatorAlgorithm( r.aCollatorAlgorithm ),
         nCompatHeader( r.nCompatHeader )
 {
-    for (sal_uInt16 i=0; i<MAXSORT; i++)
-    {
-        bDoSort[i]    = r.bDoSort[i];
-        nField[i]     = r.nField[i];
-        bAscending[i] = r.bAscending[i];
-    }
 }
 
 //------------------------------------------------------------------------
@@ -76,12 +71,8 @@ void ScSortParam::Clear()
     aCollatorLocale = ::com::sun::star::lang::Locale();
     aCollatorAlgorithm = ::rtl::OUString();
 
-    for (sal_uInt16 i=0; i<MAXSORT; i++)
-    {
-        bDoSort[i]    = false;
-        nField[i]     = 0;
-        bAscending[i] = true;
-    }
+    // Initialize to default size
+    maKeyState.assign( DEFSORT, { false, 0, true } );
 }
 
 //------------------------------------------------------------------------
@@ -103,16 +94,10 @@ ScSortParam& ScSortParam::operator=( const ScSortParam& r )
     nDestTab        = r.nDestTab;
     nDestCol        = r.nDestCol;
     nDestRow        = r.nDestRow;
+    maKeyState      = r.maKeyState;
     aCollatorLocale         = r.aCollatorLocale;
     aCollatorAlgorithm      = r.aCollatorAlgorithm;
     nCompatHeader   = r.nCompatHeader;
-
-    for (sal_uInt16 i=0; i<MAXSORT; i++)
-    {
-        bDoSort[i]    = r.bDoSort[i];
-        nField[i]     = r.nField[i];
-        bAscending[i] = r.bAscending[i];
-    }
 
     return *this;
 }
@@ -125,10 +110,20 @@ bool ScSortParam::operator==( const ScSortParam& rOther ) const
     // Anzahl der Sorts gleich?
     sal_uInt16 nLast      = 0;
     sal_uInt16 nOtherLast = 0;
-    while ( bDoSort[nLast++] && nLast < MAXSORT ) ;
-    while ( rOther.bDoSort[nOtherLast++] && nOtherLast < MAXSORT ) ;
-    nLast--;
-    nOtherLast--;
+    sal_uInt16 nSortSize = GetSortKeyCount();
+
+    if ( !maKeyState.empty() )
+    {
+        while ( maKeyState[nLast++].bDoSort && nLast < nSortSize ) ;
+        nLast--;
+    }
+
+    if ( !rOther.maKeyState.empty() )
+    {
+        while ( rOther.maKeyState[nOtherLast++].bDoSort && nOtherLast < nSortSize ) ;
+        nOtherLast--;
+    }
+
     if (   (nLast           == nOtherLast)
         && (nCol1           == rOther.nCol1)
         && (nRow1           == rOther.nRow1)
@@ -149,14 +144,17 @@ bool ScSortParam::operator==( const ScSortParam& rOther ) const
         && (aCollatorLocale.Country     == rOther.aCollatorLocale.Country)
         && (aCollatorLocale.Variant     == rOther.aCollatorLocale.Variant)
         && (aCollatorAlgorithm          == rOther.aCollatorAlgorithm)
+        && ( !maKeyState.empty() || !rOther.maKeyState.empty() )
         )
     {
         bEqual = true;
         for ( sal_uInt16 i=0; i<=nLast && bEqual; i++ )
-        {
-            bEqual = (nField[i] == rOther.nField[i]) && (bAscending[i]  == rOther.bAscending[i]);
-        }
+            bEqual = ( maKeyState[i].nField == rOther.maKeyState[i].nField ) &&
+                ( maKeyState[i].bAscending  == rOther.maKeyState[i].bAscending );
     }
+    if ( maKeyState.empty() && rOther.maKeyState.empty() )
+        bEqual = true;
+
     return bEqual;
 }
 
@@ -172,6 +170,7 @@ ScSortParam::ScSortParam( const ScSubTotalParam& rSub, const ScSortParam& rOld )
         nCompatHeader( rOld.nCompatHeader )
 {
     sal_uInt16 nNewCount = 0;
+    sal_uInt16 nSortSize = GetSortKeyCount();
     sal_uInt16 i;
 
     //  zuerst die Gruppen aus den Teilergebnissen
@@ -179,42 +178,34 @@ ScSortParam::ScSortParam( const ScSubTotalParam& rSub, const ScSortParam& rOld )
         for (i=0; i<MAXSUBTOTAL; i++)
             if (rSub.bGroupActive[i])
             {
-                if (nNewCount < MAXSORT)
+                if (nNewCount < nSortSize)
                 {
-                    bDoSort[nNewCount]    = true;
-                    nField[nNewCount]     = rSub.nField[i];
-                    bAscending[nNewCount] = rSub.bAscending;
+                    maKeyState[nNewCount] = { true, rSub.nField[i], rSub.bAscending };
                     ++nNewCount;
                 }
             }
 
     //  dann dahinter die alten Einstellungen
-    for (i=0; i<MAXSORT; i++)
-        if (rOld.bDoSort[i])
+    for (i=0; i<nSortSize; i++)
+        if (rOld.maKeyState[i].bDoSort)
         {
-            SCCOLROW nThisField = rOld.nField[i];
+            SCCOLROW nThisField = rOld.maKeyState[i].nField;
             bool bDouble = false;
             for (sal_uInt16 j=0; j<nNewCount; j++)
-                if ( nField[j] == nThisField )
+                if ( maKeyState[j].nField == nThisField )
                     bDouble = true;
             if (!bDouble)               // ein Feld nicht zweimal eintragen
             {
-                if (nNewCount < MAXSORT)
+                if (nNewCount < nSortSize)
                 {
-                    bDoSort[nNewCount]    = true;
-                    nField[nNewCount]     = nThisField;
-                    bAscending[nNewCount] = rOld.bAscending[i];
+                    maKeyState[nNewCount] = { true, nThisField, rOld.maKeyState[i].bAscending };
                     ++nNewCount;
                 }
             }
         }
 
-    for (i=nNewCount; i<MAXSORT; i++)       // Rest loeschen
-    {
-        bDoSort[i]    = false;
-        nField[i]     = 0;
-        bAscending[i] = true;
-    }
+    for (i=nNewCount; i<nSortSize; i++)       // Rest loeschen
+        maKeyState.push_back( ScSortKeyState({ false, 0, true }) );
 }
 
 //------------------------------------------------------------------------
@@ -228,15 +219,10 @@ ScSortParam::ScSortParam( const ScQueryParam& rParam, SCCOL nCol ) :
         bInplace(true),
         nDestTab(0),nDestCol(0),nDestRow(0), nCompatHeader(2)
 {
-    bDoSort[0] = true;
-    nField[0] = nCol;
-    bAscending[0] = true;
-    for (sal_uInt16 i=1; i<MAXSORT; i++)
-    {
-        bDoSort[i]    = false;
-        nField[i]     = 0;
-        bAscending[i] = true;
-    }
+    maKeyState.push_back( ScSortKeyState( { true, nCol, true } ) );
+
+    for (sal_uInt16 i=1; i<GetSortKeyCount(); i++)
+        maKeyState.push_back( ScSortKeyState( { false, 0, true } ) );
 }
 
 //------------------------------------------------------------------------
@@ -252,11 +238,11 @@ void ScSortParam::MoveToDest()
         nRow1 = sal::static_int_cast<SCROW>( nRow1 + nDifY );
         nCol2 = sal::static_int_cast<SCCOL>( nCol2 + nDifX );
         nRow2 = sal::static_int_cast<SCROW>( nRow2 + nDifY );
-        for (sal_uInt16 i=0; i<MAXSORT; i++)
+        for (sal_uInt16 i=0; i<GetSortKeyCount(); i++)
             if (bByRow)
-                nField[i] += nDifX;
+                maKeyState[i].nField += nDifX;
             else
-                nField[i] += nDifY;
+                maKeyState[i].nField += nDifY;
 
         bInplace = true;
     }

sc/source/core/data/table3.cxx
@@ -270,14 +270,14 @@ public:
 ScSortInfoArray* ScTable::CreateSortInfoArray( SCCOLROW nInd1, SCCOLROW nInd2 )
 {
     sal_uInt16 nUsedSorts = 1;
-    while ( nUsedSorts < nMaxSorts && aSortParam.bDoSort[nUsedSorts] )
+    while ( nUsedSorts < nMaxSorts && aSortParam.maKeyState[nUsedSorts].bDoSort )
         nUsedSorts++;
     ScSortInfoArray* pArray = new ScSortInfoArray( nUsedSorts, nInd1, nInd2 );
     if ( aSortParam.bByRow )
     {
         for ( sal_uInt16 nSort = 0; nSort < nUsedSorts; nSort++ )
         {
-            SCCOL nCol = static_cast<SCCOL>(aSortParam.nField[nSort]);
+            SCCOL nCol = static_cast<SCCOL>(aSortParam.maKeyState[nSort].nField);
             ScColumn* pCol = &aCol[nCol];
             for ( SCROW nRow = nInd1; nRow <= nInd2; nRow++ )
             {
@@ -292,7 +292,7 @@ ScSortInfoArray* ScTable::CreateSortInfoArray( SCCOLROW nInd1, SCCOLROW nInd2 )
     {
         for ( sal_uInt16 nSort = 0; nSort < nUsedSorts; nSort++ )
         {
-            SCROW nRow = aSortParam.nField[nSort];
+            SCROW nRow = aSortParam.maKeyState[nSort].nField;
             for ( SCCOL nCol = static_cast<SCCOL>(nInd1);
                     nCol <= static_cast<SCCOL>(nInd2); nCol++ )
             {
@@ -473,7 +473,7 @@ short ScTable::CompareCell( sal_uInt16 nSort,
                 else if (nVal1 > nVal2)
                     nRes = 1;
             }
-            if ( !aSortParam.bAscending[nSort] )
+            if ( !aSortParam.maKeyState[nSort].bAscending )
                 nRes = -nRes;
         }
         else
@@ -499,12 +499,12 @@ short ScTable::Compare( ScSortInfoArray* pArray, SCCOLROW nIndex1, SCCOLROW nInd
         ScSortInfo* pInfo2 = pArray->Get( nSort, nIndex2 );
         if ( aSortParam.bByRow )
             nRes = CompareCell( nSort,
-                pInfo1->pCell, static_cast<SCCOL>(aSortParam.nField[nSort]), pInfo1->nOrg,
-                pInfo2->pCell, static_cast<SCCOL>(aSortParam.nField[nSort]), pInfo2->nOrg );
+                pInfo1->pCell, static_cast<SCCOL>(aSortParam.maKeyState[nSort].nField), pInfo1->nOrg,
+                pInfo2->pCell, static_cast<SCCOL>(aSortParam.maKeyState[nSort].nField), pInfo2->nOrg );
         else
             nRes = CompareCell( nSort,
-                pInfo1->pCell, static_cast<SCCOL>(pInfo1->nOrg), aSortParam.nField[nSort],
-                pInfo2->pCell, static_cast<SCCOL>(pInfo2->nOrg), aSortParam.nField[nSort] );
+                pInfo1->pCell, static_cast<SCCOL>(pInfo1->nOrg), aSortParam.maKeyState[nSort].nField,
+                pInfo2->pCell, static_cast<SCCOL>(pInfo2->nOrg), aSortParam.maKeyState[nSort].nField );
     } while ( nRes == 0 && ++nSort < pArray->GetUsedSorts() );
     if( nRes == 0 )
     {
@@ -684,22 +684,22 @@ short ScTable::Compare(SCCOLROW nIndex1, SCCOLROW nIndex2)
     {
         do
         {
-            SCCOL nCol = static_cast<SCCOL>(aSortParam.nField[nSort]);
+            SCCOL nCol = static_cast<SCCOL>(aSortParam.maKeyState[nSort].nField);
             ScBaseCell* pCell1 = aCol[nCol].GetCell( nIndex1 );
             ScBaseCell* pCell2 = aCol[nCol].GetCell( nIndex2 );
             nRes = CompareCell( nSort, pCell1, nCol, nIndex1, pCell2, nCol, nIndex2 );
-        } while ( nRes == 0 && ++nSort < nMaxSorts && aSortParam.bDoSort[nSort] );
+        } while ( nRes == 0 && ++nSort < nMaxSorts && aSortParam.maKeyState[nSort].bDoSort );
     }
     else
     {
         do
         {
-            SCROW nRow = aSortParam.nField[nSort];
+            SCROW nRow = aSortParam.maKeyState[nSort].nField;
             ScBaseCell* pCell1 = aCol[nIndex1].GetCell( nRow );
             ScBaseCell* pCell2 = aCol[nIndex2].GetCell( nRow );
             nRes = CompareCell( nSort, pCell1, static_cast<SCCOL>(nIndex1),
                     nRow, pCell2, static_cast<SCCOL>(nIndex2), nRow );
-        } while ( nRes == 0 && ++nSort < nMaxSorts && aSortParam.bDoSort[nSort] );
+        } while ( nRes == 0 && ++nSort < nMaxSorts && aSortParam.maKeyState[nSort].bDoSort );
     }
     return nRes;
 }

sc/source/core/tool/dbdata.cxx
@@ -246,7 +246,7 @@ ScDBData::~ScDBData()
             aBuf.append(ScGlobal::GetRscString(STR_OPERATION_FILTER));
     }
 
-    if (mpSortParam->bDoSort[0])
+    if (mpSortParam->maKeyState[0].bDoSort)
     {
         if (aBuf.getLength())
             aBuf.appendAscii(RTL_CONSTASCII_STRINGPARAM(", "));
@@ -299,13 +299,13 @@ void ScDBData::MoveTo(SCTAB nTab, SCCOL nCol1, SCROW nRow1, SCCOL nCol2, SCROW n
     long nSortDif = bByRow ? nDifX : nDifY;
     long nSortEnd = bByRow ? static_cast<long>(nCol2) : static_cast<long>(nRow2);
 
-    for (i=0; i<MAXSORT; i++)
+    for (i=0; i<mpSortParam->GetSortKeyCount(); i++)
     {
-        mpSortParam->nField[i] += nSortDif;
-        if (mpSortParam->nField[i] > nSortEnd)
+        mpSortParam->maKeyState[i].nField += nSortDif;
+        if (mpSortParam->maKeyState[i].nField > nSortEnd)
         {
-            mpSortParam->nField[i] = 0;
-            mpSortParam->bDoSort[i] = false;
+            mpSortParam->maKeyState[i].nField = 0;
+            mpSortParam->maKeyState[i].bDoSort = false;
         }
     }
 
@@ -460,7 +460,7 @@ bool ScDBData::HasQueryParam() const
 
 bool ScDBData::HasSortParam() const
 {
-    return mpSortParam && mpSortParam->bDoSort[0];
+    return mpSortParam && mpSortParam->maKeyState[0].bDoSort;
 }
 
 bool ScDBData::HasSubTotalParam() const

sc/source/filter/xml/XMLExportDatabaseRanges.cxx
@@ -317,9 +317,9 @@ private:
 
         // Count sort items first.
         size_t nSortCount = 0;
-        for (; nSortCount < MAXSORT; ++nSortCount)
+        for (; nSortCount < aParam.GetSortKeyCount(); ++nSortCount)
         {
-            if (!aParam.bDoSort[nSortCount])
+            if (!aParam.maKeyState[nSortCount].bDoSort)
                 break;
         }
 
@@ -359,10 +359,10 @@ private:
         for (size_t i = 0; i < nSortCount; ++i)
         {
             // Convert field value from absolute to relative.
-            SCCOLROW nField = aParam.nField[i] - nFieldStart;
+            SCCOLROW nField = aParam.maKeyState[i].nField - nFieldStart;
             mrExport.AddAttribute(XML_NAMESPACE_TABLE, XML_FIELD_NUMBER, OUString::valueOf(nField));
 
-            if (!aParam.bAscending[i])
+            if (!aParam.maKeyState[i].bAscending)
                 mrExport.AddAttribute(XML_NAMESPACE_TABLE, XML_ORDER, XML_DESCENDING);
 
             if (aParam.bUserDef)

sc/source/filter/xml/xmldrani.cxx
@@ -370,11 +370,11 @@ ScDBData* ScXMLDatabaseRangeContext::ConvertToDBData(const OUString& rName)
         ScSortDescriptor::FillSortParam(aParam, aSortSequence);
 
         SCCOLROW nStartPos = aParam.bByRow ? maRange.aStart.Col() : maRange.aStart.Row();
-        for (size_t i = 0; i < MAXSORT; ++i)
+        for (size_t i = 0; i < aParam.GetSortKeyCount(); ++i)
         {
-            if (!aParam.bDoSort[i])
+            if (!aParam.maKeyState[i].bDoSort)
                 break;
-            aParam.nField[i] += nStartPos;
+            aParam.maKeyState[i].nField += nStartPos;
         }
 
         pData->SetSortParam(aParam);

commit a65ac9d88326e470209bd0e29fb345193bce6dd0

tdf#147611: fix indices
The maKeyState vector in ScSortParam is initialized with three elements,
and they are never removed. The code in ScVbaRange::Sort incorrectly used
1-based indices into that vector.

This was broken since commit a02b445c39d969fedc554fc2c500b88a27a13906
  Author: Albert Thuswaldner <albert.thuswaldner@gmail.com>
  Date:   Tue Mar 20 19:38:29 2012 +0100

    fdo#45747 remove the limitation to 3 sort entries in calc part1

It was fixed in commit 568d3912bf8ced76ecb9506bccc3bd361daba082
  Author: Kohei Yoshida <kohei.yoshida@gmail.com>
  Date:   Wed Apr 04 15:30:35 2012 -0400

    Cleanup. ScPivot(Collection) is no more.

but was restored by commit 3e887edcaacc5b0f5e35d682a259124648e84229
  Author: Markus Mohrhard <markus.mohrhard@googlemail.com>
  Date:   Thu Apr 5 05:05:40 2012 +0200

    Revert "Cleanup. ScPivot(Collection) is no more."

Change-Id: I009252e794c9365f0aef8a61daf9cbd40eca8b75

sc/source/ui/vba/vbarange.cxx
@@ -3413,9 +3413,9 @@ ScVbaRange::Sort( const uno::Any& Key1, const uno::Any& Order1, const uno::Any& 
 
     // set up defaults
 
-    sal_Int16 nOrder1 = aSortParam.maKeyState[1].bAscending ? excel::XlSortOrder::xlAscending : excel::XlSortOrder::xlDescending;
-    sal_Int16 nOrder2 = aSortParam.maKeyState[2].bAscending ? excel::XlSortOrder::xlAscending : excel::XlSortOrder::xlDescending;
-    sal_Int16 nOrder3 = aSortParam.maKeyState[3].bAscending ? excel::XlSortOrder::xlAscending : excel::XlSortOrder::xlDescending;
+    sal_Int16 nOrder1 = aSortParam.maKeyState[0].bAscending ? excel::XlSortOrder::xlAscending : excel::XlSortOrder::xlDescending;
+    sal_Int16 nOrder2 = aSortParam.maKeyState[1].bAscending ? excel::XlSortOrder::xlAscending : excel::XlSortOrder::xlDescending;
+    sal_Int16 nOrder3 = aSortParam.maKeyState[2].bAscending ? excel::XlSortOrder::xlAscending : excel::XlSortOrder::xlDescending;
 
     sal_Int16 nCustom = aSortParam.nUserIndex;
     sal_Int16 nSortMethod = excel::XlSortMethod::xlPinYin;

sc/qa/extras/macros-test.cxx
@@ -65,6 +65,7 @@ public:
     void testTdf146742();
     void testMacroButtonFormControlXlsxExport();
     void testShapeLayerId();
+    void testVbaRangeSort();
 
     CPPUNIT_TEST_SUITE(ScMacrosTest);
     CPPUNIT_TEST(testStarBasic);
@@ -94,6 +95,7 @@ public:
     CPPUNIT_TEST(testTdf146742);
     CPPUNIT_TEST(testMacroButtonFormControlXlsxExport);
     CPPUNIT_TEST(testShapeLayerId);
+    CPPUNIT_TEST(testVbaRangeSort);
 
     CPPUNIT_TEST_SUITE_END();
 };
@@ -1044,6 +1046,40 @@ void ScMacrosTest::testShapeLayerId()
     // The LayerID property of com.sun.star.drawing.Shape service has 'short' IDL type.
     // The expected run-time error is because there are only 5 layers there.
     CPPUNIT_ASSERT_EQUAL(Any(OUString("0 Expected runtime error happened")), aRet);
+    pDocSh->DoClose();
+}
+
+void ScMacrosTest::testVbaRangeSort()
+{
+    auto xComponent = loadFromDesktop("private:factory/scalc");
+
+    css::uno::Reference<css::document::XEmbeddedScripts> xDocScr(xComponent, UNO_QUERY_THROW);
+    auto xLibs = xDocScr->getBasicLibraries();
+    auto xLibrary = xLibs->createLibrary("TestLibrary");
+    xLibrary->insertByName(
+        "TestModule",
+        uno::Any(OUString(
+            "Option VBASupport 1\n"
+            "Sub TestVbaRangeSort\n"
+            "  Range(Cells(1, 1), Cells(2, 2)).Select\n"
+            "  Selection.Sort Key1:=Range(\"A1\"), Header:=False, Order1:=xlDescending, _\n"
+            "                 OrderCustom:=1, MatchCase:=False, Orientation:=xlTopToBottom, _\n"
+            "                 DataOption1:=xlSortNormal\n"
+            "End Sub\n")));
+
+    Any aRet;
+    Sequence<sal_Int16> aOutParamIndex;
+    Sequence<Any> aOutParam;
+
+    SfxObjectShell* pFoundShell = SfxObjectShell::GetShellFromComponent(xComponent);
+    ScDocShell* pDocSh = static_cast<ScDocShell*>(pFoundShell);
+    CPPUNIT_ASSERT(pDocSh);
+
+    // Without the fix in place, this call would have crashed in debug builds with failed assertion
+    SfxObjectShell::CallXScript(
+        xComponent,
+        "vnd.sun.Star.script:TestLibrary.TestModule.TestLayerID?language=Basic&location=document",
+        {}, aRet, aOutParamIndex, aOutParam);
     pDocSh->DoClose();
 }
 