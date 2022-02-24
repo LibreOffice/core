c262f68bb3089336b5ce11c4b4c8f8b8fb81b589
Xisco Fauli <xiscofauli@libreoffice.org>
2022-02-24 11:29:30 +0100

    uitest: sw: wait until the startcenter is displayed
    See https://gerrit.libreoffice.org/c/core/+/124654/5/sw/qa/uitest/writer_tests6/edit_file_properties_before_saving.py#91
    
    Change-Id: Ia0c7c8b7338354ff82edb5b2c65b8422c18a85cf

--- sw/qa/uitest/writer_tests6/edit_file_properties_before_saving.py
+++ sw/qa/uitest/writer_tests6/edit_file_properties_before_saving.py
@@ -53,6 +53,9 @@ class edit_file_properties_before_saving(UITestCase):
                         xReadOnly.executeAction("CLICK", tuple())
                         self.assertEqual("true", get_state_as_dict(xReadOnly)['Selected'])
 
+            # Wait until the startcenter is displayed
+            self.ui_test.wait_until_child_is_available('StartCenter')
+
             with self.ui_test.load_file(systemPathToFileUrl(xFilePath)) as doc2:
 
                 self.change_doc_info_setting("false")
@@ -89,6 +92,9 @@ class edit_file_properties_before_saving(UITestCase):
                             xReadOnly = xPropertiesDialog.getChild("readonly")
                             xReadOnly.executeAction("CLICK", tuple())
                             self.assertEqual("true", get_state_as_dict(xReadOnly)['Selected'])
+
+            # Wait until the startcenter is displayed
+            self.ui_test.wait_until_child_is_available('StartCenter')
 
             with self.ui_test.load_file(systemPathToFileUrl(xFilePath)) as doc2:
 
