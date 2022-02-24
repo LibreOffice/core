3892040cf9e7d35d3a04afccf9eb28a30b353b4d
Hossein <hossein@libreoffice.org>
2022-02-24 16:30:07 +0100

    tdf#145759 30.6001 -> monthDaysWithoutJanFeb
    30.6001 shows month days without Jan and Feb.
    According to the below link, it is calcuated as (365-31-28)/10 = 30.6
    but because of a floating point bug, it was used as 30.6001 as a
    workaround.
    
    "30.6001, 25 year old hack?"
    https://www.hpmuseum.org/cgi-sys/cgiwrap/hpmuseum/archv011.cgi?read=31650
    
    The value 30.6 is used as i18nutil::monthDaysWithoutJanFeb here
    instead of 30.6001. The new value is ~30.60000038 which is > 30.6, so
    the calculations should be correct. In order to make sure, a unit test
    is added, and part of the values are checked against the values
    calculated by this website:
    
    Julian Day and Civil Date Calculator
    https://core2.gsfc.nasa.gov/time/julian.html
    
    Change-Id: I8cc7e046514dc3de652a1c37399e351cb2b614dc

---
+++ include/i18nutil/calendar.hxx
@@ -0,0 +1,23 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#pragma once
+
+namespace i18nutil
+{
+/** This number shows month days without Jan and Feb.
+ *  According to the article, it is calcuated as (365-31-28)/10 = 30.6, but because
+ *  of a floating point bug, it was used as 30.6001 as a workaround.
+ *
+ *  "30.6001, 25 year old hack?"
+ *  https://www.hpmuseum.org/cgi-sys/cgiwrap/hpmuseum/archv011.cgi?read=31650 */
+constexpr double monthDaysWithoutJanFeb = (365 - 31 - 28) / 10.0;
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
