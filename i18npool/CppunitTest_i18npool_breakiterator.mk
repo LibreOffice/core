c6c920df7371ac00bf8d45ba7af404cfb8fefd21
Hossein <hossein@libreoffice.org>
2022-02-25 21:17:09 +0100

    Remove redundant _test from i18npool/CppunitTest*
    As suggested in <https://gerrit.libreoffice.org/c/core/+/125813>, the
    name of the CppunitTests contain a redundant '_test'. This patch
    removes this additional word from the name of all the CppunitTests in
    i18npool.
    
    The new names for the tests in i18npool are:
    
    CppunitTest_i18npool_break_iterator
    CppunitTest_i18npool_calendar
    CppunitTest_i18npool_characterclassification
    CppunitTest_i18npool_defaultnumberingprovider (unchanged)
    CppunitTest_i18npool_ordinalsuffix
    CppunitTest_i18npool_textsearch
    
    Change-Id: Ie613c0048a7f156c7d815620f2ccb6578c069e8c

---
+++ i18npool/CppunitTest_i18npool_breakiterator.mk
@@ -0,0 +1,37 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_CppunitTest_CppunitTest,i18npool_break_iterator))
+
+$(eval $(call gb_CppunitTest_use_external,i18npool_break_iterator,icu_headers))
+
+$(eval $(call gb_CppunitTest_use_sdk_api,i18npool_break_iterator))
+
+$(eval $(call gb_CppunitTest_use_libraries,i18npool_break_iterator,\
+	cppu \
+	cppuhelper \
+	sal \
+	unotest \
+))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,i18npool_break_iterator,\
+    i18npool/qa/cppunit/test_breakiterator \
+))
+
+$(eval $(call gb_CppunitTest_use_ure,i18npool_break_iterator))
+
+$(eval $(call gb_CppunitTest_use_components,i18npool_break_iterator,\
+	i18npool/util/i18npool \
+))
+
+$(call gb_CppunitTest_get_target,i18npool_break_iterator) : \
+    $(call gb_Library_get_target,dict_ja) \
+    $(call gb_Library_get_target,dict_zh)
+
+# vim: set noet sw=4 ts=4:
--- i18npool/CppunitTest_i18npool_textsearch.mk
+++ i18npool/CppunitTest_i18npool_textsearch.mk
@@ -7,28 +7,28 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 
-$(eval $(call gb_CppunitTest_CppunitTest,i18npool_test_textsearch))
+$(eval $(call gb_CppunitTest_CppunitTest,i18npool_textsearch))
 
-$(eval $(call gb_CppunitTest_use_sdk_api,i18npool_test_textsearch))
+$(eval $(call gb_CppunitTest_use_sdk_api,i18npool_textsearch))
 
-$(eval $(call gb_CppunitTest_use_ure,i18npool_test_textsearch))
+$(eval $(call gb_CppunitTest_use_ure,i18npool_textsearch))
 
-$(eval $(call gb_CppunitTest_add_exception_objects,i18npool_test_textsearch,\
+$(eval $(call gb_CppunitTest_add_exception_objects,i18npool_textsearch,\
 	i18npool/qa/cppunit/test_textsearch \
 ))
 
-$(eval $(call gb_CppunitTest_use_components,i18npool_test_textsearch,\
+$(eval $(call gb_CppunitTest_use_components,i18npool_textsearch,\
 	i18npool/source/search/i18nsearch \
 	i18npool/util/i18npool \
 ))
 
-$(eval $(call gb_CppunitTest_use_externals,i18npool_test_textsearch,\
+$(eval $(call gb_CppunitTest_use_externals,i18npool_textsearch,\
 	icui18n \
 	icuuc \
 	icu_headers \
 ))
 
-$(eval $(call gb_CppunitTest_use_libraries,i18npool_test_textsearch,\
+$(eval $(call gb_CppunitTest_use_libraries,i18npool_textsearch,\
 	cppu \
 	cppuhelper \
 	sal \
