f509da33f2b63be957bb4897c1bb20db1d3fc2a2
Luboš Luňák <l.lunak@collabora.com>
2022-02-25 12:22:16 +0100

    make sure that LOK "updated types" start a timer as well
    273a25c796fca9afa0dfadac57dc3f336831221c changed the handling of LOK
    messages to only set up an "updated" flag for types and then
    when flushing the value is fetched. This means that queue() is not
    called for such updates, and so the timeout to flush the messages
    wasn't started.
    
    Change-Id: I3a7324c86a55697f509f80b6c54cb949fb1daa72
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/130459
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Andras Timar <andras.timar@collabora.com>

--- desktop/inc/lib/init.hxx
+++ desktop/inc/lib/init.hxx
@@ -175,6 +175,7 @@ namespace desktop {
         typedef std::vector<int> queue_type1;
         typedef std::vector<CallbackData> queue_type2;
 
+        void startTimer();
         bool removeAll(int type);
         bool removeAll(int type, const std::function<bool (const CallbackData&)>& rTestFunc);
         bool removeAll(const std::function<bool (int, const CallbackData&)>& rTestFunc);
