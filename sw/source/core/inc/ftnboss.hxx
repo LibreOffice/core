b5a8a5c11be9e897c5ffd530fe7efd89dca6eb1b
Michael Stahl <michael.stahl@allotropia.de>
2022-02-25 22:07:10 +0100

    sw: really prevent footnote frame against delete while it's moved
    This reverts commit fe5d3fbfe63fe8b433776bd3a0508dd712b868b0
    
    It turns out that checking a single column's IsMovingFootnotes() is not
    enough - as frames can move both forwards and backwards the entire chain
    of columns would need to be checked.
    
    (Most callers of MoveLowerFootnotes() move forwards, but one place in
    tabfrm.cxx moves from a follow to master.)
    
    But it turns out that this is probably the wrong way in any case: most
    likely the intention in FormatAnchorFrameAndItsPrevs() is to format
    previous frames in the same layout environment, so if there is a section
    or column inside a footnote then this upper should be formatted, while
    if the footnote is inside a section or column this upper should not be
    formatted; this should make calls during MoveFootnotes_() safe as it
    should prevent the formatting of frames in the footnote boss moving a
    footnote that is already being moved.
    
    So tweak the fix in commit fa1bcc22921941b2cd8a0b32fe0d15655d12d607
    a little to make it more general.
    
    Also it was previously possible that for a section with columns,
    both the section branch and the column branch was taken, which seems
    supperfluous.
    
    Change-Id: I39487640322339fe4d511e845d9c6bced2ba9dad

--- sw/source/core/inc/ftnboss.hxx
+++ sw/source/core/inc/ftnboss.hxx
@@ -54,7 +54,6 @@ class SAL_DLLPUBLIC_RTTI SwFootnoteBossFrame: public SwLayoutFrame
 
     // max. height of the footnote container on this page
     SwTwips m_nMaxFootnoteHeight;
-    bool m_isMovingFootnotes = false;
 
     SwFootnoteContFrame *MakeFootnoteCont();
     SwFootnoteFrame     *FindFirstFootnote();
@@ -107,12 +106,9 @@ public:
                          SwFootnoteBossFrame*     _pOld,
                          SwFootnoteFrames&        _rFootnoteArr,
                          const bool    _bCollectOnlyPreviousFootnotes = false );
-    void    MoveFootnotes_( SwFootnoteFrames &rFootnoteArr, bool bCalc = false,
-                SwFootnoteBossFrame * pOldBoss = nullptr);
+    void    MoveFootnotes_( SwFootnoteFrames &rFootnoteArr, bool bCalc = false );
     void    MoveFootnotes( const SwContentFrame *pSrc, SwContentFrame *pDest,
                       SwTextFootnote const *pAttr );
-
-    bool IsMovingFootnotes() const { return m_isMovingFootnotes; }
 
     // should AdjustNeighbourhood be called (or Grow/Shrink)?
     SwNeighbourAdjust NeighbourhoodAdjustment() const
