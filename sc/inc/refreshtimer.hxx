LibreOffice/core

commit 287680683ca266f1fb4f447ac9bdaf76669d559d

osl::Mutex->std::mutex in ScRefreshTimer
Change-Id: I64cac28e15edc90a8456e99c22e78cc84750eebd
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/119747
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sc/inc/refreshtimer.hxx
@@ -23,20 +23,20 @@
 
 #include <tools/solar.h>
 #include <vcl/timer.hxx>
-#include <osl/mutex.hxx>
 #include "scdllapi.h"
 #include <memory>
+#include <mutex>
 
 class ScRefreshTimerControl
 {
-    ::osl::Mutex    aMutex;
+    std::mutex      aMutex;
     sal_uInt16      nBlockRefresh;
 
 public:
     ScRefreshTimerControl() : nBlockRefresh(0) {}
     void SetAllowRefresh( bool b );
     bool IsRefreshAllowed() const { return !nBlockRefresh; }
-    ::osl::Mutex& GetMutex() { return aMutex; }
+    std::mutex& GetMutex() { return aMutex; }
 };
 
 class ScRefreshTimer : public AutoTimer

sc/source/core/tool/refreshtimer.cxx
@@ -36,7 +36,7 @@ ScRefreshTimerProtector::ScRefreshTimerProtector( std::unique_ptr<ScRefreshTimer
     {
         m_rpControl->SetAllowRefresh( false );
         // wait for any running refresh in another thread to finish
-        ::osl::MutexGuard aGuard( m_rpControl->GetMutex() );
+        std::lock_guard aGuard( m_rpControl->GetMutex() );
     }
 }
 
@@ -122,7 +122,7 @@ void ScRefreshTimer::Invoke()
     if ( ppControl && *ppControl && (*ppControl)->IsRefreshAllowed() )
     {
         // now we COULD make the call in another thread ...
-        ::osl::MutexGuard aGuard( (*ppControl)->GetMutex() );
+        std::lock_guard aGuard( (*ppControl)->GetMutex() );
         Timer::Invoke();
         // restart from now on, don't execute immediately again if timed out
         // a second time during refresh

commit 11dbfe0b7fb171c105a4b38486377c5d80fa0a44

Resolves: tdf#147448 ScRefreshTimerControl mutex must be std::recursive_mutex
ScRefreshTimer::Invoke() locks the mutex and subsequent nested
locks are attempted in ScDocShell::ConvertFrom() and
ScDocShellModificator ctor by ScRefreshTimerProtector.

A std::mutex must not be owned by the calling thread when a lock
is attempted, otherwise even deadlocks may occur, as was the case
here. This is exactly the difference of std::recursive_mutex.

Likely a regression from

    commit 287680683ca266f1fb4f447ac9bdaf76669d559d
    CommitDate: Mon Aug 2 12:17:07 2021 +0200

        osl::Mutex->std::mutex in ScRefreshTimer

Change-Id: Iaa0f1da2b4b9616e9627d8d0001775f554756048

sc/inc/refreshtimer.hxx
@@ -28,14 +28,14 @@
 
 class ScRefreshTimerControl
 {
-    std::mutex      aMutex;
-    sal_uInt16      nBlockRefresh;
+    std::recursive_mutex    aMutex;
+    sal_uInt16              nBlockRefresh;
 
 public:
     ScRefreshTimerControl() : nBlockRefresh(0) {}
     void SetAllowRefresh( bool b );
     bool IsRefreshAllowed() const { return !nBlockRefresh; }
-    std::mutex& GetMutex() { return aMutex; }
+    std::recursive_mutex& GetMutex() { return aMutex; }
 };
 
 class ScRefreshTimer : public AutoTimer