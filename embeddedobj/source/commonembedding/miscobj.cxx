LibreOffice/core

commit b099da78a6f0b3e120f706714003b05d84d11e70

tdf#141529 sync linked OLE save(s) with hosting file save(s)
Before deactivating a linked OLE saved it and overwrote the
original, even when not saving the hosting document at all.
This is not intuitive from user perspective and may lead
to unexpected data loss (of the OLE content).
Reported case was especially about closing the hosted document
without saving in the understandable believe that that way the
changed OLE will not be changed on external medium.
Added mechanism for linked OLE to hold data in a hidden local
temp file, synching/writing back on hosting file save. Most
complicated was adapting the 'break link' case and ensuring
other cases to work, but looks good now from my POV

Reviewed-on: https://gerrit.libreoffice.org/c/core/+/113793
Tested-by: Armin Le Grand <Armin.Le.Grand@me.com>
Reviewed-by: Armin Le Grand <Armin.Le.Grand@me.com>
(cherry picked from commit f4c66a2272d890eaa7a6d7429e5bfa7bdb1f7162)

Change-Id: I7f63d667820b2d9725abc598a9dd7360be1f8d5a
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/113764
Tested-by: Thorsten Behrens <thorsten.behrens@allotropia.de>
Reviewed-by: Thorsten Behrens <thorsten.behrens@allotropia.de>

embeddedobj/source/commonembedding/miscobj.cxx
@@ -27,6 +27,10 @@
 #include <com/sun/star/lang/NoSupportException.hpp>
 #include <com/sun/star/beans/NamedValue.hpp>
 
+#include <com/sun/star/ucb/SimpleFileAccess.hpp>
+#include <com/sun/star/io/TempFile.hpp>
+#include <comphelper/storagehelper.hxx>
+
 #include <cppuhelper/queryinterface.hxx>
 #include <cppuhelper/interfacecontainer.h>
 #include <comphelper/mimeconfighelper.hxx>
@@ -52,8 +56,10 @@ OCommonEmbeddedObject::OCommonEmbeddedObject( const uno::Reference< uno::XCompon
 , m_bEmbeddedScriptSupport( true )
 , m_bDocumentRecoverySupport( true )
 , m_bWaitSaveCompleted( false )
-, m_bIsLink( false )
+, m_bIsLinkURL( false )
+, m_bLinkTempFileChanged( false )
 , m_bLinkHasPassword( false )
+, m_aLinkTempFile( )
 , m_bHasClonedSize( false )
 , m_nClonedMapUnit( 0 )
 {
@@ -78,8 +84,10 @@ OCommonEmbeddedObject::OCommonEmbeddedObject(
 , m_bEmbeddedScriptSupport( true )
 , m_bDocumentRecoverySupport( true )
 , m_bWaitSaveCompleted( false )
-, m_bIsLink( true )
+, m_bIsLinkURL( true )
+, m_bLinkTempFileChanged( false )
 , m_bLinkHasPassword( false )
+, m_aLinkTempFile( )
 , m_bHasClonedSize( false )
 , m_nClonedMapUnit( 0 )
 {
@@ -233,7 +241,61 @@ void OCommonEmbeddedObject::LinkInit_Impl(
         m_bReadOnly = aExportFilterName != m_aLinkFilterName;
     }
 
-    m_aDocMediaDescriptor = GetValuableArgs_Impl( aMediaDescr, false );
+    if(m_bIsLinkURL && !m_bReadOnly)
+    {
+        // tdf#141529 we have a linked OLE object. To prevent the original OLE
+        // data to be changed each time the OLE gets changed (at deactivate), copy it to
+        // a temporary file. That file will be changed on activated OLE changes then.
+        // The moment the original gets changed itself will now be associated with the
+        // file/document embedding the OLE being changed (see other additions to the
+        // task-ID above)
+        //
+        // open OLE original data as read input file
+        uno::Reference< ucb::XSimpleFileAccess3 > xTempAccess( ucb::SimpleFileAccess::create( m_xContext ) );
+        uno::Reference< io::XInputStream > xInStream( xTempAccess->openFileRead( m_aLinkURL ) );
+
+        if(xInStream.is())
+        {
+            // create temporary file
+            m_aLinkTempFile = io::TempFile::create(m_xContext);
+
+            if(m_aLinkTempFile.is())
+            {
+                // completely copy content of original OLE data
+                uno::Reference < io::XOutputStream > xTempOut = m_aLinkTempFile->getOutputStream();
+                ::comphelper::OStorageHelper::CopyInputToOutput( xInStream, xTempOut );
+                xTempOut->flush();
+                xTempOut->closeOutput();
+
+                // reset flag m_bLinkTempFileChanged, so it will also work for multiple
+                // save op's of the containing file/document
+                m_bLinkTempFileChanged = false;
+            }
+        }
+    }
+
+    if(m_aLinkTempFile.is())
+    {
+        uno::Sequence< beans::PropertyValue > aAlternativeMediaDescr(aMediaDescr.getLength());
+
+        for ( sal_Int32 a(0); a < aMediaDescr.getLength(); a++ )
+        {
+            const beans::PropertyValue& rSource(aMediaDescr[a]);
+            beans::PropertyValue& rDestination(aAlternativeMediaDescr[a]);
+
+            rDestination.Name = rSource.Name;
+            if(rSource.Name == "URL")
+                rDestination.Value <<= m_aLinkTempFile->getUri();
+            else
+                rDestination.Value = rSource.Value;
+        }
+
+        m_aDocMediaDescriptor = GetValuableArgs_Impl( aAlternativeMediaDescr, false );
+    }
+    else
+    {
+        m_aDocMediaDescriptor = GetValuableArgs_Impl( aMediaDescr, false );
+    }
 
     uno::Reference< frame::XDispatchProviderInterceptor > xDispatchInterceptor;
     for ( sal_Int32 nObjInd = 0; nObjInd < aObjectDescr.getLength(); nObjInd++ )

embeddedobj/source/commonembedding/persistence.cxx
@@ -47,6 +47,9 @@
 #include <com/sun/star/beans/IllegalTypeException.hpp>
 #include <com/sun/star/chart2/XChartDocument.hpp>
 
+#include <com/sun/star/ucb/SimpleFileAccess.hpp>
+#include <com/sun/star/io/XTruncate.hpp>
+
 #include <comphelper/fileformat.h>
 #include <comphelper/storagehelper.hxx>
 #include <comphelper/mimeconfighelper.hxx>
@@ -254,7 +257,7 @@ void OCommonEmbeddedObject::SwitchOwnPersistence( const uno::Reference< embed::X
     m_aEntryName = aNewName;
 
     // the linked document should not be switched
-    if ( !m_bIsLink )
+    if ( !m_bIsLinkURL )
     {
         uno::Reference< document::XStorageBasedDocument > xDoc( m_xDocHolder->GetComponent(), uno::UNO_QUERY );
         if ( xDoc.is() )
@@ -369,10 +372,16 @@ uno::Reference< util::XCloseable > OCommonEmbeddedObject::LoadLink_Impl()
 
     sal_Int32 nLen = 2;
     uno::Sequence< beans::PropertyValue > aArgs( nLen );
+
     aArgs[0].Name = "URL";
-    aArgs[0].Value <<= m_aLinkURL;
+    if(m_aLinkTempFile.is())
+        aArgs[0].Value <<= m_aLinkTempFile->getUri();
+    else
+        aArgs[0].Value <<= m_aLinkURL;
+
     aArgs[1].Name = "FilterName";
     aArgs[1].Value <<= m_aLinkFilterName;
+
     if ( m_bLinkHasPassword )
     {
         aArgs.realloc( ++nLen );
@@ -847,7 +856,7 @@ uno::Reference< util::XCloseable > OCommonEmbeddedObject::CreateTempDocFromLink_
 {
     uno::Reference< util::XCloseable > xResult;
 
-    SAL_WARN_IF( !m_bIsLink, "embeddedobj.common", "The object is not a linked one!" );
+    SAL_WARN_IF( !m_bIsLinkURL, "embeddedobj.common", "The object is not a linked one!" );
 
     uno::Sequence< beans::PropertyValue > aTempMediaDescr;
 
@@ -899,7 +908,12 @@ uno::Reference< util::XCloseable > OCommonEmbeddedObject::CreateTempDocFromLink_
     {
         aTempMediaDescr.realloc( 2 );
         aTempMediaDescr[0].Name = "URL";
-        aTempMediaDescr[0].Value <<= m_aLinkURL;
+
+        // tdf#141529 use URL of the linked TempFile if it exists
+        aTempMediaDescr[0].Value <<= m_aLinkTempFile.is()
+            ? m_aLinkTempFile->getUri()
+            : m_aLinkURL;
+
         aTempMediaDescr[1].Name = "FilterName";
         aTempMediaDescr[1].Value <<= m_aLinkFilterName;
     }
@@ -972,8 +986,8 @@ void SAL_CALL OCommonEmbeddedObject::setPersistentEntry(
 
     // for now support of this interface is required to allow breaking of links and converting them to normal embedded
     // objects, so the persist name must be handled correctly ( althowgh no real persist entry is used )
-    // OSL_ENSURE( !m_bIsLink, "This method implementation must not be used for links!" );
-    if ( m_bIsLink )
+    // OSL_ENSURE( !m_bIsLinkURL, "This method implementation must not be used for links!" );
+    if ( m_bIsLinkURL )
     {
         m_aEntryName = sEntName;
         return;
@@ -1151,8 +1165,8 @@ void SAL_CALL OCommonEmbeddedObject::storeToEntry( const uno::Reference< embed::
 
     // for now support of this interface is required to allow breaking of links and converting them to normal embedded
     // objects, so the persist name must be handled correctly ( althowgh no real persist entry is used )
-    // OSL_ENSURE( !m_bIsLink, "This method implementation must not be used for links!" );
-    if ( m_bIsLink )
+    // OSL_ENSURE( !m_bIsLinkURL, "This method implementation must not be used for links!" );
+    if ( m_bIsLinkURL )
         return;
 
     OSL_ENSURE( m_xParentStorage.is() && m_xObjectStorage.is(), "The object has no valid persistence!" );
@@ -1279,10 +1293,33 @@ void SAL_CALL OCommonEmbeddedObject::storeAsEntry( const uno::Reference< embed::
 
     // for now support of this interface is required to allow breaking of links and converting them to normal embedded
     // objects, so the persist name must be handled correctly ( althowgh no real persist entry is used )
-    // OSL_ENSURE( !m_bIsLink, "This method implementation must not be used for links!" );
-    if ( m_bIsLink )
+    // OSL_ENSURE( !m_bIsLinkURL, "This method implementation must not be used for links!" );
+    if ( m_bIsLinkURL )
     {
         m_aNewEntryName = sEntName;
+
+        if(m_aLinkTempFile.is() && m_bLinkTempFileChanged)
+        {
+            // tdf#141529 if we have a changed copy of the original OLE data we now
+            // need to write it back 'over' the original OLE data
+            uno::Reference < ucb::XSimpleFileAccess2 > xFileAccess(ucb::SimpleFileAccess::create( m_xContext ));
+            uno::Reference < io::XInputStream > xTempIn = m_aLinkTempFile->getInputStream();
+
+            // This is *needed* since OTempFileService calls OTempFileService::readBytes which
+            // ensures the SvStream mpStream gets/is opened, *but* also sets the mnCachedPos from
+            // OTempFileService which still points to the end-of-file (from write-cc'ing).
+            uno::Reference < io::XSeekable > xSeek( xTempIn, uno::UNO_QUERY_THROW );
+            xSeek->seek(0);
+
+            xFileAccess->writeFile(m_aLinkURL, xTempIn);
+
+            // Do *not* close input, that would remove the temporary file too early
+            // xTempIn->closeInput();
+
+            // reset flag m_bLinkTempFileChanged
+            m_bLinkTempFileChanged = false;
+        }
+
         return;
     }
 
@@ -1411,8 +1448,8 @@ void SAL_CALL OCommonEmbeddedObject::saveCompleted( sal_Bool bUseNew )
 
     // for now support of this interface is required to allow breaking of links and converting them to normal embedded
     // objects, so the persist name must be handled correctly ( althowgh no real persist entry is used )
-    // OSL_ENSURE( !m_bIsLink, "This method implementation must not be used for links!" );
-    if ( m_bIsLink )
+    // OSL_ENSURE( !m_bIsLinkURL, "This method implementation must not be used for links!" );
+    if ( m_bIsLinkURL )
     {
         if ( bUseNew )
             m_aEntryName = m_aNewEntryName;
@@ -1546,7 +1583,7 @@ void SAL_CALL OCommonEmbeddedObject::storeOwn()
     if ( !m_xDocHolder->GetComponent().is() )
         throw uno::RuntimeException();
 
-    if ( m_bIsLink )
+    if ( m_bIsLinkURL )
     {
         // TODO: just store the document to its location
         uno::Reference< frame::XStorable > xStorable( m_xDocHolder->GetComponent(), uno::UNO_QUERY_THROW );
@@ -1654,7 +1691,7 @@ void SAL_CALL OCommonEmbeddedObject::reload(
                     "The object waits for saveCompleted() call!",
                     static_cast< ::cppu::OWeakObject* >(this) );
 
-    if ( m_bIsLink )
+    if ( m_bIsLinkURL )
     {
         // reload of the link
         OUString aOldLinkFilter = m_aLinkFilterName;
@@ -1723,7 +1760,7 @@ void SAL_CALL OCommonEmbeddedObject::reload(
         if ( lArguments[nInd].Name == "ReadOnly" )
             lArguments[nInd].Value >>= m_bReadOnly;
 
-    if ( bOldReadOnlyValue != m_bReadOnly && !m_bIsLink )
+    if ( bOldReadOnlyValue != m_bReadOnly && !m_bIsLinkURL )
     {
         // close own storage
         try {
@@ -1755,7 +1792,7 @@ void SAL_CALL OCommonEmbeddedObject::breakLink( const uno::Reference< embed::XSt
     if ( m_bDisposed )
         throw lang::DisposedException(); // TODO
 
-    if (!m_bIsLink || m_nObjectState == -1)
+    if (!m_bIsLinkURL || m_nObjectState == -1)
     {
         // it must be a linked initialized object
         throw embed::WrongStateException(
@@ -1824,7 +1861,10 @@ void SAL_CALL OCommonEmbeddedObject::breakLink( const uno::Reference< embed::XSt
     else if ( m_nObjectState == embed::EmbedStates::ACTIVE )
         m_xDocHolder->Show();
 
-    m_bIsLink = false;
+    // tdf#141529 reset all stuff involved in linked state, including
+    // the OLE content copied to the temp file
+    m_bIsLinkURL = false;
+    m_aLinkTempFile.clear();
     m_aLinkFilterName.clear();
     m_aLinkURL.clear();
 }
@@ -1836,7 +1876,7 @@ sal_Bool SAL_CALL  OCommonEmbeddedObject::isLink()
     if ( m_bDisposed )
         throw lang::DisposedException(); // TODO
 
-    return m_bIsLink;
+    return m_bIsLinkURL;
 }
 
 
@@ -1846,7 +1886,7 @@ OUString SAL_CALL OCommonEmbeddedObject::getLinkURL()
     if ( m_bDisposed )
         throw lang::DisposedException(); // TODO
 
-    if ( !m_bIsLink )
+    if ( !m_bIsLinkURL )
         throw embed::WrongStateException(
                     "The object is not a link object!",
                     static_cast< ::cppu::OWeakObject* >(this) );

embeddedobj/source/inc/commonembobj.hxx
@@ -33,6 +33,7 @@
 #include <com/sun/star/awt/Rectangle.hpp>
 #include <com/sun/star/util/XCloseable.hpp>
 #include <com/sun/star/chart2/XDefaultSizeTransmitter.hpp>
+#include <com/sun/star/io/XTempFile.hpp>
 #include <cppuhelper/weak.hxx>
 #include <rtl/ref.hxx>
 
@@ -132,7 +133,8 @@ protected:
     css::awt::Rectangle m_aOwnRectangle;
     css::awt::Rectangle m_aClipRectangle;
 
-    bool m_bIsLink;
+    bool m_bIsLinkURL;
+    bool m_bLinkTempFileChanged;
 
     // embedded object related stuff
     OUString m_aEntryName;
@@ -145,6 +147,9 @@ protected:
     OUString m_aLinkFilterName;
     bool        m_bLinkHasPassword;
     OUString m_aLinkPassword;
+
+    // tdf#141529 hold a cc of a linked OLE
+    css::uno::Reference < css::io::XTempFile > m_aLinkTempFile;
 
     css::uno::Reference< css::uno::XInterface > m_xParent;
 

embeddedobj/source/commonembedding/embedobj.cxx
@@ -100,6 +100,12 @@ void OCommonEmbeddedObject::Deactivate()
     {
         try {
             xClientSite->saveObject();
+
+            // tdf#141529 take note that an eventually used linked file
+            // got changed/saved/written and that we need to copy it back if the
+            // hosting file/document gets saved
+            if(m_aLinkTempFile.is())
+                m_bLinkTempFileChanged = true;
         }
         catch( const embed::ObjectSaveVetoException& )
         {
@@ -168,7 +174,7 @@ void OCommonEmbeddedObject::SwitchStateTo_Impl( sal_Int32 nNextState )
             // after the object reaches the running state the cloned size is not necessary any more
             m_bHasClonedSize = false;
 
-            if ( m_bIsLink )
+            if ( m_bIsLinkURL )
             {
                 m_xDocHolder->SetComponent( LoadLink_Impl(), m_bReadOnly );
             }

commit 94cc0586529220620be879706a76bb69d9013a47

tdf#147590 OLE object update after refresh
Regression from b099da78a6f0b3e120f706714003b05d84d11e70
implement the refresh and overwrite case

Change-Id: I8e52f6430f454b276cb43449c6f7a3b0e07e909f

embeddedobj/source/commonembedding/miscobj.cxx
@@ -35,10 +35,16 @@
 #include <cppuhelper/queryinterface.hxx>
 #include <comphelper/mimeconfighelper.hxx>
 
+#include <vcl/weld.hxx>
+#include <unotools/resmgr.hxx>
+#include <vcl/stdtext.hxx>
+#include "../inc/strings.hrc"
+
 #include <vcl/svapp.hxx>
 #include <tools/diagnose_ex.h>
 #include <cppuhelper/supportsservice.hxx>
 #include <comphelper/sequenceashashmap.hxx>
+#include <osl/file.hxx>
 
 #include "persistence.hxx"
 
@@ -63,6 +69,9 @@ OCommonEmbeddedObject::OCommonEmbeddedObject( const uno::Reference< uno::XCompon
 , m_bIsLinkURL( false )
 , m_bLinkTempFileChanged( false )
 , m_bLinkHasPassword( false )
+, m_aLinkTempFile( )
+, m_pLinkFile( nullptr )
+, m_pTempFile( nullptr )
 , m_bHasClonedSize( false )
 , m_nClonedMapUnit( 0 )
 {
@@ -89,6 +98,9 @@ OCommonEmbeddedObject::OCommonEmbeddedObject(
 , m_bIsLinkURL( true )
 , m_bLinkTempFileChanged( false )
 , m_bLinkHasPassword( false )
+, m_aLinkTempFile( )
+, m_pLinkFile(nullptr)
+, m_pTempFile(nullptr)
 , m_bHasClonedSize( false )
 , m_nClonedMapUnit( 0 )
 {
@@ -224,27 +236,30 @@ void OCommonEmbeddedObject::LinkInit_Impl(
         // task-ID above)
         //
         // open OLE original data as read input file
-        uno::Reference< ucb::XSimpleFileAccess3 > xTempAccess( ucb::SimpleFileAccess::create( m_xContext ) );
-        uno::Reference< io::XInputStream > xInStream( xTempAccess->openFileRead( m_aLinkURL ) );
+        uno::Reference< ucb::XSimpleFileAccess > xTempInAccess( ucb::SimpleFileAccess::create( m_xContext ) );
+        uno::Reference< io::XInputStream > xInStream( xTempInAccess->openFileRead( m_aLinkURL ) );
 
         if(xInStream.is())
         {
             // create temporary file
             m_aLinkTempFile = io::TempFile::create(m_xContext);
-            uno::Reference< ucb::XSimpleFileAccess > xTempOutAccess(ucb::SimpleFileAccess::create(m_xContext));
-            // if the temp stream is used, then the temp file remains locked
-            uno::Reference< io::XOutputStream > xOutStream(xTempOutAccess->openFileWrite(m_aLinkTempFile->getUri()));
+            uno::Reference< ucb::XSimpleFileAccess > xTempOutAccess( ucb::SimpleFileAccess::create( m_xContext ) );
+            uno::Reference< io::XOutputStream > xOutStream( xTempOutAccess->openFileWrite( m_aLinkTempFile->getUri() ) );
 
             if(m_aLinkTempFile.is())
             {
-                // completely copy content of original OLE data
-                ::comphelper::OStorageHelper::CopyInputToOutput(xInStream, xOutStream);
-
+                ::comphelper::OStorageHelper::CopyInputToOutput( xInStream, xOutStream );
+                xOutStream->closeOutput();
                 // reset flag m_bLinkTempFileChanged, so it will also work for multiple
                 // save op's of the containing file/document
                 m_bLinkTempFileChanged = false;
+
+                // store the timestamp of both files
+                m_pLinkFile.reset(new FileChangedChecker(m_aLinkURL));
+                m_pTempFile.reset(new FileChangedChecker(m_aLinkTempFile->getUri()));
             }
         }
+
     }
 
     if(m_aLinkTempFile.is())
@@ -376,6 +391,103 @@ void OCommonEmbeddedObject::PostEvent_Impl( const OUString& aEventName )
         if ( m_bDisposed )
             return;
     }
+}
+
+
+static int ShowMsgDialog(TranslateId Msg, const OUString& sFileName)
+{
+    std::locale aResLocale = Translate::Create("emo");
+    OUString aMsg    = Translate::get(Msg, aResLocale);
+    OUString aBtn    = Translate::get(BTN_OVERWRITE_TEXT, aResLocale);
+    OUString aTemp   = sFileName;
+
+    osl::FileBase::getSystemPathFromFileURL(sFileName, aTemp);
+
+    aMsg = aMsg.replaceFirst("%{filename}", aTemp);
+    weld::Window* pParent = Application::GetFrameWeld(nullptr);
+
+    std::unique_ptr<weld::MessageDialog> xQueryBox(Application::CreateMessageDialog(pParent,
+        VclMessageType::Warning, VclButtonsType::NONE, aMsg));
+    xQueryBox->add_button(aBtn, RET_YES);
+    xQueryBox->add_button(GetStandardText(StandardButtonType::Cancel), RET_CANCEL);
+    xQueryBox->set_default_response(RET_CANCEL);
+
+    return xQueryBox->run();
+}
+
+
+void OCommonEmbeddedObject::HandleLinkAndTempFileSave(eSaveTmpLnkState eState)
+{
+    static bool InHndFunc = false;
+
+    // do not Refresh and autosave at the same time
+    if (InHndFunc)
+        return;
+
+    InHndFunc = true;
+
+    bool bLnkFileChg = m_pLinkFile->hasFileChanged(false);
+    bool bTmpFileChg = m_pTempFile->hasFileChanged(false);
+
+
+    if (!bLnkFileChg && !bTmpFileChg)
+    {// no changes
+        eState = eSaveTmpLnkState::NoCopy;
+    }
+    else if ((eState == eSaveTmpLnkState::CopyTempToLink) && bLnkFileChg)
+    {// Save pressed,  but the Link-file are changed, question to user for overwrite
+        // it is not importent it has bTmpFileChg, always overwite the link-file
+        if (ShowMsgDialog(STR_OVERWRITE_LINK, m_aLinkURL) == RET_CANCEL)
+            eState = eSaveTmpLnkState::NoCopy;
+    }
+    else if ((eState == eSaveTmpLnkState::CopyLinkToTemp) && bTmpFileChg)
+    {// Refresh pressed,  but the Temp-file are changed, question to user for overwrite
+        // it is not importent it has bLnkFileChg, always overwite the temp-file
+        if (ShowMsgDialog(STR_OVERWRITE_TEMP, m_aLinkURL) == RET_CANCEL)
+            eState = eSaveTmpLnkState::NoCopy;
+    }
+    // no conflict's we can copy
+
+    // lambda function var for write
+    auto writeFile = [this](const OUString& SrcName, const OUString& DesName )
+    {
+        uno::Reference < ucb::XSimpleFileAccess2 > xWriteAccess(ucb::SimpleFileAccess::create(m_xContext));
+        uno::Reference < ucb::XSimpleFileAccess > xReadAccess(ucb::SimpleFileAccess::create(m_xContext));
+        uno::Reference < io::XInputStream > xInStream(xReadAccess->openFileRead(SrcName));
+
+        // This is *needed* since OTempFileService calls OTempFileService::readBytes which
+        // ensures the SvStream mpStream gets/is opened, *but* also sets the mnCachedPos from
+        // OTempFileService which still points to the end-of-file (from write-cc'ing).
+        uno::Reference < io::XSeekable > xSeek(xInStream, uno::UNO_QUERY_THROW);
+        xSeek->seek(0);
+
+        if (xInStream.is())
+        {
+            xWriteAccess->writeFile(DesName, xInStream);
+            // reset flag m_bLinkTempFileChanged
+            m_bLinkTempFileChanged = false;
+        }
+    };
+
+    switch (eState)
+    {
+        case eSaveTmpLnkState::NoCopy:
+            break;
+        case eSaveTmpLnkState::CopyLinkToTemp: // copy Link-File to Temp-File   (Refresh)
+            writeFile( m_aLinkURL, m_aLinkTempFile->getUri());
+            m_pTempFile->hasFileChanged();
+            break;
+        case eSaveTmpLnkState::CopyTempToLink: // copy Temp-File to Link-File   (Save)
+            // tdf#141529 if we have a changed copy of the original OLE data we now
+            // need to write it back 'over' the original OLE data
+            writeFile(m_aLinkTempFile->getUri(), m_aLinkURL);
+            m_pLinkFile->hasFileChanged();
+            break;
+        default:
+            break;
+    }
+
+    InHndFunc = false;
 }
 
 

embeddedobj/source/commonembedding/persistence.cxx
@@ -392,6 +392,9 @@ uno::Reference< util::XCloseable > OCommonEmbeddedObject::LoadLink_Impl()
 
     try
     {
+        // Save handling for the Refresh-Button
+        HandleLinkAndTempFileSave(eSaveTmpLnkState::CopyLinkToTemp);
+
         // the document is not really an embedded one, it is a link
         EmbedAndReparentDoc_Impl( xDocument );
 
@@ -1281,23 +1284,8 @@ void SAL_CALL OCommonEmbeddedObject::storeAsEntry( const uno::Reference< embed::
 
         if(m_aLinkTempFile.is() && m_bLinkTempFileChanged)
         {
-            // tdf#141529 if we have a changed copy of the original OLE data we now
-            // need to write it back 'over' the original OLE data
-            uno::Reference < ucb::XSimpleFileAccess2 > xFileAccess(ucb::SimpleFileAccess::create( m_xContext ));
-
-            uno::Reference< ucb::XSimpleFileAccess > xTempAccess(ucb::SimpleFileAccess::create(m_xContext));
-            // if the temp stream is used, then the temp file remains locked
-            uno::Reference< io::XInputStream > xInStream(xTempAccess->openFileRead(m_aLinkTempFile->getUri()));
-            // This is *needed* since OTempFileService calls OTempFileService::readBytes which
-            // ensures the SvStream mpStream gets/is opened, *but* also sets the mnCachedPos from
-            // OTempFileService which still points to the end-of-file (from write-cc'ing).
-            uno::Reference < io::XSeekable > xSeek( xInStream, uno::UNO_QUERY_THROW );
-            xSeek->seek(0);
-
-            xFileAccess->writeFile(m_aLinkURL, xInStream);
-
-            // reset flag m_bLinkTempFileChanged
-            m_bLinkTempFileChanged = false;
+            // Save handling for the Save-Button
+            HandleLinkAndTempFileSave(eSaveTmpLnkState::CopyTempToLink);
         }
 
         return;

embeddedobj/source/inc/commonembobj.hxx
@@ -39,6 +39,7 @@
 #include <rtl/ref.hxx>
 #include <map>
 #include <memory>
+#include <svtools/filechangedchecker.hxx>
 
 namespace com::sun::star {
     namespace embed {
@@ -141,6 +142,8 @@ protected:
 
     bool m_bIsLinkURL;
     bool m_bLinkTempFileChanged;
+    ::std::unique_ptr<FileChangedChecker> m_pLinkFile;
+    ::std::unique_ptr<FileChangedChecker> m_pTempFile;
 
     // embedded object related stuff
     OUString m_aEntryName;
@@ -192,6 +195,14 @@ private:
     sal_Int32 ConvertVerbToState_Impl( sal_Int32 nVerb );
 
     void Deactivate();
+
+    // when State = CopyTempToLink -> the user pressed the save button
+    //                                when change in embedded part then copy to the linked-file
+    //              CopyLinkToTemp -> the user pressed the refresh button
+    //                                when change in linked-file then copy to the embedded part (temp-file)
+    enum class eSaveTmpLnkState {NoCopy, CopyTempToLink, CopyLinkToTemp};
+
+    void HandleLinkAndTempFileSave(eSaveTmpLnkState eState);
 
     void StateChangeNotification_Impl( bool bBeforeChange, sal_Int32 nOldState, sal_Int32 nNewState,::osl::ResettableMutexGuard& _rGuard );
 

embeddedobj/source/inc/strings.hrc
@@ -1,0 +1,29 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#pragma once
+
+#define NC_(Context, String) TranslateId(Context, reinterpret_cast<char const *>(u8##String))
+
+#define BTN_OVERWRITE_TEXT           NC_("BTN_OVERWRITE_TEXT", "Overwrite")
+#define STR_OVERWRITE_LINK           NC_("STR_OVERWRITE_LINK", "You have made changes to the %{filename}, saving will overwrite the data from the inserted object.\n\nDo you still want to overwrite this data?")
+#define STR_OVERWRITE_TEMP           NC_("STR_OVERWRITE_TEMP", "You have changed the data in the inserted object which will be overwritten by updating the %{filename}.\n\nDo you still want to overwrite this data?")
+
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

include/svtools/filechangedchecker.hxx
@@ -35,7 +35,9 @@ private:
 
 public:
     void resetTimer();
-    bool hasFileChanged();
+    bool hasFileChanged(bool bUpdate = true);
     FileChangedChecker(const OUString& rFilename,
             const ::std::function<void ()>& rCallback);
+    // with out Timer function
+    FileChangedChecker(const OUString& rFilename);
 };

svtools/source/misc/filechangedchecker.cxx
@@ -21,7 +21,7 @@ FileChangedChecker::FileChangedChecker(const OUString& rFilename,
     , mLastModTime()
     , mpCallback(rCallback)
 {
-    // Get the curren last file modified Status
+    // Get the current last file modified Status
     getCurrentModTime(mLastModTime);
 
     // associate the callback function for the Timer
@@ -34,14 +34,27 @@ FileChangedChecker::FileChangedChecker(const OUString& rFilename,
     resetTimer();
 }
 
+FileChangedChecker::FileChangedChecker(const OUString& rFilename)
+    : mTimer("")
+    , mFileName(rFilename)
+    , mLastModTime()
+    , mpCallback(nullptr)
+{
+    // Get the current last file modified Status
+    getCurrentModTime(mLastModTime);
+}
+
 void FileChangedChecker::resetTimer()
 {
-    // Start the Idle if it's not active
-    if(!mTimer.IsActive())
-        mTimer.Start();
+    if (mpCallback != nullptr)
+    {
+        // Start the Idle if it's not active
+        if (!mTimer.IsActive())
+            mTimer.Start();
 
-    // Set lowest Priority
-    mTimer.SetPriority(TaskPriority::LOWEST);
+        // Set lowest Priority
+        mTimer.SetPriority(TaskPriority::LOWEST);
+    }
 }
 
 bool FileChangedChecker::getCurrentModTime(TimeValue& o_rValue) const
@@ -61,7 +74,7 @@ bool FileChangedChecker::getCurrentModTime(TimeValue& o_rValue) const
     return true;
 }
 
-bool FileChangedChecker::hasFileChanged()
+bool FileChangedChecker::hasFileChanged(bool bUpdate)
 {
     // Get the current file Status
     TimeValue newTime={0,0};
@@ -75,7 +88,8 @@ bool FileChangedChecker::hasFileChanged()
     {
         // Since the file has changed, set the new status as the file status and
         // return True
-        mLastModTime = newTime ;
+        if(bUpdate)
+            mLastModTime = newTime ;
 
         return true;
     }