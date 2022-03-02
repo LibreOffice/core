44055986d20e601b3edf74c9c2ff9af3cb9ebce8
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2022-03-02 17:44:08 +0900

    sc: refactor sparkline struture to store a list of sparklines
    We need to access a list of sparklines and sparkline groups for
    a sheet. To preven going through all the columns of a sheet, we
    need to store all the created sparklines in a list. For this it
    is necessary to change the model structrue a bit. A cell now has
    a container that stores a shared_ptr to the sparkline instead of
    storing the sparkline directly. With this we can store a list
    of weak_ptr to the sparklines in a list (vector), which can be
    accessed at any time and is quite fast.
    
    This is needed by the OOXML export.
    
    Change-Id: Iaca0a41e20912775f072ea6e8cab9c44367d6f30

--- sc/inc/document.hxx
+++ sc/inc/document.hxx
@@ -103,6 +103,7 @@ class ColumnIterator;
 class ExternalDataMapper;
 class Sparkline;
 class SparklineGroup;
+class SparklineList;
 
 }
 
@@ -1254,7 +1255,8 @@ public:
 
     /** Spaklines */
     SC_DLLPUBLIC sc::Sparkline* GetSparkline(ScAddress const & rPosition);
-    SC_DLLPUBLIC sc::Sparkline* CreateSparkline(ScAddress const & rPosition, const std::shared_ptr<sc::SparklineGroup> & pSparklineGroup);
+    SC_DLLPUBLIC sc::Sparkline* CreateSparkline(ScAddress const & rPosition, std::shared_ptr<sc::SparklineGroup> const& pSparklineGroup);
+    SC_DLLPUBLIC sc::SparklineList* GetSparklineList(SCTAB nTab);
 
     /** Notes **/
     SC_DLLPUBLIC ScPostIt*       GetNote(const ScAddress& rPos);
