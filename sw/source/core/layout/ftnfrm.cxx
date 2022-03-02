e16281915ef61b657d60d9200abbe365dfc8acad
Caolán McNamara <caolanm@redhat.com>
2022-03-02 20:16:37 +0000

    add a SwFrameDeleteGuard to SwSaveFootnoteHeight
    Change-Id: Ic4f0bb7c26cc021119b09682075a05d5207b48a7
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/130897
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>
    (cherry picked from commit ac605821ecf3ea263d4812ebec352314a2dbff42)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/130855
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

--- sw/source/core/layout/ftnfrm.cxx
+++ sw/source/core/layout/ftnfrm.cxx
@@ -2863,6 +2863,7 @@ bool SwContentFrame::MoveFootnoteCntFwd( bool bMakePage, SwFootnoteBossFrame *pO
 }
 
 SwSaveFootnoteHeight::SwSaveFootnoteHeight( SwFootnoteBossFrame *pBs, const SwTwips nDeadLine ) :
+    aGuard(pBs),
     pBoss( pBs ),
     nOldHeight( pBs->GetMaxFootnoteHeight() )
 {
