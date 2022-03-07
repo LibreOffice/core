72d8931a0e1a2cc0a5c7846fac342c1980aac5a0
Julien Nabet <serval2412@yahoo.fr>
2022-03-07 21:44:07 +0100

    tdf#114150: Remove duplication of strings found in file dialogs
    Change-Id: I86f720a23dd9d400d39d9606e073b483850a9df2

--- vcl/unx/gtk3/fpicker/resourceprovider.cxx
+++ vcl/unx/gtk3/fpicker/resourceprovider.cxx
@@ -20,8 +20,8 @@
 #include <com/sun/star/ui/dialogs/CommonFilePickerElementIds.hpp>
 #include <com/sun/star/ui/dialogs/ExtendedFilePickerElementIds.hpp>
 
-#include <strings.hrc>
-#include <svdata.hxx>
+#include <fpicker/strings.hrc>
+#include <fpicker/fpsofficeResMgr.hxx>
 #include "SalGtkPicker.hxx"
 
 using namespace ::com::sun::star::ui::dialogs::ExtendedFilePickerElementIds;
@@ -34,27 +34,27 @@ const struct
     sal_Int32 ctrlId;
     TranslateId resId;
 } CtrlIdToResIdTable[] = {
-    { CHECKBOX_AUTOEXTENSION,                   STR_FPICKER_AUTO_EXTENSION },
-    { CHECKBOX_PASSWORD,                        STR_FPICKER_PASSWORD },
-    { CHECKBOX_GPGENCRYPTION,                   STR_FPICKER_GPGENCRYPT },
-    { CHECKBOX_FILTEROPTIONS,                   STR_FPICKER_FILTER_OPTIONS },
-    { CHECKBOX_READONLY,                        STR_FPICKER_READONLY },
-    { CHECKBOX_LINK,                            STR_FPICKER_INSERT_AS_LINK },
-    { CHECKBOX_PREVIEW,                         STR_FPICKER_SHOW_PREVIEW },
-    { PUSHBUTTON_PLAY,                          STR_FPICKER_PLAY },
-    { LISTBOX_VERSION_LABEL,                    STR_FPICKER_VERSION },
-    { LISTBOX_TEMPLATE_LABEL,                   STR_FPICKER_TEMPLATES },
-    { LISTBOX_IMAGE_TEMPLATE_LABEL,             STR_FPICKER_IMAGE_TEMPLATE },
-    { LISTBOX_IMAGE_ANCHOR_LABEL,               STR_FPICKER_IMAGE_ANCHOR },
-    { CHECKBOX_SELECTION,                       STR_FPICKER_SELECTION },
-    { FOLDERPICKER_TITLE,                       STR_FPICKER_FOLDER_DEFAULT_TITLE },
-    { FOLDER_PICKER_DEF_DESCRIPTION,            STR_FPICKER_FOLDER_DEFAULT_DESCRIPTION },
-    { FILE_PICKER_OVERWRITE_PRIMARY,            STR_FPICKER_ALREADYEXISTOVERWRITE_PRIMARY },
-    { FILE_PICKER_OVERWRITE_SECONDARY,          STR_FPICKER_ALREADYEXISTOVERWRITE_SECONDARY },
-    { FILE_PICKER_ALLFORMATS,                   STR_FPICKER_ALLFORMATS },
-    { FILE_PICKER_TITLE_OPEN,                   STR_FPICKER_OPEN },
-    { FILE_PICKER_TITLE_SAVE,                   STR_FPICKER_SAVE },
-    { FILE_PICKER_FILE_TYPE,                    STR_FPICKER_TYPE }
+    { CHECKBOX_AUTOEXTENSION,                   STR_SVT_FILEPICKER_AUTO_EXTENSION },
+    { CHECKBOX_PASSWORD,                        STR_SVT_FILEPICKER_PASSWORD },
+    { CHECKBOX_GPGENCRYPTION,                   STR_SVT_FILEPICKER_GPGENCRYPT },
+    { CHECKBOX_FILTEROPTIONS,                   STR_SVT_FILEPICKER_FILTER_OPTIONS },
+    { CHECKBOX_READONLY,                        STR_SVT_FILEPICKER_READONLY },
+    { CHECKBOX_LINK,                            STR_SVT_FILEPICKER_INSERT_AS_LINK },
+    { CHECKBOX_PREVIEW,                         STR_SVT_FILEPICKER_SHOW_PREVIEW },
+    { PUSHBUTTON_PLAY,                          STR_SVT_FILEPICKER_PLAY },
+    { LISTBOX_VERSION_LABEL,                    STR_SVT_FILEPICKER_VERSION },
+    { LISTBOX_TEMPLATE_LABEL,                   STR_SVT_FILEPICKER_TEMPLATES },
+    { LISTBOX_IMAGE_TEMPLATE_LABEL,             STR_SVT_FILEPICKER_IMAGE_TEMPLATE },
+    { LISTBOX_IMAGE_ANCHOR_LABEL,               STR_SVT_FILEPICKER_IMAGE_ANCHOR },
+    { CHECKBOX_SELECTION,                       STR_SVT_FILEPICKER_SELECTION },
+    { FOLDERPICKER_TITLE,                       STR_SVT_FOLDERPICKER_DEFAULT_TITLE },
+    { FOLDER_PICKER_DEF_DESCRIPTION,            STR_SVT_FOLDERPICKER_DEFAULT_DESCRIPTION },
+    { FILE_PICKER_OVERWRITE_PRIMARY,            STR_SVT_ALREADYEXISTOVERWRITE },
+    { FILE_PICKER_OVERWRITE_SECONDARY,          STR_SVT_ALREADYEXISTOVERWRITE_SECONDARY },
+    { FILE_PICKER_ALLFORMATS,                   STR_SVT_ALLFORMATS },
+    { FILE_PICKER_TITLE_OPEN,                   STR_FILEDLG_OPEN },
+    { FILE_PICKER_TITLE_SAVE,                   STR_FILEDLG_SAVE },
+    { FILE_PICKER_FILE_TYPE,                    STR_FILEDLG_TYPE }
 };
 
 static TranslateId CtrlIdToResId( sal_Int32 aControlId )
@@ -73,7 +73,7 @@ OUString SalGtkPicker::getResString( sal_Int32 aId )
     // translate the control id to a resource id
     TranslateId pResId = CtrlIdToResId( aId );
     if (pResId)
-        aResString = VclResId(pResId);
+        aResString = FpsResId(pResId);
     return aResString.replace('~', '_');
 }
 
