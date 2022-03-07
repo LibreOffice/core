0d6326f77e5ddce47cdc490295f4a79a86530c60
Andreas Heinisch <andreas.heinisch@yahoo.de>
2022-03-07 22:30:13 +0100

    tdf#80043 - Put selected text into hyperlink item
    If the user selects an entire cell by a single click, and inserts an URL
    via the hyperlink dialog, the entire content of the corresponding cell
    will be replaced instead of inserting a duplicate, or appending the URL.
    If the cell contains more than one URL or an URL including some
    additional text within the cell, the hyperlink dialog will only
    preselect the text field when opening. If this is not the case, it fill
    L@s`[p\r/1@QoI{,O5f1the URL field as well.
    
    Change-Id: I9a10c3f302a6cf91a4d9dfadfbe775075eb00fad

--- sc/source/ui/view/tabvwshe.cxx
+++ sc/source/ui/view/tabvwshe.cxx
@@ -212,13 +212,10 @@ void ScTabViewShell::InsertURLField( const OUString& rName, const OUString& rURL
     EditView*       pTableView  = pHdl->GetTableView();
     OSL_ENSURE( pTopView || pTableView, "No EditView" );
 
-    // Check if user selected a whole cell by single click,
-    // cell has content, and user didn't change the name/text
-    // of the link something different than the content via the hyperlink dialog.
-    // If true, assign the given hyperlink to the whole content
-    // instead of inserting a duplicate, or appending the url.
-    if (comphelper::LibreOfficeKit::isActive() && !bIsEditMode && !bSelectFirst
-            && pTableView && !sSeltext.isEmpty() && sSeltext == rName)
+    // Check if user selected a whole cell by single click, and cell has content.
+    // tdf#80043 - if true, replace the entire content of the selected cell instead of
+    // inserting a duplicate, or appending the url.
+    if (!bIsEditMode && !bSelectFirst && pTableView && !sSeltext.isEmpty())
     {
         nSelInd = sSeltext.getLength();
         bSelectFirst = true;
