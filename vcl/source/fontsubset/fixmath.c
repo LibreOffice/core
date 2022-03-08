434db0bd28ec13ba4f6d9e464d6eb24e1ce44d5c
Caolán McNamara <caolanm@redhat.com>
2022-03-08 21:26:11 +0000

    ofz#45373 and ofz#45362 fix ubsan issues
    replace the home grown 16.16 fixed math with an alternative
    
    Change-Id: If6fa4205aed62ff15157a8ab4dbf9b7c96e30216

---
+++ vcl/source/fontsubset/fixmath.c
@@ -0,0 +1,10 @@
+#if defined _MSC_VER && !defined __clang__
+#pragma warning(push)
+#pragma warning(disable : 4146)
+#endif
+
+#include <fix16.c>
+
+#if defined _MSC_VER && !defined __clang__
+#pragma warning(pop)
+#endif
