LibreOffice/core

commit 33c9bc0225a92f26770f9ef20b252af47853e7b9

PDF export of PDF images: preserve hyperlinks
The input file has a single page, with a full-page PDF image, which
contains a hyperlink. Similar to pdfcrop, we used to wrap this into a
form XObject, so page-level annotations like hyperlinks were lost.

Explicitly merge page-level annotations from the source page to the page
that contains the PDF image to preserve those annotations.

(cherry picked from commit 1984a5c140cc3c7c291047dacf3bddd7061d2047)

Conflicts:
	vcl/qa/cppunit/pdfexport/pdfexport.cxx

Reviewed-on: https://gerrit.libreoffice.org/c/core/+/128127
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>
(cherry picked from commit 58e6fce28f7d3e1c1504f048be5766dc732af4e7)

Change-Id: I96e8bc9d33440b91f3514486d6a8bd75047546b2
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/128253
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

vcl/source/gdi/pdfwriter_impl.cxx
@@ -8738,6 +8738,41 @@ void PDFWriterImpl::writeReferenceXObject(ReferenceXObjectEmit& rEmit)
             return;
         }
 
+        // Merge page annotations (links, etc) from pPage to our page.
+        std::vector<filter::PDFObjectElement*> aAnnots;
+        if (auto pArray = dynamic_cast<filter::PDFArrayElement*>(pPage->Lookup("Annots")))
+        {
+            for (const auto pElement : pArray->GetElements())
+            {
+                auto pReference = dynamic_cast<filter::PDFReferenceElement*>(pElement);
+                if (!pReference)
+                {
+                    continue;
+                }
+
+                filter::PDFObjectElement* pObject = pReference->LookupObject();
+                if (!pObject)
+                {
+                    continue;
+                }
+
+                // Annotation refers to an object, remember it.
+                aAnnots.push_back(pObject);
+            }
+        }
+        if (!aAnnots.empty())
+        {
+            PDFObjectCopier aCopier(*this);
+            SvMemoryStream& rDocBuffer = pPage->GetDocument().GetEditBuffer();
+            std::map<sal_Int32, sal_Int32> aMap;
+            for (const auto& pAnnot : aAnnots)
+            {
+                // Copy over the annotation and refer to its new id.
+                sal_Int32 nNewId = aCopier.copyExternalResource(rDocBuffer, *pAnnot, aMap);
+                m_aPages.back().m_aAnnotations.push_back(nNewId);
+            }
+        }
+
         nWrappedFormObject = createObject();
         // Write the form XObject wrapped below. This is a separate object from
         // the wrapper, this way there is no need to alter the stream contents.

vcl/qa/cppunit/pdfexport/pdfexport.cxx
@@ -147,6 +147,7 @@ public:
     void testDefaultVersion();
     void testMultiPagePDF();
     void testPdfImageRotate180();
+    void testPdfImageHyperlink();
 
 
     CPPUNIT_TEST_SUITE(PdfExportTest);
@@ -191,6 +192,7 @@ public:
     CPPUNIT_TEST(testDefaultVersion);
     CPPUNIT_TEST(testMultiPagePDF);
     CPPUNIT_TEST(testPdfImageRotate180);
+    CPPUNIT_TEST(testPdfImageHyperlink);
     CPPUNIT_TEST_SUITE_END();
 };
 
@@ -2250,6 +2252,36 @@ void PdfExportTest::testPdfImageRotate180()
     // i.e. the 180 degrees rotation didn't happen (via a combination of horizontal + vertical
     // flip).
     CPPUNIT_ASSERT_DOUBLES_EQUAL(-1.0, aScale.getX(), 0.01);
+}
+
+void PdfExportTest::testPdfImageHyperlink()
+{
+    // Given a Draw file, containing a PDF image, which has a hyperlink in it:
+    OUString aURL = m_directories.getURLFromSrc(DATA_DIRECTORY) + "pdf-image-hyperlink.odg";
+    mxComponent = loadFromDesktop(aURL);
+    CPPUNIT_ASSERT(mxComponent.is());
+
+    // When saving to PDF:
+    uno::Reference<frame::XStorable> xStorable(mxComponent, uno::UNO_QUERY);
+    utl::MediaDescriptor aMediaDescriptor;
+    aMediaDescriptor["FilterName"] <<= OUString("draw_pdf_Export");
+    xStorable->storeToURL(maTempFile.GetURL(), aMediaDescriptor.getAsConstPropertyValueList());
+
+    // Then make sure that link is preserved:
+    SvFileStream aFile(maTempFile.GetURL(), StreamMode::READ);
+    maMemory.WriteStream(aFile);
+    std::shared_ptr<vcl::pdf::PDFium> pPDFium = vcl::pdf::PDFiumLibrary::get();
+    std::unique_ptr<vcl::pdf::PDFiumDocument> pPdfDocument
+        = pPDFium->openDocument(maMemory.GetData(), maMemory.GetSize());
+    CPPUNIT_ASSERT(pPdfDocument);
+    CPPUNIT_ASSERT_EQUAL(1, pPdfDocument->getPageCount());
+    std::unique_ptr<vcl::pdf::PDFiumPage> pPdfPage = pPdfDocument->openPage(/*nIndex=*/0);
+    CPPUNIT_ASSERT(pPdfPage);
+    int nStartPos = 0;
+    FPDF_LINK pLinkAnnot = nullptr;
+    // Without the accompanying fix in place, this test would have failed, the hyperlink of the PDF
+    // image was lost.
+    CPPUNIT_ASSERT(FPDFLink_Enumerate(pPdfPage->getPointer(), &nStartPos, &pLinkAnnot));
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(PdfExportTest);

commit bec33d1db4f74680a2e0f67586176da59cdacc85

PDF export of PDF images: don't preserve annotations in general
Regression from 33c9bc0225a92f26770f9ef20b252af47853e7b9 (PDF export of
PDF images: preserve hyperlinks, 2022-01-07), the problem was that we
want to preserve hyperlinks, but annotations are added by the PDF export
explicitly, so it isn't a good idea to "preserve" them as well.

Fix the problem by going back to the old behavior, except when the
annotation sub-type is /Link.

This keeps hyperlinks working but doesn't lead to duplicated comments
when re-exporting an image + adding comments explicitly.

Change-Id: I910990da59bdc1150cc346f1a5471cb6da55dd2c

vcl/source/gdi/pdfwriter_impl.cxx
@@ -8436,7 +8436,7 @@ void PDFWriterImpl::writeReferenceXObject(const ReferenceXObjectEmit& rEmit)
             return;
         }
 
-        // Merge page annotations (links, etc) from pPage to our page.
+        // Merge link annotations from pPage to our page.
         std::vector<filter::PDFObjectElement*> aAnnots;
         if (auto pArray = dynamic_cast<filter::PDFArrayElement*>(pPage->Lookup("Annots")))
         {
@@ -8454,7 +8454,19 @@ void PDFWriterImpl::writeReferenceXObject(const ReferenceXObjectEmit& rEmit)
                     continue;
                 }
 
-                // Annotation refers to an object, remember it.
+                auto pType = dynamic_cast<filter::PDFNameElement*>(pObject->Lookup("Type"));
+                if (!pType || pType->GetValue() != "Annot")
+                {
+                    continue;
+                }
+
+                auto pSubtype = dynamic_cast<filter::PDFNameElement*>(pObject->Lookup("Subtype"));
+                if (!pSubtype || pSubtype->GetValue() != "Link")
+                {
+                    continue;
+                }
+
+                // Reference to a link annotation object, remember it.
                 aAnnots.push_back(pObject);
             }
         }

vcl/qa/cppunit/pdfexport/pdfexport.cxx
@@ -3127,6 +3127,26 @@ CPPUNIT_TEST_FIXTURE(PdfExportTest, testURIs)
         CPPUNIT_ASSERT_EQUAL(URIs[i].out, pURIElem->GetValue());
     }
 }
+
+CPPUNIT_TEST_FIXTURE(PdfExportTest, testPdfImageAnnots)
+{
+    // Given a document with a PDF image that has 2 comments (popup, text) and a hyperlink:
+    aMediaDescriptor["FilterName"] <<= OUString("draw_pdf_Export");
+
+    // When saving to PDF:
+    saveAsPDF(u"pdf-image-annots.odg");
+
+    // Then make sure only the hyperlink is kept, since Draw itself has its own comments:
+    std::unique_ptr<vcl::pdf::PDFiumDocument> pPdfDocument = parseExport();
+    CPPUNIT_ASSERT(pPdfDocument);
+    std::unique_ptr<vcl::pdf::PDFiumPage> pPdfPage = pPdfDocument->openPage(/*nIndex=*/0);
+    CPPUNIT_ASSERT(pPdfPage);
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 1
+    // - Actual  : 3
+    // i.e. not only the hyperlink but also the 2 comments were exported, leading to duplication.
+    CPPUNIT_ASSERT_EQUAL(1, pPdfPage->getAnnotationCount());
+}
 } // end anonymous namespace
 
 CPPUNIT_PLUGIN_IMPLEMENT();