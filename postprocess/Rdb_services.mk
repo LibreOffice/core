06e4c6fd51f60f1f7d474ede321fe031cde0b3ca
Michael Stahl <michael.stahl@allotropia.de>
2022-03-09 16:21:07 +0100

    TODO: does it need own component file?
    Change-Id: I01e169e9445b2711d95c83a07f3c26507b5c1ab1

--- postprocess/Rdb_services.mk
+++ postprocess/Rdb_services.mk
@@ -168,6 +168,9 @@ $(eval $(call gb_Rdb_add_components,services,\
 	$(if $(WITH_WEBDAV), \
 		ucb/source/ucp/webdav-neon/ucpdav1 \
 	) \
+    $(if $(filter curl,$(WITH_WEBDAV)), \
+        ucb/source/ucp/webdav-curl/ucpdav1 \
+    ) \
 	$(call gb_Helper_optional,SCRIPTING, \
 		basctl/util/basctl \
 		basic/util/sb \
