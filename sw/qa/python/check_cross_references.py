1ae64bc10552d442835f3227b12f39c7c2b711fb
Vasily Melenchuk <vasily.melenchuk@cib.de>
2022-03-09 17:00:02 +0300

    tdf#144563: remove final dot in cross-references to paragraph
    It looks like in cross-references ending with dot (".") one last
    dot is removed in case of MS Word. This is not a true for any other
    suffixes after numeration.
    
    Change-Id: I554e62cf45e643bf27823df5344e1689b5b6ae54

--- sw/qa/python/check_cross_references.py
+++ sw/qa/python/check_cross_references.py
@@ -89,16 +89,16 @@ class CheckCrossReferences(unittest.TestCase):
         FieldResult1 = "*i*"
         FieldResult2 = "+b+*i*"
         FieldResult3 = "-1-+b+*i*"
-        FieldResult4 = "1."
-        FieldResult5 = "1."
-        FieldResult6 = "A.1."
+        FieldResult4 = "1"
+        FieldResult5 = "1"
+        FieldResult6 = "A.1"
         FieldResult7 = " 2.(a)"
         FieldResult8 = " 2.(b)"
-        FieldResult9 = " 2."
+        FieldResult9 = " 2"
         FieldResult10 = " 1.(a)"
         FieldResult11 = "(b)"
         FieldResult12 = "(a)"
-        FieldResult13 = " 1."
+        FieldResult13 = " 1"
 
         # variables for current field
         xField = self.getNextField()
