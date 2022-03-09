31493ef56a36d7b5a7964d43686a065781255476
Stephan Bergmann <sbergman@redhat.com>
2022-03-09 14:28:18 +0100

    Missing test dependency
    > [_RUN_____] HSQLDBTest::testEmptyDBConnection
    [...]
    > warn:vcl.builder:997476:997476:vcl/source/window/builder.cxx:479: DBG_UNHANDLED_EXCEPTION in VclBuilder
    >     when: Unable to read .ui file exception: com.sun.star.container.NoSuchElementException message: "file:///.../instdir/share/config/soffice.cfg/dbaccess/ui/appborderwindow.ui at xmlreader/source/xmlreader.cxx:66"
    
    Change-Id: I695e19919c66e14852fca2cc23436b463544ec52

--- dbaccess/CppunitTest_dbaccess_hsqldb_test.mk
+++ dbaccess/CppunitTest_dbaccess_hsqldb_test.mk
@@ -77,4 +77,8 @@ $(eval $(call gb_CppunitTest_use_components,dbaccess_hsqldb_test,\
 
 $(eval $(call gb_CppunitTest_use_configuration,dbaccess_hsqldb_test))
 
+$(eval $(call gb_CppunitTest_use_uiconfigs,dbaccess_hsqldb_test, \
+    dbaccess \
+))
+
 # vim: set noet sw=4 ts=4:
