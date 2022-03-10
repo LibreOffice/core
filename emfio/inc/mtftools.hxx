6a37bfbb8b9414691cb5e47669a3162d947d2b07
Hossein <hossein@libreoffice.org>
2022-03-10 11:40:49 +0100

    WIP: Fix tdf#142021 tdf#84199 Implement SetPolyFillMode record
    SetPolyMillMode is a feature of EMF that was not implemented before. In
    this patch I try to implement it in read/write/display of EMF. A new
    meta action named MetaFillModeAction for changing the FillMode.
    
    This will fix tdf#142021 and the dependant tdf#84199.
    
    Current implementation fixes the rendering of the EMF with the
    winding FillRule. Previously, only even-odd FillRule was supported.
    
    gtk3, gtk4, Qt5, Qt6 and PDF should be OK for winding FillRule.
    
    Remaining issues to be fixed:
    
    * Fix incomplete conditions. Now you may get winding FillRule only.
    
    * Fix problems in svgio which aries from converting the PolyPoygons
    using basegfx::utils::createNonzeroConform(). We should no longer
    use this method.
    svgio/source/svgreader/svgstyleattributes.cxx:1134
    
    * Fix problems in X11 renderer, which arises from the way X11 uses
    "trapezoidal decomposition". The problem is with the intersections.
    basegfx/source/polygon/b2dtrapezoid.cxx:717
    
    * Write MetaFillModeAction to the EMF file.
    vcl/source/filter/wmf/emfwr.hxx
    vcl/source/filter/wmf/emfwr.cxx
    
    This is work in progress (WIP), and is not yet finished.
    
    Change-Id: I4368323729708da8b7bfc08ef7216bcaeaf88a52

--- emfio/inc/mtftools.hxx
+++ emfio/inc/mtftools.hxx
@@ -29,6 +29,7 @@
 #include <vcl/rendercontext/State.hxx>
 #include <vcl/metaact.hxx>
 #include <rtl/ref.hxx>
+#include <vcl/metaact.hxx>
 
 #include "emfiodllapi.h"
 
@@ -435,6 +436,7 @@ namespace emfio
     struct SaveStruct
     {
         BkMode              nBkMode;
+        PolyFillMode        nPolyFillMode;
         sal_uInt32          nMapMode, nGfxMode;
         vcl::text::ComplexTextLayoutFlags nTextLayoutMode;
         sal_Int32           nWinOrgX, nWinOrgY, nWinExtX, nWinExtY;
@@ -525,6 +527,8 @@ namespace emfio
         Color               maBkColor;
         vcl::text::ComplexTextLayoutFlags  mnLatestTextLayoutMode;
         vcl::text::ComplexTextLayoutFlags  mnTextLayoutMode;
+        PolyFillMode        meLatestPolyFillMode;
+        PolyFillMode        mePolyFillMode;
         BkMode              mnLatestBkMode;
         BkMode              mnBkMode;
         RasterOp            meLatestRasterOp;
@@ -574,6 +578,7 @@ namespace emfio
         bool                mbIsMapDevSet : 1;
 
         void                UpdateLineStyle();
+        void                UpdateFillMode();
         void                UpdateFillStyle();
 
         Point               ImplMap(const Point& rPt);
@@ -622,6 +627,7 @@ namespace emfio
         sal_Int32           GetGfxMode() const { return mnGfxMode; };
         void                SetArcDirection(bool bCounterClockWise);
         bool                IsArcDirectionClockWise() { return mbClockWiseArcDirection; };
+        void                setPolyFillMode(PolyFillMode nMode);
         void                SetBkMode(BkMode nMode);
         void                SetBkColor(const Color& rColor);
         void                SetTextColor(const Color& rColor);
