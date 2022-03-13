LibreOffice/core

commit 7cd5b35caa8d4fa9d0ba2b2c6ce4b88726ed2be6

return SwCursor from IShellCursorSupplier
rather than casting it

Change-Id: Ia950f521c8f73d8a0da59344db9b5aacb727aca8
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/122576
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sw/source/core/edit/autofmt.cxx
@@ -2377,7 +2377,7 @@ SwAutoFormat::SwAutoFormat( SwEditShell* pEdShell, SvxSwAutoFormatFlags const & 
                 {
                     //JP 30.09.96: DoTable() builds on PopCursor and MoveCursor after AutoFormat!
                     pEdShell->Pop(SwCursorShell::PopMode::DeleteCurrent);
-                    *pEdShell->GetCursor() = m_aDelPam;
+                    *pEdShell->GetCursor() = static_cast<SwCursor&>(m_aDelPam);
                     pEdShell->Push();
 
                     eStat = IS_END;

sw/inc/IShellCursorSupplier.hxx
@@ -20,7 +20,7 @@
 #ifndef INCLUDED_SW_INC_ISHELLCURSORSUPPLIER_HXX
 #define INCLUDED_SW_INC_ISHELLCURSORSUPPLIER_HXX
 
-class SwPaM;
+class SwCursor;
 
 namespace sw
 {
@@ -32,8 +32,8 @@ class IShellCursorSupplier
 {
 public:
     virtual ~IShellCursorSupplier() {}
-    virtual SwPaM& CreateNewShellCursor() = 0;
-    virtual SwPaM& GetCurrentShellCursor() = 0;
+    virtual SwCursor& CreateNewShellCursor() = 0;
+    virtual SwCursor& GetCurrentShellCursor() = 0;
 };
 
 } // namespace sw

sw/inc/crsrsh.hxx
@@ -300,8 +300,8 @@ public:
     virtual ~SwCursorShell() override;
 
     // create new cursor and append the old one
-    virtual SwPaM & CreateNewShellCursor() override;
-    virtual SwPaM & GetCurrentShellCursor() override;
+    virtual SwCursor & CreateNewShellCursor() override;
+    virtual SwCursor & GetCurrentShellCursor() override;
 
     SwPaM * CreateCursor();
     ///< delete the current cursor and make the following into the current
@@ -322,8 +322,7 @@ public:
     /// If document body starts with a table.
     bool StartsWithTable();
 
-    SwPaM* GetCursor( bool bMakeTableCursor = true ) const;
-    inline SwCursor* GetSwCursor() const;
+    SwCursor* GetCursor( bool bMakeTableCursor = true ) const;
     // return only the current cursor
           SwShellCursor* GetCursor_()                       { return m_pCurrentCursor; }
     const SwShellCursor* GetCursor_() const                 { return m_pCurrentCursor; }
@@ -863,11 +862,6 @@ inline SwMoveFnCollection const & SwCursorShell::MakeFindRange(
             SwDocPositions nStt, SwDocPositions nEnd, SwPaM* pPam ) const
 {
     return m_pCurrentCursor->MakeFindRange( nStt, nEnd, pPam );
-}
-
-inline SwCursor* SwCursorShell::GetSwCursor() const
-{
-    return static_cast<SwCursor*>(GetCursor());
 }
 
 inline SwPaM* SwCursorShell::GetStackCursor() const { return m_pStackCursor; }

sw/qa/extras/uiwriter/uiwriter4.cxx
@@ -2777,11 +2777,11 @@ void SwUiWriterTest4::testTdf115132()
         pWrtShell->GotoTable(rTableName);
         do
         {
-            const SwStartNode* pNd = pWrtShell->GetSwCursor()->GetNode().FindTableBoxStartNode();
+            const SwStartNode* pNd = pWrtShell->GetCursor()->GetNode().FindTableBoxStartNode();
             pWrtShell->DelRight();
-            CPPUNIT_ASSERT_EQUAL(pNd, pWrtShell->GetSwCursor()->GetNode().FindTableBoxStartNode());
+            CPPUNIT_ASSERT_EQUAL(pNd, pWrtShell->GetCursor()->GetNode().FindTableBoxStartNode());
             pWrtShell->DelLeft();
-            CPPUNIT_ASSERT_EQUAL(pNd, pWrtShell->GetSwCursor()->GetNode().FindTableBoxStartNode());
+            CPPUNIT_ASSERT_EQUAL(pNd, pWrtShell->GetCursor()->GetNode().FindTableBoxStartNode());
         } while (pWrtShell->GoNextCell(false));
     }
 }

sw/source/core/crsr/crbm.cxx
@@ -33,7 +33,7 @@ namespace
     struct CursorStateHelper
     {
         explicit CursorStateHelper(SwCursorShell const & rShell)
-            : m_pCursor(rShell.GetSwCursor())
+            : m_pCursor(rShell.GetCursor())
             , m_aSaveState(*m_pCursor)
         { }
 

sw/source/core/crsr/crsrsh.cxx
@@ -170,7 +170,7 @@ void SwCursorShell::DestroyCursor()
  * Simply returns the current shell cursor if there is no selection
  * (HasSelection()).
  */
-SwPaM & SwCursorShell::CreateNewShellCursor()
+SwCursor & SwCursorShell::CreateNewShellCursor()
 {
     if (HasSelection())
     {
@@ -183,16 +183,16 @@ SwPaM & SwCursorShell::CreateNewShellCursor()
  * Return the current shell cursor
  * @return - returns current `SwPaM` shell cursor
  */
-SwPaM & SwCursorShell::GetCurrentShellCursor()
+SwCursor & SwCursorShell::GetCurrentShellCursor()
 {
     return *GetCursor();
 }
 
 /**
  * Return pointer to the current shell cursor
- * @return - returns pointer to current `SwPaM` shell cursor
+ * @return - returns pointer to current `SwCursor` shell cursor
  */
-SwPaM* SwCursorShell::GetCursor( bool bMakeTableCursor ) const
+SwCursor* SwCursorShell::GetCursor( bool bMakeTableCursor ) const
 {
     if( m_pTableCursor )
     {
@@ -1055,7 +1055,7 @@ int SwCursorShell::CompareCursorStackMkCurrPt() const
 {
     int nRet = 0;
     const SwPosition *pFirst = nullptr, *pSecond = nullptr;
-    const SwPaM *pCur = GetCursor(), *pStack = m_pStackCursor;
+    const SwCursor *pCur = GetCursor(), *pStack = m_pStackCursor;
     // cursor on stack is needed if we compare against stack
     if( pStack  )
     {
@@ -3538,7 +3538,7 @@ sal_uLong SwCursorShell::FindAttrs( const SfxItemSet& rSet,
 void SwCursorShell::SetSelection( const SwPaM& rCursor )
 {
     StartAction();
-    SwPaM* pCursor = GetCursor();
+    SwCursor* pCursor = GetCursor();
     *pCursor->GetPoint() = *rCursor.GetPoint();
     if(rCursor.GetNext() != &rCursor)
     {

sw/source/core/edit/edlingu.cxx
@@ -652,7 +652,7 @@ void SwEditShell::SpellStart(
 
     if (pLinguIter)
     {
-        SwCursor* pSwCursor = GetSwCursor();
+        SwCursor* pSwCursor = GetCursor();
 
         SwPosition *pTmp = new SwPosition( *pSwCursor->GetPoint() );
         pSwCursor->FillFindPos( eCurr, *pTmp );

sw/source/core/frmedt/fefly1.cxx
@@ -840,7 +840,7 @@ void SwFEShell::Insert( const OUString& rGrfName, const OUString& rFltName,
     SwFlyFrameFormat* pFormat = nullptr;
     CurrShell aCurr( this );
     StartAllAction();
-    SwShellCursor *pStartCursor = dynamic_cast<SwShellCursor*>(GetSwCursor());
+    SwShellCursor *pStartCursor = dynamic_cast<SwShellCursor*>(GetCursor());
     SwShellCursor *pCursor = pStartCursor;
     do
     {

commit 9aa23b07c1b0bc3e7cc75eb0debf50be9396ff7d

tdf#147961 Crash Typing "++" and press Enter (AutoCorrect Create Table)
regression from
    commit 7cd5b35caa8d4fa9d0ba2b2c6ce4b88726ed2be6
    Author: Noel Grandin <noelgrandin@gmail.com>
    Date:   Fri Sep 24 13:21:35 2021 +0200
    return SwCursor from IShellCursorSupplier

Change-Id: I6a4e3f90653dc96a91091be1372f27c6303ba346

sw/source/core/edit/autofmt.cxx
@@ -2371,7 +2371,7 @@ SwAutoFormat::SwAutoFormat( SwEditShell* pEdShell, SvxSwAutoFormatFlags const & 
                 {
                     //JP 30.09.96: DoTable() builds on PopCursor and MoveCursor after AutoFormat!
                     pEdShell->Pop(SwCursorShell::PopMode::DeleteCurrent);
-                    *pEdShell->GetCursor() = static_cast<SwCursor&>(m_aDelPam);
+                    static_cast<SwPaM&>(*pEdShell->GetCursor()) = m_aDelPam;
                     pEdShell->Push();
 
                     eStat = IS_END;