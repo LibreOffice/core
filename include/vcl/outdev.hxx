2e6babe48786c6d73cd27ef16ff8f87116cab283
Caolán McNamara <caolanm@redhat.com>
2022-03-13 13:41:31 +0000

    ofz: Divide-by-zero
    Change-Id: I8d88558be5bcf4556d94ab86015f5e039e72da08

--- include/vcl/outdev.hxx
+++ include/vcl/outdev.hxx
@@ -1113,6 +1113,7 @@ private:
     SAL_DLLPRIVATE void         ImplDrawStrikeoutChar( tools::Long nBaseX, tools::Long nBaseY, tools::Long nX, tools::Long nY, tools::Long nWidth, FontStrikeout eStrikeout, Color aColor );
     SAL_DLLPRIVATE void         ImplDrawMnemonicLine( tools::Long nX, tools::Long nY, tools::Long nWidth );
 
+    SAL_DLLPRIVATE bool         AttemptOLEFontScaleFix(vcl::Font& rFont, tools::Long nHeight) const;
 
     ///@}
 
