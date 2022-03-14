a77981ad21aa0793f0af2ac262bd3573e0501235
Xisco Fauli <xiscofauli@libreoffice.org>
2022-03-14 12:07:50 +0100

    uitest: sc: move external links tests to their own place
    Change-Id: Ie02318a502ec9baf8c27c9a353b070ae113672f1
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/131531
    Tested-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- sc/Module_sc.mk
+++ sc/Module_sc.mk
@@ -258,6 +258,7 @@ $(eval $(call gb_Module_add_uicheck_targets,sc,\
 	UITest_calc_tests4 \
 	UITest_calc_tests6 \
 	UITest_csv_dialog \
+	UITest_external_links \
 	UITest_statistics \
 	UITest_solver \
 	UITest_goalSeek \
