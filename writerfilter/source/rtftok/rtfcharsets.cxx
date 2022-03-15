94d971bb6c44cf1eaa664e810a6cf393b1505b92
Sinduja Y <sinduja.code@gmail.com>
2022-03-15 12:39:03 +0100

    tdf#147021 Use std::size() instead of SAL_N_ELEMENTS() macro
    Change-Id: Ifed6b79a4186e3196dc1e9ac179fe8ad0ee4a342

--- writerfilter/source/rtftok/rtfcharsets.cxx
+++ writerfilter/source/rtftok/rtfcharsets.cxx
@@ -48,7 +48,7 @@ RTFEncoding const aRTFEncodings[] = {
     { 255, 850 }, // OEM
 };
 
-int nRTFEncodings = SAL_N_ELEMENTS(aRTFEncodings);
+int nRTFEncodings = std::size(aRTFEncodings);
 
 } // namespace writerfilter::rtftok
 
