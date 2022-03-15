4c7ab1171a683ef00b09125cf2eae187b7383463
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2022-03-15 15:47:08 +0900

    sc: add "Delete Sparkline" context menu command
    Change-Id: I0e2bf2172da3cbba391ca64c93fad6bccd82f3ac
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/132468
    Tested-by: Jenkins
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>
    (cherry picked from commit 84dfdac1f7139337e61288af0e75aef66c19b29a)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/132792
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

--- sc/source/ui/view/cellsh1.cxx
+++ sc/source/ui/view/cellsh1.cxx
@@ -1062,6 +1062,14 @@ void ScCellShell::ExecuteEdit( SfxRequest& rReq )
         }
         break;
 
+        case SID_DELETE_SPARKLINE:
+        {
+            pTabViewShell->DeleteContents(InsertDeleteFlags::SPARKLINES);
+
+            rReq.Done();
+        }
+        break;
+
         //  disposal (Outlines)
         //  SID_AUTO_OUTLINE, SID_OUTLINE_DELETEALL in Execute (in docsh.idl)
 
