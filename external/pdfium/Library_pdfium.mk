c9bf4a312ef4ffd611617a677426f0255e5eb743
Miklos Vajna <vmiklos@collabora.com>
2022-03-17 09:02:29 +0100

    external: update pdfium to 4933
    Stop tracking build_config.h in our repo, it can be now part of the
    release tarball. It was a mistake in the past to think that this is
    generated code, it's just external code.
    
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/131695
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Tested-by: Jenkins
    (cherry picked from commit 71b952340726190d1f178ef0dadfa89677f2c1dd)
    
    Change-Id: Ife2fed362e28c53859399244e2f1247efb1efe86

--- external/pdfium/Library_pdfium.mk
+++ external/pdfium/Library_pdfium.mk
@@ -291,6 +291,7 @@ $(eval $(call gb_Library_add_generated_exception_objects,pdfium,\
     UnpackedTarball/pdfium/core/fpdfapi/page/cpdf_transferfuncdib \
     UnpackedTarball/pdfium/core/fpdfapi/page/cpdf_transferfunc \
     UnpackedTarball/pdfium/core/fpdfapi/page/cpdf_basedcs \
+    UnpackedTarball/pdfium/core/fpdfapi/page/cpdf_indexedcs \
 ))
 
 # fpdfdoc
@@ -481,6 +482,7 @@ $(eval $(call gb_Library_add_generated_exception_objects,pdfium,\
     UnpackedTarball/pdfium/core/fxge/dib/cfx_cmyk_to_srgb \
     UnpackedTarball/pdfium/core/fxge/text_char_pos \
     UnpackedTarball/pdfium/core/fxge/cfx_face \
+    UnpackedTarball/pdfium/core/fxge/calculate_pitch \
 ))
 
 # javascript, build with pdf_enable_v8 disabled.
