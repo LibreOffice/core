9e8712ed6f9fb5dbd971e352a5709bd45fadc74f
Miklos Vajna <vmiklos@collabora.com>
2022-03-17 17:36:18 +0100

    sw image dialog: fix fallback width/height for images with relative sizes
    Make sure that we work with the up to date layout size of the image in
    the dialog, otherwise 80% shows up as 237%.
    
    Also fix SfxMacroTabPage::FillItemSet() to not put anything to the
    output item set when the macro table is empty, this way clicking OK in
    the image dialog results in an unmodified document when the user doesn't
    change anything.
    
    Change-Id: I4b987bd3e3818ee737e37ea10861f9043c25bc93
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/131714
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Tested-by: Jenkins

---
+++ sw/qa/uitest/uibase/shells/shells.py
@@ -0,0 +1,32 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+from uitest.framework import UITestCase
+from uitest.uihelper.common import get_state_as_dict
+from uitest.uihelper.common import get_url_for_data_file
+import time
+
+
+class TestSwGrfShell(UITestCase):
+    def testFormatGraphicDlg(self):
+        # Given a document with an image with a relative size:
+        with self.ui_test.load_file(get_url_for_data_file("image-rel-size.fodt")) as component:
+            self.xUITest.executeCommand(".uno:JumpToNextFrame")
+            # 120 ms in the SwView ctor
+            time.sleep(0.2)
+            # When opening the properties dialog for the image:
+            with self.ui_test.execute_dialog_through_command(".uno:GraphicDialog") as dialog:
+                # Then make sure the width is 80% as in the file:
+                widthField = dialog.getChild('width')
+                # Without the accompanying fix in place, this test would have failed with:
+                # AssertionError: '237%' != '80%'
+                # i.e. the percent width of the image was wrong.
+                self.assertEqual(get_state_as_dict(widthField)["Text"], "80%")
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
