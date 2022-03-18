434b353ec811fec464cecd8e3b2696a3936a1b91
Miklos Vajna <vmiklos@collabora.com>
2022-03-18 08:09:11 +0100

    sw layout xml dump: handle expand portions
    Towards adding dumpAsXml() for all portions that have a HandlePortion().
    
    Change-Id: I9cb1f4c1f966e5cfd7ca27e21bf8aa022979f3c2

--- sw/source/core/text/porexp.hxx
+++ sw/source/core/text/porexp.hxx
@@ -33,6 +33,8 @@ public:
 
     // Accessibility: pass information about this portion to the PortionHandler
     virtual void HandlePortion( SwPortionHandler& rPH ) const override;
+
+    void dumpAsXml(xmlTextWriterPtr pWriter) const override;
 };
 
 /// Non-breaking space or non-breaking hyphen.
