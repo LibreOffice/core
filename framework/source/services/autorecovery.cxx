LibreOffice/core

commit 4256c764aee0777770466115a97420d9b55c23ac

do not pass XComponentContext to officecfg::...::get() calls
It's used only for the ConfigurationWrapper singleton, so it's used
only the first time and then ignored. It also causes calls to
comphelper::getProcessComponentContext() for every single invocation
despite the value not being needed, and the calls may not be cheap
(it's ~5% CPU during ODS save because relatively frequent calls
to officecfg::Office::Common::Save::ODF::DefaultVersion::get()).

Change-Id: I02c17a1a9cb498aeef220ddd5a0bde5523cb0ffb
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/131056
Tested-by: Jenkins
Reviewed-by: Luboš Luňák <l.lunak@collabora.com>

framework/source/services/autorecovery.cxx
@@ -1724,8 +1724,8 @@ void AutoRecovery::implts_openConfig()
 
     try
     {
-        nMinSpaceDocSave = officecfg::Office::Recovery::AutoSave::MinSpaceDocSave::get(m_xContext);
-        nMinSpaceConfigSave = officecfg::Office::Recovery::AutoSave::MinSpaceConfigSave::get(m_xContext);
+        nMinSpaceDocSave = officecfg::Office::Recovery::AutoSave::MinSpaceDocSave::get();
+        nMinSpaceConfigSave = officecfg::Office::Recovery::AutoSave::MinSpaceConfigSave::get();
     }
     catch(const css::uno::Exception&)
     {
@@ -1748,13 +1748,13 @@ void AutoRecovery::implts_readAutoSaveConfig()
     implts_openConfig();
 
     // AutoSave [bool]
-    bool bEnabled(officecfg::Office::Recovery::AutoSave::Enabled::get(m_xContext));
+    bool bEnabled(officecfg::Office::Recovery::AutoSave::Enabled::get());
 
     /* SAFE */ {
     osl::MutexGuard g(cppu::WeakComponentImplHelperBase::rBHelper.rMutex);
     if (bEnabled)
     {
-        bool bUserEnabled(officecfg::Office::Recovery::AutoSave::UserAutoSaveEnabled::get(m_xContext));
+        bool bUserEnabled(officecfg::Office::Recovery::AutoSave::UserAutoSaveEnabled::get());
 
         m_eJob       |= Job::AutoSave;
         m_eTimerType  = AutoRecovery::E_NORMAL_AUTOSAVE_INTERVALL;
@@ -1776,7 +1776,7 @@ void AutoRecovery::implts_readAutoSaveConfig()
     } /* SAFE */
 
     // AutoSaveTimeIntervall [int] in min
-    sal_Int32 nTimeIntervall(officecfg::Office::Recovery::AutoSave::TimeIntervall::get(m_xContext));
+    sal_Int32 nTimeIntervall(officecfg::Office::Recovery::AutoSave::TimeIntervall::get());
 
     /* SAFE */ {
     osl::MutexGuard g(cppu::WeakComponentImplHelperBase::rBHelper.rMutex);
@@ -1802,7 +1802,7 @@ void AutoRecovery::implts_readConfig()
     // <- REENTRANT --------------------------------
 
     css::uno::Reference<css::container::XNameAccess> xRecoveryList(
-            officecfg::Office::Recovery::RecoveryList::get(m_xContext));
+            officecfg::Office::Recovery::RecoveryList::get());
     const OUString sRECOVERY_ITEM_BASE_IDENTIFIER(RECOVERY_ITEM_BASE_IDENTIFIER);
     const css::uno::Sequence< OUString > lItems = xRecoveryList->getElementNames();
     const OUString*                      pItems = lItems.getConstArray();
@@ -1884,7 +1884,7 @@ void AutoRecovery::implts_specifyDefaultFilterAndExtension(AutoRecovery::TDocume
         {
             implts_openConfig();
             // open module config on demand and cache the update access
-            xCFG.set(officecfg::Setup::Office::Factories::get(m_xContext),
+            xCFG.set(officecfg::Setup::Office::Factories::get(),
                     css::uno::UNO_SET_THROW);
 
             /* SAFE */ {
@@ -1985,14 +1985,14 @@ void AutoRecovery::implts_persistAllActiveViewNames()
 void AutoRecovery::implts_flushConfigItem(const AutoRecovery::TDocumentInfo& rInfo, bool bRemoveIt)
 {
     std::shared_ptr<comphelper::ConfigurationChanges> batch(
-            comphelper::ConfigurationChanges::create(m_xContext));
+            comphelper::ConfigurationChanges::create());
 
     try
     {
         implts_openConfig();
 
         css::uno::Reference<css::container::XNameAccess> xCheck(
-                officecfg::Office::Recovery::RecoveryList::get(batch));
+                officecfg::Office::Recovery::RecoveryList::get());
 
         css::uno::Reference< css::container::XNameContainer >   xModify(xCheck, css::uno::UNO_QUERY_THROW);
         css::uno::Reference< css::lang::XSingleServiceFactory > xCreate(xCheck, css::uno::UNO_QUERY_THROW);
@@ -3629,7 +3629,7 @@ void AutoRecovery::implts_doEmergencySave(const DispatchParams& aParams)
     // documents exists and was saved.
 
     std::shared_ptr<comphelper::ConfigurationChanges> batch(
-            comphelper::ConfigurationChanges::create(m_xContext));
+            comphelper::ConfigurationChanges::create());
     officecfg::Office::Recovery::RecoveryInfo::Crashed::set(true, batch);
     batch->commit();
 
@@ -3686,7 +3686,7 @@ void AutoRecovery::implts_doRecovery(const DispatchParams& aParams)
 
     // Reset the configuration hint "we were crashed"!
     std::shared_ptr<comphelper::ConfigurationChanges> batch(
-            comphelper::ConfigurationChanges::create(m_xContext));
+            comphelper::ConfigurationChanges::create());
     officecfg::Office::Recovery::RecoveryInfo::Crashed::set(false, batch);
     batch->commit();
 }
@@ -3748,7 +3748,7 @@ void AutoRecovery::implts_doSessionQuietQuit()
     // Write a hint for "stored session data" into the configuration, so
     // the on next startup we know what's happen last time
     std::shared_ptr<comphelper::ConfigurationChanges> batch(
-            comphelper::ConfigurationChanges::create(m_xContext));
+            comphelper::ConfigurationChanges::create());
     officecfg::Office::Recovery::RecoveryInfo::SessionData::set(true, batch);
     batch->commit();
 
@@ -3780,7 +3780,7 @@ void AutoRecovery::implts_doSessionRestore(const DispatchParams& aParams)
     // Reset the configuration hint for "session save"!
     SAL_INFO("fwk.autorecovery", "... reset config key 'SessionData'");
     std::shared_ptr<comphelper::ConfigurationChanges> batch(
-            comphelper::ConfigurationChanges::create(m_xContext));
+            comphelper::ConfigurationChanges::create());
     officecfg::Office::Recovery::RecoveryInfo::SessionData::set(false, batch);
     batch->commit();
 
@@ -3897,7 +3897,7 @@ void SAL_CALL AutoRecovery::getFastPropertyValue(css::uno::Any& aValue ,
     {
         case AUTORECOVERY_PROPHANDLE_EXISTS_RECOVERYDATA :
                 {
-                    bool bSessionData = officecfg::Office::Recovery::RecoveryInfo::SessionData::get(m_xContext);
+                    bool bSessionData = officecfg::Office::Recovery::RecoveryInfo::SessionData::get();
                     bool bRecoveryData = !m_lDocCache.empty();
 
                     // exists session data ... => then we can't say, that these
@@ -3910,11 +3910,11 @@ void SAL_CALL AutoRecovery::getFastPropertyValue(css::uno::Any& aValue ,
                 break;
 
         case AUTORECOVERY_PROPHANDLE_CRASHED :
-                aValue <<= officecfg::Office::Recovery::RecoveryInfo::Crashed::get(m_xContext);
+                aValue <<= officecfg::Office::Recovery::RecoveryInfo::Crashed::get();
                 break;
 
         case AUTORECOVERY_PROPHANDLE_EXISTS_SESSIONDATA :
-                aValue <<= officecfg::Office::Recovery::RecoveryInfo::SessionData::get(m_xContext);
+                aValue <<= officecfg::Office::Recovery::RecoveryInfo::SessionData::get();
                 break;
     }
 }

comphelper/source/misc/configuration.cxx
@@ -38,9 +38,9 @@ namespace com::sun::star::uno { class XComponentContext; }
 
 namespace {
 
-comphelper::detail::ConfigurationWrapper& GetTheConfigurationWrapper(const css::uno::Reference< css::uno::XComponentContext >& xContext)
+comphelper::detail::ConfigurationWrapper& GetTheConfigurationWrapper()
 {
-    static comphelper::detail::ConfigurationWrapper WRAPPER(xContext);
+    static comphelper::detail::ConfigurationWrapper WRAPPER(comphelper::getProcessComponentContext());
     return WRAPPER;
 }
 
@@ -67,10 +67,9 @@ OUString extendLocalizedPath(std::u16string_view path, OUString const & locale) 
 }
 
 std::shared_ptr< comphelper::ConfigurationChanges >
-comphelper::ConfigurationChanges::create(
-    css::uno::Reference< css::uno::XComponentContext > const & context)
+comphelper::ConfigurationChanges::create()
 {
-    return GetTheConfigurationWrapper(context).createChanges();
+    return GetTheConfigurationWrapper().createChanges();
 }
 
 comphelper::ConfigurationChanges::~ConfigurationChanges() {}
@@ -107,10 +106,9 @@ comphelper::ConfigurationChanges::getSet(OUString const & path) const
 }
 
 comphelper::detail::ConfigurationWrapper const &
-comphelper::detail::ConfigurationWrapper::get(
-    css::uno::Reference< css::uno::XComponentContext > const & context)
+comphelper::detail::ConfigurationWrapper::get()
 {
-    return GetTheConfigurationWrapper(context);
+    return GetTheConfigurationWrapper();
 }
 
 comphelper::detail::ConfigurationWrapper::ConfigurationWrapper(

cui/source/dialogs/cuicharmap.cxx
@@ -352,7 +352,7 @@ void SvxCharacterMap::updateRecentCharacterList(const OUString& sTitle, const OU
         aRecentCharFontListRange[i] = maRecentCharFontList[i];
     }
 
-    std::shared_ptr<comphelper::ConfigurationChanges> batch(comphelper::ConfigurationChanges::create(mxContext));
+    std::shared_ptr<comphelper::ConfigurationChanges> batch(comphelper::ConfigurationChanges::create());
     officecfg::Office::Common::RecentCharacters::RecentCharacterList::set(aRecentCharList, batch);
     officecfg::Office::Common::RecentCharacters::RecentCharacterFontList::set(aRecentCharFontList, batch);
     batch->commit();
@@ -391,7 +391,7 @@ void SvxCharacterMap::updateFavCharacterList(const OUString& sTitle, const OUStr
         aFavCharFontListRange[i] = maFavCharFontList[i];
     }
 
-    std::shared_ptr<comphelper::ConfigurationChanges> batch(comphelper::ConfigurationChanges::create(mxContext));
+    std::shared_ptr<comphelper::ConfigurationChanges> batch(comphelper::ConfigurationChanges::create());
     officecfg::Office::Common::FavoriteCharacters::FavoriteCharacterList::set(aFavCharList, batch);
     officecfg::Office::Common::FavoriteCharacters::FavoriteCharacterFontList::set(aFavCharFontList, batch);
     batch->commit();
@@ -444,7 +444,7 @@ void SvxCharacterMap::deleteFavCharacterFromList(std::u16string_view sTitle, std
         aFavCharFontListRange[i] = maFavCharFontList[i];
     }
 
-    std::shared_ptr<comphelper::ConfigurationChanges> batch(comphelper::ConfigurationChanges::create(mxContext));
+    std::shared_ptr<comphelper::ConfigurationChanges> batch(comphelper::ConfigurationChanges::create());
     officecfg::Office::Common::FavoriteCharacters::FavoriteCharacterList::set(aFavCharList, batch);
     officecfg::Office::Common::FavoriteCharacters::FavoriteCharacterFontList::set(aFavCharFontList, batch);
     batch->commit();
@@ -799,7 +799,7 @@ IMPL_LINK(SvxCharacterMap, RecentClearClickHdl, SvxCharView*, rView, void)
         aRecentCharFontListRange[i] = maRecentCharFontList[i];
     }
 
-    std::shared_ptr<comphelper::ConfigurationChanges> batch(comphelper::ConfigurationChanges::create(mxContext));
+    std::shared_ptr<comphelper::ConfigurationChanges> batch(comphelper::ConfigurationChanges::create());
     officecfg::Office::Common::RecentCharacters::RecentCharacterList::set(aRecentCharList, batch);
     officecfg::Office::Common::RecentCharacters::RecentCharacterFontList::set(aRecentCharFontList, batch);
     batch->commit();
@@ -812,7 +812,7 @@ IMPL_LINK_NOARG(SvxCharacterMap, RecentClearAllClickHdl, SvxCharView*, void)
     maRecentCharList.clear();
     maRecentCharFontList.clear();
 
-    std::shared_ptr<comphelper::ConfigurationChanges> batch(comphelper::ConfigurationChanges::create(mxContext));
+    std::shared_ptr<comphelper::ConfigurationChanges> batch(comphelper::ConfigurationChanges::create());
     officecfg::Office::Common::RecentCharacters::RecentCharacterList::set({ }, batch);
     officecfg::Office::Common::RecentCharacters::RecentCharacterFontList::set({ }, batch);
     batch->commit();
@@ -831,7 +831,7 @@ IMPL_LINK_NOARG(SvxCharacterMap, FavClearAllClickHdl, SvxCharView*, void)
     maFavCharList.clear();
     maFavCharFontList.clear();
 
-    std::shared_ptr<comphelper::ConfigurationChanges> batch(comphelper::ConfigurationChanges::create(mxContext));
+    std::shared_ptr<comphelper::ConfigurationChanges> batch(comphelper::ConfigurationChanges::create());
     officecfg::Office::Common::FavoriteCharacters::FavoriteCharacterList::set({ }, batch);
     officecfg::Office::Common::FavoriteCharacters::FavoriteCharacterFontList::set({ }, batch);
     batch->commit();

cui/source/dialogs/toolbarmodedlg.cxx
@@ -181,7 +181,7 @@ IMPL_LINK(ToolbarmodeDialog, OnApplyClick, weld::Button&, rButton, void)
     if (&rButton == m_pApplyAll.get())
     {
         std::shared_ptr<comphelper::ConfigurationChanges> aBatch(
-            comphelper::ConfigurationChanges::create(::comphelper::getProcessComponentContext()));
+            comphelper::ConfigurationChanges::create());
         officecfg::Office::UI::ToolbarMode::ActiveWriter::set(sCmd, aBatch);
         officecfg::Office::UI::ToolbarMode::ActiveCalc::set(sCmd, aBatch);
         officecfg::Office::UI::ToolbarMode::ActiveImpress::set(sCmd, aBatch);

cui/source/options/personalization.cxx
@@ -63,11 +63,8 @@ bool SvxPersonalizationTabPage::FillItemSet(SfxItemSet*)
         aPersona = "no";
 
     bool bModified = false;
-    uno::Reference<uno::XComponentContext> xContext(comphelper::getProcessComponentContext());
-    if (xContext.is()
-        && (aPersona != officecfg::Office::Common::Misc::Persona::get(xContext)
-            || m_aPersonaSettings
-                   != officecfg::Office::Common::Misc::PersonaSettings::get(xContext)))
+    if (aPersona != officecfg::Office::Common::Misc::Persona::get()
+        || m_aPersonaSettings != officecfg::Office::Common::Misc::PersonaSettings::get())
     {
         bModified = true;
     }
@@ -94,15 +91,9 @@ bool SvxPersonalizationTabPage::FillItemSet(SfxItemSet*)
 
 void SvxPersonalizationTabPage::Reset(const SfxItemSet*)
 {
-    uno::Reference<uno::XComponentContext> xContext(comphelper::getProcessComponentContext());
-
     // persona
-    OUString aPersona("default");
-    if (xContext.is())
-    {
-        aPersona = officecfg::Office::Common::Misc::Persona::get(xContext);
-        m_aPersonaSettings = officecfg::Office::Common::Misc::PersonaSettings::get(xContext);
-    }
+    OUString aPersona = officecfg::Office::Common::Misc::Persona::get();
+    m_aPersonaSettings = officecfg::Office::Common::Misc::PersonaSettings::get();
 
     if (aPersona == "no")
         m_xNoPersona->set_active(true);

cui/source/tabpages/tpcolor.cxx
@@ -359,7 +359,7 @@ IMPL_LINK_NOARG(SvxColorTabPage, ClickAddHdl_Impl, weld::Button&, void)
     {
         m_xSelectPalette->set_active(0);
         SelectPaletteLBHdl(*m_xSelectPalette);
-        std::shared_ptr<comphelper::ConfigurationChanges> batch(comphelper::ConfigurationChanges::create(m_context));
+        std::shared_ptr<comphelper::ConfigurationChanges> batch(comphelper::ConfigurationChanges::create());
         css::uno::Sequence< sal_Int32 > aCustomColorList(officecfg::Office::Common::UserColors::CustomColor::get());
         css::uno::Sequence< OUString > aCustomColorNameList(officecfg::Office::Common::UserColors::CustomColorName::get());
         sal_Int32 nSize = aCustomColorList.getLength();
@@ -409,7 +409,7 @@ IMPL_LINK_NOARG(SvxColorTabPage, ClickDeleteHdl_Impl, weld::Button&, void)
     if (m_xSelectPalette->get_active() != 0 || nPos == VALUESET_ITEM_NOTFOUND)
         return;
 
-    std::shared_ptr<comphelper::ConfigurationChanges> batch(comphelper::ConfigurationChanges::create(m_context));
+    std::shared_ptr<comphelper::ConfigurationChanges> batch(comphelper::ConfigurationChanges::create());
     css::uno::Sequence< sal_Int32 > aCustomColorList(officecfg::Office::Common::UserColors::CustomColor::get());
     auto aCustomColorListRange = asNonConstRange(aCustomColorList);
     css::uno::Sequence< OUString > aCustomColorNameList(officecfg::Office::Common::UserColors::CustomColorName::get());

desktop/inc/app.hxx
@@ -123,8 +123,7 @@ class Desktop final : public Application
             // throws an exception upon failure
 
     private:
-        void                    RegisterServices(
-                                    css::uno::Reference< css::uno::XComponentContext > const & context);
+        void                    RegisterServices();
         static void             DeregisterServices();
 
     public:

desktop/source/app/app.cxx
@@ -1305,7 +1305,7 @@ int Desktop::Main()
 
     Reference< XDesktop2 > xDesktop;
 
-    RegisterServices(xContext);
+    RegisterServices();
 
     SetSplashScreenProgress(25);
 

commit b68c08ffa958afbe4aea750f12ab36de26955f2a

Solve exception in implts_flushConfigItem
Regression from 4256c764aee0777770466115a97420d9b55c23ac
in framework/source/services/autorecovery.cxx
in line 1997 sloved the exception

Change-Id: I4bee90b94307978c9c5186c1c5fdaca1aa420e21

framework/source/services/autorecovery.cxx
@@ -1992,7 +1992,7 @@ void AutoRecovery::implts_flushConfigItem(const AutoRecovery::TDocumentInfo& rIn
         implts_openConfig();
 
         css::uno::Reference<css::container::XNameAccess> xCheck(
-                officecfg::Office::Recovery::RecoveryList::get());
+                officecfg::Office::Recovery::RecoveryList::get(batch));
 
         css::uno::Reference< css::container::XNameContainer >   xModify(xCheck, css::uno::UNO_QUERY_THROW);
         css::uno::Reference< css::lang::XSingleServiceFactory > xCreate(xCheck, css::uno::UNO_QUERY_THROW);