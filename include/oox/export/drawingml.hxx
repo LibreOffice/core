37c41f2c445ecf519f4137c23fb36f3942328b6b
Miklos Vajna <vmiklos@collabora.com>
2022-03-23 20:06:49 +0100

    sd theme: add PPTX export for shape fill color
    Do this only in case there are no effects on the color, as that is not
    yet handled.
    
    Note that the theme color was already stored in the grab-bag, so this is
    primarily interesting in case the theme color was changed or the source
    format was ODP.
    
    Change-Id: Ia4995be68d5f243875632eec4aaf8afbb8f4d5cc
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/131984
    Tested-by: Jenkins
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

--- include/oox/export/drawingml.hxx
+++ include/oox/export/drawingml.hxx
@@ -224,6 +224,7 @@ public:
     void WriteConnectorConnections( EscherConnectorListEntry& rConnectorEntry, sal_Int32 nStartID, sal_Int32 nEndID );
 
     bool WriteCharColor(const css::uno::Reference<css::beans::XPropertySet>& xPropertySet);
+    bool WriteFillColor(const css::uno::Reference<css::beans::XPropertySet>& xPropertySet);
 
     void WriteSolidFill( ::Color nColor, sal_Int32 nAlpha = MAX_PERCENT );
     void WriteSolidFill( const OUString& sSchemeName, const css::uno::Sequence< css::beans::PropertyValue >& aTransformations, sal_Int32 nAlpha = MAX_PERCENT );
