ba45318c942a0ed399d23b20bf82d7bd0ffa45ea
Luboš Luňák <l.lunak@collabora.com>
2022-03-29 20:44:10 +0200

    allocate columns for initDataRows()
    Triggered by TestSort::testSortImages() with INITIALCOLCOUNT set to 1.
    
    Change-Id: Ifd3e63de6411e0a4d8776ed6cc8a7b6c7c64eec6
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/132283
    Tested-by: Jenkins
    Reviewed-by: Luboš Luňák <l.lunak@collabora.com>

--- sc/source/core/data/table3.cxx
+++ sc/source/core/data/table3.cxx
@@ -476,6 +476,7 @@ std::unique_ptr<ScSortInfoArray> ScTable::CreateSortInfoArray( const sc::Reorder
         pArray->SetKeepQuery(rParam.mbHiddenFiltered);
         pArray->SetUpdateRefs(rParam.mbUpdateRefs);
 
+        CreateColumnIfNotExists(nCol2);
         initDataRows( *pArray, *this, aCol, nCol1, nRow1, nCol2, nRow2, rParam.mbHiddenFiltered,
                 rParam.maDataAreaExtras.mbCellFormats, true, true, false);
     }
@@ -519,6 +520,7 @@ std::unique_ptr<ScSortInfoArray> ScTable::CreateSortInfoArray(
             }
         }
 
+        CreateColumnIfNotExists(rSortParam.nCol2);
         initDataRows( *pArray, *this, aCol, rSortParam.nCol1, nInd1, rSortParam.nCol2, nInd2, bKeepQuery,
                 rSortParam.aDataAreaExtras.mbCellFormats, true, true, false);
     }
@@ -906,6 +908,7 @@ void ScTable::SortReorderAreaExtrasByRow( ScSortInfoArray* pArray,
     for (SCCOL nCol = rDataAreaExtras.mnStartCol; nCol < nDataCol1; nCol += nChunkCols)
     {
         const SCCOL nEndCol = std::min<SCCOL>( nCol + nChunkCols - 1, nDataCol1 - 1);
+        CreateColumnIfNotExists(nEndCol);
         initDataRows( *pArray, *this, aCol, nCol, nRow1, nEndCol, nLastRow, false,
                 rDataAreaExtras.mbCellFormats, rDataAreaExtras.mbCellNotes, rDataAreaExtras.mbCellDrawObjects, true);
         SortReorderByRow( pArray, nCol, nEndCol, pProgress, true);
@@ -914,6 +917,7 @@ void ScTable::SortReorderAreaExtrasByRow( ScSortInfoArray* pArray,
     for (SCCOL nCol = nDataCol2 + 1; nCol <= rDataAreaExtras.mnEndCol; nCol += nChunkCols)
     {
         const SCCOL nEndCol = std::min<SCCOL>( nCol + nChunkCols - 1, rDataAreaExtras.mnEndCol);
+        CreateColumnIfNotExists(nEndCol);
         initDataRows( *pArray, *this, aCol, nCol, nRow1, nEndCol, nLastRow, false,
                 rDataAreaExtras.mbCellFormats, rDataAreaExtras.mbCellNotes, rDataAreaExtras.mbCellDrawObjects, true);
         SortReorderByRow( pArray, nCol, nEndCol, pProgress, true);
