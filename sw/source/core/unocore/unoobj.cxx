e83db4b7c3475591a6d31a578137273b5800cda9
Sarper Akdemir <sarper.akdemir@collabora.com>
2022-03-30 17:02:30 +0300

    tdf#148273 docx import: fix section break format leak to bullets
    Fixes RES_PARATR_LIST_AUTOFMT leaking into the next section.
    
    Achieves this by resetting list related attributes on the cursor's
    text node in DomainMapper_Impl::RemoveLastParagraph() after the
    deletion of the paragraph.
    
    Change-Id: Ib4d09c5f190b8b8fd3bdc119ddd57d91f353de2f

--- sw/source/core/unocore/unoobj.cxx
+++ sw/source/core/unocore/unoobj.cxx
@@ -2081,6 +2081,15 @@ SwXTextCursor::setPropertyValue(
         }
         rUnoCursor.SetSkipOverProtectSections(bSet);
     }
+    else if (rPropertyName == UNO_NAME_RESET_PARAGRAPH_LIST_ATTRIBUTES)
+    {
+        SwTextNode* pTextNode= GetPaM()->GetNode().GetTextNode();
+
+        if(pTextNode)
+        {
+            pTextNode->ResetAttr(RES_PARATR_LIST_BEGIN, RES_PARATR_LIST_END);
+        }
+    }
     else
     {
         SwUnoCursorHelper::SetPropertyValue(rUnoCursor,
