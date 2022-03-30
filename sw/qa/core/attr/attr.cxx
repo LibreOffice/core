cbeafdca810107a210d3068ac5e65d27bc035817
Miklos Vajna <vmiklos@collabora.com>
2022-03-30 11:20:12 +0200

    sw: fix crash in SwAttrSet::CopyToModify()
    Regression from commit 938a4d6624a78f3e272b3c4c07f314cb0c6db723
    (tdf#128375 sw: fix copying RES_PARATR_LIST_AUTOFMT to different SwDoc,
    2019-11-01), the problem is that we may get an autofmt pool item which
    is set, but its style handle is empty. Assume that this is the same case
    as having no autofmt at all.
    
    (cherry picked from commit ddfafd9b67e6e61c144b29ac73bbafb413ddf03b)
    
    Conflicts:
    	sw/Module_sw.mk
    	sw/source/core/attr/swatrset.cxx
    
    Change-Id: I87494fd04687d31201b4ec712cb0fb1ec7362b46
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/132189
    Tested-by: Jenkins
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

---
+++ sw/qa/core/attr/attr.cxx
@@ -0,0 +1,53 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <swmodeltestbase.hxx>
+
+#include <docsh.hxx>
+#include <swdtflvr.hxx>
+#include <swmodule.hxx>
+
+namespace
+{
+constexpr OUStringLiteral DATA_DIRECTORY = u"/sw/qa/core/attr/data/";
+
+/// Covers sw/source/core/attr/ fixes.
+class Test : public SwModelTestBase
+{
+};
+
+CPPUNIT_TEST_FIXTURE(Test, testSwAttrSet)
+{
+    // Given a document with track changes and the whole document is selected:
+    SwDoc* pDoc = createSwDoc(DATA_DIRECTORY, "attr-set.docx");
+    SwDocShell* pDocShell = pDoc->GetDocShell();
+    SwWrtShell* pWrtShell = pDocShell->GetWrtShell();
+    dispatchCommand(mxComponent, ".uno:SelectAll", {});
+    dispatchCommand(mxComponent, ".uno:Cut", {});
+    dispatchCommand(mxComponent, ".uno:SelectAll", {});
+    rtl::Reference<SwTransferable> xTransferable(new SwTransferable(*pWrtShell));
+    SwModule* pMod = SW_MOD();
+    SwTransferable* pOldTransferable = pMod->m_pXSelection;
+    pMod->m_pXSelection = xTransferable.get();
+
+    // When getting the plain text version of the selection:
+    datatransfer::DataFlavor aFlavor;
+    aFlavor.MimeType = "text/plain;charset=utf-16";
+    aFlavor.DataType = cppu::UnoType<OUString>::get();
+    uno::Any aData = xTransferable->getTransferData(aFlavor);
+
+    // Then make sure we get data without crashing:
+    CPPUNIT_ASSERT(aData.hasValue());
+    pMod->m_pXSelection = pOldTransferable;
+}
+}
+
+CPPUNIT_PLUGIN_IMPLEMENT();
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
