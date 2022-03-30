8e2152d5c73605aaeaaf8ed3ff222d94f3ed177b
Miklos Vajna <vmiklos@collabora.com>
2022-03-30 11:20:12 +0200

    sw: fix crash in SwAttrSet::CopyToModify()
    Regression from commit 938a4d6624a78f3e272b3c4c07f314cb0c6db723
    (tdf#128375 sw: fix copying RES_PARATR_LIST_AUTOFMT to different SwDoc,
    2019-11-01), the problem is that we may get an autofmt pool item which
    is set, but its style handle is empty. Assume that this is the same case
    as having no autofmt at all.
    
    (cherry picked from commit ddfafd9b67e6e61c144b29ac73bbafb413ddf03b)
    
    Conflicts:
    	sw/Module_sw.mk
    	sw/source/core/attr/swatrset.cxx
    
    [ Help backporting by also picking a single hunk from commit
    1ad26c9fc237e00247f18fcc8ccc778fba88d1fd (loplugin:referencecasting add
    check for new rtl::Reference operator, 2021-02-11) as well. ]
    
    Change-Id: I87494fd04687d31201b4ec712cb0fb1ec7362b46
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/132321
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

--- sw/Module_sw.mk
+++ sw/Module_sw.mk
@@ -118,6 +118,7 @@ $(eval $(call gb_Module_add_slowcheck_targets,sw,\
     CppunitTest_sw_core_crsr \
     CppunitTest_sw_uibase_uno \
     CppunitTest_sw_core_edit \
+    CppunitTest_sw_core_attr \
 ))
 
 ifneq ($(DISABLE_GUI),TRUE)
