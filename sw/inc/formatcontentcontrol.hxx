58607e6f6738925832a7634db80a76340637e172
Miklos Vajna <vmiklos@collabora.com>
2022-03-30 08:16:21 +0200

    sw content controls: add document model
    This is meant to represent inline structured document tags (<w:sdt>
    elements in DOCX) or content controls (as Word users know this).
    
    Don't confuse this with block-level, cell-level or table-level content
    controls, which are not covered here.
    
    You may wonder why fields or fieldmarks can't be used to represent this.
    The problems are:
    
    - a fieldmark can contain a paragraph break, inline content controls
      can't
    
    - content controls must be a well-formed XML element, while fieldmarks
      can start/end at random document model positions
    
    - fieldmarks are supposed to have a field command and a result result
      (with a separator between the two), but content controls don't have a
      field command
    
    - mapping to a field has the problem that Writer fields can only have a
      single text portion / run, but content controls can have multiple ones
    
    So model content controls with a meta-like text attribute which covers
    all these cases.
    
    SwContentControl is mostly empty as a start, it can track more data in
    follow-up commits. SwTextContentControl and SwFormatContentControl are
    the matching text attr and pool item subclasses.
    
    (cherry picked from commit 3dd4f3691458ea537bc1867386269694775cfbcb)
    
    Change-Id: I1e57f7756cf87041975371483ec4fc8abf875dfe

---
+++ sw/inc/formatcontentcontrol.hxx
@@ -0,0 +1,104 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#pragma once
+
+#include <com/sun/star/text/XTextContent.hpp>
+
+#include <cppuhelper/weakref.hxx>
+#include <svl/poolitem.hxx>
+
+#include "calbck.hxx"
+
+class SwContentControl;
+class SwTextContentControl;
+class SwTextNode;
+class SwXContentControl;
+
+/// SfxPoolItem subclass that wraps an SwContentControl.
+class SwFormatContentControl final : public SfxPoolItem
+{
+    std::shared_ptr<SwContentControl> m_pContentControl;
+    SwTextContentControl* m_pTextAttr;
+
+public:
+    SwTextContentControl* GetTextAttr() { return m_pTextAttr; }
+    void SetTextAttr(SwTextContentControl* pTextAttr);
+
+    /// This method must be called when the hint is actually copied.
+    void DoCopy(SwTextNode& rTargetTextNode);
+
+    explicit SwFormatContentControl(sal_uInt16 nWhich);
+
+    explicit SwFormatContentControl(const std::shared_ptr<SwContentControl>& pContentControl,
+                                    sal_uInt16 nWhich);
+    ~SwFormatContentControl() override;
+
+    /// SfxPoolItem
+    bool operator==(const SfxPoolItem&) const override;
+    SwFormatContentControl* Clone(SfxItemPool* pPool = nullptr) const override;
+
+    /**
+     * Notify clients registered at m_pContentControl that this content control is being
+     * (re-)moved.
+     */
+    void NotifyChangeTextNode(SwTextNode* pTextNode);
+    static SwFormatContentControl* CreatePoolDefault(sal_uInt16 nWhich);
+    SwContentControl* GetContentControl() { return m_pContentControl.get(); }
+};
+
+/// Stores the properties of a content control.
+class SwContentControl : public sw::BroadcastingModify
+{
+    css::uno::WeakReference<css::text::XTextContent> m_wXContentControl;
+
+    SwFormatContentControl* m_pFormat;
+
+    /// Can be nullptr if not in a document for undo purposes.
+    SwTextNode* m_pTextNode;
+
+public:
+    SwTextContentControl* GetTextAttr() const;
+
+    SwTextNode* GetTextNode() const { return m_pTextNode; }
+
+    SwFormatContentControl* GetFormatContentControl() const { return m_pFormat; }
+
+    void SetFormatContentControl(SwFormatContentControl* pFormat) { m_pFormat = pFormat; };
+
+    void NotifyChangeTextNode(SwTextNode* pTextNode);
+
+    css::uno::WeakReference<css::text::XTextContent> GetXContentControl() const
+    {
+        return m_wXContentControl;
+    }
+
+    void SetXContentControl(const css::uno::Reference<css::text::XTextContent>& xContentCnotrol)
+    {
+        m_wXContentControl = xContentCnotrol;
+    }
+
+    virtual void SwClientNotify(const SwModify&, const SfxHint&) override;
+
+    explicit SwContentControl(SwFormatContentControl* pFormat);
+
+    virtual ~SwContentControl() override;
+};
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
