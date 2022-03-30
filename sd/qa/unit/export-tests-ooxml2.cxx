5d46ec11f6405433ec996c18e471d3f61d7d5113
Tünde Tóth <toth.tunde@nisz.hu>
2022-03-30 09:52:57 +0200

    tdf#53970 PPTX: add unittest
    Change-Id: I1bcf8f79213245fdf135d6d3b2aafea6ed99f5b3
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/132296
    Tested-by: Jenkins
    Tested-by: László Németh <nemeth@numbertext.org>
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Reviewed-by: László Németh <nemeth@numbertext.org>
    Signed-off-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/132320

--- sd/qa/unit/export-tests-ooxml2.cxx
+++ sd/qa/unit/export-tests-ooxml2.cxx
@@ -152,6 +152,7 @@ public:
     void testTextColumns_tdf140852();
     void testTextColumns_3columns();
     void testTdf59323_slideFooters();
+    void testTdf53970();
 
     CPPUNIT_TEST_SUITE(SdOOXMLExportTest2);
 
@@ -218,6 +219,7 @@ public:
     CPPUNIT_TEST(testTextColumns_tdf140852);
     CPPUNIT_TEST(testTextColumns_3columns);
     CPPUNIT_TEST(testTdf59323_slideFooters);
+    CPPUNIT_TEST(testTdf53970);
 
     CPPUNIT_TEST_SUITE_END();
 
@@ -1806,6 +1808,19 @@ void SdOOXMLExportTest2::testTdf59323_slideFooters()
     // - Expected: 1
     // - Actual  : 0
     assertXPath(pXmlDocSlide1, "/p:sld/p:cSld/p:spTree/p:sp/p:txBody/a:p/a:fld/a:rPr");
+
+    xDocShRef->DoClose();
+}
+
+void SdOOXMLExportTest2::testTdf53970()
+{
+    ::sd::DrawDocShellRef xDocShRef
+        = loadURL(m_directories.getURLFromSrc(u"/sd/qa/unit/data/odp/tdf53970.odp"), ODP);
+    xDocShRef = saveAndReload(xDocShRef.get(), PPTX);
+
+
+    // Without fix in place, the media shape was lost on export.
+    CPPUNIT_ASSERT(getPage(0, xDocShRef)->hasElements());
 
     xDocShRef->DoClose();
 }
