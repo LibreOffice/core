LibreOffice/core

commit 1da69081732c8a429840edaaf10cfb789ea68df8

add string_view variants of methods to O[U]StringBuffer
and update the stringview loplugin to detect cases where we can
use these new methods.

Change-Id: I998efe02e35c8efcb3abfb4d7186165bbe6dfb2c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/110046
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

include/rtl/strbuf.hxx
@@ -132,6 +132,17 @@ public:
 
         @param   value   the initial string value.
      */
+#if defined LIBO_INTERNAL_ONLY
+    explicit OStringBuffer(std::string_view sv)
+        : pData(nullptr)
+        , nCapacity( sv.length() + 16 )
+    {
+        if (sv.size() > sal_uInt32(std::numeric_limits<sal_Int32>::max())) {
+            throw std::bad_alloc();
+        }
+        rtl_stringbuffer_newFromStr_WithLength( &pData, sv.data(), sv.length() );
+    }
+#endif
     OStringBuffer(const OString& value)
         : pData(NULL)
         , nCapacity( value.getLength() + 16 )
@@ -251,6 +262,17 @@ public:
 
         @since LibreOffice 5.3
     */
+#if defined LIBO_INTERNAL_ONLY
+    OStringBuffer & operator =(std::string_view string) {
+        sal_Int32 n = string.length();
+        if (n >= nCapacity) {
+            ensureCapacity(n + 16); //TODO: check for overflow
+        }
+        std::memcpy(pData->buffer, string.data(), n + 1);
+        pData->length = n;
+        return *this;
+    }
+#endif
     OStringBuffer & operator =(OString const & string) {
         sal_Int32 n = string.getLength();
         if (n >= nCapacity) {
@@ -764,6 +786,12 @@ public:
         @param      str      a string.
         @return     this string buffer.
      */
+#if defined LIBO_INTERNAL_ONLY
+    OStringBuffer & insert(sal_Int32 offset, std::string_view str)
+    {
+        return insert( offset, str.data(), str.length() );
+    }
+#endif
     OStringBuffer & insert(sal_Int32 offset, const OString & str)
     {
         return insert( offset, str.getStr(), str.getLength() );

include/rtl/ustrbuf.hxx
@@ -133,6 +133,17 @@ public:
 
         @param   value   the initial contents of the buffer.
      */
+#if defined LIBO_INTERNAL_ONLY
+    explicit OUStringBuffer(std::u16string_view sv)
+        : pData(nullptr)
+        , nCapacity( sv.length() + 16 )
+    {
+        if (sv.size() > sal_uInt32(std::numeric_limits<sal_Int32>::max())) {
+            throw std::bad_alloc();
+        }
+        rtl_uStringbuffer_newFromStr_WithLength( &pData, sv.data(), sv.length() );
+    }
+#endif
     OUStringBuffer(const OUString& value)
         : pData(NULL)
         , nCapacity( value.getLength() + 16 )
@@ -257,6 +268,19 @@ public:
 
         @since LibreOffice 5.3
     */
+#if defined LIBO_INTERNAL_ONLY
+    OUStringBuffer & operator =(std::u16string_view string) {
+        sal_Int32 n = string.length();
+        if (n >= nCapacity) {
+            ensureCapacity(n + 16); //TODO: check for overflow
+        }
+        std::memcpy(
+            pData->buffer, string.data(),
+            (n + 1) * sizeof (sal_Unicode));
+        pData->length = n;
+        return *this;
+    }
+#endif
     OUStringBuffer & operator =(OUString const & string) {
         sal_Int32 n = string.getLength();
         if (n >= nCapacity) {
@@ -955,6 +979,12 @@ public:
         @param      str      a string.
         @return     this string buffer.
      */
+#if defined LIBO_INTERNAL_ONLY
+    OUStringBuffer & insert(sal_Int32 offset, std::u16string_view str)
+    {
+        return insert( offset, str.data(), str.length() );
+    }
+#endif
     OUStringBuffer & insert(sal_Int32 offset, const OUString & str)
     {
         return insert( offset, str.getStr(), str.getLength() );
@@ -1406,6 +1436,15 @@ public:
                  returned. If it does not occur as a substring starting
                  at fromIndex or beyond, -1 is returned.
     */
+#if defined LIBO_INTERNAL_ONLY
+    sal_Int32 indexOf( std::u16string_view str, sal_Int32 fromIndex = 0 ) const
+    {
+        assert( fromIndex >= 0 && fromIndex <= pData->length );
+        sal_Int32 ret = rtl_ustr_indexOfStr_WithLength( pData->buffer+fromIndex, pData->length-fromIndex,
+                                                        str.data(), str.length() );
+        return (ret < 0 ? ret : ret+fromIndex);
+    }
+#endif
     sal_Int32 indexOf( const OUString & str, sal_Int32 fromIndex = 0 ) const
     {
         assert( fromIndex >= 0 && fromIndex <= pData->length );
@@ -1475,6 +1514,13 @@ public:
                  the last such substring is returned. If it does not occur as
                  a substring, -1 is returned.
     */
+#if defined LIBO_INTERNAL_ONLY
+    sal_Int32 lastIndexOf( std::u16string_view str ) const
+    {
+        return rtl_ustr_lastIndexOfStr_WithLength( pData->buffer, pData->length,
+                                                   str.data(), str.length() );
+    }
+#endif
     sal_Int32 lastIndexOf( const OUString & str ) const
     {
         return rtl_ustr_lastIndexOfStr_WithLength( pData->buffer, pData->length,
@@ -1607,6 +1653,47 @@ public:
     {
         return stripStart(c) + stripEnd(c);
     }
+
+#if defined LIBO_INTERNAL_ONLY
+    /**
+      Returns a std::u16string_view that is a view of a substring of this string.
+
+      The substring begins at the specified beginIndex. If
+      beginIndex is negative or be greater than the length of
+      this string, behaviour is undefined.
+
+      @param     beginIndex   the beginning index, inclusive.
+      @return    the specified substring.
+    */
+    SAL_WARN_UNUSED_RESULT std::u16string_view subView( sal_Int32 beginIndex ) const
+    {
+        assert(beginIndex >= 0);
+        assert(beginIndex <= getLength());
+        return subView(beginIndex, getLength() - beginIndex);
+    }
+
+    /**
+      Returns a std::u16string_view that is a view of a substring of this string.
+
+      The substring begins at the specified beginIndex and contains count
+      characters.  If either beginIndex or count are negative,
+      or beginIndex + count are greater than the length of this string
+      then behaviour is undefined.
+
+      @param     beginIndex   the beginning index, inclusive.
+      @param     count        the number of characters.
+      @return    the specified substring.
+    */
+    SAL_WARN_UNUSED_RESULT std::u16string_view subView( sal_Int32 beginIndex, sal_Int32 count ) const
+    {
+        assert(beginIndex >= 0);
+        assert(count >= 0);
+        assert(beginIndex <= getLength());
+        assert(count <= getLength() - beginIndex);
+        return std::u16string_view(pData->buffer, sal_uInt32(pData->length)).substr(beginIndex, count);
+    }
+#endif
+
     /**
       Returns a new string buffer that is a substring of this string.
 

chart2/source/tools/ObjectIdentifier.cxx
@@ -170,7 +170,7 @@ OUString lcl_getIndexStringAfterString( const OUString& rString, const OUString&
         sal_Int32 nNextColon = rString.indexOf( ':', nIndexStart );
         if( nNextColon != -1 )
             nIndexEnd = nNextColon;
-        aRet = rString.copy(nIndexStart,nIndexEnd-nIndexStart);
+        aRet = rString.subView(nIndexStart,nIndexEnd-nIndexStart);
     }
 
     return aRet.makeStringAndClear();

compilerplugins/clang/stringview.cxx
@@ -51,8 +51,11 @@ public:
     bool VisitFunctionDecl(FunctionDecl const*);
     bool VisitCXXOperatorCallExpr(CXXOperatorCallExpr const*);
     bool VisitImplicitCastExpr(ImplicitCastExpr const*);
+    bool VisitCXXMemberCallExpr(CXXMemberCallExpr const*);
+    bool VisitCXXConstructExpr(CXXConstructExpr const*);
 
 private:
+    void handleSubExprThatCouldBeView(Expr const* expr);
     void handleCXXConstructExpr(CXXConstructExpr const* expr);
     void handleCXXMemberCallExpr(CXXMemberCallExpr const* expr);
 };
@@ -88,6 +91,20 @@ bool StringView::VisitCXXOperatorCallExpr(CXXOperatorCallExpr const* cxxOperator
         check(cxxOperatorCallExpr->getArg(1));
     else if (op == OO_Subscript)
         check(cxxOperatorCallExpr->getArg(0));
+    else if (op == OO_Equal)
+    {
+        if (loplugin::TypeCheck(cxxOperatorCallExpr->getType())
+                .Class("OUStringBuffer")
+                .Namespace("rtl")
+                .GlobalNamespace()
+            || loplugin::TypeCheck(cxxOperatorCallExpr->getType())
+                   .Class("OStringBuffer")
+                   .Namespace("rtl")
+                   .GlobalNamespace())
+        {
+            check(cxxOperatorCallExpr->getArg(1));
+        }
+    }
     return true;
 }
 
@@ -111,12 +128,18 @@ bool StringView::VisitImplicitCastExpr(ImplicitCastExpr const* expr)
     {
         return true;
     }
-    auto const e = expr->getSubExprAsWritten()->IgnoreParens();
+    handleSubExprThatCouldBeView(expr->getSubExprAsWritten());
+    return true;
+}
+
+void StringView::handleSubExprThatCouldBeView(Expr const* subExpr)
+{
+    auto const e = subExpr->IgnoreParens();
     auto const tc = loplugin::TypeCheck(e->getType());
     if (!(tc.Class("OString").Namespace("rtl").GlobalNamespace()
           || tc.Class("OUString").Namespace("rtl").GlobalNamespace()))
     {
-        return true;
+        return;
     }
     if (auto const e1 = dyn_cast<CXXConstructExpr>(e))
     {
@@ -138,7 +161,6 @@ bool StringView::VisitImplicitCastExpr(ImplicitCastExpr const* expr)
     {
         handleCXXMemberCallExpr(e3);
     }
-    return true;
 }
 
 void StringView::handleCXXConstructExpr(CXXConstructExpr const* expr)
@@ -227,6 +249,61 @@ void StringView::handleCXXMemberCallExpr(CXXMemberCallExpr const* expr)
     report(DiagnosticsEngine::Warning, "rather than copy, pass with a view using subView()",
            expr->getExprLoc())
         << expr->getSourceRange();
+}
+
+/** check for calls to O[U]StringBuffer::append that could be passed as a
+    std::u16string_view */
+bool StringView::VisitCXXMemberCallExpr(CXXMemberCallExpr const* expr)
+{
+    if (ignoreLocation(expr))
+    {
+        return true;
+    }
+    if (!loplugin::TypeCheck(expr->getType())
+             .Class("OUStringBuffer")
+             .Namespace("rtl")
+             .GlobalNamespace()
+        && !loplugin::TypeCheck(expr->getType())
+                .Class("OStringBuffer")
+                .Namespace("rtl")
+                .GlobalNamespace())
+    {
+        return true;
+    }
+    auto const dc = loplugin::DeclCheck(expr->getMethodDecl());
+    if (dc.Function("append") || dc.Function("indexOf") || dc.Function("lastIndexOf"))
+    {
+        handleSubExprThatCouldBeView(compat::IgnoreImplicit(expr->getArg(0)));
+    }
+    else if (dc.Function("insert"))
+    {
+        handleSubExprThatCouldBeView(compat::IgnoreImplicit(expr->getArg(1)));
+    }
+    return true;
+}
+
+/** check for calls to O[U]StringBuffer constructor that could be passed as a
+    std::u16string_view */
+bool StringView::VisitCXXConstructExpr(CXXConstructExpr const* expr)
+{
+    if (ignoreLocation(expr))
+    {
+        return true;
+    }
+    if (!loplugin::TypeCheck(expr->getType())
+             .Class("OUStringBuffer")
+             .Namespace("rtl")
+             .GlobalNamespace()
+        && !loplugin::TypeCheck(expr->getType())
+                .Class("OStringBuffer")
+                .Namespace("rtl")
+                .GlobalNamespace())
+    {
+        return true;
+    }
+    if (expr->getNumArgs() > 0)
+        handleSubExprThatCouldBeView(compat::IgnoreImplicit(expr->getArg(0)));
+    return true;
 }
 
 loplugin::Plugin::Registration<StringView> stringview("stringview");

compilerplugins/clang/test/stringview.cxx
@@ -140,4 +140,16 @@ void f5(char const* s1, sal_Int32 n1, char16_t const* s2, sal_Int32 n2)
     call_view(OUString(OUString::number(0)));
 }
 
+void f5(OUString s)
+{
+    // expected-error@+1 {{rather than copy, pass with a view using subView() [loplugin:stringview]}}
+    OUStringBuffer buf(s.copy(5));
+    // expected-error@+1 {{rather than copy, pass with a view using subView() [loplugin:stringview]}}
+    buf = s.copy(5);
+    // expected-error@+1 {{rather than copy, pass with a view using subView() [loplugin:stringview]}}
+    buf.append(s.copy(12));
+    // expected-error@+1 {{instead of an 'rtl::OUString', pass a 'std::u16string_view' [loplugin:stringview]}}
+    buf.append(OUString(std::u16string_view(u"foo")));
+}
+
 /* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */

cui/source/dialogs/FontFeaturesDialog.cxx
@@ -182,7 +182,7 @@ OUString FontFeaturesDialog::createFontNameWithFeatures()
             if (sal_uInt32(rItem.m_xCheck->get_active()) != rItem.m_nDefault)
             {
                 if (!bFirst)
-                    sNameSuffix.append(OUString(vcl::font::FeatureSeparator));
+                    sNameSuffix.append(vcl::font::FeatureSeparator);
                 else
                     bFirst = false;
 
@@ -197,7 +197,7 @@ OUString FontFeaturesDialog::createFontNameWithFeatures()
             if (nSelection != int(rItem.m_nDefault))
             {
                 if (!bFirst)
-                    sNameSuffix.append(OUString(vcl::font::FeatureSeparator));
+                    sNameSuffix.append(vcl::font::FeatureSeparator);
                 else
                     bFirst = false;
 

extensions/source/propctrlr/browserline.cxx
@@ -188,7 +188,7 @@ namespace pcr
         if (AllSettings::GetLayoutRTL())
         {
             sal_Unicode const cRTL_mark = 0x200F;
-            aText.append( OUString(cRTL_mark) );
+            aText.append( cRTL_mark );
         }
 
         m_xFtTitle->set_label(aText.makeStringAndClear());

framework/source/accelerators/acceleratorconfiguration.cxx
@@ -63,7 +63,7 @@ namespace framework
     static OUString lcl_getKeyString(const css::awt::KeyEvent& aKeyEvent)
     {
         const sal_Int32 nBeginIndex = 4; // "KEY_" is the prefix of an identifier...
-        OUStringBuffer sKeyBuffer((KeyMapping::get().mapCodeToIdentifier(aKeyEvent.KeyCode)).copy(nBeginIndex));
+        OUStringBuffer sKeyBuffer((KeyMapping::get().mapCodeToIdentifier(aKeyEvent.KeyCode)).subView(nBeginIndex));
 
         if ( (aKeyEvent.Modifiers & css::awt::KeyModifier::SHIFT) == css::awt::KeyModifier::SHIFT )
             sKeyBuffer.append("_SHIFT");

commit 47ceb8518ebde1b53749b94a125462e99f46350d

A string_view is not necessarily NUL-terminated
Regression introduced into LIBO_INTERNAL_ONLY code with
1da69081732c8a429840edaaf10cfb789ea68df8 "add string_view variants of methods to
O[U]StringBuffer".

And add some tests.  Assigning an OStringChar to an OStringBuffer also uses the
std::string_view assignment operator, so also test that with testOStringChar,
even though there it is relatively likely that the OStringChar temporary is
followed by null bytes, which could make the test happen to erroneously succeed.
But at least tools like ASan or Valgrind could catch that.  On the other hand,
assigning an OUStringChar to an OUStringBuffer does not use the
std::u16string_view assignment operator (and rather uses a
ConstCharArrayDetector-based one, which was similarly broken and has been fixed
in b66387574ef9c83cbfff622468496b6f0ac4d571 "Fix -Werror=array-bounds"), so
there's no test for that here.

Change-Id: I7cf10ee5ce0e4280a91d116cd82d4871a0f44af6

include/rtl/strbuf.hxx
@@ -277,7 +277,8 @@ public:
         if (n >= nCapacity) {
             ensureCapacity(n + 16); //TODO: check for overflow
         }
-        std::memcpy(pData->buffer, string.data(), n + 1);
+        std::memcpy(pData->buffer, string.data(), n);
+        pData->buffer[n] = '\0';
         pData->length = n;
         return *this;
     }

include/rtl/ustrbuf.hxx
@@ -294,7 +294,8 @@ public:
         }
         std::memcpy(
             pData->buffer, string.data(),
-            (n + 1) * sizeof (sal_Unicode));
+            n * sizeof (sal_Unicode));
+        pData->buffer[n] = '\0';
         pData->length = n;
         return *this;
     }

sal/CppunitTest_sal_rtl.mk
@@ -37,6 +37,7 @@ $(eval $(call gb_CppunitTest_add_exception_objects,sal_rtl,\
 	sal/qa/rtl/strings/test_ostring \
 	sal/qa/rtl/strings/test_ostring_concat \
 	sal/qa/rtl/strings/test_ostring_stringliterals \
+	sal/qa/rtl/strings/test_ostringbuffer \
 	sal/qa/rtl/strings/test_oustring_compare \
 	sal/qa/rtl/strings/test_oustring_concat \
 	sal/qa/rtl/strings/test_oustring_convert \

sal/qa/rtl/oustringbuffer/test_oustringbuffer_assign.cxx
@@ -9,6 +9,8 @@
 
 #include <sal/config.h>
 
+#include <string_view>
+
 #include <cppunit/TestFixture.h>
 #include <cppunit/TestAssert.h>
 #include <cppunit/extensions/HelperMacros.h>
@@ -71,6 +73,12 @@ private:
         CPPUNIT_ASSERT_EQUAL(sal_Int32(1), b4.getLength());
         CPPUNIT_ASSERT_EQUAL(u'a', b4.getStr()[0]);
         CPPUNIT_ASSERT_EQUAL(u'\0', b4.getStr()[1]);
+        b4 = std::u16string_view(u"abc").substr(
+            0, 2); // avoid the string_view accidentally being NUL-terminated
+        CPPUNIT_ASSERT_EQUAL(sal_Int32(2), b4.getLength());
+        CPPUNIT_ASSERT_EQUAL(u'a', b4.getStr()[0]);
+        CPPUNIT_ASSERT_EQUAL(u'b', b4.getStr()[1]);
+        CPPUNIT_ASSERT_EQUAL(u'\0', b4.getStr()[2]);
     }
 
     CPPUNIT_TEST_SUITE(Test);

sal/qa/rtl/strings/test_ostringbuffer.cxx
@@ -1,0 +1,56 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <sal/config.h>
+
+#include <string_view>
+
+#include <cppunit/TestFixture.h>
+#include <cppunit/TestAssert.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+#include <rtl/strbuf.hxx>
+#include <rtl/stringutils.hxx>
+#include <sal/types.h>
+
+namespace
+{
+class Test : public CppUnit::TestFixture
+{
+private:
+    void testStringView()
+    {
+        OStringBuffer b("foobar");
+        b = std::string_view("abc").substr(
+            0, 2); // avoid the string_view accidentally being NUL-terminated
+        CPPUNIT_ASSERT_EQUAL(sal_Int32(2), b.getLength());
+        CPPUNIT_ASSERT_EQUAL('a', b.getStr()[0]);
+        CPPUNIT_ASSERT_EQUAL('b', b.getStr()[1]);
+        CPPUNIT_ASSERT_EQUAL('\0', b.getStr()[2]);
+    }
+
+    void testOStringChar()
+    {
+        OStringBuffer b("foobar");
+        b = OStringChar('a');
+        CPPUNIT_ASSERT_EQUAL(sal_Int32(1), b.getLength());
+        CPPUNIT_ASSERT_EQUAL('a', b.getStr()[0]);
+        CPPUNIT_ASSERT_EQUAL('\0', b.getStr()[1]);
+    }
+
+    CPPUNIT_TEST_SUITE(Test);
+    CPPUNIT_TEST(testStringView);
+    CPPUNIT_TEST(testOStringChar);
+    CPPUNIT_TEST_SUITE_END();
+};
+}
+
+CPPUNIT_TEST_SUITE_REGISTRATION(Test);
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */