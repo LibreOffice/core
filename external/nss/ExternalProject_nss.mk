fdc0646512c920acb69a03b44d0f59e965e208f9
Michael Stahl <michael.stahl@allotropia.de>
2022-03-31 20:33:02 +0200

    nss: build with zlib module on WNT
    Change-Id: Ie875b4a8df1697de83a8f22cb1170a49792c47e6
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/132367
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

--- external/nss/ExternalProject_nss.mk
+++ external/nss/ExternalProject_nss.mk
@@ -27,10 +27,11 @@ $(call gb_ExternalProject_get_state_target,nss,build): \
 			MOZ_DEBUG_FLAGS=" " \
 			OPT_CODE_SIZE=0) \
 		OS_TARGET=WIN95 \
+		USE_SYSTEM_ZLIB=1 \
 		$(if $(filter X86_64,$(CPUNAME)),USE_64=1) \
 		$(if $(filter ARM64,$(CPUNAME)),USE_64=1) \
 		LIB="$(ILIB)" \
-		XCFLAGS="$(SOLARINC)" \
+		XCFLAGS="$(SOLARINC) $(ZLIB_CFLAGS)" \
 		$(if $(CROSS_COMPILING),\
 			CROSS_COMPILE=1 \
 			$(if $(filter ARM64,$(CPUNAME)),CPU_ARCH=aarch64) \
