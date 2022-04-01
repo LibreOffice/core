788599c7df58546900874ff0443be8522f9b4a04
Szymon Kłos <szymon.klos@collabora.com>
2022-04-01 13:20:02 +0200

    lok: fix formulabar rendering
    While forwardporting it was not taken into account that
    no pixel/logic conversion is needed.
    
    regression in:
    commit cf5b0d5972b02809e9b7f35780885445242b3f12
    Author: Szymon Kłos <szymon.klos@collabora.com>
    Date:   Mon Jan 17 13:11:19 2022 +0100
    
        RTL: lok: render correctly interim windows
    
    Change-Id: Ic6716bf9335d4a9297e3e5edfeb161702b846efb
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/132424
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Mert Tumer <mert.tumer@collabora.com>

--- vcl/source/app/salvtables.cxx
+++ vcl/source/app/salvtables.cxx
@@ -1351,7 +1351,7 @@ void SalInstanceWidget::DoRecursivePaint(vcl::Window* pWindow, const Point& rRen
             = pChild->GetOutDev()->GetOutOffYPixel() - pWindow->GetOutDev()->GetOutOffYPixel();
 
         Point aPos(rRenderLogicPos);
-        aPos += rOutput.PixelToLogic(Point(nDeltaX, nDeltaY));
+        aPos += Point(nDeltaX, nDeltaY);
 
         DoRecursivePaint(pChild, aPos, rOutput);
     }
