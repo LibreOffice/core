12f94913307450aad0e48f6ce06aac9f74807e1a
Stephan Bergmann <sbergman@redhat.com>
2022-04-02 10:48:11 +0200

    CppunitTest_sc_sparkline_test also needs the ASan hack
    ...from e854abe076155fc085b56549ced50b3ee9a095d2 "Avoid external processes
    picking up instdir/program/libnspr4.so", to avoid
    
    > Test name: SparklineImportExportTest::testSparklines
    > equality assertion failed
    > - Expected: 0
    > - Actual  : 32512
    > - failed to execute: sh .../bin/officeotron.sh .../tmp/SparklineImportExportTest__testSparklines_230wbj.tmp > .../tmp/SparklineImportExportTest__testSparklines_230wbn.tmp 2>&1
    > java: symbol lookup error: .../instdir/program/libnspr4.so: undefined symbol: __asan_init
    
    Change-Id: Icb710014278c0e38ec60a075d145b0174404ef2c
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/132452
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

--- sc/CppunitTest_sc_sparkline_test.mk
+++ sc/CppunitTest_sc_sparkline_test.mk
@@ -62,4 +62,8 @@ $(eval $(call gb_CppunitTest_use_components,sc_sparkline_test))
 
 $(eval $(call gb_CppunitTest_use_configuration,sc_sparkline_test))
 
+$(eval $(call gb_CppunitTest_add_arguments,sc_sparkline_test, \
+    -env:arg-env=$(gb_Helper_LIBRARY_PATH_VAR)"$$$${$(gb_Helper_LIBRARY_PATH_VAR)+=$$$$$(gb_Helper_LIBRARY_PATH_VAR)}" \
+))
+
 # vim: set noet sw=4 ts=4:
