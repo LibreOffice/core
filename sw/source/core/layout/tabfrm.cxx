4083ee2e8b0d59860e9f2161989ceed1ca384068
Caolán McNamara <caolanm@redhat.com>
2022-04-02 20:20:10 +0100

    forcepoint#100 drop SwBorderAttrAccess to allow cache entry to be removed
    for SwBorderAttr which gets deleted during this call
    
    also includes...
    
    Related: forcepoint#100 we don't need pAttrs for the duration of the full scope
    
    similar to the case of
    
    commit 6b1eae0334ba8bad7656a859695551ce51b62f95
    Date:   Fri May 18 08:26:14 2001 +0000
    
        Fix #87058#: Locked boraderattribut
    
    the SwCache object cannot be deleted if its locked, leading to a leak,
    we don't need pAttrs for the entire scope here so we can defer to the
    end of the scope the reacquire the lock to set pAttrs
    
    and also includes...
    
    Related: forcepoint#100 don't reacquire after every release
    
    instead release when we have to, and only reacquire if necessary
    before use of pAttrs
    
    Change-Id: Ie52aab7e5933d76b0c055389798104e4d93f39e9
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/132461
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
    Tested-by: Caolán McNamara <caolanm@redhat.com>

--- sw/source/core/layout/tabfrm.cxx
+++ sw/source/core/layout/tabfrm.cxx
@@ -2059,8 +2059,6 @@ void SwTabFrame::MakeAll(vcl::RenderContext* pRenderContext)
                     oAccess.reset();
                     m_bCalcLowers |= pLayout->Resize(
                         pLayout->GetBrowseWidthByTabFrame( *this ) );
-                    oAccess.emplace(SwFrame::GetCache(), this);
-                    pAttrs = oAccess->Get();
                 }
 
                 setFramePrintAreaValid(false);
@@ -2095,6 +2093,12 @@ void SwTabFrame::MakeAll(vcl::RenderContext* pRenderContext)
             const tools::Long nOldPrtWidth = aRectFnSet.GetWidth(getFramePrintArea());
             const tools::Long nOldFrameWidth = aRectFnSet.GetWidth(getFrameArea());
             const Point aOldPrtPos  = aRectFnSet.GetPos(getFramePrintArea());
+
+            if (!oAccess)
+            {
+                oAccess.emplace(SwFrame::GetCache(), this);
+                pAttrs = oAccess->Get();
+            }
             Format( getRootFrame()->GetCurrShell()->GetOut(), pAttrs );
 
             SwHTMLTableLayout *pLayout = GetTable()->GetHTMLTableLayout();
@@ -2105,8 +2109,6 @@ void SwTabFrame::MakeAll(vcl::RenderContext* pRenderContext)
                 oAccess.reset();
                 m_bCalcLowers |= pLayout->Resize(
                     pLayout->GetBrowseWidthByTabFrame( *this ) );
-                oAccess.emplace(SwFrame::GetCache(), this);
-                pAttrs = oAccess->Get();
             }
             if ( aOldPrtPos != aRectFnSet.GetPos(getFramePrintArea()) )
                 aNotify.SetLowersComplete( false );
@@ -2160,15 +2162,22 @@ void SwTabFrame::MakeAll(vcl::RenderContext* pRenderContext)
                             oAccess.reset();
                             m_bCalcLowers |= pHTMLLayout->Resize(
                                 pHTMLLayout->GetBrowseWidthByTabFrame( *this ) );
-
-                            oAccess.emplace(SwFrame::GetCache(), this);
-                            pAttrs = oAccess->Get();
                         }
 
                         setFramePrintAreaValid(false);
+
+                        if (!oAccess)
+                        {
+                            oAccess.emplace(SwFrame::GetCache(), this);
+                            pAttrs = oAccess->Get();
+                        }
                         Format( getRootFrame()->GetCurrShell()->GetOut(), pAttrs );
                     }
+
+                    oAccess.reset();
+
                     lcl_RecalcTable( *this, nullptr, aNotify );
+
                     m_bLowersFormatted = true;
                     if ( bKeep && KEEPTAB )
                     {
@@ -2332,11 +2341,18 @@ void SwTabFrame::MakeAll(vcl::RenderContext* pRenderContext)
                     // 6. There is no section change behind the table (see IsKeep)
                     // 7. The last table row wants to keep with its next.
                     const SwRowFrame* pLastRow = static_cast<const SwRowFrame*>(GetLastLower());
-                    if (pLastRow
-                        && IsKeep(pAttrs->GetAttrSet().GetKeep(), GetBreakItem(), true)
-                        && pLastRow->ShouldRowKeepWithNext())
+                    if (pLastRow)
                     {
-                        bFormat = true;
+                        if (!oAccess)
+                        {
+                            oAccess.emplace(SwFrame::GetCache(), this);
+                            pAttrs = oAccess->Get();
+                        }
+                        if (IsKeep(pAttrs->GetAttrSet().GetKeep(), GetBreakItem(), true)
+                            && pLastRow->ShouldRowKeepWithNext())
+                        {
+                            bFormat = true;
+                        }
                     }
                 }
 
@@ -2349,9 +2365,6 @@ void SwTabFrame::MakeAll(vcl::RenderContext* pRenderContext)
                     // Thus, find next content, table or section and, if a section
                     // is found, get its first content.
                     const SwFrame* pTmpNxt = sw_FormatNextContentForKeep( this );
-
-                    oAccess.emplace(SwFrame::GetCache(), this);
-                    pAttrs = oAccess->Get();
 
                     // The last row wants to keep with the frame behind the table.
                     // Check if the next frame is on a different page and valid.
@@ -2597,9 +2610,6 @@ void SwTabFrame::MakeAll(vcl::RenderContext* pRenderContext)
                             oAccess.reset();
 
                             GetFollow()->MakeAll(pRenderContext);
-
-                            oAccess.emplace(SwFrame::GetCache(), this);
-                            pAttrs = oAccess->Get();
 
                             GetFollow()->SetLowersFormatted(false);
                             // #i43913# - lock follow table
