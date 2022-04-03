3e460ab5cc782f7753d59867f346d82978a87d2e
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2022-04-03 13:21:59 +0900

    sc: put SparklineList implementation into SparklineList.cxx
    Change-Id: I309087a27ea0bc297c9bea9b2b8945579e055f4d
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/132552
    Tested-by: Jenkins
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

--- sc/inc/SparklineList.hxx
+++ sc/inc/SparklineList.hxx
@@ -14,7 +14,6 @@
 #include <memory>
 #include <map>
 
-#include "rangelst.hxx"
 #include "Sparkline.hxx"
 #include "SparklineGroup.hxx"
 
@@ -34,66 +33,14 @@ private:
         m_aSparklineGroupMap;
 
 public:
-    SparklineList() {}
+    SparklineList();
 
-    void addSparkline(std::shared_ptr<Sparkline> const& pSparkline)
-    {
-        auto pWeakGroup = std::weak_ptr<SparklineGroup>(pSparkline->getSparklineGroup());
+    void addSparkline(std::shared_ptr<Sparkline> const& pSparkline);
 
-        auto[iterator, bInserted]
-            = m_aSparklineGroupMap.try_emplace(pWeakGroup, std::vector<std::weak_ptr<Sparkline>>());
-        iterator->second.push_back(std::weak_ptr<Sparkline>(pSparkline));
-        if (bInserted)
-            m_aSparklineGroups.push_back(pWeakGroup);
-    }
-
-    std::vector<std::shared_ptr<SparklineGroup>> getSparklineGroups()
-    {
-        std::vector<std::shared_ptr<SparklineGroup>> toReturn;
-
-        for (auto iterator = m_aSparklineGroups.begin(); iterator != m_aSparklineGroups.end();)
-        {
-            if (auto pSparklineGroup = iterator->lock())
-            {
-                toReturn.push_back(pSparklineGroup);
-                iterator++;
-            }
-            else
-            {
-                iterator = m_aSparklineGroups.erase(iterator);
-            }
-        }
-        return toReturn;
-    }
+    std::vector<std::shared_ptr<SparklineGroup>> getSparklineGroups();
 
     std::vector<std::shared_ptr<Sparkline>>
-    getSparklinesFor(std::shared_ptr<SparklineGroup> const& pSparklineGroup)
-    {
-        std::vector<std::shared_ptr<Sparkline>> toReturn;
-
-        std::weak_ptr<SparklineGroup> pWeakGroup(pSparklineGroup);
-        auto iteratorGroup = m_aSparklineGroupMap.find(pWeakGroup);
-
-        if (iteratorGroup == m_aSparklineGroupMap.end())
-            return toReturn;
-
-        auto& rWeakSparklines = iteratorGroup->second;
-
-        for (auto iterator = rWeakSparklines.begin(); iterator != rWeakSparklines.end();)
-        {
-            if (auto aSparkline = iterator->lock())
-            {
-                toReturn.push_back(aSparkline);
-                iterator++;
-            }
-            else
-            {
-                iterator = rWeakSparklines.erase(iterator);
-            }
-        }
-
-        return toReturn;
-    }
+    getSparklinesFor(std::shared_ptr<SparklineGroup> const& pSparklineGroup);
 };
 
 } // end sc
