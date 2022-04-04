f37bbdb30e8fd4018152ca6de9ca3b220ebdbb0a
Armin Le Grand (Allotropia) <Armin.Le.Grand@me.com>
2022-04-04 16:57:08 +0200

    Advanced Diagram support: Move Diagram Text information to svx
    In a next step to get the Diagram mechanism/ModelData independent
    from oox, I moved the Text ModelData to svx, using a TextBody
    struct. This is a 1st move that covers most of what the
    algorithms to handle Diagram re-layout and other functionality
    use. This will potentially have to be extended accordingly
    when missing data is detected. It is potentially much more
    simple as the oox TextBody, by purpose.
    
    Due to functionality using that data I could now massively move
    more of it to svx.
    
    Change-Id: I6d6e6c572f119aeefa4e91eff56f58f3ceb6a31e

--- svx/source/diagram/datamodel.cxx
+++ svx/source/diagram/datamodel.cxx
@@ -19,8 +19,10 @@
 
 #include <unordered_set>
 #include <algorithm>
+#include <fstream>
 
 #include <svx/diagram/datamodel.hxx>
+#include <comphelper/xmltools.hxx>
 #include <sal/log.hxx>
 
 namespace svx::diagram {
@@ -135,8 +137,126 @@ bool DiagramData::removeNode(const OUString& rNodeId)
 
     // TODO: fix source/dest order
 
-    build(true);
+    buildDiagramDataModel(true);
+
     return true;
+}
+
+void DiagramData::getChildrenString(
+    OUStringBuffer& rBuf,
+    const svx::diagram::Point* pPoint,
+    sal_Int32 nLevel) const
+{
+    if (!pPoint)
+        return;
+
+    if (nLevel > 0)
+    {
+        for (sal_Int32 i = 0; i < nLevel-1; i++)
+            rBuf.append('\t');
+        rBuf.append('+');
+        rBuf.append(' ');
+        if(pPoint->msTextBody)
+            rBuf.append(pPoint->msTextBody->msText);
+        rBuf.append('\n');
+    }
+
+    std::vector< const svx::diagram::Point* > aChildren;
+    for (const auto& rCxn : maConnections)
+        if (rCxn.mnXMLType == TypeConstant::XML_parOf && rCxn.msSourceId == pPoint->msModelId)
+        {
+            if (rCxn.mnSourceOrder >= static_cast<sal_Int32>(aChildren.size()))
+                aChildren.resize(rCxn.mnSourceOrder + 1);
+            const auto pChild = maPointNameMap.find(rCxn.msDestId);
+            if (pChild != maPointNameMap.end())
+                aChildren[rCxn.mnSourceOrder] = pChild->second;
+        }
+
+    for (auto pChild : aChildren)
+        getChildrenString(rBuf, pChild, nLevel + 1);
+}
+
+std::vector<std::pair<OUString, OUString>> DiagramData::getChildren(const OUString& rParentId) const
+{
+    const OUString sModelId = rParentId.isEmpty() ? getRootPoint()->msModelId : rParentId;
+    std::vector<std::pair<OUString, OUString>> aChildren;
+    for (const auto& rCxn : maConnections)
+        if (rCxn.mnXMLType == TypeConstant::XML_parOf && rCxn.msSourceId == sModelId)
+        {
+            if (rCxn.mnSourceOrder >= static_cast<sal_Int32>(aChildren.size()))
+                aChildren.resize(rCxn.mnSourceOrder + 1);
+            const auto pChild = maPointNameMap.find(rCxn.msDestId);
+            if (pChild != maPointNameMap.end())
+            {
+                aChildren[rCxn.mnSourceOrder] = std::make_pair(
+                    pChild->second->msModelId,
+                    pChild->second->msTextBody ? pChild->second->msTextBody->msText : OUString());
+            }
+        }
+
+    // HACK: empty items shouldn't appear there
+    aChildren.erase(std::remove_if(aChildren.begin(), aChildren.end(),
+                                   [](const std::pair<OUString, OUString>& aItem) { return aItem.first.isEmpty(); }),
+                    aChildren.end());
+
+    return aChildren;
+}
+
+OUString DiagramData::addNode(const OUString& rText)
+{
+    const svx::diagram::Point& rDataRoot = *getRootPoint();
+    OUString sPresRoot;
+    for (const auto& aCxn : maConnections)
+        if (aCxn.mnXMLType == TypeConstant::XML_presOf && aCxn.msSourceId == rDataRoot.msModelId)
+            sPresRoot = aCxn.msDestId;
+
+    if (sPresRoot.isEmpty())
+        return OUString();
+
+    OUString sNewNodeId = OStringToOUString(comphelper::xml::generateGUIDString(), RTL_TEXTENCODING_UTF8);
+
+    svx::diagram::Point aDataPoint;
+    aDataPoint.mnXMLType = TypeConstant::XML_node;
+    aDataPoint.msModelId = sNewNodeId;
+    aDataPoint.msTextBody = std::make_shared<TextBody>();
+    aDataPoint.msTextBody->msText = rText;
+
+    OUString sDataSibling;
+    for (const auto& aCxn : maConnections)
+        if (aCxn.mnXMLType == TypeConstant::XML_parOf && aCxn.msSourceId == rDataRoot.msModelId)
+            sDataSibling = aCxn.msDestId;
+
+    OUString sPresSibling;
+    for (const auto& aCxn : maConnections)
+        if (aCxn.mnXMLType == TypeConstant::XML_presOf && aCxn.msSourceId == sDataSibling)
+            sPresSibling = aCxn.msDestId;
+
+    svx::diagram::Point aPresPoint;
+    aPresPoint.mnXMLType = TypeConstant::XML_pres;
+    aPresPoint.msModelId = OStringToOUString(comphelper::xml::generateGUIDString(), RTL_TEXTENCODING_UTF8);
+
+    aPresPoint.msPresentationAssociationId = aDataPoint.msModelId;
+    if (!sPresSibling.isEmpty())
+    {
+        // no idea where to get these values from, so copy from previous sibling
+        const svx::diagram::Point* pSiblingPoint = maPointNameMap[sPresSibling];
+        aPresPoint.msPresentationLayoutName = pSiblingPoint->msPresentationLayoutName;
+        aPresPoint.msPresentationLayoutStyleLabel = pSiblingPoint->msPresentationLayoutStyleLabel;
+        aPresPoint.mnLayoutStyleIndex = pSiblingPoint->mnLayoutStyleIndex;
+        aPresPoint.mnLayoutStyleCount = pSiblingPoint->mnLayoutStyleCount;
+    }
+
+    addConnection(svx::diagram::TypeConstant::XML_parOf, rDataRoot.msModelId, aDataPoint.msModelId);
+    addConnection(svx::diagram::TypeConstant::XML_presParOf, sPresRoot, aPresPoint.msModelId);
+    addConnection(svx::diagram::TypeConstant::XML_presOf, aDataPoint.msModelId, aPresPoint.msModelId);
+
+    // adding at the end, so that references are not invalidated in between
+    maPoints.push_back(aDataPoint);
+    maPoints.push_back(aPresPoint);
+
+    buildDiagramDataModel(true);
+
+    return sNewNodeId;
 }
 
 void DiagramData::addConnection(svx::diagram::TypeConstant nType, const OUString& sSourceId, const OUString& sDestId)
@@ -151,6 +271,206 @@ void DiagramData::addConnection(svx::diagram::TypeConstant nType, const OUString
     rCxn.msSourceId = sSourceId;
     rCxn.msDestId = sDestId;
     rCxn.mnSourceOrder = nMaxOrd + 1;
+}
+
+// #define DEBUG_OOX_DIAGRAM
+#ifdef DEBUG_OOX_DIAGRAM
+OString normalizeDotName( const OUString& rStr )
+{
+    OUStringBuffer aBuf;
+    aBuf.append('N');
+
+    const sal_Int32 nLen(rStr.getLength());
+    sal_Int32 nCurrIndex(0);
+    while( nCurrIndex < nLen )
+    {
+        const sal_Int32 aChar=rStr.iterateCodePoints(&nCurrIndex);
+        if( aChar != '-' && aChar != '{' && aChar != '}' )
+            aBuf.append((sal_Unicode)aChar);
+    }
+
+    return OUStringToOString(aBuf.makeStringAndClear(),
+                                  RTL_TEXTENCODING_UTF8);
+}
+#endif
+
+static sal_Int32 calcDepth( std::u16string_view rNodeName,
+                            const svx::diagram::Connections& rCnx )
+{
+    // find length of longest path in 'isChild' graph, ending with rNodeName
+    for (auto const& elem : rCnx)
+    {
+        if( !elem.msParTransId.isEmpty() &&
+            !elem.msSibTransId.isEmpty() &&
+            !elem.msSourceId.isEmpty() &&
+            !elem.msDestId.isEmpty() &&
+            elem.mnXMLType == TypeConstant::XML_parOf &&
+            rNodeName == elem.msDestId )
+        {
+            return calcDepth(elem.msSourceId, rCnx) + 1;
+        }
+    }
+
+    return 0;
+}
+
+void DiagramData::buildDiagramDataModel(bool /*bClearOoxShapes*/)
+{
+    // build name-object maps
+    maPointNameMap.clear();
+    maPointsPresNameMap.clear();
+    maConnectionNameMap.clear();
+    maPresOfNameMap.clear();
+
+#ifdef DEBUG_OOX_DIAGRAM
+    std::ofstream output("tree.dot");
+
+    output << "digraph datatree {" << std::endl;
+#endif
+    svx::diagram::Points& rPoints = getPoints();
+    for (auto & point : rPoints)
+    {
+#ifdef DEBUG_OOX_DIAGRAM
+        output << "\t"
+               << normalizeDotName(point.msModelId).getStr()
+               << "[";
+
+        if( !point.msPresentationLayoutName.isEmpty() )
+            output << "label=\""
+                   << OUStringToOString(
+                       point.msPresentationLayoutName,
+                       RTL_TEXTENCODING_UTF8).getStr() << "\", ";
+        else
+            output << "label=\""
+                   << OUStringToOString(
+                       point.msModelId,
+                       RTL_TEXTENCODING_UTF8).getStr() << "\", ";
+
+        switch( point.mnXMLType )
+        {
+            case TypeConstant::XML_doc: output << "style=filled, color=red"; break;
+            case TypeConstant::XML_asst: output << "style=filled, color=green"; break;
+            default:
+            case TypeConstant::XML_node: output << "style=filled, color=blue"; break;
+            case TypeConstant::XML_pres: output << "style=filled, color=yellow"; break;
+            case TypeConstant::XML_parTrans: output << "color=grey"; break;
+            case TypeConstant::XML_sibTrans: output << " "; break;
+        }
+
+        output << "];" << std::endl;
+#endif
+
+        // does currpoint have any text set?
+        if(point.msTextBody && !point.msTextBody->msText.isEmpty())
+        {
+#ifdef DEBUG_OOX_DIAGRAM
+            static sal_Int32 nCount=0;
+            output << "\t"
+                   << "textNode" << nCount
+                   << " ["
+                   << "label=\""
+                   << OUStringToOString(
+                       point.msTextBody->msText,
+                       RTL_TEXTENCODING_UTF8).getStr()
+                   << "\"" << "];" << std::endl;
+            output << "\t"
+                   << normalizeDotName(point.msModelId).getStr()
+                   << " -> "
+                   << "textNode" << nCount++
+                   << ";" << std::endl;
+#endif
+        }
+
+        const bool bInserted1 = getPointNameMap().insert(
+            std::make_pair(point.msModelId,&point)).second;
+
+        SAL_WARN_IF(!bInserted1, "oox.drawingml", "DiagramData::build(): non-unique point model id");
+
+        if( !point.msPresentationLayoutName.isEmpty() )
+        {
+            DiagramData::PointsNameMap::value_type::second_type& rVec=
+                getPointsPresNameMap()[point.msPresentationLayoutName];
+            rVec.push_back(&point);
+        }
+    }
+
+    const svx::diagram::Connections& rConnections = getConnections();
+    for (auto const& connection : rConnections)
+    {
+#ifdef DEBUG_OOX_DIAGRAM
+        if( !connection.msParTransId.isEmpty() ||
+            !connection.msSibTransId.isEmpty() )
+        {
+            if( !connection.msSourceId.isEmpty() ||
+                !connection.msDestId.isEmpty() )
+            {
+                output << "\t"
+                       << normalizeDotName(connection.msSourceId).getStr()
+                       << " -> "
+                       << normalizeDotName(connection.msParTransId).getStr()
+                       << " -> "
+                       << normalizeDotName(connection.msSibTransId).getStr()
+                       << " -> "
+                       << normalizeDotName(connection.msDestId).getStr()
+                       << " [style=dotted,"
+                       << ((connection.mnXMLType == TypeConstant::XML_presOf) ? " color=red, " : ((connection.mnXMLType == TypeConstant::XML_presParOf) ? " color=green, " : " "))
+                       << "label=\""
+                       << OUStringToOString(connection.msModelId,
+                                                 RTL_TEXTENCODING_UTF8 ).getStr()
+                       << "\"];" << std::endl;
+            }
+            else
+            {
+                output << "\t"
+                       << normalizeDotName(connection.msParTransId).getStr()
+                       << " -> "
+                       << normalizeDotName(connection.msSibTransId).getStr()
+                       << " ["
+                       << ((connection.mnXMLType == TypeConstant::XML_presOf) ? " color=red, " : ((connection.mnXMLType == TypeConstant::XML_presParOf) ? " color=green, " : " "))
+                       << "label=\""
+                       << OUStringToOString(connection.msModelId,
+                                                 RTL_TEXTENCODING_UTF8 ).getStr()
+                       << "\"];" << std::endl;
+            }
+        }
+        else if( !connection.msSourceId.isEmpty() ||
+                 !connection.msDestId.isEmpty() )
+            output << "\t"
+                   << normalizeDotName(connection.msSourceId).getStr()
+                   << " -> "
+                   << normalizeDotName(connection.msDestId).getStr()
+                   << " [label=\""
+                   << OUStringToOString(connection.msModelId,
+                                             RTL_TEXTENCODING_UTF8 ).getStr()
+                   << ((connection.mnXMLType == TypeConstant::XML_presOf) ? "\", color=red]" : ((connection.mnXMLType == TypeConstant::XML_presParOf) ? "\", color=green]" : "\"]"))
+                   << ";" << std::endl;
+#endif
+
+        const bool bInserted1 = maConnectionNameMap.insert(
+            std::make_pair(connection.msModelId,&connection)).second;
+
+        SAL_WARN_IF(!bInserted1, "oox.drawingml", "DiagramData::build(): non-unique connection model id");
+
+        if( connection.mnXMLType == TypeConstant::XML_presOf )
+        {
+            DiagramData::StringMap::value_type::second_type& rVec = getPresOfNameMap()[connection.msDestId];
+            rVec[connection.mnDestOrder] = { connection.msSourceId, sal_Int32(0) };
+        }
+    }
+
+    // assign outline levels
+    DiagramData::StringMap& rStringMap = getPresOfNameMap();
+    for (auto & elemPresOf : rStringMap)
+    {
+        for (auto & elem : elemPresOf.second)
+        {
+            const sal_Int32 nDepth = calcDepth(elem.second.msSourceId, getConnections());
+            elem.second.mnDepth = nDepth != 0 ? nDepth : -1;
+        }
+    }
+#ifdef DEBUG_OOX_DIAGRAM
+    output << "}" << std::endl;
+#endif
 }
 
 }
