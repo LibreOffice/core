LibreOffice/core

commit e63fe68fb1d0915b64fdf63f7fa6eed866fa3a0d

Qt use asChild for pointer positioning
.. and refactor some variable names.

Change-Id: I7d31a7a377f614bdd78f8d09852a2eda0cc47442
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/127153
Tested-by: Jenkins
Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>

vcl/qt5/QtFrame.cxx
@@ -512,11 +512,10 @@ void QtFrame::SetDefaultPos()
     if (m_pParent)
     {
         const qreal fRatio = devicePixelRatioF();
-        QWidget* const pWindow = m_pParent->GetQWidget()->window();
-        QWidget* const pWidget = asChild();
-        QPoint aPos = pWindow->rect().center() - pWidget->rect().center();
-        SetPosSize(round(aPos.x() * fRatio), round(aPos.y() * fRatio), 0, 0,
-                   SAL_FRAME_POSSIZE_X | SAL_FRAME_POSSIZE_Y);
+        QWidget* const pParentWin = m_pParent->asChild()->window();
+        QWidget* const pChildWin = asChild()->window();
+        QPoint aPos = (pParentWin->rect().center() - pChildWin->rect().center()) * fRatio;
+        SetPosSize(aPos.x(), aPos.y(), 0, 0, SAL_FRAME_POSSIZE_X | SAL_FRAME_POSSIZE_Y);
         assert(!m_bDefaultPos);
     }
     else
@@ -863,7 +862,7 @@ void QtFrame::SetPointerPos(tools::Long nX, tools::Long nY)
 {
     // some cursor already exists (and it has m_ePointerStyle shape)
     // so here we just reposition it
-    QCursor::setPos(m_pQWidget->mapToGlobal(QPoint(nX, nY)));
+    QCursor::setPos(asChild()->mapToGlobal(QPoint(nX, nY)));
 }
 
 void QtFrame::Flush()

vcl/qt5/QtWidget.cxx
@@ -257,6 +257,7 @@ void QtWidget::dropEvent(QDropEvent* pEvent) { m_rFrame.handleDrop(pEvent); }
 
 void QtWidget::moveEvent(QMoveEvent* pEvent)
 {
+    // already handled by QtMainWindow::moveEvent
     if (m_rFrame.m_pTopLevel)
         return;
 

commit ffddc7110324af4773957089c31e8033afad76f8

tdf#147523 Qt use inner QWidget to set pointer pos
Regression from commit e63fe68fb1d0915b64fdf63f7fa6eed866fa3a0d
("Qt use asChild for pointer positioning"), which should just have
been a cleanup for commit ca28826a087245686d7fca3ffc8ca1f03307924d
("tdf#131467 Qt set default position on first resize").

And scale the requested position by the device scale factor.

Change-Id: I50b3c628d22896e987d8cc2eaf5635cb12ba3464

vcl/qt5/QtFrame.cxx
@@ -883,7 +883,7 @@ void QtFrame::SetPointerPos(tools::Long nX, tools::Long nY)
 {
     // some cursor already exists (and it has m_ePointerStyle shape)
     // so here we just reposition it
-    QCursor::setPos(asChild()->mapToGlobal(QPoint(nX, nY)));
+    QCursor::setPos(m_pQWidget->mapToGlobal(QPoint(nX, nY) / devicePixelRatioF()));
 }
 
 void QtFrame::Flush()