LibreOffice/core

commit b00a68a8e19370e106cd76258a3c1825f43613ee

tdf#145363 Qt reparent modal dialogs on show
Simply said, one can't have two modal dialogs open at the same
parent in Qt. All modal windows must open as a stack, each
parented to the previous one. This is kind of logical.

Unexpectedly Qt totally breaks, if you open two modal dialogs on
the same window. This happens, because the existing paragraph
style dialog and the "sub" "list style" dialog are both children
of their Writer window.

I'm not sure the additionally introduced QWidget-based parent
handling is strictly needed. It seems Ok.

So for every visibility and modality change, we reparent the
Qt widget, either on top of the modal stack or restore the
original LO-based parent. The LO hierachy is never changed!

Change-Id: Id209c9aa67774440089dc50a6648ac293950087a
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/124500
Tested-by: Jenkins
Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>

vcl/inc/qt5/QtFrame.hxx
@@ -132,6 +132,7 @@ class VCLPLUG_QT_PUBLIC QtFrame : public QObject, public SalFrame
     void SetWindowStateImpl(Qt::WindowStates eState);
 
     void fixICCCMwindowGroup();
+    void modalReparent(bool bVisible);
 
 public:
     QtFrame(QtFrame* pParent, SalFrameStyleFlags nSalFrameStyle, bool bUseCairo);

vcl/qt5/QtFrame.cxx
@@ -417,24 +417,71 @@ void QtFrame::DrawMenuBar() { /* not needed */}
 
 void QtFrame::SetExtendedFrameStyle(SalExtStyle /*nExtStyle*/) { /* not needed */}
 
+void QtFrame::modalReparent(bool bVisible)
+{
+#ifndef NDEBUG
+    auto* pSalInst(static_cast<QtInstance*>(GetSalData()->m_pInstance));
+    assert(pSalInst);
+    assert(pSalInst->IsMainThread());
+    assert(!asChild()->isVisible());
+    assert(asChild()->isModal());
+#endif
+
+    if (!bVisible)
+    {
+        m_pQWidget->setParent(m_pParent ? m_pParent->asChild() : nullptr,
+                              m_pQWidget->windowFlags());
+        return;
+    }
+
+    if (!QGuiApplication::modalWindow())
+        return;
+
+    QtInstance* pInst = static_cast<QtInstance*>(GetSalData()->m_pInstance);
+    for (auto* pFrame : pInst->getFrames())
+    {
+        QWidget* pQWidget = static_cast<QtFrame*>(pFrame)->asChild();
+        if (pQWidget->windowHandle() == QGuiApplication::modalWindow())
+        {
+            m_pQWidget->setParent(pQWidget, m_pQWidget->windowFlags());
+            break;
+        }
+    }
+}
+
 void QtFrame::Show(bool bVisible, bool bNoActivate)
 {
     assert(m_pQWidget);
     if (bVisible == asChild()->isVisible())
         return;
 
+    auto* pSalInst(static_cast<QtInstance*>(GetSalData()->m_pInstance));
+    assert(pSalInst);
+
+    if (!bVisible) // hide
+    {
+        pSalInst->RunInMainThread([this]() {
+            asChild()->hide();
+            if (m_pQWidget->isModal())
+                modalReparent(false);
+        });
+        return;
+    }
+
+    // show
     SetDefaultSize();
     SetDefaultPos();
 
-    auto* pSalInst(static_cast<QtInstance*>(GetSalData()->m_pInstance));
-    assert(pSalInst);
-    pSalInst->RunInMainThread([this, bVisible, bNoActivate]() {
-        asChild()->setVisible(bVisible);
-        asChild()->raise();
+    pSalInst->RunInMainThread([this, bNoActivate]() {
+        QWidget* const pChild = asChild();
+        if (m_pQWidget->isModal())
+            modalReparent(true);
+        pChild->show();
+        pChild->raise();
         if (!bNoActivate && !isPopup())
         {
-            asChild()->activateWindow();
-            asChild()->setFocus();
+            pChild->activateWindow();
+            pChild->setFocus();
         }
     });
 }
@@ -610,7 +657,7 @@ SalFrame* QtFrame::GetParent() const { return m_pParent; }
 
 void QtFrame::SetModal(bool bModal)
 {
-    if (!isWindow())
+    if (!isWindow() || asChild()->isModal() == bModal)
         return;
 
     auto* pSalInst(static_cast<QtInstance*>(GetSalData()->m_pInstance));
@@ -622,12 +669,20 @@ void QtFrame::SetModal(bool bModal)
 
         // modality change is only effective if the window is hidden
         if (bWasVisible)
+        {
             pChild->hide();
+            if (!bModal)
+                modalReparent(false);
+        }
 
         pChild->setWindowModality(bModal ? Qt::WindowModal : Qt::NonModal);
 
         if (bWasVisible)
+        {
+            if (bModal)
+                modalReparent(true);
             pChild->show();
+        }
     });
 }
 
@@ -1180,7 +1235,14 @@ void QtFrame::SimulateKeyPress(sal_uInt16 nKeyCode)
     SAL_WARN("vcl.qt", "missing simulate keypress " << nKeyCode);
 }
 
-void QtFrame::SetParent(SalFrame* pNewParent) { m_pParent = static_cast<QtFrame*>(pNewParent); }
+void QtFrame::SetParent(SalFrame* pNewParent)
+{
+    if (m_pParent == pNewParent)
+        return;
+    m_pParent = static_cast<QtFrame*>(pNewParent);
+    if (!m_pTopLevel)
+        m_pQWidget->setParent(m_pParent->asChild(), m_pQWidget->windowFlags());
+}
 
 void QtFrame::SetPluginParent(SystemParentData* /*pNewParent*/)
 {

vcl/qt5/QtWidget.cxx
@@ -23,6 +23,7 @@
 #include <QtFrame.hxx>
 #include <QtGraphics.hxx>
 #include <QtInstance.hxx>
+#include <QtMainWindow.hxx>
 #include <QtSvpGraphics.hxx>
 #include <QtTransferable.hxx>
 #include <QtTools.hxx>
@@ -616,7 +617,10 @@ void QtWidget::focusOutEvent(QFocusEvent*)
 }
 
 QtWidget::QtWidget(QtFrame& rFrame, Qt::WindowFlags f)
-    : QWidget(Q_NULLPTR, f)
+    : QWidget(!rFrame.GetTopLevelWindow() && rFrame.GetParent()
+                  ? static_cast<QtFrame*>(rFrame.GetParent())->asChild()
+                  : Q_NULLPTR,
+              f)
     , m_rFrame(rFrame)
     , m_bNonEmptyIMPreeditSeen(false)
     , m_nDeltaX(0)

commit f0776128ceefb1027a8da7619e86aa6e320791c0

tdf#144585 Qt fix Wayland LO fake popups
So Michael Weghorn was somehow reminded of an abandoned
commit from me ("Qt5 rework parent handling") archived in
https://gerrit.libreoffice.org/c/core/+/73463.
Still I checked a diff from 7.1.7..master vcl/qt5, which
together boiled down to this change.

We already must handle "out-of-order" QWidget / SalFrame deletion
like fixed in commit 77ddb3bd2ad78304bc882b1c5ba162c2983f4b56
("tdf#123406 KDE5: Update delayed frame destruction), so the
original reason from commit a2a6a2ddc5f2f9c8e6070acaab0719277872d824
("No parent-child relationship between Qt5Widget") doesn't apply
anymore, but at least Wayland "emulated LO popups" become unusable
with parents. FWIW the problem also exists for the WASM QPA.

I'm also not sure commit b00a68a8e19370e106cd76258a3c1825f43613ee
("tdf#145363 Qt reparent modal dialogs on show") is needed anymore,
but I will leave that to an other change.

Regression from commit b00a68a8e19370e106cd76258a3c1825f43613ee
("tdf#145363 Qt reparent modal dialogs on show").

Change-Id: If726771b4e9cc3f639f21cf502b3ec5985873643

vcl/inc/qt5/QtFrame.hxx
@@ -216,6 +216,8 @@ public:
 
     void setInputLanguage(LanguageType);
     inline bool isPopup() const;
+
+    static void SetSystemEnvDataPlatform(SystemEnvData&);
     static void FillSystemEnvData(SystemEnvData&, sal_IntPtr pWindow, QWidget* pWidget);
 };
 

vcl/qt5/QtFrame.cxx
@@ -135,6 +135,8 @@ QtFrame::QtFrame(QtFrame* pParent, SalFrameStyleFlags nStyle, bool bUseCairo)
     m_nStyle = nStyle;
     m_pParent = pParent;
 
+    SetSystemEnvDataPlatform(m_aSystemData);
+
     Qt::WindowFlags aWinFlags(Qt::Widget);
     if (!(nStyle & SalFrameStyleFlags::SYSTEMCHILD))
     {
@@ -151,11 +153,18 @@ QtFrame::QtFrame(QtFrame* pParent, SalFrameStyleFlags nStyle, bool bUseCairo)
         // a focus-out event, reaching the combo box. This used to map to
         // Qt::ToolTip, which doesn't feel that correct...
         else if (isPopup())
-#ifdef EMSCRIPTEN
-            aWinFlags = Qt::ToolTip | Qt::FramelessWindowHint;
-#else
-            aWinFlags = Qt::Window | Qt::FramelessWindowHint | Qt::BypassWindowManagerHint;
-#endif
+        {
+            switch (m_aSystemData.platform)
+            {
+                case SystemEnvData::Platform::Wayland:
+                case SystemEnvData::Platform::WASM:
+                    aWinFlags = Qt::ToolTip | Qt::FramelessWindowHint;
+                    break;
+                default:
+                    aWinFlags = Qt::Window | Qt::FramelessWindowHint | Qt::BypassWindowManagerHint;
+                    break;
+            }
+        }
         else if (nStyle & SalFrameStyleFlags::TOOLWINDOW)
             aWinFlags = Qt::Tool;
         // top level windows can't be transient in Qt, so make them dialogs, if they have a parent. At least
@@ -170,7 +179,7 @@ QtFrame::QtFrame(QtFrame* pParent, SalFrameStyleFlags nStyle, bool bUseCairo)
     if (aWinFlags == Qt::Window)
     {
         m_pTopLevel = new QtMainWindow(*this, aWinFlags);
-        m_pQWidget = new QtWidget(*this, aWinFlags);
+        m_pQWidget = new QtWidget(*this);
         m_pTopLevel->setCentralWidget(m_pQWidget);
         m_pTopLevel->setFocusProxy(m_pQWidget);
     }
@@ -192,7 +201,7 @@ QtFrame::QtFrame(QtFrame* pParent, SalFrameStyleFlags nStyle, bool bUseCairo)
     fixICCCMwindowGroup();
 }
 
-void QtFrame::FillSystemEnvData(SystemEnvData& rData, sal_IntPtr pWindow, QWidget* pWidget)
+void QtFrame::SetSystemEnvDataPlatform(SystemEnvData& rData)
 {
     if (QGuiApplication::platformName() == "wayland")
         rData.platform = SystemEnvData::Platform::Wayland;
@@ -207,7 +216,10 @@ void QtFrame::FillSystemEnvData(SystemEnvData& rData, sal_IntPtr pWindow, QWidge
                  "Unsupported qt VCL platform: " << toOUString(QGuiApplication::platformName()));
         std::abort();
     }
+}
 
+void QtFrame::FillSystemEnvData(SystemEnvData& rData, sal_IntPtr pWindow, QWidget* pWidget)
+{
     rData.toolkit = SystemEnvData::Toolkit::Qt;
     rData.aShellWindow = pWindow;
     rData.pWidget = pWidget;
@@ -355,10 +367,7 @@ QWindow* QtFrame::windowHandle() const
     // set attribute 'Qt::WA_NativeWindow' first to make sure a window handle actually exists
     QWidget* pChild = asChild();
     assert(pChild->window() == pChild);
-#ifndef EMSCRIPTEN
-    // no idea, why this breaks the menubar for EMSCRIPTEN
     pChild->setAttribute(Qt::WA_NativeWindow);
-#endif
     return pChild->windowHandle();
 }
 

vcl/qt5/QtObject.cxx
@@ -41,6 +41,7 @@ QtObject::QtObject(QtFrame* pParent, bool bShow)
     if (bShow)
         m_pQWidget->show();
 
+    QtFrame::SetSystemEnvDataPlatform(m_aSystemData);
     QtFrame::FillSystemEnvData(m_aSystemData, reinterpret_cast<sal_IntPtr>(this), m_pQWidget);
 }
 

vcl/qt5/QtWidget.cxx
@@ -620,16 +620,12 @@ void QtWidget::focusOutEvent(QFocusEvent*)
 }
 
 QtWidget::QtWidget(QtFrame& rFrame, Qt::WindowFlags f)
-    : QWidget(!rFrame.GetTopLevelWindow() && rFrame.GetParent()
-                  ? static_cast<QtFrame*>(rFrame.GetParent())->asChild()
-                  : Q_NULLPTR,
-              f)
+    : QWidget(Q_NULLPTR, f)
     , m_rFrame(rFrame)
     , m_bNonEmptyIMPreeditSeen(false)
     , m_nDeltaX(0)
     , m_nDeltaY(0)
 {
-    create();
     setMouseTracking(true);
     if (!rFrame.isPopup())
         setFocusPolicy(Qt::StrongFocus);