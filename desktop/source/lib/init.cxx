80aa50f0008451be8f3c0da9806ebcd2354a4a79
Mert Tumer <mert.tumer@collabora.com>
2022-04-06 16:59:53 +0300

    lok: load template documents as regular documents
    otherwise lok cannot save them directly because
    libreoffice will try to open a save-as dialog
    For odg, change the draw8_template type draw8
    
    Signed-off-by: Mert Tumer <mert.tumer@collabora.com>
    Change-Id: I34b0ed03adcac89eaa926f8ae6c57e6f622a90f0
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/132633
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Henry Castro <hcastro@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/138074

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -2563,10 +2563,14 @@ static LibreOfficeKitDocument* lo_documentLoadWithOptions(LibreOfficeKit* pThis,
             document::MacroExecMode::NEVER_EXECUTE;
 #endif
 
+        // set AsTemplate explicitly false to be able to load template files
+        // as regular files, otherwise we cannot save them; it will try
+        // to bring saveas dialog which cannot work with LOK case
         uno::Sequence<css::beans::PropertyValue> aFilterOptions{
             comphelper::makePropertyValue("FilterOptions", sFilterOptions),
             comphelper::makePropertyValue("InteractionHandler", xInteraction),
-            comphelper::makePropertyValue("MacroExecutionMode", nMacroExecMode)
+            comphelper::makePropertyValue("MacroExecutionMode", nMacroExecMode),
+            comphelper::makePropertyValue("AsTemplate", false)
         };
 
         /* TODO
