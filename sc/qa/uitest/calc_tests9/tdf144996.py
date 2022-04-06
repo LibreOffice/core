c0919c6e5787dfb45f1a3e4b8b5ad6f1ff28f1ae
Xisco Fauli <xiscofauli@libreoffice.org>
2022-04-06 10:57:03 +0200

    tdf#144996: sc: Add unittest
    Change-Id: I8e6d9f420e9b487089cefaa1e7bf5a6082d6ce52
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/132622
    Tested-by: Jenkins
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

---
+++ sc/qa/uitest/calc_tests9/tdf144996.py
@@ -0,0 +1,48 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+from uitest.framework import UITestCase
+from uitest.uihelper.common import get_url_for_data_file
+from libreoffice.uno.propertyvalue import mkPropertyValues
+from org.libreoffice.unotest import systemPathToFileUrl
+
+class tdf144996(UITestCase):
+
+    def test_tdf144996(self):
+
+        # get_url_for_data_file calls makeCopyFromTDOC
+        # so using save doesn't affect the original file
+        xFilePath = get_url_for_data_file("tdf144996.xlsx")
+
+        with self.ui_test.load_file(xFilePath) as document:
+
+            self.assertTrue(document.isReadonly())
+
+            with self.ui_test.execute_dialog_through_command(".uno:EditDoc") as xDialog:
+                xPassword = xDialog.getChild("newpassEntry")
+                xPassword.executeAction("TYPE", mkPropertyValues({"TEXT": "password"}))
+
+            self.assertFalse(document.isReadonly())
+
+            with self.ui_test.execute_dialog_through_command(".uno:Save", close_button="save"):
+                # Confirm file format popup
+                pass
+
+        with self.ui_test.load_file(xFilePath) as document:
+
+            self.assertTrue(document.isReadonly())
+
+            # Without the fix in place, this test would have failed here because the saved document
+            # doesn't need a password to edit it
+            with self.ui_test.execute_dialog_through_command(".uno:EditDoc") as xDialog:
+                xPassword = xDialog.getChild("newpassEntry")
+                xPassword.executeAction("TYPE", mkPropertyValues({"TEXT": "password"}))
+
+            self.assertFalse(document.isReadonly())
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
