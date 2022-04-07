07a1442b2441cc032c05fd9abf5b1a8db6c7e007
Miklos Vajna <vmiklos@collabora.com>
2022-04-07 09:11:09 +0200

    sw content controls: select the content on click when showing placeholder
    - teach SwCursorShell::GetContentAtPos() about a new IsAttrAtPos::ContentControl
    
    - add a new SwCursorShell::GotoFormatContentControl() to select a
      content control, and a SwWrtShell::GotoContentControl() wrapper around
      it
    
    - combine these together in SwEditWin::MouseButtonUp()
    
    The intention is that when you open a document and you click on a
    placeholder text like "Click here to enter text", then this content is
    pre-selected (so typing overwrites it), but typing real content there
    disables this behavior.
    
    Change-Id: Ia539865da7b18c41cbfb398282842bdb2e25f0bc
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/132652
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Tested-by: Jenkins

--- sw/source/uibase/docvw/edtwin.cxx
+++ sw/source/uibase/docvw/edtwin.cxx
@@ -150,6 +150,7 @@
 #include <cntfrm.hxx>
 #include <txtfrm.hxx>
 #include <strings.hrc>
+#include <textcontentcontrol.hxx>
 
 using namespace sw::mark;
 using namespace ::com::sun::star;
@@ -4737,7 +4738,8 @@ void SwEditWin::MouseButtonUp(const MouseEvent& rMEvt)
 
                         SwContentAtPos aContentAtPos( IsAttrAtPos::Field |
                                                     IsAttrAtPos::InetAttr |
-                                                    IsAttrAtPos::SmartTag  | IsAttrAtPos::FormControl);
+                                                    IsAttrAtPos::SmartTag  | IsAttrAtPos::FormControl |
+                                                    IsAttrAtPos::ContentControl);
 
                         if( rSh.GetContentAtPos( aDocPt, aContentAtPos ) )
                         {
@@ -4791,6 +4793,15 @@ void SwEditWin::MouseButtonUp(const MouseEvent& rMEvt)
                                 {
                                     rSh.LeaveAddMode();
                                 }
+                            }
+                            else if (aContentAtPos.eContentAtPos == IsAttrAtPos::ContentControl)
+                            {
+                                auto pTextContentControl
+                                    = static_txtattr_cast<const SwTextContentControl*>(
+                                        aContentAtPos.pFndTextAttr);
+                                const SwFormatContentControl& rFormatContentControl
+                                    = pTextContentControl->GetContentControl();
+                                rSh.GotoContentControl(rFormatContentControl);
                             }
                             else if ( IsAttrAtPos::SmartTag == aContentAtPos.eContentAtPos )
                             {
