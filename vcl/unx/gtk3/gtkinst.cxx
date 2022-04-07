2236cba06cd2145a6bb37aacc4d055ab3770fd6e
Caolán McNamara <caolanm@redhat.com>
2022-04-07 16:46:33 +0100

    Red used instead of Green
    Change-Id: I1261154fcff6f4904b4360099cbf26e33b9e7463

--- vcl/unx/gtk3/gtkinst.cxx
+++ vcl/unx/gtk3/gtkinst.cxx
@@ -16699,7 +16699,7 @@ private:
     void set_text_background_color(const Color& rColor)
     {
         guint16 nRed = rColor.GetRed() << 8;
-        guint16 nGreen = rColor.GetRed() << 8;
+        guint16 nGreen = rColor.GetGreen() << 8;
         guint16 nBlue = rColor.GetBlue() << 8;
 
         PangoAttrType aFilterAttrs[] = {PANGO_ATTR_BACKGROUND, PANGO_ATTR_INVALID};
@@ -16716,7 +16716,7 @@ private:
     void set_text_foreground_color(const Color& rColor, bool bSetBold)
     {
         guint16 nRed = rColor.GetRed() << 8;
-        guint16 nGreen = rColor.GetRed() << 8;
+        guint16 nGreen = rColor.GetGreen() << 8;
         guint16 nBlue = rColor.GetBlue() << 8;
 
         PangoAttrType aFilterAttrs[] = {PANGO_ATTR_FOREGROUND, PANGO_ATTR_WEIGHT, PANGO_ATTR_INVALID};
