c37d00a4f22a973c0905a9d8f409994763cb637a
Hossein <hossein@libreoffice.org>
2022-04-12 16:07:19 +0200

    tdf#143123 - Port Java examples to Python
    Porting FirstSteps/FirstUnoContact.java to Python is done. More
    information on this example can be found in the DevGuide:
    
    LibreOffice Developer's Guide: Chapter 1 - First Steps - First Contact
    https://wiki.documentfoundation.org/Documentation/DevGuide/First_Steps
    
    Change-Id: I8a97672a0b437177f19ddce029e6ef27e97533ec
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/132914
    Tested-by: Jenkins
    Reviewed-by: Hossein <hossein@libreoffice.org>

---
+++ odk/examples/DevelopersGuide/FirstSteps/FirstUnoContact.py
@@ -0,0 +1,24 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+import uno
+import officehelper
+
+try:
+    xContext = officehelper.bootstrap()
+    print("Connected to a running office ...")
+    xMCF = xContext.getServiceManager()
+    available = "not available" if xMCF is None else "available"
+    print("remote ServiceManager is " + available)
+
+except Exception as e:
+    print(e)
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
+
