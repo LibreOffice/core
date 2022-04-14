7a1a07611668ed6a6efc14fa9f133dc27d709733
Michael Weghorn <m.weghorn@posteo.de>
2022-04-14 11:29:27 +0200

    android: Port from Android Support Lib to AndroidX
    Replace the no longer maintained Android Support Library
    with the Android Jetpack libraries.
    
    Quoting [1]:
    
    > Version 28.0.0 is the last release of the Support Library. There will be
    > no more android.support library releases. All new feature development
    > will be in the androidx namespace.
    
    Most porting was done automatically by using Android Studio's
    "Refactor" -> "Migrate to AndroidX..." function.
    
    In `android/source/res/layout/toolbar_bottom.xml`,
    two uses of
    `app:layout_behavior="android.support.design.widget.BottomSheetBehavior"`
    had to be replaced manually as described at [2], because
    the app would crash when using the old "android.support"
    values.
    
    Also drop the Android Support Library related bits from configure.ac
    
    In a quick test, this worked fine and no obvious
    difference was visible when running this in various AVDs.
    
    When trying to test this in an x86 AVD still using
    SDK 16 (Android 4.1), which is currently specified
    as Android Viewer's `minSdkVersion`, only various
    unrelated issues showed up, some of which will be
    handled in follow-up commits.
    
    After the migration, many weird errors showed up in
    Android Studio, which disappeared after invalidating
    the caches (via "File" -> "Invalidate Caches...").
    
    [1] https://developer.android.com/jetpack/androidx
    [2] https://stackoverflow.com/questions/45100963/runtimeexception-could-not-inflate-behavior-subclass
    
    Change-Id: I2a57f0ebd56e7ecc09b7d65aae17fd15088a633b

--- android/source/src/java/org/libreoffice/ColorPickerAdapter.java
+++ android/source/src/java/org/libreoffice/ColorPickerAdapter.java
@@ -3,7 +3,7 @@ package org.libreoffice;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Color;
-import android.support.v7.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
