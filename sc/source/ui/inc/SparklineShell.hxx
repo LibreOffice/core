55619ff3e24dda033e4b2ef22d9ee95272212c83
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2022-04-15 00:55:54 +0900

    sc: introduce SparklineShell and Sparkline context
    Having a separate sparkline context is very useful, so we can add
    a custom UI when the user has the cursor over a sparkline. This
    will allow a "Sparkline" tab for NotebookBar and its own deck in
    Sidebar, activated only when the sparkline is present. Also the
    pop-up menu can be customized specifically for the sparkline, but
    this may be less useful.
    
    For the sparkline context we need a custom shell - SparklineShell
    where now all the UNO commands can be implemented (not done in
    this commit).
    
    Change-Id: Idca2ad946af3afdd1b494744b80c9c093eec602c
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/133022
    Tested-by: Jenkins
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>
    (cherry picked from commit e1f3f2e00c37173e5f6f1cbb5235ab95b100bde7)

---
+++ sc/source/ui/inc/SparklineShell.hxx
@@ -0,0 +1,40 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#pragma once
+
+#include <sfx2/shell.hxx>
+
+#include <shellids.hxx>
+
+class ScTabViewShell;
+class SfxModule;
+
+namespace sc
+{
+class SparklineShell final : public SfxShell
+{
+public:
+    SFX_DECL_INTERFACE(SCID_SPARKLINE_SHELL)
+
+private:
+    /// SfxInterface initializer.
+    static void InitInterface_Impl();
+
+public:
+    SparklineShell(ScTabViewShell* pView);
+    virtual ~SparklineShell() override;
+
+private:
+    ScTabViewShell* m_pViewShell;
+};
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
