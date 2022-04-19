6500106dff0f0cd86f509ffd01542aab77c21596
Noel Grandin <noel.grandin@collabora.co.uk>
2022-04-19 13:48:55 +0200

    tdf#148635 no need to init ChartDataWrapper more than once
    halves the time to finish a chart2
    
    Change-Id: Ib7f066672878f7630c2d1c90b9487a14f2048029
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/133172
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

--- chart2/source/controller/chartapiwrapper/ChartDataWrapper.cxx
+++ chart2/source/controller/chartapiwrapper/ChartDataWrapper.cxx
@@ -577,11 +577,14 @@ void ChartDataWrapper::switchToInternalDataProvider()
     rtl::Reference< ChartModel > xChartDoc( m_spChart2ModelContact->getDocumentModel() );
     if( xChartDoc.is() )
         xChartDoc->createInternalDataProvider( true /*bCloneExistingData*/ );
+    m_xDataAccess.clear();
     initDataAccess();
 }
 
 void ChartDataWrapper::initDataAccess()
 {
+    if (m_xDataAccess)
+        return;
     rtl::Reference< ChartModel > xChartDoc( m_spChart2ModelContact->getDocumentModel() );
     if( !xChartDoc.is() )
         return;
