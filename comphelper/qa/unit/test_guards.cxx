50654c6d1c225f0845680c3d1d58c3582de9f329
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2022-04-19 15:24:08 +0900

    comphelper: cleanup test_guards, add test for ValueRestorationGuard
    Move each test case into its own test function. Also add the
    missing test for ValueRestorationGuard.
    
    Change-Id: I588ab67f82ba82ef67939dac3d22438e8799ce11

--- comphelper/qa/unit/test_guards.cxx
+++ comphelper/qa/unit/test_guards.cxx
@@ -10,47 +10,79 @@
 #include <comphelper/flagguard.hxx>
 #include <unotest/bootstrapfixturebase.hxx>
 
-CPPUNIT_TEST_FIXTURE(CppUnit::TestFixture, test_comphelperGuards)
+CPPUNIT_TEST_FIXTURE(CppUnit::TestFixture, testScopeGuard)
 {
+    // Test that comphelper::ScopeGuard executes its parameter on destruction
+
+    // initial value "true", out-of-scope ScopeGuard function executes and changes the value to "false"
     bool bFlag = true;
     {
-        // Test that comphelper::ScopeGuard executes its parameter on destruction
         comphelper::ScopeGuard aGuard([&bFlag] { bFlag = false; });
         CPPUNIT_ASSERT(bFlag);
     }
     CPPUNIT_ASSERT(!bFlag);
+}
 
-    {
-        // Test that comphelper::FlagGuard properly sets and resets the flag
-        comphelper::FlagGuard aGuard(bFlag);
-        CPPUNIT_ASSERT(bFlag);
-    }
-    CPPUNIT_ASSERT(!bFlag);
+CPPUNIT_TEST_FIXTURE(CppUnit::TestFixture, testFlagGuard)
+{
+    // Test that comphelper::FlagGuard properly sets and resets the flag
 
-    bFlag = true;
+    // initial value "false", change to "true", out-of-scope change to "false"
+    bool bFlag = false;
     {
-        // Test that comphelper::FlagGuard properly sets and resets the flag
         comphelper::FlagGuard aGuard(bFlag);
         CPPUNIT_ASSERT(bFlag);
     }
     // comphelper::FlagGuard must reset flag to false on destruction unconditionally
     CPPUNIT_ASSERT(!bFlag);
 
-    {
-        // Test that comphelper::FlagRestorationGuard properly sets and resets the flag
-        comphelper::FlagRestorationGuard aGuard(bFlag, true);
-        CPPUNIT_ASSERT(bFlag);
-    }
-    CPPUNIT_ASSERT(!bFlag);
-
+    // initial value "true", retain the value at "true", out-of-scope change to "false"
     bFlag = true;
     {
-        // Test that comphelper::FlagRestorationGuard properly sets and resets the flag
+        comphelper::FlagGuard aGuard(bFlag);
+        CPPUNIT_ASSERT(bFlag);
+    }
+    // comphelper::FlagGuard must reset flag to false on destruction unconditionally
+    CPPUNIT_ASSERT(!bFlag);
+}
+
+CPPUNIT_TEST_FIXTURE(CppUnit::TestFixture, testFlagRestorationGuard)
+{
+    // Test that comphelper::FlagRestorationGuard properly sets and resets the flag
+
+    // initial value "true", change to "false", out-of-scope change to "true"
+
+    bool bFlag = true;
+    {
         comphelper::FlagRestorationGuard aGuard(bFlag, false);
         CPPUNIT_ASSERT(!bFlag);
     }
     // comphelper::FlagRestorationGuard must reset flag to initial state on destruction
     CPPUNIT_ASSERT(bFlag);
+}
+
+CPPUNIT_TEST_FIXTURE(CppUnit::TestFixture, testValueRestorationGuard)
+{
+    // Test that comphelper::ValueRestorationGuard properly sets and resets the (int) value
+
+    int value = 199;
+
+    // set value and restore after scope ends
+    {
+        CPPUNIT_ASSERT_EQUAL(199, value);
+        comphelper::ValueRestorationGuard aGuard(value, 100);
+        CPPUNIT_ASSERT_EQUAL(100, value);
+    }
+    CPPUNIT_ASSERT_EQUAL(199, value);
+
+    // set value, manually setto another value and restore after scope ends
+    {
+        CPPUNIT_ASSERT_EQUAL(199, value);
+        comphelper::ValueRestorationGuard aGuard(value, 100);
+        CPPUNIT_ASSERT_EQUAL(100, value);
+        value = 200;
+    }
+    CPPUNIT_ASSERT_EQUAL(199, value);
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
