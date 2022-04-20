64c3bdbcd291c20a83124ba6786dfcc919e607df
Lukas <lukasmolleman@gmail.com>
2022-04-20 19:54:39 +0200

    bin/upload_symbols.py: make pythonic
    Change-Id: I44d86677f64b7b4a4212b94a574a20805bac649b

--- bin/upload_symbols.py
+++ bin/upload_symbols.py
@@ -6,11 +6,16 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 
-import requests, sys
-import platform, configparser
+import configparser
+import platform
 
-def detect_platform():
-    return platform.system()
+import sys
+import requests
+
+
+def detect_platform(detected_platform=platform.system()):
+    return detected_platform
+
 
 def main():
     if len(sys.argv) < 4:
@@ -29,9 +34,9 @@ def main():
     user = config["CrashReport"]["User"]
     password = config["CrashReport"]["Password"]
 
-    platform = detect_platform()
+    detected_platform = detect_platform()
     files = {'symbols': open(sys.argv[1], 'rb')}
-    data = {'version': sys.argv[3], 'platform': platform}
+    data = {'version': sys.argv[3], 'platform': detected_platform}
 
     if len(sys.argv) > 4 and sys.argv[4] == "--system":
         data['system'] = True
@@ -40,14 +45,15 @@ def main():
     session.get(login_url)
     csrftoken = session.cookies['csrftoken']
 
-    login_data = { 'username': user,'password': password,
-            'csrfmiddlewaretoken': csrftoken }
-    headers = { "Referer": base_url }
-    r1 = session.post(login_url, headers=headers, data=login_data)
+    login_data = {'username': user, 'password': password,
+                  'csrfmiddlewaretoken': csrftoken}
+    headers = {"Referer": base_url}
+    session.post(login_url, headers=headers, data=login_data)
 
     data['csrfmiddlewaretoken'] = csrftoken
 
-    r = session.post(upload_url, headers=headers, files=files, data=data)
+    session.post(upload_url, headers=headers, files=files, data=data)
+
 
 if __name__ == "__main__":
     main()
