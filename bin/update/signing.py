7be64d3d091020a347aa883eac5a87e8f48b3f0d
Lukas <lukasmolleman@gmail.com>
2022-04-20 19:54:39 +0200

    android and bin/update: make pythonic
    Change-Id: Iaf791bfa8d9822843b26f2a2f2c3d94c55a60a0b

--- bin/update/signing.py
+++ bin/update/signing.py
@@ -4,9 +4,12 @@ import os
 import subprocess
 import path
 
+
 def sign_mar_file(target_dir, config, mar_file, filename_prefix):
     signed_mar_file = make_complete_mar_name(target_dir, filename_prefix + '_signed')
     mar_executable = os.environ.get('MAR', 'mar')
-    subprocess.check_call([mar_executable, '-C', path.convert_to_native(target_dir), '-d', path.convert_to_native(config.certificate_path), '-n', config.certificate_name, '-s', path.convert_to_native(mar_file), path.convert_to_native(signed_mar_file)])
+    subprocess.check_call([mar_executable, '-C', path.convert_to_native(target_dir), '-d',
+                           path.convert_to_native(config.certificate_path), '-n', config.certificate_name, '-s',
+                           path.convert_to_native(mar_file), path.convert_to_native(signed_mar_file)])
 
     os.rename(signed_mar_file, mar_file)
