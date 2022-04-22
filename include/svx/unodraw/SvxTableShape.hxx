f88ea1eb91e6ab2b6a1ec53ca1451ea3d2bfe714
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2022-04-22 16:53:32 +0900

    svx: move SvxTableShape into own file and externalize
    Change-Id: I0c4b37ebafa56ed50286bdcec1f2a8d5e1362f8d

---
+++ include/svx/unodraw/SvxTableShape.hxx
@@ -0,0 +1,44 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#pragma once
+
+#include <svx/svxdllapi.h>
+#include <svx/unoshape.hxx>
+
+class SVXCORE_DLLPUBLIC SvxTableShape : public SvxShape
+{
+protected:
+    // override these for special property handling in subcasses. Return true if property is handled
+    virtual bool setPropertyValueImpl(const OUString& rName,
+                                      const SfxItemPropertyMapEntry* pProperty,
+                                      const css::uno::Any& rValue) override;
+    virtual bool getPropertyValueImpl(const OUString& rName,
+                                      const SfxItemPropertyMapEntry* pProperty,
+                                      css::uno::Any& rValue) override;
+
+    virtual void lock() override;
+    virtual void unlock() override;
+
+public:
+    explicit SvxTableShape(SdrObject* pObj);
+    virtual ~SvxTableShape() noexcept override;
+};
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
