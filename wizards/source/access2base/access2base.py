4679819f91e363bb8f46b7644d7e2064d24afeb5
Jean-Pierre Ledure <jp@ledure.be>
2022-04-22 16:28:12 +0200

    Access2Base - (access2base.py) Don't use bare except: statement
    The exception hierarchy described on page
      https://docs.python.org/3.5/library/exceptions.html#exception-hierarchy
    indicates that the default exception in bare
      except:
    statements is BaseException. This induces that the SystemExit,
    KeyboardInterrupt and GeneratorExit are also handled by the
    user script.
    
    This is a not recommended practice.
    
    Better is to use the explicit Exception built-in exception
      except Exception:
    
    Bug reported by Paul M on Telegram
    
    Change-Id: Ie1ae1f732ebc60a881e7d40ba8141aa704e9cd5c

--- wizards/source/access2base/access2base.py
+++ wizards/source/access2base/access2base.py
@@ -43,7 +43,7 @@ from platform import system as _opsys
 import datetime, os, sys, traceback
 
 _LIBRARY = ''               # Should be 'Access2Base' or 'Access2BaseDev'
-_VERSION = '7.1'            # Actual version number
+_VERSION = '7.4'            # Actual version number
 _WRAPPERMODULE = 'Python'   # Module name in the Access2Base library containing Python interfaces
 
 # CallByName types
@@ -608,7 +608,7 @@ class _A2B(object, metaclass = _Singleton):
         Script = cls.xScript(script, module)
         try:
             Returned = Script.invoke((args), (), ())[0]
-        except:
+        except Exception:
             raise TypeError("Access2Base error: method '" + script + "' in Basic module '" + module + "' call error. Check its arguments.")
         else:
             if Returned == None:
@@ -643,7 +643,7 @@ class _A2B(object, metaclass = _Singleton):
                 args = (action,) + (basic,) + (script,) + args
             try:
                 Returned = Script.invoke((args), (), ())
-            except:
+            except Exception:
                 raise TypeError("Access2Base error: method '" + script + "' call error. Check its arguments.")
 
         if isinstance(Returned[0], tuple):
