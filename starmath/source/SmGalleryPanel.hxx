deb38f243dd5386369b74c906d8fbee30ceb1af7
Mike Kaganski <mike.kaganski@collabora.com>
2022-04-25 18:24:43 +0300

    tdf#125931: Add Gallery to Math's sidebar, with Elements contents
    Change-Id: Ie04a5dc48de38f318f0ddb42efc42cea022dc62c

---
+++ starmath/source/SmGalleryPanel.hxx
@@ -0,0 +1,59 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#pragma once
+
+#include <sal/config.h>
+
+#include <sfx2/bindings.hxx>
+#include <sfx2/sidebar/IContextChangeReceiver.hxx>
+#include <sfx2/sidebar/SidebarModelUpdate.hxx>
+#include <sfx2/sidebar/PanelLayout.hxx>
+#include <vcl/customweld.hxx>
+#include <vcl/EnumContext.hxx>
+
+#include <ElementsDockingWindow.hxx>
+
+#include <memory>
+
+namespace sm::sidebar
+{
+class SmGalleryPanel : public PanelLayout
+{
+public:
+    static std::unique_ptr<PanelLayout> Create(weld::Widget& rParent, const SfxBindings& rBindings);
+    SmGalleryPanel(weld::Widget& rParent, const SfxBindings& rBindings);
+    ~SmGalleryPanel();
+
+private:
+    DECL_LINK(CategorySelectedHandle, weld::TreeView&, void);
+    DECL_LINK(ElementClickHandler, SmElement&, void);
+
+    SmViewShell* GetView() const;
+
+    const SfxBindings& mrBindings;
+
+    std::unique_ptr<weld::TreeView> mxCategoryList;
+    std::unique_ptr<SmElementsControl> mxElementsControl;
+    std::unique_ptr<weld::CustomWeld> mxElementsControlWin;
+};
+
+} // end of namespace sm::sidebar
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
