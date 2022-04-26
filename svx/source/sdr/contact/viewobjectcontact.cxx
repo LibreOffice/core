c5e6aa9d78d7653d69a412fc54fd428b77bfc25e
Pranam Lashkari <lpranam@collabora.com>
2022-04-26 22:12:48 +0530

    svx: calculate object range before using it
    Change-Id: I01f0176542e9d6f36a2f7bbc870a9e337705a5a6

--- svx/source/sdr/contact/viewobjectcontact.cxx
+++ svx/source/sdr/contact/viewobjectcontact.cxx
@@ -161,7 +161,7 @@ ViewObjectContact::ViewObjectContact(ObjectContact& rObjectContact, ViewContact&
 ViewObjectContact::~ViewObjectContact()
 {
     // invalidate in view
-    if(!maObjectRange.isEmpty())
+    if(!getObjectRange().isEmpty())
     {
         GetObjectContact().InvalidatePartOfView(maObjectRange);
     }
@@ -219,7 +219,7 @@ void ViewObjectContact::ActionChanged()
     // force ObjectRange
     getObjectRange();
 
-    if(!maObjectRange.isEmpty())
+    if(!getObjectRange().isEmpty())
     {
         // invalidate current valid range
         GetObjectContact().InvalidatePartOfView(maObjectRange);
@@ -246,7 +246,7 @@ void ViewObjectContact::triggerLazyInvalidate()
     // force ObjectRange
     getObjectRange();
 
-    if(!maObjectRange.isEmpty())
+    if(!getObjectRange().isEmpty())
     {
         // invalidate current valid range
         GetObjectContact().InvalidatePartOfView(maObjectRange);
