1774fdbee9838133fb57d30d39b8bc624907558d
Samuel Mehrbrodt <samuel.mehrbrodt@allotropia.de>
2022-04-27 11:45:04 +0200

    tdf#99708 Save formula bar height to document
    Save the current state of the Calc formula bar to the document.
    Number of visible lines is saved into the document settings
    and restored when loading that document.
    
    Also adds a UNO property, so that the formula bar height can be
    changed via UNO.
    
    Change-Id: Ifef0c9e42cb4f7465516629d2c22974367e0eb33

--- sc/source/ui/inc/viewdata.hxx
+++ sc/source/ui/inc/viewdata.hxx
@@ -329,6 +329,7 @@ private:
     bool                bMoveArea:1;
 
     bool                bGrowing;
+    sal_Int16           nFormulaBarLines;           // Visible lines in the formula bar
 
     tools::Long                m_nLOKPageUpDownOffset;
     tools::Rectangle    maLOKVisibleArea;///< The visible area in the LibreOfficeKit client.
@@ -466,6 +467,15 @@ public:
 
     double          GetPPTX() const { return nPPTX; }
     double          GetPPTY() const { return nPPTY; }
+
+    void SetFormulaBarLines(sal_Int16 nLines)
+    {
+        // Formula bar must be between 1 and 25 lines (see SpreadsheetViewSettings.idl)
+        nLines = std::max(nLines, static_cast<sal_Int16>(1));
+        nLines = std::min(nLines, static_cast<sal_Int16>(25));
+        nFormulaBarLines = nLines;
+    }
+    sal_Int16 GetFormulaBarLines() const { return nFormulaBarLines; };
 
     ScMarkType      GetSimpleArea( SCCOL& rStartCol, SCROW& rStartRow, SCTAB& rStartTab,
                                     SCCOL& rEndCol, SCROW& rEndRow, SCTAB& rEndTab ) const;
