8672f95ab410a42667fc8655bfc42f583d62e492
Mike Kaganski <mike.kaganski@collabora.com>
2022-04-27 15:06:03 +0300

    tdf#125931: add Properties panel to Math's Properties deck
    For now, it only contains buttons opening the dialogs accessible
    from Format menu. This is enough for the initial implementation,
    and should later be changed to individual controls allowing to
    set the formula properties directly.
    
    Change-Id: Ia0e52915198ddb2648d13d577d55b367f178b1ad
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/133508
    Tested-by: Jenkins
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/135401
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

---
+++ starmath/source/SmPropertiesPanel.cxx
@@ -0,0 +1,88 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#include <sal/config.h>
+
+#include <com/sun/star/beans/PropertyValue.hpp>
+#include <com/sun/star/frame/theUICommandDescription.hpp>
+#include <com/sun/star/uno/Sequence.hxx>
+
+#include <comphelper/dispatchcommand.hxx>
+#include <comphelper/processfactory.hxx>
+#include <comphelper/sequenceashashmap.hxx>
+
+#include "SmPropertiesPanel.hxx"
+
+namespace sm::sidebar
+{
+// static
+std::unique_ptr<PanelLayout> SmPropertiesPanel::Create(weld::Widget& rParent)
+{
+    return std::make_unique<SmPropertiesPanel>(rParent);
+}
+
+SmPropertiesPanel::SmPropertiesPanel(weld::Widget& rParent)
+    : PanelLayout(&rParent, "MathPropertiesPanel", "modules/smath/ui/sidebarproperties_math.ui")
+    , mpFormatFontsButton(m_xBuilder->weld_button("btnFormatFonts"))
+    , mpFormatFontSizeButton(m_xBuilder->weld_button("btnFormatFontSize"))
+    , mpFormatSpacingButton(m_xBuilder->weld_button("btnFormatSpacing"))
+    , mpFormatAlignmentButton(m_xBuilder->weld_button("btnFormatAlignment"))
+    , maButtonCommands{ { mpFormatFontsButton.get(), ".uno:ChangeFont" },
+                        { mpFormatFontSizeButton.get(), ".uno:ChangeFontSize" },
+                        { mpFormatSpacingButton.get(), ".uno:ChangeDistance" },
+                        { mpFormatAlignmentButton.get(), ".uno:ChangeAlignment" } }
+{
+    // Set localized labels to the buttons
+    auto xConfs
+        = css::frame::theUICommandDescription::get(comphelper::getProcessComponentContext());
+    if (css::uno::Reference<css::container::XNameAccess> xConf{
+            xConfs->getByName("com.sun.star.formula.FormulaProperties"), css::uno::UNO_QUERY })
+    {
+        for (const auto & [ button, command ] : maButtonCommands)
+        {
+            comphelper::SequenceAsHashMap props(xConf->getByName(command));
+            button->set_label(props.getUnpackedValueOrDefault("Name", button->get_label()));
+        }
+    }
+
+    mpFormatFontsButton->connect_clicked(LINK(this, SmPropertiesPanel, ButtonClickHandler));
+    mpFormatFontSizeButton->connect_clicked(LINK(this, SmPropertiesPanel, ButtonClickHandler));
+    mpFormatSpacingButton->connect_clicked(LINK(this, SmPropertiesPanel, ButtonClickHandler));
+    mpFormatAlignmentButton->connect_clicked(LINK(this, SmPropertiesPanel, ButtonClickHandler));
+}
+
+SmPropertiesPanel::~SmPropertiesPanel()
+{
+    maButtonCommands.clear();
+
+    mpFormatFontsButton.reset();
+    mpFormatFontSizeButton.reset();
+    mpFormatSpacingButton.reset();
+    mpFormatAlignmentButton.reset();
+}
+
+IMPL_LINK(SmPropertiesPanel, ButtonClickHandler, weld::Button&, rButton, void)
+{
+    if (OUString command = maButtonCommands[&rButton]; !command.isEmpty())
+        comphelper::dispatchCommand(command, {});
+}
+
+} // end of namespace sm::sidebar
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
