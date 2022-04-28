2e3e0788d19c7368757672ef008e70a9d0af3893
Xisco Fauli <xiscofauli@libreoffice.org>
2022-04-28 10:59:46 +0200

    tdf#37864, tdf#147496: sc: Add UItest
    Change-Id: Idfcf34a0101158f54bd73efc8629ea8e660a3b11

---
+++ sc/qa/uitest/function_wizard/tdf37864.py
@@ -0,0 +1,35 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+from uitest.framework import UITestCase
+from uitest.uihelper.common import get_state_as_dict
+from uitest.uihelper.calc import enter_text_to_cell
+from libreoffice.uno.propertyvalue import mkPropertyValues
+
+class tdf37864(UITestCase):
+
+    def test_tdf37864(self):
+
+        with self.ui_test.create_doc_in_start_center("calc") as document:
+            xCalcDoc = self.xUITest.getTopFocusWindow()
+            gridwin = xCalcDoc.getChild("grid_window")
+            enter_text_to_cell(gridwin, "A1", "test")
+
+            gridwin.executeAction("SELECT", mkPropertyValues({"CELL": "A1"}))
+
+            self.xUITest.executeCommand(".uno:Copy")
+
+            with self.ui_test.execute_modeless_dialog_through_command(".uno:FunctionDialog") as xDialog:
+                xFormula = xDialog.getChild("ed_formula")
+                xFormula.executeAction("TYPE", mkPropertyValues({"KEYCODE": 'CTRL+v'}))
+
+                # Without the fix in place, this test would have failed with
+                # AssertionError: '=test' != '=test\n'
+                self.assertEqual("=test", get_state_as_dict(xFormula)["Text"])
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
