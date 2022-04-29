8fe5abd87de8eb4df3c6c2f311f130ad00ff5815
Miklos Vajna <vmiklos@collabora.com>
2022-04-29 12:10:13 +0200

    tdf#148683 sw: fix crash on deleting text with redlining enabled, but hidden
    Regression from commit 32902f66e7749b2d06d13f50416be5323a0c0ea9
    (sw_redlinehide: make layout based Show/Hide mode the default,
    2018-11-30), deleting some text in the middle of the paragraph with the
    bugdoc results in an assertion failure in the SwDrawTextInfo ctor.
    
    Normally this doesn't happen on text deletion as we already have a
    mechanism for truncating no longer needed lines in
    SwTextFrame::Format_(), though that only checks the length of the
    current line.
    
    Fix the problem by extending this mechanism to also check the remaining
    lines: if change tracking is on and the total of all line lengths is
    more than the string length of the text frame, then time to
    re-calculate.
    
    It seems this is not needed in practice when redlining is disabled,
    though we might want to enable this for the non-redline case as well in
    the future.
    
    (cherry picked from commit 515bf5d4afa3a8ed413fd6f17f66fa98b6dbf29e)
    
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/133684
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
    Tested-by: Jenkins
    Signed-off-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/133693
    (cherry picked from commit 3b539b30c3a6559d6b4cf32c2b02fc5d81a0b794)
    
    Change-Id: Iede03e11daceb3c2b614a301e21560b075a60c01
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/133888
    Tested-by: Michael Stahl <michael.stahl@allotropia.de>
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

--- sw/qa/core/text/text.cxx
+++ sw/qa/core/text/text.cxx
@@ -8,7 +8,12 @@
  */
 
 #include <swmodeltestbase.hxx>
+
+#include <rootfrm.hxx>
+#include <unotxdoc.hxx>
 #include <wrtsh.hxx>
+#include <fmtfsize.hxx>
+#include <IDocumentRedlineAccess.hxx>
 
 static char const DATA_DIRECTORY[] = "/sw/qa/core/text/data/";
 
@@ -21,7 +26,10 @@ public:
 
 SwDoc* SwCoreTextTest::createDoc(const char* pName)
 {
-    load(DATA_DIRECTORY, pName);
+    if (!pName)
+        loadURL("private:factory/swriter", nullptr);
+    else
+        load(DATA_DIRECTORY, pName);
 
     SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument*>(mxComponent.get());
     CPPUNIT_ASSERT(pTextDoc);
@@ -44,6 +52,47 @@ CPPUNIT_TEST_FIXTURE(SwCoreTextTest, testFootnoteConnect)
     // Without the accompanying fix in place, this test would have crashed due to a use-after-free
     // in SwFootnoteFrame::GetRef().
     pWrtShell->DelLeft();
+}
+
+CPPUNIT_TEST_FIXTURE(SwCoreTextTest, testRedlineDelete)
+{
+    // Given a document with A4 paper size, some text, redlining on, but hidden:
+    SwDoc* pDoc = createDoc();
+    SwDocShell* pDocShell = pDoc->GetDocShell();
+    SwWrtShell* pWrtShell = pDocShell->GetWrtShell();
+    {
+        // Set page size to A4.
+        size_t nCurIdx = pWrtShell->GetCurPageDesc();
+        SwPageDesc aPageDesc(pWrtShell->GetPageDesc(nCurIdx));
+        SwFrameFormat& rMaster = aPageDesc.GetMaster();
+        SwFormatFrameSize aSize(ATT_FIX_SIZE);
+        aSize.SetSize(Size(11906, 16838));
+        rMaster.SetFormatAttr(aSize);
+        pWrtShell->ChgPageDesc(nCurIdx, aPageDesc);
+    }
+    OUString aBefore("aaaaaaaaa aaaaaaaaaa aa aa aa ");
+    OUString aDelete("delete eeeeeeeeeee ee eeeeeeeeeee ee eeeeee");
+    pWrtShell->Insert(aBefore + " " + aDelete
+                      + " zz zzz zzzzzzzzz zzz zzzz zzzz zzzzzzzzz zzzzzz zzz zzzzzzzzzzz zzz");
+    // Enable redlining.
+    pDocShell->SetChangeRecording(/*bActivate=*/true);
+    // Hide redlining.
+    pWrtShell->StartAllAction();
+    pWrtShell->GetLayout()->SetHideRedlines(true);
+    pWrtShell->EndAllAction();
+
+    // When deleting content in the middle of the paragraph:
+    pWrtShell->SttEndDoc(/*bStt=*/true);
+    pWrtShell->Right(CRSR_SKIP_CHARS, /*bSelect=*/false, /*nCount=*/aBefore.getLength(),
+                     /*bBasicCall=*/false);
+    pWrtShell->Right(CRSR_SKIP_CHARS, /*bSelect=*/true, /*nCount=*/aDelete.getLength(),
+                     /*bBasicCall=*/false);
+    // Without the accompanying fix in place, this test would have crashed:
+    pWrtShell->Delete();
+
+    // Then make sure that the redline is created:
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(1),
+                         pDoc->getIDocumentRedlineAccess().GetRedlineTable().size());
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();
