a8978b93f36d06d737c263e4d91d50bbae2087e6
Luboš Luňák <l.lunak@collabora.com>
2022-05-02 10:52:50 +0200

    remove the o3tl::lru_map "unlimited" hack
    It didn't quite make sense before that maxsize == 0 meant no cleanup,
    and now with items optionally being count as having size more than 1
    it makes even less sense to limit the size to max_size() of the
    containers. This comes from 16a338e173083954a9932a3a4005f17230,
    so instead make that special caller pass a large size, which in
    practice is the same.
    
    Change-Id: Id875862126200ba889211f6e4079ae5921f27650

--- o3tl/qa/test-lru_map.cxx
+++ o3tl/qa/test-lru_map.cxx
@@ -27,7 +27,6 @@ public:
     void testLruRemoval();
     void testCustomHash();
     void testRemoveIf();
-    void testNoAutoCleanup();
     void testChangeMaxSize();
     void testCustomItemSize();
 
@@ -38,7 +37,6 @@ public:
     CPPUNIT_TEST(testLruRemoval);
     CPPUNIT_TEST(testCustomHash);
     CPPUNIT_TEST(testRemoveIf);
-    CPPUNIT_TEST(testNoAutoCleanup);
     CPPUNIT_TEST(testChangeMaxSize);
     CPPUNIT_TEST(testCustomItemSize);
     CPPUNIT_TEST_SUITE_END();
@@ -293,24 +291,6 @@ void lru_map_test::testRemoveIf()
     // remove the only item
     lru.remove_if([](IntMapPair const& rPair) { return (rPair.first == 7); });
     CPPUNIT_ASSERT_EQUAL(size_t(0), lru.size());
-}
-
-void lru_map_test::testNoAutoCleanup()
-{
-    o3tl::lru_map<int, int> lru(0);
-    CPPUNIT_ASSERT_EQUAL(size_t(0), lru.size());
-    lru.insert({ 0, 0 });
-    lru.insert({ 1, 1 });
-    CPPUNIT_ASSERT_EQUAL(size_t(2), lru.size());
-    lru.insert({ 0, 0 });
-    CPPUNIT_ASSERT_EQUAL(size_t(2), lru.size());
-
-    int i = 0;
-    for (auto& rPair : lru)
-    {
-        CPPUNIT_ASSERT_EQUAL(i, rPair.first);
-        ++i;
-    }
 }
 
 void lru_map_test::testChangeMaxSize()
