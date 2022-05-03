3b289af405b4ae0a1ba65ad37ff371dae46fb730
Caolán McNamara <caolanm@redhat.com>
2022-05-03 12:42:04 +0100

    tdf#148477 caching MultiSalLayout doesn't reliably work if AdjustLayout used
    use SalLayoutFlags::GlyphItemsOnly to return before AdjustLayout is
    called and cache that
    
    Change-Id: I0592469205f370b59c5fc81822b97b0cef6a78f2

--- vcl/source/window/status.cxx
+++ vcl/source/window/status.cxx
@@ -390,11 +390,10 @@ void StatusBar::ImplDrawItem(vcl::RenderContext& rRenderContext, bool bOffScreen
     if (!(pItem->mnBits & StatusBarItemBits::UserDraw))
     {
         SalLayout* pLayoutCache = pItem->mxLayoutCache.get();
-
         if(!pLayoutCache)
         {
             // update cache
-            pItem->mxLayoutCache = rRenderContext.ImplLayout(pItem->maText, 0, -1);
+            pItem->mxLayoutCache = rRenderContext.ImplLayout(pItem->maText, 0, -1, Point(0, 0), 0, {}, SalLayoutFlags::GlyphItemsOnly);
             pLayoutCache = pItem->mxLayoutCache.get();
         }
 
@@ -1146,7 +1145,8 @@ void StatusBar::SetItemText( sal_uInt16 nItemId, const OUString& rText, int nCha
     }
     else
     {
-        std::unique_ptr<SalLayout> pSalLayout = GetOutDev()->ImplLayout(pItem->maText,0,-1);
+        std::unique_ptr<SalLayout> pSalLayout = GetOutDev()->ImplLayout(pItem->maText,0,-1,
+            Point(0, 0), 0, {}, SalLayoutFlags::GlyphItemsOnly);
         const SalLayoutGlyphs glyphs = pSalLayout ? pSalLayout->GetGlyphs() : SalLayoutGlyphs();
         nWidth = GetTextWidth( pItem->maText,0,-1,nullptr,pSalLayout ? &glyphs : nullptr) + nFudge;
         // Store the calculated layout.
