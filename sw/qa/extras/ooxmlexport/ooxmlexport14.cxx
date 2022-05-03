LibreOffice/core

commit c46950fee11f5207fb8324947280cd565ae483e7

tdf#143583 DOCX import: fix lost empty paragraphs of footnotes
Last empty paragraphs of footnotes were removed (except
in the case of the first footnote), related to the double
call of RemoveLastParagraph() during footnote load and
later during its copying.

Regression from commit 9b39ce0e66acfe812e1d50e530dc2ccdef3e1357
"tdf#76260 DOCX import: fix slow footnote import".

Change-Id: I61d9aa6765f3af1893451684dde12c199251d06b
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/120270
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/qa/extras/ooxmlexport/ooxmlexport14.cxx
@@ -1177,6 +1177,19 @@ DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testTdf143399, "tdf143399.docx")
     assertXPathContent(pXml2, "/w:endnotes/w:endnote[3]/w:p/w:r[3]/w:t[1]", "Endnotes");
 }
 
+DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testTdf143583, "tdf143583_emptyParaAtEndOfFootnote.docx")
+{
+    xmlDocUniquePtr pXml = parseExport("word/footnotes.xml");
+    CPPUNIT_ASSERT(pXml);
+    assertXPath(pXml, "/w:footnotes/w:footnote[3]/w:p", 2);
+    // This was 1
+    assertXPath(pXml, "/w:footnotes/w:footnote[4]/w:p", 2);
+    // This was 2
+    assertXPath(pXml, "/w:footnotes/w:footnote[5]/w:p", 3);
+    // This was 2
+    assertXPath(pXml, "/w:footnotes/w:footnote[6]/w:p", 3);
+}
+
 DECLARE_OOXMLEXPORT_TEST(testContSectBreakHeaderFooter, "cont-sect-break-header-footer.docx")
 {
     // Load a document with a continuous section break on page 2.

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -3210,6 +3210,7 @@ void DomainMapper_Impl::PopFootOrEndnote()
     // FIXME: add footnote IDs to handle possible differences in footnote serialization
     uno::Reference< text::XFootnotesSupplier> xFootnotesSupplier( GetTextDocument(), uno::UNO_QUERY );
     uno::Reference< text::XEndnotesSupplier> xEndnotesSupplier( GetTextDocument(), uno::UNO_QUERY );
+    bool bCopied = false;
     if ( IsInFootOrEndnote() && ( ( IsInFootnote() && GetFootnoteCount() > -1 && xFootnotesSupplier.is() ) ||
          ( !IsInFootnote() && GetEndnoteCount() > -1 && xEndnotesSupplier.is() ) ) )
     {
@@ -3251,11 +3252,12 @@ void DomainMapper_Impl::PopFootOrEndnote()
 
                 // remove temporary footnote
                 xFootnoteFirst->getAnchor()->setString("");
+                bCopied = true;
             }
         }
     }
 
-    if (!IsRTFImport())
+    if (!IsRTFImport() && !bCopied)
         RemoveLastParagraph();
 
     // In case the foot or endnote did not contain a tab.

commit 7b8c7f053537d8bc782c7e9515292d572448de85

tdf#146346 DOCX import: fix table margins in footnotes
Paragraph of the anchoring point of the floating table
needs zero top and bottom margins, if the table was a
not floating table in the footnote originally, otherwise
docDefault (Standard) margins could result bigger vertical
spaces around the table.

Quasi regression from commit c46950fee11f5207fb8324947280cd565ae483e7
"tdf#143583 DOCX import: fix lost empty paragraphs of footnotes"

See also commit e11c51eefe8c3210cef2b5850f401ba67a401d01
"tdf#95806 tdf#125877 tdf#141172 DOCX: fix tables in footnotes".

Change-Id: I4180a6f7d7c49faf0842637b23aa1c8c5680a76c

writerfilter/source/dmapper/PropertyMap.cxx
@@ -1468,8 +1468,25 @@ void SectionPropertyMap::CloseSectionGroup( DomainMapper_Impl& rDM_Impl )
                     }
                 }
 
-                xBodyText->convertToTextFrame(rInfo.m_xStart, rInfo.m_xEnd,
+                const uno::Reference< text::XTextContent >& xTextContent =
+                        xBodyText->convertToTextFrame(rInfo.m_xStart, rInfo.m_xEnd,
                                               rInfo.m_aFrameProperties);
+
+                // paragraph of the anchoring point of the floating table needs zero top and bottom
+                // margins, if the table was a not floating table in the footnote, otherwise
+                // docDefault margins could result bigger vertical spaces around the table
+                if ( rInfo.m_bConvertToFloatingInFootnote && xTextContent.is() )
+                {
+                    uno::Reference<beans::XPropertySet> xParagraph(
+                                    xTextContent->getAnchor(), uno::UNO_QUERY);
+                    if ( xParagraph.is() )
+                    {
+                        xParagraph->setPropertyValue("ParaTopMargin",
+                                    uno::makeAny(static_cast<sal_Int32>(0)));
+                        xParagraph->setPropertyValue("ParaBottomMargin",
+                                    uno::makeAny(static_cast<sal_Int32>(0)));
+                    }
+                }
 
                 uno::Reference<text::XTextTablesSupplier> xTextDocument(rDM_Impl.GetTextDocument(), uno::UNO_QUERY);
                 uno::Reference<container::XNameAccess> xTables = xTextDocument->getTextTables();

sw/qa/extras/ooxmlexport/ooxmlexport14.cxx
@@ -1253,6 +1253,12 @@ CPPUNIT_TEST_FIXTURE(Test, testTdf143583)
     assertXPath(pXml, "/w:footnotes/w:footnote[6]/w:p", 3);
 }
 
+DECLARE_OOXMLEXPORT_TEST(testTdf146346, "tdf146346.docx")
+{
+    // This was 2 (by bad docDefault vertical margins around tables in footnotes)
+    CPPUNIT_ASSERT_EQUAL(1, getPages());
+}
+
 DECLARE_OOXMLEXPORT_TEST(testContSectBreakHeaderFooter, "cont-sect-break-header-footer.docx")
 {
     // Load a document with a continuous section break on page 2.
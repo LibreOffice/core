7b8c7f053537d8bc782c7e9515292d572448de85
László Németh <nemeth@numbertext.org>
2022-05-03 11:06:59 +0200

    tdf#146346 DOCX import: fix table margins in footnotes
    Paragraph of the anchoring point of the floating table
    needs zero top and bottom margins, if the table was a
    not floating table in the footnote originally, otherwise
    docDefault (Standard) margins could result bigger vertical
    spaces around the table.
    
    Quasi regression from commit c46950fee11f5207fb8324947280cd565ae483e7
    "tdf#143583 DOCX import: fix lost empty paragraphs of footnotes"
    
    See also commit e11c51eefe8c3210cef2b5850f401ba67a401d01
    "tdf#95806 tdf#125877 tdf#141172 DOCX: fix tables in footnotes".
    
    Change-Id: I4180a6f7d7c49faf0842637b23aa1c8c5680a76c

--- sw/qa/extras/ooxmlexport/ooxmlexport14.cxx
+++ sw/qa/extras/ooxmlexport/ooxmlexport14.cxx
@@ -1253,6 +1253,12 @@ CPPUNIT_TEST_FIXTURE(Test, testTdf143583)
     assertXPath(pXml, "/w:footnotes/w:footnote[6]/w:p", 3);
 }
 
+DECLARE_OOXMLEXPORT_TEST(testTdf146346, "tdf146346.docx")
+{
+    // This was 2 (by bad docDefault vertical margins around tables in footnotes)
+    CPPUNIT_ASSERT_EQUAL(1, getPages());
+}
+
 DECLARE_OOXMLEXPORT_TEST(testContSectBreakHeaderFooter, "cont-sect-break-header-footer.docx")
 {
     // Load a document with a continuous section break on page 2.
