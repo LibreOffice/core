4d97d7aeebf80a514ff604764b288363c2197a8e
László Németh <nemeth@numbertext.org>
2022-05-05 12:04:47 +0200

    tdf#128744 sw DOCX: unprotect change tracking with verification
    Unprotect change tracking only by password verification
    instead of 1) unprotecting without the password or 2) rejecting
    the correct password.
    
    I.e. now 1) clicking on Record changes icon of Track Changes
    toolbar or Edit->Track Changes->Record asks for a password, and
    2) Unprotect Record changes on Security page of
    File->Properties... accepts the correct password with disabling
    record changes.
    
    Show also "Invalid password!" dialog disabling Record Changes
    by its icon or menu option, like Properties... dialog window does,
    if the password is invalid.
    
    Note: Still allow to unprotect OpenDocument export of a
    protected OOXML import document, because that doesn't contain
    the original password info, only a dummy RedlinePassword.
    (OpenDocument exports protect Track Changes with the simple
    RedlineProtectionKey configuration setting, so it's not
    possible to map the OOXML password info to OpenDocument without
    extending this.)
    
    Follow-up to commit d416250f4f1766e2d596ea3feef6a94b7adf29f4
    "tdf#106843 DOCX: forbid disabling protected Record Changes".
    
    See also commit bfd7730f4cf002a79dc9c02c23286850fee3f12a
    "tdf#89383 DOCX import: fix permission for editing".
    
    Change-Id: Iafcf4a6b551a7e8485d4311aee889c2522526d71

---
+++ sw/qa/uitest/writer_tests7/tdf128744.py
@@ -0,0 +1,71 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+from uitest.framework import UITestCase
+from libreoffice.uno.propertyvalue import mkPropertyValues
+from uitest.uihelper.common import get_state_as_dict
+from uitest.uihelper.common import select_pos
+from uitest.uihelper.common import get_url_for_data_file
+
+class tdf128744(UITestCase):
+
+   def test_tdf128744(self):
+        # load the sample file
+        with self.ui_test.load_file(get_url_for_data_file("tdf128744.docx")):
+
+            # first try to unprotect Record Changes with an invalid password
+
+            with self.ui_test.execute_dialog_through_command(".uno:SetDocumentProperties") as xDialog:
+                xRecordChangesCheckbox = xDialog.getChild("recordchanges")
+                self.assertEqual(get_state_as_dict(xRecordChangesCheckbox)["Selected"], "true")
+                xTabs = xDialog.getChild("tabcontrol")
+                select_pos(xTabs, "3")     #tab Security
+                xProtectBtn = xDialog.getChild("protect")
+
+                # No close_button: click on the "Ok" inside to check the "Invalid password" infobox
+                with self.ui_test.execute_blocking_action(xProtectBtn.executeAction, args=('CLICK', ()), close_button="") as xPasswordDialog:
+                    self.assertEqual(get_state_as_dict(xPasswordDialog)["DisplayText"], "Enter Password")
+                    xPassword = xPasswordDialog.getChild("pass1ed")
+                    xPassword.executeAction("TYPE", mkPropertyValues({"TEXT": "bad password"}))
+                    print(xPasswordDialog.getChildren())
+                    xOkBtn = xPasswordDialog.getChild("ok")
+                    with self.ui_test.execute_blocking_action(xOkBtn.executeAction, args=('CLICK', ())) as xInfoBox:
+                        # "Invalid password" infobox
+                        self.assertEqual(get_state_as_dict(xInfoBox)["DisplayText"], 'Information')
+
+            # now open the dialog again and read the properties, Record Changes checkbox is still enabled
+            with self.ui_test.execute_dialog_through_command(".uno:SetDocumentProperties", close_button="cancel") as xDialog:
+                xRecordChangesCheckbox = xDialog.getChild("recordchanges")
+                self.assertEqual(get_state_as_dict(xRecordChangesCheckbox)["Selected"], "true")
+                xResetBtn = xDialog.getChild("reset")
+                xResetBtn.executeAction("CLICK", tuple())
+
+            # unprotect Record Changes with the valid password "test"
+
+            with self.ui_test.execute_dialog_through_command(".uno:SetDocumentProperties") as xDialog:
+                xRecordChangesCheckbox = xDialog.getChild("recordchanges")
+                self.assertEqual(get_state_as_dict(xRecordChangesCheckbox)["Selected"], "true")
+                xTabs = xDialog.getChild("tabcontrol")
+                select_pos(xTabs, "3")     #tab Security
+                xProtectBtn = xDialog.getChild("protect")
+
+                with self.ui_test.execute_blocking_action(xProtectBtn.executeAction, args=('CLICK', ())) as xPasswordDialog:
+                    self.assertEqual(get_state_as_dict(xPasswordDialog)["DisplayText"], "Enter Password")
+                    xPassword = xPasswordDialog.getChild("pass1ed")
+                    # give the correct password
+                    xPassword.executeAction("TYPE", mkPropertyValues({"TEXT": "test"}))
+
+            # now open the dialog again and read the properties, Record Changes checkbox is disabled now
+            with self.ui_test.execute_dialog_through_command(".uno:SetDocumentProperties", close_button="cancel") as xDialog:
+                xRecordChangesCheckbox = xDialog.getChild("recordchanges")
+                self.assertEqual(get_state_as_dict(xRecordChangesCheckbox)["Selected"], "false")
+                xResetBtn = xDialog.getChild("reset")
+                xResetBtn.executeAction("CLICK", tuple())
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
