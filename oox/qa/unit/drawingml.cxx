LibreOffice/core

commit 30735bdb5a0a81619000fdd24b2d0fbf45687f01

sd theme: add PPTX import for shape fill color effects
This is always direct formatting, so FillProperties::pushToPropMap()
always has the needed info at hand.

Change-Id: I3317b618e0e8bb7688d0f0fbfe4546e2e8b4e947
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/133525
Tested-by: Jenkins
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

include/oox/drawingml/color.hxx
@@ -100,8 +100,8 @@ public:
     const OUString&     getSchemeColorName() const { return msSchemeName; }
     sal_Int16           getSchemeColorIndex() const;
     sal_Int16           getTintOrShade();
-    sal_Int16           getLumMod();
-    sal_Int16           getLumOff();
+    sal_Int16           getLumMod() const;
+    sal_Int16           getLumOff() const;
 
     /** Returns the unaltered list of transformations for interoperability purposes */
     const css::uno::Sequence< css::beans::PropertyValue >& getTransformations() const { return maInteropTransformations;}

oox/source/drawingml/color.cxx
@@ -496,7 +496,7 @@ sal_Int16 Color::getTintOrShade()
     return 0;
 }
 
-sal_Int16 Color::getLumMod()
+sal_Int16 Color::getLumMod() const
 {
     for (const auto& rTransform : maTransforms)
     {
@@ -512,7 +512,7 @@ sal_Int16 Color::getLumMod()
     return 10000;
 }
 
-sal_Int16 Color::getLumOff()
+sal_Int16 Color::getLumOff() const
 {
     for (const auto& rTransform : maTransforms)
     {

oox/source/drawingml/fillproperties.cxx
@@ -397,6 +397,12 @@ void FillProperties::pushToPropMap( ShapePropertyMap& rPropMap,
                 {
                     rPropMap.setProperty(PROP_FillColorTheme, nPhClrTheme);
                 }
+                else
+                {
+                    rPropMap.setProperty(PROP_FillColorTheme, maFillColor.getSchemeColorIndex());
+                    rPropMap.setProperty(PROP_FillColorLumMod, maFillColor.getLumMod());
+                    rPropMap.setProperty(PROP_FillColorLumOff, maFillColor.getLumOff());
+                }
 
                 eFillStyle = FillStyle_SOLID;
             }

svx/qa/unit/styles.cxx
@@ -94,17 +94,26 @@ CPPUNIT_TEST_FIXTURE(Test, testThemeChange)
     // Blue.
     CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(0x4472c4), GetShapeFillColor(xShape4));
     // The theme index of this filled shape is set by the PPTX import:
-    sal_Int32 nColorTheme = -1;
+    sal_Int16 nColorTheme = -1;
     xShape4->getPropertyValue("FillColorTheme") >>= nColorTheme;
     // 4 means accent1, this was -1 without the PPTX import bit in place.
-    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(4), nColorTheme);
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int16>(4), nColorTheme);
     uno::Reference<beans::XPropertySet> xShape5(xDrawPageShapes->getByIndex(5), uno::UNO_QUERY);
     // Blue, lighter.
     CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(0xb4c7e7), GetShapeFillColor(xShape5));
-    // Set theme index to accent 1 & effects till PPTX import is missing.
-    xShape5->setPropertyValue("FillColorTheme", uno::makeAny(static_cast<sal_Int16>(4)));
-    xShape5->setPropertyValue("FillColorLumMod", uno::makeAny(static_cast<sal_Int16>(4000)));
-    xShape5->setPropertyValue("FillColorLumOff", uno::makeAny(static_cast<sal_Int16>(6000)));
+    // The theme index, and effects (lum mod, lum off) are set by the PPTX import:
+    nColorTheme = -1;
+    xShape5->getPropertyValue("FillColorTheme") >>= nColorTheme;
+    // 4 means accent1, this was -1 without the PPTX import bit in place.
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int16>(4), nColorTheme);
+    sal_Int16 nColorLumMod = 10000;
+    xShape5->getPropertyValue("FillColorLumMod") >>= nColorLumMod;
+    // This was 10000 without the PPTX import bit in place.
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int16>(4000), nColorLumMod);
+    sal_Int16 nColorLumOff = 0;
+    xShape5->getPropertyValue("FillColorLumOff") >>= nColorLumOff;
+    // This was 0 without the PPTX import bit in place.
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int16>(6000), nColorLumOff);
 
     // When changing the master slide of slide 1 to use the theme of the second master slide:
     uno::Reference<drawing::XMasterPageTarget> xDrawPage2(

commit 667e69fb8cc1a61e7cabe4aa6ec552a4580c18df

tdf#148929 sd theme: limit PPTX import for shape fill effects to lum mod/off
Regression from 30735bdb5a0a81619000fdd24b2d0fbf45687f01 (sd theme: add PPTX
import for shape fill color effects, 2022-04-27), the bugdoc's A2 cell
lost its tinting (its background color is no longer lighter than A1)
after saving back to PPTX + import again.

The code assumed that in case a fill color has effects, it can only be
luminance offset or modulation, since that's what the PowerPoint UI
generates when setting a fill color explicitly. This did not take the
table style case into account, which uses tinting to make a color
lighter.

Fix the problem by not importing the theme index / effects if tinting is
used -- the current doc model is limited to theme index + lum mod/off
with effects.

This limitation can be removed while text color / fill color effects are
not limited to lum mod/off, but also support tinting/shading.

Change-Id: I382cc0067518cc262e261a462999170cb7db261b

include/oox/drawingml/color.hxx
@@ -99,7 +99,7 @@ public:
     /** Returns the scheme name from the a:schemeClr element for interoperability purposes */
     const OUString&     getSchemeColorName() const { return msSchemeName; }
     sal_Int16           getSchemeColorIndex() const;
-    sal_Int16           getTintOrShade();
+    sal_Int16           getTintOrShade() const;
     sal_Int16           getLumMod() const;
     sal_Int16           getLumOff() const;
 

oox/source/drawingml/color.cxx
@@ -479,7 +479,7 @@ void Color::clearTransparence()
     mnAlpha = MAX_PERCENT;
 }
 
-sal_Int16 Color::getTintOrShade()
+sal_Int16 Color::getTintOrShade() const
 {
     for(auto const& aTransform : maTransforms)
     {

oox/source/drawingml/fillproperties.cxx
@@ -397,7 +397,7 @@ void FillProperties::pushToPropMap( ShapePropertyMap& rPropMap,
                 {
                     rPropMap.setProperty(PROP_FillColorTheme, nPhClrTheme);
                 }
-                else
+                else if (maFillColor.getTintOrShade() == 0)
                 {
                     rPropMap.setProperty(PROP_FillColorTheme, maFillColor.getSchemeColorIndex());
                     rPropMap.setProperty(PROP_FillColorLumMod, maFillColor.getLumMod());

oox/qa/unit/drawingml.cxx
@@ -29,6 +29,7 @@
 #include <com/sun/star/drawing/TextHorizontalAdjust.hpp>
 #include <com/sun/star/drawing/XMasterPageTarget.hpp>
 #include <com/sun/star/text/XTextRange.hpp>
+#include <com/sun/star/table/XCellRange.hpp>
 
 #include <unotools/mediadescriptor.hxx>
 #include <unotools/tempfile.hxx>
@@ -507,6 +508,37 @@ CPPUNIT_TEST_FIXTURE(OoxDrawingmlTest, testTdf132557_footerCustomShapes)
     uno::Reference<drawing::XShape> xShapeSlideNum(xDrawPage->getByIndex(2), uno::UNO_QUERY);
     CPPUNIT_ASSERT_EQUAL(OUString("com.sun.star.drawing.CustomShape"),
                          xShapeSlideNum->getShapeType());
+}
+
+CPPUNIT_TEST_FIXTURE(OoxDrawingmlTest, testThemeTint)
+{
+    // Given a document with a table style, using theme color with tinting in the A2 cell:
+    OUString aURL = m_directories.getURLFromSrc(DATA_DIRECTORY) + "theme-tint.pptx";
+
+    // When loading that document:
+    load(aURL);
+
+    // Then make sure that we only import theming info to the doc model if the effects are limited
+    // to lum mod / off that we can handle (i.e. no tint/shade):
+    uno::Reference<drawing::XDrawPagesSupplier> xDrawPagesSupplier(getComponent(), uno::UNO_QUERY);
+    uno::Reference<drawing::XDrawPage> xDrawPage(xDrawPagesSupplier->getDrawPages()->getByIndex(0),
+                                                 uno::UNO_QUERY);
+    uno::Reference<beans::XPropertySet> xShape(xDrawPage->getByIndex(0), uno::UNO_QUERY);
+    uno::Reference<table::XCellRange> xTable;
+    CPPUNIT_ASSERT(xShape->getPropertyValue("Model") >>= xTable);
+    uno::Reference<beans::XPropertySet> xA1(xTable->getCellByPosition(0, 0), uno::UNO_QUERY);
+    sal_Int16 nFillColorTheme{};
+    CPPUNIT_ASSERT(xA1->getPropertyValue("FillColorTheme") >>= nFillColorTheme);
+    // This is OK, no problematic effects:
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int16>(4), nFillColorTheme);
+    uno::Reference<beans::XPropertySet> xA2(xTable->getCellByPosition(0, 1), uno::UNO_QUERY);
+    CPPUNIT_ASSERT(xA2->getPropertyValue("FillColorTheme") >>= nFillColorTheme);
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: -1
+    // - Actual  : 4
+    // i.e. we remembered the theme index, without being able to remember the tint effect, leading
+    // to a bad background color.
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int16>(-1), nFillColorTheme);
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();