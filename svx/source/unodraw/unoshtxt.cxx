LibreOffice/core

commit cf2449aac0141711a7610d67f7c50cd108e12443

tdf#72776 Allow text removal in the shapes
Previous fix had crafted condition for fixing one specific
case but it was braking other cases.
(svx/source/unodraw/unoshtxt.cxx)

For example it was not possible to remove text inside textbox
and save that -> it was not saved in LOK case.

Problem is that we had not saved text inside shape and due
to calling GetBackgroundTextForwarder() we did reset to
empty content. In this patch we force UpdateData() call
by releasing lock just before we try to do some final
text cursor operations what applies changes in the content
and prevents us from losing it.

Call which was invoking GetBackgroundTextForwarder() was:
file: xmloff/source/draw/ximpshap.cxx
method: SdXMLShapeContext::endFastElement
line: mxCursor->gotoEnd( false );

After that I found that we had bug which breaks test:
sd/qa/unit/import-tests.cxx    odp/shapes-test.odp
It occurs when using this patch or when not using my code
but removing locking functionality for SvxTextEditSourceImpl.
NbcSetOutlinerParaObjectForText triggers shape resizing
even when no text is present inside.

Summary:
1. revert hack from https://git.libreoffice.org/core/+/71b66b0039819f38c935b4eb5d5951ceaf6e8468%5E%21
2. trigger text update by releasing lock for a moment to keep
   correct state
3. fix bug with resizing empty shape

Change-Id: Idf1fc6caf19dfb932ce7caa21034f76fa04ce5f9
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/130474
Tested-by: Jenkins
Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>

svx/source/unodraw/unoshtxt.cxx
@@ -547,10 +547,7 @@ SvxTextForwarder* SvxTextEditSourceImpl::GetBackgroundTextForwarder()
 
         if( pOutlinerParaObject && ( bOwnParaObj || !mpObject->IsEmptyPresObj() || mpObject->getSdrPageFromSdrObject()->IsMasterPage() ) )
         {
-            // tdf#72776: do not set empty text to SdrOutliner, if it is already set
-            if( !mpOutliner->GetEditEngine().GetTextLen() || pOutlinerParaObject->Count() > 1 || ( pOutlinerParaObject->Count() == 1 &&
-                !pOutlinerParaObject->GetTextObject().GetText(0).isEmpty() ) )
-                mpOutliner->SetText( *pOutlinerParaObject );
+            mpOutliner->SetText( *pOutlinerParaObject );
 
             // put text to object and set EmptyPresObj to FALSE
             if( mpText && bOwnParaObj && mpObject->IsEmptyPresObj() && pTextObj->IsReallyEdited() )
@@ -778,7 +775,8 @@ void SvxTextEditSourceImpl::UpdateData()
             SdrTextObj* pTextObj = dynamic_cast< SdrTextObj* >( mpObject );
             if( pTextObj )
             {
-                if( mpOutliner->GetParagraphCount() != 1 || mpOutliner->GetEditEngine().GetTextLen( 0 ) )
+                if( (mpOutliner->GetParagraphCount() != 1 && mpOutliner->GetParagraphCount() != 2)
+                    || mpOutliner->GetEditEngine().GetTextLen( 0 ) )
                 {
                     pTextObj->NbcSetOutlinerParaObjectForText( mpOutliner->CreateParaObject(), mpText );
                 }

xmloff/source/draw/ximpshap.cxx
@@ -328,6 +328,13 @@ void SdXMLShapeContext::endFastElement(sal_Int32 )
 {
     if(mxCursor.is())
     {
+        // tdf#72776 force UpdateData in the EditSource so we will not override text in SdrOutliner
+        if( mxLockable.is() )
+        {
+            mxLockable->removeActionLock();
+            mxLockable->addActionLock();
+        }
+
         // delete addition newline
         mxCursor->gotoEnd( false );
         mxCursor->goLeft( 1, true );

commit 82789c9cea52c10ded4ef76355d17a62c24135ca

tdf#148818 Fix missing text in boxes with empty first paragraph
Text was lost from the text boxes with empty first paragraph. The
problem was happening when there was 2 paragraphs in a text box, in
which the first paragraph was completely empty. The regression was
introduced in cf2449aac0141711a7610d67f7c50cd108e12443 because of the
bad handling of the paragraphs, and only looking at the size of the
text of the first paragraph.

This is fixed by looking at mpOutliner->GetEditEngine().GetTextLen(i)
for all i=1..k where k=min(2,mpOutliner->GetParagraphCount()).

I have negated the condition to provide a better explanation of what
the condition should be.

Change-Id: Id5d8787df5111c734760afdd98a6fbe832047f32

svx/source/unodraw/unoshtxt.cxx
@@ -775,14 +775,15 @@ void SvxTextEditSourceImpl::UpdateData()
             SdrTextObj* pTextObj = dynamic_cast< SdrTextObj* >( mpObject );
             if( pTextObj )
             {
-                if( (mpOutliner->GetParagraphCount() != 1 && mpOutliner->GetParagraphCount() != 2)
-                    || mpOutliner->GetEditEngine().GetTextLen( 0 ) )
+                if( (mpOutliner->GetParagraphCount() == 1 && mpOutliner->GetEditEngine().GetTextLen( 0 ) == 0 )
+                        || (mpOutliner->GetParagraphCount() == 2 && mpOutliner->GetEditEngine().GetTextLen( 0 ) == 0
+                            && mpOutliner->GetEditEngine().GetTextLen( 1 ) == 0) )
                 {
-                    pTextObj->NbcSetOutlinerParaObjectForText( mpOutliner->CreateParaObject(), mpText );
+                    pTextObj->NbcSetOutlinerParaObjectForText( std::nullopt, mpText );
                 }
                 else
                 {
-                    pTextObj->NbcSetOutlinerParaObjectForText( std::nullopt, mpText );
+                    pTextObj->NbcSetOutlinerParaObjectForText( mpOutliner->CreateParaObject(), mpText );
                 }
             }
 

sd/qa/unit/import-tests-smartart.cxx
@@ -347,9 +347,8 @@ void SdImportTestSmartArt::testTdf148665()
                                                  uno::UNO_QUERY_THROW);
     CPPUNIT_ASSERT_EQUAL(sal_Int32(4), xShapeGroup->getCount());
 
-    // FIXME: tdf#148818: Text should be 'Fufufu'
     uno::Reference<text::XText> xText0(xShapeGroup->getByIndex(1), uno::UNO_QUERY_THROW);
-    CPPUNIT_ASSERT_EQUAL(OUString(""), xText0->getString());
+    CPPUNIT_ASSERT_EQUAL(OUString("\nFufufu"), xText0->getString());
     uno::Reference<text::XText> xText1(xShapeGroup->getByIndex(2), uno::UNO_QUERY_THROW);
     CPPUNIT_ASSERT_EQUAL(OUString("Susu"), xText1->getString());
     uno::Reference<text::XText> xText2(xShapeGroup->getByIndex(3), uno::UNO_QUERY_THROW);