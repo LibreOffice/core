53c71a0fa29b98d997ced6c9684c627442cfff94
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2022-05-06 16:04:47 +0900

    chart2: add initial code for rendering the Data Table
    Create a (svx) table with the data from the chart and place it below
    the x-axis line.
    
    Data table rendering is disabled in the code until the data table
    properties are available.
    
    Change-Id: I07d282c0b5e8df6b843516c8bdad538862d6575e
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/136788
    Tested-by: Tomaž Vajngerl <quikee@gmail.com>
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

--- chart2/source/view/axes/VAxisProperties.cxx
+++ chart2/source/view/axes/VAxisProperties.cxx
@@ -164,6 +164,7 @@ AxisProperties::AxisProperties( rtl::Reference< Axis > xAxisModel
     , m_eTickmarkPos( css::chart::ChartAxisMarkPosition_AT_LABELS_AND_AXIS )
     , m_bCrossingAxisHasReverseDirection(false)
     , m_bCrossingAxisIsCategoryAxes(false)
+    , m_bDisplayDataTable(false)
     , m_bDisplayLabels( true )
     , m_bTryStaggeringFirst( false )
     , m_nNumberFormatKey(0)
@@ -254,6 +255,9 @@ void AxisProperties::init( bool bCartesian )
 
     if( bCartesian )
     {
+        if (m_nDimensionIndex == 0)
+            m_bDisplayDataTable = false;
+
         if( m_nDimensionIndex == 0 && m_nAxisType == AxisType::CATEGORY
                 && m_pExplicitCategoriesProvider && m_pExplicitCategoriesProvider->hasComplexCategories() )
             m_bComplexCategories = true;
