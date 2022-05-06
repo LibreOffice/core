701591fac7eea1ac797bbc328cc934434ffb714e
Xisco Fauli <xiscofauli@libreoffice.org>
2022-05-06 14:04:12 +0200

    tdf#148920, tdf#91035: sw: Add UItest
    Change-Id: Icdeb93a8c45bfccd4f749ec24d8226e9bdd8cdd7
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/133933
    Tested-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

---
+++ sw/qa/uitest/writer_tests5/tdf148920.py
@@ -0,0 +1,33 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+from uitest.framework import UITestCase
+from uitest.uihelper.common import get_state_as_dict
+
+class tdf148920(UITestCase):
+
+   def test_tdf148920(self):
+
+        with self.ui_test.create_doc_in_start_center("writer") as document:
+
+            self.xUITest.executeCommand(".uno:StyleApply?Style:string=Text%20body&FamilyName:string=ParagraphStyles")
+
+            with self.ui_test.execute_dialog_through_command(".uno:EditStyle", close_button="cancel") as xDialog:
+                xFilter = xDialog.getChild("filter")
+
+                # Without the fix in place, this test would have failed with
+                # AssertionError: 'true' != 'false'
+                self.assertEqual("true", get_state_as_dict(xFilter)["Enabled"])
+
+                # tdf#91035: Without the fix in place, this test would have failed with
+                # AssertionError: 'All Styles' != 'Hidden Styles'
+                self.assertEqual("All Styles", get_state_as_dict(xFilter)["SelectEntryText"])
+
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
