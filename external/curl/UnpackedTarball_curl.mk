f5db5c8a3b181e73abc6c94d0924d624c43fbba1
Michael Stahl <michael.stahl@allotropia.de>
2022-05-12 11:43:59 +0200

    curl: upgrade to release 7.83.1
    Fixes CVE-2022-27774 CVE-2022-27775 CVE-2022-27776 CVE-2022-27781
    plus 6 more CVEs that shouldn't affect LO.
    
    Remove obsolete configure-eval-fix.patch.0.
    
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/134225
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
    (cherry picked from commit 58a3bf5199818e30ef4207213f29692d81b519c6)
    
    upgrade to curl-7.81.0
    
    Change-Id: I0a34239bfb16bf19e25bf374c7f36c4cdf1776c1
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/128783
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    (cherry picked from commit 40a84af1bdd7b3c414a8a78ca32b0951c03f9976)
    
    Change-Id: Ifbd7ff5acf390df1d95d6b8be0dc7751e4753bbe

--- external/curl/UnpackedTarball_curl.mk
+++ external/curl/UnpackedTarball_curl.mk
@@ -25,7 +25,6 @@ $(eval $(call gb_UnpackedTarball_add_patches,curl,\
 	external/curl/curl-msvc-disable-protocols.patch.1 \
 	external/curl/curl-7.26.0_win-proxy.patch \
 	external/curl/zlib.patch.0 \
-	external/curl/curl-debug.patch.1 \
 	external/curl/configurable-z-option.patch.0 \
 ))
 
