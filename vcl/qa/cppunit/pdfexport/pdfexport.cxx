deb1b677048b77f31a3f29bd033cfb9e61f47fcc
Xisco Fauli <xiscofauli@libreoffice.org>
2022-05-12 12:10:53 +0200

    tdf#143612: map SecondaryRefValue in form pdf export
    Change-Id: I590784eb94fb6f4f3a20c4f6d8e3fb618f60d0fc
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/134227
    Tested-by: Jenkins
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Signed-off-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/134242
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

--- vcl/qa/cppunit/pdfexport/pdfexport.cxx
+++ vcl/qa/cppunit/pdfexport/pdfexport.cxx
@@ -858,6 +858,7 @@ CPPUNIT_TEST_FIXTURE(PdfExportTest, testTdf148442)
                 CPPUNIT_ASSERT_EQUAL(OString("Yes"), pAS->GetValue());
                 CPPUNIT_ASSERT(!pN->GetItems().count("ref"));
                 CPPUNIT_ASSERT(pN->GetItems().count("Yes"));
+                CPPUNIT_ASSERT(pN->GetItems().count("Off"));
             }
             else if (nBtnCount == 2)
             {
@@ -867,6 +868,7 @@ CPPUNIT_TEST_FIXTURE(PdfExportTest, testTdf148442)
                 // Without the fix in place, this test would have failed here
                 CPPUNIT_ASSERT(pN->GetItems().count("ref"));
                 CPPUNIT_ASSERT(!pN->GetItems().count("Yes"));
+                CPPUNIT_ASSERT(pN->GetItems().count("Off"));
             }
             else
             {
@@ -874,6 +876,10 @@ CPPUNIT_TEST_FIXTURE(PdfExportTest, testTdf148442)
                 CPPUNIT_ASSERT_EQUAL(OString("Off"), pAS->GetValue());
                 CPPUNIT_ASSERT(pN->GetItems().count("ref"));
                 CPPUNIT_ASSERT(!pN->GetItems().count("Yes"));
+
+                // tdf#143612: Without the fix in place, this test would have failed here
+                CPPUNIT_ASSERT(!pN->GetItems().count("Off"));
+                CPPUNIT_ASSERT(pN->GetItems().count("refOff"));
             }
         }
     }
