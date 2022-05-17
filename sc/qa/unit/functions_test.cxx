2de31ca455f3857c28b5b386f318ef57aa8d733d
Luboš Luňák <l.lunak@collabora.com>
2022-05-17 10:18:16 +0200

    provide more details about failures in Calc 'functions' tests
    Instead of simply asserting that something failed, say which part
    of the .fods test document failed.
    
    Change-Id: Idd901186f27d2395b9e3561fbac6bfc1340b7e72

--- sc/qa/unit/functions_test.cxx
+++ sc/qa/unit/functions_test.cxx
@@ -11,6 +11,8 @@
 #include "functions_test.hxx"
 #include <document.hxx>
 
+#include <rtl/math.hxx>
+
 FunctionsTest::FunctionsTest(const OUString& rPath):
     ScBootstrapFixture(rPath)
 {
@@ -40,6 +42,28 @@ bool FunctionsTest::load(const OUString& rFilter, const OUString& rURL,
 
     ScDocument& rDoc = xDocShRef->GetDocument();
 
+    if(!rtl::math::approxEqual(1.0, rDoc.GetValue(1, 2, 0)))
+    {
+        // Cell B3 in Sheet1 has the cumulative success/failure result.
+        // Try to find the actual failure.
+        for(SCTAB tab = 1; tab <= rDoc.GetMaxTableNumber(); ++tab)
+        {
+            SCROW maxRow = rDoc.GetLastDataRow(tab, 2, 2, rDoc.MaxRow());
+            for(SCROW row = 0; row <= maxRow; ++row)
+            {
+                // Column C has the check result, column D has the formula text.
+                if(rDoc.HasStringData(2, row, tab))
+                    continue;
+                if(!rtl::math::approxEqual(1.0, rDoc.GetValue(2, row, 1)))
+                    CPPUNIT_FAIL( OUString( "Testing " + rURL + " failed, "
+                        + rDoc.GetAllTableNames()[tab] + ".A" + OUString::number(row+1)
+                        + " \'" + rDoc.GetString(3, row, 1) + "\'"
+                          " result: " + OUString::number(rDoc.GetValue(0, row, 1))
+                        + ", expected: " + OUString::number(rDoc.GetValue(1, row, 1)))
+                        .toUtf8().getStr());
+            }
+        }
+    }
     CPPUNIT_ASSERT_DOUBLES_EQUAL(1.0, rDoc.GetValue(1, 2, 0), 1e-14);
 
     xDocShRef->DoClose();
