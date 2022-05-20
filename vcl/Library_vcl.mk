a3546d291be99fba50cece6f1b625eb23e11cdb6
Caolán McNamara <caolanm@redhat.com>
2022-05-20 10:00:14 +0100

    drop old tiff filter
    Change-Id: I3bfeae4751cb9173fbe2efdbda531a9a1a491c8e

--- vcl/Library_vcl.mk
+++ vcl/Library_vcl.mk
@@ -455,9 +455,7 @@ $(eval $(call gb_Library_add_exception_objects,vcl,\
     vcl/source/filter/ipdf/pdfdocument \
     vcl/source/filter/iras/iras \
     vcl/source/filter/itga/itga \
-    vcl/source/filter/itiff/ccidecom \
     vcl/source/filter/itiff/itiff \
-    vcl/source/filter/itiff/lzwdecom \
     vcl/source/filter/ixbm/xbmread \
     vcl/source/filter/ixpm/xpmread \
     vcl/source/filter/jpeg/Exif \
