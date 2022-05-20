6c24f112ad86df77f2de27a31742bca01ed56bdf
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2022-05-20 23:55:45 +0900

    chart2: apply line props. to borders when rendering a data table
    This sets the color, line width, line style to the borders of
    the table when rendering the data table. We also need the
    ChartModel inside DataTableView, so we can access the needed
    line dashes.
    
    Change-Id: Id3bef23b75e88517d6569ad87a716f178ed64343
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/137158
    Tested-by: Jenkins
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

--- chart2/source/view/inc/DataTableView.hxx
+++ chart2/source/view/inc/DataTableView.hxx
@@ -13,23 +13,32 @@
 #include <svx/unodraw/SvxTableShape.hxx>
 #include <com/sun/star/awt/Rectangle.hpp>
 #include <DataTable.hxx>
+#include "VLineProperties.hxx"
 
 namespace chart
 {
 class VSeriesPlotter;
+class ChartModel;
 
 class DataTableView final
 {
+private:
+    rtl::Reference<::chart::ChartModel> m_xChartModel;
     rtl::Reference<SvxShapeGroupAnyD> m_xTarget;
     rtl::Reference<SvxTableShape> m_xTableShape;
     rtl::Reference<DataTable> m_xDataTableModel;
+    VLineProperties m_aLineProperties;
 
     std::vector<OUString> m_aDataSeriesNames;
     std::vector<OUString> m_aXValues;
     std::vector<std::vector<OUString>> m_pDataSeriesValues;
 
+    void setCellDefaults(css::uno::Reference<css::beans::XPropertySet>& xPropertySet, bool bLeft,
+                         bool bTop, bool bRight, bool bBottom);
+
 public:
-    DataTableView(rtl::Reference<DataTable> const& rDataTableModel);
+    DataTableView(rtl::Reference<::chart::ChartModel> const& xChartDoc,
+                  rtl::Reference<DataTable> const& rDataTableModel);
     void initializeShapes(const rtl::Reference<SvxShapeGroupAnyD>& xTarget);
     void initializeValues(std::vector<std::unique_ptr<VSeriesPlotter>>& rSeriesPlotterList);
     void createShapes(basegfx::B2DVector const& rStart, basegfx::B2DVector const& rEnd,
