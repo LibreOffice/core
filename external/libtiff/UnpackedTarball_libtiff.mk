519a5adf7d2d7dd5f895e59896fbbcc52210dfa7
Caolán McNamara <caolanm@redhat.com>
2022-05-20 09:51:19 +0100

    libtiff: enable jpeg and zlib
    Change-Id: Ied6ab75342f5cdaadefbff7f75fbe63e9d67e992
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/134667
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>

--- external/libtiff/UnpackedTarball_libtiff.mk
+++ external/libtiff/UnpackedTarball_libtiff.mk
@@ -13,4 +13,8 @@ $(eval $(call gb_UnpackedTarball_set_tarball,libtiff,$(LIBTIFF_TARBALL)))
 
 $(eval $(call gb_UnpackedTarball_set_patchlevel,libtiff,0))
 
+$(eval $(call gb_UnpackedTarball_add_patches,libtiff,\
+    external/libtiff/libtiff.linknolibs.patch \
+))
+
 # vim: set noet sw=4 ts=4:
