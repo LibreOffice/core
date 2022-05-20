e930396337cbbf8a8b9c2da63e5c47cfacac0341
Andrea Gelmini <andrea.gelmini@gelma.net>
2022-05-20 11:13:23 +0200

    WIP: dunno about "Hodler"
    Change-Id: I9fbee006237ccb70e31109023cf46b21aec325d6

--- sc/inc/SparklineCell.hxx
+++ sc/inc/SparklineCell.hxx
@@ -16,7 +16,7 @@
 
 namespace sc
 {
-/** Hodler of a sparkline, that is connected to a cell specific */
+/** -Hodler- of a sparkline, that is connected to a cell specific */
 class SC_DLLPUBLIC SparklineCell
 {
 private:
