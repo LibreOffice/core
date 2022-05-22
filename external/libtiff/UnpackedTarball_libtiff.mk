6b6384b816e0df061fe9ec9135c57e362d7d04c1
Caolán McNamara <caolanm@redhat.com>
2022-05-22 14:12:13 +0100

    tdf#131199 add some basic 16bitcielab support
    to get that final tiff loadable
    
    Change-Id: Ia772c06521c93ac860e9d3014706d677f16c8d4e

--- external/libtiff/UnpackedTarball_libtiff.mk
+++ external/libtiff/UnpackedTarball_libtiff.mk
@@ -15,6 +15,7 @@ $(eval $(call gb_UnpackedTarball_set_patchlevel,libtiff,0))
 
 $(eval $(call gb_UnpackedTarball_add_patches,libtiff,\
     external/libtiff/libtiff.linknolibs.patch \
+    external/libtiff/libtiff.16bitcielab.patch \
 ))
 
 # vim: set noet sw=4 ts=4:
