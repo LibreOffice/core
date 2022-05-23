7c3c23c97250ba1cfa60ee19675cb7c32c746b84
Stephan Bergmann <sbergman@redhat.com>
2022-05-23 10:21:54 +0200

    Missing #include <stdint.h> in various external code
    ...which is a problem presumably since GCC 13 trunk
    <https://gcc.gnu.org/git/?p=gcc.git;a=commit;h=6f038efd93593da6e661b829d1bd3877e75550f1>
    "libstdc++: Avoid including <cstdint> for std::char_traits".  (All the broken
    C++ code used unqualified uintptr_t etc. rather than std::uintptr_t etc., so I
    deemed it more appropriate to include <stdint.h> rather than <cstdint>.)
    
    Change-Id: Id9dfc383c5986126a425971c4557b90ac45ac963

--- external/pdfium/UnpackedTarball_pdfium.mk
+++ external/pdfium/UnpackedTarball_pdfium.mk
@@ -24,6 +24,8 @@ pdfium_patches += AndroidNDK19.patch.1
 # --with-latest-c++:
 pdfium_patches += gcc-c++20-comparison.patch
 
+pdfium_patches += include.patch
+
 $(eval $(call gb_UnpackedTarball_UnpackedTarball,pdfium))
 
 $(eval $(call gb_UnpackedTarball_set_tarball,pdfium,$(PDFIUM_TARBALL)))
