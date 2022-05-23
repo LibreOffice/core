3f1682a95db61bccd9fab0b6bb786e15b2951f88
Noel Grandin <noel.grandin@collabora.co.uk>
2022-05-23 18:47:15 +0200

    Revert "[API CHANGE] call ftruncate instead of ftruncate_by_name"
    This reverts commit 1d58828f2285064635ed776e0641e83ee26d34da.
    
    because we need the filename for "sandboxed LibreOffice on OS X" functionality
    
    Change-Id: I598759de0f4bd9e6387df66038fc060e8a08027e

--- sal/osl/unx/file_impl.hxx
+++ sal/osl/unx/file_impl.hxx
@@ -49,7 +49,7 @@ oslFileError openFile(
     mode_t mode);
 
 oslFileError openFilePath(
-    const char* cpFilePath,
+    const OString& filePath,
     oslFileHandle* pHandle,
     sal_uInt32 uFlags, mode_t mode );
 
