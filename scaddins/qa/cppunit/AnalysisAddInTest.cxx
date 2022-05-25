cc1bd2076c7ee511742b8cf1bc1ed15c720b9573
offtkp <parisoplop@gmail.com>
2022-05-25 03:38:40 +0300

    (WIP) tdf#148645 Accept integer 'places' value
    Change-Id: I69cfc14c428afb398f1e1c3381468bd01b702076

---
+++ scaddins/qa/cppunit/AnalysisAddInTest.cxx
@@ -0,0 +1,43 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <sal/config.h>
+#include <analysis/analysis.hxx>
+#include <comphelper/processfactory.hxx>
+#include <cppunit/TestAssert.h>
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/plugin/TestPlugIn.h>
+
+namespace
+{
+class AnalysisAddInTest : public CppUnit::TestFixture
+{
+private:
+    void testGetDec2Hex();
+
+    CPPUNIT_TEST_SUITE(AnalysisAddInTest);
+    CPPUNIT_TEST(testGetDec2Hex);
+    CPPUNIT_TEST_SUITE_END();
+};
+
+void AnalysisAddInTest::testGetDec2Hex()
+{
+    auto context = comphelper::getProcessComponentContext();
+    AnalysisAddIn addin(context);
+    // CPPUNIT_ASSERT_EQUAL("0x000004D2", someAnalysisAddIn.getDec2Hex(???, 1234, val));
+}
+
+} // namespace
+
+CPPUNIT_TEST_SUITE_REGISTRATION(AnalysisAddInTest);
+
+CPPUNIT_PLUGIN_IMPLEMENT();
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
