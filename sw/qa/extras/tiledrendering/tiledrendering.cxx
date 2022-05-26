LibreOffice/core

commit 9a76be53dfb801b754bf55f9d4b8c5f82991a62f

sw content controls, picture: add LOK API
- send a LOK_CALLBACK_CONTENT_CONTROL callback with
  action=change-picture when a file picker should be shown

- extend lok::Document::sendContentControlEvent() to be able to replace
  the placeholder with the selected URL

- update gtktiledviewer to work with these

Change-Id: Ifb3750803885fc09fc82905b0cf85b2b8ca06e77
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/134750
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

desktop/source/lib/init.cxx
@@ -6093,9 +6093,9 @@ static void doc_sendContentControlEvent(LibreOfficeKitDocument* pThis, const cha
     }
 
     // Sanity check
-    if (aMap.find("type") == aMap.end() || aMap.find("selected") == aMap.end())
+    if (aMap.find("type") == aMap.end())
     {
-        SetLastExceptionMsg("Wrong arguments for sendContentControlEvent");
+        SetLastExceptionMsg("Missing 'type' argument for sendContentControlEvent");
         return;
     }
 

include/LibreOfficeKit/LibreOfficeKit.hxx
@@ -812,14 +812,18 @@ public:
      *
      * @param pArguments arguments of the event.
      *
-     * Example argument string:
-     *
+     * Examples:
+     * To select the 3rd list item of the drop-down:
      * {
      *     "type": "drop-down",
      *     "selected": "2"
      * }
      *
-     * selects the 3rd list item of the drop-down.
+     * To change a picture place-holder:
+     * {
+     *     "type": "picture",
+     *     "changed": "file:///path/to/test.png"
+     * }
      */
     void sendContentControlEvent(const char* pArguments)
     {

include/LibreOfficeKit/LibreOfficeKitEnums.h
@@ -799,15 +799,28 @@ typedef enum
     /**
      * Sends all information for displaying metadata for a text based content control.
      *
-     * The payload example:
+     * Examples:
+     * Entered a rich text content control:
      * {
-     *      "action": "show",
-     *      "rectangles": "1418, 1694, 720, 551; 10291, 1418, 1099, 275"
+     *     "action": "show",
+     *     "rectangles": "1418, 1694, 720, 551; 10291, 1418, 1099, 275"
      * }
      *
-     * or
+     * Left a rich text content control:
      * {
-     *      "action": "hide"
+     *     "action": "hide"
+     * }
+     *
+     * Entered a dropdown content control:
+     * {
+     *     "action": "show",
+     *     "rectangles": "...",
+     *     "items": ["red", "green", "blue"]
+     * }
+     *
+     * Clicked on a picture content control's placeholder:
+     * {
+     *     "action": "change-picture"
      * }
      */
     LOK_CALLBACK_CONTENT_CONTROL = 55,

libreofficekit/source/gtk/lokdocview.cxx
@@ -1397,9 +1397,9 @@ callback (gpointer pData)
 
     case LOK_CALLBACK_CONTENT_CONTROL:
     {
-        std::stringstream aStream(pCallback->m_aPayload);
+        std::stringstream aPayloadStream(pCallback->m_aPayload);
         boost::property_tree::ptree aTree;
-        boost::property_tree::read_json(aStream, aTree);
+        boost::property_tree::read_json(aPayloadStream, aTree);
         auto aAction = aTree.get<std::string>("action");
         if (aAction == "show")
         {
@@ -1409,6 +1409,29 @@ callback (gpointer pData)
         else if (aAction == "hide")
         {
             priv->m_aContentControlRectangles.clear();
+        }
+        else if (aAction == "change-picture")
+        {
+            GtkWidget* pDialog = gtk_file_chooser_dialog_new(
+                "Open File", GTK_WINDOW(gtk_widget_get_toplevel(GTK_WIDGET(pDocView))),
+                GTK_FILE_CHOOSER_ACTION_OPEN, "Cancel", GTK_RESPONSE_CANCEL, "Open",
+                GTK_RESPONSE_ACCEPT, nullptr);
+            gint nRet = gtk_dialog_run(GTK_DIALOG(pDialog));
+            if (nRet == GTK_RESPONSE_ACCEPT)
+            {
+                GtkFileChooser* pChooser = GTK_FILE_CHOOSER(pDialog);
+                char* pFilename = gtk_file_chooser_get_uri(pChooser);
+                boost::property_tree::ptree aValues;
+                aValues.put("type", "picture");
+                aValues.put("changed", pFilename);
+                std::stringstream aStream;
+                boost::property_tree::write_json(aStream, aValues);
+                std::string aJson = aStream.str();
+                lok_doc_view_send_content_control_event(pDocView, aJson.c_str());
+
+                g_free(pFilename);
+            }
+            gtk_widget_destroy(pDialog);
         }
         g_signal_emit(pCallback->m_pDocView, doc_view_signals[CONTENT_CONTROL], 0,
                       pCallback->m_aPayload.c_str());

sw/source/uibase/uno/unotxdoc.cxx
@@ -117,6 +117,7 @@
 #include <comphelper/propertyvalue.hxx>
 #include <comphelper/storagehelper.hxx>
 #include <cppuhelper/supportsservice.hxx>
+#include <sfx2/dispatch.hxx>
 #include <swruler.hxx>
 #include <docufld.hxx>
 
@@ -3360,24 +3361,6 @@ SwXTextDocument::getSearchResultRectangles(const char* pPayload)
 
 void SwXTextDocument::executeContentControlEvent(const StringMap& rArguments)
 {
-    SwWrtShell* pWrtShell = m_pDocShell->GetWrtShell();
-    const SwPosition* pStart = pWrtShell->GetCursor()->Start();
-    SwTextNode* pTextNode = pStart->nNode.GetNode().GetTextNode();
-    if (!pTextNode)
-    {
-        return;
-    }
-
-    SwTextAttr* pAttr = pTextNode->GetTextAttrAt(pStart->nContent.GetIndex(),
-                                                 RES_TXTATR_CONTENTCONTROL, SwTextNode::PARENT);
-    if (!pAttr)
-    {
-        return;
-    }
-
-    auto pTextContentControl = static_txtattr_cast<SwTextContentControl*>(pAttr);
-    const SwFormatContentControl& rFormatContentControl = pTextContentControl->GetContentControl();
-    std::shared_ptr<SwContentControl> pContentControl = rFormatContentControl.GetContentControl();
     auto it = rArguments.find("type");
     if (it == rArguments.end())
     {
@@ -3386,6 +3369,24 @@ void SwXTextDocument::executeContentControlEvent(const StringMap& rArguments)
 
     if (it->second == "drop-down")
     {
+        SwWrtShell* pWrtShell = m_pDocShell->GetWrtShell();
+        const SwPosition* pStart = pWrtShell->GetCursor()->Start();
+        SwTextNode* pTextNode = pStart->nNode.GetNode().GetTextNode();
+        if (!pTextNode)
+        {
+            return;
+        }
+
+        SwTextAttr* pAttr = pTextNode->GetTextAttrAt(pStart->nContent.GetIndex(),
+                                                     RES_TXTATR_CONTENTCONTROL, SwTextNode::PARENT);
+        if (!pAttr)
+        {
+            return;
+        }
+
+        auto pTextContentControl = static_txtattr_cast<SwTextContentControl*>(pAttr);
+        const SwFormatContentControl& rFormatContentControl = pTextContentControl->GetContentControl();
+        std::shared_ptr<SwContentControl> pContentControl = rFormatContentControl.GetContentControl();
         if (!pContentControl->HasListItems())
         {
             return;
@@ -3400,6 +3401,25 @@ void SwXTextDocument::executeContentControlEvent(const StringMap& rArguments)
         sal_Int32 nSelection = it->second.toInt32();
         pContentControl->SetSelectedListItem(nSelection);
         pWrtShell->GotoContentControl(rFormatContentControl);
+    }
+    else if (it->second == "picture")
+    {
+        it = rArguments.find("changed");
+        if (it == rArguments.end())
+        {
+            return;
+        }
+
+        SwView* pView = m_pDocShell->GetView();
+        if (!pView)
+        {
+            return;
+        }
+
+        // The current placeholder is selected, so this will replace, not insert.
+        SfxStringItem aItem(SID_INSERT_GRAPHIC, it->second);
+        pView->GetViewFrame()->GetDispatcher()->ExecuteList(SID_INSERT_GRAPHIC,
+                                                            SfxCallMode::SYNCHRON, { &aItem });
     }
 }
 

sw/source/uibase/wrtsh/wrtsh3.cxx
@@ -29,6 +29,9 @@
 #include <com/sun/star/beans/XPropertySet.hpp>
 #include <osl/diagnose.h>
 #include <sfx2/dispatch.hxx>
+#include <comphelper/lok.hxx>
+#include <tools/json_writer.hxx>
+#include <LibreOfficeKit/LibreOfficeKitEnums.h>
 
 #include <swmodule.hxx>
 #include <wrtsh.hxx>
@@ -100,8 +103,19 @@ bool SwWrtShell::GotoContentControl(const SwFormatContentControl& rContentContro
         {
             // Replace the placeholder image with a real one.
             GetView().StopShellTimer();
-            GetView().GetViewFrame()->GetDispatcher()->Execute(SID_CHANGE_PICTURE,
-                                                               SfxCallMode::SYNCHRON);
+            if (comphelper::LibreOfficeKit::isActive())
+            {
+                tools::JsonWriter aJson;
+                aJson.put("action", "change-picture");
+                std::unique_ptr<char, o3tl::free_delete> pJson(aJson.extractData());
+                GetSfxViewShell()->libreOfficeKitViewCallback(LOK_CALLBACK_CONTENT_CONTROL,
+                                                              pJson.get());
+            }
+            else
+            {
+                GetView().GetViewFrame()->GetDispatcher()->Execute(SID_CHANGE_PICTURE,
+                                                                   SfxCallMode::SYNCHRON);
+            }
             pContentControl->SetShowingPlaceHolder(false);
         }
         return true;

commit 9851c80c81233d617a67e3cfbbe4b0b04569f04c

sw content controls, picture: add LOK API testcase
Fails with commit 9a76be53dfb801b754bf55f9d4b8c5f82991a62f (sw content
controls, picture: add LOK API, 2022-05-23) reverted.

Change-Id: I6164e597099632a2212afee86ac9a4c7555e5c61

sw/qa/extras/tiledrendering/tiledrendering.cxx
@@ -66,6 +66,8 @@
 #include <docsh.hxx>
 #include <txtfrm.hxx>
 #include <rootfrm.hxx>
+#include <fmtanchr.hxx>
+#include <textcontentcontrol.hxx>
 
 constexpr OUStringLiteral DATA_DIRECTORY = u"/sw/qa/extras/tiledrendering/data/";
 
@@ -169,6 +171,7 @@ public:
     void testRedlinePortions();
     void testContentControl();
     void testDropDownContentControl();
+    void testPictureContentControl();
 
     CPPUNIT_TEST_SUITE(SwTiledRenderingTest);
     CPPUNIT_TEST(testRegisterCallback);
@@ -258,6 +261,7 @@ public:
     CPPUNIT_TEST(testRedlinePortions);
     CPPUNIT_TEST(testContentControl);
     CPPUNIT_TEST(testDropDownContentControl);
+    CPPUNIT_TEST(testPictureContentControl);
     CPPUNIT_TEST_SUITE_END();
 
 private:
@@ -3712,6 +3716,72 @@ void SwTiledRenderingTest::testDropDownContentControl()
     // - Actual  : choose an item
     // i.e. the document text was not updated.
     CPPUNIT_ASSERT_EQUAL(OUString("green"), pTextNode->GetExpandText(pWrtShell->GetLayout()));
+}
+
+void SwTiledRenderingTest::testPictureContentControl()
+{
+    // Given a document with a picture content control:
+    SwXTextDocument* pXTextDocument = createDoc();
+    SwWrtShell* pWrtShell = pXTextDocument->GetDocShell()->GetWrtShell();
+    setupLibreOfficeKitViewCallback(pWrtShell->GetSfxViewShell());
+    uno::Reference<lang::XMultiServiceFactory> xMSF(mxComponent, uno::UNO_QUERY);
+    uno::Reference<text::XTextDocument> xTextDocument(mxComponent, uno::UNO_QUERY);
+    uno::Reference<text::XText> xText = xTextDocument->getText();
+    uno::Reference<text::XTextCursor> xCursor = xText->createTextCursor();
+    uno::Reference<beans::XPropertySet> xTextGraphic(
+        xMSF->createInstance("com.sun.star.text.TextGraphicObject"), uno::UNO_QUERY);
+    xTextGraphic->setPropertyValue("AnchorType",
+                                   uno::Any(text::TextContentAnchorType_AS_CHARACTER));
+    uno::Reference<text::XTextContent> xTextContent(xTextGraphic, uno::UNO_QUERY);
+    xText->insertTextContent(xCursor, xTextContent, false);
+    xCursor->gotoStart(/*bExpand=*/false);
+    xCursor->gotoEnd(/*bExpand=*/true);
+    uno::Reference<text::XTextContent> xContentControl(
+        xMSF->createInstance("com.sun.star.text.ContentControl"), uno::UNO_QUERY);
+    uno::Reference<beans::XPropertySet> xContentControlProps(xContentControl, uno::UNO_QUERY);
+    xContentControlProps->setPropertyValue("ShowingPlaceHolder", uno::Any(true));
+    xContentControlProps->setPropertyValue("Picture", uno::Any(true));
+    xText->insertTextContent(xCursor, xContentControl, /*bAbsorb=*/true);
+    pWrtShell->SttEndDoc(/*bStt=*/true);
+    m_aContentControl.clear();
+
+    // When clicking on that content control:
+    pWrtShell->GotoObj(/*bNext=*/true, GotoObjFlags::Any);
+    pWrtShell->EnterSelFrameMode();
+    const SwFrameFormat* pFlyFormat = pWrtShell->GetFlyFrameFormat();
+    const SwFormatAnchor& rFormatAnchor = pFlyFormat->GetAnchor();
+    const SwPosition* pAnchorPos = rFormatAnchor.GetContentAnchor();
+    SwTextNode* pTextNode = pAnchorPos->nNode.GetNode().GetTextNode();
+    SwTextAttr* pAttr = pTextNode->GetTextAttrForCharAt(0, RES_TXTATR_CONTENTCONTROL);
+    auto pTextContentControl = static_txtattr_cast<SwTextContentControl*>(pAttr);
+    auto& rFormatContentControl
+        = static_cast<SwFormatContentControl&>(pTextContentControl->GetAttr());
+    pWrtShell->GotoContentControl(rFormatContentControl);
+
+    // Then make sure that the callback is emitted:
+    // Without the accompanying fix in place, this test would have failed, no callback was emitted.
+    CPPUNIT_ASSERT(!m_aContentControl.isEmpty());
+    std::stringstream aStream(m_aContentControl.getStr());
+    boost::property_tree::ptree aTree;
+    boost::property_tree::read_json(aStream, aTree);
+    OString sAction = aTree.get_child("action").get_value<std::string>().c_str();
+    CPPUNIT_ASSERT_EQUAL(OString("change-picture"), sAction);
+
+    // And when replacing the image:
+    std::map<OUString, OUString> aArguments;
+    aArguments.emplace("type", "picture");
+    OUString aURL = m_directories.getURLFromSrc(u"sw/qa/extras/uiwriter/data/ole2.png");
+    aArguments.emplace("changed", aURL);
+    pXTextDocument->executeContentControlEvent(aArguments);
+
+    // Then make sure that the document is updated accordingly:
+    uno::Reference<drawing::XShape> xShape = getShape(1);
+    auto xGraphic = getProperty<uno::Reference<beans::XPropertySet>>(xShape, "Graphic");
+    // Without the accompanying fix in place, this test would have failed, xGraphic was empty after
+    // executeContentControlEvent().
+    CPPUNIT_ASSERT(xGraphic.is());
+    CPPUNIT_ASSERT_EQUAL(OUString("image/png"), getProperty<OUString>(xGraphic, "MimeType"));
+
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(SwTiledRenderingTest);