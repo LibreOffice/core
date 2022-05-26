LibreOffice/core

commit 3f768cddd28a2f04eb1ffa30bed4474deb6fbfc4

framework: avoid re-creating protocol handler instances all the time
Once you install an extension that adds its own protocol handlers (e.g.
<https://github.com/niocs/ProtocolHandlerExtension>), DispatchProvider
re-creates this protocol handler every time the custom menu gets opened
or a command gets dispatched.

This allows the dispatch provider to avoid managing the lifecycle of
those protocol handler instances, but in case the constructor of those
handlers is expensive, this leads to performance problems.

Introduce a map of handler instances in DispatchProvider to avoid
unnecessary re-creation and re-initialization: these instances get the
same XFrame anyway (the DispatchProvider is owned by the frame), so this
is meant to be safe.

No testcase for this -- the problem is only visible if you have an UNO
service registered in the global UNO service registry, but by the time
our cppunit tests run, that is already a fixed list, so this would be
hard to test from code.

Change-Id: I6d69906a795a2d5a67706002d635b6cb3091b856
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/133706
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

framework/source/dispatch/dispatchprovider.cxx
@@ -440,18 +440,30 @@ css::uno::Reference< css::frame::XDispatch > DispatchProvider::implts_searchProt
             SolarMutexGuard g;
 
             // create it
+            bool bInitialize = true;
             try
             {
-                xHandler.set(
-                    css::uno::Reference<css::lang::XMultiServiceFactory>(m_xContext->getServiceManager(), css::uno::UNO_QUERY_THROW)
-                      ->createInstance(aHandler.m_sUNOName),
-                    css::uno::UNO_QUERY);
+                // Only create the protocol handler instance once, the creation is expensive.
+                auto it = m_aProtocolHandlers.find(aHandler.m_sUNOName);
+                if (it == m_aProtocolHandlers.end())
+                {
+                    xHandler.set(
+                        css::uno::Reference<css::lang::XMultiServiceFactory>(m_xContext->getServiceManager(), css::uno::UNO_QUERY_THROW)
+                          ->createInstance(aHandler.m_sUNOName),
+                        css::uno::UNO_QUERY);
+                    m_aProtocolHandlers.emplace(aHandler.m_sUNOName, xHandler);
+                }
+                else
+                {
+                    xHandler = it->second;
+                    bInitialize = false;
+                }
             }
             catch(const css::uno::Exception&) {}
 
             // look if initialization is necessary
             css::uno::Reference< css::lang::XInitialization > xInit( xHandler, css::uno::UNO_QUERY );
-            if (xInit.is())
+            if (xInit.is() && bInitialize)
             {
                 css::uno::Reference< css::frame::XFrame > xOwner( m_xFrame.get(), css::uno::UNO_QUERY );
                 SAL_WARN_IF(!xOwner.is(), "fwk", "DispatchProvider::implts_searchProtocolHandler(): Couldn't get reference to my owner frame. So I can't set may needed context information for this protocol handler.");

framework/inc/dispatch/dispatchprovider.hxx
@@ -74,6 +74,8 @@ class DispatchProvider final : public ::cppu::WeakImplHelper< css::frame::XDispa
         css::uno::WeakReference< css::frame::XFrame > m_xFrame;
         /// cache of some other dispatch provider which are registered inside configuration to handle special URL protocols
         HandlerCache m_aProtocolHandlerCache;
+        std::unordered_map<OUString, css::uno::Reference<css::frame::XDispatchProvider>>
+            m_aProtocolHandlers;
 
     /* interface */
     public:

commit 4c45a73b917277f58de700af45d2132476ac8610

tdf#149261 sdext: fix crash on starting the presenter console for the 2nd time
This started with commit 3f768cddd28a2f04eb1ffa30bed4474deb6fbfc4
(framework: avoid re-creating protocol handler instances all the time,
2022-05-02). In case there are 2 monitors, then one monitor shows the
slideshow, the other shows the presenter console. The presenter
console's protocol handler in
sdext::presenter::PresenterProtocolHandler::Dispatch::Dispatch() has
mpPresenterController->GetWindowManager() as an empty reference on the
2nd time the presentation starts.

The above commit started to cache protocol handler instances at a frame
level for performance reasons, and this is meant to be safe in general,
but the presenter console's window manager is re-created between
slideshow runs, so it depends on framework/ code to re-create the
protocol handler all the time, which is problematic here.

Fix the problem by introducing a framework::CacheInfo interface that
allows protocol handler implementations to signal if they want to avoid
being cached.

This should be good enough for now, but if later it turns out that there
are too many broken protocol handlers out there, then we can consider
flipping the default and only cache handlers which explicitly opt in for
this behavior. This is not done in this commit.

Change-Id: Ic159813b1b339540bc8c4e780c4d6d7d2d4d2445

framework/source/dispatch/dispatchprovider.cxx
@@ -36,6 +36,7 @@
 #include <rtl/ustring.hxx>
 #include <vcl/svapp.hxx>
 #include <sal/log.hxx>
+#include <framework/dispatchhelper.hxx>
 
 namespace framework{
 
@@ -451,7 +452,13 @@ css::uno::Reference< css::frame::XDispatch > DispatchProvider::implts_searchProt
                         css::uno::Reference<css::lang::XMultiServiceFactory>(m_xContext->getServiceManager(), css::uno::UNO_QUERY_THROW)
                           ->createInstance(aHandler.m_sUNOName),
                         css::uno::UNO_QUERY);
-                    m_aProtocolHandlers.emplace(aHandler.m_sUNOName, xHandler);
+
+                    // Check if the handler explicitly requested to avoid caching.
+                    auto pCacheInfo = dynamic_cast<framework::CacheInfo*>(xHandler.get());
+                    if (!pCacheInfo || pCacheInfo->IsCachingAllowed())
+                    {
+                        m_aProtocolHandlers.emplace(aHandler.m_sUNOName, xHandler);
+                    }
                 }
                 else
                 {

include/framework/dispatchhelper.hxx
@@ -104,6 +104,16 @@ public:
     // XEventListener
     virtual void SAL_CALL disposing(const css::lang::EventObject& aEvent) override;
 };
+
+/// Interface that allows a protocol handler implementation to opt out from framework caching.
+class SAL_NO_VTABLE SAL_DLLPUBLIC_RTTI CacheInfo
+{
+public:
+    virtual bool IsCachingAllowed() const = 0;
+
+protected:
+    ~CacheInfo() noexcept = default;
+};
 }
 
 #endif

sdext/source/presenter/PresenterProtocolHandler.hxx
@@ -27,6 +27,7 @@
 #include <com/sun/star/lang/XInitialization.hpp>
 #include <com/sun/star/lang/XServiceInfo.hpp>
 #include <rtl/ref.hxx>
+#include <framework/dispatchhelper.hxx>
 
 namespace sdext::presenter {
 
@@ -40,6 +41,7 @@ class PresenterController;
 
 class PresenterProtocolHandler
     : protected ::cppu::BaseMutex,
+      public framework::CacheInfo,
       public PresenterProtocolHandlerInterfaceBase
 {
 public:
@@ -70,6 +72,9 @@ public:
     virtual css::uno::Sequence<css::uno::Reference<css::frame::XDispatch> > SAL_CALL
         queryDispatches(
             const css::uno::Sequence< css::frame::DispatchDescriptor>& rDescriptors) override;
+
+    /// See framework::CacheInfo::IsCachingAllowed().
+    bool IsCachingAllowed() const override { return false; }
 
 private:
     class Dispatch;