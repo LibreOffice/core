LibreOffice/core

commit 404c51f3664ffd4f7e2c1c8bb8a12ac70954fec2

officecfg,unotools,framework,sfx2: store ReadOnly for recent docs
Add Office::Histories::HistoryItem::ReadOnly flag to configuration so
that a document that was opened read-only isn't opened as editable from
recent documents.

Change-Id: I6985da287d3337a53a7e41e8e500421038eedb91
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/133385
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

framework/source/uielement/recentfilesmenucontroller.cxx
@@ -92,7 +92,7 @@ private:
     void fillPopupMenu( css::uno::Reference< css::awt::XPopupMenu > const & rPopupMenu );
     void executeEntry( sal_Int32 nIndex );
 
-    std::vector< OUString >   m_aRecentFilesItems;
+    std::vector<std::pair<OUString, bool>>   m_aRecentFilesItems;
     bool                      m_bDisabled : 1;
     bool                      m_bShowToolbarEntries;
 };
@@ -157,7 +157,7 @@ void RecentFilesMenuController::fillPopupMenu( Reference< css::awt::XPopupMenu >
         for ( int i = 0; i < nPickListMenuItems; i++ )
         {
             const SvtHistoryOptions::HistoryItem& rPickListEntry = aHistoryList[i];
-            m_aRecentFilesItems.push_back( rPickListEntry.sURL );
+            m_aRecentFilesItems.emplace_back(rPickListEntry.sURL, rPickListEntry.isReadOnly);
         }
     }
 
@@ -191,7 +191,7 @@ void RecentFilesMenuController::fillPopupMenu( Reference< css::awt::XPopupMenu >
 
             // Abbreviate URL
             OUString   aMenuTitle;
-            INetURLObject   aURL( m_aRecentFilesItems[i] );
+            INetURLObject const aURL(m_aRecentFilesItems[i].first);
             OUString aTipHelpText( aURL.getFSysPath( FSysStyle::Detect ) );
 
             if ( aURL.GetProtocol() == INetProtocol::File )
@@ -263,13 +263,15 @@ void RecentFilesMenuController::executeEntry( sal_Int32 nIndex )
     Sequence< PropertyValue > aArgsList{
         comphelper::makePropertyValue("Referer", OUString( "private:user" )),
 
+        comphelper::makePropertyValue("ReadOnly", m_aRecentFilesItems[nIndex].second),
+
         // documents in the picklist will never be opened as templates
         comphelper::makePropertyValue("AsTemplate", false),
 
         // Type detection needs to know which app we are opening it from.
         comphelper::makePropertyValue("DocumentService", m_aModuleName)
     };
-    dispatchCommand( m_aRecentFilesItems[ nIndex ], aArgsList, "_default" );
+    dispatchCommand(m_aRecentFilesItems[nIndex].first, aArgsList, "_default");
 }
 
 // XEventListener

sfx2/source/control/recentdocsviewitem.cxx
@@ -121,10 +121,13 @@ BitmapEx getModuleOverlay(const OUString& rURL)
 };
 
 RecentDocsViewItem::RecentDocsViewItem(sfx2::RecentDocsView &rView, const OUString &rURL,
-    const OUString &rTitle, std::u16string_view sThumbnailBase64, sal_uInt16 nId, tools::Long nThumbnailSize)
+    const OUString &rTitle, std::u16string_view const sThumbnailBase64,
+        sal_uInt16 const nId, tools::Long const nThumbnailSize,
+        bool const isReadOnly)
     : ThumbnailViewItem(rView, nId),
       mrParentView(rView),
       maURL(rURL),
+      m_isReadOnly(isReadOnly),
       m_bRemoveIconHighlighted(false),
       m_aRemoveRecentBitmap(BMP_RECENTDOC_REMOVE),
       m_aRemoveRecentBitmapHighlighted(BMP_RECENTDOC_REMOVE_HIGHLIGHTED)
@@ -317,6 +320,7 @@ void RecentDocsViewItem::OpenDocument()
     xTrans->parseStrict(aTargetURL);
 
     aArgsList = { comphelper::makePropertyValue("Referer", OUString("private:user")),
+                  comphelper::makePropertyValue("ReadOnly", m_isReadOnly),
                   // documents will never be opened as templates
                   comphelper::makePropertyValue("AsTemplate", false) };
 

dbaccess/source/ui/app/AppController.cxx
@@ -362,7 +362,7 @@ void SAL_CALL OApplicationController::disposing()
                             aURL.GetURLNoPass( INetURLObject::DecodeMechanism::NONE ),
                             aFilter,
                             getStrippedDatabaseName(),
-                            std::nullopt);
+                            std::nullopt, std::nullopt);
 
                     // add to recent document list
                     if ( aURL.GetProtocol() == INetProtocol::File )

dbaccess/source/ui/control/opendoccontrols.cxx
@@ -145,7 +145,7 @@ namespace dbaui
                 OUString sTitle = rHistoryItem.sTitle;
                 OUString sPassword = rHistoryItem.sPassword;
 
-                //  If the entry is an impress file then insert it into the
+                //  If the entry is a Base file then insert it into the
                 //  history list and the list box.
                 Sequence< PropertyValue > aProps;
                 xFilterFactory->getByName( sFilter ) >>= aProps;

include/unotools/historyoptions.hxx
@@ -58,6 +58,7 @@ namespace SvtHistoryOptions
         OUString sTitle;
         OUString sPassword;
         OUString sThumbnail;
+        bool isReadOnly = false;
     };
     UNOTOOLS_DLLPUBLIC std::vector< HistoryItem > GetList(EHistoryType eHistory);
 
@@ -72,7 +73,7 @@ namespace SvtHistoryOptions
     */
     UNOTOOLS_DLLPUBLIC void AppendItem(EHistoryType eHistory,
             const OUString& sURL, const OUString& sFilter, const OUString& sTitle,
-            const std::optional<OUString>& sThumbnail);
+            const std::optional<OUString>& sThumbnail, std::optional<bool> oIsReadOnly);
 
     /** Delete item from the specified list.
     */

officecfg/registry/schema/org/openoffice/Office/Histories.xcs
@@ -47,6 +47,11 @@
           <desc>Contains a base64 encoded thumbnail of the document.</desc>
         </info>
       </prop>
+      <prop oor:name="ReadOnly" oor:type="xs:boolean">
+        <info>
+          <desc>Stores whether the document was opened editable or read-only.</desc>
+        </info>
+      </prop>
     </group>
     <group oor:name="HistoryOrder">
       <info>

sfx2/inc/recentdocsview.hxx
@@ -63,7 +63,7 @@ public:
     RecentDocsView(std::unique_ptr<weld::ScrolledWindow> xWindow, std::unique_ptr<weld::Menu> xMenu);
     virtual ~RecentDocsView() override;
 
-    void insertItem(const OUString &rURL, const OUString &rTitle, const OUString& rThumbnail, sal_uInt16 nId);
+    void insertItem(const OUString &rURL, const OUString &rTitle, const OUString& rThumbnail, bool isReadOnly, sal_uInt16 nId);
 
     static bool typeMatchesExtension(ApplicationType type, std::u16string_view rExt);
 

sfx2/source/appl/newhelp.cxx
@@ -1186,7 +1186,9 @@ BookmarksTabPage_Impl::~BookmarksTabPage_Impl()
     SvtHistoryOptions::Clear( EHistoryType::HelpBookmarks );
     const sal_Int32 nCount = m_xBookmarksBox->n_children();
     for (sal_Int32 i = 0; i < nCount; ++i)
-        SvtHistoryOptions::AppendItem(EHistoryType::HelpBookmarks, m_xBookmarksBox->get_id(i), "", m_xBookmarksBox->get_text(i), std::nullopt);
+    {
+        SvtHistoryOptions::AppendItem(EHistoryType::HelpBookmarks, m_xBookmarksBox->get_id(i), "", m_xBookmarksBox->get_text(i), std::nullopt, std::nullopt);
+    }
 
     m_xBookmarksBox.reset();
     m_xBookmarksPB.reset();

commit 6547ed372deab55a34a9c1038e4b62636e910760

tdf#149170 framework,sfx2: don't add ReadOnly=false
There is a non-obvious difference in error handling when opening the
file read/write for type-detection fails in
utl::MediaDescriptor::impl_openStreamWithURL() - if ReadOnly=false is
given, it will abort the file loading, whereas without any ReadOnly it
will fall-back to a read-only opening of the file for type detection.

At this point a StillReadWriteInteraction is used, so the user is never
notified of the failure.

(regression from commit 404c51f3664ffd4f7e2c1c8bb8a12ac70954fec2)

Change-Id: I8c06fe23cc0bc0767df83f680a1a59e3700bbbbb

framework/source/uielement/recentfilesmenucontroller.cxx
@@ -264,14 +264,17 @@ void RecentFilesMenuController::executeEntry( sal_Int32 nIndex )
     Sequence< PropertyValue > aArgsList{
         comphelper::makePropertyValue("Referer", OUString( "private:user" )),
 
-        comphelper::makePropertyValue("ReadOnly", m_aRecentFilesItems[nIndex].second),
-
         // documents in the picklist will never be opened as templates
         comphelper::makePropertyValue("AsTemplate", false),
 
         // Type detection needs to know which app we are opening it from.
         comphelper::makePropertyValue("DocumentService", m_aModuleName)
     };
+    if (m_aRecentFilesItems[nIndex].second) // tdf#149170 only add if true
+    {
+        aArgsList.realloc(aArgsList.size()+1);
+        aArgsList.getArray()[aArgsList.size()-1] = comphelper::makePropertyValue("ReadOnly", true);
+    }
     dispatchCommand(m_aRecentFilesItems[nIndex].first, aArgsList, "_default");
 }
 

sfx2/source/control/recentdocsviewitem.cxx
@@ -320,9 +320,13 @@ void RecentDocsViewItem::OpenDocument()
     xTrans->parseStrict(aTargetURL);
 
     aArgsList = { comphelper::makePropertyValue("Referer", OUString("private:user")),
-                  comphelper::makePropertyValue("ReadOnly", m_isReadOnly),
                   // documents will never be opened as templates
                   comphelper::makePropertyValue("AsTemplate", false) };
+    if (m_isReadOnly) // tdf#149170 only add if true
+    {
+        aArgsList.realloc(aArgsList.size()+1);
+        aArgsList.getArray()[aArgsList.size()-1] = comphelper::makePropertyValue("ReadOnly", true);
+    }
 
     xDispatch = xDesktop->queryDispatch(aTargetURL, "_default", 0);
 