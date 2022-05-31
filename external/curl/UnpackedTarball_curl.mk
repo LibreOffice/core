225d8f31ad5fdcb802fbaa4ccb5266c80a3dd037
Michael Stahl <michael.stahl@allotropia.de>
2022-05-31 11:43:07 +0200

    curl: remove curl-7.26.0_win-proxy.patch
    There is no justification why this patch was added, upstream doesn't
    want to add such a feature[1], and maintaining feature patches in LO
    for bundled externals doesn't make sense.
    
    Also, this doesn't respect the Inet::Settings::ooInetHTTPProxy*
    settings from the configuration.
    
    Let's remove it and hope nobody complains.
    
    [1] https://curl.se/mail/lib-2022-05/0083.html
    
    Change-Id: I1ccdefb76f69e6795a28d4d25bf443555c16ab0b
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/135182
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

--- external/curl/UnpackedTarball_curl.mk
+++ external/curl/UnpackedTarball_curl.mk
@@ -23,7 +23,6 @@ $(eval $(call gb_UnpackedTarball_add_patches,curl,\
 	external/curl/curl-msvc.patch.1 \
 	external/curl/curl-msvc-zlib.patch.1 \
 	external/curl/curl-msvc-disable-protocols.patch.1 \
-	external/curl/curl-7.26.0_win-proxy.patch \
 	external/curl/zlib.patch.0 \
 	external/curl/configurable-z-option.patch.0 \
 ))
