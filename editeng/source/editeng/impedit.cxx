c04ae8ca9c62592fbc4feddc35b0544b9d3ffee0
Noel Grandin <noel.grandin@collabora.co.uk>
2022-06-02 13:20:38 +0200

    tdf#143964 insert/overwrite cursor shape lags in formula editing
    Revert "tdf#132740 bypass work if selection has not changed"
    
    This reverts commit 865641047be4a693f7a51635ce06eab15675126d.
    
    Change-Id: I69244a1e57ce979bf21946e31df7165f8791423c
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/135302
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
    (cherry picked from commit 30703dbe59e909e257876524785836e7f4d993b2)

--- editeng/source/editeng/impedit.cxx
+++ editeng/source/editeng/impedit.cxx
@@ -241,9 +241,6 @@ const OutlinerViewShell* ImpEditView::GetViewShell() const
 
 void ImpEditView::SetEditSelection( const EditSelection& rEditSelection )
 {
-    if (aEditSelection == rEditSelection)
-        return;
-
     // set state before notification
     aEditSelection = rEditSelection;
 
