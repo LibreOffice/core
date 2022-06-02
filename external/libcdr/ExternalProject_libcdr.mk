75476dc8df9d09c9fc65f1b06f1b3ec3d6ed5ad4
Hossein <hossein@libreoffice.org>
2022-06-02 06:03:37 +0200

    Use autoreconf in some external projects
    While building LibreOffifce on Raspberry Pi (armhf) platform, the
    build encounters problems while configuring some external projects,
    complaining the inconsistency with the autoconf version.
    This problem can be fixed by adding autoreconf to the .mk files.
    
    Change-Id: I247b98c2e9fe313a92644b1180abe365a78f1875

--- external/libcdr/ExternalProject_libcdr.mk
+++ external/libcdr/ExternalProject_libcdr.mk
@@ -27,6 +27,7 @@ $(call gb_ExternalProject_get_state_target,libcdr,build) :
 	$(call gb_Trace_StartRange,libcdr,EXTERNAL)
 	$(call gb_ExternalProject_run,build,\
 		export PKG_CONFIG="" \
+        && autoreconf \
 		&& MAKE=$(MAKE) $(gb_RUN_CONFIGURE) ./configure \
 			--with-pic \
 			--enable-static \
