LibreOffice/core

commit 07021596acb3d104ba129d371b1ae0b79f67a6a4

tdf#142242 Forename imported with trailing space
Fix the case where there is only a name, and no surname.

Change-Id: If11078364924c8332b113e5650e7ea2b262a357a
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/117080
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

unotools/source/config/useroptions.cxx
@@ -222,7 +222,7 @@ OUString SvtUserOptions::Impl::GetFullName () const
         sFullName += GetToken(UserOptToken::FathersName).trim();
         if (!sFullName.isEmpty())
             sFullName += " ";
-        sFullName += GetToken(UserOptToken::LastName).trim();
+        sFullName += GetToken(UserOptToken::LastName);
     }
     else
     {
@@ -231,16 +231,17 @@ OUString SvtUserOptions::Impl::GetFullName () const
             sFullName = GetToken(UserOptToken::LastName).trim();
             if (!sFullName.isEmpty())
                 sFullName += " ";
-            sFullName += GetToken(UserOptToken::FirstName).trim();
+            sFullName += GetToken(UserOptToken::FirstName);
         }
         else
         {
             sFullName = GetToken(UserOptToken::FirstName).trim();
             if (!sFullName.isEmpty())
                 sFullName += " ";
-            sFullName += GetToken(UserOptToken::LastName).trim();
+            sFullName += GetToken(UserOptToken::LastName);
         }
     }
+    sFullName = sFullName.trim();
 
     return sFullName;
 }

commit 6db3f5c058647233d25bb49d7ad0609daf18031f

tdf#146988 Opening at cursor position doesn't work in large documents
regression from
    commit 07021596acb3d104ba129d371b1ae0b79f67a6a4 (HEAD)
    Date:   Sat Jun 12 18:57:51 2021 +0200
    tdf#142242 Forename imported with trailing space

Change-Id: I792fbe5955de464f41ac61c85f1ca651121b4a99

unotools/source/config/useroptions.cxx
@@ -221,7 +221,7 @@ OUString SvtUserOptions::Impl::GetFullName () const
         sFullName += o3tl::trim(GetToken(UserOptToken::FathersName));
         if (!sFullName.isEmpty())
             sFullName += " ";
-        sFullName += GetToken(UserOptToken::LastName);
+        sFullName += o3tl::trim(GetToken(UserOptToken::LastName));
     }
     else
     {
@@ -230,14 +230,14 @@ OUString SvtUserOptions::Impl::GetFullName () const
             sFullName = GetToken(UserOptToken::LastName).trim();
             if (!sFullName.isEmpty())
                 sFullName += " ";
-            sFullName += GetToken(UserOptToken::FirstName);
+            sFullName += o3tl::trim(GetToken(UserOptToken::FirstName));
         }
         else
         {
             sFullName = GetToken(UserOptToken::FirstName).trim();
             if (!sFullName.isEmpty())
                 sFullName += " ";
-            sFullName += GetToken(UserOptToken::LastName);
+            sFullName += o3tl::trim(GetToken(UserOptToken::LastName));
         }
     }
     sFullName = sFullName.trim();