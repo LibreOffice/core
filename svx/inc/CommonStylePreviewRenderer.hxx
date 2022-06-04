e0e944a42059271d082f8bd5121f30a4536fb8f3
Noel Grandin <noelgrandin@gmail.com>
2022-06-04 19:19:15 +0200

    no need to allocate this separately
    Change-Id: I30df477cd708d021fc5b6fcfa9925e1c99be6650
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/135429
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

--- svx/inc/CommonStylePreviewRenderer.hxx
+++ svx/inc/CommonStylePreviewRenderer.hxx
@@ -9,8 +9,9 @@
 
 #pragma once
 
-#include <memory>
+#include <optional>
 
+#include <editeng/svxfont.hxx>
 #include <sfx2/objsh.hxx>
 #include <sfx2/StylePreviewRenderer.hxx>
 #include <rtl/ustring.hxx>
@@ -19,13 +20,12 @@
 
 class OutputDevice;
 class SfxStyleSheetBase;
-class SvxFont;
 
 namespace svx
 {
 class CommonStylePreviewRenderer final : public sfx2::StylePreviewRenderer
 {
-    std::unique_ptr<SvxFont> m_pFont;
+    std::optional<SvxFont> m_oFont;
     Color maFontColor;
     Color maHighlightColor;
     Color maBackgroundColor;
