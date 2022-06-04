e4b4669b1517a7366d86cb1dd6c184415f2b76c8
Caolán McNamara <caolanm@redhat.com>
2022-06-04 21:10:16 +0100

    restore %PRODUCTNAME to accessible descriptions that need them
    The original problem was that %PRODUCTNAME wasn't replaced for
    accessibility descriptions (which are reused for extended tips) under
    gtk.
    
    Universally querying all a11y descs on load to potentially replace
    %PRODUCTNAME in a11y descs at runtime led to tdf146971 which was a huge
    startup slowdown.
    
    The half way 7.3 fix was to leave a11y descs alone, but do the
    replacement when querying for the extended tip case. So the extended
    tooltips were ok, but screen readers would still say a raw
    "%PRODUCTNAME" text, hence the rewording effort to remove %PRODUCTNAME
    from the a11y descs entirely for 7.4.
    
    But there is now a few cases where some options paths exists in the a11y
    descs which is not exactly correct wrt to the text shown in the options
    dialog.
    
    Reworking the options dialog to not have %PRODUCTNAME there at all and
    updating everything to fit that sort of change would not be popular. So
    move the cases where a11y descs really should have %PRODUCTNAME in them
    out of the .ui files and into .hrc files and use specific
    set_accessibility_description calls for them via ResID which will do
    the %PRODUCTNAME replacement automatically.
    
    Hopefully the a11y runtime cost for just this handful of cases is
    negligible in the overall scheme of things.
    
    Change-Id: Ieb17d26fd581cd5804a52b371b3bb5ea43023aa3

--- sc/source/ui/optdlg/tpview.cxx
+++ sc/source/ui/optdlg/tpview.cxx
@@ -71,6 +71,7 @@ ScTpContentOptions::ScTpContentOptions(weld::Container* pPage, weld::DialogContr
     m_xFormulaCB->connect_toggled(aCBHdl);
     m_xNilCB->connect_toggled(aCBHdl);
     m_xAnnotCB->connect_toggled(aCBHdl);
+    m_xAnnotCB->set_accessible_description(ScResId(STR_A11Y_DESC_ANNOT));
     m_xValueCB->connect_toggled(aCBHdl);
     m_xAnchorCB->connect_toggled(aCBHdl);
     m_xClipMarkCB->connect_toggled(aCBHdl);
