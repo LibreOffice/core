a89d1565df26e34693d921b2ff265d199feaf290
Szymon Kłos <szymon.klos@collabora.com>
2022-06-08 14:56:46 +0200

    jsdialog: be sure we send close popup message when destroyed
    use different m_sTypeOfJSON for popups so we can later
    detect popup closing
    
    Change-Id: Id0086815317011b142b49f4e1421b76e1e663358

--- vcl/jsdialog/jsdialogbuilder.cxx
+++ vcl/jsdialog/jsdialogbuilder.cxx
@@ -515,7 +515,10 @@ JSInstanceBuilder::JSInstanceBuilder(weld::Widget* pParent, const OUString& rUIR
 {
     // when it is a popup we initialize sender in weld_popover
     if (bPopup)
+    {
+        m_sTypeOfJSON = "popup";
         return;
+    }
 
     vcl::Window* pRoot = m_xBuilder->get_widget_root();
 
@@ -662,6 +665,9 @@ JSInstanceBuilder::CreateFormulabarBuilder(vcl::Window* pParent, const OUString&
 
 JSInstanceBuilder::~JSInstanceBuilder()
 {
+    if (m_sTypeOfJSON == "popup")
+        sendClosePopup(m_nWindowId);
+
     if (m_aWindowToRelease)
     {
         m_aWindowToRelease->ReleaseLOKNotifier();
