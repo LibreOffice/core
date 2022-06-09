3b3f5f974175e783115f1a11786c9cc74a1e2e33
Luboš Luňák <l.lunak@collabora.com>
2022-06-09 15:08:41 +0200

    remove incorrect #ifdef DBG_UTIL around normal code (tdf#149476)
    This code sets sufficient size for the virtual device that saves
    the content, so it's not some optional debug code. Without this
    selecting text in Calc formula bar and dragging it around leaves
    artifacts, because the saved content is not large enough to restore
    the original widget content. This dates back to
    d51fe1189dcbe0fe06805c8d99687c125cd254eb, which supposedly made
    the code warning-free (and also correct-free).
    
    Change-Id: I52dab8c8d3a083fff9da8aed27facdf21076622e
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/135555
    Tested-by: Jenkins
    Reviewed-by: Luboš Luňák <l.lunak@collabora.com>
    (cherry picked from commit dabf52b81621f1cbff508dfb7ae2e92597e521c3)

--- editeng/source/editeng/impedit.cxx
+++ editeng/source/editeng/impedit.cxx
@@ -2175,9 +2175,6 @@ void ImpEditView::ShowDDCursor( const tools::Rectangle& rRect )
     aSaveRect.AdjustRight(1 );
     aSaveRect.AdjustBottom(1 );
 
-#ifdef DBG_UTIL
-    Size aNewSzPx( aSaveRect.GetSize() );
-#endif
     if ( !pDragAndDropInfo->pBackground )
     {
         pDragAndDropInfo->pBackground = VclPtr<VirtualDevice>::Create(rOutDev);
@@ -2187,14 +2184,13 @@ void ImpEditView::ShowDDCursor( const tools::Rectangle& rRect )
 
     }
 
-#ifdef DBG_UTIL
+    Size aNewSzPx( aSaveRect.GetSize() );
     Size aCurSzPx( pDragAndDropInfo->pBackground->GetOutputSizePixel() );
     if ( ( aCurSzPx.Width() < aNewSzPx.Width() ) ||( aCurSzPx.Height() < aNewSzPx.Height() ) )
     {
         bool bDone = pDragAndDropInfo->pBackground->SetOutputSizePixel( aNewSzPx );
         DBG_ASSERT( bDone, "Virtual Device broken?" );
     }
-#endif
 
     aSaveRect = rOutDev.PixelToLogic( aSaveRect );
 
