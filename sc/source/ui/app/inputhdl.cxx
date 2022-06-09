a9c5a92e7832ec0313efce464361402db8355232
Caolán McNamara <caolanm@redhat.com>
2022-06-09 12:46:49 +0100

    Resolves: tdf#142368 drop popover on ControlLoseFocus
    which is what appears when the Control derived inputbar loses focus
    while the main window losing focus gives WindowLoseFocus
    
    Change-Id: Iae9a2874c3dd513a1a092a18846858f819542370
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/135534
    Tested-by: Jenkins
    Reviewed-by: Adolfo Jayme Barrientos <fitojb@ubuntu.com>

--- sc/source/ui/app/inputhdl.cxx
+++ sc/source/ui/app/inputhdl.cxx
@@ -1080,14 +1080,14 @@ void ScInputHandler::GetFormulaData()
 IMPL_LINK( ScInputHandler, ShowHideTipVisibleParentListener, VclWindowEvent&, rEvent, void )
 {
     if (rEvent.GetId() == VclEventId::ObjectDying || rEvent.GetId() == VclEventId::WindowHide
-        || rEvent.GetId() == VclEventId::WindowLoseFocus)
+        || rEvent.GetId() == VclEventId::WindowLoseFocus || rEvent.GetId() == VclEventId::ControlLoseFocus)
         HideTip();
 }
 
 IMPL_LINK( ScInputHandler, ShowHideTipVisibleSecParentListener, VclWindowEvent&, rEvent, void )
 {
     if (rEvent.GetId() == VclEventId::ObjectDying || rEvent.GetId() == VclEventId::WindowHide
-        || rEvent.GetId() == VclEventId::WindowLoseFocus)
+        || rEvent.GetId() == VclEventId::WindowLoseFocus || rEvent.GetId() == VclEventId::ControlLoseFocus)
         HideTipBelow();
 }
 
