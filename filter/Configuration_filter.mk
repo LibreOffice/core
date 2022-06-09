9a5d46070e70e8cf42bea0dad37063d9ed71e375
offtkp <parisoplop@gmail.com>
2022-06-09 16:22:32 +0300

    tdf#100522 Add support for exporting SVGZ in Draw
    Detect draw_svgz_Export and compress the SVG before exporting if needed
    
    Change-Id: I7c8463292ee845a3ce2f2a3c5aa876b25dcedb2e
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/135556
    Tested-by: Jenkins
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

--- filter/Configuration_filter.mk
+++ filter/Configuration_filter.mk
@@ -714,6 +714,7 @@ $(eval $(call filter_Configuration_add_types,fcfg_langpack,fcfg_drawgraphics_typ
 	ras_Sun_Rasterfile \
 	svg_Scalable_Vector_Graphics_Draw \
 	svg_Scalable_Vector_Graphics \
+	svgz_Compressed_Scalable_Vector_Graphics \
 	svm_StarView_Metafile \
 	tga_Truevision_TARGA \
 	tif_Tag_Image_File \
@@ -744,6 +745,7 @@ $(eval $(call filter_Configuration_add_filters,fcfg_langpack,fcfg_drawgraphics_f
 	RAS___Sun_Rasterfile \
 	SVG___Scalable_Vector_Graphics_Draw \
 	SVG___Scalable_Vector_Graphics \
+	SVGZ___Compressed_Scalable_Vector_Graphics \
 	SVM___StarView_Metafile \
 	TGA___Truevision_TARGA \
 	TIF___Tag_Image_File \
@@ -764,6 +766,7 @@ $(eval $(call filter_Configuration_add_filters,fcfg_langpack,fcfg_drawgraphics_f
 	draw_jpg_Export \
 	draw_png_Export \
 	draw_svg_Export \
+	draw_svgz_Export \
 	draw_tif_Export \
 	draw_webp_Export \
 	draw_wmf_Export \
@@ -789,6 +792,7 @@ $(eval $(call filter_Configuration_add_types,fcfg_langpack,fcfg_impressgraphics_
 	ras_Sun_Rasterfile \
 	svg_Scalable_Vector_Graphics_Draw \
 	svg_Scalable_Vector_Graphics \
+	svgz_Compressed_Scalable_Vector_Graphics \
 	svm_StarView_Metafile \
 	tif_Tag_Image_File \
 	wmf_MS_Windows_Metafile \
@@ -862,6 +866,7 @@ $(eval $(call filter_Configuration_add_types,fcfg_langpack,fcfg_internalgraphics
 	psd_Adobe_Photoshop \
 	ras_Sun_Rasterfile \
 	svg_Scalable_Vector_Graphics \
+	svgz_Compressed_Scalable_Vector_Graphics \
 	svm_StarView_Metafile \
 	tga_Truevision_TARGA \
 	tif_Tag_Image_File \
@@ -903,6 +908,8 @@ $(eval $(call filter_Configuration_add_internal_filters,fcfg_langpack,fcfg_inter
 	ras_Import \
 	svg_Export \
 	svg_Import \
+	svgz_Export \
+	svgz_Import \
 	svm_Export \
 	svm_Import \
 	tga_Import \
