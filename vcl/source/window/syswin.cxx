349fde604379cb27f74d77b333400eee40014cc9
Luboš Luňák <l.lunak@collabora.com>
2022-06-09 18:23:44 +0200

    avoid uninitialized data when handling WindowState
    I get valgrind reports when I start LO with kf5 backend and
    a maximized window, since in that case position and size are not
    set (since the maximized state makes them irrelevant). The semantics
    here seem to conflict, the caller of GetWindowState() sets flags
    to say what data it wants, and SalFrame::GetWindowState() sets flags
    to say what data it provides, but as can be seen here those aren't
    necessarily the same. So actually use only those flags that are
    in both sets.
    
    Change-Id: I4559baab90d6f465382560549d52431b28119b3f

--- vcl/source/window/syswin.cxx
+++ vcl/source/window/syswin.cxx
@@ -755,7 +755,10 @@ void SystemWindow::GetWindowState(vcl::WindowData& rData) const
         return;
 
     if ( mbSysChild )
+    {
+        rData.SetMask( WindowStateMask::NONE );
         return;
+    }
 
     const vcl::Window* pWindow = this;
     while ( pWindow->mpWindowImpl->mpBorderWindow )
@@ -767,6 +770,9 @@ void SystemWindow::GetWindowState(vcl::WindowData& rData) const
         aState.setMask(vcl::WindowDataMask::All);
         if ( mpWindowImpl->mpFrame->GetWindowState( &aState ) )
         {
+            // Limit mask only to what we've received, the rest is not set.
+            nValidMask &= aState.mnMask;
+            rData.SetMask( nValidMask );
             if ( nValidMask & vcl::WindowDataMask::X )
                 rData.setX( aState.x() );
             if ( nValidMask & vcl::WindowDataMask::Y )
@@ -775,26 +781,14 @@ void SystemWindow::GetWindowState(vcl::WindowData& rData) const
                 rData.setWidth( aState.width() );
             if ( nValidMask & vcl::WindowDataMask::Height )
                 rData.setHeight( aState.height() );
-            if ( aState.mask() & vcl::WindowDataMask::MaximizedX )
-            {
+            if ( nValidMask & vcl::WindowDataMask::MaximizedX )
                 rData.SetMaximizedX( aState.GetMaximizedX() );
-                nValidMask |= vcl::WindowDataMask::MaximizedX;
-            }
-            if ( aState.mask() & vcl::WindowDataMask::MaximizedY )
-            {
+            if ( nValidMask & vcl::WindowDataMask::MaximizedY )
                 rData.SetMaximizedY( aState.GetMaximizedY() );
-                nValidMask |= vcl::WindowDataMask::MaximizedY;
-            }
-            if ( aState.mask() & vcl::WindowDataMask::MaximizedWidth )
-            {
+            if ( nValidMask & vcl::WindowDataMask::MaximizedWidth )
                 rData.SetMaximizedWidth( aState.GetMaximizedWidth() );
-                nValidMask |= vcl::WindowDataMask::MaximizedWidth;
-            }
-            if ( aState.mask() & vcl::WindowDataMask::MaximizedHeight )
-            {
+            if ( nValidMask & vcl::WindowDataMask::MaximizedHeight )
                 rData.SetMaximizedHeight( aState.GetMaximizedHeight() );
-                nValidMask |= vcl::WindowDataMask::MaximizedHeight;
-            }
             if ( nValidMask & vcl::WindowDataMask::State )
             {
                 // #94144# allow Minimize again, should be masked out when read from configuration
@@ -814,6 +808,9 @@ void SystemWindow::GetWindowState(vcl::WindowData& rData) const
         Size    aSize = GetSizePixel();
         vcl::WindowState nState = vcl::WindowState::NONE;
 
+        nValidMask &= vcl::WindowDataMask::X | WindowDataMask::Y
+            | WindowDataMask::Width | WindowDataMask::Height | WindowDataMask::State;
+        rData.SetMask( nValidMask );
         if (nValidMask & vcl::WindowDataMask::X)
             rData.setX(aPos.X());
         if (nValidMask & vcl::WindowDataMask::Y)
