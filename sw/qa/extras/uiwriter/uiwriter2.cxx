f85a5b4d5cafc80e01e2891cb1427ee94abe1d41
Michael Stahl <michael.stahl@allotropia.de>
2022-06-10 16:26:40 +0200

    tdf#139982 sw: preserve flys in Replace with redlining enabled
    The problem is that there isn't a redline type "Replace" so it's
    represented as Delete+Insert.
    
    To prevent the flys anchored in the text from being deleted, move the
    anchors to the point between the old (deleted) and new (inserted) text.
    
    (regression from commit 28b77c89dfcafae82cf2a6d85731b643ff9290e5)
    
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/135604
    Tested-by: Michael Stahl <michael.stahl@allotropia.de>
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
    (cherry picked from commit 646c6ddd91a98afddf914e3889cb269fc814c060)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/135737
    Tested-by: Jenkins
    Reviewed-by: Thorsten Behrens <thorsten.behrens@allotropia.de>
    (cherry picked from commit b7ab83bd96c70932c2223c8d0b3bc0f24327cef2)
    
    Change-Id: Ib600c9dbfb9421917e4b8d61195c48cf0b364f06
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/136220
    Tested-by: Michael Stahl <michael.stahl@allotropia.de>
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

--- sw/qa/extras/uiwriter/uiwriter2.cxx
+++ sw/qa/extras/uiwriter/uiwriter2.cxx
@@ -908,6 +908,56 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest2, testTdf140007)
                          pDoc->GetNodes()[SwNodeOffset(11)]->GetTextNode()->GetText());
 }
 
+CPPUNIT_TEST_FIXTURE(SwUiWriterTest2, testTdf139982)
+{
+    SwDoc* const pDoc = createDoc();
+    SwWrtShell* const pWrtShell = pDoc->GetDocShell()->GetWrtShell();
+
+    // turn on redlining and show changes
+    pDoc->getIDocumentRedlineAccess().SetRedlineFlags(RedlineFlags::On | RedlineFlags::ShowDelete
+                                                      | RedlineFlags::ShowInsert);
+    CPPUNIT_ASSERT_MESSAGE("redlining should be on",
+                           pDoc->getIDocumentRedlineAccess().IsRedlineOn());
+    CPPUNIT_ASSERT_MESSAGE(
+        "redlines should be visible",
+        IDocumentRedlineAccess::IsShowChanges(pDoc->getIDocumentRedlineAccess().GetRedlineFlags()));
+
+    pWrtShell->Insert("helloo");
+
+    pWrtShell->Left(CRSR_SKIP_CHARS, /*bSelect=*/false, 1, /*bBasicCall=*/false);
+    {
+        SwFormatAnchor anchor(RndStdIds::FLY_AT_CHAR);
+        anchor.SetAnchor(pWrtShell->GetCursor()->GetPoint());
+        SfxItemSet flySet(pDoc->GetAttrPool(), svl::Items<RES_ANCHOR, RES_ANCHOR>{});
+        flySet.Put(anchor);
+        SwFrameFormat const* pFly = pWrtShell->NewFlyFrame(flySet, /*bAnchValid=*/true);
+        CPPUNIT_ASSERT(pFly != nullptr);
+    }
+
+    pWrtShell->SttEndDoc(true);
+    pWrtShell->EndPara(/*bSelect=*/true);
+
+    CPPUNIT_ASSERT_EQUAL(size_t(1), pWrtShell->GetFlyCount(FLYCNTTYPE_FRM));
+
+    pWrtShell->Replace("hello", true);
+
+    // the problem was that a redline delete with the same author as redline
+    // insert has its text deleted immediately, including anchored flys.
+    CPPUNIT_ASSERT_EQUAL(size_t(1), pWrtShell->GetFlyCount(FLYCNTTYPE_FRM));
+
+    pWrtShell->Undo();
+
+    CPPUNIT_ASSERT_EQUAL(size_t(1), pWrtShell->GetFlyCount(FLYCNTTYPE_FRM));
+
+    pWrtShell->Redo();
+
+    CPPUNIT_ASSERT_EQUAL(size_t(1), pWrtShell->GetFlyCount(FLYCNTTYPE_FRM));
+
+    pWrtShell->Undo();
+
+    CPPUNIT_ASSERT_EQUAL(size_t(1), pWrtShell->GetFlyCount(FLYCNTTYPE_FRM));
+}
+
 CPPUNIT_TEST_FIXTURE(SwUiWriterTest2, testTdf54819)
 {
     load(DATA_DIRECTORY, "tdf54819.fodt");
