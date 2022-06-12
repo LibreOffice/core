LibreOffice/core

commit f71606c920a3f78294da745cd9ef1eacde010224

new loplugin:moveit
look for local variables that can be std::move'd to parameters

off by default, since it doesn't do proper data flow analysis

Change-Id: I3403a0fcffd165bdea6a772528bc53995c5fdb40
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/135527
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sfx2/source/control/dispatch.cxx
@@ -1850,7 +1850,7 @@ void SfxDispatcher::ExecutePopup( const OUString& rResName, vcl::Window* pWin, c
     else
     {
         OUString aMenuURL = "private:resource/popupmenu/" + rResName;
-        if (GetFrame()->GetViewShell()->TryContextMenuInterception(xPopupMenu, aMenuURL, aEvent))
+        if (GetFrame()->GetViewShell()->TryContextMenuInterception(xPopupMenu, aMenuURL, std::move(aEvent)))
         {
             css::uno::Reference<css::awt::XWindowPeer> xParent(aEvent.SourceWindow, css::uno::UNO_QUERY);
             xPopupMenu->execute(xParent, css::awt::Rectangle(aPos.X(), aPos.Y(), 1, 1), css::awt::PopupMenuDirection::EXECUTE_DOWN);

basic/source/classes/sbunoobj.cxx
@@ -2053,7 +2053,7 @@ void SbUnoObject::Notify( SfxBroadcaster& rBC, const SfxHint& rHint )
                         {
                             if ( pProp->isUnoStruct() )
                             {
-                                SbUnoStructRefObject* pSbUnoObject = new SbUnoStructRefObject( pProp->GetName(), aMember );
+                                SbUnoStructRefObject* pSbUnoObject = new SbUnoStructRefObject( pProp->GetName(), std::move(aMember) );
                                 SbxObjectRef xWrapper = static_cast<SbxObject*>(pSbUnoObject);
                                 pVar->PutObject( xWrapper.get() );
                             }
@@ -4679,7 +4679,7 @@ SbxVariable* SbUnoStructRefObject::Find( const OUString& rName, SbxClassType t )
             Property aProp;
             aProp.Name = rName;
             aProp.Type = css::uno::Type( it->second->getTypeClass(), it->second->getTypeName() );
-            SbUnoProperty* pProp = new SbUnoProperty( rName, eSbxType, eRealSbxType, aProp, 0, false, ( aProp.Type.getTypeClass() == css::uno::TypeClass_STRUCT) );
+            SbUnoProperty* pProp = new SbUnoProperty( rName, eSbxType, eRealSbxType, std::move(aProp), 0, false, ( aProp.Type.getTypeClass() == css::uno::TypeClass_STRUCT) );
             SbxVariableRef xVarRef = pProp;
             QuickInsert( xVarRef.get() );
             pRes = xVarRef.get();
@@ -4739,7 +4739,7 @@ void SbUnoStructRefObject::implCreateAll()
         Property aProp;
         aProp.Name = rName;
         aProp.Type = css::uno::Type( field.second->getTypeClass(), field.second->getTypeName() );
-        SbUnoProperty* pProp = new SbUnoProperty( rName, eSbxType, eRealSbxType, aProp, 0, false, ( aProp.Type.getTypeClass() == css::uno::TypeClass_STRUCT) );
+        SbUnoProperty* pProp = new SbUnoProperty( rName, eSbxType, eRealSbxType, std::move(aProp), 0, false, ( aProp.Type.getTypeClass() == css::uno::TypeClass_STRUCT) );
         SbxVariableRef xVarRef = pProp;
         QuickInsert( xVarRef.get() );
     }

chart2/source/tools/MovingAverageRegressionCurveCalculator.cxx
@@ -62,19 +62,19 @@ void SAL_CALL MovingAverageRegressionCurveCalculator::recalculateRegression(
         case MovingAverageType::Central:
         {
 
-            calculateValuesCentral(aValues);
+            calculateValuesCentral(std::move(aValues));
             break;
         }
 
         case MovingAverageType::AveragedAbscissa:
         {
-            calculateValues(aValues, true);
+            calculateValues(std::move(aValues), true);
             break;
         }
         case MovingAverageType::Prior:
         default:
         {
-            calculateValues(aValues, false);
+            calculateValues(std::move(aValues), false);
             break;
         }
     }

compilerplugins/clang/moveit.cxx
@@ -1,0 +1,250 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * Based on LLVM/Clang.
+ *
+ * This file is distributed under the University of Illinois Open Source
+ * License. See LICENSE.TXT for details.
+ *
+ */
+
+#include <cassert>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include "config_clang.h"
+#include "plugin.hxx"
+#include "check.hxx"
+
+/*
+Look for local variables that can be std::move'd into parameters.
+
+TODO
+(*) Ideally we would use a proper data-flow analysis, to detect that the var is dead after this point,
+     like the one in clang at include/clang/Analysis/CFG.h
+(*) we could expand the set of approved/interesting types
+*/
+
+namespace
+{
+class MoveIt : public loplugin::FilteringPlugin<MoveIt>
+{
+public:
+    explicit MoveIt(loplugin::InstantiationData const& data)
+        : FilteringPlugin(data)
+    {
+    }
+
+    virtual bool preRun() override
+    {
+        std::string fn(handler.getMainFileName());
+        loplugin::normalizeDotDotInFilePath(fn);
+        // false +
+        if (loplugin::hasPathnamePrefix(fn, SRCDIR "/basic/source/classes/sbunoobj.cxx"))
+            return false;
+        if (loplugin::hasPathnamePrefix(fn, SRCDIR "/svx/source/svdraw/textchainflow.cxx"))
+            return false;
+        if (loplugin::hasPathnamePrefix(fn, SRCDIR "/svx/source/unodraw/unoshtxt.cxx"))
+            return false;
+        if (loplugin::hasPathnamePrefix(fn,
+                                        SRCDIR "/connectivity/source/drivers/dbase/dindexnode.cxx"))
+            return false;
+        if (loplugin::hasPathnamePrefix(fn, SRCDIR "/basctl/source/basicide/moduldlg.cxx"))
+            return false;
+        if (loplugin::hasPathnamePrefix(fn, SRCDIR "/sw/source/core/SwNumberTree/SwNumberTree.cxx"))
+            return false;
+        if (loplugin::hasPathnamePrefix(fn, SRCDIR "/sc/source/core/tool/scmatrix.cxx"))
+            return false;
+        if (loplugin::hasPathnamePrefix(fn, SRCDIR "/sw/source/core/bastyp/calc.cxx"))
+            return false;
+        if (loplugin::hasPathnamePrefix(fn, SRCDIR "/sw/source/core/edit/edattr.cxx"))
+            return false;
+        if (loplugin::hasPathnamePrefix(fn, SRCDIR "/sw/source/core/fields/expfld.cxx"))
+            return false;
+        if (loplugin::hasPathnamePrefix(fn, SRCDIR "/sw/source/uibase/docvw/edtwin.cxx"))
+            return false;
+        if (loplugin::hasPathnamePrefix(fn, SRCDIR "/sw/source/uibase/uiview/viewling.cxx"))
+            return false;
+        if (loplugin::hasPathnamePrefix(
+                fn, SRCDIR "/writerfilter/source/dmapper/DomainMapperTableHandler.cxx"))
+            return false;
+        return true;
+    }
+
+    virtual void run() override
+    {
+        if (preRun())
+            TraverseDecl(compiler.getASTContext().getTranslationUnitDecl());
+    }
+
+    bool VisitCXXMemberCallExpr(const CXXMemberCallExpr*);
+    bool VisitCXXConstructExpr(const CXXConstructExpr*);
+
+private:
+    bool isInterestingType(QualType);
+};
+
+bool MoveIt::VisitCXXMemberCallExpr(const CXXMemberCallExpr* topExpr)
+{
+    if (ignoreLocation(topExpr))
+        return true;
+    const CXXMethodDecl* methodDecl = topExpr->getMethodDecl();
+    if (!methodDecl)
+        return true;
+
+    unsigned len = std::min(topExpr->getNumArgs(), methodDecl->getNumParams());
+    for (unsigned i = 0; i < len; ++i)
+    {
+        // check if the parameter is a moveable type
+        const ParmVarDecl* parmVarDecl = methodDecl->getParamDecl(i);
+        if (!parmVarDecl->getType()->isRecordType())
+            continue;
+        const CXXRecordDecl* recordDecl
+            = dyn_cast<CXXRecordDecl>(parmVarDecl->getType()->getAsRecordDecl());
+        if (!recordDecl || !recordDecl->hasMoveConstructor() || recordDecl->isTriviallyCopyable())
+            continue;
+        if (!isInterestingType(parmVarDecl->getType()))
+            continue;
+
+        // check if (a) we're making a copy to pass to the param and (b) we're making a copy of a local var
+        const Expr* argExpr = topExpr->getArg(i);
+        if (!argExpr)
+            continue;
+        const CXXConstructExpr* argSubExpr = dyn_cast<CXXConstructExpr>(argExpr->IgnoreImplicit());
+        if (!argSubExpr || argSubExpr->getNumArgs() == 0)
+            continue;
+        const DeclRefExpr* dre = dyn_cast<DeclRefExpr>(argSubExpr->getArg(0)->IgnoreImplicit());
+        if (!dre)
+            continue;
+        const VarDecl* localVarDecl = dyn_cast<VarDecl>(dre->getDecl());
+        if (!localVarDecl || localVarDecl->getType()->isReferenceType()
+            || localVarDecl->getType()->isPointerType() || !localVarDecl->hasLocalStorage())
+            continue;
+        // because sometimes the parameter type is some obscured STL thing
+        if (!isInterestingType(localVarDecl->getType()))
+            continue;
+
+        report(DiagnosticsEngine::Warning, "can std::move this var into this param",
+               argExpr->getBeginLoc());
+        report(DiagnosticsEngine::Note, "passing to this param", parmVarDecl->getBeginLoc());
+        report(DiagnosticsEngine::Note, "local var declared here", localVarDecl->getBeginLoc());
+        report(DiagnosticsEngine::Note, "type declared here", recordDecl->getBeginLoc());
+        //        parmVarDecl->getType()->dump();
+    }
+
+    //    StringRef aFileName = getFilenameOfLocation(
+    //        compiler.getSourceManager().getSpellingLoc(parmVarDecl->getBeginLoc()));
+    //    if (loplugin::hasPathnamePrefix(aFileName,
+    //                                    SRCDIR "/svx/source/sidebar/line/LineWidthValueSet.cxx"))
+    //        return true;
+
+    return true;
+}
+
+bool MoveIt::VisitCXXConstructExpr(const CXXConstructExpr* topExpr)
+{
+    if (ignoreLocation(topExpr))
+        return true;
+    if (isa<CXXTemporaryObjectExpr>(topExpr))
+        return true;
+    const CXXConstructorDecl* methodDecl = topExpr->getConstructor();
+    if (!methodDecl)
+        return true;
+
+    unsigned len = std::min(topExpr->getNumArgs(), methodDecl->getNumParams());
+    for (unsigned i = 0; i < len; ++i)
+    {
+        // check if the parameter is a moveable type
+        const ParmVarDecl* parmVarDecl = methodDecl->getParamDecl(i);
+        if (!parmVarDecl->getType()->isRecordType())
+            continue;
+        const CXXRecordDecl* recordDecl
+            = dyn_cast<CXXRecordDecl>(parmVarDecl->getType()->getAsRecordDecl());
+        if (!recordDecl || !recordDecl->hasMoveConstructor() || recordDecl->isTriviallyCopyable())
+            continue;
+        if (!isInterestingType(parmVarDecl->getType()))
+            continue;
+
+        // check if (a) we're making a copy to pass to the param and (b) we're making a copy of a local var
+        const Expr* argExpr = topExpr->getArg(i);
+        if (!argExpr)
+            continue;
+        const CXXConstructExpr* argSubExpr = dyn_cast<CXXConstructExpr>(argExpr->IgnoreImplicit());
+        if (!argSubExpr || argSubExpr->getNumArgs() == 0)
+            continue;
+        const DeclRefExpr* dre = dyn_cast<DeclRefExpr>(argSubExpr->getArg(0)->IgnoreImplicit());
+        if (!dre)
+            continue;
+        const VarDecl* localVarDecl = dyn_cast<VarDecl>(dre->getDecl());
+        if (!localVarDecl || localVarDecl->getType()->isReferenceType()
+            || localVarDecl->getType()->isPointerType() || !localVarDecl->hasLocalStorage())
+            continue;
+        // because sometimes the parameter type is some obscured STL thing
+        if (!isInterestingType(localVarDecl->getType()))
+            continue;
+
+        report(DiagnosticsEngine::Warning, "can std::move this var into this param",
+               argExpr->getBeginLoc());
+        report(DiagnosticsEngine::Note, "passing to this param", parmVarDecl->getBeginLoc());
+        report(DiagnosticsEngine::Note, "local var declared here", localVarDecl->getBeginLoc());
+        report(DiagnosticsEngine::Note, "type declared here", recordDecl->getBeginLoc());
+        //        parmVarDecl->getType()->dump();
+    }
+
+    //    StringRef aFileName = getFilenameOfLocation(
+    //        compiler.getSourceManager().getSpellingLoc(parmVarDecl->getBeginLoc()));
+    //    if (loplugin::hasPathnamePrefix(aFileName,
+    //                                    SRCDIR "/svx/source/sidebar/line/LineWidthValueSet.cxx"))
+    //        return true;
+
+    return true;
+}
+
+/// Exclude boring types, so that we don't generate too many low-value conversions.
+/// e.g. for now I ignore ref-counted types like Sequence and OUString and css::uno::Reference,
+/// because that generates too many changes
+bool MoveIt::isInterestingType(QualType qt)
+{
+    if (qt->isEnumeralType())
+        return false;
+    if (!qt->isRecordType())
+        return false;
+
+    auto tc = loplugin::TypeCheck(qt);
+
+    // clang-format off
+    return !tc.ClassOrStruct("iterator")
+           && !tc.ClassOrStruct("const_iterator")
+           && !tc.Typedef("iterator")
+           && !tc.Typedef("const_iterator")
+           && !tc.Class("_Safe_iterator")
+           && !tc.Typedef("string")
+           && !tc.ClassOrStruct("shared_ptr").StdNamespace()
+           && !tc.ClassOrStruct("shared_ptr").Namespace("boost")
+           && !tc.Class("B2DHomMatrix").Namespace("basegfx").GlobalNamespace()
+           && !tc.Class("Pipe").Namespace("osl")
+           && !tc.Class("Any").Namespace("uno")
+           && !tc.Class("TypeDescription").Namespace("uno")
+           && !tc.Class("UnoInterfaceReference").Namespace("uno")
+           && !tc.Class("ByteSequence").Namespace("rtl").GlobalNamespace()
+           && !tc.Class("OUString").Namespace("rtl").GlobalNamespace()
+           && !tc.Class("OString").Namespace("rtl").GlobalNamespace()
+           && !tc.Class("BinaryAny")
+           && !tc.Class("Reference")
+           && !tc.Class("SvRef").Namespace("tools").GlobalNamespace()
+           && !tc.ClassOrStruct("sk_sp") // skia shared pointer
+           && !tc.ClassOrStruct("VclPtr")
+           && !tc.Typedef("IterString") // SalInstanceTreeView::IterString
+           && !tc.Typedef("svtree_render_args")
+           && !tc.Typedef("render_args") // weld::ComboBox::render_args
+           ;
+    // clang-format on
+}
+
+/// off by default because each warning needs to be hand checked to ensure it is not a false+
+loplugin::Plugin::Registration<MoveIt> moveit("moveit", false);
+
+} // namespace
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

compilerplugins/clang/test/moveit.cxx
@@ -1,0 +1,60 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "config_clang.h"
+#include <memory>
+
+// expected-note@+3 {{type declared here [loplugin:moveit]}}
+// expected-note@+2 {{type declared here [loplugin:moveit]}}
+// expected-note@+1 {{type declared here [loplugin:moveit]}}
+struct Movable
+{
+    std::shared_ptr<int> x;
+};
+
+namespace test1
+{
+struct F
+{
+    // expected-note@+1 {{passing to this param [loplugin:moveit]}}
+    void call_by_value(Movable);
+    // expected-note@+1 {{passing to this param [loplugin:moveit]}}
+    F(Movable);
+    void foo()
+    {
+        // expected-note@+2 {{local var declared here [loplugin:moveit]}}
+        // expected-note@+1 {{local var declared here [loplugin:moveit]}}
+        Movable m;
+        // expected-error@+1 {{can std::move this var into this param [loplugin:moveit]}}
+        call_by_value(m);
+        // expected-error@+1 {{can std::move this var into this param [loplugin:moveit]}}
+        F a(m);
+        (void)a;
+    }
+};
+}
+
+namespace test2
+{
+struct F
+{
+    // expected-note@+1 {{passing to this param [loplugin:moveit]}}
+    F(Movable);
+    void foo()
+    {
+        // expected-note@+1 {{local var declared here [loplugin:moveit]}}
+        Movable m;
+        // expected-error@+1 {{can std::move this var into this param [loplugin:moveit]}}
+        F a(m);
+        (void)a;
+    }
+};
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */

cui/source/dialogs/SpellDialog.cxx
@@ -1808,7 +1808,7 @@ void SentenceEditWindow_Impl::SetAlternatives( const Reference< XSpellAlternativ
         aLocale = xAlt->getLocale();
         aAlts   = xAlt->getAlternatives();
     }
-    SpellErrorDescription aDesc( false, aWord, aLocale, aAlts, nullptr);
+    SpellErrorDescription aDesc( false, aWord, std::move(aLocale), aAlts, nullptr);
     SfxGrabBagItem aSpellErrorDescription(EE_CHAR_GRABBAG);
     aSpellErrorDescription.GetGrabBag()["SpellErrorDescription"] <<= aDesc.toSequence();
     SetAttrib(aSpellErrorDescription, m_nErrorStart, m_nErrorEnd);

cui/source/options/optasian.cxx
@@ -320,7 +320,7 @@ IMPL_LINK_NOARG(SvxAsianLayoutPage, LanguageHdl, weld::ComboBox&, void)
     }
     if(!bAvail)
     {
-        LocaleDataWrapper aWrap( aLanguageTag );
+        LocaleDataWrapper aWrap( std::move(aLanguageTag) );
         ForbiddenCharacters aForbidden = aWrap.getForbiddenCharacters();
         sStart = aForbidden.beginLine;
         sEnd = aForbidden.endLine;

cui/source/options/optgdlg.cxx
@@ -1680,8 +1680,7 @@ IMPL_LINK_NOARG(OfaLanguagesTabPage, LocaleSettingHdl, weld::ComboBox&, void)
     m_xCurrencyLB->set_active_text(aDefaultCurr);
 
     // obtain corresponding locale data
-    LanguageTag aLanguageTag( eLang);
-    LocaleDataWrapper aLocaleWrapper( aLanguageTag );
+    LocaleDataWrapper aLocaleWrapper(( LanguageTag(eLang) ));
 
     // update the decimal separator key of the related CheckBox
     OUString sTempLabel(m_sDecimalSeparatorLabel);

commit 8ec56c5ab7ff81d271820ddfa66f27fbc950dbe6

Fix context menu on image which doesnt display
- Launch Writer
- Insert any image in it
- Right click on it
=> Nothing displays
+ console shows:
warn:legacy.osl:67182:67182:vcl/source/window/menu.cxx:2750: PopupMenu::Execute: need a non-NULL window!

Regression from f71606c920a3f78294da745cd9ef1eacde010224
new loplugin:moveit

Change-Id: I89f5bfe2a3cd6a935b419d55c2f3e884c7eed2c7

sfx2/source/control/dispatch.cxx
@@ -1850,7 +1850,7 @@ void SfxDispatcher::ExecutePopup( const OUString& rResName, vcl::Window* pWin, c
     else
     {
         OUString aMenuURL = "private:resource/popupmenu/" + rResName;
-        if (GetFrame()->GetViewShell()->TryContextMenuInterception(xPopupMenu, aMenuURL, std::move(aEvent)))
+        if (GetFrame()->GetViewShell()->TryContextMenuInterception(xPopupMenu, aMenuURL, aEvent))
         {
             css::uno::Reference<css::awt::XWindowPeer> xParent(aEvent.SourceWindow, css::uno::UNO_QUERY);
             xPopupMenu->execute(xParent, css::awt::Rectangle(aPos.X(), aPos.Y(), 1, 1), css::awt::PopupMenuDirection::EXECUTE_DOWN);