cd67e988f27de819a39e75e5243b9b20ba56faab
Attila Bakos (NISZ) <bakos.attilakaroly@nisz.hu>
2022-06-14 10:38:46 +0200

    tdf#149550 sw: fix crash by implementing nested textbox copy
    Grouped shapes with a nested textbox were copied without
    the textbox with frequent crashing.
    
    Regression from commit 2951cbdf3a6e2b62461665546b47e1d253fcb834
    "tdf#143574 OOXML export/import of textboxes in group shapes".
    
    Change-Id: Ie2cc24f10706d8999026dc92ebad21f2c5673003
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/135815
    Tested-by: László Németh <nemeth@numbertext.org>
    Reviewed-by: László Németh <nemeth@numbertext.org>

--- sw/source/core/doc/DocumentLayoutManager.cxx
+++ sw/source/core/doc/DocumentLayoutManager.cxx
@@ -463,67 +463,6 @@ SwFrameFormat *DocumentLayoutManager::CopyLayoutFormat(
     if( bMakeFrames )
         pDest->MakeFrames();
 
-    // If the draw format has a TextBox, then copy its fly format as well.
-    if (rSource.Which() == RES_DRAWFRMFMT && rSource.GetOtherTextBoxFormats())
-    {
-        auto pObj = rSource.FindRealSdrObject();
-        auto pTextBoxNd = std::make_shared<SwTextBoxNode>(SwTextBoxNode(pDest));
-        pDest->SetOtherTextBoxFormats(pTextBoxNd);
-
-        if (pObj)
-        {
-            const bool bIsGroupObj = pObj->getChildrenOfSdrObject();
-            for (size_t it = 0;
-                 it < (bIsGroupObj ? pObj->getChildrenOfSdrObject()->GetObjCount() : 1); it++)
-            {
-                auto pChild = bIsGroupObj ? pObj->getChildrenOfSdrObject()->GetObj(it)
-                                          : const_cast<SdrObject*>(pObj);
-                if (auto pSourceTextBox
-                    = SwTextBoxHelper::getOtherTextBoxFormat(&rSource, RES_DRAWFRMFMT, pChild))
-                {
-                    SwFormatAnchor boxAnchor(rNewAnchor);
-                    if (RndStdIds::FLY_AS_CHAR == boxAnchor.GetAnchorId())
-                    {
-                        // AS_CHAR *must not* be set on textbox fly-frame
-                        boxAnchor.SetType(RndStdIds::FLY_AT_CHAR);
-                    }
-                    // presumably these anchors are supported though not sure
-                    assert(RndStdIds::FLY_AT_CHAR == boxAnchor.GetAnchorId()
-                           || RndStdIds::FLY_AT_PARA == boxAnchor.GetAnchorId()
-                           || boxAnchor.GetAnchorId() == RndStdIds::FLY_AT_PAGE);
-
-                    if (!bMakeFrames && rNewAnchor.GetAnchorId() == RndStdIds::FLY_AS_CHAR)
-                    {
-                        // If the draw format is as-char, then it will be copied with bMakeFrames=false, but
-                        // doing the same for the fly format would result in not making fly frames at all.
-                        bMakeFrames = true;
-                    }
-                    SwFrameFormat* pDestTextBox
-                        = CopyLayoutFormat(*pSourceTextBox, boxAnchor, bSetTextFlyAtt, bMakeFrames);
-
-                    SwAttrSet aSet(pDest->GetAttrSet());
-                    SwFormatContent aContent(
-                        pDestTextBox->GetContent().GetContentIdx()->GetNode().GetStartNode());
-                    aSet.Put(aContent);
-                    pDest->SetFormatAttr(aSet);
-
-                    // Link FLY and DRAW formats, so it becomes a text box
-                    SdrObject* pNewObj = pDest->FindRealSdrObject();
-                    if (bIsGroupObj && pNewObj
-                        && pNewObj->getChildrenOfSdrObject()
-                        && (pNewObj->getChildrenOfSdrObject()->GetObjCount() > it)
-                        && pNewObj->getChildrenOfSdrObject()->GetObj(it))
-                        pNewObj = pNewObj->getChildrenOfSdrObject()->GetObj(it);
-                    pTextBoxNd->AddTextBox(pNewObj, pDestTextBox);
-                    pDestTextBox->SetOtherTextBoxFormats(pTextBoxNd);
-                }
-
-                if (!bIsGroupObj)
-                    break;
-            }
-        }
-    }
-
     if (pDest->GetName().isEmpty())
     {
         // Format name should have unique name. Let's use object name as a fallback
@@ -531,6 +470,10 @@ SwFrameFormat *DocumentLayoutManager::CopyLayoutFormat(
         if (pObj)
             pDest->SetName(pObj->GetName());
     }
+
+    // If the draw format has a TextBox, then copy its fly format as well.
+    if (const auto& pTextBoxes = rSource.GetOtherTextBoxFormats())
+        pTextBoxes->Clone(&m_rDoc, rNewAnchor, pDest, bSetTextFlyAtt, bMakeFrames);
 
     return pDest;
 }
