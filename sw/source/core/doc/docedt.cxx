768721f297473ac7110d657bfadfeaf8584f1b20
Michael Stahl <michael.stahl@allotropia.de>
2022-06-15 15:06:59 +0200

    tdf#135976 sw: preserve flys on backspace/delete with redlining enabled
    This is a continuation of commit 85376a02348810812d515ee72140dbf56f2b6040
    for the case when redlining is turned on.
    
    Also try to restore the anchors in SwUndoRedlineDelete.
    
    (regression from commit 3345feb67f2c49a1b76639965b56968e1c5f03ee)
    
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/135909
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
    (cherry picked from commit 932a8efce878547bfd81521d0cf1ddfe8dc33ec6)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/135968
    Reviewed-by: Thorsten Behrens <thorsten.behrens@allotropia.de>
    (cherry picked from commit 03834f003c56e6646e3b274c418acd4fc344fd8e)
    
    sw: fix odd m_bCanGroup check in SwUndoRedlineDelete
    
    This looks like copypasta, presumably both flags must be true to allow
    grouping.
    
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/135908
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
    (cherry picked from commit f31f11f3222933dbc96dc672e6fa52233cda12be)
    
    Change-Id: I4199f5755398d469a606618c037ad9756cb7aeba

--- sw/source/core/doc/docedt.cxx
+++ sw/source/core/doc/docedt.cxx
@@ -28,6 +28,7 @@
 #include <mdiexp.hxx>
 #include <mvsave.hxx>
 #include <redline.hxx>
+#include <rolbck.hxx>
 #include <rootfrm.hxx>
 #include <splargs.hxx>
 #include <swcrsr.hxx>
@@ -130,7 +131,7 @@ void SaveFlyInRange( const SwNodeRange& rRg, SaveFlyArr& rArr )
 }
 
 void SaveFlyInRange( const SwPaM& rPam, const SwPosition& rInsPos,
-                       SaveFlyArr& rArr, bool bMoveAllFlys )
+        SaveFlyArr& rArr, bool bMoveAllFlys, SwHistory *const pHistory)
 {
     SwFrameFormats& rFormats = *rPam.GetPoint()->nNode.GetNode().GetDoc()->GetSpzFrameFormats();
     SwFrameFormat* pFormat;
@@ -176,6 +177,10 @@ void SaveFlyInRange( const SwPaM& rPam, const SwPosition& rInsPos,
                     || (RndStdIds::FLY_AT_CHAR == pAnchor->GetAnchorId()
                             && (bInsPos = (rInsPos == *pAPos))))
             {
+                if (pHistory)
+                {
+                    pHistory->AddChangeFlyAnchor(*pFormat);
+                }
                 SaveFly aSave( pAPos->nNode.GetIndex() - rSttNdIdx.GetIndex(),
                     (RndStdIds::FLY_AT_CHAR == pAnchor->GetAnchorId())
                         ? (pAPos->nNode == rSttNdIdx)
