8dc4691a55ca43adffa8b70f0a248154bb68635f
Luboš Luňák <l.lunak@collabora.com>
2022-06-17 10:38:03 +0200

    upstream Skia fix for Vulkan crash on texture binding (tdf#148624)
    Change-Id: Ic16a516bfde04aba0336baca58f605d6cf9fd413
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/136011
    Tested-by: Jenkins
    Reviewed-by: Luboš Luňák <l.lunak@collabora.com>
    (cherry picked from commit dce3cf219479c353f332b99a51aaa20843530100)

--- external/skia/UnpackedTarball_skia.mk
+++ external/skia/UnpackedTarball_skia.mk
@@ -37,6 +37,7 @@ skia_patches := \
     fix-graphite-ifdef.patch.1 \
     allow-no-es2restrictions.patch.1 \
     vk_mem_alloc.patch.1 \
+    tdf148624.patch.1 \
 
 $(eval $(call gb_UnpackedTarball_set_patchlevel,skia,1))
 
