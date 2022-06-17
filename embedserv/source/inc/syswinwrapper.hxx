b316aa19e20b496fa9d87b4d6656523f90d99410
leighd2008 <leighd2008@gmail.com>
2022-06-17 13:03:14 -0400

    tdf#143148 Replaces include guards with pragma once
    Change-Id: I006bbcea9ea69ffcf7c789691583430c9154af2e

--- embedserv/source/inc/syswinwrapper.hxx
+++ embedserv/source/inc/syswinwrapper.hxx
@@ -17,11 +17,10 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_EMBEDSERV_SOURCE_INC_SYSWINWRAPPER_HXX
-#define INCLUDED_EMBEDSERV_SOURCE_INC_SYSWINWRAPPER_HXX
+#pragma once
 
 #if !defined WIN32_LEAN_AND_MEAN
-# define WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
 #endif
 #include <windows.h>
 
@@ -29,148 +28,142 @@
  ** CWindow:  Our basic window class.
  **/
 
-
 class DocumentHolder;
 
+namespace winwrap
+{
 
-namespace winwrap {
+void TransformRect(LPRECT rect, HWND pWnd, HWND pWndClipTo);
 
+LRESULT APIENTRY HatchWndProc(HWND hWnd, UINT iMsg, WPARAM wParam, LPARAM lParam);
 
-    void TransformRect(LPRECT rect,HWND pWnd,HWND pWndClipTo);
+BOOL HatchWindowRegister(HINSTANCE hInst);
 
+class CWindow
+{
+protected:
+    HINSTANCE m_hInst; //Task instance
+    HWND m_hWnd; //Window handle of the window
 
-    LRESULT APIENTRY HatchWndProc(
-        HWND hWnd, UINT iMsg
-        , WPARAM wParam, LPARAM lParam);
+public:
+    //Standard Class Functions
+    CWindow(HINSTANCE);
+    ~CWindow();
 
+    //Just returns members.  No need to modify
+    HWND Window();
+    HINSTANCE Instance();
+};
 
-    BOOL HatchWindowRegister(HINSTANCE hInst);
+class Tracker
+{
+public:
+    // Constructors
+    Tracker();
+    Tracker(LPCRECT lpSrcRect, UINT nStyle);
 
-    class CWindow
+    // Style Flags
+    enum StyleFlags
     {
-    protected:
-        HINSTANCE   m_hInst;            //Task instance
-        HWND        m_hWnd;             //Window handle of the window
-
-    public:
-        //Standard Class Functions
-        CWindow(HINSTANCE);
-        ~CWindow();
-
-        //Just returns members.  No need to modify
-        HWND        Window();
-        HINSTANCE   Instance();
+        solidLine = 1,
+        dottedLine = 2,
+        hatchedBorder = 4,
+        resizeInside = 8,
+        resizeOutside = 16,
+        hatchInside = 32,
     };
 
+    // Hit-Test codes
+    enum TrackerHit
+    {
+        hitNothing = -1,
+        hitTopLeft = 0,
+        hitTopRight = 1,
+        hitBottomRight = 2,
+        hitBottomLeft = 3,
+        hitTop = 4,
+        hitRight = 5,
+        hitBottom = 6,
+        hitLeft = 7,
+        hitMiddle = 8
+    };
 
-    class Tracker {
-    public:
-        // Constructors
-        Tracker();
-        Tracker(LPCRECT lpSrcRect, UINT nStyle);
+    // Attributes
+    UINT m_nStyle; // current state
+    RECT m_rect; // current position (always in pixels)
+    SIZE m_sizeMin; // minimum X and Y size during track operation
+    int m_nHandleSize; // size of resize handles (default from WIN.INI)
 
-        // Style Flags
-        enum StyleFlags
-        {
-            solidLine = 1, dottedLine = 2, hatchedBorder = 4,
-            resizeInside = 8, resizeOutside = 16, hatchInside = 32,
-        };
+    // Operations
+    void Draw(HDC hDC) const;
+    void GetTrueRect(LPRECT lpTrueRect) const;
+    BOOL SetCursor(HWND hWnd, UINT nHitTest) const;
+    BOOL Track(HWND hWnd, POINT point, BOOL bAllowInvert = FALSE, HWND hWndClipTo = nullptr);
+    //         BOOL TrackRubberBand(HWND hWnd,POINT point,BOOL bAllowInvert = TRUE);
+    int HitTest(POINT point) const;
+    int NormalizeHit(int nHandle) const;
 
-        // Hit-Test codes
-        enum TrackerHit
-        {
-            hitNothing = -1,
-            hitTopLeft = 0, hitTopRight = 1,
-            hitBottomRight = 2, hitBottomLeft = 3,
-            hitTop = 4, hitRight = 5, hitBottom = 6,
-            hitLeft = 7, hitMiddle = 8
-        };
+    // Overridables
+    virtual void DrawTrackerRect(LPRECT lpRect, HWND hWndClipTo, HDC hDC, HWND hWnd);
+    virtual void AdjustRect(int nHandle, LPRECT lpRect);
+    virtual void OnChangedRect(const RECT& rectOld);
+    virtual UINT GetHandleMask() const;
 
-        // Attributes
-        UINT m_nStyle;      // current state
-        RECT m_rect;       // current position (always in pixels)
-        SIZE m_sizeMin;    // minimum X and Y size during track operation
-        int m_nHandleSize;  // size of resize handles (default from WIN.INI)
-
-        // Operations
-        void Draw(HDC hDC) const;
-        void GetTrueRect(LPRECT lpTrueRect) const;
-        BOOL SetCursor(HWND hWnd,UINT nHitTest) const;
-        BOOL Track(HWND hWnd,POINT point,BOOL bAllowInvert = FALSE,
-                   HWND hWndClipTo = nullptr);
-//         BOOL TrackRubberBand(HWND hWnd,POINT point,BOOL bAllowInvert = TRUE);
-        int HitTest(POINT point) const;
-        int NormalizeHit(int nHandle) const;
-
-        // Overridables
-        virtual void DrawTrackerRect(
-            LPRECT lpRect, HWND hWndClipTo,
-            HDC hDC, HWND hWnd);
-        virtual void AdjustRect(int nHandle, LPRECT lpRect);
-        virtual void OnChangedRect(const RECT& rectOld);
-        virtual UINT GetHandleMask() const;
-
-// Implementation
-    public:
-        virtual ~Tracker();
+    // Implementation
+public:
+    virtual ~Tracker();
 
 protected:
-        BOOL m_bAllowInvert; // flag passed to Track or TrackRubberBand
-        RECT m_rectLast;
-        SIZE m_sizeLast;
-        BOOL m_bErase;       // TRUE if DrawTrackerRect is called for erasing
-        BOOL m_bFinalErase;  // TRUE if DragTrackerRect called for final erase
+    BOOL m_bAllowInvert; // flag passed to Track or TrackRubberBand
+    RECT m_rectLast;
+    SIZE m_sizeLast;
+    BOOL m_bErase; // TRUE if DrawTrackerRect is called for erasing
+    BOOL m_bFinalErase; // TRUE if DragTrackerRect called for final erase
 
-        // implementation helpers
-        int HitTestHandles(POINT point) const;
-        void GetHandleRect(int nHandle,RECT* pHandleRect) const;
-        void GetModifyPointers(
-            int nHandle,int**ppx, int**ppy, int* px, int*py);
-        virtual int GetHandleSize(LPRECT lpRect = nullptr) const;
-        BOOL TrackHandle(int nHandle,HWND hWnd,POINT point,HWND hWndClipTo);
-        void Construct();
-    };
-
+    // implementation helpers
+    int HitTestHandles(POINT point) const;
+    void GetHandleRect(int nHandle, RECT* pHandleRect) const;
+    void GetModifyPointers(int nHandle, int** ppx, int** ppy, int* px, int* py);
+    virtual int GetHandleSize(LPRECT lpRect = nullptr) const;
+    BOOL TrackHandle(int nHandle, HWND hWnd, POINT point, HWND hWndClipTo);
+    void Construct();
+};
 
 //Width of the border
-#define HATCHWIN_BORDERWIDTHDEFAULT     4
+#define HATCHWIN_BORDERWIDTHDEFAULT 4
 
+class CHatchWin : public CWindow
+{
+    friend LRESULT APIENTRY HatchWndProc(HWND, UINT, WPARAM, LPARAM);
 
-    class CHatchWin : public CWindow
-    {
-        friend LRESULT APIENTRY HatchWndProc(HWND, UINT, WPARAM, LPARAM);
+public:
+    const DocumentHolder* m_pDocHolder;
+    Tracker m_aTracker;
 
-    public:
+    int m_dBorder;
+    int m_dBorderOrg;
+    WORD m_uID;
+    HWND m_hWndParent;
+    HWND m_hWndKid;
+    HWND m_hWndAssociate;
+    RECT m_rcPos;
+    RECT m_rcClip;
 
-        const DocumentHolder* m_pDocHolder;
-        Tracker               m_aTracker;
+public:
+    CHatchWin(HINSTANCE, const DocumentHolder*);
+    ~CHatchWin();
 
-        int         m_dBorder;
-        int         m_dBorderOrg;
-        WORD        m_uID;
-        HWND        m_hWndParent;
-        HWND        m_hWndKid;
-        HWND        m_hWndAssociate;
-        RECT        m_rcPos;
-        RECT        m_rcClip;
+    BOOL Init(HWND, WORD, HWND);
 
-    public:
-        CHatchWin(HINSTANCE,const DocumentHolder*);
-        ~CHatchWin();
+    HWND HwndAssociateSet(HWND);
+    HWND HwndAssociateGet();
 
-        BOOL        Init(HWND, WORD, HWND);
-
-        HWND        HwndAssociateSet(HWND);
-        HWND        HwndAssociateGet();
-
-        void        RectsSet(LPRECT, LPRECT);
-        void        ChildSet(HWND);
-        void        ShowHatch(BOOL);
-        void        SetTrans();
-    };
+    void RectsSet(LPRECT, LPRECT);
+    void ChildSet(HWND);
+    void ShowHatch(BOOL);
+    void SetTrans();
+};
 
 }
-
-#endif // INCLUDED_EMBEDSERV_SOURCE_INC_SYSWINWRAPPER_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
