f2cd187ccb78293e54a8f83ec271821a7755bedf
offtkp <parisoplop@gmail.com>
2022-06-19 23:52:58 +0300

    tdf#149449 Don't ignore CSS class redefinition inside SVGs
    Previously if a css class was redefined like so:
    
    .cls2,.cls3{fill:#fff;}.cls-2{opacity:0.1;}
    
    the second definition of .cls-2 would get ignored and opacity would
    remain 1.
    
    This patch keeps track of the names of each previously defined class and
    makes sure to append the future redefinition instead of ignoring it.
    
    Change-Id: I20b55aea247d11774cd743505a90f1466f622b1e

--- svgio/inc/svgstylenode.hxx
+++ svgio/inc/svgstylenode.hxx
@@ -19,6 +19,8 @@
 
 #pragma once
 
+#include <unordered_map>
+
 #include "svgnode.hxx"
 #include "svgstyleattributes.hxx"
 
@@ -28,7 +30,7 @@ namespace svgio::svgreader
         {
         private:
             /// use styles
-            std::vector< SvgStyleAttributes* >      maSvgStyleAttributes;
+            std::unordered_map< OUString, std::unique_ptr<SvgStyleAttributes> > maSvgStyleAttributes;
 
             bool                                    mbTextCss : 1; // true == type is 'text/css'
 
@@ -36,7 +38,6 @@ namespace svgio::svgreader
             SvgStyleNode(
                 SvgDocument& rDocument,
                 SvgNode* pParent);
-            virtual ~SvgStyleNode() override;
 
             /// #i125258# tell if this node is allowed to have a parent style (e.g. defs do not)
             virtual bool supportsParentStyle() const override;
