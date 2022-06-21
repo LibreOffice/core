5c369f2010693466b9de4ba078fde0fe7524e53f
Caolán McNamara <caolanm@redhat.com>
2022-06-21 10:38:30 +0100

    forcepoint#107: ensure to be deleted formulas are unshared first
    Change-Id: I32cee0a22616e96d85c59d76319a6f6074fcde8b
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/136228
    Tested-by: Jenkins
    Tested-by: Caolán McNamara <caolanm@redhat.com>
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    (cherry picked from commit 31c05349320aceb056d61e4ecd06b18fc31509bb)

--- sc/qa/unit/filters-test.cxx
+++ sc/qa/unit/filters-test.cxx
@@ -82,6 +82,7 @@ public:
     void testSortWithSheetExternalReferencesODS_Impl( ScDocShellRef const & xDocShRef, SCROW nRow1, SCROW nRow2,
             bool bCheckRelativeInSheet );
     void testSortWithFormattingXLS();
+    void testForcepoint107();
 
     CPPUNIT_TEST_SUITE(ScFiltersTest);
     CPPUNIT_TEST(testCVEs);
@@ -108,6 +109,7 @@ public:
     CPPUNIT_TEST(testSortWithSharedFormulasODS);
     CPPUNIT_TEST(testSortWithSheetExternalReferencesODS);
     CPPUNIT_TEST(testSortWithFormattingXLS);
+    CPPUNIT_TEST(testForcepoint107);
 
     CPPUNIT_TEST_SUITE_END();
 
@@ -867,6 +869,13 @@ void ScFiltersTest::testSortWithFormattingXLS()
     bool bSorted = aFunc.Sort(0, aSortData, true, true, true);
     CPPUNIT_ASSERT(bSorted);
     xDocSh->DoClose();
+}
+
+// just needs to not crash on recalc
+void ScFiltersTest::testForcepoint107()
+{
+    ScDocShellRef xDocSh = loadDoc(u"forcepoint107.", FORMAT_XLSX, true);
+    xDocSh->DoHardRecalc();
 }
 
 ScFiltersTest::ScFiltersTest()
