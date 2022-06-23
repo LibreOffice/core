dd29746798552c1f71e5cb5fb4893612175eba34
Andreas Heinisch <andreas.heinisch@yahoo.de>
2022-06-23 15:07:18 +0200

    tdf#147529 - BASIC MsgBox: Check for missing optional parameters
    Change-Id: Iecb47e0005c609ee1117d6fb141e810c0166806a
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/136339
    Tested-by: Jenkins
    Reviewed-by: Andreas Heinisch <andreas.heinisch@yahoo.de>
    (cherry picked from commit 522f5ad4d8c92624fffa869143aa869662a48589)

---
+++ basic/qa/vba_tests/tdf147529_optional_parameters_msgbox.vb
@@ -0,0 +1,39 @@
+'
+' This file is part of the LibreOffice project.
+'
+' This Source Code Form is subject to the terms of the Mozilla Public
+' License, v. 2.0. If a copy of the MPL was not distributed with this
+' file, You can obtain one at http://mozilla.org/MPL/2.0/.
+'
+
+Option VBASupport 1
+Option Explicit
+
+Function doUnitTest() As String
+    TestUtil.TestInit
+    verify_testOptionalParametersMsgBox
+    doUnitTest = TestUtil.GetResult()
+End Function
+
+Sub verify_testOptionalParametersMsgBox
+    On Error GoTo errorHandler
+
+    ' tdf#147529 - check for missing optional parameters
+    TestUtil.AssertEqual(TestOptionalParametersMsgBox(),          True, "TestOptionalParametersMsgBox()")
+    TestUtil.AssertEqual(TestOptionalParametersMsgBox("test"),    True, "TestOptionalParametersMsgBox(""test"")")
+    TestUtil.AssertEqual(TestOptionalParametersMsgBox("test", 1), True, "TestOptionalParametersMsgBox(""test"", 1)")
+
+    Exit Sub
+errorHandler:
+    TestUtil.ReportErrorHandler("verify_testOptionalParametersMsgBox", Err, Error$, Erl)
+End Sub
+
+Function TestOptionalParametersMsgBox(Optional text, Optional dialogType, Optional dialogTitle) As Boolean
+On Error GoTo errorHandler
+    MsgBox text, dialogType, dialogTitle
+    TestOptionalParametersMsgBox = False
+    Exit Function
+errorHandler:
+    TestUtil.AssertEqual(Err, 449, "Argument not optional (Error 449)")
+    TestOptionalParametersMsgBox = True
+End Function
