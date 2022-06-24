9f26b58d859f2b943b91d47f5059ca50b5c0cb0c
Luboš Luňák <l.lunak@collabora.com>
2022-06-24 13:52:46 +0200

    fix ByValue lookups with ScSortedRangeCache
    My fix for tdf#149071 actually disabled the optimization for all
    ByValue lookups, because in fact all such lookups have maString set.
    So lookups where the cells are a mix of numeric and string values
    need different handling. A simple solution is detecting such a mix
    when collecting the values for ScSortedRangeCache and disabling
    the optimization in such a case. But it turns out that queries
    containing such a mix are not that rare, as documents may e.g.
    do COUNTIF($C:$C) where the given column has numeric values that
    start with a textual header. So bail out only if the string cell
    actually could affect the numeric query.
    Also fix ScSortedRangeCache usage depending on query parameters,
    different instances are needed for e.g. different ScQueryOp,
    because the ScQueryEvaluator functions may return different
    results (isQueryByString() is automatically true for SC_EQUAL).
    
    Change-Id: Ib4565cbf6194e7c525c4d10d00b1c31707952a79

--- sc/inc/rangecache.hxx
+++ sc/inc/rangecache.hxx
@@ -47,11 +47,13 @@ public:
     ScSortedRangeCache(ScDocument* pDoc, const ScRange& rRange, const ScQueryParam& param,
                        ScInterpreterContext* context);
 
+    /// Returns if the cache is usable.
+    bool isValid() const { return mValid; }
+
     /// Remove from document structure and delete (!) cache on modify hint.
     virtual void Notify(const SfxHint& rHint) override;
 
     const ScRange& getRange() const { return maRange; }
-    bool isDescending() const { return mDescending; }
 
     enum class ValueType
     {
@@ -62,14 +64,16 @@ public:
     struct HashKey
     {
         ScRange range;
-        bool descending;
-        ValueType type;
+        ValueType valueType;
+        ScQueryOp queryOp;
+        ScQueryEntry::QueryType queryType;
         bool operator==(const HashKey& other) const
         {
-            return range == other.range && descending == other.descending && type == other.type;
+            return range == other.range && valueType == other.valueType && queryOp == other.queryOp
+                   && queryType == other.queryType;
         }
     };
-    HashKey getHashKey() const { return { maRange, mDescending, mValues }; }
+    HashKey getHashKey() const { return { maRange, mValueType, mQueryOp, mQueryType }; }
     static HashKey makeHashKey(const ScRange& range, const ScQueryParam& param);
 
     struct Hash
@@ -78,8 +82,9 @@ public:
         {
             // Range should be just one column.
             size_t hash = key.range.hashStartColumn();
-            o3tl::hash_combine(hash, key.descending);
-            o3tl::hash_combine(hash, key.type);
+            o3tl::hash_combine(hash, key.valueType);
+            o3tl::hash_combine(hash, key.queryOp);
+            o3tl::hash_combine(hash, key.queryType);
             return hash;
         }
     };
@@ -100,8 +105,10 @@ private:
     std::vector<size_t> mRowToIndex; // indexed by 'SCROW - maRange.aStart.Row()'
     ScRange maRange;
     ScDocument* mpDoc;
-    bool mDescending;
-    ValueType mValues;
+    bool mValid;
+    ValueType mValueType;
+    ScQueryOp mQueryOp;
+    ScQueryEntry::QueryType mQueryType;
 
     ScSortedRangeCache(const ScSortedRangeCache&) = delete;
     ScSortedRangeCache& operator=(const ScSortedRangeCache&) = delete;
