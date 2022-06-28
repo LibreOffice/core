2cc955f9109c0fc8443c9f93c1bf6bd317043cb5
Luboš Luňák <l.lunak@collabora.com>
2022-06-28 09:42:17 +0200

    lok: make sure flushPendingLOKInvalidateTiles() is called
    SwViewShellImp::AddPendingLOKInvalidation() collects invalidations,
    but this was relying on something eventually calling
    flushPendingLOKInvalidateTiles(), which wasn't guaranteed.
    If e.g. a spellchecker caused an invalidation from in idle callback
    and nothing else changed, then the LOK callback handling code
    didn't know there was something pending. So add an explicit
    call to ensure to notify about these pending invalidations.
    
    Change-Id: I0a9cb0d5aba2fdbbac126cd8a4a3412bef1cab25
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/136531
    Tested-by: Jenkins
    Reviewed-by: Luboš Luňák <l.lunak@collabora.com>
    (cherry picked from commit 32cbd1c57dbaf7cff2325c126b3adfcf2150bc23)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/136532
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

--- sfx2/source/view/viewsh.cxx
+++ sfx2/source/view/viewsh.cxx
@@ -1546,6 +1546,16 @@ void SfxViewShell::libreOfficeKitViewUpdatedCallbackPerViewId(int nType, int nVi
             << lokCallbackTypeToString(nType));
 }
 
+void SfxViewShell::libreOfficeKitViewAddPendingInvalidateTiles()
+{
+    if (pImpl->m_pLibreOfficeKitViewCallback)
+        pImpl->m_pLibreOfficeKitViewCallback->libreOfficeKitViewAddPendingInvalidateTiles();
+    else
+        SAL_INFO(
+            "sfx.view",
+            "SfxViewShell::libreOfficeKitViewAddPendingInvalidateTiles no callback set!");
+}
+
 void SfxViewShell::afterCallbackRegistered()
 {
 }
