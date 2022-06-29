LibreOffice/core

commit 4cdb960a79aff565f956f618116f3ea4b3836aa4

sd: hide 'enable remote control' checkbox when built without bluetooth
Change-Id: I3ee70bdf59abf71228a708f05dba07fd2a873128

sd/source/ui/dlg/tpoption.cxx
@@ -623,6 +623,9 @@ void SdTpOptionsMisc::SetImpressMode (void)
     // Move the printer-independent-metrics check box up two lines to change
     // places with spacing-between-paragraphs check box.
     lcl_MoveWin (aCbxUsePrinterMetrics, -2*nLineHeight);
+#ifndef ENABLE_SDREMOTE_BLUETOOTH
+    aCbxEnableSdremote.Hide();
+#endif
 }
 
 void    SdTpOptionsMisc::SetDrawMode()

commit 8d7c441097cc0279a9819fe0673b8cad40ae94db

only hide Impress Remote option if there is no sdremote at all
it was incorrectly based on bluetooth support, but the remote can also
be used in the local network.
Regression from 4cdb960a79aff565f956f618116f3ea4b3836aa4 still worked
for users who had it enabled in earlier versions, but not with new
profiles.

Change-Id: Ib4026c60fed0842b3bad483d78e46dd73d1d75f5

sd/source/ui/dlg/tpoption.cxx
@@ -491,7 +491,7 @@ IMPL_LINK_NOARG(SdTpOptionsMisc, SelectMetricHdl_Impl, weld::ComboBox&, void)
 
 void SdTpOptionsMisc::SetImpressMode()
 {
-#ifndef ENABLE_SDREMOTE_BLUETOOTH
+#ifndef ENABLE_SDREMOTE
     m_xCbxEnableSdremote->hide();
 #else
     (void) this; // loplugin:staticmethods