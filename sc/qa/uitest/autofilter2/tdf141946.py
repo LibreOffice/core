8d83f447abeaa6d93a44baeb3bb3ebc8aadbe1f0
Balazs Varga <balazs.varga.extern@allotropia.de>
2022-06-29 08:17:40 +0200

    tdf#117276 sc: Show hidden filter elements as inactive elements
    Showing hidden values in the autofilter dropdown (as inactive when
    it was hidden by another row) - without changing the behaviour of
    autofilter. First those which belongs to non-hidden rows, then those
    which belongs to hidden rows.
    
    Change-Id: Iafeb43176efe7ab422b22697d399c68c95d0319d

--- sc/qa/uitest/autofilter2/tdf141946.py
+++ sc/qa/uitest/autofilter2/tdf141946.py
@@ -50,7 +50,8 @@ class tdf141946(UITestCase):
             xFloatWindow = self.xUITest.getFloatWindow()
             xCheckListMenu = xFloatWindow.getChild("FilterDropDown")
             xList = xCheckListMenu.getChild("check_list_box")
-            self.assertEqual(2, len(xList.getChildren()))
+            # since tdf#117267, we are showing the hidden filter rows as inactive elements (2 active + 1 inactive)
+            self.assertEqual(3, len(xList.getChildren()))
             xCloseBtn = xFloatWindow.getChild("cancel")
             xCloseBtn.executeAction("CLICK", tuple())
 
