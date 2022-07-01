7edd41b4bc113a48b413179b7eb63a7478c24ea1
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2022-07-01 22:00:38 +0200

    chart2: InsertDataTableDialog for inserting/removing the data table
    Change-Id: Ie3c033c587b150723e7aa39cd5ddf5774104db9a

--- chart2/source/controller/dialogs/tp_DataTable.cxx
+++ chart2/source/controller/dialogs/tp_DataTable.cxx
@@ -9,19 +9,13 @@
 
 #include "tp_DataTable.hxx"
 
-#include <chartview/ChartSfxItemIds.hxx>
-#include <svl/eitem.hxx>
-
 namespace chart
 {
 DataTableTabPage::DataTableTabPage(weld::Container* pPage, weld::DialogController* pController,
                                    const SfxItemSet& rInAttrs)
     : SfxTabPage(pPage, pController, "modules/schart/ui/tp_DataTable.ui", "DataTableTabPage",
                  &rInAttrs)
-    , m_xCbHorizontalBorder(m_xBuilder->weld_check_button("horizontalBorderCB"))
-    , m_xCbVerticalBorder(m_xBuilder->weld_check_button("verticalBorderCB"))
-    , m_xCbOutilne(m_xBuilder->weld_check_button("outlineCB"))
-    , m_xCbKeys(m_xBuilder->weld_check_button("keysCB"))
+    , m_aDataTablePropertiesResources(*m_xBuilder)
 {
 }
 
@@ -34,78 +28,14 @@ std::unique_ptr<SfxTabPage> DataTableTabPage::Create(weld::Container* pPage,
     return std::make_unique<DataTableTabPage>(pPage, pController, *rAttrs);
 }
 
-bool DataTableTabPage::FillItemSet(SfxItemSet* rOutAttrs)
+bool DataTableTabPage::FillItemSet(SfxItemSet* pOutAttrs)
 {
-    if (m_xCbHorizontalBorder->get_state() != TRISTATE_INDET)
-    {
-        rOutAttrs->Put(
-            SfxBoolItem(SCHATTR_DATA_TABLE_HORIZONTAL_BORDER, m_xCbHorizontalBorder->get_active()));
-    }
-    if (m_xCbVerticalBorder->get_state() != TRISTATE_INDET)
-    {
-        rOutAttrs->Put(
-            SfxBoolItem(SCHATTR_DATA_TABLE_VERTICAL_BORDER, m_xCbVerticalBorder->get_active()));
-    }
-    if (m_xCbOutilne->get_state() != TRISTATE_INDET)
-    {
-        rOutAttrs->Put(SfxBoolItem(SCHATTR_DATA_TABLE_OUTLINE, m_xCbOutilne->get_active()));
-    }
-    if (m_xCbKeys->get_state() != TRISTATE_INDET)
-    {
-        rOutAttrs->Put(SfxBoolItem(SCHATTR_DATA_TABLE_KEYS, m_xCbKeys->get_active()));
-    }
-    return true;
+    return m_aDataTablePropertiesResources.writeToItemSet(*pOutAttrs);
 }
 
 void DataTableTabPage::Reset(const SfxItemSet* pInAttrs)
 {
-    const SfxPoolItem* pPoolItem = nullptr;
-    SfxItemState aState;
-
-    aState = pInAttrs->GetItemState(SCHATTR_DATA_TABLE_HORIZONTAL_BORDER, false, &pPoolItem);
-    if (aState == SfxItemState::DONTCARE)
-    {
-        m_xCbHorizontalBorder->set_state(TRISTATE_INDET);
-    }
-    else
-    {
-        if (aState == SfxItemState::SET)
-            m_xCbHorizontalBorder->set_active(
-                static_cast<const SfxBoolItem*>(pPoolItem)->GetValue());
-    }
-
-    aState = pInAttrs->GetItemState(SCHATTR_DATA_TABLE_VERTICAL_BORDER, false, &pPoolItem);
-    if (aState == SfxItemState::DONTCARE)
-    {
-        m_xCbVerticalBorder->set_state(TRISTATE_INDET);
-    }
-    else
-    {
-        if (aState == SfxItemState::SET)
-            m_xCbVerticalBorder->set_active(static_cast<const SfxBoolItem*>(pPoolItem)->GetValue());
-    }
-
-    aState = pInAttrs->GetItemState(SCHATTR_DATA_TABLE_OUTLINE, false, &pPoolItem);
-    if (aState == SfxItemState::DONTCARE)
-    {
-        m_xCbOutilne->set_state(TRISTATE_INDET);
-    }
-    else
-    {
-        if (aState == SfxItemState::SET)
-            m_xCbOutilne->set_active(static_cast<const SfxBoolItem*>(pPoolItem)->GetValue());
-    }
-
-    aState = pInAttrs->GetItemState(SCHATTR_DATA_TABLE_KEYS, false, &pPoolItem);
-    if (aState == SfxItemState::DONTCARE)
-    {
-        m_xCbKeys->set_state(TRISTATE_INDET);
-    }
-    else
-    {
-        if (aState == SfxItemState::SET)
-            m_xCbKeys->set_active(static_cast<const SfxBoolItem*>(pPoolItem)->GetValue());
-    }
+    m_aDataTablePropertiesResources.initFromItemSet(*pInAttrs);
 }
 
 } //namespace chart
