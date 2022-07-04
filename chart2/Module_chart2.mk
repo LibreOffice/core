268f0f3f0d70902acaeb3f99e0d206ad66ffd67c
Xisco Fauli <xiscofauli@libreoffice.org>
2022-07-04 11:42:22 +0200

    run CppunitTest_chart2_xshape without ENABLE_CHART_TESTS
    This was added in d8ae59e097b3fca29f071d1a792cdb98ad8139cf
    "add concept for chart xshape tests" and it's only run
    when using --enable-chart-tests
    OTOH, SdImportTest::testDocumentLayout in sd/qa/unit/import-tests.cxx
    also uses doXMLDiff to compare shapes and it's run without any
    special flag.
    Let's remove the ENABLE_CHART_TESTS check to see if jenkins or any
    tinderbox fails. If it does, we can always revert this commit.
    If it works fine, I'll remove the --enable-chart-tests machinery
    in a follow-up commit in some days
    
    Change-Id: I5d462df09303701826e5849e9e3bacca465da814
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/136796
    Tested-by: Jenkins
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- chart2/Module_chart2.mk
+++ chart2/Module_chart2.mk
@@ -38,12 +38,10 @@ $(eval $(call gb_Module_add_slowcheck_targets,chart2,\
     CppunitTest_chart2_geometry \
 ))
 
-ifeq ($(ENABLE_CHART_TESTS),TRUE)
 ifeq ($(WITH_FONTS), TRUE)
 $(eval $(call gb_Module_add_slowcheck_targets,chart2,\
     CppunitTest_chart2_xshape \
 ))
-endif
 endif
 
 $(eval $(call gb_Module_add_subsequentcheck_targets,chart2,\
