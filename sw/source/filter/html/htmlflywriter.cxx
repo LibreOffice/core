8b26fec2d57986bc511cbf659cd186b9900ae398
Luboš Luňák <l.lunak@collabora.com>
2022-07-07 17:35:56 +0200

    do not write vspace/hspace attributes to reqif
    These are not valid for the ReqIF-XHTML subset.
    
    Change-Id: Ie68560c49ba43b0f728c6e51eeb33b4829bded93

--- sw/source/filter/html/htmlflywriter.cxx
+++ sw/source/filter/html/htmlflywriter.cxx
@@ -622,7 +622,8 @@ OString SwHTMLWriter::OutFrameFormatOptions( const SwFrameFormat &rFrameFormat,
 
     if( (nFrameOpts & HtmlFrmOpts::Space) &&
         (aTwipSpc.Width() || aTwipSpc.Height()) &&
-        Application::GetDefaultDevice() )
+        Application::GetDefaultDevice() &&
+        !mbReqIF )
     {
         Size aPixelSpc =
             Application::GetDefaultDevice()->LogicToPixel( aTwipSpc,
@@ -889,7 +890,8 @@ void SwHTMLWriter::writeFrameFormatOptions(HtmlWriter& aHtml, const SwFrameForma
 
     if( (nFrameOptions & HtmlFrmOpts::Space) &&
         (aTwipSpc.Width() || aTwipSpc.Height()) &&
-        Application::GetDefaultDevice() )
+        Application::GetDefaultDevice() &&
+        !mbReqIF )
     {
         Size aPixelSpc =
             Application::GetDefaultDevice()->LogicToPixel( aTwipSpc,
