bda3eeb683195f37cef70d68fda90301df12cfbf
Noel Grandin <noel.grandin@collabora.co.uk>
2022-07-13 13:14:34 +0200

    clang-tidy modernize-pass-by-value in svtools
    Change-Id: I60e7373c924a479fed72eb4f0538006e3e422004

--- svtools/source/control/accessibleruler.hxx
+++ svtools/source/control/accessibleruler.hxx
@@ -48,7 +48,7 @@ class SvtRulerAccessible final : public ::cppu::BaseMutex, public SvtRulerAccess
 public:
     //=====  internal  ========================================================
     SvtRulerAccessible(
-        const css::uno::Reference< css::accessibility::XAccessible>& rxParent, Ruler& rRepresentation, const OUString& rName );
+        css::uno::Reference< css::accessibility::XAccessible> xParent, Ruler& rRepresentation, OUString aName );
 
     /// @throws css::uno::RuntimeException
     bool
--- svtools/source/misc/filechangedchecker.cxx
+++ svtools/source/misc/filechangedchecker.cxx
@@ -12,14 +12,15 @@
 #include <osl/file.hxx>
 
 #include <svtools/filechangedchecker.hxx>
+#include <utility>
 #include <vcl/timer.hxx>
 
-FileChangedChecker::FileChangedChecker(const OUString& rFilename,
-        const ::std::function<void ()>& rCallback)
+FileChangedChecker::FileChangedChecker(OUString aFilename,
+        ::std::function<void ()> aCallback)
     : mTimer("SVTools FileChangedChecker Timer")
-    , mFileName(rFilename)
+    , mFileName(std::move(aFilename))
     , mLastModTime()
-    , mpCallback(rCallback)
+    , mpCallback(std::move(aCallback))
 {
     // Get the current last file modified Status
     getCurrentModTime(mLastModTime);
@@ -34,9 +35,9 @@ FileChangedChecker::FileChangedChecker(const OUString& rFilename,
     resetTimer();
 }
 
-FileChangedChecker::FileChangedChecker(const OUString& rFilename)
+FileChangedChecker::FileChangedChecker(OUString aFilename)
     : mTimer("")
-    , mFileName(rFilename)
+    , mFileName(std::move(aFilename))
     , mLastModTime()
     , mpCallback(nullptr)
 {
