b6ca0d858bbe6f7a617b62e7831b579f80373101
Bartosz Kosiorek <gang65@poczta.onet.pl>
2022-07-15 22:46:03 +0200

    :tdf33787 lotuswordpro: fix increments of ordered bullets
    With previous implementation there was an bug which causing missing
    incrementing ordered bullets. For example:
    
            A. Lettered bullet uppercase 1 (A)
            A. Lettered bullet uppercase 2 (B)
            A. Lettered bullet uppercase 3 (C)
    
    This patch is fixing that, and increments is working correctly:
    
            A. Lettered bullet uppercase 1 (A)
            B. Lettered bullet uppercase 2 (B)
            C. Lettered bullet uppercase 3 (C)
    
    Change-Id: I1d1ad9ee549039077b418756efefb10bacfa1ddd

--- lotuswordpro/qa/cppunit/import_test.cxx
+++ lotuswordpro/qa/cppunit/import_test.cxx
@@ -53,6 +53,25 @@ void LotusWordProTest::tearDown()
 
 constexpr OUStringLiteral DATA_DIRECTORY = u"/lotuswordpro/qa/cppunit/data/";
 
+CPPUNIT_TEST_FIXTURE(LotusWordProTest, testtdf33787OrderedBullets)
+{
+    OUString aURL = m_directories.getURLFromSrc(DATA_DIRECTORY) + "tdf33787-ordered-bullets.lwp";
+    getComponent() = loadFromDesktop(aURL);
+    uno::Reference<text::XTextDocument> textDocument(getComponent(), uno::UNO_QUERY);
+    uno::Reference<container::XEnumerationAccess> xParaEnumAccess(textDocument->getText(),
+                                                                  uno::UNO_QUERY);
+    uno::Reference<container::XEnumeration> xParaEnum = xParaEnumAccess->createEnumeration();
+
+    //TODO Check three numbered bullets (1. 2. 3.)
+    //TODO Check three roman numeral lowercase bullets (i. ii. iii.)
+    //TODO Check three roman numeral uppercase bullets (I. II. III)
+    //TODO Check three Lettered bullet lowercase (a. b. c.)
+    //TODO Check three Lettered bullet uppercase (A. B. C.)
+    //TODO Numbered bullet w/ bracket (1) 2) 3)) with skipped bullet
+    //TODO Check Lettered bullet w/ bracket lowercase (a) b) c)) with indentation
+    //TODO Check Roman numeral w/ bracket lowercase bullet (i) ii) iii)) with indentation
+}
+
 CPPUNIT_TEST_FIXTURE(LotusWordProTest, testTdf129993)
 {
     OUString aURL = m_directories.getURLFromSrc(DATA_DIRECTORY) + "tdf129993.lwp";
