f341c289c63c3f007af83827da813dcee72c30fd
Andreas Heinisch <andreas.heinisch@yahoo.de>
2022-07-15 16:25:06 +0200

    tdf#135761 - Calc cell editing: change search preference for the word boundary
    Since ImpEditEngine::StartOfWord is only used when a selection is deleted at the left, change the search preference of the word boundary from forward to backward. For further details of a deletion of a selection check ImpEditEngine::DeleteLeftOrRight.
    
    Change-Id: Ic68901cc04e5057e058ff03304f68eef1dca7a1e

---
+++ sc/qa/uitest/calc_tests9/tdf135761.py
@@ -0,0 +1,33 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+from uitest.framework import UITestCase
+from libreoffice.uno.propertyvalue import mkPropertyValues
+from libreoffice.calc.document import get_cell_by_position
+
+class tdf135761(UITestCase):
+    def test_tdf135761_ctrl_backspace(self):
+        with self.ui_test.create_doc_in_start_center("calc") as document:
+            xCalcDoc = self.xUITest.getTopFocusWindow()
+            xGridWindow = xCalcDoc.getChild("grid_window")
+
+            # Enter text including two whitespaces in the text, and delete the left part of the text
+            xGridWindow.executeAction("SELECT", mkPropertyValues({"CELL": "A1"}))
+            xGridWindow.executeAction("TYPE", mkPropertyValues({"TEXT": "a  b"}))
+            xGridWindow.executeAction("TYPE", mkPropertyValues({"KEYCODE":"F2"}))
+            xGridWindow.executeAction("TYPE", mkPropertyValues({"KEYCODE":"LEFT"}))
+            xGridWindow.executeAction("TYPE", mkPropertyValues({"KEYCODE":"LEFT"}))
+            xGridWindow.executeAction("TYPE", mkPropertyValues({"KEYCODE":"CTRL+BACKSPACE"}))
+            xGridWindow.executeAction("TYPE", mkPropertyValues({"KEYCODE":"RETURN"}))
+
+            # Without the fix in place, this test would have failed with
+            # AssertionError: ' b' != 'a b'
+            self.assertEqual(get_cell_by_position(document, 0, 0, 0).getString(), " b")
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
