6f4566132d0e819351ccfa2c0d21926f4c8e4064
Szymon Kłos <szymon.klos@collabora.com>
2022-07-21 15:22:36 +0200

    lok: don't force formulabar update on every view switch
    This will reduce number of messages and helps to avoid
    selection change in other views when someone is editing
    a multiline cell and other user is doing something else
    at the same time (which triggers view switching to perform
    operations as different user).
    
    Change-Id: I8efc82be9793910c46bf6f33f7993e4c5734f9de
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/137304
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Rashesh Padia <rashesh.padia@collabora.com>
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>
    (cherry picked from commit a173b937aa8b8b67a423f23b05ddf02f83870034)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/137414
    Reviewed-by: Gökay ŞATIR <gokaysatir@collabora.com>

--- sc/source/ui/view/tabvwsh4.cxx
+++ sc/source/ui/view/tabvwsh4.cxx
@@ -159,7 +159,8 @@ void ScTabViewShell::Activate(bool bMDI)
             }
         }
 
-        UpdateInputHandler( /*bForce=*/ true, /*bStopEditing=*/ !comphelper::LibreOfficeKit::isActive() );
+        bool isLOK = comphelper::LibreOfficeKit::isActive();
+        UpdateInputHandler( /*bForce=*/ !isLOK, /*bStopEditing=*/ !isLOK );
 
         if ( bFirstActivate )
         {
