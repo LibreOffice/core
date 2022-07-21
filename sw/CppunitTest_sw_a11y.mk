0a79115afb6b1b9bde2e1cd9c9fa03b9813079bc
Colomban Wendling <cwendling@hypra.fr>
2022-07-21 21:51:21 +0200

    Add infrastructure and basic tests including slight UI interaction
    This introduces a couple helper base classes for implementing
    accessibility tests more easily, and includes a few tests as examples,
    including basic document layout check and minimal UI interaction
    through menus.
    
    Change-Id: I8961af8be1e7d52dc55fe27c758806d9b4c3c5d9

---
+++ sw/CppunitTest_sw_a11y.mk
@@ -0,0 +1,38 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_CppunitTest_CppunitTest,sw_a11y))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,sw_a11y, \
+	sw/qa/extras/accessibility/basics \
+))
+
+$(eval $(call gb_CppunitTest_use_libraries,sw_a11y, \
+	sal \
+	cppu \
+	subsequenttest \
+	test \
+	unotest \
+	vcl \
+))
+
+$(eval $(call gb_CppunitTest_use_api,sw_a11y,\
+	offapi \
+	udkapi \
+))
+
+$(eval $(call gb_CppunitTest_use_sdk_api,sw_a11y))
+$(eval $(call gb_CppunitTest_use_rdb,sw_a11y,services))
+$(eval $(call gb_CppunitTest_use_ure,sw_a11y))
+$(eval $(call gb_CppunitTest_use_vcl,sw_a11y))
+
+$(eval $(call gb_CppunitTest_use_instdir_configuration,sw_a11y))
+$(eval $(call gb_CppunitTest_use_common_configuration,sw_a11y))
+
+# vim: set noet sw=4 ts=4:
---
+++ sw/qa/extras/accessibility/basics.cxx
@@ -0,0 +1,96 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <com/sun/star/awt/Key.hpp>
+#include <LibreOfficeKit/LibreOfficeKitEnums.h>
+#include <vcl/scheduler.hxx>
+
+#include <test/a11y/swaccessibletestbase.hxx>
+#include <test/a11y/AccessibilityTools.hxx>
+
+using namespace css;
+
+CPPUNIT_TEST_FIXTURE(test::SwAccessibleTestBase, TestBasicStructure)
+{
+    loadFromSrc(u"/sw/qa/python/testdocuments/xtextcontent.odt");
+    auto xContext = getDocumentAccessibleContext();
+    CPPUNIT_ASSERT(xContext.is());
+
+    dumpA11YTree(xContext);
+    CPPUNIT_ASSERT_EQUAL(
+        rtl::OUString("<PARAGRAPH>String1</PARAGRAPH><PARAGRAPH/><PARAGRAPH/><PARAGRAPH/>"
+                      "<TABLE name=\"Table1-1\" description=\"Table1 on page 1\">"
+                      "<TABLE_CELL name=\"A1\" description=\"A1\">"
+                      "<PARAGRAPH>String2</PARAGRAPH>"
+                      "</TABLE_CELL>"
+                      "</TABLE>"
+                      "<PARAGRAPH/>"
+                      "<TEXT_FRAME name=\"Frame1\"><PARAGRAPH>Frame1</PARAGRAPH></TEXT_FRAME>"
+                      "<TEXT_FRAME name=\"Frame2\"><PARAGRAPH>Frame2</PARAGRAPH></TEXT_FRAME>"),
+        collectText(xContext));
+}
+
+CPPUNIT_TEST_FIXTURE(test::SwAccessibleTestBase, TestTypeSimple)
+{
+    load(u"private:factory/swriter");
+    auto xContext = getDocumentAccessibleContext();
+    CPPUNIT_ASSERT(xContext.is());
+
+    documentPostKeyEvent(LOK_KEYEVENT_KEYINPUT, 'h', 0);
+    documentPostKeyEvent(LOK_KEYEVENT_KEYINPUT, 'e', 0);
+    documentPostKeyEvent(LOK_KEYEVENT_KEYINPUT, 'l', 0);
+    documentPostKeyEvent(LOK_KEYEVENT_KEYINPUT, 'l', 0);
+    documentPostKeyEvent(LOK_KEYEVENT_KEYINPUT, 'o', 0);
+    Scheduler::ProcessEventsToIdle();
+
+    CPPUNIT_ASSERT_EQUAL(rtl::OUString("<PARAGRAPH>hello</PARAGRAPH>"), collectText(xContext));
+}
+
+CPPUNIT_TEST_FIXTURE(test::SwAccessibleTestBase, TestTypeMultiPara)
+{
+    load(u"private:factory/swriter");
+    auto xContext = getDocumentAccessibleContext();
+    CPPUNIT_ASSERT(xContext.is());
+
+    documentPostKeyEvent(LOK_KEYEVENT_KEYINPUT, 'A', 0);
+    documentPostKeyEvent(LOK_KEYEVENT_KEYINPUT, 0, awt::Key::RETURN);
+    documentPostKeyEvent(LOK_KEYEVENT_KEYINPUT, 'B', 0);
+    documentPostKeyEvent(LOK_KEYEVENT_KEYINPUT, 0, awt::Key::RETURN);
+    documentPostKeyEvent(LOK_KEYEVENT_KEYINPUT, 'C', 0);
+    Scheduler::ProcessEventsToIdle();
+
+    CPPUNIT_ASSERT_EQUAL(
+        rtl::OUString("<PARAGRAPH>A</PARAGRAPH><PARAGRAPH>B</PARAGRAPH><PARAGRAPH>C</PARAGRAPH>"),
+        collectText(xContext));
+}
+
+CPPUNIT_TEST_FIXTURE(test::SwAccessibleTestBase, TestMenuInsertPageNumber)
+{
+    load(u"private:factory/swriter");
+    CPPUNIT_ASSERT(activateMenuItem(u"Insert", u"Page Number"));
+    CPPUNIT_ASSERT_EQUAL(rtl::OUString("<PARAGRAPH>1</PARAGRAPH>"), collectText());
+}
+
+CPPUNIT_TEST_FIXTURE(test::SwAccessibleTestBase, TestMenuInsertPageBreak)
+{
+    load(u"private:factory/swriter");
+
+    CPPUNIT_ASSERT(activateMenuItem(u"Insert", u"Page Number"));
+    CPPUNIT_ASSERT(activateMenuItem(u"Insert", u"Page Break"));
+    // we need to move focus to the paragraph after the page break to insert the page number there
+    documentPostKeyEvent(LOK_KEYEVENT_KEYINPUT, 0, awt::Key::DOWN);
+    CPPUNIT_ASSERT(activateMenuItem(u"Insert", u"Page Number"));
+
+    CPPUNIT_ASSERT_EQUAL(rtl::OUString("<PARAGRAPH>1</PARAGRAPH><PARAGRAPH>2</PARAGRAPH>"),
+                         collectText());
+}
+
+CPPUNIT_PLUGIN_IMPLEMENT();
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
