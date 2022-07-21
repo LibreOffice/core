053f470316b89b6fcea1fac69e6bffd4f65f91ee
PoonamShokeen <poonamshokeen7@gmail.com>
2022-07-20 22:25:14 -0500

    tdf#143148 Use pragma once instead of include guards
    Change-Id: I7061e0985c51910ba6a8b862cd755b8b4f7016bd
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/137279
    Tested-by: Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>
    Reviewed-by: Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>

--- include/framework/interaction.hxx
+++ include/framework/interaction.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_FRAMEWORK_INTERACTION_HXX
-#define INCLUDED_FRAMEWORK_INTERACTION_HXX
+#pragma once
 
 #include <config_options.h>
 #include <rtl/ref.hxx>
@@ -99,7 +98,5 @@ public:
 };
 
 } //  namespace framework
-
-#endif // #define INCLUDED_FRAMEWORK_INTERACTION_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
