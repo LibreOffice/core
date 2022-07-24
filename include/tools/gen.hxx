b5e8b5d45e2d5c514a0de5ab7addf32ab5b24c9a
Chris Sherlock <chris.sherlock79@gmail.com>
2022-07-24 18:56:34 +1000

    tdf#143148 tools: Use pragma once instead of include guards
    Change-Id: Ie34bc66eb1dc475566b315db698976913c8aa37b

--- include/tools/gen.hxx
+++ include/tools/gen.hxx
@@ -16,8 +16,8 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
-#ifndef INCLUDED_TOOLS_GEN_HXX
-#define INCLUDED_TOOLS_GEN_HXX
+
+#pragma once
 
 #include <tools/toolsdllapi.h>
 
@@ -814,7 +814,5 @@ inline std::basic_ostream<charT, traits> & operator <<(
                       << "@(" << rectangle.getX() << ',' << rectangle.getY() << ")";
 }
 }
-
-#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
