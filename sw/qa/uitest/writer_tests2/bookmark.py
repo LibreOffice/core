2c47f63b2ff77c1e407eafb1ab7cbbfb951ebb59
Michael Stahl <michael.stahl@allotropia.de>
2022-07-26 12:12:49 +0200

    tdf#150017 vcl,sw: add UITest
    Unfortunately i couldn't figure out how to send keyboard input to the
    Edit widget in the SvTreeListBox; it's created by
    SvTreeListBox::EditText() and shows up when calling dumpHierarchy()
    on the DialogUIObject as a child of the
    SvTreeListBox/"19LclHeaderTabListBox" but the execute() doesn't send it
    events, neither when called on DialogUIObject nor on TreeListUIObject.
    
    So forward explicitly in TreeListUIObject::execute() - probably not the
    best way to do this?
    
    When using the UI, events arrive like this:
    
    0  SvInplaceEdit2::KeyInput(KeyEvent const&) at vcl/source/treelist/treelistbox.cxx:202
    1  (anonymous namespace)::MyEdit_Impl::KeyInput(KeyEvent const&) at vcl/source/treelist/treelistbox.cxx:117
    2  ImplHandleKey(vcl::Window*, MouseNotifyEvent, sal_uInt16, sal_uInt16, sal_uInt16, bool) (pWindow=0x6dc4bc0, nSVEvent=MouseNotifyEvent::KEYINPUT, nKeyCode=1026, nCharCode=0, nRepeat=0, bForward=true) at vcl/source/window/winproc.cxx:1209
    3  ImplWindowFrameProc(vcl::Window*, SalEvent, void const*) at vcl/source/window/winproc.cxx:2672
    4  SalFrame::CallCallback(SalEvent, void const*) const (this=0x8a19fb0, nEvent=SalEvent::KeyInput, pEvent=0x7ffc7f3639a0) at vcl/inc/salframe.hxx:306
    5  X11SalFrame::HandleKeyEvent(XKeyEvent*) at vcl/unx/generic/window/salframe.cxx:3190
    
    Change-Id: I0be42330aeef98ef09e21297acef6cf616b7f2d2

--- sw/qa/uitest/writer_tests2/bookmark.py
+++ sw/qa/uitest/writer_tests2/bookmark.py
@@ -90,5 +90,35 @@ class bookmarkDialog(UITestCase):
                 xGoToBtn = xBookDlg.getChild("goto")
                 xGoToBtn.executeAction("CLICK", tuple()) # goto 1st bookmark
 
+    def test_bookmark_dialog_edittext(self):
+        with self.ui_test.create_doc_in_start_center("writer") as xDoc:
+
+            xDoc.Text.insertString(xDoc.Text.getStart(), "foo", False)
+            self.xUITest.executeCommand(".uno:SelectAll")
+
+            with self.ui_test.execute_dialog_through_command(".uno:InsertBookmark", close_button="insert"):
+                pass
+
+            with self.ui_test.execute_dialog_through_command(".uno:InsertBookmark", close_button="close") as xBookDlg:
+                xBmk = xBookDlg.getChild("bookmarks")
+                xFirstListEntry = xBmk.getChild("0") #  select first bookmark
+                xFirstListEntry.executeAction("SELECT", tuple())
+                xEditBtn = xBookDlg.getChild("edittext")
+
+                xEditBtn.executeAction('CLICK', ())
+
+                # this does not work - the Edit widget has the focus but it's not forwarded
+#                xBookDlg.executeAction("TYPE", mkPropertyValues({"TEXT":"fubar"}))
+#                xBookDlg.executeAction("TYPE", mkPropertyValues({"KEYCODE": "RETURN"}))
+                # this did not work previously but now works due to explicit
+                # forwarding in TreeListUIObject::execute()
+                xBmk.executeAction("TYPE", mkPropertyValues({"TEXT":"fubar"}))
+                xBmk.executeAction("TYPE", mkPropertyValues({"KEYCODE": "RETURN"}))
+
+                x1stListEntry = xBmk.getChild("O") #  select first bookmark
+                x1stListEntry.executeAction("SELECT", tuple())
+
+                self.assertEqual(xDoc.Text.String, "fubar")
+                self.assertEqual(get_state_as_dict(x1stListEntry)["Text"], "1\tBookmark 1\tfubar\tNo\t")
 
 # vim: set shiftwidth=4 softtabstop=4 expandtab:
