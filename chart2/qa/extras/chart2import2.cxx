LibreOffice/core

commit 72613bd2a04b4b1ba614c0f645e9804ebfe73682

tdf#126425 Chart: fix missing legend with exceeding text
Ellipsize legend box contents, if the custom size of
the chart legend is too small to contain the text.

Change-Id: I91ef913f509b233912a8bfb878a1c80d577e2a88
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/96437
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

chart2/source/view/main/VLegend.cxx
@@ -408,18 +408,62 @@ awt::Size lcl_placeLegendEntries(
                         DrawModelWrapper::removeShape( aTextShapes[nEntry] );
                         aTextShapes.pop_back();
                     }
-                    if( nEntry < nNumberOfEntries )
+                    if( nEntry < nNumberOfEntries && ( nEntry != 0 || nNumberOfColumns != 1 ) )
                     {
                         DrawModelWrapper::removeShape( rEntries[ nEntry ].aSymbol );
                         rEntries.pop_back();
                         nNumberOfEntries--;
                     }
                 }
-                nSumHeight -= aRowHeights[nRow];
-                aRowHeights.pop_back();
-                nRemainingSpace = rRemainingSpace.Height - nSumHeight;
-                if( nRemainingSpace>=0 )
-                    break;
+                if (nRow == 0 && nNumberOfColumns == 1)
+                {
+                    try
+                    {
+                        OUString aLabelString = rEntries[0].aLabel[0]->getString();
+                        const OUString sDots = "...";
+                        ShapeFactory* pShapeFactory = ShapeFactory::getOrCreateShapeFactory(xShapeFactory);
+                        for (sal_Int32 nNewLen = aLabelString.getLength() - sDots.getLength(); nNewLen > 0; nNewLen--)
+                        {
+                            OUString aNewLabel = aLabelString.copy(0, nNewLen) + sDots;
+                            Reference<drawing::XShape> xEntry = pShapeFactory->createText(
+                                xTarget, aNewLabel, rTextProperties.first, rTextProperties.second, uno::Any());
+                            nSumHeight = xEntry->getSize().Height;
+                            nRemainingSpace = rRemainingSpace.Height - nSumHeight;
+                            if (nRemainingSpace >= 0)
+                            {
+                                sal_Int32 nWidth = xEntry->getSize().Width + nSymbolPlusDistanceWidth;
+                                if (rRemainingSpace.Width - nWidth >= 0)
+                                {
+                                    aTextShapes.push_back(xEntry);
+                                    rEntries[0].aLabel[0]->setString(aNewLabel);
+                                    aRowHeights[0] = nSumHeight;
+                                    aColumnWidths[0] = nWidth;
+                                    break;
+                                }
+                            }
+                            DrawModelWrapper::removeShape(xEntry);
+                        }
+                        if (aTextShapes.size() == 0)
+                        {
+                            DrawModelWrapper::removeShape(rEntries[0].aSymbol);
+                            rEntries.pop_back();
+                            nNumberOfEntries--;
+                            aRowHeights.pop_back();
+                        }
+                    }
+                    catch (const uno::Exception&)
+                    {
+                        DBG_UNHANDLED_EXCEPTION("chart2");
+                    }
+                }
+                else
+                {
+                    nSumHeight -= aRowHeights[nRow];
+                    aRowHeights.pop_back();
+                    nRemainingSpace = rRemainingSpace.Height - nSumHeight;
+                    if (nRemainingSpace >= 0)
+                        break;
+                }
             }
             nNumberOfRows = static_cast<sal_Int32>(aRowHeights.size());
         }

sw/qa/extras/layout/layout.cxx
@@ -2363,6 +2363,23 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf75659)
     // These failed, if the legend names are empty strings.
 }
 
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf126425)
+{
+    SwDoc* pDoc = createDoc("long_legendentry.docx");
+    SwDocShell* pShell = pDoc->GetDocShell();
+
+    // Dump the rendering of the first page as an XML file.
+    std::shared_ptr<GDIMetaFile> xMetaFile = pShell->GetPreviewMetaFile();
+    MetafileXmlDump dumper;
+    xmlDocUniquePtr pXmlDoc = dumpAndParse(dumper, *xMetaFile);
+    CPPUNIT_ASSERT(pXmlDoc);
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 14
+    // - Actual  : 12
+    // i.e. the text of the chart legend lost.
+    assertXPath(pXmlDoc, "//textarray", 14);
+}
+
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf123268)
 {
     SwDoc* pDoc = createDoc("tdf123268.odt");

commit 9a22e0a0c3426bd64d31b1b102bf5f31b3ecfdac

tdf#150034 Chart: limit the lenght to legend labels
Regression from commit 72613bd2a04b4b1ba614c0f645e9804ebfe73682
(tdf#126425 Chart: fix missing legend with exceeding text).

Change-Id: I23a94f6baaf620b40e9b2819738eba5c5a921722

chart2/source/view/main/VLegend.cxx
@@ -167,6 +167,14 @@ awt::Size lcl_createTextShapes(
                 if( i == 1 )
                     break;
 
+                // tdf#150034 limit legend label text
+                if (aLabelSeq[i]->getString().getLength() > 520)
+                {
+                    sal_Int32 nIndex = aLabelSeq[i]->getString().indexOf(' ', 500);
+                    aLabelSeq[i]->setString(
+                        aLabelSeq[i]->getString().copy(0, nIndex > 500 ? nIndex : 500));
+                }
+
                 aLabelString += aLabelSeq[i]->getString();
                 // workaround for Issue #i67540#
                 if( aLabelString.isEmpty())

chart2/qa/extras/chart2import2.cxx
@@ -60,6 +60,7 @@ public:
     void testTdf121281();
     void testTdf139658();
     void testTdf146066();
+    void testTdf150034();
 
     CPPUNIT_TEST_SUITE(Chart2ImportTest2);
 
@@ -101,6 +102,7 @@ public:
     CPPUNIT_TEST(testTdf121281);
     CPPUNIT_TEST(testTdf139658);
     CPPUNIT_TEST(testTdf146066);
+    CPPUNIT_TEST(testTdf150034);
 
     CPPUNIT_TEST_SUITE_END();
 };
@@ -928,6 +930,22 @@ void Chart2ImportTest2::testTdf146066()
     CPPUNIT_ASSERT_EQUAL(OUString("30"), xLabel6->getString());
     uno::Reference<text::XTextRange> xLabel7(xIndexAccess->getByIndex(7), uno::UNO_QUERY);
     CPPUNIT_ASSERT_EQUAL(OUString("35"), xLabel7->getString());
+}
+
+void Chart2ImportTest2::testTdf150034()
+{
+    load(u"/chart2/qa/extras/data/xlsx/", u"tdf150034.xlsx");
+    Reference<chart::XChartDocument> xChartDoc(getChartDocFromSheet(0, mxComponent),
+                                               UNO_QUERY_THROW);
+    Reference<drawing::XDrawPageSupplier> xDrawPageSupplier(xChartDoc, UNO_QUERY_THROW);
+    Reference<drawing::XDrawPage> xDrawPage(xDrawPageSupplier->getDrawPage(), UNO_SET_THROW);
+    Reference<drawing::XShapes> xShapes(xDrawPage->getByIndex(0), UNO_QUERY_THROW);
+    Reference<drawing::XShape> xLegend = getShapeByName(xShapes, "CID/D=0:Legend=");
+    CPPUNIT_ASSERT(xLegend.is());
+    awt::Point aPosition = xLegend->getPosition();
+
+    // This failed, if the legend flowed out of the chart area.
+    CPPUNIT_ASSERT_GREATEREQUAL(static_cast<sal_Int32>(0), aPosition.Y);
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(Chart2ImportTest2);