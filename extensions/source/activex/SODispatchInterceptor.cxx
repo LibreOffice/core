eed4e5110c1cf2d0c8c30ef1d7cbf03a7889f02c
PoonamShokeen <poonamshokeen7@gmail.com>
2022-07-27 12:24:51 -0500

    tdf#145538 Use range based for loops.
    Change-Id: I144d020c6c57937caa800f019ab175597ed8ede4

--- extensions/source/activex/SODispatchInterceptor.cxx
+++ extensions/source/activex/SODispatchInterceptor.cxx
@@ -46,9 +46,17 @@ COM_DECLSPEC_NOTHROW STDMETHODIMP SODispatchInterceptor::InterfaceSupportsErrorI
         &IID_ISODispatchInterceptor,
     };
 
-    for (std::size_t i=0;i<SAL_N_ELEMENTS(arr);i++)
+    for (auto &i : arr)
     {
+<<<<<<< HEAD
         if (InlineIsEqualGUID(*arr[i],riid))
+=======
+#ifdef _MSC_VER
+        if (InlineIsEqualGUID(*i,riid))
+#else
+        if (::ATL::InlineIsEqualGUID(*i,riid))
+#endif
+>>>>>>> 62591b5bcb68 (tdf#145538 Use range based for loops, New file is added. Old file is restored)
             return S_OK;
     }
     return S_FALSE;
