5839135e276c97381b8521b617b15d1c0f98be4d
PoonamShokeen <poonamshokeen7@gmail.com>
2022-07-27 12:24:51 -0500

    tdf#145538 Use range based for loops
    Change-Id: I144d020c6c57937caa800f019ab175597ed8ede4

--- extensions/source/activex/SODispatchInterceptor.cxx
+++ extensions/source/activex/SODispatchInterceptor.cxx
@@ -46,9 +46,9 @@ COM_DECLSPEC_NOTHROW STDMETHODIMP SODispatchInterceptor::InterfaceSupportsErrorI
         &IID_ISODispatchInterceptor,
     };
 
-    for (std::size_t i=0;i<SAL_N_ELEMENTS(arr);i++)
+    for (auto const &i : arr)
     {
-        if (InlineIsEqualGUID(*arr[i],riid))
+        if (InlineIsEqualGUID(*i,riid))
             return S_OK;
     }
     return S_FALSE;
