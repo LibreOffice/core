9a03a9002ab21b7c81ae81ac522e9a1c79002480
Michael Meeks <michael.meeks@collabora.com>
2022-07-27 14:02:48 +0100

    lok: add dumpState feature for better in-field diagnostics.
    Always suspicious that some un-expected dialog / state can cause
    strange behavior in a client. An initial cut at an API to make it
    easier to unwind such problems by exposing the toolkit state.
    
    Change-Id: If8f17943fa4837df4f9ca659a111dcdce5c23244
    Signed-off-by: Michael Meeks <michael.meeks@collabora.com>

--- include/sfx2/viewsh.hxx
+++ include/sfx2/viewsh.hxx
@@ -57,6 +57,7 @@ class Menu;
 class NotifyEvent;
 class SfxInPlaceClient;
 class SfxLokCallbackInterface;
+namespace rtl { class OStringBuffer; }
 namespace vcl { class PrinterController; }
 
 namespace com::sun::star::beans { struct PropertyValue; }
@@ -337,6 +338,8 @@ public:
 
     /// Set up a more efficient internal callback instead of LibreOfficeKitCallback.
     void setLibreOfficeKitViewCallback(SfxLokCallbackInterface* pCallback);
+    /// dump view state for diagnostics
+    void dumpLibreOfficeKitViewState(rtl::OStringBuffer &rState);
     /// Invokes the registered callback, if there are any.
     virtual void libreOfficeKitViewCallback(int nType, const char* pPayload) const override;
     virtual void libreOfficeKitViewCallbackWithViewId(int nType, const char* pPayload, int nViewId) const override;
