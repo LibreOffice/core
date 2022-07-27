d3625d968901eb93a9680db8d1165f70de3fd64e
wjh-la <wujiahuan@loongson.cn>
2022-07-27 09:14:16 +0800

    Add loongarch64 support.
    Loongarch is a new RISC ISA , which includes a 32-bit version and a 64-bit version, Here are some documents about Loongarch：
    https://github.com/loongson/LoongArch-Documentation.
    
    The loongarch64 bridges implementation refers to mips64 bridges, and the code related to abi and asm has been modified
    
    Change-Id: I1d9cd3aadf63046c8cdecc9a64842567bfa1cecc
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/137486
    Tested-by: Stephan Bergmann <sbergman@redhat.com>
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

--- bridges/Library_cpp_uno.mk
+++ bridges/Library_cpp_uno.mk
@@ -114,6 +114,15 @@ bridge_noopt_objects := cpp2uno uno2cpp
 bridge_exception_objects := except
 endif
 
+else ifeq ($(CPUNAME),LOONGARCH64)
+
+ifneq ($(filter LINUX,$(OS)),)
+bridges_SELECTED_BRIDGE := gcc3_linux_loongarch64
+bridge_asm_objects := call
+bridge_noopt_objects := cpp2uno uno2cpp
+bridge_exception_objects := except
+endif
+
 else ifeq ($(CPUNAME),POWERPC)
 
 ifneq ($(filter DRAGONFLY FREEBSD LINUX NETBSD OPENBSD,$(OS)),)
