aea6273e58ac52f7a8a64877b4f7965857332574
Eike Rathke <erack@redhat.com>
2022-07-27 18:01:11 +0200

    FilteredRow is not a property of the column, tdf#117276 follow-up
    It's only needed in the FilterEntriesHandler, so pass it there.
    
    Change-Id: I5554ad13a43ccce6aafbba82b33418f060173a43

--- sc/inc/column.hxx
+++ sc/inc/column.hxx
@@ -204,7 +204,6 @@ class ScColumn : protected ScColumnData
     SCTAB           nTab;
 
     bool mbFiltering : 1; // it is true if there is a filtering in the column
-    bool mbFilteredRow : 1; // it is true if the actual row of the filtered column is hidden
     bool mbEmptyBroadcastersPending : 1; // a broadcaster not removed during EnableDelayDeletingBroadcasters()
 
 friend class ScDocument;                    // for FillInfo
@@ -256,7 +255,6 @@ public:
     SCTAB GetTab() const { return nTab; }
     SCCOL GetCol() const { return nCol; }
     bool HasFiltering() const { return mbFiltering; }
-    bool IsFilteredRow() const { return mbFilteredRow; }
     sc::CellStoreType& GetCellStore() { return maCells; }
     const sc::CellStoreType& GetCellStore() const { return maCells; }
     sc::CellTextAttrStoreType& GetCellAttrStore() { return maCellTextAttrs; }
