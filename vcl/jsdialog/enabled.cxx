b67502cb534638c0c550c20e9ca5d65b41988690
Skyler Grey <skyler3665@gmail.com>
2022-07-28 13:31:14 +0000

    Enable JSDialogs on dialogs where it works
    - I have tested some JSDialogs to see if they work
    - For those that did, I added them to the enable list
    - Some JSDialogs which I tested are not enabled here as they need a
      little more work to avoid them breaking
    - As part of my testing, I have tested these JSDialogs using multiple
      CODE windows accessing the same dialog at once
    
    Signed-off-by: Skyler Grey <skyler3665@gmail.com>
    Change-Id: If10789b14bb32b7e753cc0e002fda77333cfd3a2

--- vcl/jsdialog/enabled.cxx
+++ vcl/jsdialog/enabled.cxx
@@ -60,7 +60,11 @@ bool isBuilderEnabled(std::u16string_view rUIFile, bool bMobile)
         || rUIFile == u"svx/ui/accessibilitycheckentry.ui"
         || rUIFile == u"cui/ui/widgettestdialog.ui"
         || rUIFile == u"modules/swriter/ui/contentcontroldlg.ui"
-        || rUIFile == u"modules/swriter/ui/contentcontrollistitemdlg.ui")
+        || rUIFile == u"modules/swriter/ui/contentcontrollistitemdlg.ui"
+        || rUIFile == u"modules/swriter/ui/splittable.ui"
+        || rUIFile == u"modules/swriter/ui/sortdialog.ui"
+        || rUIFile == u"cui/ui/splitcellsdialog.ui"
+        || rUIFile == u"cui/ui/formatnumberdialog.ui" || rUIFile == u"cui/ui/numberingformatpage.ui")
     {
         return true;
     }
