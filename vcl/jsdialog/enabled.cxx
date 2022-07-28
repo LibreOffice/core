99c23255149f45c9acc914d2ac99337b374dca60
Skyler Grey <skyler3665@gmail.com>
2022-07-28 13:31:14 +0000

    Enable JSDialogs on dialogs where it works
    - I have tested some JSDialogs to see if they work
    - For those that did, I added them to the enable list
    - Some JSDialogs which I tested are not enabled here as they need a
      little more work to avoid them breaking
    
    Signed-off-by: Skyler Grey <skyler3665@gmail.com>
    Change-Id: If10789b14bb32b7e753cc0e002fda77333cfd3a2

--- vcl/jsdialog/enabled.cxx
+++ vcl/jsdialog/enabled.cxx
@@ -60,7 +60,9 @@ bool isBuilderEnabled(std::u16string_view rUIFile, bool bMobile)
         || rUIFile == u"svx/ui/accessibilitycheckentry.ui"
         || rUIFile == u"cui/ui/widgettestdialog.ui"
         || rUIFile == u"modules/swriter/ui/contentcontroldlg.ui"
-        || rUIFile == u"modules/swriter/ui/contentcontrollistitemdlg.ui")
+        || rUIFile == u"modules/swriter/ui/contentcontrollistitemdlg.ui"
+        || rUIFile == u"modules/swriter/ui/splittable.ui"
+        || rUIFile == u"modules/swriter/ui/sortdialog.ui")
     {
         return true;
     }
