db9eacf9a340aa95eae436f0185086658d2a8444
offtkp <parisoplop@gmail.com>
2022-07-30 01:47:54 +0300

    Remove code duplication in GraphicDescriptor for some formats
    Use GraphicFormatDetector for GIF, EMF/Z, WMF/Z, SVG(Z) formats to
    eventually remove all code duplication in GraphicDescriptor.
    
    Change-Id: I6e184b3ba52289db02e0d4eebeeadde0ce0433b4

--- vcl/inc/graphic/GraphicFormatDetector.hxx
+++ vcl/inc/graphic/GraphicFormatDetector.hxx
@@ -22,9 +22,159 @@
 
 #include <tools/stream.hxx>
 #include <vector>
-
+#include <vcl/mapmod.hxx>
+#include <tools/gen.hxx>
+#include <optional>
+//  Info class for all supported file formats
+enum class GraphicFileFormat
+{
+    NOT = 0x0000,
+    BMP = 0x0001,
+    GIF = 0x0002,
+    JPG = 0x0003,
+    PCD = 0x0004,
+    PCX = 0x0005,
+    PNG = 0x0006,
+    TIF = 0x0007,
+    XBM = 0x0008,
+    XPM = 0x0009,
+    PBM = 0x000a,
+    PGM = 0x000b,
+    PPM = 0x000c,
+    RAS = 0x000d,
+    TGA = 0x000e,
+    PSD = 0x000f,
+    EPS = 0x0010,
+    WEBP = 0x0011,
+    MOV = 0x00e0,
+    PDF = 0x00e1,
+    DXF = 0x00f1,
+    MET = 0x00f2,
+    PCT = 0x00f3,
+    // retired SGF = 0x00f4,
+    SVM = 0x00f5,
+    WMF = 0x00f6,
+    // retired SGV = 0x00f7,
+    EMF = 0x00f8,
+    SVG = 0x00f9,
+    WMZ = 0x00fa,
+    EMZ = 0x00fb,
+    SVGZ = 0x00fc
+};
+struct GraphicMetadata
+{
+    Size maPixSize{};
+    Size maLogSize{};
+    std::optional<Size> maPreferredLogSize = std::nullopt;
+    std::optional<MapMode> maPreferredMapMode = std::nullopt;
+    sal_uInt16 mnBitsPerPixel = 0;
+    sal_uInt16 mnPlanes = 0;
+    GraphicFileFormat mnFormat = GraphicFileFormat::NOT;
+    sal_uInt8 mnNumberOfImageComponents = 0;
+    bool mbIsTransparent = false;
+    bool mbIsAlpha = false;
+};
 namespace vcl
 {
+OUString getImportFormatShortName(GraphicFileFormat nFormat)
+{
+    const char* pKeyName = nullptr;
+
+    switch (nFormat)
+    {
+        case GraphicFileFormat::BMP:
+            pKeyName = "BMP";
+            break;
+        case GraphicFileFormat::GIF:
+            pKeyName = "GIF";
+            break;
+        case GraphicFileFormat::JPG:
+            pKeyName = "JPG";
+            break;
+        case GraphicFileFormat::PCD:
+            pKeyName = "PCD";
+            break;
+        case GraphicFileFormat::PCX:
+            pKeyName = "PCX";
+            break;
+        case GraphicFileFormat::PNG:
+            pKeyName = "PNG";
+            break;
+        case GraphicFileFormat::XBM:
+            pKeyName = "XBM";
+            break;
+        case GraphicFileFormat::XPM:
+            pKeyName = "XPM";
+            break;
+        case GraphicFileFormat::PBM:
+            pKeyName = "PBM";
+            break;
+        case GraphicFileFormat::PGM:
+            pKeyName = "PGM";
+            break;
+        case GraphicFileFormat::PPM:
+            pKeyName = "PPM";
+            break;
+        case GraphicFileFormat::RAS:
+            pKeyName = "RAS";
+            break;
+        case GraphicFileFormat::TGA:
+            pKeyName = "TGA";
+            break;
+        case GraphicFileFormat::PSD:
+            pKeyName = "PSD";
+            break;
+        case GraphicFileFormat::EPS:
+            pKeyName = "EPS";
+            break;
+        case GraphicFileFormat::TIF:
+            pKeyName = "TIF";
+            break;
+        case GraphicFileFormat::DXF:
+            pKeyName = "DXF";
+            break;
+        case GraphicFileFormat::MET:
+            pKeyName = "MET";
+            break;
+        case GraphicFileFormat::PCT:
+            pKeyName = "PCT";
+            break;
+        case GraphicFileFormat::SVM:
+            pKeyName = "SVM";
+            break;
+        case GraphicFileFormat::WMF:
+            pKeyName = "WMF";
+            break;
+        case GraphicFileFormat::EMF:
+            pKeyName = "EMF";
+            break;
+        case GraphicFileFormat::SVG:
+            pKeyName = "SVG";
+            break;
+        case GraphicFileFormat::WMZ:
+            pKeyName = "WMZ";
+            break;
+        case GraphicFileFormat::EMZ:
+            pKeyName = "EMZ";
+            break;
+        case GraphicFileFormat::SVGZ:
+            pKeyName = "SVGZ";
+            break;
+        case GraphicFileFormat::WEBP:
+            pKeyName = "WEBP";
+            break;
+        case GraphicFileFormat::MOV:
+            pKeyName = "MOV";
+            break;
+        case GraphicFileFormat::PDF:
+            pKeyName = "PDF";
+            break;
+        default:
+            assert(false);
+    }
+
+    return OUString::createFromAscii(pKeyName);
+}
 /***
  * This function is has two modes:
  * - determine the file format when bTest = false
@@ -49,9 +199,7 @@ public:
     sal_uInt64 mnStreamPosition;
     sal_uInt64 mnStreamLength;
 
-    OUString msDetectedFormat;
-
-    GraphicFormatDetector(SvStream& rStream, OUString aFormatExtension);
+    GraphicFormatDetector(SvStream& rStream, OUString aFormatExtension, bool bExtendedInfo = false);
 
     bool detect();
 
@@ -78,6 +226,7 @@ public:
     bool checkMOV();
     bool checkPDF();
     bool checkWEBP();
+    const GraphicMetadata& getMetadata();
 
 private:
     /**
@@ -91,6 +240,9 @@ private:
      */
     sal_uInt8* checkAndUncompressBuffer(sal_uInt8* aUncompressedBuffer, sal_uInt32 nSize,
                                         sal_uInt64& nDecompressedSize);
+    bool mbExtendedInfo;
+    GraphicMetadata maMetadata;
+    bool mbWasCompressed;
 };
 }
 
