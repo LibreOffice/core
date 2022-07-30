31c2a98082b98745f10da14ed8f389ae61af3484
Justin Luth <jluth@mail.com>
2022-07-30 09:38:09 -0400

    tdf#150197 sw: convert pre-defined num styles to ListFormat
    Early in 7.x, allotropia introduced a generic list format string
    instead of prefix/suffix. Convert the pre-defined numbering
    formats to use that syntax as well.
    
    The problem was that ms export wasn't able to handle the
    old way of doing things any more.
    For example, if a user added numbering via
    the numbering style "Numbering 123",
    then on export the numbering string was lost.
    
    Change-Id: I11cd00280da2b464a9b00a5f0a8d72080f14e44c
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/137632
    Reviewed-by: Justin Luth <jluth@mail.com>
    Tested-by: Justin Luth <jluth@mail.com>
    (cherry picked from commit a106d5f75acc4a955f21583f58604ea2e7b4ce1a)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/137606
    Tested-by: Jenkins
    Reviewed-by: Vasily Melenchuk <vasily.melenchuk@cib.de>
    (cherry picked from commit 5d72a000a0f9d127d1263a6df628898af08f14f9)

---
+++ sw/qa/extras/ooxmlexport/ooxmlexport18.cxx
@@ -0,0 +1,61 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <swmodeltestbase.hxx>
+
+#include <queue>
+
+#include <com/sun/star/beans/NamedValue.hpp>
+#include <com/sun/star/drawing/XShapes.hpp>
+#include <com/sun/star/frame/XStorable.hpp>
+#include <com/sun/star/text/XFootnotesSupplier.hpp>
+#include <com/sun/star/text/XTextDocument.hpp>
+#include <com/sun/star/text/XTextFieldsSupplier.hpp>
+#include <com/sun/star/text/XTextField.hpp>
+#include <com/sun/star/util/XRefreshable.hpp>
+
+
+#include <comphelper/propertysequence.hxx>
+#include <comphelper/scopeguard.hxx>
+#include <comphelper/sequenceashashmap.hxx>
+#include <o3tl/string_view.hxx>
+#include <comphelper/propertyvalue.hxx>
+
+#include <unotxdoc.hxx>
+#include <docsh.hxx>
+
+constexpr OUStringLiteral DATA_DIRECTORY = u"/sw/qa/extras/ooxmlexport/data/";
+
+class Test : public SwModelTestBase
+{
+public:
+    Test() : SwModelTestBase(DATA_DIRECTORY, "Office Open XML Text") {}
+};
+
+CPPUNIT_TEST_FIXTURE(Test, testTdf150197_predefinedNumbering)
+{
+    createSwDoc();
+
+    // The exact numbering style doesn't matter - just any non-bullet pre-defined numbering style.
+    uno::Sequence<beans::PropertyValue> aPropertyValues = comphelper::InitPropertySequence({
+        { "Style", uno::Any(OUString("Numbering 123")) },
+        { "FamilyName", uno::Any(OUString("NumberingStyles")) },
+    });
+    dispatchCommand(mxComponent, ".uno:StyleApply", aPropertyValues);
+
+    CPPUNIT_ASSERT_EQUAL(OUString("1."), getProperty<OUString>(getParagraph(1), "ListLabelString"));
+
+    reload("Office Open XML Text", "");
+    CPPUNIT_ASSERT_EQUAL(OUString("1."), getProperty<OUString>(getParagraph(1), "ListLabelString"));
+}
+
+
+CPPUNIT_PLUGIN_IMPLEMENT();
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
