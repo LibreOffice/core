42e953a59cdcace9c631867a6deecacb76450678
Justin Luth <jluth@mail.com>
2022-07-30 09:38:09 -0400

    tdf#150197 sw: convert pre-defined num styles to ListFormat
    Early in 7.x, allotropia introduced a generic list format string
    instead of prefix/suffix. Convert the pre-defined numbering
    formats to use that syntax as well.
    
    The problem was that ms export wasn't able to handle the
    old way of doing things any more.
    For example, if a user added numbering via
    the numbering style "Numbering 123",
    then on export the numbering string was lost.
    
    Change-Id: I11cd00280da2b464a9b00a5f0a8d72080f14e44c
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/137632
    Reviewed-by: Justin Luth <jluth@mail.com>
    Tested-by: Justin Luth <jluth@mail.com>
    (cherry picked from commit a106d5f75acc4a955f21583f58604ea2e7b4ce1a)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/137606
    Tested-by: Jenkins
    Reviewed-by: Vasily Melenchuk <vasily.melenchuk@cib.de>
    (cherry picked from commit 5d72a000a0f9d127d1263a6df628898af08f14f9)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/137899
    Reviewed-by: Andras Timar <andras.timar@collabora.com>
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

--- sw/Module_sw.mk
+++ sw/Module_sw.mk
@@ -89,6 +89,7 @@ $(eval $(call gb_Module_add_slowcheck_targets,sw,\
     CppunitTest_sw_ooxmlexport15 \
     CppunitTest_sw_ooxmlexport16 \
     CppunitTest_sw_ooxmlexport17 \
+    CppunitTest_sw_ooxmlexport18 \
     CppunitTest_sw_ooxmlexport_template \
     CppunitTest_sw_ooxmlfieldexport \
     CppunitTest_sw_ooxmllinks \
