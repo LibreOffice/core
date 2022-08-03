aacea957144ea85b16674af6db33142b0f2b16f3
Stephan Bergmann <sbergman@redhat.com>
2022-08-03 17:08:35 +0200

    external/redland: Fix -Wint-conversion
    ...with recent Clang 16 trunk since
    <https://github.com/llvm/llvm-project/commit/7068aa98412ade19a34b7ed126f4669f581b2311>
    "Strengthen -Wint-conversion to default to an error", causing
    
    > raptor_parse.c:260:66: error: incompatible pointer to integer conversion returning 'void *' from a function with result type 'int' [-Wint-conversion]
    >   RAPTOR_ASSERT_OBJECT_POINTER_RETURN_VALUE(world, raptor_world, NULL);
    >                                                                  ^~~~
    
    and
    
    > raptor_serialize.c:243:66: error: incompatible pointer to integer conversion returning 'void *' from a function with result type 'int' [-Wint-conversion]
    >   RAPTOR_ASSERT_OBJECT_POINTER_RETURN_VALUE(world, raptor_world, NULL);
    >                                                                  ^~~~
    
    Change-Id: I2c1083b78777589b992c180d8759e2313bf2fe42
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/137756
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>
    (cherry picked from commit 87eff817eb723e249783f16bf69a8b4cef53e3cb)
    (cherry picked from commit cc81ab5dfb9a5a8346b95421f650ca92d37dee53)

--- external/redland/UnpackedTarball_raptor.mk
+++ external/redland/UnpackedTarball_raptor.mk
@@ -30,6 +30,7 @@ $(eval $(call gb_UnpackedTarball_add_patches,raptor,\
 	external/redland/raptor/0001-Calcualte-max-nspace-declarations-correctly-for-XML-.patch.1 \
 	external/redland/raptor/0001-CVE-2020-25713-raptor2-malformed-input-file-can-lead.patch.1 \
 	external/redland/raptor/libtool.patch \
+	external/redland/raptor/Wint-conversion.patch \
 	external/redland/raptor/raptor-libxml2-11.patch.1 \
 	external/redland/raptor/CVE-2024-57823.patch.1 \
 ))
