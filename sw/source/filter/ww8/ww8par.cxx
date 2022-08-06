LibreOffice/core

commit 96fb8f88bd56c113eb23e50d23a038509703c288

elide some SwNodeIndex temporaries
Change-Id: I5391aa409d760870ead26d43b2287b23d2d40089
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/137776
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sw/source/filter/ww8/ww8par.cxx
@@ -1555,8 +1555,7 @@ void SwWW8FltRefStack::SetAttrInDoc(const SwPosition& rTmpPos,
         case RES_TXTATR_ANNOTATION:
         case RES_TXTATR_INPUTFIELD:
         {
-            SwNodeIndex aIdx(rEntry.m_aMkPos.m_nNode, 1);
-            SwPaM aPaM(aIdx, rEntry.m_aMkPos.m_nContent);
+            SwPaM aPaM(rEntry.m_aMkPos.m_nNode.GetNode(), SwNodeOffset(1), rEntry.m_aMkPos.m_nContent);
 
             SwFormatField& rFormatField   = *static_cast<SwFormatField*>(rEntry.m_pAttr.get());
             SwField* pField = rFormatField.GetField();
@@ -4691,8 +4690,7 @@ void wwSectionManager::InsertSegments()
 
         if (pTextNd)
         {
-            SwNodeIndex aIdx(*pTextNd);
-            SwPaM aTest(aIdx);
+            SwPaM aTest(*pTextNd);
             mrReader.m_rDoc.getIDocumentContentOperations().DelFullPara(aTest);
             pTextNd = nullptr;
         }
@@ -4708,8 +4706,7 @@ void wwExtraneousParas::delete_all_from_doc()
         SwTextNode* pTextNode = rListener.GetTextNode();
         rListener.StopListening(pTextNode);
 
-        SwNodeIndex aIdx(*pTextNode);
-        SwPaM aTest(aIdx);
+        SwPaM aTest(*pTextNode);
         m_rDoc.getIDocumentContentOperations().DelFullPara(aTest);
     }
     m_aTextNodes.clear();
@@ -6358,8 +6355,7 @@ bool TestImportDOC(SvStream &rStream, const OUString &rFltName)
     xDocSh->DoInitNew();
     SwDoc *pD =  static_cast<SwDocShell*>((&xDocSh))->GetDoc();
 
-    SwNodeIndex aIdx(pD->GetNodes().GetEndOfContent(), -1);
-    SwPaM aPaM(aIdx);
+    SwPaM aPaM(pD->GetNodes().GetEndOfContent(), -1);
     pD->SetInReading(true);
     bool bRet = xReader->Read(*pD, OUString(), aPaM, OUString()) == ERRCODE_NONE;
     pD->SetInReading(false);

sw/inc/pam.hxx
@@ -192,6 +192,9 @@ public:
             const SwNodeIndex& rPt, sal_Int32 nPtContent, SwPaM* pRing = nullptr );
     SwPaM(  const SwNode& rMk, sal_Int32 nMkContent,
             const SwNode& rPt, sal_Int32 nPtContent, SwPaM* pRing = nullptr );
+    SwPaM(  const SwNode& rMk, SwNodeOffset nMkOffset, sal_Int32 nMkContent,
+            const SwNode& rPt, SwNodeOffset nPtOffset, sal_Int32 nPtContent, SwPaM* pRing = nullptr );
+    SwPaM( const SwNode& rNd, SwNodeOffset nNdOffset, sal_Int32 nContent = 0, SwPaM* pRing = nullptr );
     SwPaM( const SwNode& rNd, sal_Int32 nContent = 0, SwPaM* pRing = nullptr );
     SwPaM( const SwNodeIndex& rNd, sal_Int32 nContent = 0, SwPaM* pRing = nullptr );
     virtual ~SwPaM() override;

sw/source/core/crsr/pam.cxx
@@ -467,9 +467,44 @@ SwPaM::SwPaM( const SwNode& rMark, sal_Int32 nMarkContent,
         nMarkContent );
 }
 
+SwPaM::SwPaM( const SwNode& rMark, SwNodeOffset nMarkOffset, sal_Int32 nMarkContent,
+              const SwNode& rPoint, SwNodeOffset nPointOffset, sal_Int32 nPointContent, SwPaM* pRing )
+    : Ring( pRing )
+    , m_Bound1( rMark )
+    , m_Bound2( rPoint )
+    , m_pPoint( &m_Bound2 )
+    , m_pMark( &m_Bound1 )
+    , m_bIsInFrontOfLabel( false )
+{
+    if ( nMarkOffset )
+    {
+        m_pMark->nNode += nMarkOffset;
+    }
+    if ( nPointOffset )
+    {
+        m_pPoint->nNode += nPointOffset;
+    }
+    m_pPoint->nContent.Assign( m_pPoint->GetNode().GetContentNode(),
+        nPointContent);
+    m_pMark ->nContent.Assign( m_pMark ->GetNode().GetContentNode(),
+        nMarkContent );
+}
+
 SwPaM::SwPaM( const SwNode& rNode, sal_Int32 nContent, SwPaM* pRing )
     : Ring( pRing )
     , m_Bound1( rNode )
+    , m_Bound2( m_Bound1.GetNode().GetNodes() ) // default initialize
+    , m_pPoint( &m_Bound1 )
+    , m_pMark( &m_Bound1 )
+    , m_bIsInFrontOfLabel( false )
+{
+    m_pPoint->nContent.Assign( m_pPoint->GetNode().GetContentNode(),
+        nContent );
+}
+
+SwPaM::SwPaM( const SwNode& rNode, SwNodeOffset nNdOffset, sal_Int32 nContent, SwPaM* pRing )
+    : Ring( pRing )
+    , m_Bound1( rNode, nNdOffset )
     , m_Bound2( m_Bound1.GetNode().GetNodes() ) // default initialize
     , m_pPoint( &m_Bound1 )
     , m_pMark( &m_Bound1 )

sw/source/core/doc/docnew.cxx
@@ -685,7 +685,7 @@ void SwDoc::ClearDoc()
         pFirstNd->SetAttr( SwFormatPageDesc( pDummyPgDsc ));
 
         SwPosition aPos( *pFirstNd );
-        SwPaM const tmpPaM(aSttIdx, SwNodeIndex(GetNodes().GetEndOfContent()));
+        SwPaM const tmpPaM(aSttIdx, GetNodes().GetEndOfContent());
         ::PaMCorrAbs(tmpPaM, aPos);
     }
 
@@ -1151,8 +1151,7 @@ SwNodeIndex SwDoc::AppendDoc(const SwDoc& rSource, sal_uInt16 const nStartPageNu
     SwNodeIndex aFixupIdx( GetNodes().GetEndOfContent(), -1 );
 
     // append at the end of document / content
-    SwNodeIndex aTargetIdx( GetNodes().GetEndOfContent() );
-    SwPaM aInsertPam( aTargetIdx );
+    SwPaM aInsertPam( GetNodes().GetEndOfContent() );
 
 #ifdef DBG_UTIL
     SAL_INFO( "sw.docappend", "Pam-Nd: " << aCpyPam.GetNode().GetIndex() - aCpyPam.GetNode( false ).GetIndex() + 1

sw/source/core/doc/docredln.cxx
@@ -783,8 +783,7 @@ bool SwRedlineTable::isMoved( size_type rPos ) const
     }
     else // otherwise it is saved in pContentSect, e.g. during ODT import
     {
-        SwNodeIndex aTmpIdx( *pRedline->GetContentIdx()->GetNode().EndOfSectionNode() );
-        pPaM = new SwPaM(*pRedline->GetContentIdx(), aTmpIdx );
+        pPaM = new SwPaM(*pRedline->GetContentIdx(), *pRedline->GetContentIdx()->GetNode().EndOfSectionNode() );
         bDeletePaM = true;
     }
 
@@ -823,8 +822,7 @@ bool SwRedlineTable::isMoved( size_type rPos ) const
         else // otherwise it is saved in pContentSect, e.g. during ODT import
         {
             // saved in pContentSect, e.g. during ODT import
-            SwNodeIndex aTmpIdx( *pPair->GetContentIdx()->GetNode().EndOfSectionNode() );
-            pPairPaM = new SwPaM(*pPair->GetContentIdx(), aTmpIdx );
+            pPairPaM = new SwPaM(*pPair->GetContentIdx(), *pPair->GetContentIdx()->GetNode().EndOfSectionNode() );
             bDeletePairPaM = true;
         }
 
@@ -1983,8 +1981,7 @@ OUString SwRangeRedline::GetDescr(bool bSimplified)
     }
     else // otherwise it is saved in pContentSect
     {
-        SwNodeIndex aTmpIdx( *m_pContentSect->GetNode().EndOfSectionNode() );
-        pPaM = new SwPaM(*m_pContentSect, aTmpIdx );
+        pPaM = new SwPaM( m_pContentSect->GetNode(), *m_pContentSect->GetNode().EndOfSectionNode() );
         bDeletePaM = true;
     }
 

sw/source/core/doc/tblrwcl.cxx
@@ -595,8 +595,7 @@ bool SwTable::InsertRow_( SwDoc* pDoc, const SwSelBoxes& rBoxes,
                 SvxPrintItem aSetTracking(RES_PRINT, false);
                 SwPosition aPos(*pNewTableLine->GetTabBoxes()[0]->GetSttNd());
                 SwCursor aCursor( aPos, nullptr );
-                SwNodeIndex aInsPos(*pNewTableLine->GetTabBoxes()[0]->GetSttNd(), 1 );
-                SwPaM aPaM(aInsPos);
+                SwPaM aPaM(*pNewTableLine->GetTabBoxes()[0]->GetSttNd(), SwNodeOffset(1));
                 pDoc->getIDocumentContentOperations().InsertString( aPaM,
                         OUStringChar(CH_TXT_TRACKED_DUMMY_CHAR) );
                 pDoc->SetRowNotTracked( aCursor, aSetTracking );

sw/source/core/docnode/ndtbl.cxx
@@ -1216,7 +1216,8 @@ const SwTable* SwDoc::TextToTable( const std::vector< std::vector<SwNodeRange> >
             }
         }
         // another one to break between last cell and node after table
-        SwPaM pam(SwNodeIndex(*pPrev, +1), 0, *pPrev,
+        SwPaM pam(pPrev->GetNode(), SwNodeOffset(+1), 0,
+                  pPrev->GetNode(), SwNodeOffset(0),
                     (pPrev->GetNode().IsContentNode())
                         ? pPrev->GetNode().GetContentNode()->Len() : 0);
         rIDRA.SplitRedline(pam);
@@ -2023,7 +2024,7 @@ bool SwDoc::DeleteRowCol(const SwSelBoxes& rBoxes, RowColMode const eMode)
             }
 
             // Save the cursors (UNO and otherwise)
-            SwPaM aSavePaM( SwNodeIndex( *pTableNd->EndOfSectionNode() ) );
+            SwPaM aSavePaM( *pTableNd->EndOfSectionNode() );
             if( ! aSavePaM.Move( fnMoveForward, GoInNode ) )
             {
                 *aSavePaM.GetMark() = SwPosition( *pTableNd );
@@ -2073,7 +2074,7 @@ bool SwDoc::DeleteRowCol(const SwSelBoxes& rBoxes, RowColMode const eMode)
             }
 
             // Save the cursors (UNO and otherwise)
-            SwPaM aSavePaM( SwNodeIndex( *pTableNd->EndOfSectionNode() ) );
+            SwPaM aSavePaM( *pTableNd->EndOfSectionNode() );
             if( ! aSavePaM.Move( fnMoveForward, GoInNode ) )
             {
                 *aSavePaM.GetMark() = SwPosition( *pTableNd );

sw/source/core/frmedt/fecopy.cxx
@@ -1093,8 +1093,7 @@ bool SwFEShell::Paste(SwDoc& rClpDoc, bool bNestedTable)
                     // Note: aCpyPam is invalid now
 
                     ++aIndexBefore;
-                    SwPaM aPaM(SwPosition(aIndexBefore),
-                           SwPosition(rInsPos.nNode));
+                    SwPaM aPaM(aIndexBefore, rInsPos.nNode);
 
                     aPaM.GetDoc().MakeUniqueNumRules(aPaM);
 
@@ -1193,8 +1192,7 @@ void SwFEShell::PastePages( SwFEShell& rToFill, sal_uInt16 nStartPage, sal_uInt1
         //remove the inserted paragraph
         Undo();
         //remove the paragraph in the second doc, too
-        SwNodeIndex aIdx( rToFill.GetDoc()->GetNodes().GetEndOfExtras(), 2 );
-        SwPaM aPara( aIdx ); //DocStart
+        SwPaM aPara( rToFill.GetDoc()->GetNodes().GetEndOfExtras(), SwNodeOffset(2) ); //DocStart
         rToFill.GetDoc()->getIDocumentContentOperations().DelFullPara(aPara);
     }
     // now the page bound objects

commit 39fdec4ed07517a16392bff1db856d403037f818

ofz#49834 Segv on unknown address
regression from

commit 96fb8f88bd56c113eb23e50d23a038509703c288
Date:   Wed Aug 3 15:47:51 2022 +0200

    elide some SwNodeIndex temporaries

where the SwPam ctor a ended up getting called instead of b

a) SwPaM( const SwNode& rNode, sal_Int32 nContent, SwPaM* pRing )
b) SwPaM( const SwNode& rNd, SwNodeOffset nNdOffset, sal_Int32 nContent = 0, SwPaM* pRing = nullptr );

Change-Id: I44e9f98bcbefa72301dc9a434ce5ebe720870cab

sw/source/filter/ww8/ww8par.cxx
@@ -6351,7 +6351,7 @@ bool TestImportDOC(SvStream &rStream, const OUString &rFltName)
     xDocSh->DoInitNew();
     SwDoc *pD =  static_cast<SwDocShell*>((&xDocSh))->GetDoc();
 
-    SwPaM aPaM(pD->GetNodes().GetEndOfContent(), -1);
+    SwPaM aPaM(pD->GetNodes().GetEndOfContent(), SwNodeOffset(-1));
     pD->SetInReading(true);
     bool bRet = xReader->Read(*pD, OUString(), aPaM, OUString()) == ERRCODE_NONE;
     pD->SetInReading(false);