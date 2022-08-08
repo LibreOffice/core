fb992aefc8345a9be780c6928e4380da7c56904e
Stephan Bergmann <sbergman@redhat.com>
2022-08-08 14:09:42 +0200

    Adapt to changes in llvm::Optional
    <https://github.com/llvm/llvm-project/commit/3c49576417bab1b07764aa0b22664cbc8d3c3f53>
    "[ADT] Add has_value, value, value_or to llvm::Optional" is in Clang 15, and
    <https://github.com/llvm/llvm-project/commit/b5f8d42efe3e246d582d4a1a328fac915e4ce8dc>
    "[ADT] Deprecate Optional::{hasValue,getValue} (NFC)" is in Clang 16 trunk,
    causing
    
    > In file included from compilerplugins/clang/sharedvisitor/sharedvisitor.cxx:95:
    > compilerplugins/clang/pointerbool.cxx:118:21: error: 'hasValue' is deprecated: Use has_value instead. [-Werror,-Wdeprecated-declarations]
    >             if (ret.hasValue() && (ret.getValue() == 1 || ret.getValue() == 0))
    >                     ^
    > ~/llvm/inst/include/llvm/ADT/Optional.h:324:5: note: 'hasValue' has been explicitly marked deprecated here
    >   [[deprecated("Use has_value instead.")]] constexpr bool hasValue() const {
    >     ^
    
    etc.
    
    Change-Id: I377effe29fc6752484d2870b9a0fd66fddc26bfc
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/137982
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

--- compilerplugins/clang/pointerbool.cxx
+++ compilerplugins/clang/pointerbool.cxx
@@ -115,7 +115,7 @@ bool PointerBool::VisitCallExpr(CallExpr const* callExpr)
         if (arg->getType()->isIntegerType())
         {
             auto ret = getCallValue(arg);
-            if (ret.hasValue() && (ret.getValue() == 1 || ret.getValue() == 0))
+            if (compat::has_value(ret) && (compat::value(ret) == 1 || compat::value(ret) == 0))
                 continue;
             // something like: priv->m_nLOKFeatures & LOK_FEATURE_DOCUMENT_PASSWORD
             if (isa<BinaryOperator>(arg->IgnoreParenImpCasts()))
