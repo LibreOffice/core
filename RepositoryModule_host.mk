0d56a3f339c75a75e1bea18efc6774d062009398
Balazs Varga <balazs.varga.extern@allotropia.de>
2022-08-08 10:49:20 +0200

    WIP: WASM activate and fix the Calc build and running
    It was needed to allow accessibility for wasm calc build,
    due to many dependences in calc.
    
    This commit also revert: 26603bc9ef0116ed31c510dab82b69d3666447b5
    (WASM fix orcus native exception handling (NEH))
    Because Calc import using liborcus for loading styles.xml.
    
    Change-Id: I4c330ef8eea7d08214bf357531ee0bf7383ab788

--- RepositoryModule_host.mk
+++ RepositoryModule_host.mk
@@ -50,14 +50,12 @@ endif
 
 $(eval $(call gb_Module_add_moduledirs,libreoffice,\
 	android \
-    $(if $(ENABLE_WASM_STRIP_BASIC_CALC_DRAW_MATH_IMPRESS),, \
+    $(if $(ENABLE_WASM_STRIP_BASIC_DRAW_MATH_IMPRESS),, \
 	animations \
     ) \
 	apple_remote \
 	avmedia \
-    $(if $(ENABLE_WASM_STRIP_BASIC_CALC_DRAW_MATH_IMPRESS),, \
 	basctl \
-    ) \
 	basegfx \
 	basic \
 	bean \
@@ -129,28 +127,26 @@ $(eval $(call gb_Module_add_moduledirs,libreoffice,\
 	sal \
 	salhelper \
 	sax \
-    $(if $(ENABLE_WASM_STRIP_BASIC_CALC_DRAW_MATH_IMPRESS),, \
 	sc \
 	scaddins \
 	sccomp \
-    ) \
 	$(call gb_Helper_optional,DESKTOP,scp2) \
 	scripting \
-    $(if $(ENABLE_WASM_STRIP_BASIC_CALC_DRAW_MATH_IMPRESS),, \
+    $(if $(ENABLE_WASM_STRIP_BASIC_DRAW_MATH_IMPRESS),, \
 	sd \
 	sdext \
     ) \
 	$(call gb_Helper_optional,DESKTOP,setup_native) \
 	sfx2 \
 	shell \
-    $(if $(ENABLE_WASM_STRIP_BASIC_CALC_DRAW_MATH_IMPRESS),, \
+    $(if $(ENABLE_WASM_STRIP_BASIC_DRAW_MATH_IMPRESS),, \
 	slideshow \
     ) \
 	smoketest \
 	solenv \
 	soltools \
 	sot \
-    $(if $(ENABLE_WASM_STRIP_BASIC_CALC_DRAW_MATH_IMPRESS),, \
+    $(if $(ENABLE_WASM_STRIP_BASIC_DRAW_MATH_IMPRESS),, \
 	starmath \
     ) \
     $(if $(ENABLE_CUSTOMTARGET_COMPONENTS),static) \
