ca7288c38e1ebcf017205462b7c919c3d0b7abbb
Andreas Heinisch <andreas.heinisch@yahoo.de>
2022-08-09 20:05:43 +0200

    tdf#131326 - Invalidate cell slots and update input line with new content
    Change-Id: I4881b73bb230380388ed366818d7e0808aec11e9

---
+++ sc/qa/uitest/inputLine/tdf131326.py
@@ -0,0 +1,26 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+from uitest.framework import UITestCase
+from uitest.uihelper.calc import enter_text_to_cell
+from uitest.uihelper.common import get_state_as_dict
+
+# Bug 131326 - Formula to Value does not automatically change text in input line
+class tdf131326(UITestCase):
+
+    def test_tdf131326_formula_to_value_input_line(self):
+        with self.ui_test.create_doc_in_start_center("calc"):
+            xCalcDoc = self.xUITest.getTopFocusWindow()
+            gridwin = xCalcDoc.getChild("grid_window")
+            xInputWin = xCalcDoc.getChild("sc_input_window")
+
+            enter_text_to_cell(gridwin, "A1", "=1")
+            self.xUITest.executeCommand(".uno:ConvertFormulaToValue")
+            self.assertEqual(get_state_as_dict(xInputWin)["Text"], "1")
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
