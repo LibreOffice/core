2af5240940a52720e5ce8323438879006de0ea89
Andrea Gelmini <andrea.gelmini@gelma.net>
2022-08-10 11:33:34 +0200

    Removed duplicated included & move to pragma once
    Change-Id: If936a1b105f68f234a6537a4a9e9ab9661714a78

--- sw/source/filter/html/htmlfly.hxx
+++ sw/source/filter/html/htmlfly.hxx
@@ -17,10 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_SW_SOURCE_FILTER_HTML_HTMLFLY_HXX
-#define INCLUDED_SW_SOURCE_FILTER_HTML_HTMLFLY_HXX
-
-#include <ndindex.hxx>
+#pragma once
 
 #include <o3tl/sorted_vector.hxx>
 #include <o3tl/typed_flags_set.hxx>
@@ -129,7 +126,5 @@ class SwHTMLPosFlyFrames
                 o3tl::less_uniqueptr_to<SwHTMLPosFlyFrame>,
                 o3tl::find_partialorder_ptrequals>
 {};
-
-#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
