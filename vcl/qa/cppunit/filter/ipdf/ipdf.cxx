f2fcf1370d92545e31ed511ae5afba3b79eb495f
Caolán McNamara <caolanm@redhat.com>
2022-08-10 09:00:34 +0100

    some --disable-pdfium fixes
    Change-Id: I33867397cd5783adb90e9dc2c62b037ced131e26

--- vcl/qa/cppunit/filter/ipdf/ipdf.cxx
+++ vcl/qa/cppunit/filter/ipdf/ipdf.cxx
@@ -128,6 +128,8 @@ CPPUNIT_TEST_FIXTURE(VclFilterIpdfTest, testPDFAddVisibleSignatureLastPage)
 
     // Then: count the # of shapes on the signature widget/annotation.
     std::shared_ptr<vcl::pdf::PDFium> pPDFium = vcl::pdf::PDFiumLibrary::get();
+    if (!pPDFium)
+        return;
     SvFileStream aFile(aTempFile.GetURL(), StreamMode::READ);
     SvMemoryStream aMemory;
     aMemory.WriteStream(aFile);
