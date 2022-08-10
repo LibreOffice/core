15c1d42e94666a1ccb3acc6a428ecfc7f34fbedb
Justin Luth <jluth@mail.com>
2022-08-10 14:00:47 -0400

    NFC related tdf#85592: simplify SID_ATTR_CHAR_COLOR_BACKGROUND_EXT
    I am tring to replace Writer-only uno:BackColor
    with uno:CharBackColor. The problem is this template flag
    which gets calls in svx and then does nothing in editeng.
    
    But I don't see any reason why this call should be
    transformed into a template (_ext) call.
    We can easily just keep the same call
    and do the state-checking only slightly different.
    All this other stuff makes _EXT seem much more
    complicated than it is.
    
    If I am right that this is a Non-Functional-Change,
    then I am well on my way to fixing this bug report.
    
    Change-Id: Idc08117217837f217ea5ca390005802c2fad392b

--- sw/source/uibase/shells/textsh1.cxx
+++ sw/source/uibase/shells/textsh1.cxx
@@ -1275,11 +1275,11 @@ void SwTextShell::Execute(SfxRequest &rReq)
                     rWrtSh.SetAttrItem(
                         SvxColorItem(aSet, RES_CHRATR_COLOR) );
             }
-            else if (nSlot == SID_ATTR_CHAR_COLOR_BACKGROUND)
+            else if (nSlot == SID_ATTR_CHAR_COLOR_BACKGROUND_EXT)
             {
-                if (!pApply || pApply->nColor != SID_ATTR_CHAR_COLOR_BACKGROUND_EXT)
+                if (!pApply || pApply->nColor != SID_ATTR_CHAR_COLOR_BACKGROUND)
                 {
-                    aTempl.nColor = SID_ATTR_CHAR_COLOR_BACKGROUND_EXT;
+                    aTempl.nColor = SID_ATTR_CHAR_COLOR_BACKGROUND;
                     rEdtWin.SetApplyTemplate(aTempl);
                 }
             }
@@ -1847,6 +1847,12 @@ void SwTextShell::GetState( SfxItemSet &rSet )
             }
             break;
         case SID_ATTR_CHAR_COLOR_BACKGROUND_EXT:
+            {
+                SwEditWin& rEdtWin = GetView().GetEditWin();
+                SwApplyTemplate* pApply = rEdtWin.GetApplyTemplate();
+                rSet.Put(SfxBoolItem(nWhich, pApply && pApply->nColor == SID_ATTR_CHAR_COLOR_BACKGROUND));
+            }
+            break;
         case SID_ATTR_CHAR_COLOR_EXT:
             {
                 SwEditWin& rEdtWin = GetView().GetEditWin();
