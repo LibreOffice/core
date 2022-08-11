f4f869400922e8803f4ece673f26a31983be052b
Caolán McNamara <caolanm@redhat.com>
2022-08-11 20:54:09 +0100

    cid#1507888 Uninitialized scalar field
    Change-Id: I64747e43f76d0f214efc4bd964795517561d9693
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/138163
    Tested-by: Caolán McNamara <caolanm@redhat.com>
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>

--- vcl/inc/graphic/GraphicFormatDetector.hxx
+++ vcl/inc/graphic/GraphicFormatDetector.hxx
@@ -193,8 +193,8 @@ private:
     sal_uInt8* checkAndUncompressBuffer(sal_uInt8* aUncompressedBuffer, sal_uInt32 nSize,
                                         sal_uInt64& nDecompressedSize);
     bool mbExtendedInfo;
-    GraphicMetadata maMetadata;
     bool mbWasCompressed;
+    GraphicMetadata maMetadata;
 };
 }
 
