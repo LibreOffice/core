da04d9d3e5c321d85fe4b66d14d8f6a5b3040e21
Noel Grandin <noel.grandin@collabora.co.uk>
2022-08-11 12:14:13 +0200

    fix tsan data-race
    this check is not necessary, we are using a 32/64 bit counter (depending
    on platform) for some years now
    
    Change-Id: I3e51db3a8a0a18fee1647657369ecebf5de108e7

--- sc/inc/formularesult.hxx
+++ sc/inc/formularesult.hxx
@@ -55,20 +55,6 @@ class ScFormulaResult
     static const Multiline MULTILINE_FALSE   = 1;
     static const Multiline MULTILINE_TRUE    = 2;
 
-    // Clone token if the 16-bit only reference counter is nearing it's
-    // capacity during fill or copy&paste, leaving 4k for temporary passing
-    // around. (That should be enough for all times (TM) ;-)
-    static const sal_uInt16 MAX_TOKENREF_COUNT = 0xf000;
-    static void IncrementTokenRef( const formula::FormulaToken* & rp )
-    {
-        if (rp)
-        {
-            if (rp->GetRef() >= MAX_TOKENREF_COUNT)
-                rp = rp->Clone();
-            rp->IncRef();
-        }
-    }
-
     union
     {
         double          mfValue;    // double result direct for performance and memory consumption
