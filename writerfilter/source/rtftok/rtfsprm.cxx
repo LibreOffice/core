c63093aad97550f7d7a6698f72956656d39ce659
Michael Stahl <michael.stahl@allotropia.de>
2022-08-12 14:10:31 +0200

    tdf#150382 writerfilter,sw: RTF import of paragraph border override
    The style (default unnamed \s0, applied via \pard) defines green
    borders, which are overridden at the 2nd paragraph and cleared.
    
    Of course there isn't a single border control word to be found on the
    2nd paragraph, the override happens by omitting the borders of the
    implicitly applied style.
    
    Hence handle LN_CT_PrBase_pBdr in getDefaultSPRM().
    
    The export of the override relies on changes from commit
    967a03eb8760fb498c5ea6c32d03d1eea486bbe2.
    
    Change-Id: I081eb2908d76123e7828cab6c31ceb7b11760183
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/138193
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
    (cherry picked from commit 8f76052839b8558149d1effa28bd7b1b412d78d8)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/138277
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>

--- writerfilter/source/rtftok/rtfsprm.cxx
+++ writerfilter/source/rtftok/rtfsprm.cxx
@@ -199,6 +199,22 @@ static RTFValue::Pointer_t getDefaultSPRM(Id const id, Id nStyleType)
                 // presumably this means 100%, cf. static const int nSingleLineSpacing = 240;
                 return new RTFValue(240);
 
+            case NS_ooxml::LN_CT_PrBase_pBdr:
+            { // tdf#150382 default all paragraph borders to none
+                RTFSprms attributes;
+                RTFSprms sprms;
+                for (int i = 0; i < 4; ++i)
+                {
+                    auto const nBorder = getParagraphBorder(i);
+                    RTFSprms aAttributes;
+                    RTFSprms aSprms;
+                    aAttributes.set(NS_ooxml::LN_CT_Border_val,
+                                    new RTFValue(NS_ooxml::LN_Value_ST_Border_none));
+                    sprms.set(nBorder, new RTFValue(aAttributes, aSprms));
+                }
+                return new RTFValue(attributes, sprms);
+            }
+
             default:
                 break;
         }
