cfba0328750e1362d0ffe06119eabead75c28fc2
Skyler Grey <skyler3665@gmail.com>
2022-08-12 10:31:42 +0100

    Make the number format dialog an async jsdialog
    - The dialog needs to be async in order for multiple people to be able
      to use it at once
    - If we don't make the item set a shared pointer, we will crash out with
      an error when we try to copy it after the OK button is pressed
    - As the dialog is tabbed, we need to enable the dialog for all UI files
      that the dialog uses rather than just the main dialog UI file
    
    Change-Id: I8d684e9e9ad49b8a85ee940864d7219b4115a6e4

--- vcl/jsdialog/enabled.cxx
+++ vcl/jsdialog/enabled.cxx
@@ -62,7 +62,8 @@ bool isBuilderEnabled(std::u16string_view rUIFile, bool bMobile)
         || rUIFile == u"modules/swriter/ui/contentcontroldlg.ui"
         || rUIFile == u"modules/swriter/ui/contentcontrollistitemdlg.ui"
         || rUIFile == u"modules/swriter/ui/splittable.ui"
-        || rUIFile == u"cui/ui/splitcellsdialog.ui")
+        || rUIFile == u"cui/ui/splitcellsdialog.ui" || rUIFile == u"cui/ui/formatnumberdialog.ui"
+        || rUIFile == u"cui/ui/numberingformatpage.ui")
     {
         return true;
     }
