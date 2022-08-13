d6eab0cfbe885df65a94bc1aa62c25b6d80d0ed3
Khaled Hosny <khaled@aliftype.com>
2022-08-13 23:06:27 +0200

    svm: serialize MetaTextArrayAction’s Kashida array
    Otherwise reading SVM files would lose the Kashida but not the DX
    adjustments and would leave blanks in their place.
    
    Change-Id: Id37e09fed12da5e256d65e2c45c91869364c8d06

--- vcl/source/filter/svm/SvmReader.cxx
+++ vcl/source/filter/svm/SvmReader.cxx
@@ -704,6 +704,19 @@ rtl::Reference<MetaAction> SvmReader::TextArrayHandler(const ImplMetaReadData* p
         }
     }
 
+    if (aCompat.GetVersion() >= 3) // Version 3
+    {
+        std::vector<sal_Bool> aKashidaArray(aArray.size());
+        sal_Bool val(0);
+        for (sal_Int32 i = 0; i < aKashidaArray.size(); i++)
+        {
+            mrStream.ReadUChar(val);
+            aKashidaArray[i] = val;
+        }
+        if (!aKashidaArray.empty())
+            pAction->SetKashidaArray(std::move(aKashidaArray));
+    }
+
     if (!aArray.empty())
         pAction->SetDXArray(std::move(aArray));
 
