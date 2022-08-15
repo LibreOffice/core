9598e61508c73fae8b2840b578df3b8ecc45a115
Liu Hao <ianahao331@gmail.com>
2022-08-15 18:07:07 +0800

    tdf#148430 Use std math functions instead of rtl::math
    Change-Id: I6bcb33d51c3974d0e8905e1ffeac556a99870aab

--- sc/source/core/tool/interpr1.cxx
+++ sc/source/core/tool/interpr1.cxx
@@ -1912,7 +1912,7 @@ void ScInterpreter::ScCotHyp()
 
 void ScInterpreter::ScArcSinHyp()
 {
-    PushDouble( ::rtl::math::asinh( GetDouble()));
+    PushDouble( std::asinh( GetDouble()));
 }
 
 void ScInterpreter::ScArcCosHyp()
@@ -1921,7 +1921,7 @@ void ScInterpreter::ScArcCosHyp()
     if (fVal < 1.0)
         PushIllegalArgument();
     else
-        PushDouble( ::rtl::math::acosh( fVal));
+        PushDouble( std::acosh( fVal));
 }
 
 void ScInterpreter::ScArcTanHyp()
