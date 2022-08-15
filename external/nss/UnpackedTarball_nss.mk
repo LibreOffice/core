31406e68ac7f846a4b35f3f77afffc8e4ff8d312
Stephan Bergmann <sbergman@redhat.com>
2022-08-15 08:19:55 +0200

    external/nss: Fix -Wincompatible-function-pointer-types
    ...with recent Clang 16 trunk since
    <https://github.com/llvm/llvm-project/commit/af01f717c48f0fd2481600ed6c00441763365b62>
    "Default implicit function pointer conversions diagnostic to be an error",
    causing
    
    > ../../../pr/tests/testfile.c:126:41: error: incompatible function pointer types passing 'void (*)(void *)' to parameter of type 'void *(*)(void *)' [-Wincompatible-function-pointer-types]
    >         if (!pthread_create(&tid, NULL, start, arg)) {
    >                                         ^~~~~
    > /usr/include/pthread.h:204:15: note: passing argument to parameter '__start_routine' here
    >                            void *(*__start_routine) (void *),
    >                                    ^
    
    and
    
    > ../../../pr/tests/testfile.c:576:31: error: incompatible function pointer types passing 'PRInt32 (void *)' (aka 'int (void *)') to parameter of type 'void (*)(void *)' [-Wincompatible-function-pointer-types]
    >                               DirTest, &thrarg,
    >                               ^~~~~~~
    > ../../../pr/tests/testfile.c:93:36: note: passing argument to parameter 'start' here
    >                             void (*start)(void *arg),
    >                                    ^
    
    Change-Id: I642e5ca69289993c86abafded65ac23a63fd837e

--- external/nss/UnpackedTarball_nss.mk
+++ external/nss/UnpackedTarball_nss.mk
@@ -26,6 +26,7 @@ $(eval $(call gb_UnpackedTarball_add_patches,nss,\
 	external/nss/nss-bz1646594.patch.1 \
     external/nss/macos-dlopen.patch.0 \
     external/nss/nss-restore-manual-pre-dependencies.patch.1 \
+    external/nss/Wincompatible-function-pointer-types.patch.0 \
     $(if $(filter iOS,$(OS)), \
         external/nss/nss-ios.patch) \
     $(if $(filter ANDROID,$(OS)), \
