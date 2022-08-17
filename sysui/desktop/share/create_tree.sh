37275e452fb47bd2f4a7f19d906ba5032880f9f3
Gabor Kelemen <kelemeng@ubuntu.com>
2022-08-17 17:11:53 +0200

    [CIBO] Rebrand textual parts to CIB Office
    Change-Id: I0a1ee58bf363458e4093bbcd087af3827064f745

--- sysui/desktop/share/create_tree.sh
+++ sysui/desktop/share/create_tree.sh
@@ -60,14 +60,14 @@ office_root=${office_prefix}/${PREFIX}
 #this symlink is needed to have the API bootstrap functions running right
 ln -sf "${office_root}/program/soffice" "${DESTDIR}/${BINDIR}/${PREFIX}"
 
-if test "${PREFIX}" != libreofficepoweredbycib${PRODUCTVERSION} -a "${PREFIX}" != libreofficepoweredbycibdev${PRODUCTVERSION}  ; then
+if test "${PREFIX}" != ciboffice${PRODUCTVERSION} -a "${PREFIX}" != cibofficedev${PRODUCTVERSION}  ; then
     # compat symlinks
     mkdir -p "${DESTDIR}${office_prefix}"
-    ln -sf libreofficepoweredbycib${PRODUCTVERSION} "${DESTDIR}${office_root}"
-    ln -sf /${BINDIR}/${PREFIX} "${DESTDIR}/${BINDIR}/libreofficepoweredbycib${PRODUCTVERSION}"
+    ln -sf ciboffice${PRODUCTVERSION} "${DESTDIR}${office_root}"
+    ln -sf /${BINDIR}/${PREFIX} "${DESTDIR}/${BINDIR}/ciboffice${PRODUCTVERSION}"
 fi
 
-test "${PREFIX}" = libreofficepoweredbycibdev${PRODUCTVERSION} && mime_def_file="libreofficepoweredbycibdev${PRODUCTVERSION}.xml" || mime_def_file="libreofficepoweredbycib${PRODUCTVERSION}.xml"
+test "${PREFIX}" = cibofficedev${PRODUCTVERSION} && mime_def_file="cibofficedev${PRODUCTVERSION}.xml" || mime_def_file="ciboffice${PRODUCTVERSION}.xml"
 mkdir -p "${DESTDIR}/${PREFIXDIR}/share/mime/packages"
 cp openoffice.org.xml "${DESTDIR}/${PREFIXDIR}/share/mime/packages/$mime_def_file"
 chmod 0644 "${DESTDIR}/${PREFIXDIR}/share/mime/packages/$mime_def_file"
