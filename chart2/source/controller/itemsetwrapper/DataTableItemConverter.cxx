xen0n/libreoffice-core

chart2: add UI to the data table
This adds a new object type "Data Table", with all the object
identifiers and converters of properties.
The data table is now shown in the drop-down of chart elements.
A properties dialog was added, which allows to change properties
of a data table. This contains the area, line and font tab pages
and a new tab page specific for data tables, to change if the
horiz. or vert. borders, key or the outline should be show.

Change-Id: I9b4cd58cffbcc952daaa2c0c8f8a5a17e38ac293

chart2/source/controller/itemsetwrapper/DataTableItemConverter.cxx
@@ -1,0 +1,119 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <DataTableItemConverter.hxx>
+#include <ItemPropertyMap.hxx>
+#include <CharacterPropertyItemConverter.hxx>
+#include <GraphicPropertyItemConverter.hxx>
+#include <chartview/ChartSfxItemIds.hxx>
+#include <chartview/ExplicitScaleValues.hxx>
+#include <chartview/ExplicitValueProvider.hxx>
+#include "SchWhichPairs.hxx"
+#include <ChartModelHelper.hxx>
+#include <ChartModel.hxx>
+#include <CommonConverters.hxx>
+#include <ChartType.hxx>
+#include <ChartTypeHelper.hxx>
+#include <Diagram.hxx>
+#include <unonames.hxx>
+#include <BaseCoordinateSystem.hxx>
+#include <memory>
+
+#include <osl/diagnose.h>
+#include <o3tl/any.hxx>
+#include <svl/eitem.hxx>
+#include <svx/chrtitem.hxx>
+#include <svx/sdangitm.hxx>
+#include <svl/intitem.hxx>
+#include <rtl/math.hxx>
+
+using namespace css;
+
+namespace chart::wrapper
+{
+namespace
+{
+ItemPropertyMapType& lclDataTablePropertyMap()
+{
+    static ItemPropertyMapType aPropertyMap{
+        { SCHATTR_DATA_TABLE_HORIZONTAL_BORDER, { "HBorder", 0 } },
+        { SCHATTR_DATA_TABLE_VERTICAL_BORDER, { "VBorder", 0 } },
+        { SCHATTR_DATA_TABLE_OUTLINE, { "Outline", 0 } },
+        { SCHATTR_DATA_TABLE_KEYS, { "Keys", 0 } },
+    };
+    return aPropertyMap;
+};
+}
+
+DataTableItemConverter::DataTableItemConverter(
+    const uno::Reference<beans::XPropertySet>& rPropertySet, SfxItemPool& rItemPool,
+    SdrModel& rDrawModel, const rtl::Reference<::chart::ChartModel>& xChartDoc,
+    const awt::Size* pRefSize)
+    : ItemConverter(rPropertySet, rItemPool)
+    , m_xChartDoc(xChartDoc)
+{
+    m_aConverters.emplace_back(new GraphicPropertyItemConverter(
+        rPropertySet, rItemPool, rDrawModel, xChartDoc, GraphicObjectType::LineProperties));
+    m_aConverters.emplace_back(
+        new CharacterPropertyItemConverter(rPropertySet, rItemPool, pRefSize, "ReferencePageSize"));
+}
+
+DataTableItemConverter::~DataTableItemConverter() = default;
+
+void DataTableItemConverter::FillItemSet(SfxItemSet& rOutItemSet) const
+{
+    for (const auto& pConv : m_aConverters)
+    {
+        pConv->FillItemSet(rOutItemSet);
+    }
+
+    // own items
+    ItemConverter::FillItemSet(rOutItemSet);
+}
+
+bool DataTableItemConverter::ApplyItemSet(const SfxItemSet& rItemSet)
+{
+    bool bResult = false;
+
+    for (const auto& pConv : m_aConverters)
+    {
+        bResult = pConv->ApplyItemSet(rItemSet) || bResult;
+    }
+
+    // own items
+    return ItemConverter::ApplyItemSet(rItemSet) || bResult;
+}
+
+void DataTableItemConverter::FillSpecialItem(sal_uInt16 nWhichId, SfxItemSet& rOutItemSet) const {}
+
+bool DataTableItemConverter::ApplySpecialItem(sal_uInt16 nWhichId, const SfxItemSet& rItemSet)
+{
+    return false;
+}
+
+const WhichRangesContainer& DataTableItemConverter::GetWhichPairs() const
+{
+    return nDataTableWhichPairs;
+}
+
+bool DataTableItemConverter::GetItemProperty(tWhichIdType nWhichId,
+                                             tPropertyNameWithMemberId& rOutProperty) const
+{
+    ItemPropertyMapType& rMap(lclDataTablePropertyMap());
+    auto aIt = rMap.find(nWhichId);
+    if (aIt == rMap.cend())
+        return false;
+
+    rOutProperty = (*aIt).second;
+
+    return true;
+}
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

LibreOffice/core

Reviewed-on: https://gerrit.libreoffice.org/c/core/+/138246
Tested-by: Jenkins
Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>
(cherry picked from commit 300ea910badeef2c8038d9bb20683ea26f4b1973)
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/138495
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

chart2/source/controller/itemsetwrapper/DataTableItemConverter.cxx
@@ -1,0 +1,108 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <DataTableItemConverter.hxx>
+#include <ItemPropertyMap.hxx>
+#include <CharacterPropertyItemConverter.hxx>
+#include <GraphicPropertyItemConverter.hxx>
+#include <chartview/ChartSfxItemIds.hxx>
+#include <chartview/ExplicitScaleValues.hxx>
+#include <chartview/ExplicitValueProvider.hxx>
+#include "SchWhichPairs.hxx"
+#include <ChartModelHelper.hxx>
+#include <ChartModel.hxx>
+#include <CommonConverters.hxx>
+#include <ChartTypeHelper.hxx>
+#include <unonames.hxx>
+#include <memory>
+
+#include <osl/diagnose.h>
+#include <o3tl/any.hxx>
+#include <svl/eitem.hxx>
+#include <svx/chrtitem.hxx>
+#include <svx/sdangitm.hxx>
+#include <svl/intitem.hxx>
+#include <rtl/math.hxx>
+
+using namespace css;
+
+namespace chart::wrapper
+{
+namespace
+{
+ItemPropertyMapType& lclDataTablePropertyMap()
+{
+    static ItemPropertyMapType aPropertyMap{
+        { SCHATTR_DATA_TABLE_HORIZONTAL_BORDER, { "HBorder", 0 } },
+        { SCHATTR_DATA_TABLE_VERTICAL_BORDER, { "VBorder", 0 } },
+        { SCHATTR_DATA_TABLE_OUTLINE, { "Outline", 0 } },
+        { SCHATTR_DATA_TABLE_KEYS, { "Keys", 0 } },
+    };
+    return aPropertyMap;
+};
+}
+
+DataTableItemConverter::DataTableItemConverter(
+    const uno::Reference<beans::XPropertySet>& rPropertySet, SfxItemPool& rItemPool,
+    SdrModel& rDrawModel, uno::Reference<lang::XMultiServiceFactory> const& xFactory,
+    const awt::Size* pRefSize)
+    : ItemConverter(rPropertySet, rItemPool)
+{
+    m_aConverters.emplace_back(new GraphicPropertyItemConverter(
+        rPropertySet, rItemPool, rDrawModel, xFactory, GraphicObjectType::LineProperties));
+    m_aConverters.emplace_back(
+        new CharacterPropertyItemConverter(rPropertySet, rItemPool, pRefSize, "ReferencePageSize"));
+}
+
+DataTableItemConverter::~DataTableItemConverter() = default;
+
+void DataTableItemConverter::FillItemSet(SfxItemSet& rOutItemSet) const
+{
+    for (const auto& pConv : m_aConverters)
+    {
+        pConv->FillItemSet(rOutItemSet);
+    }
+
+    // own items
+    ItemConverter::FillItemSet(rOutItemSet);
+}
+
+bool DataTableItemConverter::ApplyItemSet(const SfxItemSet& rItemSet)
+{
+    bool bResult = false;
+
+    for (const auto& pConv : m_aConverters)
+    {
+        bResult = pConv->ApplyItemSet(rItemSet) || bResult;
+    }
+
+    // own items
+    return ItemConverter::ApplyItemSet(rItemSet) || bResult;
+}
+
+const WhichRangesContainer& DataTableItemConverter::GetWhichPairs() const
+{
+    return nDataTableWhichPairs;
+}
+
+bool DataTableItemConverter::GetItemProperty(tWhichIdType nWhichId,
+                                             tPropertyNameWithMemberId& rOutProperty) const
+{
+    ItemPropertyMapType& rMap(lclDataTablePropertyMap());
+    auto aIt = rMap.find(nWhichId);
+    if (aIt == rMap.cend())
+        return false;
+
+    rOutProperty = (*aIt).second;
+
+    return true;
+}
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */