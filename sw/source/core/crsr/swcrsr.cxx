LibreOffice/core

commit 402f36efb215338ad545caa65d39fb8a39685ea1

tdf#131990 MACROS: fixes return value of UpDown Method
This patch fixes bRet return value of UpDown method in
sw/source/core/crsr/swcrsr.cxx. Before this patch UpDown method always returns true.

Change-Id: I5ebe4bef4e6b9d7ace4941c31e9d5a03c2023997
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/95669
Tested-by: Jenkins
Reviewed-by: Gülşah Köse <gulsah.kose@collabora.com>
(cherry picked from commit 1e5cf1b3ebf39aee01ed2a3bfd5be7ee6f44d18e)
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/95811
Reviewed-by: Canberk TURAN <canberkkturann@gmail.com>
Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

sw/source/core/crsr/swcrsr.cxx
@@ -2036,6 +2036,7 @@ bool SwCursor::UpDown( bool bUp, sal_uInt16 nCnt,
                 }
                 pFrame->GetModelPositionForViewPoint( GetPoint(), aPt, &eTmpState );
             }
+            bRet = !IsSelOvr( SwCursorSelOverFlags::Toggle | SwCursorSelOverFlags::ChangePos );
         }
         else
         {
@@ -2047,12 +2048,17 @@ bool SwCursor::UpDown( bool bUp, sal_uInt16 nCnt,
             //if cursor has already been at start or end of file,
             //Update cursor to change nUpDownX.
             if ( aOldPos.nContent.GetIndex() == nOffset )
+            {
                 GetDoc()->GetEditShell()->UpdateCursor();
-            else
+                bRet = false;
+            }
+            else{
                 *GetPoint() = aPos; // just give a new position
+                bRet = true;
+            }
+
         }
 
-        bRet = !IsSelOvr( SwCursorSelOverFlags::Toggle | SwCursorSelOverFlags::ChangePos );
         DoSetBidiLevelUpDown(); // calculate cursor bidi level
     }
     return bRet;

commit 3afb29f2887a7a92db3576d8b8b611346853df39

tdf#150457 Fix crash on hovering removed footnote reference
The regression was caused by 402f36efb215338ad545caa65d39fb8a39685ea1.
Before that, 0aa0fda64057647219954480ac1bab86b0f0e433 changed the cursor
jumping behaviour to fix tdf#81226. The goal was to make LibreOffice
behave as described in the description of the issue:

"When your cursor is on the last line but not at the end of it, simply
press the down arrow to go at the end of the file (end of that line)."

The same behavior was achieved for pressing down on the last line.

The above patches allowed removing the footnotes and their numbers
(except the first number) while the reference were still present. Then,
on hovering a removed footenote reference, a crash was happening. It
should be noted that even with this patch, removing the first reference
number was not possible.

This fix limits this behavior to anywhere other than inside a footnote.
In this way, it prevents crashing.

With this fix, the behavior of the cursor goes back to what it was
before 0aa0fda64057647219954480ac1bab86b0f0e433. The user will not be
able to select all the footnotes at once. Also, removing footnote numbers
will not be possible.

It is worth noting that not being able to select all the footnotes at
once prevents the user from changing the characteristics of the
footnotes all at the same time. Also, not being able to remove the
footnote numbers, prevents the user from having footnotes with custom
numbers written manually. Both of these actions are possible in MS Word.
The other difference is that by going up, one can go out of the footnote
section in MS Word, but this is not the case for LibreOffice.

Change-Id: I1ce7d2189355f6763b6f31219e00516ff7a8164e

sw/source/core/crsr/swcrsr.cxx
@@ -2096,7 +2096,9 @@ bool SwCursor::UpDown( bool bUp, sal_uInt16 nCnt,
             }
             bRet = !IsSelOvr( SwCursorSelOverFlags::Toggle | SwCursorSelOverFlags::ChangePos );
         }
-        else
+        else if (!pFrame->IsInFootnote()) // tdf#150457 Jump to the begin/end
+                                          // of the first/last line only if the
+                                          // cursor is not inside a footenote
         {
             sal_Int32 nOffset = 0;
 
@@ -2122,6 +2124,8 @@ bool SwCursor::UpDown( bool bUp, sal_uInt16 nCnt,
             }
 
         }
+        else
+            *GetPoint() = aOldPos;
 
         DoSetBidiLevelUpDown(); // calculate cursor bidi level
     }