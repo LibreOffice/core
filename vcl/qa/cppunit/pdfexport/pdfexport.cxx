99dffc0c9807dacf21b65c4e0fd974dd2e410e6d
Caolán McNamara <caolanm@redhat.com>
2022-08-22 11:57:24 +0100

    test if this problem persists
    Change-Id: I57cfe24ec2e644943cc65db825fbd5fbd8649223

--- vcl/qa/cppunit/pdfexport/pdfexport.cxx
+++ vcl/qa/cppunit/pdfexport/pdfexport.cxx
@@ -248,8 +248,7 @@ CPPUNIT_TEST_FIXTURE(PdfExportTest, testTdf107868)
 {
 // No need to run it on Windows, since it would use GDI printing, and not trigger PDF export
 // which is the intent of the test.
-// FIXME: Why does this fail on macOS?
-#if !defined MACOSX && !defined _WIN32
+#if !defined _WIN32
 
     // Import the bugdoc and print to PDF.
     OUString aURL = m_directories.getURLFromSrc(DATA_DIRECTORY) + "tdf107868.odt";
