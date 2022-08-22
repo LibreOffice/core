c0eff5a760374faa652f7a2a77bc16713262e006
Andreas Heinisch <andreas.heinisch@yahoo.de>
2022-08-22 19:04:40 +0200

    tdf#142031 - Collect all the characters for the formula suggestion tooltip
    Change-Id: Idec350b09279f42f2ca42a1c02082d8e9e4207b8

--- sc/source/ui/app/inputhdl.cxx
+++ sc/source/ui/app/inputhdl.cxx
@@ -68,6 +68,7 @@
 #include <tabvwsh.hxx>
 #include <docsh.hxx>
 #include <scmod.hxx>
+#include <formulaopt.hxx>
 #include <uiitems.hxx>
 #include <global.hxx>
 #include <sc.hrc>
@@ -1027,16 +1028,15 @@ void ScInputHandler::GetFormulaData()
         const ScFuncDesc* pDesc = pFuncList->GetFunction( i );
         if ( pDesc->mxFuncName )
         {
-            const sal_Unicode* pName = pDesc->mxFuncName->getStr();
-            const sal_Int32 nLen = pDesc->mxFuncName->getLength();
+            OUString aFuncName(pDesc->mxFuncName->getStr());
+            aFuncName = SC_MOD()->GetFormulaOptions().GetUseEnglishFuncName()
+                            ? ScCompiler::GetCharClassEnglish()->uppercase(aFuncName)
+                            : ScCompiler::GetCharClassLocalized()->uppercase(aFuncName);
             // fdo#75264 fill maFormulaChar with all characters used in formula names
-            for ( sal_Int32 j = 0; j < nLen; j++ )
-            {
-                sal_Unicode c = pName[ j ];
-                maFormulaChar.insert( c );
-            }
-            OUString aFuncName = *pDesc->mxFuncName + aParenthesesReplacement;
-            pFormulaData->insert(ScTypedStrData(aFuncName, 0.0, 0.0, ScTypedStrData::Standard));
+            for (sal_Int32 j = 0; j < aFuncName.getLength(); j++)
+                maFormulaChar.insert(aFuncName[j]);
+            pFormulaData->insert(ScTypedStrData(aFuncName + aParenthesesReplacement, 0.0, 0.0,
+                                                ScTypedStrData::Standard));
             pDesc->initArgumentInfo();
             OUString aEntry = pDesc->getSignature();
             pFormulaDataPara->insert(ScTypedStrData(aEntry, 0.0, 0.0, ScTypedStrData::Standard));
@@ -1068,6 +1068,15 @@ void ScInputHandler::GetFormulaData()
     miAutoPosFormula = pFormulaData->end();
     rDoc.GetFormulaEntries( *pFormulaData );
     rDoc.GetFormulaEntries( *pFormulaDataPara );
+
+    // tdf#142031 - collect all the characters for the formula suggestion tooltip
+    for (auto iter = miAutoPosFormula; iter != pFormulaData->end(); ++iter)
+    {
+        const OUString aFuncName = ScGlobal::getCharClass().uppercase((*iter).GetString());
+        // fdo#75264 fill maFormulaChar with all characters used in formula names
+        for (sal_Int32 j = 0; j < aFuncName.getLength(); j++)
+            maFormulaChar.insert(aFuncName[j]);
+    }
 }
 
 IMPL_LINK( ScInputHandler, ShowHideTipVisibleParentListener, VclWindowEvent&, rEvent, void )
