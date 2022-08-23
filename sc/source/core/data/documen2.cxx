d2dde115f753716314c350c9fcec6b3e5ac076ef
Luboš Luňák <l.lunak@collabora.com>
2022-08-23 19:47:50 +0200

    try harder to ensure InterpretCellsIfNeeded() interprets (tdf#150499)
    ScFormulaCell::Interpret() tries to interpret the whole formula group
    (or the given range of it), but it's not guaranteed, and possibly
    just the called cell will be interpreted. So if a specific range
    really needs to be interpreted, handle that case.
    
    Change-Id: I7fb563ae471eefd49e5bb6c92b6aff98c42a440e

--- sc/source/core/data/documen2.cxx
+++ sc/source/core/data/documen2.cxx
@@ -89,6 +89,7 @@
 #include <datamapper.hxx>
 #include <drwlayer.hxx>
 #include <sharedstringpoolpurge.hxx>
+#include <dociter.hxx>
 #include <config_features.h>
 
 using namespace com::sun::star;
@@ -1216,6 +1217,11 @@ ScSortedRangeCache& ScDocument::GetSortedRangeCache( const ScRange & rRange, con
     // no cells are dirty.
     if(!IsThreadedGroupCalcInProgress())
         InterpretCellsIfNeeded(rRange);
+#ifdef DBG_UTIL
+    ScCellIterator it( *this, rRange );
+    for(bool has = it.first(); has; has = it.next())
+        assert(it.getType() != CELLTYPE_FORMULA || !it.getFormulaCell()->NeedsInterpret());
+#endif
     std::unique_lock guard(mScLookupMutex);
     auto [findIt, bInserted] = mxScSortedRangeCache->aCacheMap.emplace(key, nullptr);
     if (bInserted)
