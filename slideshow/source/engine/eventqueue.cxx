0681ef5c600bac5e452bbc984ccf6504df15e739
Stephan Bergmann <sbergman@redhat.com>
2022-08-23 15:37:06 +0200

    -Werror=redundant-move
    ...with recent GCC 16 trunk.  Introduced with
    1a2926a995fdbdcdae0ca6407877084f3520e539 "use std::move when popping stuff off
    stacks" but which doesn't make sense here where std::priority_queue's top()
    unconditionally returns a const reference (unlike std::stack's top()).
    
    Change-Id: I408920220e3b7ffe775fa87f9edbfdc7721058db
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/138732
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>
    (cherry picked from commit 4c27aaa787a67e05a592adcf6cf42e49b7a02eaf)

--- slideshow/source/engine/eventqueue.cxx
+++ slideshow/source/engine/eventqueue.cxx
@@ -173,7 +173,7 @@ namespace slideshow::internal
                 && !bFireAllEvents
                 && (maEvents.empty() || maEvents.top().nTime > nCurrTime))
             {
-                const EventEntry aEvent (std::move(maNextNextEvents.top()));
+                const EventEntry aEvent (maNextNextEvents.top());
                 maNextNextEvents.pop();
                 maEvents.push(aEvent);
             }
@@ -185,7 +185,7 @@ namespace slideshow::internal
             while( !maEvents.empty() &&
                    (bFireAllEvents || maEvents.top().nTime <= nCurrTime) )
             {
-                EventEntry event( std::move(maEvents.top()) );
+                EventEntry event( maEvents.top() );
                 maEvents.pop();
 
                 // only process event, if it is still 'charged',
