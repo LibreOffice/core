LibreOffice/core

commit a74c51025fa4519caaf461492e4ed8e68bd34885

tdf#146962 sw: hide deleted row at deletion in Hide Changes
In Hide Changes mode, deleting table rows with change tracking
wasn't applied on the table layout immediately, only using Show
Changes and Hide Changes again. Now the deleted row removed from
the table instead leaving an empty table row (except the last
row of a wholly deleted table).

See also commit 95213407dfcbf34056037d60243ff915340d1a2e
"tdf#146622 sw crash fix: don't delete already deleted rows".

Change-Id: I864957cafa38e631a65db0670c7b566cb689f4cd
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/130701
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/source/core/frmedt/fetab.cxx
@@ -328,32 +328,41 @@ bool SwFEShell::DeleteRow(bool bCompleteTable)
 
     CurrShell aCurr( this );
 
+    bool bRecordChanges = GetDoc()->GetDocShell()->IsChangeRecording();
+    bool bRecordAndHideChanges = bRecordChanges &&
+        GetDoc()->getIDocumentLayoutAccess().GetCurrentLayout()->IsHideRedlines();
+
+    // tracked deletion: all rows have already had tracked row change in the table selection
+    if ( bRecordChanges && !SwDoc::HasRowNotTracked( *getShellCursor( false ) ) )
+        return false;
+
+    if ( bRecordChanges )
+        StartUndo(bCompleteTable ? SwUndoId::UI_TABLE_DELETE : SwUndoId::ROW_DELETE);
+
+    StartAllAction();
+
     // tracked deletion: remove only textbox content,
     // and set IsNoTracked table line property to false
-    if ( GetDoc()->GetDocShell()->IsChangeRecording() )
+    if ( bRecordChanges )
     {
-        // all rows have already had tracked row change in the table selection
-        if ( !SwDoc::HasRowNotTracked( *getShellCursor( false ) ) )
-            return false;
-
-        SwEditShell* pEditShell = GetDoc()->GetEditShell();
-        StartUndo(bCompleteTable ? SwUndoId::UI_TABLE_DELETE : SwUndoId::ROW_DELETE);
-        StartAllAction();
-
         SvxPrintItem aNotTracked(RES_PRINT, false);
         GetDoc()->SetRowNotTracked( *getShellCursor( false ), aNotTracked );
 
         if ( SwWrtShell* pWrtShell = dynamic_cast<SwWrtShell*>(this) )
             pWrtShell->SelectTableRow();
 
-        pEditShell->Delete();
+        // don't need to remove the row frames in Show Changes mode
+        if ( !bRecordAndHideChanges )
+        {
+            SwEditShell* pEditShell = GetDoc()->GetEditShell();
+            pEditShell->Delete();
 
-        EndAllActionAndCall();
-        EndUndo(bCompleteTable ? SwUndoId::UI_TABLE_DELETE : SwUndoId::ROW_DELETE);
-        return true;
+            EndAllActionAndCall();
+            EndUndo(bCompleteTable ? SwUndoId::UI_TABLE_DELETE : SwUndoId::ROW_DELETE);
+
+            return true;
+        }
     }
-
-    StartAllAction();
 
     // search for boxes via the layout
     bool bRet;
@@ -425,6 +434,32 @@ bool SwFEShell::DeleteRow(bool bCompleteTable)
                     pNextBox = pNextBox->FindPreviousBox( pTableNd->GetTable(), pNextBox );
             }
 
+            // delete row content in Hide Changes mode
+            if ( bRecordAndHideChanges )
+            {
+                SwEditShell* pEditShell = GetDoc()->GetEditShell();
+
+                // select the rows deleted with change tracking
+                if ( SwWrtShell* pWrtShell = dynamic_cast<SwWrtShell*>(this) )
+                {
+                    pWrtShell->SelectTableRow();
+                    SwShellTableCursor* pTableCursor = GetTableCursor();
+                    auto pStt = aBoxes[0];
+                    auto pEnd = aBoxes.back();
+                    pTableCursor->DeleteMark();
+
+                    // set start and end of the selection
+                    pTableCursor->GetPoint()->nNode = *pEnd->GetSttNd();
+                    pTableCursor->Move( fnMoveForward, GoInContent );
+                    pTableCursor->SetMark();
+                    pTableCursor->GetPoint()->nNode = *pStt->GetSttNd()->EndOfSectionNode();
+                    pTableCursor->Move( fnMoveBackward, GoInContent );
+                    pWrtShell->UpdateCursor();
+                }
+
+                pEditShell->Delete();
+            }
+
             SwNodeOffset nIdx;
             if( pNextBox )      // put cursor here
                 nIdx = pNextBox->GetSttIdx() + 1;
@@ -443,6 +478,33 @@ bool SwFEShell::DeleteRow(bool bCompleteTable)
                 pPam->GetPoint()->nContent.Assign( pCNd, 0 );
                 pPam->SetMark();            // both want something
                 pPam->DeleteMark();
+            }
+
+            // remove row frames in Hide Changes mode
+            if ( bRecordAndHideChanges )
+            {
+                for (auto & rpFndLine : aFndBox.GetLines())
+                {
+                    SwTableLine* pTmpLine = rpFndLine->GetLine();
+                    SwIterator<SwRowFrame,SwFormat> aIt( *pTmpLine->GetFrameFormat() );
+                    for( SwRowFrame* pRowFrame = aIt.First(); pRowFrame; pRowFrame = aIt.Next() )
+                    {
+                        auto pTabFrame = pRowFrame->GetUpper();
+                        // FIXME remove table frame instead of keeping the last row frame
+                        if ( pTabFrame->IsTabFrame() && pTabFrame->Lower() == pTabFrame->GetLastLower() )
+                            break;
+
+                        if( pRowFrame->GetTabLine() == pTmpLine )
+                        {
+                            pRowFrame->RemoveFromLayout();
+                            SwFrame::DestroyFrame(pRowFrame);
+                        }
+                    }
+                }
+
+                EndAllActionAndCall();
+                EndUndo(bCompleteTable ? SwUndoId::UI_TABLE_DELETE : SwUndoId::ROW_DELETE);
+                return true;
             }
         }
 

sw/qa/extras/uiwriter/uiwriter3.cxx
@@ -41,6 +41,7 @@
 #include <IDocumentFieldsAccess.hxx>
 #include <IDocumentLinksAdministration.hxx>
 #include <IDocumentRedlineAccess.hxx>
+#include <rootfrm.hxx>
 
 namespace
 {
@@ -2154,6 +2155,51 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest3, testTdf146622)
     // now only the second table deleted by AcceptAllTrackedChanges
     dispatchCommand(mxComponent, ".uno:AcceptAllTrackedChanges", {});
     CPPUNIT_ASSERT_EQUAL(sal_Int32(2), xTables->getCount());
+}
+
+CPPUNIT_TEST_FIXTURE(SwUiWriterTest3, testTdf146962)
+{
+    // load a 2-row table, set Hide Changes mode and delete the first row with change tracking
+    SwDoc* pDoc = createSwDoc(DATA_DIRECTORY, "tdf116789.fodt");
+    CPPUNIT_ASSERT(pDoc);
+    SwWrtShell* pWrtShell = pDoc->GetDocShell()->GetWrtShell();
+    CPPUNIT_ASSERT(pWrtShell);
+
+    // enable redlining
+    dispatchCommand(mxComponent, ".uno:TrackChanges", {});
+    CPPUNIT_ASSERT_MESSAGE("redlining should be on",
+                           pDoc->getIDocumentRedlineAccess().IsRedlineOn());
+    // hide changes
+    dispatchCommand(mxComponent, ".uno:ShowTrackedChanges", {});
+    CPPUNIT_ASSERT(pWrtShell->GetLayout()->IsHideRedlines());
+
+    dispatchCommand(mxComponent, ".uno:DeleteRows", {});
+
+    // Without the fix in place, the deleted row would be visible
+
+    xmlDocUniquePtr pXmlDoc = parseLayoutDump();
+    // This was 2
+    assertXPath(pXmlDoc, "/root/page[1]/body/tab/row", 1);
+
+    // check it in Show Changes mode
+
+    dispatchCommand(mxComponent, ".uno:ShowTrackedChanges", {});
+    CPPUNIT_ASSERT(!pWrtShell->GetLayout()->IsHideRedlines());
+
+    discardDumpedLayout();
+    pXmlDoc = parseLayoutDump();
+    // 2 rows are visible now
+    assertXPath(pXmlDoc, "/root/page[1]/body/tab/row", 2);
+
+    // check it in Hide Changes mode again
+
+    dispatchCommand(mxComponent, ".uno:ShowTrackedChanges", {});
+    CPPUNIT_ASSERT(pWrtShell->GetLayout()->IsHideRedlines());
+
+    discardDumpedLayout();
+    pXmlDoc = parseLayoutDump();
+    // only a single row is visible again
+    assertXPath(pXmlDoc, "/root/page[1]/body/tab/row", 1);
 }
 
 CPPUNIT_TEST_FIXTURE(SwUiWriterTest3, testTdf135014)

sw/source/core/doc/docnum.cxx
@@ -1446,7 +1446,9 @@ GotoPrevLayoutTextFrame(SwNodeIndex & rIndex, SwRootFrame const*const pLayout)
     {
         if (rIndex.GetNode().IsTextNode())
         {
-            if (rIndex.GetNode().GetRedlineMergeFlag() != SwNode::Merge::None)
+            if (rIndex.GetNode().GetRedlineMergeFlag() != SwNode::Merge::None &&
+                // not a tracked row deletion in Hide Changes mode
+                rIndex.GetNode().GetTextNode()->getLayoutFrame(pLayout) )
             {
                 rIndex = *static_cast<SwTextFrame*>(rIndex.GetNode().GetTextNode()->getLayoutFrame(pLayout))->GetMergedPara()->pFirstNode;
             }

commit 178d733c1f957c8828bfeba28de26dfdf737da85

tdf#150576 sw: fix cursor pos deleting at rows deleted already
Instead of jumping in the start of the document, set cursor
after (or deleting the last row, before) the rows deleted
already in Hide Changes mode with enabled change tracking.

Regression from commit a74c51025fa4519caaf461492e4ed8e68bd34885
"tdf#146962 sw: hide deleted row at deletion in Hide Changes".

Follow-up to commit 189aa05c6ea17a8e823b4eab18ea0d1131d9d73e
"tdf#148849 sw: fix cursor pos at tracked DeleteRow in Hide Changes".

Change-Id: Ifc2a7f41a57f413d27d9b464a0e464643d15f404

sw/source/core/frmedt/fetab.cxx
@@ -416,6 +416,16 @@ bool SwFEShell::DeleteRow(bool bCompleteTable)
             }
             SwTableBox* pNextBox = pDelLine->FindNextBox( pTableNd->GetTable(),
                                                             pDelBox );
+            // skip deleted lines in Hide Changes mode with enabled change tracking
+            if ( bRecordAndHideChanges )
+            {
+                SwRedlineTable::size_type nRedlinePos = 0;
+                while( pNextBox && pNextBox->GetUpper()->IsDeleted(nRedlinePos) )
+                    pNextBox = pNextBox->GetUpper()->FindNextBox( pTableNd->GetTable(),
+                                    pNextBox->GetUpper()->GetTabBoxes().back() );
+            }
+
+            // skip protected cells
             while( pNextBox &&
                     pNextBox->GetFrameFormat()->GetProtect().IsContentProtected() )
                 pNextBox = pNextBox->FindNextBox( pTableNd->GetTable(), pNextBox );
@@ -428,6 +438,19 @@ bool SwFEShell::DeleteRow(bool bCompleteTable)
                     pDelBox = pDelBox->GetTabLines()[0]->GetTabBoxes()[0];
                 pNextBox = pDelLine->FindPreviousBox( pTableNd->GetTable(),
                                                             pDelBox );
+                // skip previous deleted lines in Hide Changes mode with enabled change tracking
+                if ( bRecordAndHideChanges )
+                {
+                    SwRedlineTable::size_type nRedlinePos = 0;
+                    while( pNextBox && pNextBox->GetUpper()->IsDeleted(nRedlinePos) )
+                    {
+                        pNextBox = pNextBox->GetUpper()->FindPreviousBox( pTableNd->GetTable(),
+                                        pNextBox->GetUpper()->GetTabBoxes()[0] );
+                        nRedlinePos = 0;
+                    }
+                }
+
+                // skip previous protected cells
                 while( pNextBox &&
                         pNextBox->GetFrameFormat()->GetProtect().IsContentProtected() )
                     pNextBox = pNextBox->FindPreviousBox( pTableNd->GetTable(), pNextBox );

sw/qa/extras/uiwriter/uiwriter3.cxx
@@ -3661,6 +3661,76 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest3, testTdf148849)
     CPPUNIT_ASSERT_EQUAL(OUString("Row 2"), rNode.GetTextNode()->GetText());
 }
 
+CPPUNIT_TEST_FIXTURE(SwUiWriterTest3, testTdf150576)
+{
+    // load a document with a table and an empty paragraph before the table
+    SwDoc* pDoc = createSwDoc(DATA_DIRECTORY, "tdf148849.fodt");
+    SwWrtShell* pWrtShell = pDoc->GetDocShell()->GetWrtShell();
+
+    // record changes
+    pDoc->getIDocumentRedlineAccess().SetRedlineFlags(RedlineFlags::On | RedlineFlags::ShowDelete
+                                                      | RedlineFlags::ShowInsert);
+    CPPUNIT_ASSERT_MESSAGE("redlining should be on",
+                           pDoc->getIDocumentRedlineAccess().IsRedlineOn());
+    // hide changes
+    dispatchCommand(mxComponent, ".uno:ShowTrackedChanges", {});
+    CPPUNIT_ASSERT(pWrtShell->GetLayout()->IsHideRedlines());
+
+    uno::Reference<text::XTextTablesSupplier> xTablesSupplier(mxComponent, uno::UNO_QUERY);
+    uno::Reference<container::XIndexAccess> xTables(xTablesSupplier->getTextTables(),
+                                                    uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(1), xTables->getCount());
+
+    // Check deletion of the first row, if the second row deleted already
+
+    // put cursor in the second table row
+    pWrtShell->Down(/*bSelect=*/false, /*nCount=*/2);
+    SwNode& rNode = pWrtShell->GetCursor()->GetPoint()->GetNode();
+    CPPUNIT_ASSERT_EQUAL(OUString("Row 2"), rNode.GetTextNode()->GetText());
+
+    // delete the second table row
+    pWrtShell->DeleteRow();
+
+    // check cursor position (row 3)
+    SwNode& rNode2 = pWrtShell->GetCursor()->GetPoint()->GetNode();
+    CPPUNIT_ASSERT_EQUAL(OUString("Row 3"), rNode2.GetTextNode()->GetText());
+
+    // put cursor in the first row
+    pWrtShell->Up(/*bSelect=*/false, /*nCount=*/1);
+    SwNode& rNode3 = pWrtShell->GetCursor()->GetPoint()->GetNode();
+    CPPUNIT_ASSERT_EQUAL(OUString("12"), rNode3.GetTextNode()->GetText());
+
+    // delete the first row
+    pWrtShell->DeleteRow();
+
+    // This was empty (cursor jumped in the start of the document instead of
+    // the next not deleted row)
+    SwNode& rNode4 = pWrtShell->GetCursor()->GetPoint()->GetNode();
+    CPPUNIT_ASSERT_EQUAL(OUString("Row 3"), rNode4.GetTextNode()->GetText());
+
+    // Check skipping previous lines
+
+    // restore deleted rows
+    dispatchCommand(mxComponent, ".uno:Undo", {});
+    dispatchCommand(mxComponent, ".uno:Undo", {});
+    Scheduler::ProcessEventsToIdle();
+    SwNode& rNode5 = pWrtShell->GetCursor()->GetPoint()->GetNode();
+    CPPUNIT_ASSERT_EQUAL(OUString("Row 2"), rNode5.GetTextNode()->GetText());
+
+    // delete the second row
+    pWrtShell->DeleteRow();
+    SwNode& rNode7 = pWrtShell->GetCursor()->GetPoint()->GetNode();
+    CPPUNIT_ASSERT_EQUAL(OUString("Row 3"), rNode7.GetTextNode()->GetText());
+
+    // delete the third, i.e. last row
+    pWrtShell->DeleteRow();
+    SwNode& rNode8 = pWrtShell->GetCursor()->GetPoint()->GetNode();
+
+    // This was empty (cursor jumped in the start of the document instead of
+    // the previous not deleted row)
+    CPPUNIT_ASSERT_EQUAL(OUString("12"), rNode8.GetTextNode()->GetText());
+}
+
 CPPUNIT_TEST_FIXTURE(SwUiWriterTest3, testTdf132603)
 {
     createSwDoc();