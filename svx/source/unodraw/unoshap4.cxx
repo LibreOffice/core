c7577330e806858f99b8c78f63e734fdb2c06b6e
Miklos Vajna <vmiklos@collabora.com>
2022-08-25 14:01:43 +0200

    Related: tdf#149971 avmedia: add doc model and render for crop of media objects
    It is possible to provide an explicit preview of media objects since
    commit 8fa1d453c94cdbb03dac646fb8db2ebd1a0e84bd (Related: tdf#149971
    svx: support explicitly provided snapshots for media shapes,
    2022-08-24), however they can't be cropped.
    
    This means that media shapes from PPTX with cropping show unexpected
    content and can also have a buggy aspect ratio.
    
    Extend avmedia::MediaItem to store cropping and take it into account
    when returning the preview bitmap in SdrMediaObj::getSnapshot(). PPTX
    import works out of the box, as oox/ already tried to set a cropping
    property on the media shape.
    
    This is just the preview, the cropping of the video itself is not yet
    implemented.
    
    (cherry picked from commit b6f0fd6a2f459ead2268e07bfd86db7e303b323f)
    
    Conflicts:
    	svx/source/unodraw/unoprov.cxx
    
    Change-Id: I8db3e0dcf252613d56eb0e6139adf097e53b15cc
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/138963
    Tested-by: Jenkins
    Reviewed-by: Sarper Akdemir <sarper.akdemir@collabora.com>

--- svx/source/unodraw/unoshap4.cxx
+++ svx/source/unodraw/unoshap4.cxx
@@ -783,7 +783,8 @@ bool SvxMediaShape::setPropertyValueImpl( const OUString& rName, const SfxItemPr
     if( ((pProperty->nWID >= OWN_ATTR_MEDIA_URL) && (pProperty->nWID <= OWN_ATTR_MEDIA_ZOOM))
         || (pProperty->nWID == OWN_ATTR_MEDIA_STREAM)
         || (pProperty->nWID == OWN_ATTR_MEDIA_MIMETYPE)
-        || (pProperty->nWID == OWN_ATTR_VALUE_GRAPHIC))
+        || (pProperty->nWID == OWN_ATTR_VALUE_GRAPHIC)
+        || (pProperty->nWID == SDRATTR_GRAFCROP))
     {
 #if HAVE_FEATURE_AVMEDIA
         SdrMediaObj* pMedia = static_cast< SdrMediaObj* >( GetSdrObject() );
@@ -888,6 +889,19 @@ bool SvxMediaShape::setPropertyValueImpl( const OUString& rName, const SfxItemPr
 #endif
         break;
 
+        case SDRATTR_GRAFCROP:
+#if HAVE_FEATURE_AVMEDIA
+        {
+            text::GraphicCrop aCrop;
+            if (rValue >>= aCrop)
+            {
+                bOk = true;
+                aItem.setCrop(aCrop);
+            }
+        }
+#endif
+        break;
+
         case OWN_ATTR_MEDIA_STREAM:
 #if HAVE_FEATURE_AVMEDIA
             try
@@ -946,7 +960,8 @@ bool SvxMediaShape::getPropertyValueImpl( const OUString& rName, const SfxItemPr
         || (pProperty->nWID == OWN_ATTR_MEDIA_TEMPFILEURL)
         || (pProperty->nWID == OWN_ATTR_MEDIA_MIMETYPE)
         || (pProperty->nWID == OWN_ATTR_FALLBACK_GRAPHIC)
-        || (pProperty->nWID == OWN_ATTR_VALUE_GRAPHIC))
+        || (pProperty->nWID == OWN_ATTR_VALUE_GRAPHIC)
+        || (pProperty->nWID == SDRATTR_GRAFCROP))
     {
         SdrMediaObj* pMedia = static_cast< SdrMediaObj* >( GetSdrObject() );
 #if HAVE_FEATURE_AVMEDIA
@@ -1027,6 +1042,15 @@ bool SvxMediaShape::getPropertyValueImpl( const OUString& rName, const SfxItemPr
                 {
                     rValue <<= aGraphic.GetXGraphic();
                 }
+            }
+#endif
+            break;
+
+            case SDRATTR_GRAFCROP:
+#if HAVE_FEATURE_AVMEDIA
+            {
+                text::GraphicCrop aCrop = aItem.getCrop();
+                rValue <<= aCrop;
             }
 #endif
             break;
