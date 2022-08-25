2bedb9c047212eef36cacbac735cd656f48c1d32
Caolán McNamara <caolanm@redhat.com>
2022-08-25 13:10:23 +0100

    tdf#150507 reenable slight hinting for cairo with ResolutionIndependentLayout
    it was disabled because of:
    tdf#150145 text appears to have a row of pixels cropped out
    which seems to be the same issue as
    https://gitlab.freedesktop.org/cairo/cairo/-/issues/390
    
    reenable, drop CAIRO_HINT_METRICS_OFF and emulate a proposed solution of
    https://gitlab.freedesktop.org/cairo/cairo/-/merge_requests/235 by
    hacking in a route to disable CAIRO_ROUND_GLYPH_POS_ON
    
    Change-Id: I3c9f982c9737b84fe0ee6b645f507e1226807d60

--- vcl/inc/unx/cairotextrender.hxx
+++ vcl/inc/unx/cairotextrender.hxx
@@ -25,9 +25,15 @@
 class GenericSalLayout;
 class SalGraphics;
 typedef struct _cairo cairo_t;
+typedef struct _cairo_font_options cairo_font_options_t;
 
 class VCL_DLLPUBLIC CairoTextRender : public FreeTypeTextRenderImpl
 {
+private:
+    // https://gitlab.freedesktop.org/cairo/cairo/-/merge_requests/235
+    // I don't want to have CAIRO_ROUND_GLYPH_POS_ON set in the cairo
+    // surfaces font_options, but that's private, so tricky to achieve
+    cairo_font_options_t*       mpRoundGlyphPosOffOptions;
 protected:
     virtual cairo_t*            getCairoContext() = 0;
     virtual void                getSurfaceOffset(double& nDX, double& nDY) = 0;
@@ -37,6 +43,8 @@ protected:
 
 public:
     virtual void                DrawTextLayout(const GenericSalLayout&, const SalGraphics&) override;
+    CairoTextRender();
+    virtual ~CairoTextRender();
 };
 
 #endif
