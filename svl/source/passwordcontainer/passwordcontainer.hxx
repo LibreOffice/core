f0ea79b79fcede8e67dbe49dc0f2dc5183ee2ad4
Arnaud VERSINI <arnaud.versini@libreoffice.org>
2022-08-26 21:56:41 +0200

    SVL : use std::mutex in passwordcontainer
    Change-Id: I1d253407313a65fa82f4f1da4a583d196b59c195

--- svl/source/passwordcontainer/passwordcontainer.hxx
+++ svl/source/passwordcontainer/passwordcontainer.hxx
@@ -22,6 +22,7 @@
 #include <utility>
 #include <vector>
 #include <map>
+#include <mutex>
 #include <optional>
 #include <com/sun/star/task/XPasswordContainer2.hpp>
 #include <com/sun/star/task/PasswordRequestMode.hpp>
@@ -239,7 +240,7 @@ class PasswordContainer : public ::cppu::WeakImplHelper<
 private:
     PasswordMap      m_aContainer;
     std::optional<StorageItem> m_xStorageFile;
-    ::osl::Mutex mMutex;
+    std::mutex mMutex;
     OUString m_aMasterPassword; // master password is set when the string is not empty
     css::uno::Reference< css::lang::XComponent > mComponent;
     SysCredentialsConfig mUrlContainer;
