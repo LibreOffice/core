d455bf3665a4539ec64c6cf6ece76437a2c4cf75
Caolán McNamara <caolanm@redhat.com>
2022-08-27 10:21:35 +0100

    cid#1500408 silence Resource leak
    Change-Id: Ic969e0dac9d5fceb2c14daee37eee44305c15220
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/138922
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>

--- sw/source/core/text/txtcache.hxx
+++ sw/source/core/text/txtcache.hxx
@@ -41,7 +41,10 @@ public:
     void SetPara(SwParaPortion* pNew, bool bDelete)
     {
         if (!bDelete)
+        {
+            // coverity[leaked_storage] - intentional, ownership transfered
             (void)m_pLine.release();
+        }
         m_pLine.reset(pNew);
     }
 };
