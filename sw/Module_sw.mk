1f6918b458e5edee8daa7c81675b30d50533249f
Miklos Vajna <vmiklos@collabora.com>
2022-08-30 12:00:32 +0200

    Split CppunitTest_sw_uiwriter3 into a uiwriter3 and a uiwriter8
    My build with 16G ram now regularly fails with:
    
    g++: internal compiler error: Killed (program cc1plus)
    Please submit a full bug report,
    with preprocessed source if appropriate.
    See <https://bugs.opensuse.org/> for instructions.
    make[1]: *** [solenv/gbuild/LinkTarget.mk:337: workdir/CxxObject/sw/qa/extras/uiwriter/uiwriter3.o] Error 4
    make[1]: *** Waiting for unfinished jobs....
    make: *** [Makefile:289: build] Error 2
    
    Change-Id: Ia758f2e905a13369a9eef7eece4104bb9ea76303
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/139025
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Tested-by: Jenkins

--- sw/Module_sw.mk
+++ sw/Module_sw.mk
@@ -116,6 +116,7 @@ $(eval $(call gb_Module_add_slowcheck_targets,sw,\
     CppunitTest_sw_uiwriter5 \
     CppunitTest_sw_uiwriter6 \
     CppunitTest_sw_uiwriter7 \
+    CppunitTest_sw_uiwriter8 \
     CppunitTest_sw_layoutwriter \
     CppunitTest_sw_layoutwriter2 \
     CppunitTest_sw_mailmerge \
