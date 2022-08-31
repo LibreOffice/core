ff8edfbf8f43c96e4053f9d735d99917bf5cc683
Miklos Vajna <vmiklos@collabora.com>
2022-08-31 11:17:43 +0200

    crashtesting: fix DOCX export of forum-mso-de-102317.docx
    This went wrong in b2bc4ea8ddae6b01f344469d988e21fed3143c42 (DOCX
    import: handle SDT around citation field, 2014-07-16), the problem is
    that a run/inline SDT is pending, but we wrote a header stream in the
    meantime, so the invariant that Start/EndParagraph() in
    DocxAttributeOutput is called in pairs doesn't help, which results in
    not-well-formed XML, which is an assertion failure in debug build.
    
    Fix the problem similar to 5f3af56b2c0ef6c628a7cfe5ce6e86f8e1765f5f (sw:
    refactoring for docx sdt blocks output, 2021-11-16), which already did
    the same for paragraph/block SDTs.
    
    (cherry picked from commit 8431cde61432893dd5ba75d8244457306a9f177b)
    
    Change-Id: I5e7273b839bb5c1f841c0fab9b68b4ca239cca18
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/140655
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

--- sw/source/filter/ww8/docxattributeoutput.cxx
+++ sw/source/filter/ww8/docxattributeoutput.cxx
@@ -6916,6 +6916,8 @@ void DocxAttributeOutput::pushToTableExportContext(DocxTableExportContext& rCont
 
     rContext.m_bStartedParaSdt = m_aParagraphSdt.m_bStartedSdt;
     m_aParagraphSdt.m_bStartedSdt = false;
+    rContext.m_bStartedRunSdt = m_aRunSdt.m_bStartedSdt;
+    m_aRunSdt.m_bStartedSdt = false;
 }
 
 void DocxAttributeOutput::popFromTableExportContext(DocxTableExportContext const & rContext)
@@ -6924,6 +6926,7 @@ void DocxAttributeOutput::popFromTableExportContext(DocxTableExportContext const
     m_tableReference->m_bTableCellOpen = rContext.m_bTableCellOpen;
     m_tableReference->m_nTableDepth = rContext.m_nTableDepth;
     m_aParagraphSdt.m_bStartedSdt = rContext.m_bStartedParaSdt;
+    m_aRunSdt.m_bStartedSdt = rContext.m_bStartedRunSdt;
 }
 
 void DocxAttributeOutput::WriteTextBox(uno::Reference<drawing::XShape> xShape)
