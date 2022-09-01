e4d9d522fec5770dd25f99384ae7a012636662cb
Julien Nabet <serval2412@yahoo.fr>
2022-09-01 14:16:37 +0200

    Revert "tdf#150712: Format->Conditional->Condition 100% CPU"
    This reverts commit 076be52655f2cf4e331b344e0f6497f41b37fa8c.
    
    Reason for revert: I misread and we replaced "xa" "xd" by empty but it's a space.
    
    Change-Id: I0246f6a3c667b4336f21ffd61fd86574009b7956
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/139175
    Tested-by: Julien Nabet <serval2412@yahoo.fr>
    Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

--- svx/source/dialog/fntctrl.cxx
+++ svx/source/dialog/fntctrl.cxx
@@ -124,9 +124,15 @@ void setFont(const SvxFont& rNewFont, SvxFont& rImplFont)
  */
 bool CleanAndCheckEmpty(OUString& rText)
 {
-    rText = rText.replaceAll("\xa", u" ");
-    rText = rText.replaceAll("\xd", u" ");
-    return rText.isEmpty();
+    bool bEmpty = true;
+    for (sal_Int32 i = 0; i < rText.getLength(); ++i)
+    {
+        if (0xa == rText[i] || 0xd == rText[i])
+            rText = rText.replaceAt(i, 1, u" ");
+        else
+            bEmpty = false;
+    }
+    return bEmpty;
 }
 } // end anonymous namespace
 
