8b5a989dcdcf4edd50c9fdb092d4180432ce4b6a
Mike Kaganski <mike.kaganski@collabora.com>
2022-09-12 19:14:53 +0300

    Fix path in comments
    include/tools/errcode.hxx was moved to include/vcl/errcode.hxx in
    commit f9f045e7830d184497554e0e438cc478fa990eb6
      Date Mon Apr 24 01:06:41 2017 +1000
        tools: move errcode.hxx to the vcl module
    
    Change-Id: I0d42839cad9118fba2d35e9dbf6ba456dcb5e3aa
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/139815
    Tested-by: Jenkins
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

--- include/LibreOfficeKit/LibreOfficeKitEnums.h
+++ include/LibreOfficeKit/LibreOfficeKitEnums.h
@@ -371,7 +371,7 @@ typedef enum
      * {
      *     "classification": "error" | "warning" | "info"
      *     "kind": "network" etc.
-     *     "code": a structured 32-bit error code, the ErrCode from LibreOffice's <tools/errcode.hxx>
+     *     "code": a structured 32-bit error code, the ErrCode from LibreOffice's <vcl/errcode.hxx>
      *     "message": freeform description
      * }
      */
