b24dfa7ef93bc31ed9ccfb3e3472109bbd384b15
galjit <qmebic1975@gmail.com>
2022-09-14 17:09:07 +0300

    Resolves tdf#132398 - New hicolor/macOS icons
    - Replaced old with new hicolor icons
    - Added files wherever were gaps
    - Changed non-oasis mimetype files to symlinks
    - Removed "draw.svg" and put "drawing.svg" in scalable
    - Added "macos" directory for special MacOS icons
    
    Change-Id: Id10a386f6144db4f0a1a7a118c44687fffbe0568
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/139754
    Tested-by: Jenkins
    Reviewed-by: Heiko Tietze <heiko.tietze@documentfoundation.org>

--- sysui/Package_osxicons.mk
+++ sysui/Package_osxicons.mk
@@ -6,34 +6,37 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
+icns_WRKDIR := $(call gb_CustomTarget_get_workdir,sysui/icns)
+icns_SRCDIR := $(SRCDIR)/sysui/desktop/icons/macos
 
-$(eval $(call gb_Package_Package,sysui_osxicons,$(SRCDIR)/sysui/desktop/icons))
+icns_mimetypes := database drawing drawing-template extension formula master-document \
+                  master-document-template presentation presentation-template spreadsheet \
+                  spreadsheet-template text text-template
+
+icns_sizes := 16x16 16x16@2x 32x32 32x32@2x 128x128 128x128@2x 256x256 256x256@2x 512x512 512x512@2x
+
+$(icns_WRKDIR)/.dir:
+	mkdir -p $(@D) && touch $@
+
+# the writer, calc, impress, draw & base app-icons are gone...
+$(icns_WRKDIR)/main.icns: $(foreach size,$(icns_sizes),$(icns_SRCDIR)/app-launcher/main.iconset/icon_$(size).png) | $(icns_WRKDIR)/.dir
+	$(call gb_Output_announce,$(@F),$(true),ICNS,4)
+	iconutil -c icns -o $@ $(<D)
+
+$(icns_WRKDIR)/generic-%.icns: $(foreach size,$(icns_sizes),$(icns_SRCDIR)/mime-generic/%.iconset/icon_$(size).png) | $(icns_WRKDIR)/.dir
+	$(call gb_Output_announce,$(@F),$(true),ICNS,3)
+	iconutil -c icns -o $@ $(<D)
+
+$(icns_WRKDIR)/oasis-%.icns: $(foreach size,$(icns_sizes),$(icns_SRCDIR)/mime-oasis/%.iconset/icon_$(size).png) | $(icns_WRKDIR)/.dir
+	$(call gb_Output_announce,$(@F),$(true),ICNS,2)
+	iconutil -c icns -o $@ $(<D)
+
+$(eval $(call gb_Package_Package,sysui_osxicons,$(call gb_CustomTarget_get_workdir,sysui/icns)))
+$(call gb_Package_get_clean_target,sysui_osxicons): $(call gb_CustomTarget_get_clean_target,sysui/icns)
 
 $(eval $(call gb_Package_add_files_with_dir,sysui_osxicons,Resources,\
-    text.icns \
-    oasis-text.icns \
-    spreadsheet.icns \
-    oasis-spreadsheet.icns \
-    presentation.icns \
-    oasis-presentation.icns \
-    drawing.icns \
-    oasis-drawing.icns \
-    master-document.icns \
-    oasis-master-document.icns \
-    oasis-master-document-template.icns \
-    formula.icns \
-    oasis-formula.icns \
-    text-template.icns \
-    oasis-text-template.icns \
-    spreadsheet-template.icns \
-    oasis-spreadsheet-template.icns \
-    presentation-template.icns \
-    oasis-presentation-template.icns \
-    drawing-template.icns \
-    oasis-drawing-template.icns \
-    oasis-database.icns \
-    extension.icns \
     main.icns \
+    $(foreach type,$(icns_mimetypes),generic-$(type).icns oasis-$(type).icns) \
 ))
 
 # vim: set ts=4 sw=4 et:
