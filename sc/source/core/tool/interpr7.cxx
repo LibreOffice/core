52fd3fa8644b9ffb1e42dcef4c34cf48ba78bb20
Michael Stahl <michael.stahl@allotropia.de>
2022-09-14 10:27:02 +0200

    libxml2: upgrade to release 2.10.2
    Fixes CVE-2022-2309
    
    Change-Id: I180218be275d3b6d38f8f74aa51c57e50d2734ee
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/139911
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
    (cherry picked from commit d621a8839cebf96fe3ac374026f344f8e68bf011)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/139954
    Reviewed-by: Thorsten Behrens <thorsten.behrens@allotropia.de>

--- sc/source/core/tool/interpr7.cxx
+++ sc/source/core/tool/interpr7.cxx
@@ -219,6 +219,7 @@ void ScInterpreter::ScFilterXML()
         case XPATH_STRING:
             PushString(OUString::createFromAscii(reinterpret_cast<char*>(pXPathObj->stringval)));
             break;
+#if LIBXML_VERSION < 21000 || defined(LIBXML_XPTR_LOCS_ENABLED)
         case XPATH_POINT:
             PushNoValue();
             break;
@@ -228,6 +229,7 @@ void ScInterpreter::ScFilterXML()
         case XPATH_LOCATIONSET:
             PushNoValue();
             break;
+#endif
         case XPATH_USERS:
             PushNoValue();
             break;
