LibreOffice/core

commit c4268efef25129b162884312b15527f1e5c3bcb0

sw content controls: add UNO API to insert this
This only allows the default richText type, the others are not yet
handled.

Change-Id: I39f73ccd9e2c1f0db5735cf97301b01482063d1d
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/132350
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

sw/source/core/unocore/unoobj.cxx
@@ -54,6 +54,7 @@
 #include <unomap.hxx>
 #include <unoprnms.hxx>
 #include <unometa.hxx>
+#include <unocontentcontrol.hxx>
 #include <unotext.hxx>
 #include <com/sun/star/text/TextMarkupType.hpp>
 #include <vcl/svapp.hxx>
@@ -804,6 +805,42 @@ bool SwXTextCursor::IsAtEndOfMeta() const
                     pXMeta->SetContentRange(pTextNode, nStart, nEnd) );
             OSL_ENSURE(bSuccess, "no pam?");
             if (bSuccess)
+            {
+                const SwPosition end(*pTextNode, nEnd);
+                if (   (*pCursor->GetPoint() == end)
+                    || (*pCursor->GetMark()  == end))
+                {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+bool SwXTextCursor::IsAtEndOfContentControl() const
+{
+    if (CursorType::ContentControl == m_eType)
+    {
+        auto pCursor( m_pUnoCursor );
+        auto pXContentControl(
+                dynamic_cast<SwXContentControl*>(m_xParentText.get()) );
+        if (!pXContentControl)
+        {
+            SAL_WARN("sw.core", "SwXTextCursor::IsAtEndOfContentControl: no content control");
+        }
+        if (pCursor && pXContentControl)
+        {
+            SwTextNode * pTextNode;
+            sal_Int32 nStart;
+            sal_Int32 nEnd;
+            const bool bSuccess(
+                    pXContentControl->SetContentRange(pTextNode, nStart, nEnd) );
+            if (!bSuccess)
+            {
+                SAL_WARN("sw.core", "SwXTextCursor::IsAtEndOfContentControl: no pam");
+            }
+            else
             {
                 const SwPosition end(*pTextNode, nEnd);
                 if (   (*pCursor->GetPoint() == end)

sw/qa/core/unocore/unocore.cxx
@@ -25,6 +25,7 @@
 #include <docsh.hxx>
 #include <ndtxt.hxx>
 #include <textlinebreak.hxx>
+#include <textcontentcontrol.hxx>
 
 using namespace ::com::sun::star;
 
@@ -307,6 +308,33 @@ CPPUNIT_TEST_FIXTURE(SwModelTestBase, testUserFieldTooltip)
     // - the property is of unexpected type or void: Title
     // i.e. reading of the tooltip was broken.
     CPPUNIT_ASSERT_EQUAL(aExpected, getProperty<OUString>(xFieldProps, "Title"));
+}
+
+CPPUNIT_TEST_FIXTURE(SwCoreUnocoreTest, testContentControlInsert)
+{
+    // Given an empty document:
+    SwDoc* pDoc = createSwDoc();
+
+    // When inserting a content control around one or more text portions:
+    uno::Reference<lang::XMultiServiceFactory> xMSF(mxComponent, uno::UNO_QUERY);
+    uno::Reference<text::XTextDocument> xTextDocument(mxComponent, uno::UNO_QUERY);
+    uno::Reference<text::XText> xText = xTextDocument->getText();
+    uno::Reference<text::XTextCursor> xCursor = xText->createTextCursor();
+    xText->insertString(xCursor, "test", /*bAbsorb=*/false);
+    xCursor->gotoStart(/*bExpand=*/false);
+    xCursor->gotoEnd(/*bExpand=*/true);
+    uno::Reference<text::XTextContent> xContentControl(
+        xMSF->createInstance("com.sun.star.text.ContentControl"), uno::UNO_QUERY);
+    xText->insertTextContent(xCursor, xContentControl, /*bAbsorb=*/true);
+
+    // Then make sure that the text attribute is inserted:
+    SwWrtShell* pWrtShell = pDoc->GetDocShell()->GetWrtShell();
+    SwNodeOffset nIndex = pWrtShell->GetCursor()->GetNode().GetIndex();
+    SwTextNode* pTextNode = pDoc->GetNodes()[nIndex]->GetTextNode();
+    SwTextAttr* pAttr = pTextNode->GetTextAttrForCharAt(0, RES_TXTATR_CONTENTCONTROL);
+    // Without the accompanying fix in place, this test would have failed, as the
+    // SwXContentControl::attach() implementation was missing.
+    CPPUNIT_ASSERT(pAttr);
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();

sw/Library_sw.mk
@@ -481,6 +481,7 @@ $(eval $(call gb_Library_add_exception_objects,sw,\
     sw/source/core/unocore/unobkm \
     sw/source/core/unocore/unochart \
     sw/source/core/unocore/unocoll \
+    sw/source/core/unocore/unocontentcontrol \
     sw/source/core/unocore/unocrsr \
     sw/source/core/unocore/unocrsrhelper \
     sw/source/core/unocore/unodraw \

sw/inc/unobaseclass.hxx
@@ -50,6 +50,7 @@ enum class CursorType
                         // a text range or cursor
     SelectionInTable,
     Meta,         // meta/meta-field
+    ContentControl,
 };
 
 /*

sw/inc/unocoll.hxx
@@ -177,6 +177,7 @@ enum class SwServiceType {
     StyleTable                      = 114,
     StyleCell                       = 115,
     LineBreak                       = 116,
+    ContentControl                  = 117,
 
     Invalid                         = USHRT_MAX
 };

sw/inc/unotextcursor.hxx
@@ -100,6 +100,7 @@ public:
 
     SwUnoCursor& GetCursor();
     bool IsAtEndOfMeta() const;
+    bool IsAtEndOfContentControl() const;
 
     void DeleteAndInsert(OUString const& rText,
                 const bool bForceExpandHints);

sw/source/core/inc/unocontentcontrol.hxx
@@ -1,0 +1,139 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#pragma once
+
+#include <memory>
+#include <deque>
+
+#include <com/sun/star/lang/XServiceInfo.hpp>
+#include <com/sun/star/lang/XUnoTunnel.hpp>
+#include <com/sun/star/beans/XPropertySet.hpp>
+#include <com/sun/star/container/XChild.hpp>
+#include <com/sun/star/container/XEnumerationAccess.hpp>
+#include <com/sun/star/text/XTextContent.hpp>
+#include <com/sun/star/text/XTextField.hpp>
+
+#include <cppuhelper/implbase.hxx>
+
+#include <unobaseclass.hxx>
+
+typedef std::deque<css::uno::Reference<css::text::XTextRange>> TextRangeList_t;
+
+class SwPaM;
+class SwTextNode;
+class SwContentControl;
+
+/**
+ * UNO API wrapper around an SwContentControl, exposed as the com.sun.star.text.ContentControl
+ * service.
+ */
+class SwXContentControl
+    : public cppu::WeakImplHelper<css::lang::XUnoTunnel, css::lang::XServiceInfo,
+                                  css::container::XEnumerationAccess, css::text::XTextContent,
+                                  css::text::XText>
+{
+public:
+    class Impl;
+
+protected:
+    sw::UnoImplPtr<Impl> m_pImpl;
+
+    void AttachImpl(const css::uno::Reference<css::text::XTextRange>& xTextRange,
+                    sal_uInt16 nWhich);
+
+    ~SwXContentControl() override;
+
+    SwXContentControl(const SwXContentControl&) = delete;
+    SwXContentControl& operator=(const SwXContentControl&) = delete;
+
+    SwXContentControl(SwDoc* pDoc, SwContentControl* pContentControl,
+                      const css::uno::Reference<css::text::XText>& xParentText,
+                      std::unique_ptr<const TextRangeList_t> pPortions);
+
+    SwXContentControl(SwDoc* pDoc);
+
+public:
+    static css::uno::Reference<css::text::XTextContent>
+    CreateXContentControl(SwContentControl& rContentControl,
+                          const css::uno::Reference<css::text::XText>& xParentText = nullptr,
+                          std::unique_ptr<const TextRangeList_t>&& pPortions
+                          = std::unique_ptr<const TextRangeList_t>());
+
+    static css::uno::Reference<css::text::XTextContent> CreateXContentControl(SwDoc& rDoc);
+
+    /// Initializes params with position of the attribute content (without CH_TXTATR).
+    bool SetContentRange(SwTextNode*& rpNode, sal_Int32& rStart, sal_Int32& rEnd) const;
+    css::uno::Reference<css::text::XText> GetParentText() const;
+
+    bool CheckForOwnMemberContentControl(const SwPaM& rPam, const bool bAbsorb);
+
+    static const css::uno::Sequence<sal_Int8>& getUnoTunnelId();
+
+    // XUnoTunnel
+    sal_Int64 SAL_CALL getSomething(const css::uno::Sequence<sal_Int8>& Identifier) override;
+
+    // XServiceInfo
+    OUString SAL_CALL getImplementationName() override;
+    sal_Bool SAL_CALL supportsService(const OUString& rServiceName) override;
+    css::uno::Sequence<OUString> SAL_CALL getSupportedServiceNames() override;
+
+    // XComponent
+    void SAL_CALL dispose() override;
+    void SAL_CALL
+    addEventListener(const css::uno::Reference<css::lang::XEventListener>& xListener) override;
+    void SAL_CALL
+    removeEventListener(const css::uno::Reference<css::lang::XEventListener>& xListener) override;
+
+    // XElementAccess
+    css::uno::Type SAL_CALL getElementType() override;
+    sal_Bool SAL_CALL hasElements() override;
+
+    // XEnumerationAccess
+    css::uno::Reference<css::container::XEnumeration> SAL_CALL createEnumeration() override;
+
+    // XTextContent
+    void SAL_CALL attach(const css::uno::Reference<css::text::XTextRange>& xTextRange) override;
+    css::uno::Reference<css::text::XTextRange> SAL_CALL getAnchor() override;
+
+    // XTextRange
+    css::uno::Reference<css::text::XText> SAL_CALL getText() override;
+    css::uno::Reference<css::text::XTextRange> SAL_CALL getStart() override;
+    css::uno::Reference<css::text::XTextRange> SAL_CALL getEnd() override;
+    OUString SAL_CALL getString() override;
+    void SAL_CALL setString(const OUString& rString) override;
+
+    // XSimpleText
+    css::uno::Reference<css::text::XTextCursor> SAL_CALL createTextCursor() override;
+    css::uno::Reference<css::text::XTextCursor> SAL_CALL createTextCursorByRange(
+        const css::uno::Reference<css::text::XTextRange>& xTextPosition) override;
+    void SAL_CALL insertString(const css::uno::Reference<css::text::XTextRange>& xRange,
+                               const OUString& aString, sal_Bool bAbsorb) override;
+    void SAL_CALL insertControlCharacter(const css::uno::Reference<css::text::XTextRange>& xRange,
+                                         sal_Int16 nControlCharacter, sal_Bool bAbsorb) override;
+
+    // XText
+    void SAL_CALL insertTextContent(const css::uno::Reference<css::text::XTextRange>& xRange,
+                                    const css::uno::Reference<css::text::XTextContent>& xContent,
+                                    sal_Bool bAbsorb) override;
+    void SAL_CALL
+    removeTextContent(const css::uno::Reference<css::text::XTextContent>& xContent) override;
+};
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sw/source/core/unocore/unocoll.cxx
@@ -56,6 +56,7 @@
 #include <unobookmark.hxx>
 #include <unorefmark.hxx>
 #include <unometa.hxx>
+#include <unocontentcontrol.hxx>
 #include <docsh.hxx>
 #include <hints.hxx>
 #include <frameformats.hxx>
@@ -455,7 +456,8 @@ const ProvNamesId_Type aProvNamesId[] =
     { CSS_TEXT_FIELDMASTER_BIBLIOGRAPHY,              SwServiceType::FieldMasterBibliography },
     { "com.sun.star.style.TableStyle",                SwServiceType::StyleTable },
     { "com.sun.star.style.CellStyle",                 SwServiceType::StyleCell },
-    { "com.sun.star.text.LineBreak",                  SwServiceType::LineBreak }
+    { "com.sun.star.text.LineBreak",                  SwServiceType::LineBreak },
+    { "com.sun.star.text.ContentControl",             SwServiceType::ContentControl }
 };
 
 const SvEventDescription* sw_GetSupportedMacroItems()
@@ -828,6 +830,9 @@ SwXServiceProvider::MakeInstance(SwServiceType nObjectType, SwDoc & rDoc)
         case SwServiceType::LineBreak:
             xRet = SwXLineBreak::CreateXLineBreak(nullptr);
             break;
+        case SwServiceType::ContentControl:
+            xRet = SwXContentControl::CreateXContentControl(rDoc);
+        break;
         default:
             throw uno::RuntimeException();
     }

commit 08bce8eeb2e18c7cea4108dbfc2519ada56e8b61

sw content controls: fix getString() and setString() on SwXContentControl
The setter was broken, it inserted the new content at the start, but
didn't delete the old content. The getter was also broken, it returned
an empty string, even if the content control had a non-empty content.

This was always a problem since c4268efef25129b162884312b15527f1e5c3bcb0
(sw content controls: add UNO API to insert this, 2022-03-31) introduced
SwXContentControl.

Fix the problem by extending SwXTextCursor, so in case m_eType is
CursorType::ContentControl, then it keeps the cursor's range inside the
boundaries of the content control.

This also allows changing the data binding code added in
b5c616d10bff3213840d4893d13b4493de71fa56 (tdf#104823: support for sdt
plain text fields, 2021-11-24) to use content controls, in a follow-up
commit.

Change-Id: Ic3ae47a4b26af090539d61035372f1784c8ffe3f
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/140040
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

sw/source/core/unocore/unoobj.cxx
@@ -746,6 +746,12 @@ namespace {
 
 enum ForceIntoMetaMode { META_CHECK_BOTH, META_INIT_START, META_INIT_END };
 
+enum ForceIntoContentControlMode
+{
+    CONTENT_CONTROL_CHECK_BOTH,
+    CONTENT_CONTROL_INIT_START,
+    CONTENT_CONTROL_INIT_END
+};
 }
 
 static bool
@@ -790,6 +796,61 @@ lcl_ForceIntoMeta(SwPaM & rCursor,
             break;
     }
     return bRet;
+}
+
+namespace
+{
+bool lcl_ForceIntoContentControl(SwPaM& rCursor, const uno::Reference<text::XText>& xParentText,
+                                 ForceIntoContentControlMode eMode)
+{
+    bool bRet = true; // means not forced in CONTENT_CONTROL_CHECK_BOTH
+    auto pXContentControl = dynamic_cast<SwXContentControl*>(xParentText.get());
+    if (!pXContentControl)
+    {
+        SAL_WARN("sw.core", "lcl_ForceIntoContentControl: no parent text");
+        throw uno::RuntimeException();
+    }
+
+    SwTextNode* pTextNode;
+    sal_Int32 nStart;
+    sal_Int32 nEnd;
+    bool bSuccess = pXContentControl->SetContentRange(pTextNode, nStart, nEnd);
+    if (!bSuccess)
+    {
+        SAL_WARN("sw.core", "lcl_ForceIntoContentControl: SetContentRange() failed");
+        throw uno::RuntimeException();
+    }
+
+    // Force the cursor back into the content control if it has moved outside.
+    SwPosition aStart(*pTextNode, nStart);
+    SwPosition aEnd(*pTextNode, nEnd);
+    switch (eMode)
+    {
+        case CONTENT_CONTROL_INIT_START:
+            *rCursor.GetPoint() = aStart;
+            break;
+
+        case CONTENT_CONTROL_INIT_END:
+            *rCursor.GetPoint() = aEnd;
+            break;
+
+        case CONTENT_CONTROL_CHECK_BOTH:
+            if (*rCursor.Start() < aStart)
+            {
+                *rCursor.Start() = aStart;
+                bRet = false;
+            }
+
+            if (*rCursor.End() > aEnd)
+            {
+                *rCursor.End() = aEnd;
+                bRet = false;
+            }
+            break;
+    }
+
+    return bRet;
+}
 }
 
 bool SwXTextCursor::IsAtEndOfMeta() const
@@ -956,6 +1017,11 @@ SwXTextCursor::goLeft(sal_Int16 nCount, sal_Bool Expand)
                     META_CHECK_BOTH)
             && bRet;
     }
+    else if (m_eType == CursorType::ContentControl)
+    {
+        bRet = lcl_ForceIntoContentControl(rUnoCursor, m_xParentText, CONTENT_CONTROL_CHECK_BOTH)
+               && bRet;
+    }
     return bRet;
 }
 
@@ -973,6 +1039,11 @@ SwXTextCursor::goRight(sal_Int16 nCount, sal_Bool Expand)
         bRet = lcl_ForceIntoMeta(rUnoCursor, m_xParentText,
                     META_CHECK_BOTH)
             && bRet;
+    }
+    else if (m_eType == CursorType::ContentControl)
+    {
+        bRet = lcl_ForceIntoContentControl(rUnoCursor, m_xParentText, CONTENT_CONTROL_CHECK_BOTH)
+               && bRet;
     }
     return bRet;
 }
@@ -1023,6 +1094,10 @@ SwXTextCursor::gotoStart(sal_Bool Expand)
     {
         lcl_ForceIntoMeta(rUnoCursor, m_xParentText, META_INIT_START);
     }
+    else if (m_eType == CursorType::ContentControl)
+    {
+        lcl_ForceIntoContentControl(rUnoCursor, m_xParentText, CONTENT_CONTROL_INIT_START);
+    }
 }
 
 void SAL_CALL
@@ -1050,6 +1125,10 @@ SwXTextCursor::gotoEnd(sal_Bool Expand)
     else if (CursorType::Meta == m_eType)
     {
         lcl_ForceIntoMeta(rUnoCursor, m_xParentText, META_INIT_END);
+    }
+    else if (m_eType == CursorType::ContentControl)
+    {
+        lcl_ForceIntoContentControl(rUnoCursor, m_xParentText, CONTENT_CONTROL_INIT_END);
     }
 }
 
@@ -1158,6 +1237,15 @@ SwXTextCursor::gotoRange(
                 static_cast<text::XWordCursor*>(this));
         }
     }
+    else if (m_eType == CursorType::ContentControl)
+    {
+        SwPaM aPaM(*pPam->GetMark(), *pPam->GetPoint());
+        if (!lcl_ForceIntoContentControl(aPaM, m_xParentText, CONTENT_CONTROL_CHECK_BOTH))
+        {
+            throw uno::RuntimeException("gotoRange: xRange is out of bounds of the content control",
+                                        static_cast<text::XWordCursor*>(this));
+        }
+    }
 
     // selection has to be expanded here
     if(bExpand)
@@ -1261,6 +1349,10 @@ SwXTextCursor::gotoNextWord(sal_Bool Expand)
         bRet = lcl_ForceIntoMeta(rUnoCursor, m_xParentText,
                     META_CHECK_BOTH);
     }
+    else if (bRet && m_eType == CursorType::ContentControl)
+    {
+        bRet = lcl_ForceIntoContentControl(rUnoCursor, m_xParentText, CONTENT_CONTROL_CHECK_BOTH);
+    }
 
     return bRet;
 }
@@ -1301,6 +1393,10 @@ SwXTextCursor::gotoPreviousWord(sal_Bool Expand)
         bRet = lcl_ForceIntoMeta(rUnoCursor, m_xParentText,
                     META_CHECK_BOTH);
     }
+    else if (bRet && m_eType == CursorType::ContentControl)
+    {
+        bRet = lcl_ForceIntoContentControl(rUnoCursor, m_xParentText, CONTENT_CONTROL_CHECK_BOTH);
+    }
 
     return bRet;
 }
@@ -1336,6 +1432,10 @@ SwXTextCursor::gotoEndOfWord(sal_Bool Expand)
         bRet = lcl_ForceIntoMeta(rUnoCursor, m_xParentText,
                     META_CHECK_BOTH);
     }
+    else if (m_eType == CursorType::ContentControl)
+    {
+        bRet = lcl_ForceIntoContentControl(rUnoCursor, m_xParentText, CONTENT_CONTROL_CHECK_BOTH);
+    }
 
     return bRet;
 }
@@ -1370,6 +1470,10 @@ SwXTextCursor::gotoStartOfWord(sal_Bool Expand)
     {
         bRet = lcl_ForceIntoMeta(rUnoCursor, m_xParentText,
                     META_CHECK_BOTH);
+    }
+    else if (m_eType == CursorType::ContentControl)
+    {
+        bRet = lcl_ForceIntoContentControl(rUnoCursor, m_xParentText, CONTENT_CONTROL_CHECK_BOTH);
     }
 
     return bRet;
@@ -1454,6 +1558,11 @@ SwXTextCursor::gotoNextSentence(sal_Bool Expand)
                     META_CHECK_BOTH)
             && bRet;
     }
+    else if (m_eType == CursorType::ContentControl)
+    {
+        bRet = lcl_ForceIntoContentControl(rUnoCursor, m_xParentText, CONTENT_CONTROL_CHECK_BOTH)
+               && bRet;
+    }
     return bRet;
 }
 
@@ -1482,6 +1591,11 @@ SwXTextCursor::gotoPreviousSentence(sal_Bool Expand)
                     META_CHECK_BOTH)
             && bRet;
     }
+    else if (m_eType == CursorType::ContentControl)
+    {
+        bRet = lcl_ForceIntoContentControl(rUnoCursor, m_xParentText, CONTENT_CONTROL_CHECK_BOTH)
+               && bRet;
+    }
     return bRet;
 }
 
@@ -1504,6 +1618,11 @@ SwXTextCursor::gotoStartOfSentence(sal_Bool Expand)
         bRet = lcl_ForceIntoMeta(rUnoCursor, m_xParentText,
                     META_CHECK_BOTH)
             && bRet;
+    }
+    else if (m_eType == CursorType::ContentControl)
+    {
+        bRet = lcl_ForceIntoContentControl(rUnoCursor, m_xParentText, CONTENT_CONTROL_CHECK_BOTH)
+               && bRet;
     }
     return bRet;
 }
@@ -1528,6 +1647,11 @@ SwXTextCursor::gotoEndOfSentence(sal_Bool Expand)
         bRet = lcl_ForceIntoMeta(rUnoCursor, m_xParentText,
                     META_CHECK_BOTH)
             && bRet;
+    }
+    else if (m_eType == CursorType::ContentControl)
+    {
+        bRet = lcl_ForceIntoContentControl(rUnoCursor, m_xParentText, CONTENT_CONTROL_CHECK_BOTH)
+               && bRet;
     }
     return bRet;
 }

sw/qa/core/unocore/unocore.cxx
@@ -345,6 +345,15 @@ CPPUNIT_TEST_FIXTURE(SwCoreUnocoreTest, testContentControlInsert)
         = static_cast<SwFormatContentControl&>(pTextContentControl->GetAttr());
     std::shared_ptr<SwContentControl> pContentControl = rFormatContentControl.GetContentControl();
     CPPUNIT_ASSERT(pContentControl->GetShowingPlaceHolder());
+
+    // Also verify that setText() and getText() works:
+    uno::Reference<text::XText> xContentControlText(xContentControl, uno::UNO_QUERY);
+    xContentControlText->setString("new");
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: new
+    // - Actual  :
+    // i.e. getString() always returned an empty string.
+    CPPUNIT_ASSERT_EQUAL(OUString("new"), xContentControlText->getString());
 }
 
 CPPUNIT_TEST_FIXTURE(SwModelTestBase, testImageTooltip)