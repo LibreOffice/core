LibreOffice/core

commit bf3124cdf61b40c94ba117a76f1b63dabdfd528e

tdf#135187 DOCX import: fix table style at character formatting
applied also on paragraph level during import.

Regression from commit 4d5c0eaf3e0d3d3bcd9e691fffee19b75f3d6631
(tdf#118812 DOCX import: fix table style preference – part 2)

See also commit 5ac6f02fdc6015a5d78071570dee310febf95fc6
(tdf#105215 DOCX import: fix paragraph-length direct formatting).

Change-Id: I14072c81fc4c54e376a004fa36ba76b56d2beb01
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/100996
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

writerfilter/source/dmapper/DomainMapperTableHandler.cxx
@@ -1094,7 +1094,8 @@ void DomainMapperTableHandler::ApplyParagraphPropertiesFromTableStyle(TableParag
     {
         // apply paragraph and character properties of the table style on table paragraphs
         // if there is no direct paragraph formatting
-        if ( !rParaProp.m_pPropertyMap->isSet(eId) )
+        bool bIsParaLevel = rParaProp.m_pPropertyMap->isSet(eId);
+        if ( !bIsParaLevel || isCharacterProperty(eId) )
         {
             if ( (eId == PROP_PARA_LEFT_MARGIN || eId == PROP_PARA_FIRST_LINE_INDENT) &&
                     rParaProp.m_pPropertyMap->isSet(PROP_NUMBERING_RULES) )
@@ -1104,6 +1105,15 @@ void DomainMapperTableHandler::ApplyParagraphPropertiesFromTableStyle(TableParag
             }
 
             OUString sPropertyName = getPropertyName(eId);
+
+            if ( bIsParaLevel && ( rParaProp.m_aParaOverrideApplied.find(sPropertyName) != rParaProp.m_aParaOverrideApplied.end() ||
+                sPropertyName.startsWith("CharFontName") ) )
+            {
+                // don't apply table style, if this character property was applied on paragraph level
+                // (or in the case of paragraph level font name settings to avoid regressions)
+                continue;
+            }
+
             auto pCellProp = std::find_if(rCellProperties.begin(), rCellProperties.end(),
                 [&](const beans::PropertyValue& rProp) { return rProp.Name == sPropertyName; });
             // this cell applies the table style property

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -1877,7 +1877,7 @@ void DomainMapper_Impl::finishParagraph( const PropertyMapPtr& pPropertyMap, con
                     xParaCursor->gotoStartOfParagraph(false);
                     if (m_nTableDepth > 0)
                     {
-                        TableParagraph aPending{xParaCursor, xCur, pParaContext, xParaProps};
+                        TableParagraph aPending{xParaCursor, xCur, pParaContext, xParaProps, std::set<OUString>()};
                         getTableManager().getCurrentParagraphs()->push_back(aPending);
                     }
 
@@ -1995,6 +1995,8 @@ void DomainMapper_Impl::finishParagraph( const PropertyMapPtr& pPropertyMap, con
                         {
                             uno::Reference<beans::XPropertySet> xRunPropertySet(xCur, uno::UNO_QUERY);
                             xParaProps->setPropertyValue( rParaProp.Name, xRunPropertySet->getPropertyValue(rParaProp.Name) );
+                            // remember this for table style handling
+                            getTableManager().getCurrentParagraphs()->back().m_aParaOverrideApplied.insert(rParaProp.Name);
                         }
                     }
 

writerfilter/source/dmapper/PropertyMap.hxx
@@ -33,6 +33,7 @@
 #include <optional>
 #include <map>
 #include <vector>
+#include <set>
 
 namespace com::sun::star {
     namespace beans {
@@ -594,6 +595,7 @@ struct TableParagraph
     css::uno::Reference<css::text::XTextRange> m_rEndParagraph;
     PropertyMapPtr m_pPropertyMap;
     css::uno::Reference<css::beans::XPropertySet> m_rPropertySet;
+    std::set<OUString> m_aParaOverrideApplied;
 };
 
 typedef std::shared_ptr< std::vector<TableParagraph> > TableParagraphVectorPtr;

sw/qa/extras/ooxmlexport/ooxmlexport11.cxx
@@ -1222,6 +1222,23 @@ DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testTdf105215, "tdf105215.docx")
     assertXPath(pXmlDoc, "/w:document/w:body/w:tbl/w:tr/w:tc/w:p/w:r[5]/w:rPr/w:rFonts", "ascii", "Lohit Devanagari");
 }
 
+DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testTdf135187, "tdf135187.docx")
+{
+    xmlDocUniquePtr pXmlDoc = parseExport("word/document.xml");
+    assertXPath(pXmlDoc, "/w:document/w:body/w:tbl/w:tr[2]/w:tc[1]/w:p/w:pPr/w:rPr/w:b", 0);
+    // FIXME: remove duplicate
+    assertXPath(pXmlDoc, "/w:document/w:body/w:tbl/w:tr[3]/w:tc[1]/w:p/w:pPr/w:rPr/w:b", 2);
+    assertXPath(pXmlDoc, "/w:document/w:body/w:tbl/w:tr[4]/w:tc[1]/w:p/w:pPr/w:rPr/w:b", 2);
+
+    assertXPath(pXmlDoc, "/w:document/w:body/w:tbl/w:tr[2]/w:tc[1]/w:p/w:r[1]/w:rPr/w:b", 1);
+    assertXPathNoAttribute(pXmlDoc, "/w:document/w:body/w:tbl/w:tr[2]/w:tc[1]/w:p/w:r[1]/w:rPr/w:b", "val");
+    // This was 0
+    assertXPath(pXmlDoc, "/w:document/w:body/w:tbl/w:tr[3]/w:tc[1]/w:p/w:r[1]/w:rPr/w:b", 1);
+    assertXPathNoAttribute(pXmlDoc, "/w:document/w:body/w:tbl/w:tr[3]/w:tc[1]/w:p/w:r[1]/w:rPr/w:b", "val");
+    assertXPath(pXmlDoc, "/w:document/w:body/w:tbl/w:tr[4]/w:tc[1]/w:p/w:r[1]/w:rPr/w:b", 1);
+    assertXPath(pXmlDoc, "/w:document/w:body/w:tbl/w:tr[4]/w:tc[1]/w:p/w:r[1]/w:rPr/w:b", "val", "false");
+}
+
 DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testTdf121597TrackedDeletionOfMultipleParagraphs, "tdf121597.odt")
 {
     CPPUNIT_ASSERT_EQUAL(1, getPages());

commit 2a3e528bb9df4a1a2564ca5236be62dfdaba77e6

tdf#136617 DOCX import: fix empty table paragraphs with direct formatting
Table paragraph styles applied on paragraphs without runs, i.e.
on empty paragraphs, despite their direct formatting, resulting
e.g. different font size, and after that different paragraph
i.e. cell height, breaking the original table layout.

Regression from commit bf3124cdf61b40c94ba117a76f1b63dabdfd528e
"tdf#135187 DOCX import: fix table style at character formatting".

Also clean-up commit 498d2b82187ec3ff58f076e0d15741e64c0505ba
"tdf#131546 DOCX import: fix performance regression at tables".

Change-Id: I41f8da61d49902c8c1683bd01632cce6d58aebd9
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/141052
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

writerfilter/source/dmapper/DomainMapperTableHandler.cxx
@@ -1112,14 +1112,6 @@ void DomainMapperTableHandler::ApplyParagraphPropertiesFromTableStyle(TableParag
 
             OUString sPropertyName = getPropertyName(eId);
 
-            if ( bIsParaLevel && ( rParaProp.m_aParaOverrideApplied.find(sPropertyName) != rParaProp.m_aParaOverrideApplied.end() ||
-                sPropertyName.startsWith("CharFontName") ) )
-            {
-                // don't apply table style, if this character property was applied on paragraph level
-                // (or in the case of paragraph level font name settings to avoid regressions)
-                continue;
-            }
-
             auto pCellProp = std::find_if(rCellProperties.begin(), rCellProperties.end(),
                 [&](const beans::PropertyValue& rProp) { return rProp.Name == sPropertyName; });
             // this cell applies the table style property
@@ -1179,6 +1171,10 @@ void DomainMapperTableHandler::ApplyParagraphPropertiesFromTableStyle(TableParag
                     uno::Reference< beans::XPropertyState > xParaProperties( xParagraph, uno::UNO_QUERY_THROW );
                     if ( xParaProperties->getPropertyState(sPropertyName) == css::beans::PropertyState_DEFAULT_VALUE )
                     {
+                        // don't overwrite empty paragraph with table style, if it has a direct paragraph formatting
+                        if ( bIsParaLevel && xParagraph->getString().getLength() == 0 )
+                            continue;
+
                         if ( eId != PROP_FILL_COLOR )
                         {
                             // apply style setting when the paragraph doesn't modify it

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -2462,7 +2462,7 @@ void DomainMapper_Impl::finishParagraph( const PropertyMapPtr& pPropertyMap, con
                     xParaCursor->gotoStartOfParagraph(false);
                     if (m_nTableDepth > 0)
                     {
-                        TableParagraph aPending{xParaCursor, xCur, pParaContext, xParaProps, std::set<OUString>()};
+                        TableParagraph aPending{xParaCursor, xCur, pParaContext, xParaProps};
                         getTableManager().getCurrentParagraphs()->push_back(aPending);
                     }
 

writerfilter/source/dmapper/PropertyMap.hxx
@@ -606,7 +606,6 @@ struct TableParagraph
     css::uno::Reference<css::text::XTextRange> m_rEndParagraph;
     PropertyMapPtr m_pPropertyMap;
     css::uno::Reference<css::beans::XPropertySet> m_rPropertySet;
-    std::set<OUString> m_aParaOverrideApplied;
 };
 
 typedef std::shared_ptr< std::vector<TableParagraph> > TableParagraphVectorPtr;

sw/qa/extras/ooxmlexport/ooxmlexport12.cxx
@@ -1874,6 +1874,18 @@ CPPUNIT_TEST_FIXTURE(Test, testTdf135187)
                 "false");
 }
 
+CPPUNIT_TEST_FIXTURE(Test, testTdf136617)
+{
+    loadAndSave("tdf136617.docx");
+
+    // This was 2
+    CPPUNIT_ASSERT_EQUAL(1, getPages());
+
+    xmlDocUniquePtr pXmlDoc = parseExport("word/document.xml");
+    assertXPath(pXmlDoc, "/w:document/w:body/w:tbl/w:tr[2]/w:tc[2]/w:p[2]/w:pPr/w:rPr/w:sz", "val",
+                "16");
+}
+
 CPPUNIT_TEST_FIXTURE(Test, testTdf121597TrackedDeletionOfMultipleParagraphs)
 {
     loadAndSave("tdf121597.odt");