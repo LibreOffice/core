LibreOffice/core

commit 8611f6e259b807b4f19c8dc0eab86ca648891ce3

ref-count SdrObject
Which means we can get rid of the majestic hack of ScCaptionPtr
Previously, SdrObject was manually managed, and the ownership
passed around in very complicated fashion.

Notes:

(*) SvxShape has a strong reference to SdrObject, where
previously it had a weak reference. It is now strong
since otherwise the SdrObject will go away very eagerly.

(*) SdrObject still has a weak reference to SvxShape

(*) In the existing places that an SdrObject is being
deleted, we now just clear the reference

(*) instead of SwVirtFlyDrawObj removing itself from the
page that contains inside it's destructor, make the call site
do the removing from the page.

(*) Needed to take the SolarMutex in UndoManagerHelper_Impl::impl_clear
because this can be called from UNO (e.g. sfx2_complex JUnit test)
and the SdrObjects need the SolarMutex when destructing.

(*) handle a tricky situation with SwDrawVirtObj in the SwDrawModel
destructor because the existing code wants mpDrawObj in
SwAnchoredObject to be sometimes owning, sometimes not, which
results in a cycle with the new code.

Change-Id: I4d79df1660e386388e5d51030653755bca02a163
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/138837
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svx/source/svdraw/svdouno.cxx
@@ -280,7 +280,7 @@ OUString SdrUnoObj::TakeObjNamePlural() const
     return SvxResId(STR_ObjNamePluralUno);
 }
 
-SdrUnoObj* SdrUnoObj::CloneSdrObject(SdrModel& rTargetModel) const
+rtl::Reference<SdrObject> SdrUnoObj::CloneSdrObject(SdrModel& rTargetModel) const
 {
     return new SdrUnoObj(rTargetModel, *this);
 }

commit 1a86be908952aea376d37473adbab091a5440a35

tdf#151417 crash in draw, activating Form controls
regression from
    commit 8611f6e259b807b4f19c8dc0eab86ca648891ce3
    Author: Noel Grandin <noel.grandin@collabora.co.uk>
    Date:   Thu May 27 10:27:46 2021 +0200
    ref-count SdrObject

Change-Id: I1798cead8ed72ad59799c40d0e99f3424f9e382b
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/141134
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svx/source/svdraw/svdouno.cxx
@@ -139,6 +139,7 @@ SdrUnoObj::SdrUnoObj(
 :   SdrRectObj(rSdrModel),
     m_pImpl( new SdrUnoObjDataHolder )
 {
+    osl_atomic_increment(&m_refCount); // prevent deletion during creation
     m_bIsUnoObj = true;
 
     m_pImpl->pEventListener = new SdrControlEventListenerImpl(this);
@@ -146,6 +147,7 @@ SdrUnoObj::SdrUnoObj(
     // only an owner may create independently
     if (!rModelName.isEmpty())
         CreateUnoControlModel(rModelName);
+    osl_atomic_decrement(&m_refCount);
 }
 
 SdrUnoObj::SdrUnoObj( SdrModel& rSdrModel, SdrUnoObj const & rSource)