bdfcad586d7b1d4d26bcba50058af7fcb279558d
Andreas Heinisch <andreas.heinisch@yahoo.de>
2022-10-13 14:20:00 +0200

    tdf#151503 - Do not evaluate a missing optional variable to a boolean
    Change-Id: I671f857344f91de63612eabcbbdb2cab9b94cc0d
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/141296
    Tested-by: Jenkins
    Reviewed-by: Andreas Heinisch <andreas.heinisch@yahoo.de>

--- basic/source/runtime/runtime.cxx
+++ basic/source/runtime/runtime.cxx
@@ -3022,6 +3022,14 @@ bool SbiRuntime::EvaluateTopOfStackAsBool()
     {
         return false;
     }
+
+    // tdf#151503 - do not evaluate a missing optional variable to a boolean
+    if (tos->GetType() == SbxERROR && IsMissing(tos.get(), 1))
+    {
+        Error(ERRCODE_BASIC_NOT_OPTIONAL);
+        return false;
+    }
+
     if ( tos->IsObject() )
     {
         //GetBool applied to an Object attempts to dereference and evaluate
