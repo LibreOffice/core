6629197f08249725e9fef4f0c5fee3c98657d838
Emmanuel Peter <awinspeters@gmail.com>
2022-10-13 16:53:55 +0100

    tdf#143148: Use pragma once instead of include guards
    Change-Id: Idf9f5b604bc60d7f1bacdd0001ce252aaeb7835a
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/141334
    Tested-by: Jenkins
    Tested-by: Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>
    Reviewed-by: Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>

--- vcl/source/filter/jpeg/JpegWriter.hxx
+++ vcl/source/filter/jpeg/JpegWriter.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_VCL_SOURCE_FILTER_JPEG_JPEGWRITER_HXX
-#define INCLUDED_VCL_SOURCE_FILTER_JPEG_JPEGWRITER_HXX
+#pragma once
 
 #include <vcl/bitmap.hxx>
 #include <vcl/BitmapReadAccess.hxx>
@@ -51,7 +50,5 @@ public:
     bool    Write( const Graphic& rGraphic );
 
 };
-
-#endif // INCLUDED_VCL_SOURCE_FILTER_JPEG_JPEGWRITER_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
