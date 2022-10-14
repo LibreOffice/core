0d3b5ebefb77ebb5738467a1a72d0cc2605403ed
Mike Kaganski <mike.kaganski@collabora.com>
2022-10-14 07:40:28 +0300

    This can be const
    Change-Id: I5e0cb22c9276e41b0a42978a253456ebd9ebefa4
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/141356
    Tested-by: Jenkins
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
    (cherry picked from commit 934584439de8331ad7c0af13c17e6892df1d1367)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/141316
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

--- include/sfx2/lokcomponenthelpers.hxx
+++ include/sfx2/lokcomponenthelpers.hxx
@@ -67,7 +67,7 @@ public:
 class SFX2_DLLPUBLIC LokStarMathHelper
 {
 public:
-    LokStarMathHelper(SfxViewShell* pViewShell)
+    LokStarMathHelper(const SfxViewShell* pViewShell)
         : mpViewShell(pViewShell)
     {
     }
@@ -84,7 +84,7 @@ private:
     css::uno::Reference<css::frame::XController>& GetXController();
     tools::Rectangle GetBoundingBox();
 
-    SfxViewShell* mpViewShell;
+    const SfxViewShell* mpViewShell;
     css::uno::Reference<css::frame::XController> mxController;
     VclPtr<vcl::Window> mpGraphicWindow;
     VclPtr<vcl::Window> mpWidgetWindow;
