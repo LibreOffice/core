df91cad99056f78496a611bcd9e5227503088adc
Justin Luth <justin.luth@collabora.com>
2022-10-13 21:21:08 -0400

    sw vba: move SwWordBasic to its own file
    We had implemented Application.WordBasic.*,
    but it can also run as a global WordBasic.*.
    
    This patch just separates it from vbaapplication.
    It was just a copy/paste, and then I ran
    opt/lo/bin/clang-format -i on the new files
    and did some minor cleanup.
    
    I just guessed at the includes I needed,
    and then commented the remaining ones out
    one by one to verifiy they were needed to compile.
    Not very scientific, but I don't know any other way.
    (running \bin\find-unneedded-includes afterwards
     didn't show anything - perhaps that would have helped.)
    
    A followup patch will add the VBAGlobal connection.
    
    For a clean backport, I first ran 1,$s/makeAny(/Any(/g
    in order to accomplish what was done with
    commit ca734f7cfa55814a85d5940e5f64d7c53638f6a7
    Author: Stephan Bergmann on Tue May 3 16:22:03 2022 +0200
        Just use Any ctor instead of makeAny in sw
        Change-Id: I2c9023ba8d07314d23ae7a65e670e8748c5e9322
    
    Change-Id: Id29ee582a5ed6186150bb41a1ab98d6c466676e7
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/141354
    Reviewed-by: Justin Luth <jluth@mail.com>
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

---
+++ sw/source/ui/vba/vbawordbasic.hxx
@@ -0,0 +1,81 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+#ifndef INCLUDED_SW_SOURCE_UI_VBA_VBAWORDBASIC_HXX
+#define INCLUDED_SW_SOURCE_UI_VBA_VBAWORDBASIC_HXX
+
+class SwVbaApplication;
+
+class SwWordBasic : public cppu::WeakImplHelper<ooo::vba::word::XWordBasic>
+{
+private:
+    SwVbaApplication* mpApp;
+
+public:
+    SwWordBasic(SwVbaApplication* pApp);
+
+    // XWordBasic
+    virtual sal_Int32 SAL_CALL getMailMergeMainDocumentType() override;
+    virtual void SAL_CALL
+    setMailMergeMainDocumentType(sal_Int32 _mailmergemaindocumenttype) override;
+
+    virtual void SAL_CALL FileOpen(const OUString& Name, const css::uno::Any& ConfirmConversions,
+                                   const css::uno::Any& ReadOnly, const css::uno::Any& AddToMru,
+                                   const css::uno::Any& PasswordDoc,
+                                   const css::uno::Any& PasswordDot, const css::uno::Any& Revert,
+                                   const css::uno::Any& WritePasswordDoc,
+                                   const css::uno::Any& WritePasswordDot) override;
+    virtual void SAL_CALL FileSave() override;
+    virtual void SAL_CALL FileSaveAs(
+        const css::uno::Any& Name, const css::uno::Any& Format, const css::uno::Any& LockAnnot,
+        const css::uno::Any& Password, const css::uno::Any& AddToMru,
+        const css::uno::Any& WritePassword, const css::uno::Any& RecommendReadOnly,
+        const css::uno::Any& EmbedFonts, const css::uno::Any& NativePictureFormat,
+        const css::uno::Any& FormsData, const css::uno::Any& SaveAsAOCELetter) override;
+    virtual void SAL_CALL FileClose(const css::uno::Any& Save) override;
+    virtual void SAL_CALL ToolsOptionsView(
+        const css::uno::Any& DraftFont, const css::uno::Any& WrapToWindow,
+        const css::uno::Any& PicturePlaceHolders, const css::uno::Any& FieldCodes,
+        const css::uno::Any& BookMarks, const css::uno::Any& FieldShading,
+        const css::uno::Any& StatusBar, const css::uno::Any& HScroll, const css::uno::Any& VScroll,
+        const css::uno::Any& StyleAreaWidth, const css::uno::Any& Tabs, const css::uno::Any& Spaces,
+        const css::uno::Any& Paras, const css::uno::Any& Hyphens, const css::uno::Any& Hidden,
+        const css::uno::Any& ShowAll, const css::uno::Any& Drawings, const css::uno::Any& Anchors,
+        const css::uno::Any& TextBoundaries, const css::uno::Any& VRuler,
+        const css::uno::Any& Highlight) override;
+    virtual css::uno::Any SAL_CALL WindowName(const css::uno::Any& Number) override;
+    virtual css::uno::Any SAL_CALL ExistingBookmark(const OUString& Name) override;
+    virtual void SAL_CALL MailMergeOpenDataSource(
+        const OUString& Name, const css::uno::Any& Format, const css::uno::Any& ConfirmConversions,
+        const css::uno::Any& ReadOnly, const css::uno::Any& LinkToSource,
+        const css::uno::Any& AddToRecentFiles, const css::uno::Any& PasswordDocument,
+        const css::uno::Any& PasswordTemplate, const css::uno::Any& Revert,
+        const css::uno::Any& WritePasswordDocument, const css::uno::Any& WritePasswordTemplate,
+        const css::uno::Any& Connection, const css::uno::Any& SQLStatement,
+        const css::uno::Any& SQLStatement1, const css::uno::Any& OpenExclusive,
+        const css::uno::Any& SubType) override;
+    virtual css::uno::Any SAL_CALL AppMaximize(const css::uno::Any& WindowName,
+                                               const css::uno::Any& State) override;
+    virtual css::uno::Any SAL_CALL DocMaximize(const css::uno::Any& State) override;
+    virtual void SAL_CALL AppShow(const css::uno::Any& WindowName) override;
+    virtual css::uno::Any SAL_CALL AppCount() override;
+};
+
+#endif // INCLUDED_SW_SOURCE_UI_VBA_VBAWORDBASIC_HXX
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
