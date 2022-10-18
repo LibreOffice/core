683c2e286e2b8f3220481625d085bdde8e1eab29
Hemant Kumar Singh <iammrhks@yahoo.com>
2022-10-18 20:53:24 +0530

    tdf#143148 Use pragma once in vcl/source/edit
    Change-Id: I62be40614baf489433a53f19397737031d1f217b
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/141509
    Tested-by: Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>
    Reviewed-by: Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>

--- vcl/source/edit/textdat2.hxx
+++ vcl/source/edit/textdat2.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_VCL_SOURCE_EDIT_TEXTDAT2_HXX
-#define INCLUDED_VCL_SOURCE_EDIT_TEXTDAT2_HXX
+#pragma once
 
 #include <vcl/seleng.hxx>
 #include <vcl/cursor.hxx>
@@ -278,7 +277,5 @@ struct TextDDInfo
         maCursor.SetStyle( CURSOR_SHADOW );
     }
 };
-
-#endif // INCLUDED_VCL_SOURCE_EDIT_TEXTDAT2_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
