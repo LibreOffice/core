77f9b14071fd5361f3c2d7eba8a9799d41bf3159
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2022-10-23 17:56:50 -0400

    svg: export embedded video
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>
    Change-Id: Ie5dcd1fb4abbaf53f48107e7def0f42daad24596
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/141690
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

--- svx/source/svdraw/svdomedia.cxx
+++ svx/source/svdraw/svdomedia.cxx
@@ -254,6 +254,16 @@ static OUString ret;
 #endif
 }
 
+const OUString& SdrMediaObj::getTempURL() const
+{
+#if HAVE_FEATURE_AVMEDIA
+    return m_xImpl->m_MediaProperties.getTempURL();
+#else
+static OUString ret;
+    return ret;
+#endif
+}
+
 void SdrMediaObj::setMediaProperties( const ::avmedia::MediaItem& rState )
 {
     mediaPropertiesChanged( rState );
