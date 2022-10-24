f9b4ee0859676555bad4f6cc2513baf7135c4bdb
László Németh <nemeth@numbertext.org>
2022-10-24 11:17:50 +0200

    tdf#124603 sw: pressing Up/Down triggers pending spell checking
    Modified text wasn't checked until the next edit operation,
    so spelling mistake of the last edited word could go unnoticed.
    Leaving the edited line by pressing Up/Down triggers
    pending spell checking to fix the problem in most cases.
    
    Regression from commit 4c91e94e892943ef5e031d65f6f42864233cb4cd
    "tdf#92036: sw: fix idle spelling loop",
    
    Note: add unit test to tdf#92036, too.
    
    Change-Id: I865eb7b5d8f872421230d457989e24c3b489690d
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/141728
    Tested-by: Jenkins
    Reviewed-by: László Németh <nemeth@numbertext.org>
    (cherry picked from commit e3ae86a38d6282db0f54d3545015ed22ee868ae5)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/141852
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- sw/source/core/crsr/viscrs.cxx
+++ sw/source/core/crsr/viscrs.cxx
@@ -1027,6 +1027,33 @@ void SwShellCursor::SaveTableBoxContent( const SwPosition* pPos )
 
 bool SwShellCursor::UpDown( bool bUp, sal_uInt16 nCnt )
 {
+    // tdf#124603 trigger pending spell checking of the node
+    if ( nCnt == 1 )
+    {
+        SwTextNode* pNode = GetPoint()->nNode.GetNode().GetTextNode();
+        if( pNode && SwTextNode::WrongState::PENDING == pNode->GetWrongDirty() )
+        {
+            SwWrtShell* pShell = pNode->GetDoc().GetDocShell()->GetWrtShell();
+            if ( pShell && !pShell->IsSelection() && !pShell->IsSelFrameMode() )
+            {
+                const SwViewOption* pVOpt = pShell->GetViewOptions();
+                if ( pVOpt && pVOpt->IsOnlineSpell() )
+                {
+                    const bool bOldViewLock = pShell->IsViewLocked();
+                    pShell->LockView( true );
+
+                    SwTextFrame* pFrame(
+                        static_cast<SwTextFrame*>(pNode->getLayoutFrame(GetShell()->GetLayout())));
+                    SwRect aRepaint(pFrame->AutoSpell_(*pNode, 0));
+                    if (aRepaint.HasArea())
+                        pShell->InvalidateWindows(aRepaint);
+
+                    pShell->LockView( bOldViewLock );
+                }
+            }
+        }
+    }
+
     return SwCursor::UpDown( bUp, nCnt,
                             &GetPtPos(), GetShell()->GetUpDownX(),
                             *GetShell()->GetLayout());
