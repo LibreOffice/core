388af06dcd2d495bbda7f589b616764a5c073bc5
Caolán McNamara <caolanm@redhat.com>
2022-10-24 10:09:52 +0100

    Related: tdf#151641 set windows WorkspaceColor similiar to other platforms
    from the same bg as the dialog/face, so we get a good "application background"
    color in dark mode
    
    Change-Id: Ieeb29ed1df177a91691b7b83d056af4068d0e170
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/141736
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

--- vcl/win/window/salframe.cxx
+++ vcl/win/window/salframe.cxx
@@ -2730,6 +2730,7 @@ void WinSalFrame::UpdateSettings( AllSettings& rSettings )
     aStyleSettings.SetHelpColor( ImplWinColorToSal( GetSysColor( COLOR_INFOBK ) ) );
     aStyleSettings.SetHelpTextColor( ImplWinColorToSal( GetSysColor( COLOR_INFOTEXT ) ) );
 
+    aStyleSettings.SetWorkspaceColor(aStyleSettings.GetFaceColor());
     aStyleSettings.SetDialogColor(aStyleSettings.GetFaceColor());
     aStyleSettings.SetDialogTextColor(aControlTextColor);
 
