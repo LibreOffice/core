LibreOffice/core

commit 2c4aabb220ad1e8f1cbc75f2361fe17893d7cc29

tdf#126788 speed up GetDocFromXMLImport
we can just static_cast to SwXMLImport, no need to use XUnoTunnel

Change-Id: I5ca1a1e2d63ae632578261630f67c9d164727cef
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/142016
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sw/source/filter/xml/xmlimp.cxx
@@ -828,7 +828,7 @@ void SwXMLImport::endDocument()
     // tdf#150753: pDoc may be null e.g. when the package lacks content.xml;
     // we should not forget to tidy up here, including unlocking draw model
     if (!pDoc)
-        pDoc = SwImport::GetDocFromXMLImport(*this);
+        pDoc = getDoc();
     assert(pDoc);
     // SJ: #i49801# -> now permitting repaints
     if (getImportFlags() == SvXMLImportFlags::ALL)
@@ -1630,16 +1630,6 @@ void SwXMLImport::initialize(
             }
         }
     }
-}
-
-SwDoc* SwImport::GetDocFromXMLImport( SvXMLImport const & rImport )
-{
-    auto pTextDoc = comphelper::getFromUnoTunnel<SwXTextDocument>(rImport.GetModel());
-    assert( pTextDoc );
-    assert( pTextDoc->GetDocShell() );
-    SwDoc* pDoc = pTextDoc->GetDocShell()->GetDoc();
-    OSL_ENSURE( pDoc, "Where is my document?" );
-    return pDoc;
 }
 
 void SwXMLImport::initXForms()

sw/source/filter/xml/XMLRedlineImportHelper.cxx
@@ -343,7 +343,7 @@ XMLRedlineImportHelper::~XMLRedlineImportHelper()
             aAny <<= true;
             m_xModelPropertySet->setPropertyValue( g_sShowChanges, aAny );
             // TODO maybe we need some property for the view-setting?
-            SwDoc *const pDoc(SwImport::GetDocFromXMLImport(m_rImport));
+            SwDoc *const pDoc(static_cast<SwXMLImport&>(m_rImport).getDoc());
             assert(pDoc);
             pDoc->GetDocumentRedlineManager().SetHideRedlines(!m_bShowChanges);
         }

sw/source/filter/xml/xmlfmt.cxx
@@ -525,7 +525,7 @@ SvXMLImportContext *SwXMLItemSetStyleContext_Impl::CreateItemSetContext(
 
     SvXMLImportContext *pContext = nullptr;
 
-    SwDoc* pDoc = SwImport::GetDocFromXMLImport( GetSwImport() );
+    SwDoc* pDoc = GetSwImport().getDoc();
 
     SfxItemPool& rItemPool = pDoc->GetAttrPool();
     switch( GetFamily() )
@@ -615,7 +615,7 @@ void SwXMLItemSetStyleContext_Impl::ConnectPageDesc()
         return;
     m_bPageDescConnected = true;
 
-    SwDoc *pDoc = SwImport::GetDocFromXMLImport( GetSwImport() );
+    SwDoc *pDoc = GetSwImport().getDoc();
 
     // #i40788# - first determine the display name of the page style,
     // then map this name to the corresponding user interface name.
@@ -673,7 +673,7 @@ bool SwXMLItemSetStyleContext_Impl::ResolveDataStyleName()
         {
             if( !m_oItemSet )
             {
-                SwDoc *pDoc = SwImport::GetDocFromXMLImport( GetSwImport() );
+                SwDoc *pDoc = GetSwImport().getDoc();
 
                 SfxItemPool& rItemPool = pDoc->GetAttrPool();
                 m_oItemSet.emplace( rItemPool, aTableBoxSetRange );
@@ -1006,7 +1006,7 @@ void SwXMLImport::FinishStyles()
 void SwXMLImport::UpdateTextCollConditions( SwDoc *pDoc )
 {
     if( !pDoc )
-        pDoc = SwImport::GetDocFromXMLImport( *this );
+        pDoc = getDoc();
 
     const SwTextFormatColls& rColls = *pDoc->GetTextFormatColls();
     const size_t nCount = rColls.size();

sw/source/filter/xml/xmlimp.hxx
@@ -48,10 +48,6 @@ enum class SfxStyleFamily;
 // for styles, autostyles and settings + meta
 #define PROGRESS_BAR_STEP 20
 
-namespace SwImport {
-    SwDoc* GetDocFromXMLImport( SvXMLImport const & );
-}
-
 // we only need this scoped enum to be flags here, in sw
 namespace o3tl
 {

sw/source/filter/xml/xmlmeta.cxx
@@ -109,7 +109,7 @@ void SwXMLImport::SetStatistics(
 
     SvXMLImport::SetStatistics(i_rStats);
 
-    SwDoc *pDoc = SwImport::GetDocFromXMLImport( *this );
+    SwDoc *pDoc = getDoc();
     SwDocStat aDocStat( pDoc->getIDocumentStatistics().GetDocStat() );
 
     sal_uInt32 nTokens = 0;

sw/source/filter/xml/xmltbli.cxx
@@ -1166,7 +1166,7 @@ SwXMLTableContext::SwXMLTableContext( SwXMLImport& rImport,
         }
     }
 
-    SwDoc *pDoc = SwImport::GetDocFromXMLImport( GetSwImport() );
+    SwDoc *pDoc = GetSwImport().getDoc();
 
     OUString sTableName;
     if( !aName.isEmpty() )
@@ -2687,7 +2687,7 @@ const SwStartNode *SwXMLTableContext::InsertTableSection(
     }
     else
     {
-        SwDoc* pDoc = SwImport::GetDocFromXMLImport( GetSwImport() );
+        SwDoc* pDoc = GetSwImport().getDoc();
         const SwEndNode *pEndNd = pPrevSttNd ? pPrevSttNd->EndOfSectionNode()
                                              : m_pTableNode->EndOfSectionNode();
         // #i78921# - make code robust

sw/source/filter/xml/xmltexti.cxx
@@ -220,7 +220,7 @@ uno::Reference< XPropertySet > SwXMLTextImportHelper::createAndInsertOLEObject(
     assert(xCursorTunnel.is() && "missing XUnoTunnel for Cursor");
     OTextCursorHelper* pTextCursor = comphelper::getFromUnoTunnel<OTextCursorHelper>(xCursorTunnel);
     SAL_WARN_IF(!pTextCursor, "sw.uno", "SwXTextCursor missing");
-    SwDoc *pDoc = SwImport::GetDocFromXMLImport( rImport );
+    SwDoc *pDoc = static_cast<SwXMLImport&>(rImport).getDoc();
 
     SfxItemSetFixed<RES_FRMATR_BEGIN, RES_FRMATR_END> aItemSet( pDoc->GetAttrPool() );
     Size aTwipSize( 0, 0 );
@@ -522,7 +522,7 @@ uno::Reference< XPropertySet > SwXMLTextImportHelper::createAndInsertOOoLink(
     assert(xCursorTunnel.is() && "missing XUnoTunnel for Cursor");
     OTextCursorHelper* pTextCursor = comphelper::getFromUnoTunnel<OTextCursorHelper>(xCursorTunnel);
     OSL_ENSURE( pTextCursor, "SwXTextCursor missing" );
-    SwDoc *pDoc = SwImport::GetDocFromXMLImport( rImport );
+    SwDoc *pDoc = static_cast<SwXMLImport&>(rImport).getDoc();
 
     SfxItemSetFixed<RES_FRMATR_BEGIN, RES_FRMATR_END> aItemSet( pDoc->GetAttrPool() );
     Size aTwipSize( 0, 0 );

commit 621a53cc24a613e75814a78905df49c12bb32856

ofz#52898 bad cast to SwXMLImport
fallout from
    commit 2c4aabb220ad1e8f1cbc75f2361fe17893d7cc29
    Author: Noel Grandin <noel.grandin@collabora.co.uk>
    Date:   Sat Oct 29 11:11:36 2022 +0200
    tdf#126788 speed up GetDocFromXMLImport
where the prior UNO_QUERY code was hiding this problem

Change-Id: I21efc27f4b04acb38138d2d44e207d5a6b9cb9ea
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/142066
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sw/source/filter/xml/xmlimp.cxx
@@ -337,6 +337,9 @@ SwXMLImport::~SwXMLImport() noexcept
         ClearShapeImport();
     }
     FinitItemImport();
+    // Call cleanup() here because the destruction of some stuff like XMLRedlineImportHelper will cast
+    // to cast their mrImport to SwXMLImport and that is illegal after this destructor is done.
+    cleanup();
 }
 
 void SwXMLImport::setTextInsertMode(