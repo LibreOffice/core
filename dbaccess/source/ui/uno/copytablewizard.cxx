LibreOffice/core

commit 85a5233275ab83fe0d6b943b298695387d97accd

error when copying table and the copy table wizard creates the primary key
then the primary key will not be IDENTITY column, and the
   ALTER TABLE...RESTART WITH
will product an error

Change-Id: If17bd0cbfe3519f9074aebb819b7d85ccea63e1a
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/141970
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

dbaccess/source/ui/uno/copytablewizard.cxx
@@ -1359,7 +1359,9 @@ void CopyTableWizard::impl_doCopy_nothrow()
                 // tdf#119962
                 const Reference< XDatabaseMetaData > xDestMetaData( m_xDestConnection->getMetaData(), UNO_SET_THROW );
                 OUString sDatabaseDest = xDestMetaData->getDatabaseProductName().toAsciiLowerCase();
-                if ( (sDatabaseDest.indexOf("hsql") != -1) || (sDatabaseDest.indexOf("firebird") != -1) )
+                // If we created a new primary key, then it won't necessarily be an IDENTITY column
+                const bool bShouldCreatePrimaryKey = rWizard.shouldCreatePrimaryKey();
+                if ( !bShouldCreatePrimaryKey && ((sDatabaseDest.indexOf("hsql") != -1) || (sDatabaseDest.indexOf("firebird") != -1)) )
                 {
                     const OUString sComposedTableName = ::dbtools::composeTableName( xDestMetaData, xTable, ::dbtools::EComposeRule::InDataManipulation, true );
 

commit 493c88ab4e597c55e920dbec2262058ecc6b6922

calling getCaughtException twice in one catch block is not allowed
It seems to work, but it's not guaranteed to do so, and violates the
documented contract.

Regression from
    commit 85a5233275ab83fe0d6b943b298695387d97accd
    Author: Noel Grandin <noel.grandin@collabora.co.uk>
    Date:   Mon Oct 31 12:08:50 2022 +0200
error when copying table and the copy table wizard creates the
primary key

Change-Id: Ib8422017e627e825865c96dd9fbacbe0e518e96b
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/142264
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

dbaccess/source/ui/uno/copytablewizard.cxx
@@ -1416,8 +1416,8 @@ void CopyTableWizard::impl_doCopy_nothrow()
     }
     catch( const Exception& )
     {
-        TOOLS_WARN_EXCEPTION("dbaccess", "");
         aError = ::cppu::getCaughtException();
+        SAL_WARN("dbaccess", exceptionToString(aError));
 
         // silence the error of the user cancelling the parameter's dialog
         SQLException aSQLError;