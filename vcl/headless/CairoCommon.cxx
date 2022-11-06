26edb7447e9f90d3cace16e31316a71148db80fe
Caolán McNamara <caolanm@redhat.com>
2022-11-06 15:58:01 +0000

    ofz#52723 Out-of-memory
    Change-Id: Id563d2d7629c88d443360ec6a1086b1079f7f48b
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/142356
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>

--- vcl/headless/CairoCommon.cxx
+++ vcl/headless/CairoCommon.cxx
@@ -715,7 +715,7 @@ bool CairoCommon::drawPolyLine(cairo_t* cr, basegfx::B2DRange* pExtents, const C
 
     cairo_set_line_join(cr, eCairoLineJoin);
     cairo_set_line_cap(cr, eCairoLineCap);
-    constexpr int MaxNormalLineWidth = 96;
+    constexpr int MaxNormalLineWidth = 94;
     if (fLineWidth > MaxNormalLineWidth)
     {
         const double fLineWidthPixel
