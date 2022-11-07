LibreOffice/core

commit ea9fff260129ccfa4755e96c08432252efbe5621

tdf#139804 Focus form controls inside document with Alt-<Mnemonic>
Change-Id: I710a23a53ca0eb256a477a78ce1b7ae01129f717
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/111865
Tested-by: Jenkins
Reviewed-by: Samuel Mehrbrodt <samuel.mehrbrodt@allotropia.de>
(cherry picked from commit 67ad205404211a2ae17c430a17ede6e9d04d0b7e)
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/112431
Reviewed-by: Adolfo Jayme Barrientos <fitojb@ubuntu.com>

sd/source/ui/view/drviews4.cxx
@@ -147,9 +147,9 @@ void DrawViewShell::DeleteActualLayer()
 
 bool DrawViewShell::KeyInput (const KeyEvent& rKEvt, ::sd::Window* pWin)
 {
-    bool bRet = false;
+    bool bRet = GetView()->KeyInput(rKEvt, pWin);
 
-    if ( !IsInputLocked() || ( rKEvt.GetKeyCode().GetCode() == KEY_ESCAPE ) )
+    if (!bRet && (!IsInputLocked() || (rKEvt.GetKeyCode().GetCode() == KEY_ESCAPE)))
     {
         if(KEY_RETURN == rKEvt.GetKeyCode().GetCode()
             && rKEvt.GetKeyCode().IsMod1()

sc/source/ui/inc/gridwin.hxx
@@ -263,7 +263,7 @@ class SAL_DLLPUBLIC_RTTI ScGridWindow : public vcl::Window, public DropTargetHel
     bool            DrawMouseButtonDown(const MouseEvent& rMEvt);
     bool            DrawMouseButtonUp(const MouseEvent& rMEvt);
     bool            DrawMouseMove(const MouseEvent& rMEvt);
-    bool            DrawKeyInput(const KeyEvent& rKEvt);
+    bool            DrawKeyInput(const KeyEvent& rKEvt, vcl::Window* pWin);
     bool            DrawCommand(const CommandEvent& rCEvt);
     bool            DrawHasMarkedObj();
     void            DrawEndAction();

sc/source/ui/view/gridwin.cxx
@@ -3289,7 +3289,7 @@ void ScGridWindow::KeyInput(const KeyEvent& rKEvt)
         if (mrViewData.GetDocShell()->GetProgress())
             return;
 
-        if (DrawKeyInput(rKEvt))
+        if (DrawKeyInput(rKEvt, this))
         {
             const vcl::KeyCode& rLclKeyCode = rKEvt.GetKeyCode();
             if (rLclKeyCode.GetCode() == KEY_DOWN

sc/source/ui/view/gridwin3.cxx
@@ -167,10 +167,15 @@ bool ScGridWindow::DrawCommand(const CommandEvent& rCEvt)
     return false;
 }
 
-bool ScGridWindow::DrawKeyInput(const KeyEvent& rKEvt)
+bool ScGridWindow::DrawKeyInput(const KeyEvent& rKEvt, vcl::Window* pWin)
 {
     ScDrawView* pDrView = mrViewData.GetScDrawView();
     FuPoor* pDraw = mrViewData.GetView()->GetDrawFuncPtr();
+
+
+    if (pDrView && pDrView->KeyInput(rKEvt, pWin))
+        return true;
+
     if (pDrView && pDraw && !mrViewData.IsRefMode())
     {
         pDraw->SetWindow( this );

svx/source/form/fmview.cxx
@@ -41,11 +41,14 @@
 #include <fmundo.hxx>
 #include <svx/dataaccessdescriptor.hxx>
 #include <comphelper/namedvaluecollection.hxx>
+#include <toolkit/helper/vclunohelper.hxx>
 #include <com/sun/star/lang/XServiceInfo.hpp>
+#include <com/sun/star/awt/XControl.hpp>
 #include <tools/debug.hxx>
 #include <svx/sdrpagewindow.hxx>
 #include <svx/sdrpaintwindow.hxx>
 #include <svx/svxids.hrc>
+#include <vcl/i18nhelp.hxx>
 
 using namespace ::com::sun::star;
 using namespace ::com::sun::star::uno;
@@ -491,6 +494,31 @@ bool FmFormView::KeyInput(const KeyEvent& rKEvt, vcl::Window* pWin)
             pFormShell->GetImpl()->handleShowPropertiesRequest_Lock();
         }
 
+    }
+
+    // tdf#139804 Allow selecting form controls with Alt-<Mnemonic>
+    if (rKeyCode.IsMod2() && rKeyCode.GetCode())
+    {
+        FmFormPage* pCurPage = GetCurPage();
+        for (size_t a = 0; a < pCurPage->GetObjCount(); ++a)
+        {
+            SdrObject* pObj = pCurPage->GetObj(a);
+            FmFormObj* pFormObject = FmFormObj::GetFormObject(pObj);
+            if (!pFormObject)
+                continue;
+
+            Reference<awt::XControl> xControl = pFormObject->GetUnoControl(*this, *pWin);
+            if (!xControl.is())
+                continue;
+            const vcl::I18nHelper& rI18nHelper = Application::GetSettings().GetUILocaleI18nHelper();
+            VclPtr<vcl::Window> pWindow = VCLUnoHelper::GetWindow(xControl->getPeer());
+            if (rI18nHelper.MatchMnemonic(pWindow->GetText(), rKEvt.GetCharCode()))
+            {
+                pWindow->GrabFocus();
+                bDone = true;
+                break;
+            }
+        }
     }
 
     if ( !bDone )

commit 429f3d8e3eddedb7ab183b6530220bc7491302e7

tdf#144399 Don't allow editing in r/o mode
Fallout from ea9fff260129ccfa4755e96c08432252efbe5621

Change-Id: I0c73d13b79389db13bb04046731bdf18b4cf6005
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/142382
Tested-by: Samuel Mehrbrodt <samuel.mehrbrodt@allotropia.de>
Reviewed-by: Samuel Mehrbrodt <samuel.mehrbrodt@allotropia.de>

sd/source/ui/view/drviews4.cxx
@@ -256,10 +256,11 @@ bool DrawViewShell::KeyInput (const KeyEvent& rKEvt, ::sd::Window* pWin)
                FreshNavigatrTree();
             }
         }
+        if (!bRet && !mbReadOnly) // tdf#139804
+        {
+            bRet = GetView()->KeyInput(rKEvt, pWin);
+        }
     }
-
-    if (!bRet)
-        bRet = GetView()->KeyInput(rKEvt, pWin);
 
     return bRet;
 }