02d1163dc8d769ae65baadaedc2a254a8e909d66
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2022-11-13 22:27:54 +0900

    sw: test online a11y check - splitting and joining nodes
    Change-Id: I9be3df3a4c00e7f1bbfc4f2462c8cf9a0f813f9d
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/142656
    Tested-by: Jenkins
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>
    (cherry picked from commit e7f78c3123c7bb7b8c830fa11241c357d89bad6a)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/144498
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

--- sw/CppunitTest_sw_core_accessibilitycheck.mk
+++ sw/CppunitTest_sw_core_accessibilitycheck.mk
@@ -25,11 +25,12 @@ $(eval $(call gb_CppunitTest_use_libraries,sw_core_accessibilitycheck, \
     svx \
     sfx \
     sw \
-	swqahelper \
+    swqahelper \
     test \
     unotest \
     utl \
     tl \
+    vcl \
 ))
 
 $(eval $(call gb_CppunitTest_use_externals,sw_core_accessibilitycheck,\
