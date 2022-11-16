LibreOffice/core

commit c2c37eadf32c80bcd8f168b9fc67f32002b3cb07

Related: tdf#151898 consider surface scaling if prescale with Bitmap::Scale
whose introduction dates back to:

commit c0ce7ca4884f7f6d1016bd1dbcc22066cb4a7797
Date:   Sat Jul 7 13:07:03 2012 +0200

    Prescale image with Bitmap::Scale to improve quality.

don't prescale past the level of detail that the surface could retain

Change-Id: I1022688d45d2bb7b692f4ba619198fccea8eab36
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/142591
Tested-by: Jenkins
Reviewed-by: Caolán McNamara <caolanm@redhat.com>

vcl/source/outdev/bitmap.cxx
@@ -154,10 +154,20 @@ void OutputDevice::DrawBitmap( const Point& rDestPt, const Size& rDestSize,
             {
                 if (nAction == MetaActionType::BMPSCALE && CanSubsampleBitmap())
                 {
-                    const double nScaleX = aPosAry.mnDestWidth  / static_cast<double>(aPosAry.mnSrcWidth);
-                    const double nScaleY = aPosAry.mnDestHeight / static_cast<double>(aPosAry.mnSrcHeight);
+                    double nScaleX = aPosAry.mnDestWidth  / static_cast<double>(aPosAry.mnSrcWidth);
+                    double nScaleY = aPosAry.mnDestHeight / static_cast<double>(aPosAry.mnSrcHeight);
 
                     // If subsampling, use Bitmap::Scale() for subsampling of better quality.
+
+                    // but hidpi surfaces like the cairo one have their own scale, so don't downscale
+                    // past the surface scaling which can retain the extra detail
+                    double fScale(1.0);
+                    if (mpGraphics->ShouldDownscaleIconsAtSurface(&fScale))
+                    {
+                        nScaleX *= fScale;
+                        nScaleY *= fScale;
+                    }
+
                     if ( nScaleX < 1.0 || nScaleY < 1.0 )
                     {
                         aBmp.Scale(nScaleX, nScaleY);

commit 31d510389928c8f8a495a85cdab4c12d1bbbd3ce

tdf#152061 Fix bitmap image scaling
In the regression introduced by c2c37eadf32c80bcd8f168b9fc67f32002b3cb07
the bitmap image scale was calculated incorrectly. Upon resizing the
image a bit, this problem went away, and the image was shown with much
better quality compared to what it was before the above commit. It
turned out that the problem happened when the scale was less than 1, and
it was happening inside "if ( nScaleX < 1.0 || nScaleY < 1.0 ) {...}".

The part that was calculating aPosAry.mnSrcWidth and aPosAry.mnSrcHeight
was wrong, because it was setting the same height and width without
considering the fScale. The bitmap was scaled using Bitmap::Scale()
parameter which was newly multipled by fScale in the above commit. To
fix the problem, these two values are now multipled by fScale.

Change-Id: I8c77d2db3b05da54f1999d3915a70e3b7cc8106f
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/142754
Tested-by: Jenkins
Tested-by: Caolán McNamara <caolanm@redhat.com>
Reviewed-by: Caolán McNamara <caolanm@redhat.com>

vcl/source/outdev/bitmap.cxx
@@ -171,8 +171,8 @@ void OutputDevice::DrawBitmap( const Point& rDestPt, const Size& rDestSize,
                     if ( nScaleX < 1.0 || nScaleY < 1.0 )
                     {
                         aBmp.Scale(nScaleX, nScaleY);
-                        aPosAry.mnSrcWidth = aPosAry.mnDestWidth;
-                        aPosAry.mnSrcHeight = aPosAry.mnDestHeight;
+                        aPosAry.mnSrcWidth = aPosAry.mnDestWidth * fScale;
+                        aPosAry.mnSrcHeight = aPosAry.mnDestHeight * fScale;
                     }
                 }
 