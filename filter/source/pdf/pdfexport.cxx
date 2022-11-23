802c73416f7f3d4fb389f2c00e4e389a2dbd9362
Miklos Vajna <vmiklos@collabora.com>
2022-11-23 13:39:40 +0100

    Related: tdf#54053 PDF export: add UNO API to customize the watermark font name
    The font name of the watermark text is currently hardwired to Helvetica,
    a sans-serif font.
    
    The problem is that this looks bad in case your document uses serif or
    monospace fonts. Or perhaps your document uses a single font and you
    want to make sure that the watermark uses exactly the same font.
    
    Fix the problem by adding a new "WatermarkFontName" PDF export filter
    option to specify the font name explicitly.
    
    Example cmdline usage:
    
    soffice --convert-to pdf:writer_pdf_Export:'{"Watermark":{"type":"string","value":"draft"}, "WatermarkFontName":{"type":"string","value":"Times"}}' test.odt
    
    (cherry picked from commit d1dd9b9733511ff451e264169537c08fa14c574f)
    
    Change-Id: I2f065e1517236695b0a95369bdcd803125018587
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/143691
    Tested-by: Jenkins
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

--- filter/source/pdf/pdfexport.cxx
+++ filter/source/pdf/pdfexport.cxx
@@ -110,6 +110,7 @@ PDFExport::PDFExport( const Reference< XComponent >& rxSrcDoc,
 
     mbIsRedactMode              ( false ),
     maWatermarkColor            ( COL_LIGHTGREEN ),
+    maWatermarkFontName         ( "Helvetica" ),
 
     mbHideViewerToolbar         ( false ),
     mbHideViewerMenubar         ( false ),
@@ -575,6 +576,14 @@ bool PDFExport::Export( const OUString& rFile, const Sequence< PropertyValue >& 
                     if (rProp.Value >>= nFontHeight)
                     {
                         moWatermarkFontHeight = nFontHeight;
+                    }
+                }
+                else if (rProp.Name == "WatermarkFontName")
+                {
+                    OUString aFontName{};
+                    if (rProp.Value >>= aFontName)
+                    {
+                        maWatermarkFontName = aFontName;
                     }
                 }
                 else if ( rProp.Name == "TiledWatermark" )
@@ -1170,7 +1179,7 @@ void PDFExport::ImplExportPage( vcl::PDFWriter& rWriter, vcl::PDFExtOutDevData& 
 
 void PDFExport::ImplWriteWatermark( vcl::PDFWriter& rWriter, const Size& rPageSize )
 {
-    vcl::Font aFont( "Helvetica", Size( 0, moWatermarkFontHeight ? *moWatermarkFontHeight : 3*rPageSize.Height()/4 ) );
+    vcl::Font aFont( maWatermarkFontName, Size( 0, moWatermarkFontHeight ? *moWatermarkFontHeight : 3*rPageSize.Height()/4 ) );
     aFont.SetItalic( ITALIC_NONE );
     aFont.SetWidthType( WIDTH_NORMAL );
     aFont.SetWeight( WEIGHT_NORMAL );
