LibreOffice/core

commit dfffe710d07f84f4152cf61ccd4a69279a26ff7c

tdf#149140 vcl,sw: PDF/UA export: add Scope attribute to table headers
Change-Id: If2402c824e637016e3c10860810501ec60732bc5
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/142000
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

vcl/source/gdi/pdfwriter_impl.cxx
@@ -1821,6 +1821,7 @@ const char* PDFWriterImpl::getAttributeTag( PDFWriter::StructAttribute eAttr )
         aAttributeStrings[ PDFWriter::ListNumbering ]       = "ListNumbering";
         aAttributeStrings[ PDFWriter::RowSpan ]             = "RowSpan";
         aAttributeStrings[ PDFWriter::ColSpan ]             = "ColSpan";
+        aAttributeStrings[ PDFWriter::Scope ]               = "Scope";
         aAttributeStrings[ PDFWriter::LinkAnnotation ]      = "LinkAnnotation";
     }
 
@@ -1857,6 +1858,9 @@ const char* PDFWriterImpl::getAttributeValueTag( PDFWriter::StructAttributeValue
         aValueStrings[ PDFWriter::Underline ]               = "Underline";
         aValueStrings[ PDFWriter::Overline ]                = "Overline";
         aValueStrings[ PDFWriter::LineThrough ]             = "LineThrough";
+        aValueStrings[ PDFWriter::Row ]                     = "Row";
+        aValueStrings[ PDFWriter::Column ]                  = "Column";
+        aValueStrings[ PDFWriter::Both ]                    = "Both";
         aValueStrings[ PDFWriter::Disc ]                    = "Disc";
         aValueStrings[ PDFWriter::Circle ]                  = "Circle";
         aValueStrings[ PDFWriter::Square ]                  = "Square";
@@ -1907,8 +1911,11 @@ OString PDFWriterImpl::emitStructureAttributes( PDFStructureElement& i_rEle )
         if( attribute.first == PDFWriter::ListNumbering )
             appendStructureAttributeLine( attribute.first, attribute.second, aList, true );
         else if( attribute.first == PDFWriter::RowSpan ||
-                 attribute.first == PDFWriter::ColSpan )
+                 attribute.first == PDFWriter::ColSpan ||
+                 attribute.first == PDFWriter::Scope)
+        {
             appendStructureAttributeLine( attribute.first, attribute.second, aTable, false );
+        }
         else if( attribute.first == PDFWriter::LinkAnnotation )
         {
             sal_Int32 nLink = attribute.second.nValue;
@@ -10905,6 +10912,15 @@ bool PDFWriterImpl::setStructureAttribute( enum PDFWriter::StructAttribute eAttr
                         eType == PDFWriter::BibEntry    ||
                         eType == PDFWriter::Code        ||
                         eType == PDFWriter::Link )
+                    {
+                        bInsert = true;
+                    }
+                }
+                break;
+            case PDFWriter::Scope:
+                if (eVal == PDFWriter::Row || eVal == PDFWriter::Column || eVal == PDFWriter::Both)
+                {
+                    if (eType == PDFWriter::TableHeader)
                     {
                         bInsert = true;
                     }

include/vcl/pdfwriter.hxx
@@ -138,7 +138,7 @@ public:
         Placement, WritingMode, SpaceBefore, SpaceAfter, StartIndent, EndIndent,
         TextIndent, TextAlign, Width, Height, BlockAlign, InlineAlign,
         LineHeight, BaselineShift, TextDecorationType, ListNumbering,
-        RowSpan, ColSpan,
+        RowSpan, ColSpan, Scope,
 
         // link destination is an artificial attribute that sets
         // the link annotation ID of a Link element
@@ -172,6 +172,8 @@ public:
         Normal,
         // TextDecorationType
         Underline, Overline, LineThrough,
+        // Scope
+        Row, Column, Both,
         // ListNumbering
         Disc, Circle, Square, Decimal, UpperRoman, LowerRoman, UpperAlpha, LowerAlpha
     };

sw/source/core/text/EnhancedPDFExportHelper.cxx
@@ -538,6 +538,8 @@ void SwTaggedPDFHelper::SetAttributes( vcl::PDFWriter::StructElement eType )
                 break;
 
             case vcl::PDFWriter::TableHeader :
+                mpPDFExtOutDevData->SetStructureAttribute(vcl::PDFWriter::Scope, vcl::PDFWriter::Column);
+                [[fallthrough]];
             case vcl::PDFWriter::TableData :
                 bPlacement =
                 bWritingMode =

vcl/qa/cppunit/pdfexport/pdfexport.cxx
@@ -3128,6 +3128,67 @@ CPPUNIT_TEST_FIXTURE(PdfExportTest, testTdf139736)
     CPPUNIT_ASSERT(nArtifacts >= 3);
 }
 
+CPPUNIT_TEST_FIXTURE(PdfExportTest, testTdf149140)
+{
+    aMediaDescriptor["FilterName"] <<= OUString("writer_pdf_Export");
+
+    // Enable PDF/UA
+    uno::Sequence<beans::PropertyValue> aFilterData(
+        comphelper::InitPropertySequence({ { "PDFUACompliance", uno::Any(true) } }));
+    aMediaDescriptor["FilterData"] <<= aFilterData;
+    saveAsPDF(u"TableTH_test_LibreOfficeWriter7.3.3_HeaderRow-HeadersInTopRow.fodt");
+
+    vcl::filter::PDFDocument aDocument;
+    SvFileStream aStream(maTempFile.GetURL(), StreamMode::READ);
+    CPPUNIT_ASSERT(aDocument.Read(aStream));
+
+    // The document has one page.
+    std::vector<vcl::filter::PDFObjectElement*> aPages = aDocument.GetPages();
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(1), aPages.size());
+
+    int nTH(0);
+    for (const auto& rDocElement : aDocument.GetElements())
+    {
+        auto pObject = dynamic_cast<vcl::filter::PDFObjectElement*>(rDocElement.get());
+        if (!pObject)
+            continue;
+        auto pType = dynamic_cast<vcl::filter::PDFNameElement*>(pObject->Lookup("Type"));
+        if (pType && pType->GetValue() == "StructElem")
+        {
+            auto pS = dynamic_cast<vcl::filter::PDFNameElement*>(pObject->Lookup("S"));
+            if (pS && pS->GetValue() == "TH")
+            {
+                int nTable(0);
+                auto pAttrs = dynamic_cast<vcl::filter::PDFArrayElement*>(pObject->Lookup("A"));
+                CPPUNIT_ASSERT(pAttrs != nullptr);
+                for (const auto& rAttrRef : pAttrs->GetElements())
+                {
+                    auto pARef = dynamic_cast<vcl::filter::PDFReferenceElement*>(rAttrRef);
+                    CPPUNIT_ASSERT(pARef != nullptr);
+                    auto pAttr = pARef->LookupObject();
+                    CPPUNIT_ASSERT(pAttr != nullptr);
+                    auto pAttrDict = pAttr->GetDictionary();
+                    CPPUNIT_ASSERT(pAttrDict != nullptr);
+                    auto pOwner
+                        = dynamic_cast<vcl::filter::PDFNameElement*>(pAttrDict->LookupElement("O"));
+                    CPPUNIT_ASSERT(pOwner != nullptr);
+                    if (pOwner->GetValue() == "Table")
+                    {
+                        auto pScope = dynamic_cast<vcl::filter::PDFNameElement*>(
+                            pAttrDict->LookupElement("Scope"));
+                        CPPUNIT_ASSERT(pScope != nullptr);
+                        CPPUNIT_ASSERT_EQUAL(OString("Column"), pScope->GetValue());
+                        ++nTable;
+                    }
+                }
+                CPPUNIT_ASSERT_EQUAL(int(1), nTable);
+                ++nTH;
+            }
+        }
+    }
+    CPPUNIT_ASSERT_EQUAL(int(6), nTH);
+}
+
 CPPUNIT_TEST_FIXTURE(PdfExportTest, testTdf142129)
 {
     loadFromURL(u"master.odm");

commit 28b06bb7236f8c6e17423dc3df446306900355f1

tdf#149140 vcl: PDF/UA export: Scope attribute exists since PDF 1.5
(regression from commit dfffe710d07f84f4152cf61ccd4a69279a26ff7c)

Change-Id: I12e077d857d25db9636f2a06f953616ae2d907bc
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/143209
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

vcl/source/gdi/pdfwriter_impl.cxx
@@ -10934,7 +10934,9 @@ bool PDFWriterImpl::setStructureAttribute( enum PDFWriter::StructAttribute eAttr
             case PDFWriter::Scope:
                 if (eVal == PDFWriter::Row || eVal == PDFWriter::Column || eVal == PDFWriter::Both)
                 {
-                    if (eType == PDFWriter::TableHeader)
+                    if (eType == PDFWriter::TableHeader
+                        && m_aContext.Version != PDFWriter::PDFVersion::PDF_A_1
+                        && PDFWriter::PDFVersion::PDF_1_5 <= m_aContext.Version)
                     {
                         bInsert = true;
                     }