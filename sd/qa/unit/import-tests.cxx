df6ff2392701b04bbafddb1119e1ac43cac71fae
Tibor Nagy <nagy.tibor2@nisz.hu>
2022-11-29 15:43:58 +0100

    tdf#152211 PPTX import: fix connector position based on line width
    Thick shape line changed the connector, e.g. direction of
    its arrow head.
    
    Follow-up to commit d8c89fb920af747ec51ce966b5d7b65e9340afbd
    "tdf#151891 PPTX import: fix regression of connector position"
    
    Regression from commit cbf66ec3e60d07efb7c3cceed9b4f0fb4f0510c8
    "tdf#89449 PPTX import: fix line connectors".
    
    Change-Id: I99d22409ec65e10609b1e371cc4d301fd73c7c82
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/143467
    Tested-by: László Németh <nemeth@numbertext.org>
    Reviewed-by: László Németh <nemeth@numbertext.org>
    (cherry picked from commit 400e44cebd993f4b9b3d878fb9264f99e005c9fb)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/143589
    Tested-by: Jenkins
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- sd/qa/unit/import-tests.cxx
+++ sd/qa/unit/import-tests.cxx
@@ -311,8 +311,8 @@ void SdImportTest::testConnectors()
     ::sd::DrawDocShellRef xDocShRef
         = loadURL(m_directories.getURLFromSrc(u"/sd/qa/unit/data/pptx/connectors.pptx"), PPTX);
 
-    sal_Int32 aEdgeValue[] = { -1167, -1167, -1591, 1476,  1356, -1357, 1604,  -1540,
-                               607,   1296,  -1638, -1060, -522, 1578,  -1291, 333 };
+    sal_Int32 aEdgeValue[] = { -1123, -1123, -1547, 1432,  1356, -1357, 1604,  -1540,
+                               599,   1288,  -1629, -1052, -513, 1569,  -1283, 333 };
 
     sal_Int32 nCount = 0;
     for (size_t i = 0; i < 18; i++)
