LibreOffice/core

commit adc042f95d3dbd65b778260025d59283146916e5

tdf#124333 PPTX import: fix Z-order of embedded OLE objects
Choose mc:Choice in a:graphicData element
if the selected p:oleObj element has a shape id
to avoid of shape duplication which created also
bad layout/overlapping because of the different Z-order
of the duplicated shape.

Change-Id: Idecff4903c2d637bc82353f13352cac72413cec1
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/140041
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

sd/qa/unit/export-tests-ooxml3.cxx
@@ -122,6 +122,7 @@ public:
     void testTdf149551_tbrl90();
     void testTdf149551_btlr();
     void testTdf94122_autoColor();
+    void testTdf124333();
 
     CPPUNIT_TEST_SUITE(SdOOXMLExportTest3);
 
@@ -209,6 +210,7 @@ public:
     CPPUNIT_TEST(testTdf149551_tbrl90);
     CPPUNIT_TEST(testTdf149551_btlr);
     CPPUNIT_TEST(testTdf94122_autoColor);
+    CPPUNIT_TEST(testTdf124333);
     CPPUNIT_TEST_SUITE_END();
 
     virtual void registerNamespaces(xmlXPathContextPtr& pXmlXPathCtx) override
@@ -2258,6 +2260,25 @@ void SdOOXMLExportTest3::testTdf94122_autoColor()
     assertXPath(pXmlDocContent3,
                 "/p:sld/p:cSld/p:spTree/p:sp[2]/p:txBody/a:p/a:r/a:rPr/a:solidFill/a:srgbClr",
                 "val", "000000");
+}
+
+void SdOOXMLExportTest3::testTdf124333()
+{
+    // Document contains one rectangle and one embedded OLE object.
+    ::sd::DrawDocShellRef xDocShRef
+        = loadURL(m_directories.getURLFromSrc(u"/sd/qa/unit/data/pptx/ole.pptx"), PPTX);
+
+    // Without the fix in place, the number of shapes was 3.
+    CPPUNIT_ASSERT_EQUAL_MESSAGE("number of shapes is incorrect", sal_Int32(2),
+                                 getPage(0, xDocShRef)->getCount());
+
+    xDocShRef = saveAndReload(xDocShRef.get(), PPTX);
+
+    // Check number of shapes after export.
+    CPPUNIT_ASSERT_EQUAL_MESSAGE("number of shapes is incorrect after export", sal_Int32(2),
+                                 getPage(0, xDocShRef)->getCount());
+
+    xDocShRef->DoClose();
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(SdOOXMLExportTest3);

oox/qa/unit/vml.cxx
@@ -192,18 +192,13 @@ CPPUNIT_TEST_FIXTURE(OoxVmlTest, testGraphicStroke)
     uno::Reference<drawing::XDrawPage> xDrawPage(xDrawPagesSupplier->getDrawPages()->getByIndex(0),
                                                  uno::UNO_QUERY);
 
-    uno::Reference<beans::XPropertySet> xShape;
-    for (sal_Int32 i = 0; i < xDrawPage->getCount(); ++i)
-    {
-        uno::Reference<lang::XServiceInfo> xInfo(xDrawPage->getByIndex(i), uno::UNO_QUERY);
-        if (!xInfo->supportsService("com.sun.star.drawing.GraphicObjectShape"))
-        {
-            continue;
-        }
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(1), xDrawPage->getCount());
 
+    uno::Reference<beans::XPropertySet> xShape;
+    uno::Reference<lang::XServiceInfo> xInfo(xDrawPage->getByIndex(0), uno::UNO_QUERY);
+    if (xInfo->supportsService("com.sun.star.drawing.OLE2Shape"))
         xShape.set(xInfo, uno::UNO_QUERY);
-        break;
-    }
+
     CPPUNIT_ASSERT(xShape.is());
 
     drawing::LineStyle eLineStyle{};

oox/source/drawingml/graphicshapecontext.cxx
@@ -256,7 +256,8 @@ void OleObjectGraphicDataContext::onEndElement()
 {
     if( getCurrentElement() == PPT_TOKEN( oleObj ) && !isMCEStateEmpty() )
     {
-        if( getMCEState() == MCE_STATE::FoundChoice && !mrOleObjectInfo.mbHasPicture )
+        if (getMCEState() == MCE_STATE::FoundChoice && !mrOleObjectInfo.mbHasPicture
+            && mrOleObjectInfo.maShapeId.isEmpty())
             setMCEState( MCE_STATE::Started );
     }
 }

oox/source/drawingml/shape.cxx
@@ -1868,6 +1868,23 @@ Reference< XShape > const & Shape::createAndInsert(
             propertySet->setPropertyValue(
                 "SoftEdgeRadius", Any(convertEmuToHmm(aEffectProperties.maSoftEdge.moRad.value())));
         }
+
+        // Set the stroke and fill-color properties of the OLE shape
+        if (aServiceName == "com.sun.star.drawing.OLE2Shape" && mxOleObjectInfo
+            && !mxOleObjectInfo->maShapeId.isEmpty())
+            if (::oox::vml::Drawing* pVmlDrawing = rFilterBase.getVmlDrawing())
+                if (const ::oox::vml::ShapeBase* pVmlShape
+                    = pVmlDrawing->getShapes().getShapeById(mxOleObjectInfo->maShapeId))
+                {
+                    // Apply stroke props from the type model of the related VML shape.
+                    ShapePropertyMap aPropMap(rFilterBase.getModelObjectHelper());
+                    pVmlShape->getTypeModel().maStrokeModel.pushToPropMap(
+                        aPropMap, rFilterBase.getGraphicHelper());
+                    // And, fill-color properties as well...
+                    pVmlShape->getTypeModel().maFillModel.pushToPropMap(
+                        aPropMap, rFilterBase.getGraphicHelper());
+                    PropertySet(xSet).setProperties(aPropMap);
+                }
     }
 
     if (mxShape.is())

commit 907da02bf8b33c080538731864225b3c44251328

tdf#152436 PPTX export regression: fix lost shape at missing object
If the object is missing, it's still possible to keep its shape
by exporting its preview graphic, as before the regression.

Regression from commit adc042f95d3dbd65b778260025d59283146916e5
"tdf#124333 PPTX import: fix Z-order of embedded OLE objects".

Change-Id: Ib2fd00f53a80572cfc9acaefea55015780c57da8
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/144040
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

oox/source/export/shapes.cxx
@@ -2538,6 +2538,16 @@ ShapeExport& ShapeExport::WriteOLE2Shape( const Reference< XShape >& xShape )
     if (!xObj.is())
     {
         SAL_WARN("oox.shape", "ShapeExport::WriteOLE2Shape: no object");
+
+        // tdf#152436 Export the preview graphic of the object if the object is missing.
+        SdrObject* pSdrOLE2(SdrObject::getSdrObjectFromXShape(xShape));
+        if (auto pOle2Obj = dynamic_cast<SdrOle2Obj*>(pSdrOLE2))
+        {
+            const Graphic* pGraphic = pOle2Obj->GetGraphic();
+            if (pGraphic)
+                WriteGraphicObjectShapePart(xShape, pGraphic);
+        }
+
         return *this;
     }
 

sd/qa/unit/export-tests-ooxml3.cxx
@@ -130,6 +130,7 @@ public:
     void testTdf124333();
     void testAutofittedTextboxIndent();
     void testTdf151622_oleIcon();
+    void testTdf152436();
 
     CPPUNIT_TEST_SUITE(SdOOXMLExportTest3);
 
@@ -221,6 +222,7 @@ public:
     CPPUNIT_TEST(testTdf124333);
     CPPUNIT_TEST(testAutofittedTextboxIndent);
     CPPUNIT_TEST(testTdf151622_oleIcon);
+    CPPUNIT_TEST(testTdf152436);
     CPPUNIT_TEST_SUITE_END();
 
     virtual void registerNamespaces(xmlXPathContextPtr& pXmlXPathCtx) override
@@ -2103,6 +2105,15 @@ void SdOOXMLExportTest3::testTdf151622_oleIcon()
     // - In ..., XPath '//p:oleObj' no attribute 'showAsIcon' exist
     // i.e. show as icon option wasn't exported.
     assertXPath(pXml, "//p:oleObj", "showAsIcon", "1");
+}
+
+void SdOOXMLExportTest3::testTdf152436()
+{
+    createSdImpressDoc("pptx/ole-emf_min.pptx");
+    saveAndReload("Impress Office Open XML");
+
+    // Check number of shapes after export.
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(1), getPage(0)->getCount());
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(SdOOXMLExportTest3);