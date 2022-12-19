ea7db3b0828f16d424910892f10a3b979dbf2ed3
Caolán McNamara <caolanm@redhat.com>
2022-12-19 10:33:18 +0000

    ofz#54240 Integer-overflow
    Change-Id: I50c8f9579a9fb07431ad0d6eebc5f0df8e40ebd4
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/144463
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>

--- vcl/source/gdi/metaact.cxx
+++ vcl/source/gdi/metaact.cxx
@@ -591,7 +591,7 @@ static bool AllowRect(const tools::Rectangle& rRect)
 
 void MetaTextAction::Execute( OutputDevice* pOut )
 {
-    if (!AllowY(maPt.Y()))
+    if (!AllowY(pOut->LogicToPixel(maPt).Y()))
         return;
 
     pOut->DrawText( maPt, maStr, mnIndex, mnLen );
@@ -724,6 +724,9 @@ MetaStretchTextAction::MetaStretchTextAction( const Point& rPt, sal_uInt32 nWidt
 
 void MetaStretchTextAction::Execute( OutputDevice* pOut )
 {
+    if (!AllowY(pOut->LogicToPixel(maPt).Y()))
+        return;
+
     pOut->DrawStretchText( maPt, mnWidth, maStr, mnIndex, mnLen );
 }
 
@@ -760,7 +763,7 @@ MetaTextRectAction::MetaTextRectAction( const tools::Rectangle& rRect,
 
 void MetaTextRectAction::Execute( OutputDevice* pOut )
 {
-    if (!AllowRect(maRect))
+    if (!AllowRect(pOut->LogicToPixel(maRect)))
         return;
 
     pOut->DrawText( maRect, maStr, mnStyle );
