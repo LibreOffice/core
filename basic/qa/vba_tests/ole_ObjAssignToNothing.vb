a179da299a7daaa13f7d6fa3612f82eeb186f0d1
Xisco Fauli <xiscofauli@libreoffice.org>
2022-12-20 10:49:04 +0100

    basic: add missing license statement in .vb test files
    Change-Id: I304615540edad3d3486bcdb17d7d6ae12fc084b4
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/144584
    Tested-by: Jenkins
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- basic/qa/vba_tests/ole_ObjAssignToNothing.vb
+++ basic/qa/vba_tests/ole_ObjAssignToNothing.vb
@@ -1,3 +1,11 @@
+'
+' This file is part of the LibreOffice project.
+'
+' This Source Code Form is subject to the terms of the Mozilla Public
+' License, v. 2.0. If a copy of the MPL was not distributed with this
+' file, You can obtain one at http://mozilla.org/MPL/2.0/.
+'
+
 Option VBASupport 1
 Function doUnitTest(TestData as String, Driver as String) as String
 Rem Ensure object assignment is by reference
