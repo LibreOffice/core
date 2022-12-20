cd274347896dc88bb137ef8cae87a86d9b977aef
Jim Raykowski <raykowj@gmail.com>
2022-12-20 00:33:05 -0900

    tdf#152029 follow up: Handle content that extends to following pages
    Brings section and table content to attention in the document view that
    extend to following pages. Previously only the frame area on the page
    these content types begin is brought to attention.
    
    Change-Id: Ia023e85cc7c14e0c50a0a0f9765a17fb207a95da
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/144583
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
    (cherry picked from commit 5930f928e777ab95ed26619c43fa5ae334685deb)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/144663
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- sw/source/uibase/inc/conttree.hxx
+++ sw/source/uibase/inc/conttree.hxx
@@ -147,6 +147,7 @@ class SwContentTree final : public SfxListener
     void BringDrawingObjectsToAttention(std::vector<const SdrObject*>& rDrawingObjectsArr);
     void BringTextFieldsToAttention(std::vector<const SwTextAttr*>& rTextAttrsArr);
     void BringFootnotesToAttention(std::vector<const SwTextAttr*>& rTextAttrsArr);
+    void BringTypesWithFlowFramesToAttention(const std::vector<const SwNode*>& rNodes);
 
     /**
      * Before any data will be deleted, the last active entry has to be found.
