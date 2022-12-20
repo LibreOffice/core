LibreOffice/core

commit 4757dfc2a520f63fba0b27cc161fe732231dbd0e

tdf#145871 sw: ODF import: don't convert subtables if outer row ...
... has fixed or min height.

The code had 2 obvious problems: the fixed height on the outer row
wasn't cleared if the inner row didn't have a fixed height, and the code
to set lastSize on the last row erroneously set the first row's height
as well due to sharing the row format.

But it turns out that this doesn't work anyway in case any of the inner
rows are variable sized, because without layout it's not possible to
determine the height of these rows, and so the lastSize is going to be
too large in many cases.

(regression from commit e366c928819c44b5c253c45dca6dae40b71c9808)

Change-Id: I42ac7c14236562f9cae228efc0e98dc2fa8c2a23
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/141079
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/core/table/swnewtable.cxx
@@ -2133,6 +2133,11 @@ void SwTable::ConvertSubtableBox(sal_uInt16 const nRow, sal_uInt16 const nBox)
     assert(!pSubTableBox->GetTabLines().empty());
     // are relative (%) heights possible? apparently not
     SwFormatFrameSize const outerSize(pSourceLine->GetFrameFormat()->GetFrameSize());
+    if (outerSize.GetHeightSizeType() != SwFrameSize::Variable)
+    {   // tdf#145871 clear fixed size in first row
+        pSourceLine->ClaimFrameFormat();
+        pSourceLine->GetFrameFormat()->ResetFormatAttr(RES_FRM_SIZE);
+    }
     tools::Long minHeights(0);
     {
         SwFrameFormat const& rSubLineFormat(*pSubTableBox->GetTabLines()[0]->GetFrameFormat());
@@ -2171,12 +2176,14 @@ void SwTable::ConvertSubtableBox(sal_uInt16 const nRow, sal_uInt16 const nBox)
             && outerSize.GetHeightSizeType() != SwFrameSize::Variable
             && minHeights < outerSize.GetHeight())
         {
+            assert(false); // this should be impossible currently, such subtable isn't converted because layout is needed to determine how much space is taken up by variable height rows
             SwFormatFrameSize lastSize(pNewLine->GetFrameFormat()->GetFrameSize());
             lastSize.SetHeight(lastSize.GetHeight() + outerSize.GetHeight() - minHeights);
             if (lastSize.GetHeightSizeType() == SwFrameSize::Variable)
             {
                 lastSize.SetHeightSizeType(SwFrameSize::Minimum);
             }
+            pNewLine->ClaimFrameFormat();
             pNewLine->GetFrameFormat()->SetFormatAttr(lastSize);
         }
         SfxPoolItem const* pRowBrush(nullptr);
@@ -2295,6 +2302,7 @@ bool SwTable::CanConvertSubtables() const
                     return false;
                 }
                 haveSubtable = true;
+                bool haveNonFixedInnerLine(false);
                 for (SwTableLine const*const pInnerLine : pBox->GetTabLines())
                 {
                     // bitmap row background will look different
@@ -2311,11 +2319,29 @@ bool SwTable::CanConvertSubtables() const
                             return false;
                         }
                     }
+                    if (SwFormatFrameSize const* pSize = rRowFormat.GetItemIfSet(RES_FRM_SIZE))
+                    {
+                        if (pSize->GetHeightSizeType() != SwFrameSize::Fixed)
+                        {
+                            haveNonFixedInnerLine = true;
+                        }
+                    }
                     for (SwTableBox const*const pInnerBox : pInnerLine->GetTabBoxes())
                     {
                         if (!pInnerBox->GetTabLines().empty())
                         {
                             return false; // nested subtable :(
+                        }
+                    }
+                }
+                if (haveNonFixedInnerLine)
+                {
+                    if (SwFormatFrameSize const* pSize = pLine->GetFrameFormat()->GetItemIfSet(RES_FRM_SIZE))
+                    {
+                        if (pSize->GetHeightSizeType() != SwFrameSize::Variable)
+                        {
+                            // not possible to distribute fixed outer row height on rows without layout
+                            return false;
                         }
                     }
                 }

commit a8c7e5ea607fbc5451a167ada35c6dff9c89abf6

sw: ODF import: fix assert in SwTable::ConvertSubtableBox()
Assert in swnewtable.cxx:2179 fires:

For ooo98643-1.odt the conversion is not possible, the default for row
height is "variable" which wasn't handled.

For ooo27428-1.sxw the conversion is possible so remove the assert;
the sum of the inner row fixed height is just smaller than the outer
row's but unsurprisingly the subtables is layouted such that the last
row gets the extra space, same as the conversion does.

(regression from commit 4757dfc2a520f63fba0b27cc161fe732231dbd0e)

Change-Id: I2635179f350c8b5b84016dc82625b73364df92a2
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/144622
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/core/table/swnewtable.cxx
@@ -2176,7 +2176,6 @@ void SwTable::ConvertSubtableBox(sal_uInt16 const nRow, sal_uInt16 const nBox)
             && outerSize.GetHeightSizeType() != SwFrameSize::Variable
             && minHeights < outerSize.GetHeight())
         {
-            assert(false); // this should be impossible currently, such subtable isn't converted because layout is needed to determine how much space is taken up by variable height rows
             SwFormatFrameSize lastSize(pNewLine->GetFrameFormat()->GetFrameSize());
             lastSize.SetHeight(lastSize.GetHeight() + outerSize.GetHeight() - minHeights);
             if (lastSize.GetHeightSizeType() == SwFrameSize::Variable)
@@ -2325,6 +2324,10 @@ bool SwTable::CanConvertSubtables() const
                         {
                             haveNonFixedInnerLine = true;
                         }
+                    }
+                    else
+                    {
+                        haveNonFixedInnerLine = true; // default
                     }
                     for (SwTableBox const*const pInnerBox : pInnerLine->GetTabBoxes())
                     {