eabb66b76c7ef454ce52228a7b84b76081bf079f
Justin Luth <justin.luth@collabora.com>
2022-12-24 15:39:06 -0500

    tdf#151548 ww8export: export formfield names
    This should have been looking for ODF_FORMCHECKBOX_NAME
    for checkboxes instead of a never-created grabbag "name".
    
    In any case, since LO 7.5 all the three true formfields
    preserve their name in the name property, so no grabbags
    are necessary any more.
    
    Unforunately the unit test is not very good.
    I copied it from the DOCX case, but I just can't
    get it to fail - even without any patching.
    Yet I know it is broken b/c it is broken in MS Word 2010,
    but now it isn't broken after the patch round-trips it.
    
    Change-Id: I88e92d4c6d0b0e26ba80c428e06e63859941a3ac
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/144815
    Tested-by: Jenkins
    Reviewed-by: Justin Luth <jluth@mail.com>

--- sw/qa/extras/ww8export/ww8export4.cxx
+++ sw/qa/extras/ww8export/ww8export4.cxx
@@ -20,6 +20,7 @@
 #include <o3tl/string_view.hxx>
 
 #include <docsh.hxx>
+#include <IDocumentMarkAccess.hxx>
 #include <IDocumentSettingAccess.hxx>
 #include <unotxdoc.hxx>
 
@@ -63,6 +64,18 @@ DECLARE_WW8EXPORT_TEST(testTdf117994_CRnumformatting, "tdf117994_CRnumformatting
     CPPUNIT_ASSERT_EQUAL(OUString("1."), parseDump("//body/txt[1]/SwParaPortion/SwLineLayout/child::*[@type='PortionType::Number']", "expand"));
     //Without this fix in place, it would become 200 (and non-bold).
     CPPUNIT_ASSERT_EQUAL(OUString("160"), parseDump("//body/txt[1]/SwParaPortion/SwLineLayout/child::*[@type='PortionType::Number']", "font-height"));
+}
+
+DECLARE_WW8EXPORT_TEST(testTdf151548_formFieldMacros, "tdf151548_formFieldMacros.doc")
+{
+    SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument *>(mxComponent.get());
+    SwDoc* pDoc = pTextDoc->GetDocShell()->GetDoc();
+    IDocumentMarkAccess* pMarkAccess = pDoc->getIDocumentMarkAccess();
+    for(auto aIter = pMarkAccess->getFieldmarksBegin(); aIter != pMarkAccess->getFieldmarksEnd(); ++aIter)
+    {
+        const OUString sName = (*aIter)->GetName();
+        CPPUNIT_ASSERT(sName == "Check1" || sName == "Check2" || sName == "Text1" || sName == "Dropdown1");
+    }
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();
