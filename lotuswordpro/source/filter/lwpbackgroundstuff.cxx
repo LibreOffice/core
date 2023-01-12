13cd316bc733fbe37db105dc705b9d172aec8809
Caolán McNamara <caolanm@redhat.com>
2023-01-12 14:50:41 +0000

    ofz#55034 Stack-buffer-overflow in vcl::bitmap::CreateFromData
    since:
    
    commit 21734247d58a6e915b058d8fa55ece949d049613
    Date:   Tue Jan 10 12:56:58 2023 +0200
    
        drop internal support for 1-bit images
    
    the input data is still 1 bit here, even if the output will be 8 bit
    
    Change-Id: Ib7eb301b2e62ebc61ee61ebb99e6de92c1f4931b
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/145410
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
    Tested-by: Caolán McNamara <caolanm@redhat.com>
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>

--- lotuswordpro/source/filter/lwpbackgroundstuff.cxx
+++ lotuswordpro/source/filter/lwpbackgroundstuff.cxx
@@ -114,7 +114,7 @@ std::unique_ptr<XFBGImage> LwpBackgroundStuff::GetFillPattern()
     GetPattern(m_nID, aPttnArray);
 
     // create bitmap object from the pattern array
-    BitmapEx aBmp = vcl::bitmap::CreateFromData(aPttnArray, 8, 8, 1, /*nBitsPerPixel*/ 8);
+    BitmapEx aBmp = vcl::bitmap::CreateFromData(aPttnArray, 8, 8, 1, /*nBitsPerPixel*/ 1);
 
     // create XOBitmap object from bitmap object
     XOBitmap aXOBitmap(aBmp);
