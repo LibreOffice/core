1f9317e341e1636c869c465e152f41f5c4037386
Stephan Bergmann <sbergman@redhat.com>
2023-01-12 08:27:00 +0100

    loplugin:reservedid (clang-cl)
    ...after ef533553559fe09b4afab651fc692885d1acf4ed "Rudimentary support for
    dynamic_cast on UNO proxy objects" added those
    
    > extern "C" IMAGE_DOS_HEADER const __ImageBase;
    
    Change-Id: I4d9cf2b7617180d66a8527e0e36631f81e0fb18d
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/145379
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

--- compilerplugins/clang/reservedid.cxx
+++ compilerplugins/clang/reservedid.cxx
@@ -169,6 +169,8 @@ bool ReservedId::VisitNamedDecl(NamedDecl const * decl) {
                 // vcl/source/window/cairo_cairo.cxx -> include/vcl/sysdata.hxx
             && s != "__CxxDetectRethrow"
                 // bridges/source/cpp_uno/msvc_win32_x86-64/mscx.hxx
+            && s != "__ImageBase"
+                // bridges/source/cpp_uno/msvc_win32_x86-64/cpp2uno.cxx, MS linker magic
             && s != "__PK11_GetKeyData"
                 // xmlsecurity/source/xmlsec/nss/nssrenam.h
             && s != "__current_exception" // bridges/inc/except.hxx, Windows
