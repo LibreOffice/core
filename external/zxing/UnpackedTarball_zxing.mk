6543eb9a5cda3cbd2cd46f35c7f0bc940cd69a45
Taichi Haradaguchi <20001722@ymail.ne.jp>
2023-01-15 12:57:42 +0900

    Mark external/zxing/missing-include.patch.0 as upstreamed
    Change-Id: Ia4c251c952889b4ae174b172839a946302d41c48
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/145524
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

--- external/zxing/UnpackedTarball_zxing.mk
+++ external/zxing/UnpackedTarball_zxing.mk
@@ -15,6 +15,10 @@ $(eval $(call gb_UnpackedTarball_set_patchlevel,zxing,1))
 
 # * external/zxing/android_include.patch.0 was upstreamed at
 # <https://github.com/zxing-cpp/zxing-cpp/pull/458> "add missing #include <cmath> for std::abs"
+# external/zxing/missing-include.patch.0 was upstreamed on
+# <https://github.com/zxing-cpp/zxing-cpp/pull/463> "c++20: add missing include"
+# and <https://github.com/zxing-cpp/zxing-cpp/commit/dd19683d866f3510ae57a077e7651eba031f2f15>
+# "c++20: add missing include"
 $(eval $(call gb_UnpackedTarball_add_patches,zxing, \
 	external/zxing/assume.__cpp_lib_string_view.patch.0 \
 	external/zxing/android_include.patch.0 \
