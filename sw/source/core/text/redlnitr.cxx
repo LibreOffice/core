LibreOffice/core

commit 2bcfb7231b5ca74f02274cfb74ca8463f78905d6

tdf#152872 sw: conditionally hide paragraph breaks
Add a 3rd kind of hiding to SwRootFrame and CheckParaRedlineMerge().

This is quite simple as only consecutive paragraphs are merged.

There is an existing similar feature described in
http://www.openoffice.org/specs/writer/hidden_text/hidden_text.sxw
which results in 0-height text frames if all text is hidden
- but that is unconditional, while Word shows the paragraph when
control chars are shown, and hides it otherwise *iff* its paragraph
marker is hidden (and there's no page break on it).

Change-Id: I8290962ea58278e17b8f84bf6b2ca4bb2325aa8f
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/145162
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/core/text/redlnitr.cxx
@@ -47,8 +47,11 @@
 #include <vcl/svapp.hxx>
 #include "redlnitr.hxx"
 #include <extinput.hxx>
+#include <fmtpdsc.hxx>
+#include <editeng/charhiddenitem.hxx>
 #include <editeng/colritem.hxx>
 #include <editeng/crossedoutitem.hxx>
+#include <editeng/formatbreakitem.hxx>
 #include <editeng/udlnitem.hxx>
 
 using namespace ::com::sun::star;
@@ -62,12 +65,15 @@ private:
     IDocumentMarkAccess const& m_rIDMA;
     bool const m_isHideRedlines;
     sw::FieldmarkMode const m_eFieldmarkMode;
+    bool const m_isHideParagraphBreaks;
     SwPosition const m_Start;
     /// next redline
     SwRedlineTable::size_type m_RedlineIndex;
     /// next fieldmark
     std::pair<sw::mark::IFieldmark const*, std::optional<SwPosition>> m_Fieldmark;
     std::optional<SwPosition> m_oNextFieldmarkHide;
+    /// previous paragraph break - because m_pStartPos/EndPos are non-owning
+    std::optional<std::pair<SwPosition, SwPosition>> m_oParagraphBreak;
     /// current start/end pair
     SwPosition const* m_pStartPos;
     SwPosition const* m_pEndPos;
@@ -77,11 +83,13 @@ public:
     SwPosition const* GetEndPos() const { return m_pEndPos; }
 
     HideIterator(SwTextNode & rTextNode,
-            bool const isHideRedlines, sw::FieldmarkMode const eMode)
+            bool const isHideRedlines, sw::FieldmarkMode const eMode,
+            sw::ParagraphBreakMode const ePBMode)
         : m_rIDRA(rTextNode.getIDocumentRedlineAccess())
         , m_rIDMA(*rTextNode.getIDocumentMarkAccess())
         , m_isHideRedlines(isHideRedlines)
         , m_eFieldmarkMode(eMode)
+        , m_isHideParagraphBreaks(ePBMode == sw::ParagraphBreakMode::Hidden)
         , m_Start(rTextNode, 0)
         , m_RedlineIndex(isHideRedlines ? m_rIDRA.GetRedlinePos(rTextNode, RedlineType::Any) : SwRedlineTable::npos)
         , m_pStartPos(nullptr)
@@ -188,12 +196,65 @@ public:
             m_pEndPos = &*m_Fieldmark.second;
             return true;
         }
-        else // nothing
+        else
         {
             assert(!pNextRedlineHide && !m_oNextFieldmarkHide);
-            m_pStartPos = nullptr;
-            m_pEndPos = nullptr;
-            return false;
+            auto const hasHiddenItem = [](auto const& rNode) {
+                auto const& rpSet(rNode.GetAttr(RES_PARATR_LIST_AUTOFMT).GetStyleHandle());
+                return rpSet ? rpSet->Get(RES_CHRATR_HIDDEN).GetValue() : false;
+            };
+            auto const hasBreakBefore = [](SwTextNode const& rNode) {
+                if (rNode.GetAttr(RES_PAGEDESC).GetPageDesc())
+                {
+                    return true;
+                }
+                switch (rNode.GetAttr(RES_BREAK).GetBreak())
+                {
+                    case SvxBreak::ColumnBefore:
+                    case SvxBreak::ColumnBoth:
+                    case SvxBreak::PageBefore:
+                    case SvxBreak::PageBoth:
+                        return true;
+                    default:
+                        break;
+                }
+                return false;
+            };
+            auto const hasBreakAfter = [](SwTextNode const& rNode) {
+                switch (rNode.GetAttr(RES_BREAK).GetBreak())
+                {
+                    case SvxBreak::ColumnAfter:
+                    case SvxBreak::ColumnBoth:
+                    case SvxBreak::PageAfter:
+                    case SvxBreak::PageBoth:
+                        return true;
+                    default:
+                        break;
+                }
+                return false;
+            };
+            if (m_isHideParagraphBreaks
+                // only merge if next node is also text node
+                && m_pEndPos->GetNodes()[m_pEndPos->GetNodeIndex()+1]->IsTextNode()
+                && hasHiddenItem(*m_pEndPos->GetNode().GetTextNode())
+                // no merge if there's a page break on any node
+                && !hasBreakBefore(*m_pEndPos->GetNodes()[m_pEndPos->GetNodeIndex()+1]->GetTextNode())
+                // first node, see SwTextFrame::GetBreak()
+                && !hasBreakAfter(*m_Start.GetNode().GetTextNode()))
+            {
+                m_oParagraphBreak.emplace(
+                    SwPosition(*m_pEndPos->GetNode().GetTextNode(), m_pEndPos->GetNode().GetTextNode()->Len()),
+                    SwPosition(*m_pEndPos->GetNodes()[m_pEndPos->GetNodeIndex()+1]->GetTextNode(), 0));
+                m_pStartPos = &m_oParagraphBreak->first;
+                m_pEndPos = &m_oParagraphBreak->second;
+                return true;
+            }
+            else // nothing
+            {
+                m_pStartPos = nullptr;
+                m_pEndPos = nullptr;
+                return false;
+            }
         }
     }
 };
@@ -221,7 +282,9 @@ CheckParaRedlineMerge(SwTextFrame & rFrame, SwTextNode & rTextNode,
     sal_Int32 nLastEnd(0);
     for (auto iter = HideIterator(rTextNode,
                 rFrame.getRootFrame()->IsHideRedlines(),
-                rFrame.getRootFrame()->GetFieldmarkMode()); iter.Next(); )
+                rFrame.getRootFrame()->GetFieldmarkMode(),
+                rFrame.getRootFrame()->GetParagraphBreakMode());
+            iter.Next(); )
     {
         SwPosition const*const pStart(iter.GetStartPos());
         SwPosition const*const pEnd(iter.GetEndPos());

sw/inc/doc.hxx
@@ -142,6 +142,7 @@ namespace sw::mark { class MarkManager; }
 namespace sw {
     enum class RedlineMode;
     enum class FieldmarkMode;
+    enum class ParagraphBreakMode;
     class MetaFieldManager;
     class UndoManager;
     class IShellCursorSupplier;
@@ -1339,7 +1340,7 @@ public:
 
     // insert section (the ODF kind of section, not the nodesarray kind)
     SwSection * InsertSwSection(SwPaM const& rRange, SwSectionData &,
-            std::tuple<SwTOXBase const*, sw::RedlineMode, sw::FieldmarkMode> const* pTOXBase,
+            std::tuple<SwTOXBase const*, sw::RedlineMode, sw::FieldmarkMode, sw::ParagraphBreakMode> const* pTOXBase,
             SfxItemSet const*const pAttr, bool const bUpdate = true);
     static sal_uInt16 IsInsRegionAvailable( const SwPaM& rRange,
                                 const SwNode** ppSttNd = nullptr );

sw/qa/extras/layout/layout2.cxx
@@ -636,6 +636,43 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter2, testTdf149711_importDOCXMoveToParagraphMar
     assertXPath(pXmlDoc, "/root/page[1]/body/txt", 5);
 }
 
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter2, testTdf152872)
+{
+    createSwDoc("hidden-para-separator.docx");
+    xmlDocUniquePtr pXmlDoc = parseLayoutDump();
+
+    assertXPath(pXmlDoc, "/root/page[1]/body/txt", 2);
+    assertXPath(pXmlDoc, "/root/page/body/txt[1]/SwParaPortion/SwLineLayout", "portion", "C DE");
+    assertXPath(pXmlDoc, "/root/page/body/txt[2]/SwParaPortion", 0); // 5 is empty
+    assertXPath(pXmlDoc, "/root/page/body/txt[2]/infos/bounds", "height", "379");
+
+    dispatchCommand(mxComponent, ".uno:ControlCodes", {});
+
+    discardDumpedLayout();
+    pXmlDoc = parseLayoutDump();
+
+    assertXPath(pXmlDoc, "/root/page[1]/body/txt", 5);
+    assertXPath(pXmlDoc, "/root/page/body/txt[1]/SwParaPortion/SwLineLayout", "portion", "C ");
+    assertXPath(pXmlDoc, "/root/page/body/txt[2]/SwParaPortion/SwLineLayout", "portion", "D");
+    // 3 is an empty paragraph with RES_CHRATR_HIDDEN which results in 0-height
+    // frame; ideally it should only be hidden when control codes are hidden
+    // and be a full-height frame now, but that needs more work...
+    assertXPath(pXmlDoc, "/root/page/body/txt[3]/infos/bounds", "height", "0");
+    assertXPath(pXmlDoc, "/root/page/body/txt[4]/SwParaPortion/SwLineLayout", "portion", "E");
+    assertXPath(pXmlDoc, "/root/page/body/txt[5]/SwParaPortion", 0); // 5 is empty
+    assertXPath(pXmlDoc, "/root/page/body/txt[5]/infos/bounds", "height", "379");
+
+    dispatchCommand(mxComponent, ".uno:ControlCodes", {});
+
+    discardDumpedLayout();
+    pXmlDoc = parseLayoutDump();
+
+    assertXPath(pXmlDoc, "/root/page[1]/body/txt", 2);
+    assertXPath(pXmlDoc, "/root/page/body/txt[1]/SwParaPortion/SwLineLayout", "portion", "C DE");
+    assertXPath(pXmlDoc, "/root/page/body/txt[2]/SwParaPortion", 0); // 5 is empty
+    assertXPath(pXmlDoc, "/root/page/body/txt[2]/infos/bounds", "height", "379");
+}
+
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter2, testTdf151954)
 {
     createSwDoc("tdf151954.docx");

sw/source/core/doc/doctxm.cxx
@@ -363,12 +363,13 @@ SwTOXBaseSection* SwDoc::InsertTableOf( const SwPaM& aPam,
     OUString sSectNm = GetUniqueTOXBaseName( *rTOX.GetTOXType(), rTOX.GetTOXName() );
     SwSectionData aSectionData( SectionType::ToxContent, sSectNm );
 
-    std::tuple<SwTOXBase const*, sw::RedlineMode, sw::FieldmarkMode> const tmp(
+    std::tuple<SwTOXBase const*, sw::RedlineMode, sw::FieldmarkMode, sw::ParagraphBreakMode> const tmp(
         &rTOX,
         pLayout && pLayout->IsHideRedlines()
             ? sw::RedlineMode::Hidden
             : sw::RedlineMode::Shown,
-        pLayout ? pLayout->GetFieldmarkMode() : sw::FieldmarkMode::ShowBoth);
+        pLayout ? pLayout->GetFieldmarkMode() : sw::FieldmarkMode::ShowBoth,
+        pLayout ? pLayout->GetParagraphBreakMode() : sw::ParagraphBreakMode::Shown);
     SwTOXBaseSection *const pNewSection = dynamic_cast<SwTOXBaseSection *>(
         InsertSwSection(aPam, aSectionData, & tmp, pSet, false));
     if (pNewSection)

sw/source/core/docnode/ndsect.cxx
@@ -152,7 +152,7 @@ static void lcl_CheckEmptyLayFrame( SwNodes const & rNds, SwSectionData& rSectio
 
 SwSection *
 SwDoc::InsertSwSection(SwPaM const& rRange, SwSectionData & rNewData,
-       std::tuple<SwTOXBase const*, sw::RedlineMode, sw::FieldmarkMode> const*const pTOXBaseAndMode,
+       std::tuple<SwTOXBase const*, sw::RedlineMode, sw::FieldmarkMode, sw::ParagraphBreakMode> const*const pTOXBaseAndMode,
                        SfxItemSet const*const pAttr, bool const bUpdate)
 {
     const SwNode* pPrvNd = nullptr;

sw/source/core/inc/UndoSection.hxx
@@ -35,13 +35,14 @@ class SwTOXBase;
 namespace sw {
     enum class RedlineMode;
     enum class FieldmarkMode;
+    enum class ParagraphBreakMode;
 };
 
 class SwUndoInsSection final : public SwUndo, private SwUndRng
 {
 private:
     const std::unique_ptr<SwSectionData> m_pSectionData;
-    std::optional<std::tuple<std::unique_ptr<SwTOXBase>, sw::RedlineMode, sw::FieldmarkMode>> m_xTOXBase; /// set iff section is TOX
+    std::optional<std::tuple<std::unique_ptr<SwTOXBase>, sw::RedlineMode, sw::FieldmarkMode, sw::ParagraphBreakMode>> m_xTOXBase; /// set iff section is TOX
     const std::unique_ptr<SfxItemSet> m_pAttrSet;
     std::unique_ptr<SwHistory> m_pHistory;
     std::unique_ptr<SwRedlineData> m_pRedlData;
@@ -56,7 +57,7 @@ private:
 public:
     SwUndoInsSection(SwPaM const&, SwSectionData const&,
         SfxItemSet const* pSet,
-        std::tuple<SwTOXBase const*, sw::RedlineMode, sw::FieldmarkMode> const* pTOXBase);
+        std::tuple<SwTOXBase const*, sw::RedlineMode, sw::FieldmarkMode, sw::ParagraphBreakMode> const* pTOXBase);
 
     virtual ~SwUndoInsSection() override;
 

sw/source/core/inc/rootfrm.hxx
@@ -47,6 +47,7 @@ namespace sw {
     };
 
     enum class FieldmarkMode { ShowCommand = 1, ShowResult = 2, ShowBoth = 3 };
+    enum class ParagraphBreakMode { Shown, Hidden };
 };
 
 enum class SwInvalidateFlags
@@ -121,6 +122,7 @@ class SW_DLLPUBLIC SwRootFrame final : public SwLayoutFrame
     bool    mbLayoutFreezed;
     bool    mbHideRedlines;
     sw::FieldmarkMode m_FieldmarkMode;
+    sw::ParagraphBreakMode m_ParagraphBreakMode;
 
     /**
      * For BrowseMode
@@ -421,10 +423,9 @@ public:
     bool IsHideRedlines() const { return mbHideRedlines; }
     void SetHideRedlines(bool);
     sw::FieldmarkMode GetFieldmarkMode() const { return m_FieldmarkMode; }
-    void SetFieldmarkMode(sw::FieldmarkMode);
-    bool HasMergedParas() const {
-        return IsHideRedlines() || GetFieldmarkMode() != sw::FieldmarkMode::ShowBoth;
-    }
+    void SetFieldmarkMode(sw::FieldmarkMode, sw::ParagraphBreakMode);
+    sw::ParagraphBreakMode GetParagraphBreakMode() const { return m_ParagraphBreakMode; }
+    bool HasMergedParas() const;
 };
 
 inline tools::Long SwRootFrame::GetBrowseWidth() const

commit ee73d14e06180d8a60fc4fc34ab71f23095e7621

tdf#152872 sw: fix crash on ooo27109-1.sxw
Has a redline that ends on the body end node.

(regression from commit 2bcfb7231b5ca74f02274cfb74ca8463f78905d6)

Change-Id: I9dc89a496791935b9d28e1874f947b70fdf0b85a
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/145569
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/core/text/redlnitr.cxx
@@ -234,6 +234,7 @@ public:
                 return false;
             };
             if (m_isHideParagraphBreaks
+                && m_pEndPos->GetNode().IsTextNode() // ooo27109-1.sxw
                 // only merge if next node is also text node
                 && m_pEndPos->GetNodes()[m_pEndPos->GetNodeIndex()+1]->IsTextNode()
                 && hasHiddenItem(*m_pEndPos->GetNode().GetTextNode())