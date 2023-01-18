LibreOffice/core

commit 6f43f00279252265fe4892b46fe0b4686ff721ed

No need for bool return value here
all of the implementations of this method return true.

Change-Id: I6dc02499af1809110edd482a48d9f6d5d42ead19
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/144620
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/source/gdi/salgdilayout.cxx
@@ -373,15 +373,15 @@ const basegfx::B2DHomMatrix& SalGraphics::getMirror( const OutputDevice& i_rOutD
     return m_aLastMirror;
 }
 
-bool SalGraphics::SetClipRegion( const vcl::Region& i_rClip, const OutputDevice& rOutDev )
+void SalGraphics::SetClipRegion( const vcl::Region& i_rClip, const OutputDevice& rOutDev )
 {
     if( (m_nLayout & SalLayoutFlags::BiDiRtl) || rOutDev.IsRTLEnabled() )
     {
         vcl::Region aMirror( i_rClip );
         mirror( aMirror, rOutDev );
-        return setClipRegion( aMirror );
+        setClipRegion( aMirror );
     }
-    return setClipRegion( i_rClip );
+    setClipRegion( i_rClip );
 }
 
 void SalGraphics::DrawPixel( tools::Long nX, tools::Long nY, const OutputDevice& rOutDev )

vcl/headless/SvpGraphicsBackend.cxx
@@ -34,10 +34,9 @@ void SvpGraphicsBackend::Init() {}
 
 void SvpGraphicsBackend::freeResources() {}
 
-bool SvpGraphicsBackend::setClipRegion(const vcl::Region& i_rClip)
+void SvpGraphicsBackend::setClipRegion(const vcl::Region& i_rClip)
 {
     m_rCairoCommon.m_aClipRegion = i_rClip;
-    return true;
 }
 
 void SvpGraphicsBackend::ResetClipRegion() { m_rCairoCommon.m_aClipRegion.SetNull(); }

vcl/inc/headless/SvpGraphicsBackend.hxx
@@ -40,7 +40,7 @@ public:
 
     OUString getRenderBackendName() const override { return "svp"; }
 
-    bool setClipRegion(vcl::Region const& rRegion) override;
+    void setClipRegion(vcl::Region const& rRegion) override;
     void ResetClipRegion() override;
 
     sal_uInt16 GetBitCount() const override;

vcl/inc/qt5/QtGraphics.hxx
@@ -68,7 +68,7 @@ public:
 
     OUString getRenderBackendName() const override { return "qt5"; }
 
-    bool setClipRegion(vcl::Region const& rRegion) override;
+    void setClipRegion(vcl::Region const& rRegion) override;
     void ResetClipRegion() override;
 
     sal_uInt16 GetBitCount() const override;

vcl/inc/quartz/salgdi.h
@@ -340,7 +340,7 @@ public:
         return "aqua";
     }
 
-    bool setClipRegion(vcl::Region const& rRegion) override;
+    void setClipRegion(vcl::Region const& rRegion) override;
     void ResetClipRegion() override;
 
     sal_uInt16 GetBitCount() const override;

vcl/inc/salgdi.hxx
@@ -183,7 +183,7 @@ public:
 
     // non virtual methods; these do possible coordinate mirroring and
     // then delegate to protected virtual methods
-    bool                        SetClipRegion( const vcl::Region&, const OutputDevice& rOutDev );
+    void                        SetClipRegion( const vcl::Region&, const OutputDevice& rOutDev );
 
     // draw --> LineColor and FillColor and RasterOp and ClipRegion
     void                        DrawPixel( tools::Long nX, tools::Long nY, const OutputDevice& rOutDev );
@@ -426,7 +426,7 @@ protected:
 
     friend class vcl::FileDefinitionWidgetDraw;
 
-    virtual bool                setClipRegion( const vcl::Region& ) = 0;
+    virtual void                setClipRegion( const vcl::Region& ) = 0;
 
     // draw --> LineColor and FillColor and RasterOp and ClipRegion
     virtual void                drawPixel( tools::Long nX, tools::Long nY ) = 0;
@@ -661,9 +661,9 @@ public:
         GetImpl()->ResetClipRegion();
     }
 
-    bool setClipRegion( const vcl::Region& i_rClip ) override
+    void setClipRegion( const vcl::Region& i_rClip ) override
     {
-        return GetImpl()->setClipRegion(i_rClip);
+        GetImpl()->setClipRegion(i_rClip);
     }
 
     void SetLineColor() override

vcl/inc/salgdiimpl.hxx
@@ -80,7 +80,7 @@ public:
 
     virtual OUString getRenderBackendName() const = 0;
 
-    virtual bool setClipRegion( const vcl::Region& ) = 0;
+    virtual void setClipRegion( const vcl::Region& ) = 0;
 
     virtual sal_uInt16 GetBitCount() const = 0;
 

vcl/inc/skia/gdiimpl.hxx
@@ -47,7 +47,7 @@ public:
     virtual OUString getRenderBackendName() const override { return "skia"; }
 
     const vcl::Region& getClipRegion() const;
-    virtual bool setClipRegion(const vcl::Region&) override;
+    virtual void setClipRegion(const vcl::Region&) override;
 
     //
     // get the depth of the device

commit 305e5fe1ba3df1dbe1f961509fe2c691b5193a6f

tdf#153080 RTL UI was misbehaving
Regression from
6f43f00279252265fe4892b46fe0b4686ff721ed

Change-Id: I472381252641bdcd1416bad0893c416a041cc49f
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/145704
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Jenkins
Reviewed-by: Aron Budea <aron.budea@collabora.com>

vcl/source/gdi/salgdilayout.cxx
@@ -381,7 +381,10 @@ void SalGraphics::SetClipRegion( const vcl::Region& i_rClip, const OutputDevice&
         mirror( aMirror, rOutDev );
         setClipRegion( aMirror );
     }
-    setClipRegion( i_rClip );
+    else
+    {
+        setClipRegion( i_rClip );
+    }
 }
 
 void SalGraphics::DrawPixel( tools::Long nX, tools::Long nY, const OutputDevice& rOutDev )