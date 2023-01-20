bd0b3aac25b5163ae21fff613131cb3ff068e816
Caolán McNamara <caolanm@redhat.com>
2023-01-20 21:08:17 +0000

    tdf#144915 effectively remove limit on number of words in dictionary
    this was 2000 entries in the initial checkin, then changed to 30000
    (in 2008). Lets makes it effectively unlimited.
    
    Change-Id: Ia757e3283f1ff41d62e6738af9d1f6674ba2fd56
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/145916
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>

--- linguistic/source/dicimp.hxx
+++ linguistic/source/dicimp.hxx
@@ -30,7 +30,7 @@
 
 #include "defs.hxx"
 
-#define DIC_MAX_ENTRIES     30000
+#define DIC_MAX_ENTRIES SAL_MAX_INT32
 
 sal_Int16 ReadDicVersion( SvStream& rStream, LanguageType &nLng, bool &bNeg, OUString &aDicName );
 
