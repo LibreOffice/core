23e6f05a8ee80c33dd624dd6582f26b3dcf8f490
Gökay Şatır <gokaysatir@collabora.com>
2023-01-20 12:06:47 +0300

    Add dropdownfielddialog.ui to JSON dialogues.
    Signed-off-by: Gökay Şatır <gokaysatir@collabora.com>
    Change-Id: I4cfc792f0b1269936723285cc5ff1324bc22c161
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/145871
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

--- vcl/jsdialog/enabled.cxx
+++ vcl/jsdialog/enabled.cxx
@@ -78,6 +78,7 @@ bool isBuilderEnabled(std::u16string_view rUIFile, bool bMobile)
         || rUIFile == u"modules/swriter/ui/splittable.ui"
         || rUIFile == u"modules/swriter/ui/translationdialog.ui"
         || rUIFile == u"modules/swriter/ui/pagenumberdlg.ui"
+        || rUIFile == u"modules/swriter/ui/dropdownfielddialog.ui"
         // sfx
         || rUIFile == u"sfx/ui/cmisinfopage.ui"
         || rUIFile == u"sfx/ui/custominfopage.ui"
