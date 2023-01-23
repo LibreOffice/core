LibreOffice/core

commit 2e7f77d3fe01e36222d5ad84c18d7d7537092972

loplugin:unusedfields
Change-Id: I37207c3610c25d9e37e77bdae16b613dd8410525
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/143015
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svx/source/form/datanavi.cxx
@@ -2066,8 +2066,6 @@ namespace svxform
         , m_xDefaultED(m_xBuilder->weld_entry("value"))
         , m_xDefaultBtn(m_xBuilder->weld_button("browse"))
         , m_xSettingsFrame(m_xBuilder->weld_widget("settingsframe"))
-        , m_xDataTypeFT(m_xBuilder->weld_label("datatypeft"))
-        , m_xDataTypeLB(m_xBuilder->weld_combo_box("datatype"))
         , m_xRequiredCB(m_xBuilder->weld_check_button("required"))
         , m_xRequiredBtn(m_xBuilder->weld_button("requiredcond"))
         , m_xRelevantCB(m_xBuilder->weld_check_button("relevant"))
@@ -3044,7 +3042,6 @@ namespace svxform
     AddInstanceDialog::AddInstanceDialog(weld::Window* pParent, bool _bEdit)
         : GenericDialogController(pParent, "svx/ui/addinstancedialog.ui", "AddInstanceDialog")
         , m_xNameED(m_xBuilder->weld_entry("name"))
-        , m_xURLFT(m_xBuilder->weld_label("urlft"))
         , m_xURLED(new SvtURLBox(m_xBuilder->weld_combo_box("url")))
         , m_xFilePickerBtn(m_xBuilder->weld_button("browse"))
         , m_xLinkInstanceCB(m_xBuilder->weld_check_button("link"))

svx/source/dialog/SafeModeDialog.cxx
@@ -50,7 +50,6 @@ SafeModeDialog::SafeModeDialog(weld::Window* pParent)
     , mxBugLink(m_xBuilder->weld_link_button("linkbutton_bugs"))
     , mxUserProfileLink(m_xBuilder->weld_link_button("linkbutton_profile"))
     , mxBtnCreateZip(m_xBuilder->weld_button("btn_create_zip"))
-    , mxExpander(m_xBuilder->weld_expander("expander"))
 {
     m_xDialog->set_centered_on_parent(false);
     mxRadioRestore->connect_toggled(LINK(this, SafeModeDialog, RadioBtnHdl));

svx/source/dialog/SafeModeDialog.hxx
@@ -48,7 +48,6 @@ private:
     std::unique_ptr<weld::LinkButton> mxBugLink;
     std::unique_ptr<weld::LinkButton> mxUserProfileLink;
     std::unique_ptr<weld::Button> mxBtnCreateZip;
-    std::unique_ptr<weld::Expander> mxExpander;
 
     // local BackupFileHelper for handling possible restores
     comphelper::BackupFileHelper maBackupFileHelper;

svx/source/inc/datanavi.hxx
@@ -364,7 +364,6 @@ namespace svxform
         std::unique_ptr<weld::Entry> m_xDefaultED;
         std::unique_ptr<weld::Button> m_xDefaultBtn;
         std::unique_ptr<weld::Widget> m_xSettingsFrame;
-        std::unique_ptr<weld::Label> m_xDataTypeFT;
         std::unique_ptr<weld::ComboBox> m_xDataTypeLB;
         std::unique_ptr<weld::CheckButton> m_xRequiredCB;
         std::unique_ptr<weld::Button> m_xRequiredBtn;
@@ -547,7 +546,6 @@ namespace svxform
         OUString                m_sAllFilterName;
 
         std::unique_ptr<weld::Entry> m_xNameED;
-        std::unique_ptr<weld::Label> m_xURLFT;
         std::unique_ptr<SvtURLBox> m_xURLED;
         std::unique_ptr<weld::Button> m_xFilePickerBtn;
         std::unique_ptr<weld::CheckButton> m_xLinkInstanceCB;

svx/source/sidebar/EmptyPanel.cxx
@@ -23,7 +23,6 @@ namespace svx::sidebar
 {
 EmptyPanel::EmptyPanel(weld::Widget* pParent)
     : PanelLayout(pParent, "EmptyPanel", "svx/ui/sidebarempty.ui")
-    , mxMessageControl(m_xBuilder->weld_label("message"))
 {
 }
 

svx/source/sidebar/EmptyPanel.hxx
@@ -31,9 +31,6 @@ class EmptyPanel final : public PanelLayout
 public:
     explicit EmptyPanel(weld::Widget* pParent);
     virtual ~EmptyPanel() override;
-
-private:
-    std::unique_ptr<weld::Label> mxMessageControl;
 };
 
 } // end of namespace svx::sidebar

svx/source/sidebar/graphic/GraphicPropertyPanel.hxx
@@ -69,10 +69,6 @@ private:
     std::unique_ptr<weld::MetricSpinButton> mxMtrContrast;
     std::unique_ptr<weld::ComboBox> mxLBColorMode;
     std::unique_ptr<weld::MetricSpinButton> mxMtrTrans;
-    std::unique_ptr<weld::MetricSpinButton> mxMtrRed;
-    std::unique_ptr<weld::MetricSpinButton> mxMtrGreen;
-    std::unique_ptr<weld::MetricSpinButton> mxMtrBlue;
-    std::unique_ptr<weld::SpinButton> mxMtrGamma;
 
     DECL_LINK( ModifyBrightnessHdl, weld::MetricSpinButton&, void );
     DECL_LINK( ModifyContrastHdl, weld::MetricSpinButton&, void );

svx/source/unodialogs/textconversiondlgs/chinese_dictionarydialog.cxx
@@ -302,11 +302,8 @@ ChineseDictionaryDialog::ChineseDictionaryDialog(weld::Window* pParent)
     , m_xRB_To_Simplified(m_xBuilder->weld_radio_button("tradtosimple"))
     , m_xRB_To_Traditional(m_xBuilder->weld_radio_button("simpletotrad"))
     , m_xCB_Reverse(m_xBuilder->weld_check_button("reverse"))
-    , m_xFT_Term(m_xBuilder->weld_label("termft"))
     , m_xED_Term(m_xBuilder->weld_entry("term"))
-    , m_xFT_Mapping(m_xBuilder->weld_label("mappingft"))
     , m_xED_Mapping(m_xBuilder->weld_entry("mapping"))
-    , m_xFT_Property(m_xBuilder->weld_label("propertyft"))
     , m_xLB_Property(m_xBuilder->weld_combo_box("property"))
     , m_xCT_DictionaryToSimplified(new DictionaryList(m_xBuilder->weld_tree_view("tradtosimpleview")))
     , m_xCT_DictionaryToTraditional(new DictionaryList(m_xBuilder->weld_tree_view("simpletotradview")))

commit adb82d978498186ea2389fc1e48a9170db62c18e

tdf#153154: m_xDataTypeLB is still in use
Regression from 2e7f77d3fe01e36222d5ad84c18d7d7537092972
"loplugin:unusedfields"

Change-Id: Ic9435ee9417f812281ad75039d4ef88f508bad62
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/145999
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Jenkins
Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

svx/source/form/datanavi.cxx
@@ -2066,6 +2066,7 @@ namespace svxform
         , m_xDefaultED(m_xBuilder->weld_entry("value"))
         , m_xDefaultBtn(m_xBuilder->weld_button("browse"))
         , m_xSettingsFrame(m_xBuilder->weld_widget("settingsframe"))
+        , m_xDataTypeLB(m_xBuilder->weld_combo_box("datatype"))
         , m_xRequiredCB(m_xBuilder->weld_check_button("required"))
         , m_xRequiredBtn(m_xBuilder->weld_button("requiredcond"))
         , m_xRelevantCB(m_xBuilder->weld_check_button("relevant"))