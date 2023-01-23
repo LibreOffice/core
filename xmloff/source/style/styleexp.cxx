LibreOffice/core

commit 05f863844d9a5613250e8d787e32752b270ec4d3

sw: paragraph styles: add ODT filter for a linked character style
And the same in the other direction: link a para style from a char
style.

With this, the ODT filter is on par with the DOCX one for this feature.

Change-Id: Idd994b933672ab47a5f87a75c92abc137d3c73b2
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/122753
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

xmloff/source/style/styleexp.cxx
@@ -237,6 +237,19 @@ bool XMLStyleExport::exportStyle(
         }
     }
 
+    // style:linked-style-name="..." (SW paragraph and character styles only)
+    if (xPropSetInfo->hasPropertyByName("LinkStyle"))
+    {
+        aAny = xPropSet->getPropertyValue("LinkStyle");
+        OUString sLinkName;
+        aAny >>= sLinkName;
+        if (!sLinkName.isEmpty())
+        {
+            GetExport().AddAttribute(XML_NAMESPACE_LO_EXT, XML_LINKED_STYLE_NAME,
+                                     GetExport().EncodeStyleName(sLinkName));
+        }
+    }
+
     // style:auto-update="..." (SW only)
     if( xPropSetInfo->hasPropertyByName( gsIsAutoUpdate ) )
     {

include/xmloff/xmlstyle.hxx
@@ -44,6 +44,7 @@ class XMLOFF_DLLPUBLIC SvXMLStyleContext : public SvXMLImportContext
     OUString     maAutoName;
     OUString     maParentName;// Will be moved to XMLPropStyle soon!!!!
     OUString     maFollow;    // Will be moved to XMLPropStyle soon!!!!
+    OUString     maLinked;
     bool         mbHidden;
 
     XmlStyleFamily mnFamily;
@@ -79,6 +80,7 @@ public:
     const OUString&  GetAutoName() const { return maAutoName; }
     const OUString&  GetParentName() const { return maParentName; }
     const OUString&  GetFollow() const { return maFollow; }
+    const OUString&  GetLinked() const { return maLinked; }
 
     XmlStyleFamily GetFamily() const { return mnFamily; }
 

include/xmloff/xmltoken.hxx
@@ -3437,6 +3437,8 @@ namespace xmloff::token {
         XML_RTL,
         XML_SYMMETRIC,
 
+        XML_LINKED_STYLE_NAME,
+
         XML_TOKEN_END
     };
 

schema/libreoffice/OpenDocument-v1.3+libreoffice-schema.rng
@@ -2731,4 +2731,13 @@ xmlns:loext="urn:org:documentfoundation:names:experimental:office:xmlns:loext:1.
     </rng:optional>
   </rng:define>
 
+  <!-- TODO no proposal -->
+  <rng:define name="style-style-attlist" combine="interleave">
+    <rng:optional>
+      <rng:attribute name="loext:linked-style-name">
+        <rng:ref name="styleNameRef"/>
+      </rng:attribute>
+    </rng:optional>
+  </rng:define>
+
 </rng:grammar>

sw/qa/extras/odfexport/data/style-link.fodt
@@ -1,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<office:document xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0" xmlns:style="urn:oasis:names:tc:opendocument:xmlns:style:1.0" xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0" xmlns:loext="urn:org:documentfoundation:names:experimental:office:xmlns:loext:1.0" office:version="1.3" office:mimetype="application/vnd.oasis.opendocument.text">
+  <office:styles>
+    <style:style style:name="List_20_Paragraph" style:display-name="List Paragraph" style:family="paragraph" loext:linked-style-name="List_20_Paragraph_20_Char"/>
+    <style:style style:name="List_20_Paragraph_20_Char" style:display-name="List Paragraph Char" style:family="text" loext:linked-style-name="List_20_Paragraph"/>
+  </office:styles>
+  <office:master-styles>
+    <style:master-page style:name="Standard"/>
+  </office:master-styles>
+  <office:body>
+    <office:text>
+      <text:p/>
+    </office:text>
+  </office:body>
+</office:document>

sw/qa/extras/odfexport/odfexport2.cxx
@@ -184,6 +184,27 @@ DECLARE_ODFEXPORT_TEST(testListFormatOdt, "listformat.odt")
     }
 }
 
+CPPUNIT_TEST_FIXTURE(Test, testStyleLink)
+{
+    // Given a document with a para and a char style that links each other, when loading that
+    // document:
+    load(mpTestDocumentPath, "style-link.fodt");
+
+    // Then make sure the char style links the para one:
+    uno::Any aCharStyle = getStyles("CharacterStyles")->getByName("List Paragraph Char");
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: List Paragraph
+    // - Actual  :
+    // i.e. the linked style was lost on import.
+    CPPUNIT_ASSERT_EQUAL(OUString("List Paragraph"), getProperty<OUString>(aCharStyle, "LinkStyle"));
+    uno::Any aParaStyle = getStyles("ParagraphStyles")->getByName("List Paragraph");
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: List Paragraph Char
+    // - Actual  :
+    // i.e. the linked style was lost on import.
+    CPPUNIT_ASSERT_EQUAL(OUString("List Paragraph Char"), getProperty<OUString>(aParaStyle, "LinkStyle"));
+}
+
 // This test started in LO 7.2. Use the odfexport.cxx if you intend to backport to 7.1.
 
 CPPUNIT_PLUGIN_IMPLEMENT();

xmloff/source/core/xmltoken.cxx
@@ -3440,6 +3440,8 @@ namespace xmloff::token {
         TOKEN("rtl",                 XML_RTL ),
         TOKEN("symmetric",           XML_SYMMETRIC ),
 
+        TOKEN("linked-style-name",   XML_LINKED_STYLE_NAME ),
+
 
 #if OSL_DEBUG_LEVEL > 0
         { 0, nullptr, std::nullopt,               XML_TOKEN_END }

xmloff/source/style/prstylei.cxx
@@ -509,6 +509,30 @@ void XMLPropStyleContext::Finish( bool bOverwrite )
         }
     }
 
+    // Connect linked style.
+    OUString aLinked(GetLinked());
+    if (!aLinked.isEmpty())
+    {
+        if (GetFamily() == XmlStyleFamily::TEXT_PARAGRAPH)
+        {
+            aLinked = GetImport().GetStyleDisplayName(XmlStyleFamily::TEXT_TEXT, aLinked);
+        }
+        else if (GetFamily() == XmlStyleFamily::TEXT_TEXT)
+        {
+            aLinked = GetImport().GetStyleDisplayName(XmlStyleFamily::TEXT_PARAGRAPH, aLinked);
+        }
+    }
+    if (!aLinked.isEmpty() && xPropSetInfo->hasPropertyByName("LinkStyle"))
+    {
+        uno::Any aAny = xPropSet->getPropertyValue("LinkStyle");
+        OUString aCurrentLinked;
+        aAny >>= aCurrentLinked;
+        if (aCurrentLinked != aLinked)
+        {
+            xPropSet->setPropertyValue("LinkStyle", uno::Any(aLinked));
+        }
+    }
+
     if ( xPropSetInfo->hasPropertyByName( "Hidden" ) )
     {
         xPropSet->setPropertyValue( "Hidden", uno::makeAny( IsHidden( ) ) );

commit 09e3aaa2d22b8fe2887ae973b55f3ad274a960ee

xmloff: ODF export: missing extension check in XMLStyleExport
(regression from commit 05f863844d9a5613250e8d787e32752b270ec4d3)

Change-Id: Iae891ecec26599c9016be17e4db8eaf99783baa6
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/146012
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

xmloff/source/style/styleexp.cxx
@@ -244,7 +244,8 @@ bool XMLStyleExport::exportStyle(
         aAny = xPropSet->getPropertyValue("LinkStyle");
         OUString sLinkName;
         aAny >>= sLinkName;
-        if (!sLinkName.isEmpty())
+        if (!sLinkName.isEmpty()
+            && (GetExport().getSaneDefaultVersion() & SvtSaveOptions::ODFSVER_EXTENDED))
         {
             GetExport().AddAttribute(XML_NAMESPACE_LO_EXT, XML_LINKED_STYLE_NAME,
                                      GetExport().EncodeStyleName(sLinkName));