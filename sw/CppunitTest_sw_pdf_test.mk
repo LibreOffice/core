8640e24b12c7df3170c9f3e7ff3edced81fd0838
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2023-01-24 22:16:08 +0900

    tdf#66580 added hybrid PDF test cases
    Change-Id: I2a58e6cd5b40266b3c55729febb7006c7a7b4170
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/146068
    Tested-by: Jenkins
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

---
+++ sw/CppunitTest_sw_pdf_test.mk
@@ -0,0 +1,66 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#*************************************************************************
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+#*************************************************************************
+
+$(eval $(call gb_CppunitTest_CppunitTest,sw_pdf_test))
+
+$(eval $(call gb_CppunitTest_use_common_precompiled_header,sw_pdf_test))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,sw_pdf_test, \
+    sw/qa/extras/pdf/HybridPdfTest \
+))
+
+$(eval $(call gb_CppunitTest_use_libraries,sw_pdf_test, \
+    comphelper \
+    cppu \
+    cppuhelper \
+    i18nlangtag \
+    msfilter \
+    sal \
+    sfx \
+    sot \
+    subsequenttest \
+    sw \
+    swqahelper \
+    svl \
+    svt \
+    test \
+    tl \
+    unotest \
+    utl \
+    vcl \
+))
+
+$(eval $(call gb_CppunitTest_use_externals,sw_pdf_test,\
+    boost_headers \
+    libxml2 \
+))
+
+$(eval $(call gb_CppunitTest_set_include,sw_pdf_test,\
+    -I$(SRCDIR)/sw/inc \
+    -I$(SRCDIR)/sw/source/core/inc \
+    -I$(SRCDIR)/sw/source/uibase/inc \
+    -I$(SRCDIR)/sw/source/filter/inc \
+    -I$(SRCDIR)/sw/qa/inc \
+    $$(INCLUDE) \
+))
+
+$(eval $(call gb_CppunitTest_use_api,sw_pdf_test,\
+    udkapi \
+    offapi \
+    oovbaapi \
+))
+
+$(eval $(call gb_CppunitTest_use_ure,sw_pdf_test))
+$(eval $(call gb_CppunitTest_use_vcl,sw_pdf_test))
+$(eval $(call gb_CppunitTest_use_rdb,sw_pdf_test,services))
+$(eval $(call gb_CppunitTest_use_configuration,sw_pdf_test))
+
+# vim: set noet sw=4 ts=4:
---
+++ sw/qa/extras/pdf/HybridPdfTest.cxx
@@ -0,0 +1,75 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <swmodeltestbase.hxx>
+#include <docsh.hxx>
+#include <unotxdoc.hxx>
+
+class HybridPdfTest : public SwModelTestBase
+{
+public:
+    HybridPdfTest()
+        : SwModelTestBase("/sw/qa/extras/pdf/data/")
+    {
+    }
+
+    void testNoHybridDataInPDF();
+    void testHybridWithAdditionalStreams();
+    void testHybridWithAdditionalStreamsAndAttachedFile();
+
+    CPPUNIT_TEST_SUITE(HybridPdfTest);
+    CPPUNIT_TEST(testNoHybridDataInPDF);
+    CPPUNIT_TEST(testHybridWithAdditionalStreams);
+    CPPUNIT_TEST(testHybridWithAdditionalStreamsAndAttachedFile);
+    CPPUNIT_TEST_SUITE_END();
+};
+
+void HybridPdfTest::testNoHybridDataInPDF()
+{
+    // Load PDF document without attached ODT document
+    UnoApiXmlTest::load(createFileURL(u"PDFOnly.pdf"), nullptr);
+    CPPUNIT_ASSERT(mxComponent.is());
+    uno::Reference<lang::XServiceInfo> xServiceInfo(mxComponent, uno::UNO_QUERY_THROW);
+    // Draw document is expected in this case - default when importing PDF
+    CPPUNIT_ASSERT(xServiceInfo->supportsService("com.sun.star.drawing.DrawingDocument"));
+}
+
+void HybridPdfTest::testHybridWithAdditionalStreams()
+{
+    // Load PDF document with an embedded ODT document
+    // The ODT document is embedded in "/AdditionalStreams" structure that is in the PDF trailer
+    createSwDoc("Hybrid_AdditionalStreamsOnly.pdf");
+    SwDoc* pDoc = getSwDoc();
+    CPPUNIT_ASSERT(pDoc);
+
+    // We can access the document text in a single paragraph that spans multiple rows
+    // This wouldn't be possible with a PDF, so the opened document has to be ODT
+    CPPUNIT_ASSERT_EQUAL(OUString("He heard quiet steps behind him. \nThat didn't bode well."),
+                         getParagraph(1)->getString());
+}
+
+void HybridPdfTest::testHybridWithAdditionalStreamsAndAttachedFile()
+{
+    // Load PDF document with an embedded ODT document
+    // The ODT document is embedded in "/AdditionalStreams" structure that is in the PDF trailer
+    // and is included as an attached file conforming to the PDF specs
+    createSwDoc("Hybrid_AdditionalStreamsAndPDFAttachedFile.pdf");
+    SwDoc* pDoc = getSwDoc();
+    CPPUNIT_ASSERT(pDoc);
+
+    // We can access the document text in a single paragraph that spans multiple rows
+    // This wouldn't be possible with a PDF, so the opened document has to be ODT
+    CPPUNIT_ASSERT_EQUAL(OUString("He heard quiet steps behind him. \nThat didn't bode well."),
+                         getParagraph(1)->getString());
+}
+
+CPPUNIT_TEST_SUITE_REGISTRATION(HybridPdfTest);
+CPPUNIT_PLUGIN_IMPLEMENT();
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
