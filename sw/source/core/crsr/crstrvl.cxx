6188d8be864251dd6748957db196036c87014bd0
Justin Luth <justin.luth@collabora.com>
2023-01-27 19:03:35 -0500

    tdf#151548 sw content controls: actually gotoFormatContentControl
    This is a squashed commit, including Caolan's
    91fa7cc3dc124877f810599fe3c315cda6d03df6
        cid#1520798 Use after free
    
    The cursor kindof needs to change when going to a content control,
    so don't return early just because there will be no selection.
    Instead, set the cursor at the beginning of the control,
    and simply skip the selection part.
    
    Without this, it was not possible for the keyboard navigation
    to jump to rich/plain text or comboboxes that were no longer
    showing placeholder content.
    
    Change-Id: I055ece1d296c6cdf70d6bc7c1df7797bf09532c9
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/146266
    Tested-by: Jenkins
    Reviewed-by: Justin Luth <jluth@mail.com>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/146663
    Tested-by: Justin Luth <jluth@mail.com>

--- sw/source/core/crsr/crstrvl.cxx
+++ sw/source/core/crsr/crstrvl.cxx
@@ -829,12 +829,6 @@ bool SwCursorShell::GotoFootnoteAnchor(const SwTextFootnote& rTextFootnote)
 bool SwCursorShell::GotoFormatContentControl(const SwFormatContentControl& rContentControl)
 {
     std::shared_ptr<SwContentControl> pContentControl = rContentControl.GetContentControl();
-    if (!pContentControl->GetShowingPlaceHolder() && !pContentControl->GetCheckbox()
-        && !pContentControl->GetSelectedListItem() && !pContentControl->GetSelectedDate())
-    {
-        return false;
-    }
-
     const SwTextContentControl* pTextContentControl = pContentControl->GetTextAttr();
     if (!pTextContentControl)
         return false;
@@ -845,16 +839,25 @@ bool SwCursorShell::GotoFormatContentControl(const SwFormatContentControl& rCont
     SwCursor* pCursor = getShellCursor(true);
     SwCursorSaveState aSaveState(*pCursor);
 
-    pCursor->SetMark();
     SwTextNode* pTextNode = pContentControl->GetTextNode();
     // Don't select the text attribute itself at the start.
     sal_Int32 nStart = pTextContentControl->GetStart() + 1;
     pCursor->GetPoint()->Assign(*pTextNode, nStart);
-    // Don't select the CH_TXTATR_BREAKWORD itself at the end.
-    sal_Int32 nEnd = *pTextContentControl->End() - 1;
-    pCursor->GetMark()->Assign(*pTextNode, nEnd);
 
-    bool bRet = !pCursor->IsSelOvr();
+    bool bRet = true;
+    // select contents for certain controls or conditions
+    if (pContentControl->GetShowingPlaceHolder() || pContentControl->GetCheckbox()
+        || pContentControl->GetSelectedListItem() || pContentControl->GetSelectedDate())
+    {
+        pCursor->SetMark();
+        // Don't select the CH_TXTATR_BREAKWORD itself at the end.
+        sal_Int32 nEnd = *pTextContentControl->End() - 1;
+        pCursor->GetMark()->Assign(*pTextNode, nEnd);
+        bRet = !pCursor->IsSelOvr();
+    }
+    else
+        ClearMark();
+
     if (bRet)
     {
         UpdateCursor(SwCursorShell::SCROLLWIN | SwCursorShell::CHKRANGE
