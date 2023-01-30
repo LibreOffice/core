2d5e0e038f4e04ff7aa330e72d7358d5335a6f3a
Samuel Mehrbrodt <samuel.mehrbrodt@allotropia.de>
2023-01-30 14:59:06 +0100

    Add missing macro infobar to isInfobarEnabled
    Change-Id: I7b9ddc0adfed2b7c27150d2d126f83e6a5ecf19d
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/146360
    Tested-by: Jenkins
    Reviewed-by: Samuel Mehrbrodt <samuel.mehrbrodt@allotropia.de>

--- sfx2/source/dialog/infobar.cxx
+++ sfx2/source/dialog/infobar.cxx
@@ -435,6 +435,8 @@ bool SfxInfoBarContainerWindow::isInfobarEnabled(std::u16string_view sId)
         return officecfg::Office::UI::Infobar::Enabled::WhatsNew::get();
     if (sId == u"hiddentrackchanges")
         return officecfg::Office::UI::Infobar::Enabled::HiddenTrackChanges::get();
+    if (sId == u"macro")
+        return officecfg::Office::UI::Infobar::Enabled::MacrosDisabled::get();
 
     return true;
 }
