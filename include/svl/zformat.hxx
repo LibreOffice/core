82ee2d6184a59ef4470c12bddeff03cd68d946e4
Andrea Gelmini <andrea.gelmini@gelma.net>
2023-01-30 20:44:43 +0100

    Fix typos
    Change-Id: Iaed6f8d3316200f8c5e0c96766a84d59b1066928
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/146367
    Tested-by: Adolfo Jayme Barrientos <fitojb@ubuntu.com>
    Reviewed-by: Adolfo Jayme Barrientos <fitojb@ubuntu.com>

--- include/svl/zformat.hxx
+++ include/svl/zformat.hxx
@@ -459,8 +459,8 @@ public:
     void GetNatNumXml(
             css::i18n::NativeNumberXmlAttributes2& rAttr,
             sal_uInt16 nNumFor ) const;
-    /** Return empty string if no NatNum modifier or unvalid nNumFor
-        otherwhise return "[NatNum1]" or "[NatNum12 ...]" */
+    /** Return empty string if no NatNum modifier or invalid nNumFor
+        otherwise return "[NatNum1]" or "[NatNum12 ...]" */
     OUString GetNatNumModifierString( sal_uInt16 nNumFor = 0 ) const;
 
     /** Switches to the first non-"gregorian" calendar, but only if the current
