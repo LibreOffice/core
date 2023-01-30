LibreOffice/core

commit 720e4258ab5f00287098ff945a184bfb43911841

use Primitive2DDecompositionVisitor in ViewObjectContact (tdf#105575)
..to avoid container construction

Change-Id: Iae7a8ea8c31b6c8bcf4d161273be7b32fe41a021
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/120779
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svx/source/sdr/contact/objectcontactofpageview.cxx
@@ -216,11 +216,25 @@ namespace sdr::contact
                 fCurrentTime);
             updateViewInformation2D(aNewViewInformation2D);
 
-            drawinglayer::primitive2d::Primitive2DContainer xPrimitiveSequence;
+            // if there is something to show, use a primitive processor to render it. There
+            // is a choice between VCL and Canvas processors currently. The decision is made in
+            // createProcessor2DFromOutputDevice and takes into account things like the
+            // Target is a MetaFile, a VDev or something else. The Canvas renderer is triggered
+            // currently using the shown boolean. Canvas is not yet the default.
+
+            // prepare OutputDevice (historical stuff, maybe soon removed)
+            rDisplayInfo.ClearGhostedDrawMode(); // reset, else the VCL-paint with the processor will not do the right thing
+            pOutDev->SetLayoutMode(ComplexTextLayoutFlags::Default); // reset, default is no BiDi/RTL
+
+            // create renderer
+            std::unique_ptr<drawinglayer::processor2d::BaseProcessor2D> pProcessor2D(
+                drawinglayer::processor2d::createProcessor2DFromOutputDevice(
+                    rTargetOutDev, getViewInformation2D()));
+
 #if HAVE_FEATURE_DESKTOP || defined( ANDROID )
             // get whole Primitive2DContainer; this will already make use of updated ViewInformation2D
             // and may use the MapMode from the Target OutDev in the DisplayInfo
-            rDrawPageVOContact.getPrimitive2DSequenceHierarchy(rDisplayInfo, xPrimitiveSequence);
+            rDrawPageVOContact.getPrimitive2DSequenceHierarchy(rDisplayInfo, *pProcessor2D);
 #else
             // Hmm, !HAVE_FEATURE_DESKTOP && !ANDROID means iOS,
             // right? But does it makes sense to use a different code
@@ -256,27 +270,8 @@ namespace sdr::contact
             if (bGetHierarchy)
                 // get whole Primitive2DContainer; this will already make use of updated ViewInformation2D
                 // and may use the MapMode from the Target OutDev in the DisplayInfo
-                rDrawPageVOContact.getPrimitive2DSequenceHierarchy(rDisplayInfo, xPrimitiveSequence);
+                rDrawPageVOContact.getPrimitive2DSequenceHierarchy(rDisplayInfo, *pProcessor2D);
 #endif
-
-            // if there is something to show, use a primitive processor to render it. There
-            // is a choice between VCL and Canvas processors currently. The decision is made in
-            // createProcessor2DFromOutputDevice and takes into account things like the
-            // Target is a MetaFile, a VDev or something else. The Canvas renderer is triggered
-            // currently using the shown boolean. Canvas is not yet the default.
-            if(!xPrimitiveSequence.empty())
-            {
-                // prepare OutputDevice (historical stuff, maybe soon removed)
-                rDisplayInfo.ClearGhostedDrawMode(); // reset, else the VCL-paint with the processor will not do the right thing
-                pOutDev->SetLayoutMode(ComplexTextLayoutFlags::Default); // reset, default is no BiDi/RTL
-
-                // create renderer
-                std::unique_ptr<drawinglayer::processor2d::BaseProcessor2D> pProcessor2D(
-                    drawinglayer::processor2d::createProcessor2DFromOutputDevice(
-                        rTargetOutDev, getViewInformation2D()));
-
-                pProcessor2D->process(xPrimitiveSequence);
-            }
 
             // #114359# restore old ClipReghion
             if(bClipRegionPushed)

include/svx/sdr/contact/viewobjectcontact.hxx
@@ -29,6 +29,7 @@ namespace vcl { class Region; }
 namespace sdr::animation {
     class PrimitiveAnimation;
 }
+namespace drawinglayer::primitive2d { class Primitive2DDecompositionVisitor; }
 
 namespace sdr::contact {
 
@@ -120,7 +121,7 @@ public:
 
     // process this primitive: Eventually also recursively travel an existing hierarchy,
     // e.g. for group objects, scenes or pages. This method will test geometrical visibility.
-    virtual void getPrimitive2DSequenceHierarchy(DisplayInfo& rDisplayInfo, drawinglayer::primitive2d::Primitive2DContainer& rContainer) const;
+    virtual void getPrimitive2DSequenceHierarchy(DisplayInfo& rDisplayInfo, drawinglayer::primitive2d::Primitive2DDecompositionVisitor& rVisitor) const;
 
     // just process the sub-hierarchy, used as tooling from getPrimitive2DSequenceHierarchy
     drawinglayer::primitive2d::Primitive2DContainer getPrimitive2DSequenceSubHierarchy(DisplayInfo& rDisplayInfo) const;

svx/inc/sdr/contact/viewobjectcontactofe3dscene.hxx
@@ -37,7 +37,7 @@ namespace sdr::contact
 
             // process this primitive: Eventually also recursively travel an existing hierarchy,
             // e.g. for group objects, scenes or pages. This method will test geometrical visibility.
-            virtual void getPrimitive2DSequenceHierarchy(DisplayInfo& rDisplayInfo, drawinglayer::primitive2d::Primitive2DContainer& rContainer ) const override;
+            virtual void getPrimitive2DSequenceHierarchy(DisplayInfo& rDisplayInfo, drawinglayer::primitive2d::Primitive2DDecompositionVisitor& rVisitor ) const override;
         };
 } // end of namespace sdr::contact
 

svx/inc/sdr/contact/viewobjectcontactofgroup.hxx
@@ -32,7 +32,7 @@ namespace sdr::contact
             virtual ~ViewObjectContactOfGroup() override;
 
             // This method recursively paints the draw hierarchy.
-            virtual void getPrimitive2DSequenceHierarchy(DisplayInfo& rDisplayInfo, drawinglayer::primitive2d::Primitive2DContainer& rContainer ) const override;
+            virtual void getPrimitive2DSequenceHierarchy(DisplayInfo& rDisplayInfo, drawinglayer::primitive2d::Primitive2DDecompositionVisitor& rVisitor ) const override;
 
         private:
             virtual bool isPrimitiveVisibleOnAnyLayer(const SdrLayerIDSet& aLayers) const override;

svx/inc/sdr/contact/viewobjectcontactofmasterpagedescriptor.hxx
@@ -35,7 +35,7 @@ namespace sdr::contact
             virtual ~ViewObjectContactOfMasterPageDescriptor() override;
 
             virtual bool isPrimitiveVisible(const DisplayInfo& rDisplayInfo) const override;
-            virtual void getPrimitive2DSequenceHierarchy(DisplayInfo& rDisplayInfo, drawinglayer::primitive2d::Primitive2DContainer& rContainer ) const override;
+            virtual void getPrimitive2DSequenceHierarchy(DisplayInfo& rDisplayInfo, drawinglayer::primitive2d::Primitive2DDecompositionVisitor& rVisitor ) const override;
         };
 
 } // end of namespace sdr::contact

svx/inc/sdr/contact/viewobjectcontactofsdrpage.hxx
@@ -127,7 +127,7 @@ namespace sdr::contact
             ViewObjectContactOfPageHierarchy(ObjectContact& rObjectContact, ViewContact& rViewContact);
             virtual ~ViewObjectContactOfPageHierarchy() override;
 
-            virtual void getPrimitive2DSequenceHierarchy(DisplayInfo& rDisplayInfo, drawinglayer::primitive2d::Primitive2DContainer& rContainer ) const override;
+            virtual void getPrimitive2DSequenceHierarchy(DisplayInfo& rDisplayInfo, drawinglayer::primitive2d::Primitive2DDecompositionVisitor& rVisitor ) const override;
         };
 } // end of namespace sdr::contact
 
@@ -167,7 +167,7 @@ namespace sdr::contact
             ViewObjectContactOfSdrPage(ObjectContact& rObjectContact, ViewContact& rViewContact);
             virtual ~ViewObjectContactOfSdrPage() override;
 
-            virtual void getPrimitive2DSequenceHierarchy(DisplayInfo& rDisplayInfo, drawinglayer::primitive2d::Primitive2DContainer& rContainer ) const override;
+            virtual void getPrimitive2DSequenceHierarchy(DisplayInfo& rDisplayInfo, drawinglayer::primitive2d::Primitive2DDecompositionVisitor& rVisitor ) const override;
         };
 } // end of namespace sdr::contact
 

svx/source/sdr/contact/viewobjectcontact.cxx
@@ -404,7 +404,7 @@ bool ViewObjectContact::isPrimitiveGhosted(const DisplayInfo& rDisplayInfo) cons
     return (GetObjectContact().DoVisualizeEnteredGroup() && !GetObjectContact().isOutputToPrinter() && rDisplayInfo.IsGhostedDrawModeActive());
 }
 
-void ViewObjectContact::getPrimitive2DSequenceHierarchy(DisplayInfo& rDisplayInfo, drawinglayer::primitive2d::Primitive2DContainer& rContainer) const
+void ViewObjectContact::getPrimitive2DSequenceHierarchy(DisplayInfo& rDisplayInfo, drawinglayer::primitive2d::Primitive2DDecompositionVisitor& rVisitor) const
 {
     // check model-view visibility
     if(!isPrimitiveVisible(rDisplayInfo))
@@ -424,7 +424,7 @@ void ViewObjectContact::getPrimitive2DSequenceHierarchy(DisplayInfo& rDisplayInf
     if(!bVisible)
         return;
 
-    rContainer.append(xRetval);
+    rVisitor.append(xRetval);
 }
 
 drawinglayer::primitive2d::Primitive2DContainer ViewObjectContact::getPrimitive2DSequenceSubHierarchy(DisplayInfo& rDisplayInfo) const

svx/source/sdr/contact/viewobjectcontactofe3dscene.cxx
@@ -121,7 +121,7 @@ namespace sdr::contact
             return xRetval;
         }
 
-        void ViewObjectContactOfE3dScene::getPrimitive2DSequenceHierarchy(DisplayInfo& rDisplayInfo, drawinglayer::primitive2d::Primitive2DContainer& rContainer) const
+        void ViewObjectContactOfE3dScene::getPrimitive2DSequenceHierarchy(DisplayInfo& rDisplayInfo, drawinglayer::primitive2d::Primitive2DDecompositionVisitor& rVisitor) const
         {
             // To get the VOCs for the contained 3D objects created to get the correct
             // Draw hierarchy and ActionChanged() working properly, travel the DrawHierarchy
@@ -129,7 +129,7 @@ namespace sdr::contact
             impInternalSubHierarchyTraveller(*this);
 
             // call parent
-            ViewObjectContactOfSdrObj::getPrimitive2DSequenceHierarchy(rDisplayInfo, rContainer);
+            ViewObjectContactOfSdrObj::getPrimitive2DSequenceHierarchy(rDisplayInfo, rVisitor);
         }
 
 } // end of namespace

commit 4fde49387eeecc35d1ead6305b1fe1e30e09cccc

tdf#152726 Pasting Selection to Sheet as BMP Image, button test is larger
regression from

    commit 720e4258ab5f00287098ff945a184bfb43911841
    Author: Noel Grandin <noel@peralex.com>
    Date:   Fri Aug 20 11:30:35 2021 +0200
use Primitive2DDecompositionVisitor in ViewObjectContact
(tdf#105575)

Revert the relevant piece, no idea what the underlying problem is. I
suspect something is being tricksy and using settings from the current
OutputDevice even though the point of the drawinglayer
getPrimitive2DSequenceHierarchy is supposedly to be independant of that.

Change-Id: I3026ff2ba5ce3751a4e4ebdf15954aae548493c6
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/146352
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svx/source/sdr/contact/objectcontactofpageview.cxx
@@ -216,25 +216,12 @@ namespace sdr::contact
             aNewViewInformation2D.setViewTime(fCurrentTime);
             updateViewInformation2D(aNewViewInformation2D);
 
-            // if there is something to show, use a primitive processor to render it. There
-            // is a choice between VCL and Canvas processors currently. The decision is made in
-            // createProcessor2DFromOutputDevice and takes into account things like the
-            // Target is a MetaFile, a VDev or something else. The Canvas renderer is triggered
-            // currently using the shown boolean. Canvas is not yet the default.
-
-            // prepare OutputDevice (historical stuff, maybe soon removed)
-            rDisplayInfo.ClearGhostedDrawMode(); // reset, else the VCL-paint with the processor will not do the right thing
-            pOutDev->SetLayoutMode(vcl::text::ComplexTextLayoutFlags::Default); // reset, default is no BiDi/RTL
-
-            // create renderer
-            std::unique_ptr<drawinglayer::processor2d::BaseProcessor2D> pProcessor2D(
-                drawinglayer::processor2d::createProcessor2DFromOutputDevice(
-                    rTargetOutDev, getViewInformation2D()));
+            drawinglayer::primitive2d::Primitive2DContainer xPrimitiveSequence;
 
 #if HAVE_FEATURE_DESKTOP || defined( ANDROID )
             // get whole Primitive2DContainer; this will already make use of updated ViewInformation2D
             // and may use the MapMode from the Target OutDev in the DisplayInfo
-            rDrawPageVOContact.getPrimitive2DSequenceHierarchy(rDisplayInfo, *pProcessor2D);
+            rDrawPageVOContact.getPrimitive2DSequenceHierarchy(rDisplayInfo, xPrimitiveSequence);
 #else
             // Hmm, !HAVE_FEATURE_DESKTOP && !ANDROID means iOS,
             // right? But does it makes sense to use a different code
@@ -270,8 +257,25 @@ namespace sdr::contact
             if (bGetHierarchy)
                 // get whole Primitive2DContainer; this will already make use of updated ViewInformation2D
                 // and may use the MapMode from the Target OutDev in the DisplayInfo
-                rDrawPageVOContact.getPrimitive2DSequenceHierarchy(rDisplayInfo, *pProcessor2D);
+                rDrawPageVOContact.getPrimitive2DSequenceHierarchy(rDisplayInfo, xPrimitiveSequence);
 #endif
+
+            // if there is something to show, use a primitive processor to render it. There
+            // is a choice between VCL and Canvas processors currently. The decision is made in
+            // createProcessor2DFromOutputDevice and takes into account things like the
+            // Target is a MetaFile, a VDev or something else. The Canvas renderer is triggered
+            // currently using the shown boolean. Canvas is not yet the default.
+            if(!xPrimitiveSequence.empty())
+            {
+                // prepare OutputDevice (historical stuff, maybe soon removed)
+                rDisplayInfo.ClearGhostedDrawMode(); // reset, else the VCL-paint with the processor will not do the right thing
+                pOutDev->SetLayoutMode(vcl::text::ComplexTextLayoutFlags::Default); // reset, default is no BiDi/RTL
+                // create renderer
+                std::unique_ptr<drawinglayer::processor2d::BaseProcessor2D> pProcessor2D(
+                    drawinglayer::processor2d::createProcessor2DFromOutputDevice(
+                        rTargetOutDev, getViewInformation2D()));
+                pProcessor2D->process(xPrimitiveSequence);
+            }
 
             // #114359# restore old ClipReghion
             if(bClipRegionPushed)