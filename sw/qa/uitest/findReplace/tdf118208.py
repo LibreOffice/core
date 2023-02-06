621d4b0afc34646cec59b53e192be47eefe6062d
Xisco Fauli <xiscofauli@libreoffice.org>
2023-02-06 17:42:29 +0100

    sw: use try/finally to reset the changes in OptionsTreeDialog
    Otherwise it might affect other tests if it hits an assert
    
    Change-Id: Ic8e35f6f9f029ea6ae4620946edb69ca7a6618bf
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/146591
    Tested-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- sw/qa/uitest/findReplace/tdf118208.py
+++ sw/qa/uitest/findReplace/tdf118208.py
@@ -15,52 +15,41 @@ from uitest.uihelper.common import get_state_as_dict, get_url_for_data_file
 
 class tdf118208(UITestCase):
 
+    def change_default_languages(self, enabled):
+        with self.ui_test.execute_dialog_through_command(".uno:OptionsTreeDialog") as xDialog:
+
+            xPages = xDialog.getChild("pages")
+            xLanguageEntry = xPages.getChild('2')                 # Language Settings
+            xLanguageEntry.executeAction("EXPAND", tuple())
+            xxLanguageEntryGeneralEntry = xLanguageEntry.getChild('0')
+            xxLanguageEntryGeneralEntry.executeAction("SELECT", tuple())          #Language
+
+            asianlanguage = xDialog.getChild("asiansupport")
+            complexlanguage = xDialog.getChild("ctlsupport")
+            if (get_state_as_dict(asianlanguage)["Selected"]) != enabled:
+                asianlanguage.executeAction("CLICK", tuple())
+            if (get_state_as_dict(complexlanguage)["Selected"]) != enabled:
+                complexlanguage.executeAction("CLICK", tuple())
+
     def test_tdf118208_search_dialog_format_crash(self):
         with self.ui_test.load_file(get_url_for_data_file("tdf118208.odt")) as writer_doc:
             # 1. Open the attached file.
             # 2. Press ctrl-H to show the search and replace dialog.
             # 3. Press the "Format..."  button.
             #Libreoffice immediately crashed.
-            with self.ui_test.execute_dialog_through_command(".uno:OptionsTreeDialog") as xDialog:
 
-                xPages = xDialog.getChild("pages")
-                xLanguageEntry = xPages.getChild('2')                 # Language Settings
-                xLanguageEntry.executeAction("EXPAND", tuple())
-                xxLanguageEntryGeneralEntry = xLanguageEntry.getChild('0')
-                xxLanguageEntryGeneralEntry.executeAction("SELECT", tuple())          #Language
+            try:
+                self.change_default_languages("false")
 
-                asianlanguage = xDialog.getChild("asiansupport")
-                complexlanguage = xDialog.getChild("ctlsupport")
-                if (get_state_as_dict(asianlanguage)["Selected"]) == "true":
-                    asianlanguage.executeAction("CLICK", tuple())
-                if (get_state_as_dict(complexlanguage)["Selected"]) == "true":
-                    complexlanguage.executeAction("CLICK", tuple())
+                with self.ui_test.execute_modeless_dialog_through_command(".uno:SearchDialog", close_button="close") as xDialog:
 
+                    format = xDialog.getChild("format")
 
-            with self.ui_test.execute_modeless_dialog_through_command(".uno:SearchDialog", close_button="close") as xDialog:
+                    with self.ui_test.execute_blocking_action(format.executeAction, args=('CLICK', ())):
+                        pass
 
-                format = xDialog.getChild("format")
+                    #verify
+                    self.assertEqual(writer_doc.Text.String[0:14], "Aaaaaaaaaaaaaa")
+            finally:
+                self.change_default_languages("true")
 
-                with self.ui_test.execute_blocking_action(format.executeAction, args=('CLICK', ())):
-                    pass
-
-                #verify
-                self.assertEqual(writer_doc.Text.String[0:14], "Aaaaaaaaaaaaaa")
-
-            #enable lang support again
-            with self.ui_test.execute_dialog_through_command(".uno:OptionsTreeDialog") as xDialog:
-
-                xPages = xDialog.getChild("pages")
-                xLanguageEntry = xPages.getChild('2')                 # Language Settings
-                xLanguageEntry.executeAction("EXPAND", tuple())
-                xxLanguageEntryGeneralEntry = xLanguageEntry.getChild('0')
-                xxLanguageEntryGeneralEntry.executeAction("SELECT", tuple())          #Language
-
-                asianlanguage = xDialog.getChild("asiansupport")
-                complexlanguage = xDialog.getChild("ctlsupport")
-                if (get_state_as_dict(asianlanguage)["Selected"]) == "false":
-                    asianlanguage.executeAction("CLICK", tuple())
-                if (get_state_as_dict(complexlanguage)["Selected"]) == "false":
-                    complexlanguage.executeAction("CLICK", tuple())
-
-# vim: set shiftwidth=4 softtabstop=4 expandtab:
