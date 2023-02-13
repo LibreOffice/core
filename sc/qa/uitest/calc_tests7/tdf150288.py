835f87280ab228f59801c28c288472f2941ea222
Xisco Fauli <xiscofauli@libreoffice.org>
2023-02-13 11:42:56 +0100

    sc: use try/finally to reset the changes in OptionsTreeDialog
    Otherwise it might affect other tests if it hits an assert
    
    Change-Id: Iee86221cc7b4bd66fc19dc63b162d72aacec5016
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/146874
    Tested-by: Jenkins
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- sc/qa/uitest/calc_tests7/tdf150288.py
+++ sc/qa/uitest/calc_tests7/tdf150288.py
@@ -16,38 +16,43 @@ from uitest.uihelper.common import select_by_text
 
 class tdf150288(UITestCase):
 
-    def test_tdf150288(self):
+    def change_date_pattern(self, pattern):
+        with self.ui_test.execute_dialog_through_command(".uno:OptionsTreeDialog") as xDialog:
+            xPages = xDialog.getChild("pages")
+            xLanguageEntry = xPages.getChild('2')
+            xLanguageEntry.executeAction("EXPAND", tuple())
+            xxLanguageEntryGeneralEntry = xLanguageEntry.getChild('0')
+            xxLanguageEntryGeneralEntry.executeAction("SELECT", tuple())
 
+            xDatePatterns = xDialog.getChild("datepatterns")
+            xLocaleSetting = xDialog.getChild("localesetting")
+
+            select_by_text(xLocaleSetting, "English (USA)")
+
+            xDatePatterns.executeAction("TYPE", mkPropertyValues({"KEYCODE":"CTRL+A"}))
+            xDatePatterns.executeAction("TYPE", mkPropertyValues({"KEYCODE":"BACKSPACE"}))
+            xDatePatterns.executeAction("TYPE", mkPropertyValues({"TEXT": pattern}))
+
+    def test_tdf150288(self):
         with self.ui_test.create_doc_in_start_center("calc") as document:
 
-            with self.ui_test.execute_dialog_through_command(".uno:OptionsTreeDialog") as xDialog:
-                xPages = xDialog.getChild("pages")
-                xLanguageEntry = xPages.getChild('2')
-                xLanguageEntry.executeAction("EXPAND", tuple())
-                xxLanguageEntryGeneralEntry = xLanguageEntry.getChild('0')
-                xxLanguageEntryGeneralEntry.executeAction("SELECT", tuple())
+            try:
+                self.change_date_pattern("D/M/Y;D/M")
 
-                xDatePatterns = xDialog.getChild("datepatterns")
-                xLocaleSetting = xDialog.getChild("localesetting")
+                xCalcDoc = self.xUITest.getTopFocusWindow()
+                xGridWin = xCalcDoc.getChild("grid_window")
 
-                select_by_text(xLocaleSetting, "English (USA)")
+                enter_text_to_cell(xGridWin, "A1", "31/8/22")
+                enter_text_to_cell(xGridWin, "A2", "1/8/22")
 
-                self.assertEqual("M/D/Y;M/D", get_state_as_dict(xDatePatterns)['Text'])
+                self.assertEqual("08/31/22", get_cell_by_position(document, 0, 0, 0).getString())
 
-                xDatePatterns.executeAction("TYPE", mkPropertyValues({"KEYCODE":"CTRL+A"}))
-                xDatePatterns.executeAction("TYPE", mkPropertyValues({"KEYCODE":"BACKSPACE"}))
-                xDatePatterns.executeAction("TYPE", mkPropertyValues({"TEXT":"D/M/Y;D/M"}))
+                # Without the fix in place, this test would have failed with
+                # AssertionError: '08/01/22' != '01/08/22'
+                self.assertEqual("08/01/22", get_cell_by_position(document, 0, 0, 1).getString())
 
-            xCalcDoc = self.xUITest.getTopFocusWindow()
-            xGridWin = xCalcDoc.getChild("grid_window")
-
-            enter_text_to_cell(xGridWin, "A1", "31/8/22")
-            enter_text_to_cell(xGridWin, "A2", "1/8/22")
-
-            self.assertEqual("08/31/22", get_cell_by_position(document, 0, 0, 0).getString())
-
-            # Without the fix in place, this test would have failed with
-            # AssertionError: '08/01/22' != '01/08/22'
-            self.assertEqual("08/01/22", get_cell_by_position(document, 0, 0, 1).getString())
+            finally:
+                # reset date pattern to default
+                self.change_date_pattern("M/D/Y;M/D")
 
 # vim: set shiftwidth=4 softtabstop=4 expandtab:
