LibreOffice/core

commit e496b55a592caf0ca07f5477285a72d50cee6501

lok: masterpage: don't render text edits from different mode
In Impress we can have 2 different EditMode : Page and MasterPage.
Don't render text edits from other views in Page mode on Master Page.

Change-Id: I62341941215165281d2663af3d3b7cca034762a9
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/138803
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Reviewed-by: Henry Castro <hcastro@collabora.com>

svx/source/svdraw/svdpntv.cxx
@@ -639,28 +639,31 @@ void SdrPaintView::EndCompleteRedraw(SdrPaintWindow& rPaintWindow, bool bPaintFo
         // look for active TextEdit. As long as this cannot be painted to a VDev,
         // it cannot get part of buffering. In that case, output evtl. prerender
         // early and paint text edit to window.
-        if(IsTextEdit() && GetSdrPageView())
+        SdrPageView* pPageView = GetSdrPageView();
+        if(IsTextEdit() && pPageView)
         {
             if (!comphelper::LibreOfficeKit::isActive() || mbPaintTextEdit)
                 static_cast< SdrView* >(this)->TextEditDrawing(rPaintWindow);
         }
 
-        if (comphelper::LibreOfficeKit::isActive())
+        if (comphelper::LibreOfficeKit::isActive() && mbPaintTextEdit && pPageView)
         {
             // Look for active text edits in other views showing the same page,
-            // and show them as well.
-            if (SdrPageView* pPageView = GetSdrPageView())
+            // and show them as well. Show only if Page/MasterPage mode is matching.
+            SdrViewIter aIter(pPageView->GetPage());
+            bool bRequireMasterPage = pPageView->GetPage() ? pPageView->GetPage()->IsMasterPage() : false;
+            for (SdrView* pView = aIter.FirstView(); pView; pView = aIter.NextView())
             {
-                SdrViewIter aIter(pPageView->GetPage());
-                for (SdrView* pView = aIter.FirstView(); pView; pView = aIter.NextView())
-                {
-                    if (pView == this)
-                        continue;
+                SdrPageView* pCurrentPageView = pView->GetSdrPageView();
+                bool bIsCurrentMasterPage = (pCurrentPageView && pCurrentPageView->GetPage()) ?
+                    pCurrentPageView->GetPage()->IsMasterPage() : false;
 
-                    if (pView->IsTextEdit() && pView->GetSdrPageView())
-                    {
-                        pView->TextEditDrawing(rPaintWindow);
-                    }
+                if (pView == this || bRequireMasterPage != bIsCurrentMasterPage)
+                    continue;
+
+                if (pView->IsTextEdit() && pView->GetSdrPageView())
+                {
+                    pView->TextEditDrawing(rPaintWindow);
                 }
             }
         }

commit b0e611260312ad8684ce76139459bffa45b07a67

lok: draw textboxes under edit
This fixes bug in impress:
- open in 2 views
- start editing textbox in 2. view on 2. slide
- start editing textbox in 1. view on 1. slide
Result: slide 2 textbox was dissapearing

We need to allow drawing textboxes edited by other views
so if we select non-editor for rendering we will see the
full content.

It was regression from
commit e496b55a592caf0ca07f5477285a72d50cee6501
lok: masterpage: don't render text edits from different mode

Change-Id: I7645496aec77d11d16621129dd6644d3b54a6010
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/146999
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Reviewed-by: Henry Castro <hcastro@collabora.com>

svx/source/svdraw/svdpntv.cxx
@@ -647,7 +647,7 @@ void SdrPaintView::EndCompleteRedraw(SdrPaintWindow& rPaintWindow, bool bPaintFo
                 static_cast< SdrView* >(this)->TextEditDrawing(rPaintWindow);
         }
 
-        if (comphelper::LibreOfficeKit::isActive() && mbPaintTextEdit && pPageView)
+        if (comphelper::LibreOfficeKit::isActive() && pPageView)
         {
             // Look for active text edits in other views showing the same page,
             // and show them as well. Show only if Page/MasterPage mode is matching.