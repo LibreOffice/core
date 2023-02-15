b4c8775526d701cfe0230ca34bfbbb9be00a71e7
Stephan Bergmann <sbergman@redhat.com>
2023-02-15 08:37:09 +0100

    tdf#153519 Another TreeListEntryUIObject heap-use-after-free
    ...similar to the one addressed with the commits leading up to
    9a23ded27470a4c57015e9e5d686259a60d464f2 "Fix thinko when to sleep"
    
    Change-Id: I13d974600275554fb3c66f836a07b96b5d6a2639
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/147039
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

--- sw/qa/uitest/navigator/tdf151051.py
+++ sw/qa/uitest/navigator/tdf151051.py
@@ -10,6 +10,7 @@
 from uitest.framework import UITestCase
 from libreoffice.uno.propertyvalue import mkPropertyValues
 from uitest.uihelper.common import get_state_as_dict, get_url_for_data_file
+import time
 
 class tdf151051(UITestCase):
 
@@ -24,6 +25,15 @@ class tdf151051(UITestCase):
 
             # wait until the navigator panel is available
             xNavigatorPanel = self.ui_test.wait_until_child_is_available('NavigatorPanel')
+
+            # HACK, see the `m_aUpdTimer.SetTimeout(1000)` in the SwContentTree ctor in
+            # sw/source/uibase/utlui/content.cxx, where that m_aUpdTimer is started by
+            # SwContentTree::ShowTree triggered from the SIDEBAR action above, and which can
+            # invalidate the TreeListEntryUIObjects used by the below code (see
+            # 2798430c8a711861fdcdfbf9ac00a0527abd3bfc "Mark the uses of TreeListEntryUIObject as
+            # dubious"); lets double that 1000 ms timeout value here to hopefully be on the safe
+            # side:
+            time.sleep(2)
 
             xContentTree = xNavigatorPanel.getChild("contenttree")
 
