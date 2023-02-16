638557b2f8baf87ab40d57beaa444038ace94d8a
Andras Timar <andras.timar@collabora.com>
2023-02-16 16:40:33 +0100

    build fix
    Change-Id: I669a835015528ea01b49509ffb79c4600091cdda

--- connectivity/source/drivers/hsqldb/HDriver.cxx
+++ connectivity/source/drivers/hsqldb/HDriver.cxx
@@ -251,15 +251,15 @@ namespace connectivity
                             std::unique_ptr<SvStream> pStream( ::utl::UcbStreamHelper::CreateStream(xStream) );
                             if (pStream)
                             {
-                                OString sLine;
+                                OStringBuffer sLine;
                                 OString sVersionString;
                                 while ( pStream->ReadLine(sLine) )
                                 {
                                     if ( sLine.isEmpty() )
                                         continue;
                                     sal_Int32 nIdx {0};
-                                    const OString sIniKey = sLine.getToken(0, '=', nIdx);
-                                    const OString sValue = sLine.getToken(0, '=', nIdx);
+                                    const std::string_view sIniKey = o3tl::getToken(sLine, 0, '=', nIdx);
+                                    const OString sValue(o3tl::getToken(sLine, 0, '=', nIdx));
                                     if( sIniKey == "hsqldb.compatible_version" )
                                     {
                                         sVersionString = sValue;
@@ -275,9 +275,9 @@ namespace connectivity
                                 if (!sVersionString.isEmpty())
                                 {
                                     sal_Int32 nIdx {0};
-                                    const sal_Int32 nMajor = sVersionString.getToken(0, '.', nIdx).toInt32();
-                                    const sal_Int32 nMinor = sVersionString.getToken(0, '.', nIdx).toInt32();
-                                    const sal_Int32 nMicro = sVersionString.getToken(0, '.', nIdx).toInt32();
+                                    const sal_Int32 nMajor = o3tl::toInt32(o3tl::getToken(sVersionString, 0, '.', nIdx));
+                                    const sal_Int32 nMinor = o3tl::toInt32(o3tl::getToken(sVersionString, 0, '.', nIdx));
+                                    const sal_Int32 nMicro = o3tl::toInt32(o3tl::getToken(sVersionString, 0, '.', nIdx));
                                     if (     nMajor > 1
                                         || ( nMajor == 1 && nMinor > 8 )
                                         || ( nMajor == 1 && nMinor == 8 && nMicro > 0 ) )
