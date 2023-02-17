b60fc720d789602c6b5420365f4dcaa6cb17bf40
Michael Weghorn <m.weghorn@posteo.de>
2023-02-17 08:55:33 +0100

    android: Drop unused LayerView#isIMEEEnabled
    ... that always returns false anyway.
    
    (And the `InputConnectionHandler#isIMEEnabled`
    that would be called in the commented out code
    doesn't exist either.)
    
    Change-Id: I6031672ee2835c64a1efcab946574ca2ce94b6a1
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/147195
    Tested-by: Jenkins
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>

--- android/source/src/java/org/mozilla/gecko/gfx/LayerView.java
+++ android/source/src/java/org/mozilla/gecko/gfx/LayerView.java
@@ -219,13 +219,6 @@ public class LayerView extends FrameLayout {
         return mInputConnectionHandler != null && mInputConnectionHandler.onKeyUp(keyCode, event);
     }
 
-    public boolean isIMEEnabled() {
-        /*if (mInputConnectionHandler != null) {
-            return mInputConnectionHandler.isIMEEnabled();
-        }*/
-        return false;
-    }
-
     public void requestRender() {
         if (mListener != null) {
             mListener.renderRequested();
