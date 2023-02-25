0dec4da8a3441353afa65de81f30d316e6500606
Caolán McNamara <caolanm@redhat.com>
2023-02-25 21:54:46 +0000

    RES_LR_SPACE should fall through to default not RES_BACKGROUND
    due to
    
    commit 29dfcc7521311e547fc069466cc3edc9fcbdbe03
    Date:   Mon Nov 23 16:17:37 2015 +0100
    
        tdf#94088 add import of HTML inline graphics
    
    splitting the condition that used to fall through to default handling
    with an unrelated RES_BACKGROUND case instead
    
    Change-Id: I26a077c755f214cf35582ec146fcf34f87cc4494
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/147690
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

--- sw/source/filter/html/swhtml.cxx
+++ sw/source/filter/html/swhtml.cxx
@@ -2953,18 +2953,6 @@ void SwHTMLParser::SetAttr_( bool bChkEnd, bool bBeforeTable,
                     pAttr = pPrev;
                     continue;
 
-                case RES_LR_SPACE:
-                    if( aAttrPam.GetPoint()->nNode.GetIndex() ==
-                        aAttrPam.GetMark()->nNode.GetIndex())
-                    {
-                        // because of numbering set this attribute directly at node
-                        pCNd->SetAttr( *pAttr->m_pItem );
-                        break;
-                    }
-                    OSL_ENSURE( false,
-                            "LRSpace set over multiple paragraphs!" );
-                    [[fallthrough]]; // (shouldn't reach this point anyway)
-
                 // tdf#94088 expand RES_BACKGROUND to the new fill attribute
                 // definitions in the range [XATTR_FILL_FIRST .. XATTR_FILL_LAST].
                 // This is the right place in the future if the adapted fill attributes
@@ -2978,6 +2966,18 @@ void SwHTMLParser::SetAttr_( bool bChkEnd, bool bBeforeTable,
                     m_xDoc->getIDocumentContentOperations().InsertItemSet(aAttrPam, aNewSet, SetAttrMode::DONTREPLACE);
                     break;
                 }
+
+                case RES_LR_SPACE:
+                    if( aAttrPam.GetPoint()->nNode.GetIndex() ==
+                        aAttrPam.GetMark()->nNode.GetIndex())
+                    {
+                        // because of numbering set this attribute directly at node
+                        pCNd->SetAttr( *pAttr->m_pItem );
+                        break;
+                    }
+                    OSL_ENSURE( false,
+                            "LRSpace set over multiple paragraphs!" );
+                    [[fallthrough]]; // (shouldn't reach this point anyway)
                 default:
 
                     // maybe jump to a bookmark
