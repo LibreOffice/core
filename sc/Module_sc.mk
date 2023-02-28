36cd611e6ff709b3e3645b67ea67ee26cf3d92fc
Xisco Fauli <xiscofauli@libreoffice.org>
2023-02-28 16:54:04 +0100

    CppunitTest_sc_subsequent_export_test: split in two
    it already has 101 tests
    
    Change-Id: I2fb88e41313049fec649e3592d811a36e10440fc
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/148013
    Tested-by: Jenkins
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- sc/Module_sc.mk
+++ sc/Module_sc.mk
@@ -87,6 +87,7 @@ $(eval $(call gb_Module_add_slowcheck_targets,sc, \
 	CppunitTest_sc_subsequent_filters_test3 \
 	CppunitTest_sc_subsequent_export_test \
 	CppunitTest_sc_subsequent_export_test2 \
+	CppunitTest_sc_subsequent_export_test3 \
 	CppunitTest_sc_uicalc \
 	CppunitTest_sc_vba_macro_test \
 	CppunitTest_sc_a11y \
