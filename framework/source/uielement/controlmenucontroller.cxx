LibreOffice/core

commit 5032262fccd0ef7f274323ae481b3230ffd42d3a

BaseMutex->std::mutex in PopupMenuControllerBase
Change-Id: I26877ac7b6e5e39f511418d978199189c62cca6c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/147416
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

framework/source/uielement/controlmenucontroller.cxx
@@ -145,9 +145,6 @@ public:
     // XPopupMenuController
     virtual void SAL_CALL updatePopupMenu() override;
 
-    // XInitialization
-    virtual void SAL_CALL initialize( const uno::Sequence< uno::Any >& aArguments ) override;
-
     // XStatusListener
     virtual void SAL_CALL statusChanged( const frame::FeatureStateEvent& Event ) override;
 
@@ -158,6 +155,9 @@ public:
     virtual void SAL_CALL disposing( const lang::EventObject& Source ) override;
 
 private:
+    // XInitialization
+    virtual void initializeImpl( std::unique_lock<std::mutex>& rGuard, const uno::Sequence< uno::Any >& aArguments ) override;
+
     class UrlToDispatchMap : public std::unordered_map< OUString,
                                                         uno::Reference< frame::XDispatch > >
     {
@@ -228,7 +228,7 @@ void SAL_CALL ControlMenuController::disposing( const EventObject& )
 {
     Reference< css::awt::XMenuListener > xHolder(this);
 
-    osl::MutexGuard aLock( m_aMutex );
+    std::unique_lock aLock( m_aMutex );
     m_xFrame.clear();
     m_xDispatch.clear();
 
@@ -240,7 +240,7 @@ void SAL_CALL ControlMenuController::disposing( const EventObject& )
 // XStatusListener
 void SAL_CALL ControlMenuController::statusChanged( const FeatureStateEvent& Event )
 {
-    osl::MutexGuard aLock( m_aMutex );
+    std::unique_lock aLock( m_aMutex );
 
     if (!m_xPopupMenu)
         return;
@@ -264,7 +264,7 @@ void SAL_CALL ControlMenuController::statusChanged( const FeatureStateEvent& Eve
 // XMenuListener
 void SAL_CALL ControlMenuController::itemActivated( const css::awt::MenuEvent& )
 {
-    osl::MutexGuard aLock( m_aMutex );
+    std::unique_lock aLock( m_aMutex );
 
     SolarMutexGuard aSolarMutexGuard;
 
@@ -282,9 +282,9 @@ void SAL_CALL ControlMenuController::itemActivated( const css::awt::MenuEvent& )
 // XPopupMenuController
 void SAL_CALL ControlMenuController::updatePopupMenu()
 {
-    osl::MutexGuard aLock( m_aMutex );
+    std::unique_lock aLock( m_aMutex );
 
-    throwIfDisposed();
+    throwIfDisposed(aLock);
 
     if ( !(m_xFrame.is() && m_xPopupMenu.is()) )
         return;
@@ -310,10 +310,9 @@ void SAL_CALL ControlMenuController::updatePopupMenu()
 }
 
 // XInitialization
-void SAL_CALL ControlMenuController::initialize( const Sequence< Any >& aArguments )
+void ControlMenuController::initializeImpl( std::unique_lock<std::mutex>& rGuard, const Sequence< Any >& aArguments )
 {
-    osl::MutexGuard aLock( m_aMutex );
-    svt::PopupMenuControllerBase::initialize(aArguments);
+    svt::PopupMenuControllerBase::initializeImpl(rGuard, aArguments);
     m_aBaseURL.clear();
 }
 

framework/inc/uielement/langselectionmenucontroller.hxx
@@ -44,9 +44,6 @@ namespace framework
             // XPopupMenuController
             virtual void SAL_CALL updatePopupMenu() override;
 
-            // XInitialization
-            virtual void SAL_CALL initialize( const css::uno::Sequence< css::uno::Any >& aArguments ) override;
-
             // XStatusListener
             virtual void SAL_CALL statusChanged( const css::frame::FeatureStateEvent& Event ) override;
 
@@ -54,6 +51,9 @@ namespace framework
             virtual void SAL_CALL disposing( const css::lang::EventObject& Source ) override;
 
         private:
+            // XInitialization
+            virtual void initializeImpl( std::unique_lock<std::mutex>& rGuard, const css::uno::Sequence< css::uno::Any >& aArguments ) override;
+
             virtual void impl_setPopupMenu() override;
             enum Mode
             {

framework/inc/uielement/newmenucontroller.hxx
@@ -40,9 +40,6 @@ namespace framework
             virtual sal_Bool SAL_CALL supportsService( const OUString& sServiceName ) override;
             virtual css::uno::Sequence< OUString > SAL_CALL getSupportedServiceNames() override;
 
-            // XInitialization
-            virtual void SAL_CALL initialize( const css::uno::Sequence< css::uno::Any >& aArguments ) override;
-
             // XStatusListener
             virtual void SAL_CALL statusChanged( const css::frame::FeatureStateEvent& Event ) override;
 
@@ -54,6 +51,9 @@ namespace framework
             virtual void SAL_CALL disposing( const css::lang::EventObject& Source ) override;
 
         private:
+            // XInitialization
+            virtual void initializeImpl( std::unique_lock<std::mutex>& rGuard, const css::uno::Sequence< css::uno::Any >& aArguments ) override;
+
             virtual void impl_setPopupMenu() override;
 
             void fillPopupMenu( css::uno::Reference< css::awt::XPopupMenu > const & rPopupMenu );

framework/inc/uielement/toolbarsmenucontroller.hxx
@@ -48,9 +48,6 @@ namespace framework
             // XPopupMenuController
             virtual void SAL_CALL setPopupMenu( const css::uno::Reference< css::awt::XPopupMenu >& PopupMenu ) override;
 
-            // XInitialization
-            virtual void SAL_CALL initialize( const css::uno::Sequence< css::uno::Any >& aArguments ) override;
-
             // XStatusListener
             virtual void SAL_CALL statusChanged( const css::frame::FeatureStateEvent& Event ) override;
 
@@ -71,6 +68,9 @@ namespace framework
             DECL_STATIC_LINK( ToolbarsMenuController, ExecuteHdl_Impl, void*, void );
 
         private:
+            // XInitialization
+            virtual void initializeImpl( std::unique_lock<std::mutex>& rGuard, const css::uno::Sequence< css::uno::Any >& aArguments ) override;
+
             void fillPopupMenu( css::uno::Reference< css::awt::XPopupMenu > const & rPopupMenu );
             css::uno::Sequence< css::uno::Sequence< css::beans::PropertyValue > > getLayoutManagerToolbars( const css::uno::Reference< css::frame::XLayoutManager >& rLayoutManager );
             css::uno::Reference< css::frame::XDispatch > getDispatchFromCommandURL( const OUString& rCommandURL );

framework/source/uielement/fontmenucontroller.cxx
@@ -111,7 +111,7 @@ void SAL_CALL FontMenuController::disposing( const EventObject& )
 {
     Reference< css::awt::XMenuListener > xHolder(this);
 
-    osl::MutexGuard aLock( m_aMutex );
+    std::unique_lock aLock( m_aMutex );
     m_xFrame.clear();
     m_xDispatch.clear();
     m_xFontListDispatch.clear();
@@ -129,12 +129,12 @@ void SAL_CALL FontMenuController::statusChanged( const FeatureStateEvent& Event 
 
     if ( Event.State >>= aFontDescriptor )
     {
-        osl::MutexGuard aLock( m_aMutex );
+        std::unique_lock aLock( m_aMutex );
         m_aFontFamilyName = aFontDescriptor.Name;
     }
     else if ( Event.State >>= aFontNameSeq )
     {
-        osl::MutexGuard aLock( m_aMutex );
+        std::unique_lock aLock( m_aMutex );
         if ( m_xPopupMenu.is() )
             fillPopupMenu( aFontNameSeq, m_xPopupMenu );
     }
@@ -143,7 +143,7 @@ void SAL_CALL FontMenuController::statusChanged( const FeatureStateEvent& Event 
 // XMenuListener
 void SAL_CALL FontMenuController::itemActivated( const css::awt::MenuEvent& )
 {
-    osl::MutexGuard aLock( m_aMutex );
+    std::unique_lock aLock( m_aMutex );
 
     if ( !m_xPopupMenu.is() )
         return;
@@ -193,12 +193,12 @@ void SAL_CALL FontMenuController::updatePopupMenu()
 {
     svt::PopupMenuControllerBase::updatePopupMenu();
 
-    osl::ClearableMutexGuard aLock( m_aMutex );
+    std::unique_lock aLock( m_aMutex );
     Reference< XDispatch > xDispatch( m_xFontListDispatch );
     css::util::URL aTargetURL;
     aTargetURL.Complete = ".uno:FontNameList";
     m_xURLTransformer->parseStrict( aTargetURL );
-    aLock.clear();
+    aLock.unlock();
 
     if ( xDispatch.is() )
     {

framework/source/uielement/fontsizemenucontroller.cxx
@@ -208,7 +208,7 @@ void SAL_CALL FontSizeMenuController::disposing( const EventObject& )
 {
     Reference< css::awt::XMenuListener > xHolder(this);
 
-    osl::MutexGuard aLock( m_aMutex );
+    std::unique_lock aLock( m_aMutex );
     m_xFrame.clear();
     m_xDispatch.clear();
     m_xCurrentFontDispatch.clear();
@@ -225,14 +225,14 @@ void SAL_CALL FontSizeMenuController::statusChanged( const FeatureStateEvent& Ev
 
     if ( Event.State >>= aFontDescriptor )
     {
-        osl::MutexGuard aLock( m_aMutex );
+        std::unique_lock aLock( m_aMutex );
 
         if ( m_xPopupMenu.is() )
             fillPopupMenu( m_xPopupMenu );
     }
     else if ( Event.State >>= aFontHeight )
     {
-        osl::MutexGuard aLock( m_aMutex );
+        std::unique_lock aLock( m_aMutex );
         m_aFontHeight = aFontHeight;
 
         if ( m_xPopupMenu.is() )
@@ -256,15 +256,15 @@ void FontSizeMenuController::impl_setPopupMenu()
 
 void SAL_CALL FontSizeMenuController::updatePopupMenu()
 {
-    osl::ClearableMutexGuard aLock( m_aMutex );
+    std::unique_lock aLock( m_aMutex );
 
-    throwIfDisposed();
+    throwIfDisposed(aLock);
 
     Reference< XDispatch > xDispatch( m_xCurrentFontDispatch );
     css::util::URL aTargetURL;
     aTargetURL.Complete = ".uno:CharFontName";
     m_xURLTransformer->parseStrict( aTargetURL );
-    aLock.clear();
+    aLock.unlock();
 
     if ( xDispatch.is() )
     {

framework/source/uielement/headermenucontroller.cxx
@@ -185,7 +185,7 @@ void SAL_CALL HeaderMenuController::disposing( const EventObject& )
 {
     Reference< css::awt::XMenuListener > xHolder(this);
 
-    osl::MutexGuard aLock( m_aMutex );
+    std::unique_lock aLock( m_aMutex );
     m_xFrame.clear();
     m_xDispatch.clear();
 
@@ -201,7 +201,7 @@ void SAL_CALL HeaderMenuController::statusChanged( const FeatureStateEvent& Even
 
     if ( Event.State >>= xModel )
     {
-        osl::MutexGuard aLock( m_aMutex );
+        std::unique_lock aLock( m_aMutex );
         m_xModel = xModel;
         if ( m_xPopupMenu.is() )
             fillPopupMenu( xModel, m_xPopupMenu );
@@ -211,17 +211,17 @@ void SAL_CALL HeaderMenuController::statusChanged( const FeatureStateEvent& Even
 // XMenuListener
 void SAL_CALL HeaderMenuController::updatePopupMenu()
 {
-    osl::ResettableMutexGuard aLock( m_aMutex );
+    std::unique_lock aLock( m_aMutex );
 
-    throwIfDisposed();
+    throwIfDisposed(aLock);
 
     Reference< css::frame::XModel > xModel( m_xModel );
-    aLock.clear();
+    aLock.unlock();
 
     if ( !xModel.is() )
         svt::PopupMenuControllerBase::updatePopupMenu();
 
-    aLock.reset();
+    aLock.lock();
     if ( m_xPopupMenu.is() && m_xModel.is() )
         fillPopupMenu( m_xModel, m_xPopupMenu );
 }

framework/source/uielement/langselectionmenucontroller.cxx
@@ -88,7 +88,7 @@ void SAL_CALL LanguageSelectionMenuController::disposing( const EventObject& )
 {
     Reference< css::awt::XMenuListener > xHolder(this);
 
-    osl::MutexGuard aLock( m_aMutex );
+    std::unique_lock aLock( m_aMutex );
     m_xFrame.clear();
     m_xDispatch.clear();
     m_xLanguageDispatch.clear();
@@ -103,7 +103,7 @@ void SAL_CALL LanguageSelectionMenuController::statusChanged( const FeatureState
 {
     SolarMutexGuard aSolarMutexGuard;
 
-    if (rBHelper.bDisposed || rBHelper.bInDispose)
+    if (m_bDisposed)
         return;
 
     m_bShowMenu = true;
@@ -233,12 +233,12 @@ void SAL_CALL LanguageSelectionMenuController::updatePopupMenu()
     svt::PopupMenuControllerBase::updatePopupMenu();
 
     // Force status update to get information about the current languages
-    osl::ClearableMutexGuard aLock( m_aMutex );
+    std::unique_lock aLock( m_aMutex );
     Reference< XDispatch > xDispatch( m_xLanguageDispatch );
     css::util::URL aTargetURL;
     aTargetURL.Complete = m_aLangStatusCommandURL;
     m_xURLTransformer->parseStrict( aTargetURL );
-    aLock.clear();
+    aLock.unlock();
 
     if ( xDispatch.is() )
     {
@@ -263,14 +263,12 @@ void SAL_CALL LanguageSelectionMenuController::updatePopupMenu()
 }
 
 // XInitialization
-void SAL_CALL LanguageSelectionMenuController::initialize( const Sequence< Any >& aArguments )
+void LanguageSelectionMenuController::initializeImpl( std::unique_lock<std::mutex>& rGuard, const Sequence< Any >& aArguments )
 {
-    osl::MutexGuard aLock( m_aMutex );
-
     bool bInitialized( m_bInitialized );
     if ( !bInitialized )
     {
-        svt::PopupMenuControllerBase::initialize(aArguments);
+        svt::PopupMenuControllerBase::initializeImpl(rGuard, aArguments);
 
         if ( m_bInitialized )
         {

commit 394dcb830c9d7bfe3a454589847be0fd5024dc86

tdf#153857 Writer hangs right-clicking on form field
regression from
    commit 5032262fccd0ef7f274323ae481b3230ffd42d3a
    Author: Noel Grandin <noel.grandin@collabora.co.uk>
    Date:   Tue Feb 21 15:05:37 2023 +0200
    BaseMutex->std::mutex in PopupMenuControllerBase

Change-Id: I01bdab09ac0ce1d9b35ed45ad36d1c5179702c6c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/147920
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

framework/source/uielement/controlmenucontroller.cxx
@@ -302,8 +302,10 @@ void SAL_CALL ControlMenuController::updatePopupMenu()
         Reference< XDispatch > xDispatch = xDispatchProvider->queryDispatch( aTargetURL, OUString(), 0 );
         if ( xDispatch.is() )
         {
+            aLock.unlock(); // the addStatusListener will call back into ::statusChanged
             xDispatch->addStatusListener( static_cast< XStatusListener* >(this), aTargetURL );
             xDispatch->removeStatusListener( static_cast< XStatusListener* >(this), aTargetURL );
+            aLock.lock();
             m_aURLToDispatchMap.emplace( aTargetURL.Complete, xDispatch );
         }
     }