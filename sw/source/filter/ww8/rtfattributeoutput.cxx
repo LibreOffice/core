LibreOffice/core

commit db115bec9254417ef7a3faf687478fe5424ab378

tdf#78510 sw,cui: split SvxLRSpaceItem for SwTextNode, SwTextFormatColl
Leave editengine and non-paragraph usages of SvxLRSpaceItem as-is for now.

Add new items RES_MARGIN_LEFT etc., order them so that paragraphs can
have 3 consecutive items RES_MARGIN_FIRSTLINE..RES_MARGIN_RIGHT and
non-paragraphs also have 2-4 consecutive items
RES_MARGIN_RIGHT..RES_MARGIN_LEFT (only the 3 paragraph ones are
actually used now).

The HTML import filter is particularly annoying because it parses CSS
stuff into SfxItemSets without knowing where the items will be applied,
so it can't know whether to create SvxLeftMarginItem or
SvxTextLeftMarginItem... the split items are created in ParseCSS1_*
functions and then converted later if necessary.

WW8 import has some weird code as well, SwWW8ImplReader::Read_LR()
creates 3 items and then something wants to set every item on its own
so SwWW8FltControlStack::SetAttrInDoc() turned out rather weird.

Convert the paragraph dialog to handle the split items (by mapping them
to SID_ATTR_PARA_FIRSTLINESPACE/SID_ATTR_PARA_LEFTSPACE/
SID_ATTR_PARA_RIGHTSPACE), but the SvxRuler looks a bit more confusing
so convert in sw shells for now and leave that for later (also unclear if
changing these slot items like SID_ATTR_PARA_LRSPACE breaks any ABIs?).

Change-Id: I40431821868fd3e1cceba121b5539ff9ae6befbc
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/147024
Tested-by: Michael Stahl <michael.stahl@allotropia.de>
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/filter/ww8/rtfattributeoutput.cxx
@@ -2997,7 +2997,9 @@ void RtfAttributeOutput::ParaTabStop(const SvxTabStopItem& rTabStop)
     // Tabs are absolute by default.
     if (m_rExport.m_rDoc.getIDocumentSettingAccess().get(
             DocumentSettingId::TABS_RELATIVE_TO_INDENT))
-        nOffset = m_rExport.GetItem(RES_LR_SPACE).GetTextLeft();
+    {
+        nOffset = m_rExport.GetItem(RES_MARGIN_TEXTLEFT).GetTextLeft();
+    }
 
     for (sal_uInt16 n = 0; n < rTabStop.Count(); n++)
     {
@@ -3088,9 +3090,10 @@ void RtfAttributeOutput::ParaNumRule_Impl(const SwTextNode* pTextNd, sal_Int32 n
     m_aStyles.append(OOO_STRING_SVTOOLS_RTF_PLAIN);
     m_aStyles.append(' ');
 
-    SvxLRSpaceItem aLR(rNdSet.Get(RES_LR_SPACE));
-    aLR.SetTextLeft(aLR.GetTextLeft() + pFormat->GetIndentAt());
-    aLR.SetTextFirstLineOffset(pFormat->GetFirstLineOffset()); //TODO: overflow
+    SvxFirstLineIndentItem firstLine(rNdSet.Get(RES_MARGIN_FIRSTLINE));
+    SvxTextLeftMarginItem leftMargin(rNdSet.Get(RES_MARGIN_TEXTLEFT));
+    leftMargin.SetTextLeft(leftMargin.GetTextLeft() + pFormat->GetIndentAt());
+    firstLine.SetTextFirstLineOffset(pFormat->GetFirstLineOffset()); //TODO: overflow
 
     sal_uInt16 nStyle = m_rExport.GetId(pFormat->GetCharFormat());
     OString* pString = m_rExport.GetStyle(nStyle);
@@ -3136,7 +3139,8 @@ void RtfAttributeOutput::ParaNumRule_Impl(const SwTextNode* pTextNd, sal_Int32 n
         m_aStyles.append(static_cast<sal_Int32>(m_rExport.GetNumberingId(*pRule)) + 1);
         m_aStyles.append(' ');
     }
-    FormatLRSpace(aLR);
+    FormatFirstLineIndent(firstLine);
+    FormatTextLeftMargin(leftMargin);
 }
 
 void RtfAttributeOutput::ParaScriptSpace(const SfxBoolItem& rScriptSpace)
@@ -3206,6 +3210,34 @@ void RtfAttributeOutput::FormatFrameSize(const SwFormatFrameSize& rSize)
 void RtfAttributeOutput::FormatPaperBin(const SvxPaperBinItem& /*rItem*/)
 {
     SAL_INFO("sw.rtf", "TODO: " << __func__);
+}
+
+void RtfAttributeOutput::FormatFirstLineIndent(SvxFirstLineIndentItem const& rFirstLine)
+{
+    m_aStyles.append(OOO_STRING_SVTOOLS_RTF_FI);
+    m_aStyles.append(static_cast<sal_Int32>(rFirstLine.GetTextFirstLineOffset()));
+}
+
+void RtfAttributeOutput::FormatTextLeftMargin(SvxTextLeftMarginItem const& rTextLeftMargin)
+{
+    m_aStyles.append(OOO_STRING_SVTOOLS_RTF_LI);
+    m_aStyles.append(static_cast<sal_Int32>(rTextLeftMargin.GetTextLeft()));
+    m_aStyles.append(OOO_STRING_SVTOOLS_RTF_LIN);
+    m_aStyles.append(static_cast<sal_Int32>(rTextLeftMargin.GetTextLeft()));
+}
+
+void RtfAttributeOutput::FormatRightMargin(SvxRightMarginItem const& rRightMargin)
+{
+// (paragraph case, this will be an else branch once others are converted)
+#if 0
+    else
+#endif
+    {
+        m_aStyles.append(OOO_STRING_SVTOOLS_RTF_RI);
+        m_aStyles.append(static_cast<sal_Int32>(rRightMargin.GetRight()));
+        m_aStyles.append(OOO_STRING_SVTOOLS_RTF_RIN);
+        m_aStyles.append(static_cast<sal_Int32>(rRightMargin.GetRight()));
+    }
 }
 
 void RtfAttributeOutput::FormatLRSpace(const SvxLRSpaceItem& rLRSpace)

commit 811dfd7f2706f56119a1943e08d9bce73b0d2203

sw: RTF export: fix assert on fdo37077-1.rtf
soffice.bin: /home/ms/lo/master/svl/source/items/itempool.cxx:822: const SfxPoolItem& SfxItemPool::GetDefaultItem(sal_uInt16) const: Assertion `!"unknown which - don't ask me for defaults"' failed.

Because editeng doesnt have RES_MARGIN_TEXTLEFT yet and it's mapped to a
slot id so the dialog works...

Looks like this TABS_RELATIVE_TO_INDENT doesn't exist in editeng anyway.

(regression from commit db115bec9254417ef7a3faf687478fe5424ab378)

Change-Id: I64169a4d6f4663adaa2a17786836c2dcc7e1f394
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/148006
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/filter/ww8/rtfattributeoutput.cxx
@@ -2994,7 +2994,11 @@ void RtfAttributeOutput::ParaTabStop(const SvxTabStopItem& rTabStop)
     if (m_rExport.m_rDoc.getIDocumentSettingAccess().get(
             DocumentSettingId::TABS_RELATIVE_TO_INDENT))
     {
-        nOffset = m_rExport.GetItem(RES_MARGIN_TEXTLEFT).GetTextLeft();
+        // don't do it for editengine text, it doesn't implement this anyway
+        if (!GetExport().m_pISet || GetExport().m_pISet->GetRanges()[0].first < RES_WHICHHINT_END)
+        {
+            nOffset = m_rExport.GetItem(RES_MARGIN_TEXTLEFT).GetTextLeft();
+        }
     }
 
     for (sal_uInt16 n = 0; n < rTabStop.Count(); n++)