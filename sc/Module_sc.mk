bccd3f299f3e29578d1a0f041aeeba3bf28f778f
Xisco Fauli <xiscofauli@libreoffice.org>
2023-03-01 10:54:39 +0100

    sc: move import tests where they belong
    and remove CppunitTest_sc_bugfix_test, which is no longer needed
    
    Change-Id: Idb1cff6580c621e325ce0b836695190d25467a0a
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/148035
    Tested-by: Jenkins
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- sc/Module_sc.mk
+++ sc/Module_sc.mk
@@ -54,7 +54,6 @@ $(eval $(call gb_Module_add_check_targets,sc,\
 	CppunitTest_sc_ucalc_sharedformula \
 	CppunitTest_sc_ucalc_sparkline \
 	CppunitTest_sc_ucalc_sort \
-	CppunitTest_sc_bugfix_test \
 	CppunitTest_sc_filters_test \
 	CppunitTest_sc_mark_test \
 	CppunitTest_sc_core \
