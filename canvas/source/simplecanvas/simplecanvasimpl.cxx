6a0b41e0556de8eba093e0823f0845652c033960
Noel Grandin <noel.grandin@collabora.co.uk>
2023-03-07 14:28:55 +0200

    osl::Mutex->std::mutex in SimpleCanvasImpl
    Change-Id: I6fc1db00360e87f1bb99fb0e77c3c82a53642d80
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/148439
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

--- canvas/source/simplecanvas/simplecanvasimpl.cxx
+++ canvas/source/simplecanvas/simplecanvasimpl.cxx
@@ -28,8 +28,7 @@
 #include <com/sun/star/rendering/PanoseWeight.hpp>
 #include <com/sun/star/rendering/XSimpleCanvas.hpp>
 #include <com/sun/star/uno/XComponentContext.hpp>
-#include <cppuhelper/basemutex.hxx>
-#include <cppuhelper/compbase.hxx>
+#include <comphelper/compbase.hxx>
 #include <o3tl/lazy_update.hxx>
 
 #include <canvas/canvastools.hxx>
@@ -94,11 +93,10 @@ namespace
     };
 
 
-    typedef ::cppu::WeakComponentImplHelper< css::rendering::XSimpleCanvas,
+    typedef ::comphelper::WeakComponentImplHelper< css::rendering::XSimpleCanvas,
                                              css::lang::XServiceName >    SimpleCanvasBase;
 
-    class SimpleCanvasImpl : private cppu::BaseMutex,
-                             public SimpleCanvasBase
+    class SimpleCanvasImpl : public SimpleCanvasBase
     {
     private:
         bool isStrokingEnabled() const
@@ -147,7 +145,6 @@ namespace
     public:
         SimpleCanvasImpl( const uno::Sequence< uno::Any >&                aArguments,
                           const uno::Reference< uno::XComponentContext >&  ) :
-            SimpleCanvasBase( m_aMutex ),
             mxCanvas( grabCanvas(aArguments) ),
             maFont([this](rendering::FontRequest const& rFontRequest) {
                    return mxCanvas->createFont(rFontRequest,
@@ -172,7 +169,7 @@ namespace
                                           sal_Bool             bold,
                                           sal_Bool             italic ) override
         {
-            ::osl::MutexGuard aGuard( m_aMutex );
+            std::unique_lock aGuard( m_aMutex );
 
             maFont->FontDescription.FamilyName = sFontName;
             maFont->CellSize = size;
@@ -184,31 +181,31 @@ namespace
 
         virtual void SAL_CALL setPenColor( ::sal_Int32 nsRgbaColor ) override
         {
-            ::osl::MutexGuard aGuard( m_aMutex );
+            std::unique_lock aGuard( m_aMutex );
             *(maRenderState.m_aPenColor) = nsRgbaColor;
         }
 
         virtual void SAL_CALL setFillColor( ::sal_Int32 nsRgbaColor ) override
         {
-            ::osl::MutexGuard aGuard( m_aMutex );
+            std::unique_lock aGuard( m_aMutex );
             *(maRenderState.m_aFillColor) = nsRgbaColor;
         }
 
         virtual void SAL_CALL setRectClip( const geometry::RealRectangle2D& aRect ) override
         {
-            ::osl::MutexGuard aGuard( m_aMutex );
+            std::unique_lock aGuard( m_aMutex );
             *(maRenderState.m_aRectClip) = aRect;
         }
 
         virtual void SAL_CALL setTransformation( const geometry::AffineMatrix2D& aTransform ) override
         {
-            ::osl::MutexGuard aGuard( m_aMutex );
+            std::unique_lock aGuard( m_aMutex );
             maRenderState.m_aTransformation = aTransform;
         }
 
         virtual void SAL_CALL drawPixel( const geometry::RealPoint2D& aPoint ) override
         {
-            ::osl::MutexGuard aGuard( m_aMutex );
+            std::unique_lock aGuard( m_aMutex );
             mxCanvas->drawPoint(aPoint,
                                 maViewState,
                                 createFillingRenderState());
@@ -217,7 +214,7 @@ namespace
         virtual void SAL_CALL drawLine( const geometry::RealPoint2D& aStartPoint,
                                         const geometry::RealPoint2D& aEndPoint ) override
         {
-            ::osl::MutexGuard aGuard( m_aMutex );
+            std::unique_lock aGuard( m_aMutex );
             mxCanvas->drawLine(aStartPoint,
                                aEndPoint,
                                maViewState,
@@ -226,7 +223,7 @@ namespace
 
         virtual void SAL_CALL drawRect( const geometry::RealRectangle2D& aRect ) override
         {
-            ::osl::MutexGuard aGuard( m_aMutex );
+            std::unique_lock aGuard( m_aMutex );
             uno::Reference< rendering::XPolyPolygon2D > xPoly(
                 rect2Poly( mxCanvas->getDevice(),
                            aRect));
@@ -243,7 +240,7 @@ namespace
 
         virtual void SAL_CALL drawPolyPolygon( const uno::Reference< rendering::XPolyPolygon2D >& xPolyPolygon ) override
         {
-            ::osl::MutexGuard aGuard( m_aMutex );
+            std::unique_lock aGuard( m_aMutex );
 
             if( isFillingEnabled() )
                 mxCanvas->drawPolyPolygon(xPolyPolygon,
@@ -259,7 +256,7 @@ namespace
                                         const geometry::RealPoint2D&    aOutPos,
                                         ::sal_Int8                      nTextDirection ) override
         {
-            ::osl::MutexGuard aGuard( m_aMutex );
+            std::unique_lock aGuard( m_aMutex );
             const basegfx::B2DHomMatrix offsetTransform(basegfx::utils::createTranslateB2DHomMatrix(aOutPos.X,aOutPos.Y));
             rendering::RenderState aRenderState( createStrokingRenderState() );
             tools::appendToRenderState(aRenderState, offsetTransform);
@@ -274,7 +271,7 @@ namespace
         virtual void SAL_CALL drawBitmap( const uno::Reference< rendering::XBitmap >& xBitmap,
                                           const geometry::RealPoint2D&                aLeftTop ) override
         {
-            ::osl::MutexGuard aGuard( m_aMutex );
+            std::unique_lock aGuard( m_aMutex );
             const basegfx::B2DHomMatrix offsetTransform(basegfx::utils::createTranslateB2DHomMatrix(aLeftTop.X,aLeftTop.Y));
             rendering::RenderState aRenderState( createStrokingRenderState() );
             tools::appendToRenderState(aRenderState, offsetTransform);
@@ -284,61 +281,61 @@ namespace
 
         virtual uno::Reference< rendering::XGraphicDevice > SAL_CALL getDevice(  ) override
         {
-            ::osl::MutexGuard aGuard( m_aMutex );
+            std::unique_lock aGuard( m_aMutex );
             return mxCanvas->getDevice();
         }
 
         virtual uno::Reference< rendering::XCanvas > SAL_CALL getCanvas(  ) override
         {
-            ::osl::MutexGuard aGuard( m_aMutex );
+            std::unique_lock aGuard( m_aMutex );
             return mxCanvas;
         }
 
         virtual rendering::FontMetrics SAL_CALL getFontMetrics(  ) override
         {
-            ::osl::MutexGuard aGuard( m_aMutex );
+            std::unique_lock aGuard( m_aMutex );
             return maFont.getOutValue()->getFontMetrics();
         }
 
         virtual uno::Reference< rendering::XCanvasFont > SAL_CALL getCurrentFont(  ) override
         {
-            ::osl::MutexGuard aGuard( m_aMutex );
+            std::unique_lock aGuard( m_aMutex );
             return maFont.getOutValue();
         }
 
         virtual ::sal_Int32 SAL_CALL getCurrentPenColor(  ) override
         {
-            ::osl::MutexGuard aGuard( m_aMutex );
+            std::unique_lock aGuard( m_aMutex );
             return maRenderState.m_aPenColor.getInValue();
         }
 
         virtual ::sal_Int32 SAL_CALL getCurrentFillColor(  ) override
         {
-            ::osl::MutexGuard aGuard( m_aMutex );
+            std::unique_lock aGuard( m_aMutex );
             return maRenderState.m_aFillColor.getInValue();
         }
 
         virtual geometry::RealRectangle2D SAL_CALL getCurrentClipRect(  ) override
         {
-            ::osl::MutexGuard aGuard( m_aMutex );
+            std::unique_lock aGuard( m_aMutex );
             return maRenderState.m_aRectClip.getInValue();
         }
 
         virtual geometry::AffineMatrix2D SAL_CALL getCurrentTransformation(  ) override
         {
-            ::osl::MutexGuard aGuard( m_aMutex );
+            std::unique_lock aGuard( m_aMutex );
             return maRenderState.m_aTransformation;
         }
 
         virtual rendering::ViewState SAL_CALL getCurrentViewState(  ) override
         {
-            ::osl::MutexGuard aGuard( m_aMutex );
+            std::unique_lock aGuard( m_aMutex );
             return maViewState;
         }
 
         virtual rendering::RenderState SAL_CALL getCurrentRenderState( sal_Bool bUseFillColor ) override
         {
-            ::osl::MutexGuard aGuard( m_aMutex );
+            std::unique_lock aGuard( m_aMutex );
             if( bUseFillColor )
                 return createFillingRenderState();
             else
