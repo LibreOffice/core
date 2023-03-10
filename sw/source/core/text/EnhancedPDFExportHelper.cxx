c105fbec07d0734b6b183ea8b4da15e8fe260f9b
Michael Stahl <michael.stahl@allotropia.de>
2023-03-10 14:44:12 +0100

    tdf#152218 sw: PDF/UA export: produce Link from footnote to body
    There was already a Link StructElem from the reference in the body to
    the footnote but it was missing for the other direction, so veraPDF
    complains about a Link annotation without StructElem:
    
      Specification: ISO 14289-1:2014, Clause: 7.18.5, Test number: 1
      Links shall be tagged according to ISO 32000-1:2008, 14.8.4.4.2, Link Element.
    
    Change-Id: I57aa153d5899bcfa505e274a01b0183978a2e995
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/148644
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

--- sw/source/core/text/EnhancedPDFExportHelper.cxx
+++ sw/source/core/text/EnhancedPDFExportHelper.cxx
@@ -1557,6 +1557,7 @@ void SwTaggedPDFHelper::BeginInlineStructureElements()
             }
             break;
 
+        case PortionType::FootnoteNum: // tdf#152218 link both directions
         case PortionType::Footnote :
             nPDFType = vcl::PDFWriter::Link;
             aPDFType = aLinkString;
