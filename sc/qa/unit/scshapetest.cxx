LibreOffice/core

commit 1f0b3c7a40edfa81bbc7a58d123a6a2dfd83e4ca

Improve 'resize with cell' handling
The patch contains a larger rework of RecalcPos and connected areas
and covers several bugs. Essentials in short:
Move initialization from RecalcPos to own method and use it in
ScXMLImport::endDocument
Do not change hidden objects, which includes not setting width or
height to zero, and be consistent in using object visibility.
Special handling of vertical flipped customshapes.
Repair anchor on import of line and measure line.
ODF conformance: Create logical rectangle from anchor instead using
size.

Details:
tdf#137044
ScDrawLayer::SetPageSize is called several times while loading a
document. It includes a call to ScDrawLayer::RecalcPos for all cell
anchored objects. An object gets initialized with the first call.
Problem was, that the row heights were not finished at that time and
anchor cells and offsets were partly calculated based on default cell
height. That results in wrong height and offset of objects.
The solution separates initialization from RecalcPos and puts it into
an own method ScDrawLayer::InitializeCellAnchoredObj. This is then
called from ScXMLImport::endDocument when row height settings are
finished.
The call to RecalcPos is not totally removed from SetPageSize but only
excluded while loading, because it is needed for size changes after
the document is loaded.

tdf#137576 partly
For measure lines and ordinary lines, which were anchored 'To cell
(resize with cell)', LibreOffice has written wrong end-cell info to
file. So reopening results in wrong lines. The geometry of lines is
based on two points. Fortunatelly the combination of position of the
cell, which contains the shape, and start and end points gives correct
absolute position of these points.
Solution is, to regenerate the initial ScDrawObjData infos from these
points and do not use the stored end-cell info. For a total fix
implementation of NbcSetSnapRect for SdrMeasureObj is needed, which is
not included here.

tdf#137020

sc/qa/unit/scshapetest.cxx
@@ -146,7 +146,7 @@ void ScShapeTest::testCustomShapeCellAnchoredRotatedShape()
     CPPUNIT_ASSERT(pObj);
 
     // Check Position and Size
-    tools::Rectangle aRect(2406, 754, 5774, 3692); // expected snap rect
+    tools::Rectangle aRect(2400, 751, 5772, 3693); // expected snap rect
     rDoc.SetDrawPageSize(0); // trigger recalcpos
     const tools::Rectangle& rShapeRect(pObj->GetSnapRect());
     const OUString sPosSizeErrors(lcl_compareRectWithTolerance(aRect, rShapeRect, 1));

sc/inc/drwlayer.hxx
@@ -136,6 +136,7 @@ public:
 
     void            MoveArea( SCTAB nTab, SCCOL nCol1,SCROW nRow1, SCCOL nCol2,SCROW nRow2,
                                 SCCOL nDx,SCROW nDy, bool bInsDel, bool bUpdateNoteCaptionPos );
+    void InitializeCellAnchoredObj(SdrObject* pObj, ScDrawObjData& rData);
     void            RecalcPos( SdrObject* pObj, ScDrawObjData& rData, bool bNegativePage, bool bUpdateNoteCaptionPos );
 
     bool            HasObjectsInRows( SCTAB nTab, SCROW nStartRow, SCROW nEndRow );
@@ -189,6 +190,7 @@ public:
     std::vector<SdrObject*> GetObjectsAnchoredToRows(SCTAB nTab, SCROW nStartRow, SCROW nEndRow);
     std::map<SCROW, std::vector<SdrObject*>> GetObjectsAnchoredToRange(SCTAB nTab, SCCOL nCol, SCROW nStartRow, SCROW nEndRow);
     bool HasObjectsAnchoredInRange(const ScRange& rRange);
+    std::vector<SdrObject*> GetObjectsAnchoredToCols(SCTAB nTab, SCCOL nStartCol, SCCOL nEndCol);
     void MoveObject(SdrObject* pObj, const ScAddress& rNewPosition);
 
     // positions for detective lines

sc/inc/table.hxx
@@ -77,7 +77,7 @@ class RowHeightContext;
 class CompileFormulaContext;
 struct SetFormulaDirtyContext;
 class ColumnIterator;
-
+class ScDrawObjData;
 }
 
 class SfxItemSet;

sc/qa/unit/filters-test.cxx
@@ -555,11 +555,8 @@ void ScFiltersTest::testLegacyCellAnchoredRotatedShape()
         ScDocShellRef xDocSh = loadDoc("legacycellanchoredrotatedhiddenshape.", FORMAT_ODS, true);
         ScDocument& rDoc = xDocSh->GetDocument();
         // ensure the imported legacy rotated shape is in the expected position
-        // when a shape is fully hidden reloading seems to result is in some errors, usually
-        // ( same but different error happens pre-patch ) - we should do better here, I regard it
-        // as a pre-existing bug though (#FIXME)
-        //Rectangle aRect( 6000, -2000, 8000, 4000 ); // proper dimensions
-        tools::Rectangle aRect( 6000, -2000, 7430, 4000 );
+        tools::Rectangle aRect( 6000, -2000, 8000, 4000 );
+
         // ensure the imported (and converted) anchor (note we internally now store the anchor in
         // terms of the rotated shape) is more or less contains the correct info
         ScDrawObjData aAnchor;
@@ -569,10 +566,11 @@ void ScFiltersTest::testLegacyCellAnchoredRotatedShape()
         aAnchor.maEnd.SetCol( 7 );
         rDoc.ShowRows(0, 9, 0, true); // show relevant rows
         rDoc.SetDrawPageSize(0); // trigger recalcpos
-
-        // apply hefty (1 mm) tolerance here, as some opensuse tinderbox
-        // failing
-        impl_testLegacyCellAnchoredRotatedShape( rDoc, aRect, aAnchor, 100 );
+        impl_testLegacyCellAnchoredRotatedShape( rDoc, aRect, aAnchor);
+        // test save and reload
+        xDocSh = saveAndReload( &(*xDocSh), FORMAT_ODS);
+        ScDocument& rDoc2 = xDocSh->GetDocument();
+        impl_testLegacyCellAnchoredRotatedShape( rDoc2, aRect, aAnchor );
 
         xDocSh->DoClose();
     }

sc/qa/unit/ucalc.cxx
@@ -2797,17 +2797,15 @@ void Test::testGraphicsInGroup()
         m_pDoc->ShowRows(0, 100, 0, false);
         m_pDoc->SetDrawPageSize(0);
 
-        const tools::Long TOLERANCE = 30; //30 hmm
+        CPPUNIT_ASSERT_EQUAL_MESSAGE("Hiding should not change the logic rectangle",
+                               const_cast<const tools::Rectangle &>(aOrigRect), rNewRect);
+        CPPUNIT_ASSERT_MESSAGE("Hiding should make invisible", !pObj->IsVisible());
 
-        CPPUNIT_ASSERT_MESSAGE("Left and Right should be unchanged",
-            testEqualsWithTolerance(aOrigRect.Left(), rNewRect.Left(), TOLERANCE) &&
-            testEqualsWithTolerance(aOrigRect.Right(), rNewRect.Right(), TOLERANCE));
-        CPPUNIT_ASSERT_MESSAGE("Height should be minimum allowed height",
-            (rNewRect.Bottom() - rNewRect.Top()) <= 1);
         m_pDoc->ShowRows(0, 100, 0, true);
         m_pDoc->SetDrawPageSize(0);
         CPPUNIT_ASSERT_EQUAL_MESSAGE("Should not change when cell anchored",
                                const_cast<const tools::Rectangle &>(aOrigRect), rNewRect);
+        CPPUNIT_ASSERT_MESSAGE("Show should make visible", pObj->IsVisible());
     }
 
     {

sc/source/core/data/table5.cxx
@@ -36,6 +36,7 @@
 #include <printopt.hxx>
 #include <bcaslot.hxx>
 #include <compressedarray.hxx>
+#include <userdat.hxx>
 
 #include <com/sun/star/sheet/TablePageBreakData.hpp>
 
@@ -583,13 +584,25 @@ bool ScTable::SetRowHidden(SCROW nStartRow, SCROW nEndRow, bool bHidden)
     else
         bChanged = mpHiddenRows->setFalse(nStartRow, nEndRow);
 
-    std::vector<SdrObject*> aRowDrawObjects;
+    // Cell anchored objects might change visibility
     ScDrawLayer* pDrawLayer = rDocument.GetDrawLayer();
-    if (pDrawLayer) {
+    if (pDrawLayer)
+    {
+        std::vector<SdrObject*> aRowDrawObjects;
         aRowDrawObjects = pDrawLayer->GetObjectsAnchoredToRows(GetTab(), nStartRow, nEndRow);
         for (auto aObj : aRowDrawObjects)
         {
-            aObj->SetVisible(!bHidden);
+            ScDrawObjData* pData = ScDrawLayer::GetObjData(aObj);
+            if (pData)
+            {
+                if (bHidden)
+                    aObj->SetVisible(false);
+                else if (!GetDoc().ColHidden(pData->maStart.Col(), pData->maStart.Tab()))
+                {
+                    // Only change visibility if object is not hidden by a hidden col
+                    aObj->SetVisible(true);
+                }
+            }
         }
     }
 
@@ -620,6 +633,28 @@ void ScTable::SetColHidden(SCCOL nStartCol, SCCOL nEndCol, bool bHidden)
         bChanged = mpHiddenCols->setTrue(nStartCol, nEndCol);
     else
         bChanged = mpHiddenCols->setFalse(nStartCol, nEndCol);
+
+    // Cell anchored objects might change visibility
+    ScDrawLayer* pDrawLayer = rDocument.GetDrawLayer();
+    if (pDrawLayer)
+    {
+        std::vector<SdrObject*> aColDrawObjects;
+        aColDrawObjects = pDrawLayer->GetObjectsAnchoredToCols(GetTab(), nStartCol, nEndCol);
+        for (auto aObj : aColDrawObjects)
+        {
+            ScDrawObjData* pData = ScDrawLayer::GetObjData(aObj);
+            if (pData)
+            {
+                if (bHidden)
+                    aObj->SetVisible(false);
+                else if (!GetDoc().RowHidden(pData->maStart.Row(), pData->maStart.Tab()))
+                {
+                    // Only change visibility if object is not hidden by a hidden row
+                    aObj->SetVisible(true);
+                }
+            }
+        }
+    }
 
     if (bChanged)
         SetStreamValid(false);

sc/source/filter/xml/xmlexprt.cxx
@@ -3473,8 +3473,10 @@ void ScXMLExport::WriteShapes(const ScMyCell& rMyCell)
         return;
 
     awt::Point aPoint;
+    // Hiding row or col does not change the shape geometry.
     tools::Rectangle aRect = pDoc->GetMMRect(rMyCell.maCellAddress.Col(), rMyCell.maCellAddress.Row(),
-        rMyCell.maCellAddress.Col(), rMyCell.maCellAddress.Row(), rMyCell.maCellAddress.Tab());
+        rMyCell.maCellAddress.Col(), rMyCell.maCellAddress.Row(), rMyCell.maCellAddress.Tab(),
+        false /*bHiddenAsZero*/);
     bool bNegativePage = pDoc->IsNegativePage(rMyCell.maCellAddress.Tab());
     if (bNegativePage)
         aPoint.X = aRect.Right();

commit 97a38dbfa998967c45efaf3303fedfa1a709a2bb

tdf#154005 sc ods fileopen: fix dropdown form control size
Dropdown form control size was increased by the size of hidden
rows or columns.

Regression from commit: 1f0b3c7a40edfa81bbc7a58d123a6a2dfd83e4ca
(Improve 'resize with cell' handling)

Change-Id: Ic903a488cab22286f95cfdf4ee559013fd7bfa02
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/148738
Tested-by: Thorsten Behrens <thorsten.behrens@allotropia.de>
Reviewed-by: Thorsten Behrens <thorsten.behrens@allotropia.de>

sc/inc/document.hxx
@@ -2036,6 +2036,7 @@ public:
     SC_DLLPUBLIC SCROW          FirstVisibleRow(SCROW nStartRow, SCROW nEndRow, SCTAB nTab) const;
     SC_DLLPUBLIC SCROW          LastVisibleRow(SCROW nStartRow, SCROW nEndRow, SCTAB nTab) const;
     SCROW                       CountVisibleRows(SCROW nStartRow, SCROW nEndRow, SCTAB nTab) const;
+    SCCOL                       CountVisibleCols(SCROW nStartCol, SCROW nEndCol, SCTAB nTab) const;
 
     SC_DLLPUBLIC bool           RowFiltered(SCROW nRow, SCTAB nTab, SCROW* pFirstRow = nullptr, SCROW* pLastRow = nullptr) const;
     bool                        HasFilteredRows(SCROW nStartRow, SCROW nEndRow, SCTAB nTab) const;

sc/source/core/data/document.cxx
@@ -4563,6 +4563,14 @@ SCROW ScDocument::CountVisibleRows(SCROW nStartRow, SCROW nEndRow, SCTAB nTab) c
     return maTabs[nTab]->CountVisibleRows(nStartRow, nEndRow);
 }
 
+SCCOL ScDocument::CountVisibleCols(SCROW nStartCol, SCROW nEndCol, SCTAB nTab) const
+{
+    if (!ValidTab(nTab) || nTab >= static_cast<SCTAB>(maTabs.size()) || !maTabs[nTab])
+        return 0;
+
+    return maTabs[nTab]->CountVisibleCols(nStartCol, nEndCol);
+}
+
 bool ScDocument::RowFiltered(SCROW nRow, SCTAB nTab, SCROW* pFirstRow, SCROW* pLastRow) const
 {
     if (!ValidTab(nTab) || nTab >= static_cast<SCTAB>(maTabs.size()) || !maTabs[nTab])

sc/source/core/data/drwlayer.cxx
@@ -703,6 +703,18 @@ void lcl_SetLogicRectFromAnchor(SdrObject* pObj, const ScDrawObjData& rAnchor, c
     if (!pObj || !pDoc || !rAnchor.maEnd.IsValid() || !rAnchor.maStart.IsValid())
         return;
 
+    SCROW nHiddenRows = 0;
+    SCCOL nHiddenCols = 0;
+    // tdf#154005: Handle hidden row/col: remove hidden row/cols size from the ScDrawObjData shape size in case of forms
+    if (pObj->GetObjIdentifier() == SdrObjKind::UNO && pObj->GetObjInventor() == SdrInventor::FmForm)
+    {
+        nHiddenRows = ((rAnchor.maEnd.Row() - rAnchor.maStart.Row()) + 1) -
+            (pDoc->CountVisibleRows(rAnchor.maStart.Row(), rAnchor.maEnd.Row(), rAnchor.maStart.Tab()));
+
+        nHiddenCols = ((rAnchor.maEnd.Col() - rAnchor.maStart.Col()) + 1) -
+            (pDoc->CountVisibleCols(rAnchor.maStart.Col(), rAnchor.maEnd.Col(), rAnchor.maStart.Tab()));
+    }
+
     // In case of a vertical mirrored custom shape, LibreOffice uses internally an additional 180deg
     // in aGeo.nRotationAngle and in turn has a different logic rectangle position. We remove flip,
     // set the logic rectangle, and apply flip again. You cannot simple use a 180deg-rotated
@@ -726,8 +738,8 @@ void lcl_SetLogicRectFromAnchor(SdrObject* pObj, const ScDrawObjData& rAnchor, c
     aStartPoint.AdjustY(rAnchor.maStartOffset.getY());
 
     const tools::Rectangle aEndCellRect(
-        pDoc->GetMMRect(rAnchor.maEnd.Col(), rAnchor.maEnd.Row(), rAnchor.maEnd.Col(),
-                        rAnchor.maEnd.Row(), rAnchor.maEnd.Tab(), false /*bHiddenAsZero*/));
+        pDoc->GetMMRect(rAnchor.maEnd.Col() - nHiddenCols, rAnchor.maEnd.Row() - nHiddenRows, rAnchor.maEnd.Col() - nHiddenCols,
+                        rAnchor.maEnd.Row() - nHiddenRows, rAnchor.maEnd.Tab(), false /*bHiddenAsZero*/));
     Point aEndPoint(aEndCellRect.Left(), aEndCellRect.Top());
     aEndPoint.AdjustX(rAnchor.maEndOffset.getX());
     aEndPoint.AdjustY(rAnchor.maEndOffset.getY());

sc/qa/unit/scshapetest.cxx
@@ -57,6 +57,7 @@ public:
     void testTdf137576_LogicRectInNewMeasureline();
     void testMeasurelineHideColSave();
     void testHideColsShow();
+    void testFormSizeWithHiddenCol();
     void testTdf138138_MoveCellWithRotatedShape();
     void testLoadVerticalFlip();
     void testTdf117948_CollapseBeforeShape();
@@ -85,6 +86,7 @@ public:
     CPPUNIT_TEST(testTdf137576_LogicRectInNewMeasureline);
     CPPUNIT_TEST(testMeasurelineHideColSave);
     CPPUNIT_TEST(testHideColsShow);
+    CPPUNIT_TEST(testFormSizeWithHiddenCol);
     CPPUNIT_TEST(testTdf138138_MoveCellWithRotatedShape);
     CPPUNIT_TEST(testLoadVerticalFlip);
     CPPUNIT_TEST(testTdf117948_CollapseBeforeShape);
@@ -757,6 +759,34 @@ void ScShapeTest::testHideColsShow()
     CPPUNIT_ASSERT_MESSAGE("Show: Object should be visible", pObj->IsVisible());
     tools::Rectangle aSnapRectShow(pObj->GetSnapRect());
     CPPUNIT_ASSERT_RECTANGLE_EQUAL_WITH_TOLERANCE(aSnapRectOrig, aSnapRectShow, 1);
+}
+
+void ScShapeTest::testFormSizeWithHiddenCol()
+{
+    // The document contains a form (Listbox) shape anchored "To Cell (resize with cell)" with starts in cell B5 and
+    // ends in cell D5. The error was the form shape was resized if there was hidden col/row.
+
+    createScDoc("tdf154005.ods");
+
+    // Get document and shape
+    ScDocument* pDoc = getScDoc();
+    SdrUnoObj* pObj = static_cast<SdrUnoObj*>(lcl_getSdrObjectWithAssert(*pDoc, 0));
+
+    // Check Position and Size
+    pDoc->SetDrawPageSize(0); // trigger recalcpos
+    tools::Rectangle aRect(2432, 3981, 4932, 4631); // expected snap rect from values in file
+    const tools::Rectangle& rShapeRect(pObj->GetSnapRect());
+    CPPUNIT_ASSERT_RECTANGLE_EQUAL_WITH_TOLERANCE(aRect, rShapeRect, 1);
+
+    // Check anchor
+    ScDrawObjData* pData = ScDrawLayer::GetObjData(pObj);
+    CPPUNIT_ASSERT_MESSAGE("expected object meta data", pData);
+
+    const OUString sActual("start col " + OUString::number(pData->maStart.Col()) + " row "
+                           + OUString::number(pData->maStart.Row()) + " end col "
+                           + OUString::number(pData->maEnd.Col()) + " row "
+                           + OUString::number(pData->maEnd.Row()));
+    CPPUNIT_ASSERT_EQUAL(OUString("start col 1 row 4 end col 3 row 4"), sActual);
 }
 
 void ScShapeTest::testTdf138138_MoveCellWithRotatedShape()