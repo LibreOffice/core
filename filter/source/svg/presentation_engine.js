6700f7f5b4eae31bf020e7d073c496c6e67a2397
Yousef_Rabia <yousef.mohamed.rabia@gmail.com>
2023-03-26 21:06:00 +0200

    tdf#147906 Use Math.hypot() for Pythagorean addition
    Change-Id: I8823038bde51002ea8c02ca7e841aca99f928a58
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/149618
    Tested-by: Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>
    Reviewed-by: Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>

--- filter/source/svg/presentation_engine.js
+++ filter/source/svg/presentation_engine.js
@@ -763,7 +763,7 @@ function getDistance(p1, p2, props) {
     var x = p2[props[0]] - p1[props[0]],
         y = p2[props[1]] - p1[props[1]];
 
-    return Math.sqrt((x * x) + (y * y));
+    return Math.hypot(x, y);
 }
 
 /**
