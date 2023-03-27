LibreOffice/core

commit e79741488cc740f49ebd4426c40b45e7139ff663

tdf#112237 Show tooltips for Assigned Commands in Customize dialog
Change-Id: Iff1946d5d6ac7f2046d36df8fbd76e32edf371c4
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/140142
Tested-by: Jenkins
Reviewed-by: Jim Raykowski <raykowj@gmail.com>

cui/source/customize/SvxNotebookbarConfigPage.cxx
@@ -240,7 +240,7 @@ void SvxConfigPage::InsertEntryIntoNotebookbarTabUI(std::u16string_view sClassId
         if (xImage.is())
             rTreeView.set_image(rIter, xImage, -1);
         rTreeView.set_text(rIter, aName, 0);
-        rTreeView.set_id(rIter, sUIItemId);
+        rTreeView.set_id(rIter, sUIItemCommand);
     }
 }
 
@@ -438,8 +438,6 @@ void SvxNotebookbarConfigPage::SelectElement()
     weld::TreeView& rTreeView = m_xContentsListBox->get_widget();
     rTreeView.bulk_insert_for_each(
         aEntries.size(), [this, &rTreeView, &aEntries](weld::TreeIter& rIter, int nIdx) {
-            OUString sId(OUString::number(nIdx));
-            rTreeView.set_id(rIter, sId);
             if (aEntries[nIdx].sActionName != "Null")
             {
                 if (aEntries[nIdx].sVisibleValue == "True")
@@ -467,6 +465,9 @@ SvxNotebookbarEntriesListBox::SvxNotebookbarEntriesListBox(std::unique_ptr<weld:
     m_xControl->connect_toggled(LINK(this, SvxNotebookbarEntriesListBox, CheckButtonHdl));
     m_xControl->connect_key_press(Link<const KeyEvent&, bool>());
     m_xControl->connect_key_press(LINK(this, SvxNotebookbarEntriesListBox, KeyInputHdl));
+    // remove the inherited connect_query_tooltip then add the new one
+    m_xControl->connect_query_tooltip(Link<const weld::TreeIter&, OUString>());
+    m_xControl->connect_query_tooltip(LINK(this, SvxNotebookbarEntriesListBox, QueryTooltip));
 }
 
 SvxNotebookbarEntriesListBox::~SvxNotebookbarEntriesListBox() {}
@@ -538,6 +539,21 @@ IMPL_LINK(SvxNotebookbarEntriesListBox, KeyInputHdl, const KeyEvent&, rKeyEvent,
         return true;
     }
     return SvxMenuEntriesListBox::KeyInputHdl(rKeyEvent);
+}
+
+IMPL_LINK(SvxNotebookbarEntriesListBox, QueryTooltip, const weld::TreeIter&, rIter, OUString)
+{
+    OUString sCommand = m_xControl->get_id(rIter);
+    if (sCommand.isEmpty())
+        return OUString();
+    OUString aModuleName(vcl::CommandInfoProvider::GetModuleIdentifier(m_pPage->GetFrame()));
+    auto aProperties = vcl::CommandInfoProvider::GetCommandProperties(sCommand, aModuleName);
+    OUString sTooltipLabel = vcl::CommandInfoProvider::GetTooltipForCommand(sCommand, aProperties,
+                                                                            m_pPage->GetFrame());
+    return CuiResId(RID_CUISTR_COMMANDLABEL) + ": "
+           + m_xControl->get_text(rIter).replaceFirst("~", "") + "\n"
+           + CuiResId(RID_CUISTR_COMMANDNAME) + ": " + sCommand + "\n"
+           + CuiResId(RID_CUISTR_COMMANDTIP) + ": " + sTooltipLabel.replaceFirst("~", "");
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

cui/source/inc/SvxNotebookbarConfigPage.hxx
@@ -77,6 +77,7 @@ class SvxNotebookbarEntriesListBox final : public SvxMenuEntriesListBox
     void ChangedVisibility(int nRow);
     DECL_LINK(CheckButtonHdl, const weld::TreeView::iter_col&, void);
     DECL_LINK(KeyInputHdl, const KeyEvent&, bool);
+    DECL_LINK(QueryTooltip, const weld::TreeIter& rIter, OUString);
 
 public:
     SvxNotebookbarEntriesListBox(std::unique_ptr<weld::TreeView> xControl, SvxConfigPage* pPg);

cui/source/customize/cfg.cxx
@@ -937,6 +937,7 @@ SvxMenuEntriesListBox::SvxMenuEntriesListBox(std::unique_ptr<weld::TreeView> xCo
     m_xControl->enable_toggle_buttons(weld::ColumnToggleType::Check);
     CreateDropDown();
     m_xControl->connect_key_press(LINK(this, SvxMenuEntriesListBox, KeyInputHdl));
+    m_xControl->connect_query_tooltip(LINK(this, SvxMenuEntriesListBox, QueryTooltip));
 }
 
 SvxMenuEntriesListBox::~SvxMenuEntriesListBox()
@@ -966,6 +967,21 @@ IMPL_LINK(SvxMenuEntriesListBox, KeyInputHdl, const KeyEvent&, rKeyEvent, bool)
         return false; // pass on to default handler
     }
     return true;
+}
+
+IMPL_LINK(SvxMenuEntriesListBox, QueryTooltip, const weld::TreeIter&, rIter, OUString)
+{
+    SvxConfigEntry *pEntry = weld::fromId<SvxConfigEntry*>(m_xControl->get_id(rIter));
+    if (!pEntry || pEntry->GetCommand().isEmpty())
+        return OUString();
+    const OUString sCommand(pEntry->GetCommand());
+    OUString aModuleName(vcl::CommandInfoProvider::GetModuleIdentifier(m_pPage->GetFrame()));
+    auto aProperties = vcl::CommandInfoProvider::GetCommandProperties(sCommand, aModuleName);
+    OUString sTooltipLabel = vcl::CommandInfoProvider::GetTooltipForCommand(sCommand, aProperties,
+                                                                            m_pPage->GetFrame());
+    return CuiResId(RID_CUISTR_COMMANDLABEL) + ": " + pEntry->GetName().replaceFirst("~", "") + "\n" +
+            CuiResId(RID_CUISTR_COMMANDNAME) + ": " + sCommand + "\n" +
+            CuiResId(RID_CUISTR_COMMANDTIP) + ": " + sTooltipLabel.replaceFirst("~", "");
 }
 
 /******************************************************************************

cui/source/inc/cfg.hxx
@@ -348,6 +348,7 @@ public:
     }
 
     DECL_LINK(KeyInputHdl, const KeyEvent&, bool);
+    DECL_LINK(QueryTooltip, const weld::TreeIter& rIter, OUString);
 
     void CreateDropDown();
 };

cui/uiconfig/ui/menuassignpage.ui
@@ -563,6 +563,7 @@
                   <object class="GtkTreeView" id="toolcontents">
                     <property name="can_focus">True</property>
                     <property name="receives_default">True</property>
+                    <property name="has_tooltip">True</property>
                     <property name="no_show_all">True</property>
                     <property name="hexpand">True</property>
                     <property name="vexpand">True</property>
@@ -619,6 +620,7 @@
                   <object class="GtkTreeView" id="menucontents">
                     <property name="can_focus">True</property>
                     <property name="receives_default">True</property>
+                    <property name="has_tooltip">True</property>
                     <property name="no_show_all">True</property>
                     <property name="hexpand">True</property>
                     <property name="vexpand">True</property>

commit 6524b8efecaab39486f54d8b95a4e124d1482b08

tdf#154296 Fix customize notebook bar regression
caused by commit e79741488cc740f49ebd4426c40b45e7139ff663

Change-Id: I04fe66124af831c73834b7f695458e1667198ddb
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/149470
Tested-by: Jenkins
Reviewed-by: Jim Raykowski <raykowj@gmail.com>
(cherry picked from commit 48b7cf3182cc8fb0b728860f9cbb489390074101)
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/149600
Tested-by: Adolfo Jayme Barrientos <fitojb@ubuntu.com>
Reviewed-by: Adolfo Jayme Barrientos <fitojb@ubuntu.com>

cui/source/customize/SvxNotebookbarConfigPage.cxx
@@ -240,7 +240,7 @@ void SvxConfigPage::InsertEntryIntoNotebookbarTabUI(std::u16string_view sClassId
         if (xImage.is())
             rTreeView.set_image(rIter, xImage, -1);
         rTreeView.set_text(rIter, aName, 0);
-        rTreeView.set_id(rIter, sUIItemCommand);
+        rTreeView.set_id(rIter, sUIItemId);
     }
 }
 
@@ -435,6 +435,7 @@ void SvxNotebookbarConfigPage::SelectElement()
 
     aEntries = std::move(aTempEntries);
 
+    static_cast<SvxNotebookbarEntriesListBox*>(m_xContentsListBox.get())->GetTooltipMap().clear();
     weld::TreeView& rTreeView = m_xContentsListBox->get_widget();
     rTreeView.bulk_insert_for_each(
         aEntries.size(), [this, &rTreeView, &aEntries](weld::TreeIter& rIter, int nIdx) {
@@ -451,6 +452,13 @@ void SvxNotebookbarConfigPage::SelectElement()
             }
             InsertEntryIntoNotebookbarTabUI(aEntries[nIdx].sClassId, aEntries[nIdx].sDisplayName,
                                             aEntries[nIdx].sActionName, rTreeView, rIter);
+            if (aEntries[nIdx].sClassId != u"GtkSeparatorMenuItem"
+                && aEntries[nIdx].sClassId != u"GtkSeparator")
+            {
+                static_cast<SvxNotebookbarEntriesListBox*>(m_xContentsListBox.get())
+                    ->GetTooltipMap()[aEntries[nIdx].sDisplayName]
+                    = aEntries[nIdx].sActionName;
+            }
         });
 
     aEntries.clear();
@@ -543,16 +551,16 @@ IMPL_LINK(SvxNotebookbarEntriesListBox, KeyInputHdl, const KeyEvent&, rKeyEvent,
 
 IMPL_LINK(SvxNotebookbarEntriesListBox, QueryTooltip, const weld::TreeIter&, rIter, OUString)
 {
-    OUString sCommand = m_xControl->get_id(rIter);
-    if (sCommand.isEmpty())
+    const OUString& rsCommand = m_aTooltipMap[m_xControl->get_id(rIter)];
+    if (rsCommand.isEmpty())
         return OUString();
     OUString aModuleName(vcl::CommandInfoProvider::GetModuleIdentifier(m_pPage->GetFrame()));
-    auto aProperties = vcl::CommandInfoProvider::GetCommandProperties(sCommand, aModuleName);
-    OUString sTooltipLabel = vcl::CommandInfoProvider::GetTooltipForCommand(sCommand, aProperties,
+    auto aProperties = vcl::CommandInfoProvider::GetCommandProperties(rsCommand, aModuleName);
+    OUString sTooltipLabel = vcl::CommandInfoProvider::GetTooltipForCommand(rsCommand, aProperties,
                                                                             m_pPage->GetFrame());
     return CuiResId(RID_CUISTR_COMMANDLABEL) + ": "
            + m_xControl->get_text(rIter).replaceFirst("~", "") + "\n"
-           + CuiResId(RID_CUISTR_COMMANDNAME) + ": " + sCommand + "\n"
+           + CuiResId(RID_CUISTR_COMMANDNAME) + ": " + rsCommand + "\n"
            + CuiResId(RID_CUISTR_COMMANDTIP) + ": " + sTooltipLabel.replaceFirst("~", "");
 }
 

cui/source/inc/SvxNotebookbarConfigPage.hxx
@@ -74,6 +74,9 @@ public:
 
 class SvxNotebookbarEntriesListBox final : public SvxMenuEntriesListBox
 {
+    typedef std::unordered_map<OUString, OUString> TooltipMap;
+    TooltipMap m_aTooltipMap;
+
     void ChangedVisibility(int nRow);
     DECL_LINK(CheckButtonHdl, const weld::TreeView::iter_col&, void);
     DECL_LINK(KeyInputHdl, const KeyEvent&, bool);
@@ -82,6 +85,8 @@ class SvxNotebookbarEntriesListBox final : public SvxMenuEntriesListBox
 public:
     SvxNotebookbarEntriesListBox(std::unique_ptr<weld::TreeView> xControl, SvxConfigPage* pPg);
     virtual ~SvxNotebookbarEntriesListBox() override;
+
+    TooltipMap& GetTooltipMap() { return m_aTooltipMap; }
 };
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */