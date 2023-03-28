LibreOffice/core

commit b484e5d9e0d22b794c2d1c73741191836158a4ad

use more SwPosition::Assign
part of the process of hiding the internals of SwPosition

Change-Id: I20842b90d0abb4435d6b06970c2545229268e5e8
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/141097
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sw/source/core/doc/docedt.cxx
@@ -520,7 +520,8 @@ uno::Any SwDoc::Spell( SwPaM& rPaM,
                     SwRootFrame const*const pLayout,
                     SwConversionArgs *pConvArgs  ) const
 {
-    SwPosition* pSttPos = rPaM.Start(), *pEndPos = rPaM.End();
+    SwPosition* const pSttPos = rPaM.Start();
+    SwPosition* const pEndPos = rPaM.End();
 
     std::unique_ptr<SwSpellArgs> pSpellArgs;
     if (pConvArgs)
@@ -600,13 +601,16 @@ uno::Any SwDoc::Spell( SwPaM& rPaM,
                             ( pConvArgs && pNd->GetTextNode()->Convert( *pConvArgs )))
                         {
                             // Cancel and remember position
-                            pSttPos->nNode = nCurrNd;
-                            pEndPos->nNode = nCurrNd;
-                            nCurrNd = nEndNd;
                             if( pSpellArgs )
                                 nSpellErrorPosition = pSpellArgs->pStartPos->GetContentIndex() > pSpellArgs->pEndPos->GetContentIndex() ?
                                             pSpellArgs->pEndPos->GetContentIndex() :
                                             pSpellArgs->pStartPos->GetContentIndex();
+                            if( nCurrNd != nEndNd )
+                            {
+                                pSttPos->Assign(nCurrNd);
+                                pEndPos->Assign(nCurrNd);
+                                nCurrNd = nEndNd;
+                            }
                         }
 
                         if( pSpellArgs && pSpellArgs->bIsGrammarCheck )
@@ -649,8 +653,8 @@ uno::Any SwDoc::Spell( SwPaM& rPaM,
                                     //put the cursor to the current error
                                     const linguistic2::SingleProofreadingError &rError = aResult.aErrors[0];
                                     nCurrNd = pNd->GetIndex();
-                                    pSttPos->nNode = nCurrNd;
-                                    pEndPos->nNode = nCurrNd;
+                                    pSttPos->Assign(nCurrNd);
+                                    pEndPos->Assign(nCurrNd);
                                     pSpellArgs->pStartPos->Assign(*pNd->GetTextNode(), aConversionMap.ConvertToModelPosition( rError.nErrorStart ).mnPos );
                                     pSpellArgs->pEndPos->Assign(*pNd->GetTextNode(), aConversionMap.ConvertToModelPosition( rError.nErrorStart + rError.nErrorLength ).mnPos );
                                     nCurrNd = nEndNd;

commit bb97bfe3168916ec54404a35f1f0d66d8c06c50d

tdf#153628 sw: fix locked spell checking in spelling dialog
Which resulted incomplete spell checking finished before
checking the document.

Regression from commit b484e5d9e0d22b794c2d1c73741191836158a4ad
"use more SwPosition::Assign".

Follow-up to commit 2780b026f912a103aea3f8dc19319b998d19a43a
"tdf#152738 sw: fix lost SwPosition in spelling dialog" and
commit 221d76260096b9e6b4c4479b1b89c95af8b05774 "simplify assignment".

Change-Id: Ib66903596847016ddaf68da42813dd3a839adb59
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/149633
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/source/core/doc/docedt.cxx
@@ -652,8 +652,10 @@ uno::Any SwDoc::Spell( SwPaM& rPaM,
                                     aRet <<= aResult;
                                     //put the cursor to the current error
                                     const linguistic2::SingleProofreadingError &rError = aResult.aErrors[0];
-                                    pSttPos->Assign(*pNd->GetTextNode(), aConversionMap.ConvertToModelPosition( rError.nErrorStart ).mnPos );
-                                    pEndPos->Assign(*pNd->GetTextNode(), aConversionMap.ConvertToModelPosition( rError.nErrorStart + rError.nErrorLength ).mnPos );
+                                    pSttPos->Assign(nCurrNd, pSttPos->GetContentIndex());
+                                    pEndPos->Assign(nCurrNd, pEndPos->GetContentIndex());
+                                    pSpellArgs->pStartPos->Assign(*pNd->GetTextNode(), aConversionMap.ConvertToModelPosition( rError.nErrorStart ).mnPos );
+                                    pSpellArgs->pEndPos->Assign(*pNd->GetTextNode(), aConversionMap.ConvertToModelPosition( rError.nErrorStart + rError.nErrorLength ).mnPos );
                                     nCurrNd = nEndNd;
                                 }
                             }