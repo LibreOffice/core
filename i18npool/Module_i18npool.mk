d502df6e8179744ded92f87addbaceb587b2b3c8
Stephan Bergmann <sbergman@redhat.com>
2023-04-04 11:16:00 +0200

    tdf#151971: Fix used implementation names of transliteration services
    ...after 04af4e4f55f3ef319a78edd4d0109e2e7eba90b6 "[API CHANGE] Fix all bad
    UNOIDL identifiers across offapi" had changed the spelling (character case) of
    some of the css.i18n.TransliterationModules[New] enum values involved here, so
    that the TmItem1 macro generated broken TMList::implName values now.  (Which in
    turn caused TransliterationImpl::loadBody to throw "unsatisfied query for
    interface of type com.sun.star.i18n.XExtendedTransliteration!"
    css::uno::RuntimeExceptions, which remained uncaught.)
    
    Also add a test verifying that loading all those transliteration services no
    longer fails throwing exceptions.  Which lead to two open TODOs:  For one, the
    value of maxCascade in i18npool/inc/transliterationImpl.hxx might come from a
    time when there were fewer TransliterationModules[New] enum values and might no
    longer be appropriate.  This would need some further investigation.  But for
    another, there are two transliteration services that cannot currently be
    instantiated.  That looks like a regression that should be fixed in a follow-up
    commit.
    
    Change-Id: Icfca3e841360d4b471013e2c96d6868a75a21a1c
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/150018
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>
    (cherry picked from commit 8e0ea143180c723bb429f6dbdd9ed370c220862b)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/149981
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>

--- i18npool/Module_i18npool.mk
+++ i18npool/Module_i18npool.mk
@@ -45,6 +45,7 @@ $(eval $(call gb_Module_add_check_targets,i18npool,\
 	CppunitTest_i18npool_textsearch \
 	CppunitTest_i18npool_calendar \
 	CppunitTest_i18npool_defaultnumberingprovider \
+	CppunitTest_i18npool_transliteration \
 ))
 
 # vim: set noet sw=4 ts=4:
