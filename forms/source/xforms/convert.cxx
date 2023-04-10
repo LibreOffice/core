LibreOffice/core

commit 73334560b2dd2d60ac58d2cc2b1a5295490b03e1

Replace some macros in forms part 16
Remove DEFAULT_DECLARE_SUBTYPE + EXCEPT + EXPAND_PROP_INIT
+ DECLARE_DEFAULT_CLONE_CTOR + DECLARE_DEFAULT_LEAF_XTOR + FORWARD_TO_PEER_1
+ IMPLEMENT_DEFAULT_CLONING
+ ADD_ENTRY (include removing:
/home/julien/lo/libreoffice/forms/source/xforms/convert.cxx:56:14: error: unused function 'lcl_toXSD_bool' [-Werror,-Wunused-function]
    OUString lcl_toXSD_bool( const Any& rAny )
             ^
/home/julien/lo/libreoffice/forms/source/xforms/convert.cxx:60:9: error: unused function 'lcl_toAny_bool' [-Werror,-Wunused-function]
    Any lcl_toAny_bool( const OUString& rStr )
        ^
/home/julien/lo/libreoffice/forms/source/xforms/convert.cxx:67:14: error: unused function 'lcl_toXSD_double' [-Werror,-Wunused-function]
    OUString lcl_toXSD_double( const Any& rAny )
             ^
/home/julien/lo/libreoffice/forms/source/xforms/convert.cxx:80:9: error: unused function 'lcl_toAny_double' [-Werror,-Wunused-function]
    Any lcl_toAny_double( const OUString& rString )
        ^
)

Change-Id: I1e6923922b7037e8d1e9d009b05a27111c94b6fa
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/124827
Tested-by: Jenkins
Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

forms/source/xforms/convert.cxx
@@ -42,8 +42,6 @@ Convert::Convert()
     init();
 }
 
-#define ADD_ENTRY(XCONVERT,TYPE) XCONVERT->maMap[ cppu::UnoType<TYPE>::get() ] = Convert_t( &lcl_toXSD_##TYPE, &lcl_toAny_##TYPE )
-
 namespace
 {
 
@@ -53,40 +51,6 @@ namespace
 
     Any lcl_toAny_OUString( const OUString& rStr )
     { return Any(rStr); }
-
-
-    OUString lcl_toXSD_bool( const Any& rAny )
-    { bool b = false; rAny >>= b; return b ? OUString("true") : OUString("false"); }
-
-
-    Any lcl_toAny_bool( const OUString& rStr )
-    {
-        bool b = ( rStr == "true"  ||  rStr == "1" );
-        return makeAny( b );
-    }
-
-
-    OUString lcl_toXSD_double( const Any& rAny )
-    {
-        double f = 0.0;
-        rAny >>= f;
-
-        return std::isfinite( f )
-            ? rtl::math::doubleToUString( f, rtl_math_StringFormat_Automatic,
-                                        rtl_math_DecimalPlaces_Max, '.',
-                                        true )
-            : OUString();
-    }
-
-
-    Any lcl_toAny_double( const OUString& rString )
-    {
-        rtl_math_ConversionStatus eStatus;
-        double f = rtl::math::stringToDouble(
-            rString, '.', ',', &eStatus );
-        return ( eStatus == rtl_math_ConversionStatus_Ok ) ? makeAny( f ) : Any();
-    }
-
 
     void lcl_appendInt32ToBuffer( const sal_Int32 _nValue, OUStringBuffer& _rBuffer, sal_Int16 _nMinDigits )
     {
@@ -268,9 +232,7 @@ namespace
 
 void Convert::init()
 {
-    ADD_ENTRY( this, OUString );
-    ADD_ENTRY( this, bool );
-    ADD_ENTRY( this, double );
+    maMap[ cppu::UnoType<OUString>::get() ] = Convert_t(&lcl_toXSD_OUString, &lcl_toAny_OUString);
     maMap[ cppu::UnoType<css::util::Date>::get() ] = Convert_t( &lcl_toXSD_UNODate, &lcl_toAny_UNODate );
     maMap[ cppu::UnoType<css::util::Time>::get() ] = Convert_t( &lcl_toXSD_UNOTime, &lcl_toAny_UNOTime );
     maMap[ cppu::UnoType<css::util::DateTime>::get() ] = Convert_t( &lcl_toXSD_UNODateTime, &lcl_toAny_UNODateTime );

forms/source/component/Button.cxx
@@ -115,7 +115,13 @@ void OButtonModel::describeFixedProperties( Sequence< Property >& _rProps ) cons
 }
 
 
-IMPLEMENT_DEFAULT_CLONING( OButtonModel )
+css::uno::Reference< css::util::XCloneable > SAL_CALL OButtonModel::createClone()
+{
+    rtl::Reference<OButtonModel> pClone = new OButtonModel(this, getContext());
+    pClone->clonedFrom(this);
+    return pClone;
+}
+
 
 // XServiceInfo
 

forms/source/component/Button.hxx
@@ -45,7 +45,14 @@ class OButtonModel  :public OClickableImageBaseModel
                     ,public OButtonModel_Base
 {
 public:
-    DECLARE_DEFAULT_LEAF_XTOR( OButtonModel );
+    OButtonModel(
+        const css::uno::Reference< css::uno::XComponentContext>& _rxFactory
+    );
+    OButtonModel(
+        const OButtonModel* _pOriginal,
+        const css::uno::Reference< css::uno::XComponentContext>& _rxFactory
+    );
+    virtual ~OButtonModel() override;
 
     // UNO
     DECLARE_UNO3_AGG_DEFAULTS( OButtonModel, OClickableImageBaseModel )

forms/source/component/CheckBox.cxx
@@ -78,7 +78,13 @@ OCheckBoxModel::~OCheckBoxModel()
 }
 
 
-IMPLEMENT_DEFAULT_CLONING( OCheckBoxModel )
+css::uno::Reference< css::util::XCloneable > SAL_CALL OCheckBoxModel::createClone()
+{
+    rtl::Reference<OCheckBoxModel> pClone = new OCheckBoxModel(this, getContext());
+    pClone->clonedFrom(this);
+    return pClone;
+}
+
 
 // XServiceInfo
 

forms/source/component/CheckBox.hxx
@@ -30,7 +30,14 @@ class OCheckBoxModel final : public OReferenceValueComponent
     bool        DbUseBool();
 
 public:
-    DECLARE_DEFAULT_LEAF_XTOR( OCheckBoxModel );
+    OCheckBoxModel(
+        const css::uno::Reference< css::uno::XComponentContext>& _rxFactory
+    );
+    OCheckBoxModel(
+        const OCheckBoxModel* _pOriginal,
+        const css::uno::Reference< css::uno::XComponentContext>& _rxFactory
+    );
+    virtual ~OCheckBoxModel() override;
 
     // XServiceInfo
     OUString SAL_CALL getImplementationName() override

forms/source/component/ComboBox.cxx
@@ -144,7 +144,12 @@ OComboBoxModel::~OComboBoxModel()
 
 // XCloneable
 
-IMPLEMENT_DEFAULT_CLONING( OComboBoxModel )
+css::uno::Reference< css::util::XCloneable > SAL_CALL OComboBoxModel::createClone()
+{
+    rtl::Reference<OComboBoxModel> pClone = new OComboBoxModel(this, getContext());
+    pClone->clonedFrom(this);
+    return pClone;
+}
 
 
 void OComboBoxModel::disposing()

forms/source/component/ComboBox.hxx
@@ -54,7 +54,14 @@ class OComboBoxModel final
     virtual css::uno::Sequence< css::uno::Type>   _getTypes() override;
 
 public:
-    DECLARE_DEFAULT_LEAF_XTOR( OComboBoxModel );
+    OComboBoxModel(
+        const css::uno::Reference< css::uno::XComponentContext>& _rxFactory
+    );
+    OComboBoxModel(
+        const OComboBoxModel* _pOriginal,
+        const css::uno::Reference< css::uno::XComponentContext>& _rxFactory
+    );
+    virtual ~OComboBoxModel() override;
 
     virtual void SAL_CALL disposing() override;
 

forms/source/component/Currency.cxx
@@ -130,7 +130,13 @@ OCurrencyModel::~OCurrencyModel()
 
 // XCloneable
 
-IMPLEMENT_DEFAULT_CLONING( OCurrencyModel )
+css::uno::Reference< css::util::XCloneable > SAL_CALL OCurrencyModel::createClone()
+{
+    rtl::Reference<OCurrencyModel> pClone = new OCurrencyModel(this, getContext());
+    pClone->clonedFrom(this);
+    return pClone;
+}
+
 
 // XServiceInfo
 

commit 26012007e4d20f47697d1336b725e60541df794a

tdf#154658: XML Form Document: Fields doesnt calculate any more
Regression from 73334560b2dd2d60ac58d2cc2b1a5295490b03e1
author	Julien Nabet <serval2412@yahoo.fr>	2021-11-07 15:40:37 +0100
committer	Julien Nabet <serval2412@yahoo.fr>	2021-11-07 21:58:53 +0100
commit	73334560b2dd2d60ac58d2cc2b1a5295490b03e1 (patch)
tree	b5bc4f69dd8ed455c78ea05ab1c5e2f3c25b909e
parent	6be03ac71e0d4927612b4a57ead3d0b245c29c77 (diff)
Replace some macros in forms part 16

A big thank you to Raal for having spotted this!

Change-Id: Ib6f1878897b16b43df287702f82835824c1f766d
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/150174
Reviewed-by: Julien Nabet <serval2412@yahoo.fr>
Tested-by: Jenkins

forms/source/xforms/convert.cxx
@@ -21,6 +21,7 @@
 #include "convert.hxx"
 
 #include <sstream>
+#include <rtl/math.hxx>
 #include <rtl/ustrbuf.hxx>
 #include <osl/diagnose.h>
 #include <tools/date.hxx>
@@ -49,6 +50,36 @@ namespace
 
     Any lcl_toAny_OUString( const OUString& rStr )
     { return Any(rStr); }
+
+    OUString lcl_toXSD_bool( const Any& rAny )
+    { bool b = false; rAny >>= b; return b ? OUString("true") : OUString("false"); }
+
+    Any lcl_toAny_bool( const OUString& rStr )
+    {
+        bool b = ( rStr == "true"  ||  rStr == "1" );
+        return Any( b );
+    }
+
+    OUString lcl_toXSD_double( const Any& rAny )
+    {
+        double f = 0.0;
+        rAny >>= f;
+
+        return std::isfinite( f )
+            ? rtl::math::doubleToUString( f, rtl_math_StringFormat_Automatic,
+                                        rtl_math_DecimalPlaces_Max, '.',
+                                        true )
+            : OUString();
+    }
+
+
+    Any lcl_toAny_double( const OUString& rString )
+    {
+        rtl_math_ConversionStatus eStatus;
+        double f = rtl::math::stringToDouble(
+            rString, '.', ',', &eStatus );
+        return ( eStatus == rtl_math_ConversionStatus_Ok ) ? Any( f ) : Any();
+    }
 
     void lcl_appendInt32ToBuffer( const sal_Int32 _nValue, OUStringBuffer& _rBuffer, sal_Int16 _nMinDigits )
     {
@@ -231,6 +262,8 @@ namespace
 void Convert::init()
 {
     maMap[ cppu::UnoType<OUString>::get() ] = Convert_t(&lcl_toXSD_OUString, &lcl_toAny_OUString);
+    maMap[ cppu::UnoType<bool>::get() ] = Convert_t(&lcl_toXSD_bool, &lcl_toAny_bool);
+    maMap[ cppu::UnoType<double>::get() ] = Convert_t(&lcl_toXSD_double, &lcl_toAny_double);
     maMap[ cppu::UnoType<css::util::Date>::get() ] = Convert_t( &lcl_toXSD_UNODate, &lcl_toAny_UNODate );
     maMap[ cppu::UnoType<css::util::Time>::get() ] = Convert_t( &lcl_toXSD_UNOTime, &lcl_toAny_UNOTime );
     maMap[ cppu::UnoType<css::util::DateTime>::get() ] = Convert_t( &lcl_toXSD_UNODateTime, &lcl_toAny_UNODateTime );