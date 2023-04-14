LibreOffice/core

commit f7e071a00542c414a7e9d7bcf4434d908f225e59

tdf#88496 DOCX: disable long repeating table header
if the pages could contain only that, hiding the
non-repeating table rows. This behavior is similar
to MSO.

See also commit 110781a3a27dffe9e6690839bdce993796a08331
(tdf#58944 DOCX import: workaround for hidden table headers).

Change-Id: I646be45c6d2c5fe9e1df0badeee4583097dc79f1
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/86928
Reviewed-by: László Németh <nemeth@numbertext.org>
Tested-by: László Németh <nemeth@numbertext.org>

sw/source/core/layout/tabfrm.cxx
@@ -1074,10 +1074,11 @@ bool SwTabFrame::Split( const SwTwips nCutPos, bool bTryToSplit, bool bTableRowK
     if ( nRowCount < nRepeat )
     {
         // First case: One of the repeated headline does not fit to the page anymore.
-        // At least one more non-heading row has to stay in this table in
-        // order to avoid loops:
+        // tdf#88496 Disable repeated headline (like for #i44910#) to avoid loops and
+        // to fix interoperability problems (very long tables only with headline)
         OSL_ENSURE( !GetIndPrev(), "Table is supposed to be at beginning" );
-        bKeepNextRow = true;
+        m_pTable->SetRowsToRepeat(0);
+        return false;
     }
     else if ( !GetIndPrev() && nRepeat == nRowCount )
     {

sw/qa/extras/ooxmlexport/ooxmlexport11.cxx
@@ -1021,6 +1021,16 @@ DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testTdf81100, "tdf81100.docx")
         assertXPath(pDump, "/root/page[3]/body/tab/row", 1);
 }
 
+DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testTdf88496, "tdf88496.docx")
+{
+    xmlDocPtr pXmlDoc = parseExport("word/styles.xml");
+    CPPUNIT_ASSERT(pXmlDoc);
+    // Switch off repeating header, there is no place for it.
+    // Now there are only 3 pages with complete table content
+    // instead of a 51-page long table only with header.
+    CPPUNIT_ASSERT_EQUAL(3, getPages());
+}
+
 DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testTdf121597TrackedDeletionOfMultipleParagraphs, "tdf121597.odt")
 {
     xmlDocPtr pXmlDoc = parseExport("word/document.xml");

commit a36fda0d7c70ffd344f9636ca436e77e41dc01f1

tdf#150149 sw: fix table header in multi-column sections
Disabling long repeating table header on pages resulted
lost table header within multi-column sections. To fix this,
revert commit f7e071a00542c414a7e9d7bcf4434d908f225e59
for tables in sections.

Regression from commit f7e071a00542c414a7e9d7bcf4434d908f225e59
"tdf#88496 DOCX: disable long repeating table header".

Change-Id: Idb7b9ea014be5430a185489cf449463f534c1916
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/150393
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/source/core/layout/tabfrm.cxx
@@ -1113,9 +1113,16 @@ bool SwTabFrame::Split( const SwTwips nCutPos, bool bTryToSplit, bool bTableRowK
         // First case: One of the repeated headline does not fit to the page anymore.
         // tdf#88496 Disable repeated headline (like for #i44910#) to avoid loops and
         // to fix interoperability problems (very long tables only with headline)
+        // tdf#150149 except in multi-column sections, where it's possible to enlarge
+        // the height of the section frame instead of using this fallback
         OSL_ENSURE( !GetIndPrev(), "Table is supposed to be at beginning" );
-        m_pTable->SetRowsToRepeat(0);
-        return false;
+        if ( !IsInSct() )
+        {
+            m_pTable->SetRowsToRepeat(0);
+            return false;
+        }
+        else
+            bKeepNextRow = true;
     }
     else if ( !GetIndPrev() && nRepeat == nRowCount )
     {

sw/qa/extras/odfexport/data/tdf150149.fodt
@@ -1,0 +1,79 @@
+<?xml version="1.0" encoding="UTF-8"?>
+
+<office:document xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0" xmlns:fo="urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0" xmlns:style="urn:oasis:names:tc:opendocument:xmlns:style:1.0" xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0" xmlns:table="urn:oasis:names:tc:opendocument:xmlns:table:1.0" office:version="1.3" office:mimetype="application/vnd.oasis.opendocument.text">
+ <office:automatic-styles>
+  <style:style style:name="Table1" style:family="table">
+   <style:table-properties style:width="5cm" table:align="left"/>
+  </style:style>
+  <style:style style:name="Sect1" style:family="section">
+   <style:section-properties text:dont-balance-text-columns="false" style:editable="false">
+    <style:columns fo:column-count="3" fo:column-gap="0.5cm">
+    </style:columns>
+   </style:section-properties>
+  </style:style>
+ </office:automatic-styles>
+ <office:body>
+  <office:text>
+   <text:section text:style-name="Sect1" text:name="Section1">
+    <table:table table:name="Table1" table:style-name="Table1">
+     <table:table-column table:number-columns-repeated="3"/>
+     <table:table-header-rows>
+     <table:table-row>
+      <table:table-cell>
+       <text:p>A</text:p>
+      </table:table-cell>
+      <table:table-cell>
+       <text:p>B</text:p>
+      </table:table-cell>
+      <table:table-cell>
+       <text:p>C</text:p>
+      </table:table-cell>
+     </table:table-row>
+     </table:table-header-rows>
+     <table:table-row>
+      <table:table-cell/>
+      <table:table-cell/>
+      <table:table-cell/>
+     </table:table-row>
+     <table:table-row>
+      <table:table-cell/>
+      <table:table-cell/>
+      <table:table-cell/>
+     </table:table-row>
+     <table:table-row>
+      <table:table-cell/>
+      <table:table-cell/>
+      <table:table-cell/>
+     </table:table-row>
+     <table:table-row>
+      <table:table-cell/>
+      <table:table-cell/>
+      <table:table-cell/>
+     </table:table-row>
+     <table:table-row>
+      <table:table-cell/>
+      <table:table-cell/>
+      <table:table-cell/>
+     </table:table-row>
+     <table:table-row>
+      <table:table-cell/>
+      <table:table-cell/>
+      <table:table-cell/>
+     </table:table-row>
+     <table:table-row>
+      <table:table-cell/>
+      <table:table-cell/>
+      <table:table-cell/>
+     </table:table-row>
+     <table:table-row>
+      <table:table-cell/>
+      <table:table-cell/>
+      <table:table-cell/>
+     </table:table-row>
+    </table:table>
+    <text:p/>
+   </text:section>
+  </office:text>
+ </office:body>
+</office:document>
+

sw/qa/extras/odfexport/odfexport.cxx
@@ -290,6 +290,16 @@ CPPUNIT_TEST_FIXTURE(Test, testTdf125877)
     CPPUNIT_ASSERT_EQUAL(sal_Int32(1), xIndexAccess->getCount());
 }
 
+CPPUNIT_TEST_FIXTURE(Test, testTdf150149)
+{
+    loadAndReload("tdf150149.fodt");
+    CPPUNIT_ASSERT_EQUAL(1, getPages());
+    xmlDocUniquePtr pXmlDoc = parseExport("content.xml");
+    // This was 0 (lost table header in multi-column section)
+    assertXPath(pXmlDoc, "//table:table-header-rows", 1);
+    assertXPath(pXmlDoc, "//table:table-header-rows/table:table-row/table:table-cell", 3);
+}
+
 DECLARE_ODFEXPORT_TEST(testTdf103567, "tdf103567.odt")
 {
     CPPUNIT_ASSERT_EQUAL(1, getShapes());