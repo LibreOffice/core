3ec73488e447a693a14a773a7fb96938036c0324
Caolán McNamara <caolanm@redhat.com>
2023-04-19 16:55:34 +0100

    Resolves: tdf#154912 make double-click to expand rows the same as arrow expand
    the arrow case does the necessary removal of the "dummy" entry which is
    necessary to get the treeview to indicate it has children, while the
    double click case skipped that step, so rearrange so both behave the
    same.
    
    Change-Id: I6161d786cd58b0eea53152074dff2b035c621e7f
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/150643
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>

--- vcl/source/app/salvtables.cxx
+++ vcl/source/app/salvtables.cxx
@@ -4815,7 +4815,7 @@ void SalInstanceTreeView::expand_row(const weld::TreeIter& rIter)
 {
     assert(m_xTreeView->IsUpdateMode() && "don't expand when frozen");
     const SalInstanceTreeIter& rVclIter = static_cast<const SalInstanceTreeIter&>(rIter);
-    if (!m_xTreeView->IsExpanded(rVclIter.iter) && signal_expanding(rIter))
+    if (!m_xTreeView->IsExpanded(rVclIter.iter) && ExpandRow(rVclIter))
         m_xTreeView->Expand(rVclIter.iter);
 }
 
@@ -5329,7 +5329,12 @@ IMPL_LINK_NOARG(SalInstanceTreeView, ExpandingHdl, SvTreeListBox*, bool)
     }
 
     // expanding
+    return ExpandRow(aIter);
+}
 
+bool SalInstanceTreeView::ExpandRow(const SalInstanceTreeIter& rIter)
+{
+    SvTreeListEntry* pEntry = rIter.iter;
     // if there's a preexisting placeholder child, required to make this
     // potentially expandable in the first place, now we remove it
     SvTreeListEntry* pPlaceHolder = GetPlaceHolderChild(pEntry);
@@ -5339,7 +5344,7 @@ IMPL_LINK_NOARG(SalInstanceTreeView, ExpandingHdl, SvTreeListBox*, bool)
         m_xTreeView->RemoveEntry(pPlaceHolder);
     }
 
-    bool bRet = signal_expanding(aIter);
+    bool bRet = signal_expanding(rIter);
 
     if (pPlaceHolder)
     {
