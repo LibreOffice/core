LibreOffice/core

commit 8611f6e259b807b4f19c8dc0eab86ca648891ce3

ref-count SdrObject
Which means we can get rid of the majestic hack of ScCaptionPtr
Previously, SdrObject was manually managed, and the ownership
passed around in very complicated fashion.

Notes:

(*) SvxShape has a strong reference to SdrObject, where
previously it had a weak reference. It is now strong
since otherwise the SdrObject will go away very eagerly.

(*) SdrObject still has a weak reference to SvxShape

(*) In the existing places that an SdrObject is being
deleted, we now just clear the reference

(*) instead of SwVirtFlyDrawObj removing itself from the
page that contains inside it's destructor, make the call site
do the removing from the page.

(*) Needed to take the SolarMutex in UndoManagerHelper_Impl::impl_clear
because this can be called from UNO (e.g. sfx2_complex JUnit test)
and the SdrObjects need the SolarMutex when destructing.

(*) handle a tricky situation with SwDrawVirtObj in the SwDrawModel
destructor because the existing code wants mpDrawObj in
SwAnchoredObject to be sometimes owning, sometimes not, which
results in a cycle with the new code.

Change-Id: I4d79df1660e386388e5d51030653755bca02a163
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/138837
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sw/inc/unodraw.hxx
@@ -43,7 +43,7 @@ class SwXShape;
 class SwFmDrawPage final : public SvxFmDrawPage
 {
     SdrPageView*        m_pPageView;
-    std::vector<SwXShape*> m_vShapes;
+    std::vector<rtl::Reference<SwXShape>> m_vShapes;
 public:
     SwFmDrawPage( SdrPage* pPage );
     virtual ~SwFmDrawPage() noexcept override;

sw/source/core/unocore/unodraw.cxx
@@ -298,11 +298,11 @@ uno::Reference<drawing::XShape> SwFmDrawPage::GetShape(SdrObject* pObj)
     SwFmDrawPage* pPage = dynamic_cast<SwFmDrawPage*>(pFormat);
     if(!pPage || pPage->m_vShapes.empty())
         return uno::Reference<drawing::XShape>(pObj->getUnoShape(), uno::UNO_QUERY);
-    for(auto pShape : pPage->m_vShapes)
+    for(const auto & pShape : pPage->m_vShapes)
     {
         SvxShape* pSvxShape = pShape->GetSvxShape();
         if (pSvxShape && pSvxShape->GetSdrObject() == pObj)
-            return uno::Reference<drawing::XShape>(static_cast<::cppu::OWeakObject*>(pShape), uno::UNO_QUERY);
+            return pShape;
     }
     return nullptr;
 }
@@ -373,7 +373,7 @@ uno::Reference< drawing::XShape > SwFmDrawPage::CreateShape( SdrObject *pObj ) c
                 pShape = new SwXShape(xCreate, nullptr);
             xRet = pShape;
         }
-        const_cast<std::vector<SwXShape*>*>(&m_vShapes)->push_back(pShape.get());
+        const_cast<std::vector<rtl::Reference<SwXShape>>*>(&m_vShapes)->push_back(pShape);
         pShape->m_pPage = this;
     }
     return xRet;
@@ -961,7 +961,6 @@ SwXShape::~SwXShape()
     EndListeningAll();
     if(m_pPage)
        const_cast<SwFmDrawPage*>(m_pPage)->RemoveShape(this);
-    m_pPage = nullptr;
 }
 
 uno::Any SwXShape::queryInterface( const uno::Type& aType )
@@ -2182,8 +2181,11 @@ void SwXShape::dispose()
             xComp->dispose();
     }
     if(m_pPage)
-        const_cast<SwFmDrawPage*>(m_pPage)->RemoveShape(this);
-    m_pPage = nullptr;
+    {
+        auto pPage = const_cast<SwFmDrawPage*>(m_pPage);
+        m_pPage = nullptr;
+        pPage->RemoveShape(this);
+    }
 }
 
 void SwXShape::addEventListener(

commit 40babcfa637957bf7b59caa3cd12a630189e3e63

tdf#154827 and tdf#154428 graphics anchored in Writer as character..
..become anchored at paragraph when reopening the document

regression from
    commit 8611f6e259b807b4f19c8dc0eab86ca648891ce3
    ref-count SdrObject

This used to work by accident, previously, the SwXGroupShape that we
need would get created and destroyed and handful of times, and then
properly created.

Now, however, the first time we create it it sticks around, which means
it does not get the m_pFormat field configured correctly.

Fix this by removing the caching of m_pFormat from SwXShape, and just
fetching it from the model when we need it.

Change-Id: I10fe2ad0b877b3d928d5288e73cfed373b27a506
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/150687
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sw/inc/unodraw.hxx
@@ -132,7 +132,6 @@ class SwXShape : public SwXShapeBaseClass, public SvtListener
     friend class SwXGroupShape;
     friend class SwFmDrawPage;
     const SwFmDrawPage* m_pPage;
-    SwFrameFormat* m_pFormat;
 
     css::uno::Reference< css::uno::XAggregation > m_xShapeAgg;
     // reference to <XShape>, determined in the
@@ -195,12 +194,6 @@ class SwXShape : public SwXShapeBaseClass, public SvtListener
         @throws css::uno::RuntimeException
     */
     css::uno::Any _getPropAtAggrObj( const OUString& _rPropertyName );
-    void SetFrameFormat(SwFrameFormat* pFormat)
-    {
-        EndListeningAll();
-        StartListening(pFormat->GetNotifier());
-        m_pFormat = pFormat;
-    }
 
 protected:
     virtual ~SwXShape() override;
@@ -252,7 +245,7 @@ public:
     virtual OUString SAL_CALL getShapeType(  ) override;
 
     SwShapeDescriptor_Impl*     GetDescImpl() {return m_pImpl.get();}
-    SwFrameFormat* GetFrameFormat() const { return m_pFormat; }
+    SwFrameFormat* GetFrameFormat() const;
     const css::uno::Reference< css::uno::XAggregation >& GetAggregationInterface() const {return m_xShapeAgg;}
 
     // helper

sw/source/core/unocore/unodraw.cxx
@@ -639,7 +639,7 @@ void SwFmDrawPage::add(const uno::Reference< drawing::XShape > & xShape)
                                     static_cast< cppu::OWeakObject * > ( this ) );
 
     // we're already registered in the model / SwXDrawPage::add() already called
-    if(pShape->m_pPage || pShape->m_pFormat || !pShape->m_bDescriptor )
+    if(pShape->m_pPage || !pShape->m_bDescriptor )
         return;
 
     // we're inserted elsewhere already
@@ -787,7 +787,6 @@ void SwFmDrawPage::add(const uno::Reference< drawing::XShape > & xShape)
         {
             pFormat->SetFormatName(pSvxShape->GetSdrObject()->GetName(), false);
         }
-        pShape->SetFrameFormat(pFormat);
     }
     pShape->m_bDescriptor = false;
 
@@ -917,7 +916,6 @@ SwXShape::SwXShape(
         uno::Reference<uno::XInterface> & xShape,
         SwDoc const*const pDoc)
     : m_pPage(nullptr)
-    , m_pFormat(nullptr)
     , m_pPropSet(aSwMapProvider.GetPropertySet(PROPERTY_MAP_TEXT_SHAPE))
     , m_pPropertyMapEntries(aSwMapProvider.GetPropertyMapEntries(PROPERTY_MAP_TEXT_SHAPE))
     , m_pImpl(new SwShapeDescriptor_Impl(pDoc))
@@ -948,14 +946,18 @@ SwXShape::SwXShape(
     SdrObject* pObj = SdrObject::getSdrObjectFromXShape(m_xShapeAgg);
     if(pObj)
     {
-        auto pFormat = ::FindFrameFormat( pObj );
-        if(pFormat)
-            SetFrameFormat(pFormat);
-
         lcl_addShapePropertyEventFactories( *pObj, *this );
         m_pImpl->m_bInitializedPropertyNotifier = true;
     }
 
+}
+
+SwFrameFormat* SwXShape::GetFrameFormat() const
+{
+    SdrObject* pObj = SdrObject::getSdrObjectFromXShape(m_xShapeAgg);
+    if(pObj)
+        return ::FindFrameFormat( pObj );
+    return nullptr;
 }
 
 void SwXShape::AddExistingShapeToFormat( SdrObject const & _rObj )
@@ -972,12 +974,7 @@ void SwXShape::AddExistingShapeToFormat( SdrObject const & _rObj )
         if ( pSwShape )
         {
             if ( pSwShape->m_bDescriptor )
-            {
-                auto pFormat = ::FindFrameFormat( pCurrent );
-                if ( pFormat )
-                    pSwShape->SetFrameFormat(pFormat);
                 pSwShape->m_bDescriptor = false;
-            }
 
             if ( !pSwShape->m_pImpl->m_bInitializedPropertyNotifier )
             {
@@ -2091,7 +2088,6 @@ void SwXShape::Notify(const SfxHint& rHint)
 {
     if(rHint.GetId() == SfxHintId::Dying)
     {
-        m_pFormat = nullptr;
         EndListeningAll();
     }
 }
@@ -2835,12 +2831,6 @@ void SwXGroupShape::add( const uno::Reference< XShape >& xShape )
         }
     }
     pSwShape->m_bDescriptor = false;
-    //add the group member to the format of the group
-    SwFrameFormat* pShapeFormat = ::FindFrameFormat( pSvxShape->GetSdrObject() );
-
-    if(pShapeFormat)
-        pSwShape->SetFrameFormat(pShapeFormat);
-
 }
 
 void SwXGroupShape::remove( const uno::Reference< XShape >& xShape )