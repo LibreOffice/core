8c904d45f3f7ac62056dcd4327394bdfd56432d6
Caolán McNamara <caolanm@redhat.com>
2023-04-20 20:58:21 +0100

    assume IFrame script/macro support isn't needed
    seems undocumented at least
    
    Change-Id: I316e4f4f25ddb7cf6b7bac4d856a721b987207a3
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/151019
    Tested-by: Jenkins
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- sfx2/source/inc/eventsupplier.hxx
+++ sfx2/source/inc/eventsupplier.hxx
@@ -81,6 +81,7 @@ public:
                                     SfxObjectShell* i_document );
     static void Execute( css::uno::Sequence < css::beans::PropertyValue > const & aEventData, const css::document::DocumentEvent& aTrigger, SfxObjectShell* pDoc );
 
+private:
     /// Check if script URL whitelist exists, and if so, if current script url is part of it
     static bool isScriptURLAllowed(const OUString& aScriptURL);
 };
