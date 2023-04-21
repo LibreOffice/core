LibreOffice/core

commit 3a14b2040ecd95a23acbc3e8d0f8087505135f11

tdf#148032 sw: add redline comment for tracked comments
Tracked insertion/deletion of comments were shown as
plain text insertion/deletion in that case, too, when
there is no other text change in the redline (only
the invisible placeholder character CH_TXTATR_INWORD
of the comment). This was fixed by the new action
icons of the Manage Changes dialog window before,
but for better accessibility, now redline of a newly
inserted/deleted comment gets also a redline comment
"Comment added"/"Comment deleted", shown in Manage
Changes dialog window with the Comment added/Comment
deleted action icons, as suggested by Heiko Tietze.

Note: because the redline comment (at least the
tracked insertions) are stored in the document,
this is a back-compatible solution.

Follow-up to commit 128d3b454467e34538903b2eab45e866f976b314
"tdf#73125 sw: add Insert/Delete Comment Manage Changes action icons".

Change-Id: I7a9e82ae1b9519e4883956f69de0aafc289f7f99
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/131659
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/qa/uitest/writer_tests/trackedChanges.py
@@ -10,6 +10,7 @@
 # tests for tracked changes ; tdf912270
 from uitest.framework import UITestCase
 from uitest.uihelper.common import get_state_as_dict, get_url_for_data_file, type_text
+from libreoffice.uno.propertyvalue import mkPropertyValues
 
 class trackedchanges(UITestCase):
 
@@ -321,5 +322,35 @@ class trackedchanges(UITestCase):
 
             tables = document.getTextTables()
             self.assertEqual(3, len(tables))
+
+    def test_tdf148032(self):
+
+        with self.ui_test.load_file(get_url_for_data_file("trackedChanges.odt")) as document:
+
+            xWriterDoc = self.xUITest.getTopFocusWindow()
+            xWriterEdit = xWriterDoc.getChild("writer_edit")
+
+            # adding new Comment
+            self.xUITest.executeCommand(".uno:InsertAnnotation")
+
+            # wait until the comment is available
+            xComment1 = self.ui_test.wait_until_child_is_available('Comment1')
+
+            xEditView1 = xComment1.getChild("editview")
+            xEditView1.executeAction("TYPE", mkPropertyValues({"TEXT": "This is the First Comment"}))
+            self.assertEqual(get_state_as_dict(xComment1)["Text"], "This is the First Comment" )
+            self.assertEqual(get_state_as_dict(xComment1)["Resolved"], "false" )
+            self.assertEqual(get_state_as_dict(xComment1)["Author"], "Unknown Author" )
+            self.assertEqual(get_state_as_dict(xComment1)["ReadOnly"], "false" )
+
+            xComment1.executeAction("LEAVE", mkPropertyValues({}))
+
+            with self.ui_test.execute_modeless_dialog_through_command(".uno:AcceptTrackedChanges", close_button="close") as xTrackDlg:
+                changesList = xTrackDlg.getChild("writerchanges")
+                self.assertEqual(6, len(changesList.getChildren()))
+
+                xChild = changesList.getChild(0)
+                # This was False (missing comment)
+                self.assertEqual(True, get_state_as_dict(xChild)["Text"].endswith('\tComment added'))
 
 # vim: set shiftwidth=4 softtabstop=4 expandtab:

sw/inc/redline.hxx
@@ -208,6 +208,9 @@ public:
     OUString const & GetAuthorString( sal_uInt16 nPos = 0 ) const;
     const DateTime& GetTimeStamp( sal_uInt16 nPos = 0) const;
     RedlineType GetType( sal_uInt16 nPos = 0 ) const;
+    // text content of the redline is only an annotation placeholder
+    // (i.e. a comment, but don't confuse it with comment of the redline)
+    bool IsAnnotation() const;
     const OUString& GetComment( sal_uInt16 nPos = 0 ) const;
 
     void SetComment( const OUString& rS ) { m_pRedlineData->SetComment( rS ); }

sw/inc/strings.hrc
@@ -1280,6 +1280,8 @@
 #define STR_REDLINE_FORMATTED                   NC_("STR_REDLINE_FORMATTED", "Formats")
 #define STR_REDLINE_TABLECHG                    NC_("STR_REDLINE_TABLECHG", "Table Changes")
 #define STR_REDLINE_FMTCOLLSET                  NC_("STR_REDLINE_FMTCOLLSET", "Applied Paragraph Styles")
+#define STR_REDLINE_COMMENT_ADDED               NC_("STR_REDLINE_COMMENT_ADDED", "Comment added")
+#define STR_REDLINE_COMMENT_DELETED             NC_("STR_REDLINE_COMMENT_DELETED", "Comment deleted")
 #define STR_PAGE                                NC_("STR_PAGE", "Page ")
 #define STR_PAGE_COUNT                          NC_("STR_PAGE_COUNT", "Page %1 of %2")
 #define STR_PAGE_COUNT_CUSTOM                   NC_("STR_PAGE_COUNT_CUSTOM", "Page %1 of %2 (Page %3)")

sw/source/core/doc/docredln.cxx
@@ -53,6 +53,7 @@
 #include <viscrs.hxx>
 #include <rootfrm.hxx>
 #include <strings.hrc>
+#include <swtypes.hxx>
 #include <wrtsh.hxx>
 #include <txtfld.hxx>
 
@@ -1103,6 +1104,14 @@ SwRangeRedline::SwRangeRedline(RedlineType eTyp, const SwPaM& rPam )
     m_bIsVisible = true;
     if( !rPam.HasMark() )
         DeleteMark();
+
+    // set default comment for single annotations added or deleted
+    if ( IsAnnotation() )
+    {
+        SetComment( RedlineType::Delete == eTyp
+            ? SwResId(STR_REDLINE_COMMENT_DELETED)
+            : SwResId(STR_REDLINE_COMMENT_ADDED) );
+    }
 }
 
 SwRangeRedline::SwRangeRedline( const SwRedlineData& rData, const SwPaM& rPam )
@@ -1910,6 +1919,11 @@ const DateTime& SwRangeRedline::GetTimeStamp( sal_uInt16 nPos ) const
 RedlineType SwRangeRedline::GetType( sal_uInt16 nPos ) const
 {
     return GetRedlineData(nPos).m_eType;
+}
+
+bool SwRangeRedline::IsAnnotation() const
+{
+    return GetText().getLength() == 1 && GetText()[0] == CH_TXTATR_INWORD;
 }
 
 const OUString& SwRangeRedline::GetComment( sal_uInt16 nPos ) const

sw/source/uibase/misc/redlndlg.cxx
@@ -308,16 +308,14 @@ OUString SwRedlineAcceptDlg::GetActionImage(const SwRangeRedline& rRedln, sal_uI
             ? OUString(BMP_REDLINE_ROW_INSERTION)
             : rRedln.IsMoved()
                 ? OUString(BMP_REDLINE_MOVED_INSERTION)
-                : ( rRedln.GetText().getLength() == 1 &&
-                    rRedln.GetText()[0] == CH_TXTATR_INWORD )
+                : rRedln.IsAnnotation()
                     ? OUString(BMP_REDLINE_COMMENT_INSERTION)
                     : OUString(BMP_REDLINE_INSERTED);
         case RedlineType::Delete:  return bRowChanges
             ? OUString(BMP_REDLINE_ROW_DELETION)
             : rRedln.IsMoved()
                 ? OUString(BMP_REDLINE_MOVED_DELETION)
-                : ( rRedln.GetText().getLength() == 1 &&
-                    rRedln.GetText()[0] == CH_TXTATR_INWORD )
+                : rRedln.IsAnnotation()
                     ? OUString(BMP_REDLINE_COMMENT_DELETION)
                     : OUString(BMP_REDLINE_DELETED);
         case RedlineType::Format:  return BMP_REDLINE_FORMATTED;

commit 77ce7aaa30d20abc1e6221a60ce5e69a52bfaf19

tdf#153016 sw DOC: don't export automatic redline comments
DOC format doesn't support comments of change tracking, so DOC
export converts them to annotations. But skip this conversion
in case of automatic redline comments of tracked annotations
("Commend added" and "Comment deleted") to avoid of double
annotations (the original tracked annotation and the newly
added annotation of the comment of the change tracking).

Regression from commit 3a14b2040ecd95a23acbc3e8d0f8087505135f11
"tdf#148032 sw: add redline comment for tracked comments"

Change-Id: I563859d203886a7f81f6a9e1f164ea5524d80d1f
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/150735
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/source/filter/ww8/ww8atr.cxx
@@ -1106,7 +1106,10 @@ void WW8AttributeOutput::StartRun( const SwRedlineData* pRedlineData, sal_Int32 
     {
         const OUString &rComment = pRedlineData->GetComment();
         //Only possible to export to main text
-        if (!rComment.isEmpty() && (m_rWW8Export.m_nTextTyp == TXT_MAINTEXT))
+        if (!rComment.isEmpty() && (m_rWW8Export.m_nTextTyp == TXT_MAINTEXT) &&
+            // tdf#153016 don't export the new automatic comments added by tdf#148032
+            rComment != SwResId(STR_REDLINE_COMMENT_DELETED) &&
+            rComment != SwResId(STR_REDLINE_COMMENT_ADDED))
         {
             if (m_rWW8Export.m_pAtn->IsNewRedlineComment(pRedlineData))
             {

sw/qa/uitest/writer_tests/trackedChanges.py
@@ -347,6 +347,48 @@ class trackedchanges(UITestCase):
                 # This was False (missing comment)
                 self.assertEqual(True, get_state_as_dict(xChild)["Text"].endswith('\tComment added'))
 
+
+    def get_annotation_count(self, document):
+            n = 0
+            textfields = document.getTextFields()
+            for textfield in textfields:
+                if textfield.supportsService("com.sun.star.text.TextField.Annotation"):
+                    n = n + 1
+            return n
+
+    def test_tdf153016_annotation_in_DOC(self):
+        # load a test document, and add a tracked comment
+        with TemporaryDirectory() as tempdir:
+                xFilePath = os.path.join(tempdir, 'temp_drop_down_form_field.doc')
+
+                with self.ui_test.load_file(get_url_for_data_file('drop_down_form_field.doc')) as document:
+
+                    self.xUITest.executeCommand(".uno:TrackChanges")
+                    self.xUITest.executeCommand('.uno:SelectAll')
+
+                    self.assertEqual(0, self.get_annotation_count(document))
+
+                    self.xUITest.executeCommand('.uno:InsertAnnotation')
+
+                    self.assertEqual(1, self.get_annotation_count(document))
+
+                    # Save Copy as
+                    with self.ui_test.execute_dialog_through_command('.uno:SaveAs', close_button="") as xDialog:
+                        xFileName = xDialog.getChild('file_name')
+                        xFileName.executeAction('TYPE', mkPropertyValues({'KEYCODE':'CTRL+A'}))
+                        xFileName.executeAction('TYPE', mkPropertyValues({'KEYCODE':'BACKSPACE'}))
+                        xFileName.executeAction('TYPE', mkPropertyValues({'TEXT': xFilePath}))
+
+                        xOpen = xDialog.getChild("open")
+                        # DOC confirmation dialog is displayed
+                        with self.ui_test.execute_dialog_through_action(xOpen, "CLICK", close_button="save"):
+                            pass
+
+                with self.ui_test.load_file(systemPathToFileUrl(xFilePath)) as document:
+                    # This was 2
+                    self.assertEqual(1, self.get_annotation_count(document))
+
+
     def test_tdf147179(self):
         with self.ui_test.load_file(get_url_for_data_file("TC-table-del-add.docx")) as document:
             xWriterDoc = self.xUITest.getTopFocusWindow()