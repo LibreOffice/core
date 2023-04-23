d590454062cfef1b6ede8b75311610874f63e5fa
Mike Kaganski <mike.kaganski@collabora.com>
2023-04-23 20:14:16 +0300

    Use getXWeak in oox
    Change-Id: I8211a1fe19bbd900f866c46d5b7ec68a37bc38cf
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/150859
    Tested-by: Jenkins
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

--- oox/source/export/ColorPropertySet.cxx
+++ oox/source/export/ColorPropertySet.cxx
@@ -67,7 +67,7 @@ Property SAL_CALL lcl_ColorPropertySetInfo::getPropertyByName( const OUString& a
 {
     if( aName == m_aColorPropName )
         return m_aColorProp;
-    throw UnknownPropertyException( m_aColorPropName, static_cast< uno::XWeak * >( this ));
+    throw UnknownPropertyException( m_aColorPropName, getXWeak());
 }
 
 sal_Bool SAL_CALL lcl_ColorPropertySetInfo::hasPropertyByName( const OUString& Name )
