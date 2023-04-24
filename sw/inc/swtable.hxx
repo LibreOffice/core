LibreOffice/core

commit 05366b8e6683363688de8708a3d88cf144c7a2bf

tdf#60382 sw offapi: add change tracking of table/row deletion
This is a minimal extension of the text range based change
tracking to record and apply table row and table deletions
with full Undo/Redo support.

Add property IsNotTracked to com::sun::star::text::TextTableRow.

During recording of track changes, deletion of table rows wasn't
recorded: the rows removed completely with their text content.
Now the deletion deletes the cell content with change tracking,
setting also IsNotTracked property of table rows to FALSE. If
all tracked deletions were accepted in a row, and the result is
an empty row, the row will be removed, if its IsNotTracked
property is FALSE.

Note: Deletion of empty lines isn't recorded (they are simply
deleted). Hiding deleted rows hasn't been supported yet in
the Hide Changes mode.

OpenDocument supports only track changes of text ranges, but
not changes of the table structure, e.g. deletion of table
rows. For the native export it needs to extend ODF, and
depending on this future extension, can be based also on
SwExtraRedlineTable (which lacks of recording and Undo/Redo,
but supports OOXML round-trip of tracked table changes).
See also commit d688069023959ab97d14eb1dbfd5bf6ad3c1b160
"Add support for 'Table Row Redlines' in SW core" and its
follow-up commits.

Change-Id: I2e3807cf8ae8212bd51c210ef1c20c85878d0da8
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/115804
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/inc/swtable.hxx
@@ -444,6 +444,8 @@ public:
     void RemoveFromTable();
     const SwStartNode *GetSttNd() const { return m_pStartNode; }
     sal_uLong GetSttIdx() const;
+    // it doesn't contain box content
+    bool IsEmpty() const;
 
     // Search next/previous box with content.
     SwTableBox* FindNextBox( const SwTable&, const SwTableBox*,

sw/source/core/table/swtable.cxx
@@ -1868,6 +1868,21 @@ sal_uLong SwTableBox::GetSttIdx() const
     return m_pStartNode ? m_pStartNode->GetIndex() : 0;
 }
 
+bool SwTableBox::IsEmpty() const
+{
+    const SwStartNode *pSttNd = GetSttNd();
+    if( pSttNd &&
+        pSttNd->GetIndex() + 2 == pSttNd->EndOfSectionIndex() )
+    {
+        const SwContentNode *pCNd =
+            pSttNd->GetNodes()[pSttNd->GetIndex()+1]->GetContentNode();
+        if( pCNd && !pCNd->Len() )
+            return true;
+    }
+
+    return false;
+}
+
     // retrieve information from the client
 bool SwTable::GetInfo( SfxPoolItem& rInfo ) const
 {

offapi/com/sun/star/text/TextTableRow.idl
@@ -105,6 +105,14 @@ published service TextTableRow
         @since LibreOffice 6.1
      */
     [optional, property] com::sun::star::graphic::XGraphic BackGraphic;
+
+    /** If TRUE, the table row wasn't deleted or inserted with its tracked cell content
+
+        @since LibreOffice 7.2
+     */
+
+        [optional, property, maybevoid] boolean IsNotTracked;
+
 };
 
 

sw/inc/doc.hxx
@@ -1489,6 +1489,8 @@ public:
     bool BalanceRowHeight( const SwCursor& rCursor, bool bTstOnly, const bool bOptimize );
     void SetRowBackground( const SwCursor& rCursor, const SvxBrushItem &rNew );
     static bool GetRowBackground( const SwCursor& rCursor, std::unique_ptr<SvxBrushItem>& rToFill );
+    /// rNotTracked = false means that the row was deleted or inserted with its tracked cell content
+    void SetRowNotTracked( const SwCursor& rCursor, const SvxPrintItem &rNotTracked );
     void SetTabBorders( const SwCursor& rCursor, const SfxItemSet& rSet );
     void SetTabLineStyle( const SwCursor& rCursor,
                           const Color* pColor, bool bSetLine,

sw/inc/unoprnms.hxx
@@ -736,6 +736,7 @@
 #define UNO_NAME_ITEMS "Items"
 #define UNO_NAME_SELITEM "SelectedItem"
 #define UNO_NAME_IS_SPLIT_ALLOWED "IsSplitAllowed"
+#define UNO_NAME_IS_NOT_TRACKED "IsNotTracked"
 #define UNO_NAME_CHAR_HIDDEN "CharHidden"
 #define UNO_NAME_IS_FOLLOWING_TEXT_FLOW "IsFollowingTextFlow"
 #define UNO_NAME_WIDTH_TYPE "WidthType"

sw/qa/extras/uiwriter/uiwriter2.cxx
@@ -3666,6 +3666,83 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest2, testTdf118311)
     assertXPath(pXmlDoc, "//page[1]//body/tab", 0);
 }
 
+CPPUNIT_TEST_FIXTURE(SwUiWriterTest2, testRedlineTableRowDeletion)
+{
+    // load a 1-row table, and delete the row with enabled change tracking:
+    // now the row is not deleted silently, but keeps the deleted cell contents,
+    // and only accepting all of them will result the deletion of the table row.
+    SwDoc* pDoc = createDoc("tdf118311.fodt");
+
+    SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument*>(mxComponent.get());
+    CPPUNIT_ASSERT(pTextDoc);
+
+    // turn on red-lining and show changes
+    pDoc->getIDocumentRedlineAccess().SetRedlineFlags(RedlineFlags::On | RedlineFlags::ShowDelete
+                                                      | RedlineFlags::ShowInsert);
+    CPPUNIT_ASSERT_MESSAGE("redlining should be on",
+                           pDoc->getIDocumentRedlineAccess().IsRedlineOn());
+    CPPUNIT_ASSERT_MESSAGE(
+        "redlines should be visible",
+        IDocumentRedlineAccess::IsShowChanges(pDoc->getIDocumentRedlineAccess().GetRedlineFlags()));
+
+    // check table
+    xmlDocUniquePtr pXmlDoc = parseLayoutDump();
+    assertXPath(pXmlDoc, "//page[1]//body/tab");
+
+    // delete table row with enabled change tracking
+    dispatchCommand(mxComponent, ".uno:DeleteRows", {});
+
+    // This was deleted without change tracking
+    discardDumpedLayout();
+    pXmlDoc = parseLayoutDump();
+    assertXPath(pXmlDoc, "//page[1]//body/tab");
+
+    // accept the deletion of the content of the first cell
+
+    SwEditShell* const pEditShell(pDoc->GetEditShell());
+    CPPUNIT_ASSERT_EQUAL(static_cast<SwRedlineTable::size_type>(2), pEditShell->GetRedlineCount());
+    pEditShell->AcceptRedline(0);
+
+    // table row was still not deleted
+    discardDumpedLayout();
+    pXmlDoc = parseLayoutDump();
+    assertXPath(pXmlDoc, "//page[1]//body/tab");
+
+    // accept last redline
+    pEditShell->AcceptRedline(0);
+
+    // table row (and the 1-row table) was deleted finally
+    discardDumpedLayout();
+    pXmlDoc = parseLayoutDump();
+    assertXPath(pXmlDoc, "//page[1]//body/tab", 0);
+
+    // Undo, and delete the row without change tracking
+
+    dispatchCommand(mxComponent, ".uno:Undo", {});
+    dispatchCommand(mxComponent, ".uno:Undo", {});
+    dispatchCommand(mxComponent, ".uno:Undo", {});
+
+    // table exists again
+    discardDumpedLayout();
+    pXmlDoc = parseLayoutDump();
+    assertXPath(pXmlDoc, "//page[1]//body/tab");
+
+    // disable change tracking
+    pDoc->getIDocumentRedlineAccess().SetRedlineFlags(RedlineFlags::ShowDelete
+                                                      | RedlineFlags::ShowInsert);
+
+    CPPUNIT_ASSERT_MESSAGE("redlining should be off",
+                           !pDoc->getIDocumentRedlineAccess().IsRedlineOn());
+
+    // delete table row without change tracking
+    dispatchCommand(mxComponent, ".uno:DeleteRows", {});
+
+    // the table (row) was deleted
+    discardDumpedLayout();
+    pXmlDoc = parseLayoutDump();
+    assertXPath(pXmlDoc, "//page[1]//body/tab", 0);
+}
+
 CPPUNIT_TEST_FIXTURE(SwUiWriterTest2, testTdf128335)
 {
     // Load the bugdoc, which has 3 textboxes.

sw/source/core/bastyp/init.cxx
@@ -206,6 +206,8 @@ sal_uInt16 const aTableSetRange[] = {
 
 sal_uInt16 const aTableLineSetRange[] = {
     RES_FILL_ORDER,     RES_FRM_SIZE,
+    // IsNotTracked
+    RES_PRINT,          RES_PRINT,
     RES_LR_SPACE,       RES_UL_SPACE,
     RES_BACKGROUND,     RES_SHADOW,
     RES_ROW_SPLIT,      RES_ROW_SPLIT,

sw/source/core/doc/DocumentRedlineManager.cxx
@@ -38,6 +38,7 @@
 #include <strings.hrc>
 #include <swmodule.hxx>
 #include <osl/diagnose.h>
+#include <editeng/prntitem.hxx>
 
 using namespace com::sun::star;
 
@@ -430,6 +431,36 @@ namespace
         return nullptr;
     }
 
+    // delete the empty tracked table row (i.e. if it's last tracked deletion was accepted)
+    void lcl_DeleteTrackedTableRow ( SwPosition* pPos )
+    {
+        if ( const SwTableBox* pBox = pPos->nNode.GetNode().GetTableBox() )
+        {
+            const SwTableLine* pLine = pBox->GetUpper();
+            const SvxPrintItem *pIsNoTrackedProp =
+                    pLine->GetFrameFormat()->GetAttrSet().GetItem<SvxPrintItem>(RES_PRINT);
+            // table row property "IsNotTracked" is set and its value is false
+            if ( pIsNoTrackedProp && !pIsNoTrackedProp->GetValue() )
+            {
+                bool bEmptyLine = true;
+                const SwTableBoxes & rBoxes = pLine->GetTabBoxes();
+                for (size_t nBox = 0; nBox < rBoxes.size(); ++nBox)
+                {
+                    if ( !rBoxes[nBox]->IsEmpty() )
+                    {
+                        bEmptyLine = false;
+                        break;
+                    }
+                }
+                if ( bEmptyLine )
+                {
+                    SwCursor aCursor( *pPos, nullptr );
+                    pPos->GetDoc().DeleteRow( aCursor );
+                }
+            }
+        }
+    }
+
     bool lcl_AcceptRedline( SwRedlineTable& rArr, SwRedlineTable::size_type& rPos,
                             bool bCallDelete,
                             const SwPosition* pSttRng = nullptr,
@@ -565,7 +596,10 @@ namespace
                     rDoc.getIDocumentRedlineAccess().SetRedlineFlags_intern( eOld & ~RedlineFlags(RedlineFlags::On | RedlineFlags::Ignore));
 
                     if( pCSttNd && pCEndNd )
+                    {
                         rDoc.getIDocumentContentOperations().DeleteAndJoin( aPam );
+                        lcl_DeleteTrackedTableRow( aPam.End() );
+                    }
                     else if (pCSttNd && !pCEndNd)
                         {
                             aPam.GetBound().nContent.Assign( nullptr, 0 );

commit 5e6d8efbf3fb456ca370df04b21d2e340d9831ad

tdf#154859 sw: fix deletion of tracked row insertion
Row deletion of tracked row insertions resulted an
empty, not tracked row. To fix this, disable row
deletion on tracked row insertions, allowing only to
manage changes for removing the row, e.g. by clicking
on Reject Change of the context menu of the table row,
like MSO does.

This also avoids of clicking again on the row deletion
to create an empty tracked row deletion from the
empty not tracked row, and originally, from the tracked
row insertion.

Regression from commit 05366b8e6683363688de8708a3d88cf144c7a2bf
"tdf#60382 sw offapi: add change tracking of table/row deletion".

Follow-up to commit 0204c00f241313e1d292b4c3ea117d42af7dec69
"tdf#147453 sw: disable Delete Table functions on tracked deletions".

See also commit 95c003d75e0f8b255344715a35358072b5eba99d
"tdf#146145 sw: 1-click Accept/Reject of table row changes".

Change-Id: Ied38860939359caa52e09987cf968953399611ec
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/150938
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/inc/swtable.hxx
@@ -426,6 +426,8 @@ public:
     // Cache also the type of the redline associated to the changed table row.
     SwRedlineTable::size_type UpdateTextChangesOnly(
         SwRedlineTable::size_type& rRedlinePos, bool bUpdateProperty = true) const;
+    // is it a tracked row
+    bool IsTracked(SwRedlineTable::size_type& rRedlinePos, bool bOnlyDeleted = false) const;
     // is it a tracked deleted row
     bool IsDeleted(SwRedlineTable::size_type& rRedlinePos) const;
     // set/get (if it's possible, cached) redline type

sw/source/core/table/swtable.cxx
@@ -1892,17 +1892,23 @@ SwRedlineTable::size_type SwTableLine::UpdateTextChangesOnly(
     return nRet;
 }
 
-bool SwTableLine::IsDeleted(SwRedlineTable::size_type& rRedlinePos) const
+bool SwTableLine::IsTracked(SwRedlineTable::size_type& rRedlinePos, bool bOnlyDeleted) const
 {
    SwRedlineTable::size_type nPos = UpdateTextChangesOnly(rRedlinePos);
    if ( nPos != SwRedlineTable::npos )
    {
        const SwRedlineTable& aRedlineTable =
            GetFrameFormat()->GetDoc()->getIDocumentRedlineAccess().GetRedlineTable();
-       if ( RedlineType::Delete == aRedlineTable[nPos]->GetType() )
+       if ( RedlineType::Delete == aRedlineTable[nPos]->GetType() ||
+            ( !bOnlyDeleted && RedlineType::Insert == aRedlineTable[nPos]->GetType() ) )
            return true;
    }
    return false;
+}
+
+bool SwTableLine::IsDeleted(SwRedlineTable::size_type& rRedlinePos) const
+{
+   return IsTracked(rRedlinePos, true);
 }
 
 RedlineType SwTableLine::GetRedlineType() const

sw/source/uibase/shells/tabsh.cxx
@@ -446,17 +446,13 @@ static void lcl_TabGetMaxLineWidth(const SvxBorderLine* pBorderLine, SvxBorderLi
     rBorderLine.SetColor(pBorderLine->GetColor());
 }
 
-static bool lcl_BoxesInDeletedRows(SwWrtShell &rSh, const SwSelBoxes& rBoxes)
+static bool lcl_BoxesInTrackedRows(SwWrtShell &rSh, const SwSelBoxes& rBoxes)
 {
-    // cursor and selection are there only in deleted rows in Show Changes mode
-    if ( rSh.GetLayout()->IsHideRedlines() )
-        return false;
-
-    // not selected or all selected rows are deleted
+    // cursor and selection are there only in tracked rows
     bool bRet = true;
     SwRedlineTable::size_type nRedlinePos = 0;
     if ( rBoxes.empty() )
-        bRet = rSh.GetCursor()->GetPointNode().GetTableBox()->GetUpper()->IsDeleted(nRedlinePos);
+        bRet = rSh.GetCursor()->GetPointNode().GetTableBox()->GetUpper()->IsTracked(nRedlinePos);
     else
     {
         tools::Long nBoxes = rBoxes.size();
@@ -465,7 +461,7 @@ static bool lcl_BoxesInDeletedRows(SwWrtShell &rSh, const SwSelBoxes& rBoxes)
         {
             SwTableLine* pLine = rBoxes[i]->GetUpper();
             if ( pLine != pPrevLine )
-                bRet &= pLine->IsDeleted(nRedlinePos);
+                bRet &= pLine->IsTracked(nRedlinePos);
             pPrevLine = pLine;
         }
     }
@@ -1450,7 +1446,7 @@ void SwTableShell::GetState(SfxItemSet &rSet)
                 {
                     SwSelBoxes aBoxes;
                     ::GetTableSel( rSh, aBoxes, SwTableSearchType::Row );
-                    if( ::HasProtectedCells( aBoxes ) || lcl_BoxesInDeletedRows( rSh, aBoxes ) )
+                    if( ::HasProtectedCells( aBoxes ) || lcl_BoxesInTrackedRows( rSh, aBoxes ) )
                         rSet.DisableItem( nSlot );
                 }
                 break;