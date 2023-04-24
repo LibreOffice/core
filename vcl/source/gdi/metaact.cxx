26b7d00c24c4d85d004e3840fb9053e4c88c1fee
Caolán McNamara <caolanm@redhat.com>
2023-04-24 16:27:52 +0100

    ofz#58241 Integer-overflow
    Change-Id: Ie5b82df4642c786e309022e3b5a436aef66d1137
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/150945
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>

--- vcl/source/gdi/metaact.cxx
+++ vcl/source/gdi/metaact.cxx
@@ -1318,6 +1318,8 @@ void MetaHatchAction::Execute( OutputDevice* pOut )
 {
     if (!AllowRect(pOut->LogicToPixel(maPolyPoly.GetBoundRect())))
         return;
+    if (!AllowDim(pOut->LogicToPixel(Point(maHatch.GetDistance(), 0)).X()))
+        return;
 
     pOut->DrawHatch( maPolyPoly, maHatch );
 }
