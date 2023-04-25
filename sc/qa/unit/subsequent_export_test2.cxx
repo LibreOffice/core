LibreOffice/core

commit bc0a9076aa43a0782bcf81e55d3f84f6af0f68e8

ooxml: Preserve shape theme attribute for solid fill
Users can select the fill color for a shape among the theme-defined
colors. This results in the following XML:

  <wps:spPr>
    ...
    <a:solidFill>
      <a:schemeClr val="accent2"/>
    </a:solidFill>
    ...
  </wps:spPr>

Now we store both the original fill color and the name of the
theme-defined color, if it exists, on the import phase. They are put
into the InteropGrabBag of the shape with the names
OriginalSolidFillClr and SpPrSolidFillSchemeClr. Additionally, we
needed to to store the decoded theme color inside StyleFillRef.

On the export phase we have to take into account several combinations
of factors:
* If the final color for the shape fill is different from the
  original color, we must ignore any theme attributes and write the
  new color.
* If the fill color is unchanged and some theme color exists, we must
  write the theme color.
* If the fill color is unchanged and no theme color exists, we must
  check if the original color matches the style-defined color. If it
  does, we must not write any <a:solidFill> tag.
* Otherwise we must write the <a:solidFill> tag with the RGB color.

The method putPropertiesToGrabBag was added to the Shape object for
convenience.

The data files for some /sd/qa/ unit tests were updated to reflect
the new properties inside the Shape InteropGrabBag.

Change-Id: If0915c5442872a8acab0a8a081f60c89c97277bd

oox/source/export/drawingml.cxx
@@ -174,10 +174,65 @@ void DrawingML::WriteSolidFill( sal_uInt32 nColor )
     mpFS->endElementNS( XML_a, XML_solidFill );
 }
 
+void DrawingML::WriteSolidFill( OUString sSchemeName )
+{
+    mpFS->startElementNS( XML_a, XML_solidFill, FSEND );
+    mpFS->singleElementNS( XML_a, XML_schemeClr, XML_val,
+                           OUStringToOString( sSchemeName, RTL_TEXTENCODING_ASCII_US ).getStr(),
+                           FSEND );
+    mpFS->endElementNS( XML_a, XML_solidFill );
+}
+
 void DrawingML::WriteSolidFill( Reference< XPropertySet > rXPropSet )
 {
-    if ( GetProperty( rXPropSet, "FillColor" ) )
-        WriteSolidFill( *((sal_uInt32*) mAny.getValue()) & 0xffffff );
+    // get fill color
+    sal_uInt32 nFillColor;
+    if ( !GetProperty( rXPropSet, "FillColor" ) )
+        return;
+    mAny >>= nFillColor;
+
+    // get InteropGrabBag and search the relevant attributes
+    OUString sColorFillScheme;
+    sal_uInt32 nOriginalColor;
+    Sequence< PropertyValue > aStyleProperties;
+    if ( GetProperty( rXPropSet, "InteropGrabBag" ) )
+    {
+        Sequence< PropertyValue > aGrabBag;
+        mAny >>= aGrabBag;
+        for( sal_Int32 i=0; i < aGrabBag.getLength(); ++i )
+            if( aGrabBag[i].Name == "SpPrSolidFillSchemeClr" )
+                aGrabBag[i].Value >>= sColorFillScheme;
+            else if( aGrabBag[i].Name == "OriginalSolidFillClr" )
+                aGrabBag[i].Value >>= nOriginalColor;
+            else if( aGrabBag[i].Name == "StyleFillRef" )
+                aGrabBag[i].Value >>= aStyleProperties;
+    }
+
+    // write XML
+    if ( nFillColor != nOriginalColor )
+        // the user has set a different color for the shape
+        WriteSolidFill( nFillColor & 0xffffff );
+    else if ( !sColorFillScheme.isEmpty() )
+        // the shape had a scheme color and the user didn't change it
+        WriteSolidFill( sColorFillScheme );
+    else if ( aStyleProperties.hasElements() )
+    {
+        sal_uInt32 nThemeColor;
+        for( sal_Int32 i=0; i < aStyleProperties.getLength(); ++i )
+            if( aStyleProperties[i].Name == "Color" )
+            {
+                aStyleProperties[i].Value >>= nThemeColor;
+                break;
+            }
+        if ( nFillColor != nThemeColor )
+            // the shape contains a theme but it wasn't being used
+            WriteSolidFill( nFillColor & 0xffffff );
+        // in case the shape used the style color and the user didn't change it,
+        // we must not write a <a: solidFill> tag.
+    }
+    else
+        // the shape had a custom color and the user didn't change it
+        WriteSolidFill( nFillColor & 0xffffff );
 }
 
 void DrawingML::WriteGradientStop( sal_uInt16 nStop, sal_uInt32 nColor )

include/oox/drawingml/shape.hxx
@@ -219,6 +219,8 @@ protected:
 
     void                putPropertyToGrabBag(
                             const ::com::sun::star::beans::PropertyValue& pProperty );
+    void                putPropertiesToGrabBag(
+                            const ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue >& aProperties );
 
     std::vector< ShapePtr >     maChildren;               // only used for group shapes
     com::sun::star::awt::Size   maChSize;                 // only used for group shapes

include/oox/export/drawingml.hxx
@@ -118,6 +118,7 @@ public:
     void WriteConnectorConnections( EscherConnectorListEntry& rConnectorEntry, sal_Int32 nStartID, sal_Int32 nEndID );
 
     void WriteSolidFill( sal_uInt32 nColor );
+    void WriteSolidFill( OUString sSchemeName );
     void WriteSolidFill( ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet > rXPropSet );
     void WriteGradientFill( ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet > rXPropSet );
     void WriteBlipFill( ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet > rXPropSet, OUString sURLPropName, sal_Int32 nXmlNamespace );

oox/source/drawingml/shape.cxx
@@ -568,11 +568,13 @@ Reference< XShape > Shape::createAndInsert(
                 OUString sColorScheme = pFillRef->maPhClr.getSchemeName();
                 if( !sColorScheme.isEmpty() )
                 {
-                    Sequence< PropertyValue > aProperties(2);
+                    Sequence< PropertyValue > aProperties(3);
                     aProperties[0].Name = "SchemeClr";
                     aProperties[0].Value = Any( sColorScheme );
                     aProperties[1].Name = "Idx";
                     aProperties[1].Value = Any( pFillRef->mnThemedIdx );
+                    aProperties[2].Name = "Color";
+                    aProperties[2].Value = Any( nFillPhClr );
 
                     PropertyValue pStyleFillRef;
                     pStyleFillRef.Name = "StyleFillRef";
@@ -764,6 +766,18 @@ Reference< XShape > Shape::createAndInsert(
                 mxShape->setPosition(awt::Point(aShapeRectHmm.X, aShapeRectHmm.Y));
                 mxShape->setSize(awt::Size(aShapeRectHmm.Width, aShapeRectHmm.Height));
             }
+
+            Sequence< PropertyValue > aProperties( 1 );
+            aProperties[0].Name = "OriginalSolidFillClr";
+            aProperties[0].Value = aShapeProps[PROP_FillColor];
+            OUString sColorFillScheme = aFillProperties.maFillColor.getSchemeName();
+            if( !aFillProperties.maFillColor.isPlaceHolder() && !sColorFillScheme.isEmpty() )
+            {
+                aProperties.realloc( 2 );
+                aProperties[1].Name = "SpPrSolidFillSchemeClr";
+                aProperties[1].Value = Any( sColorFillScheme );
+            }
+            putPropertiesToGrabBag( aProperties );
         }
 
         // These can have a custom geometry, so position should be set here,
@@ -1066,6 +1080,33 @@ void Shape::putPropertyToGrabBag( const PropertyValue& pProperty )
         aGrabBag.realloc( length + 1 );
         aGrabBag[length] = pProperty;
 
+        xSet->setPropertyValue( aGrabBagPropName, Any( aGrabBag ) );
+    }
+}
+
+void Shape::putPropertiesToGrabBag( const Sequence< PropertyValue >& aProperties )
+{
+    Reference< XPropertySet > xSet( mxShape, UNO_QUERY );
+    Reference< XPropertySetInfo > xSetInfo( xSet->getPropertySetInfo() );
+    const OUString& aGrabBagPropName = OUString( UNO_NAME_MISC_OBJ_INTEROPGRABBAG );
+    if( mxShape.is() && xSet.is() && xSetInfo.is() && xSetInfo->hasPropertyByName( aGrabBagPropName ) )
+    {
+        // get existing grab bag
+        Sequence< PropertyValue > aGrabBag;
+        xSet->getPropertyValue( aGrabBagPropName ) >>= aGrabBag;
+        sal_Int32 length = aGrabBag.getLength();
+
+        // update grab bag size to contain the new items
+        aGrabBag.realloc( length + aProperties.getLength() );
+
+        // put the new items
+        for( sal_Int32 i=0; i < aProperties.getLength(); ++i )
+        {
+            aGrabBag[length + i].Name = aProperties[i].Name;
+            aGrabBag[length + i].Value = aProperties[i].Value;
+        }
+
+        // put it back to the shape
         xSet->setPropertyValue( aGrabBagPropName, Any( aGrabBag ) );
     }
 }

sd/qa/unit/data/xml/fdo47434_0.xml
@@ -24,10 +24,12 @@
    <Line3 column1="0.000000" column2="0.000000" column3="1.000000"/>
   </Transformation>
   <InteropGrabBag>
+   <PropertyValue name="OriginalSolidFillClr" handle="0" propertyState="DIRECT_VALUE"/>
    <PropertyValue name="StyleFillRef">
     <StyleFillRef>
      <PropertyValue name="SchemeClr" value="accent1" handle="0" propertyState="DIRECT_VALUE"/>
      <PropertyValue name="Idx" value="0" handle="0" propertyState="DIRECT_VALUE"/>
+     <PropertyValue name="Color" value="5210557" handle="0" propertyState="DIRECT_VALUE"/>
     </StyleFillRef>
    </PropertyValue>
   </InteropGrabBag>
@@ -92,10 +94,12 @@
    <Line3 column1="0.000000" column2="0.000000" column3="1.000000"/>
   </Transformation>
   <InteropGrabBag>
+   <PropertyValue name="OriginalSolidFillClr" handle="0" propertyState="DIRECT_VALUE"/>
    <PropertyValue name="StyleFillRef">
     <StyleFillRef>
      <PropertyValue name="SchemeClr" value="accent1" handle="0" propertyState="DIRECT_VALUE"/>
      <PropertyValue name="Idx" value="0" handle="0" propertyState="DIRECT_VALUE"/>
+     <PropertyValue name="Color" value="5210557" handle="0" propertyState="DIRECT_VALUE"/>
     </StyleFillRef>
    </PropertyValue>
   </InteropGrabBag>
@@ -160,10 +164,12 @@
    <Line3 column1="0.000000" column2="0.000000" column3="1.000000"/>
   </Transformation>
   <InteropGrabBag>
+   <PropertyValue name="OriginalSolidFillClr" handle="0" propertyState="DIRECT_VALUE"/>
    <PropertyValue name="StyleFillRef">
     <StyleFillRef>
      <PropertyValue name="SchemeClr" value="accent1" handle="0" propertyState="DIRECT_VALUE"/>
      <PropertyValue name="Idx" value="0" handle="0" propertyState="DIRECT_VALUE"/>
+     <PropertyValue name="Color" value="5210557" handle="0" propertyState="DIRECT_VALUE"/>
     </StyleFillRef>
    </PropertyValue>
   </InteropGrabBag>
@@ -228,10 +234,12 @@
    <Line3 column1="0.000000" column2="0.000000" column3="1.000000"/>
   </Transformation>
   <InteropGrabBag>
+   <PropertyValue name="OriginalSolidFillClr" handle="0" propertyState="DIRECT_VALUE"/>
    <PropertyValue name="StyleFillRef">
     <StyleFillRef>
      <PropertyValue name="SchemeClr" value="accent1" handle="0" propertyState="DIRECT_VALUE"/>
      <PropertyValue name="Idx" value="0" handle="0" propertyState="DIRECT_VALUE"/>
+     <PropertyValue name="Color" value="5210557" handle="0" propertyState="DIRECT_VALUE"/>
     </StyleFillRef>
    </PropertyValue>
   </InteropGrabBag>

sd/qa/unit/data/xml/fdo71434_0.xml
@@ -6,6 +6,8 @@
    <Line2 column1="0.000000" column2="19046.000000" column3="-75.000000"/>
    <Line3 column1="0.000000" column2="0.000000" column3="1.000000"/>
   </Transformation>
-  <InteropGrabBag/>
+  <InteropGrabBag>
+   <PropertyValue name="OriginalSolidFillClr" handle="0" propertyState="DIRECT_VALUE"/>
+  </InteropGrabBag>
  </XShape>
 </XShapes>

sd/qa/unit/data/xml/n593612_0.xml
@@ -13,7 +13,10 @@
    <Line2 column1="0.000000" column2="15193.000000" column3="1324.000000"/>
    <Line3 column1="0.000000" column2="0.000000" column3="1.000000"/>
   </Transformation>
-  <InteropGrabBag/>
+  <InteropGrabBag>
+   <PropertyValue name="OriginalSolidFillClr" value="3968147" handle="0" propertyState="DIRECT_VALUE"/>
+   <PropertyValue name="SpPrSolidFillSchemeClr" value="accent1" handle="0" propertyState="DIRECT_VALUE"/>
+  </InteropGrabBag>
   <CustomShapeGeometry>
    <PropertyValue name="AdjustmentValues">
     <AdjustmentValues/>

sd/qa/unit/data/xml/n762695_0.xml
@@ -14,10 +14,13 @@
    <Line3 column1="0.000000" column2="0.000000" column3="1.000000"/>
   </Transformation>
   <InteropGrabBag>
+   <PropertyValue name="OriginalSolidFillClr" value="12834459" handle="0" propertyState="DIRECT_VALUE"/>
+   <PropertyValue name="SpPrSolidFillSchemeClr" value="accent3" handle="0" propertyState="DIRECT_VALUE"/>
    <PropertyValue name="StyleFillRef">
     <StyleFillRef>
      <PropertyValue name="SchemeClr" value="accent1" handle="0" propertyState="DIRECT_VALUE"/>
      <PropertyValue name="Idx" value="1" handle="0" propertyState="DIRECT_VALUE"/>
+     <PropertyValue name="Color" value="5210557" handle="0" propertyState="DIRECT_VALUE"/>
     </StyleFillRef>
    </PropertyValue>
   </InteropGrabBag>
@@ -107,7 +110,9 @@
    <Line2 column1="-1004.091629" column2="1026.011940" column3="7175.000000"/>
    <Line3 column1="0.000000" column2="0.000000" column3="1.000000"/>
   </Transformation>
-  <InteropGrabBag/>
+  <InteropGrabBag>
+   <PropertyValue name="OriginalSolidFillClr" handle="0" propertyState="DIRECT_VALUE"/>
+  </InteropGrabBag>
   <CustomShapeGeometry>
    <PropertyValue name="AdjustmentValues">
     <AdjustmentValues>
@@ -185,7 +190,9 @@
    <Line2 column1="1003.384523" column2="1026.011940" column3="6171.000000"/>
    <Line3 column1="0.000000" column2="0.000000" column3="1.000000"/>
   </Transformation>
-  <InteropGrabBag/>
+  <InteropGrabBag>
+   <PropertyValue name="OriginalSolidFillClr" handle="0" propertyState="DIRECT_VALUE"/>
+  </InteropGrabBag>
   <CustomShapeGeometry>
    <PropertyValue name="AdjustmentValues">
     <AdjustmentValues>

commit 2ef83c8782ed947b47f9292a12355fc3bb078078

tdf#91332 XLSX export: fix missing solidFill
Background color of shape inherited from theme
lost after export.

Regression from commit bc0a9076aa43a0782bcf81e55d3f84f6af0f68e8
"ooxml: Preserve shape theme attribute for solid fill".

Change-Id: I2d8298ac17332ba3ad6a627ce8b07c23087ac7b1
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/150674
Tested-by: Jenkins
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

oox/source/export/drawingml.cxx
@@ -530,7 +530,8 @@ void DrawingML::WriteSolidFill( const Reference< XPropertySet >& rXPropSet )
             WriteSolidFill(::Color(ColorTransparency, nFillColor & 0xffffff), nAlpha);
         }
     }
-    else if ( !sColorFillScheme.isEmpty() )
+    // tdf#91332 LO doesn't export the actual theme.xml in XLSX.
+    else if ( !sColorFillScheme.isEmpty() && GetDocumentType() != DOCUMENT_XLSX )
     {
         // the shape had a scheme color and the user didn't change it
         WriteSolidFill( sColorFillScheme, aTransformations, nAlpha );

sc/qa/unit/subsequent_export_test2.cxx
@@ -1224,6 +1224,25 @@ CPPUNIT_TEST_FIXTURE(ScExportTest2, testTdf126024XLSX)
     assertXPath(pXmlRels, "/rels:Relationships/rels:Relationship", "TargetMode", "External");
 }
 
+CPPUNIT_TEST_FIXTURE(ScExportTest2, testTdf91332)
+{
+    createScDoc("xlsx/tdf91332.xlsx");
+    saveAndReload("Calc Office Open XML");
+
+    uno::Reference<drawing::XDrawPagesSupplier> xDoc(mxComponent, uno::UNO_QUERY_THROW);
+    uno::Reference<drawing::XDrawPage> xPage(xDoc->getDrawPages()->getByIndex(0),
+                                             uno::UNO_QUERY_THROW);
+    uno::Reference<beans::XPropertySet> xShapeProps(xPage->getByIndex(0), uno::UNO_QUERY_THROW);
+
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: rgba[90cf47ff]
+    // - Actual  : rgba[ffffffff]
+    // i.e. fill color inherited from theme lost after export.
+    Color nColor;
+    xShapeProps->getPropertyValue("FillColor") >>= nColor;
+    CPPUNIT_ASSERT_EQUAL(Color(0x90cf47), nColor);
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */