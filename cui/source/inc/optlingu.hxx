88a82f69351842ddd2491564aee8038fc990f63e
Aron Budea <aron.budea@collabora.com>
2023-04-30 01:00:33 +0200

    cui, lok: Hide options irrelevant for online in Spelling Options dialog
    User-defined Dictionaries part and
    Get more dictionaries URL + icon.
    
    Change-Id: I73825a481e1981145abbf36b44485fb1628fbe13
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/151211
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Andras Timar <andras.timar@collabora.com>

--- cui/source/inc/optlingu.hxx
+++ cui/source/inc/optlingu.hxx
@@ -118,6 +118,7 @@ private:
     std::unique_ptr<weld::Button> m_xLinguDicsDelPB;
     std::unique_ptr<weld::TreeView> m_xLinguOptionsCLB;
     std::unique_ptr<weld::Button> m_xLinguOptionsEditPB;
+    std::unique_ptr<weld::Box> m_xMoreDictsBox;
     std::unique_ptr<weld::LinkButton> m_xMoreDictsLink;
 
     void    AddDicBoxEntry( const css::uno::Reference< css::linguistic2::XDictionary > &rxDic, sal_uInt16 nIdx );
