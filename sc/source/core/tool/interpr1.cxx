6b6b83b43fcae13e30a9ca67b421187a5e0d7142
Eike Rathke <erack@redhat.com>
2023-04-29 18:59:30 +0200

    Resolves: tdf#119659 Check found position for result vector
    Change-Id: Idc454462c47b6c7b7b7cb6c86b08650d059221c7
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/151186
    Reviewed-by: Eike Rathke <erack@redhat.com>
    Tested-by: Jenkins

--- sc/source/core/tool/interpr1.cxx
+++ sc/source/core/tool/interpr1.cxx
@@ -7078,7 +7078,8 @@ void ScInterpreter::ScLookup()
         if (pResMat)
         {
             VectorMatrixAccessor aResMatAcc(*pResMat, bVertical);
-            // result array is matrix.
+            // Result array is matrix.
+            // Note this does not replicate the other dimension.
             if (o3tl::make_unsigned(nDelta) >= aResMatAcc.GetElementCount())
             {
                 PushNA();
@@ -7212,6 +7213,12 @@ void ScInterpreter::ScLookup()
     {
         VectorMatrixAccessor aResMatAcc(*pResMat, bVertical);
         // Use the matrix result array.
+        // Note this does not replicate the other dimension.
+        if (o3tl::make_unsigned(nDelta) >= aResMatAcc.GetElementCount())
+        {
+            PushNA();
+            return;
+        }
         if (aResMatAcc.IsValue(nDelta))
             PushDouble(aResMatAcc.GetDouble(nDelta));
         else
