a0a61ffc29cde98d32dcd3b8d71cc8c21cb6e493
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2023-04-30 15:24:39 +0900

    docmodel: extract Transformation from ThemeColor into own file
    Change-Id: I45316e377cf895773037f500c521111f7e4d1ed7
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/151223
    Tested-by: Tomaž Vajngerl <quikee@gmail.com>
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

---
+++ include/docmodel/color/Transformation.hxx
@@ -0,0 +1,72 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#pragma once
+
+#include <docmodel/dllapi.h>
+#include <vector>
+
+namespace model
+{
+/** Color transformation type */
+enum class TransformationType
+{
+    Undefined,
+    Red,
+    RedMod,
+    RedOff,
+    Green,
+    GreenMod,
+    GreenOff,
+    Blue,
+    BlueMod,
+    BlueOff,
+    Alpha,
+    AlphaMod,
+    AlphaOff,
+    Hue,
+    HueMod,
+    HueOff,
+    Sat,
+    SatMod,
+    SatOff,
+    Lum,
+    LumMod,
+    LumOff,
+    Shade,
+    Tint,
+    Gray,
+    Comp,
+    Inv,
+    Gamma,
+    InvGamma
+};
+
+/** Definition of a color transformation.
+ *
+ * This just defines how a color should be transformed (changed). The
+ * type defines what kind of transformation should occur and the value
+ * defines by how much.
+ */
+struct DOCMODEL_DLLPUBLIC Transformation
+{
+    TransformationType meType = TransformationType::Undefined;
+
+    sal_Int16 mnValue = 0; /// percentage value -10000 to +10000
+
+    bool operator==(const Transformation& rTransformation) const
+    {
+        return meType == rTransformation.meType && mnValue == rTransformation.mnValue;
+    }
+};
+
+} // end of namespace model
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
