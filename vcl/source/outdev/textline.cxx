831ccb770f318a4be039431b14ce0cd247174e44
Caolán McNamara <caolanm@redhat.com>
2023-05-02 15:41:36 +0100

    ofz#57493 Timeout
    Change-Id: I2a61016c771012f23415e753ca91bf254584131a
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/151293
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>

--- vcl/source/outdev/textline.cxx
+++ vcl/source/outdev/textline.cxx
@@ -353,7 +353,7 @@ void OutputDevice::ImplDrawStraightTextLine( tools::Long nBaseX, tools::Long nBa
                                              bool bIsAbove )
 {
     static bool bFuzzing = utl::ConfigManager::IsFuzzing();
-    if (bFuzzing && nWidth > 1000000)
+    if (bFuzzing && nWidth > 100000)
     {
         SAL_WARN("vcl.gdi", "drawLine, skipping suspicious TextLine of length: "
                                 << nWidth << " for fuzzing performance");
