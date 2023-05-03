4adf6852cc3fd0eddf53c2ab4f66efed0668b699
Dennis Francis <dennis.francis@collabora.com>
2023-05-03 13:00:09 +0530

    sc: use the current cell's numfmt to format...
    to format the values in the validation list when the items are numbers.
    This is better than applying no formatting since we lost track of the
    source formatting when values are passed through a matrix.
    
    Change-Id: I06432bd93ef8d01181dd16d2f5ee99eb0477c094
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/151313
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Henry Castro <hcastro@collabora.com>
    (cherry picked from commit 2f6d1cefc184fda3ba292f1718b034202989d7b3)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/151386
    Tested-by: Jenkins
    Reviewed-by: Dennis Francis <dennis.francis@collabora.com>

--- sc/source/core/data/validat.cxx
+++ sc/source/core/data/validat.cxx
@@ -856,6 +856,7 @@ bool ScValidationData::GetSelectionFromFormula(
     rMatch = -1;
 
     SvNumberFormatter* pFormatter = GetDocument()->GetFormatTable();
+    sal_uInt32 nDestFormat = pDocument->GetNumberFormat(rPos.Col(), rPos.Row(), rPos.Tab());
 
     SCSIZE  nCol, nRow, nCols, nRows, n = 0;
     pValues->GetDimensions( nCols, nRows );
@@ -957,7 +958,7 @@ bool ScValidationData::GetSelectionFromFormula(
                     }
                     else
                     {
-                        pFormatter->GetInputLineString( nMatVal.fVal, 0, aValStr );
+                        pFormatter->GetInputLineString( nMatVal.fVal, nDestFormat, aValStr );
                     }
                 }
 
