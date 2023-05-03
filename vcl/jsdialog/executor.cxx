6be76f54a4e94b952975dfded25afff60257c6dd
Attila Szűcs <attila.szucs@collabora.com>
2023-05-03 04:30:55 +0200

    LOK: Navi-4 tree dblclick
    Removed sendUpdate that was called 1/sec, so selection dont revert 1/sec.
    Put sendUpdate into set_cursor(), that seems to be a better spot.
    
    Fixed set_cursor usage in executor, that caused slection to not work
    in dblclick at all.
    Because set_cursor(int) use not absolute position, but relative to parent
    And root was used as parent, so it picked SwContent from the 1. lvl,
    but the 1. lvl items are not real items, just SwContentTypes ..
    e.g.:Headings.. that cannot be activated.. so never happened anything.
    
    Change-Id: Iea373af3a0832a4f97202122bd36022eddf26efe
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/151308
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/151703
    Tested-by: Jenkins

--- vcl/jsdialog/executor.cxx
+++ vcl/jsdialog/executor.cxx
@@ -441,7 +441,8 @@ bool ExecuteAction(const OUString& nWindowId, const OUString& rWidget, StringMap
                     std::unique_ptr<weld::TreeIter> itEntry(pTreeView->make_iterator());
                     pTreeView->get_iter_abs_pos(*itEntry, nAbsPos);
                     pTreeView->select(*itEntry);
-                    pTreeView->set_cursor(*itEntry);
+                    pTreeView->set_cursor_without_notify(*itEntry);
+                    pTreeView->grab_focus();
                     LOKTrigger::trigger_changed(*pTreeView);
                     return true;
                 }
@@ -450,8 +451,11 @@ bool ExecuteAction(const OUString& nWindowId, const OUString& rWidget, StringMap
                     sal_Int32 nRow = o3tl::toInt32(rData["data"]);
 
                     pTreeView->unselect_all();
+                    std::unique_ptr<weld::TreeIter> itEntry(pTreeView->make_iterator());
+                    pTreeView->get_iter_abs_pos(*itEntry, nRow);
                     pTreeView->select(nRow);
-                    pTreeView->set_cursor(nRow);
+                    pTreeView->set_cursor_without_notify(*itEntry);
+                    pTreeView->grab_focus();
                     LOKTrigger::trigger_changed(*pTreeView);
                     LOKTrigger::trigger_row_activated(*pTreeView);
                     return true;
@@ -461,6 +465,8 @@ bool ExecuteAction(const OUString& nWindowId, const OUString& rWidget, StringMap
                     sal_Int32 nAbsPos = o3tl::toInt32(rData["data"]);
                     std::unique_ptr<weld::TreeIter> itEntry(pTreeView->make_iterator());
                     pTreeView->get_iter_abs_pos(*itEntry, nAbsPos);
+                    pTreeView->set_cursor_without_notify(*itEntry);
+                    pTreeView->grab_focus();
                     pTreeView->expand_row(*itEntry);
                     return true;
                 }
