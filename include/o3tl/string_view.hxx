LibreOffice/core

commit fa0c012d6c06e9a92093dacf997fe3151272648e

Provide std::u16string_view based o3tl::iterateCodePoints
...as requested in the comments of
<https://gerrit.libreoffice.org/c/core/+/151303> "a11y: Fix returning unpaired
surrogates when retrieving characters" (incl. the additional preAdjustIndex
parameter).

The type of the indexUtf16 parameter obviously needed to be adapted to
std::u16string_view's std::size_t.  But there is no obvious best choice for the
type of the incrementCodePoints parameter (int? std::ssize_t?), so lets leave it
as sal_Int32.

For simplicity of avoiding a Library_o3tl, and to allow o3tl::iterateCodePoints
to be used in the implementation of rtl_uString_iterateCodePoints now,
o3tl::iterateCodePoints is provided as an inline function defined in the include
file.

Change-Id: I8280ca11d2a943bd2b7150a266807b358f321a72
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/151366
Tested-by: Jenkins
Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

include/o3tl/string_view.hxx
@@ -17,8 +17,10 @@
 #include <string_view>
 
 #include <o3tl/intcmp.hxx>
+#include <rtl/character.hxx>
 #include <rtl/ustring.h>
 #include <rtl/math.h>
+#include <sal/types.h>
 
 namespace o3tl
 {
@@ -504,6 +506,58 @@ inline double toDouble(std::u16string_view str)
 inline double toDouble(std::string_view str)
 {
     return rtl_math_stringToDouble(str.data(), str.data() + str.size(), '.', 0, nullptr, nullptr);
+}
+
+// Similar to OUString::iterateCodePoins, but for std::string_view.
+// If preAdjustIndex is true: prior to any other operation, *indexUtf16 is adjusted by -1 if it
+// originally pointed into the middle of a surrogate pair.
+inline sal_uInt32 iterateCodePoints(std::u16string_view string, std::size_t* indexUtf16,
+                                    sal_Int32 incrementCodePoints = 1, bool preAdjustIndex = false)
+{
+    std::size_t n;
+    char16_t cu;
+    sal_uInt32 cp;
+    assert(indexUtf16 != nullptr);
+    n = *indexUtf16;
+    assert(n <= string.length());
+    if (preAdjustIndex && n != 0 && rtl::isHighSurrogate(string[n - 1])
+        && rtl::isLowSurrogate(string[n]))
+    {
+        --n;
+    }
+    while (incrementCodePoints < 0)
+    {
+        assert(n >= 0);
+        cu = string[--n];
+        if (rtl::isLowSurrogate(cu) && n != 0 && rtl::isHighSurrogate(string[n - 1]))
+        {
+            --n;
+        }
+        ++incrementCodePoints;
+    }
+    assert(n < string.length());
+    cu = string[n];
+    if (rtl::isHighSurrogate(cu) && string.length() - n >= 2 && rtl::isLowSurrogate(string[n + 1]))
+    {
+        cp = rtl::combineSurrogates(cu, string[n + 1]);
+    }
+    else
+    {
+        cp = cu;
+    }
+    while (incrementCodePoints > 0)
+    {
+        assert(n < string.length());
+        cu = string[n++];
+        if (rtl::isHighSurrogate(cu) && n != string.length() && rtl::isLowSurrogate(string[n]))
+        {
+            ++n;
+        }
+        --incrementCodePoints;
+    }
+    assert(n <= string.length());
+    *indexUtf16 = n;
+    return cp;
 }
 
 } // namespace

sal/rtl/ustring.cxx
@@ -26,6 +26,9 @@
 #include <string>
 
 #include <config_options.h>
+#include <o3tl/intcmp.hxx>
+#include <o3tl/safeint.hxx>
+#include <o3tl/string_view.hxx>
 #include <osl/diagnose.h>
 #include <osl/interlck.h>
 #include <osl/mutex.h>
@@ -769,43 +772,17 @@ sal_uInt32 SAL_CALL rtl_uString_iterateCodePoints(
     rtl_uString const * string, sal_Int32 * indexUtf16,
     sal_Int32 incrementCodePoints)
 {
-    sal_Int32 n;
-    sal_Unicode cu;
-    sal_uInt32 cp;
     assert(string != nullptr && indexUtf16 != nullptr);
-    n = *indexUtf16;
-    assert(n >= 0 && n <= string->length);
-    while (incrementCodePoints < 0) {
-        assert(n > 0);
-        cu = string->buffer[--n];
-        if (rtl::isLowSurrogate(cu) && n != 0 &&
-            rtl::isHighSurrogate(string->buffer[n - 1]))
-        {
-            --n;
-        }
-        ++incrementCodePoints;
-    }
-    assert(n >= 0 && n < string->length);
-    cu = string->buffer[n];
-    if (rtl::isHighSurrogate(cu) && string->length - n >= 2 &&
-        rtl::isLowSurrogate(string->buffer[n + 1]))
-    {
-        cp = rtl::combineSurrogates(cu, string->buffer[n + 1]);
-    } else {
-        cp = cu;
-    }
-    while (incrementCodePoints > 0) {
-        assert(n < string->length);
-        cu = string->buffer[n++];
-        if (rtl::isHighSurrogate(cu) && n != string->length &&
-            rtl::isLowSurrogate(string->buffer[n]))
-        {
-            ++n;
-        }
-        --incrementCodePoints;
-    }
-    assert(n >= 0 && n <= string->length);
-    *indexUtf16 = n;
+    assert(
+        *indexUtf16 >= 0
+        && o3tl::cmp_less_equal(*indexUtf16, std::numeric_limits<std::size_t>::max()));
+        // using o3tl::cmp_less_equal nicely avoids potential
+        // -Wtautological-constant-out-of-range-compare
+    std::size_t i = *indexUtf16;
+    auto const cp = o3tl::iterateCodePoints(
+        std::u16string_view(string->buffer, string->length), &i, incrementCodePoints);
+    assert(i <= o3tl::make_unsigned(std::numeric_limits<sal_Int32>::max()));
+    *indexUtf16 = i;
     return cp;
 }
 

commit f614e082055dd3cad9885d6e1968d71f1e1b5a05

Fix typo in assert
I accidentally broke this in fa0c012d6c06e9a92093dacf997fe3151272648e "Provide
std::u16string_view based o3tl::iterateCodePoints" when moving the body of code
from rtl_uString_iterateCodePoints to here and then adapting it from sal_Int32-
base OUString to std::size_t-based std::u16string_view.

Change-Id: I5bd97413ac8c8482f409ad5be3797fef47191d33
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/151409
Tested-by: Stephan Bergmann <sbergman@redhat.com>
Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

include/o3tl/string_view.hxx
@@ -527,7 +527,7 @@ inline sal_uInt32 iterateCodePoints(std::u16string_view string, std::size_t* ind
     }
     while (incrementCodePoints < 0)
     {
-        assert(n >= 0);
+        assert(n > 0);
         cu = string[--n];
         if (rtl::isLowSurrogate(cu) && n != 0 && rtl::isHighSurrogate(string[n - 1]))
         {