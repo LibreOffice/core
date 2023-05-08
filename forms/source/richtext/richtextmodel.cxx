63b8e8bda29efbdde0e0bd7f7d2b18d5a9b7b135
Caolán McNamara <caolanm@redhat.com>
2023-05-08 09:35:20 +0100

    tdf#152974 revert change to ORichTextModel::getPropertyDefaultByHandle
    properties that match the default are not exported, so we're basically
    stuck with what is in there for compatibility with older versions, so
    revert
    
    commit ca46afebb7dc0ec4375e995fa90edccbed6c2530
    Date:   Tue Oct 4 16:22:51 2022 +0100
    
        default to 'flat' instead of '3D Look' for form controls
    
    and set m_nBorder to the desired new default in the ctor but
    leave getPropertyDefaultByHandle as it always was
    
    Change-Id: Ia862fc6851248fc9b16b3b4c505fdaf6fdb4b2ac
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/151497
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>

--- forms/source/richtext/richtextmodel.cxx
+++ forms/source/richtext/richtextmodel.cxx
@@ -64,7 +64,9 @@ namespace frm
         m_nClassId = FormComponentType::TEXTFIELD;
 
         getPropertyDefaultByHandle( PROPERTY_ID_DEFAULTCONTROL          ) >>= m_sDefaultControl;
-        getPropertyDefaultByHandle( PROPERTY_ID_BORDER                  ) >>= m_nBorder;
+        // Default to 'flat' instead of '3D Look' for form controls, but don't change
+        // getPropertyDefaultByHandle, see tdf#152974
+        m_nBorder = 2;
         getPropertyDefaultByHandle( PROPERTY_ID_ENABLED                 ) >>= m_bEnabled;
         getPropertyDefaultByHandle( PROPERTY_ID_ENABLEVISIBLE           ) >>= m_bEnableVisible;
         getPropertyDefaultByHandle( PROPERTY_ID_HARDLINEBREAKS          ) >>= m_bHardLineBreaks;
@@ -450,7 +452,9 @@ namespace frm
         }
     }
 
-
+    // note tdf#152974, we can't simply change a default here because properties
+    // that match the default are not exported, so for compatibility these
+    // can't be changed without some sort of solution for that
     Any ORichTextModel::getPropertyDefaultByHandle( sal_Int32 _nHandle ) const
     {
         Any aDefault;
@@ -506,7 +510,7 @@ namespace frm
             break;
 
         case PROPERTY_ID_BORDER:
-            aDefault <<= sal_Int16(2);
+            aDefault <<= sal_Int16(1);
             break;
 
         default:
