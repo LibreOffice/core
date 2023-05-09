LibreOffice/core

commit 3b7db802731826b6cc3b55100470b0c61c1f2dfa

tdf#105404 [API CHANGE] add index to accessiblity change event
Which shaves 80% off the time off breaking up a vector image on Linux.

Change-Id: Id8e7daad001b6120d1fb98e382357da5b55e92ca
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/151352
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/unx/gtk3/a11y/atklistener.cxx
@@ -163,28 +163,40 @@ void AtkListener::updateChildList(
 
 void AtkListener::handleChildAdded(
     const uno::Reference< accessibility::XAccessibleContext >& rxParent,
-    const uno::Reference< accessibility::XAccessible>& rxAccessible)
+    const uno::Reference< accessibility::XAccessible>& rxAccessible,
+    sal_Int32 nIndexHint)
 {
     AtkObject * pChild = rxAccessible.is() ? atk_object_wrapper_ref( rxAccessible ) : nullptr;
 
-    if( pChild )
+    if( !pChild )
+        return;
+
+    if (nIndexHint != -1)
     {
+        sal_Int64 nStateSet = rxParent->getAccessibleStateSet();
+        if( !(nStateSet & accessibility::AccessibleStateType::DEFUNC)
+              || (nStateSet & accessibility::AccessibleStateType::MANAGES_DESCENDANTS) )
+        {
+            m_aChildList.insert(m_aChildList.begin() + nIndexHint, rxAccessible);
+        }
+    }
+    else
         updateChildList(rxParent);
 
-        atk_object_wrapper_add_child( mpWrapper, pChild,
-            atk_object_get_index_in_parent( pChild ));
+    atk_object_wrapper_add_child( mpWrapper, pChild,
+        atk_object_get_index_in_parent( pChild ));
 
-        g_object_unref( pChild );
-    }
+    g_object_unref( pChild );
 }
 
 /*****************************************************************************/
 
 void AtkListener::handleChildRemoved(
     const uno::Reference< accessibility::XAccessibleContext >& rxParent,
-    const uno::Reference< accessibility::XAccessible>& rxChild)
+    const uno::Reference< accessibility::XAccessible>& rxChild,
+    int nChildIndexHint)
 {
-    sal_Int32 nIndex = -1;
+    sal_Int32 nIndex = nChildIndexHint;
 
     // Locate the child in the children list
     const size_t nmax = m_aChildList.size();
@@ -231,7 +243,14 @@ void AtkListener::handleChildRemoved(
         xBroadcaster->removeAccessibleEventListener(xListener);
     }
 
-    updateChildList(rxParent);
+    // update child list
+    sal_Int64 nStateSet = rxParent->getAccessibleStateSet();
+    if(!( (nStateSet & accessibility::AccessibleStateType::DEFUNC)
+        || (nStateSet & accessibility::AccessibleStateType::MANAGES_DESCENDANTS) ))
+    {
+        assert( m_aChildList[nIndex] == rxParent->getAccessibleChild(nIndex) );
+        m_aChildList.erase(m_aChildList.begin() + nIndex);
+    }
 
     AtkObject * pChild = atk_object_wrapper_ref( rxChild, false );
     if( pChild )
@@ -442,10 +461,10 @@ void AtkListener::notifyEvent( const accessibility::AccessibleEventObject& aEven
             g_return_if_fail( xParent.is() );
 
             if( aEvent.OldValue >>= xChild )
-                handleChildRemoved(xParent, xChild);
+                handleChildRemoved(xParent, xChild, aEvent.IndexHint);
 
             if( aEvent.NewValue >>= xChild )
-                handleChildAdded(xParent, xChild);
+                handleChildAdded(xParent, xChild, aEvent.IndexHint);
             break;
         }
 

accessibility/source/extended/accessiblelistboxentry.cxx
@@ -116,7 +116,7 @@ namespace accessibility
                                                    const css::uno::Any& _aNewValue )
     {
         Reference< uno::XInterface > xSource( *this );
-        AccessibleEventObject aEventObj( xSource, _nEventId, _aNewValue, _aOldValue );
+        AccessibleEventObject aEventObj( xSource, _nEventId, _aNewValue, _aOldValue, -1 );
 
         if (m_nClientId)
             comphelper::AccessibleEventNotifier::addEvent( m_nClientId, aEventObj );

accessibility/source/extended/textwindowaccessibility.cxx
@@ -119,7 +119,7 @@ void Paragraph::notifyEvent(::sal_Int16 nEventId,
     if (m_nClientId)
         comphelper::AccessibleEventNotifier::addEvent( m_nClientId, css::accessibility::AccessibleEventObject(
                              getXWeak(),
-                             nEventId, rNewValue, rOldValue) );
+                             nEventId, rNewValue, rOldValue, -1) );
 }
 
 // virtual

chart2/source/controller/accessibility/AccessibleBase.cxx
@@ -350,7 +350,7 @@ void AccessibleBase::BroadcastAccEvent(
     // the const cast is needed, because UNO parameters are never const
     const AccessibleEventObject aEvent(
         const_cast< uno::XWeak * >( static_cast< const uno::XWeak * >( this )),
-        nId, rNew, rOld );
+        nId, rNew, rOld, -1 );
 
     // let the notifier handle this event
     ::comphelper::AccessibleEventNotifier::addEvent( m_nEventNotifierId, aEvent );

editeng/source/accessibility/AccessibleContextBase.cxx
@@ -89,7 +89,7 @@ bool AccessibleContextBase::SetState (sal_Int64 aState)
             CommitChange(
                 AccessibleEventId::STATE_CHANGED,
                 aNewValue,
-                uno::Any());
+                uno::Any(), -1);
         }
         return true;
     }
@@ -112,7 +112,7 @@ bool AccessibleContextBase::ResetState (sal_Int64 aState)
         CommitChange(
             AccessibleEventId::STATE_CHANGED,
             uno::Any(),
-            aOldValue);
+            aOldValue, -1);
         return true;
     }
     else
@@ -144,7 +144,7 @@ void AccessibleContextBase::SetRelationSet (
     for (int i=0; aRelationDescriptors[i].first!=AccessibleRelationType::INVALID; i++)
         if (mxRelationSet->containsRelation(aRelationDescriptors[i].first)
         != rxNewRelationSet->containsRelation(aRelationDescriptors[i].first))
-        CommitChange (aRelationDescriptors[i].second, uno::Any(), uno::Any());
+        CommitChange (aRelationDescriptors[i].second, uno::Any(), uno::Any(), -1);
 
     mxRelationSet = rxNewRelationSet;
 }
@@ -425,7 +425,7 @@ void AccessibleContextBase::SetAccessibleDescription (
     CommitChange(
         AccessibleEventId::DESCRIPTION_CHANGED,
         aNewValue,
-        aOldValue);
+        aOldValue, -1);
 }
 
 
@@ -447,7 +447,7 @@ void AccessibleContextBase::SetAccessibleName (
     CommitChange(
         AccessibleEventId::NAME_CHANGED,
         aNewValue,
-        aOldValue);
+        aOldValue, -1);
 }
 
 
@@ -460,7 +460,8 @@ OUString AccessibleContextBase::CreateAccessibleName()
 void AccessibleContextBase::CommitChange (
     sal_Int16 nEventId,
     const uno::Any& rNewValue,
-    const uno::Any& rOldValue)
+    const uno::Any& rOldValue,
+    sal_Int32 nValueIndex)
 {
     // Do not call FireEvent and do not even create the event object when no
     // listener has been registered yet.  Creating the event object can
@@ -471,7 +472,8 @@ void AccessibleContextBase::CommitChange (
             static_cast<XAccessibleContext*>(this),
             nEventId,
             rNewValue,
-            rOldValue);
+            rOldValue,
+            nValueIndex);
 
         FireEvent (aEvent);
     }

editeng/source/accessibility/AccessibleEditableTextPara.cxx
@@ -551,7 +551,7 @@ namespace accessibility
     {
         uno::Reference < XAccessibleContext > xThis( const_cast< AccessibleEditableTextPara* > (this)->getAccessibleContext() );
 
-        AccessibleEventObject aEvent(xThis, nEventId, rNewValue, rOldValue);
+        AccessibleEventObject aEvent(xThis, nEventId, rNewValue, rOldValue, -1);
 
         // #106234# Delegate to EventNotifier
         if( getNotifierClientId() != -1 )

editeng/source/accessibility/AccessibleImageBullet.cxx
@@ -408,7 +408,7 @@ namespace accessibility
 
         uno::Reference < XAccessibleContext > xThis( const_cast< AccessibleImageBullet* > (this)->getAccessibleContext() );
 
-        AccessibleEventObject aEvent(xThis, nEventId, rNewValue, rOldValue);
+        AccessibleEventObject aEvent(xThis, nEventId, rNewValue, rOldValue, -1);
 
         // #106234# Delegate to EventNotifier
         ::comphelper::AccessibleEventNotifier::addEvent( getNotifierClientId(),

include/editeng/AccessibleContextBase.hxx
@@ -82,7 +82,8 @@ public:
     */
     void CommitChange (sal_Int16 aEventId,
         const css::uno::Any& rNewValue,
-        const css::uno::Any& rOldValue);
+        const css::uno::Any& rOldValue,
+        sal_Int32 nValueIndex);
 
     /** Set a new description and, provided that the new name differs from
         the old one, broadcast an accessibility event.

commit e4f5c7e3bf9a791ce777d1027aa17c93ad8f28a3

tdf#155186 crash Inserting tables and switching slides (gtk3)
regression from

    commit 3b7db802731826b6cc3b55100470b0c61c1f2dfa
    Author: Noel Grandin <noel.grandin@collabora.co.uk>
    Date:   Thu May 4 10:06:14 2023 +0200
    tdf#105404 [API CHANGE] add index to accessiblity change event

where something is forgetting to send events for some of the changes, so
just ignore a bad indexhint here

Change-Id: I9228b480be10b2f267c2a76cb30a9faf1476639e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/151536
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/unx/gtk3/a11y/atklistener.cxx
@@ -171,6 +171,12 @@ void AtkListener::handleChildAdded(
     if( !pChild )
         return;
 
+    if (nIndexHint != -1 && (nIndexHint < 0 || nIndexHint >= static_cast<sal_Int32>(m_aChildList.size())))
+    {
+        SAL_WARN("vcl", "index hint out of range, ignoring");
+        nIndexHint = -1;
+    }
+
     bool bNeedToFullFullChildList = true;
     if (nIndexHint != -1)
     {