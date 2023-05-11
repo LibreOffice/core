LibreOffice/core

commit 3b7db802731826b6cc3b55100470b0c61c1f2dfa

tdf#105404 [API CHANGE] add index to accessiblity change event
Which shaves 80% off the time off breaking up a vector image on Linux.

Change-Id: Id8e7daad001b6120d1fb98e382357da5b55e92ca
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/151352
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

editeng/source/accessibility/AccessibleContextBase.cxx
@@ -89,7 +89,7 @@ bool AccessibleContextBase::SetState (sal_Int64 aState)
             CommitChange(
                 AccessibleEventId::STATE_CHANGED,
                 aNewValue,
-                uno::Any());
+                uno::Any(), -1);
         }
         return true;
     }
@@ -112,7 +112,7 @@ bool AccessibleContextBase::ResetState (sal_Int64 aState)
         CommitChange(
             AccessibleEventId::STATE_CHANGED,
             uno::Any(),
-            aOldValue);
+            aOldValue, -1);
         return true;
     }
     else
@@ -144,7 +144,7 @@ void AccessibleContextBase::SetRelationSet (
     for (int i=0; aRelationDescriptors[i].first!=AccessibleRelationType::INVALID; i++)
         if (mxRelationSet->containsRelation(aRelationDescriptors[i].first)
         != rxNewRelationSet->containsRelation(aRelationDescriptors[i].first))
-        CommitChange (aRelationDescriptors[i].second, uno::Any(), uno::Any());
+        CommitChange (aRelationDescriptors[i].second, uno::Any(), uno::Any(), -1);
 
     mxRelationSet = rxNewRelationSet;
 }
@@ -425,7 +425,7 @@ void AccessibleContextBase::SetAccessibleDescription (
     CommitChange(
         AccessibleEventId::DESCRIPTION_CHANGED,
         aNewValue,
-        aOldValue);
+        aOldValue, -1);
 }
 
 
@@ -447,7 +447,7 @@ void AccessibleContextBase::SetAccessibleName (
     CommitChange(
         AccessibleEventId::NAME_CHANGED,
         aNewValue,
-        aOldValue);
+        aOldValue, -1);
 }
 
 
@@ -460,7 +460,8 @@ OUString AccessibleContextBase::CreateAccessibleName()
 void AccessibleContextBase::CommitChange (
     sal_Int16 nEventId,
     const uno::Any& rNewValue,
-    const uno::Any& rOldValue)
+    const uno::Any& rOldValue,
+    sal_Int32 nValueIndex)
 {
     // Do not call FireEvent and do not even create the event object when no
     // listener has been registered yet.  Creating the event object can
@@ -471,7 +472,8 @@ void AccessibleContextBase::CommitChange (
             static_cast<XAccessibleContext*>(this),
             nEventId,
             rNewValue,
-            rOldValue);
+            rOldValue,
+            nValueIndex);
 
         FireEvent (aEvent);
     }

sd/source/ui/accessibility/AccessibleDocumentViewBase.cxx
@@ -599,7 +599,7 @@ void AccessibleDocumentViewBase::SetAccessibleOLEObject (
             CommitChange (
                 AccessibleEventId::CHILD,
                 uno::Any(),
-                uno::Any (mxAccessibleOLEObject));
+                uno::Any (mxAccessibleOLEObject), -1);
 
     // Assume that the accessible OLE Object disposes itself correctly.
 
@@ -613,7 +613,7 @@ void AccessibleDocumentViewBase::SetAccessibleOLEObject (
         CommitChange (
             AccessibleEventId::CHILD,
             uno::Any (mxAccessibleOLEObject),
-            uno::Any());
+            uno::Any(), -1);
 }
 
 //=====  methods from AccessibleSelectionBase ==================================================

svx/source/accessibility/AccessibleShape.cxx
@@ -906,7 +906,7 @@ void SAL_CALL
     CommitChange (
         AccessibleEventId::VISIBLE_DATA_CHANGED,
         uno::Any(),
-        uno::Any());
+        uno::Any(), -1);
 
     // Name and Description may have changed.  Update the local
     // values accordingly.
@@ -923,7 +923,7 @@ void AccessibleShape::ViewForwarderChanged()
     // and/or position) of the shape has changed.
     CommitChange (AccessibleEventId::VISIBLE_DATA_CHANGED,
         uno::Any(),
-        uno::Any());
+        uno::Any(), -1);
 
     // Tell children manager of the modified view forwarder.
     if (mpChildrenManager != nullptr)
@@ -976,7 +976,7 @@ OUString AccessibleShape::GetFullAccessibleName (AccessibleShape *shape)
         CommitChange(
             AccessibleEventId::NAME_CHANGED,
             aNewValue,
-            aOldValue);
+            aOldValue, -1);
     }
     aAccName = sName;
     return sName;

accessibility/source/extended/accessiblelistboxentry.cxx
@@ -116,7 +116,7 @@ namespace accessibility
                                                    const css::uno::Any& _aNewValue )
     {
         Reference< uno::XInterface > xSource( *this );
-        AccessibleEventObject aEventObj( xSource, _nEventId, _aNewValue, _aOldValue );
+        AccessibleEventObject aEventObj( xSource, _nEventId, _aNewValue, _aOldValue, -1 );
 
         if (m_nClientId)
             comphelper::AccessibleEventNotifier::addEvent( m_nClientId, aEventObj );

accessibility/source/extended/textwindowaccessibility.cxx
@@ -119,7 +119,7 @@ void Paragraph::notifyEvent(::sal_Int16 nEventId,
     if (m_nClientId)
         comphelper::AccessibleEventNotifier::addEvent( m_nClientId, css::accessibility::AccessibleEventObject(
                              getXWeak(),
-                             nEventId, rNewValue, rOldValue) );
+                             nEventId, rNewValue, rOldValue, -1) );
 }
 
 // virtual

chart2/source/controller/accessibility/AccessibleBase.cxx
@@ -350,7 +350,7 @@ void AccessibleBase::BroadcastAccEvent(
     // the const cast is needed, because UNO parameters are never const
     const AccessibleEventObject aEvent(
         const_cast< uno::XWeak * >( static_cast< const uno::XWeak * >( this )),
-        nId, rNew, rOld );
+        nId, rNew, rOld, -1 );
 
     // let the notifier handle this event
     ::comphelper::AccessibleEventNotifier::addEvent( m_nEventNotifierId, aEvent );

editeng/source/accessibility/AccessibleEditableTextPara.cxx
@@ -551,7 +551,7 @@ namespace accessibility
     {
         uno::Reference < XAccessibleContext > xThis( const_cast< AccessibleEditableTextPara* > (this)->getAccessibleContext() );
 
-        AccessibleEventObject aEvent(xThis, nEventId, rNewValue, rOldValue);
+        AccessibleEventObject aEvent(xThis, nEventId, rNewValue, rOldValue, -1);
 
         // #106234# Delegate to EventNotifier
         if( getNotifierClientId() != -1 )

editeng/source/accessibility/AccessibleImageBullet.cxx
@@ -408,7 +408,7 @@ namespace accessibility
 
         uno::Reference < XAccessibleContext > xThis( const_cast< AccessibleImageBullet* > (this)->getAccessibleContext() );
 
-        AccessibleEventObject aEvent(xThis, nEventId, rNewValue, rOldValue);
+        AccessibleEventObject aEvent(xThis, nEventId, rNewValue, rOldValue, -1);
 
         // #106234# Delegate to EventNotifier
         ::comphelper::AccessibleEventNotifier::addEvent( getNotifierClientId(),

commit d309298d1f70f4ffbf699b1aa1b2bfe08be8c4ef

tdf#155235 Draw crashes on close: SIGSEGV in SalInstance:GetYieldMutex()
This is a regression from
    commit 3b7db802731826b6cc3b55100470b0c61c1f2dfa
    Author: Noel Grandin <noel.grandin@collabora.co.uk>
    Date:   Thu May 4 10:06:14 2023 +0200
    tdf#105404 [API CHANGE] add index to accessiblity change event

The list of children in DocumentFocusListener is getting out of sync
because something is not sending add/remove events for accessible
objects, a problem which was hidden before the commit above.

I can't find the add/remove problem, so do the next best thing - fix the
dispose methods of various accessible objects so that they clear
references to other objects, which results in the relevant objects being
destructed in an order which does not lead to crashes.

Change-Id: Iac7f48d6e3f2bdf30edb5c7fd3c5de861aec4b7a
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/151657
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

editeng/source/accessibility/AccessibleContextBase.cxx
@@ -404,6 +404,8 @@ void SAL_CALL AccessibleContextBase::disposing()
         comphelper::AccessibleEventNotifier::revokeClientNotifyDisposing( mnClientId, *this );
         mnClientId =  0;
     }
+    mxParent.clear();
+    mxRelationSet.clear();
 }
 
 

sd/source/ui/accessibility/AccessibleDocumentViewBase.cxx
@@ -480,6 +480,8 @@ void AccessibleDocumentViewBase::impl_dispose()
     mxController = nullptr;
 
     maShapeTreeInfo.SetDocumentWindow (nullptr);
+    maShapeTreeInfo.dispose();
+    mxAccessibleOLEObject.clear();
 }
 
 //=====  XEventListener  ======================================================

svx/source/accessibility/AccessibleShape.cxx
@@ -887,6 +887,10 @@ void AccessibleShape::disposing (const lang::EventObject& aEvent)
     {
         TOOLS_WARN_EXCEPTION("svx", "caught exception while disposing");
     }
+    mpChildrenManager.reset();
+    mxShape.clear();
+    maShapeTreeInfo.dispose();
+    mpText.reset();
 }
 
 // document::XShapeEventListener

svx/source/accessibility/AccessibleShapeTreeInfo.cxx
@@ -74,8 +74,11 @@ AccessibleShapeTreeInfo& AccessibleShapeTreeInfo::operator= (const AccessibleSha
 
 AccessibleShapeTreeInfo::~AccessibleShapeTreeInfo()
 {
-    SolarMutexGuard g;
-    mpWindow.reset();
+    if (mpWindow)
+    {
+        SolarMutexGuard g;
+        mpWindow.reset();
+    }
 }
 
 void AccessibleShapeTreeInfo::SetDocumentWindow (