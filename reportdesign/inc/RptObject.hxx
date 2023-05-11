LibreOffice/core

commit 09cb778b6eb7d3a5b9029965a1320b49c90e7295

clean up SdrObject cloning
using operator= implies that overwriting an SdrObject is a
useful operation, but that is not at all true - they are typically
linked into and referred to by many other things.

So rather use a copy-constructor.

Also clean up a couple of weird "do some stuff after the clone"
code into the main copy constructor.

Change-Id: Iefc1481b527602748b5f3abed06e7cca66c0581c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/110633
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

reportdesign/inc/RptObject.hxx
@@ -190,8 +190,6 @@ public:
     virtual OOle2Obj* CloneSdrObject(SdrModel& rTargetModel) const override;
     virtual void initializeOle() override;
 
-    OOle2Obj& operator=(const OOle2Obj& rObj);
-
     void initializeChart( const css::uno::Reference< css::frame::XModel>& _xModel);
 
 private:
@@ -203,6 +201,8 @@ private:
         SdrModel& rSdrModel,
         const OUString& _sComponentName,
         SdrObjKind _nType);
+    // copy constructor
+    OOle2Obj(SdrModel& rSdrModel, const OOle2Obj& rSource);
 
     virtual void NbcMove( const Size& rSize ) override;
     virtual void NbcResize(const Point& rRef, const Fraction& xFact, const Fraction& yFact) override;
@@ -241,6 +241,8 @@ protected:
         const css::uno::Reference< css::report::XReportComponent>& _xComponent,
         const OUString& rModelName,
         SdrObjKind _nObjectType);
+    // copy constructor
+    OUnoObject(SdrModel& rSdrModel, OUnoObject const & rSource);
 
     // protected destructor
     virtual ~OUnoObject() override;
@@ -269,8 +271,6 @@ public:
     virtual SdrObjKind GetObjIdentifier() const override;
     virtual SdrInventor GetObjInventor() const override;
     virtual OUnoObject* CloneSdrObject(SdrModel& rTargetModel) const override;
-
-    OUnoObject& operator=(const OUnoObject& rObj);
 
 private:
     virtual void impl_setUnoShape( const css::uno::Reference< css::uno::XInterface >& rxUnoShape ) override;

reportdesign/source/core/sdr/RptObject.cxx
@@ -586,6 +586,22 @@ OUnoObject::OUnoObject(
 }
 
 OUnoObject::OUnoObject(
+    SdrModel& rSdrModel, OUnoObject const & rSource)
+:   SdrUnoObj(rSdrModel, rSource.getUnoControlModelTypeName())
+    ,OObjectBase(rSource.getServiceName())
+    ,m_nObjectType(rSource.m_nObjectType)
+    // tdf#119067
+    ,m_bSetDefaultLabel(rSource.m_bSetDefaultLabel)
+{
+    if ( !rSource.getUnoControlModelTypeName().isEmpty() )
+        impl_initializeModel_nothrow();
+    Reference<XPropertySet> xSource(const_cast<OUnoObject&>(rSource).getUnoShape(), uno::UNO_QUERY);
+    Reference<XPropertySet> xDest(getUnoShape(), uno::UNO_QUERY);
+    if ( xSource.is() && xDest.is() )
+        comphelper::copyProperties(xSource.get(), xDest.get());
+}
+
+OUnoObject::OUnoObject(
     SdrModel& rSdrModel,
     const uno::Reference< report::XReportComponent>& _xComponent,
     const OUString& rModelName,
@@ -886,21 +902,7 @@ void OUnoObject::impl_setUnoShape( const uno::Reference< uno::XInterface >& rxUn
 
 OUnoObject* OUnoObject::CloneSdrObject(SdrModel& rTargetModel) const
 {
-    return CloneHelper< OUnoObject >(rTargetModel);
-}
-
-OUnoObject& OUnoObject::operator=(const OUnoObject& rObj)
-{
-    if( this == &rObj )
-        return *this;
-    SdrUnoObj::operator=(rObj);
-
-    Reference<XPropertySet> xSource(const_cast<OUnoObject&>(rObj).getUnoShape(), uno::UNO_QUERY);
-    Reference<XPropertySet> xDest(getUnoShape(), uno::UNO_QUERY);
-    if ( xSource.is() && xDest.is() )
-        comphelper::copyProperties(xSource.get(), xDest.get());
-
-    return *this;
+    return new OUnoObject(rTargetModel, *this);
 }
 
 // OOle2Obj
@@ -927,6 +929,28 @@ OOle2Obj::OOle2Obj(
     ,m_bOnlyOnce(true)
 {
     m_bIsListening = true;
+}
+
+static uno::Reference< chart2::data::XDatabaseDataProvider > lcl_getDataProvider(const uno::Reference < embed::XEmbeddedObject >& _xObj);
+
+OOle2Obj::OOle2Obj(SdrModel& rSdrModel, OOle2Obj const & rSource)
+:   SdrOle2Obj(rSdrModel, rSource)
+    ,OObjectBase(rSource.getServiceName())
+    ,m_nType(rSource.m_nType)
+    ,m_bOnlyOnce(rSource.m_bOnlyOnce)
+{
+    m_bIsListening = true;
+
+    OReportModel& rRptModel(static_cast< OReportModel& >(getSdrModelFromSdrObject()));
+    svt::EmbeddedObjectRef::TryRunningState( GetObjRef() );
+    impl_createDataProvider_nothrow(rRptModel.getReportDefinition().get());
+
+    uno::Reference< chart2::data::XDatabaseDataProvider > xSource( lcl_getDataProvider(rSource.GetObjRef()) );
+    uno::Reference< chart2::data::XDatabaseDataProvider > xDest( lcl_getDataProvider(GetObjRef()) );
+    if ( xSource.is() && xDest.is() )
+        comphelper::copyProperties(xSource.get(),xDest.get());
+
+    initializeChart(rRptModel.getReportDefinition().get());
 }
 
 OOle2Obj::~OOle2Obj()
@@ -1092,27 +1116,7 @@ static uno::Reference< chart2::data::XDatabaseDataProvider > lcl_getDataProvider
 // Clone() should make a complete copy of the object.
 OOle2Obj* OOle2Obj::CloneSdrObject(SdrModel& rTargetModel) const
 {
-    return CloneHelper< OOle2Obj >(rTargetModel);
-}
-
-OOle2Obj& OOle2Obj::operator=(const OOle2Obj& rObj)
-{
-    if( this == &rObj )
-        return *this;
-    SdrOle2Obj::operator=(rObj);
-
-    OReportModel& rRptModel(static_cast< OReportModel& >(getSdrModelFromSdrObject()));
-    svt::EmbeddedObjectRef::TryRunningState( GetObjRef() );
-    impl_createDataProvider_nothrow(rRptModel.getReportDefinition().get());
-
-    uno::Reference< chart2::data::XDatabaseDataProvider > xSource( lcl_getDataProvider(rObj.GetObjRef()) );
-    uno::Reference< chart2::data::XDatabaseDataProvider > xDest( lcl_getDataProvider(GetObjRef()) );
-    if ( xSource.is() && xDest.is() )
-        comphelper::copyProperties(xSource.get(),xDest.get());
-
-    initializeChart(rRptModel.getReportDefinition().get());
-
-    return *this;
+    return new OOle2Obj(rTargetModel, *this);
 }
 
 void OOle2Obj::impl_createDataProvider_nothrow(const uno::Reference< frame::XModel>& _xModel)

basctl/source/dlged/dlgedobj.cxx
@@ -74,6 +74,44 @@ DlgEdObj::DlgEdObj(SdrModel& rSdrModel)
 {
 }
 
+DlgEdObj::DlgEdObj(SdrModel& rSdrModel, DlgEdObj const & rSource)
+:   SdrUnoObj(rSdrModel, rSource)
+    ,bIsListening(false)
+{
+    // set parent form
+    pDlgEdForm = rSource.pDlgEdForm;
+
+    // add child to parent form
+    pDlgEdForm->AddChild( this );
+
+    Reference< beans::XPropertySet > xPSet( GetUnoControlModel(), UNO_QUERY );
+    if ( xPSet.is() )
+    {
+        // set new name
+        OUString aOUniqueName( GetUniqueName() );
+        Any aUniqueName;
+        aUniqueName <<= aOUniqueName;
+        xPSet->setPropertyValue( DLGED_PROP_NAME, aUniqueName );
+
+        Reference< container::XNameContainer > xCont( GetDlgEdForm()->GetUnoControlModel() , UNO_QUERY );
+        if ( xCont.is() )
+        {
+            // set tabindex
+            Sequence< OUString > aNames = xCont->getElementNames();
+            xPSet->setPropertyValue( DLGED_PROP_TABINDEX, Any(static_cast<sal_Int16>(aNames.getLength())) );
+
+            // insert control model in dialog model
+            Reference< awt::XControlModel > xCtrl( xPSet , UNO_QUERY );
+            xCont->insertByName( aOUniqueName, Any(xCtrl) );
+
+            pDlgEdForm->UpdateTabOrderAndGroups();
+        }
+    }
+
+    // start listening
+    StartListening();
+}
+
 DlgEdObj::DlgEdObj(
     SdrModel& rSdrModel,
     const OUString& rModelName,
@@ -868,62 +906,16 @@ SdrObjKind DlgEdObj::GetObjIdentifier() const
     }
 }
 
-void DlgEdObj::clonedFrom(const DlgEdObj* _pSource)
-{
-    // set parent form
-    pDlgEdForm = _pSource->pDlgEdForm;
-
-    // add child to parent form
-    pDlgEdForm->AddChild( this );
-
-    Reference< beans::XPropertySet > xPSet( GetUnoControlModel(), UNO_QUERY );
-    if ( xPSet.is() )
-    {
-        // set new name
-        OUString aOUniqueName( GetUniqueName() );
-        Any aUniqueName;
-        aUniqueName <<= aOUniqueName;
-        xPSet->setPropertyValue( DLGED_PROP_NAME, aUniqueName );
-
-        Reference< container::XNameContainer > xCont( GetDlgEdForm()->GetUnoControlModel() , UNO_QUERY );
-        if ( xCont.is() )
-        {
-            // set tabindex
-            Sequence< OUString > aNames = xCont->getElementNames();
-            xPSet->setPropertyValue( DLGED_PROP_TABINDEX, Any(static_cast<sal_Int16>(aNames.getLength())) );
-
-            // insert control model in dialog model
-            Reference< awt::XControlModel > xCtrl( xPSet , UNO_QUERY );
-            xCont->insertByName( aOUniqueName, Any(xCtrl) );
-
-            pDlgEdForm->UpdateTabOrderAndGroups();
-        }
-    }
-
-    // start listening
-    StartListening();
-}
-
 DlgEdObj* DlgEdObj::CloneSdrObject(SdrModel& rTargetModel) const
 {
-    DlgEdObj* pDlgEdObj = CloneHelper< DlgEdObj >(rTargetModel);
-    DBG_ASSERT( pDlgEdObj != nullptr, "DlgEdObj::Clone: invalid clone!" );
-    if ( pDlgEdObj )
-        pDlgEdObj->clonedFrom( this );
-
-    return pDlgEdObj;
+    return new DlgEdObj(rTargetModel, *this);
 }
 
 SdrObjectUniquePtr DlgEdObj::getFullDragClone() const
 {
     // no need to really add the clone for dragging, it's a temporary
     // object
-    SdrObjectUniquePtr pObj( new SdrUnoObj(
-        getSdrModelFromSdrObject(),
-        OUString()) );
-    *pObj = *static_cast<const SdrUnoObj*>(this);
-
-    return pObj;
+    return SdrObjectUniquePtr(new SdrUnoObj(getSdrModelFromSdrObject(), *this));
 }
 
 void DlgEdObj::NbcMove( const Size& rSize )

basctl/source/inc/dlgedobj.hxx
@@ -59,6 +59,8 @@ private:
 
 protected:
     DlgEdObj(SdrModel& rSdrModel);
+    // copy constructor
+    DlgEdObj(SdrModel& rSdrModel, DlgEdObj const & rSource);
     DlgEdObj(
         SdrModel& rSdrModel,
         const OUString& rModelName,
@@ -91,11 +93,6 @@ protected:
         sal_Int32& nXOut, sal_Int32& nYOut, sal_Int32& nWidthOut, sal_Int32& nHeightOut );
 
 public:
-    DlgEdObj(DlgEdObj const &) = delete; // due to SdrUnoObj
-    DlgEdObj(DlgEdObj &&) = delete; // due to SdrUnoObj
-    DlgEdObj & operator =(DlgEdObj const &) = default;
-    DlgEdObj & operator =(DlgEdObj &&) = default;
-
     void SetDlgEdForm( DlgEdForm* pForm ) { pDlgEdForm = pForm; }
     DlgEdForm* GetDlgEdForm() const { return pDlgEdForm; }
 
@@ -103,7 +100,6 @@ public:
     virtual SdrObjKind GetObjIdentifier() const override;
 
     virtual DlgEdObj* CloneSdrObject(SdrModel& rTargetModel) const override;                                          // not working yet
-    void clonedFrom(const DlgEdObj* _pSource);                          // not working yet
 
     // FullDrag support
     virtual SdrObjectUniquePtr getFullDragClone() const override;

include/svx/cube3d.hxx
@@ -65,15 +65,13 @@ public:
         const E3dDefaultAttributes& rDefault,
         const basegfx::B3DPoint& aPos,
         const basegfx::B3DVector& r3DSize);
+    E3dCubeObj(SdrModel& rSdrModel, E3dCubeObj const &);
     E3dCubeObj(SdrModel& rSdrModel);
 
     virtual SdrObjKind GetObjIdentifier() const override;
     virtual SdrObjectUniquePtr DoConvertToPolyObj(bool bBezier, bool bAddText) const override;
 
     virtual E3dCubeObj* CloneSdrObject(SdrModel& rTargetModel) const override;
-
-    // implemented mainly for the purposes of Clone()
-    E3dCubeObj& operator=(const E3dCubeObj& rObj);
 
     // Set local parameters with geometry recreation
     void SetCubePos(const basegfx::B3DPoint& rNew);

include/svx/lathe3d.hxx
@@ -56,6 +56,7 @@ public:
         SdrModel& rSdrModel,
         const E3dDefaultAttributes& rDefault,
         const basegfx::B2DPolyPolygon& rPoly2D);
+    E3dLatheObj(SdrModel& rSdrModel, E3dLatheObj const & rSource);
     E3dLatheObj(SdrModel& rSdrModel);
 
     // HorizontalSegments:
@@ -101,9 +102,6 @@ public:
     virtual SdrObjKind GetObjIdentifier() const override;
 
     virtual E3dLatheObj* CloneSdrObject(SdrModel& rTargetModel) const override;
-
-    // implemented mainly for the purposes of Clone()
-    E3dLatheObj& operator=(const E3dLatheObj& rObj);
 
     virtual SdrObjectUniquePtr DoConvertToPolyObj(bool bBezier, bool bAddText) const override;
 

include/svx/obj3d.hxx
@@ -83,6 +83,7 @@ protected:
     // and no instances should be created from anyone, so i move the constructors
     // to protected area
     E3dObject(SdrModel& rSdrModel);
+    E3dObject(SdrModel& rSdrModel, E3dObject const & rSource);
 
     // protected destructor
     virtual ~E3dObject() override;
@@ -125,7 +126,6 @@ public:
     virtual OUString TakeObjNameSingul() const override;
     virtual OUString TakeObjNamePlural() const override;
     virtual E3dObject* CloneSdrObject(SdrModel& rTargetModel) const override;
-    E3dObject& operator=( const E3dObject& rObj );
 
     virtual std::unique_ptr<SdrObjGeoData> NewGeoData() const override;
     virtual void          SaveGeoData(SdrObjGeoData& rGeo) const override;
@@ -170,6 +170,7 @@ protected:
     virtual ~E3dCompoundObject() override;
 
 public:
+    E3dCompoundObject(SdrModel& rSdrModel, E3dCompoundObject const & rSource);
     E3dCompoundObject(SdrModel& rSdrModel);
 
     virtual basegfx::B2DPolyPolygon TakeXorPoly() const override;
@@ -180,9 +181,6 @@ public:
     virtual void RecalcSnapRect() override;
 
     virtual E3dCompoundObject* CloneSdrObject(SdrModel& rTargetModel) const override;
-
-    // implemented mainly for the purposes of Clone()
-    E3dCompoundObject& operator=(const E3dCompoundObject& rObj);
 };
 
 #endif // INCLUDED_SVX_OBJ3D_HXX

include/svx/scene3d.hxx
@@ -83,6 +83,7 @@ protected:
 
 public:
     E3dScene(SdrModel& rSdrModel);
+    E3dScene(SdrModel& rSdrModel, E3dScene const &);
 
     virtual void StructureChanged() override;
 
@@ -128,7 +129,6 @@ public:
     void removeAllNonSelectedObjects();
 
     virtual E3dScene* CloneSdrObject(SdrModel& rTargetModel) const override;
-    E3dScene& operator=(const E3dScene&);
 
     virtual std::unique_ptr<SdrObjGeoData> NewGeoData() const override;
     virtual void          SaveGeoData(SdrObjGeoData& rGeo) const override;

commit 957ac2bd3e6be97179f87f96fb9b5f13c0635834

tdf#150732 ReportBuilder,Moving fields breaks connection of field to datasource
regression from
    commit 09cb778b6eb7d3a5b9029965a1320b49c90e7295
    Author: Noel <noel.grandin@collabora.co.uk>
    Date:   Tue Feb 9 13:42:22 2021 +0200
    clean up SdrObject cloning

The changed order of initialisation of the copied objects meant that
they ended up with a missing servicename, which broke copying of
properties

Change-Id: I76a2a4721f1e3669684094a86cd501c03f80206d
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/151672
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

reportdesign/inc/RptObject.hxx
@@ -145,8 +145,7 @@ private:
         SdrModel& rSdrModel,
         const css::uno::Reference< css::report::XReportComponent>& _xComponent);
     OCustomShape(
-        SdrModel& rSdrModel,
-        const OUString& _sComponentName);
+        SdrModel& rSdrModel);
 
     virtual void NbcMove( const Size& rSize ) override;
     virtual void NbcResize(const Point& rRef, const Fraction& xFact, const Fraction& yFact) override;
@@ -195,7 +194,6 @@ private:
         SdrObjKind _nType);
     OOle2Obj(
         SdrModel& rSdrModel,
-        const OUString& _sComponentName,
         SdrObjKind _nType);
     // copy constructor
     OOle2Obj(SdrModel& rSdrModel, const OOle2Obj& rSource);
@@ -228,7 +226,6 @@ class REPORTDESIGN_DLLPUBLIC OUnoObject final : public SdrUnoObj , public OObjec
     bool         m_bSetDefaultLabel;
 
     OUnoObject(SdrModel& rSdrModel,
-        const OUString& _sComponentName,
         const OUString& rModelName,
         SdrObjKind _nObjectType);
     OUnoObject(

reportdesign/source/core/sdr/RptObject.cxx
@@ -99,7 +99,7 @@ rtl::Reference<SdrObject> OObjectBase::createObject(
                 rtl::Reference<OUnoObject> pUnoObj = new OUnoObject(
                     rTargetModel,
                     _xComponent,
-                    OUString("com.sun.star.form.component.FixedText"),
+                    "com.sun.star.form.component.FixedText",
                     SdrObjKind::ReportDesignFixedText);
                 pNewObj = pUnoObj;
 
@@ -112,14 +112,14 @@ rtl::Reference<SdrObject> OObjectBase::createObject(
             pNewObj = new OUnoObject(
                 rTargetModel,
                 _xComponent,
-                OUString("com.sun.star.form.component.DatabaseImageControl"),
+                "com.sun.star.form.component.DatabaseImageControl",
                 SdrObjKind::ReportDesignImageControl);
             break;
         case SdrObjKind::ReportDesignFormattedField:
             pNewObj = new OUnoObject(
                 rTargetModel,
                 _xComponent,
-                OUString("com.sun.star.form.component.FormattedField"),
+                "com.sun.star.form.component.FormattedField",
                 SdrObjKind::ReportDesignFormattedField);
             break;
         case SdrObjKind::ReportDesignHorizontalFixedLine:
@@ -127,7 +127,7 @@ rtl::Reference<SdrObject> OObjectBase::createObject(
             pNewObj = new OUnoObject(
                 rTargetModel,
                 _xComponent,
-                OUString("com.sun.star.awt.UnoControlFixedLineModel"),
+                "com.sun.star.awt.UnoControlFixedLineModel",
                 nType);
             break;
         case SdrObjKind::CustomShape:
@@ -314,6 +314,7 @@ OObjectBase::OObjectBase(OUString _sComponentName)
 :m_sComponentName(std::move(_sComponentName))
 ,m_bIsListening(false)
 {
+    assert(!m_sComponentName.isEmpty());
 }
 
 OObjectBase::~OObjectBase()
@@ -438,10 +439,9 @@ OCustomShape::OCustomShape(
 }
 
 OCustomShape::OCustomShape(
-    SdrModel& rSdrModel,
-    const OUString& _sComponentName)
+    SdrModel& rSdrModel)
 :   SdrObjCustomShape(rSdrModel)
-    ,OObjectBase(_sComponentName)
+    ,OObjectBase(SERVICE_SHAPE)
 {
     m_bIsListening = true;
 }
@@ -545,13 +545,37 @@ void OCustomShape::setUnoShape( const uno::Reference< drawing::XShape >& rxUnoSh
     m_xReportComponent.clear();
 }
 
+static OUString ObjectTypeToServiceName(SdrObjKind _nObjectType)
+{
+    switch (_nObjectType)
+    {
+    case SdrObjKind::ReportDesignFixedText:
+        return SERVICE_FIXEDTEXT;
+    case SdrObjKind::ReportDesignImageControl:
+        return SERVICE_IMAGECONTROL;
+    case SdrObjKind::ReportDesignFormattedField:
+        return SERVICE_FORMATTEDFIELD;
+    case SdrObjKind::ReportDesignVerticalFixedLine:
+    case SdrObjKind::ReportDesignHorizontalFixedLine:
+        return SERVICE_FIXEDLINE;
+    case SdrObjKind::CustomShape:
+        return SERVICE_SHAPE;
+    case SdrObjKind::ReportDesignSubReport:
+        return SERVICE_REPORTDEFINITION;
+    case SdrObjKind::OLE2:
+        return "com.sun.star.chart2.ChartDocument";
+    default:
+        break;
+    }
+    assert(false && "Unknown object id");
+    return "";
+}
 OUnoObject::OUnoObject(
     SdrModel& rSdrModel,
-    const OUString& _sComponentName,
     const OUString& rModelName,
     SdrObjKind _nObjectType)
 :   SdrUnoObj(rSdrModel, rModelName)
-    ,OObjectBase(_sComponentName)
+    ,OObjectBase(ObjectTypeToServiceName(_nObjectType))
     ,m_nObjectType(_nObjectType)
     // tdf#119067
     ,m_bSetDefaultLabel(false)
@@ -563,7 +587,7 @@ OUnoObject::OUnoObject(
 OUnoObject::OUnoObject(
     SdrModel& rSdrModel, OUnoObject const & rSource)
 :   SdrUnoObj(rSdrModel, rSource)
-    ,OObjectBase(rSource.getServiceName())
+    ,OObjectBase(ObjectTypeToServiceName(rSource.m_nObjectType)) // source may not have a service name
     ,m_nObjectType(rSource.m_nObjectType)
     // tdf#119067
     ,m_bSetDefaultLabel(rSource.m_bSetDefaultLabel)
@@ -900,10 +924,9 @@ OOle2Obj::OOle2Obj(
 
 OOle2Obj::OOle2Obj(
     SdrModel& rSdrModel,
-    const OUString& _sComponentName,
     SdrObjKind _nType)
 :   SdrOle2Obj(rSdrModel)
-    ,OObjectBase(_sComponentName)
+    ,OObjectBase(ObjectTypeToServiceName(_nType))
     ,m_nType(_nType)
     ,m_bOnlyOnce(true)
 {

reportdesign/source/ui/report/dlgedfac.cxx
@@ -49,25 +49,25 @@ IMPL_STATIC_LINK(
         switch( aParams.nObjIdentifier )
         {
             case SdrObjKind::ReportDesignFixedText:
-                    pNewObj = new OUnoObject(aParams.rSdrModel, SERVICE_FIXEDTEXT
-                                                        ,OUString("com.sun.star.form.component.FixedText")
+                    pNewObj = new OUnoObject(aParams.rSdrModel
+                                                        ,"com.sun.star.form.component.FixedText"
                                                         ,SdrObjKind::ReportDesignFixedText);
                     break;
             case SdrObjKind::ReportDesignImageControl:
-                    pNewObj = new OUnoObject(aParams.rSdrModel, SERVICE_IMAGECONTROL
-                                                        ,OUString("com.sun.star.form.component.DatabaseImageControl")
+                    pNewObj = new OUnoObject(aParams.rSdrModel
+                                                        ,"com.sun.star.form.component.DatabaseImageControl"
                                                         ,SdrObjKind::ReportDesignImageControl);
                     break;
             case SdrObjKind::ReportDesignFormattedField:
-                    pNewObj = new OUnoObject(aParams.rSdrModel, SERVICE_FORMATTEDFIELD
-                                                        ,OUString("com.sun.star.form.component.FormattedField")
+                    pNewObj = new OUnoObject(aParams.rSdrModel
+                                                        ,"com.sun.star.form.component.FormattedField"
                                                         ,SdrObjKind::ReportDesignFormattedField);
                     break;
             case SdrObjKind::ReportDesignVerticalFixedLine:
             case SdrObjKind::ReportDesignHorizontalFixedLine:
                 {
-                    rtl::Reference<OUnoObject> pObj = new OUnoObject(aParams.rSdrModel, SERVICE_FIXEDLINE
-                                                        ,OUString("com.sun.star.awt.UnoControlFixedLineModel")
+                    rtl::Reference<OUnoObject> pObj = new OUnoObject(aParams.rSdrModel
+                                                        ,"com.sun.star.awt.UnoControlFixedLineModel"
                                                         ,aParams.nObjIdentifier);
                     pNewObj = pObj;
                     if ( aParams.nObjIdentifier == SdrObjKind::ReportDesignHorizontalFixedLine )
@@ -78,13 +78,13 @@ IMPL_STATIC_LINK(
                 }
                 break;
             case SdrObjKind::CustomShape:
-                pNewObj = new OCustomShape(aParams.rSdrModel, SERVICE_SHAPE);
+                pNewObj = new OCustomShape(aParams.rSdrModel);
                 break;
             case SdrObjKind::ReportDesignSubReport:
-                pNewObj = new OOle2Obj(aParams.rSdrModel, SERVICE_REPORTDEFINITION, SdrObjKind::ReportDesignSubReport);
+                pNewObj = new OOle2Obj(aParams.rSdrModel, SdrObjKind::ReportDesignSubReport);
                 break;
             case SdrObjKind::OLE2:
-                pNewObj = new OOle2Obj(aParams.rSdrModel, "com.sun.star.chart2.ChartDocument", SdrObjKind::OLE2);
+                pNewObj = new OOle2Obj(aParams.rSdrModel, SdrObjKind::OLE2);
                 break;
             default:
                 OSL_FAIL("Unknown object id");