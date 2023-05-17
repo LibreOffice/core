LibreOffice/core

commit ac859a4c6a7fce4efee9cdd45821a0c9e40e9e9a

tdf#147842 shrink selection to data area when applying to entire sheet
This takes the time to apply the formating from "who knows how long" to about 500ms on my machine.

Change-Id: I202d023c58ea191bf080ef3a85068e8acab52dec
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/141463
Tested-by: Noel Grandin <noel.grandin@collabora.co.uk>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sc/source/ui/view/viewfunc.cxx
@@ -1171,6 +1171,34 @@ void ScViewFunc::ApplyPatternLines( const ScPatternAttr& rAttr, const SvxBoxItem
     StartFormatArea();
 }
 
+static void ShrinkToDataArea(ScMarkData& rFuncMark, ScDocument& rDoc)
+{
+    // tdf#147842 if the marked area is the entire sheet, then shrink it to the data area.
+    // Otherwise ctrl-A, perform-action, will take a very long time as it tries to modify
+    // cells then we are not using.
+    if (rFuncMark.IsMultiMarked())
+        return;
+    ScRange aMarkArea = rFuncMark.GetMarkArea();
+    const ScSheetLimits& rLimits = rDoc.GetSheetLimits();
+    if (aMarkArea.aStart.Row() != 0 || aMarkArea.aStart.Col() != 0)
+        return;
+    if (aMarkArea.aEnd.Row() != rLimits.MaxRow() || aMarkArea.aEnd.Col() != rLimits.MaxCol())
+        return;
+    if (aMarkArea.aStart.Tab() != aMarkArea.aEnd.Tab())
+        return;
+    SCCOL nStartCol = aMarkArea.aStart.Col();
+    SCROW nStartRow = aMarkArea.aStart.Row();
+    SCCOL nEndCol = aMarkArea.aEnd.Col();
+    SCROW nEndRow = aMarkArea.aEnd.Row();
+    rDoc.ShrinkToDataArea(aMarkArea.aStart.Tab(), nStartCol, nStartRow, nEndCol, nEndRow);
+    aMarkArea.aStart.SetCol(nStartCol);
+    aMarkArea.aStart.SetRow(nStartRow);
+    aMarkArea.aEnd.SetCol(nEndCol);
+    aMarkArea.aEnd.SetRow(nEndRow);
+    rFuncMark.ResetMark();
+    rFuncMark.SetMarkArea(aMarkArea);
+}
+
 //  pattern only
 
 void ScViewFunc::ApplySelectionPattern( const ScPatternAttr& rAttr, bool bCursorOnly )
@@ -1179,6 +1207,7 @@ void ScViewFunc::ApplySelectionPattern( const ScPatternAttr& rAttr, bool bCursor
     ScDocShell* pDocSh      = rViewData.GetDocShell();
     ScDocument& rDoc        = pDocSh->GetDocument();
     ScMarkData aFuncMark( rViewData.GetMarkData() );       // local copy for UnmarkFiltered
+    ShrinkToDataArea( aFuncMark, rDoc );
     ScViewUtil::UnmarkFiltered( aFuncMark, rDoc );
 
     bool bRecord = true;

commit c38c6a12cc7e5c640d7dbc9f7374da3f5a0cc869

tdf#154114 Formatting active cell applies to entire cell range
regression from
    commit ac859a4c6a7fce4efee9cdd45821a0c9e40e9e9a
    Author: Noel Grandin <noel.grandin@collabora.co.uk>
    Date:   Mon Oct 17 16:36:23 2022 +0200
tdf#147842 shrink selection to data area when applying to entire
sheet

Change-Id: I6fd8504d3995878fc9aedd461c0f374695d1f48e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/151873
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sc/source/ui/view/viewfunc.cxx
@@ -1217,7 +1217,8 @@ void ScViewFunc::ApplySelectionPattern( const ScPatternAttr& rAttr, bool bCursor
     ScDocShell* pDocSh      = rViewData.GetDocShell();
     ScDocument& rDoc        = pDocSh->GetDocument();
     ScMarkData aFuncMark( rViewData.GetMarkData() );       // local copy for UnmarkFiltered
-    ShrinkToDataArea( aFuncMark, rDoc );
+    if (aFuncMark.IsMarked()) // do not make it marked if it is not already marked
+        ShrinkToDataArea( aFuncMark, rDoc );
     ScViewUtil::UnmarkFiltered( aFuncMark, rDoc );
 
     bool bRecord = true;