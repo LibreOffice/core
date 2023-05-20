40285fdccd995ae304cf1d552cb239f29276f817
Patrick Luby <plubius@neooffice.org>
2023-05-20 08:50:12 -0400

    Set entitlements in non-release in $(INSTROOTBASE) and .dmg package
    Also, when building a .dmg for macOS, skip codesigning for non-release
    builds, both with and without the macOS sandbox, if there is no identity
    set but set entitlements to allow Xcode's Instruments application to
    connect to the application.
    
    Lastly, add entitlements when building soffice in $(INSTROOTBASE) if
    this is a non-release build.
    
    Change-Id: I764bf5bd5d44e878669c4287906e6efd6aac593f
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/152655
    Tested-by: Jenkins
    Reviewed-by: Patrick Luby <plubius@neooffice.org>
    (cherry picked from commit 55dfc98293a28af6bf81e63c2293977956c7250a)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/152662

--- desktop/CustomTarget_soffice.mk
+++ desktop/CustomTarget_soffice.mk
@@ -9,6 +9,23 @@
 
 $(eval $(call gb_CustomTarget_CustomTarget,desktop/soffice))
 
+ifeq ($(OS), MACOSX)
+
+ifeq (,$(ENABLE_RELEASE_BUILD))
+
+# Add entitlements if this is a non-release build. Just to be safe,
+# this target will always be run and MACOSX_CODESIGNING_IDENTITY is
+# set to empty as we don't want to sign $(INSTROOTBASE). Ignore
+# failures as it appears that setting only entitlements can fail
+# with certain macOS SDKs.
+$(call gb_CustomTarget_get_target,desktop/soffice) : \
+	$(INSTROOT)/$(LIBO_BIN_FOLDER)/soffice
+	-MACOSX_CODESIGNING_IDENTITY= $(SRCDIR)/solenv/bin/macosx-codesign-app-bundle $(INSTROOTBASE)
+
+endif
+
+else
+
 $(call gb_CustomTarget_get_target,desktop/soffice) : \
 	$(call gb_CustomTarget_get_workdir,desktop/soffice)/soffice.sh
 
@@ -24,5 +41,7 @@ else
 	cp $< $@
 endif
 	$(call gb_Trace_EndRange,$(subst $(WORKDIR)/,,$@),SED)
+
+endif
 
 # vim:set shiftwidth=4 tabstop=4 noexpandtab:
