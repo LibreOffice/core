58926cc60c7868785c8db126fc199f6731269b86
Regina Henschel <rb.henschel@t-online.de>
2023-05-20 19:26:26 +0200

    tdf#155412 ooxml export typeface attribute is mandatory
    The attribute 'typeface' is required for <a:ea>, <a:cs> and <a:latin>
    elements, see CT_TextFont in ISO/IEC 29500-1:2016. Its value may be
    the empty string.
    
    Change-Id: I7c9316fa40ad6d1aabccb4191fee11be553c453b
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/152024
    Tested-by: Jenkins
    Reviewed-by: Regina Henschel <rb.henschel@t-online.de>

--- oox/qa/unit/export.cxx
+++ oox/qa/unit/export.cxx
@@ -1316,6 +1316,22 @@ CPPUNIT_TEST_FIXTURE(Test, testThemeColorTransparency)
     assertXPath(pXmlDoc, sElement + "a:solidFill/a:schemeClr/a:lumMod", "val", "75000");
     assertXPath(pXmlDoc, sElement + "a:solidFill/a:schemeClr/a:alpha", "val", "20000");
 }
+
+CPPUNIT_TEST_FIXTURE(Test, testThemeFontTypeface)
+{
+    // Saving the document had produced a file which PowerPoint wants to repair. The mandatory
+    // attribute 'typeface' was missing in the <a:ea> elements.
+
+    loadFromURL(u"tdf155412_typeface.pptx");
+
+    save("Impress Office Open XML");
+    xmlDocUniquePtr pXmlDoc = parseExport("ppt/theme/theme1.xml");
+
+    // Make sure typeface is written.
+    OString sElement = "/a:theme/a:themeElements/a:fontScheme/";
+    assertXPath(pXmlDoc, sElement + "a:majorFont/a:ea", "typeface", "");
+    assertXPath(pXmlDoc, sElement + "a:minorFont/a:ea", "typeface", "");
+}
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();
