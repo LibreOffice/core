ce281dc683dad4820acdaeadeece21d9f3ebff49
Caolán McNamara <caolan.mcnamara@collabora.com>
2023-05-23 21:15:52 +0100

    upgrade to libxml2-2.11.4
    'checked' field removed with:
    https://gitlab.gnome.org/GNOME/libxml2/-/commit/ce76ebfd1312459951d555ad9d87fb9a89eede55
    
    win32 'run_debug' removed with:
    https://gitlab.gnome.org/GNOME/libxml2/-/commit/59f2f60e3eff07324f1c7cd861a444d714be750b
    
    add libxml2-XMLCALL-redefine.patch.0 to avoid:
    UnpackedTarball\libxml2\include\libxml/xmlexports.h(41): error C2220: the following warning is treated as an error
    UnpackedTarball\libxml2\include\libxml/xmlexports.h(41): warning C4005: 'XMLCALL': macro redefinition
    UnpackedTarball\expat\lib\expat_external.h(69): note: see previous definition of 'XMLCALL'
    
    Change-Id: Ia9b1540dc1b4eccf91662c8d995c036f71de6fc6
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/152176
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    (cherry picked from commit fd6cbd983a3021d22321854d0414bdc42c6cb1b7)

--- external/libxml2/UnpackedTarball_libxml2.mk
+++ external/libxml2/UnpackedTarball_libxml2.mk
@@ -16,6 +16,7 @@ $(eval $(call gb_UnpackedTarball_update_autoconf_configs,libxml2))
 $(eval $(call gb_UnpackedTarball_add_patches,libxml2,\
 	external/libxml2/libxml2-global-symbols.patch \
 	external/libxml2/libxml2-vc10.patch \
+	external/libxml2/libxml2-XMLCALL-redefine.patch.0 \
 	$(if $(filter ANDROID,$(OS)),external/libxml2/libxml2-android.patch) \
 	external/libxml2/libxml2-icu.patch.0 \
 ))
