d3f662245ba81a0d775aab670c0d920cf347aa48
Caolán McNamara <caolan.mcnamara@collabora.com>
2023-05-23 21:22:42 +0100

    revert apparent overwrite of UITest_sfx2_doc.mk with ExternalPackage_libffi.mk
    sfx2/UITest_sfx2_doc.mk:12: *** gb_ExternalPackage_use_external_project: only one project allowed.  Stop.
    
    in:
    
    commit d2715de5b0a3bd4b5eabe99d8cf1edf52ca5b65f
    Date:   Tue Jul 14 23:35:57 2020 +0200
    
        python3: update to 3.8.4
    
    Change-Id: I6869376eebbfbb7509bb38df7a80a06eb032ee73
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/152177
    Tested-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>

--- sfx2/UITest_sfx2_doc.mk
+++ sfx2/UITest_sfx2_doc.mk
@@ -7,14 +7,14 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 
-$(eval $(call gb_ExternalPackage_ExternalPackage,libffi,libffi))
+$(eval $(call gb_UITest_UITest,sfx2_doc))
 
-$(eval $(call gb_ExternalPackage_use_external_project,libffi,libffi))
-
-ifeq ($(COM),MSC)
-$(eval $(call gb_ExternalPackage_add_files,libffi,$(LIBO_LIB_FOLDER)/python-core-$(PYTHON_VERSION)/lib, \
-    $(HOST_PLATFORM)/.libs/libffi-7.dll \
+$(eval $(call gb_UITest_add_modules,sfx2_doc,$(SRCDIR)/sfx2/qa/uitest,\
+	doc/ \
 ))
-endif
+
+$(eval $(call gb_UITest_set_defs,sfx2_doc, \
+    TDOC="$(SRCDIR)/sfx2/qa/uitest/doc/data" \
+))
 
 # vim: set noet sw=4 ts=4:
