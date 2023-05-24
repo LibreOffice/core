LibreOffice/core

commit eb85de8e6b61fb3fcb6c03ae0145f7fe5478bccf

sw: layout: if fly's anchor moves forward, move fly off SwPageFrame
The problem is that on Show Changes->Hide Changes->Show Changes
in a 311 page document, the fly "Grafik1" was initially on page 203
but ends up on page 204, with a fly-sized gap on page 194.

In a 25 page cut down version of the bugdoc, on layout action 659
the fly's anchor SwTextFrame moves from page 21 to page 22, but
the fly remains in the SwPageFrame's m_pSortedObjs, because it's
not the anchor frame itself that moves but a distant previous frame,
and page 21 goes valid.

0  SwFlowFrame::PasteTree(SwFrame*, SwLayoutFrame*, SwFrame*, SwFrame*) (pStart=0x57c9e30, pParent=0xba15c50, pSibling=0x5a0f920, pOldParent=0xb057690) at sw/source/core/layout/flowfrm.cxx:586
1  SwFlowFrame::MoveSubTree(SwLayoutFrame*, SwFrame*) (this=0x57c9f48, pParent=0xba15c50, pSibling=0x5a0f920) at sw/source/core/layout/flowfrm.cxx:677
2  SwFlowFrame::MoveFwd(bool, bool, bool) (this=0x57c9f48, bMakePage=true, bPageBreak=false, bMoveAlways=false) at sw/source/core/layout/flowfrm.cxx:2061
3  SwContentFrame::MakeAll(OutputDevice*) (this=0x57c9e30) at sw/source/core/layout/calcmove.cxx:1831
4  SwFrame::OptPrepareMake() (this=0x57c9e30) at sw/source/core/layout/calcmove.cxx:399
5  SwFrame::OptCalc() const (this=0x57c9e30) at sw/source/core/inc/frame.hxx:1065
6  SwLayAction::FormatContent_(SwContentFrame const*, SwPageFrame const*) (this=0x7ffec0191b30, pContent=0x57c9e30, pPage=0xb9a1fd0) at sw/source/core/layout/layact.cxx:1833

In subsequent layout actions the anchor frame moves forward one
page at a time, until in action 665, when things get really interesting.

On page 24, the anchor text frame 582 is formatted for the first time,
and it moves the fly to page 24, after positioning it on the page.

2  SwPageFrame::MoveFly(SwFlyFrame*, SwPageFrame*) (this=0xb9a1fd0, pToMove=0x641d310, pDest=0x9125bb0) at sw/source/core/layout/flylay.cxx:972

sw/inc/anchoredobject.hxx
@@ -295,6 +295,8 @@ class SW_DLLPUBLIC SwAnchoredObject
         /** method to invalidate position of the anchored object */
         virtual void InvalidateObjPos() = 0;
 
+        virtual void RegisterAtPage(SwPageFrame &) = 0;
+
         /** method to perform necessary invalidations for the positioning of
             objects, for whose the wrapping style influence has to be considered
             on the object positioning.

sw/source/core/layout/layact.cxx
@@ -62,6 +62,8 @@
 #include <sortedobjs.hxx>
 #include <objectformatter.hxx>
 #include <fntcache.hxx>
+#include <fmtanchr.hxx>
+#include <comphelper/scopeguard.hxx>
 #include <vector>
 #include <tools/diagnose_ex.h>
 
@@ -1624,8 +1626,45 @@ bool SwLayAction::FormatLayoutTab( SwTabFrame *pTab, bool bAddRect )
     return bChanged;
 }
 
-bool SwLayAction::FormatContent( const SwPageFrame *pPage )
+bool SwLayAction::FormatContent(SwPageFrame *const pPage)
 {
+    ::comphelper::ScopeGuard g([this, pPage]() {
+        if (IsAgain())
+        {
+            return; // pPage probably deleted
+        }
+        if (auto const* pObjs = pPage->GetSortedObjs())
+        {
+            std::vector<std::pair<SwAnchoredObject*, SwPageFrame*>> moved;
+            for (auto const pObj : *pObjs)
+            {
+                SwPageFrame *const pAnchorPage(pObj->FindPageFrameOfAnchor());
+                assert(pAnchorPage);
+                if (pAnchorPage != pPage
+                    && pPage->GetPhyPageNum() < pAnchorPage->GetPhyPageNum()
+                    && pObj->GetFrameFormat().GetAnchor().GetAnchorId()
+                        != RndStdIds::FLY_AS_CHAR)
+                {
+                    moved.emplace_back(pObj, pAnchorPage);
+                }
+            }
+            for (auto const& [pObj, pAnchorPage] : moved)
+            {
+                pObj->RegisterAtPage(*pAnchorPage);
+                ::Notify_Background(pObj->GetDrawObj(), pPage,
+                    pObj->GetObjRect(), PrepareHint::FlyFrameLeave, false);
+            }
+            if (!moved.empty())
+            {
+                pPage->InvalidateFlyLayout();
+                if (auto *const pContent = pPage->FindLastBodyContent())
+                {
+                    pContent->InvalidateSize();
+                }
+            }
+        }
+    });
+
     const SwContentFrame *pContent = pPage->ContainsContent();
     const SwViewShell *pSh = m_pRoot->GetCurrShell();
     const bool bBrowse = pSh && pSh->GetViewOptions()->getBrowseMode();

sw/inc/anchoreddrawobject.hxx
@@ -94,6 +94,7 @@ class SwAnchoredDrawObject final : public SwAnchoredObject
             page frame
         */
         virtual void RegisterAtCorrectPage() override;
+        virtual void RegisterAtPage(SwPageFrame &) override;
 
         virtual bool SetObjTop_( const SwTwips _nTop) override;
         virtual bool SetObjLeft_( const SwTwips _nLeft) override;

sw/source/core/inc/flyfrm.hxx
@@ -136,6 +136,7 @@ protected:
         page frame
     */
     virtual void RegisterAtCorrectPage() override;
+    virtual void RegisterAtPage(SwPageFrame &) override;
 
     virtual bool SetObjTop_( const SwTwips _nTop ) override;
     virtual bool SetObjLeft_( const SwTwips _nLeft ) override;

sw/source/core/inc/flyfrms.hxx
@@ -167,6 +167,7 @@ class SwFlyAtContentFrame final: public SwFlyFreeFrame
         #i28701#
     */
     virtual void RegisterAtCorrectPage() override;
+    virtual void RegisterAtPage(SwPageFrame &) override;
     virtual void SwClientNotify(const SwModify&, const SfxHint&) override;
 
 public:

sw/source/core/inc/layact.hxx
@@ -111,7 +111,7 @@ class SwLayAction
 
     bool FormatLayout( OutputDevice* pRenderContext, SwLayoutFrame *, bool bAddRect = true );
     bool FormatLayoutTab( SwTabFrame *, bool bAddRect );
-    bool FormatContent( const SwPageFrame* pPage );
+    bool FormatContent(SwPageFrame * pPage);
     void FormatContent_( const SwContentFrame* pContent,
                        const SwPageFrame* pPage );
     bool IsShortCut( SwPageFrame *& );

sw/source/core/layout/anchoreddrawobject.cxx
@@ -919,10 +919,18 @@ void SwAnchoredDrawObject::RegisterAtCorrectPage()
     }
     if ( pPageFrame && GetPageFrame() != pPageFrame )
     {
-        if ( GetPageFrame() )
-            GetPageFrame()->RemoveDrawObjFromPage( *this );
-        pPageFrame->AppendDrawObjToPage( *this );
+        RegisterAtPage(*pPageFrame);
     }
+}
+
+void SwAnchoredDrawObject::RegisterAtPage(SwPageFrame & rPageFrame)
+{
+    assert(GetPageFrame() != &rPageFrame);
+    if (GetPageFrame())
+    {
+        GetPageFrame()->RemoveDrawObjFromPage( *this );
+    }
+    rPageFrame.AppendDrawObjToPage( *this );
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sw/source/core/layout/fly.cxx
@@ -2840,6 +2840,11 @@ void SwFlyFrame::RegisterAtCorrectPage()
     // default behaviour is to do nothing.
 }
 
+void SwFlyFrame::RegisterAtPage(SwPageFrame &)
+{
+    // default behaviour is to do nothing.
+}
+
 /** method to determine, if a <MakeAll()> on the Writer fly frame is possible
 
     OD 2004-05-11 #i28701#

commit 35f7f1ad9c2aba1644a46dfb4da0a86f72c6a224

tdf#148897 sw: layout: force invalidate background when moving fly off page
The fly 15 moves forward in SwLayAction::FormatContent(), then back
again later, without being positioned on the next page, and it happens
to go to the same position that it originally had.

Then SwFlyNotify sees that the position is the same, and doesn't do any
invalidation of the background, so the fly overlaps a text frame.

When the fly is moved by the object positioning code (before the below
commit), it first gets a new position on the next page, and then moves
back, so all invalidations happen.

Force the invalidation of the background in this case.

(regression from commit eb85de8e6b61fb3fcb6c03ae0145f7fe5478bccf
 "sw: layout: if fly's anchor moves forward, move fly off SwPageFrame")

Change-Id: Ifdc9460defe7b1f37cbb05310906146919e8fb6a
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/152245
Tested-by: Michael Stahl <michael.stahl@allotropia.de>
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/inc/anchoredobject.hxx
@@ -109,6 +109,8 @@ class SW_DLLPUBLIC SwAnchoredObject
         // page frame starts.
         bool mbTmpConsiderWrapInfluence;
 
+        bool mbForceNotifyNewBackground = false;
+
         mutable SwRect maObjRectWithSpaces;
         mutable bool mbObjRectWithSpacesValid;
         mutable SwRect maLastObjRect;
@@ -422,6 +424,9 @@ class SW_DLLPUBLIC SwAnchoredObject
         void SetTmpConsiderWrapInfluence( const bool _bTmpConsiderWrapInfluence );
         bool IsTmpConsiderWrapInfluence() const { return mbTmpConsiderWrapInfluence;}
         void ClearTmpConsiderWrapInfluence();
+
+        bool IsForceNotifyNewBackground() { return mbForceNotifyNewBackground; }
+        void SetForceNotifyNewBackground(bool const b) { mbForceNotifyNewBackground = b; }
 
         /** method to determine, if the anchored object is overlapping with a
             previous column

sw/source/core/layout/flylay.cxx
@@ -220,7 +220,7 @@ void SwFlyFreeFrame::MakeAll(vcl::RenderContext* /*pRenderContext*/)
             else
                 // #i26791# - use new method <MakeObjPos()>
                 MakeObjPos();
-            if( aOldPos == aRectFnSet.GetPos(getFrameArea()) )
+            if (!IsForceNotifyNewBackground() && aOldPos == aRectFnSet.GetPos(getFrameArea()))
             {
                 if( !isFrameAreaPositionValid() && GetAnchorFrame()->IsInSct() &&
                     !GetAnchorFrame()->FindSctFrame()->isFrameAreaDefinitionValid() )

sw/source/core/layout/frmtool.cxx
@@ -691,6 +691,11 @@ void SwFlyNotify::ImplDestroy()
         }
         pFly->ResetNotifyBack();
     }
+    if (pFly->IsForceNotifyNewBackground())
+    {
+        pFly->NotifyBackground(pFly->FindPageFrame(), pFly->GetObjRectWithSpaces(), PrepareHint::FlyFrameArrive);
+        pFly->SetForceNotifyNewBackground(false);
+    }
 
     //Have the size or the position changed,
     //so should the view know this.

sw/source/core/layout/layact.cxx
@@ -1682,6 +1682,12 @@ bool SwLayAction::FormatContent(SwPageFrame *const pPage)
                 pObj->InvalidateObjPos();
                 ::Notify_Background(pObj->GetDrawObj(), pPage,
                     pObj->GetObjRect(), PrepareHint::FlyFrameLeave, false);
+                // tdf#148897 in case the fly moves back to this page before
+                // being positioned again, the SwFlyNotify / ::Notify() could
+                // conclude that it didn't move at all and not call
+                // NotifyBackground(); note: pObj->SetObjTop(FAR_AWAY) results
+                // in wrong positions, use different approach!
+                pObj->SetForceNotifyNewBackground(true);
             }
             if (!moved.empty())
             {

sw/qa/extras/layout/layout2.cxx
@@ -12,6 +12,7 @@
 #include <com/sun/star/text/XTextFrame.hpp>
 #include <com/sun/star/text/XTextTable.hpp>
 #include <com/sun/star/linguistic2/XHyphenator.hpp>
+#include <com/sun/star/view/XSelectionSupplier.hpp>
 
 #include <comphelper/scopeguard.hxx>
 #include <comphelper/sequence.hxx>
@@ -142,6 +143,87 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter2, testTdf100680_as_char_wrap)
     auto pDump = parseLayoutDump();
     assertXPath(pDump, "/root/page/header/txt/SwParaPortion/SwLineLayout[3]");
     // If the third line missing that assert will fire, as was before the fix.
+}
+
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter2, testTdf148897)
+{
+    createSwDoc("tdf148897.odt");
+
+    xmlDocUniquePtr pXmlDoc = parseLayoutDump();
+
+    assertXPath(pXmlDoc, "/root/page[1]/sorted_objs/fly", 1);
+    assertXPath(pXmlDoc, "/root/page[1]/body/txt[2]/anchored/fly", 1);
+    assertXPath(pXmlDoc, "/root/page[2]/sorted_objs/fly", 1);
+    assertXPath(pXmlDoc, "/root/page[2]/body/txt[3]/anchored/fly", 1);
+    assertXPath(pXmlDoc, "/root/page[3]/sorted_objs/fly", 0);
+    assertXPath(pXmlDoc, "/root/page[3]/body/txt/anchored/fly", 0);
+    assertXPath(pXmlDoc, "/root/page[4]/sorted_objs/fly", 1);
+    assertXPath(pXmlDoc, "/root/page[4]/body/txt[1]/anchored/fly", 1);
+    // fly portion exists, no overlapping text
+    assertXPath(pXmlDoc, "/root/page[4]/body/txt[1]/SwParaPortion/SwLineLayout[1]/SwFixPortion",
+                "height", "5797");
+    assertXPath(pXmlDoc, "/root/page[5]/sorted_objs/fly", 0);
+    assertXPath(pXmlDoc, "/root/page", 5);
+
+    auto xModel = mxComponent.queryThrow<frame::XModel>();
+    uno::Reference<drawing::XShape> xShape(getShapeByName(u"Image3"));
+    uno::Reference<view::XSelectionSupplier> xCtrl(xModel->getCurrentController(), uno::UNO_QUERY);
+    xCtrl->select(uno::Any(xShape));
+
+    dispatchCommand(mxComponent, ".uno:Delete", {});
+
+    discardDumpedLayout();
+    pXmlDoc = parseLayoutDump();
+
+    assertXPath(pXmlDoc, "/root/page[1]/sorted_objs/fly", 0);
+    assertXPath(pXmlDoc, "/root/page[1]/body/txt/anchored/fly", 0);
+    assertXPath(pXmlDoc, "/root/page[2]/sorted_objs/fly", 1);
+    assertXPath(pXmlDoc, "/root/page[2]/body/txt[1]/anchored/fly", 1);
+    assertXPath(pXmlDoc, "/root/page[3]/sorted_objs/fly", 1);
+    assertXPath(pXmlDoc, "/root/page[3]/body/txt[2]/anchored/fly", 1);
+    // fly portion exists, no overlapping text
+    assertXPath(pXmlDoc, "/root/page[3]/body/txt[1]/SwParaPortion/SwLineLayout[1]/SwFixPortion",
+                "height", "5797");
+    assertXPath(pXmlDoc, "/root/page[4]/sorted_objs/fly", 0);
+    assertXPath(pXmlDoc, "/root/page[4]/body/txt/anchored/fly", 0);
+    assertXPath(pXmlDoc, "/root/page", 4);
+
+    dispatchCommand(mxComponent, ".uno:Undo", {});
+
+    discardDumpedLayout();
+    pXmlDoc = parseLayoutDump();
+
+    assertXPath(pXmlDoc, "/root/page[1]/sorted_objs/fly", 1);
+    assertXPath(pXmlDoc, "/root/page[1]/body/txt[2]/anchored/fly", 1);
+    assertXPath(pXmlDoc, "/root/page[2]/sorted_objs/fly", 1);
+    assertXPath(pXmlDoc, "/root/page[2]/body/txt[3]/anchored/fly", 1);
+    assertXPath(pXmlDoc, "/root/page[3]/sorted_objs/fly", 0);
+    assertXPath(pXmlDoc, "/root/page[3]/body/txt/anchored/fly", 0);
+    assertXPath(pXmlDoc, "/root/page[4]/sorted_objs/fly", 1);
+    assertXPath(pXmlDoc, "/root/page[4]/body/txt[1]/anchored/fly", 1);
+    // fly portion exists, no overlapping text
+    assertXPath(pXmlDoc, "/root/page[4]/body/txt[1]/SwParaPortion/SwLineLayout[1]/SwFixPortion",
+                "height", "5797");
+    assertXPath(pXmlDoc, "/root/page[5]/sorted_objs/fly", 0);
+    assertXPath(pXmlDoc, "/root/page", 5);
+
+    dispatchCommand(mxComponent, ".uno:Redo", {});
+
+    discardDumpedLayout();
+    pXmlDoc = parseLayoutDump();
+
+    assertXPath(pXmlDoc, "/root/page[1]/sorted_objs/fly", 0);
+    assertXPath(pXmlDoc, "/root/page[1]/body/txt/anchored/fly", 0);
+    assertXPath(pXmlDoc, "/root/page[2]/sorted_objs/fly", 1);
+    assertXPath(pXmlDoc, "/root/page[2]/body/txt[1]/anchored/fly", 1);
+    assertXPath(pXmlDoc, "/root/page[3]/sorted_objs/fly", 1);
+    assertXPath(pXmlDoc, "/root/page[3]/body/txt[2]/anchored/fly", 1);
+    // fly portion exists, no overlapping text
+    assertXPath(pXmlDoc, "/root/page[3]/body/txt[1]/SwParaPortion/SwLineLayout[1]/SwFixPortion",
+                "height", "5797");
+    assertXPath(pXmlDoc, "/root/page[4]/sorted_objs/fly", 0);
+    assertXPath(pXmlDoc, "/root/page[4]/body/txt/anchored/fly", 0);
+    assertXPath(pXmlDoc, "/root/page", 4);
 }
 
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter2, testRedlineCharAttributes)