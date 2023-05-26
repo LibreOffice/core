LibreOffice/core

commit e84b310b59825fd572d79def98c3d21566aac603

vcl,drawinglayer,svx,sw,sd: PDF/UA export: Annot StructElem for SdrMediaObj
veraPDF complains:

  Specification: ISO 14289-1:2014, Clause: 7.18.1, Test number: 1
  An annotation, excluding annotations of subtype Widget, Popup or
  Link, shall be nested within an Annot tag

This is very similar to Link annotations, that is to say, extremely
complicated to get it thought the convoluted PDF export code, with
additional complication that the StructElem is produced by drawinglayer
and the page annotation by sw.

Put another map into PDFExtOutDevData where sw code puts stuff for the
SdrObject that drawinglayer can find.

The test had the problem that PDFObjectParser::parse() could not handle:

<</Nums[
0 [ 6 0 R ]
1 6 0 R
]>>

Fix dropping the "1".

Change-Id: If5bf7c552e26ebb7e631030b8aaecd4281e77acc
(cherry picked from commit c78e90bd28cc4d6d3bde473535107784b12d9c0d)
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/149008
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

svx/source/sdr/contact/viewobjectcontact.cxx
@@ -35,6 +35,7 @@
 #include <svx/svdpage.hxx>
 #include <svx/svdotext.hxx>
 #include <vcl/pdfwriter.hxx>
+#include <vcl/pdfextoutdevdata.hxx>
 
 using namespace com::sun::star;
 
@@ -404,6 +405,8 @@ drawinglayer::primitive2d::Primitive2DContainer const & ViewObjectContact::getPr
                     eElement = vcl::PDFWriter::Section;
                 else if (nIdentifier == SdrObjKind::Table)
                     eElement = vcl::PDFWriter::Table;
+                else if (nIdentifier == SdrObjKind::Media)
+                    eElement = vcl::PDFWriter::Annot;
                 else if ( nIdentifier == SdrObjKind::TitleText )
                     eElement = vcl::PDFWriter::Heading;
                 else if ( nIdentifier == SdrObjKind::OutlineText )
@@ -429,13 +432,22 @@ drawinglayer::primitive2d::Primitive2DContainer const & ViewObjectContact::getPr
                         nAnchorId = pUserCall->GetPDFAnchorStructureElementId(*pSdrObj);
                     }
 
+                    ::std::vector<sal_Int32> annotIds;
+                    if (eElement == vcl::PDFWriter::Annot)
+                    {
+                        auto const pPDFExtOutDevData(GetObjectContact().GetPDFExtOutDevData());
+                        assert(pPDFExtOutDevData);
+                        annotIds = pPDFExtOutDevData->GetScreenAnnotIds(pSdrObj);
+                    }
+
                     drawinglayer::primitive2d::Primitive2DReference xReference(
                         new drawinglayer::primitive2d::StructureTagPrimitive2D(
                             eElement,
                             bBackground,
                             bImage,
                             std::move(xNewPrimitiveSequence),
-                            nAnchorId));
+                            nAnchorId,
+                            &annotIds));
                     xNewPrimitiveSequence = drawinglayer::primitive2d::Primitive2DContainer { xReference };
                 }
             }

drawinglayer/source/primitive2d/structuretagprimitive2d.cxx
@@ -31,13 +31,18 @@ namespace drawinglayer::primitive2d
             bool bBackground,
             bool bIsImage,
             Primitive2DContainer&& aChildren,
-            sal_Int32 const nAnchorStructureElementId)
+            sal_Int32 const nAnchorStructureElementId,
+            ::std::vector<sal_Int32> const*const pAnnotIds)
         :   GroupPrimitive2D(std::move(aChildren)),
             maStructureElement(rStructureElement),
             mbBackground(bBackground),
             mbIsImage(bIsImage)
         ,   m_nAnchorStructureElementId(nAnchorStructureElementId)
         {
+            if (pAnnotIds)
+            {
+                m_AnnotIds = *pAnnotIds;
+            }
         }
 
         bool StructureTagPrimitive2D::operator==(const BasePrimitive2D& rPrimitive) const

drawinglayer/source/processor2d/vclmetafileprocessor2d.cxx
@@ -2460,6 +2460,7 @@ void VclMetafileProcessor2D::processStructureTagPrimitive2D(
                 case vcl::PDFWriter::Table:
                 case vcl::PDFWriter::Formula:
                 case vcl::PDFWriter::Figure:
+                case vcl::PDFWriter::Annot:
                 {
                     auto const range(rStructureTagCandidate.getB2DRange(getViewInformation2D()));
                     tools::Rectangle const aLogicRect(
@@ -2470,6 +2471,10 @@ void VclMetafileProcessor2D::processStructureTagPrimitive2D(
                 }
                 default:
                     break;
+            }
+            if (rTagElement == vcl::PDFWriter::Annot)
+            {
+                mpPDFExtOutDevData->SetStructureAnnotIds(rStructureTagCandidate.GetAnnotIds());
             }
             if (rTagElement == vcl::PDFWriter::TableHeader)
             {

include/drawinglayer/primitive2d/structuretagprimitive2d.hxx
@@ -51,6 +51,8 @@ namespace drawinglayer::primitive2d
             bool                                    mbIsImage;
             /// anchor structure element (Writer)
             sal_Int32 m_nAnchorStructureElementId;
+            /// for Annot structure element, the ids of the annotations
+            ::std::vector<sal_Int32> m_AnnotIds;
 
         public:
             /// constructor
@@ -59,7 +61,8 @@ namespace drawinglayer::primitive2d
                 bool bBackground,
                 bool bIsImage,
                 Primitive2DContainer&& aChildren,
-                sal_Int32 nAnchorStructureElementId = -1);
+                sal_Int32 nAnchorStructureElementId = -1,
+                ::std::vector<sal_Int32> const* pAnnotIds = nullptr);
 
             /// data read access
             const vcl::PDFWriter::StructElement& getStructureElement() const { return maStructureElement; }
@@ -67,6 +70,7 @@ namespace drawinglayer::primitive2d
             bool isImage() const { return mbIsImage; }
             bool isTaggedSdrObject() const;
             sal_Int32 GetAnchorStructureElementId() const { return m_nAnchorStructureElementId; }
+            ::std::vector<sal_Int32> GetAnnotIds() const { return m_AnnotIds; }
 
             /// compare operator
             virtual bool operator==(const BasePrimitive2D& rPrimitive) const override;

include/svx/sdr/contact/objectcontact.hxx
@@ -30,6 +30,10 @@ namespace tools { class Rectangle; }
 class SdrPageView;
 class OutputDevice;
 
+namespace vcl {
+    class PDFExtOutDevData;
+}
+
 namespace basegfx {
     class B2DRange;
     class B2DHomMatrix;
@@ -138,6 +142,7 @@ public:
     // pdf export? Default is false
     virtual bool isOutputToPDFFile() const;
     virtual bool isExportTaggedPDF() const;
+    virtual ::vcl::PDFExtOutDevData const* GetPDFExtOutDevData() const;
 
     // gray display mode
     virtual bool isDrawModeGray() const;

include/svx/sdr/contact/objectcontactofpageview.hxx
@@ -93,6 +93,7 @@ namespace sdr::contact
             // pdf export? Default is false
             virtual bool isOutputToPDFFile() const override;
             virtual bool isExportTaggedPDF() const override;
+            virtual ::vcl::PDFExtOutDevData const* GetPDFExtOutDevData() const override;
 
             // gray display mode
             virtual bool isDrawModeGray() const override;

include/vcl/pdfextoutdevdata.hxx
@@ -25,10 +25,12 @@
 #include <vcl/pdfwriter.hxx>
 #include <vcl/extoutdevdata.hxx>
 #include <vector>
+#include <map>
 #include <memory>
 
 class Graphic;
 class GDIMetaFile;
+class SdrObject;
 
 namespace vcl
 {
@@ -91,6 +93,8 @@ class VCL_DLLPUBLIC PDFExtOutDevData final : public ExtOutDevData
 
     std::vector< PDFExtOutDevBookmarkEntry > maBookmarks;
     std::vector<OUString> maChapterNames;
+    // map from annotation SdrObject to annotation index
+    ::std::map<SdrObject const*, ::std::vector<sal_Int32>> m_ScreenAnnotations;
 
 public:
 
@@ -266,7 +270,10 @@ public:
     sal_Int32 CreateLink(const tools::Rectangle& rRect, OUString const& rAltText, sal_Int32 nPageNr = -1);
 
     /// Create a Screen annotation.
-    sal_Int32 CreateScreen(const tools::Rectangle& rRect, OUString const& rAltText, sal_Int32 nPageNr);
+    sal_Int32 CreateScreen(const tools::Rectangle& rRect, OUString const& rAltText, sal_Int32 nPageNr, SdrObject const* pObj);
+
+    /// Get back the annotations created for one SdrObject.
+    ::std::vector<sal_Int32> const& GetScreenAnnotIds(SdrObject const* pObj) const;
 
     /** Set the destination for a link
         <p>will change a URL type link to a dest link if necessary</p>
@@ -437,6 +444,11 @@ public:
     the new bounding box for the structural element
      */
     void SetStructureBoundingBox( const tools::Rectangle& rRect );
+
+    /** set the annotations that should be referenced as children of the
+        current structural element.
+     */
+    void SetStructureAnnotIds(::std::vector<sal_Int32> const& rAnnotIds);
 
     /** set the ActualText attribute of a structural element
 

include/vcl/pdfwriter.hxx
@@ -127,7 +127,7 @@ public:
         Table, TableRow, TableHeader, TableData,
 
         // inline level elements
-        Span, Quote, Note, Reference, BibEntry, Code, Link,
+        Span, Quote, Note, Reference, BibEntry, Code, Link, Annot,
 
         // illustration elements
         Figure, Formula, Form
@@ -1146,6 +1146,11 @@ The following structure describes the permissions used in PDF security
     the new bounding box for the structural element
      */
     void SetStructureBoundingBox( const tools::Rectangle& rRect );
+
+    /** set the annotations that should be referenced as children of the
+        current structural element.
+     */
+    void SetStructureAnnotIds(::std::vector<sal_Int32> const& rAnnotIds);
 
     /** set the ActualText attribute of a structural element
 

commit 8061cd765c7854667550d6aafda5832715648876

tdf#155436 svx: PDF export: fix crash on SdrMediaObj without URL
Both sw and sd don't call CreateScreen() if there is no URL.

(regression from commit e84b310b59825fd572d79def98c3d21566aac603)

Change-Id: I087511bb6ed41b0e39321f6ca8684f60261f01cf
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/152282
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

svx/source/sdr/contact/viewobjectcontact.cxx
@@ -31,6 +31,7 @@
 #include <drawinglayer/primitive2d/transformprimitive2d.hxx>
 #include <drawinglayer/primitive2d/structuretagprimitive2d.hxx>
 #include <svx/svdobj.hxx>
+#include <svx/svdomedia.hxx>
 #include <svx/svdmodel.hxx>
 #include <svx/svdpage.hxx>
 #include <svx/svdotext.hxx>
@@ -433,7 +434,8 @@ drawinglayer::primitive2d::Primitive2DContainer const & ViewObjectContact::getPr
                     }
 
                     ::std::vector<sal_Int32> annotIds;
-                    if (eElement == vcl::PDFWriter::Annot)
+                    if (eElement == vcl::PDFWriter::Annot
+                        && !static_cast<SdrMediaObj*>(pSdrObj)->getURL().isEmpty())
                     {
                         auto const pPDFExtOutDevData(GetObjectContact().GetPDFExtOutDevData());
                         assert(pPDFExtOutDevData);