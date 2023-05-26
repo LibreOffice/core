LibreOffice/core

commit 81ef84648515965bf67afaced946227d0f63a71e

(related: tdf#135192) svx: PDF/UA export: tag background as Artifact
 ISO 14289-1:2014, Clause: 7.1, Test number: 3
 Content shall be marked as Artifact or tagged as real content

This needs to have an effect on ViewObjectContactOfSdrPage etc.
but let's put it in ViewObjectContact and hope anything that's not
SdrObject can be tagged as Artifact.

Also VclMetafileProcessor2D::processStructureTagPrimitive2D() has a very
peculiar habit of ignoring any obvious way to create an Artifact,
which should perhaps be fixed?

Change-Id: I8d22c36a8e31692d3ee24af692026e02b8faa70a
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/143502
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
Tested-by: Michael Stahl <michael.stahl@allotropia.de>

svx/source/sdr/contact/viewobjectcontact.cxx
@@ -385,58 +385,73 @@ drawinglayer::primitive2d::Primitive2DContainer const & ViewObjectContact::getPr
 
     // Check if we need to embed to a StructureTagPrimitive2D, too. This
     // was done at ImplRenderPaintProc::createRedirectedPrimitive2DSequence before
-    if(!xNewPrimitiveSequence.empty() && nullptr != pSdrObj && GetObjectContact().isExportTaggedPDF())
+    if (!xNewPrimitiveSequence.empty() && GetObjectContact().isExportTaggedPDF())
     {
-        vcl::PDFWriter::StructElement eElement(vcl::PDFWriter::NonStructElement);
-        const SdrInventor nInventor(pSdrObj->GetObjInventor());
-        const SdrObjKind nIdentifier(pSdrObj->GetObjIdentifier());
-        const bool bIsTextObj(nullptr != DynCastSdrTextObj(pSdrObj));
-
-        // Note: SwFlyDrawObj/SwVirtFlyDrawObj have SdrInventor::Swg - these
-        // are *not* handled here because not all of them are painted
-        // completely with primitives, so a tag here does not encapsulate them.
-        // The tag must be created by SwTaggedPDFHelper until this is fixed.
-        if ( nInventor == SdrInventor::Default )
+        if (nullptr != pSdrObj)
         {
-            if ( nIdentifier == SdrObjKind::Group )
-                eElement = vcl::PDFWriter::Section;
-            else if (nIdentifier == SdrObjKind::Table)
-                eElement = vcl::PDFWriter::Table;
-            else if ( nIdentifier == SdrObjKind::TitleText )
-                eElement = vcl::PDFWriter::Heading;
-            else if ( nIdentifier == SdrObjKind::OutlineText )
-                eElement = vcl::PDFWriter::Division;
-            else if ( !bIsTextObj || !static_cast<const SdrTextObj&>(*pSdrObj).HasText() )
-                eElement = vcl::PDFWriter::Figure;
-            else
-                eElement = vcl::PDFWriter::Division;
-        }
+            vcl::PDFWriter::StructElement eElement(vcl::PDFWriter::NonStructElement);
+            const SdrInventor nInventor(pSdrObj->GetObjInventor());
+            const SdrObjKind nIdentifier(pSdrObj->GetObjIdentifier());
+            const bool bIsTextObj(nullptr != DynCastSdrTextObj(pSdrObj));
 
-        if(vcl::PDFWriter::NonStructElement != eElement)
-        {
-            SdrPage* pSdrPage(pSdrObj->getSdrPageFromSdrObject());
-
-            if(pSdrPage)
+            // Note: SwFlyDrawObj/SwVirtFlyDrawObj have SdrInventor::Swg - these
+            // are *not* handled here because not all of them are painted
+            // completely with primitives, so a tag here does not encapsulate them.
+            // The tag must be created by SwTaggedPDFHelper until this is fixed.
+            if ( nInventor == SdrInventor::Default )
             {
-                const bool bBackground(pSdrPage->IsMasterPage());
-                const bool bImage(SdrObjKind::Graphic == pSdrObj->GetObjIdentifier());
-                // note: there must be output device here, in PDF export
-                sal_Int32 nAnchorId(-1);
-                if (auto const pUserCall = pSdrObj->GetUserCall())
-                {
-                    nAnchorId = pUserCall->GetPDFAnchorStructureElementId(
-                        *pSdrObj, *GetObjectContact().TryToGetOutputDevice());
-                }
-
-                drawinglayer::primitive2d::Primitive2DReference xReference(
-                    new drawinglayer::primitive2d::StructureTagPrimitive2D(
-                        eElement,
-                        bBackground,
-                        bImage,
-                        std::move(xNewPrimitiveSequence),
-                        nAnchorId));
-                xNewPrimitiveSequence = drawinglayer::primitive2d::Primitive2DContainer { xReference };
+                if ( nIdentifier == SdrObjKind::Group )
+                    eElement = vcl::PDFWriter::Section;
+                else if (nIdentifier == SdrObjKind::Table)
+                    eElement = vcl::PDFWriter::Table;
+                else if ( nIdentifier == SdrObjKind::TitleText )
+                    eElement = vcl::PDFWriter::Heading;
+                else if ( nIdentifier == SdrObjKind::OutlineText )
+                    eElement = vcl::PDFWriter::Division;
+                else if ( !bIsTextObj || !static_cast<const SdrTextObj&>(*pSdrObj).HasText() )
+                    eElement = vcl::PDFWriter::Figure;
+                else
+                    eElement = vcl::PDFWriter::Division;
             }
+
+            if(vcl::PDFWriter::NonStructElement != eElement)
+            {
+                SdrPage* pSdrPage(pSdrObj->getSdrPageFromSdrObject());
+
+                if(pSdrPage)
+                {
+                    const bool bBackground(pSdrPage->IsMasterPage());
+                    const bool bImage(SdrObjKind::Graphic == pSdrObj->GetObjIdentifier());
+                    // note: there must be output device here, in PDF export
+                    sal_Int32 nAnchorId(-1);
+                    if (auto const pUserCall = pSdrObj->GetUserCall())
+                    {
+                        nAnchorId = pUserCall->GetPDFAnchorStructureElementId(
+                            *pSdrObj, *GetObjectContact().TryToGetOutputDevice());
+                    }
+
+                    drawinglayer::primitive2d::Primitive2DReference xReference(
+                        new drawinglayer::primitive2d::StructureTagPrimitive2D(
+                            eElement,
+                            bBackground,
+                            bImage,
+                            std::move(xNewPrimitiveSequence),
+                            nAnchorId));
+                    xNewPrimitiveSequence = drawinglayer::primitive2d::Primitive2DContainer { xReference };
+                }
+            }
+        }
+        else
+        {
+            // page backgrounds etc should be tagged as artifacts:
+            xNewPrimitiveSequence = drawinglayer::primitive2d::Primitive2DContainer {
+                    new drawinglayer::primitive2d::StructureTagPrimitive2D(
+                        // lies to force silly VclMetafileProcessor2D to emit NonStructElement
+                        vcl::PDFWriter::Division,
+                        true,
+                        true,
+                        std::move(xNewPrimitiveSequence))
+                };
         }
     }
 

commit 370533da3f07169791c0a17013ca55c57df2f3c9

tdf#155190 svx,sw: PDF export: don't tag SwNoTextFrame as Artifact
The problem is that inside of the Figure tag, in
SwNoTextFrame::ImplPaintPictureGraphic(), ViewContactOfSwNoTextFrame
and ViewObjectContactOfSwNoTextFrame are used to create and process
another primitive sequence.

ViewObjectContactOfSwNoTextFrame does not have access to a SdrObject,
because that was already processed by the outer layer of code that
called the SwFlyFrame painting code.

Avoid running the code that assumes anything without an SdrObject is an
artifact by disabling PDF tags altogether in
ViewObjectContactOfSwNoTextFrame.

(regression from commit 81ef84648515965bf67afaced946227d0f63a71e)

Change-Id: I9fabe7f7e5296f8d850448ac44865f87cd164591
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/152335
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

include/svx/sdr/contact/viewobjectcontact.hxx
@@ -116,6 +116,9 @@ public:
     // This method will not handle included hierarchies and not check visibility.
     drawinglayer::primitive2d::Primitive2DContainer const & getPrimitive2DSequence(const DisplayInfo& rDisplayInfo) const;
 
+    /// check if getPrimitive2DSequence() should create tag
+    virtual bool isExportPDFTags() const;
+
     // test this VOC for visibility concerning model-view stuff like e.g. Layer
     virtual bool isPrimitiveVisible(const DisplayInfo& rDisplayInfo) const;
 

svx/source/sdr/contact/viewobjectcontact.cxx
@@ -341,6 +341,11 @@ void ViewObjectContact::createPrimitive2DSequence(const DisplayInfo& rDisplayInf
     rVisitor.visit(xRetval);
 }
 
+bool ViewObjectContact::isExportPDFTags() const
+{
+    return GetObjectContact().isExportTaggedPDF();
+}
+
 drawinglayer::primitive2d::Primitive2DContainer const & ViewObjectContact::getPrimitive2DSequence(const DisplayInfo& rDisplayInfo) const
 {
     // only some of the top-level apps are any good at reliably invalidating us (e.g. writer is not)
@@ -387,7 +392,7 @@ drawinglayer::primitive2d::Primitive2DContainer const & ViewObjectContact::getPr
 
     // Check if we need to embed to a StructureTagPrimitive2D, too. This
     // was done at ImplRenderPaintProc::createRedirectedPrimitive2DSequence before
-    if (!xNewPrimitiveSequence.empty() && GetObjectContact().isExportTaggedPDF())
+    if (!xNewPrimitiveSequence.empty() && isExportPDFTags())
     {
         if (nullptr != pSdrObj)
         {

sw/source/core/doc/notxtfrm.cxx
@@ -993,6 +993,9 @@ protected:
         const sdr::contact::DisplayInfo& rDisplayInfo,
         drawinglayer::primitive2d::Primitive2DDecompositionVisitor& rVisitor) const override;
 
+    // tdf#155190 disable this so superclass doesn't wrongly produce NonStruct
+    virtual bool isExportPDFTags() const override { return false; }
+
 public:
     ViewObjectContactOfSwNoTextFrame(
         sdr::contact::ObjectContact& rObjectContact,

vcl/qa/cppunit/pdfexport/pdfexport.cxx
@@ -3598,6 +3598,94 @@ CPPUNIT_TEST_FIXTURE(PdfExportTest, testTdf135192)
     CPPUNIT_ASSERT_EQUAL(int(1), nTable);
 }
 
+CPPUNIT_TEST_FIXTURE(PdfExportTest, testTdf155190)
+{
+    aMediaDescriptor["FilterName"] <<= OUString("writer_pdf_Export");
+
+    // Enable PDF/UA
+    uno::Sequence<beans::PropertyValue> aFilterData(
+        comphelper::InitPropertySequence({ { "PDFUACompliance", uno::Any(true) } }));
+    aMediaDescriptor["FilterData"] <<= aFilterData;
+
+    saveAsPDF(u"tdf155190.odt");
+
+    vcl::filter::PDFDocument aDocument;
+    SvFileStream aStream(maTempFile.GetURL(), StreamMode::READ);
+    CPPUNIT_ASSERT(aDocument.Read(aStream));
+
+    // The document has one page.
+    std::vector<vcl::filter::PDFObjectElement*> aPages = aDocument.GetPages();
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(1), aPages.size());
+
+    auto nDiv(0);
+    auto nFigure(0);
+    for (const auto& rDocElement : aDocument.GetElements())
+    {
+        auto pObject1 = dynamic_cast<vcl::filter::PDFObjectElement*>(rDocElement.get());
+        if (!pObject1)
+            continue;
+        auto pType1 = dynamic_cast<vcl::filter::PDFNameElement*>(pObject1->Lookup("Type"));
+
+        auto pS1 = dynamic_cast<vcl::filter::PDFNameElement*>(pObject1->Lookup("S"));
+        // start with the text box
+        if (pType1 && pType1->GetValue() == "StructElem" && pS1 && pS1->GetValue() == "Div")
+        {
+            ++nDiv;
+            auto pKids1 = dynamic_cast<vcl::filter::PDFArrayElement*>(pObject1->Lookup("K"));
+            CPPUNIT_ASSERT(pKids1);
+            for (auto pKid1 : pKids1->GetElements())
+            {
+                auto pRefKid1 = dynamic_cast<vcl::filter::PDFReferenceElement*>(pKid1);
+                if (pRefKid1)
+                {
+                    auto pObject2 = pRefKid1->LookupObject();
+                    CPPUNIT_ASSERT(pObject2);
+                    auto pType2
+                        = dynamic_cast<vcl::filter::PDFNameElement*>(pObject2->Lookup("Type"));
+                    CPPUNIT_ASSERT(pType2);
+                    CPPUNIT_ASSERT_EQUAL(OString("StructElem"), pType2->GetValue());
+                    auto pS2 = dynamic_cast<vcl::filter::PDFNameElement*>(pObject2->Lookup("S"));
+                    CPPUNIT_ASSERT_EQUAL(OString("FigureCaption"), pS2->GetValue());
+                    auto pKids2
+                        = dynamic_cast<vcl::filter::PDFArrayElement*>(pObject2->Lookup("K"));
+                    CPPUNIT_ASSERT(pKids2);
+                    // there are additional children, MCID ref
+                    for (auto pKid2 : pKids2->GetElements())
+                    {
+                        auto pRefKid2 = dynamic_cast<vcl::filter::PDFReferenceElement*>(pKid2);
+                        if (pRefKid2)
+                        {
+                            auto pObject3 = pRefKid2->LookupObject();
+                            CPPUNIT_ASSERT(pObject3);
+                            auto pType3 = dynamic_cast<vcl::filter::PDFNameElement*>(
+                                pObject3->Lookup("Type"));
+                            if (pType3 && pType3->GetValue() == "StructElem")
+                            {
+                                auto pS3 = dynamic_cast<vcl::filter::PDFNameElement*>(
+                                    pObject3->Lookup("S"));
+                                CPPUNIT_ASSERT_EQUAL(OString("Figure"), pS3->GetValue());
+                                auto pAlt = dynamic_cast<vcl::filter::PDFHexStringElement*>(
+                                    pObject3->Lookup("Alt"));
+                                CPPUNIT_ASSERT_EQUAL(
+                                    OUString("Picture of apples"),
+                                    ::vcl::filter::PDFDocument::DecodeHexStringUTF16BE(*pAlt));
+                                auto pKids3 = dynamic_cast<vcl::filter::PDFArrayElement*>(
+                                    pObject3->Lookup("K"));
+                                CPPUNIT_ASSERT(pKids3);
+                                // the problem was that this didn't reference an MCID
+                                CPPUNIT_ASSERT(!pKids3->GetElements().empty());
+                                ++nFigure;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    CPPUNIT_ASSERT_EQUAL(static_cast<decltype(nDiv)>(1), nDiv);
+    CPPUNIT_ASSERT_EQUAL(static_cast<decltype(nDiv)>(1), nFigure);
+}
+
 CPPUNIT_TEST_FIXTURE(PdfExportTest, testMediaShapeAnnot)
 {
     aMediaDescriptor["FilterName"] <<= OUString("writer_pdf_Export");