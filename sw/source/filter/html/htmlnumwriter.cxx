8e51d79e1a1fd13dcb8809c520e28a51c7bcb553
Mike Kaganski <mike.kaganski@collabora.com>
2023-05-29 14:47:20 +0300

    ReqIF: 'ol' elements must have no 'type' attribute
    Change-Id: I62098bac9dc3751f7b939cbf94f1561b4539a294
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/152368
    Tested-by: Jenkins
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

--- sw/source/filter/html/htmlnumwriter.cxx
+++ sw/source/filter/html/htmlnumwriter.cxx
@@ -224,8 +224,10 @@ Writer& OutHTML_NumberBulletListStart( SwHTMLWriter& rWrt,
 
             // determine the type by the format
             char cType = 0;
-            switch( eType )
+            if (!rWrt.mbReqIF) // No 'type' attribute in ReqIF
             {
+                switch (eType)
+                {
                 case SVX_NUM_CHARS_UPPER_LETTER:
                 case SVX_NUM_CHARS_UPPER_LETTER_N:
                     cType = 'A';
@@ -240,6 +242,7 @@ Writer& OutHTML_NumberBulletListStart( SwHTMLWriter& rWrt,
                 case SVX_NUM_ROMAN_LOWER:
                     cType = 'i';
                     break;
+                }
             }
             if( cType )
             {
