7e985c49d2d948691b29f819e9d9bebdd669922e
Michael Stahl <michael.stahl@allotropia.de>
2023-06-01 20:19:36 +0200

    tdf#152983 sw: layout: fix crash in SwContentFrame::Cut()
    Surprisingly, SwContentFrame::Cut() is called in a situation where the
    frame is not connected to any SwPageFrame yet - it is in a follow fly of
    a fly chain that is anchored in a footer, and SwFlyFrame::ChainFrames()
    removes it.
    
    (regression from commit b9ef71476fd70bc13f50ebe80390e0730d1b7afb)
    
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/152510
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
    (cherry picked from commit d3b64de9581bd5e36ced007ff2e9139bd0343a35)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/152533
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    (cherry picked from commit ce863cf085e093e7385818a7d6d598a161e33a28)
    
    Change-Id: Iad84b7b422126e050493dc3b181d47c4dca2fae2

--- sw/qa/extras/layout/layout.cxx
+++ sw/qa/extras/layout/layout.cxx
@@ -829,6 +829,12 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter, TestTdf150606)
 }
 #endif
 
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter, TestTdf152983)
+{
+    //just care it doesn't crash/assert
+    createDoc("tdf152983-1-min.docx");
+}
+
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testRedlineFlysInFootnote)
 {
     loadURL("private:factory/swriter", nullptr);
