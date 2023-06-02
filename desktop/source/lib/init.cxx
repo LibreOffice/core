LibreOffice/core

commit 1cd46ba9140b7f5f077d8e9da405b180857f008d

Remove the allowed language check from UI languages.
Allowed langauges list is for spell checking algorithms. No need to use it for UI shortcuts.

Signed-off-by: Gökay Şatır <gokaysatir@gmail.com>
Change-Id: I996861b79247269e9b788b008fe6fc26e5ef44c8
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/152416
Tested-by: Andras Timar <andras.timar@collabora.com>
Reviewed-by: Andras Timar <andras.timar@collabora.com>

desktop/source/lib/init.cxx
@@ -7219,27 +7219,18 @@ void preLoadShortCutAccelerators()
     {
         OUString language = LanguageTag(installedLocales[i]).getLocale().Language;
 
-        if (!comphelper::LibreOfficeKit::isAllowlistedLanguage(language))
-        {
-            // Language is listed by COOL and also installed in core. We can create the short cut accelerator.
+        // Set the UI language to current one, before creating the accelerator.
+        std::shared_ptr<comphelper::ConfigurationChanges> batch(comphelper::ConfigurationChanges::create());
+        officecfg::Setup::L10N::ooLocale::set(installedLocales[i], batch);
+        batch->commit();
 
-            // Set the UI language to current one, before creating the accelerator.
-            std::shared_ptr<comphelper::ConfigurationChanges> batch(comphelper::ConfigurationChanges::create());
-            officecfg::Setup::L10N::ooLocale::set(installedLocales[i], batch);
-            batch->commit();
-
-            // Supported module names: Writer, Calc, Draw, Impress
-            std::vector<OUString> supportedModuleNames = { "com.sun.star.text.TextDocument", "com.sun.star.sheet.SpreadsheetDocument", "com.sun.star.drawing.DrawingDocument", "com.sun.star.presentation.PresentationDocument" };
-            // Create the accelerators.
-            for (std::size_t j = 0; j < supportedModuleNames.size(); j++)
-            {
-                OUString key = supportedModuleNames[j] + installedLocales[i];
-                acceleratorConfs[key] = svt::AcceleratorExecute::lok_createNewAcceleratorConfiguration(::comphelper::getProcessComponentContext(), supportedModuleNames[j]);
-            }
-        }
-        else
+        // Supported module names: Writer, Calc, Draw, Impress
+        std::vector<OUString> supportedModuleNames = { "com.sun.star.text.TextDocument", "com.sun.star.sheet.SpreadsheetDocument", "com.sun.star.drawing.DrawingDocument", "com.sun.star.presentation.PresentationDocument" };
+        // Create the accelerators.
+        for (std::size_t j = 0; j < supportedModuleNames.size(); j++)
         {
-            std::cerr << "Language is installed in core but not in the list of COOL languages: " << language << "\n";
+            OUString key = supportedModuleNames[j] + installedLocales[i];
+            acceleratorConfs[key] = svt::AcceleratorExecute::lok_createNewAcceleratorConfiguration(::comphelper::getProcessComponentContext(), supportedModuleNames[j]);
         }
     }
 

commit b0fa2ea7decadae61ad10e340698a8dd4af45e6e

Removing unused variable in preLoadShortCutAccelerators
When all warnings are treated as errors the following error was
reported:

desktop/source/lib/init.cxx:
OUString language =
LanguageTag(installedLocales[i]).getLocale().Language;

The problem started after commit
1cd46ba9140b7f5f077d8e9da405b180857f008d.
Remove the allowed language check from UI languages.

Change-Id: Id1f4269fe262b61b4fb05d61bf9c64a747275c28
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/152561
Reviewed-by: Gökay ŞATIR <gokaysatir@collabora.com>
Tested-by: Aron Budea <aron.budea@collabora.com>

desktop/source/lib/init.cxx
@@ -7217,8 +7217,6 @@ void preLoadShortCutAccelerators()
 
     for (sal_Int32 i = 0; i < installedLocales.getLength(); i++)
     {
-        OUString language = LanguageTag(installedLocales[i]).getLocale().Language;
-
         // Set the UI language to current one, before creating the accelerator.
         std::shared_ptr<comphelper::ConfigurationChanges> batch(comphelper::ConfigurationChanges::create());
         officecfg::Setup::L10N::ooLocale::set(installedLocales[i], batch);