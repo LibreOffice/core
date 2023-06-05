bdad6055eb32fa6e7ee9e43cf1a54edbd2318fcd
Xisco Fauli <xiscofauli@libreoffice.org>
2023-06-05 14:17:07 +0200

    tdf#153972: sc: Add UItest
    Change-Id: I7479be100d2c0abd9484a4095b3b2acf958beffd
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/152616
    Tested-by: Jenkins
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

---
+++ sc/qa/uitest/autofilter2/tdf153972.py
@@ -0,0 +1,60 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+from uitest.framework import UITestCase
+from uitest.uihelper.common import get_url_for_data_file, get_state_as_dict
+from libreoffice.uno.propertyvalue import mkPropertyValues
+from libreoffice.calc.document import get_cell_by_position, is_row_hidden
+
+class tdf153972(UITestCase):
+    def test_tdf153972(self):
+
+        with self.ui_test.load_file(get_url_for_data_file("tdf153972.ods")) as doc:
+            calcDoc = self.xUITest.getTopFocusWindow()
+            xGridWin = calcDoc.getChild("grid_window")
+
+            xGridWin.executeAction("LAUNCH", mkPropertyValues({"AUTOFILTER": "", "COL": "0", "ROW": "0"}))
+            xFloatWindow = self.xUITest.getFloatWindow()
+            xMenu = xFloatWindow.getChild("menu")
+
+            # Filter by Color
+            xMenu.executeAction("TYPE", mkPropertyValues({"KEYCODE":"DOWN"}))
+            xMenu.executeAction("TYPE", mkPropertyValues({"KEYCODE":"DOWN"}))
+            xMenu.executeAction("TYPE", mkPropertyValues({"KEYCODE":"DOWN"}))
+            xMenu.executeAction("TYPE", mkPropertyValues({"KEYCODE":"RETURN"}))
+
+            xSubFloatWindow = self.xUITest.getFloatWindow()
+            xSubMenu = xSubFloatWindow.getChild("background")
+
+            # Without the fix in place, this test would have failed with
+            # AssertionError: 4 != 0
+            self.assertEqual(4, len(xSubMenu.getChildren()))
+            self.assertEqual('Lime', get_state_as_dict(xSubMenu.getChild('0'))['Text'])
+            self.assertEqual('Red', get_state_as_dict(xSubMenu.getChild('1'))['Text'])
+            self.assertEqual('Yellow', get_state_as_dict(xSubMenu.getChild('2'))['Text'])
+            self.assertEqual('No Fill', get_state_as_dict(xSubMenu.getChild('3'))['Text'])
+
+            # Choose Red
+            xSubMenu.executeAction("TYPE", mkPropertyValues({"KEYCODE":"DOWN"}))
+            xSubMenu.executeAction("TYPE", mkPropertyValues({"KEYCODE":"RETURN"}))
+
+            self.assertTrue(is_row_hidden(doc, 1))
+            self.assertTrue(is_row_hidden(doc, 2))
+            self.assertTrue(is_row_hidden(doc, 3))
+            self.assertTrue(is_row_hidden(doc, 4))
+            self.assertTrue(is_row_hidden(doc, 5))
+            self.assertFalse(is_row_hidden(doc, 6))
+            self.assertTrue(is_row_hidden(doc, 7))
+            self.assertTrue(is_row_hidden(doc, 8))
+            self.assertFalse(is_row_hidden(doc, 9))
+            self.assertTrue(is_row_hidden(doc, 10))
+            self.assertFalse(is_row_hidden(doc, 11))
+            self.assertTrue(is_row_hidden(doc, 12))
+            self.assertFalse(is_row_hidden(doc, 13))
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
