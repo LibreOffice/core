59c2e114f76247158a0da9dcf91d6449d5d61224
Marco Cecchetti <marco.cecchetti@collabora.com>
2023-06-07 14:03:56 +0200

    lok: a11y: when we are inside a table notify table and current cell info
    When we get in one or more tables we notify row and column count.
    When we get out one or more table we notify we are leaving a table.
    When the fosused cell changes we notify the new row/col index.
    The paragraph content is notified together with table info, so that
    client has some opportunity for getting the screen reader to report
    together content and table info.
    
    Change-Id: Ic524259aa1879a70fc3de2467bdee27475352b7d
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/155577
    Tested-by: Jenkins
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -1798,6 +1798,7 @@ void CallbackFlushHandler::queue(const int type, CallbackData& aCallbackData)
         case LOK_CALLBACK_A11Y_FOCUS_CHANGED:
         case LOK_CALLBACK_A11Y_CARET_CHANGED:
         case LOK_CALLBACK_A11Y_TEXT_SELECTION_CHANGED:
+        case LOK_CALLBACK_A11Y_FOCUSED_CELL_CHANGED:
         case LOK_CALLBACK_COLOR_PALETTES:
         {
             const auto& pos = std::find(m_queue1.rbegin(), m_queue1.rend(), type);
@@ -1860,6 +1861,7 @@ void CallbackFlushHandler::queue(const int type, CallbackData& aCallbackData)
             case LOK_CALLBACK_A11Y_FOCUS_CHANGED:
             case LOK_CALLBACK_A11Y_CARET_CHANGED:
             case LOK_CALLBACK_A11Y_TEXT_SELECTION_CHANGED:
+            case LOK_CALLBACK_A11Y_FOCUSED_CELL_CHANGED:
             case LOK_CALLBACK_COLOR_PALETTES:
             {
                 if (removeAll(type))
