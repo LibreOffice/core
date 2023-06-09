2b6c09164714e037e7c5bebb1586a7c0672c383a
Justin Luth <jluth@mail.com>
2023-06-08 20:24:28 -0400

    tdf#152046 Revert "tdf#100680 sw DOCX compatibility: fix wrap of as_char flys"
    This reverts 7.4 commit 41b012767feb8552b60a68c7be18d80c403304bf,
    
    The premiss of the commit is that as-char flies needed to be handled differently,
    and yet at the spot where it was implemented, we have no idea whether we
    are even dealing with an as-char fly!!!
    
    As this bug report report shows, it affects paragraphy that don't
    have any fly whatsoever, let alone an as-char fly.
    
    Change-Id: I2fedb2d610093933081e861a16a25de2f2716258
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/152765
    Tested-by: Jenkins
    Reviewed-by: Justin Luth <jluth@mail.com>

--- sw/source/uibase/uno/SwXDocumentSettings.cxx
+++ sw/source/uibase/uno/SwXDocumentSettings.cxx
@@ -154,7 +154,6 @@ enum SwDocumentSettingsPropertyHandles
     HANDLE_AUTO_FIRST_LINE_INDENT_DISREGARD_LINE_SPACE,
     HANDLE_HYPHENATE_URLS,
     HANDLE_DO_NOT_BREAK_WRAPPED_TABLES,
-    HANDLE_WORD_LIKE_WRAP_FOR_AS_CHAR_FLYS,
     HANDLE_NO_NUMBERING_SHOW_FOLLOWBY,
     HANDLE_DROP_CAP_PUNCTUATION,
     HANDLE_USE_VARIABLE_WIDTH_NBSP,
@@ -258,7 +257,6 @@ static rtl::Reference<MasterPropertySetInfo> lcl_createSettingsInfo()
         { OUString("AutoFirstLineIndentDisregardLineSpace"), HANDLE_AUTO_FIRST_LINE_INDENT_DISREGARD_LINE_SPACE, cppu::UnoType<bool>::get(), 0 },
         { OUString("HyphenateURLs"), HANDLE_HYPHENATE_URLS, cppu::UnoType<bool>::get(), 0 },
         { OUString("DoNotBreakWrappedTables"), HANDLE_DO_NOT_BREAK_WRAPPED_TABLES, cppu::UnoType<bool>::get(), 0 },
-        { OUString("WordLikeWrapForAsCharFlys"), HANDLE_WORD_LIKE_WRAP_FOR_AS_CHAR_FLYS, cppu::UnoType<bool>::get(), 0 },
         { OUString("NoNumberingShowFollowBy"), HANDLE_NO_NUMBERING_SHOW_FOLLOWBY, cppu::UnoType<bool>::get(), 0 },
         { OUString("DropCapPunctuation"), HANDLE_DROP_CAP_PUNCTUATION, cppu::UnoType<bool>::get(), 0 },
         { OUString("UseVariableWidthNBSP"), HANDLE_USE_VARIABLE_WIDTH_NBSP, cppu::UnoType<bool>::get(), 0 },
@@ -1082,14 +1080,6 @@ void SwXDocumentSettings::_setSingleValue( const comphelper::PropertyInfo & rInf
             }
         }
         break;
-        case HANDLE_WORD_LIKE_WRAP_FOR_AS_CHAR_FLYS:
-        {
-            bool bTmp;
-            if (rValue >>= bTmp)
-                mpDoc->getIDocumentSettingAccess().set(
-                    DocumentSettingId::WRAP_AS_CHAR_FLYS_LIKE_IN_OOXML, bTmp);
-        }
-        break;
         case HANDLE_NO_NUMBERING_SHOW_FOLLOWBY:
         {
             bool bTmp;
@@ -1646,12 +1636,6 @@ void SwXDocumentSettings::_getSingleValue( const comphelper::PropertyInfo & rInf
         {
             rValue <<= mpDoc->getIDocumentSettingAccess().get(
                 DocumentSettingId::DO_NOT_BREAK_WRAPPED_TABLES);
-        }
-        break;
-        case HANDLE_WORD_LIKE_WRAP_FOR_AS_CHAR_FLYS:
-        {
-            rValue <<= mpDoc->getIDocumentSettingAccess().get(
-                DocumentSettingId::WRAP_AS_CHAR_FLYS_LIKE_IN_OOXML);
         }
         break;
         case HANDLE_NO_NUMBERING_SHOW_FOLLOWBY:
