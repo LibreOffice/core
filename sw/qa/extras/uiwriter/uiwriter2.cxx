a7a1f426374f865ef31f5c5162eac4c1c11efc6c
Michael Stahl <michael.stahl@allotropia.de>
2023-06-09 13:59:58 +0200

    cool#6580 sw: fix infinite loop when changing document language
    If there's a footnote in the document, changing the document langauge
    goes into an infinite loop in FindParentText(), because the selection
    created by ExtendedSelectAll(true) is actually invalid, apparently
    the intention is that only very limited functions may be called while it
    is active.
    
    Don't handle this invalid "very" extended selection like one created by
    ExtendedSelectAll(false).
    
    (regression from commit d81379db730a163c5ff75d4f3a3cddbd7b5eddda)
    
    Change-Id: Icf1032715cf2e0a05bf485039c483440c08bb6bb
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/152797
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
    (cherry picked from commit ca9341cf60f3f9350662d30b61f6eadefca24667)

--- sw/qa/extras/uiwriter/uiwriter2.cxx
+++ sw/qa/extras/uiwriter/uiwriter2.cxx
@@ -23,6 +23,10 @@
 #include <i18nlangtag/languagetag.hxx>
 #include <vcl/scheduler.hxx>
 #include <vcl/settings.hxx>
+#include <sfx2/dispatch.hxx>
+#include <sfx2/viewfrm.hxx>
+#include <svx/svxids.hrc>
+#include <view.hxx>
 #include <ndtxt.hxx>
 #include <swdtflvr.hxx>
 #include <wrtsh.hxx>
@@ -254,6 +258,20 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest2, testTdf101534)
     pWrtShell->GetCurAttr(aSet);
     // This failed, direct formatting was lost.
     CPPUNIT_ASSERT(aSet.HasItem(RES_LR_SPACE));
+}
+
+CPPUNIT_TEST_FIXTURE(SwUiWriterTest2, testExtendedSelectAllHang)
+{
+    SwDoc* const pDoc = createDoc();
+    SwWrtShell* const pWrtShell = pDoc->GetDocShell()->GetWrtShell();
+
+    pWrtShell->InsertFootnote("");
+    pWrtShell->StartOfSection();
+    SwView* pView = pDoc->GetDocShell()->GetView();
+    SfxStringItem aLangString(SID_LANGUAGE_STATUS, "Default_Spanish (Bolivia)");
+    // this looped
+    pView->GetViewFrame()->GetDispatcher()->ExecuteList(SID_LANGUAGE_STATUS, SfxCallMode::SYNCHRON,
+                                                        { &aLangString });
 }
 
 CPPUNIT_TEST_FIXTURE(SwUiWriterTest2, testRedlineMoveInsertInDelete)
