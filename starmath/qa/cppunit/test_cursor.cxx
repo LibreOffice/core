cf2d0807642b443bb320abed7464dae10f7c5ee0
Thorsten Behrens <thorsten.behrens@allotropia.de>
2023-06-12 16:43:49 +0200

    Revert "tdf#146547 Mark read-only docs as modified"
    This reverts commit cb46fe877fd000ad74eb167a3a6127e89c8990f9.
    
    Causes tdf#155638: "Forms: Form couldn't be closed after it has been
    opened"

--- starmath/qa/cppunit/test_cursor.cxx
+++ starmath/qa/cppunit/test_cursor.cxx
@@ -57,7 +57,6 @@ void Test::setUp()
     SmGlobals::ensure();
 
     xDocShRef = new SmDocShell(SfxModelFlags::EMBEDDED_OBJECT);
-    xDocShRef->DoInitNew();
 }
 
 void Test::tearDown()
--- starmath/qa/cppunit/test_nodetotextvisitors.cxx
+++ starmath/qa/cppunit/test_nodetotextvisitors.cxx
@@ -90,7 +90,6 @@ void Test::setUp()
     SmGlobals::ensure();
 
     xDocShRef = new SmDocShell(SfxModelFlags::EMBEDDED_OBJECT);
-    xDocShRef->DoInitNew();
 }
 
 void Test::tearDown()
