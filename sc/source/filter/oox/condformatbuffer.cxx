631f99be735735fd3422516441f18bff7782972c
Caolán McNamara <caolan.mcnamara@collabora.com>
2023-06-14 15:37:08 +0100

    cid#1532384 Out-of-bounds read
    sizeof returns num of bytes, not num of elements
    
    Change-Id: I9dea109e5f322d93ee10680f120b15554efa92e1
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/153070
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Henry Castro <hcastro@collabora.com>

--- sc/source/filter/oox/condformatbuffer.cxx
+++ sc/source/filter/oox/condformatbuffer.cxx
@@ -265,7 +265,7 @@ static ::Color IndexedColors[] = {
     else if (rAttribs.hasAttribute(XML_indexed))
     {
         sal_uInt32 nIndexed = rAttribs.getUnsigned(XML_indexed, 0);
-        if (nIndexed < sizeof(IndexedColors))
+        if (nIndexed < std::size(IndexedColors))
             nColor = IndexedColors[nIndexed];
     }
 
