fb3948cc559528013e77549f3502c8c1c954b7f6
Michael Stahl <michael.stahl@allotropia.de>
2023-06-14 15:35:24 +0200

    tdf#155690 writerfilter: fix import of bookmark in table...
    ... at start of section.
    
    (regression from commit 2e8aad6d45c53d554ccaf26de998ede708cfc289)
    
    Change-Id: I1e0e720758b607b69645b29c46f4092264289f9e
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/153053
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
    (cherry picked from commit 142aa77265361492e74707b08c5bcd366e7205ad)

--- writerfilter/source/dmapper/DomainMapper_Impl.cxx
+++ writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -6003,6 +6003,9 @@ void DomainMapper_Impl::StartOrEndBookmark( const OUString& rId )
                 else
                 {
                     xCursor = xText->createTextCursorByRange( aBookmarkIter->second.m_xTextRange );
+                }
+                if (!aBookmarkIter->second.m_bIsStartOfText)
+                {
                     xCursor->goRight( 1, false );
                 }
 
@@ -6131,6 +6134,9 @@ void DomainMapper_Impl::startOrEndPermissionRange(sal_Int32 permissinId)
                 else
                 {
                     xCursor = xText->createTextCursorByRange(aPermIter->second.m_xTextRange);
+                }
+                if (!aPermIter->second.m_bIsStartOfText)
+                {
                     xCursor->goRight(1, false);
                 }
 
