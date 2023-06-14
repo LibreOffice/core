LibreOffice/core

commit 2e8aad6d45c53d554ccaf26de998ede708cfc289

fdo#39056 fdo#75431 Section Properties if section starts with table
Section properties are not imported if the section starts with a table for DOCX,
and also for a few RTF files with combination of tables and section breaks.
SwXBodyText::createTextCursorByRange is not able to find the start of section and hence
section properties are not applied.

As a workaround added an empty paragraph at the beginning of every section
which has the the first element as a table. And then removed it when the
section ends ( DomainMapper::lcl_endSectionGroup is called).

Also handled to add the paragraph earlier, if there is a bookmark
so that the bookmark is not attached to the dummy paragraph.

Conflicts:
	sw/qa/extras/ooxmlexport/ooxmlexport.cxx

Reviewed on:
	https://gerrit.libreoffice.org/9097

Change-Id: I717ba40e22b055d974bc83d4414aeb2945e16d26

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -178,6 +178,8 @@ DomainMapper_Impl::DomainMapper_Impl(
         m_bIsParaMarkerChange( false ),
         m_bParaChanged( false ),
         m_bIsFirstParaInSection( true ),
+        m_bDummyParaAddedForTableInSection( false ),
+        m_bTextFrameInserted(false),
         m_bIsLastParaInSection( false ),
         m_bIsInComments( false ),
         m_bParaSectpr( false ),
@@ -280,6 +282,36 @@ void DomainMapper_Impl::SetDocumentSettingsProperty( const OUString& rPropName, 
         }
     }
 }
+void DomainMapper_Impl::RemoveDummyParaForTableInSection()
+{
+    SetIsDummyParaAddedForTableInSection(false);
+    PropertyMapPtr pContext = GetTopContextOfType(CONTEXT_SECTION);
+    SectionPropertyMap* pSectionContext = dynamic_cast< SectionPropertyMap* >( pContext.get() );
+    uno::Reference< text::XTextCursor > xCursor = GetTopTextAppend()->createTextCursorByRange(pSectionContext->GetStartingRange());
+
+    uno::Reference<container::XEnumerationAccess> xEnumerationAccess(xCursor, uno::UNO_QUERY);
+    if (xEnumerationAccess.is() && m_aTextAppendStack.size() == 1 )
+    {
+        uno::Reference<container::XEnumeration> xEnumeration = xEnumerationAccess->createEnumeration();
+        uno::Reference<lang::XComponent> xParagraph(xEnumeration->nextElement(), uno::UNO_QUERY);
+        xParagraph->dispose();
+    }
+}
+void DomainMapper_Impl::AddDummyParaForTableInSection()
+{
+
+    if (!m_aTextAppendStack.empty())
+    {
+        uno::Reference< text::XTextAppend >  xTextAppend = m_aTextAppendStack.top().xTextAppend;
+        uno::Reference< text::XTextCursor > xCrsr = xTextAppend->getText()->createTextCursor();
+        uno::Reference< text::XText > xText = xTextAppend->getText();
+        if(xCrsr.is() && xText.is())
+        {
+            xTextAppend->finishParagraph(  uno::Sequence< beans::PropertyValue >() );
+            SetIsDummyParaAddedForTableInSection(true);
+        }
+    }
+}
 
 void DomainMapper_Impl::RemoveLastParagraph( )
 {
@@ -360,6 +392,27 @@ void DomainMapper_Impl::SetIsFirstParagraphInSection( bool bIsFirst )
 bool DomainMapper_Impl::GetIsFirstParagraphInSection()
 {
     return m_bIsFirstParaInSection;
+}
+
+
+void DomainMapper_Impl::SetIsDummyParaAddedForTableInSection( bool bIsAdded )
+{
+    m_bDummyParaAddedForTableInSection = bIsAdded;
+}
+
+bool DomainMapper_Impl::GetIsDummyParaAddedForTableInSection()
+{
+    return m_bDummyParaAddedForTableInSection;
+}
+
+void DomainMapper_Impl::SetIsTextFrameInserted( bool bIsInserted )
+{
+    m_bTextFrameInserted  = bIsInserted;
+}
+
+bool DomainMapper_Impl::GetIsTextFrameInserted()
+{
+    return m_bTextFrameInserted;
 }
 
 void DomainMapper_Impl::SetParaSectpr(bool bParaSectpr)
@@ -1752,6 +1805,7 @@ void DomainMapper_Impl::PushShapeContext( const uno::Reference< drawing::XShape 
             bool checkZOredrStatus = false;
             if (xSInfo->supportsService("com.sun.star.text.TextFrame"))
             {
+                SetIsTextFrameInserted(true);
                 // Extract the special "btLr text frame" mode, requested by oox, if needed.
                 // Extract vml ZOrder from FrameInteropGrabBag
                 uno::Reference<beans::XPropertySet> xShapePropertySet(xShape, uno::UNO_QUERY);
@@ -4122,6 +4176,18 @@ void DomainMapper_Impl::PopFieldContext()
 
 void DomainMapper_Impl::AddBookmark( const OUString& rBookmarkName, const OUString& rId )
 {
+    /*
+     * Add the dummy paragraph to handle section properties
+     * iff the first element in the section is a table. If the dummy para is not added yet, then add it;
+     * So bookmark is not attched to the wrong paragraph.
+     */
+    if(getTableManager( ).isInCell() && m_nTableDepth == 0 && GetIsFirstParagraphInSection()
+                    && !GetIsDummyParaAddedForTableInSection() &&!GetIsTextFrameInserted())
+    {
+        AddDummyParaForTableInSection();
+    }
+
+    bool bIsAfterDummyPara = GetIsDummyParaAddedForTableInSection() && GetIsFirstParagraphInSection();
     if (m_aTextAppendStack.empty())
         return;
     uno::Reference< text::XTextAppend >  xTextAppend = m_aTextAppendStack.top().xTextAppend;
@@ -4137,8 +4203,10 @@ void DomainMapper_Impl::AddBookmark( const OUString& rBookmarkName, const OUStri
                 uno::Reference< text::XTextContent > xBookmark( m_xTextFactory->createInstance( sBookmarkService ), uno::UNO_QUERY_THROW );
                 uno::Reference< text::XTextCursor > xCursor;
                 uno::Reference< text::XText > xText = aBookmarkIter->second.m_xTextRange->getText();
-                if( aBookmarkIter->second.m_bIsStartOfText )
+                if( aBookmarkIter->second.m_bIsStartOfText && !bIsAfterDummyPara)
+                {
                     xCursor = xText->createTextCursorByRange( xText->getStart() );
+                }
                 else
                 {
                     xCursor = xText->createTextCursorByRange( aBookmarkIter->second.m_xTextRange );
@@ -4164,7 +4232,9 @@ void DomainMapper_Impl::AddBookmark( const OUString& rBookmarkName, const OUStri
             if (xTextAppend.is())
             {
                 uno::Reference< text::XTextCursor > xCursor = xTextAppend->createTextCursorByRange( xTextAppend->getEnd() );
-                bIsStart = !xCursor->goLeft(1, false);
+
+                if(!bIsAfterDummyPara)
+                    bIsStart = !xCursor->goLeft(1, false);
                 xCurrent = xCursor->getStart();
             }
             m_aBookmarkMap.insert(BookmarkMap_t::value_type( rId, BookmarkInsertPosition( bIsStart, rBookmarkName, xCurrent ) ));

sw/qa/extras/ooxmlexport/ooxmlexport.cxx
@@ -3181,6 +3181,18 @@ DECLARE_OOXMLEXPORT_TEST(testFDO77117, "fdo77117.docx")
     // This checks textbox textrun size of font which is in group shape.
     CPPUNIT_ASSERT_EQUAL(11.f, getProperty<float>(xShape, "CharHeight"));
 }
+
+DECLARE_OOXMLEXPORT_TEST(testFDO75431, "fdo75431.docx")
+{
+    xmlDocPtr pXmlDoc = parseExport("word/document.xml");
+
+    if (!pXmlDoc)
+       return;
+
+    assertXPath(pXmlDoc, "//w:tbl", 2);
+    assertXPath(pXmlDoc, "//w:p/w:pPr/w:sectPr/w:type", "val", "nextPage");
+}
+
 #endif
 
 CPPUNIT_PLUGIN_IMPLEMENT();

writerfilter/source/dmapper/DomainMapper.cxx
@@ -2323,7 +2323,20 @@ void DomainMapper::sprmWithProps( Sprm& rSprm, PropertyMapPtr rContext )
     }
     break;
     case NS_ooxml::LN_tblStart:
+
+        /*
+         * Hack for Importing Section Properties
+         * LO is not able to import section properties if first element in the
+         * section is a table. So in case first element is a table add a dummy para
+         * and remove it again when lcl_endSectionGroup is called
+         */
+        if(m_pImpl->m_nTableDepth == 0 && m_pImpl->GetIsFirstParagraphInSection()
+                && !m_pImpl->GetIsDummyParaAddedForTableInSection() && !m_pImpl->GetIsTextFrameInserted())
+        {
+            m_pImpl->AddDummyParaForTableInSection();
+        }
         m_pImpl->m_nTableDepth++;
+
     break;
     case NS_ooxml::LN_tblEnd:
         m_pImpl->m_nTableDepth--;
@@ -2450,6 +2463,7 @@ void DomainMapper::lcl_startSectionGroup()
     {
         m_pImpl->PushProperties(CONTEXT_SECTION);
     }
+    m_pImpl->SetIsFirstParagraphInSection(true);
 }
 
 void DomainMapper::lcl_endSectionGroup()
@@ -2474,7 +2488,13 @@ void DomainMapper::lcl_endSectionGroup()
         SectionPropertyMap* pSectionContext = dynamic_cast< SectionPropertyMap* >( pContext.get() );
         OSL_ENSURE(pSectionContext, "SectionContext unavailable!");
         if(pSectionContext)
+        {
             pSectionContext->CloseSectionGroup( *m_pImpl );
+            // Remove the dummy paragraph if added for
+            // handling the section properties if section starts with a table
+            if (m_pImpl->GetIsDummyParaAddedForTableInSection())
+                m_pImpl->RemoveDummyParaForTableInSection();
+        }
         m_pImpl->PopProperties(CONTEXT_SECTION);
     }
 }
@@ -2765,7 +2785,7 @@ void DomainMapper::lcl_utext(const sal_uInt8 * data_, size_t len)
             bool bRemove = !m_pImpl->GetParaChanged() && m_pImpl->GetParaSectpr() && !bSingleParagraph;
             m_pImpl->SetParaSectpr(false);
             m_pImpl->finishParagraph(m_pImpl->GetTopContextOfType(CONTEXT_PARAGRAPH));
-            if (bRemove)
+            if (bRemove && !m_pImpl->GetIsDummyParaAddedForTableInSection())
                 m_pImpl->RemoveLastParagraph();
         }
         else

writerfilter/source/dmapper/DomainMapperTableManager.hxx
@@ -147,6 +147,11 @@ public:
         m_nLayoutType = nLayoutType;
     }
 
+    bool isInCell()
+    {
+        return TableManager::isInCell();
+    }
+
 };
 
 }}

writerfilter/source/dmapper/DomainMapper_Impl.hxx
@@ -378,6 +378,8 @@ private:
     /// If the current paragraph has any runs.
     bool                            m_bParaChanged;
     bool                            m_bIsFirstParaInSection;
+    bool                            m_bDummyParaAddedForTableInSection;
+    bool                            m_bTextFrameInserted;
     bool                            m_bIsLastParaInSection;
     bool                            m_bIsInComments;
     /// If the current paragraph contains section property definitions.
@@ -451,11 +453,17 @@ public:
     void EndParaMarkerChange( );
     void ChainTextFrames();
 
+    void RemoveDummyParaForTableInSection();
+    void AddDummyParaForTableInSection();
     void RemoveLastParagraph( );
     void SetIsLastParagraphInSection( bool bIsLast );
     bool GetIsLastParagraphInSection();
     void SetIsFirstParagraphInSection( bool bIsFirst );
     bool GetIsFirstParagraphInSection();
+    void SetIsDummyParaAddedForTableInSection( bool bIsAdded );
+    bool GetIsDummyParaAddedForTableInSection();
+    void SetIsTextFrameInserted( bool bIsInserted );
+    bool GetIsTextFrameInserted();
     void SetParaSectpr(bool bParaSectpr);
     bool GetParaSectpr();
     /// Setter method for m_bSdt.

writerfilter/source/dmapper/PropertyMap.hxx
@@ -228,6 +228,8 @@ public:
         m_xStartingRange = xRange;
     }
 
+    ::com::sun::star::uno::Reference< ::com::sun::star::text::XTextRange > GetStartingRange() const { return m_xStartingRange; }
+
     ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySet > GetPageStyle(
             const ::com::sun::star::uno::Reference< ::com::sun::star::container::XNameContainer >& xStyles,
             const ::com::sun::star::uno::Reference < ::com::sun::star::lang::XMultiServiceFactory >& xTextFactory,

commit 142aa77265361492e74707b08c5bcd366e7205ad

tdf#155690 writerfilter: fix import of bookmark in table...
... at start of section.

(regression from commit 2e8aad6d45c53d554ccaf26de998ede708cfc289)

Change-Id: I1e0e720758b607b69645b29c46f4092264289f9e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/153053
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -8344,6 +8344,9 @@ void DomainMapper_Impl::StartOrEndBookmark( const OUString& rId )
                 else
                 {
                     xCursor = xText->createTextCursorByRange( aBookmarkIter->second.m_xTextRange );
+                }
+                if (!aBookmarkIter->second.m_bIsStartOfText)
+                {
                     xCursor->goRight( 1, false );
                 }
 
@@ -8485,6 +8488,9 @@ void DomainMapper_Impl::startOrEndPermissionRange(sal_Int32 permissinId)
                 else
                 {
                     xCursor = xText->createTextCursorByRange(aPermIter->second.m_xTextRange);
+                }
+                if (!aPermIter->second.m_bIsStartOfText)
+                {
                     xCursor->goRight(1, false);
                 }
 

sw/qa/extras/ooxmlexport/ooxmlexport13.cxx
@@ -58,6 +58,53 @@ DECLARE_OOXMLEXPORT_TEST(testTdf126994_lostPageBreak, "tdf126994_lostPageBreak.d
     CPPUNIT_ASSERT_EQUAL_MESSAGE( "Number of Pages", 3, getPages() );
 }
 
+DECLARE_OOXMLEXPORT_TEST(testTdf155690, "tdf155690.docx")
+{
+    uno::Reference<text::XBookmarksSupplier> xBookmarksSupplier(mxComponent, uno::UNO_QUERY);
+    uno::Reference<container::XNameAccess> xBookmarks = xBookmarksSupplier->getBookmarks();
+    {
+        uno::Reference<text::XTextContent> xMark(xBookmarks->getByName("row1_1"), uno::UNO_QUERY);
+        CPPUNIT_ASSERT(xMark.is());
+        // the problem was that the start was after the H
+        CPPUNIT_ASSERT_EQUAL(OUString("Hello world"), xMark->getAnchor()->getString());
+    }
+    {
+        uno::Reference<text::XTextContent> xMark(xBookmarks->getByName("row1_2"), uno::UNO_QUERY);
+        CPPUNIT_ASSERT(xMark.is());
+        CPPUNIT_ASSERT_EQUAL(OUString("Hello world"), xMark->getAnchor()->getString());
+    }
+    {
+        uno::Reference<text::XTextContent> xMark(xBookmarks->getByName("row1_3"), uno::UNO_QUERY);
+        CPPUNIT_ASSERT(xMark.is());
+        CPPUNIT_ASSERT_EQUAL(OUString("ello world"), xMark->getAnchor()->getString());
+    }
+    {
+        uno::Reference<text::XTextContent> xMark(xBookmarks->getByName("row1_4"), uno::UNO_QUERY);
+        CPPUNIT_ASSERT(xMark.is());
+        CPPUNIT_ASSERT_EQUAL(OUString("Hello world"), xMark->getAnchor()->getString());
+    }
+    {
+        uno::Reference<text::XTextContent> xMark(xBookmarks->getByName("row2_1"), uno::UNO_QUERY);
+        CPPUNIT_ASSERT(xMark.is());
+        CPPUNIT_ASSERT_EQUAL(OUString("Hello world"), xMark->getAnchor()->getString());
+    }
+    {
+        uno::Reference<text::XTextContent> xMark(xBookmarks->getByName("row2_1"), uno::UNO_QUERY);
+        CPPUNIT_ASSERT(xMark.is());
+        CPPUNIT_ASSERT_EQUAL(OUString("Hello world"), xMark->getAnchor()->getString());
+    }
+    {
+        uno::Reference<text::XTextContent> xMark(xBookmarks->getByName("row2_3"), uno::UNO_QUERY);
+        CPPUNIT_ASSERT(xMark.is());
+        CPPUNIT_ASSERT_EQUAL(OUString("ello world"), xMark->getAnchor()->getString());
+    }
+    {
+        uno::Reference<text::XTextContent> xMark(xBookmarks->getByName("row2_4"), uno::UNO_QUERY);
+        CPPUNIT_ASSERT(xMark.is());
+        CPPUNIT_ASSERT_EQUAL(OUString("Hello world"), xMark->getAnchor()->getString());
+    }
+}
+
 CPPUNIT_TEST_FIXTURE(Test, testTdf121374_sectionHF)
 {
     loadAndReload("tdf121374_sectionHF.odt");