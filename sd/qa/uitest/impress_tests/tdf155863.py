257dbf548a4f844422d793b240e44844b3eb7aea
Tibor Nagy <nagy.tibor2@nisz.hu>
2023-06-16 09:34:29 +0200

    tdf#155863 sd: fix resizing of cropped images to original size
    Resizing with the function "Original Size" (e.g. in the context menu) resulted distortion in case of cropped images, if the original image
    and its cropping have different aspect ratios. Now zoom the cropped
    image to the original resolution instead of stretching it to the
    same size.
    
    Change-Id: I5e59f8b48dc03844a739c3eb803e3195a12d9c6d
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/153170
    Tested-by: László Németh <nemeth@numbertext.org>
    Reviewed-by: László Németh <nemeth@numbertext.org>
    (cherry picked from commit a4e12cbfc69cfe668fa30756a3c5843e911e22b1)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/153342
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

---
+++ sd/qa/uitest/impress_tests/tdf155863.py
@@ -0,0 +1,34 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+from uitest.framework import UITestCase
+from libreoffice.uno.propertyvalue import mkPropertyValues
+from uitest.uihelper.common import get_state_as_dict, get_url_for_data_file
+
+class tdf155863(UITestCase):
+    def test_tdf155863(self):
+        # This document contains an image with an original size of 7.99cm x 5.74cm.
+        # The image has been cropped 0.8cm on the left, 3.59cm on the right, 0.99cm at the top,
+        # and 0.75cm at the bottom.
+        # Also, it has been resized to a width of 2.1cm and a height of 2.33cm.
+        with self.ui_test.load_file(get_url_for_data_file("tdf155863.odp")) as document:
+            xImpressDoc = self.xUITest.getTopFocusWindow()
+            xEditWin = xImpressDoc.getChild("impress_win")
+
+            # select the image on the first slide
+            xEditWin.executeAction("SELECT", mkPropertyValues({"OBJECT":"Image"}))
+
+            # click "Original Size"
+            self.xUITest.executeCommand(".uno:OriginalSize")
+
+            # tdf#155863: Without the fix in place, the image will be distorted due to the wrong size.
+            self.assertEqual(3597, document.DrawPages[0].getByIndex(0).Size.Width)
+            self.assertEqual(3998, document.DrawPages[0].getByIndex(0).Size.Height)
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
