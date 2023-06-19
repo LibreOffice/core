LibreOffice/core

commit 12a9009a1c19ee26c65fb44fc90f3432c88ab6a5

sw floattable: fix bad position of follow fly if anchor is positioned late
The bugdoc has a split fly, the master is centered horizontally. That
means the follow should be centered as well, but it was aligned to the
left.

The problem is that the follow fly was positioned when the anchor was
not positioned at all (its top left was 0,0) and then there was no
invalidation of the fly position to recalc it later when the anchor got
positioned. The existing InvalidatePos() call is not enough because once
a fly is formatted, its position gets locked and only
unlockPositionOfObjects() at the end of the layout process will unlock
it.

Fix the problem similar to what lcl_InvalidateLowerObjs() for SwTabFrame
does: if we know that the position of the anchor changed, then unlock
the position before invalidating it.

If this leads to unwanted re-positioning of flys, it would be perhaps
possible to do this when the anchor position changes from 0,0 to a valid
position, but for now just do this all time the anchor position changes.

Change-Id: I811bdfa1eb6705ff3de6ec77111e9500617e8bee
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/149367
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

sw/source/core/text/frmform.cxx
@@ -340,25 +340,31 @@ bool SwTextFrame::CalcFollow(TextFrameIndex const nTextOfst)
 
 void SwTextFrame::MakePos()
 {
+    Point aOldPos = getFrameArea().Pos();
     SwFrame::MakePos();
 
-    // Find the master frame.
-    const SwTextFrame* pMaster = this;
-    while (pMaster->IsFollow())
+    // Recalc split flys if our position changed.
+    if (aOldPos != getFrameArea().Pos())
     {
-        pMaster = pMaster->FindMaster();
-    }
-    // Find which flys are effectively anchored to this frame.
-    for (const auto& pFly : pMaster->GetSplitFlyDrawObjs())
-    {
-        SwTextFrame* pFlyAnchor = pFly->FindAnchorCharFrame();
-        if (pFlyAnchor != this)
+        // Find the master frame.
+        const SwTextFrame* pMaster = this;
+        while (pMaster->IsFollow())
         {
-            continue;
+            pMaster = pMaster->FindMaster();
         }
-        // Possibly this fly was positioned relative to us, invalidate its position now that our
-        // position is changed.
-        pFly->InvalidatePos();
+        // Find which flys are effectively anchored to this frame.
+        for (const auto& pFly : pMaster->GetSplitFlyDrawObjs())
+        {
+            SwTextFrame* pFlyAnchor = pFly->FindAnchorCharFrame();
+            if (pFlyAnchor != this)
+            {
+                continue;
+            }
+            // Possibly this fly was positioned relative to us, invalidate its position now that our
+            // position is changed.
+            pFly->UnlockPosition();
+            pFly->InvalidatePos();
+        }
     }
 
     // Inform LOK clients about change in position of redlines (if any)

sw/qa/core/layout/flycnt.cxx
@@ -509,6 +509,40 @@ CPPUNIT_TEST_FIXTURE(Test, testSplitFlyCompat14Body)
     SwFrame* pRow2 = pTab2->GetLower();
     CPPUNIT_ASSERT(!pRow2->GetNext());
 }
+
+CPPUNIT_TEST_FIXTURE(Test, testSplitFlyFollowHorizontalPosition)
+{
+    // Given a document with 2 pages, master fly on page 1, follow fly on page 2:
+    SwModelTestBase::FlySplitGuard aGuard;
+    createSwDoc("floattable-hori-pos.docx");
+
+    // When laying out that document:
+    calcLayout();
+
+    // Then make sure that the follow fly doesn't have a different horizontal position:
+    SwDoc* pDoc = getSwDoc();
+    SwRootFrame* pLayout = pDoc->getIDocumentLayoutAccess().GetCurrentLayout();
+    auto pPage1 = dynamic_cast<SwPageFrame*>(pLayout->Lower());
+    CPPUNIT_ASSERT(pPage1);
+    const SwSortedObjs& rPage1Objs = *pPage1->GetSortedObjs();
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(1), rPage1Objs.size());
+    auto pPage1Fly = dynamic_cast<SwFlyAtContentFrame*>(rPage1Objs[0]);
+    CPPUNIT_ASSERT(pPage1Fly);
+    tools::Long nPage1FlyLeft = pPage1Fly->getFrameArea().Left();
+    auto pPage2 = dynamic_cast<SwPageFrame*>(pPage1->GetNext());
+    CPPUNIT_ASSERT(pPage2);
+    const SwSortedObjs& rPage2Objs = *pPage2->GetSortedObjs();
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(1), rPage2Objs.size());
+    auto pPage2Fly = dynamic_cast<SwFlyAtContentFrame*>(rPage2Objs[0]);
+    CPPUNIT_ASSERT(pPage2Fly);
+    tools::Long nPage2FlyLeft = pPage2Fly->getFrameArea().Left();
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 5528
+    // - Actual  : 284
+    // i.e. the follow fly was pushed towards the left, instead of having the same position as the
+    // master fly.
+    CPPUNIT_ASSERT_EQUAL(nPage1FlyLeft, nPage2FlyLeft);
+}
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

commit 01eff4a68b05dd4eeee94bc4b3b018059efa60d4

sw floattable: avoid layout loop for negative vert offset in MakePos()
The bugdoc has 2 paragraphs and a floating table between them. The
floating table has a large enough negative vertical offset so it "pushes
down" the paragraph that is before the table in the doc model. Our
layout didn't push the first paragraph down, the second paragraph
overlapped with the table and the whole layout process was just stopped
by the layout loop control.

What happened is that we realized that we have to push down the first
paragraph, so an SwFlyPortion was created for it. Then we made an
UnlockPosition() in SwTextFrame::MakePos(), so it could be calculated
again. This lead to an oscillation, and the calculated vertical position
of the floating table's fly changed between 964 (good) and 2990 (bad)
twips.

Fix the problem by limiting when SwTextFrame::MakePos() calls
UnlockPosition(). The general strategy is that flys are only positioned
once during a layout pass. The exception from this is when the fly moved
to a new page and it's not yet positioned. So if we unlock when the fly
only has the page frame top left position, then we keep the original
use-case working, but fix the layout loop.

Regression from commit 12a9009a1c19ee26c65fb44fc90f3432c88ab6a5 (sw
floattable: fix bad position of follow fly if anchor is positioned late,
2023-03-23), where I didn't realize how dangerous UnlockPosition() is.

Change-Id: I0aa0a52db712a464105e8040497fd429e0604309
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/153245
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

sw/CppunitTest_sw_core_text.mk
@@ -14,6 +14,7 @@ $(eval $(call gb_CppunitTest_CppunitTest,sw_core_text))
 $(eval $(call gb_CppunitTest_use_common_precompiled_header,sw_core_text))
 
 $(eval $(call gb_CppunitTest_add_exception_objects,sw_core_text, \
+    sw/qa/core/text/frmform \
     sw/qa/core/text/text \
 ))
 

sw/source/core/text/frmform.cxx
@@ -364,7 +364,13 @@ void SwTextFrame::MakePos()
             }
             // Possibly this fly was positioned relative to us, invalidate its position now that our
             // position is changed.
-            pFly->UnlockPosition();
+            SwPageFrame* pPageFrame = pFly->FindPageFrame();
+            if (pPageFrame && pFly->getFrameArea().Pos() == pPageFrame->getFrameArea().Pos())
+            {
+                // The position was just adjusted to be be inside the page frame, so not really
+                // positioned, unlock the position once to allow a recalc.
+                pFly->UnlockPosition();
+            }
             pFly->InvalidatePos();
         }
     }

sw/qa/core/text/frmform.cxx
@@ -1,0 +1,64 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <swmodeltestbase.hxx>
+
+#include <memory>
+
+#include <IDocumentLayoutAccess.hxx>
+#include <rootfrm.hxx>
+#include <sortedobjs.hxx>
+#include <anchoredobject.hxx>
+#include <pagefrm.hxx>
+
+namespace
+{
+/// Covers sw/source/core/text/frmform.cxx fixes.
+class Test : public SwModelTestBase
+{
+public:
+    Test()
+        : SwModelTestBase("/sw/qa/core/text/data/")
+    {
+    }
+};
+
+CPPUNIT_TEST_FIXTURE(Test, testFloattableNegativeVertOffset)
+{
+    // Given a document with 2 paragraphs, floating table is between the two (so anchored to the
+    // 2nd) and with a negative vertical offset:
+    createSwDoc("floattable-negative-vert-offset.docx");
+
+    // When laying out that document:
+    calcLayout();
+
+    // Then make sure that the negative vertical offset shifts both paragraphs down:
+    SwDoc* pDoc = getSwDoc();
+    SwRootFrame* pLayout = pDoc->getIDocumentLayoutAccess().GetCurrentLayout();
+    auto pPage = dynamic_cast<SwPageFrame*>(pLayout->Lower());
+    CPPUNIT_ASSERT(pPage);
+    CPPUNIT_ASSERT(pPage->GetSortedObjs());
+    const SwSortedObjs& rPageObjs = *pPage->GetSortedObjs();
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(1), rPageObjs.size());
+    SwAnchoredObject* pPageObj = rPageObjs[0];
+    const SwRect& rFlyRect = pPageObj->GetObjRectWithSpaces();
+    SwFrame* pBody = pPage->FindBodyCont();
+    SwFrame* pPara1 = pBody->GetLower();
+    SwFrame* pPara2 = pPara1->GetNext();
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected less than: 993
+    // - Actual  : 2709
+    // i.e. the expectation that the fly doesn't overlap with the 2nd paragraph was not true.
+    // Instead we got a layout loop, aborted by the loop control, and the fly overlapped with the
+    // 2nd paragraph.
+    CPPUNIT_ASSERT_LESS(pPara2->getFrameArea().Top(), rFlyRect.Bottom());
+}
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */