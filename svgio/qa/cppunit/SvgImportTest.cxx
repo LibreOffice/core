47a7f7e41e4c89ebeacab26d31dc78a2ffa94311
Xisco Fauli <xiscofauli@libreoffice.org>
2023-06-23 12:59:17 +0200

    tdf#156034: check for css style further up in the hierarchy
    Change-Id: I92c6673f1249cc4a273c490cdc0496474ce1f0c5
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/153498
    Tested-by: Jenkins
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
    (cherry picked from commit dc7f80159823ef00048d9f5c2716e3122654cbbb)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/153518

--- svgio/qa/cppunit/SvgImportTest.cxx
+++ svgio/qa/cppunit/SvgImportTest.cxx
@@ -320,6 +320,25 @@ CPPUNIT_TEST_FIXTURE(Test, testTdf145896)
     assertXPath(pDocument, "/primitive2D/transform/polypolygoncolor[3]", "color", "#0000ff");
 }
 
+CPPUNIT_TEST_FIXTURE(Test, testTdf156034)
+{
+    Primitive2DSequence aSequence = parseSvg(u"/svgio/qa/cppunit/data/tdf156034.svg");
+    CPPUNIT_ASSERT_EQUAL(1, static_cast<int>(aSequence.getLength()));
+
+    drawinglayer::Primitive2dXmlDump dumper;
+    xmlDocUniquePtr pDocument = dumper.dumpAndParse(aSequence);
+
+    CPPUNIT_ASSERT (pDocument);
+
+    // Without the fix in place, this test would have failed with
+    // - Expected: #008000
+    // - Actual  : #0000ff
+    assertXPath(pDocument, "/primitive2D/transform/polypolygoncolor[1]", "color", "#008000");
+    assertXPath(pDocument, "/primitive2D/transform/polypolygoncolor[2]", "color", "#008000");
+    assertXPath(pDocument, "/primitive2D/transform/polypolygoncolor[3]", "color", "#008000");
+    assertXPath(pDocument, "/primitive2D/transform/polypolygoncolor[4]", "color", "#008000");
+}
+
 CPPUNIT_TEST_FIXTURE(Test, testTdf156018)
 {
     Primitive2DSequence aSequence = parseSvg(u"/svgio/qa/cppunit/data/tdf156018.svg");
