LibreOffice/core

commit bc72514f90d90e1ab3fed8167663e835edf03508

tdf#53970 PPTX: fix export of embedded media files
Embedded media files lost in documents created
with Impress after PPTX export.

Change-Id: I453b58f9cfa6a33653e9216fb82b66970a9ec31b
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/132095
Tested-by: Jenkins
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

oox/source/export/shapes.cxx
@@ -1662,6 +1662,7 @@ static const NameToConvertMapType& lcl_GetConverters()
         { "com.sun.star.drawing.EllipseShape"              , &ShapeExport::WriteEllipseShape },
         { "com.sun.star.drawing.GraphicObjectShape"        , &ShapeExport::WriteGraphicObjectShape },
         { "com.sun.star.drawing.LineShape"                 , &ShapeExport::WriteLineShape },
+        { "com.sun.star.drawing.MediaShape"                , &ShapeExport::WriteGraphicObjectShape },
         { "com.sun.star.drawing.OpenBezierShape"           , &ShapeExport::WriteOpenPolyPolygonShape },
         { "com.sun.star.drawing.PolyPolygonShape"          , &ShapeExport::WriteClosedPolyPolygonShape },
         { "com.sun.star.drawing.PolyLineShape"             , &ShapeExport::WriteOpenPolyPolygonShape },

oox/source/export/drawingml.cxx
@@ -1428,6 +1428,11 @@ void DrawingML::WriteMediaNonVisualProperties(const css::uno::Reference<css::dra
             aMimeType = "audio/mp4";
             eMediaType = Relationship::AUDIO;
         }
+        else if (aExtension.equalsIgnoreAsciiCase(".mp3"))
+        {
+            aMimeType = "audio/mp3";
+            eMediaType = Relationship::AUDIO;
+        }
     }
 
     OUString aVideoFileRelId;

commit 863a32171ed8efdf1aaee59918e49613e7ccd7a9

tdf155903 DOCX export: fix corrupt file with embedded media
Regression from commit bc72514f90d90e1ab3fed8167663e835edf03508
"tdf#53970 PPTX: fix export of embedded media files".

Change-Id: I04521227346817d91f720b1f6a77beb7f4a01f83
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/153619
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>
Tested-by: Jenkins

oox/source/export/shapes.cxx
@@ -1272,7 +1272,10 @@ void ShapeExport::WriteGraphicObjectShapePart( const Reference< XShape >& xShape
         xShapeProps->getPropertyValue("Graphic") >>= xGraphic;
     }
 
-    bool bHasMediaURL = xShapeProps.is() && xShapeProps->getPropertySetInfo()->hasPropertyByName("MediaURL") && (xShapeProps->getPropertyValue("MediaURL") >>= sMediaURL);
+    // tdf#155903 Only for PPTX, Microsoft does not support this feature in Word and Excel.
+    bool bHasMediaURL = GetDocumentType() == DOCUMENT_PPTX && xShapeProps.is()
+                        && xShapeProps->getPropertySetInfo()->hasPropertyByName("MediaURL")
+                        && (xShapeProps->getPropertyValue("MediaURL") >>= sMediaURL);
 
     if (!xGraphic.is() && !bHasMediaURL)
     {

sw/qa/extras/ooxmlexport/ooxmlexport18.cxx
@@ -864,6 +864,12 @@ DECLARE_OOXMLEXPORT_TEST(testTdf155736, "tdf155736_PageNumbers_footer.docx")
     CPPUNIT_ASSERT_EQUAL(OUString("Page * of *"), parseDump("/root/page[2]/footer/txt/text()"));
 }
 
+DECLARE_OOXMLEXPORT_TEST(testTdf155903, "tdf155903.odt")
+{
+    // Without the accompanying fix in place, this test would have crashed,
+    // because the exported file was corrupted.
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */