LibreOffice/core

commit dc03f59374d4fc74dd44ddedc903bb9c00a74bb7

tdf#155526 Add Period to trend line equation
Insert Period value earlier to have it ready for representation
Allow Moving average equation in dialog UI

Change-Id: I1d20572c39462e60b60ab7eec1f174a3cf934d4a
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/153094
Tested-by: Jenkins
Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

chart2/source/controller/dialogs/ObjectNameProvider.cxx
@@ -601,18 +601,9 @@ OUString ObjectNameProvider::getHelpText( std::u16string_view rObjectCID, const 
                         xCalculator->setXYNames ( aXName, aYName );
                         RegressionCurveHelper::initializeCurveCalculator( xCalculator, xSeries, xChartModel );
 
-                        // change text for Moving Average
-                        OUString aWildcard( "%PERIOD" );
-                        sal_Int32 nIndex = xCalculator->getRepresentation().indexOf( aWildcard );
-                        if( nIndex != -1 )
-                        {  // replace period
-                                aRet = xCalculator->getRepresentation();
-                                aRet = aRet.replaceAt( nIndex, aWildcard.getLength(), OUString::number(aPeriod) );
-                        }
-
                         // replace formula
-                        aWildcard = "%FORMULA";
-                        nIndex = aRet.indexOf( aWildcard );
+                        OUString aWildcard = "%FORMULA";
+                        sal_Int32 nIndex = aRet.indexOf( aWildcard );
                         if( nIndex != -1 )
                         {
                             OUString aFormula ( xCalculator->getRepresentation() );

chart2/source/controller/dialogs/res_Trendline.cxx
@@ -372,10 +372,8 @@ void TrendlineResources::UpdateControlStates()
     m_xFmtFld_InterceptValue->set_sensitive( bInterceptAvailable );
     if( bMovingAverage )
     {
-        m_xCB_ShowEquation->set_state(TRISTATE_FALSE);
         m_xCB_ShowCorrelationCoeff->set_state(TRISTATE_FALSE);
     }
-    m_xCB_ShowEquation->set_sensitive( !bMovingAverage );
     m_xCB_ShowCorrelationCoeff->set_sensitive( !bMovingAverage );
     m_xCB_RegressionMovingType->set_sensitive(bMovingAverage);
     m_xNF_Period->set_sensitive(bMovingAverage);
@@ -428,8 +426,6 @@ void TrendlineResources::SetNbPoints( sal_Int32 nNbPoints )
 
 IMPL_LINK_NOARG(TrendlineResources, ShowEquation, weld::Toggleable&, void)
 {
-    m_xEE_XName->set_sensitive(m_xCB_ShowEquation->get_active());
-    m_xEE_YName->set_sensitive(m_xCB_ShowEquation->get_active());
     UpdateControlStates();
 }
 

chart2/source/tools/MovingAverageRegressionCurveCalculator.cxx
@@ -157,7 +157,15 @@ OUString MovingAverageRegressionCurveCalculator::ImplGetRepresentation(
     const uno::Reference< util::XNumberFormatter >& /*xNumFormatter*/,
     sal_Int32 /*nNumberFormatKey*/, sal_Int32* /*pFormulaLength = nullptr */ ) const
 {
-    return SchResId( STR_OBJECT_MOVING_AVERAGE_WITH_PARAMETERS );
+    OUString aRet = SchResId( STR_OBJECT_MOVING_AVERAGE_WITH_PARAMETERS );
+    // change text for Moving Average
+    OUString aWildcard( "%PERIOD" );
+    sal_Int32 nIndex = aRet.indexOf( aWildcard );
+    if( nIndex != -1 )
+    {  // replace period
+        aRet = aRet.replaceAt( nIndex, aWildcard.getLength(), OUString::number(mPeriod) );
+    }
+    return aRet;
 }
 
 } //  namespace chart

commit 125b0f6609332523d30bef600979b698c2d216f8

tdf#155526 Restore status bar for moving average
Previous commit	dc03f59374d4fc74dd44ddedc903bb9c00a74bb7 broke text in
status bar when a moving average trend line is selected

Change-Id: I259fb9aee1eccf9fac93038c6dd2755490769f88
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/153677
Tested-by: Jenkins
Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

chart2/source/controller/dialogs/ObjectNameProvider.cxx
@@ -597,33 +597,41 @@ OUString ObjectNameProvider::getHelpText( std::u16string_view rObjectCID, const 
                             if ( !(xEqProp->getPropertyValue( "YName") >>= aYName) )
                                 aYName = "f(x)";
                         }
-                        xCalculator->setRegressionProperties(aDegree, bForceIntercept, aInterceptValue, 2, aMovingType);
+                        xCalculator->setRegressionProperties(aDegree, bForceIntercept, aInterceptValue, aPeriod, aMovingType);
                         xCalculator->setXYNames ( aXName, aYName );
                         RegressionCurveHelper::initializeCurveCalculator( xCalculator, xSeries, xChartModel );
 
-                        // replace formula
-                        OUString aWildcard = "%FORMULA";
-                        sal_Int32 nIndex = aRet.indexOf( aWildcard );
-                        if( nIndex != -1 )
+                        // change text for Moving Average
+                        if ( RegressionCurveHelper::getRegressionType( xCurve ) == SvxChartRegress::MovingAverage )
                         {
-                            OUString aFormula ( xCalculator->getRepresentation() );
-                            if ( cDecSeparator != '.' )
-                            {
-                                aFormula = aFormula.replace( '.', cDecSeparator );
-                            }
-                            aRet = aRet.replaceAt( nIndex, aWildcard.getLength(), aFormula );
+                            aRet = xCalculator->getRepresentation();
                         }
-
-                        // replace r^2
-                        aWildcard = "%RSQUARED";
-                        nIndex = aRet.indexOf( aWildcard );
-                        if( nIndex != -1 )
+                        else
                         {
-                            double fR( xCalculator->getCorrelationCoefficient());
-                            aRet = aRet.replaceAt(
-                                nIndex, aWildcard.getLength(),
-                                ::rtl::math::doubleToUString(
-                                    fR*fR, rtl_math_StringFormat_G, 4, cDecSeparator, true ));
+                            // replace formula
+                            OUString aWildcard = "%FORMULA";
+                            sal_Int32 nIndex = aRet.indexOf( aWildcard );
+                            if( nIndex != -1 )
+                            {
+                                OUString aFormula ( xCalculator->getRepresentation() );
+                                if ( cDecSeparator != '.' )
+                                {
+                                    aFormula = aFormula.replace( '.', cDecSeparator );
+                                }
+                                aRet = aRet.replaceAt( nIndex, aWildcard.getLength(), aFormula );
+                            }
+
+                            // replace r^2
+                            aWildcard = "%RSQUARED";
+                            nIndex = aRet.indexOf( aWildcard );
+                            if( nIndex != -1 )
+                            {
+                                double fR( xCalculator->getCorrelationCoefficient());
+                                aRet = aRet.replaceAt(
+                                    nIndex, aWildcard.getLength(),
+                                    ::rtl::math::doubleToUString(
+                                        fR*fR, rtl_math_StringFormat_G, 4, cDecSeparator, true ));
+                            }
                         }
                     }
                     catch( const uno::Exception & )