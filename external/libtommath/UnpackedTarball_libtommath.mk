56de0572a3e581e678b5e0f1583bd6de29db20bb
Taichi Haradaguchi <20001722@ymail.ne.jp>
2023-07-03 17:27:57 +0900

    LibTomMath: upgrade to release 1.2.0
    * external/libtommath/clang-cl.patch: removed "typedef unsigned __int128    mp_word" from tommmath.h
    * external/libtommath/libtommath-msvc.patch: fixed upstream
    
    Change-Id: I38fe730ff0e9649c6a0ad8d64b723a27e3434012
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/153871
    Tested-by: Jenkins
    Reviewed-by: Taichi Haradaguchi <20001722@ymail.ne.jp>
    (cherry picked from commit 13bc0489d98b1f49a4f82fccb89d640439db4f4e)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/157248
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

--- external/libtommath/UnpackedTarball_libtommath.mk
+++ external/libtommath/UnpackedTarball_libtommath.mk
@@ -14,8 +14,6 @@ $(eval $(call gb_UnpackedTarball_set_tarball,libtommath,$(LIBTOMMATH_TARBALL)))
 $(eval $(call gb_UnpackedTarball_set_patchlevel,libtommath,0))
 
 $(eval $(call gb_UnpackedTarball_add_patches,libtommath,\
-	external/libtommath/libtommath-msvc.patch \
-	external/libtommath/clang-cl.patch \
 ))
 
 # vim: set noet sw=4 ts=4:
