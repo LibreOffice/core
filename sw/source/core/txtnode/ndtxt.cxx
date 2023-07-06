LibreOffice/core

commit c684301352bcca709bc8d1c99e0f08e7096e9716

(related: tdf#149595) sw: also re-sort SwSortedObjs on SwPageFrame
... if SwTextNode content changes; for the unit test here we get:

  Error: elements in iterator range [first, last) are not partitioned by
  the predicate __comp and value __val.

Also, the SwTextNode may have multiple text frames on multiple pages.

Change-Id: I05d4276e4b4b94fe3cd2cdfda3418ad51fa08f5f
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/144108
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/core/txtnode/ndtxt.cxx
@@ -57,6 +57,7 @@
 #include <paratr.hxx>
 #include <txtfrm.hxx>
 #include <ftnfrm.hxx>
+#include <pagefrm.hxx>
 #include <rootfrm.hxx>
 #include <expfld.hxx>
 #include <section.hxx>
@@ -1593,10 +1594,21 @@ void SwTextNode::Update(
 
     //Any drawing objects anchored into this text node may be sorted by their
     //anchor position which may have changed here, so resort them
-    SwContentFrame* pContentFrame = getLayoutFrame(GetDoc().getIDocumentLayoutAccess().GetCurrentLayout());
-    SwSortedObjs* pSortedObjs = pContentFrame ? pContentFrame->GetDrawObjs() : nullptr;
-    if (pSortedObjs)
-        pSortedObjs->UpdateAll();
+    SwIterator<SwTextFrame, SwTextNode, sw::IteratorMode::UnwrapMulti> iter(*this);
+    for (SwTextFrame* pFrame = iter.First(); pFrame; pFrame = iter.Next())
+    {
+        SwSortedObjs * pSortedObjs(pFrame->GetDrawObjs());
+        if (pSortedObjs)
+        {
+            pSortedObjs->UpdateAll();
+        }
+        // also sort the objs on the page frame
+        pSortedObjs = pFrame->FindPageFrame()->GetSortedObjs();
+        if (pSortedObjs) // doesn't exist yet if called for inserting as-char fly
+        {
+            pSortedObjs->UpdateAll();
+        }
+    }
 
     // Update the paragraph signatures.
     if (SwEditShell* pEditShell = GetDoc().GetEditShell())

commit a62af0444515ab28bc0ffdc8ff7f320ca962bbc0

sw: fix crash in SwTextNode::Update
FindPageFrame might return nullptr
Introduced with c684301352bcca709bc8d1c99e0f08e7096e9716
"(related: tdf#149595) sw: also re-sort SwSortedObjs on SwPageFrame"
See https://crashreport.libreoffice.org/stats/signature/SwTextNode::Update(SwContentIndex%20const%20&,long,SwContentIndexReg::UpdateMode)

Change-Id: I2e8df1428400f6752eafaaa6102a1fc3de3f0daa
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/153977
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/core/txtnode/ndtxt.cxx
@@ -1603,7 +1603,9 @@ void SwTextNode::Update(
             pSortedObjs->UpdateAll();
         }
         // also sort the objs on the page frame
-        pSortedObjs = pFrame->FindPageFrame()->GetSortedObjs();
+        if (SwPageFrame *pPage = pFrame->FindPageFrame())
+            pSortedObjs = pPage->GetSortedObjs();
+
         if (pSortedObjs) // doesn't exist yet if called for inserting as-char fly
         {
             pSortedObjs->UpdateAll();