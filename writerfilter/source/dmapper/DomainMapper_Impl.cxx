c93083d583e7b29a27bf8ff6e2664fe0613a6542
László Németh <nemeth@numbertext.org>
2023-07-06 17:31:20 +0200

    tdf#153804 DOCX import: fix lost footnotes in case of comments
    Incomplete handling of m_bIsFootnote had a conflict with
    XNote stream COMMENTS (i.e. not FOOTNOTES and ENDNOTES)
    since commit 96a856f87f16cca2e039c973c18d57c8b9dca362
    in DomainMapper::lcl_checkId(), resulting lost (order or
    content) of footnotes, when were comments in the document.
    
    Regression from commit 96a856f87f16cca2e039c973c18d57c8b9dca362
    "tdf#152206 DOCX import: fix mixed first footnote".
    
    Change-Id: I8d6901e8db1e183b959fecd350d6a263e79b5591
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/154147
    Tested-by: László Németh <nemeth@numbertext.org>
    Reviewed-by: László Németh <nemeth@numbertext.org>
    (cherry picked from commit cca6ccf0f5e2f72c96f4f8879b8d894faea28b81)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/154185
    Tested-by: Jenkins

--- writerfilter/source/dmapper/DomainMapper_Impl.cxx
+++ writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -4087,7 +4087,7 @@ void DomainMapper_Impl::PopFootOrEndnote()
     }
     m_aRedlines.pop();
     m_eSkipFootnoteState = SkipFootnoteSeparator::OFF;
-    m_bInFootOrEndnote = false;
+    m_bInFootOrEndnote = m_bInFootnote = false;
     m_pFootnoteContext = nullptr;
     m_bFirstParagraphInCell = m_bSaveFirstParagraphInCell;
 }
