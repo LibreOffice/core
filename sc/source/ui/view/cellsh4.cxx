4adc868328e958a4a9cead3731bd3468497c97c8
Heiko Tietze <heiko.tietze@documentfoundation.org>
2023-07-17 16:03:41 +0200

    Resolves tdf#112876 - Make use of scroll lock configurable
    Change-Id: I8e2f238e59601fcc1d92fc0593c3d20e75396cc5
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/154528
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
    Tested-by: Jenkins

--- sc/source/ui/view/cellsh4.cxx
+++ sc/source/ui/view/cellsh4.cxx
@@ -27,6 +27,7 @@
 #include <inputhdl.hxx>
 #include <inputwin.hxx>
 #include <document.hxx>
+#include <officecfg/Office/Calc.hxx>
 #include <sc.hrc>
 
 void ScCellShell::ExecuteCursor( SfxRequest& rReq )
@@ -121,10 +122,13 @@ void ScCellShell::ExecuteCursor( SfxRequest& rReq )
     // If ScrollLock key is active, cell cursor stays on the current cell while
     // scrolling the grid.
     bool bScrollLock = false;
-    KeyIndicatorState eState = pFrameWin->GetIndicatorState();
-    if (eState & KeyIndicatorState::SCROLLLOCK)
-        bScrollLock = true;
-
+    // tdf#112876 - allow to disable for special keyboards
+    if (officecfg::Office::Calc::Input::UseScrollLock::get())
+    {
+        KeyIndicatorState eState = pFrameWin->GetIndicatorState();
+        if (eState & KeyIndicatorState::SCROLLLOCK)
+            bScrollLock = true;
+    }
     //OS: once for all should do, however!
     pTabViewShell->ExecuteInputDirect();
     switch ( nSlotId )
