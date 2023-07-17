LibreOffice/core

commit ea858ca92488309789e83666681a358af1d9f6f6

gdb: BigPtrArrayPrinter, add the suffix, so 0->0ul
On pc Debian testing with GNU gdb (Debian 13.2-1) 13.2, I got this trying to debug tdf#131173

Thread 1 "soffice.bin" hit Breakpoint 1, SwUndoTableMerge::UndoImpl (this=0x5599a0d302b0, rContext=...) at sw/source/core/undo/untbl.cxx:1878
1878	        SwStartNode* pSttNd = rDoc.GetNodes().MakeTextSection( aIdx.GetNode(),
(gdb) p rDoc.GetNodes()
$1 = (SwNodes &) @0x5599a0477000: {<BigPtrArray> = BigPtrArray of length 18Python Exception <class 'gdb.error'>: No type named std::_Head_base<0, BlockInfo**, false>.
, m_vIndices = 0x5599a04a4478, m_rMyDoc = @0x5599a0479590, m_pEndOfPostIts = 0x5599a04739b0, m_pEndOfInserts = 0x5599a042da70,
  m_pEndOfAutotext = 0x5599a04764b0, m_pEndOfRedlines = 0x5599a04765e0, m_pEndOfContent = std::unique_ptr<SwNode> = {get() = 0x5599a0478d20},
  m_aOutlineNodes = {<o3tl::sorted_vector<SwNode*, CompareSwOutlineNodes, o3tl::find_unique, true>> = {m_vector = std::__debug::vector of length 0, capacity 0}, static npos = 18446744073709551615},
  m_bInNodesDel = false, m_bInDelUpdOutline = false}

with the patch, I got:
(gdb) p rDoc.GetNodes()
$1 = (SwNodes &) @0x55c379492b10: {<BigPtrArray> = BigPtrArray of length 18 = {
[   0] 0x55c37948c9a0            StartNode ,
[   1] 0x55c3794859a0              EndNode ,
[   2] 0x55c379492bb0            StartNode ,
[   3] 0x55c37948a6c0              EndNode ,
[   4] 0x55c3794925a0            StartNode ,
[   5] 0x55c37948ff10              EndNode ,
[   6] 0x55c379492630            StartNode ,
[   7] 0x55c379490040              EndNode ,
[   8] 0x55c3794926c0            StartNode ,
[   9]  0x55c3795bd170           TableNode ,
[  10]   0x55c379606280          StartNode ,
[  11]    0x55c379616ee8          TextNode "",
[  12]   0x55c37960cd70            EndNode ,
[  13]  0x55c379612780             EndNode ,
[  14]  0x55c3794b6628            TextNode "",
[  15]  0x55c37962d848            TextNode "",
[  16]  0x55c379616778            TextNode "",

solenv/gdb/libreoffice/sw.py
@@ -194,7 +194,7 @@ class BigPtrArrayPrinter(object):
             # libstdc++ unique_ptr is a std::tuple which contains multiple
             # _M_head_impl members and gdb may pick the wrong one by default
             # so have to manually cast it to the one that contains the array
-            self.blocks = array['m_ppInf']['_M_t']['_M_t'].cast(gdb.lookup_type("std::_Head_base<0, BlockInfo**, false>"))['_M_head_impl']
+            self.blocks = array['m_ppInf']['_M_t']['_M_t'].cast(gdb.lookup_type("std::_Head_base<0ul, BlockInfo**, false>"))['_M_head_impl']
             self.count = array['m_nSize']
             self.pos = 0
             self.block_count = array['m_nBlock']

commit 2c524ab87e336418c3ee7370f76284a53dff1c82

solenv: gdb: BigPtrArrayPrinter, try both 0 and 0ul
Apparently commit ea858ca92488309789e83666681a358af1d9f6f6 broke it on
Fedora 38, where i now get:
  gdb.error: No type named std::_Head_base<0ul, BlockInfo**, false>.

So try both variants.

Change-Id: If344077df7a6b329286bc1296446562301305946
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/154525
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

solenv/gdb/libreoffice/sw.py
@@ -194,7 +194,12 @@ class BigPtrArrayPrinter(object):
             # libstdc++ unique_ptr is a std::tuple which contains multiple
             # _M_head_impl members and gdb may pick the wrong one by default
             # so have to manually cast it to the one that contains the array
-            self.blocks = array['m_ppInf']['_M_t']['_M_t'].cast(gdb.lookup_type("std::_Head_base<0ul, BlockInfo**, false>"))['_M_head_impl']
+            try:
+                # supposedly works on Debian gdb 13.2
+                self.blocks = array['m_ppInf']['_M_t']['_M_t'].cast(gdb.lookup_type("std::_Head_base<0ul, BlockInfo**, false>"))['_M_head_impl']
+            except gdb.error:
+                # works on Fedora gdb 13.2
+                self.blocks = array['m_ppInf']['_M_t']['_M_t'].cast(gdb.lookup_type("std::_Head_base<0, BlockInfo**, false>"))['_M_head_impl']
             self.count = array['m_nSize']
             self.pos = 0
             self.block_count = array['m_nBlock']