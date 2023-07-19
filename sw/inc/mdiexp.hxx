e9dcbb2a5965e062c84b2e222239e83fdf3f3227
sahil <gautamsahil1947@gmail.com>
2023-07-20 02:48:59 +0530

    tdf#143148 Use pragma once in sw
    Change-Id: Ibb1fc8d8419dd4e1c805134fab3ee91476e5c5ef
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/154662
    Tested-by: Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>
    Reviewed-by: Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>

--- sw/inc/mdiexp.hxx
+++ sw/inc/mdiexp.hxx
@@ -16,8 +16,7 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
-#ifndef INCLUDED_SW_INC_MDIEXP_HXX
-#define INCLUDED_SW_INC_MDIEXP_HXX
+#pragma once
 
 #include <tools/long.hxx>
 #include "tblenum.hxx"
@@ -52,7 +51,5 @@ void RepaintPagePreview( SwViewShell const * pVwSh, const SwRect& rRect );
 TableChgMode GetTableChgDefaultMode();
 
 bool JumpToSwMark( SwViewShell const * pVwSh, std::u16string_view rMark );
-
-#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
