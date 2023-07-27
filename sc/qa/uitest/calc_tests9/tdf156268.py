f58dc86c9c8a257f620b009e243ab591a4a67ced
Hossein <hossein@libreoffice.org>
2023-07-27 02:44:12 +0200

    tdf#156268 Add UITest for Calc ### display crash
    UITest loads a minimal .fods example to check if Calc crashes with that.
    The example file does not crash without UI, as it needs the ###
    indication to be painted on the screen. Calc does not crash with the
    CppUnitTest, tested using testImportCrash() to load the file in a test
    added to sc/qa/unit/subsequent_filters_test4.cxx. Also, PDF export works
    without problem. But, UITest leads to crash without the fix in place.
    
    The UITest can be run using:
    
     make UITest_calc_tests9 UITEST_TEST_NAME=tdf156268.Tdf156268.test_tdf156268
    
    Change-Id: I9712d70f057cfc11f10fd98caadbf6e10ee67d5e
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/154977
    Tested-by: Jenkins
    Reviewed-by: Hossein <hossein@libreoffice.org>

---
+++ sc/qa/uitest/calc_tests9/tdf156268.py
@@ -0,0 +1,24 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+from uitest.framework import UITestCase
+from libreoffice.uno.propertyvalue import mkPropertyValues
+from libreoffice.calc.document import get_cell_by_position
+from uitest.uihelper.common import get_url_for_data_file
+
+class Tdf156268(UITestCase):
+
+   def test_tdf156268(self):
+        # In order to simulate copy&paste from external source,
+        # import the text in Writer, copy it and paste it in Calc
+        with self.ui_test.load_file(get_url_for_data_file("tdf156268.fods")):
+
+            self.xUITest.executeCommand(".uno:SelectAll")
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
