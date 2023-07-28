LibreOffice/core

commit 7ab349296dac79dad3fec09f60348efcbb9ea17e

sw: fix copying and deleting of section content via API
The problem happens if a section starts or ends with a table:

SwXTextSection::getAnchor() may return a SwXTextRange with one position
in a table cell and another position in a different table cell, or
outside the table, neither of which is valid to set the cursor via
SwXTextViewCurosor::gotoRange().

Introduce a new special mode for SwXTextRange, RANGE_IS_SECTION,
analogous to RANGE_IS_TABLE but actually working.

Only SwXTextView can be used to create XTransferables, and it requires
selecting first via either the SwXTextViewCursor or select().

It's currently not possible to select the content of a section in the
ViewShell in these problematic cases, and would be some effort to add.

So add a new interface XTransferableTextSupplier that can be used to
create XTransferable from the SwXTextRange with RANGE_IS_SECTION.

The core CopyRange() and DeleteRange() functions can deal with SwPaMs
that start or end in non-text-nodes, so pass the whole section content to
these in SwXTextRange::setString() and in
SwXTextView::getTransferableFromTextRange().

Change-Id: If7e3210e8a26f5618317c294f2b2f3ed5c217f1c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/106293
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@cib.de>

sw/source/core/unocore/unoobj2.cxx
@@ -650,20 +650,23 @@ public:
     const enum RangePosition m_eRangePosition;
     SwDoc& m_rDoc;
     uno::Reference<text::XText> m_xParentText;
-    const SwFrameFormat* m_pTableFormat;
+    const SwFrameFormat* m_pTableOrSectionFormat;
     const ::sw::mark::IMark* m_pMark;
 
     Impl(SwDoc& rDoc, const enum RangePosition eRange,
-            SwFrameFormat* const pTableFormat,
+            SwFrameFormat* const pTableOrSectionFormat,
             const uno::Reference<text::XText>& xParent = nullptr)
         : m_rPropSet(*aSwMapProvider.GetPropertySet(PROPERTY_MAP_TEXT_CURSOR))
         , m_eRangePosition(eRange)
         , m_rDoc(rDoc)
         , m_xParentText(xParent)
-        , m_pTableFormat(pTableFormat)
+        , m_pTableOrSectionFormat(pTableOrSectionFormat)
         , m_pMark(nullptr)
     {
-        m_pTableFormat && StartListening(pTableFormat->GetNotifier());
+        if (m_pTableOrSectionFormat)
+        {
+            StartListening(pTableOrSectionFormat->GetNotifier());
+        }
     }
 
     virtual ~Impl() override
@@ -679,7 +682,7 @@ public:
             m_rDoc.getIDocumentMarkAccess()->deleteMark(m_pMark);
             m_pMark = nullptr;
         }
-        m_pTableFormat = nullptr;
+        m_pTableOrSectionFormat = nullptr;
         EndListeningAll();
     }
 
@@ -687,7 +690,7 @@ public:
     void SetMark(::sw::mark::IMark& rMark)
     {
         EndListeningAll();
-        m_pTableFormat = nullptr;
+        m_pTableOrSectionFormat = nullptr;
         m_pMark = &rMark;
         StartListening(rMark.GetNotifier());
     }
@@ -701,7 +704,7 @@ void SwXTextRange::Impl::Notify(const SfxHint& rHint)
     if(rHint.GetId() == SfxHintId::Dying)
     {
         EndListeningAll();
-        m_pTableFormat = nullptr;
+        m_pTableOrSectionFormat = nullptr;
         m_pMark = nullptr;
     }
 }
@@ -714,7 +717,7 @@ SwXTextRange::SwXTextRange(SwPaM const & rPam,
     SetPositions(rPam);
 }
 
-SwXTextRange::SwXTextRange(SwFrameFormat& rTableFormat)
+SwXTextRange::SwXTextRange(SwTableFormat& rTableFormat)
     : m_pImpl(
         new SwXTextRange::Impl(*rTableFormat.GetDoc(), RANGE_IS_TABLE, &rTableFormat) )
 {
@@ -724,6 +727,13 @@ SwXTextRange::SwXTextRange(SwFrameFormat& rTableFormat)
     SwPaM aPam( aPosition );
 
     SetPositions( aPam );
+}
+
+SwXTextRange::SwXTextRange(SwSectionFormat& rSectionFormat)
+    : m_pImpl(
+        new SwXTextRange::Impl(*rSectionFormat.GetDoc(), RANGE_IS_SECTION, &rSectionFormat) )
+{
+    // no SetPositions here for now
 }
 
 SwXTextRange::~SwXTextRange()
@@ -753,6 +763,18 @@ void SwXTextRange::SetPositions(const SwPaM& rPam)
     m_pImpl->SetMark(*pMark);
 }
 
+static void DeleteTable(SwDoc & rDoc, SwTable& rTable)
+{
+    SwSelBoxes aSelBoxes;
+    for (auto& rBox : rTable.GetTabSortBoxes())
+    {
+        aSelBoxes.insert(rBox);
+    }
+    // note: if the table is the content in the section, this will create
+    // a new text node - that's desirable here
+    rDoc.DeleteRowCol(aSelBoxes);
+}
+
 void SwXTextRange::DeleteAndInsert(
         const OUString& rText, const bool bForceExpandHints)
 {
@@ -764,11 +786,80 @@ void SwXTextRange::DeleteAndInsert(
 
     const SwPosition aPos(GetDoc().GetNodes().GetEndOfContent());
     SwCursor aCursor(aPos, nullptr);
-    if (!GetPositions(aCursor))
-        return;
 
     UnoActionContext aAction(& m_pImpl->m_rDoc);
-    m_pImpl->m_rDoc.GetIDocumentUndoRedo().StartUndo(SwUndoId::INSERT, nullptr);
+
+    if (RANGE_IS_SECTION == m_pImpl->m_eRangePosition)
+    {
+        if (!m_pImpl->m_pTableOrSectionFormat)
+        {
+            throw uno::RuntimeException("disposed?");
+        }
+        m_pImpl->m_rDoc.GetIDocumentUndoRedo().StartUndo(SwUndoId::INSERT, nullptr);
+        SwSectionFormat const& rFormat(
+            *static_cast<SwSectionFormat const*>(m_pImpl->m_pTableOrSectionFormat));
+        SwNodeIndex const start(*rFormat.GetSectionNode());
+        SwNodeIndex const end(*start.GetNode().EndOfSectionNode());
+
+        // delete tables at start
+        for (SwNodeIndex i = start; i < end; )
+        {
+            SwNode & rNode(i.GetNode());
+            if (rNode.IsSectionNode())
+            {
+                ++i;
+                continue;
+            }
+            else if (SwTableNode *const pTableNode = rNode.GetTableNode())
+            {
+                DeleteTable(m_pImpl->m_rDoc, pTableNode->GetTable());
+                // where does that leave index? presumably behind?
+            }
+            else
+            {
+                assert(rNode.IsTextNode());
+                break;
+            }
+        }
+        // delete tables at end
+        for (SwNodeIndex i = end; start <= i; )
+        {
+            --i;
+            SwNode & rNode(i.GetNode());
+            if (rNode.IsEndNode())
+            {
+                if (SwTableNode *const pTableNode = rNode.StartOfSectionNode()->GetTableNode())
+                {
+                    DeleteTable(m_pImpl->m_rDoc, pTableNode->GetTable());
+                }
+                else
+                {
+                    assert(rNode.StartOfSectionNode()->IsSectionNode());
+                    continue;
+                }
+            }
+            else
+            {
+                assert(rNode.IsTextNode());
+                break;
+            }
+        }
+        // now there should be a text node at the start and end of it!
+        *aCursor.GetPoint() = SwPosition(start);
+        aCursor.Move( fnMoveForward, GoInContent );
+        assert(aCursor.GetPoint()->nNode <= end);
+        aCursor.SetMark();
+        *aCursor.GetPoint() = SwPosition(end);
+        aCursor.Move( fnMoveBackward, GoInContent );
+        assert(start <= aCursor.GetPoint()->nNode);
+    }
+    else
+    {
+        if (!GetPositions(aCursor))
+            return;
+        m_pImpl->m_rDoc.GetIDocumentUndoRedo().StartUndo(SwUndoId::INSERT, nullptr);
+    }
+
     if (aCursor.HasMark())
     {
         m_pImpl->m_rDoc.getIDocumentContentOperations().DeleteAndJoin(aCursor);
@@ -836,9 +927,9 @@ SwXTextRange::getText()
     if (!m_pImpl->m_xParentText.is())
     {
         if (m_pImpl->m_eRangePosition == RANGE_IS_TABLE &&
-            m_pImpl->m_pTableFormat)
+            m_pImpl->m_pTableOrSectionFormat)
         {
-            SwTable const*const pTable = SwTable::FindTable( m_pImpl->m_pTableFormat );
+            SwTable const*const pTable = SwTable::FindTable( m_pImpl->m_pTableOrSectionFormat );
             SwTableNode const*const pTableNode = pTable->GetTableNode();
             const SwPosition aPosition( *pTableNode );
             m_pImpl->m_xParentText =
@@ -870,6 +961,15 @@ SwXTextRange::getStart()
         // start and end are this, if it's a table
         xRet = this;
     }
+    else if (RANGE_IS_SECTION == m_pImpl->m_eRangePosition
+            && m_pImpl->m_pTableOrSectionFormat)
+    {
+        auto const pSectFormat(static_cast<SwSectionFormat const*>(m_pImpl->m_pTableOrSectionFormat));
+        SwPaM aPaM(*pSectFormat->GetContent().GetContentIdx());
+        aPaM.Move( fnMoveForward, GoInContent );
+        assert(aPaM.GetPoint()->nNode < *pSectFormat->GetContent().GetContentIdx()->GetNode().EndOfSectionNode());
+        xRet = new SwXTextRange(aPaM, m_pImpl->m_xParentText);
+    }
     else
     {
         throw uno::RuntimeException("disposed?");
@@ -898,6 +998,15 @@ SwXTextRange::getEnd()
         // start and end are this, if it's a table
         xRet = this;
     }
+    else if (RANGE_IS_SECTION == m_pImpl->m_eRangePosition
+            && m_pImpl->m_pTableOrSectionFormat)
+    {
+        auto const pSectFormat(static_cast<SwSectionFormat const*>(m_pImpl->m_pTableOrSectionFormat));
+        SwPaM aPaM(*pSectFormat->GetContent().GetContentIdx()->GetNode().EndOfSectionNode());
+        aPaM.Move( fnMoveBackward, GoInContent );
+        assert(*pSectFormat->GetContent().GetContentIdx() < aPaM.GetPoint()->nNode);
+        xRet = new SwXTextRange(aPaM, m_pImpl->m_xParentText);
+    }
     else
     {
         throw uno::RuntimeException("disposed?");
@@ -913,7 +1022,7 @@ OUString SAL_CALL SwXTextRange::getString()
     // for tables there is no bookmark, thus also no text
     // one could export the table as ASCII here maybe?
     SwPaM aPaM(GetDoc().GetNodes());
-    if (GetPositions(aPaM) && aPaM.HasMark())
+    if (GetPositions(aPaM, sw::TextRangeMode::AllowNonTextNode) && aPaM.HasMark())
     {
         SwUnoCursorHelper::GetTextFromPam(aPaM, sRet);
     }
@@ -927,8 +1036,27 @@ void SAL_CALL SwXTextRange::setString(const OUString& rString)
     DeleteAndInsert(rString, false);
 }
 
-bool SwXTextRange::GetPositions(SwPaM& rToFill) const
+bool SwXTextRange::GetPositions(SwPaM& rToFill, ::sw::TextRangeMode const eMode) const
 {
+    if (RANGE_IS_SECTION == m_pImpl->m_eRangePosition
+        && eMode == ::sw::TextRangeMode::AllowNonTextNode)
+    {
+        if (auto const pSectFormat = static_cast<SwSectionFormat const*>(m_pImpl->m_pTableOrSectionFormat))
+        {
+            SwNodeIndex const*const pSectionNode(pSectFormat->GetContent().GetContentIdx());
+            assert(pSectionNode);
+            assert(pSectionNode->GetNodes().IsDocNodes());
+            rToFill.GetPoint()->nNode = *pSectionNode;
+            ++rToFill.GetPoint()->nNode;
+            rToFill.GetPoint()->nContent.Assign(rToFill.GetPoint()->nNode.GetNode().GetContentNode(), 0);
+            rToFill.SetMark();
+            rToFill.GetMark()->nNode = *pSectionNode->GetNode().EndOfSectionNode();
+            --rToFill.GetMark()->nNode;
+            rToFill.GetMark()->nContent.Assign(rToFill.GetMark()->nNode.GetNode().GetContentNode(),
+                rToFill.GetMark()->nNode.GetNode().GetContentNode() ? rToFill.GetMark()->nNode.GetNode().GetContentNode()->Len() : 0);
+            return true;
+        }
+    }
     ::sw::mark::IMark const * const pBkmk = m_pImpl->GetBookmark();
     if(pBkmk)
     {
@@ -950,7 +1078,8 @@ bool SwXTextRange::GetPositions(SwPaM& rToFill) const
 namespace sw {
 
 bool XTextRangeToSwPaM( SwUnoInternalPaM & rToFill,
-        const uno::Reference< text::XTextRange > & xTextRange)
+        const uno::Reference<text::XTextRange> & xTextRange,
+        ::sw::TextRangeMode const eMode)
 {
     bool bRet = false;
 
@@ -984,7 +1113,7 @@ bool XTextRangeToSwPaM( SwUnoInternalPaM & rToFill,
     }
     if(pRange && &pRange->GetDoc() == &rToFill.GetDoc())
     {
-        bRet = pRange->GetPositions(rToFill);
+        bRet = pRange->GetPositions(rToFill, eMode);
     }
     else
     {

offapi/UnoApi_offapi.mk
@@ -2132,6 +2132,7 @@ $(eval $(call gb_UnoApi_add_idlfiles,offapi,com/sun/star/datatransfer,\
 	XTransferableEx \
 	XTransferableSource \
 	XTransferableSupplier \
+	XTransferableTextSupplier \
 ))
 $(eval $(call gb_UnoApi_add_idlfiles,offapi,com/sun/star/datatransfer/clipboard,\
 	ClipboardEvent \

offapi/com/sun/star/datatransfer/XTransferableTextSupplier.idl
@@ -1,0 +1,44 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#ifndef __com_sun_star_datatransfer_XTransferableTextSupplier_idl__
+#define __com_sun_star_datatransfer_XTransferableTextSupplier_idl__
+
+#include <com/sun/star/datatransfer/XTransferable.idl>
+#include <com/sun/star/text/XTextRange.idl>
+
+
+module com { module sun { module star { module datatransfer {
+
+/** @since LO 7.2
+ */
+interface XTransferableTextSupplier
+{
+    /** Provide access to a transferable representation of a given text
+        range.
+
+        @param xRange   a text range known to the supplier.
+
+        @returns
+        a transferable object representing the given text range.
+
+        @see com::sun::star::datatransfer::XTransferable
+    */
+    XTransferable getTransferableForTextRange(
+        [in] com::sun::star::text::XTextRange xRange);
+
+};
+
+
+}; }; }; };
+
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sw/inc/unotextrange.hxx
@@ -55,10 +55,16 @@ public:
 
 namespace sw {
 
+    enum class TextRangeMode {
+        RequireTextNode,
+        AllowNonTextNode
+    };
+
     void DeepCopyPaM(SwPaM const & rSource, SwPaM & rTarget);
 
     SW_DLLPUBLIC bool XTextRangeToSwPaM(SwUnoInternalPaM& rToFill,
-            const css::uno::Reference< css::text::XTextRange > & xTextRange);
+            const css::uno::Reference<css::text::XTextRange> & xTextRange,
+            TextRangeMode eMode = TextRangeMode::RequireTextNode);
 
     css::uno::Reference< css::text::XText >
         CreateParentXText(SwDoc & rDoc, const SwPosition& rPos);
@@ -95,6 +101,7 @@ private:
         RANGE_IN_TEXT,  // "ordinary" css::text::TextRange
         RANGE_IN_CELL,  // position created with a cell that has no uno object
         RANGE_IS_TABLE, // anchor of a table
+        RANGE_IS_SECTION, // anchor of a section
     };
 
     void    SetPositions(SwPaM const& rPam);
@@ -112,11 +119,14 @@ public:
             const css::uno::Reference< css::text::XText > & xParent,
             const enum RangePosition eRange = RANGE_IN_TEXT);
     // only for RANGE_IS_TABLE
-    SwXTextRange(SwFrameFormat& rTableFormat);
+    SwXTextRange(SwTableFormat& rTableFormat);
+    // only for RANGE_IS_SECTION
+    SwXTextRange(SwSectionFormat& rSectionFormat);
 
     const SwDoc& GetDoc() const;
           SwDoc& GetDoc();
-    bool GetPositions(SwPaM & rToFill) const;
+    bool GetPositions(SwPaM & rToFill,
+        ::sw::TextRangeMode eMode = ::sw::TextRangeMode::RequireTextNode) const;
 
     static css::uno::Reference< css::text::XTextRange > CreateXTextRange(
             SwDoc & rDoc,

sw/qa/extras/uiwriter/uiwriter.cxx
@@ -1374,7 +1374,8 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest, testTdf134250)
     CPPUNIT_ASSERT_EQUAL(sal_Int32(1), xSections->getCount());
 
     uno::Reference<text::XTextContent> xTextContent(xSections->getByIndex(0), uno::UNO_QUERY);
-    CPPUNIT_ASSERT(xTextContent->getAnchor()->getString().endsWith("bar"));
+    CPPUNIT_ASSERT_EQUAL(OUString("foo" SAL_NEWLINE_STRING "bar"),
+            xTextContent->getAnchor()->getString());
 
     // select all with table at start -> 3 times
     dispatchCommand(mxComponent, ".uno:SelectAll", {});
@@ -1403,7 +1404,8 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest, testTdf134250)
     CPPUNIT_ASSERT_EQUAL(sal_Int32(1), xSections->getCount());
 
     // Without the fix in place, section's content would have been gone after undo
-    CPPUNIT_ASSERT(xTextContent->getAnchor()->getString().endsWith("bar"));
+    CPPUNIT_ASSERT_EQUAL(OUString("foo" SAL_NEWLINE_STRING "bar"),
+            xTextContent->getAnchor()->getString());
 
     dispatchCommand(mxComponent, ".uno:Redo", {});
     Scheduler::ProcessEventsToIdle();
@@ -1416,7 +1418,8 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest, testTdf134250)
 
     CPPUNIT_ASSERT_EQUAL(sal_Int32(1), xTables->getCount());
     CPPUNIT_ASSERT_EQUAL(sal_Int32(1), xSections->getCount());
-    CPPUNIT_ASSERT(xTextContent->getAnchor()->getString().endsWith("bar"));
+    CPPUNIT_ASSERT_EQUAL(OUString("foo" SAL_NEWLINE_STRING "bar"),
+            xTextContent->getAnchor()->getString());
 
     dispatchCommand(mxComponent, ".uno:Redo", {});
     Scheduler::ProcessEventsToIdle();

sw/qa/extras/unowriter/data/tdf134250.fodt
@@ -1,0 +1,86 @@
+<?xml version="1.0" encoding="UTF-8"?>
+
+<office:document xmlns:officeooo="http://openoffice.org/2009/office" xmlns:css3t="http://www.w3.org/TR/css3-text/" xmlns:grddl="http://www.w3.org/2003/g/data-view#" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:formx="urn:openoffice:names:experimental:ooxml-odf-interop:xmlns:form:1.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:chart="urn:oasis:names:tc:opendocument:xmlns:chart:1.0" xmlns:svg="urn:oasis:names:tc:opendocument:xmlns:svg-compatible:1.0" xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0" xmlns:oooc="http://openoffice.org/2004/calc" xmlns:style="urn:oasis:names:tc:opendocument:xmlns:style:1.0" xmlns:ooow="http://openoffice.org/2004/writer" xmlns:meta="urn:oasis:names:tc:opendocument:xmlns:meta:1.0" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:rpt="http://openoffice.org/2005/report" xmlns:draw="urn:oasis:names:tc:opendocument:xmlns:drawing:1.0" xmlns:config="urn:oasis:names:tc:opendocument:xmlns:config:1.0" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:fo="urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0" xmlns:ooo="http://openoffice.org/2004/office" xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0" xmlns:dr3d="urn:oasis:names:tc:opendocument:xmlns:dr3d:1.0" xmlns:table="urn:oasis:names:tc:opendocument:xmlns:table:1.0" xmlns:number="urn:oasis:names:tc:opendocument:xmlns:datastyle:1.0" xmlns:of="urn:oasis:names:tc:opendocument:xmlns:of:1.2" xmlns:calcext="urn:org:documentfoundation:names:experimental:calc:xmlns:calcext:1.0" xmlns:tableooo="http://openoffice.org/2009/table" xmlns:drawooo="http://openoffice.org/2010/draw" xmlns:loext="urn:org:documentfoundation:names:experimental:office:xmlns:loext:1.0" xmlns:dom="http://www.w3.org/2001/xml-events" xmlns:field="urn:openoffice:names:experimental:ooo-ms-interop:xmlns:field:1.0" xmlns:math="http://www.w3.org/1998/Math/MathML" xmlns:form="urn:oasis:names:tc:opendocument:xmlns:form:1.0" xmlns:script="urn:oasis:names:tc:opendocument:xmlns:script:1.0" xmlns:xforms="http://www.w3.org/2002/xforms" office:version="1.2" office:mimetype="application/vnd.oasis.opendocument.text">
+ <office:font-face-decls>
+  <style:font-face style:name="Liberation Serif" svg:font-family="&apos;Liberation Serif&apos;" style:font-family-generic="roman" style:font-pitch="variable"/>
+  <style:font-face style:name="Lohit Devanagari" svg:font-family="&apos;Lohit Devanagari&apos;" style:font-family-generic="system" style:font-pitch="variable"/>
+  <style:font-face style:name="Source Han Serif CN" svg:font-family="&apos;Source Han Serif CN&apos;" style:font-family-generic="system" style:font-pitch="variable"/>
+ </office:font-face-decls>
+ <office:styles>
+  <style:default-style style:family="graphic">
+   <style:graphic-properties svg:stroke-color="#3465a4" draw:fill-color="#729fcf" fo:wrap-option="no-wrap" draw:shadow-offset-x="0.3cm" draw:shadow-offset-y="0.3cm" draw:start-line-spacing-horizontal="0.283cm" draw:start-line-spacing-vertical="0.283cm" draw:end-line-spacing-horizontal="0.283cm" draw:end-line-spacing-vertical="0.283cm" style:flow-with-text="false"/>
+   <style:paragraph-properties style:text-autospace="ideograph-alpha" style:line-break="strict" style:writing-mode="lr-tb" style:font-independent-line-spacing="false">
+    <style:tab-stops/>
+   </style:paragraph-properties>
+   <style:text-properties style:use-window-font-color="true" loext:opacity="0%" style:font-name="Liberation Serif" fo:font-size="12pt" fo:language="de" fo:country="DE" style:letter-kerning="true" style:font-name-asian="Source Han Serif CN" style:font-size-asian="10.5pt" style:language-asian="zh" style:country-asian="CN" style:font-name-complex="Lohit Devanagari" style:font-size-complex="12pt" style:language-complex="hi" style:country-complex="IN"/>
+  </style:default-style>
+  <style:default-style style:family="paragraph">
+   <style:paragraph-properties fo:orphans="2" fo:widows="2" fo:hyphenation-ladder-count="no-limit" style:text-autospace="ideograph-alpha" style:punctuation-wrap="hanging" style:line-break="strict" style:tab-stop-distance="1.251cm" style:writing-mode="page"/>
+   <style:text-properties style:use-window-font-color="true" loext:opacity="0%" style:font-name="Liberation Serif" fo:font-size="12pt" fo:language="de" fo:country="DE" style:letter-kerning="true" style:font-name-asian="Source Han Serif CN" style:font-size-asian="10.5pt" style:language-asian="zh" style:country-asian="CN" style:font-name-complex="Lohit Devanagari" style:font-size-complex="12pt" style:language-complex="hi" style:country-complex="IN" fo:hyphenate="false" fo:hyphenation-remain-char-count="2" fo:hyphenation-push-char-count="2" loext:hyphenation-no-caps="false"/>
+  </style:default-style>
+  <style:default-style style:family="table">
+   <style:table-properties table:border-model="collapsing"/>
+  </style:default-style>
+  <style:default-style style:family="table-row">
+   <style:table-row-properties fo:keep-together="auto"/>
+  </style:default-style>
+  <style:style style:name="Standard" style:family="paragraph" style:class="text"/>
+  <style:style style:name="Table_20_Contents" style:display-name="Table Contents" style:family="paragraph" style:parent-style-name="Standard" style:class="extra">
+   <style:paragraph-properties fo:orphans="0" fo:widows="0" text:number-lines="false" text:line-number="0"/>
+  </style:style>
+
+  <text:notes-configuration text:note-class="footnote" style:num-format="1" text:start-value="0" text:footnotes-position="page" text:start-numbering-at="document"/>
+  <text:notes-configuration text:note-class="endnote" style:num-format="i" text:start-value="0"/>
+  <text:linenumbering-configuration text:number-lines="false" text:offset="0.499cm" style:num-format="1" text:number-position="left" text:increment="5"/>
+ </office:styles>
+ <office:automatic-styles>
+  <style:style style:name="Table1" style:family="table">
+   <style:table-properties style:width="17cm" table:align="margins"/>
+  </style:style>
+  <style:style style:name="Table1.A" style:family="table-column">
+   <style:table-column-properties style:column-width="17cm" style:rel-column-width="65535*"/>
+  </style:style>
+  <style:style style:name="Table1.A1" style:family="table-cell">
+   <style:table-cell-properties fo:padding="0.097cm" fo:border="0.05pt solid #000000"/>
+  </style:style>
+  <style:style style:name="P1" style:family="paragraph" style:parent-style-name="Standard">
+   <style:text-properties officeooo:rsid="001fe5a9" officeooo:paragraph-rsid="001fe5a9"/>
+  </style:style>
+  <style:style style:name="P2" style:family="paragraph" style:parent-style-name="Table_20_Contents">
+   <style:text-properties officeooo:rsid="001fe5a9" officeooo:paragraph-rsid="001fe5a9"/>
+  </style:style>
+  <style:style style:name="T1" style:family="text">
+   <style:text-properties fo:font-weight="bold" style:font-weight-asian="bold" style:font-weight-complex="bold"/>
+  </style:style>
+  <style:style style:name="Sect1" style:family="section">
+   <style:section-properties style:editable="false">
+    <style:columns fo:column-count="1" fo:column-gap="0cm"/>
+   </style:section-properties>
+  </style:style>
+  <style:page-layout style:name="pm1">
+   <style:page-layout-properties fo:page-width="21.001cm" fo:page-height="29.7cm" style:num-format="1" style:print-orientation="portrait" fo:margin-top="2cm" fo:margin-bottom="2cm" fo:margin-left="2cm" fo:margin-right="2cm" style:writing-mode="lr-tb" style:footnote-max-height="0cm">
+    <style:footnote-sep style:width="0.018cm" style:distance-before-sep="0.101cm" style:distance-after-sep="0.101cm" style:line-style="solid" style:adjustment="left" style:rel-width="25%" style:color="#000000"/>
+   </style:page-layout-properties>
+   <style:header-style/>
+   <style:footer-style/>
+  </style:page-layout>
+ </office:automatic-styles>
+ <office:master-styles>
+  <style:master-page style:name="Standard" style:page-layout-name="pm1"/>
+ </office:master-styles>
+ <office:body>
+  <office:text>
+   <text:section text:style-name="Sect1" text:name="Section1">
+    <table:table table:name="Table1" table:style-name="Table1">
+     <table:table-column table:style-name="Table1.A"/>
+     <table:table-row>
+      <table:table-cell table:style-name="Table1.A1" office:value-type="string">
+       <text:p text:style-name="P2">foo</text:p>
+      </table:table-cell>
+     </table:table-row>
+    </table:table>
+    <text:p text:style-name="P1">b<text:span text:style-name="T1">a</text:span><text:bookmark text:name="Bookmark 1"/>r</text:p>
+   </text:section>
+  </office:text>
+ </office:body>
+</office:document>

commit 0f39ecfed57f3f32d4f5cadf50b7dd9de77f8d7e

tdf#149555 sw: convert RANGE_IS_SECTION SwXTextRange if possible
The writerfilter import got an exception in
SectionPropertyMap::CloseSectionGroup() because XTextRangeToSwPaM()
can't handle RANGE_IS_SECTION SwXTextRange.

(regression from commit 7ab349296dac79dad3fec09f60348efcbb9ea17e)

Change-Id: I38b37fdec64f4699c0fa1a090c72a8676ff386a6
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/155026
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/core/unocore/unoobj2.cxx
@@ -1049,20 +1049,40 @@ void SAL_CALL SwXTextRange::setString(const OUString& rString)
 
 bool SwXTextRange::GetPositions(SwPaM& rToFill, ::sw::TextRangeMode const eMode) const
 {
-    if (RANGE_IS_SECTION == m_pImpl->m_eRangePosition
-        && eMode == ::sw::TextRangeMode::AllowNonTextNode)
+    if (RANGE_IS_SECTION == m_pImpl->m_eRangePosition)
     {
         if (auto const pSectFormat = static_cast<SwSectionFormat const*>(m_pImpl->m_pTableOrSectionFormat))
         {
-            SwNodeIndex const*const pSectionNode(pSectFormat->GetContent().GetContentIdx());
-            assert(pSectionNode);
-            assert(pSectionNode->GetNodes().IsDocNodes());
-            rToFill.GetPoint()->Assign( pSectionNode->GetNode(), SwNodeOffset(1) );
-            rToFill.SetMark();
-            rToFill.GetMark()->Assign( *pSectionNode->GetNode().EndOfSectionNode(), SwNodeOffset(-1) );
-            if (const SwContentNode* pCNd = rToFill.GetMark()->GetContentNode())
-                rToFill.GetMark()->AssignEndIndex(*pCNd);
-            return true;
+            if (eMode == ::sw::TextRangeMode::AllowNonTextNode)
+            {
+                SwNodeIndex const*const pSectionNode(pSectFormat->GetContent().GetContentIdx());
+                assert(pSectionNode);
+                assert(pSectionNode->GetNodes().IsDocNodes());
+                rToFill.GetPoint()->Assign( pSectionNode->GetNode(), SwNodeOffset(1) );
+                rToFill.SetMark();
+                rToFill.GetMark()->Assign( *pSectionNode->GetNode().EndOfSectionNode(), SwNodeOffset(-1) );
+                if (const SwContentNode* pCNd = rToFill.GetMark()->GetContentNode())
+                    rToFill.GetMark()->AssignEndIndex(*pCNd);
+                return true;
+            }
+            else
+            {
+                SwPaM aPaM(*pSectFormat->GetContent().GetContentIdx());
+                aPaM.Move(fnMoveForward, GoInContent);
+                assert(aPaM.GetPoint()->GetNode() < *pSectFormat->GetContent().GetContentIdx()->GetNode().EndOfSectionNode());
+                aPaM.SetMark();
+                *aPaM.GetPoint() = SwPosition(*pSectFormat->GetContent().GetContentIdx()->GetNode().EndOfSectionNode());
+                aPaM.Move(fnMoveBackward, GoInContent);
+                assert(*pSectFormat->GetContent().GetContentIdx() < aPaM.GetPoint()->GetNode());
+                // tdf#149555 if there is no table involved, only nested
+                // sections, then PaM is valid
+                if (aPaM.GetPoint()->GetNode().FindTableNode()
+                    == aPaM.GetMark()->GetNode().FindTableNode())
+                {
+                    rToFill = aPaM;
+                    return true;
+                }
+            }
         }
     }
     ::sw::mark::IMark const * const pBkmk = m_pImpl->GetBookmark();