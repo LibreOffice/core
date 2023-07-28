LibreOffice/core

commit 462f9d1f589a7afd66d3fc61925467d3b68e5b31

tdf#142600 - List tables in order of their appearance
Change-Id: I1e653bf3a646161825b465ea2fc20ab48308f0ca
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/116767
Tested-by: Jenkins
Reviewed-by: Eike Rathke <erack@redhat.com>

sc/source/ui/miscdlgs/linkarea.cxx
@@ -250,11 +250,15 @@ void ScLinkedAreaDlg::UpdateSourceRanges()
             m_xLbRanges->append_text("CSV_all");
         }
 
-        ScAreaNameIterator aIter(m_pSourceShell->GetDocument());
-        ScRange aDummy;
-        OUString aName;
-        while ( aIter.Next( aName, aDummy ) )
-            m_xLbRanges->append_text(aName);
+        // tdf#142600 - list tables in order of their appearance in the document's source
+        const ScRangeName* pRangeName = m_pSourceShell->GetDocument().GetRangeName();
+        for (size_t i = 1; i <= pRangeName->index_size(); i++)
+        {
+            if (const ScRangeData* pRangeData = pRangeName->findByIndex(i))
+            {
+                m_xLbRanges->append_text(pRangeData->GetName());
+            }
+        }
     }
 
     m_xLbRanges->thaw();

sc/inc/rangenam.hxx
@@ -239,6 +239,7 @@ public:
     SC_DLLPUBLIC iterator begin();
     SC_DLLPUBLIC iterator end();
     SC_DLLPUBLIC size_t size() const;
+    SC_DLLPUBLIC size_t index_size() const;
     bool empty() const;
 
     /** Insert object into set.

sc/source/core/tool/rangenam.cxx
@@ -801,6 +801,11 @@ size_t ScRangeName::size() const
     return m_Data.size();
 }
 
+size_t ScRangeName::index_size() const
+{
+    return maIndexToData.size();
+}
+
 bool ScRangeName::empty() const
 {
     return m_Data.empty();

commit 2f2cd7558e1d01f9670436412d62d03f28bab2c1

tdf#142600 - External links dialog: list database ranges
Regression from commit 462f9d1f589a7afd66d3fc61925467d3b68e5b31 where
the database ranges were dropped.

Change-Id: Ic0e453fc6fe62849d72b6acf88ed7ebd3c47775b
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/155009
Reviewed-by: Eike Rathke <erack@redhat.com>
Tested-by: Jenkins

sc/source/ui/miscdlgs/linkarea.cxx
@@ -29,6 +29,7 @@
 #include <svtools/sfxecode.hxx>
 #include <o3tl/string_view.hxx>
 
+#include <dbdata.hxx>
 #include <linkarea.hxx>
 #include <docsh.hxx>
 #include <tablink.hxx>
@@ -258,6 +259,13 @@ void ScLinkedAreaDlg::UpdateSourceRanges()
             {
                 m_xLbRanges->append_text(pRangeData->GetName());
             }
+        }
+        // tdf#142600 - list database ranges
+        if (const auto pDBs = m_pSourceShell->GetDocument().GetDBCollection())
+        {
+            const auto& rNamedDBs = pDBs->getNamedDBs();
+            for (const auto& rNamedDB : rNamedDBs)
+                m_xLbRanges->append_text(rNamedDB->GetName());
         }
     }
 