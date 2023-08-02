LibreOffice/core

commit af34f4ea62d8aabbab5d4028034aa2482c16fe8a

avoid skia assert and slow-path
Otherwise, several lines below, we might call
    mpAlphaVDev->GetBitmap
which will return no bitmap (because the resulting bitmap would be
empty).
And then either we will hit the assert a few lines lower, or we will go
into the very very slow path below.

Change-Id: I41a62cddef5e0a6822ca09ed954e3fe431ca0021
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/154097
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/source/outdev/bitmap.cxx
@@ -315,6 +315,8 @@ void OutputDevice::DrawDeviceAlphaBitmap( const Bitmap& rBmp, const AlphaMask& r
 
     if (aDstRect.Intersection(tools::Rectangle(aOutPt, aOutSz)).IsEmpty())
         return;
+    if (ImplLogicToDevicePixel(aOutSz).IsEmpty()) // nothing to draw
+        return;
 
     {
         Point aRelPt = aOutPt + Point(mnOutOffX, mnOutOffY);

commit 37421677cea4969ffecdeb2a58488bae22f81782

tdf#156514 Check spelling red underlines are gone
Regression from
    commit af34f4ea62d8aabbab5d4028034aa2482c16fe8a
    Author: Noel Grandin <noel.grandin@collabora.co.uk>
    Date:   Thu Jul 6 10:52:07 2023 +0200
    avoid skia assert and slow-path

Change-Id: Ib81c7d341a88e72fb0a48a39703485261ac6679c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/155217
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/source/outdev/bitmap.cxx
@@ -314,8 +314,6 @@ void OutputDevice::DrawDeviceAlphaBitmap( const Bitmap& rBmp, const AlphaMask& r
 
     if (aDstRect.Intersection(tools::Rectangle(aOutPt, aOutSz)).IsEmpty())
         return;
-    if (ImplLogicToDevicePixel(aOutSz).IsEmpty()) // nothing to draw
-        return;
 
     {
         Point aRelPt = aOutPt + Point(mnOutOffX, mnOutOffY);
@@ -341,6 +339,8 @@ void OutputDevice::DrawDeviceAlphaBitmap( const Bitmap& rBmp, const AlphaMask& r
         // try to blend the alpha bitmap with the alpha virtual device
         if (mpAlphaVDev)
         {
+            if (ImplLogicToDevicePixel(aOutSz).IsEmpty()) // nothing to draw
+                return;
             Bitmap aAlphaBitmap( mpAlphaVDev->GetBitmap( aRelPt, aOutSz ) );
             if (SalBitmap* pSalAlphaBmp2 = aAlphaBitmap.ImplGetSalBitmap().get())
             {