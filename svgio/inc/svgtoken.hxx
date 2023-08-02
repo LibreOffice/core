LibreOffice/core

commit 13a41e7a12598c7896d6dc8d34aba6af5b80b83c

tdf#150124: do nothing when parent is of unkown type
Change-Id: I745b2a81200b0d8138b5f1b844849f20571d9546
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/153878
Tested-by: Jenkins
Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

svgio/source/svgreader/svgdocumenthandler.cxx
@@ -413,8 +413,7 @@ namespace
 
                 default:
                 {
-                    /// invalid token, ignore
-                    SAL_INFO( "svgio", "Unknown Base SvgToken <" + aName + "> (!)" );
+                    mpTarget = new SvgNode(SVGToken::Unknown, maDocument, mpTarget);
                     break;
                 }
             }
@@ -498,6 +497,8 @@ namespace
                 /// structural element pattern
                 case SVGToken::Pattern:
 
+                default:
+
                 /// content handling after parsing
                 {
                     if(mpTarget)
@@ -515,10 +516,6 @@ namespace
                         OSL_ENSURE(false, "Closing token, but no context (!)");
                     }
                     break;
-                }
-                default:
-                {
-                    /// invalid token, ignore
                 }
             }
 

svgio/qa/cppunit/SvgImportTest.cxx
@@ -135,6 +135,20 @@ CPPUNIT_TEST_FIXTURE(Test, testSymbol)
     assertXPath(pDocument, "/primitive2D/transform/polypolygoncolor", "color", "#00d000");
 }
 
+CPPUNIT_TEST_FIXTURE(Test, testTdf150124)
+{
+    Primitive2DSequence aSequence = parseSvg(u"/svgio/qa/cppunit/data/tdf150124.svg");
+    CPPUNIT_ASSERT_EQUAL(1, static_cast<int>(aSequence.getLength()));
+
+    drawinglayer::Primitive2dXmlDump dumper;
+    xmlDocUniquePtr pDocument = dumper.dumpAndParse(aSequence);
+
+    CPPUNIT_ASSERT (pDocument);
+
+    assertXPathChildren(pDocument, "/primitive2D", 1);
+    assertXPath(pDocument, "/primitive2D/hiddengeometry", 1);
+}
+
 CPPUNIT_TEST_FIXTURE(Test, testTdf155819)
 {
     Primitive2DSequence aSequence = parseSvg(u"/svgio/qa/cppunit/data/tdf155819.svg");

svgio/qa/cppunit/data/tdf150124.svg
@@ -1,0 +1,12 @@
+<svg id="svg-root" width="100%" height="100%"
+  viewBox="0 0 480 360" xmlns="http://www.w3.org/2000/svg"
+  xmlns:xlink="http://www.w3.org/1999/xlink">
+  <invented>
+  <rect id='r1' x='0' y='0' width='50' height='50' fill='red'/>
+  <g>
+    <rect id='r2' x='60' y='0' width='50' height='50' fill='green'/>
+  </g>
+  <circle id="myCircle" cx="50" cy="50" r="40" stroke="blue" />
+  </invented>
+  <use href="myCircle" x="20" fill="white" stroke="red" />
+</svg>

svgio/source/svgreader/svgnode.cxx
@@ -395,20 +395,10 @@ namespace {
             mbDecomposing(false),
             mbCssStyleVectorBuilt(false)
         {
-            OSL_ENSURE(SVGToken::Unknown != maType, "SvgNode with unknown type created (!)");
-
-            if(pParent)
+            // tdf#150124 ignore when parent is unknown
+            if(pParent && pParent->getType() != SVGToken::Unknown)
             {
                 pParent->maChildren.emplace_back(this);
-            }
-            else
-            {
-#ifdef DBG_UTIL
-                if(SVGToken::Svg != getType())
-                {
-                    OSL_ENSURE(false, "No parent for this node (!)");
-                }
-#endif
             }
         }
 

commit 76f50549f4898313ef4a18ce32d1f0a625ce766d

tdf#156579: treat switch element as a group
Regression from

commit 13a41e7a12598c7896d6dc8d34aba6af5b80b83c
Author: Xisco Fauli <xiscofauli@libreoffice.org>
Date:   Mon Jul 3 14:11:43 2023 +0200

tdf#150124: do nothing when parent is of unkown type

because 'switch' is not a known token. For now it, treat
it as a group

No need to have a unittest for this

Change-Id: I4cae92a010e45cd729db475a7abccab19a5d5403
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/155250
Tested-by: Jenkins
Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

svgio/inc/svgtoken.hxx
@@ -135,6 +135,7 @@ namespace svgio::svgreader
             G,
             Svg,
             Symbol,
+            Switch,
             Use,
             A,
 

svgio/source/svgreader/svgdocumenthandler.cxx
@@ -203,6 +203,7 @@ namespace
                     mpTarget->parseAttributes(xAttribs);
                     break;
                 }
+                case SVGToken::Switch: //TODO: Support switch element
                 case SVGToken::Defs:
                 case SVGToken::G:
                 {
@@ -487,6 +488,7 @@ namespace
                 case SVGToken::G:
                 case SVGToken::Svg:
                 case SVGToken::Symbol:
+                case SVGToken::Switch:
                 case SVGToken::Use:
                 case SVGToken::A:
 

svgio/source/svgreader/svgtoken.cxx
@@ -129,6 +129,7 @@ constexpr auto aSVGTokenMapperList = frozen::make_unordered_map<std::u16string_v
     { u"g", SVGToken::G },
     { u"svg", SVGToken::Svg },
     { u"symbol", SVGToken::Symbol },
+    { u"switch", SVGToken::Switch },
     { u"use", SVGToken::Use },
     { u"a", SVGToken::A },
 
@@ -286,6 +287,7 @@ constexpr auto  aSVGLowerCaseTokenMapperList = frozen::make_unordered_map<std::u
     { u"g", SVGToken::G },
     { u"svg", SVGToken::Svg },
     { u"symbol", SVGToken::Symbol },
+    { u"switch", SVGToken::Switch },
     { u"use", SVGToken::Use },
     { u"a", SVGToken::A },
 