LibreOffice/core

commit 7dff37c65e08985b80ac79e13f5571819a8e739d

fdo#81276: sw: HTML import: fix inverted test in CSS import
(regression from 5089bdb234a93bcb62b597c18b8ab66048246b13)

Change-Id: I5b45c4c47ba66385b2b528b5a3039bda37b060d6

sw/qa/extras/htmlexport/htmlexport.cxx
@@ -72,6 +72,19 @@ private:
 
 #define DECLARE_HTMLEXPORT_ROUNDTRIP_TEST(TestName, filename) DECLARE_SW_ROUNDTRIP_TEST(TestName, filename, HtmlExportTest)
 
+DECLARE_HTMLEXPORT_ROUNDTRIP_TEST(testFdo81276, "fdo81276.html")
+{
+    uno::Reference<container::XNameAccess> xPageStyles(getStyles("PageStyles"));
+    uno::Reference<beans::XPropertySet> xStyle(xPageStyles->getByName("HTML"), uno::UNO_QUERY);
+    // some rounding going on here?
+    CPPUNIT_ASSERT(abs(sal_Int32(29700) - getProperty<sal_Int32>(xStyle, "Width")) < 10);
+    CPPUNIT_ASSERT(abs(sal_Int32(21006) - getProperty<sal_Int32>(xStyle, "Height")) < 10);
+    CPPUNIT_ASSERT(abs(sal_Int32(500) - getProperty<sal_Int32>(xStyle, "LeftMargin")) < 10);
+    CPPUNIT_ASSERT(abs(sal_Int32(500) - getProperty<sal_Int32>(xStyle, "RightMargin")) < 10);
+    CPPUNIT_ASSERT(abs(sal_Int32(2000) - getProperty<sal_Int32>(xStyle, "TopMargin")) < 10);
+    CPPUNIT_ASSERT(abs(sal_Int32(500) - getProperty<sal_Int32>(xStyle, "BottomMargin")) < 10);
+}
+
 DECLARE_HTMLEXPORT_ROUNDTRIP_TEST(testFdo62336, "fdo62336.docx")
 {
     // The problem was essentially a crash during table export as docx/rtf/html

sw/qa/extras/htmlexport/data/fdo81276.html
@@ -1,0 +1,21 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html lang="ru-ru" >
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <style>
+
+    @page { size: 29.7cm 21cm;
+            margin-left: 0.5cm;
+            margin-right: 0.5cm;
+            margin-top: 2cm;
+            margin-bottom: 0.5cm
+    }
+
+    </style>
+</head>
+
+<body>
+
+</body>
+</html>

sw/source/filter/html/htmlcss1.cxx
@@ -1688,7 +1688,7 @@ void SwHTMLParser::NewStyle()
             sType = rOption.GetString();
     }
 
-    bIgnoreRawData = !sType.getLength() &&
+    bIgnoreRawData = sType.getLength() &&
                      !sType.getToken(0,';').equalsAscii(sCSS_mimetype);
 }
 

commit da468c92cfd93c810a79ce7769832367df4ea2b8

CppunitTest_sw_htmlexport: clean up testFdo81276
Still fails with commit 7dff37c65e08985b80ac79e13f5571819a8e739d
(fdo#81276: sw: HTML import: fix inverted test in CSS import,
2014-07-22) reverted, but now avoids the almost unused
DECLARE_HTMLEXPORT_ROUNDTRIP_TEST macro.

Change-Id: I8298606b8521009e733ba1e0ce6e513cd8f3db2b
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/155198
Tested-by: Jenkins
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

sw/qa/extras/htmlexport/htmlexport.cxx
@@ -290,17 +290,24 @@ void SwHtmlDomExportTest::ImportFromReqif(const OUString& rUrl)
     mxComponent = loadFromDesktop(rUrl, "com.sun.star.text.TextDocument", aLoadProperties);
 }
 
-DECLARE_HTMLEXPORT_ROUNDTRIP_TEST(testFdo81276, "fdo81276.html")
+CPPUNIT_TEST_FIXTURE(HtmlExportTest, testFdo81276)
 {
-    uno::Reference<container::XNameAccess> xPageStyles(getStyles("PageStyles"));
-    uno::Reference<beans::XPropertySet> xStyle(xPageStyles->getByName("HTML"), uno::UNO_QUERY);
-    // some rounding going on here?
-    CPPUNIT_ASSERT(abs(sal_Int32(29700) - getProperty<sal_Int32>(xStyle, "Width")) < 10);
-    CPPUNIT_ASSERT(abs(sal_Int32(21006) - getProperty<sal_Int32>(xStyle, "Height")) < 10);
-    CPPUNIT_ASSERT(abs(sal_Int32(500) - getProperty<sal_Int32>(xStyle, "LeftMargin")) < 10);
-    CPPUNIT_ASSERT(abs(sal_Int32(500) - getProperty<sal_Int32>(xStyle, "RightMargin")) < 10);
-    CPPUNIT_ASSERT(abs(sal_Int32(2000) - getProperty<sal_Int32>(xStyle, "TopMargin")) < 10);
-    CPPUNIT_ASSERT(abs(sal_Int32(500) - getProperty<sal_Int32>(xStyle, "BottomMargin")) < 10);
+    auto verify = [this]() {
+        uno::Reference<container::XNameAccess> xPageStyles(getStyles("PageStyles"));
+        uno::Reference<beans::XPropertySet> xStyle(xPageStyles->getByName("HTML"), uno::UNO_QUERY);
+        // some rounding going on here?
+        CPPUNIT_ASSERT(abs(sal_Int32(29700) - getProperty<sal_Int32>(xStyle, "Width")) < 10);
+        CPPUNIT_ASSERT(abs(sal_Int32(21006) - getProperty<sal_Int32>(xStyle, "Height")) < 10);
+        CPPUNIT_ASSERT(abs(sal_Int32(500) - getProperty<sal_Int32>(xStyle, "LeftMargin")) < 10);
+        CPPUNIT_ASSERT(abs(sal_Int32(500) - getProperty<sal_Int32>(xStyle, "RightMargin")) < 10);
+        CPPUNIT_ASSERT(abs(sal_Int32(2000) - getProperty<sal_Int32>(xStyle, "TopMargin")) < 10);
+        CPPUNIT_ASSERT(abs(sal_Int32(500) - getProperty<sal_Int32>(xStyle, "BottomMargin")) < 10);
+    };
+    setImportFilterName(mpFilter);
+    createSwDoc("fdo81276.html");
+    verify();
+    saveAndReload(mpFilter);
+    verify();
 }
 
 CPPUNIT_TEST_FIXTURE(HtmlExportTest, testFdo62336)