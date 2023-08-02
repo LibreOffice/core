LibreOffice/core

commit 1c7cbd685633d44eac554629572f3401c450f855

use AlphaMask for variables when calling GetAlphaMask
Right now this doesn't make any difference, since Bitmap is the
superclass of AlphaMask.
But when I switch to using alpha instead of transparency, passing
AlphaMask around is going to mean something different to passing plain
Bitmap around.

Change-Id: Ic1541e5f0a3b97331793cd23e23115faa7f382b0
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/151463
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/source/filter/egif/egif.cxx
@@ -242,7 +242,7 @@ bool GIFWriter::CreateAccess( const BitmapEx& rBmpEx )
 {
     if( bStatus )
     {
-        Bitmap aMask( rBmpEx.GetAlphaMask() );
+        AlphaMask aMask( rBmpEx.GetAlphaMask() );
 
         aAccBmp = rBmpEx.GetBitmap();
         bTransparent = false;

desktop/source/lib/init.cxx
@@ -3944,7 +3944,7 @@ static void doc_paintTile(LibreOfficeKitDocument* pThis,
     pDevice->EnableMapMode(false);
     BitmapEx aBmpEx = pDevice->GetBitmapEx({ 0, 0 }, { nCanvasWidth, nCanvasHeight });
     Bitmap aBmp = aBmpEx.GetBitmap();
-    Bitmap aAlpha = aBmpEx.GetAlphaMask();
+    AlphaMask aAlpha = aBmpEx.GetAlphaMask();
     Bitmap::ScopedReadAccess sraBmp(aBmp);
     Bitmap::ScopedReadAccess sraAlpha(aAlpha);
 

drawinglayer/source/primitive2d/graphicprimitivehelper2d.cxx
@@ -257,7 +257,7 @@ namespace drawinglayer::primitive2d
                         case Disposal::Not:
                         {
                             maVirtualDevice->DrawBitmapEx(rAnimationFrame.maPositionPixel, rAnimationFrame.maBitmapEx);
-                            Bitmap aAlphaMask = rAnimationFrame.maBitmapEx.GetAlphaMask();
+                            AlphaMask aAlphaMask = rAnimationFrame.maBitmapEx.GetAlphaMask();
 
                             if (aAlphaMask.IsEmpty())
                             {
@@ -277,7 +277,7 @@ namespace drawinglayer::primitive2d
                         case Disposal::Back:
                         {
                             // #i70772# react on no mask, for primitives, too.
-                            const Bitmap & rMask(rAnimationFrame.maBitmapEx.GetAlphaMask());
+                            const AlphaMask & rMask(rAnimationFrame.maBitmapEx.GetAlphaMask());
                             const Bitmap & rContent(rAnimationFrame.maBitmapEx.GetBitmap());
 
                             maVirtualDeviceMask->Erase();

forms/source/component/imgprod.cxx
@@ -366,7 +366,7 @@ void ImageProducer::ImplUpdateConsumer( const Graphic& rGraphic )
     if( !pBmpAcc )
         return;
 
-    Bitmap              aMask( aBmpEx.GetAlphaMask() );
+    AlphaMask              aMask( aBmpEx.GetAlphaMask() );
     BitmapReadAccess*   pMskAcc = !aMask.IsEmpty() ? aMask.AcquireReadAccess() : nullptr;
     const tools::Long          nWidth = pBmpAcc->Width();
     const tools::Long          nHeight = pBmpAcc->Height();
@@ -380,7 +380,7 @@ void ImageProducer::ImplUpdateConsumer( const Graphic& rGraphic )
     if( !pMskAcc )
     {
         aMask = Bitmap(aBmp.GetSizePixel(), vcl::PixelFormat::N8_BPP, &Bitmap::GetGreyPalette(256));
-        aMask.Erase( COL_BLACK );
+        aMask.Erase( 0 );
         pMskAcc = aMask.AcquireReadAccess();
     }
 

slideshow/source/engine/shapes/gdimtftools.cxx
@@ -283,7 +283,7 @@ bool getAnimationFromGraphic( VectorOfMtfAnimationFrames&   o_rFrames,
             {
                 pVDev->DrawBitmapEx(rAnimationFrame.maPositionPixel,
                                     rAnimationFrame.maBitmapEx);
-                Bitmap aMask = rAnimationFrame.maBitmapEx.GetAlphaMask();
+                AlphaMask aMask = rAnimationFrame.maBitmapEx.GetAlphaMask();
 
                 if( aMask.IsEmpty() )
                 {

vcl/source/filter/eps/eps.cxx
@@ -819,7 +819,7 @@ void PSWriter::ImplWriteActions( const GDIMetaFile& rMtf, VirtualDevice& rVDev )
                 Bitmap aBitmap( aBitmapEx.GetBitmap() );
                 if ( mbGrayScale )
                     aBitmap.Convert( BmpConversion::N8BitGreys );
-                Bitmap aMask( aBitmapEx.GetAlphaMask() );
+                AlphaMask aMask( aBitmapEx.GetAlphaMask() );
                 Point aPoint( static_cast<const MetaBmpExAction*>(pMA)->GetPoint() );
                 Size aSize( rVDev.PixelToLogic( aBitmap.GetSizePixel() ) );
                 ImplBmp( &aBitmap, &aMask, aPoint, aSize.Width(), aSize.Height() );
@@ -832,7 +832,7 @@ void PSWriter::ImplWriteActions( const GDIMetaFile& rMtf, VirtualDevice& rVDev )
                 Bitmap aBitmap( aBitmapEx.GetBitmap() );
                 if ( mbGrayScale )
                     aBitmap.Convert( BmpConversion::N8BitGreys );
-                Bitmap aMask( aBitmapEx.GetAlphaMask() );
+                AlphaMask aMask( aBitmapEx.GetAlphaMask() );
                 Point aPoint = static_cast<const MetaBmpExScaleAction*>(pMA)->GetPoint();
                 Size aSize( static_cast<const MetaBmpExScaleAction*>(pMA)->GetSize() );
                 ImplBmp( &aBitmap, &aMask, aPoint, aSize.Width(), aSize.Height() );
@@ -847,7 +847,7 @@ void PSWriter::ImplWriteActions( const GDIMetaFile& rMtf, VirtualDevice& rVDev )
                 Bitmap      aBitmap( aBitmapEx.GetBitmap() );
                 if ( mbGrayScale )
                     aBitmap.Convert( BmpConversion::N8BitGreys );
-                Bitmap      aMask( aBitmapEx.GetAlphaMask() );
+                AlphaMask   aMask( aBitmapEx.GetAlphaMask() );
                 Point aPoint = static_cast<const MetaBmpExScalePartAction*>(pMA)->GetDestPoint();
                 Size aSize = static_cast<const MetaBmpExScalePartAction*>(pMA)->GetDestSize();
                 ImplBmp( &aBitmap, &aMask, aPoint, aSize.Width(), aSize.Height() );
@@ -907,7 +907,7 @@ void PSWriter::ImplWriteActions( const GDIMetaFile& rMtf, VirtualDevice& rVDev )
                         // gradient action
 
                         }
-                        Bitmap aMask( aBitmapEx.GetAlphaMask() );
+                        AlphaMask aMask( aBitmapEx.GetAlphaMask() );
                         ImplBmp( &aBitmap, &aMask, Point( aRect.Left(), aRect.Top() ), aRect.GetWidth(), aRect.GetHeight() );
                     }
                     else

vcl/source/filter/wmf/emfwr.cxx
@@ -1294,7 +1294,7 @@ void EMFWriter::ImplWrite( const GDIMetaFile& rMtf )
             {
                 const MetaBmpExAction*  pA = static_cast<const MetaBmpExAction *>(pAction);
                 Bitmap                  aBmp( pA->GetBitmapEx().GetBitmap() );
-                Bitmap                  aMsk( pA->GetBitmapEx().GetAlphaMask() );
+                AlphaMask               aMsk( pA->GetBitmapEx().GetAlphaMask() );
 
                 if( !aMsk.IsEmpty() )
                 {
@@ -1312,7 +1312,7 @@ void EMFWriter::ImplWrite( const GDIMetaFile& rMtf )
             {
                 const MetaBmpExScaleAction* pA = static_cast<const MetaBmpExScaleAction*>(pAction);
                 Bitmap                      aBmp( pA->GetBitmapEx().GetBitmap() );
-                Bitmap                      aMsk( pA->GetBitmapEx().GetAlphaMask() );
+                AlphaMask                   aMsk( pA->GetBitmapEx().GetAlphaMask() );
 
                 if( !aMsk.IsEmpty() )
                 {
@@ -1332,7 +1332,7 @@ void EMFWriter::ImplWrite( const GDIMetaFile& rMtf )
                 BitmapEx                        aBmpEx( pA->GetBitmapEx() );
                 aBmpEx.Crop( tools::Rectangle( pA->GetSrcPoint(), pA->GetSrcSize() ) );
                 Bitmap                          aBmp( aBmpEx.GetBitmap() );
-                Bitmap                          aMsk( aBmpEx.GetAlphaMask() );
+                AlphaMask                       aMsk( aBmpEx.GetAlphaMask() );
 
                 if( !aMsk.IsEmpty() )
                 {

vcl/source/filter/wmf/wmfwr.cxx
@@ -1245,7 +1245,7 @@ void WMFWriter::WriteRecords( const GDIMetaFile & rMTF )
             {
                 const MetaBmpExAction*  pA = static_cast<const MetaBmpExAction *>(pMA);
                 Bitmap                  aBmp( pA->GetBitmapEx().GetBitmap() );
-                Bitmap                  aMsk( pA->GetBitmapEx().GetAlphaMask() );
+                AlphaMask               aMsk( pA->GetBitmapEx().GetAlphaMask() );
 
                 if( !aMsk.IsEmpty() )
                 {
@@ -1263,7 +1263,7 @@ void WMFWriter::WriteRecords( const GDIMetaFile & rMTF )
             {
                 const MetaBmpExScaleAction* pA = static_cast<const MetaBmpExScaleAction*>(pMA);
                 Bitmap                      aBmp( pA->GetBitmapEx().GetBitmap() );
-                Bitmap                      aMsk( pA->GetBitmapEx().GetAlphaMask() );
+                AlphaMask                   aMsk( pA->GetBitmapEx().GetAlphaMask() );
 
                 if( !aMsk.IsEmpty() )
                 {
@@ -1283,7 +1283,7 @@ void WMFWriter::WriteRecords( const GDIMetaFile & rMTF )
                 BitmapEx                        aBmpEx( pA->GetBitmapEx() );
                 aBmpEx.Crop( tools::Rectangle( pA->GetSrcPoint(), pA->GetSrcSize() ) );
                 Bitmap                          aBmp( aBmpEx.GetBitmap() );
-                Bitmap                          aMsk( aBmpEx.GetAlphaMask() );
+                AlphaMask                       aMsk( aBmpEx.GetAlphaMask() );
 
                 if( !aMsk.IsEmpty() )
                 {

commit f76acf9bd97e65ba50303b6e5a25e5877996ebe1

tdf#156525 Save as > HTML loses drawing object as invalid gif
regression from
    commit 1c7cbd685633d44eac554629572f3401c450f855
    Author: Noel Grandin <noel.grandin@collabora.co.uk>
    Date:   Sun May 7 16:56:21 2023 +0200
    use AlphaMask for variables when calling GetAlphaMask

where after my change, the code is now calling
    Bitmap::Replace(AlphaMask,...)
instead of
    Bitmap::Replace(Bitmap,...)
and those two methods do quite different things.

However, we have to
(*) restore Bitmap::Replace(Bitmap,...) which was removed in
    commit 8270eb5d5600cc84dbf5f0e339f90c4519ef88bb
    Author: Noel Grandin <noel.grandin@collabora.co.uk>
    Date:   Fri May 19 13:35:31 2023 +0200
    loplugin:unusedmethods
(*) restore BitmapWriteAccess::SetPaletteEntryCount which was removed in
    commit 74cd0d0b281f8df75612bfb600df2eae62c4d21d
    Author: Noel Grandin <noel.grandin@collabora.co.uk>
    Date:   Thu Jun 29 13:53:30 2023 +0200
    loplugin:unusedmethods
(*) Invert the mask/alpha layer, since after
    commit 81994cb2b8b32453a92bcb011830fcb884f22ff3
    Author: Noel Grandin <noelgrandin@gmail.com>
    Date:   Fri Apr 16 20:33:10 2021 +0200
    Convert internal vcl bitmap formats transparency->alpha (II)
we are dealing with real alpha, and not transparency.

Also add an assert in GIFWriter::WriteAccess, since it is a logic error
to get here and have the image in the wrong format.

Change-Id: I0e09b3ca82af0bd5b58d80e0a6eac4c7bdf7c48e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/155254
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

include/vcl/bitmap.hxx
@@ -430,6 +430,18 @@ public:
      */
     bool                    Replace( const AlphaMask& rAlpha, const Color& rMergeColor );
 
+    /** Replace all pixel where the given mask/alpha layer is on with the specified color
+
+        @param rMask
+        Mask specifying which pixel should be replaced
+
+        @param rReplaceColor
+        Color to be placed in all changed pixel
+
+        @return true, if the operation was completed successfully.
+     */
+    bool                    ReplaceMask( const AlphaMask& rMask, const Color& rReplaceColor );
+
     /** Replace all pixel having the search color with the specified color
 
         @param rSearchColor

vcl/inc/bitmap/BitmapWriteAccess.hxx
@@ -39,6 +39,13 @@ public:
         mpBuffer->maPalette = rPalette;
     }
 
+    void SetPaletteEntryCount(sal_uInt16 nCount)
+    {
+        assert(mpBuffer && "Access is not valid!");
+
+        mpBuffer->maPalette.SetEntryCount(nCount);
+    }
+
     void SetPaletteColor(sal_uInt16 nColor, const BitmapColor& rBitmapColor)
     {
         assert(mpBuffer && "Access is not valid!");

vcl/source/bitmap/bitmappaint.cxx
@@ -884,6 +884,81 @@ vcl::Region Bitmap::CreateRegion(const Color& rColor, const tools::Rectangle& rR
     return aRegion;
 }
 
+bool Bitmap::ReplaceMask(const AlphaMask& rMask, const Color& rReplaceColor)
+{
+    ScopedReadAccess pMaskAcc(const_cast<AlphaMask&>(rMask));
+    BitmapScopedWriteAccess pAcc(*this);
+
+    if (!pMaskAcc || !pAcc)
+        return false;
+
+    const tools::Long nWidth = std::min(pMaskAcc->Width(), pAcc->Width());
+    const tools::Long nHeight = std::min(pMaskAcc->Height(), pAcc->Height());
+    const BitmapColor aMaskWhite(pMaskAcc->GetBestMatchingColor(COL_WHITE));
+    BitmapColor aReplace;
+
+    if (pAcc->HasPalette())
+    {
+        const sal_uInt16 nActColors = pAcc->GetPaletteEntryCount();
+        const sal_uInt16 nMaxColors = 1 << pAcc->GetBitCount();
+
+        // default to the nearest color
+        aReplace = pAcc->GetBestMatchingColor(rReplaceColor);
+
+        // for paletted images without a matching palette entry
+        // look for an unused palette entry (NOTE: expensive!)
+        if (pAcc->GetPaletteColor(aReplace.GetIndex()) != BitmapColor(rReplaceColor))
+        {
+            // if the palette has empty entries use the last one
+            if (nActColors < nMaxColors)
+            {
+                pAcc->SetPaletteEntryCount(nActColors + 1);
+                pAcc->SetPaletteColor(nActColors, rReplaceColor);
+                aReplace = BitmapColor(static_cast<sal_uInt8>(nActColors));
+            }
+            else
+            {
+                std::unique_ptr<bool[]> pFlags(new bool[nMaxColors]);
+
+                // Set all entries to false
+                std::fill(pFlags.get(), pFlags.get() + nMaxColors, false);
+
+                for (tools::Long nY = 0; nY < nHeight; nY++)
+                {
+                    Scanline pScanline = pAcc->GetScanline(nY);
+                    for (tools::Long nX = 0; nX < nWidth; nX++)
+                        pFlags[pAcc->GetIndexFromData(pScanline, nX)] = true;
+                }
+
+                for (sal_uInt16 i = 0; i < nMaxColors; i++)
+                {
+                    // Hurray, we do have an unused entry
+                    if (!pFlags[i])
+                    {
+                        pAcc->SetPaletteColor(i, rReplaceColor);
+                        aReplace = BitmapColor(static_cast<sal_uInt8>(i));
+                    }
+                }
+            }
+        }
+    }
+    else
+        aReplace = rReplaceColor;
+
+    for (tools::Long nY = 0; nY < nHeight; nY++)
+    {
+        Scanline pScanline = pAcc->GetScanline(nY);
+        Scanline pScanlineMask = pMaskAcc->GetScanline(nY);
+        for (tools::Long nX = 0; nX < nWidth; nX++)
+        {
+            if (pMaskAcc->GetPixelFromData(pScanlineMask, nX) == aMaskWhite)
+                pAcc->SetPixelOnData(pScanline, nX, aReplace);
+        }
+    }
+
+    return true;
+}
+
 bool Bitmap::Replace(const AlphaMask& rAlpha, const Color& rMergeColor)
 {
     Bitmap aNewBmp(GetSizePixel(), vcl::PixelFormat::N24_BPP);

vcl/source/filter/egif/egif.cxx
@@ -252,7 +252,8 @@ bool GIFWriter::CreateAccess( const BitmapEx& rBmpEx )
             if( aAccBmp.Convert( BmpConversion::N8BitTrans ) )
             {
                 aMask.Convert( BmpConversion::N1BitThreshold );
-                aAccBmp.Replace( aMask, BMP_COL_TRANS );
+                aMask.Invert();
+                aAccBmp.ReplaceMask( aMask, BMP_COL_TRANS );
                 bTransparent = true;
             }
             else
@@ -467,6 +468,9 @@ void GIFWriter::WriteAccess()
     if( !bNative )
         pBuffer.reset(new sal_uInt8[ nWidth ]);
 
+    assert(bStatus && "should not calling here if status is bad");
+    assert( 8 == m_pAcc->GetBitCount() && m_pAcc->HasPalette()
+            && "by the time we get here, the image should be in palette format");
     if( !(bStatus && ( 8 == m_pAcc->GetBitCount() ) && m_pAcc->HasPalette()) )
         return;
 