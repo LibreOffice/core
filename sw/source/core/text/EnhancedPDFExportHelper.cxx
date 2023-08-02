74eafad84463d097adc8fe680f57b2c9039e0a56
Michael Stahl <michael.stahl@allotropia.de>
2023-08-02 15:01:20 +0200

    tdf#156306 sw: PDF export: duplicate structure element from ...
    ... lazy open LIBody - with mpFrameInfo, not mpNumInfo - causes this
    assert and later an infinite loop as the SE is its own parent:
    
    sw/source/core/text/EnhancedPDFExportHelper.cxx:468: void SwTaggedPDFHelper::BeginTag(vcl::PDFWriter::StructElement, const rtl::OUString&): Assertion `rFrameTagSet.find(pKey) == rFrameTagSet.end()' failed.
    
    (regression from commit d467f1aa3d028f399826c97e2eecedcd79efcf65)
    
    Change-Id: I90f057c45a22e5ef44ae5ae9d1cb1d6b8870379f
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/155253
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
    (cherry picked from commit 16aa870598d00eb3e6ac15435fef080d3b87cbc1)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/155237
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

--- sw/source/core/text/EnhancedPDFExportHelper.cxx
+++ sw/source/core/text/EnhancedPDFExportHelper.cxx
@@ -449,7 +449,7 @@ void SwTaggedPDFHelper::BeginTag( vcl::PDFWriter::StructElement eType, const OUS
 {
     void const* pKey(nullptr);
 
-    if ( mpFrameInfo )
+    if (mpFrameInfo && eType != vcl::PDFWriter::LIBody)
     {
         const SwFrame& rFrame = mpFrameInfo->mrFrame;
 
