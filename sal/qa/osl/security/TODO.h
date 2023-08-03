cdc2b4fe656d92e3817195323993698e052b6fa5
Gabor Kelemen <gabor.kelemen.extern@allotropia.de>
2023-08-03 18:38:00 +0200

    Remove unused header file
    Seems like it was committed accidentally
    
    Change-Id: Idc086b7b92ef9c4a8b3bafa2114b7da3e8d42618
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/155350
    Tested-by: Jenkins
    Reviewed-by: Thorsten Behrens <thorsten.behrens@allotropia.de>

--- sal/qa/osl/security/TODO.h
+++
@@ -1,8 +0,0 @@
-#include "sal/types.h"
-#define T1(x) do { x; } while (0)
-#define T2(x) do { x; } while (sal_False)
-#define T3(x) do { x; } while (false)
-#define T4(x, y) do { x; } while (y)
-#define T5(x) T4(x, 0)
-#define T6(x) T4(x, sal_False)
-#define T7(x) T4(x, false)
