150a1528882347882e857649290fe81d5aa08345
Colomban Wendling <cwendling@hypra.fr>
2023-08-03 16:09:27 +0200

    gtk3 a11y tests: Increase DBus timeout
    We apparently get recurrent spurious timeout failures in the CI
    related to DBus timeout, so increase that value.
    
    The chosen value is a bit arbitrary, but is below the maximum
    recommended by the documentation (even though the recommendation hardly
    applies to our use case), and more than 3 times larger than the
    default.  That will hopefully be enough.
    
    Change-Id: I244f711f46353009743015d00b303aeb87332936
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/155310
    Tested-by: Jenkins
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>

--- vcl/qa/cppunit/a11y/atspi2/atspi2testbase.hxx
+++ vcl/qa/cppunit/a11y/atspi2/atspi2testbase.hxx
@@ -79,6 +79,9 @@ public:
     {
         if (!atspi_is_initialized())
             atspi_init();
+
+        // increase timeout to avoid spurious avoid CI failures
+        atspi_set_timeout(2500, 15000);
     }
 
     virtual void setUp() override
