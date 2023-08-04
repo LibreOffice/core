LibreOffice/core

commit 81994cb2b8b32453a92bcb011830fcb884f22ff3

Convert internal vcl bitmap formats transparency->alpha (II)
(Second attempt at landing this)

Image formats and graphics APIs use alpha, not transparency,
so change our internal formats and data structures to work directly
with alpha, so we don't need to modify data before we push it to
graphics APIs.

Add a couple of new Color constants to make the intention
of the vcl code clearer.

Notes
(*) On macOS, tweaking the logic in CreateWithSalBitmapAndMask
to more accurately reflect the requirements of the
CGImageCreateWithMask function seems to fix some
tests.

(*) The vcl code does not properly support gradients
with transparency. So the previous code was wrong, and this
change is going to result in slightly different wrongness.

Change-Id: I9e21c2e98d88ecfdc5f75db13bd1ffff7c38db98
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/114168
Tested-by: Jenkins
Reviewed-by: Patrick Luby <plubius@neooffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

drawinglayer/source/primitive2d/GlowSoftEgdeShadowTools.cxx
@@ -58,10 +58,10 @@ AlphaMask ProcessAndBlurAlphaMask(const Bitmap& rMask, double fErodeDilateRadius
     else if (fErodeDilateRadius < 0)
         BitmapFilter::Filter(mask, BitmapErodeFilter(-fErodeDilateRadius, 0xFF));
 
-    if (nTransparency)
+    if (nTransparency != 255)
     {
         const Color aTransparency(nTransparency, nTransparency, nTransparency);
-        mask.Replace(COL_BLACK, aTransparency);
+        mask.Replace(COL_WHITE, aTransparency);
     }
 
     // We need 8-bit grey mask for blurring
@@ -71,6 +71,8 @@ AlphaMask ProcessAndBlurAlphaMask(const Bitmap& rMask, double fErodeDilateRadius
     BitmapFilter::Filter(mask, BitmapFilterStackBlur(fBlurRadius));
 
     mask.Scale(rMask.GetSizePixel());
+
+    mask.Invert(); // convert transparency to alpha
 
     return AlphaMask(mask.GetBitmap());
 }

commit 0e7d4ea64f84bdc62c10692dcd97c3a32e72e16e

tdf#156508 Blur shadow is solid in edit and presentation mode
regression from
    commit 81994cb2b8b32453a92bcb011830fcb884f22ff3
    Author: Noel Grandin <noelgrandin@gmail.com>
    Date:   Fri Apr 16 20:33:10 2021 +0200
    Convert internal vcl bitmap formats transparency->alpha (II)

Change-Id: I14f62547e3301cab0eff79716bf7bc2a51ead563
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/155338
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

drawinglayer/source/primitive2d/GlowSoftEgdeShadowTools.cxx
@@ -58,10 +58,10 @@ AlphaMask ProcessAndBlurAlphaMask(const Bitmap& rMask, double fErodeDilateRadius
     else if (fErodeDilateRadius < 0)
         BitmapFilter::Filter(mask, BitmapErodeFilter(-fErodeDilateRadius, 0xFF));
 
-    if (nTransparency != 255)
+    if (nTransparency)
     {
         const Color aTransparency(nTransparency, nTransparency, nTransparency);
-        mask.Replace(COL_WHITE, aTransparency);
+        mask.Replace(COL_BLACK, aTransparency);
     }
 
     // We need 8-bit grey mask for blurring
@@ -71,8 +71,6 @@ AlphaMask ProcessAndBlurAlphaMask(const Bitmap& rMask, double fErodeDilateRadius
     BitmapFilter::Filter(mask, BitmapFilterStackBlur(fBlurRadius));
 
     mask.Scale(rMask.GetSizePixel());
-
-    mask.Invert(); // convert transparency to alpha
 
     return AlphaMask(mask.GetBitmap());
 }