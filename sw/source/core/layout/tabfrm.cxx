LibreOffice/core

commit 7f85415a2f07d62bf688cb33680054940d4dd7f1

move SwVirtPageNumInfo to SfxHint
- also remove now obsolete GetInfo overrides

Change-Id: Iaac75ed2e53daead06242ce4620fd2b879909e02
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/155061
Tested-by: Jenkins
Reviewed-by: Bjoern Michaelsen <bjoern.michaelsen@libreoffice.org>

sw/source/core/layout/tabfrm.cxx
@@ -3512,6 +3512,14 @@ void SwTabFrame::SwClientNotify(const SwModify& rMod, const SfxHint& rHint)
         HandleTableHeadlineChange();
         return;
     }
+    else if(rHint.GetId() == SfxHintId::SwVirtPageNumHint)
+    {
+        auto& rVirtPageNumHint = const_cast<sw::VirtPageNumHint&>(static_cast<const sw::VirtPageNumHint&>(rHint));
+        if(!IsInDocBody() || IsFollow() || rVirtPageNumHint.IsFound())
+            return;
+        if(const SwPageFrame* pPage = FindPageFrame())
+            pPage->UpdateVirtPageNumInfo(rVirtPageNumHint, this);
+    }
     else if (rHint.GetId() != SfxHintId::SwLegacyModify)
         return;
     auto pLegacy = static_cast<const sw::LegacyModifyHint*>(&rHint);
@@ -3636,17 +3644,6 @@ void SwTabFrame::UpdateAttr_( const SfxPoolItem *pOld, const SfxPoolItem *pNew,
         SwModify aMod;
         SwLayoutFrame::SwClientNotify(aMod, sw::LegacyModifyHint(pOld, pNew));
     }
-}
-
-bool SwTabFrame::GetInfo(SfxPoolItem& rHint) const
-{
-    if(RES_VIRTPAGENUM_INFO == rHint.Which() && IsInDocBody() && !IsFollow())
-    {
-        SwVirtPageNumInfo& rInfo = static_cast<SwVirtPageNumInfo&>(rHint);
-        if(const SwPageFrame* pPage = FindPageFrame())
-            return pPage->UpdateVirtPageNumInfo(rInfo, this);
-    }
-    return true;
 }
 
 SwFrame *SwTabFrame::FindLastContentOrTable()

compilerplugins/clang/unusedmethods.results
@@ -1935,8 +1935,6 @@ include/svl/typedwhich.hxx:31
 include/svl/typedwhich.hxx:31
      TypedWhichId::TypedWhichId(TypedWhichId<type-parameter-?-?>,typename enable_if<std::is_base_of_v<SwUpdateAttr, derived_type>, int>::type)
 include/svl/typedwhich.hxx:31
-     TypedWhichId::TypedWhichId(TypedWhichId<type-parameter-?-?>,typename enable_if<std::is_base_of_v<SwVirtPageNumInfo, derived_type>, int>::type)
-include/svl/typedwhich.hxx:31
      TypedWhichId::TypedWhichId(TypedWhichId<type-parameter-?-?>,typename enable_if<std::is_base_of_v<SwWrtShellItem, derived_type>, int>::type)
 include/svl/typedwhich.hxx:31
      TypedWhichId::TypedWhichId(TypedWhichId<type-parameter-?-?>,typename enable_if<std::is_base_of_v<XColorItem, derived_type>, int>::type)

include/svl/hint.hxx
@@ -152,6 +152,7 @@ enum class SfxHintId {
     SwDocPosUpdate,
     SwDocPosUpdateAtIndex,
     SwTableHeadingChange,
+    SwVirtPageNumHint,
 
     ThisIsAnSdrHint
 };

svl/source/items/poolitem.cxx
@@ -437,7 +437,6 @@
 //     class SwTableFormulaUpdate : public SwMsgPoolItem
 //     class SwAutoFormatGetDocNode: public SwMsgPoolItem
 //     class SwAttrSetChg: public SwMsgPoolItem
-//     class SwVirtPageNumInfo: public SwMsgPoolItem
 //     class SwFindNearestNode : public SwMsgPoolItem
 //     class SwStringMsgPoolItem : public SwMsgPoolItem
 // class SwFormatDrop: public SfxPoolItem, public SwClient

sw/inc/hintids.hxx
@@ -141,7 +141,7 @@ class SwTransparencyGrf;
 class SwFormatRuby;
 class SwTableFormulaUpdate;
 class SwAutoFormatGetDocNode;
-class SwVirtPageNumInfo;
+class VirtPageNumHint;
 class SwFindNearestNode;
 class SwFltAnchor;
 class SwFltTOX;
@@ -433,7 +433,6 @@ constexpr TypedWhichId<SwAttrSetChg> RES_ATTRSET_CHG(169);
 constexpr TypedWhichId<SwUpdateAttr> RES_UPDATE_ATTR(170);
 constexpr TypedWhichId<SwAutoFormatGetDocNode> RES_AUTOFMT_DOCNODE(176);
 constexpr TypedWhichId<SwMsgPoolItem> RES_HIDDENPARA_PRINT(178);
-constexpr TypedWhichId<SwVirtPageNumInfo> RES_VIRTPAGENUM_INFO(180);
 constexpr TypedWhichId<SwPtrMsgPoolItem> RES_REMOVE_UNO_OBJECT(181);
 // empty
 constexpr TypedWhichId<SwFindNearestNode> RES_FINDNEARESTNODE(184);

sw/inc/hints.hxx
@@ -261,6 +261,32 @@ class TableHeadingChange final: public SfxHint
 public:
     TableHeadingChange() : SfxHint(SfxHintId::SwTableHeadingChange) {};
 };
+class VirtPageNumHint final: public SfxHint
+{
+    const SwPageFrame* m_pPage;
+    const SwPageFrame* m_pOrigPage;
+    const SwFrame* m_pFrame;
+    bool m_bFound;
+    /** Multiple attributes can be attached to a single paragraph / table
+     The frame, in the end, has to decide which attribute takes effect and which physical page it involves */
+public:
+    VirtPageNumHint(const SwPageFrame* pPg);
+    const SwPageFrame* GetPage() const
+        { return m_pPage; }
+    const SwPageFrame* GetOrigPage() const
+        { return m_pOrigPage; }
+    const SwFrame* GetFrame() const
+        { return m_pFrame; }
+    void SetInfo(const SwPageFrame* pPg, const SwFrame *pF)
+        { m_pFrame = pF; m_pPage = pPg; }
+    void SetFound()
+    {
+        assert(!m_bFound);
+        m_bFound = true;
+    }
+    bool IsFound()
+        { return m_bFound; }
+};
 }
 
 class SwUpdateAttr final : public SwMsgPoolItem
@@ -359,22 +385,6 @@ public:
 #endif
 };
 
-class SwVirtPageNumInfo final : public SwMsgPoolItem
-{
-    const SwPageFrame *m_pPage;
-    const SwPageFrame *m_pOrigPage;
-    const SwFrame     *m_pFrame;
-    /** Multiple attributes can be attached to a single paragraph / table
-     The frame, in the end, has to decide which attribute takes effect and which physical page it involves */
-public:
-    SwVirtPageNumInfo( const SwPageFrame *pPg );
-
-    const SwPageFrame *GetPage() const          { return m_pPage;    }
-    const SwPageFrame *GetOrigPage() const      { return m_pOrigPage;}
-    const SwFrame *GetFrame() const             { return m_pFrame; }
-    void  SetInfo( const SwPageFrame *pPg,
-                   const SwFrame *pF )    { m_pFrame = pF; m_pPage = pPg; }
-};
 
 class SwFindNearestNode final : public SwMsgPoolItem
 {

sw/source/core/attr/hints.cxx
@@ -69,6 +69,15 @@ RedlineUnDelText::RedlineUnDelText(sal_Int32 const nS, sal_Int32 const nL)
 {
 }
 
+VirtPageNumHint::VirtPageNumHint(const SwPageFrame* pPg):
+    SfxHint(SfxHintId::SwVirtPageNumHint),
+    m_pPage(nullptr),
+    m_pOrigPage(pPg),
+    m_pFrame(nullptr),
+    m_bFound(false)
+{
+}
+
 } // namespace sw
 
 SwUpdateAttr::SwUpdateAttr( sal_Int32 nS, sal_Int32 nE, sal_uInt16 nW )
@@ -158,11 +167,6 @@ const SfxPoolItem* GetDfltAttr( sal_uInt16 nWhich )
     return aAttrTab[ nWhich - POOLATTR_BEGIN ];
 }
 #endif
-
-SwVirtPageNumInfo::SwVirtPageNumInfo( const SwPageFrame *pPg ) :
-    SwMsgPoolItem( RES_VIRTPAGENUM_INFO ), m_pPage( nullptr ), m_pOrigPage( pPg ), m_pFrame( nullptr )
-{
-}
 
 SwFindNearestNode::SwFindNearestNode( const SwNode& rNd )
     : SwMsgPoolItem( RES_FINDNEARESTNODE ), m_pNode( &rNd ), m_pFound( nullptr )

sw/source/core/inc/pagefrm.hxx
@@ -36,7 +36,9 @@ class SwAttrSetChg;
 namespace vcl { class Font; }
 class SwSortedObjs;
 class SwAnchoredObject;
-class SwVirtPageNumInfo;
+namespace sw {
+    class VirtPageNumHint;
+}
 
 enum class SwPageFrameInvFlags : sal_uInt8
 {
@@ -351,7 +353,7 @@ public:
     const SwHeaderFrame* GetHeaderFrame() const;
     const SwFooterFrame* GetFooterFrame() const;
 
-    bool UpdateVirtPageNumInfo(SwVirtPageNumInfo& rHint, const SwFrame* pFrame) const;
+    void UpdateVirtPageNumInfo(sw::VirtPageNumHint& rHint, const SwFrame* pFrame) const;
 
     void dumpAsXml(xmlTextWriterPtr writer = nullptr) const override;
 };

commit 1e4b0e5a36b5fe5b1a391e8417b28a7900acc560

fix "move SwVirtPageNumInfo to SfxHint"
commit 7f85415a2f07d62bf688cb33680054940d4dd7f1 caused

> 0x7f206906bd30: note: object is of type 'sw::VirtPageNumHint'
>  80 60 00 00  f0 33 8b 16 20 7f 00 00  6d 00 00 00 00 00 00 00  00 00
00 00 00 00 00 00  80 94 5c 00
>               ^~~~~~~~~~~~~~~~~~~~~~~
>               vptr for 'sw::VirtPageNumHint'
>     #0 0x7f2007e0e38b in SwTabFrame::SwClientNotify(SwModify const&,
SfxHint const&) /sw/source/core/layout/tabfrm.cxx:3525:20
>     #1 0x7f2004957b22 in SwModify::CallSwClientNotify(SfxHint const&)
const /sw/source/core/attr/calbck.cxx:322:18
>     #2 0x7f2004957e25 in
sw::BroadcastingModify::CallSwClientNotify(SfxHint const&) const
/sw/source/core/attr/calbck.cxx:327:15
>     #3 0x7f2007ea97eb in SwFrame::GetVirtPageNum() const
/sw/source/core/layout/trvlfrm.cxx:1844:19
>     #4 0x7f20089f092b in
SwTextFormatter::NewFieldPortion(SwTextFormatInfo&, SwTextAttr const*)
const /sw/source/core/text/txtfld.cxx:160:47
>     #5 0x7f20089f9be6 in
SwTextFormatter::NewExtraPortion(SwTextFormatInfo&)
/sw/source/core/text/txtfld.cxx:382:20
>     #6 0x7f200863dbe8 in
SwTextFormatter::NewPortion(SwTextFormatInfo&,
std::optional<o3tl::strong_int<int, Tag_TextFrameIndex> >)
/sw/source/core/text/itrform2.cxx:1737:28
>     #7 0x7f200862661f in
SwTextFormatter::BuildPortions(SwTextFormatInfo&)
/sw/source/core/text/itrform2.cxx:440:27
>     #8 0x7f200865f573 in
SwTextFormatter::FormatLine(o3tl::strong_int<int, Tag_TextFrameIndex>)
/sw/source/core/text/itrform2.cxx:1952:9
>     #9 0x7f200844aef2 in SwTextFrame::FormatLine(SwTextFormatter&,
bool) /sw/source/core/text/frmform.cxx:1303:44
>     #10 0x7f2008459474 in SwTextFrame::Format_(SwTextFormatter&,
SwTextFormatInfo&, bool) /sw/source/core/text/frmform.cxx:1663:23
>     #11 0x7f200845edcb in SwTextFrame::FormatImpl(OutputDevice*,
SwParaPortion*, std::__debug::vector<SwAnchoredObject*,
std::allocator<SwAnchoredObject*> >&)
/sw/source/core/text/frmform.cxx:1854:5

sw/source/core/layout/tabfrm.cxx
@@ -3519,6 +3519,7 @@ void SwTabFrame::SwClientNotify(const SwModify& rMod, const SfxHint& rHint)
             return;
         if(const SwPageFrame* pPage = FindPageFrame())
             pPage->UpdateVirtPageNumInfo(rVirtPageNumHint, this);
+        return;
     }
     else if (rHint.GetId() != SfxHintId::SwLegacyModify)
         return;