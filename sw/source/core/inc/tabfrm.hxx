LibreOffice/core

commit 59987d3c77ec7dbf59fbea9f47cc226f4e8903f9

tdf#150606 sw: layout: leave follow SwTabFrame invalid in columns
With the minimised bugdoc, on Undo, some 12 or so pages are created, and
the end of the deleted table is shown in the document view.

So first the last pages are layouted, and then once those are done
layout proceeds from page 1.

The problem is that on page 9, a table remains in the 2nd column of the
section, and there is nothing in the 1st column and on pages 5-8.

This is because ::CalcContent() is the last thing that formats the table
on page 9, and it explicitly disables MoveBwd() for follow tables via
m_bLockBackMove.

That appears to be intentional but because it's already been there since
initial CVS import it's unclear why; it might be based on the assumption
that a previous column would have moved over rows from the follow table
already.

Try to encourage the table to try to MoveBwd() at a later time, when
it's being formatted from the SwLayAction::FormatContent_() loop, which
appears to help.

(somehow regression from commit 723728cd358693b8f4bc9d913541aa4479f2bd48)

Change-Id: I421525269948aa58888f5302e6db234c89230f00
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/152485
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/core/layout/fly.cxx
@@ -1822,8 +1822,12 @@ void CalcContent( SwLayoutFrame *pLay, bool bNoColl )
             }
             if ( pFrame->IsTabFrame() )
             {
-                if ( static_cast<SwTabFrame*>(pFrame)->IsFollow() )
+                if (static_cast<SwTabFrame*>(pFrame)->m_bLockBackMove)
+                {
+                    assert(static_cast<SwTabFrame*>(pFrame)->IsFollow());
                     static_cast<SwTabFrame*>(pFrame)->m_bLockBackMove = false;
+                    pFrame->InvalidatePos();
+                }
             }
 
             pFrame = bPrevInvalid ? pTmpPrev : pFrame->FindNext();

sw/qa/extras/layout/layout.cxx
@@ -1145,6 +1145,71 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testRedlineFlysInHeader)
     }
 }
 
+#if !defined(MACOSX)
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter, TestTdf150606)
+{
+    createSwDoc("tdf150606-1-min.odt");
+
+    SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument*>(mxComponent.get());
+    CPPUNIT_ASSERT(pTextDoc);
+    SwWrtShell* pWrtShell = pTextDoc->GetDocShell()->GetWrtShell();
+
+    xmlDocUniquePtr pXmlDoc = parseLayoutDump();
+
+    assertXPath(pXmlDoc, "/root/page[1]/body/section/column[1]/body/tab", 1);
+    assertXPath(pXmlDoc, "/root/page[1]/body/section/column[2]/body/tab", 1);
+    assertXPath(pXmlDoc, "/root/page[1]/body/section/column", 2);
+    assertXPath(pXmlDoc, "/root/page[2]/body/section/column[1]/body/tab", 1);
+    assertXPath(pXmlDoc, "/root/page[2]/body/section/column[2]/body/tab", 1);
+    assertXPath(pXmlDoc, "/root/page[2]/body/section/column", 2);
+    assertXPath(pXmlDoc, "/root/page[3]/body/section/column[1]/body/tab", 1);
+    assertXPath(pXmlDoc, "/root/page[3]/body/section/column[2]/body/tab", 1);
+    assertXPath(pXmlDoc, "/root/page[3]/body/section/column", 2);
+    assertXPath(pXmlDoc, "/root/page[4]/body/section/column[1]/body/tab", 1);
+    assertXPath(pXmlDoc, "/root/page[4]/body/section/column[2]/body/tab", 1);
+    assertXPath(pXmlDoc, "/root/page[4]/body/section/column", 2);
+    // on page 5 the table is split across balanced columns
+    assertXPath(pXmlDoc, "/root/page[5]/body/section/column[1]/body/tab", 1);
+    assertXPath(pXmlDoc, "/root/page[5]/body/section/column[2]/body/tab", 1);
+    assertXPath(pXmlDoc, "/root/page[5]/body/section/column", 2);
+    assertXPath(pXmlDoc, "/root/page", 5);
+
+    pWrtShell->Down(false, 1);
+    dispatchCommand(mxComponent, ".uno:DeleteTable", {});
+
+    discardDumpedLayout();
+    pXmlDoc = parseLayoutDump();
+
+    assertXPath(pXmlDoc, "/root/page[1]/body/section/column/body/tab", 0);
+    assertXPath(pXmlDoc, "/root/page", 1);
+
+    pWrtShell->Undo();
+    Scheduler::ProcessEventsToIdle();
+
+    discardDumpedLayout();
+    pXmlDoc = parseLayoutDump();
+
+    assertXPath(pXmlDoc, "/root/page[1]/body/section/column[1]/body/tab", 1);
+    assertXPath(pXmlDoc, "/root/page[1]/body/section/column[2]/body/tab", 1);
+    assertXPath(pXmlDoc, "/root/page[1]/body/section/column", 2);
+    assertXPath(pXmlDoc, "/root/page[2]/body/section/column[1]/body/tab", 1);
+    assertXPath(pXmlDoc, "/root/page[2]/body/section/column[2]/body/tab", 1);
+    assertXPath(pXmlDoc, "/root/page[2]/body/section/column", 2);
+    assertXPath(pXmlDoc, "/root/page[3]/body/section/column[1]/body/tab", 1);
+    assertXPath(pXmlDoc, "/root/page[3]/body/section/column[2]/body/tab", 1);
+    assertXPath(pXmlDoc, "/root/page[3]/body/section/column", 2);
+    assertXPath(pXmlDoc, "/root/page[4]/body/section/column[1]/body/tab", 1);
+    assertXPath(pXmlDoc, "/root/page[4]/body/section/column[2]/body/tab", 1);
+    assertXPath(pXmlDoc, "/root/page[4]/body/section/column", 2);
+    // on page 5 the table is split across balanced columns
+    // (problem was that there were empty pages and table was on page 10)
+    assertXPath(pXmlDoc, "/root/page[5]/body/section/column[1]/body/tab", 1);
+    assertXPath(pXmlDoc, "/root/page[5]/body/section/column[2]/body/tab", 1);
+    assertXPath(pXmlDoc, "/root/page[5]/body/section/column", 2);
+    assertXPath(pXmlDoc, "/root/page", 5);
+}
+#endif
+
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter, TestTdf137025)
 {
     // Check the padding of the textbox

commit a9b19f78f3cdcbf5c949a85b45877e903114cc54

tdf#156551 tdf#150606 sw: layout: only invalidate SwTabFrame if it...
... wants to move back.

In this document, the table 1709 splits across 3 columns of the section
1702 with 1 row each, and when its follows are invalidated in
CalcContent(), the later SwTabFrame::MakeAll() of the 1st follow joins
the 2nd follow and invalidates the size of the section frame, causing a
loop.

So only invalidate if that actually looks necessary.

(regression from commit 59987d3c77ec7dbf59fbea9f47cc226f4e8903f9)

Change-Id: I360c8f697a7666a19a08d8ebcabbcfa3a7cdf844
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/155452
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/core/inc/tabfrm.hxx
@@ -61,6 +61,7 @@ class SW_DLLPUBLIC SwTabFrame final: public SwLayoutFrame, public SwFlowFrame
     bool m_bCalcLowers      :1; /// For stability of the content in MakeAll
     bool m_bLowersFormatted :1; /// Communication between MakeAll and Layact
     bool m_bLockBackMove    :1; /// The Master took care of the BackMove test
+    bool m_bWantBackMove    :1; /// Table wants to move back but was locked
     bool m_bResizeHTMLTable :1; /// Call the Resize of the HTMLTableLayout in the MakeAll
                                 /// This is an optimization, so that we don't have to call
                                 /// it in ContentFrame::Grow; there it might be called for

sw/source/core/layout/fly.cxx
@@ -1843,7 +1843,12 @@ void CalcContent( SwLayoutFrame *pLay, bool bNoColl )
                 {
                     assert(static_cast<SwTabFrame*>(pFrame)->IsFollow());
                     static_cast<SwTabFrame*>(pFrame)->m_bLockBackMove = false;
-                    pFrame->InvalidatePos();
+                    // tdf#150606 encourage it to move back in FormatLayout()
+                    if (static_cast<SwTabFrame*>(pFrame)->m_bWantBackMove)
+                    {
+                        static_cast<SwTabFrame*>(pFrame)->m_bWantBackMove = false;
+                        pFrame->InvalidatePos();
+                    }
                 }
             }
 

sw/source/core/layout/tabfrm.cxx
@@ -79,6 +79,7 @@ SwTabFrame::SwTabFrame( SwTable &rTab, SwFrame* pSib )
     , m_bCalcLowers(false)
     , m_bLowersFormatted(false)
     , m_bLockBackMove(false)
+    , m_bWantBackMove(false)
     , m_bResizeHTMLTable(false)
     , m_bONECalcLowers(false)
     , m_bHasFollowFlowLine(false)
@@ -124,6 +125,7 @@ SwTabFrame::SwTabFrame( SwTabFrame &rTab )
     , m_bCalcLowers(false)
     , m_bLowersFormatted(false)
     , m_bLockBackMove(false)
+    , m_bWantBackMove(false)
     , m_bResizeHTMLTable(false)
     , m_bONECalcLowers(false)
     , m_bHasFollowFlowLine(false)
@@ -3803,9 +3805,17 @@ bool SwTabFrame::ShouldBwdMoved( SwLayoutFrame *pNewUpper, bool &rReformat )
             }
             else if (!m_bLockBackMove)
                 bMoveAnyway = true;
+            else
+            {
+                m_bWantBackMove = true;
+            }
         }
         else if (!m_bLockBackMove)
             bMoveAnyway = true;
+        else
+        {
+            m_bWantBackMove = true;
+        }
 
         if ( bMoveAnyway )
         {
@@ -3818,7 +3828,7 @@ bool SwTabFrame::ShouldBwdMoved( SwLayoutFrame *pNewUpper, bool &rReformat )
             // This frame fits into pNewUpper in case it has no space, but this
             // frame is empty.
             bFits = nSpace >= 0;
-        if (!m_bLockBackMove && bFits)
+        if (bFits)
         {
             // #i26945# - check, if follow flow line
             // contains frame, which are moved forward due to its object
@@ -3837,7 +3847,17 @@ bool SwTabFrame::ShouldBwdMoved( SwLayoutFrame *pNewUpper, bool &rReformat )
             // 'return nHeight <= nSpace' to 'return nTmpHeight < nSpace'.
             // This obviously results in problems with table frames in
             // sections. Remember: Every twip is sacred.
-            return nTmpHeight <= nSpace;
+            if (nTmpHeight <= nSpace)
+            {
+                if (m_bLockBackMove)
+                {
+                    m_bWantBackMove = true;
+                }
+                else
+                {
+                    return true;
+                }
+            }
         }
     }
     return false;