LibreOffice/core

commit 99f43923b66a98b75c78a50577f19293aa480998

sw: fix divide by 0
See https://crashreport.libreoffice.org/stats/signature/operator/(Fraction%20const%20&,Fraction%20const%20&)

Change-Id: Ia93e2969d6eb0bde71c8419f2aa90bb7aa231f61
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/150553
Reviewed-by: Caolán McNamara <caolanm@redhat.com>
Tested-by: Jenkins
Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
(cherry picked from commit d01dae0cbabc27f2ff2a242316206067cff73cf8)
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/150563
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/uibase/wrtsh/wrtsh1.cxx
@@ -900,8 +900,13 @@ void SwWrtShell::CalcAndSetScale( svt::EmbeddedObjectRef& xObj,
     }
     else
     {
-        aArea.Width ( tools::Long( aArea.Width()  / pCli->GetScaleWidth() ) );
-        aArea.Height( tools::Long( aArea.Height() / pCli->GetScaleHeight() ) );
+        tools::Long nWidth(pCli->GetScaleWidth());
+        tools::Long nHeight(pCli->GetScaleHeight());
+        if (nWidth && nHeight)
+        {
+            aArea.Width ( aArea.Width()  / nWidth );
+            aArea.Height( aArea.Height() / nHeight );
+        }
     }
 
     pCli->SetObjAreaAndScale( aArea.SVRect(), aScaleWidth, aScaleHeight );

commit fc5aae69934000d81492668b3a40889bba5d3099

tdf#156270: use double for width/height
Otherwise 0.313159 is rounded to 0

Regression from
    commit 99f43923b66a98b75c78a50577f19293aa480998
    Author: Xisco Fauli <xiscofauli@libreoffice.org>
    Date:   Tue Apr 18 14:37:32 2023 +0200

    sw: fix divide by 0

Change-Id: Ib8bf0a770834e0ff73d215c27788df7a3b98404d
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/155561
Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
Tested-by: Xisco Fauli <xiscofauli@libreoffice.org>
Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

sw/source/uibase/wrtsh/wrtsh1.cxx
@@ -915,8 +915,8 @@ void SwWrtShell::CalcAndSetScale( svt::EmbeddedObjectRef& xObj,
     }
     else
     {
-        tools::Long nWidth(pCli->GetScaleWidth());
-        tools::Long nHeight(pCli->GetScaleHeight());
+        double nWidth(pCli->GetScaleWidth());
+        double nHeight(pCli->GetScaleHeight());
         if (nWidth && nHeight)
         {
             aArea.Width ( aArea.Width()  / nWidth );