LibreOffice/core

commit 7c66fc45239d2589e90fd694d54b3ff826b1bd15

use internal iterator for SfxBroadcaster
So we can avoid exposing the internal listener vector.
(which allows further optimisations)

Change-Id: If288141a37314dcc01d203029dc51c71ec2b7f54
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/152857
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svl/source/notify/SfxBroadcaster.cxx
@@ -131,16 +131,33 @@ void SfxBroadcaster::RemoveListener(SfxListener& rListener)
     m_RemovedPositions.push_back(positionOfRemovedElement);
 }
 
-bool SfxBroadcaster::HasListeners() const { return (GetListenerCount() != 0); }
+void SfxBroadcaster::ForAllListeners(std::function<bool(SfxListener*)> f) const
+{
+    for (size_t i = 0; i < m_Listeners.size(); ++i)
+    {
+        SfxListener* const pListener = m_Listeners[i];
+        if (pListener)
+            f(pListener);
+    }
+}
+
+std::vector<SfxListener*> SfxBroadcaster::GetListenersForUnitTest() const
+{
+    std::vector<SfxListener*> rv;
+    for (size_t i = 0; i < m_Listeners.size(); ++i)
+    {
+        SfxListener* const pListener = m_Listeners[i];
+        if (pListener)
+            rv.push_back(pListener);
+    }
+    return rv;
+}
+
+bool SfxBroadcaster::HasListeners() const { return GetListenerCount() != 0; }
 
 size_t SfxBroadcaster::GetListenerCount() const
 {
-    assert(m_Listeners.size() >= m_RemovedPositions.size());
     return m_Listeners.size() - m_RemovedPositions.size();
 }
-
-size_t SfxBroadcaster::GetSizeOfVector() const { return m_Listeners.size(); }
-
-SfxListener* SfxBroadcaster::GetListener(size_t nNo) const { return m_Listeners[nNo]; }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

include/svl/SfxBroadcaster.hxx
@@ -21,6 +21,7 @@
 
 #include <svl/svldllapi.h>
 #include <vector>
+#include <functional>
 
 class SfxListener;
 class SfxHint;
@@ -52,15 +53,13 @@ public:
     /** Get the number of listeners which are registered at this broadcaster */
     size_t                  GetListenerCount() const;
 
-    /** Get the size of the internally stored vector.
-     * Use it to iterate over all listeners.
-     */
-    size_t GetSizeOfVector() const;
+    /** Iterate over all the listeners and call the passed function.
+        return true to break the loop.  */
+    void  ForAllListeners(std::function<bool(SfxListener*)> f) const;
 
-    /** Get a listener by its position in the internally stored vector.
-     * Note that this method may return NULL
+    /** Get a vector of the current listeners - used by unit test code.
      */
-    SfxListener* GetListener( size_t nNo ) const;
+    std::vector<SfxListener*> GetListenersForUnitTest() const;
 
     friend class SfxListener;
     friend class ::SfxBroadcasterTest;

include/svx/svdviter.hxx
@@ -20,6 +20,7 @@
 #pragma once
 
 #include <svx/svxdllapi.h>
+#include <functional>
 
 /*
 
@@ -50,34 +51,15 @@ Not considered are:
 
 */
 
-class OutputDevice;
 class SdrView;
-class SdrPageView;
 class SdrModel;
 class SdrPage;
 class SdrObject;
-class SdrLayerIDSet;
 
-class SVXCORE_DLLPUBLIC SdrViewIter
+namespace SdrViewIter
 {
-    const SdrModel* mpModel;
-    const SdrPage* mpPage;
-    const SdrObject* mpObject;
-    SdrView* mpCurrentView;
-
-    size_t mnListenerNum;
-
-private:
-    SVX_DLLPRIVATE void          ImpInitVars();
-    SVX_DLLPRIVATE SdrView*      ImpFindView();
-    SVX_DLLPRIVATE bool          ImpCheckPageView(SdrPageView const * pPV) const;
-
-public:
-    SdrViewIter(const SdrPage* pPage);
-    SdrViewIter(const SdrObject* pObject);
-
-    SdrView* FirstView();
-    SdrView* NextView();
+    SVXCORE_DLLPUBLIC void ForAllViews(const SdrPage* pPage, std::function<void(SdrView*)> f);
+    SVXCORE_DLLPUBLIC void ForAllViews(const SdrObject* pObject, std::function<void(SdrView*)> f);
 };
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sc/source/core/data/stlsheet.cxx
@@ -297,18 +297,18 @@ bool ScStyleSheet::IsUsed() const
         }
         case SfxStyleFamily::Frame:
         {
-            const size_t nListenerCount = GetSizeOfVector();
-            for (size_t n = 0; n < nListenerCount; ++n)
-            {
-                auto pUser(dynamic_cast<svl::StyleSheetUser*>(GetListener(n)));
-                if (pUser && pUser->isUsedByModel())
+            ForAllListeners([this] (SfxListener* pListener)
                 {
-                    eUsage = Usage::USED;
-                    break;
-                }
-                else
-                    eUsage = Usage::NOTUSED;
-            }
+                    auto pUser(dynamic_cast<svl::StyleSheetUser*>(pListener));
+                    if (pUser && pUser->isUsedByModel())
+                    {
+                        eUsage = Usage::USED;
+                        return true; // break loop
+                    }
+                    else
+                        eUsage = Usage::NOTUSED;
+                    return false;
+                });
             return eUsage == Usage::USED;
         }
         default:

sd/source/core/stlpool.cxx
@@ -1369,15 +1369,16 @@ SdStyleSheetVector SdStyleSheetPool::CreateChildList( SdStyleSheet const * pShee
 {
     SdStyleSheetVector aResult;
 
-    const size_t nListenerCount = pSheet->GetSizeOfVector();
-    for (size_t n = 0; n < nListenerCount; ++n)
-    {
-        SdStyleSheet* pChild = dynamic_cast< SdStyleSheet* >( pSheet->GetListener(n) );
-        if(pChild && pChild->GetParent() == pSheet->GetName())
+    pSheet->ForAllListeners(
+        [&pSheet, &aResult] (SfxListener* pListener)
         {
-            aResult.emplace_back( pChild );
-        }
-    }
+            SdStyleSheet* pChild = dynamic_cast< SdStyleSheet* >( pListener );
+            if(pChild && pChild->GetParent() == pSheet->GetName())
+            {
+                aResult.emplace_back( pChild );
+            }
+            return false;
+        });
 
     return aResult;
 }

sd/source/core/stlsheet.cxx
@@ -282,19 +282,19 @@ bool SdStyleSheet::IsUsed() const
 {
     bool bResult = false;
 
-    const size_t nListenerCount = GetSizeOfVector();
-    for (size_t n = 0; n < nListenerCount; ++n)
-    {
-        SfxListener* pListener = GetListener(n);
-        if( pListener == this )
-            continue;
+    ForAllListeners(
+        [this, &bResult] (SfxListener* pListener)
+        {
+            if( pListener == this )
+                return false; // continue
 
-        const svl::StyleSheetUser* const pUser(dynamic_cast<svl::StyleSheetUser*>(pListener));
-        if (pUser)
-            bResult = pUser->isUsedByModel();
-        if (bResult)
-            break;
-    }
+            const svl::StyleSheetUser* const pUser(dynamic_cast<svl::StyleSheetUser*>(pListener));
+            if (pUser)
+                bResult = pUser->isUsedByModel();
+            if (bResult)
+                return true; // break loop
+            return false;
+        });
 
     if( !bResult )
     {
@@ -335,15 +335,19 @@ bool SdStyleSheet::IsEditable()
     if (!IsUserDefined())
         return false;
 
-    const size_t nListenerCount = GetSizeOfVector();
-    for (size_t n = 0; n < nListenerCount; ++n)
-    {
-        SfxListener* pListener = GetListener(n);
-        if (pListener == this)
-            continue;
-        if (dynamic_cast<SdStyleSheet*>(pListener))
+    bool bFoundOne = false;
+    ForAllListeners(
+        [this, &bFoundOne] (SfxListener* pListener)
+        {
+            if (pListener != this && dynamic_cast<SdStyleSheet*>(pListener))
+            {
+                bFoundOne = true;
+                return true; // break loop
+            }
             return false;
-    }
+        });
+    if (bFoundOne)
+        return false;
 
     std::unique_lock aGuard(m_aMutex);
     return maModifyListeners.getLength(aGuard) <= 1;

sd/source/ui/view/unmodpg.cxx
@@ -83,15 +83,12 @@ void ModifyPageUndoAction::Undo()
 {
     // invalidate Selection, there could be objects deleted in this UNDO
     // which are no longer allowed to be selected then.
-    SdrViewIter aIter(mpPage);
-    SdrView* pView = aIter.FirstView();
-
-    while(pView)
-    {
-        if(pView->AreObjectsMarked())
-            pView->UnmarkAll();
-        pView = aIter.NextView();
-    }
+    SdrViewIter::ForAllViews(mpPage,
+        [] (SdrView* pView)
+        {
+            if(pView->AreObjectsMarked())
+                pView->UnmarkAll();
+        });
 
     mpPage->SetAutoLayout( meOldAutoLayout );
 
@@ -130,15 +127,12 @@ void ModifyPageUndoAction::Redo()
 {
     // invalidate Selection, there could be objects deleted in this UNDO
     // which are no longer allowed to be selected then.
-    SdrViewIter aIter(mpPage);
-    SdrView* pView = aIter.FirstView();
-
-    while(pView)
-    {
-        if(pView->AreObjectsMarked())
-            pView->UnmarkAll();
-        pView = aIter.NextView();
-    }
+    SdrViewIter::ForAllViews(mpPage,
+        [] (SdrView* pView)
+        {
+            if(pView->AreObjectsMarked())
+                pView->UnmarkAll();
+        });
 
     mpPage->meAutoLayout = meNewAutoLayout;
 

svx/source/svdraw/svdedtv.cxx
@@ -1060,16 +1060,14 @@ bool SdrEditView::IsUndoEnabled() const
 
 void SdrEditView::EndTextEditAllViews() const
 {
-    size_t nViews = GetModel().GetListenerCount();
-    for (size_t nView = 0; nView < nViews; ++nView)
-    {
-        SdrObjEditView* pView = dynamic_cast<SdrObjEditView*>(GetModel().GetListener(nView));
-        if (!pView)
-            continue;
-
-        if (pView->IsTextEdit())
-            pView->SdrEndTextEdit();
-    }
+    GetModel().ForAllListeners(
+        [](SfxListener* pListener)
+        {
+            SdrObjEditView* pView = dynamic_cast<SdrObjEditView*>(pListener);
+            if (pView && pView->IsTextEdit())
+                pView->SdrEndTextEdit();
+            return false;
+        });
 }
 
 void SdrEditView::EndTextEditCurrentView(bool bDontDeleteReally)

commit ab4de498475930ba829a23262f82e32df36f1ea2

fix SfxBroadcaster::ForAllListeners
regression from
    commit 7c66fc45239d2589e90fd694d54b3ff826b1bd15
    Author: Noel Grandin <noel.grandin@collabora.co.uk>
    Date:   Thu Jun 1 14:22:57 2023 +0200
    use internal iterator for SfxBroadcaster

Change-Id: Ibd3abf23337c8fb0937d245474f2b89c8936a3ff
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/155589
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svl/source/notify/SfxBroadcaster.cxx
@@ -137,7 +137,8 @@ void SfxBroadcaster::ForAllListeners(std::function<bool(SfxListener*)> f) const
     {
         SfxListener* const pListener = m_Listeners[i];
         if (pListener)
-            f(pListener);
+            if (f(pListener))
+                break;
     }
 }
 