LibreOffice/core

commit 209dce614c43f63f63f5b42a746665c0ec1cbfe3

sw: fix ODT import of paragraph marker formatting
The DOCX bugdoc had a numbering portion which was not bold, even if all
characters in the paragrpah was bold. This was rendered fine, but once
it was saved to ODT + reloaded, the numbering portion became bold as
well, which is buggy.

What happens here is that there is one span that covers the entire
paragraph (and is bold) and there is an empty span at paragraph end
(which is not bold), so the ODT export is fine. But once we import it
back, the first span gets "upgraded" to paragraph-level formatting
(because SetAttrMode::NOFORMATATTR is not used when inserting the hints)
and the second span is not mapped back to the original
RES_PARATR_LIST_AUTOFMT in the text node.

Fix the problem by 1) improving SwXTextCursor::setPropertyValue() to
work with SetAttrMode::NOFORMATATTR when multiple spans are inserted and
by 2) extending SwUnoCursorHelper::SetCursorPropertyValue() to create
RES_PARATR_LIST_AUTOFMT for empty spans at paragraph end. This way the
original doc model and the one created after ODT export + import is much
closer to each other.

This builds on top of 6249858a8972aef077e0249bd93cfe8f01bce4d6 (sw: ODT
import/export of DOCX's paragraph marker formatting, 2022-12-19),
previously the ODT export and import of paragraph marker formatting was
completely missing.

(cherry picked from commit 1feb1aa08421f9d0934ab65ce94cf6054818c0f3)

Also includes commit de235fe13a2e5a4db043f44e6d5636e308f2b979 (sw layout
xml dump: show numbering portion weight, 2022-12-19).

Change-Id: I139e11217dcbc18744aeeb80638090781aa74933
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/144602
Tested-by: Jenkins
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

xmloff/source/text/txtparai.cxx
@@ -1821,6 +1821,19 @@ void XMLParaContext::endFastElement(sal_Int32 )
 
     if (m_xHints)
     {
+        bool bSetNoFormatAttr = false;
+        uno::Reference<beans::XPropertySet> xCursorProps(xAttrCursor, uno::UNO_QUERY);
+        if (m_xHints->GetHints().size() > 1)
+        {
+            // We have multiple hints, then make try to ask the cursor to not upgrade our character
+            // attributes to paragraph-level formatting, which would lead to incorrect rendering.
+            uno::Reference<beans::XPropertySetInfo> xCursorPropsInfo = xCursorProps->getPropertySetInfo();
+            bSetNoFormatAttr = xCursorPropsInfo->hasPropertyByName("NoFormatAttr");
+        }
+        if (bSetNoFormatAttr)
+        {
+            xCursorProps->setPropertyValue("NoFormatAttr", uno::Any(true));
+        }
         for (const auto & i : m_xHints->GetHints())
         {
             XMLHint_Impl *const pHint = i.get();
@@ -1959,6 +1972,10 @@ void XMLParaContext::endFastElement(sal_Int32 )
                 SAL_WARN( "xmloff.text", "What's this" );
                 break;
             }
+        }
+        if (bSetNoFormatAttr)
+        {
+            xCursorProps->setPropertyValue("NoFormatAttr", uno::Any(false));
         }
     }
     m_xHints.reset();

sw/inc/unoprnms.hxx
@@ -531,6 +531,7 @@ inline constexpr OUStringLiteral UNO_NAME_IS_SKIP_HIDDEN_TEXT = u"IsSkipHiddenTe
 inline constexpr OUStringLiteral UNO_NAME_IS_SKIP_PROTECTED_TEXT = u"IsSkipProtectedText";
 inline constexpr OUStringLiteral UNO_NAME_RESET_PARAGRAPH_LIST_ATTRIBUTES
     = u"ResetParagraphListAttributes";
+inline constexpr OUStringLiteral UNO_NAME_NO_FORMAT_ATTR = u"NoFormatAttr";
 inline constexpr OUStringLiteral UNO_NAME_DOCUMENT_INDEX_MARKS = u"DocumentIndexMarks";
 inline constexpr OUStringLiteral UNO_NAME_FOOTNOTE_IS_COLLECT_AT_TEXT_END
     = u"FootnoteIsCollectAtTextEnd";

sw/inc/unotextcursor.hxx
@@ -76,6 +76,7 @@ private:
     const CursorType            m_eType;
     const css::uno::Reference< css::text::XText > m_xParentText;
     sw::UnoCursorPointer m_pUnoCursor;
+    SetAttrMode m_nAttrMode = SetAttrMode::DEFAULT;
 
     SwUnoCursor& GetCursorOrThrow() {
         if(!m_pUnoCursor)

sw/qa/core/unocore/unocore.cxx
@@ -877,6 +877,25 @@ CPPUNIT_TEST_FIXTURE(SwCoreUnocoreTest, testParagraphMarkerODFExport)
         getXPath(pXmlDoc, "//SwParaPortion/SwLineLayout/SwFieldPortion", "font-color"));
 }
 
+CPPUNIT_TEST_FIXTURE(SwCoreUnocoreTest, testParagraphMarkerFormattedRun)
+{
+    // Given a document with a bold run and non-bold paragraph marker:
+    createSwDoc("paragraph-marker-formatted-run.docx");
+
+    // When saving that as ODT + reload:
+    reload("writer8", nullptr);
+
+    // Then make sure that the numbering portion is still non-bold, matching Word:
+    xmlDocUniquePtr pXmlDoc = parseLayoutDump();
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: normal
+    // - Actual  : bold
+    // i.e. the numbering portion was bold, while its weight should be normal.
+    CPPUNIT_ASSERT_EQUAL(
+        OUString("normal"),
+        getXPath(pXmlDoc, "//SwParaPortion/SwLineLayout/SwFieldPortion", "font-weight"));
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sw/source/core/text/porfld.cxx
@@ -481,6 +481,11 @@ void SwFieldPortion::dumpAsXml(xmlTextWriterPtr pWriter, const OUString& rText,
         (void)xmlTextWriterWriteFormatAttribute(pWriter, BAD_CAST("font-color"), "%08" SAL_PRIxUINT32, sal_uInt32(m_pFont->GetColor()));
         (void)xmlTextWriterWriteAttribute(pWriter, BAD_CAST("font-height"), BAD_CAST(OString::number(m_pFont->GetSize(m_pFont->GetActual()).Height()).getStr()));
         (void)xmlTextWriterWriteAttribute(pWriter, BAD_CAST("font-width"), BAD_CAST(OString::number(m_pFont->GetSize(m_pFont->GetActual()).Width()).getStr()));
+        {
+            std::stringstream ss;
+            ss << m_pFont->GetWeight();
+            (void)xmlTextWriterWriteAttribute(pWriter, BAD_CAST("font-weight"), BAD_CAST(ss.str().c_str()));
+        }
     }
 
     (void)xmlTextWriterEndElement(pWriter);

sw/source/core/text/xmldump.cxx
@@ -509,6 +509,11 @@ void SwFont::dumpAsXml(xmlTextWriterPtr writer) const
     (void)xmlTextWriterWriteFormatAttribute(writer, BAD_CAST("ptr"), "%p", this);
     // do not use Color::AsRGBHexString() as that omits the transparency
     (void)xmlTextWriterWriteFormatAttribute(writer, BAD_CAST("color"), "%08" SAL_PRIxUINT32, sal_uInt32(GetColor()));
+    {
+        std::stringstream ss;
+        ss << GetWeight();
+        (void)xmlTextWriterWriteAttribute(writer, BAD_CAST("weight"), BAD_CAST(ss.str().c_str()));
+    }
     (void)xmlTextWriterEndElement(writer);
 }
 

sw/source/core/unocore/unoobj.cxx
@@ -230,6 +230,57 @@ lcl_setAutoStyle(IStyleAccess & rStyleAccess, const uno::Any & rValue,
     rSet.Put(aFormat);
 };
 
+/// Tries to map rValue to RES_PARATR_LIST_AUTOFMT on the current paragraph, returns true on
+/// success.
+static bool lcl_setListAutoStyle(SwPaM& rPam, const uno::Any& rValue, SfxItemSet& rItemSet)
+{
+    // See if this is an empty range at the end of a paragraph.
+    if (rPam.Start()->GetNodeIndex() != rPam.End()->GetNodeIndex())
+    {
+        return false;
+    }
+
+    if (rPam.Start()->GetContentIndex() != rPam.End()->GetContentIndex())
+    {
+        return false;
+    }
+
+    SwTextNode* pTextNode = rPam.GetPointNode().GetTextNode();
+    if (!pTextNode)
+    {
+        return false;
+    }
+
+    if (rPam.Start()->GetContentIndex() != pTextNode->Len())
+    {
+        return false;
+    }
+
+    // Look up the style content based on the name.
+    OUString sStyle;
+    if (!(rValue >>= sStyle))
+    {
+        return false;
+    }
+
+    IStyleAccess& rStyleAccess = rPam.GetDoc().GetIStyleAccess();
+    std::shared_ptr<SfxItemSet> pStyle
+        = rStyleAccess.getByName(sStyle, IStyleAccess::AUTO_STYLE_CHAR);
+    if (!pStyle)
+    {
+        return false;
+    }
+
+    // Set the style on the text node.
+    SwFormatAutoFormat aItem(RES_PARATR_LIST_AUTOFMT);
+    aItem.SetStyleHandle(pStyle);
+    pTextNode->SetAttr(aItem);
+    // Clear the style from the hints array. Without clearing, it would contain some style which
+    // happened to be there previously.
+    rItemSet.ClearItem(RES_TXTATR_AUTOFMT);
+    return true;
+}
+
 void
 SwUnoCursorHelper::SetTextFormatColl(const uno::Any & rAny, SwPaM & rPaM)
 {
@@ -448,6 +499,11 @@ SwUnoCursorHelper::SetCursorPropertyValue(
             lcl_setCharStyle(rPam.GetDoc(), rValue, rItemSet);
         break;
         case RES_TXTATR_AUTOFMT:
+            if (lcl_setListAutoStyle(rPam, rValue, rItemSet))
+            {
+                break;
+            }
+
             lcl_setAutoStyle(rPam.GetDoc().GetIStyleAccess(),
                     rValue, rItemSet, false);
         break;
@@ -2016,7 +2072,8 @@ SwUnoCursorHelper::GetPropertyStates(
         if(!pEntry)
         {
             if (pNames[i] == UNO_NAME_IS_SKIP_HIDDEN_TEXT ||
-                pNames[i] == UNO_NAME_IS_SKIP_PROTECTED_TEXT)
+                pNames[i] == UNO_NAME_IS_SKIP_PROTECTED_TEXT ||
+                pNames[i] == UNO_NAME_NO_FORMAT_ATTR)
             {
                 pStates[i] = beans::PropertyState_DEFAULT_VALUE;
                 continue;
@@ -2209,6 +2266,7 @@ SwXTextCursor::getPropertySetInfo()
         {
             { UNO_NAME_IS_SKIP_HIDDEN_TEXT, FN_SKIP_HIDDEN_TEXT, cppu::UnoType<bool>::get(), PROPERTY_NONE,     0},
             { UNO_NAME_IS_SKIP_PROTECTED_TEXT, FN_SKIP_PROTECTED_TEXT, cppu::UnoType<bool>::get(), PROPERTY_NONE,     0},
+            { UNO_NAME_NO_FORMAT_ATTR, 0, cppu::UnoType<bool>::get(), PROPERTY_NONE,     0},
         };
         const uno::Reference< beans::XPropertySetInfo >  xInfo =
             m_rPropSet.getPropertySetInfo();
@@ -2256,10 +2314,26 @@ SwXTextCursor::setPropertyValue(
             pTextNode->ResetAttr(RES_PARATR_LIST_BEGIN, RES_PARATR_LIST_END);
         }
     }
+    else if (rPropertyName == UNO_NAME_NO_FORMAT_ATTR)
+    {
+        bool bSet(false);
+        if (!(rValue >>= bSet))
+        {
+            throw lang::IllegalArgumentException();
+        }
+        if (bSet)
+        {
+            m_nAttrMode = SetAttrMode::NOFORMATATTR;
+        }
+        else
+        {
+            m_nAttrMode = SetAttrMode::DEFAULT;
+        }
+    }
     else
     {
         SwUnoCursorHelper::SetPropertyValue(rUnoCursor,
-                m_rPropSet, rPropertyName, rValue);
+                m_rPropSet, rPropertyName, rValue, m_nAttrMode);
     }
 }
 
@@ -2572,7 +2646,8 @@ SwXTextCursor::getPropertyDefaults(
             if (!pEntry)
             {
                 if (pNames[i] == UNO_NAME_IS_SKIP_HIDDEN_TEXT ||
-                    pNames[i] == UNO_NAME_IS_SKIP_PROTECTED_TEXT)
+                    pNames[i] == UNO_NAME_IS_SKIP_PROTECTED_TEXT ||
+                    pNames[i] == UNO_NAME_NO_FORMAT_ATTR)
                 {
                     continue;
                 }

commit 2d0f43befdaa1010062b534dffa82adad8c956c8

tdf#156321 ODT import: fix lost char format on outer span elements
Regression from commit 209dce614c43f63f63f5b42a746665c0ec1cbfe3 (sw: fix
ODT import of paragraph marker formatting, 2022-12-20), the bugdoc has a
first paragraph with nested spans (the outer sets the boldness) and the
text does not appear as bold in Writer anymore.

What appears to be the problem is that our model is just a list of spans
for a paragraph, so nesting is converted to a list of spans, but now the
outer span char format is lost.

Fix the problem by making the condition of the 'no upgrade from char
format to para format' more strict: do this in case we have empty spans.
That fixes this new use-case and leaves the original one fixed.

Note that in the long run it may make sense to always disable the
upgrade from char format to para format, but that has to be done in a
way that does not break the import of nested spans.

Change-Id: Ie6fafb3e9649e50790c7ecae8e136e6600f3341e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/155648
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

xmloff/source/text/txtparai.cxx
@@ -34,6 +34,7 @@
 #include <com/sun/star/beans/XPropertySet.hpp>
 #include <com/sun/star/beans/XPropertySetInfo.hpp>
 #include <com/sun/star/text/ControlCharacter.hpp>
+#include <com/sun/star/text/XTextRangeCompare.hpp>
 #include <com/sun/star/container/XIndexReplace.hpp>
 #include <com/sun/star/drawing/XShapes.hpp>
 #include <com/sun/star/container/XEnumerationAccess.hpp>
@@ -1852,9 +1853,28 @@ void XMLParaContext::endFastElement(sal_Int32 )
     {
         bool bSetNoFormatAttr = false;
         uno::Reference<beans::XPropertySet> xCursorProps(xAttrCursor, uno::UNO_QUERY);
-        if (m_xHints->GetHints().size() > 1 || m_aMarkerStyleName.hasValue())
+        int nEmptyHints = 0;
+        uno::Reference<text::XTextRangeCompare> xCompare(xTxtImport->GetText(), uno::UNO_QUERY);
+        if (xCompare.is())
         {
-            // We have multiple hints, then make try to ask the cursor to not upgrade our character
+            try
+            {
+                for (const auto& pHint : m_xHints->GetHints())
+                {
+                    if (xCompare->compareRegionStarts(pHint->GetStart(), pHint->GetEnd()) == 0)
+                    {
+                        ++nEmptyHints;
+                    }
+                }
+            }
+            catch (const uno::Exception&)
+            {
+                TOOLS_WARN_EXCEPTION("xmloff.text", "");
+            }
+        }
+        if (nEmptyHints > 0 || m_aMarkerStyleName.hasValue())
+        {
+            // We have at least one empty hint, then make try to ask the cursor to not upgrade our character
             // attributes to paragraph-level formatting, which would lead to incorrect rendering.
             uno::Reference<beans::XPropertySetInfo> xCursorPropsInfo = xCursorProps->getPropertySetInfo();
             bSetNoFormatAttr = xCursorPropsInfo->hasPropertyByName("NoFormatAttr");

xmloff/qa/unit/text.cxx
@@ -9,6 +9,7 @@
 
 #include <test/unoapixml_test.hxx>
 
+#include <com/sun/star/awt/FontWeight.hpp>
 #include <com/sun/star/beans/XPropertySet.hpp>
 #include <com/sun/star/beans/PropertyValues.hpp>
 #include <com/sun/star/frame/XStorable.hpp>
@@ -1247,6 +1248,31 @@ CPPUNIT_TEST_FIXTURE(XmloffStyleTest, testParagraphScopedTabDistance)
                 "tab-stop-distance", "10cm");
 
     assertXPath(pXmlDoc, "//text:p[@text:style-name='P1']");
+}
+
+CPPUNIT_TEST_FIXTURE(XmloffStyleTest, testNestedSpans)
+{
+    // Given a document with a first paragraph that has a nested span, the outer span setting the
+    // boldness:
+    // When importing that document:
+    loadFromURL(u"nested-spans.odt");
+
+    // Then make sure the text portion is bold, not normal:
+    uno::Reference<text::XTextDocument> xTextDocument(mxComponent, uno::UNO_QUERY);
+    uno::Reference<container::XEnumerationAccess> xParagraphsAccess(xTextDocument->getText(),
+                                                                    uno::UNO_QUERY);
+    uno::Reference<container::XEnumeration> xParagraphs = xParagraphsAccess->createEnumeration();
+    uno::Reference<container::XEnumerationAccess> xParagraph(xParagraphs->nextElement(),
+                                                             uno::UNO_QUERY);
+    uno::Reference<container::XEnumeration> xPortions = xParagraph->createEnumeration();
+    uno::Reference<beans::XPropertySet> xTextPortion(xPortions->nextElement(), uno::UNO_QUERY);
+    float fWeight{};
+    xTextPortion->getPropertyValue("CharWeight") >>= fWeight;
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 150 (awt::FontWeight::BOLD)
+    // - Actual  : 100 (awt::FontWeight::NORMAL)
+    // i.e. the boldness was lost on import.
+    CPPUNIT_ASSERT_EQUAL(awt::FontWeight::BOLD, fWeight);
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();