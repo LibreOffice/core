99a817c3f41a115e53a95ada33f0611b2572725b
Miklos Vajna <vmiklos@collabora.com>
2023-08-18 11:21:25 +0200

    Related: tdf#156351 sw floattable: fix missing top border in follow table
    The problem was that in case a table has a table border but no cell
    border, then the follow table was missing a top border in case the
    follow table had multiple rows.
    
    This was already working for single-row follow tables since commit
    a4da71fb824f2d4ecc7c01f4deb2865ba52f5f4c (INTEGRATION: CWS fmebugs04
    (1.115.46); FILE MERGED 2008/05/13 13:56:19 fme 1.115.46.2: #i9860# Top
    border for tables - correction 2008/05/13 13:49:23 fme 1.115.46.1:
    i#9860 Top border for tables, 2008-06-06).
    
    Fix the problem by checking for the case when the first row in a follow
    table has no own bottom border and then take that bottom border from the
    last row instead, if that has a bottom border.
    
    The reported case around the missing bottom border in the master table
    still needs work.
    
    Change-Id: Ib418da99067a42efcf3bd0e2d6a9467797de619a
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/155824
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Tested-by: Jenkins
    (cherry picked from commit 53798fef2cc0b5b0b9706081a4af5ceca964a41b)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/155788
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

---
+++ sw/qa/core/layout/paintfrm.cxx
@@ -0,0 +1,71 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <swmodeltestbase.hxx>
+
+#include <o3tl/string_view.hxx>
+
+#include <docsh.hxx>
+#include <unotxdoc.hxx>
+
+namespace
+{
+/// Covers sw/source/core/layout/paintfrm.cxx fixes.
+class Test : public SwModelTestBase
+{
+public:
+    Test()
+        : SwModelTestBase("/sw/qa/core/layout/data/")
+    {
+    }
+};
+
+CPPUNIT_TEST_FIXTURE(Test, testSplitTableBorder)
+{
+    // Given a document with a split table, table borders are defined, but cell borders are not:
+    createSwDoc("split-table-border.odt");
+    SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument*>(mxComponent.get());
+    SwDocShell* pShell = pTextDoc->GetDocShell();
+
+    // When rendering that document:
+    std::shared_ptr<GDIMetaFile> xMetaFile = pShell->GetPreviewMetaFile();
+
+    // Then make sure that the follow table has a top border:
+    MetafileXmlDump aDumper;
+    xmlDocUniquePtr pXmlDoc = dumpAndParse(aDumper, *xMetaFile);
+    xmlXPathObjectPtr pXmlObj = getXPathNode(pXmlDoc, "//polyline[@style='solid']/point");
+    xmlNodeSetPtr pXmlNodes = pXmlObj->nodesetval;
+    int nHorizontalBorders = 0;
+    // Count the horizontal borders:
+    for (int i = 0; i < xmlXPathNodeSetGetLength(pXmlNodes); i += 2)
+    {
+        xmlNodePtr pStart = pXmlNodes->nodeTab[i];
+        xmlNodePtr pEnd = pXmlNodes->nodeTab[i + 1];
+        xmlChar* pStartY = xmlGetProp(pStart, BAD_CAST("y"));
+        xmlChar* pEndY = xmlGetProp(pEnd, BAD_CAST("y"));
+        sal_Int32 nStartY = o3tl::toInt32(reinterpret_cast<char const*>(pStartY));
+        sal_Int32 nEndY = o3tl::toInt32(reinterpret_cast<char const*>(pEndY));
+        if (nStartY != nEndY)
+        {
+            // Vertical border.
+            continue;
+        }
+
+        ++nHorizontalBorders;
+    }
+    xmlXPathFreeObject(pXmlObj);
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 3
+    // - Actual  : 2
+    // i.e. the top border in the follow table was missing.
+    CPPUNIT_ASSERT_EQUAL(3, nHorizontalBorders);
+}
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
