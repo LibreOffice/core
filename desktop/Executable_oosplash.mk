LibreOffice/core

commit 4c00a5bce384c5fa3b46cf5bb29c7e9b1e506ea0

Assume that, if we have X11, it supports Xinerama
By now, Xinerama is old enough that we can use the X11 server supports
it

Change-Id: Ida95902916697808c611a53274b1f0299fd298b7
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/154666
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

desktop/Executable_oosplash.mk
@@ -23,7 +23,7 @@ $(eval $(call gb_Executable_add_cobjects,oosplash,\
     desktop/unx/source/start \
 ))
 
-ifneq ($(USE_XINERAMA),)
+ifneq ($(USING_X11),)
 
 $(eval $(call gb_Executable_add_defs,oosplash,\
     -DUSE_XINERAMA \

desktop/unx/source/splashx.c
@@ -16,7 +16,7 @@
 #include <X11/Xatom.h>
 #include <X11/Xutil.h>
 
-#ifdef USE_XINERAMA
+#ifdef USING_X11
 #include <X11/extensions/Xinerama.h>
 #endif
 
@@ -406,7 +406,7 @@ static int splash_init_display( struct splash* splash, int argc, char** argv )
 {
     char *display_name = NULL;
     int i;
-#ifdef USE_XINERAMA
+#ifdef USING_X11
     int n_xinerama_screens = 1;
     XineramaScreenInfo* p_screens = NULL;
 #endif
@@ -442,7 +442,7 @@ static int splash_init_display( struct splash* splash, int argc, char** argv )
     splash->display_x_pos = 0;
     splash->display_y_pos = 0;
 
-#ifdef USE_XINERAMA
+#ifdef USING_X11
     p_screens = XineramaQueryScreens( splash->display, &n_xinerama_screens );
     if( p_screens )
     {

vcl/Library_vclplug_gen.mk
@@ -133,7 +133,7 @@ $(eval $(call gb_Library_add_defs,vclplug_gen,\
 endif
 
 ## handle Xinerama
-ifneq ($(USE_XINERAMA),)
+ifneq ($(USING_X11),)
 $(eval $(call gb_Library_add_defs,vclplug_gen,\
     -DUSE_XINERAMA_XORG \
 ))
@@ -146,7 +146,7 @@ $(eval $(call gb_Library_add_libs,vclplug_gen,\
     -Wl$(COMMA)-Bstatic -lXinerama -Wl$(COMMA)-Bdynamic \
 ))
 endif
-endif # USE_XINERAMA
+endif # USING_X11
 
 ifeq ($(OS),LINUX)
 $(eval $(call gb_Library_add_libs,vclplug_gen,\

config_host.mk.in
@@ -711,7 +711,6 @@ export UNIX_DLAPI_LIBS=@UNIX_DLAPI_LIBS@
 export USE_HEADLESS_CODE=@USE_HEADLESS_CODE@
 export USE_LD=@USE_LD@
 export USE_LIBRARY_BIN_TAR=@USE_LIBRARY_BIN_TAR@
-export USE_XINERAMA=@USE_XINERAMA@
 export UPDATE_CONFIG=@UPDATE_CONFIG@
 export UUIDGEN=@UUIDGEN@
 export VALGRIND_CFLAGS=$(gb_SPACE)@VALGRIND_CFLAGS@

configure.ac
@@ -13572,7 +13572,7 @@ fi
 AC_SUBST(ENABLE_SILENT_MSI)
 
 AC_MSG_CHECKING([whether and how to use Xinerama])
-if test "$_os" = "Linux" -o "$_os" = "FreeBSD"; then
+if test "$USING_X11" = TRUE; then
     if test "$x_libraries" = "default_x_libraries"; then
         XINERAMALIB=`$PKG_CONFIG --variable=libdir xinerama`
         if test "x$XINERAMALIB" = x; then
@@ -13626,14 +13626,13 @@ if test "$_os" = "Linux" -o "$_os" = "FreeBSD"; then
         AC_CHECK_LIB([Xinerama], [XineramaIsActive], [:],
             [AC_MSG_ERROR(Xinerama not functional?)], [$XINERAMA_EXTRA_LIBS])
     else
-        AC_MSG_RESULT([no, libXinerama not found or wrong architecture.])
+        AC_MSG_ERROR([libXinerama not found or wrong architecture.])
     fi
 else
     USE_XINERAMA=
     XINERAMA_LINK=none
     AC_MSG_RESULT([no])
 fi
-AC_SUBST(USE_XINERAMA)
 AC_SUBST(XINERAMA_LINK)
 
 AC_MSG_CHECKING([whether to use non-standard RGBA32 cairo pixel order])

vcl/unx/generic/app/saldisp.cxx
@@ -39,10 +39,7 @@
 #endif
 #include <X11/keysym.h>
 #include <X11/Xatom.h>
-
-#ifdef USE_XINERAMA_XORG
 #include <X11/extensions/Xinerama.h>
-#endif
 
 #include <i18nlangtag/languagetag.hxx>
 #include <tools/debug.hxx>
@@ -2213,7 +2210,6 @@ void SalDisplay::InitXinerama()
         m_bXinerama = false;
         return; // multiple screens mean no xinerama
     }
-#if defined(USE_XINERAMA_XORG)
     if( !XineramaIsActive( pDisp_ ) )
         return;
 
@@ -2236,7 +2232,6 @@ void SalDisplay::InitXinerama()
         m_bXinerama = m_aXineramaScreens.size() > 1;
     }
     XFree( pScreens );
-#endif
 #if OSL_DEBUG_LEVEL > 1
     if( m_bXinerama )
     {

commit 3eee14d4e769ad063e71ccb476f722188a66d50f

tdf#156825 Half of the splash screen is visible on both monitors under Linux
Blind fix, since I don't have two screens.

regression from
    commit 4c00a5bce384c5fa3b46cf5bb29c7e9b1e506ea0
    Author: Noel Grandin <noel.grandin@collabora.co.uk>
    Date:   Thu Jul 20 11:27:17 2023 +0200
    Assume that, if we have X11, it supports Xinerama

It looks like I missed the removal of some USE_XINERAMA pieces

Change-Id: I402db170916efe935d8ff9f96cc9b327070809b7
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/155939
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

desktop/Executable_oosplash.mk
@@ -24,15 +24,9 @@ $(eval $(call gb_Executable_add_cobjects,oosplash,\
 ))
 
 ifneq ($(USING_X11),)
-
-$(eval $(call gb_Executable_add_defs,oosplash,\
-    -DUSE_XINERAMA \
-))
-
 $(eval $(call gb_Executable_add_libs,oosplash,\
     -lXinerama \
 ))
-
 endif
 
 ifneq ($(DISABLE_GUI),TRUE)

desktop/unx/source/splashx.c
@@ -16,9 +16,7 @@
 #include <X11/Xatom.h>
 #include <X11/Xutil.h>
 
-#ifdef USING_X11
 #include <X11/extensions/Xinerama.h>
-#endif
 
 #include <osl/endian.h>
 #include <fcntl.h>
@@ -406,10 +404,8 @@ static int splash_init_display( struct splash* splash, int argc, char** argv )
 {
     char *display_name = NULL;
     int i;
-#ifdef USING_X11
     int n_xinerama_screens = 1;
     XineramaScreenInfo* p_screens = NULL;
-#endif
 
     for ( i = 0; i < argc; i++ )
     {
@@ -442,7 +438,6 @@ static int splash_init_display( struct splash* splash, int argc, char** argv )
     splash->display_x_pos = 0;
     splash->display_y_pos = 0;
 
-#ifdef USING_X11
     p_screens = XineramaQueryScreens( splash->display, &n_xinerama_screens );
     if( p_screens )
     {
@@ -459,7 +454,6 @@ static int splash_init_display( struct splash* splash, int argc, char** argv )
         }
         XFree( p_screens );
     }
-#endif
     return 1;
 }
 
@@ -812,6 +806,6 @@ struct splash* splash_create(rtl_uString* pAppPath, int argc, char** argv)
 }
 
 
-#endif // ENABLE_QUICKSTART_LIBPNG
+#endif // defined(ENABLE_QUICKSTART_LIBPNG) && HAVE_FEATURE_UI
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

solenv/gbuild/PrecompiledHeaders.mk
@@ -149,7 +149,6 @@ gb_PrecompiledHeader_ignore_flags_system := \
 -DSYSTEM_NSS \
 -DHAVE_VALGRIND_HEADERS \
 -DUSE_RANDR \
--DUSE_XINERAMA_XORG \
 -DDISABLE_CVE_TESTS \
 -DCPPUNIT_PLUGIN_EXPORT='extern "C" SAL_DLLPUBLIC_EXPORT' \
 -DOOO_DLLIMPLEMENTATION_TEST \

vcl/Library_vclplug_gen.mk
@@ -134,9 +134,6 @@ endif
 
 ## handle Xinerama
 ifneq ($(USING_X11),)
-$(eval $(call gb_Library_add_defs,vclplug_gen,\
-    -DUSE_XINERAMA_XORG \
-))
 ifeq ($(XINERAMA_LINK),dynamic)
 $(eval $(call gb_Library_add_libs,vclplug_gen,\
     -lXinerama \