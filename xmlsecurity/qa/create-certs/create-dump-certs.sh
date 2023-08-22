a6636c451b54728741be0a770b053e093134b90d
TokieSan <eltokhy@aucegypt.edu>
2023-08-22 09:42:11 +0300

    XMLSecurity Module Documentation Update
    Updated the README Documentation file for xmlsecurity module. Fixed
    broken sxd file for signatures workflow. Added dumb certificates creator
    shell script for testing purposes in qa.
    
    Change-Id: Ibee17193a1a38348f2e7fc460dfdd0c54dd31f9b
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/155932
    Tested-by: Jenkins
    Reviewed-by: Thorsten Behrens <thorsten.behrens@allotropia.de>

---
+++ xmlsecurity/qa/create-certs/create-dump-certs.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# This script generates 100 certificates (modifiable) for cases of testing UI latency for instance.
+#
+# Basically, it creates certificates with sender same as reciever following User [number] pattern.
+#
+
+for ((i=1; i<=100; i++))
+do
+    gpg --batch --gen-key <<EOF
+        %no-protection
+        Key-Type: RSA
+        Key-Length: 2048
+        Subkey-Type: RSA
+        Subkey-Length: 2048
+        Name-Real: User $i
+        Name-Email: user$i@example.com
+        Expire-Date: 1y
+        %commit
+EOF
+
+    gpg --armor --export user$i@example.com > user$i.asc
+
+    echo "Generated certificate for User $i"
+done
