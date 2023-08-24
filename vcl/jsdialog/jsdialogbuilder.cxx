62d5a5c376f06a22fdfd8708d2aefd7e987a4478
Szymon Kłos <szymon.klos@collabora.com>
2023-08-24 12:28:32 +0200

    jsdialog: don't send update if we not change selection
    Change-Id: I3eb1e5df4133f010d0c4f7abde0f73bfeda75a82
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/156046
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/156237
    Tested-by: Jenkins

--- vcl/jsdialog/jsdialogbuilder.cxx
+++ vcl/jsdialog/jsdialogbuilder.cxx
@@ -1655,6 +1655,9 @@ void JSComboBox::set_entry_text(const OUString& rText)
 
 void JSComboBox::set_active(int pos)
 {
+    if (pos == get_active())
+        return;
+
     SalInstanceComboBoxWithEdit::set_active(pos);
     sendUpdate();
 }
