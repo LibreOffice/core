005f1547b8e5b37da243320b2eb7a84a47f4c821
Adam Seskunas <adamseskunas@gmail.com>
2023-08-29 10:40:43 -0700

    tdf#143148 Use pragma once in uibase-app
    Change-Id: I95cf120f9aaf47bb4c588471a8cbdb4c0ceb6c99
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/156262
    Tested-by: Jenkins
    Tested-by: Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>
    Reviewed-by: Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>

--- sw/source/uibase/app/appenv.hxx
+++ sw/source/uibase/app/appenv.hxx
@@ -7,8 +7,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-#ifndef INCLUDED_SW_SOURCE_UIBASE_APP_APPENV_HXX
-#define INCLUDED_SW_SOURCE_UIBASE_APP_APPENV_HXX
+#pragma once
 
 #include <rtl/ustring.hxx>
 
@@ -16,7 +15,5 @@ class SwWrtShell;
 class SwFieldMgr;
 
 OUString InsertLabEnvText(SwWrtShell&, SwFieldMgr&, const OUString&);
-
-#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
