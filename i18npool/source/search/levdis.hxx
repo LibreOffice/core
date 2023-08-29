abbdb9cf2a40132f67cd6e0f2e9ad686ff36798b
Yli875 <qq3202390739@gmail.com>
2023-08-29 02:25:08 -0700

    tdf#143148 Use pragma once instead of include guards
    Change-Id: I84408aa0d48dd7804b70d012c85035e0572fa4e0
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/156256
    Tested-by: Jenkins
    Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

--- i18npool/source/search/levdis.hxx
+++ i18npool/source/search/levdis.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_I18NPOOL_SOURCE_SEARCH_LEVDIS_HXX
-#define INCLUDED_I18NPOOL_SOURCE_SEARCH_LEVDIS_HXX
+#pragma once
 
 #include <sal/types.h>
 #include <memory>
@@ -208,7 +207,5 @@ public:
     }
 };
 
-
-#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
