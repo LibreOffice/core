LibreOffice/core

commit 7b355669c6ddeab2e6cec692d6afdff41c61d0fb

Function to load graphic swapped out (loaded on demand)
When a document is loaded it takes a lot of time and memory to
load the graphic that are in the documet, so avoid that and just
store the compressed graphic into a temporary file (handeled by
GfxLink) and load when we really need to show the graphic.

GraphicObject cached some attributes from Graphic, but this
attributes now aren't available immediately so this attributes
are removed form GraphicObject and now delegate to the Graphic
itself. GetSizeBytes attribute however was removed as it is
only used in some tests.

GfxLink initial values were moved to the constructor and are
not set in the header file anymore (as it is the recommended
way to do it).

The SdImportTest::testDocumentLayout failed as it looks like the
dump sometimes didn't include the width and height of the null
bitmap (which is set to 32x32) of the FillBitmap in some
situations, but then in other situations it did include this
attributes. With this change the width and height are always
included for the FillBitmap which looks like it is more correct.

Change-Id: Ia1218f93b1735402b7828404f65660e2d4acf32f
Reviewed-on: https://gerrit.libreoffice.org/53016
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

vcl/source/gdi/impgraph.cxx
@@ -30,6 +30,7 @@
 #include <unotools/ucbstreamhelper.hxx>
 #include <unotools/tempfile.hxx>
 #include <vcl/outdev.hxx>
+#include <vcl/graphicfilter.hxx>
 #include <vcl/virdev.hxx>
 #include <vcl/gfxlink.hxx>
 #include <vcl/cvtgrf.hxx>
@@ -103,6 +104,8 @@ Size GraphicReader::GetPreviewSize() const
 
 GraphicID::GraphicID(ImpGraphic const & rGraphic)
 {
+    rGraphic.ensureAvailable();
+
     mnID1 = static_cast<sal_uLong>(rGraphic.ImplGetType()) << 28;
     mnID2 = mnID3 = mnID4 = 0;
 
@@ -176,7 +179,8 @@ ImpGraphic::ImpGraphic() :
         mnSizeBytes     ( 0 ),
         mbSwapOut       ( false ),
         mbDummyContext  ( false ),
-        maLastUsed (std::chrono::high_resolution_clock::now())
+        maLastUsed (std::chrono::high_resolution_clock::now()),
+        mbPrepared      ( false )
 {
 }
 
@@ -194,6 +198,7 @@ ImpGraphic::ImpGraphic(const ImpGraphic& rImpGraphic)
     , maPdfData(rImpGraphic.maPdfData)
     , maGraphicExternalLink(rImpGraphic.maGraphicExternalLink)
     , maLastUsed (std::chrono::high_resolution_clock::now())
+    , mbPrepared (rImpGraphic.mbPrepared)
 {
     if( rImpGraphic.mpGfxLink )
         mpGfxLink = o3tl::make_unique<GfxLink>( *rImpGraphic.mpGfxLink );
@@ -221,6 +226,7 @@ ImpGraphic::ImpGraphic(ImpGraphic&& rImpGraphic)
     , maPdfData(std::move(rImpGraphic.maPdfData))
     , maGraphicExternalLink(rImpGraphic.maGraphicExternalLink)
     , maLastUsed (std::chrono::high_resolution_clock::now())
+    , mbPrepared (rImpGraphic.mbPrepared)
 {
     rImpGraphic.ImplClear();
     rImpGraphic.mbDummyContext = false;
@@ -232,7 +238,8 @@ ImpGraphic::ImpGraphic(GraphicExternalLink const & rGraphicExternalLink) :
         mbSwapOut       ( false ),
         mbDummyContext  ( false ),
         maGraphicExternalLink(rGraphicExternalLink),
-        maLastUsed (std::chrono::high_resolution_clock::now())
+        maLastUsed (std::chrono::high_resolution_clock::now()),
+        mbPrepared (false)
 {
 }
 
@@ -242,7 +249,8 @@ ImpGraphic::ImpGraphic( const Bitmap& rBitmap ) :
         mnSizeBytes     ( 0 ),
         mbSwapOut       ( false ),
         mbDummyContext  ( false ),
-        maLastUsed (std::chrono::high_resolution_clock::now())
+        maLastUsed (std::chrono::high_resolution_clock::now()),
+        mbPrepared (false)
 {
 }
 
@@ -252,7 +260,8 @@ ImpGraphic::ImpGraphic( const BitmapEx& rBitmapEx ) :
         mnSizeBytes     ( 0 ),
         mbSwapOut       ( false ),
         mbDummyContext  ( false ),
-        maLastUsed (std::chrono::high_resolution_clock::now())
+        maLastUsed (std::chrono::high_resolution_clock::now()),
+        mbPrepared (false)
 {
 }
 
@@ -262,7 +271,8 @@ ImpGraphic::ImpGraphic(const VectorGraphicDataPtr& rVectorGraphicDataPtr)
     mbSwapOut( false ),
     mbDummyContext  ( false ),
     maVectorGraphicData(rVectorGraphicDataPtr),
-    maLastUsed (std::chrono::high_resolution_clock::now())
+    maLastUsed (std::chrono::high_resolution_clock::now()),
+    mbPrepared (false)
 {
 }
 
@@ -273,7 +283,8 @@ ImpGraphic::ImpGraphic( const Animation& rAnimation ) :
         mnSizeBytes     ( 0 ),
         mbSwapOut       ( false ),
         mbDummyContext  ( false ),
-        maLastUsed (std::chrono::high_resolution_clock::now())
+        maLastUsed (std::chrono::high_resolution_clock::now()),
+        mbPrepared (false)
 {
 }
 
@@ -283,7 +294,8 @@ ImpGraphic::ImpGraphic( const GDIMetaFile& rMtf ) :
         mnSizeBytes     ( 0 ),
         mbSwapOut       ( false ),
         mbDummyContext  ( false ),
-        maLastUsed (std::chrono::high_resolution_clock::now())
+        maLastUsed (std::chrono::high_resolution_clock::now()),
+        mbPrepared (false)
 {
 }
 
@@ -321,6 +333,7 @@ ImpGraphic& ImpGraphic::operator=( const ImpGraphic& rImpGraphic )
 
         mbSwapOut = rImpGraphic.mbSwapOut;
         mpSwapFile = rImpGraphic.mpSwapFile;
+        mbPrepared = rImpGraphic.mbPrepared;
 
         mpGfxLink.reset();
 
@@ -355,6 +368,7 @@ ImpGraphic& ImpGraphic::operator=(ImpGraphic&& rImpGraphic)
     maVectorGraphicData = std::move(rImpGraphic.maVectorGraphicData);
     maPdfData = std::move(rImpGraphic.maPdfData);
     maGraphicExternalLink = rImpGraphic.maGraphicExternalLink;
+    mbPrepared = rImpGraphic.mbPrepared;
 
     rImpGraphic.ImplClear();
     rImpGraphic.mbDummyContext = false;
@@ -371,7 +385,11 @@ bool ImpGraphic::operator==( const ImpGraphic& rImpGraphic ) const
 
     if( this == &rImpGraphic )
         bRet = true;
-    else if( !ImplIsSwapOut() && ( rImpGraphic.meType == meType ) )
+    else if (mbPrepared && rImpGraphic.mbPrepared)
+    {
+        bRet = (*mpGfxLink == *rImpGraphic.mpGfxLink);
+    }
+    else if (isAvailable() && rImpGraphic.isAvailable())
     {
         switch( meType )
         {
@@ -427,16 +445,22 @@ bool ImpGraphic::operator==( const ImpGraphic& rImpGraphic ) const
 
 const VectorGraphicDataPtr& ImpGraphic::getVectorGraphicData() const
 {
+    ensureAvailable();
+
     return maVectorGraphicData;
 }
 
 void ImpGraphic::setPdfData(const uno::Sequence<sal_Int8>& rPdfData)
 {
+    ensureAvailable();
+
     maPdfData = rPdfData;
 }
 
 const uno::Sequence<sal_Int8>& ImpGraphic::getPdfData() const
 {
+    ensureAvailable();
+
     return maPdfData;
 }
 
@@ -446,6 +470,11 @@ void ImpGraphic::ImplCreateSwapInfo()
     {
         maSwapInfo.maPrefMapMode = ImplGetPrefMapMode();
         maSwapInfo.maPrefSize = ImplGetPrefSize();
+        maSwapInfo.mbIsAnimated = ImplIsAnimated();
+        maSwapInfo.mbIsEPS = ImplIsEPS();
+        maSwapInfo.mbIsTransparent = ImplIsTransparent();
+        maSwapInfo.mbIsAlpha = ImplIsAlpha();
+        maSwapInfo.mnAnimationLoopCount = ImplGetAnimationLoopCount();
     }
 }
 
@@ -483,10 +512,37 @@ ImpSwapFile::~ImpSwapFile()
     }
 }
 
+void ImpGraphic::ImplSetPrepared()
+{
+    mbPrepared = true;
+    mbSwapOut = true;
+    meType = GraphicType::Bitmap;
+
+    SvMemoryStream aMemoryStream(const_cast<sal_uInt8*>(mpGfxLink->GetData()), mpGfxLink->GetDataSize(), StreamMode::READ | StreamMode::WRITE);
+
+    GraphicDescriptor aDescriptor(aMemoryStream, nullptr);
+    if (aDescriptor.Detect(true))
+    {
+        maSwapInfo.maPrefSize = aDescriptor.GetSizePixel();
+        maSwapInfo.maPrefMapMode = MapMode(MapUnit::MapPixel);
+    }
+    maSwapInfo.mnAnimationLoopCount = 0;
+    maSwapInfo.mbIsAnimated = false;
+    maSwapInfo.mbIsEPS = false;
+    maSwapInfo.mbIsTransparent = false;
+    maSwapInfo.mbIsAlpha = false;
+
+    if (mpGfxLink->GetType() == GfxLinkType::NativeGif)
+    {
+        maSwapInfo.mbIsAnimated = true;
+    }
+}
+
 void ImpGraphic::ImplClear()
 {
     mpSwapFile.reset();
     mbSwapOut = false;
+    mbPrepared = false;
 
     // cleanup
     ImplClearGraphics();
@@ -512,11 +568,13 @@ bool ImpGraphic::ImplIsTransparent() const
 {
     bool bRet(true);
 
-    ensureAvailable();
-
-    if( meType == GraphicType::Bitmap && !maVectorGraphicData.get())
+    if (mbSwapOut)
     {
-        bRet = ( mpAnimation ? mpAnimation->IsTransparent() : maEx.IsTransparent() );
+        bRet = maSwapInfo.mbIsTransparent;
+    }
+    else if (meType == GraphicType::Bitmap && !maVectorGraphicData.get())
+    {
+        bRet = mpAnimation ? mpAnimation->IsTransparent() : maEx.IsTransparent();
     }
 
     return bRet;
@@ -526,15 +584,17 @@ bool ImpGraphic::ImplIsAlpha() const
 {
     bool bRet(false);
 
-    ensureAvailable();
-
-    if(maVectorGraphicData.get())
+    if (mbSwapOut)
+    {
+        bRet = maSwapInfo.mbIsAlpha;
+    }
+    else if (maVectorGraphicData.get())
     {
         bRet = true;
     }
-    else if( meType == GraphicType::Bitmap )
+    else if (meType == GraphicType::Bitmap)
     {
-        bRet = ( nullptr == mpAnimation ) && maEx.IsAlpha();
+        bRet = (nullptr == mpAnimation && maEx.IsAlpha());
     }
 
     return bRet;
@@ -542,17 +602,27 @@ bool ImpGraphic::ImplIsAlpha() const
 
 bool ImpGraphic::ImplIsAnimated() const
 {
-    ensureAvailable();
-    return( mpAnimation != nullptr );
+    return mbSwapOut ? maSwapInfo.mbIsAnimated : mpAnimation != nullptr;
 }
 
 bool ImpGraphic::ImplIsEPS() const
 {
-    ensureAvailable();
+    if (mbSwapOut)
+        return maSwapInfo.mbIsEPS;
 
     return( ( meType == GraphicType::GdiMetafile ) &&
             ( maMetaFile.GetActionSize() > 0 ) &&
             ( maMetaFile.GetAction( 0 )->GetType() == MetaActionType::EPS ) );
+}
+
+bool ImpGraphic::isAvailable() const
+{
+    return !mbPrepared && !mbSwapOut;
+}
+
+bool ImpGraphic::makeAvailable()
+{
+    return ensureAvailable();
 }
 
 Bitmap ImpGraphic::ImplGetBitmap(const GraphicConversionParameters& rParameters) const
@@ -790,8 +860,10 @@ Size ImpGraphic::ImplGetPrefSize() const
 {
     Size aSize;
 
-    if( ImplIsSwapOut() )
+    if (ImplIsSwapOut())
+    {
         aSize = maSwapInfo.maPrefSize;
+    }
     else
     {
         switch( meType )
@@ -835,6 +907,8 @@ Size ImpGraphic::ImplGetPrefSize() const
 
 void ImpGraphic::ImplSetPrefSize( const Size& rPrefSize )
 {
+    ensureAvailable();
+
     switch( meType )
     {
         case GraphicType::NONE:
@@ -875,8 +949,10 @@ MapMode ImpGraphic::ImplGetPrefMapMode() const
 {
     MapMode aMapMode;
 
-    if( ImplIsSwapOut() )
+    if (ImplIsSwapOut())
+    {
         aMapMode = maSwapInfo.maPrefMapMode;
+    }
     else
     {
         switch( meType )
@@ -916,6 +992,8 @@ MapMode ImpGraphic::ImplGetPrefMapMode() const
 
 void ImpGraphic::ImplSetPrefMapMode( const MapMode& rPrefMapMode )
 {
+    ensureAvailable();
+
     switch( meType )
     {
         case GraphicType::NONE:
@@ -956,6 +1034,9 @@ sal_uLong ImpGraphic::ImplGetSizeBytes() const
 {
     if( 0 == mnSizeBytes )
     {
+        if (mbPrepared)
+            ensureAvailable();
+
         if( meType == GraphicType::Bitmap )
         {
             if(maVectorGraphicData.get())
@@ -1070,12 +1151,16 @@ void ImpGraphic::ImplStartAnimation( OutputDevice* pOutDev, const Point& rDestPt
 
 void ImpGraphic::ImplStopAnimation( OutputDevice* pOutDev, long nExtraData )
 {
+    ensureAvailable();
+
     if( ImplIsSupportedGraphic() && !ImplIsSwapOut() && mpAnimation )
         mpAnimation->Stop( pOutDev, nExtraData );
 }
 
 void ImpGraphic::ImplSetAnimationNotifyHdl( const Link<Animation*,void>& rLink )
 {
+    ensureAvailable();
+
     if( mpAnimation )
         mpAnimation->SetNotifyHdl( rLink );
 }
@@ -1083,6 +1168,8 @@ void ImpGraphic::ImplSetAnimationNotifyHdl( const Link<Animation*,void>& rLink )
 Link<Animation*,void> ImpGraphic::ImplGetAnimationNotifyHdl() const
 {
     Link<Animation*,void> aLink;
+
+    ensureAvailable();
 
     if( mpAnimation )
         aLink = mpAnimation->GetNotifyHdl();
@@ -1092,8 +1179,10 @@ Link<Animation*,void> ImpGraphic::ImplGetAnimationNotifyHdl() const
 
 sal_uInt32 ImpGraphic::ImplGetAnimationLoopCount() const
 {
-    ensureAvailable();
-    return( mpAnimation ? mpAnimation->GetLoopCount() : 0 );
+    if (mbSwapOut)
+        return maSwapInfo.mnAnimationLoopCount;
+
+    return mpAnimation ? mpAnimation->GetLoopCount() : 0;
 }
 
 void ImpGraphic::ImplSetContext( const std::shared_ptr<GraphicReader>& pReader )
@@ -1104,6 +1193,8 @@ void ImpGraphic::ImplSetContext( const std::shared_ptr<GraphicReader>& pReader )
 
 bool ImpGraphic::ImplReadEmbedded( SvStream& rIStm )
 {
+    ensureAvailable();
+
     MapMode         aMapMode;
     Size            aSize;
     sal_uInt32      nId;
@@ -1405,17 +1496,37 @@ bool ImpGraphic::ImplSwapOut( SvStream* xOStm )
 bool ImpGraphic::ensureAvailable() const
 {
     auto pThis = const_cast<ImpGraphic*>(this);
-    pThis->maLastUsed = std::chrono::high_resolution_clock::now();
+
     if (ImplIsSwapOut())
         return pThis->ImplSwapIn();
+
+    pThis->maLastUsed = std::chrono::high_resolution_clock::now();
     return true;
+}
+
+bool ImpGraphic::loadPrepared()
+{
+    Graphic aGraphic;
+    if (mpGfxLink->LoadNative(aGraphic))
+    {
+        *this = *aGraphic.ImplGetImpGraphic();
+        return true;
+    }
+    return false;
 }
 
 bool ImpGraphic::ImplSwapIn()
 {
     bool bRet = false;
 
-    if( ImplIsSwapOut() )
+    if (!ImplIsSwapOut())
+        return bRet;
+
+    if (mbPrepared)
+    {
+        bRet = loadPrepared();
+    }
+    else
     {
         OUString aSwapURL;
 
@@ -1762,6 +1873,8 @@ void WriteImpGraphic(SvStream& rOStm, const ImpGraphic& rImpGraphic)
 {
     if (rOStm.GetError())
         return;
+
+    rImpGraphic.ensureAvailable();
 
     if (rImpGraphic.ImplIsSwapOut())
     {

sd/qa/unit/data/xml/fdo64586_0.xml
@@ -4,7 +4,7 @@
   <FillTransparenceGradient style="LINEAR" startColor="000000" endColor="ffffff" angle="0" border="0" xOffset="50" yOffset="50" startIntensity="100" endIntensity="100" stepCount="0"/>
   <FillGradient style="LINEAR" startColor="000000" endColor="ffffff" angle="0" border="0" xOffset="50" yOffset="50" startIntensity="100" endIntensity="100" stepCount="0"/>
   <FillHatch style="SINGLE" color="3465a4" distance="20" angle="0"/>
-  <FillBitmap/>
+  <FillBitmap width="32" height="32"/>
   <LineDash style="RECT" dots="1" dotLen="20" dashes="1" dashLen="20" distance="20"/>
   <LineStart/>
   <LineEnd/>
@@ -18,7 +18,7 @@
   <FillTransparenceGradient style="LINEAR" startColor="000000" endColor="ffffff" angle="0" border="0" xOffset="50" yOffset="50" startIntensity="100" endIntensity="100" stepCount="0"/>
   <FillGradient style="LINEAR" startColor="000000" endColor="ffffff" angle="0" border="0" xOffset="50" yOffset="50" startIntensity="100" endIntensity="100" stepCount="0"/>
   <FillHatch style="SINGLE" color="3465a4" distance="20" angle="0"/>
-  <FillBitmap/>
+  <FillBitmap width="32" height="32"/>
   <LineDash style="RECT" dots="1" dotLen="20" dashes="1" dashLen="20" distance="20"/>
   <LineStart/>
   <LineEnd/>

sd/qa/unit/data/xml/n758621_0.xml
@@ -4,7 +4,7 @@
   <FillTransparenceGradient style="LINEAR" startColor="000000" endColor="ffffff" angle="0" border="0" xOffset="50" yOffset="50" startIntensity="100" endIntensity="100" stepCount="0"/>
   <FillGradient style="LINEAR" startColor="000000" endColor="ffffff" angle="0" border="0" xOffset="50" yOffset="50" startIntensity="100" endIntensity="100" stepCount="0"/>
   <FillHatch style="SINGLE" color="3465a4" distance="20" angle="0"/>
-  <FillBitmap/>
+  <FillBitmap width="32" height="32"/>
   <LineDash style="RECT" dots="1" dotLen="20" dashes="1" dashLen="20" distance="20"/>
   <LineStart/>
   <LineEnd/>
@@ -18,7 +18,7 @@
   <FillTransparenceGradient style="LINEAR" startColor="000000" endColor="ffffff" angle="0" border="0" xOffset="50" yOffset="50" startIntensity="100" endIntensity="100" stepCount="0"/>
   <FillGradient style="LINEAR" startColor="000000" endColor="ffffff" angle="0" border="0" xOffset="50" yOffset="50" startIntensity="100" endIntensity="100" stepCount="0"/>
   <FillHatch style="SINGLE" color="3465a4" distance="20" angle="0"/>
-  <FillBitmap/>
+  <FillBitmap width="32" height="32"/>
   <LineDash style="RECT" dots="1" dotLen="20" dashes="1" dashLen="20" distance="20"/>
   <LineStart/>
   <LineEnd/>

sd/qa/unit/data/xml/n758621_1.xml
@@ -4,7 +4,7 @@
   <FillTransparenceGradient style="LINEAR" startColor="000000" endColor="ffffff" angle="0" border="0" xOffset="50" yOffset="50" startIntensity="100" endIntensity="100" stepCount="0"/>
   <FillGradient style="LINEAR" startColor="000000" endColor="ffffff" angle="0" border="0" xOffset="50" yOffset="50" startIntensity="100" endIntensity="100" stepCount="0"/>
   <FillHatch style="SINGLE" color="3465a4" distance="20" angle="0"/>
-  <FillBitmap/>
+  <FillBitmap width="32" height="32"/>
   <LineDash style="RECT" dots="1" dotLen="20" dashes="1" dashLen="20" distance="20"/>
   <LineStart/>
   <LineEnd/>
@@ -18,7 +18,7 @@
   <FillTransparenceGradient style="LINEAR" startColor="000000" endColor="ffffff" angle="0" border="0" xOffset="50" yOffset="50" startIntensity="100" endIntensity="100" stepCount="0"/>
   <FillGradient style="LINEAR" startColor="000000" endColor="ffffff" angle="0" border="0" xOffset="50" yOffset="50" startIntensity="100" endIntensity="100" stepCount="0"/>
   <FillHatch style="SINGLE" color="3465a4" distance="20" angle="0"/>
-  <FillBitmap/>
+  <FillBitmap width="32" height="32"/>
   <LineDash style="RECT" dots="1" dotLen="20" dashes="1" dashLen="20" distance="20"/>
   <LineStart/>
   <LineEnd/>

sd/qa/unit/data/xml/n819614_0.xml
@@ -4,7 +4,7 @@
   <FillTransparenceGradient style="LINEAR" startColor="000000" endColor="ffffff" angle="0" border="0" xOffset="50" yOffset="50" startIntensity="100" endIntensity="100" stepCount="0"/>
   <FillGradient style="LINEAR" startColor="000000" endColor="ffffff" angle="0" border="0" xOffset="50" yOffset="50" startIntensity="100" endIntensity="100" stepCount="0"/>
   <FillHatch style="SINGLE" color="3465a4" distance="20" angle="0"/>
-  <FillBitmap/>
+  <FillBitmap width="32" height="32"/>
   <LineDash style="RECT" dots="1" dotLen="20" dashes="1" dashLen="20" distance="20"/>
   <LineStart/>
   <LineEnd/>

include/svx/svdograf.hxx
@@ -156,8 +156,8 @@ public:
     bool IsEPS() const;
     bool IsSwappedOut() const;
 
-    const MapMode&          GetGrafPrefMapMode() const;
-    const Size&             GetGrafPrefSize() const;
+    MapMode          GetGrafPrefMapMode() const;
+    Size             GetGrafPrefSize() const;
 
     void                    SetGrafStreamURL( const OUString& rGraphicStreamURL );
     OUString const &        GetGrafStreamURL() const;

include/vcl/GraphicObject.hxx
@@ -172,19 +172,9 @@ class VCL_DLLPUBLIC GraphicObject
 private:
     Graphic                 maGraphic;
     GraphicAttr             maAttr;
-    Size                    maPrefSize;
-    MapMode                 maPrefMapMode;
-    sal_uLong               mnSizeBytes;
-    GraphicType             meType;
     OUString                maUserData;
     std::unique_ptr<GrfSimpleCacheObj> mxSimpleCache;
-    sal_uInt32              mnAnimationLoopCount;
 
-    bool                    mbTransparent   : 1;
-    bool                    mbAnimated      : 1;
-    bool                    mbEPS           : 1;
-
-    void                    VCL_DLLPRIVATE ImplAssignGraphicData();
     bool                    VCL_DLLPRIVATE ImplGetCropParams(
                                 OutputDevice const * pOut,
                                 Point& rPt,
@@ -342,13 +332,12 @@ public:
 
     OString                 GetUniqueID() const;
 
-    GraphicType             GetType() const { return meType; }
-    const Size&             GetPrefSize() const { return maPrefSize; }
-    const MapMode&          GetPrefMapMode() const { return maPrefMapMode; }
-    sal_uLong               GetSizeBytes() const { return mnSizeBytes; }
-    bool                    IsTransparent() const { return mbTransparent; }
-    bool                    IsAnimated() const { return mbAnimated; }
-    bool                    IsEPS() const { return mbEPS; }
+    GraphicType             GetType() const;
+    Size                    GetPrefSize() const;
+    MapMode                 GetPrefMapMode() const;
+    bool                    IsTransparent() const;
+    bool                    IsAnimated() const;
+    bool                    IsEPS() const;
 
     bool                    Draw(
                                 OutputDevice* pOut,

include/vcl/gfxlink.hxx
@@ -64,17 +64,17 @@ private:
 
     };
 
-    GfxLinkType     meType = GfxLinkType::NONE;
-    sal_uInt32      mnUserId = 0;
+    GfxLinkType     meType;
+    sal_uInt32      mnUserId;
 
     std::shared_ptr<sal_uInt8> mpSwapInData;
     std::shared_ptr<SwapOutData> mpSwapOutData;
 
-    sal_uInt32      mnSwapInDataSize = 0;
+    sal_uInt32      mnSwapInDataSize;
     MapMode         maPrefMapMode;
     Size            maPrefSize;
-    bool            mbPrefMapModeValid = false;
-    bool            mbPrefSizeValid = false;
+    bool            mbPrefMapModeValid;
+    bool            mbPrefSizeValid;
 
     SAL_DLLPRIVATE std::shared_ptr<sal_uInt8> GetSwapInData() const;
 public:

commit 6d8c6e8d60956fd36094035a526c1a29a902204b

fix ImpGraphic::operator==
This was broken in
    commit 7b355669c6ddeab2e6cec692d6afdff41c61d0fb
    Author: Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
    Date:   Sat Apr 14 15:13:05 2018 +0900
    Function to load graphic swapped out (loaded on demand)

Change-Id: I8ad120b65da5a7c53e8f90c6e01207f75da11c34
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/156310
Tested-by: Noel Grandin <noel.grandin@collabora.co.uk>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/source/gdi/impgraph.cxx
@@ -283,61 +283,56 @@ ImpGraphic& ImpGraphic::operator=(ImpGraphic&& rImpGraphic)
     return *this;
 }
 
-bool ImpGraphic::operator==( const ImpGraphic& rImpGraphic ) const
+bool ImpGraphic::operator==( const ImpGraphic& rOther ) const
 {
+    if( this == &rOther )
+        return true;
+
+    if (mbPrepared && rOther.mbPrepared)
+        return (*mpGfxLink == *rOther.mpGfxLink);
+
+    if (!isAvailable() || !rOther.isAvailable())
+        return false;
+
+    if ( meType != rOther.meType )
+        return false;
+
     bool bRet = false;
+    switch( meType )
+    {
+        case GraphicType::NONE:
+        case GraphicType::Default:
+            return true;
 
-    if( this == &rImpGraphic )
-        bRet = true;
-    else if (mbPrepared && rImpGraphic.mbPrepared)
-    {
-        bRet = (*mpGfxLink == *rImpGraphic.mpGfxLink);
-    }
-    else if (isAvailable() && rImpGraphic.isAvailable())
-    {
-        switch( meType )
+        case GraphicType::GdiMetafile:
+            return ( rOther.maMetaFile == maMetaFile );
+
+        case GraphicType::Bitmap:
         {
-            case GraphicType::NONE:
+            if(maVectorGraphicData)
+            {
+                if(maVectorGraphicData == rOther.maVectorGraphicData)
+                {
+                    // equal instances
+                    bRet = true;
+                }
+                else if(rOther.maVectorGraphicData)
+                {
+                    // equal content
+                    bRet = (*maVectorGraphicData) == (*rOther.maVectorGraphicData);
+                }
+            }
+            else if( mpAnimation )
+            {
+                if( rOther.mpAnimation && ( *rOther.mpAnimation == *mpAnimation ) )
+                    bRet = true;
+            }
+            else if( !rOther.mpAnimation && ( rOther.maBitmapEx == maBitmapEx ) )
+            {
                 bRet = true;
-            break;
-
-            case GraphicType::GdiMetafile:
-            {
-                if( rImpGraphic.maMetaFile == maMetaFile )
-                    bRet = true;
             }
-            break;
-
-            case GraphicType::Bitmap:
-            {
-                if(maVectorGraphicData)
-                {
-                    if(maVectorGraphicData == rImpGraphic.maVectorGraphicData)
-                    {
-                        // equal instances
-                        bRet = true;
-                    }
-                    else if(rImpGraphic.maVectorGraphicData)
-                    {
-                        // equal content
-                        bRet = (*maVectorGraphicData) == (*rImpGraphic.maVectorGraphicData);
-                    }
-                }
-                else if( mpAnimation )
-                {
-                    if( rImpGraphic.mpAnimation && ( *rImpGraphic.mpAnimation == *mpAnimation ) )
-                        bRet = true;
-                }
-                else if( !rImpGraphic.mpAnimation && ( rImpGraphic.maBitmapEx == maBitmapEx ) )
-                {
-                    bRet = true;
-                }
-            }
-            break;
-
-            case GraphicType::Default:
-            break;
         }
+        break;
     }
 
     return bRet;

sd/qa/unit/data/xml/fdo64586_0.xml
@@ -4,7 +4,7 @@
   <FillTransparenceGradient style="LINEAR" startColor="000000" endColor="000000" angle="0" border="0" xOffset="50" yOffset="50" startIntensity="100" endIntensity="100" stepCount="0"/>
   <FillGradient style="LINEAR" startColor="000000" endColor="ffffff" angle="0" border="0" xOffset="50" yOffset="50" startIntensity="100" endIntensity="100" stepCount="0"/>
   <FillHatch style="SINGLE" color="3465a4" distance="20" angle="0"/>
-  <FillBitmap width="32" height="32"/>
+  <FillBitmap/>
   <LineDash style="RECT" dots="1" dotLen="20" dashes="1" dashLen="20" distance="20"/>
   <LineStart/>
   <LineEnd/>
@@ -18,7 +18,7 @@
   <FillTransparenceGradient style="LINEAR" startColor="000000" endColor="000000" angle="0" border="0" xOffset="50" yOffset="50" startIntensity="100" endIntensity="100" stepCount="0"/>
   <FillGradient style="LINEAR" startColor="000000" endColor="ffffff" angle="0" border="0" xOffset="50" yOffset="50" startIntensity="100" endIntensity="100" stepCount="0"/>
   <FillHatch style="SINGLE" color="3465a4" distance="20" angle="0"/>
-  <FillBitmap width="32" height="32"/>
+  <FillBitmap/>
   <LineDash style="RECT" dots="1" dotLen="20" dashes="1" dashLen="20" distance="20"/>
   <LineStart/>
   <LineEnd/>

sd/qa/unit/data/xml/n758621_0.xml
@@ -4,7 +4,7 @@
   <FillTransparenceGradient style="LINEAR" startColor="000000" endColor="000000" angle="0" border="0" xOffset="50" yOffset="50" startIntensity="100" endIntensity="100" stepCount="0"/>
   <FillGradient style="LINEAR" startColor="000000" endColor="ffffff" angle="0" border="0" xOffset="50" yOffset="50" startIntensity="100" endIntensity="100" stepCount="0"/>
   <FillHatch style="SINGLE" color="3465a4" distance="20" angle="0"/>
-  <FillBitmap width="32" height="32"/>
+  <FillBitmap/>
   <LineDash style="RECT" dots="1" dotLen="20" dashes="1" dashLen="20" distance="20"/>
   <LineStart/>
   <LineEnd/>
@@ -18,7 +18,7 @@
   <FillTransparenceGradient style="LINEAR" startColor="000000" endColor="000000" angle="0" border="0" xOffset="50" yOffset="50" startIntensity="100" endIntensity="100" stepCount="0"/>
   <FillGradient style="LINEAR" startColor="000000" endColor="ffffff" angle="0" border="0" xOffset="50" yOffset="50" startIntensity="100" endIntensity="100" stepCount="0"/>
   <FillHatch style="SINGLE" color="3465a4" distance="20" angle="0"/>
-  <FillBitmap width="32" height="32"/>
+  <FillBitmap/>
   <LineDash style="RECT" dots="1" dotLen="20" dashes="1" dashLen="20" distance="20"/>
   <LineStart/>
   <LineEnd/>

sd/qa/unit/data/xml/n758621_1.xml
@@ -4,7 +4,7 @@
   <FillTransparenceGradient style="LINEAR" startColor="000000" endColor="000000" angle="0" border="0" xOffset="50" yOffset="50" startIntensity="100" endIntensity="100" stepCount="0"/>
   <FillGradient style="LINEAR" startColor="000000" endColor="ffffff" angle="0" border="0" xOffset="50" yOffset="50" startIntensity="100" endIntensity="100" stepCount="0"/>
   <FillHatch style="SINGLE" color="3465a4" distance="20" angle="0"/>
-  <FillBitmap width="32" height="32"/>
+  <FillBitmap/>
   <LineDash style="RECT" dots="1" dotLen="20" dashes="1" dashLen="20" distance="20"/>
   <LineStart/>
   <LineEnd/>
@@ -18,7 +18,7 @@
   <FillTransparenceGradient style="LINEAR" startColor="000000" endColor="000000" angle="0" border="0" xOffset="50" yOffset="50" startIntensity="100" endIntensity="100" stepCount="0"/>
   <FillGradient style="LINEAR" startColor="000000" endColor="ffffff" angle="0" border="0" xOffset="50" yOffset="50" startIntensity="100" endIntensity="100" stepCount="0"/>
   <FillHatch style="SINGLE" color="3465a4" distance="20" angle="0"/>
-  <FillBitmap width="32" height="32"/>
+  <FillBitmap/>
   <LineDash style="RECT" dots="1" dotLen="20" dashes="1" dashLen="20" distance="20"/>
   <LineStart/>
   <LineEnd/>

sd/qa/unit/data/xml/n819614_0.xml
@@ -4,7 +4,7 @@
   <FillTransparenceGradient style="LINEAR" startColor="000000" endColor="000000" angle="0" border="0" xOffset="50" yOffset="50" startIntensity="100" endIntensity="100" stepCount="0"/>
   <FillGradient style="LINEAR" startColor="000000" endColor="ffffff" angle="0" border="0" xOffset="50" yOffset="50" startIntensity="100" endIntensity="100" stepCount="0"/>
   <FillHatch style="SINGLE" color="3465a4" distance="20" angle="0"/>
-  <FillBitmap width="32" height="32"/>
+  <FillBitmap/>
   <LineDash style="RECT" dots="1" dotLen="20" dashes="1" dashLen="20" distance="20"/>
   <LineStart/>
   <LineEnd/>