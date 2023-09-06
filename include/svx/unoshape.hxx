170edf783a0363fd66c4f82b75a0890ea9c77816
Caolán McNamara <caolan.mcnamara@collabora.com>
2023-09-06 10:05:23 +0100

    add referer to ole objects
    so we can identify what document is requesting their contents
    
    extends:
    
    commit 5668e73beb30b95abc6520b7432c54972ca3ab2c
    Date:   Wed Nov 20 14:43:45 2013 +0100
    
        avmedia: Implement "block untrusted referer links" feature
    
        See f0a9ca24fd4bf79cac908bf0d6fdb8905dc504db "rhbz#887420 Implement 'block
        untrusted referer links' feature" for details.  This adds some further /*TODO?*/
        comments, and one known problem (marked /*TODO!*/) is that movies/sounds are not
        blocked during a slideshow presentation.
    
    to these objects too, namely OLE2Shape and derivatives AppletShape,
    FrameShape and PluginShape
    
    so in paranoid mode we won't load the contents of such objects from
    documents considered "untrusted".
    
    Change-Id: I6d988035d0cd09fd3fade5f6885fe336c95579ab
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/156598
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>
    (cherry picked from commit 125cf1525361c6cd699574f60b4cf12868188568)

--- include/svx/unoshape.hxx
+++ include/svx/unoshape.hxx
@@ -603,6 +603,8 @@ public:
 // #i118485# changed parent to SvxShapeText to allow Text handling over UNO API
 class SVXCORE_DLLPUBLIC SvxOle2Shape : public SvxShapeText
 {
+private:
+    OUString referer_;
 protected:
     // override these for special property handling in subcasses. Return true if property is handled
     virtual bool setPropertyValueImpl( const OUString& rName, const SfxItemPropertyMapEntry* pProperty, const css::uno::Any& rValue ) override;
@@ -612,8 +614,8 @@ protected:
 
     SvGlobalName GetClassName_Impl(OUString& rHexCLSID);
 public:
-    SvxOle2Shape(SdrObject* pObj);
-    SvxOle2Shape(SdrObject* pObject, const SfxItemPropertyMapEntry* pPropertyMap, const SvxItemPropertySet* pPropertySet);
+    SvxOle2Shape(SdrObject* pObj, OUString referer);
+    SvxOle2Shape(SdrObject* pObject, OUString referer, const SfxItemPropertyMapEntry* pPropertyMap, const SvxItemPropertySet* pPropertySet);
     virtual ~SvxOle2Shape() noexcept override;
 
     bool createObject( const SvGlobalName &aClassName );
