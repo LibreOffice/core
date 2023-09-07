e653ddd24ab4b75db212431461035ecbd9d625fa
Gökay Şatır <gokaysatir@gmail.com>
2023-09-07 11:22:25 +0300

    We can allow one to answer their own comment.
    Other office implementations allow this.
    
    I think we can enable reply button for the owner of the comment, unless there is a good reason to not to.
    
    Signed-off-by: Gökay Şatır <gokaysatir@gmail.com>
    Change-Id: I6969122efe24d2804b4092e0afe0709a2fd1ef98
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/156688
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

--- sw/source/uibase/docvw/AnnotationMenuButton.cxx
+++ sw/source/uibase/docvw/AnnotationMenuButton.cxx
@@ -66,13 +66,11 @@ IMPL_LINK_NOARG(SwAnnotationWin, ToggleHdl, weld::Toggleable&, void)
     if (!mxMenuButton->get_active())
         return;
 
-    bool bReplyVis = true;
-
     bool bReadOnly = IsReadOnly();
     if (bReadOnly)
     {
         mxMenuButton->set_item_visible("reply", false);
-        bReplyVis = false;
+        mxMenuButton->set_item_visible("sep1", false); // Separator after reply button.
         mxMenuButton->set_item_visible("resolve", false);
         mxMenuButton->set_item_visible("unresolve", false);
         mxMenuButton->set_item_visible("resolvethread", false);
@@ -81,6 +79,8 @@ IMPL_LINK_NOARG(SwAnnotationWin, ToggleHdl, weld::Toggleable&, void)
     }
     else
     {
+        mxMenuButton->set_item_visible("reply", !IsProtected());
+        mxMenuButton->set_item_visible("sep1", !IsProtected());
         mxMenuButton->set_item_visible("resolve", !IsResolved());
         mxMenuButton->set_item_visible("unresolve", IsResolved());
         mxMenuButton->set_item_visible("resolvethread", !IsThreadResolved());
@@ -92,28 +92,6 @@ IMPL_LINK_NOARG(SwAnnotationWin, ToggleHdl, weld::Toggleable&, void)
     mxMenuButton->set_item_visible("deleteby", !bReadOnly);
     mxMenuButton->set_item_visible("deleteall", !bReadOnly);
     mxMenuButton->set_item_visible("formatall", !bReadOnly);
-
-    if (IsProtected())
-    {
-        mxMenuButton->set_item_visible("reply", false);
-        bReplyVis = false;
-    }
-    else
-    {
-        SvtUserOptions aUserOpt;
-        OUString sAuthor;
-        if ((sAuthor = aUserOpt.GetFullName()).isEmpty())
-        {
-            if ((sAuthor = aUserOpt.GetID()).isEmpty())
-            {
-                sAuthor = SwResId(STR_REDLINE_UNKNOWN_AUTHOR);
-            }
-        }
-        // do not allow to reply to ourself and no answer possible if this note is in a protected section
-        bReplyVis = sAuthor != GetAuthor();
-        mxMenuButton->set_item_visible("reply", bReplyVis);
-    }
-    mxMenuButton->set_item_visible("sep1", bReplyVis);
 }
 
 IMPL_LINK(SwAnnotationWin, KeyInputHdl, const KeyEvent&, rKeyEvt, bool)
