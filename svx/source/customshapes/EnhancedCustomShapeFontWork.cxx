LibreOffice/core

commit b153d147c9d813f78dd551fc186c261f52c929a1

remove support for BITMASK in vcl backends
Rather use a proper alpha channel if we need transparency.

This is another small step towards merged alpha in our vcl
layer.

I suspect the intent in a lot of this code was to save memory.
Which have been a thing way back then, but these days our
backends mostly end up doing a copy-and-convert to a real
alpha channel anyway, so the existing code is actually
now a pessimisation.

Change-Id: I4a2bcbb2f76b841f05bc00580f364492829c69de
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/114808
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svx/source/customshapes/EnhancedCustomShapeFontWork.cxx
@@ -192,7 +192,7 @@ static void CalculateHorizontalScalingFactor(
     aFont.SetOrientation( 0_deg10 );
     // initializing virtual device
 
-    ScopedVclPtrInstance< VirtualDevice > pVirDev(DeviceFormat::BITMASK);
+    ScopedVclPtrInstance< VirtualDevice > pVirDev(DeviceFormat::DEFAULT);
     pVirDev->SetMapMode(MapMode(MapUnit::Map100thMM));
     pVirDev->SetFont( aFont );
 
@@ -303,7 +303,7 @@ static void GetTextAreaOutline(
             aFont.SetWeight( rWeightItem.GetWeight() );
 
             // initializing virtual device
-            ScopedVclPtrInstance< VirtualDevice > pVirDev(DeviceFormat::BITMASK);
+            ScopedVclPtrInstance< VirtualDevice > pVirDev(DeviceFormat::DEFAULT);
             pVirDev->SetMapMode(MapMode(MapUnit::Map100thMM));
             pVirDev->SetFont( aFont );
             pVirDev->EnableRTL();

canvas/source/vcl/backbuffer.cxx
@@ -27,14 +27,9 @@
 
 namespace vclcanvas
 {
-    BackBuffer::BackBuffer( const OutputDevice& rRefDevice,
-                            bool                bMonochromeBuffer ) :
-        maVDev( VclPtr<VirtualDevice>::Create( rRefDevice,
-                                   bMonochromeBuffer ? DeviceFormat::BITMASK : DeviceFormat::DEFAULT ) )
+    BackBuffer::BackBuffer( const OutputDevice& rRefDevice ) :
+        maVDev( VclPtr<VirtualDevice>::Create( rRefDevice, DeviceFormat::DEFAULT ) )
     {
-        if( bMonochromeBuffer )
-            return;
-
         tools::SetDefaultDeviceAntiAliasing( maVDev );
     }
 

canvas/source/vcl/backbuffer.hxx
@@ -31,15 +31,8 @@ namespace vclcanvas
     class BackBuffer : public OutDevProvider
     {
     public:
-        /** Create a backbuffer for given reference device
-
-            @param bMonochromeBuffer
-            When false, default depth of reference device is
-            chosen. When true, the buffer will be monochrome, i.e. one
-            bit deep.
-         */
-        BackBuffer( const OutputDevice& rRefDevice,
-                    bool                bMonochromeBuffer=false );
+        /** Create a backbuffer for given reference device */
+        BackBuffer( const OutputDevice& rRefDevice );
         virtual ~BackBuffer() override;
 
         virtual OutputDevice&       getOutDev() override;

canvas/source/vcl/canvascustomsprite.cxx
@@ -58,9 +58,8 @@ namespace vclcanvas
         BackBufferSharedPtr pBackBuffer = std::make_shared<BackBuffer>( rOutDevProvider->getOutDev() );
         pBackBuffer->setSize( aSize );
 
-        // create mask backbuffer, with one bit color depth
-        BackBufferSharedPtr pBackBufferMask = std::make_shared<BackBuffer>( rOutDevProvider->getOutDev(),
-                                                             true );
+        // create mask backbuffer
+        BackBufferSharedPtr pBackBufferMask = std::make_shared<BackBuffer>( rOutDevProvider->getOutDev() );
         pBackBufferMask->setSize( aSize );
 
         // TODO(F1): Implement alpha vdev (could prolly enable

cui/source/dialogs/screenshotannotationdlg.cxx
@@ -234,7 +234,7 @@ ScreenshotAnnotationDlg_Impl::ScreenshotAnnotationDlg_Impl(
         maDimmedDialogBitmap.Adjust(-15, 0, 0, 0, 0);
 
         // init paint buffering VirtualDevice
-        mxVirtualBufferDevice = VclPtr<VirtualDevice>::Create(*Application::GetDefaultDevice(), DeviceFormat::DEFAULT, DeviceFormat::BITMASK);
+        mxVirtualBufferDevice = VclPtr<VirtualDevice>::Create(*Application::GetDefaultDevice(), DeviceFormat::DEFAULT);
         mxVirtualBufferDevice->SetOutputSizePixel(maParentDialogSize);
         mxVirtualBufferDevice->SetFillColor(COL_TRANSPARENT);
 

drawinglayer/source/primitive2d/graphicprimitivehelper2d.cxx
@@ -367,7 +367,7 @@ namespace drawinglayer::primitive2d
             maGraphic(rGraphic),
             maAnimation(rGraphic.GetAnimation()),
             maVirtualDevice(*Application::GetDefaultDevice()),
-            maVirtualDeviceMask(*Application::GetDefaultDevice(), DeviceFormat::BITMASK),
+            maVirtualDeviceMask(*Application::GetDefaultDevice()),
             mnNextFrameToPrepare(SAL_MAX_UINT32),
             maBufferedFirstFrame(),
             maBufferedPrimitives(),

filter/source/msfilter/msdffimp.cxx
@@ -4472,7 +4472,7 @@ SdrObject* SvxMSDffManager::ImportShape( const DffRecordHeader& rHd, SvStream& r
                             rOutliner.SetStyleSheetPool(static_cast< SfxStyleSheetPool* >(pRet->getSdrModelFromSdrObject().GetStyleSheetPool()));
                             rOutliner.SetUpdateMode( false );
                             rOutliner.SetText( *pParaObj );
-                            ScopedVclPtrInstance< VirtualDevice > pVirDev(DeviceFormat::BITMASK);
+                            ScopedVclPtrInstance< VirtualDevice > pVirDev(DeviceFormat::DEFAULT);
                             pVirDev->SetMapMode(MapMode(MapUnit::Map100thMM));
                             sal_Int32 i, nParagraphs = rOutliner.GetParagraphCount();
                             if ( nParagraphs )

include/vcl/salgtype.hxx
@@ -28,7 +28,6 @@
 enum class DeviceFormat {
                             NONE = -1,
                             DEFAULT = 0,
-                            BITMASK = 1,
 #ifdef IOS
                             GRAYSCALE = 8
 #endif

commit 819dec3533750a3a8f483351febe40aae85ca0b7

tdf#157173 Enlarging Fontwork shape replaces used font
regression from
    commit b153d147c9d813f78dd551fc186c261f52c929a1
    Author: Noel Grandin <noel.grandin@collabora.co.uk>
    Date:   Wed Apr 28 15:27:14 2021 +0200
    remove support for BITMASK in vcl backends

So the code that I removed had
    if (meFormat == DeviceFormat::BITMASK)
        SetAntialiasing( AntialiasingFlags::DisableText );
in the
    VirtualDevice::ImplInitVirDev
method.

Possibly we will need to make similar fixes in other places that used
BITMASK, but this looks like the only spot that really needs it.

No idea exactly how this line of code relates to the observed bug
though.

Change-Id: I858f15cf70a452568128d5ccccb365d9a11f7062
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/156815
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svx/source/customshapes/EnhancedCustomShapeFontWork.cxx
@@ -230,6 +230,7 @@ static void CalculateHorizontalScalingFactor(
     ScopedVclPtrInstance< VirtualDevice > pVirDev(DeviceFormat::WITHOUT_ALPHA);
     pVirDev->SetMapMode(MapMode(MapUnit::Map100thMM));
     pVirDev->SetFont( aFont );
+    pVirDev->SetAntialiasing( AntialiasingFlags::DisableText );
 
     if ( nOutlinesCount2d & 1 )
         bSingleLineMode = true;
@@ -343,6 +344,8 @@ static void GetTextAreaOutline(
             ScopedVclPtrInstance< VirtualDevice > pVirDev(DeviceFormat::WITHOUT_ALPHA);
             pVirDev->SetMapMode(MapMode(MapUnit::Map100thMM));
             pVirDev->SetFont( aFont );
+            pVirDev->SetAntialiasing( AntialiasingFlags::DisableText );
+
             pVirDev->EnableRTL();
             if ( rParagraph.nFrameDirection == SvxFrameDirection::Horizontal_RL_TB )
                 pVirDev->SetLayoutMode( vcl::text::ComplexTextLayoutFlags::BiDiRtl );