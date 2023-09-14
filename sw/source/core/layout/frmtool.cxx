LibreOffice/core

commit 31e66bd07c1082bb375be8aaf7835f019351d9bb

sw: fix ignored frames in AppendAllObjs()
The problem is that AppendAllObjs() doesn't check if MakeFrames()
actually created frames, it just assumes success.

If there are frames anchored in frames, then it could go through
the circular_buffer, find a dependent frame before its anchor frame,
unsuccessfully call MakeFrames(), then call MakeFrames() on the anchor
frame, and then the vector is empty.

A surprising aspect is that push_back on a boost::circular_buffer will
silently pop the first element if it's already "full".  Possibly this
is what caused tdf#112447.

1. insert section
2. in paragraph in section, insert frame
3. repeat 2
4. drag anchor of frame 1 into body of frame 2
5. edit section, click hide
6. edit section, un-click hide
7. only one frame is displayed

(regression from 575e222a1742918be052f2b716ddf57ce0008404 and/or
 ce2fce9a41729774689080c8b5552b60c2e6ee2d)

Change-Id: Ie782252ac388524dfb083f655320a50e95239b24
Reviewed-on: https://gerrit.libreoffice.org/58676
Tested-by: Jenkins
Reviewed-by: Michael Stahl <Michael.Stahl@cib.de>

sw/source/core/layout/frmtool.cxx
@@ -1190,18 +1190,24 @@ void AppendAllObjs(const SwFrameFormats* pTable, const SwFrame* pSib)
         if(!isConnected)
         {
             pFormat->MakeFrames();
-            pFirstRequeued = nullptr;
+            pFormat->CallSwClientNotify(sw::GetObjectConnectedHint(isConnected, pRoot));
         }
-        else
+        // do this *before* push_back! the circular_buffer can be "full"!
+        vFormatsToConnect.pop_front();
+        if (!isConnected)
         {
             if(pFirstRequeued == pFormat)
                 // If nothing happens anymore we can stop.
                 break;
             if(!pFirstRequeued)
                 pFirstRequeued = pFormat;
+            assert(!vFormatsToConnect.full());
             vFormatsToConnect.push_back(pFormat);
         }
-        vFormatsToConnect.pop_front();
+        else
+        {
+            pFirstRequeued = nullptr;
+        }
     }
 }
 

commit e2a0530a26b5c5b5f48d3e3c695b839d0e5c0164

fix use of reference
where we may end up with a reference to a different element of the queue
than we want.

regression from
    commit 31e66bd07c1082bb375be8aaf7835f019351d9bb
    Author: Michael Stahl <Michael.Stahl@cib.de>
    Date:   Mon Aug 6 19:03:22 2018 +0200
    sw: fix ignored frames in AppendAllObjs()

Change-Id: I94801df84b4b130315f67292851fbd129b2fe7d1
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/156919
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
Tested-by: Jenkins

sw/source/core/layout/frmtool.cxx
@@ -1411,7 +1411,7 @@ void AppendAllObjs(const sw::FrameFormats<sw::SpzFrameFormat*>* pTable, const Sw
     const SwFrameFormat* pFirstRequeued(nullptr);
     while(!vFormatsToConnect.empty())
     {
-        auto& pFormat = vFormatsToConnect.front();
+        SwFrameFormat* pFormat = vFormatsToConnect.front();
         bool isConnected(false);
         pFormat->CallSwClientNotify(sw::GetObjectConnectedHint(isConnected, pRoot));
         if(!isConnected)