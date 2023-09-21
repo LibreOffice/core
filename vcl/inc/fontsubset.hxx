0f2918c25daab25cd0721d69f0506fe04456262d
Bogdan Buzea <buzea.bogdan@libreoffice.org>
2023-09-21 17:35:18 +0300

    tdf#143148: Use pragma once instead of include guards
    Change-Id: I3f0c0b4e9004c45e8f1d12065a322b7a91b09150
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/157152
    Tested-by: Jenkins
    Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

--- vcl/inc/fontsubset.hxx
+++ vcl/inc/fontsubset.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_VCL_INC_FONTSUBSET_HXX
-#define INCLUDED_VCL_INC_FONTSUBSET_HXX
+#pragma once
 
 #include <rtl/ustring.hxx>
 #include <tools/gen.hxx>
@@ -87,7 +86,5 @@ private:
 };
 
 int VCL_DLLPUBLIC TestFontSubset(const void* data, sal_uInt32 size);
-
-#endif // INCLUDED_VCL_INC_FONTSUBSET_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
