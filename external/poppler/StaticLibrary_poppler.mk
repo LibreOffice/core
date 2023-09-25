9b1fdad86d0e652f134a5d78dc6f9aefd35bf739
Michael Stahl <michael.stahl@allotropia.de>
2023-09-25 15:50:43 +0200

    poppler: upgrade to release 23.09.0
    Fixes CVE-2023-34872
    
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/157247
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
    (cherry picked from commit 3d31dfa9b99d0df5e400c1a6d7e8df6c46129b3c)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/157253
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
    (cherry picked from commit 6d1c0e802eaefa1c42051c1456be61cef2dc2e86)
    
    Change-Id: I289b3016695a01aff7d393cb09d66cd726d9b592

--- external/poppler/StaticLibrary_poppler.mk
+++ external/poppler/StaticLibrary_poppler.mk
@@ -11,7 +11,10 @@ $(eval $(call gb_StaticLibrary_StaticLibrary,poppler))
 
 $(eval $(call gb_StaticLibrary_use_unpacked,poppler,poppler))
 
-$(eval $(call gb_StaticLibrary_use_external,poppler,libjpeg))
+$(eval $(call gb_StaticLibrary_use_externals,poppler,\
+	libjpeg \
+	zlib \
+))
 
 $(eval $(call gb_StaticLibrary_set_warnings_disabled,poppler))
 
@@ -90,6 +93,7 @@ $(eval $(call gb_StaticLibrary_add_generated_exception_objects,poppler,\
 	UnpackedTarball/poppler/poppler/FDPDFDocBuilder \
 	UnpackedTarball/poppler/poppler/FILECacheLoader \
 	UnpackedTarball/poppler/poppler/FileSpec \
+	UnpackedTarball/poppler/poppler/FlateEncoder \
 	UnpackedTarball/poppler/poppler/FontEncodingTables \
 	UnpackedTarball/poppler/poppler/FontInfo \
 	UnpackedTarball/poppler/poppler/Form \
