LibreOffice/core

commit cf18038c66075f7a18d89e47f3a2ab1a5bf7c4fd

tdf#81894 sc spelling: eliminate separate(inaccurate) check
ExecuteSpellPopup is quite accurate at identifying whether
the spelling at PosPixel is a spelling error or not.
If it is not a misspelled word, then it just silently returns.

If we make this a bDone flag, then it will just move
on to the normal right-click dialog.

There are a few additional scenarios that need to be
handled (mainly in terms of showing the edit-mode menu
instead of the full format menu, and also not
always returning the correct row/col when in edit mode).
I will do them in separate patches so that
each edge case can be clearly seen and handled.
They all rely on this patch being applied first.

This is the base patch. Too many things simply don't
work with the interim check, so I removed it.
(Part of the problem is that EditView kicks on and off,
and then a new CompleteOnlineSpelling needs to be run before
IsWrongSpelledWordAtPos can return a meaningful answer.)

This patch specifically fixes the problem of not
opening SpellPopup on a misspelled word in a wrapped-cell.
Steps to reproduce:
1.) Open LOv4304.ods from bug 81894 comment 6.
2.) Right-click on "spellng" in cell B12.

Before the patch, a full format popup was displayed
(not in edit mode) because IsSpellErrorAtPos returned
false, when it should have been true.

With this patch, spelling suggestions are provided.

As an example of an edge case, also notice:
3.) double-click on cell B4 (to enter edit mode)
4.) right click on "spellng" which appears to be in D4

Before the patch, nothing happened at all,
but with the patch at least the fall-back menu is displayed.
(EditView never switches out of cell B,
so SpellPopup is looking at wrong information.)

Change-Id: I91d413debc58de832dc7413034618e9e2bae3dd5
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/130297
Tested-by: Jenkins
Reviewed-by: Justin Luth <jluth@mail.com>
Reviewed-by: Eike Rathke <erack@redhat.com>

sc/source/ui/view/gridwin.cxx
@@ -3229,12 +3229,9 @@ void ScGridWindow::Command( const CommandEvent& rCEvt )
             if (aPos.Col() >= 0 && (aSpellCheckCell.meType == CELLTYPE_STRING || aSpellCheckCell.meType == CELLTYPE_EDIT))
                 nColSpellError = aPos.Col();
 
+            // Is there a missspelled word somewhere in the cell?
+            // A "yes" does not mean that the word under the mouse pointer is wrong though.
             bSpellError = (mpSpellCheckCxt->isMisspelled(nColSpellError, nCellY));
-            if (bSpellError)
-            {
-                // Check and see if a misspelled word is under the mouse pointer.
-                bSpellError = IsSpellErrorAtPos(aPosPixel, nColSpellError, nCellY);
-            }
         }
 
         //  #i18735# First select the item under the mouse pointer.
@@ -3248,7 +3245,7 @@ void ScGridWindow::Command( const CommandEvent& rCEvt )
     if ( !bEdit )
     {
         // Edit cell with spelling errors?
-        // tdf#127341 the formally used GetEditUrl(aPosPixel) additionally
+        // tdf#127341 the formerly used GetEditUrl(aPosPixel) additionally
         // to bSpellError activated EditMode here for right-click on URL
         // which prevents the regular context-menu from appearing. Since this
         // is more expected than the context-menu for editing an URL, I removed
@@ -3300,13 +3297,18 @@ void ScGridWindow::Command( const CommandEvent& rCEvt )
 
             const OUString sOldText = pHdl ? pHdl->GetEditString() : "";
 
+            // Only done/shown if a misspelled word is actually under the mouse pointer.
             Link<SpellCallbackInfo&,void> aLink = LINK( this, ScGridWindow, PopupSpellingHdl );
-            pEditView->ExecuteSpellPopup(aMenuPos, aLink);
+            bDone = pEditView->ExecuteSpellPopup(aMenuPos, aLink);
 
             if (pHdl && pHdl->GetEditString() != sOldText)
                 pHdl->EnterHandler();
 
-            bDone = true;
+            if (!bDone && nColSpellError != nCellX)
+            {
+                // NOTE: This call can change the selection, and the view state (edit mode, etc).
+                SelectForContextMenu(aPosPixel, nCellX, nCellY);
+            }
         }
     }
     else if ( !bMouse )
@@ -5765,60 +5767,6 @@ bool ScGridWindow::GetEditUrl( const Point& rPos,
         return bRet;
     }
     return false;
-}
-
-bool ScGridWindow::IsSpellErrorAtPos( const Point& rPos, SCCOL nCol1, SCROW nRow )
-{
-    if (!mpSpellCheckCxt)
-        return false;
-
-    SCTAB nTab = mrViewData.GetTabNo();
-    ScDocShell* pDocSh = mrViewData.GetDocShell();
-    ScDocument& rDoc = pDocSh->GetDocument();
-
-    ScAddress aCellPos(nCol1, nRow, nTab);
-    ScRefCellValue aCell(rDoc, aCellPos);
-    if (aCell.meType != CELLTYPE_STRING && aCell.meType != CELLTYPE_EDIT)
-        return false;
-
-    const std::vector<editeng::MisspellRanges>* pRanges = mpSpellCheckCxt->getMisspellRanges(nCol1, nRow);
-    if (!pRanges)
-        return false;
-
-    const ScPatternAttr* pPattern = rDoc.GetPattern(nCol1, nRow, nTab);
-
-    tools::Rectangle aEditRect = mrViewData.GetEditArea(eWhich, nCol1, nRow, this, pPattern, false);
-    if (rPos.Y() < aEditRect.Top())
-        return false;
-
-    std::shared_ptr<ScFieldEditEngine> pEngine = createEditEngine(pDocSh, *pPattern);
-
-    Size aPaperSize(1000000, 1000000);
-    pEngine->SetPaperSize(aPaperSize);
-
-    if (aCell.meType == CELLTYPE_EDIT)
-        pEngine->SetTextCurrentDefaults(*aCell.mpEditText);
-    else
-        pEngine->SetTextCurrentDefaults(aCell.mpString->getString());
-
-    tools::Long nTextWidth = static_cast<tools::Long>(pEngine->CalcTextWidth());
-
-    MapMode aEditMode = mrViewData.GetLogicMode(eWhich);
-    tools::Rectangle aLogicEdit = PixelToLogic(aEditRect, aEditMode);
-    Point aLogicClick = PixelToLogic(rPos, aEditMode);
-
-    aLogicEdit.setWidth(nTextWidth + 1);
-
-    if (!aLogicEdit.Contains(aLogicClick))
-        return false;
-
-    pEngine->SetControlWord(pEngine->GetControlWord() | EEControlBits::ONLINESPELLING);
-    pEngine->SetAllMisspellRanges(*pRanges);
-
-    EditView aTempView(pEngine.get(), this);
-    aTempView.SetOutputArea(aLogicEdit);
-
-    return aTempView.IsWrongSpelledWordAtPos(rPos);
 }
 
 bool ScGridWindow::HasScenarioButton( const Point& rPosPixel, ScRange& rScenRange )

editeng/source/editeng/editview.cxx
@@ -978,7 +978,7 @@ static void LOKSendSpellPopupMenu(const weld::Menu& rMenu, LanguageType nGuessLa
     pViewShell->libreOfficeKitViewCallback(LOK_CALLBACK_CONTEXT_MENU, aStream.str().c_str());
 }
 
-void EditView::ExecuteSpellPopup(const Point& rPosPixel, const Link<SpellCallbackInfo&,void> &rCallBack)
+bool EditView::ExecuteSpellPopup(const Point& rPosPixel, const Link<SpellCallbackInfo&,void> &rCallBack)
 {
     OutputDevice& rDevice = pImpEditView->GetOutputDevice();
     Point aPos(rDevice.PixelToLogic(rPosPixel));
@@ -987,7 +987,7 @@ void EditView::ExecuteSpellPopup(const Point& rPosPixel, const Link<SpellCallbac
     Reference< linguistic2::XSpellChecker1 >  xSpeller( pImpEditView->pEditEngine->pImpEditEngine->GetSpeller() );
     ESelection aOldSel = GetSelection();
     if ( !(xSpeller.is() && pImpEditView->IsWrongSpelledWord( aPaM, true )) )
-        return;
+        return false;
 
     // PaMtoEditCursor returns Logical units
     tools::Rectangle aTempRect = pImpEditView->pEditEngine->pImpEditEngine->PaMtoEditCursor( aPaM, GetCursorFlags::TextOnly );
@@ -1167,7 +1167,7 @@ void EditView::ExecuteSpellPopup(const Point& rPosPixel, const Link<SpellCallbac
         xPopupMenu->remove("autocorrectdlg");
 
         LOKSendSpellPopupMenu(*xPopupMenu, nGuessLangWord, nGuessLangPara, nWords);
-        return;
+        return true;
     }
 
     OString sId = xPopupMenu->popup_at_rect(pPopupParent, aTempRect);
@@ -1267,6 +1267,7 @@ void EditView::ExecuteSpellPopup(const Point& rPosPixel, const Link<SpellCallbac
     {
         SetSelection( aOldSel );
     }
+    return true;
 }
 
 OUString EditView::SpellIgnoreWord()

include/editeng/editview.hxx
@@ -305,7 +305,7 @@ public:
 
     bool            IsCursorAtWrongSpelledWord();
     bool            IsWrongSpelledWordAtPos( const Point& rPosPixel, bool bMarkIfWrong = false );
-    void            ExecuteSpellPopup(const Point& rPosPixel, const Link<SpellCallbackInfo&,void>& rCallBack);
+    bool            ExecuteSpellPopup(const Point& rPosPixel, const Link<SpellCallbackInfo&,void>& rCallBack);
     OUString        SpellIgnoreWord();
 
     void                InsertField( const SvxFieldItem& rFld );

sc/source/ui/inc/gridwin.hxx
@@ -283,8 +283,6 @@ class SAL_DLLPUBLIC_RTTI ScGridWindow : public vcl::Window, public DropTargetHel
     bool            GetEditUrl( const Point& rPos,
                                 OUString* pName=nullptr, OUString* pUrl=nullptr, OUString* pTarget=nullptr );
 
-    bool IsSpellErrorAtPos( const Point& rPos, SCCOL nCol1, SCROW nRow );
-
     bool            HitRangeFinder( const Point& rMouse, RfCorner& rCorner, sal_uInt16* pIndex,
                                     SCCOL* pAddX, SCROW* pAddY );
 

commit 4cf4516bac88fb8f58743e6ae68eab0393059d7f

tdf#157038 sc: avoid spell-check dialog from outside selection
Fixes 7.4 regression from cf18038c66075f7a18d89e47f3a2ab1a5bf7c4fd

To avoid the HIGHLY LIKELY risk of losing the selection
because of the LOW POTENTIAL of the right click
being over a misspelling (that overflowed from outside the selection),
simply don't even attempt to spell-check in that case.

The opposite was already true (and is even worse).
If the misspelling originates in the selected cells,
but has overflowed into an unselected area,
right clicking on the misspelling is deliberately ignored
by SelectForContextMenu().

Change-Id: I7dfc5b190036f1705d848cb621e527cbff37e425
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/157430
Tested-by: Jenkins
Reviewed-by: Justin Luth <jluth@mail.com>

sc/source/ui/view/gridwin.cxx
@@ -3314,9 +3314,22 @@ void ScGridWindow::Command( const CommandEvent& rCEvt )
             if (aPos.Col() >= 0 && (aSpellCheckCell.getType() == CELLTYPE_STRING || aSpellCheckCell.getType() == CELLTYPE_EDIT))
                 nColSpellError = aPos.Col();
 
-            // Is there a misspelled word somewhere in the cell?
+            // Is there possibly a misspelled word somewhere in the cell?
             // A "yes" does not mean that the word under the mouse pointer is wrong though.
             bSpellError = (mpSpellCheckCxt->isMisspelled(nColSpellError, nCellY));
+            // Avoid situations where selecting the cell-with-wrong-spelling would be bad
+            if (bSpellError)
+            {
+                // When the mouse is over an empty cell, text with spelling errors
+                // potentially could have overflowed underneath the mouse pointer
+                if (nColSpellError != nCellX)
+                {
+                    // If the mouse is over a selected cell, only consider spell-checking
+                    // if the cell with the misspelling is also selected. tdf#157038
+                    if (mrViewData.GetMarkData().IsCellMarked(nCellX, nCellY))
+                        bSpellError = mrViewData.GetMarkData().IsCellMarked(nColSpellError, nCellY);
+                }
+            }
         }
 
         //  #i18735# First select the item under the mouse pointer.