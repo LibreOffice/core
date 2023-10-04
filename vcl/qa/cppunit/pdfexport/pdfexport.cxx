LibreOffice/core

commit d467f1aa3d028f399826c97e2eecedcd79efcf65

tdf#154982 drawinglayer,svx,sw,vcl: PDF export: hell flys and shapes...
... should be below their anchor paragraph in the structure tree.

Refactor SwEnhancedPDFExportHelper (etc.) to use the new
EnsureStructureElement()/InitStructureElement() functions instead of
SetCurrentStructureElement() for the frames, and allow it for flys that
don't have their anchor paragraphs created yet because the hell layer is
exported before the document body.

Change-Id: I1be3b54002e8196772e6f9d81dd0fd0c85b6e34b
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/154399
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

drawinglayer/source/processor2d/vclmetafileprocessor2d.cxx
@@ -2527,7 +2527,7 @@ void VclMetafileProcessor2D::processStructureTagPrimitive2D(
     // structured tag primitive
     const vcl::PDFWriter::StructElement& rTagElement(rStructureTagCandidate.getStructureElement());
     bool bTagUsed((vcl::PDFWriter::NonStructElement != rTagElement));
-    sal_Int32 nPreviousElement(-1);
+    bool bNeedEndAnchor(false);
 
     if (!rStructureTagCandidate.isTaggedSdrObject())
     {
@@ -2539,19 +2539,12 @@ void VclMetafileProcessor2D::processStructureTagPrimitive2D(
         // foreground object: tag as regular structure element
         if (!rStructureTagCandidate.isBackground())
         {
-            if (rStructureTagCandidate.GetAnchorStructureElementId() != -1)
+            if (rStructureTagCandidate.GetAnchorStructureElementKey() != nullptr)
             {
-                auto const nTemp = mpPDFExtOutDevData->GetCurrentStructureElement();
-                bool const bSuccess = mpPDFExtOutDevData->SetCurrentStructureElement(
-                    rStructureTagCandidate.GetAnchorStructureElementId());
-                if (bSuccess)
-                {
-                    nPreviousElement = nTemp;
-                }
-                else
-                {
-                    SAL_WARN("drawinglayer", "anchor structure element not found?");
-                }
+                sal_Int32 const id = mpPDFExtOutDevData->EnsureStructureElement(
+                    rStructureTagCandidate.GetAnchorStructureElementKey());
+                mpPDFExtOutDevData->BeginStructureElement(id);
+                bNeedEndAnchor = true;
             }
             mpPDFExtOutDevData->WrapBeginStructureElement(rTagElement);
             switch (rTagElement)
@@ -2627,13 +2620,9 @@ void VclMetafileProcessor2D::processStructureTagPrimitive2D(
     {
         // write end tag
         mpPDFExtOutDevData->EndStructureElement();
-        if (nPreviousElement != -1)
+        if (bNeedEndAnchor)
         {
-#ifndef NDEBUG
-            bool const bSuccess =
-#endif
-                mpPDFExtOutDevData->SetCurrentStructureElement(nPreviousElement);
-            assert(bSuccess);
+            mpPDFExtOutDevData->EndStructureElement();
         }
     }
 }

sw/source/core/text/EnhancedPDFExportHelper.cxx
@@ -96,7 +96,7 @@ TableColumnsMap SwEnhancedPDFExportHelper::s_aTableColumnsMap;
 LinkIdMap SwEnhancedPDFExportHelper::s_aLinkIdMap;
 NumListIdMap SwEnhancedPDFExportHelper::s_aNumListIdMap;
 NumListBodyIdMap SwEnhancedPDFExportHelper::s_aNumListBodyIdMap;
-FrameTagIdMap SwEnhancedPDFExportHelper::s_aFrameTagIdMap;
+FrameTagSet SwEnhancedPDFExportHelper::s_FrameTagSet;
 
 LanguageType SwEnhancedPDFExportHelper::s_eLanguageDefault = LANGUAGE_SYSTEM;
 
@@ -361,32 +361,16 @@ SwTaggedPDFHelper::~SwTaggedPDFHelper()
 #endif
 }
 
-static auto GetReopenTagFromFrame(SwFrame const& rFrame) -> sal_Int32
-{
-    void const*const pKey = lcl_GetKeyFromFrame(rFrame);
-
-    if (pKey)
-    {
-        FrameTagIdMap const& rFrameTagIdMap(SwEnhancedPDFExportHelper::GetFrameTagIdMap());
-        auto const it(rFrameTagIdMap.find(pKey));
-        if (it != rFrameTagIdMap.end())
-        {
-            return (*it).second;
-        }
-    }
-    return -1;
-}
-
-sal_Int32 SwDrawContact::GetPDFAnchorStructureElementId(SdrObject const& rObj)
+void const* SwDrawContact::GetPDFAnchorStructureElementKey(SdrObject const& rObj)
 {
     SwFrame const*const pAnchorFrame(GetAnchoredObj(&rObj)->GetAnchorFrame());
-    return pAnchorFrame ? GetReopenTagFromFrame(*pAnchorFrame) : -1;
+    return pAnchorFrame ? lcl_GetKeyFromFrame(*pAnchorFrame) : nullptr;
 }
 
 bool SwTaggedPDFHelper::CheckReopenTag()
 {
     bool bRet = false;
-    sal_Int32 nReopenTag = -1;
+    void const* pReopenKey(nullptr);
     bool bContinue = false; // in some cases we just have to reopen a tag without early returning
 
     if ( mpFrameInfo )
@@ -423,21 +407,27 @@ bool SwTaggedPDFHelper::CheckReopenTag()
 
         if ( pKeyFrame )
         {
-            nReopenTag = GetReopenTagFromFrame(*pKeyFrame);
+            void const*const pKey = lcl_GetKeyFromFrame(*pKeyFrame);
+            FrameTagSet& rFrameTagSet(SwEnhancedPDFExportHelper::GetFrameTagSet());
+            if (rFrameTagSet.find(pKey) != rFrameTagSet.end()
+                || rFrame.IsFlyFrame()) // for hell layer flys
+            {
+                pReopenKey = pKey;
+            }
         }
     }
 
-    if ( -1 != nReopenTag )
+    if (pReopenKey)
     {
-        m_nRestoreCurrentTag = mpPDFExtOutDevData->GetCurrentStructureElement();
-        const bool bSuccess = mpPDFExtOutDevData->SetCurrentStructureElement( nReopenTag );
-        OSL_ENSURE( bSuccess, "Failed to reopen tag" );
+        sal_Int32 const id = mpPDFExtOutDevData->EnsureStructureElement(pReopenKey);
+        mpPDFExtOutDevData->BeginStructureElement(id);
+        ++m_nEndStructureElement;
 
 #if OSL_DEBUG_LEVEL > 1
         aStructStack.push_back( 99 );
 #endif
 
-        bRet = bSuccess;
+        bRet = true;
     }
 
     return bRet && !bContinue;
@@ -458,8 +448,33 @@ void SwTaggedPDFHelper::CheckRestoreTag() const
 
 void SwTaggedPDFHelper::BeginTag( vcl::PDFWriter::StructElement eType, const OUString& rString )
 {
+    void const* pKey(nullptr);
+
+    if ( mpFrameInfo )
+    {
+        const SwFrame& rFrame = mpFrameInfo->mrFrame;
+
+        if ( ( rFrame.IsPageFrame() && !static_cast<const SwPageFrame&>(rFrame).GetPrev() ) ||
+             ( rFrame.IsFlowFrame() && !SwFlowFrame::CastFlowFrame(&rFrame)->IsFollow() && SwFlowFrame::CastFlowFrame(&rFrame)->HasFollow() ) ||
+             ( rFrame.IsTextFrame() && rFrame.GetDrawObjs() ) ||
+             ( rFrame.IsRowFrame() && rFrame.IsInSplitTableRow() ) ||
+             ( rFrame.IsCellFrame() && const_cast<SwFrame&>(rFrame).GetNextCellLeaf() ) )
+        {
+            pKey = lcl_GetKeyFromFrame(rFrame);
+
+            if (pKey)
+            {
+                FrameTagSet& rFrameTagSet(SwEnhancedPDFExportHelper::GetFrameTagSet());
+                assert(rFrameTagSet.find(pKey) == rFrameTagSet.end());
+                rFrameTagSet.emplace(pKey);
+            }
+        }
+    }
+
     // write new tag
-    const sal_Int32 nId = mpPDFExtOutDevData->WrapBeginStructureElement( eType, rString );
+    const sal_Int32 nId = mpPDFExtOutDevData->EnsureStructureElement(pKey);
+    mpPDFExtOutDevData->InitStructureElement(nId, eType, rString);
+    mpPDFExtOutDevData->BeginStructureElement(nId);
     ++m_nEndStructureElement;
 
 #if OSL_DEBUG_LEVEL > 1
@@ -502,21 +517,6 @@ void SwTaggedPDFHelper::BeginTag( vcl::PDFWriter::StructElement eType, const OUS
             SwNodeNum const*const pNodeNum(pTextNd->GetNum(rTextFrame.getRootFrame()));
             NumListBodyIdMap& rNumListBodyIdMap = SwEnhancedPDFExportHelper::GetNumListBodyIdMap();
             rNumListBodyIdMap[ pNodeNum ] = nId;
-        }
-
-        if ( ( rFrame.IsPageFrame() && !static_cast<const SwPageFrame&>(rFrame).GetPrev() ) ||
-             ( rFrame.IsFlowFrame() && !SwFlowFrame::CastFlowFrame(&rFrame)->IsFollow() && SwFlowFrame::CastFlowFrame(&rFrame)->HasFollow() ) ||
-             ( rFrame.IsTextFrame() && rFrame.GetDrawObjs() ) ||
-             ( rFrame.IsRowFrame() && rFrame.IsInSplitTableRow() ) ||
-             ( rFrame.IsCellFrame() && const_cast<SwFrame&>(rFrame).GetNextCellLeaf() ) )
-        {
-            const void* pKey = lcl_GetKeyFromFrame( rFrame );
-
-            if ( pKey )
-            {
-                FrameTagIdMap& rFrameTagIdMap = SwEnhancedPDFExportHelper::GetFrameTagIdMap();
-                rFrameTagIdMap[ pKey ] = nId;
-            }
         }
     }
 
@@ -1710,7 +1710,7 @@ SwEnhancedPDFExportHelper::SwEnhancedPDFExportHelper( SwEditShell& rSh,
     s_aLinkIdMap.clear();
     s_aNumListIdMap.clear();
     s_aNumListBodyIdMap.clear();
-    s_aFrameTagIdMap.clear();
+    s_FrameTagSet.clear();
 
 #if OSL_DEBUG_LEVEL > 1
     aStructStack.clear();

vcl/qa/cppunit/pdfexport/pdfexport.cxx
@@ -3596,20 +3596,20 @@ CPPUNIT_TEST_FIXTURE(PdfExportTest, testTdf57423)
             {
                 switch (nFigure)
                 {
-                    case 0:
+                    case 2:
                         CPPUNIT_ASSERT_EQUAL(OUString(u"QR Code - Tells how to get to Mosegaard"),
                                              ::vcl::filter::PDFDocument::DecodeHexStringUTF16BE(
                                                  *dynamic_cast<vcl::filter::PDFHexStringElement*>(
                                                      pObject->Lookup("Alt"))));
                         break;
-                    case 1:
+                    case 0:
                         CPPUNIT_ASSERT_EQUAL(OUString(u"Title: Arrows - Description:  Explains the "
                                                       u"different arrow appearances"),
                                              ::vcl::filter::PDFDocument::DecodeHexStringUTF16BE(
                                                  *dynamic_cast<vcl::filter::PDFHexStringElement*>(
                                                      pObject->Lookup("Alt"))));
                         break;
-                    case 2:
+                    case 1:
                         CPPUNIT_ASSERT_EQUAL(
                             OUString(u"My blue triangle - Does not need further description"),
                             ::vcl::filter::PDFDocument::DecodeHexStringUTF16BE(
@@ -3662,6 +3662,83 @@ CPPUNIT_TEST_FIXTURE(PdfExportTest, testTdf57423)
     CPPUNIT_ASSERT_EQUAL(int(3), nFigure);
     CPPUNIT_ASSERT_EQUAL(int(1), nFormula);
     CPPUNIT_ASSERT_EQUAL(int(4), nDiv);
+}
+
+CPPUNIT_TEST_FIXTURE(PdfExportTest, testTdf154982)
+{
+    aMediaDescriptor["FilterName"] <<= OUString("writer_pdf_Export");
+
+    // Enable PDF/UA
+    uno::Sequence<beans::PropertyValue> aFilterData(
+        comphelper::InitPropertySequence({ { "PDFUACompliance", uno::Any(true) } }));
+    aMediaDescriptor["FilterData"] <<= aFilterData;
+    saveAsPDF(u"tdf154982.odt");
+
+    vcl::filter::PDFDocument aDocument;
+    SvFileStream aStream(maTempFile.GetURL(), StreamMode::READ);
+    CPPUNIT_ASSERT(aDocument.Read(aStream));
+
+    // The document has one page.
+    std::vector<vcl::filter::PDFObjectElement*> aPages = aDocument.GetPages();
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(1), aPages.size());
+
+    int nFigure(0);
+    for (const auto& rDocElement : aDocument.GetElements())
+    {
+        auto pObject = dynamic_cast<vcl::filter::PDFObjectElement*>(rDocElement.get());
+        if (!pObject)
+            continue;
+        auto pType = dynamic_cast<vcl::filter::PDFNameElement*>(pObject->Lookup("Type"));
+        if (pType && pType->GetValue() == "StructElem")
+        {
+            auto pS = dynamic_cast<vcl::filter::PDFNameElement*>(pObject->Lookup("S"));
+            if (pS && pS->GetValue() == "Figure")
+            {
+                switch (nFigure)
+                {
+                    case 0:
+                        CPPUNIT_ASSERT_EQUAL(
+                            OUString(u"Here comes the signature - Please sign here"),
+                            ::vcl::filter::PDFDocument::DecodeHexStringUTF16BE(
+                                *dynamic_cast<vcl::filter::PDFHexStringElement*>(
+                                    pObject->Lookup("Alt"))));
+                        break;
+                    case 1:
+                        CPPUNIT_ASSERT_EQUAL(OUString(u"Home"),
+                                             ::vcl::filter::PDFDocument::DecodeHexStringUTF16BE(
+                                                 *dynamic_cast<vcl::filter::PDFHexStringElement*>(
+                                                     pObject->Lookup("Alt"))));
+                        break;
+                }
+
+                // the problem was that the figures in the hell layer were not
+                // below their anchor paragraphs in the structure tree
+                auto pParentRef
+                    = dynamic_cast<vcl::filter::PDFReferenceElement*>(pObject->Lookup("P"));
+                CPPUNIT_ASSERT(pParentRef);
+                auto pParent(pParentRef->LookupObject());
+                CPPUNIT_ASSERT(pParent);
+                auto pParentType
+                    = dynamic_cast<vcl::filter::PDFNameElement*>(pParent->Lookup("Type"));
+                CPPUNIT_ASSERT_EQUAL(OString("StructElem"), pParentType->GetValue());
+                auto pParentS = dynamic_cast<vcl::filter::PDFNameElement*>(pParent->Lookup("S"));
+                CPPUNIT_ASSERT_EQUAL(OString("Standard"), pParentS->GetValue());
+
+                auto pPParentRef
+                    = dynamic_cast<vcl::filter::PDFReferenceElement*>(pParent->Lookup("P"));
+                CPPUNIT_ASSERT(pPParentRef);
+                auto pPParent(pPParentRef->LookupObject());
+                CPPUNIT_ASSERT(pPParent);
+                auto pPParentType
+                    = dynamic_cast<vcl::filter::PDFNameElement*>(pPParent->Lookup("Type"));
+                CPPUNIT_ASSERT_EQUAL(OString("StructElem"), pPParentType->GetValue());
+                auto pPParentS = dynamic_cast<vcl::filter::PDFNameElement*>(pPParent->Lookup("S"));
+                CPPUNIT_ASSERT_EQUAL(OString("Document"), pPParentS->GetValue());
+                ++nFigure;
+            }
+        }
+    }
+    CPPUNIT_ASSERT_EQUAL(int(2), nFigure);
 }
 
 CPPUNIT_TEST_FIXTURE(PdfExportTest, testTdf135192)

drawinglayer/source/primitive2d/structuretagprimitive2d.cxx
@@ -31,13 +31,13 @@ namespace drawinglayer::primitive2d
             bool bBackground,
             bool bIsImage,
             Primitive2DContainer&& aChildren,
-            sal_Int32 const nAnchorStructureElementId,
+            void const*const pAnchorStructureElementKey,
             ::std::vector<sal_Int32> const*const pAnnotIds)
         :   GroupPrimitive2D(std::move(aChildren)),
             maStructureElement(rStructureElement),
             mbBackground(bBackground),
             mbIsImage(bIsImage)
-        ,   m_nAnchorStructureElementId(nAnchorStructureElementId)
+        ,   m_pAnchorStructureElementKey(pAnchorStructureElementKey)
         {
             if (pAnnotIds)
             {

include/drawinglayer/primitive2d/structuretagprimitive2d.hxx
@@ -50,7 +50,7 @@ namespace drawinglayer::primitive2d
             /// flag for image (OBJ_GRAF)
             bool                                    mbIsImage;
             /// anchor structure element (Writer)
-            sal_Int32 m_nAnchorStructureElementId;
+            void const* m_pAnchorStructureElementKey;
             /// for Annot structure element, the ids of the annotations
             ::std::vector<sal_Int32> m_AnnotIds;
 
@@ -61,7 +61,7 @@ namespace drawinglayer::primitive2d
                 bool bBackground,
                 bool bIsImage,
                 Primitive2DContainer&& aChildren,
-                sal_Int32 nAnchorStructureElementId = -1,
+                void const* pAnchorStructureElementKey = nullptr,
                 ::std::vector<sal_Int32> const* pAnnotIds = nullptr);
 
             /// data read access
@@ -69,7 +69,7 @@ namespace drawinglayer::primitive2d
             bool isBackground() const { return mbBackground; }
             bool isImage() const { return mbIsImage; }
             bool isTaggedSdrObject() const;
-            sal_Int32 GetAnchorStructureElementId() const { return m_nAnchorStructureElementId; }
+            void const* GetAnchorStructureElementKey() const { return m_pAnchorStructureElementKey; }
             ::std::vector<sal_Int32> GetAnnotIds() const { return m_AnnotIds; }
 
             /// compare operator

include/svx/svdobj.hxx
@@ -129,7 +129,7 @@ class SVXCORE_DLLPUBLIC SdrObjUserCall
 public:
     virtual ~SdrObjUserCall();
     virtual void Changed(const SdrObject& rObj, SdrUserCallType eType, const tools::Rectangle& rOldBoundRect);
-    virtual sal_Int32 GetPDFAnchorStructureElementId(SdrObject const& rObj);
+    virtual void const* GetPDFAnchorStructureElementKey(SdrObject const& rObj);
 };
 
 class SVXCORE_DLLPUBLIC SdrObjMacroHitRec

svx/source/sdr/contact/viewobjectcontact.cxx
@@ -436,10 +436,10 @@ drawinglayer::primitive2d::Primitive2DContainer const & ViewObjectContact::getPr
                     const bool bBackground(pSdrPage->IsMasterPage());
                     const bool bImage(SdrObjKind::Graphic == pSdrObj->GetObjIdentifier());
                     // note: there must be output device here, in PDF export
-                    sal_Int32 nAnchorId(-1);
+                    void const* pAnchorKey(nullptr);
                     if (auto const pUserCall = pSdrObj->GetUserCall())
                     {
-                        nAnchorId = pUserCall->GetPDFAnchorStructureElementId(*pSdrObj);
+                        pAnchorKey = pUserCall->GetPDFAnchorStructureElementKey(*pSdrObj);
                     }
 
                     ::std::vector<sal_Int32> annotIds;
@@ -457,7 +457,7 @@ drawinglayer::primitive2d::Primitive2DContainer const & ViewObjectContact::getPr
                             bBackground,
                             bImage,
                             std::move(xNewPrimitiveSequence),
-                            nAnchorId,
+                            pAnchorKey,
                             &annotIds));
                     xNewPrimitiveSequence = drawinglayer::primitive2d::Primitive2DContainer { xReference };
                 }

svx/source/svdraw/svdobj.cxx
@@ -136,9 +136,9 @@ void SdrObjUserCall::Changed(const SdrObject& /*rObj*/, SdrUserCallType /*eType*
 {
 }
 
-sal_Int32 SdrObjUserCall::GetPDFAnchorStructureElementId(SdrObject const&)
+void const* SdrObjUserCall::GetPDFAnchorStructureElementKey(SdrObject const&)
 {
-    return -1;
+    return nullptr;
 }
 
 SdrObjMacroHitRec::SdrObjMacroHitRec() :

commit a8f522f4257729ea11178ebf165fdd3a2b7be240

vcl,drawinglayer,sw: PDF/UA export: footnote-container-in-list problem
The problem is that if there is a footnote in a list, the footnote
container becomes a child of the LI element that contains the footnote
anchor, which is of course nonsense.

This is because SwTaggedPDFHelper::CheckReopenTag() stopped using
SetCurrentStructureElement(), but unfortunately it's required so that
once the list ends, the correct parent element is restored for whatever
follows the list.

There is a similar problem in VclMetafileProcessor2D.

(regression from commit d467f1aa3d028f399826c97e2eecedcd79efcf65)

Change-Id: I03dee5dc2e11accb97279e5f325808c5b85507a1
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/157501
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

drawinglayer/source/processor2d/vclmetafileprocessor2d.cxx
@@ -2527,7 +2527,7 @@ void VclMetafileProcessor2D::processStructureTagPrimitive2D(
     // structured tag primitive
     const vcl::PDFWriter::StructElement& rTagElement(rStructureTagCandidate.getStructureElement());
     bool bTagUsed((vcl::PDFWriter::NonStructElement != rTagElement));
-    bool bNeedEndAnchor(false);
+    ::std::optional<sal_Int32> oAnchorParent;
 
     if (!rStructureTagCandidate.isTaggedSdrObject())
     {
@@ -2543,8 +2543,8 @@ void VclMetafileProcessor2D::processStructureTagPrimitive2D(
             {
                 sal_Int32 const id = mpPDFExtOutDevData->EnsureStructureElement(
                     rStructureTagCandidate.GetAnchorStructureElementKey());
-                mpPDFExtOutDevData->BeginStructureElement(id);
-                bNeedEndAnchor = true;
+                oAnchorParent.emplace(mpPDFExtOutDevData->GetCurrentStructureElement());
+                mpPDFExtOutDevData->SetCurrentStructureElement(id);
             }
             mpPDFExtOutDevData->WrapBeginStructureElement(rTagElement);
             switch (rTagElement)
@@ -2620,9 +2620,9 @@ void VclMetafileProcessor2D::processStructureTagPrimitive2D(
     {
         // write end tag
         mpPDFExtOutDevData->EndStructureElement();
-        if (bNeedEndAnchor)
+        if (oAnchorParent)
         {
-            mpPDFExtOutDevData->EndStructureElement();
+            mpPDFExtOutDevData->SetCurrentStructureElement(*oAnchorParent);
         }
     }
 }

sw/source/core/text/EnhancedPDFExportHelper.cxx
@@ -458,7 +458,15 @@ bool SwTaggedPDFHelper::CheckReopenTag()
 
     if (pReopenKey)
     {
-        OpenTagImpl(pReopenKey);
+        // note: it would be possible to get rid of the SetCurrentStructureElement()
+        // - which is quite ugly - for most cases by recreating the parents until the
+        // current ancestor, but there are special cases cell frame rowspan > 1 follow
+        // and footnote frame follow where the parent of the follow is different from
+        // the parent of the first one, and so in PDFExtOutDevData the wrong parent
+        // would be restored and used for next elements.
+        m_nRestoreCurrentTag = mpPDFExtOutDevData->GetCurrentStructureElement();
+        sal_Int32 const id = mpPDFExtOutDevData->EnsureStructureElement(pReopenKey);
+        mpPDFExtOutDevData->SetCurrentStructureElement(id);
 
         bRet = true;
     }

vcl/source/gdi/pdfextoutdevdata.cxx
@@ -828,6 +828,9 @@ void PDFExtOutDevData::InitStructureElement(sal_Int32 const id,
     mpPageSyncData->mParaInts.push_back(id);
     mpPageSyncData->mParaStructElements.push_back( eType );
     mpPageSyncData->mParaOUStrings.push_back( rAlias );
+    // update parent: required for hell fly anchor frames in sw, so that on the actual
+    // anchor frame EndStructureElement() resets mCurrentStructElement properly.
+    mpGlobalSyncData->mStructParents[id] = mpGlobalSyncData->mCurrentStructElement;
 }
 
 void PDFExtOutDevData::BeginStructureElement(sal_Int32 const id)

vcl/qa/cppunit/pdfexport/pdfexport.cxx
@@ -3733,7 +3733,7 @@ CPPUNIT_TEST_FIXTURE(PdfExportTest, testSpans)
                 CPPUNIT_ASSERT(pKids1);
                 // assume there are no MCID ref at this level
                 auto vKids1 = pKids1->GetElements();
-                CPPUNIT_ASSERT_EQUAL(size_t(1), vKids1.size());
+                CPPUNIT_ASSERT_EQUAL(size_t(2), vKids1.size());
                 auto pRefKid10 = dynamic_cast<vcl::filter::PDFReferenceElement*>(vKids1[0]);
                 CPPUNIT_ASSERT(pRefKid10);
                 auto pObject10 = pRefKid10->LookupObject();
@@ -3868,8 +3868,7 @@ CPPUNIT_TEST_FIXTURE(PdfExportTest, testSpans)
                 CPPUNIT_ASSERT(pKids1011);
                 // assume there are no MCID ref at this level
                 auto vKids1011 = pKids1011->GetElements();
-                //CPPUNIT_ASSERT_EQUAL(size_t(1), vKids1011.size());
-                //FIXME Div ???
+                CPPUNIT_ASSERT_EQUAL(size_t(1), vKids1011.size());
 
                 auto pRefKid10110 = dynamic_cast<vcl::filter::PDFReferenceElement*>(vKids1011[0]);
                 CPPUNIT_ASSERT(pRefKid10110);
@@ -4076,6 +4075,78 @@ CPPUNIT_TEST_FIXTURE(PdfExportTest, testSpans)
                                      dynamic_cast<vcl::filter::PDFNameElement*>(
                                          pDictA103101->LookupElement("TextDecorationType"))
                                          ->GetValue());
+
+                // now the footnote container - following the list
+                auto pRefKid11 = dynamic_cast<vcl::filter::PDFReferenceElement*>(vKids1[1]);
+                CPPUNIT_ASSERT(pRefKid11);
+                auto pObject11 = pRefKid11->LookupObject();
+                CPPUNIT_ASSERT(pObject11);
+                auto pType11
+                    = dynamic_cast<vcl::filter::PDFNameElement*>(pObject11->Lookup("Type"));
+                CPPUNIT_ASSERT_EQUAL(OString("StructElem"), pType11->GetValue());
+                auto pS11 = dynamic_cast<vcl::filter::PDFNameElement*>(pObject11->Lookup("S"));
+                CPPUNIT_ASSERT_EQUAL(OString("Div"), pS11->GetValue());
+
+                auto pKids11 = dynamic_cast<vcl::filter::PDFArrayElement*>(pObject11->Lookup("K"));
+                CPPUNIT_ASSERT(pKids11);
+                // assume there are no MCID ref at this level
+                auto vKids11 = pKids11->GetElements();
+                CPPUNIT_ASSERT_EQUAL(size_t(1), vKids11.size());
+
+                auto pRefKid110 = dynamic_cast<vcl::filter::PDFReferenceElement*>(vKids11[0]);
+                CPPUNIT_ASSERT(pRefKid110);
+                auto pObject110 = pRefKid110->LookupObject();
+                CPPUNIT_ASSERT(pObject110);
+                auto pType110
+                    = dynamic_cast<vcl::filter::PDFNameElement*>(pObject110->Lookup("Type"));
+                CPPUNIT_ASSERT_EQUAL(OString("StructElem"), pType110->GetValue());
+                auto pS110 = dynamic_cast<vcl::filter::PDFNameElement*>(pObject110->Lookup("S"));
+                CPPUNIT_ASSERT_EQUAL(OString("Note"), pS110->GetValue());
+
+                auto pKids110
+                    = dynamic_cast<vcl::filter::PDFArrayElement*>(pObject110->Lookup("K"));
+                CPPUNIT_ASSERT(pKids110);
+                // assume there are no MCID ref at this level
+                auto vKids110 = pKids110->GetElements();
+                CPPUNIT_ASSERT_EQUAL(size_t(2), vKids110.size());
+
+                auto pRefKid1100 = dynamic_cast<vcl::filter::PDFReferenceElement*>(vKids110[0]);
+                CPPUNIT_ASSERT(pRefKid1100);
+                auto pObject1100 = pRefKid1100->LookupObject();
+                CPPUNIT_ASSERT(pObject1100);
+                auto pType1100
+                    = dynamic_cast<vcl::filter::PDFNameElement*>(pObject1100->Lookup("Type"));
+                CPPUNIT_ASSERT_EQUAL(OString("StructElem"), pType1100->GetValue());
+                auto pS1100 = dynamic_cast<vcl::filter::PDFNameElement*>(pObject1100->Lookup("S"));
+                CPPUNIT_ASSERT_EQUAL(OString("Lbl"), pS1100->GetValue());
+
+                auto pKids1100
+                    = dynamic_cast<vcl::filter::PDFArrayElement*>(pObject1100->Lookup("K"));
+                CPPUNIT_ASSERT(pKids1100);
+                // assume there are no MCID ref at this level
+                auto vKids1100 = pKids1100->GetElements();
+                CPPUNIT_ASSERT_EQUAL(size_t(1), vKids1100.size());
+
+                auto pRefKid11000 = dynamic_cast<vcl::filter::PDFReferenceElement*>(vKids1100[0]);
+                CPPUNIT_ASSERT(pRefKid11000);
+                auto pObject11000 = pRefKid11000->LookupObject();
+                CPPUNIT_ASSERT(pObject11000);
+                auto pType11000
+                    = dynamic_cast<vcl::filter::PDFNameElement*>(pObject11000->Lookup("Type"));
+                CPPUNIT_ASSERT_EQUAL(OString("StructElem"), pType11000->GetValue());
+                auto pS11000
+                    = dynamic_cast<vcl::filter::PDFNameElement*>(pObject11000->Lookup("S"));
+                CPPUNIT_ASSERT_EQUAL(OString("Link"), pS11000->GetValue());
+
+                auto pRefKid1101 = dynamic_cast<vcl::filter::PDFReferenceElement*>(vKids110[1]);
+                CPPUNIT_ASSERT(pRefKid1101);
+                auto pObject1101 = pRefKid1101->LookupObject();
+                CPPUNIT_ASSERT(pObject1101);
+                auto pType1101
+                    = dynamic_cast<vcl::filter::PDFNameElement*>(pObject1101->Lookup("Type"));
+                CPPUNIT_ASSERT_EQUAL(OString("StructElem"), pType1101->GetValue());
+                auto pS1101 = dynamic_cast<vcl::filter::PDFNameElement*>(pObject1101->Lookup("S"));
+                CPPUNIT_ASSERT_EQUAL(OString("Footnote"), pS1101->GetValue());
 
                 ++nDoc;
             }