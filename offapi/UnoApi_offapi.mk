LibreOffice/core

commit d9e322d60f65ff20631dab87baa5a2c7c71daaa2

replace ErrorInfo with simpler mechanism
Instead of returning ErrCode class everywhere, return a new
class ErrrCodeMsg, which combines an ErrCode with the other
parameters that are used to control the error reporting.

I do not change everything that uses ErrCode here, I started
from SfxBaseController/SfxMedium and worked outwards.

This change serves two purposes

(1) Replace the extremely whacky ErrorInfo mechanism we were
using to smuggle information into the error handler reporting
mechanism with a very straightforward approach of just combining it
into the error class.
(2) Allow us to capture the source location that produced the error,
which makes debugging the source of a problem soooo much easier.

Change-Id: I978b8f00c9851b41a216c7ebdef2ef94251d5519
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/157440
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sfx2/source/doc/objmisc.cxx
@@ -209,7 +209,7 @@ std::vector<InfobarData>& SfxObjectShell::getPendingInfobars()
     return Get_Impl()->m_aPendingInfobars;
 }
 
-void SfxObjectShell::SetError(ErrCode lErr)
+void SfxObjectShell::SetError(const ErrCodeMsg& lErr)
 {
     if (pImpl->lErr==ERRCODE_NONE)
     {
@@ -217,14 +217,14 @@ void SfxObjectShell::SetError(ErrCode lErr)
     }
 }
 
-ErrCode SfxObjectShell::GetErrorIgnoreWarning() const
+ErrCodeMsg SfxObjectShell::GetErrorIgnoreWarning() const
 {
     return GetErrorCode().IgnoreWarning();
 }
 
-ErrCode SfxObjectShell::GetErrorCode() const
+ErrCodeMsg SfxObjectShell::GetErrorCode() const
 {
-    ErrCode lError=pImpl->lErr;
+    ErrCodeMsg lError=pImpl->lErr;
     if(!lError && GetMedium())
         lError=GetMedium()->GetErrorCode();
     return lError;
@@ -1693,7 +1693,7 @@ bool SfxObjectShell::IsUIActive() const
 
 bool SfxObjectShell::UseInteractionToHandleError(
                     const uno::Reference< task::XInteractionHandler >& xHandler,
-                    ErrCode nError )
+                    const ErrCodeMsg& nError )
 {
     bool bResult = false;
 
@@ -1709,7 +1709,7 @@ bool SfxObjectShell::UseInteractionToHandleError(
             };
 
             task::ErrorCodeRequest aErrorCode;
-            aErrorCode.ErrCode = sal_uInt32(nError);
+            aErrorCode.ErrCode = sal_uInt32(nError.GetCode());
             aInteraction <<= aErrorCode;
             xHandler->handle(::framework::InteractionRequest::CreateRequest (aInteraction,lContinuations));
             bResult = pAbort->wasSelected();

sfx2/source/doc/sfxbasemodel.cxx
@@ -1697,7 +1697,7 @@ void SAL_CALL SfxBaseModel::storeSelf( const    Sequence< beans::PropertyValue >
 
     pParams.reset();
 
-    ErrCode nErrCode = m_pData->m_pObjectShell->GetErrorIgnoreWarning() ? m_pData->m_pObjectShell->GetErrorIgnoreWarning()
+    ErrCodeMsg nErrCode = m_pData->m_pObjectShell->GetErrorIgnoreWarning() ? m_pData->m_pObjectShell->GetErrorIgnoreWarning()
                                                            : ERRCODE_IO_CANTWRITE;
     m_pData->m_pObjectShell->ResetError();
 
@@ -1714,7 +1714,7 @@ void SAL_CALL SfxBaseModel::storeSelf( const    Sequence< beans::PropertyValue >
 
         throw task::ErrorCodeIOException(
             "SfxBaseModel::storeSelf: " + nErrCode.toString(),
-            Reference< XInterface >(), sal_uInt32(nErrCode));
+            Reference< XInterface >(), sal_uInt32(nErrCode.GetCode()));
     }
 }
 
@@ -1882,14 +1882,14 @@ void SAL_CALL SfxBaseModel::initNew()
         throw frame::DoubleInitializationException();
 
     bool bRes = m_pData->m_pObjectShell->DoInitNew();
-    ErrCode nErrCode = m_pData->m_pObjectShell->GetErrorIgnoreWarning() ?
+    ErrCodeMsg nErrCode = m_pData->m_pObjectShell->GetErrorIgnoreWarning() ?
                        m_pData->m_pObjectShell->GetErrorIgnoreWarning() : ERRCODE_IO_CANTCREATE;
     m_pData->m_pObjectShell->ResetError();
 
     if ( !bRes )
         throw task::ErrorCodeIOException(
             "SfxBaseModel::initNew: " + nErrCode.toString(),
-            Reference< XInterface >(), sal_uInt32(nErrCode));
+            Reference< XInterface >(), sal_uInt32(nErrCode.GetCode()));
 }
 
 namespace {
@@ -1936,7 +1936,7 @@ void SAL_CALL SfxBaseModel::load(   const Sequence< beans::PropertyValue >& seqA
 
     SfxMedium* pMedium = new SfxMedium( seqArguments );
 
-    ErrCode nError = ERRCODE_NONE;
+    ErrCodeMsg nError = ERRCODE_NONE;
     if (!getFilterProvider(*pMedium).isEmpty())
     {
         if (!m_pData->m_pObjectShell->DoLoadExternal(pMedium))
@@ -2793,14 +2793,15 @@ void SfxBaseModel::loadCmisProperties( )
     }
 }
 
-SfxMedium* SfxBaseModel::handleLoadError( ErrCode nError, SfxMedium* pMedium )
+SfxMedium* SfxBaseModel::handleLoadError( const ErrCodeMsg& rError, SfxMedium* pMedium )
 {
-    if (!nError)
+    if (!rError)
     {
         // No error condition.
         return pMedium;
     }
 
+    ErrCodeMsg nError = rError;
     bool bSilent = false;
     const SfxBoolItem* pSilentItem = pMedium->GetItemSet().GetItem(SID_SILENT, false);
     if( pSilentItem )
@@ -2830,7 +2831,7 @@ SfxMedium* SfxBaseModel::handleLoadError( ErrCode nError, SfxMedium* pMedium )
         nError = nError ? nError : ERRCODE_IO_CANTREAD;
         throw task::ErrorCodeIOException(
             "SfxBaseModel::handleLoadError: 0x" + nError.toString(),
-            Reference< XInterface >(), sal_uInt32(nError));
+            Reference< XInterface >(), sal_uInt32(nError.GetCode()));
     }
     else
         pMedium->SetWarningError(nError);
@@ -3204,7 +3205,7 @@ void SfxBaseModel::impl_store(  const   OUString&                   sURL        
 
     pItemSet.reset();
 
-    ErrCode nErrCode = m_pData->m_pObjectShell->GetErrorCode();
+    ErrCodeMsg nErrCode = m_pData->m_pObjectShell->GetErrorCode();
     if ( !bRet && !nErrCode )
     {
         SAL_WARN("sfx.doc", "Storing has failed, no error is set!");
@@ -3223,7 +3224,7 @@ void SfxBaseModel::impl_store(  const   OUString&                   sURL        
                 SfxErrorContext aEc( ERRCTX_SFX_SAVEASDOC, m_pData->m_pObjectShell->GetTitle() );
 
                 task::ErrorCodeRequest aErrorCode;
-                aErrorCode.ErrCode = sal_uInt32(nErrCode);
+                aErrorCode.ErrCode = sal_uInt32(nErrCode.GetCode());
                 SfxMedium::CallApproveHandler( xHandler, Any( aErrorCode ), false );
             }
         }
@@ -3259,7 +3260,7 @@ void SfxBaseModel::impl_store(  const   OUString&                   sURL        
         aErrCode << nErrCode;
         throw task::ErrorCodeIOException(
             "SfxBaseModel::impl_store <" + sURL + "> failed: " + OUString::fromUtf8(aErrCode.str()),
-            Reference< XInterface >(), sal_uInt32(nErrCode));
+            Reference< XInterface >(), sal_uInt32(nErrCode.GetCode()));
     }
 }
 
@@ -3828,11 +3829,11 @@ void SAL_CALL SfxBaseModel::loadFromStorage( const Reference< embed::XStorage >&
     // load document
     if ( !m_pData->m_pObjectShell->DoLoad(pMedium) )
     {
-        ErrCode nError = m_pData->m_pObjectShell->GetErrorCode();
+        ErrCodeMsg nError = m_pData->m_pObjectShell->GetErrorCode();
         nError = nError ? nError : ERRCODE_IO_CANTREAD;
         throw task::ErrorCodeIOException(
             "SfxBaseModel::loadFromStorage: " + nError.toString(),
-            Reference< XInterface >(), sal_uInt32(nError));
+            Reference< XInterface >(), sal_uInt32(nError.GetCode()));
     }
     loadCmisProperties( );
 }
@@ -3881,7 +3882,7 @@ void SAL_CALL SfxBaseModel::storeToStorage( const Reference< embed::XStorage >& 
         }
     }
 
-    ErrCode nError = m_pData->m_pObjectShell->GetErrorCode();
+    ErrCodeMsg nError = m_pData->m_pObjectShell->GetErrorCode();
     m_pData->m_pObjectShell->ResetError();
 
     // the warnings are currently not transported
@@ -3890,7 +3891,7 @@ void SAL_CALL SfxBaseModel::storeToStorage( const Reference< embed::XStorage >& 
         nError = nError ? nError : ERRCODE_IO_GENERAL;
         throw task::ErrorCodeIOException(
             "SfxBaseModel::storeToStorage: " + nError.toString(),
-            Reference< XInterface >(), sal_uInt32(nError));
+            Reference< XInterface >(), sal_uInt32(nError.GetCode()));
     }
 }
 
@@ -3906,11 +3907,11 @@ void SAL_CALL SfxBaseModel::switchToStorage( const Reference< embed::XStorage >&
     {
         if ( !m_pData->m_pObjectShell->SwitchPersistence( xStorage ) )
         {
-            ErrCode nError = m_pData->m_pObjectShell->GetErrorCode();
+            ErrCodeMsg nError = m_pData->m_pObjectShell->GetErrorCode();
             nError = nError ? nError : ERRCODE_IO_GENERAL;
             throw task::ErrorCodeIOException(
                 "SfxBaseModel::switchToStorage: " + nError.toString(),
-                Reference< XInterface >(), sal_uInt32(nError));
+                Reference< XInterface >(), sal_uInt32(nError.GetCode()));
         }
         else
         {

uui/source/iahndl.cxx
@@ -1225,7 +1225,7 @@ bool ErrorResource::getString(ErrCode nErrorCode, OUString &rString) const
 {
     for (const std::pair<TranslateId, ErrCode>* pStringArray = m_pStringArray; pStringArray->first; ++pStringArray)
     {
-        if (nErrorCode.StripWarningAndDynamic() == pStringArray->second.StripWarningAndDynamic())
+        if (nErrorCode.StripWarning() == pStringArray->second.StripWarning())
         {
             rString = Translate::get(pStringArray->first, m_rResLocale);
             return true;

commit dd009cffc38a280d3b072ae4dc9ee9cda2e1f13f

tdf#157650 Unhelpful error when XML in content.xml is malformed
regression from
    commit d9e322d60f65ff20631dab87baa5a2c7c71daaa2
    replace ErrorInfo with simpler mechanism
we need to tunnel the necessary information through the uui stuff, so
add a new Exception class to do that.

Change-Id: Ie1fde486e8637b802340795348a5e5aad6a54e96
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/157732
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

offapi/UnoApi_offapi.mk
@@ -3699,6 +3699,7 @@ $(eval $(call gb_UnoApi_add_idlfiles,offapi,com/sun/star/task,\
 	DocumentPasswordRequest2 \
 	ErrorCodeIOException \
 	ErrorCodeRequest \
+	ErrorCodeRequest2 \
 	InteractionClassification \
 	MasterPasswordRequest \
 	NoMasterException \

offapi/com/sun/star/task/ErrorCodeRequest2.idl
@@ -1,0 +1,42 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+
+ module com {  module sun {  module star {  module task {
+
+
+/** represents a general error exception.
+    It can be used to transport the error code information.
+    E.g. that can be useful for interactions.
+
+    This is designed to mesh with tools::ErrCodeMsg
+
+    @since LibreOffice 24.2
+ */
+exception ErrorCodeRequest2 : com::sun::star::task::ErrorCodeRequest
+{
+        string Arg1;
+        string Arg2;
+        short DialogMask;
+};
+
+
+}; }; }; };
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sfx2/source/doc/objmisc.cxx
@@ -48,7 +48,7 @@
 
 #include <com/sun/star/uno/Reference.h>
 #include <com/sun/star/uno/Any.h>
-#include <com/sun/star/task/ErrorCodeRequest.hpp>
+#include <com/sun/star/task/ErrorCodeRequest2.hpp>
 
 #include <comphelper/processfactory.hxx>
 #include <comphelper/string.hxx>
@@ -1757,8 +1757,9 @@ bool SfxObjectShell::UseInteractionToHandleError(
                 pAbort, pApprove
             };
 
-            task::ErrorCodeRequest aErrorCode;
-            aErrorCode.ErrCode = sal_uInt32(nError.GetCode());
+            task::ErrorCodeRequest2 aErrorCode(OUString(), uno::Reference<XInterface>(),
+                sal_Int32(sal_uInt32(nError.GetCode())), nError.GetArg1(), nError.GetArg2(),
+                static_cast<sal_Int16>(nError.GetDialogMask()));
             aInteraction <<= aErrorCode;
             xHandler->handle(::framework::InteractionRequest::CreateRequest (aInteraction,lContinuations));
             bResult = pAbort->wasSelected();

sfx2/source/doc/sfxbasemodel.cxx
@@ -30,7 +30,7 @@
 #include <com/sun/star/datatransfer/UnsupportedFlavorException.hpp>
 #include <com/sun/star/task/XInteractionHandler.hpp>
 #include <com/sun/star/task/ErrorCodeIOException.hpp>
-#include <com/sun/star/task/ErrorCodeRequest.hpp>
+#include <com/sun/star/task/ErrorCodeRequest2.hpp>
 #include <com/sun/star/view/XSelectionSupplier.hpp>
 #include <com/sun/star/view/XPrintJobListener.hpp>
 #include <com/sun/star/lang/DisposedException.hpp>
@@ -3220,8 +3220,9 @@ void SfxBaseModel::impl_store(  const   OUString&                   sURL        
                 // TODO/LATER: a general way to set the error context should be available
                 SfxErrorContext aEc( ERRCTX_SFX_SAVEASDOC, m_pData->m_pObjectShell->GetTitle() );
 
-                task::ErrorCodeRequest aErrorCode;
-                aErrorCode.ErrCode = sal_uInt32(nErrCode.GetCode());
+                task::ErrorCodeRequest2 aErrorCode(OUString(), uno::Reference<XInterface>(),
+                    sal_Int32(sal_uInt32(nErrCode.GetCode())), nErrCode.GetArg1(), nErrCode.GetArg2(),
+                    static_cast<sal_Int16>(nErrCode.GetDialogMask()));
                 SfxMedium::CallApproveHandler( xHandler, Any( aErrorCode ), false );
             }
         }

uui/source/iahndl.cxx
@@ -32,7 +32,7 @@
 #include <com/sun/star/lang/XMultiServiceFactory.hpp>
 #include <com/sun/star/script/ModuleSizeExceededRequest.hpp>
 #include <com/sun/star/task/ErrorCodeIOException.hpp>
-#include <com/sun/star/task/ErrorCodeRequest.hpp>
+#include <com/sun/star/task/ErrorCodeRequest2.hpp>
 #include <com/sun/star/task/InteractionHandler.hpp>
 #include <com/sun/star/task/XInteractionAbort.hpp>
 #include <com/sun/star/task/XInteractionApprove.hpp>
@@ -710,6 +710,17 @@ UUIInteractionHelper::handleRequest_impl(
             return true;
         }
 
+        task::ErrorCodeRequest2 aErrorCodeRequest2;
+        if (aAnyRequest >>= aErrorCodeRequest2)
+        {
+            handleGenericErrorRequest(
+                    ErrCodeMsg(ErrCode(aErrorCodeRequest2.ErrCode), aErrorCodeRequest2.Arg1, aErrorCodeRequest2.Arg2, static_cast<DialogMask>(aErrorCodeRequest2.DialogMask)),
+                    rRequest->getContinuations(),
+                    bObtainErrorStringOnly,
+                    bHasErrorString,
+                    rErrorString);
+            return true;
+        }
         task::ErrorCodeRequest aErrorCodeRequest;
         if (aAnyRequest >>= aErrorCodeRequest)
         {
@@ -1047,7 +1058,7 @@ UUIInteractionHelper::handleNameClashResolveRequest(
 
 void
 UUIInteractionHelper::handleGenericErrorRequest(
-    ErrCode nErrorCode,
+    ErrCodeMsg nErrorCode,
     uno::Sequence< uno::Reference<
         task::XInteractionContinuation > > const & rContinuations,
     bool bObtainErrorStringOnly,
@@ -1073,10 +1084,9 @@ UUIInteractionHelper::handleGenericErrorRequest(
         // Note: It's important to convert the transported long to the
         // required  unsigned long value. Otherwise using as flag field
         // can fail ...
-        ErrCode  nError(nErrorCode);
-        bool bWarning = !nError.IgnoreWarning();
+        bool bWarning = !nErrorCode.IgnoreWarning();
 
-        if ( nError == ERRCODE_SFX_INCOMPLETE_ENCRYPTION )
+        if ( nErrorCode == ERRCODE_SFX_INCOMPLETE_ENCRYPTION )
         {
             // the security warning box needs a special title
             OUString aErrorString;

uui/source/iahndl.hxx
@@ -190,7 +190,7 @@ private:
 
     void
     handleGenericErrorRequest(
-        ErrCode nErrorCode,
+        ErrCodeMsg nErrorCode,
         css::uno::Sequence<
             css::uno::Reference< css::task::XInteractionContinuation > > const &  rContinuations,
         bool bObtainErrorStringOnly,