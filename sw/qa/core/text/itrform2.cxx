LibreOffice/core

commit a4af5432753408c4eea8a8d56c2f48202160c5fe

tdf#120262 sw floattable, legacy: fix text wrap around fly when no content fits
The bugdoc is created from DOC (so tables are shifted to the left,
slightly) and has 2 floating tables on 2 pages. Writer puts them to a
single page, so they overlap, which is not wanted.

The trouble is that there is some space (but not much) on the right of
the floating table on page 1, and Writer wraps the empty anchor of the
floating table on the right of the floating table, while Word puts it
below the table, so no overlap happens.

Fix the problem by extending the work from commit
8f8b31abd02876c3601e343b8b3274754f8a61b6 (compatibility setting for MS
Word wrapping text in less space (bnc#822908), 2013-08-06), and work
with that limit in SwTextFormatter::CalcFlyWidth(). This way Writer
keeps its behavior that PARALLEL wrap text mode requires no minimal text
width for existing documents, but correctly ~300 twips minimal text
width (Word formats).

The bugdoc still has a footer vs floating table overlap, but at least no
2 floating tables overlap now.

Change-Id: Ib307ede08ead272fe16dca261db615fd8e9c547d
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/154509
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

sw/source/core/text/itrform2.cxx
@@ -2802,12 +2802,12 @@ void SwTextFormatter::CalcFlyWidth( SwTextFormatInfo &rInf )
 
     bool bFullLine =  aLine.Left()  == aInter.Left() &&
                             aLine.Right() == aInter.Right();
-    if (!bFullLine && bWordFlyWrap)
+    if (!bFullLine && bWordFlyWrap && !GetTextFrame()->IsInTab())
     {
-        // Word >= 2013 style: if there is minimal space remaining, then handle that similar to a
-        // full line and put the actual empty paragraph below the fly.
-        bFullLine = std::abs(aLine.Left() - aInter.Left()) < MINLAY
-                    && std::abs(aLine.Right() - aInter.Right()) < MINLAY;
+        // Word style: if there is minimal space remaining, then handle that similar to a full line
+        // and put the actual empty paragraph below the fly.
+        bFullLine = std::abs(aLine.Left() - aInter.Left()) < TEXT_MIN_SMALL
+                    && std::abs(aLine.Right() - aInter.Right()) < TEXT_MIN_SMALL;
     }
 
     // Although no text is left, we need to format another line,

sw/qa/core/text/itrform2.cxx
@@ -54,6 +54,34 @@ CPPUNIT_TEST_FIXTURE(Test, testFloattableWrapEmptyParagraph)
     const SwSortedObjs& rPageObjs2 = *pPage2->GetSortedObjs();
     CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(1), rPageObjs2.size());
 }
+
+CPPUNIT_TEST_FIXTURE(Test, testFloattableLegacyWrapEmptyParagraph)
+{
+    // Given a document with 2 pages, a floating table on both pages (from DOC, so the table is
+    // shifted towards the left page edge slightly):
+    createSwDoc("floattable-wrap-empty-para-legacy.docx");
+
+    // When calculating the layout:
+    calcLayout();
+
+    // Then make sure that each page has exactly 1 floating table:
+    SwDoc* pDoc = getSwDoc();
+    SwRootFrame* pLayout = pDoc->getIDocumentLayoutAccess().GetCurrentLayout();
+    auto pPage = dynamic_cast<SwPageFrame*>(pLayout->Lower());
+    CPPUNIT_ASSERT(pPage);
+    CPPUNIT_ASSERT(pPage->GetSortedObjs());
+    const SwSortedObjs& rPageObjs = *pPage->GetSortedObjs();
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 1
+    // - Actual  : 2
+    // i.e. both tables were on page 1, leading to an overlap.
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(1), rPageObjs.size());
+    auto pPage2 = dynamic_cast<SwPageFrame*>(pPage->GetNext());
+    CPPUNIT_ASSERT(pPage2);
+    CPPUNIT_ASSERT(pPage2->GetSortedObjs());
+    const SwSortedObjs& rPageObjs2 = *pPage2->GetSortedObjs();
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(1), rPageObjs2.size());
+}
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sw/qa/core/text/text.cxx
@@ -1419,10 +1419,10 @@ CPPUNIT_TEST_FIXTURE(SwCoreTextTest, testParaUpperMarginFlyIntersect)
         nHeight += getXPath(pXmlDoc, xPath, "height").toInt32();
     }
     // Without the accompanying fix in place, this test would have failed with:
-    // - Expected: 521 (~500)
+    // - Expected: 542 (~500)
     // - Actual  : 857 (~1000)
     // I.e. both upper and lower margin was taken into account.
-    CPPUNIT_ASSERT_EQUAL(521, nHeight);
+    CPPUNIT_ASSERT_EQUAL(542, nHeight);
 }
 
 CPPUNIT_TEST_FIXTURE(SwCoreTextTest, testTdf129810)

sw/source/core/inc/txtfly.hxx
@@ -86,6 +86,9 @@ public:
                                        const bool bRight );
 };
 
+// MS Word wraps on sides with even less space (value guessed).
+#define TEXT_MIN_SMALL 300
+
 /**
    The purpose of this class is to be the universal interface between
    formatting/text output and the possibly overlapping free-flying frames.

sw/source/core/text/txtfly.cxx
@@ -1388,9 +1388,6 @@ SwRect SwTextFly::AnchoredObjToRect( const SwAnchoredObject* pAnchoredObj,
 // Wrap only on sides with at least 2cm space for the text
 #define TEXT_MIN 1134
 
-// MS Word wraps on sides with even less space (value guessed).
-#define TEXT_MIN_SMALL 300
-
 // Wrap on both sides up to a frame width of 1.5cm
 #define FRAME_MAX 850
 

commit 4a5fb05d5e2448453477ce14862a8cf9846ecb49

tdf#157571 sw floattable: fix incorrect blank space after table-in-shape
Regression from a4af5432753408c4eea8a8d56c2f48202160c5fe (tdf#120262 sw
floattable, legacy: fix text wrap around fly when no content fits,
2023-07-17), the bugdoc has a shape which contains a table, and lots of
empty paragraphs next to it wrap around the shape. Writer didn't wrap
these empty paragraphs, so some of the page 1 content was shifted to
page 2.

What happened here is that in case there is a really small space for the
wrapping text around a floating object, then Word has some minimal
limit. If the available horizontal space is smaller than the limit, we
don't even try to wrap, even if the content (an empty paragraph) would
fit. It was assumed that this limit is the shape for normal anchored
objects and floating tables, but the two bugdocs show that there are two
different limits here.

Fix the problem by going back to MINLAY as the default limit where we
start wrapping, and only increase that to TEXT_MIN_SMALL when wrapping
around floating tables. That fixes the bugdoc and keeps the older
floating table use-case working as well.

This also allows reverting changes to testParaUpperMarginFlyIntersect,
to assert the non-floating-table case again.

Change-Id: I8f8a776c6ad5bdfa0ee4f197b600463fef6431f0
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/157743
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

sw/source/core/text/itrform2.cxx
@@ -2806,8 +2806,27 @@ void SwTextFormatter::CalcFlyWidth( SwTextFormatInfo &rInf )
     {
         // Word style: if there is minimal space remaining, then handle that similar to a full line
         // and put the actual empty paragraph below the fly.
-        bFullLine = std::abs(aLine.Left() - aInter.Left()) < TEXT_MIN_SMALL
-                    && std::abs(aLine.Right() - aInter.Right()) < TEXT_MIN_SMALL;
+        SwTwips nLimit = MINLAY;
+        for (const auto& pObj : *rTextFly.GetAnchoredObjList())
+        {
+            auto pFlyFrame = pObj->DynCastFlyFrame();
+            if (!pFlyFrame)
+            {
+                continue;
+            }
+
+            if (!pFlyFrame->IsFlySplitAllowed())
+            {
+                continue;
+            }
+
+            // We wrap around a floating table, that has a larger minimal wrap distance.
+            nLimit = TEXT_MIN_SMALL;
+            break;
+        }
+
+        bFullLine = std::abs(aLine.Left() - aInter.Left()) < nLimit
+                    && std::abs(aLine.Right() - aInter.Right()) < nLimit;
     }
 
     // Although no text is left, we need to format another line,

sw/qa/core/text/itrform2.cxx
@@ -82,6 +82,36 @@ CPPUNIT_TEST_FIXTURE(Test, testFloattableLegacyWrapEmptyParagraph)
     const SwSortedObjs& rPageObjs2 = *pPage2->GetSortedObjs();
     CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(1), rPageObjs2.size());
 }
+
+CPPUNIT_TEST_FIXTURE(Test, testFlyMinimalWrap)
+{
+    // Given a document with a first page that has a shape and a table in it (not floating table),
+    // some empty paragraphs wrapping around the shape:
+    createSwDoc("fly-minimal-wrap.docx");
+
+    // When calculating the layout:
+    calcLayout();
+
+    // Then make sure the wrap happens, so the 2nd page only has 2 paragraphs:
+    SwDoc* pDoc = getSwDoc();
+    SwRootFrame* pLayout = pDoc->getIDocumentLayoutAccess().GetCurrentLayout();
+    auto pPage = dynamic_cast<SwPageFrame*>(pLayout->Lower());
+    CPPUNIT_ASSERT(pPage);
+    CPPUNIT_ASSERT(pPage->GetSortedObjs());
+    const SwSortedObjs& rPageObjs = *pPage->GetSortedObjs();
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(2), rPageObjs.size());
+    auto pPage2 = dynamic_cast<SwPageFrame*>(pPage->GetNext());
+    CPPUNIT_ASSERT(pPage2);
+    CPPUNIT_ASSERT(!pPage2->GetSortedObjs());
+    SwLayoutFrame* pBody2 = pPage2->FindBodyCont();
+    SwFrame* pPage2Para1 = pBody2->GetLower();
+    CPPUNIT_ASSERT(pPage2Para1);
+    SwFrame* pPage2Para2 = pPage2Para1->GetNext();
+    CPPUNIT_ASSERT(pPage2Para2);
+    // Without the accompanying fix in place, this test would have failed, the second page had 19
+    // text frames in the body frame, not 2.
+    CPPUNIT_ASSERT(!pPage2Para2->GetNext());
+}
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sw/qa/core/text/text.cxx
@@ -1419,10 +1419,10 @@ CPPUNIT_TEST_FIXTURE(SwCoreTextTest, testParaUpperMarginFlyIntersect)
         nHeight += getXPath(pXmlDoc, xPath, "height").toInt32();
     }
     // Without the accompanying fix in place, this test would have failed with:
-    // - Expected: 542 (~500)
+    // - Expected: 521 (~500)
     // - Actual  : 857 (~1000)
     // I.e. both upper and lower margin was taken into account.
-    CPPUNIT_ASSERT_EQUAL(542, nHeight);
+    CPPUNIT_ASSERT_EQUAL(521, nHeight);
 }
 
 CPPUNIT_TEST_FIXTURE(SwCoreTextTest, testTdf129810)