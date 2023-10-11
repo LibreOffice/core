e455e99ae9eb004676bc5d82629836e07605c228
Thorsten Behrens <thorsten.behrens@allotropia.de>
2023-10-11 10:41:42 +0200

    curl: fix socks return error if hostname too long for remote resolve
    This fixes https://curl.se/docs/CVE-2023-38545.html
    
    Change-Id: Id70486c8ec4e7754e7a07d81b22702e435ce6727

--- external/curl/UnpackedTarball_curl.mk
+++ external/curl/UnpackedTarball_curl.mk
@@ -22,6 +22,7 @@ $(eval $(call gb_UnpackedTarball_add_patches,curl,\
 	external/curl/curl-msvc-disable-protocols.patch.1 \
 	external/curl/curl-7.26.0_win-proxy.patch \
 	external/curl/zlib.patch.0 \
+	external/curl/CVE-2023-38545_7.87.0.patch \
 ))
 
 ifeq ($(SYSTEM_NSS),)
