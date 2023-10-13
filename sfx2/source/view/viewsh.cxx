cfba837abe3137b2bb20ddb414bb91078c4a1c9f
Paris Oplopoios <paris.oplopoios@collabora.com>
2023-10-13 15:55:51 +0300

    Revert "Make sure views get new themes on .uno:ChangeTheme"
    Seems to cause more issues than it solves, reverting for now
    
    This reverts commit 884fd220d0025a92510d3ff4710c8c517c8f271e.
    
    Change-Id: If32a9d3bd922afe5611e5258987eec9ec685bc8e
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/157951
    Tested-by: Jenkins
    Reviewed-by: Paris Oplopoios <parisoplop@gmail.com>

--- sfx2/source/view/viewsh.cxx
+++ sfx2/source/view/viewsh.cxx
@@ -2805,12 +2805,6 @@ vcl::Window* SfxViewShell::GetEditWindowForActiveOLEObj() const
     return {};
 }
 
-OUString SfxViewShell::GetColorConfigName() const
-{
-    SAL_WARN("sfx.view", "SfxViewShell::GetColorConfigName not overridden!");
-    return {};
-}
-
 void SfxViewShell::SetLOKLanguageTag(const OUString& rBcp47LanguageTag)
 {
     LanguageTag aTag(rBcp47LanguageTag, true);
