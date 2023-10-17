40b8b9ee5fb6de66ee455f8ffd64750e59f0587a
Miklos Vajna <vmiklos@collabora.com>
2023-10-17 08:28:10 +0200

    n#775899 sw: add testcase for the FloattableNomargins compat flag
    This was added in commit 50a1df360c907d8419ce49f098b6bc87a37a9956
    (n#775899 sw: add FloattableNomargins compat flag, 2012-08-23), without
    a testcase, so it was hard to make later changes without breaking the
    old behavior.
    
    Later commit 0898871b7b9492ada947ebc7b8429c5cb56db23c (n#775899
    testcase, 2012-08-27) did add a testcase for the bug, it was a different
    sub-task there, so the test document had no margins, while this compat
    flag is about some special handling around non-zero paragraph margins
    and floating tables.
    
    So add a new test that fails without the
    DocumentSettingId::FLOATTABLE_NOMARGINS block in
    SwBorderAttrs::CalcLeft() to make sure that this keeps working after I
    fix tdf#157573, which is a related problem.
    
    Change-Id: I09661be726e3db6be51d0cbb44cb5c504510e5ac
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/158069
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Tested-by: Jenkins

---
+++ sw/qa/core/layout/frmtool.cxx
@@ -0,0 +1,40 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <swmodeltestbase.hxx>
+
+#include <IDocumentLayoutAccess.hxx>
+#include <pagefrm.hxx>
+#include <rootfrm.hxx>
+
+namespace
+{
+/// Covers sw/source/core/layout/frmtool.cxx fixes.
+class Test : public SwModelTestBase
+{
+public:
+    Test()
+        : SwModelTestBase("/sw/qa/core/layout/data/")
+    {
+    }
+};
+
+CPPUNIT_TEST_FIXTURE(Test, testFloattableNoMargins)
+{
+    createSwDoc("floattable-nomargins.docx");
+
+    SwDoc* pDoc = getSwDoc();
+    SwRootFrame* pLayout = pDoc->getIDocumentLayoutAccess().GetCurrentLayout();
+    auto pPage1 = pLayout->Lower()->DynCastPageFrame();
+    CPPUNIT_ASSERT(pPage1);
+    CPPUNIT_ASSERT(!pPage1->GetNext());
+}
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
