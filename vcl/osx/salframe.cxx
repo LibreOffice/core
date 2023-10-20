LibreOffice/core

commit b69db38a38b09e158e8d46d8b717db85860ca874

tdf#152173 Don't display tooltip windows when mousing over non-key windows
Don't display tooltips when mousing over non-key windows even if the
application is active as the tooltip window will pull the non-key
window in front of the key window.

Change-Id: I425b9af09de0272ebee84a218e2e5ae8c200a4e5
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/151614
Tested-by: Jenkins
Reviewed-by: Patrick Luby <plubius@neooffice.org>

vcl/osx/salframe.cxx
@@ -462,7 +462,10 @@ void AquaSalFrame::Show(bool bVisible, bool bNoActivate)
     // parent window (to ensure that the tooltip is above the parent window),
     // displaying a tooltip pulls the parent window in front of the windows
     // of all other inactive applications.
-    if (bVisible && (mnStyle & SalFrameStyleFlags::TOOLTIP) && ![NSApp isActive])
+    // Also, don't display tooltips when mousing over non-key windows even if
+    // the application is active as the tooltip window will pull the non-key
+    // window in front of the key window.
+    if (bVisible && (mnStyle & SalFrameStyleFlags::TOOLTIP) && (![NSApp isActive] || (mpParent && ![ mpParent->mpNSWindow isKeyWindow])))
         return;
 
     mbShown = bVisible;

commit fb68f0761c3cfadf73f261d2d44de7b4e277e0af

tdf#157565 show tooltip if any parent window is the key window
Commit b69db38a38b09e158e8d46d8b717db85860ca874 caused tooltips
to fail to appear if their immediate parent window was not the
key window. So, look through the parent window's parent windows
and, if any of those windows are the kwy window, show the tooltip.

Change-Id: Icf1aed1144fdeac03b4b208de8ed8ee2db8ad4a0
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/158212
Tested-by: Jenkins
Reviewed-by: Patrick Luby <plubius@neooffice.org>

vcl/osx/salframe.cxx
@@ -468,8 +468,29 @@ void AquaSalFrame::Show(bool bVisible, bool bNoActivate)
     // Also, don't display tooltips when mousing over non-key windows even if
     // the application is active as the tooltip window will pull the non-key
     // window in front of the key window.
-    if (bVisible && (mnStyle & SalFrameStyleFlags::TOOLTIP) && (![NSApp isActive] || (mpParent && ![ mpParent->mpNSWindow isKeyWindow])))
-        return;
+    if (bVisible && (mnStyle & SalFrameStyleFlags::TOOLTIP))
+    {
+        if (![NSApp isActive])
+            return;
+
+        if (mpParent)
+        {
+            // tdf#157565 show tooltip if any parent window is the key window
+            bool bKeyWindowFound = false;
+            NSWindow *pParent = mpParent->mpNSWindow;
+            while (pParent)
+            {
+                if ([pParent isKeyWindow])
+                {
+                    bKeyWindowFound = true;
+                    break;
+                }
+                pParent = [pParent parentWindow];
+            }
+            if (!bKeyWindowFound)
+                return;
+        }
+    }
 
     mbShown = bVisible;
     if(bVisible)