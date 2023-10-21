ef29791179ca6cfcb5c82a65fefcbe29e039831d
Mike Kaganski <mike.kaganski@collabora.com>
2023-10-21 17:42:56 +0300

    Related: tdf#89178 Add an option to avoid converting some fields into text
    ... during mail merge.
    
    In some modes (generating individual documents; creating PDF) the mail merge
    process converts all fields into text. But sometimes it is undesirable for
    fields not involved into mail merge itself:
    
    * It is inconsistent with how MS Word behaves;
    * The generated editable documents could benefit from having other fields
      kept as fields;
    * Some fields, when exported to PDF, produce different results: e.g.,
      placeholder fields are output as empty spaces, not as placeholder text.
    
    An expert boolean configuration option is added:
    Office/Writer/FormLetter/ConvertToTextOnlyMMFields; it is false by default,
    in which case, the behavior is unchanged. When true, all fields in the mail
    merge document, except for database fields and hidden text fields, are not
    converted to text during mail merge process.
    
    Change-Id: Ibdb505ed3f2762db063bb0a91b674d27ecbc2e7f
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/158306
    Tested-by: Jenkins
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/158476
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

--- sw/inc/doc.hxx
+++ sw/inc/doc.hxx
@@ -1445,6 +1445,7 @@ public:
     // restore the invisible content if it's available on the undo stack
     bool RestoreInvisibleContent();
 
+    // Replace fields by text - mailmerge support
     SAL_DLLPRIVATE bool ConvertFieldsToText(SwRootFrame const& rLayout);
 
     // Create sub-documents according to given collection.
