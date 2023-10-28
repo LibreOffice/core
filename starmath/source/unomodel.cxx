f64479dd729d83d3fa84fa1e97540f4ae0b37a2b
Mike Kaganski <mike.kaganski@collabora.com>
2023-10-28 18:06:34 +0300

    pPrinter may be nullptr
    E.g., calling storeToURL, generating a PDF, on an embedded formula.
    
    Change-Id: If74af90f9c3dfd54a328782ddecd0adf52b51a5a
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/158591
    Tested-by: Jenkins
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

--- starmath/source/unomodel.cxx
+++ starmath/source/unomodel.cxx
@@ -910,8 +910,9 @@ uno::Sequence< beans::PropertyValue > SAL_CALL SmModel::getRenderer(
         throw RuntimeException();
 
     SmPrinterAccess aPrinterAccess( *pDocSh );
-    Printer *pPrinter = aPrinterAccess.GetPrinter();
-    Size    aPrtPaperSize ( pPrinter->GetPaperSize() );
+    Size aPrtPaperSize;
+    if (Printer *pPrinter = aPrinterAccess.GetPrinter())
+        aPrtPaperSize = pPrinter->GetPaperSize();
 
     // if paper size is 0 (usually if no 'real' printer is found),
     // guess the paper size
@@ -981,11 +982,16 @@ void SAL_CALL SmModel::render(
         return;
 
     SmPrinterAccess aPrinterAccess( *pDocSh );
-    Printer *pPrinter = aPrinterAccess.GetPrinter();
 
-    Size    aPrtPaperSize ( pPrinter->GetPaperSize() );
-    Size    aOutputSize   ( pPrinter->GetOutputSize() );
-    Point   aPrtPageOffset( pPrinter->GetPageOffset() );
+    Size aPrtPaperSize;
+    Size aOutputSize;
+    Point aPrtPageOffset;
+    if (Printer *pPrinter = aPrinterAccess.GetPrinter())
+    {
+        aPrtPaperSize = pPrinter->GetPaperSize();
+        aOutputSize = pPrinter->GetOutputSize();
+        aPrtPageOffset = pPrinter->GetPageOffset();
+    }
 
     // no real printer ??
     if (aPrtPaperSize.IsEmpty())
