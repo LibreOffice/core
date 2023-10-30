a04fc307ba2706bb9e5b8441aaf4380797b5f397
Xisco Fauli <xiscofauli@libreoffice.org>
2023-10-30 16:09:34 +0100

    CppunitTest_vcl_pdfexport: split into two
    It already has 109 tests
    
    Change-Id: Ic16c5ee68f020c2ee1662354786fb82aada62e0b
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/158667
    Tested-by: Jenkins
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

---
+++ vcl/CppunitTest_vcl_pdfexport2.mk
@@ -0,0 +1,52 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_CppunitTest_CppunitTest,vcl_pdfexport2))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,vcl_pdfexport2, \
+    vcl/qa/cppunit/pdfexport/pdfexport2 \
+))
+
+$(eval $(call gb_CppunitTest_use_sdk_api,vcl_pdfexport2))
+
+$(eval $(call gb_CppunitTest_use_libraries,vcl_pdfexport2, \
+	basegfx \
+	comphelper \
+	cppu \
+	cppuhelper \
+	sal \
+	subsequenttest \
+	test \
+	unotest \
+	utl \
+	tl \
+	vcl \
+	xmlsecurity \
+))
+
+$(eval $(call gb_CppunitTest_use_externals,vcl_pdfexport2, \
+	boost_headers \
+	$(if $(filter PDFIUM,$(BUILD_TYPE)),pdfium) \
+))
+
+$(eval $(call gb_CppunitTest_use_sdk_api,vcl_pdfexport2))
+
+$(eval $(call gb_CppunitTest_use_ure,vcl_pdfexport2))
+$(eval $(call gb_CppunitTest_use_vcl,vcl_pdfexport2))
+
+$(eval $(call gb_CppunitTest_use_rdb,vcl_pdfexport2,services))
+
+$(eval $(call gb_CppunitTest_use_configuration,vcl_pdfexport2))
+
+# assert if font/glyph fallback occurs
+$(eval $(call gb_CppunitTest_set_non_application_font_use,vcl_pdfexport2,abort))
+
+$(eval $(call gb_CppunitTest_use_more_fonts,vcl_pdfexport2))
+
+# vim: set noet sw=4 ts=4:
