d4c905920ed13d0727c7d2c4e2215776d6f4e18a
Mike Kaganski <mike.kaganski@collabora.com>
2023-11-07 16:05:07 +0300

    Pass XInteractionHandler to hasTrustedScriptingSignature instead of a bool
    This allows to use the same interaction handler there, as used in
    DocumentMacroMode::adjustMacroMode. hasTrustedScriptingSignature
    used to find its own interaction handler; and that would conflict
    with e.g. ODatabaseModelImpl::adjustMacroMode_AutoReject, which
    passes nullptr to adjustMacroMode, with intention to not show any
    UI; but with signed macros (see tdf#97694), the UI would still
    appear.
    
    Change-Id: Ia209f96bef67dccfe1da23c4d172ac47497f8eb1
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/159070
    Tested-by: Jenkins
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/159267
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

--- dbaccess/source/core/dataaccess/ModelImpl.cxx
+++ dbaccess/source/core/dataaccess/ModelImpl.cxx
@@ -1361,7 +1361,8 @@ SignatureState ODatabaseModelImpl::getScriptingSignatureState()
     return m_nScriptingSignatureState;
 }
 
-bool ODatabaseModelImpl::hasTrustedScriptingSignature(bool bAllowUIToAddAuthor)
+bool ODatabaseModelImpl::hasTrustedScriptingSignature(
+    const css::uno::Reference<css::task::XInteractionHandler>& _rxInteraction)
 {
     bool bResult = false;
 
@@ -1393,20 +1394,15 @@ bool ODatabaseModelImpl::hasTrustedScriptingSignature(bool bAllowUIToAddAuthor)
                                   });
         }
 
-        if (!bResult && bAllowUIToAddAuthor)
+        if (!bResult && _rxInteraction)
         {
-            Reference<XInteractionHandler> xInteraction;
-            xInteraction = m_aMediaDescriptor.getOrDefault("InteractionHandler", xInteraction);
-            if (xInteraction.is())
-            {
-                task::DocumentMacroConfirmationRequest aRequest;
-                aRequest.DocumentURL = m_sDocFileLocation;
-                aRequest.DocumentStorage = xStorage;
-                aRequest.DocumentSignatureInformation = aInfo;
-                aRequest.DocumentVersion = aODFVersion;
-                aRequest.Classification = task::InteractionClassification_QUERY;
-                bResult = SfxMedium::CallApproveHandler(xInteraction, uno::Any(aRequest), true);
-            }
+            task::DocumentMacroConfirmationRequest aRequest;
+            aRequest.DocumentURL = m_sDocFileLocation;
+            aRequest.DocumentStorage = xStorage;
+            aRequest.DocumentSignatureInformation = aInfo;
+            aRequest.DocumentVersion = aODFVersion;
+            aRequest.Classification = task::InteractionClassification_QUERY;
+            bResult = SfxMedium::CallApproveHandler(_rxInteraction, uno::Any(aRequest), true);
         }
     }
     catch (uno::Exception&)
