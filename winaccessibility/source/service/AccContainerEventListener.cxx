0a2622760a967e9d64cfdc632548dd42c1836324
Stephan Bergmann <sbergman@redhat.com>
2023-11-08 13:54:14 +0100

    Drop presumably redundant null check
    In a18bdb3bc05e761704cc345a66a9d642bc4f4a0a "Integrate branch of IAccessible2",
    this had originally been
    
    >     if (pAgent && pAgent->IsStateManageDescendant(pXAccessible))
    >     {
    >         return;
    >     }
    
    so lets assume the `pAgent` check was only there to avoid a null deref
    
    Change-Id: I320c181ff7d86d7c328bf9e5139dce2df1facb7d
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/159130
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>
    Tested-by: Jenkins

--- winaccessibility/source/service/AccContainerEventListener.cxx
+++ winaccessibility/source/service/AccContainerEventListener.cxx
@@ -438,7 +438,7 @@ void AccContainerEventListener::UpdateAllChildrenState(XAccessible* pXAccessible
         return;
     }
 
-    if (m_pObjManager && AccObjectWinManager::IsStateManageDescendant(pXAccessible))
+    if (AccObjectWinManager::IsStateManageDescendant(pXAccessible))
     {
         return;
     }
