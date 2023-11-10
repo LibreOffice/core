e021d5dee19f1e06c59aa2aa6d064d2b5f7e313b
Mike Kaganski <mike.kaganski@collabora.com>
2023-11-10 16:16:49 +0100

    Add missing license headers
    Change-Id: I4c94fb9d92b02b42b185d205887fa7bce1c45be6
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/159235
    Tested-by: Jenkins
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

--- include/linguistic/translate.hxx
+++ include/linguistic/translate.hxx
@@ -1,3 +1,12 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 #pragma once
 #include <linguistic/lngdllapi.h>
 #include <rtl/string.hxx>
@@ -7,3 +16,5 @@ namespace linguistic
 LNG_DLLPUBLIC OString Translate(const OString& rTargetLang, const OString& rAPIUrl,
                                 const OString& rAuthKey, const OString& rData);
 } // namespace
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
