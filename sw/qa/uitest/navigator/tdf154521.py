2368e09cd13aa58d597470f73fb0f2e181e1929c
Stephan Bergmann <sbergman@redhat.com>
2023-11-10 08:12:50 +0100

    Disable flakey navigator UITest again
    f2ba17fecb60d936599efb975d96a3ea06c2a43f "disable more flakey navigator UITest",
    then 0a0c5a5b0521bea6cb695731d448e62c41ed4c57 "UITest_sw_navigator: avoid
    oneprocess" enabled it again, apparently in the false hopes that non-oneprocess
    would magically fix this issue.  But this still hangs frequently, e.g.
    <https://ci.libreoffice.org//job/lo_tb_master_linux_dbg/45650/> hung here (and
    got killed by kill-wrapper) with the python.bin process at
    
    > Thread 1 (Thread 0x7f85438bbb80 (LWP 217701) "python3"):
    > Traceback (most recent call first):
    >   <built-in method sleep of module object at remote 0x7f854143b728>
    >   File "/home/tdf/lode/jenkins/workspace/lo_tb_master_linux_dbg/uitest/uitest/test.py", line 72, in wait_until_property_is_updated
    >     time.sleep(DEFAULT_SLEEP)
    >   File "/home/tdf/lode/jenkins/workspace/lo_tb_master_linux_dbg/sw/qa/uitest/navigator/tdf154521.py", line 158, in test_query_selected_bookmark
    >     self.ui_test.wait_until_property_is_updated(xContentTree, "SelectEntryText", "Bookmark 2")
    >   File "/usr/lib64/python3.6/unittest/case.py", line 622, in run
    >     testMethod()
    >   File "/usr/lib64/python3.6/unittest/case.py", line 670, in __call__
    >     return self.run(*args, **kwds)
    >   File "/usr/lib64/python3.6/unittest/suite.py", line 122, in run
    >     test(result)
    >   File "/usr/lib64/python3.6/unittest/suite.py", line 84, in __call__
    >     return self.run(*args, **kwds)
    >   File "/usr/lib64/python3.6/unittest/runner.py", line 176, in run
    >     test(result)
    >   File "/home/tdf/lode/jenkins/workspace/lo_tb_master_linux_dbg/uitest/test_main.py", line 138, in <module>
    >     result = unittest.TextTestRunner(stream=sys.stdout, verbosity=2).run(test_suite)
    
    and the soffice.bin process just idling with nothing going on.
    
    Change-Id: I44a38b12050cfe2cf4831631406df9018018e2ed
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/159260
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

--- sw/qa/uitest/navigator/tdf154521.py
+++ sw/qa/uitest/navigator/tdf154521.py
@@ -130,73 +130,74 @@ class tdf154521(UITestCase):
 
             xContentTree = xNavigatorPanel.getChild("contenttree")
 
-            self.ui_test.wait_until_property_is_updated(xContentTree, "SelectEntryText", "Bookmark 1")
-            self.assertEqual(get_state_as_dict(xContentTree)["SelectEntryText"], "Bookmark 1")
-            self.assertEqual(get_state_as_dict(xContentTree)["SelectionCount"], "1")
+# disable flakey UITest
+#            self.ui_test.wait_until_property_is_updated(xContentTree, "SelectEntryText", "Bookmark 1")
+#            self.assertEqual(get_state_as_dict(xContentTree)["SelectEntryText"], "Bookmark 1")
+#            self.assertEqual(get_state_as_dict(xContentTree)["SelectionCount"], "1")
 
-#            self.xUITest.executeCommand(".uno:Escape")
+##            self.xUITest.executeCommand(".uno:Escape")
 
-            # get the title of SwNavigatorPanel with emptying it to access to the selected bookmark
-            self.assertEqual(self.getTitle(xDoc), "Navigator")
-            # title was emptied
-            self.assertEqual(self.getTitle(xDoc), "")
+#            # get the title of SwNavigatorPanel with emptying it to access to the selected bookmark
+#            self.assertEqual(self.getTitle(xDoc), "Navigator")
+#            # title was emptied
+#            self.assertEqual(self.getTitle(xDoc), "")
 
-            # Select nested bookmark in Navigator
+#            # Select nested bookmark in Navigator
 
-            xContentTree.executeAction("TYPE", mkPropertyValues({"KEYCODE": "RETURN"}))
+#            xContentTree.executeAction("TYPE", mkPropertyValues({"KEYCODE": "RETURN"}))
 
-            # This jumped to Bookmark 1 after selection
-            self.ui_test.wait_until_property_is_updated(xContentTree, "SelectEntryText", "Bookmark 1")
-            self.assertEqual(get_state_as_dict(xContentTree)["SelectEntryText"], "Bookmark 1")
-            self.assertEqual(get_state_as_dict(xContentTree)["SelectionCount"], "1")
+#            # This jumped to Bookmark 1 after selection
+#            self.ui_test.wait_until_property_is_updated(xContentTree, "SelectEntryText", "Bookmark 1")
+#            self.assertEqual(get_state_as_dict(xContentTree)["SelectEntryText"], "Bookmark 1")
+#            self.assertEqual(get_state_as_dict(xContentTree)["SelectionCount"], "1")
 
-            # This was "Navigator"
-            self.assertEqual(self.getTitle(xDoc), "Bookmark 1")
+#            # This was "Navigator"
+#            self.assertEqual(self.getTitle(xDoc), "Bookmark 1")
 
-            # Try the same selection with Bookmark 2
-            xContentTree.executeAction("TYPE", mkPropertyValues({"KEYCODE": "UP"}))
-            self.ui_test.wait_until_property_is_updated(xContentTree, "SelectEntryText", "Bookmark 2")
-            self.assertEqual(get_state_as_dict(xContentTree)["SelectEntryText"], "Bookmark 2")
-            self.assertEqual(get_state_as_dict(xContentTree)["SelectionCount"], "1")
-            xContentTree.executeAction("TYPE", mkPropertyValues({"KEYCODE": "RETURN"}))
-            self.ui_test.wait_until_property_is_updated(xContentTree, "SelectEntryText", "Bookmark 2")
-            self.assertEqual(get_state_as_dict(xContentTree)["SelectEntryText"], "Bookmark 2")
-            self.assertEqual(get_state_as_dict(xContentTree)["SelectionCount"], "1")
+#            # Try the same selection with Bookmark 2
+#            xContentTree.executeAction("TYPE", mkPropertyValues({"KEYCODE": "UP"}))
+#            self.ui_test.wait_until_property_is_updated(xContentTree, "SelectEntryText", "Bookmark 2")
+#            self.assertEqual(get_state_as_dict(xContentTree)["SelectEntryText"], "Bookmark 2")
+#            self.assertEqual(get_state_as_dict(xContentTree)["SelectionCount"], "1")
+#            xContentTree.executeAction("TYPE", mkPropertyValues({"KEYCODE": "RETURN"}))
+#            self.ui_test.wait_until_property_is_updated(xContentTree, "SelectEntryText", "Bookmark 2")
+#            self.assertEqual(get_state_as_dict(xContentTree)["SelectEntryText"], "Bookmark 2")
+#            self.assertEqual(get_state_as_dict(xContentTree)["SelectionCount"], "1")
 
-            # This was "Navigator"
-            self.assertEqual(self.getTitle(xDoc), "Bookmark 2")
+#            # This was "Navigator"
+#            self.assertEqual(self.getTitle(xDoc), "Bookmark 2")
 
-            # Try the same selection with Bookmark 3
+#            # Try the same selection with Bookmark 3
 
-            # why we need this extra UP?
-            xContentTree.executeAction("TYPE", mkPropertyValues({"KEYCODE": "UP"}))
-            self.ui_test.wait_until_property_is_updated(xContentTree, "SelectEntryText", "Bookmark 2")
-            xContentTree.executeAction("TYPE", mkPropertyValues({"KEYCODE": "UP"}))
-            self.ui_test.wait_until_property_is_updated(xContentTree, "SelectEntryText", "Bookmark 3")
+#            # why we need this extra UP?
+#            xContentTree.executeAction("TYPE", mkPropertyValues({"KEYCODE": "UP"}))
+#            self.ui_test.wait_until_property_is_updated(xContentTree, "SelectEntryText", "Bookmark 2")
+#            xContentTree.executeAction("TYPE", mkPropertyValues({"KEYCODE": "UP"}))
+#            self.ui_test.wait_until_property_is_updated(xContentTree, "SelectEntryText", "Bookmark 3")
 
-            self.assertEqual(get_state_as_dict(xContentTree)["SelectEntryText"], "Bookmark 3")
-            self.assertEqual(get_state_as_dict(xContentTree)["SelectionCount"], "1")
-            xContentTree.executeAction("TYPE", mkPropertyValues({"KEYCODE": "RETURN"}))
-            self.ui_test.wait_until_property_is_updated(xContentTree, "SelectEntryText", "Bookmark 3")
-            self.assertEqual(get_state_as_dict(xContentTree)["SelectEntryText"], "Bookmark 3")
-            self.assertEqual(get_state_as_dict(xContentTree)["SelectionCount"], "1")
+#            self.assertEqual(get_state_as_dict(xContentTree)["SelectEntryText"], "Bookmark 3")
+#            self.assertEqual(get_state_as_dict(xContentTree)["SelectionCount"], "1")
+#            xContentTree.executeAction("TYPE", mkPropertyValues({"KEYCODE": "RETURN"}))
+#            self.ui_test.wait_until_property_is_updated(xContentTree, "SelectEntryText", "Bookmark 3")
+#            self.assertEqual(get_state_as_dict(xContentTree)["SelectEntryText"], "Bookmark 3")
+#            self.assertEqual(get_state_as_dict(xContentTree)["SelectionCount"], "1")
 
-            # This was "Navigator"
-            self.assertEqual(self.getTitle(xDoc), "Bookmark 3")
+#            # This was "Navigator"
+#            self.assertEqual(self.getTitle(xDoc), "Bookmark 3")
 
-            # go to the previous item
+#            # go to the previous item
 
-            # why we need this extra UP?
-            xContentTree.executeAction("TYPE", mkPropertyValues({"KEYCODE": "UP"}))
-            self.ui_test.wait_until_property_is_updated(xContentTree, "SelectEntryText", "Bookmark 3")
-            xContentTree.executeAction("TYPE", mkPropertyValues({"KEYCODE": "UP"}))
-            self.ui_test.wait_until_property_is_updated(xContentTree, "SelectEntryText", "Bookmarks")
-            self.assertEqual(get_state_as_dict(xContentTree)["SelectEntryText"], "Bookmarks")
-            xContentTree.executeAction("TYPE", mkPropertyValues({"KEYCODE": "RETURN"}))
+#            # why we need this extra UP?
+#            xContentTree.executeAction("TYPE", mkPropertyValues({"KEYCODE": "UP"}))
+#            self.ui_test.wait_until_property_is_updated(xContentTree, "SelectEntryText", "Bookmark 3")
+#            xContentTree.executeAction("TYPE", mkPropertyValues({"KEYCODE": "UP"}))
+#            self.ui_test.wait_until_property_is_updated(xContentTree, "SelectEntryText", "Bookmarks")
+#            self.assertEqual(get_state_as_dict(xContentTree)["SelectEntryText"], "Bookmarks")
+#            xContentTree.executeAction("TYPE", mkPropertyValues({"KEYCODE": "RETURN"}))
 
-            # This was "Navigator"
-            self.assertEqual(self.getTitle(xDoc), "")
+#            # This was "Navigator"
+#            self.assertEqual(self.getTitle(xDoc), "")
 
-            self.xUITest.executeCommand(".uno:Sidebar")
+#            self.xUITest.executeCommand(".uno:Sidebar")
 
 # vim: set shiftwidth=4 softtabstop=4 expandtab:
