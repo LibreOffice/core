LibreOffice/core

commit a76a135c9e9bdecd38970e293e72eeeeca000d27

flatten some vcl code
Change-Id: I3747c9ccc97ac59a2e41c1fe4427d1a2f458352a
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/145017
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/source/bitmap/BitmapSimpleColorQuantizationFilter.cxx
@@ -19,92 +19,75 @@ BitmapEx BitmapSimpleColorQuantizationFilter::execute(BitmapEx const& aBitmapEx)
 {
     Bitmap aBitmap = aBitmapEx.GetBitmap();
 
-    bool bRet = false;
+    if (vcl::numberOfColors(aBitmap.getPixelFormat()) > sal_Int64(mnNewColorCount))
+        return BitmapEx(aBitmap);
 
-    if (vcl::numberOfColors(aBitmap.getPixelFormat()) <= sal_Int64(mnNewColorCount))
+    Bitmap aNewBmp;
+    Bitmap::ScopedReadAccess pRAcc(aBitmap);
+    if (!pRAcc)
+        return BitmapEx();
+
+    const sal_uInt16 nColorCount = std::min(mnNewColorCount, sal_uInt16(256));
+    auto ePixelFormat = vcl::PixelFormat::INVALID;
+    if (nColorCount <= 2)
+        ePixelFormat = vcl::PixelFormat::N1_BPP;
+    else
+        ePixelFormat = vcl::PixelFormat::N8_BPP;
+
+    Octree aOct(*pRAcc, nColorCount);
+    const BitmapPalette& rPal = aOct.GetPalette();
+
+    aNewBmp = Bitmap(aBitmap.GetSizePixel(), ePixelFormat, &rPal);
+    BitmapScopedWriteAccess pWAcc(aNewBmp);
+    if (!pWAcc)
+        return BitmapEx();
+
+    const sal_Int32 nWidth = pRAcc->Width();
+    const sal_Int32 nHeight = pRAcc->Height();
+
+    if (pRAcc->HasPalette())
     {
-        bRet = true;
+        for (sal_Int32 nY = 0; nY < nHeight; nY++)
+        {
+            Scanline pScanline = pWAcc->GetScanline(nY);
+            Scanline pScanlineRead = pRAcc->GetScanline(nY);
+            for (sal_Int32 nX = 0; nX < nWidth; nX++)
+            {
+                auto c = pRAcc->GetPaletteColor(pRAcc->GetIndexFromData(pScanlineRead, nX));
+                pWAcc->SetPixelOnData(
+                    pScanline, nX,
+                    BitmapColor(static_cast<sal_uInt8>(aOct.GetBestPaletteIndex(c))));
+            }
+        }
     }
     else
     {
-        Bitmap aNewBmp;
-        Bitmap::ScopedReadAccess pRAcc(aBitmap);
-        const sal_uInt16 nColorCount = std::min(mnNewColorCount, sal_uInt16(256));
-        auto ePixelFormat = vcl::PixelFormat::INVALID;
-
-        if (nColorCount <= 2)
-            ePixelFormat = vcl::PixelFormat::N1_BPP;
-        else
-            ePixelFormat = vcl::PixelFormat::N8_BPP;
-
-        if (pRAcc)
+        for (sal_Int32 nY = 0; nY < nHeight; nY++)
         {
-            Octree aOct(*pRAcc, nColorCount);
-            const BitmapPalette& rPal = aOct.GetPalette();
-
-            aNewBmp = Bitmap(aBitmap.GetSizePixel(), ePixelFormat, &rPal);
-            BitmapScopedWriteAccess pWAcc(aNewBmp);
-
-            if (pWAcc)
+            Scanline pScanline = pWAcc->GetScanline(nY);
+            Scanline pScanlineRead = pRAcc->GetScanline(nY);
+            for (sal_Int32 nX = 0; nX < nWidth; nX++)
             {
-                const sal_Int32 nWidth = pRAcc->Width();
-                const sal_Int32 nHeight = pRAcc->Height();
-
-                if (pRAcc->HasPalette())
-                {
-                    for (sal_Int32 nY = 0; nY < nHeight; nY++)
-                    {
-                        Scanline pScanline = pWAcc->GetScanline(nY);
-                        Scanline pScanlineRead = pRAcc->GetScanline(nY);
-                        for (sal_Int32 nX = 0; nX < nWidth; nX++)
-                        {
-                            auto c = pRAcc->GetPaletteColor(
-                                pRAcc->GetIndexFromData(pScanlineRead, nX));
-                            pWAcc->SetPixelOnData(
-                                pScanline, nX,
-                                BitmapColor(static_cast<sal_uInt8>(aOct.GetBestPaletteIndex(c))));
-                        }
-                    }
-                }
-                else
-                {
-                    for (sal_Int32 nY = 0; nY < nHeight; nY++)
-                    {
-                        Scanline pScanline = pWAcc->GetScanline(nY);
-                        Scanline pScanlineRead = pRAcc->GetScanline(nY);
-                        for (sal_Int32 nX = 0; nX < nWidth; nX++)
-                        {
-                            auto c = pRAcc->GetPixelFromData(pScanlineRead, nX);
-                            pWAcc->SetPixelOnData(
-                                pScanline, nX,
-                                BitmapColor(static_cast<sal_uInt8>(aOct.GetBestPaletteIndex(c))));
-                        }
-                    }
-                }
-
-                pWAcc.reset();
-                bRet = true;
+                auto c = pRAcc->GetPixelFromData(pScanlineRead, nX);
+                pWAcc->SetPixelOnData(
+                    pScanline, nX,
+                    BitmapColor(static_cast<sal_uInt8>(aOct.GetBestPaletteIndex(c))));
             }
-
-            pRAcc.reset();
-        }
-
-        if (bRet)
-        {
-            const MapMode aMap(aBitmap.GetPrefMapMode());
-            const Size aSize(aBitmap.GetPrefSize());
-
-            aBitmap = aNewBmp;
-
-            aBitmap.SetPrefMapMode(aMap);
-            aBitmap.SetPrefSize(aSize);
         }
     }
 
-    if (bRet)
-        return BitmapEx(aBitmap);
+    pWAcc.reset();
+    pRAcc.reset();
 
-    return BitmapEx();
+    const MapMode aMap(aBitmap.GetPrefMapMode());
+    const Size aSize(aBitmap.GetPrefSize());
+
+    aBitmap = aNewBmp;
+
+    aBitmap.SetPrefMapMode(aMap);
+    aBitmap.SetPrefSize(aSize);
+
+    return BitmapEx(aBitmap);
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

vcl/source/bitmap/BitmapMedianFilter.cxx
@@ -54,164 +54,152 @@ BitmapEx BitmapMedianFilter::execute(BitmapEx const& rBitmapEx) const
     Bitmap aBitmap(rBitmapEx.GetBitmap());
 
     Bitmap::ScopedReadAccess pReadAcc(aBitmap);
-    bool bRet = false;
+    if (!pReadAcc)
+        return BitmapEx();
 
-    if (pReadAcc)
+    Bitmap aNewBmp(aBitmap.GetSizePixel(), vcl::PixelFormat::N24_BPP);
+    BitmapScopedWriteAccess pWriteAcc(aNewBmp);
+    if (!pWriteAcc)
+        return BitmapEx();
+
+    const sal_Int32 nWidth = pWriteAcc->Width(), nWidth2 = nWidth + 2;
+    const sal_Int32 nHeight = pWriteAcc->Height(), nHeight2 = nHeight + 2;
+    std::unique_ptr<sal_Int32[]> pColm(new sal_Int32[nWidth2]);
+    std::unique_ptr<sal_Int32[]> pRows(new sal_Int32[nHeight2]);
+    std::unique_ptr<BitmapColor[]> pColRow1(new BitmapColor[nWidth2]);
+    std::unique_ptr<BitmapColor[]> pColRow2(new BitmapColor[nWidth2]);
+    std::unique_ptr<BitmapColor[]> pColRow3(new BitmapColor[nWidth2]);
+    BitmapColor* pRowTmp1 = pColRow1.get();
+    BitmapColor* pRowTmp2 = pColRow2.get();
+    BitmapColor* pRowTmp3 = pColRow3.get();
+    BitmapColor* pColor;
+    sal_Int32 nY, nX, i;
+    sal_Int32 nR1, nR2, nR3, nR4, nR5, nR6, nR7, nR8, nR9;
+    sal_Int32 nG1, nG2, nG3, nG4, nG5, nG6, nG7, nG8, nG9;
+    sal_Int32 nB1, nB2, nB3, nB4, nB5, nB6, nB7, nB8, nB9;
+
+    // create column LUT
+    for (i = 0; i < nWidth2; i++)
+        pColm[i] = (i > 0) ? (i - 1) : 0;
+
+    pColm[nWidth + 1] = pColm[nWidth];
+
+    // create row LUT
+    for (i = 0; i < nHeight2; i++)
+        pRows[i] = (i > 0) ? (i - 1) : 0;
+
+    pRows[nHeight + 1] = pRows[nHeight];
+
+    // read first three rows of bitmap color
+    if (nHeight2 > 2)
     {
-        Bitmap aNewBmp(aBitmap.GetSizePixel(), vcl::PixelFormat::N24_BPP);
-        BitmapScopedWriteAccess pWriteAcc(aNewBmp);
-
-        if (pWriteAcc)
+        for (i = 0; i < nWidth2; i++)
         {
-            const sal_Int32 nWidth = pWriteAcc->Width(), nWidth2 = nWidth + 2;
-            const sal_Int32 nHeight = pWriteAcc->Height(), nHeight2 = nHeight + 2;
-            std::unique_ptr<sal_Int32[]> pColm(new sal_Int32[nWidth2]);
-            std::unique_ptr<sal_Int32[]> pRows(new sal_Int32[nHeight2]);
-            std::unique_ptr<BitmapColor[]> pColRow1(new BitmapColor[nWidth2]);
-            std::unique_ptr<BitmapColor[]> pColRow2(new BitmapColor[nWidth2]);
-            std::unique_ptr<BitmapColor[]> pColRow3(new BitmapColor[nWidth2]);
-            BitmapColor* pRowTmp1 = pColRow1.get();
-            BitmapColor* pRowTmp2 = pColRow2.get();
-            BitmapColor* pRowTmp3 = pColRow3.get();
-            BitmapColor* pColor;
-            sal_Int32 nY, nX, i;
-            sal_Int32 nR1, nR2, nR3, nR4, nR5, nR6, nR7, nR8, nR9;
-            sal_Int32 nG1, nG2, nG3, nG4, nG5, nG6, nG7, nG8, nG9;
-            sal_Int32 nB1, nB2, nB3, nB4, nB5, nB6, nB7, nB8, nB9;
-
-            // create column LUT
-            for (i = 0; i < nWidth2; i++)
-                pColm[i] = (i > 0) ? (i - 1) : 0;
-
-            pColm[nWidth + 1] = pColm[nWidth];
-
-            // create row LUT
-            for (i = 0; i < nHeight2; i++)
-                pRows[i] = (i > 0) ? (i - 1) : 0;
-
-            pRows[nHeight + 1] = pRows[nHeight];
-
-            // read first three rows of bitmap color
-            if (nHeight2 > 2)
-            {
-                for (i = 0; i < nWidth2; i++)
-                {
-                    pColRow1[i] = pReadAcc->GetColor(pRows[0], pColm[i]);
-                    pColRow2[i] = pReadAcc->GetColor(pRows[1], pColm[i]);
-                    pColRow3[i] = pReadAcc->GetColor(pRows[2], pColm[i]);
-                }
-            }
-
-            // do median filtering
-            for (nY = 0; nY < nHeight;)
-            {
-                Scanline pScanline = pWriteAcc->GetScanline(nY);
-                for (nX = 0; nX < nWidth; nX++)
-                {
-                    pColor = pRowTmp1 + nX;
-                    nR1 = pColor->GetRed();
-                    nG1 = pColor->GetGreen();
-                    nB1 = pColor->GetBlue();
-                    nR2 = (++pColor)->GetRed();
-                    nG2 = pColor->GetGreen();
-                    nB2 = pColor->GetBlue();
-                    nR3 = (++pColor)->GetRed();
-                    nG3 = pColor->GetGreen();
-                    nB3 = pColor->GetBlue();
-
-                    pColor = pRowTmp2 + nX;
-                    nR4 = pColor->GetRed();
-                    nG4 = pColor->GetGreen();
-                    nB4 = pColor->GetBlue();
-                    nR5 = (++pColor)->GetRed();
-                    nG5 = pColor->GetGreen();
-                    nB5 = pColor->GetBlue();
-                    nR6 = (++pColor)->GetRed();
-                    nG6 = pColor->GetGreen();
-                    nB6 = pColor->GetBlue();
-
-                    pColor = pRowTmp3 + nX;
-                    nR7 = pColor->GetRed();
-                    nG7 = pColor->GetGreen();
-                    nB7 = pColor->GetBlue();
-                    nR8 = (++pColor)->GetRed();
-                    nG8 = pColor->GetGreen();
-                    nB8 = pColor->GetBlue();
-                    nR9 = (++pColor)->GetRed();
-                    nG9 = pColor->GetGreen();
-                    nB9 = pColor->GetBlue();
-
-                    MNMX6(nR1, nR2, nR3, nR4, nR5, nR6);
-                    MNMX5(nR7, nR2, nR3, nR4, nR5);
-                    MNMX4(nR8, nR2, nR3, nR4);
-                    MNMX3(nR9, nR2, nR3);
-
-                    MNMX6(nG1, nG2, nG3, nG4, nG5, nG6);
-                    MNMX5(nG7, nG2, nG3, nG4, nG5);
-                    MNMX4(nG8, nG2, nG3, nG4);
-                    MNMX3(nG9, nG2, nG3);
-
-                    MNMX6(nB1, nB2, nB3, nB4, nB5, nB6);
-                    MNMX5(nB7, nB2, nB3, nB4, nB5);
-                    MNMX4(nB8, nB2, nB3, nB4);
-                    MNMX3(nB9, nB2, nB3);
-
-                    // set destination color
-                    pWriteAcc->SetPixelOnData(pScanline, nX,
-                                              BitmapColor(static_cast<sal_uInt8>(nR2),
-                                                          static_cast<sal_uInt8>(nG2),
-                                                          static_cast<sal_uInt8>(nB2)));
-                }
-
-                if (++nY < nHeight)
-                {
-                    if (pRowTmp1 == pColRow1.get())
-                    {
-                        pRowTmp1 = pColRow2.get();
-                        pRowTmp2 = pColRow3.get();
-                        pRowTmp3 = pColRow1.get();
-                    }
-                    else if (pRowTmp1 == pColRow2.get())
-                    {
-                        pRowTmp1 = pColRow3.get();
-                        pRowTmp2 = pColRow1.get();
-                        pRowTmp3 = pColRow2.get();
-                    }
-                    else
-                    {
-                        pRowTmp1 = pColRow1.get();
-                        pRowTmp2 = pColRow2.get();
-                        pRowTmp3 = pColRow3.get();
-                    }
-
-                    for (i = 0; i < nWidth2; i++)
-                        pRowTmp3[i] = pReadAcc->GetColor(pRows[nY + 2], pColm[i]);
-                }
-            }
-
-            pWriteAcc.reset();
-
-            bRet = true;
-        }
-
-        pReadAcc.reset();
-
-        if (bRet)
-        {
-            const MapMode aMap(aBitmap.GetPrefMapMode());
-            const Size aPrefSize(aBitmap.GetPrefSize());
-
-            aBitmap = aNewBmp;
-
-            aBitmap.SetPrefMapMode(aMap);
-            aBitmap.SetPrefSize(aPrefSize);
+            pColRow1[i] = pReadAcc->GetColor(pRows[0], pColm[i]);
+            pColRow2[i] = pReadAcc->GetColor(pRows[1], pColm[i]);
+            pColRow3[i] = pReadAcc->GetColor(pRows[2], pColm[i]);
         }
     }
 
-    if (bRet)
-        return BitmapEx(aBitmap);
+    // do median filtering
+    for (nY = 0; nY < nHeight;)
+    {
+        Scanline pScanline = pWriteAcc->GetScanline(nY);
+        for (nX = 0; nX < nWidth; nX++)
+        {
+            pColor = pRowTmp1 + nX;
+            nR1 = pColor->GetRed();
+            nG1 = pColor->GetGreen();
+            nB1 = pColor->GetBlue();
+            nR2 = (++pColor)->GetRed();
+            nG2 = pColor->GetGreen();
+            nB2 = pColor->GetBlue();
+            nR3 = (++pColor)->GetRed();
+            nG3 = pColor->GetGreen();
+            nB3 = pColor->GetBlue();
 
-    return BitmapEx();
+            pColor = pRowTmp2 + nX;
+            nR4 = pColor->GetRed();
+            nG4 = pColor->GetGreen();
+            nB4 = pColor->GetBlue();
+            nR5 = (++pColor)->GetRed();
+            nG5 = pColor->GetGreen();
+            nB5 = pColor->GetBlue();
+            nR6 = (++pColor)->GetRed();
+            nG6 = pColor->GetGreen();
+            nB6 = pColor->GetBlue();
+
+            pColor = pRowTmp3 + nX;
+            nR7 = pColor->GetRed();
+            nG7 = pColor->GetGreen();
+            nB7 = pColor->GetBlue();
+            nR8 = (++pColor)->GetRed();
+            nG8 = pColor->GetGreen();
+            nB8 = pColor->GetBlue();
+            nR9 = (++pColor)->GetRed();
+            nG9 = pColor->GetGreen();
+            nB9 = pColor->GetBlue();
+
+            MNMX6(nR1, nR2, nR3, nR4, nR5, nR6);
+            MNMX5(nR7, nR2, nR3, nR4, nR5);
+            MNMX4(nR8, nR2, nR3, nR4);
+            MNMX3(nR9, nR2, nR3);
+
+            MNMX6(nG1, nG2, nG3, nG4, nG5, nG6);
+            MNMX5(nG7, nG2, nG3, nG4, nG5);
+            MNMX4(nG8, nG2, nG3, nG4);
+            MNMX3(nG9, nG2, nG3);
+
+            MNMX6(nB1, nB2, nB3, nB4, nB5, nB6);
+            MNMX5(nB7, nB2, nB3, nB4, nB5);
+            MNMX4(nB8, nB2, nB3, nB4);
+            MNMX3(nB9, nB2, nB3);
+
+            // set destination color
+            pWriteAcc->SetPixelOnData(pScanline, nX,
+                                      BitmapColor(static_cast<sal_uInt8>(nR2),
+                                                  static_cast<sal_uInt8>(nG2),
+                                                  static_cast<sal_uInt8>(nB2)));
+        }
+
+        if (++nY < nHeight)
+        {
+            if (pRowTmp1 == pColRow1.get())
+            {
+                pRowTmp1 = pColRow2.get();
+                pRowTmp2 = pColRow3.get();
+                pRowTmp3 = pColRow1.get();
+            }
+            else if (pRowTmp1 == pColRow2.get())
+            {
+                pRowTmp1 = pColRow3.get();
+                pRowTmp2 = pColRow1.get();
+                pRowTmp3 = pColRow2.get();
+            }
+            else
+            {
+                pRowTmp1 = pColRow1.get();
+                pRowTmp2 = pColRow2.get();
+                pRowTmp3 = pColRow3.get();
+            }
+
+            for (i = 0; i < nWidth2; i++)
+                pRowTmp3[i] = pReadAcc->GetColor(pRows[nY + 2], pColm[i]);
+        }
+    }
+
+    pWriteAcc.reset();
+    pReadAcc.reset();
+
+    const MapMode aMap(aBitmap.GetPrefMapMode());
+    const Size aPrefSize(aBitmap.GetPrefSize());
+
+    aBitmap = aNewBmp;
+
+    aBitmap.SetPrefMapMode(aMap);
+    aBitmap.SetPrefSize(aPrefSize);
+
+    return BitmapEx(aBitmap);
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

vcl/source/bitmap/BitmapMonochromeFilter.cxx
@@ -18,83 +18,71 @@ BitmapEx BitmapMonochromeFilter::execute(BitmapEx const& aBitmapEx) const
 {
     Bitmap aBitmap = aBitmapEx.GetBitmap();
     Bitmap::ScopedReadAccess pReadAcc(aBitmap);
-    bool bRet = false;
+    if (!pReadAcc)
+        return BitmapEx();
 
-    if (pReadAcc)
+    Bitmap aNewBmp(aBitmap.GetSizePixel(), vcl::PixelFormat::N1_BPP);
+    BitmapScopedWriteAccess pWriteAcc(aNewBmp);
+    if (!pWriteAcc)
+        return BitmapEx();
+
+    const BitmapColor aBlack(pWriteAcc->GetBestMatchingColor(COL_BLACK));
+    const BitmapColor aWhite(pWriteAcc->GetBestMatchingColor(COL_WHITE));
+    const sal_Int32 nWidth = pWriteAcc->Width();
+    const sal_Int32 nHeight = pWriteAcc->Height();
+
+    if (pReadAcc->HasPalette())
     {
-        Bitmap aNewBmp(aBitmap.GetSizePixel(), vcl::PixelFormat::N1_BPP);
-        BitmapScopedWriteAccess pWriteAcc(aNewBmp);
-
-        if (pWriteAcc)
+        for (sal_Int32 nY = 0; nY < nHeight; nY++)
         {
-            const BitmapColor aBlack(pWriteAcc->GetBestMatchingColor(COL_BLACK));
-            const BitmapColor aWhite(pWriteAcc->GetBestMatchingColor(COL_WHITE));
-            const sal_Int32 nWidth = pWriteAcc->Width();
-            const sal_Int32 nHeight = pWriteAcc->Height();
-
-            if (pReadAcc->HasPalette())
+            Scanline pScanline = pWriteAcc->GetScanline(nY);
+            Scanline pScanlineRead = pReadAcc->GetScanline(nY);
+            for (sal_Int32 nX = 0; nX < nWidth; nX++)
             {
-                for (sal_Int32 nY = 0; nY < nHeight; nY++)
+                const sal_uInt8 cIndex = pReadAcc->GetIndexFromData(pScanlineRead, nX);
+                if (pReadAcc->GetPaletteColor(cIndex).GetLuminance() >= mcThreshold)
                 {
-                    Scanline pScanline = pWriteAcc->GetScanline(nY);
-                    Scanline pScanlineRead = pReadAcc->GetScanline(nY);
-                    for (sal_Int32 nX = 0; nX < nWidth; nX++)
-                    {
-                        const sal_uInt8 cIndex = pReadAcc->GetIndexFromData(pScanlineRead, nX);
-                        if (pReadAcc->GetPaletteColor(cIndex).GetLuminance() >= mcThreshold)
-                        {
-                            pWriteAcc->SetPixelOnData(pScanline, nX, aWhite);
-                        }
-                        else
-                        {
-                            pWriteAcc->SetPixelOnData(pScanline, nX, aBlack);
-                        }
-                    }
+                    pWriteAcc->SetPixelOnData(pScanline, nX, aWhite);
+                }
+                else
+                {
+                    pWriteAcc->SetPixelOnData(pScanline, nX, aBlack);
                 }
             }
-            else
-            {
-                for (sal_Int32 nY = 0; nY < nHeight; nY++)
-                {
-                    Scanline pScanline = pWriteAcc->GetScanline(nY);
-                    Scanline pScanlineRead = pReadAcc->GetScanline(nY);
-                    for (sal_Int32 nX = 0; nX < nWidth; nX++)
-                    {
-                        if (pReadAcc->GetPixelFromData(pScanlineRead, nX).GetLuminance()
-                            >= mcThreshold)
-                        {
-                            pWriteAcc->SetPixelOnData(pScanline, nX, aWhite);
-                        }
-                        else
-                        {
-                            pWriteAcc->SetPixelOnData(pScanline, nX, aBlack);
-                        }
-                    }
-                }
-            }
-
-            pWriteAcc.reset();
-            bRet = true;
         }
-
-        pReadAcc.reset();
-
-        if (bRet)
+    }
+    else
+    {
+        for (sal_Int32 nY = 0; nY < nHeight; nY++)
         {
-            const MapMode aMap(aBitmap.GetPrefMapMode());
-            const Size aSize(aBitmap.GetPrefSize());
-
-            aBitmap = aNewBmp;
-
-            aBitmap.SetPrefMapMode(aMap);
-            aBitmap.SetPrefSize(aSize);
+            Scanline pScanline = pWriteAcc->GetScanline(nY);
+            Scanline pScanlineRead = pReadAcc->GetScanline(nY);
+            for (sal_Int32 nX = 0; nX < nWidth; nX++)
+            {
+                if (pReadAcc->GetPixelFromData(pScanlineRead, nX).GetLuminance() >= mcThreshold)
+                {
+                    pWriteAcc->SetPixelOnData(pScanline, nX, aWhite);
+                }
+                else
+                {
+                    pWriteAcc->SetPixelOnData(pScanline, nX, aBlack);
+                }
+            }
         }
     }
 
-    if (bRet)
-        return BitmapEx(aBitmap);
+    pWriteAcc.reset();
+    pReadAcc.reset();
 
-    return BitmapEx();
+    const MapMode aMap(aBitmap.GetPrefMapMode());
+    const Size aSize(aBitmap.GetPrefSize());
+
+    aBitmap = aNewBmp;
+
+    aBitmap.SetPrefMapMode(aMap);
+    aBitmap.SetPrefSize(aSize);
+
+    return BitmapEx(aBitmap);
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

vcl/source/bitmap/BitmapScaleConvolutionFilter.cxx
@@ -188,79 +188,71 @@ bool ImplScaleConvolutionVer(Bitmap& rSource, Bitmap& rTarget, const double& rSc
     }
 
     Bitmap::ScopedReadAccess pReadAcc(rSource);
+    if(!pReadAcc)
+        return false;
 
-    if(pReadAcc)
+    std::vector<sal_Int16> aWeights;
+    std::vector<sal_Int32> aPixels;
+    std::vector<sal_Int32> aCounts;
+    sal_Int32 aNumberOfContributions(0);
+
+    const sal_Int32 nWidth(rSource.GetSizePixel().Width());
+    ImplCalculateContributions(nHeight, nNewHeight, aNumberOfContributions, aWeights, aPixels, aCounts, aKernel);
+    rTarget = Bitmap(Size(nWidth, nNewHeight), vcl::PixelFormat::N24_BPP);
+    BitmapScopedWriteAccess pWriteAcc(rTarget);
+    if(!pWriteAcc)
+        return false;
+
+    std::vector<BitmapColor> aScanline(nHeight);
+    for(sal_Int32 x(0); x < nWidth; x++)
     {
-        std::vector<sal_Int16> aWeights;
-        std::vector<sal_Int32> aPixels;
-        std::vector<sal_Int32> aCounts;
-        sal_Int32 aNumberOfContributions(0);
-
-        const sal_Int32 nWidth(rSource.GetSizePixel().Width());
-        ImplCalculateContributions(nHeight, nNewHeight, aNumberOfContributions, aWeights, aPixels, aCounts, aKernel);
-        rTarget = Bitmap(Size(nWidth, nNewHeight), vcl::PixelFormat::N24_BPP);
-        BitmapScopedWriteAccess pWriteAcc(rTarget);
-        bool bResult(pWriteAcc);
-
-        if(pWriteAcc)
+        for(sal_Int32 y(0); y < nHeight; y++)
+                if(pReadAcc->HasPalette())
+                    aScanline[y] = pReadAcc->GetPaletteColor(pReadAcc->GetPixelIndex(y, x));
+                else
+                    aScanline[y] = pReadAcc->GetPixel(y, x);
+        for(sal_Int32 y(0); y < nNewHeight; y++)
         {
-            std::vector<BitmapColor> aScanline(nHeight);
-            for(sal_Int32 x(0); x < nWidth; x++)
+            const sal_Int32 aBaseIndex(y * aNumberOfContributions);
+            sal_Int32 aSum(0);
+            sal_Int32 aValueRed(0);
+            sal_Int32 aValueGreen(0);
+            sal_Int32 aValueBlue(0);
+
+            for(sal_Int32 j(0); j < aCounts[y]; j++)
             {
-                for(sal_Int32 y(0); y < nHeight; y++)
-                        if(pReadAcc->HasPalette())
-                            aScanline[y] = pReadAcc->GetPaletteColor(pReadAcc->GetPixelIndex(y, x));
-                        else
-                            aScanline[y] = pReadAcc->GetPixel(y, x);
-                for(sal_Int32 y(0); y < nNewHeight; y++)
-                {
-                    const sal_Int32 aBaseIndex(y * aNumberOfContributions);
-                    sal_Int32 aSum(0);
-                    sal_Int32 aValueRed(0);
-                    sal_Int32 aValueGreen(0);
-                    sal_Int32 aValueBlue(0);
-
-                    for(sal_Int32 j(0); j < aCounts[y]; j++)
-                    {
-                        const sal_Int32 aIndex(aBaseIndex + j);
-                        const sal_Int16 aWeight(aWeights[aIndex]);
-                        aSum += aWeight;
-                        const BitmapColor & aColor = aScanline[aPixels[aIndex]];
-                        aValueRed += aWeight * aColor.GetRed();
-                        aValueGreen += aWeight * aColor.GetGreen();
-                        aValueBlue += aWeight * aColor.GetBlue();
-                    }
-
-                    assert(aSum != 0);
-
-                    const BitmapColor aResultColor(
-                        static_cast< sal_uInt8 >(MinMax(static_cast< sal_Int32 >(aValueRed / aSum), 0, 255)),
-                        static_cast< sal_uInt8 >(MinMax(static_cast< sal_Int32 >(aValueGreen / aSum), 0, 255)),
-                        static_cast< sal_uInt8 >(MinMax(static_cast< sal_Int32 >(aValueBlue / aSum), 0, 255)));
-
-                    if(pWriteAcc->HasPalette())
-                    {
-                        pWriteAcc->SetPixelIndex(y, x, static_cast< sal_uInt8 >(pWriteAcc->GetBestPaletteIndex(aResultColor)));
-                    }
-                    else
-                    {
-                        pWriteAcc->SetPixel(y, x, aResultColor);
-                    }
-                }
+                const sal_Int32 aIndex(aBaseIndex + j);
+                const sal_Int16 aWeight(aWeights[aIndex]);
+                aSum += aWeight;
+                const BitmapColor & aColor = aScanline[aPixels[aIndex]];
+                aValueRed += aWeight * aColor.GetRed();
+                aValueGreen += aWeight * aColor.GetGreen();
+                aValueBlue += aWeight * aColor.GetBlue();
             }
-        }
 
-        aWeights.clear();
-        aCounts.clear();
-        aPixels.clear();
+            assert(aSum != 0);
 
-        if(bResult)
-        {
-            return true;
+            const BitmapColor aResultColor(
+                static_cast< sal_uInt8 >(MinMax(static_cast< sal_Int32 >(aValueRed / aSum), 0, 255)),
+                static_cast< sal_uInt8 >(MinMax(static_cast< sal_Int32 >(aValueGreen / aSum), 0, 255)),
+                static_cast< sal_uInt8 >(MinMax(static_cast< sal_Int32 >(aValueBlue / aSum), 0, 255)));
+
+            if(pWriteAcc->HasPalette())
+            {
+                pWriteAcc->SetPixelIndex(y, x, static_cast< sal_uInt8 >(pWriteAcc->GetBestPaletteIndex(aResultColor)));
+            }
+            else
+            {
+                pWriteAcc->SetPixel(y, x, aResultColor);
+            }
         }
     }
 
-    return false;
+    aWeights.clear();
+    aCounts.clear();
+    aPixels.clear();
+
+    return true;
 }
 
 bool ImplScaleConvolution(Bitmap& rBitmap, const double& rScaleX, const double& rScaleY, const Kernel& aKernel)

commit b0c68499ab36d2a1bdf6f1bc7d4cd259b99700e7

tdf#158224 Cannot convert bitmap to polygons
regression from
    commit a76a135c9e9bdecd38970e293e72eeeeca000d27
    Author: Noel Grandin <noelgrandin@gmail.com>
    Date:   Tue Jan 3 20:14:21 2023 +0200
    flatten some vcl code

Change-Id: Ibb2a059b283dc2e3bf3fecbf24ff71332a204d5d
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/159495
Tested-by: Noel Grandin <noel.grandin@collabora.co.uk>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/source/bitmap/BitmapSimpleColorQuantizationFilter.cxx
@@ -19,7 +19,7 @@ BitmapEx BitmapSimpleColorQuantizationFilter::execute(BitmapEx const& aBitmapEx)
 {
     Bitmap aBitmap = aBitmapEx.GetBitmap();
 
-    if (vcl::numberOfColors(aBitmap.getPixelFormat()) > sal_Int64(mnNewColorCount))
+    if (vcl::numberOfColors(aBitmap.getPixelFormat()) <= sal_Int64(mnNewColorCount))
         return BitmapEx(aBitmap);
 
     Bitmap aNewBmp;