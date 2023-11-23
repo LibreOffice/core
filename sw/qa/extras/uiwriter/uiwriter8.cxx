LibreOffice/core

commit 0c008ab081aa5bbf53f8562e95e547deae5afc2e

tdf#157132: restore behaviour for TBL_RELBOXNAME
Regression from 8a3dc12a13a3b9e99dbd5000ca6a1d541cf472f7
"8a3dc12a13a3b9e99dbd5000ca6a1d541cf472f7"
TBL_RELBOXNAME was no longer calling ToRelBoxNm

Change-Id: I296d84ab955b4415bf9173ccf72b4d51e55152f8
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/158454
Tested-by: Jenkins
Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

sw/source/core/table/swtable.cxx
@@ -1727,6 +1727,8 @@ void SwTable::UpdateFields(TableFormulaUpdateFlags eFlags)
         {
             if(eFlags == TBL_BOXPTR)
                 pBoxFormula->TryBoxNmToPtr();
+            else if(eFlags == TBL_RELBOXNAME)
+                pBoxFormula->ToRelBoxNm(this);
             else
                 pBoxFormula->ChangeState();
         }

sw/qa/extras/uiwriter/uiwriter8.cxx
@@ -1480,6 +1480,48 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest8, testTdf146573)
     CPPUNIT_ASSERT_EQUAL(OUString("204"), xCellA4->getString());
 }
 
+CPPUNIT_TEST_FIXTURE(SwUiWriterTest8, testTdf157132)
+{
+    createSwDoc("tdf157132.odt");
+
+    SwDoc* pDoc = getSwDoc();
+    SwWrtShell* pWrtShell = pDoc->GetDocShell()->GetWrtShell();
+
+    // Go to cell A2
+    pWrtShell->Down(/*bSelect=*/false, /*nCount=*/1);
+
+    // Select A2 and A3 and copy
+    pWrtShell->Down(/*bSelect=*/true, /*nCount=*/1);
+
+    dispatchCommand(mxComponent, ".uno:Copy", {});
+
+    // Go to A4 and paste
+    pWrtShell->Down(/*bSelect=*/false, /*nCount=*/1);
+
+    dispatchCommand(mxComponent, ".uno:Paste", {});
+
+    uno::Reference<text::XTextTablesSupplier> xTextTablesSupplier(mxComponent, uno::UNO_QUERY);
+    uno::Reference<container::XIndexAccess> xTables(xTextTablesSupplier->getTextTables(),
+                                                    uno::UNO_QUERY);
+
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(1), xTables->getCount());
+
+    uno::Reference<text::XTextTable> xTextTable(xTables->getByIndex(0), uno::UNO_QUERY);
+
+    uno::Reference<text::XTextRange> xCellA2(xTextTable->getCellByName("A2"), uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(OUString("2"), xCellA2->getString());
+    uno::Reference<text::XTextRange> xCellA3(xTextTable->getCellByName("A3"), uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(OUString("3"), xCellA3->getString());
+    uno::Reference<text::XTextRange> xCellA4(xTextTable->getCellByName("A4"), uno::UNO_QUERY);
+
+    // Without the fix in place, this test would have failed with
+    // - Expected: 6
+    // - Actual  : 2
+    CPPUNIT_ASSERT_EQUAL(OUString("6"), xCellA4->getString());
+    uno::Reference<text::XTextRange> xCellA5(xTextTable->getCellByName("A5"), uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(OUString("7"), xCellA5->getString());
+}
+
 CPPUNIT_TEST_FIXTURE(SwUiWriterTest8, testTdf147938)
 {
     createSwDoc("tdf147938.fodt");

commit 32ce5fe4ed19a79b6f15a5d4d1892e6cc8d778d9

tdf#158336: Only Change current table
For that, use the same logic as TryBoxNmToPtr

Regression from 0c008ab081aa5bbf53f8562e95e547deae5afc2e
"tdf#157132: restore behaviour for TBL_RELBOXNAME"

Change-Id: I8f93ad3a9686001fd6bd7226400925e43b81ec23
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/159865
Tested-by: Jenkins
Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

sw/inc/cellatr.hxx
@@ -75,6 +75,7 @@ public:
         { return const_cast<SwTableBoxFormula*>(this)->GetTableBox(); }
 
     void TryBoxNmToPtr();
+    void TryRelBoxNm();
     void ToSplitMergeBoxNmWithHistory(SwTableFormulaUpdate& rUpdate, SwHistory* pHistory);
     void ChangeState()
     {

sw/source/core/attr/cellatr.cxx
@@ -104,6 +104,18 @@ void SwTableBoxFormula::TryBoxNmToPtr()
         BoxNmToPtr(&pTableNd->GetTable());
     }
 }
+
+void SwTableBoxFormula::TryRelBoxNm()
+{
+    const SwNode* pNd = GetNodeOfFormula();
+    if (!pNd || &pNd->GetNodes() != &pNd->GetDoc().GetNodes())
+        return;
+    if(const SwTableNode* pTableNd = pNd->FindTableNode())
+    {
+        ToRelBoxNm(&pTableNd->GetTable());
+    }
+}
+
 void SwTableBoxFormula::ToSplitMergeBoxNmWithHistory(SwTableFormulaUpdate& rUpdate, SwHistory* pHistory)
 {
     if(!pHistory)

sw/source/core/table/swtable.cxx
@@ -1728,7 +1728,7 @@ void SwTable::UpdateFields(TableFormulaUpdateFlags eFlags)
             if(eFlags == TBL_BOXPTR)
                 pBoxFormula->TryBoxNmToPtr();
             else if(eFlags == TBL_RELBOXNAME)
-                pBoxFormula->ToRelBoxNm(this);
+                pBoxFormula->TryRelBoxNm();
             else
                 pBoxFormula->ChangeState();
         }

sw/qa/extras/uiwriter/uiwriter8.cxx
@@ -1517,7 +1517,7 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest8, testTdf157132)
     uno::Reference<container::XIndexAccess> xTables(xTextTablesSupplier->getTextTables(),
                                                     uno::UNO_QUERY);
 
-    CPPUNIT_ASSERT_EQUAL(sal_Int32(1), xTables->getCount());
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(2), xTables->getCount());
 
     uno::Reference<text::XTextTable> xTextTable(xTables->getByIndex(0), uno::UNO_QUERY);
 
@@ -1532,6 +1532,21 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest8, testTdf157132)
     // - Actual  : 2
     CPPUNIT_ASSERT_EQUAL(OUString("6"), xCellA4->getString());
     uno::Reference<text::XTextRange> xCellA5(xTextTable->getCellByName("A5"), uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(OUString("7"), xCellA5->getString());
+
+    xTextTable.set(xTables->getByIndex(1), uno::UNO_QUERY);
+
+    xCellA2.set(xTextTable->getCellByName("A2"), uno::UNO_QUERY);
+
+    // tdf#158336: Without the fix in place, this test would have failed with
+    // - Expected: 2
+    // - Actual  : ** Expression is faulty **
+    CPPUNIT_ASSERT_EQUAL(OUString("2"), xCellA2->getString());
+    xCellA3.set(xTextTable->getCellByName("A3"), uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(OUString("3"), xCellA3->getString());
+    xCellA4.set(xTextTable->getCellByName("A4"), uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(OUString("6"), xCellA4->getString());
+    xCellA5.set(xTextTable->getCellByName("A5"), uno::UNO_QUERY);
     CPPUNIT_ASSERT_EQUAL(OUString("7"), xCellA5->getString());
 }
 