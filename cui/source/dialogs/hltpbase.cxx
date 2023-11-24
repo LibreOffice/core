LibreOffice/core

commit 89d3735e05b98223a49a387421386fd736fc3de6

tdf#146576 - Propose clipboard content when inserting a hyperlink
Change-Id: I30067b88b3d1e196f7ecfd150f5215cc93adf095
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/154931
Tested-by: Jenkins
Reviewed-by: Andreas Heinisch <andreas.heinisch@yahoo.de>

cui/source/dialogs/hltpbase.cxx
@@ -450,14 +450,39 @@ void SvxHyperlinkTabPageBase::Reset( const SfxItemSet& rItemSet)
 
     if ( pHyperlinkItem )
     {
+        // tdf#146576 - propose clipboard content when inserting a hyperlink
+        OUString aStrURL(pHyperlinkItem->GetURL());
+        // Store initial URL
+        maStrInitURL = aStrURL;
+        if (aStrURL.isEmpty())
+        {
+            if (auto xClipboard = GetSystemClipboard())
+            {
+                if (auto xTransferable = xClipboard->getContents())
+                {
+                    css::datatransfer::DataFlavor aFlavor;
+                    SotExchange::GetFormatDataFlavor(SotClipboardFormatId::STRING, aFlavor);
+                    if (xTransferable->isDataFlavorSupported(aFlavor))
+                    {
+                        OUString aClipBoardConentent;
+                        if (xTransferable->getTransferData(aFlavor) >>= aClipBoardConentent)
+                        {
+                            INetURLObject aURL;
+                            aURL.SetSmartURL(aClipBoardConentent);
+                            if (!aURL.HasError())
+                                aStrURL
+                                    = aURL.GetMainURL(INetURLObject::DecodeMechanism::Unambiguous);
+                        }
+                    }
+                }
+            }
+        }
+
         // set dialog-fields
         FillStandardDlgFields (pHyperlinkItem);
 
         // set all other fields
-        FillDlgFields ( pHyperlinkItem->GetURL() );
-
-        // Store initial URL
-        maStrInitURL = pHyperlinkItem->GetURL();
+        FillDlgFields(aStrURL);
     }
 }
 

sw/qa/uitest/writer_tests3/hyperlinkdialog.py
@@ -105,6 +105,22 @@ class HyperlinkDialog(UITestCase):
                 # i.e. the last used tab in the hyperlink dialog was not remembered
                 self.assertEqual("1", get_state_as_dict(xTab)["CurrPagePos"])
 
+    def test_tdf146576_propose_clipboard_content(self):
+        with self.ui_test.create_doc_in_start_center("writer"):
+            # Insert a sample URL
+            xWriterDoc = self.xUITest.getTopFocusWindow()
+            xWriterEdit = xWriterDoc.getChild("writer_edit")
+            xWriterEdit.executeAction("TYPE", mkPropertyValues({"TEXT": "www.libreoffice.org"}))
+
+            # Copy URL and open the hyperlink dialog
+            self.xUITest.executeCommand(".uno:SelectAll")
+            self.xUITest.executeCommand(".uno:Copy")
+            with self.ui_test.execute_dialog_through_command(".uno:HyperlinkDialog", close_button="cancel") as xDialog:
+                xTab = xDialog.getChild("tabcontrol")
+                select_pos(xTab, "0")
+                # Check if the content of the clipboard is proposed as URL in the hyperlink dialog
+                xTarget = xDialog.getChild("target")
+                self.assertEqual(get_state_as_dict(xTarget)["Text"].lower(), "http://www.libreoffice.org/")
 
     def test_tdf141166(self):
         # Skip this test for --with-help=html and --with-help=online, as that would fail with a

commit 516f800f84b533db0082b1f39c19d1af40ab29c8

tdf#158345: Opening Hyperlink dialog leads to crash
terminate called after throwing an instance of 'com::sun::star::datatransfer::UnsupportedFlavorException'

relevant part of bt from this exception:
0  0x00007f34fd6b0231 in __cxa_throw () at /lib/x86_64-linux-gnu/libstdc++.so.6
1  0x00007f34e9d8207d in x11::X11Transferable::getTransferData(com::sun::star::datatransfer::DataFlavor const&) (this=0x562594fc9af0, rFlavor=...)
    at /home/julien/lo/libreoffice/vcl/unx/generic/dtrans/X11_transferable.cxx:58
2  0x00007f34e9d825ec in non-virtual thunk to x11::X11Transferable::getTransferData(com::sun::star::datatransfer::DataFlavor const&) () at /home/julien/lo/libreoffice/instdir/program/libvclplug_genlo.so
3  0x00007f34e15589f2 in SvxHyperlinkTabPageBase::Reset(SfxItemSet const&) (this=0x5625958378f0, rItemSet=SfxItemSet of pool 0x562591482920 with parent 0x0 and Which ranges: [(10361, 10362)] = {...})
    at /home/julien/lo/libreoffice/cui/source/dialogs/hltpbase.cxx:468

See full bt here: https://bugs.documentfoundation.org/attachment.cgi?id=191029

Regression from:
89d3735e05b98223a49a387421386fd736fc3de6^!
tdf#146576 - Propose clipboard content when inserting a hyperlink

Change-Id: Ic111b8348f2288c4e9e6760fe6f7f48faadec582
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/159939
Tested-by: Julien Nabet <serval2412@yahoo.fr>
Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

cui/source/dialogs/hltpbase.cxx
@@ -37,6 +37,7 @@
 #include <strings.hrc>
 #include <dialmgr.hxx>
 #include <bitmaps.hlst>
+#include <com/sun/star/datatransfer/UnsupportedFlavorException.hpp>
 
 using namespace ::ucbhelper;
 
@@ -465,13 +466,21 @@ void SvxHyperlinkTabPageBase::Reset( const SfxItemSet& rItemSet)
                     if (xTransferable->isDataFlavorSupported(aFlavor))
                     {
                         OUString aClipBoardConentent;
-                        if (xTransferable->getTransferData(aFlavor) >>= aClipBoardConentent)
+                        try
                         {
-                            INetURLObject aURL;
-                            aURL.SetSmartURL(aClipBoardConentent);
-                            if (!aURL.HasError())
-                                aStrURL
-                                    = aURL.GetMainURL(INetURLObject::DecodeMechanism::Unambiguous);
+                            if (xTransferable->getTransferData(aFlavor) >>= aClipBoardConentent)
+                            {
+                                INetURLObject aURL;
+                                aURL.SetSmartURL(aClipBoardConentent);
+                                if (!aURL.HasError())
+                                    aStrURL
+                                        = aURL.GetMainURL(INetURLObject::DecodeMechanism::Unambiguous);
+                            }
+                        }
+                        // tdf#158345: Opening Hyperlink dialog leads to crash
+                        // MimeType = "text/plain;charset=utf-16"
+                        catch(const css::datatransfer::UnsupportedFlavorException&)
+                        {
                         }
                     }
                 }