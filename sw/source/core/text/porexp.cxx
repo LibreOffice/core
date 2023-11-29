LibreOffice/core

commit 28675af84ae8e2342bd78be3696dc09de6ce5cc5

tdf#41652: Variable width NBSP
Adds a flag that when enabled (opt-in for both old and new documents) changes behaviour of NBSP to not be strictly fixed width.

This commit also implements this behaviour such that NBSP has always the width of a standard space (0x20) on the given line. This change is only noticeable when the paragraph is justified.

Lastly a tilde or a degree character is now shown in place of NBSP when nonprintable characters are enabled, since the gray field alone is not visible in many circumstances.

Change-Id: Iabb30b5930ced62691cc4304f60c5e6dc886ed61
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/149576
Tested-by: Jenkins
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

sw/source/core/text/porexp.cxx
@@ -18,6 +18,7 @@
  */
 
 #include <viewopt.hxx>
+#include <IDocumentSettingAccess.hxx>
 #include <SwPortionHandler.hxx>
 #include "inftxt.hxx"
 #include "porexp.hxx"
@@ -204,14 +205,57 @@ bool SwBlankPortion::Format( SwTextFormatInfo &rInf )
 
 void SwBlankPortion::Paint( const SwTextPaintInfo &rInf ) const
 {
-    if( !m_bMulti ) // No gray background for multiportion brackets
-        rInf.DrawViewOpt( *this, PortionType::Blank );
-    SwExpandPortion::Paint( rInf );
+    if (m_cChar == CHAR_HARDBLANK)
+    {
+        if (rInf.GetOpt().IsBlank())
+        {
+            // Draw background
+            rInf.DrawViewOpt(*this, PortionType::Blank);
+
+            // Draw tilde or degree sign
+            OUString aMarker = (rInf.GetTextFrame()->GetDoc().getIDocumentSettingAccess()
+                                    .get(DocumentSettingId::USE_VARIABLE_WIDTH_NBSP)
+                                  ? u"~"
+                                  : u"°");
+
+            SwPosSize aMarkerSize(rInf.GetTextSize(aMarker));
+            Point aPos(rInf.GetPos());
+
+            std::shared_ptr<SwRect> pPortionRect = std::make_shared<SwRect>();
+            rInf.CalcRect(*this, pPortionRect.get());
+            aPos.AdjustX((pPortionRect->Width() / 2) - (aMarkerSize.Width() / 2));
+
+            SwTextPaintInfo aInf(rInf, &aMarker);
+            aInf.SetPos(aPos);
+            SwTextPortion aMarkerPor;
+            aMarkerPor.Width(aMarkerSize.Width());
+            aMarkerPor.Height(aMarkerSize.Height());
+            aMarkerPor.SetAscent(GetAscent());
+
+            Color colorBackup = aInf.GetFont()->GetColor();
+            aInf.GetFont()->SetColor(NON_PRINTING_CHARACTER_COLOR);
+            aInf.DrawText(aMarkerPor, TextFrameIndex(aMarker.getLength()), true);
+            aInf.GetFont()->SetColor(colorBackup);
+        }
+    }
+    else
+    {
+        if (!m_bMulti) // No gray background for multiportion brackets
+            rInf.DrawViewOpt(*this, PortionType::Blank);
+
+        SwExpandPortion::Paint(rInf);
+    }
 }
 
-bool SwBlankPortion::GetExpText( const SwTextSizeInfo&, OUString &rText ) const
+bool SwBlankPortion::GetExpText( const SwTextSizeInfo& rInf, OUString &rText ) const
 {
-    rText = OUString(m_cChar);
+    if (m_cChar == CHAR_HARDBLANK
+        && rInf.GetTextFrame()->GetDoc().getIDocumentSettingAccess().get(
+            DocumentSettingId::USE_VARIABLE_WIDTH_NBSP))
+        rText = OUString(CH_BLANK);
+    else
+        rText = OUString(m_cChar);
+
     return true;
 }
 

sw/qa/core/text/text.cxx
@@ -1186,6 +1186,77 @@ CPPUNIT_TEST_FIXTURE(SwCoreTextTest, testNumberPortionNoformat)
         getXPath(pXmlDoc, "//SwParaPortion/SwLineLayout/SwFieldPortion/SwFont", "color"));
 }
 
+CPPUNIT_TEST_FIXTURE(SwCoreTextTest, testTdf41652NBSPWidth)
+{
+    sal_Int32 nSectionAfterNBSPX_legacy_leftAligned;
+    sal_Int32 nSectionAfterNBSPX_legacy_justified;
+    sal_Int32 nSectionAfterNBSPX_optionDisabled_leftAligned;
+    sal_Int32 nSectionAfterNBSPX_optionDisabled_justified;
+    sal_Int32 nSectionAfterNBSPX_optionEnabled_leftAligned;
+    sal_Int32 nSectionAfterNBSPX_optionEnabled_justified;
+
+    // Measure the X position of sections after NBSPs in a legacy file (no option value set)
+    {
+        createSwDoc("tdf41652_legacy.fodt");
+        SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument*>(mxComponent.get());
+        SwDocShell* pShell = pTextDoc->GetDocShell();
+        std::shared_ptr<GDIMetaFile> xMetaFile = pShell->GetPreviewMetaFile();
+        MetafileXmlDump aDumper;
+        xmlDocUniquePtr pXmlDoc = dumpAndParse(aDumper, *xMetaFile);
+
+        nSectionAfterNBSPX_legacy_leftAligned = getXPath(pXmlDoc, "//textarray[3]", "x").toInt32();
+        nSectionAfterNBSPX_legacy_justified = getXPath(pXmlDoc, "//textarray[8]", "x").toInt32();
+    }
+
+    // Measure the X of sections after NBSPs in a file with the option enabled
+    {
+        createSwDoc("tdf41652_variableNBSPdisabled.fodt");
+        SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument*>(mxComponent.get());
+        SwDocShell* pShell = pTextDoc->GetDocShell();
+        std::shared_ptr<GDIMetaFile> xMetaFile = pShell->GetPreviewMetaFile();
+        MetafileXmlDump aDumper;
+        xmlDocUniquePtr pXmlDoc = dumpAndParse(aDumper, *xMetaFile);
+
+        nSectionAfterNBSPX_optionDisabled_leftAligned
+            = getXPath(pXmlDoc, "//textarray[3]", "x").toInt32();
+        nSectionAfterNBSPX_optionDisabled_justified
+            = getXPath(pXmlDoc, "//textarray[8]", "x").toInt32();
+    }
+
+    // Measure the X of the sections after NBSPs in a file with the option enabled
+    {
+        createSwDoc("tdf41652_variableNBSPenabled.fodt");
+        SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument*>(mxComponent.get());
+        SwDocShell* pShell = pTextDoc->GetDocShell();
+        std::shared_ptr<GDIMetaFile> xMetaFile = pShell->GetPreviewMetaFile();
+        MetafileXmlDump aDumper;
+        xmlDocUniquePtr pXmlDoc = dumpAndParse(aDumper, *xMetaFile);
+
+        nSectionAfterNBSPX_optionEnabled_leftAligned
+            = getXPath(pXmlDoc, "//textarray[3]", "x").toInt32();
+        nSectionAfterNBSPX_optionEnabled_justified
+            = getXPath(pXmlDoc, "//textarray[8]", "x").toInt32();
+    }
+
+    // Assert left aligned NBSP for the legacy file is larger than zero
+    CPPUNIT_ASSERT(nSectionAfterNBSPX_legacy_leftAligned > 0);
+    // Assert both NBSPs have same width for the legacy file
+    CPPUNIT_ASSERT_EQUAL(nSectionAfterNBSPX_legacy_leftAligned,
+                         nSectionAfterNBSPX_legacy_justified);
+    // Assert left aligned NBSP is same width for legacy file as for the disabled file
+    CPPUNIT_ASSERT_EQUAL(nSectionAfterNBSPX_legacy_leftAligned,
+                         nSectionAfterNBSPX_optionDisabled_leftAligned);
+    // Assert justified NBSP is same width for legacy file as for the disabled file
+    CPPUNIT_ASSERT_EQUAL(nSectionAfterNBSPX_legacy_justified,
+                         nSectionAfterNBSPX_optionDisabled_justified);
+    // Assert left aligned NBSP is same width for the disabled file as for the enabled file
+    CPPUNIT_ASSERT_EQUAL(nSectionAfterNBSPX_optionDisabled_leftAligned,
+                         nSectionAfterNBSPX_optionEnabled_leftAligned);
+    // Assert justified NBSP is wider for the enabled file
+    CPPUNIT_ASSERT(nSectionAfterNBSPX_optionDisabled_justified
+                   < nSectionAfterNBSPX_optionEnabled_justified);
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

include/unotools/compatibility.hxx
@@ -60,6 +60,8 @@ class SvtCompatibilityEntry
             MsWordTrailingBlanks,
             SubtractFlysAnchoredAtFlys,
             EmptyDbFieldHidesPara,
+            UseVariableWidthNBSP,
+
             /// special entry: optcomp.cxx converts the other values to
             /// integers but not this one because it doesn't have its own
             /// checkbox, so keep it at the end!

officecfg/registry/schema/org/openoffice/Office/Compatibility.xcs
@@ -128,6 +128,12 @@
         </info>
         <value>true</value>
       </prop>
+      <prop oor:name="UseVariableWidthNBSP" oor:type="xs:boolean" oor:nillable="false">
+        <info>
+          <desc>Render NBSP as having variable size, instead of having a fixed size</desc>
+        </info>
+        <value>false</value>
+      </prop>
     </group>
   </templates>
   <component>

sw/inc/IDocumentSettingAccess.hxx
@@ -126,7 +126,9 @@ enum class DocumentSettingId
     NO_NUMBERING_SHOW_FOLLOWBY,
     // drop cap punctuation: smaller dashes, bullet, asterisks, quotation marks etc.
     // by extending the rounding box of the glyph to the baseline
-    DROP_CAP_PUNCTUATION
+    DROP_CAP_PUNCTUATION,
+    // render NBSP as standard-space-width (prettier when justified)
+    USE_VARIABLE_WIDTH_NBSP,
 };
 
 /** Provides access to settings of a document

commit 78872cc81bf4b1377c9cae8f9040a2443d0690c0

tdf#157768 sw: fix lost character background of NBSP
Regression from commit 28675af84ae8e2342bd78be3696dc09de6ce5cc5
"tdf#41652: Variable width NBSP".

Change-Id: I066ecadac8961b182f8580147b197f13dd0daae4
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/160054
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/source/core/text/porexp.cxx
@@ -211,6 +211,7 @@ void SwBlankPortion::Paint( const SwTextPaintInfo &rInf ) const
     // Draw field shade (can be disabled individually)
     if (!m_bMulti) // No gray background for multiportion brackets
         rInf.DrawViewOpt(*this, PortionType::Blank);
+    SwExpandPortion::Paint(rInf);
 
     if (m_cChar == CHAR_HARDBLANK)
     {
@@ -241,10 +242,6 @@ void SwBlankPortion::Paint( const SwTextPaintInfo &rInf ) const
             aInf.DrawText(aMarkerPor, TextFrameIndex(aMarker.getLength()), true);
             aInf.GetFont()->SetColor(colorBackup);
         }
-    }
-    else
-    {
-        SwExpandPortion::Paint(rInf);
     }
 }
 

sw/qa/core/text/text.cxx
@@ -1263,9 +1263,9 @@ CPPUNIT_TEST_FIXTURE(SwCoreTextTest, testTdf41652NBSPWidth)
         xmlDocUniquePtr pXmlDoc = dumpAndParse(aDumper, *xMetaFile);
 
         nSectionAfterNBSPX_legacy_leftAligned
-            = getXPath(pXmlDoc, "//textarray[3]"_ostr, "x"_ostr).toInt32();
+            = getXPath(pXmlDoc, "//textarray[4]"_ostr, "x"_ostr).toInt32();
         nSectionAfterNBSPX_legacy_justified
-            = getXPath(pXmlDoc, "//textarray[8]"_ostr, "x"_ostr).toInt32();
+            = getXPath(pXmlDoc, "//textarray[10]"_ostr, "x"_ostr).toInt32();
     }
 
     // Measure the X of sections after NBSPs in a file with the option enabled
@@ -1278,9 +1278,9 @@ CPPUNIT_TEST_FIXTURE(SwCoreTextTest, testTdf41652NBSPWidth)
         xmlDocUniquePtr pXmlDoc = dumpAndParse(aDumper, *xMetaFile);
 
         nSectionAfterNBSPX_optionDisabled_leftAligned
-            = getXPath(pXmlDoc, "//textarray[3]"_ostr, "x"_ostr).toInt32();
+            = getXPath(pXmlDoc, "//textarray[4]"_ostr, "x"_ostr).toInt32();
         nSectionAfterNBSPX_optionDisabled_justified
-            = getXPath(pXmlDoc, "//textarray[8]"_ostr, "x"_ostr).toInt32();
+            = getXPath(pXmlDoc, "//textarray[10]"_ostr, "x"_ostr).toInt32();
     }
 
     // Measure the X of the sections after NBSPs in a file with the option enabled
@@ -1293,9 +1293,9 @@ CPPUNIT_TEST_FIXTURE(SwCoreTextTest, testTdf41652NBSPWidth)
         xmlDocUniquePtr pXmlDoc = dumpAndParse(aDumper, *xMetaFile);
 
         nSectionAfterNBSPX_optionEnabled_leftAligned
-            = getXPath(pXmlDoc, "//textarray[3]"_ostr, "x"_ostr).toInt32();
+            = getXPath(pXmlDoc, "//textarray[4]"_ostr, "x"_ostr).toInt32();
         nSectionAfterNBSPX_optionEnabled_justified
-            = getXPath(pXmlDoc, "//textarray[8]"_ostr, "x"_ostr).toInt32();
+            = getXPath(pXmlDoc, "//textarray[10]"_ostr, "x"_ostr).toInt32();
     }
 
     // Assert left aligned NBSP for the legacy file is larger than zero