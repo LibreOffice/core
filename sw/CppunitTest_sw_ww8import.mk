eb077be775237d5f2584d422f9abc5c86120a183
Miklos Vajna <vmiklos@collabora.com>
2023-11-29 20:04:01 +0100

    CppunitTest_sw_ww8import: avoid external fonts
    - sw/qa/extras/ww8import/data/tdf124601.doc:
      - replace 'Open Sans Light' with 'Noto Sans'
      - replace 'Open Sans' with 'Noto Sans'
    
    Change-Id: I3f3018d9d2a216b8ddb1d26cab845b27f0f7e9f2
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/160128
    Tested-by: Jenkins
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

--- sw/CppunitTest_sw_ww8import.mk
+++ sw/CppunitTest_sw_ww8import.mk
@@ -61,4 +61,8 @@ $(eval $(call gb_CppunitTest_use_rdb,sw_ww8import,services))
 
 $(eval $(call gb_CppunitTest_use_configuration,sw_ww8import))
 
+ifneq ($(filter MORE_FONTS,$(BUILD_TYPE)),)
+$(eval $(call gb_CppunitTest_set_non_application_font_use,sw_ww8import,abort))
+endif
+
 # vim: set noet sw=4 ts=4:
