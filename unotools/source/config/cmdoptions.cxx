LibreOffice/core

commit 1576ef8532cf2cea2da361db9c5707f1994e65e2

osl::Mutex->std::mutex in SvtCommandOptions
Change-Id: I80eaba645351e2f3a3c67586fdced3325608633e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/127413
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

unotools/source/config/cmdoptions.cxx
@@ -84,6 +84,12 @@ class SvtCmdOptions
         CommandHashMap m_aCommandHashMap;
 };
 
+std::mutex& GetOwnStaticMutex()
+{
+    static std::mutex theCommandOptionsMutex;
+    return theCommandOptionsMutex;
+}
+
 }
 
 typedef ::std::vector< css::uno::WeakReference< css::frame::XFrame > > SvtFrameVector;
@@ -185,7 +191,7 @@ SvtCommandOptions_Impl::~SvtCommandOptions_Impl()
 
 void SvtCommandOptions_Impl::Notify( const Sequence< OUString >& )
 {
-    MutexGuard aGuard( SvtCommandOptions::GetOwnStaticMutex() );
+    std::unique_lock aGuard( GetOwnStaticMutex() );
 
     Sequence< OUString >    lNames   = impl_GetPropertyNames ();
     Sequence< Any >         lValues  = GetProperties         ( lNames         );
@@ -296,13 +302,14 @@ std::weak_ptr<SvtCommandOptions_Impl> g_pCommandOptions;
 SvtCommandOptions::SvtCommandOptions()
 {
     // Global access, must be guarded (multithreading!).
-    MutexGuard aGuard( GetOwnStaticMutex() );
+    std::unique_lock aGuard( GetOwnStaticMutex() );
 
     m_pImpl = g_pCommandOptions.lock();
     if( !m_pImpl )
     {
         m_pImpl = std::make_shared<SvtCommandOptions_Impl>();
         g_pCommandOptions = m_pImpl;
+        aGuard.unlock(); // because holdConfigItem will call this constructor
         ItemHolder1::holdConfigItem(EItem::CmdOptions);
     }
 }
@@ -310,7 +317,7 @@ SvtCommandOptions::SvtCommandOptions()
 SvtCommandOptions::~SvtCommandOptions()
 {
     // Global access, must be guarded (multithreading!)
-    MutexGuard aGuard( GetOwnStaticMutex() );
+    std::unique_lock aGuard( GetOwnStaticMutex() );
 
     m_pImpl.reset();
 }
@@ -319,7 +326,7 @@ SvtCommandOptions::~SvtCommandOptions()
 
 bool SvtCommandOptions::HasEntries( CmdOption eOption ) const
 {
-    MutexGuard aGuard( GetOwnStaticMutex() );
+    std::unique_lock aGuard( GetOwnStaticMutex() );
     return m_pImpl->HasEntries( eOption );
 }
 
@@ -327,7 +334,7 @@ bool SvtCommandOptions::HasEntries( CmdOption eOption ) const
 
 bool SvtCommandOptions::Lookup( CmdOption eCmdOption, const OUString& aCommandURL ) const
 {
-    MutexGuard aGuard( GetOwnStaticMutex() );
+    std::unique_lock aGuard( GetOwnStaticMutex() );
     return m_pImpl->Lookup( eCmdOption, aCommandURL );
 }
 
@@ -335,16 +342,8 @@ bool SvtCommandOptions::Lookup( CmdOption eCmdOption, const OUString& aCommandUR
 
 void SvtCommandOptions::EstablishFrameCallback(const css::uno::Reference< css::frame::XFrame >& xFrame)
 {
-    MutexGuard aGuard( GetOwnStaticMutex() );
+    std::unique_lock aGuard( GetOwnStaticMutex() );
     m_pImpl->EstablishFrameCallback(xFrame);
-}
-
-//  private method
-
-Mutex& SvtCommandOptions::GetOwnStaticMutex()
-{
-    static osl::Mutex theCommandOptionsMutex;
-    return theCommandOptionsMutex;
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

include/unotools/cmdoptions.hxx
@@ -110,17 +110,6 @@ public:
     void EstablishFrameCallback(const css::uno::Reference<css::frame::XFrame>& xFrame);
 
 private:
-    /*-****************************************************************************************************
-        @short      return a reference to a static mutex
-        @descr      These class is partially threadsafe (for de-/initialization only).
-                    All access methods aren't safe!
-                    We create a static mutex only for one ime and use at different times.
-        @return     A reference to a static mutex member.
-    **-***************************************************************************************************/
-
-    UNOTOOLS_DLLPRIVATE static ::osl::Mutex& GetOwnStaticMutex();
-
-private:
     std::shared_ptr<SvtCommandOptions_Impl> m_pImpl;
 
 }; // class SvtCmdOptions

commit de9667d419bae2c03adf9a9c08b20cd519137cba

tdf#157915 Error when running a Basic macro with instruction commitChanges
regression from
    commit 1576ef8532cf2cea2da361db9c5707f1994e65e2
    Author: Noel Grandin <noelgrandin@gmail.com>
    Date:   Thu Dec 23 18:40:44 2021 +0200
    osl::Mutex->std::mutex in SvtCommandOptions

Note that this only fixes part of the issue - specifically the part that
the original reporter described.

There is a further issue, not fixed here, which only happens on
Linux/GTK, where the messagebox dialog manages to end up behind the
About dialog, and consequently cannot be closed, which freezes the
program.

Change-Id: Idbb7f51e494a69886c80eb92efaa7cf7c6911c16
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/160393
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

unotools/source/config/cmdoptions.cxx
@@ -222,7 +222,9 @@ void SvtCommandOptions_Impl::Notify( const Sequence< OUString >& )
         css::uno::Reference< css::frame::XFrame > xFrame(pIt->get(), css::uno::UNO_QUERY);
         if (xFrame.is())
         {
+            aGuard.unlock(); // because we can call back into ourself
             xFrame->contextChanged();
+            aGuard.lock();
             ++pIt;
         }
         else