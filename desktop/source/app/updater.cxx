LibreOffice/core

commit 926e4e469d0285ee72f8da80f00370b4b3120417

Rename online updater functions and strcmp relpath
Try to be more explicit about the newly allocated absolute path and
the relative offset and actually compare the relative part.

-get_full_path(const NS_tchar *relpath)
+new_absolute_path(const NS_tchar *relpath)

-get_relative_path(const NS_tchar *fullpath)
+get_relative_offset(const NS_tchar *abs_path)

Change-Id: I02abd722fc89a1688502b9c5cd375954f9f0bc05
Reviewed-on: https://gerrit.libreoffice.org/60168
Tested-by: Jenkins
Reviewed-by: Jan-Marek Glogowski <glogow@fbihome.de>

onlineupdate/source/update/updater/updater.cxx
@@ -301,14 +301,15 @@ EnvHasValue(const char *name)
 #endif
 
 /**
- * Coverts a relative update path to a full path.
+ * Coverts a relative update path to a absolute path related to the working
+ * or install directory. Allocates a new NS_tchar[] based path!
  *
  * @param  relpath
  *         The relative path to convert to a full path.
  * @return valid filesystem full path or nullptr if memory allocation fails.
  */
 static NS_tchar*
-get_full_path(const NS_tchar *relpath)
+new_absolute_path(const NS_tchar *relpath)
 {
     NS_tchar *destpath = sStagedUpdate ? gWorkingDirPath : gInstallDirPath;
     size_t lendestpath = NS_tstrlen(destpath);
@@ -358,36 +359,36 @@ bool is_userprofile_in_instdir()
 }
 
 /**
- * Converts a full update path into a relative path; reverses get_full_path.
+ * Get a pointer in the absolute path, relative to the working or install
+ * directory. Returns itself, if not absolute or outside of the directory.
  *
- * @param  fullpath
- *         The absolute path to convert into a relative path.
+ * @param  abs_path
+ *         An absolute path.
  * return pointer to the location within fullpath where the relative path starts
  *        or fullpath itself if it already looks relative.
  */
 static const NS_tchar*
-get_relative_path(const NS_tchar *fullpath)
+get_relative_offset(const NS_tchar *abs_path)
 {
     // If the path isn't absolute, just return it as-is.
 #ifdef _WIN32
-    if (fullpath[1] != ':' && fullpath[2] != '\\')
+    if (abs_path[1] != ':' && abs_path[2] != '\\')
     {
 #else
-    if (fullpath[0] != '/')
+    if (abs_path[0] != '/')
     {
 #endif
-        return fullpath;
+        return abs_path;
     }
 
     NS_tchar *prefix = sStagedUpdate ? gWorkingDirPath : gInstallDirPath;
 
-    // If the path isn't long enough to be absolute, return it as-is.
-    if (NS_tstrlen(fullpath) <= NS_tstrlen(prefix))
-    {
-        return fullpath;
-    }
-
-    return fullpath + NS_tstrlen(prefix) + 1;
+    size_t len = NS_tstrlen(prefix);
+    if (NS_tstrlen(abs_path) <= len)
+        return abs_path;
+    if (0 != strncmp(abs_path, prefix, len))
+        return abs_path;
+    return abs_path + len + 1;
 }
 
 /**
@@ -1136,7 +1137,7 @@ RemoveFile::Parse(NS_tchar *line)
     mRelPath.reset(new NS_tchar[MAXPATHLEN]);
     NS_tstrcpy(mRelPath.get(), validPath);
 
-    mFile.reset(get_full_path(validPath));
+    mFile.reset(new_absolute_path(validPath));
     if (!mFile)
     {
         return PARSE_ERROR;
@@ -1264,7 +1265,7 @@ RemoveDir::Parse(NS_tchar *line)
     mRelPath.reset(new NS_tchar[MAXPATHLEN]);
     NS_tstrcpy(mRelPath.get(), validPath);
 
-    mDir.reset(get_full_path(validPath));
+    mDir.reset(new_absolute_path(validPath));
     if (!mDir)
     {
         return PARSE_ERROR;
@@ -1390,7 +1391,7 @@ AddFile::Parse(NS_tchar *line)
     mRelPath.reset(new NS_tchar[MAXPATHLEN]);
     NS_tstrcpy(mRelPath.get(), validPath);
 
-    mFile.reset(get_full_path(validPath));
+    mFile.reset(new_absolute_path(validPath));
     if (!mFile)
     {
         return PARSE_ERROR;
@@ -1586,7 +1587,7 @@ PatchFile::Parse(NS_tchar *line)
     mFileRelPath.reset(new NS_tchar[MAXPATHLEN]);
     NS_tstrcpy(mFileRelPath.get(), validPath);
 
-    mFile.reset(get_full_path(validPath));
+    mFile.reset(new_absolute_path(validPath));
     if (!mFile)
     {
         return PARSE_ERROR;
@@ -1812,7 +1813,7 @@ AddIfFile::Parse(NS_tchar *line)
 {
     // format "<testfile>" "<newfile>"
 
-    mTestFile.reset(get_full_path(get_valid_path(&line)));
+    mTestFile.reset(new_absolute_path(get_valid_path(&line)));
     if (!mTestFile)
         return PARSE_ERROR;
 
@@ -1879,7 +1880,7 @@ AddIfNotFile::Parse(NS_tchar *line)
 {
     // format "<testfile>" "<newfile>"
 
-    mTestFile.reset(get_full_path(get_valid_path(&line)));
+    mTestFile.reset(new_absolute_path(get_valid_path(&line)));
     if (!mTestFile)
         return PARSE_ERROR;
 
@@ -1946,7 +1947,7 @@ PatchIfFile::Parse(NS_tchar *line)
 {
     // format "<testfile>" "<patchfile>" "<filetopatch>"
 
-    mTestFile.reset(get_full_path(get_valid_path(&line)));
+    mTestFile.reset(new_absolute_path(get_valid_path(&line)));
     if (!mTestFile)
         return PARSE_ERROR;
 
@@ -4036,7 +4037,7 @@ int add_dir_entries(const NS_tchar *dirpath, ActionList *list)
 
     NS_tsnprintf(searchspec, sizeof(searchspec)/sizeof(searchspec[0]),
                  NS_T("%s*"), dirpath);
-    std::unique_ptr<const NS_tchar[]> pszSpec(get_full_path(searchspec));
+    std::unique_ptr<const NS_tchar[]> pszSpec(new_absolute_path(searchspec));
 
     hFindFile = FindFirstFileW(pszSpec.get(), &finddata);
     if (hFindFile != INVALID_HANDLE_VALUE)
@@ -4116,7 +4117,7 @@ int add_dir_entries(const NS_tchar *dirpath, ActionList *list)
         char chars[MAXNAMLEN];
     } ent_buf;
     struct dirent* ent;
-    std::unique_ptr<NS_tchar[]> searchpath(get_full_path(dirpath));
+    std::unique_ptr<NS_tchar[]> searchpath(new_absolute_path(dirpath));
 
     DIR* dir = opendir(searchpath.get());
     if (!dir)
@@ -4157,7 +4158,7 @@ int add_dir_entries(const NS_tchar *dirpath, ActionList *list)
         else
         {
             // Add the file to be removed to the ActionList.
-            NS_tchar *quotedpath = get_quoted_path(get_relative_path(foundpath));
+            NS_tchar *quotedpath = get_quoted_path(get_relative_offset(foundpath));
             if (!quotedpath)
             {
                 closedir(dir);
@@ -4180,7 +4181,7 @@ int add_dir_entries(const NS_tchar *dirpath, ActionList *list)
     closedir(dir);
 
     // Add the directory to be removed to the ActionList.
-    NS_tchar *quotedpath = get_quoted_path(get_relative_path(dirpath));
+    NS_tchar *quotedpath = get_quoted_path(get_relative_offset(dirpath));
     if (!quotedpath)
         return PARSE_ERROR;
 
@@ -4206,7 +4207,7 @@ int add_dir_entries(const NS_tchar *dirpath, ActionList *list)
     int rv = OK;
     FTS *ftsdir;
     FTSENT *ftsdirEntry;
-    std::unique_ptr<NS_tchar[]> searchpath(get_full_path(dirpath));
+    std::unique_ptr<NS_tchar[]> searchpath(new_absolute_path(dirpath));
 
     // Remove the trailing slash so the paths don't contain double slashes. The
     // existence of the slash has already been checked in DoUpdate.
@@ -4242,7 +4243,7 @@ int add_dir_entries(const NS_tchar *dirpath, ActionList *list)
                 // Add the file to be removed to the ActionList.
                 NS_tsnprintf(foundpath, sizeof(foundpath)/sizeof(foundpath[0]),
                              NS_T("%s"), ftsdirEntry->fts_accpath);
-                quotedpath = get_quoted_path(get_relative_path(foundpath));
+                quotedpath = get_quoted_path(get_relative_offset(foundpath));
                 if (!quotedpath)
                 {
                     rv = UPDATER_QUOTED_PATH_MEM_ERROR;
@@ -4261,7 +4262,7 @@ int add_dir_entries(const NS_tchar *dirpath, ActionList *list)
                 // Add the directory to be removed to the ActionList.
                 NS_tsnprintf(foundpath, sizeof(foundpath)/sizeof(foundpath[0]),
                              NS_T("%s/"), ftsdirEntry->fts_accpath);
-                quotedpath = get_quoted_path(get_relative_path(foundpath));
+                quotedpath = get_quoted_path(get_relative_offset(foundpath));
                 if (!quotedpath)
                 {
                     rv = UPDATER_QUOTED_PATH_MEM_ERROR;
@@ -4384,10 +4385,10 @@ GetManifestContents(const NS_tchar *manifest)
 int AddPreCompleteActions(ActionList *list)
 {
 #ifdef MACOSX
-    std::unique_ptr<NS_tchar[]> manifestPath(get_full_path(
+    std::unique_ptr<NS_tchar[]> manifestPath(new_absolute_path(
             NS_T("Contents/Resources/precomplete")));
 #else
-    std::unique_ptr<NS_tchar[]> manifestPath(get_full_path(
+    std::unique_ptr<NS_tchar[]> manifestPath(new_absolute_path(
             NS_T("precomplete")));
 #endif
 

commit 13595f6220095d120e13ccb6fbfac1de4c803fe7

Fix --enable-online-update-mar on Windows
The code had apparently rotten a bit,

* a wchar_t vs. char16_t confusion in desktop/source/app/updater.cxx

* code broken by 926e4e469d0285ee72f8da80f00370b4b3120417 "Rename online updater
  functions and strcmp relpath" in
  onlineupdate/source/update/updater/updater.cxx

* -DUNICODE missing in some places (so that plain Windows functions resolve to
  the ...W variant), which had been set centrally in the past IIRC

* silencing some warnings like "C4267: 'initializing': conversion from 'size_t'
  to 'int', possible loss of data" (where silencing is the right thing to do for
  effectively extern code); no sure why those apparently didn't hit in the past,
  maybe it is all warnings that compilers only started to emit in recent years,
  or only for recent -std:c++... modes

* silencing some "Conversion from string literal loses const qualifier" errors
  with -Zc:strictStrings-; these hit at least with recent VS 2022 Preview and
  --with-latest-c++, where -Zc:strictStrings is apparently now on by default

Change-Id: I7fe46f5aa2b42fc9c03f24f7f0236512b4d3b936
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/160451
Tested-by: Jenkins
Reviewed-by: Stephan Bergmann <stephan.bergmann@allotropia.de>

desktop/source/app/updater.cxx
@@ -28,6 +28,7 @@
 #include <rtl/ustring.hxx>
 #include <unotools/tempfile.hxx>
 #include <unotools/configmgr.hxx>
+#include <o3tl/char16_t2wchar_t.hxx>
 #include <osl/file.hxx>
 #include <rtl/process.h>
 #include <sal/log.hxx>
@@ -316,7 +317,7 @@ bool update()
         SAL_WARN("desktop.updater", "Updater executable path: " << aUpdaterPath);
         for (size_t i = 0; i < 8 + rtl_getAppCommandArgCount(); ++i)
         {
-            SAL_WARN("desktop.updater", pArgs[i]);
+            SAL_WARN("desktop.updater", OUString(o3tl::toU(pArgs[i])));
         }
         bSuccess = false;
     }

onlineupdate/Executable_mar.mk
@@ -46,4 +46,6 @@ $(eval $(call gb_Executable_add_cobjects,mar,\
 	onlineupdate/source/libmar/tool/mar \
 ))
 
+$(eval $(call gb_Executable_set_warnings_not_errors,mar))
+
 # vim:set shiftwidth=4 tabstop=4 noexpandtab: */

onlineupdate/Executable_test_updater_dialog.mk
@@ -60,4 +60,8 @@ $(eval $(call gb_Executable_add_exception_objects,test_updater_dialog,\
  	onlineupdate/workben/test_dialog \
 ))
 
+ifeq ($(OS),WNT)
+$(eval $(call gb_Executable_add_defs,test_updater_dialog,-DUNICODE))
+endif
+
 # vim:set shiftwidth=4 tabstop=4 noexpandtab: */

onlineupdate/Executable_update_service.mk
@@ -55,4 +55,8 @@ $(eval $(call gb_Executable_add_exception_objects,update_service,\
 	onlineupdate/source/service/workmonitor \
 ))
 
+$(eval $(call gb_Executable_add_cxxflags,update_service,-Zc:strictStrings-))
+
+$(eval $(call gb_Executable_add_defs,update_service,-DUNICODE))
+
 # vim:set shiftwidth=4 tabstop=4 noexpandtab: */

onlineupdate/Executable_updater.mk
@@ -82,4 +82,14 @@ $(eval $(call gb_Executable_add_exception_objects,updater,\
 		onlineupdate/source/update/updater/win_dirent )\
 ))
 
+$(eval $(call gb_Executable_set_warnings_not_errors,updater))
+
+ifeq ($(OS),WNT)
+
+$(eval $(call gb_Executable_add_cxxflags,updater,-Zc:strictStrings-))
+
+$(eval $(call gb_Executable_add_defs,updater,-DUNICODE))
+
+endif
+
 # vim:set shiftwidth=4 tabstop=4 noexpandtab: */

onlineupdate/StaticLibrary_libmar.mk
@@ -24,4 +24,6 @@ $(eval $(call gb_StaticLibrary_use_static_libraries,\
 	libmarverify \
 ))
 
+$(eval $(call gb_StaticLibrary_set_warnings_not_errors,libmar))
+
 # vim:set shiftwidth=4 tabstop=4 noexpandtab: */

onlineupdate/StaticLibrary_libmarverify.mk
@@ -29,4 +29,6 @@ $(eval $(call gb_StaticLibrary_add_cobjects,libmarverify,\
 	onlineupdate/source/libmar/verify/cryptox \
 ))
 
+$(eval $(call gb_StaticLibrary_set_warnings_not_errors,libmarverify))
+
 # vim:set shiftwidth=4 tabstop=4 noexpandtab: */

onlineupdate/StaticLibrary_updatehelper.mk
@@ -28,4 +28,10 @@ $(eval $(call gb_StaticLibrary_add_exception_objects,updatehelper,\
 	onlineupdate/source/update/common/updatelogging \
 ))
 
+$(eval $(call gb_StaticLibrary_set_warnings_not_errors,updatehelper))
+
+ifeq ($(OS),WNT)
+$(eval $(call gb_StaticLibrary_add_cxxflags,updatehelper,-Zc:strictStrings-))
+endif
+
 # vim:set shiftwidth=4 tabstop=4 noexpandtab: */

onlineupdate/source/update/updater/updater.cxx
@@ -386,7 +386,7 @@ get_relative_offset(const NS_tchar *abs_path)
     size_t len = NS_tstrlen(prefix);
     if (NS_tstrlen(abs_path) <= len)
         return abs_path;
-    if (0 != strncmp(abs_path, prefix, len))
+    if (0 != NS_tstrncmp(abs_path, prefix, len))
         return abs_path;
     return abs_path + len + 1;
 }