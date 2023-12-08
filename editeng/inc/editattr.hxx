326660093c903427285cfebaef12ecab1236b2db
László Németh <nemeth@numbertext.org>
2023-12-08 08:13:42 +0100

    tdf#129357 editeng: show soft hyphen in text boxes and shapes
    Shapes and text boxes didn't show the optional hyphen
    at line break.
    
    Change-Id: I5cc842964fc91571e5c55995981de697da966b14
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/160453
    Tested-by: Jenkins
    Reviewed-by: László Németh <nemeth@numbertext.org>

--- editeng/inc/editattr.hxx
+++ editeng/inc/editattr.hxx
@@ -54,6 +54,7 @@ class SfxGrabBagItem;
 
 #define CH_FEATURE_OLD  (sal_uInt8)         0xFF
 #define CH_FEATURE      u'\x0001'
+#define CH_SOFTHYPHEN   u'\x00AD'
 
 // DEF_METRIC: For my pool, the DefMetric should always appear when
 // GetMetric (nWhich)!
