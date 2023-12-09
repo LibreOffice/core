LibreOffice/core

commit e1473bc935b68147cb019572928e19a03f188f18

Upgrade lcms2 to 2.15
Added Windows-ARM64 support in release 2.13, so external/lcms2/lcms2-win-arm64.patch.1
and external/lcms2/lcms2_sln are not neccesary now.

Fix external/lcms2/lcms2-2.4-windows.patch.

Change-Id: I3b1ca0e715a5f500966d58971188f95a1f232883
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/157100
Tested-by: Jenkins
Reviewed-by: Taichi Haradaguchi <20001722@ymail.ne.jp>

external/lcms2/UnpackedTarball_lcms2.mk
@@ -18,10 +18,6 @@ $(eval $(call gb_UnpackedTarball_set_patchlevel,lcms2,3))
 $(eval $(call gb_UnpackedTarball_add_patches,lcms2,\
 	external/lcms2/lcms2-2.4-windows.patch \
 	external/lcms2/c++17.patch.1 \
-	external/lcms2/lcms2-win-arm64.patch.1 \
 ))
-
-# Can't include in ARM64 patch, as diff fails to detect text in it
-$(eval $(call gb_UnpackedTarball_add_file,lcms2,Projects/VC2019/lcms2.sln,external/lcms2/lcms2_sln))
 
 # vim: set noet sw=4 ts=4:

download.lst
@@ -359,8 +359,8 @@ LANGTAGREG_TARBALL := language-subtag-registry-2023-08-02.tar.bz2
 # three static lines
 # so that git cherry-pick
 # will not run into conflicts
-LCMS2_SHA256SUM := 18663985e864100455ac3e507625c438c3710354d85e5cbb7cd4043e11fe10f5
-LCMS2_TARBALL := lcms2-2.12.tar.gz
+LCMS2_SHA256SUM := b20cbcbd0f503433be2a4e81462106fa61050a35074dc24a4e356792d971ab39
+LCMS2_TARBALL := lcms2-2.15.tar.gz
 # three static lines
 # so that git cherry-pick
 # will not run into conflicts

external/lcms2/ExternalPackage_lcms2.mk
@@ -14,15 +14,11 @@ $(eval $(call gb_ExternalPackage_use_external_project,lcms2,lcms2))
 ifeq ($(DISABLE_DYNLOADING),)
 ifeq ($(OS),MACOSX)
 $(eval $(call gb_ExternalPackage_add_file,lcms2,$(LIBO_LIB_FOLDER)/liblcms2.2.dylib,src/.libs/liblcms2.2.dylib))
-else ifeq ($(OS),WNT)
-ifeq ($(COM),GCC)
-$(eval $(call gb_ExternalPackage_add_file,lcms2,$(LIBO_LIB_FOLDER)/liblcms2-2.dll,src/.libs/liblcms2-2.dll))
 else ifeq ($(COM),MSC)
 $(eval $(call gb_ExternalPackage_add_file,lcms2,$(LIBO_LIB_FOLDER)/lcms2.dll,bin/lcms2.dll))
-endif # $(COM)
-else  # $(OS) != WNT/MACOSX
-$(eval $(call gb_ExternalPackage_add_file,lcms2,$(LIBO_LIB_FOLDER)/liblcms2.so.2,src/.libs/liblcms2.so.2.0.12))
-endif # $(OS)
+else
+$(eval $(call gb_ExternalPackage_add_file,lcms2,$(LIBO_LIB_FOLDER)/liblcms2.so.2,src/.libs/liblcms2.so.2.0.15))
+endif
 endif # $(DISABLE_DYNLOADING)
 
 # vim: set noet sw=4 ts=4:

external/lcms2/README
@@ -1,3 +1,5 @@
-lcms2 (little cms engine)
-lcms2: Little cms is a small, speed optimized color management engine.
-with the new libcdr, writerperfect will depend on lcms2
+lcms2 (Little CMS color engine)
+lcms2: Little CMS is a small, speed optimized color management engine.
+With the new libcdr, writerperfect depends on lcms2.
+
+From [https://www.littlecms.com/color-engine/].

external/lcms2/lcms2-2.4-windows.patch
@@ -1,20 +1,30 @@
-TODO: Is this still needed?
+This patch is neccesary for Windows (32-bit) build.
 
---- misc/lcms2-2.4/include/lcms2.h	2011-12-15 16:45:47.000000000 +0100
-+++ misc/build/lcms2-2.4/include/lcms2.h	2012-03-17 22:53:28.731585981 +0100
-@@ -192,6 +192,15 @@ typedef int                  cmsBool;
+(In Jenkins' build message)
+> icc_transform.o : error LNK2019: unresolved external symbol _cmsGetColorSpace referenced in function "public: static class std::unique_ptr<class fxcodec::IccTransform,struct std::default_delete<class fxcodec::IccTransform> > __cdecl fxcodec::IccTransform::CreateTransformSRGB(class pdfium::span<unsigned char const >)" (?CreateTransformSRGB@IccTransform@fxcodec@@SA?AV?$unique_ptr@VIccTransform@fxcodec@@U?$default_delete@VIccTransform@fxcodec@@@std@@@std@@V?$span@$$CBE@pdfium@@@Z)
+> icc_transform.o : error LNK2019: unresolved external symbol _cmsChannelsOf referenced in function "public: static class std::unique_ptr<class fxcodec::IccTransform,struct std::default_delete<class fxcodec::IccTransform> > __cdecl fxcodec::IccTransform::CreateTransformSRGB(class pdfium::span<unsigned char const >)" (?CreateTransformSRGB@IccTransform@fxcodec@@SA?AV?$unique_ptr@VIccTransform@fxcodec@@U?$default_delete@VIccTransform@fxcodec@@@std@@@std@@V?$span@$$CBE@pdfium@@@Z)
+> icc_transform.o : error LNK2019: unresolved external symbol _cmsOpenProfileFromMem referenced in function "public: static class std::unique_ptr<class fxcodec::IccTransform,struct std::default_delete<class fxcodec::IccTransform> > __cdecl fxcodec::IccTransform::CreateTransformSRGB(class pdfium::span<unsigned char const >)" (?CreateTransformSRGB@IccTransform@fxcodec@@SA?AV?$unique_ptr@VIccTransform@fxcodec@@U?$default_delete@VIccTransform@fxcodec@@@std@@@std@@V?$span@$$CBE@pdfium@@@Z)
+> icc_transform.o : error LNK2019: unresolved external symbol _cmsCloseProfile referenced in function "public: void __thiscall fxcodec::`anonymous namespace'::CmsProfileDeleter::operator()(void *)" (??RCmsProfileDeleter@?A0xf5ac6891@fxcodec@@QAEXPAX@Z)
+> icc_transform.o : error LNK2019: unresolved external symbol _cmsCreate_sRGBProfile referenced in function "public: static class std::unique_ptr<class fxcodec::IccTransform,struct std::default_delete<class fxcodec::IccTransform> > __cdecl fxcodec::IccTransform::CreateTransformSRGB(class pdfium::span<unsigned char const >)" (?CreateTransformSRGB@IccTransform@fxcodec@@SA?AV?$unique_ptr@VIccTransform@fxcodec@@U?$default_delete@VIccTransform@fxcodec@@@std@@@std@@V?$span@$$CBE@pdfium@@@Z)
+> icc_transform.o : error LNK2019: unresolved external symbol _cmsCreateTransform referenced in function "public: static class std::unique_ptr<class fxcodec::IccTransform,struct std::default_delete<class fxcodec::IccTransform> > __cdecl fxcodec::IccTransform::CreateTransformSRGB(class pdfium::span<unsigned char const >)" (?CreateTransformSRGB@IccTransform@fxcodec@@SA?AV?$unique_ptr@VIccTransform@fxcodec@@U?$default_delete@VIccTransform@fxcodec@@@std@@@std@@V?$span@$$CBE@pdfium@@@Z)
+> icc_transform.o : error LNK2019: unresolved external symbol _cmsDeleteTransform referenced in function "public: __thiscall fxcodec::IccTransform::~IccTransform(void)" (??1IccTransform@fxcodec@@QAE@XZ)
+> icc_transform.o : error LNK2019: unresolved external symbol _cmsDoTransform referenced in function "public: void __thiscall fxcodec::IccTransform::Translate(class pdfium::span<float const >,class pdfium::span<float>)" (?Translate@IccTransform@fxcodec@@QAEXV?$span@$$CBM@pdfium@@V?$span@M@4@@Z)
+> 
+> C:\cygwin\home\tdf\lode\jenkins\workspace\gerrit_windows\instdir\program\pdfiumlo.dll : fatal error LNK1120: 8 unresolved externals
+
+--- misc/lcms2/include/lcms2.h	2023-03-01 03:53:11.000000000 +0900
++++ misc/build/lcms2/include/lcms2.h	2023-11-29 15:47:08.535240600 +0900
+@@ -226,6 +226,13 @@ typedef int                  cmsBool;
  #endif  // CMS_USE_BIG_ENDIAN
  
  
 +// LibreOffice always builds this as DLL and with the stdcall calling
 +// convention, so make this usable from outside without having to
 +// specify CMS_DLL manually whenever the library is used.
-+#ifndef CMS_DLL_BUILD
-+#ifndef CMS_DLL
++#if !defined(CMS_DLL_BUILD) && !defined(CMS_DLL) && !defined(_WIN64)
 +#define CMS_DLL
-+#endif
 +#endif
 +
  // Calling convention -- this is hardly platform and compiler dependent
- #ifdef CMS_IS_WINDOWS_
+ #if defined(CMS_IS_WINDOWS_) && !defined(__GNUC__)
  #  if defined(CMS_DLL) || defined(CMS_DLL_BUILD)

commit ebb2dff479556b0a8a855443c7bb9ff62b3f89f0

fix lcms2 build on Windows AARCH64 - missig output path option
broke with e1473bc935b68147cb019572928e19a03f188f18 - the shipped config
doesn't set the outdir for arm64 build, so the dll doesn't end up in the
expected location.
Note this only patches the 2019 config that is currently also hardcoded
in the makefile

Change-Id: I21e90cd15922bbe37b534b04362450e1f87662ef
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/160497
Tested-by: Jenkins
Reviewed-by: Taichi Haradaguchi <20001722@ymail.ne.jp>

external/lcms2/UnpackedTarball_lcms2.mk
@@ -17,6 +17,7 @@ $(eval $(call gb_UnpackedTarball_set_patchlevel,lcms2,3))
 
 $(eval $(call gb_UnpackedTarball_add_patches,lcms2,\
 	external/lcms2/lcms2-2.4-windows.patch \
+	external/lcms2/lcms2-windows_aarch64_outdir.patch.1 \
 	external/lcms2/c++17.patch.1 \
 ))
 

external/lcms2/lcms2-windows_aarch64_outdir.patch.1
@@ -1,0 +1,26 @@
+diff -ur lcms2.org/Projects/VC2019/lcms2_DLL/lcms2_DLL.vcxproj lcms2/Projects/VC2019/lcms2_DLL/lcms2_DLL.vcxproj
+--- lcms2.org/Projects/VC2019/lcms2_DLL/lcms2_DLL.vcxproj	2023-12-08 18:49:59.819483000 +0100
++++ lcms2/Projects/VC2019/lcms2_DLL/lcms2_DLL.vcxproj	2023-12-08 18:50:59.460115500 +0100
+@@ -92,6 +92,7 @@
+     <_ProjectFileVersion>10.0.21006.1</_ProjectFileVersion>
+     <OutDir Condition="'$(Configuration)|$(Platform)'=='Debug|Win32'">..\..\..\bin\</OutDir>
+     <OutDir Condition="'$(Configuration)|$(Platform)'=='Debug|x64'">..\..\..\bin\</OutDir>
++    <OutDir Condition="'$(Configuration)|$(Platform)'=='Debug|ARM64'">..\..\..\bin\</OutDir>
+     <IntDir Condition="'$(Configuration)|$(Platform)'=='Debug|Win32'">$(Configuration)_$(Platform)\</IntDir>
+     <IntDir Condition="'$(Configuration)|$(Platform)'=='Debug|ARM64'">$(Configuration)_$(Platform)\</IntDir>
+     <IntDir Condition="'$(Configuration)|$(Platform)'=='Debug|x64'">$(Configuration)_$(Platform)\</IntDir>
+@@ -100,6 +101,7 @@
+     <LinkIncremental Condition="'$(Configuration)|$(Platform)'=='Debug|x64'">true</LinkIncremental>
+     <OutDir Condition="'$(Configuration)|$(Platform)'=='Release|Win32'">..\..\..\bin\</OutDir>
+     <OutDir Condition="'$(Configuration)|$(Platform)'=='Release|x64'">..\..\..\bin\</OutDir>
++    <OutDir Condition="'$(Configuration)|$(Platform)'=='Release|ARM64'">..\..\..\bin\</OutDir>
+     <IntDir Condition="'$(Configuration)|$(Platform)'=='Release|Win32'">$(Configuration)_$(Platform)\</IntDir>
+     <IntDir Condition="'$(Configuration)|$(Platform)'=='Release|ARM64'">$(Configuration)_$(Platform)\</IntDir>
+     <IntDir Condition="'$(Configuration)|$(Platform)'=='Release|x64'">$(Configuration)_$(Platform)\</IntDir>
+@@ -340,4 +342,4 @@
+   <Import Project="$(VCTargetsPath)\Microsoft.Cpp.targets" />
+   <ImportGroup Label="ExtensionTargets">
+   </ImportGroup>
+-</Project>
+\ No newline at end of file
++</Project>