f4a6549c225a3b2b22f4071f6d0cb0b0553e0050
codewithvk <vivek.javiya@collabora.com>
2023-12-10 23:30:47 +0530

    Make format condition, chart delete and pivot table error async
    todo:
    Make executeDlg_ObjectProperties_withoutUndoGuard dialogs to async.
    
    Signed-off-by: codewithvk <vivek.javiya@collabora.com>
    Change-Id: I9927c1008d34370b9394aaf653afb575f69f2d45
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/160557
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/161703
    Tested-by: Caolán McNamara <caolan.mcnamara@collabora.com>

--- chart2/source/controller/main/ChartController_Window.cxx
+++ chart2/source/controller/main/ChartController_Window.cxx
@@ -1602,10 +1602,10 @@ bool ChartController::execute_KeyInput( const KeyEvent& rKEvt )
         bReturn = executeDispatch_Delete();
         if( ! bReturn )
         {
-            std::unique_ptr<weld::MessageDialog> xInfoBox(Application::CreateMessageDialog(pChartWindow->GetFrameWeld(),
+            std::shared_ptr<weld::MessageDialog> xInfoBox(Application::CreateMessageDialog(pChartWindow->GetFrameWeld(),
                                                           VclMessageType::Info, VclButtonsType::Ok,
                                                           SchResId(STR_ACTION_NOTPOSSIBLE)));
-            xInfoBox->run();
+            xInfoBox->runAsync(xInfoBox, [] (int) {});
         }
     }
 
