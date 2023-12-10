053c549fddff84d3f8e1d1a2962091fd5f69c662
Stephan Bergmann <stephan.bergmann@allotropia.de>
2023-12-10 22:05:04 +0100

    external/libwps: operation between different enumeration types
    > MSWrite.cpp:1424:63: error: invalid arithmetic between different enumeration types ('MSWriteParserInternal::BitmapFileHeaderOffset' and 'MSWriteParserInternal::BitmapInfoHeaderV2Offset')
    >  1424 |         unsigned offset = MSWriteParserInternal::BM_FILE_STRUCT_SIZE + MSWriteParserInternal::BM_INFO_V2_STRUCT_SIZE + colors * unsigned(sizeof(MSWriteParserInternal::BitmapPalette));
    >       |                           ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ^ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    etc. as reported now with --with-latest-c++ (i.e., in C++26 mode) by Clang 18
    trunk since
    <https://github.com/llvm/llvm-project/commit/1cbd52f791d3f088246526c0801634edb65cee31>
    "[Clang] Implement P2864R2 Remove Deprecated Arithmetic Conversion on
    Enumerations (#73105)"
    
    Change-Id: I6307978bc39e0f965144e0df3ac9a28eb817fe10
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/160552
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <stephan.bergmann@allotropia.de>

--- external/libwps/UnpackedTarball_libwps.mk
+++ external/libwps/UnpackedTarball_libwps.mk
@@ -11,13 +11,14 @@ $(eval $(call gb_UnpackedTarball_UnpackedTarball,libwps))
 
 $(eval $(call gb_UnpackedTarball_set_tarball,libwps,$(WPS_TARBALL)))
 
-$(eval $(call gb_UnpackedTarball_set_patchlevel,libwps,1))
+$(eval $(call gb_UnpackedTarball_set_patchlevel,libwps,0))
 
 $(eval $(call gb_UnpackedTarball_update_autoconf_configs,libwps))
 
 $(eval $(call gb_UnpackedTarball_add_patches,libwps,\
 	$(if $(SYSTEM_REVENGE),,external/libwps/rpath.patch.0) \
 	external/libwps/libtool.patch.0 \
+	external/libwps/enumarith.patch \
 ))
 
 ifneq ($(OS),MACOSX)
