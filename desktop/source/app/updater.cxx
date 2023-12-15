LibreOffice/core

commit 58bfd8f982950566018f77ce8895963d914058f8

Fix backward vs. forward slashes on Windows
Change-Id: I24771a526848901ccedb15aa5dd7b373fe854977
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/160765
Tested-by: Jenkins
Reviewed-by: Stephan Bergmann <stephan.bergmann@allotropia.de>

desktop/source/app/updater.cxx
@@ -86,7 +86,12 @@ const char* pSofficeExeName = "soffice.exe";
 
 OUString normalizePath(const OUString& rPath)
 {
-    OUString aPath =  rPath.replaceAll("//", "/");
+    OUString aPath =  rPath;
+#if defined WNT
+    aPath = aPath.replace('\\', '/');
+#endif
+
+    aPath.replaceAll("//", "/");
 
     // remove final /
     if (aPath.endsWith("/"))
@@ -108,7 +113,10 @@ OUString normalizePath(const OUString& rPath)
         aPath = aTempPath.copy(0, i) + aPath.copy(nIndex + 3);
     }
 
-    return aPath.replaceAll("\\", "/");
+#if defined WNT
+    aPath = aPath.replace('/', '\\');
+#endif
+    return aPath;
 }
 
 void CopyFileToDir(const OUString& rTempDirURL, const OUString & rFileName, const OUString& rOldDir)

commit 94233c2e1e86c3617f327e8bfd3cd4a980adf200

-Werror,-Wunused-result
...caused by typo in previous 58bfd8f982950566018f77ce8895963d914058f8 "Fix
backward vs. forward slashes on Windows"

Change-Id: Ibd1967688c6ab4c962818595d435c7f821ad3d64
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/160820
Tested-by: Jenkins
Reviewed-by: Stephan Bergmann <stephan.bergmann@allotropia.de>

desktop/source/app/updater.cxx
@@ -91,7 +91,7 @@ OUString normalizePath(const OUString& rPath)
     aPath = aPath.replace('\\', '/');
 #endif
 
-    aPath.replaceAll("//", "/");
+    aPath = aPath.replaceAll("//", "/");
 
     // remove final /
     if (aPath.endsWith("/"))