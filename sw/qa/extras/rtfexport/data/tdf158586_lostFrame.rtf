LibreOffice/core

commit 4e2f2489c4c7436f8b3a21a530bc625cbef4e365

tdf#153178 writerfilter: do not create text frame spuriously
There are different RTF keywords that end up in RTFFrame, and some of
them cause Word to create a frame, while others don't.

Getting this right requires implementing support for all the missing
wrapping keywords, as most of them create a frame in Word but
\wrapdefault does not.

Change-Id: I1e55d15180564f3d66a5ee7d98274ae18f032872
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/159228
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

writerfilter/source/rtftok/rtfdocumentimpl.cxx
@@ -3995,9 +3995,9 @@ RTFSprms RTFFrame::getSprms()
 
 bool RTFFrame::hasProperties() const
 {
-    return m_nX != 0 || m_nY != 0 || m_nW != 0 || m_nH != 0 || m_nHoriPadding != 0
-           || m_nVertPadding != 0 || m_nHoriAlign != 0 || m_nHoriAnchor != 0 || m_nVertAlign != 0
-           || m_nVertAnchor != 0;
+    // tdf#153178 \dxfrtext \dfrmtxtx \dfrmtxty \wrapdefault do *not* create frame
+    return m_nX != 0 || m_nY != 0 || m_nW != 0 || m_nH != 0
+           || (m_oWrap && *m_oWrap != NS_ooxml::LN_Value_doc_ST_Wrap_auto);
 }
 
 } // namespace writerfilter

sw/qa/extras/rtfexport/data/tdf153178.rtf
@@ -1,0 +1,7 @@
+{\rtf1
+\tqr\tlul\tx2025\dxfrtext142\dfrmtxtx142\dfrmtxty142\wrapdefault\faauto\adjustright\rin0\lin57\itap0 \rtlch\fcs1 \afs26\alang1025 \ltrch\fcs0 \fs26\lang1031\langfe3079\cgrid\langnp1031\langfenp3079
+KKKKKKKK
+\par
+\pard
+\par
+}

sw/qa/extras/rtfexport/rtfexport7.cxx
@@ -22,6 +22,7 @@
 #include <com/sun/star/text/HoriOrientation.hpp>
 #include <com/sun/star/text/XTextTable.hpp>
 #include <com/sun/star/text/XTextFieldsSupplier.hpp>
+#include <com/sun/star/text/XTextFramesSupplier.hpp>
 #include <com/sun/star/text/XTextTablesSupplier.hpp>
 #include <com/sun/star/text/XBookmarksSupplier.hpp>
 
@@ -657,6 +658,15 @@ DECLARE_RTFEXPORT_TEST(testWatermark, "watermark.rtf")
     // Check font size
     CPPUNIT_ASSERT(xPropertySet->getPropertyValue("CharHeight") >>= nFontSize);
     CPPUNIT_ASSERT_EQUAL(float(66), nFontSize);
+}
+
+DECLARE_RTFEXPORT_TEST(testTdf153178, "tdf153178.rtf")
+{
+    // the problem was that a frame was created
+    uno::Reference<text::XTextFramesSupplier> xTextFramesSupplier(mxComponent, uno::UNO_QUERY);
+    uno::Reference<container::XIndexAccess> xIndexAccess(xTextFramesSupplier->getTextFrames(),
+                                                         uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(0), xIndexAccess->getCount());
 }
 
 DECLARE_RTFEXPORT_TEST(testTdf109790, "tdf109790.rtf")

writerfilter/source/rtftok/rtfdispatchflag.cxx
@@ -1242,6 +1242,26 @@ RTFError RTFDocumentImpl::dispatchFlag(RTFKeyword nKeyword)
             m_aStates.top().getFrame().setSprm(NS_ooxml::LN_CT_FramePr_wrap,
                                                NS_ooxml::LN_Value_doc_ST_Wrap_notBeside);
             break;
+        case RTFKeyword::OVERLAY:
+            m_aStates.top().getFrame().setSprm(NS_ooxml::LN_CT_FramePr_wrap,
+                                               NS_ooxml::LN_Value_doc_ST_Wrap_none);
+            break;
+        case RTFKeyword::WRAPAROUND:
+            m_aStates.top().getFrame().setSprm(NS_ooxml::LN_CT_FramePr_wrap,
+                                               NS_ooxml::LN_Value_doc_ST_Wrap_around);
+            break;
+        case RTFKeyword::WRAPTHROUGH:
+            m_aStates.top().getFrame().setSprm(NS_ooxml::LN_CT_FramePr_wrap,
+                                               NS_ooxml::LN_Value_doc_ST_Wrap_through);
+            break;
+        case RTFKeyword::WRAPTIGHT:
+            m_aStates.top().getFrame().setSprm(NS_ooxml::LN_CT_FramePr_wrap,
+                                               NS_ooxml::LN_Value_doc_ST_Wrap_tight);
+            break;
+        case RTFKeyword::WRAPDEFAULT:
+            m_aStates.top().getFrame().setSprm(NS_ooxml::LN_CT_FramePr_wrap,
+                                               NS_ooxml::LN_Value_doc_ST_Wrap_auto);
+            break;
         case RTFKeyword::MNOR:
             m_bMathNor = true;
             break;

commit cf58950b281c85c366e0948557151a0fe104ed81

partial revert tdf#153178 writerfilter: do not create text frame spuriously2
If HoriAlign/VertAlign are defined, that is enough to create a frame.

This fixes mstahl's 7.6.4 regression originating with
commit 4e2f2489c4c7436f8b3a21a530bc625cbef4e365

make CppunitTest_sw_rtfexport8 CPPUNIT_TEST_NAME=testTdf158586_lostFrame

Change-Id: Ibb639673cebcd8d5ec79f5551a5703caf948e0d8
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/160744
Reviewed-by: Justin Luth <jluth@mail.com>
Tested-by: Jenkins
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

writerfilter/source/rtftok/rtfdocumentimpl.cxx
@@ -4010,7 +4010,9 @@ bool RTFFrame::hasProperties() const
 {
     // tdf#153178 \dxfrtext \dfrmtxtx \dfrmtxty \wrapdefault do *not* create frame
     return m_nX != 0 || m_nY != 0 || m_nW != 0 || m_nH != 0
+           || (m_nHoriAlign && m_nHoriAlign != NS_ooxml::LN_Value_doc_ST_XAlign_left)
            || (m_nHoriAnchor && m_nHoriAnchor != NS_ooxml::LN_Value_doc_ST_HAnchor_text)
+           || (m_nVertAlign && m_nVertAlign != NS_ooxml::LN_Value_doc_ST_YAlign_inline)
            || (m_nVertAnchor && m_nVertAnchor != NS_ooxml::LN_Value_doc_ST_VAnchor_margin)
            || (m_oWrap && *m_oWrap != NS_ooxml::LN_Value_doc_ST_Wrap_auto);
 }

sw/qa/extras/rtfexport/data/tdf158586_lostFrame.rtf
@@ -1,0 +1,17 @@
+{\rtf1
+
+\paperw8419\paperh5953
+
+\spltpgpar
+
+\ltrpar \sectd
+
+\pard\plain \posxc\posyc First page textbox \par
+\pard 1st page
+
+
+\page \sect \sectd \sbknone
+
+\pard\plain 2nd page
+\par
+}

sw/qa/extras/rtfexport/rtfexport8.cxx
@@ -73,6 +73,17 @@ DECLARE_RTFEXPORT_TEST(testTdf158586_1, "tdf158586_pageBreak1.rtf")
     // assertXPathContent(pLayout, "//page[2]/body/txt"_ostr, "Second page");
 }
 
+DECLARE_RTFEXPORT_TEST(testTdf158586_lostFrame, "tdf158586_lostFrame.rtf")
+{
+    // The anchor and align properties are sufficient to define a frame
+    const auto& pLayout = parseLayoutDump();
+    assertXPath(pLayout, "//anchored"_ostr, 1);
+    assertXPathContent(pLayout, "//page[1]/body//txt"_ostr, "1st page");
+    // assertXPathContent(pLayout, "//page[2]/body//txt"_ostr, "2nd page");
+
+    // CPPUNIT_ASSERT_EQUAL(2, getPages());
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */