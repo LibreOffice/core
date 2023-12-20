LibreOffice/core

commit 91f35f22f0447769c08ca89e27a39b40df18fffa

tdf#105844 package: remove root document from manifest ...
... for ODF wholesome encryption.

  4.3  <manifest:file-entry>
  For directories, the manifest file should contain a <manifest:file-entry> element only if a directory contains a document or a sub document.

Because the "encrypted-package" is not a document but a package, we
should probably omit the file-entry for the root document.

ZipPackage::writeTempFile() always generates the root document becuase
it's needed for GPG properties, and ManifestExport filters it out.

A bit tricky to implement, because there isn't a clean distinction
between the package and the root document/storage in the package module,
in particular there's no other place than the root storage to store the
MediaType property.

Change-Id: Id7e72a64e2faa074dce80cd5fefb2fa189e2e3ee
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/160717
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

package/source/manifest/ManifestExport.cxx
@@ -81,6 +81,7 @@ ManifestExport::ManifestExport( uno::Reference< xml::sax::XDocumentHandler > con
     // find the mediatype of the document if any
     OUString aDocMediaType;
     OUString aDocVersion;
+    bool isWholesomeEncryption(false);
     const uno::Sequence<beans::PropertyValue>* pRootFolderPropSeq = nullptr;
     for (const uno::Sequence < beans::PropertyValue >& rSequence : rManList)
     {
@@ -109,12 +110,27 @@ ManifestExport::ManifestExport( uno::Reference< xml::sax::XDocumentHandler > con
 
         if ( aPath == "/" )
         {
+            assert(aDocMediaType.isEmpty());
+            // unfortunately no aMediaType in some cases where non-documents
+            // are stored as StorageFormats::PACKAGE instead of sensible
+            // StorageFormats::ZIP, such as SvxXMLXTableExportComponent and
+            // SwXMLTextBlocks, which results in an empty "mimetype" etc but
+            // can't be easily fixed; try to exclude these cases by checking
+            // for aVersion, but of course then forgetting to set both version
+            // and type on an actual document can't be found :(
+            assert(!aMediaType.isEmpty() || aVersion.isEmpty());
             aDocMediaType = aMediaType;
             aDocVersion = aVersion;
             pRootFolderPropSeq = &rSequence;
-            break;
+        }
+
+        if (aPath == "encrypted-package")
+        {
+            isWholesomeEncryption = true;
+            assert(aDocMediaType.isEmpty() || aDocMediaType == aMediaType);
         }
     }
+    assert(pRootFolderPropSeq);
 
     bool bProvideDTD = false;
     bool bAcceptNonemptyVersion = false;
@@ -293,7 +309,12 @@ ManifestExport::ManifestExport( uno::Reference< xml::sax::XDocumentHandler > con
     // now write individual file entries
     for (const uno::Sequence<beans::PropertyValue>& rSequence : rManList)
     {
+        if (&rSequence == pRootFolderPropSeq && isWholesomeEncryption)
+        {
+            continue; // no root document, but embedded package => omit
+        }
         rtl::Reference<::comphelper::AttributeList> pAttrList = new ::comphelper::AttributeList;
+        OUString fullPath;
         OUString aString;
         const uno::Any *pVector = nullptr, *pSalt = nullptr, *pIterationCount = nullptr, *pDigest = nullptr, *pDigestAlg = nullptr, *pEncryptAlg = nullptr, *pStartKeyAlg = nullptr, *pDerivedKeySize = nullptr;
         for (const beans::PropertyValue& rValue : rSequence)
@@ -312,8 +333,8 @@ ManifestExport::ManifestExport( uno::Reference< xml::sax::XDocumentHandler > con
             }
             else if (rValue.Name == sFullPathProperty )
             {
-                rValue.Value >>= aString;
-                pAttrList->AddAttribute ( ATTRIBUTE_FULL_PATH, aString );
+                rValue.Value >>= fullPath;
+                pAttrList->AddAttribute(ATTRIBUTE_FULL_PATH, fullPath);
             }
             else if (rValue.Name == sSizeProperty )
             {
@@ -337,6 +358,11 @@ ManifestExport::ManifestExport( uno::Reference< xml::sax::XDocumentHandler > con
                 pStartKeyAlg = &rValue.Value;
             else if (rValue.Name == sDerivedKeySizeProperty )
                 pDerivedKeySize = &rValue.Value;
+        }
+        assert(!fullPath.isEmpty());
+        if (isWholesomeEncryption)
+        {   // there may be signatures in META-INF too
+            assert(fullPath == "encrypted-package" || fullPath.startsWith("META-INF/"));
         }
 
         xHandler->ignorableWhitespace ( sWhiteSpace );
@@ -390,6 +416,7 @@ ManifestExport::ManifestExport( uno::Reference< xml::sax::XDocumentHandler > con
             }
             else if (nEncAlgID == xml::crypto::CipherID::AES_GCM_W3C)
             {
+                assert(bStoreStartKeyGeneration || pKeyInfoProperty);
                 SAL_WARN_IF(nDerivedKeySize != 32, "package.manifest", "Unexpected key size is provided!");
                 if (nDerivedKeySize != 32)
                 {

package/source/zipapi/ZipFile.cxx
@@ -647,7 +647,8 @@ uno::Reference< XInputStream > ZipFile::createStreamForZipEntry(
 #ifndef EMSCRIPTEN
     static const sal_Int32 nThreadingThreshold = 10000;
 
-    if( xSrcStream->available() > nThreadingThreshold )
+    // "encrypted-package" is the only data stream, no point in threading it
+    if (rEntry.sPath != "encrypted-package" && nThreadingThreshold < xSrcStream->available())
         xBufStream = new XBufferedThreadedStream(xSrcStream, xSrcStream->getSize());
     else
 #endif

package/source/zippackage/ZipPackage.cxx
@@ -417,25 +417,36 @@ void ZipPackage::parseManifest()
             }
         }
 
-        if ( !bManifestParsed )
+        if (!bManifestParsed || m_xRootFolder->GetMediaType().isEmpty())
         {
             // the manifest.xml could not be successfully parsed, this is an inconsistent package
             if ( aPackageMediatype.startsWith("application/vnd.") )
             {
                 // accept only types that look similar to own mediatypes
                 m_xRootFolder->SetMediaType( aPackageMediatype );
-                m_bMediaTypeFallbackUsed = true;
+                // if there is an encrypted inner package, there is no root
+                // document, because instead there is a package, and it is not
+                // an error
+                if (!m_xRootFolder->hasByName("encrypted-package"))
+                {
+                    m_bMediaTypeFallbackUsed = true;
+                }
             }
         }
         else if ( !m_bForceRecovery )
         {
-            // the mimetype stream should contain the information from manifest.xml
-            if ( m_xRootFolder->GetMediaType() != aPackageMediatype )
+            // the mimetype stream should contain the same information as manifest.xml
+            OUString const mediaTypeXML(m_xRootFolder->hasByName("encrypted-package")
+                ? m_xRootFolder->doGetByName("encrypted-package").xPackageEntry->GetMediaType()
+                : m_xRootFolder->GetMediaType());
+            if (mediaTypeXML != aPackageMediatype)
+            {
                 throw ZipIOException(
                     THROW_WHERE
                     "mimetype conflicts with manifest.xml, \""
-                    + m_xRootFolder->GetMediaType() + "\" vs. \""
+                    + mediaTypeXML + "\" vs. \""
                     + aPackageMediatype + "\"" );
+            }
         }
 
         m_xRootFolder->removeByName( sMimetype );
@@ -1269,6 +1280,8 @@ uno::Reference< io::XInputStream > ZipPackage::writeTempFile()
         static constexpr OUStringLiteral sFullPath(u"FullPath");
         const bool bIsGpgEncrypt = m_aGpgProps.hasElements();
 
+        // note: this is always created here (needed for GPG), possibly
+        // filtered out later in ManifestExport
         if ( m_nFormat == embed::StorageFormats::PACKAGE )
         {
             uno::Sequence < PropertyValue > aPropSeq(

svx/source/xml/xmlxtexp.cxx
@@ -227,7 +227,13 @@ bool SvxXMLXTableExportComponent::save(
         if( !bToStorage || !xStorage.is() )
         { // local URL -> SfxMedium route
             if( bSaveAsStorage )
+            {
+                // ideally this should use a ZIP_STORAGE_FORMAT_STRING storage
+                // but changing it to that could cause problems loading the
+                // file with an old version of LO that expects to find in the
+                // user profile a PACKAGE_STORAGE_FORMAT_STRING storage
                 xSubStorage = ::comphelper::OStorageHelper::GetStorageFromURL( rURL, eCreate );
+            }
             else
             {
                 pMedium.reset(new SfxMedium( rURL, StreamMode::WRITE | StreamMode::TRUNC ));

commit 2c0f520c03941f6ec09c8a52bd2b2bd38ba2b9a4

package: ODF encryption: fix 2 GPG problems
1. ManifestImport should attach KeyInfo to the first file-entry, not to
   a "/" entry which may not exist or may be preceded by another
   file-entry which requires the KeyInfo to decrypt
   (regression from commit 91f35f22f0447769c08ca89e27a39b40df18fffa)

2. manifest:key-size on manifest:key-derivation is unfortunately
   specified to have a default value of 16, but with AES256 the actual
   size is 32, and for GPG the ManifestExport forgot to export it

Change-Id: I05dd2c9e219ba7f20ba33bf0b35d47a9e3df74cc
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/161072
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

package/inc/PackageConstants.hxx
@@ -47,9 +47,10 @@ const sal_Int32 n_ConstDigestDecrypt = 1056; // 1024 + 32
 #define PKG_MNFST_DERKEYSIZE 11 //DerivedKeySize
 #define PKG_MNFST_KDF 12 // KeyDerivationFunction
 #define PKG_MNFST_ARGON2ARGS 13 // Argon2 arguments
+#define PKG_MNFST_KEYINFO 14 // PGP KeyInfo
 
 #define PKG_SIZE_NOENCR_MNFST 3
-#define PKG_SIZE_ENCR_MNFST 14 // max size
+#define PKG_SIZE_ENCR_MNFST 15 // max size
 
 // the properties related constants
 inline constexpr OUString ENCRYPTION_KEY_PROPERTY = u"EncryptionKey"_ustr;

package/source/manifest/ManifestExport.cxx
@@ -527,15 +527,16 @@ ManifestExport::ManifestExport( uno::Reference< xml::sax::XDocumentHandler > con
                     pNewAttrList->AddAttribute(ATTRIBUTE_ITERATION_COUNT, aBuffer.makeStringAndClear());
                 }
 
-                if (bStoreStartKeyGeneration)
-                {
-                    aBuffer.append(nDerivedKeySize);
-                    pNewAttrList->AddAttribute ( ATTRIBUTE_KEY_SIZE, aBuffer.makeStringAndClear() );
-                }
-
                 *pSalt >>= aSequence;
                 ::comphelper::Base64::encode(aBuffer, aSequence);
                 pNewAttrList->AddAttribute ( ATTRIBUTE_SALT, aBuffer.makeStringAndClear() );
+            }
+
+            // ODF 1.3 specifies the default as 16 so have to write it for PGP
+            if (bStoreStartKeyGeneration || pKeyInfoProperty)
+            {
+                aBuffer.append(nDerivedKeySize);
+                pNewAttrList->AddAttribute(ATTRIBUTE_KEY_SIZE, aBuffer.makeStringAndClear());
             }
 
             xHandler->ignorableWhitespace(sWhiteSpace);

package/source/manifest/ManifestImport.cxx
@@ -445,11 +445,11 @@ void SAL_CALL ManifestImport::endElement( const OUString& aName )
         return;
 
     if ( aConvertedName == ELEMENT_FILE_ENTRY && aStack.back().m_bValid ) {
-        // root folder gets KeyInfo entry if any, for PGP encryption
-        if (!bIgnoreEncryptData && !aKeys.empty() && aSequence[PKG_MNFST_FULLPATH].Value.get<OUString>() == "/" )
+        // the first entry gets KeyInfo element if any, for PGP encryption
+        if (!bIgnoreEncryptData && !aKeys.empty() && rManVector.empty())
         {
-            aSequence[PKG_SIZE_NOENCR_MNFST].Name = "KeyInfo";
-            aSequence[PKG_SIZE_NOENCR_MNFST].Value <<= comphelper::containerToSequence(aKeys);
+            aSequence[PKG_MNFST_KEYINFO].Name = "KeyInfo";
+            aSequence[PKG_MNFST_KEYINFO].Value <<= comphelper::containerToSequence(aKeys);
         }
         std::erase_if(aSequence, isEmpty);
 