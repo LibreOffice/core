12941b0338df32c739b9fdb55c71210f3d1a779a
Caolán McNamara <caolan.mcnamara@collabora.com>
2024-01-02 12:31:01 +0000

    ofz#65480: Integer-overflow
    Change-Id: Ie771e6e37237907698e4c39eb50cd940500b86ca
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/161540
    Tested-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    (cherry picked from commit 69181dd430543db21fc0e61b70033ef484708c1c)

--- package/source/zipapi/ZipFile.cxx
+++ package/source/zipapi/ZipFile.cxx
@@ -980,8 +980,10 @@ sal_Int32 ZipFile::readCEN()
             aEntry.nSize = nSize;
             aEntry.nOffset = nOffset;
 
-            aEntry.nOffset += nLocPos;
-            aEntry.nOffset *= -1;
+            if (o3tl::checked_add<sal_Int64>(aEntry.nOffset, nLocPos, aEntry.nOffset))
+                throw ZipException("Integer-overflow");
+            if (o3tl::checked_multiply<sal_Int64>(aEntry.nOffset, -1, aEntry.nOffset))
+                throw ZipException("Integer-overflow");
 
             if ( aEntry.nPathLen < 0 )
                 throw ZipException("unexpected name length" );
