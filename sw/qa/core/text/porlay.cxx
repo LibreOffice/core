ae974a9928aa435316f15626642d41c5dddafe99
Miklos Vajna <vmiklos@collabora.com>
2024-01-03 16:52:18 +0100

    tdf#158999 sw: fix bad zero text frame height with no text and auto superscript
    The problem was that the bugdoc had an empty paragraph with superscript
    (as char format) enabled and that was simply lost in the layout. Adding
    text to the paragraph or removing the superscript formatting worked
    around the problem.
    
    What happened is that SwSubFont::GetHeight() has a somewhat surprising
    explicit check for escapement, and there the CalcEscHeight() call might
    be invoked on an SwSubFont whose m_nOrgHeight is 0, while the intent is
    to just return the original height in case of automatic subscript or
    superscript.
    
    Fix the problem by checking the result of GetDefaultAscentAndHeight() in
    SwLineLayout::CalcLine(), and in case it would simply turn the line into
    an invisible one for an empty line with sub/subscript then go back to
    the already correctly calculated line height.
    
    This needed adjusting a test in CppunitTest_sw_ooxmlexport12 because the
    bugdoc there had such a paragraph, effectively testing if we lose
    content during layout, which is not wanted.
    
    Change-Id: I1e836531851ea5c9a983ae69da5be8a042cc6e39
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/161588
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Tested-by: Jenkins

---
+++ sw/qa/core/text/porlay.cxx
@@ -0,0 +1,60 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <swmodeltestbase.hxx>
+
+#include <memory>
+
+#include <IDocumentLayoutAccess.hxx>
+#include <doc.hxx>
+#include <frame.hxx>
+#include <pagefrm.hxx>
+#include <rootfrm.hxx>
+#include <txtfrm.hxx>
+
+namespace
+{
+/// Covers sw/source/core/text/porlay.cxx fixes.
+class Test : public SwModelTestBase
+{
+public:
+    Test()
+        : SwModelTestBase("/sw/qa/core/text/data/")
+    {
+    }
+};
+
+CPPUNIT_TEST_FIXTURE(Test, testEmptySuperscript)
+{
+    // Given a document with 4 paragraphs, the 2nd is empty and is marked as superscript:
+    createSwDoc("empty-superscript.docx");
+
+    // When laying out that document:
+    calcLayout();
+
+    // Then make sure all 4 text frames have visible heights:
+    SwDoc* pDoc = getSwDoc();
+    SwRootFrame* pLayout = pDoc->getIDocumentLayoutAccess().GetCurrentLayout();
+    auto pPage = pLayout->GetLower()->DynCastPageFrame();
+    auto pText1 = pPage->FindFirstBodyContent()->DynCastTextFrame();
+    CPPUNIT_ASSERT_GREATER(static_cast<SwTwips>(0), pText1->getFrameArea().Height());
+    auto pText2 = pText1->GetNext()->DynCastTextFrame();
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected greater than: 0
+    // - Actual  : 0
+    // i.e. the 2nd paragraph was not visible.
+    CPPUNIT_ASSERT_GREATER(static_cast<SwTwips>(0), pText2->getFrameArea().Height());
+    auto pText3 = pText2->GetNext()->DynCastTextFrame();
+    CPPUNIT_ASSERT_GREATER(static_cast<SwTwips>(0), pText3->getFrameArea().Height());
+    auto pText4 = pText3->GetNext()->DynCastTextFrame();
+    CPPUNIT_ASSERT_GREATER(static_cast<SwTwips>(0), pText4->getFrameArea().Height());
+}
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
