9c50de48b4c369b5fb91850253e63b7daa30adc7
Miklos Vajna <vmiklos@collabora.com>
2024-01-03 16:52:18 +0100

    tdf#158999 sw: fix bad zero text frame height with no text and auto superscript
    The problem was that the bugdoc had an empty paragraph with superscript
    (as char format) enabled and that was simply lost in the layout. Adding
    text to the paragraph or removing the superscript formatting worked
    around the problem.
    
    What happened is that SwSubFont::GetHeight() has a somewhat surprising
    explicit check for escapement, and there the CalcEscHeight() call might
    be invoked on an SwSubFont whose m_nOrgHeight is 0, while the intent is
    to just return the original height in case of automatic subscript or
    superscript.
    
    Fix the problem by checking the result of GetDefaultAscentAndHeight() in
    SwLineLayout::CalcLine(), and in case it would simply turn the line into
    an invisible one for an empty line with sub/subscript then go back to
    the already correctly calculated line height.
    
    This needed adjusting a test in CppunitTest_sw_ooxmlexport12 because the
    bugdoc there had such a paragraph, effectively testing if we lose
    content during layout, which is not wanted.
    
    Change-Id: I1e836531851ea5c9a983ae69da5be8a042cc6e39
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/161588
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Tested-by: Jenkins
    (cherry picked from commit ae974a9928aa435316f15626642d41c5dddafe99)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/161756

--- sw/CppunitTest_sw_core_text.mk
+++ sw/CppunitTest_sw_core_text.mk
@@ -17,6 +17,7 @@ $(eval $(call gb_CppunitTest_add_exception_objects,sw_core_text, \
     sw/qa/core/text/frmform \
     sw/qa/core/text/itratr \
     sw/qa/core/text/itrform2 \
+    sw/qa/core/text/porlay \
     sw/qa/core/text/porrst \
     sw/qa/core/text/text \
 ))
