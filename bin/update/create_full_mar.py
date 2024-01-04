b235e5e2f0933a013c6177f7e31ae82b45e9e048
Stephan Bergmann <stephan.bergmann@allotropia.de>
2024-01-04 12:00:27 +0100

    Fix `make create-update-info` (for Windows, at least)
    * The shell scripts that had once been copied from Mozilla to bin/update/*.sh
      are now included in
      onlineupdate-c003be8b9727672e7d30972983b375f4c200233f-2.tar.xz (which is still
      generated from the same
      <https://github.com/mozilla/gecko-dev/commit/c003be8b9727672e7d30972983b375f4c200233f>
      that was used for the original tarball in
      3a445cb49795fabe0d8caaf12bfc38eb9e12d5fc "Turn onlineupdate into
      external/onlineupdate").
    
    * The additional modifications in external/onlineupdate/lo.patch are:
    
    ** Allowing to pass the list of files into the mar tool via -f instead of on the
       command line, to avoid "command line too long" errors on Windows, inspired by
       the modifications once made directly in our old downstream sources with
       4165dd4e465a86ba6abe9afb3abfda5ef72493ea "add a way to create mar file from
       file", 8e4d49340bd235a7db8fde1d24dd1d63ddc4d571 "use the new file based
       approach for the mar creation", and fb13ed6955cd66017e5348b915af371a184ea633
       "add the manifest file to the mar file".  (To keep things simple for now, it
       still uses a hard-coded maximum of 10000 lines in the file, marked with a
       TODO.)
    
    ** Not failing when "precomplete file is missing!"  (There is a comment
    
        // Applications aren't required to have a precomplete manifest. The mar
        // generation scripts enforce the presence of a precomplete manifest.
    
        in
        workdir/UnpackedTarball/onlineupdate/onlineupdate/source/update/updater/updater.cpp
        and it appears to be OK that we don't have such a  precomplete manifest file
        and just skip that test.)
    
    * In the Makefile.gbuild create-update-info, the create_full_mar.py script needs
      to be called with a Unix pathname on Windows, or else the
    
      #!/usr/bin/env python3
    
      shebang in that script would get confused.
    
    * The related Makefile.gbuild targets upload-update-info and create-partial-info
      have not been addressed yet.
    
    Change-Id: Iab4e083ddbe99e07d846e202f20c6031e2983e1b
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/161656
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <stephan.bergmann@allotropia.de>
    (cherry picked from commit c9973227dae088a5fb04cee12b2c99e0f62d6e28)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/161649

--- bin/update/create_full_mar.py
+++ bin/update/create_full_mar.py
@@ -10,8 +10,6 @@ from tools import uncompress_file_to_dir, get_file_info, make_complete_mar_name
 from signing import sign_mar_file
 from path import UpdaterPath, convert_to_unix, convert_to_native
 
-current_dir_path = os.path.dirname(os.path.realpath(convert_to_unix(__file__)))
-
 
 def main():
     parser = argparse.ArgumentParser()
@@ -21,6 +19,7 @@ def main():
     parser.add_argument('certificate_path')
     parser.add_argument('certificate_name')
     parser.add_argument('base_url')
+    parser.add_argument('version')
     args = parser.parse_args()
 
     certificate_path = args.certificate_path
@@ -29,6 +28,7 @@ def main():
     filename_prefix = args.filename_prefix
     workdir = args.workdir
     product_name = args.product_name
+    version = args.version
 
     update_path = UpdaterPath(workdir)
     update_path.ensure_dir_exist()
@@ -42,7 +42,10 @@ def main():
     uncompress_dir = uncompress_file_to_dir(tar_file, temp_dir)
 
     mar_file = make_complete_mar_name(target_dir, filename_prefix)
-    path = os.path.join(current_dir_path, 'make_full_update.sh')
+    path = os.path.join(
+        workdir, 'UnpackedTarball/onlineupdate/tools/update-packaging/make_full_update.sh')
+    os.putenv('MOZ_PRODUCT_VERSION', version)
+    os.putenv('MAR_CHANNEL_ID', 'LOOnlineUpdater')
     subprocess.call([path, convert_to_native(mar_file), convert_to_native(uncompress_dir)])
 
     sign_mar_file(target_dir, certificate_path, certificate_name, mar_file, filename_prefix)
