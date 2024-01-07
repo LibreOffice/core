3d8ab97760049c64b7bbadde1a0b89ef9acde690
Caolán McNamara <caolan.mcnamara@collabora.com>
2024-01-07 13:37:57 +0000

    Resolves: tdf#159046 hang on export of math objects in cells
    regression from:
    
    commit a9d6b6ef049dd41c91a30c03df0ba38ba8dcada8
    Date:   Thu Oct 5 14:12:51 2023 +0100
    
        crash copying text + chart to clipboard with InputOptions::TextWysiwyg
    
        of true, the mxPoolHelper is still null when trying to create a
        Printer to use as a reference device
    
    revert that, and use a different simpler workaround for the original
    crash case.
    
    Change-Id: I74cec710aec033e1652e637ed056dae1c261aa98
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/161740
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>

--- sc/inc/document.hxx
+++ sc/inc/document.hxx
@@ -2712,8 +2712,6 @@ public:
     const ScTable* FetchTable( SCTAB nTab ) const;
 
     ScRefCellValue GetRefCellValue( const ScAddress& rPos );
-
-    void SharePooledResources( const ScDocument* pSrcDoc );
 private:
     ScRefCellValue GetRefCellValue( const ScAddress& rPos, sc::ColumnBlockPosition& rBlockPos );
 
@@ -2722,6 +2720,8 @@ private:
     SCSIZE GetPatternCount( SCTAB nTab, SCCOL nCol ) const;
     SCSIZE GetPatternCount( SCTAB nTab, SCCOL nCol, SCROW nRow1, SCROW nRow2 ) const;
     void   ReservePatternCount( SCTAB nTab, SCCOL nCol, SCSIZE nReserve );
+
+    void SharePooledResources( const ScDocument* pSrcDoc );
 
     void EndListeningIntersectedGroup(
         sc::EndListeningContext& rCxt, const ScAddress& rPos, std::vector<ScAddress>* pGroupPos );
