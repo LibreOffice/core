acd3f201a93ddac406a1e86f8b055fdbd2a64400
Attila Szűcs <attila.szucs@collabora.com>
2024-01-09 17:45:19 +0100

    tdf#154248 Impress: fix color of hyperlink
    Added a new FindAttrib method that searches in the attribs
    a bit different.
    The original FindAttrib searches in attribs as if their position
    intervals are closed from both side [Start,End].
    However, the actual attribs array was created using PaMs as positions,
    and these are right-opened intervals [Start,End)
    
    Change-Id: I9a46b6b27ce447366fc20af1b46fd60b5c745359
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/161836
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/162158
    Tested-by: Caolán McNamara <caolan.mcnamara@collabora.com>

--- editeng/inc/editattr.hxx
+++ editeng/inc/editattr.hxx
@@ -105,6 +105,8 @@ public:
 
     bool    IsIn( sal_Int32 nIndex ) const
                 { return ( ( nStart <= nIndex ) && ( nEnd >= nIndex ) ); }
+    bool    IsInLeftClosedRightOpen( sal_Int32 nIndex ) const
+                { return ( ( nStart <= nIndex ) && ( nEnd > nIndex ) ); }
     bool    IsInside( sal_Int32 nIndex ) const
                 { return ( ( nStart < nIndex ) && ( nEnd > nIndex ) ); }
     bool        IsEmpty() const
