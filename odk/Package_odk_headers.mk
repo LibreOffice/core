f41c106764cfed80c31d13a49ad879491eaad738
Hossein <hossein@libreoffice.org>
2024-01-09 10:55:01 +0000

    tdf#149256 Ship LibreOfficeKit headers in packages
    With LibreOfficeKit (LOK) headers in place, it would be possible to
    create C++ applications that can access LibreOffice functionality
    without building LibreOffice, only by installing SDK and build tools.
    
    Change-Id: I8efc8ac1cec7b3b89e4d7eb2c8d15afeb595672c
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/134863
    Tested-by: Hossein <hossein@libreoffice.org>
    Tested-by: Jenkins
    Reviewed-by: Hossein <hossein@libreoffice.org>
    (cherry picked from commit 1305edfbc27121d7fc8973ccafb92f76d5401ac1)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/170548
    Reviewed-by: Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>

--- odk/Package_odk_headers.mk
+++ odk/Package_odk_headers.mk
@@ -185,6 +185,11 @@ $(eval $(call gb_Package_add_files_with_dir,odk_headers,$(SDKDIRNAME)/include,\
 	uno/mapping.hxx \
 	uno/sequence2.h \
 	uno/threadpool.h \
+	LibreOfficeKit/LibreOfficeKit.hxx \
+	LibreOfficeKit/LibreOfficeKitEnums.h \
+	LibreOfficeKit/LibreOfficeKit.h \
+	LibreOfficeKit/LibreOfficeKitInit.h \
+	LibreOfficeKit/LibreOfficeKitTypes.h \
 ))
 
 # vim: set noet sw=4 ts=4:
