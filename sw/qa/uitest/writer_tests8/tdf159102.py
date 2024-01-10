c3025432d84bfb913c75f1aeefa66497f2d33bde
László Németh <nemeth@numbertext.org>
2024-01-10 20:41:04 +0100

    tdf#159102 sw smart justify: fix automatic hyphenation
    As before with soft hyphens, automatic hyphenation
    could result too much shrinking, because of calculating
    with an extra non-existing space in the line.
    
    Also try to shrink the line only if a space likely
    will be available in it.
    
    During testing, extend user dictionary temporarily
    with custom automatic hyphenation to avoid of false tests
    because of non-available hyphenation patterns. (Note: maybe
    the other tests with non-user dictionary based automatic
    hyphenations are not correct.)
    
    Follow-up to commit d511367c102ef2ada0f73dbe81744d39865d58ba
    "tdf#195085 sw smart justify: fix bad shrinking at soft hyphen".
    
    Change-Id: I58ecb8f1ea9f55ef2457d7ff4aca6aefa59a6dd1
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/162199
    Tested-by: Jenkins
    Reviewed-by: László Németh <nemeth@numbertext.org>
    (cherry picked from commit 7c1f4dd740c32050480f3ab678805ad3c4c748bb)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/162219
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

---
+++ sw/qa/uitest/writer_tests8/tdf159102.py
@@ -0,0 +1,81 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+from uitest.framework import UITestCase
+from uitest.uihelper.common import get_state_as_dict, get_url_for_data_file
+from libreoffice.uno.propertyvalue import mkPropertyValues
+
+# handle tdf#119908 smart justify with automatic hyphenation
+
+class tdf159102(UITestCase):
+
+    def set_custom_hyphenation(self):
+        with self.ui_test.execute_dialog_through_command(".uno:OptionsTreeDialog") as xDialog:
+
+            xPages = xDialog.getChild("pages")
+            xLanguageEntry = xPages.getChild('2')                 # Language Settings
+            xLanguageEntry.executeAction("EXPAND", tuple())
+            xxLanguageEntryWritingAidsEntry = xLanguageEntry.getChild('1')
+            xxLanguageEntryWritingAidsEntry.executeAction("SELECT", tuple())          # Writing Aids
+
+            # add hyphenation "cur=sus" to the custom dictionary to solve the non-accessible
+            # hyphenation patterns for the test
+
+            # Select an editable dictionary (list of Ignored words)
+            dictionaries = xDialog.getChild("lingudicts")
+            hasEditableDictionary = False
+            for i in dictionaries.getChildren():
+                entry = dictionaries.getChild(i)
+                entry_label = get_state_as_dict(entry)["Text"]
+                if entry_label == "List of Ignored Words [All]":
+                    hasEditableDictionary = True
+                    entry.executeAction("SELECT", tuple())          # an editable user dictionary
+                    break
+
+            self.assertEqual(True, hasEditableDictionary)
+
+            # open Edit dialog window
+            edit = xDialog.getChild("lingudictsedit")
+            with self.ui_test.execute_blocking_action(edit.executeAction, args=('CLICK', ()), close_button="close") as xEdit:
+                # write cur=sus into the input box
+                inputbox = xEdit.getChild("word")
+                inputbox.executeAction("TYPE", mkPropertyValues({"TEXT": "cur=sus"}))
+                add = xEdit.getChild("newreplace")
+                add.executeAction("CLICK", tuple())
+
+    def test_tdf159102_smart_justify_with_automatic_hyphenation(self):
+        xToolkit = self.xContext.ServiceManager.createInstance('com.sun.star.awt.Toolkit')
+        with self.ui_test.load_file(get_url_for_data_file("tdf159102.fodt")) as writer_doc:
+            # we must not depend on the installed hyphenation patterns,
+            # so extend user dictionary temporarily with the hyphenation cur=sus
+            self.set_custom_hyphenation()
+            xToolkit.processEventsToIdle()
+            # delete the text of the first line
+            self.xUITest.executeCommand(".uno:GoToEndOfLine")
+            self.xUITest.executeCommand('.uno:StartOfDocumentSel')
+            self.xUITest.executeCommand('.uno:Delete')
+            paragraphs = writer_doc.Text.createEnumeration()
+            para1 = paragraphs.nextElement()
+            # This was "stas.", i.e. too much shrinking (accept also " cursus egestas." in the
+            # case of missing hyphenation patterns on some test platforms).
+            try:
+                self.assertEqual("sus egestas.", para1.String)
+            except AssertionError:
+                self.assertEqual(" cursus egestas.", para1.String)
+
+            # check next paragraph (containing different text portions)
+            self.xUITest.executeCommand(".uno:GoDown")
+            self.xUITest.executeCommand(".uno:GoToEndOfLine")
+            self.xUITest.executeCommand('.uno:StartOfDocumentSel')
+            self.xUITest.executeCommand('.uno:Delete')
+            paragraphs = writer_doc.Text.createEnumeration()
+            para1 = paragraphs.nextElement()
+            try:
+                self.assertEqual("sus egestas.", para1.String)
+            except AssertionError:
+                self.assertEqual(" cursus egestas.", para1.String)
