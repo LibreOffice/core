d8116658ecf66d915ae8cf6510a76ee628c45431
László Németh <nemeth@numbertext.org>
2024-01-10 20:41:04 +0100

    tdf#159102 sw smart justify: fix automatic hyphenation
    As before with soft hyphens, automatic hyphenation
    could result too much shrinking, because of calculating
    with an extra non-existing space in the line.
    
    Also try to shrink the line only if a space likely
    will be available in it.
    
    During testing, extend user dictionary temporarily
    with custom automatic hyphenation to avoid of false tests
    because of non-available hyphenation patterns. (Note: maybe
    the other tests with non-user dictionary based automatic
    hyphenations are not correct.)
    
    Follow-up to commit d511367c102ef2ada0f73dbe81744d39865d58ba
    "tdf#195085 sw smart justify: fix bad shrinking at soft hyphen".
    
    Change-Id: I58ecb8f1ea9f55ef2457d7ff4aca6aefa59a6dd1
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/162199
    Tested-by: Jenkins
    Reviewed-by: László Németh <nemeth@numbertext.org>
    (cherry picked from commit 7c1f4dd740c32050480f3ab678805ad3c4c748bb)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/162219
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- sw/source/core/text/portxt.cxx
+++ sw/source/core/text/portxt.cxx
@@ -325,14 +325,20 @@ bool SwTextPortion::Format_( SwTextFormatInfo &rInf )
 
         // call with an extra space: shrinking can result a new word in the line
         // and a new space before that, which is also a shrank space
-        // (except if the line was already broken at a soft hyphen, i.e. inside a word)
-        if ( aGuess.BreakPos() < TextFrameIndex(rInf.GetText().getLength()) &&
+        // (except if the line was already broken inside a word with hyphenation)
+        // TODO: handle the case, if the line contains extra amount of spaces
+        if (
+             // no automatic hyphenation
+             !aGuess.HyphWord().is() &&
+             // no hyphenation at soft hyphen
+             aGuess.BreakPos() < TextFrameIndex(rInf.GetText().getLength()) &&
              rInf.GetText()[sal_Int32(aGuess.BreakPos())] != CHAR_SOFTHYPHEN )
         {
             ++nSpacesInLine;
         }
 
-        bFull = !aGuess.Guess( *this, rInf, Height(), nSpacesInLine );
+        if ( nSpacesInLine > 0 )
+            bFull = !aGuess.Guess( *this, rInf, Height(), nSpacesInLine );
     }
 
     // these are the possible cases:
