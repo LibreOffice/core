300c18921cc67fa3202184f19f48b8ea654207b7
Stephan Bergmann <stephan.bergmann@allotropia.de>
2024-01-12 13:51:57 +0100

    Further adapting of create-partial-info
    ...along the lines of what had already been done for create-update-info, passing
    files to the mar executable with an -f files.txt instead of on the command line
    (which could grow too long for Windows), and taking the archive metadata into
    account.
    
    For the latter, `make create-partial-info` now expects a second make variable,
    ONLINEUPDATE_MAR_OLDMETADATA, in addition to ONLINEUPDATE_MAR_OLDARCHIVE.  And
    there is a new TODO in update/create_partial_update.py that the old and new
    metadata should be compared for problematic changes.  (For now, any such changes
    would just get lost by an update.)
    
    Change-Id: Ide45adedb72958fc32c5b26aa7ebb01530ccf3d7
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/161972
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <stephan.bergmann@allotropia.de>
    (cherry picked from commit a00266e2f8967f8eea0ce4d13c818e33ae188399)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/161991
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

--- bin/update/create_partial_update.py
+++ bin/update/create_partial_update.py
@@ -30,6 +30,7 @@ def main():
     product_name = sys.argv[7]
     version = sys.argv[8]
     old_archive = sys.argv[9]
+    old_metadata = sys.argv[10]
 
     old_uncompress_dir = uncompress_file_to_dir(old_archive, updater_path.get_previous_build_dir())
     versionini = os.path.join(old_uncompress_dir, 'program', 'version.ini') #TODO: Linux, macOS
@@ -43,12 +44,33 @@ def main():
     if old_build_id is None:
         raise Exception(f'Cannot find buildid in {versionini}')
 
+    #TODO: check for problematic changes between old and new metadata
+    with open(old_metadata) as meta:
+        for l in meta:
+            m = re.fullmatch('skip (.*)', l.rstrip())
+            if m and m.group(1).startswith(f'{product_name}/'):
+                path = m.group(1)[len(f'{product_name}/'):]
+                os.remove(os.path.join(old_uncompress_dir, path))
+
     new_tar_file_glob = os.path.join(updater_path.get_workdir(), "installation", product_name, "archive", "install", "*", f'{product_name}_*_archive*')
     new_tar_files = glob.glob(new_tar_file_glob)
     if len(new_tar_files) != 1:
         raise Exception(f'`{new_tar_file_glob}` does not match exactly one file')
     new_tar_file = new_tar_files[0]
     new_uncompress_dir = uncompress_file_to_dir(new_tar_file, updater_path.get_current_build_dir())
+
+    new_metadata = os.path.join(
+        updater_path.get_workdir(), 'installation', product_name, 'archive', 'install', 'metadata')
+    ifsfile = os.path.join(updater_path.get_mar_dir(), 'ifs')
+    with open(new_metadata) as meta, open(ifsfile, 'w') as ifs:
+        for l in meta:
+            m = re.fullmatch('(skip|cond) (.*)', l.rstrip())
+            if m and m.group(2).startswith(f'{product_name}/'):
+                path = m.group(2)[len(f'{product_name}/'):]
+                if m.group(1) == 'skip':
+                    os.remove(os.path.join(new_uncompress_dir, path))
+                else:
+                    ifs.write(f'"{path}" "{path}"\n')
 
     update_dir = updater_path.get_update_dir()
 
@@ -58,7 +80,8 @@ def main():
     os.putenv('MOZ_PRODUCT_VERSION', version)
     os.putenv('MAR_CHANNEL_ID', 'LOOnlineUpdater')
     subprocess.call([os.path.join(workdir, 'UnpackedTarball/onlineupdate/tools/update-packaging/make_incremental_update.sh'), convert_to_native(mar_file),
-                     convert_to_native(old_uncompress_dir), convert_to_native(new_uncompress_dir)])
+                     convert_to_native(old_uncompress_dir), convert_to_native(new_uncompress_dir),
+                     convert_to_native(ifsfile)])
 
     sign_mar_file(update_dir, certificate_path, certificate_name, mar_file, mar_name_prefix)
 
