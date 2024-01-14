41afcf4fa89abe23177e6a26d1a9e8611f351852
Mike Kaganski <mike.kaganski@collabora.com>
2024-01-14 12:16:48 +0600

    tdf#159174: there may be no tabs in the clipboard document
    Change-Id: I053d91bf44e39e14ade9face14084a107d0df898
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/162041
    Tested-by: Jenkins
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
    (cherry picked from commit 0b93cc83993b3cd78aa05556342839a21c96dc5a)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/162092
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- sc/source/ui/view/printfun.cxx
+++ sc/source/ui/view/printfun.cxx
@@ -466,6 +466,9 @@ static void lcl_HidePrint( const ScTableInfo& rTabInfo, SCCOL nX1, SCCOL nX2 )
 void ScPrintFunc::DrawToDev(ScDocument& rDoc, OutputDevice* pDev, double /* nPrintFactor */,
                             const tools::Rectangle& rBound, ScViewData* pViewData, bool bMetaFile)
 {
+    if (rDoc.GetMaxTableNumber() < 0)
+        return;
+
     //! evaluate nPrintFactor !!!
 
     SCTAB nTab = 0;
