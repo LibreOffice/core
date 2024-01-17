LibreOffice/core

commit 21473f46fda5f1c7f220493667a96fff7c78a7b3

sw: fix crashtesting assert on forum-mso-en-13115.docx
itrpaint.cxx:436: SwTextPainter::DrawTextLine: Assertion `!roTaggedParagraph' failed.

The problem is that there are 2 SwNumberPortion, and the 1st one doesn't
have the m_bHasFollow flag set.

The numbering suffix is 5 consecutive ' ' spaces, which is oddly enough
considered a hook char in SwFieldPortion::Format() but not in
SwTextFormatInfo::ScanPortionEnd().

This inconsistency appears to cause the problem, and was already there
in initial CVS import, but nothing bad seems to happen when it is
resolved.

(regression from commit 9b38beadf9eaf027b201cdf0ecb2bce5611014dd
 and commit df5328ff1e472af9c69d4ff83fa13c71d644ddec)

Change-Id: I602d9adb63220cb63ce8a477b617e9aeae45fb8e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/152274
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/core/text/porfld.cxx
@@ -395,7 +395,6 @@ bool SwFieldPortion::Format( SwTextFormatInfo &rInf )
                 switch (cNew)
                 {
                     case CH_BREAK:
-                    case ' ': // ??? why is this one not in ScanPortionEnd?
                     case CH_TAB:
                     case CHAR_HARDHYPHEN: // non-breaking hyphen
                     case CHAR_SOFTHYPHEN:

commit 4cdb542348b20d8a558cd6413c7cccdc50241fa9

tdf#159101 sw: Fix space appearing at start of line when field wraps
Adding the ' ' to ScanPortionEnd fails pretty quickly with infinite
loop.

The important part of fixing the forum-mso-en-13115.docx was that the
SetHasFollow(true) condition is not affected by a ' '.

(regression from commit 21473f46fda5f1c7f220493667a96fff7c78a7b3)

Change-Id: I6452673c0fe05ca549b63a398a977fc71db64bae
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/162167
Tested-by: Jenkins
Reviewed-by: Matthew Kogan <matthew@epoq.co.uk>
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/core/text/porfld.cxx
@@ -402,10 +402,13 @@ bool SwFieldPortion::Format( SwTextFormatInfo &rInf )
             // These characters should not be contained in the follow
             // field portion. They are handled via the HookChar mechanism.
             const sal_Unicode nNew = !aNew.isEmpty() ? aNew[0] : 0;
-            auto IsHook = [](const sal_Unicode cNew) -> bool
+            auto IsHook = [](const sal_Unicode cNew, bool const isSpace = false) -> bool
             {
                 switch (cNew)
                 {
+                    case ' ': // tdf#159101 this one is not in ScanPortionEnd
+                              // but is required for justified text
+                        return isSpace;
                     case CH_BREAK:
                     case CH_TAB:
                     case CHAR_HARDHYPHEN: // non-breaking hyphen
@@ -422,7 +425,7 @@ bool SwFieldPortion::Format( SwTextFormatInfo &rInf )
                         return false;
                 }
             };
-            if (IsHook(nNew))
+            if (IsHook(nNew, true))
             {
                 if (nNew == CH_BREAK)
                 {