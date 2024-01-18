ec10bd238991a8eda71aec7d261eafa7e47ced0d
Stephan Bergmann <stephan.bergmann@allotropia.de>
2024-01-18 13:16:10 +0100

    -Werror,-Wunused-parameter (Emscripten)
    Change-Id: Ia2e9ab91def1454bd06044b0e38f2d0720b44033
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/162246
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <stephan.bergmann@allotropia.de>

--- cui/source/dialogs/QrCodeGenDialog.cxx
+++ cui/source/dialogs/QrCodeGenDialog.cxx
@@ -272,6 +272,9 @@ bool QrCodeGenDialog::runAsync(const std::shared_ptr<QrCodeGenDialog>& rControll
 
         rFunc(nResult);
     });
+#else
+    (void)rController;
+    (void)rFunc;
 #endif
     return true;
 }
