LibreOffice/core

commit 27d04f6dbf38aa28fb7215590d578c4567db5770

tdf#119038 DOCX: fix FollowTextFlow handling
This fix also a clean-up for the following commits:

ad8857dab30e099a0cf6ec18d184a6c836b33317
(tdf#130120 DOCX: export o:allowincell)
14ad64270e4fbca3c24da6f55f260b1fb229556a
(tdf#129888 DOCX shape import: handle o:allowincell)
10f29d8bf05d44ca8bc11d34d1294ec17f8ac0f1
(tdf#87569 tdf#109411 DOCX import: fix shape anchor in tables)

where these patches ignored the option "Follow text flow"
and there was no possibility to switch this setting on GUI.
Now this is handled on GUI as well by replacing the
grab-bag with UNO property "IsFollowingTextFlow".

The tdf#119038 bug also fixed.

Note: Unit tests for the commits mentioned above were
modified for testing the new property, so new tests were
not added.

Change-Id: Ie35c6e280f8b33d8535ca4cd9749f110f9d592ee
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/90753
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

writerfilter/source/ooxml/OOXMLFastContextHandler.cxx
@@ -37,6 +37,7 @@
 #include <dmapper/PropertyIds.hxx>
 #include <comphelper/propertysequence.hxx>
 #include <comphelper/sequenceashashmap.hxx>
+#include <dmapper/PropertyIds.hxx>
 
 static const sal_Unicode uCR = 0xd;
 static const sal_Unicode uFtnEdnRef = 0x2;
@@ -69,7 +70,8 @@ OOXMLFastContextHandler::OOXMLFastContextHandler
   mpStream(nullptr),
   mnTableDepth(0),
   inPositionV(false),
-  mbLayoutInCell(true),
+  mbAllowInCell(true),
+  mbIsVMLfound(false),
   m_xContext(context),
   m_bDiscardChildren(false),
   m_bTookChoice(false)
@@ -90,7 +92,8 @@ OOXMLFastContextHandler::OOXMLFastContextHandler(OOXMLFastContextHandler * pCont
   mpParserState(pContext->mpParserState),
   mnTableDepth(pContext->mnTableDepth),
   inPositionV(pContext->inPositionV),
-  mbLayoutInCell(pContext->mbLayoutInCell),
+  mbAllowInCell(pContext->mbAllowInCell),
+  mbIsVMLfound(pContext->mbIsVMLfound),
   m_xContext(pContext->m_xContext),
   m_bDiscardChildren(pContext->m_bDiscardChildren),
   m_bTookChoice(pContext->m_bTookChoice)
@@ -1670,34 +1673,15 @@ void OOXMLFastContextHandlerShape::sendShape( Token_t Element )
 
             bool bIsPicture = Element == ( NMSP_dmlPicture | XML_pic );
 
-
             //tdf#87569: Fix table layout with correcting anchoring
             //If anchored object is in table, Word calculates its position from cell border
             //instead of page (what is set in the sample document)
             uno::Reference<beans::XPropertySet> xShapePropSet(xShape, uno::UNO_QUERY);
-            if(xShapePropSet && bIsPicture) //TODO make grabbag for textboxes as well
+            if (mnTableDepth > 0 && xShapePropSet.is() && mbIsVMLfound) //if we had a table
             {
-                uno::Sequence<beans::PropertyValue> aShapeGrabBag;
-                xShapePropSet->getPropertyValue("InteropGrabBag") >>= aShapeGrabBag;
-                beans::PropertyValue aLayInCell;
-                aLayInCell.Name = "LayoutInCell";
-                aLayInCell.Value <<= mbLayoutInCell;
-                aShapeGrabBag.realloc(1+aShapeGrabBag.size());
-                aShapeGrabBag[aShapeGrabBag.size() -1] = aLayInCell;
-                xShapePropSet->setPropertyValue("InteropGrabBag", uno::makeAny(aShapeGrabBag));
+                xShapePropSet->setPropertyValue(dmapper::getPropertyName(dmapper::PROP_FOLLOW_TEXT_FLOW),
+                                                uno::makeAny(mbAllowInCell));
             }
-            if (mnTableDepth > 0 && mbLayoutInCell) //if we had a table
-            {
-                sal_Int16 nCurrentHorOriRel = {}; // spurious -Werror=maybe-uninitialized
-                xShapePropSet->getPropertyValue("HoriOrientRelation") >>= nCurrentHorOriRel;
-                //and the correction:
-                if (nCurrentHorOriRel == com::sun::star::text::RelOrientation::PAGE_FRAME)
-                {
-                    xShapePropSet->setPropertyValue("HoriOrientRelation",
-                                                    uno::makeAny(text::RelOrientation::FRAME));
-                }
-            }
-
             // Notify the dmapper that the shape is ready to use
             if ( !bIsPicture )
             {
@@ -1748,7 +1732,7 @@ OOXMLFastContextHandlerShape::lcl_createFastChildContext
     bool bGroupShape = Element == Token_t(NMSP_vml | XML_group);
     // drawingML version also counts as a group shape.
     bGroupShape |= mrShapeContext->getStartToken() == Token_t(NMSP_wpg | XML_wgp);
-
+    mbIsVMLfound = (getNamespace(Element) == NMSP_vmlOffice) || (getNamespace(Element) == NMSP_vml);
     switch (oox::getNamespace(Element))
     {
         case NMSP_doc:
@@ -1771,9 +1755,8 @@ OOXMLFastContextHandlerShape::lcl_createFastChildContext
                                                            this);
 
                     //tdf129888 store allowincell attribute of the VML shape
-                    mbLayoutInCell = true;
                     if (Attribs->hasAttribute(NMSP_vmlOffice | XML_allowincell))
-                        mbLayoutInCell
+                        mbAllowInCell
                             = !(Attribs->getValue(NMSP_vmlOffice | XML_allowincell) == "f");
 
                     if (!bGroupShape)

sw/qa/extras/ooxmlexport/ooxmlexport14.cxx
@@ -98,10 +98,10 @@ DECLARE_OOXMLIMPORT_TEST(testTdf129888vml, "tdf129888vml.docx")
     //to be calculated from the page frame instead of the table:
 
     uno::Reference<beans::XPropertySet> xShapeProperties(getShape(1), uno::UNO_QUERY);
-    sal_Int16 nValue;
-    xShapeProperties->getPropertyValue("HoriOrientRelation") >>= nValue;
+    bool bValue;
+    xShapeProperties->getPropertyValue("IsFollowingTextFlow") >>= bValue;
     CPPUNIT_ASSERT_EQUAL_MESSAGE("tdf129888vml The line shape has bad place!",
-                                 text::RelOrientation::PAGE_FRAME, nValue);
+                                 false, bValue);
 }
 
 DECLARE_OOXMLIMPORT_TEST(testTdf129888dml, "tdf129888dml.docx")
@@ -112,18 +112,18 @@ DECLARE_OOXMLIMPORT_TEST(testTdf129888dml, "tdf129888dml.docx")
     //to be calculated from the page frame instead of the table:
 
     uno::Reference<beans::XPropertySet> xShapeProperties(getShape(1), uno::UNO_QUERY);
-    sal_Int16 nValue;
-    xShapeProperties->getPropertyValue("HoriOrientRelation") >>= nValue;
+    bool bValue;
+    xShapeProperties->getPropertyValue("IsFollowingTextFlow") >>= bValue;
     CPPUNIT_ASSERT_EQUAL_MESSAGE("tdf129888dml The shape has bad place!",
-                                 text::RelOrientation::PAGE_FRAME, nValue);
+        false, bValue);
 }
 
 DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testTdf130120, "tdf130120.docx")
 {
-   //Text for exporting the allowincell attribute:
+    //Text for exporting the allowincell attribute:
     xmlDocPtr p_XmlDoc = parseExport("word/document.xml");
     assertXPath(p_XmlDoc, "/w:document/w:body/w:tbl/w:tr/w:tc/w:p/w:r/mc:AlternateContent/"
-                          "mc:Choice/w:drawing/wp:anchor","layoutInCell","0");
+        "mc:Choice/w:drawing/wp:anchor", "layoutInCell", "0");
 }
 
 
@@ -131,10 +131,10 @@ DECLARE_OOXMLEXPORT_TEST(testTdf87569v, "tdf87569_vml.docx")
 {
     //the original tdf87569 sample has vml shapes...
     uno::Reference<beans::XPropertySet> xShapeProperties(getShape(1), uno::UNO_QUERY);
-    sal_Int16 nValue;
-    xShapeProperties->getPropertyValue("HoriOrientRelation") >>= nValue;
+    bool bValue;
+    xShapeProperties->getPropertyValue("IsFollowingTextFlow") >>= bValue;
     CPPUNIT_ASSERT_EQUAL_MESSAGE("tdf87569_vml: The Shape is not in the table!",
-                                 text::RelOrientation::FRAME, nValue);
+                                 true, bValue);
 }
 
 DECLARE_ODFEXPORT_TEST(testArabicZeroNumbering, "arabic-zero-numbering.docx")
@@ -249,10 +249,10 @@ DECLARE_OOXMLEXPORT_TEST(testTdf87569d, "tdf87569_drawingml.docx")
 {
     //if the original tdf87569 sample is upgraded it will have drawingml shapes...
     uno::Reference<beans::XPropertySet> xShapeProperties(getShape(1), uno::UNO_QUERY);
-    sal_Int16 nValue;
-    xShapeProperties->getPropertyValue("HoriOrientRelation") >>= nValue;
+    bool bValue;
+    xShapeProperties->getPropertyValue("IsFollowingTextFlow") >>= bValue;
     CPPUNIT_ASSERT_EQUAL_MESSAGE("tdf87569_drawingml: The Shape is not in the table!",
-                                 text::RelOrientation::FRAME, nValue);
+                                 true, bValue);
 }
 
 DECLARE_OOXMLEXPORT_TEST(testTdf130610, "tdf130610_bold_in_2_styles.ott")

sw/source/core/doc/textboxhelper.cxx
@@ -62,8 +62,8 @@ void SwTextBoxHelper::create(SwFrameFormat* pShape)
         pShape->GetDoc()->GetDocShell()->GetBaseModel(), uno::UNO_QUERY);
     uno::Reference<text::XTextContentAppend> xTextContentAppend(xTextDocument->getText(),
                                                                 uno::UNO_QUERY);
-    xTextContentAppend->appendTextContent(xTextFrame, uno::Sequence<beans::PropertyValue>());
 
+    xTextContentAppend->appendTextContent(xTextFrame, uno::Sequence<beans::PropertyValue>());
     // Link FLY and DRAW formats, so it becomes a text box (needed for syncProperty calls).
     uno::Reference<text::XTextFrame> xRealTextFrame(xTextFrame, uno::UNO_QUERY);
     auto pTextFrame = dynamic_cast<SwXTextFrame*>(xRealTextFrame.get());
@@ -110,6 +110,8 @@ void SwTextBoxHelper::create(SwFrameFormat* pShape)
     syncProperty(pShape, RES_FRM_SIZE, MID_FRMSIZE_SIZE, uno::makeAny(xShape->getSize()));
 
     uno::Reference<beans::XPropertySet> xShapePropertySet(xShape, uno::UNO_QUERY);
+    syncProperty(pShape, RES_FOLLOW_TEXT_FLOW, MID_FOLLOW_TEXT_FLOW,
+                 xShapePropertySet->getPropertyValue(UNO_NAME_IS_FOLLOWING_TEXT_FLOW));
     syncProperty(pShape, RES_HORI_ORIENT, MID_HORIORIENT_ORIENT,
                  xShapePropertySet->getPropertyValue(UNO_NAME_HORI_ORIENT));
     syncProperty(pShape, RES_HORI_ORIENT, MID_HORIORIENT_RELATION,

sw/source/core/unocore/unotext.cxx
@@ -1478,8 +1478,8 @@ SwXText::insertTextContentWithProperties(
 uno::Reference< text::XTextRange > SAL_CALL
 SwXText::appendTextContent(
     const uno::Reference< text::XTextContent >& xTextContent,
-    const uno::Sequence< beans::PropertyValue >&
-        rCharacterAndParagraphProperties)
+    const uno::Sequence< beans::PropertyValue >& rCharacterAndParagraphProperties
+    )
 {
     // Right now this doesn't need a guard, as it's just calling the insert
     // version, that has it already.

sw/source/filter/ww8/docxsdrexport.cxx
@@ -471,15 +471,9 @@ void DocxSdrExport::startDMLAnchorInline(const SwFrameFormat* pFrameFormat, cons
         {
             uno::Reference<drawing::XShape> xShape((const_cast<SdrObject*>(pObj)->getUnoShape()),
                                                    uno::UNO_QUERY);
-            uno::Sequence<beans::PropertyValue> propList = lclGetProperty(xShape, "InteropGrabBag");
-            if (propList.hasElements())
-            {
-                auto pLclProp = std::find_if(
-                    std::begin(propList), std::end(propList),
-                    [](const beans::PropertyValue& rProp) { return rProp.Name == "LayoutInCell"; });
-                if (pLclProp && pLclProp != propList.end())
-                    pLclProp->Value >>= bLclInTabCell;
-            }
+            uno::Reference<beans::XPropertySet> xShapeProps(xShape, uno::UNO_QUERY);
+            if (xShapeProps.is())
+                xShapeProps->getPropertyValue("IsFollowingTextFlow") >>= bLclInTabCell;
         }
         if (bLclInTabCell)
             attrList->add(XML_layoutInCell, "1");

writerfilter/source/dmapper/GraphicImport.cxx
@@ -270,7 +270,7 @@ public:
         ,nVertOrient(  text::VertOrientation::NONE )
         ,nVertRelation( text::RelOrientation::FRAME )
         ,nWrap(text::WrapTextMode_NONE)
-        ,bLayoutInCell(false)
+        ,bLayoutInCell(true)
         ,bOpaque( !rDMapper.IsInHeaderFooter() )
         ,bContour(false)
         ,bContourOutside(true)
@@ -662,7 +662,6 @@ void GraphicImport::lcl_attribute(Id nName, Value& rValue)
             {
                 uno::Reference< drawing::XShape> xShape;
                 rValue.getAny( ) >>= xShape;
-
                 if ( xShape.is( ) )
                 {
                     // Is it a graphic image
@@ -847,7 +846,12 @@ void GraphicImport::lcl_attribute(Id nName, Value& rValue)
                             eAnchorType = text::TextContentAnchorType_AT_CHARACTER;
 
                         xShapeProps->setPropertyValue("AnchorType", uno::makeAny(eAnchorType));
-
+                        if (m_pImpl->bLayoutInCell && bTextBox && m_pImpl->rDomainMapper.IsInTable()
+                            && m_pImpl->nHoriRelation == text::RelOrientation::PAGE_FRAME)
+                            m_pImpl->nHoriRelation = text::RelOrientation::FRAME;
+                        if(m_pImpl->rDomainMapper.IsInTable())
+                            xShapeProps->setPropertyValue(getPropertyName(PROP_FOLLOW_TEXT_FLOW),
+                                uno::makeAny(m_pImpl->bLayoutInCell));
                         //only the position orientation is handled in applyPosition()
                         m_pImpl->applyPosition(xShapeProps);
 
@@ -884,25 +888,6 @@ void GraphicImport::lcl_attribute(Id nName, Value& rValue)
                                 xShapeProps->setPropertyValue("RotateAngle", uno::makeAny(nRotation));
                         }
 
-                        //tdf#109411 If anchored object is in table, Word calculates its position from cell border
-                        //instead of page (what is set in the sample document)
-                        if (xShapeProps)
-                        {
-                            uno::Sequence<beans::PropertyValue> aShapeGrabBag;
-                            xShapeProps->getPropertyValue("InteropGrabBag") >>= aShapeGrabBag;
-                            beans::PropertyValue aLayInCell;
-                            aLayInCell.Name = "LayoutInCell";
-                            aLayInCell.Value <<= m_pImpl->bLayoutInCell;
-                            aShapeGrabBag.realloc(1 + aShapeGrabBag.size());
-                            aShapeGrabBag[aShapeGrabBag.size() - 1] = aLayInCell;
-                            xShapeProps->setPropertyValue("InteropGrabBag",
-                                                            uno::makeAny(aShapeGrabBag));
-                        }
-                        if (m_pImpl->rDomainMapper.IsInTable() && m_pImpl->bLayoutInCell &&
-                            m_pImpl->nHoriRelation == text::RelOrientation::PAGE_FRAME && IsGraphic())
-                        {
-                            m_pImpl->nHoriRelation = text::RelOrientation::FRAME;
-                        }
 
                         m_pImpl->applyRelativePosition(xShapeProps, /*bRelativeOnly=*/true);
 
@@ -1370,9 +1355,9 @@ uno::Reference<text::XTextContent> GraphicImport::createGraphicObject(uno::Refer
                 }
                 xGraphicObjectProperties->setPropertyValue(getPropertyName( PROP_SURROUND ),
                     uno::makeAny(static_cast<sal_Int32>(m_pImpl->nWrap)));
-                if( m_pImpl->rDomainMapper.IsInTable() && m_pImpl->bLayoutInCell )
+                if( m_pImpl->rDomainMapper.IsInTable())
                     xGraphicObjectProperties->setPropertyValue(getPropertyName( PROP_FOLLOW_TEXT_FLOW ),
-                        uno::makeAny(true));
+                        uno::makeAny(m_pImpl->bLayoutInCell));
 
                 xGraphicObjectProperties->setPropertyValue(getPropertyName( PROP_SURROUND_CONTOUR ),
                     uno::makeAny(m_pImpl->bContour));

writerfilter/source/ooxml/OOXMLFastContextHandler.hxx
@@ -226,7 +226,8 @@ protected:
     const css::uno::Reference< css::uno::XComponentContext >& getComponentContext() const { return m_xContext;}
 
     bool inPositionV;
-    bool mbLayoutInCell; // o:allowincell
+    bool mbAllowInCell; // o:allowincell
+    bool mbIsVMLfound;
     OOXMLValue::Pointer_t mpGridAfter;
 
 private:

commit ad0266eb84eafa32ccc4e0ddf3c6392860bc9b13

tdf#153909 docx import: wrap through shapes shouldn't FollowTextFlow
This resolves a NISZ 6.4.0 regression from
commit 27d04f6dbf38aa28fb7215590d578c4567db5770
    tdf#119038 DOCX: fix FollowTextFlow handling

MSO is all over the place in how they implement layoutInCell
(aka FollowTextFlow in LO). There are lots of compat15 differences
for this, but I don't notice compat coming into play with this bug.

For reference, see commit e993638d5ecd33783f2eebdccfa87a81e5a8a2c5
Author: Miklos Vajna on Mon Jan 24 12:53:25 2022 +0100
    DOCX import: fix <wp:anchor layoutInCell="1"> with <wp:wrapNone>

I'm sure this is the WRONG code spot for handling FollowTextFlow,
but anywhere else and we lose the "it came from a vml shape"
limiting information, so I'm not going to try to relocate this code.

Inline (as-character) shapes ignore layoutInCell,
so the type of anchor (floating or inline) shouldn't matter
and therefore doesn't need to be part of the if () clause.

The header can be special (in compat14 it means everything
is wrap through), but at least in this case it is not limited
to IsInHeaderHeader. Using a non-header as the unit test.

make CppunitTest_sw_ooxmlexport21 \
    CPPUNIT_TEST_NAME=testTdf153909_followTextFlow

The first patchset contains a list of all of the unit tests
that were impacted by this patch: nothing interesting.

Change-Id: I0c4c7924833550533ad1b0b7609840a666d4d589
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/162324
Reviewed-by: Justin Luth <jluth@mail.com>
Tested-by: Jenkins

sw/CppunitTest_sw_ooxmlexport21.mk
@@ -1,0 +1,14 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#*************************************************************************
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+#*************************************************************************
+
+$(eval $(call sw_ooxmlexport_test,21))
+
+# vim: set noet sw=4 ts=4:

sw/Module_sw.mk
@@ -95,6 +95,7 @@ $(eval $(call gb_Module_add_slowcheck_targets,sw,\
     CppunitTest_sw_ooxmlexport18 \
     CppunitTest_sw_ooxmlexport19 \
     CppunitTest_sw_ooxmlexport20 \
+    CppunitTest_sw_ooxmlexport21 \
     CppunitTest_sw_ooxmlexport_template \
     CppunitTest_sw_ooxmlfieldexport \
     CppunitTest_sw_ooxmllinks \

writerfilter/source/ooxml/OOXMLFastContextHandler.cxx
@@ -1826,8 +1826,20 @@ void OOXMLFastContextHandlerShape::sendShape( Token_t Element )
     uno::Reference<beans::XPropertySet> xShapePropSet(xShape, uno::UNO_QUERY);
     if (mnTableDepth > 0 && xShapePropSet.is() && mbIsVMLfound) //if we had a table
     {
+        bool bForceShapeIntoCell = mbAllowInCell;
+        // According to tdf#153909 and GraphicImport's LN_shape handling,
+        // through-anchored shapes should not force the shape into the cell
+        if (bForceShapeIntoCell)
+        {
+            text::WrapTextMode nSurround = text::WrapTextMode_NONE;
+            xShapePropSet->getPropertyValue("Surround") >>= nSurround;
+            sal_Int32 nHoriRelation = -1;
+            xShapePropSet->getPropertyValue("HoriOrientRelation") >>= nHoriRelation;
+            bForceShapeIntoCell = (nSurround != text::WrapTextMode_THROUGH)
+                                   || (nHoriRelation != text::RelOrientation::FRAME);
+        }
         xShapePropSet->setPropertyValue(dmapper::getPropertyName(dmapper::PROP_FOLLOW_TEXT_FLOW),
-                                        uno::Any(mbAllowInCell));
+                                        uno::Any(bForceShapeIntoCell));
     }
     // Notify the dmapper that the shape is ready to use
     if ( !bIsPicture )

sw/qa/extras/ooxmlexport/ooxmlexport21.cxx
@@ -1,0 +1,50 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <swmodeltestbase.hxx>
+
+#include <com/sun/star/beans/XPropertyState.hpp>
+#include <com/sun/star/text/XDocumentIndex.hpp>
+#include <com/sun/star/text/XTextTable.hpp>
+#include <com/sun/star/style/LineSpacing.hpp>
+#include <com/sun/star/style/LineSpacingMode.hpp>
+
+#include <pam.hxx>
+#include <unotxdoc.hxx>
+#include <docsh.hxx>
+
+namespace
+{
+class Test : public SwModelTestBase
+{
+public:
+    Test()
+        : SwModelTestBase("/sw/qa/extras/ooxmlexport/data/", "Office Open XML Text")
+    {
+    }
+};
+
+DECLARE_OOXMLEXPORT_TEST(testTdf153909_followTextFlow, "tdf153909_followTextFlow.docx")
+{
+    // Although MSO's UI reports "layoutInCell" for the rectangle, it isn't specified or honored
+    CPPUNIT_ASSERT(!getProperty<bool>(getShape(1), "IsFollowingTextFlow"));
+
+    // Given a table with a rectangle anchored in it (wrap-through) that appears above the table...
+    xmlDocUniquePtr pDump = parseLayoutDump();
+    sal_Int32 nRectBottom
+        = getXPath(pDump, "//anchored/SwAnchoredDrawObject/bounds"_ostr, "bottom"_ostr).toInt32();
+    sal_Int32 nTableTop = getXPath(pDump, "//tab/row/infos/bounds"_ostr, "top"_ostr).toInt32();
+    // The entire table must be below the rectangle
+    CPPUNIT_ASSERT(nTableTop > nRectBottom);
+}
+
+} // end of anonymous namespace
+CPPUNIT_PLUGIN_IMPLEMENT();
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */