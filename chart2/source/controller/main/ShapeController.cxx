LibreOffice/core

commit c5bb73cae7c172ad0f02f8c67dd57b53337f1d78

Get the whole thing to build after the method sig change in SdrObject.

chart2/source/controller/main/ShapeController.cxx
@@ -479,7 +479,7 @@ void ShapeController::executeDispatch_RenameObject()
             SdrObject* pSelectedObj = pDrawViewWrapper->getSelectedObject();
             if ( pSelectedObj )
             {
-                String aName( pSelectedObj->GetName() );
+                rtl::OUString aName = pSelectedObj->GetName();
                 SvxAbstractDialogFactory* pFact = SvxAbstractDialogFactory::Create();
                 if ( pFact )
                 {
@@ -488,8 +488,10 @@ void ShapeController::executeDispatch_RenameObject()
                     pDlg->SetCheckNameHdl( LINK( this, ShapeController, CheckNameHdl ) );
                     if ( pDlg.get() && ( pDlg->Execute() == RET_OK ) )
                     {
-                        pDlg->GetName( aName );
-                        if ( aName != pSelectedObj->GetName() )
+                        String aTmp;
+                        pDlg->GetName(aTmp);
+                        aName = aTmp;
+                        if (pSelectedObj->GetName().equals(aName))
                         {
                             pSelectedObj->SetName( aName );
                         }

chart2/source/controller/main/ElementSelector.cxx
@@ -154,7 +154,7 @@ void SelectorListBox::UpdateChartElementsListAndSelection()
             {
                 ListBoxEntryData aEntry;
                 SdrObject* pSelectedObj = DrawViewWrapper::getSdrObject( aSelectedOID.getAdditionalShape() );
-                ::rtl::OUString aName( pSelectedObj ? pSelectedObj->GetName() : String() );
+                rtl::OUString aName = pSelectedObj ? pSelectedObj->GetName() : rtl::OUString();
                 aEntry.UIName = ( aName.isEmpty() ?  ::rtl::OUString( String( SchResId( STR_OBJECT_SHAPE ) ) ) : aName );
                 aEntry.OID = aSelectedOID;
                 m_aEntries.push_back( aEntry );

filter/source/msfilter/eschesdo.cxx
@@ -176,9 +176,9 @@ sal_uInt32 ImplEESdrWriter::ImplWriteShape( ImplEESdrObject& rObj,
             break;
 
         // #i51348# get shape name
-        String aShapeName;
+        rtl::OUString aShapeName;
         if( const SdrObject* pSdrObj = rObj.GetSdrObject() )
-            if( pSdrObj->GetName().Len() > 0 )
+            if (!pSdrObj->GetName().isEmpty())
                 aShapeName = pSdrObj->GetName();
 
         Point aTextRefPoint;
@@ -223,7 +223,7 @@ sal_uInt32 ImplEESdrWriter::ImplWriteShape( ImplEESdrObject& rObj,
         EscherPropertyContainer aPropOpt( mpEscherEx->GetGraphicProvider(), mpPicStrm, aRect100thmm );
 
         // #i51348# shape name
-        if( aShapeName.Len() > 0 )
+        if (!aShapeName.isEmpty())
             aPropOpt.AddOpt( ESCHER_Prop_wzName, aShapeName );
         if ( InteractionInfo* pInteraction = mpHostAppData ? mpHostAppData->GetInteractionInfo():NULL )
         {

filter/source/msfilter/msdffimp.cxx
@@ -4375,7 +4375,7 @@ SdrObject* SvxMSDffManager::ImportGraphic( SvStream& rSt, SfxItemSet& rSet, cons
         if ( pRet && bGrfRead && !aVisArea.IsEmpty() )
             pRet->SetBLIPSizeRectangle( aVisArea );
 
-        if ( !pRet->GetName().Len() )                   // SJ 22.02.00 : PPT OLE IMPORT:
+        if (pRet->GetName().isEmpty())                   // SJ 22.02.00 : PPT OLE IMPORT:
         {                                               // name is already set in ImportOLE !!
             // JP 01.12.99: SetName before SetModel - because in the other order the Bug 70098 is active
             if ( ( eFlags & mso_blipflagType ) != mso_blipflagComment )

sc/source/core/data/drwlayer.cxx
@@ -1648,7 +1648,7 @@ inline sal_Bool IsNamedObject( SdrObject* pObj, const String& rName )
     //  sal_True if rName is the object's Name or PersistName
     //  (used to find a named object)
 
-    return ( pObj->GetName() == rName ||
+    return ( pObj->GetName().equals(rName) ||
             ( pObj->GetObjIdentifier() == OBJ_OLE2 &&
               static_cast<SdrOle2Obj*>(pObj)->GetPersistName() == rName ) );
 }
@@ -1725,7 +1725,7 @@ void ScDrawLayer::EnsureGraphicNames()
 
             while (pObject)
             {
-                if ( pObject->GetObjIdentifier() == OBJ_GRAF && pObject->GetName().Len() == 0 )
+                if ( pObject->GetObjIdentifier() == OBJ_GRAF && pObject->GetName().isEmpty())
                     pObject->SetName( GetNewGraphicName( &nCounter ) );
 
                 pObject = aIter.Next();

sc/source/ui/drawfunc/drawsh5.cxx
@@ -500,7 +500,7 @@ void ScDrawShell::ExecDrawFunc( SfxRequest& rReq )
 
                     if(SC_LAYER_INTERN != pSelected->GetLayer())
                     {
-                        String aName(pSelected->GetName());
+                        rtl::OUString aName = pSelected->GetName();
 
                         SvxAbstractDialogFactory* pFact = SvxAbstractDialogFactory::Create();
                         OSL_ENSURE(pFact, "Dialogdiet fail!");
@@ -512,14 +512,16 @@ void ScDrawShell::ExecDrawFunc( SfxRequest& rReq )
                         if(RET_OK == pDlg->Execute())
                         {
                             ScDocShell* pDocSh = pViewData->GetDocShell();
-                            pDlg->GetName(aName);
+                            String aTmp;
+                            pDlg->GetName(aTmp);
+                            aName = aTmp;
 
-                            if(aName != pSelected->GetName())
+                            if (!aName.equals(pSelected->GetName()))
                             {
                                 // handle name change
                                 const sal_uInt16 nObjType(pSelected->GetObjIdentifier());
 
-                                if(OBJ_GRAF == nObjType && 0L == aName.Len())
+                                if (OBJ_GRAF == nObjType && aName.isEmpty())
                                 {
                                     //  graphics objects must have names
                                     //  (all graphics are supposed to be in the navigator)

sd/source/core/drawdoc2.cxx
@@ -111,7 +111,7 @@ SdrObject* SdDrawDocument::GetObj(const String& rObjName) const
         {
             pObj = aIter.Next();
 
-            if( ( rObjName == pObj->GetName() ) ||
+            if( ( pObj->GetName().equals(rObjName) ) ||
                 ( SdrInventor == pObj->GetObjInventor() &&
                   OBJ_OLE2 == pObj->GetObjIdentifier() &&
                   rObjName == static_cast< SdrOle2Obj* >( pObj )->GetPersistName() ) )
@@ -138,7 +138,7 @@ SdrObject* SdDrawDocument::GetObj(const String& rObjName) const
         {
             pObj = aIter.Next();
 
-            if( ( rObjName == pObj->GetName() ) ||
+            if( ( pObj->GetName().equals(rObjName) ) ||
                 ( SdrInventor == pObj->GetObjInventor() &&
                   OBJ_OLE2 == pObj->GetObjIdentifier() &&
                   rObjName == static_cast< SdrOle2Obj* >( pObj )->GetPersistName() ) )

sw/source/ui/shells/drwbassh.cxx
@@ -640,7 +640,7 @@ IMPL_LINK( SwDrawBaseShell, CheckGroupShapeNameHdl, AbstractSvxNameDialog*, pNam
         while( aIter.IsMore() )
         {
             SdrObject* pTempObj = aIter.Next();
-            if ( pObj != pTempObj && pTempObj->GetName() == sNewName )
+            if ( pObj != pTempObj && pTempObj->GetName().equals(sNewName) )
             {
                 nRet = 0;
                 break;

commit 0ad94d52c022a0acb20be21b5a1dfcf445e12f0c

fix bug in renaming chart objects
introduced by
    commit c5bb73cae7c172ad0f02f8c67dd57b53337f1d78
    Author: Kohei Yoshida <kohei.yoshida@suse.com>
    Date:   Tue Jan 31 16:03:46 2012 -0500
Get the whole thing to build after the method sig change in
SdrObject.

Change-Id: I1ca887d09857f6476980381853a9ae4946fd03e1
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/162364
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

chart2/source/controller/main/ShapeController.cxx
@@ -440,7 +440,7 @@ void ShapeController::executeDispatch_RenameObject()
     if ( pDlg->Execute() == RET_OK )
     {
         pDlg->GetName(aName);
-        if (pSelectedObj->GetName() == aName)
+        if (pSelectedObj->GetName() != aName)
         {
             pSelectedObj->SetName( aName );
         }