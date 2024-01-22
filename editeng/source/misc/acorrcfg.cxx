LibreOffice/core

commit c4fc18308074634e9578ad12d94d937f259aa22a

Autocorrect: Add option for autoformat bulleted lists after space
Enable option in Autocoorect => bulleted lists after pressing space
Example: press * or - and space will change into bullets

Change-Id: I9741ad4c3eb4123074f06b762b58fe7ffdb44613
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/156616
Tested-by: Jenkins
Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>

editeng/source/misc/acorrcfg.cxx
@@ -421,6 +421,7 @@ Sequence<OUString>  SvxSwAutoCorrCfg::GetPropertyNames()
         "Format/ByInput/ApplyNumbering/SpecialCharacter/FontCharset",   //45
         "Format/ByInput/ApplyNumbering/SpecialCharacter/FontPitch",     //46
         "Format/Option/SetDOIAttribute",                                //47
+        "Format/ByInput/ApplyNumberingAfterSpace",                      //48
     };
     const int nCount = 48;
     Sequence<OUString> aNames(nCount);
@@ -573,6 +574,7 @@ void SvxSwAutoCorrCfg::Load(bool bInit)
                 }
                 break;// "Format/ByInput/ApplyNumbering/SpecialCharacter/FontPitch",
                 case   47: rSwFlags.bSetDOIAttr = *o3tl::doAccess<bool>(pValues[nProp]); break; // "Format/Option/SetDOIAttribute",
+                case 48 : rSwFlags.bSetNumRuleAfterSpace = *o3tl::doAccess<bool>(pValues[nProp]); break; // "Format/ByInput/ApplyNumberingAfterSpace",
             }
         }
     }
@@ -643,6 +645,7 @@ void SvxSwAutoCorrCfg::ImplCommit()
          css::uno::Any(rParent.bAutoFmtByInput), // "Format/ByInput/Enable"
          css::uno::Any(rSwFlags.bChgToEnEmDash), // "Format/ByInput/ChangeDash"
          css::uno::Any(rSwFlags.bSetNumRule),
+         css::uno::Any(rSwFlags.bSetNumRuleAfterSpace),
             // "Format/ByInput/ApplyNumbering/Enable"
          css::uno::Any(rSwFlags.bSetBorder), // "Format/ByInput/ChangeToBorders"
          css::uno::Any(rSwFlags.bCreateTable), // "Format/ByInput/ChangeToTable"

cui/inc/strings.hrc
@@ -334,6 +334,7 @@
 // To translators: %1 will be replaced with a percentage, e.g. "10%"
 #define RID_CUISTR_RIGHT_MARGIN                     NC_("RID_SVXSTR_RIGHT_MARGIN", "Combine single line paragraphs if length greater than %1")
 #define RID_CUISTR_NUM                              NC_("RID_SVXSTR_NUM", "Bulleted and numbered lists. Bullet symbol: %1")
+#define RID_SVXSTR_NUM_FORMAT_AFTER_SPACE           NC_("RID_SVXSTR_NUM_FORMAT_AFTER_SPACE", "Bulleted and numbered lists immediate after pressing space")
 #define RID_CUISTR_BORDER                           NC_("RID_SVXSTR_BORDER", "Apply border")
 #define RID_CUISTR_CREATE_TABLE                     NC_("RID_SVXSTR_CREATE_TABLE", "Create table")
 #define RID_CUISTR_REPLACE_TEMPLATES                NC_("RID_SVXSTR_REPLACE_TEMPLATES", "Apply Styles")

cui/source/inc/autocdlg.hxx
@@ -97,6 +97,7 @@ class OfaSwAutoFmtOptionsPage : public SfxTabPage
     OUString        sDash;
     OUString        sRightMargin;
     OUString        sNum;
+    OUString        sBulletsAfterSpace;
     OUString        sBorder;
     OUString        sTable;
     OUString        sReplaceTemplates;

cui/source/tabpages/autocdlg.cxx
@@ -344,6 +344,7 @@ enum OfaAutoFmtOptions
     IGNORE_DBLSPACE,
     CORRECT_CAPS_LOCK,
     APPLY_NUMBERING,
+    APPLY_NUMBERING_AFTER_SPACE,
     INSERT_BORDER,
     CREATE_TABLE,
     REPLACE_STYLES,
@@ -372,6 +373,7 @@ OfaSwAutoFmtOptionsPage::OfaSwAutoFmtOptionsPage(weld::Container* pPage, weld::D
     , sDash(CuiResId(RID_CUISTR_DASH))
     , sRightMargin(CuiResId(RID_CUISTR_RIGHT_MARGIN))
     , sNum(CuiResId(RID_CUISTR_NUM))
+    , sBulletsAfterSpace(CuiResId(RID_SVXSTR_NUM_FORMAT_AFTER_SPACE))
     , sBorder(CuiResId(RID_CUISTR_BORDER))
     , sTable(CuiResId(RID_CUISTR_CREATE_TABLE))
     , sReplaceTemplates(CuiResId(RID_CUISTR_REPLACE_TEMPLATES))
@@ -498,6 +500,10 @@ bool OfaSwAutoFmtOptionsPage::FillItemSet( SfxItemSet*  )
     bModified |= pOpt->bSetNumRule != bCheck;
     pOpt->bSetNumRule = bCheck;
 
+    bCheck = m_xCheckLB->get_toggle(APPLY_NUMBERING_AFTER_SPACE, CBCOL_SECOND) == TRISTATE_TRUE;
+    bModified |= pOpt->bSetNumRuleAfterSpace != bCheck;
+    pOpt->bSetNumRuleAfterSpace = bCheck;
+
     bCheck = m_xCheckLB->get_toggle(INSERT_BORDER, CBCOL_SECOND) == TRISTATE_TRUE;
     bModified |= pOpt->bSetBorder != bCheck;
     pOpt->bSetBorder = bCheck;
@@ -578,6 +584,7 @@ void OfaSwAutoFmtOptionsPage::Reset( const SfxItemSet* )
     CreateEntry(sNoDblSpaces,       CBCOL_SECOND);
     CreateEntry(sCorrectCapsLock,   CBCOL_SECOND);
     CreateEntry(sNum.replaceFirst("%1", sBulletChar), CBCOL_SECOND);
+    CreateEntry(sBulletsAfterSpace,  CBCOL_SECOND);
     CreateEntry(sBorder,            CBCOL_SECOND);
     CreateEntry(sTable,             CBCOL_SECOND);
     CreateEntry(sReplaceTemplates,  CBCOL_SECOND);
@@ -607,6 +614,7 @@ void OfaSwAutoFmtOptionsPage::Reset( const SfxItemSet* )
     m_xCheckLB->set_toggle(IGNORE_DBLSPACE, bool(nFlags & ACFlags::IgnoreDoubleSpace) ? TRISTATE_TRUE : TRISTATE_FALSE, CBCOL_SECOND);
     m_xCheckLB->set_toggle(CORRECT_CAPS_LOCK, bool(nFlags & ACFlags::CorrectCapsLock) ? TRISTATE_TRUE : TRISTATE_FALSE, CBCOL_SECOND);
     m_xCheckLB->set_toggle(APPLY_NUMBERING, pOpt->bSetNumRule ? TRISTATE_TRUE : TRISTATE_FALSE, CBCOL_SECOND);
+    m_xCheckLB->set_toggle(APPLY_NUMBERING_AFTER_SPACE, pOpt->bSetNumRuleAfterSpace ? TRISTATE_TRUE : TRISTATE_FALSE, CBCOL_SECOND);
     m_xCheckLB->set_toggle(INSERT_BORDER, pOpt->bSetBorder ? TRISTATE_TRUE : TRISTATE_FALSE, CBCOL_SECOND);
     m_xCheckLB->set_toggle(CREATE_TABLE, pOpt->bCreateTable ? TRISTATE_TRUE : TRISTATE_FALSE, CBCOL_SECOND);
     m_xCheckLB->set_toggle(REPLACE_STYLES, pOpt->bReplaceStyles ? TRISTATE_TRUE : TRISTATE_FALSE, CBCOL_SECOND);

editeng/source/misc/swafopt.cxx
@@ -46,6 +46,7 @@ SvxSwAutoFormatFlags::SvxSwAutoFormatFlags()
     bDelEmptyNode =
     bWithRedlining =
     bAutoCmpltEndless =
+    bSetNumRuleAfterSpace =
     bAutoCmpltAppendBlank = false;
 
     bAutoCmpltShowAsTip =

include/editeng/swafopt.hxx
@@ -102,6 +102,7 @@ struct EDITENG_DLLPUBLIC SvxSwAutoFormatFlags
     bool bAFormatByInput : 1;
     bool bDelEmptyNode : 1;
     bool bSetNumRule : 1;
+    bool bSetNumRuleAfterSpace : 1;
 
     bool bChgOrdinalNumber : 1;
     bool bChgToEnEmDash : 1;

officecfg/registry/schema/org/openoffice/Office/Writer.xcs
@@ -4386,6 +4386,14 @@
             </info>
             <value>true</value>
           </prop>
+           <prop oor:name="ApplyBulletsAfterSpace" oor:type="xs:boolean" oor:nillable="false">
+            <!-- UIHints: Tools  AutoCorrect/AutoFormat  Options  Apply styles -->
+            <info>
+              <desc>Specifies whether a numbered or bulleted text is created automatically while typing * or - and then space</desc>
+              <label>Bulleted and numbered lists immediate after pressing space</label>
+            </info>
+            <value>false</value>
+          </prop>
           <group oor:name="ApplyNumbering">
             <info>
               <desc>Contains settings for automatic numbering.</desc>

sw/inc/editsh.hxx
@@ -843,7 +843,7 @@ public:
     std::vector<OUString> GetChunkForAutoText();
 
     /// Set our styles according to the respective rules.
-    void AutoFormat( const SvxSwAutoFormatFlags* pAFlags );
+    void AutoFormat( const SvxSwAutoFormatFlags* pAFlags, bool bCurrentParagraphOnly );
 
     static SvxSwAutoFormatFlags* GetAutoFormatFlags();
     static void SetAutoFormatFlags(SvxSwAutoFormatFlags const *);

commit ca33b8b35a10243dc13e68c93e7c7512eef937ec

tdf#159313: pass properties in correct order
Regression after commit c4fc18308074634e9578ad12d94d937f259aa22a
(Autocorrect: Add option for autoformat bulleted lists after space,
2023-09-07).

Change-Id: I35fafc1441b4f67886a86d4d67764a91146b8ece
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/162359
Tested-by: Jenkins
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

editeng/source/misc/acorrcfg.cxx
@@ -645,7 +645,6 @@ void SvxSwAutoCorrCfg::ImplCommit()
          css::uno::Any(rParent.bAutoFmtByInput), // "Format/ByInput/Enable"
          css::uno::Any(rSwFlags.bChgToEnEmDash), // "Format/ByInput/ChangeDash"
          css::uno::Any(rSwFlags.bSetNumRule),
-         css::uno::Any(rSwFlags.bSetNumRuleAfterSpace),
             // "Format/ByInput/ApplyNumbering/Enable"
          css::uno::Any(rSwFlags.bSetBorder), // "Format/ByInput/ChangeToBorders"
          css::uno::Any(rSwFlags.bCreateTable), // "Format/ByInput/ChangeToTable"
@@ -679,7 +678,9 @@ void SvxSwAutoCorrCfg::ImplCommit()
             // "Format/ByInput/ApplyNumbering/SpecialCharacter/FontCharset"
          css::uno::Any(sal_Int32(rSwFlags.aByInputBulletFont.GetPitch())),
             // "Format/ByInput/ApplyNumbering/SpecialCharacter/FontPitch"
-         css::uno::Any(rSwFlags.bSetDOIAttr)});
+         css::uno::Any(rSwFlags.bSetDOIAttr),
+         css::uno::Any(rSwFlags.bSetNumRuleAfterSpace), // "Format/ByInput/ApplyNumberingAfterSpace"
+        });
             // "Format/Option/SetDOIAttribute"
 }
 