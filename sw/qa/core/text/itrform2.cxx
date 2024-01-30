LibreOffice/core

commit 9bad5be0ffdcdee92d40162b598ed2ab2815e5d5

sw content controls, checkbox: add PDF export
Map this to vcl::PDFWriter::CheckBoxWidget, which uses the /FT/Btn
widget, i.e. a (toggle) button which is not a pushbutton or a
radiobutton).

Also avoid a border for content controls: it just cuts out some part of
the rich text/plain text content control filled in text + causes a
double border for checkboxes, so not useful.

Change-Id: If8dd63fe71d7c2ba9bea4bf2ecc6621fefb74fd6
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/139864
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

sw/source/core/text/itrform2.cxx
@@ -934,8 +934,19 @@ bool SwContentControlPortion::DescribePDFControl(const SwTextPaintInfo& rInf) co
     {
         case SwContentControlType::RICH_TEXT:
         case SwContentControlType::PLAIN_TEXT:
+        {
             pDescriptor = std::make_unique<vcl::PDFWriter::EditWidget>();
             break;
+        }
+        case SwContentControlType::CHECKBOX:
+        {
+            pDescriptor = std::make_unique<vcl::PDFWriter::CheckBoxWidget>();
+            auto pCheckBoxWidget = static_cast<vcl::PDFWriter::CheckBoxWidget*>(pDescriptor.get());
+            pCheckBoxWidget->Checked = pContentControl->GetChecked();
+            pCheckBoxWidget->OnValue = pContentControl->GetCheckedState();
+            pCheckBoxWidget->OffValue = pContentControl->GetUncheckedState();
+            break;
+        }
         default:
             break;
     }
@@ -944,9 +955,6 @@ bool SwContentControlPortion::DescribePDFControl(const SwTextPaintInfo& rInf) co
     {
         return false;
     }
-
-    pDescriptor->Border = true;
-    pDescriptor->BorderColor = COL_BLACK;
 
     SwRect aLocation;
     rInf.CalcRect(*this, &aLocation);

include/vcl/filter/PDFiumLibrary.hxx
@@ -33,6 +33,7 @@
 #include <vcl/pdf/PDFFillMode.hxx>
 #include <vcl/pdf/PDFFindFlags.hxx>
 #include <vcl/pdf/PDFErrorType.hxx>
+#include <vcl/pdf/PDFFormFieldType.hxx>
 
 class SvMemoryStream;
 
@@ -100,6 +101,7 @@ public:
     virtual size_t getAttachmentPointsCount() = 0;
     virtual std::vector<basegfx::B2DPoint> getAttachmentPoints(size_t nIndex) = 0;
     virtual std::vector<basegfx::B2DPoint> getLineGeometry() = 0;
+    virtual PDFFormFieldType getFormFieldType(PDFiumDocument* pDoc) = 0;
 };
 
 class PDFiumTextPage;

include/vcl/pdf/PDFFormFieldType.hxx
@@ -1,0 +1,29 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#pragma once
+
+namespace vcl::pdf
+{
+enum class PDFFormFieldType
+{
+    Unknown = 0,
+    PushButton = 1,
+    CheckBox = 2,
+    RadioButton = 3,
+    ComboBox = 4,
+    ListBox = 5,
+    TextField = 6,
+    Signature = 7
+};
+
+} // namespace vcl::pdf
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

include/vcl/pdfwriter.hxx
@@ -334,7 +334,7 @@ public:
         }
     };
 
-    struct CheckBoxWidget final : public AnyWidget
+    struct VCL_DLLPUBLIC CheckBoxWidget final : public AnyWidget
     {
         bool                Checked;
         OUString            OnValue; // the value of the checkbox if it is selected

sw/qa/core/text/text.cxx
@@ -656,6 +656,43 @@ CPPUNIT_TEST_FIXTURE(SwCoreTextTest, testContentControlPDF)
     CPPUNIT_ASSERT_EQUAL(1, pPage->getAnnotationCount());
 }
 
+CPPUNIT_TEST_FIXTURE(SwCoreTextTest, testCheckboxContentControlPDF)
+{
+    // Given a file with a checkbox content control:
+    SwDoc* pDoc = createSwDoc();
+    SwWrtShell* pWrtShell = pDoc->GetDocShell()->GetWrtShell();
+    pWrtShell->InsertContentControl(SwContentControlType::CHECKBOX);
+
+    // When exporting to PDF:
+    uno::Reference<frame::XStorable> xStorable(mxComponent, uno::UNO_QUERY);
+    utl::MediaDescriptor aMediaDescriptor;
+    aMediaDescriptor["FilterName"] <<= OUString("writer_pdf_Export");
+    xStorable->storeToURL(maTempFile.GetURL(), aMediaDescriptor.getAsConstPropertyValueList());
+
+    // Then make sure that a checkbox form widget is emitted:
+    SvFileStream aFile(maTempFile.GetURL(), StreamMode::READ);
+    SvMemoryStream aMemory;
+    aMemory.WriteStream(aFile);
+    std::shared_ptr<vcl::pdf::PDFium> pPDFium = vcl::pdf::PDFiumLibrary::get();
+    if (!pPDFium)
+    {
+        return;
+    }
+    std::unique_ptr<vcl::pdf::PDFiumDocument> pPdfDocument
+        = pPDFium->openDocument(aMemory.GetData(), aMemory.GetSize(), OString());
+    std::unique_ptr<vcl::pdf::PDFiumPage> pPage = pPdfDocument->openPage(0);
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 1
+    // - Actual  : 0
+    // i.e. the checkbox content control was just exported as normal text.
+    CPPUNIT_ASSERT_EQUAL(1, pPage->getAnnotationCount());
+    std::unique_ptr<vcl::pdf::PDFiumAnnotation> pAnnotation = pPage->getAnnotation(0);
+    CPPUNIT_ASSERT_EQUAL(vcl::pdf::PDFAnnotationSubType::Widget, pAnnotation->getSubType());
+    // Also check the form widget type:
+    CPPUNIT_ASSERT_EQUAL(vcl::pdf::PDFFormFieldType::CheckBox,
+                         pAnnotation->getFormFieldType(pPdfDocument.get()));
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

vcl/source/pdf/PDFiumLibrary.cxx
@@ -137,6 +137,24 @@ static_assert(static_cast<int>(vcl::pdf::PDFErrorType::Security) == FPDF_ERR_SEC
 static_assert(static_cast<int>(vcl::pdf::PDFErrorType::Page) == FPDF_ERR_PAGE,
               "PDFErrorType::Page value mismatch");
 
+static_assert(static_cast<int>(vcl::pdf::PDFFormFieldType::Unknown) == FPDF_FORMFIELD_UNKNOWN,
+              "PDFFormFieldType::Unknown value mismatch");
+static_assert(static_cast<int>(vcl::pdf::PDFFormFieldType::PushButton) == FPDF_FORMFIELD_PUSHBUTTON,
+              "PDFFormFieldType::PushButton value mismatch");
+static_assert(static_cast<int>(vcl::pdf::PDFFormFieldType::CheckBox) == FPDF_FORMFIELD_CHECKBOX,
+              "PDFFormFieldType::CheckBox value mismatch");
+static_assert(static_cast<int>(vcl::pdf::PDFFormFieldType::RadioButton)
+                  == FPDF_FORMFIELD_RADIOBUTTON,
+              "PDFFormFieldType::RadioButton value mismatch");
+static_assert(static_cast<int>(vcl::pdf::PDFFormFieldType::ComboBox) == FPDF_FORMFIELD_COMBOBOX,
+              "PDFFormFieldType::ComboBox value mismatch");
+static_assert(static_cast<int>(vcl::pdf::PDFFormFieldType::ListBox) == FPDF_FORMFIELD_LISTBOX,
+              "PDFFormFieldType::ListBox value mismatch");
+static_assert(static_cast<int>(vcl::pdf::PDFFormFieldType::TextField) == FPDF_FORMFIELD_TEXTFIELD,
+              "PDFFormFieldType::TextField value mismatch");
+static_assert(static_cast<int>(vcl::pdf::PDFFormFieldType::Signature) == FPDF_FORMFIELD_SIGNATURE,
+              "PDFFormFieldType::Signature value mismatch");
+
 namespace
 {
 /// Callback class to be used with FPDF_SaveWithVersion().
@@ -231,6 +249,7 @@ public:
     size_t getAttachmentPointsCount() override;
     std::vector<basegfx::B2DPoint> getAttachmentPoints(size_t nIndex) override;
     std::vector<basegfx::B2DPoint> getLineGeometry() override;
+    PDFFormFieldType getFormFieldType(PDFiumDocument* pDoc) override;
 };
 
 class PDFiumPageObjectImpl final : public PDFiumPageObject
@@ -1107,6 +1126,13 @@ std::vector<basegfx::B2DPoint> PDFiumAnnotationImpl::getLineGeometry()
         aLine.emplace_back(aEnd.x, aEnd.y);
     }
     return aLine;
+}
+
+PDFFormFieldType PDFiumAnnotationImpl::getFormFieldType(PDFiumDocument* pDoc)
+{
+    auto pDocImpl = static_cast<PDFiumDocumentImpl*>(pDoc);
+    return PDFFormFieldType(
+        FPDFAnnot_GetFormFieldType(pDocImpl->getFormHandlePointer(), mpAnnotation));
 }
 
 namespace

commit 256e2c679bcbb3ea446884d0ff4e3f8687b82ede

tdf#159452 sw content control, PDF export: fix checked checkboxes
Regression from commit 9bad5be0ffdcdee92d40162b598ed2ab2815e5d5 (sw
content controls, checkbox: add PDF export, 2022-09-13), we used to
export checkbox content controls as plain text, but once checkbox
content controls are exported as forms, the state of the checkboxes are
lost.

Writer content control checkboxes support custom values for the checked
and unchecked states, but the PDF export does not. On one hand,
PDFWriterImpl::createDefaultCheckBoxAppearance() assumes that the
checked state should be a checkmark, not the Writer default 'BALLOT BOX
WITH X' (U+2612). On the other hand, the PDF spec section 12.7.4.2.3
"Check Boxes" says that the checked state should be "Yes", which
explains why our checked state is not recognized by PDF readers.

Fix the problem by making the export of checked/unchecked states
conditional in SwContentControlPortion::DescribePDFControl(): the
checked state then shows up as expected.

Leave the unchecked case unchanged, the current markup there doesn't
cause problems.

Change-Id: I9063d8607c8cccfa080921af38b3cbfe40905115
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/162765
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

sw/source/core/text/itrform2.cxx
@@ -1018,8 +1018,13 @@ bool SwContentControlPortion::DescribePDFControl(const SwTextPaintInfo& rInf) co
             pDescriptor = std::make_unique<vcl::PDFWriter::CheckBoxWidget>();
             auto pCheckBoxWidget = static_cast<vcl::PDFWriter::CheckBoxWidget*>(pDescriptor.get());
             pCheckBoxWidget->Checked = pContentControl->GetChecked();
-            pCheckBoxWidget->OnValue = pContentControl->GetCheckedState();
-            pCheckBoxWidget->OffValue = pContentControl->GetUncheckedState();
+            // If it's checked already, then leave the default "Yes" OnValue unchanged, so the
+            // appropriate appearance is found by PDF readers.
+            if (!pCheckBoxWidget->Checked)
+            {
+                pCheckBoxWidget->OnValue = pContentControl->GetCheckedState();
+                pCheckBoxWidget->OffValue = pContentControl->GetUncheckedState();
+            }
             break;
         }
         case SwContentControlType::DROP_DOWN_LIST:

sw/qa/core/text/itrform2.cxx
@@ -16,6 +16,10 @@
 #include <sortedobjs.hxx>
 #include <pagefrm.hxx>
 #include <cntfrm.hxx>
+#include <docsh.hxx>
+#include <wrtsh.hxx>
+#include <formatcontentcontrol.hxx>
+#include <textcontentcontrol.hxx>
 
 namespace
 {
@@ -165,6 +169,42 @@ CPPUNIT_TEST_FIXTURE(Test, testSplitFlyAnchorLeftMargin)
     // - Actual  : 0
     // i.e. the left margin was lost.
     CPPUNIT_ASSERT_EQUAL(static_cast<SwTwips>(6480), pLastPara->getFramePrintArea().Left());
+}
+
+CPPUNIT_TEST_FIXTURE(Test, testCheckedCheckboxContentControlPDF)
+{
+    std::shared_ptr<vcl::pdf::PDFium> pPDFium = vcl::pdf::PDFiumLibrary::get();
+    if (!pPDFium)
+        return;
+
+    // Given a file with a checked checkbox content control:
+    createSwDoc();
+    SwDoc* pDoc = getSwDoc();
+    SwWrtShell* pWrtShell = pDoc->GetDocShell()->GetWrtShell();
+    pWrtShell->InsertContentControl(SwContentControlType::CHECKBOX);
+    // Toggle it, so we get a checked one:
+    SwTextContentControl* pTextContentControl = pWrtShell->CursorInsideContentControl();
+    const SwFormatContentControl& rFormatContentControl = pTextContentControl->GetContentControl();
+    pWrtShell->GotoContentControl(rFormatContentControl);
+
+    // When exporting to PDF:
+    save("writer_pdf_Export");
+
+    // Then make sure that a checked checkbox form widget is emitted:
+    std::unique_ptr<vcl::pdf::PDFiumDocument> pPdfDocument = parsePDFExport();
+    std::unique_ptr<vcl::pdf::PDFiumPage> pPage = pPdfDocument->openPage(0);
+    CPPUNIT_ASSERT_EQUAL(1, pPage->getAnnotationCount());
+    std::unique_ptr<vcl::pdf::PDFiumAnnotation> pAnnotation = pPage->getAnnotation(0);
+    CPPUNIT_ASSERT_EQUAL(vcl::pdf::PDFAnnotationSubType::Widget, pAnnotation->getSubType());
+    CPPUNIT_ASSERT_EQUAL(vcl::pdf::PDFFormFieldType::CheckBox,
+                         pAnnotation->getFormFieldType(pPdfDocument.get()));
+    OUString aActual = pAnnotation->getFormFieldValue(pPdfDocument.get());
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: Yes
+    // - Actual  : Off
+    // i.e. the /AP -> /N key of the checkbox widget annotation object didn't have a sub-key that
+    // would match /V, leading to not showing the checked state.
+    CPPUNIT_ASSERT_EQUAL(OUString("Yes"), aActual);
 }
 }
 