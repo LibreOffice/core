LibreOffice/core

commit 4d7a98b582dc70bbffc78e6622969e218f108433

Related tdf#58434 - Treat ZWSP and WJ as hard space
U+200B ZERO WIDTH SPACE and U+2060 WORD JOINER are not highlighted
anymore if Formatting Aids > Non-breaking spaces is off

Change-Id: Id35fb725bbfa14683ceb31d2043848a66328b364
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/155019
Tested-by: Heiko Tietze <heiko.tietze@documentfoundation.org>
Reviewed-by: خالد حسني <khaled@libreoffice.org>
Reviewed-by: Heiko Tietze <heiko.tietze@documentfoundation.org>

sw/source/core/text/inftxt.cxx
@@ -1494,6 +1494,7 @@ void SwTextPaintInfo::DrawViewOpt( const SwLinePortion &rPor,
         {
             bDraw = PortionType::Footnote != nWhich || m_pFrame->IsFootnoteAllowed();
         }
+        bDraw &= GetOpt().IsHardBlank();
         break;
     case PortionType::Bookmark:
         // no shading

commit 788ebd183b095ffb7369c4d518acd14bad72ae82

Resolves tdf#159328 - Fix field shading regression
Introduced by 4d7a98b582dc70bbffc78e6622969e218f108433

Change-Id: Ibcc057e47c62d53dfa9e6410230c4b231df8551c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/162799
Tested-by: Jenkins
Reviewed-by: Heiko Tietze <heiko.tietze@documentfoundation.org>

sw/source/core/text/inftxt.cxx
@@ -1426,8 +1426,8 @@ void SwTextPaintInfo::DrawViewOpt( const SwLinePortion &rPor,
                   || m_pFrame->GetTextNodeForParaProps()->HasMarkedLabel())) // #i27615#
         {
             bDraw = PortionType::Footnote != nWhich || m_pFrame->IsFootnoteAllowed();
+            bDraw &= GetOpt().IsHardBlank();
         }
-        bDraw &= GetOpt().IsHardBlank();
         break;
     case PortionType::Bookmark:
         // no shading