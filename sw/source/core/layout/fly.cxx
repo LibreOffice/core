LibreOffice/core

commit 191babee4f0ec643b80e96b0cd98c2d04ff96e4e

sw: fix layout crash in SwObjectFormatterTextFrame::DoFormatObj()
Since commit 15b886f460919ea3dce425a621dc017c2992a96b this is happening
in testForcepoint93, but only on WNT.

The crash is in SwObjectFormatterTextFrame::DoFormatObj() calling
SwObjectFormatterTextFrame::InvalidateFollowObjs() and the mrPageFrame
has been deleted; there is also the m_bAgain flag set in the SwLayAction
up the stack, but the SwObjectFormatter::mpLayAction is null so
DoFormatObj() cannot check this condition.

In the caller CalcContent(), get the SwLayAction from the SwViewShell
and pass it to the SwObjectFormatter, which appears to fix the problem.

Change-Id: Ib3dc34ce39b4aad723251271c24bdd7a6598ab1c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/159518
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/core/layout/fly.cxx
@@ -1566,7 +1566,8 @@ void SwFlyFrame::Format( vcl::RenderContext* /*pRenderContext*/, const SwBorderA
 //                          Thus, <bNoCalcFollow> no longer used by <FormatWidthCols(..)>.
 void CalcContent( SwLayoutFrame *pLay, bool bNoColl )
 {
-    vcl::RenderContext* pRenderContext = pLay->getRootFrame()->GetCurrShell()->GetOut();
+    SwViewShell & rShell(*pLay->getRootFrame()->GetCurrShell());
+    vcl::RenderContext* pRenderContext = rShell.GetOut();
     SwSectionFrame* pSect;
     bool bCollect = false;
     if( pLay->IsSctFrame() )
@@ -1727,7 +1728,8 @@ void CalcContent( SwLayoutFrame *pLay, bool bNoColl )
                             }
                         }
 
-                        if ( !SwObjectFormatter::FormatObj( *pAnchoredObj, pAnchorFrame, pAnchorPageFrame ) )
+                        if (!SwObjectFormatter::FormatObj(*pAnchoredObj, pAnchorFrame, pAnchorPageFrame,
+                                rShell.Imp()->IsAction() ? &rShell.Imp()->GetLayAction() : nullptr))
                         {
                             bRestartLayoutProcess = true;
                             break;

sw/qa/extras/layout/layout.cxx
@@ -3480,15 +3480,12 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testForcepoint91) { createSwWebDoc("forcepo
 //just care it doesn't crash/assert
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testForcepoint92) { createSwDoc("forcepoint92.doc"); }
 
-#ifndef _MSC_VER
-//FIXME: crashes only on WNT with RTF import changes - debug next week
 //just care it doesn't crash/assert
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testForcepoint93)
 {
     createSwDoc("forcepoint93-1.rtf");
     createSwDoc("forcepoint93-2.rtf");
 }
-#endif
 
 //just care it doesn't crash/assert
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testForcepoint94) { createSwWebDoc("forcepoint94.html"); }

sw/source/core/inc/objectformatter.hxx
@@ -166,7 +166,8 @@ class SwObjectFormatter
         */
         static bool FormatObj( SwAnchoredObject& _rAnchoredObj,
                                SwFrame* _pAnchorFrame = nullptr,
-                               const SwPageFrame* _pPageFrame = nullptr );
+                               const SwPageFrame* _pPageFrame = nullptr,
+                               SwLayAction* pLayAction = nullptr );
 };
 
 #endif

sw/source/core/layout/objectformatter.cxx
@@ -167,7 +167,8 @@ bool SwObjectFormatter::FormatObjsAtFrame( SwFrame& _rAnchorFrame,
 */
 bool SwObjectFormatter::FormatObj( SwAnchoredObject& _rAnchoredObj,
                                    SwFrame* _pAnchorFrame,
-                                   const SwPageFrame* _pPageFrame )
+                                   const SwPageFrame* _pPageFrame,
+                                   SwLayAction * pLayAction)
 {
     bool bSuccess( true );
 
@@ -181,7 +182,7 @@ bool SwObjectFormatter::FormatObj( SwAnchoredObject& _rAnchoredObj,
 
     // create corresponding object formatter
     std::unique_ptr<SwObjectFormatter> pObjFormatter =
-        SwObjectFormatter::CreateObjFormatter( rAnchorFrame, rPageFrame, nullptr/*_pLayAction*/ );
+        SwObjectFormatter::CreateObjFormatter(rAnchorFrame, rPageFrame, pLayAction);
 
     if ( pObjFormatter )
     {

commit 13865c4da30a3a2a5ab8efbd568c47952d3ffcee

tdf#159015 sw: layout: fix infinite loop in ::CalcContent()
SwObjectFormatter::FormatObj() will just return if IsAgain() is set, so
the while loop here won't make any progress.

(regression from commit 191babee4f0ec643b80e96b0cd98c2d04ff96e4e)

Change-Id: I1c194b148760ae05cf4dee1d5729be28e87d6ba0
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/162880
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/core/layout/fly.cxx
@@ -43,6 +43,7 @@
 #include <svx/svdoashp.hxx>
 #include <svx/svdpage.hxx>
 #include <layouter.hxx>
+#include <layact.hxx>
 #include <pagefrm.hxx>
 #include <rootfrm.hxx>
 #include <viewimp.hxx>
@@ -1741,6 +1742,10 @@ void CalcContent( SwLayoutFrame *pLay, bool bNoColl )
                         if (!SwObjectFormatter::FormatObj(*pAnchoredObj, pAnchorFrame, pAnchorPageFrame,
                                 rShell.Imp()->IsAction() ? &rShell.Imp()->GetLayAction() : nullptr))
                         {
+                            if (rShell.Imp()->IsAction() && rShell.Imp()->GetLayAction().IsAgain())
+                            {   // tdf#159015 will always fail, don't loop
+                                return;
+                            }
                             bRestartLayoutProcess = true;
                             break;
                         }