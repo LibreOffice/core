7d392796df7ee52279285cfe67d800c3aa3cbe61
Stephan Bergmann <stephan.bergmann@allotropia.de>
2024-02-06 13:27:59 +0100

    external/zxcvbn-c: There is no std::basic_string<int>
    ...and at least LLVM 19 trunk libc++ complains about it now since
    <c3668779c13596e223c26fbd49670d18cd638c40> "[libc++] Remove deprecated
    char_traits base template (#72694)" with
    
    > In file included from dict-generate.cpp:25:
    > In file included from ~/llvm/inst/bin/../include/c++/v1/iostream:43:
    > In file included from ~/llvm/inst/bin/../include/c++/v1/ios:223:
    > In file included from ~/llvm/inst/bin/../include/c++/v1/__locale:24:
    > ~/llvm/inst/bin/../include/c++/v1/string:746:43: error: implicit instantiation of undefined template 'std::char_traits<int>'
    >   746 |   static_assert((is_same<_CharT, typename traits_type::char_type>::value),
    >       |                                           ^
    > dict-generate.cpp:861:18: note: in instantiation of template class 'std::basic_string<int>' requested here
    >   861 |     StringOfInts Chld;
    >       |                  ^
    > ~/llvm/inst/bin/../include/c++/v1/__fwd/string.h:23:29: note: template is declared here
    >    23 | struct _LIBCPP_TEMPLATE_VIS char_traits;
    >       |                             ^
    
    etc., so use a std::vector<int> instead
    
    Change-Id: I51e8296edf7b16925ff01679e671525256055552
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/163048
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <stephan.bergmann@allotropia.de>

--- external/zxcvbn-c/UnpackedTarball_zxcvbn-c.mk
+++ external/zxcvbn-c/UnpackedTarball_zxcvbn-c.mk
@@ -11,8 +11,11 @@ $(eval $(call gb_UnpackedTarball_UnpackedTarball,zxcvbn-c))
 
 $(eval $(call gb_UnpackedTarball_set_tarball,zxcvbn-c,$(ZXCVBN_C_TARBALL)))
 
+# * external/zxcvbn-c/0001-There-is-no-std-basic_string-int.patch.1 sent upstream as
+#   <https://github.com/tsyrogit/zxcvbn-c/pull/31> "There is no std::basic_string<int>":
 $(eval $(call gb_UnpackedTarball_add_patches,zxcvbn-c,\
 	external/zxcvbn-c/zxcvbn-c-2.5-do-not-use-stdafx.patch \
+	external/zxcvbn-c/0001-There-is-no-std-basic_string-int.patch.1 \
 ))
 
 # vim: set noet sw=4 ts=4:
