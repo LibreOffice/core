LibreOffice/core

commit be53f32655973c7a18824d5145eed992be788d2f

rename vcl::Window::Update to PaintImmediately
To make the code easier to read.

Change-Id: Iebc648150391939fba5d1cd815c72dbcf02ceec6
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/90378
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/source/treelist/svimpbox.cxx
@@ -308,7 +308,7 @@ IMPL_LINK( SvImpLBox, ScrollUpDownHdl, ScrollBar *, pScrollBar, void )
     if( m_pView->IsEditingActive() )
     {
         m_pView->EndEditing( true ); // Cancel
-        m_pView->Update();
+        m_pView->PaintImmediately();
     }
     BeginScroll();
 
@@ -341,11 +341,11 @@ void SvImpLBox::CursorDown()
     {
         m_nFlags &= ~LBoxFlags::Filling;
         ShowCursor( false );
-        m_pView->Update();
+        m_pView->PaintImmediately();
         m_pStartEntry = pNextFirstToDraw;
         tools::Rectangle aArea( GetVisibleArea() );
         m_pView->Scroll( 0, -(m_pView->GetEntryHeight()), aArea, ScrollFlags::NoChildren );
-        m_pView->Update();
+        m_pView->PaintImmediately();
         ShowCursor( true );
         m_pView->NotifyScrolled();
     }
@@ -363,12 +363,12 @@ void SvImpLBox::CursorUp()
     m_nFlags &= ~LBoxFlags::Filling;
     long nEntryHeight = m_pView->GetEntryHeight();
     ShowCursor( false );
-    m_pView->Update();
+    m_pView->PaintImmediately();
     m_pStartEntry = pPrevFirstToDraw;
     tools::Rectangle aArea( GetVisibleArea() );
     aArea.AdjustBottom( -nEntryHeight );
     m_pView->Scroll( 0, nEntryHeight, aArea, ScrollFlags::NoChildren );
-    m_pView->Update();
+    m_pView->PaintImmediately();
     ShowCursor( true );
     m_pView->NotifyScrolled();
 }
@@ -390,22 +390,21 @@ void SvImpLBox::PageDown( sal_uInt16 nDelta )
     ShowCursor( false );
 
     m_nFlags &= ~LBoxFlags::Filling;
-    m_pView->Update();
     m_pStartEntry = pNext;
 
     if( nRealDelta >= m_nVisibleCount )
     {
         m_pView->Invalidate( GetVisibleArea() );
-        m_pView->Update();
+        m_pView->PaintImmediately();
     }
     else
     {
         tools::Rectangle aArea( GetVisibleArea() );
         long nScroll = m_pView->GetEntryHeight() * static_cast<long>(nRealDelta);
         nScroll = -nScroll;
-        m_pView->Update();
+        m_pView->PaintImmediately();
         m_pView->Scroll( 0, nScroll, aArea, ScrollFlags::NoChildren );
-        m_pView->Update();
+        m_pView->PaintImmediately();
         m_pView->NotifyScrolled();
     }
 
@@ -428,20 +427,19 @@ void SvImpLBox::PageUp( sal_uInt16 nDelta )
     m_nFlags &= ~LBoxFlags::Filling;
     ShowCursor( false );
 
-    m_pView->Update();
     m_pStartEntry = pPrev;
     if( nRealDelta >= m_nVisibleCount )
     {
         m_pView->Invalidate( GetVisibleArea() );
-        m_pView->Update();
+        m_pView->PaintImmediately();
     }
     else
     {
         long nEntryHeight = m_pView->GetEntryHeight();
         tools::Rectangle aArea( GetVisibleArea() );
-        m_pView->Update();
+        m_pView->PaintImmediately();
         m_pView->Scroll( 0, nEntryHeight*nRealDelta, aArea, ScrollFlags::NoChildren );
-        m_pView->Update();
+        m_pView->PaintImmediately();
         m_pView->NotifyScrolled();
     }
 
@@ -706,7 +704,7 @@ IMPL_LINK( SvImpLBox, ScrollLeftRightHdl, ScrollBar *, pScrollBar, void )
         if( m_pView->IsEditingActive() )
         {
             m_pView->EndEditing( true ); // Cancel
-            m_pView->Update();
+            m_pView->PaintImmediately();
         }
         m_pView->nFocusWidth = -1;
         KeyLeftRight( nDelta );
@@ -716,7 +714,7 @@ IMPL_LINK( SvImpLBox, ScrollLeftRightHdl, ScrollBar *, pScrollBar, void )
 void SvImpLBox::KeyLeftRight( long nDelta )
 {
     if( !(m_nFlags & LBoxFlags::InResize) )
-        m_pView->Update();
+        m_pView->PaintImmediately();
     BeginScroll();
     m_nFlags &= ~LBoxFlags::Filling;
     ShowCursor( false );
@@ -1251,7 +1249,7 @@ void SvImpLBox::AdjustScrollBars( Size& rSize )
         if( m_pView->IsEditingActive() )
         {
             m_pView->EndEditing( true ); // Cancel
-            m_pView->Update();
+            m_pView->PaintImmediately();
         }
         m_pView->nFocusWidth = -1;
         KeyLeftRight( nTemp );
@@ -1837,7 +1835,7 @@ void SvImpLBox::EntryInserted( SvTreeListEntry* pEntry )
     ShowVerSBar();
     ShowCursor( true );
     if( m_pStartEntry != m_pView->First() && (m_nFlags & LBoxFlags::Filling) )
-        m_pView->Update();
+        m_pView->PaintImmediately();
 }
 
 
@@ -3248,7 +3246,7 @@ IMPL_LINK(SvImpLBox, MyUserEvent, void*, pArg, void )
     if( !pArg )
     {
         m_pView->Invalidate();
-        m_pView->Update();
+        m_pView->PaintImmediately();
     }
     else
     {

commit 30fb8cde7579fb5da15c1d8da9611198f2f5ce78

tdf#159641 TreeView repaint problem with PgUp in kf5/gen
regression from
    commit be53f32655973c7a18824d5145eed992be788d2f
    Author: Noel Grandin <noel.grandin@collabora.co.uk>
    Date:   Thu Mar 12 13:55:55 2020 +0200
    rename vcl::Window::Update to PaintImmediately

Change-Id: I74fc87e984e1f085d2351cbae033e51920608e3d
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/163154
Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>
Tested-by: Jenkins

vcl/source/treelist/svimpbox.cxx
@@ -387,6 +387,7 @@ void SvImpLBox::PageDown( sal_uInt16 nDelta )
     ShowCursor( false );
 
     m_nFlags &= ~LBoxFlags::Filling;
+    m_pView->PaintImmediately();
     m_pStartEntry = pNext;
 
     if( nRealDelta >= m_nVisibleCount )
@@ -424,6 +425,7 @@ void SvImpLBox::PageUp( sal_uInt16 nDelta )
     m_nFlags &= ~LBoxFlags::Filling;
     ShowCursor( false );
 
+    m_pView->PaintImmediately();
     m_pStartEntry = pPrev;
     if( nRealDelta >= m_nVisibleCount )
     {