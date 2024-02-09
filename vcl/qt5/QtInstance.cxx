49111fe70fec5a5f074e6c1b1dd60f6b57139a25
Michael Weghorn <m.weghorn@posteo.de>
2024-02-09 21:57:11 +0100

    qt: Set desktop file name without ".desktop" suffix
    This fixes this warning when starting LO with the qt6
    VCL plugin on Wayland (using a local qtbase dev build):
    
        > QGuiApplication::setDesktopFileName: the specified desktop file name ends with .desktop.
        > For compatibility reasons, the .desktop suffix will be removed. Please specify a desktop file name without .desktop suffix
    
    The icon is also still shown fine with qt5/kf5
    (qtbase at 5.15.10+dfsg-6 from Debian testing).
    
    Change-Id: I0eca1b3767ff65653d5f6b3104550fb0d8aea293
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/163204
    Tested-by: Jenkins
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>

--- vcl/qt5/QtInstance.cxx
+++ vcl/qt5/QtInstance.cxx
@@ -310,7 +310,7 @@ void QtInstance::AfterAppInit()
     // set the default application icon via desktop file just on Wayland,
     // as this otherwise overrides the individual desktop icons on X11.
     if (QGuiApplication::platformName() == "wayland")
-        QGuiApplication::setDesktopFileName(QStringLiteral("libreoffice-startcenter.desktop"));
+        QGuiApplication::setDesktopFileName(QStringLiteral("libreoffice-startcenter"));
     QGuiApplication::setLayoutDirection(AllSettings::GetLayoutRTL() ? Qt::RightToLeft
                                                                     : Qt::LeftToRight);
 }
