582ef812702413dbe7fb0f132bca3e3e4c2e1d40
Michael Stahl <michael.stahl@allotropia.de>
2024-02-09 17:51:03 +0100

    tdf#158983 writerfilter: RTF import: fix page breaks and shape anchors
    Somehow, not sure why, the added import of \wrapdefault in commit
    86c0f58b6f9f392865196606173d1b98a6897f32 caused the page break in
    fdo55504-1.rtf to get lost.
    
    The first problem is that there is a \sbknone before the first \sect -
    this should not have any effect before \sect because \sbk* affect the
    *previous* section break, but it's not an option to simply ignore it
    (even if it works for this bugdoc) because it may be that there is no
    \sectd after \sect and then it will have an effect for the later
    section.
    
    The problem was in handling \page: here the premature \sbknone caused a
    sectBreak() which ate the page break; ignore it here by checking
    m_bHadSect.
    
    The second problem then was that now all but the first shape were
    anchored on page 2.
    
    This was because RTFDocumentImpl::beforePopState() for \shape of the 1st
    shape called parBreak() and that set the bIsFirstParaInSection flag.
    
    This flag prevented DomainMapper::lcl_utext() in the
    "if (m_pImpl->isBreakDeferred(PAGE_BREAK)) if (GetSplitPgBreakAndParaMark())"
    branch from inserting another paragraph break that is necessary to
    preserve the already inserted shapes anchored to the 2nd paragraph on
    page 1.
    
    (This is how it works for the equivalent DOCX document, with settings.xml
    edited to add w:splitPgBreakAndParaMark and remove "compatibilityMode"
    etc. because Word 2013 doesn't set these correctly.)
    
    The consequence is that when the second SwTextNode is converted to a
    text frame, all the shape anchors move to the next paragraph, the one
    with the RES_BREAK on it.
    
    Fix this by limiting the parBreak() handling in
    RTFDocumentImpl::beforePopState() to when the shape is a SwGrfNode,
    which is the scenario in the commit 0d9132c5046e15540abc20e45d64080708626441
    "fdo#47036 fix RTF import of shapes inside text frames at the start of the doc"
    - the testFdo47036 fails if the block is removed completely.
    
    This caused 2 test failures, but both cases look the same as in Word
    2013 now:
    
      Test name: (anonymous namespace)::testTdf158826_extraCR::Load_Verify_Reload_Verify
      An uncaught exception of type com.sun.star.uno.RuntimeException
      - unsatisfied query for interface of type com.sun.star.text.XTextTable!
    
      rtfexport2.cxx:537:Assertion
      Test name: (anonymous namespace)::testFdo47495::Load_Verify_Reload_Verify
      equality assertion failed
      - Expected: 2
      - Actual  : 1
    
    Change-Id: I43fa9431721650a6d748d1f4bda9aeaa7a9c6b45
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/163200
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

--- sw/qa/extras/rtfexport/rtfexport8.cxx
+++ sw/qa/extras/rtfexport/rtfexport8.cxx
@@ -109,6 +109,27 @@ DECLARE_RTFEXPORT_TEST(testTdf158586_lostFrame, "tdf158586_lostFrame.rtf")
     CPPUNIT_ASSERT_EQUAL(2, getPages());
 }
 
+DECLARE_RTFEXPORT_TEST(testTdf158983, "fdo55504-1-min.rtf")
+{
+    // the problem was that the page break was missing and the shapes were
+    // all anchored to the same node
+
+    const auto& pLayout = parseLayoutDump();
+    assertXPath(pLayout, "/root/page[1]/body/section/txt"_ostr, 1);
+    assertXPath(pLayout, "/root/page[1]/body/section/txt/anchored/fly"_ostr, 1);
+    // Word shows these shapes anchored in the fly, not body, but at least they are not lost
+    assertXPath(pLayout, "/root/page[1]/body/section/txt/anchored/SwAnchoredDrawObject"_ostr, 2);
+    // page break, paragraph break, section break.
+    assertXPath(pLayout, "/root/page[2]/body/section[1]/txt"_ostr, 1);
+    assertXPath(pLayout, "/root/page[2]/body/section[1]/txt/anchored"_ostr, 0);
+    assertXPath(pLayout, "/root/page[2]/body/section[2]/txt"_ostr, 1);
+    assertXPath(pLayout, "/root/page[2]/body/section[2]/txt/anchored/fly"_ostr, 1);
+    // Word shows these shapes anchored in the fly, not body, but at least they are not lost
+    assertXPath(pLayout, "/root/page[2]/body/section[2]/txt/anchored/SwAnchoredDrawObject"_ostr, 2);
+
+    CPPUNIT_ASSERT_EQUAL(2, getPages());
+}
+
 DECLARE_RTFEXPORT_TEST(testAnnotationPar, "tdf136445-1-min.rtf")
 {
     // the problem was that the paragraph break following annotation was missing
@@ -130,8 +151,8 @@ DECLARE_RTFEXPORT_TEST(testTdf158826_extraCR, "tdf158826_extraCR.rtf")
     // The page break defined before the document content should not cause a page break
     CPPUNIT_ASSERT_EQUAL(1, getPages());
 
-    // There is a two-column floating table [that SHOULD be getParagraphOrTable(1)]
-    uno::Reference<text::XTextTable> xTable(getParagraphOrTable(2), uno::UNO_QUERY_THROW);
+    // There is a two-column floating table
+    uno::Reference<text::XTextTable> xTable(getParagraphOrTable(1), uno::UNO_QUERY_THROW);
 }
 
 DECLARE_RTFEXPORT_TEST(testTdf158830, "tdf158830.rtf")
