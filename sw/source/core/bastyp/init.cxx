dd294936fd50abea99540e69091c120ba0e2ef64
Andrea Gelmini <andrea.gelmini@gelma.net>
2024-02-13 18:41:27 +0100

    Fix typo
    Change-Id: Iacd32b9b7556e706b8bfdcce1738ec3925366020
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/163319
    Tested-by: Julien Nabet <serval2412@yahoo.fr>
    Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

--- sw/source/core/bastyp/init.cxx
+++ sw/source/core/bastyp/init.cxx
@@ -337,7 +337,7 @@ ItemInfoPackage& getItemInfoPackageSwAttributes()
             { RES_TXTATR_AUTOFMT, new SwFormatAutoFormat,  0, SFX_ITEMINFOFLAG_NONE },
 
             // We cannot yet create the SwFormatINetFormat, that would crash since SW_MOD
-            // is not initialized and the translated ressource strings would not be available.
+            // is not initialized and the translated resource strings would not be available.
             // Luckily this mechanism allows to also flag this ItemInfo as 'incomplete' using
             // a nullptr as ItemPtr and implementing on-demand creation, see ::getItemInfo
             { RES_TXTATR_INETFMT, nullptr,  FN_TXTATR_INET, SFX_ITEMINFOFLAG_SUPPORT_SURROGATE },
