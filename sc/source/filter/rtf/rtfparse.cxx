LibreOffice/core

commit ed24564ce11683731b820c29d5a46e073ab7a2a7

SV_DECL_VARARR_SORT(ScRTFColTwips) o3tl::sorted_vector
Change-Id: I6d048af2b32ad02952cfe91d155ca17f5067ecee

sc/source/filter/rtf/rtfparse.cxx
@@ -46,9 +46,6 @@
 
 
 
-SV_IMPL_VARARR_SORT( ScRTFColTwips, sal_uLong );
-
-
 
 ScRTFParser::ScRTFParser( EditEngine* pEditP ) :
         ScEEParser( pEditP ),
@@ -126,12 +123,13 @@ inline void ScRTFParser::NextRow()
 
 sal_Bool ScRTFParser::SeekTwips( sal_uInt16 nTwips, SCCOL* pCol )
 {
-    sal_uInt16 nPos;
-    sal_Bool bFound = pColTwips->Seek_Entry( nTwips, &nPos );
+    ScRTFColTwips::const_iterator it = pColTwips->find( nTwips );
+    sal_Bool bFound = it != pColTwips->end();
+    sal_uInt16 nPos = it - pColTwips->end();
     *pCol = static_cast<SCCOL>(nPos);
     if ( bFound )
         return sal_True;
-    sal_uInt16 nCount = pColTwips->Count();
+    sal_uInt16 nCount = pColTwips->size();
     if ( !nCount )
         return false;
     SCCOL nCol = *pCol;
@@ -173,7 +171,7 @@ void ScRTFParser::ColAdjust()
                 nColMax = nCol;
         }
         nStartAdjust = (sal_uLong)~0;
-        pColTwips->Remove( (sal_uInt16)0, pColTwips->Count() );
+        pColTwips->clear();
     }
 }
 
@@ -252,7 +250,7 @@ void ScRTFParser::NewCellRow( ImportInfo* /*pInfo*/ )
             const ScRTFCellDefault& rD = maDefaultList[i];
             SCCOL nCol;
             if ( !SeekTwips(rD.nTwips, &nCol) )
-                pColTwips->Insert( rD.nTwips );
+                pColTwips->insert( rD.nTwips );
         }
     }
     pDefMerge = NULL;

sc/source/filter/inc/rtfparse.hxx
@@ -33,6 +33,7 @@
 
 #ifdef SC_RTFPARSE_CXX
 #include <boost/ptr_container/ptr_vector.hpp>
+#include <o3tl/sorted_vector.hxx>
 
 struct ScRTFCellDefault
 {
@@ -46,7 +47,7 @@ struct ScRTFCellDefault
 };
 
 // deswegen ULONG, typedef bringt's auch nicht :-(
-SV_DECL_VARARR_SORT( ScRTFColTwips, sal_uLong, 16 )
+class ScRTFColTwips : public o3tl::sorted_vector<sal_uLong> {};
 
 #else       // SC_RTFPARSE_CXX
 

commit 4df426e429e1aed92f074d8acd3ba3a5ec335ba9

tdf#101313 Copy-paste table from writer to calc
With Merged Cells from Writer to Calc,
Cells Placed in Wrong Position (Wrong cell offsets).

regression from
    commit ed24564ce11683731b820c29d5a46e073ab7a2a7
    Author: Noel Grandin <noel@peralex.com>
    Date:   Thu Jul 19 15:22:31 2012 +0200
    SV_DECL_VARARR_SORT(ScRTFColTwips) o3tl::sorted_vector

Change-Id: I8c90c19f6a27a368fd5807b3eaab84ce820e26e5
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/163518
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sc/source/filter/rtf/rtfparse.cxx
@@ -101,8 +101,8 @@ inline void ScRTFParser::NextRow()
 
 bool ScRTFParser::SeekTwips( sal_uInt16 nTwips, SCCOL* pCol )
 {
-    ScRTFColTwips::const_iterator it = aColTwips.find( nTwips );
-    bool bFound = it != aColTwips.end();
+    ScRTFColTwips::const_iterator it = aColTwips.lower_bound( nTwips );
+    bool bFound = it != aColTwips.end() && *it == nTwips;
     sal_uInt16 nPos = it - aColTwips.begin();
     *pCol = static_cast<SCCOL>(nPos);
     if ( bFound )