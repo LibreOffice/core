LibreOffice/core

commit 868140fcc1311259b9d5f666637b33d226511a53

tdf#60558 sw floattable: allow wrap of table on the right of a floattable
The bugdoc shows that Word wraps inline tables around floating tables if
they have enough space, but Writer didn't do this.

Table frames may wrap fly frames by adding some top, left or right
margin to the table at a layout level, this is calculated in
SwTabFrame::CalcFlyOffsets(). There we currently decide the give a top
margin to such tables, which fixes the overlap problem, but the bugdoc
is now of 2 pages instead of 1 page, since we don't wrap.

Fix the problem by improving the "shift down" case by checking if
shifting to the right would also work (has enough space). If so, do that
in case the fly frame is a split fly.

Note that this could be done for all flys as well, but that would have
to be conditional on some Word compat flag and that's not needed to fix
the bugdoc, so leave that for later.

Change-Id: Idb45413257758fd0334b17ef348ba28010a52316
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/160331
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

sw/source/core/layout/tabfrm.cxx
@@ -3179,6 +3179,7 @@ bool SwTabFrame::CalcFlyOffsets( SwTwips& rUpper,
             nSurround = text::WrapTextMode_PARALLEL;
 
         bool bShiftDown = css::text::WrapTextMode_NONE == nSurround;
+        bool bSplitFly = pFly->IsFlySplitAllowed();
         if (!bShiftDown && bAddVerticalFlyOffsets)
         {
             if (nSurround == text::WrapTextMode_PARALLEL && isHoriOrientShiftDown)
@@ -3201,6 +3202,18 @@ bool SwTabFrame::CalcFlyOffsets( SwTwips& rUpper,
                 // normally an SwFlyPortion is created instead that increases the height
                 // of the first table row.
                 bShiftDown = aTabRange.overlaps(aFlyRange);
+
+                if (bSplitFly && pFly->GetAnchorFrame()->GetUpper() == GetUpper())
+                {
+                    // Split fly followed by an inline table. Check if we have enough space to shift
+                    // to the right instead.
+                    SwTwips nShiftedTabRight = aFlyRectWithoutSpaces.Right() + getFramePrintArea().Width();
+                    SwTwips nRightShiftDeadline = pFly->GetAnchorFrame()->GetUpper()->getFrameArea().Right();
+                    if (aRectFnSet.XDiff(nRightShiftDeadline, nShiftedTabRight) >= 0)
+                    {
+                        bShiftDown = false;
+                    }
+                }
             }
         }
 
@@ -3241,9 +3254,16 @@ bool SwTabFrame::CalcFlyOffsets( SwTwips& rUpper,
             }
         }
 
+        bool bFlyHoriOrientLeft = text::HoriOrientation::LEFT == rHori.GetHoriOrient();
+        if (bSplitFly && !bFlyHoriOrientLeft)
+        {
+            // If a split fly is oriented "from left", we already checked if it has enough space on
+            // the right, so from-left and left means the same here.
+            bFlyHoriOrientLeft = rHori.GetHoriOrient() == text::HoriOrientation::NONE;
+        }
         if ((css::text::WrapTextMode_RIGHT == nSurround
              || css::text::WrapTextMode_PARALLEL == nSurround)
-            && text::HoriOrientation::LEFT == rHori.GetHoriOrient()
+            && bFlyHoriOrientLeft
             && !bShiftDown)
         {
             const tools::Long nWidth

sw/qa/core/layout/tabfrm.cxx
@@ -169,6 +169,36 @@ CPPUNIT_TEST_FIXTURE(Test, testSplitFlyTableJoin)
     CPPUNIT_ASSERT(pFly->GetPrecede());
     CPPUNIT_ASSERT(!pFly->HasFollow());
 }
+
+CPPUNIT_TEST_FIXTURE(Test, testSplitFlyWrappedByTable)
+{
+    // Given a document with a floating table, wrapped by an inline table:
+    // When laying out the document:
+    createSwDoc("floattable-wrapped-by-table.docx");
+
+    // Then make sure the inline table wraps around the floating table:
+    SwDoc* pDoc = getSwDoc();
+    SwRootFrame* pLayout = pDoc->getIDocumentLayoutAccess().GetCurrentLayout();
+    auto pPage = pLayout->Lower()->DynCastPageFrame();
+    CPPUNIT_ASSERT(pPage);
+    // Get the top of the inline table, ignoring margins:
+    CPPUNIT_ASSERT(pPage->GetSortedObjs());
+    SwSortedObjs& rPageObjs = *pPage->GetSortedObjs();
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(1), rPageObjs.size());
+    auto pFly = rPageObjs[0]->DynCastFlyFrame()->DynCastFlyAtContentFrame();
+    CPPUNIT_ASSERT(pFly);
+    // Get the bottom of of the floating table, ignoring margins:
+    SwTwips nFloatingBottom = pFly->getFrameArea().Top() + pFly->getFramePrintArea().Height();
+    SwFrame* pBody = pPage->FindBodyCont();
+    auto pTab = pBody->GetLower()->GetNext()->DynCastTabFrame();
+    SwTwips nInlineTop = pTab->getFrameArea().Top() + pTab->getFramePrintArea().Top();
+    // Make sure the inline table is on the right of the floating one, not below it:
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected less than: 7287
+    // - Actual  : 7287
+    // i.e. the inline table was under the floating one, not on the right of it.
+    CPPUNIT_ASSERT_LESS(nFloatingBottom, nInlineTop);
+}
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

commit 2da4acdbf8c5a8ba3ef51e5f5dc3439716e71a91

tdf#159285 sw floattable: fix loop with inner table wrapped by inner table
Regression from 868140fcc1311259b9d5f666637b33d226511a53 (tdf#60558 sw
floattable: allow wrap of table on the right of a floattable,
2023-12-05), the bugdoc layout looped on load.

Somehow the big while() loop in SwTabFrame::MakeAll() never finishes: it
always tries again but can't reach a state where all of frame area
position, frame area size and frame print area is valid.

Fix the problem by going back to the old behavior (floating table is
wrapped by text frames, not by table frames) for the nested table case:
that keeps the old tdf#60558 use-case working and fixes the new
tdf#159285 use-case.

At some point it would be useful to support the combination of nested
floating tables and the "floating table wrapped by table" combination,
but that will be a new layout feature.

Change-Id: Ia3fdbd08de87e13ddef086ae1339e79a8833674d
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/163630
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

sw/source/core/layout/tabfrm.cxx
@@ -3373,7 +3373,15 @@ bool SwTabFrame::CalcFlyOffsets( SwTwips& rUpper,
         }
 
         bool bFlyHoriOrientLeft = text::HoriOrientation::LEFT == rHori.GetHoriOrient();
-        if (bSplitFly && !bFlyHoriOrientLeft)
+
+        bool bToplevelSplitFly = false;
+        if (bSplitFly)
+        {
+            // Floating table wrapped by table: avoid this in the nested case.
+            bToplevelSplitFly = !pFly->GetAnchorFrame()->IsInTab();
+        }
+
+        if (bToplevelSplitFly && !bFlyHoriOrientLeft)
         {
             // Only shift to the right if we don't have enough space on the left.
             SwTwips nTabWidth = getFramePrintArea().Width();

sw/qa/core/layout/tabfrm.cxx
@@ -221,6 +221,20 @@ CPPUNIT_TEST_FIXTURE(Test, testInlineTableThenSplitFly)
     // large positive one.
     CPPUNIT_ASSERT_LESS(static_cast<SwTwips>(0), nInlineLeft);
 }
+
+CPPUNIT_TEST_FIXTURE(Test, testSplitFlyWrappedByTableNested)
+{
+    // Given a document with 3 tables, one inline toplevel and two inner ones (one inline, one
+    // floating):
+    // When laying out that document:
+    // Without the accompanying fix in place, this test would have failed here with a layout loop.
+    createSwDoc("floattable-wrapped-by-table-nested.docx");
+
+    // Than make sure we have 3 tables, but only one of them is floating:
+    SwDoc* pDoc = getSwDoc();
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(3), pDoc->GetTableFrameFormats()->GetFormatCount());
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(1), pDoc->GetSpzFrameFormats()->GetFormatCount());
+}
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */