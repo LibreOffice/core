8291531b6fc59e19c5e6bf36cb43cae807e2b4c8
Michael Stahl <michael.stahl@allotropia.de>
2024-02-20 11:54:13 +0100

    tdf#153196 writerfilter: fix page style for even/odd section break
    This is a bit of a special case, where the first section starts with an
    evenPage break (\sbkeven), which causes a Left-only page style to be
    created.
    
    In completeCopyHeaderFooter(), the HeaderTextFirst and FooterTextFirst
    are copied from the source style to the Left-only page style, but then
    they also need to be deleted from the source style, because the
    Left-only page style is the one that is used for the first page of the
    section, and the source style is used for the subsequent pages.
    
    Additionally, when there is *only* a "first" header/footer, and no
    previous section has one to inherit, Word will not display a
    header/footer at all on subsequent pages; a PageStyle will always have a
    header/footer if it has a HeaderTextFirst/FooterTextFirst.
    
    In this case, delete the header/footer from the source style.
    
    Unfortunately exporting this doesn't work ideally, a spurious evenPage
    footer will be created, both due to the FooterShare being automatically
    reset for no obvious reason in ItemSetToPageDesc(), and
    setProperty("FooterIsShared", true) "stashing" the left footer since
    commit b802ab694a8a7357d4657f3e11b571144fa7c7bf.
    
    (presumably regression from commit b32881b6723072c8d1a652ea147d12e75766d504)
    
    Change-Id: Ie4f9c49605df690e9705e14777c0e4bcb0dfad8e
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/163668
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
    (cherry picked from commit 340f8ea4ae7f11b4d3a95499188a29fe801867cf)

--- sw/source/uibase/utlui/uitool.cxx
+++ sw/source/uibase/utlui/uitool.cxx
@@ -389,6 +389,7 @@ void ItemSetToPageDesc( const SfxItemSet& rSet, SwPageDesc& rPageDesc )
             if(rMaster.GetFooter().IsActive())
             {
                 rMaster.SetFormatAttr(SwFormatFooter(false));
+                // why reset this? but not doing it causes testTdf112694 to fail
                 rPageDesc.ChgFooterShare(false);
             }
         }
