5f0badf730f1875b3c3658e04223a464d21a3c44
Michael Stahl <michael.stahl@allotropia.de>
2024-02-21 19:14:27 +0100

    tdf#159519 libxmlsec: avoid linking function not existent in Windows 7
    xsec_xmlsec.dll is linked to libxmlsec-mscng.dll, which is
    linked to Win32 bcrypt.dll, which doesn't have BCryptKeyDerivation on
    Windows 7.
    
    Use GetProcAddress() to retrieve BCryptKeyDerivation at runtime; the
    function is not used by LibreOffice.
    
    (regression from commit 26bf26272bf525b59b4a4ce18b3ce14c1febfd7b
     and (due to revert) commit bfd479abf0d1d8ce36c3b0dcc6c824216f88a95b)
    
    Change-Id: I0c0c94ee0cdfd48ae7506d9dfb51fcaf8289d460
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/163709
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
    (cherry picked from commit 0690f3b7b981417a7b1f2fffd87c593a2a2a15d5)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/163740
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- external/xmlsec/UnpackedTarball_xmlsec.mk
+++ external/xmlsec/UnpackedTarball_xmlsec.mk
@@ -10,6 +10,8 @@
 xmlsec_patches :=
 # Remove this when Ubuntu 20.04 is EOL in 2025.
 xmlsec_patches += old-nss.patch.1
+# Remove this when Windows 7 is no longer supported
+xmlsec_patches += BCryptKeyDerivation.patch.1
 
 $(eval $(call gb_UnpackedTarball_UnpackedTarball,xmlsec))
 
