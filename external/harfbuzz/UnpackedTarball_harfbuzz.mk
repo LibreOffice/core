af47a78e11d4819bb4b1ae1a1277babd10d815b7
Patrick Luby <guibmacdev@gmail.com>
2024-02-20 19:06:04 -0500

    tdf#159529 clear hb_ot_face_t data after fetching 'sbix' and 'glyf' tables
    The 'sbix' and 'glyf' tables can be very large for color emoji fonts
    so clear any cached data in hb_ot_face_t after fetching that table's
    extents.
    
    Change-Id: Id5deda9114b7199a4194ff16879e5791acd1cd82
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/163674
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>

--- external/harfbuzz/UnpackedTarball_harfbuzz.mk
+++ external/harfbuzz/UnpackedTarball_harfbuzz.mk
@@ -16,6 +16,7 @@ $(eval $(call gb_UnpackedTarball_update_autoconf_configs,harfbuzz))
 $(eval $(call gb_UnpackedTarball_set_patchlevel,harfbuzz,0))
 
 $(eval $(call gb_UnpackedTarball_add_patches,harfbuzz, \
+    external/harfbuzz/tdf159529.patch.0 \
 ))
 
 # vim: set noet sw=4 ts=4:
