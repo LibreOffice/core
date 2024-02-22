5e7beab02a72f60b9984745f77ca840e2d546d74
Stephan Bergmann <stephan.bergmann@allotropia.de>
2024-02-22 10:49:14 +0100

    unoembind sequence tests
    (cherry-picked from 24cd81bc29c440a537e12cbd163aea1b5d68e891)
    
    Change-Id: I151969acde242bc9c7ee88e2b2da44c8c2101456
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/163816
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <stephan.bergmann@allotropia.de>

--- unotest/source/embindtest/embindtest.cxx
+++ unotest/source/embindtest/embindtest.cxx
@@ -85,6 +85,155 @@ public:
     {
         return value.m1 == -123456 && value.m2 == 100.5 && value.m3 == u"hä";
     }
+
+    css::uno::Sequence<sal_Bool> SAL_CALL getSequenceBoolean() override
+    {
+        return { true, true, false };
+    }
+
+    sal_Bool SAL_CALL isSequenceBoolean(css::uno::Sequence<sal_Bool> const& value) override
+    {
+        return value == css::uno::Sequence<sal_Bool>{ true, true, false };
+    }
+
+    css::uno::Sequence<sal_Int8> SAL_CALL getSequenceByte() override { return { -12, 1, 12 }; }
+
+    sal_Bool SAL_CALL isSequenceByte(css::uno::Sequence<sal_Int8> const& value) override
+    {
+        return value == css::uno::Sequence<sal_Int8>{ -12, 1, 12 };
+    }
+
+    css::uno::Sequence<sal_Int16> SAL_CALL getSequenceShort() override
+    {
+        return { -1234, 1, 1234 };
+    }
+
+    sal_Bool SAL_CALL isSequenceShort(css::uno::Sequence<sal_Int16> const& value) override
+    {
+        return value == css::uno::Sequence<sal_Int16>{ -1234, 1, 1234 };
+    }
+
+    css::uno::Sequence<sal_uInt16> SAL_CALL getSequenceUnsignedShort() override
+    {
+        return { 1, 10, 54321 };
+    }
+
+    sal_Bool SAL_CALL isSequenceUnsignedShort(css::uno::Sequence<sal_uInt16> const& value) override
+    {
+        return value == css::uno::Sequence<sal_uInt16>{ 1, 10, 54321 };
+    }
+
+    css::uno::Sequence<sal_Int32> SAL_CALL getSequenceLong() override
+    {
+        return { -123456, 1, 123456 };
+    }
+
+    sal_Bool SAL_CALL isSequenceLong(css::uno::Sequence<sal_Int32> const& value) override
+    {
+        return value == css::uno::Sequence<sal_Int32>{ -123456, 1, 123456 };
+    }
+
+    css::uno::Sequence<sal_uInt32> SAL_CALL getSequenceUnsignedLong() override
+    {
+        return { 1, 10, 3456789012 };
+    }
+
+    sal_Bool SAL_CALL isSequenceUnsignedLong(css::uno::Sequence<sal_uInt32> const& value) override
+    {
+        return value == css::uno::Sequence<sal_uInt32>{ 1, 10, 3456789012 };
+    }
+
+    css::uno::Sequence<sal_Int64> SAL_CALL getSequenceHyper() override
+    {
+        return { -123456789, 1, 123456789 };
+    }
+
+    sal_Bool SAL_CALL isSequenceHyper(css::uno::Sequence<sal_Int64> const& value) override
+    {
+        return value == css::uno::Sequence<sal_Int64>{ -123456789, 1, 123456789 };
+    }
+
+    css::uno::Sequence<sal_uInt64> SAL_CALL getSequenceUnsignedHyper() override
+    {
+        return { 1, 10, 9876543210 };
+    }
+
+    sal_Bool SAL_CALL isSequenceUnsignedHyper(css::uno::Sequence<sal_uInt64> const& value) override
+    {
+        return value == css::uno::Sequence<sal_uInt64>{ 1, 10, 9876543210 };
+    }
+
+    css::uno::Sequence<float> SAL_CALL getSequenceFloat() override
+    {
+        return { -10.25, 1.5, 10.75 };
+    }
+
+    sal_Bool SAL_CALL isSequenceFloat(css::uno::Sequence<float> const& value) override
+    {
+        return value == css::uno::Sequence<float>{ -10.25, 1.5, 10.75 };
+    }
+
+    css::uno::Sequence<double> SAL_CALL getSequenceDouble() override
+    {
+        return { -100.5, 1.25, 100.75 };
+    }
+
+    sal_Bool SAL_CALL isSequenceDouble(css::uno::Sequence<double> const& value) override
+    {
+        return value == css::uno::Sequence<double>{ -100.5, 1.25, 100.75 };
+    }
+
+    css::uno::Sequence<sal_Unicode> SAL_CALL getSequenceChar() override
+    {
+        return { 'a', 'B', u'Ö' };
+    }
+
+    sal_Bool SAL_CALL isSequenceChar(css::uno::Sequence<sal_Unicode> const& value) override
+    {
+        return value == css::uno::Sequence<sal_Unicode>{ 'a', 'B', u'Ö' };
+    }
+
+    css::uno::Sequence<OUString> SAL_CALL getSequenceString() override
+    {
+        return { u"foo"_ustr, u"barr"_ustr, u"bazzz"_ustr };
+    }
+
+    sal_Bool SAL_CALL isSequenceString(css::uno::Sequence<OUString> const& value) override
+    {
+        return value == css::uno::Sequence<OUString>{ u"foo"_ustr, u"barr"_ustr, u"bazzz"_ustr };
+    }
+
+    css::uno::Sequence<css::uno::Sequence<OUString>> SAL_CALL getSequenceSequenceString() override
+    {
+        return { {}, { u"foo"_ustr, u"barr"_ustr }, { u"baz"_ustr } };
+    }
+
+    sal_Bool SAL_CALL
+    isSequenceSequenceString(css::uno::Sequence<css::uno::Sequence<OUString>> const& value) override
+    {
+        return value
+               == css::uno::Sequence<css::uno::Sequence<OUString>>{ {},
+                                                                    { u"foo"_ustr, u"barr"_ustr },
+                                                                    { u"baz"_ustr } };
+    }
+
+    css::uno::Sequence<org::libreoffice::embindtest::Struct> SAL_CALL getSequenceStruct() override
+    {
+        return { { -123456, -100.5, u"foo"_ustr },
+                 { 1, 1.25, u"barr"_ustr },
+                 { 123456, 100.75, u"bazzz"_ustr } };
+    }
+
+    sal_Bool SAL_CALL
+    isSequenceStruct(css::uno::Sequence<org::libreoffice::embindtest::Struct> const& value) override
+    {
+        return value
+               == css::uno::Sequence<org::libreoffice::embindtest::Struct>{
+                      { -123456, -100.5, u"foo"_ustr },
+                      { 1, 1.25, u"barr"_ustr },
+                      { 123456, 100.75, u"bazzz"_ustr }
+                  };
+    }
 };
 }
 
