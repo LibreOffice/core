0f5dd28e6a2492f3e2b3ae583d94bd10173767df
Stephan Bergmann <stephan.bergmann@allotropia.de>
2024-02-22 10:25:32 +0100

    A first cut at embindtest
    (UNO boolean should arguably map to JS Boolean, not Number)
    
    (cherry-picked from 29b109dfc0f836e34b61b2fbbe69d2b0728dc7b8)
    
    Change-Id: I21847c558b14ead053aa5c5ea94a508a73aaf36b
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/163722
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <stephan.bergmann@allotropia.de>

--- unotest/source/embindtest/embindtest.js
+++ unotest/source/embindtest/embindtest.js
@@ -13,6 +13,80 @@ Module.addOnPostRun(function() {
     let test = new Module.unoembind_uno.org.libreoffice.embindtest.Test(
         Module.getUnoComponentContext());
     console.assert(typeof test === 'object');
+    {
+        let v = test.getBoolean();
+        console.log(v);
+        console.assert(v === 1); //TODO: true
+        console.assert(test.isBoolean(v));
+    }
+    {
+        let v = test.getByte();
+        console.log(v);
+        console.assert(v === -12);
+        console.assert(test.isByte(v));
+    }
+    {
+        let v = test.getShort();
+        console.log(v);
+        console.assert(v === -1234);
+        console.assert(test.isShort(v));
+    }
+    {
+        let v = test.getUnsignedShort();
+        console.log(v);
+        console.assert(v === 54321);
+        console.assert(test.isUnsignedShort(v));
+    }
+    {
+        let v = test.getLong();
+        console.log(v);
+        console.assert(v === -123456);
+        console.assert(test.isLong(v));
+    }
+    {
+        let v = test.getUnsignedLong();
+        console.log(v);
+        console.assert(v === 3456789012);
+        console.assert(test.isUnsignedLong(v));
+    }
+    {
+        let v = test.getHyper();
+        console.log(v);
+        console.assert(v === -123456789n);
+        console.assert(test.isHyper(v));
+    }
+    {
+        let v = test.getUnsignedHyper();
+        console.log(v);
+        console.assert(v === 9876543210n);
+        console.assert(test.isUnsignedHyper(v));
+    }
+    {
+        let v = test.getFloat();
+        console.log(v);
+        console.assert(v === -10.25);
+        console.assert(test.isFloat(v));
+    }
+    {
+        let v = test.getDouble();
+        console.log(v);
+        console.assert(v === 100.5);
+        console.assert(test.isDouble(v));
+    }
+    {
+        let v = test.getString();
+        console.log(v);
+        console.assert(v === 'hä');
+        console.assert(test.isString(v));
+    }
+    {
+        let v = test.getStruct();
+        console.log(v.m1 + ', ' + v.m2 + ', ' + v.m3);
+        console.assert(v.m1 === -123456);
+        console.assert(v.m2 === 100.5);
+        console.assert(v.m3 === 'hä');
+        console.assert(test.isStruct(v));
+    }
 });
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
