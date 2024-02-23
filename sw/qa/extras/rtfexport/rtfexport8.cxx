LibreOffice/core

commit bb198176684c3d9377e26c04a29ec66deb811949

MCGR: Make MCGR default for oox im/export, cleanup
Following an error in CppunitTest_chart2_export3 I updated
the transparency definition at WriteGradientFill and
corrected usages.

Had to correct/adapt some Chart UnitTests. Some of these
changes are temporary since this will/has to change when
ODF MCGR im/export is integrated. I checked that all of
these cases actually work, comparing im LO and MSO.

Adapted some Chart2ImportTest to directly compare/check
now for the fully imported tranparence gradient with
available higher precision.

Adapted OoxDrawingmlTest testGradientMultiStepTransparency
to use new MCGR capabilities.

Adapted testTextframeGradient and tested the turn-around
with rtf gradients. These are a little bit limited and
needed some extra care.

Adapted testTextframeGradient.
Adapted SdOOXMLExportTest1, testTdf94238
Adapted SdOOXMLExportTest1, testTdf128345GradientAxial
Adapted SdOOXMLExportTest2, testTdf105739
Adapted SdOOXMLExportTest3, testTdf127372
Adapted SdOOXMLExportTest3, testTdf127379
Adapted SdMiscTest, testFillGradient
Adapted testTextframeGradient
Adapted ScFiltersTest3, testTdf129789
Adapted SdUiImpressTest, testPageFillGradient

Adapted SdOOXMLExportTest1, testTdf128345GradientLinear by
using better double-to-integer rounding (basegfx::fround) in
DrawingML::WriteGradientStop. After double calculations
this makes the tansition to integer correct and stable. Also
took back change at testTdf128345ChartArea_CG_TS_export
which showed the same flaw before.

2nd look @testTdf128345Legend_CS_TG_axial_export made me
add that stuff again and adapt the axial ColorStop adding
in the export to not export the middle enty twice. Extended
test a little bit, too.
Only do not add value if it starts at 0.0 aka StartColor,
else adding it is corect.

Adapted some tests CPPUNIT_ASSERT to CPPUNIT_ASSERT_EQUAL
after being pointed to it from gerrit_linux_clang_dbgutil
build.

Change-Id: I4a993053da8960035671b655e67908f36e59b5fe

sw/source/filter/ww8/rtfattributeoutput.cxx
@@ -3707,29 +3707,39 @@ void RtfAttributeOutput::FormatFillGradient(const XFillGradientItem& rFillGradie
     m_aFlyProperties.push_back(std::make_pair<OString, OString>(
         "fillType", OString::number(7))); // Shade using the fillAngle
 
-    const XGradient& rGradient = rFillGradient.GetGradientValue();
-    const Color aStartColor(rGradient.GetColorStops().front().getStopColor());
-    m_aFlyProperties.push_back(std::make_pair<OString, OString>(
-        "fillBackColor", OString::number(wwUtility::RGBToBGR(aStartColor))));
+    const XGradient& rGradient(rFillGradient.GetGradientValue());
+    const basegfx::ColorStops& rColorStops(rGradient.GetColorStops());
 
-    const Color aEndColor(rGradient.GetColorStops().back().getStopColor());
+    // MCGR: It would be best to export the full MCGR definition here
+    // with all ColorStops in rColorStops, but rtf does not support this.
+    // Best thing to do and to stay compatible is to export front/back
+    // colors as start/end and - when more than two ColorStops are defined -
+    // guess that GradientStyle_AXIAL is used and thus create a "fillFocus"
+    // entry
+    // NOTE: I also found that loading file from testTextframeGradient
+    // "textframe-gradient.rtf" and save-as *inverts* the gradient, so I
+    // exchanged here fillColor/fillBackColor to get the correct order
+    const Color aStartColor(rColorStops.front().getStopColor());
     m_aFlyProperties.push_back(std::make_pair<OString, OString>(
-        "fillColor", OString::number(wwUtility::RGBToBGR(aEndColor))));
+        "fillColor", OString::number(wwUtility::RGBToBGR(aStartColor))));
 
-    switch (rGradient.GetGradientStyle())
+    if (rColorStops.size() < 3)
     {
-        case css::awt::GradientStyle_LINEAR:
-            break;
-        case css::awt::GradientStyle_AXIAL:
-            m_aFlyProperties.push_back(
-                std::make_pair<OString, OString>("fillFocus", OString::number(50)));
-            break;
-        case css::awt::GradientStyle_RADIAL:
-        case css::awt::GradientStyle_ELLIPTICAL:
-        case css::awt::GradientStyle_SQUARE:
-        case css::awt::GradientStyle_RECT:
-        default:
-            break;
+        // two-color version, use back as 2nd color
+        const Color aEndColor(rColorStops.back().getStopColor());
+        m_aFlyProperties.push_back(std::make_pair<OString, OString>(
+            "fillBackColor", OString::number(wwUtility::RGBToBGR(aEndColor))));
+    }
+    else
+    {
+        // assume what was formally GradientStyle_AXIAL, see above and also refer to
+        // FillModel::pushToPropMap 'fFocus' value and usage.
+        // The 2nd color is the in-between color, use it
+        const Color aEndColor(rColorStops[1].getStopColor());
+        m_aFlyProperties.push_back(std::make_pair<OString, OString>(
+            "fillBackColor", OString::number(wwUtility::RGBToBGR(aEndColor))));
+        m_aFlyProperties.push_back(
+            std::make_pair<OString, OString>("fillFocus", OString::number(50)));
     }
 }
 

chart2/qa/extras/chart2geometry.cxx
@@ -170,6 +170,12 @@ void Chart2GeometryTest::testTdf128345ChartArea_CG_TS_export()
     // Without the patch the transparency was lost in saved pptx file.
     loadFromURL(u"odp/tdf128345_ChartArea_CG_TS.odp");
 
+    // MCGR: Similar to testTdf128345Legend_CS_TG_axial_export:
+    // Checked that it works with the existing import file,
+    // but will change with ODF MCGR im/export again.
+    // Adapting for now, but need to re-check values after
+    // ODF im/export for MCGR is integrated
+
     // Make sure the chart area has a transparency in gradient stops in saved pptx file.
     save("Impress MS PowerPoint 2007 XML");
     xmlDocUniquePtr pXmlDoc = parseExport("ppt/charts/chart1.xml");
@@ -188,6 +194,12 @@ void Chart2GeometryTest::testTdf128345ChartArea_CG_TS_import()
     // Make sure chart area has transparency when pptx document is opened and resaved as odp.
     // As of Aug 2020, the import generates a transparency gradient. When import is changed to
     // generate solid transparency, the test needs to be adapted.
+
+    // MCGR: Similar to testTdf128345Legend_CS_TG_axial_export:
+    // Checked that it works with the existing import file,
+    // but will change with ODF MCGR im/export again. We will need to
+    // update the *.odp input file. Disable unclear values for now and
+    // adapt when ODF im/export for MCGR is integrated
     loadFromURL(u"pptx/tdf128345_ChartArea_CG_TS.pptx");
 
     // Find transparency gradient name
@@ -216,7 +228,7 @@ void Chart2GeometryTest::testTdf128345ChartArea_CG_TS_import()
     assertXPath(pXmlDoc2, sStart + " and @draw:start='30%']");
     assertXPath(pXmlDoc2, sStart + " and @draw:end='30%']");
     assertXPath(pXmlDoc2, sStart + " and @draw:angle='30deg']");
-    assertXPath(pXmlDoc2, sStart + " and @draw:border='20%']");
+    assertXPath(pXmlDoc2, sStart + " and @draw:border='0%']"); // MCGR: no border anymore 20% -> 0%
 }
 
 void Chart2GeometryTest::testTdf128345ChartWall_CS_TG_export()
@@ -283,13 +295,21 @@ void Chart2GeometryTest::testTdf128345Legend_CS_TG_axial_export()
     CPPUNIT_ASSERT(pXmlDoc);
 
     OString sPathStart("//c:chartSpace/c:chart/c:legend/c:spPr/a:gradFill");
-    assertXPath(pXmlDoc, sPathStart + "/a:gsLst/a:gs", 3); // axial
-    // no element for 0% transparent
+
+    // MCGR: three entries due to axial being mirrored+expanded to linear
+    assertXPath(pXmlDoc, sPathStart + "/a:gsLst/a:gs", 3);
+
+    // MCGR: start entry, no transparence, pos zero
     assertXPath(pXmlDoc, sPathStart + "/a:gsLst/a:gs[1]/a:srgbClr/a:alpha", 0);
-    // 100% transparent = opacity 0. It comes from "axial", therefore it is in the middle.
+    assertXPath(pXmlDoc, sPathStart + "/a:gsLst/a:gs[1]", "pos", "0");
+
+    // MCGR: middle entry, 100% transparence, pos 0.5
     assertXPath(pXmlDoc, sPathStart + "/a:gsLst/a:gs[2]/a:srgbClr/a:alpha", "val", "0");
     assertXPath(pXmlDoc, sPathStart + "/a:gsLst/a:gs[2]", "pos", "50000");
+
+    // MCGR: end entry, no transparence, pos 1.0
     assertXPath(pXmlDoc, sPathStart + "/a:gsLst/a:gs[3]/a:srgbClr/a:alpha", 0);
+    assertXPath(pXmlDoc, sPathStart + "/a:gsLst/a:gs[3]", "pos", "100000");
 }
 
 void Chart2GeometryTest::testTdf128345Legend_CS_TG_axial_import()
@@ -320,8 +340,9 @@ void Chart2GeometryTest::testTdf128345Legend_CS_TG_axial_import()
     const OString sAttribute("@draw:name='" + OU2O(sOUOpacityName) + "'");
     const OString sStart("//office:document-styles/office:styles/draw:opacity[" + sAttribute);
     assertXPath(pXmlDoc2, sStart + "]", 1);
-    assertXPath(pXmlDoc2, sStart + " and @draw:style='axial']");
-    assertXPath(pXmlDoc2, sStart + " and @draw:start='0%']");
+    // MCGR: Needs odf im/export for MCGR, then adapt.
+    assertXPath(pXmlDoc2, sStart + " and @draw:style='linear']"); // MCGR: axial -> linear
+    assertXPath(pXmlDoc2, sStart + " and @draw:start='100%']"); // MCGR: 0% -> 100%
     assertXPath(pXmlDoc2, sStart + " and @draw:end='100%']");
 }
 

chart2/qa/extras/chart2import.cxx
@@ -32,8 +32,9 @@
 #include <iterator>
 
 #include <com/sun/star/util/Color.hpp>
-#include <com/sun/star/awt/Gradient.hpp>
+#include <com/sun/star/awt/Gradient2.hpp>
 #include <com/sun/star/lang/XMultiServiceFactory.hpp>
+#include <basegfx/utils/gradienttools.hxx>
 
 class Chart2ImportTest : public ChartTest
 {
@@ -618,12 +619,23 @@ CPPUNIT_TEST_FIXTURE(Chart2ImportTest, testBnc889755)
     //tdf#139940 - the title's gradient was lost and was filled with solid blue, instead of a "blue underline".
     uno::Reference<drawing::XDrawPagesSupplier> xDoc(mxComponent, uno::UNO_QUERY_THROW);
     uno::Reference<drawing::XDrawPage> xPage(xDoc->getDrawPages()->getByIndex(0), uno::UNO_QUERY_THROW);
+
     // Shape "Title 3"
+    // MCGR: Use the whole completely imported transparency gradient to check for correctness
     uno::Reference<beans::XPropertySet> xShapeProps(xPage->getByIndex(4), uno::UNO_QUERY_THROW);
-    awt::Gradient aTransparence;
+    awt::Gradient2 aTransparence;
     xShapeProps->getPropertyValue("FillTransparenceGradient") >>= aTransparence;
-    CPPUNIT_ASSERT(aTransparence.StartColor != aTransparence.EndColor);
-    CPPUNIT_ASSERT_EQUAL(COL_WHITE, Color(ColorTransparency, aTransparence.StartColor));
+
+    basegfx::ColorStops aColorStops;
+    basegfx::utils::fillColorStopsFromGradient2(aColorStops, aTransparence);
+
+    CPPUNIT_ASSERT_EQUAL(size_t(3), aColorStops.size());
+    CPPUNIT_ASSERT(basegfx::fTools::equal(aColorStops[0].getStopOffset(), 0.0));
+    CPPUNIT_ASSERT_EQUAL(aColorStops[0].getStopColor(), basegfx::BColor(0.25, 0.25, 0.25));
+    CPPUNIT_ASSERT(basegfx::fTools::equal(aColorStops[1].getStopOffset(), 0.070000000000000007));
+    CPPUNIT_ASSERT_EQUAL(aColorStops[1].getStopColor(), basegfx::BColor(0.25, 0.25, 0.25));
+    CPPUNIT_ASSERT(basegfx::fTools::equal(aColorStops[2].getStopOffset(), 0.080000000000000002));
+    CPPUNIT_ASSERT_EQUAL(aColorStops[2].getStopColor(), basegfx::BColor(1.0, 1.0, 1.0));
 }
 
 CPPUNIT_TEST_FIXTURE(Chart2ImportTest, testBnc882383)
@@ -652,14 +664,22 @@ CPPUNIT_TEST_FIXTURE(Chart2ImportTest, testTransparencyGradientValue)
     xPropertySet->getPropertyValue("FillTransparenceGradientName") >>= sTranspGradientName;
     CPPUNIT_ASSERT(!sTranspGradientName.isEmpty());
 
-    awt::Gradient aTransparenceGradient;
+    awt::Gradient2 aTransparenceGradient;
     uno::Reference< lang::XMultiServiceFactory > xFact(xChartDoc, uno::UNO_QUERY);
     CPPUNIT_ASSERT(xFact.is());
     uno::Reference< container::XNameAccess > xTransparenceGradient(xFact->createInstance("com.sun.star.drawing.TransparencyGradientTable"), uno::UNO_QUERY);
     uno::Any rTransparenceValue = xTransparenceGradient->getByName(sTranspGradientName);
     CPPUNIT_ASSERT(rTransparenceValue >>= aTransparenceGradient);
-    CPPUNIT_ASSERT_EQUAL(sal_Int32(3355443), aTransparenceGradient.EndColor);
-    CPPUNIT_ASSERT_EQUAL(sal_Int32(5000268), aTransparenceGradient.StartColor);
+
+    basegfx::ColorStops aColorStops;
+    basegfx::utils::fillColorStopsFromGradient2(aColorStops, aTransparenceGradient);
+
+    // MCGR: Use the whole completely imported transparency gradient to check for correctness
+    CPPUNIT_ASSERT_EQUAL(size_t(2), aColorStops.size());
+    CPPUNIT_ASSERT(basegfx::fTools::equal(aColorStops[0].getStopOffset(), 0.0));
+    CPPUNIT_ASSERT_EQUAL(aColorStops[0].getStopColor(), basegfx::BColor(0.3, 0.3, 0.3));
+    CPPUNIT_ASSERT(basegfx::fTools::equal(aColorStops[1].getStopOffset(), 1.0));
+    CPPUNIT_ASSERT_EQUAL(aColorStops[1].getStopColor(), basegfx::BColor(0.2, 0.2, 0.2));
 }
 
 CPPUNIT_TEST_FIXTURE(Chart2ImportTest, testSimpleStrictXLSX)

include/oox/export/drawingml.hxx
@@ -355,8 +355,7 @@ public:
     void WriteColor( const OUString& sColorSchemeName, const css::uno::Sequence< css::beans::PropertyValue >& aTransformations, sal_Int32 nAlpha = MAX_PERCENT );
     void WriteColor( const ::Color nColor, const css::uno::Sequence< css::beans::PropertyValue >& aTransformations, sal_Int32 nAlpha = MAX_PERCENT );
     void WriteColorTransformations( const css::uno::Sequence< css::beans::PropertyValue >& aTransformations, sal_Int32 nAlpha = MAX_PERCENT );
-    void WriteGradientStop(sal_uInt16 nStop, ::Color nColor, sal_Int32 nAlpha = MAX_PERCENT);
-    void WriteGradientStop2(double fOffset, const basegfx::BColor& rColor, const basegfx::BColor& rAlpha);
+    void WriteGradientStop(double fOffset, const basegfx::BColor& rColor, const basegfx::BColor& rAlpha);
     void WriteLineArrow( const css::uno::Reference< css::beans::XPropertySet >& rXPropSet, bool bLineStart );
     void WriteConnectorConnections( sal_Int32 nStartGlueId, sal_Int32 nEndGlueId, sal_Int32 nStartID, sal_Int32 nEndID );
 
@@ -377,10 +376,7 @@ public:
     */
     void WriteGradientFill(
         const css::awt::Gradient2* pColorGradient, sal_Int32 nFixColor,
-        const css::awt::Gradient2* pTransparenceGradient, sal_Int32 nFixTransparence);
-    void WriteGradientFill2(
-        const css::awt::Gradient2* pColorGradient, sal_Int32 nFixColor,
-        const css::awt::Gradient2* pTransparenceGradient, sal_Int32 nFixTransparence);
+        const css::awt::Gradient2* pTransparenceGradient, double fFixTransparence = 0.0);
 
     void WriteGrabBagGradientFill( const css::uno::Sequence< css::beans::PropertyValue >& aGradientStops, const css::awt::Gradient2& rGradient);
 

oox/CppunitTest_oox_drawingml.mk
@@ -31,6 +31,7 @@ $(eval $(call gb_CppunitTest_use_libraries,oox_drawingml, \
     tl \
     unotest \
     utl \
+    basegfx \
 ))
 
 $(eval $(call gb_CppunitTest_use_sdk_api,oox_drawingml))

oox/CppunitTest_oox_shape.mk
@@ -33,6 +33,7 @@ $(eval $(call gb_CppunitTest_use_libraries,oox_shape, \
     test \
     unotest \
     utl \
+    basegfx \
 ))
 
 $(eval $(call gb_CppunitTest_use_sdk_api,oox_shape))

oox/qa/unit/drawingml.cxx
@@ -9,7 +9,7 @@
 
 #include <test/unoapi_test.hxx>
 
-#include <com/sun/star/awt/Gradient.hpp>
+#include <com/sun/star/awt/Gradient2.hpp>
 #include <com/sun/star/awt/Rectangle.hpp>
 #include <com/sun/star/beans/XPropertySet.hpp>
 #include <com/sun/star/container/XNamed.hpp>
@@ -35,6 +35,7 @@
 #include <docmodel/theme/Theme.hxx>
 
 #include <comphelper/sequenceashashmap.hxx>
+#include <basegfx/utils/gradienttools.hxx>
 
 using namespace ::com::sun::star;
 
@@ -181,7 +182,7 @@ CPPUNIT_TEST_FIXTURE(OoxDrawingmlTest, testGradientMultiStepTransparency)
     uno::Reference<container::XNamed> xShape(xDrawPage->getByIndex(1), uno::UNO_QUERY);
     CPPUNIT_ASSERT_EQUAL(OUString("Rectangle 4"), xShape->getName());
     uno::Reference<beans::XPropertySet> xShapeProps(xShape, uno::UNO_QUERY);
-    awt::Gradient aTransparence;
+    awt::Gradient2 aTransparence;
     xShapeProps->getPropertyValue("FillTransparenceGradient") >>= aTransparence;
 
     // Without the accompanying fix in place, this test would have failed with:
@@ -189,7 +190,13 @@ CPPUNIT_TEST_FIXTURE(OoxDrawingmlTest, testGradientMultiStepTransparency)
     // - Actual  : 3487029 (0x353535)
     // i.e. the end transparency was not 100%, but was 21%, leading to an unexpected visible line on
     // the right of this shape.
-    CPPUNIT_ASSERT_EQUAL(COL_WHITE, Color(ColorTransparency, aTransparence.EndColor));
+    // MCGR: Use the completely imported transparency gradient to check for correctness
+    basegfx::ColorStops aColorStops;
+    basegfx::utils::fillColorStopsFromGradient2(aColorStops, aTransparence);
+
+    CPPUNIT_ASSERT_EQUAL(size_t(5), aColorStops.size());
+    CPPUNIT_ASSERT(basegfx::fTools::equal(aColorStops[4].getStopOffset(), 1.0));
+    CPPUNIT_ASSERT_EQUAL(aColorStops[4].getStopColor(), basegfx::BColor(0.0, 0.0, 0.0));
 }
 
 CPPUNIT_TEST_FIXTURE(OoxDrawingmlTest, testShapeTextAlignment)

commit 9ba68c769d41d1075152a22bf37e78fb9320317b

tdf#159824 MCGR rtf export: don't lose gradient's axial-ness
This fixes alg's 7.6 regression from
commit bb198176684c3d9377e26c04a29ec66deb811949

Found when trying to import these strung-out-linears
as real axials.

make CppunitTest_sw_rtfexport \
    CPPUNIT_TEST_NAME=testTdf159824_axialGradient

Change-Id: I220f1bf689b4b219bc0ae187e95aedb1a29a7233
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/163705
Tested-by: Jenkins
Reviewed-by: Justin Luth <jluth@mail.com>
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

sw/source/filter/ww8/rtfattributeoutput.cxx
@@ -3751,6 +3751,12 @@ void RtfAttributeOutput::FormatFillGradient(const XFillGradientItem& rFillGradie
         const Color aEndColor(rColorStops.back().getStopColor());
         m_aFlyProperties.push_back(std::make_pair<OString, OString>(
             "fillBackColor"_ostr, OString::number(wwUtility::RGBToBGR(aEndColor))));
+
+        if (rGradient.GetGradientStyle() == awt::GradientStyle_AXIAL)
+        {
+            m_aFlyProperties.push_back(
+                std::make_pair<OString, OString>("fillFocus"_ostr, OString::number(50)));
+        }
     }
     else
     {

sw/qa/extras/rtfexport/rtfexport8.cxx
@@ -10,6 +10,8 @@
 #include <swmodeltestbase.hxx>
 
 #include <com/sun/star/awt/FontWeight.hpp>
+#include <com/sun/star/awt/Gradient2.hpp>
+#include <com/sun/star/drawing/FillStyle.hpp>
 #include <com/sun/star/text/GraphicCrop.hpp>
 #include <com/sun/star/text/XFootnote.hpp>
 #include <com/sun/star/text/XFootnotesSupplier.hpp>
@@ -21,7 +23,9 @@
 #include <com/sun/star/style/ParagraphAdjust.hpp>
 #include <com/sun/star/style/TabStop.hpp>
 
+#include <basegfx/utils/gradienttools.hxx>
 #include <comphelper/sequenceashashmap.hxx>
+#include <docmodel/uno/UnoGradientTools.hxx>
 #include <tools/UnitConversion.hxx>
 #include <comphelper/propertyvalue.hxx>
 
@@ -176,6 +180,36 @@ DECLARE_RTFEXPORT_TEST(testTdf158826_extraCR, "tdf158826_extraCR.rtf")
 
     // There is a two-column floating table
     uno::Reference<text::XTextTable> xTable(getParagraphOrTable(1), uno::UNO_QUERY_THROW);
+}
+
+CPPUNIT_TEST_FIXTURE(Test, testTdf159824_axialGradient)
+{
+    // given a frame with an axial gradient (white - green - white)
+    loadAndReload("tdf159824_axialGradient.odt");
+
+    uno::Reference<text::XTextFramesSupplier> xTextFramesSupplier(mxComponent, uno::UNO_QUERY);
+    uno::Reference<container::XIndexAccess> xIndexAccess(xTextFramesSupplier->getTextFrames(),
+                                                         uno::UNO_QUERY);
+    uno::Reference<beans::XPropertySet> xFrame(xIndexAccess->getByIndex(0), uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(drawing::FillStyle_GRADIENT,
+                         getProperty<drawing::FillStyle>(xFrame, "FillStyle"));
+    awt::Gradient2 aGradient = getProperty<awt::Gradient2>(xFrame, "FillGradient");
+
+    //const Color aColA(0x127622); // green
+    //const Color aColB(0xffffff); // white
+
+    // MCGR: Use the completely imported transparency gradient to check for correctness
+    basegfx::BColorStops aColorStops = model::gradient::getColorStopsFromUno(aGradient.ColorStops);
+
+    // expected: a 3-color linear gradient (or better yet a 2-color AXIAL gradient)
+    CPPUNIT_ASSERT_EQUAL(size_t(3), aColorStops.size());
+    CPPUNIT_ASSERT_EQUAL(awt::GradientStyle_LINEAR, aGradient.Style);
+    CPPUNIT_ASSERT(basegfx::fTools::equal(aColorStops[0].getStopOffset(), 0.0));
+    //CPPUNIT_ASSERT_EQUAL(aColB, Color(aColorStops[0].getStopColor()));
+    // CPPUNIT_ASSERT(basegfx::fTools::equal(aColorStops[1].getStopOffset(), 0.5));
+    // CPPUNIT_ASSERT_EQUAL(aColA, Color(aColorStops[1].getStopColor()));
+    // CPPUNIT_ASSERT(basegfx::fTools::equal(aColorStops[2].getStopOffset(), 1.0));
+    // CPPUNIT_ASSERT_EQUAL(aColB, Color(aColorStops[2].getStopColor()));
 }
 
 DECLARE_RTFEXPORT_TEST(testTdf158830, "tdf158830.rtf")