8e5a5b7af90ebe4ad35b89ec30f4f19191d9b1ec
AhmedHamed <ahmedhamed3699@gmail.com>
2024-02-24 17:32:31 +0200

    tdf#145614 Convert #define to enum
    Change-Id: I949fb5327032228b7aecfea762e23fdea692ca78
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/163887
    Tested-by: Jenkins
    Reviewed-by: Hossein <hossein@libreoffice.org>

--- connectivity/source/inc/propertyids.hxx
+++ connectivity/source/inc/propertyids.hxx
@@ -36,74 +36,78 @@ namespace dbtools
     };
 }
 
-#define PROPERTY_ID_QUERYTIMEOUT                    1
-#define PROPERTY_ID_MAXFIELDSIZE                    2
-#define PROPERTY_ID_MAXROWS                         3
-#define PROPERTY_ID_CURSORNAME                      4
-#define PROPERTY_ID_RESULTSETCONCURRENCY            5
-#define PROPERTY_ID_RESULTSETTYPE                   6
-#define PROPERTY_ID_FETCHDIRECTION                  7
-#define PROPERTY_ID_FETCHSIZE                       8
-#define PROPERTY_ID_ESCAPEPROCESSING                9
-#define PROPERTY_ID_USEBOOKMARKS                    10
-// Column
-#define PROPERTY_ID_NAME                            11
-#define PROPERTY_ID_TYPE                            12
-#define PROPERTY_ID_TYPENAME                        13
-#define PROPERTY_ID_PRECISION                       14
-#define PROPERTY_ID_SCALE                           15
-#define PROPERTY_ID_ISNULLABLE                      16
-#define PROPERTY_ID_ISAUTOINCREMENT                 17
-#define PROPERTY_ID_ISROWVERSION                    18
-#define PROPERTY_ID_DESCRIPTION                     19
-#define PROPERTY_ID_DEFAULTVALUE                    20
+enum PropertyId
+{
+    PROPERTY_ID_QUERYTIMEOUT = 1,
+    PROPERTY_ID_MAXFIELDSIZE = 2,
+    PROPERTY_ID_MAXROWS = 3,
+    PROPERTY_ID_CURSORNAME = 4,
+    PROPERTY_ID_RESULTSETCONCURRENCY = 5,
+    PROPERTY_ID_RESULTSETTYPE = 6,
+    PROPERTY_ID_FETCHDIRECTION = 7,
+    PROPERTY_ID_FETCHSIZE = 8,
+    PROPERTY_ID_ESCAPEPROCESSING = 9,
+    PROPERTY_ID_USEBOOKMARKS = 10,
 
-#define PROPERTY_ID_REFERENCEDTABLE                 21
-#define PROPERTY_ID_UPDATERULE                      22
-#define PROPERTY_ID_DELETERULE                      23
-#define PROPERTY_ID_CATALOG                         24
-#define PROPERTY_ID_ISUNIQUE                        25
-#define PROPERTY_ID_ISPRIMARYKEYINDEX               26
-#define PROPERTY_ID_ISCLUSTERED                     27
-#define PROPERTY_ID_ISASCENDING                     28
-#define PROPERTY_ID_SCHEMANAME                      29
-#define PROPERTY_ID_CATALOGNAME                     30
+    // Column
+    PROPERTY_ID_NAME = 11,
+    PROPERTY_ID_TYPE = 12,
+    PROPERTY_ID_TYPENAME = 13,
+    PROPERTY_ID_PRECISION = 14,
+    PROPERTY_ID_SCALE = 15,
+    PROPERTY_ID_ISNULLABLE = 16,
+    PROPERTY_ID_ISAUTOINCREMENT = 17,
+    PROPERTY_ID_ISROWVERSION = 18,
+    PROPERTY_ID_DESCRIPTION = 19,
+    PROPERTY_ID_DEFAULTVALUE = 20,
 
-#define PROPERTY_ID_COMMAND                         31
-#define PROPERTY_ID_CHECKOPTION                     32
-#define PROPERTY_ID_PASSWORD                        33
-#define PROPERTY_ID_RELATEDCOLUMN                   34
+    PROPERTY_ID_REFERENCEDTABLE = 21,
+    PROPERTY_ID_UPDATERULE = 22,
+    PROPERTY_ID_DELETERULE = 23,
+    PROPERTY_ID_CATALOG = 24,
+    PROPERTY_ID_ISUNIQUE = 25,
+    PROPERTY_ID_ISPRIMARYKEYINDEX = 26,
+    PROPERTY_ID_ISCLUSTERED = 27,
+    PROPERTY_ID_ISASCENDING = 28,
+    PROPERTY_ID_SCHEMANAME = 29,
+    PROPERTY_ID_CATALOGNAME = 30,
 
-#define PROPERTY_ID_FUNCTION                        35
-#define PROPERTY_ID_TABLENAME                       36
-#define PROPERTY_ID_REALNAME                        37
-#define PROPERTY_ID_DBASEPRECISIONCHANGED           38
-#define PROPERTY_ID_ISCURRENCY                      39
-#define PROPERTY_ID_ISBOOKMARKABLE                  40
+    PROPERTY_ID_COMMAND = 31,
+    PROPERTY_ID_CHECKOPTION = 32,
+    PROPERTY_ID_PASSWORD = 33,
+    PROPERTY_ID_RELATEDCOLUMN = 34,
 
-#define PROPERTY_ID_INVALID_INDEX                   41
-#define PROPERTY_ID_HY010                           43
-#define PROPERTY_ID_LABEL                           44
-#define PROPERTY_ID_DELIMITER                       45
-#define PROPERTY_ID_FORMATKEY                       46
-#define PROPERTY_ID_LOCALE                          47
-#define PROPERTY_ID_IM001                           48
+    PROPERTY_ID_FUNCTION = 35,
+    PROPERTY_ID_TABLENAME = 36,
+    PROPERTY_ID_REALNAME = 37,
+    PROPERTY_ID_DBASEPRECISIONCHANGED = 38,
+    PROPERTY_ID_ISCURRENCY = 39,
+    PROPERTY_ID_ISBOOKMARKABLE = 40,
 
-#define PROPERTY_ID_AUTOINCREMENTCREATION           49
+    PROPERTY_ID_INVALID_INDEX = 41,
+    PROPERTY_ID_HY010 = 43,
+    PROPERTY_ID_LABEL = 44,
+    PROPERTY_ID_DELIMITER = 45,
+    PROPERTY_ID_FORMATKEY = 46,
+    PROPERTY_ID_LOCALE = 47,
+    PROPERTY_ID_IM001 = 48,
 
-#define PROPERTY_ID_PRIVILEGES                      50
-#define PROPERTY_ID_HAVINGCLAUSE                    51
+    PROPERTY_ID_AUTOINCREMENTCREATION = 49,
 
-#define PROPERTY_ID_ISSIGNED                        52
-#define PROPERTY_ID_AGGREGATEFUNCTION               53
-#define PROPERTY_ID_ISSEARCHABLE                    54
+    PROPERTY_ID_PRIVILEGES = 50,
+    PROPERTY_ID_HAVINGCLAUSE = 51,
 
-#define PROPERTY_ID_APPLYFILTER                     55
-#define PROPERTY_ID_FILTER                          56
-#define PROPERTY_ID_MASTERFIELDS                    57
-#define PROPERTY_ID_DETAILFIELDS                    58
-#define PROPERTY_ID_FIELDTYPE                       59
-#define PROPERTY_ID_VALUE                           60
-#define PROPERTY_ID_ACTIVE_CONNECTION               61
+    PROPERTY_ID_ISSIGNED = 52,
+    PROPERTY_ID_AGGREGATEFUNCTION = 53,
+    PROPERTY_ID_ISSEARCHABLE = 54,
+
+    PROPERTY_ID_APPLYFILTER = 55,
+    PROPERTY_ID_FILTER = 56,
+    PROPERTY_ID_MASTERFIELDS = 57,
+    PROPERTY_ID_DETAILFIELDS = 58,
+    PROPERTY_ID_FIELDTYPE = 59,
+    PROPERTY_ID_VALUE = 60,
+    PROPERTY_ID_ACTIVE_CONNECTION = 61
+};
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
