LibreOffice/core

commit 1876feb8a8805b2f80537e2828c152ccbdf67fe2

tdf#136905 NBB: let ValueSetWithTextControl set optimal height
There was no way to specify a "good font size" to use
for .uno:AttributePageSize in the notebookbar.
The font "resized to match to size of the box"
which is hard-coded to aSize(250, 300).

(Even if ValueSet::SetOptimalSize worked, it would set an
inadequate height.)

So it seems like the best thing to do is simply add a function
that allows the box height to be modified.

Using the fontsize from GetDefaultFont is not correct.
Use the OS-defined label font size instead, which seems to be
the most common choice - GetPushButtonFont would has also worked.

I verified that the label font size is controled by
the OS' font preference.

The ability to define the box's optimal height
is still (somewhat) necessary. It isn't good enough
to just "use the system font size".
I tested with an OS font size of 48 (instead of 11),
and in that case the box height was too small
(but with the font only using 4/9's of a 12pt space,
even a 24pt font looked OK without adjusting optimal height).

Change-Id: I0a0774dea9c2a6c21a8e84439318a94f39783413
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/154286
Tested-by: Jenkins
Reviewed-by: Justin Luth <jluth@mail.com>

svx/source/sidebar/tools/ValueSetWithTextControl.cxx
@@ -43,6 +43,15 @@ void ValueSetWithTextControl::SetDrawingArea(weld::DrawingArea* pDrawingArea)
     SetColCount();
 }
 
+void ValueSetWithTextControl::SetOptimalDrawingAreaHeight()
+{
+    const vcl::Font aFont(Application::GetSettings().GetStyleSettings().GetLabelFont());
+    const sal_Int32 nRowHeight = aFont.GetFontSize().Height() * 9 / 4; // see UserDraw()
+    const Size aSize(GetOutputSizePixel().Width(), nRowHeight * maItems.size());
+    GetDrawingArea()->set_size_request(aSize.Width(), aSize.Height());
+    SetOutputSizePixel(aSize);
+}
+
 void ValueSetWithTextControl::AddItem(
     const OUString& rItemText,
     const OUString& rItemText2 )

include/svx/sidebar/ValueSetWithTextControl.hxx
@@ -40,6 +40,7 @@ public:
 
     SVX_DLLPRIVATE virtual void SetDrawingArea(weld::DrawingArea* pDrawingArea) override;
 
+    void SetOptimalDrawingAreaHeight();
     void AddItem(const OUString& rItemText, const OUString& rItemText2);
 
     SVX_DLLPRIVATE virtual void UserDraw(const UserDrawEvent& rUDEvt) override;

sw/source/uibase/sidebar/PageSizeControl.cxx
@@ -165,6 +165,7 @@ PageSizeControl::PageSizeControl(PageSizePopup* pControl, weld::Widget* pParent)
     }
     mxSizeValueSet->SetNoSelection();
     mxSizeValueSet->SetSelectHdl( LINK(this, PageSizeControl, ImplSizeHdl ) );
+    mxSizeValueSet->SetOptimalDrawingAreaHeight();
     mxSizeValueSet->Show();
     mxSizeValueSet->Resize();
 

commit 72986a5e505f8b62b30aa890c136e07cef3f9a2b

tdf#159865 tdf#136905 NBB: font looked too small in page size choices
This fixes my 24.2 regression
from commit 1876feb8a8805b2f80537e2828c152ccbdf67fe2

I considered reverting it, but decided to tweak the font size instead.
If I reverted, the size of the choices was displayed in 16pt font.

I wanted to keep the sizing as "scientific" as possible,
but fixing the rounding issues didn't really make the font
stand out as much as desired.
In this case, my system font of 11 became 10 for the page size dialog,
due to rounding, which is still a significant 10%.

Although that still looks too small, it seems fairly close
to the other button on the notebook bar, at least to my eyes.
But it is definitely smaller/less clear than the "more option" button.

Perhaps it has to do with the visible size of a font
compared to the full size (with leading)? Fonts are often
sized as 11/13 or 12/14 for example.
Adding 20% to the font size makes it look about right.
In my case, it ends up chosing a font height of 13.

Change-Id: Ic0a7296aa74be542e2a38ce52cf5fd23e5d8c7f4
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/163886
Tested-by: Jenkins
Reviewed-by: Justin Luth <jluth@mail.com>

svx/source/sidebar/tools/ValueSetWithTextControl.cxx
@@ -45,8 +45,10 @@ void ValueSetWithTextControl::SetDrawingArea(weld::DrawingArea* pDrawingArea)
 void ValueSetWithTextControl::SetOptimalDrawingAreaHeight()
 {
     const vcl::Font aFont(Application::GetSettings().GetStyleSettings().GetLabelFont());
-    const sal_Int32 nRowHeight = aFont.GetFontSize().Height() * 9 / 4; // see UserDraw()
-    const Size aSize(GetOutputSizePixel().Width(), nRowHeight * maItems.size());
+    double fRowHeight = aFont.GetFontSize().Height() * 9 / 4.0; // see UserDraw()
+    // It still looks too small. Probably the height specified should be the leading point size?
+    fRowHeight *= 1.2; // add 20% leading
+    const Size aSize(GetOutputSizePixel().Width(), fRowHeight * maItems.size());
     GetDrawingArea()->set_size_request(aSize.Width(), aSize.Height());
     SetOutputSizePixel(aSize);
 }