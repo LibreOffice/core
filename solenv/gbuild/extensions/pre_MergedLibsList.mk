LibreOffice/core

commit ae17922b7d0f797129dd5fbd5514d691ac9ebeaa

add more libs into --enable-mergelibs=more build
Change-Id: Idec36cb54e998c9e4830d5e51a6d2df7d94170fe
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/163350
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

solenv/gbuild/extensions/pre_MergedLibsList.mk
@@ -130,6 +130,11 @@ gb_MERGE_LIBRARY_LIST += \
 	date \
 	$(call gb_Helper_optional,DBCONNECTIVITY,dbpool2) \
 	dlgprov \
+	$(if $(ENABLE_EVOAB2),evoab) \
+	$(call gb_Helper_optional,DBCONNECTIVITY, \
+		flat \
+		file) \
+	$(if $(ENABLE_FIREBIRD_SDBC),firebird_sdbc) \
 	$(if $(filter WNT,$(OS)), \
 		fps \
 	) \
@@ -138,13 +143,26 @@ gb_MERGE_LIBRARY_LIST += \
 	) \
 	graphicfilter \
 	hwp \
+	$(if $(ENABLE_JAVA),hsqldb) \
 	log \
 	$(if $(ENABLE_LWP),lwpft) \
+	$(if $(filter $(OS),MACOSX), \
+		macab1 \
+		macabdrv1 \
+	) \
+	mozbootstrap \
 	msforms \
+	$(call gb_Helper_optional,DBCONNECTIVITY,mysql_jdbc) \
+	$(call gb_Helper_optional,MARIADBC,$(call gb_Helper_optional,DBCONNECTIVITY,mysqlc)) \
+	odbc \
 	pdffilter \
+	$(if $(BUILD_POSTGRESQL_SDBC), \
+		postgresql-sdbc \
+		postgresql-sdbc-impl) \
 	pricing \
 	$(call gb_Helper_optional,SCRIPTING,protocolhandler) \
 	$(call gb_Helper_optional,SCRIPTING,scriptframe) \
+	sdbc2 \
 	slideshow \
 	$(if $(filter WNT,$(OS)), \
 		smplmail \

commit 045e551875cbd68ead6371b56ff13b3842648c1a

remove hsqldb from mergedlibs
regression from
    commit ae17922b7d0f797129dd5fbd5514d691ac9ebeaa
    Author: Noel Grandin <noel.grandin@collabora.co.uk>
    Date:   Wed Feb 14 09:33:05 2024 +0200
    add more libs into --enable-mergelibs=more build

sberg is seeing
    This breaks e.g.  dbaccess_hsqldb_test  with
    java.lang.UnsatisfiedLinkError: java.lang.UnsatisfiedLinkError:

likely because the jar file is trying to load libhsqldb.so instead of
libmerged.so

Change-Id: I9f42aea6471dbe35b27a584465b333a1d30caf5b
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/164022
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

solenv/gbuild/extensions/pre_MergedLibsList.mk
@@ -144,7 +144,6 @@ gb_MERGE_LIBRARY_LIST += \
 	) \
 	graphicfilter \
 	hwp \
-	$(if $(ENABLE_JAVA),hsqldb) \
 	log \
 	$(if $(ENABLE_LWP),lwpft) \
 	$(if $(filter $(OS),MACOSX), \