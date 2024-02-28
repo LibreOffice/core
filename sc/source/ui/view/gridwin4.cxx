d0e2b97186925a687989d3a8c546d6dba98dc7ae
Caolán McNamara <caolan.mcnamara@collabora.com>
2024-02-28 12:04:28 +0000

    assertion fails on last rows selection / modification
    Open Calc hello world spreadsheet
    Click on cell in column H
    Press ctrl + shift + down arrow to select and go to the end of sheet (cell H1048576)
    Click on the H1048576 and type something, Save
    Refresh browser / reload spreadsheet
    try to select modified cell, remove content
    
    Result: Crash
    
    assert(nEndRow >= nStartRow);
    nEndRow   = 1048575
    nStartRow = 1048576
    
    likely an assert since:
    
    commit a86c00414a43c5d87981ffae1018cb242c5e5e1d
    Date:   Fri Jan 19 14:27:10 2024 +0200
    
        cool#6893 reduce allocation in ScGridWindow::PaintTile
    
    but seems harmless for this specific example
    
    Change-Id: I1d492c357a7a0a23b9f9cc974c4b9f684aba8648
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/163987
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>

--- sc/source/ui/view/gridwin4.cxx
+++ sc/source/ui/view/gridwin4.cxx
@@ -1544,8 +1544,14 @@ void ScGridWindow::PaintTile( VirtualDevice& rDevice,
     nBottomRightTileCol++;
     nBottomRightTileRow++;
 
+    if (nTopLeftTileCol > rDoc.MaxCol())
+        nTopLeftTileCol = rDoc.MaxCol();
+
     if (nBottomRightTileCol > rDoc.MaxCol())
         nBottomRightTileCol = rDoc.MaxCol();
+
+    if (nTopLeftTileRow > MAXTILEDROW)
+        nTopLeftTileRow = MAXTILEDROW;
 
     if (nBottomRightTileRow > MAXTILEDROW)
         nBottomRightTileRow = MAXTILEDROW;
