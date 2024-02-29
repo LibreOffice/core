LibreOffice/core

commit 89e032e9c4c51f52680c7d8bacf59ab2a34f2180

tdf#158314: show Empty and Error entries as non-selected and inactive...
...when hidden by autofilter.

The "show hidden filter elements as inactive" feature was added and improved by:

commit 2d1df9f3dccc10f13b8585ad18afce1542ebc4d1
(tdf#117276 sc: Show hidden filter elements as inactive elements)

commit 2085e90fe8ac129bc4dbac4612d1ea7544335dae
(FilteredRow is not a property of the column, tdf#117276 follow-up)

commit 7321db3cadc8c0e4437ca04e5dcb652734ea9c26
(Related tdf#117276 sc: Show hidden filter elements as inactive elements)

commit 19533948370dc1ccd7334dbe1a8b7fc8330b10c0
(Name FilteredRow what it is, not hidden; tdf#117276 follow-up)

Those changes correctly made normal hidden filter elements as inactive,
but failed to do so for Empty and Error entries.

This patch shows the hidden Empty and Error entries as inactive and unselected.
Also, do not show the Empty entry at the top of the list in case it is inactive.

Change-Id: Ibcd758cebc0692b04b162cdfc3e06eceb86b17da
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/162166
Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
Reviewed-by: Kevin Suo <suokunlong@126.com>
Tested-by: Kevin Suo <suokunlong@126.com>

sc/source/core/data/column3.cxx
@@ -2584,7 +2584,7 @@ class FilterEntriesHandler
         {
             if (!mrFilterEntries.mbHasEmpties)
             {
-                mrFilterEntries.push_back(ScTypedStrData(OUString()));
+                mrFilterEntries.push_back(ScTypedStrData(OUString(), 0.0, 0.0, ScTypedStrData::Standard, false, mbFilteredRow));
                 mrFilterEntries.mbHasEmpties = true;
             }
             return;
@@ -2614,7 +2614,7 @@ class FilterEntriesHandler
                     OUString aErr = ScGlobal::GetErrorString(nErr);
                     if (!aErr.isEmpty())
                     {
-                        mrFilterEntries.push_back(ScTypedStrData(std::move(aErr)));
+                        mrFilterEntries.push_back(ScTypedStrData(std::move(aErr), 0.0, 0.0, ScTypedStrData::Standard, false, mbFilteredRow));
                         return;
                     }
                 }

sc/qa/uitest/autofilter2/tdf158314_EmptyError.py
@@ -1,0 +1,94 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+from uitest.framework import UITestCase
+from uitest.uihelper.calc import enter_text_to_cell
+from uitest.uihelper.common import get_state_as_dict
+from libreoffice.uno.propertyvalue import mkPropertyValues
+from libreoffice.calc.document import is_row_hidden
+
+# Bug 158314 - Autofilter dropdown list always shows "Empty" entry as active
+
+class tdf158314_EmptyEntries(UITestCase):
+    def testTdf158314(self):
+        with self.ui_test.create_doc_in_start_center("calc") as calcDoc:
+            xCalcDoc = self.xUITest.getTopFocusWindow()
+            xGridWin = xCalcDoc.getChild("grid_window")
+
+            # Fill the sheet with test data
+            enter_text_to_cell(xGridWin, "A1", "a")
+            enter_text_to_cell(xGridWin, "A2", "1")
+            enter_text_to_cell(xGridWin, "A3", "2")
+            enter_text_to_cell(xGridWin, "A4", "3")
+            enter_text_to_cell(xGridWin, "A5", "4")
+
+            enter_text_to_cell(xGridWin, "B1", "b")
+            enter_text_to_cell(xGridWin, "B2", "5")
+            enter_text_to_cell(xGridWin, "B3", "")
+            enter_text_to_cell(xGridWin, "B4", "=1/0")
+            enter_text_to_cell(xGridWin, "B5", "8")
+
+            # Select the data range and set autofilter
+            xGridWin.executeAction("SELECT", mkPropertyValues({"RANGE": "A1:B5"}))
+            self.xUITest.executeCommand(".uno:DataFilterAutoFilter")
+
+            # Click the autofilter dropdown in column A
+            xGridWin.executeAction("LAUNCH", mkPropertyValues({"AUTOFILTER": "", "COL": "0", "ROW": "0"}))
+            xFloatWindow = self.xUITest.getFloatWindow()
+            xCheckListMenu = xFloatWindow.getChild("FilterDropDown")
+            xTreeList = xCheckListMenu.getChild("check_list_box")
+
+            #  Select the first entry only. Uncheck all other entries.
+            for i in xTreeList.getChildren():
+                if i != "0":
+                    xEntry = xTreeList.getChild(i)
+                    xEntry.executeAction("CLICK", tuple())
+
+            xOkButton = xFloatWindow.getChild("ok")
+            xOkButton.executeAction("CLICK", tuple())
+
+            # Check that only row#2 is visible
+            self.assertFalse(is_row_hidden(calcDoc, 1))
+            self.assertTrue(is_row_hidden(calcDoc, 2))
+            self.assertTrue(is_row_hidden(calcDoc, 3))
+            self.assertTrue(is_row_hidden(calcDoc, 4))
+
+            # Click the autofilter dropdown in column B
+            xGridWin.executeAction("LAUNCH", mkPropertyValues({"AUTOFILTER": "", "COL": "1", "ROW": "0"}))
+            xFloatWindow = self.xUITest.getFloatWindow()
+            xCheckListMenu = xFloatWindow.getChild("FilterDropDown")
+            xTreeList = xCheckListMenu.getChild("check_list_box")
+
+            # There should be at least one entry in the dropdown
+            # (i.e., the feature of showing inactive autofilter entries may be disabled)
+            self.assertEqual(True, len(xTreeList.getChildren()) >= 1)
+
+            for i in xTreeList.getChildren():
+                state = get_state_as_dict(xTreeList.getChild(i))
+                # The Text of the first element should be '5' and it should be checked and active
+                if i == "0":
+                    self.assertEqual("5", state["Text"])
+                    self.assertEqual("true", state["IsChecked"])
+                    self.assertEqual("true", state["IsSelected"])
+                    self.assertEqual("false", state["IsSemiTransparent"])
+                # All other elements (including the Empty and #DIV/0!) should be unchecked and inactive
+                else:
+                    self.assertEqual("false", state["IsChecked"])
+                    self.assertEqual("false", state["IsSelected"])
+                    self.assertEqual("true",  state["IsSemiTransparent"])
+
+            # Close the popup window
+            xOkButton = xFloatWindow.getChild("ok")
+            xOkButton.executeAction("CLICK", tuple())
+
+            # Check again that only row#2 is visible
+            self.assertFalse(is_row_hidden(calcDoc, 1))
+            self.assertTrue(is_row_hidden(calcDoc, 2))
+            self.assertTrue(is_row_hidden(calcDoc, 3))
+            self.assertTrue(is_row_hidden(calcDoc, 4))
+# vim: set shiftwidth=4 softtabstop=4 expandtab:

sc/source/ui/view/gridwin.cxx
@@ -1041,8 +1041,8 @@ void ScGridWindow::LaunchAutoFilterMenu(SCCOL nCol, SCROW nRow)
     mpAutoFilterPopup->setMemberSize(aFilterEntries.size());
     for (auto it = aFilterEntries.begin(); it != aFilterEntries.end(); ++it)
     {
-        // tdf#140745 show (empty) entry on top of the checkbox list
-        if (it->GetString().isEmpty())
+        // tdf#140745 show (empty) entry on top of the checkbox list if not hidden by filter
+        if (it->GetString().isEmpty() && !it->IsHiddenByFilter())
         {
             const OUString& aStringVal = it->GetString();
             const double aDoubleVal = it->GetValue();
@@ -1051,7 +1051,8 @@ void ScGridWindow::LaunchAutoFilterMenu(SCCOL nCol, SCROW nRow)
                 bSelected = aSelectedString.count(aStringVal) > 0;
             else if (bQueryByNonEmpty)
                 bSelected = false;
-            mpAutoFilterPopup->addMember(aStringVal, aDoubleVal, bSelected, it->IsHiddenByFilter());
+            // it->IsHiddenByFilter() is always false here so no need to evaluate it
+            mpAutoFilterPopup->addMember(aStringVal, aDoubleVal, bSelected, false);
             aFilterEntries.maStrData.erase(it);
             break;
         }

commit e66f5cb353bc0ea1020061bab4981380f98499b9

tdf#159420: Show empty option even if empty is filtered out
in other columns

Regression from 89e032e9c4c51f52680c7d8bacf59ab2a34f2180
"tdf#158314: show Empty and Error entries as non-selected and inactive...
...when hidden by autofilter."

The mbHasEmpties variable was added in
9c1826d98065c30411cbf2e731560165ca2b7668
"sc-perf: do not add a million empty filter entries just to sort and
discard"

Change-Id: Ie0d81fd57f68038fac62cb6a3442e93ed547167a
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/162752
Tested-by: Jenkins
Reviewed-by: Kevin Suo <suokunlong@126.com>

sc/inc/filterentries.hxx
@@ -18,11 +18,14 @@ struct ScFilterEntries
 {
     std::vector<ScTypedStrData> maStrData;
     bool                        mbHasDates;
-    bool                        mbHasEmpties;
+    bool                        mbHasHiddenEmpties;
+    bool                        mbHasUnHiddenEmpties;
     std::set<Color>             maTextColors;
     std::set<Color>             maBackgroundColors;
 
-    ScFilterEntries() : mbHasDates(false), mbHasEmpties(false) {}
+    ScFilterEntries() : mbHasDates(false),
+                        mbHasHiddenEmpties(false),
+                        mbHasUnHiddenEmpties(false) {}
 
     std::vector<ScTypedStrData>::iterator       begin()         { return maStrData.begin(); }
     std::vector<ScTypedStrData>::iterator       end()           { return maStrData.end(); }

sc/source/core/data/column3.cxx
@@ -2583,10 +2583,21 @@ class FilterEntriesHandler
 
         if (bIsEmptyCell)
         {
-            if (!mrFilterEntries.mbHasEmpties)
+            if (mbFilteredRow)
             {
-                mrFilterEntries.push_back(ScTypedStrData(OUString(), 0.0, 0.0, ScTypedStrData::Standard, false, mbFilteredRow));
-                mrFilterEntries.mbHasEmpties = true;
+                if (!mrFilterEntries.mbHasHiddenEmpties)
+                {
+                    mrFilterEntries.push_back(ScTypedStrData(OUString(), 0.0, 0.0, ScTypedStrData::Standard, false, true));
+                    mrFilterEntries.mbHasHiddenEmpties = true;
+                }
+            }
+            else
+            {
+                if (!mrFilterEntries.mbHasUnHiddenEmpties)
+                {
+                    mrFilterEntries.push_back(ScTypedStrData(OUString(), 0.0, 0.0, ScTypedStrData::Standard, false, false));
+                    mrFilterEntries.mbHasUnHiddenEmpties = true;
+                }
             }
             return;
         }

sc/qa/uitest/autofilter2/tdf159420.py
@@ -1,0 +1,126 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+from uitest.framework import UITestCase
+from uitest.uihelper.calc import enter_text_to_cell
+from uitest.uihelper.common import get_state_as_dict
+from libreoffice.uno.propertyvalue import mkPropertyValues
+from libreoffice.calc.document import is_row_hidden
+
+class tdf159420(UITestCase):
+
+    def testTdf159420(self):
+        with self.ui_test.create_doc_in_start_center("calc") as calcDoc:
+            xCalcDoc = self.xUITest.getTopFocusWindow()
+            xGridWin = xCalcDoc.getChild("grid_window")
+
+            # Fill the sheet with test data
+            enter_text_to_cell(xGridWin, "A1", "a")
+            enter_text_to_cell(xGridWin, "A2", "2")
+            enter_text_to_cell(xGridWin, "A3", "2")
+            enter_text_to_cell(xGridWin, "A4", "2")
+            enter_text_to_cell(xGridWin, "A5", "4")
+
+            enter_text_to_cell(xGridWin, "B1", "b")
+            enter_text_to_cell(xGridWin, "B2", "")
+            enter_text_to_cell(xGridWin, "B3", "")
+            enter_text_to_cell(xGridWin, "B4", "8")
+            enter_text_to_cell(xGridWin, "B5", "8")
+
+            enter_text_to_cell(xGridWin, "C1", "c")
+
+            # Select the data range and set autofilter
+            xGridWin.executeAction("SELECT", mkPropertyValues({"RANGE": "A1:C5"}))
+            self.xUITest.executeCommand(".uno:DataFilterAutoFilter")
+
+            # Click the autofilter dropdown in column A
+            xGridWin.executeAction("LAUNCH", mkPropertyValues({"AUTOFILTER": "", "COL": "0", "ROW": "0"}))
+            xFloatWindow = self.xUITest.getFloatWindow()
+            xCheckListMenu = xFloatWindow.getChild("FilterDropDown")
+            xTreeList = xCheckListMenu.getChild("check_list_box")
+
+            self.assertEqual(2, len(xTreeList.getChildren()))
+
+            xEntry1 = xTreeList.getChild(0)
+            self.assertEqual("2", get_state_as_dict(xEntry1)['Text'])
+            self.assertEqual("true", get_state_as_dict(xEntry1)['IsChecked'])
+            self.assertEqual("false", get_state_as_dict(xEntry1)['IsSemiTransparent'])
+
+            xEntry2 = xTreeList.getChild(1)
+            self.assertEqual("4", get_state_as_dict(xEntry2)['Text'])
+            self.assertEqual("true", get_state_as_dict(xEntry2)['IsChecked'])
+            self.assertEqual("false", get_state_as_dict(xEntry2)['IsSemiTransparent'])
+
+            #  Uncheck the second entry
+            xEntry2.executeAction("CLICK", tuple())
+
+            xOkButton = xFloatWindow.getChild("ok")
+            xOkButton.executeAction("CLICK", tuple())
+
+            # Check that only row#2 is visible
+            self.assertFalse(is_row_hidden(calcDoc, 1))
+            self.assertFalse(is_row_hidden(calcDoc, 2))
+            self.assertFalse(is_row_hidden(calcDoc, 3))
+            self.assertTrue(is_row_hidden(calcDoc, 4))
+
+            # Click the autofilter dropdown in column B
+            xGridWin.executeAction("LAUNCH", mkPropertyValues({"AUTOFILTER": "", "COL": "1", "ROW": "0"}))
+            xFloatWindow = self.xUITest.getFloatWindow()
+            xCheckListMenu = xFloatWindow.getChild("FilterDropDown")
+            xTreeList = xCheckListMenu.getChild("check_list_box")
+
+            self.assertEqual(2, len(xTreeList.getChildren()))
+
+            xEntry1 = xTreeList.getChild(0)
+            self.assertEqual("(empty)", get_state_as_dict(xEntry1)['Text'])
+            self.assertEqual("true", get_state_as_dict(xEntry1)['IsChecked'])
+            self.assertEqual("false", get_state_as_dict(xEntry1)['IsSemiTransparent'])
+
+            xEntry2 = xTreeList.getChild(1)
+            self.assertEqual("8", get_state_as_dict(xEntry2)['Text'])
+            self.assertEqual("true", get_state_as_dict(xEntry2)['IsChecked'])
+            self.assertEqual("false", get_state_as_dict(xEntry2)['IsSemiTransparent'])
+
+            #  Uncheck the first entry
+            xEntry1.executeAction("CLICK", tuple())
+
+            # Close the popup window
+            xOkButton = xFloatWindow.getChild("ok")
+            xOkButton.executeAction("CLICK", tuple())
+
+            self.assertTrue(is_row_hidden(calcDoc, 1))
+            self.assertTrue(is_row_hidden(calcDoc, 2))
+            self.assertFalse(is_row_hidden(calcDoc, 3))
+            self.assertTrue(is_row_hidden(calcDoc, 4))
+
+            # Click the autofilter dropdown in column C
+            xGridWin.executeAction("LAUNCH", mkPropertyValues({"AUTOFILTER": "", "COL": "2", "ROW": "0"}))
+            xFloatWindow = self.xUITest.getFloatWindow()
+            xCheckListMenu = xFloatWindow.getChild("FilterDropDown")
+            xTreeList = xCheckListMenu.getChild("check_list_box")
+
+            self.assertEqual(1, len(xTreeList.getChildren()))
+
+            xEntry1 = xTreeList.getChild(0)
+            self.assertEqual("(empty)", get_state_as_dict(xEntry1)['Text'])
+
+            # Without the fix in place, this test would have failed with
+            # AssertionError: 'true' != 'false'
+            self.assertEqual("true", get_state_as_dict(xEntry1)['IsChecked'])
+            self.assertEqual("false", get_state_as_dict(xEntry1)['IsSemiTransparent'])
+
+            # Close the popup window
+            xOkButton = xFloatWindow.getChild("ok")
+            xOkButton.executeAction("CLICK", tuple())
+
+            self.assertTrue(is_row_hidden(calcDoc, 1))
+            self.assertTrue(is_row_hidden(calcDoc, 2))
+            self.assertFalse(is_row_hidden(calcDoc, 3))
+            self.assertTrue(is_row_hidden(calcDoc, 4))
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab: