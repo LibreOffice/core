LibreOffice/core

commit 359e5af31c330a137fed6ba3140b77cd759c28bb

tdf#155917 sw: fix Repeat of Insert File with Fly
There is a fly frame anchored on the 1st node of the inserted file.

On Repeat, first SwUndoInsLayFormat::RepeatImpl() runs and inserts
another fly, adding 3 nodes before the body of the document, then
SwUndoInserts::RepeatImpl() creates SwPaM from its stored positions that
is off by 3 nodes and invalid (crossing cell boundaries).

The problem is that the way the Undo objects are created in
SwReader::Read() is wrong, and there is alreay a better way not to
create the SwUndoInsLayFormat manually from the outside but
automatically in SwUndoInserts::SetInsertRange(), which is already used
in Paste - why Insert File never used this has always been a mystery.

The only difference appears to be that SwReader::Read() also handled
at-page anchored flys which SetInsertRange() didn't, as these probably
can't be part of a Paste, but that is easy to add.

(regression from commit 28b77c89dfcafae82cf2a6d85731b643ff9290e5)

Change-Id: I19d7a4d668ae8bed0986162da6ec5b5ea3118091
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/162208
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/core/undo/untblk.cxx
@@ -176,6 +176,11 @@ bool SwUndoInserts::IsCreateUndoForNewFly(SwFormatAnchor const& rAnchor,
 {
     assert(nStartNode <= nEndNode);
 
+    if (rAnchor.GetAnchorId() == RndStdIds::FLY_AT_PAGE)
+    {
+        return true; // needed for SwUndoInserts/SwReader::Read()
+    }
+
     // check all at-char flys at the start/end nodes:
     // ExcludeFlyAtStartEnd will exclude them!
     SwNode const*const pAnchorNode = rAnchor.GetAnchorNode();

sw/source/filter/basflt/shellio.cxx
@@ -251,9 +251,7 @@ ErrCodeMsg SwReader::Read( const Reader& rOptions )
                 // ok, here IsAlive is a misnomer...
                 if (!aFlyFrameArr.IsAlive(pFrameFormat))
                 {
-                    if  (   (RndStdIds::FLY_AT_PAGE == rAnchor.GetAnchorId())
-                        // TODO: why is this not handled via SetInsertRange?
-                        ||  SwUndoInserts::IsCreateUndoForNewFly(rAnchor,
+                    if (SwUndoInserts::IsCreateUndoForNewFly(rAnchor,
                                 pUndoPam->GetPoint()->GetNodeIndex(),
                                 pUndoPam->GetMark()->GetNodeIndex()))
                     {
@@ -268,16 +266,6 @@ ErrCodeMsg SwReader::Read( const Reader& rOptions )
                         }
                         else
                         {
-                            if( bSaveUndo )
-                            {
-                                mxDoc->getIDocumentRedlineAccess().SetRedlineFlags_intern( eOld );
-                                // UGLY: temp. enable undo
-                                mxDoc->GetIDocumentUndoRedo().DoUndo(true);
-                                mxDoc->GetIDocumentUndoRedo().AppendUndo(
-                                    std::make_unique<SwUndoInsLayFormat>( pFrameFormat, SwNodeOffset(0), 0 ) );
-                                mxDoc->GetIDocumentUndoRedo().DoUndo(false);
-                                mxDoc->getIDocumentRedlineAccess().SetRedlineFlags_intern( RedlineFlags::Ignore );
-                            }
                             if( pFrameFormat->HasWriterListeners() )
                             {
                                 // Draw-Objects create a Frame when being inserted; thus delete them
@@ -313,7 +301,7 @@ ErrCodeMsg SwReader::Read( const Reader& rOptions )
         if( bSaveUndo )
         {
             mxDoc->getIDocumentRedlineAccess().SetRedlineFlags_intern( eOld );
-            pUndo->SetInsertRange( *pUndoPam, false );
+            pUndo->SetInsertRange(*pUndoPam, true);
             // UGLY: temp. enable undo
             mxDoc->GetIDocumentUndoRedo().DoUndo(true);
             mxDoc->GetIDocumentUndoRedo().AppendUndo( std::move(pUndo) );

commit cf4df3e51b609613134e3146f526d8e6b07946c3

sw: for Undo, add FLY_AT_PAGE in GetFlysAnchoredAt()
SwUndoInserts::IsCreateUndoForNewFly() now returns true for these, so
they get Undo objects on Paste, which is of course wrong.

(regression from commit 359e5af31c330a137fed6ba3140b77cd759c28bb)

Change-Id: Id5abb38c662a9c55826cde6d8b151613c852de72
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/164221
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/inc/undobj.hxx
@@ -244,7 +244,7 @@ class SwUndoInsLayFormat;
 namespace sw {
 
 std::optional<std::vector<SwFrameFormat*>>
-GetFlysAnchoredAt(SwDoc & rDoc, SwNodeOffset nSttNode);
+GetFlysAnchoredAt(SwDoc & rDoc, SwNodeOffset nSttNode, bool isAtPageIncluded);
 
 }
 

sw/source/core/doc/DocumentContentOperationsManager.cxx
@@ -5066,7 +5066,7 @@ bool DocumentContentOperationsManager::CopyImplImpl(SwPaM& rPam, SwPosition& rPo
     }
     else
     {
-        pFlys = sw::GetFlysAnchoredAt(rDoc, rPos.GetNodeIndex());
+        pFlys = sw::GetFlysAnchoredAt(rDoc, rPos.GetNodeIndex(), false);
         pFlysAtInsPos = pFlys ? &*pFlys : nullptr;
     }
 

sw/source/core/txtnode/ndtxt.cxx
@@ -462,7 +462,7 @@ SwTextNode *SwTextNode::SplitContentNode(const SwPosition & rPos,
     }
 
     bool bSplitFly = false;
-    std::optional<std::vector<SwFrameFormat*>> oFlys = sw::GetFlysAnchoredAt(GetDoc(), GetIndex());
+    std::optional<std::vector<SwFrameFormat*>> oFlys = sw::GetFlysAnchoredAt(GetDoc(), GetIndex(), false);
     if (oFlys.has_value())
     {
         // See if one of the flys is a split fly. If so, we need to keep

sw/source/core/undo/untblk.cxx
@@ -38,7 +38,7 @@
 namespace sw {
 
 std::optional<std::vector<SwFrameFormat*>>
-GetFlysAnchoredAt(SwDoc & rDoc, SwNodeOffset const nSttNode)
+GetFlysAnchoredAt(SwDoc & rDoc, SwNodeOffset const nSttNode, bool const isAtPageIncluded)
 {
     std::optional<std::vector<SwFrameFormat*>> pFrameFormats;
     const size_t nArrLen = rDoc.GetSpzFrameFormats()->size();
@@ -47,10 +47,11 @@ GetFlysAnchoredAt(SwDoc & rDoc, SwNodeOffset const nSttNode)
         SwFrameFormat *const pFormat = (*rDoc.GetSpzFrameFormats())[n];
         SwFormatAnchor const*const pAnchor = &pFormat->GetAnchor();
         SwNode const*const pAnchorNode = pAnchor->GetAnchorNode();
-        if (pAnchorNode
-             && nSttNode == pAnchorNode->GetIndex()
-             && ((pAnchor->GetAnchorId() == RndStdIds::FLY_AT_PARA)
-                 || (pAnchor->GetAnchorId() == RndStdIds::FLY_AT_CHAR)))
+        if ((pAnchorNode
+                && nSttNode == pAnchorNode->GetIndex()
+                && ((pAnchor->GetAnchorId() == RndStdIds::FLY_AT_PARA)
+                    || (pAnchor->GetAnchorId() == RndStdIds::FLY_AT_CHAR)))
+            || (isAtPageIncluded && pAnchor->GetAnchorId() == RndStdIds::FLY_AT_PAGE))
         {
             if (!pFrameFormats)
                 pFrameFormats.emplace();
@@ -89,7 +90,7 @@ SwUndoInserts::SwUndoInserts( SwUndoId nUndoId, const SwPaM& rPam )
         // These flys will be saved in pFrameFormats array (only flys which exist BEFORE insertion!)
         // Then in SwUndoInserts::SetInsertRange the flys saved in pFrameFormats will NOT create Undos.
         // m_FlyUndos will only be filled with newly inserted flys.
-        m_pFrameFormats = sw::GetFlysAnchoredAt(rDoc, m_nSttNode);
+        m_pFrameFormats = sw::GetFlysAnchoredAt(rDoc, m_nSttNode, true);
     }
     // consider Redline
     if( rDoc.getIDocumentRedlineAccess().IsRedlineOn() )
@@ -392,7 +393,7 @@ void SwUndoInserts::RedoImpl(::sw::UndoRedoContext & rContext)
     if( ( m_nSttNode != m_nEndNode || m_nSttContent != m_nEndContent ) && m_oUndoNodeIndex)
     {
         auto const pFlysAtInsPos(sw::GetFlysAnchoredAt(rDoc,
-            rPam.GetPoint()->GetNodeIndex()));
+            rPam.GetPoint()->GetNodeIndex(), false));
 
         ::std::optional<SwNodeIndex> oMvBkwrd = MovePtBackward(rPam);
 