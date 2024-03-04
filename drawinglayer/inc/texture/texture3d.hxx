0adeffdaef5f3d0afdde4426dce29b23a87fe46d
Patrick Luby <guibmacdev@gmail.com>
2024-03-03 19:34:09 -0500

    tdf#159515 rename impGetTransparence() to impGetAlpha()
    Commit 81994cb2b8b32453a92bcb011830fcb884f22ff3 makes
    impGetTransparence() return alpha instead of transparence. So,
    rename the function and skip reverting the result to obtain the
    alpha value.
    
    Change-Id: Ied3bf8ae4a0de2ae36074dba9f78ccc93480b15a
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/164322
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
    Reviewed-by: Patrick Luby <guibomacdev@gmail.com>
    Tested-by: Patrick Luby <guibomacdev@gmail.com>
    (cherry picked from commit b4caea04cb63cbdc25bab871a5b20aa16e2f3a77)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/164371
    Tested-by: Jenkins

--- drawinglayer/inc/texture/texture3d.hxx
+++ drawinglayer/inc/texture/texture3d.hxx
@@ -66,7 +66,7 @@ namespace drawinglayer::texture
 
             // helpers
             bool impIsValid(const basegfx::B2DPoint& rUV, sal_Int32& rX, sal_Int32& rY) const;
-            sal_uInt8 impGetTransparence(sal_Int32 rX, sal_Int32 rY) const;
+            sal_uInt8 impGetAlpha(sal_Int32 rX, sal_Int32 rY) const;
 
         public:
             GeoTexSvxBitmapEx(
