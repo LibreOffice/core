LibreOffice/core

commit 6d8da2b2deb4be2182ca1852cec7eb38a4c654eb

tdf#96943 sw Hide Whitespace: don't create pages for widow / orphan ...
... paragraphs that would otherwise fit nominal size of the page frame

Change-Id: I90c3de9150b17c951e1ac4158babb7a71afee9ee

sw/source/core/text/widorp.cxx
@@ -37,6 +37,10 @@
 #include "itrtxt.hxx"
 #include "sectfrm.hxx"
 #include "ftnfrm.hxx"
+#include "rootfrm.hxx"
+#include "viewopt.hxx"
+#include "pagefrm.hxx"
+#include "fmtfsize.hxx"
 
 #undef WIDOWTWIPS
 
@@ -126,8 +130,24 @@ bool SwTextFrameBreak::IsInside( SwTextMargin &rLine ) const
         // The Frame has a height to fit on the page.
         SwTwips nHeight =
             (*fnRect->fnYDiff)( (m_pFrame->GetUpper()->*fnRect->fnGetPrtBottom)(), m_nOrigin );
+        SwTwips nDiff = nHeight - nLineHeight;
+
+        SwViewShell* pShell = m_pFrame->getRootFrame()->GetCurrShell();
+        if (pShell && pShell->GetViewOptions()->IsWhitespaceHidden())
+        {
+            if (nDiff < 0)
+            {
+                SwPageFrame* pPageFrame = m_pFrame->FindPageFrame();
+                const SwFrameFormat* pPageFormat = static_cast<const SwFrameFormat*>(pPageFrame->GetRegisteredIn());
+                const Size& rPageSize = pPageFormat->GetFrameSize().GetSize();
+                long nWhitespace = rPageSize.getHeight() - pPageFrame->Frame().Height();
+                if (nWhitespace > -nDiff)
+                    nDiff = 0;
+            }
+        }
+
         // If everything is inside the existing frame the result is true;
-        bFit = nHeight >= nLineHeight;
+        bFit = nDiff >= 0;
 
         // --> OD #i103292#
         if ( !bFit )

sw/qa/extras/uiwriter/uiwriter.cxx
@@ -177,6 +177,7 @@ public:
     void testTdf77014();
     void testTdf92648();
     void testTdf96515();
+    void testTdf96943();
     void testTdf96536();
     void testTdf96479();
 
@@ -262,6 +263,7 @@ public:
     CPPUNIT_TEST(testTdf77014);
     CPPUNIT_TEST(testTdf92648);
     CPPUNIT_TEST(testTdf96515);
+    CPPUNIT_TEST(testTdf96943);
     CPPUNIT_TEST(testTdf96536);
     CPPUNIT_TEST(testTdf96479);
     CPPUNIT_TEST_SUITE_END();
@@ -2942,6 +2944,23 @@ void SwUiWriterTest::testTdf96515()
     calcLayout();
 
     // This was 2, a new page was created for the new paragraph.
+    CPPUNIT_ASSERT_EQUAL(1, getPages());
+}
+
+void SwUiWriterTest::testTdf96943()
+{
+    // Enable hide whitespace mode.
+    SwDoc* pDoc = createDoc("tdf96943.odt");
+    SwWrtShell* pWrtShell = pDoc->GetDocShell()->GetWrtShell();
+    SwViewOption aViewOptions(*pWrtShell->GetViewOptions());
+    aViewOptions.SetHideWhitespaceMode(true);
+    pWrtShell->ApplyViewOptions(aViewOptions);
+
+    // Insert a new character at the end of the document.
+    pWrtShell->SttEndDoc(/*bStt=*/false);
+    pWrtShell->Insert("d");
+
+    // This was 2, a new page was created for the new layout line.
     CPPUNIT_ASSERT_EQUAL(1, getPages());
 }
 

commit c5ac0ffae47dd2da74aaddd26e100aeb3b814c0b

tdf#159706 sw hide whitespace: fix lost follow on enabling this
Regression from commit 6d8da2b2deb4be2182ca1852cec7eb38a4c654eb
(tdf#96943 sw Hide Whitespace: don't create pages for widow / orphan
..., 2016-01-07), the reduced bugdoc has 3 paragraphs, 1.5 paragraphs
are on page 1, 1.5 paragraphs are on page 2. Hiding whitespace moves the
entire paragraph 2 to page 1, which is unexpected.

Checking the original use-case, the
CheckPageHeightValidForHideWhitespace() call in
SwTextFrameBreak::IsInside() can be removed, and still the original
"type character at the end of the document results in a new, unwanted
page" problem is not back. Likely at some point in the meantime we gave
up on these reduced page heights for the last page, so the original
tweak for widow/orphan lines is not needed anymore.

Fix the problem reverting the original, no longer needed fix, but
keeping its testTdf96943 test in the CppunitTest_sw_uiwriter4 suite.

Such reducing of last page doesn't happen with Word 2021, so probably
this change in behavior is wanted.

Change-Id: I5d46d0cd84d0071ffb9ef3f8e4ca7335e61bb226
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/164408
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

sw/CppunitTest_sw_core_text.mk
@@ -20,6 +20,7 @@ $(eval $(call gb_CppunitTest_add_exception_objects,sw_core_text, \
     sw/qa/core/text/porlay \
     sw/qa/core/text/porrst \
     sw/qa/core/text/text \
+    sw/qa/core/text/widorp \
 ))
 
 $(eval $(call gb_CppunitTest_use_libraries,sw_core_text, \

sw/source/core/text/widorp.cxx
@@ -131,11 +131,6 @@ bool SwTextFrameBreak::IsInside( SwTextMargin const &rLine ) const
             aRectFnSet.YDiff( aRectFnSet.GetPrtBottom(*m_pFrame->GetUpper()), m_nOrigin );
         SwTwips nDiff = nHeight - nLineHeight;
 
-        // Hide whitespace may require not to insert a new page.
-        SwPageFrame* pPageFrame = m_pFrame->FindPageFrame();
-        if (!pPageFrame->CheckPageHeightValidForHideWhitespace(nDiff))
-            nDiff = 0;
-
         // If everything is inside the existing frame the result is true;
         bFit = nDiff >= 0;
 

sw/qa/core/text/widorp.cxx
@@ -1,0 +1,65 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <swmodeltestbase.hxx>
+
+#include <memory>
+
+#include <IDocumentLayoutAccess.hxx>
+#include <doc.hxx>
+#include <frame.hxx>
+#include <layfrm.hxx>
+#include <pagefrm.hxx>
+#include <rootfrm.hxx>
+#include <txtfrm.hxx>
+#include <docsh.hxx>
+#include <wrtsh.hxx>
+#include <viewopt.hxx>
+
+namespace
+{
+/// Covers sw/source/core/text/widorp.cxx fixes.
+class Test : public SwModelTestBase
+{
+public:
+    Test()
+        : SwModelTestBase("/sw/qa/core/text/data/")
+    {
+    }
+};
+
+CPPUNIT_TEST_FIXTURE(Test, testHideWhitespaceWidorp)
+{
+    // Given a document with 3 paragraphs: 1.5 on page 1, 1.5 on page 2:
+    createSwDoc("hide-whitespace-widorp.odt");
+
+    // When hiding whitespace:
+    SwDocShell* pDocShell = getSwDocShell();
+    SwWrtShell* pWrtShell = pDocShell->GetWrtShell();
+    SwViewOption aViewOptions(*pWrtShell->GetViewOptions());
+    aViewOptions.SetHideWhitespaceMode(true);
+    pWrtShell->ApplyViewOptions(aViewOptions);
+    calcLayout();
+
+    // Then make sure that paragraph 2 is still split:
+    SwDoc* pDoc = pDocShell->GetDoc();
+    SwRootFrame* pLayout = pDoc->getIDocumentLayoutAccess().GetCurrentLayout();
+    auto pPage = pLayout->Lower()->DynCastPageFrame();
+    CPPUNIT_ASSERT(pPage);
+    SwFrame* pBody = pPage->FindBodyCont();
+    SwFrame* pPara1 = pBody->GetLower();
+    auto pPara2 = pPara1->GetNext()->DynCastTextFrame();
+    CPPUNIT_ASSERT(pPara2);
+    // Without the accompanying fix in place, this test would have failed, the entire paragraph 2
+    // went to page 1, so it was not split anymore.
+    CPPUNIT_ASSERT(pPara2->HasFollow());
+}
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */