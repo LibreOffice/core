LibreOffice/core

commit 477e489e71b4a96ff10d9f2d2b802d91dec3e319

forcepoint#108 sw: tweak a bit
* the fly-at-fly case didn't work, iteration also skips it
* delete the bad redline section in the nodes array

Change-Id: I21d3635fb53068e79984b0ea74a0a0913513bd51
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/138434
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/filter/xml/XMLRedlineImportHelper.cxx
@@ -567,7 +567,7 @@ inline bool XMLRedlineImportHelper::IsReady(const RedlineInfo* pRedline)
              !pRedline->bNeedsAdjustment );
 }
 
-/// recursively check if rPos or its anchor (if in fly or footnote) is in rPam
+/// recursively check if rPos or its anchor (if in fly or footnote) is in redline section
 static auto RecursiveContains(SwStartNode const& rRedlineSection, SwNode const& rPos) -> bool
 {
     if (rRedlineSection.GetIndex() <= rPos.GetIndex()
@@ -596,10 +596,13 @@ static auto RecursiveContains(SwStartNode const& rRedlineSection, SwNode const& 
                     return false;
                 }
                 else if (rAnchor.GetAnchorId() == RndStdIds::FLY_AT_FLY)
-                {   // anchor is on a start node - loop! recursion will take *its* start node and skip it.
+                {   // anchor is on a start node, avoid skipping it:
                     pStartNode = rAnchor.GetContentAnchor()->GetNode().GetStartNode();
                     assert(pStartNode);
-                    continue;
+                    // pass the next node to recursive call - it will call
+                    // call StartOfSectionNode on it and go back to pStartNode
+                    SwNodeIndex const next(*pStartNode, +1);
+                    return RecursiveContains(rRedlineSection, next.GetNode());
                 }
                 else
                 {
@@ -622,6 +625,9 @@ static auto RecursiveContains(SwStartNode const& rRedlineSection, SwNode const& 
             case SwHeaderStartNode:
             case SwFooterStartNode:
                 return false; // headers aren't anchored
+            break;
+            default:
+                assert(false);
             break;
         }
     }
@@ -709,7 +715,12 @@ void XMLRedlineImportHelper::InsertIntoDocument(RedlineInfo* pRedlineInfo)
         // should be enough to check 1 position of aPaM bc CheckNodesRange() above
         && RecursiveContains(*pRedlineInfo->pContentIndex->GetNode().GetStartNode(), aPaM.GetPoint()->GetNode()))
     {
-        SAL_WARN("sw", "Recursive change tracking, ignoring");
+        SAL_WARN("sw.xml", "Recursive change tracking, removing");
+        // reuse aPaM to remove it from nodes that will be deleted
+        *aPaM.GetPoint() = SwPosition(pRedlineInfo->pContentIndex->GetNode());
+        aPaM.SetMark();
+        *aPaM.GetMark() = SwPosition(*pRedlineInfo->pContentIndex->GetNode().EndOfSectionNode());
+        pDoc->getIDocumentContentOperations().DeleteRange(aPaM);
     }
     else
     {

commit 126dd55667826f5d1268a570f7e2930b1b78fc43

tdf#152710 fixes squashed required by testTdf134250
tdf#152710 sw: invalidate SwUnoCursors properly in DeleteRangeImpl()

This crashes with:

 list.cxx:44: corrupt document structure, bailing out of infinite loop
 ndtxt.cxx:5437: void SwTextNode::TriggerNodeUpdate(const sw::LegacyModifyHint&): Assertion `dynamic_cast<SwTextFormatColl const*>(static_cast<const SwFormatChg*>(pOldValue)->pChangedFormat)' failed.

Because the redline from 7 to 9 is deleted, but then some cursor ends up
on node 10 which is invalid as it is an end node.

[   6] 0x60666a0            StartNode ,
[   7]  0x61195e0           StartNode ,
[   8]   0x61197a8           TextNode "tainment",
[   9]  0x6119670             EndNode ,
[  10] 0x6066730              EndNode ,

The first problem is that DeleteRangeImpl() uses the point node as the
target position for PaMCorrAbs(), but in this case the point node will
be deleted.

PaMCorrAbs() has a check to invalidate SwUnoCursors that would be moved
out of their parent sections, but due to the first problem it can't
check it, and the second problem is that lcl_FindUnoCursorSection()
doesn't work on redline sections, as those have node type
SwNormalStartNode.

After fixing the invalidation, subsequent access to the SwXTextCursor
throws exceptions and importing the file fails.

(regression from commit 477e489e71b4a96ff10d9f2d2b802d91dec3e319)

Thanks to Dave Gilbert for identifying the problematic DeleteRange()
call.

Change-Id: I48a373cc122073b82bc47513fdae684f45b0efb8
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/145077
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
(cherry picked from commit 8e05bdd26f21fc304978ff3b454cf355841ec75f)

tdf#152710 sw: call and fix DeleteSection() instead

Turns out there's a function to delete a complete nodes array section -
and it has the same problem?  Why does it move indexes only from
startnode + 1?  Let's try to fix it to be more consistent.

sw/source/core/doc/DocumentContentOperationsManager.cxx
@@ -1972,6 +1972,18 @@ DocumentContentOperationsManager::CopyRange( SwPaM& rPam, SwPosition& rPos, cons
     return bRet;
 }
 
+static auto GetCorrPosition(SwPaM const& rPam) -> SwPosition
+{
+    // tdf#152710 target position must be on node that survives deletion
+    // so that PaMCorrAbs can invalidate SwUnoCursors properly
+    return rPam.GetPoint()->nNode.GetNode().IsContentNode()
+            ? *rPam.GetPoint()
+            : rPam.GetMark()->nNode.GetNode().IsContentNode()
+                ? *rPam.GetMark()
+                // this would be the result in SwNodes::RemoveNode()
+                : SwPosition(SwNodeIndex(rPam.End()->nNode.GetNode(), +1));
+}
+
 /// Delete a full Section of the NodeArray.
 /// The passed Node is located somewhere in the designated Section.
 void DocumentContentOperationsManager::DeleteSection( SwNode *pNode )
@@ -1989,8 +2001,9 @@ void DocumentContentOperationsManager::DeleteSection( SwNode *pNode )
 
     {
         // move all Cursor/StackCursor/UnoCursor out of the to-be-deleted area
-        SwNodeIndex aMvStt( aSttIdx, 1 );
-        SwDoc::CorrAbs( aMvStt, aEndIdx, SwPosition( aSttIdx ), true );
+        SwPaM const range(aSttIdx, aEndIdx);
+        SwPosition const pos(GetCorrPosition(range));
+        ::PaMCorrAbs(range, pos);
     }
 
     m_rDoc.GetNodes().DelNodes( aSttIdx, aEndIdx.GetIndex() - aSttIdx.GetIndex() + 1 );
@@ -4098,7 +4111,10 @@ bool DocumentContentOperationsManager::DeleteRangeImpl(SwPaM & rPam, SwDeleteFla
     // Move all cursors out of the deleted range, but first copy the
     // passed PaM, because it could be a cursor that would be moved!
     SwPaM aDelPam( *rPam.GetMark(), *rPam.GetPoint() );
-    ::PaMCorrAbs( aDelPam, *aDelPam.GetPoint() );
+    {
+        SwPosition const pos(GetCorrPosition(aDelPam));
+        ::PaMCorrAbs(aDelPam, pos);
+    }
 
     bool const bSuccess( DeleteRangeImplImpl(aDelPam, flags) );
     if (bSuccess)

sw/source/core/doc/doccorr.cxx
@@ -33,11 +33,14 @@ namespace
     /// returns NULL if no restrictions apply
     const SwStartNode* lcl_FindUnoCursorSection( const SwNode& rNode )
     {
-        const SwStartNode* pStartNode = rNode.StartOfSectionNode();
+        const SwStartNode* pStartNode = rNode.IsStartNode() ? rNode.GetStartNode() : rNode.StartOfSectionNode();
         while( ( pStartNode != nullptr ) &&
                ( pStartNode->StartOfSectionNode() != pStartNode ) &&
-               ( pStartNode->GetStartNodeType() == SwNormalStartNode ) )
+               // section node is only start node allowing overlapped delete
+               pStartNode->IsSectionNode() )
+        {
             pStartNode = pStartNode->StartOfSectionNode();
+        }
 
         return pStartNode;
     }

sw/source/filter/xml/XMLRedlineImportHelper.cxx
@@ -726,9 +726,8 @@ void XMLRedlineImportHelper::InsertIntoDocument(RedlineInfo* pRedlineInfo)
         SAL_WARN("sw.xml", "Recursive change tracking, removing");
         // reuse aPaM to remove it from nodes that will be deleted
         *aPaM.GetPoint() = SwPosition(pRedlineInfo->pContentIndex->GetNode());
-        aPaM.SetMark();
-        *aPaM.GetMark() = SwPosition(*pRedlineInfo->pContentIndex->GetNode().EndOfSectionNode());
-        pDoc->getIDocumentContentOperations().DeleteRange(aPaM);
+        aPaM.DeleteMark();
+        pDoc->getIDocumentContentOperations().DeleteSection(&aPaM.GetPoint()->nNode.GetNode());
     }
     else
     {