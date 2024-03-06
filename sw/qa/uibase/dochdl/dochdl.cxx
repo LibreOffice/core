LibreOffice/core

commit 7a8dc25defee31edbb75a2f8c35f92ee2d3f3a83

sw lok: simplify SwTransferable::isComplex()
Commit 169a87563a3940299811d874b4df0ad13591771c (LOK: Implement
getSelectionType, 2019-06-24) implemented detecting complex selections
by copying the selection to a new SwDoc, which probably had two
benefits: first, the created SwPaM instance didn't touch the document;
second, this means no bounds has to be set when scanning nodes: the
entire document is the selection.

Later commit 7fe30d1cb00c576469d6cbe5606268a9cdf35bd3 (LOK: detect
Graphics in isComplex for Writer, 2019-06-25) got rid of the SwPaM
(which would register itself into text nodes), so now it's possible to
not touch the document, even if we work on the original document.

Instead, solve the partial scanning by iterating over the list of
selections. This is meant to be faster, and also less likely to crash in
case some internal document model invariant is broken.

No testcase, testComplexSelection in CppunitTest_desktop_lib already
covers thie behavior.

Change-Id: Ia528c06a48defd06c5e34ed7d61621e5ce10bb06
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/111377
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

sw/source/uibase/dochdl/swdtflvr.cxx
@@ -119,6 +119,7 @@
 #include <iodetect.hxx>
 #include <unotextrange.hxx>
 #include <unoframe.hxx>
+#include <txatbase.hxx>
 #include <vcl/uitest/logger.hxx>
 #include <vcl/uitest/eventdescription.hxx>
 
@@ -426,33 +427,34 @@ namespace
 
 sal_Bool SAL_CALL SwTransferable::isComplex()
 {
-    // Copy into a new Doc so we don't mess with the existing one.
-    //FIXME: We *should* be able to avoid this and improve the performance.
-    m_pClpDocFac.reset(new SwDocFac);
-    SwDoc& rTmpDoc = lcl_GetDoc(*m_pClpDocFac);
-
-    rTmpDoc.getIDocumentFieldsAccess()
-        .LockExpFields(); // never update fields - leave text as it is
-    lclOverWriteDoc(*m_pWrtShell, rTmpDoc);
-
     sal_Int32 nTextLength = 0;
-    const SwNode* pEndOfContent = &m_pWrtShell->GetDoc()->GetNodes().GetEndOfContent();
-    SwNodes& aNodes = rTmpDoc.GetNodes();
-    for( sal_uLong nIndex = 0; nIndex < aNodes.Count(); ++nIndex)
+    SwNodes& aNodes = m_pWrtShell->GetDoc()->GetNodes();
+    for (SwPaM& rPaM : m_pWrtShell->GetCursor()->GetRingContainer())
     {
-        SwNode& rNd = *aNodes[nIndex];
-        if (&rNd == pEndOfContent)
-            break;
-
-        if (rNd.IsOLENode() || rNd.IsGrfNode())
-            return true; // Complex
-
-        SwTextNode* pTextNode = rNd.GetTextNode();
-        if (pTextNode)
+        for (sal_uLong nIndex = rPaM.GetMark()->nNode.GetIndex();
+             nIndex <= rPaM.GetPoint()->nNode.GetIndex(); ++nIndex)
         {
-            nTextLength += pTextNode->GetText().getLength();
-            if (nTextLength >= 1024 * 512)
-                return true; // Complex
+            SwNode& rNd = *aNodes[nIndex];
+
+            SwTextNode* pTextNode = rNd.GetTextNode();
+            if (pTextNode)
+            {
+                if (pTextNode->HasHints())
+                {
+                    for (size_t nHint = 0; pTextNode->GetSwpHints().Count(); ++nHint)
+                    {
+                        SwTextAttr* pHint = pTextNode->GetSwpHints().Get(nHint);
+                        if (pHint->Which() == RES_TXTATR_FLYCNT)
+                        {
+                            return true; // Complex
+                        }
+                    }
+                }
+
+                nTextLength += pTextNode->GetText().getLength();
+                if (nTextLength >= 1024 * 512)
+                    return true; // Complex
+            }
         }
     }
 

commit 1bca99617ad54d966625caadd71e52134c70ae44

cool#8465 sw lok: classify anchored images as complex selection
Regression from commit 7a8dc25defee31edbb75a2f8c35f92ee2d3f3a83 (sw lok:
simplify SwTransferable::isComplex(), 2021-02-23), in case as-char
imagse were part of a selection, we considered that complex, but at-char
was considered as simple, which is inconsistent.

This was not intentional, simply the rework to avoid copying the
selection to a temporary document lost this functionality.

Fix the problem by using CollectFrameAtNode() to find at-char images,
which tries to use the layout, so is not meant to be too slow.

An alternative would be sw::GetFlysAnchoredAt(), but that doesn't try to
use the layout, so avoid that.

Change-Id: I647d0f3934f9553de69afbf2de059631e2b5619c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/164453
Tested-by: Jenkins
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

sw/source/uibase/dochdl/swdtflvr.cxx
@@ -122,6 +122,7 @@
 #include <unotextrange.hxx>
 #include <unoframe.hxx>
 #include <txatbase.hxx>
+#include <unoparaframeenum.hxx>
 #include <vcl/uitest/logger.hxx>
 #include <vcl/uitest/eventdescription.hxx>
 
@@ -457,6 +458,14 @@ sal_Bool SAL_CALL SwTransferable::isComplex()
                             return true; // Complex
                         }
                     }
+                }
+
+                FrameClientSortList_t vFrames;
+                ::CollectFrameAtNode(rNd, vFrames, true);
+                if (!vFrames.empty())
+                {
+                    // There is an at-char anchored object to this node, that's complex.
+                    return true;
                 }
 
                 nTextLength += pTextNode->GetText().getLength();

sw/qa/uibase/dochdl/dochdl.cxx
@@ -18,6 +18,7 @@
 #include <swdtflvr.hxx>
 #include <wrtsh.hxx>
 #include <view.hxx>
+#include <fmtanchr.hxx>
 
 /// Covers sw/source/uibase/dochdl/ fixes.
 class SwUibaseDochdlTest : public SwModelTestBase
@@ -74,6 +75,31 @@ CPPUNIT_TEST_FIXTURE(SwUibaseDochdlTest, testComplexSelection)
     // Without the accompanying fix in place, this test would have crashed, because we read past the
     // end of the hints array.
     CPPUNIT_ASSERT(!xTransfer->isComplex());
+}
+
+CPPUNIT_TEST_FIXTURE(SwUibaseDochdlTest, testComplexSelectionAtChar)
+{
+    // Given a document with an at-char anchored image:
+    createSwDoc();
+    SwDoc* pDoc = getSwDoc();
+    SwDocShell* pDocShell = pDoc->GetDocShell();
+    SwWrtShell* pWrtShell = pDocShell->GetWrtShell();
+    SfxItemSet aFrameSet(pDoc->GetAttrPool(), svl::Items<RES_FRMATR_BEGIN, RES_FRMATR_END - 1>);
+    SwFormatAnchor aAnchor(RndStdIds::FLY_AT_CHAR);
+    aFrameSet.Put(aAnchor);
+    Graphic aGrf;
+    pWrtShell->SwFEShell::Insert(OUString(), OUString(), &aGrf, &aFrameSet);
+    pWrtShell->UnSelectFrame();
+
+    // When checking if the selection is simple or complex:
+    pWrtShell->SelAll();
+    uno::Reference<datatransfer::XTransferable2> xTransfer = new SwTransferable(*pWrtShell);
+    bool bComplex = xTransfer->isComplex();
+
+    // Then make sure it's complex:
+    // Without the accompanying fix in place, this test would have failed, a selection containing an
+    // image was considered simple.
+    CPPUNIT_ASSERT(bComplex);
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();