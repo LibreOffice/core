LibreOffice/core

commit c303981cfd95ce1c3881366023d5495ae2edce97

tdf#156724 sw: layout: fix tables not splitting due to footnotes differently
Revert commit 610c6f02b11b4b4c555a78b0feb2a1eb35159e39 and
and 61a78a523a6131ff98b5d846368e5626fe58d99c instead do the
opposite: never calc content frames in FormatLayout().

There were a few cases where documents looked worse with the fix, such
as the somewhat pathological tdf120139-1.odt and tdf124474-1.odt, but
typically these went from a bad layout to a worse layout, e.g.
--convert-to pdf tdf120139-1.odt went from 11 minutes to 33 minutes
(dbgutil) with twice as many more half-empty pages.

Worse is that the previous fix appears to prevent tdf#128437 from
working.

It should also be less risky to never calc content frames in
FormatLayout(), because with IsPaintLocked() check that used to be done
led to doing it only for pages which were visible when loading the
document, so any regressions with this new approach would appear on few
pages at the start of the document, instead of many pages at the end.

Note that without the previous commit, this would cause
* CppunitTest_sw_core_layout CPPUNIT_TEST_NAME="testTablePrintAreaLeft"
  to fail
* tdf#137523 SwLayoutWriter3 testTdf137523 to fail,
  *only* on the last text frame

This also appears to fix tdf#125749.

Change-Id: I3d72f8e9d2b89aa3738e554308fd9dce12e92238
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/155988
Tested-by: Michael Stahl <michael.stahl@allotropia.de>
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/core/layout/tabfrm.cxx
@@ -646,7 +646,8 @@ inline void TableSplitRecalcLock( SwFlowFrame *pTab ) { pTab->LockJoin(); }
 inline void TableSplitRecalcUnlock( SwFlowFrame *pTab ) { pTab->UnlockJoin(); }
 
 static bool lcl_RecalcSplitLine( SwRowFrame& rLastLine, SwRowFrame& rFollowLine,
-                          SwTwips nRemainingSpaceForLastRow, SwTwips nAlreadyFree )
+                          SwTwips nRemainingSpaceForLastRow, SwTwips nAlreadyFree,
+                          bool & rIsFootnoteGrowth)
 {
     bool bRet = true;
 
@@ -654,6 +655,34 @@ static bool lcl_RecalcSplitLine( SwRowFrame& rLastLine, SwRowFrame& rFollowLine,
     SwTabFrame& rTab = static_cast<SwTabFrame&>(*rLastLine.GetUpper());
     SwRectFnSet aRectFnSet(rTab.GetUpper());
     SwTwips nCurLastLineHeight = aRectFnSet.GetHeight(rLastLine.getFrameArea());
+
+    SwTwips nFootnoteHeight(0);
+    if (SwFootnoteBossFrame const*const pBoss = rTab.FindFootnoteBossFrame())
+    {
+        if (SwFootnoteContFrame const*const pCont = pBoss->FindFootnoteCont())
+        {
+            for (SwFootnoteFrame const* pFootnote = static_cast<SwFootnoteFrame const*>(pCont->Lower());
+                 pFootnote != nullptr;
+                 pFootnote = static_cast<SwFootnoteFrame const*>(pFootnote->GetNext()))
+            {
+                SwContentFrame const*const pAnchor = pFootnote->GetRef();
+                SwTabFrame const* pTab = pAnchor->FindTabFrame();
+                if (pTab == &rTab)
+                {
+                    while (pTab->GetUpper()->IsInTab())
+                    {
+                        pTab = pTab->GetUpper()->FindTabFrame();
+                    }
+                    // TODO currently do this only for top-level tables?
+                    // otherwise would need to check rTab's follow and any upper table's follow?
+                    if (pTab == &rTab)
+                    {
+                        nFootnoteHeight += aRectFnSet.GetHeight(pFootnote->getFrameArea());
+                    }
+                }
+            }
+        }
+    }
 
     // If there are nested cells in rLastLine, the recalculation of the last
     // line needs some preprocessing.
@@ -759,7 +788,15 @@ static bool lcl_RecalcSplitLine( SwRowFrame& rLastLine, SwRowFrame& rFollowLine,
                     {
                         nFollowFootnotes += aRectFnSet.GetHeight(pFootnote->getFrameArea());
                     }
+                    if (pTab == &rTab)
+                    {
+                        nFootnoteHeight -= aRectFnSet.GetHeight(pFootnote->getFrameArea());
+                    }
                 }
+            }
+            if (nFootnoteHeight < 0)
+            {   // tdf#156724 footnotes have grown, try to split again
+                rIsFootnoteGrowth = true;
             }
         }
     }
@@ -1020,7 +1057,8 @@ static bool lcl_FindSectionsInRow( const SwRowFrame& rRow )
     return bRet;
 }
 
-bool SwTabFrame::Split( const SwTwips nCutPos, bool bTryToSplit, bool bTableRowKeep )
+bool SwTabFrame::Split(const SwTwips nCutPos, bool bTryToSplit,
+        bool bTableRowKeep, bool & rIsFootnoteGrowth)
 {
     bool bRet = true;
 
@@ -1381,7 +1419,7 @@ bool SwTabFrame::Split( const SwTwips nCutPos, bool bTryToSplit, bool bTableRowK
         // we also don't shrink here, because we will be doing that in lcl_RecalcSplitLine
 
         // recalculate the split line
-        bRet = lcl_RecalcSplitLine( *pLastRow, *pFollowRow, nRemainingSpaceForLastRow, nShrink );
+        bRet = lcl_RecalcSplitLine(*pLastRow, *pFollowRow, nRemainingSpaceForLastRow, nShrink, rIsFootnoteGrowth);
 
         // RecalcSplitLine did not work. In this case we conceal the split error:
         if (!bRet && !bSplitRowAllowed)
@@ -2641,7 +2679,10 @@ void SwTabFrame::MakeAll(vcl::RenderContext* pRenderContext)
                     }
 
                     oAccess.reset();
-                    const bool bSplitError = !Split( nDeadLine, bTryToSplit, ( bTableRowKeep && !(bAllowSplitOfRow || bEmulateTableKeepSplitAllowed) ) );
+                    bool isFootnoteGrowth(false);
+                    const bool bSplitError = !Split(nDeadLine, bTryToSplit,
+                        (bTableRowKeep && !(bAllowSplitOfRow || bEmulateTableKeepSplitAllowed)),
+                        isFootnoteGrowth);
 
                     // tdf#130639 don't start table on a new page after the fallback "switch off repeating header"
                     if (bSplitError && nRepeat > GetTable()->GetRowsToRepeat())
@@ -2674,7 +2715,11 @@ void SwTabFrame::MakeAll(vcl::RenderContext* pRenderContext)
                     {
                         lcl_RecalcRow(*static_cast<SwRowFrame*>(Lower()), LONG_MAX);
                         setFrameAreaPositionValid(false);
-                        bTryToSplit = false;
+                        // tdf#156724 if the table added footnotes, try to split *again*
+                        if (!isFootnoteGrowth)
+                        {
+                            bTryToSplit = false;
+                        }
                         continue;
                     }
 

sw/source/core/inc/tabfrm.hxx
@@ -108,7 +108,7 @@ class SW_DLLPUBLIC SwTabFrame final: public SwLayoutFrame, public SwFlowFrame
      * created and constructed and inserted directly after this.
      * Join() gets the Follow's content and destroys it.
      */
-    bool Split( const SwTwips nCutPos, bool bTryToSplit, bool bTableRowKeep );
+    bool Split(const SwTwips nCutPos, bool bTryToSplit, bool bTableRowKeep, bool & rIsFootnoteGrowth);
     void Join();
 
     void UpdateAttr_(

sw/source/core/layout/layact.cxx
@@ -1431,10 +1431,7 @@ bool SwLayAction::FormatLayout( OutputDevice *pRenderContext, SwLayoutFrame *pLa
                 PopFormatLayout();
             }
         }
-        else if (!pLay->IsColBodyFrame())
-        {   // tdf#156724 unconditionally for frames in tables, so their footnotes exist before trying to split
-            pLow->OptCalc();
-        }
+        // else: don't calc content frames any more
 
         if ( IsAgain() )
             return false;

commit 7d3b439de357c8e97c0a73f6f63e67ec980e432d

tdf#157241 sw: layout: inconsistent conditions in lcl_RecalcSplitLine()
A mistake, when checking before the condition is "if (pTab == &rTab)"
but when checking after it is "if (pTab)" - the latter is correct as the
former is checked *inside* the block, after resolving to top-level table.

This caused erroneously setting rIsFootnoteGrowth = true in
SwTabFrame::MakeAll() of tab 939 in the bugdoc, when in fact the footnote
container shrunk from 5 to 2 footnotes.

(regression from commit c303981cfd95ce1c3881366023d5495ae2edce97)

Change-Id: Ib4cad9fef0e500ebe7a3636c2371a4108b32fb5f
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/164717
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/core/layout/tabfrm.cxx
@@ -667,7 +667,7 @@ static bool lcl_RecalcSplitLine( SwRowFrame& rLastLine, SwRowFrame& rFollowLine,
             {
                 SwContentFrame const*const pAnchor = pFootnote->GetRef();
                 SwTabFrame const* pTab = pAnchor->FindTabFrame();
-                if (pTab == &rTab)
+                if (pTab)
                 {
                     while (pTab->GetUpper()->IsInTab())
                     {