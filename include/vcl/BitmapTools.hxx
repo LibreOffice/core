LibreOffice/core

commit acd40306b3737d50cf24462b2d3e451092543e64

reduce symbol visibility in vcl
Change-Id: Ifaa37246e79678aa0c548b9ca8a9faf8036b9195
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/164804
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

include/vcl/BitmapTools.hxx
@@ -31,11 +31,11 @@ namespace vcl::bitmap {
 #if !ENABLE_WASM_STRIP_PREMULTIPLY
 typedef std::array<std::array<sal_uInt8, 256>, 256> lookup_table;
 
-VCL_DLLPUBLIC lookup_table const & get_premultiply_table();
-VCL_DLLPUBLIC lookup_table const & get_unpremultiply_table();
+lookup_table const & get_premultiply_table();
+lookup_table const & get_unpremultiply_table();
 #endif
 
-VCL_DLLPUBLIC sal_uInt8 unpremultiply(sal_uInt8 c, sal_uInt8 a);
+sal_uInt8 unpremultiply(sal_uInt8 c, sal_uInt8 a);
 VCL_DLLPUBLIC sal_uInt8 premultiply(sal_uInt8 c, sal_uInt8 a);
 
 BitmapEx VCL_DLLPUBLIC loadFromName(const OUString& rFileName, const ImageLoadFlags eFlags = ImageLoadFlags::NONE);

commit 7454d3adf09cbc841e3681f9e32be57bdfb518c7

Make vcl::bitmap::get_unpremultiply_table VCL_DLLPUBLIC again
This fixes my GCC 13.2.0 --enable-gtk4 build on Debian
testing, which started failing after

    commit acd40306b3737d50cf24462b2d3e451092543e64
    Date:   Thu Mar 14 12:22:13 2024 +0200

        reduce symbol visibility in vcl

like this:

    /home/michi/development/git/libreoffice-WORKTREE/vcl/unx/gtk4/../gtk3/salnativewidgets-gtk.cxx:2195: error: undefined reference to 'vcl::bitmap::get_unpremultiply_table()'
    collect2: error: ld returned 1 exit status
    make[1]: *** [/home/michi/development/git/libreoffice-WORKTREE/vcl/Library_vclplug_gtk4.mk:20: /home/michi/development/git/libreoffice-WORKTREE/instdir/program/libvclplug_gtk4lo.so] Error 1
    make[1]: *** Waiting for unfinished jobs....
    make: *** [Makefile:290: build] Error 2

Change-Id: I0921801f6ad0f20f5250ca15a2749d7da044fcd8
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/164856
Tested-by: Jenkins
Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>

include/vcl/BitmapTools.hxx
@@ -32,7 +32,7 @@ namespace vcl::bitmap {
 typedef std::array<std::array<sal_uInt8, 256>, 256> lookup_table;
 
 lookup_table const & get_premultiply_table();
-lookup_table const & get_unpremultiply_table();
+VCL_DLLPUBLIC lookup_table const & get_unpremultiply_table();
 #endif
 
 sal_uInt8 unpremultiply(sal_uInt8 c, sal_uInt8 a);