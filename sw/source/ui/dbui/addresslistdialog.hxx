b4134d3554711bfb1c05e8f36f9f4b5bc52aec3f
wilmhit <wilmhit@disroot.org>
2024-03-17 16:38:43 +0100

    tdf#143148: Use pragma once in dbui addresslistdialog
    Change-Id: Id0ebec238de937482111760f0a9732b621d082af
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/164952
    Tested-by: Jenkins
    Tested-by: Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>
    Reviewed-by: Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>

--- sw/source/ui/dbui/addresslistdialog.hxx
+++ sw/source/ui/dbui/addresslistdialog.hxx
@@ -16,8 +16,7 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
-#ifndef INCLUDED_SW_SOURCE_UI_DBUI_ADDRESSLISTDIALOG_HXX
-#define INCLUDED_SW_SOURCE_UI_DBUI_ADDRESSLISTDIALOG_HXX
+#pragma once
 
 #include <sfx2/basedlgs.hxx>
 #include <swdbdata.hxx>
@@ -96,6 +95,5 @@ public:
     const SwDBData&     GetDBData() const       {return m_aDBData;}
     OUString     GetFilter() const;
 };
-#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
