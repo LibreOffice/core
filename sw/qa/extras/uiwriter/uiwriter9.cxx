LibreOffice/core

commit c456f839a597f537f1c59becd7d0bb6c86248ef8

tdf#115965 tdf#92622 NoList default in menu,tool,sidebar
Change-Id: Icbc612b44593c5366f3e7f81112f4013884f3599
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/108841
Tested-by: Jenkins
Reviewed-by: Heiko Tietze <heiko.tietze@documentfoundation.org>

sw/source/uibase/shells/txtnum.cxx
@@ -104,6 +104,18 @@ void SwTextShell::ExecEnterNum(SfxRequest &rReq)
     }
     break;
 
+    case FN_NUM_BULLET_OFF:
+    {
+        GetShell().StartAllAction();
+        SfxRequest aReq(GetView().GetViewFrame(), FN_NUM_BULLET_ON);
+        aReq.AppendItem(SfxBoolItem(FN_PARAM_1, false));
+        aReq.Done();
+        GetShell().NumOrBulletOff();
+        GetShell().DelNumRules();
+        GetShell().EndAllAction();
+    }
+    break;
+
     case FN_NUMBER_BULLETS:
     case SID_OUTLINE_BULLET:
     {

officecfg/registry/data/org/openoffice/Office/UI/WriterCommands.xcu
@@ -1953,7 +1953,7 @@
       </node>
       <node oor:name=".uno:RemoveBullets" oor:op="replace">
         <prop oor:name="Label" oor:type="xs:string">
-          <value xml:lang="en-US">Numbering Off</value>
+          <value xml:lang="en-US">No List</value>
         </prop>
         <prop oor:name="Properties" oor:type="xs:int">
           <value>7</value>

svx/uiconfig/ui/sidebarparagraph.ui
@@ -642,6 +642,17 @@
                 <property name="show_arrow">False</property>
                 <property name="icon_size">2</property>
                 <child>
+                  <object class="GtkToolButton" id=".uno:RemoveBullets">
+                    <property name="visible">True</property>
+                    <property name="can_focus">False</property>
+                    <property name="use_underline">True</property>
+                  </object>
+                  <packing>
+                    <property name="expand">False</property>
+                    <property name="homogeneous">False</property>
+                  </packing>
+                </child>
+                <child>
                   <object class="GtkMenuToolButton" id=".uno:DefaultBullet">
                     <property name="visible">True</property>
                     <property name="can_focus">False</property>

sw/sdi/_listsh.sdi
@@ -39,13 +39,6 @@ interface BaseTextList
         DisableFlags="SfxDisableFlags::SwOnProtectedCursor";
     ]
 
-    FN_NUM_BULLET_OFF // status(final|play)
-    [
-        ExecMethod = Execute ;
-        StateMethod = GetState ;
-        DisableFlags="SfxDisableFlags::SwOnProtectedCursor";
-    ]
-
     FN_NUM_BULLET_OUTLINE_DOWN  // status(final|play)
     [
         ExecMethod = Execute ;

sw/sdi/_textsh.sdi
@@ -759,6 +759,12 @@ interface BaseText
         StateMethod = GetState ;
         DisableFlags="SfxDisableFlags::SwOnProtectedCursor";
     ]
+    FN_NUM_BULLET_OFF
+    [
+        ExecMethod = ExecEnterNum;
+        StateMethod = GetState;
+        DisableFlags="SfxDisableFlags::SwOnProtectedCursor";
+    ]
     FN_INSERT_FRAME // status(final|play)
     [
         ExecMethod = ExecInsert ;

sw/source/uibase/shells/listsh.cxx
@@ -157,16 +157,6 @@ void SwListShell::Execute(SfxRequest &rReq)
             rReq.Done();
             break;
 
-        case FN_NUM_BULLET_OFF:
-        {
-            rReq.Ignore();
-            SfxRequest aReq( GetView().GetViewFrame(), FN_NUM_BULLET_ON );
-            aReq.AppendItem( SfxBoolItem( FN_PARAM_1, false ) );
-            aReq.Done();
-            rSh.DelNumRules();
-            break;
-        }
-
         case FN_NUM_BULLET_OUTLINE_DOWN:
             if ( bOutline )
                 lcl_OutlineUpDownWithSubPoints( rSh, false, false );

sw/source/uibase/shells/textsh1.cxx
@@ -2036,6 +2036,11 @@ void SwTextShell::GetState( SfxItemSet &rSet )
                 rSet.Put(SfxBoolItem(FN_NUM_BULLET_ON,rSh.SelectionHasBullet()));
             break;
 
+            case FN_NUM_BULLET_OFF:
+                rSet.Put(SfxBoolItem(FN_NUM_BULLET_OFF,(!rSh.SelectionHasBullet() &&
+                    !rSh.SelectionHasNumber())));
+            break;
+
             case FN_BUL_NUM_RULE_INDEX:
             case FN_NUM_NUM_RULE_INDEX:
             case FN_OUTLINE_RULE_INDEX:

sw/uiconfig/swriter/menubar/menubar.xml
@@ -424,8 +424,9 @@
       </menu:menu>
       <menu:menu menu:id=".uno:FormatBulletsMenu">
         <menu:menupopup>
-          <menu:menuitem menu:id=".uno:DefaultBullet"/>
-          <menu:menuitem menu:id=".uno:DefaultNumbering"/>
+          <menu:menuitem menu:id=".uno:RemoveBullets" menu:style="radio"/>
+          <menu:menuitem menu:id=".uno:DefaultBullet" menu:style="radio"/>
+          <menu:menuitem menu:id=".uno:DefaultNumbering" menu:style="radio"/>
           <menu:menuseparator/>
           <menu:menuitem menu:id=".uno:DecrementLevel"/>
           <menu:menuitem menu:id=".uno:IncrementLevel"/>
@@ -439,7 +440,6 @@
           <menu:menuseparator/>
           <menu:menuitem menu:id=".uno:InsertNeutralParagraph"/>
           <menu:menuitem menu:id=".uno:NumberingStart"/>
-          <menu:menuitem menu:id=".uno:RemoveBullets"/>
           <menu:menuseparator/>
           <menu:menuitem menu:id=".uno:JumpDownThisLevel"/>
           <menu:menuitem menu:id=".uno:JumpUpThisLevel"/>
@@ -579,6 +579,7 @@
       <menu:menuitem menu:id=".uno:QuoteCharStyle" menu:style="radio"/>
       <menu:menuitem menu:id=".uno:SourceCharStyle" menu:style="radio"/>
       <menu:menuseparator/>
+      <menu:menuitem menu:id=".uno:RemoveBullets" menu:style="radio"/>
       <menu:menuitem menu:id=".uno:BulletListStyle" menu:style="radio"/>
       <menu:menuitem menu:id=".uno:NumberListStyle" menu:style="radio"/>
       <menu:menuitem menu:id=".uno:AlphaListStyle" menu:style="radio"/>

commit 91e5d1ef52964dc43875ed62acd766524fa7cc5c

tdf#159054 sw: fix .uno:DefaultNumber toggle on Heading numbering
This fixes a 7.2 regression from
commit c456f839a597f537f1c59becd7d0bb6c86248ef8
Author: Anshu on Wed Jan 6 15:04:16 2021 +0530
    tdf#115965 tdf#92622 NoList default in menu,tool,sidebar

The current implementation would set the number format
to SVX_NUM_NUMBER_NONE, but it still looks like a number to the toolbar,
so toggling didn't work since 7.4's bcede3c0ed94d4
It also never cleared out the prefix/suffix when toggled off,
and it affected ALL paragraphs using that outline numbering style.

Remove numbering was doing the same kind of thing.

I think the toggle was trying to follow the drop-down behaviour
of modifying the outline numbering style itself.
While I think the drop-down handling could be considered appropriate,
using the toggle to pseudo-disable the entire style just seems wrong.

This patch kills that, and in addition
automatically gains all the nice undo/redo functionality.

This is one tiny step in making outline numbering go away.

make CppunitTest_sw_uiwriter9 \
    CPPUNIT_TEST_NAME=testTdf159054_disableOutlineNumbering

Change-Id: Id6a24ac25479cb8f35bf1c2c58344390ed7512fe
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/164985
Reviewed-by: Justin Luth <jluth@mail.com>
Tested-by: Justin Luth <jluth@mail.com>
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

sw/source/core/doc/docnum.cxx
@@ -1368,10 +1368,15 @@ void SwDoc::DelNumRules(const SwPaM& rPam, SwRootFrame const*const pLayout)
             {
                 pTNd->ChkCondColl();
             }
-            else if( !pOutlNd &&
-                     static_cast<SwTextFormatColl*>(pTNd->GetFormatColl())->IsAssignedToListLevelOfOutlineStyle() )
+            else
             {
-                pOutlNd = pTNd;
+                auto pParaStyle = static_cast<SwTextFormatColl*>(pTNd->GetFormatColl());
+                if (pParaStyle && pParaStyle->IsAssignedToListLevelOfOutlineStyle())
+                {
+                    if (!pOutlNd)
+                        pOutlNd = pTNd;
+                    pTNd->SetCountedInList(false);
+                }
             }
         }
     }

sw/source/uibase/shells/txtnum.cxx
@@ -111,7 +111,6 @@ void SwTextShell::ExecEnterNum(SfxRequest &rReq)
         SfxRequest aReq(GetView().GetViewFrame(), FN_NUM_BULLET_ON);
         aReq.AppendItem(SfxBoolItem(FN_PARAM_1, false));
         aReq.Done();
-        GetShell().NumOrBulletOff();
         GetShell().DelNumRules();
         GetShell().EndAllAction();
     }

sw/source/uibase/wrtsh/wrtsh1.cxx
@@ -1704,36 +1704,7 @@ void SwWrtShell::NumOrBulletOff()
     if (!pCurNumRule)
         return;
 
-    if (pCurNumRule->IsOutlineRule())
-    {
-        SwNumRule aNumRule(*pCurNumRule);
-
-        SwTextNode * pTextNode =
-            sw::GetParaPropsNode(*GetLayout(), GetCursor()->GetPoint()->GetNode());
-
-        if (pTextNode)
-        {
-            int nLevel = pTextNode->GetActualListLevel();
-
-            if (nLevel < 0)
-                nLevel = 0;
-
-            if (nLevel >= MAXLEVEL)
-                nLevel = MAXLEVEL - 1;
-
-            SwNumFormat aFormat(aNumRule.Get(o3tl::narrowing<sal_uInt16>(nLevel)));
-
-            aFormat.SetNumberingType(SVX_NUM_NUMBER_NONE);
-            aNumRule.Set(nLevel, aFormat);
-
-            // no start or continuation of a list - the outline style is only changed.
-            SetCurNumRule( aNumRule, false );
-        }
-    }
-    else
-    {
-        DelNumRules();
-    }
+    DelNumRules();
 
     // #126346# - Cursor can not be anymore in front of
     // a label, because numbering/bullet is switched off.

sw/qa/extras/uiwriter/uiwriter9.cxx
@@ -429,20 +429,26 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest9, testTdf159054_disableOutlineNumbering)
     dispatchCommand(mxComponent, ".uno:DefaultNumbering", {});
 
     // the selected paragraphs should definitely have the list label removed
-    CPPUNIT_ASSERT_EQUAL(OUString("."), getProperty<OUString>(xPara1, "ListLabelString"));
-    CPPUNIT_ASSERT_EQUAL(OUString("."), getProperty<OUString>(xPara2, "ListLabelString"));
-    // current implementation: the numbering style itself is changed too - affects all Heading 1's.
-    // A valid alternative implementation would be to simply SetCountInList(false) for 1st para,
-    // and leave the Outline style format alone.
-    CPPUNIT_ASSERT_EQUAL(OUString("."), getProperty<OUString>(xPara3, "ListLabelString"));
+    CPPUNIT_ASSERT_EQUAL(OUString(""), getProperty<OUString>(xPara1, "ListLabelString"));
+    CPPUNIT_ASSERT_EQUAL(OUString(""), getProperty<OUString>(xPara2, "ListLabelString"));
+    // the third paragraph must retain the existing numbering format
+    CPPUNIT_ASSERT_EQUAL(OUString("A."), getProperty<OUString>(xPara3, "ListLabelString"));
 
     // on the selection, simulate pressing the toolbar button to toggle ON numbering again
     dispatchCommand(mxComponent, ".uno:DefaultNumbering", {});
 
-    // current implementation: the entire format was removed, so turn (default) numbering on
-    CPPUNIT_ASSERT_EQUAL(OUString("."), getProperty<OUString>(xPara1, "ListLabelString"));
-    CPPUNIT_ASSERT_EQUAL(OUString("."), getProperty<OUString>(xPara2, "ListLabelString"));
-    CPPUNIT_ASSERT_EQUAL(OUString("."), getProperty<OUString>(xPara3, "ListLabelString"));
+    // the outline numbering format must be re-applied to the first two paragraphs
+    CPPUNIT_ASSERT_EQUAL(OUString("A."), getProperty<OUString>(xPara1, "ListLabelString"));
+    CPPUNIT_ASSERT_EQUAL(OUString("B."), getProperty<OUString>(xPara2, "ListLabelString"));
+    CPPUNIT_ASSERT_EQUAL(OUString("C."), getProperty<OUString>(xPara3, "ListLabelString"));
+
+    // on the selection, simulate a right click - list - No list
+    dispatchCommand(mxComponent, ".uno:RemoveBullets", {});
+
+    // the selected paragraphs should definitely have the list label removed
+    CPPUNIT_ASSERT_EQUAL(OUString(""), getProperty<OUString>(xPara1, "ListLabelString"));
+    CPPUNIT_ASSERT_EQUAL(OUString(""), getProperty<OUString>(xPara2, "ListLabelString"));
+    CPPUNIT_ASSERT_EQUAL(OUString("A."), getProperty<OUString>(xPara3, "ListLabelString"));
 }
 
 CPPUNIT_TEST_FIXTURE(SwUiWriterTest9, testTdf158375_dde_disable)