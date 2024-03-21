LibreOffice/core

commit aaf6ce108e91b1504befe19afcee471e3316ae7a

cool#7492 sfx2 lok: set language/locale on async sidebar update
Create two Calc views, set the first view language to English, second
view language to German. Type in the English view, double-click on a
chart in the German view. The sidebar in the German view will have
English strings in it. This doesn't happen if there is no typing right
after the chart activation in the English view.

What happens is that the sidebar update is async, and
sfx2::sidebar::SidebarController::notifyContextChangeEvent() gets
called, which registers an aync event when it calls
AsynchronousCall::RequestCall(). Then later this job gets scheduled, but
possibly by that time the active view is the English one, leading to
English strings when chart::ColumnChartDialogController::getName() calls
SchResId(), which works from the language of the current view.

Fix the problem similar to what commit
fb7b0b944741e4efae8d92a6e305036aff906c7a (cool#7492 sfx2 lok: just set
language/locale on async binding update, 2024-01-09), did: set the
language/locale from the current view before executing the async job and
restore the old value once we're done.

Extract the now duplicated code to a new SfxLokLanguageGuard, so in case
more places have a problem with incorrect l10n, then it's meant to be a
one-liner to fix further places.

Change-Id: I52724a24d93fb753175a3b9b99bc33178519d981
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/161920
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

sfx2/source/sidebar/SidebarController.cxx
@@ -17,6 +17,9 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 #include <sfx2/sidebar/SidebarController.hxx>
+
+#include <boost/property_tree/json_parser.hpp>
+
 #include <sfx2/sidebar/Deck.hxx>
 #include <sidebar/DeckDescriptor.hxx>
 #include <sidebar/DeckTitleBar.hxx>
@@ -125,8 +128,8 @@ SidebarController::SidebarController (
       mnRequestedForceFlags(SwitchFlag_NoForce),
       mbMinimumSidebarWidth(officecfg::Office::UI::Sidebar::General::MinimumWidth::get()),
       msCurrentDeckId(gsDefaultDeckId),
-      maPropertyChangeForwarder([this](){ return this->BroadcastPropertyChange(); }),
-      maContextChangeUpdate([this](){ return this->UpdateConfigurations(); }),
+      maPropertyChangeForwarder(mpViewFrame, [this](){ return this->BroadcastPropertyChange(); }),
+      maContextChangeUpdate(mpViewFrame, [this](){ return this->UpdateConfigurations(); }),
       mbFloatingDeckClosed(!pParentWindow->IsFloatingMode()),
       mnSavedSidebarWidth(pParentWindow->GetSizePixel().Width()),
       maFocusManager([this](const Panel& rPanel){ return this->ShowPanel(rPanel); }),
@@ -805,18 +808,35 @@ void SidebarController::SwitchToDeck (
     {
         if (const SfxViewShell* pViewShell = mpViewFrame->GetViewShell())
         {
+            boost::property_tree::ptree aTree;
+            aTree.put("locale", comphelper::LibreOfficeKit::getLocale().getBcp47());
+            bool bStateChanged = false;
             if (msCurrentDeckId != rDeckDescriptor.msId)
             {
                 const std::string hide = UnoNameFromDeckId(msCurrentDeckId, GetCurrentContext());
                 if (!hide.empty())
-                    pViewShell->libreOfficeKitViewCallback(LOK_CALLBACK_STATE_CHANGED,
-                                                           OString(hide + "=false"));
+                {
+                    aTree.put("commandName", hide);
+                    aTree.put("state", "false");
+                    bStateChanged = true;
+                }
             }
 
             const std::string show = UnoNameFromDeckId(rDeckDescriptor.msId, GetCurrentContext());
             if (!show.empty())
+            {
+                aTree.put("commandName", show);
+                aTree.put("state", "true");
+                bStateChanged = true;
+            }
+
+            if (bStateChanged)
+            {
+                std::stringstream aStream;
+                boost::property_tree::write_json(aStream, aTree);
                 pViewShell->libreOfficeKitViewCallback(LOK_CALLBACK_STATE_CHANGED,
-                                                       OString(show + "=true"));
+                                                       OString(aStream.str()));
+            }
         }
     }
 

sc/qa/unit/tiledrendering/tiledrendering.cxx
@@ -3245,6 +3245,32 @@ CPPUNIT_TEST_FIXTURE(ScTiledRenderingTest, testStatusBarLocale)
     CPPUNIT_ASSERT_EQUAL(std::string("de-DE"), aLocale);
 }
 
+CPPUNIT_TEST_FIXTURE(ScTiledRenderingTest, testSidebarLocale)
+{
+    ScModelObj* pModelObj = createDoc("chart.ods");
+    int nView1 = SfxLokHelper::getView();
+    ViewCallback aView1;
+    SfxViewShell* pView1 = SfxViewShell::Current();
+    pView1->SetLOKLocale("en-US");
+    SfxLokHelper::createView();
+    ViewCallback aView2;
+    SfxViewShell* pView2 = SfxViewShell::Current();
+    pView2->SetLOKLocale("de-DE");
+    TestLokCallbackWrapper::InitializeSidebar();
+    Scheduler::ProcessEventsToIdle();
+    aView2.m_aStateChanges.clear();
+
+    pModelObj->postMouseEvent(LOK_MOUSEEVENT_MOUSEBUTTONDOWN, /*x=*/1,/*y=*/1,/*count=*/2, /*buttons=*/1, /*modifier=*/0);
+    pModelObj->postMouseEvent(LOK_MOUSEEVENT_MOUSEBUTTONUP, /*x=*/1, /*y=*/1, /*count=*/2, /*buttons=*/1, /*modifier=*/0);
+    SfxLokHelper::setView(nView1);
+    Scheduler::ProcessEventsToIdle();
+
+    auto it = aView2.m_aStateChanges.find(".uno:Sidebar");
+    CPPUNIT_ASSERT(it != aView2.m_aStateChanges.end());
+    std::string aLocale = it->second.get<std::string>("locale");
+    CPPUNIT_ASSERT_EQUAL(std::string("de-DE"), aLocale);
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

desktop/qa/desktop_lib/test_desktop_lib.cxx
@@ -68,6 +68,7 @@
 #include <vcl/filter/PDFiumLibrary.hxx>
 #include <svtools/colorcfg.hxx>
 #include <sal/types.h>
+#include <test/lokcallback.hxx>
 
 #if USE_TLS_NSS
 #include <nss.h>
@@ -3266,33 +3267,11 @@ void DesktopLOKTest::testMultiDocuments()
     }
 }
 
-namespace
-{
-    SfxChildWindow* lcl_initializeSidebar()
-    {
-        // in init.cxx we do setupSidebar which creates the controller, do it here
-
-        SfxViewShell* pViewShell = SfxViewShell::Current();
-        CPPUNIT_ASSERT(pViewShell);
-
-        SfxViewFrame& rViewFrame = pViewShell->GetViewFrame();
-        SfxChildWindow* pSideBar = rViewFrame.GetChildWindow(SID_SIDEBAR);
-        CPPUNIT_ASSERT(pSideBar);
-
-        auto pDockingWin = dynamic_cast<sfx2::sidebar::SidebarDockingWindow *>(pSideBar->GetWindow());
-        CPPUNIT_ASSERT(pDockingWin);
-
-        pDockingWin->GetOrCreateSidebarController(); // just to create the controller
-
-        return pSideBar;
-    }
-};
-
 void DesktopLOKTest::testControlState()
 {
     LibLODocument_Impl* pDocument = loadDoc("search.ods");
     pDocument->pClass->postUnoCommand(pDocument, ".uno:StarShapes", nullptr, false);
-    lcl_initializeSidebar();
+    TestLokCallbackWrapper::InitializeSidebar();
     Scheduler::ProcessEventsToIdle();
 
     boost::property_tree::ptree aState;
@@ -3306,7 +3285,7 @@ void DesktopLOKTest::testMetricField()
 {
     LibLODocument_Impl* pDocument = loadDoc("search.ods");
     pDocument->pClass->postUnoCommand(pDocument, ".uno:StarShapes", nullptr, false);
-    SfxChildWindow* pSideBar = lcl_initializeSidebar();
+    SfxChildWindow* pSideBar = TestLokCallbackWrapper::InitializeSidebar();
     Scheduler::ProcessEventsToIdle();
 
     vcl::Window* pWin = pSideBar->GetWindow();

include/sfx2/lokhelper.hxx
@@ -250,6 +250,18 @@ void SfxLokHelper::forEachOtherView(ViewShellType* pThisViewShell, FunctionType 
     }
 }
 
+/// If LOK is active, switch to the language/locale of the provided shell and back on delete.
+class SfxLokLanguageGuard
+{
+    bool m_bSetLanguage;
+    const SfxViewShell* m_pOldShell;
+    const SfxViewShell* m_pNewShell;
+
+public:
+    SfxLokLanguageGuard(SfxViewShell* pNewShell);
+    ~SfxLokLanguageGuard();
+};
+
 #endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

include/sfx2/sidebar/AsynchronousCall.hxx
@@ -23,6 +23,7 @@
 #include <functional>
 
 struct ImplSVEvent;
+class SfxViewFrame;
 
 namespace sfx2::sidebar
 {
@@ -33,7 +34,7 @@ class AsynchronousCall
 public:
     typedef ::std::function<void()> Action;
 
-    AsynchronousCall(Action aAction);
+    AsynchronousCall(const SfxViewFrame* pViewFrame, Action aAction);
     ~AsynchronousCall();
 
     void RequestCall();
@@ -43,6 +44,7 @@ public:
 private:
     Action maAction;
     ImplSVEvent* mnCallId;
+    const SfxViewFrame* mpViewFrame;
 
     DECL_LINK(HandleUserCall, void*, void);
 };

include/test/lokcallback.hxx
@@ -17,6 +17,8 @@
 
 #include <vector>
 
+class SfxChildWindow;
+
 /**
 A helper to convert SfxLokCallbackInterface to a LIbreOfficeKitCallback for tests.
 
@@ -43,6 +45,8 @@ public:
     virtual void dumpState(rtl::OStringBuffer&) override{};
 
     virtual void Invoke() override;
+
+    static SfxChildWindow* InitializeSidebar();
 
 private:
     void callCallback(int nType, const char* pPayload, int nViewId);

sfx2/source/control/bindings.cxx
@@ -1214,30 +1214,10 @@ void SfxBindings::UpdateControllers_Impl
 
 IMPL_LINK( SfxBindings, NextJob, Timer *, pTimer, void )
 {
-    bool bSetView = false;
-    SfxViewShell* pOldShell = nullptr;
-    if (comphelper::LibreOfficeKit::isActive() && pDispatcher)
-    {
-        SfxViewFrame* pFrame = pDispatcher->GetFrame();
-        SfxViewShell* pNewShell = pFrame ? pFrame->GetViewShell() : nullptr;
-        pOldShell = SfxViewShell::Current();
-        if (pNewShell && pNewShell != pOldShell)
-        {
-            // The current view ID is not the one that belongs to this frame, update
-            // language/locale.
-            comphelper::LibreOfficeKit::setLanguageTag(pNewShell->GetLOKLanguageTag());
-            comphelper::LibreOfficeKit::setLocale(pNewShell->GetLOKLocale());
-            bSetView = true;
-        }
-    }
+    SfxViewFrame* pFrame = pDispatcher ? pDispatcher->GetFrame() : nullptr;
+    SfxLokLanguageGuard aGuard(pFrame ? pFrame->GetViewShell() : nullptr);
 
     NextJob_Impl(pTimer);
-
-    if (bSetView && pOldShell)
-    {
-        comphelper::LibreOfficeKit::setLanguageTag(pOldShell->GetLOKLanguageTag());
-        comphelper::LibreOfficeKit::setLocale(pOldShell->GetLOKLocale());
-    }
 }
 
 bool SfxBindings::NextJob_Impl(Timer const * pTimer)

commit 55feb670ca28e0a48ac82a65b5559598704d993e

cool#8278 sfx2 lok: fix unexpected non-json sidebar status update
Open an Impress document via LOK, open the slide layout sidebar, click
the toggle icon so it gets closed: the toggle icon will signal that the
sidebar is open, when it's closed already.

This is a regression from commit
aaf6ce108e91b1504befe19afcee471e3316ae7a (cool#7492 sfx2 lok: set
language/locale on async sidebar update, 2024-01-11), previously we
always emitted LOK_CALLBACK_STATE_CHANGED callbacks with plain text
payloads for the sidebar, where the locale with implicit (with all its
issues), but the above scenario worked fine.

Fix the problem by making SidebarController::disposeDecks() consistent
with SwitchToDeck(), so now we always emit JSON payloads for the sidebar
deck changes.

An alternative would be to improve the code around extractUnoCommand()
in online.git to handle a mix of plain text and JSON payloads, but the
plain text payload is tricky to extend, so using more JSON payloads
sounds like a better fix.

Change-Id: I5b75c2987c230c6720181a1e95ae579727943235
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/165055
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

desktop/source/lib/init.cxx
@@ -150,6 +150,7 @@
 #include <sfx2/DocumentSigner.hxx>
 #include <sfx2/sidebar/SidebarDockingWindow.hxx>
 #include <sfx2/sidebar/SidebarController.hxx>
+#include <sfx2/sidebar/Sidebar.hxx>
 #include <svl/numformat.hxx>
 #include <svx/dialmgr.hxx>
 #include <svx/strings.hrc>
@@ -984,49 +985,6 @@ void ExecuteOrientationChange()
 
     if ( mxUndoManager.is() )
         mxUndoManager->leaveUndoContext();
-}
-
-void setupSidebar(std::u16string_view sidebarDeckId = u"")
-{
-    SfxViewShell* pViewShell = SfxViewShell::Current();
-    SfxViewFrame* pViewFrame = pViewShell ? &pViewShell->GetViewFrame() : nullptr;
-    if (pViewFrame)
-    {
-        if (!pViewFrame->GetChildWindow(SID_SIDEBAR))
-            pViewFrame->SetChildWindow(SID_SIDEBAR, false /* create it */, true /* focus */);
-
-        pViewFrame->ShowChildWindow(SID_SIDEBAR, true);
-
-        // Force synchronous population of panels
-        SfxChildWindow *pChild = pViewFrame->GetChildWindow(SID_SIDEBAR);
-        if (!pChild)
-            return;
-
-        auto pDockingWin = dynamic_cast<sfx2::sidebar::SidebarDockingWindow *>(pChild->GetWindow());
-        if (!pDockingWin)
-            return;
-
-        pViewFrame->ShowChildWindow( SID_SIDEBAR );
-
-        const rtl::Reference<sfx2::sidebar::SidebarController>& xController
-            = pDockingWin->GetOrCreateSidebarController();
-
-        xController->FadeIn();
-        xController->RequestOpenDeck();
-
-        if (!sidebarDeckId.empty())
-        {
-            xController->SwitchToDeck(sidebarDeckId);
-        }
-        else
-        {
-            xController->SwitchToDefaultDeck();
-        }
-
-        pDockingWin->SyncUpdate();
-    }
-    else
-        SetLastExceptionMsg(u"No view shell or sidebar"_ustr);
 }
 
 void hideSidebar()
@@ -5269,12 +5227,18 @@ static void doc_postUnoCommand(LibreOfficeKitDocument* pThis, const char* pComma
     }
     else if (gImpl && aCommand == ".uno:LOKSidebarWriterPage")
     {
-        setupSidebar(u"WriterPageDeck");
+        if (!sfx2::sidebar::Sidebar::Setup(u"WriterPageDeck"))
+        {
+            SetLastExceptionMsg(u"failed to set up sidebar"_ustr);
+        }
         return;
     }
     else if (gImpl && aCommand == ".uno:SidebarShow")
     {
-        setupSidebar();
+        if (!sfx2::sidebar::Sidebar::Setup(u""))
+        {
+            SetLastExceptionMsg(u"failed to set up sidebar"_ustr);
+        }
         return;
     }
     else if (gImpl && aCommand == ".uno:SidebarHide")

include/sfx2/sidebar/Sidebar.hxx
@@ -61,6 +61,8 @@ public:
     static bool IsPanelVisible(
         std::u16string_view rsPanelId,
         const css::uno::Reference<css::frame::XFrame>& rxFrame);
+
+    static bool Setup(std::u16string_view sidebarDeckId = u"");
 };
 
 } // end of namespace sfx2::sidebar

sfx2/source/sidebar/Sidebar.cxx
@@ -20,11 +20,13 @@
 #include <sfx2/sidebar/Sidebar.hxx>
 #include <sfx2/sidebar/SidebarController.hxx>
 #include <sfx2/sidebar/ResourceManager.hxx>
+#include <sfx2/sidebar/SidebarDockingWindow.hxx>
 #include <sidebar/PanelDescriptor.hxx>
 #include <sidebar/Tools.hxx>
 #include <sfx2/sidebar/FocusManager.hxx>
 #include <sfx2/childwin.hxx>
 #include <sfx2/sfxsids.hrc>
+#include <sfx2/viewsh.hxx>
 #include <com/sun/star/frame/XDispatch.hpp>
 
 using namespace css;
@@ -121,6 +123,50 @@ bool Sidebar::IsPanelVisible(
         return false;
 
     return pController->IsDeckVisible(xPanelDescriptor->msDeckId);
+}
+
+bool Sidebar::Setup(std::u16string_view sidebarDeckId)
+{
+    SfxViewShell* pViewShell = SfxViewShell::Current();
+    SfxViewFrame* pViewFrame = pViewShell ? &pViewShell->GetViewFrame() : nullptr;
+    if (pViewFrame)
+    {
+        if (!pViewFrame->GetChildWindow(SID_SIDEBAR))
+            pViewFrame->SetChildWindow(SID_SIDEBAR, false /* create it */, true /* focus */);
+
+        pViewFrame->ShowChildWindow(SID_SIDEBAR, true);
+
+        // Force synchronous population of panels
+        SfxChildWindow *pChild = pViewFrame->GetChildWindow(SID_SIDEBAR);
+        if (!pChild)
+            return false;
+
+        auto pDockingWin = dynamic_cast<sfx2::sidebar::SidebarDockingWindow *>(pChild->GetWindow());
+        if (!pDockingWin)
+            return false;
+
+        pViewFrame->ShowChildWindow( SID_SIDEBAR );
+
+        const rtl::Reference<sfx2::sidebar::SidebarController>& xController
+            = pDockingWin->GetOrCreateSidebarController();
+
+        xController->FadeIn();
+        xController->RequestOpenDeck();
+
+        if (!sidebarDeckId.empty())
+        {
+            xController->SwitchToDeck(sidebarDeckId);
+        }
+        else
+        {
+            xController->SwitchToDefaultDeck();
+        }
+
+        pDockingWin->SyncUpdate();
+        return true;
+    }
+    else
+        return false;
 }
 
 } // end of namespace sfx2::sidebar

sfx2/source/sidebar/SidebarController.cxx
@@ -229,8 +229,16 @@ void SidebarController::disposeDecks()
         {
             const std::string hide = UnoNameFromDeckId(msCurrentDeckId, GetCurrentContext());
             if (!hide.empty())
+            {
+                // Be consistent with SwitchToDeck(), so both places emit JSON.
+                boost::property_tree::ptree aTree;
+                aTree.put("commandName", hide);
+                aTree.put("state", "false");
+                std::stringstream aStream;
+                boost::property_tree::write_json(aStream, aTree);
                 pViewShell->libreOfficeKitViewCallback(LOK_CALLBACK_STATE_CHANGED,
-                                                       OString(hide + "=false"));
+                                                       OString(aStream.str()));
+            }
         }
 
         if (mpParentWindow)

sd/qa/unit/tiledrendering/tiledrendering.cxx
@@ -54,6 +54,7 @@
 #include <vcl/BitmapReadAccess.hxx>
 #include <vcl/virdev.hxx>
 #include <o3tl/string_view.hxx>
+#include <sfx2/sidebar/Sidebar.hxx>
 
 #include <chrono>
 #include <cstdlib>
@@ -783,6 +784,7 @@ public:
     bool m_bViewSelectionSet;
     boost::property_tree::ptree m_aCommentCallbackResult;
     OString m_ShapeSelection;
+    std::map<std::string, boost::property_tree::ptree> m_aStateChanges;
     TestLokCallbackWrapper m_callbackWrapper;
 
     ViewCallback()
@@ -893,6 +895,26 @@ public:
             std::stringstream aStream(pPayload);
             boost::property_tree::read_json(aStream, m_aCommentCallbackResult);
             m_aCommentCallbackResult = m_aCommentCallbackResult.get_child("comment");
+        }
+        break;
+        case LOK_CALLBACK_STATE_CHANGED:
+        {
+            std::stringstream aStream(pPayload);
+            if (!aStream.str().starts_with("{"))
+            {
+                break;
+            }
+
+            boost::property_tree::ptree aTree;
+            boost::property_tree::read_json(aStream, aTree);
+            auto it = aTree.find("commandName");
+            if (it == aTree.not_found())
+            {
+                break;
+            }
+
+            std::string aCommandName = it->second.get_value<std::string>();
+            m_aStateChanges[aCommandName] = aTree;
         }
         break;
         }
@@ -2940,6 +2962,25 @@ CPPUNIT_TEST_FIXTURE(SdTiledRenderingTest, testShapeEditInMultipleViews)
         CPPUNIT_ASSERT_EQUAL(false, pView1->IsTextEdit());
         CPPUNIT_ASSERT_EQUAL(false, pView2->IsTextEdit());
     }
+}
+
+CPPUNIT_TEST_FIXTURE(SdTiledRenderingTest, testSidebarHide)
+{
+    // Given an impress document, with a visible sidebar:
+    createDoc("dummy.odp");
+    ViewCallback aView;
+    sfx2::sidebar::Sidebar::Setup(u"");
+    Scheduler::ProcessEventsToIdle();
+    aView.m_aStateChanges.clear();
+
+    // When hiding the slide layout deck:
+    dispatchCommand(mxComponent, ".uno:ModifyPage", {});
+
+    // Then make sure we get a state change for this, in JSON format:
+    auto it = aView.m_aStateChanges.find(".uno:ModifyPage");
+    // Without the accompanying fix in place, this test would have failed, we got the state change
+    // in plain text, which was inconsistent (show was JSON, hide was plain text).
+    CPPUNIT_ASSERT(it != aView.m_aStateChanges.end());
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();