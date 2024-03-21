7c76332b52d9b3b967beecc1fe163eea783c471e
Christopher Loi <loichris97@gmail.com>
2024-03-21 05:24:32 -0700

    tdf#143148: Replace include guards with pragma once
    Change-Id: I00cee3dc61231a0cc344d404eed4cedda41f4d7d
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/165100
    Reviewed-by: Christopher Loi <loichris97@gmail.com>
    Tested-by: Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>
    Reviewed-by: Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>

--- svx/source/inc/celltypes.hxx
+++ svx/source/inc/celltypes.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_SVX_SOURCE_INC_CELLTYPES_HXX
-#define INCLUDED_SVX_SOURCE_INC_CELLTYPES_HXX
+#pragma once
 
 #include <rtl/ref.hxx>
 #include <vector>
@@ -80,7 +79,5 @@ private:
     T maIter;
 };
 }
-
-#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
