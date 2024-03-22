LibreOffice/core

commit d918beda2ab42668014b0dd42996b6ccc97e8c3a

tdf#158814 DOCX import: fix unwanted header with type="first" & no titlePg
The bugdoc had no header in Word, but had one in Writer, since commit
17e51f427b3f0cec74ac8e0a1b3f51189006ae6f (DOCX import: first page header
should always set default headers as well, 2014-11-21).

The code has changed a log in the meantime, today we import first page
headers and left/right page headers as a single page style, but still
code was missing to detect the case when <w:headerReference
w:type="first"> was not followed by <w:titlePg>, which is an indicator
that the first page header/footer should be used.

Fix the problem by new flags to SectionPropertyMap to track if we ever
seen a first/left/right header. This allows making an informed decision
in SectionPropertyMap::setHeaderFooterProperties(): if the header is on,
but we effectively don't have none of a first, left or right header,
then it's time to turn it off, similar to what the DOC import does.

Note that this only changes behavior for headers, but if there is a
practical need, then the same could be also done for footers as well.
Instead of adding a new test, notice that testTdf112694 in
CppunitTest_sw_core_header_footer explicitly tests this case: a first
header which is not a title page. So change that testcase to assert the
behavior now matches Word and drop the FIXME.

Change-Id: Ib604e786d7a5a197d4b562533326206697de882a
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/162992
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

writerfilter/source/dmapper/PropertyMap.cxx
@@ -571,6 +571,12 @@ void SectionPropertyMap::setHeaderFooterProperties(DomainMapper_Impl& rDM_Impl)
     m_aPageStyle->setPropertyValue(getPropertyName(PROP_HEADER_IS_SHARED), uno::Any(!bEvenAndOdd));
     m_aPageStyle->setPropertyValue(getPropertyName(PROP_FOOTER_IS_SHARED), uno::Any(!bEvenAndOdd));
     m_aPageStyle->setPropertyValue(getPropertyName(PROP_FIRST_IS_SHARED), uno::Any(!m_bTitlePage));
+
+    bool bHadFirstHeader = m_bHadFirstHeader && m_bTitlePage;
+    if (bHasHeader && !bHadFirstHeader && !m_bHadLeftHeader && !m_bHadRightHeader)
+    {
+        m_aPageStyle->setPropertyValue(sHeaderIsOn, uno::Any(false));
+    }
 }
 
 void SectionPropertyMap::SetBorder( BorderPosition ePos, sal_Int32 nLineDistance, const table::BorderLine2& rBorderLine, bool bShadow )

sw/qa/core/header_footer/HeaderFooterTest.cxx
@@ -467,10 +467,8 @@ CPPUNIT_TEST_FIXTURE(HeaderFooterTest, testTdf112694)
     auto verify = [this]() {
         uno::Any aPageStyle = getStyles("PageStyles")->getByName("Standard");
         // Header was on when header for file was for explicit first pages only
-        // (marked via <w:titlePg>).
-        //CPPUNIT_ASSERT(!getProperty<bool>(aPageStyle, "HeaderIsOn"));
-        // TODO - can't disable headers/footers selectively (only fo first page)
-        CPPUNIT_ASSERT(getProperty<bool>(aPageStyle, "HeaderIsOn"));
+        // but <w:titlePg> was missing.
+        CPPUNIT_ASSERT(!getProperty<bool>(aPageStyle, "HeaderIsOn"));
     };
 
     createSwDoc("tdf112694.docx");

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -3814,7 +3814,10 @@ void DomainMapper_Impl::PushPageHeaderFooter(PagePartType ePagePartType, PageTyp
         if (eType == PageType::LEFT)
         {
             if (bHeader)
+            {
                 pSectionContext->m_bLeftHeader = true;
+                pSectionContext->m_bHadLeftHeader = true;
+            }
             else
                 pSectionContext->m_bLeftFooter = true;
 
@@ -3823,7 +3826,10 @@ void DomainMapper_Impl::PushPageHeaderFooter(PagePartType ePagePartType, PageTyp
         else if (eType == PageType::FIRST)
         {
             if (bHeader)
+            {
                 pSectionContext->m_bFirstHeader = true;
+                pSectionContext->m_bHadFirstHeader = true;
+            }
             else
                 pSectionContext->m_bFirstFooter = true;
 
@@ -3832,7 +3838,10 @@ void DomainMapper_Impl::PushPageHeaderFooter(PagePartType ePagePartType, PageTyp
         else
         {
             if (bHeader)
+            {
                 pSectionContext->m_bRightHeader = true;
+                pSectionContext->m_bHadRightHeader = true;
+            }
             else
                 pSectionContext->m_bRightFooter = true;
 

writerfilter/source/dmapper/PropertyMap.hxx
@@ -433,6 +433,9 @@ public:
     bool m_bLeftFooter = false;
     bool m_bRightHeader = false;
     bool m_bRightFooter = false;
+    bool m_bHadFirstHeader = false;
+    bool m_bHadLeftHeader = false;
+    bool m_bHadRightHeader = false;
 
     static void removeXTextContent(css::uno::Reference<css::text::XText> const& rxText);
 };

commit c900850742efd4e1fb7c79c13c1b9a17fcd4981d

Relatted: tdf#160139 RTF paste: don't turn off headers/footers
Regression from commit d918beda2ab42668014b0dd42996b6ccc97e8c3a
(tdf#158814 DOCX import: fix unwanted header with type="first" & no
titlePg, 2024-02-05), pasting shape text into the body text of Writer
turned off the header, which was not intentional.

The original use-case was DOCX/RTF import, and the paste case was just
not considered.

Fix the problem by leaving the paste alone: we already omit a number of
actions in this case (e.g. not overwrite styles), don't turn off
headers, either.

Note that the original problem is wider: we would probably need to track
what page styles are created and only touch those, or something similar.

Change-Id: If08fa7956e98766d5807332c5c0baa25b46afe38
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/165191
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

writerfilter/source/dmapper/PropertyMap.cxx
@@ -574,7 +574,7 @@ void SectionPropertyMap::setHeaderFooterProperties(DomainMapper_Impl& rDM_Impl)
     m_aPageStyle->setPropertyValue(getPropertyName(PROP_FIRST_IS_SHARED), uno::Any(!m_bTitlePage));
 
     bool bHadFirstHeader = m_bHadFirstHeader && m_bTitlePage;
-    if (bHasHeader && !bHadFirstHeader && !m_bHadLeftHeader && !m_bHadRightHeader)
+    if (bHasHeader && !bHadFirstHeader && !m_bHadLeftHeader && !m_bHadRightHeader && rDM_Impl.IsNewDoc())
     {
         m_aPageStyle->setPropertyValue(sHeaderIsOn, uno::Any(false));
     }

writerfilter/qa/cppunittests/dmapper/PropertyMap.cxx
@@ -15,6 +15,12 @@
 #include <com/sun/star/beans/XPropertySet.hpp>
 #include <com/sun/star/drawing/XDrawPageSupplier.hpp>
 #include <com/sun/star/table/BorderLine2.hpp>
+#include <com/sun/star/text/XTextDocument.hpp>
+#include <com/sun/star/document/XFilter.hpp>
+#include <com/sun/star/document/XImporter.hpp>
+
+#include <unotools/streamwrap.hxx>
+#include <comphelper/propertyvalue.hxx>
 
 using namespace ::com::sun::star;
 
@@ -134,6 +140,40 @@ CPPUNIT_TEST_FIXTURE(Test, testNegativePageBorderNoMargin)
     // - Actual  : 0
     // i.e. the border negative distance was lost.
     CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(-1147), nTopBorderDistance);
+}
+
+CPPUNIT_TEST_FIXTURE(Test, testPasteHeaderDisable)
+{
+    // Given an empty document with a turned on header:
+    mxComponent = loadFromDesktop("private:factory/swriter", "com.sun.star.text.TextDocument");
+    uno::Reference<style::XStyleFamiliesSupplier> xStyleFamiliesSupplier(mxComponent,
+                                                                         uno::UNO_QUERY);
+    uno::Reference<container::XNameAccess> xStyleFamilies
+        = xStyleFamiliesSupplier->getStyleFamilies();
+    uno::Reference<container::XNameAccess> xStyleFamily(xStyleFamilies->getByName("PageStyles"),
+                                                        uno::UNO_QUERY);
+    uno::Reference<beans::XPropertySet> xStyle(xStyleFamily->getByName("Standard"), uno::UNO_QUERY);
+    xStyle->setPropertyValue("HeaderIsOn", uno::Any(true));
+
+    // When pasting RTF content:
+    uno::Reference<text::XTextDocument> xTextDocument(mxComponent, uno::UNO_QUERY);
+    uno::Reference<text::XTextRange> xText = xTextDocument->getText();
+    uno::Reference<text::XTextRange> xBodyEnd = xText->getEnd();
+    uno::Reference<document::XFilter> xFilter(
+        m_xSFactory->createInstance("com.sun.star.comp.Writer.RtfFilter"), uno::UNO_QUERY);
+    uno::Reference<document::XImporter> xImporter(xFilter, uno::UNO_QUERY);
+    xImporter->setTargetDocument(mxComponent);
+    std::unique_ptr<SvStream> pStream(new SvMemoryStream);
+    pStream->WriteOString("{\\rtf1 paste}");
+    pStream->Seek(0);
+    uno::Reference<io::XStream> xStream(new utl::OStreamWrapper(std::move(pStream)));
+    uno::Sequence aDescriptor{ comphelper::makePropertyValue("InputStream", xStream),
+                               comphelper::makePropertyValue("InsertMode", true),
+                               comphelper::makePropertyValue("TextInsertModeRange", xBodyEnd) };
+    CPPUNIT_ASSERT(xFilter->filter(aDescriptor));
+
+    // Then make sure the header stays on:
+    CPPUNIT_ASSERT(xStyle->getPropertyValue("HeaderIsOn").get<bool>());
 }
 }
 