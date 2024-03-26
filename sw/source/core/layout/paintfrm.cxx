LibreOffice/core

commit 79811c27f34d3e752de1bc3959605c5d58ac2365

tdf#129905 Create toggle for Section boundaries
Currently in Writer it is not possible to enable/disable Section Boundaries independently from Text Boundaries (which are toggleable via View - Text Boundaries). Hence its only possible to enable/disable them together.

This patch creates a new command that allows to enable/disable Section Boundaries (now in View - Section Boundaries) indepentently from Text Boundaries.

Change-Id: I76981ed1a055ce8d1e0f83c3055fef75df37f019
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/146077
Tested-by: Jenkins
Reviewed-by: Heiko Tietze <heiko.tietze@documentfoundation.org>

sw/source/core/layout/paintfrm.cxx
@@ -6681,7 +6681,7 @@ void SwPageFrame::RefreshSubsidiary( const SwRect &rRect ) const
 void SwLayoutFrame::RefreshLaySubsidiary( const SwPageFrame *pPage,
                                         const SwRect &rRect ) const
 {
-    const bool bSubsOpt   = isSubsidiaryLinesEnabled();
+    const bool bSubsOpt = isSubsidiaryLinesEnabled() || isSubsidiaryLinesForSectionsEnabled();
     if ( bSubsOpt )
         PaintSubsidiaryLines( pPage, rRect );
 
@@ -6918,6 +6918,9 @@ static drawinglayer::primitive2d::Primitive2DContainer lcl_CreateColumnAreaDelim
 void SwPageFrame::PaintSubsidiaryLines( const SwPageFrame *,
                                         const SwRect & ) const
 {
+    if (!SwViewOption::IsDocBoundaries())
+        return;
+
     if ( gProp.pSGlobalShell->IsHeaderFooterEdit() )
         return;
 
@@ -6989,6 +6992,9 @@ void SwColumnFrame::PaintSubsidiaryLines( const SwPageFrame *,
 void SwSectionFrame::PaintSubsidiaryLines( const SwPageFrame * pPage,
                                         const SwRect & rRect ) const
 {
+    if (!SwViewOption::IsSectionBoundaries())
+        return;
+
     const bool bNoLowerColumn = !Lower() || !Lower()->IsColumnFrame();
     if ( bNoLowerColumn )
     {
@@ -7007,6 +7013,9 @@ void SwBodyFrame::PaintSubsidiaryLines( const SwPageFrame *,
 
 void SwHeadFootFrame::PaintSubsidiaryLines( const SwPageFrame *, const SwRect & ) const
 {
+    if (!SwViewOption::IsDocBoundaries())
+        return;
+
     if ( gProp.pSGlobalShell->IsHeaderFooterEdit() )
     {
         SwRect aArea( getFramePrintArea() );

officecfg/registry/data/org/openoffice/Office/UI/WriterCommands.xcu
@@ -2825,6 +2825,14 @@
           <value>1</value>
         </prop>
       </node>
+      <node oor:name=".uno:SectionBoundaries" oor:op="replace">
+        <prop oor:name="Label" oor:type="xs:string">
+          <value xml:lang="en-US">Sec~tion Boundaries</value>
+        </prop>
+        <prop oor:name="Properties" oor:type="xs:int">
+          <value>1</value>
+        </prop>
+      </node>
       <node oor:name=".uno:ThesaurusDialog" oor:op="replace">
         <prop oor:name="Label" oor:type="xs:string">
           <value xml:lang="en-US">~Thesaurus...</value>

sw/inc/cmdid.h
@@ -163,6 +163,7 @@ class SwUINumRuleItem;
 #define FN_VLINEAL              (FN_VIEW + 16)  /* Vertical Liner */
 #define FN_VSCROLLBAR           (FN_VIEW + 17)  /* Vertical Scrollbar */
 #define FN_HSCROLLBAR           (FN_VIEW + 18)  /* Horizontal Scrollbar */
+#define FN_VIEW_SECTION_BOUNDARIES (FN_VIEW + 19)  /* View section boundaries */
 
 #define FN_VIEW_META_CHARS      (FN_VIEW + 24)  /* View meta chars */
 #define FN_VIEW_MARKS           (FN_VIEW + 25)  /* View marks */

sw/sdi/_viewsh.sdi
@@ -883,6 +883,12 @@ interface BaseTextEditView
         StateMethod = StateViewOptions ;
     ]
 
+    FN_VIEW_SECTION_BOUNDARIES // status()
+    [
+        ExecMethod = ExecViewOptions ;
+        StateMethod = StateViewOptions ;
+    ]
+
     FN_VIEW_GRAPHIC // status()
     [
         ExecMethod = ExecViewOptions ;

sw/sdi/swriter.sdi
@@ -6669,6 +6669,24 @@ SfxBoolItem ViewBounds FN_VIEW_BOUNDS
     GroupId = SfxGroupId::View;
 ]
 
+SfxBoolItem SectionBoundaries FN_VIEW_SECTION_BOUNDARIES
+
+[
+    AutoUpdate = FALSE,
+    FastCall = FALSE,
+    ReadOnlyDoc = TRUE,
+    Toggle = FALSE,
+    Container = FALSE,
+    RecordAbsolute = FALSE,
+    RecordPerSet;
+
+
+    AccelConfig = TRUE,
+    MenuConfig = TRUE,
+    ToolBoxConfig = TRUE,
+    GroupId = SfxGroupId::View;
+]
+
 SfxBoolItem VRuler FN_VLINEAL
 
 [

sw/source/uibase/uiview/view0.cxx
@@ -192,6 +192,7 @@ void SwView::RecheckBrowseMode()
             FN_VLINEAL,             /*20216*/
             FN_VSCROLLBAR,      /*20217*/
             FN_HSCROLLBAR,      /*20218*/
+            FN_VIEW_SECTION_BOUNDARIES, /*20219*/
             FN_VIEW_META_CHARS, /**/
             FN_VIEW_MARKS,      /**/
             //FN_VIEW_FIELDNAME,    /**/
@@ -253,6 +254,8 @@ void SwView::StateViewOptions(SfxItemSet &rSet)
             break;
             case FN_VIEW_BOUNDS:
                 aBool.SetValue( SwViewOption::IsDocBoundaries()); break;
+            case FN_VIEW_SECTION_BOUNDARIES:
+                aBool.SetValue(SwViewOption::IsSectionBoundaries()); break;
             case FN_VIEW_GRAPHIC:
                 aBool.SetValue( pOpt->IsGraphic() ); break;
             case FN_VIEW_FIELDS:
@@ -409,6 +412,12 @@ void SwView::ExecViewOptions(SfxRequest &rReq)
         if( STATE_TOGGLE == eState )
             bFlag = !SwViewOption::IsDocBoundaries();
         SwViewOption::SetAppearanceFlag(ViewOptFlags::DocBoundaries, bFlag, true );
+        break;
+
+    case FN_VIEW_SECTION_BOUNDARIES:
+        if( STATE_TOGGLE == eState )
+            bFlag = !SwViewOption::IsSectionBoundaries();
+        SwViewOption::SetAppearanceFlag(ViewOptFlags::SectionBoundaries, bFlag, true );
         break;
 
     case SID_GRID_VISIBLE:

sw/uiconfig/swriter/menubar/menubar.xml
@@ -207,6 +207,7 @@
       <menu:menuitem menu:id=".uno:ControlCodes"/>
       <menu:menuitem menu:id=".uno:ViewBounds" menu:style="text"/>
       <menu:menuitem menu:id=".uno:TableBoundaries" menu:style="text"/>
+      <menu:menuitem menu:id=".uno:SectionBoundaries" menu:style="text"/>
       <menu:menuitem menu:id=".uno:ShowGraphics" menu:style="text"/>
       <menu:menuitem menu:id=".uno:ShowWhitespace"/>
       <menu:menuseparator/>

commit 47af9e64a573684eb42faa097e327e31777a2b58

tdf#129905 tdf#160365 sw: don't always draw text boundary on frames
This fixes a 7.6 regression caused by
commit 79811c27f34d3e752de1bc3959605c5d58ac2365
    tdf#129905 Create toggle for Section boundaries

This fixes the toggle for View - Text boundaries,
so now images can have their thin gray border removed.

This also fixes the toggle for View - Table boundaries.

Change-Id: Idb75debf173d64b14a6864c61b4524e46898975f
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/165297
Reviewed-by: Justin Luth <jluth@mail.com>
Tested-by: Jenkins
Reviewed-by: Rafael Lima <rafael.palma.lima@gmail.com>
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

sw/source/core/layout/paintfrm.cxx
@@ -7004,7 +7004,8 @@ void SwPageFrame::RefreshSubsidiary( const SwRect &rRect ) const
 void SwLayoutFrame::RefreshLaySubsidiary( const SwPageFrame *pPage,
                                         const SwRect &rRect ) const
 {
-    const bool bSubsOpt = isSubsidiaryLinesEnabled() || isSubsidiaryLinesForSectionsEnabled();
+    const bool bSubsOpt
+        = isSubsidiaryLinesEnabled() || (IsSctFrame() && isSubsidiaryLinesForSectionsEnabled());
     if ( bSubsOpt )
         PaintSubsidiaryLines( pPage, rRect );
 