e95ada626ccf0c1e915a1582c372242ef9b9b59a
Mike Kaganski <mike.kaganski@collabora.com>
2024-03-27 17:50:53 +0500

    Revert "Downgrade sfx::SfxBindings aAutoTimer to an idle"
    This reverts commit cb9c9d990600baad9e5a63c80ddaa46e98248161.

--- sfx2/source/control/bindings.cxx
+++ sfx2/source/control/bindings.cxx
@@ -146,7 +146,6 @@ SfxBindings::SfxBindings()
 
     // all caches are valid (no pending invalidate-job)
     // create the list of caches
-    pImpl->aAutoTimer.SetPriority(TaskPriority::HIGH_IDLE);
     pImpl->aAutoTimer.SetInvokeHandler( LINK(this, SfxBindings, NextJob) );
 }
 
