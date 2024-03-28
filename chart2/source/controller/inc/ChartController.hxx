1e1975e6c4f2369e757324d3a2b7910b5c0aed99
Balazs Varga <balazs.varga.extern@allotropia.de>
2024-03-28 17:55:30 +0100

    tdf#39052 - Chart: make characters formatable in editable chart textshapes
    Editable textshapes include main chart title, sub chart title, axis titles.
    
    In chart2 the chart2::XFormattedString and chart2::XFormattedString2 store
    the formatted characters from textshapes, so we need to set all the character
    properties from the EditTextObject and need to add them to to the XFormattedString
    array with all the related texts which are formatted individually.
    
    For formatting of the characters the .uno:FontDialog command can be used,
    which can be called from the chart menubar, toolbar and with right click
    on the text when we are in edit mode in the textshape.
    
    (Note: in the next patch the OOXML export will be fixed.)
    
    cherry-pick from commit: 4f994cec388377cc5c2bddb804bd92eb4cd7dc8d
    
    Change-Id: I5750a5fe694b384dc6b28e2ef03ac1f2b03957db
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/165501
    Tested-by: Jenkins
    Tested-by: Gabor Kelemen <gabor.kelemen.extern@allotropia.de>
    Reviewed-by: Balazs Varga <balazs.varga.extern@allotropia.de>

--- chart2/source/controller/inc/ChartController.hxx
+++ chart2/source/controller/inc/ChartController.hxx
@@ -322,6 +322,7 @@ public:
     void setDrawMode( ChartDrawMode eMode ) { m_eDrawMode = eMode; }
 
     bool isShapeContext() const;
+    bool IsTextEdit() const;
 
     ViewElementListProvider getViewElementListProvider();
     DrawModelWrapper* GetDrawModelWrapper();
@@ -483,6 +484,8 @@ private:
     void executeDispatch_MoveSeries( bool bForward );
 
     bool EndTextEdit();
+    css::uno::Sequence< css::uno::Reference<css::chart2::XFormattedString >> GetFormattedTitle(
+        const EditTextObject& aEdit, const css::uno::Reference< css::drawing::XShape >& xShape );
 
     void executeDispatch_View3D();
     void executeDispatch_PositionAndSize( const ::css::uno::Sequence< ::css::beans::PropertyValue >* pArgs = nullptr );
@@ -513,7 +516,7 @@ private:
         const css::uno::Sequence< css::beans::PropertyValue >& rArgs );
 
     DECL_LINK( DoubleClickWaitingHdl, Timer*, void );
-    void execute_DoubleClick( const Point* pMousePixel );
+    void execute_DoubleClick( const Point* pMousePixel, bool &bEditText );
     void startDoubleClickWaiting();
     void stopDoubleClickWaiting();
 
