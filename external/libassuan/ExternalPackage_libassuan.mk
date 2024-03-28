904501114d8b6754ab65bb11c08c145f02fab8fe
Xisco Fauli <xiscofauli@libreoffice.org>
2024-03-28 11:49:58 +0100

    libassuan: upgrade to 2.5.7
    Change-Id: I6114c7a77395f14e83d2315ac6f9b6829ddb1c6a
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/165456
    Tested-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
    (cherry picked from commit 0343d7153e77ba79a59bc5f6c51159af9010813e)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/165472
    Tested-by: Jenkins
    Reviewed-by: Taichi Haradaguchi <20001722@ymail.ne.jp>

--- external/libassuan/ExternalPackage_libassuan.mk
+++ external/libassuan/ExternalPackage_libassuan.mk
@@ -15,7 +15,7 @@ ifneq ($(DISABLE_DYNLOADING),TRUE)
 
 ifeq ($(OS),LINUX)
 
-$(eval $(call gb_ExternalPackage_add_file,libassuan,$(LIBO_LIB_FOLDER)/libassuan.so.0,src/.libs/libassuan.so.0.8.6))
+$(eval $(call gb_ExternalPackage_add_file,libassuan,$(LIBO_LIB_FOLDER)/libassuan.so.0,src/.libs/libassuan.so.0.8.7))
 
 else ifeq ($(OS),MACOSX)
 
