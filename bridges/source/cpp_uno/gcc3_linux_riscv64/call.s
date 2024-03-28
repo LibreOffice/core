4becdfe1ef8df45c4e1385d51f82fcae7f835e78
Sakura286 <sakura286@outlook.com>
2024-03-28 18:33:32 +0800

    riscv64 bridge: replace tab to space in call.s
    Change-Id: I357f400050444336e26e73e1099b4b8ab9e8bdd1
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/165454
    Reviewed-by: Stephan Bergmann <stephan.bergmann@allotropia.de>
    Tested-by: Jenkins

--- bridges/source/cpp_uno/gcc3_linux_riscv64/call.s
+++ bridges/source/cpp_uno/gcc3_linux_riscv64/call.s
@@ -24,56 +24,56 @@
     .type privateSnippetExecutor, %function
 privateSnippetExecutor:
     .cfi_startproc
-    addi	sp,sp,-160
+    addi sp,sp,-160
     .cfi_def_cfa_offset 160
-    sd	ra,152(sp)
+    sd   ra,152(sp)
     .cfi_offset 1, -8
-    fsd	fa0,80(sp)
-    fsd	fa1,88(sp)
-    fsd	fa2,96(sp)
-    fsd	fa3,104(sp)
-    fsd	fa4,112(sp)
-    fsd	fa5,120(sp)
-    fsd	fa6,128(sp)
-    fsd	fa7,136(sp)
-    sd	a0,16(sp)
-    sd	a1,24(sp)
-    sd	a2,32(sp)
-    sd	a3,40(sp)
-    sd	a4,48(sp)
-    sd	a5,56(sp)
-    sd	a6,64(sp)
-    sd	a7,72(sp)
+    fsd  fa0,80(sp)
+    fsd  fa1,88(sp)
+    fsd  fa2,96(sp)
+    fsd  fa3,104(sp)
+    fsd  fa4,112(sp)
+    fsd  fa5,120(sp)
+    fsd  fa6,128(sp)
+    fsd  fa7,136(sp)
+    sd   a0,16(sp)
+    sd   a1,24(sp)
+    sd   a2,32(sp)
+    sd   a3,40(sp)
+    sd   a4,48(sp)
+    sd   a5,56(sp)
+    sd   a6,64(sp)
+    sd   a7,72(sp)
     // a0 = functionIndex
     // a1 = vtableOffset
     // a2 = gpreg
     // a3 = fpreg
     // a4 = overflw
     // a5 = pRegisterReturn
-    add	a0,t4,zero
-    add	a1,t5,zero
-    addi	a2,sp,16
-    addi	a3,sp,80
-    addi	a4,sp,160
-    add	a5,sp,zero
+    add  a0,t4,zero
+    add  a1,t5,zero
+    addi a2,sp,16
+    addi a3,sp,80
+    addi a4,sp,160
+    add  a5,sp,zero
     // jump to cpp_vtable_call
-    jalr	ra,t6,0
+    jalr ra,t6,0
 
-    bne	a0,zero,.OneFloatOneInt
-    ld	a0,0(sp)
-    ld	a1,8(sp)
-    fld	fa0,0(sp)
-    fld	fa1,8(sp)
-    jal	zero,.EndProgram
+    bne  a0,zero,.OneFloatOneInt
+    ld   a0,0(sp)
+    ld   a1,8(sp)
+    fld  fa0,0(sp)
+    fld  fa1,8(sp)
+    jal  zero,.EndProgram
 .OneFloatOneInt:
-    ld	a0,0(sp)
-    fld	fa0,8(sp)
+    ld   a0,0(sp)
+    fld  fa0,8(sp)
 .EndProgram:
-    ld	ra,152(sp)
+    ld   ra,152(sp)
     .cfi_restore 1
-    addi	sp,sp,160
+    addi sp,sp,160
     .cfi_def_cfa_offset 0
-    jalr	zero,ra,0
+    jalr zero,ra,0
     .cfi_endproc
     .size privateSnippetExecutor, .-privateSnippetExecutor
     .section .note.GNU-stack, "", @progbits
