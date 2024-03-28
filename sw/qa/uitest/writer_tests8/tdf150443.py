403abdc86af5f7ec145d14e2315493c510b1b869
Noel Grandin <noel.grandin@collabora.co.uk>
2024-03-28 12:31:04 +0200

    disable part of tdf150443 UITest for now
    Very unreliable.
    
    Running
       make UITest_writer_tests8
    by itself will fail 50% of the time for me.
    
    Change-Id: I841429d69249f3dd302013b781e7664d25e1387c
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/165453
    Tested-by: Noel Grandin <noel.grandin@collabora.co.uk>
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

--- sw/qa/uitest/writer_tests8/tdf150443.py
+++ sw/qa/uitest/writer_tests8/tdf150443.py
@@ -28,19 +28,22 @@ class tdf150443(UITestCase):
                 xToolkit.processEventsToIdle()
                 self.assertEqual(get_state_as_dict(xWriterEdit)["CurrentPage"], "4")
 
-            # reject the tracked table row in Manage Changes dialog window
-            with self.ui_test.execute_modeless_dialog_through_command(".uno:AcceptTrackedChanges", close_button="close") as xTrackDlg:
-                changesList = xTrackDlg.getChild("writerchanges")
-
-                items = len(changesList.getChildren())
-
-                # select tree parent of the actual tracked row deletion in tree list
-                changesList.executeAction("TYPE", mkPropertyValues({"KEYCODE": "LEFT"}))
-                xToolkit.processEventsToIdle()
-
-                # Without the fix in place, it would have crashed here
-                xAccBtn = xTrackDlg.getChild("reject")
-                xAccBtn.executeAction("CLICK", tuple())
-                self.assertEqual(items - 1, len(changesList.getChildren()))
+# Disable this for now. It seems like sometimes, depending on load, the manage changes dialog box will not
+# appear, and then the getChild call will fail. Either that or the wrong window is top-level.
+#
+#            # reject the tracked table row in Manage Changes dialog window
+#            with self.ui_test.execute_modeless_dialog_through_command(".uno:AcceptTrackedChanges", close_button="close") as xTrackDlg:
+#                changesList = xTrackDlg.getChild("writerchanges")
+#
+#                items = len(changesList.getChildren())
+#
+#                # select tree parent of the actual tracked row deletion in tree list
+#                changesList.executeAction("TYPE", mkPropertyValues({"KEYCODE": "LEFT"}))
+#                xToolkit.processEventsToIdle()
+#
+#                # Without the fix in place, it would have crashed here
+#                xAccBtn = xTrackDlg.getChild("reject")
+#                xAccBtn.executeAction("CLICK", tuple())
+#                self.assertEqual(items - 1, len(changesList.getChildren()))
 
 # vim: set shiftwidth=4 softtabstop=4 expandtab:
