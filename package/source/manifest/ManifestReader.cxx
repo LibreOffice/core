9a5b67ea271afc4449638207eecc52e88a7a891e
Gabor Kelemen <gabor.kelemen.extern@allotropia.de>
2024-03-29 13:59:40 +0100

    tdf#146619 Drop unused 'using namespace' in: package/
    Change-Id: I635a5c4bc988bff5a99f7d8487eb7c0f67c15d0e
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/165522
    Tested-by: Jenkins
    Reviewed-by: Gabor Kelemen <gabor.kelemen.extern@allotropia.de>

--- package/source/manifest/ManifestReader.cxx
+++ package/source/manifest/ManifestReader.cxx
@@ -33,7 +33,6 @@ using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star::beans;
 using namespace ::com::sun::star::io;
 using namespace ::com::sun::star::lang;
-using namespace ::com::sun::star::registry;
 using namespace ::com::sun::star::packages;
 using namespace ::com::sun::star::xml::sax;
 
