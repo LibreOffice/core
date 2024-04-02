LibreOffice/core

commit 60e32969a98cad348cf8e55e8f93abc3d6e9c70c

tdf#155393 SdNavigator: Enhancement to navigate in Notes view
Change-Id: Icea71217fa5ffadc637bd7d9b8cae2fa18ec93bc
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/152075
Tested-by: Jenkins
Reviewed-by: Stéphane Guillou <stephane.guillou@libreoffice.org>
Reviewed-by: Heiko Tietze <heiko.tietze@documentfoundation.org>
Reviewed-by: Jim Raykowski <raykowj@gmail.com>

sd/source/ui/dlg/navigatr.cxx
@@ -778,6 +778,7 @@ void SdNavigatorControllerItem::StateChangedAtToolBoxControl( sal_uInt16 nSId,
                 static_cast<::sd::DrawViewShell *>(pDrawDocShell->GetViewShell());
         if (pDrawViewShell)
         {
+            pNavigatorWin->FreshTree(pDrawDocShell->GetDoc());
             bool bEditModePage(pDrawViewShell->GetEditMode() == EditMode::Page);
             pNavigatorWin->mxToolbox->set_sensitive(bEditModePage);
             pNavigatorWin->mxLbDocs->set_sensitive(bEditModePage);

sd/source/ui/dlg/sdtreelb.cxx
@@ -56,8 +56,20 @@
 #include <vcl/commandevent.hxx>
 
 #include <svx/svdview.hxx>
+#include <DrawViewShell.hxx>
 
 using namespace com::sun::star;
+
+namespace {
+
+sd::DrawViewShell* lcl_getDrawViewShell(const SdDrawDocument* pDoc)
+{
+    if (!pDoc || !pDoc->GetDocSh())
+        return nullptr;
+    return static_cast<sd::DrawViewShell*>(pDoc->GetDocSh()->GetViewShell());
+}
+
+}
 
 bool SdPageObjsTLV::bIsInDrag = false;
 
@@ -251,6 +263,13 @@ bool SdPageObjsTLV::IsEqualToDoc( const SdDrawDocument* pInDoc )
     if( !m_pDoc )
         return false;
 
+    sd::DrawViewShell* pDrawViewShell = lcl_getDrawViewShell(m_pDoc);
+    if (!pDrawViewShell)
+        return false;
+    PageKind eDrawViewShellPageKind = pDrawViewShell->GetPageKind();
+    if (eDrawViewShellPageKind != PageKind::Standard && eDrawViewShellPageKind != PageKind::Notes)
+        return false;
+
     std::unique_ptr<weld::TreeIter> xEntry(m_xTreeView->make_iterator());
     if (!m_xTreeView->get_iter_first(*xEntry))
         xEntry.reset();
@@ -262,7 +281,7 @@ bool SdPageObjsTLV::IsEqualToDoc( const SdDrawDocument* pInDoc )
     while( nPage < nMaxPages )
     {
         const SdPage* pPage = static_cast<const SdPage*>( m_pDoc->GetPage( nPage ) );
-        if( pPage->GetPageKind() == PageKind::Standard )
+        if (pPage->GetPageKind() == eDrawViewShellPageKind)
         {
             bool bRet = IsEqualToShapeList(xEntry, *pPage, pPage->GetName());
             if (!bRet)
@@ -1247,11 +1266,21 @@ void SdPageObjsTLV::Fill(const SdDrawDocument* pInDoc, bool bAllPages, const OUS
     sal_uInt16 nPage = 0;
     const sal_uInt16 nMaxPages = m_pDoc->GetPageCount();
 
+    sd::DrawViewShell* pDrawViewShell = lcl_getDrawViewShell(m_pDoc);
+    if (!pDrawViewShell)
+        return;
+    PageKind eDrawViewShellPageKind = pDrawViewShell->GetPageKind();
+
     while( nPage < nMaxPages )
     {
         const SdPage* pPage = static_cast<const SdPage*>( m_pDoc->GetPage( nPage ) );
-        if(  (m_bShowAllPages || pPage->GetPageKind() == PageKind::Standard)
-             && (pPage->GetPageKind() != PageKind::Handout)   ) //#94954# never list the normal handout page ( handout-masterpage is used instead )
+        PageKind ePagePageKind = pPage->GetPageKind();
+        if ((m_bShowAllPages ||
+             (ePagePageKind == PageKind::Standard &&
+              eDrawViewShellPageKind == PageKind::Standard) ||
+             (ePagePageKind == PageKind::Notes &&
+              eDrawViewShellPageKind == PageKind::Notes)) &&
+                ePagePageKind != PageKind::Handout) //#94954# never list the normal handout page ( handout-masterpage is used instead )
         {
             bool bPageExcluded = pPage->IsExcluded();
 

commit 878ab17b0fad66fb7accd9573ef7f66403d593b9

tdf#139944 SdNavigator: disable in master mode
Improve where the disable is done and in addition to making disabled
also clear the objects tree.

Also fixes a drag and drop ordering scrolling bug introduced by commit
60e32969a98cad348cf8e55e8f93abc3d6e9c70c

Change-Id: I702c85bc1ba16837fb7f8c2246bdb29d96e6cc6f
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/165606
Tested-by: Jenkins
Reviewed-by: Jim Raykowski <raykowj@gmail.com>

sd/source/ui/dlg/navigatr.cxx
@@ -157,6 +157,24 @@ void SdNavigatorWin::FreshTree( const SdDrawDocument* pDoc )
 {
     SdDrawDocument* pNonConstDoc = const_cast<SdDrawDocument*>(pDoc); // const as const can...
     sd::DrawDocShell* pDocShell = pNonConstDoc->GetDocSh();
+    ::sd::ViewShell* pViewShell = pDocShell->GetViewShell();
+
+    // tdf#139944 disable navigator in master mode
+    if (pViewShell)
+    {
+        if (const sd::DrawViewShell* pDrawViewShell = static_cast<::sd::DrawViewShell*>(pViewShell))
+        {
+            if (pDrawViewShell->GetEditMode() == EditMode::MasterPage)
+            {
+                m_xContainer->set_sensitive(false);
+                mxTlbObjects->clear();
+                return;
+            }
+            else
+                m_xContainer->set_sensitive(true);
+        }
+    }
+
     const OUString& aDocShName( pDocShell->GetName() );
     OUString aDocName = pDocShell->GetMedium()->GetName();
     if (!mxTlbObjects->IsEqualToDoc(pDoc))
@@ -165,7 +183,8 @@ void SdNavigatorWin::FreshTree( const SdDrawDocument* pDoc )
         RefreshDocumentLB();
         mxLbDocs->set_active_text(aDocShName);
     }
-    if (const sd::ViewShell* pViewShell = pDocShell->GetViewShell())
+
+    if (pViewShell)
         lcl_select_marked_object(pViewShell, mxTlbObjects.get());
 }
 
@@ -175,6 +194,22 @@ void SdNavigatorWin::InitTreeLB( const SdDrawDocument* pDoc )
     ::sd::DrawDocShell* pDocShell = pNonConstDoc->GetDocSh();
     OUString aDocShName( pDocShell->GetName() );
     ::sd::ViewShell* pViewShell = pDocShell->GetViewShell();
+
+    // tdf#139944 disable navigator in master mode
+    if (pViewShell)
+    {
+        if (const sd::DrawViewShell* pDrawViewShell = static_cast<::sd::DrawViewShell*>(pViewShell))
+        {
+            if (pDrawViewShell->GetEditMode() == EditMode::MasterPage)
+            {
+                m_xContainer->set_sensitive(false);
+                mxTlbObjects->clear();
+                return;
+            }
+            else
+                m_xContainer->set_sensitive(true);
+        }
+    }
 
     // Restore the 'ShowAllShapes' flag from the last time (in this session)
     // that the navigator was shown.
@@ -782,20 +817,6 @@ void SdNavigatorControllerItem::StateChangedAtToolBoxControl( sal_uInt16 nSId,
     NavDocInfo* pInfo = pNavigatorWin->GetDocInfo();
     if( !(pInfo && pInfo->IsActive()) )
         return;
-
-    if (::sd::DrawDocShell* pDrawDocShell = pInfo->GetDrawDocShell())
-    {
-        const auto pDrawViewShell =
-                static_cast<::sd::DrawViewShell *>(pDrawDocShell->GetViewShell());
-        if (pDrawViewShell)
-        {
-            pNavigatorWin->FreshTree(pDrawDocShell->GetDoc());
-            bool bEditModePage(pDrawViewShell->GetEditMode() == EditMode::Page);
-            pNavigatorWin->mxToolbox->set_sensitive(bEditModePage);
-            pNavigatorWin->mxLbDocs->set_sensitive(bEditModePage);
-            pNavigatorWin->mxTlbObjects->set_sensitive(bEditModePage);
-        }
-    }
 
     const SfxUInt32Item& rStateItem = dynamic_cast<const SfxUInt32Item&>(*pItem);
     NavState nState = static_cast<NavState>(rStateItem.GetValue());