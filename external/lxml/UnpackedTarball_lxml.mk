91d9c8ffcbef8076dee39b8a3abb70264877d833
Stephan Bergmann <stephan.bergmann@allotropia.de>
2024-04-03 20:19:17 +0200

    external/lxml: Silence -Wincompatible-pointer-types
    ...as seen with recent GCC 14 trunk,
    
    > gcc -Wno-unused-result -Wsign-compare -DDYNAMIC_ANNOTATIONS_ENABLED=1 -g -O0 -Wall -I/home/sberg/lo/core/workdir/UnpackedTarball/zlib -I/home/sberg/lo/core/workdir/UnpackedTarball/python3 -I/home/sberg/lo/core/workdir/UnpackedTarball/python3/Include -fPIC -DCYTHON_CLINE_IN_TRACEBACK=0 -I/home/sberg/lo/core/workdir/UnpackedTarball/libxml2/include -I/home/sberg/lo/core/workdir/UnpackedTarball/libxslt -Isrc -Isrc/lxml/includes -I/home/sberg/lo/core/workdir/UnpackedTarball/python3/Include -I/home/sberg/lo/core/workdir/UnpackedTarball/python3 -c src/lxml/etree.c -o build/temp.linux-x86_64-3.8-pydebug/src/lxml/etree.o -w
    > src/lxml/etree.c: In function ‘__pyx_pf_4lxml_5etree_11TreeBuilder_4data’:
    > src/lxml/etree.c:137698:66: error: passing argument 1 of ‘__pyx_f_4lxml_5etree_11TreeBuilder__handleSaxData’ from incompatible pointer type [-Wincompatible-pointer-types]
    > 137698 |   __pyx_t_1 = __pyx_f_4lxml_5etree_11TreeBuilder__handleSaxData(((struct __pyx_obj_4lxml_5etree__SaxParserTarget *)__pyx_v_self), __pyx_v_data); if (unlikely(__pyx_t_1 == ((int)-1))) __PYX_ERR(3, 832, __pyx_L1_error)
    >        |                                                                 ~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    >        |                                                                  |
    >        |                                                                  struct __pyx_obj_4lxml_5etree__SaxParserTarget *
    > src/lxml/etree.c:137074:105: note: expected ‘struct __pyx_obj_4lxml_5etree_TreeBuilder *’ but argument is of type ‘struct __pyx_obj_4lxml_5etree__SaxParserTarget *’
    > 137074 | static int __pyx_f_4lxml_5etree_11TreeBuilder__handleSaxData(struct __pyx_obj_4lxml_5etree_TreeBuilder *__pyx_v_self, PyObject *__pyx_v_data) {
    >        |                                                              ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~
    
    etc.
    
    Change-Id: I19f6768bf031c04730972c1fa4a006e2a7d50e27
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/165758
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <stephan.bergmann@allotropia.de>
    (cherry picked from commit f09b9b2785732319fd3199337b3528de3a561393)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/175428
    Tested-by: Stephan Bergmann <stephan.bergmann@allotropia.de>

--- external/lxml/UnpackedTarball_lxml.mk
+++ external/lxml/UnpackedTarball_lxml.mk
@@ -17,6 +17,7 @@ $(eval $(call gb_UnpackedTarball_add_patches,lxml, \
 	external/lxml/0001-Make-regexp-string-raw-to-correct-its-escape-sequenc.patch.1 \
 	external/lxml/replace-setuptools-with-distutils.patch.1 \
 	external/lxml/Wincompatible-function-pointer-types.patch \
+	external/lxml/Wincompatible-pointer-types.patch \
 ))
 
 # vim: set noet sw=4 ts=4:
