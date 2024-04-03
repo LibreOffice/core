LibreOffice/core

commit 75706c003c3e224047e2f5b079aebde4e3d40d74

tdf#146619 Remove unused #includes from C/C++ files
the 'desktop' module was cleaned

Change-Id: Ia7dcb77a42ca7f9be67e30d10cd54697bf93b6d0
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/165663
Tested-by: Jenkins
Reviewed-by: Gabor Kelemen <gabor.kelemen.extern@allotropia.de>

desktop/source/app/app.cxx
@@ -60,7 +60,6 @@
 #include <com/sun/star/system/SystemShellExecuteFlags.hpp>
 #include <com/sun/star/frame/Desktop.hpp>
 #include <com/sun/star/frame/StartModule.hpp>
-#include <com/sun/star/view/XPrintable.hpp>
 #include <com/sun/star/awt/XTopWindow.hpp>
 #include <com/sun/star/util/URLTransformer.hpp>
 #include <com/sun/star/util/XURLTransformer.hpp>
@@ -118,7 +117,6 @@
 #include <svl/eitem.hxx>
 #include <basic/sbstar.hxx>
 #include <desktop/crashreport.hxx>
-#include <tools/time.hxx>
 #include <tools/urlobj.hxx>
 #include <comphelper/diagnose_ex.hxx>
 #include <svtools/fontsubstconfig.hxx>

desktop/IwyuFilter_desktop.yaml
@@ -94,3 +94,9 @@ excludelist:
     desktop/source/splash/splash.cxx:
     # Needed for rtl::math::round
     - rtl/math.hxx
+    desktop/source/lib/lokandroid.cxx:
+    # Needed for osl_getThreadTextEncoding
+    - osl/thread.hxx
+    desktop/source/deployment/misc/dp_misc.cxx:
+    # Needed for osl_getThreadTextEncoding
+    - osl/thread.hxx

desktop/qa/desktop_lib/test_desktop_lib.cxx
@@ -11,7 +11,6 @@
 #include <memory>
 #include <string_view>
 
-#include <com/sun/star/frame/Desktop.hpp>
 #include <com/sun/star/lang/XComponent.hpp>
 #include <com/sun/star/text/XTextDocument.hpp>
 #include <com/sun/star/awt/Key.hpp>
@@ -31,8 +30,6 @@
 #include <vcl/uitest/uiobject.hxx>
 #include <comphelper/processfactory.hxx>
 #include <rtl/math.hxx>
-#include <rtl/uri.hxx>
-#include <sfx2/app.hxx>
 #include <sfx2/childwin.hxx>
 #include <sfx2/lokhelper.hxx>
 #include <test/unoapi_test.hxx>
@@ -45,14 +42,11 @@
 #include <sfx2/viewsh.hxx>
 #include <sfx2/viewfrm.hxx>
 #include <sfx2/bindings.hxx>
-#include <sfx2/sidebar/SidebarController.hxx>
-#include <sfx2/sidebar/SidebarDockingWindow.hxx>
 #include <unotools/datetime.hxx>
 #include <unotools/syslocaleoptions.hxx>
 #include <comphelper/string.hxx>
 #include <comphelper/scopeguard.hxx>
 #include <cairo.h>
-#include <config_features.h>
 #include <config_fonts.h>
 #include <config_mpl.h>
 #include <tools/json_writer.hxx>
@@ -2343,6 +2337,7 @@ void DesktopLOKTest::testPaintPartTileDifferentSchemes()
 }
 
 #if HAVE_MORE_FONTS
+#include <rtl/uri.hxx>
 void DesktopLOKTest::testGetFontSubset()
 {
     LibLODocument_Impl* pDocument = loadDoc("blank_text.odt");

desktop/source/app/appinit.cxx
@@ -24,7 +24,6 @@
 #include <dp_shared.hxx>
 #include "cmdlineargs.hxx"
 #include <strings.hrc>
-#include <com/sun/star/registry/XSimpleRegistry.hpp>
 #include <com/sun/star/lang/XInitialization.hpp>
 #include <com/sun/star/lang/XMultiServiceFactory.hpp>
 #include <com/sun/star/uno/Exception.hpp>

desktop/source/app/dispatchwatcher.cxx
@@ -20,7 +20,6 @@
 #include <sal/config.h>
 
 #include <sal/log.hxx>
-#include <sfx2/docfile.hxx>
 #include <sfx2/docfilt.hxx>
 #include <sfx2/fcontnr.hxx>
 #include <svl/fstathelper.hxx>

desktop/source/app/sofficemain.cxx
@@ -30,7 +30,6 @@
 #include <prewin.h>
 
 #include <rtl/bootstrap.hxx>
-#include <sal/log.hxx>
 #include <sal/main.h>
 #include <tools/extendapplicationenvironment.hxx>
 #include <vcl/svmain.hxx>

desktop/source/deployment/gui/dp_gui_dialog2.cxx
@@ -38,7 +38,6 @@
 #include <vcl/commandevent.hxx>
 #include <vcl/svapp.hxx>
 
-#include <osl/mutex.hxx>
 #include <sal/log.hxx>
 #include <rtl/ustrbuf.hxx>
 

desktop/source/deployment/inc/dp_version.hxx
@@ -21,9 +21,9 @@
 
 #include <sal/config.h>
 
-#include <rtl/ustring.hxx>
 #include "dp_misc_api.hxx"
 
+#include <string_view>
 
 namespace dp_misc {
 

commit 374ac88c2cab8065eca3b9479c0039f6893fedf0

desktop: fix build with enable-online-update
Regression from 75706c003c3e224047e2f5b079aebde4e3d40d74
"tdf#146619 Remove unused #includes from C/C++ files"

See https://ci.libreoffice.org/job/lo_daily_tb_win/1718/console

Change-Id: I32f406aa44dfe37c472cf4de7805141daf37ab66
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/165738
Tested-by: Jenkins
Reviewed-by: Gabor Kelemen <gabor.kelemen.extern@allotropia.de>

desktop/source/app/app.cxx
@@ -127,6 +127,10 @@
 #include "langselect.hxx"
 #include <salhelper/thread.hxx>
 
+#if HAVE_FEATURE_UPDATE_MAR
+#include <tools/time.hxx>
+#endif
+
 #if defined MACOSX
 #include <errno.h>
 #include <sys/wait.h>