ade498ce77693984d923037109d10f51d11b5f05
Mike Kaganski <mike.kaganski@collabora.com>
2024-04-07 18:23:52 +0500

    tdf#160526, tdf#160549: fix split conditions at page start
    A single large object in a paragraph must be moved down, when the page
    has other content before this. On the other hand, there must not be
    moving down, when an unsuccessful attempt to move was already done (so
    the master frame is empty), or even the first time, when the frame is
    at the page body start.
    
    Change-Id: Ib8e2fe7b77c622d9cfac22722ca6b55dba7ad8ae
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/165869
    Tested-by: Jenkins
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
    Signed-off-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/165889

--- sw/qa/extras/layout/layout3.cxx
+++ sw/qa/extras/layout/layout3.cxx
@@ -2353,6 +2353,27 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter3, testPageBreakInHiddenSection)
     assertXPath(pXmlDoc, "//page[4]/body/section/infos/bounds"_ostr, "height"_ostr, u"0"_ustr);
 }
 
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter3, testTdf160549)
+{
+    // Given a document with a large as-char object, alone in its paragraph, shifted down by a
+    // header object: it must not hang in a layout loop on import (similar to i84870, but not
+    // fixed by its fix)
+    createSwDoc("tdf160549.fodt");
+    // The object is the first in the document; it must not move to the next page
+    CPPUNIT_ASSERT_EQUAL(1, getPages());
+}
+
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter3, testTdf160526)
+{
+    // Given a document with a large as-char object, alone in its paragraph, shifted down by
+    // another body object
+    createSwDoc("tdf160526.fodt");
+    // It must move to the next page
+    CPPUNIT_ASSERT_EQUAL(2, getPages());
+    auto pExportDump = parseLayoutDump();
+    assertXPath(pExportDump, "//page[2]/body/txt/anchored/SwAnchoredDrawObject"_ostr);
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
