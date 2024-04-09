7b51ad7697e91ff58de1fdeeec021647d0592a80
Oliver Specht <oliver.specht@cib.de>
2024-04-09 15:28:00 +0200

    tdf#160267 Fix SVG and add PNG format from the clipboard
    Since 6c04b8356c0764ba8ac51448eee5ff577b08d9e2 SVG was added
    to the supported clipboard formats but it did not work.
    SVG support is now fixed and PNG format is added in the gallery.
    
    Change-Id: Ic9cb2eebfc37bd38605dfaa3a1c353a7532af357
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/175715
    Reviewed-by: Thorsten Behrens <thorsten.behrens@allotropia.de>
    Tested-by: Thorsten Behrens <thorsten.behrens@allotropia.de>

--- svx/source/gallery2/galmisc.cxx
+++ svx/source/gallery2/galmisc.cxx
@@ -494,7 +494,8 @@ bool GalleryTransferable::GetData( const datatransfer::DataFlavor& rFlavor, cons
     {
         bRet = SetGDIMetaFile( mpGraphicObject->GetGraphic().GetGDIMetaFile() );
     }
-    else if( ( SotClipboardFormatId::BITMAP == nFormat ) && mpGraphicObject )
+    else if( ( SotClipboardFormatId::BITMAP == nFormat || SotClipboardFormatId::PNG == nFormat)
+        && mpGraphicObject )
     {
         bRet = SetBitmapEx( mpGraphicObject->GetGraphic().GetBitmapEx(), rFlavor );
     }
