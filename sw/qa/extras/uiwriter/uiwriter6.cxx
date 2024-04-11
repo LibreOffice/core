5de5a5bde5024b5cdf8cfc1aa342c387f0cb1565
Oliver Specht <oliver.specht@cib.de>
2024-04-11 08:56:17 +0200

    Improve case rotation in sentence case
    Rotating case (Shift+F3) applies sentence case only
    if multiple words are already selected or selects
    sentence if SENTENCE_CASE is going to be applied.
    
    Change-Id: I9e8536d7744a344d7ad54150783e91e843e0e81e
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/165986
    Tested-by: Jenkins
    Reviewed-by: Thorsten Behrens <thorsten.behrens@allotropia.de>

--- sw/qa/extras/uiwriter/uiwriter6.cxx
+++ sw/qa/extras/uiwriter/uiwriter6.cxx
@@ -721,10 +721,6 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest6, testTdf157988)
 
     dispatchCommand(mxComponent, ".uno:ChangeCaseRotateCase", {});
 
-    CPPUNIT_ASSERT(getParagraph(1)->getString().startsWith("Integer sodalesSODALES"));
-
-    dispatchCommand(mxComponent, ".uno:ChangeCaseRotateCase", {});
-
     CPPUNIT_ASSERT(getParagraph(1)->getString().startsWith("Integer sodales tincidunt"));
 
     dispatchCommand(mxComponent, ".uno:ChangeCaseRotateCase", {});
@@ -740,10 +736,6 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest6, testTdf157988)
     dispatchCommand(mxComponent, ".uno:Escape", {});
 
     dispatchCommand(mxComponent, ".uno:GoRight", {});
-
-    dispatchCommand(mxComponent, ".uno:ChangeCaseRotateCase", {});
-
-    CPPUNIT_ASSERT(getParagraph(1)->getString().startsWith("Integer sodalesSODALES"));
 
     dispatchCommand(mxComponent, ".uno:ChangeCaseRotateCase", {});
 
