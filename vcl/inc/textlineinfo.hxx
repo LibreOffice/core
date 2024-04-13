7d38bdbf96c42d367dfa2cc7e2916c814aa08de2
HakimOttey <hakimotteybusiness@gmail.com>
2024-04-12 20:35:25 -0400

    tdf#143148 replace includes guards in vcl textlineinfo with pragma
    Change-Id: I733263074a5c4911499a6b5becfbbc87cbda968d
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/166044
    Tested-by: Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>
    Reviewed-by: Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>

--- vcl/inc/textlineinfo.hxx
+++ vcl/inc/textlineinfo.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_VCL_INC_TEXTLINEINFO_HXX
-#define INCLUDED_VCL_INC_TEXTLINEINFO_HXX
+#pragma once
 
 #include <tools/long.hxx>
 
@@ -68,7 +67,5 @@ private:
     std::vector<ImplTextLineInfo>  mvLines;
 
 };
-
-#endif // INCLUDED_VCL_INC_TEXTLINEINFO_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
