123fdb0c7ac53f01406208f3c219e2716599effd
Omkar Santosh Nikam <omkar.nikam.2026@gmail.com>
2024-04-17 14:44:58 +0530

    tdf#143148: Use pragma once instead of include guards
    Change-Id: I84080f2b647ccdd91fa44401b4edae4bf31a92a3
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/166167
    Tested-by: Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>
    Reviewed-by: Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>

--- include/basic/sberrors.hxx
+++ include/basic/sberrors.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_BASIC_SBERRORS_HXX
-#define INCLUDED_BASIC_SBERRORS_HXX
+#pragma once
 
 #include <comphelper/errcode.hxx>
 
@@ -170,7 +169,5 @@
 #define ERRCODE_BASIC_OPER_NOT_PERFORM      ErrCode( ErrCodeArea::Sbx, ErrCodeClass::Compiler, 136 )  // Can't perform requested operation
 #define ERRCODE_BASIC_TOO_MANY_DLL          ErrCode( ErrCodeArea::Sbx, ErrCodeClass::Compiler, 137 )  // Too many dll application clients
 #define ERRCODE_BASIC_LOOP_NOT_INIT         ErrCode( ErrCodeArea::Sbx, ErrCodeClass::Compiler, 138 )  // For loop not initialized
-
-#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
