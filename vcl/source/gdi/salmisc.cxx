LibreOffice/core

commit 6588c30ed4477627b2623560ca867682b189bc80

vcl: separate scanline direction from ScanlineFormat
ScanlineFormat enum is used to indicate 2 things - scanline format
type and scanline direction (TopDown or BottomUp). This makes it
complex to manipulate with (using bit arithmetics) and hard to use
in general for no benefit, so this commit separates direction out
from the ScanlineFormat into ScanlineDirection enum. ScanlineFormat
is now just a simple enum class (not a bit field).

Change-Id: Iad55d0a4c8c07b71221c2facf7cf6a2d518fec0c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/163943
Tested-by: Jenkins
Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

vcl/source/gdi/salmisc.cxx
@@ -92,7 +92,7 @@ static void ImplPALToTC( const BitmapBuffer& rSrcBuffer, BitmapBuffer const & rD
     const ColorMask&    rDstMask = rDstBuffer.maColorMask;
     const BitmapColor*  pColBuf = rSrcBuffer.maPalette.ImplGetColorBuffer();
 
-    if( RemoveScanline( rSrcBuffer.mnFormat ) == ScanlineFormat::N1BitMsbPal )
+    if (rSrcBuffer.meFormat == ScanlineFormat::N1BitMsbPal)
     {
         const BitmapColor   aCol0( pColBuf[ 0 ] );
         const BitmapColor   aCol1( pColBuf[ 1 ] );
@@ -114,7 +114,7 @@ static void ImplPALToTC( const BitmapBuffer& rSrcBuffer, BitmapBuffer const & rD
             DOUBLE_SCANLINES();
         }
     }
-    else if( RemoveScanline( rSrcBuffer.mnFormat ) == ScanlineFormat::N8BitPal )
+    else if (rSrcBuffer.meFormat == ScanlineFormat::N8BitPal)
     {
         for (tools::Long nActY = 0; nActY < rDstBuffer.mnHeight; ++nActY)
         {
@@ -150,7 +150,7 @@ static void ImplTCToTC( const BitmapBuffer& rSrcBuffer, BitmapBuffer const & rDs
     const ColorMask&    rSrcMask = rSrcBuffer.maColorMask;
     const ColorMask&    rDstMask = rDstBuffer.maColorMask;
 
-    if( RemoveScanline( rSrcBuffer.mnFormat ) == ScanlineFormat::N24BitTcBgr )
+    if (rSrcBuffer.meFormat == ScanlineFormat::N24BitTcBgr)
     {
         BitmapColor aCol;
         sal_uInt8* pPixel = nullptr;
@@ -235,7 +235,7 @@ std::optional<BitmapBuffer> StretchAndConvert(
     std::optional<BitmapBuffer> pDstBuffer(std::in_place);
 
     // set function for getting pixels
-    pFncGetPixel = BitmapReadAccess::GetPixelFunction( rSrcBuffer.mnFormat );
+    pFncGetPixel = BitmapReadAccess::GetPixelFunction(rSrcBuffer.meFormat);
     if( !pFncGetPixel )
     {
         // should never come here
@@ -246,8 +246,7 @@ std::optional<BitmapBuffer> StretchAndConvert(
     }
 
     // set function for setting pixels
-    const ScanlineFormat nDstScanlineFormat = RemoveScanline( nDstBitmapFormat );
-    switch( nDstScanlineFormat )
+    switch (nDstBitmapFormat)
     {
         IMPL_CASE_SET_FORMAT( N1BitMsbPal, 1 );
         IMPL_CASE_SET_FORMAT( N8BitPal, 8 );
@@ -270,7 +269,7 @@ std::optional<BitmapBuffer> StretchAndConvert(
     }
 
     // fill destination buffer
-    pDstBuffer->mnFormat = nDstBitmapFormat;
+    pDstBuffer->meFormat = nDstBitmapFormat;
     pDstBuffer->mnWidth = rTwoRect.mnDestWidth;
     pDstBuffer->mnHeight = rTwoRect.mnDestHeight;
     tools::Long nScanlineBase;
@@ -300,8 +299,8 @@ std::optional<BitmapBuffer> StretchAndConvert(
     }
 
     // do we need a destination palette or color mask?
-    if( ( nDstScanlineFormat == ScanlineFormat::N1BitMsbPal ) ||
-        ( nDstScanlineFormat == ScanlineFormat::N8BitPal ) )
+    if (nDstBitmapFormat == ScanlineFormat::N1BitMsbPal ||
+        nDstBitmapFormat == ScanlineFormat::N8BitPal)
     {
         assert(pDstPal && "destination buffer requires palette");
         if (!pDstPal)
@@ -310,7 +309,7 @@ std::optional<BitmapBuffer> StretchAndConvert(
         }
         pDstBuffer->maPalette = *pDstPal;
     }
-    else if(nDstScanlineFormat == ScanlineFormat::N32BitTcMask )
+    else if (nDstBitmapFormat == ScanlineFormat::N32BitTcMask)
     {
         assert(pDstMask && "destination buffer requires color mask");
         if (!pDstMask)
@@ -376,7 +375,7 @@ std::optional<BitmapBuffer> StretchAndConvert(
     // source scanline buffer
     Scanline pTmpScan;
     tools::Long nOffset;
-    if( rSrcBuffer.mnFormat & ScanlineFormat::TopDown )
+    if (rSrcBuffer.meDirection == ScanlineDirection::TopDown)
     {
         pTmpScan = rSrcBuffer.mpBits;
         nOffset = rSrcBuffer.mnScanlineSize;
@@ -391,7 +390,7 @@ std::optional<BitmapBuffer> StretchAndConvert(
         pSrcScan[ i ] = pTmpScan;
 
     // destination scanline buffer
-    if( pDstBuffer->mnFormat & ScanlineFormat::TopDown )
+    if (pDstBuffer->meDirection == ScanlineDirection::TopDown)
     {
         pTmpScan = pDstBuffer->mpBits;
         nOffset = pDstBuffer->mnScanlineSize;

include/vcl/BitmapBuffer.hxx
@@ -39,7 +39,8 @@ struct VCL_DLLPUBLIC BitmapBuffer
     tools::Long     mnScanlineSize;
     BitmapPalette   maPalette;
     sal_uInt8*      mpBits;
-    ScanlineFormat  mnFormat;
+    ScanlineFormat  meFormat = ScanlineFormat::NONE;
+    ScanlineDirection meDirection = ScanlineDirection::BottomUp;
     ColorMask       maColorMask;
     sal_uInt16      mnBitCount;
 };

include/vcl/BitmapInfoAccess.hxx
@@ -52,7 +52,7 @@ public:
     {
         assert(mpBuffer && "Access is not valid!");
 
-        return mpBuffer && (mpBuffer->mnFormat & ScanlineFormat::TopDown);
+        return mpBuffer && mpBuffer->meDirection == ScanlineDirection::TopDown;
     }
 
     bool IsBottomUp() const { return !IsTopDown(); }
@@ -61,7 +61,7 @@ public:
     {
         assert(mpBuffer && "Access is not valid!");
 
-        return mpBuffer ? RemoveScanline(mpBuffer->mnFormat) : ScanlineFormat::NONE;
+        return mpBuffer ? mpBuffer->meFormat : ScanlineFormat::NONE;
     }
 
     sal_uInt32 GetScanlineSize() const

include/vcl/BitmapReadAccess.hxx
@@ -49,7 +49,7 @@ public:
         assert(mpBuffer && "Access is not valid!");
         assert(nY < mpBuffer->mnHeight && "y-coordinate out of range!");
 
-        if (mpBuffer->mnFormat & ScanlineFormat::TopDown)
+        if (mpBuffer->meDirection == ScanlineDirection::TopDown)
         {
             return mpBuffer->mpBits + (nY * mpBuffer->mnScanlineSize);
         }

include/vcl/Scanline.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_VCL_SCANLINE_HXX
-#define INCLUDED_VCL_SCANLINE_HXX
+#pragma once
 
 #include <o3tl/typed_flags_set.hxx>
 #include <sal/types.h>
@@ -26,35 +25,28 @@
 typedef sal_uInt8*        Scanline;
 typedef const sal_uInt8*  ConstScanline;
 
-enum class ScanlineFormat {
-    NONE              = 0x00000000,
-
-    N1BitMsbPal       = 0x00000001,
-
-    N8BitPal          = 0x00000010,
-
-    N24BitTcBgr       = 0x00000100,
-    N24BitTcRgb       = 0x00000200,
-
-    N32BitTcAbgr      = 0x00000800,
-    N32BitTcArgb      = 0x00001000,
-    N32BitTcBgra      = 0x00002000,
-    N32BitTcRgba      = 0x00004000,
-    N32BitTcMask      = 0x00008000,
-
-    TopDown           = 0x00010000 // scanline adjustment
+enum class ScanlineFormat : sal_uInt8
+{
+    NONE,
+    // 1 Bit - still needed?
+    N1BitMsbPal,
+    // 8 Bit Palette
+    N8BitPal,
+    // 24 Bit
+    N24BitTcBgr,
+    N24BitTcRgb,
+    // 32 Bit
+    N32BitTcAbgr,
+    N32BitTcArgb,
+    N32BitTcBgra,
+    N32BitTcRgba,
+    N32BitTcMask,
 };
 
-namespace o3tl
+enum class ScanlineDirection : sal_uInt8
 {
-    template<> struct typed_flags<ScanlineFormat> : is_typed_flags<ScanlineFormat, 0x0001fb11> {};
-}
-
-inline ScanlineFormat RemoveScanline(ScanlineFormat nFormat)
-{
-    return nFormat & ~ScanlineFormat::TopDown;
-}
-
-#endif // INCLUDED_VCL_SCANLINE_HXX
+    BottomUp,
+    TopDown
+};
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

include/vcl/cairo.hxx
@@ -36,22 +36,22 @@
  without needing to swizzle it for use as a canvas ImageData.
 */
 #if ENABLE_CAIRO_RGBA
-#define SVP_24BIT_FORMAT (ScanlineFormat::N24BitTcRgb | ScanlineFormat::TopDown)
-#define SVP_CAIRO_FORMAT (ScanlineFormat::N32BitTcRgba | ScanlineFormat::TopDown)
+#define SVP_24BIT_FORMAT (ScanlineFormat::N24BitTcRgb)
+#define SVP_CAIRO_FORMAT (ScanlineFormat::N32BitTcRgba)
 #define SVP_CAIRO_BLUE 1
 #define SVP_CAIRO_GREEN 2
 #define SVP_CAIRO_RED 0
 #define SVP_CAIRO_ALPHA 3
 #elif defined OSL_BIGENDIAN
-#define SVP_24BIT_FORMAT (ScanlineFormat::N24BitTcRgb | ScanlineFormat::TopDown)
-#define SVP_CAIRO_FORMAT (ScanlineFormat::N32BitTcArgb | ScanlineFormat::TopDown)
+#define SVP_24BIT_FORMAT (ScanlineFormat::N24BitTcRgb)
+#define SVP_CAIRO_FORMAT (ScanlineFormat::N32BitTcArgb)
 #define SVP_CAIRO_BLUE 3
 #define SVP_CAIRO_GREEN 2
 #define SVP_CAIRO_RED 1
 #define SVP_CAIRO_ALPHA 0
 #else
-#define SVP_24BIT_FORMAT (ScanlineFormat::N24BitTcBgr | ScanlineFormat::TopDown)
-#define SVP_CAIRO_FORMAT (ScanlineFormat::N32BitTcBgra | ScanlineFormat::TopDown)
+#define SVP_24BIT_FORMAT (ScanlineFormat::N24BitTcBgr)
+#define SVP_CAIRO_FORMAT (ScanlineFormat::N32BitTcBgra)
 #define SVP_CAIRO_BLUE 0
 #define SVP_CAIRO_GREEN 1
 #define SVP_CAIRO_RED 2

vcl/headless/BitmapHelper.cxx
@@ -39,9 +39,10 @@ BitmapHelper::BitmapHelper(const SalBitmap& rSourceBitmap, const bool bForceARGB
         const SalTwoRect aTwoRect
             = { 0, 0, pSrc->mnWidth, pSrc->mnHeight, 0, 0, pSrc->mnWidth, pSrc->mnHeight };
         std::optional<BitmapBuffer> pTmp
-            = (pSrc->mnFormat == SVP_24BIT_FORMAT
-                   ? FastConvert24BitRgbTo32BitCairo(pSrc)
-                   : StretchAndConvert(*pSrc, aTwoRect, SVP_CAIRO_FORMAT));
+            = (pSrc->meFormat == SVP_24BIT_FORMAT
+               && pSrc->meDirection == ScanlineDirection::TopDown)
+                  ? FastConvert24BitRgbTo32BitCairo(pSrc)
+                  : StretchAndConvert(*pSrc, aTwoRect, SVP_CAIRO_FORMAT);
         aTmpBmp.Create(std::move(pTmp));
 
         assert(aTmpBmp.GetBitCount() == 32);

vcl/headless/CairoCommon.cxx
@@ -1993,11 +1993,12 @@ std::optional<BitmapBuffer> FastConvert24BitRgbTo32BitCairo(const BitmapBuffer* 
     if (pSrc == nullptr)
         return std::nullopt;
 
-    assert(pSrc->mnFormat == SVP_24BIT_FORMAT);
+    assert(pSrc->meFormat == SVP_24BIT_FORMAT);
     const tools::Long nWidth = pSrc->mnWidth;
     const tools::Long nHeight = pSrc->mnHeight;
     std::optional<BitmapBuffer> pDst(std::in_place);
-    pDst->mnFormat = (ScanlineFormat::N32BitTcArgb | ScanlineFormat::TopDown);
+    pDst->meFormat = ScanlineFormat::N32BitTcArgb;
+    pDst->meDirection = ScanlineDirection::TopDown;
     pDst->mnWidth = nWidth;
     pDst->mnHeight = nHeight;
     pDst->mnBitCount = 32;
@@ -2039,33 +2040,27 @@ std::optional<BitmapBuffer> FastConvert24BitRgbTo32BitCairo(const BitmapBuffer* 
         for (tools::Long x = 0; x < nWidth; ++x)
         {
 #if ENABLE_CAIRO_RGBA
-            static_assert((SVP_CAIRO_FORMAT & ~ScanlineFormat::TopDown)
-                              == ScanlineFormat::N32BitTcRgba,
+            static_assert(SVP_CAIRO_FORMAT == ScanlineFormat::N32BitTcRgba,
                           "Expected SVP_CAIRO_FORMAT set to N32BitTcBgra");
-            static_assert((SVP_24BIT_FORMAT & ~ScanlineFormat::TopDown)
-                              == ScanlineFormat::N24BitTcRgb,
+            static_assert(SVP_24BIT_FORMAT == ScanlineFormat::N24BitTcRgb,
                           "Expected SVP_24BIT_FORMAT set to N24BitTcRgb");
             pD[0] = pS[0];
             pD[1] = pS[1];
             pD[2] = pS[2];
             pD[3] = 0xff; // Alpha
 #elif defined OSL_BIGENDIAN
-            static_assert((SVP_CAIRO_FORMAT & ~ScanlineFormat::TopDown)
-                              == ScanlineFormat::N32BitTcArgb,
+            static_assert(SVP_CAIRO_FORMAT == ScanlineFormat::N32BitTcArgb,
                           "Expected SVP_CAIRO_FORMAT set to N32BitTcBgra");
-            static_assert((SVP_24BIT_FORMAT & ~ScanlineFormat::TopDown)
-                              == ScanlineFormat::N24BitTcRgb,
+            static_assert(SVP_24BIT_FORMAT == ScanlineFormat::N24BitTcRgb,
                           "Expected SVP_24BIT_FORMAT set to N24BitTcRgb");
             pD[0] = 0xff; // Alpha
             pD[1] = pS[0];
             pD[2] = pS[1];
             pD[3] = pS[2];
 #else
-            static_assert((SVP_CAIRO_FORMAT & ~ScanlineFormat::TopDown)
-                              == ScanlineFormat::N32BitTcBgra,
+            static_assert(SVP_CAIRO_FORMAT == ScanlineFormat::N32BitTcBgra,
                           "Expected SVP_CAIRO_FORMAT set to N32BitTcBgra");
-            static_assert((SVP_24BIT_FORMAT & ~ScanlineFormat::TopDown)
-                              == ScanlineFormat::N24BitTcBgr,
+            static_assert(SVP_24BIT_FORMAT == ScanlineFormat::N24BitTcBgr,
                           "Expected SVP_24BIT_FORMAT set to N24BitTcBgr");
             pD[0] = pS[0];
             pD[1] = pS[1];

commit 92edab3d95ff5ee8a81a453b3224157839dfd32c

tdf#160016 tdf#160704 Set the direction from src in StretchAndConvert
The direction of the BitmapBuffer was never set, so it defaulted
to BottomUp. The best is to set the direction to be the same as
source - It's unlikely they would be different.

Regression from 6588c30ed4477627b2623560ca867682b189bc80
"vcl: separate scanline direction from ScanlineFormat"

Change-Id: I0e02106309141d09160defee0a0d72d34642a6ea
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/166182
Tested-by: Jenkins
Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

vcl/source/gdi/salmisc.cxx
@@ -234,6 +234,8 @@ std::optional<BitmapBuffer> StretchAndConvert(
     FncSetPixel     pFncSetPixel;
     std::optional<BitmapBuffer> pDstBuffer(std::in_place);
 
+
+    pDstBuffer->meDirection = rSrcBuffer.meDirection;
     // set function for getting pixels
     pFncGetPixel = BitmapReadAccess::GetPixelFunction(rSrcBuffer.meFormat);
     if( !pFncGetPixel )