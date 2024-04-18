1675c3e2f14c68873e63cb725e0dd6e4b70cb3c3
Justin Luth <justin.luth@collabora.com>
2024-04-18 17:30:06 -0400

    xlsx export: never export short-hand version of range address
    If all the columns are in focus, LO was exporting
    A10:XFD11 as 10:11.
    
    Amazingly, we can't read that. Seriously?
    Doesn't Microsoft ever export in shorthand?
    
    I thought it would be helpful to find any instances
    where LO is exporting shorthand, to verify that
    we really do break import of round-tripped files.
    
    I made a brief attempt at fixing the import,
    but it will require lots of changes
    and break lots of assumptions.
    Obviously it can't be a hugely common problem
    because I didn't find any bug reports about it...
    I abandoned my attempt because it is too prone to disaster.
    
    Change-Id: If59c316d0af466e5cf71ecc459bdf57bb2cd5144
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/166269
    Tested-by: Jenkins
    Reviewed-by: Justin Luth <jluth@mail.com>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    (cherry picked from commit a29d91ac403f1ed431ca95b8b9c290bd354c3ae7)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/166515
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

--- sc/source/filter/excel/xestream.cxx
+++ sc/source/filter/excel/xestream.cxx
@@ -745,7 +745,8 @@ OString XclXmlUtils::ToOString( const ScDocument& rDoc, const ScRange& rRange, b
 OString XclXmlUtils::ToOString( const ScDocument& rDoc, const ScRangeList& rRangeList )
 {
     OUString s;
-    rRangeList.Format(s, ScRefFlags::VALID, rDoc, FormulaGrammar::CONV_XL_OOX, ' ');
+    rRangeList.Format(s, ScRefFlags::VALID, rDoc, FormulaGrammar::CONV_XL_OOX, ' ',
+                      /*FullAddressNotation=*/true);
     return s.toUtf8();
 }
 
