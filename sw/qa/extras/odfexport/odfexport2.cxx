0c1e01cd8370b1c05e12dc965bf3e95bc2fd7378
Michael Stahl <michael.stahl@allotropia.de>
2024-04-19 18:04:47 +0200

    tdf#114287 tdf#159366 xmloff: ODF import: revert text:list override
    This reverts commit ade0a153f453500f15343380ac937252992733e0 "tdf#114287
    xmloff: ODF import: fix text:list override of list style" and subsequent
    commits 7cf5faec6fdbc27dd77d2d36fb2ff205322cba0d and
    1b2a6b98291cf8b7022951be19b915fe2a9e18e6.
    
    It turns out that there is actually a paragraph in ODF 1.2 and later
    that gives paragraph's indent priority over the list style's, which i
    unfortunately missed when investigating the above issues:
    
      17.20  <style:list-level-label-alignment>
      ...
    
      The fo:text-indent and fo:margin-left attributes are evaluated only
      for paragraphs inside list items whose paragraph styles do not specify
      them. If one of the two properties, or both, are specified by the
      paragraph style, the text indent and/or left margin are taken from the
      paragraph style. In this case the paragraph style's properties are
      used to determine the indent and margin for the text lines and thus
      also the alignment position.
    
    This is usually interpreted as "on the same level", so applying a
    list-style at a paragraph style overrides the indents inherited from a
    parent paragraph style, but if the style then specifies its own indents
    that overrides the list style's indents.
    
    Furthermore it turns out that Google Docs now imports this bugdoc the
    same way as LO 7.5 does; unclear if that changed recently (it shows
    it like Word does in the preview, but like LO 7.5 does after opening it).
    
    This means the way MS Word imports the bugdoc, which remains unchanged,
    now looks like a bug and we should not change LO to be compatible with
    it (when the same structure is created in Word, storing to ODF and
    loading again, it looks different too).
    
    Change-Id: I0c65bde38de9e6fcbccec55899c60c3245adb5ba
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/166309
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
    Tested-by: Jenkins
    (cherry picked from commit 546741148863ae0b2a25cc994a7323c8113cb573)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/166717
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- sw/qa/extras/odfexport/odfexport2.cxx
+++ sw/qa/extras/odfexport/odfexport2.cxx
@@ -852,9 +852,9 @@ DECLARE_ODFEXPORT_TEST(testTdf114287, "tdf114287.odt")
     xmlDocUniquePtr pXmlDoc = parseLayoutDump();
     assertXPath(pXmlDoc, "/root/page[1]/body/txt[2]/infos/prtBounds", "left", "2268");
     assertXPath(pXmlDoc, "/root/page[1]/body/txt[2]/infos/prtBounds", "right", "11339");
-    // the problem was that the list style name of the list must override the
-    // paragraph style even though it's the same list style
-    assertXPath(pXmlDoc, "/root/page[1]/body/txt[9]/infos/prtBounds", "left", "357");
+    // the list style name of the list is the same as the list style name of the
+    // paragraph, but in any case the margins of the paragraph take precedence
+    assertXPath(pXmlDoc, "/root/page[1]/body/txt[9]/infos/prtBounds", "left", "2268");
     assertXPath(pXmlDoc, "/root/page[1]/body/txt[9]/infos/prtBounds", "right", "11339");
     assertXPath(pXmlDoc, "/root/page[1]/body/txt[16]/infos/prtBounds", "left", "357");
     assertXPath(pXmlDoc, "/root/page[1]/body/txt[16]/infos/prtBounds", "right", "11339");
