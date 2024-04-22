ca574d5eef68af970cb49f90c7fc4fe813c9009c
Patrick Luby <guibmacdev@gmail.com>
2024-04-22 19:36:14 -0400

    tdf#160714 use crop box for bounds of embedded PDF object
    If there is no crop box, fallback to the media box just to be safe.
    
    Change-Id: I29f99a43e550cf09a1534c0aa01ffd6a55536fb7
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/166544
    Tested-by: Jenkins
    Reviewed-by: Patrick Luby <guibomacdev@gmail.com>
    (cherry picked from commit 4b31f87e918c38a7eb30ceb85563a5c98b426da5)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/166670
    Reviewed-by: Adolfo Jayme Barrientos <fitojb@ubuntu.com>

--- vcl/qa/cppunit/pdfexport/pdfexport2.cxx
+++ vcl/qa/cppunit/pdfexport/pdfexport2.cxx
@@ -40,6 +40,7 @@
 #include <o3tl/string_view.hxx>
 
 #include <vcl/filter/PDFiumLibrary.hxx>
+#include <vcl/pdfread.hxx>
 #include <comphelper/propertyvalue.hxx>
 #include <cmath>
 
@@ -4580,7 +4581,10 @@ CPPUNIT_TEST_FIXTURE(PdfExportTest2, testRexportMediaBoxOrigin)
         = { // Rotation by $\theta$ $cos(\theta), sin(\theta), -sin(\theta), cos(\theta)$
             0, -1, 1, 0,
             // Translate x,y
-            -aOrigin[1] - aSize[1] / 2 + aSize[0] / 2, aOrigin[0] + aSize[0] / 2 + aSize[1] / 2
+            -aOrigin[1] - aSize[1] / vcl::PDF_INSERT_MAGIC_SCALE_FACTOR / 2
+                + aSize[0] / vcl::PDF_INSERT_MAGIC_SCALE_FACTOR / 2,
+            aOrigin[0] + aSize[0] / vcl::PDF_INSERT_MAGIC_SCALE_FACTOR / 2
+                + aSize[1] / vcl::PDF_INSERT_MAGIC_SCALE_FACTOR / 2
           };
 
     for (sal_Int32 nIdx = 0; nIdx < 6; ++nIdx)
