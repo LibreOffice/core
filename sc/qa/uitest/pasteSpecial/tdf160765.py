985a85437c1352079ff1812b6039874b5226882b
Andreas Heinisch <andreas.heinisch@yahoo.de>
2024-04-22 20:06:50 +0200

    tdf#160765 - Save content for undo when pasting notes
    even if no content was changed after pasting special. Otherwise, the undo functionality crashes due to undoing a cell with no content where the annotation indicator will remain (unod/redo of note captions are handled via drawing undo).
    
    Change-Id: I7007fce510d6e9896cbda11a1e14a61b5ccb34a9
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/166469
    Tested-by: Jenkins
    Reviewed-by: Stéphane Guillou <stephane.guillou@libreoffice.org>
    Reviewed-by: Andreas Heinisch <andreas.heinisch@yahoo.de>
    (cherry picked from commit a1694b194c2b3e89250c5b79a69e7dd184b69976)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/166523
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
    (cherry picked from commit 7e2e1d7fd4531f57bbc30a0a8e1cb8c40519a349)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/166527
    Tested-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Reviewed-by: Patrick Luby <guibomacdev@gmail.com>
    Reviewed-by: Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>

---
+++ sc/qa/uitest/pasteSpecial/tdf160765.py
@@ -0,0 +1,75 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+from uitest.framework import UITestCase
+
+from libreoffice.calc.document import get_cell_by_position
+from libreoffice.uno.propertyvalue import mkPropertyValues
+from uitest.uihelper.calc import enter_text_to_cell
+from libreoffice.calc.paste_special import reset_default_values
+
+class tdf160765(UITestCase):
+    def test_tdf160765_undo_paste_comment(self):
+        with self.ui_test.create_doc_in_start_center("calc") as document:
+            xGridWin = self.xUITest.getTopFocusWindow().getChild("grid_window")
+
+            # Write text to cell A1 and B1
+            enter_text_to_cell(xGridWin, "A1", "A1 sample text")
+            enter_text_to_cell(xGridWin, "B1", "B1 sample text")
+
+            # Insert a comment in cell B1
+            xArgs = mkPropertyValues({"Text": "Comment 1"})
+            self.xUITest.executeCommandWithParameters(".uno:InsertAnnotation", xArgs)
+
+            # Insert a comment in cell A2
+            xGridWin.executeAction("SELECT", mkPropertyValues({"CELL":"A2"}))
+            xArgs = mkPropertyValues({"Text": "Comment 2"})
+            self.xUITest.executeCommandWithParameters(".uno:InsertAnnotation", xArgs)
+
+            # Copy cell A2 to clipboard
+            xGridWin.executeAction("SELECT", mkPropertyValues({"CELL": "A2"}))
+            self.xUITest.executeCommand(".uno:Copy")
+
+            # Set cursor to cells and paste data using special options (check only comments)
+            targetCells = ["A1", "B1"]
+            for index, targetCell in enumerate(targetCells):
+                xGridWin.executeAction("SELECT", mkPropertyValues({"CELL": targetCell}))
+                with self.ui_test.execute_dialog_through_command(".uno:PasteSpecial") as xPasteSpecialDlg:
+                    reset_default_values(self, xPasteSpecialDlg)
+                    xDateTimeChkBox = xPasteSpecialDlg.getChild("datetime")
+                    xDateTimeChkBox.executeAction("CLICK", tuple())
+                    xTextChkBox = xPasteSpecialDlg.getChild("text")
+                    xTextChkBox.executeAction("CLICK", tuple())
+                    xNumbersChkBox = xPasteSpecialDlg.getChild("numbers")
+                    xNumbersChkBox.executeAction("CLICK", tuple())
+                    xCommentsChkBox = xPasteSpecialDlg.getChild("comments")
+                    xCommentsChkBox.executeAction("CLICK", tuple())
+
+            # Undo both inserted comments
+            self.xUITest.executeCommand(".uno:Undo")
+            # Without the fix in place, this test would have failed with
+            # AssertionError: 'Comment 1' != ''
+            # i.e., the cell does not contain any comment
+            self.assertEqual("Comment 1", get_cell_by_position(document, 0, 1, 0).Annotation.String)
+            self.xUITest.executeCommand(".uno:Undo")
+            self.assertEqual("", get_cell_by_position(document, 0, 0, 0).Annotation.String)
+
+            # Redo both inserted comments
+            self.xUITest.executeCommand(".uno:Redo")
+            # Without the fix in place, this test would have failed with
+            # AssertionError: 'Comment 2' != ''
+            # i.e., the cell does not contain the restored comment
+            self.assertEqual("Comment 2", get_cell_by_position(document, 0, 0, 0).Annotation.String)
+            self.xUITest.executeCommand(".uno:Redo")
+            # Without the fix in place, this test would have failed with
+            # AssertionError: 'Comment 2' != ''
+            # i.e., the cell does not contain the restored comment
+            self.assertEqual("Comment 2", get_cell_by_position(document, 0, 0, 0).Annotation.String)
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
