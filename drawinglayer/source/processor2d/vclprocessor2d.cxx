331fcd66d047d4b12e4b958f88f92825ddd2acbd
Mike Kaganski <mike.kaganski@collabora.com>
2024-04-26 01:00:21 +0500

    tdf#157240: also set font color together with font
    Commit b008831a5545e5a777d77547ef96b9798d795f30 (tdf#152990 set the font
    after the MapMode is (potentially) set, 2023-01-14) had moved setting of
    font forward, but left setting the font color behind. This fixed that.
    
    Change-Id: I73fadc7c139da3b6f84b7fc875c585d02fc1c719
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/166654
    Tested-by: Jenkins
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

--- drawinglayer/source/processor2d/vclprocessor2d.cxx
+++ drawinglayer/source/processor2d/vclprocessor2d.cxx
@@ -329,8 +329,6 @@ void VclProcessor2D::RenderTextSimpleOrDecoratedPortionPrimitive2D(
                 mpOutputDevice->SetLayoutMode(nRTLLayoutMode);
             }
 
-            mpOutputDevice->SetTextColor(Color(aRGBFontColor));
-
             OUString aText(rTextCandidate.getText());
             sal_Int32 nPos = rTextCandidate.getTextPosition();
             sal_Int32 nLen = rTextCandidate.getTextLength();
@@ -428,6 +426,7 @@ void VclProcessor2D::RenderTextSimpleOrDecoratedPortionPrimitive2D(
             // tdf#152990 set the font after the MapMode is (potentially) set so canvas uses the desired
             // font size
             mpOutputDevice->SetFont(aFont);
+            mpOutputDevice->SetTextColor(Color(aRGBFontColor));
 
             if (!aDXArray.empty())
             {
