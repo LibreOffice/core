7ea2bf1c78bc139141c4d8726b1a2345d29fb361
Miklos Vajna <vmiklos@collabora.com>
2024-04-25 08:40:35 +0200

    CppunitTest_sw_writerfilter_rtftok: turn on set_non_application_font_use
    Similar to commit dc4d7500c9d283e26d1553ce11366a217cf1f69d (Fix
    CppunitTest_sd_import_tests-smartart non_application_font_use,
    2023-10-23), got rid of a case where the font name was not explicit.
    
    Change-Id: I06963f97d90e5a6c9efa0490890b30770b31dba8
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/166611
    Tested-by: Jenkins
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

--- sw/CppunitTest_sw_writerfilter_rtftok.mk
+++ sw/CppunitTest_sw_writerfilter_rtftok.mk
@@ -58,4 +58,8 @@ $(eval $(call gb_CppunitTest_use_configuration,sw_writerfilter_rtftok))
 # by a link relation
 $(call gb_CppunitTest_get_target,sw_writerfilter_rtftok) : $(call gb_Library_get_target,sw_writerfilter)
 
+ifneq ($(filter MORE_FONTS,$(BUILD_TYPE)),)
+$(eval $(call gb_CppunitTest_set_non_application_font_use,sw_writerfilter_rtftok,abort))
+endif
+
 # vim: set noet sw=4 ts=4:
