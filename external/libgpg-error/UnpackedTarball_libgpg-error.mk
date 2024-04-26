ae74c335cdc78a383aa6c26b15cc13fe29499009
Xisco Fauli <xiscofauli@libreoffice.org>
2024-04-26 09:50:09 +0200

    libgpg-error: upgrade to 1.55
    Downloaded from https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.55.tar.bz2
    
    Change-Id: Ia7592c071caedcfb4fb75b9516f44932000f2567
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/184611
    Tested-by: Jenkins
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/184967
    Reviewed-by: Andras Timar <andras.timar@collabora.com>
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

--- external/libgpg-error/UnpackedTarball_libgpg-error.mk
+++ external/libgpg-error/UnpackedTarball_libgpg-error.mk
@@ -17,10 +17,12 @@ $(eval $(call gb_UnpackedTarball_add_patches,libgpg-error, \
 	$(if $(filter MSC,$(COM)),external/libgpg-error/w32-build-fixes.patch) \
 	$(if $(filter MSC,$(COM)),external/libgpg-error/w32-build-fixes-2.patch.1) \
 	$(if $(filter MSC,$(COM)),external/libgpg-error/w32-disable-dllinit.patch.1) \
-	external/libgpg-error/w32-build-fixes-4.patch \
+	$(if $(filter MSC,$(COM)),external/libgpg-error/w32-build-fixes-4.patch) \
 	$(if $(filter MSC,$(COM)),external/libgpg-error/w32-build-fixes-5.patch) \
+	$(if $(filter MSC,$(COM)),external/libgpg-error/w32-build-fixes-6.patch) \
 	$(if $(filter LINUX,$(OS)),external/libgpg-error/libgpgerror-bundled-soname.patch.1) \
 	external/libgpg-error/clang-cl.patch \
+	external/libgpg-error/undeclared-environ-macOS.patch \
 ))
 
 # vim: set noet sw=4 ts=4:
