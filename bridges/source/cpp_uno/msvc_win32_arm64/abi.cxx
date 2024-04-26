3af9a33aadd29d6c7d76cd09a50ec2b478b8bc63
Stephan Bergmann <stephan.bergmann@allotropia.de>
2024-04-26 10:17:36 +0200

    Drop unused RETURN_KIND_HFA_FLOAT/DOUBLE from msvc_win32_arm64 UNO bridge
    Change-Id: I8c6fbed8c587affda69285c203a3a93fa2e2e603
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/166699
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <stephan.bergmann@allotropia.de>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/174224
    Tested-by: Balazs Varga <balazs.varga.extern@allotropia.de>
    Reviewed-by: Balazs Varga <balazs.varga.extern@allotropia.de>

--- bridges/source/cpp_uno/msvc_win32_arm64/abi.cxx
+++ bridges/source/cpp_uno/msvc_win32_arm64/abi.cxx
@@ -18,95 +18,10 @@
  */
 
 #include <sal/config.h>
-#include <sal/types.h>
 
 #include <cassert>
 
 #include "abi.hxx"
-
-enum StructKind
-{
-    STRUCT_KIND_EMPTY,
-    STRUCT_KIND_FLOAT,
-    STRUCT_KIND_DOUBLE,
-    STRUCT_KIND_POD,
-    STRUCT_KIND_DTOR
-};
-
-static StructKind getStructKind(typelib_CompoundTypeDescription const* type)
-{
-    StructKind k = type->pBaseTypeDescription == 0 ? STRUCT_KIND_EMPTY
-                                                   : getStructKind(type->pBaseTypeDescription);
-
-    for (sal_Int32 i = 0; i != type->nMembers; ++i)
-    {
-        StructKind k2 = StructKind();
-        switch (type->ppTypeRefs[i]->eTypeClass)
-        {
-            case typelib_TypeClass_BOOLEAN:
-            case typelib_TypeClass_BYTE:
-            case typelib_TypeClass_SHORT:
-            case typelib_TypeClass_UNSIGNED_SHORT:
-            case typelib_TypeClass_LONG:
-            case typelib_TypeClass_UNSIGNED_LONG:
-            case typelib_TypeClass_HYPER:
-            case typelib_TypeClass_UNSIGNED_HYPER:
-            case typelib_TypeClass_CHAR:
-            case typelib_TypeClass_ENUM:
-                k2 = STRUCT_KIND_POD;
-                break;
-            case typelib_TypeClass_FLOAT:
-                k2 = STRUCT_KIND_FLOAT;
-                break;
-            case typelib_TypeClass_DOUBLE:
-                k2 = STRUCT_KIND_DOUBLE;
-                break;
-            case typelib_TypeClass_STRING:
-            case typelib_TypeClass_TYPE:
-            case typelib_TypeClass_ANY:
-            case typelib_TypeClass_SEQUENCE:
-            case typelib_TypeClass_INTERFACE:
-                k2 = STRUCT_KIND_DTOR;
-                break;
-            case typelib_TypeClass_STRUCT:
-            {
-                typelib_TypeDescription* td = 0;
-                TYPELIB_DANGER_GET(&td, type->ppTypeRefs[i]);
-                k2 = getStructKind(reinterpret_cast<typelib_CompoundTypeDescription const*>(td));
-                TYPELIB_DANGER_RELEASE(td);
-                break;
-            }
-            default:
-                assert(false);
-        }
-        switch (k2)
-        {
-            case STRUCT_KIND_EMPTY:
-                // this means an empty sub-object, which nevertheless obtains a byte
-                // of storage (TODO: does it?), so the full object cannot be a
-                // homogeneous collection of float or double
-            case STRUCT_KIND_POD:
-                assert(k != STRUCT_KIND_DTOR);
-                k = STRUCT_KIND_POD;
-                break;
-            case STRUCT_KIND_FLOAT:
-            case STRUCT_KIND_DOUBLE:
-                if (k == STRUCT_KIND_EMPTY)
-                {
-                    k = k2;
-                }
-                else if (k != k2)
-                {
-                    assert(k != STRUCT_KIND_DTOR);
-                    k = STRUCT_KIND_POD;
-                }
-                break;
-            case STRUCT_KIND_DTOR:
-                return STRUCT_KIND_DTOR;
-        }
-    }
-    return k;
-}
 
 ReturnKind getReturnKind(typelib_TypeDescription const* type)
 {
@@ -134,24 +49,9 @@ ReturnKind getReturnKind(typelib_TypeDescription const* type)
         case typelib_TypeClass_TYPE:
         case typelib_TypeClass_ANY:
         case typelib_TypeClass_SEQUENCE:
+        case typelib_TypeClass_STRUCT:
         case typelib_TypeClass_INTERFACE:
             return RETURN_KIND_INDIRECT;
-        case typelib_TypeClass_STRUCT:
-            if (type->nSize > 16)
-            {
-                return RETURN_KIND_INDIRECT;
-            }
-            switch (getStructKind(reinterpret_cast<typelib_CompoundTypeDescription const*>(type)))
-            {
-                case STRUCT_KIND_FLOAT:
-                    return RETURN_KIND_INDIRECT;
-                case STRUCT_KIND_DOUBLE:
-                    return RETURN_KIND_INDIRECT;
-                case STRUCT_KIND_DTOR:
-                    return RETURN_KIND_INDIRECT;
-                default:
-                    return RETURN_KIND_INDIRECT;
-            }
     }
 }
 
