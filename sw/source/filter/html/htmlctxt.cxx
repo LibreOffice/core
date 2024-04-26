LibreOffice/core

commit db115bec9254417ef7a3faf687478fe5424ab378

tdf#78510 sw,cui: split SvxLRSpaceItem for SwTextNode, SwTextFormatColl
Leave editengine and non-paragraph usages of SvxLRSpaceItem as-is for now.

Add new items RES_MARGIN_LEFT etc., order them so that paragraphs can
have 3 consecutive items RES_MARGIN_FIRSTLINE..RES_MARGIN_RIGHT and
non-paragraphs also have 2-4 consecutive items
RES_MARGIN_RIGHT..RES_MARGIN_LEFT (only the 3 paragraph ones are
actually used now).

The HTML import filter is particularly annoying because it parses CSS
stuff into SfxItemSets without knowing where the items will be applied,
so it can't know whether to create SvxLeftMarginItem or
SvxTextLeftMarginItem... the split items are created in ParseCSS1_*
functions and then converted later if necessary.

WW8 import has some weird code as well, SwWW8ImplReader::Read_LR()
creates 3 items and then something wants to set every item on its own
so SwWW8FltControlStack::SetAttrInDoc() turned out rather weird.

Convert the paragraph dialog to handle the split items (by mapping them
to SID_ATTR_PARA_FIRSTLINESPACE/SID_ATTR_PARA_LEFTSPACE/
SID_ATTR_PARA_RIGHTSPACE), but the SvxRuler looks a bit more confusing
so convert in sw shells for now and leave that for later (also unclear if
changing these slot items like SID_ATTR_PARA_LRSPACE breaks any ABIs?).

Change-Id: I40431821868fd3e1cceba121b5539ff9ae6befbc
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/147024
Tested-by: Michael Stahl <michael.stahl@allotropia.de>
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/filter/html/htmlctxt.cxx
@@ -565,18 +565,36 @@ void SwHTMLParser::InsertAttrs( SfxItemSet &rItemSet,
 
     SfxItemIter aIter( rItemSet );
 
+    const SvxFirstLineIndentItem * pFirstLineItem(nullptr);
+    const SvxTextLeftMarginItem * pTextLeftMargin(nullptr);
+    const SvxRightMarginItem * pRightMargin(nullptr);
+
     for (const SfxPoolItem* pItem = aIter.GetCurItem(); pItem; pItem = aIter.NextItem())
     {
-        HTMLAttr **ppAttr = nullptr;
-
         switch( pItem->Which() )
         {
-        case RES_LR_SPACE:
+        case RES_MARGIN_FIRSTLINE:
+            {
+                pFirstLineItem = static_cast<const SvxFirstLineIndentItem*>(pItem);
+            }
+            break;
+        case RES_MARGIN_TEXTLEFT:
+            {
+                pTextLeftMargin = static_cast<const SvxTextLeftMarginItem*>(pItem);
+            }
+            break;
+        case RES_MARGIN_RIGHT:
+            {
+                pRightMargin = static_cast<const SvxRightMarginItem*>(pItem);
+            }
+            break;
+        }
+    }
+
+#if 1
             {
                 // Paragraph indents need to be added and are generated for each paragraphs
                 // (here for the first paragraph only, all the following in SetTextCollAttrs)
-                const SvxLRSpaceItem *pLRItem =
-                    static_cast<const SvxLRSpaceItem *>(pItem);
 
                 // Get old paragraph indents without the top context (that's the one we're editing)
                 sal_uInt16 nOldLeft = 0, nOldRight = 0;
@@ -598,7 +616,8 @@ void SwHTMLParser::InsertAttrs( SfxItemSet &rItemSet,
                 if( rPropInfo.m_bLeftMargin )
                 {
                     OSL_ENSURE( rPropInfo.m_nLeftMargin < 0 ||
-                            rPropInfo.m_nLeftMargin == pLRItem->GetTextLeft(),
+                            !pTextLeftMargin ||
+                            rPropInfo.m_nLeftMargin == pTextLeftMargin->GetTextLeft(),
                             "left margin does not match with item" );
                     if( rPropInfo.m_nLeftMargin < 0 &&
                         -rPropInfo.m_nLeftMargin > nOldLeft )
@@ -609,7 +628,8 @@ void SwHTMLParser::InsertAttrs( SfxItemSet &rItemSet,
                 if( rPropInfo.m_bRightMargin )
                 {
                     OSL_ENSURE( rPropInfo.m_nRightMargin < 0 ||
-                            rPropInfo.m_nRightMargin == pLRItem->GetRight(),
+                            !pRightMargin ||
+                            rPropInfo.m_nRightMargin == pRightMargin->GetRight(),
                             "right margin does not match with item" );
                     if( rPropInfo.m_nRightMargin < 0 &&
                         -rPropInfo.m_nRightMargin > nOldRight )
@@ -617,21 +637,31 @@ void SwHTMLParser::InsertAttrs( SfxItemSet &rItemSet,
                     else
                         nRight = nOldRight + static_cast< sal_uInt16 >(rPropInfo.m_nRightMargin);
                 }
-                if( rPropInfo.m_bTextIndent )
-                    nIndent = pLRItem->GetTextFirstLineOffset();
+                if (rPropInfo.m_bTextIndent && pFirstLineItem)
+                    nIndent = pFirstLineItem->GetTextFirstLineOffset();
 
                 // Remember the value for the following paragraphs
                 pContext->SetMargins( nLeft, nRight, nIndent );
 
                 // Set the attribute on the current paragraph
-                SvxLRSpaceItem aLRItem( *pLRItem );
-                aLRItem.SetTextFirstLineOffset( nIndent );
-                aLRItem.SetTextLeft( nLeft );
-                aLRItem.SetRight( nRight );
-                NewAttr(m_xAttrTab, &m_xAttrTab->pLRSpace, aLRItem);
-                EndAttr( m_xAttrTab->pLRSpace, false );
+                SvxFirstLineIndentItem const firstLine(nIndent, RES_MARGIN_FIRSTLINE);
+                NewAttr(m_xAttrTab, &m_xAttrTab->pFirstLineIndent, firstLine);
+                EndAttr(m_xAttrTab->pFirstLineIndent, false);
+                SvxTextLeftMarginItem const leftMargin(nLeft, RES_MARGIN_TEXTLEFT);
+                NewAttr(m_xAttrTab, &m_xAttrTab->pTextLeftMargin, leftMargin);
+                EndAttr(m_xAttrTab->pTextLeftMargin, false);
+                SvxRightMarginItem const rightMargin(nRight, RES_MARGIN_RIGHT);
+                NewAttr(m_xAttrTab, &m_xAttrTab->pRightMargin, rightMargin);
+                EndAttr(m_xAttrTab->pRightMargin, false);
             }
-            break;
+#endif
+
+    for (const SfxPoolItem* pItem = aIter.GetCurItem(); pItem; pItem = aIter.NextItem())
+    {
+        HTMLAttr **ppAttr = nullptr;
+
+        switch( pItem->Which() )
+        {
 
         case RES_UL_SPACE:
             if( !rPropInfo.m_bTopMargin || !rPropInfo.m_bBottomMargin )

commit ac05822b8957d80018d3ce1cbf97cd9faa0dfe99

tdf#154581 tdf#157411 tdf#158549 sw: HTML import: fix SfxItemIter reuse
This is obviously wrong, as ALG just pointed out: the aIter is at the
end after the 1st loop, so the 2nd loop does nothing.
Thx Gabor for checking all the reported bugs.

(regression from commit db115bec9254417ef7a3faf687478fe5424ab378)

Change-Id: Iae37b9ab5c630d0ee774f42c84d5e76349b92a90
Tested-by: Gabor Kelemen <gabor.kelemen.extern@allotropia.de>
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/166643
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
Tested-by: Jenkins

sw/source/filter/html/htmlctxt.cxx
@@ -656,7 +656,8 @@ void SwHTMLParser::InsertAttrs( SfxItemSet &rItemSet,
             }
 #endif
 
-    for (const SfxPoolItem* pItem = aIter.GetCurItem(); pItem; pItem = aIter.NextItem())
+    SfxItemIter aIter2(rItemSet);
+    for (const SfxPoolItem* pItem = aIter2.GetCurItem(); pItem; pItem = aIter2.NextItem())
     {
         HTMLAttr **ppAttr = nullptr;
 