LibreOffice/core

commit c804c5354855188b5a37219cfe11dc079dc235f4

sw content control: fix lost properties on copy&paste
E.g. a checkbox content control was turned into a rich text one on
copy&paste, so toggling didn't work on the copied content control.

Change-Id: Ia7262e0b88dff12a2f87acd7b0e6cc3fbde3c3a8
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/148613
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

sw/qa/core/txtnode/txtnode.cxx
@@ -30,6 +30,7 @@
 #include <txatbase.hxx>
 #include <ndtxt.hxx>
 #include <textcontentcontrol.hxx>
+#include <swdtflvr.hxx>
 
 /// Covers sw/source/core/txtnode/ fixes.
 class SwCoreTxtnodeTest : public SwModelTestBase
@@ -326,6 +327,36 @@ CPPUNIT_TEST_FIXTURE(SwCoreTxtnodeTest, testDateContentControlKeyboard)
     // Without the accompanying fix in place, this test would have failed, the date popup was
     // mouse-only.
     CPPUNIT_ASSERT(bShouldOpen);
+}
+
+CPPUNIT_TEST_FIXTURE(SwCoreTxtnodeTest, testContentControlCopy)
+{
+    // Given a document with a content control:
+    createSwDoc();
+    SwDoc* pDoc = getSwDoc();
+    SwWrtShell* pWrtShell = pDoc->GetDocShell()->GetWrtShell();
+    pWrtShell->InsertContentControl(SwContentControlType::CHECKBOX);
+
+    // When copying that content control:
+    pWrtShell->SelAll();
+    rtl::Reference<SwTransferable> xTransfer = new SwTransferable(*pWrtShell);
+    xTransfer->Copy();
+    // Kill the selection, go to the end of the document:
+    pWrtShell->EndOfSection();
+    TransferableDataHelper aHelper(xTransfer);
+    SwTransferable::Paste(*pWrtShell, aHelper);
+
+    // Then make sure that the copy is also a checkbox:
+    SwContentControlManager& rManager = pDoc->GetContentControlManager();
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(2), rManager.GetCount());
+    const SwFormatContentControl& rFormat1 = rManager.Get(0)->GetContentControl();
+    CPPUNIT_ASSERT_EQUAL(SwContentControlType::CHECKBOX, rFormat1.GetContentControl()->GetType());
+    const SwFormatContentControl& rFormat2 = rManager.Get(1)->GetContentControl();
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 1 (CHECKBOX)
+    // - Actual  : 0 (RICH_TEXT)
+    // i.e. properties were not copied from the source to the destination content control.
+    CPPUNIT_ASSERT_EQUAL(SwContentControlType::CHECKBOX, rFormat2.GetContentControl()->GetType());
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();

sw/source/core/txtnode/attrcontentcontrol.cxx
@@ -183,6 +183,42 @@ SwContentControl::SwContentControl(SwFormatContentControl* pFormat)
     , m_pFormat(pFormat)
     , m_pTextNode(nullptr)
 {
+    if (!pFormat)
+    {
+        return;
+    }
+
+    const std::shared_ptr<SwContentControl>& pOther = pFormat->GetContentControl();
+    if (!pOther)
+    {
+        return;
+    }
+
+    SetShowingPlaceHolder(pOther->m_bShowingPlaceHolder);
+    SetCheckbox(pOther->m_bCheckbox);
+    SetChecked(pOther->m_bChecked);
+    SetCheckedState(pOther->m_aCheckedState);
+    SetUncheckedState(pOther->m_aUncheckedState);
+    SetListItems(pOther->m_aListItems);
+    SetPicture(pOther->m_bPicture);
+    SetDate(pOther->m_bDate);
+    SetDateFormat(pOther->m_aDateFormat);
+    SetDateLanguage(pOther->m_aDateLanguage);
+    SetCurrentDate(pOther->m_aCurrentDate);
+    SetPlainText(pOther->m_bPlainText);
+    SetComboBox(pOther->m_bComboBox);
+    SetDropDown(pOther->m_bDropDown);
+    SetPlaceholderDocPart(pOther->m_aPlaceholderDocPart);
+    SetDataBindingPrefixMappings(pOther->m_aDataBindingPrefixMappings);
+    SetDataBindingXpath(pOther->m_aDataBindingXpath);
+    SetDataBindingStoreItemID(pOther->m_aDataBindingStoreItemID);
+    SetColor(pOther->m_aColor);
+    SetAppearance(pOther->m_aAppearance);
+    SetAlias(pOther->m_aAlias);
+    SetTag(pOther->m_aTag);
+    SetId(pOther->m_nId);
+    SetTabIndex(pOther->m_nTabIndex);
+    SetLock(pOther->m_aLock);
 }
 
 SwContentControl::~SwContentControl() {}

commit 06aeb9c61d50bba7edafe17f9d3513af26b0782f

tdf#159683 sw content controls, plain text: fix crash with the clipboard doc
Regression from commit c804c5354855188b5a37219cfe11dc079dc235f4 (sw
content control: fix lost properties on copy&paste, 2023-03-10), select
a plain text content control, copy it to the clipboard, quit: assertion
fails during shutdown because the doc's "placeholder text" char style is
still referenced by a client.

What happens here is that the SwContentControl copy ctor copies the
plain text flag, and that flag is only read in SwTextNode::InsertHint(),
so that causes the problem. Note how that code is inconsistent: we avoid
the creation of dummy characters in the copy case, but we still try to
adjust the start/end of the content control attribute in the copy case,
which makes not much sense.

Fix the problem by not adjusting the content control attribute
boundaries in the copy case, since the original intention was to do
thees corrections only at a UI level, during interactive edit.

It's not clear why this inconsistency had an influence on the clients of
the char style, though.

Change-Id: I86b0516464f24fc453dcd97588dafb8afd010a9e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/166882
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

sw/source/core/txtnode/thints.cxx
@@ -1698,7 +1698,9 @@ bool SwTextNode::InsertHint( SwTextAttr * const pAttr, const SetAttrMode nMode )
         // for all of its content.
         auto* pTextContentControl = static_txtattr_cast<SwTextContentControl*>(
             GetTextAttrAt(pAttr->GetStart(), RES_TXTATR_CONTENTCONTROL, ::sw::GetTextAttrMode::Parent));
-        if (pTextContentControl)
+        // If the caller is SwTextNode::CopyText, we just copy an existing attribute, no need to
+        // correct it.
+        if (pTextContentControl && !(nMode & SetAttrMode::NOTXTATRCHR))
         {
             auto& rFormatContentControl
                 = static_cast<SwFormatContentControl&>(pTextContentControl->GetAttr());

sw/qa/core/txtnode/txtnode.cxx
@@ -539,6 +539,29 @@ CPPUNIT_TEST_FIXTURE(SwCoreTxtnodeTest, testSplitFlyAnchorSplit)
     CPPUNIT_ASSERT_EQUAL(OUString("PortionType::Fly"), aPortionType);
 }
 
+CPPUNIT_TEST_FIXTURE(SwCoreTxtnodeTest, testPlainContentControlCopy)
+{
+    // Given a document with a plain text content control, all text selected and copied to the
+    // clipboard:
+    createSwDoc("plain-content-control-copy.docx");
+    SwDocShell* pDocShell = getSwDocShell();
+    SwWrtShell* pWrtShell = pDocShell->GetWrtShell();
+    pWrtShell->SelAll();
+    {
+        rtl::Reference<SwTransferable> xTransfer = new SwTransferable(*pWrtShell);
+        xTransfer->Copy();
+    }
+
+    // When closing that document, then make sure we don't crash on shutdown:
+    uno::Reference<frame::XModel> xModel(mxComponent, uno::UNO_QUERY);
+    uno::Reference<util::XCloseable> xFrame(xModel->getCurrentController()->getFrame(),
+                                            uno::UNO_QUERY);
+    // Without the accompanying fix in place, this resulted in an assertion failure, a char style
+    // still had clients by the time it was deleted.
+    xFrame->close(false);
+    mxComponent.clear();
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */