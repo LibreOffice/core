61c704e99bb5f600095066383cd66befab62d3f1
Stephan Bergmann <stephan.bergmann@allotropia.de>
2024-05-02 10:40:35 +0200

    Simplify condition
    ...after this got wrapped in an outer
    
    > ifeq ($(OS),EMSCRIPTEN)
    
    in cf0b0f0dd04fae98b686cd5768673c217a58fab6 "Emscripten: Only add the --pre-js
    code to the soffice executable"
    
    (cherry-picked from 9d84957863d274501fe130319d972a3fc2572503)
    
    Change-Id: Ic2b9ae7709a06146a206ebaa2ee8881387dae6b1
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/166998
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <stephan.bergmann@allotropia.de>

--- desktop/Executable_soffice_bin.mk
+++ desktop/Executable_soffice_bin.mk
@@ -33,7 +33,7 @@ $(call gb_LinkTarget_get_target,$(call gb_Executable_get_linktarget,soffice_bin)
 # don't sort; later can override previous settings!
 $(eval $(call gb_Executable_add_prejs,soffice_bin,$(SRCDIR)/static/emscripten/environment.js))
 $(eval $(call gb_Executable_add_prejs,soffice_bin,$(call gb_CustomTarget_get_workdir,static/emscripten_fs_image)/soffice.data.js.link))
-ifeq ($(OS)-$(ENABLE_QT5),EMSCRIPTEN-TRUE)
+ifeq ($(ENABLE_QT5),TRUE)
 $(eval $(call gb_Executable_add_prejs,soffice_bin,$(SRCDIR)/static/emscripten/soffice_args.js))
 endif
 endif
