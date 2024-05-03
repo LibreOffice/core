62b08851dd7866c830124b30d5722b9541cb2d93
Julien Nabet <serval2412@yahoo.fr>
2024-05-03 10:44:03 +0200

    Related tdf#150082: remove duplicate gssapi_client/gssapi_errmsg
    Change-Id: I38be66163320c1408f5bbb846727994c92e3c246
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/167036
    Reviewed-by: Julien Nabet <serval2412@yahoo.fr>
    Tested-by: Julien Nabet <serval2412@yahoo.fr>

--- external/mariadb-connector-c/StaticLibrary_mariadb-connector-c.mk
+++ external/mariadb-connector-c/StaticLibrary_mariadb-connector-c.mk
@@ -70,10 +70,6 @@ $(eval $(call gb_StaticLibrary_add_generated_cobjects,mariadb-connector-c,\
 	UnpackedTarball/mariadb-connector-c/libmariadb/ma_client_plugin \
 	UnpackedTarball/mariadb-connector-c/plugins/auth/my_auth \
 	UnpackedTarball/mariadb-connector-c/plugins/auth/caching_sha2_pw \
-	$(ifneq $(filter $(OS),WNT), \
-		UnpackedTarball/mariadb-connector-c/plugins/auth/gssapi_client \
-		UnpackedTarball/mariadb-connector-c/plugins/auth/gssapi_errmsg \
-	) \
 	UnpackedTarball/mariadb-connector-c/plugins/pvio/pvio_socket \
 	$(if $(filter $(OS),WNT), \
 		UnpackedTarball/mariadb-connector-c/libmariadb/win32_errmsg \
