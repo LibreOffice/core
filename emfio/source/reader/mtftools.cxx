84811847cf104a236ad505b2ce262e0b30bbaca3
Caolán McNamara <caolan.mcnamara@collabora.com>
2024-05-04 15:24:00 +0100

    ofz#68612 Integer-overflow
    Change-Id: Ie68a11300f7d2c3667a1d5702cad8e78e6a6c35a
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/167130
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Tested-by: Jenkins

--- emfio/source/reader/mtftools.cxx
+++ emfio/source/reader/mtftools.cxx
@@ -1753,8 +1753,8 @@ namespace emfio
             // check whether there is a font rotation applied via transformation
             Point aP1( ImplMap( Point() ) );
             Point aP2( ImplMap( Point( 0, 100 ) ) );
-            aP2.AdjustX( -(aP1.X()) );
-            aP2.AdjustY( -(aP1.Y()) );
+            aP2.setX(o3tl::saturating_sub(aP2.X(), aP1.X()));
+            aP2.setY(o3tl::saturating_sub(aP2.Y(), aP1.Y()));
             double fX = aP2.X();
             double fY = aP2.Y();
             if ( fX )
