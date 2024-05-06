LibreOffice/core

commit 5c04c4474236cc50009aea6d89f7c443c861af19

tdf#40142 - Consider level settings of the various text nodes
Consider level settings of the various text nodes when creating a TOC
from custom styles.

Change-Id: I70759f9bcf63e8591ce89bc71afabb1da0c2f29c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/148886
Tested-by: Jenkins
Reviewed-by: Andreas Heinisch <andreas.heinisch@yahoo.de>

sw/source/core/doc/doctxm.cxx
@@ -1373,6 +1373,8 @@ void SwTOXBaseSection::UpdateTemplate(const SwTextNode* pOwnChapterNode,
                 if (pTextNd->GetText().getLength() &&
                     pTextNd->getLayoutFrame(pLayout) &&
                     pTextNd->GetNodes().IsDocNodes() &&
+                    // tdf#40142 - consider level settings of the various text nodes
+                    o3tl::make_unsigned(pTextNd->GetAttrOutlineLevel()) <= GetLevel() &&
                     (!pLayout || !pLayout->HasMergedParas()
                         || static_cast<SwTextFrame*>(pTextNd->getLayoutFrame(pLayout))->GetTextNodeForParaProps() == pTextNd) &&
                     (!IsFromChapter() || IsHeadingContained(pOwnChapterNode, *pTextNd)))

sw/qa/extras/uiwriter/uiwriter8.cxx
@@ -2484,13 +2484,25 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest8, testTdf97899)
     CPPUNIT_ASSERT(!sCharStyleName.isEmpty());
 }
 
+CPPUNIT_TEST_FIXTURE(SwUiWriterTest8, testTdf40142)
+{
+    createSwDoc("tdf40142.odt");
+    dispatchCommand(mxComponent, ".uno:UpdateAllIndexes", {});
+
+    xmlDocUniquePtr pLayout = parseLayoutDump();
+    // Without the fix in place, this test would have failed with
+    // - Expected: 2
+    // - Actual  : 4
+    assertXPath(pLayout, "/root/page[1]/body/section[2]/txt", 2);
+}
+
 CPPUNIT_TEST_FIXTURE(SwUiWriterTest8, testTdf151462)
 {
     createSwDoc("tdf151462.odt");
     dispatchCommand(mxComponent, ".uno:UpdateAllIndexes", {});
 
     xmlDocUniquePtr pLayout = parseLayoutDump();
-    // tdf#151462 - without the fix in place, there would be just the first index entry
+    // Without the fix in place, there would be just the first index entry
     assertXPath(pLayout,
                 "/root/page[1]/body/txt[2]/anchored/fly/section/txt[1]/SwParaPortion/"
                 "SwLineLayout[1]/SwLinePortion[1]",
@@ -2504,7 +2516,7 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest8, testTdf151462)
                 "SwLineLayout[1]/SwLinePortion[1]",
                 "portion", "sub three");
 
-    // tdf#151462 - without the fix in place, there would be just the first index entry
+    // Without the fix in place, there would be just the first index entry
     assertXPath(pLayout,
                 "/root/page[1]/body/txt[6]/anchored/fly/section/txt[1]/SwParaPortion/"
                 "SwLineLayout[1]/SwLinePortion[1]",

commit 7178d6197b892e456a8db8a3d085a24bfd52cf32

tdf#40142 sw: only check level for table of contents
Only Table of Contents has level settings, the level is always 0 for the
other ToX types, which erroneously excludes nodes (as can be seen with
testFDO77715 failing with upcoming tdf#160402 bugfix).

(regression from commit 5c04c4474236cc50009aea6d89f7c443c861af19)

Change-Id: Iec1c2b8eaba0ad20b2ad7d8c6b20603315a7a83d
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/167096
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/core/doc/doctxm.cxx
@@ -1377,7 +1377,8 @@ void SwTOXBaseSection::UpdateTemplate(const SwTextNode* pOwnChapterNode,
                     pTextNd->getLayoutFrame(pLayout) &&
                     pTextNd->GetNodes().IsDocNodes() &&
                     // tdf#40142 - consider level settings of the various text nodes
-                    o3tl::make_unsigned(pTextNd->GetAttrOutlineLevel()) <= GetLevel() &&
+                    (TOX_CONTENT != SwTOXBase::GetType() ||
+                     o3tl::make_unsigned(pTextNd->GetAttrOutlineLevel()) <= GetLevel()) &&
                     (!pLayout || !pLayout->HasMergedParas()
                         || static_cast<SwTextFrame*>(pTextNd->getLayoutFrame(pLayout))->GetTextNodeForParaProps() == pTextNd) &&
                     (!IsFromChapter() || IsHeadingContained(pOwnChapterNode, *pTextNd)))