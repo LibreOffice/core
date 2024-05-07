606623d0fc5321754d9a054881ae9cac94cd9f04
Xisco Fauli <xiscofauli@libreoffice.org>
2024-05-07 14:07:14 +0200

    lxml: upgrade to 5.2.1
    * 0001-Make-regexp-string-raw-to-correct-its-escape-sequenc.patch.1 got
      obsoleted with https://github.com/lxml/lxml/commit/9686dd9c7670d18acff6360c8444520273d5f1b2
    
    * Wincompatible-function-pointer-types.patch and
    Wincompatible-pointer-types.patch got obsoleted with
    https://github.com/lxml/lxml/commit/6bdf4c321b92e002123b4c162c75524dbeb4d54d
    
    Change-Id: I8f0dcba3bd64a1766a2292b687c8959c739ced6d
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/167278
    Tested-by: Jenkins
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- external/lxml/UnpackedTarball_lxml.mk
+++ external/lxml/UnpackedTarball_lxml.mk
@@ -14,10 +14,7 @@ $(eval $(call gb_UnpackedTarball_set_tarball,lxml,$(LXML_TARBALL)))
 $(eval $(call gb_UnpackedTarball_set_patchlevel,lxml,0))
 
 $(eval $(call gb_UnpackedTarball_add_patches,lxml, \
-	external/lxml/0001-Make-regexp-string-raw-to-correct-its-escape-sequenc.patch.1 \
 	external/lxml/replace-setuptools-with-distutils.patch.1 \
-	external/lxml/Wincompatible-function-pointer-types.patch \
-	external/lxml/Wincompatible-pointer-types.patch \
 ))
 
 # vim: set noet sw=4 ts=4:
