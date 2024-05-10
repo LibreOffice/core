LibreOffice/core

commit 9fc28d7ef5def30a23960cf15334af1aec27460f

Use officecfg instead of SvtSlideSorterBarOptions class
Change-Id: Ie2965f343e6df31502596c35e1d17c22bbccb142
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/161451
Tested-by: Jenkins
Reviewed-by: Thorsten Behrens <thorsten.behrens@allotropia.de>

sd/source/ui/framework/module/SlideSorterModule.cxx
@@ -19,8 +19,10 @@
 
 #include "SlideSorterModule.hxx"
 
+#include <comphelper/lok.hxx>
 #include <framework/FrameworkHelper.hxx>
 #include <framework/ConfigurationController.hxx>
+#include <officecfg/Office/Impress.hxx>
 #include <DrawController.hxx>
 #include <com/sun/star/drawing/framework/XTabBar.hpp>
 #include <com/sun/star/drawing/framework/TabBarButton.hpp>
@@ -29,7 +31,6 @@
 
 #include <strings.hrc>
 #include <sdresid.hxx>
-#include <svtools/slidesorterbaropt.hxx>
 
 using namespace ::com::sun::star;
 using namespace ::com::sun::star::uno;
@@ -80,17 +81,17 @@ SlideSorterModule::SlideSorterModule (
 
     UpdateViewTabBar(nullptr);
 
-    if (SvtSlideSorterBarOptions().GetVisibleImpressView())
+    if (officecfg::Office::Impress::MultiPaneGUI::SlideSorterBar::Visible::ImpressView::get().has_value() && (!getenv("LO_TESTNAME") || !comphelper::LibreOfficeKit::isActive()))
         AddActiveMainView(FrameworkHelper::msImpressViewURL);
-    if (SvtSlideSorterBarOptions().GetVisibleOutlineView())
+    if (officecfg::Office::Impress::MultiPaneGUI::SlideSorterBar::Visible::OutlineView::get())
         AddActiveMainView(FrameworkHelper::msOutlineViewURL);
-    if (SvtSlideSorterBarOptions().GetVisibleNotesView())
+    if (officecfg::Office::Impress::MultiPaneGUI::SlideSorterBar::Visible::NotesView::get())
         AddActiveMainView(FrameworkHelper::msNotesViewURL);
-    if (SvtSlideSorterBarOptions().GetVisibleHandoutView())
+    if (officecfg::Office::Impress::MultiPaneGUI::SlideSorterBar::Visible::HandoutView::get())
         AddActiveMainView(FrameworkHelper::msHandoutViewURL);
-    if (SvtSlideSorterBarOptions().GetVisibleSlideSorterView())
+    if (officecfg::Office::Impress::MultiPaneGUI::SlideSorterBar::Visible::SlideSorterView::get().has_value() && !comphelper::LibreOfficeKit::isActive())
         AddActiveMainView(FrameworkHelper::msSlideSorterURL);
-    if (SvtSlideSorterBarOptions().GetVisibleDrawView())
+    if (officecfg::Office::Impress::MultiPaneGUI::SlideSorterBar::Visible::DrawView::get())
         AddActiveMainView(FrameworkHelper::msDrawViewURL);
 
     mxConfigurationController->addConfigurationChangeListener(
@@ -105,12 +106,14 @@ SlideSorterModule::~SlideSorterModule()
 
 void SlideSorterModule::SaveResourceState()
 {
-    SvtSlideSorterBarOptions().SetVisibleImpressView(IsResourceActive(FrameworkHelper::msImpressViewURL));
-    SvtSlideSorterBarOptions().SetVisibleOutlineView(IsResourceActive(FrameworkHelper::msOutlineViewURL));
-    SvtSlideSorterBarOptions().SetVisibleNotesView(IsResourceActive(FrameworkHelper::msNotesViewURL));
-    SvtSlideSorterBarOptions().SetVisibleHandoutView(IsResourceActive(FrameworkHelper::msHandoutViewURL));
-    SvtSlideSorterBarOptions().SetVisibleSlideSorterView(IsResourceActive(FrameworkHelper::msSlideSorterURL));
-    SvtSlideSorterBarOptions().SetVisibleDrawView(IsResourceActive(FrameworkHelper::msDrawViewURL));
+    auto xChanges = comphelper::ConfigurationChanges::create();
+    officecfg::Office::Impress::MultiPaneGUI::SlideSorterBar::Visible::ImpressView::set(IsResourceActive(FrameworkHelper::msImpressViewURL),xChanges);
+    officecfg::Office::Impress::MultiPaneGUI::SlideSorterBar::Visible::OutlineView::set(IsResourceActive(FrameworkHelper::msOutlineViewURL),xChanges);
+    officecfg::Office::Impress::MultiPaneGUI::SlideSorterBar::Visible::NotesView::set(IsResourceActive(FrameworkHelper::msNotesViewURL),xChanges);
+    officecfg::Office::Impress::MultiPaneGUI::SlideSorterBar::Visible::HandoutView::set(IsResourceActive(FrameworkHelper::msHandoutViewURL),xChanges);
+    officecfg::Office::Impress::MultiPaneGUI::SlideSorterBar::Visible::SlideSorterView::set(IsResourceActive(FrameworkHelper::msSlideSorterURL),xChanges);
+    officecfg::Office::Impress::MultiPaneGUI::SlideSorterBar::Visible::DrawView::set(IsResourceActive(FrameworkHelper::msDrawViewURL),xChanges);
+    xChanges->commit();
 }
 
 void SAL_CALL SlideSorterModule::notifyConfigurationChange (

sd/source/ui/unoidl/unomodel.cxx
@@ -33,6 +33,7 @@
 #include <com/sun/star/embed/Aspects.hpp>
 
 #include <officecfg/Office/Common.hxx>
+#include <officecfg/Office/Impress.hxx>
 #include <comphelper/indexedpropertyvalues.hxx>
 #include <comphelper/lok.hxx>
 #include <comphelper/propertyvalue.hxx>
@@ -71,7 +72,6 @@
 #include <svx/svdpool.hxx>
 #include <svx/svdpagv.hxx>
 #include <svtools/unoimap.hxx>
-#include <svtools/slidesorterbaropt.hxx>
 #include <svx/unoshape.hxx>
 #include <editeng/unonrule.hxx>
 #include <editeng/eeitem.hxx>
@@ -2631,10 +2631,10 @@ void SdXImpressDocument::initializeForTiledRendering(const css::uno::Sequence<cs
     // format
     auto xChanges = comphelper::ConfigurationChanges::create();
     officecfg::Office::Common::Save::Document::WarnAlienFormat::set(false, xChanges);
-    xChanges->commit();
 
-    if (!getenv("LO_TESTNAME"))
-        SvtSlideSorterBarOptions().SetVisibleImpressView(true);
+    if (!getenv("LO_TESTNAME") || !comphelper::LibreOfficeKit::isActive())
+        officecfg::Office::Impress::MultiPaneGUI::SlideSorterBar::Visible::ImpressView::set(true,xChanges);
+    xChanges->commit();
 }
 
 void SdXImpressDocument::postKeyEvent(int nType, int nCharCode, int nKeyCode)

commit 423d240dc680817efae645d8305402f922bb452d

sd: properly read values of optional config items for SlideSorterBar
Guessing this has regressed with
9fc28d7ef5def30a23960cf15334af1aec27460f

Read the values from std::optional<bool> properly.

The default values are taken from the configuration
officecfg/registry/data/org/openoffice/Office/Impress.xcu

Change-Id: I93d5537a9e06dc6985a2ec375353d9869b92e08c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/167353
Reviewed-by: Sarper Akdemir <sarper@libreoffice.org>
Tested-by: Jenkins

sd/source/ui/framework/module/SlideSorterModule.cxx
@@ -81,17 +81,19 @@ SlideSorterModule::SlideSorterModule (
 
     UpdateViewTabBar(nullptr);
 
-    if (officecfg::Office::Impress::MultiPaneGUI::SlideSorterBar::Visible::ImpressView::get().has_value() && (!getenv("LO_TESTNAME") || !comphelper::LibreOfficeKit::isActive()))
+    if (officecfg::Office::Impress::MultiPaneGUI::SlideSorterBar::Visible::ImpressView::get().value_or(true)
+        && (!getenv("LO_TESTNAME") || !comphelper::LibreOfficeKit::isActive()))
         AddActiveMainView(FrameworkHelper::msImpressViewURL);
-    if (officecfg::Office::Impress::MultiPaneGUI::SlideSorterBar::Visible::OutlineView::get())
+    if (officecfg::Office::Impress::MultiPaneGUI::SlideSorterBar::Visible::OutlineView::get().value_or(true))
         AddActiveMainView(FrameworkHelper::msOutlineViewURL);
-    if (officecfg::Office::Impress::MultiPaneGUI::SlideSorterBar::Visible::NotesView::get())
+    if (officecfg::Office::Impress::MultiPaneGUI::SlideSorterBar::Visible::NotesView::get().value_or(true))
         AddActiveMainView(FrameworkHelper::msNotesViewURL);
-    if (officecfg::Office::Impress::MultiPaneGUI::SlideSorterBar::Visible::HandoutView::get())
+    if (officecfg::Office::Impress::MultiPaneGUI::SlideSorterBar::Visible::HandoutView::get().value_or(false))
         AddActiveMainView(FrameworkHelper::msHandoutViewURL);
-    if (officecfg::Office::Impress::MultiPaneGUI::SlideSorterBar::Visible::SlideSorterView::get().has_value() && !comphelper::LibreOfficeKit::isActive())
+    if (officecfg::Office::Impress::MultiPaneGUI::SlideSorterBar::Visible::SlideSorterView::get().value_or(false)
+        && !comphelper::LibreOfficeKit::isActive())
         AddActiveMainView(FrameworkHelper::msSlideSorterURL);
-    if (officecfg::Office::Impress::MultiPaneGUI::SlideSorterBar::Visible::DrawView::get())
+    if (officecfg::Office::Impress::MultiPaneGUI::SlideSorterBar::Visible::DrawView::get().value_or(true))
         AddActiveMainView(FrameworkHelper::msDrawViewURL);
 
     mxConfigurationController->addConfigurationChangeListener(