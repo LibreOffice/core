7b0d2829fe691e013486578b6e93cf94ceb0cd36
Michael Weghorn <m.weghorn@posteo.de>
2024-05-16 05:26:48 +0200

    android: Port to non-deprecated androidx PreferenceManager
    The `android.preference.PreferenceManager` class was
    deprecated in API level 29 [1].
    
    Port to `androidx.preference.PreferenceManager` from
    the AndroidX Preference library instead.
    
    All that needs to be done for this is to add the new library
    dependency and adjust the import, no further code changes
    needed for this step. (There are uses of more deprecated
    preferences-related classes, though. Those will have to be
    dealt with separately.)
    
    This addresses depreaction warnings like
    
        > Task :compileStrippedUIEditingDebugJavaWithJavac
        /home/michi/development/git/libreoffice-WORKTREE-android/android/source/src/java/org/libreoffice/LibreOfficeMainActivity.java:16: warning: [deprecation] PreferenceManager in android.preference has been deprecated
        import android.preference.PreferenceManager;
                                 ^
    
    [1] https://developer.android.com/reference/android/preference/PreferenceManager
    
    Change-Id: Ied168ffbd0591d69e0797e2a0a5bab067e133d22
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/167716
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>
    Tested-by: Jenkins

--- android/source/src/java/org/libreoffice/LocaleHelper.java
+++ android/source/src/java/org/libreoffice/LocaleHelper.java
@@ -4,8 +4,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.content.res.Resources;
-import android.os.Build;
-import android.preference.PreferenceManager;
+import androidx.preference.PreferenceManager;
 
 import java.util.Locale;
 
