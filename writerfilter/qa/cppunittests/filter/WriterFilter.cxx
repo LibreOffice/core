10b1f037288f107357bd4402ff65528f76544b0c
Miklos Vajna <vmiklos@collabora.com>
2024-05-21 08:15:53 +0200

    tdf#160984 sw continuous endnotes: enable DOCX import
    This was working for DOC already.
    
    For DOCX, this was already enabled once with commit
    f9982c24066d6dd2f938cc20176af0f196bc018f (tdf#58521 DOCX import: enable
    ContinuousEndnotes compat flag, 2021-07-13), but then it was reverted
    later with commit commit eeda1b35a6e87d5349545464da33d997c52f15e3
    (Revert "tdf#58521 DOCX import: enable ContinuousEndnotes compat flag",
    2021-08-10), because of tdf#143456.
    
    Enable it again, now that the section-based layout seems good enough to
    handle larger number of endnotes, e.g. the 48 endnotes from tdf#143456.
    
    (cherry picked from commit 1ae5ea3f78cca11ba18f2dd1a06f875263336a3b)
    
    Change-Id: Id221f31f9208e84db2c358546d4d6ceea991b6b3
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/167924
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

--- writerfilter/qa/cppunittests/filter/WriterFilter.cxx
+++ writerfilter/qa/cppunittests/filter/WriterFilter.cxx
@@ -50,6 +50,32 @@ CPPUNIT_TEST_FIXTURE(Test, testDoNotMirrorRtlDrawObjs)
     // i.e. the shape was on the left margin.
     CPPUNIT_ASSERT_GREATER(nBodyRight, nShapeLeft);
 }
+
+CPPUNIT_TEST_FIXTURE(Test, testInlineEndnoteAndFootnoteDOCX)
+{
+    // Given a DOCX file with an endnote and then a footnote:
+    loadFromFile(u"inline-endnote-and-footnote.docx");
+
+    // When laying out that document:
+    uno::Reference<frame::XModel> xModel(mxComponent, uno::UNO_QUERY);
+    css::uno::Reference<qa::XDumper> xDumper(xModel->getCurrentController(), uno::UNO_QUERY);
+    OString aDump = xDumper->dump("layout").toUtf8();
+    auto pCharBuffer = reinterpret_cast<const xmlChar*>(aDump.getStr());
+    xmlDocUniquePtr pXmlDoc(xmlParseDoc(pCharBuffer));
+
+    // Then make sure the footnote is below the endnote:
+    // Without the accompanying fix in place, this test would have failed with:
+    // - xpath should match exactly 1 node
+    // i.e. the endnote was also in the footnote container, not at the end of the body text.
+    sal_Int32 nEndnoteTop
+        = getXPath(pXmlDoc, "/root/page/body/section/column/ftncont/ftn/infos/bounds"_ostr,
+                   "top"_ostr)
+              .toInt32();
+    sal_Int32 nFootnoteTop
+        = getXPath(pXmlDoc, "/root/page/ftncont/ftn/infos/bounds"_ostr, "top"_ostr).toInt32();
+    // Endnote at the end of body text, footnote at page bottom.
+    CPPUNIT_ASSERT_LESS(nFootnoteTop, nEndnoteTop);
+}
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();
