034b68eb3a015553254c7238b13db8f94514080d
Caolán McNamara <caolan.mcnamara@collabora.com>
2024-05-24 20:43:42 +0100

    ofz#68840 another putcontig8bitYCbCr22tile issue
    Change-Id: I6e119cf1b3bec609f94784eb0c439835875d6112
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/168031
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>

--- external/libtiff/UnpackedTarball_libtiff.mk
+++ external/libtiff/UnpackedTarball_libtiff.mk
@@ -17,6 +17,7 @@ $(eval $(call gb_UnpackedTarball_add_patches,libtiff,\
     external/libtiff/libtiff.linknolibs.patch \
     external/libtiff/0001-ofz-54685-Timeout.patch \
     external/libtiff/ofz65182.patch \
+    external/libtiff/ofz68840.patch \
 ))
 
 # vim: set noet sw=4 ts=4:
