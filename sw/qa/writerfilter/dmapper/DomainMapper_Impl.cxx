LibreOffice/core

commit 19bca24486315cc35f873486e6a2dd18394d0614

tdf#126287: docx import: use defered linebreak
In some cases when we have combo linebreak, endparagraph and
end section at very bottom of page, MS Word does ignores linebreak
to avoid empty page with same section and lonely end paragraph mark.

With defered linebreak we could simulate and ignore such linebreaks
during import.

Change-Id: Ie6ee4c0990ee1c2e853691a76953c83191664e43
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/119709
Tested-by: Jenkins
Reviewed-by: Thorsten Behrens <thorsten.behrens@allotropia.de>

writerfilter/source/dmapper/DomainMapper.cxx
@@ -3225,6 +3225,18 @@ void DomainMapper::lcl_startParagraphGroup()
 
 void DomainMapper::lcl_endParagraphGroup()
 {
+    if (m_pImpl->isBreakDeferred(LINE_BREAK))
+    {
+        if (m_pImpl->GetIsLastParagraphInSection())
+            m_pImpl->clearDeferredBreak(LINE_BREAK);
+
+        while (m_pImpl->isBreakDeferred(LINE_BREAK))
+        {
+            m_pImpl->clearDeferredBreak(LINE_BREAK);
+            m_pImpl->appendTextPortion("\n", m_pImpl->GetTopContext());
+        }
+    }
+
     m_pImpl->PopProperties(CONTEXT_PARAGRAPH);
     if (m_pImpl->hasTableManager())
        m_pImpl->getTableManager().endParagraphGroup();
@@ -3385,6 +3397,13 @@ void DomainMapper::lcl_text(const sal_uInt8 * data_, size_t len)
                 case 0x0e: //column break
                     m_pImpl->deferBreak(COLUMN_BREAK);
                     return;
+                case 0x0a: //line break
+                    if (m_pImpl->GetIsLastParagraphInSection())
+                    {
+                        m_pImpl->deferBreak(LINE_BREAK);
+                        return;
+                    }
+                    break;
                 case 0x07:
                     m_pImpl->getTableManager().text(data_, len);
                     return;
@@ -3417,6 +3436,13 @@ void DomainMapper::lcl_text(const sal_uInt8 * data_, size_t len)
 
         // GetTopContext() is changed by inserted breaks, but we want to keep the current context
         PropertyMapPtr pContext = m_pImpl->GetTopContext();
+
+        while (m_pImpl->isBreakDeferred(LINE_BREAK))
+        {
+            m_pImpl->clearDeferredBreak(LINE_BREAK);
+            m_pImpl->appendTextPortion("\n", pContext);
+        }
+
         if (!m_pImpl->GetFootnoteContext())
         {
             if (m_pImpl->isBreakDeferred(PAGE_BREAK))
@@ -3640,6 +3666,18 @@ void DomainMapper::lcl_utext(const sal_uInt8 * data_, size_t len)
         m_pImpl->m_bHasFtnSep = true;
         return;
     }
+    else if (len == 1 && sText[0] == '\r')
+    {
+        // Clear "last" one linebreak at end of section
+        if (m_pImpl->GetIsLastParagraphInSection() && m_pImpl->isBreakDeferred(LINE_BREAK))
+            m_pImpl->clearDeferredBreak(LINE_BREAK);
+        // And emit all other linebreaks
+        while (m_pImpl->isBreakDeferred(LINE_BREAK))
+        {
+            m_pImpl->clearDeferredBreak(LINE_BREAK);
+            m_pImpl->appendTextPortion("\n", m_pImpl->GetTopContext());
+        }
+    }
     else if (len == 1 && sText[0] == '\t' )
     {
         if ( m_pImpl->m_bCheckFirstFootnoteTab && m_pImpl->IsInFootOrEndnote() )
@@ -3670,6 +3708,12 @@ void DomainMapper::lcl_utext(const sal_uInt8 * data_, size_t len)
 
     try
     {
+        while (m_pImpl->isBreakDeferred(LINE_BREAK))
+        {
+            m_pImpl->clearDeferredBreak(LINE_BREAK);
+            m_pImpl->appendTextPortion("\n", m_pImpl->GetTopContext());
+        }
+
         m_pImpl->getTableManager().utext(data_, len);
 
         if (bNewLine)

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -286,6 +286,7 @@ DomainMapper_Impl::DomainMapper_Impl(
         m_bIsFirstSection( true ),
         m_bIsColumnBreakDeferred( false ),
         m_bIsPageBreakDeferred( false ),
+        m_nLineBreaksDeferred( 0 ),
         m_bSdtEndDeferred(false),
         m_bParaSdtEndDeferred(false),
         m_bStartTOC(false),
@@ -1114,9 +1115,12 @@ void DomainMapper_Impl::deferBreak( BreakType deferredBreakType)
 {
     switch (deferredBreakType)
     {
-    case COLUMN_BREAK:
-            m_bIsColumnBreakDeferred = true;
+    case LINE_BREAK:
+        m_nLineBreaksDeferred++;
         break;
+    case COLUMN_BREAK:
+        m_bIsColumnBreakDeferred = true;
+    break;
     case PAGE_BREAK:
             // See SwWW8ImplReader::HandlePageBreakChar(), page break should be
             // ignored inside tables.
@@ -1134,6 +1138,8 @@ bool DomainMapper_Impl::isBreakDeferred( BreakType deferredBreakType )
 {
     switch (deferredBreakType)
     {
+    case LINE_BREAK:
+        return m_nLineBreaksDeferred > 0;
     case COLUMN_BREAK:
         return m_bIsColumnBreakDeferred;
     case PAGE_BREAK:
@@ -1147,6 +1153,10 @@ void DomainMapper_Impl::clearDeferredBreak(BreakType deferredBreakType)
 {
     switch (deferredBreakType)
     {
+    case LINE_BREAK:
+        assert(m_nLineBreaksDeferred > 0);
+        m_nLineBreaksDeferred--;
+        break;
     case COLUMN_BREAK:
         m_bIsColumnBreakDeferred = false;
         break;
@@ -1160,6 +1170,7 @@ void DomainMapper_Impl::clearDeferredBreak(BreakType deferredBreakType)
 
 void DomainMapper_Impl::clearDeferredBreaks()
 {
+    m_nLineBreaksDeferred = 0;
     m_bIsColumnBreakDeferred = false;
     m_bIsPageBreakDeferred = false;
 }

writerfilter/source/dmapper/DomainMapper_Impl.hxx
@@ -114,7 +114,8 @@ enum { NUMBER_OF_CONTEXTS = CONTEXT_LIST + 1 };
 enum BreakType
 {
     PAGE_BREAK,
-    COLUMN_BREAK
+    COLUMN_BREAK,
+    LINE_BREAK
 };
 
 /**
@@ -478,6 +479,7 @@ private:
     bool                                                                            m_bIsFirstSection;
     bool                                                                            m_bIsColumnBreakDeferred;
     bool                                                                            m_bIsPageBreakDeferred;
+    sal_Int32                                                                       m_nLineBreaksDeferred;
     /// If we want to set "sdt end" on the next character context.
     bool                                                                            m_bSdtEndDeferred;
     /// If we want to set "paragraph sdt end" on the next paragraph context.

sw/qa/extras/ooxmlexport/ooxmlexport17.cxx
@@ -165,6 +165,11 @@ DECLARE_OOXMLEXPORT_TEST(TestWPGZOrder, "testWPGZOrder.docx")
     }
 }
 
+DECLARE_OOXMLEXPORT_TEST(testTdf126287, "tdf126287.docx")
+{
+    CPPUNIT_ASSERT_EQUAL(2, getPages());
+}
+
 DECLARE_OOXMLEXPORT_TEST(testTdf123642_BookmarkAtDocEnd, "tdf123642.docx")
 {
     // get bookmark interface

commit e00479404af5058b982c447e485af995d552e372

tdf#161318 sw clearing break: fix this at section end
Regression from commit 19bca24486315cc35f873486e6a2dd18394d0614
(tdf#126287: docx import: use defered linebreak, 2022-02-07), the bugdoc
has a single paragraph in the first section, containing a clearing
break, which is lost. This leads to overlapping text as the text is
shifted up.

Seems the intention was to avoid a line break at the very end of the
document, as that can lead to an empty page with "next page" section
breaks, with non-clearing line breaks.

Fix the problem by only doing this for non-clearing line breaks: that
keeps the old use-case working, but the new, clearing line break then
shifts down the text, so no text overlap happens.

Switching from appendTextPortion() to HandleLineBreak() helps because
HandleLineBreak() does exactly appendTextPortion("\n") in the
non-clearing case, but knows about the stream stack's line break clear
status.

Change-Id: I38868eeeac55e20e86b668e9baf7e0d6a4976608
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/168278
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

sw/source/writerfilter/dmapper/DomainMapper.cxx
@@ -4347,13 +4347,13 @@ void DomainMapper::lcl_utext(const sal_Unicode *const data_, size_t len)
     else if (len == 1 && sText[0] == '\r')
     {
         // Clear "last" one linebreak at end of section
-        if (m_pImpl->GetIsLastParagraphInSection() && m_pImpl->isBreakDeferred(LINE_BREAK))
+        if (m_pImpl->GetIsLastParagraphInSection() && m_pImpl->isBreakDeferred(LINE_BREAK) && !m_pImpl->HasLineBreakClear())
             m_pImpl->clearDeferredBreak(LINE_BREAK);
         // And emit all other linebreaks
         while (m_pImpl->isBreakDeferred(LINE_BREAK))
         {
             m_pImpl->clearDeferredBreak(LINE_BREAK);
-            m_pImpl->appendTextPortion(u"\n"_ustr, m_pImpl->GetTopContext());
+            m_pImpl->HandleLineBreak(m_pImpl->GetTopContext());
         }
     }
     else if (len == 1 && sText[0] == '\t' )

sw/source/writerfilter/dmapper/DomainMapper_Impl.cxx
@@ -5176,6 +5176,11 @@ void DomainMapper_Impl::HandleLineBreakClear(sal_Int32 nClear)
     }
 }
 
+bool DomainMapper_Impl::HasLineBreakClear() const
+{
+    return m_StreamStateStack.top().oLineBreakClear.has_value();
+}
+
 void DomainMapper_Impl::HandleLineBreak(const PropertyMapPtr& pPropertyMap)
 {
     if (!m_StreamStateStack.top().oLineBreakClear.has_value())

sw/source/writerfilter/dmapper/DomainMapper_Impl.hxx
@@ -1234,6 +1234,9 @@ public:
     /// Handles <w:br w:clear="...">.
     void HandleLineBreakClear(sal_Int32 nClear);
 
+    /// Checks if we have a pending <w:br w:clear="...">.
+    bool HasLineBreakClear() const;
+
     /// Handles <w:br>.
     void HandleLineBreak(const PropertyMapPtr& pPropertyMap);
 

sw/qa/writerfilter/dmapper/DomainMapper_Impl.cxx
@@ -411,6 +411,29 @@ CPPUNIT_TEST_FIXTURE(Test, testRedlinedShapeThenSdt)
     CPPUNIT_ASSERT_EQUAL(u"ContentControl"_ustr,
                          xPortion->getPropertyValue("TextPortionType").get<OUString>());
 }
+
+CPPUNIT_TEST_FIXTURE(Test, testClearingBreakSectEnd)
+{
+    // Given a file with a single-paragraph section, ends with a clearing break:
+    // When importing that document:
+    loadFromFile(u"clearing-break-sect-end.docx");
+
+    // Then make sure the clearing break is not lost before a cont sect break:
+    uno::Reference<text::XTextDocument> xTextDocument(mxComponent, uno::UNO_QUERY);
+    uno::Reference<container::XEnumerationAccess> xParaEnumAccess(xTextDocument->getText(),
+                                                                  uno::UNO_QUERY);
+    uno::Reference<container::XEnumeration> xParaEnum = xParaEnumAccess->createEnumeration();
+    uno::Reference<container::XEnumerationAccess> xPortionEnumAccess(xParaEnum->nextElement(),
+                                                                     uno::UNO_QUERY);
+    uno::Reference<container::XEnumeration> xPortionEnum = xPortionEnumAccess->createEnumeration();
+    uno::Reference<beans::XPropertySet> xPortion(xPortionEnum->nextElement(), uno::UNO_QUERY);
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: LineBreak
+    // - Actual  : Text
+    // i.e. the clearing break at sect end was lost, leading to text overlap.
+    CPPUNIT_ASSERT_EQUAL(u"LineBreak"_ustr,
+                         xPortion->getPropertyValue("TextPortionType").get<OUString>());
+}
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */