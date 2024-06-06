LibreOffice/core

commit bde746141f9b382156a5ce79ebf82c5e32fc455b

tdf#146547 sfx2: allow read-only documents to be modified
This is particularly useful for a Writer document that contains a
section that is set to be editable in a read-only document, so that the
user is warned when closing such a document without saving it.

Originally this check was added in commit
27db57efc51487b3976fbf73df1868b8fb78d201 "CWS fwkbugfix05"
"#i39869#: readonly docs should never become modified"
but the actual problem there was that the read-only document was
displayed in a wizard dialog, not in a document view, so let's instead
check that the document is some kind of "internal" document.

Also the dialog's Save button should do something, and since the
document is read-only, a Save As dialog appears appropriate.

Change-Id: I339cbe92a2c9eb74a1f5061246f921037a47f79c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/153180
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
(cherry picked from commit aca32a55456aa4e907b216fb490b3c15d26c3d55)
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/153147
Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>

sfx2/source/doc/objmisc.cxx
@@ -247,7 +247,12 @@ void SfxObjectShell::EnableSetModified( bool bEnable )
 
 bool SfxObjectShell::IsEnableSetModified() const
 {
-    return pImpl->m_bEnableSetModified && !IsReadOnly();
+    // tdf#146547 read-only does not prevent modified, instead try to prevent
+    // setting "internal" documents that may be displayed in some dialog but
+    // which the user didn't load or activate to modified.
+    return pImpl->m_bEnableSetModified && !IsPreview()
+        && eCreateMode != SfxObjectCreateMode::ORGANIZER
+        && eCreateMode != SfxObjectCreateMode::INTERNAL;
 }
 
 

sfx2/source/doc/objembed.cxx
@@ -21,6 +21,7 @@
 #include <sfx2/app.hxx>
 #include <objshimp.hxx>
 #include <sfx2/event.hxx>
+#include <sfx2/sfxbasemodel.hxx>
 
 #include <comphelper/fileformat.h>
 #include <tools/fract.hxx>
@@ -84,8 +85,14 @@ void SfxObjectShell::SetVisArea( const tools::Rectangle & rVisArea )
         pImpl->m_aVisArea = rVisArea;
         if ( GetCreateMode() == SfxObjectCreateMode::EMBEDDED )
         {
-            if ( IsEnableSetModified() )
+            if (IsEnableSetModified()
+                // Base forms use EMBEDDED but they actually live in their own
+                // frame - resizing that shouldn't set it to modified.
+                && pImpl->pBaseModel
+                && pImpl->pBaseModel->getIdentifier() != "com.sun.star.sdb.FormDesign")
+            {
                 SetModified();
+            }
 
             SfxGetpApp()->NotifyEvent(SfxEventHint( SfxEventHintId::VisAreaChanged, GlobalEventConfig::GetEventName(GlobalEventId::VISAREACHANGED), this));
         }

sfx2/source/doc/objxtor.cxx
@@ -565,7 +565,13 @@ bool SfxObjectShell::PrepareClose
         {
             // Save by each Dispatcher
             const SfxPoolItem *pPoolItem;
-            if ( IsSaveVersionOnClose() )
+            if (IsReadOnly())
+            {
+                SfxBoolItem aWarnItem( SID_FAIL_ON_WARNING, bUI );
+                const SfxPoolItem* ppArgs[] = { &aWarnItem, nullptr };
+                pPoolItem = pFrame->GetBindings().ExecuteSynchron(SID_SAVEASDOC, ppArgs);
+            }
+            else if (IsSaveVersionOnClose())
             {
                 SfxStringItem aItem( SID_DOCINFO_COMMENTS, SfxResId(STR_AUTOMATICVERSION) );
                 SfxBoolItem aWarnItem( SID_FAIL_ON_WARNING, bUI );

starmath/qa/cppunit/test_cursor.cxx
@@ -58,6 +58,7 @@ void Test::setUp()
     SmGlobals::ensure();
 
     xDocShRef = new SmDocShell(SfxModelFlags::EMBEDDED_OBJECT);
+    xDocShRef->DoInitNew();
 }
 
 void Test::tearDown()

starmath/qa/cppunit/test_node.cxx
@@ -53,6 +53,7 @@ void NodeTest::setUp()
     mxDocShell = new SmDocShell(SfxModelFlags::EMBEDDED_OBJECT |
                                 SfxModelFlags::DISABLE_EMBEDDED_SCRIPTS |
                                 SfxModelFlags::DISABLE_DOCUMENT_RECOVERY);
+    mxDocShell->DoInitNew();
 }
 
 void NodeTest::tearDown()

starmath/qa/cppunit/test_nodetotextvisitors.cxx
@@ -91,6 +91,7 @@ void Test::setUp()
     SmGlobals::ensure();
 
     xDocShRef = new SmDocShell(SfxModelFlags::EMBEDDED_OBJECT);
+    xDocShRef->DoInitNew();
 }
 
 void Test::tearDown()

sw/qa/python/check_xmodifiable2.py
@@ -173,17 +173,16 @@ class XModifiable2(unittest.TestCase):
         xDoc = self._uno.openTemplateFromTDOC('WriteProtected.odt')
 
         # perform unit test:
-        #       it is unable to set modified flag using text editing
-        #       when modification of the flag was disabled as
+        #       it is able to set modified flag using text editing despite
         #       ODT file was marked to be opened as read-only
-        self.assertFalse(xDoc.isSetModifiedEnabled())
+        self.assertTrue(xDoc.isSetModifiedEnabled())
         self.assertFalse(xDoc.isModified())
 
         cursor = xDoc.Text.createTextCursor()
         xDoc.Text.insertString(cursor, "The first paragraph", 0)
 
-        self.assertFalse(xDoc.isSetModifiedEnabled())
-        self.assertFalse(xDoc.isModified())
+        self.assertTrue(xDoc.isSetModifiedEnabled())
+        self.assertTrue(xDoc.isModified())
 
         # clean up
         xDoc.close(True)

vcl/qa/cppunit/filter/ipdf/ipdf.cxx
@@ -110,6 +110,10 @@ CPPUNIT_TEST_FIXTURE(VclFilterIpdfTest, testPDFAddVisibleSignatureLastPage)
     SdrView* pView = SfxViewShell::Current()->GetDrawView();
     svx::SignatureLineHelper::setShapeCertificate(pView, xCert);
 
+    // the document is modified now, but Sign function can't show SaveAs dialog
+    // in unit test, so just clear the modified
+    pObjectShell->SetModified(false);
+
     // When: do the actual signing.
     pObjectShell->SignDocumentContentUsingCertificate(xCert);
 

commit 3e7478ef9706e74ac551c1f94090e163e74c7fd8

tdf#157931 sfx2: fix modified status of Base forms
The problem is that when a Base form is not in design mode, it's not
expected that it will be stored.

There are checks for m_bOpenInDesign in ODocumentDefinition::save() and
ODocumentDefinition::saveAs() that were added in commit
9cfe62966ff1e0039d95a07d0c613675a53e3075 "CWS dba201b" but removing
these runs into another check in OCommonEmbeddedObject::storeOwn()
of m_bReadOnly and then it still fails, presumably because the storage
was opened readonly.

Let's restore SfxObjectShell::IsEnableSetModified() to check
IsReadOnly(), but only for Base form documents, which seems simplest.

(regression from commit bde746141f9b382156a5ce79ebf82c5e32fc455b)

Change-Id: I886389e2bd462d27ed5c46bfd2132893bb2d5a81
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/168481
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
Tested-by: Jenkins

sfx2/source/doc/objmisc.cxx
@@ -258,7 +258,13 @@ bool SfxObjectShell::IsEnableSetModified() const
     // which the user didn't load or activate to modified.
     return pImpl->m_bEnableSetModified && !IsPreview()
         && eCreateMode != SfxObjectCreateMode::ORGANIZER
-        && eCreateMode != SfxObjectCreateMode::INTERNAL;
+        && eCreateMode != SfxObjectCreateMode::INTERNAL
+        // tdf#157931 form documents only in design mode
+        && ((pImpl->pBaseModel
+                && !pImpl->pBaseModel->impl_isDisposed()
+                && pImpl->pBaseModel->IsInitialized()
+                && pImpl->pBaseModel->getIdentifier() != "com.sun.star.sdb.FormDesign")
+            || !IsReadOnly());
 }
 
 