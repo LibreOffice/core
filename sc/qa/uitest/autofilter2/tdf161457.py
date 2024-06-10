LibreOffice/core

commit 2d1df9f3dccc10f13b8585ad18afce1542ebc4d1

tdf#117276 sc: Show hidden filter elements as inactive elements
Showing hidden values in the autofilter dropdown (as inactive when
it was hidden by another row) - without changing the behaviour of
autofilter. First those which belongs to non-hidden rows, then those
which belongs to hidden rows.

TODO: maybe we can add a global option where the user can switch on/off this feature.

Change-Id: Iafeb43176efe7ab422b22697d399c68c95d0319d
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/136595
Tested-by: Jenkins
Reviewed-by: Balazs Varga <balazs.varga.extern@allotropia.de>

sc/source/core/tool/typedstrdata.cxx
@@ -12,19 +12,33 @@
 
 #include <unotools/collatorwrapper.hxx>
 
+bool ScTypedStrData::LessHiddenRows::operator() (const ScTypedStrData& left, const ScTypedStrData& right) const
+{
+    return left.mbIsHiddenByFilter < right.mbIsHiddenByFilter;
+}
+
 bool ScTypedStrData::LessCaseSensitive::operator() (const ScTypedStrData& left, const ScTypedStrData& right) const
 {
     if (left.meStrType != right.meStrType)
         return left.meStrType < right.meStrType;
 
     if (left.meStrType == Value)
+    {
+        if (left.mfValue == right.mfValue)
+            return left.mbIsHiddenByFilter < right.mbIsHiddenByFilter;
         return left.mfValue < right.mfValue;
+    }
 
     if (left.mbIsDate != right.mbIsDate)
         return left.mbIsDate < right.mbIsDate;
 
-    return ScGlobal::GetCaseCollator().compareString(
-        left.maStrValue, right.maStrValue) < 0;
+    sal_Int32 nEqual = ScGlobal::GetCaseCollator().compareString(
+        left.maStrValue, right.maStrValue);
+
+    if (!nEqual)
+        return left.mbIsHiddenByFilter < right.mbIsHiddenByFilter;
+
+    return nEqual < 0;
 }
 
 bool ScTypedStrData::LessCaseInsensitive::operator() (const ScTypedStrData& left, const ScTypedStrData& right) const
@@ -33,13 +47,22 @@ bool ScTypedStrData::LessCaseInsensitive::operator() (const ScTypedStrData& left
         return left.meStrType < right.meStrType;
 
     if (left.meStrType == Value)
+    {
+        if (left.mfValue == right.mfValue)
+            return left.mbIsHiddenByFilter < right.mbIsHiddenByFilter;
         return left.mfValue < right.mfValue;
+    }
 
     if (left.mbIsDate != right.mbIsDate)
         return left.mbIsDate < right.mbIsDate;
 
-    return ScGlobal::GetCollator().compareString(
-        left.maStrValue, right.maStrValue) < 0;
+    sal_Int32 nEqual = ScGlobal::GetCollator().compareString(
+        left.maStrValue, right.maStrValue);
+
+    if (!nEqual)
+        return left.mbIsHiddenByFilter < right.mbIsHiddenByFilter;
+
+    return nEqual < 0;
 }
 
 bool ScTypedStrData::EqualCaseSensitive::operator() (const ScTypedStrData& left, const ScTypedStrData& right) const
@@ -79,12 +102,13 @@ bool ScTypedStrData::operator< (const ScTypedStrData& r) const
 }
 
 ScTypedStrData::ScTypedStrData(
-    const OUString& rStr, double fVal, double fRVal, StringType nType, bool bDate ) :
+    const OUString& rStr, double fVal, double fRVal, StringType nType, bool bDate, bool bIsHiddenByFilter ) :
     maStrValue(rStr),
     mfValue(fVal),
     mfRoundedValue(fRVal),
     meStrType(nType),
-    mbIsDate( bDate ) {}
+    mbIsDate( bDate ),
+    mbIsHiddenByFilter(bIsHiddenByFilter) {}
 
 FindTypedStrData::FindTypedStrData(const ScTypedStrData& rVal, bool bCaseSens) :
     maVal(rVal), mbCaseSens(bCaseSens) {}

cui/uiconfig/ui/accelconfigpage.ui
@@ -32,6 +32,8 @@
       <column type="gint"/>
       <!-- column-name sensitive1 -->
       <column type="gboolean"/>
+      <!-- column-name sensitive2 -->
+      <column type="gboolean"/>
     </columns>
   </object>
   <object class="GtkTreeStore" id="liststore4">

include/vcl/toolkit/treelistbox.hxx
@@ -127,6 +127,7 @@ public:
     int GetHeight(const SvTreeListBox* pView, const SvTreeListEntry* pEntry) const;
     static int GetHeight(const SvViewDataEntry* pData, sal_uInt16 nItemPos);
     void Enable(bool bEnabled) { mbDisabled = !bEnabled; }
+    bool isEnable() const { return !mbDisabled; }
 
     virtual void Paint(const Point& rPos, SvTreeListBox& rOutDev, vcl::RenderContext& rRenderContext, const SvViewDataEntry* pView, const SvTreeListEntry& rEntry) = 0;
 

include/vcl/weld.hxx
@@ -1033,8 +1033,9 @@ public:
     virtual OUString get_text(int row, int col = -1) const = 0;
     // col index -1 sets the first text column
     virtual void set_text(int row, const OUString& rText, int col = -1) = 0;
-    // col index -1 sets the first text column
+    // col index -1 sets all columns
     virtual void set_sensitive(int row, bool bSensitive, int col = -1) = 0;
+    virtual bool get_sensitive(int row, int col) const = 0;
     virtual void set_id(int row, const OUString& rId) = 0;
     // col index -1 sets the expander toggle, enable_toggle_buttons must have been called to create that column
     virtual void set_toggle(int row, TriState eState, int col = -1) = 0;
@@ -1132,8 +1133,9 @@ public:
     virtual void set_extra_row_indent(const TreeIter& rIter, int nIndentLevel) = 0;
     // col index -1 sets the first text column
     virtual void set_text(const TreeIter& rIter, const OUString& rStr, int col = -1) = 0;
-    // col index -1 sets the first text column
+    // col index -1 sets all columns
     virtual void set_sensitive(const TreeIter& rIter, bool bSensitive, int col = -1) = 0;
+    virtual bool get_sensitive(const TreeIter& rIter, int col) const = 0;
     virtual void set_text_emphasis(const TreeIter& rIter, bool bOn, int col) = 0;
     virtual bool get_text_emphasis(const TreeIter& rIter, int col) const = 0;
     virtual void set_text_align(const TreeIter& rIter, double fAlign, int col) = 0;
@@ -1334,6 +1336,7 @@ public:
     virtual TreeView* get_drag_source() const = 0;
 
     using Widget::set_sensitive;
+    using Widget::get_sensitive;
 };
 
 class VCL_DLLPUBLIC IconView : virtual public Widget

sc/inc/column.hxx
@@ -204,6 +204,7 @@ class ScColumn : protected ScColumnData
     SCTAB           nTab;
 
     bool mbFiltering : 1; // it is true if there is a filtering in the column
+    bool mbFilteredRow : 1; // it is true if the actual row of the filtered column is hidden
     bool mbEmptyBroadcastersPending : 1; // a broadcaster not removed during EnableDelayDeletingBroadcasters()
 
 friend class ScDocument;                    // for FillInfo
@@ -255,6 +256,7 @@ public:
     SCTAB GetTab() const { return nTab; }
     SCCOL GetCol() const { return nCol; }
     bool HasFiltering() const { return mbFiltering; }
+    bool IsFilteredRow() const { return mbFilteredRow; }
     sc::CellStoreType& GetCellStore() { return maCells; }
     const sc::CellStoreType& GetCellStore() const { return maCells; }
     sc::CellTextAttrStoreType& GetCellAttrStore() { return maCellTextAttrs; }
@@ -598,7 +600,7 @@ public:
 
     void GetFilterEntries(
         sc::ColumnBlockConstPosition& rBlockPos, SCROW nStartRow, SCROW nEndRow,
-        ScFilterEntries& rFilterEntries, bool bFiltering );
+        ScFilterEntries& rFilterEntries, bool bFiltering, bool bHiddenRow = false );
 
     bool GetDataEntries( SCROW nRow, std::set<ScTypedStrData>& rStrings) const;
 

sc/inc/typedstrdata.hxx
@@ -26,21 +26,28 @@ public:
     };
 
     ScTypedStrData(
-        OUString&& rStr, double fVal = 0.0, double fRVal = 0.0, StringType nType = Standard, bool bDate = false ) :
+        OUString&& rStr, double fVal = 0.0, double fRVal = 0.0, StringType nType = Standard, bool bDate = false, bool bIsHiddenByFilter = false ) :
         maStrValue(std::move(rStr)),
         mfValue(fVal),
         mfRoundedValue(fRVal),
         meStrType(nType),
-        mbIsDate( bDate ) {}
+        mbIsDate( bDate ),
+        mbIsHiddenByFilter(bIsHiddenByFilter) {}
 
     ScTypedStrData( const OUString& rStr, double fVal = 0.0, double fRVal = 0.0, StringType eType = Standard,
-                    bool bDate = false );
+                    bool bDate = false, bool bIsHiddenByFilter = false );
 
     bool IsDate() const { return mbIsDate;}
+    bool IsHiddenByFilter() const { return mbIsHiddenByFilter;}
     const OUString& GetString() const { return maStrValue;}
     StringType GetStringType() const { return meStrType;}
     double GetValue() const { return mfValue; }
     double GetRoundedValue() const { return mfRoundedValue; }
+
+    struct LessHiddenRows
+    {
+        bool operator() (const ScTypedStrData& left, const ScTypedStrData& right) const;
+    };
 
     struct LessCaseSensitive
     {
@@ -70,6 +77,7 @@ private:
     double mfRoundedValue; // rounded value by format code
     StringType meStrType;
     bool   mbIsDate;
+    bool   mbIsHiddenByFilter;
 };
 
 class FindTypedStrData

sc/qa/uitest/autofilter/autofilter.py
@@ -237,7 +237,8 @@ class AutofilterTest(UITestCase):
             xFloatWindow = self.xUITest.getFloatWindow()
             xCheckListMenu = xFloatWindow.getChild("FilterDropDown")
             xTreeList = xCheckListMenu.getChild("check_list_box")
-            self.assertEqual(5, len(xTreeList.getChildren()))
+            # since tdf#117267, we are showing the hidden filter rows as inactive elements (5 active + 3 inactive)
+            self.assertEqual(8, len(xTreeList.getChildren()))
             xOkBtn = xFloatWindow.getChild("cancel")
             xOkBtn.executeAction("CLICK", tuple())
 
@@ -245,7 +246,8 @@ class AutofilterTest(UITestCase):
             xFloatWindow = self.xUITest.getFloatWindow()
             xCheckListMenu = xFloatWindow.getChild("FilterDropDown")
             xTreeList = xCheckListMenu.getChild("check_list_box")
-            self.assertEqual(3, len(xTreeList.getChildren()))
+            # since tdf#117267, we are showing the hidden filter rows as inactive elements (3 active + 9 inactive)
+            self.assertEqual(12, len(xTreeList.getChildren()))
             xOkBtn = xFloatWindow.getChild("cancel")
             xOkBtn.executeAction("CLICK", tuple())
 
@@ -258,7 +260,8 @@ class AutofilterTest(UITestCase):
             xFloatWindow = self.xUITest.getFloatWindow()
             xCheckListMenu = xFloatWindow.getChild("FilterDropDown")
             xTreeList = xCheckListMenu.getChild("check_list_box")
-            self.assertEqual(9, len(xTreeList.getChildren()))
+            # since tdf#117267, we are showing the hidden filter rows as inactive elements (9 active + 3 inactive)
+            self.assertEqual(12, len(xTreeList.getChildren()))
             xOkBtn = xFloatWindow.getChild("cancel")
             xOkBtn.executeAction("CLICK", tuple())
 
@@ -279,7 +282,8 @@ class AutofilterTest(UITestCase):
             xFloatWindow = self.xUITest.getFloatWindow()
             xCheckListMenu = xFloatWindow.getChild("FilterDropDown")
             xTreeList = xCheckListMenu.getChild("check_list_box")
-            self.assertEqual(3, len(xTreeList.getChildren()))
+            # since tdf#117267, we are showing the hidden filter rows as inactive elements (3 active + 5 inactive)
+            self.assertEqual(8, len(xTreeList.getChildren()))
             xOkBtn = xFloatWindow.getChild("cancel")
             xOkBtn.executeAction("CLICK", tuple())
 
@@ -287,7 +291,8 @@ class AutofilterTest(UITestCase):
             xFloatWindow = self.xUITest.getFloatWindow()
             xCheckListMenu = xFloatWindow.getChild("FilterDropDown")
             xTreeList = xCheckListMenu.getChild("check_list_box")
-            self.assertEqual(4, len(xTreeList.getChildren()))
+            # since tdf#117267, we are showing the hidden filter rows as inactive elements (4 active + 8 inactive)
+            self.assertEqual(12, len(xTreeList.getChildren()))
             xOkBtn = xFloatWindow.getChild("cancel")
             xOkBtn.executeAction("CLICK", tuple())
 
@@ -300,7 +305,8 @@ class AutofilterTest(UITestCase):
             xFloatWindow = self.xUITest.getFloatWindow()
             xCheckListMenu = xFloatWindow.getChild("FilterDropDown")
             xTreeList = xCheckListMenu.getChild("check_list_box")
-            self.assertEqual(3, len(xTreeList.getChildren()))
+            # since tdf#117267, we are showing the hidden filter rows as inactive elements (3 active + 1 inactive)
+            self.assertEqual(4, len(xTreeList.getChildren()))
             xOkBtn = xFloatWindow.getChild("cancel")
             xOkBtn.executeAction("CLICK", tuple())
 
@@ -326,7 +332,8 @@ class AutofilterTest(UITestCase):
             xFloatWindow = self.xUITest.getFloatWindow()
             xCheckListMenu = xFloatWindow.getChild("FilterDropDown")
             xTreeList = xCheckListMenu.getChild("check_list_box")
-            self.assertEqual(2, len(xTreeList.getChildren()))
+            # since tdf#117267, we are showing the hidden filter rows as inactive elements (2 active + 1 inactive)
+            self.assertEqual(3, len(xTreeList.getChildren()))
             xOkBtn = xFloatWindow.getChild("cancel")
             xOkBtn.executeAction("CLICK", tuple())
 

sc/qa/uitest/autofilter2/tdf117276_autofilter_reset.py
@@ -47,8 +47,10 @@ class tdf117276_autofilter_reset(UITestCase):
         self.assertTrue(is_row_hidden(document, 4))
         self.assertFalse(is_row_hidden(document, 5))
 
-        self.assertEqual(1, self.get_values_count_in_AutoFilter(xGridWindow, "0"))
-        self.assertEqual(2, self.get_values_count_in_AutoFilter(xGridWindow, "1"))
+        # since tdf#117267, we are showing the hidden filter rows as inactive elements (1 active + 4 inactive)
+        self.assertEqual(5, self.get_values_count_in_AutoFilter(xGridWindow, "0"))
+        # since tdf#117267, we are showing the hidden filter rows as inactive elements (2 active + 1 inactive)
+        self.assertEqual(3, self.get_values_count_in_AutoFilter(xGridWindow, "1"))
 
     def test_run(self):
         with self.ui_test.create_doc_in_start_center("calc") as document:
@@ -135,7 +137,8 @@ class tdf117276_autofilter_reset(UITestCase):
             self.assertFalse(is_row_hidden(document, 5))
 
             self.assertEqual(5, self.get_values_count_in_AutoFilter(xGridWindow, "0"))
-            self.assertEqual(2, self.get_values_count_in_AutoFilter(xGridWindow, "1"))
+            # since tdf#117267, we are showing the hidden filter rows as inactive elements (2 active + 1 inactive)
+            self.assertEqual(3, self.get_values_count_in_AutoFilter(xGridWindow, "1"))
 
             # 4. open filter of column B and deselect "Unique b5"
             xGridWindow.executeAction("LAUNCH", mkPropertyValues({"AUTOFILTER": "", "COL": "1", "ROW": "0"}))

commit db153afbac7acc1ae46f78beef4221e9aad07305

tdf#161457 - Fix autofilter result is wrong when cells contain
formatted rounded values.

Filtered formatted values were not sorted with the rounded values
but with the original values and the duplicated values caused wrong
filter results.

Regression from commit: 2d1df9f3dccc10f13b8585ad18afce1542ebc4d1
(tdf#117276 sc: Show hidden filter elements as inactive elements)

Change-Id: I7540ba5fa98e6bbe0b014448bf2449432261d542
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/168592
Tested-by: Jenkins
Reviewed-by: Balazs Varga <balazs.varga.extern@allotropia.de>

sc/source/core/tool/typedstrdata.cxx
@@ -26,7 +26,7 @@ bool ScTypedStrData::LessCaseSensitive::operator() (const ScTypedStrData& left, 
 
     if (left.meStrType == Value)
     {
-        if (left.mfValue == right.mfValue)
+        if (left.mfRoundedValue == right.mfRoundedValue)
             return left.mbIsHiddenByFilter < right.mbIsHiddenByFilter;
         return left.mfValue < right.mfValue;
     }
@@ -50,7 +50,7 @@ bool ScTypedStrData::LessSortCaseSensitive::operator() (const ScTypedStrData& le
 
     if (left.meStrType == Value)
     {
-        if (left.mfValue == right.mfValue)
+        if (left.mfRoundedValue == right.mfRoundedValue)
             return left.mbIsHiddenByFilter < right.mbIsHiddenByFilter;
         return left.mfValue < right.mfValue;
     }
@@ -73,7 +73,7 @@ bool ScTypedStrData::LessCaseInsensitive::operator() (const ScTypedStrData& left
 
     if (left.meStrType == Value)
     {
-        if (left.mfValue == right.mfValue)
+        if (left.mfRoundedValue == right.mfRoundedValue)
             return left.mbIsHiddenByFilter < right.mbIsHiddenByFilter;
         return left.mfValue < right.mfValue;
     }
@@ -97,7 +97,7 @@ bool ScTypedStrData::LessSortCaseInsensitive::operator() (const ScTypedStrData& 
 
     if (left.meStrType == Value)
     {
-        if (left.mfValue == right.mfValue)
+        if (left.mfRoundedValue == right.mfRoundedValue)
             return left.mbIsHiddenByFilter < right.mbIsHiddenByFilter;
         return left.mfValue < right.mfValue;
     }

sc/qa/uitest/autofilter2/tdf161457.py
@@ -1,0 +1,42 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+from uitest.framework import UITestCase
+from uitest.uihelper.common import get_state_as_dict, get_url_for_data_file
+from libreoffice.uno.propertyvalue import mkPropertyValues
+
+class tdf161457(UITestCase):
+
+    def test_tdf161457(self):
+
+        with self.ui_test.load_file(get_url_for_data_file("tdf161457.ods")) as calc_doc:
+
+            xCalcDoc = self.xUITest.getTopFocusWindow()
+            xGridWin = xCalcDoc.getChild("grid_window")
+
+            # Click the autofilter dropdown in column B
+            xGridWin.executeAction("LAUNCH", mkPropertyValues({"AUTOFILTER": "", "COL": "1", "ROW": "0"}))
+            xFloatWindow = self.xUITest.getFloatWindow()
+            xCheckListMenu = xFloatWindow.getChild("FilterDropDown")
+            xTreeList = xCheckListMenu.getChild("check_list_box")
+
+            self.assertEqual(2, len(xTreeList.getChildren()))
+
+            xEntry1 = xTreeList.getChild(0)
+            self.assertEqual("1.00", get_state_as_dict(xEntry1)['Text'])
+            self.assertEqual("true", get_state_as_dict(xEntry1)['IsChecked'])
+            self.assertEqual("false", get_state_as_dict(xEntry1)['IsSemiTransparent'])
+
+            xEntry2 = xTreeList.getChild(1)
+            self.assertEqual("5.00", get_state_as_dict(xEntry2)['Text'])
+            self.assertEqual("true", get_state_as_dict(xEntry2)['IsChecked'])
+            # Without the fix in place, this test would have failed with
+            # AssertionError: 'true' != 'false'
+            self.assertEqual("false", get_state_as_dict(xEntry2)['IsSemiTransparent'])
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab: