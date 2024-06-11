ed54d14d5c9bf46b48741e37a12664921aed1c2a
Xisco Fauli <xiscofauli@libreoffice.org>
2024-06-11 16:16:13 +0200

    CppunitTest_dbaccess_macros_test: remove redundant test
    This was added in 0f4451c0b3bda3d14c7672ca670b4de959495365
    "first part of a c++ based test for base" as an example on
    how to test base documents.
    At this point we already have other base tests doing
    more interesting stuff so I believe this test is not needed
    anymore
    
    Change-Id: Ice747800f3ab6d887d63595fe9dbdd531acf9014
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/168684
    Tested-by: Jenkins
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- dbaccess/Module_dbaccess.mk
+++ dbaccess/Module_dbaccess.mk
@@ -52,7 +52,6 @@ $(eval $(call gb_Module_add_check_targets,dbaccess,\
 	CppunitTest_dbaccess_dialog_save \
 	CppunitTest_dbaccess_empty_stdlib_save \
 	CppunitTest_dbaccess_nolib_save \
-	CppunitTest_dbaccess_macros_test \
 	CppunitTest_dbaccess_hsqlschema_import \
 ))
 endif
