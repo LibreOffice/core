bdd325090323ac7e6486ef16482ec960f55e1402
Mike Kaganski <mike.kaganski@collabora.com>
2024-06-14 14:48:53 +0500

    Make isProductVersionUpgraded update ooSetupLastVersion again
    This was changed in commit dd889b290304b73f96a9a8e6e0f144d3aa2ba7e1
    (Resolves tdf#159573 and tdf#137931 - WhatsNew or Welcome dialog,
    2024-03-27).
    
    This change doesn't require opening a module (and checking if WhatsNew
    dialog is needed) to update the configuration - it will be updated
    immediately in runGraphicsRenderTests.
    
    Change-Id: I595b6898c46998e7a0805ffbf7a710dbf0d2a5e4
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/168855
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
    Tested-by: Jenkins
    (cherry picked from commit 00ed1c5c02c20b98dc70ef9e01c9280d9d6b5193)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/168874
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

---
+++ unotools/source/misc/VersionConfig.cxx
@@ -0,0 +1,57 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <sal/config.h>
+
+#include <com/sun/star/lang/IllegalArgumentException.hpp>
+
+#include <officecfg/Setup.hxx>
+
+#include <o3tl/string_view.hxx>
+#include <sal/log.hxx>
+#include <unotools/configmgr.hxx>
+#include <unotools/VersionConfig.hxx>
+
+namespace utl
+{
+bool isProductVersionUpgraded()
+{
+    static const bool bUpgraded = []() {
+        OUString sSetupVersion = utl::ConfigManager::getProductVersion();
+        sal_Int32 iCurrent = o3tl::toInt32(o3tl::getToken(sSetupVersion, 0, '.')) * 10
+                             + o3tl::toInt32(o3tl::getToken(sSetupVersion, 1, '.'));
+        OUString sLastVersion
+            = officecfg::Setup::Product::ooSetupLastVersion::get().value_or("0.0");
+        sal_Int32 iLast = o3tl::toInt32(o3tl::getToken(sLastVersion, 0, '.')) * 10
+                          + o3tl::toInt32(o3tl::getToken(sLastVersion, 1, '.'));
+        if (iCurrent > iLast)
+        {
+            //update lastversion
+            try
+            {
+                std::shared_ptr<comphelper::ConfigurationChanges> batch(
+                    comphelper::ConfigurationChanges::create());
+                officecfg::Setup::Product::ooSetupLastVersion::set(sSetupVersion, batch);
+                batch->commit();
+            }
+            catch (css::lang::IllegalArgumentException&)
+            { //If the value was readOnly.
+                SAL_WARN("desktop.updater", "Updating property ooSetupLastVersion to version "
+                                                << sSetupVersion
+                                                << " failed (read-only property?)");
+            }
+            return true;
+        }
+        return false;
+    }();
+    return bUpgraded;
+}
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
