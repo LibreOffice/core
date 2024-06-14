LibreOffice/core

commit 337a9a454c1bc95214111578d3f9c0622c55c509

use for-range on Sequence in chart2
Change-Id: Ief02e5d5284b0cbad26b04c0a282dccfee577b90
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/94398
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

chart2/source/model/template/ChartTypeTemplate.cxx
@@ -73,9 +73,9 @@ void lcl_ensureCorrectLabelPlacement( const Reference< beans::XPropertySet >& xP
         return;
 
     bool bValid = false;
-    for( sal_Int32 nN = 0; nN < rAvailablePlacements.getLength(); nN++ )
+    for( sal_Int32 i : rAvailablePlacements )
     {
-        if( rAvailablePlacements[nN] == nLabelPlacement )
+        if( i == nLabelPlacement )
         {
             bValid = true;
             break;
@@ -155,13 +155,11 @@ uno::Reference< XDiagram > SAL_CALL ChartTypeTemplate::createDiagramByDataSource
             xInterpreter->interpretDataSource(
                 xDataSource, aArguments, Sequence< Reference< XDataSeries > >() ));
 
-        Sequence< Sequence< Reference< XDataSeries > > > aSeries( aData.Series );
-        sal_Int32 i, j, nCount = 0;
-        for( i=0; i<aSeries.getLength(); ++i )
-        {
-            for( j=0; j<aSeries[i].getLength(); ++j, ++nCount )
-                lcl_applyDefaultStyle( aSeries[i][j], nCount, xDia );
-        }
+        const Sequence< Sequence< Reference< XDataSeries > > > aSeries( aData.Series );
+        sal_Int32 nCount = 0;
+        for( auto const & i : aSeries )
+            for( auto const & j : i )
+                lcl_applyDefaultStyle( j, nCount++, xDia );
 
         Sequence< Reference< XChartType > > aOldChartTypesSeq;
         FillDiagram( xDia, aData.Series, aData.Categories, aOldChartTypesSeq );
@@ -219,12 +217,12 @@ void SAL_CALL ChartTypeTemplate::changeDiagram( const uno::Reference< XDiagram >
         }
         aSeriesSeq = aData.Series;
 
-        sal_Int32 i, j, nIndex = 0;
-        for( i=0; i<aSeriesSeq.getLength(); ++i )
-            for( j=0; j<aSeriesSeq[i].getLength(); ++j, ++nIndex )
+        sal_Int32 nIndex = 0;
+        for( auto const & i : std::as_const(aSeriesSeq) )
+            for( auto const & j : i )
             {
                 if( nIndex >= nFormerSeriesCount )
-                    lcl_applyDefaultStyle( aSeriesSeq[i][j], nIndex, xDiagram );
+                    lcl_applyDefaultStyle( j, nIndex++, xDiagram );
             }
 
         // remove charttype groups from all coordinate systems
@@ -235,11 +233,11 @@ void SAL_CALL ChartTypeTemplate::changeDiagram( const uno::Reference< XDiagram >
         OSL_ASSERT( xCoordSysCnt.is());
         if( xCoordSysCnt.is())
         {
-            Sequence< Reference< XCoordinateSystem > > aCooSysSeq(
+            const Sequence< Reference< XCoordinateSystem > > aCooSysSeq(
                 xCoordSysCnt->getCoordinateSystems());
-            for( sal_Int32 nCooSysIdx = 0; nCooSysIdx < aCooSysSeq.getLength(); ++nCooSysIdx )
+            for( Reference< XCoordinateSystem > const & coords : aCooSysSeq )
             {
-                Reference< XChartTypeContainer > xContainer( aCooSysSeq[nCooSysIdx], uno::UNO_QUERY );
+                Reference< XChartTypeContainer > xContainer( coords, uno::UNO_QUERY );
                 if( xContainer.is() )
                     xContainer->setChartTypes( Sequence< Reference< XChartType > >() );
             }
@@ -436,12 +434,12 @@ void SAL_CALL ChartTypeTemplate::resetStyles( const Reference< chart2::XDiagram 
     bool bPercent = (getStackMode(0) == StackMode::YStackedPercent);
     if( bPercent )
     {
-        Sequence< Reference< chart2::XAxis > > aAxisSeq( AxisHelper::getAllAxesOfDiagram( xDiagram ) );
-        for( sal_Int32 i=0; i<aAxisSeq.getLength(); ++i )
+        const Sequence< Reference< chart2::XAxis > > aAxisSeq( AxisHelper::getAllAxesOfDiagram( xDiagram ) );
+        for( Reference< chart2::XAxis > const & axis : aAxisSeq )
         {
-            if( AxisHelper::getDimensionIndexOfAxis( aAxisSeq[i], xDiagram )== 1 )
+            if( AxisHelper::getDimensionIndexOfAxis( axis, xDiagram )== 1 )
             {
-                Reference< beans::XPropertySet > xAxisProp( aAxisSeq[i], uno::UNO_QUERY );
+                Reference< beans::XPropertySet > xAxisProp( axis, uno::UNO_QUERY );
                 if( xAxisProp.is())
                 {
                     // set number format to source format
@@ -457,31 +455,26 @@ void SAL_CALL ChartTypeTemplate::resetStyles( const Reference< chart2::XDiagram 
         uno::Reference< XCoordinateSystemContainer > xCooSysContainer( xDiagram, uno::UNO_QUERY );
         if( xCooSysContainer.is() )
         {
-            uno::Sequence< uno::Reference< XCoordinateSystem > > aCooSysList( xCooSysContainer->getCoordinateSystems() );
-            for( sal_Int32 nCS = 0; nCS < aCooSysList.getLength(); ++nCS )
+            const uno::Sequence< uno::Reference< XCoordinateSystem > > aCooSysList( xCooSysContainer->getCoordinateSystems() );
+            for( uno::Reference< XCoordinateSystem > const & xCooSys : aCooSysList )
             {
-                uno::Reference< XCoordinateSystem > xCooSys( aCooSysList[nCS] );
-
                 //iterate through all chart types in the current coordinate system
                 uno::Reference< XChartTypeContainer > xChartTypeContainer( xCooSys, uno::UNO_QUERY );
                 OSL_ASSERT( xChartTypeContainer.is());
                 if( !xChartTypeContainer.is() )
                     continue;
-                uno::Sequence< uno::Reference< XChartType > > aChartTypeList( xChartTypeContainer->getChartTypes() );
-                for( sal_Int32 nT = 0; nT < aChartTypeList.getLength(); ++nT )
+                const uno::Sequence< uno::Reference< XChartType > > aChartTypeList( xChartTypeContainer->getChartTypes() );
+                for( uno::Reference< XChartType > const & xChartType : aChartTypeList )
                 {
-                    uno::Reference< XChartType > xChartType( aChartTypeList[nT] );
-
                     //iterate through all series in this chart type
                     uno::Reference< XDataSeriesContainer > xDataSeriesContainer( xChartType, uno::UNO_QUERY );
                     OSL_ASSERT( xDataSeriesContainer.is());
                     if( !xDataSeriesContainer.is() )
                         continue;
 
-                    uno::Sequence< uno::Reference< XDataSeries > > aSeriesList( xDataSeriesContainer->getDataSeries() );
-                    for( sal_Int32 nS = 0; nS < aSeriesList.getLength(); ++nS )
+                    const uno::Sequence< uno::Reference< XDataSeries > > aSeriesList( xDataSeriesContainer->getDataSeries() );
+                    for( Reference< XDataSeries > const & xSeries : aSeriesList )
                     {
-                        Reference< XDataSeries > xSeries(aSeriesList[nS]);
                         Reference< beans::XPropertySet > xSeriesProp( xSeries, uno::UNO_QUERY );
                         if(!xSeries.is() || !xSeriesProp.is() )
                             continue;
@@ -600,11 +593,10 @@ void ChartTypeTemplate::adaptScales(
     )
 {
     bool bSupportsCategories( supportsCategories() );
-    for( sal_Int32 nCooSysIdx=0; nCooSysIdx<aCooSysSeq.getLength(); ++nCooSysIdx )
+    for( Reference< XCoordinateSystem > const & xCooSys : aCooSysSeq )
     {
         try
         {
-            Reference< XCoordinateSystem > xCooSys( aCooSysSeq[nCooSysIdx] );
             if( !xCooSys.is() )
                 continue;
 
@@ -719,9 +711,8 @@ void ChartTypeTemplate::adaptAxes(
     if( !rCoordSys.hasElements() )
         return;
 
-    for( sal_Int32 nCooSysIdx=0; nCooSysIdx < rCoordSys.getLength(); ++nCooSysIdx )
+    for( Reference< XCoordinateSystem > const & xCooSys : rCoordSys )
     {
-        Reference< XCoordinateSystem > xCooSys( rCoordSys[nCooSysIdx] );
         if( !xCooSys.is() )
             continue;
         sal_Int32 nDimCount = xCooSys->getDimension();
@@ -868,10 +859,8 @@ void ChartTypeTemplate::copyPropertiesFromOldToNewCoordinateSystem(
     OUString aNewChartType( xNewChartType->getChartType() );
 
     Reference< beans::XPropertySet > xSource;
-    sal_Int32 nN=0;
-    for( nN=0; nN<rOldChartTypesSeq.getLength();++nN)
+    for( Reference< XChartType > const & xOldType : rOldChartTypesSeq )
     {
-        Reference< XChartType > xOldType( rOldChartTypesSeq[nN] );
         if( xOldType.is() && xOldType->getChartType() == aNewChartType )
         {
             xSource.set( Reference< beans::XPropertySet >(xOldType, uno::UNO_QUERY ) );

chart2/qa/extras/chart2export.cxx
@@ -775,15 +775,15 @@ void Chart2ExportTest::testEmbeddingsGrabBag()
    const char* const testEmbeddedFileNames[] {"word/embeddings/Microsoft_Excel_Worksheet3.xlsx",
                                         "word/embeddings/Microsoft_Excel_Worksheet2.xlsx",
                                         "word/embeddings/Microsoft_Excel_Worksheet1.xlsx"};
-   for(int i = 0; i < aGrabBag.getLength(); ++i)
+   for(beans::PropertyValue const & prop : std::as_const(aGrabBag))
    {
-       if (aGrabBag[i].Name == "OOXEmbeddings")
+       if (prop.Name == "OOXEmbeddings")
        {
            bEmbeddings = true;
            uno::Sequence<beans::PropertyValue> aEmbeddingsList(0);
            uno::Reference<io::XInputStream> aEmbeddingXlsxStream;
            OUString aEmbeddedfileName;
-           CPPUNIT_ASSERT(aGrabBag[i].Value >>= aEmbeddingsList); // PropertyValue of proper type
+           CPPUNIT_ASSERT(prop.Value >>= aEmbeddingsList); // PropertyValue of proper type
            sal_Int32 length = aEmbeddingsList.getLength();
            CPPUNIT_ASSERT_EQUAL(sal_Int32(3), length);
            for(int j = 0; j < length; ++j)
@@ -1513,15 +1513,15 @@ void Chart2ExportTest::testEmbeddingsOleObjectGrabBag()
    CPPUNIT_ASSERT(aGrabBag.hasElements()); // Grab Bag not empty
    bool bEmbeddings = false;
    const char* const testEmbeddedFileNames[] = {"word/embeddings/oleObject1.bin"};
-   for(int i = 0; i < aGrabBag.getLength(); ++i)
+   for(beans::PropertyValue const & prop : std::as_const(aGrabBag))
    {
-       if (aGrabBag[i].Name == "OOXEmbeddings")
+       if (prop.Name == "OOXEmbeddings")
        {
            bEmbeddings = true;
            uno::Sequence<beans::PropertyValue> aEmbeddingsList(0);
            uno::Reference<io::XInputStream> aEmbeddingXlsxStream;
            OUString aEmbeddedfileName;
-           CPPUNIT_ASSERT(aGrabBag[i].Value >>= aEmbeddingsList); // PropertyValue of proper type
+           CPPUNIT_ASSERT(prop.Value >>= aEmbeddingsList); // PropertyValue of proper type
            sal_Int32 length = aEmbeddingsList.getLength();
            CPPUNIT_ASSERT_EQUAL(sal_Int32(1), length);
            for(int j = 0; j < length; ++j)

chart2/qa/extras/charttest.hxx
@@ -329,11 +329,11 @@ Reference< chart2::data::XDataSequence > getLabelDataSequenceFromDoc(
         getDataSeriesFromDoc( xChartDoc, nDataSeries, nChartType );
     CPPUNIT_ASSERT(xDataSeries.is());
     Reference< chart2::data::XDataSource > xDataSource( xDataSeries, uno::UNO_QUERY_THROW );
-    Sequence< Reference< chart2::data::XLabeledDataSequence > > xDataSequences =
+    const Sequence< Reference< chart2::data::XLabeledDataSequence > > xDataSequences =
         xDataSource->getDataSequences();
-    for(sal_Int32 i = 0; i < xDataSequences.getLength(); ++i)
+    for(auto const & lds : xDataSequences)
     {
-        Reference< chart2::data::XDataSequence> xLabelSeq = xDataSequences[i]->getLabel();
+        Reference< chart2::data::XDataSequence> xLabelSeq = lds->getLabel();
         if(!xLabelSeq.is())
             continue;
 
@@ -351,11 +351,11 @@ Reference< chart2::data::XDataSequence > getDataSequenceFromDocByRole(
         getDataSeriesFromDoc( xChartDoc, nDataSeries, nChartType );
     CPPUNIT_ASSERT(xDataSeries.is());
     Reference< chart2::data::XDataSource > xDataSource( xDataSeries, uno::UNO_QUERY_THROW );
-    Sequence< Reference< chart2::data::XLabeledDataSequence > > xDataSequences =
+    const Sequence< Reference< chart2::data::XLabeledDataSequence > > xDataSequences =
         xDataSource->getDataSequences();
-    for(sal_Int32 i = 0; i < xDataSequences.getLength(); ++i)
+    for(auto const & lds : xDataSequences)
     {
-        Reference< chart2::data::XDataSequence> xLabelSeq = xDataSequences[i]->getValues();
+        Reference< chart2::data::XDataSequence> xLabelSeq = lds->getValues();
         uno::Reference< beans::XPropertySet > xProps(xLabelSeq, uno::UNO_QUERY);
         if(!xProps.is())
             continue;
@@ -389,20 +389,20 @@ std::vector<std::vector<double> > getDataSeriesYValuesFromChartType( const Refer
 {
     Reference<chart2::XDataSeriesContainer> xDSCont(xCT, uno::UNO_QUERY);
     CPPUNIT_ASSERT(xDSCont.is());
-    Sequence<uno::Reference<chart2::XDataSeries> > aDataSeriesSeq = xDSCont->getDataSeries();
+    const Sequence<uno::Reference<chart2::XDataSeries> > aDataSeriesSeq = xDSCont->getDataSeries();
 
     double fNan;
     rtl::math::setNan(&fNan);
 
     std::vector<std::vector<double> > aRet;
-    for (sal_Int32 i = 0; i < aDataSeriesSeq.getLength(); ++i)
+    for (uno::Reference<chart2::XDataSeries> const & ds : aDataSeriesSeq)
     {
-        uno::Reference<chart2::data::XDataSource> xDSrc(aDataSeriesSeq[i], uno::UNO_QUERY);
+        uno::Reference<chart2::data::XDataSource> xDSrc(ds, uno::UNO_QUERY);
         CPPUNIT_ASSERT(xDSrc.is());
-        uno::Sequence<Reference<chart2::data::XLabeledDataSequence> > aDataSeqs = xDSrc->getDataSequences();
-        for (sal_Int32 j = 0; j < aDataSeqs.getLength(); ++j)
+        const uno::Sequence<Reference<chart2::data::XLabeledDataSequence> > aDataSeqs = xDSrc->getDataSequences();
+        for (auto const & lds : aDataSeqs)
         {
-            Reference<chart2::data::XDataSequence> xValues = aDataSeqs[j]->getValues();
+            Reference<chart2::data::XDataSequence> xValues = lds->getValues();
             CPPUNIT_ASSERT(xValues.is());
             Reference<beans::XPropertySet> xPropSet(xValues, uno::UNO_QUERY);
             if (!xPropSet.is())
@@ -412,13 +412,13 @@ std::vector<std::vector<double> > getDataSeriesYValuesFromChartType( const Refer
             xPropSet->getPropertyValue("Role") >>= aRoleName;
             if (aRoleName == "values-y")
             {
-                uno::Sequence<uno::Any> aData = xValues->getData();
+                const uno::Sequence<uno::Any> aData = xValues->getData();
                 std::vector<double> aValues;
                 aValues.reserve(aData.getLength());
-                for (sal_Int32 nVal = 0; nVal < aData.getLength(); ++nVal)
+                for (uno::Any const & any : aData)
                 {
                     double fVal;
-                    if (aData[nVal] >>= fVal)
+                    if (any >>= fVal)
                         aValues.push_back(fVal);
                     else
                         aValues.push_back(fNan);
@@ -437,17 +437,17 @@ std::vector<uno::Sequence<uno::Any> > getDataSeriesLabelsFromChartType( const Re
 
     Reference<chart2::XDataSeriesContainer> xDSCont(xCT, uno::UNO_QUERY);
     CPPUNIT_ASSERT(xDSCont.is());
-    Sequence<uno::Reference<chart2::XDataSeries> > aDataSeriesSeq = xDSCont->getDataSeries();
+    const Sequence<uno::Reference<chart2::XDataSeries> > aDataSeriesSeq = xDSCont->getDataSeries();
 
     std::vector<uno::Sequence<uno::Any> > aRet;
-    for (sal_Int32 i = 0; i < aDataSeriesSeq.getLength(); ++i)
+    for (auto const & ds : aDataSeriesSeq)
     {
-        uno::Reference<chart2::data::XDataSource> xDSrc(aDataSeriesSeq[i], uno::UNO_QUERY);
+        uno::Reference<chart2::data::XDataSource> xDSrc(ds, uno::UNO_QUERY);
         CPPUNIT_ASSERT(xDSrc.is());
-        uno::Sequence<Reference<chart2::data::XLabeledDataSequence> > aDataSeqs = xDSrc->getDataSequences();
-        for (sal_Int32 j = 0; j < aDataSeqs.getLength(); ++j)
+        const uno::Sequence<Reference<chart2::data::XLabeledDataSequence> > aDataSeqs = xDSrc->getDataSequences();
+        for (auto const & lds : aDataSeqs)
         {
-            Reference<chart2::data::XDataSequence> xValues = aDataSeqs[j]->getValues();
+            Reference<chart2::data::XDataSequence> xValues = lds->getValues();
             CPPUNIT_ASSERT(xValues.is());
             Reference<beans::XPropertySet> xPropSet(xValues, uno::UNO_QUERY);
             if (!xPropSet.is())
@@ -457,7 +457,7 @@ std::vector<uno::Sequence<uno::Any> > getDataSeriesLabelsFromChartType( const Re
             xPropSet->getPropertyValue("Role") >>= aRoleName;
             if (aRoleName == aLabelRole)
             {
-                Reference<chart2::data::XLabeledDataSequence> xLabel = aDataSeqs[j];
+                Reference<chart2::data::XLabeledDataSequence> xLabel = lds;
                 CPPUNIT_ASSERT(xLabel.is());
                 Reference<chart2::data::XDataSequence> xDS2 = xLabel->getLabel();
                 CPPUNIT_ASSERT(xDS2.is());
@@ -544,10 +544,10 @@ OUString getTitleString( const Reference<chart2::XTitled>& xTitled )
 {
     uno::Reference<chart2::XTitle> xTitle = xTitled->getTitleObject();
     CPPUNIT_ASSERT(xTitle.is());
-    uno::Sequence<uno::Reference<chart2::XFormattedString> > aFSSeq = xTitle->getText();
+    const uno::Sequence<uno::Reference<chart2::XFormattedString> > aFSSeq = xTitle->getText();
     OUString aText;
-    for (sal_Int32 i = 0; i < aFSSeq.getLength(); ++i)
-        aText += aFSSeq[i]->getString();
+    for (auto const & fs : aFSSeq)
+        aText += fs->getString();
 
     return aText;
 }

chart2/source/controller/chartapiwrapper/MinMaxLineWrapper.cxx
@@ -157,11 +157,10 @@ void SAL_CALL MinMaxLineWrapper::setPropertyValue( const OUString& rPropertyName
     Reference< beans::XPropertySet > xPropSet;
 
     Reference< chart2::XDiagram > xDiagram( m_spChart2ModelContact->getChart2Diagram() );
-    Sequence< Reference< chart2::XChartType > > aTypes(
+    const Sequence< Reference< chart2::XChartType > > aTypes(
             ::chart::DiagramHelper::getChartTypesFromDiagram( xDiagram ) );
-    for( sal_Int32 nN = 0; nN < aTypes.getLength(); nN++ )
+    for( Reference< chart2::XChartType > const & xType : aTypes )
     {
-        Reference< chart2::XChartType > xType( aTypes[nN] );
         if( xType->getChartType() == CHART2_SERVICE_NAME_CHARTTYPE_CANDLESTICK )
         {
             Reference< chart2::XDataSeriesContainer > xSeriesContainer(xType,uno::UNO_QUERY);
@@ -195,11 +194,10 @@ uno::Any SAL_CALL MinMaxLineWrapper::getPropertyValue( const OUString& rProperty
     Reference< beans::XPropertySet > xPropSet;
 
     Reference< chart2::XDiagram > xDiagram( m_spChart2ModelContact->getChart2Diagram() );
-    Sequence< Reference< chart2::XChartType > > aTypes(
+    const Sequence< Reference< chart2::XChartType > > aTypes(
             ::chart::DiagramHelper::getChartTypesFromDiagram( xDiagram ) );
-    for( sal_Int32 nN = 0; nN < aTypes.getLength(); nN++ )
+    for( Reference< chart2::XChartType > const & xType : aTypes )
     {
-        Reference< chart2::XChartType > xType( aTypes[nN] );
         if( xType->getChartType() == CHART2_SERVICE_NAME_CHARTTYPE_CANDLESTICK )
         {
             Reference< chart2::XDataSeriesContainer > xSeriesContainer(xType,uno::UNO_QUERY);
@@ -344,18 +342,16 @@ uno::Any SAL_CALL MinMaxLineWrapper::getPropertyDefault( const OUString& rProper
 void SAL_CALL MinMaxLineWrapper::setAllPropertiesToDefault(  )
 {
     const Sequence< beans::Property >& rPropSeq = *StaticMinMaxLineWrapperPropertyArray::get();
-    for(sal_Int32 nN=0; nN<rPropSeq.getLength(); nN++)
+    for(beans::Property const & prop : rPropSeq)
     {
-        OUString aPropertyName( rPropSeq[nN].Name );
-        setPropertyToDefault( aPropertyName );
+        setPropertyToDefault( prop.Name );
     }
 }
 void SAL_CALL MinMaxLineWrapper::setPropertiesToDefault( const uno::Sequence< OUString >& rNameSeq )
 {
-    for(sal_Int32 nN=0; nN<rNameSeq.getLength(); nN++)
+    for(OUString const & s : rNameSeq)
     {
-        OUString aPropertyName( rNameSeq[nN] );
-        setPropertyToDefault( aPropertyName );
+        setPropertyToDefault( s );
     }
 }
 uno::Sequence< uno::Any > SAL_CALL MinMaxLineWrapper::getPropertyDefaults( const uno::Sequence< OUString >& rNameSeq )

chart2/source/controller/chartapiwrapper/TitleWrapper.cxx
@@ -90,12 +90,12 @@ Any WrappedTitleStringProperty::getPropertyValue( const Reference< beans::XPrope
     Reference< chart2::XTitle > xTitle(xInnerPropertySet,uno::UNO_QUERY);
     if(xTitle.is())
     {
-        Sequence< Reference< chart2::XFormattedString > > aStrings( xTitle->getText());
+        const Sequence< Reference< chart2::XFormattedString > > aStrings( xTitle->getText());
 
         OUStringBuffer aBuf;
-        for( sal_Int32 i = 0; i < aStrings.getLength(); ++i )
+        for( Reference< chart2::XFormattedString > const & formattedStr : aStrings )
         {
-            aBuf.append( aStrings[ i ]->getString());
+            aBuf.append( formattedStr->getString());
         }
         aRet <<= aBuf.makeStringAndClear();
     }
@@ -311,12 +311,12 @@ void TitleWrapper::setFastCharacterPropertyValue(
     if( !xTitle.is())
         return;
 
-    Sequence< Reference< chart2::XFormattedString > > aStrings( xTitle->getText());
+    const Sequence< Reference< chart2::XFormattedString > > aStrings( xTitle->getText());
     const WrappedProperty* pWrappedProperty = getWrappedProperty( nHandle );
 
-    for( sal_Int32 i = 0; i < aStrings.getLength(); ++i )
+    for( Reference< chart2::XFormattedString > const & formattedStr : aStrings )
     {
-        Reference< beans::XFastPropertySet > xFastPropertySet( aStrings[ i ], uno::UNO_QUERY );
+        Reference< beans::XFastPropertySet > xFastPropertySet( formattedStr, uno::UNO_QUERY );
         Reference< beans::XPropertySet > xPropSet( xFastPropertySet, uno::UNO_QUERY );
 
         if( pWrappedProperty )

chart2/source/controller/chartapiwrapper/UpDownBarWrapper.cxx
@@ -162,14 +162,13 @@ void SAL_CALL UpDownBarWrapper::setPropertyValue( const OUString& rPropertyName,
 {
     Reference< beans::XPropertySet > xPropSet;
 
-    Sequence< Reference< chart2::XChartType > > aTypes(
+    const Sequence< Reference< chart2::XChartType > > aTypes(
             ::chart::DiagramHelper::getChartTypesFromDiagram( m_spChart2ModelContact->getChart2Diagram() ) );
-    for( sal_Int32 nN = 0; nN < aTypes.getLength(); nN++ )
+    for( Reference< chart2::XChartType > const & xType : aTypes )
     {
-        Reference< chart2::XChartType > xType( aTypes[nN] );
         if( xType->getChartType() == CHART2_SERVICE_NAME_CHARTTYPE_CANDLESTICK )
         {
-            Reference< beans::XPropertySet > xTypeProps( aTypes[nN], uno::UNO_QUERY );
+            Reference< beans::XPropertySet > xTypeProps( xType, uno::UNO_QUERY );
             if(xTypeProps.is())
             {
                 xTypeProps->getPropertyValue( m_aPropertySetName ) >>= xPropSet;
@@ -185,14 +184,13 @@ uno::Any SAL_CALL UpDownBarWrapper::getPropertyValue( const OUString& rPropertyN
 
     Reference< beans::XPropertySet > xPropSet;
 
-    Sequence< Reference< chart2::XChartType > > aTypes(
+    const Sequence< Reference< chart2::XChartType > > aTypes(
             ::chart::DiagramHelper::getChartTypesFromDiagram( m_spChart2ModelContact->getChart2Diagram() ) );
-    for( sal_Int32 nN = 0; nN < aTypes.getLength(); nN++ )
+    for( Reference< chart2::XChartType > const & xType : aTypes )
     {
-        Reference< chart2::XChartType > xType( aTypes[nN] );
         if( xType->getChartType() == CHART2_SERVICE_NAME_CHARTTYPE_CANDLESTICK )
         {
-            Reference< beans::XPropertySet > xTypeProps( aTypes[nN], uno::UNO_QUERY );
+            Reference< beans::XPropertySet > xTypeProps( xType, uno::UNO_QUERY );
             if(xTypeProps.is())
             {
                 xTypeProps->getPropertyValue( m_aPropertySetName ) >>= xPropSet;
@@ -311,18 +309,16 @@ uno::Any SAL_CALL UpDownBarWrapper::getPropertyDefault( const OUString& rPropert
 void SAL_CALL UpDownBarWrapper::setAllPropertiesToDefault(  )
 {
     const Sequence< beans::Property >& rPropSeq = *StaticUpDownBarWrapperPropertyArray::get();
-    for(sal_Int32 nN=0; nN<rPropSeq.getLength(); nN++)
+    for(beans::Property const & prop : rPropSeq)
     {
-        OUString aPropertyName( rPropSeq[nN].Name );
-        setPropertyToDefault( aPropertyName );
+        setPropertyToDefault( prop.Name );
     }
 }
 void SAL_CALL UpDownBarWrapper::setPropertiesToDefault( const uno::Sequence< OUString >& rNameSeq )
 {
-    for(sal_Int32 nN=0; nN<rNameSeq.getLength(); nN++)
+    for(OUString const & s : rNameSeq)
     {
-        OUString aPropertyName( rNameSeq[nN] );
-        setPropertyToDefault( aPropertyName );
+        setPropertyToDefault( s );
     }
 }
 uno::Sequence< uno::Any > SAL_CALL UpDownBarWrapper::getPropertyDefaults( const uno::Sequence< OUString >& rNameSeq )

chart2/source/controller/chartapiwrapper/WrappedGapwidthProperty.cxx
@@ -71,12 +71,12 @@ void WrappedBarPositionProperty_Base::setPropertyValue( const Any& rOuterValue, 
     if( m_nDimensionIndex!=1 )
         return;
 
-    Sequence< Reference< chart2::XChartType > > aChartTypeList( DiagramHelper::getChartTypesFromDiagram( xDiagram ) );
-    for( sal_Int32 nN = 0; nN < aChartTypeList.getLength(); nN++ )
+    const Sequence< Reference< chart2::XChartType > > aChartTypeList( DiagramHelper::getChartTypesFromDiagram( xDiagram ) );
+    for( Reference< chart2::XChartType > const & chartType : aChartTypeList )
     {
         try
         {
-            Reference< beans::XPropertySet > xProp( aChartTypeList[nN], uno::UNO_QUERY );
+            Reference< beans::XPropertySet > xProp( chartType, uno::UNO_QUERY );
             if( xProp.is() )
             {
                 Sequence< sal_Int32 > aBarPositionSequence;

commit 013282b1421c0bba66ef54ccbfe4a649a4b97628

tdf#160522 Chart > 4 columns, Only 4 colors
regression from
    commit 337a9a454c1bc95214111578d3f9c0622c55c509
    Author: Noel Grandin <noel.grandin@collabora.co.uk>
    Date:   Mon May 18 09:17:04 2020 +0200
    use for-range on Sequence in chart2

Change-Id: I94f9460e45e5910af1de905605158f9dc1dbedfb
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/168868
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

chart2/source/model/template/ChartTypeTemplate.cxx
@@ -220,7 +220,8 @@ void ChartTypeTemplate::changeDiagram( const rtl::Reference< Diagram >& xDiagram
             for( auto const & j : i )
             {
                 if( nIndex >= nFormerSeriesCount )
-                    lcl_applyDefaultStyle( j, nIndex++, xDiagram );
+                    lcl_applyDefaultStyle( j, nIndex, xDiagram );
+                nIndex++;
             }
 
         // remove charttype groups from all coordinate systems