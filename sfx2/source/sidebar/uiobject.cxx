8a10012039204b208aea3bc2f9da9fe1162c4a2a
Michael Weghorn <m.weghorn@posteo.de>
2024-06-17 13:26:18 +0200

    tdf#159835 sfx2: Use reference instead of pointer
    This e.g. makes it clear that this is never null.
    Also use an `m` prefix for the class member.
    
    Change-Id: Ia66245f17e7f0ca9e57750e8606c8ccc1387eb97
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/169004
    Tested-by: Jenkins
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>

--- sfx2/source/sidebar/uiobject.cxx
+++ sfx2/source/sidebar/uiobject.cxx
@@ -40,7 +40,7 @@ void TabBarUIObject::execute(const OUString& rAction, const StringMap& rParamete
     if (rAction == "CLICK")
     {
         if (rParameters.find(u"POS"_ustr) != rParameters.end())
-            mxTabBar->pParentSidebarController->OpenThenToggleDeck(
+            mxTabBar->mrParentSidebarController.OpenThenToggleDeck(
                 mxTabBar->GetDeckIdForIndex(rParameters.find(u"POS"_ustr)->second.toInt32()));
     }
     else
