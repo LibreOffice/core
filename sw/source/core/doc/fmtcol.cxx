4499e123614be1de2c021c0c4f5f80d5c3e18401
Miklos Vajna <vmiklos@collabora.com>
2024-06-21 16:22:56 +0200

    sw doc model xml dump: show parent of para styles
    At some stage needs an SwFormat::dumpAsXml() but that's not yet done
    here.
    
    Change-Id: I10d2e0f653fc6d0e351bc0aa17f98de8f01766fb
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/169337
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Tested-by: Jenkins

--- sw/source/core/doc/fmtcol.cxx
+++ sw/source/core/doc/fmtcol.cxx
@@ -551,6 +551,11 @@ void SwTextFormatColl::dumpAsXml(xmlTextWriterPtr pWriter) const
     (void)xmlTextWriterWriteFormatAttribute(pWriter, BAD_CAST("ptr"), "%p", this);
     (void)xmlTextWriterWriteFormatAttribute(pWriter, BAD_CAST("symbol"), "%s", BAD_CAST(typeid(*this).name()));
     (void)xmlTextWriterWriteAttribute(pWriter, BAD_CAST("name"), BAD_CAST(GetName().toUtf8().getStr()));
+    if (SwFormat* pDerivedFrom = DerivedFrom())
+    {
+        (void)xmlTextWriterWriteAttribute(pWriter, BAD_CAST("derived-from"),
+                                          BAD_CAST(pDerivedFrom->GetName().toUtf8().getStr()));
+    }
     if (mpNextTextFormatColl)
     {
         (void)xmlTextWriterWriteAttribute(
