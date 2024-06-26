LibreOffice/core

commit 534d3818aedfa95ad73935235462f5ec2817f5da

tdf#160897 sw: layout: fail SwTabFrame::Split on footnote overlap
The bugdoc has a table in a section, and the cells contain many
footnotes.

Somehow commit c303981cfd95ce1c3881366023d5495ae2edce97 introduced some
layout looping, and it also looks worse than in 7.5.

During lcl_RecalcRow(rLastLine) it may happen that first some footnotes
are moved off the page, then the table/section grow during text
formatting, then the footnotes are moved back onto the page, so the
footnote container is the same size as before, the body is the same size
as before (because it shrinks whenever the footnote container grows),
but the section and table don't fit into the body any more.

This fixes the layout loops, and also there are no visible overlaps of
body(table) and footnotes any more; however, the layout is obviously not
perfect, many footnotes are on the wrong page and there are some gaps of
empty space on some pages, but at least none of the footnotes are
numbered "0" any more.

In 7.5, there was overlap of the body and the footnotes on page 1 and 3,
and also the irritating effect that moving the mouse over
the bottom of page 3 would relayout the document multiple times...

Some other ideas that don't work:

* it doesn't help to call Shrink() on the section frame, because it has
  ToMaximize() and does not actually shrink.
* one aspect of the loop is that the section is always size-invalidated
  even when it doesn't change its size during SwTabFormat::MakeAll()
  and the idea was to detect this (invalidation flag set on upper but
  same area as before) and reset the flag, but this prevents shrinking
  the section to fit on the page

Change-Id: I6074b5c2615a0d7f613edebe92b5350efdd7fe02
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/167693
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/core/layout/tabfrm.cxx
@@ -760,6 +760,9 @@ static bool lcl_RecalcSplitLine( SwRowFrame& rLastLine, SwRowFrame& rFollowLine,
     // #i26945# - include check, if objects fit
     const SwTwips nDistanceToUpperPrtBottom =
         aRectFnSet.BottomDist(rTab.getFrameArea(), aRectFnSet.GetPrtBottom(*rTab.GetUpper()));
+    // also check the footnote boss - it *may* be smaller than the upper now!
+    const SwTwips nDistanceToFootnoteBodyPrtBottom =
+        aRectFnSet.BottomDist(rTab.getFrameArea(), aRectFnSet.GetPrtBottom(*rTab.FindFootnoteBossFrame()->FindBodyCont()));
     // tdf#125685 ignore footnotes that are anchored in follow-table of this
     // table - if split is successful they move to the next page/column anyway
     assert(rTab.GetFollow() == rFollowLine.GetUpper());
@@ -802,6 +805,14 @@ static bool lcl_RecalcSplitLine( SwRowFrame& rLastLine, SwRowFrame& rFollowLine,
     }
     if (nDistanceToUpperPrtBottom + nFollowFootnotes < 0 || !rTab.DoesObjsFit())
         bRet = false;
+
+    // apparently checking nFootnoteHeight here does *not* guarantee that it fits into the body
+    if (bRet && nDistanceToFootnoteBodyPrtBottom + nFollowFootnotes < 0)
+    {
+        assert(rTab.GetUpper() != rTab.FindFootnoteBossFrame()->FindBodyCont());
+        SAL_INFO("sw.layout", "SwTabFrame Split failed because of footnote growth");
+        bRet = false; // tdf#160897
+    }
 
     // 2. Check if each cell in the last line has at least one content frame.
 

commit 68e7413b5f9ce4abd2a4e23bf27cfc335e52f03f

tdf#156724 sw: layout: ignore footnotes when splitting float table row
(regression from commit 534d3818aedfa95ad73935235462f5ec2817f5da)

Change-Id: I169d88375a93c288604a89ef89f7e895830446c8
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/169570
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/core/layout/tabfrm.cxx
@@ -807,7 +807,8 @@ static bool lcl_RecalcSplitLine( SwRowFrame& rLastLine, SwRowFrame& rFollowLine,
         bRet = false;
 
     // apparently checking nFootnoteHeight here does *not* guarantee that it fits into the body
-    if (bRet && nDistanceToFootnoteBodyPrtBottom + nFollowFootnotes < 0)
+    if (bRet && rTab.IsInDocBody()
+        && nDistanceToFootnoteBodyPrtBottom + nFollowFootnotes < 0)
     {
         assert(rTab.GetUpper() != rTab.FindFootnoteBossFrame()->FindBodyCont());
         SAL_INFO("sw.layout", "SwTabFrame Split failed because of footnote growth");

sw/qa/extras/layout/layout3.cxx
@@ -2290,6 +2290,18 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter3, testTdf138124)
                 1);
 }
 
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter3, TestTdf161348)
+{
+    createSwDoc("fdo48718-1.docx");
+
+    xmlDocUniquePtr pXml = parseLayoutDump();
+
+    // the floating table is on page 1
+    // apparently both parts of the split table are on this text frame
+    assertXPath(pXml, "/root/page[1]/body/txt[2]/anchored/fly"_ostr, 2);
+    assertXPath(pXml, "/root/page[1]/body/txt[2]/anchored/fly/tab"_ostr, 2);
+}
+
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter3, testTdf154113)
 {
     createSwDoc("three_sections.fodt");
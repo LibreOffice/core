68e7413b5f9ce4abd2a4e23bf27cfc335e52f03f
Michael Stahl <michael.stahl@allotropia.de>
2024-06-26 15:44:44 +0200

    tdf#156724 sw: layout: ignore footnotes when splitting float table row
    (regression from commit 534d3818aedfa95ad73935235462f5ec2817f5da)
    
    Change-Id: I169d88375a93c288604a89ef89f7e895830446c8
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/169570
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

--- sw/qa/extras/layout/layout3.cxx
+++ sw/qa/extras/layout/layout3.cxx
@@ -2290,6 +2290,18 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter3, testTdf138124)
                 1);
 }
 
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter3, TestTdf161348)
+{
+    createSwDoc("fdo48718-1.docx");
+
+    xmlDocUniquePtr pXml = parseLayoutDump();
+
+    // the floating table is on page 1
+    // apparently both parts of the split table are on this text frame
+    assertXPath(pXml, "/root/page[1]/body/txt[2]/anchored/fly"_ostr, 2);
+    assertXPath(pXml, "/root/page[1]/body/txt[2]/anchored/fly/tab"_ostr, 2);
+}
+
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter3, testTdf154113)
 {
     createSwDoc("three_sections.fodt");
