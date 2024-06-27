LibreOffice/core

commit 1f93816dd5d75aca98f210017d0b58b7ef1d08da

loplugin:stringadd in sw
when applying my upcoming patch to also consider O[U]StringBuffer

Change-Id: I0999a0b04236c276d49c541bbbe3932ba3e5e05d
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/149678
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sw/source/ui/misc/glossary.cxx
@@ -85,8 +85,7 @@ static OUString lcl_GetValidShortCut( const OUString& rName )
     while( rName[nStart-1]==' ' && nStart < nSz )
         nStart++;
 
-    OUStringBuffer aBuf;
-    aBuf.append(rName[nStart-1]);
+    OUStringBuffer aBuf(OUString::number(rName[nStart-1]));
 
     for( ; nStart < nSz; ++nStart )
     {

sw/qa/extras/tiledrendering/tiledrendering.cxx
@@ -3085,8 +3085,8 @@ CPPUNIT_TEST_FIXTURE(SwTiledRenderingTest, testDropDownFormFieldButton)
         OStringBuffer aItemList;
         for (auto &item : aItems)
         {
-            aItemList.append(item.second.get_value<std::string>().c_str());
-            aItemList.append(";");
+            aItemList.append(item.second.get_value<std::string>().c_str()
+                + OString::Concat(";"));
         }
         CPPUNIT_ASSERT_EQUAL(OString("2019/2020;2020/2021;2021/2022;2022/2023;2023/2024;2024/2025;"), aItemList.toString());
 

sw/source/core/crsr/crsrsh.cxx
@@ -1330,14 +1330,14 @@ OUString SwCursorShell::getPageRectangles()
     OUStringBuffer aBuf;
     for (const SwFrame* pFrame = pLayout->GetLower(); pFrame; pFrame = pFrame->GetNext())
     {
-        aBuf.append(pFrame->getFrameArea().Left());
-        aBuf.append(", ");
-        aBuf.append(pFrame->getFrameArea().Top());
-        aBuf.append(", ");
-        aBuf.append(pFrame->getFrameArea().Width());
-        aBuf.append(", ");
-        aBuf.append(pFrame->getFrameArea().Height());
-        aBuf.append("; ");
+        aBuf.append(OUString::number(pFrame->getFrameArea().Left())
+            + ", "
+            + OUString::number(pFrame->getFrameArea().Top())
+            + ", "
+            + OUString::number(pFrame->getFrameArea().Width())
+            + ", "
+            + OUString::number(pFrame->getFrameArea().Height())
+            + "; ");
     }
     if (!aBuf.isEmpty())
         aBuf.setLength( aBuf.getLength() - 2); // remove the last "; "

sw/source/core/doc/dbgoutsw.cxx
@@ -298,9 +298,7 @@ static OUString lcl_dbg_out(const SwpHints & rHints)
 
     for (size_t i = 0; i < rHints.Count(); ++i)
     {
-        aStr.append("  ");
-        aStr.append(lcl_dbg_out(*rHints.Get(i)));
-        aStr.append("\n");
+        aStr.append("  " + lcl_dbg_out(*rHints.Get(i)) + "\n");
     }
 
     aStr.append("]\n");
@@ -647,8 +645,7 @@ static OUString lcl_dbg_out(SwOutlineNodes const & rNodes)
 
     for (size_t i = 0; i < rNodes.size(); i++)
     {
-        aStr.append(lcl_dbg_out(*rNodes[i]));
-        aStr.append("\n");
+        aStr.append(lcl_dbg_out(*rNodes[i]) + "\n");
     }
 
     aStr.append("]\n");
@@ -669,10 +666,7 @@ static OUString lcl_dbg_out(const SvxNumberFormat & rFormat)
 
 static OUString lcl_dbg_out(const SwNumRule & rRule)
 {
-    OUStringBuffer aResult("[ ");
-
-    aResult.append(rRule.GetName());
-    aResult.append(" [");
+    OUStringBuffer aResult("[ " + rRule.GetName() + " [");
 
     for (sal_uInt8 n = 0; n < MAXLEVEL; n++)
     {
@@ -682,9 +676,7 @@ static OUString lcl_dbg_out(const SwNumRule & rRule)
         aResult.append(lcl_dbg_out(rRule.Get(n)));
     }
 
-    aResult.append("]");
-
-    aResult.append("]");
+    aResult.append("]]");
 
     return aResult.makeStringAndClear();
 }
@@ -792,9 +784,7 @@ static OUString lcl_dbg_out(const SwFormTokens & rTokens)
         if (aIt != rTokens.begin())
             aStr.append(", ");
 
-        aStr.append(lcl_TokenType2Str(aIt->eTokenType));
-        aStr.append(": ");
-        aStr.append(lcl_dbg_out(*aIt));
+        aStr.append(lcl_TokenType2Str(aIt->eTokenType) + ": " + lcl_dbg_out(*aIt));
     }
 
     aStr.append("]");

sw/source/core/fields/reffld.cxx
@@ -404,8 +404,7 @@ static OUString lcl_formatStringByCombiningCharacter(std::u16string_view sText, 
     OUStringBuffer sRet(sText.size() * 2);
     for (size_t i = 0; i < sText.size(); ++i)
     {
-        sRet.append(sText[i]);
-        sRet.append(cChar);
+        sRet.append(OUStringChar(sText[i]) + OUStringChar(cChar));
     }
     return sRet.makeStringAndClear();
 }

sw/source/core/layout/dbg_lay.cxx
@@ -675,8 +675,7 @@ void SwImplProtocol::Record_( const SwFrame* pFrame, PROT nFunction, DbgAction n
                             lcl_Start(aOut, m_aLayer, nAct);
                             if( pParam )
                             {
-                                aOut.append(' ');
-                                aOut.append(static_cast<sal_Int32>(*static_cast<sal_uInt16*>(pParam)));
+                                aOut.append(" " + OString::number(static_cast<sal_Int32>(*static_cast<sal_uInt16*>(pParam))));
                             }
                             break;
         case PROT::GrowTest:
@@ -703,8 +702,7 @@ void SwImplProtocol::Record_( const SwFrame* pFrame, PROT nFunction, DbgAction n
                             lcl_Start(aOut, m_aLayer, nAct);
                             if( pParam )
                             {
-                                aOut.append(' ');
-                                aOut.append(static_cast<sal_Int64>(*static_cast<tools::Long*>(pParam)));
+                                aOut.append(" " + OString::number(static_cast<sal_Int64>(*static_cast<tools::Long*>(pParam))));
                             }
                             break;
         case PROT::PrintArea:  aOut.append("PROT::PrintArea");
@@ -751,31 +749,31 @@ void SwImplProtocol::Record_( const SwFrame* pFrame, PROT nFunction, DbgAction n
                                 SwRect& rFrame = *static_cast<SwRect*>(pParam);
                                 if( pFrame->getFrameArea().Pos() != rFrame.Pos() )
                                 {
-                                    aOut.append("PosChg: (");
-                                    aOut.append(static_cast<sal_Int64>(rFrame.Left()));
-                                    aOut.append(", ");
-                                    aOut.append(static_cast<sal_Int64>(rFrame.Top()));
-                                    aOut.append(") -> (");
-                                    aOut.append(static_cast<sal_Int64>(pFrame->getFrameArea().Left()));
-                                    aOut.append(", ");
-                                    aOut.append(static_cast<sal_Int64>(pFrame->getFrameArea().Top()));
-                                    aOut.append(") ");
+                                    aOut.append("PosChg: ("
+                                        + OString::number(static_cast<sal_Int64>(rFrame.Left()))
+                                        + ", "
+                                        + OString::number(static_cast<sal_Int64>(rFrame.Top()))
+                                        + ") -> ("
+                                        + OString::number(static_cast<sal_Int64>(pFrame->getFrameArea().Left()))
+                                        + ", "
+                                        + OString::number(static_cast<sal_Int64>(pFrame->getFrameArea().Top()))
+                                        + ") ");
                                 }
                                 if( pFrame->getFrameArea().Height() != rFrame.Height() )
                                 {
-                                    aOut.append("Height: ");
-                                    aOut.append(static_cast<sal_Int64>(rFrame.Height()));
-                                    aOut.append(" -> ");
-                                    aOut.append(static_cast<sal_Int64>(pFrame->getFrameArea().Height()));
-                                    aOut.append(" ");
+                                    aOut.append("Height: "
+                                        + OString::number(static_cast<sal_Int64>(rFrame.Height()))
+                                        + " -> "
+                                        + OString::number(static_cast<sal_Int64>(pFrame->getFrameArea().Height()))
+                                        + " ");
                                 }
                                 if( pFrame->getFrameArea().Width() != rFrame.Width() )
                                 {
-                                    aOut.append("Width: ");
-                                    aOut.append(static_cast<sal_Int64>(rFrame.Width()));
-                                    aOut.append(" -> ");
-                                    aOut.append(static_cast<sal_Int64>(pFrame->getFrameArea().Width()));
-                                    aOut.append(' ');
+                                    aOut.append("Width: "
+                                        + OString::number(static_cast<sal_Int64>(rFrame.Width()))
+                                        + " -> "
+                                        + OString::number(static_cast<sal_Int64>(pFrame->getFrameArea().Width()))
+                                        + " ");
                                 }
                                 break;
                             }

sw/source/core/tox/ToxTextGenerator.cxx
@@ -451,8 +451,7 @@ ToxTextGenerator::ConstructPageNumberPlaceholder(size_t numberOfToxSources)
     // Place holder for the PageNumber; we only respect the first one
     retval.append(C_NUM_REPL);
     for (size_t i = 1; i < numberOfToxSources; ++i) {
-        retval.append(SwTOXMark::S_PAGE_DELI);
-        retval.append(C_NUM_REPL);
+        retval.append(SwTOXMark::S_PAGE_DELI + OUStringChar(C_NUM_REPL));
     }
     retval.append(C_END_PAGE_NUM);
     return retval.makeStringAndClear();

sw/source/core/txtnode/atrftn.cxx
@@ -205,8 +205,7 @@ OUString SwFormatFootnote::GetFootnoteText(SwRootFrame const& rLayout) const
             while ( !aIdx.GetNode().IsEndNode() ) {
                 if ( aIdx.GetNode().IsTextNode() )
                 {
-                    buf.append("  ");
-                    buf.append(aIdx.GetNode().GetTextNode()->GetExpandText(&rLayout));
+                    buf.append("  " + aIdx.GetNode().GetTextNode()->GetExpandText(&rLayout));
                 }
                 ++aIdx;
             }

commit 2ad283a8e7e474549328bd985c741b39d7cbd1d2

Fix autotext shortcut generation
The first character was replaced with a number.

Fallout from 1f93816dd5d75aca98f210017d0b58b7ef1d08da

Change-Id: I3b9a39336d5f6f3c10c85231f86fd196a29e3644

sw/source/ui/misc/glossary.cxx
@@ -85,7 +85,8 @@ static OUString lcl_GetValidShortCut( const OUString& rName )
     while( rName[nStart-1]==' ' && nStart < nSz )
         nStart++;
 
-    OUStringBuffer aBuf(OUString::number(rName[nStart-1]));
+    OUStringBuffer aBuf;
+    aBuf.append(rName[nStart-1]);
 
     for( ; nStart < nSz; ++nStart )
     {