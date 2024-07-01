a4cdd8f9581d5f61e122039b9388d83609d4ecef
Caolán McNamara <caolan.mcnamara@collabora.com>
2024-07-01 11:43:51 +0100

    crashtesting: divide by zero
    since:
    
    commit 7b807a6b793198c00db9b2c2c3a5eb3ebfe16ad9
    Date:   Fri Jun 21 12:28:30 2024 +0200
    
        tdf#161086 bitmap location wrong with SVG with an embedded PNG
    
    bodge it by using the requested width/height and not the possibly
    zero on fail actual sizes
    
    Change-Id: Ic55ba6ce3cc14fa598299cdd1f844ee7c63fb174
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/169800
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Tested-by: Jenkins
    (cherry picked from commit af2175bb87b8e8a7184916e110e2b22ec542357d)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/169815
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- drawinglayer/source/processor2d/vclpixelprocessor2d.cxx
+++ drawinglayer/source/processor2d/vclpixelprocessor2d.cxx
@@ -1092,8 +1092,7 @@ void VclPixelProcessor2D::processPatternFillPrimitive2D(
         mpOutputDevice->IntersectClipRegion(vcl::Region(aMask));
         Wallpaper aWallpaper(aTileImage);
         aWallpaper.SetColor(COL_TRANSPARENT);
-        Point aPaperPt(aMaskRect.getX() % aTileImage.GetSizePixel().Width(),
-                       aMaskRect.getY() % aTileImage.GetSizePixel().Height());
+        Point aPaperPt(aMaskRect.getX() % nTileWidth, aMaskRect.getY() % nTileHeight);
         tools::Rectangle aPaperRect(aPaperPt, aTileImage.GetSizePixel());
         aWallpaper.SetRect(aPaperRect);
         mpOutputDevice->DrawWallpaper(aMaskRect, aWallpaper);
@@ -1122,8 +1121,7 @@ void VclPixelProcessor2D::processPatternFillPrimitive2D(
     {
         Wallpaper aWallpaper(aTileImage);
         aWallpaper.SetColor(COL_TRANSPARENT);
-        Point aPaperPt(aMaskRect.getX() % aTileImage.GetSizePixel().Width(),
-                       aMaskRect.getY() % aTileImage.GetSizePixel().Height());
+        Point aPaperPt(aMaskRect.getX() % nTileWidth, aMaskRect.getY() % nTileHeight);
         tools::Rectangle aPaperRect(aPaperPt, aTileImage.GetSizePixel());
         aWallpaper.SetRect(aPaperRect);
         mpOutputDevice->DrawWallpaper(aMaskRect, aWallpaper);
