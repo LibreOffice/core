LibreOffice/core

commit d7fd378b533c42f51d1d363f5da30b1fa1281f67

Harmonization of warning and error colors
* hard-coded colors from infobar moved to settings
* weld::LabelType::Warning and ::Error uses these colors
* gtk3 native configuration removed

Change-Id: Ia80584e9267b8385f7f6b25322f5a85a2570af68
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/165161
Tested-by: Jenkins
Reviewed-by: Heiko Tietze <heiko.tietze@documentfoundation.org>

sfx2/source/dialog/infobar.cxx
@@ -44,6 +44,8 @@ void GetInfoBarColors(InfobarType ibType, BColor& rBackgroundColor, BColor& rFor
 {
     rMessageColor = basegfx::BColor(0.0, 0.0, 0.0);
 
+    const StyleSettings& rSettings = Application::GetSettings().GetStyleSettings();
+
     switch (ibType)
     {
         case InfobarType::INFO: // blue; #004785/0,71,133; #BDE5F8/189,229,248
@@ -55,17 +57,15 @@ void GetInfoBarColors(InfobarType ibType, BColor& rBackgroundColor, BColor& rFor
             rForegroundColor = basegfx::BColor(0.196, 0.333, 0.047);
             break;
         case InfobarType::WARNING: // orange; #704300/112,67,0; #FEEFB3/254,239,179
-            rBackgroundColor = basegfx::BColor(0.996, 0.937, 0.702);
-            rForegroundColor = basegfx::BColor(0.439, 0.263, 0.0);
+            rBackgroundColor = rSettings.GetWarningColor().getBColor();
+            rForegroundColor = rSettings.GetWarningTextColor().getBColor();
             break;
         case InfobarType::DANGER: // red; #7A0006/122,0,6; #FFBABA/255,186,186
-            rBackgroundColor = basegfx::BColor(1.0, 0.729, 0.729);
-            rForegroundColor = basegfx::BColor(0.478, 0.0, 0.024);
+            rBackgroundColor = rSettings.GetErrorColor().getBColor();
+            rForegroundColor = rSettings.GetErrorTextColor().getBColor();
             break;
     }
 
-    //remove this?
-    const StyleSettings& rSettings = Application::GetSettings().GetStyleSettings();
     if (rSettings.GetHighContrastMode())
     {
         rBackgroundColor = rSettings.GetLightColor().getBColor();

include/vcl/settings.hxx
@@ -353,6 +353,15 @@ public:
     void                            SetWarningColor( const Color& rColor );
     const Color&                    GetWarningColor() const;
 
+    void                            SetWarningTextColor( const Color& rColor );
+    const Color&                    GetWarningTextColor() const;
+
+    void                            SetErrorColor( const Color& rColor );
+    const Color&                    GetErrorColor() const;
+
+    void                            SetErrorTextColor( const Color& rColor );
+    const Color&                    GetErrorTextColor() const;
+
     void                            SetHelpColor( const Color& rColor );
     const Color&                    GetHelpColor() const;
 

vcl/source/app/salvtables.cxx
@@ -5949,14 +5949,16 @@ void SalInstanceLabel::set_label_type(weld::LabelType eType)
             m_xLabel->SetControlBackground();
             break;
         case weld::LabelType::Warning:
-            m_xLabel->SetControlForeground();
+            m_xLabel->SetControlForeground(
+                m_xLabel->GetSettings().GetStyleSettings().GetWarningTextColor());
             m_xLabel->SetControlBackground(
                 m_xLabel->GetSettings().GetStyleSettings().GetWarningColor());
             break;
         case weld::LabelType::Error:
-            m_xLabel->SetControlForeground();
+            m_xLabel->SetControlForeground(
+                m_xLabel->GetSettings().GetStyleSettings().GetErrorTextColor());
             m_xLabel->SetControlBackground(
-                m_xLabel->GetSettings().GetStyleSettings().GetHighlightColor());
+                m_xLabel->GetSettings().GetStyleSettings().GetErrorColor());
             break;
         case weld::LabelType::Title:
             m_xLabel->SetControlForeground(

vcl/source/app/settings.cxx
@@ -137,6 +137,9 @@ struct ImplStyleData
     Color                           maRadioCheckTextColor;
     Color                           maShadowColor;
     Color                           maWarningColor;
+    Color                           maWarningTextColor;
+    Color                           maErrorColor;
+    Color                           maErrorTextColor;
     Color                           maVisitedLinkColor;
     Color                           maToolTextColor;
     Color                           maWindowColor;
@@ -562,6 +565,9 @@ ImplStyleData::ImplStyleData( const ImplStyleData& rData ) :
     maRadioCheckTextColor( rData.maRadioCheckTextColor ),
     maShadowColor( rData.maShadowColor ),
     maWarningColor( rData.maWarningColor ),
+    maWarningTextColor( rData.maWarningTextColor ),
+    maErrorColor( rData.maErrorColor ),
+    maErrorTextColor( rData.maErrorTextColor ),
     maVisitedLinkColor( rData.maVisitedLinkColor ),
     maToolTextColor( rData.maToolTextColor ),
     maWindowColor( rData.maWindowColor ),
@@ -662,7 +668,10 @@ void ImplStyleData::SetStandardStyles()
     maShadowColor               = COL_GRAY;
     maDarkShadowColor           = COL_BLACK;
 
-    maWarningColor              = COL_YELLOW;
+    maWarningColor              = Color(0xFE, 0xEF, 0xB3); // tdf#105829
+    maWarningTextColor          = Color(0x70, 0x43, 0x00);
+    maErrorColor                = Color(0xFF, 0xBA, 0xBA);
+    maErrorTextColor            = Color(0x7A, 0x00, 0x06);
 
     maDefaultButtonTextColor                      = COL_BLACK;
     maButtonTextColor                             = COL_BLACK;
@@ -812,6 +821,45 @@ const Color&
 StyleSettings::GetWarningColor() const
 {
     return mxData->maWarningColor;
+}
+
+void
+StyleSettings::SetWarningTextColor( const Color& rColor )
+{
+    CopyData();
+    mxData->maWarningTextColor = rColor;
+}
+
+const Color&
+StyleSettings::GetWarningTextColor() const
+{
+    return mxData->maWarningTextColor;
+}
+
+void
+StyleSettings::SetErrorColor( const Color& rColor )
+{
+    CopyData();
+    mxData->maErrorColor = rColor;
+}
+
+const Color&
+StyleSettings::GetErrorColor() const
+{
+    return mxData->maErrorColor;
+}
+
+void
+StyleSettings::SetErrorTextColor( const Color& rColor )
+{
+    CopyData();
+    mxData->maErrorTextColor = rColor;
+}
+
+const Color&
+StyleSettings::GetErrorTextColor() const
+{
+    return mxData->maErrorTextColor;
 }
 
 void
@@ -2520,6 +2568,9 @@ bool StyleSettings::operator ==( const StyleSettings& rSet ) const
          (mxData->maShadowColor             == rSet.mxData->maShadowColor)              &&
          (mxData->maDarkShadowColor         == rSet.mxData->maDarkShadowColor)          &&
          (mxData->maWarningColor            == rSet.mxData->maWarningColor)             &&
+         (mxData->maWarningTextColor        == rSet.mxData->maWarningTextColor)         &&
+         (mxData->maErrorColor              == rSet.mxData->maErrorColor)               &&
+         (mxData->maErrorTextColor          == rSet.mxData->maErrorTextColor)           &&
          (mxData->maButtonTextColor         == rSet.mxData->maButtonTextColor)          &&
          (mxData->maDefaultActionButtonTextColor == rSet.mxData->maDefaultActionButtonTextColor) &&
          (mxData->maActionButtonTextColor   == rSet.mxData->maActionButtonTextColor)    &&

vcl/unx/gtk3/gtkinst.cxx
@@ -17925,9 +17925,11 @@ public:
                 break;
             case weld::LabelType::Warning:
                 set_text_background_color(Application::GetSettings().GetStyleSettings().GetWarningColor());
+                set_text_foreground_color(Application::GetSettings().GetStyleSettings().GetWarningTextColor(), false);
                 break;
             case weld::LabelType::Error:
-                set_text_background_color(Application::GetSettings().GetStyleSettings().GetHighlightColor());
+                set_text_background_color(Application::GetSettings().GetStyleSettings().GetErrorColor());
+                set_text_foreground_color(Application::GetSettings().GetStyleSettings().GetErrorTextColor(), false);
                 break;
             case weld::LabelType::Title:
                 set_text_foreground_color(Application::GetSettings().GetStyleSettings().GetLightColor(), true);

vcl/unx/gtk3/salnativewidgets-gtk.cxx
@@ -2435,11 +2435,6 @@ bool GtkSalGraphics::updateSettings(AllSettings& rSettings)
         aStyleSet.SetActiveColor( aHighlightColor );
         aStyleSet.SetActiveTextColor( aHighlightTextColor );
 
-        // warning color
-        GdkRGBA warning_color;
-        if (gtk_style_context_lookup_color(pCStyle, "warning_color", &warning_color))
-            aStyleSet.SetWarningColor(getColor(warning_color));
-
         // field background color
         style_context_set_state(pCStyle, GTK_STATE_FLAG_NORMAL);
         ::Color aBackFieldColor = style_context_get_background_color(pCStyle);

commit c65127e5f6efcc5db93d2f80b9592f458200d596

tdf#158416: restore infobar text colours
...which were lost with de1b926fa294273caefb29a507e117b8487e882b.
rMessageColor is put back into the switch, but uses the settings' warning
and error colours introduced by d7fd378b533c42f51d1d363f5da30b1fa1281f67
where applicable.
Colours were checked for sufficient contrast in
https://gerrit.libreoffice.org/c/core/+/47825

Change-Id: Iba42020b665e35c34f09b3788bcc973b0f8531d1
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/168761
Tested-by: Jenkins
Reviewed-by: Heiko Tietze <heiko.tietze@documentfoundation.org>

sfx2/source/dialog/infobar.cxx
@@ -42,8 +42,6 @@ namespace
 void GetInfoBarColors(InfobarType ibType, BColor& rBackgroundColor, BColor& rForegroundColor,
                       BColor& rMessageColor)
 {
-    rMessageColor = basegfx::BColor(0.0, 0.0, 0.0);
-
     const StyleSettings& rSettings = Application::GetSettings().GetStyleSettings();
 
     switch (ibType)
@@ -51,18 +49,22 @@ void GetInfoBarColors(InfobarType ibType, BColor& rBackgroundColor, BColor& rFor
         case InfobarType::INFO: // blue; #004785/0,71,133; #BDE5F8/189,229,248
             rBackgroundColor = basegfx::BColor(0.741, 0.898, 0.973);
             rForegroundColor = basegfx::BColor(0.0, 0.278, 0.522);
+            rMessageColor = basegfx::BColor(0.0, 0.278, 0.522);
             break;
         case InfobarType::SUCCESS: // green; #32550C/50,85,12; #DFF2BF/223,242,191
             rBackgroundColor = basegfx::BColor(0.874, 0.949, 0.749);
             rForegroundColor = basegfx::BColor(0.196, 0.333, 0.047);
+            rMessageColor = basegfx::BColor(0.196, 0.333, 0.047);
             break;
         case InfobarType::WARNING: // orange; #704300/112,67,0; #FEEFB3/254,239,179
             rBackgroundColor = rSettings.GetWarningColor().getBColor();
             rForegroundColor = rSettings.GetWarningTextColor().getBColor();
+            rMessageColor = rSettings.GetWarningTextColor().getBColor();
             break;
         case InfobarType::DANGER: // red; #7A0006/122,0,6; #FFBABA/255,186,186
             rBackgroundColor = rSettings.GetErrorColor().getBColor();
             rForegroundColor = rSettings.GetErrorTextColor().getBColor();
+            rMessageColor = rSettings.GetErrorTextColor().getBColor();
             break;
     }
 