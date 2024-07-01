913e26de02067d49cdbe2746f9e3c3c9bed63236
Attila Szűcs <attila.szucs@collabora.com>
2024-07-01 08:48:43 +0200

    SW: transform document structure
    Added a new UNO command: .uno:TransformDocumentStructure
    it need a sring parameter conaining a JSON strucure, that
    list what to change (transform), like this:
    
    {
        "Transforms": {
            "ContentControls.ByIndex.0": {
                "content": "new value for the first content control"
            },
            "ContentControls.ByTag.name": {
                "content": "John Smith"
            }
        }
    }
    
    this is just a 1. version it can only set text of a plain-text
    content control,
    or the state of a checkbox,
    or the alias (i may will be removed .. we have to decide later
    about all setable propertyes of contentc controls, what to let
    transform)
    
    Change-Id: I1772d979a7eb43961d62896f3c393482ed7dfa89
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/170538
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Tested-by: Jenkins

--- sw/inc/cmdid.h
+++ sw/inc/cmdid.h
@@ -151,6 +151,7 @@ class SwUINumRuleItem;
 #define FN_REDLINE_REJECT_ALL        (FN_EDIT2 + 44)    /* Redlining Reject All*/
 #define FN_REDLINE_ACCEPT_TONEXT     (FN_EDIT2 + 45)    /* Redlining Accept and jump to next*/
 #define FN_REDLINE_REJECT_TONEXT     (FN_EDIT2 + 46)    /* Redlining Reject and jump to next*/
+#define FN_TRANSFORM_DOCUMENT_STRUCTURE (FN_EDIT2 + 47) /* overwrite text of content control, and more*/
 
 // Region: View
 #define FN_DRAW_WRAP_DLG        TypedWhichId<SfxInt16Item>(FN_VIEW + 3)   /* Draw wrapping dlg */
