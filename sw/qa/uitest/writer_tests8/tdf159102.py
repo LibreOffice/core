6eee182e46bb568a58620a02e5827532d15c8220
László Németh <nemeth@numbertext.org>
2024-07-02 02:44:58 +0200

    tdf#159102 sw: add missing get_url_for_data_file to the unit test
    Partial revert of commit 5fa3abd3b28b7c30d015dc058f7719112003f51e
    "tdf#158803 Remove unused imports from uitest".
    
    Follow-up to commit c6d7b7fa686b742c6ffef42aa6cc280358babe0d
    "tdf#159102 sw: enable unit test again with extended test document".
    
    Change-Id: Id6397d322f25f1b3e981d8e206a54bb79a54b831
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/169848
    Tested-by: Jenkins
    Reviewed-by: László Németh <nemeth@numbertext.org>

--- sw/qa/uitest/writer_tests8/tdf159102.py
+++ sw/qa/uitest/writer_tests8/tdf159102.py
@@ -7,7 +7,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 from uitest.framework import UITestCase
-from uitest.uihelper.common import get_state_as_dict
+from uitest.uihelper.common import get_state_as_dict, get_url_for_data_file
 from libreoffice.uno.propertyvalue import mkPropertyValues
 from libreoffice.linguistic.linguservice import get_lingu_service_manager
 
