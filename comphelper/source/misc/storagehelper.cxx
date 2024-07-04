ad80710678393efc68cd8fc94808492a841177e7
Michael Stahl <michael.stahl@allotropia.de>
2024-07-04 14:07:25 +0200

    comphelper: treat zip file path segments '.' and '..' as invalid
    This will prevent also opening with RepairPackage, would need to adapt
    ZipPackage::getZipFileContents() a bit, but let's hope nobody acutally
    has such files.
    
    Also treat path that starts with "/" as invalid, presumably it's not
    allowed by APPNOTE.TXT:
    "The name of the file, with optional relative path."
    
    Change-Id: Ic694ea2fb34f5de1d490a9a251cf56e4004e9673
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/169994
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
    Tested-by: Jenkins
    (cherry picked from commit 6005260078c126bf3f1cf4d6f1ebb631453f5ac7)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/169964
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
    (cherry picked from commit fda9ae6f1cef7512f0560828f67553b91641bd26)

--- comphelper/source/misc/storagehelper.cxx
+++ comphelper/source/misc/storagehelper.cxx
@@ -568,10 +568,17 @@ bool OStorageHelper::IsValidZipEntryFileName( const OUString& aName, bool bSlash
 bool OStorageHelper::IsValidZipEntryFileName(
     const sal_Unicode *pChar, sal_Int32 nLength, bool bSlashAllowed )
 {
+    long nDots{0};
     for ( sal_Int32 i = 0; i < nLength; i++ )
     {
         switch ( pChar[i] )
         {
+            case '.':
+                if (nDots != -1)
+                {
+                    ++nDots;
+                }
+                break;
             case '\\':
             case '?':
             case '<':
@@ -581,15 +588,17 @@ bool OStorageHelper::IsValidZipEntryFileName(
             case ':':
                 return false;
             case '/':
-                if ( !bSlashAllowed )
+                if (!bSlashAllowed || nDots == 1 || nDots == 2 || i == 0)
                     return false;
+                nDots = 0;
                 break;
             default:
+                nDots = -1;
                 if ( pChar[i] < 32  || (pChar[i] >= 0xD800 && pChar[i] <= 0xDFFF) )
                     return false;
         }
     }
-    return true;
+    return nDots != 1 && nDots != 2;
 }
 
 
