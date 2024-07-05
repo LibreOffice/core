7216a5a4ff2210e04732a37b16dbebeb5cce0e31
Xisco Fauli <xiscofauli@libreoffice.org>
2024-07-05 13:30:22 +0200

    CppunitTest_dbaccess_embeddeddb_performancetest: fix dependencies
    Otherwise it fails with
    
    EmbeddedDBPerformanceTest::testPerformance finished in: 426ms
    macros_test.cxx:74:Assertion
    Test name: EmbeddedDBPerformanceTest::testPerformance
    assertion failed
    - Expression: xComponent.is()
    
    After this patch, the tests still fail, but for another reason.
    
    Change-Id: I4fc11a9feef79efd71be8d6bab76f8777c92d115
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/170012
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Tested-by: Jenkins

--- dbaccess/CppunitTest_dbaccess_embeddeddb_performancetest.mk
+++ dbaccess/CppunitTest_dbaccess_embeddeddb_performancetest.mk
@@ -41,32 +41,7 @@ $(eval $(call gb_CppunitTest_use_api,dbaccess_embeddeddb_performancetest,\
 $(eval $(call gb_CppunitTest_use_ure,dbaccess_embeddeddb_performancetest))
 $(eval $(call gb_CppunitTest_use_vcl,dbaccess_embeddeddb_performancetest))
 
-$(eval $(call gb_CppunitTest_use_components,dbaccess_embeddeddb_performancetest,\
-    basic/util/sb \
-    comphelper/util/comphelp \
-    configmgr/source/configmgr \
-    connectivity/source/cpool/dbpool2 \
-    connectivity/source/drivers/firebird/firebird_sdbc \
-    connectivity/source/manager/sdbc2 \
-    dbaccess/util/dba \
-    dbaccess/source/filter/xml/dbaxml \
-    dbaccess/util/dbu \
-    filter/source/config/cache/filterconfig1 \
-    framework/util/fwk \
-    i18npool/util/i18npool \
-    linguistic/source/lng \
-    package/source/xstor/xstor \
-    package/util/package2 \
-    sax/source/expatwrap/expwrap \
-    sfx2/util/sfx \
-    svl/source/fsstor/fsstorage \
-    svl/util/svl \
-    toolkit/util/tk \
-    ucb/source/core/ucb1 \
-    ucb/source/ucp/file/ucpfile1 \
-    unotools/util/utl \
-    xmloff/util/xo \
-))
+$(eval $(call gb_CppunitTest_use_rdb,dbaccess_embeddeddb_performancetest,services))
 
 $(eval $(call gb_CppunitTest_use_configuration,dbaccess_embeddeddb_performancetest))
 
