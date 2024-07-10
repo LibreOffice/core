7958f5b111954632ca2792a0c3d18d79de5397c6
Rafael Lima <rafael.palma.lima@gmail.com>
2024-07-10 19:29:52 +0200

    tdf#161785 Fix order overlays are repainted
    Prior to this patch, the selection overlay was painted above the cursor when scrolling/resizing the window. This patch reorders it so that the selection overlay is always below the cursor.
    
    Change-Id: I5a9a66eeb6285b4a9b8954a44189ddcb6039d155
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/170299
    Reviewed-by: Heiko Tietze <heiko.tietze@documentfoundation.org>
    Tested-by: Jenkins
    Tested-by: Heiko Tietze <heiko.tietze@documentfoundation.org>
    (cherry picked from commit 2f113fe5b2aeb2b9ca7866d88c31c0cc09889e2a)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/170359
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- sc/source/ui/view/gridwin.cxx
+++ sc/source/ui/view/gridwin.cxx
@@ -6239,11 +6239,11 @@ void ScGridWindow::CursorChanged()
 
 void ScGridWindow::ImpCreateOverlayObjects()
 {
+    UpdateHighlightOverlay();
+    UpdateSelectionOverlay();
+    UpdateAutoFillOverlay();
     UpdateCursorOverlay();
     UpdateCopySourceOverlay();
-    UpdateSelectionOverlay();
-    UpdateHighlightOverlay();
-    UpdateAutoFillOverlay();
     UpdateDragRectOverlay();
     UpdateHeaderOverlay();
     UpdateShrinkOverlay();
