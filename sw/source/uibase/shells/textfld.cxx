5e3677e243f2b2e13da84e2fddb4f95782c5e3de
Justin Luth <jluth@mail.com>
2024-07-19 09:50:11 -0400

    tdf#164033 tdf#161741 tdf#161705 pgnum wizard: avoid undo crash (sledgehammer)
    I have zero knowledge of the many intricate pitfalls of undo/redo,
    so there is no way I could create a "proper" fix.
    
    LO 7.6 introduced the page number wizard which has more prominently
    exposed existing ways to make undo/redo of page style changes
    to crash. So treat this as pre-LO 7.2
    and simply erase the undo stack after running the wizard.
    ------------------------------------------------------------------
    The ability to undo/redo the "Header/footer change" started in
    bibisect-linux-64-7.2 commit 910d9a081daf11ea53235d227c3dc9623942a824
    commit 8d8486f43c1a8a51157bfc3e0b87090b05a9229e
    Author: Daniel Arato (NISZ) on Mon Feb 22 16:59:38 2021 +0100
        tdf#46561 sw: fix lost undo stack setting header/footer
    
    -   if( bHeaderFooterChanged )
    -       GetIDocumentUndoRedo().DelAllUndoObj();
    
    Change-Id: I00c529784bfd7b2cd1230474c05eea8516e6c1e3
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/170771
    Tested-by: Jenkins
    Tested-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
    (cherry picked from commit f84369daa125717f3c87fe34abb0b91c6f6ba3d5)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/184903
    Reviewed-by: Justin Luth <jluth@mail.com>

--- sw/source/uibase/shells/textfld.cxx
+++ sw/source/uibase/shells/textfld.cxx
@@ -1466,6 +1466,10 @@ FIELD_INSERT:
                 rSh.EndAllAction();
                 rSh.LockView(false);
                 rDoc.GetIDocumentUndoRedo().EndUndo(SwUndoId::INSERT_PAGE_NUMBER, nullptr);
+
+                // avoid various ways to crash related to undo of SwPageDesc (tdf#161741, tdf#161705)
+                if (bChangePageDesc)
+                    rDoc.GetIDocumentUndoRedo().DelAllUndoObj();
             }
             pDlg->disposeOnce();
         });
