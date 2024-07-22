LibreOffice/core

commit f98f684d22e6323185104ca9c082241c53dfc2b3

remove RecalcObjOrdNums in DoTextBoxZOrderCorrection
(1) We already do this recalculation automatically using a dirty flag.

(2) Add a custom method so we don't trigger that recalculation here

Speeds up display of a complex DOCX file load by 10%

Change-Id: I2a8c975ba0711172bc3e0c20c0e89f08351b6126
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/170431
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sw/source/core/doc/textboxhelper.cxx
@@ -1533,17 +1533,10 @@ bool SwTextBoxHelper::DoTextBoxZOrderCorrection(SwFrameFormat* pShape, const Sdr
             if (pDrawModel)
             {
                 // Not really sure this will work on all pages, but it seems it will.
-                auto pPage = pDrawModel->GetPage(0);
-                // Recalc all Z-orders
-                pPage->RecalcObjOrdNums();
                 // If the shape is behind the frame, is good, but if there are some objects
                 // between of them that is wrong so put the frame exactly one level higher
                 // than the shape.
-                if (pFrmObj->GetOrdNum() != pShpObj->GetOrdNum() + 1)
-                {
-                    pPage->SetObjectOrdNum(pFrmObj->GetOrdNum(), pShpObj->GetOrdNum() + 1);
-                    pPage->RecalcObjOrdNums();
-                }
+                pFrmObj->ensureSortedImmediatelyAfter(*pShpObj);
                 return true; // Success
             }
             SAL_WARN("sw.core", "SwTextBoxHelper::DoTextBoxZOrderCorrection(): "

include/svx/svdobj.hxx
@@ -376,6 +376,10 @@ public:
     /// SdrObjects in the SdrObjList.
     sal_uInt32 GetOrdNum() const;
 
+    /// Ensure this object is sorted immediatedly after rFirst
+    /// ie. rFirst.GetOrdNum() + 1 == this->GetOrdNum()
+    void ensureSortedImmediatelyAfter(const SdrObject& rFirst);
+
     // setting the order number should only happen from the model or from the page
     void SetOrdNum(sal_uInt32 nNum);
 

svx/source/svdraw/svdobj.cxx
@@ -888,6 +888,36 @@ void SdrObject::SetOrdNum(sal_uInt32 nNum)
     m_nOrdNum = nNum;
 }
 
+/// Try to ensure the desired result __without__ triggering RecalcObjOrdNums
+void SdrObject::ensureSortedImmediatelyAfter(const SdrObject& rFirst)
+{
+    SdrObjList* pParentList = getParentSdrObjListFromSdrObject();
+    assert(pParentList == rFirst.getParentSdrObjListFromSdrObject());
+    bool bDirty = pParentList->IsObjOrdNumsDirty();
+    if (!bDirty)
+    {
+        pParentList->SetObjectOrdNum(GetOrdNum(), rFirst.GetOrdNum() + 1);
+    }
+    else
+    {
+        std::optional<decltype(pParentList->begin())> itFound1, itFound2;
+        for (auto it = pParentList->begin(), itEnd = pParentList->end(); it != itEnd; ++it)
+        {
+            if (*it == this)
+                itFound1 = it;
+            else if (*it == &rFirst)
+                itFound2 = it;
+            if (itFound1 && itFound2)
+            {
+                auto ord1 = std::distance(pParentList->begin(), *itFound1);
+                auto ord2 = std::distance(pParentList->begin(), *itFound2);
+                pParentList->SetObjectOrdNum(ord1, ord2 + 1);
+                break;
+            }
+        }
+    }
+}
+
 void SdrObject::GetGrabBagItem(css::uno::Any& rVal) const
 {
     if (m_pGrabBagItem != nullptr)

commit f08fdf3e81e63469609eaafaf790cb3fea6b27c4

crashtesting tdf135164-3.docx
./instdir/program/soffice --headless --convert-to odt
~/Downloads/tdf135164-3.docx

fails after
    commit f98f684d22e6323185104ca9c082241c53dfc2b3
    Author: Noel Grandin <noel.grandin@collabora.co.uk>
    Date:   Sat Jul 13 18:50:34 2024 +0200
    remove RecalcObjOrdNums in DoTextBoxZOrderCorrection

add a check to catch this case and flatten the method to make it more
readable

Change-Id: I7827807c77f51b28f16b1042b77b8211ab5416ec
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/170839
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sw/source/core/doc/textboxhelper.cxx
@@ -1513,42 +1513,48 @@ bool SwTextBoxHelper::DoTextBoxZOrderCorrection(SwFrameFormat* pShape, const Sdr
 
     pShpObj = pShape->FindRealSdrObject();
 
-    if (pShpObj)
+    if (!pShpObj)
     {
-        auto pTextBox = getOtherTextBoxFormat(pShape, RES_DRAWFRMFMT, pObj);
-        if (!pTextBox)
-            return false;
-        SdrObject* pFrmObj = pTextBox->FindRealSdrObject();
-        if (!pFrmObj)
-        {
-            // During loading there is no ready SdrObj for z-ordering, so create and cache it here
-            pFrmObj
-                = SwXTextFrame::GetOrCreateSdrObject(*dynamic_cast<SwFlyFrameFormat*>(pTextBox));
-        }
-        if (pFrmObj)
-        {
-            // Get the draw model from the doc
-            SwDrawModel* pDrawModel
-                = pShape->GetDoc()->getIDocumentDrawModelAccess().GetDrawModel();
-            if (pDrawModel)
-            {
-                // Not really sure this will work on all pages, but it seems it will.
-                // If the shape is behind the frame, is good, but if there are some objects
-                // between of them that is wrong so put the frame exactly one level higher
-                // than the shape.
-                pFrmObj->ensureSortedImmediatelyAfter(*pShpObj);
-                return true; // Success
-            }
-            SAL_WARN("sw.core", "SwTextBoxHelper::DoTextBoxZOrderCorrection(): "
-                                "No Valid Draw model for SdrObject for the shape!");
-        }
+        SAL_WARN("sw.core", "SwTextBoxHelper::DoTextBoxZOrderCorrection(): "
+                            "No Valid SdrObject for the shape!");
+        return false;
+    }
+
+    auto pTextBox = getOtherTextBoxFormat(pShape, RES_DRAWFRMFMT, pObj);
+    if (!pTextBox)
+        return false;
+    SdrObject* pFrmObj = pTextBox->FindRealSdrObject();
+    if (!pFrmObj)
+    {
+        // During loading there is no ready SdrObj for z-ordering, so create and cache it here
+        pFrmObj = SwXTextFrame::GetOrCreateSdrObject(*dynamic_cast<SwFlyFrameFormat*>(pTextBox));
+    }
+    if (!pFrmObj)
+    {
         SAL_WARN("sw.core", "SwTextBoxHelper::DoTextBoxZOrderCorrection(): "
                             "No Valid SdrObject for the frame!");
+        return false;
     }
-    SAL_WARN("sw.core", "SwTextBoxHelper::DoTextBoxZOrderCorrection(): "
-                        "No Valid SdrObject for the shape!");
-
-    return false;
+    // Get the draw model from the doc
+    SwDrawModel* pDrawModel = pShape->GetDoc()->getIDocumentDrawModelAccess().GetDrawModel();
+    if (!pDrawModel)
+    {
+        SAL_WARN("sw.core", "SwTextBoxHelper::DoTextBoxZOrderCorrection(): "
+                            "No Valid Draw model for SdrObject for the shape!");
+        return false;
+    }
+    if (!pFrmObj->getParentSdrObjListFromSdrObject())
+    {
+        SAL_WARN("sw.core", "SwTextBoxHelper::DoTextBoxZOrderCorrection(): "
+                            "Frame object is not inserted into any parent");
+        return false;
+    }
+    // Not really sure this will work on all pages, but it seems it will.
+    // If the shape is behind the frame, is good, but if there are some objects
+    // between of them that is wrong so put the frame exactly one level higher
+    // than the shape.
+    pFrmObj->ensureSortedImmediatelyAfter(*pShpObj);
+    return true; // Success
 }
 
 void SwTextBoxHelper::synchronizeGroupTextBoxProperty(bool pFunc(SwFrameFormat*, SdrObject*),