LibreOffice/core

commit 5082d50d24c3fec4487c724a15eb0d54a82ecd0d

writerfilter: use content controls for text in block SDTs
Text inside block SDTs was shown as Text Fields wich ignored properties such as
alias and formatting. Now those texts are imported as content controls like in
the case of run SDTs.

Added the ability for content controls to remember and export the "multiline"
property of block SDT text.

Some existing tests have been changed due to some different export results.

Change-Id: Ice1eb4ca6dd53c99d5abb239371f8ac896c3b6e4
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/156867
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

sw/qa/extras/ooxmlexport/ooxmlfieldexport.cxx
@@ -636,7 +636,7 @@ CPPUNIT_TEST_FIXTURE(Test, testSdtCompanyMultipara)
     // Here is just a simple text node, so there should be either one or zero paragraph
     // (in this case sdt element is inside paragraph)
     assertXPath(pXmlDoc, "//w:sdtContent/w:p", 0);
-    assertXPath(pXmlDoc, "//w:sdtContent/w:r", 1);
+    assertXPath(pXmlDoc, "//w:sdtContent/w:r", 2);
 }
 
 DECLARE_OOXMLEXPORT_TEST(testFixedDateFields, "fixed-date-field.docx")

writerfilter/source/dmapper/SdtHelper.cxx
@@ -83,6 +83,7 @@ SdtHelper::SdtHelper(DomainMapper_Impl& rDM_Impl,
     : m_rDM_Impl(rDM_Impl)
     , m_xComponentContext(std::move(xContext))
     , m_aControlType(SdtControlType::unknown)
+    , m_bHasUnusedText(false)
     , m_bHasElements(false)
     , m_bOutsideAParagraph(false)
     , m_bPropertiesXMLsLoaded(false)
@@ -334,31 +335,102 @@ void SdtHelper::createPlainTextControl()
 {
     assert(getControlType() == SdtControlType::plainText);
 
-    OUString aDefaultText = m_aSdtTexts.makeStringAndClear();
+    if (!m_xFieldStartRange.is())
+        return;
 
-    // create field
-    uno::Reference<css::text::XTextField> xControlModel(
-        m_rDM_Impl.GetTextFactory()->createInstance("com.sun.star.text.TextField.Input"),
-        uno::UNO_QUERY);
+    uno::Reference<text::XTextCursor> xCrsr;
+    uno::Reference<text::XText> xText;
+    if (m_rDM_Impl.HasTopText())
+    {
+        uno::Reference<text::XTextAppend> xTextAppend = m_rDM_Impl.GetTopTextAppend();
+        if (xTextAppend.is())
+        {
+            xText = m_rDM_Impl.GetTopTextAppend()->getEnd()->getText();
+            xCrsr = xText->createTextCursorByRange(m_xFieldStartRange);
+        }
+    }
+    if (!xCrsr.is())
+        return;
 
-    // set properties
-    uno::Reference<beans::XPropertySet> xPropertySet(xControlModel, uno::UNO_QUERY);
+    try
+    {
+        bool bIsInTable = (m_rDM_Impl.hasTableManager() && m_rDM_Impl.getTableManager().isInTable())
+                              != (m_rDM_Impl.m_nTableDepth > 0)
+                          && m_rDM_Impl.GetIsDummyParaAddedForTableInSection();
+        if (bIsInTable)
+            xCrsr->goRight(1, false);
+        xCrsr->gotoEnd(true);
+    }
+    catch (uno::Exception&)
+    {
+        TOOLS_WARN_EXCEPTION("writerfilter.dmapper",
+                             "Cannot get the right text range for date field");
+        return;
+    }
 
     std::optional<OUString> oData = getValueFromDataBinding();
     if (oData.has_value())
-        aDefaultText = *oData;
+        xCrsr->setString(*oData);
 
-    xPropertySet->setPropertyValue("Content", uno::Any(aDefaultText));
+    uno::Reference<text::XTextContent> xContentControl(
+        m_rDM_Impl.GetTextFactory()->createInstance("com.sun.star.text.ContentControl"),
+        uno::UNO_QUERY);
+    uno::Reference<beans::XPropertySet> xContentControlProps(xContentControl, uno::UNO_QUERY);
 
-    PropertyMap aMap;
-    aMap.InsertProps(m_rDM_Impl.GetTopContext());
+    for (const beans::PropertyValue& prop : getInteropGrabBagAndClear())
+    {
+        OUString sPropertyName;
+        if (prop.Name == "ooxml:CT_SdtPr_showingPlcHdr")
+            sPropertyName = "ShowingPlaceHolder";
+        else if (prop.Name == "ooxml:CT_SdtPr_alias")
+            sPropertyName = "Alias";
+        else if (prop.Name == "ooxml:CT_SdtPr_tag")
+            sPropertyName = "Tag";
+        else if (prop.Name == "ooxml:CT_SdtPr_id")
+            sPropertyName = "Id";
+        else if (prop.Name == "ooxml:CT_SdtPr_tabIndex")
+            sPropertyName = "TabIndex";
+        else if (prop.Name == "ooxml:CT_SdtPr_lock")
+            sPropertyName = "Lock";
+        else if (prop.Name == "ooxml:CT_SdtPlaceholder_docPart"
+                 || prop.Name == "ooxml:CT_SdtPr_dataBinding" || prop.Name == "ooxml:CT_SdtPr_color"
+                 || prop.Name == "ooxml:CT_SdtPr_appearance" || prop.Name == "ooxml:CT_SdtPr_text")
+        {
+            uno::Sequence<beans::PropertyValue> aInternalGrabBag;
+            prop.Value >>= aInternalGrabBag;
+            for (const beans::PropertyValue& internalProp : aInternalGrabBag)
+            {
+                if (internalProp.Name == "ooxml:CT_SdtPlaceholder_docPart_val")
+                    sPropertyName = "PlaceholderDocPart";
+                else if (internalProp.Name == "ooxml:CT_DataBinding_prefixMappings")
+                    sPropertyName = "DataBindingPrefixMappings";
+                else if (internalProp.Name == "ooxml:CT_DataBinding_xpath")
+                    sPropertyName = "DataBindingXpath";
+                else if (internalProp.Name == "ooxml:CT_DataBinding_storeItemID")
+                    sPropertyName = "DataBindingStoreItemID";
+                else if (internalProp.Name == "ooxml:CT_SdtAppearance_val")
+                    sPropertyName = "Appearance";
+                else if (internalProp.Name == "ooxml:CT_SdtColor_val")
+                    sPropertyName = "Color";
+                else if (internalProp.Name == "ooxml:CT_SdtText_multiLine")
+                    sPropertyName = "MultiLine";
+                if (!sPropertyName.isEmpty())
+                {
+                    xContentControlProps->setPropertyValue(sPropertyName, internalProp.Value);
+                }
+                sPropertyName.clear();
+            }
+        }
 
-    // add it into document
-    m_rDM_Impl.appendTextContent(xControlModel, aMap.GetPropertyValues());
+        if (!sPropertyName.isEmpty())
+        {
+            xContentControlProps->setPropertyValue(sPropertyName, prop.Value);
+        }
+    }
 
-    // Store all unused sdt parameters from grabbag
-    xPropertySet->setPropertyValue(UNO_NAME_MISC_OBJ_INTEROPGRABBAG,
-                                   uno::Any(getInteropGrabBagAndClear()));
+    xContentControlProps->setPropertyValue("PlainText", uno::Any(true));
+
+    xText->insertTextContent(xCrsr, xContentControl, /*bAbsorb=*/true);
 
     // clean up
     clear();
@@ -366,7 +438,7 @@ void SdtHelper::createPlainTextControl()
 
 void SdtHelper::createDateContentControl()
 {
-    if (!m_xDateFieldStartRange.is())
+    if (!m_xFieldStartRange.is())
         return;
 
     uno::Reference<text::XTextCursor> xCrsr;
@@ -383,7 +455,7 @@ void SdtHelper::createDateContentControl()
 
     try
     {
-        xCrsr->gotoRange(m_xDateFieldStartRange, false);
+        xCrsr->gotoRange(m_xFieldStartRange, false);
         // tdf#138093: Date selector reset, if placed inside table
         // Modified to XOR relationship and adding dummy paragraph conditions
         bool bIsInTable = (m_rDM_Impl.hasTableManager() && m_rDM_Impl.getTableManager().isInTable())
@@ -520,6 +592,7 @@ void SdtHelper::clear()
     m_sDataBindingXPath.clear();
     m_sDataBindingStoreItemID.clear();
     m_aGrabBag.clear();
+    m_bHasUnusedText = false;
     m_bShowingPlcHdr = false;
     m_bChecked = false;
     m_aCheckedState.clear();

offapi/com/sun/star/text/ContentControl.idl
@@ -147,6 +147,12 @@ service ContentControl
         @since LibreOffice 7.6
     */
     [optional, property] string Lock;
+
+    /** Indicates if the control accepts soft breaks.
+
+        @since LibreOffice 24.2
+    */
+    [optional, property] string MultiLine;
 };
 
 

sw/inc/formatcontentcontrol.hxx
@@ -173,7 +173,7 @@ class SW_DLLPUBLIC SwContentControl final : public sw::BroadcastingModify
     /// The appearance: just remembered.
     OUString m_aAppearance;
 
-    /// The alias: just remembered.
+    /// The alias.
     OUString m_aAlias;
 
     /// The tag: just remembered.
@@ -187,6 +187,9 @@ class SW_DLLPUBLIC SwContentControl final : public sw::BroadcastingModify
 
     /// The control and content locks: mostly just remembered.
     OUString m_aLock;
+
+    /// The multiline property: just remembered.
+    OUString m_aMultiLine;
 
     /// Stores a list item index, in case the doc model is not yet updated.
     // i.e. temporarily store the selected item until the text is inserted by GotoContentControl.
@@ -388,6 +391,10 @@ public:
 
     // At the implementation level, define whether the user can directly modify the contents.
     bool GetReadWrite() const { return m_bReadWrite; }
+
+    void SetMultiLine(const OUString& rMultiline) { m_aMultiLine = rMultiline; }
+
+    const OUString& GetMultiLine() const { return m_aMultiLine; }
 
     SwContentControlType GetType() const;
 };

sw/inc/unoprnms.hxx
@@ -941,6 +941,7 @@ inline constexpr OUStringLiteral UNO_NAME_TAG = u"Tag";
 inline constexpr OUStringLiteral UNO_NAME_ID = u"Id";
 inline constexpr OUStringLiteral UNO_NAME_TAB_INDEX = u"TabIndex";
 inline constexpr OUStringLiteral UNO_NAME_LOCK = u"Lock";
+inline constexpr OUStringLiteral UNO_NAME_MULTILINE = u"MultiLine";
 inline constexpr OUStringLiteral UNO_NAME_DATE_STRING = u"DateString";
 inline constexpr OUStringLiteral UNO_NAME_PARA_ID = u"ParaId";
 inline constexpr OUStringLiteral UNO_NAME_PARA_ID_PARENT = u"ParaIdParent";

sw/qa/extras/ooxmlexport/ooxmlexport17.cxx
@@ -683,28 +683,12 @@ DECLARE_OOXMLEXPORT_TEST(testTdf123642_BookmarkAtDocEnd, "tdf123642.docx")
 
 DECLARE_OOXMLEXPORT_TEST(testTdf148361, "tdf148361.docx")
 {
-    if (isExported())
-    {
-        // Block SDT is turned into run SDT on export, so the next import will have this as content
-        // control, not as a field.
-        OUString aActual = getParagraph(1)->getString();
-        // This was "itadmin".
-        CPPUNIT_ASSERT_EQUAL(OUString("itadmin"), aActual);
-    }
-    else
-    {
-        // Refresh fields and ensure cross-reference to numbered para is okay
-        uno::Reference<text::XTextFieldsSupplier> xTextFieldsSupplier(mxComponent, uno::UNO_QUERY);
-        uno::Reference<container::XEnumerationAccess> xFieldsAccess(xTextFieldsSupplier->getTextFields());
+    // Plain text Block SDT is imported as content control
+    OUString aActual = getParagraph(1)->getString();
+    // This was "itadmin".
+    CPPUNIT_ASSERT_EQUAL(OUString("itadmin"), aActual);
 
-        uno::Reference<container::XEnumeration> xFields(xFieldsAccess->createEnumeration());
-        CPPUNIT_ASSERT(xFields->hasMoreElements());
-
-        uno::Reference<text::XTextField> xTextField1(xFields->nextElement(), uno::UNO_QUERY);
-        CPPUNIT_ASSERT_EQUAL(OUString("itadmin"), xTextField1->getPresentation(false));
-    }
-
-    OUString aActual = getParagraph(2)->getString();
+    aActual = getParagraph(2)->getString();
     // This was "itadmin".
     CPPUNIT_ASSERT_EQUAL(OUString("[Type text]"), aActual);
 }
@@ -936,9 +920,7 @@ DECLARE_OOXMLEXPORT_TEST(testTdf81507, "tdf81507.docx")
 
     // Ensure that we have <w:text/>
     assertXPath(pXmlDoc, "/w:document/w:body/w:p[3]/w:sdt/w:sdtPr/w:text");
-
-    // Ensure that we have no <w:text/> (not quite correct case, but to ensure import/export are okay)
-    assertXPath(pXmlDoc, "/w:document/w:body/w:p[4]/w:sdt/w:sdtPr/w:text", 0);
+    assertXPath(pXmlDoc, "/w:document/w:body/w:p[4]/w:sdt/w:sdtPr/w:text");
 }
 
 DECLARE_OOXMLEXPORT_TEST(testTdf139948, "tdf139948.docx")

sw/qa/extras/ooxmlexport/ooxmlexport19.cxx
@@ -1108,7 +1108,10 @@ CPPUNIT_TEST_FIXTURE(Test, testTdf157136)
     {
         // 1st paragraph - block content control
         auto xRun = getRun(getParagraph(1), 1);
-        CPPUNIT_ASSERT_EQUAL(OUString("Click or tap here to enter text.\r"), xRun->getString());
+        auto xContentControl
+            = getProperty<css::uno::Reference<css::text::XTextRange>>(xRun, "ContentControl");
+        CPPUNIT_ASSERT_EQUAL(OUString("Click or tap here to enter text."),
+                             xContentControl->getString());
         // Without the fix in place, this would fail with
         // - Expected: Placeholder Text
         // - Actual  :

sw/qa/extras/ooxmlexport/ooxmlexport3.cxx
@@ -1003,7 +1003,8 @@ CPPUNIT_TEST_FIXTURE(Test, testGlossaryWithEmail)
 
     // preserve the ShowingPlaceholder setting on both block SDTs.
     pXmlDoc = parseExport("word/document.xml");
-    assertXPath(pXmlDoc,"/w:document/w:body/w:p/w:sdt/w:sdtPr/w:showingPlcHdr", 2);
+    assertXPath(pXmlDoc, "/w:document/w:body/w:p/w:sdt/w:sdtPr/w:showingPlcHdr", 1);
+    assertXPath(pXmlDoc, "/w:document/w:body/w:p/w:hyperlink/w:sdt/w:sdtPr/w:showingPlcHdr", 1);
 }
 
 DECLARE_OOXMLEXPORT_TEST(testFdo71785, "fdo71785.docx")

commit 62974cf6a8148377091cb709e7e00dacac152093

tdf#158661 docx import plaintext sdt: remove unnecessary goRight
This fixes a 24.2 regression
from commit 5082d50d24c3fec4487c724a15eb0d54a82ecd0d
   writerfilter: use content controls for text in block SDTs

which was made even worse
with 24.2 commit 7c4dba1deffd81f647a4a3be7a79f68f3bf9f1ba.

Initially, the starting mark was made as soon as SdtContent was seen.
After that, you could get a w:tbl, or a w:tc which could trigger
the start of a table, and potentially add a dummy paragraph
which needed to be jumped over (at least for the first SDT,
but not for subsequent SDTs).

In the second patch, the starting mark was made just before
the text was appended, so all of the cell/table creation
has already been completed, so there is no more need to
jump over any dummy paragraphs, even for the first table.

Affected unit tests were
- table-start-2-sdt.docx
- sdt-company-multipara.docx
- paragraph-sdt.docx

make CppunitTest_sw_ooxmlfieldexport CPPUNIT_TEST_NAME=testParagraphSdt
make CppunitTest_sw_ooxmlfieldexport \
    CPPUNIT_TEST_NAME=testTdf158661_blockSDT

Change-Id: I4a8359cec27f88f6a2fc0464aa240b523a279e42
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/170920
Tested-by: Jenkins
Reviewed-by: Justin Luth <jluth@mail.com>

sw/source/writerfilter/dmapper/SdtHelper.cxx
@@ -360,11 +360,6 @@ void SdtHelper::createPlainTextControl()
 
     try
     {
-        bool bIsInTable = (m_rDM_Impl.hasTableManager() && m_rDM_Impl.getTableManager().isInTable())
-                              != (0 < m_rDM_Impl.m_StreamStateStack.top().nTableDepth)
-                          && m_rDM_Impl.GetIsDummyParaAddedForTableInSection();
-        if (bIsInTable)
-            xCrsr->goRight(1, false);
         xCrsr->gotoEnd(true);
     }
     catch (uno::Exception&)

sw/qa/extras/ooxmlexport/ooxmlfieldexport.cxx
@@ -465,6 +465,68 @@ CPPUNIT_TEST_FIXTURE(Test, testParagraphSdt)
     xmlDocUniquePtr pXmlDoc = parseExport(u"word/document.xml"_ustr);
     // The path to w:sdt contained a w:p.
     assertXPath(pXmlDoc, "/w:document/w:body/w:tbl/w:tr/w:tc/w:p/w:sdt"_ostr);
+
+    uno::Reference<text::XTextTablesSupplier> xTablesSupplier(mxComponent, uno::UNO_QUERY);
+    uno::Reference<container::XIndexAccess> xTables(xTablesSupplier->getTextTables(), uno::UNO_QUERY);
+    uno::Reference<text::XTextTable> xTable(xTables->getByIndex(0), uno::UNO_QUERY);
+    uno::Reference<table::XCell> xCell = xTable->getCellByName(u"A1"_ustr);
+    uno::Reference<container::XEnumerationAccess> xParagraphsAccess(xCell, uno::UNO_QUERY);
+    uno::Reference<container::XEnumeration> xParagraphs = xParagraphsAccess->createEnumeration();
+    uno::Reference<container::XEnumerationAccess> xParagraph(xParagraphs->nextElement(),
+                                                            uno::UNO_QUERY);
+    uno::Reference<container::XEnumeration> xPortions = xParagraph->createEnumeration();
+    uno::Reference<beans::XPropertySet> xTextPortion(xPortions->nextElement(), uno::UNO_QUERY);
+    OUString aPortionType;
+    xTextPortion->getPropertyValue(u"TextPortionType"_ustr) >>= aPortionType;
+    CPPUNIT_ASSERT_EQUAL(u"ContentControl"_ustr, aPortionType);
+    uno::Reference<text::XTextContent> xContentControl;
+    xTextPortion->getPropertyValue(u"ContentControl"_ustr) >>= xContentControl;
+    uno::Reference<beans::XPropertySet> xContentControlProps(xContentControl, uno::UNO_QUERY);
+    uno::Reference<container::XEnumerationAccess> xContentControlEnumAccess(xContentControl, uno::UNO_QUERY);
+    uno::Reference<container::XEnumeration> xContentControlEnum = xContentControlEnumAccess->createEnumeration();
+    uno::Reference<text::XTextRange> xTextPortionRange(xContentControlEnum->nextElement(), uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(u"Android Security Final Design Report"_ustr, xTextPortionRange->getString());
+}
+
+CPPUNIT_TEST_FIXTURE(Test, testTdf158661_blockSDT)
+{
+    loadAndReload("tdf158661_blockSDT.docx");
+    uno::Reference<text::XTextTablesSupplier> xTablesSupplier(mxComponent, uno::UNO_QUERY);
+    uno::Reference<container::XIndexAccess> xTables(xTablesSupplier->getTextTables(), uno::UNO_QUERY);
+    uno::Reference<text::XTextTable> xTable(xTables->getByIndex(0), uno::UNO_QUERY);
+    uno::Reference<table::XCell> xCell = xTable->getCellByName(u"A1"_ustr);
+    uno::Reference<container::XEnumerationAccess> xParagraphsAccess(xCell, uno::UNO_QUERY);
+    uno::Reference<container::XEnumeration> xParagraphs = xParagraphsAccess->createEnumeration();
+    uno::Reference<container::XEnumerationAccess> xParagraph(xParagraphs->nextElement(),
+                                                            uno::UNO_QUERY);
+    uno::Reference<container::XEnumeration> xPortions = xParagraph->createEnumeration();
+    uno::Reference<beans::XPropertySet> xTextPortion(xPortions->nextElement(), uno::UNO_QUERY);
+    OUString aPortionType;
+    xTextPortion->getPropertyValue(u"TextPortionType"_ustr) >>= aPortionType;
+    CPPUNIT_ASSERT_EQUAL(u"ContentControl"_ustr, aPortionType);
+    uno::Reference<text::XTextContent> xContentControl;
+    xTextPortion->getPropertyValue(u"ContentControl"_ustr) >>= xContentControl;
+    uno::Reference<beans::XPropertySet> xContentControlProps(xContentControl, uno::UNO_QUERY);
+    uno::Reference<container::XEnumerationAccess> xContentControlEnumAccess(xContentControl, uno::UNO_QUERY);
+    uno::Reference<container::XEnumeration> xContentControlEnum = xContentControlEnumAccess->createEnumeration();
+    uno::Reference<text::XTextRange> xTextPortionRange(xContentControlEnum->nextElement(), uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(u"Survey Strategy"_ustr, xTextPortionRange->getString());
+
+    xTable.set(xTables->getByIndex(1), uno::UNO_QUERY);
+    xCell = xTable->getCellByName(u"A1"_ustr);
+    xParagraphsAccess.set(xCell, uno::UNO_QUERY);
+    xParagraphs = xParagraphsAccess->createEnumeration();
+    xParagraph.set(xParagraphs->nextElement(), uno::UNO_QUERY);
+    xPortions = xParagraph->createEnumeration();
+    xTextPortion.set(xPortions->nextElement(), uno::UNO_QUERY);
+    xTextPortion->getPropertyValue(u"TextPortionType"_ustr) >>= aPortionType;
+    CPPUNIT_ASSERT_EQUAL(u"ContentControl"_ustr, aPortionType);
+    xTextPortion->getPropertyValue(u"ContentControl"_ustr) >>= xContentControl;
+    xContentControlProps.set(xContentControl, uno::UNO_QUERY);
+    xContentControlEnumAccess.set(xContentControl, uno::UNO_QUERY);
+    xContentControlEnum = xContentControlEnumAccess->createEnumeration();
+    xTextPortionRange.set(xContentControlEnum->nextElement(), uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(u"Test"_ustr, xTextPortionRange->getString());
 }
 
 CPPUNIT_TEST_FIXTURE(Test, testSdt2Run)