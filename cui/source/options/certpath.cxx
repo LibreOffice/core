LibreOffice/core

commit db9e1ef04d13ecc1b8f8f156b059173143ea03ea

weld CertPathDialog
Change-Id: I73c5aa3694d2580b707c2ce769e3e9ffbc076ec8
Reviewed-on: https://gerrit.libreoffice.org/67406
Tested-by: Jenkins
Reviewed-by: Caolán McNamara <caolanm@redhat.com>
Tested-by: Caolán McNamara <caolanm@redhat.com>

cui/source/options/certpath.cxx
@@ -23,32 +23,32 @@
 
 using namespace ::com::sun::star;
 
-CertPathDialog::CertPathDialog(vcl::Window* pParent)
-    : ModalDialog(pParent, "CertDialog", "cui/ui/certdialog.ui")
+CertPathDialog::CertPathDialog(weld::Window* pParent)
+    : GenericDialogController(pParent, "cui/ui/certdialog.ui", "CertDialog")
+    , m_xAddBtn(m_xBuilder->weld_button("add"))
+    , m_xOKBtn(m_xBuilder->weld_button("ok"))
+    , m_xCertPathList(m_xBuilder->weld_tree_view("paths"))
+    , m_xAddDialogLabel(m_xBuilder->weld_label("certdir"))
+    , m_xManualLabel(m_xBuilder->weld_label("manual"))
 {
-    get(m_pOKBtn, "ok");
-    get(m_pAddBtn, "add");
-    get(m_pCertPathListContainer, "paths");
-    Size aSize(LogicToPixel(Size(210, 60), MapMode(MapUnit::MapAppFont)));
-    m_pCertPathListContainer->set_width_request(aSize.Width());
-    m_pCertPathListContainer->set_height_request(aSize.Height());
-    m_pCertPathList =
-        VclPtr<svx::SvxRadioButtonListBox>::Create(*m_pCertPathListContainer, 0);
-    m_sAddDialogText = get<FixedText>("certdir")->GetText();
-    m_sManual = get<FixedText>("manual")->GetText();
+    m_sAddDialogText = m_xAddDialogLabel->get_label();
+    m_sManual = m_xManualLabel->get_label();
 
-    static long aStaticTabs[] = { 0, 15, 75 };
-    m_pCertPathList->SvSimpleTable::SetTabs( SAL_N_ELEMENTS(aStaticTabs), aStaticTabs );
+    m_xCertPathList->set_size_request(m_xCertPathList->get_approximate_digit_width() * 70,
+                                      m_xCertPathList->get_height_rows(6));
 
-    OUString sProfile(get<FixedText>("profile")->GetText());
-    OUString sDirectory(get<FixedText>("dir")->GetText());
-    OUString sHeader = "\t" + sProfile + "\t" + sDirectory;
+    std::vector<int> aWidths;
+    aWidths.push_back(m_xCertPathList->get_approximate_digit_width() * 3 + 6);
+    aWidths.push_back(m_xCertPathList->get_approximate_digit_width() * 20);
+    m_xCertPathList->set_column_fixed_widths(aWidths);
 
-    m_pCertPathList->InsertHeaderEntry( sHeader, HEADERBAR_APPEND, HeaderBarItemBits::LEFT );
-    m_pCertPathList->SetCheckButtonHdl( LINK( this, CertPathDialog, CheckHdl_Impl ) );
+    std::vector<int> aRadioColumns;
+    aRadioColumns.push_back(0);
+    m_xCertPathList->set_toggle_columns_as_radio(aRadioColumns);
 
-    m_pAddBtn->SetClickHdl( LINK( this, CertPathDialog, AddHdl_Impl ) );
-    m_pOKBtn->SetClickHdl( LINK( this, CertPathDialog, OKHdl_Impl ) );
+    m_xCertPathList->connect_toggled(LINK(this, CertPathDialog, CheckHdl_Impl));
+    m_xAddBtn->connect_clicked( LINK( this, CertPathDialog, AddHdl_Impl ) );
+    m_xOKBtn->connect_clicked( LINK( this, CertPathDialog, OKHdl_Impl ) );
 
     try
     {
@@ -69,24 +69,20 @@ CertPathDialog::CertPathDialog(vcl::Window* pParent)
 
             if (!profile.isEmpty())
             {
+                m_xCertPathList->insert(nullptr, -1, nullptr, nullptr, nullptr,
+                                        nullptr, nullptr, false);
+                const int nRow = m_xCertPathList->n_children() - 1;
+                m_xCertPathList->set_toggle(nRow, false, 0);
+                OUString sEntry = OUString::createFromAscii(productNames[i]) + ":" + profile;
+                m_xCertPathList->set_text(nRow, sEntry, 1);
                 OUString sProfilePath = xMozillaBootstrap->getProfilePath( productTypes[i], profile );
-                OUString sEntry = "\t" + OUString::createFromAscii(productNames[i]) + ":" + profile
-                                + "\t" + sProfilePath;
-                SvTreeListEntry *pEntry = m_pCertPathList->InsertEntry(sEntry);
-                OUString* pCertPath = new OUString(sProfilePath);
-                pEntry->SetUserData(pCertPath);
+                m_xCertPathList->set_text(nRow, sProfilePath, 2);
+                m_xCertPathList->set_id(nRow, sProfilePath);
             }
         }
     }
     catch (const uno::Exception&)
     {
-    }
-
-    SvTreeListEntry *pEntry = m_pCertPathList->GetEntry(0);
-    if (pEntry)
-    {
-        m_pCertPathList->SetCheckButtonState(pEntry, SvButtonState::Checked);
-        HandleCheckEntry(pEntry);
     }
 
     try
@@ -105,9 +101,15 @@ CertPathDialog::CertPathDialog(vcl::Window* pParent)
     const char* pEnv = getenv("MOZILLA_CERTIFICATE_FOLDER");
     if (pEnv)
         AddCertPath("$MOZILLA_CERTIFICATE_FOLDER", OUString(pEnv, strlen(pEnv), osl_getThreadTextEncoding()));
+
+    if (m_xCertPathList->n_children())
+    {
+        m_xCertPathList->set_toggle(0, true, 0);
+        HandleEntryChecked(0);
+    }
 }
 
-IMPL_LINK_NOARG(CertPathDialog, OKHdl_Impl, Button*, void)
+IMPL_LINK_NOARG(CertPathDialog, OKHdl_Impl, weld::Button&, void)
 {
     try
     {
@@ -122,90 +124,67 @@ IMPL_LINK_NOARG(CertPathDialog, OKHdl_Impl, Button*, void)
         SAL_WARN("cui.options", "CertPathDialog::OKHdl_Impl(): " << e);
     }
 
-    EndDialog(RET_OK);
+    m_xDialog->response(RET_OK);
 }
 
 OUString CertPathDialog::getDirectory() const
 {
-    SvTreeListEntry* pEntry = m_pCertPathList->FirstSelected();
-    void* pCertPath = pEntry ? pEntry->GetUserData() : nullptr;
-    return pCertPath ? *static_cast<OUString*>(pCertPath) : OUString();
+    int nEntry = m_xCertPathList->get_selected_index();
+    if (nEntry == -1)
+        return OUString();
+    return m_xCertPathList->get_id(nEntry);
 }
 
 CertPathDialog::~CertPathDialog()
 {
-    disposeOnce();
 }
 
-void CertPathDialog::dispose()
+IMPL_LINK(CertPathDialog, CheckHdl_Impl, const row_col&, rRowCol, void)
 {
-    SvTreeListEntry* pEntry = m_pCertPathList->First();
-    while (pEntry)
+    HandleEntryChecked(rRowCol.first);
+}
+
+void CertPathDialog::HandleEntryChecked(int nRow)
+{
+    m_xCertPathList->select(nRow);
+    bool bChecked = m_xCertPathList->get_toggle(nRow, 0);
+    if (bChecked)
     {
-        OUString* pCertPath = static_cast<OUString*>(pEntry->GetUserData());
-        delete pCertPath;
-        pEntry = m_pCertPathList->Next( pEntry );
-    }
-    m_pCertPathList.disposeAndClear();
-    m_pCertPathListContainer.clear();
-    m_pAddBtn.clear();
-    m_pOKBtn.clear();
-    ModalDialog::dispose();
-}
-
-IMPL_LINK( CertPathDialog, CheckHdl_Impl, SvTreeListBox*, pList, void )
-{
-    SvTreeListEntry* pEntry = pList ? m_pCertPathList->GetEntry(m_pCertPathList->GetCurMousePoint())
-                                : m_pCertPathList->FirstSelected();
-    if (pEntry)
-        m_pCertPathList->HandleEntryChecked(pEntry);
-}
-
-void CertPathDialog::HandleCheckEntry( SvTreeListEntry* _pEntry )
-{
-    m_pCertPathList->Select( _pEntry );
-    SvButtonState eState = m_pCertPathList->GetCheckButtonState( _pEntry );
-
-    if (SvButtonState::Checked == eState)
-    {
-        // uncheck the other entries
-        SvTreeListEntry* pEntry = m_pCertPathList->First();
-        while (pEntry)
+        // we have radio button behavior -> so uncheck the other entries
+        int nCount = m_xCertPathList->n_children();
+        for (int i = 0; i < nCount; ++i)
         {
-            if (pEntry != _pEntry)
-                m_pCertPathList->SetCheckButtonState(pEntry, SvButtonState::Unchecked);
-            pEntry = m_pCertPathList->Next(pEntry);
+            if (i != nRow)
+                m_xCertPathList->set_toggle(i, false, 0);
         }
     }
-    else
-        m_pCertPathList->SetCheckButtonState(_pEntry, SvButtonState::Checked);
 }
 
 void CertPathDialog::AddCertPath(const OUString &rProfile, const OUString &rPath)
 {
-    SvTreeListEntry* pEntry = m_pCertPathList->First();
-    while (pEntry)
+    for (int i = 0, nCount = m_xCertPathList->n_children(); i < nCount; ++i)
     {
-        OUString* pCertPath = static_cast<OUString*>(pEntry->GetUserData());
+        OUString sCertPath = m_xCertPathList->get_id(i);
         //already exists, just select the original one
-        if (*pCertPath == rPath)
+        if (sCertPath == rPath)
         {
-            m_pCertPathList->SetCheckButtonState(pEntry, SvButtonState::Checked);
-            HandleCheckEntry(pEntry);
+            m_xCertPathList->set_toggle(i, true, 0);
+            HandleEntryChecked(i);
             return;
         }
-        pEntry = m_pCertPathList->Next(pEntry);
     }
 
-    OUString sEntry( "\t" + rProfile + "\t" + rPath );
-    pEntry = m_pCertPathList->InsertEntry(sEntry);
-    OUString* pCertPath = new OUString(rPath);
-    pEntry->SetUserData(pCertPath);
-    m_pCertPathList->SetCheckButtonState(pEntry, SvButtonState::Checked);
-    HandleCheckEntry(pEntry);
+    m_xCertPathList->insert(nullptr, -1, nullptr, nullptr, nullptr,
+                            nullptr, nullptr, false);
+    const int nRow = m_xCertPathList->n_children() - 1;
+    m_xCertPathList->set_toggle(nRow, true, 0);
+    m_xCertPathList->set_text(nRow, rProfile, 1);
+    m_xCertPathList->set_text(nRow, rPath, 2);
+    m_xCertPathList->set_id(nRow, rPath);
+    HandleEntryChecked(nRow);
 }
 
-IMPL_LINK_NOARG(CertPathDialog, AddHdl_Impl, Button*, void)
+IMPL_LINK_NOARG(CertPathDialog, AddHdl_Impl, weld::Button&, void)
 {
     try
     {

commit f2f1782d23abc48dadc21a82181417c557b2090d

tdf#161909: Fix resize for cert listings (GtkBox instead of GtkGrid)
- "Certificate Path" dialog (select Mozilla profile for NSS)
  - resizing broken since commit db9e1ef04d13ecc1b8f8f156b059173143ea03ea
  - also increased default width to account for long filesystem paths
- "Digital Signatures" dialog
- "Select Certificate" dialog
  - resizable=True is the default (Glade removes explicit defaults)
  - Make long translations wrap instead of being cut.

Glade set expand=False and fill=True as defaults inside GtkBox.

Change-Id: I2d7719e83e5162d06d30e25b3e307fa23e0d605e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/170694
Tested-by: Jenkins
Reviewed-by: Thorsten Behrens <thorsten.behrens@allotropia.de>

cui/source/options/certpath.cxx
@@ -29,7 +29,7 @@ CertPathDialog::CertPathDialog(weld::Window* pParent)
     , m_sAddDialogText(m_xBuilder->weld_label(u"certdir"_ustr)->get_label())
     , m_sManualLabel(m_xBuilder->weld_label(u"manual"_ustr)->get_label())
 {
-    m_xCertPathList->set_size_request(m_xCertPathList->get_approximate_digit_width() * 70,
+    m_xCertPathList->set_size_request(m_xCertPathList->get_approximate_digit_width() * 140,
                                       m_xCertPathList->get_height_rows(6));
 
     // needed for VLCPLUGIN != gtk3 (e.g. "gen")

cui/uiconfig/ui/certdialog.ui
@@ -116,15 +116,15 @@
             <property name="label-xalign">0</property>
             <property name="shadow-type">none</property>
             <child>
-              <!-- n-columns=1 n-rows=3 -->
-              <object class="GtkGrid" id="grid1">
+              <object class="GtkBox" id="box1">
                 <property name="visible">True</property>
                 <property name="can-focus">False</property>
                 <property name="margin-start">12</property>
                 <property name="margin-top">6</property>
                 <property name="hexpand">True</property>
                 <property name="vexpand">True</property>
-                <property name="row-spacing">6</property>
+                <property name="orientation">vertical</property>
+                <property name="spacing">6</property>
                 <child>
                   <object class="GtkLabel" id="label2">
                     <property name="visible">True</property>
@@ -139,13 +139,14 @@
                     <property name="yalign">0</property>
                   </object>
                   <packing>
-                    <property name="left-attach">0</property>
-                    <property name="top-attach">0</property>
+                    <property name="expand">False</property>
+                    <property name="fill">True</property>
+                    <property name="position">0</property>
                   </packing>
                 </child>
                 <child>
                   <!-- n-columns=2 n-rows=1 -->
-                  <object class="GtkGrid" id="grid2">
+                  <object class="GtkGrid" id="grid1">
                     <property name="can-focus">False</property>
                     <property name="no-show-all">True</property>
                     <child>
@@ -173,8 +174,9 @@
                     </child>
                   </object>
                   <packing>
-                    <property name="left-attach">0</property>
-                    <property name="top-attach">1</property>
+                    <property name="expand">False</property>
+                    <property name="fill">True</property>
+                    <property name="position">1</property>
                   </packing>
                 </child>
                 <child>
@@ -236,8 +238,9 @@
                     </child>
                   </object>
                   <packing>
-                    <property name="left-attach">0</property>
-                    <property name="top-attach">2</property>
+                    <property name="expand">True</property>
+                    <property name="fill">True</property>
+                    <property name="position">2</property>
                   </packing>
                 </child>
               </object>

xmlsecurity/uiconfig/ui/digitalsignaturesdialog.ui
@@ -75,13 +75,13 @@
           </packing>
         </child>
         <child>
-          <!-- n-columns=1 n-rows=8 -->
-          <object class="GtkGrid" id="grid1">
+          <object class="GtkBox" id="box1">
             <property name="visible">True</property>
             <property name="can-focus">False</property>
             <property name="hexpand">True</property>
             <property name="vexpand">True</property>
-            <property name="row-spacing">6</property>
+            <property name="orientation">vertical</property>
+            <property name="spacing">6</property>
             <child>
               <object class="GtkLabel" id="dochint">
                 <property name="can-focus">False</property>
@@ -90,8 +90,9 @@
                 <property name="xalign">0</property>
               </object>
               <packing>
-                <property name="left-attach">0</property>
-                <property name="top-attach">0</property>
+                <property name="expand">False</property>
+                <property name="fill">True</property>
+                <property name="position">0</property>
               </packing>
             </child>
             <child>
@@ -200,8 +201,9 @@
                 </child>
               </object>
               <packing>
-                <property name="left-attach">0</property>
-                <property name="top-attach">4</property>
+                <property name="expand">True</property>
+                <property name="fill">True</property>
+                <property name="position">1</property>
               </packing>
             </child>
             <child>
@@ -281,13 +283,14 @@
                 </child>
               </object>
               <packing>
-                <property name="left-attach">0</property>
-                <property name="top-attach">7</property>
+                <property name="expand">False</property>
+                <property name="fill">True</property>
+                <property name="position">2</property>
               </packing>
             </child>
             <child>
               <!-- n-columns=5 n-rows=1 -->
-              <object class="GtkGrid" id="grid2">
+              <object class="GtkGrid" id="grid1">
                 <property name="visible">True</property>
                 <property name="can-focus">False</property>
                 <property name="hexpand">True</property>
@@ -347,8 +350,9 @@
                 </child>
               </object>
               <packing>
-                <property name="left-attach">0</property>
-                <property name="top-attach">3</property>
+                <property name="expand">False</property>
+                <property name="fill">True</property>
+                <property name="position">3</property>
               </packing>
             </child>
             <child>
@@ -359,8 +363,9 @@
                 <property name="xalign">0</property>
               </object>
               <packing>
-                <property name="left-attach">0</property>
-                <property name="top-attach">1</property>
+                <property name="expand">False</property>
+                <property name="fill">True</property>
+                <property name="position">4</property>
               </packing>
             </child>
             <child>
@@ -371,13 +376,14 @@
                 <property name="xalign">0</property>
               </object>
               <packing>
-                <property name="left-attach">0</property>
-                <property name="top-attach">2</property>
+                <property name="expand">False</property>
+                <property name="fill">True</property>
+                <property name="position">5</property>
               </packing>
             </child>
             <child>
               <!-- n-columns=2 n-rows=4 -->
-              <object class="GtkGrid" id="grid3">
+              <object class="GtkGrid" id="grid2">
                 <property name="visible">True</property>
                 <property name="can-focus">False</property>
                 <property name="column-spacing">12</property>
@@ -479,8 +485,9 @@
                 </child>
               </object>
               <packing>
-                <property name="left-attach">0</property>
-                <property name="top-attach">5</property>
+                <property name="expand">False</property>
+                <property name="fill">True</property>
+                <property name="position">6</property>
               </packing>
             </child>
             <child>

xmlsecurity/uiconfig/ui/selectcertificatedialog.ui
@@ -22,7 +22,6 @@
     <property name="can-focus">False</property>
     <property name="border-width">6</property>
     <property name="title" translatable="yes" context="selectcertificatedialog|SelectCertificateDialog">Select Certificate</property>
-    <property name="resizable">False</property>
     <property name="modal">True</property>
     <property name="type-hint">dialog</property>
     <child internal-child="vbox">
@@ -88,34 +87,41 @@
           </packing>
         </child>
         <child>
-          <!-- n-columns=1 n-rows=6 -->
-          <object class="GtkGrid" id="grid1">
+          <object class="GtkBox" id="box1">
             <property name="visible">True</property>
             <property name="can-focus">False</property>
             <property name="hexpand">True</property>
             <property name="vexpand">True</property>
-            <property name="row-spacing">12</property>
-            <property name="column-spacing">12</property>
+            <property name="orientation">vertical</property>
+            <property name="spacing">12</property>
             <child>
               <object class="GtkLabel" id="sign">
                 <property name="can-focus">False</property>
                 <property name="label" translatable="yes" context="selectcertificatedialog|sign">Select the certificate you want to use for signing:</property>
+                <property name="wrap">True</property>
+                <property name="width-chars">56</property>
+                <property name="max-width-chars">56</property>
                 <property name="xalign">0</property>
               </object>
               <packing>
-                <property name="left-attach">0</property>
-                <property name="top-attach">0</property>
+                <property name="expand">False</property>
+                <property name="fill">True</property>
+                <property name="position">0</property>
               </packing>
             </child>
             <child>
               <object class="GtkLabel" id="encrypt">
                 <property name="can-focus">False</property>
                 <property name="label" translatable="yes" context="selectcertificatedialog|encrypt">Select the certificate you want to use for encryption:</property>
+                <property name="wrap">True</property>
+                <property name="width-chars">56</property>
+                <property name="max-width-chars">56</property>
                 <property name="xalign">0</property>
               </object>
               <packing>
-                <property name="left-attach">0</property>
-                <property name="top-attach">1</property>
+                <property name="expand">False</property>
+                <property name="fill">True</property>
+                <property name="position">1</property>
               </packing>
             </child>
             <child>
@@ -222,8 +228,9 @@
                 </child>
               </object>
               <packing>
-                <property name="left-attach">0</property>
-                <property name="top-attach">3</property>
+                <property name="expand">True</property>
+                <property name="fill">True</property>
+                <property name="position">2</property>
               </packing>
             </child>
             <child>
@@ -240,8 +247,9 @@
                 </child>
               </object>
               <packing>
-                <property name="left-attach">0</property>
-                <property name="top-attach">2</property>
+                <property name="expand">False</property>
+                <property name="fill">True</property>
+                <property name="position">3</property>
               </packing>
             </child>
             <child>
@@ -274,15 +282,16 @@
                     </child>
                   </object>
                   <packing>
-                    <property name="expand">True</property>
+                    <property name="expand">False</property>
                     <property name="fill">True</property>
                     <property name="position">1</property>
                   </packing>
                 </child>
               </object>
               <packing>
-                <property name="left-attach">0</property>
-                <property name="top-attach">5</property>
+                <property name="expand">False</property>
+                <property name="fill">True</property>
+                <property name="position">4</property>
               </packing>
             </child>
             <child>
@@ -331,8 +340,9 @@
                 </child>
               </object>
               <packing>
-                <property name="left-attach">0</property>
-                <property name="top-attach">4</property>
+                <property name="expand">False</property>
+                <property name="fill">True</property>
+                <property name="position">5</property>
               </packing>
             </child>
           </object>