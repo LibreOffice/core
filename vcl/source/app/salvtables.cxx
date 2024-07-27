85511e27d259d6b19cc6c463fc06368c14511bb8
Caolán McNamara <caolan.mcnamara@collabora.com>
2024-07-27 20:25:35 +0100

    Resolves: tdf#160509 use BorderWindow client for dialog parent
    the BorderWindow has no ultimate SystemWindow parent so dialogs
    using this as a parent end up automatically picking a parent
    of whatever happens to be in the foreground and not the intended
    associated frame
    
    Change-Id: I713e3c40b0cad934b978ba0b1b46a3c6064be24f
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/171104
    Reviewed-by: Patrick Luby <guibomacdev@gmail.com>
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Tested-by: Jenkins

--- vcl/source/app/salvtables.cxx
+++ vcl/source/app/salvtables.cxx
@@ -7560,6 +7560,8 @@ weld::Window* SalFrame::GetFrameWeld() const
         if (pWindow)
         {
             assert(pWindow == pWindow->GetFrameWindow());
+            // resolve from a possible BorderWindow to the ClientWindow (returns itself if not)
+            pWindow = pWindow->ImplGetWindow();
             m_xFrameWeld.reset(new SalInstanceWindow(pWindow, nullptr, false));
         }
     }
