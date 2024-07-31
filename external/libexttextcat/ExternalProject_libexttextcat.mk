f01975a0877170e89bc486eb05e8256bd295c170
Stephan Bergmann <stephan.bergmann@allotropia.de>
2024-07-31 09:56:21 +0200

    external/libexttextcat: Only pass gb_DEBUGINFO_FLAGS in debug builds
    broken since 797681b4f569212a0f306151167534e23a2e37af "Pass all relevant CFLAGS
    on to external/libexttextcat"
    
    Change-Id: Idd648c8cd7eaa7e78815dd2071d0a3b786fa6d23
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/171273
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <stephan.bergmann@allotropia.de>
    (cherry picked from commit 374c785daff03f10116781d364f3024aff19d46c)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/173631
    Tested-by: Stephan Bergmann <stephan.bergmann@allotropia.de>

--- external/libexttextcat/ExternalProject_libexttextcat.mk
+++ external/libexttextcat/ExternalProject_libexttextcat.mk
@@ -20,7 +20,7 @@ $(call gb_ExternalProject_get_state_target,libexttextcat,build):
 			$(if $(verbose),--disable-silent-rules,--enable-silent-rules) \
 			$(if $(ENABLE_WERROR),--enable-werror,--disable-werror) \
 			$(gb_CONFIGURE_PLATFORMS) \
-		CFLAGS="$(CFLAGS) $(gb_VISIBILITY_FLAGS) $(gb_DEBUGINFO_FLAGS) $(call gb_ExternalProject_get_build_flags,libexttextcat) \
+		CFLAGS="$(CFLAGS) $(gb_VISIBILITY_FLAGS) $(if $(debug),$(gb_DEBUGINFO_FLAGS)) $(call gb_ExternalProject_get_build_flags,libexttextcat) \
 			$(if $(COM_IS_CLANG),-Qunused-arguments)" \
 		LDFLAGS="$(call gb_ExternalProject_get_link_flags,libexttextcat)" \
 		&& $(MAKE) \
