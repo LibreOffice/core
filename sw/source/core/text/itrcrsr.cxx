LibreOffice/core

commit 468e5b8e0a7fefe1ca53faeb15f5f6527c37a268

tdf#157816 sw: fix getting position in field portion follow
SwSpecialPos can be used to get index inside a field, but
SwTextCursor::GetModelPositionForViewPoint() has several problems:
* the field portion follow has length 0 so an early return is taken
* the nCharOfst is set to the index in the portion, but it needs to
  also count preceding portions of the same field in the line,
  and nLineOfst needs to be set as well, because the SwPosition
  corresponds to the start of the field
* m_bFieldInfo must be set to guarantee SwPosition before the field
  in the nLenght==0 branch, but then there are 2 branches that first
  set nLength=0 and then decrement it, resulting in a SwPosition 1
  char before the field.

Change-Id: Ib7d30981e41b40f4c068fa6d84211c000ecde753
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/158570
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/core/text/itrcrsr.cxx
@@ -1528,7 +1528,11 @@ TextFrameIndex SwTextCursor::GetModelPositionForViewPoint( SwPosition *pPos, con
                     --nCurrStart;
             }
         }
-        return nCurrStart;
+        if (!pPor->InFieldGrp() || !static_cast<SwFieldPortion const*>(pPor)->IsFollow()
+            || !pCMS || !pCMS->m_pSpecialPos)
+        {
+            return nCurrStart;
+        }
     }
     if (TextFrameIndex(1) == nLength || pPor->InFieldGrp())
     {
@@ -1758,18 +1762,56 @@ TextFrameIndex SwTextCursor::GetModelPositionForViewPoint( SwPosition *pPos, con
                 if ( pPor->InFieldGrp() && pCMS && pCMS->m_pSpecialPos )
                 {
                     pCMS->m_pSpecialPos->nCharOfst = sal_Int32(nLength);
+                    // follow portions: need to add the length of all previous
+                    // portions for the same field
+                    if (static_cast<SwFieldPortion const*>(pPor)->IsFollow())
+                    {
+                        int nLines(0);
+                        std::vector<SwFieldPortion const*> portions;
+                        for (SwLineLayout const* pLine = GetInfo().GetParaPortion();
+                                true; pLine = pLine->GetNext())
+                        {
+                            for (SwLinePortion const* pLP = pLine; pLP && pLP != pPor; pLP = pLP->GetNextPortion())
+                            {
+                                if (pLP->InFieldGrp())
+                                {
+                                    SwFieldPortion const* pField(static_cast<SwFieldPortion const*>(pLP));
+                                    if (!pField->IsFollow())
+                                    {
+                                        nLines = 0;
+                                        portions.clear();
+                                    }
+                                    if (pLine == m_pCurr)
+                                    {
+                                        portions.emplace_back(pField);
+                                    }
+                                }
+                            }
+                            if (pLine == m_pCurr)
+                            {
+                                break;
+                            }
+                            ++nLines;
+                        }
+                        for (SwFieldPortion const* pField : portions)
+                        {
+                            pCMS->m_pSpecialPos->nCharOfst += pField->GetExp().getLength();
+                        }
+                        pCMS->m_pSpecialPos->nLineOfst = nLines;
+                    }
                     nLength = TextFrameIndex(0);
+                }
+                else if (bFieldInfo && nLength == pPor->GetLen() &&
+                         (! pPor->GetNextPortion() ||
+                          ! pPor->GetNextPortion()->IsPostItsPortion()))
+                {
+                    --nLength;
                 }
 
                 // set cursor bidi level
                 if ( pCMS )
                     pCMS->m_nCursorBidiLevel =
                         aDrawInf.GetCursorBidiLevel();
-
-                if( bFieldInfo && nLength == pPor->GetLen() &&
-                    ( ! pPor->GetNextPortion() ||
-                      ! pPor->GetNextPortion()->IsPostItsPortion() ) )
-                    --nLength;
             }
             if( nOldProp )
                 const_cast<SwFont*>(GetFnt())->SetProportion( nOldProp );

commit 97dcffe188f0dec4dbd0af894fe7739e1a2abcbb

tdf#162035 sw: fix crash with field in SwMultiPortion
SwTextCursor::GetModelPositionForViewPoint() is recursive for
SwMultiPortions, overriding m_pCurr via SwTextCursorSave, so the
SwSpecialPos counting code needs to be recursive too.

(regression from commit 468e5b8e0a7fefe1ca53faeb15f5f6527c37a268)

Change-Id: Ic3f2f359a21dcae692400efdd693cc4042f9426e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/171247
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/core/text/itrcrsr.cxx
@@ -1330,6 +1330,46 @@ static bool ConsiderNextPortionForCursorOffset(const SwLinePortion* pPor, SwTwip
     return true;
 }
 
+static auto SearchLine(SwLineLayout const*const pLineOfFoundPor,
+    SwLinePortion const*const pFoundPor,
+    int & rLines, std::vector<SwFieldPortion const*> & rPortions,
+    SwLineLayout const*const pLine) -> bool
+{
+    for (SwLinePortion const* pLP = pLine; pLP; pLP = pLP->GetNextPortion())
+    {
+        if (pLP == pFoundPor)
+        {
+            return true;
+        }
+        if (pLP->InFieldGrp())
+        {
+            SwFieldPortion const* pField(static_cast<SwFieldPortion const*>(pLP));
+            if (!pField->IsFollow())
+            {
+                rLines = 0;
+                rPortions.clear();
+            }
+            if (pLine == pLineOfFoundPor)
+            {
+                rPortions.emplace_back(pField);
+            }
+        }
+        else if (pLP->IsMultiPortion())
+        {
+            SwMultiPortion const*const pMulti(static_cast<SwMultiPortion const*>(pLP));
+            for (SwLineLayout const* pMLine = &pMulti->GetRoot();
+                    pMLine; pMLine = pMLine->GetNext())
+            {
+                if (SearchLine(pLineOfFoundPor, pFoundPor, rLines, rPortions, pMLine))
+                {
+                    return true;
+                }
+            }
+        }
+    }
+    return (pLine == pLineOfFoundPor);
+}
+
 // Return: Offset in String
 TextFrameIndex SwTextCursor::GetModelPositionForViewPoint( SwPosition *pPos, const Point &rPoint,
                                     bool bChgNode, SwCursorMoveState* pCMS ) const
@@ -1780,23 +1820,7 @@ TextFrameIndex SwTextCursor::GetModelPositionForViewPoint( SwPosition *pPos, con
                         for (SwLineLayout const* pLine = GetInfo().GetParaPortion();
                                 true; pLine = pLine->GetNext())
                         {
-                            for (SwLinePortion const* pLP = pLine; pLP && pLP != pPor; pLP = pLP->GetNextPortion())
-                            {
-                                if (pLP->InFieldGrp())
-                                {
-                                    SwFieldPortion const* pField(static_cast<SwFieldPortion const*>(pLP));
-                                    if (!pField->IsFollow())
-                                    {
-                                        nLines = 0;
-                                        portions.clear();
-                                    }
-                                    if (pLine == m_pCurr)
-                                    {
-                                        portions.emplace_back(pField);
-                                    }
-                                }
-                            }
-                            if (pLine == m_pCurr)
+                            if (SearchLine(m_pCurr, pPor, nLines, portions, pLine))
                             {
                                 break;
                             }