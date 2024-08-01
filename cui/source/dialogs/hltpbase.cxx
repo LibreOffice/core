LibreOffice/core

commit f690e8218a74a0f7792407a1d46198883d2528ff

make event config dialog async
Change-Id: Ibbfbc5024fb2be4906d0c312577310f28dde212d
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/162434
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

cui/source/factory/dlgfact.cxx
@@ -141,7 +141,7 @@ IMPL_ABSTDLG_CLASS_ASYNC(AbstractThesaurusDialog,SfxDialogController)
 IMPL_ABSTDLG_CLASS_ASYNC(CuiAbstractControllerAsync,weld::DialogController)
 IMPL_ABSTDLG_CLASS_ASYNC(CuiAbstractTabController,SfxTabDialogController)
 IMPL_ABSTDLG_CLASS(CuiAbstractController)
-IMPL_ABSTDLG_CLASS(CuiAbstractSingleTabController)
+IMPL_ABSTDLG_CLASS_ASYNC(CuiAbstractSingleTabController, SfxSingleTabDialogController)
 IMPL_ABSTDLG_CLASS_ASYNC(CuiAbstractWidgetTestControllerAsync,weld::GenericDialogController)
 
 short AbstractHyphenWordDialog_Impl::Execute()

cui/source/factory/dlgfact.hxx
@@ -114,7 +114,7 @@ DECL_ABSTDLG_CLASS_SHARED_ASYNC(CuiAbstractWidgetTestControllerAsync, VclAbstrac
 };
 
 // CuiAbstractSingleTabController_Impl
-DECL_ABSTDLG_CLASS_UNIQUE(CuiAbstractSingleTabController, SfxAbstractDialog, SfxSingleTabDialogController)
+DECL_ABSTDLG_CLASS_SHARED_ASYNC(CuiAbstractSingleTabController, SfxAbstractDialog, SfxSingleTabDialogController)
     virtual const SfxItemSet*   GetOutputItemSet() const override;
     //From class Window.
     virtual void          SetText( const OUString& rStr ) override;

sc/source/ui/drawfunc/drawsh.cxx
@@ -441,32 +441,39 @@ void ScDrawShell::ExecuteMacroAssign(SdrObject* pObj, weld::Window* pWin)
         xFrame = GetViewShell()->GetViewFrame().GetFrame().GetFrameInterface();
 
     SvxAbstractDialogFactory* pFact = SvxAbstractDialogFactory::Create();
-    ScopedVclPtr<SfxAbstractDialog> pMacroDlg(pFact->CreateEventConfigDialog( pWin, aItemSet, xFrame ));
-    if ( pMacroDlg->Execute() != RET_OK )
-        return;
-
-    const SfxItemSet* pOutSet = pMacroDlg->GetOutputItemSet();
-    const SvxMacroItem* pItem = pOutSet->GetItemIfSet( SID_ATTR_MACROITEM, false );
-    if( !pItem )
-        return;
-
-    OUString sMacro;
-    const SvxMacro* pMacro = pItem->GetMacroTable().Get( SvMacroItemId::OnClick );
-    if ( pMacro )
-        sMacro = pMacro->GetMacName();
-
-    if ( pObj->IsGroupObject() )
-    {
-        SdrObjList* pOL = pObj->GetSubList();
-        for (const rtl::Reference<SdrObject>& pChildObj : *pOL)
+    VclPtr<SfxAbstractDialog> pMacroDlg(pFact->CreateEventConfigDialog( pWin, aItemSet, xFrame ));
+    pMacroDlg->StartExecuteAsync(
+        [this, pMacroDlg, pObj, pInfo] (sal_Int32 nResult) mutable -> void
         {
-            pInfo = ScDrawLayer::GetMacroInfo( pChildObj.get(), true );
-            pInfo->SetMacro( sMacro );
+            ScopedVclPtr<SfxAbstractDialog> pDlgDisposer(std::move(pMacroDlg));
+            if (nResult != RET_OK)
+                return;
+
+            const SfxItemSet* pOutSet = pDlgDisposer->GetOutputItemSet();
+            const SvxMacroItem* pItem = pOutSet->GetItemIfSet( SID_ATTR_MACROITEM, false );
+            if( !pItem )
+                return;
+
+            OUString sMacro;
+            const SvxMacro* pMacro = pItem->GetMacroTable().Get( SvMacroItemId::OnClick );
+            if ( pMacro )
+                sMacro = pMacro->GetMacName();
+
+            if ( pObj->IsGroupObject() )
+            {
+                SdrObjList* pOL = pObj->GetSubList();
+                for (const rtl::Reference<SdrObject>& pChildObj : *pOL)
+                {
+                    pInfo = ScDrawLayer::GetMacroInfo( pChildObj.get(), true );
+                    pInfo->SetMacro( sMacro );
+                }
+            }
+            else
+                pInfo->SetMacro( sMacro );
+            setModified();
         }
-    }
-    else
-        pInfo->SetMacro( sMacro );
-    setModified();
+    );
+
 }
 
 void ScDrawShell::ExecuteLineDlg( const SfxRequest& rReq )

svx/source/dialog/imapwnd.cxx
@@ -665,15 +665,21 @@ void IMapWindow::DoMacroAssign()
     aSet.Put( aMacroItem );
 
     SvxAbstractDialogFactory* pFact = SvxAbstractDialogFactory::Create();
-    ScopedVclPtr<SfxAbstractDialog> pMacroDlg(pFact->CreateEventConfigDialog(GetDrawingArea(), aSet, mxDocumentFrame));
+    VclPtr<SfxAbstractDialog> pMacroDlg(pFact->CreateEventConfigDialog(GetDrawingArea(), aSet, mxDocumentFrame));
 
-    if ( pMacroDlg->Execute() == RET_OK )
-    {
-        const SfxItemSet* pOutSet = pMacroDlg->GetOutputItemSet();
-        pIMapObj->SetMacroTable( pOutSet->Get( SID_ATTR_MACROITEM ).GetMacroTable() );
-        pModel->SetChanged();
-        UpdateInfo( false );
-    }
+    pMacroDlg->StartExecuteAsync(
+        [this, pMacroDlg, pIMapObj] (sal_Int32 nResult)->void
+        {
+            if (nResult == RET_OK)
+            {
+                const SfxItemSet* pOutSet = pMacroDlg->GetOutputItemSet();
+                pIMapObj->SetMacroTable( pOutSet->Get( SID_ATTR_MACROITEM ).GetMacroTable() );
+                pModel->SetChanged();
+                UpdateInfo( false );
+            }
+            pMacroDlg->disposeOnce();
+        }
+    );
 }
 
 void IMapWindow::DoPropertyDialog()

sw/source/ui/misc/glossary.cxx
@@ -657,18 +657,26 @@ IMPL_LINK(SwGlossaryDlg, MenuHdl, const OUString&, rItemIdent, void)
         aSet.Put( aItem );
         aSet.Put( SwMacroAssignDlg::AddEvents( MACASSGN_AUTOTEXT ) );
 
-        const SvxMacroItem* pMacroItem;
         SvxAbstractDialogFactory* pFact = SvxAbstractDialogFactory::Create();
-        ScopedVclPtr<SfxAbstractDialog> pMacroDlg(pFact->CreateEventConfigDialog(m_xDialog.get(), aSet,
+        VclPtr<SfxAbstractDialog> pMacroDlg(pFact->CreateEventConfigDialog(m_xDialog.get(), aSet,
             m_pShell->GetView().GetViewFrame().GetFrame().GetFrameInterface() ));
-        if ( pMacroDlg && pMacroDlg->Execute() == RET_OK &&
-            (pMacroItem = pMacroDlg->GetOutputItemSet()->GetItemIfSet( RES_FRMMACRO, false )) )
-        {
-            const SvxMacroTableDtor& rTable = pMacroItem->GetMacroTable();
-            m_pGlossaryHdl->SetMacros( m_xShortNameEdit->get_text(),
-                                        rTable.Get( SvMacroItemId::SwStartInsGlossary ),
-                                        rTable.Get( SvMacroItemId::SwEndInsGlossary ) );
-        }
+        if (pMacroDlg)
+            pMacroDlg->StartExecuteAsync(
+                [this, pMacroDlg] (sal_Int32 nResult)->void
+                {
+                    if (nResult == RET_OK)
+                    {
+                        if (const SvxMacroItem* pMacroItem = pMacroDlg->GetOutputItemSet()->GetItemIfSet( RES_FRMMACRO, false ))
+                        {
+                            const SvxMacroTableDtor& rTable = pMacroItem->GetMacroTable();
+                            m_pGlossaryHdl->SetMacros( m_xShortNameEdit->get_text(),
+                                                        rTable.Get( SvMacroItemId::SwStartInsGlossary ),
+                                                        rTable.Get( SvMacroItemId::SwEndInsGlossary ) );
+                        }
+                    }
+                    pMacroDlg->disposeOnce();
+                }
+            );
     }
     else if (rItemIdent == "import")
     {

sw/source/ui/chrdlg/chardlg.cxx
@@ -308,9 +308,22 @@ IMPL_LINK_NOARG(SwCharURLPage, InsertFileHdl, weld::Button&, void)
 
 IMPL_LINK_NOARG(SwCharURLPage, EventHdl, weld::Button&, void)
 {
-    if (SwView* pView = GetActiveView())
-        m_bModified |= SwMacroAssignDlg::INetFormatDlg(GetFrameWeld(),
-                        pView->GetWrtShell(), m_oINetMacroTable);
+    SwView* pView = GetActiveView();
+    if (!pView)
+        return;
+
+    SvxMacroItem aItem( RES_FRMMACRO );
+    if( !m_oINetMacroTable )
+        m_oINetMacroTable.emplace();
+    else
+        aItem.SetMacroTable( *m_oINetMacroTable );
+
+    SwMacroAssignDlg::INetFormatDlg(GetFrameWeld(), pView->GetWrtShell(), aItem,
+        [this] (const SvxMacroItem& rItem)
+        {
+            m_oINetMacroTable.emplace(rItem.GetMacroTable());
+            m_bModified = true;
+        });
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sw/source/ui/dialog/macassgn.cxx
@@ -100,33 +100,32 @@ SfxEventNamesItem SwMacroAssignDlg::AddEvents( DlgEventType eType )
     return aItem;
 }
 
-bool SwMacroAssignDlg::INetFormatDlg(weld::Window* pParent, SwWrtShell& rSh,
-                                     std::optional<SvxMacroTableDtor>& rpINetItem )
+void SwMacroAssignDlg::INetFormatDlg(weld::Window* pParent, SwWrtShell& rSh, const SvxMacroItem& rItem,
+                                     std::function<void(const SvxMacroItem&)> onItemSelectedFunc )
 {
-    bool bRet = false;
     SfxItemSetFixed<RES_FRMMACRO, RES_FRMMACRO, SID_EVENTCONFIG, SID_EVENTCONFIG> aSet( rSh.GetAttrPool() );
-    SvxMacroItem aItem( RES_FRMMACRO );
-    if( !rpINetItem )
-        rpINetItem.emplace();
-    else
-        aItem.SetMacroTable( *rpINetItem );
-
-    aSet.Put( aItem );
+    aSet.Put( rItem );
     aSet.Put( AddEvents( MACASSGN_INETFMT ) );
 
     SvxAbstractDialogFactory* pFact = SvxAbstractDialogFactory::Create();
-    ScopedVclPtr<SfxAbstractDialog> pMacroDlg( pFact->CreateEventConfigDialog(pParent, aSet,
+    VclPtr<SfxAbstractDialog> pMacroDlg( pFact->CreateEventConfigDialog(pParent, aSet,
         rSh.GetView().GetViewFrame().GetFrame().GetFrameInterface() ) );
-    if ( pMacroDlg && pMacroDlg->Execute() == RET_OK )
-    {
-        const SfxItemSet* pOutSet = pMacroDlg->GetOutputItemSet();
-        if( const SvxMacroItem* pItem = pOutSet->GetItemIfSet( RES_FRMMACRO, false ))
+    if ( !pMacroDlg )
+        return;
+    pMacroDlg->StartExecuteAsync(
+        [pMacroDlg, onItemSelectedFunc] (sal_Int32 nResult)->void
         {
-            rpINetItem.emplace(pItem->GetMacroTable());
-            bRet = true;
+            if (nResult == RET_OK)
+            {
+                const SfxItemSet* pOutSet = pMacroDlg->GetOutputItemSet();
+                if( const SvxMacroItem* pItem = pOutSet->GetItemIfSet( RES_FRMMACRO, false ))
+                {
+                    onItemSelectedFunc(*pItem);
+                }
+            }
+            pMacroDlg->disposeOnce();
         }
-    }
-    return bRet;
+    );
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sw/source/uibase/inc/macassgn.hxx
@@ -40,8 +40,8 @@ class SwMacroAssignDlg
 {
 public:
     static SfxEventNamesItem AddEvents(DlgEventType eType);
-    static bool INetFormatDlg(weld::Window* pParent, SwWrtShell& rSh,
-                              std::optional<SvxMacroTableDtor>& rpINetItem);
+    static void INetFormatDlg(weld::Window* pParent, SwWrtShell& rSh, const SvxMacroItem& rItem,
+                              std::function<void(const SvxMacroItem&)> onItemSelectedFunc);
 };
 
 #endif

commit 514d7384470434a0b0b119e369ff615e2a1499c9

tdf#161968 UI Assign macro to shape crashes CALC
regression from
    commit f690e8218a74a0f7792407a1d46198883d2528ff
    Author: Noel Grandin <noel.grandin@collabora.co.uk>
    Date:   Tue Jan 23 13:24:29 2024 +0200
    make event config dialog async

Change-Id: Ie68aeb1171831da3b457afd945ccaaa248e652af
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/171370
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Jenkins

cui/source/dialogs/hltpbase.cxx
@@ -334,12 +334,12 @@ IMPL_LINK_NOARG(SvxHyperlinkTabPageBase, ClickScriptHdl_Impl, weld::Button&, voi
         aItem.SetMacroTable( *pMacroTbl );
 
     // create empty itemset for macro-dlg
-    SfxItemSetFixed<SID_ATTR_MACROITEM, SID_ATTR_MACROITEM> aItemSet( SfxGetpApp()->GetPool() );
-    aItemSet.Put ( aItem );
+    auto xItemSet = std::make_unique<SfxItemSetFixed<SID_ATTR_MACROITEM, SID_ATTR_MACROITEM>>( SfxGetpApp()->GetPool() );
+    xItemSet->Put( aItem );
 
     DisableClose( true );
 
-    SfxMacroAssignDlg aDlg(mpDialog->getDialog(), mxDocumentFrame, aItemSet);
+    SfxMacroAssignDlg aDlg(mpDialog->getDialog(), mxDocumentFrame, std::move(xItemSet));
 
     // add events
     SfxMacroTabPage *pMacroPage = aDlg.GetTabPage();

cui/source/factory/dlgfact.cxx
@@ -1212,10 +1212,10 @@ VclPtr<SfxAbstractDialog> AbstractDialogFactory_Impl::CreateCharMapDialog(weld::
 }
 
 VclPtr<SfxAbstractDialog> AbstractDialogFactory_Impl::CreateEventConfigDialog(weld::Widget* pParent,
-                                                                              const SfxItemSet& rAttr,
+                                                                              std::unique_ptr<const SfxItemSet> xAttr,
                                                                               const Reference< XFrame >& rDocumentFrame)
 {
-    return VclPtr<CuiAbstractSingleTabController_Impl>::Create(std::make_unique<SfxMacroAssignDlg>(pParent, rDocumentFrame, rAttr));
+    return VclPtr<CuiAbstractSingleTabController_Impl>::Create(std::make_unique<SfxMacroAssignDlg>(pParent, rDocumentFrame, std::move(xAttr)));
 }
 
 VclPtr<SfxAbstractDialog> AbstractDialogFactory_Impl::CreateSfxDialog(weld::Window* pParent,

cui/source/factory/dlgfact.hxx
@@ -444,7 +444,7 @@ public:
                                                              const SfxItemSet& rAttr,
                                                              const css::uno::Reference< css::frame::XFrame >& rFrame) override;
     virtual VclPtr<SfxAbstractDialog>    CreateEventConfigDialog(weld::Widget* pParent,
-                                                                 const SfxItemSet& rAttr,
+                                                                 std::unique_ptr<const SfxItemSet> xAttr,
                                                                  const css::uno::Reference< css::frame::XFrame >& rFrame) override;
     virtual VclPtr<VclAbstractDialog>    CreateFrameDialog(weld::Window* pParent, const css::uno::Reference< css::frame::XFrame >& rxFrame,
                                                            sal_uInt32 nResId, sal_uInt16 nPageId, const OUString& rParameter) override;

cui/source/inc/macroass.hxx
@@ -80,11 +80,13 @@ class SfxMacroAssignDlg : public SfxSingleTabDialogController
 public:
     SfxMacroAssignDlg(weld::Widget* pParent,
                       const css::uno::Reference< css::frame::XFrame >& rxDocumentFrame,
-                      const SfxItemSet& rSet);
+                      std::unique_ptr<const SfxItemSet> xSet);
     SfxMacroTabPage* GetTabPage()
     {
         return static_cast<SfxMacroTabPage*>(m_xSfxPage.get());
     }
+private:
+    std::unique_ptr<const SfxItemSet> mxItemSet;
 };
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

cui/source/tabpages/macroass.cxx
@@ -387,11 +387,13 @@ std::unique_ptr<SfxTabPage> SfxMacroTabPage::Create(weld::Container* pPage, weld
 }
 
 SfxMacroAssignDlg::SfxMacroAssignDlg(weld::Widget* pParent,
-    const Reference< XFrame >& rxDocumentFrame, const SfxItemSet& rSet)
-    : SfxSingleTabDialogController(pParent, &rSet,u"cui/ui/eventassigndialog.ui"_ustr,
-                                   u"EventAssignDialog"_ustr)
+    const Reference< XFrame >& rxDocumentFrame, std::unique_ptr<const SfxItemSet> xSet)
+    : SfxSingleTabDialogController(pParent, nullptr, u"cui/ui/eventassigndialog.ui"_ustr,
+                                   u"EventAssignDialog"_ustr),
+      mxItemSet(std::move(xSet))
 {
-    std::unique_ptr<SfxMacroTabPage> xPage = CreateSfxMacroTabPage(get_content_area(), this, rSet);
+    SetInputSet(mxItemSet.get());
+    std::unique_ptr<SfxMacroTabPage> xPage = CreateSfxMacroTabPage(get_content_area(), this, *mxItemSet);
     xPage->SetFrame(rxDocumentFrame);
     SetTabPage(std::move(xPage));
     GetTabPage()->LaunchFillGroup();

include/svx/svxdlg.hxx
@@ -418,7 +418,7 @@ public:
                                                                         sal_uInt32 nResId )=0;
     virtual VclPtr<SfxAbstractDialog>       CreateCharMapDialog(weld::Window* pParent, const SfxItemSet& rAttr,
                                                                 const css::uno::Reference<css::frame::XFrame>& rFrame) = 0;
-    virtual VclPtr<SfxAbstractDialog>       CreateEventConfigDialog(weld::Widget* pParent, const SfxItemSet& rAttr,
+    virtual VclPtr<SfxAbstractDialog>       CreateEventConfigDialog(weld::Widget* pParent, std::unique_ptr<const SfxItemSet> xAttr,
                                                                     const css::uno::Reference< css::frame::XFrame >& rFrame) = 0;
     virtual VclPtr<AbstractSvxPostItDialog>    CreateSvxPostItDialog(weld::Widget* pParent, const SfxItemSet& rCoreSet, bool bPrevNext = false) = 0;
     virtual VclPtr<VclAbstractDialog>          CreateSvxScriptOrgDialog(weld::Window* pParent, const OUString& rLanguage) override = 0;

sc/source/ui/drawfunc/drawsh.cxx
@@ -426,19 +426,19 @@ void ScDrawShell::ExecuteMacroAssign(SdrObject* pObj, weld::Window* pWin)
     }
 
     // create empty itemset for macro-dlg
-    SfxItemSetFixed<SID_ATTR_MACROITEM, SID_ATTR_MACROITEM, SID_EVENTCONFIG, SID_EVENTCONFIG> aItemSet(SfxGetpApp()->GetPool() );
-    aItemSet.Put ( aItem );
+    auto xItemSet = std::make_unique<SfxItemSetFixed<SID_ATTR_MACROITEM, SID_ATTR_MACROITEM, SID_EVENTCONFIG, SID_EVENTCONFIG>>( SfxGetpApp()->GetPool() );
+    xItemSet->Put ( aItem );
 
     SfxEventNamesItem aNamesItem(SID_EVENTCONFIG);
     aNamesItem.AddEvent( ScResId(RID_SCSTR_ONCLICK), OUString(), SvMacroItemId::OnClick );
-    aItemSet.Put( aNamesItem );
+    xItemSet->Put( aNamesItem );
 
     css::uno::Reference < css::frame::XFrame > xFrame;
     if (GetViewShell())
         xFrame = GetViewShell()->GetViewFrame().GetFrame().GetFrameInterface();
 
     SvxAbstractDialogFactory* pFact = SvxAbstractDialogFactory::Create();
-    VclPtr<SfxAbstractDialog> pMacroDlg(pFact->CreateEventConfigDialog( pWin, aItemSet, xFrame ));
+    VclPtr<SfxAbstractDialog> pMacroDlg(pFact->CreateEventConfigDialog( pWin, std::move(xItemSet), xFrame ));
     pMacroDlg->StartExecuteAsync(
         [this, pMacroDlg, pObj, pInfo] (sal_Int32 nResult) mutable -> void
         {

svx/source/dialog/imapwnd.cxx
@@ -673,21 +673,21 @@ void IMapWindow::DoMacroAssign()
     if ( !pSdrObj )
         return;
 
-    SfxItemSetFixed<SID_ATTR_MACROITEM, SID_ATTR_MACROITEM, SID_EVENTCONFIG, SID_EVENTCONFIG>
-        aSet(*pIMapPool);
+    auto xSet = std::make_unique<SfxItemSetFixed<SID_ATTR_MACROITEM, SID_ATTR_MACROITEM, SID_EVENTCONFIG, SID_EVENTCONFIG>>
+        (*pIMapPool);
 
     SfxEventNamesItem aNamesItem(SID_EVENTCONFIG);
     aNamesItem.AddEvent( u"MouseOver"_ustr, u""_ustr, SvMacroItemId::OnMouseOver );
     aNamesItem.AddEvent( u"MouseOut"_ustr, u""_ustr, SvMacroItemId::OnMouseOut );
-    aSet.Put( aNamesItem );
+    xSet->Put( aNamesItem );
 
     SvxMacroItem    aMacroItem(SID_ATTR_MACROITEM);
     IMapObject*     pIMapObj = GetIMapObj( pSdrObj );
     aMacroItem.SetMacroTable( pIMapObj->GetMacroTable() );
-    aSet.Put( aMacroItem );
+    xSet->Put( aMacroItem );
 
     SvxAbstractDialogFactory* pFact = SvxAbstractDialogFactory::Create();
-    VclPtr<SfxAbstractDialog> pMacroDlg(pFact->CreateEventConfigDialog(GetDrawingArea(), aSet, mxDocumentFrame));
+    VclPtr<SfxAbstractDialog> pMacroDlg(pFact->CreateEventConfigDialog(GetDrawingArea(), std::move(xSet), mxDocumentFrame));
 
     pMacroDlg->StartExecuteAsync(
         [this, pMacroDlg, pIMapObj] (sal_Int32 nResult)->void

sw/source/ui/misc/glossary.cxx
@@ -633,7 +633,7 @@ IMPL_LINK(SwGlossaryDlg, MenuHdl, const OUString&, rItemIdent, void)
     }
     else if (rItemIdent == "macro")
     {
-        SfxItemSetFixed<RES_FRMMACRO, RES_FRMMACRO, SID_EVENTCONFIG, SID_EVENTCONFIG> aSet( m_pShell->GetAttrPool() );
+        auto xSet = std::make_unique<SfxItemSetFixed<RES_FRMMACRO, RES_FRMMACRO, SID_EVENTCONFIG, SID_EVENTCONFIG>>( m_pShell->GetAttrPool() );
 
         SvxMacro aStart(OUString(), OUString(), STARBASIC);
         SvxMacro aEnd(OUString(), OUString(), STARBASIC);
@@ -645,11 +645,11 @@ IMPL_LINK(SwGlossaryDlg, MenuHdl, const OUString&, rItemIdent, void)
         if( aEnd.HasMacro() )
             aItem.SetMacro( SvMacroItemId::SwEndInsGlossary, aEnd );
 
-        aSet.Put( aItem );
-        aSet.Put( SwMacroAssignDlg::AddEvents( MACASSGN_AUTOTEXT ) );
+        xSet->Put( aItem );
+        xSet->Put( SwMacroAssignDlg::AddEvents( MACASSGN_AUTOTEXT ) );
 
         SvxAbstractDialogFactory* pFact = SvxAbstractDialogFactory::Create();
-        VclPtr<SfxAbstractDialog> pMacroDlg(pFact->CreateEventConfigDialog(m_xDialog.get(), aSet,
+        VclPtr<SfxAbstractDialog> pMacroDlg(pFact->CreateEventConfigDialog(m_xDialog.get(), std::move(xSet),
             m_pShell->GetView().GetViewFrame().GetFrame().GetFrameInterface() ));
         if (pMacroDlg)
             pMacroDlg->StartExecuteAsync(