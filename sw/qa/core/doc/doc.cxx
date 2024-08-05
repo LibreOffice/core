LibreOffice/core

commit c761df1e42fd11acc5fc05b0baacd803c3788ca6

tdf#84678 DOCX import: fix handling of textbox margins
Regression from commit d379d18666aa42031359ca8eb34b0021960347ae (oox:
import WPS shape with text as shape with textbox, 2014-06-18), the
problem for a long time was that in Writer text frames without borders
couldn't have text margins, either.

Recently a compat setting was added in the WW8 filter for this
situation, so use that in the DOCX import as well, and improve
SwTextBoxHelper to handle the margin properties.

Change-Id: I472bbc414f21f2ec7334482c460a5c3be3e95e94
Reviewed-on: https://gerrit.libreoffice.org/30255
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>

sw/source/core/doc/textboxhelper.cxx
@@ -32,6 +32,7 @@
 
 #include <editeng/unoprnms.hxx>
 #include <editeng/charrotateitem.hxx>
+#include <editeng/memberids.hrc>
 #include <svx/svdoashp.hxx>
 #include <svx/svdpage.hxx>
 #include <svl/itemiter.hxx>
@@ -354,6 +355,14 @@ void SwTextBoxHelper::syncProperty(SwFrameFormat* pShape, const OUString& rPrope
         syncProperty(pShape, RES_TEXT_VERT_ADJUST, 0, rValue);
     else if (rPropertyName == UNO_NAME_TEXT_AUTOGROWHEIGHT)
         syncProperty(pShape, RES_FRM_SIZE, MID_FRMSIZE_IS_AUTO_HEIGHT, rValue);
+    else if (rPropertyName == UNO_NAME_TEXT_LEFTDIST)
+        syncProperty(pShape, RES_BOX, LEFT_BORDER_DISTANCE, rValue);
+    else if (rPropertyName == UNO_NAME_TEXT_RIGHTDIST)
+        syncProperty(pShape, RES_BOX, RIGHT_BORDER_DISTANCE, rValue);
+    else if (rPropertyName == UNO_NAME_TEXT_UPPERDIST)
+        syncProperty(pShape, RES_BOX, TOP_BORDER_DISTANCE, rValue);
+    else if (rPropertyName == UNO_NAME_TEXT_LOWERDIST)
+        syncProperty(pShape, RES_BOX, BOTTOM_BORDER_DISTANCE, rValue);
 }
 
 void SwTextBoxHelper::getProperty(SwFrameFormat* pShape, sal_uInt16 nWID, sal_uInt8 nMemberId, css::uno::Any& rValue)
@@ -490,6 +499,23 @@ void SwTextBoxHelper::syncProperty(SwFrameFormat* pShape, sal_uInt16 nWID, sal_u
             break;
         case RES_TEXT_VERT_ADJUST:
             aPropertyName = UNO_NAME_TEXT_VERT_ADJUST;
+            break;
+        case RES_BOX:
+            switch (nMemberId)
+            {
+            case LEFT_BORDER_DISTANCE:
+                aPropertyName = UNO_NAME_LEFT_BORDER_DISTANCE;
+                break;
+            case RIGHT_BORDER_DISTANCE:
+                aPropertyName = UNO_NAME_RIGHT_BORDER_DISTANCE;
+                break;
+            case TOP_BORDER_DISTANCE:
+                aPropertyName = UNO_NAME_TOP_BORDER_DISTANCE;
+                break;
+            case BOTTOM_BORDER_DISTANCE:
+                aPropertyName = UNO_NAME_BOTTOM_BORDER_DISTANCE;
+                break;
+            }
             break;
         }
 

sw/qa/extras/ooxmlexport/ooxmlexport9.cxx
@@ -63,6 +63,13 @@ DECLARE_OOXMLEXPORT_TEST(testTdf103389, "tdf103389.docx")
     assertXPath(pXmlDoc, "/w:document/w:body/w:p[2]/w:r/mc:AlternateContent/mc:Choice/w:drawing/wp:inline/a:graphic/a:graphicData/wpg:wgp/wps:wsp/wps:spPr/a:prstGeom", "prst", "rect");
 }
 
+DECLARE_OOXMLEXPORT_TEST(testTdf84678, "tdf84678.docx")
+{
+    // This was 0, left margin inside a shape+text wasn't imported from DOCX.
+    // 360000 EMU, but layout uses twips.
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(567), parseDump("/root/page/body/txt/anchored/fly/infos/prtBounds", "left").toInt32());
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

writerfilter/source/filter/WriterFilter.cxx
@@ -299,6 +299,7 @@ void WriterFilter::setTargetDocument(const uno::Reference< lang::XComponent >& x
     xSettings->setPropertyValue("TabOverMargin", uno::makeAny(true));
     xSettings->setPropertyValue("TreatSingleColumnBreakAsPageBreak", uno::makeAny(true));
     xSettings->setPropertyValue("PropLineSpacingShrinksFirstLine", uno::makeAny(true));
+    xSettings->setPropertyValue("AllowSpacingWithoutBorders", uno::makeAny(true));
 }
 
 void WriterFilter::setSourceDocument(const uno::Reference< lang::XComponent >& xDoc) throw (lang::IllegalArgumentException, uno::RuntimeException, std::exception)

commit f31c60023243459b0565945790b094d10fe1c13c

tdf#162075 sw textbox: map a shape's wordwrap=no to the frame's width type
Regression from commit c761df1e42fd11acc5fc05b0baacd803c3788ca6
(tdf#84678 DOCX import: fix handling of textbox margins, 2016-10-25),
the header hape was supposed to have a single line, but it had a
separate line for each character in the shape text.

What happened is that the nominal size of the shape was not enough to
provide enough space for the text, now that we handle spacing between
the frame border and text, and the shape had automatic height set,
instead of automatic width.

Fix the problem by extending SwTextBoxHelper::create() to map the
shape's TextWordWrap property to the inner frame's WidthType. As the
tdf#81567 testcase points out, this requires extending the matching
shape update code as well, because word wrap is always disabled
initially, and it's enabled only after the shape's text is processed.

Also adjust sw/qa/extras/layout/data/tdf138951.odt, which expected word
wrap but didn't request it -- the original tdf#138951 bugdoc keeps
working after this commit.

Change-Id: I17c896f83731a6653e3a0a1493949e27d5259232
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/171475
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

sw/source/core/doc/textboxhelper.cxx
@@ -170,6 +170,10 @@ void SwTextBoxHelper::create(SwFrameFormat* pShape, SdrObject* pObject, bool bCo
                  xShapePropertySet->getPropertyValue(UNO_NAME_VERT_ORIENT_POSITION), pObject);
     syncProperty(pShape, RES_FRM_SIZE, MID_FRMSIZE_IS_AUTO_HEIGHT,
                  xShapePropertySet->getPropertyValue(UNO_NAME_TEXT_AUTOGROWHEIGHT), pObject);
+    // tdf#162075 shape word wrap to frame width type on shape creation.
+    bool bTextWordwrap = xShapePropertySet->getPropertyValue(UNO_NAME_TEXT_WORDWRAP).get<bool>();
+    syncProperty(pShape, RES_FRM_SIZE, MID_FRMSIZE_WIDTH_TYPE,
+                 uno::Any(bTextWordwrap ? text::SizeType::FIX : text::SizeType::MIN), pObject);
     syncProperty(pShape, RES_TEXT_VERT_ADJUST, 0,
                  xShapePropertySet->getPropertyValue(UNO_NAME_TEXT_VERT_ADJUST), pObject);
     text::WritingMode eMode;
@@ -665,6 +669,16 @@ void SwTextBoxHelper::syncProperty(SwFrameFormat* pShape, std::u16string_view rP
         sal_Int16 eMode2;
         if (rValue >>= eMode2)
             syncProperty(pShape, RES_FRAMEDIR, 0, uno::Any(eMode2), pObj);
+    }
+    else if (rPropertyName == u"TextWordWrap")
+    {
+        // tdf#81567 shape word wrap to frame width type on shape update.
+        bool bTextWordwrap{};
+        if (rValue >>= bTextWordwrap)
+        {
+            syncProperty(pShape, RES_FRM_SIZE, MID_FRMSIZE_WIDTH_TYPE,
+                         uno::Any(bTextWordwrap ? text::SizeType::FIX : text::SizeType::MIN), pObj);
+        }
     }
     else
         SAL_INFO("sw.core", "SwTextBoxHelper::syncProperty: unhandled property: "

sw/qa/core/doc/doc.cxx
@@ -641,6 +641,25 @@ CPPUNIT_TEST_FIXTURE(SwCoreDocTest, testDefaultPageDescUsed)
     CPPUNIT_ASSERT(rPageDesc.IsUsed());
 }
 
+CPPUNIT_TEST_FIXTURE(SwCoreDocTest, testTextBoxWordWrap)
+{
+    // Given a document with a shape in the header that extends horizontally when there is enough
+    // content:
+    createSwDoc("text-box-word-wrap.docx");
+
+    // When checking the layout size of the shape:
+    xmlDocUniquePtr pXmlDoc = parseLayoutDump();
+    auto nFlyHeight
+        = getXPath(pXmlDoc, "//anchored/fly/infos/bounds"_ostr, "height"_ostr).toInt32();
+
+    // Then make sure it has a small height, hosting just one line:
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 773
+    // - Actual  : 5183
+    // i.e. the shape had new lines for each character instead of 1 line.
+    CPPUNIT_ASSERT_LESS(static_cast<sal_Int32>(1000), nFlyHeight);
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */