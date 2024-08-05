LibreOffice/core

commit 04184aa7e3aada8f4d938d20dfdb54b3a7dd3896

tdf#157829 sw: Implemented line break underflow for bidi portions
Previously, bidi portions did not handle underflow in word wrapping.
This caused bidi portions to break at format changes during wrapping,
instead of backtracking to a valid break opportunity.

Change-Id: Ib86effe2642a9d133242c28538026d9b4a7e90e9
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/171006
Tested-by: Jenkins
Reviewed-by: Jonathan Clark <jonathan@libreoffice.org>

sw/source/core/text/pormulti.cxx
@@ -1923,7 +1923,7 @@ static bool lcl_ExtractFieldFollow( SwLineLayout* pLine, SwLinePortion* &rpField
 // next line, this function is called to truncate
 // the rest of the remaining multi portion
 static void lcl_TruncateMultiPortion(SwMultiPortion& rMulti, SwTextFormatInfo& rInf,
-                                     TextFrameIndex const nStartIdx, bool bIsBidiPortion)
+                                     TextFrameIndex const nStartIdx)
 {
     rMulti.GetRoot().Truncate();
     rMulti.GetRoot().SetLen(TextFrameIndex(0));
@@ -1939,28 +1939,25 @@ static void lcl_TruncateMultiPortion(SwMultiPortion& rMulti, SwTextFormatInfo& r
     rMulti.SetLen(TextFrameIndex(0));
     rInf.SetIdx( nStartIdx );
 
-    if (bIsBidiPortion)
+    if (rMulti.IsBidi())
     {
         // The truncated portion is a bidi portion. Bidi portions contain ordinary text, and may
         // potentially underflow in the case that none of the text fits on the current line.
-        if (auto* pPrevPor = rInf.GetLast();
-            pPrevPor && pPrevPor->GetWhichPor() == PortionType::Text)
-        {
-            // Check if the start of the bidi portion is a valid break. In that case, truncating
-            // the multi portion is sufficient.
-            css::i18n::LineBreakHyphenationOptions aHyphOptions;
-            css::i18n::LineBreakUserOptions aUserOptions;
-            css::lang::Locale aLocale;
-            auto aResult = g_pBreakIt->GetBreakIter()->getLineBreak(
-                rInf.GetText(), sal_Int32(nStartIdx), aLocale, sal_Int32(rInf.GetLineStart()),
-                aHyphOptions, aUserOptions);
 
-            if (aResult.breakIndex != sal_Int32{ nStartIdx })
-            {
-                // The start of the bidi portion is not a valid break. Instead, a break should be
-                // inserted into a previous text portion on this line.
-                rInf.SetUnderflow(&rMulti);
-            }
+        // Check if the start of the bidi portion is a valid break. In that case, truncating
+        // the multi portion is sufficient.
+        css::i18n::LineBreakHyphenationOptions aHyphOptions;
+        css::i18n::LineBreakUserOptions aUserOptions;
+        css::lang::Locale aLocale;
+        auto aResult = g_pBreakIt->GetBreakIter()->getLineBreak(
+            rInf.GetText(), sal_Int32(nStartIdx), aLocale, sal_Int32(rInf.GetLineStart()),
+            aHyphOptions, aUserOptions);
+
+        if (aResult.breakIndex != sal_Int32{ nStartIdx })
+        {
+            // The start of the bidi portion is not a valid break. Instead, a break should be
+            // inserted into a previous text portion on this line.
+            rInf.SetUnderflow(&rMulti);
         }
     }
 }
@@ -2119,6 +2116,15 @@ bool SwTextFormatter::BuildMultiPortion( SwTextFormatInfo &rInf,
         aInf.SetFirstMulti( bFirstMulti );
         aInf.SetNumDone( rInf.IsNumDone() );
         aInf.SetFootnoteDone( rInf.IsFootnoteDone() );
+
+        // tdf#157829: Bidi portions contain text; word wrapping should underflow.
+        // By default, the SwTextFormatInfo constructor assumes the current index is the start of
+        // a new line. As a result, Writer cut breaks MultiPortions as if they were wider than the
+        // entire document. This is incorrect behavior for bidi portions.
+        if (rMulti.IsBidi())
+        {
+            aInf.SetLineStart(rInf.GetLineStart());
+        }
 
         // if there's a bookmark at the start of the MultiPortion, it will be
         // painted with the rotation etc. of the MultiPortion; move it *inside*
@@ -2373,7 +2379,7 @@ bool SwTextFormatter::BuildMultiPortion( SwTextFormatInfo &rInf,
             else
             {
                 // we try to keep our ruby portion together
-                lcl_TruncateMultiPortion(rMulti, rInf, nStartIdx, /*bIsBidiPortion=*/false);
+                lcl_TruncateMultiPortion(rMulti, rInf, nStartIdx);
                 pTmp = nullptr;
                 // A follow field portion may still be waiting. If nobody wants
                 // it, we delete it.
@@ -2383,7 +2389,7 @@ bool SwTextFormatter::BuildMultiPortion( SwTextFormatInfo &rInf,
         else if( rMulti.HasRotation() )
         {
             // we try to keep our rotated portion together
-            lcl_TruncateMultiPortion(rMulti, rInf, nStartIdx, /*bIsBidiPortion=*/false);
+            lcl_TruncateMultiPortion(rMulti, rInf, nStartIdx);
             pTmp = new SwRotatedPortion( nMultiLen + rInf.GetIdx(),
                                          rMulti.GetDirection() );
         }
@@ -2391,9 +2397,9 @@ bool SwTextFormatter::BuildMultiPortion( SwTextFormatInfo &rInf,
         // a new SwBidiPortion, this would cause a memory leak
         else if( rMulti.IsBidi() && ! m_pMulti )
         {
-            if (!rMulti.GetLen())
+            if (aInf.IsUnderflow() || !rMulti.GetLen())
             {
-                lcl_TruncateMultiPortion(rMulti, rInf, nStartIdx, /*bIsBidiPortion=*/true);
+                lcl_TruncateMultiPortion(rMulti, rInf, nStartIdx);
             }
 
             // If there is a HolePortion at the end of the bidi portion,

sw/qa/extras/layout/layout3.cxx
@@ -3119,6 +3119,34 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter3, TestTdf146081)
     CPPUNIT_ASSERT_EQUAL(nTotalHeight, nHeight1 * 4);
 }
 
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter3, TestTdf157829LTR)
+{
+    // Verify that line breaking inside a bidi portion triggers underflow to previous bidi portions
+    createSwDoc("tdf157829-ltr.fodt");
+    auto pXmlDoc = parseLayoutDump();
+
+    assertXPath(pXmlDoc, "//page"_ostr, 1);
+
+    assertXPath(pXmlDoc, "/root/page/body/txt/SwParaPortion/SwLineLayout[1]"_ostr, "portion"_ostr,
+                u"English English English "_ustr);
+    assertXPath(pXmlDoc, "/root/page/body/txt/SwParaPortion/SwLineLayout[2]"_ostr, "portion"_ostr,
+                u"עברית English"_ustr);
+}
+
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter3, TestTdf157829RTL)
+{
+    // Verify that line breaking inside a bidi portion triggers underflow to previous bidi portions
+    createSwDoc("tdf157829-rtl.fodt");
+    auto pXmlDoc = parseLayoutDump();
+
+    assertXPath(pXmlDoc, "//page"_ostr, 1);
+
+    assertXPath(pXmlDoc, "/root/page/body/txt/SwParaPortion/SwLineLayout[1]"_ostr, "portion"_ostr,
+                u"עברית עברית עברית עברית "_ustr);
+    assertXPath(pXmlDoc, "/root/page/body/txt/SwParaPortion/SwLineLayout[2]"_ostr, "portion"_ostr,
+                u"English עברית"_ustr);
+}
+
 } // end of anonymous namespace
 
 CPPUNIT_PLUGIN_IMPLEMENT();

commit cb7c70cd19237268b748d3e35f8bbed2150042ea

tdf#162314 sw: Fix hang while laying out bidi portions with underflow
Regression from 04184aa7e3aada8f4d938d20dfdb54b3a7dd3896
(tdf#157829 sw: Implemented line break underflow for bidi portions)

Change-Id: If5d550bd6e0f1099bc37532b2172436c12805104
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/171432
Reviewed-by: Jonathan Clark <jonathan@libreoffice.org>
Tested-by: Jenkins

sw/source/core/text/pormulti.cxx
@@ -1921,11 +1921,63 @@ static bool lcl_ExtractFieldFollow( SwLineLayout* pLine, SwLinePortion* &rpField
     return bRet;
 }
 
+// Determines if any part of the bidi portion fits on the current line
+namespace
+{
+enum class BidiTruncationType
+{
+    None,
+    Truncate,
+    Underflow
+};
+
+BidiTruncationType lcl_BidiPortionNeedsTruncation(SwMultiPortion& rMulti,
+                                                  SwTextFormatInfo& rExternalInf,
+                                                  SwTextFormatInfo& rLocalInf,
+                                                  TextFrameIndex const nStartIdx)
+{
+    if (!rLocalInf.IsUnderflow())
+    {
+        // Some amount of text fits in the bidi portion without triggering underflow,
+        // so the portion should not be truncated.
+        return BidiTruncationType::None;
+    }
+
+    auto nCurrLen = rMulti.GetLen();
+
+    css::i18n::LineBreakHyphenationOptions aHyphOptions;
+    css::i18n::LineBreakUserOptions aUserOptions;
+    css::lang::Locale aLocale;
+    auto aResult = g_pBreakIt->GetBreakIter()->getLineBreak(
+        rExternalInf.GetText(), sal_Int32(nStartIdx + nCurrLen), aLocale,
+        sal_Int32(rExternalInf.GetLineStart()), aHyphOptions, aUserOptions);
+
+    if (aResult.breakIndex < sal_Int32(nStartIdx))
+    {
+        // The bidi portion doesn't fit on the line, and the first break opportunity
+        // is before the bidi portion. Underflow to the preceding text.
+        return BidiTruncationType::Underflow;
+    }
+
+    if (aResult.breakIndex > sal_Int32(nStartIdx)
+        && aResult.breakIndex <= sal_Int32(nStartIdx + nCurrLen))
+    {
+        // The bidi portion fits on this line, but ended with underflow.
+        return BidiTruncationType::None;
+    }
+
+    // The bidi portion doesn't fit on the line, but a break position exists between the bidi
+    // portion and the preceding text. Truncating is sufficient.
+    return BidiTruncationType::Truncate;
+}
+}
+
 // If a multi portion completely has to go to the
 // next line, this function is called to truncate
 // the rest of the remaining multi portion
 static void lcl_TruncateMultiPortion(SwMultiPortion& rMulti, SwTextFormatInfo& rInf,
-                                     TextFrameIndex const nStartIdx)
+                                     TextFrameIndex const nStartIdx,
+                                     BidiTruncationType nBidiTruncType = BidiTruncationType::None)
 {
     rMulti.GetRoot().Truncate();
     rMulti.GetRoot().SetLen(TextFrameIndex(0));
@@ -1945,17 +1997,7 @@ static void lcl_TruncateMultiPortion(SwMultiPortion& rMulti, SwTextFormatInfo& r
     {
         // The truncated portion is a bidi portion. Bidi portions contain ordinary text, and may
         // potentially underflow in the case that none of the text fits on the current line.
-
-        // Check if the start of the bidi portion is a valid break. In that case, truncating
-        // the multi portion is sufficient.
-        css::i18n::LineBreakHyphenationOptions aHyphOptions;
-        css::i18n::LineBreakUserOptions aUserOptions;
-        css::lang::Locale aLocale;
-        auto aResult = g_pBreakIt->GetBreakIter()->getLineBreak(
-            rInf.GetText(), sal_Int32(nStartIdx), aLocale, sal_Int32(rInf.GetLineStart()),
-            aHyphOptions, aUserOptions);
-
-        if (aResult.breakIndex != sal_Int32{ nStartIdx })
+        if (nBidiTruncType == BidiTruncationType::Underflow)
         {
             // The start of the bidi portion is not a valid break. Instead, a break should be
             // inserted into a previous text portion on this line.
@@ -2399,9 +2441,10 @@ bool SwTextFormatter::BuildMultiPortion( SwTextFormatInfo &rInf,
         // a new SwBidiPortion, this would cause a memory leak
         else if( rMulti.IsBidi() && ! m_pMulti )
         {
-            if (aInf.IsUnderflow() || !rMulti.GetLen())
+            auto nTruncType = lcl_BidiPortionNeedsTruncation(rMulti, rInf, aInf, nStartIdx);
+            if (nTruncType != BidiTruncationType::None)
             {
-                lcl_TruncateMultiPortion(rMulti, rInf, nStartIdx);
+                lcl_TruncateMultiPortion(rMulti, rInf, nStartIdx, nTruncType);
             }
 
             // If there is a HolePortion at the end of the bidi portion,

sw/qa/extras/layout/data/tdf162314.fodt
@@ -1,0 +1,131 @@
+<?xml version='1.0' encoding='UTF-8'?>
+<office:document xmlns:css3t="http://www.w3.org/TR/css3-text/" xmlns:grddl="http://www.w3.org/2003/g/data-view#" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xforms="http://www.w3.org/2002/xforms" xmlns:dom="http://www.w3.org/2001/xml-events" xmlns:script="urn:oasis:names:tc:opendocument:xmlns:script:1.0" xmlns:form="urn:oasis:names:tc:opendocument:xmlns:form:1.0" xmlns:math="http://www.w3.org/1998/Math/MathML" xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0" xmlns:ooo="http://openoffice.org/2004/office" xmlns:fo="urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0" xmlns:config="urn:oasis:names:tc:opendocument:xmlns:config:1.0" xmlns:ooow="http://openoffice.org/2004/writer" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:drawooo="http://openoffice.org/2010/draw" xmlns:oooc="http://openoffice.org/2004/calc" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:calcext="urn:org:documentfoundation:names:experimental:calc:xmlns:calcext:1.0" xmlns:style="urn:oasis:names:tc:opendocument:xmlns:style:1.0" xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0" xmlns:of="urn:oasis:names:tc:opendocument:xmlns:of:1.2" xmlns:tableooo="http://openoffice.org/2009/table" xmlns:draw="urn:oasis:names:tc:opendocument:xmlns:drawing:1.0" xmlns:dr3d="urn:oasis:names:tc:opendocument:xmlns:dr3d:1.0" xmlns:rpt="http://openoffice.org/2005/report" xmlns:formx="urn:openoffice:names:experimental:ooxml-odf-interop:xmlns:form:1.0" xmlns:svg="urn:oasis:names:tc:opendocument:xmlns:svg-compatible:1.0" xmlns:chart="urn:oasis:names:tc:opendocument:xmlns:chart:1.0" xmlns:officeooo="http://openoffice.org/2009/office" xmlns:table="urn:oasis:names:tc:opendocument:xmlns:table:1.0" xmlns:field="urn:openoffice:names:experimental:ooo-ms-interop:xmlns:field:1.0" xmlns:number="urn:oasis:names:tc:opendocument:xmlns:datastyle:1.0" xmlns:meta="urn:oasis:names:tc:opendocument:xmlns:meta:1.0" xmlns:loext="urn:org:documentfoundation:names:experimental:office:xmlns:loext:1.0" office:version="1.3" office:mimetype="application/vnd.oasis.opendocument.text">
+ <office:meta><meta:creation-date>2024-08-01T21:09:25.469645393</meta:creation-date><meta:editing-duration>PT6M9S</meta:editing-duration><meta:editing-cycles>20</meta:editing-cycles><meta:generator>LibreOfficeDev/25.2.0.0.alpha0$Linux_X86_64 LibreOffice_project/cfd3b14fd4e1ee889dd356523e5cdaa639786d37</meta:generator><dc:date>2024-08-02T04:46:00.059644155</dc:date><meta:document-statistic meta:table-count="0" meta:image-count="0" meta:object-count="0" meta:page-count="1" meta:paragraph-count="1" meta:word-count="5" meta:character-count="17" meta:non-whitespace-character-count="13"/></office:meta>
+ <office:font-face-decls>
+  <style:font-face style:name="Amiri" svg:font-family="Amiri" style:font-pitch="variable"/>
+  <style:font-face style:name="Liberation Serif" svg:font-family="'Liberation Serif'" style:font-family-generic="roman" style:font-pitch="variable"/>
+  <style:font-face style:name="Noto Sans Arabic" svg:font-family="'Noto Sans Arabic'" style:font-family-generic="swiss" style:font-pitch="variable"/>
+  <style:font-face style:name="Tahoma" svg:font-family="Tahoma" style:font-family-generic="system" style:font-pitch="variable"/>
+ </office:font-face-decls>
+ <office:styles>
+  <style:default-style style:family="graphic">
+   <style:graphic-properties svg:stroke-color="#3465a4" draw:fill-color="#729fcf" fo:wrap-option="no-wrap" draw:shadow-offset-x="0.1181in" draw:shadow-offset-y="0.1181in" draw:start-line-spacing-horizontal="0.1114in" draw:start-line-spacing-vertical="0.1114in" draw:end-line-spacing-horizontal="0.1114in" draw:end-line-spacing-vertical="0.1114in" style:writing-mode="lr-tb" style:flow-with-text="false"/>
+   <style:paragraph-properties style:text-autospace="ideograph-alpha" style:line-break="strict" loext:tab-stop-distance="0in" style:writing-mode="lr-tb" style:font-independent-line-spacing="false">
+    <style:tab-stops/>
+   </style:paragraph-properties>
+   <style:text-properties style:use-window-font-color="true" loext:opacity="0%" style:font-name="Liberation Serif" fo:font-size="12pt" fo:language="en" fo:country="US" style:letter-kerning="true" style:font-name-asian="Tahoma" style:font-size-asian="10.5pt" style:language-asian="zh" style:country-asian="CN" style:font-name-complex="Tahoma" style:font-size-complex="12pt" style:language-complex="fa" style:country-complex="IR"/>
+  </style:default-style>
+  <style:default-style style:family="paragraph">
+   <style:paragraph-properties fo:orphans="2" fo:widows="2" fo:hyphenation-ladder-count="no-limit" fo:hyphenation-keep="auto" loext:hyphenation-keep-type="column" style:text-autospace="ideograph-alpha" style:punctuation-wrap="hanging" style:line-break="strict" style:tab-stop-distance="0.278in" style:writing-mode="page"/>
+   <style:text-properties style:use-window-font-color="true" loext:opacity="0%" style:font-name="Liberation Serif" fo:font-size="12pt" fo:language="en" fo:country="US" style:letter-kerning="true" style:font-name-asian="Tahoma" style:font-size-asian="10.5pt" style:language-asian="zh" style:country-asian="CN" style:font-name-complex="Tahoma" style:font-size-complex="12pt" style:language-complex="fa" style:country-complex="IR" fo:hyphenate="false" fo:hyphenation-remain-char-count="2" fo:hyphenation-push-char-count="2" loext:hyphenation-no-caps="false" loext:hyphenation-no-last-word="false" loext:hyphenation-word-char-count="5" loext:hyphenation-zone="no-limit"/>
+  </style:default-style>
+  <style:default-style style:family="table">
+   <style:table-properties table:border-model="collapsing"/>
+  </style:default-style>
+  <style:default-style style:family="table-row">
+   <style:table-row-properties fo:keep-together="auto"/>
+  </style:default-style>
+  <style:style style:name="Standard" style:family="paragraph" style:class="text"/>
+  <text:outline-style style:name="Outline">
+   <text:outline-level-style text:level="1" loext:num-list-format="%1%" style:num-format="">
+    <style:list-level-properties text:list-level-position-and-space-mode="label-alignment">
+     <style:list-level-label-alignment text:label-followed-by="listtab"/>
+    </style:list-level-properties>
+   </text:outline-level-style>
+   <text:outline-level-style text:level="2" loext:num-list-format="%2%" style:num-format="">
+    <style:list-level-properties text:list-level-position-and-space-mode="label-alignment">
+     <style:list-level-label-alignment text:label-followed-by="listtab"/>
+    </style:list-level-properties>
+   </text:outline-level-style>
+   <text:outline-level-style text:level="3" loext:num-list-format="%3%" style:num-format="">
+    <style:list-level-properties text:list-level-position-and-space-mode="label-alignment">
+     <style:list-level-label-alignment text:label-followed-by="listtab"/>
+    </style:list-level-properties>
+   </text:outline-level-style>
+   <text:outline-level-style text:level="4" loext:num-list-format="%4%" style:num-format="">
+    <style:list-level-properties text:list-level-position-and-space-mode="label-alignment">
+     <style:list-level-label-alignment text:label-followed-by="listtab"/>
+    </style:list-level-properties>
+   </text:outline-level-style>
+   <text:outline-level-style text:level="5" loext:num-list-format="%5%" style:num-format="">
+    <style:list-level-properties text:list-level-position-and-space-mode="label-alignment">
+     <style:list-level-label-alignment text:label-followed-by="listtab"/>
+    </style:list-level-properties>
+   </text:outline-level-style>
+   <text:outline-level-style text:level="6" loext:num-list-format="%6%" style:num-format="">
+    <style:list-level-properties text:list-level-position-and-space-mode="label-alignment">
+     <style:list-level-label-alignment text:label-followed-by="listtab"/>
+    </style:list-level-properties>
+   </text:outline-level-style>
+   <text:outline-level-style text:level="7" loext:num-list-format="%7%" style:num-format="">
+    <style:list-level-properties text:list-level-position-and-space-mode="label-alignment">
+     <style:list-level-label-alignment text:label-followed-by="listtab"/>
+    </style:list-level-properties>
+   </text:outline-level-style>
+   <text:outline-level-style text:level="8" loext:num-list-format="%8%" style:num-format="">
+    <style:list-level-properties text:list-level-position-and-space-mode="label-alignment">
+     <style:list-level-label-alignment text:label-followed-by="listtab"/>
+    </style:list-level-properties>
+   </text:outline-level-style>
+   <text:outline-level-style text:level="9" loext:num-list-format="%9%" style:num-format="">
+    <style:list-level-properties text:list-level-position-and-space-mode="label-alignment">
+     <style:list-level-label-alignment text:label-followed-by="listtab"/>
+    </style:list-level-properties>
+   </text:outline-level-style>
+   <text:outline-level-style text:level="10" loext:num-list-format="%10%" style:num-format="">
+    <style:list-level-properties text:list-level-position-and-space-mode="label-alignment">
+     <style:list-level-label-alignment text:label-followed-by="listtab"/>
+    </style:list-level-properties>
+   </text:outline-level-style>
+  </text:outline-style>
+  <text:notes-configuration text:note-class="footnote" style:num-format="1" text:start-value="0" text:footnotes-position="page" text:start-numbering-at="document"/>
+  <text:notes-configuration text:note-class="endnote" style:num-format="i" text:start-value="0"/>
+  <text:linenumbering-configuration text:number-lines="false" text:offset="0.1965in" style:num-format="1" text:number-position="left" text:increment="5"/>
+  </office:styles>
+ <office:automatic-styles>
+  <style:style style:name="P1" style:family="paragraph" style:parent-style-name="Standard">
+   <style:paragraph-properties fo:text-align="justify" style:justify-single-word="false" style:writing-mode="rl-tb"/>
+   <style:text-properties fo:font-size="28pt" style:font-size-asian="28pt" style:font-name-complex="Amiri" style:font-size-complex="28pt"/>
+  </style:style>
+  <style:style style:name="T1" style:family="text">
+   <style:text-properties/>
+  </style:style>
+  <style:style style:name="T2" style:family="text">
+   <style:text-properties/>
+  </style:style>
+  <style:style style:name="T3" style:family="text">
+   <style:text-properties/>
+  </style:style>
+  <style:style style:name="T4" style:family="text">
+   <style:text-properties fo:font-size="44pt" style:font-size-asian="44pt" style:font-name-complex="Noto Sans Arabic" style:font-size-complex="44pt"/>
+  </style:style>
+  <style:style style:name="T5" style:family="text">
+   <style:text-properties/>
+  </style:style>
+  <style:page-layout style:name="pm1">
+   <style:page-layout-properties fo:page-width="8.5in" fo:page-height="11in" style:num-format="1" style:print-orientation="portrait" fo:margin-top="0.7874in" fo:margin-bottom="0.7874in" fo:margin-left="0.7874in" fo:margin-right="0.7874in" style:writing-mode="lr-tb" style:layout-grid-color="#c0c0c0" style:layout-grid-lines="20" style:layout-grid-base-height="0.278in" style:layout-grid-ruby-height="0.139in" style:layout-grid-mode="none" style:layout-grid-ruby-below="false" style:layout-grid-print="false" style:layout-grid-display="false" style:footnote-max-height="0in" loext:margin-gutter="0in">
+    <style:footnote-sep style:width="0.0071in" style:distance-before-sep="0.0398in" style:distance-after-sep="0.0398in" style:line-style="solid" style:adjustment="left" style:rel-width="25%" style:color="#000000"/>
+   </style:page-layout-properties>
+   <style:header-style/>
+   <style:footer-style/>
+  </style:page-layout>
+  <style:style style:name="dp1" style:family="drawing-page">
+   <style:drawing-page-properties draw:background-size="full"/>
+  </style:style>
+ </office:automatic-styles>
+ <office:master-styles>
+  <style:master-page style:name="Standard" style:page-layout-name="pm1" draw:style-name="dp1"/>
+ </office:master-styles>
+ <office:body>
+  <office:text>
+   <text:sequence-decls>
+    <text:sequence-decl text:display-outline-level="0" text:name="Illustration"/>
+    <text:sequence-decl text:display-outline-level="0" text:name="Table"/>
+    <text:sequence-decl text:display-outline-level="0" text:name="Text"/>
+    <text:sequence-decl text:display-outline-level="0" text:name="Drawing"/>
+    <text:sequence-decl text:display-outline-level="0" text:name="Figure"/>
+   </text:sequence-decls>
+   <text:p text:style-name="P1"><text:span text:style-name="T1">A</text:span><text:span text:style-name="T2">a aa aa</text:span><text:span text:style-name="T3">a</text:span><text:span text:style-name="T2">a</text:span><text:span text:style-name="T1"> </text:span><text:span text:style-name="T4">﷽</text:span> <text:span text:style-name="T5">aaaa</text:span></text:p>
+  </office:text>
+ </office:body>
+</office:document>

sw/qa/extras/layout/layout3.cxx
@@ -3137,6 +3137,21 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter3, TestTdf157829RTL)
                 u"English עברית"_ustr);
 }
 
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter3, TestTdf162314)
+{
+    // Regression test for bidi portion line breaking where the portion layout ends with underflow,
+    // but the bidi portion should not be truncated.
+    createSwDoc("tdf162314.fodt");
+    auto pXmlDoc = parseLayoutDump();
+
+    assertXPath(pXmlDoc, "//page"_ostr, 1);
+
+    assertXPath(pXmlDoc, "/root/page/body/txt/SwParaPortion/SwLineLayout[1]"_ostr, "portion"_ostr,
+                u"Aa aa aaaa ﷽ "_ustr);
+    assertXPath(pXmlDoc, "/root/page/body/txt/SwParaPortion/SwLineLayout[2]"_ostr, "portion"_ostr,
+                u"aaaa"_ustr);
+}
+
 } // end of anonymous namespace
 
 CPPUNIT_PLUGIN_IMPLEMENT();