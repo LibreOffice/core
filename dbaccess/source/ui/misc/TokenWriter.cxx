LibreOffice/core

commit af5a1b5925dbc816dced8813282d953fd5adbaaa

Drop uses of css::uno::Sequence::getConstArray in dbaccess
where it was obsoleted by commits 2484de6728bd11bb7949003d112f1ece2223c7a1
(Remove non-const Sequence::begin()/end() in internal code, 2021-10-15) and
fb3c04bd1930eedacd406874e1a285d62bbf27d9 (Drop non-const Sequence::operator[]
in internal code 2021-11-05).

Change-Id: I4f8dc4f430dc49fa29012b8f064094daceb5e1b4
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/166818
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
Tested-by: Jenkins

dbaccess/source/ui/misc/TokenWriter.cxx
@@ -382,7 +382,6 @@ bool ORTFImportExport::Write()
         Reference<XColumnsSupplier> xColSup(m_xObject,UNO_QUERY);
         Reference<XNameAccess> xColumns = xColSup->getColumns();
         Sequence< OUString> aNames(xColumns->getElementNames());
-        const OUString* pIter = aNames.getConstArray();
 
         sal_Int32 nCount = aNames.getLength();
         bool bUseResultMetaData = false;
@@ -413,11 +412,10 @@ bool ORTFImportExport::Write()
                 sColumnName = m_xResultSetMetaData->getColumnName(i);
             else
             {
-                sColumnName = *pIter;
+                sColumnName = aNames[i];
                 Reference<XPropertySet> xColumn;
                 xColumns->getByName(sColumnName) >>= xColumn;
                 xColumn->getPropertyValue(PROPERTY_ALIGN) >>= nAlign;
-                ++pIter;
             }
 
             const char* pChar;
@@ -458,25 +456,22 @@ bool ORTFImportExport::Write()
         sal_Int32 kk=0;
         if ( m_aSelection.hasElements() )
         {
-            const Any* pSelIter = m_aSelection.getConstArray();
-            const Any* pEnd   = pSelIter + m_aSelection.getLength();
-
-            bool bContinue = true;
-            for( ; pSelIter != pEnd && bContinue; ++pSelIter )
+            for (auto& any : m_aSelection)
             {
                 if ( m_bBookmarkSelection )
                 {
-                    bContinue = m_xRowLocate->moveToBookmark( *pSelIter );
+                    if (!m_xRowLocate->moveToBookmark(any))
+                        break;
                 }
                 else
                 {
                     sal_Int32 nPos = -1;
-                    OSL_VERIFY( *pSelIter >>= nPos );
-                    bContinue = ( m_xResultSet->absolute( nPos ) );
+                    OSL_VERIFY(any >>= nPos);
+                    if (!m_xResultSet->absolute(nPos))
+                        break;
                 }
 
-                if ( bContinue )
-                    appendRow( pHorzChar.get(), nCount, k, kk );
+                appendRow(pHorzChar.get(), nCount, k, kk);
             }
         }
         else
@@ -755,10 +750,7 @@ void OHTMLImportExport::WriteTables()
         m_xObject->getPropertyValue(PROPERTY_ROW_HEIGHT) >>= nHeight;
 
         // 1. writing the column description
-        const OUString* pIter = aNames.getConstArray();
-        const OUString* pEnd = pIter + aNames.getLength();
-
-        for( sal_Int32 i=0;pIter != pEnd; ++pIter,++i )
+        for (sal_Int32 i = 0; i < aNames.getLength(); ++i)
         {
             sal_Int32 nAlign = 0;
             pFormat[i] = 0;
@@ -766,7 +758,7 @@ void OHTMLImportExport::WriteTables()
             if ( !bUseResultMetaData )
             {
                 Reference<XPropertySet> xColumn;
-                xColumns->getByName(*pIter) >>= xColumn;
+                xColumns->getByName(aNames[i]) >>= xColumn;
                 xColumn->getPropertyValue(PROPERTY_ALIGN) >>= nAlign;
                 pFormat[i] = ::comphelper::getINT32(xColumn->getPropertyValue(PROPERTY_FORMATKEY));
                 pColWidth[i] = ::comphelper::getINT32(xColumn->getPropertyValue(PROPERTY_WIDTH));
@@ -782,7 +774,7 @@ void OHTMLImportExport::WriteTables()
             if(i == aNames.getLength()-1)
                 IncIndent(-1);
 
-            WriteCell(pFormat[i],pColWidth[i],nHeight,pHorJustify[i],*pIter,OOO_STRING_SVTOOLS_HTML_tableheader);
+            WriteCell(pFormat[i],pColWidth[i],nHeight,pHorJustify[i],aNames[i],OOO_STRING_SVTOOLS_HTML_tableheader);
         }
 
         IncIndent(-1);

dbaccess/source/core/api/FilteredContainer.cxx
@@ -71,15 +71,11 @@ static sal_Int32 createWildCardVector(Sequence< OUString >& _rTableFilter, std::
     return nShiftPos;
 }
 
-    static bool lcl_isElementAllowed(  const OUString& _rName,
+    static bool lcl_isElementAllowed(std::u16string_view _rName,
                                 const Sequence< OUString >& _rTableFilter,
                                 const std::vector< WildCard >& _rWCSearch )
     {
-        sal_Int32 nTableFilterLen = _rTableFilter.getLength();
-
-        const OUString* tableFilter = _rTableFilter.getConstArray();
-        const OUString* tableFilterEnd = _rTableFilter.getConstArray() + nTableFilterLen;
-        bool bFilterMatch = std::find( tableFilter, tableFilterEnd, _rName ) != tableFilterEnd;
+        bool bFilterMatch = std::find(_rTableFilter.begin(), _rTableFilter.end(), _rName) != _rTableFilter.end();
         // the table is allowed to "pass" if we had no filters at all or any of the non-wildcard filters matches
         if (!bFilterMatch && !_rWCSearch.empty())
         {   // or if one of the wildcard expression matches
@@ -207,15 +203,12 @@ static sal_Int32 createWildCardVector(Sequence< OUString >& _rTableFilter, std::
             TableInfos aUnfilteredTables;
             aUnfilteredTables.swap( aFilteredTables );
 
-            const OUString* pTableTypeFilterBegin = _tableTypeFilter.getConstArray();
-            const OUString* pTableTypeFilterEnd = pTableTypeFilterBegin + _tableTypeFilter.getLength();
-
             for (auto & unfilteredTable : aUnfilteredTables)
             {
                 // ensure that we know the table type
                 lcl_ensureType( unfilteredTable, _metaData, _masterContainer );
 
-                if ( std::find( pTableTypeFilterBegin, pTableTypeFilterEnd, *unfilteredTable.sType ) != pTableTypeFilterEnd )
+                if (std::find(_tableTypeFilter.begin(), _tableTypeFilter.end(), *unfilteredTable.sType) != _tableTypeFilter.end())
                     aFilteredTables.push_back(unfilteredTable);
             }
         }
@@ -267,11 +260,10 @@ static sal_Int32 createWildCardVector(Sequence< OUString >& _rTableFilter, std::
 
             TableInfos aUnfilteredTables;
 
-            Sequence< OUString > aNames = m_xMasterContainer->getElementNames();
-            const OUString*  name = aNames.getConstArray();
-            const OUString*  nameEnd = name + aNames.getLength();
-            for ( ; name != nameEnd; ++name )
-                aUnfilteredTables.emplace_back( *name );
+            Sequence<OUString> aNames = m_xMasterContainer->getElementNames();
+            aUnfilteredTables.reserve(aNames.getLength());
+            for (auto& name : aNames)
+                aUnfilteredTables.emplace_back(name);
 
             reFill( lcl_filter( std::move(aUnfilteredTables),
                 _rTableFilter, _rTableTypeFilter, m_xMetaData, m_xMasterContainer ) );
@@ -308,14 +300,9 @@ static sal_Int32 createWildCardVector(Sequence< OUString >& _rTableFilter, std::
             {
                 if ( _rTableTypeFilter.hasElements() )
                 {
-                    const OUString* tableType    = _rTableTypeFilter.getConstArray();
-                    const OUString* tableTypeEnd = tableType + _rTableTypeFilter.getLength();
-                    for ( ; tableType != tableTypeEnd; ++tableType )
-                    {
-                        if ( *tableType == sInherentTableTypeRestriction )
-                            break;
-                    }
-                    if ( tableType == tableTypeEnd )
+                    if (std::find(_rTableTypeFilter.begin(), _rTableTypeFilter.end(),
+                                  sInherentTableTypeRestriction)
+                        == _rTableTypeFilter.end())
                     {   // the only table type which can be part of this container is not allowed
                         // by the externally provided table type filter.
                         m_bConstructed = true;

dbaccess/source/core/api/KeySet.cxx
@@ -293,13 +293,11 @@ void OKeySet::construct(const Reference< XResultSet>& _xDriverSet, const OUStrin
     const Sequence< OUString> aSeq = xSelectTables->getElementNames();
     if ( aSeq.getLength() > 1 ) // special handling for join
     {
-        const OUString* pIter = aSeq.getConstArray();
-        const OUString* const pEnd  = pIter + aSeq.getLength();
-        for(;pIter != pEnd;++pIter)
+        for (auto& name : aSeq)
         {
-            if ( *pIter != m_sUpdateTableName )
+            if (name != m_sUpdateTableName)
             {
-                connectivity::OSQLTable xSelColSup(xSelectTables->getByName(*pIter),uno::UNO_QUERY);
+                connectivity::OSQLTable xSelColSup(xSelectTables->getByName(name), uno::UNO_QUERY);
                 Reference<XPropertySet> xProp(xSelColSup,uno::UNO_QUERY);
                 OUString sSelectTableName = ::dbtools::composeTableName( xMeta, xProp, ::dbtools::EComposeRule::InDataManipulation, false );
 
@@ -375,19 +373,14 @@ void OKeySet::executeStatement(OUStringBuffer& io_aFilter, Reference<XSingleSele
     io_xAnalyzer->setFilter(io_aFilter.makeStringAndClear());
     if ( bFilterSet )
     {
-        Sequence< Sequence< PropertyValue > > aFilter2 = io_xAnalyzer->getStructuredFilter();
-        const Sequence< PropertyValue >* pOr = aFilter2.getConstArray();
-        const Sequence< PropertyValue >* pOrEnd = pOr + aFilter2.getLength();
-        for(;pOr != pOrEnd;++pOr)
+        for (auto& rOr : io_xAnalyzer->getStructuredFilter())
         {
-            const PropertyValue* pAnd = pOr->getConstArray();
-            const PropertyValue* pAndEnd = pAnd + pOr->getLength();
-            for(;pAnd != pAndEnd;++pAnd)
+            for (auto& rAnd : rOr)
             {
                 OUString sValue;
-                if ( !(pAnd->Value >>= sValue) || !( sValue == "?" || sValue.startsWith( ":" ) ) )
+                if (!(rAnd.Value >>= sValue) || !(sValue == "?" || sValue.startsWith(":")))
                 { // we have a criteria which has to be taken into account for updates
-                    m_aFilterColumns.push_back(pAnd->Name);
+                    m_aFilterColumns.push_back(rAnd.Name);
                 }
             }
         }
@@ -1386,27 +1379,22 @@ void getColumnPositions(const Reference<XNameAccess>& _rxQueryColumns,
                             bool i_bAppendTableName)
     {
         // get the real name of the columns
-        Sequence< OUString> aSelNames(_rxQueryColumns->getElementNames());
-        const OUString* pSelIter     = aSelNames.getConstArray();
-        const OUString* pSelEnd      = pSelIter + aSelNames.getLength();
-
-        const OUString* pTblColumnIter   = _aColumnNames.getConstArray();
-        const OUString* pTblColumnEnd    = pTblColumnIter + _aColumnNames.getLength();
-
         ::comphelper::UStringMixEqual bCase(o_rColumnNames.key_comp().isCaseSensitive());
 
-        for(sal_Int32 nPos = 1;pSelIter != pSelEnd;++pSelIter,++nPos)
+        sal_Int32 nPos = 0;
+        for (auto& queryColumnName : _rxQueryColumns->getElementNames())
         {
-            Reference<XPropertySet> xQueryColumnProp(_rxQueryColumns->getByName(*pSelIter),UNO_QUERY_THROW);
+            ++nPos;
+            Reference<XPropertySet> xQueryColumnProp(_rxQueryColumns->getByName(queryColumnName),UNO_QUERY_THROW);
             OUString sRealName,sTableName;
             OSL_ENSURE(xQueryColumnProp->getPropertySetInfo()->hasPropertyByName(PROPERTY_REALNAME),"Property REALNAME not available!");
             OSL_ENSURE(xQueryColumnProp->getPropertySetInfo()->hasPropertyByName(PROPERTY_TABLENAME),"Property TABLENAME not available!");
             xQueryColumnProp->getPropertyValue(PROPERTY_REALNAME)   >>= sRealName;
             xQueryColumnProp->getPropertyValue(PROPERTY_TABLENAME)  >>= sTableName;
 
-            for(;pTblColumnIter != pTblColumnEnd;++pTblColumnIter)
+            for (auto& tableColumnName : _aColumnNames)
             {
-                if(bCase(sRealName,*pTblColumnIter) && bCase(_rsUpdateTableName,sTableName) && o_rColumnNames.find(*pTblColumnIter) == o_rColumnNames.end())
+                if(bCase(sRealName,tableColumnName) && bCase(_rsUpdateTableName,sTableName) && o_rColumnNames.find(tableColumnName) == o_rColumnNames.end())
                 {
                     sal_Int32 nType = 0;
                     xQueryColumnProp->getPropertyValue(PROPERTY_TYPE)   >>= nType;
@@ -1436,7 +1424,6 @@ void getColumnPositions(const Reference<XNameAccess>& _rxQueryColumns,
                     break;
                 }
             }
-            pTblColumnIter = _aColumnNames.getConstArray();
         }
     }
 }

dbaccess/source/core/api/OptimisticSet.cxx
@@ -96,13 +96,10 @@ void OptimisticSet::construct(const Reference< XResultSet>& _xDriverSet,const OU
     const Reference<XNameAccess> xQueryColumns = xQueryColSup->getColumns();
     const Reference<XTablesSupplier> xTabSup(m_xComposer,UNO_QUERY);
     const Reference<XNameAccess> xTables = xTabSup->getTables();
-    const Sequence< OUString> aTableNames = xTables->getElementNames();
-    const OUString* pTableNameIter = aTableNames.getConstArray();
-    const OUString* pTableNameEnd = pTableNameIter + aTableNames.getLength();
-    for( ; pTableNameIter != pTableNameEnd ; ++pTableNameIter)
+    for (auto& tableName : xTables->getElementNames())
     {
         std::unique_ptr<SelectColumnsMetaData> pKeyColumNames(new SelectColumnsMetaData(bCase));
-        findTableColumnsMatching_throw(xTables->getByName(*pTableNameIter),*pTableNameIter,xMeta,xQueryColumns,pKeyColumNames);
+        findTableColumnsMatching_throw(xTables->getByName(tableName),tableName,xMeta,xQueryColumns,pKeyColumNames);
         m_pKeyColumnNames->insert(pKeyColumNames->begin(),pKeyColumNames->end());
     }
 

dbaccess/source/core/api/RowSet.cxx
@@ -1927,7 +1927,7 @@ void ORowSet::execute_NoApprove_NoNewConn(ResettableMutexGuard& _rClearForNotifi
                             Sequence< OUString> aSeq = m_xColumns->getElementNames();
                             if( i <= aSeq.getLength())
                             {
-                                m_xColumns->getByName(aSeq.getConstArray()[i-1]) >>= xColumn;
+                                m_xColumns->getByName(aSeq[i-1]) >>= xColumn;
                             }
                         }
                     }
@@ -2085,25 +2085,23 @@ Sequence< sal_Int32 > SAL_CALL ORowSet::deleteRows( const Sequence< Any >& rows 
     notifyAllListenersRowBeforeChange(aGuard,aEvt);
 
     Sequence< sal_Int32 > aResults( rows.getLength() );
-    const Any* row = rows.getConstArray();
-    const Any* rowEnd = rows.getConstArray() + rows.getLength();
     sal_Int32* result = aResults.getArray();
-    for ( ; row != rowEnd; ++row, ++result )
+    for (sal_Int32 i = 0; i < rows.getLength(); ++i)
     {
-        *result = 0;
-        if ( !m_pCache->moveToBookmark( *row ) )
+        result[i] = 0;
+        if (!m_pCache->moveToBookmark(rows[i]))
             continue;
         sal_Int32 nDeletePosition = m_pCache->getRow();
 
         // first notify the clones so that they can save their position
-        notifyRowSetAndClonesRowDelete( *row );
+        notifyRowSetAndClonesRowDelete(rows[i]);
 
         // now delete the row
         if ( !m_pCache->deleteRow() )
             continue;
-        *result = 1;
+        result[i] = 1;
         // now notify that we have deleted
-        notifyRowSetAndClonesRowDeleted( *row, nDeletePosition );
+        notifyRowSetAndClonesRowDeleted(rows[i], nDeletePosition);
     }
     aEvt.Rows = aResults.getLength();
 
@@ -2752,13 +2750,13 @@ ORowSetClone::ORowSetClone( const Reference<XComponentContext>& _rContext, ORowS
     if ( rParent.m_pColumns )
     {
         Sequence< OUString> aSeq = rParent.m_pColumns->getElementNames();
-        const OUString* pIter    = aSeq.getConstArray();
-        const OUString* pEnd     = pIter + aSeq.getLength();
         aColumns->reserve(aSeq.getLength()+1);
-        for(sal_Int32 i=1;pIter != pEnd ;++pIter,++i)
+        sal_Int32 i = 0;
+        for (auto& columnName : aSeq)
         {
+            ++i;
             Reference<XPropertySet> xColumn;
-            rParent.m_pColumns->getByName(*pIter) >>= xColumn;
+            rParent.m_pColumns->getByName(columnName) >>= xColumn;
             if(xColumn->getPropertySetInfo()->hasPropertyByName(PROPERTY_DESCRIPTION))
                 aDescription = comphelper::getString(xColumn->getPropertyValue(PROPERTY_DESCRIPTION));
 
@@ -2774,8 +2772,8 @@ ORowSetClone::ORowSetClone( const Reference<XComponentContext>& _rContext, ORowS
                     return this->getValue(column);
                 });
             aColumns->emplace_back(pColumn);
-            pColumn->setName(*pIter);
-            aNames.push_back(*pIter);
+            pColumn->setName(columnName);
+            aNames.push_back(columnName);
             m_aDataColumns.push_back(pColumn.get());
 
             pColumn->setFastPropertyValue_NoBroadcast(PROPERTY_ID_ALIGN,xColumn->getPropertyValue(PROPERTY_ALIGN));

dbaccess/source/core/api/RowSetCache.cxx
@@ -279,18 +279,15 @@ ORowSetCache::ORowSetCache(const Reference< XResultSet >& _xRs,
             m_nPrivileges = Privilege::SELECT;
             bool bNoInsert = false;
 
-            Sequence< OUString> aNames(xColumns->getElementNames());
-            const OUString* pIter    = aNames.getConstArray();
-            const OUString* pEnd     = pIter + aNames.getLength();
-            for(;pIter != pEnd;++pIter)
+            for (auto& columnName : xColumns->getElementNames())
             {
-                Reference<XPropertySet> xColumn(xColumns->getByName(*pIter),UNO_QUERY);
+                Reference<XPropertySet> xColumn(xColumns->getByName(columnName), UNO_QUERY);
                 OSL_ENSURE(xColumn.is(),"Column in table is null!");
                 if(xColumn.is())
                 {
                     sal_Int32 nNullable = 0;
                     xColumn->getPropertyValue(PROPERTY_ISNULLABLE) >>= nNullable;
-                    if(nNullable == ColumnValue::NO_NULLS && aColumnNames.find(*pIter) == aColumnNames.end())
+                    if(nNullable == ColumnValue::NO_NULLS && aColumnNames.find(columnName) == aColumnNames.end())
                     { // we found a column where null is not allowed so we can't insert new values
                         bNoInsert = true;
                         break; // one column is enough

dbaccess/source/core/api/SingleSelectQueryComposer.cxx
@@ -1364,8 +1364,8 @@ OUString OSingleSelectQueryComposer::getTableAlias(const Reference< XPropertySet
         column->getPropertyValue(PROPERTY_NAME)         >>= aColumnName;
 
         Sequence< OUString> aNames(m_pTables->getElementNames());
-        const OUString* pBegin     = aNames.getConstArray();
-        const OUString* const pEnd = pBegin + aNames.getLength();
+        const OUString* pBegin     = aNames.begin();
+        const OUString* const pEnd = aNames.end();
 
         if(aTable.isEmpty())
         { // we haven't found a table name, now we must search every table for this column
@@ -1564,21 +1564,22 @@ namespace
         std::u16string_view rQuoteString)
     {
         OUStringBuffer sRet;
-        const Sequence< PropertyValue >* pOrIter = filter.getConstArray();
-        const Sequence< PropertyValue >* pOrEnd = pOrIter + filter.getLength();
-        while ( pOrIter != pOrEnd )
+        for (auto& rOr : filter)
         {
-            if ( pOrIter->hasElements() )
+            if (rOr.hasElements())
             {
+                if (!sRet.isEmpty())
+                    sRet.append(STR_OR);
                 sRet.append(L_BRACKET);
-                const PropertyValue* pAndIter = pOrIter->getConstArray();
-                const PropertyValue* pAndEnd = pAndIter + pOrIter->getLength();
-                while ( pAndIter != pAndEnd )
+                OUStringBuffer sAnd;
+                for (auto& rAnd : rOr)
                 {
-                    sRet.append(pAndIter->Name);
+                    if (!sAnd.isEmpty())
+                        sAnd.append(STR_AND);
+                    sAnd.append(rAnd.Name);
                     OUString sValue;
-                    pAndIter->Value >>= sValue;
-                    const OUString sColumnName = lcl_getDecomposedColumnName( pAndIter->Name, rQuoteString );
+                    rAnd.Value >>= sValue;
+                    const OUString sColumnName = lcl_getDecomposedColumnName( rAnd.Name, rQuoteString );
                     if ( i_xSelectColumns.is() && i_xSelectColumns->hasByName(sColumnName) )
                     {
                         Reference<XPropertySet> xColumn(i_xSelectColumns->getByName(sColumnName),UNO_QUERY);
@@ -1586,18 +1587,12 @@ namespace
                     }
                     else
                     {
-                        sValue = i_aPredicateInputController.getPredicateValueStr(pAndIter->Name,sValue);
+                        sValue = i_aPredicateInputController.getPredicateValueStr(rAnd.Name,sValue);
                     }
-                    lcl_addFilterCriteria_throw(pAndIter->Handle,sValue,sRet);
-                    ++pAndIter;
-                    if ( pAndIter != pAndEnd )
-                        sRet.append(STR_AND);
+                    lcl_addFilterCriteria_throw(rAnd.Handle, sValue, sAnd);
                 }
-                sRet.append(R_BRACKET);
+                sRet.append(OUString::unacquired(sAnd) + R_BRACKET);
             }
-            ++pOrIter;
-            if ( pOrIter != pOrEnd && !sRet.isEmpty() )
-                sRet.append(STR_OR);
         }
         return sRet.makeStringAndClear();
     }
@@ -1726,12 +1721,8 @@ void OSingleSelectQueryComposer::setConditionByColumn( const Reference< XPropert
                         aSQL.append( "\'" );
                     }
                     aSQL.append( "0x" );
-                    const sal_Int8* pBegin  = aSeq.getConstArray();
-                    const sal_Int8* pEnd    = pBegin + aSeq.getLength();
-                    for(;pBegin != pEnd;++pBegin)
-                    {
-                        aSQL.append( static_cast<sal_Int32>(*pBegin), 16 );
-                    }
+                    for (sal_Int32 byte : aSeq)
+                        aSQL.append(byte, 16);
                     if(nSearchable == ColumnSearch::CHAR)
                         aSQL.append( "\'" );
                 }

dbaccess/source/core/api/View.cxx
@@ -85,13 +85,9 @@ namespace dbaccess
         std::vector<Type> aOwnTypes;
         aOwnTypes.reserve(aTypes.getLength());
 
-        const Type* pIter = aTypes.getConstArray();
-        const Type* pEnd = pIter + aTypes.getLength();
-        for(;pIter != pEnd ;++pIter)
-        {
-            if( *pIter != aAlterType || m_xViewAccess.is() )
-                aOwnTypes.push_back(*pIter);
-        }
+        for (auto& type : aTypes)
+            if (m_xViewAccess || type != aAlterType)
+                aOwnTypes.push_back(type);
 
         return Sequence< Type >(aOwnTypes.data(), aOwnTypes.size());
     }

commit aed5979ecc6befd64a024ba07064696f24640fec

Fix off-by-1 error
Regression after commit af5a1b5925dbc816dced8813282d953fd5adbaaa
Drop uses of css::uno::Sequence::getConstArray in dbaccess, 2024-04-30.

Change-Id: Ifaecdeb2898f2e9089b1f893a1e9ba46352cc353
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/171505
Tested-by: Jenkins
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

dbaccess/source/ui/misc/TokenWriter.cxx
@@ -412,7 +412,7 @@ bool ORTFImportExport::Write()
                 sColumnName = m_xResultSetMetaData->getColumnName(i);
             else
             {
-                sColumnName = aNames[i];
+                sColumnName = aNames[i - 1];
                 Reference<XPropertySet> xColumn;
                 xColumns->getByName(sColumnName) >>= xColumn;
                 xColumn->getPropertyValue(PROPERTY_ALIGN) >>= nAlign;