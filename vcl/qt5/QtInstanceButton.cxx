c336d044357d14f40644b0db321e66132203e959
Michael Weghorn <m.weghorn@posteo.de>
2024-08-06 14:58:12 +0200

    tdf#162351 tdf#130857 qt weld: Add basic QtInstanceButton
    Add a very basic native Qt implementation for `weld::Button`.
    It currently doesn't really implement many methods and
    just triggers an assert when they get called.
    
    Change-Id: I7a15266e978ca3bb030c847822a82d8d58c4c189
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/171549
    Tested-by: Jenkins
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>
    (cherry picked from commit f0ad3a95c1e841281048a784bd96e8d34e090c5b)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/171532
    Reviewed-by: Adolfo Jayme Barrientos <fitojb@ubuntu.com>

---
+++ vcl/qt5/QtInstanceButton.cxx
@@ -0,0 +1,56 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <QtInstanceButton.hxx>
+
+QtInstanceButton::QtInstanceButton(QPushButton* pButton)
+    : QtInstanceWidget(pButton)
+    , m_pButton(pButton)
+{
+    assert(m_pButton);
+}
+
+void QtInstanceButton::set_label(const OUString& rText)
+{
+    assert(m_pButton);
+    m_pButton->setText(toQString(rText));
+}
+
+void QtInstanceButton::set_image(VirtualDevice* /*pDevice*/)
+{
+    assert(false && "Not implemented yet");
+}
+
+void QtInstanceButton::set_image(const css::uno::Reference<css::graphic::XGraphic>& /*rImage*/)
+{
+    assert(false && "Not implemented yet");
+}
+
+void QtInstanceButton::set_from_icon_name(const OUString& /*rIconName*/)
+{
+    assert(false && "Not implemented yet");
+}
+
+OUString QtInstanceButton::get_label() const
+{
+    assert(m_pButton);
+    return toOUString(m_pButton->text());
+}
+
+void QtInstanceButton::set_font(const vcl::Font& /*rFont*/)
+{
+    assert(false && "Not implemented yet");
+}
+
+void QtInstanceButton::set_custom_button(VirtualDevice* /*pDevice*/)
+{
+    assert(false && "Not implemented yet");
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
